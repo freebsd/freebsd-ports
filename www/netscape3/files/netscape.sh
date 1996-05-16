8d808e32fd6ab4dd3de03ea9167eb5be6da5366e
Jordan K. Hubbard <jkh@FreeBSD.org>
1996-05-16 01:38:49 +0000

    Explicitly initialize CLASSPATH to avoid confusion. Submitted by:	Brian Clapper <bmc@telebase.com>

--- www/netscape3/files/netscape.sh
+++ www/netscape3/files/netscape.sh
@@ -2,4 +2,5 @@
 export XKEYSYMDB ; XKEYSYMDB=@X11BASE@/lib/X11/XKeysymDB
 export XNLSPATH  ; XNLSPATH=@X11BASE@/lib/X11/nls
 export XAPPLRESDIR ; XAPPLRESDIR=@X11BASE@/lib/X11/app-defaults
+export CLASSPATH ; CLASSPATH=.:/usr/local/lib/netscape
 exec /usr/local/lib/netscape/netscape.bin $*
