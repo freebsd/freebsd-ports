959d95bcd7306a7fbb6fcc2d2f1a243ec2287f40
andreas <andreas@FreeBSD.org>
1997-02-22 14:56:46 +0000

    New port dnews 2.7q. This is the last official release of dnews and it's a FreeBSD binary ;-) Remember 4 weeks trial, after that you have to get a licence, even if you are a school or univerity, which get it for free.

---
+++ news/dnews/files/dnews.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# start dnews if it's really there ... usually in /usr/local ...
+
+[ -x /usr/local/dnews/dnews_start ] \
+	&& /usr/local/dnews/dnews_start > /dev/null 2>&1 &
+
+# dnews started successfully if exit status = 0
+
+if [ $? -eq 0 ]; then
+	echo -n ' dnews'
+fi
