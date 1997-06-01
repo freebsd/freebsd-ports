81ef827ef3de5b03537ced439664bf6c3c3cdc6d
James FitzGibbon <jfitz@FreeBSD.org>
1997-06-01 20:38:13 +0000

    Fixed perl-style variable usage in a sh script PR: Reviewed by: Submitted by: Obtained from:

--- games/quakeserver/files/qserver.sh
+++ games/quakeserver/files/qserver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id$
+# $Id: qserver.sh,v 1.1 1997/05/31 00:52:03 jfitz Exp $
 #
 
 QSERVER=%%PREFIX%%/quakeserver/unixded
@@ -15,15 +15,15 @@ QARGS=
 # A few other ideas/options:
 
 # To run a game better suited for modem play:
-#$QARGS=	+sys_ticrate 0.1
+#QARGS=	+sys_ticrate 0.1
 
 # To run a registered game (assuming that you've put the registered *.pak
 # files into %%PREFIX%%/quakeserver/reg/
-#$QARGS=	-game reg
+#QARGS=	-game reg
 
 # To force the server to bind to just one IP address (to run multiple
 # servers on the same machine)
-#$QARGS=	-ip x.x.x.x
+#QARGS=	-ip x.x.x.x
 
 # For more ideas, look at %%PREFIX%%/quakeserver/readme.unixded as well as
 # the documentation that comes with Quake.
