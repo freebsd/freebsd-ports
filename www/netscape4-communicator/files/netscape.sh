a892406ec5b666c7de2557b60a6508415e8837f4
ache <ache@FreeBSD.org>
1997-06-01 00:06:22 +0000

    Upgrade to 4.0b5

--- www/netscape4-communicator/files/netscape.sh
+++ www/netscape4-communicator/files/netscape.sh
@@ -2,5 +2,6 @@
 export XKEYSYMDB ; XKEYSYMDB=@X11BASE@/lib/X11/XKeysymDB
 export XNLSPATH  ; XNLSPATH=@X11BASE@/lib/X11/nls
 export XAPPLRESDIR ; XAPPLRESDIR=@X11BASE@/lib/X11/app-defaults
-export CLASSPATH ; CLASSPATH=.:/usr/local/lib/netscape
-exec /usr/local/lib/netscape/netscape.bin $*
+export MOZILLA_HOME; MOZILLA_HOME=${MOZILLA_HOME:=@PREFIX@/lib/netscape}
+export CLASSPATH ; CLASSPATH=.:$MOZILLA_HOME
+exec $MOZILLA_HOME/netscape.bin $*
