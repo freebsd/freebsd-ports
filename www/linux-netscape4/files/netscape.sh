985e1a2c32de271f14cc2c07c42ad879ccbafe35
Andrey A. Chernov <ache@FreeBSD.org>
1997-07-03 01:06:26 +0000

    Linux port because BSDI one cause division by zero

---
+++ www/linux-netscape4/files/netscape.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export XKEYSYMDB ; XKEYSYMDB=@X11BASE@/lib/X11/XKeysymDB
+export XNLSPATH  ; XNLSPATH=@X11BASE@/lib/X11/nls
+export XAPPLRESDIR ; XAPPLRESDIR=@X11BASE@/lib/X11/app-defaults
+export MOZILLA_HOME; MOZILLA_HOME=${MOZILLA_HOME:=@PREFIX@/lib/netscape}
+export CLASSPATH ; CLASSPATH=.:$MOZILLA_HOME
+exec $MOZILLA_HOME/netscape.bin $*
