4de244cf34383dcb0c18ed91211d7b2338b75b4f
jdp <jdp@FreeBSD.org>
1997-07-16 20:59:55 +0000

    Direct cvsupd's stdout and stderr into "/var/tmp/cvsupd.out" for better crash diagnosis.  May it never get tested, ha ha ha.
    Eliminate the lite2 and smp releases of the src-sys collection.
    
    Add the new mail-archive collection.
    
    Run cvsup with the "-1" flag to disable automatic retries.  Let cron
    take care of that.
    
    State in DESCR that version 15.1 or later of CVSup is required.

--- net/cvsup-mirror/files/cvsupd.sh
+++ net/cvsup-mirror/files/cvsupd.sh
@@ -5,6 +5,7 @@ if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/cvsupd\.sh\$"); then
     exit 1
 fi
 base=${PREFIX}/etc/cvsup
+out=/var/tmp/cvsupd.out
 
 export PATH=/bin:/usr/bin:${PREFIX}/sbin
 umask 2
@@ -13,4 +14,5 @@ test -x ${PREFIX}/sbin/cvsupd || exit 1
 echo -n " cvsupd"
 cd ${base} || exit
 . config.sh || exit
-su -m ${user} -c "cvsupd -C ${maxclients} -l @${facility}"
+su -m ${user} -c \
+    "cvsupd -e -C ${maxclients} -l @${facility}" >>${out} 2>&1
