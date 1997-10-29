2fbae568d97e3fbefe19c82d4b7b76c8def0c49c
tg <tg@FreeBSD.org>
1997-10-29 09:07:09 +0000

    Import atari800 port. This is an emulator of the old 8bit computer.
    PR:		3411
    Submitted by:	Joel Sutton <sutton@aardvark.apana.org.au>

---
+++ emulators/atari800/files/config.h
@@ -0,0 +1,19 @@
+/* 
+     FreeBSD ports collection version of config.h 
+     by Joel Sutton 22nd April 1997
+
+     This file has been created to simplify installation of the Atari
+     Emulator. Voxware does work and if you wish to install it
+     uncomment the #define line below. Other options are not yet
+     supported.
+
+ */
+
+#ifndef __CONFIG__
+#define __CONFIG__
+/* #define LINUX_JOYSTICK */
+/* #define DIRECT_VIDEO   */
+/* #define JOYMOUSE       */
+/* #define VOXWARE        */
+#define UNALIGNED_LONG_OK
+#endif
