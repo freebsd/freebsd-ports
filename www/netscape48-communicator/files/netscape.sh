a92f9bf4d07087ccae2249455373a4ed34f727d6
asami <asami@FreeBSD.org>
1997-11-15 13:09:43 +0000

    Split netscape4 into communicator and navigator, so the two maintainers shall never run into each other again.

--- www/netscape48-communicator/files/netscape.sh
+++ www/netscape48-communicator/files/netscape.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export MOZILLA_HOME; MOZILLA_HOME=${MOZILLA_HOME:=@PREFIX@/lib/netscape}
 export CLASSPATH ; CLASSPATH=.:$MOZILLA_HOME
-exec $MOZILLA_HOME/netscape.bin $*
+exec $MOZILLA_HOME/communicator-4.04.bin $*
