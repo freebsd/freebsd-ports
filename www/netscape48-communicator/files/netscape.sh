529eabf9819b9875999b898652719f29778cb636
Andrey A. Chernov <ache@FreeBSD.org>
1997-12-08 04:17:57 +0000

    Use XCMSDB hack to stop hang

--- www/netscape48-communicator/files/netscape.sh
+++ www/netscape48-communicator/files/netscape.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export MOZILLA_HOME; MOZILLA_HOME=${MOZILLA_HOME:=@PREFIX@/lib/netscape}
 export CLASSPATH ; CLASSPATH=.:$MOZILLA_HOME
+export XCMSDB; XCMSDB=/dev/null
 exec $MOZILLA_HOME/communicator-4.04.bin $*
