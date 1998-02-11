94d3db799163fc6288a766fb808517be656b6334
David E. O'Brien <obrien@FreeBSD.org>
1998-02-11 03:31:31 +0000

    Netscape Communicator with US strong encryption.

---
+++ www/netscape4-communicator.us/files/netscape.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export MOZILLA_HOME; MOZILLA_HOME=${MOZILLA_HOME:=@PREFIX@/lib/netscape.us}
+export CLASSPATH ; CLASSPATH=.:$MOZILLA_HOME
+export XCMSDB; XCMSDB=/dev/null
+exec $MOZILLA_HOME/communicator-4.04.us.bin $*
