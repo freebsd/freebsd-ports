9bb4bf9603931bf193ee4a27153bdfbb88b48576
Matthew Hunt <mph@FreeBSD.org>
1998-05-05 01:00:22 +0000

    Import of latex2html, which turns LaTeX into HTML
    PR:		6502
    Submitted by:	Brett Taylor <brett@peloton.physics.montana.edu>

---
+++ textproc/latex2html/files/local.pm
@@ -0,0 +1,28 @@
+#################################################################
+# local.pm
+#
+# Local Configuration for LaTeX2HTML
+#
+# This file is created automatically. Do not edit!
+#
+#################################################################
+
+package main;
+
+### start pstoimg configuration ###
+$GS_LIB = '/usr/local/share/ghostscript/5.0'; # Inserted by configure-pstoimg
+$PNMCAT = '/usr/X11R6/bin/pnmcat'; # Inserted by configure-pstoimg
+$PPMQUANT = '/usr/X11R6/bin/ppmquant'; # Inserted by configure-pstoimg
+$PNMFLIP = '/usr/X11R6/bin/pnmflip'; # Inserted by configure-pstoimg
+$PPMTOGIF = '/usr/X11R6/bin/ppmtogif'; # Inserted by configure-pstoimg
+$HOWTO_TRANSPARENT_GIF = 'netpbm'; # Inserted by configure-pstoimg
+$GS_DEVICE = 'ppmraw'; # Inserted by configure-pstoimg
+$GS = '/usr/local/bin/gs'; # Inserted by configure-pstoimg
+$PNMFILE = '/usr/X11R6/bin/pnmfile'; # Inserted by configure-pstoimg
+$HOWTO_INTERLACE_GIF = 'netpbm'; # Inserted by configure-pstoimg
+$PBMMAKE = '/usr/X11R6/bin/pbmmake'; # Inserted by configure-pstoimg
+$PNMCROP = '/usr/X11R6/bin/pnmcrop'; # Inserted by configure-pstoimg
+
+### end pstoimg configuration ###
+
+1;
