a3658c9e7cafaebf7f736a36f1ac4cefad0bb7c3
David E. O'Brien <obrien@FreeBSD.org>
1998-08-13 17:49:15 +0000

    Add ``osfnc.h'' from glibc++ 2.7.2 (of FBSD 2.2.7 fame) so /usr/src/gnu/usr.bin/groff will compile.

---
+++ lang/glibstdc++28/files/osfcn.h
@@ -0,0 +1,19 @@
+/* This file is taken from the glibc++ 2.7.x as installed on a 
+   FreeBSD 2.27 system */
+
+#ifndef OSFCN_H
+#define OSFCN_H 1
+
+/* #include <std.h> */
+#include <time.h>
+#include <sys/types.h>
+#if _G_HAVE_SYS_SOCKET
+#include <sys/socket.h>
+#endif
+#if _G_HAVE_SYS_RESOURCE
+#include <sys/time.h>
+#include <sys/resource.h>
+#endif
+
+
+#endif
