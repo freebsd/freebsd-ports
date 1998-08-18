e67fec16cc7719ae0e557b32b0c99f0827d3b7ce
Andrey A. Chernov <ache@FreeBSD.org>
1998-08-18 01:43:58 +0000

    fix version number

--- www/netscape4-communicator/files/netscape.sh
+++ www/netscape4-communicator/files/netscape.sh
@@ -2,4 +2,4 @@
 export MOZILLA_HOME; MOZILLA_HOME=${MOZILLA_HOME:=@PREFIX@/lib/netscape}
 export CLASSPATH ; CLASSPATH=.:$MOZILLA_HOME
 export XCMSDB; XCMSDB=/dev/null
-exec $MOZILLA_HOME/communicator-4.05.bin $*
+exec $MOZILLA_HOME/communicator-4.06.bin $*
