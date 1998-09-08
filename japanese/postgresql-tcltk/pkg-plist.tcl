165d5d4db9561857e3c183949f54c39262f12386
Jun Kuriyama <kuriyama@FreeBSD.org>
1998-09-08 15:30:19 +0000

    a robust, next generation, object-relational DBMS plus Multibyte-patch
    Submitted by:	Ichiro Fukuhara <ichiro@ichiro.org>

---
+++ japanese/postgresql-tcltk/pkg-plist.tcl
@@ -0,0 +1,216 @@
+etc/rc.d/pgsql.sh
+%%INSTALL_DIR%%/.profile
+%%INSTALL_DIR%%/bin/cleardbdir
+%%INSTALL_DIR%%/bin/createdb
+%%INSTALL_DIR%%/bin/createuser
+%%INSTALL_DIR%%/bin/destroydb
+%%INSTALL_DIR%%/bin/destroyuser
+%%INSTALL_DIR%%/bin/ecpg
+%%INSTALL_DIR%%/bin/initdb
+%%INSTALL_DIR%%/bin/initlocation
+%%INSTALL_DIR%%/bin/ipcclean
+%%INSTALL_DIR%%/bin/pg_dump
+%%INSTALL_DIR%%/bin/pg_dumpall
+%%INSTALL_DIR%%/bin/pg_id
+%%INSTALL_DIR%%/bin/pg_passwd
+%%INSTALL_DIR%%/bin/pg_version
+%%INSTALL_DIR%%/bin/pgtclsh
+%%INSTALL_DIR%%/bin/pgtksh
+%%INSTALL_DIR%%/bin/postgres
+%%INSTALL_DIR%%/bin/postmaster
+%%INSTALL_DIR%%/bin/psql
+%%INSTALL_DIR%%/data/PG_VERSION
+%%INSTALL_DIR%%/data/base/template1/PG_VERSION
+%%INSTALL_DIR%%/data/base/template1/pg_aggregate
+%%INSTALL_DIR%%/data/base/template1/pg_am
+%%INSTALL_DIR%%/data/base/template1/pg_amop
+%%INSTALL_DIR%%/data/base/template1/pg_amproc
+%%INSTALL_DIR%%/data/base/template1/pg_attrdef
+%%INSTALL_DIR%%/data/base/template1/pg_attrdef_adrelid_index
+%%INSTALL_DIR%%/data/base/template1/pg_attribute
+%%INSTALL_DIR%%/data/base/template1/pg_attribute_attrelid_index
+%%INSTALL_DIR%%/data/base/template1/pg_attribute_relid_attnam_index
+%%INSTALL_DIR%%/data/base/template1/pg_attribute_relid_attnum_index
+%%INSTALL_DIR%%/data/base/template1/pg_class
+%%INSTALL_DIR%%/data/base/template1/pg_class_oid_index
+%%INSTALL_DIR%%/data/base/template1/pg_class_relname_index
+%%INSTALL_DIR%%/data/base/template1/pg_description
+%%INSTALL_DIR%%/data/base/template1/pg_description_objoid_index
+%%INSTALL_DIR%%/data/base/template1/pg_index
+%%INSTALL_DIR%%/data/base/template1/pg_inheritproc
+%%INSTALL_DIR%%/data/base/template1/pg_inherits
+%%INSTALL_DIR%%/data/base/template1/pg_internal.init
+%%INSTALL_DIR%%/data/base/template1/pg_ipl
+%%INSTALL_DIR%%/data/base/template1/pg_language
+%%INSTALL_DIR%%/data/base/template1/pg_listener
+%%INSTALL_DIR%%/data/base/template1/pg_opclass
+%%INSTALL_DIR%%/data/base/template1/pg_operator
+%%INSTALL_DIR%%/data/base/template1/pg_parg
+%%INSTALL_DIR%%/data/base/template1/pg_proc
+%%INSTALL_DIR%%/data/base/template1/pg_proc_oid_index
+%%INSTALL_DIR%%/data/base/template1/pg_proc_proname_index
+%%INSTALL_DIR%%/data/base/template1/pg_proc_prosrc_index
+%%INSTALL_DIR%%/data/base/template1/pg_relcheck
+%%INSTALL_DIR%%/data/base/template1/pg_relcheck_rcrelid_index
+%%INSTALL_DIR%%/data/base/template1/pg_rewrite
+%%INSTALL_DIR%%/data/base/template1/pg_statistic
+%%INSTALL_DIR%%/data/base/template1/pg_trigger
+%%INSTALL_DIR%%/data/base/template1/pg_trigger_tgrelid_index
+%%INSTALL_DIR%%/data/base/template1/pg_type
+%%INSTALL_DIR%%/data/base/template1/pg_type_oid_index
+%%INSTALL_DIR%%/data/base/template1/pg_type_typname_index
+%%INSTALL_DIR%%/data/base/template1/pg_user
+%%INSTALL_DIR%%/data/base/template1/pg_version
+%%INSTALL_DIR%%/data/pg_database
+%%INSTALL_DIR%%/data/pg_geqo.sample
+%%INSTALL_DIR%%/data/pg_group
+%%INSTALL_DIR%%/data/pg_hba.conf
+%%INSTALL_DIR%%/data/pg_log
+%%INSTALL_DIR%%/data/pg_pwd
+%%INSTALL_DIR%%/data/pg_shadow
+%%INSTALL_DIR%%/data/pg_variable
+%%INSTALL_DIR%%/include/access/attnum.h
+%%INSTALL_DIR%%/include/c.h
+%%INSTALL_DIR%%/include/commands/trigger.h
+%%INSTALL_DIR%%/include/config.h
+%%INSTALL_DIR%%/include/ecpglib.h
+%%INSTALL_DIR%%/include/ecpgtype.h
+%%INSTALL_DIR%%/include/executor/spi.h
+%%INSTALL_DIR%%/include/fmgr.h
+%%INSTALL_DIR%%/include/lib/dllist.h
+%%INSTALL_DIR%%/include/libpgtcl.h
+%%INSTALL_DIR%%/include/libpq-fe.h
+%%INSTALL_DIR%%/include/libpq/libpq-fs.h
+%%INSTALL_DIR%%/include/libpq/pqcomm.h
+%%INSTALL_DIR%%/include/os.h
+%%INSTALL_DIR%%/include/postgres.h
+%%INSTALL_DIR%%/include/postgres_ext.h
+%%INSTALL_DIR%%/include/sqlca.h
+%%INSTALL_DIR%%/include/utils/elog.h
+%%INSTALL_DIR%%/include/utils/geo_decls.h
+%%INSTALL_DIR%%/include/utils/palloc.h
+%%INSTALL_DIR%%/lib/global1.bki.source
+%%INSTALL_DIR%%/lib/global1.description
+%%INSTALL_DIR%%/lib/libecpg.a
+%%INSTALL_DIR%%/lib/libecpg.so
+%%INSTALL_DIR%%/lib/libecpg.so.1.1
+%%INSTALL_DIR%%/lib/libpgtcl.a
+%%INSTALL_DIR%%/lib/libpgtcl.so
+%%INSTALL_DIR%%/lib/libpgtcl.so.1.0
+%%INSTALL_DIR%%/lib/libpq.a
+%%INSTALL_DIR%%/lib/libpq.so
+%%INSTALL_DIR%%/lib/libpq.so.1.1
+%%INSTALL_DIR%%/lib/local1_template1.bki.source
+%%INSTALL_DIR%%/lib/local1_template1.description
+%%INSTALL_DIR%%/lib/pg_geqo.sample
+%%INSTALL_DIR%%/lib/pg_hba.conf.sample
+%%INSTALL_DIR%%/post-install-notes
+share/doc/%%INSTALL_DIR%%/FAQ
+share/doc/%%INSTALL_DIR%%/FAQ_FreeBSD
+share/doc/%%INSTALL_DIR%%/FAQ_Irix
+share/doc/%%INSTALL_DIR%%/FAQ_Linux
+share/doc/%%INSTALL_DIR%%/Makefile
+share/doc/%%INSTALL_DIR%%/README.GEQO
+share/doc/%%INSTALL_DIR%%/README.fsync
+share/doc/%%INSTALL_DIR%%/README.mb
+share/doc/%%INSTALL_DIR%%/README.mb.jp
+share/doc/%%INSTALL_DIR%%/README.support
+share/doc/%%INSTALL_DIR%%/TODO
+share/doc/%%INSTALL_DIR%%/TODO.GEQO
+share/doc/%%INSTALL_DIR%%/admin.ps.gz
+share/doc/%%INSTALL_DIR%%/admin.tar.gz
+share/doc/%%INSTALL_DIR%%/bug.template
+share/doc/%%INSTALL_DIR%%/postgres.tar.gz
+share/doc/%%INSTALL_DIR%%/programmer.ps.gz
+share/doc/%%INSTALL_DIR%%/programmer.tar.gz
+share/doc/%%INSTALL_DIR%%/src/Makefile
+share/doc/%%INSTALL_DIR%%/src/graphics/catalogs.gif
+share/doc/%%INSTALL_DIR%%/src/graphics/clientserver.ag
+share/doc/%%INSTALL_DIR%%/src/graphics/clientserver.gif
+share/doc/%%INSTALL_DIR%%/src/graphics/connections.ag
+share/doc/%%INSTALL_DIR%%/src/graphics/connections.gif
+share/doc/%%INSTALL_DIR%%/src/graphics/layout.ag
+share/doc/%%INSTALL_DIR%%/src/graphics/layout.gif
+share/doc/%%INSTALL_DIR%%/src/sgml/Makefile
+share/doc/%%INSTALL_DIR%%/src/sgml/admin.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/advanced.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/arch-dev.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/arch-pg.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/arch.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/array.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/biblio.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/compiler.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/contacts.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/datatype.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/dfunc.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/docguide.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/ecpg.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/environ.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/extend.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/func-ref.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/geqo.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/gist.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/inherit.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/install.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/intro-pg.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/intro.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/jdbc.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/libpgtcl.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/libpq.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/lobj.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/manage.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/odbc.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/pgaccess.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/ports.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/postgres.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/programmer.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/protocol.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/psql.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/query-ug.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/query.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/recovery.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/regress.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/release.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/rules.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/spi.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/start-ag.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/start.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/storage.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/trigger.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/tutorial.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/user.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/xaggr.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/xfunc.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/xindex.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/xoper.sgml
+share/doc/%%INSTALL_DIR%%/src/sgml/xtypes.sgml
+share/doc/%%INSTALL_DIR%%/tutorial.ps.gz
+share/doc/%%INSTALL_DIR%%/tutorial.tar.gz
+share/doc/%%INSTALL_DIR%%/user.ps.gz
+share/doc/%%INSTALL_DIR%%/user.tar.gz
+share/doc/%%INSTALL_DIR%%/userguide.ps
+@dirrm share/doc/%%INSTALL_DIR%%/src/graphics
+@dirrm share/doc/%%INSTALL_DIR%%/src/sgml
+@dirrm share/doc/%%INSTALL_DIR%%/src
+@dirrm share/doc/%%INSTALL_DIR%%
+@dirrm %%INSTALL_DIR%%/bin
+@dirrm %%INSTALL_DIR%%/data/base/template1
+@dirrm %%INSTALL_DIR%%/data/base
+@dirrm %%INSTALL_DIR%%/data
+@dirrm %%INSTALL_DIR%%/include/access
+@dirrm %%INSTALL_DIR%%/include/commands
+@dirrm %%INSTALL_DIR%%/include/executor
+@dirrm %%INSTALL_DIR%%/include/include
+@dirrm %%INSTALL_DIR%%/include/lib
+@dirrm %%INSTALL_DIR%%/include/libpq
+@dirrm %%INSTALL_DIR%%/include/port/bsd
+@dirrm %%INSTALL_DIR%%/include/port
+@dirrm %%INSTALL_DIR%%/include/utils
+@dirrm %%INSTALL_DIR%%/include
+@dirrm %%INSTALL_DIR%%/lib
+@dirrm %%INSTALL_DIR%%/man/man1
+@dirrm %%INSTALL_DIR%%/man/man3
+@dirrm %%INSTALL_DIR%%/man/man5
+@dirrm %%INSTALL_DIR%%/man/manl
+@dirrm %%INSTALL_DIR%%/man
+@dirrm %%INSTALL_DIR%%
