a09b649f1ed860182e2ab46b318f032e933d0796
Andreas Klemm <andreas@FreeBSD.org>
1998-09-30 18:16:08 +0000

    - integrate Ralf S. Engelschall's apache13-modssl,   i. e. apache13-php3 supports SSL now!
    - upgrade to Apache 1.3.2 and PHP 3.0.4
    
    - enable track-vars for PHP3 by default to support phplib
      (http://phplib.shonline.de/)
      Pointed out by: Thomas Kuehne <thomas@todo.de>
    
    - losts of minor changes (install php-icons, index.php3 as DirectoryIndex,
      support .php3 and .phtml suffixes, enable application/x-httpd-php3-source
      for .phps and .php3s, install default php3.ini-dist, ...)
    Submitted by:	Dirk Froemberg <ibex@physik.TU-Berlin.DE>

--- www/mod_php3/scripts/configure.php
+++ www/mod_php3/scripts/configure.php
@@ -4,16 +4,18 @@ if [ -f ${CURDIR}/Makefile.inc ]; then
 	exit
 fi
 
-/usr/bin/dialog --title "PHP configuration options" --clear \
+/usr/bin/dialog --title "configuration options" --clear \
 	--checklist "\n\
 Please select desired options:" -1 -1 9 \
-GD		"GD library support" ON \
-FreeType	"TrueType font rendering (implies GD)" OFF \
-zlib		"zlib library support" ON \
-MySQL		"MySQL database support" ON \
-PostgreSQL	"PostgreSQL database support" OFF \
-mSQL		"mSQL database support" OFF \
-dBase		"dBase database support" OFF 2> /tmp/checklist.tmp.$$
+tuning		"Apache: performance tuning" OFF \
+modssl		"Apache: SSL support" OFF \
+GD		"PHP3:   GD library support" ON \
+FreeType	"PHP3:   TrueType font rendering (implies GD)" OFF \
+zlib		"PHP3:   zlib library support" ON \
+MySQL		"PHP3:   MySQL database support" ON \
+PostgreSQL	"PHP3:   PostgreSQL database support" OFF \
+mSQL		"PHP3:   mSQL database support" OFF \
+dBase		"PHP3:   dBase database support" OFF 2> /tmp/checklist.tmp.$$
 
 retval=$?
 
@@ -36,6 +38,9 @@ esac
 
 while [ "$1" ]; do
 	case $1 in
+		\"tuning\")
+			echo "APACHE_PERF_TUNING=	YES" >> ${CURDIR}/Makefile.inc
+			;;
 		\"GD\")
 			echo "BUILD_DEPENDS+=		\${PREFIX}/lib/libgd.a:\${PORTSDIR}/graphics/gd" >> ${CURDIR}/Makefile.inc
 			echo "PHP3_CONF_ARGS+=	--with-gd" >> ${CURDIR}/Makefile.inc
@@ -64,6 +69,50 @@ while [ "$1" ]; do
 			;;
 		\"dBase\")
 			echo "PHP3_CONF_ARGS+=	--with-dbase" >> ${CURDIR}/Makefile.inc
+			;;
+		\"modssl\")
+			cat << EOF >> ${CURDIR}/Makefile.inc
+PKGNAME=	apache-php\${VERSION_PHP3}-\${VERSION_APACHE}+mod_ssl-\${VERSION_MODSSL}
+MASTER_SITES+=	http://www.engelschall.com/sw/mod_ssl/distrib/ \\
+		ftp://ftp.engelschall.com/sw/mod_ssl/ \\
+		ftp://ftp.ulpgc.es/pub/mod_ssl/ \\
+		ftp://glock.missouri.edu/pub/mod_ssl/
+DISTFILES+=	mod_ssl-\${VERSION_MODSSL}-\${VERSION_APACHE}\${EXTRACT_SUFX}
+
+BUILD_DEPENDS+=	ssleay:\${PORTSDIR}/security/SSLeay \\
+		\${PREFIX}/lib/libssl.a:\${PORTSDIR}/security/SSLeay \\
+		\${PREFIX}/lib/libcrypto.a:\${PORTSDIR}/security/SSLeay
+RUN_DEPENDS+=	ssleay:\${PORTSDIR}/security/SSLeay
+
+VERSION_MODSSL=	2.0.11
+
+RESTRICTED=	"Contains cryptography"
+
+CONFIGURE_ARGS+=--enable-module=ssl
+CONFIGURE_ENV+=	SSL_BASE='SYSTEM' PATH="\${PREFIX}/bin:\${PATH}"
+
+PATCHDIR=	\${.CURDIR}/patches.modssl
+PLIST=		\${PKGDIR}/PLIST.modssl
+SSL=		ssl
+
+pre-patch:
+	@cd \${WRKDIR}/mod_ssl-\${VERSION_MODSSL}-\${VERSION_APACHE} \\
+	&& \${ECHO_MSG} "===>  Applying mod_ssl-\${VERSION_MODSSL} extension" \\
+	&& ./configure --with-apache=../\${DISTNAME}
+
+post-patch:
+	@cd \${WRKSRC} \\
+	&& find . -type f -name "*.orig" -print | xargs \${RM} -f
+
+post-build:
+	@cd \${WRKSRC} \\
+	&& \${MAKE} certificate TYPE=dummy >/dev/null 2>&1
+
+certificate:
+	@cd \${WRKSRC} \\
+	&& \${ECHO_MSG} "===>  Creating Test Certificate for Server" \\
+	&& \${MAKE} certificate TYPE=\$(TYPE) CRT=\$(CRT) KEY=\$(KEY)
+EOF
 			;;
 	esac
 	shift
