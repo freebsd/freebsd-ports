e767a0c7a7d5c149ba70ab22ac8842a6cf3ec845
billf <billf@FreeBSD.org>
1998-11-25 22:04:29 +0000

    Typo "sbin/upsmon" -> "sbin/upsmond"
    Submitted by:	Igor Vinokurov

--- sysutils/upsmon/files/upsmon.sh
+++ sysutils/upsmon/files/upsmon.sh
@@ -3,5 +3,5 @@
 # see !!PREFIX!!/share/doc/upsmon/INSTALL for command line option details
 
 if [ -x !!PREFIX!!/sbin/upsmon ]; then
-	!!PREFIX!!/sbin/upsmon -p /dev/cuaa0 && echo -n ' upsmond'
+	!!PREFIX!!/sbin/upsmond -p /dev/cuaa0 && echo -n ' upsmond'
 fi
