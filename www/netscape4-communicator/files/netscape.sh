f8e0872e983e10dc0318fcd2b4d40fc328f26e26
SADA Kenji <sada@FreeBSD.org>
1998-12-26 19:23:55 +0000

    Justify version notation.

--- www/netscape4-communicator/files/netscape.sh
+++ www/netscape4-communicator/files/netscape.sh
@@ -2,4 +2,4 @@
 export MOZILLA_HOME; MOZILLA_HOME=${MOZILLA_HOME:=@PREFIX@/lib/netscape}
 export CLASSPATH ; CLASSPATH=.:$MOZILLA_HOME
 export XCMSDB; XCMSDB=/dev/null
-exec $MOZILLA_HOME/communicator-4.07.bin $*
+exec $MOZILLA_HOME/communicator-4.08.bin $*
