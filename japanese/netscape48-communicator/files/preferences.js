ea6cb8add3c27ffec132eca72be23b967dfc2d11
SADA Kenji <sada@FreeBSD.org>
1998-12-26 21:52:55 +0000

    Update to 4.08.

--- japanese/netscape48-communicator/files/preferences.js
+++
@@ -1,5 +0,0 @@
-// Netscape User Preferences
-// This is a generated file!  Do not edit.
-
-user_pref("intl.character_set", 2309);
-user_pref("intl.font_spec_list", "schumacher-clean-120-noscale-fixed-x-user-defined,schumacher-clean-120-noscale-prop-x-user-defined,nsPseudoFont-courier-180-noscale-fixed-UTF-8,nsPseudoFont-helvetica-180-noscale-prop-UTF-8,isas-fangsong ti-120-noscale-fixed-gb_2312-80,isas-fangsong ti-120-noscale-prop-gb_2312-80,daewoo-gothic-0-noscale-fixed-ks_c_5601-1987,daewoo-gothic-0-noscale-prop-ks_c_5601-1987,misc-fixed-130-noscale-fixed-jis_x0201,misc-fixed-130-noscale-prop-jis_x0201,netscape-fixed-130-noscale-fixed-jis_x0208-1983,netscape-fixed-130-noscale-prop-jis_x0208-1983,misc-fixed-130-noscale-fixed-iso-8859-1,adobe-new century schoolbook-140-noscale-prop-iso-8859-1,");
