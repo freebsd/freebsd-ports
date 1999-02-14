ce5da22b7076167a515a6a0b602b22b67022e5ff
cvs2svn <cvs2svn@FreeBSD.org>
1999-02-14 10:41:19 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_3_1_0'.

--- news/inn-current/files/innd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ $# -eq 0 -o x$1 = xstart ]; then
-   if [ -x !!PREFIX!!/etc/rc.news -a -f !!PREFIX!!/news/lib/history.pag ]; then
-      limits -C news !!PREFIX!!/etc/rc.news && echo ' inn'
-   fi
-fi
-if [ x$1 = xstop ]; then
-   !!PREFIX!!/news/bin/ctlinnd shutdown machine is going down
-fi
