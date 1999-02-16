7cbbe38b2cd8d091d0ddc0dc486c786521ae2144
Andreas Klemm <andreas@FreeBSD.org>
1999-02-16 22:01:06 +0000

    New port inn2.2-stable after repository copy. Thanks to the ports meister ;-) The new features are really cool (cnfs,...). BTW, why can't we use raw partitions for cnfs ???
    Please read the file INSTALL carefully in the top level
    dir of sources ... it explains installation step by step.
    
    You still might have to watch for console messages,
    complaining about missing file or directories (news.times
    for example).
    
    All in all. inn22 is really fun !

--- news/inn-stable/files/innd.sh
+++ news/inn-stable/files/innd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $# -eq 0 -o x$1 = xstart ]; then
-   if [ -x !!PREFIX!!/etc/rc.news -a -f !!PREFIX!!/news/lib/history.pag ]; then
-      limits -C news !!PREFIX!!/etc/rc.news && echo ' inn'
+   if [ -x !!PREFIX!!/news/bin/rc.news -a -f !!PREFIX!!/news/db/history.pag ]; then
+      limits -C news !!PREFIX!!/bin/rc.news && echo ' inn'
    fi
 fi
 if [ x$1 = xstop ]; then
