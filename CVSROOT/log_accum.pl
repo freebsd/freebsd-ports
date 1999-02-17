9861d3cabe6e391ea67eb9e09322c701e9199f2d
peter <peter@FreeBSD.org>
1999-02-17 17:14:40 +0000

    Update for post-resend changes use.  (the majordomo resend on hub is using bulk_mailer to inject the mail, the address list is ignored now, this broke the cvs-committers list)

--- CVSROOT/log_accum.pl
+++ CVSROOT/log_accum.pl
@@ -1,6 +1,6 @@
 #!/usr/local/bin/perl
 #
-# $Id: log_accum.pl,v 1.29 1998/07/27 13:01:39 wosch Exp $
+# $Id: log_accum.pl,v 1.30 1999/02/16 13:39:30 peter Exp $
 #
 # Perl filter to handle the log messages from the checkin of files in
 # a directory.  This script will group the lists of files by log
@@ -388,6 +388,7 @@ sub mail_notification {
     if ($subject ne "") {
 	print(MAIL $subject, "\n");
     }
+    print MAIL "Precedence: bulk\n");
     print (MAIL "\n");
 
     print(MAIL join("\n", @text));
