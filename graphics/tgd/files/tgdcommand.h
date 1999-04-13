3e734d460e14036a1ee01096cf73044957d0471a
mharo <mharo@FreeBSD.org>
1999-04-13 07:06:36 +0000

    Add support for PREFIX, CFLAGS and less warnings when building with CFLAGS=-Wall
    PR:		10933
    Submitted by:	me

---
+++ graphics/tgd/files/tgdcommand.h
@@ -0,0 +1,6 @@
+#ifndef TGDCOMMAND_H
+#define TGDCOMMAND_H
+
+void tgdcommand( int, char ** );
+
+#endif
---
+++ graphics/tgd/files/tgdgd.h
@@ -0,0 +1,47 @@
+#ifndef TGDGD_H
+#define TGDGD_H
+
+void imagearc( int, char ** );
+void imageblue( int, char ** );
+void imageboundssafe( int, char ** );
+void imagechar( int, char ** );
+void imagecolorallocate( int, char ** );
+void imagecolordeallocate( int, char ** );
+void imagecolorclosest( int, char ** );
+void imagecolorexact( int, char ** );
+void imagecolortransparent( int, char ** );
+void imagecopy( int, char ** );
+void imagecopyresized( int, char ** );
+void imagecreate( int, char ** );
+void imagecreatefromgd( int, char ** );
+void imagecreatefromgif( int, char ** );
+void imagecreatefromxbm( int, char ** );
+void imagecolorstotal( int, char ** );
+void imagedashedline( int, char ** );
+void imagedestroy( int, char ** );
+void imagefill( int, char ** );
+void imagefilltoborder( int, char ** );
+void imagefilledrectangle( int, char ** );
+void imagegd( int, char ** );
+void imagegetinterlaced( int, char ** );
+void imagegetpixel( int, char ** );
+void imagegettransparent( int, char ** );
+void imagegif( int, char ** );
+void imagegreen( int, char ** );
+void imageinterlace( int, char ** );
+void imageline( int, char ** );
+void imagefilledpolygon( int, char ** );
+void imagepolygon( int, char ** );
+void imagerectangle( int, char ** );
+void imagered( int, char ** );
+void imagesx( int, char ** );
+void imagesy( int, char ** );
+void imagesetbrush( int, char ** );
+void imagesetpixel( int, char ** );
+void imagesetstyle( int, char ** );
+void imagesettile( int, char ** );
+void imagestring( int, char ** );
+void imagestringup( int, char ** );
+void imagecharup( int, char ** );
+
+#endif
