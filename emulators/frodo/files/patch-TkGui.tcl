ab38a6b0e5e87370f7d68921f088ab6432e9fc85
Steve Price <steve@FreeBSD.org>
1999-05-01 02:55:16 +0000

    If wish is installed the configuration dialog doesn't get called.
    PR:		11346
    Submitted by:	maintainer

--- emulators/frodo/files/patch-TkGui.tcl
+++ emulators/frodo/files/patch-TkGui.tcl
@@ -1,23 +1,14 @@
-*** ../TkGui.tcl	Wed Aug  6 20:56:34 1997
---- ../TkGui.tcl.neu	Thu Dec  4 00:38:15 1997
-***************
-*** 1,9 ****
-! #!/usr/local/bin/X11/wish
-  
-  # Frodo Tk GUI by Lutz Vieweg <lkv@mania.robin.de>
-  # requires Tk >= 4.1
-  
-! package require Tk 4.1
-  
-  set prefname "$env(HOME)/.frodorc"
-  
---- 1,9 ----
-! #!/usr/local/bin/wish8.0 -f
-  
-  # Frodo Tk GUI by Lutz Vieweg <lkv@mania.robin.de>
-  # requires Tk >= 4.1
-  
-! ## package require Tk 4.1
-  
-  set prefname "$env(HOME)/.frodorc"
-  
+--- ../TkGui.tcl.orig	Wed Aug  6 20:56:34 1997
++++ ../TkGui.tcl	Mon Apr 26 21:49:18 1999
+@@ -1,9 +1,9 @@
+-#!/usr/local/bin/X11/wish
++#!/usr/local/bin/wish8.0 -f
+ 
+ # Frodo Tk GUI by Lutz Vieweg <lkv@mania.robin.de>
+ # requires Tk >= 4.1
+ 
+-package require Tk 4.1
++## package require Tk 4.1
+ 
+ set prefname "$env(HOME)/.frodorc"
+ 
