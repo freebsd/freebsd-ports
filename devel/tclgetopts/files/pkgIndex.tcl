d88566d716659abbf62bf80d314a40a2bf04fb93
Steve Price <steve@FreeBSD.org>
1999-05-03 03:23:01 +0000

    Initial import of tclGetOpts version 1.0. Pure TCL code for command line options parsing.
    PR:		10661
    Submitted by:	Mikhail Teterin <mi@aldan.algebra.com>

---
+++ devel/tclgetopts/files/pkgIndex.tcl
@@ -0,0 +1,6 @@
+# This file was NOT auto generated! pkg_mkIndex(n) will NOT re-create it -mi
+
+package ifneeded foxgetopt 1.0 [list tclPkgSetup $dir foxgetopt 1.0 {
+	{foxTypedOpts.tcl	source	typedopts}
+	{foxGetOpt.tcl		source	getopt}
+}]
