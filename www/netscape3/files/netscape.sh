db0cfc7fb17e74dcdc7bb76b722f958f2c22dd4b
Steve Price <steve@FreeBSD.org>
1999-05-09 18:24:29 +0000

    Hand off all arguments in the netscape wrapper script.
    PR:		4083
    Submitted by:	Jesse Rosenstock <jmr@ugcs.caltech.edu>

--- www/netscape3/files/netscape.sh
+++ www/netscape3/files/netscape.sh
@@ -3,4 +3,4 @@ export XKEYSYMDB ; XKEYSYMDB=@X11BASE@/lib/X11/XKeysymDB
 export XNLSPATH  ; XNLSPATH=@X11BASE@/lib/X11/nls
 export XAPPLRESDIR ; XAPPLRESDIR=@X11BASE@/lib/X11/app-defaults
 export CLASSPATH ; CLASSPATH=.:/usr/local/lib/netscape
-exec /usr/local/lib/netscape/netscape.bin $*
+exec /usr/local/lib/netscape/netscape.bin "$@"
