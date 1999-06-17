1a40479f29810ae07100742f821596df9d3a60b8
billf <billf@FreeBSD.org>
1999-06-17 12:50:54 +0000

    I copied this from being a patch to being a file. Boy do I feel silly that I forgot to stript the patch information from it.
    Submitted by:	maintainer

--- net/rtsp_proxy/files/rtsp_proxy.sh
+++ net/rtsp_proxy/files/rtsp_proxy.sh
@@ -1,5 +1,2 @@
---- rtsp_proxy.sh	Wed Dec 31 16:00:00 1969
-+++ rtsp_proxy.sh	Tue Jun  8 22:54:49 1999
-@@ -0,0 +1,2 @@
-+#!/bin/sh
-+[ -x /usr/local/sbin/rtsp_proxy ] && /usr/local/sbin/rtsp_proxy > /dev/null & && echo $! > /var/run/rtsp_proxy.pid && echo -n ' rtsp_proxy'
+#!/bin/sh
+[ -x /usr/local/sbin/rtsp_proxy ] && /usr/local/sbin/rtsp_proxy > /dev/null & && echo $! > /var/run/rtsp_proxy.pid && echo -n ' rtsp_proxy'
