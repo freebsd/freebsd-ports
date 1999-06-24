4b396d7a9d6f541be1dc7e953ada75b5d75c09c9
Jun Kuriyama <kuriyama@FreeBSD.org>
1999-06-24 15:23:12 +0000

    XSL Transformations (XSLT) implementation in Java.

---
+++ textproc/xt/files/xt-xml4j.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# $Id$
+
+export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xml4j.jar:${CLASSPATH}
+
+java -Dcom.jclark.xsl.sax.parser=com.ibm.xml.parser.SAXDriver com.jclark.xsl.sax.Driver $*
+
