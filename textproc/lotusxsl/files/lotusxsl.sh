06ebdfb354d7b07026966d521117288d0c77cfa2
Jun Kuriyama <kuriyama@FreeBSD.org>
1999-06-25 15:10:31 +0000

    XSLT (XSL Transformations) implementation.

---
+++ textproc/lotusxsl/files/lotusxsl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# $Id$
+
+export CLASSPATH=%%PREFIX%%/share/java/classes/xml4j.jar:%%PREFIX%%/share/java/classes/lotusxsl.jar:${CLASSPATH}
+
+java com.lotus.xsl.xml4j2dom.XML4JLiaison4dom $* -v
+
+# -xsl ??.xsl -in ??.xml -out ??.html
