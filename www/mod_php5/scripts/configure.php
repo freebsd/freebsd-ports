97ed60a7a9c13e4013effcbc8c9bec6de9ab312c
dirk <dirk@FreeBSD.org>
1999-08-16 18:41:13 +0000

    s/PHP3/PHP/g; s/php3/php/g;
    whereever appropriate.
    
    This was done in preparation for a repository copy to apache13-php4
    to keep the diffs between apache13-php3 and apache13-php4 as small as
    possible.

--- www/mod_php5/scripts/configure.php
+++ www/mod_php5/scripts/configure.php
@@ -9,18 +9,18 @@ fi
 Please select desired options:" -1 -1 14 \
 tuning		"Apache: performance tuning" OFF \
 modssl		"Apache: SSL support" OFF \
-GD		"PHP3:   GD library support" OFF \
-FreeType	"PHP3:   TrueType font rendering (implies GD)" OFF \
-zlib		"PHP3:   zlib library support" ON \
-mcrypt		"PHP3:   Encryption support" OFF \
-mhash		"PHP3:   Crypto-hashing support" OFF \
-pdflib		"PHP3:   pdflib support (implies zlib)" OFF \
-IMAP		"PHP3:   IMAP support" OFF \
-MySQL		"PHP3:   MySQL database support" ON \
-PostgreSQL	"PHP3:   PostgreSQL database support" OFF \
-mSQL		"PHP3:   mSQL database support" OFF \
-dBase		"PHP3:   dBase database support" OFF \
-OpenLDAP	"PHP3:   OpenLDAP support" OFF \
+GD		"PHP:    GD library support" OFF \
+FreeType	"PHP:    TrueType font rendering (implies GD)" OFF \
+zlib		"PHP:    zlib library support" ON \
+mcrypt		"PHP:    Encryption support" OFF \
+mhash		"PHP:    Crypto-hashing support" OFF \
+pdflib		"PHP:    pdflib support (implies zlib)" OFF \
+IMAP		"PHP:    IMAP support" OFF \
+MySQL		"PHP:    MySQL database support" ON \
+PostgreSQL	"PHP:    PostgreSQL database support" OFF \
+mSQL		"PHP:    mSQL database support" OFF \
+dBase		"PHP:    dBase database support" OFF \
+OpenLDAP	"PHP:    OpenLDAP support" OFF \
 2> /tmp/checklist.tmp.$$
 
 retval=$?
@@ -49,65 +49,65 @@ while [ "$1" ]; do
 			echo "APACHE_PERF_TUNING=	YES"
 			;;
 		\"GD\")
-			echo "BUILD_DEPENDS+=		\${PREFIX}/lib/libgd.a:\${PORTSDIR}/graphics/gd"
-			echo "PHP3_CONF_ARGS+=	--with-gd=\${PREFIX}"
+			echo "BUILD_DEPENDS+=	\${PREFIX}/lib/libgd.a:\${PORTSDIR}/graphics/gd"
+			echo "PHP_CONF_ARGS+=	--with-gd=\${PREFIX}"
 			GD=1
 			;;
 		\"FreeType\")
-			echo "LIB_DEPENDS+=		ttf.3:\${PORTSDIR}/print/freetype"
-			echo "PHP3_CONF_ARGS+=	--with-ttf=\${PREFIX}"
+			echo "LIB_DEPENDS+=	ttf.3:\${PORTSDIR}/print/freetype"
+			echo "PHP_CONF_ARGS+=	--with-ttf=\${PREFIX}"
 			if [ -z "$GD" ]; then
 				set $* \"GD\"
 			fi
 			;;
 		\"zlib\")
-			echo "PHP3_CONF_ARGS+=	--with-zlib"
+			echo "PHP_CONF_ARGS+=	--with-zlib"
 			ZLIB=1
 			;;
 		\"mcrypt\")
-			echo "LIB_DEPENDS+=		mcrypt.2:\${PORTSDIR}/security/libmcrypt"
-			echo "PHP3_CONF_ARGS+=	--with-mcrypt=\${PREFIX}"
+			echo "LIB_DEPENDS+=	mcrypt.2:\${PORTSDIR}/security/libmcrypt"
+			echo "PHP_CONF_ARGS+=	--with-mcrypt=\${PREFIX}"
 			;;
 		\"mhash\")
-			echo "LIB_DEPENDS+=		mhash.1:\${PORTSDIR}/security/mhash"
-			echo "PHP3_CONF_ARGS+=	--with-mhash=\${PREFIX}"
+			echo "LIB_DEPENDS+=	mhash.1:\${PORTSDIR}/security/mhash"
+			echo "PHP_CONF_ARGS+=	--with-mhash=\${PREFIX}"
 			;;
 		\"pdflib\")
-			echo "LIB_DEPENDS+=		pdf.2:\${PORTSDIR}/print/pdflib"
-			echo "PHP3_CONF_ARGS+=	--with-pdflib=\${PREFIX} \\"
-			echo "			--with-jpeg-dir=\${PREFIX} \\"
-			echo "			--with-tiff-dir=\${PREFIX}"
+			echo "LIB_DEPENDS+=	pdf.2:\${PORTSDIR}/print/pdflib"
+			echo "PHP_CONF_ARGS+=	--with-pdflib=\${PREFIX} \\"
+			echo "		--with-jpeg-dir=\${PREFIX} \\"
+			echo "		--with-tiff-dir=\${PREFIX}"
 			if [ -z "$ZLIB" ]; then
 				set $* \"zlib\"
 			fi
 			;;
 		\"IMAP\")
-			echo "BUILD_DEPENDS+=		\${PREFIX}/lib/libc-client4.a:\${PORTSDIR}/mail/imap-uw"
-			echo "PHP3_CONF_ARGS+=	--with-imap=\${PREFIX}"
+			echo "BUILD_DEPENDS+=	\${PREFIX}/lib/libc-client4.a:\${PORTSDIR}/mail/imap-uw"
+			echo "PHP_CONF_ARGS+=	--with-imap=\${PREFIX}"
 			;;
 		\"MySQL\")
-			echo "LIB_DEPENDS+=		mysqlclient.6:\${PORTSDIR}/databases/mysql322-client"
-			echo "PHP3_CONF_ARGS+=	--with-mysql=\${PREFIX}"
+			echo "LIB_DEPENDS+=	mysqlclient.6:\${PORTSDIR}/databases/mysql322-client"
+			echo "PHP_CONF_ARGS+=	--with-mysql=\${PREFIX}"
 			;;
 		\"PostgreSQL\")
-			echo "BUILD_DEPENDS+=		\${PREFIX}/pgsql/bin/psql:\${PORTSDIR}/databases/postgresql"
-			echo "PHP3_CONF_ARGS+=	--with-pgsql=\${PREFIX}/pgsql"
+			echo "BUILD_DEPENDS+=	\${PREFIX}/pgsql/bin/psql:\${PORTSDIR}/databases/postgresql"
+			echo "PHP_CONF_ARGS+=	--with-pgsql=\${PREFIX}/pgsql"
 			;;
 		\"mSQL\")
-			echo "BUILD_DEPENDS+=		msql:\${PORTSDIR}/databases/msql"
-			echo "PHP3_CONF_ARGS+=	--with-msql=\${PREFIX}"
+			echo "BUILD_DEPENDS+=	msql:\${PORTSDIR}/databases/msql"
+			echo "PHP_CONF_ARGS+=	--with-msql=\${PREFIX}"
 			;;
 		\"dBase\")
-			echo "PHP3_CONF_ARGS+=	--with-dbase"
+			echo "PHP_CONF_ARGS+=	--with-dbase"
 			;;
 		\"OpenLDAP\")
-			echo "BUILD_DEPENDS+=		\${PREFIX}/lib/libldap.a:\${PORTSDIR}/net/openldap"
-			echo "BUILD_DEPENDS+=		\${PREFIX}/lib/liblber.a:\${PORTSDIR}/net/openldap"
-			echo "PHP3_CONF_ARGS+=	--with-ldap=\${PREFIX}"
+			echo "BUILD_DEPENDS+=	\${PREFIX}/lib/libldap.a:\${PORTSDIR}/net/openldap"
+			echo "BUILD_DEPENDS+=	\${PREFIX}/lib/liblber.a:\${PORTSDIR}/net/openldap"
+			echo "PHP_CONF_ARGS+=	--with-ldap=\${PREFIX}"
 			;;
 		\"modssl\")
 			cat << EOF
-PKGNAME=	apache+php3+mod_ssl-\${VERSION_APACHE}+\${VERSION_PHP3}+\${VERSION_MODSSL}
+PKGNAME=	apache+php+mod_ssl-\${VERSION_APACHE}+\${VERSION_PHP}+\${VERSION_MODSSL}
 MASTER_SITES+=	http://www.modssl.org/source/ \\
 		ftp://ftp.modssl.org/source/ \\
 		ftp://ftp.ecrc.net/pub/security/mod_ssl/ \\
