e7b1f04eaaac7db443159ce139560ecee15eb095
dan <dan@FreeBSD.org>
1999-10-03 21:38:11 +0000

    Apply small fix from PR ports/14090.
    Submitted by: Eoin Lawless <eoin@maths.tcd.ie>

--- sysutils/upsmon/files/upsmon.sh
+++ sysutils/upsmon/files/upsmon.sh
@@ -2,6 +2,6 @@
 
 # see !!PREFIX!!/share/doc/upsmon/INSTALL for command line option details
 
-if [ -x !!PREFIX!!/sbin/upsmon ]; then
+if [ -x !!PREFIX!!/sbin/upsmond ]; then
 	!!PREFIX!!/sbin/upsmond -p /dev/cuaa0 && echo -n ' upsmond'
 fi
