412d33cbd66716e8bf04d351deb40ecb16922750
Daniel Baker <dbaker@FreeBSD.org>
1999-12-26 22:52:33 +0000

    Initial import of "dnetc."
    dnetc is the new line of distributed.net clients (formally in
    the ports collection as misc/rc5des).

---
+++ misc/dnetc/files/dnetc.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+dir="CHANGETHIS"
+
+if [ ! -d $dir ]; then
+ logger -i "dnetc client directory doesn't exist. ($dir)."
+ exit
+fi
+
+if [ ! -f $dir/dnetc ]; then
+ logger -i "dnetc client doesn't exist. ($dir/rc5des)."
+ exit
+fi
+
+if [ ! -f $dir/dnetc.ini ]; then
+ logger -i "dnetc client config file doesn't exist. ($dir/rc5des.ini)."
+ exit
+fi
+
+email=`grep "^id=" $dir/dnetc.ini|cut -d"=" -f2`
+
+if [ $email = "rc5@distributed.net" ]; then
+ logger -i "dnetc client running with default email address. ($email)"
+fi
+
+echo -n " dnetc"
+su -m nobody -c "$dir/dnetc -quiet" 2>/dev/null >/dev/null &
