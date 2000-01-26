04fb81e5f18ccb33c583af058a162a453d52434d
joe <joe@FreeBSD.org>
2000-01-26 22:06:13 +0000

    Virgin import of Vladimir N. Silyaev's port of the linux version of the VMWare virtual machine.
    PR:		ports/16337
    Submitted by:	vsilyaev@mindspring.com

---
+++ emulators/vmware2/files/vmware.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+# Start or stop vmware
+#
+
+vmware_dir=@@PREFIX@@/lib/vmware
+networking=@@NETWORKING@@
+host_ip=@@HOST_IP@@
+netmask=@@NETMASK@@
+
+[ -x $vmware_dir/bin/vmware ] || exit
+exec >/dev/null 
+
+case $1 in
+	start)
+        	kldload ${vmware_dir}/lib/modules/vmmon.ko
+		if [ $networking -eq 1 ]; then
+        		kldload ${vmware_dir}/lib/modules/vmnet.ko
+			echo -n >/dev/vmnet1
+			ifconfig vmnet1 $host_ip netmask $netmask
+		fi
+		echo -n " VMware" >/dev/tty
+		;;
+
+	stop)
+        	kldunload vmmon
+		if [ $networking -eq 1 ]; then
+			ifconfig vmnet1 down
+			ifconfig vmnet1 delete $host_ip
+        		kldunload vmnet
+		fi	
+		;;
+
+	*)
+		echo "usage: `basename $0` {start|stop}" >&2 
+		exit 64
+		;;
+esac
