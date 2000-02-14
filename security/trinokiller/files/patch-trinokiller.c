47878a089b1e9bbf13aba188512cd56babea2b60
David E. O'Brien <obrien@FreeBSD.org>
2000-02-14 03:46:32 +0000

    This program remotely kills trino nodes on version 1.07b2+f3 and below. It abuses the careless coding in trinoo nodes and causes them to exit.

---
+++ security/trinokiller/files/patch-trinokiller.c
@@ -0,0 +1,10 @@
+--- trinokiller.c.orig	Sun Feb 13 19:41:24 2000
++++ trinokiller.c	Sun Feb 13 19:41:48 2000
+@@ -12,6 +12,7 @@
+  *
+  */
+ 
++#include <sys/types.h>
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <sys/socket.h>
