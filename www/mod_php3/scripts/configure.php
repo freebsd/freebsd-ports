44c2f43c629b46fec387850ea9df5cc7625cb3f7
kris <kris@FreeBSD.org>
2000-02-22 07:26:35 +0000

    USE_OPENSSL
    Submitted by:	Jim Bloom <bloom@acm.org>

--- www/mod_php3/scripts/configure.php
+++ www/mod_php3/scripts/configure.php
@@ -127,20 +127,20 @@ while [ "$1" ]; do
 PKGNAME=	apache+php+mod_ssl-\${VERSION_APACHE}+\${VERSION_PHP}+\${VERSION_MODSSL}
 DISTFILES+=	mod_ssl-\${VERSION_MODSSL}-\${VERSION_APACHE}\${EXTRACT_SUFX}
 
-BUILD_DEPENDS+=	openssl:\${PORTSDIR}/security/openssl \\
-		mm-config:\${PORTSDIR}/devel/mm \\
+USE_OPENSSL=	RSA
+
+.include <bsd.port.pre.mk>
+
+BUILD_DEPENDS+=	mm-config:\${PORTSDIR}/devel/mm \\
 		\${PREFIX}/lib/libmm.a:\${PORTSDIR}/devel/mm
-LIB_DEPENDS+=	crypto.1:\${PORTSDIR}/security/openssl \\
-		ssl.1:\${PORTSDIR}/security/openssl
-RUN_DEPENDS+=	openssl:\${PORTSDIR}/security/openssl
 
 VERSION_MODSSL=	2.5.0
 
-RESTRICTED=	"Contains cryptography"
-
 CONFIGURE_ARGS+=--enable-module=ssl \\
 		--enable-module=define
-CONFIGURE_ENV+=	SSL_BASE='SYSTEM' EAPI_MM='SYSTEM' PATH="\${PREFIX}/bin:\${PATH}"
+CONFIGURE_ENV+=	SSL_BASE='SYSTEM' RSA_BASE='SYSTEM' EAPI_MM='SYSTEM' PATH="\${PREFIX}/bin:\${PATH}"
+
+EXTRA_PATCHES+=	\${PATCHDIR}/ssl_patch-aa
 
 PLIST=		\${PKGDIR}/PLIST.modssl
 SSL=		ssl
