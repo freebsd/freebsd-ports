05935329df6fa9a665144a2c4e8a2bc54edd6022
dirk <dirk@FreeBSD.org>
2000-02-27 19:50:36 +0000

    Upgrade to Apache 1.3.12 and mod_ssl 2.6.0.

--- www/mod_php5/scripts/configure.php
+++ www/mod_php5/scripts/configure.php
@@ -142,7 +142,7 @@ USE_OPENSSL=	RSA
 BUILD_DEPENDS+=	mm-config:\${PORTSDIR}/devel/mm \\
 		\${PREFIX}/lib/libmm.a:\${PORTSDIR}/devel/mm
 
-VERSION_MODSSL=	2.5.0
+VERSION_MODSSL=	2.6.0
 
 CONFIGURE_ARGS+=--enable-module=ssl \\
 		--enable-module=define
