a71e988cc9b0b66d3fbc1939ca81b5a797b29bb4
dbaker <dbaker@FreeBSD.org>
2000-02-27 20:07:13 +0000

    Correct a few missed rc5des->dnetc issues from the conversion. Fix a potential syntax error.

--- misc/dnetc/files/dnetc.sh
+++ misc/dnetc/files/dnetc.sh
@@ -8,18 +8,18 @@ if [ ! -d $dir ]; then
 fi
 
 if [ ! -f $dir/dnetc ]; then
- logger -i "dnetc client doesn't exist. ($dir/rc5des)."
+ logger -i "dnetc client doesn't exist. ($dir/dnetc)."
  exit
 fi
 
 if [ ! -f $dir/dnetc.ini ]; then
- logger -i "dnetc client config file doesn't exist. ($dir/rc5des.ini)."
+ logger -i "dnetc client config file doesn't exist. ($dir/dnetc.ini)."
  exit
 fi
 
 email=`grep "^id=" $dir/dnetc.ini|cut -d"=" -f2`
 
-if [ $email = "rc5@distributed.net" ]; then
+if [ "$email" = "rc5@distributed.net" ]; then
  logger -i "dnetc client running with default email address. ($email)"
 fi
 
