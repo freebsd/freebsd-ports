a8ecbfc6fa48413552209e0f22a07b44aac8a717
dirk <dirk@FreeBSD.org>
2000-02-27 21:14:28 +0000

    Sometimes graphics/gd depends on print/freetype sometimes not...
    So split GD and FreeType into separate options again.

--- www/mod_php5/scripts/configure.php
+++ www/mod_php5/scripts/configure.php
@@ -15,7 +15,8 @@ fi
 Please select desired options:" -1 -1 14 \
 tuning		"Apache: performance tuning" OFF \
 modssl		"Apache: SSL support" OFF \
-GD		"PHP:    GD support & FreeType font rendering" OFF \
+GD		"PHP:    GD library support" OFF \
+FreeType	"PHP:    TrueType font rendering (implies GD)" OFF \
 zlib		"PHP:    zlib library support" ON \
 mcrypt		"PHP:    Encryption support" OFF \
 mhash		"PHP:    Crypto-hashing support" OFF \
@@ -59,9 +60,15 @@ while [ "$1" ]; do
 			;;
 		\"GD\")
 			echo "LIB_DEPENDS+=	gd.0:\${PORTSDIR}/graphics/gd"
-			echo "LIB_DEPENDS+=	ttf.4:\${PORTSDIR}/print/freetype"
 			echo "PHP_CONF_ARGS+=	--with-gd=shared,\${PREFIX}"
+			GD=1
+			;;
+		\"FreeType\")
+			echo "LIB_DEPENDS+=	ttf.4:\${PORTSDIR}/print/freetype"
 			echo "PHP_CONF_ARGS+=	--with-ttf=\${PREFIX}"
+			if [ -z "$GD" ]; then
+				set $* \"GD\"
+			fi
 			;;
 		\"zlib\")
 			echo "PHP_CONF_ARGS+=	--with-zlib"
