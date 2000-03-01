51b9eaadc260eff898cc1598a332bf701ea61ae2
Dirk Froemberg <dirk@FreeBSD.org>
2000-03-01 22:45:34 +0000

    sybase and sybase-ct are mutually exclusive. So split Sybase into SybaseDB and SybaseCT and yell if there are selected both.
    Reported by:	Dmitry Petrov <DPetrov@nchcapital.com>

--- www/mod_php3/scripts/configure.php
+++ www/mod_php3/scripts/configure.php
@@ -25,7 +25,8 @@ IMAP		"PHP:    IMAP support" OFF \
 MySQL		"PHP:    MySQL database support" ON \
 PostgreSQL	"PHP:    PostgreSQL database support" OFF \
 mSQL		"PHP:    mSQL database support" OFF \
-Sybase		"PHP:    Sybase/MS-SQL database support" OFF \
+SybaseDB	"PHP:    Sybase/MS-SQL database support (DB-lib)" OFF \
+SybaseCT	"PHP:    Sybase/MS-SQL database support (CT-lib)" OFF \
 dBase		"PHP:    dBase database support" OFF \
 OpenLDAP	"PHP:    OpenLDAP support" OFF \
 SNMP		"PHP:    SNMP support" OFF \
@@ -111,11 +112,25 @@ while [ "$1" ]; do
 			echo "BUILD_DEPENDS+=	msql:\${PORTSDIR}/databases/msql"
 			echo "PHP_CONF_ARGS+=	--with-msql=\${PREFIX}"
 			;;
-		\"Sybase\")
+		\"SybaseDB\")
 			echo "LIB_DEPENDS+=	sybdb.0:\${PORTSDIR}/databases/freetds"
-			echo "LIB_DEPENDS+=	ct.0:\${PORTSDIR}/databases/freetds"
 			echo "PHP_CONF_ARGS+=	--with-sybase=\${PREFIX}"
+			if [ "$SYBASECT" ]; then
+				echo "SybaseDB and SybaseCT are mutually exclusive." > /dev/stderr
+				rm -f ${WRKDIRPREFIX}${CURDIR}/Makefile.inc
+				exit 1
+			fi
+			SYBASEDB=1
+			;;
+		\"SybaseCT\")
+			echo "LIB_DEPENDS+=	ct.0:\${PORTSDIR}/databases/freetds"
 			echo "PHP_CONF_ARGS+=	--with-sybase-ct=\${PREFIX}"
+			if [ "$SYBASEDB" ]; then
+				echo "SybaseDB and SybaseCT are mutually exclusive." > /dev/stderr
+				rm -f ${WRKDIRPREFIX}${CURDIR}/Makefile.inc
+				exit 1
+			fi
+			SYBASECT=1
 			;;
 		\"dBase\")
 			echo "PHP_CONF_ARGS+=	--with-dbase"
