beedd2eaac2616ca036c2b85790b3ef658ee9bc4
knu <knu@FreeBSD.org>
2000-03-25 20:24:32 +0000

    - Clean up and minor fixes   - Make portlint satisfy   - Avoid creating files out of WRKDIR   - Remove the void directory "patch" (What ever was this?)   - Adjust whitespace   - Fix some ungrammatical wordings   - Modify COMMENT and DESCR - Drop fakeprocfs support due to the import of Linux procfs to the base system - Show MESSAGE after installation - Change maintainer to the -emulation list because:   - Vladimir seems too busy or unreachable these days   - This port depends very much on Linux compatibility stuff - Get it to check if the system has Linux procfs module and stop if not
    PR:		ports/16960 (add WWW to DESCR)
    No response by:	maintainer

--- emulators/vmware3/files/vmware.sh
+++ emulators/vmware3/files/vmware.sh
@@ -3,7 +3,6 @@
 # Start or stop vmware
 #
 # $FreeBSD$
-#
 
 vmware_dir=@@PREFIX@@/lib/vmware
 networking=@@NETWORKING@@
@@ -14,27 +13,27 @@ netmask=@@NETMASK@@
 exec >/dev/null
 
 case $1 in
-	start)
-        	kldload ${vmware_dir}/lib/modules/vmmon.ko
-		if [ $networking -eq 1 ]; then
-        		kldload ${vmware_dir}/lib/modules/vmnet.ko
-			echo -n >/dev/vmnet1
-			ifconfig vmnet1 $host_ip netmask $netmask
-		fi
-		echo -n " VMware" >/dev/tty
-		;;
+start)
+    kldload ${vmware_dir}/lib/modules/vmmon.ko
+    if [ $networking -eq 1 ]; then
+	kldload ${vmware_dir}/lib/modules/vmnet.ko
+	echo -n >/dev/vmnet1
+	ifconfig vmnet1 $host_ip netmask $netmask
+    fi
+    echo -n " VMware" >/dev/tty
+    ;;
 
-	stop)
-        	kldunload vmmon
-		if [ $networking -eq 1 ]; then
-			ifconfig vmnet1 down
-			ifconfig vmnet1 delete $host_ip
-        		kldunload vmnet
-		fi
-		;;
+stop)
+    kldunload vmmon
+    if [ $networking -eq 1 ]; then
+	ifconfig vmnet1 down
+	ifconfig vmnet1 delete $host_ip
+	kldunload vmnet
+    fi
+    ;;
 
-	*)
-		echo "usage: `basename $0` {start|stop}" >&2
-		exit 64
-		;;
+*)
+    echo "usage: `basename $0` {start|stop}" >&2
+    exit 64
+    ;;
 esac
