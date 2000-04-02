ee03bd5f3cd37c9a41be131558535c73472e4e58
dirk <dirk@FreeBSD.org>
2000-04-02 22:15:13 +0000

    - move startup file from Makefile to files/mysql-server.sh - check for start/stop arguments in startup file - don't rip off --force option from mysql_install_db

---
+++ databases/mysql54-server/files/mysql-server.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+case "$1" in
+	start)
+		if [ -x %%PREFIX%%/bin/safe_mysqld ]; then
+			%%PREFIX%%/bin/safe_mysqld --user=mysql > /dev/null & && echo -n ' mysqld'
+		fi
+		;;
+	stop)
+		/usr/bin/killall mysqld > /dev/null 2>&1 && echo -n ' mysqld'
+		;;
+	*)
+		echo ""
+		echo "Usage: `basename $0` { start | stop }"
+		echo ""
+		exit 1
+		;;
+esac
