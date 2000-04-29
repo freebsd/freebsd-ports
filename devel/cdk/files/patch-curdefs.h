421a2364e78ecc1888e813b38714616c43a59aa9
David E. O'Brien <obrien@FreeBSD.org>
2000-04-29 07:17:06 +0000

    Upgrade to version 4.9.9.
    Submitted by:	KATO Tsuguru <tkato@prontomail.ne.jp>

---
+++ devel/cdk/files/patch-curdefs.h
@@ -0,0 +1,10 @@
+--- include/curdefs.h.orig	Fri Dec 11 13:24:06 1998
++++ include/curdefs.h	Thu Mar 30 22:02:22 2000
+@@ -518,4 +518,7 @@
+ #ifndef ACS_S2
+ #define ACS_S2		'2'
+ #endif
++#ifndef ACS_S9
++#define ACS_S9		'9'
++#endif
+ #endif /* CURDEF_H */
