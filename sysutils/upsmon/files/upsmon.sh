760c82818cd013bcce2a7cce3abd1e019b5f8cc3
steve <steve@FreeBSD.org>
2000-04-30 19:25:13 +0000

    Be a little less chatty on startup in upsmon.sh.
    PR:		17949
    Submitted by:	nik

--- sysutils/upsmon/files/upsmon.sh
+++ sysutils/upsmon/files/upsmon.sh
@@ -3,5 +3,5 @@
 # see !!PREFIX!!/share/doc/upsmon/INSTALL for command line option details
 
 if [ -x !!PREFIX!!/sbin/upsmond ]; then
-	!!PREFIX!!/sbin/upsmond -p /dev/cuaa0 && echo -n ' upsmond'
+	!!PREFIX!!/sbin/upsmond -p /dev/cuaa0 2>&1 > /dev/null && echo -n ' upsmond'
 fi
