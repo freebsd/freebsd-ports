1d0d02363b999d9d2056db244f6d54ec1f79f352
Dirk Froemberg <dirk@FreeBSD.org>
2000-05-02 14:02:03 +0000

    Upgrade to mod_ssl 2.6.4.

--- www/mod_php3/scripts/configure.php
+++ www/mod_php3/scripts/configure.php
@@ -166,7 +166,7 @@ USE_OPENSSL=	RSA
 BUILD_DEPENDS+=	mm-config:\${PORTSDIR}/devel/mm \\
 		\${PREFIX}/lib/libmm.a:\${PORTSDIR}/devel/mm
 
-VERSION_MODSSL=	2.6.3
+VERSION_MODSSL=	2.6.4
 
 CONFIGURE_ARGS+=--enable-module=ssl \\
 		--enable-module=define
