dd9ae51b98feb4f8732da91d1337f72551b9c865
dirk <dirk@FreeBSD.org>
2000-05-16 20:26:34 +0000

    Add option for FTP support.
    Submitted by:	Eric D. Futch <efutch@nyct.net>

--- www/mod_php5/scripts/configure.php
+++ www/mod_php5/scripts/configure.php
@@ -27,6 +27,7 @@ dBase		"PHP:    dBase database support" OFF \
 OpenLDAP	"PHP:    OpenLDAP support" OFF \
 SNMP		"PHP:    SNMP support" OFF \
 XML		"PHP:    XML support" OFF \
+FTP		"PHP:    File Transfer Protocol support" OFF \
 2> /tmp/checklist.tmp.$$
 
 	retval=$?
@@ -135,6 +136,9 @@ while [ "$1" ]; do
 			echo "BUILD_DEPENDS+=	\${PREFIX}/include/xml/xmlparse.h:\${PORTSDIR}/textproc/expat"
 			echo "BUILD_DEPENDS+=	\${PREFIX}/include/xml/xmltok.h:\${PORTSDIR}/textproc/expat"
 			echo "PHP_CONF_ARGS+=	--with-xml=\${PREFIX}"
+			;;
+		\"FTP\")
+			echo "PHP_CONF_ARGS+=	--with-ftp"
 			;;
 		\"modssl\")
 			cat << EOF
