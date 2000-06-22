7a59d1b67eec9b94a239fff0cc90188b91fc4af1
Will Andrews <will@FreeBSD.org>
2000-06-22 05:31:13 +0000

    Remove a dead MASTER_SITE_WINDOWMAKER and MASTER_SITE_SUNSITE.
    PR:		19306
    Submitted by:	Trevor Johnson <trevor@jpj.net> (SUNSITE)
    		KATO Tsuguru <tkato@prontomail.ne.jp> (WINDOWMAKER)

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -79,7 +79,6 @@ MASTER_SITE_TEX_CTAN+=  \
 
 MASTER_SITE_SUNSITE+=	\
 	ftp://metalab.unc.edu/pub/Linux/%SUBDIR%/ \
-	ftp://ftp.infomagic.com/pub/mirrors/linux/sunsite/%SUBDIR%/ \
 	ftp://ftp.freesoftware.com/pub/linux/sunsite/%SUBDIR%/ \
 	ftp://ftp.sourceforge.net/pub/mirrors/metalab/Linux/%SUBDIR%/ \
 	ftp://ftp.sun.ac.za/pub/linux/sunsite/%SUBDIR%/ \
@@ -141,7 +140,6 @@ MASTER_SITE_AFTERSTEP+=	\
 MASTER_SITE_WINDOWMAKER+= \
 	ftp://ftp.windowmaker.org/pub/%SUBDIR%/ \
 	ftp://ftp.goldweb.com.au/pub/WindowMaker/%SUBDIR%/ \
-	ftp://ftp.io.com/pub/%SUBDIR%/ \
 	ftp://ftp.kddlabs.co.jp/X11/window_managers/windowmaker/%SUBDIR%/ \
 	ftp://ftp.ameth.org/pub/mirrors/ftp.windowmaker.org/%SUBDIR%/ \
 	ftp://ftp.minet.net/pub/windowmaker/%SUBDIR%/ \
