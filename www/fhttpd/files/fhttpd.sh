df1f78d4befe3b5050493f8c152b2ef65816ddae
jedgar <jedgar@FreeBSD.org>
2000-07-14 01:55:13 +0000

    Convert startup script to {start|stop} usage

--- www/fhttpd/files/fhttpd.sh
+++ www/fhttpd/files/fhttpd.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-[ -x /usr/local/sbin/fhttpd ] && /usr/local/sbin/fhttpd -c /usr/local/etc/fhttpd.conf -l /usr/local/share/fhttpd/fhttpd.log >/dev/null 2>&1 && echo -n " fhttpd"
+
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
+	echo "$0: Cannot determine the PREFIX" >&2
+	exit 1
+fi
+
+case "$1" in
+start)
+	[ -x ${PREFIX}/sbin/fhttpd ] && ${PREFIX}/sbin/fhttpd -c ${PREFIX}/etc/fhttpd.conf -l ${PREFIX}/share/fhttpd/fhttpd.log > /dev/null 2>&1 && echo -n ' fhttpd'
+	;;
+stop)
+	killall fhttpd && echo -n ' fhttpd'
+	;;
+*)
+	echo "Usage: `basename $0` {start|stop}" >&2
+	;;
+esac
+
+exit 0
