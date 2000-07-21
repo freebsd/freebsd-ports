bd2fc1f005f43faa3990090ce7f26f6c51f48d9a
David E. O'Brien <obrien@FreeBSD.org>
2000-07-21 05:42:18 +0000

    Upgrade to version 3.0b1pl17.

--- net/isc-dhcp3/files/patch-clparse.c
+++ net/isc-dhcp3/files/patch-clparse.c
@@ -1,11 +1,5 @@
---- client/clparse.c.orig	Wed May 10 02:20:11 2000
-+++ client/clparse.c	Wed May 10 02:20:31 2000
-@@ -74,7 +74,7 @@
- 	top_level_config.backoff_cutoff = 15;
- 	top_level_config.initial_interval = 3;
- 	top_level_config.bootp_policy = P_ACCEPT;
--	top_level_config.script_name = "/etc/dhclient-script";
-+	top_level_config.script_name = CL_DEFAULT_SCRIPT_NAME;
- 	top_level_config.requested_options = default_requested_options;
- 
- 	top_level_config.on_receipt = new_group ("read_client_conf");
+--- client/clparse.c.orig	Wed Jul 19 21:13:10 2000
++++ client/clparse.c	Thu Jul 20 21:52:53 2000
+@@ -46 +46 @@
+-char client_script_name [] = "/etc/dhclient-script";
++char client_script_name [] = CL_DEFAULT_SCRIPT_NAME;
