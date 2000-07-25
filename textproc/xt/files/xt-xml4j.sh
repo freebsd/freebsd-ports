cbb284e4300a0589a3aa56eaad98abd1063495a6
kuriyama <kuriyama@FreeBSD.org>
2000-07-25 07:24:02 +0000

    Modify class path in xt-xml4j.sh and add xt-xp.sh.
    Submitted by:	dirk

--- textproc/xt/files/xt-xml4j.sh
+++ textproc/xt/files/xt-xml4j.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # $FreeBSD$
 
-export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xml4j.jar:${CLASSPATH}
+export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xerces.jar:${CLASSPATH}
 
-java -Dcom.jclark.xsl.sax.parser=com.ibm.xml.parsers.SAXParser com.jclark.xsl.sax.Driver $*
-
+java -Dcom.jclark.xsl.sax.parser=org.apache.xerces.parsers.SAXParser com.jclark.xsl.sax.Driver $*
