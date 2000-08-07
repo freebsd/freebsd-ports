b8c25cfe25f18373d39eefcffe67e44cfcc3441c
sobomax <sobomax@FreeBSD.org>
2000-08-07 11:31:22 +0000

    Update to 0.86. - gnomify and sobomaxify the port; - provide a shell script to make dia work just OOB.
    PR:		18915 (partially)
    Submitted by:	Benno Rice <benno@netizen.com.au>

---
+++ graphics/dia/files/dia.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+DIA_LIB_PATH=$DIA_LIB_PATH:%%PREFIX%%/lib/dia
+DIA_SHAPE_PATH=%%PREFIX%%/share/gnome/dia/shapes
+DIA_SHEET_PATH=%%PREFIX%%/share/gnome/dia/sheets
+export DIA_LIB_PATH DIA_SHAPE_PATH DIA_INT_SHAPE_PATH DIA_SHEET_PATH
+
+exec %%PREFIX%%/bin/dia-bin $*
