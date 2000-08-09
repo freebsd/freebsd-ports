5240ab0753cd52085330056b6b944330ec869863
David E. O'Brien <obrien@FreeBSD.org>
2000-08-09 09:23:43 +0000

    Update to version 1.38.
    Reported by:	Chris D. Faulhaber <jedgar@fxp.org>

--- net/ngrep/files/patch-ngrep.c
+++ net/ngrep/files/patch-ngrep.c
@@ -1,19 +1,7 @@
---- ngrep.c.orig	Fri Oct 22 05:21:26 1999
-+++ ngrep.c	Mon Jan 31 12:58:36 2000
-@@ -59,6 +59,7 @@
- int link_offset;
- pcap_t *pd;
- 
+--- ngrep.c.orig	Mon Jun 26 12:52:50 2000
++++ ngrep.c	Wed Aug  9 02:16:59 2000
+@@ -50,0 +51 @@
 +extern FILE *yyin;
- 
- int main(int argc, char **argv) {
-   char c;
-@@ -141,7 +142,7 @@
-       free(filter); 
-       filter = get_filter(&argv[optind-1]); 
- 
+@@ -199 +200 @@
 -      PCAP_RESTART();
 +      PCAP_RESTART(yyin);
-       if (pcap_compile(pd,&pcapfilter,filter,0,mask.s_addr)) {
- 	pcap_perror(pd,"pcap compile");
- 	exit(-1);
