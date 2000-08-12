ae98b5718eaddc8ff4ced5d511d2fee52232f331
nbm <nbm@FreeBSD.org>
2000-08-12 19:13:06 +0000

    Update to 4.08
    PR:		ports/19936
    Submitted by:	Simon Marlow <simonmar@microsoft.com> (maintainer)

--- lang/ghc5/files/build.mk
+++ lang/ghc5/files/build.mk
@@ -1,5 +1,5 @@
 ProjectsToBuild = glafp-utils hslibs ghc
-GhcLibHcOpts = -O -split-objs -odir $*
+GhcLibHcOpts = -O -split-objs -H32m
 SRC_HAPPY_OPTS += -c
-GhcLibWays=
+GhcLibWays=p
 GhcLibsWithReadline = YES
