35577fddec24ca361813cd5c97d2f535e0fd7340
David E. O'Brien <obrien@FreeBSD.org>
2000-08-27 00:35:58 +0000

    "Fix a small nit that corrects an oversight in one of our newer features..."
    Submitted by:	Dan Frasnelli <dfrasnel@corewar.com> (ngrep developer)

--- net/ngrep/files/patch-ngrep.c
+++ net/ngrep/files/patch-ngrep.c
@@ -1,7 +1,28 @@
 --- ngrep.c.orig	Mon Jun 26 12:52:50 2000
-+++ ngrep.c	Wed Aug  9 02:16:59 2000
-@@ -50,0 +51 @@
++++ ngrep.c	Sat Aug 26 17:34:40 2000
+@@ -48,6 +48,7 @@
+ #include "regex.h"
+ #include "ngrep.h"
+ 
 +extern FILE *yyin;
-@@ -199 +200 @@
+ 
+ static char rcsver[] = "$Revision: 1.38 $";
+ 
+@@ -196,7 +197,7 @@
+       free(filter); 
+       filter = get_filter(&argv[optind-1]); 
+ 
 -      PCAP_RESTART();
 +      PCAP_RESTART(yyin);
+       if (pcap_compile(pd, &pcapfilter, filter, 0, mask.s_addr)) {
+ 	pcap_perror(pd, "pcap compile");
+ 	clean_exit(-1);
+@@ -653,7 +654,7 @@
+   struct tm *t = localtime(&h->ts.tv_sec);
+ 
+   printf("%02d/%02d/%02d %02d:%02d:%02d.%06d ",
+-	 t->tm_year+1900, t->tm_mon, t->tm_mday, t->tm_hour,
++	 t->tm_year+1900, t->tm_mon+1, t->tm_mday, t->tm_hour,
+ 	 t->tm_min, t->tm_sec, h->ts.tv_usec);
+ }
+ 
