9a3f0edd65a527cbaa3d789ff955ad2891c36980
tg <tg@FreeBSD.org>
2000-09-22 11:43:23 +0000

    Upgrade to 16.1.1.
    PR:		21446
    Submitted by:	Johann Visagie <johann@egenetics.com>

--- math/py-numeric17/files/patch-setup.py
+++ math/py-numeric17/files/patch-setup.py
@@ -1,32 +1,12 @@
 --- setup.py.orig	Tue Sep 12 22:27:29 2000
-+++ setup.py	Mon Sep 18 10:00:53 2000
-@@ -22,9 +22,17 @@
- 
- 
- # Locate the directories in which the BLAS and LAPACK are located.
--# if you know where they are, put it here:
--BLASLIBDIR = None
--LAPACKLIBDIR = None  
-+# if you know where they are, pass them via your environment or edit
-+# the except: case.
-+try:
-+    BLASLIBDIR = os.environ['BLASLIBDIR']
-+except KeyError:
-+    BLASLIBDIR = None
-+try:
-+    LAPACKLIBDIR = os.environ['LAPACKLIBDIR']
-+except KeyError:
-+    LAPACKLIBDIR = None
-+
++++ setup.py	Thu Sep 21 17:17:55 2000
+@@ -27,6 +27,9 @@
+ LAPACKLIBDIR = None  
  # places to look and what to look for
  POSSIBLE_DIRECTORIES = ['/usr/local/lib', '/usr/lib', 'lapack_lite']
++# add sys.prefix + '/lib', on the assumption that python was
++# installed in the same directory as all other local software
++POSSIBLE_DIRECTORIES.append(sys.prefix + '/lib')
  if sys.platform == 'win32':
-@@ -91,7 +99,7 @@
-                       Extension('umath', ['Src/umathmodule.c']),
-                       Extension('lapack_lite',
-                                 ['Src/lapack_litemodule.c'],
--                                libraries=['lapack', 'blas'],
-+                                libraries=['lapack', 'blas', 'g2c'],
-                                 library_dirs=[BLASLIBDIR, LAPACKLIBDIR]),
-                       Extension('ranlib',
-                                 ['Src/ranlibmodule.c',
+     BLASNAMES = ['blas.lib']
+     LAPACKNAMES = ['lapack.lib']
