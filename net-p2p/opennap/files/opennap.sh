8c7499bdfa85657ea6c2517ada0ef4a4db489beb
cvs2svn <cvs2svn@FreeBSD.org>
2000-09-23 01:36:53 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_4_1_1'.

--- net-p2p/opennap/files/opennap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/local/bin/opennap >/dev/null 2>&1 &
-echo -n ' opennap'
--- net/isc-dhcp31-server/files/patch-clparse.c
+++
@@ -1,5 +0,0 @@
---- client/clparse.c.orig	Wed Jul 19 21:13:10 2000
-+++ client/clparse.c	Thu Jul 20 21:52:53 2000
-@@ -46 +46 @@
--char client_script_name [] = "/etc/dhclient-script";
-+char client_script_name [] = CL_DEFAULT_SCRIPT_NAME;
