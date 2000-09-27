fd9d27296256d06fd016c0b39ccc3619b0943805
David E. O'Brien <obrien@FreeBSD.org>
2000-09-27 10:36:13 +0000

    Update to version 3.0 Beta 2, Patchlevel l5.

--- net/isc-dhcp40-server/files/patch-dhclient.c
+++ net/isc-dhcp40-server/files/patch-dhclient.c
@@ -1,32 +1,32 @@
---- client/dhclient.c.orig	Wed Jul 19 23:13:11 2000
-+++ client/dhclient.c	Sun Aug  6 20:43:14 2000
-@@ -64,6 +64,7 @@
- int log_priority;
+--- client/dhclient.c.orig	Thu Sep 14 05:42:01 2000
++++ client/dhclient.c	Wed Sep 27 03:31:33 2000
+@@ -78,6 +78,7 @@
+ u_int16_t remote_port;
  int no_daemon;
  int save_scripts;
 +int onetry;
  
  static void usage PROTO ((void));
  
-@@ -120,6 +121,8 @@
- 			if (++i == argc)
- 				usage ();
- 			server = argv [i];
+@@ -176,6 +177,8 @@
+ 		} else if (!strcmp (argv [i], "-w")) {
+ 			/* do not exit if there are no broadcast interfaces. */
+ 			persist = 1;
 +		} else if (!strcmp (argv [i], "-1")) {
 +			onetry = 1;
   		} else if (argv [i][0] == '-') {
   		    usage ();
   		} else {
-@@ -273,7 +276,7 @@
+@@ -417,7 +420,7 @@
+ 	log_info (arr);
+ 	log_info (url);
  
- static void usage ()
- {
 -	log_error ("Usage: dhclient [-d] [-D] [-q] [-p <port>] %s",
-+	log_error ("Usage: dhclient [-d] [-D] [-q] [-1] [-p <port>] %s",
++	log_error ("Usage: dhclient [-1] [-d] [-D] [-q] [-p <port>] %s",
  		   "[-s server]");
- 	log_error ("                [-lf lease-file] [-pf pid-file]%s",
+ 	log_fatal ("                [-lf lease-file] [-pf pid-file]%s",
  		   "[-cf config-file] [interface]");
-@@ -1239,6 +1242,10 @@
+@@ -1402,6 +1405,10 @@
  	/* No leases were available, or what was available didn't work, so
  	   tell the shell script that we failed to allocate an address,
  	   and try again later. */
@@ -34,15 +34,6 @@
 +		exit(2);
 +		log_info ("Unable to obtain a lease on first try - exiting.\n");
 +	}
- 	log_info ("No working leases in persistent database - sleeping.\n");
+ 	log_info ("No working leases in persistent database - sleeping.");
  	script_init (client, "FAIL", (struct string_list *)0);
  	if (client -> alias)
-@@ -2157,7 +2164,7 @@
- 
- int dhcp_option_ev_name (buf, buflen, option)
- 	char *buf;
--	unsigned buflen;
-+	size_t buflen;
- 	struct option *option;
- {
- 	int i;
