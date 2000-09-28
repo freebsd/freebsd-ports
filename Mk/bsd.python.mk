db799bc7ded93b1163ff680b91cd9e6da5d7d18f
Thomas Gellekum <tg@FreeBSD.org>
2000-09-28 11:24:31 +0000

    - Upgrade version number for Python 2.0b2. - Use full path for PYTHON_CMD - Add PYTHON_PKGNAMEPREFIX - Add PYTHON_REL - Prevent against multiple inclusion

--- Mk/bsd.python.mk
+++ Mk/bsd.python.mk
@@ -4,6 +4,8 @@
 # $FreeBSD$
 #
 
+.if !defined(Python_Include)
+
 Python_Include=				bsd.python.mk
 Python_Include_MAINTAINER=	tg@FreeBSD.org
 
@@ -16,7 +18,7 @@ Python_Include_MAINTAINER=	tg@FreeBSD.org
 # The variables:
 #
 # PYTHON_CMD:	Python's command line file name, including the version
-#				number (used for dependencies), but not using the full path.
+#				number (used for dependencies).
 #				default: depends on the version of your python binary
 #
 # PYTHON_DISTFILE:	The ${DISTFILE} for your python version. Needed for
@@ -26,6 +28,10 @@ Python_Include_MAINTAINER=	tg@FreeBSD.org
 #
 # PYTHON_LIBDIR:	Base of the python library tree
 #					default: ${LOCALBASE}/lib/python${PYTHON_VERSION}
+#
+# PYTHON_PKGNAMEPREFIX:	Use this as a ${PKGNAMEPREFIX} to distinguish
+#						packages for different Python versions.
+#						default: py${PYTHON_SUFFIX}-
 #
 # PYTHON_PORTSDIR:	The source of your binary's port. Needed for the
 #					RUN_DEPENDS.
@@ -38,6 +44,9 @@ Python_Include_MAINTAINER=	tg@FreeBSD.org
 # PYTHON_SITELIBDIR:	Location of the site-packages tree. Don't change,
 #					unless you know what you do.
 #						default: ${PYTHON_LIBDIR}/site-packages
+#
+# PYTHON_SUFFIX:	Yet another short version number, primarily intended
+#					for ${PYTHON_PKGNAMEPREFIX}.
 #
 # PYTHON_VERSION:	Version of the python binary in your ${PATH}, in the 
 #					format "python2.0".	Set this in your makefile in case you
@@ -62,28 +71,31 @@ Python_Include_MAINTAINER=	tg@FreeBSD.org
 _PYTHON_VERSION!=	(python -c 'import sys; print sys.version[:3]') 2> /dev/null \
 					|| echo 1.6
 PYTHON_VERSION?=	python${_PYTHON_VERSION}
+_PYTHON_SUFFIX=		${_PYTHON_VERSION:S/.//g}
 PYTHON_PORTVERSION!=	(${PYTHON_VERSION} -c 'import string, sys; \
 								print string.split(sys.version)[0]') 2> /dev/null \
 					|| echo 1.6
 
 # Python-2.0
 .if ${PYTHON_VERSION} == "python2.0"
-PYDISTUTILS=	${PYTHON_LIBDIR}/distutils/core.py:${PORTSDIR}/lang/python
-PYXML=			${PYTHON_LIBDIR}/xml/__init__.py:${PORTSDIR}/lang/python
+PYDISTUTILS=	${PYTHON_LIBDIR}/distutils/core.py:${PYTHON_PORTSDIR}
+PYXML=			${PYTHON_LIBDIR}/xml/__init__.py:${PYTHON_PORTSDIR}
 
-PYTHON_DISTFILE=	BeOpen-Python-2.0b1.tar.gz
+PYTHON_DISTFILE=	BeOpen-Python-2.0b2.tar.gz
 PYTHON_PORTSDIR=	${PORTSDIR}/lang/python-beta
-PYTHON_REL=			190
-PYTHON_WRKSRC=		${WRKDIR}/Python-2.0b1
+PYTHON_REL=			191
+PYTHON_SUFFIX=		${_PYTHON_SUFFIX}
+PYTHON_WRKSRC=		${WRKDIR}/Python-2.0b2
 
 # Python-1.6
 .elif ${PYTHON_VERSION} == "python1.6"
-PYDISTUTILS=	${PYTHON_LIBDIR}/distutils/core.py:${PORTSDIR}/misc/py-distutils
+PYDISTUTILS=	${PYTHON_LIBDIR}/distutils/core.py:${PYTHON_PORTSDIR}
 PYXML=			${PYTHON_LIBDIR}/site-packages/xml/__init__.py:${PORTSDIR}/textproc/py-xml
 
 PYTHON_DISTFILE=	Python-1.6.tar.gz
 PYTHON_PORTSDIR=	${PORTSDIR}/lang/python
 PYTHON_REL=			160
+PYTHON_SUFFIX=		# empty, default version
 PYTHON_WRKSRC=		${WRKDIR}/Python-1.6
 
 # Python-1.5
@@ -94,6 +106,7 @@ PYXML=			${PYTHON_LIBDIR}/site-packages/xml/__init__.py:${PORTSDIR}/textproc/py-
 PYTHON_DISTFILE=	py152.tgz
 PYTHON_PORTSDIR=	${PORTSDIR}/lang/python15
 PYTHON_REL=			152
+PYTHON_SUFFIX=		${_PYTHON_SUFFIX}
 PYTHON_WRKSRC=		${WRKDIR}/Python-1.5.2
 
 .else
@@ -103,12 +116,13 @@ PYTHON_WRKSRC=		${WRKDIR}/Python-1.5.2
 	@${FALSE}
 .endif
 
-PYTHON_CMD=		${PYTHON_VERSION}
-PYTHON_LIBDIR=	${LOCALBASE}/lib/${PYTHON_VERSION}
-PYTHON_SITELIBDIR=	${PYTHON_LIBDIR}/site-packages
+PYTHON_CMD=				${LOCALBASE}/bin/${PYTHON_VERSION}
+PYTHON_LIBDIR=			${LOCALBASE}/lib/${PYTHON_VERSION}
+PYTHON_PKGNAMEPREFIX=	py${PYTHON_SUFFIX}-
+PYTHON_SITELIBDIR=		${PYTHON_LIBDIR}/site-packages
 
 # dependencies
-PYTHON_NO_DEPENDS?=	NO
+PYTHON_NO_DEPENDS?=		NO
 
 .if ${PYTHON_NO_DEPENDS} == "NO"
 BUILD_DEPENDS+=	${PYTHON_CMD}:${PYTHON_PORTSDIR}
@@ -117,3 +131,7 @@ RUN_DEPENDS+=	${PYTHON_CMD}:${PYTHON_PORTSDIR}
 
 # pkg/PLIST substrings
 PLIST_SUB+=		PYTHON_VERSION=${PYTHON_VERSION}
+
+# XXX Hm, should I export some of the variables above to *_ENV?
+
+.endif		# !defined(Python_Include)
