85ea42b265a8e28cd195517822635e87b9ac62d2
kevlo <kevlo@FreeBSD.org>
2000-10-20 15:31:09 +0000

    Import big5fs-1.0 big5fs reads Big5 filenames on Joliet and VFAT filesystems.
    PR: 22146
    Submitted by: Jing-Tang Keith Jang <keith@freebsd.sinica.edu.tw>

---
+++ chinese/big5fs/files/big5fs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+case "$1" in
+start)
+	if [ -x @@PREFIX@@/modules/big5cd9660.ko ]; then
+		echo -n 'Big5 cd9660'
+		kldload /usr/local/modules/big5cd9660.ko
+	fi
+	if [ -x @@PREFIX@@/modules/big5msdos.ko ]; then
+		echo -n 'Big5 msdos'
+		kldload /usr/local/modules/big5msdos.ko
+	fi
+	;;
+stop)
+	kldunload -v -n big5cd9660 && echo -n 'Big5 cd9660'
+	kldunload -v -n big5msdos && echo -n 'Big5 msdos'
+	;;
+*)
+	echo "Usage: big5fs.sh {start|stop}" >&2
+	exit 1
+	;;
+esac
