e2d3ad85083d3c969cf305d321ecf5d228712a8e
Thomas Gellekum <tg@FreeBSD.org>
2000-10-23 12:37:30 +0000

    Upgrade to 1.6.

---
+++ graphics/graphviz/files/patch-lefty:ws:x11:libfilereq:SFinternal.h
@@ -0,0 +1,10 @@
+--- lefty/ws/x11/libfilereq/SFinternal.h.orig	Tue Apr 18 05:56:37 2000
++++ lefty/ws/x11/libfilereq/SFinternal.h	Tue Oct 17 18:27:51 2000
+@@ -29,6 +29,7 @@
+ #include <X11/Xos.h>
+ #include <X11/Xaw/Text.h>
+ #include <X11/Xaw/AsciiText.h>
++#include <sys/types.h>
+ 
+ #define SEL_FILE_CANCEL		-1
+ #define SEL_FILE_OK		0
