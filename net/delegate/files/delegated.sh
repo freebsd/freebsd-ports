4faf354efe6fb9d30ccc76448ca9869e0a750b6c
knu <knu@FreeBSD.org>
2000-10-29 13:41:28 +0000

    Update to 6.1.19. Add patches for -current.
    PR:		ports/19665 (update to 6.1.16), ports/19914 (update to 6.1.17)
    Submitted by:	Naoki IDO <ido@hitachi-ms.co.jp>
    Additional patches by:	knu

--- net/delegate/files/delegated.sh
+++ net/delegate/files/delegated.sh
@@ -3,12 +3,11 @@
 umask 027
 if [ -x /usr/local/sbin/delegated ]; then
         echo -n ' delegated'
-        /usr/local/sbin/delegated -P8080 \
-	MANAGER=delegate-master@your.host.domain \
+	/usr/local/sbin/delegated -P8080 -Txt \
+	ADMIN=delegate-master@your.host.domain \
         CHARCODE=JIS \
         CACHEDIR=/var/spool/delegate/cache \
         EXPIRE=7d \
         RELIABLE=hostname \
         > /dev/null 2>&1
-
 fi
