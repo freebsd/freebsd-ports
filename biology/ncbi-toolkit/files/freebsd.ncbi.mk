7b5cfa8d1a453638bc04a1e490802969017d13f0
jeh <jeh@FreeBSD.org>
2000-10-29 13:56:24 +0000

    New Port: biology/ncbi-toolkit National Center for Biotechnology Information (NCBI) Development Toolkit.
    PR:		22388
    Submitted by:	Tony Maher <tony.maher@ebioinformatics.com>

---
+++ biology/ncbi-toolkit/files/freebsd.ncbi.mk
@@ -0,0 +1,27 @@
+#
+# $Id$ freebsd.ncbi.mk
+#
+# That platform is not supported and is contributed to NCBI by 
+#                     freebsd-bio@freebsd.net
+#
+NCBI_DEFAULT_LCL = bsd
+NCBI_MAKE_SHELL = /bin/sh
+NCBI_CC = cc
+NCBI_CFLAGS1 = ${CFLAGS} -c
+NCBI_LDFLAGS1 = -O
+NCBI_OPTFLAG =
+NCBI_BIN_MASTER = /home/coremake/ncbi/bin
+NCBI_BIN_COPY = /home/coremake/ncbi/bin
+NCBI_INCDIR = /home/coremake/ncbi/include
+NCBI_LIBDIR = /home/coremake/ncbi/lib
+NCBI_ALTLIB = /home/coremake/ncbi/altlib
+#will work only when you have Motif installed!
+NCBI_VIBFLAG = -I/usr/X11R6/include -L/usr/X11R6/lib -DWIN_MOTIF
+NCBI_VIBLIBS = -lXm -lXmu -lXp -lXpm -lXt -lX11 -lXext 
+NCBI_DISTVIBLIBS = -L/usr/X11R6/lib /usr/X11R6/lib/libXm.a /usr/X11R6/lib/libXp.a /usr/X11R6/lib/libXpm.a -lXmu -lXt -lX11 -lXext
+NCBI_OTHERLIBS = -lm
+NCBI_RANLIB = ranlib
+# Used by makedis.csh
+NCBI_MT_OTHERLIBS = 
+NCBI_THREAD_OBJ = ncbithr.o
+NETENTREZVERSION = 2.02c2ASN1SPEC6
