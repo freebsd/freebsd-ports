88fb63b3acee4b262776cb0fbb39830c766cf869
Maxim Sobolev <sobomax@FreeBSD.org>
2000-11-11 16:41:05 +0000

    - Update to 0.3.0; - reassing maintainership from dr@domix.de to jdunn@aquezada.com; - use USE_PYTHON; - pre-compile .py files; - other minor cleanups.
    PR:		22763
    Submitted by:	Julian Dunn <jdunn@aquezada.com> (new maintainer)

--- www/adzapper/files/rc.adzapper.sh
+++ www/adzapper/files/rc.adzapper.sh
@@ -3,7 +3,9 @@
 case $1 in
 start)
 	if [ -f __PREFIX__/etc/adzapper.conf ]; then
-		__PREFIX__/sbin/adzapper &
+		__PREFIX__/sbin/adzapper \
+			1>>/var/log/adzapper.log \
+			2>>/var/log/adzapper.log &
 		test $? -eq 0 && echo -n ' adzapper'
 	fi
 	;;
