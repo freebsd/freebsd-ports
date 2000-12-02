46e1c4f448d6600ada8a20686eafccd63fe7a96e
obrien <obrien@FreeBSD.org>
2000-12-02 08:49:49 +0000

    Allow the selection of the [sys]log facility to use.
    Submitted by:	David W. Chapman Jr. <dwcjr@inethouston.net>

---
+++ net/isc-dhcp3-server/files/patch-site.h
@@ -0,0 +1,5 @@
+--- includes/site.h.orig	Thu Sep 28 11:24:17 2000
++++ includes/site.h.fbsd	Sat Dec  2 00:46:53 2000
+@@ -159 +159 @@
+-/* #define DHCPD_LOG_FACILITY LOG_DAEMON */
++#define DHCPD_LOG_FACILITY LOG_!!USE_FACILITY!!
