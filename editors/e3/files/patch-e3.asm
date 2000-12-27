bd6038efb4bdc90def8cca9a04ee6a86734d06fb
gj <gj@FreeBSD.org>
2000-12-27 22:14:37 +0000

    Add a patch to make Ctrl-D/Ctrl-U behave the way a seasoned vi user expects - without the patch they do exactly the opposite of what is expected.
    Approved by:	the maintainer

---
+++ editors/e3/files/patch-e3.asm
@@ -0,0 +1,13 @@
+--- e3.asm.orig	Wed Dec 27 22:00:59 2000
++++ e3.asm	Wed Dec 27 22:01:04 2000
+@@ -3169,8 +3169,8 @@
+ 	dw KeyVICmdI	-_start	;33h
+ 	dw KeyVICmdR	-_start ;34h
+ 	dw KeyVICmdd	-_start ;35h
+-	dw KeyHalfPgDn	-_start ;36h
+-	dw KeyHalfPgUp	-_start ;37h
++	dw KeyHalfPgUp	-_start ;36h
++	dw KeyHalfPgDn	-_start ;37h
+ 	dw KeyVI1Char	-_start ;38h
+ 	dw KeyEmaCtrlS	-_start ;39h
+ 	dw KeyEmaCtrlR	-_start ;3Ah
