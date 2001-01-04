5c6e15c4924c72bdf08cc688784c932ac059a206
nbm <nbm@FreeBSD.org>
2001-01-04 09:38:37 +0000

    Update to 1.4.
    PR:		24058
    Submitted by:	Joseph Scott <joseph@randomnetworks.com> (maintainer)

--- editors/e3/files/patch-e3.asm
+++ editors/e3/files/patch-e3.asm
@@ -1,13 +1,13 @@
---- e3.asm.orig	Wed Dec 27 22:00:59 2000
-+++ e3.asm	Wed Dec 27 22:01:04 2000
-@@ -3169,8 +3169,8 @@
+--- e3.asm	Wed Jan  3 14:21:19 2001
++++ e3.asm.new	Wed Jan  3 14:21:47 2001
+@@ -3620,8 +3620,8 @@
  	dw KeyVICmdI	-_start	;33h
  	dw KeyVICmdR	-_start ;34h
  	dw KeyVICmdd	-_start ;35h
 -	dw KeyHalfPgDn	-_start ;36h
 -	dw KeyHalfPgUp	-_start ;37h
-+	dw KeyHalfPgUp	-_start ;36h
 +	dw KeyHalfPgDn	-_start ;37h
++	dw KeyHalfPgUp	-_start ;36h
  	dw KeyVI1Char	-_start ;38h
- 	dw KeyEmaCtrlS	-_start ;39h
- 	dw KeyEmaCtrlR	-_start ;3Ah
+ 	dw KeyVIfsearch	-_start ;39h
+ 	dw KeyVIbsearch	-_start ;3Ah
