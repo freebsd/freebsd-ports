ad0ce2c369ceca6cb4ee2cb0385a21a3717b05a4
clive <clive@FreeBSD.org>
2001-01-06 08:38:56 +0000

    o Update opennap from 0.37 -> 0.38 o Correct pkg-plist o Fix the ${PREFIX}/etc/rc.d/opennap.sh, let it support {start|stop}
    PR: 23653
    Submitted by: Joseph Scottj <joseph@randomnetworks.com>
    Timed out from: MAINTAINER

--- audio/opennap/files/opennap.sh
+++ audio/opennap/files/opennap.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-/usr/local/bin/opennap >/dev/null 2>&1 &
-echo -n ' opennap'
+
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
+    echo "$0: Cannot determine the PREFIX" >&2
+    exit 1
+fi
+
+case "$1" in
+start)
+	[ -x ${PREFIX}/sbin/opennap ] && ${PREFIX}/sbin/opennap > /dev/null 2>&1 & && echo -n ' opennap'
+	;;
+stop)
+	[ -r ${PREFIX}/share/opennap/pid ] && kill -15 `cat ${PREFIX}/share/opennap/pid` > /dev/null && rm -f ${PREFIX}/share/opennap/pid &&echo -n ' opennap'
+	;;
+*)
+	echo "Usage: `basename $0` {start|stop}" >&2
+	;;
+esac
+
+exit 0
