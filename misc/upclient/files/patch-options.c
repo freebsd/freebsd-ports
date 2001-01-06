59fc53e8e854533f01842bef4505a70fbbbfc986
Kevin Lo <kevlo@FreeBSD.org>
2001-01-06 17:25:07 +0000

    Update to 4.2.1.21
    PR: 24108
    Submitted by: MAINTAINER

--- misc/upclient/files/patch-options.c
+++ misc/upclient/files/patch-options.c
@@ -1,5 +1,5 @@
---- src/options.c.orig	Mon Dec 18 02:20:24 2000
-+++ src/options.c	Sat Dec 30 02:48:01 2000
+--- src/options.c.orig	Wed Dec 27 00:41:53 2000
++++ src/options.c	Sat Dec 30 06:09:57 2000
 @@ -23,11 +23,7 @@
  int  cfg_proxyport				= 8080;
  char cfg_proxyuser[PROXYUSER_MAXLEN+1]		= "";
@@ -12,15 +12,15 @@
  #if defined(PLATFORM_UNIXWARE)
  int  cfg_sendload	= 0;
  #else
-@@ -99,11 +95,7 @@
-   else if(strcmp(key, "PidFile") == 0)
+@@ -124,11 +120,7 @@
      strcpy(cfg_pidfile, value);
-   else if(strcmp(key, "SendIdleTime") == 0)
+   }
+   else if(strcmp(key, "SendIdleTime") == 0) {
 -#if defined(PLATFORM_BSD)
 -    ;
 -#else
      cfg_sendidle = atol(value);
 -#endif
-   else if(strcmp(key, "SendLoadAvg") == 0)
+   }
+   else if(strcmp(key, "SendLoadAvg") == 0) {
  #if defined(PLATFORM_UNIXWARE)
-     ;
