75a5a30609b7087dd7263fa08565001ffa296295
kevlo <kevlo@FreeBSD.org>
2001-01-09 16:31:24 +0000

    Upgrade to 0.5
    PR: 24184
    Submitted by: MAINTAINER

--- textproc/py-martel/files/patch-Generate.py
+++ textproc/py-martel/files/patch-Generate.py
@@ -1,3 +1,32 @@
+From: "Andrew Dalke" <dalke@acm.org>
+To: <biopython-dev@biopython.org>
+Date: Wed, 6 Dec 2000 01:12:29 -0700
+
+There's a bug in Martel-0.4 and earlier versions.
+
+Suppose you have  ([<>][ABC])+[<>]?
+and want to match it against
+
+   <A<B<
+
+The "<A" matches the first [<>][ABC].  The "<B" matches
+the second [<>][ABC].  The parser tries to match the final
+"<" against [<>][ABC] and should fail then try to match
+the "<" against [<>]? .
+
+The bug was that it would match the "<" against the [<>] in
+[<>][ABC] and fail at that point.  It gives an assertion error
+about "l" being greater than "r".
+
+Here's the patch.  The only consequence should be a small hit
+in performance.
+
+                    Andrew
+
+[ Note from port maintainer:
+  This patch was somehow not incorporated into Martel 0.5 ]
+
+
 --- Generate.py.orig	Wed Nov 22 09:26:48 2000
 +++ Generate.py	Thu Dec  7 12:27:09 2000
 @@ -268,11 +268,11 @@
