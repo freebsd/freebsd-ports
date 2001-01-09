34c261aa49b0970e92716e7c1c7e3ada6b401ff8
Clive Lin <clive@FreeBSD.org>
2001-01-09 03:52:01 +0000

    Wrong file in previous commit.
    Bugged by: keichii@iteration.net

--- chinese/bitchx/files/patch-include::config.h
+++ chinese/bitchx/files/patch-include::config.h
@@ -1,5 +1,5 @@
---- include/config.h.orig	Tue Jan  9 01:51:42 2001
-+++ include/config.h	Tue Jan  9 02:03:44 2001
+--- include/config.h~	Tue Jan  9 11:43:18 2001
++++ include/config.h	Tue Jan  9 11:45:40 2001
 @@ -52,7 +52,7 @@
   * have a continuation char.
   */
@@ -9,34 +9,36 @@
  			"[efnet] "\
  				"irc.total.net "\
  				"ircd.c-com.net "\
-@@ -479,7 +479,7 @@
+@@ -239,6 +239,23 @@
+  */
+ #undef WANT_HEBREW
  
- #define DEFAULT_HTTP_GRAB OFF
- #define DEFAULT_HELP_WINDOW OFF
--#define DEFAULT_NICK_COMPLETION ON
-+#define DEFAULT_NICK_COMPLETION OFF
- #define DEFAULT_NICK_COMPLETION_LEN 2
- #define DEFAULT_NICK_COMPLETION_TYPE 0  /* 0 1 2 */
- #define DEFAULT_NOTIFY ON
-@@ -514,9 +514,9 @@
- #define DEFAULT_AINV 0
- #define DEFAULT_ANNOY_KICK OFF
- #define DEFAULT_AOP_VAR OFF
--#define DEFAULT_AUTO_AWAY ON
--#define DEFAULT_KICK_OPS ON
--#define DEFAULT_AUTO_REJOIN ON
-+#define DEFAULT_AUTO_AWAY OFF
-+#define DEFAULT_KICK_OPS OFF
-+#define DEFAULT_AUTO_REJOIN OFF
- #define DEFAULT_DEOPFLOOD ON
- #if defined(__EMXPM__) || defined(WIN32)
- #define DEFAULT_CODEPAGE 437
-@@ -757,7 +757,7 @@
- #endif
++#define WANT_BIG5
++
++/* XXX I hope this could be integrated into BX.
++ * XXX Well, this should be done via libc,
++ *     but FreeBSD libc support is quite LAME.
++ *     Macro below are copied from lynx.
++ *
++ *                            clive@FreeBSD.org
++ */
++#ifdef WANT_BIG5
++/* XXX I didn't check the encoding range of big5+. This is standard big5. */
++#define is_big5_los(lo) (((char)0x40<=lo)&&(lo<=(char)0x7E))        /* standard */
++#define is_big5_lox(lo) (((char)0xA1<=lo)&&(lo<=(char)0xFE))        /* extended */
++#define is_big5_hi(hi)  (((char)0xA1<=hi)&&(hi<=(char)0xFE))
++#define is_big5(hi,lo) is_big5_hi(hi) && (is_big5_los(lo) || is_big5_lox(lo))
++#endif WANT_BIG5
++
+ /* if you use cidentd the filename is called .authlie instead of .noident.
+  * as well some modifications to the format of the file were made. So we 
+  * require some pre-knowledge of what to expect. WinNT identd servers will 
+@@ -340,7 +357,7 @@
+  * when you've defined LATIN1. The problem is that ansi graphics will look
+  * ugly if you use the scroll up feature (PageUp/PageDown)
+  */
+-#undef ASCII_LOGO
++#define ASCII_LOGO
  
- #undef PARANOID		/* #define this if your paranoid about dcc hijacking */
--#undef WANT_CHAN_NICK_SERV	/* do we want to include some chan/nick/oper server commands */
-+#define WANT_CHAN_NICK_SERV	/* do we want to include some chan/nick/oper server commands */
- 
- /* new epic stuff */
- #define OLD_STATUS_S_EXPANDO_BEHAVIOR
+ /* If you define REVERSE_WHITE_BLACK, then the format codes for black and
+  * white color are revepsed. (%W, %w is bold black and black, %K, %k is bold
