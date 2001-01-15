116e7472d2bd5da3111fcc4ff992a8707eb4dc28
Maxim Sobolev <sobomax@FreeBSD.org>
2001-01-15 17:03:40 +0000

    Add aureal-kmod - a port to provide convinient way for building FreeBSD driver for Aureal based soundcards. This driver could not be added to the base system because it's uses binary-only Linux Vortex Core object modules.
    The port supports 4-STABLE and 5-CURRENT, however 4-STABLE isn't tested, so
    please report your mileage to me.

---
+++ misc/aureal-kmod/files/patch-au88x0.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- au88x0.c.orig	Mon Jan 15 18:44:49 2001
++++ au88x0.c	Mon Jan 15 18:44:49 2001
+@@ -32,7 +32,7 @@
+  */
+ 
+ #include <dev/sound/pcm/sound.h>
+-#include <dev/sound/pci/au88x0.h>
++#include "au88x0.h"
+ 
+ #include <pci/pcireg.h>
+ #include <pci/pcivar.h>
