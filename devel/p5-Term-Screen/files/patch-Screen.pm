bd0780efa2743c557f42017f6641f3bc1b8f0865
will <will@FreeBSD.org>
2001-01-15 06:22:07 +0000

    Add p5-Term-Screen 1.00, a perl module implementing a basic screen and input class.
    PR:		23614
    Submitted by:	Ying-Chieh Liao <ijliao@csie.nctu.edu.tw>

---
+++ devel/p5-Term-Screen/files/patch-Screen.pm
@@ -0,0 +1,11 @@
+--- Screen.pm.orig	Tue Jan  9 13:10:07 2001
++++ Screen.pm	Tue Jan  9 13:11:37 2001
+@@ -5,7 +5,7 @@
+ 
+ =head1 NAME
+ 
+-Term::Screen
++Term::Screen - A Simple all perl Term::Cap based screen positioning module
+ 
+ =head1 SYNOPSIS
+ 
