6bec4838d31ffd2c7cc740a720079d622ae9c63c
Akinori MUSHA <knu@FreeBSD.org>
2001-01-16 18:27:26 +0000

    Add ja-libconv, Japanese charset conversion library powered by iconv(3).

---
+++ japanese/libjconv/files/patch-info.c
@@ -0,0 +1,11 @@
+--- info.c.orig	Sat Aug  5 05:52:38 2000
++++ info.c	Fri Jan 12 02:12:40 2001
+@@ -13,7 +13,7 @@
+ 
+ #define DEBUG_DO(x)
+ 
+-#define JCONV_DEFAULT_CONFFILE "/etc/libjconv/default.conf"
++#define JCONV_DEFAULT_CONFFILE "!!LOCALBASE!!/etc/libjconv/default.conf"
+ 
+ typedef struct {
+ 	char *name;
