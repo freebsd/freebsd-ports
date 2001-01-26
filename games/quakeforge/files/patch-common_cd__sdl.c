d33af9f4f4d98011086e142f1630d939fa5e52ad
Maxim Sobolev <sobomax@FreeBSD.org>
2001-01-26 16:55:46 +0000

    Respect SDL_CONFIG like others SDL-based ports do.

---
+++ games/quakeforge/files/patch-common_cd__sdl.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- common/cd_sdl.c	2001/01/24 21:40:56	1.1
++++ common/cd_sdl.c	2001/01/24 21:41:17
+@@ -24,7 +24,7 @@
+ //  of podged this together from the SDL headers, and the other cd-rom code.
+ //       -- Mark Baker <homer1@together.net>
+ 
+-#include <SDL/SDL.h>
++#include <SDL.h>
+ 
+ #include "quakedef.h"
+ 
---
+++ games/quakeforge/files/patch-common_vid__sdl.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- common/vid_sdl.c	2001/01/24 21:40:56	1.1
++++ common/vid_sdl.c	2001/01/24 21:41:25
+@@ -21,7 +21,7 @@
+ */
+ // vid_sdl.h -- sdl video driver 
+ 
+-#include <SDL/SDL.h>
++#include <SDL.h>
+ #include "quakedef.h"
+ #include "d_local.h"
+ 
