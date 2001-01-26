f34ca8f2d442804f92e6c62c3f59a3e045b78aeb
Dirk Meyer <dinoex@FreeBSD.org>
2001-01-26 18:15:28 +0000

    Master Site check for new version (0.8 is alpha) Port changed to new layout. removed old references to PNG move dependency to make portlint happy. Fixed spaces. libaa was renamed to aalib added a patch to compile for 4.1, 4.2 and future 4.3 release. plist improved to clean include/qsys. cleaned plist of empty lines.
    PR:		20344
    Submitted by:	n_hibma@qubesoft.com
    Reviewed by:	will

---
+++ graphics/qdraw/files/patch-unix_gcc.mk
@@ -0,0 +1,21 @@
+--- make/unix_gcc.mk.orig	Fri Jul 28 20:54:11 2000
++++ make/unix_gcc.mk	Fri Jan 26 07:05:06 2001
+@@ -34,12 +34,18 @@
+ GCCPATH=	/usr
+ GCC=		cc
+ ifneq "${freebsd}" "40"
++ifneq "${freebsd}" "41"
++ifneq "${freebsd}" "42"
++ifneq "${freebsd}" "43"
+ ifneq "${freebsd}" "50"
+ GCCBASE=	/usr/local
+ GCCTARGET=	i386-unknown-freebsd3.0
+ GCCVER=		egcs-2.91.62
+ GCCPATH=	${GCCBASE}/lib/gcc-lib/${GCCTARGET}/${GCCVER}
+ GCC=		egcc
++endif
++endif
++endif
+ endif
+ endif
+ endif
