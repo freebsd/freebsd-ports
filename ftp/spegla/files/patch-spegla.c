28faf0cb193686b44ca7b83b2ce60ddae0c6a65a
Jun Kuriyama <kuriyama@FreeBSD.org>
2001-02-01 08:33:04 +0000

    Fix \0 termination at %s substitution for configfile. Be sure vsnprintf() may return value which is larger than buffer length.
    Approved by:	maintainer

--- ftp/spegla/files/patch-spegla.c
+++ ftp/spegla/files/patch-spegla.c
@@ -1,5 +1,5 @@
 --- spegla.c.orig	Sat May 27 22:38:14 2000
-+++ spegla.c	Fri Jan 19 19:36:43 2001
++++ spegla.c	Thu Feb  1 15:39:24 2001
 @@ -728,7 +728,7 @@
  #ifdef ULTRIX
  	if ((f.fd_req.bfreen * 1024) < minfree)
@@ -9,3 +9,11 @@
  #endif
  		e_errx(1, "%s: Not enough space left", path);
  
+@@ -953,6 +953,7 @@
+ 		}
+ 		buf[i] = p[j];
+ 	}
++	buf[i] = '\0';
+ 	*val = buf;
+ ret:
+ 	if (show)
