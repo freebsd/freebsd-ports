cf390065f5ff18d3571054d68a6a324e1baf0d84
Mikhail Teterin <mi@FreeBSD.org>
2001-02-07 22:36:13 +0000

    Bring to the latest version. Link all of the little external libraries needed by the tclhttpd into one file. I submitted this back in December, but nobody cared since. Now I can commit this myself :)
    PR:		ports/23310
    Submitted by:	mi

---
+++ www/tclhttpd/files/metaPackage.c
@@ -0,0 +1,13 @@
+#include <tcl.h>
+
+Tcl_PackageInitProc Crypt_Init, Limit_Init, Setuid_Init, Utime_Init;
+
+int Tclhttpdbin_Init(Tcl_Interp *I) {
+
+	Crypt_Init(I);
+	Limit_Init(I);
+	Setuid_Init(I);
+	Utime_Init(I);
+
+	return TCL_OK;
+}
