4a5f62750d21fa298140b857926e6784f1ab6a01
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-02-16 11:07:08 +0000

    add c2lib c2lib is a library of basic structures and memory allocators for C. It is designed to look similar to C++ STL with many powerful string features borrowed also from Perl.

---
+++ devel/c2lib/files/patch-pstring.h
@@ -0,0 +1,10 @@
+--- pstring.h.orig	Fri Feb 16 18:46:13 2001
++++ pstring.h	Fri Feb 16 18:45:20 2001
+@@ -21,6 +21,7 @@
+ #ifndef PSTRING_H
+ #define PSTRING_H
+ 
++#include <sys/types.h>
+ #include <regex.h>
+ 
+ #include <pool.h>
