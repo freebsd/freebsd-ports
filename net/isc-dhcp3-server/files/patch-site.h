bb72cada91afb18208e9499c7366eea67aac131b
roam <roam@FreeBSD.org>
2001-02-17 14:17:30 +0000

    Update to 3.0b2pl16, assign maintainership to Cyrille Lefevre.
    PR:		24911
    Submitted by:	Cyrille Lefevre <clefevre@citeweb.net> - new maintainer

--- net/isc-dhcp3-server/files/patch-site.h
+++
@@ -1,5 +0,0 @@
---- includes/site.h.orig	Thu Sep 28 11:24:17 2000
-+++ includes/site.h	Sat Dec  2 00:46:53 2000
-@@ -159 +159 @@
--/* #define DHCPD_LOG_FACILITY LOG_DAEMON */
-+#define DHCPD_LOG_FACILITY LOG_!!USE_FACILITY!!
