36f5c5852b2b6725dada4a8d07f8403440b6ba4e
Steve Price <steve@FreeBSD.org>
2001-02-26 04:19:31 +0000

    #include sys/systm.h for the splXXX definitions.
    PR:		24478
    Submitted by:	Nickolay Dudorov <nnd@mail.nsk.ru>

--- audio/aureal-kmod/files/patch-au88x0.c
+++ audio/aureal-kmod/files/patch-au88x0.c
@@ -3,12 +3,13 @@ $FreeBSD$
 
 --- au88x0.c.orig	Mon Jan 15 18:44:49 2001
 +++ au88x0.c	Mon Jan 15 18:44:49 2001
-@@ -32,7 +32,7 @@
+@@ -32,7 +32,8 @@
   */
  
  #include <dev/sound/pcm/sound.h>
 -#include <dev/sound/pci/au88x0.h>
 +#include "au88x0.h"
++#include <sys/systm.h>
  
  #include <pci/pcireg.h>
  #include <pci/pcivar.h>
