531a10b09a90bea795d8f7169bc8fe568a4166c0
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-02-27 07:10:53 +0000

    add uds UDS (Useful Development Stuff) Collection

---
+++ devel/uds/files/patch-uds::procstream.cc
@@ -0,0 +1,12 @@
+--- uds/procstream.cc.orig	Tue Feb 27 13:50:01 2001
++++ uds/procstream.cc	Tue Feb 27 13:50:08 2001
+@@ -22,8 +22,8 @@
+ 
+ // includes
+ 
+-#include <sys/socket.h>
+ #include <sys/types.h>
++#include <sys/socket.h>
+ #include <sys/wait.h>
+ #include <uds/procstream.hh>
+ #include <uds/sys_util.hh>
---
+++ devel/uds/files/patch-uds::sys_util.hh
@@ -0,0 +1,10 @@
+--- uds/sys_util.hh.orig	Tue Feb 27 13:54:05 2001
++++ uds/sys_util.hh	Tue Feb 27 13:57:50 2001
+@@ -25,6 +25,7 @@
+ 
+ // includes
+ 
++#include <sys/types.h>
+ #include <uds/stdexcpt.hh>
+ 
+ using namespace std;
