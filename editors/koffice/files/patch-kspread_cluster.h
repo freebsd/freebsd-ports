8a9d7a704f118e35133fe4de3788f9562cbf2f2e
will <will@FreeBSD.org>
2001-02-27 22:30:09 +0000

    Add patches to fix build with KDE 2.1.  Bump PORTREVISION to denote new package based on KDE 2.1.  Change MAINTAINER back to myself.  This should fix kde2 metaport once again.
    Submitted by:	many many people...

---
+++ editors/koffice/files/patch-kspread_cluster.h
@@ -0,0 +1,11 @@
+--- kspread/kspread_cluster.h	Thu Jul 13 14:35:01 2000
++++ kspread/kspread_cluster.h.new	Tue Feb 27 17:09:23 2001
+@@ -1,6 +1,8 @@
+ #ifndef kspread_cluster_h
+ #define kspread_cluster_h
+ 
++#include <stdlib.h>
++
+ class KSpreadCell;
+ class ColumnLayout;
+ class RowLayout;
