9bae3c33b40066027c41f2e0dd285c88d7b4fdbe
will <will@FreeBSD.org>
2001-03-01 07:36:16 +0000

    Fix KOffice for KDE 2.1 for sure.

---
+++ editors/koffice-kde4/files/patch-kpobject.h
@@ -0,0 +1,11 @@
+--- kpresenter/kpobject.h	Sun Sep 24 08:32:43 2000
++++ kpresenter/kpobject.h.new	Wed Feb 28 18:44:16 2001
+@@ -27,7 +27,7 @@
+ #include <qbrush.h>
+ #include <qcursor.h>
+ #include <qcolor.h>
+-
++#include <stdlib.h>
+ #include "global.h"
+ 
+ #include <koStream.h>
