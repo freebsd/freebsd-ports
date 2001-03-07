9ee2163169ca82f3e87588734f70328a02b8a8c5
roam <roam@FreeBSD.org>
2001-03-07 10:52:05 +0000

    Add libesmtp, a library for sending email messages via SMTP.
    PR:		25269
    Submitted by:	Anton Berezin <tobez@tobez.org>

---
+++ mail/libesmtp/files/patch-headers.c
@@ -0,0 +1,10 @@
+--- headers.c.orig	Wed Feb 21 23:07:41 2001
++++ headers.c	Wed Feb 21 23:07:57 2001
+@@ -23,6 +23,7 @@
+ #include <stdarg.h>
+ #include <string.h>
+ #include <stdlib.h>
++#include <time.h>
+ 
+ #include "libesmtp-private.h"
+ #include "headers.h"
