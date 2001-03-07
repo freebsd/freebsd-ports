0f55ea36d6843286b73fac9f3507f51f45bf2ce2
ijliao <ijliao@FreeBSD.org>
2001-03-07 11:49:53 +0000

    add regexx Regexx is a complete regular expressions C++ solution

---
+++ devel/regexx/files/patch-src::regexx.cc
@@ -0,0 +1,9 @@
+--- src/regexx.cc.orig	Fri Mar  2 01:23:27 2001
++++ src/regexx.cc	Fri Mar  2 01:23:54 2001
+@@ -28,5 +28,5 @@
+ 
+ #include "regexx.hh"
+-#include "../pcre/pcre.h"
++#include "pcre.h"
+ 
+ const unsigned int&
