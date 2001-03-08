b84af273e4c16cbe0b40ae460da8199d713c02fa
sf <sf@FreeBSD.org>
2001-03-08 20:06:36 +0000

    update to 3.0b2pl18.
    PR:		25501
    Submitted by:	maintainer

--- net/isc-dhcp3-server/files/patch-dhclient.c
+++ net/isc-dhcp3-server/files/patch-dhclient.c
@@ -1,14 +1,14 @@
---- client/dhclient.c.orig	Thu Jan 25 09:18:06 2001
-+++ client/dhclient.c	Fri Jan 26 06:15:50 2001
+--- client/dhclient.c.orig	Thu Feb 15 23:17:05 2001
++++ client/dhclient.c	Fri Mar  2 05:51:43 2001
 @@ -78,6 +78,7 @@
  u_int16_t remote_port;
  int no_daemon;
  int save_scripts;
 +int onetry;
+ struct string_list *client_env;
+ int client_env_count;
  
- static void usage PROTO ((void));
- 
-@@ -104,6 +105,7 @@
+@@ -106,6 +107,7 @@
  	int no_dhclient_conf = 0;
  	int no_dhclient_db = 0;
  	int no_dhclient_pid = 0;
@@ -16,7 +16,7 @@
  	char *s;
  
  #ifdef SYSLOG_4_2
-@@ -159,6 +161,11 @@
+@@ -161,6 +163,13 @@
                                  usage ();
                          path_dhclient_db = argv [i];
  			no_dhclient_db = 1;
@@ -25,19 +25,12 @@
 +                                usage ();
 +                        client_script_name = argv [i];
 +			no_dhclient_script = 1;
++		} else if (!strcmp (argv [i], "-1")) {
++			onetry = 1;
  		} else if (!strcmp (argv [i], "-q")) {
  			quiet = 1;
  			quiet_interface_discovery = 1;
-@@ -176,6 +183,8 @@
- 		} else if (!strcmp (argv [i], "-w")) {
- 			/* do not exit if there are no broadcast interfaces. */
- 			persist = 1;
-+		} else if (!strcmp (argv [i], "-1")) {
-+			onetry = 1;
-  		} else if (argv [i][0] == '-') {
-  		    usage ();
-  		} else {
-@@ -208,6 +217,9 @@
+@@ -224,6 +233,9 @@
  	if (!no_dhclient_pid && (s = getenv ("PATH_DHCLIENT_PID"))) {
  		path_dhclient_pid = s;
  	}
@@ -47,22 +40,23 @@
  
  	/* first kill of any currently running client */
  	if (release_mode) {
-@@ -423,10 +435,11 @@
+@@ -445,10 +457,12 @@
  	log_info (arr);
  	log_info (url);
  
 -	log_error ("Usage: dhclient [-d] [-D] [-q] [-p <port>] %s",
-+	log_error ("usage: dhclient [-1] [-d] [-D] [-q] [-p <port>] %s",
++	log_error ("Usage: dhclient [-1] [-d] [-D] [-q] [-p <port>] %s",
  		   "[-s server]");
 -	log_fatal ("                [-lf lease-file] [-pf pid-file]%s",
--		   "[-cf config-file] [interface]");
+-		   "[-cf config-file] [interface] [-e VAR=val]");
 +	log_error ("                [-cf config-file] [-lf lease-file] %s",
-+		   "[-pf pid-file] [-sf script-file]");
-+	log_fatal ("                [interface]");
++		   "[-pf pid-file]");
++	log_fatal ("                [-sf script-file] [interface] %s",
++		   "[-e VAR=val]");
  }
  
  isc_result_t find_class (struct class **c,
-@@ -1432,6 +1445,10 @@
+@@ -1453,6 +1467,10 @@
  	/* No leases were available, or what was available didn't work, so
  	   tell the shell script that we failed to allocate an address,
  	   and try again later. */
