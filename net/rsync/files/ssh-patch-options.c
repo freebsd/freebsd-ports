31bb97e684e4f8b217087195fdf98e175b84cb58
David E. O'Brien <obrien@FreeBSD.org>
2001-03-08 11:40:26 +0000

    Change the default remote protocol from RSH to SSH.
    Submitted by:	knu

---
+++ net/rsync/files/ssh-patch-options.c
@@ -0,0 +1,5 @@
+--- options.c.orig	Thu Mar  8 03:31:32 2001
++++ options.c	Thu Mar  8 03:31:32 2001
+@@ -141 +141 @@
+-  rprintf(F," -e, --rsh=COMMAND           specify rsh replacement\n");
++  rprintf(F," -e, --rsh=COMMAND           specify rsh replacement (default %s)\n", RSYNC_RSH);
---
+++ net/rsync/files/ssh-patch-rsync.h
@@ -0,0 +1,5 @@
+--- rsync.h.orig	Sat Aug 19 06:10:39 2000
++++ rsync.h	Thu Mar  8 03:31:33 2001
+@@ -75 +75 @@
+-#define RSYNC_RSH "rsh"
++#define RSYNC_RSH "ssh"
