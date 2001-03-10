05febb9e7d5d59840af6d5386725a07dd8d20f43
Dirk Meyer <dinoex@FreeBSD.org>
2001-03-10 18:31:01 +0000

    patch to make it compile under "alpha" on beast.

--- emulators/frodo/files/patch-CmdPipe.cpp
+++ emulators/frodo/files/patch-CmdPipe.cpp
@@ -1,14 +1,24 @@
-*** CmdPipe.cpp	Wed Aug  6 20:56:26 1997
---- CmdPipe.cpp.neu	Fri Sep  5 14:15:37 1997
-***************
-*** 29,34 ****
---- 29,37 ----
-  #if defined(__linux__)
-  	#include <sys/time.h>
-  #endif
-+ #if defined(__FreeBSD__)
-+ 	#include <sys/time.h>
-+ #endif
-  
-  	#include <time.h>
-  	#include <errno.h>
+--- CmdPipe.cpp.orig	Wed Aug  6 20:56:26 1997
++++ CmdPipe.cpp	Sat Mar 10 19:19:59 2001
+@@ -16,9 +16,11 @@
+ 	#include <string.h>
+ 	#include <signal.h>
+ 
++#if ! defined(__FreeBSD__)
+ #if defined(__alpha__)
+ 	#include <cma.h>
+ #endif
++#endif
+ 
+ #if defined(AIX)
+ 	#include <sys/select.h>
+@@ -27,6 +29,9 @@
+ #endif
+ 
+ #if defined(__linux__)
++	#include <sys/time.h>
++#endif
++#if defined(__FreeBSD__)
+ 	#include <sys/time.h>
+ #endif
+ 
