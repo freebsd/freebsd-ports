2a6b85739de7b0726e0724f4f120fb80799ec0c9
Will Andrews <will@FreeBSD.org>
2001-03-10 19:46:38 +0000

    Add mtrxmath 0.9.3, a small tool for matrix mathemetics.
    PR:		24202
    Submitted by:	George Reid <greid@ukug.uk.freebsd.org>

---
+++ math/mtrxmath/files/patch-mult.c
@@ -0,0 +1,10 @@
+--- mult.c.orig	Tue Jan  9 20:40:21 2001
++++ mult.c	Tue Jan  9 20:40:28 2001
+@@ -11,7 +11,6 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+-#include <malloc.h>
+ #include "mtrxmath.h"
+ 
+ MATRIX_PTR matrix_multiply( MATRIX_PTR first,  MATRIX_PTR second)
