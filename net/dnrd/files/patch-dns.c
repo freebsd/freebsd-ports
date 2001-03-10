80206bcb9084541dc164caa7db3c061cd2093b2d
will <will@FreeBSD.org>
2001-03-10 19:33:51 +0000

    Add dnrd 2.9, a proxy DNS daemon.
    PR:		24107
    Submitted by:	George Reid <greid@ukug.uk.freebsd.org>

---
+++ net/dnrd/files/patch-dns.c
@@ -0,0 +1,10 @@
+--- dns.c	Thu Jan  4 10:37:21 2001
++++ dns.c.new	Sat Mar 10 14:29:38 2001
+@@ -27,6 +27,7 @@
+ #include <ctype.h>
+ 
+ #include <netdb.h>
++#include <sys/types.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
