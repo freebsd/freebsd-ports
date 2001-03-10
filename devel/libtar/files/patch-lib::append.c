30ca9191d0ad9703bb9f01268084988b066ac6df
Will Andrews <will@FreeBSD.org>
2001-03-10 19:40:15 +0000

    Add libtar 1.2, a library for manipulating POSIX and GNU tar files.
    PR:		24114
    Submitted by:	George Reid <greid@ukug.uk.freebsd.org>

---
+++ devel/libtar/files/patch-lib::append.c
@@ -0,0 +1,10 @@
+--- lib/append.c.orig	Sat Jan  6 17:47:16 2001
++++ lib/append.c	Sat Jan  6 17:47:40 2001
+@@ -16,7 +16,6 @@
+ #include <errno.h>
+ #include <fcntl.h>
+ #include <sys/param.h>
+-#include <sys/sysmacros.h>
+ 
+ #include <compat.h>
+ 
---
+++ devel/libtar/files/patch-lib::encode.c
@@ -0,0 +1,10 @@
+--- lib/encode.c.orig	Sat Jan  6 17:47:54 2001
++++ lib/encode.c	Sat Jan  6 17:47:59 2001
+@@ -15,7 +15,6 @@
+ 
+ #include <pwd.h>
+ #include <grp.h>
+-#include <sys/sysmacros.h>
+ 
+ #ifdef STDC_HEADERS
+ # include <string.h>
