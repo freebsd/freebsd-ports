a4742ef9622cd3e9429f540efd50c9c0a04fe3f6
knu <knu@FreeBSD.org>
2001-03-12 13:03:30 +0000

    Update to 0.4.0.

--- textproc/ruby-sablot/files/patch-extconf.rb
+++ textproc/ruby-sablot/files/patch-extconf.rb
@@ -1,18 +1,15 @@
---- extconf.rb.orig	Sun Dec  3 23:50:31 2000
-+++ extconf.rb	Sat Mar 10 00:35:37 2001
-@@ -2,10 +2,10 @@
- 
- dir_config('sablot')
- 
--$LOCAL_LIBS += " -L/usr/local/lib -lsablot -lxmlparse -lxmltok"
--
--if have_header("sablot.h") &&
--    have_library("sablot", "SablotProcessStrings") &&
--    have_library("xmltok", "XML_ParserCreate")
-+if have_header("expat.h") &&
-+    have_header("sablot.h") &&
-+    have_library("expat", "XML_ParserCreate") &&
-+    have_library("iconv") &&
-+    have_library("sablot", "SablotProcessStrings")
-   create_makefile("sablot")
+--- extconf.rb.orig	Mon Mar 12 02:09:20 2001
++++ extconf.rb	Mon Mar 12 22:00:21 2001
+@@ -8,8 +8,10 @@
  end
+ 
+ if have_header("sablot.h") and
+-    have_library("xmltok", "XmlParseXmlDecl") and
+-    have_library("xmlparse", "XML_ParserCreate") and
++    (have_library("expat", "XML_ParserCreate") or
++     (have_library("xmltok", "XmlParseXmlDecl") and
++      have_library("xmlparse", "XML_ParserCreate"))) and
++    have_library("iconv") and
+     have_library("sablot", "SablotProcessStrings")
+   create_makefile("sablot")
+   STDERR.print <<EOB
