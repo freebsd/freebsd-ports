405f149c23a7e560f72789651e94f0d880871573
FUJISHIMA Satsuki <sf@FreeBSD.org>
2001-03-14 16:19:46 +0000

    support PREFIX properly.
    PR:		25555
    Submitted by:	KATO Tsuguru <tkato@prontomail.ne.jp>

--- net/freewais-sf/files/freebsd.sh
+++ net/freewais-sf/files/freebsd.sh
@@ -1,5 +1,8 @@
 locincpth="$PREFIX/include";
 libpth="/usr/lib";
+myhostname="localhost";
+mansrc="$PREFIX/man/man1"
+man1dir="$PREFIX/man/man1"
 man3dir="$PREFIX/man/man3";
 privlic="$PREFIX/lib/";
 optimize="$CFLAGS";
