26c39274ac5db4c064e113cfc05afaad4547e598
Trevor Johnson <trevor@FreeBSD.org>
2001-03-15 14:34:35 +0000

    Patch aops.c for a header problem.  Remove BROKEN line.
    reported by:	kris

---
+++ audio/csound/files/patch-aops.c
@@ -0,0 +1,16 @@
+--- aops.c.orig	Thu Jan  4 03:48:26 2001
++++ aops.c	Thu Mar 15 06:26:31 2001
+@@ -1,3 +1,4 @@
++#include <sys/time.h>
+ #include "cs.h"		/*					AOPS.C	*/
+ #include "aops.h"
+ #include <math.h>
+@@ -421,8 +422,6 @@
+ #ifdef LINUX
+ void rtclock(EVAL *p)
+ {
+-#include <sys/time.h>
+-#include <unistd.h>
+     struct timeval tv;
+     struct timezone tz;
+     gettimeofday(&tv, &tz);
