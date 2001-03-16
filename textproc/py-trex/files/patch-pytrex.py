5d917390e2d6eb8b0f6f7d9b2d09bfa479d822f2
ijliao <ijliao@FreeBSD.org>
2001-03-16 16:24:07 +0000

    add py-trex Python implementation of Tree Regular Expressions for XML (TREX)

---
+++ textproc/py-trex/files/patch-pytrex.py
@@ -0,0 +1,7 @@
+--- pytrex.py.orig	Sat Mar 17 00:00:31 2001
++++ pytrex.py	Sat Mar 17 00:02:26 2001
+@@ -1,3 +1,4 @@
++#!%%LOCALBASE%%/bin/python
+ # PyTREX: A clean-room implementation of TREX in Python
+ # by James Tauber
+ #
