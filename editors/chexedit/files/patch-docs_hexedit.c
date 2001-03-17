6080ef750440ed579628eabf651ff586d0ee07f6
vanilla <vanilla@FreeBSD.org>
2001-03-17 16:33:32 +0000

    Full screen text mode Hex editor using the [n]curses library
    PR:		ports/24761
    Submitted by:	Roman Shterenzon <roman@xpert.com>

---
+++ editors/chexedit/files/patch-docs_hexedit.c
@@ -0,0 +1,30 @@
+--- docs/hexedit.1.orig	Fri Apr 23 22:16:42 1999
++++ docs/hexedit.1	Wed Jan 31 22:39:36 2001
+@@ -1,14 +1,14 @@
+ .\" Copyright (C) 1998,1999 Adam Rogoyski
+-.TH HEXEDIT 1
++.TH CHEXEDIT 1
+ .SH NAME
+-hexedit \- Full screen curses Hex editor
++chexedit \- Full screen curses Hex editor
+ .SH SYNOPSIS 
+ .nr a \n(.j
+ .ad l
+ .nr i \n(.i
+-.in +\w'\fBhexedit 'u
++.in +\w'\fBchexedit 'u
+ .ti \niu
+-.B hexedit
++.B chexedit
+ .de OP
+ .ie \\n(.$-1 .RI "[\ \fB\\$1fP" "\\$2" "\ ]"
+ .el .RB "[\ " "\\$1" "\ ]"
+@@ -18,7 +18,7 @@
+ .br
+ .ad \na
+ .SH DESCRIPTION 
+-.I Hexedit
++.I Chexedit
+ is an editor to edit binary (or any) files or disks.  The Display consists
+ of the current offset from 0 (the first byte), the next sixteen bytes
+ (aligned by bytes or 32-bit words), and it's ASCII or EBCDIC text
