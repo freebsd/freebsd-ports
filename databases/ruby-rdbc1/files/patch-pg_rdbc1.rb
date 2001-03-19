e868461a1a00e9526dc7be48a92c7223302b08aa
Akinori MUSHA <knu@FreeBSD.org>
2001-03-19 08:58:27 +0000

    Add ruby-rdbc1, Ruby Database Connectivity.

---
+++ databases/ruby-rdbc1/files/patch-pg_rdbc1.rb
@@ -0,0 +1,11 @@
+--- pg_rdbc1.rb.orig	Fri May 26 03:24:12 2000
++++ pg_rdbc1.rb	Mon Mar 19 17:44:34 2001
+@@ -11,7 +11,7 @@
+ require "rdbc1.rb"
+ 
+ module PG_RDBC1
+-  require "../postgres/postgres.so"
++  require "postgres"
+   include RDBC1
+ 
+   # Driver Creator will inherite me.
