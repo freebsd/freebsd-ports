94092f3358357a6c85e1767c4e014501d7411e01
Maxim Sobolev <sobomax@FreeBSD.org>
2001-03-21 18:55:23 +0000

    Fix Russian messages. They are not compiled in by default, so PORTREVISION was not bumbed.
    Submitted by:	"Alexandr A. Listopad" <laa@lucky.net>

---
+++ net/micq/files/patch-..::lang::russian.h
@@ -0,0 +1,59 @@
+
+$FreeBSD$
+
+--- ../lang/russian.h	2001/03/21 18:30:33	1.1
++++ ../lang/russian.h	2001/03/21 18:34:40
+@@ -37,29 +37,29 @@
+ #define PHONE_INFO_STR 		"Телефон        : "
+ #define FAX_INFO_STR   		"Факс           : "
+ #define STREET_INFO_STR 	"Улица          : "
+-#define CELLULAR_INFO_STR 	"Cellular       : "
+-#define ZIP_INFO_STR 		"Zip            : "
++#define CELLULAR_INFO_STR 	"Сотовый        : "
++#define ZIP_INFO_STR 		"Индекс         : "
+ #define COUNTRY_INFO_STR 	"Страна         : "
+ #define COUNTRY_CODE_INFO_STR 	"Код страны     : "
+ #define WORK_COUNTRY_CODE_INFO_STR  "Код страны работы : " 
+ #define WORK_COUNTRY_INFO_STR	"Страна работы  : " 
+-#define WORK_HOMEPAGE_INFO_STR 	"Странца работы : " 
++#define WORK_HOMEPAGE_INFO_STR 	"Страница работы: " 
+ #define WORK_OCCUPATION_INFO_STR "Занятие        : "
+ #define WORK_JOB_POS_INFO_STR 	"Должность      : " 
+ #define WORK_DEPT_INFO_STR 	"Департамент    : " 
+ #define WORK_COMP_NAME_INFO_STR "Место работы   : " 
+-#define WORK_ZIP_INFO_STR       "Zip работы     : " 
++#define WORK_ZIP_INFO_STR       "Рабочий индекс : " 
+ #define WORK_FAX_INFO_STR 	"Рабочий факс   : " 
+ #define WORK_ADDRESS_INFO_STR 	"Раюочий адрес  : " 
+ #define WORK_PHONE_INFO_STR 	"Рабочий телефон: " 
+ #define WORK_LOCATION_INFO_STR 	"Местоположение работы : "
+-#define ABOUT_INFO_STR "Инфа : "
++#define ABOUT_INFO_STR "Информация : "
+ #define INFO_NE_STR "Не указано"
+ #define AGE_INFO_STR "Возраст      : "
+ #define FEMALE_INFO_STR "Пол : Женский"
+ #define MALE_INFO_STR "Пол : Мужской"
+ #define SEX_UNKNOWN_INFO_STR "Пол : Не указан"
+-#define HOMEPAGE_INFO_STR "Homepage     : "
++#define HOMEPAGE_INFO_STR "Домашняя страница : "
+ #define BIRTH_INFO_STR "Родился      : "
+ #define LANGUAGE_INFO_STR "Языки        : "
+ 
+@@ -75,7 +75,7 @@
+ #define UPDATE_YEAR_OF_BIRTH_STR "Введите год рождения (4 цифры): "
+ #define UPDATE_MONTH_OF_BIRTH_STR "Введите месяц рождения: "
+ #define UPDATE_DAY_OF_BIRTH_STR "Введите день рождения: "
+-#define ABOUT_PROMPT_STR "Инфа> "
++#define ABOUT_PROMPT_STR "Информация> "
+ /***************************************************************************/
+ #define ABOUT_HELP_STR "\t\tОбновляет ваши заметки о пользователе.\n"
+ 
+@@ -87,7 +87,7 @@
+ #define FAX_UPDATE_STR "Введите факс: "
+ #define STREET_UPDATE_STR "Введите адрес: "
+ #define CELLULAR_UPDATE_STR "Введите номер квартиры: "
+-#define ZIP_UPDATE_STR "Введите zip (должен быть числовым): "
++#define ZIP_UPDATE_STR "Введите индекс (должен быть числовым): "
+ #define COUNTRY_UPDATE_STR "Введите телефонный код страны: "
+ #define TIME_ZONE_UPDATE_STR "Введите свою временную зону (+/- 0-12): "
+ #define LAST_HELP_STR "\t\tПоказывает последнее сообщение от <nick>.\n\t\t\tОни должны быть в Вашем контакт листе.\n"
