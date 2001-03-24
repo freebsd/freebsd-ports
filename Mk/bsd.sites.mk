0d0f26950293a45fc82d97e5c774ca79f3c5660f
Bill Fenner <fenner@FreeBSD.org>
2001-03-24 17:21:47 +0000

    "ftp.ru.FreeBSD.og" doesn't exist.  There isn't even an ".og" domain. =)
    Found by:	distfile survey

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -19,7 +19,7 @@ MASTER_SITE_LOCAL?= \
 	ftp://ftp.se.FreeBSD.org/pub/FreeBSD/ports/local-distfiles/%SUBDIR%/ \
 	ftp://ftp.jp.FreeBSD.org/pub/FreeBSD/ports/local-distfiles/%SUBDIR%/ \
 	ftp://ftp.uk.FreeBSD.org/pub/FreeBSD/ports/local-distfiles/%SUBDIR%/ \
-	ftp://ftp.ru.FreeBSD.og/pub/FreeBSD/ports/local-distfiles/%SUBDIR%/ \
+	ftp://ftp.ru.FreeBSD.org/pub/FreeBSD/ports/local-distfiles/%SUBDIR%/ \
 	ftp://ftp.nectec.or.th/pub/FreeBSD/ports/local-distfiles/%SUBDIR%/
 
 MASTER_SITE_PORTS_JP+=	\
