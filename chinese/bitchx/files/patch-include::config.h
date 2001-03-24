a695ad456a0a38d79fba187ba0bd4a4be012b5f3
Clive Lin <clive@FreeBSD.org>
2001-03-24 18:40:46 +0000

    o DONTWANT_NOTIFY_BITCHX_COM o More localization:   o Add complete servers list of galaxy network.   o Change some BX behaviors to reflect TW people's habits.
    Submitted by: keichii

--- chinese/bitchx/files/patch-include::config.h
+++ chinese/bitchx/files/patch-include::config.h
@@ -1,15 +1,53 @@
---- include/config.h~	Tue Jan  9 11:43:18 2001
-+++ include/config.h	Tue Jan  9 11:45:40 2001
-@@ -52,7 +52,7 @@
+--- include/config.h~	Sun Mar 25 02:28:49 2001
++++ include/config.h	Sun Mar 25 02:30:53 2001
+@@ -35,7 +35,7 @@
+  * too peruse this function all you like. It's not a BACKDOOR in any way, 
+  * shape or form.
+  */
+-#define WANT_NOTIFY_BITCHX_COM ON
++#define WANT_NOTIFY_BITCHX_COM OFF
+ 
+ 
+ /*
+@@ -51,7 +51,36 @@
+  * on any servers you add into this list. also the very last server should not 
   * have a continuation char.
   */
- #define DEFAULT_SERVER	"[ODNet] "\
--				"irc.bitchx.com "\
+-#define DEFAULT_SERVER	"[ODNet] "\
++#define DEFAULT_SERVER	"[galaxynet] "\
 +				"irc.taiwan.com "\
++				"Atlanta.GA.US.GalaxyNet.Org "\
++				"Boston.MA.US.GalaxyNet.Org "\
++				"Charlotte.NC.US.GalaxyNet.Org "\
++				"Exiled.US.GalaxyNet.Org "\
++				"Gymnet.US.GalaxyNet.Org "\
++				"Honolulu.HI.US.GalaxyNet.Org "\
++				"Imns.US.GalaxyNet.Org "\
++				"Intermedia.US.GalaxyNet.Org "\
++				"Sedona.AZ.US.GalaxyNet.Org "\
++				"K-Systems.SG.GalaxyNet.Org "\
++				"Lycos.SG.GalaxyNet.Org "\
++				"Pacific.SG.GalaxyNet.Org "\
++				"Singapore.SG.GalaxyNet.Org "\
++				"Singnet.SG.GalaxyNet.Org "\
++				"Swiftech.SG.GalaxyNet.Org "\
++				"Swiftech.SG.GalaxyNet.Org "\
++				"Caen.FR.GalaxyNet.Org "\
++				"Copenhagen.DK.GalaxyNet.Org "\
++				"Online.BE.GalaxyNet.Org "\
++				"Opaltelecom.UK.GalaxyNet.Org "\
++				"Oslo.NO.GalaxyNet.Org "\
++				"Vltmedia.SE.GalaxyNet.Org "\
++				"Xcal.UK.GalaxyNet.Org "\
++				"Zeelandnet.NL.GalaxyNet.Org "\
++				"Netvision.IL.GalaxyNet.Org "\
++				"Auckland.NZ.GalaxyNet.Org "\
++				"Chariot.AU.GalaxyNet.Org "\
++			"[ODNet] "\
+ 				"irc.bitchx.com "\
  			"[efnet] "\
  				"irc.total.net "\
- 				"ircd.c-com.net "\
-@@ -239,6 +239,23 @@
+@@ -245,6 +274,23 @@
   */
  #undef WANT_HEBREW
  
@@ -33,7 +71,7 @@
  /* if you use cidentd the filename is called .authlie instead of .noident.
   * as well some modifications to the format of the file were made. So we 
   * require some pre-knowledge of what to expect. WinNT identd servers will 
-@@ -340,7 +357,7 @@
+@@ -346,7 +392,7 @@
   * when you've defined LATIN1. The problem is that ansi graphics will look
   * ugly if you use the scroll up feature (PageUp/PageDown)
   */
@@ -42,3 +80,54 @@
  
  /* If you define REVERSE_WHITE_BLACK, then the format codes for black and
   * white color are revepsed. (%W, %w is bold black and black, %K, %k is bold
+@@ -379,7 +425,7 @@
+ 
+ #define DEFAULT_PING_TYPE 1
+ #define DEFAULT_MSGLOG ON
+-#define DEFAULT_AUTO_NSLOOKUP OFF
++#define DEFAULT_AUTO_NSLOOKUP ON
+ #define DEFAULT_ALT_CHARSET ON
+ #define DEFAULT_FLOOD_KICK ON
+ #define DEFAULT_FLOOD_PROTECTION ON
+@@ -388,9 +434,9 @@
+ #define DEFAULT_LLOOK_DELAY 120
+ #define DEFAULT_ALWAYS_SPLIT_BIGGEST ON
+ #define DEFAULT_AUTO_UNMARK_AWAY OFF
+-#define DEFAULT_AUTO_WHOWAS OFF
++#define DEFAULT_AUTO_WHOWAS ON
+ #define DEFAULT_BANTIME 600
+-#define DEFAULT_BEEP ON
++#define DEFAULT_BEEP OFF
+ #define DEFAULT_BEEP_MAX 3
+ #define DEFAULT_BEEP_WHEN_AWAY OFF
+ #define DEFAULT_BOLD_VIDEO ON
+@@ -474,7 +520,7 @@
+ #define DEFAULT_NOTIFY ON
+ #define DEFAULT_QUEUE_SENDS 0
+ #define DEFAULT_MAX_SERVER_RECONNECT 2
+-#define DEFAULT_SERVER_GROUPS OFF
++#define DEFAULT_SERVER_GROUPS ON
+ #define DEFAULT_WINDOW_DESTROY_PART OFF
+ #define DEFAULT_WINDOW_DESTROY_QUERY OFF
+ #define DEFAULT_SUPPRESS_SERVER_MOTD ON
+@@ -593,9 +639,9 @@
+ #endif 
+ /* _USE_LOCAL_CONFIG */
+ 
+-#define DEFAULT_KICK_REASON "Bitch-X BaBy!"
++#define DEFAULT_KICK_REASON "Bye loser!"
+ #define DEFAULT_PROTECT_CHANNELS "*"
+-#define DEFAULT_SHITLIST_REASON "Surplus Lamerz must go!!!!"
++#define DEFAULT_SHITLIST_REASON "Stupid people must die."
+ #define DEFAULT_BEEP_ON_MSG "MSGS"
+ #define DEFAULT_CMDCHARS "/"
+ #define DEFAULT_CONTINUED_LINE "          "
+@@ -606,7 +652,7 @@
+ #define DEFAULT_SHELL "/bin/sh"
+ #define DEFAULT_SHELL_FLAGS "-c"
+ #define DEFAULT_USERINFO ""
+-#define DEFAULT_XTERM "rxvt"
++#define DEFAULT_XTERM "xterm-color"
+ #define DEFAULT_XTERM_OPTIONS "-bg black -fg white"
+ #define DEFAULT_DCC_DLDIR "~"
+ 
