39675045a03d067b2bb578aa844f758297e41374
Andreas Klemm <andreas@FreeBSD.org>
2001-03-25 15:54:48 +0000

    - bump portrevision because important features were added to port   to make it more functional and userfriendly - added cflowd's startup script to FILESDIR, add startup options to   it, that are only valid for the patched cflowd. So later we overwrite   the sample startup script of "stock" cflowd.   This is done this way, because cflowd also can be used without flowscan.   But *if* we use cflowd with flowscan, we have to manage things to work   with flowscan. - install sample Makefile to graphs directory, so you can generate   graphs more easily as being described in the docu (see INSTALL) - Added comments to pkg-message file, that this Makefile has to be   updated whenever you add or remove protocol types in   ${PREFIX}/var/db/flows/bin/CampusIO.cf - comment post-install section more.. - keep_me file could be removed, since we install the sample Makefile   now in the graphs subdir

---
+++ net-mgmt/flowscan/files/cflowd.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+# rc script for cflowd
+# modified to use proper commandline options for flowscan
+# Andreas Klemm <andreas@FreeBSD.org>, So  25 Mär 2001 12:46:10 CEST
+# $FreeBSD$
+
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
+    echo "$0: Cannot determine the PREFIX" >&2
+    exit 1
+fi
+
+case "$1" in
+'start')
+	[ -x $PREFIX/sbin/cflowdmux -a -s $PREFIX/etc/cflowd.conf ] \
+	&& $PREFIX/sbin/cflowdmux $PREFIX/etc/cflowd.conf \
+	&& echo -n " cflowdmux"
+	sleep 2
+	[ -x $PREFIX/sbin/cflowd -a -s $PREFIX/etc/cflowd.conf ] \
+	&& $PREFIX/sbin/cflowd -s 300 -O 0 -m $PREFIX/etc/cflowd.conf \
+	&& echo -n " cflowd"
+	sleep 2
+	[ -x $PREFIX/sbin/cfdcollect -a -s $PREFIX/etc/cfdcollect.conf ] \
+	&& $PREFIX/sbin/cfdcollect $PREFIX/etc/cfdcollect.conf \
+	&& echo -n " cfdcollect"
+	;;
+ 
+'stop')
+	for i in cfdcollect cflowd cflowdmux
+	do
+		sleep 2
+		killall -15 $i && echo "graceful shutdown of $i"
+	done
+	;;
+esac
+
+exit 0
