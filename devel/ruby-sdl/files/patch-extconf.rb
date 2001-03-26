50a862a7b9c0a0eab6b04098db62d227b8faf5c2
Akinori MUSHA <knu@FreeBSD.org>
2001-03-26 18:05:55 +0000

    Oops.  The libc_r weirdness of 4-STABLE and prior prevents this from working.  Mark as broken for systems < 500018.

--- devel/ruby-sdl/files/patch-extconf.rb
+++ devel/ruby-sdl/files/patch-extconf.rb
@@ -1,5 +1,5 @@
 --- extconf.rb.orig	Sun Mar 25 17:10:56 2001
-+++ extconf.rb	Tue Mar 27 01:16:54 2001
++++ extconf.rb	Tue Mar 27 02:56:07 2001
 @@ -1,7 +1,9 @@
  require 'mkmf'
  
@@ -7,8 +7,8 @@
 -$LDFLAGS += `sdl-config --libs`.chomp
 +sdl_config = with_config('sdl-config', 'sdl-config')
 +
-+$CFLAGS += `#{sdl_config} --cflags`.chomp
-+$LDFLAGS += `#{sdl_config} --libs`.chomp
++$CFLAGS += ' ' + `#{sdl_config} --cflags`.chomp
++$LDFLAGS += ' ' + `#{sdl_config} --libs`.chomp
  
  if have_library("SDL_mixer","Mix_OpenAudio") then
    $CFLAGS+= " -D HAVE_SDL_MIXER "
