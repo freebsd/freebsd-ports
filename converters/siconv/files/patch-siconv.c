fe9cc65532951e0bff6b8e17abae8922cefd95db
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-03-26 17:10:50 +0000

    add siconv siconv will convert data from one character set to another

---
+++ converters/siconv/files/patch-siconv.c
@@ -0,0 +1,29 @@
+--- siconv.c.orig	Tue Mar 27 00:55:22 2001
++++ siconv.c	Tue Mar 27 01:02:20 2001
+@@ -27,7 +27,7 @@
+ *****************************************************************************/
+ 
+ 
+-#include <iconv.h>
++#include <giconv.h>
+ #include <errno.h>
+ #include <stdio.h>     /* printf() */
+ #include <unistd.h>    /* read() & write() */
+@@ -139,7 +139,7 @@
+ void usage(void) {
+   printf("usage:\n");
+   printf("\n");
+-  printf("  convert stdin from iso-8859-1 to utf8 on stdout\n");
++  printf("  convert stdin from iso-8859-1 to utf-8 on stdout\n");
+   printf("  $ siconv\n");
+   printf("  convert stdin from charset-A to charset-B on stdout\n");
+   printf("  $ siconv charset-A charset-B\n");
+@@ -149,7 +149,7 @@
+ 
+ int main(int argc, char *argv[]) {
+   char *from = "iso-8859-1";
+-  char *to = "unicode";
++  char *to = "utf-8";
+ 
+   if ( argc == 3 ) {
+     from = argv[1];
