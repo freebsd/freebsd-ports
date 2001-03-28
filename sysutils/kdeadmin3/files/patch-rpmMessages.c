0df4aac7f09eaca9d3b4104e6fb1cc979069acf7
Dmitry Sivachenko <demon@FreeBSD.org>
2001-03-28 14:51:51 +0000

    Fix typo.

--- sysutils/kdeadmin3/files/patch-rpmMessages.c
+++ sysutils/kdeadmin3/files/patch-rpmMessages.c
@@ -1,5 +1,5 @@
---- rpmMessages.c.orig	Wed Mar 28 18:43:45 2001
-+++ rpmMessages.c	Wed Mar 28 18:43:55 2001
+--- kpackage/rpmMessages.c.orig	Wed Mar 28 18:43:45 2001
++++ kpackage/rpmMessages.c	Wed Mar 28 18:43:55 2001
 @@ -7,7 +7,6 @@
  #include <stdio.h>
  #include <string.h>
