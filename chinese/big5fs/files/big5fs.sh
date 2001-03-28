c30a3aad8700cbc992d281dcf72a133f31a73db7
keith <keith@FreeBSD.org>
2001-03-28 17:44:25 +0000

    Add another kernel module:	big5ntfs.ko
    This kernel module lets users read BIG5 filenames on an NTFS.  The author
    is <huangant.bbs@bbs.cs.ntou.edu.tw>.  The original patch is located at:
    http://www.cis.nctu.edu.tw/~gis89501/bsd/ntfs/chinese_ntfs-4.3b2-ro.patch
    It is slightly modified to accomodate port building.
    
    It was tested on 4.3-BETA with Windows 2000 by the author, and 5.0-CURRENT
    by me (also with 2000).

--- chinese/big5fs/files/big5fs.sh
+++ chinese/big5fs/files/big5fs.sh
@@ -10,10 +10,15 @@ start)
 		echo -n ' Big5-msdos'
 		kldload /usr/local/modules/big5msdos.ko
 	fi
+	if [ -x @@PREFIX@@/modules/big5ntfs.ko ]; then
+		echo -n ' Big5 ntfs'
+		kldload /usr/local/modules/big5ntfs.ko
+	fi
 	;;
 stop)
 	kldunload -v -n big5cd9660 && echo -n ' Big5-cd9660'
 	kldunload -v -n big5msdos && echo -n ' Big5-msdos'
+	kldunload -v -n big5ntfs && echo -n ' Big5-ntfs'
 	;;
 *)
 	echo "Usage: big5fs.sh {start|stop}" >&2
