063c3053c4a48765995a8bb42bfad5860f06ae80
demon <demon@FreeBSD.org>
2001-03-28 14:46:25 +0000

    Fix build on machines with rpm installed.
    PR:		26167

---
+++ sysutils/kdeadmin3/files/patch-rpmMessages.c
@@ -0,0 +1,10 @@
+--- rpmMessages.c.orig	Wed Mar 28 18:43:45 2001
++++ rpmMessages.c	Wed Mar 28 18:43:55 2001
+@@ -7,7 +7,6 @@
+ #include <stdio.h>
+ #include <string.h>
+ /* stupid RPM headers! */
+-typedef __off64_t off64_t;
+ #include <rpm/rpmlib.h>
+ #include "rpmMessages.h"
+ 
