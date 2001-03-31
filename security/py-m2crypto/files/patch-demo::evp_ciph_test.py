a50c43fd88ac1c67412212ffb09bce029d30c6d9
will <will@FreeBSD.org>
2001-03-31 01:52:00 +0000

    Add py-m2crypto 0.06, a python wrapper for OpenSSL.
    PR:		26155
    Submitted by:	Peter Haight <peterh@sapros.com>

---
+++ security/py-m2crypto/files/patch-demo::evp_ciph_test.py
@@ -0,0 +1,10 @@
+--- ../demo/evp_ciph_test.py.orig	Tue Mar 27 12:34:18 2001
++++ ../demo/evp_ciph_test.py	Tue Mar 27 12:34:25 2001
+@@ -47,7 +47,6 @@
+ 
+ if __name__=='__main__':
+     ciphers=['bf_ecb', 'bf_cbc', 'bf_cfb', 'bf_ofb',\
+-        'idea_ecb', 'idea_cbc', 'idea_cfb', 'idea_ofb',\
+         'cast5_ecb', 'cast5_cbc', 'cast5_cfb', 'cast5_ofb',\
+         'rc5_ecb', 'rc5_cbc', 'rc5_cfb', 'rc5_ofb',\
+         'des_ecb', 'des_cbc', 'des_cfb', 'des_ofb',\
