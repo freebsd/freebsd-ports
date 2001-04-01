63628b3ac927d4b9ef92e65ccc90be3efa8955a5
will <will@FreeBSD.org>
2001-04-01 05:58:49 +0000

    Add simple patch to fix bogon where kpresenter crashes on startup.
    Suggested by:	demon
    Obtained from:	KDE PR system
    Originator:	percy@caldera.de

---
+++ editors/koffice-kde3/files/patch-kpresenter::kpresenter_view.cc
@@ -0,0 +1,11 @@
+--- kpresenter/kpresenter_view.cc	Sat Oct 14 08:52:09 2000
++++ kpresenter/kpresenter_view.cc.new	Sat Mar 31 22:38:40 2001
+@@ -116,7 +116,7 @@
+ };
+ 
+ static const char *pagedown_xpm[] = {
+-"    16    16        2            1",
++"    14    14        2            1",
+ ". c #000000",
+ "# c none",
+ "##############",
