4e2aec370c6e79edd496fbe2385b6fbcb80605d0
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2001-04-01 05:41:12 +0000

    New port: FURI is a java Gnutella distributed, peer-to-peer file sharing system

---
+++ net/furi/files/furi.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export CLASSPATH
+
+if [ -z "${CLASSPATH}" ]
+then
+	CLASSPATH=/usr/local/share/java/classes/jfc-1.1.1/swingall.jar:/usr/local/share/java/classes/furi/furi.jar:/usr/local/jre1.1.8/lib:/usr/local/jdk1.1.8/lib/classes.zip
+else
+	CLASSPATH=${CLASSPATH}:/usr/local/share/java/classes/jfc-1.1.1/swingall.jar:/usr/local/share/java/classes/furi/furi.jar:/usr/local/jre1.1.8/lib:/usr/local/jdk1.1.8/lib/classes.zip
+fi
+
+exec /usr/local/bin/javavm furi.f
