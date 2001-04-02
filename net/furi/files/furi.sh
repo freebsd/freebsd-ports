5cda479d4a72f5f2640653f267c346d3898fcddf
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2001-04-02 03:02:24 +0000

    Properly add variable to the environment after last partially bogus commit

--- net/furi/files/furi.sh
+++ net/furi/files/furi.sh
@@ -4,7 +4,7 @@ if [ -z "${CLASSPATH}" ]
 then
 	CLASSPATH=/usr/local/share/java/classes/jfc-1.1.1/swingall.jar:/usr/local/share/java/classes/furi/furi.jar:/usr/local/jre1.1.8/lib:/usr/local/jdk1.1.8/lib/classes.zip
 else
-	CLASSPATH=${CLASSPATH}:/usr/local/share/java/classes/jfc-1.1.1/swingall.jar:/usr/local/share/java/classes/furi/furi.jar:/usr/local/jre1.1.8/lib:/usr/local/jdk1.1.8/lib/classes.zip
+	CLASSPATH="${CLASSPATH}:/usr/local/share/java/classes/jfc-1.1.1/swingall.jar:/usr/local/share/java/classes/furi/furi.jar:/usr/local/jre1.1.8/lib:/usr/local/jdk1.1.8/lib/classes.zip"
 fi
 
-exec /usr/local/bin/javavm furi.f &
+exec env CLASSPATH="${CLASSPATH}" /usr/local/bin/javavm furi.f &
