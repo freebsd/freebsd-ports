76838d9bad252996a2703ab1f99efa09769f8ede
lioux <lioux@FreeBSD.org>
2001-04-02 02:18:38 +0000

    Do not pollute the ambient with internal variables

--- net/furi/files/furi.sh
+++ net/furi/files/furi.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export CLASSPATH
-
 if [ -z "${CLASSPATH}" ]
 then
 	CLASSPATH=/usr/local/share/java/classes/jfc-1.1.1/swingall.jar:/usr/local/share/java/classes/furi/furi.jar:/usr/local/jre1.1.8/lib:/usr/local/jdk1.1.8/lib/classes.zip
@@ -9,4 +7,4 @@ else
 	CLASSPATH=${CLASSPATH}:/usr/local/share/java/classes/jfc-1.1.1/swingall.jar:/usr/local/share/java/classes/furi/furi.jar:/usr/local/jre1.1.8/lib:/usr/local/jdk1.1.8/lib/classes.zip
 fi
 
-exec /usr/local/bin/javavm furi.f
+exec /usr/local/bin/javavm furi.f &
