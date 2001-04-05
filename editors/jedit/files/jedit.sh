137226003c5c8944176dc347f8601e819346ba99
sobomax <sobomax@FreeBSD.org>
2001-04-05 10:31:58 +0000

    Update to 3.0.2.

--- editors/jedit/files/jedit.sh
+++ editors/jedit/files/jedit.sh
@@ -3,4 +3,4 @@
 CLASSPATH=${CLASSPATH}:%%PREFIX%%/share/java/jedit/swingall.jar:%%PREFIX%%/share/java/jedit/jedit.jar
 export CLASSPATH
 
-exec %%LOCALBASE%%/bin/javavm org.gjt.sp.jedit.jEdit $@
+exec %%LOCALBASE%%/bin/javavm org.gjt.sp.jedit.jEdit "$@"
