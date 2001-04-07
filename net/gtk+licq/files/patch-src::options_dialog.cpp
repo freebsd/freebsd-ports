1c02f45ba64675cb2fb1b79bb22f9fba84b0546d
Will Andrews <will@FreeBSD.org>
2001-04-07 02:58:04 +0000

    Add gtk+licq 0.50.1, a gtk+ plugin for licq.
    PR:		22109
    Submitted by:	Jeremy Norris <ishmael27@home.com>

---
+++ net/gtk+licq/files/patch-src::options_dialog.cpp
@@ -0,0 +1,9 @@
+--- src/options_dialog.cpp.orig	Sat Jul  1 10:55:29 2000
++++ src/options_dialog.cpp	Tue Sep 19 21:47:10 2000
+@@ -1,5 +1,6 @@
+ #include "gtk-gui.h"
+ 
++#include <sys/unistd.h>
+ #include <sys/time.h>
+ #include <stdio.h>
+ #include <sys/types.h>
---
+++ net/gtk+licq/files/patch-src::plugin_dialog.cpp
@@ -0,0 +1,9 @@
+--- src/plugin_dialog.cpp.orig	Mon Mar 27 17:59:06 2000
++++ src/plugin_dialog.cpp	Tue Sep 19 21:47:13 2000
+@@ -1,5 +1,6 @@
+ #include "gtk-gui.h"
+ 
++#include <sys/unistd.h>
+ #include <sys/time.h>
+ #include <stdio.h>
+ #include <sys/types.h>
