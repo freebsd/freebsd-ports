42adf95880d950ae71ac9667f575382e117b99dc
obrien <obrien@FreeBSD.org>
2001-04-08 15:48:29 +0000

    rsync sometimes freezes during execution if it is run over ssh. rsync turns on blocking I/O mode if remote shell command is rsh (ie. matched RSYNC_RSH) to work around some broken rsh implementations on other platforms.
    The submitter of the rsh->ssh patches overloaded RSYNC_RSH for ssh.
    That turns out to be a Wrong Thing.  This change implements the parallel
    RSYNC_SSH.
    
    PR:		26376
    Submitted by:	adrian

--- net/rsync/files/ssh-patch-rsync.h
+++ net/rsync/files/ssh-patch-rsync.h
@@ -1,5 +1,8 @@
 --- rsync.h.orig	Sat Aug 19 06:10:39 2000
-+++ rsync.h	Thu Mar  8 03:31:33 2001
-@@ -75 +75 @@
--#define RSYNC_RSH "rsh"
-+#define RSYNC_RSH "ssh"
++++ rsync.h	Sat Apr  7 23:31:00 2001
+@@ -75,4 +75,5 @@
+ #define RSYNC_RSH "rsh"
+ #endif
++#define RSYNC_SSH "ssh"
+ 
+ #include <sys/types.h>
