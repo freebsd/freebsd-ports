b872ec2a6febd5479def340dce4034ab3c200753
sobomax <sobomax@FreeBSD.org>
2001-04-11 10:14:48 +0000

    Update to 1.3.13.
    Prompted by:	"Zach Dykstra" <dykstraz@central.edu>

---
+++ games/asc/files/patch-source::ai::ai_common.h
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- source/ai/ai_common.h	2001/04/11 09:18:02	1.1
++++ source/ai/ai_common.h	2001/04/11 09:18:09
+@@ -25,6 +25,7 @@
+ #include <math.h>
+ #include <algorithm>
+ #include <memory>
++#include <machine/float.h>
+ 
+ #include "ai.h"
+ 
