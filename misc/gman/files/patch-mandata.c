9e81a1303c860e73c84434122fa8de1c1c3f3436
ijliao <ijliao@FreeBSD.org>
2001-04-14 11:21:15 +0000

    upgrade to 0.9.2
    PR:		26555
    Submitted by:	tkato@prontomail.ne.jp

--- misc/gman/files/patch-mandata.c
+++ misc/gman/files/patch-mandata.c
@@ -1,43 +1,7 @@
---- mandata.c.orig	Mon Mar 26 16:40:49 2001
-+++ mandata.c	Sat Mar 31 00:21:55 2001
-@@ -58,7 +58,7 @@
- 	if(path_name) free(path_name);
- }
- 
--ManPath::GetSize(int section_ID)
-+int ManPath::GetSize(int section_ID)
- {
- 	int count;
- 	int i;
-@@ -67,7 +67,7 @@
- 	return count;
- }
- 
--ManPath::GetItems(int section_ID, ManItem * buffer[])
-+int ManPath::GetItems(int section_ID, ManItem * buffer[])
- {
- 	int count;
- 	int i;
-@@ -80,7 +80,7 @@
- 
- struct stat state;
- 
--ManPath::LoadManPath(char * path_name)
-+int ManPath::LoadManPath(char * path_name)
- {
- 	DIR *dirp;
- 	#if defined(SYS_DIR)||defined(NDIR)
-@@ -125,7 +125,7 @@
- 	return 0;
- }
- 
--ManPath::LoadManSubPath(char * sub_name)
-+int ManPath::LoadManSubPath(char * sub_name)
- {
- 	DIR *dirp;
- 	#if defined(SYS_DIR)||defined(NDIR)
-@@ -242,11 +242,11 @@
- 	char loc_name[BUFFER_SIZE];
+--- mandata.c.orig	Sat Mar 31 19:16:19 2001
++++ mandata.c	Sat Apr 14 05:12:49 2001
+@@ -243,11 +243,11 @@
+ 	char buffer2[BUFFER_SIZE];
  	switch ((int)context->get_value("show_mode")) {
  	case 0:
 -		sprintf(buffer,"%s -T '%s manual page' -n GMan -e man ",
@@ -52,7 +16,7 @@
  		//g_warning(buffer);
  		if(!fork())
  		{
-@@ -257,9 +257,9 @@
+@@ -258,9 +258,9 @@
  		break;
  	case 1:
  		sprintf(loc_name," ~/.gman.%s.ps ",get_display_name(buffer1));
