24e186934609267989161df4a1b9480682058755
Clive Lin <clive@FreeBSD.org>
2001-04-14 01:06:55 +0000

    Resolve conflicts with the updating of news/tin to make it buildable.
    It seems that tin itself now has more ability to handle MB.
    More inspection on chinese/tin may be required to remove obsoleted method.
    
    (I will forward this commit log to its MAINTAINER and
     freebsd-taiwan-chinese@maillist.tw.freebsd.org)

--- chinese/tin/files/patch-charset.c
+++ chinese/tin/files/patch-charset.c
@@ -1,20 +1,20 @@
---- src/charset.c.orig	Mon Jul 24 21:30:22 2000
-+++ src/charset.c	Sat Sep 23 14:03:14 2000
-@@ -330,7 +330,7 @@
- 	unsigned char *c;
- 
- 	for (c= (unsigned char *)buf; *c; c++) {
--		if (!my_isprint(*c))
-+		if (!my_isprint(*c) && *c != 27)
- 			*c = '?';
- 	}
- }
-@@ -348,7 +348,7 @@
- 	unsigned char *c;
+--- src/charset.c.orig	Mon Jan 29 18:32:23 2001
++++ src/charset.c	Sat Apr 14 08:50:15 2001
+@@ -335,7 +335,7 @@
  
  	for (c = (unsigned char *)buf; *c; c++) {
--		if (!(my_isprint(*c) || *c == 8 || *c == 9 || *c == 10 || *c == 12 || *c == 13))
-+		if (!(my_isprint(*c) || *c == 8 || *c == 9 || *c == 10 || *c == 12 || *c == 13 || *c==27))
+ #ifdef ENABLE_MBLEN
+-		if (!my_isprint(*c) && (t_len = mblen(c, MAX(2,MB_CUR_MAX))) <= 1)
++		if (!my_isprint(*c) && (t_len = mblen(c, MAX(2,MB_CUR_MAX))) <= 1  && *c != 27)
  			*c = '?';
- 	}
- }
+ 		while (--t_len > 0)
+ 			c++;
+@@ -364,7 +364,7 @@
+ 
+ 	for (c = (unsigned char *)buf; *c; c++) {
+ #	ifdef ENABLE_MBLEN
+-		if (!(my_isprint(*c) || *c == 8 || *c == 9 || *c == 10 || *c == 12 || *c == 13) && (t_len = mblen(c, MAX(2,MB_CUR_MAX))) <= 1)
++		if (!(my_isprint(*c) || *c == 8 || *c == 9 || *c == 10 || *c == 12 || *c == 13  || *c==27) && (t_len = mblen(c, MAX(2,MB_CUR_MAX))) <= 1)
+ 			*c = '?';
+ 		while (--t_len > 0)
+ 			c++;
