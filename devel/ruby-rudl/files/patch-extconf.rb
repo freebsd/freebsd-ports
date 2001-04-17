1c4294e2e9e5b5cd972d28dfadc312cf944a88fc
Akinori MUSHA <knu@FreeBSD.org>
2001-04-17 13:03:51 +0000

    Update to 0.2.

--- devel/ruby-rudl/files/patch-extconf.rb
+++ devel/ruby-rudl/files/patch-extconf.rb
@@ -1,35 +1,12 @@
---- extconf.rb.orig	Tue Apr 10 07:12:54 2001
-+++ extconf.rb	Tue Apr 10 16:56:53 2001
-@@ -5,20 +5,20 @@
- 
- makeMakefile=false
- 
--havePthread=have_library('pthread')
-+sdl_config = with_config("sdl-config", "sdl-config")
+--- extconf.rb.orig	Wed Apr 11 08:36:36 2001
++++ extconf.rb	Tue Apr 17 22:01:42 2001
+@@ -24,8 +24,7 @@
+ have_header('SDL_image.h')
+ have_header('SDL_mixer.h')
  
 -if have_library('SDL', 'SDL_Quit') and 
--		have_library('SDLmain') then
-+have_library('pthread')
+-		have_library('SDLmain') and 
++if have_func('SDL_Quit') and 
+ 		have_header('SDL.h') then
  
--	if /mswin32|cygwin/ =~ RUBY_PLATFORM then
--		# Make sure SDL.h is to be found somewhere!
--		makeMakefile=have_header('SDL.h')
--	else
--		$CFLAGS += `sdl-config --cflags`.chomp
--		$LDFLAGS += `sdl-config --libs`.chomp
--		makeMakefile=have_header('SDL.h') and havePthread
--	end
-+if /mswin32|cygwin/ =~ RUBY_PLATFORM then
-+	have_library('SDL', 'SDL_Quit')
-+	have_library('SDLmain')
-+else
-+	$CFLAGS += `#{sdl_config} --cflags`.chomp
-+	$LDFLAGS += `#{sdl_config} --libs`.chomp
- end
- 
--if makeMakefile then create_makefile('RuDL') end
-+if have_func('SDL_Quit') then
-+	makeMakefile=have_header('SDL.h')
-+end
- 
-+if makeMakefile then create_makefile('RuDL') end
+ 	create_makefile('RuDL')
