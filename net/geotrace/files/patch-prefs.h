1a1c7f34b275761c0f86df6202f1b1f1e50faf7b
ijliao <ijliao@FreeBSD.org>
2001-04-19 10:58:10 +0000

    add geotrace Geotrace is a geographical traceroute utility.

---
+++ net/geotrace/files/patch-prefs.h
@@ -0,0 +1,11 @@
+--- prefs.h.orig	Thu Apr 19 18:34:56 2001
++++ prefs.h	Thu Apr 19 18:38:00 2001
+@@ -3,7 +3,7 @@
+ #define TIMEOUT_DEF	5
+ #define RESOLVE_DEF	1
+ #define GEO_PREFS_FILE	".geotrace"
+-#define MAPS_DIR_DEF	"maps/"
++#define MAPS_DIR_DEF	"%%PREFIX%%/etc/maps/"
+ #define START_MAP_DEF	"World50.gif"
+ #define LINE_COLOR_RED_DEF	0
+ #define LINE_COLOR_GREEN_DEF	0
