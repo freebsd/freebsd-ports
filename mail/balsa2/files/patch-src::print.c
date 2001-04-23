9fb377c18ee82137477237e78d720379f623171a
sobomax <sobomax@FreeBSD.org>
2001-04-23 15:55:53 +0000

    Update to 1.1.4.

---
+++ mail/balsa2/files/patch-src::print.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/print.c	2001/04/23 15:33:41	1.1
++++ src/print.c	2001/04/23 15:33:56
+@@ -22,7 +22,7 @@
+ #include <gnome.h>
+ #include <config.h>
+ #include <balsa-app.h>
+-#include <iconv.h>
++#include <giconv.h>
+ 
+ #include "print.h"
+ 
