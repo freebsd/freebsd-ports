56893991f5e95e6b4364af3875d7ccda8a5153ff
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-04-23 06:26:56 +0000

    upgrade to 0.1.3

--- security/crank/files/patch-plugins.c
+++ security/crank/files/patch-plugins.c
@@ -1,11 +1,11 @@
---- plugins.c.orig	Thu Apr 19 03:34:39 2001
-+++ plugins.c	Thu Apr 19 03:35:01 2001
-@@ -60,7 +60,7 @@
-     if (!g_module_supported())
- 	g_error("Plugins not supported on this platform.");
- 
--    if (!(plugin_dir = opendir("plugins")))
-+    if (!(plugin_dir = opendir(PLUGIN_DIR)))
- 	g_error("Could not open plugin directory - %s", PLUGIN_DIR);
- 
-     /* Loop over each file in the plugin directory */
+--- plugins.c.orig	Mon Apr 23 14:14:51 2001
++++ plugins.c	Mon Apr 23 14:16:13 2001
+@@ -61,7 +61,7 @@
+ 	g_warning("Plugins not supported on this platform.");
+ 	return;
+     }
+-    if (!(plugin_dir = opendir("plugins"))) {
++    if (!(plugin_dir = opendir(PLUGIN_DIR))) {
+ 	g_warning("Could not open plugin directory - %s", PLUGIN_DIR);
+ 	return;
+     }
