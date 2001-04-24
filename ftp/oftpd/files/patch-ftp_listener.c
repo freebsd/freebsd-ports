269968ece393501f15270daac023528664df6c18
kevlo <kevlo@FreeBSD.org>
2001-04-24 13:12:34 +0000

    Upgrade to version 0.3.5
    PR: 26779
    Submitted by: MAINTAINER

--- ftp/oftpd/files/patch-ftp_listener.c
+++ ftp/oftpd/files/patch-ftp_listener.c
@@ -1,10 +1,11 @@
---- src/ftp_listener.c.old	Sat Mar 31 12:01:10 2001
-+++ src/ftp_listener.c	Sat Mar 31 12:01:24 2001
-@@ -3,6 +3,7 @@
-  */
- 
- #include <config.h>
-+#include <sys/types.h>
- #include <sys/socket.h>
- #include <netinet/in.h>
- #include <arpa/inet.h>
+--- src/ftp_listener.c.orig	Wed Apr 18 23:42:48 2001
++++ src/ftp_listener.c	Sun Apr 22 14:26:41 2001
+@@ -361,7 +361,7 @@
+          if (fd >= 0) {
+              
+              tcp_nodelay = 1;
+-             if (setsockopt(fd, SOL_TCP, TCP_NODELAY, (void *)&tcp_nodelay,
++             if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, (void *)&tcp_nodelay,
+                  sizeof(int)) != 0)
+              {
+                  syslog(LOG_ERR,
--- ftp/oftpd/files/patch-ftp_session.c
+++ ftp/oftpd/files/patch-ftp_session.c
@@ -1,5 +1,5 @@
---- src/ftp_session.c.orig	Thu Mar 29 00:41:18 2001
-+++ src/ftp_session.c	Sat Mar 31 12:07:49 2001
+--- src/ftp_session.c.orig	Wed Apr 18 23:43:37 2001
++++ src/ftp_session.c	Mon Apr 23 00:38:15 2001
 @@ -3,13 +3,13 @@
   */
  
@@ -15,3 +15,33 @@
  #include <fcntl.h>
  #include <sys/socket.h>
  #include <errno.h>
+@@ -1130,7 +1130,8 @@
+         /* for sendfile(), we still have to use a loop to avoid 
+            having our watchdog time us out on large files - it does
+            allow us to avoid an extra copy to/from user space */
+-#ifdef HAVE_LINUX_SENDFILE
++#ifdef FOO_HAVE_LINUX_SENDFILE
++// This doesn't work yet. I'm working with the author to fix it.
+         offset = f->file_offset;
+         file_size = stat_buf.st_size - offset;
+         while (offset < stat_buf.st_size) {
+@@ -1139,11 +1140,14 @@
+             if (amt_to_send > 65536) {
+                 amt_to_send = 65536;
+             }
+-            sendfile_ret = sendfile(socket_fd, 
+-                                    file_fd, 
+-                                    &offset, 
+-                                    amt_to_send);
+-            if (sendfile_ret != amt_to_send) {
++            sendfile_ret = sendfile(file_fd,
++                                    socket_fd, 
++                                    offset,
++                                    amt_to_send,
++                                    NULL,
++                                    NULL,
++                                    0);
++            if (sendfile_ret != 0) {
+                 reply(f, 550, "Error sending file; %s.", strerror(errno));
+                 goto exit_retr;
+             }
