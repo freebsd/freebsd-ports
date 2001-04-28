c91263a992545ee424984f07d75e40f399e8330e
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-04-28 06:25:18 +0000

    upgrade to 0.7.7

--- net/pfinger/files/patch-src::finger.h
+++ net/pfinger/files/patch-src::finger.h
@@ -1,11 +1,11 @@
---- src/finger.h.orig	Thu Apr 26 01:39:28 2001
-+++ src/finger.h	Thu Apr 26 01:39:38 2001
+--- src/finger.h.orig	Sat Apr 28 14:21:17 2001
++++ src/finger.h	Sat Apr 28 14:21:43 2001
 @@ -21,12 +21,12 @@
  #ifndef FINGER_H
  #define FINGER_H
  
 +#include <sys/types.h>
- #include <utmp.h>
+ #include "utmplib.h"
  #ifndef UT_NAMESIZE
  #define UT_NAMESIZE 16
  #endif
