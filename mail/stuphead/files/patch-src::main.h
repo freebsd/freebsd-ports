36039c0cc24c37acc932121ab78cad8daa177743
Alexander Langer <alex@FreeBSD.org>
2001-05-09 14:30:05 +0000

    Update to 0.5.2.
    PR:		28085
    Submitted by:	Sergey A. Osokin <osa@freebsd.org.ru>

---
+++ mail/stuphead/files/patch-src::main.h
@@ -0,0 +1,14 @@
+--- src/main.h.orig	Fri May  4 20:46:11 2001
++++ src/main.h	Fri May  4 20:46:57 2001
+@@ -71,9 +71,9 @@
+ #define DEFAULT_INC_PROGRAM	"inc"
+ /* #define DEFAULT_INC_PATH	"/usr/bin/imget" */
+ /* #define DEFAULT_INC_PROGRAM	"imget" */
+-#define DEFAULT_SPOOL_PATH	"/var/spool/mail"
++#define DEFAULT_SPOOL_PATH	"/var/mail"
+ 
+-#define DEFAULT_ISPELL_COMMAND	"ispell -d russian -a"
++#define DEFAULT_ISPELL_COMMAND	"ispell-ru -a"
+ 
+ #define BUFFSIZE			8192
+ #define MAXPATHLEN			4095
