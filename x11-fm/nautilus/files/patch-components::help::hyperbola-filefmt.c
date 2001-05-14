12cd9781e43fe05ea79416465381e02b427f568d
sobomax <sobomax@FreeBSD.org>
2001-05-14 17:18:52 +0000

    Update to 1.0.3.

---
+++ x11-fm/nautilus/files/patch-components::help::hyperbola-filefmt.c
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- components/help/hyperbola-filefmt.c	2001/05/14 15:56:20	1.1
++++ components/help/hyperbola-filefmt.c	2001/05/14 15:56:32
+@@ -30,6 +30,7 @@
+ #include <libgnome/gnome-util.h>
+ #include <limits.h>
+ #include <parser.h>
++#include <sys/types.h>
+ #include <regex.h>
+ #include <string.h>
+ #include <tree.h>
--- x11-fm/nautilus/files/patch-libnautilus-extensions::nautilus-medusa-support.c
+++ x11-fm/nautilus/files/patch-libnautilus-extensions::nautilus-medusa-support.c
@@ -1,14 +1,14 @@
 
 $FreeBSD$
 
---- libnautilus-extensions/nautilus-medusa-support.c	2001/05/10 11:39:29	1.1
-+++ libnautilus-extensions/nautilus-medusa-support.c	2001/05/10 11:40:00
-@@ -108,7 +108,7 @@
- 		/* Process files have numbers */
- 		if (nautilus_str_to_int (file->d_name,
- 					 &process_number)) {
--			stat_file_name = g_strdup_printf ("/proc/%d/stat", process_number);
-+			stat_file_name = g_strdup_printf ("/proc/%d/status", process_number);
- 			stat_file = fopen (stat_file_name, "r");
- 			g_free (stat_file_name);
+--- libnautilus-private/nautilus-medusa-support.c.orig	Wed Apr 11 21:36:16 2001
++++ libnautilus-private/nautilus-medusa-support.c	Mon May 14 17:45:24 2001
+@@ -84,7 +84,7 @@
+ 		 */
+ 		status = NAUTILUS_CRON_STATUS_OFF;
  
+-		stat_file_name = g_strdup_printf ("/proc/%d/stat", process_number);
++		stat_file_name = g_strdup_printf ("/proc/%d/status", process_number);
+ 		stat_file = fopen (stat_file_name, "r");
+ 		g_free (stat_file_name);
+ 		
