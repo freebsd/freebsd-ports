e6b8a5f53d3f7f36a7ec8dc4bedd6c8cdf63eea5
Maxim Sobolev <sobomax@FreeBSD.org>
2001-05-15 10:07:28 +0000

    Don't warn about non-existing {X11,LOCAL}BASE/libdata/pkgconfig.
    Prompted by:	olgeni, ade

--- devel/pkgconfig/files/patch-pkg.c
+++ devel/pkgconfig/files/patch-pkg.c
@@ -1,14 +1,37 @@
 
 $FreeBSD$
 
---- pkg.c	2001/04/29 11:38:41	1.1
-+++ pkg.c	2001/04/29 11:39:06
-@@ -97,6 +97,8 @@
+--- pkg.c.orig	Wed Jan  3 11:34:49 2001
++++ pkg.c	Tue May 15 11:48:04 2001
+@@ -40,7 +40,7 @@
+  * locations, ignoring duplicates
+  */
+ static void
+-scan_dir (const char *dirname)
++scan_dir (const char *dirname, void *quiet)
+ {
+   DIR *dir = opendir (dirname);
+   struct dirent *dent;
+@@ -51,8 +51,9 @@
+ 
+   if (!dir)
+     {
+-      fprintf (stderr, "Warning: cannot open directory '%s' in package search path: %s\n",
+-               dirname, g_strerror (errno));
++      if (!quiet)
++        fprintf (stderr, "Warning: cannot open directory '%s' in package search path: %s\n",
++                 dirname, g_strerror (errno));
+       return;
+     }
+ 
+@@ -96,7 +97,9 @@
+       locations = g_hash_table_new (g_str_hash, g_str_equal);
        
        g_slist_foreach (search_dirs, (GFunc)scan_dir, NULL);
-       scan_dir (PKGLIBDIR);
-+      scan_dir (XPKGLIBDIR);
-+      scan_dir (LPKGLIBDIR);
+-      scan_dir (PKGLIBDIR);
++      scan_dir (PKGLIBDIR, (void *)0);
++      scan_dir (XPKGLIBDIR, (void *)1);
++      scan_dir (LPKGLIBDIR, (void *)1);
      }
  }
  
