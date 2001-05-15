7715b6ee6b2544d56b375af95847140f001e4d99
dinoex <dinoex@FreeBSD.org>
2001-05-15 20:00:38 +0000

    - Add support for future 4.4 Release

--- graphics/qdraw/files/patch-unix_gcc.mk
+++ graphics/qdraw/files/patch-unix_gcc.mk
@@ -1,6 +1,7 @@
 --- make/unix_gcc.mk.orig	Fri Jul 28 20:54:11 2000
-+++ make/unix_gcc.mk	Fri Jan 26 07:05:06 2001
-@@ -34,12 +34,20 @@
++++ make/unix_gcc.mk	Tue May 15 21:53:41 2001
+@@ -33,13 +33,23 @@
+ ifdef freebsd
  GCCPATH=	/usr
  GCC=		cc
 +ifneq "${freebsd}" "34"
@@ -8,6 +9,7 @@
 +ifneq "${freebsd}" "41"
 +ifneq "${freebsd}" "42"
 +ifneq "${freebsd}" "43"
++ifneq "${freebsd}" "44"
  ifneq "${freebsd}" "50"
  GCCBASE=	/usr/local
  GCCTARGET=	i386-unknown-freebsd3.0
@@ -15,6 +17,7 @@
  GCCPATH=	${GCCBASE}/lib/gcc-lib/${GCCTARGET}/${GCCVER}
  GCC=		egcc
  endif
++endif
 +endif
 +endif
 +endif
