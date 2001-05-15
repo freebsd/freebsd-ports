76a7903fb88d1e81253a8aa3eaa17cb753c2dac4
Akinori MUSHA <knu@FreeBSD.org>
2001-05-15 19:01:19 +0000

    Update to 0.4.

--- devel/ruby-rudl/files/patch-extconf.rb
+++ devel/ruby-rudl/files/patch-extconf.rb
@@ -1,12 +1,14 @@
---- extconf.rb.orig	Wed Apr 11 08:36:36 2001
-+++ extconf.rb	Tue Apr 17 22:01:42 2001
-@@ -24,8 +24,7 @@
+--- extconf.rb.orig	Wed May 16 02:55:22 2001
++++ extconf.rb	Wed May 16 03:51:59 2001
+@@ -24,9 +24,7 @@
  have_header('SDL_image.h')
  have_header('SDL_mixer.h')
  
 -if have_library('SDL', 'SDL_Quit') and 
 -		have_library('SDLmain') and 
+-		have_header('SDL.h') then
+-
 +if have_func('SDL_Quit') and 
- 		have_header('SDL.h') then
- 
- 	create_makefile('RuDL')
++    have_header('SDL.h') then
+ 	create_makefile('RUDL')
+ end
