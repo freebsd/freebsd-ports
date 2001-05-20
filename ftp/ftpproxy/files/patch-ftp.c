f16ad320ace6ae5434af7dc33e8fe83940fc9dfc
Will Andrews <will@FreeBSD.org>
2001-05-20 03:15:26 +0000

    Add ftpproxy 1.0.1, a ftp proxy.
    PR:		27390
    Submitted by:	Philippe Le Berre <philippe@le-berre.com>

---
+++ ftp/ftpproxy/files/patch-ftp.c
@@ -0,0 +1,19 @@
+*** ftp.orig	Wed May 16 09:53:13 2001
+--- ftp.c	Wed May 16 09:54:08 2001
+***************
+*** 30,36 ****
+  
+  #include <time.h>
+  #include <signal.h>
+! #include <wait.h>
+  
+  #include <sys/types.h>
+  #include <sys/stat.h>
+--- 30,36 ----
+  
+  #include <time.h>
+  #include <signal.h>
+! #include <sys/wait.h>
+  
+  #include <sys/types.h>
+  #include <sys/stat.h>
---
+++ ftp/ftpproxy/files/patch-main.c
@@ -0,0 +1,19 @@
+*** main.orig	Wed May 16 09:50:37 2001
+--- main.c	Wed May 16 09:50:57 2001
+***************
+*** 29,35 ****
+  #include <stdarg.h>
+  
+  #include <signal.h>
+! #include <wait.h>
+  
+  #include <sys/types.h>
+  #include <sys/stat.h>
+--- 29,35 ----
+  #include <stdarg.h>
+  
+  #include <signal.h>
+! #include <sys/wait.h>
+  
+  #include <sys/types.h>
+  #include <sys/stat.h>
