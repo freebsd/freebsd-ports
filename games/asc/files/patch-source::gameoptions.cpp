003c4a120d241086ca090cfe9ad31505b835c9ae
sobomax <sobomax@FreeBSD.org>
2001-05-23 05:46:56 +0000

    Update to 1.3.16.

--- games/asc/files/patch-source::gameoptions.cpp
+++ games/asc/files/patch-source::gameoptions.cpp
@@ -1,18 +1,20 @@
 
 $FreeBSD$
 
---- source/gameoptions.cpp.orig	Sat Feb 17 20:16:39 2001
-+++ source/gameoptions.cpp	Sat Feb 17 20:18:31 2001
-@@ -96,13 +96,13 @@
- 
+--- source/gameoptions.cpp.orig	Tue May 22 22:28:53 2001
++++ source/gameoptions.cpp	Tue May 22 22:30:29 2001
+@@ -103,7 +103,7 @@
     defaultSuperVisorPassword.setName ( "" );
  
+ 
 -  #if USE_HOME_DIRECTORY == 0
-+  #ifndef _UNIX_
++  #if _UNIX_ == 0
     searchPath[0].setName ( ".\\" );
     for ( int i = 1; i < getSearchPathNum(); i++ )
        searchPath[i].setName ( NULL );
-   #else
+@@ -111,7 +111,7 @@
+    for ( int i = 0; i < getSearchPathNum(); i++ )
+       searchPath[i].setName ( NULL );
     searchPath[0].setName ( "~/.asc/" );
 -   searchPath[1].setName ( "/var/local/games/asc/" );
 +   searchPath[1].setName ( PREFIX "/share/asc/" );
