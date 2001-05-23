44f420bc2419c2c97968994e466e934ed6588470
Maxim Sobolev <sobomax@FreeBSD.org>
2001-05-23 05:46:56 +0000

    Update to 1.3.16.

--- games/asc/files/patch-source::unix::global_os.h
+++ games/asc/files/patch-source::unix::global_os.h
@@ -1,9 +1,9 @@
 
 $FreeBSD$
 
---- source/unix/global_os.h	2001/02/16 21:38:57	1.1
-+++ source/unix/global_os.h	2001/02/16 21:39:25
-@@ -38,7 +38,7 @@
+--- source/unix/global_os.h	2001/05/22 19:32:32	1.1
++++ source/unix/global_os.h	2001/05/22 19:33:06
+@@ -43,7 +43,7 @@
   #define global_os_h_included
  
    #ifndef minimalIO
@@ -12,3 +12,12 @@ $FreeBSD$
     #include sdlheader
    #endif 
  
+@@ -60,7 +60,7 @@
+   #endif
+ 
+   #define CASE_SENSITIVE_FILE_NAMES 1
+-  #define USE_HOME_DIRECTORY 1
++  #define USE_HOME_DIRECTORY 0
+ 
+ 
+ #endif
