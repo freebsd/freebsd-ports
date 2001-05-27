a5829f97bac6213325e8eddaed126fbf88b15f2f
ijliao <ijliao@FreeBSD.org>
2001-05-27 13:49:54 +0000

    upgrade to 4.3.2
    PR:		27273
    Submitted by:	maintainer

--- biology/nab/files/config.h
+++ biology/nab/files/config.h
@@ -1,7 +1,8 @@
 ###############################################################################
 # Configuration section:  look at sections (1) to (4) below, making
-#      changes that seem necessary.  For most UNIX systems, no changes
-#      should be necessary.
+#      changes that seem necessary.  For many UNIX systems, no changes
+#      should be necessary, but check especially the compiler options in
+#      section (4).
 #
 ###############################################################################
 #
@@ -9,7 +10,7 @@
 #    make command line (e.g. "make ARCH=sun4 NABHOME=/thr/eigen/nab5")
 #    or as environment variables.
 #
-#    NABHOME should be an absolute path to the directory above this one.
+#    NABHOME should be an absolute path to this directory.
 #    ARCH is used to distinguish compilations for various architectures.
 #
 #    Files created by "make install" end up in:
@@ -20,12 +21,8 @@
 #       You can choose ARCH as you wish, but it would generally 
 #       be related to the output of the "uname" command.
 #
-#BINDIR=$(NABHOME)/bin/$(ARCH)
-#LIBDIR=$(NABHOME)/lib/$(ARCH)
-#NABHOME=/usr/local/share/nab
-#ARCH=
-BINDIR=$(NABHOME)/bin
-LIBDIR=$(NABHOME)/lib
+BINDIR=$(NABHOME)/bin/$(ARCH)
+LIBDIR=$(NABHOME)/lib/$(ARCH)
 INCDIR=$(NABHOME)/include
 #
 ###############################################################################
@@ -37,9 +34,8 @@ INCDIR=$(NABHOME)/include
 #      included in FLIBS are always searched.)
 #
 #FLIBS=$(LIBDIR)/cifparse.a $(LIBDIR)/libsym.a
-#FLIBS=../cifparse/cifparse.a
 #
-#  following for Sun4, Solaris 2.x
+#  following for linking in fortran subroutines on Sun4, Solaris 2.x
 #
 #FLIBS=$(LIBDIR)/cifparse.a $(LIBDIR)/libsym.a -lF77 -lM77 -lsunmath
 #
@@ -69,32 +65,40 @@ SHELL=		/bin/sh
 #
 #  Set the C compiler, etc. 
 #
-#               (For GNU:  cc-->gcc; lex-->flex; yacc-->bison -y -t;
-#                Note: If you lexer is "really" flex, you need to set
-#                LEX=flex below.  For example, on many linux distributions,
-#                /usr/bin/lex is really just a pointer to /usr/bin/flex,
-#                so LEX=flex is necessary.  In general, gcc seems to need
-#                flex.)
+#          For GNU:  cc-->gcc; lex-->flex; yacc-->bison -y -t;
+#          Note: If you lexer is "really" flex, you need to set
+#          LEX=flex below.  For example, on many linux distributions,
+#          /usr/bin/lex is really just a pointer to /usr/bin/flex,
+#          so LEX=flex is necessary.  In general, gcc seems to need
+#          flex.
 #
+#          For 64-bit IRIX, add "-n32" unless 64-bit objects are 
+#          needed; it is also useful to add "-Wl,-dont_warn_unused"
+#          to keep the loader from complaining about libraries that
+#          are not used.
+#
+#          For RedHat Linux 5.1 (and other linuxes?) add -DHAVE_STRERROR
+#
+#	   For FreeBSD 4.2-STABLE, there seems to be a dependency
+#	   on bison.  Berkeley yacc was very problematic.
 CC=		cc
 LEX=    	flex
-YACC=   	yacc
+YACC=   	bison -y -t
 AR=		ar
 #
-#  Set the C-preprocessor.  Code for the preprocessor from gcc is in
-#    ../cccp;  it gets installed as $(NABHOME)/bin/$(ARCH)/cpp;
+#  Set the C-preprocessor.  Code for a small preprocessor is in
+#    uccp-0.7;  it gets installed as $(NABHOME)/bin/$(ARCH)/ucpp;
 #    this should generally *not* be changed.
 #
-#CPP=    $(NABHOME)/bin/$(ARCH)/cpp -lang-c++
-#CPP=    ../cccp/cpp -lang-c++
-CPP=    cc -E -x c++
+#  Again, for FreeBSD 4.2-STABLE, I had problems using the system
+#    C-preprocessor.
+#
+CPP=    ucpp -l
 #
 #  Use these CFLAGS if AVS = 0
-#  Notes: for 64-bit IRIX, add "-n32" to both CFLAGS and OCFLAGS
-#         for RedHat Linux 5.1 (and other linuxes?) add -DHAVE_STRERROR
 #
+CFLAGS= 
 OCFLAGS= -O
-CFLAGS= -DHAVE_STRERROR ${OCFLAGS}
 NABFLAGS=
 #
 #  Use these CFLAGS if AVS = 1
@@ -103,7 +107,7 @@ NABFLAGS=
 #OCFLAGS= 	-O -DAVS=$(AVS) -DAVSDIR='"$(AVSDIR)"' -I$(AVSINCDIR)
 #
 #
-#  For Win95, set SFX to ".exe"; for Unix,NT leave it empty:
+#  For Win95/98, set SFX to ".exe"; for Unix,NT leave it empty:
 #
 #SFX=.exe
 SFX=
