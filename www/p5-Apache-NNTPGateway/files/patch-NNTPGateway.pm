d01082dac50f71caa3abf42f35e0bd967a3ae381
Vanilla I. Shu <vanilla@FreeBSD.org>
2001-05-27 02:15:02 +0000

    A NNTP interface for mod_perl enabled Apache web server
    PR:		ports/24347
    Submitted by:	Sergey Skvortsov <skv@protey.ru>

---
+++ www/p5-Apache-NNTPGateway/files/patch-NNTPGateway.pm
@@ -0,0 +1,19 @@
+*** NNTPGateway.pm.orig	Mon Jan 15 18:13:44 2001
+--- NNTPGateway.pm	Mon Jan 15 18:14:10 2001
+***************
+*** 99,105 ****
+  # MESSAGES, stuff that is printed on the users's screen...  
+  # The current default language: Should be an entry in Messages_Map &
+  # LANGS_OK
+! my $USR_LANG = 'fr';
+  # Allowed languages choice.
+  my %LANGS_OK = ( 'fr' => 1, 'en' => 1 );
+  # All messages that could be printed.
+--- 99,105 ----
+  # MESSAGES, stuff that is printed on the users's screen...  
+  # The current default language: Should be an entry in Messages_Map &
+  # LANGS_OK
+! my $USR_LANG = 'en';
+  # Allowed languages choice.
+  my %LANGS_OK = ( 'fr' => 1, 'en' => 1 );
+  # All messages that could be printed.
