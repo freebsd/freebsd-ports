f1fd8a9dbf1b664c1e41b88fe298880b2085ae6d
sobomax <sobomax@FreeBSD.org>
2001-05-31 12:45:33 +0000

    Add gctpc 1.3, an USGS General Cartographic Transformation Package (GCTP).
    PR:		26927
    Submitted by:	Randall Hopper <aa8vb@nc.rr.com>

---
+++ misc/gctpc/files/sphdz.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDED_SPHDZ_H
+#define INCLUDED_SPHDZ_H
+
+int
+sphdz(long int isph, double *parm, double *r_major, double *r_minor, double *radius);
+
+#endif  /* INCLUDED_SPHDZ_H */
---
+++ misc/gctpc/files/untfz.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDED_UNTFZ_H
+#define INCLUDED_UNTFZ_H
+
+long
+untfz(long int inunit, long int outunit, double *factor);
+
+#endif  /* INCLUDED_UNTFZ_H */
