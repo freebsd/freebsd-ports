0092ce50fbacb23dcd9c0547ef82872062c1dc05
ijliao <ijliao@FreeBSD.org>
2001-06-01 03:47:52 +0000

    add spar A modular math parser

---
+++ math/spar/files/patch-spar::readline.c
@@ -0,0 +1,10 @@
+--- spar/readline.c.orig	Fri Jun  1 11:25:10 2001
++++ spar/readline.c	Fri Jun  1 11:25:24 2001
+@@ -30,6 +30,7 @@
+ #include "cmd_table.h"
+ 
+ #ifdef HAVE_READLINE_HISTORY_H
++#include <stdio.h>
+ #include <readline/history.h>
+ 
+ int
