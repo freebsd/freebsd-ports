c1ddf382fc71fd529a6edc91913d71b67c596d22
kevlo <kevlo@FreeBSD.org>
2001-06-05 12:42:34 +0000

    Update to version 1.0.7
    PR: 27881
    Submitted by: MAINTAINER

--- ftp/ftpproxy/files/patch-ftp.c
+++ ftp/ftpproxy/files/patch-ftp.c
@@ -1,19 +1,21 @@
 *** ftp.orig	Wed May 16 09:53:13 2001
 --- ftp.c	Wed May 16 09:54:08 2001
 ***************
-*** 30,36 ****
+*** 31,38 ****
   
   #include <time.h>
   #include <signal.h>
 ! #include <wait.h>
-  
+  #include <ctype.h>
+
   #include <sys/types.h>
   #include <sys/stat.h>
---- 30,36 ----
+--- 31,38 ----
   
   #include <time.h>
   #include <signal.h>
 ! #include <sys/wait.h>
-  
+  #include <ctype.h>
+
   #include <sys/types.h>
   #include <sys/stat.h>
