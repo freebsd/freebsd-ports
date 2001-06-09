68337827d3f99f3c8b327a33a9725bd9e5c2c41a
ijliao <ijliao@FreeBSD.org>
2001-06-09 17:35:10 +0000

    upgrade to 0.5.2

--- math/spar/files/patch-spar::readline.c
+++ math/spar/files/patch-spar::readline.c
@@ -1,5 +1,5 @@
---- spar/readline.c.orig	Fri Jun  1 11:25:10 2001
-+++ spar/readline.c	Fri Jun  1 11:25:24 2001
+--- text-fe/readline.c.orig	Fri Jun  1 11:25:10 2001
++++ text-fe/readline.c	Fri Jun  1 11:25:24 2001
 @@ -30,6 +30,7 @@
  #include "cmd_table.h"
  
