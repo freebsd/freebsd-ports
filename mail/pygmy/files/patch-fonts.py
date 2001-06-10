1e0bcc4aeac3d5f56f4b1990c6f61a430f77f482
sobomax <sobomax@FreeBSD.org>
2001-06-10 12:13:25 +0000

    Update to 0.5.9.

--- mail/pygmy/files/patch-fonts.py
+++ mail/pygmy/files/patch-fonts.py
@@ -1,14 +1,11 @@
 
 $FreeBSD$
 
---- msg.py.orig	Fri May 18 18:26:45 2001
-+++ msg.py	Sun May 20 09:28:55 2001
-@@ -20,7 +20,7 @@
+--- fonts.py.orig	Sun Jun 10 13:43:07 2001
++++ fonts.py	Sun Jun 10 13:43:19 2001
+@@ -1,4 +1,4 @@
+ ## $Id: fonts.py,v 1.1 2001/06/07 14:46:57 kjetilja Exp $
  
- ## Fonts
  BOLD_FONT = '-*-helvetica-bold-r-normal--12-120-75-75-p-70-iso8859-1'
 -NORMAL_FONT = '-*-helvetica-medium-r-normal--12-120-75-75-p-70-iso8859-1'
 +NORMAL_FONT = '-*-helvetica-bold-r-normal--12-120-75-75-p-70-iso8859-1'
- 
- ## Error messages from the GUI
- err1 = """Error opening destination file"""
