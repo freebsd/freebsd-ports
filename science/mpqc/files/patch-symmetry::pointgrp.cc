3f8dc1279c9e087be35982488ff82bea1e0020f8
Will Andrews <will@FreeBSD.org>
2001-06-11 03:27:04 +0000

    Add mpqc 1.2.5, the massively parallel quantum computing library and program.
    PR:		26908
    Submitted by:	batman <batman@udel.edu>

---
+++ science/mpqc/files/patch-symmetry::pointgrp.cc
@@ -0,0 +1,10 @@
+--- src/lib/math/symmetry/pointgrp.cc.orig	Wed Feb 21 20:52:39 2001
++++ src/lib/math/symmetry/pointgrp.cc	Fri Apr 27 15:28:50 2001
+@@ -54,6 +54,7 @@
+ #include <string.h>
+ #include <ctype.h>
+ #include <math.h>
++#include <float.h>
+
+ #include <util/misc/formio.h>
+ #include <util/state/stateio.h>
