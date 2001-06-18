6a9125be8eb14b7ecc3c471c2c489196e0a8b631
Akinori MUSHA <knu@FreeBSD.org>
2001-06-18 13:16:29 +0000

    Add a couple of new MASTER_SITE_RINGSERVER entries to a comment section.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -176,6 +176,7 @@ MASTER_SITE_PERL_CPAN+=	\
 #	ring.data-hotel.net		ring.yamanashi.ac.jp
 #	ring.nihon-u.ac.jp		ring.ctc.jp
 #	ring.riken.go.jp		ring.hosei.ac.jp
+#	ring.dcs.pref.mie.jp		ring.wakwak.com
 #
 # (The latest list is available at http://www.ring.gr.jp/)
 #
