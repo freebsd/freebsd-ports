d7adf1ec7e6907f7d5e664835112f992d112c6ef
Ade Lovett <ade@FreeBSD.org>
2001-06-19 08:13:50 +0000

    Add gnome-db support.

---
+++ devel/libglade/files/patch-glade::glade-gnomedb.c
@@ -0,0 +1,12 @@
+--- glade/glade-gnomedb.c.orig	Mon Feb 12 19:37:54 2001
++++ glade/glade-gnomedb.c	Tue Jun 19 03:01:45 2001
+@@ -46,3 +46,3 @@
+      
+-	wid = gnome_db_login_new (NULL, NULL, NULL);
++	wid = gnome_db_login_new (NULL);
+      
+@@ -57,3 +57,3 @@
+      
+-	wid2 = gnome_db_login_new (NULL,NULL,NULL);
++	wid2 = gnome_db_login_new (NULL);
+           
