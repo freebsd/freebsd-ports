23024b73f18e48e87cf1fbb84d8309b69cae61bb
keith <keith@FreeBSD.org>
2001-06-22 14:26:17 +0000

    Change msdosfs directory in -current to /sys/fs/msdosfs and /sys/modules/msdosfs.
    Bump PORTREVISION.

--- chinese/big5fs/files/big5fs.sh
+++ chinese/big5fs/files/big5fs.sh
@@ -2,23 +2,29 @@
 
 case "$1" in
 start)
-	if [ -x @@PREFIX@@/modules/big5cd9660.ko ]; then
+	if [ -x @@PREFIX@@/modules/cd9660.ko ]; then
 		echo -n ' Big5-cd9660'
-		kldload /usr/local/modules/big5cd9660.ko
+		kldload /usr/local/modules/cd9660.ko
 	fi
-	if [ -x @@PREFIX@@/modules/big5msdos.ko ]; then
+	if [ -x @@PREFIX@@/modules/msdos.ko ]; then
 		echo -n ' Big5-msdos'
-		kldload /usr/local/modules/big5msdos.ko
+		kldload /usr/local/modules/msdos.ko
 	fi
-	if [ -x @@PREFIX@@/modules/big5ntfs.ko ]; then
+	# msdosfs.ko instead of msdos.ko after FreeBSD 5.0-CURRENT
+	if [ -x @@PREFIX@@/modules/msdosfs.ko ]; then
+		echo -n ' Big5-msdosfs'
+		kldload /usr/local/modules/msdosfs.ko
+	fi
+	if [ -x @@PREFIX@@/modules/ntfs.ko ]; then
 		echo -n ' Big5-ntfs'
-		kldload /usr/local/modules/big5ntfs.ko
+		kldload /usr/local/modules/ntfs.ko
 	fi
 	;;
 stop)
-	kldunload -v -n big5cd9660 && echo -n ' Big5-cd9660'
-	kldunload -v -n big5msdos && echo -n ' Big5-msdos'
-	kldunload -v -n big5ntfs && echo -n ' Big5-ntfs'
+	kldunload -v -n cd9660 && echo -n ' Big5-cd9660'
+	kldunload -v -n msdos && echo -n ' Big5-msdos'
+	kldunload -v -n msdosfs && echo -n ' Big5-msdosfs'
+	kldunload -v -n ntfs && echo -n ' Big5-ntfs'
 	;;
 *)
 	echo "Usage: big5fs.sh {start|stop}" >&2
