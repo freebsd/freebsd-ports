8c410f8959c541f12ee77adbf6f2c4e8f5232b42
Peter Pentchev <roam@FreeBSD.org>
2001-06-27 13:34:06 +0000

    Add ncftpd, a well-known commercial FTP server with a restrictive 30-day evaluation license.
    PR:		28270
    Submitted by:	Justin Stanford <jus@security.za.net>

---
+++ ftp/ncftpd/scripts/ncftpd.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
+    echo "$0: Cannot determine the PREFIX" >&2
+    exit 1
+fi
+
+args="${PREFIX}/etc/ncftpd/general.cf ${PREFIX}/etc/ncftpd/domain.cf"
+
+case "$1" in
+start)
+        if [ -x ${PREFIX}/sbin/ncftpd ]; then
+		echo -n ' ncftpd'
+		${PREFIX}/sbin/ncftpd -d $args
+	fi
+        ;;
+stop)
+        killall ncftpd && echo -n ' ncftpd'
+        ;;
+*)
+        echo "Usage: `basename $0` {start|stop}" >&2
+        exit 64
+        ;;
+esac
+
+exit 0
