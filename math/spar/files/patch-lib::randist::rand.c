778cff84713115855e143329d1c4f574ed7a4e09
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-06-30 19:00:36 +0000

    upgrade to 0.5.10 drop maintainership

---
+++ math/spar/files/patch-lib::randist::rand.c
@@ -0,0 +1,10 @@
+--- lib/randist/rand.c.orig	Sun Jul  1 02:45:44 2001
++++ lib/randist/rand.c	Sun Jul  1 02:46:03 2001
+@@ -64,6 +64,7 @@
+  *  struct timeb to be defined.  They can be
+  *  found in timeb.h.
+  */ 
++#include <sys/types.h>
+ #include <sys/timeb.h>
+ void 
+ sl_randomize (void) 
