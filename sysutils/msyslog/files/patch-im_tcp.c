4c631e3d3f05c534152989863ad68a125b4de1b2
kevlo <kevlo@FreeBSD.org>
2001-07-01 15:36:45 +0000

    Update to version 1.05
    PR: 28559
    Submitted by: MAINTAINER

--- sysutils/msyslog/files/patch-im_tcp.c
+++ sysutils/msyslog/files/patch-im_tcp.c
@@ -1,14 +1,22 @@
---- src/modules/im_tcp.c.orig	Thu Apr 26 00:43:50 2001
-+++ src/modules/im_tcp.c	Sun May 13 00:49:45 2001
-@@ -72,11 +72,6 @@
+--- src/modules/im_tcp.c.orig	Thu Jun 14 07:30:35 2001
++++ src/modules/im_tcp.c	Sun Jul  1 03:34:58 2001
+@@ -61,6 +61,7 @@
+ #include <sys/types.h>
+ #include <sys/socket.h>
+ #include <sys/uio.h>
++#include <sys/param.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <errno.h>
+@@ -71,11 +72,6 @@
+ 
  #include "../modules.h"
  #include "../syslogd.h"
- 
+-
 -/* recvfrom() and others like socklen_t, Irix doesn't provide it */   
 -#ifndef HAVE_SOCKLEN_T
 -  typedef int socklen_t;
 -#endif
--
+ 
  struct tcp_conn {
  	struct tcp_conn *next;
- 	int		 fd;
