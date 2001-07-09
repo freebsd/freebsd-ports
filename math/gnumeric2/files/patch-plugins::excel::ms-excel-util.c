c728c60608f17c34c4a8946977b2473fc89fbc2a
Maxim Sobolev <sobomax@FreeBSD.org>
2001-07-09 16:46:59 +0000

    Update to 0.67.

--- math/gnumeric2/files/patch-plugins::excel::ms-excel-util.c
+++
@@ -1,7 +0,0 @@
---- plugins/excel/ms-excel-util.c.orig	Mon May  7 18:58:07 2001
-+++ plugins/excel/ms-excel-util.c	Tue May 22 17:03:18 2001
-@@ -383,3 +383,3 @@
- 	charset = nl_langinfo (CODESET);
--#elif
-+#else
- 	{
