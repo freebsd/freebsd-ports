3b52a48f9e39d1bfb103913a0854601985d029c8
Sheldon Hearn <sheldonh@FreeBSD.org>
2001-07-11 11:04:01 +0000

    Fix the IPv6 bind() call properly.  The author didn't spot the flaw in my patch when I initially submitted it.  This is his fully tested version.

--- mail/exim-old/files/patch-src::daemon.c
+++ mail/exim-old/files/patch-src::daemon.c
@@ -1,12 +1,17 @@
 --- src/daemon.c.orig	Tue Jul 10 11:52:32 2001
 +++ src/daemon.c	Tue Jul 10 11:53:33 2001
-@@ -841,7 +841,8 @@
+@@ -841,7 +841,13 @@
  
      for (i = 9; i >= 0; i--)
        {
 -      if (bind(listen_sockets[sk], (struct sockaddr *)&sin, sizeof(sin)) < 0)
 +      if (bind(listen_sockets[sk], (struct sockaddr *)&sin,
-+         (ipv6_address) ? sizeof(sin.v6) : sizeof(sin.v4)) < 0)
++               #ifdef HAVE_IPV6
++               ipv6_socket? sizeof(sin.v6) : sizeof(sin.v4)
++               #else
++               sizeof(sin)
++               #endif
++           ) < 0)
          {
          char *msg = strerror(errno);
          char *addr = (ipa->address[0] == 0)? "(any)" : ipa->address;
