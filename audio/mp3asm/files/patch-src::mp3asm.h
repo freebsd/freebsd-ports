51d90687cb2391d008eba361a277c096e86de74b
ijliao <ijliao@FreeBSD.org>
2001-07-20 03:28:15 +0000

    add mp3asm MP3 frame level editor

---
+++ audio/mp3asm/files/patch-src::mp3asm.h
@@ -0,0 +1,10 @@
+--- src/mp3asm.h.orig	Fri Jul 20 11:17:05 2001
++++ src/mp3asm.h	Fri Jul 20 11:17:58 2001
+@@ -30,7 +30,6 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <sysexits.h>
+-#include <argz.h>
+ #include <ctype.h>
+ 
+ #define LOGBUFSIZE 4096
