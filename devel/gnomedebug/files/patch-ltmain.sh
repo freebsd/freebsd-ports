8b2f1c4680a207d327fab50cfb97109487bf266a
sobomax <sobomax@FreeBSD.org>
2001-07-24 12:15:21 +0000

    Add gnomedebug 0.1.6, a GNOME Debugging Framework (GDF).

---
+++ devel/gnomedebug/files/patch-ltmain.sh
@@ -0,0 +1,18 @@
+
+$FreeBSD$
+
+--- ltmain.sh	2001/07/23 09:45:58	1.1
++++ ltmain.sh	2001/07/23 09:46:28
+@@ -4175,10 +4175,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
