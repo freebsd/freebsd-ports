92237bf9447f50416c85e9eafc493ecd07ec501e
Peter Pentchev <roam@FreeBSD.org>
2001-08-06 20:40:48 +0000

    Fix an mtree problem on bento: the installation utility was changing the modes on several directories to 2755. Guess I should have noticed this at my first installworld after testing this port.  Bad me, no cookie.
    Submitted by:	bento

--- sysutils/safecat/files/patch-hier.c
+++ sysutils/safecat/files/patch-hier.c
@@ -1,10 +1,21 @@
---- hier.c	Tue May 22 20:26:57 2001
-+++ hier.c	Tue May 22 20:27:32 2001
-@@ -9,9 +9,6 @@
+--- hier.c	Mon Aug  6 23:27:18 2001
++++ hier.c	Mon Aug  6 23:27:45 2001
+@@ -2,16 +2,13 @@
  
-   d(auto_home,"man",-1,-1,02755);
-   d(auto_home,"man/man1",-1,-1,02755);
+ void hier()
+ {
+-  h(auto_home,-1,-1,02755);
+-  d(auto_home,"bin",-1,-1,02755);
++  h(auto_home,-1,-1,0755);
++  d(auto_home,"bin",-1,-1,0755);
+   c(auto_home,"bin","safecat",-1,-1,0755);
+   c(auto_home,"bin","maildir",-1,-1,0755);
+ 
+-  d(auto_home,"man",-1,-1,02755);
+-  d(auto_home,"man/man1",-1,-1,02755);
 -  d(auto_home,"man/cat1",-1,-1,02755);
++  d(auto_home,"man",-1,-1,0755);
++  d(auto_home,"man/man1",-1,-1,0755);
    c(auto_home,"man/man1","safecat.1",-1,-1,0644);
 -  c(auto_home,"man/cat1","safecat.0",-1,-1,0644);
    c(auto_home,"man/man1","maildir.1",-1,-1,0644);
