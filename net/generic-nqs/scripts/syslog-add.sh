bc5d3e0b7b439f8d81d643141549fce77a1c8764
wjv <wjv@FreeBSD.org>
2001-08-06 10:10:25 +0000

    - Change MASTER_SITES - Modify installation script to be more discreet - Bump PORTREVISION - Update maintainer's email address
    PR:		29204
    Submitted by:	Tony Maher <tonym@biolateral.com.au> (MAINTAINER)

--- net/generic-nqs/scripts/syslog-add.sh
+++ net/generic-nqs/scripts/syslog-add.sh
@@ -5,8 +5,8 @@ PATH="/usr/bin:/bin"; export PATH
 if grep -q nqs /etc/syslog.conf
 then :
 else
-	echo "local0.*					/var/log/nqs" >> /etc/syslog.conf  
-	echo ""
-	echo "/etc/syslog.conf modified to support nqs (using local0.*)"
-	echo ""
+	echo 'You need to add a line as follows to /etc/syslog.conf'
+	echo ''
+	echo 'local0.*					/var/log/nqs'
+	echo ''
 fi
