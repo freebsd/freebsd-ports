59ec816477783522cd1b717eaca9ffc810a9bd59
sobomax <sobomax@FreeBSD.org>
2001-08-06 11:12:21 +0000

    Fix a breakage caused  by the libgda upgrade to 0.2.90.

--- devel/libglade2/files/patch-glade::glade-gnomedb.c
+++ devel/libglade2/files/patch-glade::glade-gnomedb.c
@@ -1,12 +1,32 @@
---- glade/glade-gnomedb.c.orig	Mon Feb 12 19:37:54 2001
-+++ glade/glade-gnomedb.c	Tue Jun 19 03:01:45 2001
-@@ -46,3 +46,3 @@
+
+$FreeBSD$
+
+--- glade/glade-gnomedb.c.orig	Tue Feb 13 03:37:54 2001
++++ glade/glade-gnomedb.c	Mon Aug  6 12:02:09 2001
+@@ -44,7 +44,7 @@
+ {
+ 	GtkWidget *wid;
       
 -	wid = gnome_db_login_new (NULL, NULL, NULL);
 +	wid = gnome_db_login_new (NULL);
       
-@@ -57,3 +57,3 @@
+ 	return wid;
+ }
+@@ -55,7 +55,7 @@
+ 	GtkWidget *wid = NULL;
+ 	GtkWidget *wid2 = NULL;
       
 -	wid2 = gnome_db_login_new (NULL,NULL,NULL);
 +	wid2 = gnome_db_login_new (NULL);
            
+ 	wid = gnome_db_logindlg_new (GNOME_DB_LOGIN (wid2), NULL);
+      
+@@ -150,7 +150,7 @@
+ {
+ 	GtkWidget *wid;
+        
+-	wid = gnome_db_error_new (NULL);
++	wid = gnome_db_error_new ();
+      
+ 	return (wid);
+ }
