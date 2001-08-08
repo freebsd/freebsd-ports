376569e0bdd842410a7092e14cc7e441348bd33f
ijliao <ijliao@FreeBSD.org>
2001-08-08 10:31:54 +0000

    make it buildable for 1.5.9
    PR:		29539
    Submitted by:	MAINTAINER

--- chinese/tin/files/patch-page.c
+++ chinese/tin/files/patch-page.c
@@ -1,38 +1,40 @@
---- src/page.c.orig	Wed Feb 21 03:18:52 2001
-+++ src/page.c	Mon Apr 30 03:28:06 2001
-@@ -844,7 +844,7 @@
- 
- 	search_line = curr_line;	/* Reset search to start from top of display */
- 
--	buff = my_malloc(cCOLS+1);	/* Need to account for \n */
-+	buff = my_malloc(LEN+1);	/* Need to account for \n */
- 
- 	if (part == 0) {
- 		ClearScreen();
-@@ -867,7 +867,7 @@
- 		curr = &artline[curr_line+i];
- 		fseek (note_fp, curr->offset, SEEK_SET);
- 
--		fgets (buff, cCOLS+1, note_fp);
-+		fgets (buff, LEN+1, note_fp);
+--- src/page.c.orig	Mon Jul 23 18:14:53 2001
++++ src/page.c	Wed Aug  8 18:24:04 2001
+@@ -838,8 +838,8 @@
+ 		fseek (file, curr->offset, SEEK_SET);
+ 		if ((line = tin_fgets (file, FALSE)) == NULL)
+ 			break;	/* ran out of message */
+-		if ((int) strlen(line) >= cCOLS)
+-			line[cCOLS] = '\0';
++		if ((int) strlen(line) >= LEN)
++			line[LEN] = '\0';
  
  		/*
  		 * rotN encoding on body and sig data only
-@@ -886,7 +886,7 @@
- 		strip_line(buff);
+@@ -858,7 +858,7 @@
+ 		strip_line(line);
  
  #ifndef USE_CURSES
--		snprintf (screen[i+PAGE_HEADER].col, cCOLS, "%s" cCRLF, buff);
-+		snprintf (screen[i+PAGE_HEADER].col, LEN, "%s" cCRLF, buff);
+-		snprintf (screen[i + scroll_region_top].col, cCOLS, "%s" cCRLF, line);
++		snprintf (screen[i + scroll_region_top].col, LEN, "%s" cCRLF, line);
  #endif /* !USE_CURSES */
  
- 		MoveCursor (i+PAGE_HEADER, 0);
-@@ -1346,7 +1346,7 @@
+ 		MoveCursor (i + scroll_region_top, 0);
+@@ -918,7 +918,7 @@
+ 
+ 	search_line = curr_line;	/* Reset search to start from top of display */
+ 
+-	buff = my_malloc(cCOLS + 1);	/* Need to account for \n */
++	buff = my_malloc(LEN + 1);	/* Need to account for \n */
+ 
+ 	if (part == 0) {
+ 		ClearScreen();
+@@ -1364,7 +1364,7 @@
  					chunk += 50;
- 					pgart.rawl = my_realloc((char *)pgart.rawl, sizeof(t_lineinfo) * chunk);
+ 					pgart.rawl = my_realloc((char *) pgart.rawl, sizeof(t_lineinfo) * chunk);
  				}
--			} while ((fgets(buff, cCOLS+1, pgart.raw)) != NULL);
-+			} while ((fgets(buff, LEN+1, pgart.raw)) != NULL);
+-			} while ((fgets(buff, cCOLS + 1, pgart.raw)) != NULL);
++			} while ((fgets(buff, LEN + 1, pgart.raw)) != NULL);
  
  			j--;
- 			pgart.rawl = my_realloc((char *)pgart.rawl, sizeof(t_lineinfo) * j);
+ 			pgart.rawl = my_realloc((char *) pgart.rawl, sizeof(t_lineinfo) * j);
