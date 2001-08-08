adbdc766081e0b16234a6d9608a97b6fc698811c
lioux <lioux@FreeBSD.org>
2001-08-08 06:52:42 +0000

    New port reed version 4.6: A text pager with autoscrolling and more
    PR:		29533
    Submitted by:	Pete Fritchman <petef@databits.net>

---
+++ misc/reed/files/patch-src::reed.h
@@ -0,0 +1,10 @@
+--- src/reed.h.orig	Thu Nov 30 19:09:49 2000
++++ src/reed.h	Tue Aug  7 23:14:10 2001
+@@ -19,6 +19,7 @@
+ #include <string.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
++#include <sys/syslimits.h>
+ #include <sys/dir.h>
+ #include <regex.h>
+ #include <pwd.h>
