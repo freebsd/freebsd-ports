6bb0d1576042ccd41c95fce02a2d1d1270a9cd14
tg <tg@FreeBSD.org>
2001-08-09 11:06:25 +0000

    Upgrade to 0.5.0.

---
+++ mail/gnumail/files/patch-..:Headers:MailboxManagerController.h
@@ -0,0 +1,10 @@
+--- ../Headers/MailboxManagerController.h.orig	Fri Jul 20 17:55:43 2001
++++ ../Headers/MailboxManagerController.h	Wed Aug  8 18:05:15 2001
+@@ -21,6 +21,7 @@
+ //  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ 
+ #import <AppKit/AppKit.h>
++#import <AppKit/NSWindowController.h>
+ 
+ @class MailboxManager;
+ 
