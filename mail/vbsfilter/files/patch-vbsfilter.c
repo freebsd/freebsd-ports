0e95ba3bb05933ac22f59441efb647d5493356b2
dinoex <dinoex@FreeBSD.org>
2001-08-13 19:20:39 +0000

    - Update to version 1.7
     * August 6 2001 -- vbsfilter-1.7
     * Various fixes for the case where headers and/or body are empty.
     * Patch sent in by Sergiy Zhuk <serge@yahoo-inc.com>
     *
     * Update "dangerous extension" list.
     * Noted by David F. Russell <David.F.Russell@ncmail.net>
     *
     * July 25 2001 -- vbsfilter-1.6
     * Also recognise 'name=blah' as well as the more usual 'name="blah"'.
     * Patch sent in by Andrea Adams <andrea@vividimage.com>
     *
     * July 9 2001 -- vbsfilter-1.5
     * Add support to reject arbitrary extensions.
     *
     * March 31 2001 -- vbsfilter-1.4
     * Update for sendmail-8.12
     *

--- mail/vbsfilter/files/patch-vbsfilter.c
+++ mail/vbsfilter/files/patch-vbsfilter.c
@@ -1,6 +1,6 @@
---- vbsfilter-1.3.c.neu.orig	Wed Feb 14 10:45:06 2001
-+++ vbsfilter-1.3.c	Wed Feb 14 12:40:38 2001
-@@ -2,6 +2,10 @@
+--- vbsfilter-1.7.c.orig	Mon Aug 13 21:11:36 2001
++++ vbsfilter-1.7.c	Mon Aug 13 21:15:05 2001
+@@ -9,6 +9,10 @@
  #include <stdio.h>
  #include <string.h>
  #include <sysexits.h>
@@ -11,20 +11,3 @@
  #include "libmilter/mfapi.h"
  
  /*
-@@ -172,7 +176,7 @@
- sfsistat mlfi_body(SMFICTX *ctx, u_char *bodyp, size_t bodylen) {
- 	struct mlfiPriv *priv = MLFIPRIV;
- 	
--	priv->body = (char *) realloc(priv->body, priv->len + bodylen);
-+	priv->body = (char *) realloc(priv->body, priv->len + bodylen + 1);
- 
- 	if (!priv->body) {
- 		/* can't accept this message right now */
-@@ -181,6 +185,7 @@
- 
- 	memcpy((u_char*)(priv->body+priv->len), bodyp, bodylen);
- 	priv->len += bodylen;
-+	priv->body[priv->len] = 0;
- 
- 	/* continue processing */
- 	return SMFIS_CONTINUE;
