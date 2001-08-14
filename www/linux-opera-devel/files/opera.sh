b8aa76a2df1e681b5d5ce1eaafd5972e2e1985f9
Dag-Erling Smørgrav <des@FreeBSD.org>
2001-08-14 14:14:28 +0000

    Upgrade to 5.05 TP1.
    Note that plugins won't work unless you happen to have a Linux version of
    Motif (or OpenMotif) installed; there is no linux-openmotif port (yet) so
    you'll have to figure this one out on your own.  Once you do, though, both
    www/linux-flashplugin and java/linux-jdk13 should work with no further
    tweaks (as should any other Netscape-compatible plugin that installs into
    ${PREFIX}/lib/netscape-linux/plugins).  Happy browsing!

--- www/linux-opera-devel/files/opera.sh
+++ www/linux-opera-devel/files/opera.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-export OPERA_DIR=%%PREFIX%%/share/opera
-exec %%PREFIX%%/bin/opera-bin "$@"
+OPERA_DIR="%%PREFIX%%/share/opera"
+OPERA_PLUGIN_PATH="%%PREFIX%%/share/opera/plugins"
+for plugin_dir in \
+    %%PREFIX%%/lib/netscape-linux/plugins \
+    %%PREFIX%%/linux-jdk1.3.1/jre/plugin/i386/ns4 \
+    ;
+do
+    if [ -d "${plugin_dir}" ] ; then
+        OPERA_PLUGIN_PATH="${OPERA_PLUGIN_PATH}:${plugin_dir}"
+    fi
+done
+export OPERA_DIR OPERA_PLUGIN_PATH
+exec %%DATADIR%%/bin/opera-static "$@"
