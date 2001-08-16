44e5d4aa86d4650a6d66b67d34233c9a99ae9bd1
Maxim Sobolev <sobomax@FreeBSD.org>
2001-08-16 13:13:50 +0000

    Update to 0.9.2.

--- editors/AbiWord2/files/patch-..::libiconv::include::iconv.h
+++ editors/AbiWord2/files/patch-..::libiconv::include::iconv.h
@@ -1,9 +1,7 @@
 
 $FreeBSD$
 
---- ../libiconv/include/iconv.h.orig	Tue Feb  1 10:35:10 2000
-+++ ../libiconv/include/iconv.h	Wed Aug  8 16:17:46 2001
-@@ -1,2 +1 @@
--#define LIBICONV_PLUG
--#include "libiconv.h"
-+#include "giconv.h"
+--- ../libiconv/include/iconv.h.orig	Thu Aug 16 11:14:18 2001
++++ ../libiconv/include/iconv.h	Thu Aug 16 11:28:42 2001
+@@ -0,0 +1 @@
++#include <giconv.h>
