c80f479ecc46237f8d6b3f9bfa9535951e6ff809
kiri <kiri@FreeBSD.org>
2001-08-17 12:10:05 +0000

    Update 1.94.1 -> 1.94.2. Use bsd.emacs.mk.

--- mail/mew/files/message.sh
+++ mail/mew/files/message.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  
-if [ -z "${SITE_STARTUP_FILE}" ]; then
-    ${CAT} <<EOF > ${WRKTMPDIR}/MESSAGE
+if [ -z "${EMACS_SETUPEL}" ]; then
+    cat <<EOF > ${WRKTMPDIR}/pkg-message
 *************************************************************************
 * You must set at least following elisps to your own dot.emacs file:    *
 *                                                                       *
@@ -12,11 +12,11 @@ if [ -z "${SITE_STARTUP_FILE}" ]; then
 *************************************************************************
 EOF
 else
-    ${CAT} <<EOF > ${WRKTMPDIR}/MESSAGE
+    cat <<EOF > ${WRKTMPDIR}/pkg-message
 *************************************************************************
 * You must set at least following elisps to your own dot.emacs file:    *
 *                                                                       *
-*       (require 'mew-${EMACS_PORT_NAME}-startup)                                  *
+*       (require '`basename ${EMACS_SETUPEL} .el`)                                  *
 *       (autoload 'mew "mew" nil t)                                     *
 *       (autoload 'mew-send "mew" nil t)                                *
 *       (setq mew-mail-domain-list '("Name_Server_Domain_Name"))        *
