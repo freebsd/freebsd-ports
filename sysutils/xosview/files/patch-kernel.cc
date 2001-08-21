ed01dc74c0f7b76a4fcb510a41f22fd168e5ecdf
obrien <obrien@FreeBSD.org>
2001-08-21 19:32:41 +0000

    Upgrade to 1.7.4.

--- sysutils/xosview/files/patch-kernel.cc
+++ sysutils/xosview/files/patch-kernel.cc
@@ -1,6 +1,14 @@
---- bsd/kernel.cc.orig	Mon Jul  5 21:09:30 1999
-+++ bsd/kernel.cc	Tue Aug 21 10:49:26 2001
-@@ -284,7 +284,12 @@
+--- bsd/kernel.cc.orig	Mon Jul 17 11:52:31 2000
++++ bsd/kernel.cc	Tue Aug 21 12:23:41 2001
+@@ -46,6 +46,7 @@
+ #endif
+ 
+ #ifdef HAVE_DEVSTAT
++#include <sys/dkstat.h>
+ #include <devstat.h>
+ #include <stdlib.h>	/*  For malloc().  */
+ void DevStat_Init();
+@@ -297,7 +298,12 @@
    while (nlp && nlp->n_name) {
      if ((nlp->n_type == 0) || (nlp->n_value == 0))
        /*errx (-1, "kvm_nlist() lookup failed for symbol '%s'.", nlp->n_name);*/
@@ -13,7 +21,7 @@
      nlp++;
    }
  #ifdef HAVE_DEVSTAT
-@@ -457,7 +462,7 @@
+@@ -493,7 +499,7 @@
  //
  
  void
@@ -22,7 +30,7 @@
    int	totalinuse, totalsize;
    int rnswap, nswap = swapctl(SWAP_NSWAP, 0, 0);
    struct swapent *swapiter;
-@@ -792,25 +797,25 @@
+@@ -828,25 +834,25 @@
    OpenKDIfNeeded(); 
    nintr = (nlst[EINTRCNT_SYM_INDEX].n_value -
  	   nlst[INTRCNT_SYM_INDEX].n_value)   / sizeof(int);
@@ -55,18 +63,14 @@
      /* FreeBSD has an array of interrupt counts, indexed by device number.
         These are also indirected by IRQ num with intr_countp: */
      safe_kvm_read (nlst[INTRCOUNTP_SYM_INDEX].n_value,
-@@ -839,10 +844,10 @@
+@@ -875,8 +881,8 @@
        intrCount[i] = kvm_intrptrs[i];
  #endif /* _BSDI_VERSION */
  
 -#else /* XOSVIEW_FREEBSD */
 -  //  NetBSD/OpenBSD version, based on vmstat.c.  Note that the pc532
--  //  platform does support intrcnt and eintrcnt, but vmstat uses
--  //  the more advanced event counters to provide software
 +#else /* XOSVIEW_FREEBSD & i386 */
-+  //  NetBSD/OpenBSD & FreeBSD/Alpha version, based on vmstat.c.  
-+  //  Note that the pc532 platform does support intrcnt and eintrcnt, but
-+  //  vmstat uses the more advanced event counters to provide software
++  //  NetBSD/OpenBSD & FreeBSD/Alpha version, based on vmstat.c.  Note that the pc532 
+   //  platform does support intrcnt and eintrcnt, but vmstat uses
+   //  the more advanced event counters to provide software
    //  counts.  We'll just use the intrcnt array here.  If anyone
-   //  has problems, please mail me.  bgrayson
-   {
