4980edc0122e67e40fcc9701fe54f65f3fb8a41d
Maxim Sobolev <sobomax@FreeBSD.org>
2001-08-21 15:34:42 +0000

    Update to 0.10.0.

---
+++ news/pan/files/patch-pan::filters::filter-phrase.h
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- pan/filters/filter-phrase.h	2001/08/21 15:06:27	1.1
++++ pan/filters/filter-phrase.h	2001/08/21 15:06:43
+@@ -24,7 +24,7 @@
+ #ifndef __FILTER_PHRASE_H__
+ #define __FILTER_PHRASE_H__
+ 
+-#include <regex.h>
++#include <gnuregex.h>
+ 
+ #include <pan/filters/filter.h>
+ 
