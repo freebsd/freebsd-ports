f42144af5dc6ecf96d02d1cfbb0960f096a04690
alex <alex@FreeBSD.org>
2001-08-23 19:26:57 +0000

    - Depend on apache13 - install files to www/data.default and not deprecated apache/share/htdocs - PERL-patch files in post-patch, not in post-extract - Install to LOCALBASE, not hardcoded /usr/local              ^^^^^^^^ ARGH
    Apache-Dependency reported by:	bento

---
+++ ftp/ftplocate/files/patch-install.pl
@@ -0,0 +1,18 @@
+--- install.pl.orig	Thu Aug 23 21:12:47 2001
++++ install.pl	Thu Aug 23 21:17:01 2001
+@@ -251,6 +251,7 @@
+ 
+ $defhtmldir="/usr/local/apache/share/htdocs";
+ foreach (
++	"/usr/local/www/data.default",
+ 	"/usr/local/apache/share/htdocs",
+ 	"/usr/local/apache/htdocs",
+ 	"/opt/apache/share/htdocs", 
+@@ -261,6 +262,7 @@
+ 
+ $defcgidir="/usr/local/apache/share/cgi-bin";
+ foreach (
++	"/usr/local/www/cgi-bin.default",
+ 	"/usr/local/apache/share/cgi-bin",
+ 	"/usr/local/apache/cgi-bin",
+ 	"/opt/apache/share/cgi-bin", 
