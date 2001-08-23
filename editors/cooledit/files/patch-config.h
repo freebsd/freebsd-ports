fb5f04ef295b0221af3b932af5866c3dc69a478a
Alexander Langer <alex@FreeBSD.org>
2001-08-23 18:34:56 +0000

    Fix linker errors by undefining HAVE_WCHAR_H (ours seems incomplete) and defining DO_NOT_USE_VGALIB manually in config.h. Works great now.
    Reported by:	bento

---
+++ editors/cooledit/files/patch-config.h
@@ -0,0 +1,11 @@
+--- config.h.in.old	Thu Aug 23 19:43:12 2001
++++ config.h.in	Thu Aug 23 19:43:44 2001
+@@ -420,7 +420,7 @@
+ #undef HAVE_UNISTD_H
+ 
+ /* Define if you have the <wchar.h> header file.  */
+-#undef HAVE_WCHAR_H
++/* #undef HAVE_WCHAR_H */
+ 
+ /* Define if you have the i library (-li).  */
+ #undef HAVE_LIBI
