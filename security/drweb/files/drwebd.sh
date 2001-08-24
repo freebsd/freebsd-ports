b44f2d6a94fcd0acf9efe06318e00ae69ce6add9
ache <ache@FreeBSD.org>
2001-08-24 15:42:40 +0000

    DrWeb antivirus, FreeBSD binary

---
+++ security/drweb/files/drwebd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# $FreeBSD$
+
+drwebd='!!PREFIX!!/drwebd'
+
+case "$1" in
+start)
+	if [ -x $drwebd ]; then
+	    echo -n ' drwebd'
+	    $drwebd
+	fi
+	;;
+stop)
+	/usr/bin/killall drwebd
+	echo -n ' drwebd'
+	;;
+*)
+	echo "Usage: `basename $0` {start|stop}" >&2
+	exit 64
+	;;
+esac
+
+exit 0
