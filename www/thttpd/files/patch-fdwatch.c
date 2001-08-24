c24eedb261bbbdf87b2b1bd1c58ae22f0fd03137
Pete Fritchman <petef@FreeBSD.org>
2001-08-24 00:19:33 +0000

    Fix kqueue support for -current
    PR:		30009
    Submitted by:	maintainer
    Reviewed by:	ade

--- www/thttpd/files/patch-fdwatch.c
+++ www/thttpd/files/patch-fdwatch.c
@@ -1,21 +1,12 @@
---- fdwatch.c.orig	Tue Apr 24 18:40:22 2001
-+++ fdwatch.c	Tue Apr 24 19:00:57 2001
-@@ -32,6 +32,7 @@
- #include <sys/time.h>
- #include <sys/resource.h>
- #include <syslog.h>
-+#include <sys/param.h>
+--- fdwatch.c.orig	Fri Aug 24 00:19:39 2001
++++ fdwatch.c	Fri Aug 24 00:20:30 2001
+@@ -311,6 +311,9 @@
+     if ( kqchanges == (struct kevent*) 0 || kqevents == (struct kevent*) 0 ||
+ 	 kqrfdidx == (int*) 0 )
+ 	return -1;
++    memset(kqchanges, '\0', sizeof(struct kevent) * 2 * nfiles);
++    memset(kqevents, '\0', sizeof(struct kevent) * nfiles);
++    memset(kqrfdidx, '\0', sizeof(int) * nfiles);
+     return 0;
+     }
  
- #ifndef MIN
- #define MIN(a,b) ((a) < (b) ? (a) : (b))
-@@ -44,6 +45,10 @@
- #include <sys/poll.h>
- #endif /* HAVE_SYS_POLL_H */
- #endif /* HAVE_POLL_H */
-+
-+#ifdef HAVE_FREEBSD_CURRENT
-+#undef HAVE_KQUEUE
-+#endif
- 
- #ifdef HAVE_SYS_EVENT_H
- #include <sys/event.h>
