3aedf84118d4254e7dfba69bf5fa44b740e274f0
Kevin Lo <kevlo@FreeBSD.org>
2001-08-27 15:26:07 +0000

    - Install the psm* commands correctly, they used to be the same binary but   aren't with the current version. Sigh. - Install docs only if NOPORTDOCS is not set. - Fix some warnings, remove -pedantic and -Wall to silence the rest. - Bump PORTREVISION
    PR: 30098
    Submitted by: MAINTAINER

---
+++ graphics/hppsmtools/files/patch-psmdir.c
@@ -0,0 +1,10 @@
+--- psmdir.c.orig	Sun Aug 26 12:29:38 2001
++++ psmdir.c	Sun Aug 26 12:29:44 2001
+@@ -3,7 +3,6 @@
+ #include <unistd.h>
+ #include <signal.h>
+ #include <string.h>
+-#include <malloc.h>
+ #include <math.h>
+ #include "psm.h"
+ #include "config.h"
