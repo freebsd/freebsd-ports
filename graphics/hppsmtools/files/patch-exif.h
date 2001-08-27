f5fc119faafb7e10637ad3120a007828ee740a23
kevlo <kevlo@FreeBSD.org>
2001-08-27 15:26:07 +0000

    - Install the psm* commands correctly, they used to be the same binary but   aren't with the current version. Sigh. - Install docs only if NOPORTDOCS is not set. - Fix some warnings, remove -pedantic and -Wall to silence the rest. - Bump PORTREVISION
    PR: 30098
    Submitted by: MAINTAINER

---
+++ graphics/hppsmtools/files/patch-exif.h
@@ -0,0 +1,11 @@
+--- exif.h.orig	Sun Aug 26 12:32:35 2001
++++ exif.h	Sun Aug 26 12:32:44 2001
+@@ -94,8 +94,6 @@
+ 
+ #define EXIF_INVALID_RATIONAL 1e40
+ 
+-typedef unsigned char uint8_t;
+-
+ /* The next two types are actually private */
+ typedef struct
+ {	int16_t tag;
---
+++ graphics/hppsmtools/files/patch-psmdel.c
@@ -0,0 +1,10 @@
+--- psmdel.c.orig	Sun Aug 26 12:29:07 2001
++++ psmdel.c	Sun Aug 26 12:29:11 2001
+@@ -3,7 +3,6 @@
+ #include <unistd.h>
+ #include <signal.h>
+ #include <string.h>
+-#include <malloc.h>
+ #include "psm.h"
+ #include "config.h"
+ 
