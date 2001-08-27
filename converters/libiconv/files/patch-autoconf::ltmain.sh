c76df34348bb582162f158b6cbe750b1b131c57c
sobomax <sobomax@FreeBSD.org>
2001-08-27 09:39:53 +0000

    - Remove useless USE_LIBTOOL; - properly name shared libraries (get rid of bogus .so.X.Y convention); - add files missed from pkg-plist (documentation); - don't waste time building/running tests included into distribution.

---
+++ converters/libiconv/files/patch-autoconf::ltmain.sh
@@ -0,0 +1,18 @@
+
+$FreeBSD$
+
+--- autoconf/ltmain.sh	2001/08/27 09:19:40	1.1
++++ autoconf/ltmain.sh	2001/08/27 09:20:41
+@@ -4175,10 +4175,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
