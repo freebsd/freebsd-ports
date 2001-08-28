bf8c25233c270311ba422f93df9bd74aa4ee61bb
Maxim Sobolev <sobomax@FreeBSD.org>
2001-08-28 12:04:42 +0000

    - Rework optional GNOME dependency, so it would not confuse bento; - enable gnomeprint support in Gnome case; - use "@*" to pass arguments from the helper script to the binary instead of   $*.
    Submitted by:	bento

--- graphics/dia/files/dia.sh
+++ graphics/dia/files/dia.sh
@@ -4,4 +4,4 @@ DIA_SHAPE_PATH=%%PREFIX%%/share/gnome/dia/shapes
 DIA_SHEET_PATH=%%PREFIX%%/share/gnome/dia/sheets
 export DIA_LIB_PATH DIA_SHAPE_PATH DIA_INT_SHAPE_PATH DIA_SHEET_PATH
 
-exec %%PREFIX%%/bin/dia-bin $*
+exec %%PREFIX%%/bin/dia-bin "$@"
