574bcefe7fd05961ba971c0a0e594d40a2418920
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2001-08-29 16:24:21 +0000

    New port xmms-crystality version 0.92: Realtime XMMS plugin for remastering mp3 sound
    PR:		29839
    Submitted by:	Patrick Li <pat@databits.net>

---
+++ audio/xmms-crystality/files/patch-crystality.c
@@ -0,0 +1,11 @@
+--- crystality.c.orig	Fri Aug 17 22:13:00 2001
++++ crystality.c	Fri Aug 17 22:13:17 2001
+@@ -20,7 +20,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <math.h>
+-#include <linux/soundcard.h>
++#include <machine/soundcard.h>
+ 
+ #include "crystality.h"
+ 
---
+++ audio/xmms-crystality/files/patch-crystality.h
@@ -0,0 +1,11 @@
+--- crystality.h.orig	Fri Aug 17 22:13:07 2001
++++ crystality.h	Fri Aug 17 22:13:32 2001
+@@ -17,7 +17,7 @@
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  */
+ 
+-#include <linux/soundcard.h>
++#include <machine/soundcard.h>
+ 
+ #define int16 short
+ #define int32 long
