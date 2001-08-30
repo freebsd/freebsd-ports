4fb39439ef6e860eef5976d45a3ec377a89ada5d
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2001-08-30 14:09:10 +0000

    o update to 2.4.94 o remove unnecessary patches o PATCH_* and LIB_DEPENDS (iconv) added o INSTALLS_SHLIB and LDCONFIG_DIRS added o scripts/pre-configure changes: 	-e added 	PREFIX -> LOCALBASE for external programs 	new iconv support added (untested) 	config.sh generation fixed
    PR:		29868
    Submitted by:	MAINTAINER

--- mail/elm+ME/files/patch-newmbox.c
+++ mail/elm+ME/files/patch-newmbox.c
@@ -1,11 +1,10 @@
---- src/newmbox.c.orig	Tue Feb  6 19:48:32 2001
-+++ src/newmbox.c	Sat May 12 21:28:05 2001
-@@ -568,6 +568,33 @@
- 		}	       
+--- src/newmbox.c.orig	Sun Aug 19 07:24:27 2001
++++ src/newmbox.c	Sun Aug 19 07:25:50 2001
+@@ -585,6 +585,36 @@
  	    }
  
-+	    else if (NULL != (tmphdr = locate_header_by_name(parsed_headers,
-+							"Newsgroups"))) {	    
+ 	    if (NULL != (tmphdr = locate_header_by_name(parsed_headers,
++							"Newsgroups"))) {
 +		struct addr_item * addrs = 
 +		    break_down_address(tmphdr->body,
 +				       !(current_header -> status & 
@@ -17,20 +16,24 @@
 +		    if (current_header->to)
 +			free_addr_items(current_header->to);
 +		    current_header->to = addrs;
-+		    if (debug > 15 && debugfile) {
++
++#if DEBUG
++		    if (Debug.active > 10) {
 +			struct addr_item * p;
 +			for (p =  current_header->to; p->addr; p++) {
-+			    elm_fprintf(debugfile,
-+					FRM("--   to[%d] addr='%s' comment='%S' fullname='%S'\n"),
-+					p - current_header->to,
-+					p->addr,
-+					p->comment,
-+					p->fullname);
++			    DPRINT(Debug,15,(&Debug, 
++					     "--   newsgroup[%d] addr='%s' comment='%S' fullname='%S'\n",
++					     p - current_header->to,
++					     p->addr,
++					     p->comment,
++					     p->fullname));
 +			}
 +		    }
++#endif
 +		}	       
 +	    }
 +
- 	    if (NULL != (tmphdr = locate_header_by_name(parsed_headers,
++	    if (NULL != (tmphdr = locate_header_by_name(parsed_headers,
  							"Cc"))) {	    
  		struct addr_item * addrs = 
+ 		    break_down_address(tmphdr->body,
