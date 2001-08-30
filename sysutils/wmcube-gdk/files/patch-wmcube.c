ed949dc01f72bd88915e78af427524f8814c281f
lioux <lioux@FreeBSD.org>
2001-08-30 04:24:25 +0000

    New port wmcube version 0.98: wmcube-gdk is a modified version of the dockapp wmCube
    PR:		29781
    Submitted by:	Patrick Li <pat@databits.net>

---
+++ sysutils/wmcube-gdk/files/patch-wmcube.c
@@ -0,0 +1,10 @@
+--- wmcube.c.orig	Thu Aug 16 13:04:38 2001
++++ wmcube.c	Thu Aug 16 13:05:00 2001
+@@ -38,7 +38,6 @@
+ #include <math.h>
+ 
+ /* forgotten includes */
+-#include <getopt.h>
+ #include <dirent.h>
+ 
+ #include <sys/wait.h>
