aeb76ac389ab58472d79d202a2e3eda19210274f
Trevor Johnson <trevor@FreeBSD.org>
2001-08-30 04:50:19 +0000

    new port of StarPlot 0.92.1, an astral viewer with projection from the third dimension
    Submitted by:	Patrick Li <pat@databits.net>
    PR:		29910
    
    	She went into a trance, and while she was in the trance,
    	she gave some latitude and longitude figures.  We focused
    	our satellite cameras on that point and the plane was there.
    
    	--President Jimmy Carter

---
+++ astro/starplot/files/patch-gui::filedialogs.cc
@@ -0,0 +1,11 @@
+--- gui/filedialogs.cc.orig	Mon Aug 20 13:14:42 2001
++++ gui/filedialogs.cc	Mon Aug 20 13:14:52 2001
+@@ -159,7 +159,7 @@
+   if (select_function == help_ok_sel || select_function == help_open_sel) {
+     if (!help_browser)
+       gtk_file_selection_set_filename (GTK_FILE_SELECTION(filew),
+-				       "/usr/bin/");
++				       "~/");
+     else
+       gtk_file_selection_set_filename (GTK_FILE_SELECTION(filew),
+ 				       help_browser);
