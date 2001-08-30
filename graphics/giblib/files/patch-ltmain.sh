57cba1be5a151b26c777232b7d4359bbcc46e530
Pete Fritchman <petef@FreeBSD.org>
2001-08-30 00:29:47 +0000

    Update to 1.2.1
    PR:		30204
    Submitted by:	maintainer

---
+++ graphics/giblib/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Sun Aug 12 17:08:05 2001
++++ ltmain.sh	Wed Aug 29 16:31:31 2001
+@@ -4673,10 +4674,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
