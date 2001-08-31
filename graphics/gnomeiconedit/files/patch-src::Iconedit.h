d4c05776565a37e8990d90542434f175339a956b
Maxim Sobolev <sobomax@FreeBSD.org>
2001-08-31 13:34:27 +0000

    Unbroke.
    Obtained from:	NetBSD (partially)

---
+++ graphics/gnomeiconedit/files/patch-src::Iconedit.h
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/Iconedit.h	2001/08/31 13:21:17	1.1
++++ src/Iconedit.h	2001/08/31 13:21:43
+@@ -13,7 +13,7 @@
+ #endif				/* __cplusplus */
+ 
+ /** typedefs **/
+-#include <liboaf/gnome-factory.h>
++#include <libgnorba/gnome-factory.h>
+ #if !defined(ORBIT_DECL_GNOME_Iconedit_Iconedit) && !defined(_GNOME_Iconedit_Iconedit_defined)
+ #define ORBIT_DECL_GNOME_Iconedit_Iconedit 1
+ #define _GNOME_Iconedit_Iconedit_defined 1
