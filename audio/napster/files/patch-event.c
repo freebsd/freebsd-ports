f439e178635726387908b8057acf56cc0f2029d1
petef <petef@FreeBSD.org>
2001-08-31 18:28:42 +0000

    - update to 1.4.6 - remove my email message/.sig which somehow got included in patch-event.c

--- audio/napster/files/patch-event.c
+++ audio/napster/files/patch-event.c
@@ -1,5 +1,3 @@
-Oops, forgot to include patch-event.c:
-
 --- event.c.orig	Tue Jul  3 00:46:38 2001
 +++ event.c	Tue Jul  3 00:47:24 2001
 @@ -11,6 +11,7 @@
@@ -10,8 +8,3 @@ Oops, forgot to include patch-event.c:
  #include <sys/socket.h>
  #include <stdlib.h>
  #include <ctype.h>
-
---
-Pete Fritchman <petef@databits.net>
-Databits Network Services, Inc. <http://databits.net>
-finger petef@databits.net for PGP key
