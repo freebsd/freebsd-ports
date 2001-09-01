5fa3173ffa62df371b926fd363e600a4f774867d
Adrian Chadd <adrian@FreeBSD.org>
2001-09-01 08:34:14 +0000

    Remove the RunCache stuff and let squid handle its own child maintainence. This now means that 'stop' as an argument works correctly, and doesn't end up having squid restarted.
    I've added some sleep code to wait for squid to complete its shutdown,
    but I have commented it out for the time being as I don't know whether
    its good practice to have your machine sleep during shutdown.
    I'll research it some more. :)

--- www/squid25/files/squid.sh
+++ www/squid25/files/squid.sh
@@ -7,11 +7,16 @@ fi
 
 case "$1" in
 start)
-	if [ -x ${PREFIX}/sbin/RunCache -a -f ${PREFIX}/etc/squid/squid.conf ]; then
-		(cd /tmp; ${PREFIX}/sbin/RunCache >/dev/null 2>&1 &) ; echo -n ' squid'
+	if [ -x ${PREFIX}/sbin/squid -a -f ${PREFIX}/etc/squid/squid.conf ]; then
+		(cd /${PREFIX}/squid/logs; ${PREFIX}/sbin/squid >/dev/null 2>&1 &) ; echo -n ' squid'
 	fi
 	;;
 stop)
+		${PREFIX}/sbin/squid -k shutdown 2>&1
+		# Uncomment this if you'd like the system to (attempt to
+		# wait for) squid to shut down cleanly
+		#echo "Sleeping for 45 seconds to allow squid to shutdown.."
+		#sleep 45 
 	;;
 *)
 	echo "Usage: `basename $0` {start|stop}" >&2
