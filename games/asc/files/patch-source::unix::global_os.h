8dad0acf77e88260f7d644ba117371a60d03c0c5
sobomax <sobomax@FreeBSD.org>
2001-09-03 15:16:55 +0000

    Update to 1.5.13.

--- games/asc/files/patch-source::unix::global_os.h
+++ games/asc/files/patch-source::unix::global_os.h
@@ -1,18 +1,20 @@
 
 $FreeBSD$
 
---- source/unix/global_os.h	2001/05/22 19:32:32	1.1
-+++ source/unix/global_os.h	2001/05/22 19:33:06
-@@ -43,7 +43,7 @@
+--- source/unix/global_os.h.orig	Mon Sep  3 17:26:45 2001
++++ source/unix/global_os.h	Mon Sep  3 17:32:50 2001
+@@ -53,8 +53,8 @@
   #define global_os_h_included
  
    #ifndef minimalIO
 -   #define sdlheader "SDL/SDL.h"
+-   #define sdlmixerheader "SDL/SDL_mixer.h"
 +   #define sdlheader "SDL.h"
++   #define sdlmixerheader "SDL_mixer.h"
     #include sdlheader
    #endif 
  
-@@ -60,7 +60,7 @@
+@@ -77,7 +77,7 @@
    #endif
  
    #define CASE_SENSITIVE_FILE_NAMES 1
