0c247757633bbccedc7c07f9ca850ae7f98a7cf0
wjv <wjv@FreeBSD.org>
2001-09-05 13:21:36 +0000

    - Downgrade to version 1.05 at the express request of the author.  The 1.1x   code tree is, in the author's opinion, not even ready for a -devel port. - Bumped PORTEPOCH because of the above. - Make SPOOLDIR a variable which may be altered by the user at install time.
    Submitted by:	Mark Tranchant <mark@tranchant.freeserve.co.uk>

--- ftp/ftpq/files/patch-src:ftpq.h
+++ ftp/ftpq/files/patch-src:ftpq.h
@@ -1,29 +1,11 @@
---- src/ftpq.h.orig	Fri Jul 27 16:56:05 2001
-+++ src/ftpq.h	Sat Jul 28 09:15:43 2001
-@@ -20,6 +20,9 @@
- 
- /* includes */
- #include "../config.h"
-+#ifdef HAVE_SYS_TYPES_H
-+	#include <sys/types.h>
-+#endif
- #ifdef HAVE_CTYPE_H
- 	#include <ctype.h>
- #endif
-@@ -56,15 +59,12 @@
- #ifdef HAVE_SYS_TIME_H
- 	#include <sys/time.h>
- #endif
--#ifdef HAVE_SYS_TYPES_H
--	#include <sys/types.h>
--#endif
- #ifdef HAVE_UNISTD_H
- 	#include <unistd.h>
- #endif
+--- src/ftpq.h.orig	Tue Sep  4 13:00:00 2001
++++ src/ftpq.h	Wed Sep  5 15:02:03 2001
+@@ -35,7 +35,7 @@
+ #include <pwd.h>
  
  /* useful defined constants */
 -#define SPOOLDIR    "/tmp/ftpq/"   /* must end in /. Check Makefile, too. */
-+#define SPOOLDIR    "/var/spool/ftpq/"   /* must end in /. Check Makefile, too. */
++#define SPOOLDIR    "%%SPOOLDIR%%/"/* must end in /. Check Makefile, too. */
  #define HOSTRESP    2048           /* ftp server response buffer, bytes */
  #define BUFSIZE     2048           /* general purpose global buffer */
  #define SMTP_PORT     25
