a6c6c07eb55ff0b1752b971fce7acb9936ba3fd1
lioux <lioux@FreeBSD.org>
2001-09-07 06:59:34 +0000

    o update to 0.6 o install additional documentation (support NOPORTDOCS) o add MASTER_SITE_SOURCEFORGE to MASTER_SITES o update DESCR since Englightement is supported
    PR:		30397
    Submitted by:	MAINTAINER

---
+++ misc/menushki/files/patch-konst.string.h
@@ -0,0 +1,10 @@
+--- ktools-1.1/include/konst.string.h.orig	Thu Sep  6 10:50:02 2001
++++ ktools-1.1/include/konst.string.h	Thu Sep  6 10:49:40 2001
+@@ -3,6 +3,7 @@
+ 
+ #include <string>
+ #include <vector>
++#include <time.h>
+ 
+ #include "konst.list.h"
+ #include "konst.conf.h"
