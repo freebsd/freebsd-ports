8d8939752a41658e5c1f48e3c36e7d0489a64653
Jimmy Olgeni <olgeni@FreeBSD.org>
2001-09-07 19:54:19 +0000

    Add devel/libast, "A library of assorted spiffy things, previously known as libmej, the Eterm helper library".
    This port is required by the Eterm 0.9.1 upgrade.
    
    Submitted by:	Jeremy Norris <ishmael27@home.com>

---
+++ devel/libast/files/patch-libast.h
@@ -0,0 +1,10 @@
+--- libast.h.orig	Sat Jun 16 02:30:28 2001
++++ libast.h	Fri Sep  7 01:01:50 2001
+@@ -40,6 +40,7 @@
+ # endif
+ #endif
+ 
++#include <limits.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
