04b3bf83f2d0d1e366e2e0470c6aed3144578308
nectar <nectar@FreeBSD.org>
2001-09-07 19:45:46 +0000

    Update 0.4d -> 0.4e

--- security/heimdal/files/kdc.sh
+++ security/heimdal/files/kdc.sh
@@ -1,28 +1,54 @@
 #!/bin/sh
 
-kdc=%%PREFIX%%/libexec/kdc
+# Installation prefix for Heimdal
+PREFIX="%%PREFIX%%"
+
+# One of `none', `master', or `slave' 
+KDC_ROLE=none
+KDC_FLAGS=""
+IPROP_MASTER_HOST=""
+
+KDC="${PREFIX}/libexec/kdc"
+KPASSWDD="${PREFIX}/libexec/kpasswdd"
+IPROPD_MASTER="${PREFIX}/libexec/ipropd-master"
+IPROPD_SLAVE="${PREFIX}/libexec/ipropd-slave"
+
+case "${KDC_ROLE}" in
+none)	run_kdc=0 run_master=0 run_slave=0 ;;
+master)	run_kdc=1 run_master=1 run_slave=0 ;;
+slave) 	run_kdc=1 run_master=0 run_slave=1 ;;
+*)	echo "KDC_ROLE is not set." >&2
+	exit 64
+	;;
+esac
+
+do_start() {
+    if test "${run_kdc}" -eq "1"; then
+	${KDC} ${KDC_FLAGS} &
+	test "${run_master}" -eq "1" && ${KPASSWDD} &
+	test "${run_master}" -eq "1" && ${IPROPD_MASTER} &
+	test "${run_slave}"  -eq "1" && ${IPROPD_SLAVE} ${IPROP_MASTER_HOST} &
+    fi
+    echo -n ' kdc'
+}
+
+do_stop() {
+    for pid in kdc kpasswdd ipropd-master ipropd-slave; do
+	test -f "/var/run/${pid}.pid" && kill `cat /var/run/${pid}.pid`
+    done
+    echo -n ' kdc'
+}
+
+do_restart() {
+    do_stop
+    sleep 1
+    do_start
+}
 
 case "$1" in
-start)
-    if [ -x $kdc ]; then
-        echo -n ' kdc'
-        $kdc  &
-    fi
-    ;;
-stop)
-    if [ -f /var/run/kdc.pid ]; then
-        kill -TERM `cat /var/run/kdc.pid`
-        rm -f /var/run/kdc.pid
-        echo -n ' kdc'
-    fi
-    ;;
-restart)
-    if [ -f /var/run/kdc.pid ]; then
-        $0 stop
-	sleep 2
-    fi
-    $0 start
-    ;;
+start)		do_start   ;;
+stop)		do_stop    ;;
+restart)	do_restart ;;
 *)
     echo "Usage: `basename $0` {start|stop}" >&2
     exit 64
