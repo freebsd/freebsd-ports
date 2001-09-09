7a171e7ff7528e2417897e9bc0b432fd67ce7510
kevlo <kevlo@FreeBSD.org>
2001-09-09 15:46:14 +0000

    - Bump PORTREVISION - With the roll-out of itcl/itk/iwidgets 3.2 a few changes to this   port should be done.
    PR: 29706
    Submitted by: MAINTAINER

--- net/smm++/files/patch-itk.sh
+++ net/smm++/files/patch-itk.sh
@@ -5,7 +5,7 @@
  
  # all possible itkwish executables
 -itkarray=(itkwish3.1 itkwish3 itkwish30 itkwish)
-+itkarray="itkwish3.1 itkwish3 itkwish30 itkwish3.0 itkwish"
++itkarray="itkwish3.1 itkwish3 itkwish30 itkwish3.0 itkwish wish8.3 wish8.2"
  
 -# $PATH exploded as an array
 -explode=(`echo $PATH | sed 's/:/ /g'`)
