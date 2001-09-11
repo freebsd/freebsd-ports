0a6d9ef5dfb993682e0f0a309d63ed2708e86611
Maxim Sobolev <sobomax@FreeBSD.org>
2001-09-11 07:41:34 +0000

    Update to 0.6.6.

--- devel/oaf/files/patch-liboaf::oaf-fork-server.c
+++ devel/oaf/files/patch-liboaf::oaf-fork-server.c
@@ -1,9 +1,9 @@
 
 $FreeBSD$
 
---- liboaf/oaf-registration.c	2001/09/04 11:05:52	1.1
-+++ liboaf/oaf-registration.c	2001/09/04 11:06:15
-@@ -395,6 +395,7 @@
+--- liboaf/oaf-fork-server.c	2001/09/06 07:05:36	1.1
++++ liboaf/oaf-fork-server.c	2001/09/06 07:06:12
+@@ -267,6 +267,7 @@
  		if (od_iorstr)
  		  oaf_setenv ("OAF_OD_IOR", od_iorstr);
                  
