68a1a63a632073537b227bf517e566c6b65edc0a
Maxim Sobolev <sobomax@FreeBSD.org>
2001-09-11 14:41:38 +0000

    Update to 0.9.7.

---
+++ editors/gedit/files/patch-ltmain.sh
@@ -0,0 +1,18 @@
+
+$FreeBSD$
+
+--- ltmain.sh	2001/08/27 09:51:26	1.1
++++ ltmain.sh	2001/08/27 09:51:42
+@@ -4175,10 +4175,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
