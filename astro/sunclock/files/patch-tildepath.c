eb65aa014c237ebc30bfbc68cf96eb39aaf74681
David W. Chapman Jr. <dwcjr@FreeBSD.org>
2001-09-14 03:47:54 +0000

    Update to 3.50.p1 Give pat@databits.net maintainership
    PR:		30024
    Submitted by:	pat@databits.net

---
+++ astro/sunclock/files/patch-tildepath.c
@@ -0,0 +1,10 @@
+--- tildepath.c.orig	Fri Aug 24 07:06:53 2001
++++ tildepath.c	Fri Aug 24 07:06:59 2001
+@@ -38,7 +38,6 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <pwd.h>
+-#include <malloc.h>
+ #include <string.h>
+ 
+ /*
