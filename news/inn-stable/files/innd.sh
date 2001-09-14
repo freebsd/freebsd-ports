b23421555b50e501788626fff7dd383d957006d3
des <des@FreeBSD.org>
2001-09-14 23:14:00 +0000

    Upgrade to 20010913, and take care to run rc.news as news, not root.

--- news/inn-stable/files/innd.sh
+++ news/inn-stable/files/innd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $# -eq 0 -o x$1 = xstart ]; then
    if [ -x !!PREFIX!!/news/bin/rc.news -a -f !!PREFIX!!/news/db/history.pag ]; then
-      limits -C news !!PREFIX!!/news/bin/rc.news && echo ' inn'
+      su news -c !!PREFIX!!/news/bin/rc.news && echo ' inn'
    fi
 fi
 if [ x$1 = xstop ]; then
