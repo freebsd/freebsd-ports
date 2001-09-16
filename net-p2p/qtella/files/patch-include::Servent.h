0174c9bcaa30d7b7eb1a30bed7e3cbf1c8fa6569
Will Andrews <will@FreeBSD.org>
2001-09-16 20:44:09 +0000

    Add qtella 0.2.1, a Gnutella client using Qt.
    PR:		30466
    Submitted by:	Jonathan Belson (jon@witchspace.com)

---
+++ net-p2p/qtella/files/patch-include::Servent.h
@@ -0,0 +1,10 @@
+--- include/Servent.h.orig	Sun Sep  2 20:38:03 2001
++++ include/Servent.h	Sun Sep  2 20:38:14 2001
+@@ -7,6 +7,7 @@
+ #include <string>
+ #include <list>
+ #include <set>
++#include <time.h>
+ 
+ #include "Gnutella.h"
+ #include "Address.h"
