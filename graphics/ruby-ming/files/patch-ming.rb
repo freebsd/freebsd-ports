3f4ffff96b114333af00c98ee270871d67ab45eb
knu <knu@FreeBSD.org>
2001-09-17 18:28:12 +0000

    Fix silly syntax bugs in ming.rb.
    Reported by:	Tom Hukins <tom@FreeBSD.org>

---
+++ graphics/ruby-ming/files/patch-ming.rb
@@ -0,0 +1,18 @@
+--- ming.rb.orig	Mon Sep 17 20:48:39 2001
++++ ming.rb	Tue Sep 18 03:22:32 2001
+@@ -344,6 +344,7 @@
+ 
+ # deprecated
+ class SWFMovieClip <SWFSprite
++end
+ 
+ class SWFGradient <SWFBase
+ 	def initialize
+@@ -375,6 +376,7 @@
+ 
+ # deprecated
+ class SWFMorphShape <SWFMorph
++end
+ 
+ class SWFFont <SWFBase
+ 
