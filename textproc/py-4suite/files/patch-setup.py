585dc28b8b0b74eb187bbac28430b8d20fa50d01
Johann Visagie <wjv@FreeBSD.org>
2001-09-17 10:27:15 +0000

    - Update to version 0.11.1

--- textproc/py-4suite/files/patch-setup.py
+++ textproc/py-4suite/files/patch-setup.py
@@ -7,18 +7,15 @@ the "correct" location:
 
 - It removes some redundant documentation.
 
-- It splits data_files into two parts, one which contains only documentation,
-  and one which contains all other files (currently, juse one!)
-
 Note that setup.py is processed further in the port Makefile's post-patch
 target.
 
--- Johann Visagie <johann@egenetics.com>
+-- Johann Visagie <wjv@FreeBSD.org>
 
 
---- setup.py.orig	Tue May  1 00:38:56 2001
-+++ setup.py	Fri May  4 17:23:15 2001
-@@ -304,21 +304,7 @@
+--- setup.py.orig	Thu Sep 13 20:21:05 2001
++++ setup.py	Fri Sep 14 16:40:29 2001
+@@ -228,22 +228,7 @@
  #
  ############################################################
  
@@ -27,6 +24,7 @@ target.
 -                                'docs/text/CREDITS',
 -                                'docs/text/PACKAGES',
 -                                'docs/text/README',
+-                                'docs/text/ReleaseNotes',
 -                                'docs/text/REBUILDING',
 -                                'docs/text/TODO',
 -                                'docs/ChangeLog',
@@ -41,35 +39,31 @@ target.
                           files=['docs/text/COPYRIGHT',
                                  'docs/text/CREDITS',
                                  'docs/text/PACKAGES',
-@@ -435,9 +421,6 @@
-               Data_Files(copy_to='test_suite/4ODS/Tools',
-                          files=glob.glob('Ods/Tools/test_suite/*.*')),
+@@ -330,7 +315,7 @@
+                          files=get_files('test_suite/Rdf/Inference/*.*')),
  
--              Data_Files(use_install_cmd='install_lib',
--                         copy_to='Ft/DbDom',
--                         files=['DbDom/dom.odl']),
-               Data_Files(copy_to='test_suite/DbDom',
-                          files=['Lib/TestSuite.py']),
-               Data_Files(copy_to='test_suite/DbDom',
-@@ -457,8 +440,10 @@
-               Data_Files(copy_to='demo/4ODS/book_mark_manager',
-                          files=glob.glob('Ods/demo/book_mark_manager/*.*')),
+ 
+-              Data_Files(copy_to='profile/Rdf',
++              Data_Files(copy_to='test_suite/profile/Rdf',
+                          files=get_files('profile/Rdf/*.*')),
+ 
+ 
+@@ -353,7 +338,7 @@
+               Data_Files(copy_to='test_suite/Ods/borrowed',
+                          files=get_files('test_suite/Ods/borrowed/*.*')),
+ 
+-              Data_Files(copy_to='profile/Ods',
++              Data_Files(copy_to='test_suite/profile/Ods',
+                          files=get_files('profile/Ods/*.*')),
+ 
+ 
+@@ -377,9 +362,6 @@
+               Data_Files(copy_to='demos/4ODS/tutorial',
+                          files=get_files('demos/4ODS/tutorial/*.*')),
                ]
+-
 -for df in data_files:
 -    df.base_dir = '$base/doc/$dist_name-$dist_version'
-+data_files2 = [Data_Files(use_install_cmd='install_lib',
-+                         copy_to='Ft/DbDom',
-+                         files=['DbDom/dom.odl'])
-+              ]
  
  ############################################################
  #
-@@ -545,7 +530,7 @@
-     py_files=py_files,
-     l10n=l10n,
-     
--    data_files=data_files,
-+    data_files=data_files + data_files2,
-     ext_modules=ext_modules,
-     scripts=DistExt.EnsureScripts('Xslt/bin/4xslt',
-                                   'Rdf/exec/4rdf',
