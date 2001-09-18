3c2bd91eb296c5737e4661f64732230ee5072515
Akinori MUSHA <knu@FreeBSD.org>
2001-09-18 07:36:04 +0000

    Update to 0.9.1. japanese/eterm becomes a slave port of x11/eterm.
    PR:		ports/30629
    Submitted by:	MANTANI Nobutaka <nobutaka@nobutaka.com> (MAINTAINER)

---
+++ japanese/eterm/files/extra-patch-src:feature.h
@@ -0,0 +1,13 @@
+--- src/feature.h.orig	Sat Sep 15 17:19:46 2001
++++ src/feature.h	Sat Sep 15 17:20:24 2001
+@@ -254,8 +254,8 @@
+ #define APL_NAME	"Eterm"
+ 
+ /* COLORTERM, TERM environment variables */
+-#define TERMENV       "Eterm"
+-#define COLORTERMENV  "Eterm"
++#define TERMENV       "kterm"
++#define COLORTERMENV  "kterm"
+ 
+ #ifdef NO_MOUSE_REPORT
+ # ifndef NO_MOUSE_REPORT_SCROLLBAR
