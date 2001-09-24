fbdc727a528880f12eefdba676693a0eca4375d5
sobomax <sobomax@FreeBSD.org>
2001-09-24 09:58:35 +0000

    Update to 0.10.0.91.

---
+++ news/pan2/files/patch-pan::text.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- pan/text.c	2001/09/24 08:24:33	1.1
++++ pan/text.c	2001/09/24 08:25:03
+@@ -23,8 +23,6 @@
+ #include <ctype.h>
+ #include <string.h>
+ 
+-#include <alloca.h>
+-
+ #include <libgnomeui/libgnomeui.h>
+ 
+ #include <gdk-pixbuf/gdk-pixbuf.h>                                                           
