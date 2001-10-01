db6db4ebe2c003dc53f8208460c29138d980f080
knu <knu@FreeBSD.org>
2001-10-01 20:37:44 +0000

    Remove the support for XEmacs19 and XEmacs20.
    Approved by:	shige (MAINTAINER)

--- Mk/bsd.emacs.mk
+++ Mk/bsd.emacs.mk
@@ -1,5 +1,5 @@
 #
-#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.13 2001-02-05 07:51:50 shige Exp $
+#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.14 2001-10-01 20:37:44 knu Exp $
 #
 #	bsd.emacs.mk - 19990829 Shigeyuki Fukushima.
 #
@@ -92,32 +92,6 @@ EMACS_LIBDIR_WITH_VER?=	share/${EMACS_NAME}/${EMACS_VER}
 EMACS_PORTSDIR=		${PORTSDIR}/editors/mule
 EMACS_COMMON_PORT=	YES
 EMACS_HAS_MULE=		YES
-
-# XEmacs-19.x
-.elif (${EMACS_PORT_NAME} == "xemacs")
-EMACS_NAME=		xemacs
-EMACS_VER=		19.16
-EMACS_MAJOR_VER=	19
-EMACS_LIBDIR?=		lib/${EMACS_NAME}
-EMACS_LIBDIR_WITH_VER?=	lib/${EMACS_NAME}-${EMACS_VER}
-EMACS_PORTSDIR=		${PORTSDIR}/editors/xemacs
-EMACS_COMMON_PORT=	NO
-EMACS_HAS_MULE=		NO
-# subdirectory of emacs core elisp files
-EMACS_CORE_SUBDIR=	prim
-
-# XEmacs-20.x
-.elif (${EMACS_PORT_NAME} == "xemacs20")
-EMACS_NAME=		xemacs
-EMACS_VER=		20.4
-EMACS_MAJOR_VER=	20
-EMACS_LIBDIR?=		lib/${EMACS_NAME}
-EMACS_LIBDIR_WITH_VER?=	lib/${EMACS_NAME}-${EMACS_VER}
-EMACS_PORTSDIR=		${PORTSDIR}/editors/xemacs20
-EMACS_COMMON_PORT=	NO
-EMACS_HAS_MULE=		NO
-# subdirectory of emacs core elisp files
-EMACS_CORE_SUBDIR=	prim
 
 # XEmacs-21.x
 .elif (${EMACS_PORT_NAME} == "xemacs21")
