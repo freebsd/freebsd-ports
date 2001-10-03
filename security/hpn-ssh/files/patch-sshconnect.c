7824f7761f26a51025cd162832b5022a6f5c0d24
dinoex <dinoex@FreeBSD.org>
2001-10-03 15:19:27 +0000

    - Update to OpenSSH 2.9.9p2 - security-patch for cookie files obsolete - MD5 password support activated
    Approved by:	dwcjr@FreeBSD.org

--- security/hpn-ssh/files/patch-sshconnect.c
+++ security/hpn-ssh/files/patch-sshconnect.c
@@ -1,12 +1,13 @@
---- sshconnect.c.orig	Fri Apr 13 01:34:36 2001
-+++ sshconnect.c	Fri Jun  1 08:01:05 2001
-@@ -504,10 +504,12 @@
- 		local = (ntohl(((struct sockaddr_in *)hostaddr)->sin_addr.s_addr) >> 24) == IN_LOOPBACKNET;
+--- sshconnect.c.orig	Wed Aug  8 00:29:09 2001
++++ sshconnect.c	Wed Oct  3 14:28:15 2001
+@@ -577,11 +577,13 @@
+ 		    sin_addr.s_addr) >> 24) == IN_LOOPBACKNET;
  		salen = sizeof(struct sockaddr_in);
  		break;
 +#ifdef INET6
  	case AF_INET6:
- 		local = IN6_IS_ADDR_LOOPBACK(&(((struct sockaddr_in6 *)hostaddr)->sin6_addr));
+ 		local = IN6_IS_ADDR_LOOPBACK(
+ 		    &(((struct sockaddr_in6 *)hostaddr)->sin6_addr));
  		salen = sizeof(struct sockaddr_in6);
  		break;
 +#endif
