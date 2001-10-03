5adaf17fcc14c0818d548fdfbf28ab41fecaad86
dinoex <dinoex@FreeBSD.org>
2001-10-03 13:15:16 +0000

    - Update to OpenSSH 2.9.9 - convert portname into lowercase - PREFIX support for default sshd_config - security-patch for cookie files obsolete

---
+++ security/openssh/files/patch-rijndael.c
@@ -0,0 +1,10 @@
+--- rijndael.c.orig	Thu Sep 13 11:48:39 2001
++++ rijndael.c	Wed Oct  3 13:40:27 2001
+@@ -27,6 +27,7 @@
+  */
+ #include <stdlib.h>
+ #include <string.h>
++#include <sys/types.h>
+ 
+ #include "rijndael.h"
+ 
