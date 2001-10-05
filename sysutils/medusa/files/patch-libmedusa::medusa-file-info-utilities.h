5720c40936dbacb999e4f9e5ffb6ec35bcbbaa6c
Maxim Sobolev <sobomax@FreeBSD.org>
2001-10-05 12:43:25 +0000

    Unbroke on recent -stable.

---
+++ sysutils/medusa/files/patch-libmedusa::medusa-file-info-utilities.h
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- libmedusa/medusa-file-info-utilities.h	2001/10/05 15:43:42	1.1
++++ libmedusa/medusa-file-info-utilities.h	2001/10/05 15:44:00
+@@ -29,6 +29,7 @@
+ #ifndef MEDUSA_FILE_INFO_UTILITIES_H
+ #define MEDUSA_FILE_INFO_UTILITIES_H
+ 
++#include <sys/types.h>
+ #include <glib.h>
+ #include <time.h>
+ 
