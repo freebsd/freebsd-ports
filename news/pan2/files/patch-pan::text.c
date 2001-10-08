93d60504ba28b53a0391547fe8560fcc45f522ce
Maxim Sobolev <sobomax@FreeBSD.org>
2001-10-08 17:00:04 +0000

    Update to 0.10.0.92.

--- news/pan2/files/patch-pan::text.c
+++ news/pan2/files/patch-pan::text.c
@@ -1,14 +1,14 @@
 
 $FreeBSD$
 
---- pan/text.c	2001/09/24 08:24:33	1.1
-+++ pan/text.c	2001/09/24 08:25:03
+--- pan/text.c.orig	Fri Oct  5 00:39:24 2001
++++ pan/text.c	Mon Oct  8 19:54:22 2001
 @@ -23,8 +23,6 @@
  #include <ctype.h>
  #include <string.h>
  
 -#include <alloca.h>
 -
- #include <libgnomeui/libgnomeui.h>
- 
- #include <gdk-pixbuf/gdk-pixbuf.h>                                                           
+ #include <glib.h>
+ #include <gtk/gtkmain.h>
+ #include <gtk/gtkwidget.h>
