3fbe134b36d51376a2fe534b57c74f61fce567ce
sobomax <sobomax@FreeBSD.org>
2001-10-11 12:08:51 +0000

    - Correct pkg-plist - don't use absolute pathnames there; - don't install useless .la file; - use libgnugetopt in rioutil(1).

---
+++ audio/rioutil/files/patch-ltmain.sh
@@ -0,0 +1,18 @@
+
+$FreeBSD$
+
+--- ltmain.sh	2001/10/11 11:48:44	1.1
++++ ltmain.sh	2001/10/11 11:49:30
+@@ -4175,10 +4175,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
