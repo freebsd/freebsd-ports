c4a653303c485297ae7929069b0f3ed71147bb41
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-10-14 18:21:19 +0000

    enable freetype support
    PR:		30122
    Submitted by:	KANOU Hiroki <kanou@mil.allnet.ne.jp>

---
+++ print/ttf2pt1/files/patch-ft.c
@@ -0,0 +1,11 @@
+--- ft.c.orig	Wed May 16 10:31:08 2001
++++ ft.c	Mon Aug 27 17:58:55 2001
+@@ -14,7 +14,7 @@
+ #include <sys/types.h>
+ #include <freetype/freetype.h>
+ #include <freetype/ftglyph.h>
+-#include <freetype/ftnames.h>
++#include <freetype/ftsnames.h>
+ #include <freetype/ttnameid.h>
+ #include <freetype/ftoutln.h>
+ #include "pt1.h"
