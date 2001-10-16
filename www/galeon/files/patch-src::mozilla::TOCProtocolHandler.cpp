67f0e231258aecd65a63041a0ab7573459504018
sobomax <sobomax@FreeBSD.org>
2001-10-16 07:42:36 +0000

    Update to 0.12.4.

--- www/galeon/files/patch-src::mozilla::TOCProtocolHandler.cpp
+++ www/galeon/files/patch-src::mozilla::TOCProtocolHandler.cpp
@@ -1,14 +1,16 @@
 
 $FreeBSD$
 
---- src/mozilla/TOCProtocolHandler.cpp	2001/10/08 16:33:57	1.1
-+++ src/mozilla/TOCProtocolHandler.cpp	2001/10/08 16:34:17
-@@ -21,11 +21,7 @@
+--- src/mozilla/TOCProtocolHandler.cpp	2001/10/16 07:21:34	1.1
++++ src/mozilla/TOCProtocolHandler.cpp	2001/10/16 07:21:49
+@@ -21,13 +21,7 @@
  #include "outputbuffer.h"
  
  #include <algorithm>
 -#if (GCC_VERSION < VERSION2(3,0)) && (GCC_VERSION >= VERSION2(2,95))
 -#include <g++-3/string>
+-#elif (GCC_VERSION < VERSION2(3,0)) && (GCC_VERSION > VERSION2(2,90))
+-#include <g++-2/string>
 -#else
  #include <string>
 -#endif
