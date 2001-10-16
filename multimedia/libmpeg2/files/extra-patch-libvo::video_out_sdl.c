b8c447e03f6d3f62f37ae9a70d7dcaa7e675021e
lioux <lioux@FreeBSD.org>
2001-10-16 19:25:34 +0000

    New port libmpeg2 version 0.2.0: A free library for decoding mpeg-2 and mpeg-1 video streams

---
+++ multimedia/libmpeg2/files/extra-patch-libvo::video_out_sdl.c
@@ -0,0 +1,11 @@
+--- libvo/video_out_sdl.c.orig	Tue Oct 16 13:00:54 2001
++++ libvo/video_out_sdl.c	Tue Oct 16 13:01:07 2001
+@@ -31,7 +31,7 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <inttypes.h>
+-#include <SDL/SDL.h>
++#include <SDL11/SDL.h>
+ 
+ #include "video_out.h"
+ #include "video_out_internal.h"
