be18c7bf2ce7da8a74889c9a1d7570a2c9817201
nectar <nectar@FreeBSD.org>
2001-10-16 14:37:37 +0000

    Fix import of DateTime module.
    Submitted by:	olgeni

---
+++ databases/py-PyGreSQL/files/patch-pgdb.py
@@ -0,0 +1,12 @@
+--- pgdb.py.orig        Thu Oct 11 12:14:35 2001
++++ pgdb.py     Thu Oct 11 12:14:42 2001
+@@ -58,7 +58,7 @@
+ import string
+ import exceptions
+ import types
+-import DateTime
++import mx.DateTime
+ import time
+
+ ### module constants
+
