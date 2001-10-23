14e414f599801134fede1d2e1f6a09826b624015
sobomax <sobomax@FreeBSD.org>
2001-10-23 13:30:14 +0000

    Update to 0.2.93.

--- databases/libgda2/files/patch-providers::gda-default-server::sqlite::tool::lemon.c
+++ databases/libgda2/files/patch-providers::gda-default-server::sqlite::tool::lemon.c
@@ -1,8 +1,8 @@
 
 $FreeBSD$
 
---- providers/gda-default-server/sqlite/tool/lemon.c.orig	Fri Aug  3 17:47:37 2001
-+++ providers/gda-default-server/sqlite/tool/lemon.c	Mon Aug  6 10:59:21 2001
+--- providers/gda-default-server/sqlite/lemon.c.orig	Fri Aug  3 17:47:37 2001
++++ providers/gda-default-server/sqlite/lemon.c	Mon Aug  6 10:59:21 2001
 @@ -1327,7 +1327,7 @@
  /*
  ** Return a pointer to the next structure in the linked list.
