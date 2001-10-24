00764ef839e7d44a2f4f020100228a4b5edac4de
joe <joe@FreeBSD.org>
2001-10-24 00:52:58 +0000

    Change the recommendations on how to test the syntax of this file.

--- CVSROOT/cfg_local.pm
+++ CVSROOT/cfg_local.pm
@@ -6,8 +6,10 @@
 # scripts.  It is loaded by cfg.pm and overrides the default
 # configuration in that file.
 #
-# It is advised that you test it with 'perl -cw cfg_local.pm'
-# before you commit any changes.
+# It is advised that you test it with
+#     'env CVSROOT=/path/to/cvsroot perl -cw cfg.pm'
+# before you commit any changes.  The check is to cfg.pm which
+# loads this file.
 ####################################################################
 ####################################################################
 
