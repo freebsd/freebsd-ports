72396385cb56b17d43bad195e406f42e832e5680
sf <sf@FreeBSD.org>
2001-10-29 04:55:47 +0000

    fix build for -CURRENT: malloc.h is depriciated.
    PR:		31572
    Submitted by:	MANTANI Nobutaka <nobutaka@nobutaka.com>

---
+++ graphics/libxine/files/patch-src:input:input_rtp.c
@@ -0,0 +1,11 @@
+--- src/input/input_rtp.c.orig	Sat Oct 27 19:14:06 2001
++++ src/input/input_rtp.c	Sat Oct 27 19:14:14 2001
+@@ -67,7 +67,6 @@
+ #endif
+ 
+ #include <stdio.h>
+-#include <malloc.h>
+ #include <unistd.h>
+ #include <fcntl.h>
+ #include <string.h>
+
---
+++ multimedia/libxine/files/patch-src:libspudec:spu.c
@@ -0,0 +1,10 @@
+--- src/libspudec/spu.c.orig	Sat Oct 27 19:15:22 2001
++++ src/libspudec/spu.c	Sat Oct 27 19:15:31 2001
+@@ -63,7 +63,6 @@
+ #include <unistd.h>
+ #include <string.h>
+ #include <inttypes.h>
+-#include <malloc.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
+ 
