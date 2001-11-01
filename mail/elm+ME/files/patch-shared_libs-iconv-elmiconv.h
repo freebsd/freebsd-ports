299ecccc242b6cf309cfa54f7572ddba347b2490
demon <demon@FreeBSD.org>
2001-11-01 23:07:24 +0000

    Update to 2.4.95a + numerious enhancments
    PR:		31553
    Submitted by:	MAINTAINER

---
+++ mail/elm+ME/files/patch-shared_libs-iconv-elmiconv.h
@@ -0,0 +1,11 @@
+--- shared_libs/iconv/elmiconv.h.orig	Wed Oct 31 09:23:34 2001
++++ shared_libs/iconv/elmiconv.h	Thu Nov  1 00:24:13 2001
+@@ -8,7 +8,7 @@
+ 
+ #include "elmshared.h"
+ 
+-#include <iconv.h>
++#include <giconv.h>
+ 
+ #include "../../hdrs/cs_imp.h"
+ 
