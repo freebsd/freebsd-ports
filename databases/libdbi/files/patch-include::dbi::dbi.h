fe4a71305a81b72c7f07699e7c960f8e91d38bf6
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-11-04 06:20:39 +0000

    add libdbi Database Independent Abstraction Layer for C

---
+++ databases/libdbi/files/patch-include::dbi::dbi.h
@@ -0,0 +1,10 @@
+--- include/dbi/dbi.h.orig	Sun Nov  4 11:48:26 2001
++++ include/dbi/dbi.h	Sun Nov  4 11:48:37 2001
+@@ -29,6 +29,7 @@
+ 
+ #include <stdlib.h>
+ #include <stdarg.h>
++#include <time.h>
+ 
+ /* opaque type definitions */
+ typedef void * dbi_plugin;
