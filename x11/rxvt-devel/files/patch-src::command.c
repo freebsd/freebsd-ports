e57ca72c0f00b73955d3d93bf935ee2251dd069d
lioux <lioux@FreeBSD.org>
2001-11-07 20:38:01 +0000

    Update to 2.7.7

--- x11/rxvt-devel/files/patch-src::command.c
+++ x11/rxvt-devel/files/patch-src::command.c
@@ -1,11 +1,11 @@
 --- src/command.c.orig	Thu Apr  5 03:52:39 2001
 +++ src/command.c	Fri Jun 22 13:36:57 2001
-@@ -2796,7 +2796,7 @@ rxvt_tt_printf(rxvt_t *r, const char *fm
+@@ -2878,7 +2878,7 @@
      unsigned char   buf[256];
  
      va_start(arg_ptr, fmt);
--    vsprintf(buf, fmt, arg_ptr);
-+    vsnprintf(buf, sizeof(buf), fmt, arg_ptr);
+-    vsprintf((char *)buf, fmt, arg_ptr);
++    vsnprintf((char *)buf, sizeof((char *) buf), fmt, arg_ptr);
      va_end(arg_ptr);
-     rxvt_tt_write(r, buf, STRLEN(buf));
+     rxvt_tt_write(r, buf, (unsigned int)STRLEN(buf));
  }
