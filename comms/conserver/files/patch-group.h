db1ca33e4dafe9edfad75d77dd335eb70c6a2b0e
obrien <obrien@FreeBSD.org>
2001-11-08 17:55:34 +0000

    Our [md5] encrypted passwords can be up to 64 characters in length.
    Submitted by:	brian

---
+++ comms/conserver/files/patch-group.h
@@ -0,0 +1,5 @@
+--- etc/conserver/group.h.orig	Wed Aug  2 11:39:42 2000
++++ etc/conserver/group.h	Thu Nov  8 09:53:10 2001
+@@ -29 +29 @@
+-#define MAXPSWDLEN	16	/* max length of encrypted password	*/
++#define MAXPSWDLEN	64	/* max length of encrypted password	*/
