a7b3e35aa4b11478fae164f7350e7c3a152322c4
obrien <obrien@FreeBSD.org>
2001-11-08 18:09:50 +0000

    Use the shorter "http://cvsweb.FreeBSD.org/" spelling now.

---
+++ mail/cvsmail/files/patch-cvsmail.c
@@ -0,0 +1,5 @@
+--- cvsmail.c.orig	Sun Sep 16 13:47:37 2001
++++ cvsmail.c	Thu Nov  8 10:08:44 2001
+@@ -56 +56 @@
+-	url_base = "http://www.FreeBSD.org/cgi/cvsweb.cgi/";
++	url_base = "http://cvsweb.FreeBSD.org/;
