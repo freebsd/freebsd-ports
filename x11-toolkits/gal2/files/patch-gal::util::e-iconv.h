c0102bb53e49da194a60126a626540d5d9657079
sobomax <sobomax@FreeBSD.org>
2001-11-09 16:35:48 +0000

    Update to 0.18.

---
+++ x11-toolkits/gal2/files/patch-gal::util::e-iconv.h
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- gal/util/e-iconv.h	2001/11/07 14:06:41	1.1
++++ gal/util/e-iconv.h	2001/11/07 14:06:52
+@@ -24,7 +24,7 @@
+ #ifndef _E_ICONV_H_
+ #define _E_ICONV_H_
+ 
+-#include <iconv.h>
++#include <giconv.h>
+ 
+ #ifdef __cplusplus
+ extern "C" {
