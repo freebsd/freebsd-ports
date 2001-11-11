cbc1a1c189c598e93a6d73ca3d19bab95cd377c9
matusita <matusita@FreeBSD.org>
2001-11-11 06:59:28 +0000

    Add vmware-guestd, a daemon for VMware 3.x FreeBSD guest OS.

---
+++ emulators/vmware-guestd4/files/vmware-guestd.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
+    echo "$0: Cannot determine the PREFIX" >&2
+    exit 1
+fi
+
+case "$1" in
+start)
+	exec 2>/dev/null
+	ulimit -c 0
+	if ${PREFIX}/sbin/checkvm >/dev/null; then
+		${PREFIX}/sbin/vmware-guestd & && echo -n ' vmware-guestd'
+	fi
+	;;
+stop)
+	exec 2>/dev/null
+	ulimit -c 0
+	if ${PREFIX}/sbin/checkvm >/dev/null; then
+		killall vmware-guestd && echo -n ' vmware-guestd'
+	fi
+	;;
+*)
+	echo "Usage: `basename $0` {start|stop}" >&2
+	;;
+esac
+
+exit 0
