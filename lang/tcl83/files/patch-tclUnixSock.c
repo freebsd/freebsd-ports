575626bd415ca33cbd9dca7b8e6289bba337ddab
Dirk Meyer <dinoex@FreeBSD.org>
2001-11-14 22:28:19 +0000

    Fix patch, as the previous one did only half the job.
    Submitted by:	Roberto@eurocontrol.fr

--- lang/tcl83/files/patch-tclUnixSock.c
+++ lang/tcl83/files/patch-tclUnixSock.c
@@ -1,5 +1,14 @@
 --- tclUnixSock.c.orig	Fri Apr 16 02:48:05 1999
-+++ tclUnixSock.c	Wed Nov 14 09:25:20 2001
++++ tclUnixSock.c	Wed Nov 14 23:21:18 2001
+@@ -65,7 +65,7 @@
+ char *
+ Tcl_GetHostName()
+ {
+-#ifndef NO_UNAME
++#if 0
+     struct utsname u;
+     struct hostent *hp;
+ #else
 @@ -80,7 +80,7 @@
      }
  
