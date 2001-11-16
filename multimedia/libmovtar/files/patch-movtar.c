2f79220caf7a319d3c2c60cf3bb1ae55fee6215e
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2001-11-16 03:39:39 +0000

    New port libmovtar version 0.1.2: Supports reading/writing of MJPEG video in YUV 4:2:2 JPEG

---
+++ multimedia/libmovtar/files/patch-movtar.c
@@ -0,0 +1,10 @@
+--- movtar.c.orig	Tue Oct 16 00:02:46 2001
++++ movtar.c	Tue Oct 16 00:03:25 2001
+@@ -40,7 +40,6 @@
+ #include <tar.h>
+ #include <glib.h>
+ #include <sys/types.h>
+-#include <linux/types.h>
+ 
+ #include "movtar.h"
+ 
---
+++ multimedia/libmovtar/files/patch-tar.c
@@ -0,0 +1,10 @@
+--- tar.c.orig	Tue Oct 16 00:03:05 2001
++++ tar.c	Tue Oct 16 00:03:09 2001
+@@ -16,7 +16,6 @@
+ #include <glib.h>
+ 
+ #include <sys/types.h>
+-#include <linux/types.h>
+ 
+ #include "movtar.h"
+ 
