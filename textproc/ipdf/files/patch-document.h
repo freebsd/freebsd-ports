a4a1a3253b056679e956111db215fd766eb12216
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-11-16 05:53:50 +0000

    add ipdf Creates indexed pdf documents from text files

---
+++ textproc/ipdf/files/patch-document.h
@@ -0,0 +1,10 @@
+--- document.h.orig	Fri Nov 16 13:49:25 2001
++++ document.h	Fri Nov 16 13:49:33 2001
+@@ -4,6 +4,7 @@
+ #include <error.h>
+ #endif
+ 
++#include <sys/types.h>
+ #include <regex.h>
+ 
+ #define __DOCUMENT_H__
