395a4bbef39d86968cc9de3698cce35176582ea4
Patrick Li <pat@FreeBSD.org>
2001-11-16 06:14:36 +0000

    Update to 2.02
    PR:	31765
    Submitted by:	maintainer
    Reviewed by:	lioux,maintainer
    Approved by:	lioux

---
+++ java/netrexx/files/patch-bin::NetRexxC.cmd
@@ -0,0 +1,7 @@
+--- bin/NetRexxC.cmd	Sun Aug 20 12:44:40 2000
++++ bin/NetRexxC.cmd.new	Sun Nov  4 21:30:11 2001
+@@ -1,3 +1,4 @@
++/*usr/bin/true; exec rexx "$0" "$@" # REXX program */
+ /* Translate and compile a NetRexx program             */
+ /*                                                     */
+ /* use as:  NetRexxC hello [file2]...                  */
