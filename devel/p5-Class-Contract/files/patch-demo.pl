04e0bdf1af47ce6513a8ba840a5a02da012941eb
ijliao <ijliao@FreeBSD.org>
2001-11-18 03:20:10 +0000

    add p5-Class-Contract Design-by-Contract OO in Perl
    PR:		31782
    Submitted by:	Sergey Skvortsov <skv@protey.ru>

---
+++ devel/p5-Class-Contract/files/patch-demo.pl
@@ -0,0 +1,8 @@
+--- demo.pl.orig	Mon Nov  5 17:24:22 2001
++++ demo.pl	Mon Nov  5 17:24:29 2001
+@@ -1,4 +1,4 @@
+-#! /usr/local/bin/perl -w
++#!/usr/bin/perl -w
+ 
+ # NOTES:
+ #	Invariants and pre- and post-conditions are expected
