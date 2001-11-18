849ac4847d549f97f63694bb3d66025effeed26d
Dirk Froemberg <dirk@FreeBSD.org>
2001-11-18 22:29:38 +0000

    Add support for domxml.
    PR:		ports/28948, ports/29565

--- www/mod_php4/scripts/configure.php
+++ www/mod_php4/scripts/configure.php
@@ -35,6 +35,7 @@ OpenSSL		"OpenSSL support" OFF \
 SNMP		"SNMP support" OFF \
 XML		"XML support" OFF \
 XSLT		"Sablotron support (implies XML and iconv)" OFF \
+DOMXML		"DOM support" OFF \
 FTP		"File Transfer Protocol support" OFF \
 CURL		"CURL support" OFF \
 gettext		"gettext library support" OFF \
@@ -211,6 +212,10 @@ while [ "$1" ]; do
 			if [ -z "$ICONV" ]; then
 				set $* \"iconv\"
 			fi
+			;;
+		\"DOMXML\")
+			echo "LIB_DEPENDS+=	xml2.5:\${PORTSDIR}/textproc/libxml2"
+			echo "CONFIGURE_ARGS+=--with-dom=\${PREFIX}"
 			;;
 		\"FTP\")
 			echo "CONFIGURE_ARGS+=--enable-ftp"
