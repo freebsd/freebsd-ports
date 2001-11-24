2153508fdf0d0d930f514fb11b8e9ed58310821f
dwcjr <dwcjr@FreeBSD.org>
2001-11-24 01:56:44 +0000

    Update to 3.7
    PR:		32103
    Submitted by:	maintainer

---
+++ devel/pcre/files/patch-ltmain.sh
@@ -0,0 +1,28 @@
+
+$FreeBSD$
+
+--- ltmain.sh.orig	Fri Aug 17 03:16:16 2001
++++ ltmain.sh	Fri Nov 16 17:54:42 2001
+@@ -2408,6 +2408,9 @@
+ 	  *-*-netbsd*)
+ 	    # Don't link with libc until the a.out ld.so is fixed.
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test $build_libtool_need_lc = "yes"; then
+@@ -4175,10 +4178,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
