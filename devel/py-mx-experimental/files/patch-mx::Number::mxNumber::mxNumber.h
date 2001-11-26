3c5d9ab754ea82b8bf1547e9ca11d5e753be9da1
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-11-26 04:52:03 +0000

    libgmp dependency
    Submitted by:	anthony.kim@vw.com

---
+++ devel/py-mx-experimental/files/patch-mx::Number::mxNumber::mxNumber.h
@@ -0,0 +1,11 @@
+--- mx/Number/mxNumber/mxNumber.h.orig	Mon Nov 26 12:46:34 2001
++++ mx/Number/mxNumber/mxNumber.h	Mon Nov 26 12:46:43 2001
+@@ -30,7 +30,7 @@
+ #endif
+ 
+ /* Include the GNU MP header file */
+-#include "gmp.h"
++#include <gmp.h>
+ 
+ #ifdef __cplusplus
+ extern "C" {
