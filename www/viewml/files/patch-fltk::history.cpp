a658cf7a3872e90311161294e2a4489ba3832f73
sobomax <sobomax@FreeBSD.org>
2001-11-26 17:09:18 +0000

    Unbroke on -current: don't include malloc.h.

---
+++ www/viewml/files/patch-fltk::history.cpp
@@ -0,0 +1,10 @@
+
+$FreeBSD$
+
+--- fltk/history.cpp	2001/11/26 17:09:52	1.1
++++ fltk/history.cpp	2001/11/26 17:09:59
+@@ -1,4 +1,3 @@
+-#include <malloc.h>
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <string.h>
