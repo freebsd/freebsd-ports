f7102790f2620ba2595f3695b4b51e86ece8edd5
sobomax <sobomax@FreeBSD.org>
2001-11-30 15:27:47 +0000

    Update to 0.39.1.

---
+++ devel/gtranslator/files/patch-src::message.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/message.c	2001/11/29 08:31:06	1.1
++++ src/message.c	2001/11/29 08:31:15
+@@ -38,7 +38,7 @@
+ #include "utils_gui.h"
+ 
+ #include <string.h>
+-#include <regex.h>
++#include <gnuregex.h>
+ #include <gtk/gtk.h>
+ 
+ #include <libgnomeui/gnome-app.h>
