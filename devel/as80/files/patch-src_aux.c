eb708516802338ff4f62846507f8d0f7ed521e84
Christian Weisgerber <naddy@FreeBSD.org>
2001-12-01 15:36:22 +0000

    * Update to 0.8. * Prune obsolete and excess verbiage from comment and description. * Add homepage URL.
    Approved by:	greid

---
+++ devel/as80/files/patch-src_aux.c
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- src/aux.c.orig	Fri Nov 30 02:36:50 2001
++++ src/aux.c	Fri Nov 30 02:36:51 2001
+@@ -22,6 +22,7 @@
+ 
+ #include <stdlib.h>
+ #include <ctype.h>
++#include <string.h>
+ #include "aux.h"
+ 
+ 
---
+++ devel/as80/files/patch-src_symtbl.c
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- src/symtbl.c.orig	Fri Nov 30 02:35:55 2001
++++ src/symtbl.c	Fri Nov 30 02:36:08 2001
+@@ -22,6 +22,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include "types.h"
+ #include "symtbl.h"
+ 
