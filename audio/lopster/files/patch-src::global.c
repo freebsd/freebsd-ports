0219cf27b1ea77fdb677a7e9acddf4e09d61c87f
Steve Price <steve@FreeBSD.org>
2001-12-02 18:32:20 +0000

    Update to version 1.0.1.
    PR:		32363
    Submitted by:	maintainer

--- audio/lopster/files/patch-src::global.c
+++ audio/lopster/files/patch-src::global.c
@@ -1,11 +1,11 @@
---- src/global.c.orig	Tue May 29 21:02:46 2001
-+++ src/global.c	Tue May 29 21:02:57 2001
-@@ -2734,7 +2734,7 @@
+--- src/global.c.orig	Sun Nov  4 20:30:19 2001
++++ src/global.c	Wed Nov 28 21:52:10 2001
+@@ -3597,7 +3597,7 @@
    global.auto_save = NULL;
    global.execs = NULL;
    global.pings = NULL;
--  global.ping_command = strdup("/bin/ping -c 3 $IP");
-+  global.ping_command = strdup("/sbin/ping -c 3 $IP");
+-  global.ping_command = l_strdup("/bin/ping -c 3 $IP");
++  global.ping_command = l_strdup("/sbin/ping -c 3 $IP");
  
    global.browse_width[0] = 369;
    global.browse_width[1] = 80;
