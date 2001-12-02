6452587524f1fd1b2551642992114010afb51f36
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2001-12-02 21:54:10 +0000

    o Update to 0.61 o Greatly simplify the patches removing sigprocmask protection   around send/receive since there is already a global signal handler

--- net-p2p/dctc/files/patch-src:keyboard.c
+++ net-p2p/dctc/files/patch-src:keyboard.c
@@ -1,14 +1,16 @@
---- src/keyboard.c.orig	Sat Nov 24 13:29:15 2001
-+++ src/keyboard.c	Sat Nov 24 13:43:51 2001
-@@ -31,6 +31,7 @@
+--- src/keyboard.c	Sat Dec  1 08:53:15 2001
++++ src/keyboard.c	Sun Dec  2 19:34:12 2001
+@@ -31,6 +31,9 @@
  #include <netdb.h>
  #include <glib.h>
  
++#include "config.h"
++
 +#if !(defined(BSD) && (BSD >= 199103))
         #if defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)
         /* union semun is defined by including <sys/sem.h> */
         #else
-@@ -42,6 +43,7 @@
+@@ -42,6 +45,7 @@
                 struct seminfo *__buf;      /* buffer for IPC_INFO */
         };
         #endif
--- net/dctc/files/patch-src:sema.c
+++ net/dctc/files/patch-src:sema.c
@@ -1,18 +1,17 @@
---- src/sema.c.orig	Sat Nov 24 13:48:16 2001
-+++ src/sema.c	Sat Nov 24 14:53:39 2001
-@@ -31,6 +31,11 @@
+--- src/sema.c	Sat Nov 24 08:29:56 2001
++++ src/sema.c	Sun Dec  2 19:34:12 2001
+@@ -29,8 +29,10 @@
+ #include <limits.h>
+ #include <pthread.h>
  
++#include "config.h"
  #include "sema.h"
  
-+#if (defined(__unix__) || defined(unix)) && !defined(USG)
-+#include <sys/param.h>
-+#endif
-+
 +#if !(defined(BSD) && (BSD >= 199103))
         #if defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)
         /* union semun is defined by including <sys/sem.h> */
         #else
-@@ -42,6 +47,7 @@
+@@ -42,6 +44,7 @@
                 struct seminfo *__buf;      /* buffer for IPC_INFO */
         };
         #endif
@@ -20,12 +19,3 @@
  
  #ifndef IPC_ALLOC
  #define IPC_ALLOC 0
-@@ -201,7 +207,7 @@
- 
-    pthread_attr_init (&thread_attr);
-    pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
--   if(pthread_create(&thread_id,&thread_attr, (void*)sema_master,(void*)semid)!=0)
-+   if(pthread_create(&thread_id,&thread_attr, sema_master,(void*)semid)!=0)
- 	{
- 		/* if the creation of the clock thread fails, release the master sema */
- 		/* else nobody will try to create a new clock and all xfers will hang */
