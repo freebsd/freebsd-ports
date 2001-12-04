75ead950c010128bdc7e7f595f7f27b3ea7cffe7
David E. O'Brien <obrien@FreeBSD.org>
2001-12-04 01:13:37 +0000

    Upgrade to 2.5.0. The main change is the integration of the KAME IPv6 patch. Also one minor nit was fixed about the rsyncd PID file.
    Submitted by:	naddy

--- net/rsync/files/ssh-patch-options.c
+++ net/rsync/files/ssh-patch-options.c
@@ -2,4 +2,4 @@
 +++ options.c	Thu Mar  8 03:31:32 2001
 @@ -141 +141 @@
 -  rprintf(F," -e, --rsh=COMMAND           specify rsh replacement\n");
-+  rprintf(F," -e, --rsh=COMMAND           specify rsh replacement (default %s)\n", RSYNC_RSH);
++  rprintf(F," -e, --rsh=COMMAND           specify rsh replacement (default %s)\n", RSYNC_SSH);
