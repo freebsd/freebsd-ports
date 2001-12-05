603d63963dc92b2b46632990055f981842020f0f
Ben Smithurst <ben@FreeBSD.org>
2001-12-05 18:39:21 +0000

    Update to 1.9

--- mail/cvsmail/files/patch-cvsmail.c
+++
@@ -1,5 +0,0 @@
---- cvsmail.c.orig	Sun Sep 16 13:47:37 2001
-+++ cvsmail.c	Thu Nov  8 10:08:44 2001
-@@ -56 +56 @@
--	url_base = "http://www.FreeBSD.org/cgi/cvsweb.cgi/";
-+	url_base = "http://cvsweb.FreeBSD.org/";
