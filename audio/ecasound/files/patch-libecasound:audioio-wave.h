84263f126313f4632e66fecacbc5e0bb18835afe
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2001-12-06 19:18:09 +0000

    o Update to 2.0.3 o Style fix: replace all spaces with tabs where appropriate o Add WITH_OPTMIZED_CFLAGS knob, build without any unless defined o Advertise WITH* options
    PR:		32139
    Submitted by:	maintainer

---
+++ audio/ecasound/files/patch-libecasound:audioio-wave.h
@@ -0,0 +1,11 @@
+--- libecasound/audioio-wave.h.orig	Sun Sep 23 10:50:32 2001
++++ libecasound/audioio-wave.h	Sat Nov 24 09:22:36 2001
+@@ -3,7 +3,7 @@
+ 
+ #include <string>
+ #include <iostream>
+-#include <stdint.h>
++/* #include <stdint.h> */
+ #include <sys/types.h>
+ 
+ #include "audioio-types.h"
