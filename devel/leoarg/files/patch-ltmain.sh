823e89fdf1240aaaca7fb04c1109e50516aa1809
Ying-Chieh Liao <ijliao@FreeBSD.org>
2001-12-08 20:44:26 +0000

    update master site & WWW fix pkg-plist
    PR:		32612
    Submitted by:	Ports Fury

---
+++ devel/leoarg/files/patch-ltmain.sh
@@ -0,0 +1,25 @@
+--- ltmain.sh.orig	Fri May 11 05:34:59 2001
++++ ltmain.sh	Sat Dec  8 22:23:50 2001
+@@ -2408,6 +2408,9 @@
+ 	  *-*-netbsd*)
+ 	    # Don't link with libc until the a.out ld.so is fixed.
+ 	    ;;
++	  *-*-netbsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test $build_libtool_need_lc = "yes"; then
+@@ -4175,10 +4178,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
