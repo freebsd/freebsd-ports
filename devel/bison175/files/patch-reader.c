d42fa21499073073fff0e2a243c37789fb8609de
obrien <obrien@FreeBSD.org>
2001-12-10 04:41:18 +0000

    Back this down to version 1.28 until the Bison developers can acutally do some regression tests on their later releases. Versions 1.29 and 1.30 seg fault where 1.28 did not.

--- devel/bison175/files/patch-reader.c
+++ devel/bison175/files/patch-reader.c
@@ -1,23 +1,22 @@
---- src/reader.c.orig	Sun Sep 23 02:10:06 2001
-+++ src/reader.c	Sun Sep 23 02:08:08 2001
-@@ -65,6 +65,8 @@
+--- src/reader.c.orig	Mon Jun 14 15:19:05 1999
++++ src/reader.c	Mon Jan 17 03:38:56 2000
+@@ -44,6 +44,7 @@
+ int rline_allocated;
  
- static bucket *errtoken;
- static bucket *undeftoken;
-+
+ extern char *program_name;
 +extern int broken_undeftoken_init;
- 
- 
- /*===================\
-@@ -1959,7 +1961,10 @@
-      It is always token number 2.  */
-   undeftoken = getsym ("$undefined.");
-   undeftoken->class = token_sym;
+ extern int definesflag;
+ extern int nolinesflag;
+ extern int noparserflag;
+@@ -199,7 +200,10 @@
+   /* it is always token number 2.  */
+   undeftoken = getsym("$undefined.");
+   undeftoken->class = STOKEN;
 -  undeftoken->user_token_number = 2;
-+  /* XXX ``broken_undeftoken_init'' makes Bison 1.29 bug-compatable
++  /* XXX ``broken_undeftoken_init'' makes Bison 1.28 bug-compatable
 +     with Bison 1.25.  FreeBSD depends on this behavior when compiling
 +     EGCS-1.1.2's cc1plus.  */
 +  undeftoken->user_token_number = broken_undeftoken_init ? 0 : 2;
- 
-   /* Read the declaration section.  Copy %{ ... %} groups to
-      TABLE_OBSTACK and FDEFINES file.  Also notice any %token, %left,
+   /* Read the declaration section.  Copy %{ ... %} groups to ftable and fdefines file.
+      Also notice any %token, %left, etc. found there.  */
+   if (noparserflag)
