9dc65d5ec1c2706e040511bb20a564005e575a0d
Alfred Perlstein <alfred@FreeBSD.org>
2001-12-11 07:18:38 +0000

    Remove malloc.h to make workee on -current.

---
+++ graphics/kdegraphics2/files/patch-font.cpp
@@ -0,0 +1,10 @@
+--- kdvi/font.cpp	Fri Nov  9 21:20:17 2001
++++ kdvi/font.cpp.new	Mon Dec 10 22:48:01 2001
+@@ -3,7 +3,6 @@
+ 
+ #include <kdebug.h>
+ #include <klocale.h>
+-#include <malloc.h>
+ #include <qapplication.h>
+ #include <qfile.h>
+ #include <stdio.h>
