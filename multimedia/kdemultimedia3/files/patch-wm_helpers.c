efcf301ce5aa3cb438454d38ae12e78c4a0975a6
alfred <alfred@FreeBSD.org>
2001-12-11 06:40:47 +0000

    remove include of malloc.h to unbreak on -current

---
+++ multimedia/kdemultimedia3/files/patch-wm_helpers.c
@@ -0,0 +1,10 @@
+--- kscd/libwm/wm_helpers.c	Mon Dec 10 20:02:44 2001
++++ kscd/libwm/wm_helpers.c.new	Mon Dec 10 20:02:51 2001
+@@ -30,7 +30,6 @@
+ 
+ #include <stdio.h>
+ #include <string.h>
+-#include <malloc.h>
+ #include <errno.h>
+ #include <stdarg.h>
+ #include <sys/time.h>
