fff79f9e2ad26d79dab4fddd72a149b44f198198
obrien <obrien@FreeBSD.org>
2001-12-11 01:58:33 +0000

    This PORTEPOCH for life crap is stupid. Back out the downgrade, I would have never agreed to it if I had know... This leaves a window of downgradededness 18 hours -- people can just live with that.

--- devel/bison-devel/files/patch-getargs.c
+++ devel/bison-devel/files/patch-getargs.c
@@ -1,18 +1,18 @@
---- src/getargs.c.orig	Mon Jun 14 15:19:05 1999
-+++ src/getargs.c	Mon Jan 17 03:38:56 2000
-@@ -31,6 +31,7 @@
- int noparserflag = 0;
- int toknumflag = 0;
- int rawtoknumflag = 0;
+--- src/getargs.c.orig	Sun Sep 23 02:08:44 2001
++++ src/getargs.c	Sun Sep 23 02:09:27 2001
+@@ -35,6 +35,7 @@
+ int statistics_flag = 0;
+ int yacc_flag = 0;	/* for -y */
+ int graph_flag = 0;
 +int broken_undeftoken_init;
- char *spec_name_prefix; /* for -p.  */
- char *spec_file_prefix; /* for -b. */
- extern int fixed_outfiles;/* for -y */
-@@ -44,6 +45,7 @@
  
- struct option longopts[] =
+ const char *skeleton = NULL;
+ 
+@@ -42,6 +43,7 @@
+ const char *shortopts = "yvgdhrltknVo:b:p:S:";
+ static struct option longopts[] =
  {
 +  {"broken-undeftoken-init", 0, &broken_undeftoken_init, 1},
-   {"debug", 0, &debugflag, 1},
-   {"defines", 0, &definesflag, 1},
-   {"file-prefix", 1, 0, 'b'},
+   /* Operation modes. */
+   {"help",		no_argument,	0, 'h'},
+   {"version",		no_argument, 	0, 'V'},
