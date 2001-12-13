f6262756ddb9358e7f5ba5b6b280590143bb4207
ijliao <ijliao@FreeBSD.org>
2001-12-13 08:06:39 +0000

    *** empty log message ***

---
+++ devel/ac-archive/files/patch-macro2html.cpp
@@ -0,0 +1,10 @@
+--- macro2html.cpp	Mon Aug 20 08:36:17 2001
++++ macro2html.cpp.new	Mon Dec 10 23:46:29 2001
+@@ -16,6 +16,7 @@
+ #include <vector>
+ #include <memory>
+ #include <cerrno>
++#include <sys/types.h>
+ #include <regex.h>
+ 
+ using namespace std;
