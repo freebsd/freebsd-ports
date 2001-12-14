67726e612186ee1dc522fa5a8476949d6012a09d
Maxim Sobolev <sobomax@FreeBSD.org>
2001-12-14 11:57:04 +0000

    - Fix a malloc.h syndrome on -CURRENT; - clue configure script about location of pilot-link's files, so there are no   harmless, but annoying warnings anymore.

---
+++ palm/gnome-pilot-conduits/files/patch-mal-conduit::mal::client::unix::MAL31DBConfig.c
@@ -0,0 +1,17 @@
+
+$FreeBSD$
+
+--- mal-conduit/mal/client/unix/MAL31DBConfig.c	2001/12/14 11:48:37	1.1
++++ mal-conduit/mal/client/unix/MAL31DBConfig.c	2001/12/14 11:49:50
+@@ -20,7 +20,11 @@
+  */
+ 
+ #include <MAL31DBConfig.h>
++#ifdef __FreeBSD__
++#include <stdlib.h>
++#else
+ #include <malloc.h>
++#endif
+ 
+ /*---------------------------------------------------------------------------*/
+ void
