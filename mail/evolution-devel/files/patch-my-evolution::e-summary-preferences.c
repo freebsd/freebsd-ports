2e5922223d8f1b75a293efed280c8376ca8bfef4
Maxim Sobolev <sobomax@FreeBSD.org>
2001-12-15 11:55:07 +0000

    - Add information for fetching Ukrainian weather forecasts; - add Daily Daemon News and FreeBSD Newsflash newsfeeds.
    No PORTREVISION bump, because these changes aren't significant enough.

---
+++ mail/evolution-devel/files/patch-my-evolution::e-summary-preferences.c
@@ -0,0 +1,17 @@
+
+$FreeBSD$
+
+--- my-evolution/e-summary-preferences.c	2001/12/15 11:37:58	1.1
++++ my-evolution/e-summary-preferences.c	2001/12/15 11:41:56
+@@ -440,9 +440,11 @@
+ 	{"http://www.bsdtoday.com/backend/bt.rdf", "BSD Today"},
+ 	{"http://beyond2000.com/b2k.rdf", "Beyond 2000"},
+ 	{"http://www.cnn.com/cnn.rss", "CNN"},
++	{"http://daily.daemonnews.org/ddn.rdf.php3", "Daily Daemon News"},
+         {"http://www.debianplanet.org/debianplanet/backend.php", "Debian Planet"},
+ 	{"http://www.dictionary.com/wordoftheday/wotd.rss", N_("Dictionary.com Word of the Day")},
+ 	{"http://www.dvdreview.com/rss/newschannel.rss", "DVD Review"},
++	{"http://www.freebsd.org/news/news.rdf", "FreeBSD Newsflash"},
+ 	{"http://freshmeat.net/backend/fm.rdf", "Freshmeat"},
+ 	{"http://news.gnome.org/gnome-news/rdf", "GNotices"},
+ 	{"http://headlines.internet.com/internetnews/prod-news/news.rss", "Internet.com"},
