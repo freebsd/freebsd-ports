2f21e989773e3a152b762a2e20fd375cfc15fa99
Dmitry Sivachenko <demon@FreeBSD.org>
2001-12-18 15:20:34 +0000

    Do not include malloc.h
    PR:		32756
    Submitted by:	Martin Blapp <mb@imp.ch>

---
+++ devel/kdesdk/files/patch-kstartperf-libkstartperf.c
@@ -0,0 +1,11 @@
+
+--- kstartperf/libkstartperf.c.orig	Tue Dec 11 15:13:51 2001
++++ kstartperf/libkstartperf.c	Tue Dec 11 15:13:55 2001
+@@ -13,7 +13,6 @@
+  * (C) 2000 Bill Soudan <soudan@kde.org>
+  */
+ 
+-#include <malloc.h>
+ #include <unistd.h>
+ #include <stdlib.h>
+ #include <stdio.h>
