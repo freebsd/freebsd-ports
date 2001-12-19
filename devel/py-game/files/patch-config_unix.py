df3f356e9b4f771d74491750bde0255738b3d2c3
Maxim Sobolev <sobomax@FreeBSD.org>
2001-12-19 12:58:28 +0000

    Update to 1.3.

--- devel/py-game/files/patch-config_unix.py
+++ devel/py-game/files/patch-config_unix.py
@@ -1,9 +1,9 @@
 
 $FreeBSD$
 
---- config_unix.py	2001/09/03 13:07:39	1.1
-+++ config_unix.py	2001/09/03 13:10:16
-@@ -41,7 +41,7 @@
+--- config_unix.py.orig	Sat Nov  3 19:27:56 2001
++++ config_unix.py	Wed Dec 19 14:49:24 2001
+@@ -43,7 +43,7 @@
  
  sdl_lib_name = 'SDL'
  if sys.platform.find('bsd') != -1:
@@ -12,3 +12,12 @@ $FreeBSD$
  
  DEPS = [
      Dependency('SDL', 'SDL.h', 'lib'+sdl_lib_name+'.so', sdl_lib_name),
+@@ -74,7 +74,7 @@
+ 
+     print 'Hunting dependencies...'
+     if localbase:
+-        incdirs = [localbase + '/include/SDL']
++        incdirs = [localbase + '/include/SDL11', localbase + '/include/smpeg']
+         libdirs = [localbase + '/lib']
+     else:
+         incdirs = []
