bc1a3a0b668b177f7c51ec10aa652025b9df8280
sobomax <sobomax@FreeBSD.org>
2001-12-19 20:20:23 +0000

    Don't filter libc_r on 5-CURRENT.

--- x11-toolkits/gdl/files/patch-ltmain.sh
+++ x11-toolkits/gdl/files/patch-ltmain.sh
@@ -1,19 +1,28 @@
 
 $FreeBSD$
 
---- ltmain.sh	2001/08/27 09:51:26	1.1
-+++ ltmain.sh	2001/08/27 09:51:42
-@@ -2408,6 +2408,9 @@
- 	  *-*-netbsd*)
- 	    # Don't link with libc until the a.out ld.so is fixed.
+--- ltmain.sh.orig	Sun Dec 16 07:16:09 2001
++++ ltmain.sh	Tue Dec 18 07:52:22 2001
+@@ -1052,7 +1052,7 @@
+ 	  esac
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	 *-*-openbsd* | *-*-freebsd*)
++	 *-*-openbsd* | *-*-freebsd[234]*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -2359,6 +2359,9 @@
+ 	    # problems, so we reset it completely
+ 	    verstring=""
  	    ;;
 +	  *-*-freebsd*)
 +	    # FreeBSD doesn't need this...
 +	    ;;
  	  *)
- 	    # Add libc to deplibs on all other systems if necessary.
- 	    if test "$build_libtool_need_lc" = "yes"; then
-@@ -4175,10 +4178,12 @@
+ 	    verstring="0.0"
+ 	    ;;
+@@ -4247,10 +4250,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
