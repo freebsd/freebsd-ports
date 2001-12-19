785dd7c14950a5412551afb8f78444564b33bcb7
petef <petef@FreeBSD.org>
2001-12-19 20:10:41 +0000

    Add p5-Class-ObjectTemplate 0.4, an optimized template builder base class.
    PR:		32640
    Submitted by:	Tony Maher <tonym@biolateral.com.au>

---
+++ devel/p5-Class-ObjectTemplate/files/patch-ObjectTemplate.pm
@@ -0,0 +1,12 @@
+$FreeBSD$
+
+--- ObjectTemplate.pm.orig	Sun Dec  9 13:38:22 2001
++++ ObjectTemplate.pm	Sun Dec  9 13:38:35 2001
+@@ -194,6 +194,7 @@
+ 
+ 1;
+ __END__
++
+ =head1 NAME
+ 
+ Class::ObjectTemplate - Perl extension for an optimized template
