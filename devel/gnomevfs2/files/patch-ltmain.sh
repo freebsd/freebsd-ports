d0fba3e0f107d49d6d81f245f106a4678e92c69f
Maxim Sobolev <sobomax@FreeBSD.org>
2001-12-20 06:19:02 +0000

    Backout previous change - it seems that new revision of the patch doesn't apply everywhere.
    Submitted by:	Todd Punderson <todd@doonga.net>

--- devel/gnomevfs2/files/patch-ltmain.sh
+++ devel/gnomevfs2/files/patch-ltmain.sh
@@ -1,28 +1,19 @@
 
 $FreeBSD$
 
---- ltmain.sh.orig	Sun Dec 16 07:16:09 2001
-+++ ltmain.sh	Tue Dec 18 07:52:22 2001
-@@ -1052,7 +1052,7 @@
- 	  esac
- 	 elif test "X$arg" = "X-lc_r"; then
- 	  case $host in
--	 *-*-openbsd* | *-*-freebsd*)
-+	 *-*-openbsd* | *-*-freebsd[234]*)
- 	    # Do not include libc_r directly, use -pthread flag.
- 	    continue
- 	    ;;
-@@ -2359,6 +2359,9 @@
- 	    # problems, so we reset it completely
- 	    verstring=""
+--- ltmain.sh	2001/08/27 09:51:26	1.1
++++ ltmain.sh	2001/08/27 09:51:42
+@@ -2408,6 +2408,9 @@
+ 	  *-*-netbsd*)
+ 	    # Don't link with libc until the a.out ld.so is fixed.
  	    ;;
 +	  *-*-freebsd*)
 +	    # FreeBSD doesn't need this...
 +	    ;;
  	  *)
- 	    verstring="0.0"
- 	    ;;
-@@ -4247,10 +4250,12 @@
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test "$build_libtool_need_lc" = "yes"; then
+@@ -4175,10 +4178,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
