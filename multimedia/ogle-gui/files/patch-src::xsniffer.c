0121d79ee011764feea55f037bac1bdb443eaf49
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2001-12-28 00:53:08 +0000

    Update to 0.8.2

--- multimedia/ogle-gui/files/patch-src::xsniffer.c
+++ multimedia/ogle-gui/files/patch-src::xsniffer.c
@@ -1,35 +1,38 @@
---- src/xsniffer.c.orig	Thu Sep  6 14:10:17 2001
-+++ src/xsniffer.c	Thu Oct 25 18:22:04 2001
-@@ -16,6 +16,10 @@
+--- src/xsniffer.c.orig	Sat Dec  8 14:28:37 2001
++++ src/xsniffer.c	Sat Dec  8 15:33:28 2001
+@@ -16,6 +16,11 @@
   * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
   */
  
 +#ifdef HAVE_CONFIG_H
 +#  include <config.h>
 +#endif
++#define HAVE_SYS_PARAM_H
 +
  #include <stdio.h>
  #include <stdlib.h>
  #include <unistd.h>
-@@ -35,6 +39,9 @@
- #include "xsniffer.h"
+@@ -35,6 +40,10 @@
  #include "callbacks.h"
+ #include "bindings.h"
  
 +#ifdef HAVE_SYS_PARAM_H
 +#include <sys/param.h>
 +#endif
- 
++
  extern int msgqid;
  
-@@ -66,7 +73,12 @@
+ 
+@@ -64,7 +73,13 @@
+   init_actions(nav2);
    
-   fprintf(stderr, "xsniff_mouse\n");
    while(1) {
 -    DVDNextEvent(nav2, &mev);
++
 +#if (defined(BSD) && (BSD >= 199306))
 +	if (DVDNextEventNonBlocking(nav2, &mev) != DVD_E_Ok)
 +#else
-+	if (DVDNextEvent(nav2, &mev) != DVD_E_Ok) 
++	if (DVDNextEvent(nav2, &mev) != DVD_E_Ok)
 +#endif
 +		pthread_exit(NULL);
      
