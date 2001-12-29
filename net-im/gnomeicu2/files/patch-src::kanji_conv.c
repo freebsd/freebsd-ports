1dd602ade37fc66ab47cdd358836be4f341e182a
Jacques Vidrine <nectar@FreeBSD.org>
2001-12-29 04:07:34 +0000

    Update 0.96.1 -> 0.98
    PR:		ports/33146
    Submitted by:	Jochem Kossen <j.kossen@home.nl>
    
    Fix the locale directories.
    Add a WITH_SOCKS5 knob while I'm here.

--- net-im/gnomeicu2/files/patch-src::kanji_conv.c
+++ net-im/gnomeicu2/files/patch-src::kanji_conv.c
@@ -1,7 +1,11 @@
---- src/kanji_conv.c.orig	Wed Jan  3 16:14:39 2001
-+++ src/kanji_conv.c	Thu Apr 19 15:51:21 2001
-@@ -32,3 +32,3 @@
+--- src/kanji_conv.c.orig	Mon Dec 24 12:51:08 2001
++++ src/kanji_conv.c	Mon Dec 24 12:51:21 2001
+@@ -30,7 +30,7 @@
+ #include <locale.h>
+ 
  #ifdef HAVE_ICONV
 -#include <iconv.h>
 +#include <giconv.h>
  #endif
+ 
+ #include "kanji_conv.h"
