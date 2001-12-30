f580057c885790681524f05282f4111c87d64157
petef <petef@FreeBSD.org>
2001-12-30 19:52:44 +0000

    - update to 0.5.8 - don't rewrite do-install, patch the distribution's Makefile instead
    PR:		33334
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

---
+++ devel/domc/files/patch-src::expatls.c
@@ -0,0 +1,10 @@
+--- src/expatls.c.orig	Thu Dec 20 19:00:30 2001
++++ src/expatls.c	Sun Dec 30 11:34:02 2001
+@@ -25,6 +25,7 @@
+ /* expatls.c - DOM_DocumentLS
+  */
+ 
++#include <sys/types.h>
+ #include <stdlib.h>
+ #include <limits.h>
+ #include <ctype.h>
