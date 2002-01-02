b5d2711efb7f472ff440adf5f0cd1b1e7a027424
clive <clive@FreeBSD.org>
2002-01-02 13:44:34 +0000

    Fix a wrong comment. I've introduced encoding range of Big5+ in rev 1.3.
    Found by: Yet another boring multibytes digging task within my company.

--- chinese/bitchx/files/patch-include::config.h
+++ chinese/bitchx/files/patch-include::config.h
@@ -61,7 +61,7 @@
 + *                            clive@FreeBSD.org
 + */
 +#ifdef WANT_BIG5
-+/* XXX I didn't check the encoding range of big5+. This is standard big5. */
++/* Encoding range below belongs to Big5+ */
 +#define is_big5_los(lo) (((char)0x40<=lo)&&(lo<=(char)0x7E))        /* standard */
 +#define is_big5_lox(lo) (((char)0x80<=lo)&&(lo<=(char)0xFE))        /* extended */
 +#define is_big5_hi(hi)  (((char)0x81<=hi)&&(hi<=(char)0xFE))
