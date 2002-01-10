b8c7d3a9754a74f1d2daa7568455c3237aedaf07
Trevor Johnson <trevor@FreeBSD.org>
2002-01-10 14:58:53 +0000

    Downloads via Sourceforge's Project Filelist Web pages are served from telia.dl.sourceforge.net (also known as sourceforge.aleron.net--it is in the USA, not Sweden), so I have placed it in the first position. According to Fenner's survey, projects.sourceforge.net and prdownload.sourceforge.net have been offline since late October and early November of 2001.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -227,6 +227,7 @@ MASTER_SITE_SOURCEWARE+= \
 	ftp://sunsite.org.uk/Mirrors/sourceware.cygnus.com/pub/%SUBDIR%/
 
 MASTER_SITE_SOURCEFORGE+= \
+	http://telia.dl.sourceforge.net/%SUBDIR%/ \
 	http://prdownloads.sourceforge.net/%SUBDIR%/ \
 	ftp://ftp3.sourceforge.net/pub/sourceforge/%SUBDIR%/ \
 	http://ftp3.sourceforge.net/%SUBDIR%/ \
@@ -234,8 +235,6 @@ MASTER_SITE_SOURCEFORGE+= \
 	http://ftp1.sourceforge.net/%SUBDIR%/ \
 	ftp://ftp2.sourceforge.net/pub/sourceforge/%SUBDIR%/ \
 	http://ftp2.sourceforge.net/%SUBDIR%/ \
-	ftp://prdownload.sourceforge.net/pub/%SUBDIR%/ \
-	ftp://projects.sourceforge.net/pub/%SUBDIR%/ \
 	ftp://ftp.kddlabs.co.jp/sourceforge/%SUBDIR%/ \
 	ftp://ftp.chg.ru/pub/sourceforge/%SUBDIR%/
 
