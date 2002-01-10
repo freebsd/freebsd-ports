a21212ed3e1c566f603d8e9b4df6da5b963d736f
Maxim Sobolev <sobomax@FreeBSD.org>
2002-01-10 12:10:07 +0000

    Update to 1.6.5.

---
+++ deskutils/gnucash/files/patch-ltmain.sh
@@ -0,0 +1,28 @@
+
+$FreeBSD$
+
+--- ltmain.sh.orig	Mon Nov 12 01:53:44 2001
++++ ltmain.sh	Thu Jan 10 13:04:25 2002
+@@ -2408,6 +2408,9 @@
+ 	  *-*-netbsd*)
+ 	    # Don't link with libc until the a.out ld.so is fixed.
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test "$build_libtool_need_lc" = "yes"; then
+@@ -4175,10 +4178,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
