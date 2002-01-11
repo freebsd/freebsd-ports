b0bb492427f4ce934b3a0b6fc1e0ca4107c251cb
Patrick Li <pat@FreeBSD.org>
2002-01-11 03:18:37 +0000

    Update to 0.7.0
    PR:		33762
    Submitted by:	Oliver Lehmann <lehmann@ans-netz.de>

--- x11/launch.app/files/patch-execute.c
+++ x11/launch.app/files/patch-execute.c
@@ -1,22 +1,18 @@
-
-$FreeBSD$
-
---- execute.c	2001/08/20 09:08:11	1.1
-+++ execute.c	2001/08/20 09:09:29
-@@ -157,13 +157,13 @@
+--- execute.c.orig	Thu Oct 18 13:56:26 2001
++++ execute.c	Thu Jan 10 13:48:12 2002
+@@ -161,13 +161,13 @@
  	pid = fork ();
  	if (pid == 0)
  	{
 -		if (setuid (pw->pw_gid))
 +		if (setgid (pw->pw_gid))
  		{
- 			fprintf (stderr, "unable to set group id\n");
+ //			fprintf (stderr, "unable to set group id\n");
+ 			SMRunAttentionPanel (wm_screen, LaunchPanel->window,
+           "Command Execution", "Unable to set group ID.", NULL, NULL, "Ok");
  		}
 -		if (setgid (pw->pw_uid))
 +		if (setuid (pw->pw_uid))
  		{
--			fprintf (stderr, "unable to set group id\n");
-+			fprintf (stderr, "unable to set user id\n");
- 		}
- 		setenv ("HOME", pw->pw_dir, 1);
- 		setenv ("USER", pw->pw_name, 1);
+ //			fprintf (stderr, "unable to set user id\n");
+ 			SMRunAttentionPanel (wm_screen, LaunchPanel->window,
