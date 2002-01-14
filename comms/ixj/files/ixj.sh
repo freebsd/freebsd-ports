ed1e1f00ba0f9ee9e18ef7043823836fc706f128
Roger Hardiman <roger@FreeBSD.org>
2002-01-14 14:52:46 +0000

    Add FreeBSD port of the ixj driver for Quicknet LineJack and PhoneJack cards. These ISA and PCI telephony cards allow VoIP and Video Conferencing using the OpenH323 suite of applications with a standard telephone handset (including making the phone ring for incomming calls, allowing dialing via the telephone and using the phone's speaker and mic for the audio (freeing up the sound card). They also feature a hardware G.723.1 audio codec making it easier to talk to NetMeeting and H.323 users.

---
+++ comms/ixj/files/ixj.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+$PREFIX=%%PREFIX%%
+
+case "$1" in
+  restart)
+    $0 stop
+    sleep 2
+    $0 start
+    ;;
+  stop)
+    kldstat -n ixj 2>/dev/null >/dev/null && kldunload ixj
+    ;;
+  start)
+    echo -n ' ixj'
+    rm -f /dev/phone0 /dev/phone1 /dev/phone2 /dev/phone3
+    mknod /dev/phone0 c 155 0
+    mknod /dev/phone1 c 155 1
+    mknod /dev/phone2 c 155 2
+    mknod /dev/phone3 c 155 3
+
+    kldstat -n ixj 2>/dev/null >/dev/null || kldload $PREFIX/modules/ixj.ko
+    ;;
+esac
