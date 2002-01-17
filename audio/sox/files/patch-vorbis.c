a31ac75b65bb73cc90d28dc66267a65c331181f7
Christian Weisgerber <naddy@FreeBSD.org>
2002-01-17 19:22:41 +0000

    * Upgrade to 12.17.3.  Changes in this version include:   - integrated GSM support   - new soxmix program
    * Bump libvorbis version number in LIB_DEPENDS.
    * Use new Ogg Vorbis RC3 encoder API.
    
    * Replace incomplete list of supported formats and effects with a general
      blurb in DESCR.
    * Update home page URL in DESCR.
    
    Reviewed by:	Dan Nelson <dnelson@emsphone.com>

---
+++ audio/sox/files/patch-vorbis.c
@@ -0,0 +1,17 @@
+
+$FreeBSD$
+
+--- vorbis.c.orig	Thu Jan 17 01:11:39 2002
++++ vorbis.c	Thu Jan 17 01:13:07 2002
+@@ -337,9 +337,8 @@
+ 	fprintf(stdout, "Channels: %d  Rate: %ld\n", ft->info.channels,
+ 		rate);
+ 
+-	/* Set encoding to average bit rate of 128kbps with no min or max */
+-	vorbis_encode_init(&ve->vi, ft->info.channels, ft->info.rate,
+-			   -1, 128000, -1);
++	/* Set encoding to average bit rate of 112kbps VBR */
++	vorbis_encode_init_vbr(&ve->vi, ft->info.channels, ft->info.rate, 0.3f);
+ 
+ 	vorbis_analysis_init(&ve->vd, &ve->vi);
+ 	vorbis_block_init(&ve->vd, &ve->vb);
