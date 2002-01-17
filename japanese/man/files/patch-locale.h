3b7845cfc3397def43248432cc26601412d48fbe
kuriyama <kuriyama@FreeBSD.org>
2002-01-17 07:29:50 +0000

    Fix a bug that jman always searched "ja" subdirectory ignoring LC_ALL, LC_CTYPE, and LANG.
    Submitted by:	horikawa@jp.FreeBSD.org, MORI Kouji <moriko@hh.iij4u.or.jp>

--- japanese/man/files/patch-locale.h
+++
@@ -1,9 +0,0 @@
---- man/locale.h	2001/07/17 01:42:54
-+++ man/locale.h	2001/12/13 02:17:39
-@@ -20,5 +20,5 @@
- char *directive(int);
- int try_section_ml(char *, char *, char *, int, int);
- int try_section(char *, char *, char *, int);
--int man_ml(char *);
-+int man_ml(char *, int);
- int man(char *);
