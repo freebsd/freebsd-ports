ccc7312692d584a7f7f9d561f85d3790c0c63972
sobomax <sobomax@FreeBSD.org>
2002-01-18 13:04:05 +0000

    Update to 1.3.12 and merge in dlsym() patches from gtk12 port.

---
+++ devel/glib20/files/patch-gmodule::gmodule-dl.c
@@ -0,0 +1,23 @@
+
+$FreeBSD$
+
+--- gmodule/gmodule-dl.c.orig	Sat Feb 17 08:28:07 2001
++++ gmodule/gmodule-dl.c	Fri Jan 18 09:48:45 2002
+@@ -104,6 +104,7 @@
+ static gpointer
+ _g_module_self (void)
+ {
++#ifndef __FreeBSD__
+   gpointer handle;
+   
+   /* to query symbols from the program itself, special link options
+@@ -115,6 +116,9 @@
+     g_module_set_error (fetch_dlerror (TRUE));
+   
+   return handle;
++#else
++  return RTLD_DEFAULT;
++#endif
+ }
+ 
+ static void
