5ba6110a0862e1085cc7566781446bfc8691d3aa
naddy <naddy@FreeBSD.org>
2002-01-19 15:37:11 +0000

    proper threads fix from Xiph CVS

--- audio/vorbis-tools/files/patch-ogg123::buffer.c
+++ audio/vorbis-tools/files/patch-ogg123::buffer.c
@@ -2,16 +2,30 @@
 $FreeBSD$
 
 --- ogg123/buffer.c.orig	Thu Dec 20 01:24:53 2001
-+++ ogg123/buffer.c	Wed Jan 16 03:26:15 2002
-@@ -88,9 +88,11 @@
++++ ogg123/buffer.c	Sat Jan 19 16:24:44 2002
+@@ -85,12 +85,6 @@
+   buf_t *buf = (buf_t *)arg;
  
-   /* Cleanup thread data structures */
-   pthread_mutex_unlock(&buf->mutex);
-+#if 0	/* KLUDGE: disabling this keeps ogg123 from crashing */
-   pthread_mutex_destroy(&buf->mutex);
-   pthread_cond_destroy(&buf->playback_cond);
-   pthread_cond_destroy(&buf->write_cond);
-+#endif
+   DEBUG("Enter buffer_thread_cleanup");
+-
+-  /* Cleanup thread data structures */
+-  pthread_mutex_unlock(&buf->mutex);
+-  pthread_mutex_destroy(&buf->mutex);
+-  pthread_cond_destroy(&buf->playback_cond);
+-  pthread_cond_destroy(&buf->write_cond);
  }
  
+ 
+@@ -430,6 +424,12 @@
+ void buffer_destroy (buf_t *buf)
+ {
+   DEBUG("buffer_destroy");
++
++  /* Cleanup pthread variables */
++  pthread_mutex_destroy(&buf->mutex);
++  pthread_cond_destroy(&buf->write_cond);
++  pthread_cond_destroy(&buf->playback_cond);
++  
+   free(buf);
+ }
  
