fb66e46a1a2127a7321b67265554e83f6eda0740
sobomax <sobomax@FreeBSD.org>
2002-01-21 11:45:56 +0000

    Update to 2.0.6.

--- print/freetype2/files/patch-include::freetype::config::ftoption.h
+++ print/freetype2/files/patch-include::freetype::config::ftoption.h
@@ -1,14 +1,14 @@
 
 $FreeBSD$
 
---- include/freetype/config/ftoption.h	2001/07/03 08:51:20	1.1
-+++ include/freetype/config/ftoption.h	2001/07/03 08:51:56
-@@ -321,7 +321,7 @@
+--- include/freetype/config/ftoption.h.orig	Mon Jan  7 12:05:34 2002
++++ include/freetype/config/ftoption.h	Mon Jan 21 12:37:05 2002
+@@ -375,7 +375,7 @@
    /* By undefining this, you will only compile the code necessary to load  */
    /* TrueType glyphs without hinting.                                      */
    /*                                                                       */
--#undef  TT_CONFIG_OPTION_BYTECODE_INTERPRETER
-+#define  TT_CONFIG_OPTION_BYTECODE_INTERPRETER
+-#undef TT_CONFIG_OPTION_BYTECODE_INTERPRETER
++#define TT_CONFIG_OPTION_BYTECODE_INTERPRETER
  
  
    /*************************************************************************/
