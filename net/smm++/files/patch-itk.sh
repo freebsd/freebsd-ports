c604d1d7d400f2448736293b945dfb187b27d103
FUJISHIMA Satsuki <sf@FreeBSD.org>
2002-01-22 22:13:49 +0000

    update to 5.1. change MASTER_SITES.
    PR:		34154
    Submitted by:	maintainer

--- net/smm++/files/patch-itk.sh
+++ net/smm++/files/patch-itk.sh
@@ -1,46 +1,11 @@
---- ../../smm5r0/Install.unix/itk.sh	Mon Mar 19 08:46:07 2001
-+++ itk.sh	Sun Jul 15 17:01:08 2001
-@@ -1,32 +1,16 @@
- #!/bin/sh
+--- itk.sh.orig	Tue Jan 22 18:48:12 2002
++++ itk.sh	Tue Jan 22 18:48:48 2002
+@@ -4,7 +4,7 @@
+ #
  
- # all possible itkwish executables
--itkarray=(itkwish3.1 itkwish3 itkwish30 itkwish)
+ # all possible itkwish executables 
+-itkarray="itkwish3.1 itkwish3 itkwish30 itkwish3.0 itkwish" 
 +itkarray="itkwish3.1 itkwish3 itkwish30 itkwish3.0 itkwish wish8.3 wish8.2"
  
--# $PATH exploded as an array
--explode=(`echo $PATH | sed 's/:/ /g'`)
--
--element_count=${#explode[@]}
--i=0
--found=0
--while [ $i -lt ${element_count} ]
--do
--    second_count=${#itkarray[@]}
--    x=0
--    while [ $x -lt ${second_count} ]
--    do
--        if [ -x ${explode[$i]}/${itkarray[$x]} ]; then
--            smm=${explode[$i]}/${itkarray[$x]}
--	    found=1
--            break
--        fi
--        let "x=$x + 1"
--    done
--    let "i = $i + 1"
-+set $itkarray
-+while [ ! -z "$1" ]; do
-+	smm=`which $1`
-+	if [ $? -eq 0 ]; then
-+		echo ${smm}
-+		exit 0
-+	fi
-+	shift
- done
--if [ ${found} -eq 1 ] ; then
--    echo ${smm}
--    exit 0
--else
--    exit 1
--fi
-+
-+exit 1
+ set $itkarray 
+ while [ ! -z "$1" ]; do 
