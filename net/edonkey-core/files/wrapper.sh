841a9787b49526343fe6d4b4b5ad4e2cf024b59c
lioux <lioux@FreeBSD.org>
2002-01-22 15:47:28 +0000

    o New port edonkey-core version 0.2000.15.16.58.0.2: eDonkey2000   'core' command line client o README.FreeBSD worked with pat
    Reviewed by:	pat

---
+++ net/edonkey-core/files/wrapper.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PREFIX="%%PREFIX%%"
+PROGRAM="${0}"
+DIRNAME=${HOME}/.edonkey2000-core
+
+if [ ! -d ${DIRNAME} ]
+then
+	mkdir -p ${DIRNAME}
+fi
+
+if [ ! -h ${DIRNAME}/${PROGRAM##*/} ]
+then
+	ln -sf ${PREFIX}/bin/${PROGRAM##*/}-real ${DIRNAME}/${PROGRAM##*/}
+fi
+
+echo " ${*} " | grep " \-createdironly " ||
+{
+	cd ${DIRNAME} &&
+	echo Running under ${DIRNAME} &&
+	exec ./${PROGRAM##*/} ${*} ||
+	echo PROBLEM. Cannot create directory ${DIRNAME}, make sure you have the proper permissions to create it and try again
+}
