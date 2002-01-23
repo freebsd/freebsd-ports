7595022d6ffd9473b13d2a6e6093d83a81e63adc
sobomax <sobomax@FreeBSD.org>
2002-01-23 15:22:19 +0000

    Update to 1.0.4.

--- devel/gnomevfs/files/patch-ltmain.sh
+++ devel/gnomevfs/files/patch-ltmain.sh
@@ -1,19 +1,27 @@
 
 $FreeBSD$
 
---- ltmain.sh	2001/08/27 09:51:26	1.1
-+++ ltmain.sh	2001/08/27 09:51:42
-@@ -2408,6 +2408,9 @@
- 	  *-*-netbsd*)
- 	    # Don't link with libc until the a.out ld.so is fixed.
+--- ltmain.sh.orig	Sun Jan 20 16:11:07 2002
++++ ltmain.sh	Tue Jan 22 13:34:20 2002
+@@ -1060,6 +1060,7 @@
+ 
+       -module)
+ 	module=yes
++	build_old_libs=no
+ 	continue
+ 	;;
+ 
+@@ -2442,6 +2443,9 @@
+ 	  *-*-openbsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
  	    ;;
 +	  *-*-freebsd*)
 +	    # FreeBSD doesn't need this...
 +	    ;;
  	  *)
  	    # Add libc to deplibs on all other systems if necessary.
- 	    if test "$build_libtool_need_lc" = "yes"; then
-@@ -4175,10 +4178,12 @@
+ 	    if test $build_libtool_need_lc = "yes"; then
+@@ -4208,10 +4212,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
