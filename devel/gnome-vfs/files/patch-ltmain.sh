7a22e9429067686fb1a61ace6a62bfca18364b18
sobomax <sobomax@FreeBSD.org>
2002-01-24 15:09:56 +0000

    Ensure that the -pthread flag isn't omitted by libtool. I hope this would solve problems people having on 4-STABLE after yesterday's GNOME megaupgrade.

--- devel/gnome-vfs/files/patch-ltmain.sh
+++ devel/gnome-vfs/files/patch-ltmain.sh
@@ -2,16 +2,22 @@
 $FreeBSD$
 
 --- ltmain.sh.orig	Sun Jan 20 16:11:07 2002
-+++ ltmain.sh	Tue Jan 22 13:34:20 2002
-@@ -1060,6 +1060,7 @@
++++ ltmain.sh	Thu Jan 24 17:07:06 2002
+@@ -1058,8 +1058,13 @@
+ 	continue
+ 	;;
  
++      -pthread)
++	compiler_flags="$compiler_flags -pthread"
++	;;
++
        -module)
  	module=yes
 +	build_old_libs=no
  	continue
  	;;
  
-@@ -2442,6 +2443,9 @@
+@@ -2442,6 +2447,9 @@
  	  *-*-openbsd*)
  	    # Do not include libc due to us having libc/libc_r.
  	    ;;
@@ -21,7 +27,7 @@ $FreeBSD$
  	  *)
  	    # Add libc to deplibs on all other systems if necessary.
  	    if test $build_libtool_need_lc = "yes"; then
-@@ -4208,10 +4212,12 @@
+@@ -4208,10 +4216,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
