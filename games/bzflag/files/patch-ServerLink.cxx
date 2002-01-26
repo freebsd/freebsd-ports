1198b0d00e9b2f324b62b5119d47f22916b6b59f
nsayer <nsayer@FreeBSD.org>
2002-01-26 17:42:17 +0000

    Update to e.4. USB Joystick functionality has been merged. Huzzah!
    PR:		ports/34303
    Submitted by:	EliteNecrosan@hotmail.com

--- games/bzflag/files/patch-ServerLink.cxx
+++ games/bzflag/files/patch-ServerLink.cxx
@@ -1,11 +1,11 @@
---- src/bzflag/ServerLink.cxx.orig	Sun Apr 15 13:32:03 2001
-+++ src/bzflag/ServerLink.cxx	Sun Apr 15 13:32:20 2001
-@@ -761,7 +761,7 @@
+--- src/bzflag/ServerLink.cxx.orig	Sat Jan 26 09:26:30 2002
++++ src/bzflag/ServerLink.cxx	Sat Jan 26 09:25:55 2002
+@@ -781,7 +781,7 @@
    if ((urecvfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
  	return; // we cannot comply
    }
 -  for (int portno=17200; portno < 65000; portno++) {
 +  for (int portno=49152; portno < 65535; portno++) {
-   	bzero((unsigned char *)&serv_addr, sizeof(serv_addr)); 
+   	::memset((unsigned char *)&serv_addr, 0, sizeof(serv_addr));
    	serv_addr.sin_family = AF_INET;
    	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
