2ba2fce29c90d33ac9baeb573d5a602e8bfb865f
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2002-01-29 15:34:26 +0000

    Add gorua, Gtk+ on Ruby User Agent for 2ch BBS.

---
+++ japanese/gorua/files/patch-goRua.rb
@@ -0,0 +1,8 @@
+--- goRua.rb.orig	Mon Jan 28 12:27:32 2002
++++ goRua.rb	Mon Jan 28 20:07:13 2002
+@@ -1,4 +1,4 @@
+-#!/usr/bin/ruby -Ke
++#!%%RUBY_WITHOUT_SUFFIX%% -Ke
+ # goRua -- Gtk+ on Ruby User Agent for 2ch version 0.11d
+ # ½Õ»³ À¬¸ã haruyama@unixuser.org
+ # $Id: goRua.rb,v 1.45 2002/01/28 03:27:32 haruyama Exp $
