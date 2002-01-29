5ab4ca8f903bb093f1c13527b2bf110eeced5df3
Maxim Sobolev <sobomax@FreeBSD.org>
2002-01-29 00:21:14 +0000

    Update to 0.5.1.
    Submitted by:	Anders Andersson <anders@hack.org>
    		Joe Clarke <marcus@marcuscom.com>

---
+++ deskutils/mrproject/files/patch-ltmain.sh
@@ -0,0 +1,39 @@
+--- ltmain.sh.orig	Sun Jan 20 16:11:07 2002
++++ ltmain.sh	Thu Jan 24 17:07:06 2002
+@@ -1058,8 +1058,13 @@
+ 	continue
+ 	;;
+ 
++      -pthread)
++	compiler_flags="$compiler_flags -pthread"
++	;;
++
+       -module)
+ 	module=yes
++	build_old_libs=no
+ 	continue
+ 	;;
+ 
+@@ -2442,6 +2447,9 @@
+ 	  *-*-openbsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test $build_libtool_need_lc = "yes"; then
+@@ -4208,10 +4216,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
