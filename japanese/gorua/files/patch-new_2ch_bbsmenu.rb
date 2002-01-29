ddb020909fdf33c868e5b4c20169cd2404987c3c
nobutaka <nobutaka@FreeBSD.org>
2002-01-29 15:34:26 +0000

    Add gorua, Gtk+ on Ruby User Agent for 2ch BBS.

---
+++ japanese/gorua/files/patch-new_2ch_bbsmenu.rb
@@ -0,0 +1,8 @@
+--- new_2ch_bbsmenu.rb.orig	Tue Jan 22 15:03:21 2002
++++ new_2ch_bbsmenu.rb	Mon Jan 28 20:07:13 2002
+@@ -1,4 +1,4 @@
+-#!/usr/bin/ruby -Ke
++#!%%RUBY_WITHOUT_SUFFIX%% -Ke
+ # Thanks:: >>63
+ # $Id: new_2ch_bbsmenu.rb,v 1.2 2002/01/22 06:03:21 haruyama Exp $
+ # このファイルはGPLで配付されています.
