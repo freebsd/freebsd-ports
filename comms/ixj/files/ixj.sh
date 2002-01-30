665f6820347250f01b9bfbc34c68e29b2d291257
Roger Hardiman <roger@FreeBSD.org>
2002-01-30 15:46:25 +0000

    Set the right permissions on the /dev entries

--- comms/ixj/files/ixj.sh
+++ comms/ixj/files/ixj.sh
@@ -18,6 +18,10 @@ case "$1" in
     mknod /dev/phone1 c 155 1
     mknod /dev/phone2 c 155 2
     mknod /dev/phone3 c 155 3
+    chmod 660 /dev/phone0
+    chmod 660 /dev/phone1
+    chmod 660 /dev/phone2
+    chmod 660 /dev/phone3
 
     kldstat -n ixj 2>/dev/null >/dev/null || kldload $PREFIX/modules/ixj.ko
     ;;
