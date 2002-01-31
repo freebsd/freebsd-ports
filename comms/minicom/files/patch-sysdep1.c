fd433cb2ea27607ce01175fd9ee5a21a8d7efb5b
Anders Nordby <anders@FreeBSD.org>
2002-01-31 01:03:58 +0000

    Update to version 2.00.0. Unforbid because known vulnerabilities have been fixed. Take over maintainership.
    Approved by:	obrien (previous maintainer)

--- comms/minicom/files/patch-sysdep1.c
+++ comms/minicom/files/patch-sysdep1.c
@@ -1,27 +1,53 @@
---- sysdep1.c.orig	Wed Mar 15 08:45:03 2000
-+++ sysdep1.c	Fri Nov 10 09:26:24 2000
-@@ -69 +69 @@
+--- src/sysdep1.c.orig	Sat Jun 24 18:01:29 2000
++++ src/sysdep1.c	Sun Jan 27 19:15:37 2002
+@@ -71,12 +71,12 @@
+ void m_setrts(fd)
+ int fd;
+ {
 -#if defined(TIOCM_RTS) && defined(TIOCMODG)
 +#if defined(TIOCM_RTS) && defined(TIOCMGET)
-@@ -72 +72 @@
+   int mcs=0;
+ 
 -  ioctl(fd, TIOCMODG, &mcs);
 +  ioctl(fd, TIOCMGET, &mcs);
-@@ -74 +74 @@
+   mcs |= TIOCM_RTS;
 -  ioctl(fd, TIOCMODS, &mcs);
 +  ioctl(fd, TIOCMSET, &mcs);
-@@ -180 +180 @@
+ #endif
+ #ifdef _COHERENT
+   ioctl(fd, TIOCSRTS, 0);
+@@ -182,10 +182,10 @@
+ int m_getdcd(fd)
+ int fd;
+ {
 -#ifdef TIOCMODG
 +#ifdef TIOCMGET
-@@ -183 +183 @@
+   int mcs=0;
+    
 -  ioctl(fd, TIOCMODG, &mcs);
 +  ioctl(fd, TIOCMGET, &mcs);
-@@ -221,2 +221,2 @@
+   return(mcs & TIOCM_CAR ? 1 : 0);
+ #else
+   (void)fd;
+@@ -223,8 +223,8 @@
+   ioctl(fd, TIOCLGET, &lsw);
+ #  endif
+ #endif
 -#ifdef TIOCMODG
 -  ioctl(fd, TIOCMODG, &m_word);
 +#ifdef TIOCMGET
 +  ioctl(fd, TIOCMGET, &m_word);
-@@ -243,2 +243,2 @@
+ #endif
+ }
+ 
+@@ -245,8 +245,8 @@
+   ioctl(fd, TIOCLSET, &lsw);
+ #  endif
+ #endif
 -#ifdef TIOCMODS
 -  ioctl(fd, TIOCMODS, &m_word);
 +#ifdef TIOCMSET
 +  ioctl(fd, TIOCMSET, &m_word);
+ #endif
+ }
+ 
