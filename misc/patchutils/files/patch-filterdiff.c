7817bb65fd03ac1c350d0d3e93d2f2ba39c94665
ijliao <ijliao@FreeBSD.org>
2002-02-01 11:26:25 +0000

    upgrade to 0.2.8 change mastersite & WWW

--- misc/patchutils/files/patch-filterdiff.c
+++ misc/patchutils/files/patch-filterdiff.c
@@ -1,10 +1,22 @@
---- filterdiff.c.orig	Mon Jul 23 17:52:37 2001
-+++ filterdiff.c	Mon Jul 23 17:52:53 2001
-@@ -22,6 +22,7 @@
+--- filterdiff.c.orig	Fri Feb  1 19:21:43 2002
++++ filterdiff.c	Fri Feb  1 19:21:55 2002
+@@ -24,6 +24,9 @@
  #include "config.h"
  #endif
  
-+#include <sys/types.h>
- #include <fnmatch.h>
++#ifdef HAVE_SYS_TYPES_H
++# include <sys/types.h> // for ssize_t
++#endif /* HAVE_SYS_TYPES_H */
+ #ifdef HAVE_ERROR_H
+ # include <error.h>
+ #endif /* HAVE_ERROR_H */
+@@ -34,9 +37,6 @@
  #include <stdio.h>
  #include <stdlib.h>
+ #include <string.h>
+-#ifdef HAVE_SYS_TYPES_H
+-# include <sys/types.h> // for ssize_t
+-#endif /* HAVE_SYS_TYPES_H */
+ #include <time.h>
+ 
+ #include "util.h"
