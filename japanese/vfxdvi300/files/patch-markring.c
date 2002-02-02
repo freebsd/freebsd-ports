c7adbe5054f2c2468ea1b2334a4f827845a23330
Yoshio MITA <mita@FreeBSD.org>
2002-02-02 00:20:19 +0000

    MAINTAINER's fix. Use <stdlib.h> instead of <malloc.h>
    Submitted by:	Seigo Tanimura <tanimura@r.dl.itc.u-tokyo.ac.jp>

---
+++ japanese/vfxdvi300/files/patch-markring.c
@@ -0,0 +1,12 @@
+--- markring.c.org	Sat Feb  2 08:07:42 2002
++++ markring.c	Sat Feb  2 08:06:15 2002
+@@ -8,7 +8,7 @@
+ 
+ #include "markring.h"
+ #include <stdio.h>
+-#include <malloc.h>
++#include <stdlib.h>
+ 
+ void
+   MakeRingNull(ring)
+
