961873521f76236a97f67859ac85a5e737044674
Patrick Li <pat@FreeBSD.org>
2002-02-02 04:53:50 +0000

    Update to 1.5 Give maintainership to submitter
    PR:		34527
    Submitted by:	Anders Andersson <anders@hack.org>

---
+++ x11/gdkxft/files/patch-src::gdkxft.c
@@ -0,0 +1,11 @@
+--- src/gdkxft.c.orig	Fri Feb  1 13:20:17 2002
++++ src/gdkxft.c	Fri Feb  1 13:20:39 2002
+@@ -32,7 +32,7 @@
+ #include <gdk/gdk.h>
+ #include <gdk/gdkprivate.h>
+ 
+-#include <iconv.h>
++#include <giconv.h>
+ 
+ #include "realfuncs.h"
+ 
