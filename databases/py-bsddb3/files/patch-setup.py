ead33bb6a4b7a047e5499bbc7b554f741146850e
wjv <wjv@FreeBSD.org>
2002-02-06 13:38:23 +0000

    - Add a patch to get this port to link against libdb3, since it is dependent   upon databases/db3.
    Approved by:	No response from maintainer in 24h

--- databases/py-bsddb3/files/patch-setup.py
+++ databases/py-bsddb3/files/patch-setup.py
@@ -1,6 +1,17 @@
---- setup.py.orig	Mon Apr  9 12:29:06 2001
-+++ setup.py	Mon Apr  9 12:29:15 2001
-@@ -90,7 +90,7 @@
+--- setup.py.orig	Fri Sep 14 00:15:30 2001
++++ setup.py	Tue Feb  5 12:37:16 2002
+@@ -68,8 +68,8 @@
+     # figure out from the base setting where the lib and .h are
+     if not incdir: incdir = os.path.join(BERKELEYDB_DIR, 'include')
+     if not libdir: libdir = os.path.join(BERKELEYDB_DIR, 'lib')
+-    if not '-ldb' in LIBS:
+-        libname = ['db']
++    if not '-ldb3' in LIBS:
++        libname = ['db3']
+     else:
+         libname = []
+     utils = []
+@@ -93,7 +93,7 @@
      if not status and string.find(results, 'libdb.') >= 0:
          static = 1
  
