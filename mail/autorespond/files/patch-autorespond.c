0b847f7b91ac68e22170e664f26fa633a43a6923
roam <roam@FreeBSD.org>
2002-02-07 10:32:26 +0000

    Update autorespond to 2.0.2, adding a patch to comment out a definition of strcasestr(), which is already provided by our libc's <string.h>.
    Take over maintainership from Neil Blakey-Milner with many
    thanks for his work on this and other ports!
    
    PR:		34024
    Submitted by:	Oliver Lehmann <lehmann@ans-netz.de>
    Approved by:	nbm (former maintainer)

---
+++ mail/autorespond/files/patch-autorespond.c
@@ -0,0 +1,19 @@
+--- autorespond.c	Thu Feb  7 12:07:44 2002
++++ autorespond.c	Thu Feb  7 12:07:59 2002
+@@ -377,6 +377,7 @@
+ /*********************************************************
+ ** find string in string - ignore case **/
+ 
++/*
+ char *strcasestr( char *_s1, char *_s2 )
+ {
+ 	char *s1;
+@@ -399,7 +400,7 @@
+ 	else
+ 		return _s1 + (ptr - s1);
+ }
+-
++*/
+ 
+ 
+ 
