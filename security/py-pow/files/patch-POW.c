63b45c1017653defa70572dfd47ae623ad4d1f85
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-02-11 14:20:24 +0000

    add py-pow 0.6.1 Python OpenSSL Wrappers

---
+++ security/py-pow/files/patch-POW.c
@@ -0,0 +1,11 @@
+--- POW.c.orig	Mon Feb 11 22:12:50 2002
++++ POW.c	Mon Feb 11 22:13:01 2002
+@@ -34,7 +34,7 @@
+ /*                                                                           */
+ /*****************************************************************************/
+ 
+-#include <python2.1/Python.h>
++#include <Python.h>
+ 
+ #include <openssl/crypto.h>
+ #include <openssl/rand.h>
