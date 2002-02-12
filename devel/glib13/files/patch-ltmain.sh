7e1cbe2a8b51859bf33f810487f01a8c634beed5
sobomax <sobomax@FreeBSD.org>
2002-02-12 08:31:19 +0000

    - Update to 1.3.13 - the version shipped with the GNOME2 alpha 2; - switch to .bz2 distfile; - move distfile into gnome2 DIST_SUBDIR.

--- devel/glib13/files/patch-ltmain.sh
+++ devel/glib13/files/patch-ltmain.sh
@@ -1,9 +1,26 @@
 
 $FreeBSD$
 
---- ltmain.sh	2001/08/27 09:51:26	1.1
-+++ ltmain.sh	2001/08/27 09:51:42
-@@ -2408,6 +2408,9 @@
+--- ltmain.sh.orig	Wed Jan 23 11:38:40 2002
++++ ltmain.sh	Sun Feb  3 01:44:17 2002
+@@ -1037,8 +1037,16 @@
+ 	continue
+ 	;;
+ 
++      -pthread)
++	compile_command="$compile_command -pthread"
++	finalize_command="$finalize_command -pthread"
++	compiler_flags="$compiler_flags -pthread"
++	continue
++	;;
++
+       -module)
+ 	module=yes
++	build_old_libs=no
+ 	continue
+ 	;;
+ 
+@@ -2412,6 +2420,9 @@
  	  *-*-netbsd*)
  	    # Don't link with libc until the a.out ld.so is fixed.
  	    ;;
@@ -13,7 +30,7 @@ $FreeBSD$
  	  *)
  	    # Add libc to deplibs on all other systems if necessary.
  	    if test "$build_libtool_need_lc" = "yes"; then
-@@ -4175,10 +4178,12 @@
+@@ -4179,10 +4190,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
