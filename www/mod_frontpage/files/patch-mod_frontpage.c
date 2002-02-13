e40d7bb5e81ba6a9d19892d8d859f3cd8abfccc2
Martin Blapp <mbr@FreeBSD.org>
2002-02-13 22:28:59 +0000

    Fix typo

--- www/mod_frontpage/files/patch-mod_frontpage.c
+++ www/mod_frontpage/files/patch-mod_frontpage.c
@@ -1,5 +1,14 @@
 --- mod_frontpage.c	Tue Feb  5 13:56:31 2002
 +++ mod_frontpage.c	Tue Feb  5 18:01:27 2002
+@@ -124,7 +124,7 @@
+ 			    "FrontPage disabled for server %s:%d\n",
+ 			    s->server_hostname, s->port);
+ 		/*
+-		 * As default, enable the mod_frontpage cgi administration
++		 * As default, disable the mod_frontpage cgi administration
+ 		 */
+ 		if (c->admindisabled == -1)
+ 			c->admindisabled = TRUE;
 @@ -491,10 +491,6 @@
  		return log_scripterror (r, c, FORBIDDEN, APLOG_ERR|APLOG_NOERRNO,
  		    "fpEXEC stub writable by group or others");
