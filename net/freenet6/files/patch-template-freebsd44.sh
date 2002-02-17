030569867387f67fed01238ba00e4e3e701a95aa
James E. Housley <jeh@FreeBSD.org>
2002-02-17 03:16:35 +0000

    With this patch it is possible to run the Freenet6 service on freebsd 4.5
    PR:		35016
    Submitted by:	Edwin Groothuis <edwin@mavetju.org> MAINTAINER

--- net/freenet6/files/patch-template-freebsd44.sh
+++ net/freenet6/files/patch-template-freebsd44.sh
@@ -1,9 +1,11 @@
---- template/freebsd44.sh.orig	Tue Aug 28 04:15:58 2001
-+++ template/freebsd44.sh	Thu Oct 11 15:22:00 2001
-@@ -106,6 +106,9 @@
+--- template/freebsd44.sh.orig	Thu Nov  8 03:54:41 2001
++++ template/freebsd44.sh	Sun Feb 17 12:18:47 2002
+@@ -117,7 +117,10 @@
+ 
     # Delete any default IPv6 route first
-    Exec $route delete -inet6 default
-    Exec $route add -inet6 default  -interface $TSP_TUNNEL_INTERFACE
+    ExecNoCheck $route delete -inet6 default
+-   Exec $route add -inet6 default  -interface $TSP_TUNNEL_INTERFACE
++   Exec $route add -inet6 default $TSP_SERVER_ADDRESS_IPV6
 +
 +   # Save interface for later
 +   echo $TSP_TUNNEL_INTERFACE > /var/run/tspc.if
