a9e2fca6268cbbe7190c17dc2a9ab74bdac2dc0d
nsayer <nsayer@FreeBSD.org>
2002-02-21 15:13:16 +0000

    Fix damaged patch. Oopsie.

--- security/pam_smb/files/patch-smbval::smbencrypt.c
+++ security/pam_smb/files/patch-smbval::smbencrypt.c
@@ -1,8 +1,8 @@
---- smbval/smbencrypt.c.orig	Tue Feb 19 10:34:51 2002
-+++ smbval/smbencrypt.c	Tue Feb 19 10:39:28 2002
+--- smbval/smbencrypt.c.orig	Sat Aug 19 12:58:07 2000
++++ smbval/smbencrypt.c	Thu Feb 21 07:11:09 2002
 @@ -21,6 +21,7 @@
  */
-=20
+ 
  #include <string.h>
 +#include <sys/types.h>
  #include <arpa/inet.h>
