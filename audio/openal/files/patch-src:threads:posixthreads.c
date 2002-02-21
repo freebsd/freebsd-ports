e29128b955d99d10201c71602e7f119b8dd33a3d
Patrick Li <pat@FreeBSD.org>
2002-02-21 01:26:32 +0000

    - Update to 20020217 - Pass the maintainership wand to submitter
    PR:		35162
    Submitted by:	Erik Greenwald <erik@smluc.org>

--- audio/openal/files/patch-src:threads:posixthreads.c
+++ audio/openal/files/patch-src:threads:posixthreads.c
@@ -1,11 +1,20 @@
---- src/threads/posixthreads.c~	Mon Aug 28 19:14:08 2000
-+++ src/threads/posixthreads.c	Mon Sep 25 19:17:19 2000
-@@ -82,7 +82,7 @@
+--- src/threads/posixthreads.c.orig	Wed May  9 12:40:39 2001
++++ src/threads/posixthreads.c	Wed Feb 20 12:37:03 2002
+@@ -19,6 +19,10 @@
+ extern int pthread_atfork(void (*)(void), void (*)(void), void (*)(void));
+ #endif
+ 
++#if defined(BSD_TARGET)
++int pthread_atfork(void (*a)(void),void (*b)(void),void (*c)(void)){return -1;}
++#endif
++
+ typedef int (*ptfunc)(void *);
+ 
+ static void *RunThread(void *data) {
+@@ -88,7 +92,7 @@
  }
  
  extern unsigned int Posix_SelfThread(void) {
 -	return (unsigned int) pthread_self();
 +	return (unsigned long)(unsigned long *) pthread_self();
  }
- 
- extern void Posix_ExitThread(UNUSED(int retval)) {
