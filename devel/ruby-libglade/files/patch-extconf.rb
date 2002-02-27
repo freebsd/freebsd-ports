b5601bdde62e58f2ea2cff077290db6d23641997
Akinori MUSHA <knu@FreeBSD.org>
2002-02-27 06:04:39 +0000

    - Make extconf.rb use libglade-config to search the new location of   the libglade header files.
    - Properly LIB_DEPENDS on libglade.
    
    Submitted by:	olgeni

--- devel/ruby-libglade/files/patch-extconf.rb
+++ devel/ruby-libglade/files/patch-extconf.rb
@@ -1,6 +1,6 @@
---- extconf.rb.orig	Mon Jun 25 07:08:39 2001
-+++ extconf.rb	Tue Jun 26 06:06:49 2001
-@@ -1,5 +1,13 @@
+--- extconf.rb.orig     Mon Jun 25 00:08:39 2001
++++ extconf.rb  Mon Feb 25 14:02:41 2002
+@@ -1,5 +1,17 @@
  require 'mkmf'
  
 +glib_config = with_config("glib-config", "glib-config")
@@ -11,10 +11,14 @@
 +$CFLAGS += " " + %x(#{gtk_config} --cflags).chomp
 +$LDFLAGS += " " + %x(#{gtk_config} --libs).chomp
 +
++libglade_config = with_config("libglade-config", "libglade-config")
++$CFLAGS += " " + %x(#{libglade_config} --cflags).chomp
++$LDFLAGS += " " + %x(#{libglade_config} --libs).chomp
++
  $objs = ["rbglade.o"]
  
  dir_config("glade")
-@@ -16,5 +24,3 @@
+@@ -16,5 +28,3 @@
  end
  
  create_makefile("lglade")
