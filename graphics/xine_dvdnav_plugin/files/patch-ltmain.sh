fc10b422eb1db63c12b3268f43db54f6e0466976
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2002-03-01 18:28:22 +0000

    Add xine_dvdnav_plugin, a dvd input plugin for xine.

---
+++ graphics/xine_dvdnav_plugin/files/patch-ltmain.sh
@@ -0,0 +1,23 @@
+--- ltmain.sh.orig	Tue Jan 29 21:23:16 2002
++++ ltmain.sh	Tue Jan 29 21:25:19 2002
+@@ -944,6 +944,7 @@
+ 	;;
+ 
+       -avoid-version)
++	build_old_libs=no
+ 	avoid_version=yes
+ 	continue
+ 	;;
+@@ -4175,10 +4176,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
