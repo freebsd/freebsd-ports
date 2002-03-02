379f9dc83c1d69243c4f839957178c02bc30eb1a
alfred <alfred@FreeBSD.org>
2002-03-02 10:43:22 +0000

    remove another g_free on a string culled from g_getenv.

--- x11-wm/xfce/files/patch-xfumed.c
+++ x11-wm/xfce/files/patch-xfumed.c
@@ -1,5 +1,5 @@
 --- xfumed/xfumed.c.orig	Wed Nov  7 13:09:02 2001
-+++ xfumed/xfumed.c	Sat Mar  2 02:32:50 2002
++++ xfumed/xfumed.c	Sat Mar  2 02:48:53 2002
 @@ -163,7 +163,6 @@
  	g_error ("xfumed: Couldn't open file %s\n", filename);
      }
@@ -8,3 +8,11 @@
    g_free (filename);
  
    if (xfmenu->ctree)
+@@ -476,7 +475,6 @@
+ 
+   home = g_getenv ("HOME");
+   filename = g_strconcat (home, "/", RCFILE, NULL);
+-  g_free (home);
+   backup = g_strconcat (filename, ".bak", NULL);
+ 
+   if ((backup_fp = fopen (backup, "w")) && (fp = fopen (filename, "r")))
