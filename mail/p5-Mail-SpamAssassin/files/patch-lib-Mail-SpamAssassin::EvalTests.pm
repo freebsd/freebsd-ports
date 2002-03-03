de423208120bf2c96d692024bc70f800ca60bd77
ache <ache@FreeBSD.org>
2002-03-03 10:19:28 +0000

    Fix situation when legal hotmail headers treated as fake ones

--- mail/p5-Mail-SpamAssassin/files/patch-lib-Mail-SpamAssassin::EvalTests.pm
+++ mail/p5-Mail-SpamAssassin/files/patch-lib-Mail-SpamAssassin::EvalTests.pm
@@ -1,5 +1,14 @@
---- lib/Mail/SpamAssassin/EvalTests.pm.bak	Fri Jan 25 07:36:02 2002
-+++ lib/Mail/SpamAssassin/EvalTests.pm	Tue Feb 26 19:17:43 2002
+--- lib/Mail/SpamAssassin/EvalTests.pm.orig	Fri Jan 25 07:36:02 2002
++++ lib/Mail/SpamAssassin/EvalTests.pm	Sun Mar  3 13:02:37 2002
+@@ -115,7 +115,7 @@
+   my $rcvd = $self->get ('Received');
+   $rcvd =~ s/\s+/ /gs;		# just spaces, simplify the regexp
+ 
+-  my $ip = $self->get ('X-Originating-Ip');
++  my $ip = $self->get ('X-Originating-IP');
+   if ($ip =~ /\d+\.\d+\.\d+\.\d+/) { $ip = 1; } else { $ip = 0; }
+ 
+   # Hotmail formats its received headers like this:
 @@ -688,10 +688,17 @@
  
  sub check_for_faraway_charset_in_headers {
