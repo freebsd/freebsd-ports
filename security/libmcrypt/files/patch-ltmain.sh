b76ea8545b2064464c7b86abd9eb1d7cc93269dd
Patrick Li <pat@FreeBSD.org>
2002-03-03 06:51:45 +0000

    Update to 2.4.22
    PR:		35499
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

--- security/libmcrypt/files/patch-ltmain.sh
+++ security/libmcrypt/files/patch-ltmain.sh
@@ -1,6 +1,6 @@
---- ltmain.sh.orig	Tue Nov 13 21:50:10 2001
-+++ ltmain.sh	Sat Nov 24 22:47:52 2001
-@@ -953,6 +953,7 @@
+--- ltmain.sh.orig	Fri Feb  1 20:23:29 2002
++++ ltmain.sh	Sat Mar  2 15:26:31 2002
+@@ -958,6 +958,7 @@
  	;;
  
        -avoid-version)
@@ -8,30 +8,3 @@
  	avoid_version=yes
  	continue
  	;;
-@@ -1040,7 +1041,7 @@
- 	    # These systems don't actually have a C library (as such)
- 	    test "X$arg" = "X-lc" && continue
- 	    ;;
--	  *-*-openbsd*)
-+	  *-*-openbsd* | *-*-freebsd* )
- 	    # Do not include libc due to us having libc/libc_r.
- 	    test "X$arg" = "X-lc" && continue
- 	    ;;
-@@ -1048,7 +1049,7 @@
- 	 fi
- 	 if test "X$arg" = "X-lc_r"; then
- 	  case $host in
--	  *-*-openbsd*)
-+	  *-*-openbsd* | *-*-freebsd* )
- 	    # Do not include libc_r directly, use -pthread flag.
- 	    continue
- 	    ;;
-@@ -2439,7 +2440,7 @@
- 	  *-*-netbsd*)
- 	    # Don't link with libc until the a.out ld.so is fixed.
- 	    ;;
--	  *-*-openbsd*)
-+	  *-*-openbsd* | *-*-freebsd* )
- 	    # Do not include libc due to us having libc/libc_r.
- 	    ;;
- 	  *)
