ff4659f33afbeecfa3368226273f9ae28b77147b
ache <ache@FreeBSD.org>
2002-03-04 10:16:03 +0000

    Upgrade to 2.11

--- mail/p5-Mail-SpamAssassin/files/patch-lib-Mail-SpamAssassin::EvalTests.pm
+++ mail/p5-Mail-SpamAssassin/files/patch-lib-Mail-SpamAssassin::EvalTests.pm
@@ -1,14 +1,5 @@
 --- lib/Mail/SpamAssassin/EvalTests.pm.orig	Fri Jan 25 07:36:02 2002
 +++ lib/Mail/SpamAssassin/EvalTests.pm	Sun Mar  3 13:02:37 2002
-@@ -115,7 +115,7 @@
-   my $rcvd = $self->get ('Received');
-   $rcvd =~ s/\s+/ /gs;		# just spaces, simplify the regexp
- 
--  my $ip = $self->get ('X-Originating-Ip');
-+  my $ip = $self->get ('X-Originating-IP');
-   if ($ip =~ /\d+\.\d+\.\d+\.\d+/) { $ip = 1; } else { $ip = 0; }
- 
-   # Hotmail formats its received headers like this:
 @@ -688,10 +688,17 @@
  
  sub check_for_faraway_charset_in_headers {
@@ -18,7 +9,7 @@
    my @locales = $self->get_my_locales();
    for my $h (qw(From Subject)) {
 -    my $hdr = $self->get($h);
-+# Can't use just get() because it un-mime header
++# Can't use just get() because it un-mime header and strip charset part
 +    my @hdrs = $self->{msg}->get_header ($h);
 +    if ($#hdrs >= 0) {
 +      $hdr = join (" ", @hdrs);
