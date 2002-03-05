646ae9dfa26b57c38028c56074302d2254ac102f
Martin Blapp <mbr@FreeBSD.org>
2002-03-05 15:52:12 +0000

    Fix tar.gz extraction. Our tar seems not to like the included tar files and exits with error value 141. But only if the gunzip stream is redirected from STDIN. If our tar is used with -z, everything works. Looks like a BSD tar bug.

--- editors/openoffice/files/patch-solenv+inc+tg_ext.mk
+++ editors/openoffice/files/patch-solenv+inc+tg_ext.mk
@@ -1,5 +1,14 @@
 --- ../solenv/inc/tg_ext.mk.orig	Wed Nov 14 13:59:42 2001
 +++ ../solenv/inc/tg_ext.mk	Mon Mar  4 17:30:39 2002
+@@ -107,7 +107,7 @@
+ 	
+ $(MISC)$/%.unpack : $(PRJ)$/download$/%.tar.gz
+ 	@+-$(RM) $@
+-	@+echo $(assign UNPACKCMD := gunzip -c $(BACK_PATH)download$/$(TARFILE_NAME).tar.gz | tar $(TAR_EXCLUDE_SWITCH) -xvf - ) > $(NULLDEV)
++	@+echo $(assign UNPACKCMD := tar $(TAR_EXCLUDE_SWITCH) -xzvf $(BACK_PATH)download$/$(TARFILE_NAME).tar.gz ) > $(NULLDEV)
+ 	@+$(COPY) $(mktmp $(UNPACKCMD)) $@
+ 
+ $(MISC)$/%.unpack : $(PRJ)$/download$/%.tar
 @@ -148,7 +148,11 @@
  .IF "$(BSCLIENT)"=="TRUE"
  	+cd $(PACKAGE_DIR) && $(TYPE) $(BACK_PATH)$(PATCH_FILE_NAME) | $(GNUPATCH) -f -b -p2 && $(TOUCH) $(PATCH_FLAG_FILE)
