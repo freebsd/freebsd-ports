9fd76d06c7e1f51e3bfd36af2e62d7957facfc55
nobutaka <nobutaka@FreeBSD.org>
2002-03-06 14:34:49 +0000

    Support PTHREAD_CFLAGS/PTHREAD_LIBS properly. [*] Avoid installing useless .la files. [*] Update MASTER_SITES and DISTFILES.
    PR:		34384
    Submitted by:	KATO Tsuguru <tkato@prontomail.com> [*]
    Approved by:	maintainer

---
+++ graphics/xine_d4d_plugin/files/patch-ltmain.sh
@@ -0,0 +1,49 @@
+--- ltmain.sh.orig	Mon Nov 12 16:27:33 2001
++++ ltmain.sh	Fri Jan 25 23:31:44 2002
+@@ -956,6 +956,7 @@
+ 	;;
+ 
+       -avoid-version)
++	build_old_libs=no
+ 	avoid_version=yes
+ 	continue
+ 	;;
+@@ -1043,14 +1044,14 @@
+ 	    # These systems don't actually have a C library (as such)
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+ 	  esac
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -2446,7 +2447,7 @@
+ 	  *-*-netbsd*)
+ 	    # Don't link with libc until the a.out ld.so is fixed.
+ 	    ;;
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    ;;
+ 	  *)
+@@ -4215,10 +4216,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
