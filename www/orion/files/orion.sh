91b4ceeadb31ef3a9f4221f44473cc46af6fba43
Ernst de Haan <znerd@FreeBSD.org>
2002-03-07 19:47:15 +0000

    Not deleting the PID file but emptying it. Reasoning behind this is that /var/run is not writable to 'www'. Also cleaned things up here and there.
    Bumped PORTREVISION.

--- www/orion/files/orion.sh
+++ www/orion/files/orion.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 # Set some variables
-USER_NAME=%%USER_NAME%%
 MYSELF=`basename $0`
 
 case "$1" in
 	start)
-		su -f -m ${USER_NAME} -c "exec %%CONTROL_SCRIPT%% start" && echo -n ' %%APP_SHORTNAME%%'
+		su -f -m %%USER%% -c "exec %%CONTROL_SCRIPT%% start" && echo -n ' %%APP_SHORTNAME%%'
 		;;
 	stop)
-		su -f -m ${USER_NAME} -c "exec %%CONTROL_SCRIPT%% stop" && echo -n ' %%APP_SHORTNAME%%'
+		su -f -m %%USER%% -c "exec %%CONTROL_SCRIPT%% stop" && echo -n ' %%APP_SHORTNAME%%'
 		;;
 	*)
 		echo ""
