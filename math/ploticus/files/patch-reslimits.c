34207a7342622298138d29695340f91ded161cf2
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-03-07 08:28:18 +0000

    upgrade to 2.03

--- math/ploticus/files/patch-reslimits.c
+++ math/ploticus/files/patch-reslimits.c
@@ -1,13 +1,10 @@
---- reslimits.c.orig	Wed Jan 23 22:39:27 2002
-+++ reslimits.c	Wed Jan 23 22:41:29 2002
-@@ -9,8 +9,9 @@
+--- reslimits.c.orig	Thu Mar  7 16:11:25 2002
++++ reslimits.c	Thu Mar  7 16:11:48 2002
+@@ -9,6 +9,7 @@
  #ifndef NORLIMIT
  
  #include <stdio.h>
--#include <sys/resource.h>
 +#include <sys/types.h>
  #include <sys/time.h>
-+#include <sys/resource.h>
+ #include <sys/resource.h>
  
- reslimits( type, value )
- char *type;
