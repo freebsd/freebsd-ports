c24876afe207814915be00a8a42f0181bbbcf3ca
Patrick Li <pat@FreeBSD.org>
2002-03-08 05:20:55 +0000

    Add new port x11/wmappl - An application launcher dockapp similar to wmbutton
    PR:		35513
    Submitted by:	oleg dashevskii <od@iclub.nsu.ru>

---
+++ x11/wmappl/files/patch-args.h
@@ -0,0 +1,10 @@
+--- args.h.orig	Thu Nov  2 04:52:00 2000
++++ args.h	Thu Mar  7 23:59:51 2002
+@@ -29,7 +29,6 @@
+ 
+ /* defaults macros */
+ #define WMAPPLRC ".wmapplrc"
+-#define ICONPATH "/usr/share/icons/wmappl:/usr/share/pixmaps/mini"
+ 
+ 
+ /* MACRO defining the path separator */
