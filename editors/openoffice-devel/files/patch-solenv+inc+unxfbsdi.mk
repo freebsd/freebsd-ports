2c257f58628134326dc74b20c913c3e8b211312c
mbr <mbr@FreeBSD.org>
2002-03-09 00:18:08 +0000

    Add -DOSVERSION to FreeBSD build. Remove the netbsd unxbsdi.mk file.

--- editors/openoffice-devel/files/patch-solenv+inc+unxfbsdi.mk
+++ editors/openoffice-devel/files/patch-solenv+inc+unxfbsdi.mk
@@ -1,5 +1,14 @@
---- ../solenv/inc/unxfbsdi.mk	2001/10/24 17:21:47	1.4
-+++ ../solenv/inc/unxfbsdi.mk	2002/03/03 19:39:21
+--- ../solenv/inc/unxfbsdi.mk.orig	Sat Mar  9 01:11:29 2002
++++ ../solenv/inc/unxfbsdi.mk	Sat Mar  9 01:08:23 2002
+@@ -13,7 +13,7 @@
+ cc=				gcc
+ CC=				g++
+ CDEFS+=			-D_USE_NAMESPACE=1 -DX86 -DNEW_SOLAR -DSTLPORT_VERSION=400
+-CDEFS+=			-D_REENTRANT -D_PTHREADS -D_THREAD_SAFE
++CDEFS+=			-D_REENTRANT -D_PTHREADS -D_THREAD_SAFE -DOSVERSION=$(OSVERSION)
+ CFLAGS+= -w -c $(INCLUDE)
+ CFLAGS+= -I/usr/X11R6/include
+ CFLAGSCC= -pipe
 @@ -42,12 +42,12 @@
  LINKFLAGS=
  LINKFLAGSAPPGUI= -Wl,-export-dynamic
