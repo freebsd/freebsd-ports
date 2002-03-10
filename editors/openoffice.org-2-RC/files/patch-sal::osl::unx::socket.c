3a53c53e45164857fac316f90d71ef6734d957d6
mbr <mbr@FreeBSD.org>
2002-03-10 22:42:07 +0000

    AddrLen is socklen_t, not int

--- editors/openoffice.org-2-RC/files/patch-sal::osl::unx::socket.c
+++ editors/openoffice.org-2-RC/files/patch-sal::osl::unx::socket.c
@@ -1,5 +1,5 @@
---- ../sal/osl/unx/socket.c.orig	Sun Mar  3 18:12:48 2002
-+++ ../sal/osl/unx/socket.c	Sun Mar  3 18:16:32 2002
+--- ../sal/osl/unx/socket.c.orig	Fri Jun 22 16:22:14 2001
++++ ../sal/osl/unx/socket.c	Sun Mar 10 23:33:56 2002
 @@ -82,7 +82,7 @@
  #undef HAVE_POLL_H
  #endif
@@ -9,3 +9,12 @@
  #include <sys/poll.h>
  #define HAVE_POLL_H
  #endif /* HAVE_POLL_H */
+@@ -1965,7 +1965,7 @@
+ /*****************************************************************************/
+ oslSocketAddr SAL_CALL osl_getLocalAddrOfSocket(oslSocket pSocket)
+ {
+-#ifdef LINUX
++#ifdef LINUX || defined(FREEBSD)
+ 	socklen_t AddrLen;
+ #else
+ 	/* mfe: Solaris 'cc +w' means Addrlen should be signed! */
