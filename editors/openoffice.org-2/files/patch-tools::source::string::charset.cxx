ce1afb567b1cecfd16df8cd0b3b43f8d83b916ee
Martin Blapp <mbr@FreeBSD.org>
2002-03-11 09:06:34 +0000

    Fix one #ifdef and add two more checks

--- editors/openoffice.org-2/files/patch-tools::source::string::charset.cxx
+++ editors/openoffice.org-2/files/patch-tools::source::string::charset.cxx
@@ -1,5 +1,5 @@
 --- ../tools/source/string/charset.cxx.orig	Sun Mar 10 23:55:47 2002
-+++ ../tools/source/string/charset.cxx	Sun Mar 10 23:57:30 2002
++++ ../tools/source/string/charset.cxx	Mon Mar 11 00:00:56 2002
 @@ -64,12 +64,14 @@
  #elif defined( OS2 )
  #define INCL_DOSNLS
@@ -10,9 +10,27 @@
  #include <stdlib.h>
  #include <strings.h>
  #include <locale.h>
-+if !defined(FREEBSD) || OSVERSION > 500000
++#if !defined(FREEBSD) || OSVERSION > 500000
  #include <langinfo.h>
 +#endif
  #ifndef _TOOLS_LANG_HXX
  #include <lang.hxx>
  #endif
+@@ -126,7 +128,7 @@
+ 
+ // -----------------------------------------------------------------------
+ 
+-#if defined(NETBSD) || defined(SCO)
++#if defined(NETBSD) || defined(SCO) || defined(FREEBSD)
+ 
+ // check if there is a charset qualifier at the end of the given locale string
+ // e.g. de.ISO8859-15 or de.ISO8859-15@euro which strongly indicates what
+@@ -707,7 +709,7 @@
+ #ifdef UNX
+         CharSet nTextEncoding;
+ 
+-#if defined(NETBSD) || defined(SCO)
++#if defined(NETBSD) || defined(SCO) || defined(FREEBSD)
+         nTextEncoding = GetSystemCharSetFromEnvironment();
+         if (nTextEncoding == RTL_TEXTENCODING_DONTKNOW)
+             nTextEncoding = GetSystemCharSetFromSystemLanguage();
