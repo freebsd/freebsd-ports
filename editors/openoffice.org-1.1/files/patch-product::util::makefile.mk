35790f4327ef4f8cd0d78116afd73307bf2d6dfb
Martin Blapp <mbr@FreeBSD.org>
2002-03-12 13:29:11 +0000

    Copy stlport library to the right place. Fix build

--- editors/openoffice.org-1.1/files/patch-product::util::makefile.mk
+++ editors/openoffice.org-1.1/files/patch-product::util::makefile.mk
@@ -1,6 +1,6 @@
 --- ../product/util/makefile.mk.orig	Wed Feb 27 12:32:19 2002
-+++ ../product/util/makefile.mk	Mon Mar 11 14:10:27 2002
-@@ -115,6 +115,15 @@
++++ ../product/util/makefile.mk	Tue Mar 12 14:08:34 2002
+@@ -115,13 +115,26 @@
  STLPORTLIBNAME=stlport_vc6.dll
  ADDITIONAL_DLLS= \
  	$(DESTDIRDLL)$/w9xucwrp.dll
@@ -12,11 +12,22 @@
 +DESTDIRLIB=$(DESTDIR)$/freebsd$/lib
 +DESTDIRDLL=$(DESTDIRLIB)
 +STLPORTLIBNAME=libstlport_gcc.so
-+BINDINGDLL=gcc2_uno
++BINDINGDLL=$(COMNAME)_uno
  .ENDIF
  
  STLPORTDEST=$(DESTDIRDLL)$/$(STLPORTLIBNAME)
-@@ -367,8 +376,8 @@
+ .IF "$(STLPORT4)" != ""
+ # in OOo build environment
+ .IF "($STLPORT4)" != "NO_STLPORT4"
++.IF "$(OS)" != "FREEBSD"
+ STLPORTOUT=$(SOLARVER)$/$(UPD)$/$(INPATH)$/bin$/$(STLPORTLIBNAME)
++.ELSE
++STLPORTOUT=$(STLPORT4)$/lib$/$(STLPORTLIBNAME)
++.ENDIF
+ .ELSE	
+ STLPORTOUT=$(STLPORT4)$/lib$/$(STLPORTLIBNAME)
+ .ENDIF
+@@ -367,8 +380,8 @@
  	$(GNUCOPY) -p $(DLLOUT)$/$(@:f) $@
  
  $(DESTDIRDLL)$/%$(MY_VERSION_LINKPOSTFIX) : $(DESTDIRDLL)$/%$(MY_VERSION_DLLPOSTFIX) $(DIRLIST)
@@ -27,7 +38,7 @@
  
  
  #---------------------------------------------------------
-@@ -379,7 +388,7 @@
+@@ -379,7 +392,7 @@
  #
  #$(DESTDIRDLL)$/libstdc++.so.3 : $(DESTDIRDLL)$/libstdc++.so.3.0.1 $(DIRLIST)
  #	-rm -f $@
@@ -36,7 +47,7 @@
  
  $(DESTDIRDLL)$/libgcc_s.so.1 : $(DLLOUT)$/libgcc_s.so.1 $(DIRLIST)
  	-rm -f $@
-@@ -387,7 +396,7 @@
+@@ -387,7 +400,7 @@
  
  $(DESTDIRDLL)$/libgcc_s.so : $(DESTDIRDLL)$/libgcc_s.so.1 $(DIRLIST)
  	-rm -f $@
