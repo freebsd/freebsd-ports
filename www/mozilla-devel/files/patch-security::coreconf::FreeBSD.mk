b902740e00685305dc1edfa671adf29c9c968bb1
Maxim Sobolev <sobomax@FreeBSD.org>
2002-03-12 18:16:09 +0000

    Long awaited 0.9.9 update. For some reason configure doesn't pick up our request to enable chatzilla, therefore it is not enabled now - the issue is under investigation, but anybody is free to smash maintainers with a relevant patch.

--- www/mozilla-devel/files/patch-security::coreconf::FreeBSD.mk
+++ www/mozilla-devel/files/patch-security::coreconf::FreeBSD.mk
@@ -1,6 +1,9 @@
---- security/coreconf/FreeBSD.mk.orig	Tue Sep 19 05:53:08 2000
-+++ security/coreconf/FreeBSD.mk	Thu Feb 14 14:19:02 2002
-@@ -35,27 +35,27 @@
+
+$FreeBSD$
+
+--- security/coreconf/FreeBSD.mk.orig	Sat Mar  9 00:17:36 2002
++++ security/coreconf/FreeBSD.mk	Tue Mar 12 17:19:15 2002
+@@ -35,9 +35,7 @@
  
  include $(CORE_DEPTH)/coreconf/UNIX.mk
  
@@ -11,40 +14,12 @@
  RANLIB			= ranlib
  
  ifeq ($(OS_TEST),alpha)
- CPU_ARCH		= alpha
- else
--OS_REL_CFLAGS		= -Di386
- CPU_ARCH		= x86
- endif
- 
--OS_CFLAGS		= $(DSO_CFLAGS) $(OS_REL_CFLAGS) -ansi -Wall -pipe $(THREAD_FLAG) -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
-+OS_CFLAGS		= $(DSO_CFLAGS) $(OS_REL_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
-+
-+DSO_CFLAGS		= -fPIC
-+DSO_LDOPTS		= -shared -Wl,-soname -Wl,$(notdir $@)
- 
- #
- # The default implementation strategy for FreeBSD is pthreads.
- #
- ifndef CLASSIC_NSPR
- USE_PTHREADS		= 1
--DEFINES			+= -D_THREAD_SAFE
--THREAD_FLAG		= -pthread
-+DEFINES			+= -D_THREAD_SAFE -D_REENTRANT
-+DSO_LDOPTS	+= -pthread
- endif
- 
- ARCH			= freebsd
-@@ -68,11 +68,7 @@
+@@ -70,7 +68,7 @@
  DLL_SUFFIX		= so.1.0
  endif
  
--DSO_CFLAGS		= -fPIC
--DSO_LDOPTS		= -Bshareable
--DSO_LDFLAGS		=
--
--MKSHLIB			= $(LD) $(DSO_LDOPTS)
+-MKSHLIB			= $(CC) $(DSO_LDOPTS)
 +MKSHLIB			= $(CC) $(DSO_LDOPTS) -o $@
- 
- G++INCLUDES		= -I/usr/include/g++
- 
+ ifdef MAPFILE
+ # Add LD options to restrict exported symbols to those in the map file
+ endif
