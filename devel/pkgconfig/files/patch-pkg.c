77c7e24bc96994d6bae7fbf4b4e1e0b82c512433
Ade Lovett <ade@FreeBSD.org>
2002-03-12 22:37:52 +0000

    On -stable anyway, there is a problem getting "other" libs and cflags from dependency packages (for example, from gthread-2.0).  This prevents ports like ORbit2 from compiling.  It seems to be a pretty serious oversight on the pkgconfig folks, so I'm going to submit this patch to them.
    Basically, this patch adds recursive scanning capabilities to the
    other_cflags and other_libs functions.
    
    PR:		35831
    Submitted by:	Joe Clarke <marcus@marcuscom.com>

--- devel/pkgconfig/files/patch-pkg.c
+++ devel/pkgconfig/files/patch-pkg.c
@@ -1,19 +1,16 @@
-
-$FreeBSD$
-
---- pkg.c.orig	Mon May 28 16:02:53 2001
-+++ pkg.c	Wed Jul 11 14:42:22 2001
-@@ -71,7 +71,7 @@
+--- pkg.c.orig	Thu Feb  7 14:32:13 2002
++++ pkg.c	Tue Mar 12 16:54:50 2002
+@@ -93,7 +93,7 @@
   * locations, ignoring duplicates
   */
  static void
 -scan_dir (const char *dirname)
 +scan_dir (const char *dirname, void *quiet)
  {
-   DIR *dir = opendir (dirname);
+   DIR *dir;
    struct dirent *dent;
-@@ -82,7 +82,8 @@
- 
+@@ -113,7 +113,8 @@
+   free (dirname_copy);
    if (!dir)
      {
 -      debug_spew ("Cannot open directory '%s' in package search path: %s\n",
@@ -22,8 +19,8 @@ $FreeBSD$
                    dirname, g_strerror (errno));
        return;
      }
-@@ -141,7 +142,9 @@
-       locations = g_hash_table_new (g_str_hash, g_str_equal);
+@@ -177,7 +178,9 @@
+       path_positions = g_hash_table_new (g_str_hash, g_str_equal);
        
        g_slist_foreach (search_dirs, (GFunc)scan_dir, NULL);
 -      scan_dir (PKGLIBDIR);
@@ -33,3 +30,45 @@ $FreeBSD$
      }
  }
  
+@@ -901,6 +904,13 @@
+   while (tmp != NULL)
+     {
+       Package *pkg = tmp->data;
++	  GSList *reqs = pkg->requires;
++
++	  if (reqs)
++		{
++		  g_string_append (str, packages_get_other_libs(reqs));
++		  g_string_append (str, " ");
++		}
+ 
+       if (pkg->other_libs)
+         {
+@@ -924,10 +934,13 @@
+   char *L_libs;
+   char *other_libs;
+   GString *str;
++  GSList *tmp;
+   char *retval;
+   
+   str = g_string_new ("");  
+ 
++  tmp = pkgs;
++
+   other_libs = packages_get_other_libs (pkgs);
+   L_libs = packages_get_L_libs (pkgs);
+   l_libs = packages_get_l_libs (pkgs);
+@@ -986,6 +999,13 @@
+   while (tmp != NULL)
+     {
+       Package *pkg = tmp->data;
++	  GSList *reqs = pkg->requires;
++
++	  if (reqs)
++		{
++          g_string_append (str, packages_get_other_cflags(reqs));         
++		  g_string_append (str, " ");
++		}
+ 
+       if (pkg->other_cflags)
+         {
