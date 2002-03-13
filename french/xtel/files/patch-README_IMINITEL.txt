570073a9c71de0b075eb01199d55df6e900f81f2
Ade Lovett <ade@FreeBSD.org>
2002-03-13 00:47:20 +0000

    Cleanups to fix FORBIDDEN and other miscellaneous stuff.
    PR:		34313
    Submitted by:	MAINTAINER

---
+++ french/xtel/files/patch-README_IMINITEL.txt
@@ -0,0 +1,8 @@
+--- README_IMINITEL.txt.orig	Fri Jan  4 21:59:14 2002
++++ README_IMINITEL.txt	Fri Jan  4 23:10:53 2002
+@@ -74,4 +74,4 @@
+                           de la connexion PPP (login/password)
+ 
+  /etc/ppp/peers/iminitel  Script de lancement de pppd, contient le nom de
+-                          la ligne série utilisée (par défaut ttyS0)
++                          la ligne série utilisée (par défaut cuaa0)
--- french/xtel/files/patch-mdmdetect.c
+++ french/xtel/files/patch-mdmdetect.c
@@ -1,5 +1,5 @@
---- mdmdetect.c.orig	Sun Feb 11 01:02:58 2001
-+++ mdmdetect.c	Tue Jul 24 00:10:05 2001
+--- mdmdetect.c.orig	Sat Jan 26 22:38:32 2002
++++ mdmdetect.c	Sat Jan 26 22:44:35 2002
 @@ -37,7 +37,16 @@
  #include <sys/types.h>
  #include <sys/socket.h>
@@ -17,3 +17,51 @@
  #ifdef SVR4
  #include <sys/mkdev.h>
  #endif /* SVR4 */
+@@ -71,6 +80,18 @@
+     exit (1);
+ }
+ 
++char *CtrlArg (arg)
++char *arg;
++{
++  static char ret[240];
++
++  if(strlcpy(ret, arg, 240) >= 240) {
++	fprintf(stderr, "Argument trop long: %s\n", arg);
++	exit (1);
++  }
++  return (ret);
++}
++
+ /* Sortie */
+ static void the_end (r)
+ int r;
+@@ -228,23 +249,24 @@
+ 
+   progname = xtel_basename(av[0]);
+ 
+-  if (ac < 2)
++  if (ac < 2 || ac > 7)
+     Usage (progname);
+ 
+   /* Lecture de la ligne de commande */
+   while (--ac) {
+-    if ((cp = *++av) == NULL)
++    if ((cp = CtrlArg(*++av)) == NULL) {
+       break;
++    }
+     if (*cp == '-' && *++cp) {
+       switch(*cp) {
+       case 'b' :
+-	builder = *++av; break;
++	builder = CtrlArg(*++av); break;
+ 
+       case 'd' :
+ 	++debug; break;
+ 
+       case 'l' :
+-	modem_list = *++av; break;
++	modem_list = CtrlArg(*++av); break;
+ 
+       case 'q' :
+ 	query = 1; break;
