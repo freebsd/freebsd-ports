a61805ed6e2b0909dc74842a50446a31e291bd23
mbr <mbr@FreeBSD.org>
2002-03-13 16:31:53 +0000

    Fix breakage with "patch -i" which is missing in STABLE. For some strange reason it exists in recent CURRENT, but is not documented.

---
+++ editors/openoffice/files/patch-external+jpeg+jpegpatch.sh
@@ -0,0 +1,11 @@
+--- ../external/jpeg/jpegpatch.sh.orig	Wed Mar 13 17:04:35 2002
++++ ../external/jpeg/jpegpatch.sh	Wed Mar 13 17:08:02 2002
+@@ -112,7 +112,7 @@
+ 
+   for PNAME in `cat flist.pat`
+   do
+-      patch -i pat/$PNAME.pat $PNAME
++      patch < pat/$PNAME.pat $PNAME
+   done
+ 
+   cp -rf jpeg-6b/* rtufiles/
