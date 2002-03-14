c85a28ca672519105978bd8c38ad2fdbc979c473
pat <pat@FreeBSD.org>
2002-03-14 07:39:54 +0000

    Chase checksum and bump PORTREVISION - author fix command line parsing and symlink bug
    PR:		35811
    Submitted by:	maintainer

--- sysutils/tree/files/patch-tree.c
+++ sysutils/tree/files/patch-tree.c
@@ -2,12 +2,12 @@ $FreeBSD$
 
 --- tree.c.orig	Mon Jan 29 22:38:30 2001
 +++ tree.c	Mon Dec 24 08:09:39 2001
-@@ -422,7 +422,7 @@
-       if (!noindent) indent();
- 
-       if (inodeflag || devflag || pflag || sflag || uflag || gflag || Dflag) fprintf(outfile,"[");
--      if (inodeflag) fprintf(outfile,"%7ld",(*dir)->inode);
-+      if (inodeflag) fprintf(outfile,"%7ld",(long)(*dir)->inode);
-       if (inodeflag && (devflag || pflag || uflag || gflag || sflag || Dflag)) fprintf(outfile,"%s",sp);
-       if (devflag) fprintf(outfile,"%3d",(*dir)->dev);
-       if (devflag && (pflag || uflag || gflag || sflag || Dflag)) fprintf(outfile,"%s",sp);
+@@ -417,7 +417,7 @@
+       if (uflag && (gflag || sflag || Dflag)) fprintf(outfile,"%s",sp);
+       if (gflag) fprintf(outfile,"%-8.8s",gidtoname((*dir)->gid));
+       if (gflag && (sflag || Dflag)) fprintf(outfile,"%s",sp);
+-      if (sflag) fprintf(outfile,"%9ld",(*dir)->size);
++      if (sflag) fprintf(outfile,"%9ld",(long)(*dir)->size);
+       if (sflag && Dflag) fprintf(outfile,"%s",sp);
+       if (Dflag) fprintf(outfile,"%s",do_date((*dir)->mtime));
+       if (pflag || sflag || uflag || gflag || Dflag) fprintf(outfile,"]%s%s",sp,sp);
