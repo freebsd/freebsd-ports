44788447e67d451e70b99a93d9d2c550ff680fb3
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-03-15 09:53:50 +0000

    upgrade to 0.2.11

--- misc/patchutils/files/patch-filterdiff.c
+++ misc/patchutils/files/patch-filterdiff.c
@@ -1,22 +1,18 @@
---- filterdiff.c.orig	Fri Feb  1 19:21:43 2002
-+++ filterdiff.c	Fri Feb  1 19:21:55 2002
-@@ -24,6 +24,9 @@
+--- filterdiff.c.orig	Fri Mar 15 17:51:27 2002
++++ filterdiff.c	Fri Mar 15 17:52:32 2002
+@@ -24,12 +24,12 @@
  #include "config.h"
  #endif
  
-+#ifdef HAVE_SYS_TYPES_H
-+# include <sys/types.h> // for ssize_t
-+#endif /* HAVE_SYS_TYPES_H */
- #ifdef HAVE_ERROR_H
- # include <error.h>
- #endif /* HAVE_ERROR_H */
-@@ -34,9 +37,6 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
--#ifdef HAVE_SYS_TYPES_H
--# include <sys/types.h> // for ssize_t
--#endif /* HAVE_SYS_TYPES_H */
- #include <time.h>
- 
- #include "util.h"
+-#ifdef HAVE_ERROR_H
+-# include <error.h>
+-#endif /* HAVE_ERROR_H */
+ #ifdef HAVE_SYS_TYPES_H
+ # include <sys/types.h> // for ssize_t
+ #endif /* HAVE_SYS_TYPES_H */
++#ifdef HAVE_ERROR_H
++# include <error.h>
++#endif /* HAVE_ERROR_H */
+ #include <fnmatch.h>
+ #include <getopt.h>
+ #include <locale.h>
