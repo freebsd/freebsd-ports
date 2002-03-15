8e87bc6020d68962a9954972bcc371aa1b586dae
sobomax <sobomax@FreeBSD.org>
2002-03-15 09:39:01 +0000

    Distfile had been rerolled without version bump, therefore update checksum. There are some non-whitespace differencies beween the old and new versions, so that bump PORTREVISION.
    Submitted by:	bento

---
+++ devel/gtranslator/files/patch-src::find.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/find.c	2002/03/15 09:02:54	1.1
++++ src/find.c	2002/03/15 09:03:04
+@@ -31,7 +31,7 @@
+ #include "utils.h"
+ 
+ #include <string.h>
+-#include <regex.h>
++#include <gnuregex.h>
+ #include <gtk/gtkeditable.h>
+ #include <libgnomeui/gnome-app-util.h>
+ 
---
+++ devel/gtranslator/files/patch-src::message.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/message.c	2002/03/15 09:03:55	1.1
++++ src/message.c	2002/03/15 09:04:04
+@@ -39,7 +39,7 @@
+ #include "utils_gui.h"
+ 
+ #include <string.h>
+-#include <regex.h>
++#include <gnuregex.h>
+ #include <gtk/gtk.h>
+ 
+ #include <libgnomeui/gnome-app.h>
