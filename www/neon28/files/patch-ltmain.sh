343495a317821247b68f224c83ef11e677f7a823
Patrick Li <pat@FreeBSD.org>
2002-03-15 05:12:38 +0000

    Do not install .la files again
    PR:		35910
    Submitted by:	maintainer

--- www/neon28/files/patch-ltmain.sh
+++ www/neon28/files/patch-ltmain.sh
@@ -1,6 +1,3 @@
-
-$FreeBSD$
-
 --- ltmain.sh.orig	Wed Jan 23 11:38:40 2002
 +++ ltmain.sh	Sun Feb  3 01:44:17 2002
 @@ -1037,8 +1037,16 @@
@@ -30,3 +27,16 @@ $FreeBSD$
  	  *)
  	    # Add libc to deplibs on all other systems if necessary.
  	    if test "$build_libtool_need_lc" = "yes"; then
+@@ -4179,10 +4190,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
