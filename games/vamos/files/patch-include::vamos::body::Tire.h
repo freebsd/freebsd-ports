389fe8f412eeca6d8bfd42c3b350353251609a32
ijliao <ijliao@FreeBSD.org>
2002-03-16 03:47:18 +0000

    add vamos 0.1.11 An automotive simulation framework

---
+++ games/vamos/files/patch-include::vamos::body::Tire.h
@@ -0,0 +1,10 @@
+--- include/vamos/body/Tire.h.orig	Sat Mar 16 01:47:43 2002
++++ include/vamos/body/Tire.h	Sat Mar 16 01:47:47 2002
+@@ -24,7 +24,6 @@
+ #include <vamos/geometry/Surface.h>
+ #include <vamos/geometry/Three_Vector.h>
+ #include <vamos/body/Component.h>
+-#include <vamos/body/Spring_Device.h>
+ #include <plib/sl.h>
+ 
+ using Vamos_Geometry::Three_Vector;
