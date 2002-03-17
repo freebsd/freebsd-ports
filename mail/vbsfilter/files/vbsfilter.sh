e6c52c5d9ee5aa9a5fe155778f72ab33a95e1bb2
dinoex <dinoex@FreeBSD.org>
2002-03-17 18:00:07 +0000

    - Update noattach to 1.0beta11 - syncronize sample startup-scripts
    PR:		36005
    Submitted by:	oli@amun.isnic.is

--- mail/vbsfilter/files/vbsfilter.sh
+++ mail/vbsfilter/files/vbsfilter.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-# $FreeBSD: /tmp/pcvs/ports/mail/vbsfilter/files/Attic/vbsfilter.sh,v 1.2 2001-10-07 06:33:30 dinoex Exp $
+# $FreeBSD: /tmp/pcvs/ports/mail/vbsfilter/files/Attic/vbsfilter.sh,v 1.3 2002-03-17 18:00:07 dinoex Exp $
 # 
 if ! test -x %%PREFIX%%/libexec/vbsfilter
 then
 	exit 0
 fi
-case $1 in
+case "$1" in
 start)
 	rm -f /var/run/vbsfilter
 	%%PREFIX%%/libexec/vbsfilter -p local:/var/run/vbsfilter &&
@@ -17,7 +17,7 @@ stop)
 	rm -f /var/run/vbsfilter
 	;;
 *)
-	echo "Usage: $0: [ start | stop ]" 2>&1
+	echo "Usage: ${0##*/}: { start | stop }" 2>&1
 	exit 65
 	;;
 esac
