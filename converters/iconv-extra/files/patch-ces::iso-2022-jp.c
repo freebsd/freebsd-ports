bedce9492033fa6327a172ff7160c9a2bde1ad0e
sobomax <sobomax@FreeBSD.org>
2002-03-18 09:43:25 +0000

    Iconv cleanup, stage 1a: remove `g' prefix from binaries, headers, libraries and manpages installed by libiconv (aka GNU iconv) and add similar `b'-prefixes into components installed by iconv (aka BSD iconv). The reason why we do this is because unfortunately BSD iconv become abadonware (author doesn't maintain it anymore), while number of ports that need functional iconv library grows very fast, thus creating significant overhead due to patches needed to make ports working with prefixed GNU iconv. For compatibility reasons create symlinks in GNU iconv, so that all ports that expect GNU iconv to have `g' prefix should work unmodified until all those `g'-hacks are wiped out.
    Prompted by:			ache
    Reviewed by (in principle):	freebsd-ports, freebsd-gnome

---
+++ converters/iconv-extra/files/patch-ces::iso-2022-jp.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- ces/iso-2022-jp.c	2002/03/17 11:15:25	1.1
++++ ces/iso-2022-jp.c	2002/03/17 11:15:46
+@@ -31,7 +31,7 @@
+  */
+ 
+ #define ICONV_INTERNAL
+-#include <iconv.h>
++#include <biconv.h>
+ 
+ static const iconv_ces_iso2022_ccs ccsattr[] = {
+ 	{"us-ascii",		"\x1b(B", 3, ICONV_SHIFT_SI},
