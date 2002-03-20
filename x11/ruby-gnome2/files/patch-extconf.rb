b165f8eb6afa560d438d1a46a850935ea32cc91f
Akinori MUSHA <knu@FreeBSD.org>
2002-03-20 13:24:30 +0000

    Perform extconf.rb cleanup with no functional change.

--- x11/ruby-gnome2/files/patch-extconf.rb
+++ x11/ruby-gnome2/files/patch-extconf.rb
@@ -1,18 +1,32 @@
---- extconf.rb.orig	Sat Sep 23 06:00:02 2000
-+++ extconf.rb	Wed Feb 28 20:59:59 2001
-@@ -21,11 +21,12 @@
+--- extconf.rb.orig	Mon Feb  4 13:00:24 2002
++++ extconf.rb	Wed Mar 20 22:03:22 2002
+@@ -7,25 +7,17 @@
+ #
+ # detect Gnome configurations
+ #
+-config_cmds = [
+-    ["gnome-config", "--libs", "--cflags", "gnome", "gnomeui"],
+-]
+-if ARGV.size > 0
+-  config_cmds.unshift([ARGV[0], "--libs", "--cflags"])
+-end
+ 
+ begin
+-  config_cmd = "gnome-config"
++  config_cmd = with_config("gnome-config", "gnome-config")
+   config_libs = "--libs"
+   config_cflags = "--cflags"
    config_library = "gnomeui"
    version = `#{config_cmd} --version`
    if not version.chomp.empty? then
 -    $LDFLAGS, *libs =
-+    ldflags, *libs =
-       `#{config_cmd} #{config_libs} #{config_library}`.chomp.split(/(-l.*)/)
-+    $LDFLAGS += ldflags
-     $libs = libs.join(' ') + ' ' + $libs
+-      `#{config_cmd} #{config_libs} #{config_library}`.chomp.split(/(-l.*)/)
+-    $libs = libs.join(' ') + ' ' + $libs
 -    $CFLAGS = `#{config_cmd} #{config_cflags} #{config_library}`.chomp
 -    $CFLAGS = $CFLAGS + " -DHAVE_GDKIMLIB -I../../gtk/src  -I../../gdkimlib"
-+    $CFLAGS += `#{config_cmd} #{config_cflags} #{config_library}`.chomp
-+    $CFLAGS += " -DHAVE_GDKIMLIB -I../../gtk/src  -I../../gdkimlib"
++    $LDFLAGS += ' ' + `#{config_cmd} #{config_libs} #{config_library}`.chomp
++    $CFLAGS += " -DHAVE_GDKIMLIB -I../../gtk/src  -I../../gdkimlib " +
++      `#{config_cmd} #{config_cflags} #{config_library}`.chomp
    else
-     raise "can't find a config command"
+     raise "Can't find a config command"
    end
