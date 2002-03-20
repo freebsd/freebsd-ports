b325b7631c3b46faf2e3d3a7f8ee5796a7377d43
knu <knu@FreeBSD.org>
2002-03-20 12:36:12 +0000

    ruby-libglade is now part of the Ruby/GNOME suite.
    This version is called 0.27 along with the other components, so bump
    PORTEPOCH.

--- devel/ruby-libglade/files/patch-extconf.rb
+++ devel/ruby-libglade/files/patch-extconf.rb
@@ -1,26 +1,32 @@
---- extconf.rb.orig     Mon Jun 25 00:08:39 2001
-+++ extconf.rb  Mon Feb 25 14:02:41 2002
-@@ -1,5 +1,17 @@
- require 'mkmf'
- 
-+glib_config = with_config("glib-config", "glib-config")
-+$CFLAGS += " " + %x(#{glib_config} --cflags).chomp
-+$LDFLAGS += " " + %x(#{glib_config} --libs).chomp
-+
-+gtk_config = with_config("gtk-config", "gtk-config")
-+$CFLAGS += " " + %x(#{gtk_config} --cflags).chomp
-+$LDFLAGS += " " + %x(#{gtk_config} --libs).chomp
-+
-+libglade_config = with_config("libglade-config", "libglade-config")
-+$CFLAGS += " " + %x(#{libglade_config} --cflags).chomp
-+$LDFLAGS += " " + %x(#{libglade_config} --libs).chomp
-+
- $objs = ["rbglade.o"]
- 
- dir_config("glade")
-@@ -16,5 +28,3 @@
+--- extconf.rb.orig	Wed Dec 12 10:24:28 2001
++++ extconf.rb	Wed Mar 20 21:25:58 2002
+@@ -9,24 +9,16 @@
+ #
+ # detect libglade configurations
+ #
+-config_cmds = ["libglade-config"]
++config_cmd = with_config("libglade-config", "libglade-config")
+ while /^--/ =~ ARGV[0]
+   ARGV.shift
  end
+-if ARGV.size > 0
+-  config_cmds.unshift(ARGV[0])
+-end
  
- create_makefile("lglade")
--system('echo LDFLAGS+=`gtk-config --libs` >> Makefile')
--system('echo CFLAGS+=`gtk-config --cflags` >> Makefile')
+ begin
+-  config_cmds.each do |config_cmd|
+-    version = `#{config_cmd} --version`
+-    if not version.chomp.empty?
+-      config_libs, config_cflags = "--libs", "--cflags"
+-      $LDFLAGS, *libs = `#{config_cmd} #{config_libs}`.chomp.split(/(-l.*)/)
+-      $libs = libs.join(' ') + ' ' + $libs
+-      $CFLAGS = `#{config_cmd} #{config_cflags}`.chomp
+-      break
+-    end
++  version = `#{config_cmd} --version`
++  if not version.chomp.empty?
++    $CFLAGS += ' ' + `#{config_cmd} --cflags`.chomp
++    $LDFLAGS += ' ' + `#{config_cmd} --libs`.chomp
+   end
+ rescue
+   $LDFLAGS = '-L/usr/X11R6/lib -L/usr/local/lib'
