e97d40d183c9f3d7b58caed4d4e0c7dea5a54f46
anders <anders@FreeBSD.org>
2002-03-23 18:28:13 +0000

    Update to version 1.08f. Reclaim maintainership.

--- sysutils/msyslog/files/patch-im_udp.c
+++ sysutils/msyslog/files/patch-im_udp.c
@@ -1,15 +1,14 @@
---- src/modules/im_udp.c.orig	Thu Apr 26 00:43:50 2001
-+++ src/modules/im_udp.c	Sun May 13 23:44:03 2001
-@@ -58,12 +58,6 @@
+--- src/modules/im_udp.c.orig	Sat Mar 23 18:14:04 2002
++++ src/modules/im_udp.c	Sat Mar 23 18:14:24 2002
+@@ -58,11 +58,6 @@
  #include "../modules.h"
  #include "../syslogd.h"
  
--/* recvfrom() and others like socklen_t, Irix doesn't provide it */   
+-/* recvfrom() and others like socklen_t, Irix doesn't provide it */
 -#ifndef HAVE_SOCKLEN_T
 -  typedef int socklen_t;
 -#endif
 -
--
- /*
-  * get messge
-  *
+ struct im_udp_ctx {
+ 	int	flags;
+ };
