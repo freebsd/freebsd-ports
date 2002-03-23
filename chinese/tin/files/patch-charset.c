b442a7a41d4b8eb90287c6cdac708b8acb582b94
Kevin Lo <kevlo@FreeBSD.org>
2002-03-23 10:19:58 +0000

    Update to 1.5.11
    PR: 36212
    Submitted by: Yen-Ming Lee <leeym@utopia.leeym.com>

--- chinese/tin/files/patch-charset.c
+++ chinese/tin/files/patch-charset.c
@@ -1,5 +1,5 @@
---- src/charset.c.orig	Mon Jan 29 18:32:23 2001
-+++ src/charset.c	Sat Apr 14 08:50:15 2001
+--- src/charset.c.orig	Tue Jan 29 22:31:11 2002
++++ src/charset.c	Fri Mar 22 22:55:40 2002
 @@ -335,7 +335,7 @@
  
  	for (c = (unsigned char *)buf; *c; c++) {
@@ -9,12 +9,12 @@
  			*c = '?';
  		while (--t_len > 0)
  			c++;
-@@ -364,7 +364,7 @@
+@@ -363,7 +363,7 @@
  
  	for (c = (unsigned char *)buf; *c; c++) {
- #	ifdef ENABLE_MBLEN
+ #ifdef ENABLE_MBLEN
 -		if (!(my_isprint(*c) || *c == 8 || *c == 9 || *c == 10 || *c == 12 || *c == 13) && (t_len = mblen(c, MAX(2,MB_CUR_MAX))) <= 1)
-+		if (!(my_isprint(*c) || *c == 8 || *c == 9 || *c == 10 || *c == 12 || *c == 13  || *c==27) && (t_len = mblen(c, MAX(2,MB_CUR_MAX))) <= 1)
++		if (!(my_isprint(*c) || *c == 8 || *c == 9 || *c == 10 || *c == 12 || *c == 13 || *c == 27) && (t_len = mblen(c, MAX(2,MB_CUR_MAX))) <= 1)
  			*c = '?';
  		while (--t_len > 0)
  			c++;
