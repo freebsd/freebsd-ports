b71058a7934ae735a6389f36d1299914fc911a90
pat <pat@FreeBSD.org>
2002-03-24 03:52:36 +0000

    Fix for -current - do not include malloc.h
    Submitted by:	bento

---
+++ graphics/bbrb/files/patch-src::bbrblist.h
@@ -0,0 +1,10 @@
+--- src/bbrblist.h.orig	Sat Mar 23 22:46:19 2002
++++ src/bbrblist.h	Sat Mar 23 22:46:33 2002
+@@ -25,7 +25,6 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <ctype.h>
+-#include <malloc.h>
+ #include <time.h>
+ #include "filefunctions.h"
+ #include "filethumb.h"
