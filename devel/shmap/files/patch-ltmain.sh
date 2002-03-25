d22816f7022f43d21a685fd2a434a031dd1444e9
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-03-25 12:14:24 +0000

    - Do not install useless .la file
    PR:		36289
    Submitted by:	Ports Fury

---
+++ devel/shmap/files/patch-ltmain.sh
@@ -0,0 +1,42 @@
+--- ltmain.sh.orig	Thu Sep 20 11:29:54 2001
++++ ltmain.sh	Thu Mar 21 04:16:02 2002
+@@ -1040,7 +1040,7 @@
+ 	    # These systems don't actually have a C library (as such)
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+@@ -1048,7 +1048,7 @@
+ 	 fi
+ 	 if test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -2439,7 +2439,7 @@
+ 	  *-*-netbsd*)
+ 	    # Don't link with libc until the a.out ld.so is fixed.
+ 	    ;;
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    ;;
+ 	  *)
+@@ -4208,10 +4208,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
