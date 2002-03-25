a7dc96946eb6ed0ca083f66d3a58032f5be34f82
Patrick Li <pat@FreeBSD.org>
2002-03-25 05:44:08 +0000

    Update to 1.5.0
    Approved by:	petef(maintainer)

---
+++ audio/napster/files/patch-cmds.c
@@ -0,0 +1,12 @@
+--- cmds.c.orig	Sat Mar  9 17:29:08 2002
++++ cmds.c	Sat Mar  9 17:29:41 2002
+@@ -19,8 +19,8 @@
+ #include <dlfcn.h>
+ #include <sys/types.h>
+ #include <sys/socket.h>
+-#include <arpa/inet.h>
+ #include <netinet/in.h>
++#include <arpa/inet.h>
+ #include <sys/time.h>
+ #include <unistd.h>
+ #include <signal.h>
