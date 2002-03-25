33b44ad0d1d012dc713e132e7e684cd425ec308f
sobomax <sobomax@FreeBSD.org>
2002-03-25 20:16:20 +0000

    Update to 1.0.2.

---
+++ www/gtkhtml38/files/patch-src::htmltext.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/htmltext.c	2002/03/25 14:12:56	1.1
++++ src/htmltext.c	2002/03/25 14:13:10
+@@ -25,7 +25,7 @@
+ #include <stdio.h>
+ #include <string.h>
+ #include <sys/types.h>
+-#include <regex.h>
++#include <gnuregex.h>
+ #include <gal/widgets/e-unicode.h>
+ 
+ #include "htmltext.h"
