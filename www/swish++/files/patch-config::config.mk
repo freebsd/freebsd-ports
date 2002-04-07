74893bd2ec4942fdd861642cd3fabcaf9080c788
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-04-07 01:31:38 +0000

    upgrade to 5.7 take over maintainership
    PR:		36825
    Submitted by:	Oliver Braun <obraun@informatik.unibw-muenchen.de>

--- www/swish++/files/patch-config::config.mk
+++ www/swish++/files/patch-config::config.mk
@@ -1,5 +1,5 @@
---- config/config.mk.orig	Sun Jan 13 11:38:36 2002
-+++ config/config.mk	Sun Jan 13 11:41:51 2002
+--- config/config.mk.orig	Sun Apr  7 00:04:51 2002
++++ config/config.mk	Sun Apr  7 00:07:14 2002
 @@ -35,8 +35,8 @@
  #
  ###############################################################################
@@ -19,24 +19,7 @@
  else
  PTHREAD_LIB:=	-lpthread
  endif
-@@ -118,7 +117,6 @@
- #		The command to remove files recursively and ignore errors;
- #		usually "rm -fr" for Unix or "erase" for Windows.
- 
--PERL:=		/usr/local/bin/perl
- #		The full path to the Perl 5 executable; usually "/bin/perl" or
- #		"/usr/local/bin/perl" for Unix or "\Perl\bin\perl" for
- #		Windows.  You need this only if you intend on using
-@@ -145,15 +143,14 @@
- #
- ###############################################################################
- 
--#CC:=		g++
--CC:=		/usr/local/bin/g++
-+CC:=		g++
- #		The C++ compiler you are using; usually "CC" or "g++".
- 
- #DEBUG:=		true
+@@ -152,7 +151,7 @@
  ifdef DEBUG
  OPTIM:=		-g
  else
@@ -45,7 +28,7 @@
  #		The optimization level.  Many compilers allow a digit after the
  #		O to specify the level of optimization; if so, set yours to the
  #		highest number your compiler allows without eliciting problems
-@@ -196,7 +193,7 @@
+@@ -195,7 +194,7 @@
  INSTALL:=	$(ROOT)/install-sh
  #		Install command; usually "$(ROOT)/install-sh".
  
