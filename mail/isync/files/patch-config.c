2be32a7c47adfae2af87424430b200ee8624b06e
eivind <eivind@FreeBSD.org>
2002-04-13 13:58:57 +0000

    Update and bugfix. WARNING: The format for the mailboxes has changed, so you will get duplicate messages if you have old mailboxes lying around.  I have delayed this upgrade as I was planning to write a conversion script, but feel that it has been too long already.
    Bugfix by:	nectar

---
+++ mail/isync/files/patch-config.c
@@ -0,0 +1,8 @@
+--- config.c.orig	Mon Apr  1 19:20:55 2002
++++ config.c	Mon Apr  1 19:21:06 2002
+@@ -327,5 +327,4 @@
+     free (global.user);
+     free (global.maildir);
+     free (global.host);
+-    free (global.pass);
+ }
