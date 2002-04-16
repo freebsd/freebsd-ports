95c229ce1686f0b085a0d004f9831fa44b1c010d
Martin Blapp <mbr@FreeBSD.org>
2002-04-16 17:59:29 +0000

    mktemp(3) needs unistd.h Recieved from Carlos F. A. Paniago <pan@cnpm.embrapa.br>

---
+++ editors/openoffice-1.1/files/patch-stoc+source+implementationregistration+implreg.cxx
@@ -0,0 +1,11 @@
+--- ../stoc/source/implementationregistration/implreg.cxx.orig	Tue Apr 16 19:54:58 2002
++++ ../stoc/source/implementationregistration/implreg.cxx	Tue Apr 16 19:55:26 2002
+@@ -60,6 +60,8 @@
+  ************************************************************************/
+ #include <list>
+ 
++#include <unistd.h>
++
+ #ifndef _CPPUHELPER_QUERYINTERFACE_HXX_
+ #include <cppuhelper/queryinterface.hxx>
+ #endif
