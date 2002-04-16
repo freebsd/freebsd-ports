1b712c05fca0c9e382f975be9e67244aeeadfc03
Maxim Sobolev <sobomax@FreeBSD.org>
2002-04-16 17:54:35 +0000

    - Clean-up build system a bit; - fix remaining two places where CFLAGS weren't respected; - use cpio(1) to copy hierarchies instead of tar(1), so that it is possible   to build the port as ordinary user, but still have installed files be   owned by root; - use slightly different solution for the problem with 777 permissions on   directories created during the build. Instead of harcoding resulting   prmission apply umask(2) to all mode arguments of [f]chmod() calls.   Since Rotor has no notion of file-level security anyway this should be   an optimal way to fix the problem.
    Submitted by:	Patrick Backlund <pbacklun@cc.hut.fi> (cpio fix)
    		sobomax (the rest)

--- lang/cli/files/patch-env.sh
+++ lang/cli/files/patch-env.sh
@@ -2,27 +2,29 @@
 $FreeBSD$
 
 --- env.sh.orig	Fri Mar 22 03:47:54 2002
-+++ env.sh	Fri Mar 29 20:01:48 2002
-@@ -69,14 +69,14 @@
++++ env.sh	Tue Apr 16 18:38:49 2002
+@@ -69,7 +69,15 @@
  export DEBUGGING_SUPPORTED_BUILD=1
  export SVR_WKS_DIRS=wks
  
 -if test X"$1" = "Xfree"
-+if true
++if test X"$1" = "Xretail"
++then
++echo "Retail Environment"
++export DDKBUILDENV=free
++export C_DEFINES="${C_DEFINES} -DNTMAKEENV -DNDEBUG -DPERF_TRACKING"
++export BUILD_ALT_DIR=r
++export TARGETCOMPLUS="${ROTOR_DIR}/build/v1.x86fre.rotor"
++export NTDEBUG=retail
++else if test X"$1" = "Xfree"
  then
  echo "Free Environment"
  export DDKBUILDENV=free
- export C_DEFINES="${C_DEFINES} -DNTMAKEENV -DNDEBUG -DPERF_TRACKING"
- export BUILD_ALT_DIR=r
- export TARGETCOMPLUS="${ROTOR_DIR}/build/v1.x86fre.rotor"
--export NTDEBUG=ntsdnodbg
-+export NTDEBUG=retail
- else if test X"$1" = "Xchecked"
- then
- echo "Checked Environment"
-@@ -104,3 +104,5 @@
- export PATH=${PATH}:${TARGETCOMPLUS}/int_tools
- export LD_LIBRARY_PATH="${TARGETCOMPLUS}"
- export LD_LIB_DIRS="-L${TARGETCOMPLUS}"
-+
-+exec "$@"
+@@ -93,6 +101,7 @@
+ export BUILD_ALT_DIR=df
+ export TARGETCOMPLUS="${ROTOR_DIR}/build/v1.x86fstchk.rotor"
+ export NTDEBUG=ntsd
++fi
+ fi
+ fi
+  
