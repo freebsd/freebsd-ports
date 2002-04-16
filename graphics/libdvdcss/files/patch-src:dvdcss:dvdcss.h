a182f958106b84557b5f2d50aec5443622766c62
lioux <lioux@FreeBSD.org>
2002-04-16 22:19:44 +0000

    Update to 1.1.0
    Reviewed by:	pat

--- graphics/libdvdcss/files/patch-src:dvdcss:dvdcss.h
+++ graphics/libdvdcss/files/patch-src:dvdcss:dvdcss.h
@@ -1,16 +1,17 @@
---- extras/libdvdcss/videolan/dvdcss.h.orig	Sun Oct 14 20:58:16 2001
-+++ extras/libdvdcss/videolan/dvdcss.h	Sun Oct 14 20:59:33 2001
-@@ -22,6 +22,8 @@
+--- src/dvdcss/dvdcss.h.orig	Tue Apr 16 16:04:36 2002
++++ src/dvdcss/dvdcss.h	Tue Apr 16 16:05:06 2002
+@@ -22,6 +22,9 @@
   * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
   *****************************************************************************/
  
-+#ifndef		__DVDCSS_H__
-+#define		__DVDCSS_H__	1
++#ifndef	__DVDCSS_H__
++#define	__DVDCSS_H__
++
  /*****************************************************************************
   * The libdvdcss structure
   *****************************************************************************/
-@@ -59,3 +61,4 @@
+@@ -62,3 +65,4 @@
                                      int i_flags );
  extern char *        dvdcss_error ( dvdcss_handle );
  
-+#endif
++#endif	/* __DVDCSS_H__ */
