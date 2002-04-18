ef5c60fd36b9dc86e9d3d869aa0cd9d29f169f9c
lioux <lioux@FreeBSD.org>
2002-04-18 03:08:01 +0000

    o Patch to work with liba52 update to 0.7.3 o Bump PORTREVISION (liba52 had a slight interface update without   a library version bump)
    Approved by:	maintainer (2 weeks timeout)

--- graphics/libxine/files/patch-src:liba52:xine_decoder.c
+++ graphics/libxine/files/patch-src:liba52:xine_decoder.c
@@ -1,6 +1,6 @@
---- src/liba52/xine_decoder.c.orig	Fri Oct 19 17:55:03 2001
-+++ src/liba52/xine_decoder.c	Fri Oct 19 17:55:31 2001
-@@ -31,8 +31,8 @@
+--- src/liba52/xine_decoder.c.orig	Sat Jan  5 21:43:16 2002
++++ src/liba52/xine_decoder.c	Fri Apr  5 17:52:50 2002
+@@ -36,8 +36,8 @@
  #include <fcntl.h>
  
  #include "audio_out.h"
@@ -10,4 +10,54 @@
 +#include <a52dec/a52_internal.h>
  #include "buffer.h"
  #include "xine_internal.h"
- #include "cpu_accel.h"
+ #include "xineutils.h"
+@@ -59,7 +59,7 @@
+   int              frame_length, frame_todo;
+   uint16_t         syncword;
+ 
+-  a52_state_t      a52_state;
++  a52_state_t      *a52_state;
+   int              a52_flags;
+   int              a52_bit_rate;
+   int              a52_sample_rate;
+@@ -158,8 +158,11 @@
+   this->pts           = 0;
+   this->last_pts      = 0;
+ 
+-  if( !this->samples )
+-    this->samples = a52_init (xine_mm_accel());
++  this->a52_state = a52_init (xine_mm_accel());
++
++  if( (this->a52_state) != NULL )
++    if( !this->samples )
++      this->samples = a52_samples (this->a52_state);
+ 
+   /*
+    * find out if this driver supports a52 output
+@@ -288,7 +291,7 @@
+     
+     a52_output_flags = this->a52_flags_map[this->a52_flags & A52_CHANNEL_MASK];
+     
+-    if (a52_frame (&this->a52_state, 
++    if (a52_frame (this->a52_state, 
+ 		   this->frame_buffer, 
+ 		   &a52_output_flags,
+ 		   &level, 384)) {
+@@ -297,7 +300,7 @@
+     }
+     
+     if (this->disable_dynrng)
+-      a52_dynrng (&this->a52_state, NULL, NULL);
++      a52_dynrng (this->a52_state, NULL, NULL);
+ 
+     this->have_lfe = a52_output_flags & A52_LFE;
+     if (this->have_lfe)
+@@ -337,7 +340,7 @@
+     int_samples = buf->mem;
+ 
+     for (i = 0; i < 6; i++) {
+-      if (a52_block (&this->a52_state, this->samples)) {
++      if (a52_block (this->a52_state)) {
+ 	printf ("liba52: a52_block error\n");
+ 	return; 
+       }
