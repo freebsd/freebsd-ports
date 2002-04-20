c3d21ea972ebbdf2cb19c30b759fb2019a30c18e
ijliao <ijliao@FreeBSD.org>
2002-04-20 15:24:12 +0000

    - Do not install usesess .la file - Utilize NOPORTDOCS
    PR:		37274
    Submitted by:	Ports Fury

---
+++ devel/libformat/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Jan 10 04:26:41 2002
++++ ltmain.sh	Wed Apr 17 01:13:43 2002
+@@ -4247,10 +4247,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
