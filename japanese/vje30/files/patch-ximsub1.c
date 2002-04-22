7b1715fb10669c52ced8a1eb8e1ac4c6a9728926
Akinori MUSHA <knu@FreeBSD.org>
2002-04-22 21:29:46 +0000

    - Unbreak on CURRENT: malloc.h -> stdlib.h. - Remove -lxpg4. - Cosmetique.

--- japanese/vje30/files/patch-ximsub1.c
+++ japanese/vje30/files/patch-ximsub1.c
@@ -1,28 +1,20 @@
 --- delta/newFrontend/ximsub1.c.orig	Tue Aug  1 03:00:00 2000
-+++ delta/newFrontend/ximsub1.c	Thu Jan 11 21:24:21 2001
-@@ -259,10 +259,11 @@
- 
++++ delta/newFrontend/ximsub1.c	Tue Apr 23 06:24:39 2002
+@@ -260,7 +260,7 @@
  		if (&fxim_table[i].client == xim_client) {
  		    FreeRecvQueue(fxim_table[i].client.pending);
-+
  #if 1/*991109*/
 -		    if (xim_client->v_data) {
-+		    /* if (xim_client->v_data) { */
++		    if (1) { /* (xim_client->v_data) { */
  	 		CloseVJE (xim_client);
--		    }
-+		    /* } */
+ 		    }
  #endif
- 		    fxim_table[i].client.used = False;
- 		    if (xim_client->preedit->FontName) {
-@@ -295,9 +296,9 @@
+@@ -295,7 +295,7 @@
  	    FreeRecvQueue(fxim_table[i].client.pending);
  #if 1/*991109*/
  	    xim_client = &fxim_table[i].client;
 -	    if (xim_client->v_data) {
-+	    /* if (xim_client->v_data) { */
++	    if (1) { /* (xim_client->v_data) { */
  	 		CloseVJE (xim_client);
--	    }
-+	    /* } */
+ 	    }
  #endif
- 	    fxim_table[i].client.used = False;
- 
