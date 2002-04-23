7d3e7fa9bbf134d2826635a7d8e7d84dc0f22144
will <will@FreeBSD.org>
2002-04-23 06:18:22 +0000

    fix compiling on current: s/malloc.h/stdlib.h/

---
+++ graphics/kdegraphics3/files/patch-kdvi::font.cpp
@@ -0,0 +1,11 @@
+--- kdvi/font.cpp.orig	Mon Apr 22 23:45:41 2002
++++ kdvi/font.cpp	Mon Apr 22 23:45:53 2002
+@@ -3,7 +3,7 @@
+ 
+ #include <kdebug.h>
+ #include <klocale.h>
+-#include <malloc.h>
++#include <stdlib.h>
+ #include <qapplication.h>
+ #include <qfile.h>
+ #include <stdio.h>
