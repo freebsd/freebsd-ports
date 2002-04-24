431d14d499226050c5655bb0621f7e62e59e51a3
Johann Visagie <wjv@FreeBSD.org>
2002-04-24 09:40:52 +0000

    - Update to version 0.17.4

--- devel/py-twisted/files/patch-setup.py
+++ devel/py-twisted/files/patch-setup.py
@@ -1,16 +1,11 @@
---- setup.py.orig	Mon Jan 28 11:41:12 2002
-+++ setup.py	Wed Feb 13 15:20:01 2002
-@@ -82,8 +82,11 @@
-     setup_args['platforms'] = "win32 posix"
- 
+--- setup.py.orig	Thu Apr 18 21:49:03 2002
++++ setup.py	Wed Apr 24 11:15:18 2002
+@@ -216,7 +216,7 @@
  if os.name == 'posix':
--    import glob
--    setup_args['scripts'] = filter(os.path.isfile, glob.glob('bin/*'))
-+    setup_args['scripts'] = ['bin/accepttests', 'bin/cpyrght', 'bin/faucet',
-+                             'bin/gnusto', 'bin/im', 'bin/malyon',
-+                             'bin/manhole', 'bin/mapeditor', 'bin/mktap',
-+                             'bin/runtests', 'bin/t-im', 'bin/twistd',
-+                             'bin/twistd-debug']
- 
- #'"
- # for building C banana...
+     setup_args['scripts'] = [
+         'bin/manhole', 'bin/mktap', 'bin/twistd',
+-        'bin/im', 'bin/t-im', 'bin/tap2deb',
++        'bin/im', 'bin/t-im',
+         'bin/coil',
+     ]
+ else:
