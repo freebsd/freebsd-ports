86c37e0bf7207d64ddf53feb9a9d82023d3794b0
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-04-27 18:21:21 +0000

    - Update Gimp-Print driver to version 4.2.1 - Fix build on -current - Fix checksum of gdevdj9.c.gz (again) - Get much improved ps2epsi stuff from ghostscript-gnu version
    PR:		37500
    Submitted by:	Ports Fury

--- print/ghostscript-afpl/files/patch-eplaser:gdevescv.c
+++ print/ghostscript-afpl/files/patch-eplaser:gdevescv.c
@@ -1,6 +1,16 @@
 --- eplaser/gdevescv.c.orig	Fri Dec 21 15:50:22 2001
-+++ eplaser/gdevescv.c	Fri Mar 15 04:27:39 2002
-@@ -1760,13 +1760,13 @@
++++ eplaser/gdevescv.c	Sat Apr 27 13:45:53 2002
+@@ -37,8 +37,7 @@
+ 
+  */
+ 
+-#include <stdlib.h>
+-#include <unistd.h>
++#include <string.h>
+ 
+ #include "math_.h"
+ #include "gx.h"
+@@ -1760,13 +1759,13 @@
  
  /* Process the next piece of an image. */
  private int
@@ -16,7 +26,7 @@
      gx_device *dev = info->dev;
  #endif
      gx_device_vector *const	vdev = (gx_device_vector *) dev;
-@@ -1784,7 +1784,7 @@
+@@ -1784,7 +1783,7 @@
  
      {
  
@@ -25,7 +35,7 @@
  	if (height == 260)
  	    height = 1;
  #endif
-@@ -1922,13 +1922,13 @@
+@@ -1922,13 +1921,13 @@
  
  
  private int
--- print/ghostscript-gpl/files/patch-src:dviprlib.c
+++ print/ghostscript-gpl/files/patch-src:dviprlib.c
@@ -1,23 +1,13 @@
---- src/dviprlib.c.orig	Tue May 13 00:14:09 1997
-+++ src/dviprlib.c	Thu Apr  4 03:18:57 2002
-@@ -1433,7 +1433,8 @@
+--- src/dviprlib.c.orig	Tue May 13 16:14:09 1997
++++ src/dviprlib.c	Mon Apr 22 23:02:58 2002
+@@ -1433,7 +1433,9 @@
  char *dviprt_prtcodename[] = { CFG_PRTCODE_NAME, NULL };
  char *dviprt_encodename[] = { CFG_ENCODE_NAME, NULL };
  
 -private FILE *dviprt_messagestream = stderr;
 +private FILE *dviprt_messagestream;
-+private int dviprt_messagestream_init;
++private void dviprt_messagestream_construct (void) __attribute__((constructor));
++private void dviprt_messagestream_construct (void) { dviprt_messagestream = stderr; }
  
  /*--- library functions ---*/
  int
-@@ -1505,6 +1506,10 @@
- liblocal int
- dviprt_printmessage(char *str,int len)
- {
-+  if (!dviprt_messagestream_init) {
-+    dviprt_messagestream = stderr;
-+    dviprt_messagestream_init = 1;
-+  }
-   if (dviprt_messagestream && str) {
-     if (len >= 0) fwrite(str,len,1,dviprt_messagestream);
-     else fputs(str,dviprt_messagestream);
