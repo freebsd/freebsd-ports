9ce7cde8f6c23c8ae68da61b442a4305b37ccc92
pat <pat@FreeBSD.org>
2002-04-28 23:38:19 +0000

    Update to 0.57
    PR:		37335
    Submitted by:	Maxim Konovalov <maxim@macomnet.ru>
    Reviewed by:	maintainer
    Approved by:	maintainer

--- net/flow-tools/files/patch-ftpaths.h
+++ net/flow-tools/files/patch-ftpaths.h
@@ -1,19 +1,21 @@
---- lib/ftpaths.h.in	Tue Jul 17 09:51:05 2001
-+++ lib/ftpaths.h.in	Sat Jan 12 06:28:15 2002
-@@ -29,11 +29,11 @@
+--- lib/ftpaths.h.in	Sun Apr 21 04:08:41 2002
++++ lib/ftpaths.h.in	Mon Apr 22 11:29:07 2002
+@@ -29,12 +29,12 @@
  #ifndef FTPATHS_H
  #define FTPATHS_H
  
--#define FT_FILE_MAP             "@localstatedir@/cfg/map"
+-#define FT_PATH_MAP             "@localstatedir@/cfg/map"
 +#define FT_FILE_MAP             "@localstatedir@/etc/flow-tools/map"
  
--#define FT_FILE_IP_PROT         "@localstatedir@/sym/ip-prot"
--#define FT_FILE_IP_CLASS        "@localstatedir@/sym/ip-class"
--#define FT_FILE_TCP_PORT        "@localstatedir@/sym/tcp-port"
--#define FT_FILE_ASN             "@localstatedir@/sym/asn"
-+#define FT_FILE_IP_PROT         "@localstatedir@/share/flow-tools/ip-prot"
-+#define FT_FILE_IP_CLASS        "@localstatedir@/share/flow-tools/ip-class"
-+#define FT_FILE_TCP_PORT        "@localstatedir@/share/flow-tools/tcp-port"
-+#define FT_FILE_ASN             "@localstatedir@/share/flow-tools/asn"
+-#define FT_PATH_IP_PROT         "@localstatedir@/sym/ip-prot"
+-#define FT_PATH_IP_CLASS        "@localstatedir@/sym/ip-class"
+-#define FT_PATH_TCP_PORT        "@localstatedir@/sym/tcp-port"
+-#define FT_PATH_ASN             "@localstatedir@/sym/asn"
+-#define FT_PATH_TAG             "@localstatedir@/cfg/tag_defs"
++#define FT_PATH_IP_PROT         "@localstatedir@/share/flow-tools/ip-prot"
++#define FT_PATH_IP_CLASS        "@localstatedir@/share/flow-tools/ip-class"
++#define FT_PATH_TCP_PORT        "@localstatedir@/share/flow-tools/tcp-port"
++#define FT_PATH_ASN             "@localstatedir@/share/flow-tools/asn"
++#define FT_PATH_TAG             "@localstatedir@/etc/flow-tools/tag_defs"
  
  #endif /* FTPATHS_H */
