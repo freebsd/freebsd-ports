c23560af96c1d85305b08124b5443e22ba9930c0
mbr <mbr@FreeBSD.org>
2002-04-28 11:48:18 +0000

    Make changing passwords running. Add patch posted to sourge-forge with little modifications. Bump PORTREVISION.

---
+++ www/mod_frontpage/files/patch-path.h
@@ -0,0 +1,10 @@
+--- path.h.orig	Tue Feb  5 16:39:14 2002
++++ path.h	Sun Apr 28 13:33:08 2002
+@@ -67,6 +67,7 @@
+ #define _HELP "/help"
+ #define _EXES "/exes"
+ #define _ADMIN "/admin"
++#define _FPPASSWD "/passwd.htm"
+ 
+ #define VTIHELP  _VTI_BIN "/_vti_adm/help"
+ #define SHTMLDLL _VTI_BIN "/shtml.dll"
