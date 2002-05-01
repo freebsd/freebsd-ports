a517ea22e47ca51323a5a5eadf71b6cbe7be6079
Dmitry Sivachenko <demon@FreeBSD.org>
2002-05-01 07:26:03 +0000

    Fix build on -current; Fix detection of freetype library.
    PR:		36657
    Submitted by:	MAINTAINER

---
+++ audio/zinf/files/patch-io-signature-src-signaturepmo.cpp
@@ -0,0 +1,10 @@
+--- ./io/signature/src/signaturepmo.cpp.orig	Thu Oct 26 18:51:32 2000
++++ ./io/signature/src/signaturepmo.cpp	Sun Apr  7 01:40:32 2002
+@@ -27,7 +27,6 @@
+ #endif
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <malloc.h>
+ 
+ /* project headers */
+ #include "config.h"
---
+++ audio/zinf/files/patch-io-wavout-src-waveoutpmo.cpp
@@ -0,0 +1,10 @@
+--- ./io/wavout/src/wavoutpmo.cpp.orig	Tue Oct 16 01:23:08 2001
++++ ./io/wavout/src/wavoutpmo.cpp	Sun Apr  7 01:40:28 2002
+@@ -28,7 +28,6 @@
+ #endif
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <malloc.h>
+ 
+ /* project headers */
+ #include "config.h"
