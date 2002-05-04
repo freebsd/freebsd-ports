4edb1bde8bc4427514424ea425953f6703b490a6
pat <pat@FreeBSD.org>
2002-05-04 17:24:29 +0000

    Update to 1.03
    PR:		37752
    Submitted by:	maintainer

--- audio/py-xmms/files/patch-setup.py
+++ audio/py-xmms/files/patch-setup.py
@@ -1,37 +1,42 @@
---- setup.py.orig	Mon Mar 18 01:08:13 2002
-+++ setup.py	Tue Mar 19 09:42:54 2002
-@@ -34,9 +34,12 @@
- #     end of the gcc command, which is useless in this case. So, I use
- #     "include_dirs" instead.
+--- setup.py.orig	Sun Apr  7 22:52:01 2002
++++ setup.py	Sun May  5 00:02:33 2002
+@@ -20,7 +20,7 @@
+ # Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ # MA 02111-1307, USA.
  
-+import os, re
-+
- PACKAGE = "PyXMMS"
- VERSION = "1.02"
+-import os, string, sys
++import os, string, sys, re
+ from distutils.core import setup, Extension
+ 
+ # Notes:
+@@ -36,7 +36,8 @@
+ 
+ PACKAGE = "pyxmms"
+ VERSION = "1.03"
 -GLIB_CONFIG = "glib-config"
 +GLIB_CONFIG = os.environ['GLIB_CONFIG']
 +XMMS_CONFIG = os.environ['XMMS_CONFIG']
  
  def main():
      glib_opts = {}
-@@ -51,6 +54,9 @@
-     # Suppress the -I in each -Idir output by glib-config --cflags (ugly)
+@@ -52,6 +53,9 @@
      glib_include_dirs = map(lambda s: s[2:],
                              string.split(glib_opts["cflags"], ' '))
-+    
+ 
 +    XMMSINCDIR = re.findall('-I([^ ]+)', os.popen(XMMS_CONFIG+" --cflags").read())
 +    XMMSLIBDIR = re.findall('-L([^ ]+)', os.popen(XMMS_CONFIG+" --libs").read())
- 
++
      setup(name=PACKAGE,
            version=VERSION,
-@@ -69,8 +75,9 @@
+           description="A Python interface to XMMS",
+@@ -69,8 +73,8 @@
            keywords=["xmms"],
            py_modules=["xmms"],
            ext_modules=[Extension("_xmms", ["_xmmsmodule.c"],
 -                                 include_dirs=glib_include_dirs,
+-                                 libraries=["xmms"],
 +                                 include_dirs=glib_include_dirs+XMMSINCDIR,
-                                  libraries=["xmms"],
-+				 library_dirs=XMMSLIBDIR,
++                                 libraries=["xmms"], library_dirs=XMMSLIBDIR,
                                   extra_link_args=[glib_opts["libs"]])])
  
  if __name__ == "__main__": main()
