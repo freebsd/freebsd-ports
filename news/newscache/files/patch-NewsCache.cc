4e1e5aaf280bc2cf4fe21e07b3e5c6a6992d54b0
anders <anders@FreeBSD.org>
2002-05-04 15:37:10 +0000

    Add NewsCache, a GPLed cache server for USENET News.
    PR:		35827
    Submitted by:	Tilman Linneweh <tilman@arved.de>

---
+++ news/newscache/files/patch-NewsCache.cc
@@ -0,0 +1,10 @@
+--- src/NewsCache.cc.orig      Sat Feb 23 18:06:38 2002
++++ src/NewsCache.cc   Sat Feb 23 18:06:48 2002
+@@ -18,7 +18,6 @@
+  */
+ #include "config.h"
+
+-#include <crypt.h>
+ #include <ctype.h>
+ #include <unistd.h>
+ #include <string.h>
