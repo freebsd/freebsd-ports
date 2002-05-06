8a0adde4e490d6e598fd318ca45042d4ebd9597e
ijliao <ijliao@FreeBSD.org>
2002-05-06 14:47:14 +0000

    add libshbuf 0.0.2 Library for Shared Buffer IPC

---
+++ devel/libshbuf/files/patch-shbuf.c
@@ -0,0 +1,10 @@
+--- src/shbuf.c.orig	Mon May  6 22:37:36 2002
++++ src/shbuf.c	Mon May  6 22:38:02 2002
+@@ -24,6 +24,7 @@
+ #include <signal.h>
+ #include <errno.h>
+ #include <stdio.h>
++#include <string.h>
+ 
+ #include "shbuf.h"
+ #include "internal.h"
