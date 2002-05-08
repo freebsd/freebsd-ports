0201c1639c58cc28f1446da1c7df6e9d2c22c4f0
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-05-08 22:40:48 +0000

    Add gnomemedia2 after repo copy from gnomemedia.  This is a port of the multimedia tools for the GNOME 2.0 desktop.

--- audio/gnomemedia2/files/patch-gmix::prefs.c
+++ audio/gnomemedia2/files/patch-gmix::prefs.c
@@ -1,5 +1,5 @@
---- gmix/prefs.c.orig	Fri Apr 12 11:53:51 2002
-+++ gmix/prefs.c	Fri Apr 12 11:53:57 2002
+--- gmix/prefs.c.orig	Tue May  7 00:52:42 2002
++++ gmix/prefs.c	Tue May  7 00:52:54 2002
 @@ -38,7 +38,7 @@
  #ifdef HAVE_LINUX_SOUNDCARD_H 
  #include <linux/soundcard.h>
