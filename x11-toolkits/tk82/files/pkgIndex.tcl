efa7952a2b7e777017baf4723a9091e977e4ebfb
Dirk Meyer <dinoex@FreeBSD.org>
2002-05-09 16:39:18 +0000

    - Set soname for libtk84.so - Add pkgIndex.tcl submitted by doconnor@gsoft.com.au   so "package require Tk" is now supported. - Add pkgIndex.tcl for tk83 and tk84

---
+++ x11-toolkits/tk82/files/pkgIndex.tcl
@@ -0,0 +1,3 @@
+# Tcl package index file, version 1.0
+
+package ifneeded Tk 8.2 [list load "[file join [file dirname $dir] libtk82.so.1]" Tk]
---
+++ x11-toolkits/tk84/files/pkgIndex.tcl
@@ -0,0 +1,3 @@
+# Tcl package index file, version 1.0
+
+package ifneeded Tk 8.4 [list load "[file join [file dirname $dir] libtk84.so.1]" Tk]
