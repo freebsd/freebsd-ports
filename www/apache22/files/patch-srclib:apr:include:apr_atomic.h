90cd8b73d73c0fac38dd1142b0af1c63e3abf8ae
Cy Schubert <cy@FreeBSD.org>
2002-05-09 03:55:18 +0000

    1.  Upgrade 2.0.35 --> 2.0.36
    2.  Port printed message to "pw userdel www" if port removed permanently.
        However master.passwd 1.25.2.5 has user www by default, so this is no
        longer correct advice.  Removed pkg-deinstall to correct this.
    
    PR:		37849 and 36907
    Approved by:	MAINTAINER:  Hye-Shik Chang <perky@fallin.lv>

--- www/apache22/files/patch-srclib:apr:include:apr_atomic.h
+++ www/apache22/files/patch-srclib:apr:include:apr_atomic.h
@@ -1,6 +1,6 @@
---- srclib/apr/include/apr_atomic.h.orig	Sun Apr  7 22:54:09 2002
-+++ srclib/apr/include/apr_atomic.h	Sun Apr  7 22:55:03 2002
-@@ -191,10 +191,10 @@
+--- srclib/apr/include/apr_atomic.h.orig	Wed May  1 03:41:59 2002
++++ srclib/apr/include/apr_atomic.h	Tue May  7 19:37:45 2002
+@@ -175,10 +175,10 @@
  #include <machine/atomic.h>
  
  #define apr_atomic_t apr_uint32_t
@@ -12,6 +12,6 @@
 +#define apr_atomic_dec(mem)          (atomic_subtract_int(mem,1),mem)
 +#define apr_atomic_inc(mem)          (atomic_add_int(mem,1),mem)
 +#define apr_atomic_set(mem, val)     (atomic_set_int(mem, val),mem)
- #define apr_atomic_read(mem)         *mem
+ #define apr_atomic_read(mem)         (*mem)
  
  #define APR_ATOMIC_NEED_CAS_DEFAULT 1
