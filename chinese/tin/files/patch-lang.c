6076627375b2868ac55e462ce4718b38a73779ca
clive <clive@FreeBSD.org>
2002-05-09 19:28:07 +0000

    In case tin links against libiconv now, teach tin to recognize BIG5 charset.
    Noted by: My post became 8bit stripped.

---
+++ chinese/tin/files/patch-lang.c
@@ -0,0 +1,11 @@
+--- src/lang.c.orig	Fri May 10 03:18:34 2002
++++ src/lang.c	Fri May 10 03:18:45 2002
+@@ -849,7 +849,7 @@
+ 	"ISO-8859-7", "ISO-8859-9", "ISO-8859-10", "ISO-8859-13", "ISO-8859-14",
+ 	"ISO-8859-15", "ISO-8859-16",
+ 	"KOI8-RU", "KOI8-R", "KOI8-U",
+-	"EUC-CN", "EUC-JP", "EUC-KR", "EUC-TW",
++	"EUC-CN", "EUC-JP", "EUC-KR", "EUC-TW", "BIG5",
+ 	"ISO-2022-CN", "ISO-2022-CN-EXT", "ISO-2022-JP", "ISO-2022-JP-1",
+ 	"ISO-2022-JP-2", "ISO-2022-KR",
+ 	"UTF-8"
