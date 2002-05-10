985640e79fd3a3ca4c2e6cc6fd026640b4736c2d
sobomax <sobomax@FreeBSD.org>
2002-05-10 08:32:22 +0000

    Unbroke with recent py-game.

---
+++ games/farblazer/files/patch-code::sprites::objrocket.py
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- code/sprites/objrocket.py	2002/05/10 00:24:42	1.1
++++ code/sprites/objrocket.py	2002/05/10 00:24:53
+@@ -6,7 +6,6 @@
+ import random
+ import pygame
+ from pygame.locals import *
+-from pygame.UserRect import UserRect
+ import game, gfx
+ 
+ from baseairobj import AirObj
