6790d79e77123bcf166be07789e7925d87e544b3
SADA Kenji <sada@FreeBSD.org>
2002-05-10 20:30:14 +0000

    Update to 2.4.98b. This is done over PR ports/34472
    PR:		ports/37130
    Submitted by:	maintainer

--- mail/elm+ME/files/patch-newmbox.c
+++ mail/elm+ME/files/patch-newmbox.c
@@ -1,39 +1,39 @@
---- src/newmbox.c.orig	Sun Aug 19 07:24:27 2001
-+++ src/newmbox.c	Sun Aug 19 07:25:50 2001
-@@ -585,6 +585,36 @@
- 	    }
+--- src/newmbox.c.orig	Wed Mar 20 08:04:48 2002
++++ src/newmbox.c	Tue Apr 16 05:02:54 2002
+@@ -422,6 +422,36 @@
+     }
  
- 	    if (NULL != (tmphdr = locate_header_by_name(parsed_headers,
-+							"Newsgroups"))) {
-+		struct addr_item * addrs = 
-+		    break_down_address(tmphdr->body,
-+				       !(current_header -> status & 
-+					 NOHDRENCODING) &&
-+				       is_rfc1522(tmphdr->body),
-+				       current_header->header_charset);
-+
-+		if (addrs) {
-+		    if (current_header->to)
-+			free_addr_items(current_header->to);
-+		    current_header->to = addrs;
-+
+     if (NULL != (tmphdr = locate_header_by_name(parsed_headers,
++						"Newsgroup"))) {	    
++	struct addr_item * addrs = 
++	    break_down_address(tmphdr->body,
++			       !(current_header -> status & 
++				 NOHDRENCODING) &&
++			       is_rfc1522(tmphdr->body),
++			       current_header->header_charset);
++	
++	if (addrs) {
++	    if (current_header->to)
++		free_addr_items(current_header->to);
++	    current_header->to = addrs;
++	    
 +#if DEBUG
-+		    if (Debug.active > 10) {
-+			struct addr_item * p;
-+			for (p =  current_header->to; p->addr; p++) {
-+			    DPRINT(Debug,15,(&Debug, 
-+					     "--   newsgroup[%d] addr='%s' comment='%S' fullname='%S'\n",
-+					     p - current_header->to,
-+					     p->addr,
-+					     p->comment,
-+					     p->fullname));
-+			}
-+		    }
-+#endif
-+		}	       
++	    if (Debug.active > 10) {
++		struct addr_item * p;
++		for (p =  current_header->to; p->addr; p++) {
++		    DPRINT(Debug,15,(&Debug, 
++				     "--   newsgroup[%d] addr='%s' comment='%S' fullname='%S'\n",
++				     p - current_header->to,
++				     p->addr,
++				     p->comment,
++				     p->fullname));
++		}
 +	    }
++#endif
++	}	       
++    }
 +
-+	    if (NULL != (tmphdr = locate_header_by_name(parsed_headers,
- 							"Cc"))) {	    
- 		struct addr_item * addrs = 
- 		    break_down_address(tmphdr->body,
++    if (NULL != (tmphdr = locate_header_by_name(parsed_headers,
+ 						"Cc"))) {	    
+ 	struct addr_item * addrs = 
+ 	    break_down_address(tmphdr->body,
