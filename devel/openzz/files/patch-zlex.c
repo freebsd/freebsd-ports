60c2dd3728253e6102afd48753eaea2de01867d7
ijliao <ijliao@FreeBSD.org>
2002-05-13 19:06:04 +0000

    add openzz 1.0.2 An interpreted dynamic parser

---
+++ devel/openzz/files/patch-zlex.c
@@ -0,0 +1,10 @@
+--- src/zlex.c.orig	Tue May 14 02:39:53 2002
++++ src/zlex.c	Tue May 14 02:40:04 2002
+@@ -33,7 +33,6 @@
+ #include <ctype.h>
+ //#include <varargs.h>
+ #include <stdarg.h>
+-#include <printf.h>
+ #include "avl.h"
+ #include "zlex.h"
+ #include "mem.h"
