45930064e81836aae2a16489537606b061e59cf2
Jun Kuriyama <kuriyama@FreeBSD.org>
2002-05-14 08:21:42 +0000

    Upgrade to 1.03. Support latest -current's atq command output.

--- sysutils/p5-Schedule-At/files/patch-At.pm
+++ sysutils/p5-Schedule-At/files/patch-At.pm
@@ -1,6 +1,6 @@
---- At.pm.orig	Thu Feb 12 20:08:37 1998
-+++ At.pm	Wed Jan 10 11:56:50 2001
-@@ -264,6 +264,7 @@
+--- At.pm.orig	Mon Mar 25 20:52:01 2002
++++ At.pm	Tue May 14 17:17:41 2002
+@@ -258,6 +258,7 @@
  Jose A. Rodriguez (josear@ac.upc.es)
  
  =cut
@@ -8,10 +8,10 @@
  
  ###############################################################################
  # OS dependent code
-@@ -330,4 +331,16 @@
- 	$AT{'getCommand'} = 'at -c %JOBID% |';
- 	$AT{'parseJobList'} = 
- 		sub { (substr($_[0], 27), substr($_[0], 0, 17)) } ;
+@@ -327,6 +328,25 @@
+ 		my @fields = split("\t", $_[0]);
+ 		($fields[0], substr($fields[1], 0, 16)) 
+ 	};
 +}
 +
 +sub AtCfg_freebsd {
@@ -23,5 +23,14 @@
 +	$AT{'headings'} = ['Date'];
 +	$AT{'getCommand'} = 'at -c %JOBID% |';
 +	$AT{'parseJobList'} = 
-+		sub { my @a = split(/\s+/, $_[0]); ($a[4], "$a[0] $a[1]") } ;
++	  sub {
++	    my @a = split(/\s+/, $_[0]);
++	    if (/^\d\d/) {
++	      return ($a[4], "$a[0] $a[1]");
++	    } elsif (defined $a[8]) {
++	      return ($a[8], "$a[0] $a[1] $a[2] $a[3] $a[4] $a[5]");
++	    }
++	  };
  }
+ 
+ sub AtCfg_aix {
