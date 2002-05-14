c42d285aa1ec317480cedddfd7e1f072641bb783
mbr <mbr@FreeBSD.org>
2002-05-14 17:42:20 +0000

    Remove -g from build. We have special makeflags for this in OO.

--- editors/openoffice-1.0/files/patch-solenv+inc+unxfbsdi.mk
+++ editors/openoffice-1.0/files/patch-solenv+inc+unxfbsdi.mk
@@ -37,11 +37,11 @@
 +.ENDIF
 +
 +# flags for C and C++ Compiler
-+CFLAGS+=-g -w -c $(INCLUDE) -I$(SOLARENV)/unxfbsdi/usr/include
++CFLAGS+=-w -c $(INCLUDE) -I$(SOLARENV)/unxfbsdi/usr/include
 +CFLAGS+=-I/usr/X11R6/include
 +
 +# flags for the C++ Compiler
-+CFLAGSCC=-pipe -g
++CFLAGSCC=-pipe
 +CFLAGSCXX=-pipe -fno-for-scope -fpermissive
 +
 +# Flags for enabling exception handling
