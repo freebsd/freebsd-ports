7de5f0562c64934cd9cd1e8f6db84d2ba7c3b058
sobomax <sobomax@FreeBSD.org>
2002-05-14 07:57:22 +0000

    Update to 0.4.8pl9.
    PR:		37950
    Submitted by:	Hiroo ONO <hiroo@oikumene.gcd.org>

--- net/micq/files/patch-src::util.c
+++ net/micq/files/patch-src::util.c
@@ -1,29 +1,46 @@
-
-$FreeBSD$
-
---- src/util.c	2002/04/05 10:00:08	1.1
-+++ src/util.c	2002/04/05 10:03:37
-@@ -343,7 +343,7 @@
- 
-     timeval = time (0);
-     va_start (args, str);
--    sprintf (buf, "\n%-24.24s ", ctime (&timeval));
-+    snprintf (buf, sizeof(buf), "\n%-24.24s ", ctime (&timeval));
-     vsprintf (&buf[strlen (buf)], str, args);
-     va_end (args);
- 
-@@ -356,12 +356,12 @@
-             else
-                 return -1;
-         }
--        sprintf (buffer, "%s%ld.log", prG->logplace, uin);
-+        snprintf (buffer, sizeof(buffer), "%s%ld.log", prG->logplace, uin);
- 
- #if HAVE_SYMLINK
-         if (ContactFindNick (uin))
-         {
--            sprintf (symbuf, "%s%s.log", prG->logplace, ContactFindNick (uin));
-+            snprintf (symbuf, sizeof(symbuf), "%s%s.log", prG->logplace, ContactFindNick (uin));
-             symlink (buffer, symbuf);
-         }
- #endif
+*** src/util.c.orig	Sat May 11 03:01:05 2002
+--- src/util.c	Sat May 11 20:10:23 2002
+***************
+*** 319,325 ****
+  
+      timeval = time (0);
+      va_start (args, str);
+!     sprintf (buf, "\n%-24.24s ", ctime (&timeval));
+      vsprintf (&buf[strlen (buf)], str, args);
+      va_end (args);
+  
+--- 319,325 ----
+  
+      timeval = time (0);
+      va_start (args, str);
+!     snprintf (buf, sizeof(buf), "\n%-24.24s ", ctime (&timeval));
+      vsprintf (&buf[strlen (buf)], str, args);
+      va_end (args);
+  
+***************
+*** 332,343 ****
+              else
+                  return -1;
+          }
+!         sprintf (buffer, "%suin%ld.log", prG->logplace, uin);
+  
+  #if HAVE_SYMLINK
+          if (ContactFindNick (uin))
+          {
+!             sprintf (symbuf, "%s%s.log", prG->logplace, ContactFindNick (uin));
+              for (b = symbuf + strlen (prG->logplace); (b = strchr (b, '/')); )
+                  *b = '_';
+              symlink (buffer, symbuf);
+--- 332,343 ----
+              else
+                  return -1;
+          }
+!         snprintf (buffer, sizeof (buffer), "%suin%ld.log", prG->logplace, uin);
+  
+  #if HAVE_SYMLINK
+          if (ContactFindNick (uin))
+          {
+!             snprintf (symbuf, sizeof (symbuf), "%s%s.log", prG->logplace, ContactFindNick (uin));
+              for (b = symbuf + strlen (prG->logplace); (b = strchr (b, '/')); )
+                  *b = '_';
+              symlink (buffer, symbuf);
