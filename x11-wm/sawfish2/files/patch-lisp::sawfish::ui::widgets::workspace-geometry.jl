766776cfd2408e757e0ccf0b9f06d90c624a735c
sobomax <sobomax@FreeBSD.org>
2002-05-14 22:26:39 +0000

    Update to 20020509 (version in GNOME2 RC2) after a repo-copies from lang/librep, x11-toolkits/rep-gtk and x11-wm/sawfish.

---
+++ x11-wm/sawfish2/files/patch-lisp::sawfish::ui::widgets::workspace-geometry.jl
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- lisp/sawfish/ui/widgets/workspace-geometry.jl	2002/04/29 19:16:50	1.1
++++ lisp/sawfish/ui/widgets/workspace-geometry.jl	2002/04/29 19:17:07
+@@ -28,7 +28,6 @@
+     (open rep
+ 	  gui.gtk-2.gtk
+ 	  gui.gtk-2.gnome-canvas
+-	  gui.gtk-2.gdk-pixbuf
+ 	  rep.io.files
+ 	  sawfish.gtk.widget
+ 	  sawfish.ui.layout)
