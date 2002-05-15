d86839bb3d3014c508834c7062179635f9a58eb4
kuriyama <kuriyama@FreeBSD.org>
2002-05-15 06:41:00 +0000

    Upgrade to 5.0.1.
    Maintainer note:
      I have not yet checked this major version bump enough.
      Please use net/net-snmp4 port for stable environment.
      And please report any problem with this version.

--- net-mgmt/net-snmp53/files/patch-hr_disk.c
+++ net-mgmt/net-snmp53/files/patch-hr_disk.c
@@ -1,11 +1,12 @@
---- agent/mibgroup/host/hr_disk.c.orig	Tue Feb 26 01:57:26 2002
-+++ agent/mibgroup/host/hr_disk.c	Fri Mar 29 16:06:29 2002
-@@ -185,7 +185,7 @@
- #elif defined(solaris2)
-     Add_HR_Disk_entry ( "/dev/rdsk/c%dt%dd0s%d", 0, 1, 0, 15, "/dev/rdsk/c%dt%dd0s0", 0, 7 );
-     Add_HR_Disk_entry ( "/dev/rdsk/c%dd%ds%d",   0, 1, 0, 15, "/dev/rdsk/c%dd%ds0", 0, 7 );
--#elif defined(freebsd4)
+--- agent/mibgroup/host/hr_disk.c.orig	Sat Apr 20 16:30:03 2002
++++ agent/mibgroup/host/hr_disk.c	Wed Apr 24 01:33:15 2002
+@@ -185,6 +185,9 @@
+                       "/dev/rdsk/c%dt%dd0s0", 0, 7);
+     Add_HR_Disk_entry("/dev/rdsk/c%dd%ds%d", 0, 1, 0, 15,
+                       "/dev/rdsk/c%dd%ds0", 0, 7);
 +#elif defined(__FreeBSD__) && __FreeBSD__ >= 4
-     Add_HR_Disk_entry ( "/dev/ad%ds%d%c", 0, 1, 1, 4, "/dev/ad%ds%d", 'a', 'h');
-     Add_HR_Disk_entry ( "/dev/da%ds%d%c", 0, 1, 1, 4, "/dev/da%ds%d", 'a', 'h');
++    Add_HR_Disk_entry("/dev/ad%ds%d%c", 0, 1, 1, 4, "/dev/ad%ds%d", 'a', 'h');
++    Add_HR_Disk_entry("/dev/da%ds%d%c", 0, 1, 1, 4, "/dev/da%ds%d", 'a', 'h');
  #elif defined(freebsd3)
+     Add_HR_Disk_entry("/dev/wd%ds%d%c", 0, 1, 1, 4, "/dev/wd%ds%d", 'a',
+                       'h');
--- net/net-snmp/files/patch-hr_storage.c
+++ net/net-snmp/files/patch-hr_storage.c
@@ -1,11 +1,11 @@
---- agent/mibgroup/host/hr_storage.c.orig	Fri Feb  8 19:42:25 2002
-+++ agent/mibgroup/host/hr_storage.c	Sat Feb 16 23:37:31 2002
-@@ -583,7 +583,15 @@
- 			for (i = 0; i < sizeof(mbstat.m_mtypes)/sizeof(mbstat.m_mtypes[0]); i++)
- 			    long_return += mbstat.m_mtypes[i];
+--- agent/mibgroup/host/hr_storage.c.orig	Sat Apr 20 16:30:03 2002
++++ agent/mibgroup/host/hr_storage.c	Wed Apr 24 01:37:29 2002
+@@ -604,7 +604,15 @@
+                      i++)
+                     long_return += mbstat.m_mtypes[i];
  #elif defined(MBSTAT_SYMBOL)
 +#if !defined(__FreeBSD__) || __FreeBSD_version < 500021
- 			long_return = mbstat.m_mbufs;
+                 long_return = mbstat.m_mbufs;
 +#elif __FreeBSD_version < 500024
 +			/* mbuf stats disabled */
 +			return NULL;
@@ -14,14 +14,14 @@
 +			return NULL;
 +#endif
  #elif defined(NO_DUMMY_VALUES)
- 			return NULL;
+                 return NULL;
  #else
-@@ -634,7 +642,15 @@
- 				+ (mclpool.pr_nget - mclpool.pr_nput)
- 				    * mclpool.pr_size;
+@@ -654,7 +662,15 @@
+                     * mbpool.pr_size + (mclpool.pr_nget - mclpool.pr_nput)
+                     * mclpool.pr_size;
  #elif defined(MBSTAT_SYMBOL)
 +#if !defined(__FreeBSD__) || __FreeBSD_version < 500021
- 			long_return = mbstat.m_clusters - mbstat.m_clfree;	/* unlikely, but... */
+                 long_return = mbstat.m_clusters - mbstat.m_clfree;      /* unlikely, but... */
 +#elif __FreeBSD_version < 500024
 +			/* mbuf stats disabled */
 +			return NULL;
@@ -30,5 +30,5 @@
 +			return NULL;
 +#endif
  #elif defined(NO_DUMMY_VALUES)
- 			return NULL;
+                 return NULL;
  #else
