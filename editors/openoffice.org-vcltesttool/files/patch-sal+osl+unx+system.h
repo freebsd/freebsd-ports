8ba7b2bfab4fb25f343b1abd9e62b3f13109dbd8
Martin Blapp <mbr@FreeBSD.org>
2002-05-18 12:30:12 +0000

    Make setup start. Patch from Alexander N. Kabaev

--- editors/openoffice.org-vcltesttool/files/patch-sal+osl+unx+system.h
+++ editors/openoffice.org-vcltesttool/files/patch-sal+osl+unx+system.h
@@ -1,6 +1,6 @@
 --- ../sal/osl/unx/system.h.orig	Mon Mar 11 01:41:13 2002
 +++ ../sal/osl/unx/system.h	Mon Mar 11 01:41:25 2002
-@@ -216,10 +216,7 @@
+@@ -216,12 +216,9 @@
  #   elif BYTE_ORDER == PDP_ENDIAN
  #   	define _PDP_ENDIAN
  #   endif
@@ -8,6 +8,10 @@
 -#   define  pthread_testcancel()
  #	define  NO_PTHREAD_RTL
 -#	define  NO_PTHREAD_PRIORITY
- #	define  CMD_ARG_PRG					__progname		
- #	define  CMD_ARG_ENV					environ
+-#	define  CMD_ARG_PRG					__progname		
+-#	define  CMD_ARG_ENV					environ
++#	define  CMD_ARG_PROC_STREAM
++#	define  CMD_ARG_PROC_NAME			"/proc/%u/cmdline"
  #endif
+ 
+ #ifdef SCO
