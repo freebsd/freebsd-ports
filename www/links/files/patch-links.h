e5236f47c221bc848e6912811c2dabf54adc944a
demon <demon@FreeBSD.org>
2002-05-19 16:18:59 +0000

    Upgrade to version 0.97.
    PR:		38180
    Submitted by:	"Sergey A.Osokin" <osa@freebsd.org.ru>

--- www/links/files/patch-links.h
+++
@@ -1,8 +0,0 @@
---- links.h.orig	Sat Mar  2 05:35:30 2002
-+++ links.h	Fri Apr 26 09:11:47 2002
-@@ -1984,4 +1984,5 @@
- extern int no_connect;
- extern int base_session;
-+extern int force_html;
- 
- #define D_DUMP		1
