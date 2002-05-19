8666bfd04d16a3c1e217f9fe13da4818b878eb8d
ijliao <ijliao@FreeBSD.org>
2002-05-19 19:37:05 +0000

    add milter support fix startup script
    PR:		38297
    Submitted by:	Martin Matuska <matuska@wu-wien.ac.at>, maintainer

--- security/amavisd/files/amavisd.sh
+++ security/amavisd/files/amavisd.sh
@@ -7,10 +7,18 @@ fi
 
 case "$1" in
 start)
-	[ -x ${PREFIX}/sbin/amavisd ] && su - %%AMAVISUSER%% -c ${PREFIX}/sbin/amavisd > /dev/null 2>&1 && echo -n ' amavisd'
+	( [ -x ${PREFIX}/sbin/amavisd ] && [ -x ${PREFIX}/sbin/amavis-milter ] ) || exit 1
+	AMAVISD=$(/bin/ps -xU %%AMAVISUSER%% | grep amavisd | awk '{ print $1 }')
+	( /bin/test "$AMAVISD" ) && \
+	su - %%AMAVISUSER%% -c "/bin/kill $AMAVISD" > /dev/null
+	rm -rf /var/amavis/amavis*.sock
+	su - %%AMAVISUSER%% -c ${PREFIX}/sbin/amavisd > /dev/null 2>&1 && echo -n ' amavisd'
 	;;
 stop)
-	echo -n ' amavisd'
+	( [ -x ${PREFIX}/sbin/amavisd ] && [ -x ${PREFIX}/sbin/amavis-milter ] ) || exit 1
+	AMAVISD=$(/bin/ps -xU %%AMAVISUSER%% | grep amavisd | awk '{ print $1 }')
+	( /bin/test "$AMAVISD") && \
+	su - %%AMAVISUSER%% -c "/bin/kill $AMAVISD" > /dev/null && echo -n ' amavisd'
 	;;
 *)
 	echo "Usage: `basename $0` {start|stop}" >&2
