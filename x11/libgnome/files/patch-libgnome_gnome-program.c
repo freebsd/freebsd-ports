202e4cfc4f1e7a0638f159693816696f7262fa5b
marcus <marcus@FreeBSD.org>
2002-05-20 22:42:49 +0000

    Fix some help related paths.  Bump PORTREVISION.

--- x11/libgnome/files/patch-libgnome_gnome-program.c
+++ x11/libgnome/files/patch-libgnome_gnome-program.c
@@ -1,5 +1,5 @@
---- libgnome/gnome-program.c.orig	Fri May 17 20:18:53 2002
-+++ libgnome/gnome-program.c	Fri May 17 20:19:50 2002
+--- libgnome/gnome-program.c.orig	Fri Mar 22 11:02:45 2002
++++ libgnome/gnome-program.c	Mon May 20 18:28:24 2002
 @@ -882,12 +882,12 @@
  	attr_rel = "";
  	break;
@@ -15,6 +15,15 @@
  	attr_name = GNOME_PARAM_GNOME_DATADIR;
  	attr_rel = "/pixmaps";
  	break;
+@@ -899,7 +899,7 @@
+     case GNOME_FILE_DOMAIN_HELP:
+ 	prefix_rel = "/share/gnome/help";
+ 	attr_name = GNOME_PARAM_GNOME_DATADIR;
+-	attr_rel = "/gnome/help";
++	attr_rel = "/help";
+ 	break;
+     case GNOME_FILE_DOMAIN_APP_LIBDIR:
+ 	prefix_rel = "/lib";
 @@ -914,13 +914,13 @@
  	search_path = FALSE;
  	break;
@@ -31,3 +40,18 @@
  	attr_name = GNOME_PARAM_APP_DATADIR;
  	attr_rel = "/pixmaps";
  	search_path = FALSE;
+@@ -941,12 +941,12 @@
+ 
+ 	attr_name = GNOME_PARAM_APP_DATADIR;
+ 
+-	len = strlen ("/gnome/help/") + 
++	len = strlen ("/help/") + 
+ 		strlen (program->_priv->app_id) + 1;
+ 	attr_rel = g_alloca (len);
+ 	if (attr_rel == NULL /* bad things */)
+ 		return NULL;
+-	g_snprintf (attr_rel, len, "/gnome/help/%s", program->_priv->app_id);
++	g_snprintf (attr_rel, len, "/help/%s", program->_priv->app_id);
+ 
+ 	search_path = FALSE;
+ 	break;
