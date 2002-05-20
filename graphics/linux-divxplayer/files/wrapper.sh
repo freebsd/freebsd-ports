48ca83c32da847fc6de4120ee06a550170169ed3
lioux <lioux@FreeBSD.org>
2002-05-20 04:26:21 +0000

    o New port divxplayer version 2.0 alpha: DivX(TM) Video Player for   Linux from DivX.com o It is still alpha material so probably does not work for most.   PKGMESSAGE warns about that

---
+++ graphics/linux-divxplayer/files/wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PROGRAM="${0}"
+DATADIR="%%DATADIR%%"
+DIVX_SKIN_DIR=${DATADIR}/Skins
+
+export DIVX_SKIN_DIR
+
+if [ -f ~/.divxPlayer/DivXPlayer.dbf ]
+then
+	rm -f ~/.divxPlayer/DivXPlayer.dbf
+fi
+
+exec ${PROGRAM}.bin ${*}
