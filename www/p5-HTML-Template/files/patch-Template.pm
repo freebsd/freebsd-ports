6befc04f3f0085e29b081698d47050d21b605c91
Jun Kuriyama <kuriyama@FreeBSD.org>
2002-05-20 22:59:54 +0000

    Add a patch to fix debug message indent bug (cosmetic).
    Depends on File::Spec only if required.  For latest -current,
    perl5 port is installed on $PREFIX and this conflicts with
    p5-File-Spec port's manpages.
    
    I use "5.6.1" literally in .if exists() check because bsd.port.pre.mk
    does not define $PREL_VERSION.

--- www/p5-HTML-Template/files/patch-Template.pm
+++ www/p5-HTML-Template/files/patch-Template.pm
@@ -1,5 +1,5 @@
---- Template.pm.orig	Tue Feb 19 22:15:56 2002
-+++ Template.pm	Tue Feb 19 22:18:31 2002
+--- Template.pm.orig	Sat Feb  2 08:01:37 2002
++++ Template.pm	Tue Mar 12 11:24:20 2002
 @@ -863,7 +863,9 @@
  use strict; # and no funny business, either.
  
@@ -10,3 +10,12 @@
  
  # define accessor constants used to improve readability of array
  # accesses into "objects".  I used to use 'use constant' but that
+@@ -2011,7 +2013,7 @@
+ 	push(@ifstack, $cond);
+ 	
+       } elsif ($which eq '/TMPL_IF' or $which eq '/TMPL_UNLESS') {
+-	$options->{debug} and print STDERR "### HTML::Template Debug ###$fname : line $fcounter : $which end\n";
++	$options->{debug} and print STDERR "### HTML::Template Debug ### $fname : line $fcounter : $which end\n";
+ 	
+ 	my $cond = pop(@ifstack);
+ 	die "HTML::Template->new() : found </${which}> with no matching <TMPL_IF> at $fname : line $fcounter." unless defined $cond;
