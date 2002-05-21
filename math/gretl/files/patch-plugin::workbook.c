55903ecd9ec4544dcb02b09b46dd83a3e35808cd
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-05-21 02:37:33 +0000

    upgrade to 0.994 unbreak
    PR:		37687
    Submitted by:	Ports Fury

---
+++ math/gretl/files/patch-plugin::workbook.c
@@ -0,0 +1,10 @@
+--- plugin/workbook.c.orig	Thu Apr 11 05:33:44 2002
++++ plugin/workbook.c	Thu May  2 01:27:51 2002
+@@ -2,6 +2,7 @@
+ 
+ #include <stdio.h>
+ #include <glib.h>
++#include <wchar.h>
+ #include "biff.h"
+ #include "importer.h"
+ 
