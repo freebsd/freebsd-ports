2043fa016ebe8abab8a6f8db2eabad334eff5866
trevor <trevor@FreeBSD.org>
2002-05-22 21:57:39 +0000

    Make this less crude.

--- astro/celestia/files/patch-src_celestia_glutmain.cpp
+++ astro/celestia/files/patch-src_celestia_glutmain.cpp
@@ -1,13 +1,13 @@
 $FreeBSD$
 
 --- src/celestia/glutmain.cpp.orig	Tue Apr 30 10:36:11 2002
-+++ src/celestia/glutmain.cpp	Fri May 17 17:47:50 2002
-@@ -509,7 +509,7 @@ int main(int argc, char* argv[])
++++ src/celestia/glutmain.cpp	Sat May 18 16:16:21 2002
+@@ -509,7 +509,7 @@
      // Set the simulation starting time to the current system time
      time_t curtime=time(NULL);
      appCore->start((double) curtime / 86400.0 + (double) astro::Date(1970, 1, 1));
 -    #ifdef MACOSX
-+    #if 1
++    #if defined(MACOSX) || defined(__FreeBSD__)
      /* localtime in Darwin is is reentrant only
         equiv to Linux localtime_r()
         should probably port !MACOSX code to use this too, available since
