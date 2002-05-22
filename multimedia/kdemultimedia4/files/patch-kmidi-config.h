bbc55d00b8e3ac47feff1306b3bd4d48e6a8f8cf
Andrew Gallatin <gallatin@FreeBSD.org>
2002-05-22 16:28:43 +0000

    Fix this so it works on FreeBSD/alpha 4.x:
    The existing patch does different things for 5.x so as to use the
    standard __bswapSIZE(x) inline for 5.x. and adds i386 centric code
    for FreeBSD 4.x.  This change to the patch modifies the 4.x code
    so as to just call htonX(x) on alpha where __byte_swap_X don't exist.
    
    Approved by: will

--- multimedia/kdemultimedia4/files/patch-kmidi-config.h
+++ multimedia/kdemultimedia4/files/patch-kmidi-config.h
@@ -1,8 +1,16 @@
---- ./kmidi/config.h.old	Sun Apr 28 23:33:03 2002
-+++ ./kmidi/config.h	Sun Apr 28 23:33:55 2002
-@@ -264,8 +264,8 @@
+--- kmidi/config.h.orig	Sun Oct 21 20:07:36 2001
++++ kmidi/config.h	Wed May 22 10:37:04 2002
+@@ -261,11 +261,16 @@
+ 	/* And a better idea would be to attempt the various asm 
+ 	optimizations ourselves IMO, but hey, this is free */
+ 	#if __FreeBSD_version <= 500000
++#if defined (__i386__)
  		#define XCHG_SHORT(x) __byte_swap_word(x)
  		#define XCHG_LONG(x) __byte_swap_long(x)
++#else
++		#define XCHG_SHORT(x) htons(x)
++		#define XCHG_LONG(x) htonl(x)
++#endif
  	#else
 -		#define XCHG_SHORT(x) __uint8_swap_uint16(x)
 -		#define XCHG_LONG(x) __uint8_swap_uint32(x)
