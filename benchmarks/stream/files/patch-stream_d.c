1b405c8e29ebad0c023e810daf97d329455b6165
sobomax <sobomax@FreeBSD.org>
2002-05-22 08:04:38 +0000

    Fix patch which doesn't apply.
    Submitted by:	bento

--- benchmarks/stream/files/patch-stream_d.c
+++ benchmarks/stream/files/patch-stream_d.c
@@ -1,12 +1,13 @@
+
 $FreeBSD$
 
---- stream_d.c.orig	Tue Nov  6 15:02:21 2001
-+++ stream_d.c	Tue Nov  6 15:03:11 2001
+--- stream_d.c.orig	Wed May 22 11:02:08 2002
++++ stream_d.c	Wed May 22 11:02:37 2002
 @@ -23,7 +23,7 @@
   *          that should be good to about 5% precision.
   */
  
--# define N	1000000
+-# define N	2000000
 +# define N	NSIZE
  # define NTIMES	10
  # define OFFSET	0
