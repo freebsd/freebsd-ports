ff6df975ded69be20fe45ee71a60685e07940402
Patrick Li <pat@FreeBSD.org>
2002-05-23 04:57:24 +0000

    Update to 0.20020412 Change maintainer email
    PR:		38444
    Submitted by:	maintainer

--- multimedia/xvid/files/patch-src:portab.h
+++ multimedia/xvid/files/patch-src:portab.h
@@ -1,22 +1,16 @@
---- ../../src/portab.h.orig	Sat Apr  6 15:40:58 2002
-+++ ../../src/portab.h	Sat Apr  6 15:41:21 2002
-@@ -52,6 +52,7 @@
- 
- #elif defined(LINUX) || defined(DJGPP)
- 
-+#include "../../../config.h"
- 
- #ifdef _DEBUG
- 
-@@ -72,7 +73,11 @@
+--- ../../src/portab.h.orig	Thu May 23 04:52:08 2002
++++ ../../src/portab.h	Thu May 23 04:54:41 2002
+@@ -88,7 +88,13 @@
  
  #if defined(LINUX)
  
++#include "../../../config.h"
++
 +#if defined(HAVE_STDINT_H)
  #include <stdint.h>
 +#elif defined(HAVE_INTTYPES_H)
 +#include <inttypes.h>
 +#endif
  
- #else
- 
+ #define DECLARE_ALIGNED_MATRIX(name,sizex,sizey,type,alignment) \
+ 	type name##_storage[(sizex)*(sizey)+(alignment)-1]; \
