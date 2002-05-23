62d188866d18ba2d2d3c740d9e0fc4e4e33edae2
CHOI Junho <cjh@FreeBSD.org>
2002-05-23 16:57:21 +0000

    -current build fix.
    Submitted by:	bento

---
+++ korean/hanterm/files/patch-charproc.c
@@ -0,0 +1,13 @@
+*** charproc.c.orig	Fri May 24 01:55:07 2002
+--- charproc.c	Fri May 24 01:55:12 2002
+***************
+*** 81,88 ****
+  
+  extern Widget toplevel;
+  extern void exit();
+- extern char *malloc();
+- extern char *realloc();
+  
+  extern void FlushScroll();
+  extern void InsertLine();
+--- 81,86 ----
