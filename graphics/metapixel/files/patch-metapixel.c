0cc8b14548b289ea442fcaa1d1166f20d3a94cac
ijliao <ijliao@FreeBSD.org>
2002-05-23 19:07:58 +0000

    add metapixel 0.7 Metapixel is a program for generating photomosaics
    PR:		38464
    Submitted by:	Tilman Linneweh <tilman@arved.de>

---
+++ graphics/metapixel/files/patch-metapixel.c
@@ -0,0 +1,11 @@
+--- metapixel.c.orig	Fri May 24 03:05:45 2002
++++ metapixel.c	Fri May 24 03:06:16 2002
+@@ -1127,7 +1127,7 @@
+ 		break;
+ 
+ 	    case 256 :
+-		printf("metapixel " METAPIXEL_VERSION "\n"
++		printf("metapixel \n"
+ 		       "\n"
+ 		       "Copyright (C) 1997-2000 Mark Probst\n"
+ 		       "\n"
