1aadf412fd30e1112377e1b1f3035a6aadc5cef9
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-05-28 16:08:26 +0000

    Update to the latest GNOME 2.0 snapshot, the "Stay on target!" release.

--- devel/bonobo-activation/files/patch-server::activation-server-main.c
+++ devel/bonobo-activation/files/patch-server::activation-server-main.c
@@ -1,14 +1,14 @@
 
 $FreeBSD$
 
---- server/activation-server-main.c	2002/02/13 17:50:37	1.1
-+++ server/activation-server-main.c	2002/02/13 17:51:02
-@@ -196,7 +196,7 @@
-                                 g_string_append (gnome_od_source_dir,
-                                                  gnome_dirs[i]);
-                                 g_string_append (gnome_od_source_dir,
--                                                 "/lib/bonobo/servers:");
-+                                                 "/libdata/bonobo/servers:");
-                         }
-                         g_strfreev (gnome_dirs);
- 			g_string_append_c (real_od_source_dir, ':');
+--- server/activation-server-main.c.orig	Tue May 14 04:51:55 2002
++++ server/activation-server-main.c	Mon May 27 13:36:25 2002
+@@ -127,7 +127,7 @@
+                         g_string_append (gnome_od_source_dir,
+                                          gnome_dirs[i]);
+                         g_string_append (gnome_od_source_dir,
+-                                         "/lib/bonobo/servers:");
++                                         "/libdata/bonobo/servers:");
+                 }
+                 g_strfreev (gnome_dirs);
+                 g_string_append_c (real_od_source_dir, ':');
