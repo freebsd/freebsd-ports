3e567ddefa6ccfa804cd077ffacff7eb55144676
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-05-29 18:30:14 +0000

    add audacity 0.98 Audacity is a GUI editor for digital audio waveforms
    PR:		38699
    Submitted by:	Tilman Linneweh <tilman@arved.de>

---
+++ audio/audacity/files/patch-audioarts.c
@@ -0,0 +1,11 @@
+--- snd/audioarts.c.orig	Wed May 29 19:10:43 2002
++++ snd/audioarts.c	Wed May 29 19:10:51 2002
+@@ -11,7 +11,7 @@
+ 
+ #include <sys/time.h>
+ 
+-#include <kde/artsc/artsc.h>
++#include <artsc/artsc.h>
+ 
+ /* snd includes */
+ 
