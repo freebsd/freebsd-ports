4fe37ac34eb49bc0cc57d912a81bf5aa3cb3ace4
ijliao <ijliao@FreeBSD.org>
2002-05-30 17:36:03 +0000

    add terminatorX 3.72 Realtime audio synthesizer that allows you to "scratch" on audio data
    PR:		38054
    Submitted by:	Tilman Linneweh <tilman@arved.de>

---
+++ audio/terminatorx/files/patch-src-tX-audiofile.h
@@ -0,0 +1,11 @@
+--- src/tX_audiofile.h.orig	Mon May 13 21:30:21 2002
++++ src/tX_audiofile.h	Mon May 13 21:30:45 2002
+@@ -71,7 +71,7 @@
+ #define TX_FILE_MPG123 2
+ #define TX_FILE_OGG123 3
+ 
+-#include <linux/limits.h>
++#include <limits.h>
+ #include "tX_types.h"
+ #include <stdio.h>
+ 
