f5de0fa72683c82bd4c29a48032e62ab3f3135ec
Pete Fritchman <petef@FreeBSD.org>
2002-05-30 22:35:28 +0000

    Only poll the CD drive every 10 seconds (instead of every second) to see if there is a CD in the drive to avoid annoying kernel messages.
    PR:		38720
    Submitted by:	maintainer

--- audio/grip/files/patch-src-grip.c
+++ audio/grip/files/patch-src-grip.c
@@ -1,5 +1,5 @@
---- src/grip.c.orig	Wed May  1 18:30:22 2002
-+++ src/grip.c	Wed May  1 18:30:33 2002
+--- src/grip.c.orig	Sun Mar 10 21:59:44 2002
++++ src/grip.c	Wed May 29 21:57:19 2002
 @@ -144,7 +144,7 @@
  
    app=gnome_app_new(PACKAGE,_("Grip"));
@@ -9,3 +9,29 @@
  
    gtk_object_set_user_data(GTK_OBJECT(app),(gpointer)ginfo);
  
+@@ -511,8 +511,10 @@
+   if(ginfo->rewinding) Rewind(ginfo);
+ 
+ #ifdef GRIPCD
+-  if(!ginfo->have_disc)
+-    CheckNewDisc(ginfo);
++  if (!(time(0) % 10)) {
++    if(!ginfo->have_disc)
++      CheckNewDisc(ginfo);
++  }
+ 
+   if(ginfo->auto_eject_countdown && !(--ginfo->auto_eject_countdown))
+     EjectDisc(&(ginfo->disc));
+@@ -522,8 +524,10 @@
+   if(ginfo->ripping|ginfo->encoding) UpdateRipProgress(ginfo);
+ 
+   if(!ginfo->ripping) {
+-    if(!ginfo->have_disc)
+-      CheckNewDisc(ginfo);
++    if (!(time(0) % 10)) {
++      if(!ginfo->have_disc)
++	CheckNewDisc(ginfo);
++    }
+     
+     UpdateDisplay(ginfo);
+   }
