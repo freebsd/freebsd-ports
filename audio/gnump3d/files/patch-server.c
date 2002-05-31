4edf9c49325f7e26b0bfc5b7e713a405d7ef5488
pat <pat@FreeBSD.org>
2002-05-31 23:25:47 +0000

    Update to 0.9.9.8
    PR:		38779
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

--- audio/gnump3d/files/patch-server.c
+++ audio/gnump3d/files/patch-server.c
@@ -1,10 +1,10 @@
---- src/server.c.orig	Mon May 27 20:51:04 2002
-+++ src/server.c	Mon May 27 20:51:22 2002
+--- src/server.c.orig	Thu May 30 03:58:51 2002
++++ src/server.c	Fri May 31 19:35:18 2002
 @@ -32,6 +32,7 @@
   */
  
  
 +#include <sys/types.h>
- #include <arpa/inet.h>
  #include <ctype.h>
- #include <netinet/in.h>
+ #include <netdb.h>
+ #include <pwd.h>
