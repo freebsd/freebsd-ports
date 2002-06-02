8838b31221503c14d5140ca6cefb782d412c0a41
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2002-06-02 13:45:15 +0000

    Sync with xine-0.9.10. Bump PORTREVISION.

--- graphics/xine_d5d_plugin/files/patch-src:input_dvd.c
+++ graphics/xine_d5d_plugin/files/patch-src:input_dvd.c
@@ -1,5 +1,5 @@
---- src/input_dvd.c.orig	Mon May 20 19:34:22 2002
-+++ src/input_dvd.c	Mon May 20 19:34:31 2002
+--- src/input_dvd.c.orig	Thu May  2 06:35:36 2002
++++ src/input_dvd.c	Sun Jun  2 04:17:03 2002
 @@ -33,7 +33,6 @@
  #include <string.h>
  #include <errno.h>
@@ -8,3 +8,12 @@
  #include <sys/wait.h>
  
  #include <sys/poll.h>
+@@ -906,7 +905,7 @@
+   dvdnav_input_plugin_t *this;
+   config_values_t *config = xine->config;
+ 
+-  if (iface != 6) {
++  if (iface != 7) {
+     printf("d5d input plugin doesn't support plugin API version %d.\n"
+ 	   "PLUGIN DISABLED.\n"
+ 	   "This means there's a version mismatch between xine and this input"
