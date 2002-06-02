fc8edb0c1541ada274942a674a3b8acf4c3f8a30
Martin Blapp <mbr@FreeBSD.org>
2002-06-02 21:43:03 +0000

    For now, do not add libstd++ lib to the package. A libgcc_s.so is missing on CURRENT. I'll investigate here what's wrong.

--- editors/openoffice.org-2-devel/files/patch-product+util+makefile.mk
+++ editors/openoffice.org-2-devel/files/patch-product+util+makefile.mk
@@ -1,36 +1,67 @@
---- ../product/util/makefile.mk.orig	Mon Mar 11 17:09:13 2002
-+++ ../product/util/makefile.mk	Wed Apr  3 00:40:40 2002
-@@ -157,6 +157,15 @@
- STLPORTLIBNAME=stlport_vc6.dll
+--- ../product/util/makefile.mk.orig	Sun Jun  2 22:47:14 2002
++++ ../product/util/makefile.mk	Sun Jun  2 23:32:44 2002
+@@ -108,12 +108,20 @@
+ STLPORTLIBNAME=libstlport_gcc.so
+ BINDINGDLL=$(COMNAME)_uno
+ .IF "$(COMID)"=="gcc3"
++.IF "$(CCNUMVER)">="000300010000"
++ADDITIONAL_DLLS= \
++	$(DESTDIRDLL)$/libgcc_s.so.1		\
++	$(DESTDIRDLL)$/libgcc_s.so		\
++	$(DESTDIRDLL)$/libstdc++.so.4		\
++	$(DESTDIRDLL)$/libstdc++.so.4.0.0
++.ELSE
  ADDITIONAL_DLLS= \
- 	$(DESTDIRDLL)$/w9xucwrp.dll
-+.ELIF "$(OS)"=="FREEBSD"
-+###########
-+# FREEBSD
-+###########
-+DESTDIRBIN=$(DESTDIR)$/freebsd$/bin
-+DESTDIRLIB=$(DESTDIR)$/freebsd$/lib
-+DESTDIRDLL=$(DESTDIRLIB)
-+STLPORTLIBNAME=libstlport_gcc.so
-+BINDINGDLL=gcc2_uno
+ 	$(DESTDIRDLL)$/libgcc_s.so.1 		\
+ 	$(DESTDIRDLL)$/libgcc_s.so 		\
+ 	$(DESTDIRDLL)$/libstdc++.so.3 		\
+ 	$(DESTDIRDLL)$/libstdc++.so.3.0.1
  .ENDIF
++.ENDIF
+ .ELIF "$(OS)"=="FREEBSD"
+ ###########
+ # FREEBSD
+@@ -123,13 +131,6 @@
+ DESTDIRDLL=$(DESTDIRLIB)
+ STLPORTLIBNAME=libstlport_gcc.so
+ BINDINGDLL=$(COMNAME)_uno
+-.IF "$(COMID)"=="gcc3"
+-ADDITIONAL_DLLS= \
+-	$(DESTDIRDLL)$/libgcc_s.so.1 		\
+-	$(DESTDIRDLL)$/libgcc_s.so 		\
+-	$(DESTDIRDLL)$/libstdc++.so.3 		\
+-	$(DESTDIRDLL)$/libstdc++.so.3.0.1
+-.ENDIF
+ .ELIF "$(OS)"=="NETBSD"
+ ###########
+ # NETBSD
+@@ -447,6 +448,20 @@
  
- STLPORTDEST=$(DESTDIRDLL)$/$(STLPORTLIBNAME)
-@@ -428,7 +437,7 @@
- 
- $(DESTDIRDLL)$/libstdc++.so.3 : $(DESTDIRDLL)$/libstdc++.so.3.0.1 $(DIRLIST)
+ #---------------------------------------------------------
+ # special targets for linux gcc3 
++
++.IF "$(OS)"=="LINUX"
++.IF "$(CCNUMVER)">="000300010000"
++
++$(DESTDIRDLL)$/libstdc++.so.4.0.0 : $(DLLOUT)$/libstdc++.so.4.0.0 $(DIRLIST)
++	-rm -f $@
++	$(GNUCOPY) -p $(DLLOUT)$/libstdc++.so.4.0.0 $@
++
++$(DESTDIRDLL)$/libstdc++.so.4 : $(DESTDIRDLL)$/libstdc++.so.4.0.0 $(DIRLIST)
++	-rm -f $@
++	+ln -fs libstdc++.so.4.0.0 $@
++
++.ELSE
++
+ $(DESTDIRDLL)$/libstdc++.so.3.0.1 : $(DLLOUT)$/libstdc++.so.3.0.1 $(DIRLIST)
  	-rm -f $@
--	+ln -s libstdc++.so.3.0.1 $@
-+	+ln -fs libstdc++.so.3.0.1 $@
- 
- $(DESTDIRDLL)$/libgcc_s.so.1 : $(DLLOUT)$/libgcc_s.so.1 $(DIRLIST)
- 	-rm -f $@
-@@ -436,7 +445,7 @@
- 
+ 	$(GNUCOPY) -p $(DLLOUT)$/libstdc++.so.3.0.1 $@
+@@ -462,6 +477,8 @@
  $(DESTDIRDLL)$/libgcc_s.so : $(DESTDIRDLL)$/libgcc_s.so.1 $(DIRLIST)
  	-rm -f $@
--	+ln -s libgcc_s.so.1 $@
-+	+ln -fs libgcc_s.so.1 $@
+ 	+ln -fs libgcc_s.so.1 $@
++.ENDIF
++.ENDIF
  #-------------------------------------------------------------
  
  $(DESTDIRJAR)$/%.jar : $(BINOUT)$/%.jar $(DIRLIST)
