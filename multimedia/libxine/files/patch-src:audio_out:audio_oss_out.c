a33011311469ccaf1df44220ca5f8ea9369154fd
nobutaka <nobutaka@FreeBSD.org>
2002-06-02 13:39:37 +0000

    (1) Update to 0.9.10. (2) Add SDL support.
    Submitted by:	mi (2)

--- multimedia/libxine/files/patch-src:audio_out:audio_oss_out.c
+++ multimedia/libxine/files/patch-src:audio_out:audio_oss_out.c
@@ -1,14 +1,11 @@
---- src/audio_out/audio_oss_out.c.old	Tue Jan 15 21:55:15 2002
-+++ src/audio_out/audio_oss_out.c	Thu Apr 11 13:27:47 2002
-@@ -49,7 +49,11 @@
- #if defined(__OpenBSD__)
- #include <soundcard.h>
- #elif defined(__FreeBSD__)
-+#if __FreeBSD__ > 3
-+#include <sys/soundcard.h>
-+#else
- #include <machine/soundcard.h>
-+#endif
+--- src/audio_out/audio_oss_out.c.orig	Sun Jun  2 01:54:46 2002
++++ src/audio_out/audio_oss_out.c	Sun Jun  2 01:55:37 2002
+@@ -51,6 +51,8 @@
+ #elif defined (__FreeBSD__)
+ #  if __FreeBSD__ < 4
+ #   include <machine/soundcard.h>
++#  else
++#   include <sys/soundcard.h>
+ #  endif
  #else
- #if defined(__linux__)
- #include <linux/config.h> /* Check for DEVFS */
+ # if defined(__linux__)
