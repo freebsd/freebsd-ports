1b6012aca29ffea91218cc329e7b010f583c0f1d
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2002-06-02 13:39:37 +0000

    (1) Update to 0.9.10. (2) Add SDL support.
    Submitted by:	mi (2)

---
+++ multimedia/libxine/files/patch-src:video_out:video_out_sdl.c
@@ -0,0 +1,11 @@
+--- src/video_out/video_out_sdl.c.orig	Sun Jun  2 02:04:24 2002
++++ src/video_out/video_out_sdl.c	Sun Jun  2 02:04:30 2002
+@@ -47,7 +47,7 @@
+ #include <string.h>
+ #include <inttypes.h>
+ #include <math.h>
+-#include <SDL/SDL.h>
++#include <SDL11/SDL.h>
+ 
+ #include "video_out.h"
+ #include "xine_internal.h"
