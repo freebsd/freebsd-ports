5de713145a23b75822a57c64b62d74f1e9c06a03
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-06-02 02:36:34 +0000

    Update to 0.4.

--- x11-wm/metacity-setup/files/patch-src_callbacks.c
+++ x11-wm/metacity-setup/files/patch-src_callbacks.c
@@ -1,13 +1,10 @@
---- src/callbacks.c.orig	Tue May 28 11:52:13 2002
-+++ src/callbacks.c	Tue May 28 11:53:24 2002
-@@ -30,9 +30,7 @@
- 	gchar *possibleThemeDirs[4] =
- 		{ g_build_filename (g_get_home_dir (), ".metacity", "themes",
- 				    NULL),
--		"/gnome/head/INSTALL/share/metacity/themes",
--		"/usr/share/metacity/themes",
--		"/usr/local/share/metacity/themes"
-+		"%%X11BASE%%/share/gnome/metacity/themes"
- 	};
- 	for (i = 0; i < 4; i++)
- 	{
+--- src/callbacks.c.orig	Sat Jun  1 22:27:11 2002
++++ src/callbacks.c	Sat Jun  1 22:27:18 2002
+@@ -9,7 +9,6 @@
+ #include <glade/glade.h>
+ #include <libgnomevfs/gnome-vfs-async-ops.h>
+ #include <libgnomevfs/gnome-vfs-ops.h>
+-#include "capplet-widget.h"
+ #include "stdio.h"
+ #include "stdlib.h"
+ #include "string.h"
