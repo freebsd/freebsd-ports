1b75d7344f5d9aaea20580c23e6bd847f33834fc
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-06-04 16:22:52 +0000

    add viewklass 0.52 C++ Framework for Motif

---
+++ x11-toolkits/viewklass/files/patch-VkTrace.C
@@ -0,0 +1,10 @@
+--- VkTrace.C.orig	Wed Jun  5 00:04:51 2002
++++ VkTrace.C	Wed Jun  5 00:05:25 2002
+@@ -28,6 +28,7 @@
+ #include <stdio.h>
+ #include <time.h>
+ #include <string.h>
++#include <stdarg.h>
+ 
+ #include <Vk/VkTrace.h>
+ 
