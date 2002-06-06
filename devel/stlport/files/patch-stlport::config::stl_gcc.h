9341e0e836da494e83ec2bd8b665ca0e6f7ad5ce
mbr <mbr@FreeBSD.org>
2002-06-06 21:47:30 +0000

    Fix compile for gcc3. On CURRENT, the exception tests are failing. This works fine on STABLE with gcc3. This is also the reason for the non working Openoffice port, which depends on this port.

---
+++ devel/stlport/files/patch-stlport::config::stl_gcc.h
@@ -0,0 +1,16 @@
+--- stlport/config/stl_gcc.h	Thu Jan 10 19:41:58 2002
++++ stlport/config/stl_gcc.h	Thu Jun  6 23:15:40 2002
+@@ -250,8 +250,13 @@
+ 
+ # if (__GNUC__ >= 3)
+ 
++#ifdef __FreeBSD__
++#  define _STLP_NATIVE_INCLUDE_PATH ../g++
++#  define _STLP_NATIVE_OLD_STREAMS_INCLUDE_PATH ../g++/backward
++#else
+ #  define _STLP_NATIVE_INCLUDE_PATH ../g++-v3
+ #  define _STLP_NATIVE_OLD_STREAMS_INCLUDE_PATH ../g++-v3/backward
++#endif
+ 
+ # elif (__GNUC_MINOR__ < 8)
+ 
