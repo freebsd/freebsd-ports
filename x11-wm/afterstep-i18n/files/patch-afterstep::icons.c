46cc7fce324ae78262e71541696fd22a7e193a35
petef <petef@FreeBSD.org>
2002-06-09 14:24:35 +0000

    Add the same patch that's in the afterstep port:  fix a resources leak & bump PORTREVISION.
    While I'm here, use master site grouping to download the icon files.
    
    Submitted by:	Andriy Gapon <agapon@usa.com>

---
+++ x11-wm/afterstep-i18n/files/patch-afterstep::icons.c
@@ -0,0 +1,21 @@
+$FreeBSD$
+
+--- afterstep/icons.c.orig	Thu Mar 20 19:09:21 1997
++++ afterstep/icons.c	Sun May 26 18:15:10 2002
+@@ -779,10 +779,12 @@
+ 	}
+     } 
+ 
+-    if(tmp_win->flags & ICON_MOVED)
+-      CreateIconWindow(tmp_win,tmp_win->icon_x_loc,tmp_win->icon_y_loc);
+-    else
+-      CreateIconWindow(tmp_win, def_x, def_y);
++    if(tmp_win->icon_pixmap_w == None) {
++      if(tmp_win->flags & ICON_MOVED)
++        CreateIconWindow(tmp_win,tmp_win->icon_x_loc,tmp_win->icon_y_loc);
++      else
++        CreateIconWindow(tmp_win, def_x, def_y);
++    }
+ 
+   AutoPlace(tmp_win);
+   tmp_win->flags |= ICONIFIED;
