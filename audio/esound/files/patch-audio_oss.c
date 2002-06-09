536c69d9042aa2b6ac0440d0c89ec1aa6f4c4a39
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-06-09 06:04:00 +0000

    Update to 0.2.27.

--- audio/esound/files/patch-audio_oss.c
+++ audio/esound/files/patch-audio_oss.c
@@ -1,10 +1,14 @@
-
-$FreeBSD$
-
---- audio_oss.c	2001/10/09 14:15:03	1.1
-+++ audio_oss.c	2001/10/09 14:15:40
-@@ -12,7 +12,7 @@
+--- audio_oss.c.orig	Tue Jun  4 08:55:37 2002
++++ audio_oss.c	Sun Jun  9 01:55:21 2002
+@@ -10,9 +10,14 @@
+ #  endif
+ #endif
  
++#if defined(__sparc__) || defined(__powerpc__)
++#define AFMT_S16_NE AFMT_S16_BE
++#else
++#define AFMT_S16_NE AFMT_S16_LE
++#endif
  
  /* FreeBSD uses a different identifier? what other BSDs? */
 -#ifdef __FreeBSD__
