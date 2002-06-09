c2b1840688c294608efb2b8601d11100eebe83cd
Pete Fritchman <petef@FreeBSD.org>
2002-06-09 17:28:18 +0000

    Unbreak:  fix compilation.

---
+++ math/oleo/files/patch-src::utils.c
@@ -0,0 +1,13 @@
+$FreeBSD$
+
+--- src/utils.c.orig	Sun Jun  9 13:13:57 2002
++++ src/utils.c	Sun Jun  9 13:14:22 2002
+@@ -66,7 +66,7 @@
+ #define _IOSTRG 0
+ #endif
+ 
+-extern int sys_nerr;
++// extern int sys_nerr;
+ 
+ struct id
+   {
