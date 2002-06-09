65ddada6f19556bf3505c153765984df452688df
ijliao <ijliao@FreeBSD.org>
2002-06-09 18:58:48 +0000

    upgrade to 1.7.1 drop maintainership

--- net/dictd/files/patch-dictd.c
+++ net/dictd/files/patch-dictd.c
@@ -1,11 +1,11 @@
---- dictd.c~	Mon Jan 29 01:51:25 2001
-+++ dictd.c	Mon Jan 29 01:59:29 2001
-@@ -97,7 +97,7 @@
+--- dictd.c.orig	Mon Jun 10 02:39:20 2002
++++ dictd.c	Mon Jun 10 02:40:01 2002
+@@ -113,7 +113,7 @@
  
  static void reaper( int dummy )
  {
--#if defined(__osf__) || (defined(__sparc__) && defined(__svr4__))
-+#if defined(__FreeBSD__) || defined(__osf__) || (defined(__sparc__) && defined(__svr4__) )
+-#if defined(__osf__) || (defined(__sparc) && defined(__SVR4))
++#if defined(__FreeBSD__) || defined(__osf__) || (defined(__sparc) && defined(__SVR4))
     int        status;
  #else
     union wait status;
