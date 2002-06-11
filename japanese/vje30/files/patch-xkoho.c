a04e3046aee6c280198b16641310e189e548e20e
Akinori MUSHA <knu@FreeBSD.org>
2002-06-11 04:58:19 +0000

    My patches have been integrated by the vendor, so use the latest tarballs and remove the patches from the port.  Keep PORTREVISION unbumped because the resulted binaries will be identical.

--- japanese/vje30/files/patch-xkoho.c
+++
@@ -1,9 +0,0 @@
---- delta/newFrontend/xkoho.c.orig	Fri May 17 14:12:28 2002
-+++ delta/newFrontend/xkoho.c	Wed May 29 00:20:49 2002
-@@ -1795,4 +1795,6 @@
- 	if (!rc) {
- 		return (FALSE);
- 	}
-+
-+	return (TRUE);
- }
