38ea1767a1e08f15ec6bbce65ec8113b454b9239
alfred <alfred@FreeBSD.org>
2002-06-12 20:30:11 +0000

    don't include malloc.h

---
+++ emulators/wine-devel/files/patch-winefile.h
@@ -0,0 +1,10 @@
+--- programs/winefile/winefile.h.old	Wed Jun 12 13:11:02 2002
++++ programs/winefile/winefile.h	Wed Jun 12 13:11:07 2002
+@@ -38,7 +38,6 @@
+ #include <commctrl.h>
+ #include <shellapi.h>
+ #include <stdlib.h>
+-#include <malloc.h>
+ #include <stdio.h>
+ #include <tchar.h>
+ #include <ctype.h>
