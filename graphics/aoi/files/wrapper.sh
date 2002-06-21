3aba8560039f48e34f1620b7b457b09e4bd1c035
lioux <lioux@FreeBSD.org>
2002-06-21 18:20:26 +0000

    New port aoi version 1.1: Art Of Illusion, An open source Java written 3D modelling and rendering studio

---
+++ graphics/aoi/files/wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+PREFIX="%%PREFIX%%"
+JAVAVM="%%JAVAVM%%"
+JAR_DIR="%%JAR_DIR%%"
+JAR_FILE="%%JAR_FILE%%"
+
+( cd ${PREFIX}/${JAR_DIR} && \
+	${JAVAVM} -jar ${PREFIX}/${JAR_DIR}/${JAR_FILE} $* )
