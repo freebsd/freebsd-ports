f1d426cc4b0376c603414bf5c0799cce636f641d
pat <pat@FreeBSD.org>
2002-06-22 18:10:18 +0000

    Keep checksum current: author rerolled distfile and the only change is to not define HAVE_MKSTEMPS by default. Add a patch to define HAVE_MKSTEMPS since we have it in our implementation.
    PR:             ports/39582
    Submitted by:   maintainer

---
+++ textproc/pdftohtml/files/patch-aconf.h
@@ -0,0 +1,11 @@
+--- aconf.h     Thu Jun 20 03:44:31 2002
++++ /home/meshko/xpdf/work/pdftohtml/aconf.h    Tue Apr 16 18:02:02 2002
+@@ -51,7 +51,7 @@
+ /* #undef HAVE_BSTRING_H */
+ #define HAVE_POPEN 1
+ #define HAVE_MKSTEMP 1
+-/* #define HAVE_MKSTEMPS 1 */
++#define HAVE_MKSTEMPS 1
+ /* #undef SELECT_TAKES_INT */
+ #define GHOSTSCRIPT "gs"
+
