a82070940049d0deb11d719fefad6b018a1d7a76
Patrick Li <pat@FreeBSD.org>
2002-06-24 04:54:43 +0000

    Add new port wmfmixer - Yet another audio mixer
    PR:		ports/39622
    Submitted by:	Alexey Dokuchaev <danfe@regency.nsu.ru>

---
+++ audio/wmfmixer/files/patch-wmfmixer.c
@@ -0,0 +1,11 @@
+--- wmfmixer.c.orig	Fri Jun 21 14:58:35 2002
++++ wmfmixer.c	Thu Jun 20 22:16:34 2002
+@@ -21,7 +21,7 @@
+ 
+ #include "wmfmixer.xpm"
+ 
+-#include "linux/soundcard.h"
++#include "sys/soundcard.h"
+ 
+   /***********/
+  /* Defines */
