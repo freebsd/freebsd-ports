028d13cd1ee86ee88b1d64ed7b23f69301bd02c0
ijliao <ijliao@FreeBSD.org>
2002-06-25 06:38:17 +0000

    upgrade to 2.2.8

--- devel/gindent/files/patch-indent.h
+++ devel/gindent/files/patch-indent.h
@@ -1,5 +1,5 @@
---- indent.h.orig	Thu Nov 16 19:01:04 2000
-+++ indent.h	Fri Apr 20 01:16:45 2001
+--- src/indent.h.orig	Thu Nov 16 19:01:04 2000
++++ src/indent.h	Fri Apr 20 01:16:45 2001
 @@ -63 +63 @@
 -#define DEFAULT_RIGHT_MARGIN 78
 +#define DEFAULT_RIGHT_MARGIN 79
