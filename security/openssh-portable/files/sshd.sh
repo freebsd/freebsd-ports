ce07a2c968498cee6c9fcdaceead0ee568474925
dinoex <dinoex@FreeBSD.org>
2002-06-26 04:05:57 +0000

    Migrate configuration files to $PREFIX/etc/ssh/ Add ${PREFIX}/etc/rc.d/sshd.sh.sample

---
+++ security/openssh-portable/files/sshd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+case "$1" in
+start)
+	__PREFIX__/sbin/sshd
+	echo -n ' sshd'
+	;;
+stop)
+	if [ -f /var/run/sshd.pid ]; then
+		kill -TERM `cat /var/run/sshd.pid`
+		rm -f /var/run/sshd.pid
+		echo -n ' sshd'
+	fi
+	;;
+restart)
+	if [ -f /var/run/sshd.pid ]; then
+		kill -HUP `cat /var/run/sshd.pid`
+		echo 'sshd restarted'
+	fi
+	;;
+*)
+	echo "Usage: ${0##*/}: { start | stop | restart }" 2>&1
+	exit 65
+	;;
+esac
