6e92b681172f0b572e7431589d5f0146f1eecc51
Dirk Meyer <dinoex@FreeBSD.org>
2002-06-26 15:21:27 +0000

    Security FIX, Please update to this Version.
    Options for both:
    USE_OPENSSL_BASE=yes
    	uses an older opensssl in the base system.
    
    Options for portable:
    OPENSSH_OVERWRITE_BASE=yes
    	includes USE_OPENSSL_BASE=yes
    	installls in the paths of the base system

--- security/openssh-portable/files/patch-auth2-chall.c
+++ security/openssh-portable/files/patch-auth2-chall.c
@@ -1,32 +1,5 @@
---- auth2-chall.c.orig	Wed Jun 19 02:27:55 2002
-+++ auth2-chall.c	Wed Jun 26 16:59:05 2002
-@@ -40,20 +40,20 @@
- 
- #ifdef BSD_AUTH
- extern KbdintDevice bsdauth_device;
--#else
--#ifdef SKEY
-+#elif defined(USE_PAM)
-+extern KbdintDevice pam_device;
-+#elif defined(SKEY)
- extern KbdintDevice skey_device;
- #endif
--#endif
- 
- KbdintDevice *devices[] = {
- #ifdef BSD_AUTH
- 	&bsdauth_device,
--#else
--#ifdef SKEY
-+#elif defined(USE_PAM)
-+	&pam_device,
-+#elif defined(SKEY)
- 	&skey_device,
- #endif
--#endif
- 	NULL
- };
- 
+--- auth2-chall.c	2002/06/19 00:27:55	1.18
++++ auth2-chall.c	2002/06/26 13:55:37	1.19
 @@ -63,6 +63,7 @@
  	char *devices;
  	void *ctxt;
