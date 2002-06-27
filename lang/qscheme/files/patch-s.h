b622a506453fdb027cc659e558df7885166ba80e
will <will@FreeBSD.org>
2002-06-27 14:32:30 +0000

    Add qscheme 0.5.1, a small and fast Scheme interpreter.
    PR:		34999
    Submitted by:	erik@smluc.org

---
+++ lang/qscheme/files/patch-s.h
@@ -0,0 +1,11 @@
+--- s.h.orig	Sun Feb 10 12:45:20 2002
++++ s.h	Sun Feb 10 12:45:32 2002
+@@ -203,7 +203,7 @@
+ 
+ #ifdef SCM_WITH_THREADS
+ /*** same as in Boehm's GC */
+-#define SCM_SIG_SUSPEND		SIGPWR
++#define SCM_SIG_SUSPEND		SIGUSR1
+ #define SCM_SIG_RESUME		SIGXCPU
+ #endif
+ 
