b22fa20a4c5e78f371b76827b2c090cb15d2b9da
dinoex <dinoex@FreeBSD.org>
2002-06-28 05:28:07 +0000

    Defaults changed: (Gregory Sutter)  ChallengeResponseAuthentication no  UseLogin no
    patch for configure, to detect MAP_ANON submitted by:
    Christophe Labouisse,Michael Handler,Gert Doering,Phil Oleson,Dave Baker
    
    fix missing includes for "canohost.h"

---
+++ security/hpn-ssh/files/patch-sshpty.c
@@ -0,0 +1,12 @@
+--- sshpty.c.orig	Wed Jun 26 01:21:42 2002
++++ sshpty.c	Fri Jun 28 07:09:38 2002
+@@ -30,6 +30,9 @@
+ #ifdef HAVE_PTY_H
+ # include <pty.h>
+ #endif
++#ifdef __FreeBSD__
++#include <libutil.h>
++#endif
+ #if defined(HAVE_DEV_PTMX) && defined(HAVE_SYS_STROPTS_H)
+ # include <sys/stropts.h>
+ #endif
