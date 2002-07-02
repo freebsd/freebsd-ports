cd4aae388c58cfa1b4de2b7ef9f91cc8922a2ffd
Dmitry Sivachenko <demon@FreeBSD.org>
2002-07-02 08:44:07 +0000

    Update: Apache http server: 1.3.26 Russian patch (RA): 30.14 mod_ssl:            2.8.10
    PR:		40037
    Submitted by:	"Lev A. Serebryakov" <lev@serebryakov.spb.ru> (maintainer)

---
+++ russian/apache13-modssl/files/apache.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
+    echo "$0: Cannot determine the PREFIX" >&2
+    exit 1
+fi
+
+case "$1" in
+start)
+	[ -x ${PREFIX}/sbin/apachectl ] && ${PREFIX}/sbin/apachectl startssl > /dev/null && echo -n ' apache'
+	;;
+stop)
+	[ -r /var/run/httpd.pid ] && ${PREFIX}/sbin/apachectl stop > /dev/null && echo -n ' apache'
+	;;
+*)
+	echo "Usage: `basename $0` {start|stop}" >&2
+	;;
+esac
+
+exit 0
