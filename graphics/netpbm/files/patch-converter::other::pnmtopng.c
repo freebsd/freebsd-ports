3ae6bbc008097c5b6dbfdc21088c62eabba087ce
pat <pat@FreeBSD.org>
2002-07-06 01:17:43 +0000

    Update to 10.3
    PR:		40245
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

--- graphics/netpbm/files/patch-converter::other::pnmtopng.c
+++ graphics/netpbm/files/patch-converter::other::pnmtopng.c
@@ -1,16 +1,16 @@
---- converter/other/pnmtopng.c.orig	Thu Apr 25 09:37:11 2002
-+++ converter/other/pnmtopng.c	Tue Jun 18 00:00:00 2002
-@@ -1857,8 +1857,13 @@
-       fprintf(stderr,"pnmtopng version %s.\n", VERSION);
-       fprintf(stderr, "   Compiled with libpng %s; using libpng %s.\n",
-         PNG_LIBPNG_VER_STRING, png_libpng_ver);
+--- converter/other/pnmtopng.c.orig	Sun Jun 23 12:51:37 2002
++++ converter/other/pnmtopng.c	Sat Jul  6 00:00:00 2002
+@@ -1903,8 +1903,13 @@
+     */
+     fprintf(stderr, "   Compiled with libpng %s.\n",
+             PNG_LIBPNG_VER_STRING);
 +#ifdef zlib_version
-       fprintf(stderr, "   Compiled with zlib %s; using zlib %s.\n",
-         ZLIB_VERSION, zlib_version);
+     fprintf(stderr, "   Compiled with zlib %s; using zlib %s.\n",
+             ZLIB_VERSION, zlib_version);
 +#else
-+      fprintf(stderr, "   Compiled with zlib %s.\n",
-+        ZLIB_VERSION);
++    fprintf(stderr, "   Compiled with zlib %s.\n",
++            ZLIB_VERSION);
 +#endif
-       fprintf(stderr,    
-         "   Compiled with %d-bit netpbm support (PPM_OVERALLMAXVAL = %d).\n",
-         pm_maxvaltobits (PPM_OVERALLMAXVAL), PPM_OVERALLMAXVAL);
+     fprintf(stderr,    
+             "   Compiled with %d-bit netpbm support "
+             "(PPM_OVERALLMAXVAL = %d).\n",
