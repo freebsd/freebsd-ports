0360be38d574b7347c65887704ca3468159fa3ff
Hye-Shik Chang <perky@FreeBSD.org>
2002-07-08 07:42:04 +0000

    Fix more fatal bugs on htpasswd(1)
    Requested by:	"Jon Noack" <noackjr@compgeek.com>
    Obtained from:	Apache Group CVS (rev 1.49-1.51)

--- www/apache22/files/patch-support:htpasswd.c
+++ www/apache22/files/patch-support:htpasswd.c
@@ -1,6 +1,14 @@
---- support/htpasswd.c	2002/06/18 02:58:36	1.48
-+++ support/htpasswd.c	2002/06/19 17:31:19	1.49
-@@ -375,7 +375,7 @@
+--- support/htpasswd.c.orig	Tue Jun 18 11:58:36 2002
++++ support/htpasswd.c	Mon Jul  8 16:30:14 2002
+@@ -236,6 +236,7 @@
+     strcpy(record, user);
+     strcat(record, ":");
+     strcat(record, cpw);
++    strcat(record, "\n");
+     return 0;
+ }
+ 
+@@ -375,7 +376,7 @@
          }
      }
  
@@ -8,4 +16,12 @@
 +    if ((*mask & APHTP_NEWFILE) && (*mask & APHTP_NOFILE)) {
          apr_file_printf(errfile, "%s: -c and -n options conflict\n", argv[0]);
          exit(ERR_SYNTAX);
+     }
+@@ -592,6 +593,7 @@
+                  */
+                 apr_file_printf(errfile, "Updating ");
+                 putline(ftemp, record);
++                found++;
+             }
+         }
      }
