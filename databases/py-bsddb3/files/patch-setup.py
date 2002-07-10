6f1ad5073ee167680df8853603d68b7889b9e703
perky <perky@FreeBSD.org>
2002-07-10 19:15:39 +0000

    Update to 3.4.0 (now builds with db4 by default unless defined(WITH_DB3))

--- databases/py-bsddb3/files/patch-setup.py
+++ databases/py-bsddb3/files/patch-setup.py
@@ -1,13 +1,13 @@
---- setup.py.orig	Fri Sep 14 00:15:30 2001
-+++ setup.py	Tue Feb  5 12:37:16 2002
+--- setup.py.orig	Thu Jul 11 03:48:34 2002
++++ setup.py	Thu Jul 11 03:51:34 2002
 @@ -68,8 +68,8 @@
      # figure out from the base setting where the lib and .h are
      if not incdir: incdir = os.path.join(BERKELEYDB_DIR, 'include')
      if not libdir: libdir = os.path.join(BERKELEYDB_DIR, 'lib')
 -    if not '-ldb' in LIBS:
 -        libname = ['db']
-+    if not '-ldb3' in LIBS:
-+        libname = ['db3']
++    if not '-l%%DBVERSION%%' in LIBS:
++        libname = ['%%DBVERSION%%']
      else:
          libname = []
      utils = []
