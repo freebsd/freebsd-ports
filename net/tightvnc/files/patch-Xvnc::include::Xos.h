4747f9a4b5c239b541645d317eb6b276d1918d0b
Clive Lin <clive@FreeBSD.org>
2002-07-12 18:53:30 +0000

    Fix build fault on -CURRENT.

---
+++ net/tightvnc/files/patch-Xvnc::include::Xos.h
@@ -0,0 +1,10 @@
+--- Xvnc/include/Xos.h.orig	Sat Jul 13 02:14:08 2002
++++ Xvnc/include/Xos.h	Sat Jul 13 02:14:15 2002
+@@ -151,7 +151,6 @@
+ #endif /* X_NOT_POSIX else */
+ 
+ #ifdef CSRG_BASED
+-#include <stdlib.h>
+ #include <unistd.h>
+ #endif /* CSRG_BASED */
+ 
