d0e524741440959ec2ac9b369ac325b85aff2fff
pat <pat@FreeBSD.org>
2002-07-14 18:45:12 +0000

    Fix mangled patches (whitespace)
    PR:		ports/40533
    Submitted by:	maintainer

--- audio/cmt/files/patch-cmt.cpp
+++ audio/cmt/files/patch-cmt.cpp
@@ -1,15 +1,15 @@
---- cmt.cpp.orig       Wed Jun  5 23:15:32 2002
-+++ cmt.cpp    Wed Jun  5 23:16:33 2002
+--- cmt.cpp.orig	Sat Jul 13 20:59:44 2002
++++ cmt.cpp	Sat Jul 13 21:00:21 2002
 @@ -115,9 +115,9 @@
  void CMT_Descriptor::
  addPort(LADSPA_PortDescriptor          iPortDescriptor,
-       const char *                   pcPortName,
--      LADSPA_PortRangeHintDescriptor iHintDescriptor = 0,
--      LADSPA_Data                    fLowerBound = 0,
--      LADSPA_Data                    fUpperBound = 0) {
-+      LADSPA_PortRangeHintDescriptor iHintDescriptor,
-+      LADSPA_Data                    fLowerBound,
-+      LADSPA_Data                    fUpperBound) {
-
+ 	const char *                   pcPortName,
+-	LADSPA_PortRangeHintDescriptor iHintDescriptor = 0,
+-	LADSPA_Data                    fLowerBound = 0,
+-	LADSPA_Data                    fUpperBound = 0) {
++	LADSPA_PortRangeHintDescriptor iHintDescriptor,
++	LADSPA_Data                    fLowerBound,
++	LADSPA_Data                    fUpperBound) {
+ 
    unsigned long lOldPortCount = PortCount;
    unsigned long lNewPortCount = PortCount + 1;
