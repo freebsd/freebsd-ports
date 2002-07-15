f5c06a6998e3231b83ec9274d1a79284266d762a
ijliao <ijliao@FreeBSD.org>
2002-07-15 10:24:23 +0000

    add xrootconsole 0.5 Writes any log live onto the background
    PR:		40513
    Submitted by:	"incabulos@mac.com &" <root@muddy.ath.cx>

---
+++ x11/xrootconsole/files/patch-main.c
@@ -0,0 +1,10 @@
+--- main.c.orig	Mon Jul  1 14:24:02 2002
++++ main.c	Mon Jul 15 18:14:14 2002
+@@ -40,6 +40,7 @@
+ #include "util.h"
+ #include <X11/Xlib.h>
+ #include <X11/Xutil.h>
++#include <X11/Xos.h>
+ #include <assert.h>
+ #include <errno.h>
+ #include <stdio.h>
