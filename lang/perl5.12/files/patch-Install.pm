09147ee10221603a955483994e9e797abdab29e9
Anton Berezin <tobez@FreeBSD.org>
2002-07-21 17:09:12 +0000

    Update to 5.8.0 (after repocopy).

--- lang/perl5.12/files/patch-Install.pm
+++ lang/perl5.12/files/patch-Install.pm
@@ -1,3 +1,5 @@
+$FreeBSD$
+
 --- lib/ExtUtils/Install.pm.orig	Fri Feb 23 03:57:55 2001
 +++ lib/ExtUtils/Install.pm	Wed Jul 10 17:57:13 2002
 @@ -132,7 +132,7 @@
@@ -9,12 +11,3 @@
  		if (-f $targetfile){
  		    forceunlink($targetfile) unless $nonono;
  		} else {
-@@ -156,7 +156,7 @@
- 	    } else {
- 		inc_uninstall($_,$File::Find::dir,$verbose,0); # nonono set to 0
- 	    }
--	    $packlist->{$origfile}++;
-+	    $packlist->{$targetfile}++;
- 
- 	}, ".");
- 	chdir($cwd) or Carp::croak("Couldn't chdir to $cwd: $!");
