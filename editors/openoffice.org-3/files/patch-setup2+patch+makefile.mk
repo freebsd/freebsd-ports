ce81c4a8e6e2fd58582a33277742056e6c515e67
mbr <mbr@FreeBSD.org>
2002-07-21 23:06:51 +0000

    Add PAM support

---
+++ editors/openoffice.org-3/files/patch-setup2+patch+makefile.mk
@@ -0,0 +1,11 @@
+--- ../setup2/patch/makefile.mk.orig	Sun Jul 21 00:21:46 2002
++++ ../setup2/patch/makefile.mk	Sun Jul 21 00:21:52 2002
+@@ -92,7 +92,7 @@
+ .IF "$(OS)"=="SOLARIS"
+ 	STATIC_LIBS+= -Bdynamic -lnsl -lsocket -lposix4
+ .ENDIF
+-.IF "$(OS)"=="LINUX"
++.IF "$(OS)"=="LINUX" || "$(OS)"=="FREEBSD"
+ 	STATIC_LIBS+= -Bdynamic -lcrypt
+ .ENDIF
+ .ELSE
