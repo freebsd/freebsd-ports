822ca745ad67b4240545331c7b498529c87df061
ijliao <ijliao@FreeBSD.org>
2002-07-21 16:48:56 +0000

    fix buffer overflow problem
    PR:		40832
    Submitted by:	Edwin Groothuis <edwin@mavetju.org>

--- games/race/files/patch-textures.c
+++ games/race/files/patch-textures.c
@@ -1,8 +1,11 @@
---- src/textures.c.orig	Tue Jul 16 23:50:37 2002
-+++ src/textures.c	Tue Jul 16 23:50:46 2002
-@@ -35,7 +35,7 @@
+--- src/textures.c.orig	Fri Mar  8 01:44:46 2002
++++ src/textures.c	Sun Jul 21 21:14:50 2002
+@@ -33,9 +33,9 @@
+ 
+ int loadtexture(char *dir, char *file, int filter, int repeat, int id){
    SDL_Surface	*image;
-   char		location[6+strlen(dir)+strlen(file)];
+-  char		location[6+strlen(dir)+strlen(file)];
++  char		location[60+strlen(dir)+strlen(file)];
    
 -  sprintf(location,"data/%s/%s",dir,file);
 +  sprintf(location,"%%DATADIR%%/data/%s/%s",dir,file);
