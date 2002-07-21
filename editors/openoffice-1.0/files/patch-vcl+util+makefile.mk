4668165b1ad52a7f12f5f5e13bfbfb63dffa2460
Martin Blapp <mbr@FreeBSD.org>
2002-07-21 22:59:15 +0000

    Activate sound support.

---
+++ editors/openoffice-1.0/files/patch-vcl+util+makefile.mk
@@ -0,0 +1,14 @@
+--- ../vcl/util/makefile.mk.orig	Sun Jul 21 00:12:09 2002
++++ ../vcl/util/makefile.mk	Sun Jul 21 00:12:11 2002
+@@ -293,9 +293,9 @@
+ .ENDIF          # "$(OS)"=="MACOSX"
+ .ENDIF          # "$(OS)"=="SOLARIS"
+ 
+-.IF "$(OS)"=="LINUX" || "$(OS)"=="SOLARIS"
++.IF "$(OS)"=="LINUX" || "$(OS)"=="SOLARIS" || "$(OS)"=="FREEBSD"
+ SHL1STDLIBS += -laudio
+-.ENDIF          # "$(OS)"=="LINUX" || "$(OS)"=="SOLARIS"
++.ENDIF          # "$(OS)"=="LINUX" || "$(OS)"=="SOLARIS" || "$(OS)"=="FREEBSD"
+ 
+ .ENDIF          # "$(GUI)"=="UNX"
+ 
