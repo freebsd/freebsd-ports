d1bb93dbec056e823db3bcfb3db59f18c20909f6
kuriyama <kuriyama@FreeBSD.org>
2002-07-22 05:57:58 +0000

    Upgrade to 5.0.2. Remove statvfs hack.

--- net-mgmt/net-snmp53/files/patch-hr_storage.c
+++ net-mgmt/net-snmp53/files/patch-hr_storage.c
@@ -1,6 +1,24 @@
---- agent/mibgroup/host/hr_storage.c.orig	Sat Apr 20 16:30:03 2002
-+++ agent/mibgroup/host/hr_storage.c	Wed Apr 24 01:37:29 2002
-@@ -604,7 +604,15 @@
+--- agent/mibgroup/host/hr_storage.c.orig	Thu Jul  4 22:00:31 2002
++++ agent/mibgroup/host/hr_storage.c	Mon Jul 22 14:38:58 2002
+@@ -148,7 +148,7 @@
+ #define HRFS_mount	mnt_mountp
+ #define HRFS_statfs	statvfs
+ 
+-#elif defined(HAVE_STATVFS)
++#elif defined(HAVE_STATVFS) && defined(HAVE_MNTENT)
+ 
+ extern struct mntent *HRFS_entry;
+ extern int      fscount;
+@@ -563,7 +563,7 @@
+         }
+     case HRSTORE_UNITS:
+         if (store_idx < HRS_TYPE_FS_MAX)
+-#if STRUCT_STATVFS_HAS_F_FRSIZE
++#if defined(STRUCT_STATVFS_HAS_F_FRSIZE) && defined(HAVE_MNTENT)
+             long_return = stat_buf.f_frsize;
+ #else
+             long_return = stat_buf.f_bsize;
+@@ -646,7 +646,15 @@
                       i++)
                      long_return += mbstat.m_mtypes[i];
  #elif defined(MBSTAT_SYMBOL)
@@ -16,7 +34,7 @@
  #elif defined(NO_DUMMY_VALUES)
                  return NULL;
  #else
-@@ -654,7 +662,15 @@
+@@ -704,7 +712,15 @@
                      * mbpool.pr_size + (mclpool.pr_nget - mclpool.pr_nput)
                      * mclpool.pr_size;
  #elif defined(MBSTAT_SYMBOL)
