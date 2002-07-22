4bd953d9e9f77c9e1688cc3fec99356749069fce
Jun Kuriyama <kuriyama@FreeBSD.org>
2002-07-22 01:03:42 +0000

    Add nagios-plugins after repocopy from netsaint-plugins.
    Submitted by:	Blaz Zupan <blaz@si.FreeBSD.org>
    Repocopied by:	joe

--- net-mgmt/nagios-plugins/files/patch-check_ssh.c
+++ net-mgmt/nagios-plugins/files/patch-check_ssh.c
@@ -1,15 +1,15 @@
---- plugins/check_ssh.c.orig	Fri Jun 21 12:17:43 2002
-+++ plugins/check_ssh.c	Fri Jun 21 12:23:16 2002
-@@ -246,7 +246,7 @@
- 			printf("%s\n", output);
+--- plugins/check_ssh.c.orig	Thu Feb 28 07:42:59 2002
++++ plugins/check_ssh.c	Sun Jul 14 09:59:41 2002
+@@ -250,7 +250,7 @@
+ 			printf ("%s\n", output);
  		ssh_proto = output + 4;
- 		ssh_server = ssh_proto + strspn(ssh_proto, "0123456789-. ");
--		ssh_proto[strspn(ssh_proto, "0123456789-. ")] = 0;
-+		ssh_proto[strspn(ssh_proto, "0123456789. ")] = 0;
+ 		ssh_server = ssh_proto + strspn (ssh_proto, "0123456789-. ");
+-		ssh_proto[strspn (ssh_proto, "0123456789-. ")] = 0;
++		ssh_proto[strspn (ssh_proto, "0123456789. ")] = 0;
  		printf
  			("SSH ok - protocol version %s - server version %s\n",
- 		       ssh_proto, ssh_server);
-@@ -272,7 +272,7 @@
+ 			 ssh_proto, ssh_server);
+@@ -277,7 +277,7 @@
  {
  	printf
  		("Usage:\n"
