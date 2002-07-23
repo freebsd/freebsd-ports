b0c55b07ca7363a20e3d729e52e7c84888a9ef85
Christian Weisgerber <naddy@FreeBSD.org>
2002-07-23 22:30:18 +0000

    * Unbreak build of FORTRAN parts.
    PR:		40928
    Submitted by:	Amar Takhar <verm@drunkmonk.net>
    
    * Fix build on -CURRENT.

---
+++ math/femlab/files/patch-fsource_y.tab.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- fsource/y.tab.c.orig	Wed Jul 24 00:14:46 2002
++++ fsource/y.tab.c	Wed Jul 24 00:14:56 2002
+@@ -38,7 +38,7 @@
+ #include <memory.h>
+ #endif
+ 
+-#include <values.h>
++#include <float.h>
+ 
+ #ifdef __cplusplus
+ 
