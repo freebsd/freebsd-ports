de29081f22479e04b6876a1c8debb69a4512068d
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2002-07-25 15:29:55 +0000

    Update to 0.2.7

--- graphics/xine_d5d_plugin/files/patch-src:input_dvd.c
+++ graphics/xine_d5d_plugin/files/patch-src:input_dvd.c
@@ -1,5 +1,5 @@
---- src/input_dvd.c.orig	Thu May  2 06:35:36 2002
-+++ src/input_dvd.c	Sun Jun  2 04:17:03 2002
+--- src/input_dvd.c.orig	Tue Jul  9 06:44:02 2002
++++ src/input_dvd.c	Mon Jul 15 18:58:29 2002
 @@ -33,7 +33,6 @@
  #include <string.h>
  #include <errno.h>
@@ -8,12 +8,3 @@
  #include <sys/wait.h>
  
  #include <sys/poll.h>
-@@ -906,7 +905,7 @@
-   dvdnav_input_plugin_t *this;
-   config_values_t *config = xine->config;
- 
--  if (iface != 6) {
-+  if (iface != 7) {
-     printf("d5d input plugin doesn't support plugin API version %d.\n"
- 	   "PLUGIN DISABLED.\n"
- 	   "This means there's a version mismatch between xine and this input"
