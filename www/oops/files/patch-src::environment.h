5ee4f82222d510126f16f3377d45efc12733c048
Christian Weisgerber <naddy@FreeBSD.org>
2002-07-26 12:14:00 +0000

    Fix build on -CURRENT.

--- www/oops/files/patch-src::environment.h
+++ www/oops/files/patch-src::environment.h
@@ -1,6 +1,9 @@
---- src/environment.h.orig	Wed Dec 13 17:33:19 2000
-+++ src/environment.h	Wed Dec 13 17:33:40 2000
-@@ -55,7 +55,7 @@
+
+$FreeBSD$
+
+--- src/environment.h.orig	Wed Dec 12 16:49:39 2001
++++ src/environment.h	Thu Jul 25 20:10:35 2002
+@@ -67,7 +67,7 @@
  #include	<thread.h>
  #endif
  #include	<pthread.h>
@@ -8,3 +11,18 @@
 +#if	!defined(HAVE_PTHREAD_RWLOCK_INIT) && !defined(FREEBSD)
  #include	"rwlock.h"
  #endif
+ 
+@@ -119,11 +119,11 @@
+ #include	<sys/stat.h>
+ #endif
+ 
+-#if defined(HAVE_SYS_INTTYPES_H)
++#if defined(HAVE_INTTYPES_H)
+ #include    <inttypes.h>
+ #endif
+ 
+-#if !defined(HAVE_SYS_INTTYPES_H) && !defined(_STDINT_H)
++#if !defined(HAVE_INTTYPES_H) && !defined(_STDINT_H)
+ typedef unsigned int    uintptr_t;
+ #endif
+ 
