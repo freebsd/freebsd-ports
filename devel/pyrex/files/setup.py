af740b3b32776772a49011c732387ee72b88c71c
Hye-Shik Chang <perky@FreeBSD.org>
2002-07-29 00:15:47 +0000

    Update to 0.4

--- devel/pyrex/files/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name = 'pyrex',
-    version = "%%PORTVERSION%%",
-    packages = ['Pyrex', 'Pyrex.Compiler', 'Pyrex.Mac'],
-    scripts = ['pyrexc'],
-    )
