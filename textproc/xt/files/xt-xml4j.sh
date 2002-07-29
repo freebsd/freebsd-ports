44cbe378e9298ea11d53dc4e71f3c6170fc8a965
Jun Kuriyama <kuriyama@FreeBSD.org>
2002-07-29 04:34:38 +0000

    Fit to new bsd.java.mk.
    Submitted by:	KUNISHIMA Takeo <kunishi@acm.org>
    PR:		ports/40999

--- textproc/xt/files/xt-xml4j.sh
+++ textproc/xt/files/xt-xml4j.sh
@@ -3,4 +3,4 @@
 
 export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xerces.jar:${CLASSPATH}
 
-java -Dcom.jclark.xsl.sax.parser=org.apache.xerces.parsers.SAXParser com.jclark.xsl.sax.Driver $*
+javavm -Dcom.jclark.xsl.sax.parser=org.apache.xerces.parsers.SAXParser com.jclark.xsl.sax.Driver $*
