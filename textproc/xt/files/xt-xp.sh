b742d03f1e2bb3e562970021587e23c78a532640
kuriyama <kuriyama@FreeBSD.org>
2002-07-29 04:34:38 +0000

    Fit to new bsd.java.mk.
    Submitted by:	KUNISHIMA Takeo <kunishi@acm.org>
    PR:		ports/40999

--- textproc/xt/files/xt-xp.sh
+++ textproc/xt/files/xt-xp.sh
@@ -3,4 +3,4 @@
 
 export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xp-0.5.jar:%%PREFIX%%/share/java/classes/sax.jar:${CLASSPATH}
 
-java com.jclark.xsl.sax.Driver $*
+javavm com.jclark.xsl.sax.Driver $*
