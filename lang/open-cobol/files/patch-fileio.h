828b2e2e855e7774d59497fa92db02e8265f2ee3
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-08-06 13:09:46 +0000

    add open-cobol 0.9.7 An open-source COBOL compiler

---
+++ lang/open-cobol/files/patch-fileio.h
@@ -0,0 +1,11 @@
+--- libcob/fileio.h.orig	Tue Aug  6 15:19:11 2002
++++ libcob/fileio.h	Tue Aug  6 15:19:18 2002
+@@ -21,7 +21,7 @@
+ #define COB_FILEIO_H
+ 
+ #include <stdio.h>
+-#include <db.h>
++#include <db4/db.h>
+ #include <libcob/common.h>
+ 
+ #define COB_EQ	1 	/* x == y */
