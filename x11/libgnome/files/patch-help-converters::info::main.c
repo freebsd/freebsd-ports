c6bc0f61ba9a49bbeb8f9e7dce21015b1d3fbb5d
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-08-07 07:19:55 +0000

    Update to 2.0.2.

--- x11/libgnome/files/patch-help-converters::info::main.c
+++ x11/libgnome/files/patch-help-converters::info::main.c
@@ -1,25 +1,22 @@
-
-$FreeBSD$
-
---- help-converters/info/main.c	2002/02/20 18:02:08	1.1
-+++ help-converters/info/main.c	2002/02/20 18:08:43
-@@ -87,13 +87,15 @@
- 	    char buf[PATH_MAX];
+--- help-converters/info/main.c.orig	Mon Aug  5 14:06:06 2002
++++ help-converters/info/main.c	Wed Aug  7 01:52:35 2002
+@@ -104,13 +104,15 @@
+                 char buf[PATH_MAX];
  
- 	    /* First, find the directory that the info file is in. */
--	    dirs[0] = "/usr/info";
--	    dirs[1] = "/usr/share/info";
-+	    dirs[0] = "/usr/share/info";
-+	    dirs[1] = PREFIX "/info";
-+	    dirs[2] = LOCALBASE "/info";
-+	    dirs[3] = X11BASE "/info";
- 	    /* We now have at least one directory to look in. This is
- 	     * necessary because we may not have an 'INFOPATH' set */
--	    ndirs = 2;
-+	    ndirs = 4;
- 	    if(infopath)
--	      for(ndirs = 2, ctmp = strtok(infopath, ":"); ndirs < 64 && ctmp; ndirs++, ctmp = strtok(NULL, ":"))
-+	      for(ndirs = 4, ctmp = strtok(infopath, ":"); ndirs < 64 && ctmp; ndirs++, ctmp = strtok(NULL, ":"))
- 		dirs[ndirs] = strdup(ctmp);
- 
- 	    for(i = 0; i < ndirs; i++)
+                 /* First, find the directory that the info file is in. */
+-                dirs[0] = "/usr/info";
+-                dirs[1] = "/usr/share/info";
++		dirs[0] = "/usr/share/info";
++		dirs[1] = PREFIX "/info";
++		dirs[2] = LOCALBASE "/info";
++		dirs[3] = X11BASE "/info";
+                 /* We now have at least one directory to look in. This is
+                  * necessary because we may not have an 'INFOPATH' set */
+-                ndirs = 2;
++                ndirs = 4;
+                 if(infopath) {
+-                        for (ndirs = 2, ctmp = strtok(infopath, ":"); ndirs < 64 && ctmp; ndirs++, ctmp = strtok(NULL, ":")) {
++                        for (ndirs = 4, ctmp = strtok(infopath, ":"); ndirs < 64 && ctmp; ndirs++, ctmp = strtok(NULL, ":")) {
+                                 dirs[ndirs] = strdup(ctmp);
+                         }
+                 }
