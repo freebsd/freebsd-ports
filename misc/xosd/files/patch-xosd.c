0d1aa7eff6feba82fb5a958c41bb6ea886513e1d
perky <perky@FreeBSD.org>
2002-08-09 05:24:27 +0000

    Update to 1.0.0

--- misc/xosd/files/patch-xosd.c
+++ misc/xosd/files/patch-xosd.c
@@ -1,16 +1,16 @@
---- xosd.c	Thu Mar 22 18:55:45 2001
-+++ xosd.c.new	Sun May 20 23:13:52 2001
-@@ -270,7 +270,7 @@
+--- src/xosd.c.orig	Fri Aug  9 13:49:59 2002
++++ src/xosd.c	Fri Aug  9 13:50:30 2002
+@@ -291,7 +291,7 @@
        if (!XCheckWindowEvent (osd->display, osd->window, ExposureMask, &report))
  	 {
- 	 MUTEX_RELEASE ();
--	 usleep (500);
-+	 usleep (100000);
- 	 continue;
+ 	   MUTEX_RELEASE ();
+-	   usleep (500);
++	   usleep (100000);
+ 	   continue;
  	 }
        MUTEX_RELEASE ();
-@@ -304,7 +304,7 @@
-    
+@@ -326,7 +326,7 @@
+ 
     while (!osd->done)
        {
 -      usleep (1000);
