a47386a36d95bc12e52fb543460ca374810eaee9
naddy <naddy@FreeBSD.org>
2002-08-12 23:15:57 +0000

    Update to 0.3.2.
    PR:		41586
    Submitted by:	Christian Brueffer <chris@unixpages.org>

---
+++ x11-clocks/xmms-alarm/files/patch-ltmain.sh
@@ -0,0 +1,18 @@
+
+$FreeBSD$
+
+--- ltmain.sh.orig	Tue Aug 13 01:08:36 2002
++++ ltmain.sh	Tue Aug 13 01:08:53 2002
+@@ -4247,10 +4247,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
