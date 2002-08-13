984590a5e37dbc239f3c3b8cc96eca493ee961cc
Jim Mock <jim@FreeBSD.org>
2002-08-13 01:39:52 +0000

    Update to 0.4.3.
    Submitted by:	Marijn Meijles <marijn@bitpit.net>

---
+++ net-im/ayttm/files/patch-yahoo.c
@@ -0,0 +1,12 @@
+--- modules/yahoo2/yahoo.c.orig	Sun Jul 14 17:43:14 2002
++++ modules/yahoo2/yahoo.c	Wed Aug  7 23:55:36 2002
+@@ -24,8 +24,8 @@
+  */
+ 
+ #include <netdb.h>
+-#include <sys/socket.h>
+ #include <sys/types.h>
++#include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
+ 
