1d3d4ec4597b8c52210b1b65a89dafc25a22133c
Anders Nordby <anders@FreeBSD.org>
2002-08-13 21:39:47 +0000

    Fix startup-script for -current /bin/sh issues, and set PREFIX during install.
    Requested by:	obrien

--- www/thttpd/files/thttpd.sh
+++ www/thttpd/files/thttpd.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
-    echo "$0: Cannot determine the PREFIX" >&2
-    exit 1
-fi
-
 case "$1" in
 start)
-	[ -x ${PREFIX}/sbin/thttpd_wrapper ] && ${PREFIX}/sbin/thttpd_wrapper & && echo -n ' thttpd'
+	if [ -x %%PREFIX%%/sbin/thttpd_wrapper ]; then
+		%%PREFIX%%/sbin/thttpd_wrapper &
+		echo -n ' thttpd'
+	fi
 	;;
 stop)
 	kill `cat /var/run/thttpd_wrapper.pid` && killall thttpd && echo -n ' thttpd'
