8e92340f89af61a862a7618c6b57cb32eec69f22
Christian Weisgerber <naddy@FreeBSD.org>
2002-08-14 00:03:26 +0000

    * Always depend on external libvorbis when building with Ogg Vorbis support. * This port does not install any shared libraries. * Don't clobber an existing configuration file or remove a modified one. * Don't install .la files for plugins. * Fix build on -CURRENT. * Fix build on 64-bit platforms.

--- audio/gnump3d/files/patch-config::ltmain.sh
+++ audio/gnump3d/files/patch-config::ltmain.sh
@@ -1,5 +1,8 @@
---- config/ltmain.sh.orig	Sun Jul 14 08:53:50 2002
-+++ config/ltmain.sh	Tue Jul 16 00:38:55 2002
+
+$FreeBSD$
+
+--- config/ltmain.sh.orig	Sun Jul 14 01:53:50 2002
++++ config/ltmain.sh	Tue Aug 13 18:00:19 2002
 @@ -967,6 +967,7 @@
  	;;
  
@@ -8,3 +11,25 @@
  	avoid_version=yes
  	continue
  	;;
+@@ -1061,7 +1062,7 @@
+ 	  esac
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	 *-*-openbsd* | *-*-freebsd*)
++	 *-*-openbsd*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -4273,10 +4274,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
