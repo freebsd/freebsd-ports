64b0530867a4385e80341f5f704889d3a34c3eae
Eric Anholt <anholt@FreeBSD.org>
2002-08-17 21:54:31 +0000

    Add enigma 0.50, enigma is a reimplementation of Oxyd, a puzzle game.
    PR:		40967
    Submitted by:	Edwin Groothuis

---
+++ games/enigma/files/patch-video.cc
@@ -0,0 +1,10 @@
+--- px/video.cc.orig	Sat Aug 17 12:06:45 2002
++++ px/video.cc	Sat Aug 17 12:06:56 2002
+@@ -21,7 +21,6 @@
+ #include "tools.hh"
+ 
+ #include "SDL_image.h"
+-#include "SDL_syswm.h"
+ 
+ #include <cassert>
+ #include <string>
