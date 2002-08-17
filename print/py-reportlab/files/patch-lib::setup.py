12623f4c416e2ab6c5af57469059c76495dfe1ff
perky <perky@FreeBSD.org>
2002-08-17 10:02:38 +0000

    Update to 1.15

--- print/py-reportlab/files/patch-lib::setup.py
+++ print/py-reportlab/files/patch-lib::setup.py
@@ -1,46 +1,47 @@
---- reportlab/lib/setup.py.orig	Sun May 26 18:30:32 2002
-+++ reportlab/lib/setup.py	Fri May 31 07:38:09 2002
+--- reportlab/lib/setup.py.orig	Thu Jul 25 04:56:37 2002
++++ reportlab/lib/setup.py	Sat Aug 17 18:51:20 2002
 @@ -7,7 +7,7 @@
- 	import os, sys
- 	from distutils.core import setup, Extension
+     import os, sys
+     from distutils.core import setup, Extension
  
--	if sys.platform in ['win32', 'sunos5', 'freebsd4', 'aix4', 'mac', 'darwin']:
-+	if sys.platform in ['win32', 'sunos5', 'freebsd4', 'freebsd5', 'aix4', 'mac', 'darwin']:
- 		LIBS=[]
- 	else:
- 		raise ValueError, "Don't know about platform:"+sys.platform
-@@ -18,23 +18,27 @@
- 			author = "Robin Becker",
- 			author_email = "robin@reportlab.com",
- 			url = "http://www.reportlab.com",
--			packages = [],
--			ext_modules =	[Extension(	'_rl_accel',
--										['_rl_accel.c'],
-+			packages = ['reportlab',
-+				'reportlab.lib', 'reportlab.extensions', 'reportlab.graphics',
-+				'reportlab.pdfbase', 'reportlab.pdfgen', 'reportlab.platypus',
-+				'reportlab.tools', 'reportlab.tools.docco',
-+				'reportlab.tools.py2pdf', 'reportlab.tools.pythonpoint'],
-+			ext_modules =	[Extension(	'reportlab.lib._rl_accel',
-+										['reportlab/lib/_rl_accel.c'],
- 										include_dirs=[],
- 										define_macros=[],
- 										library_dirs=[],
- 										libraries=LIBS,	# libraries to link against
- 										),
--							Extension(	'sgmlop',
--										['sgmlop.c'],
-+							Extension(	'reportlab.lib.sgmlop',
-+										['reportlab/lib/sgmlop.c'],
- 										include_dirs=[],
- 										define_macros=[],
- 										library_dirs=[],
- 										libraries=LIBS,	# libraries to link against
- 										),
--							Extension(	'pyHnj',
--										['pyHnjmodule.c','hyphen.c', 'hnjalloc.c'],
-+							Extension(	'reportlab.lib.pyHnj',
-+										['reportlab/lib/pyHnjmodule.c','reportlab/lib/hyphen.c', 'reportlab/lib/hnjalloc.c'],
- 										include_dirs=[],
- 										define_macros=[],
- 										library_dirs=[],
+-    if sys.platform in ['win32', 'sunos5', 'freebsd4', 'aix4', 'mac', 'darwin']:
++    if sys.platform in ['win32', 'sunos5', 'freebsd4', 'freebsd5', 'aix4', 'mac', 'darwin']:
+         LIBS=[]
+     else:
+         raise ValueError, "Don't know about platform:"+sys.platform
+@@ -18,23 +18,28 @@
+             author = "Robin Becker",
+             author_email = "robin@reportlab.com",
+             url = "http://www.reportlab.com",
+-            packages = [],
+-            ext_modules =   [Extension( '_rl_accel',
+-                                        ['_rl_accel.c'],
++            packages = ['reportlab',
++                'reportlab.lib', 'reportlab.extensions', 'reportlab.graphics',
++                'reportlab.graphics.charts', 'reportlab.graphics.widgets',
++                'reportlab.pdfbase', 'reportlab.pdfgen', 'reportlab.platypus',
++                'reportlab.tools', 'reportlab.tools.docco',
++                'reportlab.tools.py2pdf', 'reportlab.tools.pythonpoint'],
++            ext_modules =  [Extension(  'reportlab.lib._rl_accel',
++                                        ['reportlab/lib/_rl_accel.c'],
+                                         include_dirs=[],
+                                         define_macros=[],
+                                         library_dirs=[],
+                                         libraries=LIBS, # libraries to link against
+                                         ),
+-                            Extension(  'sgmlop',
+-                                        ['sgmlop.c'],
++                            Extension(  'reportlab.lib.sgmlop',
++                                        ['reportlab/lib/sgmlop.c'],
+                                         include_dirs=[],
+                                         define_macros=[],
+                                         library_dirs=[],
+                                         libraries=LIBS, # libraries to link against
+                                         ),
+-                            Extension(  'pyHnj',
+-                                        ['pyHnjmodule.c','hyphen.c', 'hnjalloc.c'],
++                            Extension(  'reportlab.lib.pyHnj',
++                                        ['reportlab/lib/pyHnjmodule.c','reportlab/lib/hyphen.c', 'reportlab/lib/hnjalloc.c'],
+                                         include_dirs=[],
+                                         define_macros=[],
+                                         library_dirs=[],
--- print/py-reportlab/files/patch-rl_config.py
+++ print/py-reportlab/files/patch-rl_config.py
@@ -1,38 +1,42 @@
---- reportlab/rl_config.py.orig	Mon Jul 29 15:53:02 2002
-+++ reportlab/rl_config.py	Mon Jul 29 15:56:21 2002
-@@ -18,28 +18,20 @@
- emptyTableAction=			'error'					# one of 'error', 'indicate', 'ignore'
+--- reportlab/rl_config.py.orig	Thu Jul 25 04:56:34 2002
++++ reportlab/rl_config.py	Sat Aug 17 18:56:07 2002
+@@ -18,29 +18,21 @@
+ emptyTableAction=           'error'                 # one of 'error', 'indicate', 'ignore'
  
  # places to look for T1Font information
--T1SearchPath =	('c:/Program Files/Adobe/Acrobat 5.0/Resource/Font', #Win32, Acrobat 5
--				 'c:/Program Files/Adobe/Acrobat 4.0/Resource/Font', #Win32, Acrobat 4
--				 '%(disk)s/Applications/Python %(sys_version)s/reportlab/fonts', #Mac?
--				 '/usr/lib/Acrobat5/Resource/Font', #Linux, Acrobat 5?
--				 '/usr/lib/Acrobat4/Resource/Font', #Linux, Acrobat 4
--				 '%(REPORTLAB_DIR)s/fonts' #special
-+T1SearchPath =	('/usr/local/lib/Acrobat5/Resource/Font',
-+				'/usr/local/lib/Acrobat4/Resource/Font',
-+				'%(REPORTLAB_DIR)s/fonts' #special
- 				 )
+-T1SearchPath =  ('c:/Program Files/Adobe/Acrobat 5.0/Resource/Font', #Win32, Acrobat 5
+-                 'c:/Program Files/Adobe/Acrobat 4.0/Resource/Font', #Win32, Acrobat 4
+-                 '%(disk)s/Applications/Python %(sys_version)s/reportlab/fonts', #Mac?
+-                 '/usr/lib/Acrobat5/Resource/Font', #Linux, Acrobat 5?
+-                 '/usr/lib/Acrobat4/Resource/Font', #Linux, Acrobat 4
+-                 '%(REPORTLAB_DIR)s/fonts' #special
+-                 )
++T1SearchPath =  ('/usr/local/lib/Acrobat5/Resource/Font',
++		 '/usr/local/lib/Acrobat4/Resource/Font',
++		 '%(REPORTLAB_DIR)s/fonts' #special
++		)
  
  # places to look for TT Font information
 -TTFSearchPath = (
--				'c:/winnt/fonts',
--				'c:/windows/fonts',
--				'/usr/lib/X11/fonts/TrueType/',
--			 	'%(REPORTLAB_DIR)s/fonts' #special
+-                'c:/winnt/fonts',
+-                'c:/windows/fonts',
+-                '/usr/lib/X11/fonts/TrueType/',
+-                '%(REPORTLAB_DIR)s/fonts' #special
+-                )
 +TTFSearchPath = ('/usr/X11R6/lib/X11/fonts/TrueType/',
-+				'%(REPORTLAB_DIR)s/fonts' #special
- 				)
++		 '%(REPORTLAB_DIR)s/fonts' #special
++		)
  
  # places to look for CMap files - should ideally merge with above
  CMapSearchPath = ('/usr/local/Acrobat5/Resource/CMap',
--				  '/usr/local/Acrobat4/Resource/CMap',
--				  'C:\\Program Files\\Adobe\\Acrobat\\Resource\\CMap',
--				  'C:\\Program Files\\Adobe\\Acrobat 5.0\\Resource\\CMap',
--				  'C:\\Program Files\\Adobe\\Acrobat 4.0\\Resource\\CMap'
-+				'/usr/local/Acrobat4/Resource/CMap',
-+				'/usr/local/share/ghostscript/Resource/CMap'
- 				  )
+-                  '/usr/local/Acrobat4/Resource/CMap',
+-                  'C:\\Program Files\\Adobe\\Acrobat\\Resource\\CMap',
+-                  'C:\\Program Files\\Adobe\\Acrobat 5.0\\Resource\\CMap',
+-                  'C:\\Program Files\\Adobe\\Acrobat 4.0\\Resource\\CMap'
+-                  )
++		  '/usr/local/Acrobat4/Resource/CMap',
++		  '/usr/local/share/ghostscript/Resource/CMap'
++		 )
  
  #### Normally don't need to edit below here ####
+ import os, sys, string
