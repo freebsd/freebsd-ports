a17318fac1d39cb67fccb5e2c4c8159786602a4c
sobomax <sobomax@FreeBSD.org>
2002-08-18 07:40:20 +0000

    Update to 1.6.7.

--- finance/gnucash/files/patch-lib::srfi::srfi-8.scm
+++ finance/gnucash/files/patch-lib::srfi::srfi-8.scm
@@ -1,14 +1,14 @@
 
 $FreeBSD$
 
---- lib/srfi/srfi-8.scm	2002/06/21 12:07:11	1.1
-+++ lib/srfi/srfi-8.scm	2002/06/21 12:17:48
+--- lib/srfi/srfi-8.scm.orig	Tue Jun 25 23:53:35 2002
++++ lib/srfi/srfi-8.scm	Sat Aug 17 22:34:33 2002
 @@ -22,7 +22,8 @@
  (cond
   ((or (string=? "1.3" (version))
        (string=? "1.3.4" (version))
--      (string=? "1.4" (version)))
-+      (string=? "1.4" (version))
+-      (string=? "1.4" (substring (version) 0 3)))
++      (string=? "1.4" (substring (version) 0 3))
 +      (string=? "1.4.1" (version)))
    
    (use-modules (ice-9 slib))
