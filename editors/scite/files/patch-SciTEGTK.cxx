49b191e6ec06df4967598184f3404f574493f548
Maxim Sobolev <sobomax@FreeBSD.org>
2002-08-19 16:07:53 +0000

    Use USE_GNOMENG.
    Suggested by:	Roberto Gerola <roberto.gerola@speedtech.it>

---
+++ editors/scite/files/patch-SciTEGTK.cxx
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- SciTEGTK.cxx	2002/08/19 16:01:39	1.1
++++ SciTEGTK.cxx	2002/08/19 16:02:02
+@@ -1882,7 +1882,7 @@
+ 	return 0;
+ }
+ 
+-void SciTEGTK::AddToPopUp(const char *label, int cmd=0, bool enabled=true) {
++void SciTEGTK::AddToPopUp(const char *label, int cmd, bool enabled) {
+ 	SString localised = LocaliseString(label);
+ 	localised.insert(0, "/");
+ 	GtkItemFactoryEntry itemEntry = {
