24c80e44441aaca87c84f5382ac7c710494192a7
ijliao <ijliao@FreeBSD.org>
2002-08-20 06:59:52 +0000

    upgrade to 4.8.2

--- net-im/centericq/files/patch-conscommon.cc
+++ net-im/centericq/files/patch-conscommon.cc
@@ -1,20 +1,11 @@
---- kkconsui-0.1/src/conscommon.cc.orig	Tue Jul 16 19:18:01 2002
-+++ kkconsui-0.1/src/conscommon.cc	Tue Jul 16 19:20:11 2002
-@@ -28,6 +28,8 @@
- #include <sys/filio.h>
- #endif
- 
-+#include <iostream>
-+
- bool kintf_graph = true, kintf_refresh = true;
- 
- void (*kt_resize_event)(void);
-@@ -255,7 +257,7 @@
+--- kkconsui-0.1/src/conscommon.cc.orig	Tue Aug 20 14:50:35 2002
++++ kkconsui-0.1/src/conscommon.cc	Tue Aug 20 14:51:10 2002
+@@ -256,7 +256,7 @@
  #include <fribidi/fribidi.h>
  }
  
 -string makebidi(const string &buf, int lpad = 0) {
 +string makebidi(const string &buf, int lpad) {
+     if(!use_fribidi) return buf;
+ 
      FriBidiChar *us, *out_us;
-     char *outstring, *cbuf;
-     int size;
