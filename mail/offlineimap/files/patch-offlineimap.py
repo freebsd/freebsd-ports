b3d53b396e3faae433490f73e6dcd45acc20f5ed
nbm <nbm@FreeBSD.org>
2002-08-20 16:19:00 +0000

    Add offlineimap 3.2.2, a powerful IMAP/Maildir synchronization tool.

---
+++ mail/offlineimap/files/patch-offlineimap.py
@@ -0,0 +1,8 @@
+--- offlineimap.py.orig	Thu Aug  8 10:31:46 2002
++++ offlineimap.py	Thu Aug  8 10:32:33 2002
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python2.2
++#!/usr/bin/env python2.2
+ 
+ # Copyright (C) 2002 John Goerzen
+ # <jgoerzen@complete.org>
