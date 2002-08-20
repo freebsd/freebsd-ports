1c80bacc6a1e9ecb8724d9ddb14f189be03d2ff2
Christian Weisgerber <naddy@FreeBSD.org>
2002-08-20 19:26:03 +0000

    * Update to version 1.0-pre1, which incorporates all our portability   fixes plus miscellaneous small improvements. * Enable plugins again by installing .la files (oops).

--- audio/gnump3d/files/patch-config::ltmain.sh
+++ audio/gnump3d/files/patch-config::ltmain.sh
@@ -20,16 +20,3 @@ $FreeBSD$
  	    # Do not include libc_r directly, use -pthread flag.
  	    continue
  	    ;;
-@@ -4273,10 +4274,12 @@
- 	fi
- 
- 	# Install the pseudo-library for information purposes.
-+	if false; then
- 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
- 	instname="$dir/$name"i
- 	$show "$install_prog $instname $destdir/$name"
- 	$run eval "$install_prog $instname $destdir/$name" || exit $?
-+	fi
- 
- 	# Maybe install the static library, too.
- 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
