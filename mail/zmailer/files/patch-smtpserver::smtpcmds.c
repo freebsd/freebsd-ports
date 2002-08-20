825b91d744b4aabc451ddb8f3ed8e2f8c6199913
nectar <nectar@FreeBSD.org>
2002-08-20 00:22:49 +0000

    Fix potential remote buffer overflow.
    Reported by:	3APA3A <3APA3A@SECURITY.NNOV.RU>

---
+++ mail/zmailer/files/patch-smtpserver::smtpcmds.c
@@ -0,0 +1,11 @@
+--- smtpserver/smtpcmds.c	Mon Aug 19 19:19:54 2002
++++ smtpserver/smtpcmds.c.orig	Mon Aug 19 19:16:28 2002
+@@ -223,7 +223,7 @@
+     if (STYLE(SS->cfinfo, 'h')) {
+       char argbuf[MAXHOSTNAMELEN+30];
+       char *s;
+-      snprintf(argbuf, sizeof(argbuf), "%s %s", SS->rhostname,
++      sprintf(argbuf,"%s %s", SS->rhostname,
+ 	      ((SS->ihostaddr && (SS->ihostaddr[0] != '\0'))
+ 	       ? SS->ihostaddr : "[0.0.0.0]"));
+       if ((s = router(SS, RKEY_HELLO, 1, argbuf, strlen(argbuf))) == NULL)
