8f6c8ffd686e1a3166765c9b8c36ecfbc3696279
lioux <lioux@FreeBSD.org>
2002-08-21 00:01:59 +0000

    New port L-Breeder version 1.0: Allows you to display and breed L-system forms
    PR:		38725
    Submitted by:	David Yeske <dyeske@yahoo.com>

---
+++ biology/L-Breeder/files/patch-LBreeder::MyTextFrame.cpp
@@ -0,0 +1,7 @@
+--- LBreeder/MyTextFrame.cpp.orig	Mon Jan 21 02:05:11 2002
++++ LBreeder/MyTextFrame.cpp	Mon Jan 21 02:05:26 2002
+@@ -1,3 +1,4 @@
++#include "wx/button.h"
+ #include "wx/filedlg.h"
+ #include "MyTextFrame.h"
+ #include "Ltypes.h" //for debugging messages
