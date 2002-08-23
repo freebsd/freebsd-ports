19b23597bba62813eb1aede48400be2e134d0c42
petef <petef@FreeBSD.org>
2002-08-23 21:24:50 +0000

    Update to 0.6.2.
    Submitted by:	"Brian K. White" <brian@aljex.com>

--- games/eboard/files/patch-network.cc
+++ games/eboard/files/patch-network.cc
@@ -1,10 +1,10 @@
---- network.cc.orig	Mon Mar 25 10:19:21 2002
-+++ network.cc	Mon Mar 25 10:19:52 2002
+--- network.cc.orig	Wed Aug 14 00:20:32 2002
++++ network.cc	Wed Aug 14 00:23:24 2002
 @@ -26,6 +26,7 @@
  
  
  #include <iostream.h>
 +#include <arpa/inet.h>
+ #include <time.h>
  #include <sys/time.h>
  #include <sys/types.h>
- #include <sys/wait.h>
