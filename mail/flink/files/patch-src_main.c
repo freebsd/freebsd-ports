5a1aa653d77b6b5ca51250156d31f88861cf4097
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-08-28 06:58:12 +0000

    Add flink, a mail checker applet for GNOME 1.
    PR:		42104
    Submitted by:	Juan Salaverria <rael@vectorstar.net>

---
+++ mail/flink/files/patch-src_main.c
@@ -0,0 +1,10 @@
+--- src/main.c.ori	Tue Aug 27 00:40:53 2002
++++ src/main.c	Tue Aug 27 00:41:04 2002
+@@ -26,6 +26,7 @@
+ #include <stdio.h>
+ #include <netdb.h>
+ #include <unistd.h>
++#include <sys/time.h>
+ #include <sys/resource.h>
+ #include <sys/types.h>
+ #include <pthread.h>
---
+++ mail/flink/files/patch-src_poplib.c
@@ -0,0 +1,12 @@
+--- src/poplib.c.ori	Tue Aug 27 00:46:06 2002
++++ src/poplib.c	Tue Aug 27 00:46:17 2002
+@@ -20,8 +20,8 @@
+  *
+  ***************************************************************************/
+ 
+-#include <sys/socket.h>
+ #include <sys/types.h>
++#include <sys/socket.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <netdb.h>
