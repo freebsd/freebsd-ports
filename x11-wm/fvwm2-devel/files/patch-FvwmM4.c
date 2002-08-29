bff307e788567436e8bbfdde4e723a9b8d724432
cy <cy@FreeBSD.org>
2002-08-29 19:55:40 +0000

    Update 2.5.2 --> 2.5.3

--- x11-wm/fvwm2-devel/files/patch-FvwmM4.c
+++ x11-wm/fvwm2-devel/files/patch-FvwmM4.c
@@ -1,11 +1,11 @@
---- modules/FvwmM4/FvwmM4.c.orig	Tue Jun 18 04:53:39 2002
-+++ modules/FvwmM4/FvwmM4.c	Mon Jul  1 00:02:14 2002
-@@ -71,7 +71,7 @@
- int  m4_prefix_defines;		/* Add "m4_" to the names of the defines */
- char m4_options[BUFSIZ];	/* Command line options to m4 */
- char m4_outfile[BUFSIZ] = "";	/* The output filename for m4 */
--char *m4_prog = "m4";		/* Name of the m4 program */
-+char *m4_prog = "gm4";		/* Name of the m4 program */
- int  m4_default_quotes;		/* Use default m4 quotes */
- char *m4_startquote = "`";	   /* Left quote characters for m4 */
- char *m4_endquote = "'";	   /* Right quote characters for m4 */
+--- modules/FvwmM4/FvwmM4.c.orig	Mon Aug 12 14:00:13 2002
++++ modules/FvwmM4/FvwmM4.c	Thu Aug 29 12:09:55 2002
+@@ -70,7 +70,7 @@
+ int  m4_prefix_defines;         /* Add "m4_" to the names of the defines */
+ char m4_options[BUFSIZ];        /* Command line options to m4 */
+ char m4_outfile[BUFSIZ] = "";   /* The output filename for m4 */
+-char *m4_prog = "m4";           /* Name of the m4 program */
++char *m4_prog = "gm4";           /* Name of the m4 program */
+ int  m4_default_quotes;         /* Use default m4 quotes */
+ char *m4_startquote = "`";         /* Left quote characters for m4 */
+ char *m4_endquote = "'";           /* Right quote characters for m4 */
