c875bd2ca938b6613a525bfddf61543fcb48c552
Jun Kuriyama <kuriyama@FreeBSD.org>
2002-08-29 02:58:16 +0000

    Unbreak on -current.

--- net/net-snmp4/files/patch-hr_storage.c
+++ net/net-snmp4/files/patch-hr_storage.c
@@ -1,6 +1,24 @@
---- agent/mibgroup/host/hr_storage.c.orig	Fri Feb  8 19:42:25 2002
-+++ agent/mibgroup/host/hr_storage.c	Sat Feb 16 23:37:31 2002
-@@ -583,7 +583,15 @@
+--- agent/mibgroup/host/hr_storage.c.orig	Mon Mar 18 22:39:56 2002
++++ agent/mibgroup/host/hr_storage.c	Thu Aug 29 11:54:11 2002
+@@ -145,7 +145,7 @@
+ #define HRFS_mount	mnt_mountp
+ #define HRFS_statfs	statvfs
+ 
+-#elif defined(HAVE_STATVFS)
++#elif defined(HAVE_STATVFS) && defined(HAVE_MNTENT)
+ 
+ extern struct mntent *HRFS_entry;
+ extern int fscount;
+@@ -520,7 +520,7 @@
+ 	    }
+ 	case HRSTORE_UNITS:
+ 	    if ( store_idx < HRS_TYPE_FS_MAX )
+-#if STRUCT_STATVFS_HAS_F_FRSIZE
++#if defined(STRUCT_STATVFS_HAS_F_FRSIZE) && defined(HAVE_MNTENT)
+ 		long_return = stat_buf.f_frsize;
+ #else
+ 		long_return = stat_buf.f_bsize;
+@@ -601,7 +601,15 @@
  			for (i = 0; i < sizeof(mbstat.m_mtypes)/sizeof(mbstat.m_mtypes[0]); i++)
  			    long_return += mbstat.m_mtypes[i];
  #elif defined(MBSTAT_SYMBOL)
@@ -16,7 +34,7 @@
  #elif defined(NO_DUMMY_VALUES)
  			return NULL;
  #else
-@@ -634,7 +642,15 @@
+@@ -660,7 +668,15 @@
  				+ (mclpool.pr_nget - mclpool.pr_nput)
  				    * mclpool.pr_size;
  #elif defined(MBSTAT_SYMBOL)
