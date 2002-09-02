f87756f890fe068e5fff2b23f352ec3abc8cb146
ume <ume@FreeBSD.org>
2002-09-02 18:22:56 +0000

    Update GKrellM to 2.0.0 after repocopy.

---
+++ sysutils/gkrellm2/files/gkrellmd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Start or stop gkrellmd
+# $FreeBSD$
+
+PREFIX=%%PREFIX%%
+
+case "$1" in
+start)
+	if [ -x ${PREFIX}/sbin/gkrellmd -a \
+	     -f ${PREFIX}/etc/gkrellmd.conf ]; then
+		${PREFIX}/sbin/gkrellmd &
+		echo -n ' gkrellmd'
+	fi
+	;;
+stop)
+	killall gkrellmd && echo -n ' gkrellmd'
+	;;
+*)
+	echo "Usage: `basename $0` {start|stop}" >&2
+	;;
+esac
+
+exit 0
