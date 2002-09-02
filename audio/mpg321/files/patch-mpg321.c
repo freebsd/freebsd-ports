213b2478c02ba1609742a8498ac94be47d799aac
Christian Weisgerber <naddy@FreeBSD.org>
2002-09-02 23:46:52 +0000

    Fix format string bugs.
    Submitted by:	Moritz Jodeit <moritz@jodeit.org>

--- audio/mpg321/files/patch-mpg321.c
+++ audio/mpg321/files/patch-mpg321.c
@@ -2,7 +2,25 @@
 $FreeBSD$
 
 --- mpg321.c.orig	Sun Mar 24 06:49:20 2002
-+++ mpg321.c	Tue Aug 27 22:01:26 2002
++++ mpg321.c	Tue Sep  3 01:29:40 2002
+@@ -188,7 +188,7 @@
+             
+             else
+             {
+-                printf(names[i]);
++                printf("%s", names[i]);
+                 free(names[i]);
+             }
+         }
+@@ -203,7 +203,7 @@
+             if (!names[i])  {
+                 fprintf (stderr, emptystring);
+             }   else    {
+-                fprintf (stderr, names[i]);
++                fprintf (stderr, "%s", names[i]);
+                 free (names[i]);
+             }
+             if (i%2) fprintf (stderr, "\n");
 @@ -509,9 +509,6 @@
  
          mad_decoder_finish(&decoder);
