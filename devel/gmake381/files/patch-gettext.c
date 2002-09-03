c20a8be8432fcc1f5fa5c274987fbb13e2853390
Maxim Sobolev <sobomax@FreeBSD.org>
2002-09-03 07:20:50 +0000

    Fix a problem with gmake segfaulting with some locales.
    PR:		41075
    Submitted by:	naddy

--- devel/gmake381/files/patch-gettext.c
+++ devel/gmake381/files/patch-gettext.c
@@ -1,8 +1,8 @@
 
 $FreeBSD$
 
---- gettext.c	2002/06/06 22:10:23	1.1
-+++ gettext.c	2002/06/06 22:11:09
+--- gettext.c.orig	Wed Feb  9 08:02:18 2000
++++ gettext.c	Sun Jul 28 04:22:31 2002
 @@ -22,6 +22,10 @@
  # include <config.h>
  #endif
@@ -14,3 +14,19 @@ $FreeBSD$
  #ifdef __GNUC__
  # define alloca __builtin_alloca
  # define HAVE_ALLOCA 1
+@@ -1294,6 +1298,15 @@
+ 		    {
+ 		      FREE_BLOCKS (block_list);
+ 		      return added;
++		    }
++		  if (string_space != new_pool)
++		    {
++		      size_t i;
++		      for (i = 0; i < nmap; i++)
++			{
++			  map[i].alias += new_pool - string_space;
++			  map[i].value += new_pool - string_space;
++			}
+ 		    }
+ 		  string_space = new_pool;
+ 		  string_space_max = new_size;
