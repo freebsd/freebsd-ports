e82b7146c045844f0d6469e7255aa89924d6eda2
ijliao <ijliao@FreeBSD.org>
2002-09-05 02:56:17 +0000

    upgrade to 1.0.6 drop maintainership

--- devel/uds/files/patch-uds::streamsocket.cc
+++ devel/uds/files/patch-uds::streamsocket.cc
@@ -1,10 +1,10 @@
---- uds/streamsocket.cc.orig	Thu May 10 19:59:24 2001
-+++ uds/streamsocket.cc	Thu May 10 19:59:52 2001
+--- uds/streamsocket.cc.orig	Thu Sep  5 02:38:14 2002
++++ uds/streamsocket.cc	Thu Sep  5 02:38:24 2002
 @@ -25,6 +25,7 @@
  #include "config.h"
  #ifdef HAVE_SOCKET
  
 +#include <sys/types.h>
+ #include <sys/socket.h>
  #include <netinet/in.h>
  #include <sys/un.h>
- #include <uds/stdexcpt.hh>
