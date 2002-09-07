717d3b5242140fff6dd2aface536d5d664a0ef4e
Jacques Vidrine <nectar@FreeBSD.org>
2002-09-07 15:35:32 +0000

    Fix missing <sys/types.h> in hestest.c (noticed in -CURRENT).

---
+++ dns/hesiod/files/patch-hestest.c
@@ -0,0 +1,12 @@
+*** hestest.c.orig	Sat Sep  7 10:33:20 2002
+--- hestest.c	Sat Sep  7 10:33:26 2002
+***************
+*** 24,29 ****
+--- 24,30 ----
+  #include <netdb.h>
+  #include <errno.h>
+  #include <pwd.h>
++ #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netinet/in.h>
+  #include <stdarg.h>
