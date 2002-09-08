3e9c07aa9ff347231ed1787df99b1ce52cb7d832
Kris Kennaway <kris@FreeBSD.org>
2002-09-08 01:39:05 +0000

    Fix build on -current (machine/soundcard.h -> sys/soundcard.h)

--- graphics/synaesthesia/files/patch-sound.cc
+++ graphics/synaesthesia/files/patch-sound.cc
@@ -1,5 +1,14 @@
---- sound.cc.orig	Fri May 25 23:13:55 2001
-+++ sound.cc	Fri May 25 23:13:59 2001
+--- sound.cc.orig	Wed Jun  7 22:41:06 2000
++++ sound.cc	Sat Sep  7 17:45:37 2002
+@@ -39,7 +39,7 @@
+ #include <linux/cdrom.h>
+ //#include <linux/ucdrom.h>
+ #else
+-#include <machine/soundcard.h>
++#include <sys/soundcard.h>
+ #include <sys/cdio.h>
+ #define CDROM_LEADOUT 0xAA
+ #define CD_FRAMES 75 /* frames per second */
 @@ -423,7 +423,7 @@
      }
    }
@@ -8,3 +17,4 @@
 +  data = new u_short[NumSamples*2];  
    memset((char*)data,0,NumSamples*4);
  }
+ 
