896ccafe996b72d4cd1f9fdbf986b9a5fc00a972
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2002-09-09 03:02:16 +0000

    bayespam version 0.9.2: qmail spam filter written in Perl using Bayesian classification

---
+++ mail/bayespam/files/patch-bayes_spam_check.pl
@@ -0,0 +1,12 @@
+--- bayes_spam_check.pl.orig	Sun Sep  8 17:06:34 2002
++++ bayes_spam_check.pl	Sun Sep  8 17:06:40 2002
+@@ -144,7 +144,7 @@
+  -h, --help          : this (help) message
+  -r, --rating        : corpus rating file to use
+ 
+-example: $0 --rating -o bayes_rating.dat
++example: $0 --rating bayes_rating.dat
+ 
+ EOF
+-}
++}
