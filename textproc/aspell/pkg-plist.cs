a96e0710145409d4441ab0fe5dbf0e2d39ab8fa6
Dirk Meyer <dinoex@FreeBSD.org>
2002-09-10 03:05:27 +0000

    convert language variants into meta ports PR:		42396 Submitted by:	coop9211@uidaho.edu Reviewed by:	thierry@pompo.net

---
+++ textproc/aspell/pkg-plist.cs
@@ -0,0 +1,4 @@
+share/aspell/cs.dat
+share/aspell/cs.multi
+share/aspell/cs.rws
+share/aspell/czech.alias
---
+++ textproc/aspell/pkg-plist.pl
@@ -0,0 +1,4 @@
+share/aspell/pl.dat
+share/aspell/pl.multi
+share/aspell/pl.rws
+share/aspell/polish.alias
