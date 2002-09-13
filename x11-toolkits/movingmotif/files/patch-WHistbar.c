a8585cf66ebf8bb8c4bf61ab5b12e3840e25923d
ijliao <ijliao@FreeBSD.org>
2002-09-13 19:00:01 +0000

    add movingmotif 2.1.0 Display scientific data as dynamic plot

---
+++ x11-toolkits/movingmotif/files/patch-WHistbar.c
@@ -0,0 +1,10 @@
+--- src/WHistbar.c.orig	Sat Sep 14 02:50:22 2002
++++ src/WHistbar.c	Sat Sep 14 02:50:27 2002
+@@ -78,7 +78,6 @@
+ #endif
+ #include <math.h>
+ #include  <limits.h>
+-#include <values.h>
+ #include <X11/Xlib.h>
+ #include<Xm/PrimitiveP.h>
+ #include<Xm/DrawP.h>
