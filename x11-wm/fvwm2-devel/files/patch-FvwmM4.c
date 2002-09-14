a61ad777e520b4264d72fba2ee6d0def17c43850
cy <cy@FreeBSD.org>
2002-09-14 00:54:55 +0000

    ${PERL} -> ${REINPLACE_CMD}
    Fully qualify the path to GNU M4 FvwmM4.

--- x11-wm/fvwm2-devel/files/patch-FvwmM4.c
+++ x11-wm/fvwm2-devel/files/patch-FvwmM4.c
@@ -5,7 +5,7 @@
  char m4_options[BUFSIZ];        /* Command line options to m4 */
  char m4_outfile[BUFSIZ] = "";   /* The output filename for m4 */
 -char *m4_prog = "m4";           /* Name of the m4 program */
-+char *m4_prog = "gm4";           /* Name of the m4 program */
++char *m4_prog = "%%LOCALBASE%%/bin/gm4";           /* Name of the m4 program */
  int  m4_default_quotes;         /* Use default m4 quotes */
  char *m4_startquote = "`";         /* Left quote characters for m4 */
  char *m4_endquote = "'";           /* Right quote characters for m4 */
