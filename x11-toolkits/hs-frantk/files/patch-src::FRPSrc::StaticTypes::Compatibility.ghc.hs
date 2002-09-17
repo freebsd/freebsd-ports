c9c205a023f588617b69adb01a9cc604c9f5c141
Oliver Braun <obraun@FreeBSD.org>
2002-09-17 20:31:31 +0000

    * Make FranTk work with ghc (possible after updating ghc to 5.04.1) * Build for ghc by default * Add knobs WITHOUT_GHC and WITH_HUGS * Bump PORTREVISION

---
+++ x11-toolkits/hs-frantk/files/patch-src::FRPSrc::StaticTypes::Compatibility.ghc.hs
@@ -0,0 +1,10 @@
+--- FRPSrc/StaticTypes/Compatibility.ghc.hs.orig	Fri Aug 23 11:36:12 2002
++++ FRPSrc/StaticTypes/Compatibility.ghc.hs	Fri Aug 23 11:36:36 2002
+@@ -15,6 +15,7 @@
+ import IOExts
+ import Concurrent
+ import Exception
++import Int
+ 
+ double2Float = doubleToFloat
+ 
---
+++ x11-toolkits/hs-frantk/files/patch-src::TclHaskellSrc::ConcTcl.hs
@@ -0,0 +1,10 @@
+--- TclHaskellSrc/ConcTcl.hs.orig	Fri Aug 23 11:42:21 2002
++++ TclHaskellSrc/ConcTcl.hs	Fri Aug 23 11:42:33 2002
+@@ -4,6 +4,7 @@
+ import IOExts
+ import IO
+ import TclCompatibility
++import CVar
+ 
+ -- Some functions used by FranTk to support worker threads.
+ 
