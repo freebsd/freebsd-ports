353e269ea77463fca90e74c3a12da1a1fabd2d13
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-09-17 13:46:31 +0000

    fix for strcasestr problem
    PR:		35244
    Submitted by:	Miguel Mendez <flynn@energyhq.homeip.net>

--- x11-fm/endeavour/files/patch-endeavour::string.cpp
+++ x11-fm/endeavour/files/patch-endeavour::string.cpp
@@ -1,23 +1,24 @@
---- endeavour/string.cpp.orig	Tue Jan 29 17:05:25 2002
-+++ endeavour/string.cpp	Tue Jan 29 17:11:26 2002
-@@ -35,7 +35,7 @@
+--- endeavour/string.cpp.orig	Sat Feb 23 16:24:43 2002
++++ endeavour/string.cpp	Sat Feb 23 16:27:03 2002
+@@ -35,7 +35,9 @@
  #endif	/* __MSW__ */
  
  const char *strseekblank(const char *s);
--const char *strcasestr(const char *haystack, const char *needle);
-+// const char *strcasestr(const char *haystack, const char *needle);
++#ifdef NEED_STRCASESTR
+ const char *strcasestr(const char *haystack, const char *needle);
++#endif
  int strpfx(const char *str, const char *pfx);
  int strcasepfx(const char *str, const char *pfx);
  void strtoupper(char *s);
-@@ -219,6 +219,7 @@
+@@ -219,6 +221,7 @@
   *	Case insensitive version of strstr(). Returns the pointer to
   *	needle in haystack if found or NULL on no match.
   */
-+#ifndef __FreeBSD__
++#ifdef NEED_STRCASESTR
  const char *strcasestr(const char *haystack, const char *needle)
  {
  	const char *strptr1, *strptr2, *strptr3;
-@@ -276,7 +277,7 @@
+@@ -276,7 +279,7 @@
  
          return(NULL);
  }
--- x11-fm/endeavour/files/patch-include::string.h
+++ x11-fm/endeavour/files/patch-include::string.h
@@ -1,10 +1,10 @@
---- include/string.h.orig	Tue Jan 29 16:54:50 2002
-+++ include/string.h	Tue Jan 29 16:55:21 2002
+--- include/string.h.orig	Sat Feb 23 16:37:45 2002
++++ include/string.h	Sat Feb 23 16:37:48 2002
 @@ -24,7 +24,9 @@
  extern int strcasecmp(const char *s1, const char *s2);
  #endif
  extern const char *strseekblank(const char *s);
-+#ifndef __FreeBSD__
++#ifdef NEED_STRCASESTR
  extern const char *strcasestr(const char *haystack, const char *needle);
 +#endif
  extern int strpfx(const char *str, const char *pfx);
