a16583b40d0c0952de125dbd1a7738bfbac3dc6f
perky <perky@FreeBSD.org>
2002-09-18 10:54:46 +0000

    Update to 1.0.4

--- misc/xosd/files/patch-xosd.c
+++ misc/xosd/files/patch-xosd.c
@@ -1,20 +1,20 @@
---- src/xosd.c.orig	Fri Aug  9 13:49:59 2002
-+++ src/xosd.c	Fri Aug  9 13:50:30 2002
-@@ -291,7 +291,7 @@
-       if (!XCheckWindowEvent (osd->display, osd->window, ExposureMask, &report))
- 	 {
- 	   MUTEX_RELEASE ();
--	   usleep (500);
-+	   usleep (100000);
- 	   continue;
- 	 }
-       MUTEX_RELEASE ();
-@@ -326,7 +326,7 @@
+--- src/libxosd/xosd.c.orig	Wed Sep 18 19:52:22 2002
++++ src/libxosd/xosd.c	Wed Sep 18 19:53:00 2002
+@@ -198,7 +198,7 @@
+     pthread_mutex_lock (&osd->mutex);
+     if (!XCheckWindowEvent (osd->display, osd->window, ExposureMask, &report)) {
+       pthread_mutex_unlock (&osd->mutex);
+-      usleep (500);
++      usleep (100000);
+       continue;
+     }
+     pthread_mutex_unlock (&osd->mutex);
+@@ -242,7 +242,7 @@
+   if (osdv == NULL) return NULL;
  
-    while (!osd->done)
-       {
--      usleep (1000);
-+      usleep (200000);
-       MUTEX_GET ();
-       if (osd->timeout != -1 && 
- 	  osd->mapped && 
+   while (!osd->done) {
+-    usleep (1000);
++    usleep (200000);
+     pthread_mutex_lock (&osd->mutex);
+     if (osd->timeout != -1 && osd->mapped && osd->timeout_time <= time(NULL)) {
+       pthread_mutex_unlock (&osd->mutex);
