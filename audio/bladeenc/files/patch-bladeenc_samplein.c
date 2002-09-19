4e32857159ef2b4477087ba3b79b5b00b96d740e
Dirk Froemberg <dirk@FreeBSD.org>
2002-09-19 08:33:17 +0000

    Fix redefinition of uint and ushort to make it build on bento again.

---
+++ audio/bladeenc/files/patch-bladeenc_samplein.c
@@ -0,0 +1,10 @@
+--- bladeenc/samplein.c.orig	Tue May  8 14:11:53 2001
++++ bladeenc/samplein.c	Thu Sep 19 10:25:54 2002
+@@ -31,6 +31,7 @@
+ #include	<stdio.h>
+ #include	<stdlib.h>
+ #include	<string.h>
++#include	<sys/types.h>
+ 
+ #include	"system.h"
+ #include	"samplein.h"
