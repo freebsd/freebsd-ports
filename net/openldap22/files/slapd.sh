4766598175dc03bfdeb4267b6b7f37e07681cb40
ijliao <ijliao@FreeBSD.org>
2002-09-19 21:49:55 +0000

    1. typo fix 2. example fix
    PR:		42660 (1), 42662 (2)
    Submitted by:	Attila Nagy <bra@fsn.hu>

--- net/openldap22/files/slapd.sh
+++ net/openldap22/files/slapd.sh
@@ -4,16 +4,16 @@
 
 slapd_program=@@PREFIX@@/libexec/slapd
 
-# Uncommnet one of the following:
+# Uncomment one of the following:
 #
 # IPv4 Only
-#slapd_args='-h "ldap://0.0.0.0";'
+#slapd_args='-h ldap://0.0.0.0;'
 #
 # IPv6 and IPv4
 #slapd_ags='-h "ldap://[::] ldap://0.0.0.0";'
 #
 # IPv6 Only
-#slapd_args='-h "ldap://[::];'
+#slapd_args='-h ldap://[::];'
 #
 #
 slapd_args=
