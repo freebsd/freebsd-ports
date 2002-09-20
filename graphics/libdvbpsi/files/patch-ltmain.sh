701dbe8cbbcb86f99b1e9169741e31d37bf69e14
ijliao <ijliao@FreeBSD.org>
2002-09-20 06:29:23 +0000

    - Support CFLAGS/CXXFLAGS properly - Do not install useless .la files
    PR:		42891
    Submitted by:	Ports Fury

---
+++ graphics/libdvbpsi/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Tue Jan  8 03:30:35 2002
++++ ltmain.sh	Mon Sep 16 21:54:46 2002
+@@ -4247,10 +4247,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
