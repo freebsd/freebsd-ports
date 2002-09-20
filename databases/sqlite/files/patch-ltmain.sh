afe51991001dcb52c1a7c33dfe109812d7eec83e
knu <knu@FreeBSD.org>
2002-09-20 07:43:55 +0000

    Renumber the shlib major 2 instead of 0, which was unwanted regression.
    While I'm here, add libtclsqlite.la to ALL_TARGET so the libtclsqlite
    library is properly built in the build stage instead of the install
    stage.

---
+++ databases/sqlite/files/patch-ltmain.sh
@@ -0,0 +1,11 @@
+--- ltmain.sh.orig	Fri Sep 28 10:34:45 2001
++++ ltmain.sh	Fri Sep 20 15:00:05 2002
+@@ -852,7 +852,7 @@
+     perm_rpath=
+     temp_rpath=
+     thread_safe=no
+-    vinfo=
++    vinfo=2:0:0
+ 
+     # We need to know -static, to get the right output filenames.
+     for arg
