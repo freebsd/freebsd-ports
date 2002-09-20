cd05b71edd1067f1f80f9f5eafa4b4f1f003a03d
Alan Eldridge <alane@FreeBSD.org>
2002-09-20 23:59:45 +0000

    Update to kde3.
    Submitted by:	Dave Johnson.

---
+++ devel/umbrello/files/patch-uml-uml.cpp
@@ -0,0 +1,11 @@
+--- uml/uml.cpp.org	Sat Sep 15 10:10:46 2001
++++ uml/uml.cpp	Tue Sep 10 21:14:56 2002
+@@ -12,6 +12,8 @@
+ #include <kconfig.h>
+ #include <kstdaction.h>
+ #include <kprinter.h>
++#include <kstatusbar.h>
++
+ // application specific includes
+ #include "uml.h"
+ #include "umlview.h"
