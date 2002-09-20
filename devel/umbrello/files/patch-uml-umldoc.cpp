9f5e6618a5cb0b910ba77c1bd1a900b6d5161d13
alane <alane@FreeBSD.org>
2002-09-20 23:59:45 +0000

    Update to kde3.
    Submitted by:	Dave Johnson.

---
+++ devel/umbrello/files/patch-uml-umldoc.cpp
@@ -0,0 +1,10 @@
+--- uml/umldoc.cpp.org	Thu Sep 20 08:13:37 2001
++++ uml/umldoc.cpp	Tue Sep 10 21:14:56 2002
+@@ -1,6 +1,7 @@
+ // include files for Qt
+ #include <qdir.h>
+ #include <qwidget.h>
++#include <qpainter.h>
+ 
+ // include files for KDE
+ #include <klocale.h>
