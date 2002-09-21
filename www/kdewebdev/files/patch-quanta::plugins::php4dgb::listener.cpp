9bece8de934635f432e8bd81bc51af98e5f1cc95
Alan Eldridge <alane@FreeBSD.org>
2002-09-21 00:22:38 +0000

    Updated to kde3.
    Submitted by:	Michael Collette

--- www/kdewebdev/files/patch-quanta::plugins::php4dgb::listener.cpp
+++
@@ -1,8 +0,0 @@
---- quanta/plugins/php4dbg/listener.cpp.orig	Thu Jan 24 17:27:25 2002
-+++ quanta/plugins/php4dbg/listener.cpp	Thu Jan 24 17:27:41 2002
-@@ -1,4 +1,4 @@
--
-+#include <sys/types.h>
- #include "listener.h"
- 
- #include <stdarg.h>
