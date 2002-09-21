cd734abda273910f486146824cf9febedfe87f6a
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-09-21 06:01:33 +0000

    add blop 0.1.0 Bandlimited oscillator plugins for LADSPA-aware audio applications
    PR:		38450
    Submitted by:	Tilman Linneweh <tilman@arved.de>

---
+++ audio/blop/files/patch-wdatutil.c
@@ -0,0 +1,10 @@
+--- wdatutil.c.orig    Wed May 15 18:50:21 2002
++++ wdatutil.c Wed May 15 18:51:20 2002
+@@ -2,6 +2,7 @@
+ #include <math.h>
+ #include "common.h"
+ #include "wdatutil.h"
++#include <stdlib.h>
+
+ #ifdef __cplusplus
+ extern "C" {
