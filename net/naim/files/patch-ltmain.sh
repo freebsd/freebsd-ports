85e692ec61895d9ce867af052d4ce793334490fd
ijliao <ijliao@FreeBSD.org>
2002-09-22 17:58:50 +0000

    1. add missing files into plist 2. properly use PORTDOCS in plist
    PR:		43253 (1)
    Submitted by:	Edwin Groothuis <edwin@mavetju.org>

---
+++ net/naim/files/patch-ltmain.sh
@@ -0,0 +1,25 @@
+--- ltmain.sh.orig	Mon Sep 23 01:54:26 2002
++++ ltmain.sh	Mon Sep 23 01:55:41 2002
+@@ -2444,6 +2444,9 @@
+ 	  *-*-openbsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test $build_libtool_need_lc = "yes"; then
+@@ -4210,10 +4213,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
