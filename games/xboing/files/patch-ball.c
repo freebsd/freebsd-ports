1ac33876789c5ca4d244cce04ec77c1dc3addd4b
Dirk Meyer <dinoex@FreeBSD.org>
2002-09-23 06:08:25 +0000

    Fixing a build error listed on bento PR:		43276 Submitted by:	edwin@mavetju.org

---
+++ games/xboing/files/patch-ball.c
@@ -0,0 +1,11 @@
+--- ball.c.old	Mon Sep 23 12:48:00 2002
++++ ball.c	Mon Sep 23 12:48:09 2002
+@@ -50,7 +50,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <math.h>
+-#include <values.h>
++#include <limits.h>
+ #include <xpm.h>
+ #include <X11/Xos.h>
+ 
