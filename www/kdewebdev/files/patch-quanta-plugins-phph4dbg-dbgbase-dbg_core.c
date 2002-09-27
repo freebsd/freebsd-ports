288f3f33bfcbf51f0ef58f99cfceacda4497a6bb
Alan Eldridge <alane@FreeBSD.org>
2002-09-27 21:09:00 +0000

    1. Fixed broken current build, and a warning on stable. 2. Changed a lib dep to a build dep only. 3. Bumped portrevision because of dependency change (sorry people, I know 	I'm thrashing you....)
    Submitted by:	Mikhail Teterin <mi+mx@aldan.algebra.com>
    Approved by:	Kris (approved fixing quanta, tell me if you want to back
    		out, kris)

---
+++ www/kdewebdev/files/patch-quanta-plugins-phph4dbg-dbgbase-dbg_core.c
@@ -0,0 +1,6 @@
+--- quanta/plugins/php4dbg/dbgbase/dbg_core.c	Sat Apr 21 09:48:32 2001
++++ quanta/plugins/php4dbg/dbgbase/dbg_core.c	Fri Sep 27 14:12:29 2002
+@@ -15,3 +15,2 @@
+ #include <string.h>
+-#include <malloc.h>
+ #include <stdlib.h>
