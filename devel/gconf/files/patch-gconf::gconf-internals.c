e5fbb8095a181925e012e8ab41fc47477e9cf3fc
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-09-27 06:05:21 +0000

    Make gconf find gconfd-2 after its move to ${PREFIX}/libexec.  This fixes some compatibility problems between GNOME 1 and GNOME 2.
    Reported by:	Adam Weinberger <adam@vectors.cx>
    Tested by:	Adam Weinberger <adam@vectors.cx>
    Approved by:	portmgr (sobomax)

--- devel/gconf/files/patch-gconf::gconf-internals.c
+++ devel/gconf/files/patch-gconf::gconf-internals.c
@@ -1,8 +1,5 @@
-
-$FreeBSD$
-
---- gconf/gconf-internals.c	2001/11/07 11:27:13	1.1
-+++ gconf/gconf-internals.c	2001/11/07 11:27:52
+--- gconf/gconf-internals.c.orig	Thu Mar 14 22:39:51 2002
++++ gconf/gconf-internals.c	Fri Sep 27 00:37:23 2002
 @@ -28,6 +28,9 @@
  #include <string.h>
  #include <sys/stat.h>
@@ -13,3 +10,12 @@ $FreeBSD$
  #include <unistd.h>
  #include <stdlib.h>
  #include <stdio.h>
+@@ -3040,6 +3043,8 @@
+ 
+       if (gconf_file_exists (GCONF_BINDIR"/gconfd-2"))
+         argv[0] = g_strconcat (GCONF_BINDIR, "/gconfd-2", NULL);
++      else if (gconf_file_exists (GCONF_PREFIX"/libexec/gconfd-2"))
++	argv[0] = g_strconcat (GCONF_PREFIX, "/libexec/gconfd-2", NULL);
+       else
+         argv[0] = g_strconcat (GCONF_BINDIR, "/" GCONFD, NULL);
+ 
