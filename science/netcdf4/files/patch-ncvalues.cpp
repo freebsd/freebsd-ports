114b7928ef6fc6036f443bd09b3f4b6e029af172
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-10-01 02:32:59 +0000

    make the patch work on both -current and -stable

--- science/netcdf4/files/patch-ncvalues.cpp
+++ science/netcdf4/files/patch-ncvalues.cpp
@@ -1,19 +1,25 @@
---- cxx/ncvalues.cpp.orig	Mon Sep 16 16:28:57 2002
-+++ cxx/ncvalues.cpp	Mon Sep 16 16:29:42 2002
-@@ -310,7 +310,7 @@
+--- cxx/ncvalues.cpp.orig	Wed Dec 23 02:21:49 1998
++++ cxx/ncvalues.cpp	Tue Oct  1 10:27:32 2002
+@@ -310,7 +310,11 @@
        os << the_values[i] << ", ";
      if (the_number > 0)
        os << the_values[the_number-1] ;
--    os.flags(save);
++#if (__GNUC__ == 2)
+     os.flags(save);
++#else
 +    os.flags(std::_Ios_Fmtflags(save));
++#endif
      return os;
  }
  
-@@ -322,6 +322,6 @@
+@@ -322,6 +326,10 @@
        os << the_values[i] << ", ";
      if (the_number > 0)
        os << the_values[the_number-1];
--    os.flags(save);
++#if (__GNUC__ == 2)
+     os.flags(save);
++#else
 +    os.flags(std::_Ios_Fmtflags(save));
++#endif
      return os;
  }
