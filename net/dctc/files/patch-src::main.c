974f09989e971d930e0a670dfba35969df2c219a
lioux <lioux@FreeBSD.org>
2002-10-05 17:20:14 +0000

    o Update to 0.83.5 o Add hack to disable use of GNU auto* tools
    Reviewed by:	Munish Chopra <mchopra@engmail.uwaterloo.ca>

--- net/dctc/files/patch-src::main.c
+++ net/dctc/files/patch-src::main.c
@@ -1,19 +1,6 @@
---- src/main.c.orig	Sun Aug  4 10:58:23 2002
-+++ src/main.c	Sun Aug  4 11:00:57 2002
-@@ -34,7 +34,12 @@
- #include <sys/param.h>
- #include <sys/utsname.h>
- #include <sys/un.h>
-+#if !(defined(BSD) && (BSD >= 199103))
- #include <linux/sem.h>     /* for the value of SEMVMX */
-+#else
-+#include <sys/ipc.h>
-+#include <sys/sem.h>	/* for the value of SEMVMX */
-+#endif
- #include <errno.h>
- #include <getopt.h>
- #include <string.h>
-@@ -1253,6 +1258,7 @@
+--- src/main.c.orig	Sun Sep 29 14:07:48 2002
++++ src/main.c	Sun Sep 29 14:08:52 2002
+@@ -1269,6 +1269,7 @@
  
  	char *virtual_share_path=NULL;
  
