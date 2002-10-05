9854034982686a78f28e1d1967eccd1a214376bb
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2002-10-05 17:36:29 +0000

    Fix build in -CURRENT
    PR:		43546
    Prompted by:	elevati0n <elevati0n@motocross.com>
    Submitted by:	mbr,
    		Lars Eggert <larse@isi.edu>

--- graphics/ogle/files/extra-patch-include:ogle_endian.h
+++ graphics/ogle/files/extra-patch-include:ogle_endian.h
@@ -1,9 +1,18 @@
 --- include/ogle_endian.h.orig	Thu Oct  4 05:42:11 2001
-+++ include/ogle_endian.h	Wed Apr 10 01:16:32 2002
-@@ -39,6 +39,13 @@
++++ include/ogle_endian.h	Sat Sep 21 17:47:15 2002
+@@ -38,7 +38,22 @@
+ #  define FROM_BE_32(x) (bswap32(x))
  #elif defined(HAVE_SYS_ENDIAN_H)
  #  include <sys/endian.h>
++#if defined(__FreeBSD__)
++#if __FreeBSD_version >= 500000
++#  define FROM_BE_32(x) (bswap32(x))
++#else
  #  define FROM_BE_32(x) (swap32(x))
++#endif
++#else
++#  define FROM_BE_32(x) (swap32(x))
++#endif
 +#elif defined(__FreeBSD__)
 +#  define _KERNEL
 +#  define I486_CPU	/* Will crash unless 486+ */
