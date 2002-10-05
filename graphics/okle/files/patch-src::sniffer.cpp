57fdf5f788814ed30f900b96c967ccd56bb9b479
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2002-10-05 17:45:18 +0000

    Fix build in -CURRENT
    Submitted by:	mbr

---
+++ graphics/okle/files/patch-src::sniffer.cpp
@@ -0,0 +1,10 @@
+--- src/sniffer.cpp.orig	Tue Aug  6 21:53:56 2002
++++ src/sniffer.cpp	Sat Sep 21 15:51:04 2002
+@@ -6,6 +6,7 @@
+ 
+ #include "sniffer.h"
+ 
++#include <stdlib.h>
+ #include <kapp.h>
+ #include <kwin.h>
+ #include <X11/Xutil.h>
