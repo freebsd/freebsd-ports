a75a305a702524c7db200500fe57e0f632904f7e
lioux <lioux@FreeBSD.org>
2002-10-05 17:45:18 +0000

    Fix build in -CURRENT
    Submitted by:	mbr

---
+++ multimedia/okle/files/patch-src::okle_win.cpp
@@ -0,0 +1,10 @@
+--- src/okle_win.cpp.orig	Sat Sep 21 17:36:39 2002
++++ src/okle_win.cpp	Sat Sep 21 15:50:48 2002
+@@ -3,6 +3,7 @@
+ #include "sniffer.h"
+ #include "language.h"
+ 
++#include <stdlib.h>
+ #include <klocale.h>
+ #include <kconfig.h>
+ #include <qpushbutton.h>
