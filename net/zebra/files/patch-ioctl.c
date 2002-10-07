c04b69cc50ea27d4954608635499bea711944256
sobomax <sobomax@FreeBSD.org>
2002-10-07 14:43:54 +0000

    Properly zero-out struct ifreq before passing it to the SIOCSIFFLAGS ioctl(2), so that it doesn't occasionally trigger promisc mode on interface.
    Reported/tested by:	Vladimir B. Grebenschikov <vova@sw.ru>

--- net/zebra/files/patch-ioctl.c
+++ net/zebra/files/patch-ioctl.c
@@ -1,6 +1,25 @@
---- zebra/ioctl.c.orig	Wed Dec 12 18:02:16 2001
-+++ zebra/ioctl.c	Wed Dec 12 18:02:30 2001
-@@ -478,6 +478,9 @@
+
+$FreeBSD$
+
+--- zebra/ioctl.c.orig	Tue Oct 23 11:31:29 2001
++++ zebra/ioctl.c	Fri Oct  4 19:45:04 2002
+@@ -349,6 +349,7 @@
+   int ret;
+   struct ifreq ifreq;
+ 
++  bzero(&ifreq, sizeof(struct ifreq));
+   ifreq_set_name (&ifreq, ifp);
+ 
+   ifreq.ifr_flags = ifp->flags;
+@@ -371,6 +372,7 @@
+   int ret;
+   struct ifreq ifreq;
+ 
++  bzero(&ifreq, sizeof(struct ifreq));
+   ifreq_set_name (&ifreq, ifp);
+ 
+   ifreq.ifr_flags = ifp->flags;
+@@ -473,6 +475,9 @@
    mask.sin6_len = sizeof (struct sockaddr_in6);
  #endif
    memcpy (&addreq.ifra_prefixmask, &mask, sizeof (struct sockaddr_in6));
@@ -8,5 +27,5 @@
 +  addreq.ifra_lifetime.ia6t_vltime = 0xffffffff;
 +  addreq.ifra_lifetime.ia6t_pltime = 0xffffffff;
    
-   ret = if_ioctl_ipv6 (SIOCAIFADDR_IN6, (caddr_t) &addreq);
-   if (ret < 0)
+   addreq.ifra_lifetime.ia6t_pltime = ND6_INFINITE_LIFETIME; 
+   addreq.ifra_lifetime.ia6t_vltime = ND6_INFINITE_LIFETIME; 
