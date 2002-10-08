d5ee8c22349b1b9b7927fa909d371548d830b317
nbm <nbm@FreeBSD.org>
2002-10-08 14:04:04 +0000

    Upgrade offlineimap to 3.99.0
    PR:		43824
    Submitted by:	Sergei Kolobov <sergei@kolobov.com>

--- mail/offlineimap/files/patch-offlineimap.py
+++
@@ -1,8 +0,0 @@
---- offlineimap.py.orig	Thu Aug  8 10:31:46 2002
-+++ offlineimap.py	Thu Aug  8 10:32:33 2002
-@@ -1,4 +1,4 @@
--#!/usr/bin/python2.2
-+#!/usr/bin/env python2.2
- 
- # Copyright (C) 2002 John Goerzen
- # <jgoerzen@complete.org>
