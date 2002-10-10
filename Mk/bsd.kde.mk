69a4c09708c54e4569a468304d051a3c19368317
Alan Eldridge <alane@FreeBSD.org>
2002-10-10 23:54:33 +0000

    1. Welcome to KDE 3.0.4. 2. Added a tagged version of MASTER_SITE_KDE, called MASTER_SITE_KDE_kde,    like this:    MASTER_SITE_KDE_kde=    ${MASTER_SITE_KDE:S@%/@%/:kde@g}

--- Mk/bsd.kde.mk
+++ Mk/bsd.kde.mk
@@ -66,6 +66,7 @@ USE_KDELIBS_VER=2
 LIB_DEPENDS+=	kdecore:${PORTSDIR}/x11/kdelibs3
 USE_QT_VER=		3
 PREFIX=			${KDE_PREFIX}
+MASTER_SITE_KDE_kde=	${MASTER_SITE_KDE:S@%/@%/:kde@g}
 
 .else
 
@@ -96,7 +97,7 @@ CONFIGURE_ENV+=        MOC="${MOC}" QTDIR="${QTDIR}"
 
 # Yeah, it's namespace pollution, but this is really the best place for this
 # stuff since arts/kdelibs use it.
-KDE_VERSION=	3.0.3
+KDE_VERSION=	3.0.4
 KDE_ORIGVER=	3.0.1
 KDE_PREFIX?=	${LOCALBASE}
 
