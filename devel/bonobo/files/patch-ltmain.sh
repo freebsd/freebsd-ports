ba507ed45f55cd45e6e45aabd14939e1931b136b
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-10-11 06:32:10 +0000

    Don't explicitly link against libc.  Bump PORTREVISION.

--- devel/bonobo/files/patch-ltmain.sh
+++ devel/bonobo/files/patch-ltmain.sh
@@ -1,45 +1,43 @@
-
-$FreeBSD$
-
---- ltmain.sh.orig	Wed Jan 23 11:38:40 2002
-+++ ltmain.sh	Sun Feb  3 01:44:17 2002
-@@ -1037,8 +1037,16 @@
+--- ltmain.sh.orig	Sun Oct  6 15:24:35 2002
++++ ltmain.sh	Fri Oct 11 02:13:11 2002
+@@ -1249,8 +1249,17 @@
  	continue
  	;;
  
 +      -pthread)
-+	compile_command="$compile_command -pthread"
-+	finalize_command="$finalize_command -pthread"
-+	compiler_flags="$compiler_flags -pthread"
-+	continue
-+	;;
++      compile_command="$compile_command -pthread"
++      finalize_command="$finalize_command -pthread"
++      compiler_flags="$compiler_flags -pthread"
++      continue
++      ;;
++
 +
        -module)
  	module=yes
-+	build_old_libs=no
++        build_old_libs=no
  	continue
  	;;
  
-@@ -2412,6 +2420,9 @@
- 	  *-*-netbsd*)
- 	    # Don't link with libc until the a.out ld.so is fixed.
+@@ -2841,6 +2850,9 @@
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    test "X$arg" = "X-lc" && continue
  	    ;;
-+	  *-*-freebsd*)
-+	    # FreeBSD doesn't need this...
-+	    ;;
- 	  *)
++          *-*-freebsd*)
++            # FreeBSD doesn't need this...
++            ;;
+  	  *)
  	    # Add libc to deplibs on all other systems if necessary.
  	    if test "$build_libtool_need_lc" = "yes"; then
-@@ -4179,10 +4190,12 @@
+@@ -4715,10 +4727,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
-+	if /usr/bin/false; then
++        if /usr/bin/false; then
  	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
  	instname="$dir/$name"i
  	$show "$install_prog $instname $destdir/$name"
  	$run eval "$install_prog $instname $destdir/$name" || exit $?
-+	fi
++        fi
  
  	# Maybe install the static library, too.
  	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
