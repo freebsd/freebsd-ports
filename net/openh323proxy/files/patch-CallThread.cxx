3df4fee44594b671cff19f6cb61f68974a28fe86
Roger Hardiman <roger@FreeBSD.org>
2002-10-11 09:57:26 +0000

    Upgrade to the latest version

---
+++ net/openh323proxy/files/patch-CallThread.cxx
@@ -0,0 +1,12 @@
+*** CallThread.cxx.orig	Fri Oct 11 10:50:05 2002
+--- CallThread.cxx	Fri Oct 11 10:50:19 2002
+***************
+*** 85,90 ****
+--- 85,91 ----
+  #include <ptlib/svcproc.h>
+  #include <q931.h>
+  #include <h245.h>
++ #include <h323.h>
+  #include "Log.h"
+  #include "CallTabl.h"
+  #include "EndpointTabl.h"
