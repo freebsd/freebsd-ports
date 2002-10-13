838ce26f5fda580254c3b64f81e5d676a688d714
Mark Peek <mp@FreeBSD.org>
2002-10-13 21:22:39 +0000

    Patches to get FreeBSD changes working with gdb-5.3.

--- devel/gdb66/files/patch-config_i386_nm-fbsd.h
+++ devel/gdb66/files/patch-config_i386_nm-fbsd.h
@@ -1,35 +1,10 @@
---- gdb/config/i386/nm-fbsd.h	Sat Jul 21 13:16:44 2001
-+++ gdb/config/i386/nm-fbsd.h	Mon Jun  3 10:04:54 2002
-@@ -148,5 +152,32 @@
- #define ld_2		d_sdt
+--- gdb/config/i386/nm-fbsd.h	Fri Jun 28 11:44:49 2002
++++ gdb/config/i386/nm-fbsd.h	Sun Oct 13 10:53:27 2002
+@@ -26,6 +26,7 @@
+ #define I386_USE_GENERIC_WATCHPOINTS
+ #endif
  
- #endif /* !SVR4_SHARED_LIBS */
-+
-+extern int kernel_debugging;
-+extern int kernel_writablecore;
-+
-+#if __FreeBSD_version >= 500032
-+#define ADDITIONAL_OPTIONS \
-+       {"kernel", no_argument, &kernel_debugging, 1}, \
-+       {"k", no_argument, &kernel_debugging, 1}, \
-+       {"wcore", no_argument, &kernel_writablecore, 1}, \
-+       {"w", no_argument, &kernel_writablecore, 1},
-+
-+#define ADDITIONAL_OPTION_HELP \
-+       "\
-+  --kernel           Enable kernel debugging.\n\
-+  --wcore            Make core file writable (only works for /dev/mem).\n\
-+                     This option only works while debugging a kernel !!\n\
-+"
-+
-+#define DEFAULT_PROMPT kernel_debugging?"(kgdb) ":"(gdb) "
-+
-+/* misuse START_PROGRESS to test whether we're running as kgdb */   
-+/* START_PROGRESS is called at the top of main */
-+#undef START_PROGRESS
-+#define START_PROGRESS(STR,N) \
-+  if (!strcmp(STR, "kgdb")) \
-+     kernel_debugging = 1;
-+#endif
++#include "config/nm-fbsd.h"
+ #include "i386/nm-i386.h"
  
- #endif /* NM_FBSD_H */
+ #ifdef HAVE_SYS_PARAM_H
--- devel/gdb66/files/patch-target.c
+++ devel/gdb66/files/patch-target.c
@@ -1,14 +1,11 @@
---- gdb/target.c.orig	Thu Jan 31 17:01:21 2002
-+++ gdb/target.c	Fri Jun  7 16:36:06 2002
-@@ -1361,7 +1361,11 @@
+--- gdb/target.c	Mon Aug 26 12:18:33 2002
++++ gdb/target.c	Sun Oct 13 10:51:32 2002
+@@ -1429,7 +1429,7 @@
    for (t = target_structs; t < target_structs + target_struct_size;
         ++t)
      {
-+#if defined(__FreeBSD__) && defined(__i386__)
+-      if ((*t)->to_stratum == core_stratum)
 +      if ((*t)->to_stratum == (kernel_debugging ? kcore_stratum : core_stratum))
-+#else
-       if ((*t)->to_stratum == core_stratum)
-+#endif
  	{
  	  runable = *t;
  	  ++count;
