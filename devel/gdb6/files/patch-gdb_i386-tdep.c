05a8ca9a5603952776bf00d35b64700edf36c24f
Mark Peek <mp@FreeBSD.org>
2002-10-15 19:29:44 +0000

    Fix patch to remove bogus prefix pathnames.

--- devel/gdb6/files/patch-gdb_i386-tdep.c
+++ devel/gdb6/files/patch-gdb_i386-tdep.c
@@ -1,5 +1,5 @@
---- gdb+dejagnu-5.2.90_20021011/gdb/i386-tdep.c	Mon Aug 26 11:35:25 2002
-+++ newgdb/gdb/i386-tdep.c	Sun Oct 13 14:15:58 2002
+--- gdb/i386-tdep.c	Mon Aug 26 11:35:25 2002
++++ gdb/i386-tdep.c	Sun Oct 13 14:15:58 2002
 @@ -534,6 +534,9 @@
  static CORE_ADDR
  i386_frame_saved_pc (struct frame_info *frame)
