6e09382d65be40d52b63a8fc2542a45c0400210f
trevor <trevor@FreeBSD.org>
2002-10-17 10:14:05 +0000

    Netscape 4.07 and 4.08 have multiple published security holes. Please use Netscape 4.80 instead.

--- www/netscape4-communicator.us/files/netscape.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export MOZILLA_HOME; MOZILLA_HOME=${MOZILLA_HOME:=@PREFIX@/lib/netscape.us}
-export CLASSPATH ; CLASSPATH=.:$MOZILLA_HOME
-export XCMSDB; XCMSDB=/dev/null
-exec $MOZILLA_HOME/communicator-4.07.us.bin $*
