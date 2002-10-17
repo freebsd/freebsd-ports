96b64b48ee8f555b5da7a16d68fbd1148a1c47cb
marcus <marcus@FreeBSD.org>
2002-10-17 19:21:04 +0000

    Add totem, a xine-based video player for the GNOME 2 desktop.
    PR:		44177
    Submitted by:	Hendrik Scholz <hendrik@scholz.net>

---
+++ graphics/totem/files/patch-src::video-utils.c
@@ -0,0 +1,10 @@
+--- src/video-utils.c.orig	Thu Oct 17 14:24:44 2002
++++ src/video-utils.c	Thu Oct 17 14:25:06 2002
+@@ -1,7 +1,6 @@
+ 
+ #include "video-utils.h"
+ 
+-#include <stdint.h>
+ #include <gdk/gdk.h>
+ #include <gdk/gdkx.h>
+ #include <X11/Xlib.h>
