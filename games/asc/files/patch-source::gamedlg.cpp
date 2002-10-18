1febd762e2c553ec8b6e5e871b8f065ad7ce618f
Maxim Sobolev <sobomax@FreeBSD.org>
2002-10-18 16:53:52 +0000

    Fix two patches that I've forgot to update before committing.

--- games/asc/files/patch-source::gamedlg.cpp
+++ games/asc/files/patch-source::gamedlg.cpp
@@ -1,13 +1,13 @@
 
 $FreeBSD$
 
---- source/gamedlg.cpp	2002/10/18 12:57:12	1.2
-+++ source/gamedlg.cpp	2002/10/18 12:57:38
-@@ -3794,7 +3794,7 @@
+--- source/gamedlg.cpp.orig	Fri Oct 18 19:39:24 2002
++++ source/gamedlg.cpp	Fri Oct 18 19:40:48 2002
+@@ -3788,7 +3791,7 @@
                              int step =  ( target->service[displayed[mp]].maxAmount - target->service[displayed[mp]].minAmount ) / 100;
                              if ( step == 0 )
                                 step = 1;
--                            step = int( pow ( 10, int ( log10 ( (double)step ))));
+-                            step = int( pow ( 10, int ( log10 ( step ))));
 +                            step = int( pow ( (double)10, int ( log10 ( (double)step ))));
  
                              oldpos[mp] = newpos[mp];
