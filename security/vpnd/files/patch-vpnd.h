bed92389d0dde39debea14fcdda47e72e58ca6a6
kris <kris@FreeBSD.org>
2002-10-20 21:36:06 +0000

    Fix build on -current (machine/param.h -> sys/param.h)

---
+++ security/vpnd/files/patch-vpnd.h
@@ -0,0 +1,11 @@
+--- vpnd.h.orig	Sun Oct 20 14:36:45 2002
++++ vpnd.h	Sun Oct 20 14:37:04 2002
+@@ -52,7 +52,7 @@
+ #include <net/if.h>
+ #include <net/slip.h>
+ #include <net/if_var.h>
+-#include <machine/param.h>
++#include <sys/param.h>
+ #include <sys/mbuf.h>
+ #include <net/slcompress.h>
+ #include <net/if_slvar.h>
