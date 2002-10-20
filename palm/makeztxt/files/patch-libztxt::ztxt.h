7f7b331ebc8872b76df8f5641f979fc1599d5490
kris <kris@FreeBSD.org>
2002-10-20 23:10:16 +0000

    makeztxt is a simple command line program that takes a plain ASCII text file and compresses it into a zTXT database.  makeztxt will remove newline characters at the end of lines that contain text so that the paragraphs flow better on the Palm screen.
    Submitted by:	Roman Shterenzon <romanbsd@yahoo.com>

---
+++ palm/makeztxt/files/patch-libztxt::ztxt.h
@@ -0,0 +1,10 @@
+--- libztxt/ztxt.h.old	Sat Mar  2 07:03:18 2002
++++ libztxt/ztxt.h	Sun Sep 29 15:46:52 2002
+@@ -31,6 +31,7 @@
+ #define _ZTXT_H_ 1
+ 
+ 
++#include <sys/types.h>
+ #include <regex.h>
+ #include "databaseHdrs.h"
+ #include <weasel_common.h>
---
+++ palm/makeztxt/files/patch-libztxt::ztxt_process.c
@@ -0,0 +1,10 @@
+--- libztxt/ztxt_process.c.old	Sat Mar  2 07:03:18 2002
++++ libztxt/ztxt_process.c	Sun Sep 29 15:52:49 2002
+@@ -30,6 +30,7 @@
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <string.h>
++#include <sys/types.h>
+ #include <regex.h>
+ #include <zlib.h>
+ #include "ztxt.h"
