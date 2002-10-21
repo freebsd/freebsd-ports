2081afbf024af69c0cf21564632b79da9431b9a8
Maxim Sobolev <sobomax@FreeBSD.org>
2002-10-21 11:41:32 +0000

    Unbreak on -current.
    Submitted by:	bento

---
+++ graphics/mpeg_encode/files/patch-opts.c
@@ -0,0 +1,17 @@
+
+$FreeBSD$
+
+--- opts.c	2002/10/21 11:32:56	1.1
++++ opts.c	2002/10/21 11:33:04
+@@ -37,9 +37,10 @@
+ 
+ #include <stdio.h>
+ #include <string.h>
+-#include "opts.h"
+ #include <malloc.h>
+ #include <math.h>
++
++#include "opts.h"
+ 
+ /*==============*
+  * EXTERNALS    *
