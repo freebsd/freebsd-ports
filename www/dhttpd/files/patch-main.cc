824d2a80b8b5cdede52418e1936d03c013bf4124
Kris Kennaway <kris@FreeBSD.org>
2002-10-21 00:41:02 +0000

    Fix build on -current (#include <stdlib.h> for exit() prototype) and respect CC and CFLAGS

--- www/dhttpd/files/patch-main.cc
+++ www/dhttpd/files/patch-main.cc
@@ -1,6 +1,6 @@
---- src/main.cc	Tue Mar  4 09:10:41 1997
-+++ src/main.cc.orig	Mon Jun 24 15:12:32 2002
-@@ -18,7 +18,6 @@
+--- src/main.cc.orig	Mon Mar  3 17:10:41 1997
++++ src/main.cc	Sun Oct 20 17:41:25 2002
+@@ -18,10 +18,10 @@
   */
  
  #include <errno.h>
@@ -8,3 +8,7 @@
  #include <pwd.h>
  #include <signal.h>
  #include <stdio.h>
++#include <stdlib.h>
+ #include <unistd.h>
+ #include <sys/socket.h>
+ #include <sys/types.h>
