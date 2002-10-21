ec93ba9f33c476e559d01cfd1d90a2233e003ea7
kris <kris@FreeBSD.org>
2002-10-21 02:16:19 +0000

    Partially fix build on -current.  This is still broken due to an unzeroed malloc buffer somewhere.

--- security/tea-total/files/patch-arch.h
+++ security/tea-total/files/patch-arch.h
@@ -1,13 +1,18 @@
---- arch.h.orig	Fri Jan 12 02:20:52 2001
-+++ arch.h	Fri Jan 12 02:24:00 2001
-@@ -11,6 +11,9 @@
+--- arch.h.orig	Sun Dec 31 09:16:53 2000
++++ arch.h	Sun Oct 20 19:01:52 2002
+@@ -9,10 +9,14 @@
+  * warranties of merchantability and fitness for a particular purpose.
+  */
  
 +#if defined(ARCH_OPENBSD) || defined(ARCH_FREEBSD)
  #if defined(ARCH_OPENBSD)
 -
  #include <machine/types.h>
  #include <machine/endian.h>
-+#else
-+#include <sys/inttypes.h>
++#endif
++#if defined(ARCH_FREEBSD)
++#include <sys/types.h>
 +#endif
  typedef uint32_t u32;
+ typedef int32_t s32;
+ typedef uint16_t u16;
