7722b5e6d0e55347396ae8bd8d11a515d0100814
mbr <mbr@FreeBSD.org>
2002-10-22 23:16:35 +0000

    Readd removed BSD support. test(1) is in /bin.

--- editors/openoffice-1.1-devel/files/patch-setup2+source+custom+jvmsetup+loader+jvmsetup.sh
+++ editors/openoffice-1.1-devel/files/patch-setup2+source+custom+jvmsetup+loader+jvmsetup.sh
@@ -1,9 +1,9 @@
---- ../setup2/source/custom/jvmsetup/loader/jvmsetup.sh.orig	Sat Apr  6 12:22:21 2002
-+++ ../setup2/source/custom/jvmsetup/loader/jvmsetup.sh	Wed May  1 22:38:21 2002
-@@ -66,6 +66,7 @@
+--- ../setup2/source/custom/jvmsetup/loader/jvmsetup.sh.orig	Fri Jun 14 15:36:11 2002
++++ ../setup2/source/custom/jvmsetup/loader/jvmsetup.sh	Fri Oct 18 23:45:45 2002
+@@ -65,6 +65,7 @@
+ sd_platform=`uname -s`
  case $sd_platform in
  	SCO_SV) test=/bin/test     ;;
- 	NetBSD) test=/bin/test     ;;
 +	FreeBSD) test=/bin/test    ;;
  	*)      test=/usr/bin/test ;;
  esac
