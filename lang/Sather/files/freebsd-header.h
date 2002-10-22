cff40c1083c3488509943a5a615b07027bbbdaf2
ijliao <ijliao@FreeBSD.org>
2002-10-22 06:12:06 +0000

    - Fix build - Add several libraries, utilities and documents
    PR:		44349
    Submitted by:	Ports Fury

---
+++ lang/Sather/files/freebsd-header.h
@@ -0,0 +1,14 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#ifndef ZONES
+#  include <gc.h>
+#endif
+
+#include "../../Common/c_header.h"
+#include "../../Common/runtime.h"
+
+#undef GC_ENABLE_INCREMENTAL
+
+#endif
+
