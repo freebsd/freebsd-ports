3b4950b4f3cff17b537a2cb022db47d882b57f54
mbr <mbr@FreeBSD.org>
2002-10-22 23:11:22 +0000

    FreeBSD check got removed. Add it again.

--- editors/openoffice-1.1/files/patch-psprint+source+printer+printerinfomanager.cxx
+++ editors/openoffice-1.1/files/patch-psprint+source+printer+printerinfomanager.cxx
@@ -1,11 +1,11 @@
---- ../psprint/source/printer/printerinfomanager.cxx.orig	Sat Apr  6 11:56:35 2002
-+++ ../psprint/source/printer/printerinfomanager.cxx	Wed May  1 22:42:58 2002
+--- ../psprint/source/printer/printerinfomanager.cxx.orig	Wed Oct 16 00:02:09 2002
++++ ../psprint/source/printer/printerinfomanager.cxx	Fri Oct 18 20:02:54 2002
 @@ -883,7 +883,7 @@
  
  static const struct SystemCommandParameters aParms[] =
  {
--#if defined(LINUX) || defined(NETBSD)
+-#ifdef LINUX
 +#if defined(LINUX) || defined(NETBSD) || defined(FREEBSD)
-     { "/usr/sbin/lpc status", "lpr -P (PRINTER)", "", ":", 0 },
-     { "lpc status", "lpr -P (PRINTER)", "", ":", 0 },
-     { "LANG=C;LC_ALL=C;export LANG LC_ALL;lpstat -s", "lp -d (PRINTER)", "system for ", ": ", 1 }
+     { "/usr/sbin/lpc status", "lpr -P \"(PRINTER)\"", "", ":", 0 },
+     { "lpc status", "lpr -P \"(PRINTER)\"", "", ":", 0 },
+     { "LANG=C;LC_ALL=C;export LANG LC_ALL;lpstat -s", "lp -d \"(PRINTER)\"", "system for ", ": ", 1 }
