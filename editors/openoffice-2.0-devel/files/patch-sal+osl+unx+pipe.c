a829bc12f68e70fa4ca676e9e530a824d15f3797
mbr <mbr@FreeBSD.org>
2002-10-22 23:14:19 +0000

    Use SUN_LEN makro for unix domain socket name lenght.

--- editors/openoffice-2.0-devel/files/patch-sal+osl+unx+pipe.c
+++ editors/openoffice-2.0-devel/files/patch-sal+osl+unx+pipe.c
@@ -1,11 +1,11 @@
---- ../sal/osl/unx/pipe.c.orig	Mon Feb 18 13:48:55 2002
-+++ ../sal/osl/unx/pipe.c	Wed Apr  3 00:59:15 2002
+--- ../sal/osl/unx/pipe.c.orig	Thu Jul 11 16:01:39 2002
++++ ../sal/osl/unx/pipe.c	Fri Oct 18 19:56:38 2002
 @@ -264,7 +264,11 @@
      
  	addr.sun_family = AF_UNIX;
  	strncpy(addr.sun_path, name, sizeof(addr.sun_path));
 +#if defined(FREEBSD)
-+	 len = SUN_LEN(&addr);
++	len = SUN_LEN(&addr);
 +#else
  	len = sizeof(addr);
 +#endif
