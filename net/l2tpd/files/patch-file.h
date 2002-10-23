a3f10c843be05f56567e034d6c6c361b64d14480
sobomax <sobomax@FreeBSD.org>
2002-10-23 13:46:26 +0000

    Update to 0.69.

--- net/l2tpd/files/patch-file.h
+++ net/l2tpd/files/patch-file.h
@@ -1,16 +1,20 @@
 
 $FreeBSD$
 
---- file.h.orig	Fri Jan 11 17:54:44 2002
-+++ file.h	Tue Oct 22 12:41:18 2002
-@@ -45,8 +45,8 @@
+--- file.h.orig	Wed Apr 10 23:09:33 2002
++++ file.h	Wed Oct 23 16:34:37 2002
+@@ -49,10 +49,10 @@
  #define SENSE_ALLOW -1
  #define SENSE_DENY 0
  
 -#define DEFAULT_AUTH_FILE "/etc/l2tp/l2tp-secrets"
--#define CONFIG_FILE "/etc/l2tp/l2tpd.conf"
+-#define ALT_DEFAULT_AUTH_FILE "/etc/l2tpd/l2tp-secrets"
+-#define DEFAULT_CONFIG_FILE "/etc/l2tp/l2tpd.conf"
+-#define ALT_DEFAULT_CONFIG_FILE "/etc/l2tpd/l2tpd.conf"
 +#define DEFAULT_AUTH_FILE (PREFIX "/etc/l2tpd/l2tp-secrets")
-+#define CONFIG_FILE (PREFIX "/etc/l2tpd/l2tpd.conf")
++#define ALT_DEFAULT_AUTH_FILE (PREFIX "/etc/l2tpd/l2tp-secrets")
++#define DEFAULT_CONFIG_FILE (PREFIX "/etc/l2tpd/l2tpd.conf")
++#define ALT_DEFAULT_CONFIG_FILE (PREFIX "/etc/l2tpd/l2tpd.conf")
+ #define DEFAULT_PID_FILE "/var/run/l2tpd.pid"
  
  /* Definition of an LNS */
- struct lns {
