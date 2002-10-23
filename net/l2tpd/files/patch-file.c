eb535fb4a77eb3e17ae518776beb0f7dcc78bf14
Maxim Sobolev <sobomax@FreeBSD.org>
2002-10-23 13:46:26 +0000

    Update to 0.69.

--- net/l2tpd/files/patch-file.c
+++ net/l2tpd/files/patch-file.c
@@ -1,13 +1,16 @@
 
 $FreeBSD$
 
---- file.c	2002/10/22 09:41:27	1.1
-+++ file.c	2002/10/22 09:43:30
-@@ -17,7 +17,6 @@
+--- file.c.orig	Sat Aug 10 03:55:14 2002
++++ file.c	Wed Oct 23 16:33:17 2002
+@@ -18,10 +18,7 @@
  #include <unistd.h>
  #include <stdlib.h>
  #include <netdb.h>
 -#include <netinet/in.h>
+ #include <time.h>
+-#include <sys/types.h>
+-#include <sys/socket.h>
  
  #include "l2tp.h"
- struct lns *lnslist;
+ 
