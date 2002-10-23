012104d24d2cece53ad4237be41e59855570a21f
nork <nork@FreeBSD.org>
2002-10-23 13:13:24 +0000

    Add Darwin Streaming Server(4.1.1), a server which send streaming for QuickTime, MPEG4.
    PR:			ports/35383
    Submitted by:		Steve Wills <steve@stevenwills.com>
    Reviewed by:		Steve Wills <steve@stevenwills.com>
    Sponsored by:		Ensure Technology Ltd.

---
+++ net/DarwinStreamingServer/files/darwin_streaming_server.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/${0##*/}\$"); then
+	echo "$0: Cannot determine the PREFIX" >&2
+	exit 64
+fi
+
+if [ -r /etc/defaults/rc.conf ]; then
+	. /etc/defaults/rc.conf
+	source_rc_confs
+elif [ -r /etc/rc.conf ]; then
+	. /etc/rc.conf
+fi
+
+case "$1" in
+start)
+	case ${darwin_streaming_server_enable} in
+	[Yy][Ee][Ss])
+		/usr/bin/nice -5 ${darwin_streaming_server_program:-${PREFIX}/sbin/DarwinStreamingServer} ${darwin_streaming_server_flags} && \
+		echo -n ' DarwinStreamingServer'
+		;;
+	esac
+	;;
+stop)
+	case ${darwin_streaming_server_enable} in
+	[Yy][Ee][Ss])
+		/usr/bin/killall DarwinStreamingS && echo -n ' DarwinStreamingServer'
+		;;
+	esac
+	;;
+*)
+	echo "Usage: `basename $0` {start|stop}" >&2
+	;;
+esac
+
+exit 0
---
+++ net/DarwinStreamingServer/files/patch-WebAdmin::src::streamingadminserver.pl
@@ -0,0 +1,29 @@
+--- WebAdmin/src/streamingadminserver.pl.orig	Wed Apr 24 09:22:15 2002
++++ WebAdmin/src/streamingadminserver.pl	Mon Sep 30 02:48:53 2002
+@@ -43,7 +43,7 @@
+ }
+ else
+ {
+-    eval "use Net::SSleay";
++    eval "use Net::SSLeay";
+ }
+ $ssl_available = 0;
+ if (!$@) {
+@@ -241,7 +241,7 @@
+ 	  "gbrowse", "0",
+ 	  "ssl", "0",
+ 	  "crtfile", "/etc/streaming/streamingadminserver.pem",
+-	  "keyfile", "/etc/streaming/streamingadminserver.pem",
++	  "keyfile", "/etc/streaming/streamingadminserver.key",
+ 	  #"keypasswordfile", "",
+ 	  "qtssQTPasswd", "/usr/local/bin/qtpasswd",
+ 	  "qtssPlaylistBroadcaster", "/usr/local/bin/PlaylistBroadcaster",
+@@ -249,7 +249,7 @@
+ 	  "helpurl", "http://helpqt.apple.com/dssWebAdminHelpR3/dssWebAdmin.help/DSSHelp.htm",
+ 	  "qtssAdmin", "streamingadmin",
+ 	  "cacheMessageFiles", "0",
+-	  #"pidfile", "/var/streaming/streamingadminserver.pid"
++	  "pidfile", "/var/streaming/streamingadminserver.pid"
+ 	  );
+ }
+ 
