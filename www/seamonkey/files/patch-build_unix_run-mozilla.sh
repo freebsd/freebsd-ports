003058e18e23d2613ff372ef7aeee429c68bb6a6
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-10-23 04:26:25 +0000

    * Add a patch that fixes some localization issues [1] * Fix a problem with plugins not actually loading * Hopefully correct some issues with installs erroring out with Error 1 * Conditionalize Xft support (by default it's enabled, but can be disabled   by defined WITHOUT_XFT)
    Submitted by:	Jung-uk Kim <jkim@niksun.com> [1]
    Obtained from:	Mozilla's Bugzilla bug 174989 [1]

---
+++ www/seamonkey/files/patch-build_unix_run-mozilla.sh
@@ -0,0 +1,30 @@
+--- build/unix/run-mozilla.sh.orig	Wed Oct 23 00:08:03 2002
++++ build/unix/run-mozilla.sh	Wed Oct 23 00:08:59 2002
+@@ -355,7 +355,7 @@
+ fi
+ ##
+ ## Set LD_LIBRARY_PATH
+-LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
++LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%PREFIX%%/lib/browser_plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
+ if [ -n "$LD_LIBRARYN32_PATH" ]
+ then
+ 	LD_LIBRARYN32_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARYN32_PATH+":$LD_LIBRARYN32_PATH"}
+@@ -392,7 +392,7 @@
+         export XSUNTRANSPORT XSUNSMESIZE
+ fi
+ # Font path for Xft
+-FONTCONFIG_PATH="/etc/fonts:${MOZILLA_FIVE_HOME}/res/Xft"
++FONTCONFIG_PATH="%%PREFIX%%/etc/fonts:${MOZILLA_FIVE_HOME}/res/Xft"
+ export FONTCONFIG_PATH
+ 
+ if [ "$moz_debug" -eq 1 ]
+@@ -435,6 +435,9 @@
+ #
+ export MOZILLA_FIVE_HOME LD_LIBRARY_PATH
+ export SHLIB_PATH LIBPATH LIBRARY_PATH ADDON_PATH DYLD_LIBRARY_PATH
++
++MOZ_PLUGIN_PATH=%%PREFIX%%/lib/browser_plugins
++export MOZ_PLUGIN_PATH
+ 
+ if [ $moz_debug -eq 1 ]
+ then
