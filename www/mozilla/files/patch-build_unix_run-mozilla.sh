feeb650168744e79aae0b87434baa948d95898b2
marcus <marcus@FreeBSD.org>
2002-10-23 04:43:15 +0000

    * Fix a problem with plugins not actually loading * Hopefully correct some issues with installs erroring out with Error 1

--- www/mozilla/files/patch-build_unix_run-mozilla.sh
+++ www/mozilla/files/patch-build_unix_run-mozilla.sh
@@ -1,5 +1,14 @@
---- build/unix/run-mozilla.sh.orig	Thu Oct 17 00:00:21 2002
-+++ build/unix/run-mozilla.sh	Thu Oct 17 00:01:24 2002
+--- build/unix/run-mozilla.sh.orig	Wed Oct 23 00:08:03 2002
++++ build/unix/run-mozilla.sh	Wed Oct 23 00:08:59 2002
+@@ -355,7 +355,7 @@
+ fi
+ ##
+ ## Set LD_LIBRARY_PATH
+-LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
++LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%PREFIX%%/lib/browser_plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
+ if [ -n "$LD_LIBRARYN32_PATH" ]
+ then
+ 	LD_LIBRARYN32_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARYN32_PATH+":$LD_LIBRARYN32_PATH"}
 @@ -392,7 +392,7 @@
          export XSUNTRANSPORT XSUNSMESIZE
  fi
@@ -14,7 +23,7 @@
  export MOZILLA_FIVE_HOME LD_LIBRARY_PATH
  export SHLIB_PATH LIBPATH LIBRARY_PATH ADDON_PATH DYLD_LIBRARY_PATH
 +
-+MOZ_PLUGIN_PATH %%PREFIX%%/lib/browser_plugins
++MOZ_PLUGIN_PATH=%%PREFIX%%/lib/browser_plugins
 +export MOZ_PLUGIN_PATH
  
  if [ $moz_debug -eq 1 ]
