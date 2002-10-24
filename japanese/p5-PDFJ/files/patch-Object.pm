f3162438a756b37c4ab66def66b49827d6ccd2a4
Jun Kuriyama <kuriyama@FreeBSD.org>
2002-10-24 07:46:28 +0000

    Make this module "perl -w" friendly. Respect $PKGNAMEPREFIX in ../Makefile.inc.

---
+++ japanese/p5-PDFJ/files/patch-Object.pm
@@ -0,0 +1,10 @@
+--- PDFJ/Object.pm.orig	Thu Oct 24 16:32:34 2002
++++ PDFJ/Object.pm	Thu Oct 24 16:37:38 2002
+@@ -133,6 +133,7 @@
+ 
+ sub makeoutput {
+ 	my $self = shift;
++	$self->{outputtype} = "" if (not defined $self->{outputtype});
+ 	if( $self->{outputtype} !~ /^(literal|hex|hexliteral)$/ ) {
+ 		$self->{outputtype} = 
+ 			$self->{value} =~ /[\x80-\xff]/ ? 'hex' : 'literal';
