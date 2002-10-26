79d26d0fc37937244ba00745d6802d53ac903d6f
Akinori MUSHA <knu@FreeBSD.org>
2002-10-26 17:26:42 +0000

    s/ftp.dnsbalance.ring.gr.jp/www.dnsbalance.ring.gr.jp/

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -299,7 +299,7 @@ MASTER_SITE_REDHAT_LINUX+= \
 # servers at the DNS lookup level. (safe to use with "fetch -A")
 #
 MASTER_SITE_RINGSERVER+=	\
-	ftp://ftp.dnsbalance.ring.gr.jp/pub/%SUBDIR%/ \
+	ftp://www.dnsbalance.ring.gr.jp/pub/%SUBDIR%/ \
 	http://www.t.ring.gr.jp/archives/%SUBDIR%/
 
 MASTER_SITE_RUBY+= \
