bf85e9c0564b44cb1adea0065bb355d8f97f9f7d
obraun <obraun@FreeBSD.org>
2002-10-26 21:51:40 +0000

    Upgrade to 5.0.1.
    PR:		44493
    Submitted by:	maintainer

--- irc/tr-ircd/files/patch-include::config.h
+++ irc/tr-ircd/files/patch-include::config.h
@@ -1,27 +1,18 @@
---- include/config.h.orig	Sat May  4 15:57:33 2002
-+++ include/config.h	Sat May  4 15:59:21 2002
-@@ -33,12 +33,12 @@
- #define KLINEFILE	IRCD_PREFIX "/etc/kline.conf"
- #define	MOTD		IRCD_PREFIX "/etc/ircd.motd"
- #define	SHORTMOTD	IRCD_PREFIX "/etc/ircd.smotd"
--#define	LOGFILE		IRCD_PREFIX "/etc/ircd.log"
-+#define	LOGFILE		            "/var/log/ircd.log"
- #define LINKSFILE	IRCD_PREFIX "/etc/ircd.links"
+--- include/config.h.orig	Sat Oct 26 10:03:41 2002
++++ include/config.h	Sat Oct 26 10:13:43 2002
+@@ -38,11 +38,11 @@
+ #define LANGPATH 	IRCD_PREFIX_LIB "/languages"
+ #define CHANMODEPATH 	IRCD_PREFIX_LIB "/chanmodes"
  
--#define IRCD_PIDFILE    IRCD_PREFIX "/tmp/ircd.pid"
-+#define IRCD_PIDFILE                "/var/run/ircd.pid"
+-#define LOGFILE		IRCD_PREFIX_VAR "/logfiles/ircd.log"
+-#define IRCD_PIDFILE    IRCD_PREFIX_VAR "/ircd.pid"
+-#define LINKSFILE       IRCD_PREFIX_VAR "/ircd.links"
++#define LOGFILE		IRCD_PREFIX_VAR "/log/ircd.log"
++#define IRCD_PIDFILE    IRCD_PREFIX_VAR "/run/ircd.pid"
++#define LINKSFILE       IRCD_PREFIX_VAR "/run/ircd.links"
  
--#define HELPPATH	IRCD_PREFIX "/help/en_us"
-+#define HELPPATH	IRCD_PREFIX "/share/tr-ircd/help/en_us"
+-#define HELPPATH	IRCD_PREFIX_DOC "/help/en_us"
++#define HELPPATH	IRCD_PREFIX_DOC "/tr-ircd/help/en_us"
  
- /*
-  * LOCKFILE - Exclusive use of ircd.conf and kline.conf during writes
-@@ -51,7 +51,7 @@
-  * minutes have elapsed.
-  */
- 
--#define LOCKFILE 	IRCD_PREFIX "/tmp/ircd.conf.lock"
-+#define LOCKFILE 	            "/tmp/ircd.conf.lock"
- 
- #define USE_FAKEHOST
+ #define USE_FAKEHOST 1
  
