8e89a2b09cf469a098f296215bac08f9a9b54c39
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-10-28 16:55:22 +0000

    upgrade to 0.2.0
    PR:		44556
    Submitted by:	Erwin Lansing <erwin@lansing.dk>

---
+++ games/wmpuzzle/files/patch-wmpuzzle.h
@@ -0,0 +1,11 @@
+--- wmpuzzle.h.orig	Mon Oct 21 21:20:19 2002
++++ wmpuzzle.h	Sun Oct 27 17:28:09 2002
+@@ -8,7 +8,7 @@
+ #define WMPUZZLE_H_INCLUDED
+ 
+ #include <config.h>
+-#include <linux.xpm>
++#include <daemon.xpm>
+ 
+ #define EINTERNAL 200
+ #define EINVALXPM 201
