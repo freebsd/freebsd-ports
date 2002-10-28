a9d4075aeaada37644ce9d1aafd7febca4e0aafb
ijliao <ijliao@FreeBSD.org>
2002-10-28 16:55:22 +0000

    upgrade to 0.2.0
    PR:		44556
    Submitted by:	Erwin Lansing <erwin@lansing.dk>

--- games/wmpuzzle/files/patch-wmpuzzle.c
+++ games/wmpuzzle/files/patch-wmpuzzle.c
@@ -1,30 +1,13 @@
-$FreeBSD$
-
---- wmpuzzle.c.orig	Thu Apr 18 00:13:52 2002
-+++ wmpuzzle.c	Wed May 15 21:21:20 2002
-@@ -22,6 +22,7 @@
- #include "xpm/eagle.xpm"
- #include "xpm/earth.xpm"
- #include "xpm/linux.xpm"
-+#include "xpm/daemon.xpm"
+--- wmpuzzle.c.orig	Mon Oct 21 22:26:35 2002
++++ wmpuzzle.c	Sun Oct 27 17:28:07 2002
+@@ -254,8 +254,8 @@
+ 		}
+ 	}
+ 	if (image == NULL) {
+-		image = strdup("linux");
+-		*wmpuzzle = linux_xpm;
++		image = strdup("daemon");
++		*wmpuzzle = daemon_xpm;
+ 	}
+ }
  
- #define PROGRAM_NAME     "wmpuzzle"
- #define PROGRAM_VERSION  "0.1.1"
-@@ -203,6 +204,8 @@
- 				*wmpuzzle = earth_xpm;
- 			else if (!strcmp(optarg, "linux"))
- 				*wmpuzzle = linux_xpm;
-+			else if (!strcmp(optarg, "daemon"))
-+				*wmpuzzle = daemon_xpm;
- 			else {
- 				fprintf(stderr, "%s: invalid image `%s'\n", PROGRAM_NAME, optarg);
- 				exit(1);
-@@ -224,7 +227,7 @@
- 			printf("Usage: %s [-h] [-i <image>] [-k] [-s <count>] [-v]\n", PROGRAM_NAME);
- 			printf("  -h, --help             displays this command line summary.\n");
- 			printf("  -i, --image <image>    uses <image>, valid images are `debian', `eagle',\n");
--			printf("                         `earth', and `linux'.\n");
-+			printf("                         `earth', `linux', and `daemon'.\n");
- 			printf("  -k, --keyboard         enables the arrow keys on the keyboard.\n");
- 			printf("  -s, --shuffle <count>  shuffles the image <count> times.\n");
- 			printf("  -v, --version          displays the version number.\n");
