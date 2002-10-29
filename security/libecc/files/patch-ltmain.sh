5a5c9305f2ad47e61df2a0af9e68b6c1d6599faa
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-10-29 17:01:03 +0000

    add libecc 0.3.0 C++ Elliptic Curve Library

---
+++ security/libecc/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Sep 27 08:33:23 2002
++++ ltmain.sh	Wed Oct 30 00:58:58 2002
+@@ -4210,10 +4211,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
