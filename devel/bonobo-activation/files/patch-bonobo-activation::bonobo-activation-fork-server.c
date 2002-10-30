35ee867cbffcc1180a66a187c3103d873dc57258
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-10-30 00:36:47 +0000

    Update to 1.0.4.

--- devel/bonobo-activation/files/patch-bonobo-activation::bonobo-activation-fork-server.c
+++ devel/bonobo-activation/files/patch-bonobo-activation::bonobo-activation-fork-server.c
@@ -1,11 +1,11 @@
---- bonobo-activation/bonobo-activation-fork-server.c.orig	Wed Jul 24 16:46:27 2002
-+++ bonobo-activation/bonobo-activation-fork-server.c	Wed Jul 24 16:47:00 2002
-@@ -349,6 +349,8 @@
+--- bonobo-activation/bonobo-activation-fork-server.c.orig	Tue Oct 29 05:51:18 2002
++++ bonobo-activation/bonobo-activation-fork-server.c	Tue Oct 29 19:24:23 2002
+@@ -396,6 +396,8 @@
  	} else if ((childpid = fork ())) {
  		_exit (0);	/* de-zombifier process, just exit */
  	} else {
 +	    	sigprocmask (SIG_SETMASK, &omask, NULL);
 +
-                 if (display != NULL) {
-                         bonobo_activation_setenv ("DISPLAY", display);
-                 }
+ 		if (display)
+ 			bonobo_activation_setenv ("DISPLAY", display);
+ 		if (session_manager)
