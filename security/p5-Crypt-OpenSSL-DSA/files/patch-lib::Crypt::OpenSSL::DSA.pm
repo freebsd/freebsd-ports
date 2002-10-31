3caa7fa62190daf16599ab1cc788a7a2164396d8
Norikatsu Shigemura <nork@FreeBSD.org>
2002-10-31 09:55:22 +0000

    Update to 0.10
    PR:		ports/44777
    Submitted by:	Kimura Fuyuki <fuyuki@hadaly.org>

---
+++ security/p5-Crypt-OpenSSL-DSA/files/patch-lib::Crypt::OpenSSL::DSA.pm
@@ -0,0 +1,10 @@
+--- lib/Crypt/OpenSSL/DSA.pm.orig	Thu Sep 26 09:18:37 2002
++++ lib/Crypt/OpenSSL/DSA.pm	Thu Oct 31 18:45:29 2002
+@@ -1,7 +1,6 @@
+ package Crypt::OpenSSL::DSA;
+ 
+ use strict;
+-use warnings;
+ 
+ require DynaLoader;
+ 
