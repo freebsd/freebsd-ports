2ee755479a662187adfae34c7ecdae49f528b4e1
edwin <edwin@FreeBSD.org>
2002-10-31 03:16:29 +0000

    Make games/pengpong buildable again.
    PR:		ports/44455 (wrong subject)
    Submitted by:	edwin@freebsd.org
    Approved by:	pat@FreeBSD.org

---
+++ games/pengpong/files/patch-SFont.c
@@ -0,0 +1,9 @@
+--- SFont.c	Thu Aug 30 04:00:45 2001
++++ SFont.c	Fri Oct 25 21:07:58 2002
+@@ -1,6 +1,7 @@
+ #include <SDL.h>
+ #include "SFont.h"
+ #include <string.h>
++#include <stdlib.h>
+ 
+ SFont_FontInfo InternalFont;
