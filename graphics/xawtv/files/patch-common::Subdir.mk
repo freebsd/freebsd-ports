0ee4b9379837839e53dd473d203450991f092c9a
Patrick Li <pat@FreeBSD.org>
2002-11-01 00:39:36 +0000

    Update to 3.78
    PR:		ports/44764
    Submitted by:	maintainer

--- graphics/xawtv/files/patch-common::Subdir.mk
+++ graphics/xawtv/files/patch-common::Subdir.mk
@@ -1,11 +1,16 @@
---- common/Subdir.mk.orig	Sat Jul 27 10:24:27 2002
-+++ common/Subdir.mk	Sat Jul 27 10:22:47 2002
-@@ -16,7 +16,7 @@
+--- common/Subdir.mk.orig	Wed Oct 30 21:06:25 2002
++++ common/Subdir.mk	Wed Oct 30 21:06:41 2002
+@@ -16,10 +16,10 @@
  	common/midictrl.o
  
  # RegEdit.c is good old K&R ...
 -common/RegEdit.o : CFLAGS += -Wno-missing-prototypes -Wno-strict-prototypes
+-common/channel-no-x11.o: CFLAGS += -DNO_X11=1 
 +#common/RegEdit.o : CFLAGS += -Wno-missing-prototypes -Wno-strict-prototypes
++#common/channel-no-x11.o: CFLAGS += -DNO_X11=1 
  
  common/channel-no-x11.o:: common/channel.c
- 	$(CC) $(CFLAGS) -DNO_X11=1 -Wp,-MD,$*.dep -c -o $@ $<
+ 	@$(echo_compile_c)
+-	@$(compile_c)
++	@$(compile_c) -DNO_X11=1
+ 	@$(fixup_deps)
