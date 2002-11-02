0a72d59d6bb9c124300b741589c047eef7f324c3
Edwin Groothuis <edwin@FreeBSD.org>
2002-11-02 07:48:23 +0000

    Patched the patch-file of textproc/p5-PDF-API2 the wrong way. Thanks to Erwin Lansing to show me the right way.
    PR:		ports/44785
    Submitted by:	Erwin Lansing <erwin@lansing.dk>

--- textproc/p5-PDF-API3/files/patch-lib::Text::PDF::FileAPI.pm
+++ textproc/p5-PDF-API3/files/patch-lib::Text::PDF::FileAPI.pm
@@ -1,10 +1,10 @@
-+--- lib/Text/PDF/FileAPI.pm.orig       Thu Oct 31 15:46:58 2002
-++++ lib/Text/PDF/FileAPI.pm    Thu Oct 31 15:47:06 2002
-+@@ -3,7 +3,6 @@
-+ use strict;
-+ no strict "refs";
-+ use vars qw($cr %types $VERSION @ISA);
-+-no warnings qw(uninitialized);
-+
-+ use IO::File;
-+
+--- lib/Text/PDF/FileAPI.pm.orig       Fri Nov  1 13:01:57 2002
++++ lib/Text/PDF/FileAPI.pm    Fri Nov  1 13:02:03 2002
+@@ -3,7 +3,6 @@
+ use strict;
+ no strict "refs";
+ use vars qw($cr %types $VERSION @ISA);
+-no warnings qw(uninitialized);
+
+ use IO::File;
+
