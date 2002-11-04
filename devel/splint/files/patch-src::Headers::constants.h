999300f50eeec81665fd37164be486face8addb0
nectar <nectar@FreeBSD.org>
2002-11-04 13:27:23 +0000

    Let splint process <inttypes.h>.  Now it passes its own test suite.
    Submitted by:	Stacey Son <sson@verio.net>

---
+++ devel/splint/files/patch-src::Headers::constants.h
@@ -0,0 +1,11 @@
+--- src/Headers/constants.h.orig	Wed Oct 30 16:08:45 2002
++++ src/Headers/constants.h	Wed Oct 30 16:09:42 2002
+@@ -97,7 +97,7 @@
+ # define PPMRCODE "@@MR@@"  
+ 
+ /*@constant observer char *DEFAULT_SYSTEMDIR; @*/
+-# define DEFAULT_SYSTEMDIR "/usr/"
++# define DEFAULT_SYSTEMDIR "/usr/include/:/usr/lib/:/usr/local/include/:/usr/local/lib/"
+ 
+ /*@constant char DEFAULT_COMMENTCHAR; @*/
+ # define DEFAULT_COMMENTCHAR '@'
