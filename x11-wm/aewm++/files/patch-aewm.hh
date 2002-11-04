0770f23915b2a0ec58b90b6128eff9745bbe13b8
Christian Weisgerber <naddy@FreeBSD.org>
2002-11-04 00:46:58 +0000

    * Fix compilation on -CURRENT (GCC3).
    PR:		44856
    Submitted by:	Volker Stolz <vs@foldr.org>
    
    * Actually respect CXX.

---
+++ x11-wm/aewm++/files/patch-aewm.hh
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- aewm.hh.orig	Mon Nov  4 01:32:18 2002
++++ aewm.hh	Mon Nov  4 01:32:35 2002
+@@ -24,6 +24,8 @@
+ #ifndef _AEWM_HH_
+ #define _AEWM_HH_
+ 
++using namespace std;
++
+ #include <stdio.h>
+ #include <unistd.h>
+ #include <sys/wait.h>
