266a6d44cdb9993738dffb2c3b706a6ca59eeb36
dinoex <dinoex@FreeBSD.org>
2002-11-04 03:11:15 +0000

    - Fix GGC problem on current Submitted by:	vs@foldr.org

---
+++ net/licq-console/files/patch-console.h
@@ -0,0 +1,11 @@
+--- src/console.h.orig	Mon Nov  4 00:22:38 2002
++++ src/console.h	Mon Nov  4 00:22:50 2002
+@@ -5,6 +5,8 @@
+ #include "config.h"
+ #endif
+ 
++using namespace std;
++
+ #include "licq_icqd.h"
+ #include "licq_log.h"
+ #include "window.h"
