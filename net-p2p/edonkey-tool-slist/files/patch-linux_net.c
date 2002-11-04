4c7926365ef3adce4f4c4e4a7a9f3f544c713c1d
lioux <lioux@FreeBSD.org>
2002-11-04 23:55:30 +0000

    Fix regexp and patches to work with recently added USE_REINPLACE

---
+++ net-p2p/edonkey-tool-slist/files/patch-linux_net.c
@@ -0,0 +1,10 @@
+--- linux_net.c.orig	Mon Nov  4 21:50:07 2002
++++ linux_net.c	Mon Nov  4 21:50:15 2002
+@@ -27,6 +27,7 @@
+ #include <arpa/inet.h>
+ #include <netdb.h>
+ #include <unistd.h>
++#include <sys/time.h>
+ 
+ /* connect with timeout
+ *
