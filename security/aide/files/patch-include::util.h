9647b77dfd20c0905a562668394662542749b1bd
Cy Schubert <cy@FreeBSD.org>
2002-11-05 13:46:25 +0000

    Update 0.7 --> 0.9
    The aide database was in /var/adm/aide and is now in /var/db/aide. /var/adm
    is a SYSV directory that does not exist on FreeBSD.

---
+++ security/aide/files/patch-include::util.h
@@ -0,0 +1,9 @@
+--- include/util.h.orig	Wed May 29 01:04:26 2002
++++ include/util.h	Fri Jul 12 06:17:39 2002
+@@ -57,6 +57,4 @@
+ 
+ void init_sighandler(void);
+ 
+-char* strnstr(char* haystack,char* needle,int n);
+-
+ #endif
