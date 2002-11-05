265547f25df9312d1978197d2cf25b572baebba9
Trevor Johnson <trevor@FreeBSD.org>
2002-11-05 20:19:43 +0000

    Enable compilation on -CURRENT.
    Submitted by:	Volker Stolz
    PR:		44931

---
+++ astro/openuniverse/files/patch-src_ou.h
@@ -0,0 +1,12 @@
+$FreeBSD$
+
+--- src/ou.h.orig	Sun Jun  4 12:35:09 2000
++++ src/ou.h	Tue Nov  5 12:17:24 2002
+@@ -19,6 +19,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <math.h>
+ 
+ #ifdef WIN32
