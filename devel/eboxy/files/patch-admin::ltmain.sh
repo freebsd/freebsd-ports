6f0cb0ff51ae668870ed6c6f8ffb24d465d76d62
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-11-06 17:03:40 +0000

    upgrade to 0.3.3
    PR:		44967
    Submitted by:	Ports Fury

---
+++ devel/eboxy/files/patch-admin::ltmain.sh
@@ -0,0 +1,15 @@
+--- admin/ltmain.sh.orig	Tue Oct  1 17:43:57 2002
++++ admin/ltmain.sh	Sat Nov  2 17:38:55 2002
+@@ -4746,10 +4746,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
