dc2190846a4490c208e610f342a00f26834a4084
Edwin Groothuis <edwin@FreeBSD.org>
2002-11-07 12:52:37 +0000

    - Fix hagelslag on -CURRENT by including missing header - CFLAGS police
    PR:		ports/44890
    Submitted by:	Volker Stolz <vs@foldr.org>

---
+++ net/hagelslag/files/patch-src-SearchManager.cpp
@@ -0,0 +1,10 @@
+--- src/SearchManager.cpp.orig	Mon Nov  4 21:18:37 2002
++++ src/SearchManager.cpp	Mon Nov  4 21:19:01 2002
+@@ -9,6 +9,7 @@
+ 
+ #include <time.h>
+ #include <stdio.h>
++#include <string.h>
+ 
+ #include "Config.h"
+ #include "Constants.h"
