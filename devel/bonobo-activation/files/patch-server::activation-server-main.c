9b2a20c771de145a5b051386ff690540544d371a
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-11-10 21:33:21 +0000

    Re-introduce 1.0.4.  The previous problem where GNOME would hang at startup with 1.0.4 should be fixed.
    Not objected to by:	gnome

--- devel/bonobo-activation/files/patch-server::activation-server-main.c
+++ devel/bonobo-activation/files/patch-server::activation-server-main.c
@@ -1,9 +1,6 @@
-
-$FreeBSD$
-
---- server/activation-server-main.c.orig	Tue May 14 04:51:55 2002
-+++ server/activation-server-main.c	Mon May 27 13:36:25 2002
-@@ -127,7 +127,7 @@
+--- server/activation-server-main.c.orig	Tue Oct 29 05:51:19 2002
++++ server/activation-server-main.c	Fri Nov  8 03:21:04 2002
+@@ -131,7 +131,7 @@
                          g_string_append (gnome_od_source_dir,
                                           gnome_dirs[i]);
                          g_string_append (gnome_od_source_dir,
@@ -12,3 +9,12 @@ $FreeBSD$
                  }
                  g_strfreev (gnome_dirs);
                  g_string_append_c (real_od_source_dir, ':');
+@@ -197,6 +197,8 @@
+          * terminal, etc.
+          */
+         setsid ();
++
++	unsetenv ("SESSION_MANAGER");
+         
+ 	/* internationalization. */
+ 	setlocale (LC_ALL, "");
