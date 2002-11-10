d2addcb43cd24b15a5df2b6d07af18183f62e575
marcus <marcus@FreeBSD.org>
2002-11-10 21:33:21 +0000

    Re-introduce 1.0.4.  The previous problem where GNOME would hang at startup with 1.0.4 should be fixed.
    Not objected to by:	gnome

--- devel/bonobo-activation/files/patch-bonobo-activation::bonobo-activation-fork-server.c
+++ devel/bonobo-activation/files/patch-bonobo-activation::bonobo-activation-fork-server.c
@@ -1,11 +1,11 @@
---- bonobo-activation/bonobo-activation-fork-server.c.orig	Wed Jul 24 16:46:27 2002
-+++ bonobo-activation/bonobo-activation-fork-server.c	Wed Jul 24 16:47:00 2002
-@@ -349,6 +349,8 @@
+--- bonobo-activation/bonobo-activation-fork-server.c.orig	Fri Nov  8 03:19:52 2002
++++ bonobo-activation/bonobo-activation-fork-server.c	Fri Nov  8 03:20:04 2002
+@@ -396,6 +396,8 @@
  	} else if ((childpid = fork ())) {
  		_exit (0);	/* de-zombifier process, just exit */
  	} else {
 +	    	sigprocmask (SIG_SETMASK, &omask, NULL);
 +
-                 if (display != NULL) {
-                         bonobo_activation_setenv ("DISPLAY", display);
-                 }
+ 		if (display)
+ 			bonobo_activation_setenv ("DISPLAY", display);
+ 		if (session_manager)
