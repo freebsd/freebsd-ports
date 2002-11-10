9eeffbcbf876a4ee058d8cedfb6ce956696494ee
Dirk Froemberg <dirk@FreeBSD.org>
2002-11-10 20:24:24 +0000

    Only kill mysqld we started. Don't kill all mysqld.
    Submitted by:	sobomax (modified by me a bit)

--- databases/mysql60-server/files/mysql-server.sh
+++ databases/mysql60-server/files/mysql-server.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+DB_DIR=%%DB_DIR%%
+PIDFILE=${DB_DIR}/`/bin/hostname -s`.pid
+
 case "$1" in
 	start)
 		if [ -x %%PREFIX%%/bin/safe_mysqld ]; then
-			%%PREFIX%%/bin/safe_mysqld --user=mysql > /dev/null &
+			%%PREFIX%%/bin/safe_mysqld --user=mysql --datadir=${DB_DIR} --pid-file=${PIDFILE} > /dev/null &
 			echo -n ' mysqld'
 		fi
 		;;
 	stop)
-		/usr/bin/killall mysqld > /dev/null 2>&1 && echo -n ' mysqld'
+		if [ -f ${PIDFILE} ]; then
+			/bin/kill `cat ${PIDFILE}` > /dev/null 2>&1 && echo -n ' mysqld'
+		else
+			echo "mysql-server isn't running"
+		fi
 		;;
 	*)
 		echo ""
