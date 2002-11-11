b8b6157940288d89b2d554bae83ca653aa12e575
marcus <marcus@FreeBSD.org>
2002-11-11 19:44:47 +0000

    Chase the shared lib versions of gal and gtkhtml.

--- Mk/bsd.gnomeng.mk
+++ Mk/bsd.gnomeng.mk
@@ -171,7 +171,7 @@ libglade_MAKE_ENV=	LIBGLADE_CONFIG="${LIBGLADE_CONFIG}"
 libglade_DETECT=	${LIBGLADE_CONFIG}
 libglade_USE_GNOME_IMPL=gnomedb
 
-gal_LIB_DEPENDS=	gal.19:${PORTSDIR}/x11-toolkits/gal
+gal_LIB_DEPENDS=	gal.21:${PORTSDIR}/x11-toolkits/gal
 gal_DETECT=		${X11BASE}/etc/galConf.sh
 gal_USE_GNOME_IMPL=	libglade
 
@@ -179,7 +179,7 @@ glibwww_LIB_DEPENDS=	glibwww.1:${PORTSDIR}/www/glibwww
 glibwww_DETECT=		${X11BASE}/etc/glibwwwConf.sh
 glibwww_USE_GNOME_IMPL=	gnomelibs
 
-gtkhtml_LIB_DEPENDS=	gtkhtml.21:${PORTSDIR}/www/gtkhtml
+gtkhtml_LIB_DEPENDS=	gtkhtml-1.1.3:${PORTSDIR}/www/gtkhtml
 gtkhtml_DETECT=		${X11BASE}/etc/gtkhtmlConf.sh
 gtkhtml_USE_GNOME_IMPL=	glibwww gal libghttp libcapplet
 
