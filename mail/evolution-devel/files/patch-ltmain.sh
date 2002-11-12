624cb4d85b919a50995e9969e71e6fcd58d10031
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-11-12 00:01:04 +0000

    Update to 1.2.0.  This is a big release, and I don't want to polute the the CVS log with all the details.  However, almost every aspect has been touched.  Note, that this is still a GNOME 1.x application!
    More info on the features can be found at Ximian:
    http://www.ximian.com/products/evolution/

---
+++ mail/evolution-devel/files/patch-ltmain.sh
@@ -0,0 +1,42 @@
+--- ltmain.sh.orig	Fri Sep 13 15:38:11 2002
++++ ltmain.sh	Fri Oct 11 01:44:24 2002
+@@ -1073,8 +1073,16 @@
+ 	continue
+ 	;;
+ 
++      -pthread)
++      compile_command="$compile_command -pthread"
++      finalize_command="$finalize_command -pthread"
++      compiler_flags="$compiler_flags -pthread"
++      continue
++      ;;
++
+       -module)
+ 	module=yes
++	build_old_libs=no
+ 	continue
+ 	;;
+ 
+@@ -2502,6 +2510,9 @@
+ 	  *-*-openbsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test $build_libtool_need_lc = "yes"; then
+@@ -4286,10 +4297,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
