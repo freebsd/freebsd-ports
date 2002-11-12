8dd6391924385c0a76772694330931437bd17212
obraun <obraun@FreeBSD.org>
2002-11-12 23:25:10 +0000

    Upgrade to 1.51.
    PR:		ports/45155
    Submitted by:	Volker Stolz <vs@foldr.org>

---
+++ sysutils/xsysstats/files/patch-xsysstats.c
@@ -0,0 +1,8 @@
+--- xsysstats.c.orig	Sat Nov  9 08:44:11 2002
++++ xsysstats.c	Sat Nov  9 08:44:45 2002
+@@ -1,5 +1,4 @@
+ #include "xsysstats.h"
+-#include "headers.h"
+ #include "patchlevel.h"
+ 
+ struct base_types {
