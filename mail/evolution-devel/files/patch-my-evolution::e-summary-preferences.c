ddd8f4bbe560893ede58033a341959f6bbb57cfc
marcus <marcus@FreeBSD.org>
2002-11-12 00:01:04 +0000

    Update to 1.2.0.  This is a big release, and I don't want to polute the the CVS log with all the details.  However, almost every aspect has been touched.  Note, that this is still a GNOME 1.x application!
    More info on the features can be found at Ximian:
    http://www.ximian.com/products/evolution/

--- mail/evolution-devel/files/patch-my-evolution::e-summary-preferences.c
+++ mail/evolution-devel/files/patch-my-evolution::e-summary-preferences.c
@@ -1,17 +1,13 @@
-
-$FreeBSD$
-
---- my-evolution/e-summary-preferences.c	2001/12/15 11:37:58	1.1
-+++ my-evolution/e-summary-preferences.c	2001/12/15 11:41:56
-@@ -440,9 +440,11 @@
- 	{"http://www.bsdtoday.com/backend/bt.rdf", "BSD Today"},
- 	{"http://beyond2000.com/b2k.rdf", "Beyond 2000"},
- 	{"http://www.cnn.com/cnn.rss", "CNN"},
+--- my-evolution/e-summary-preferences.c.orig	Thu Sep 26 15:23:36 2002
++++ my-evolution/e-summary-preferences.c	Tue Oct 15 17:22:04 2002
+@@ -581,8 +581,10 @@
+ 	{"http://barrapunto.com/gnome.rdf", "Barrapunto GNOME", FALSE,},
+ 	{"http://www.bsdtoday.com/backend/bt.rdf", "BSD Today", FALSE},
+ 	{"http://beyond2000.com/b2k.rdf", "Beyond 2000", FALSE},
 +	{"http://daily.daemonnews.org/ddn.rdf.php3", "Daily Daemon News"},
-         {"http://www.debianplanet.org/debianplanet/backend.php", "Debian Planet"},
- 	{"http://www.dictionary.com/wordoftheday/wotd.rss", N_("Dictionary.com Word of the Day")},
- 	{"http://www.dvdreview.com/rss/newschannel.rss", "DVD Review"},
+ 	{"http://www.dictionary.com/wordoftheday/wotd.rss", N_("Dictionary.com Word of the Day"), FALSE},
+ 	{"http://www.dvdreview.com/rss/newschannel.rss", "DVD Review", FALSE},
 +	{"http://www.freebsd.org/news/news.rdf", "FreeBSD Newsflash"},
- 	{"http://freshmeat.net/backend/fm.rdf", "Freshmeat"},
- 	{"http://news.gnome.org/gnome-news/rdf", "GNotices"},
- 	{"http://headlines.internet.com/internetnews/prod-news/news.rss", "Internet.com"},
+ 	{"http://freshmeat.net/backend/fm.rdf", "Freshmeat", FALSE},
+ 	{"http://news.gnome.org/gnome-news/rdf", "GNotices", FALSE},
+ 	{"http://headlines.internet.com/internetnews/prod-news/news.rss", "Internet.com", FALSE},
