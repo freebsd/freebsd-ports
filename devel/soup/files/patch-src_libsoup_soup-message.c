8b51014c7a09a7ba1d3185b6c19334f7087176d4
marcus <marcus@FreeBSD.org>
2002-11-12 07:53:46 +0000

    Fix a problem with soup causing Evolution to segfault when trying to get RDF newsfeeds.  This is an attempt to solve both this crash as well as get it building on 64-bot architectures.

--- devel/soup/files/patch-src_libsoup_soup-message.c
+++ devel/soup/files/patch-src_libsoup_soup-message.c
@@ -1,11 +1,22 @@
---- src/libsoup/soup-message.c.orig	Sun Sep 29 23:50:30 2002
-+++ src/libsoup/soup-message.c	Sun Sep 29 23:59:10 2002
-@@ -922,7 +922,7 @@
+--- src/libsoup/soup-message.c.orig	Wed Apr 10 12:33:17 2002
++++ src/libsoup/soup-message.c	Tue Nov 12 02:52:40 2002
+@@ -907,7 +907,7 @@
+ 
+ 	SoupHandlerKind   kind;
+ 	union {
+-		guint             errorcode;
++		unsigned long             errorcode;
+ 		SoupErrorClass    errorclass;
+ 		const gchar      *header;
+ 	} data;
+@@ -922,8 +922,9 @@
  		redirect_handler, 
  		NULL, 
  		RESPONSE_HEADER_HANDLER, 
 -		{ (guint) "Location" }
-+		{ (guint) 0 }
++		{ (unsigned long) "Location" }
  	},
++
  	/* 
  	 * Handle authorization.
+ 	 */
