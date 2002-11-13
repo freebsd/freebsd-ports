5773181e82b6bf875b0cd2ba83be5e6f3c32e150
SADA Kenji <sada@FreeBSD.org>
2002-11-13 10:50:49 +0000

    - upgrade French dictionary to fix problem - add three new dictionaries PR:	ports/44244 Submitted by:	maintainer

---
+++ textproc/aspell/pkg-plist.cy
@@ -0,0 +1,4 @@
+share/aspell/cy.dat
+share/aspell/cy.multi
+share/aspell/cy.rws
+share/aspell/welsh.alias
---
+++ textproc/aspell/pkg-plist.el
@@ -0,0 +1,5 @@
+share/aspell/el.dat
+share/aspell/el_phonet.dat
+share/aspell/el.multi
+share/aspell/el.rws
+share/aspell/greek.alias
