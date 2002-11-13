10c836257ca59a8f77a9f635cad6c9e68c2161df
dirk <dirk@FreeBSD.org>
2002-11-13 10:38:20 +0000

    -STABLE exits with signal 11 while connecting via TCP/IP if empty prototypes are used. -CURRENT needs empty prototypes to build. Use __FreeBSD_version to differentiate.
    Set CXX to $CC on -STABLE again to get around stability problems
    under heavy load.

--- databases/mysql323-server/files/patch-sql_mysqld.cc
+++ databases/mysql323-server/files/patch-sql_mysqld.cc
@@ -1,11 +1,12 @@
---- sql/mysqld.cc.orig	Wed Aug 14 00:08:13 2002
-+++ sql/mysqld.cc	Sun Nov 10 10:23:55 2002
-@@ -92,7 +92,7 @@
+--- sql/mysqld.cc.orig	Thu Oct 10 12:17:28 2002
++++ sql/mysqld.cc	Tue Nov 12 23:23:27 2002
+@@ -92,7 +92,8 @@
  int allow_severity = LOG_INFO;
  int deny_severity = LOG_WARNING;
  
 -#ifdef __STDC__
-+#if defined(__STDC__) && !defined(__FreeBSD__)
++#include <osreldate.h>
++#if defined(__STDC__) && __FreeBSD_version < 500000
  #define my_fromhost(A)	   fromhost(A)
  #define my_hosts_access(A) hosts_access(A)
  #define my_eval_client(A)  eval_client(A)
