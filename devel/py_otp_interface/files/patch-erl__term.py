b2cf40e6be0fd431d767dd61f7de9d59e2368f07
Jimmy Olgeni <olgeni@FreeBSD.org>
2002-11-16 20:22:55 +0000

    Bugfix: non-integer lists terminate with a NIL value that should be skipped when unpacking terms (also submitted to author).

--- devel/py_otp_interface/files/patch-erl__term.py
+++ devel/py_otp_interface/files/patch-erl__term.py
@@ -2,7 +2,7 @@
 $FreeBSD$
 
 --- erl_term.py.orig	Wed May 29 00:09:25 2002
-+++ erl_term.py	Sat Nov 16 00:53:45 2002
++++ erl_term.py	Sat Nov 16 17:40:38 2002
 @@ -282,7 +282,7 @@
          if dataLen < 5:
              return (None, data)
@@ -12,6 +12,15 @@ $FreeBSD$
  
      elif data0 == MAGIC_FLOAT:
          if dataLen < 32:
+@@ -373,7 +373,7 @@
+         (elements, remainingData) = _UnpackTermSeq(arity, data[5:])
+         if elements == None:
+             return (None, data)
+-        return (ErlList(elements), remainingData)
++        return (ErlList(elements), remainingData[1:]) # skip MAGIC_NIL
+ 
+     elif data0 == MAGIC_BINARY:
+         if dataLen < 5:
 @@ -552,11 +552,11 @@
      
  def _PackString(term):
