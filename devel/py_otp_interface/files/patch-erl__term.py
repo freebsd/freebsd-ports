12a846028174589e7e6df50a75d6fa21d9acefa2
Jimmy Olgeni <olgeni@FreeBSD.org>
2002-11-16 00:18:29 +0000

    Caught bug (typo) in erl_term._PackString. Also submitted to author.

--- devel/py_otp_interface/files/patch-erl__term.py
+++ devel/py_otp_interface/files/patch-erl__term.py
@@ -1,8 +1,8 @@
 
 $FreeBSD$
 
---- erl_term.py.orig	Fri Nov 15 18:53:36 2002
-+++ erl_term.py	Fri Nov 15 18:54:03 2002
+--- erl_term.py.orig	Wed May 29 00:09:25 2002
++++ erl_term.py	Sat Nov 16 00:53:45 2002
 @@ -282,7 +282,7 @@
          if dataLen < 5:
              return (None, data)
@@ -12,3 +12,17 @@ $FreeBSD$
  
      elif data0 == MAGIC_FLOAT:
          if dataLen < 32:
+@@ -552,11 +552,11 @@
+     
+ def _PackString(term):
+     if len(term) == 0:
+-        return PackList([])
++        return _PackList([])
+     elif len(term) <= 65535:
+         return _PackInt1(MAGIC_STRING) + _PackInt2(len(term)) + term
+     else:
+-        return PackList(map(lambda c: ord(c), term))
++        return _PackList(map(lambda c: ord(c), term))
+ 
+ def _PackList(term):
+     if len(term) == 0:
