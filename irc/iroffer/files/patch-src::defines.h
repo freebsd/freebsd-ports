d739bdf1dcc24bd072e23168e260f4b71069ec0b
Edwin Groothuis <edwin@FreeBSD.org>
2002-11-17 12:37:45 +0000

    Unbreak on -current (1 down, 727 to go) Noticed on: bento

---
+++ irc/iroffer/files/patch-src::defines.h
@@ -0,0 +1,10 @@
+--- src/defines.h.orig	Sun Nov 17 23:37:55 2002
++++ src/defines.h	Sun Nov 17 23:38:02 2002
+@@ -176,6 +176,6 @@
+ #error "*** ERROR: This OS Is Not Supported Or You Didn't Run Configure ***"
+ #endif
+ 
+-#endif _IROFFER_DEFINES
++#endif
+ 
+ /* End of File */
---
+++ irc/iroffer/files/patch-src::globals.h
@@ -0,0 +1,10 @@
+--- src/globals.h.orig	Sun Nov 17 23:37:17 2002
++++ src/globals.h	Sun Nov 17 23:37:31 2002
+@@ -140,6 +140,6 @@
+ 
+ GEX meminfo_t meminfo[MAXMEMINFO];
+ 
+-#endif _IROFFER_GLOBALS
++#endif
+ 
+ /* End of File */
