17e7e0c32cfa5e7eda7ab9120cd0d049c514bb5f
sada <sada@FreeBSD.org>
2002-11-17 06:21:50 +0000

    Remove all netscape ports with a vulnerability of JRE. cf. http://sunsolve.sun.com/pub-cgi/retrieve.pl?doctype=coll&doc=secbull/218&type=0&nav=sec.sba Submitted by:	trevor Silently agreed by:	maintainers

--- korean/netscape3/files/netscape.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export LANG ; LANG=ko_KR.EUC
-export XKEYSYMDB ; XKEYSYMDB=@X11BASE@/lib/X11/XKeysymDB
-export XNLSPATH  ; XNLSPATH=@PREFIX@/lib/netscape/nls
-export XAPPLRESDIR ; XAPPLRESDIR=@PREFIX@/lib/netscape/$LANG
-export CLASSPATH ; CLASSPATH=.:@PREFIX@/lib/netscape
-exec /usr/local/lib/netscape/netscape.bin $*
