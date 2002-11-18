dae21723c26316ee0cb5dc91a2d30c88f038fb43
edwin <edwin@FreeBSD.org>
2002-11-18 23:12:20 +0000

    Make games/groundhog building with gcc321 again.

--- games/groundhog/files/patch-src-highscore.cc
+++ games/groundhog/files/patch-src-highscore.cc
@@ -1,13 +1,11 @@
 --- src/highscore.cc/old	Sat May  4 23:43:58 2002
 +++ src/highscore.cc	Thu May 23 14:10:49 2002
-@@ -26,6 +26,10 @@
+@@ -26,6 +26,8 @@
     std::string filename = getenv("HOME");
     filename += "/.groundhog.highscore";
     std::ifstream in(filename.c_str());
-+   if (in==NULL) {
-+       cout << "Couldn't open highscore file (this is normal the first time the game is ran).\n";
++   if (in==NULL)
 +       return;
-+   }
     in >> _beginner >> _intermediate >> _expert;
  }
  
