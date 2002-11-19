fa3824fc601e36958e76872797c6dd5c4b450ddc
arved <arved@FreeBSD.org>
2002-11-19 16:41:28 +0000

    Update to 1.5.4
    PR:		43694
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

---
+++ cad/qcad/files/patch-rprgdef.h
@@ -0,0 +1,25 @@
+--- rprgdef.h.orig	Fri Sep 20 19:26:57 2002
++++ rprgdef.h	Tue Nov 19 16:48:27 2002
+@@ -23,12 +23,14 @@
+ 
+ 
+ // Uncomment for Linux
++/*
+ #define DEF_LINUX           // Are we running Linux?
+ #define RUNNING_LINUX   true
+ #define RUNNING_WINDOWS false
+ #define RUNNING_SOLARIS false
+ #define RUNNING_IRIX    false
+ #define DATADIR         "/usr/share"
++*/
+ 
+ 
+ #ifdef __FreeBSD__
+@@ -38,6 +40,7 @@
+ #define RUNNING_WINDOWS false
+ #define RUNNING_SOLARIS false
+ #define RUNNING_IRIX    false
++#define DATADIR		"/usr/local/share"
+ #endif
+ 
+ 
