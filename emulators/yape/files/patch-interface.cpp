165f72d80a70691b9654ac00cead0004cebecfa5
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-11-19 14:39:00 +0000

    add yape 0.32.3 Yet Another Commodore +4 Emulator
    PR:		45004
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu>

---
+++ emulators/yape/files/patch-interface.cpp
@@ -0,0 +1,11 @@
+--- interface.cpp.orig	Thu Oct 31 23:43:08 2002
++++ interface.cpp	Thu Oct 31 23:43:35 2002
+@@ -10,7 +10,7 @@
+ 	(c) 2000, 2001 Attila Grósz
+ */
+ 
+-#include "SDL/SDL.h"
++#include "SDL.h"
+ #include "tedmem.h"
+ #include "interface.h"
+ #include "archdep.h"
