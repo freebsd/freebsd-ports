68b1e8a25e7038655f61323ef3b1a6a2a2aee275
edwin <edwin@FreeBSD.org>
2002-11-19 04:54:44 +0000

    (it might be a smart thing to also commit the new files too) Make games/lincity building on -current again. Noticed on: bento

---
+++ games/lincity/files/patch-mps.cxx
@@ -0,0 +1,10 @@
+--- mps.cxx.orig	Tue Nov 19 05:30:41 2002
++++ mps.cxx	Tue Nov 19 05:30:56 2002
+@@ -4,6 +4,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <math.h>
++#include <string.h>
+ #if !defined (WIN32)
+ #include <strings.h>
+ #include <unistd.h>
---
+++ games/lincity/files/patch-screen.cxx
@@ -0,0 +1,10 @@
+--- screen.cxx.orig	Tue Nov 19 05:29:40 2002
++++ screen.cxx	Tue Nov 19 05:29:50 2002
+@@ -3,6 +3,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <math.h>
+ #if !defined (WIN32)
+ #include <strings.h>
