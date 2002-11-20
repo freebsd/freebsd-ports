5e8f2683068a37fd672b07490f0ac4947150dab2
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-11-20 12:27:06 +0000

    - Do not install useless .la files - Support CXFLAGS/CXXFLAGS properly
    PR:		45426
    Submitted by:	Ports Fury

---
+++ devel/ppl/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Mon Jul  1 23:54:06 2002
++++ ltmain.sh	Mon Nov 11 22:34:15 2002
+@@ -4772,10 +4772,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
