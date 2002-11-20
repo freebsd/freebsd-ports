7be8a4e237cda94d21978e4a60846b14b9d4e54c
ijliao <ijliao@FreeBSD.org>
2002-11-20 13:31:44 +0000

    upgrade to 1.8.1
    PR:		45458
    Submitted by:	Ports Fury

--- www/harvest/files/patch-src::common::util::strerror.c
+++ www/harvest/files/patch-src::common::util::strerror.c
@@ -1,5 +1,5 @@
---- ./src/common/util/strerror.c.orig	Sun Oct 20 17:43:50 2002
-+++ ./src/common/util/strerror.c	Sun Oct 20 17:43:59 2002
+--- src/common/util/strerror.c.orig	Mon Mar 17 07:16:32 1997
++++ src/common/util/strerror.c	Wed Nov 13 08:58:11 2002
 @@ -89,8 +89,10 @@
  char *strerror(n)
       int n;
