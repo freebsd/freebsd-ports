0097a8100a176fa42b699ee14751a8ed8480331f
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-11-20 13:15:12 +0000

    - Do not install useless .la files - Utilize NOPORTDOCS
    PR:		45446
    Submitted by:	Ports Fury

---
+++ databases/db43/files/patch-dist::ltmain.sh
@@ -0,0 +1,32 @@
+--- ../dist/ltmain.sh.orig	Sat Nov 17 01:15:01 2001
++++ ../dist/ltmain.sh	Sat Nov  9 17:08:02 2002
+@@ -1043,14 +1043,14 @@
+ 	    # These systems don't actually have a C library (as such)
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+ 	  esac
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -4217,10 +4217,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
