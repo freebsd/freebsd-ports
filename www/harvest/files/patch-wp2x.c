20f9b653f16269f5fee15e883e2a373c6433782d
ijliao <ijliao@FreeBSD.org>
2002-11-21 05:22:31 +0000

    fix build on -current
    Noted by:	kris

---
+++ www/harvest/files/patch-wp2x.c
@@ -0,0 +1,10 @@
+--- components/gatherer/WordPerfect/wp2x-2.3/wp2x.c.orig	Thu Nov 21 11:33:54 2002
++++ components/gatherer/WordPerfect/wp2x-2.3/wp2x.c	Thu Nov 21 13:13:25 2002
+@@ -155,7 +155,6 @@
+ extern char *Cdecl sys_errlist[];
+ #endif
+ extern int   Cdecl errno;
+-extern int   Cdecl sys_nerr;
+ 
+ /************************************************************************/
+ /* Some common idioms                                                   */
