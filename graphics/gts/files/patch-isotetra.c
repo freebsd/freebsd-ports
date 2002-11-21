f7322ffadffea76a9b94c905bab65d84f5011868
Ying-Chieh Liao <ijliao@FreeBSD.org>
2002-11-21 09:35:05 +0000

    add missing header files
    Noted by:	kris

---
+++ graphics/gts/files/patch-isotetra.c
@@ -0,0 +1,10 @@
+--- src/isotetra.c.orig	Thu Nov 21 16:27:11 2002
++++ src/isotetra.c	Thu Nov 21 16:27:34 2002
+@@ -18,6 +18,7 @@
+  */
+ 
+ #include <math.h>
++#include <string.h>
+ #include <gts.h>
+ #ifdef NATIVE_WIN32
+ # include <memory.h>
