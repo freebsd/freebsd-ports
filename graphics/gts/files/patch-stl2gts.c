b28c3f2813d0bcd26ea738ade24e5493a9bb3d76
ijliao <ijliao@FreeBSD.org>
2002-11-21 09:35:05 +0000

    add missing header files
    Noted by:	kris

---
+++ graphics/gts/files/patch-stl2gts.c
@@ -0,0 +1,10 @@
+--- tools/stl2gts.c.orig	Thu Nov 21 16:30:54 2002
++++ tools/stl2gts.c	Thu Nov 21 16:31:06 2002
+@@ -19,6 +19,7 @@
+ 
+ #include <math.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include "config.h"
+ #ifdef HAVE_GETOPT_H
+ #  include <getopt.h>
