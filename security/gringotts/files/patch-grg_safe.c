ceae2fc8badf12ba44bfd1f7b7e8d136e7825c38
anders <anders@FreeBSD.org>
2002-11-23 23:20:38 +0000

    Update to 1.2.1.

--- security/gringotts/files/patch-grg_safe.c
+++ security/gringotts/files/patch-grg_safe.c
@@ -1,6 +1,6 @@
---- src/grg_safe.c.orig	Sat Oct 26 14:09:40 2002
-+++ src/grg_safe.c	Sat Oct 26 14:15:28 2002
-@@ -34,6 +34,7 @@
+--- src/grg_safe.c.orig	Sat Nov 23 18:42:36 2002
++++ src/grg_safe.c	Sat Nov 23 18:43:22 2002
+@@ -33,6 +33,7 @@
  
  #include <stdlib.h>
  #include <string.h>
@@ -8,7 +8,7 @@
  #include <regex.h>
  #include <unistd.h>
  #include <fcntl.h>
-@@ -43,7 +44,6 @@
+@@ -42,7 +43,6 @@
  #include <sys/fsuid.h>
  #endif
  #include <sys/time.h>
@@ -16,21 +16,21 @@
  #include <sys/resource.h>
  
  #define GRG_SAFE			0
-@@ -64,8 +64,6 @@
+@@ -56,8 +56,6 @@
+ grg_mlockall_and_drop_root_privileges (void)
+ {
+   //drop eventual group root privileges
+-  setgid (getgid ());
+-  setgid (getgid ());		//twice for counter "saved IDs", cfr. Secure Programming HowTo
+ #ifdef HAVE_SYS_FSUID_H
+   setfsgid (getgid ());
+   setfsgid (getgid ());
+@@ -81,8 +79,6 @@
  #endif
  
- 	//drop eventual group root privileges
--	setgid(getgid ());
--	setgid(getgid ()); //twice for counter "saved IDs", cfr. Secure Programming HowTo
+       //drop root privileges
+-      setuid (getuid ());
+-      setuid (getuid ());
  #ifdef HAVE_SYS_FSUID_H
- 	setfsgid(getgid ());
- 	setfsgid(getgid ());
-@@ -84,8 +82,6 @@
- #endif
- 
- 		//drop root privileges
--		setuid(getuid());
--		setuid(getuid());
- #ifdef HAVE_SYS_FSUID_H
- 		setfsuid(getuid());
- 		setfsuid(getuid());
+       setfsuid (getuid ());
+       setfsuid (getuid ());
