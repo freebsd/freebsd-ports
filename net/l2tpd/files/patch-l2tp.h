bcdca39b525838b23864850073a7a8f34ff94f32
sobomax <sobomax@FreeBSD.org>
2002-11-23 00:40:51 +0000

    - Update to 0.69; - fix an incompatibility, which causes daemon to eat 100% CPU even when completely idle (ages old problem with select()'ing on FIFO without writers opened for reading in non-blocking mode); - replace incomplete and ugly daemonize code with native daemon(3).
    Sponsored by:	United Networks of Ukraine

--- net/l2tpd/files/patch-l2tp.h
+++ net/l2tpd/files/patch-l2tp.h
@@ -1,11 +1,8 @@
-
-$FreeBSD$
-
---- l2tp.h.orig	Tue Jan 15 18:59:27 2002
-+++ l2tp.h	Tue Oct 22 12:42:08 2002
-@@ -20,7 +20,10 @@
+--- l2tp.h.orig	Mon Aug 19 20:26:23 2002
++++ l2tp.h	Tue Nov 12 13:00:09 2002
+@@ -21,7 +21,10 @@
  
- #define MAXSTRLEN 120		/* Maximum length of common strings */
+ #define MAXSTRLEN 120           /* Maximum length of common strings */
  
 +#include <sys/types.h>
 +#include <sys/socket.h>
@@ -14,3 +11,11 @@ $FreeBSD$
  #include <termios.h>
  #include "osport.h"
  #include "scheduler.h"
+@@ -211,6 +214,7 @@
+ extern int ppd;
+ extern int switch_io;           /* jz */
+ extern int control_fd;
++extern int control_fdw;
+ extern int start_pppd (struct call *c, struct ppp_opts *);
+ extern void magic_lac_dial (void *);
+ extern int get_entropy (char *, int);
--- net/l2tpd/files/patch-l2tpd.c
+++ net/l2tpd/files/patch-l2tpd.c
@@ -1,9 +1,9 @@
 
 $FreeBSD$
 
---- l2tpd.c.orig	Fri Jan 11 17:54:44 2002
-+++ l2tpd.c	Tue Oct 22 12:43:45 2002
-@@ -33,8 +33,6 @@
+--- l2tpd.c.orig	Mon Aug 19 17:12:17 2002
++++ l2tpd.c	Sat Nov 23 02:02:04 2002
+@@ -34,8 +34,6 @@
  #include <netdb.h>
  #include <string.h>
  #include <fcntl.h>
@@ -12,3 +12,58 @@ $FreeBSD$
  #ifdef USE_KERNEL
  #include <sys/ioctl.h>
  #endif
+@@ -46,6 +44,7 @@
+ struct utsname uts;
+ int ppd = 1;                    /* Packet processing delay */
+ int control_fd;                 /* descriptor of control area */
++int control_fdw;                 /* descriptor of control area */
+ char *args;
+ 
+ char *dial_no_tmp;              /* jz: Dialnumber for Outgoing Call */
+@@ -384,6 +383,7 @@
+ 
+         /* close the control pipe fd */
+         close (control_fd);
++	close (control_fdw);
+ 
+         execv (PPPD, stropt);
+         log (LOG_WARN, "%s: Exec of %s failed!\n", __FUNCTION__, PPPD);
+@@ -920,7 +920,9 @@
+     }
+     /* Otherwise select goes nuts */
+     close (control_fd);
++    close (control_fdw);
+     control_fd = open (CONTROL_PIPE, O_RDONLY | O_NONBLOCK, 0600);
++    control_fdw = open (CONTROL_PIPE, O_WRONLY | O_NONBLOCK, 0600);
+ }
+ 
+ void usage(void) {
+@@ -985,6 +987,7 @@
+     char buf[STRLEN];
+     int pidfilewritten=0;
+ 
++#ifndef __FreeBSD__
+     if((pid = fork()) < 0) {
+         log(LOG_LOG, "%s: Unable to fork ()\n",__FUNCTION__);
+         close(server_socket);
+@@ -998,6 +1001,12 @@
+                          to do a proper fix for this */
+     close(1);
+     close(2);
++#else
++    if (daemon(0, 0) != 0) {
++	log(LOG_LOG, "%s: daemon () failed\n", __FUNCTION__);
++	exit(1);
++    }
++#endif
+ 
+     /* Read previous pid file. */
+     if ((i = open(gconfig.pidfile,O_RDONLY)) > 0) {
+@@ -1075,6 +1084,7 @@
+     init_scheduler ();
+     mkfifo (CONTROL_PIPE, 0600);
+     control_fd = open (CONTROL_PIPE, O_RDONLY | O_NONBLOCK, 0600);
++    control_fdw = open (CONTROL_PIPE, O_WRONLY | O_NONBLOCK, 0600);
+     if (control_fd < 0)
+     {
+         log (LOG_CRIT, "%s: Unable to open " CONTROL_PIPE " for reading.",
