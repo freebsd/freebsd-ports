cd895d43859a50bd8450c7f276b04e298e03cbd9
Norikatsu Shigemura <nork@FreeBSD.org>
2002-11-23 15:29:50 +0000

    Update to 1.8.2.
    PR:		ports/45630
    Submitted by:	Ports Fury

--- www/harvest/files/patch-src::gatherer::essence::file::print.c
+++ www/harvest/files/patch-src::gatherer::essence::file::print.c
@@ -1,12 +1,15 @@
---- src/gatherer/essence/file/print.c.orig	Sat Dec 13 01:02:15 1997
-+++ src/gatherer/essence/file/print.c	Wed Nov 13 08:56:34 2002
-@@ -78,8 +78,8 @@
+--- src/gatherer/essence/file/print.c.orig	Sat Nov 16 08:09:53 2002
++++ src/gatherer/essence/file/print.c	Sat Nov 23 04:28:48 2002
+@@ -78,10 +78,12 @@
  warning(f, a)
       char *f, *a;
  {
--	extern int errno, sys_nerr;
++#if !defined(__FreeBSD__)
+ 	extern int errno, sys_nerr;
+ #ifndef __CYGWIN__
  #ifdef NEED_SYS_ERRLIST
-+	extern int errno, sys_nerr;
  	extern char *sys_errlist[];
++#endif
+ #endif
  #endif
  	int myerrno;
