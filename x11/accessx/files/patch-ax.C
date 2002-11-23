ed60b911cdb17b8da68a7d46caf48d26be498122
edwin <edwin@FreeBSD.org>
2002-11-23 00:45:46 +0000

    Maintainer update to build on -current again
    PR:		ports/45378
    Submitted by:	Stephen Montgomery-Smith <stephen@math.missouri.edu>

---
+++ x11/accessx/files/patch-ax.C
@@ -0,0 +1,11 @@
+--- ax.C-orig	Tue Mar 13 23:42:10 2001
++++ ax.C	Sun Nov 17 19:26:06 2002
+@@ -713,7 +713,7 @@
+ ///////////////////////////////////////////////////////////////////////////////
+ 
+ 
+-void main(int argc, char** argv)
++int main(int argc, char** argv)
+ {
+   Access ax;
+   ax.QueryServer();
