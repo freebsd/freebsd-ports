fd40a363d3c875da91e6884ca31f8a52647c69e7
arved <arved@FreeBSD.org>
2002-11-23 13:18:17 +0000

    Fix Build on -CURRENT.
    Approved by:	maintainer

---
+++ net-im/sim-im/files/patch-service.cpp
@@ -0,0 +1,19 @@
+--- sim/libicq/service.cpp.orig	Thu Nov 21 21:52:08 2002
++++ sim/libicq/service.cpp	Thu Nov 21 21:53:46 2002
+@@ -301,14 +301,14 @@
+     time(&now);
+     Buffer b;
+     b << (char)3;
+-    b.pack(now);
++    b.pack((unsigned long)now);
+     b.pack((unsigned short)0);
+     b.pack((unsigned short)1);
+     b.pack((unsigned short)1);
+     b.pack((char*)SHARED_FILES_SIGN, 16);
+     b << (char)4 << (unsigned short)1;
+     b.pack((unsigned long)(bState ? 1 : 0));
+-    b.pack(now);
++    b.pack((unsigned long)now);
+     b.pack((unsigned long)0);
+     b.pack((unsigned short)1);
+     sendUpdate(b);
