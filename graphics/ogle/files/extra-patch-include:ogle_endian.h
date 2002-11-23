057d8fc994fe22451e7705144a9a73589aecdb6e
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2002-11-23 21:34:36 +0000

    Fix a logic error: add <sys/param.h> in patch-include:ogle_endian.h instead of extra-patch-include:ogle_endian.h since the former is optional. Therefore, all checks inside patch-include:ogle_endian.h will work regardless of application of extra-patch-include:ogle_endian.h

--- graphics/ogle/files/extra-patch-include:ogle_endian.h
+++ graphics/ogle/files/extra-patch-include:ogle_endian.h
@@ -1,16 +1,6 @@
 --- include/ogle_endian.h.orig	Thu Oct  4 00:41:49 2001
 +++ include/ogle_endian.h	Sun Nov 10 17:12:23 2002
-@@ -29,6 +29,9 @@
- #  define FROM_BE_32(x) (x)
- #else
- 
-+#if (defined(__unix__) || defined(unix)) && !defined(USG)
-+#include <sys/param.h>
-+#endif
- 
- #if defined(HAVE_BYTESWAP_H)
- #  include <byteswap.h>
-@@ -39,6 +42,13 @@
+@@ -39,6 +39,13 @@
  #elif defined(HAVE_SYS_ENDIAN_H)
  #  include <sys/endian.h>
  #  define FROM_BE_32(x) (swap32(x))
--- multimedia/ogle/files/patch-include:ogle_endian.h
+++ multimedia/ogle/files/patch-include:ogle_endian.h
@@ -1,8 +1,13 @@
 --- include/ogle_endian.h.orig	Sun Nov 10 17:13:10 2002
 +++ include/ogle_endian.h	Sun Nov 10 17:13:29 2002
-@@ -36,6 +36,9 @@
- #elif defined(HAVE_SYS_BSWAP_H)
- #  include <sys/bswap.h>
+@@ -27,2 +27,6 @@
+ 
++#if (defined(__unix__) || defined(unix)) && !defined(USG)
++#include <sys/param.h>
++#endif
++
+ #ifdef WORDS_BIGENDIAN
+@@ -38,5 +42,8 @@
  #  define FROM_BE_32(x) (bswap32(x))
 -#elif defined(HAVE_SYS_ENDIAN_H)
 +#elif defined(HAVE_SYS_ENDIAN_H) && !defined(__FreeBSD__)
@@ -11,3 +16,4 @@
 +#elif defined(HAVE_SYS_ENDIAN_H) && defined(__FreeBSD__) && __FreeBSD_version >= 470000
 +#  include <sys/endian.h>
 +#  define FROM_BE_32(x) (be32toh(x))
+ #else
