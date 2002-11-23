fe3466912a25ed1a59598eb9f20680d2eb65f9dd
cjh <cjh@FreeBSD.org>
2002-11-23 11:40:45 +0000

    Fix build on -current. - patch-bb: borrowed from editors/emacs (same fix) - src/hangul.c: macro names must be identifiers

---
+++ korean/hanemacs/files/patch-src:hangul.c
@@ -0,0 +1,11 @@
+--- src/hangul.c.orig	Sat Nov 23 20:33:21 2002
++++ src/hangul.c	Sat Nov 23 20:33:46 2002
+@@ -28,7 +28,7 @@
+ 
+ #include "hangul.h"
+ 
+-#ifdef 0
++#if 0
+ #include "stdio.h"
+ #include "unistd.h"
+ #include "fcntl.h"
