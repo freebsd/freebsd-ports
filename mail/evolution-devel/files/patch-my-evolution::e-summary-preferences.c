ff8f9f8c538307d138333fe68bba043de4dc84e7
Joe Marcus Clarke <marcus@FreeBSD.org>
2002-11-24 06:23:52 +0000

    Add the FreeBSD GNOME Project News RDF link to the My Evolution news feeds list.  Bump PORTREVISION.

--- mail/evolution-devel/files/patch-my-evolution::e-summary-preferences.c
+++ mail/evolution-devel/files/patch-my-evolution::e-summary-preferences.c
@@ -1,13 +1,14 @@
---- my-evolution/e-summary-preferences.c.orig	Thu Sep 26 15:23:36 2002
-+++ my-evolution/e-summary-preferences.c	Tue Oct 15 17:22:04 2002
-@@ -581,8 +581,10 @@
+--- my-evolution/e-summary-preferences.c.orig	Thu Oct 24 18:14:17 2002
++++ my-evolution/e-summary-preferences.c	Sun Nov 24 01:23:59 2002
+@@ -581,8 +581,11 @@
  	{"http://barrapunto.com/gnome.rdf", "Barrapunto GNOME", FALSE,},
  	{"http://www.bsdtoday.com/backend/bt.rdf", "BSD Today", FALSE},
  	{"http://beyond2000.com/b2k.rdf", "Beyond 2000", FALSE},
-+	{"http://daily.daemonnews.org/ddn.rdf.php3", "Daily Daemon News"},
++	{"http://daily.daemonnews.org/ddn.rdf.php3", "Daily Daemon News", FALSE},
  	{"http://www.dictionary.com/wordoftheday/wotd.rss", N_("Dictionary.com Word of the Day"), FALSE},
  	{"http://www.dvdreview.com/rss/newschannel.rss", "DVD Review", FALSE},
-+	{"http://www.freebsd.org/news/news.rdf", "FreeBSD Newsflash"},
++	{"http://www.freebsd.org/gnome/news.rdf", "FreeBSD GNOME Project News", FALSE},
++	{"http://www.freebsd.org/news/news.rdf", "FreeBSD Newsflash", FALSE},
  	{"http://freshmeat.net/backend/fm.rdf", "Freshmeat", FALSE},
- 	{"http://news.gnome.org/gnome-news/rdf", "GNotices", FALSE},
+ 	{"http://www.gnomedesktop.org/backend.php", "Footnotes - GNOME News", FALSE},
  	{"http://headlines.internet.com/internetnews/prod-news/news.rss", "Internet.com", FALSE},
