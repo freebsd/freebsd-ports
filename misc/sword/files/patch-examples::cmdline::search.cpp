a795cfbd3cf6798e4bccc0ec8fa15503562dce39
edwin <edwin@FreeBSD.org>
2002-11-24 11:01:08 +0000

    maintainer update for misc/sword to version 1.5.5
    PR:		ports/45406
    Submitted by:	Willem van Engen <wvengen@stack.nl>

--- misc/sword/files/patch-examples::cmdline::search.cpp
+++
@@ -1,8 +0,0 @@
-diff -ruN sword-1.5.3.orig/examples/cmdline/search.cpp sword-1.5.3/examples/cmdline/search.cpp
---- examples/cmdline/search.cpp	Mon Nov  6 13:18:43 2000
-+++ examples/cmdline/search.cpp	Wed Jun 19 14:11:08 2002
-@@ -1,3 +1,4 @@
-+#include <iostream.h>
- #include <stdio.h>
- #include <rawtext.h>
- #include <swmgr.h>
--- misc/sword15/files/patch-examples::cmdline::threaded_search.cpp
+++
@@ -1,8 +0,0 @@
-diff -ruN sword-1.5.3.orig/examples/cmdline/threaded_search.cpp sword-1.5.3/examples/cmdline/threaded_search.cpp
---- examples/cmdline/threaded_search.cpp	Thu Mar 15 19:24:39 2001
-+++ examples/cmdline/threaded_search.cpp	Wed Jun 19 14:12:14 2002
-@@ -1,3 +1,4 @@
-+#include <iostream.h>
- #include <stdio.h>
- #include <rawtext.h>
- #include <swmgr.h>
