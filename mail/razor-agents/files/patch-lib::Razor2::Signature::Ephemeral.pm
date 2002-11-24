094bdd5dc14fda68277a58df2960cbbe07b4bab0
Yen-Ming Lee <leeym@FreeBSD.org>
2002-11-24 06:18:46 +0000

    Remove output messages of debug() in Razor2::Signature::Ephemeral which will add extra junk on the front of all emails and break Mail::SpamAssassin.
    Noticed by:	Roddie Hasan <roddie@krweb.net>
    		Mark Jones <mlist@hackerjones.org>

---
+++ mail/razor-agents/files/patch-lib::Razor2::Signature::Ephemeral.pm
@@ -0,0 +1,11 @@
+--- lib/Razor2/Signature/Ephemeral.pm.orig	Sun Nov 24 14:03:24 2002
++++ lib/Razor2/Signature/Ephemeral.pm	Sun Nov 24 14:03:29 2002
+@@ -166,7 +166,7 @@
+ 
+ sub debug { 
+     my $message = shift;
+-    print "$message\n";
++#    print "$message\n";
+ }
+ 
+ 
