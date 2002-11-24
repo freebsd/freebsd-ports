830f862b30baacbfedd3ea1fe3764582268efc1e
edwin <edwin@FreeBSD.org>
2002-11-24 10:11:54 +0000

    Make emulators/bsvc work on -current again
    PR:		ports/44852
    Submitted by:	Volker Stolz <vs@foldr.org>

---
+++ emulators/bsvc/files/patch-src-Framework-Tools.hxx
@@ -0,0 +1,10 @@
+--- Framework/Tools.hxx.orig	Sun Nov  3 12:12:32 2002
++++ Framework/Tools.hxx	Sun Nov  3 12:13:28 2002
+@@ -18,6 +18,7 @@
+ #define TOOLS_HXX
+ 
+ #include <string>
++#include <ctype.h>
+ 
+ #ifdef USE_STD
+   using namespace std;
