b925b0e3c83f65d46ae6e566c8bfa6e87df268b1
obrien <obrien@FreeBSD.org>
2002-11-25 18:57:54 +0000

    Rather than try to duplicate the declarations from stdio.h, just use them!

--- lang/egcs/files/patch-strerror.c
+++ lang/egcs/files/patch-strerror.c
@@ -1,11 +1,7 @@
---- libiberty/strerror.c.orig	Sun Nov  3 21:37:47 2002
-+++ libiberty/strerror.c	Sun Nov  3 21:38:08 2002
-@@ -462,7 +462,7 @@
- 
- #else
- 
+--- libiberty/strerror.c.orig	Fri May 15 16:42:41 1998
++++ libiberty/strerror.c	Sun Nov 24 14:25:18 2002
+@@ -465,2 +465,2 @@
 -extern int sys_nerr;
-+extern __const int sys_nerr;
- extern char *sys_errlist[];
- 
- #endif
+-extern char *sys_errlist[];
++/* extern int sys_nerr;		declared just fine by stdio.h, thank you very much */
++/* extern char *sys_errlist[];	declared just fine by stdio.h, thank you very much */
