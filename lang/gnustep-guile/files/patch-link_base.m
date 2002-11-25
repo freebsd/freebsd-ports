3b02ed127d2cf67ada358b7ee9a13203f3d925be
dinoex <dinoex@FreeBSD.org>
2002-11-25 05:17:45 +0000

    fix Bento build problmem, itroduced by gnustep-base-1.5.1

---
+++ lang/gnustep-guile/files/patch-link_base.m
@@ -0,0 +1,20 @@
+--- Library/link_base.m.orig	Tue Jul 31 05:02:34 2001
++++ Library/link_base.m	Mon Nov 25 06:16:58 2002
+@@ -77,7 +77,7 @@
+     CSTR(NSDecimalSeparator);
+     CSTR(NSDefaultRunLoopMode);
+     CSTR(NSFileDeviceIdentifier);
+-    CSTR(NSFileGroupOwnerAccountNumber);
++    CSTR(NSFileGroupOwnerAccountID);
+     CSTR(NSFileHandleConnectionAcceptedNotification);
+     CSTR(NSFileHandleNotificationDataItem);
+     CSTR(NSFileHandleNotificationFileHandleItem);
+@@ -85,7 +85,7 @@
+     CSTR(NSFileHandleReadCompletionNotification);
+     CSTR(NSFileHandleReadToEndOfFileCompletionNotification);
+     CSTR(NSFileModificationDate);
+-    CSTR(NSFileOwnerAccountNumber);
++    CSTR(NSFileOwnerAccountID);
+     CSTR(NSFilePosixPermissions);
+     CSTR(NSFileReferenceCount);
+     CSTR(NSFileSize);
