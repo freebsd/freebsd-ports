60b8a222574e98885333bb4a75c8f8ff57168b05
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2002-11-26 21:30:16 +0000

    Correct previous commit

--- graphics/fxtv/files/patch-tvcapture.c
+++ graphics/fxtv/files/patch-tvcapture.c
@@ -1,5 +1,5 @@
---- work/fxtv-1.03/tvcapture.c.orig	Tue Nov 26 19:30:12 2002
-+++ work/fxtv-1.03/tvcapture.c	Tue Nov 26 19:30:27 2002
+--- tvcapture.c.orig	Tue Nov 26 19:30:12 2002
++++ tvcapture.c	Tue Nov 26 19:30:27 2002
 @@ -34,6 +34,7 @@
  #include <stdlib.h>
  #include <fcntl.h>
