eb9948b3311ba6cd91abae703c7dd9f76a958b16
pat <pat@FreeBSD.org>
2002-11-26 23:45:08 +0000

    Fix build on -current and PERL->REINPLACE_CMD
    Submitted by:	bento

---
+++ astro/wmsolar/files/patch-Vsop.h
@@ -0,0 +1,10 @@
+--- Vsop.h.orig	Tue Nov 26 15:41:59 2002
++++ Vsop.h	Tue Nov 26 15:42:13 2002
+@@ -12,6 +12,7 @@
+ #define VSOP__H
+ 
+ #include "PlanetData.h"  // for enum Planet
++#include <math.h>
+ 
+ // * * * * * simple support structs * * * * *
+ 
