958d33e1deb4db5521e64b90c4726ba09beee623
cjh <cjh@FreeBSD.org>
2002-11-27 09:48:39 +0000

    Update to 4.8. Back from hell!
    Approved by:	portmgr

---
+++ korean/netscape48-communicator/files/preferences.js
@@ -0,0 +1,5 @@
+// Netscape User Preferences
+// This is a generated file!  Do not edit.
+
+user_pref("intl.character_set", 1292);
+user_pref("intl.font_spec_list", "misc-gothic-120-noscale-fixed-x-ksc5601-11,misc-gothic-120-noscale-prop-x-ksc5601-11,schumacher-clean-120-noscale-fixed-x-user-defined,schumacher-clean-120-noscale-prop-x-user-defined,nsPseudoFont-courier-120-noscale-fixed-UTF-8,nsPseudoFont-helvetica-120-noscale-prop-UTF-8,isas-fangsong ti-120-noscale-fixed-gb_2312-80,isas-fangsong ti-120-noscale-prop-gb_2312-80,hanyang-kodig-140-noscale-fixed-ks_c_5601-1987,hanyang-kodig-140-noscale-prop-ks_c_5601-1987,misc-fixed-130-noscale-fixed-jis_x0201,misc-fixed-130-noscale-prop-jis_x0201,netscape-fixed-130-noscale-fixed-jis_x0208-1983,netscape-fixed-130-noscale-prop-jis_x0208-1983,sp-courier new-120-noscale-fixed-iso-8859-2,sp-arial-120-noscale-prop-iso-8859-2,adobe-courier-100-noscale-fixed-iso-8859-1,adobe-helvetica-120-noscale-prop-iso-8859-1,");
