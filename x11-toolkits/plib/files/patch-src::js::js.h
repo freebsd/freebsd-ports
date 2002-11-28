88ac656b2a8a2ddf40663e78503727d410a8d405
knu <knu@FreeBSD.org>
2002-11-28 12:42:56 +0000

    Replace <machine/joystick.h> with <sys/joystick.h> to fix the build.

--- x11-toolkits/plib/files/patch-src::js::js.h
+++ x11-toolkits/plib/files/patch-src::js::js.h
@@ -1,5 +1,14 @@
 --- src/js/js.h.orig	Mon Sep  2 15:05:43 2002
-+++ src/js/js.h	Wed Sep  4 04:44:26 2002
++++ src/js/js.h	Thu Nov 28 21:43:15 2002
+@@ -63,7 +63,7 @@
+ #  include <fcntl.h>
+ 
+ #  if defined(__FreeBSD__) || defined(__NetBSD__)
+-#    include <machine/joystick.h>
++#    include <sys/joystick.h>
+ #    define JS_DATA_TYPE joystick
+ #    define JS_RETURN (sizeof(struct JS_DATA_TYPE))
+ #  elif defined(__linux__)
 @@ -132,9 +132,9 @@
  
  
