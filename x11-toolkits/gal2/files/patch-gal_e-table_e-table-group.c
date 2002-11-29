c35d4f09f3bf447d710a357dfbee5fc41581e436
marcus <marcus@FreeBSD.org>
2002-11-29 03:31:53 +0000

    Fix build on Alpha.
    Submitted by:	beta

---
+++ x11-toolkits/gal2/files/patch-gal_e-table_e-table-group.c
@@ -0,0 +1,26 @@
+--- e-table-group.c.orig	Thu Nov 28 13:51:22 2002
++++ e-table-group.c	Thu Nov 28 22:23:01 2002
+@@ -22,6 +22,7 @@
+  */
+ 
+ #include <config.h>
++#include <stdarg.h>
+ #include <gtk/gtksignal.h>
+ #include <libgnomecanvas/gnome-canvas-rect-ellipse.h>
+ #include "e-table-group.h"
+@@ -129,13 +130,14 @@
+ 			 ETableHeader     *header,
+ 			 ETableModel      *model)
+ {
++	va_list args;
+ 	etg->full_header = full_header;
+ 	gtk_object_ref (GTK_OBJECT(etg->full_header));
+ 	etg->header = header;
+ 	gtk_object_ref (GTK_OBJECT(etg->header));
+ 	etg->model = model;
+ 	gtk_object_ref (GTK_OBJECT(etg->model));
+-	gnome_canvas_item_construct (GNOME_CANVAS_ITEM (etg), parent, 0, NULL);
++	gnome_canvas_item_construct (GNOME_CANVAS_ITEM (etg), parent, 0, args);
+ }
+ 
+ /**
