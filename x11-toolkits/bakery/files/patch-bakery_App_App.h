17abd40f965e7aebce90b822b9f1801545814e55
marcus <marcus@FreeBSD.org>
2002-12-02 01:28:20 +0000

    Add a forgotten patch that makes the whole thing work on -CURRENT.  Thanks to kris for making me revisit this.

---
+++ x11-toolkits/bakery/files/patch-bakery_App_App.h
@@ -0,0 +1,11 @@
+--- bakery/App/App.h.orig	Sun Dec  1 20:26:16 2002
++++ bakery/App/App.h	Sun Dec  1 20:26:11 2002
+@@ -19,6 +19,8 @@
+ #ifndef BAKERY_APP_H
+ #define BAKERY_APP_H
+ 
++using namespace std;
++
+ #include <gnome--.h>
+ #include "libgnome/gnome-util.h" //For ::gnome_pixmap_file().
+ #include <gtk--.h>
