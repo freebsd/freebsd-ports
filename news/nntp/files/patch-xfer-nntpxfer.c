07c41ede2be3d4b94531816e3ca1809a5a12ba54
Dirk Meyer <dinoex@FreeBSD.org>
2002-12-05 18:13:49 +0000

    - Fix build on CURRENT - make fully PREFIX aware

--- news/nntp/files/patch-xfer-nntpxfer.c
+++ news/nntp/files/patch-xfer-nntpxfer.c
@@ -1,5 +1,5 @@
---- xfer/nntpxfer.c.orig	Mon Nov  4 05:18:32 2002
-+++ xfer/nntpxfer.c	Mon Nov  4 05:25:19 2002
+--- xfer/nntpxfer.c.orig	Thu Dec  5 10:04:30 2002
++++ xfer/nntpxfer.c	Thu Dec  5 10:07:22 2002
 @@ -60,6 +60,9 @@
  #include <sys/time.h>
  #endif
@@ -22,3 +22,14 @@
  char *artlist[MAX_ARTICLES];
  int server;			/* stream socket to the nntp server */
  FILE * rd_fp, * wr_fp;
+@@ -649,8 +656,10 @@
+ errmsg(code)
+ int code;
+ {
++#ifndef __FreeBSD__
+ 	extern int sys_nerr;
+ 	extern char *sys_errlist[];
++#endif
+ 	static char ebuf[6+5+1];
+ 
+ 	if (code > sys_nerr || code < 0) {
