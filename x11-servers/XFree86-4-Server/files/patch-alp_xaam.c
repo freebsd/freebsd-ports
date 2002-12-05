af21c9400b5e2d044fa59235ae08d976d585bab9
Eric Anholt <anholt@FreeBSD.org>
2002-12-05 01:25:32 +0000

    [1] Quiet some prolific debug output in cirrus driver. [2] Stop SIGILLs when running GL clients on systems with SSE but no     CPU_ENABLE_SSE in the kernel. [3] Fix imake complaints after XFree86-4-libraries patch to stop libXThrStub     build on post-500043 -current.
    PR:		[1] 38590
    Approved by:	portmgr

---
+++ x11-servers/XFree86-4-Server/files/patch-alp_xaam.c
@@ -0,0 +1,9 @@
+--- programs/Xserver/hw/xfree86/drivers/cirrus/alp_xaam.c.orig	Thu Oct 17 19:22:24 2002
++++ programs/Xserver/hw/xfree86/drivers/cirrus/alp_xaam.c	Thu Oct 17 19:22:31 2002
+@@ -1,5 +1,5 @@
+ /* (c) Itai Nahshon */
+-#define DEBUG
++
+ /* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/cirrus/alp_xaam.c,v 1.7 2001/10/01 13:44:05 eich Exp $ */
+ 
+ #include "xf86.h"
