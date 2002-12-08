408830a1f16d3286276a98614e2fbec95a41f517
Josef Karthauser <joe@FreeBSD.org>
2002-12-08 23:43:52 +0000

    Use the new version of libusb.
    In addition, Kenneth Merry sent in a patch to s10sh which allows
    him to operate his camera. The owner of the code doesn't seem to
    respond to e-mail, so this functionality has been rolled in with
    the local patch that already existed.
    
    PR:	ports/46060

--- graphics/s10sh/files/patch-usb.c
+++ graphics/s10sh/files/patch-usb.c
@@ -1,5 +1,5 @@
---- usb.c.orig	Tue Mar 13 14:46:18 2001
-+++ usb.c	Sun Apr 21 00:59:11 2002
+--- usb.c.orig	Tue Mar 13 06:46:18 2001
++++ usb.c	Sat Dec  7 06:16:19 2002
 @@ -84,6 +84,12 @@
  						printf("Canon S20 found\n");
  					return USB_INIT_S20;
@@ -13,3 +13,24 @@
  				case PRODUCT_ID_S100_EU:
  				case PRODUCT_ID_S100_US:
  					*camera_dev = dev;
+@@ -97,6 +103,12 @@
+ 						printf("Canon G1 found\n");
+ 					return USB_INIT_G1;
+ 					break;
++				case PRODUCT_ID_G3:
++					*camera_dev = dev;
++					if (opt_debug)
++						printf("Canon G3 found\n");
++					return USB_INIT_G3;
++					break;
+ 				case PRODUCT_ID_NEXTDIGICAM1:
+ 				case PRODUCT_ID_NEXTDIGICAM2:
+ 				case PRODUCT_ID_NEXTDIGICAM3:
+@@ -118,6 +130,7 @@
+ 						dev->descriptor.idProduct);
+ 					break;
+ 				}
++				break;
+ 			default:
+ 				if (opt_debug)
+ 					printf("Unknown vendor ID: %04X\n",
