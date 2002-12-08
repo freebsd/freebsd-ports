f30f6fc628e3b3152840509f01daeef9ed1b4475
joe <joe@FreeBSD.org>
2002-12-08 23:43:52 +0000

    Use the new version of libusb.
    In addition, Kenneth Merry sent in a patch to s10sh which allows
    him to operate his camera. The owner of the code doesn't seem to
    respond to e-mail, so this functionality has been rolled in with
    the local patch that already existed.
    
    PR:	ports/46060

--- graphics/s10sh/files/patch-usb.h
+++ graphics/s10sh/files/patch-usb.h
@@ -1,10 +1,12 @@
---- usb.h.orig	Tue Mar 13 14:46:18 2001
-+++ usb.h	Sun Apr 21 00:59:11 2002
-@@ -19,12 +19,14 @@
+--- usb.h.orig	Tue Mar 13 06:46:18 2001
++++ usb.h	Sat Dec  7 06:16:19 2002
+@@ -19,15 +19,20 @@
  #define USB_INIT_S20            2       /* S20 found */
  #define USB_INIT_S100		3	/* S100 (Digital Ixus) found */
  #define USB_INIT_G1		4	/* G1 found */
 +#define USB_INIT_A20		5	/* A20 found */
++#define USB_INIT_G2		6	/* G2 found */
++#define USB_INIT_G3		7	/* G3 found */
  #define USB_INIT_NEW		100	/* Unsupported PowerShot found! */
  #define USB_INIT_FAILED         -1      /* Unable to initialize USB */
  
@@ -15,3 +17,7 @@
  #define PRODUCT_ID_S100_US	0x3045	/* S100, aka. Digital Ixus, Elph */
  #define PRODUCT_ID_S100_EU	0x3047	/* S100, aka. Digital Ixus, Elph */
  #define PRODUCT_ID_G1		0x3048	/* PowerShot G1 */
++#define PRODUCT_ID_G3		0x306E
+ 
+ /* The Canon USB protocol of the S10, S20, S100, G1 is the same.
+  * We can hope that the next cameras will adopt a compatible protocol
