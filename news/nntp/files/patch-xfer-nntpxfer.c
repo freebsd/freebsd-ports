c459689c126d6c187783214bd1a2a73a0254b801
Dirk Meyer <dinoex@FreeBSD.org>
2002-12-09 21:10:30 +0000

    Fix a build problem with ANSI_SOURCE and CFLAGS

--- news/nntp/files/patch-xfer-nntpxfer.c
+++ news/nntp/files/patch-xfer-nntpxfer.c
@@ -1,16 +1,6 @@
---- xfer/nntpxfer.c.orig	Thu Dec  5 10:04:30 2002
-+++ xfer/nntpxfer.c	Thu Dec  5 10:07:22 2002
-@@ -60,6 +60,9 @@
- #include <sys/time.h>
- #endif
- 
-+#ifdef BSD_44
-+#define _ANSI_SOURCE
-+#endif
- #include <stdio.h>
- #include <errno.h>
- #include <ctype.h>
-@@ -107,7 +110,11 @@
+--- xfer/nntpxfer.c.orig	Fri Dec  6 07:51:46 2002
++++ xfer/nntpxfer.c	Fri Dec  6 07:58:39 2002
+@@ -107,7 +107,11 @@
  char	*rindex();
  u_long	inet_addr();
  
@@ -22,7 +12,7 @@
  char *artlist[MAX_ARTICLES];
  int server;			/* stream socket to the nntp server */
  FILE * rd_fp, * wr_fp;
-@@ -649,8 +656,10 @@
+@@ -649,8 +653,10 @@
  errmsg(code)
  int code;
  {
