dd95692b8f075205f840e44677d75f31bfcb795f
edwin <edwin@FreeBSD.org>
2002-12-11 12:37:28 +0000

    Make audio/midimountain compiled again with the new GCC
    PR:		ports/46169
    Submitted by:	Kuang-che Wu <kcwu@kcwu.dyndns.org>

---
+++ audio/midimountain/files/patch-midimountain::mcEvent.h
@@ -0,0 +1,11 @@
+--- midimountain/mcEvent.h	Tue May  1 10:41:05 2001
++++ midimountain/mcEvent.h	Wed Dec 11 03:33:41 2002
+@@ -25,6 +25,8 @@
+ 
+ #include <mcTrack.h>
+ #include <mcList.h>
++#include <iostream>
++using namespace std;
+ 
+ const short cWFOK = 0;
+ const short cWFPlaying = 1;
