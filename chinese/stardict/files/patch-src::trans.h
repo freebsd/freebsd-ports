9619e568ecf0b019d89657a0ceb8b03640dec53f
dinoex <dinoex@FreeBSD.org>
2002-12-14 20:26:19 +0000

    Fix build on -current PR:		46259 Submitted by:	kcwu@kcwu.dyndns.org

---
+++ chinese/stardict/files/patch-src::trans.h
@@ -0,0 +1,12 @@
+--- src/trans.h.orig	Sun Dec 15 00:37:55 2002
++++ src/trans.h	Sun Dec 15 00:38:48 2002
+@@ -44,7 +44,7 @@
+ #include <Xm/PushB.h>
+ #include <Xm/DrawnB.h>
+ #include <Xm/ArrowB.h>
+-#include <strstream.h>
+-#include <fstream.h>
++#include <sstream>
++#include <fstream>
+ 
+ #include "regdef.h"
