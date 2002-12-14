f0f72ef4e18fd56bf8dba08146481d140e0e26e6
Patrick Li <pat@FreeBSD.org>
2002-12-14 22:28:16 +0000

    Fix build
    Submitted by:	bento

---
+++ x11-fonts/xfed/files/patch-defs.h
@@ -0,0 +1,9 @@
+--- defs.h.orig	Sat Dec 14 15:23:10 2002
++++ defs.h	Sat Dec 14 15:24:30 2002
+@@ -1,6 +1,4 @@
+ extern int errno;
+-extern char *malloc();
+-extern char *realloc();
+ 
+ struct box {
+ 	int w, h;
