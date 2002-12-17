74c406094da97a70b1553e61716f0f65d15961ce
dougb <dougb@FreeBSD.org>
2002-12-17 22:33:31 +0000

    Upgrade to 4.51, mostly a bugfix release, but containing a few new features, Disable-Sender, Scramble-Message-Id, Spell-Check-Before-Sending.
    Pine users are strongly encouraged to upgrade.
    
    Approved by:	portmgr (knu)

--- mail/pine4/files/patch-reply.c
+++ mail/pine4/files/patch-reply.c
@@ -1,6 +1,6 @@
---- pine/reply.c.orig	Tue Aug  7 15:52:03 2001
-+++ pine/reply.c	Fri Sep 28 00:29:19 2001
-@@ -4997,23 +4997,24 @@
+--- pine/reply.c.orig	Wed Dec  4 21:10:12 2002
++++ pine/reply.c	Mon Dec 16 03:40:23 2002
+@@ -5092,7 +5092,10 @@
  char *
  generate_message_id()
  {
@@ -12,26 +12,33 @@
      char        *id;
      time_t       now;
      struct tm   *now_x;
+@@ -5100,14 +5103,11 @@
  
      now   = time((time_t *)0);
      now_x = localtime(&now);
 -    id    = (char *)fs_get(128 * sizeof(char));
 +    id    = (char *)fs_get(384 * sizeof(char));
  
--    if(now_x->tm_sec == osec){
--	cnt++;
--    }else{
+-    if(now_x->tm_sec == osec)
+-      cnt++;
+-    else{
 -	cnt = 0;
 -	osec = now_x->tm_sec;
 -    }
--    sprintf(id,"<Pine.%.4s.%.20s.%02d%02d%02d%02d%02d%02d%X.%d@%.50s>",
--	    SYSTYPE, pine_version, (now_x->tm_year) % 100, now_x->tm_mon + 1,
 +    cnt = 'A';
 +    if(gettimeofday(&tp, &tzp) == 0)
-+	    cnt += (time_t)tp.tv_usec % ('Z' - 'A');
-+
++	cnt += (time_t)tp.tv_usec % ('Z' - 'A');
+ 
+     hostpart = F_ON(F_ROT13_MESSAGE_ID, ps_global)
+ 		 ? rot13(ps_global->hostname)
+@@ -5116,8 +5116,8 @@
+     if(!hostpart)
+       hostpart = cpystr("huh");
+ 
+-    sprintf(id,"<Pine.%.4s.%.20s.%02d%02d%02d%02d%02d%02d%X.%d@%.50s>",
+-	    SYSTYPE, pine_version, (now_x->tm_year) % 100, now_x->tm_mon + 1,
 +    sprintf(id,"<%d%02d%02d%02d%02d%02d.%c%d@%s>",
 +	    (now_x->tm_year) + 1900, now_x->tm_mon + 1,
  	    now_x->tm_mday, now_x->tm_hour, now_x->tm_min, now_x->tm_sec, 
- 	    cnt, getpid(), ps_global->hostname);
+ 	    cnt, getpid(), hostpart);
  
