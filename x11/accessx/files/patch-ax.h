fb16006cf634b865efeb53743fb2910a89a340e7
Edwin Groothuis <edwin@FreeBSD.org>
2002-12-17 04:39:46 +0000

    Make x11/accessx compile under -current again. Noticed on: bento

---
+++ x11/accessx/files/patch-ax.h
@@ -0,0 +1,11 @@
+--- ax.h.orig	Mon Dec 16 20:36:59 2002
++++ ax.h	Mon Dec 16 20:37:06 2002
+@@ -22,7 +22,7 @@
+ 
+ // Include Files
+ 
+-#include <iostream.h>
++#include <iostream>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
