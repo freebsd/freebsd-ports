edfd4fe7c7d031d632cfb5e6580e4c042087b48e
edwin <edwin@FreeBSD.org>
2002-12-17 04:39:46 +0000

    Make x11/accessx compile under -current again. Noticed on: bento

---
+++ x11/accessx/files/patch-Access.C
@@ -0,0 +1,11 @@
+--- Access.C.orig	Mon Dec 16 20:35:51 2002
++++ Access.C	Mon Dec 16 20:36:29 2002
+@@ -21,6 +21,8 @@
+ #include <X11/XKBlib.h>
+ #include "Access.h"
+ 
++using namespace std;
++
+ //
+ // Access()
+ //
