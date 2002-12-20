353f68dddac3a140a505da8e217011a54e092f1a
edwin <edwin@FreeBSD.org>
2002-12-20 12:35:43 +0000

    Unbreak ftp/ftplocate
    PR:		ports/46324
    Submitted by:	Michael L. Hostbaek <mich@freebsdcluster.org>

--- ftp/ftplocate/files/patch-install.pl
+++ ftp/ftplocate/files/patch-install.pl
@@ -4,7 +4,7 @@
  
  $defhtmldir="/usr/local/apache/share/htdocs";
  foreach (
-+	"/usr/local/www/data.default",
++	"/usr/local/www/data",
  	"/usr/local/apache/share/htdocs",
  	"/usr/local/apache/htdocs",
  	"/opt/apache/share/htdocs", 
@@ -12,7 +12,7 @@
  
  $defcgidir="/usr/local/apache/share/cgi-bin";
  foreach (
-+	"/usr/local/www/cgi-bin.default",
++	"/usr/local/www/cgi-bin",
  	"/usr/local/apache/share/cgi-bin",
  	"/usr/local/apache/cgi-bin",
  	"/opt/apache/share/cgi-bin", 
