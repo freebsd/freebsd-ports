887a47eb5050ff0ccdf55715f5679d5c02c8f7a6
Christian Weisgerber <naddy@FreeBSD.org>
2002-12-22 15:46:21 +0000

    Fix for -CURRENT: do not build superfluous local copy of inet_ntop() with conflicting prototype.

--- misc/yaunc/files/patch-my_hdrs.h
+++ misc/yaunc/files/patch-my_hdrs.h
@@ -1,9 +1,13 @@
---- my_hdrs.h.orig	Thu Mar 23 11:32:02 2000
-+++ my_hdrs.h	Sun Dec 15 05:10:26 2002
-@@ -35,7 +35,6 @@
+
+$FreeBSD$
+
+--- my_hdrs.h.orig	Thu Mar 23 03:32:02 2000
++++ my_hdrs.h	Sun Dec 22 16:33:44 2002
+@@ -34,8 +34,6 @@
+ ssize_t Recvfrom(int, void *, size_t, int, struct sockaddr *, socklen_t *);
  
  /* other networking function */
- int inet_pton(int, const char *, void *);
+-int inet_pton(int, const char *, void *);
 -const char * inet_ntop(int, const void *, char *, size_t);
  void Inet_pton(int, const char *, void *);
  
