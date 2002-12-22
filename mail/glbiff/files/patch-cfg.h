fde8f9af7c83a056e441d3e74a1ddaeaf3a2ac71
Pete Fritchman <petef@FreeBSD.org>
2002-12-22 21:22:46 +0000

    Fix build on -current.
    PR:		46473
    Submitted by:	Kuang-che Wu <kcwu@kcwu.homeip.net>

---
+++ mail/glbiff/files/patch-cfg.h
@@ -0,0 +1,12 @@
+--- cfg.h.orig	Mon Dec 23 03:45:36 2002
++++ cfg.h	Mon Dec 23 03:47:15 2002
+@@ -20,7 +20,8 @@
+  * 
+  */
+ 
+-#include <stl.h>
++#include <list>
++using namespace std;
+ #include <string.h>
+ #include "rgb.h"
+ #include "astro.h"
