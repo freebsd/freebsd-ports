2b335d4b285d871ef50b99984e97d21c87d1efd3
ijliao <ijliao@FreeBSD.org>
2002-12-22 12:51:20 +0000

    fix build on -current
    PR:		46459
    Submitted by:	Ports Fury

---
+++ biology/libgenome/files/patch-include::gn::gnException.h
@@ -0,0 +1,10 @@
+--- include/gn/gnException.h.orig	Sat Dec  1 05:57:29 2001
++++ include/gn/gnException.h	Sun Dec 22 12:44:39 2002
+@@ -4,6 +4,7 @@
+ #include "gnClone.h"
+ #include "gn/gnExceptionCode.h"
+ #include <string>
++#include <iostream>
+ #include <list>
+ 
+ class GNDLLEXPORT gnException
