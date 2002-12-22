864d61582ae3a6d659954e2e0bf543b5f3080cae
petef <petef@FreeBSD.org>
2002-12-22 21:22:46 +0000

    Fix build on -current.
    PR:		46473
    Submitted by:	Kuang-che Wu <kcwu@kcwu.homeip.net>

---
+++ mail/glbiff/files/patch-cfg.cc
@@ -0,0 +1,10 @@
+--- cfg.cc.orig	Mon Dec 23 03:45:30 2002
++++ cfg.cc	Mon Dec 23 03:45:48 2002
+@@ -23,7 +23,6 @@
+ // This file defines subroutines to handle the
+ // configuration file
+ 
+-#include <stl.h>
+ #include <stdio.h>
+ #include <iostream.h>
+ #include <ctype.h>
---
+++ mail/glbiff/files/patch-glbiff.cc
@@ -0,0 +1,11 @@
+--- glbiff.cc.orig	Mon Dec 23 03:47:27 2002
++++ glbiff.cc	Mon Dec 23 03:47:48 2002
+@@ -37,6 +37,8 @@
+ 
+ #include <X11/Xlib.h>
+ 
++#include <iostream>
++
+ #include "draw.h"
+ #include "glbiff.h"
+ #include "mail.h"
