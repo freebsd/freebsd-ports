515eedb8f752585810db6c2ea8c3bb52e0990ba4
Dirk Meyer <dinoex@FreeBSD.org>
2002-12-23 22:15:34 +0000

    - use opentool in GNUstep.sh

--- lang/gnustep-base/files/GNUstep.sh
+++ lang/gnustep-base/files/GNUstep.sh
@@ -2,22 +2,23 @@
 
 PREFIX=%%PREFIX%%
 IFFILE=${PREFIX}/etc/gdomap_if
-PIDFILE=/var/run/gdomap.pid
-GNUSTEP_SYSTEM_ROOT=${PREFIX}/GNUstep/System
+PIDFILE="/var/run/gdomap.pid"
+GNUSTEP_SYSTEM_ROOT="${PREFIX}/GNUstep/System"
 
 case "$1" in
 start)
 	if [ -e ${GNUSTEP_SYSTEM_ROOT}/Makefiles/GNUstep.sh ]; then
 		. ${GNUSTEP_SYSTEM_ROOT}/Makefiles/GNUstep.sh
-		if [ -x ${GNUSTEP_SYSTEM_ROOT}/Tools/${GNUSTEP_HOST_CPU}/${GNUSTEP_HOST_OS}/gdomap ]; then
-			rm -f ${PIDFILE}
-			${GNUSTEP_SYSTEM_ROOT}/Tools/${GNUSTEP_HOST_CPU}/${GNUSTEP_HOST_OS}/gdomap -a ${IFFILE} -I ${PIDFILE}
-			echo -n ' GNUstep'
-		fi
+		rm -f ${PIDFILE}
+		opentool gdomap -a ${IFFILE} -I ${PIDFILE}
+		opentool gdnc
+		echo -n ' GNUstep'
 	fi
 	;;
 stop)
 	[ -r ${PIDFILE} ] && kill $(cat ${PIDFILE}) && echo -n ' GNUstep'
+	killall gdnc
+	rm -f ${PIDFILE}
 	;;
 *)
 	echo "Usage: `basename $0` {start|stop}" >&2
