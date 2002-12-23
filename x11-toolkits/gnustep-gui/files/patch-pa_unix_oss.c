9ebf8104cd6d7a103cfc1026838f2e77ba71b7d7
dinoex <dinoex@FreeBSD.org>
2002-12-23 22:13:53 +0000

    - enable sound for CURRENT - apply PTHREAD hack - add regression Test - prepare MASTER_SITE_GNUSTEP

---
+++ x11-toolkits/gnustep-gui/files/patch-pa_unix_oss.c
@@ -0,0 +1,14 @@
+--- Tools/gsnd/portaudio/pa_unix_oss/pa_unix_oss.c.orig	Wed Oct  2 05:34:54 2002
++++ Tools/gsnd/portaudio/pa_unix_oss/pa_unix_oss.c	Mon Dec  2 09:21:34 2002
+@@ -110,7 +110,11 @@
+ #ifdef __linux__
+ #include <linux/soundcard.h>
+ #else
++#ifdef __FreeBSD__
++#include <sys/soundcard.h>
++#else
+ #include <machine/soundcard.h> /* JH20010905 */
++#endif
+ #endif
+ 
+ #include "portaudio.h"
