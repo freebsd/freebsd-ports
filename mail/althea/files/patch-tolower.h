f8c9e39d5fef22dac51eee6486b193c7d6c46769
ijliao <ijliao@FreeBSD.org>
2002-12-29 05:21:44 +0000

    fix build on -current
    PR:		46594
    Submitted by:	Ports Fury

---
+++ mail/althea/files/patch-tolower.h
@@ -0,0 +1,10 @@
+--- tolower.h.orig	Wed Jan 23 02:30:15 2002
++++ tolower.h	Fri Dec 27 03:27:01 2002
+@@ -1,5 +1,7 @@
+ #ifndef TOLOWER_H
+ #define TOLOWER_H
++namespace std {};
++using namespace std;
+ 
+ #include <iostream>
+ #include <string>
