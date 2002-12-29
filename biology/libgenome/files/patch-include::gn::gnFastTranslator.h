584eaa9957d6e2d7184a5cc337cd94e6318b4a8a
ijliao <ijliao@FreeBSD.org>
2002-12-29 04:44:13 +0000

    add missing include
    PR:		46584
    Submitted by:	Ports Fury

---
+++ biology/libgenome/files/patch-include::gn::gnFastTranslator.h
@@ -0,0 +1,10 @@
+--- include/gn/gnFastTranslator.h.orig	Tue Mar 19 08:52:51 2002
++++ include/gn/gnFastTranslator.h	Fri Dec 27 21:15:56 2002
+@@ -17,6 +17,7 @@
+ #include "gn/gnDefs.h"
+ 
+ #include <string>
++#include <iostream>
+ #include <vector>
+ #include <map>
+ #include "gn/gnClone.h"
