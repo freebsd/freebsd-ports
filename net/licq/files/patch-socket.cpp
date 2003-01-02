89a0a7629e0a81324af9609947842373cc4ac81d
dinoex <dinoex@FreeBSD.org>
2003-01-02 04:05:20 +0000

    - make socks-patch portable

--- net/licq/files/patch-socket.cpp
+++ net/licq/files/patch-socket.cpp
@@ -1,27 +1,29 @@
---- src/socket.cpp.orig	Tue Jul 23 12:22:13 2002
-+++ src/socket.cpp	Thu Aug  8 03:07:32 2002
-@@ -13,6 +13,7 @@
+--- src/socket.cpp.orig	Tue Jul 23 07:22:13 2002
++++ src/socket.cpp	Mon Dec 16 19:21:03 2002
+@@ -13,6 +13,9 @@
  #include <netdb.h>
  #include <fcntl.h>
  #include <unistd.h>
++#ifdef __FreeBSD__
 +#include <osreldate.h>
++#endif
  
  #ifdef HAVE_INET_ATON
  #include <arpa/inet.h>
-@@ -304,6 +305,8 @@
+@@ -304,6 +307,8 @@
    // Setup the local structure
  #ifdef USE_SOCKS5
    int sizeofSockaddr = sizeof(struct sockaddr_in);
-+#elif __FreeBSD_version < 400013
++#elif defined(__FreeBSD__) && __FreeBSD_version < 400013
 +  int sizeofSockaddr = sizeof(struct sockaddr_in);
  #else
    socklen_t sizeofSockaddr = sizeof(struct sockaddr_in);
  #endif
-@@ -416,7 +419,11 @@
+@@ -416,7 +421,11 @@
      m_sRemoteAddr.sin_family = AF_INET;
  
      // if connect fails then call CloseConnection to clean up before returning
-+#if __FreeBSD_version < 400013
++#if defined(__FreeBSD__) && __FreeBSD_version < 400013
 +    int sizeofSockaddr = sizeof(struct sockaddr);
 +#else
      socklen_t sizeofSockaddr = sizeof(struct sockaddr);
@@ -29,12 +31,12 @@
      if (connect(m_nDescriptor, (struct sockaddr *)&m_sRemoteAddr, sizeofSockaddr) < 0)
      {
        // errno has been set
-@@ -691,6 +698,8 @@
+@@ -691,6 +700,8 @@
  void TCPSocket::RecvConnection(TCPSocket &newSocket)
  {
  #ifdef USE_SOCKS5
 +  int sizeofSockaddr = sizeof(struct sockaddr_in);
-+#elif __FreeBSD_version < 400013
++#elif defined(__FreeBSD__) && __FreeBSD_version < 400013
    int sizeofSockaddr = sizeof(struct sockaddr_in);
  #else
    socklen_t sizeofSockaddr = sizeof(struct sockaddr_in);
