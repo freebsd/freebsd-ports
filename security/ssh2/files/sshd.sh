cff16e3928514749f01f78399db6e69f46036010
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-01-02 19:35:26 +0000

    1.)	If WITH_STATIC_SFTP is defined, ssh-chrootmgr works. 2.)	If libX11.a exists and xauth not, the build of ssh2 fails. This 	patch fix this. 3.)	ssh2/files/sshd.sh looks for the wrong pid file in /var/run. 	This patch fix this and adds 2> /dev/null to the sshd2 startup
    PR:		46012
    Submitted by:	maintainer

--- security/ssh2/files/sshd.sh
+++ security/ssh2/files/sshd.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 case "$1" in
 	start)
-		!!PREFIX!!/sbin/sshd
+		!!PREFIX!!/sbin/sshd 2> /dev/null
 		echo -n ' sshd'
 		;;
 	stop)
-		if [ -f /var/run/sshd.pid ]; then
-			kill -TERM `cat /var/run/sshd.pid`
-			rm -f /var/run/sshd.pid
+		if [ -f /var/run/sshd2_22.pid ]; then
+			kill -TERM `cat /var/run/sshd2_22.pid`
+			rm -f /var/run/sshd2_22.pid
 			echo -n ' sshd'
 		fi
 		;;
 	restart)
-		if [ -f /var/run/sshd.pid ]; then
-			kill -HUP `cat /var/run/sshd.pid`
+		if [ -f /var/run/sshd2_22.pid ]; then
+			kill -HUP `cat /var/run/sshd2_22.pid`
 			echo 'sshd restarted'
 		fi
 		;;
