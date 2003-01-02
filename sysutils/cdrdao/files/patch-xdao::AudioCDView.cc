21e18d8230a87a5a696b59290df9cd2cdd64d8dd
Alexander Leidinger <netchild@FreeBSD.org>
2003-01-02 15:53:38 +0000

    From the submitter: - Don't default to '-O2' when compiling the cdparanoia-lib but respect CFLAGS,   this should work around the generation of broken code with GCC and an   optimazation-level greater than 1. - Fix a bug in the cdparanoia-lib that can cause problems on some !386. - Remove the unneeded usage of what seems to be an extension of G++, i.e.   arguments to the constructor when allocating an array of objects with new,   fixes compilation with compilers stricter obeying the standards. - Always symlink ${MACHINE_ARCH}-freebsd-cc.rul to the i386-version on !i386 in   the RULES-directory of scglib to be consistent with sysutils/cdrtools, cdrdao   also should compile and work on any platform with the fixes above. - hier(7) the location of the global cdrdao.etc to LOCALBASE/etc/cdrdao.conf. - Make the gcdmaster gnome-frontend compile with GCC3, use HAVE_GOME-magic to   build it. - Clean up some stuff.
    NOTE: Gcdmaster has a very obvious bug, if one creates a new audio-CD by
          "New Audio CD project" -> "Edit" -> "Append Track" (select audio-file)
          it will get the length of the track wrong, display a garbage waveform
          and play nothing when hitting the play-button, same for further added
          tracks. This can be worked around by saving the project and re-opening
          it, now appending further tracks also works and the burnt CD is fine.
          This is _not_ a bug of this port but a bug in gcdmaster itself and
          is totally reproducable on Linux, therefore please don't mail the
          maintainer of this port about it expect you have a fix, thanks.
    
    Submitted by:	maintainer

---
+++ sysutils/cdrdao/files/patch-xdao::AudioCDView.cc
@@ -0,0 +1,11 @@
+--- xdao/AudioCDView.cc.orig	Sat Nov 16 23:13:42 2002
++++ xdao/AudioCDView.cc	Sat Nov 16 23:13:54 2002
+@@ -17,7 +17,7 @@
+  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+  */
+ 
+-#include <iostream.h>
++#include <iostream>
+ 
+ #include "xcdrdao.h"
+ #include "guiUpdate.h"
