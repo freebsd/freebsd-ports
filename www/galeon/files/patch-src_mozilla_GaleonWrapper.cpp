2c690ca911fb2e7dbef28237583b1069d3f0bdcf
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-01-02 19:05:56 +0000

    Update to 1.2.7.

--- www/galeon/files/patch-src_mozilla_GaleonWrapper.cpp
+++ www/galeon/files/patch-src_mozilla_GaleonWrapper.cpp
@@ -1,11 +1,29 @@
---- src/mozilla/GaleonWrapper.cpp.orig	Mon Sep 16 12:42:40 2002
-+++ src/mozilla/GaleonWrapper.cpp	Mon Sep 16 12:42:47 2002
-@@ -182,7 +182,7 @@
- 	}
- 	else
+--- src/mozilla/GaleonWrapper.cpp.orig	Thu Jan  2 13:55:14 2003
++++ src/mozilla/GaleonWrapper.cpp	Thu Jan  2 13:56:16 2003
+@@ -80,8 +80,10 @@
+ #include "caps/nsIPrincipal.h"
+ #include "nsIDeviceContext.h"
+ #include "nsIPresContext.h"
++#if MOZILLA_SNAPSHOT > 2
+ #include "nsITypeAheadFind.h"
+ #include "nsSupportsPrimitives.h"
++#endif
+ 
+ GaleonWrapper::GaleonWrapper ()
+ {
+@@ -584,6 +586,7 @@
+ 			      PRBool search_in_frames,
+ 			      PRBool *didFind)
+ {
++#if MOZILLA_SNAPSHOT > 2
+ 	if (!interactive)
  	{
--#if MOZILLA_SNAPSHOT > 1
-+#if MOZILLA_SNAPSHOT > 0
- 		result = print->PrintPreview(options, nsnull, nsnull);
- #else
- 		result = print->PrintPreview(options);
+ 		nsresult rv;
+@@ -610,6 +613,7 @@
+ 			}
+ 		}
+ 	}
++#endif
+ 
+ 	nsCOMPtr<nsIWebBrowserFind> finder (do_GetInterface(mWebBrowser));
+ 
