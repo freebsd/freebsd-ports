41e3fb3dffcd3bbb650cee8eba23087088fda6dd
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-01-02 19:01:20 +0000

    upgrade to 3.2.0
    PR:		46127
    Submitted by:	Ports Fury

---
+++ devel/ftnchek/files/patch-makehtml.c
@@ -0,0 +1,10 @@
+--- makehtml.c.orig	Thu Nov 21 08:16:41 2002
++++ makehtml.c	Sat Dec  7 03:18:59 2002
+@@ -43,6 +43,7 @@
+ 
+ #include <stdio.h>
+ #include <ctype.h>
++#include <sys/types.h>
+ #include <string.h>
+ #include <errno.h>
+ #include "ftnchek.h"
