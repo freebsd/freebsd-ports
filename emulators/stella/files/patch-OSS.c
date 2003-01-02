50282cc176b2c02d4f477fd72b2cb69293d8fd34
ijliao <ijliao@FreeBSD.org>
2003-01-02 08:02:49 +0000

    upgrade to 1.2
    PR:		46627
    Submitted by:	Keith Jones <kmjones@blueyonder.co.uk>

---
+++ emulators/stella/files/patch-OSS.c
@@ -0,0 +1,15 @@
+--- ../ui/sound/OSS.c.orig	Tue Jan  8 17:11:32 2002
++++ ../ui/sound/OSS.c	Mon Dec 30 04:08:03 2002
+@@ -32,12 +32,7 @@
+ #include <sys/types.h>
+ #include <unistd.h>
+ #include <string.h>
+-
+-#ifdef __FreeBSD__
+-  #include <machine/soundcard.h>
+-#else
+-  #include <sys/soundcard.h>
+-#endif
++#include <sys/soundcard.h>
+ 
+ #include "TIASound.h"
