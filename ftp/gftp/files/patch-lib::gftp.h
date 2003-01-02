5a762d7629e899e8592b3d77dc8d22f68629d82b
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-01-02 19:46:05 +0000

    upgrade to 2.0.14
    PR:		46020
    Submitted by:	maintainer

---
+++ ftp/gftp/files/patch-lib::gftp.h
@@ -0,0 +1,11 @@
+--- lib/gftp.h.orig	Wed Nov 27 17:19:22 2002
++++ lib/gftp.h	Wed Nov 27 17:19:43 2002
+@@ -35,7 +35,7 @@
+ #endif
+ #include <sys/wait.h>
+ #include <sys/utsname.h>
+-#include <sys/sysmacros.h>
++/* #include <sys/sysmacros.h> */
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
+ #include <glib.h>
