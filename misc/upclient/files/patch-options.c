453db036e363e1679a5c679d949a188d051ce008
petef <petef@FreeBSD.org>
2003-01-03 06:35:32 +0000

    Update to 5.0.b6.
    PR:		46339
    Submitted by:	maintainer

--- misc/upclient/files/patch-options.c
+++ misc/upclient/files/patch-options.c
@@ -1,11 +1,11 @@
---- src/options.c.orig	Wed Oct  2 06:50:49 2002
-+++ src/options.c	Sat Oct 19 22:54:06 2002
-@@ -55,7 +55,7 @@
+--- src/options.c.orig	Sat Nov 30 03:27:15 2002
++++ src/options.c	Wed Dec 18 01:43:00 2002
+@@ -60,7 +60,7 @@
+  * Minimum value is enforced. A warning is logged if greater than upper limit.
   */
- char cfg_authkey[AUTHKEY_REQUIRED_LEN+1]	= "";
- char cfg_upserver[UPSERVER_MAXLEN+1]		= "uptimes.wonko.com";
--int  cfg_interval				= 300;
-+int  cfg_interval				= 550;
- char cfg_pidfile[PIDFILE_MAXLEN+1]		= PIDFILE;
- char cfg_proxyserver[PROXYSERVER_MAXLEN+1]	= "";
- int  cfg_udpport				= 49153;
+ #define MINIMUM_INTERVAL  30    /* enforced */
+-#define DEFAULT_INTERVAL 300
++#define DEFAULT_INTERVAL 550
+ #define UPPER_INTERVAL   600    /* warning if exceeded */
+ 
+ /* Macros */
