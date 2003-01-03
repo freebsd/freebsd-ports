0e3c4ba4ffa176daa4ab9ecc653552a4035089bb
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-01-03 06:08:40 +0000

    fix bin name
    PR:		43466
    Submitted by:	MOROHOSHI Akihiko <moro@remus.dti.ne.jp>

---
+++ editors/gnuserv/files/patch-gnuserv.el
@@ -0,0 +1,11 @@
+--- gnuserv.el.orig	Thu Jan 25 19:41:59 2001
++++ gnuserv.el	Sun Sep 29 19:16:03 2002
+@@ -173,7 +173,7 @@
+ 		(function :tag "Other"))
+   :group 'gnuserv)
+ 
+-(defcustom gnuserv-program "gnuserv"
++(defcustom gnuserv-program "gnuserv-emacs"
+   "*Program to use as the editing server."
+   :type 'string
+   :group 'gnuserv)
