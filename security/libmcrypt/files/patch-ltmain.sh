2dbae2e962303a1e0e64275195b6173422415ceb
ijliao <ijliao@FreeBSD.org>
2003-01-03 19:15:09 +0000

    upgrade to 2.5.4
    PR:		46432
    Submitted by:	Ports Fury

--- security/libmcrypt/files/patch-ltmain.sh
+++ security/libmcrypt/files/patch-ltmain.sh
@@ -1,6 +1,6 @@
---- ltmain.sh.orig	Fri Feb  1 20:23:29 2002
-+++ ltmain.sh	Sat Mar  2 15:26:31 2002
-@@ -958,6 +958,7 @@
+--- ltmain.sh.orig	Wed Dec 18 06:19:10 2002
++++ ltmain.sh	Sat Dec 21 02:44:34 2002
+@@ -973,6 +973,7 @@
  	;;
  
        -avoid-version)
@@ -8,3 +8,16 @@
  	avoid_version=yes
  	continue
  	;;
+@@ -4325,10 +4326,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
