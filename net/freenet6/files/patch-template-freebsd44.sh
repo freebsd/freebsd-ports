12d6fd41653925476f214ed340a09595ebfd41e6
edwin <edwin@FreeBSD.org>
2003-01-03 11:16:27 +0000

    Remove calls to gifconfig(8). Still works fine on 4.5, don't know about 4.4.
    PR:		ports/46668
    Submitted by:	Kirk Strauser <kirk@strauser.com>
    Approved by:	maintainer

--- net/freenet6/files/patch-template-freebsd44.sh
+++ net/freenet6/files/patch-template-freebsd44.sh
@@ -1,6 +1,23 @@
 --- template/freebsd44.sh.orig	Sat Feb 23 06:32:37 2002
-+++ template/freebsd44.sh	Sat Mar  2 09:53:54 2002
-@@ -118,6 +118,9 @@
++++ template/freebsd44.sh	Fri Jan  3 22:13:35 2003
+@@ -64,7 +64,6 @@
+ Display 1 '--- Start of configuration script. ---'
+ Display 1 "Script: " `basename $0`
+ 
+-gifconfig=/usr/sbin/gifconfig
+ ifconfig=/sbin/ifconfig
+ route=/sbin/route
+ rtadvd=/usr/sbin/rtadvd
+@@ -104,7 +103,7 @@
+       Exec $ifconfig $TSP_TUNNEL_INTERFACE destroy
+    fi
+    Exec $ifconfig $TSP_TUNNEL_INTERFACE create
+-   Exec $gifconfig $TSP_TUNNEL_INTERFACE $TSP_CLIENT_ADDRESS_IPV4 $TSP_SERVER_ADDRESS_IPV4
++   Exec $ifconfig $TSP_TUNNEL_INTERFACE tunnel $TSP_CLIENT_ADDRESS_IPV4 $TSP_SERVER_ADDRESS_IPV4
+ 
+    #
+    # Configured tunnel config (IPv6) 
+@@ -118,6 +117,9 @@
     # Delete any default IPv6 route first
     ExecNoCheck $route delete -inet6 default
     Exec $route add -inet6 default $TSP_SERVER_ADDRESS_IPV6
