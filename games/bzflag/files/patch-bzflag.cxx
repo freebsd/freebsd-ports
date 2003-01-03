9b9f2251a6ef86b1544c4ba545247cd7ea1f26bf
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-01-03 20:16:07 +0000

    upgrade to 1.7g0
    PR:		46586
    Submitted by:	Ports Fury

--- games/bzflag/files/patch-bzflag.cxx
+++ games/bzflag/files/patch-bzflag.cxx
@@ -1,6 +1,6 @@
---- src/bzflag/bzflag.cxx.orig	Sat Nov 23 15:16:44 2002
-+++ src/bzflag/bzflag.cxx	Sat Nov 23 15:17:53 2002
-@@ -756,7 +756,7 @@
+--- src/bzflag/bzflag.cxx.orig	Fri Dec  6 02:22:44 2002
++++ src/bzflag/bzflag.cxx	Mon Dec  9 22:09:36 2002
+@@ -769,7 +769,7 @@
  
    // read resources
    {
@@ -8,13 +8,4 @@
 +    #if 1
  	ifstream resourceStream(getConfigFileName(), ios::in);
       #else
-     ifstream resourceStream(getConfigFileName(), ios::in | ios::nocreate);
-@@ -768,7 +768,7 @@
- 
- #if !defined(_WIN32) & !defined(macintosh)
-     else {
--      ifstream resourceStream2(getConfigFileName2(), ios::in | ios::nocreate);
-+      ifstream resourceStream2(getConfigFileName2(), ios::in);
-       if (resourceStream2) {
- 	startupInfo.hasConfiguration = True;
- 	resourceStream2 >> db;
+     ifstream resourceStream(getConfigFileName(), ios::in);
