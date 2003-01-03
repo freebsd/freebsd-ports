a50268d00196eeb658505540d76ad827cc822da5
arved <arved@FreeBSD.org>
2003-01-03 17:55:27 +0000

    Update to 0.10
    PR:		46307
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

---
+++ lang/open-cobol/files/patch-config::ltmain.sh
@@ -0,0 +1,15 @@
+--- config/ltmain.sh.orig	Fri Nov 22 17:03:54 2002
++++ config/ltmain.sh	Thu Dec 12 20:35:02 2002
+@@ -4280,10 +4280,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
