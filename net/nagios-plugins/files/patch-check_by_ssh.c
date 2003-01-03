40e5056d192fdc0429a063ae8e5291c0917a4ff8
Pete Fritchman <petef@FreeBSD.org>
2003-01-03 19:08:00 +0000

    Update to 1.3.0.b2.
    Submitted by:	maintainer

--- net/nagios-plugins/files/patch-check_by_ssh.c
+++ net/nagios-plugins/files/patch-check_by_ssh.c
@@ -1,20 +1,20 @@
---- plugins/check_by_ssh.c.orig	Thu Feb 28 07:42:57 2002
-+++ plugins/check_by_ssh.c	Sun Jul 14 09:50:43 2002
-@@ -382,6 +382,8 @@
+--- plugins/check_by_ssh.c.orig	Wed Nov 20 02:19:07 2002
++++ plugins/check_by_ssh.c	Tue Nov 26 14:20:28 2002
+@@ -361,6 +361,8 @@
  		 "   list of nagios service names, separated by ':' [optional]\n"
  		 "-n, --name=NAME\n"
  		 "   short name of host in nagios configuration [optional]\n"
 +		 "-v, --verbose\n"
 +		 "   display command being executed\n"
- 		 "\n"
- 		 "The most common mode of use is to refer to a local identity file with\n"
- 		 "the '-i' option. In this mode, the identity pair should have a null\n"
-@@ -405,7 +407,7 @@
+ 		 "-4, --use-ipv4\n"
+ 		 "   tell ssh to use IPv4\n"
+ 		 "-6, --use-ipv6\n"
+@@ -388,7 +390,7 @@
  {
  	printf
  		("Usage:\n"
--		 "check_by_ssh [-f] [-t timeout] [-i identity] [-l user] -H <host> <command>\n"
-+		 "check_by_ssh [-fv] [-t timeout] [-i identity] [-l user] -H <host> <command>\n"
- 		 "             [-n name] [-s servicelist] [-O outputfile] [-P port]\n"
+-		 "check_by_ssh [-f46] [-t timeout] [-i identity] [-l user] -H <host> -C <command>\n"
++		 "check_by_ssh [-fv46] [-t timeout] [-i identity] [-l user] -H <host> -C <command>\n"
+ 		 "             [-n name] [-s servicelist] [-O outputfile] [-p port]\n"
  		 "check_by_ssh  -V prints version info\n"
  		 "check_by_ssh  -h prints more detailed help\n");
