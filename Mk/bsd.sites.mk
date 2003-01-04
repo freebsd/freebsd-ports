e9ef928f2046c0f8ce94551ee6e9d9ec43bd08e4
Akinori MUSHA <knu@FreeBSD.org>
2003-01-04 15:50:12 +0000

    Shuffle MASTER_SITE_RUBY a bit.
    Some people say that the ruby-1.6.8 distfile on xyz.lcs.mit.edu was
    corrupt.  It was not corrupt when I tried yesterday, but anyway.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -301,10 +301,10 @@ MASTER_SITE_RINGSERVER+=	\
 	http://www.t.ring.gr.jp/archives/%SUBDIR%/
 
 MASTER_SITE_RUBY+= \
-	ftp://xyz.lcs.mit.edu/pub/ruby/%SUBDIR%/ \
-	http://www.ibiblio.org/pub/languages/ruby/%SUBDIR%/ \
-	${MASTER_SITE_RINGSERVER:S,%SUBDIR%,lang/ruby/&,} \
 	ftp://ftp.iij.ad.jp/pub/lang/ruby/%SUBDIR%/ \
+	http://www.ibiblio.org/pub/languages/ruby/%SUBDIR%/ \
+	ftp://xyz.lcs.mit.edu/pub/ruby/%SUBDIR%/ \
+	${MASTER_SITE_RINGSERVER:S,%SUBDIR%,lang/ruby/&,} \
 	ftp://ftp.ruby-lang.org/pub/ruby/%SUBDIR%/ \
 	ftp://ftp.fu-berlin.de/unix/languages/ruby/%SUBDIR%/ \
 	ftp://ftp.easynet.be/ruby/ruby/%SUBDIR%/ \
