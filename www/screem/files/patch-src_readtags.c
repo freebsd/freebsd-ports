68bf2d4b09b60f1e9a36e4ec694bd955d3557f1a
marcus <marcus@FreeBSD.org>
2003-01-05 08:19:20 +0000

    Update to 0.5.9.
    Approved by:	maintainer

---
+++ www/screem/files/patch-src_readtags.c
@@ -0,0 +1,10 @@
+--- src/readtags.c.orig	Sat Jan  4 23:43:42 2003
++++ src/readtags.c	Sat Jan  4 23:48:00 2003
+@@ -8,6 +8,7 @@
+ *   This module contains functions for reading tag files.
+ */
+ 
++#include <config.h>
+ #include <glib.h>
+ 
+ #include <stdlib.h>
