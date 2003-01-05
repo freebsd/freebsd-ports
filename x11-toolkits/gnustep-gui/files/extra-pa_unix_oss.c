d69ce359c61fbc41a40670ed1097d7274c3c52c1
dinoex <dinoex@FreeBSD.org>
2003-01-05 04:35:51 +0000

    kill anonther malloc.h problem

--- x11-toolkits/gnustep-gui/files/extra-pa_unix_oss.c
+++ x11-toolkits/gnustep-gui/files/extra-pa_unix_oss.c
@@ -1,6 +1,16 @@
 --- Tools/gsnd/portaudio/pa_unix_oss/pa_unix_oss.c.orig	Wed Oct  2 05:34:54 2002
-+++ Tools/gsnd/portaudio/pa_unix_oss/pa_unix_oss.c	Mon Dec  2 09:21:34 2002
-@@ -110,7 +110,11 @@
++++ Tools/gsnd/portaudio/pa_unix_oss/pa_unix_oss.c	Sun Jan  5 05:33:48 2003
+@@ -95,7 +95,9 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#ifndef __FreeBSD__
+ #include <malloc.h>
++#endif
+ #include <memory.h>
+ #include <math.h>
+ #include <sys/ioctl.h>
+@@ -110,7 +112,11 @@
  #ifdef __linux__
  #include <linux/soundcard.h>
  #else
