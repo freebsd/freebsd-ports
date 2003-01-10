71f42afed38c396eb89ba5148f323356783f918a
lioux <lioux@FreeBSD.org>
2003-01-10 18:21:21 +0000

    - After repo copy to ffmpeg045 port so that both older and faster   version 0.4.5 is preserved while the current version matures: o Update to 0.4.6 o Remove ONLY_FOR_ARCHS, no longer i386 only o Add PKGMESSAGE explaining the differences between this and ffmpeg045   port
    Reviewed by:	Steve O'Hara-Smith <steve@sohara.org>
    Repo copied by:	cvs (joe)
    Approved by:	portmgr (self)

--- multimedia/ffmpeg-devel/files/patch-libav::avformat.h
+++
@@ -1,9 +0,0 @@
---- libav/avformat.h	Mon Jul  8 04:38:40 2002
-+++ libav/avformat.h	Sun Jul 14 15:22:58 2002
-@@ -284,5 +284,5 @@
- int video_grab_init(void);
- int audio_init(void);
- 
--extern const char *v4l_device;
-+extern const char *video_device;
- extern const char *audio_device;
