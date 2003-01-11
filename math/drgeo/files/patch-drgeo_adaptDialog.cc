7a4b56a0718e83f2cb50c583e67d8cce84310e59
marcus <marcus@FreeBSD.org>
2003-01-11 08:35:25 +0000

    Add drgenius, a geometry exploration application built on top of GNOME.
    Requested by:	P. U. Kruppa <520023893678-0001@t-online.de>

---
+++ math/drgeo/files/patch-drgeo_adaptDialog.cc
@@ -0,0 +1,10 @@
+--- drgeo_adaptDialog.cc.orig	Sat Jan 11 03:21:20 2003
++++ drgeo_adaptDialog.cc	Sat Jan 11 03:21:27 2003
+@@ -24,7 +24,6 @@
+ #include <stdio.h>
+ #include <time.h>
+ #include <unistd.h>
+-#include <crypt.h>
+ #include <string.h>
+ 
+ #include <glade/glade.h>
