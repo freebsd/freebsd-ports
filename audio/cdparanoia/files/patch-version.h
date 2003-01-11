3755e0a4f7af82a77f5e70f44f43e083523cd7ba
anholt <anholt@FreeBSD.org>
2003-01-11 09:15:00 +0000

    Fix cooked ioctl bug ignoring the last track, add porter's copyright.
    Submitted by:	corecode@corecode.ath.cx (maintainer)

---
+++ audio/cdparanoia/files/patch-version.h
@@ -0,0 +1,18 @@
+Index: version.h
+===================================================================
+RCS file: /home/cvs/cdparanoia/version.h,v
+retrieving revision 1.1.1.1
+retrieving revision 1.2
+diff -u -r1.1.1.1 -r1.2
+--- version.h	2003/01/05 09:46:26	1.1.1.1
++++ version.h	2003/01/07 00:49:01	1.2
+@@ -8,6 +8,8 @@
+ 
+ 
+ #define VERSION "cdparanoia III release 9.8 (March 23, 2001)\n"\
+-                "(C) 2001 Monty <monty@xiph.org> and Xiphophorus\n\n"\
++                "(C) 2001 Monty <monty@xiph.org> and Xiphophorus\n"\
++		"FreeBSD porting (c) 2003\n"\
++		"\tSimon 'corecode' Schubert <corecode@corecode.ath.cx>\n\n"\
+ 		"Report bugs to paranoia@xiph.org\n"\
+ 		"http://www.xiph.org/paranoia/\n"
