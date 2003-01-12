ee3158aafdb2b270d028a3f93cccc151f51ac400
marcus <marcus@FreeBSD.org>
2003-01-12 18:43:29 +0000

    Attempt to fix build on sparc64 and ia64.  I tried to test, but panther doesn't have libc_r.  The build did get further than the errors reported on bento.
    Reported by:	bento

--- devel/nspr/files/patch-..::pr::include::md::_freebsd.h
+++ devel/nspr/files/patch-..::pr::include::md::_freebsd.h
@@ -1,9 +1,6 @@
-
-$FreeBSD$
-
---- ../pr/include/md/_freebsd.h.orig	Sat Feb 16 09:12:55 2002
-+++ ../pr/include/md/_freebsd.h	Thu Aug  8 19:31:23 2002
-@@ -40,6 +40,7 @@
+--- ../pr/include/md/_freebsd.h.orig	Sat Feb 16 02:12:55 2002
++++ ../pr/include/md/_freebsd.h	Sun Jan 12 03:10:59 2003
+@@ -40,14 +40,19 @@
  #if __FreeBSD__ >= 2
  #include <osreldate.h>  /* for __FreeBSD_version */
  #endif
@@ -11,3 +8,16 @@ $FreeBSD$
  #include <sys/syscall.h>
  
  #define PR_LINKER_ARCH	"freebsd"
+ #define _PR_SI_SYSNAME  "FREEBSD"
+ #if defined(__i386__)
+ #define _PR_SI_ARCHITECTURE "x86"
+-#elif defined(__alpha)
++#elif defined(__alpha__)
+ #define _PR_SI_ARCHITECTURE "alpha"
++#elif defined(__sparc64__)
++#define _PR_SI_ARCHITECTURE "sparc"
++#elif defined(__ia64__)
++#define _PR_SI_ARCHITECTURE "ia64"
+ #else
+ #error "Unknown CPU architecture"
+ #endif
