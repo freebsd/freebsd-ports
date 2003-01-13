2d5b393e18889d2d01e0229abd4b1733dc8cdaad
arved <arved@FreeBSD.org>
2003-01-13 10:31:04 +0000

    Update to 2.5.1
    PR:		46935
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

---
+++ finance/qhacc/files/patch-src::main.cpp
@@ -0,0 +1,10 @@
+--- src/main.cpp.orig	Thu Jan  2 02:42:35 2003
++++ src/main.cpp	Wed Jan  8 04:51:12 2003
+@@ -26,7 +26,6 @@
+ #include "qhaccwidget.h"
+ 
+ #include <stdlib.h>
+-#include <getopt.h>
+ #include <locale.h>
+ 
+ #include <qapplication.h>
