ab1334990d4d6a5737739c32f1a6cb546f610e44
sobomax <sobomax@FreeBSD.org>
2003-01-15 18:25:48 +0000

    Update to 2.4.0.

--- x11-toolkits/wxgtk-devel/files/patch-include::wx::wxchar.h
+++ x11-toolkits/wxgtk-devel/files/patch-include::wx::wxchar.h
@@ -1,19 +1,18 @@
 
 $FreeBSD$
 
---- include/wx/wxchar.h.orig	Mon Dec 10 20:07:02 2001
-+++ include/wx/wxchar.h	Sat Jan 19 12:14:57 2002
-@@ -532,11 +532,11 @@
- #endif //!Unicode
+--- include/wx/wxchar.h.orig	Sun Sep 15 21:19:48 2002
++++ include/wx/wxchar.h	Sat Oct 26 18:06:12 2002
+@@ -554,10 +554,10 @@
+ #endif // wxUSE_WCHAR_T
  
  // checks whether the passed in pointer is NULL and if the string is empty
 -inline bool wxIsEmpty(const wxChar *p) { return !p || !*p; }
 +WXDLLEXPORT inline bool wxIsEmpty(const wxChar *p) { return !p || !*p; }
  
- #ifndef wxNEED_WX_STRING_H
  // safe version of strlen() (returns 0 if passed NULL pointer)
--inline size_t wxStrlen(const wxChar *psz)
-+WXDLLEXPORT inline size_t wxStrlen(const wxChar *psz)
-    { return psz ? wxStrlen_(psz) : 0; }
- #endif
+-inline size_t wxStrlen(const wxChar *psz) { return psz ? wxStrlen_(psz) : 0; }
++WXDLLEXPORT inline size_t wxStrlen(const wxChar *psz) { return psz ? wxStrlen_(psz) : 0; }
+ 
+ WXDLLEXPORT bool wxOKlibc(); // for internal use
  
