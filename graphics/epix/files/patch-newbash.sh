9d3f1af935a76fd36b10eff4fc8f58343c4da1a4
Tilman Keskinoz <arved@FreeBSD.org>
2003-01-16 23:38:13 +0000

    Update to 0.8.8a
    PR:		47076
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

--- graphics/epix/files/patch-newbash.sh
+++ graphics/epix/files/patch-newbash.sh
@@ -1,6 +1,6 @@
---- newbash.sh.orig	Sun Oct 27 23:53:05 2002
-+++ newbash.sh	Sun Oct 27 23:53:54 2002
-@@ -1,7 +1,7 @@
+--- newbash.sh.orig	Tue Sep  3 01:50:01 2002
++++ newbash.sh	Sat Jan 11 15:16:11 2003
+@@ -1,10 +1,10 @@
  #!/bin/sh
  #
  for FILE in elaps laps make_header post-uninst.sh pre-install.sh prepix \
@@ -8,4 +8,8 @@
 +	helpfiles.sh samples/makefigs Makefile
  do
  	mv $FILE $FILE.orig
- 	sed 's+/bin/bash+/usr/local/bin/bash+g' $FILE.orig > $FILE
+-	sed 's+/bin/bash+/usr/local/bin/bash+g' $FILE.orig > $FILE
++	sed 's+/bin/bash+%%LOCALBASE%%/bin/bash+g' $FILE.orig > $FILE
+ ##                       ^-----------------^
+ ## Replace the portion from + to + of the line above with the path to
+ ## bash on your system. Sample replacement line:
