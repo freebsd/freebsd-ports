4383068c54c6d058b94f5032947549eaef521368
Edwin Groothuis <edwin@FreeBSD.org>
2003-01-16 05:56:50 +0000

    upgrade net/xbone port from 1.3.1 to 2.0
    PR:		ports/46849
    Submitted by:	Yu-Shun Wang <yushunwa@isi.edu>

---
+++ net/xbone/files/patch-XB_Defs.pm
@@ -0,0 +1,11 @@
+--- xbone/lib/XB_Defs.pm.orig	Thu Dec 19 16:50:25 2002
++++ xbone/lib/XB_Defs.pm	Thu Dec 19 16:51:40 2002
+@@ -153,7 +153,7 @@
+ ############################################################
+ 
+ $XB_Defs::XBONE_VERSION_DATE =	    # Date this version was created.
+-  "30/10/2001";                     # "<day_num>/<month_num>/<year_num>"
++  "1/1/2003";                       # "<day_num>/<month_num>/<year_num>"
+ # `date "+%d/%m/%Y"`;               # <- for development
+ 
+ $XB_Defs::XBONE_OBSOLETE_LIMIT =    # Seconds after the version date
