5f98089877d01fcf95940d4d348f97a5e98023b6
Maxim Sobolev <sobomax@FreeBSD.org>
2003-01-17 19:44:11 +0000

    Update to 2.1.3.

--- print/freetype2/files/patch-include::freetype::config::ftoption.h
+++ print/freetype2/files/patch-include::freetype::config::ftoption.h
@@ -1,22 +1,13 @@
 
 $FreeBSD$
 
---- include/freetype/config/ftoption.h.orig	Sat Jun  8 04:05:56 2002
-+++ include/freetype/config/ftoption.h	Fri Jun 21 15:49:27 2002
-@@ -82,7 +82,7 @@
+--- include/freetype/config/ftoption.h.orig	Thu Nov  7 00:32:53 2002
++++ include/freetype/config/ftoption.h	Fri Jan 17 20:44:08 2003
+@@ -378,7 +378,7 @@
+   /*   do not #undef this macro here, since the build system might         */
+   /*   define for certain configurations                                   */
    /*                                                                       */
-   /*   Define this if you want generic cmap support.                       */
-   /*                                                                       */
--#define FT_CONFIG_OPTION_USE_CMAPS
-+#undef FT_CONFIG_OPTION_USE_CMAPS
- 
- 
-   /*************************************************************************/
-@@ -375,7 +375,7 @@
-   /* By undefining this, you will only compile the code necessary to load  */
-   /* TrueType glyphs without hinting.                                      */
-   /*                                                                       */
--#undef  TT_CONFIG_OPTION_BYTECODE_INTERPRETER
+-/* #define  TT_CONFIG_OPTION_BYTECODE_INTERPRETER */
 +#define  TT_CONFIG_OPTION_BYTECODE_INTERPRETER
  
  
