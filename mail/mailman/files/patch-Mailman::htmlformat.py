038555dd50756bf6828c12b13aa2bbcb8864b434
wjv <wjv@FreeBSD.org>
2003-01-17 12:58:06 +0000

    - Update to the long-anticipated version 2.1

--- mail/mailman/files/patch-Mailman::htmlformat.py
+++ mail/mailman/files/patch-Mailman::htmlformat.py
@@ -1,11 +1,7 @@
---- Mailman/htmlformat.py.orig	Wed Apr  3 00:28:59 2002
-+++ Mailman/htmlformat.py	Thu Apr  4 11:28:51 2002
-@@ -503,15 +503,17 @@
- # These are the URLs which the image logos link to.  The Mailman home page now
- # points at the gnu.org site instead of the www.list.org mirror.
- #
--from mm_cfg import MAILMAN_URL
-+from mm_cfg import MAILMAN_URL, LOGO_OS
+--- Mailman/htmlformat.py.orig	Sun Apr 21 17:27:02 2002
++++ Mailman/htmlformat.py	Fri Sep 27 16:17:48 2002
+@@ -600,12 +600,14 @@
+ from mm_cfg import MAILMAN_URL
  PYTHON_URL  = 'http://www.python.org/'
  GNU_URL     = 'http://www.gnu.org/'
 +FREEBSD_URL = 'http://www.freebsd.org/'
@@ -19,27 +15,26 @@
  
  
  def MailmanLogo():
-@@ -525,12 +527,21 @@
+@@ -617,17 +619,17 @@
+                  '<br>version %s' % (logo(DELIVERED_BY), mm_cfg.VERSION)
+         pylink = '<img src="%s" alt="Python Powered" border=0>' % \
                   logo(PYTHON_POWERED)
-         gnulink = '<img src="%s" alt="GNU\'s Not Unix" border=0>' % \
-                   logo(GNU_HEAD)
+-        gnulink = '<img src="%s" alt="GNU\'s Not Unix" border=0>' % \
+-                  logo(GNU_HEAD)
 -        t.AddRow([mmlink, pylink, gnulink])
 +        freebsdlink = '<img src="%s" alt="Powered by FreeBSD" border=0>' % \
-+                      logo(FREEBSD_POWERED)
-+        if LOGO_OS == 'FreeBSD':
-+	    t.AddRow([mmlink, pylink, freebsdlink])
-+        else:
-+	    t.AddRow([mmlink, pylink, gnulink])
++                 logo(FREEBSD_POWERED)
++        t.AddRow([mmlink, pylink, freebsdlink])
      else:
          # use only textual links
+         version = mm_cfg.VERSION
          mmlink = Link(MAILMAN_URL,
-                       'Delivered by Mailman<br>version %s' % mm_cfg.VERSION)
-         pylink = Link(PYTHON_URL, 'Python Powered')
-         gnulink = Link(GNU_URL, "Gnu's Not Unix")
+                       _('Delivered by Mailman<br>version %(version)s'))
+         pylink = Link(PYTHON_URL, _('Python Powered'))
+-        gnulink = Link(GNU_URL, _("Gnu's Not Unix"))
 -        t.AddRow([mmlink, pylink, gnulink])
 +        freebsdlink = Link(FREEBSD_URL, "Powered by FreeBSD")
-+        if LOGO_OS == 'FreeBSD':
-+            t.AddRow([mmlink, pylink, freebsdlink])
-+        else:
-+            t.AddRow([mmlink, pylink, gnulink])
++        t.AddRow([mmlink, pylink, freebsdlink])
      return t
+ 
+ 
