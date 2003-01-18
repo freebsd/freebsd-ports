983c41ef444248c65929fa787f0701e59e8afd52
arved <arved@FreeBSD.org>
2003-01-18 23:03:48 +0000

    Add kmamerun, A KDE frontend to xmame
    PR:		47198
    Submitted by:	Scott A. Moberly <smoberly@karamazov.org>

---
+++ emulators/kmamerun/files/patch-kmamerun+main.cpp
@@ -0,0 +1,12 @@
+*** kmamerun/main.cpp.orig	Sat Jan 18 13:47:33 2003
+--- kmamerun/main.cpp	Sat Jan 18 13:47:54 2003
+***************
+*** 22,27 ****
+--- 22,28 ----
+  #include <kcmdlineargs.h>
+  #include <kaboutdata.h>
+  #include <klocale.h>
++ #include <stdlib.h>
+  
+  QString globalGameName;
+  QString globalGameFullName;
