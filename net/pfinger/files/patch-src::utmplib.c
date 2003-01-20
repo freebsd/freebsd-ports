44fafde2da35234868cd70ad1b542641782b8831
ijliao <ijliao@FreeBSD.org>
2003-01-20 03:46:26 +0000

    upgrade to 0.7.10 drop maintainership

--- net/pfinger/files/patch-src::utmplib.c
+++ net/pfinger/files/patch-src::utmplib.c
@@ -1,10 +1,10 @@
---- src/utmplib.c.orig	Tue Nov 27 01:52:39 2001
-+++ src/utmplib.c	Tue Nov 27 01:53:08 2001
+--- src/utmplib.c.orig	Mon Jan 20 11:42:57 2003
++++ src/utmplib.c	Mon Jan 20 11:43:05 2003
 @@ -32,6 +32,7 @@
  
  #if !defined(HAVE_SETUTENT) && !defined (HAVE_UTMPX_H)
  
 +#include <sys/types.h>
- #include <syslog.h>
  #include <time.h> /* Don't ask... */
  #include <utmp.h>
+ #include <fcntl.h>
