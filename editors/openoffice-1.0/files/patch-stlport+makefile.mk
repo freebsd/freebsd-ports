d0202038f21c35c789ee7d1c0fc1484a8ff89a01
Martin Blapp <mbr@FreeBSD.org>
2003-01-21 22:13:13 +0000

    Update to 1.02

--- editors/openoffice-1.0/files/patch-stlport+makefile.mk
+++ editors/openoffice-1.0/files/patch-stlport+makefile.mk
@@ -1,32 +1,36 @@
---- ../stlport/makefile.mk.orig	Tue Mar 19 15:49:17 2002
-+++ ../stlport/makefile.mk	Tue May 28 23:03:45 2002
-@@ -106,11 +106,29 @@
+--- ../stlport/makefile.mk.orig	Tue Jan  7 13:13:37 2003
++++ ../stlport/makefile.mk	Sat Jan 18 13:01:53 2003
+@@ -98,16 +98,28 @@
  
  .IF "$(COM)"=="GCC"
  .IF "$(COMID)"=="gcc3"
-+
-+.IF "$(OS)"=="FREEBSD"
-+BUILD_FLAGS=-f gcc-3.0-freebsd.mak
+-BUILD_FLAGS=-f gcc-3.0.mak
++.IF # "$(OS)"=="FREEBSD"
++    BUILD_FLAGS=-f gcc-3.0-freebsd.mak
 +.ELSE
- BUILD_FLAGS=-f gcc-3.0.mak
++    BUILD_FLAGS=-f gcc-3.0.mak
 +.ENDIF
-+
  .ELSE # "$(COMID)"=="gcc3"
-+
-+.IF "$(OS)"=="FREEBSD"
-+BUILD_FLAGS=-f gcc-freebsd.mak
+-# MacOS X/Darwin need a special makefile
++# FreeBSD / MacOS X/Darwin need a special makefile
+ .IF "$(OS)"=="MACOSX"
+     BUILD_FLAGS=-f gcc-apple-macosx.mak
+-.ELSE # "$(OS)"=="MACOSX"
++.ELSIF # "$(OS)"=="FREEBSD"
++    BUILD_FLAGS=-f gcc-freebsd.mak
 +.ELSE
- BUILD_FLAGS=-f gcc.mak
-+.ENDIF
-+
+     BUILD_FLAGS=-f gcc.mak
+-.ENDIF # "$(OS)"=="MACOSX"
++.ENDIF # "$(OS)"=="MACOSX || $(OS)"=="FREEBSD"
  .ENDIF # "$(COMID)"=="gcc3"
+-BUILD_ACTION=make
 +
 +.IF "$(OS)"=="FREEBSD"
-+BUILD_ACTION=gmake
++    BUILD_ACTION=gmake
 +.ELSE
- BUILD_ACTION=make
++    BUILD_ACTION=make
 +.ENDIF
 +
+ # build in parallel
+ BUILD_FLAGS+= -j$(MAXPROCESS)
  .ENDIF
- 
- .IF "$(COM)"=="C52"
--- editors/openoffice.org-1.0/files/patch-product+settings+settings.mk
+++ editors/openoffice.org-1.0/files/patch-product+settings+settings.mk
@@ -1,26 +1,75 @@
---- ../product/settings/settings.mk.orig	Wed May 29 00:16:16 2002
-+++ ../product/settings/settings.mk	Wed May 29 00:18:00 2002
-@@ -83,9 +83,8 @@
+--- ../product/settings/settings.mk.orig	Sat Jan 18 18:23:17 2003
++++ ../product/settings/settings.mk	Sat Jan 18 18:37:12 2003
+@@ -225,3 +226,72 @@
  
- OS=SOLARIS
- PS=/
--CC=CC
--LINK=CC
--LIB=CC
+ endif
+ 
++ifeq "$(PLATFORM)" "FreeBSD"
++# Settings for Linux using gcc compiler
++
++OS=LINUX
++PLATFORM=linux
++PS=/
 +LINK=$(CC)
 +LIB=$(CC)
- ECHO=@echo
- MKDIR=mkdir -p
- CAT=cat
-@@ -136,9 +135,8 @@
- OS=LINUX
- PLATFORM=linux
- PS=/
--CC=gcc
--LINK=gcc
--LIB=gcc
-+LINK=$(CC)
-+LIB=$(CC)
- ECHO=@echo
- MKDIR=mkdir -p
- CAT=cat
++ECHO=@echo
++MKDIR=mkdir -p
++CAT=cat
++OBJ_EXT=o
++SHAREDLIB_EXT=so
++SHAREDLIB_PRE=lib
++SHAREDLIB_OUT=$(OUT_LIB)
++PACKAGE_LIB_DIR=linux_x86.plt
++    
++GCC_VERSION=$(shell $(CC) -dumpversion)
++
++ifeq "$(shell echo $(GCC_VERSION) | cut -c 1)" "3"
++COMID=gcc3
++CPPU_ENV=gcc3
++else
++COMID=GCC
++CPPU_ENV=gcc2
++endif
++
++OSEP=\<
++CSEP=\>
++    
++DEL=rm -f
++DELRECURSIVE=rm -rf
++COPY=cp
++URLPREFIX=file://
++
++# Include UDK version numbers
++include $(PRJ)/include/udkversion.mk
++
++SALLIB=-lsal
++CPPULIB=-lcppu
++CPPUHELPERLIB=-lcppuhelper$(COMID)
++SALHELPERLIB=-lsalhelper$(COMID)
++STLPORTLIB=-lstlport_gcc
++
++EMPTYSTRING=
++PATH_SEPARATOR=:
++
++# -O is necessary for inlining (see gcc documentation)
++ifeq "$(DEBUG)" "yes"
++CC_FLAGS=-c -g
++else
++CC_FLAGS=-c -O
++endif
++SDK_JAVA_INCLUDES = -I$(OO_SDK_JAVA_HOME)/include -I$(OO_SDK_JAVA_HOME)/include/linux
++CC_INCLUDES=-I. -I/usr/include -I$(OUT)/inc/examples -I$(PRJ)/include
++STL_INCLUDES=-I$(OO_STLPORT_HOME)/stlport
++CC_DEFINES=-DUNX -DGCC -DLINUX -DCPPU_ENV=$(CPPU_ENV)
++
++# define for used compiler necessary for UNO
++#-DCPPU_ENV=gcc2 -- gcc 2.91/2.95
++#-DCPPU_ENV=gcc3 -- gcc3 3.0
++
++CC_OUTPUT_SWITCH=-o
++
++LIBRARY_LINK_FLAGS=-shared
++EXE_LINK_FLAGS=-Wl -export-dynamic
++LINK_LIBS=-L$(OUT)/lib -L$(PRJ)/$(PLATFORM)/lib -L$(OFFICE_PROGRAM_PATH)
++
++endif
