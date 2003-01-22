738a2ee53add6427c9a5e807fdb068bec219cab2
Max Khon <fjoe@FreeBSD.org>
2003-01-22 15:36:23 +0000

    Fix compiling with jasper 1.600.0 (and later) while retaining ability to compile with earlier jasper versions.

---
+++ graphics/ImageMagick/files/patch-coders::jp2.c
@@ -0,0 +1,18 @@
+--- coders/jp2.c.orig	Sat Oct 12 23:41:32 2002
++++ coders/jp2.c	Wed Jan 22 21:11:31 2003
+@@ -498,6 +498,15 @@
+ }
+ 
+ #if defined(HasJP2)
++
++#ifndef JAS_IMAGE_CM_GRAY
++#define JAS_IMAGE_CM_GRAY JAS_IMAGE_CS_GRAY
++#endif
++
++#ifndef JAS_IMAGE_CM_RGB
++#define JAS_IMAGE_CM_RGB JAS_IMAGE_CS_RGB
++#endif
++
+ /*
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ %                                                                             %
