61c2a9eb930ad1ad159b633f7c0d9a6f28dfa7e2
Martin Blapp <mbr@FreeBSD.org>
2003-01-22 14:55:22 +0000

    STABLE has still no iswspace(). Add define for it. No change for > 5.0.

--- editors/openoffice/files/patch-setup2+mow+source+loader+loader.c
+++ editors/openoffice/files/patch-setup2+mow+source+loader+loader.c
@@ -1,17 +1,30 @@
---- ../setup2/mow/source/loader/loader.c.orig	Thu Dec 13 17:44:58 2001
-+++ ../setup2/mow/source/loader/loader.c	Wed Apr  3 01:07:58 2002
-@@ -615,6 +615,14 @@
- "    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
+--- ../setup2/mow/source/loader/loader.c.orig	Thu Oct 31 21:46:52 2002
++++ ../setup2/mow/source/loader/loader.c	Wed Jan 22 14:56:44 2003
+@@ -85,6 +85,12 @@
+ # include <sys/statvfs.h>
+ #endif
+ 
++#ifdef FREEBSD
++#if (OSVERSION < 500000)
++#define iswspace(c) ((c)==' ' || (c)=='\t' || (c)=='\n')
++#endif
++#endif
++
+ #include "bitmap"
+ #include "logo.xpm"
+ 
+@@ -650,6 +656,14 @@
  "    export LD_LIBRARY_PATH\n" \
  "    ;;\n" \
+ "  Linux)\n" \
++"    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
++"    export LD_LIBRARY_PATH\n" \
++"    ;;\n" \
 +"  FreeBSD)\n" \
 +"    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
 +"    export LD_LIBRARY_PATH\n" \
 +"    ;;\n" \
 +"  NetBSD)\n" \
-+"    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
-+"    export LD_LIBRARY_PATH\n" \
-+"    ;;\n" \
- "      *)\n" \
+ "    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
+ "    export LD_LIBRARY_PATH\n" \
  "    ;;\n" \
- "esac\n" \
