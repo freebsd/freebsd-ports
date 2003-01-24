a0f7fe90a025ffba298357bc66e1bfc3b77e6dd3
Eric Anholt <anholt@FreeBSD.org>
2003-01-24 04:14:16 +0000

    [1] Fix missing symbols in i810 driver [2] Fix xdm on Alpha by using NetBSD's version of Wraphelp.c [3] Report buttons 7 and when using sysmouse protocol [4] Fix missing symbols in r128 driver. [5] Reset /usr/X11R6/bin/X to Xwrapper-4 after install if Xwrapper-4 exists.
    PR:		[1] i386/44203
    		[2] ports/43664
    		[3] ports/43232

--- x11/XFree86-4-libraries/files/patch-mouse.c
+++ x11/XFree86-4-libraries/files/patch-mouse.c
@@ -1,11 +1,13 @@
---- programs/Xserver/hw/xfree86/input/mouse/mouse.c.orig	Wed Dec 19 08:05:22 2001
-+++ programs/Xserver/hw/xfree86/input/mouse/mouse.c	Fri Aug  2 15:35:01 2002
-@@ -1505,7 +1505,7 @@
+--- programs/Xserver/hw/xfree86/input/mouse/mouse.c.orig	Wed Dec 19 16:05:22 2001
++++ programs/Xserver/hw/xfree86/input/mouse/mouse.c	Thu Jan  9 22:48:18 2003
+@@ -1505,8 +1505,8 @@
  	    dy = - ((char)(pBuf[2]) + (char)(pBuf[4]));
  	    /* FreeBSD sysmouse sends additional data bytes */
  	    if (pMse->protoPara[4] >= 8) {
 -		dz = ((char)(pBuf[5] << 1) + (char)(pBuf[6] << 1)) / 2;
+-		buttons |= (int)(~pBuf[7] & 0x07) << 3;
 +		dz = ((signed char)(pBuf[5] << 1) + (signed char)(pBuf[6] << 1)) >> 1;
- 		buttons |= (int)(~pBuf[7] & 0x07) << 3;
++		buttons |= (int)(~pBuf[7] & 0x7f) << 3;
  	    }
  	    break;
+ 
