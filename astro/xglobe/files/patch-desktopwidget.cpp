9ea2b6115ca5238f5f080f8d7d220cf5f5439ee4
arved <arved@FreeBSD.org>
2003-01-25 22:29:56 +0000

    Update WWW. Use qt3 instead of qt1 Fix Build on CURRENT
    Approved by:	kris (maintainer)

---
+++ astro/xglobe/files/patch-desktopwidget.cpp
@@ -0,0 +1,10 @@
+--- desktopwidget.cpp.orig	Sun Jan 26 09:35:35 2003
++++ desktopwidget.cpp	Sun Jan 26 09:38:52 2003
+@@ -18,6 +18,7 @@
+  */
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include "desktopwidget.h"
+ 
+ DesktopWidget::DesktopWidget(QWidget *parent, const char *name)
