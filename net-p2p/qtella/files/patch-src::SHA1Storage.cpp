3432d86f03f43e90e4a5ce33126212399a3ed7c3
Sean Chittenden <seanc@FreeBSD.org>
2003-01-28 02:29:47 +0000

    People still use 4.X???  Whoa!
    Fix qtella for -STABLE users with hack to get around headers and types
    moving around in GCC.
    
    Submitted by:	maintainer && "Scott I. Remick" <siremick@yahoo.com>

--- net-p2p/qtella/files/patch-src::SHA1Storage.cpp
+++ net-p2p/qtella/files/patch-src::SHA1Storage.cpp
@@ -1,11 +1,5 @@
 --- src/SHA1Storage.cpp.orig	Fri Dec 20 14:06:10 2002
 +++ src/SHA1Storage.cpp	Fri Dec 20 14:13:03 2002
-@@ -1,4 +1,4 @@
--#include <iostream.h>
-+#include <iostream>
- #include <qdir.h>
- #include <qfile.h>
- #include <qtextstream.h>
 @@ -100,7 +100,7 @@
  QString *SHA1Storage::get(SharedFile *sf)
  { 
