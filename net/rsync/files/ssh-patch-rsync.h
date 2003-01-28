47d2e9ff10ff7d7af9a6a7d4c109404b1878ef76
obraun <obraun@FreeBSD.org>
2003-01-28 16:50:01 +0000

    Upgrade to 2.5.6. SSH-patches are no longer needed, using ./configure --with-rsh=ssh instead.
    Tested on sparc64 by:	Hiroki Sato <hrs@eos.ocn.ne.jp>

--- net/rsync/files/ssh-patch-rsync.h
+++
@@ -1,8 +0,0 @@
---- rsync.h.orig	Sat Aug 19 06:10:39 2000
-+++ rsync.h	Sat Apr  7 23:31:00 2001
-@@ -75,4 +75,5 @@
- #define RSYNC_RSH "rsh"
- #endif
-+#define RSYNC_SSH "ssh"
- 
- #include <sys/types.h>
