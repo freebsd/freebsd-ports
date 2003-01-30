a4f6471048770351ee7d1eb350e77dd6d4707c6e
nork <nork@FreeBSD.org>
2003-01-30 17:30:49 +0000

    o use daemon(3) instead of just fork(2) when daemonize. o make rc.d script more modern, accordingly. o Bump PORTREVISION.
    PR:		ports/27875
    Submitted by:	Takanori Saneto <sanewo@ba2.so-net.ne.jp>

--- audio/rplay/files/rplayd.sh
+++ audio/rplay/files/rplayd.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/${0##*/}\$"); then
     echo "$0: Cannot determine the PREFIX" >&2
     exit 1
 fi
 
 case "$1" in
 start)
-	[ -x ${PREFIX}/sbin/rplayd ] && ( ${PREFIX}/sbin/rplayd & ) && echo -n ' rplayd'
+	[ -x ${PREFIX}/sbin/rplayd ] && ${PREFIX}/sbin/rplayd && echo -n ' rplayd'
 	;;
 stop)
 	killall rplayd && echo -n ' rplayd'
 	;;
 *)
 	echo "Usage: `basename $0` {start|stop}" >&2
+	exit 64
 	;;
 esac
 
