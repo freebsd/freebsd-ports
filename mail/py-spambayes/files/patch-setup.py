5d63b0a57c4eb42ae2962dad91139c234590583b
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2003-02-03 04:13:32 +0000

    New port spambayes version 1.0.a.1: A Bayesian anti-spam filter written in Python

---
+++ mail/py-spambayes/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	Sat Feb  1 15:52:48 2003
++++ setup.py	Sat Feb  1 15:53:53 2003
+@@ -36,6 +36,7 @@
+            'hammiecli.py',
+            'hammiesrv.py',
+            'hammiefilter.py',
++           'mboxtrain.py',
+            'pop3proxy.py',
+            'proxytrainer.py',
+            'proxytee.py',
