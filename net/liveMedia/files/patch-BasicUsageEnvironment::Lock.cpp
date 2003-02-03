9c134d1ad05d17ee8c8960bdc28696d487acb582
nork <nork@FreeBSD.org>
2003-02-03 17:26:33 +0000

    Fix build on -CURRENT.  This patch is temporary fix. And reduce compiler warning for -CURRENT.
    Submitted by:	Anders Andersson <anders@hack.org>

---
+++ net/liveMedia/files/patch-BasicUsageEnvironment::Lock.cpp
@@ -0,0 +1,10 @@
+--- BasicUsageEnvironment/Lock.cpp.orig	Mon Feb  3 20:10:22 2003
++++ BasicUsageEnvironment/Lock.cpp	Tue Feb  4 01:55:14 2003
+@@ -19,6 +19,7 @@
+ 
+ #include "Lock.hh"
+ #include <signal.h>
++#include "NetCommon.h"
+ 
+ ///// Read-Write Locks /////
+ 
