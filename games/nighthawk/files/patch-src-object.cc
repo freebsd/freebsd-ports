559ce1ee1e50fe2079ef2cb09eb89d8957322de3
markp <markp@FreeBSD.org>
2003-02-04 13:47:16 +0000

    Fix build under 5.0
    Submitted by:	des

---
+++ games/nighthawk/files/patch-src-object.cc
@@ -0,0 +1,10 @@
+--- src/object.cc.orig	Wed Feb  5 00:17:43 2003
++++ src/object.cc	Wed Feb  5 00:17:59 2003
+@@ -14,6 +14,7 @@
+ extern "C" {
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <math.h>
+ #include <X11/Xlib.h>
+ #include <X11/keysym.h>
