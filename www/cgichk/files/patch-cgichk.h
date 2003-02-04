6e94bf3d7a28e032ff300ecd51ac8a957e654f6f
markp <markp@FreeBSD.org>
2003-02-04 14:05:41 +0000

    Fix build after REINPLACE change. Change MAINTAINER to ports.

---
+++ www/cgichk/files/patch-cgichk.h
@@ -0,0 +1,10 @@
+--- cgichk.h.orig	Wed Feb  5 00:36:57 2003
++++ cgichk.h	Wed Feb  5 00:37:05 2003
+@@ -19,6 +19,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <sys/types.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <netdb.h>
