c2659354a557e25b9b43815629f25f9a7db089c4
Edwin Groothuis <edwin@FreeBSD.org>
2003-02-04 04:14:57 +0000

    Maintainer Update misc/gkrellmbgchg
    	Updating port to version 0.0.4
    
    PR:		ports/47711
    Submitted by:	Chris Stuart <firewolf@lightningfire.net>

---
+++ misc/gkrellmbgchg/files/patch-gkrellmbgchg.h
@@ -0,0 +1,11 @@
+--- gkrellmbgchg.h.orig	Tue Jan 21 22:27:41 2003
++++ gkrellmbgchg.h	Tue Jan 21 22:27:56 2003
+@@ -6,7 +6,7 @@
+ #define CONFIG_NAME "Background Changer"
+ #define CONFIG_KEYWORD "bgchg"
+ #define STYLE_NAME "bgchg"
+-#define GKRELLMBGCHG_VERSION "0.0.3"
++#define GKRELLMBGCHG_VERSION "0.0.4"
+ 
+ #ifdef GRAVITY
+ #define PLACEMENT (MON_UPTIME | GRAVITY(12))
