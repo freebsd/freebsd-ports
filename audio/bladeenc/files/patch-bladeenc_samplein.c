156290e063dd792a38fb2631313f381faf9181e8
Dirk Froemberg <dirk@FreeBSD.org>
2003-02-05 19:23:54 +0000

    Fix prototypes for myFseek(). Otherwise could lead to code execution during encoding.
    Submitted by:	jedgar
    Inspired by:	http://www.pivx.com/luigi/adv/blade942-adv.txt

--- audio/bladeenc/files/patch-bladeenc_samplein.c
+++ audio/bladeenc/files/patch-bladeenc_samplein.c
@@ -1,5 +1,5 @@
 --- bladeenc/samplein.c.orig	Tue May  8 14:11:53 2001
-+++ bladeenc/samplein.c	Thu Sep 19 10:25:54 2002
++++ bladeenc/samplein.c	Wed Feb  5 20:18:03 2003
 @@ -31,6 +31,7 @@
  #include	<stdio.h>
  #include	<stdlib.h>
@@ -8,3 +8,21 @@
  
  #include	"system.h"
  #include	"samplein.h"
+@@ -75,7 +76,7 @@
+ static	int				initAIFF (SI_Stream *psInfo);
+ static	uint			readAIFFSamples (SI_Stream *psInfo, int nSamples, short *wpSamples);
+ 
+-static	int				myFseek (FILE *fp, int offset);
++static	int				myFseek (FILE *fp, unsigned int offset);
+ 
+ 
+ 
+@@ -613,7 +614,7 @@
+ 
+ /* We can't use the real fseek() since you can't seek in a stream (stdin) */
+ 
+-static int					myFseek (FILE *fp, int offset)
++static int					myFseek (FILE *fp, unsigned int offset)
+ {
+ 	char					dummy[256];
+ 
