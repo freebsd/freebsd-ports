d6d5c6b3706ccb17db35ac5adf9a5b36887bf228
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-02-05 08:33:53 +0000

    Install the modules .la files.  This is one of the rare exceptions where this is required to get the application working.

--- deskutils/gnucash/files/patch-ltmain.sh
+++ deskutils/gnucash/files/patch-ltmain.sh
@@ -27,16 +27,3 @@
  	  *)
  	    # Add libc to deplibs on all other systems if necessary.
  	    if test $build_libtool_need_lc = "yes"; then
-@@ -4286,10 +4297,12 @@
- 	fi
- 
- 	# Install the pseudo-library for information purposes.
-+	if /usr/bin/false; then
- 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
- 	instname="$dir/$name"i
- 	$show "$install_prog $instname $destdir/$name"
- 	$run eval "$install_prog $instname $destdir/$name" || exit $?
-+	fi
- 
- 	# Maybe install the static library, too.
- 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
