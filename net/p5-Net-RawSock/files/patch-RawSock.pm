a8ed3a568be44d8f95a6ab0893c7b236e3606d16
edwin <edwin@FreeBSD.org>
2003-02-05 02:37:04 +0000

    New port: net/p5-Net-RawSock
    	Net::RawSock provides a base function to send raw IP datagrams
    	from Perl. The raw IP datagram is packed into a buffer and
    	written on the network layer with the write_ip() function.
    
    PR:		ports/47868
    Submitted by:	Nicolas Jombart <ecu@ipv42.net>

---
+++ net/p5-Net-RawSock/files/patch-RawSock.pm
@@ -0,0 +1,11 @@
+--- RawSock.pm.orig	Mon Feb  3 14:28:08 2003
++++ RawSock.pm	Mon Feb  3 14:28:30 2003
+@@ -41,7 +41,7 @@
+ 
+ =head1 AUTHOR
+ 
+-Stephane Aubert E<lt>Stephane.Aubert@hsc-labs.frE<gt>
++Stephane Aubert E<lt>Stephane.Aubert@hsc-labs.comE<gt>
+ 
+ =cut
+ 
