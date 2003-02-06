7e9a644f3a1a85a10041a0b5e8457235c470b319
marcus <marcus@FreeBSD.org>
2003-02-06 00:34:57 +0000

    Update to 2.5.2.

--- textproc/libxml2/files/patch-xmlcatalog.c
+++ textproc/libxml2/files/patch-xmlcatalog.c
@@ -1,11 +1,11 @@
---- xmlcatalog.c.orig	Mon Jun  3 15:30:00 2002
-+++ xmlcatalog.c	Mon Jun  3 15:30:17 2002
-@@ -41,7 +41,7 @@
- 
+--- xmlcatalog.c.orig	Sun Jan 26 14:45:48 2003
++++ xmlcatalog.c	Wed Feb  5 19:18:49 2003
+@@ -42,7 +42,7 @@
  #ifdef LIBXML_CATALOG_ENABLED
  
+ #ifndef XML_SGML_DEFAULT_CATALOG
 -#define XML_SGML_DEFAULT_CATALOG "/etc/sgml/catalog"
 +#define XML_SGML_DEFAULT_CATALOG "%%LOCALBASE%%/share/sgml/catalog"
+ #endif
  
  /************************************************************************
-  * 									*
