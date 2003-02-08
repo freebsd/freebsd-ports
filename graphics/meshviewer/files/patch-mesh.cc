2ea36c10d8d290571bae8de8b3806bd022bf9bba
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-02-08 10:27:24 +0000

    add meshviewer 0.1.2 Mesh Viewer

---
+++ graphics/meshviewer/files/patch-mesh.cc
@@ -0,0 +1,11 @@
+--- mesh/mesh.cc.orig	Sat Feb  8 18:14:00 2003
++++ mesh/mesh.cc	Sat Feb  8 18:14:11 2003
+@@ -9,7 +9,7 @@
+ #include <set>
+ 
+ // for MAXFLOAT
+-#include <values.h>
++#include <limits.h>
+ 
+ #include "mesh.h"
+ 
