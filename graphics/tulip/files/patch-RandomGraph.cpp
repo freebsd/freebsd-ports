db2f1d53554ddedcab7477982dcb889b3a151be8
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-02-08 15:09:40 +0000

    add tulip 1.2.2 A system dedicated to the visualization of huge graphs

---
+++ graphics/tulip/files/patch-RandomGraph.cpp
@@ -0,0 +1,7 @@
+--- plugins/import/RandomGraph.cpp.orig	Fri Jan 10 12:57:54 2003
++++ plugins/import/RandomGraph.cpp	Fri Jan 10 12:58:04 2003
+@@ -1,3 +1,4 @@
++#include <sys/types.h>
+ #include <sys/timeb.h>
+ #include<qinputdialog.h>
+ #include "TulipPlugin.h"
---
+++ graphics/tulip/files/patch-RandomSimpleGraph.cpp
@@ -0,0 +1,7 @@
+--- plugins/import/RandomSimpleGraph.cpp.orig	Fri Jan 10 12:59:47 2003
++++ plugins/import/RandomSimpleGraph.cpp	Fri Jan 10 12:59:59 2003
+@@ -1,3 +1,4 @@
++#include <sys/types.h>
+ #include <sys/timeb.h>
+ #include<qinputdialog.h>
+ #include "TulipPlugin.h"
