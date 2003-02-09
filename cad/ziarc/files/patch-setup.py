9a27501579d6242814fac64f77727d6b3a437fb2
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-02-09 17:11:07 +0000

    add ziarc 20020729 ZiaRC is a Resistor Calculator

---
+++ cad/ziarc/files/patch-setup.py
@@ -0,0 +1,9 @@
+--- setup.py.orig	Sat Feb  8 00:14:36 2003
++++ setup.py	Sat Feb  8 00:14:41 2003
+@@ -31,6 +31,5 @@
+ 
+ from distutils.core import setup
+ import glob
+-import py2exe
+ 
+ setup(name="main_ziarc.py", scripts=["main_ziarc.py"],data_files=[("icons",glob.glob("icons\\*.*"))],)
