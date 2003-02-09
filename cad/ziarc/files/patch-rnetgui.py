842c78a4e797193c598d05b2692dcc191ea9598f
ijliao <ijliao@FreeBSD.org>
2003-02-09 17:11:07 +0000

    add ziarc 20020729 ZiaRC is a Resistor Calculator

---
+++ cad/ziarc/files/patch-rnetgui.py
@@ -0,0 +1,20 @@
+--- rnetgui.py.orig	Mon Feb 10 01:00:29 2003
++++ rnetgui.py	Mon Feb 10 01:03:25 2003
+@@ -765,7 +765,7 @@
+ 
+ 	for fnam, tg, tool, help, id, sep in ziplist:
+ 
+-	    path=os.path.join(homepath,"icons",fnam)+".png"
++	    path=os.path.join("%%DATADIR%%","icons",fnam)+".png"
+ 
+ 	    bitmap=wxBitmapFromImage(wxImage(path, wxBITMAP_TYPE_PNG))
+ 	    self.tb.AddTool(id,bitmap,wxNullBitmap, tg, -1, tool, help)
+@@ -958,7 +958,7 @@
+ 
+     def About(self, event) : 
+ 
+-	path=os.path.join(homepath,"icons","ziarc.png")
++	path=os.path.join("%%DATADIR%%","icons","ziarc.png")
+ 	bitmap=wxBitmapFromImage(wxImage(path, wxBITMAP_TYPE_PNG))
+  
+         about=MyAbout(self, bitmap)
