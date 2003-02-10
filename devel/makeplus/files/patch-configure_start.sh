e4fd971438220cc8da321f2dfb451f99e402083e
alane <alane@FreeBSD.org>
2003-02-10 04:41:15 +0000

    sorry, did not realize until i got to devel/c2lib how bad it was

--- devel/makeplus/files/patch-configure_start.sh
+++ devel/makeplus/files/patch-configure_start.sh
@@ -1,19 +1,34 @@
---- configure_start.sh.orig	Sun Feb  9 22:27:05 2003
-+++ configure_start.sh	Sun Feb  9 22:27:43 2003
-@@ -45,7 +45,7 @@
+--- configure_start.sh.orig	Sat Feb  8 05:00:52 2003
++++ configure_start.sh	Sun Feb  9 22:49:19 2003
+@@ -45,16 +45,17 @@
  pkgetcdir	= $pkgetcdir
  pkglibdir	= $pkglibdir
  pkgincludedir	= $pkgincludedir
 -manprefix	= $datadir
-+manprefix	= $prefix
- mandir		= $mandir
- man1dir		= $man1dir
- man2dir		= $man2dir
-@@ -57,4 +57,5 @@
- man8dir		= $man8dir
- manndir		= $manndir
- manldir		= $manldir
+-mandir		= $mandir
+-man1dir		= $man1dir
+-man2dir		= $man2dir
+-man3dir		= $man3dir
+-man4dir		= $man4dir
+-man5dir		= $man5dir
+-man6dir		= $man6dir
+-man7dir		= $man7dir
+-man8dir		= $man8dir
+-manndir		= $manndir
+-manldir		= $manldir
 -EOF
 \ No newline at end of file
++manprefix	= \$(prefix)
++mandir		= \$(manprefix)/man
++man1dir		= \$(mandir)/man1
++man2dir		= \$(mandir)/man2
++man3dir		= \$(mandir)/man3
++man4dir		= \$(mandir)/man4
++man5dir		= \$(mandir)/man5
++man6dir		= \$(mandir)/man6
++man7dir		= \$(mandir)/man7
++man8dir		= \$(mandir)/man8
++manndir		= \$(mandir)/mann
++manldir		= \$(mandir)/manl
 +EOF
 +
