6ce96ae4b4c79fc92f08eee1c508a77b60ab371c
Patrick Li <pat@FreeBSD.org>
2003-02-14 20:44:23 +0000

    Update to 2.5.c

--- games/qstat/files/patch-config.c
+++ games/qstat/files/patch-config.c
@@ -1,15 +1,6 @@
---- config.c.orig	Tue Aug  6 21:13:34 2002
-+++ config.c	Sun Sep 22 09:00:30 2002
-@@ -157,7 +157,7 @@
- 
-     var= getenv( "HOME");
-     if ( var != NULL && var[0] != '\0')  {
--	sprintf( path, "%s/%s", var, HOME_CONFIG_FILE);
-+	snprintf( path, (sizeof(path) -1),"%s/%s", var, HOME_CONFIG_FILE);
- 	rc= try_load_config_file( path, 0);
- 	if ( rc == 0 || rc == -1)
- 	    return rc;
-@@ -197,7 +197,7 @@
+--- config.c.orig	Fri Nov  8 19:06:04 2002
++++ config.c	Fri Feb 14 13:07:09 2003
+@@ -204,7 +204,7 @@
      filename= getenv( "HOME");
      if ( filename != NULL && filename[0] != '\0')  {
  	char path[1024];
