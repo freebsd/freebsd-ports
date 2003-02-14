810344f2bbbf07d043951085f642deea917a2086
Christian Weisgerber <naddy@FreeBSD.org>
2003-02-14 15:24:10 +0000

    Import dvorak7min 1.6.
    Dvorak7min is an ncurses based dvorak typing tutor for those of you
    trying to get fluent in this keyboard layout.
    
    PR:		47706
    Submitted by:	Li-lun Wang <llwang@infor.org>

---
+++ misc/dvorak7min/files/patch-dvorak7min.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- dvorak7min.c.orig	Wed Feb 12 23:15:04 2003
++++ dvorak7min.c	Wed Feb 12 23:15:32 2003
+@@ -41,7 +41,7 @@
+  *  
+  */
+ 
+-#include <strings.h>
++#include <string.h>
+ #include <stdlib.h>
+ #include <ctype.h>
+ #include <time.h>
