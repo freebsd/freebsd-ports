bdfac6efe28a1779cbfcbb09872d8aa903ed1eb9
Tilman Keskinoz <arved@FreeBSD.org>
2003-02-15 22:14:58 +0000

    Don't use qt-Legacy Headers.
    PR:		48304
    Submitted by:	 Michael Collette <metrol@metrol.net>

---
+++ databases/unixODBC/files/patch-DataManager::classISQL.h
@@ -0,0 +1,11 @@
+--- DataManager/classISQL.h.orig	Thu Dec 13 04:55:58 2001
++++ DataManager/classISQL.h	Sat Feb 15 03:22:44 2003
+@@ -17,7 +17,7 @@
+ #include <qcombo.h>
+ #include <qslider.h>
+ #include <qtabbar.h>
+-#include <qmlined.h>
++#include <qtextedit.h>
+ #include <qfile.h>
+ #include <qfiledialog.h>
+ #include <qtextstream.h>
