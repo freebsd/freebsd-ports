5c8d5bbef54b498a8a0a3ca91a4b5ff72f4015b1
Christian Weisgerber <naddy@FreeBSD.org>
2003-02-16 15:39:00 +0000

    - Use MASTER_SITE_SOURCEFORGE - Support install macros - Utilize NOPORTDOCS
    PR:		47490
    Submitted by:	Ports Fury

---
+++ mail/muttzilla/files/patch-mzbase.c
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- mzbase.c.orig	Sun Feb 16 16:35:13 2003
++++ mzbase.c	Sun Feb 16 16:35:28 2003
+@@ -25,6 +25,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <sys/stat.h>
+ #include <unistd.h>
+ #include <fcntl.h>
