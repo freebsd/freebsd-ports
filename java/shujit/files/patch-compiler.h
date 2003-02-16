7f05c36ddb2f9f17fae44c01a15d8f8dd30646ed
Maxim Sobolev <sobomax@FreeBSD.org>
2003-02-16 08:33:22 +0000

    - Update to 0.7.4; - use USE_JAVA.
    Submitted by:	des (USE_JAVA)

--- java/shujit/files/patch-compiler.h
+++ java/shujit/files/patch-compiler.h
@@ -1,21 +1,14 @@
 
 $FreeBSD$
 
---- compiler.h	2002/04/26 17:13:45	1.1
-+++ compiler.h	2002/04/26 17:17:19
-@@ -61,13 +61,13 @@
+--- compiler.h	2003/02/16 00:04:39	1.1
++++ compiler.h	2003/02/16 00:05:09
+@@ -60,7 +60,7 @@
+ 
  // Additional type definition
  
- #if !(defined(_ILP32) || defined(_LP64))
--#  ifndef _UINT16_T
-+#  if !defined(_UINT16_T) && !defined(_UINT16_T_DECLARED)
+-#if !(defined(_ILP32) || defined(_LP64) || defined(_SYS_INTTYPES_H_) /* for FreeBSD */)
++#if !(defined(_ILP32) || defined(_LP64) || !defined(_SYS_INTTYPES_H_) /* for FreeBSD */)
+ #  ifndef _UINT16_T
  #    define _UINT16_T
  typedef unsigned short uint16_t;
- #    endif	// _UINT16_T
- 
- #  ifndef __BIT_TYPES_DEFINED__
--#    ifndef _INT16_T
-+#    if !defined(_INT16_T) && !defined(_INT16_T_DECLARED)
- #      define _INT16_T
- typedef short int16_t;
- #    endif	// _INT16_T
