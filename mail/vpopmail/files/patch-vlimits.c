c8ea705c5e174f8f0554357c9bf567c447bea1c8
Peter Pentchev <roam@FreeBSD.org>
2003-02-17 12:21:10 +0000

    Update to vpopmail-5.3.16: - WITH_MYSQL_LIMITS works now, move it where users can actually see it   and remove the BROKEN mark; - remove the vmysql.h patch that defined the 'limits' table creation -   it was fixed in the meantime; - remove the annoying configure script check for id == 0.

--- mail/vpopmail/files/patch-vlimits.c
+++ mail/vpopmail/files/patch-vlimits.c
@@ -1,27 +1,27 @@
---- vlimits.c.orig	Sat Nov  2 22:39:03 2002
-+++ vlimits.c	Sat Nov  2 22:41:10 2002
-@@ -49,7 +49,7 @@
+--- vlimits.c.old	Fri Feb 14 14:44:38 2003
++++ vlimits.c	Fri Feb 14 14:45:21 2003
+@@ -46,7 +46,7 @@
  
      /* get filename */
-     vget_assign(domain, dir, sizeof(dir), &uid, &gid);
+     vget_assign(mydomain, dir, sizeof(dir), &uid, &gid);
 -    strncat(dir, "/.qmailadmin-limits", sizeof(dir));
 +    strncat(dir, "/.qmailadmin-limits", sizeof(dir) - strlen(dir) - 1);
  
      /* open file */
      if ((fs = fopen(dir, "r")) != NULL) {
-@@ -143,7 +143,7 @@
+@@ -209,7 +209,7 @@
  
      /* get filename */
-     vget_assign(domain, dir, sizeof(dir), &uid, &gid);
+     vget_assign(mydomain, dir, sizeof(dir), &uid, &gid);
 -    strncat(dir, "/.qmailadmin-limits", sizeof(dir));
 +    strncat(dir, "/.qmailadmin-limits", sizeof(dir) - strlen(dir) - 1);
  
      /* open file */
      if ((fs = fopen(dir, "w+")) != NULL) {
-@@ -183,7 +183,7 @@
+@@ -265,7 +265,7 @@
  
      /* get filename */
-     vget_assign(domain, dir, sizeof(dir), &uid, &gid);
+     vget_assign(mydomain, dir, sizeof(dir), &uid, &gid);
 -    strncat(dir, "/.qmailadmin-limits", sizeof(dir));
 +    strncat(dir, "/.qmailadmin-limits", sizeof(dir) - strlen(dir) - 1);
      return unlink(dir);
