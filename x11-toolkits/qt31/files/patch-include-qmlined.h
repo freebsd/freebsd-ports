3437b2f114304aff411252735b69fe758778bd2e
lioux <lioux@FreeBSD.org>
2003-02-17 20:36:21 +0000

    o Fix a include inside a header file. It was pointing to a no longer   existent file [1]. This fixes unixODBC port GUI build. o qt applications should be built with thread support per default.   Thus, add this to default qmake.conf for FreeBSD. [2] o Bump PORTREVISION due to aforementioned
    PR:		48303 [1]
    Submitted by:	Michael Collette <metrol@metrol.net> [1],
    		arved [2]
    Reviewed by:	Lauri Watts <lauri@kde.org> (FreeBSD KDE Team)

---
+++ x11-toolkits/qt31/files/patch-include-qmlined.h
@@ -0,0 +1,9 @@
+--- include/qmlined.h.orig	Sat Feb 15 02:26:37 2003
++++ include/qmlined.h	Sat Feb 15 02:27:45 2003
+@@ -22,5 +22,5 @@
+ *****************************************************************************/
+ #ifndef QMLINED_H
+ #define QMLINED_H
+-#include "qtmultilineedit.h"
++#include "qtextedit.h"
+ #endif
