1b5279be82fb930a15af1b21e11de2824216cb2d
nork <nork@FreeBSD.org>
2003-02-19 17:04:37 +0000

    Update to 1.3b.
    PR:		ports/48462
    Submitted by:	Yoichi ASAI <yatt@luna2.org> (maintainer)

--- japanese/mozilla-jlp-devel/files/mozilla.sh
+++ japanese/mozilla-jlp-devel/files/mozilla.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 
-LANG=@LOCALENAME@; export LANG
-@LIBXPG4@
+LANG=%%LOCALENAME%%; export LANG
+%%LIBXPG4%%
 
-cd @PREFIX@/lib/mozilla-devel
-exec ./mozilla "$@"
+MOZILLA_DIR="%%PREFIX%%/lib/%%MOZILLA%%"
+MOZILLA_EXEC="mozilla"
+LOCATION='new-tab'
+
+cd $MOZILLA_DIR                                     || exit 1
+
+case $1 in
+    -mail)
+    	REMOTE_COMMAND="xfeDoCommand (openInbox)"
+	;;
+    -*)
+    	exec ./$MOZILLA_EXEC "$@"
+	;;
+    *)
+    	REMOTE_COMMAND="openURL($@, $LOCATION)"
+	;;
+esac
+    
+# process found
+./$MOZILLA_EXEC -remote "ping()"                    &&
+./$MOZILLA_EXEC -remote "$REMOTE_COMMAND"           && exit 0
+
+# no existing process
+exec ./$MOZILLA_EXEC "$@"
+
