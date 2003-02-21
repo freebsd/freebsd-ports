75e0e76533e0eeaaf9f1e467f87f2ea235b8d664
Greg Lewis <glewis@FreeBSD.org>
2003-02-21 16:12:35 +0000

    . Commit the version of these patches with the correct paths.  Grrr.

--- java/jdk12/files/patch-HAE_API_bsd.c
+++ java/jdk12/files/patch-HAE_API_bsd.c
@@ -1,10 +1,7 @@
-Index: src/bsd/native/sun/audio/HAE_API_bsd.c
-===================================================================
-RCS file: /home/Java/CVS/JDK2/javasrc/src/bsd/native/sun/audio/HAE_API_bsd.c,v
-retrieving revision 1.7
-diff -u -r1.7 HAE_API_bsd.c
---- src/bsd/native/sun/audio/HAE_API_bsd.c	11 Sep 2001 05:01:58 -0000	1.7
-+++ src/bsd/native/sun/audio/HAE_API_bsd.c	20 Feb 2003 07:10:48 -0000
+$FreeBSD$
+
+--- ../../src/bsd/native/sun/audio/HAE_API_bsd.c	11 Sep 2001 05:01:58 -0000	1.7
++++ ../../src/bsd/native/sun/audio/HAE_API_bsd.c	20 Feb 2003 07:10:48 -0000
 @@ -51,7 +51,7 @@
  #include <sys/wait.h>
  #include <errno.h>
