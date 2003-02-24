e6c025efa2900686166baa47782a68b26fd41405
anders <anders@FreeBSD.org>
2003-02-24 00:21:01 +0000

    Add dv2jpg, a tool to convert type-2 DV codec-encoded AVI streams to mjpeg-encoded AVI.

---
+++ multimedia/dv2jpg/files/patch-avijoin.c
@@ -0,0 +1,9 @@
+--- avijoin.c.orig	Sun Jun 24 02:04:47 2001
++++ avijoin.c	Sun Feb 16 01:24:49 2003
+@@ -1,5 +1,5 @@
+ #include <stdio.h>
+-#include "/opt/vidpb/lib/avilib.h"
++#include "avilib.h"
+ 
+ #define MAX_FILES 255
+ #define OURBUFSIZ 4096000
