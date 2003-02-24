967c7b5392bd5528975eb4c19f1380c064260888
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2003-02-24 14:13:22 +0000

    Sync with libxine update. Add the extra patch to fix build error on 5.0-RELEASE.

---
+++ multimedia/xine/files/extra-patch-src:xitk:common.h
@@ -0,0 +1,11 @@
+--- src/xitk/common.h.orig	Mon Feb 24 22:39:16 2003
++++ src/xitk/common.h	Mon Feb 24 22:39:46 2003
+@@ -31,6 +31,8 @@
+ #include <xine.h>
+ #include <xine/xineutils.h>
+ 
++#define ushort unsigned short
++
+ #include "Imlib-light/Imlib.h"
+ 
+ #include "xitk.h"
