4c1439501fa98c6a2058f1576501a9a426564a16
edwin <edwin@FreeBSD.org>
2003-02-26 11:25:55 +0000

    security/cyrus-sasl: Bento fails to build libkerberos + Upgrade
    	Bento fails to proberly package the port on -CURRENT due
    	to -CURRENT no longer has libdes.* as the des functions
    	have been moved to the libcrypto.* library.
    
    PR:		48442
    Submitted by:	Scot W. Hetzel <hetzels@westbend.net>

--- security/cyrus-sasl/files/pwcheck.sh
+++ security/cyrus-sasl/files/pwcheck.sh
@@ -3,64 +3,105 @@
 # $FreeBSD$
 #
 
-action=$1
-
-PREFIX=%%PREFIX%%
-
-# Suck in the configuration variables.
-if [ -z "${source_rc_confs_defined}" ]; then
-        if [ -r /etc/defaults/rc.conf ]; then
-                . /etc/defaults/rc.conf
-                source_rc_confs
-        elif [ -r /etc/rc.conf ]; then
-                . /etc/rc.conf
-        fi
-fi
-
-# The following sasl_pwcheck_* variables may be defined in rc.conf
+# PROVIDE: cyrus_pwcheck
+# REQUIRE: DAEMON
+# BEFORE: mail imap
+# KEYWORD: FreeBSD shutdown
 #
-# 	sasl_pwcheck_enable  -	Set to YES to enable pwcheck
-#				Default: %%ENABLEPWCHECK%%
+# NOTE for FreeBSD 5.0+:
+# If you want this script to start with the base rc scripts
+# move cyrus_pwcheck.sh to /etc/rc.d/cyrus_pwcheck
+
+# Define the following cyrus_pwcheck_* variables in one of the following:
+#       /etc/rc.conf
+#       /etc/rc.conf.d/pwcheck
+#       ${prefix}/etc/rc.conf.d/pwcheck
 #
-#	sasl_pwcheck_program -	Path to pwcheck program (pwcheck/pwcheck_pam)
-#				Default: ${PREFIX}/sbin/%%PWCHECK%%
+#       cyrus_pwcheck_enable  - Set to YES to enable pwcheck
+#				Default: %%ENABLE_PWCHECK%%
+#
+#       cyrus_pwcheck_program - Path to pwcheck program (pwcheck/pwcheck_pam)
+#				Default: ${prefix}/sbin/%%PWCHECK%%
 
-if [ -z "${sasl_pwcheck_enable}" ] ; then
-	sasl_pwcheck_enable=%%ENABLEPWCHECK%%
-fi
+prefix=%%PREFIX%%
 
-if [ -z "${sasl_pwcheck_program}" ]; then
-	sasl_pwcheck_program=${PREFIX}/sbin/%%PWCHECK%%
-fi
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
 
-rc=0
+	name="cyrus_pwcheck"
+	rcvar=`set_rcvar`
+	command=${prefix}/sbin/%%PWCHECK%%
+	pidfile="/var/run/pwcheck.pid"
 
-case "${sasl_pwcheck_enable}" in
-    [Yy][Ee][Ss])
-	case "${action}" in
+	# The below may be removed when load_local_rc_config is added to rc.subr
+	
+	if [ -f ${prefix}/etc/rc.conf.d/"$name" ]; then
+        	debug "Sourcing ${prefix}/etc/rc.conf.d/${name}"
+        	. ${prefix}/etc/rc.conf.d/"$name"
+	fi
 
-	    start)
-		if [ -x ${sasl_pwcheck_program} ] ; then
-		    ${sasl_pwcheck_program} && echo -n " pwcheck"
+	load_rc_config $name
+
+	if [ -z "${cyrus_pwcheck_enable}" ] ; then
+        	cyrus_pwcheck_enable=%%ENABLE_PWCHECK%%
+	fi
+
+	# The above may be removed when load_local_rc_config is added to rc.subr
+	#
+	# load_local_rc_config $name
+	run_rc_command "$1"
+else
+	# Suck in the configuration variables.
+	if [ -z "${source_rc_confs_defined}" ]; then
+		if [ -r /etc/defaults/rc.conf ]; then
+			. /etc/defaults/rc.conf
+			source_rc_confs
+		elif [ -r /etc/rc.conf ]; then
+			. /etc/rc.conf
 		fi
-		;;
+	fi
 
-	    stop)
-		if [ -r /var/run/pwcheck.pid ]; then
-		    kill `cat /var/run/pwcheck.pid` && echo -n " pwcheck"
-		    rm /var/run/pwcheck.pid
-		fi
-		;;
+	if [ -f ${prefix}/etc/rc.conf.d/cyrus_pwcheck" ]; then
+		. ${prefix}/etc/rc.conf.d/cyrus_pwcheck}"
+	fi
 
+	if [ -z "${cyrus_pwcheck_enable}" ] ; then
+		cyrus_pwcheck_enable=%%ENABLE_PWCHECK%%
+	fi
+
+	if [ -z "${cyrus_pwcheck_program}" ]; then
+		cyrus_pwcheck_program=${prefix}/sbin/%%PWCHECK%%
+	fi
+
+	rc=0
+
+	case "${cyrus_pwcheck_enable}" in
+	    [Yy][Ee][Ss])
+		case "${action}" in
+
+		    start)
+			if [ -x ${cyrus_pwcheck_program} ] ; then
+			    ${cyrus_pwcheck_program} && echo -n " cyrus_pwcheck"
+			fi
+			;;
+
+		    stop)
+			if [ -r /var/run/pwcheck.pid ]; then
+			    kill `cat /var/run/pwcheck.pid` && echo -n " cyrus_pwcheck"
+			    rm /var/run/pwcheck.pid
+			fi
+			;;
+
+		    *)
+			echo "usage: $0 {start|stop}" 1>&2
+			rc=64
+			;;
+		esac
+		;;
 	    *)
-		echo "usage: $0 {start|stop}" 1>&2
-		rc=64
+		rc=0
 		;;
 	esac
-	;;
-    *)
-	rc=0
-	;;
-esac
 
-exit $rc
+	exit $rc
+fi
--- security/cyrus-sasl/files/saslauthd.sh
+++ security/cyrus-sasl/files/saslauthd.sh
@@ -3,73 +3,124 @@
 # $FreeBSD$
 #
 
-action=$1
-
-PREFIX=%%PREFIX%%
-
-# Suck in the configuration variables.
-if [ -z "${source_rc_confs_defined}" ]; then
-        if [ -r /etc/defaults/rc.conf ]; then
-                . /etc/defaults/rc.conf
-                source_rc_confs
-        elif [ -r /etc/rc.conf ]; then
-                . /etc/rc.conf
-        fi
-fi
-
-# The following sasl_saslauthd_* variables may be defined in rc.conf
+# PROVIDE: saslauthd1
+# REQUIRE: DAEMON
+# BEFORE: mail imap
+# KEYWORD: FreeBSD shutdown
 #
-# 	sasl_saslauthd1_enable  - Set to YES to enable saslauthd
-#				 Default: %%ENABLESASLAUTHD%%
+# NOTE for FreeBSD 5.0+:
+# If you want this script to start with the base rc scripts
+# move saslauthd1.sh to /etc/rc.d/saslauthd1
+
+# Define the following saslauthd1_* variables in one of the following:
+#	/etc/rc.conf
+#	/etc/rc.conf.d/saslauthd1
+#	${prefix}/etc/rc.conf.d/saslauthd1
 #
-#	sasl_saslauthd1_program - Path to saslauthd program
-#				 Default: ${PREFIX}/sbin/saslauthd1
+#       saslauthd1_enable  - Set to YES to enable saslauthd
+#			     Default: %%ENABLE_SASLAUTHD%%
 #
-#	sasl_saslauthd1_flags   - Flags to saslauthd program
-#				 Default: -a pam
+#       saslauthd1_program - Path to saslauthd program
+#                            Default: ${prefix}/sbin/saslauthd1
+#
+#       saslauthd1_flags   - Flags to saslauthd program
+#                            Default: -a pam
 
-if [ -z "${sasl_saslauthd1_enable}" ]; then
-	sasl_saslauthd1_enable=%%ENABLESASLAUTHD%%
-fi
+prefix=%%PREFIX%%
 
-if [ -z "${sasl_saslauthd1_program}" ]; then
-	sasl_saslauthd1_program=${PREFIX}/sbin/saslauthd1
-fi
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
 
-if [ -z "${sasl_saslauthd1_flags}" ]; then
-	sasl_saslauthd1_flags="-a pam"
-fi
+	name="saslauthd1"
+	rcvar=`set_rcvar`
+	command="${prefix}/sbin/${name}"
+	pidfile="/var/state/${name}/mux.pid"
 
-rc=0
+	# The below may be removed when load_local_rc_config is added to rc.subr
 
-case "${sasl_saslauthd1_enable}" in
-    [Yy][Ee][Ss])
-	case "${action}" in
+	if [ -f ${prefix}/etc/rc.conf.d/"$name" ]; then
+		debug "Sourcing ${prefix}/etc/rc.conf.d/${name}"
+		. ${prefix}/etc/rc.conf.d/"$name"
+	fi
 
-	    start)
-		if [ -x ${sasl_saslauthd1_program} ] ; then
-		    ${sasl_saslauthd1_program} ${sasl_saslauthd1_flags} \
-			&& echo -n " saslauthd1"
+	load_rc_config $name
+
+	if [ -z "${saslauthd1_enable}" ]; then
+		saslauthd1_enable=%%ENABLE_SASLAUTHD%%
+	fi
+
+	# The above may be removed when load_local_rc_config is added to rc.subr
+	#
+	#load_local_rc_config $name
+	run_rc_command "$1"
+else
+	# Suck in the configuration variables.
+	if [ -z "${source_rc_confs_defined}" ]; then
+		if [ -r /etc/defaults/rc.conf ]; then
+			. /etc/defaults/rc.conf
+			source_rc_confs
+		elif [ -r /etc/rc.conf ]; then
+			. /etc/rc.conf
 		fi
-		;;
+	fi
 
-	    stop)
-		if [ -r /var/state/saslauthd1/mux.pid ]; then
-		    kill `cat /var/state/saslauthd1/mux.pid` && \
-			echo -n " saslauthd1"
-		    rm /var/state/saslauthd1/mux.pid
-		fi
-		;;
+	if [ -f ${prefix}/etc/rc.conf.d/saslauthd1 ]; then
+		. ${prefix}/etc/rc.conf.d/saslauthd1
+	fi
 
+	if [ -n "${sasl_saslauthd1_enable}" ]; then
+		echo "sasl_saslauthd1_enable has been depreciated, use saslauthd1_enable instead"
+		saslauthd1_enable=$sasl_saslauthd1_enable
+	fi
+
+	if [ -z "${saslauthd1_enable}" ]; then
+		saslauthd1_enable=%%ENABLE_SASLAUTHD%%
+	fi
+
+	if [ -z "${saslauthd1_program}" ]; then
+		saslauthd1_program=${prefix}/sbin/saslauthd1
+	fi
+
+	if [ -n "${sasl_saslauthd1_flags}" ]; then
+        	echo "sasl_saslauthd1_flags has been depreciated, use saslauthd1_flags instead"
+        	saslauthd1_flags=$sasl_saslauthd1_flags
+	fi
+
+	if [ -z "${saslauthd1_flags}" ]; then
+		saslauthd1_flags="-a pam"
+	fi
+
+	rc=0
+
+	case "${saslauthd1_enable}" in
+	    [Yy][Ee][Ss])
+		case "${1}" in
+
+		    start)
+			if [ -x ${saslauthd1_program} ] ; then
+			    ${saslauthd1_program} ${saslauthd1_flags} \
+				&& echo -n " saslauthd1"
+			fi
+			;;
+
+		    stop)
+			if [ -r /var/state/saslauthd1/mux.pid ]; then
+			    kill `cat /var/state/saslauthd1/mux.pid` && \
+				echo -n " saslauthd1"
+			    rm /var/state/saslauthd1/mux.pid
+			fi
+			;;
+
+		    *)
+			echo "usage: $0 {start|stop}" 1>&2
+			rc=64
+			;;
+		esac
+		;;
 	    *)
-		echo "usage: $0 {start|stop}" 1>&2
-		rc=64
+		rc=0
 		;;
 	esac
-	;;
-    *)
-	rc=0
-	;;
-esac
 
-exit $rc
+	exit $rc
+fi
