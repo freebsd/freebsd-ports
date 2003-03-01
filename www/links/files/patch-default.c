09aeb3ea5dd3ac3bc943ec52ec7208a3198529a3
Dmitry Sivachenko <demon@FreeBSD.org>
2003-03-01 19:13:14 +0000

    Update to the latest version (2.1.p9). Remove patch-width since it is useless without -dump option which is absent in links version2...
    Submitted by:	Thomas Seck <tmseck-lists@netcologne.de>

--- www/links/files/patch-default.c
+++ www/links/files/patch-default.c
@@ -1,11 +1,11 @@
---- default.c.orig	Sun May 19 20:12:20 2002
-+++ default.c	Sun May 19 20:12:23 2002
-@@ -267,7 +267,7 @@
+--- default.c.orig	Tue Feb 11 21:54:38 2003
++++ default.c	Sat Mar  1 16:03:31 2003
+@@ -300,7 +300,7 @@
  	get_system_name();
  	links_home = get_home(&first_use);
  	if (!links_home) {
 -		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have $HOME variable set correctly and that you have write permission to your home directory.\n\007");
-+		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have $HOME variable set correctly and that you have write permission to your home directory.\n");
++		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have set the $HOME variable correctly and that you have write permission to your home directory.\n");
  		sleep(3);
  		return;
  	}
--- www/links/files/patch-types.c
+++ www/links/files/patch-types.c
@@ -1,6 +1,7 @@
---- types.c.orig	Fri Apr 26 09:11:15 2002
-+++ types.c	Fri Apr 26 09:12:27 2002
-@@ -66,5 +66,6 @@
+--- types.c.orig	Sat Mar  1 19:06:39 2003
++++ types.c	Sat Mar  1 19:07:45 2003
+@@ -705,7 +705,8 @@
+ 		if (*ct == '.') ext = ct + 1;
  		else if (dir_sep(*ct)) ext = NULL;
  	if (ext) while (ext[extl] && !dir_sep(ext[extl]) && !end_of_dir(ext[extl])) extl++;
 -	if ((extl == 3 && !casecmp(ext, "htm", 3)) ||
@@ -8,3 +9,4 @@
 +	    (extl == 3 && !casecmp(ext, "htm", 3)) ||
  	    (extl == 4 && !casecmp(ext, "html", 4))) return stracpy("text/html");
  	foreach(e, extensions) if (is_in_list(e->ext, ext, extl)) return stracpy(e->ct);
+ 
