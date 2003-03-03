3e9654fb5db1ceacd66a0b456f628eaaf6ef3ae5
perky <perky@FreeBSD.org>
2003-03-03 07:31:30 +0000

    Update to 4.1.3

--- databases/py-bsddb3/files/patch-setup.py
+++ databases/py-bsddb3/files/patch-setup.py
@@ -1,9 +1,9 @@
---- setup.py.orig	Thu Jul 11 03:48:34 2002
-+++ setup.py	Thu Jul 11 03:51:34 2002
-@@ -68,8 +68,8 @@
-     # figure out from the base setting where the lib and .h are
-     if not incdir: incdir = os.path.join(BERKELEYDB_DIR, 'include')
-     if not libdir: libdir = os.path.join(BERKELEYDB_DIR, 'lib')
+--- setup.py.orig	Mon Feb  3 11:28:52 2003
++++ setup.py	Mon Mar  3 16:22:31 2003
+@@ -73,8 +73,8 @@
+         incdir = os.path.join(BERKELEYDB_DIR, 'include')
+     if not libdir:
+         libdir = os.path.join(BERKELEYDB_DIR, 'lib')
 -    if not '-ldb' in LIBS:
 -        libname = ['db']
 +    if not '-l%%DBVERSION%%' in LIBS:
@@ -11,8 +11,8 @@
      else:
          libname = []
      utils = []
-@@ -93,7 +93,7 @@
-     if not status and string.find(results, 'libdb.') >= 0:
+@@ -96,7 +96,7 @@
+     if not status and results.find('libdb.') >= 0:
          static = 1
  
 -    if static:
