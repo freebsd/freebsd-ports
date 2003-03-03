69eb318e59e8308d699106d4c76d9027aa36fbf0
znerd <znerd@FreeBSD.org>
2003-03-03 21:36:44 +0000

    Fixes: - unbreak with new gcc on -current ('using namespace std') - CFLAGS/env police - condense port's Makefile - install additional shell script - install documentation (honour NOPORTDOCS)
    PR:		48464
    Submitted by:	Volker Stolz <stolz@i2.informatik.rwth-aachen.de>
    Tested on:	-STABLE, -CURRENT

---
+++ java/jlint/files/patch-field_desc.hh
@@ -0,0 +1,13 @@
+--- field_desc.hh.orig	Wed Feb 19 17:20:15 2003
++++ field_desc.hh	Wed Feb 19 17:22:10 2003
+@@ -1,10 +1,7 @@
+ #ifndef FIELD_DESC_HH
+ #define FIELD_DESC_HH
+ 
+-#ifdef VISUAL_CPP
+ using namespace std;
+-#pragma warning (disable : 4786)
+-#endif
+ 
+ #include "component_desc.hh"
+ #include "utf_string.hh"
