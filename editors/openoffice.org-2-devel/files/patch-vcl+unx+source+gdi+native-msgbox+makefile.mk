00f0af247692800f9036d0b843d01ade3dc1c6f8
mbr <mbr@FreeBSD.org>
2003-03-12 01:04:08 +0000

    Update to cws_srx644_ooo11beta. It still doesn't build but this commit will help to track the things down.
    There are known problems with weak symbols and exceptions. Some c++ tools
    inside the port will fail if you try to compile.
    
    The cpp_uno bridge has been updated to the latest linux version available.
    Added 'make update' target to upgrade the source to the latest version.
    
    Approved by:	kris (portmgr)

--- editors/openoffice.org-2-devel/files/patch-vcl+unx+source+gdi+native-msgbox+makefile.mk
+++ editors/openoffice.org-2-devel/files/patch-vcl+unx+source+gdi+native-msgbox+makefile.mk
@@ -1,16 +1,19 @@
---- ../vcl/unx/source/gdi/native-msgbox/makefile.mk.orig	Thu Feb 20 18:29:08 2003
-+++ ../vcl/unx/source/gdi/native-msgbox/makefile.mk	Wed Mar  5 19:25:00 2003
-@@ -75,8 +75,12 @@
+--- ../vcl/unx/source/gdi/native-msgbox/makefile.mk.orig	Thu Feb 20 17:29:08 2003
++++ ../vcl/unx/source/gdi/native-msgbox/makefile.mk	Tue Mar 11 16:39:29 2003
+@@ -75,9 +75,15 @@
  
  HASGNOME2=
  
 -.IF "$(OS)$(CPU)"=="LINUXI" || "$(OS)$(CPU)"=="FREEBSDI"
 +.IF "$(OS)$(CPU)"=="LINUXI"
  MSGBOX_GNOME=msgbox-gnome-linux-intel
-+HASGNOME2=TRUE
+ HASGNOME2=TRUE
 +.ENDIF
++.IF "$(_HAS_GNOME2)"=="TRUE"
 +.IF "$(OS)$(CPU)"=="FREEBSDI"
 +MSGBOX_GNOME=msgbox-gnome-freebsd-intel
- HASGNOME2=TRUE
++HASGNOME2=TRUE
++.ENDIF
  .ENDIF
  .IF "$(OS)$(CPU)"=="SOLARISS"
+ MSGBOX_GNOME=msgbox-gnome-solaris-sparc
