a15e4b59a997ed027cc140dc9f43d62fc765f98d
mbr <mbr@FreeBSD.org>
2003-03-23 17:14:57 +0000

    Use the linux map files for the gcc3 case.

--- editors/openoffice.org-2.0/files/patch-salhelper+source+makefile.mk
+++ editors/openoffice.org-2.0/files/patch-salhelper+source+makefile.mk
@@ -1,13 +1,13 @@
---- ../salhelper/source/makefile.mk.orig	Wed Nov 14 14:19:21 2001
-+++ ../salhelper/source/makefile.mk	Sun Mar  3 17:58:15 2002
-@@ -109,6 +109,10 @@
+--- ../salhelper/source/makefile.mk.orig	Sun Sep  1 18:22:39 2002
++++ ../salhelper/source/makefile.mk	Wed Mar 12 23:54:12 2003
+@@ -114,6 +114,10 @@
  SHL1VERSIONMAP=lngi.map
  .ELIF "$(OS)$(CPU)$(COMNAME)"=="LINUXIgcc3"
  SHL1VERSIONMAP=gcc3_linux_intel.map
 +.ELIF "$(OS)$(CPU)$(COMNAME)"=="FREEBSDIgcc2"
-+SHL1VERSIONMAP=gcc2_freebsd_intel.map
++SHL1VERSIONMAP=gcc2_linux_intel.map
 +.ELIF "$(OS)$(CPU)$(COMNAME)"=="FREEBSDIgcc3"
-+SHL1VERSIONMAP=gcc3_freebsd_intel.map
++SHL1VERSIONMAP=gcc3_linux_intel.map
  .ENDIF
  
- 
+ # --- Targets ------------------------------------------------------
