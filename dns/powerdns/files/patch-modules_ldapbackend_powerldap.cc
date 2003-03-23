c1af577b0c46aa2311035aeb8619707703b31f32
dinoex <dinoex@FreeBSD.org>
2003-03-23 04:53:39 +0000

    - Update to 2.9.6 - make ports work as a master port - add support for LDAP, submitted by richard@unixguru.nl - add build for recursor

---
+++ dns/powerdns/files/patch-modules_ldapbackend_powerldap.cc
@@ -0,0 +1,12 @@
+*** modules/ldapbackend/powerldap.cc.orig	Thu Mar 13 20:49:23 2003
+--- modules/ldapbackend/powerldap.cc	Thu Mar 13 20:49:46 2003
+***************
+*** 3,9 ****
+  
+  #include <map>
+  #include <vector>
+- #include <crypt.h>
+  #include <exception>
+  #include <stdexcept>
+  #include <string>
+--- 3,8 ----
