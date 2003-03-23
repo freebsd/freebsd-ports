fc29615f54eafd7ec8a30c62b6c71056badffceb
arved <arved@FreeBSD.org>
2003-03-23 23:15:26 +0000

    Update to 0.9.1.

--- math/graphthing/files/patch-src-graph.cc
+++ math/graphthing/files/patch-src-graph.cc
@@ -1,37 +1,11 @@
---- src/graph.cc.orig	Tue Feb 12 05:51:39 2002
-+++ src/graph.cc	Fri Oct 25 17:16:21 2002
-@@ -82,8 +82,14 @@
- 	}
- 
+--- src/graph.cc.orig	Sun Mar 23 23:55:29 2003
++++ src/graph.cc	Sun Mar 23 23:58:26 2003
+@@ -96,7 +96,7 @@
  	/* We don't want anything skipped */
--	fs.setf (0);
--
-+#if __FreeBSD__
-+#include <osreldate.h>
+ #ifdef USING_FREEBSD
+ #include <osreldate.h>
+-#if __FreeBSD_verson >= 500035
 +#if __FreeBSD_version >= 500035
-+	fs.setf(std::ios_base::fmtflags(0));
-+#else
-+	fs.setf(0);
-+#endif
-+#endif
- 	new_graph = new Graph ();
- 	yy_gt_fs = &fs;
- 	success = (yy_gt_parse () == 0);
-@@ -105,7 +111,7 @@
- 	fs.open (fname, std::fstream::out);
- 	if (!fs.is_open ()) {
- 		/* std::cerr << "*** Couldn't open \"" << fname << "\"\n"; */
--		throw runtime_error ("Couldn't open file.");
-+		throw std::runtime_error ("Couldn't open file.");
- 	}
- 
- 	fs << *this;
-@@ -128,7 +134,7 @@
- 	fs.open (fname, std::fstream::out);
- 	if (!fs.is_open ()) {
- 		/* std::cerr << "*** Couldn't open \"" << fname << "\"\n"; */
--		throw runtime_error ("Couldn't open file.");
-+		throw std::runtime_error ("Couldn't open file.");
- 	}
- 
- 	minx = miny = 999999;
+ 	fs.setf (std::ios_base::fmtflags (0));
+ #else
+ 	fs.setf (0);
