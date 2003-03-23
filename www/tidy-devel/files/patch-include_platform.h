50b016899b478cd0afddfdf39e39d086bd3ae6c6
naddy <naddy@FreeBSD.org>
2003-03-23 13:46:34 +0000

    Update to 030319. This fixes a bunch of worrisome warnings on 64-bit platforms.
    PR:		49121
    Submitted by:	Thierry Thomas <thierry@pompo.net>

--- www/tidy-devel/files/patch-include_platform.h
+++ www/tidy-devel/files/patch-include_platform.h
@@ -1,11 +1,38 @@
---- ./include/platform.h.orig	Mon Jul  8 08:50:02 2002
-+++ ./include/platform.h	Sat Jul 27 10:55:37 2002
-@@ -28,7 +28,7 @@
-   variable is not set. Contributed by Todd Lewis.
+--- include/platform.h.orig	Thu Mar 20 08:57:01 2003
++++ include/platform.h	Fri Mar 21 10:34:00 2003
+@@ -33,7 +33,7 @@
+   It enables tidy to find config files named ~/.tidyrc if 
+   the HTML_TIDY environment variable is not set.
+ */
+-/* #define USER_CONFIG_FILE "~/.tidyrc" */
++#define USER_CONFIG_FILE "~/.tidyrc"
+ 
+ /*
+   Uncomment the following #define if your
+@@ -48,7 +48,7 @@
+   Contributed by Todd Lewis.
  */
  
 -/* #define SUPPORT_GETPWNAM */
 +#define SUPPORT_GETPWNAM
  
+ 
  /* Enable/disable support for Big5 and Shift_JIS character encodings */
- #ifndef SUPPORT_ASIAN_ENCODINGS
+@@ -443,16 +443,14 @@
+ /* hack for gnu sys/types.h file  which defines uint and ulong */
+ /* you may need to delete the #ifndef and #endif on your system */
+ 
+-#ifndef __USE_MISC
+ #if defined(BE_OS) || defined(SOLARIS_OS) || defined(BSD_BASED_OS) || defined(MAC_OS_X) || defined(OSF_OS) || defined(IRIX_OS) || defined(AIX_OS)
+ #include <sys/types.h>
++typedef unsigned long ulong;
+ #else
+ #if !defined(HPUX_OS) && !defined(CYGWIN_OS)
+ typedef unsigned int uint;
+ #endif
+-typedef unsigned long ulong;
+ #endif
+-#endif /* __USE_MISC */
+ 
+ #ifndef TIDY_EXPORT /* Define it away for most builds */
+ #define TIDY_EXPORT
