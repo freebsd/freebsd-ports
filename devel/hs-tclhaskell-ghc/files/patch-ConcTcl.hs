7984d3166bb1d93bf0104595a33e6e775d20db45
obraun <obraun@FreeBSD.org>
2003-03-24 15:38:59 +0000

    Add hs-tclhaskell-ghc 1.2, tcl/Tk binding for Haskell.
    PR:		ports/48948
    Submitted by:	Volker Stolz <stolz@i2.informatik.rwth-aachen.de>

---
+++ devel/hs-tclhaskell-ghc/files/patch-ConcTcl.hs
@@ -0,0 +1,10 @@
+--- ConcTcl.hs.orig	Wed Mar  5 11:38:45 2003
++++ ConcTcl.hs	Wed Mar  5 11:50:13 2003
+@@ -1,6 +1,7 @@
+ module ConcTcl where
+ import TclPrimImpl
+ import Concurrent
++import CVar
+ import IOExts
+ import IO
+ import TclCompatibility
