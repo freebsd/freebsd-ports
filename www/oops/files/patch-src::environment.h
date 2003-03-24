775f41988abb6b9a3d7bf339f7fffdd98f86969d
Max Khon <fjoe@FreeBSD.org>
2003-03-24 15:13:35 +0000

    - Update to 1.5.22-20030321 - Assign maintainership to Sergey Osokin
    PR:		50187

--- www/oops/files/patch-src::environment.h
+++ www/oops/files/patch-src::environment.h
@@ -1,9 +1,20 @@
-
-$FreeBSD$
-
---- src/environment.h.orig	Wed Dec 12 16:49:39 2001
-+++ src/environment.h	Thu Jul 25 20:10:35 2002
-@@ -67,7 +67,7 @@
+--- src/environment.h.orig	Wed Dec 12 18:49:39 2001
++++ src/environment.h	Fri Mar 21 19:15:34 2003
+@@ -52,11 +52,13 @@
+ #if	defined(HAVE_CRYPT_H)
+ #include	<crypt.h>
+ #endif
++/*
+ #if	defined(HAVE_GETOPT_H)
+ #include	<getopt.h>
+ #elif	!defined(HAVE_GETOPT)
+ #include	"getopt.h"
+ #endif
++*/
+ 
+ #if	defined(HAVE_ZLIB)
+   #include	<zlib.h>
+@@ -67,7 +69,7 @@
  #include	<thread.h>
  #endif
  #include	<pthread.h>
@@ -12,7 +23,7 @@ $FreeBSD$
  #include	"rwlock.h"
  #endif
  
-@@ -119,11 +119,11 @@
+@@ -119,11 +121,11 @@
  #include	<sys/stat.h>
  #endif
  
