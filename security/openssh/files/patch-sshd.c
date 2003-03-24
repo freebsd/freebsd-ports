2ce78dad6ffee05efad22e48dc372ac0a5fe71a6
Dirk Meyer <dinoex@FreeBSD.org>
2003-03-24 04:09:05 +0000

    - merge patch from openssh-portable   to initialize resolver libary before   chroot to /var/empty if UsePrivilegeSeparation=yes

--- security/openssh/files/patch-sshd.c
+++ security/openssh/files/patch-sshd.c
@@ -1,6 +1,17 @@
---- /home/bright/ssh/ssh/sshd.c	Thu Aug 17 13:06:34 2000
-+++ sshd.c	Fri Feb  9 11:19:08 2001
-@@ -49,6 +49,12 @@
+--- sshd.c.orig	Mon Mar 24 05:01:09 2003
++++ sshd.c	Mon Mar 24 05:01:36 2003
+@@ -49,6 +49,10 @@
+ #include <openssl/md5.h>
+ #include <openssl/rand.h>
+ 
++#ifdef __FreeBSD__
++#include <resolv.h>
++#endif
++
+ #include "ssh.h"
+ #include "ssh1.h"
+ #include "ssh2.h"
+@@ -88,6 +92,12 @@
  int deny_severity = LOG_WARNING;
  #endif /* LIBWRAP */
  
@@ -13,3 +24,21 @@
  #ifndef O_NOCTTY
  #define O_NOCTTY	0
  #endif
+@@ -1365,6 +1375,17 @@
+ 	    setsockopt(sock_in, SOL_SOCKET, SO_KEEPALIVE, &on,
+ 	    sizeof(on)) < 0)
+ 		error("setsockopt SO_KEEPALIVE: %.100s", strerror(errno));
++
++#ifdef __FreeBSD__
++	/*
++	 * Initialize the resolver.  This may not happen automatically
++	 * before privsep chroot().                                   
++	 */
++	if ((_res.options & RES_INIT) == 0) {
++		debug("res_init()");         
++		res_init();         
++	}
++#endif
+ 
+ 	/*
+ 	 * Register our connection.  This turns encryption off because we do
