2dc569b341810003ed31b9ddfe7d6ace04c4f905
edwin <edwin@FreeBSD.org>
2003-03-28 08:40:10 +0000

    cad/leocad checksum update and minor include file fix
    PR:		ports/50355
    Submitted by:	Scott A. Morris <samorris@jomche.org>

---
+++ cad/leocad/files/patch-gtkmisc.cpp
@@ -0,0 +1,10 @@
+--- linux/gtkmisc.cpp  Sun Jul 23 14:17:56 2000
++++ linux/gtkmisc.cpp Wed Mar 26 14:17:55 2003
+@@ -4,6 +4,7 @@
+ 
+ #include <gtk/gtk.h>
+ #include <gdk/gdkx.h>
++#include <string.h>
+ #include "gtkmisc.h"
+ #include "globals.h"
+ #include "project.h"
