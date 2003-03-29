2c36078d184e6b3fde44793800a6c8ca0b0639f1
cvs2svn <cvs2svn@FreeBSD.org>
2003-03-29 09:53:18 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_4_8_0'.

--- finance/moneydance/files/moneydance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-MDDIR=%%MDDIR%%
-
-for j in $MDDIR/*.jar
-do
-	cp=$cp:$j
-done
-
-exec %%PREFIX%%/bin/javavm -cp ${cp#:} Moneydance
--- x11-toolkits/qt32/files/patch-include-qmlined.h
+++
@@ -1,9 +0,0 @@
---- include/qmlined.h.orig	Sat Feb 15 02:26:37 2003
-+++ include/qmlined.h	Sat Feb 15 02:27:45 2003
-@@ -22,5 +22,5 @@
- *****************************************************************************/
- #ifndef QMLINED_H
- #define QMLINED_H
--#include "qtmultilineedit.h"
-+#include "qtextedit.h"
- #endif
