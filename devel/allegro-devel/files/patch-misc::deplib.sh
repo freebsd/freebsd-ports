f89a6b459429792c4da8df43a2448f7a1313ea0f
Edwin Groothuis <edwin@FreeBSD.org>
2003-03-30 03:04:33 +0000

    devel/allegro: updating to 4.1.4 (devel version).
    	This PR updates allegro to the last 4.1.4, fixes allegro-config,
    	and adds samples.
    
    PR:		ports/45344
    Submitted by:	Thierry Thomas <thierry@pompo.net>

--- devel/allegro-devel/files/patch-misc::deplib.sh
+++ devel/allegro-devel/files/patch-misc::deplib.sh
@@ -1,5 +1,5 @@
---- misc/deplib.sh.orig	Thu Jul  4 05:18:01 2002
-+++ misc/deplib.sh	Tue Nov  5 22:56:54 2002
+--- misc/deplib.sh.orig	Wed Oct 23 08:08:27 2002
++++ misc/deplib.sh	Sun Nov 10 00:40:21 2002
 @@ -10,7 +10,7 @@
      staticlib="lib${1}.a"
      staticobj="\$(${2}_OBJECTS)"
@@ -13,8 +13,8 @@
      echo "	rm -f \$@"
      # gf: This bit is obviously gcc-specific
      # eb: Yes, but the GNU C Compiler doesn't always spell 'gcc'
--    echo "	\$(CC) -shared -o \$@ ${shareobj} \$(LDFLAGS) -Wl,-soname,lib${1}.so.\$(shared_major_minor) \$(LIBS)"
-+    echo "	\$(CC) -shared -o \$@ ${shareobj} \$(LDFLAGS) -Wl,-soname,lib${1}.so.\$(shared_version) \$(LIBS)"
+-    echo "	\$(CC) -shared -o \$@ ${shareobj} \$(LDFLAGS) -Wl,-h,lib${1}.so.\$(shared_major_minor) \$(LIBS)"
++    echo "	\$(CC) -shared -o \$@ ${shareobj} \$(LDFLAGS) -Wl,-h,lib${1}.so.\$(shared_version) \$(LIBS)"
      echo ""
      echo "\$(LIBDIR)/${unsharelib}: ${unshareobj}"
      echo "	rm -f \$@"
