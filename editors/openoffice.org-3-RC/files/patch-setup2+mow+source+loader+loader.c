3abeb95c6db29877d3b3d6334ff2ec0d0ebb759e
mbr <mbr@FreeBSD.org>
2003-03-31 16:05:51 +0000

    Readd previous deleted part. It is still needed for 4.x

--- editors/openoffice.org-3-RC/files/patch-setup2+mow+source+loader+loader.c
+++ editors/openoffice.org-3-RC/files/patch-setup2+mow+source+loader+loader.c
@@ -1,5 +1,18 @@
---- ../setup2/mow/source/loader/loader.c.orig	Sun Mar 30 23:14:14 2003
-+++ ../setup2/mow/source/loader/loader.c	Sun Mar 30 23:14:50 2003
+--- ../setup2/mow/source/loader/loader.c.orig	Thu Oct 31 21:46:52 2002
++++ ../setup2/mow/source/loader/loader.c	Wed Jan 22 14:56:44 2003
+@@ -85,6 +85,12 @@
+ # include <sys/statvfs.h>
+ #endif
+ 
++#ifdef FREEBSD
++#if (OSVERSION < 500000)
++#define iswspace(c) ((c)==' ' || (c)=='\t' || (c)=='\n')
++#endif
++#endif
++
+ #include "bitmap"
+ #include "logo.xpm"
+ 
 @@ -580,6 +580,8 @@
  #define GCC_FILE_PATTERN                    "libgcc_s.so.1"
  #define CPLUS_FILE_PATTERN                  "libstdc++.so.3.0.1"
@@ -9,3 +22,18 @@
  #else
  #define STL_FILE_PATTERN                    "libstlport_sunpro.so"
  #endif
+@@ -650,6 +656,14 @@
+ "    export LD_LIBRARY_PATH\n" \
+ "    ;;\n" \
+ "  Linux)\n" \
++"    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
++"    export LD_LIBRARY_PATH\n" \
++"    ;;\n" \
++"  FreeBSD)\n" \
++"    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
++"    export LD_LIBRARY_PATH\n" \
++"    ;;\n" \
++"  NetBSD)\n" \
+ "    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
+ "    export LD_LIBRARY_PATH\n" \
+ "    ;;\n" \
