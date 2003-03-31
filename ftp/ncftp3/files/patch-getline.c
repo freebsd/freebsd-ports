45f379a48530bc7cc5eb756fc4fff82377223fa4
obrien <obrien@FreeBSD.org>
2003-03-31 02:08:23 +0000

    Work around bug where NcFTP3 will exit unexpectedly when it gets multibyte (such as big5) characters:
    	ncftp3 some.ftp.site and execute "ls <Big5>.txt"
    
    PR:		50458
    Submitted by:	Yen-Ming Lee <leeym@utopia.leeym.com>

--- ftp/ncftp3/files/patch-getline.c
+++ ftp/ncftp3/files/patch-getline.c
@@ -1,5 +1,14 @@
 --- ncftp/getline.c.orig	Sat Apr  7 16:55:16 2001
 +++ ncftp/getline.c	Sun Jul 29 11:10:37 2001
+@@ -367,7 +367,7 @@
+ {
+     int             c;
+ #ifdef __unix__
+-    char            ch;
++    unsigned char   ch;
+ #endif
+ 
+ #ifdef __unix__
 @@ -974,7 +974,12 @@
  	                kill(0, sig);
  	                gl_init();
