9a6254b013480b8182904dee32cb409d75f1991a
Martin Blapp <mbr@FreeBSD.org>
2003-03-31 12:47:00 +0000

    SOLARINI pointed to the wrong file. ssrx644.ini itself has still Linux dependent stuff in the FreeBSD section. Clean this up. Register the stlport lib.

--- editors/openoffice-devel/files/patch-setup2+mow+source+loader+loader.c
+++ editors/openoffice-devel/files/patch-setup2+mow+source+loader+loader.c
@@ -1,30 +1,11 @@
---- ../setup2/mow/source/loader/loader.c.orig	Thu Oct 31 21:46:52 2002
-+++ ../setup2/mow/source/loader/loader.c	Wed Jan 22 14:56:44 2003
-@@ -85,6 +85,12 @@
- # include <sys/statvfs.h>
+--- ../setup2/mow/source/loader/loader.c.orig	Sun Mar 30 23:14:14 2003
++++ ../setup2/mow/source/loader/loader.c	Sun Mar 30 23:14:50 2003
+@@ -580,6 +580,8 @@
+ #define GCC_FILE_PATTERN                    "libgcc_s.so.1"
+ #define CPLUS_FILE_PATTERN                  "libstdc++.so.3.0.1"
+ #define STL_FILE_PATTERN                    "libstlport_gcc.so"
++#elif defined (FREEBSD)
++#define STL_FILE_PATTERN                    "libstlport_gcc.so"
+ #else
+ #define STL_FILE_PATTERN                    "libstlport_sunpro.so"
  #endif
- 
-+#ifdef FREEBSD
-+#if (OSVERSION < 500000)
-+#define iswspace(c) ((c)==' ' || (c)=='\t' || (c)=='\n')
-+#endif
-+#endif
-+
- #include "bitmap"
- #include "logo.xpm"
- 
-@@ -650,6 +656,14 @@
- "    export LD_LIBRARY_PATH\n" \
- "    ;;\n" \
- "  Linux)\n" \
-+"    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
-+"    export LD_LIBRARY_PATH\n" \
-+"    ;;\n" \
-+"  FreeBSD)\n" \
-+"    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
-+"    export LD_LIBRARY_PATH\n" \
-+"    ;;\n" \
-+"  NetBSD)\n" \
- "    LD_LIBRARY_PATH=.:./lib:$LD_LIBRARY_PATH\n" \
- "    export LD_LIBRARY_PATH\n" \
- "    ;;\n" \
