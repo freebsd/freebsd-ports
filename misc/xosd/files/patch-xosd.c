683113cb1c11f5bc0876cdc6b24e7f0a1430f8f1
Hye-Shik Chang <perky@FreeBSD.org>
2003-03-31 05:05:05 +0000

    Update to 2.1.3

--- misc/xosd/files/patch-xosd.c
+++ misc/xosd/files/patch-xosd.c
@@ -1,20 +1,11 @@
---- src/libxosd/xosd.c.orig	Wed Sep 18 19:52:22 2002
-+++ src/libxosd/xosd.c	Wed Sep 18 19:53:00 2002
-@@ -198,7 +198,7 @@
-     pthread_mutex_lock (&osd->mutex);
-     if (!XCheckWindowEvent (osd->display, osd->window, ExposureMask, &report)) {
-       pthread_mutex_unlock (&osd->mutex);
--      usleep (500);
-+      usleep (100000);
-       continue;
-     }
-     pthread_mutex_unlock (&osd->mutex);
-@@ -242,7 +242,7 @@
-   if (osdv == NULL) return NULL;
+--- src/libxosd/xosd.c.orig	Wed Feb 19 10:53:59 2003
++++ src/libxosd/xosd.c	Mon Mar 31 14:00:23 2003
+@@ -278,7 +278,7 @@
+ 
+   DEBUG("event thread started");
+   assert (osd);
+-  usleep (500);
++  usleep (100000);
  
    while (!osd->done) {
--    usleep (1000);
-+    usleep (200000);
-     pthread_mutex_lock (&osd->mutex);
-     if (osd->timeout != -1 && osd->mapped && osd->timeout_time <= time(NULL)) {
-       pthread_mutex_unlock (&osd->mutex);
+     //DEBUG("checking window event");
