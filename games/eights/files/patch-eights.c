4d0c4776a358418b9873a5d4a43d61abc5a9a469
edwin <edwin@FreeBSD.org>
2003-04-01 10:44:26 +0000

    kind of reroll, removed line was: [/usr/ports/games/eights] root@k7>diff -b  eights/eights.c eights.c.new 8,9d7 < #define debug <
    Noticed on: bento

---
+++ games/eights/files/patch-eights.c
@@ -0,0 +1,11 @@
+--- eights.c	Sun Nov 10 08:50:52 2002
++++ eights.c	Sat May 20 12:36:22 2000
+@@ -5,6 +5,8 @@
+ #include <unistd.h>
+ #include <time.h> 
+ 
++#define debug 
++
+ #ifdef DEBUG
+ #undef debug
+ #define debug printf
