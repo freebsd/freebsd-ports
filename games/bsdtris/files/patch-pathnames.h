c19f42a07d5307c1c67e88402b66f578a1d7632f
Edwin Groothuis <edwin@FreeBSD.org>
2003-04-02 13:02:40 +0000

    games/tetris -> games/bsdtris
    PR:		ports/50535

---
+++ games/bsdtris/files/patch-pathnames.h
@@ -0,0 +1,8 @@
+--- pathnames.h.orig	Tue Apr  1 23:06:45 2003
++++ pathnames.h	Tue Apr  1 23:07:14 2003
+@@ -39,4 +39,4 @@
+  *	@(#)pathnames.h	8.1 (Berkeley) 5/31/93
+  */
+ 
+-#define _PATH_SCOREFILE	"/var/games/tetris.scores"
++#define _PATH_SCOREFILE	"%%PREFIX%%/var/games/bsdtris.scores"
