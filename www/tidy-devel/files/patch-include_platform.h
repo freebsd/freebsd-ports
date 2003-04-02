d4dcaaeaaa6896bb9d9034c9420b303886d1f64b
adamw <adamw@FreeBSD.org>
2003-04-02 05:20:32 +0000

    Update to 030330.
    PR:		50505
    Submitted by:	Thierry Thomas <thierry@pompo.net> (maintainer)

--- www/tidy-devel/files/patch-include_platform.h
+++ www/tidy-devel/files/patch-include_platform.h
@@ -1,5 +1,5 @@
---- include/platform.h.orig	Thu Mar 20 08:57:01 2003
-+++ include/platform.h	Fri Mar 21 10:34:00 2003
+--- include/platform.h.orig	Mon Mar 31 09:57:04 2003
++++ include/platform.h	Mon Mar 31 21:23:33 2003
 @@ -33,7 +33,7 @@
    It enables tidy to find config files named ~/.tidyrc if 
    the HTML_TIDY environment variable is not set.
@@ -18,21 +18,3 @@
  
  
  /* Enable/disable support for Big5 and Shift_JIS character encodings */
-@@ -443,16 +443,14 @@
- /* hack for gnu sys/types.h file  which defines uint and ulong */
- /* you may need to delete the #ifndef and #endif on your system */
- 
--#ifndef __USE_MISC
- #if defined(BE_OS) || defined(SOLARIS_OS) || defined(BSD_BASED_OS) || defined(MAC_OS_X) || defined(OSF_OS) || defined(IRIX_OS) || defined(AIX_OS)
- #include <sys/types.h>
-+typedef unsigned long ulong;
- #else
- #if !defined(HPUX_OS) && !defined(CYGWIN_OS)
- typedef unsigned int uint;
- #endif
--typedef unsigned long ulong;
- #endif
--#endif /* __USE_MISC */
- 
- #ifndef TIDY_EXPORT /* Define it away for most builds */
- #define TIDY_EXPORT
