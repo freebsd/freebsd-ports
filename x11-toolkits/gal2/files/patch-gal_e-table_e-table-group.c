a268068302da31e8955728dfa06022e2f2cd52b7
marcus <marcus@FreeBSD.org>
2003-04-04 06:47:22 +0000

    Update to 1.99.2.

--- x11-toolkits/gal2/files/patch-gal_e-table_e-table-group.c
+++ x11-toolkits/gal2/files/patch-gal_e-table_e-table-group.c
@@ -1,26 +1,11 @@
---- gal/e-table/e-table-group.c.orig	Wed Nov 20 10:02:06 2002
-+++ gal/e-table/e-table-group.c	Thu Feb  6 22:35:27 2003
-@@ -22,6 +22,7 @@
-  */
- 
- #include <config.h>
-+#include <stdarg.h>
- #include <gtk/gtksignal.h>
- #include <libgnomecanvas/gnome-canvas-rect-ellipse.h>
- #include "e-table-group.h"
-@@ -128,13 +129,14 @@
- 			 ETableHeader     *header,
- 			 ETableModel      *model)
- {
-+	va_list args;
- 	etg->full_header = full_header;
- 	g_object_ref (etg->full_header);
- 	etg->header = header;
- 	g_object_ref (etg->header);
- 	etg->model = model;
+--- gal/e-table/e-table-group.c.orig	Mon Feb 17 14:23:16 2003
++++ gal/e-table/e-table-group.c	Wed Mar 12 22:13:38 2003
+@@ -136,7 +136,7 @@
  	g_object_ref (etg->model);
--	gnome_canvas_item_construct (GNOME_CANVAS_ITEM (etg), parent, 0, NULL);
-+	gnome_canvas_item_construct (GNOME_CANVAS_ITEM (etg), parent, 0, args);
+ 	g_object_set (G_OBJECT (etg),
+ 		"parent", parent,
+-		NULL);
++		(void*)0);
  }
  
  /**
