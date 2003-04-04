f9788f8b942b1237bf6ebcaa89d67e0d7485be70
Maxim Sobolev <sobomax@FreeBSD.org>
2003-04-04 10:40:24 +0000

    Update to 1.5.0.

--- net/b2bua/files/patch-util::threads::VSemaphore.hxx
+++ net/b2bua/files/patch-util::threads::VSemaphore.hxx
@@ -1,13 +1,14 @@
 
 $FreeBSD$
 
---- util/threads/VSemaphore.hxx	2003/01/19 14:09:49	1.1
-+++ util/threads/VSemaphore.hxx	2003/01/19 14:10:16
-@@ -59,6 +59,7 @@
+--- util/threads/VSemaphore.hxx.orig	Tue Jun 25 20:01:25 2002
++++ util/threads/VSemaphore.hxx	Fri Apr  4 13:06:17 2003
+@@ -54,7 +54,7 @@
+ static const char* const VSemaphore_hxx_version =
+     "$Id: VSemaphore.hxx,v 1.4 2002/06/25 17:01:25 bko Exp $";
+ 
+-#ifdef WIN32
++#if defined(WIN32) || defined(__FreeBSD__)
+ #include <pthread.h>
  #endif
  
- #if defined( __linux__ ) || defined(__FreeBSD__) || defined (WIN32)
-+#include <pthread.h>
- #include <semaphore.h>
- #else
- #include <synch.h>
