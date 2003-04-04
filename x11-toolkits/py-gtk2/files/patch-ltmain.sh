6d7a2cf7d7dea8f758143c66ba121c539ac7b202
marcus <marcus@FreeBSD.org>
2003-04-04 07:58:03 +0000

    Update to 1.99.16.
    Submitted by:	Mezz bsdforums.org <reigncracks@hotmail.com>

--- x11-toolkits/py-gtk2/files/patch-ltmain.sh
+++ x11-toolkits/py-gtk2/files/patch-ltmain.sh
@@ -1,10 +1,10 @@
---- ltmain.sh.orig	Mon May 27 06:33:15 2002
-+++ ltmain.sh	Fri Nov  8 20:57:04 2002
-@@ -1073,8 +1073,16 @@
+--- ltmain.sh.orig	Sun Mar 23 21:36:18 2003
++++ ltmain.sh	Sun Mar 23 21:38:57 2003
+@@ -1085,8 +1085,16 @@
  	continue
  	;;
  
-+      -pthread)
++	-pthread)
 +	compile_command="$compile_command -pthread"
 +	finalize_command="$finalize_command -pthread"
 +	compiler_flags="$compiler_flags -pthread"
