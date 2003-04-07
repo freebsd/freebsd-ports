74406d4eaf8ad817ab09aa0cc66ec53996163b92
ijliao <ijliao@FreeBSD.org>
2003-04-07 05:24:22 +0000

    add yasm 0.2.1 A complete rewrite of the NASM assembler

---
+++ devel/yasm/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- config/ltmain.sh.orig	Mon Apr  7 13:08:30 2003
++++ config/ltmain.sh	Mon Apr  7 13:08:44 2003
+@@ -5288,10 +5288,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
