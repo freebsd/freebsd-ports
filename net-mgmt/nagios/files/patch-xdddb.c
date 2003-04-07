74feb01ba0fc763344d31498c5f9948bc2111e1d
Edwin Groothuis <edwin@FreeBSD.org>
2003-04-07 01:16:37 +0000

    MAINTAINER UPDATE: net/nagios
    	- move nagios workfile directory from /usr/local/var/nagios
    	  to /var/spool/nagios. /usr/local is read-only on some NFS
    	  booted systems.
    	- remove scripts/checkps, everybody should have a working
    	  /bin/ps by now.
    	- add new switches WITH_MYSQL and WITH_PGSQL which enable
    	  support for putting configuration files into a MySQL or
    	  PostgreSQL database, based on a patch submitted in PR
    	  48102 by Krzysztof Stryjek <wtp@mud.pl>
    
    PR:		ports/48988
    Submitted by:	Blaz Zupan <blaz@si.FreeBSD.org>

---
+++ net-mgmt/nagios/files/patch-xdddb.c
@@ -0,0 +1,11 @@
+--- xdata/xdddb.c.orig	Thu May 16 04:46:11 2002
++++ xdata/xdddb.c	Thu Mar  6 23:15:54 2003
+@@ -51,7 +51,7 @@
+ #endif
+ 
+ #ifdef USE_XDDPGSQL
+-#include <pgsql/libpq-fe.h>
++#include <libpq-fe.h>
+ #endif
+ 
+ 
---
+++ net-mgmt/nagios2/files/patch-xeddb.c
@@ -0,0 +1,11 @@
+--- xdata/xeddb.c.orig	Tue Feb 26 05:04:09 2002
++++ xdata/xeddb.c	Thu Mar  6 23:16:01 2003
+@@ -44,7 +44,7 @@
+ #endif
+ 
+ #ifdef USE_XEDPGSQL
+-#include <pgsql/libpq-fe.h>
++#include <libpq-fe.h>
+ #endif
+ 
+ 
