66b7fff0ec008d315baa24c8c50a7550631eb75e
trevor <trevor@FreeBSD.org>
2003-04-08 20:38:59 +0000

    Enable compilation with GCC 3 (tested with 3.2.2).
    Obtained from:	aewm++ port

---
+++ x11-wm/jewel/files/patch-basemenu.hh
@@ -0,0 +1,11 @@
+--- basemenu.hh.orig	Mon Aug 19 17:53:47 2002
++++ basemenu.hh	Tue Apr  8 19:53:56 2003
+@@ -24,6 +24,8 @@
+ #ifndef _BASEMENU_HH_
+ #define _BASEMENU_HH_
+ 
++using namespace std;
++
+ // Parts below borrowed from fspanel.c
+ static unsigned short cols[] = {
+ 	0xd75c, 0xd75c, 0xd75c,		  /* 0. light gray */
