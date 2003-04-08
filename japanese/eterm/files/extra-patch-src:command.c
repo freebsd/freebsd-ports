ac314e96f4e39e2ca97036e76281efcc98dcb609
nobutaka <nobutaka@FreeBSD.org>
2003-04-08 15:10:51 +0000

    Fix build error.
    Submitted by:	Yosifumi Nakagawa <yosifumi@fides.dti.ne.jp>,
    		hosokawa,
    		Philip Paeps <philip@paeps.cx>

--- japanese/eterm/files/extra-patch-src:command.c
+++ japanese/eterm/files/extra-patch-src:command.c
@@ -1,13 +1,13 @@
---- src/command.c.orig	Sat Sep 15 16:57:02 2001
-+++ src/command.c	Sat Sep 15 16:57:24 2001
-@@ -1737,10 +1737,6 @@
-     setlocale(LC_ALL, "");
-     fontset = XCreateFontSet(Xdisplay, fontname, &ml, &mc, &ds);
-     FREE(fontname);
--    if (mc) {
--      XFreeStringList(ml);
--      fontset = 0;
--    }
-   }
-   return fontset;
+--- src/command.c.orig	Fri Oct 25 12:57:17 2002
++++ src/command.c	Sat Apr  5 09:16:50 2003
+@@ -1755,10 +1755,6 @@
+         setlocale(LC_ALL, "");
+         fontset = XCreateFontSet(Xdisplay, fontname, &ml, &mc, &ds);
+         FREE(fontname);
+-        if (mc) {
+-            XFreeStringList(ml);
+-            fontset = 0;
+-        }
+     }
+     return fontset;
  }
