78a8eba4ce5205a1252734ee566e5d66bbafa905
sumikawa <sumikawa@FreeBSD.org>
2003-04-09 18:55:29 +0000

    Upgrade to 21.3.

--- japanese/emacs-emcws/files/patch-lisp_dired.el
+++ japanese/emacs-emcws/files/patch-lisp_dired.el
@@ -1,6 +1,6 @@
---- lisp/dired.el.orig	Sat Sep 22 01:41:58 2001
-+++ lisp/dired.el	Wed Dec  5 20:06:04 2001
-@@ -1532,6 +1532,7 @@
+--- lisp/dired.el.orig	Fri Feb  7 20:48:13 2003
++++ lisp/dired.el	Thu Mar 27 12:12:15 2003
+@@ -1535,6 +1535,7 @@
  			          "\\|" yyyy s s "?"
  			          "\\|" s "?" yyyy
  			     "\\)"))
@@ -8,10 +8,10 @@
  	 (japanese
  	  (concat mm k "?" s dd k "?" s "+"
  		  "\\(" HH:MM "\\|" yyyy k "?" "\\)")))
-@@ -1543,7 +1544,7 @@
+@@ -1546,7 +1547,7 @@
  	 ;; This avoids recognizing `jservice  10  1024' as a date in the line:
  	 ;; drwxr-xr-x  3 jservice  10  1024 Jul  2  1997 esg-host
-     (concat ".*[0-9][kMGTPEZY]?" 
+     (concat ".*[0-9][kKMGTPEZY]?" 
 -	    s "\\(" western "\\|" japanese "\\|" iso "\\)" s))
 +	    s "\\(" western "\\|" freebsd-ja "\\|" japanese "\\|" iso "\\)" s))
    "Regular expression to match up to the file name in a directory listing.
