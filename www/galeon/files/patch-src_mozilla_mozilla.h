a2bf6d197fc99cae3a061f9adaa29a96b26b619e
marcus <marcus@FreeBSD.org>
2003-04-10 17:04:59 +0000

    Fix a signed vs. unsigned problem with Mozilla 1.3.

--- www/galeon/files/patch-src_mozilla_mozilla.h
+++ www/galeon/files/patch-src_mozilla_mozilla.h
@@ -1,6 +1,6 @@
---- src/mozilla/mozilla.h.orig	Mon Apr  7 23:44:55 2003
-+++ src/mozilla/mozilla.h	Mon Apr  7 23:45:01 2003
-@@ -256,14 +256,14 @@
+--- src/mozilla/mozilla.h.orig	Fri Oct 25 20:43:51 2002
++++ src/mozilla/mozilla.h	Thu Apr 10 13:02:14 2003
+@@ -256,14 +256,18 @@
  				  char *force_character_set);
  
  /* Cookies manager */
@@ -10,10 +10,13 @@
  GList *mozilla_list_cookies (void);
 -void mozilla_set_permission (GaleonEmbed *embed, gboolean permit, 
 -				    int type);
--GList *mozilla_get_permissions (int type);
 +void mozilla_set_permission (GaleonEmbed *embed, gboolean allow, 
 +			     guint type);
++#if MOZILLA_SNAPSHOT > 6
 +GList *mozilla_get_permissions (guint type);
++#else
+ GList *mozilla_get_permissions (int type);
++#endif
  gboolean mozilla_remove_cookies (GList *gone, gboolean block);
 -gboolean mozilla_remove_permissions (GList *gone, int type);
 +gboolean mozilla_remove_permissions (GList *gone, guint type);
