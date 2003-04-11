f9f81321b15330dc01ba9041d1f34d63bb54f3c7
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-04-11 03:07:48 +0000

    oops... forgot to add this file last commit

---
+++ math/yacas/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Apr 11 02:52:51 2003
++++ ltmain.sh	Fri Apr 11 02:53:05 2003
+@@ -4318,10 +4318,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
