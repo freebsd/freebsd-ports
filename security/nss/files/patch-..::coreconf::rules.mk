c716629f9468c11f9e2a54e6b00bb5819cdbc941
marcus <marcus@FreeBSD.org>
2003-04-12 20:58:55 +0000

    Update to 3.8.
    PR:	50797
    Submitted by:	Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- security/nss/files/patch-..::coreconf::rules.mk
+++ security/nss/files/patch-..::coreconf::rules.mk
@@ -1,13 +1,13 @@
 
 $FreeBSD$
 
---- ../coreconf/rules.mk.orig	Thu Feb 21 23:23:13 2002
-+++ ../coreconf/rules.mk	Thu Aug  8 20:02:24 2002
-@@ -106,6 +106,7 @@
+--- ../coreconf/rules.mk.orig	Thu Apr  3 16:48:00 2003
++++ ../coreconf/rules.mk	Fri Apr 11 01:01:50 2003
+@@ -110,6 +110,7 @@
  endif
  ifdef SHARED_LIBRARY
  	$(INSTALL) -m 775 $(SHARED_LIBRARY) $(SOURCE_LIB_DIR)
 +	ln -sf $(notdir $(SHARED_LIBRARY)) $(SOURCE_LIB_DIR)/$(notdir $(SHARED_LIBRARY:.so.1=.so))
- ifeq ($(OS_TARGET),OpenVMS)
- 	$(INSTALL) -m 775 $(SHARED_LIBRARY:$(DLL_SUFFIX)=vms) $(SOURCE_LIB_DIR)
  endif
+ ifdef IMPORT_LIBRARY
+ 	$(INSTALL) -m 775 $(IMPORT_LIBRARY) $(SOURCE_LIB_DIR)
