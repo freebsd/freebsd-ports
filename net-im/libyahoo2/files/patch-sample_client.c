7c945ed78c6ef25413deecbc75a6a6920939981f
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-04-12 09:06:35 +0000

    fix strange build problem on stable

---
+++ net-im/libyahoo2/files/patch-sample_client.c
@@ -0,0 +1,10 @@
+--- src/sample_client.c.orig	Sat Apr 12 17:04:56 2003
++++ src/sample_client.c	Sat Apr 12 17:05:25 2003
+@@ -51,7 +51,6 @@
+ #include <yahoo2_callbacks.h>
+ #include "yahoo_util.h"
+ 
+-int fileno(FILE * stream);
+ 
+ 
+ #define MAX_PREF_LEN 255
