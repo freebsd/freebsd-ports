2dc30ea5203b198fdd4419f75615e44440104f05
marcus <marcus@FreeBSD.org>
2003-04-13 19:33:02 +0000

    Fix build on -STABLE.
    PR:	50907
    Submitted by:	Gordon Burditt <gordon@hammy.lonestar.org>

---
+++ finance/libofx/files/patch-lib_ofx_utilities.cpp
@@ -0,0 +1,13 @@
+*** lib/ofx_utilities.old	Sun Apr 13 12:39:29 2003
+--- lib/ofx_utilities.cpp	Sun Apr 13 12:11:04 2003
+***************
+*** 16,21 ****
+--- 16,22 ----
+   *                                                                         *
+   ***************************************************************************/
+  #include <config.h>
++ #include <locale.h>
+  #include <iostream>
+  #include "ParserEventGeneratorKit.h"
+  #include "SGMLApplication.h"
+
