800dddd27cab4c08c919df62e0a9e95ddc269ed8
Martin Blapp <mbr@FreeBSD.org>
2003-04-13 22:48:30 +0000

    Fix breakage in these patches. I just copied them from the openoffice-devel port but some parts in the context seem to have changed.

--- editors/openoffice/files/patch-codemaker+source+codemaker+global.cxx
+++ editors/openoffice/files/patch-codemaker+source+codemaker+global.cxx
@@ -1,9 +1,9 @@
 --- ../codemaker/source/codemaker/global.cxx.orig	Fri Apr  4 00:14:50 2003
 +++ ../codemaker/source/codemaker/global.cxx	Fri Apr  4 00:14:53 2003
-@@ -164,11 +164,7 @@
-     strncat(tmpPattern, "/", sizeof(tmpPattern)-1-strlen(tmpPattern));
-     strncat(tmpPattern, pPrefix, sizeof(tmpPattern)-1-strlen(tmpPattern));
-     strncat(tmpPattern, "XXXXXX", sizeof(tmpPattern)-1-strlen(tmpPattern));
+@@ -149,11 +149,7 @@
+ 	strcat(tmpPattern, "\\");
+ 	strcat(tmpPattern, pPrefix);
+ 	strcat(tmpPattern, "XXXXXX");
 -#if defined(FREEBSD) || defined(MACOSX)
 -	pTmpName = mkstemp(tmpPattern);
 -#else
--- editors/openoffice/files/patch-registry+source+registry.cxx
+++ editors/openoffice/files/patch-registry+source+registry.cxx
@@ -1,9 +1,9 @@
 --- ../registry/source/registry.cxx.orig	Fri Apr  4 00:09:08 2003
 +++ ../registry/source/registry.cxx	Fri Apr  4 00:06:25 2003
-@@ -145,11 +145,7 @@
-                                        + RTL_CONSTASCII_LENGTH("/reg_XXXXXX") ) );
-     strncat(tmpPattern, "/reg_XXXXXX", sizeof(tmpPattern)-1-strlen(tmpPattern));
+@@ -134,11 +134,7 @@
  
+ #ifdef UNX
+ 	strcat(tmpPattern, "/reg_XXXXXX");
 -#if defined(FREEBSD) || defined(MACOSX)
 -	pTmpName = mkstemp(tmpPattern);
 -#else
