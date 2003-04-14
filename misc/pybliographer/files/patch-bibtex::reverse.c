1ac3488ef979de2d24558ae063a346e6aaf17100
Tilman Keskinoz <arved@FreeBSD.org>
2003-04-14 12:35:41 +0000

    Add patch, necessary for 4.x
    PR:		50577
    Submitted by:	Mark Linimon <linimon@lonesome.com>

---
+++ misc/pybliographer/files/patch-bibtex::reverse.c
@@ -0,0 +1,11 @@
+--- bibtex/reverse.c.dist	Tue Apr  1 19:22:52 2003
++++ bibtex/reverse.c	Tue Apr  1 19:22:09 2003
+@@ -26,7 +26,7 @@
+ #endif
+ 
+ #include <string.h>
+-#include <regex.h>
++#include <gnuregex.h>
+ 
+ #ifdef HAVE_STDBOOL_H
+ #include <stdbool.h>
