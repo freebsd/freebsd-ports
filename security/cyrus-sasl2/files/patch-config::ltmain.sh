45e33242b8b5fed04013ca4f31b8b05d7793736a
Hajimu UMEMOTO <ume@FreeBSD.org>
2003-04-15 17:03:28 +0000

    - update to 2.1.13 - fixes for 5-CURRENT (changed behaviour of 'expr') - enable NTLM module - support for db4 and db4.1 - support for OpenLDAP 2.1
    PR:		ports/50962
    Submitted by:	Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- security/cyrus-sasl2/files/patch-config::ltmain.sh
+++ security/cyrus-sasl2/files/patch-config::ltmain.sh
@@ -1,7 +1,32 @@
-Index: config/ltmain.sh
-diff -u config/ltmain.sh.orig config/ltmain.sh
 --- config/ltmain.sh.orig	Tue Sep  3 23:59:40 2002
 +++ config/ltmain.sh	Sat Oct 12 08:03:59 2002
+@@ -1894,7 +1894,7 @@
+ 	  if test $? -eq 0 ; then
+ 	    ldd_output=`ldd conftest`
+ 	    for i in $deplibs; do
+-	      name="`expr $i : '-l\(.*\)'`"
++	      name="`expr X$i : 'X-l\(.*\)'`"
+ 	      # If $name is empty we are operating on a -L argument.
+ 	      if test "$name" != "" ; then
+ 		libname=`eval \\$echo \"$libname_spec\"`
+@@ -1919,7 +1919,7 @@
+ 	    # Error occured in the first compile.  Let's try to salvage the situation:
+ 	    # Compile a seperate program for each library.
+ 	    for i in $deplibs; do
+-	      name="`expr $i : '-l\(.*\)'`"
++	      name="`expr X$i : 'X-l\(.*\)'`"
+ 	     # If $name is empty we are operating on a -L argument.
+ 	      if test "$name" != "" ; then
+ 		$rm conftest
+@@ -1959,7 +1959,7 @@
+ 	  set dummy $deplibs_check_method
+ 	  file_magic_regex="`expr \"$deplibs_check_method\" : \"$2 \(.*\)\"`"
+ 	  for a_deplib in $deplibs; do
+-	    name="`expr $a_deplib : '-l\(.*\)'`"
++	    name="`expr X$a_deplib : 'X-l\(.*\)'`"
+ 	    # If $name is empty we are operating on a -L argument.
+ 	    if test "$name" != "" ; then
+ 	      libname=`eval \\$echo \"$libname_spec\"`
 @@ -3391,8 +3391,8 @@
  	# Install the pseudo-library for information purposes.
  	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
