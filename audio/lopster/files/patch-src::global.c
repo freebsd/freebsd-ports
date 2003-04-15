7fa8a92a2811b1d15ac5ead9f0ec32b7815f69a8
arved <arved@FreeBSD.org>
2003-04-15 11:48:05 +0000

    Update to 1.2.0
    PR:		49960
    Submitted by:	Oliver Lehmann <lehmann@ans-netz.de>
    Approved by:	maintainer

--- audio/lopster/files/patch-src::global.c
+++ audio/lopster/files/patch-src::global.c
@@ -1,25 +1,9 @@
 --- src/global.c.orig	Sun Nov  4 13:30:19 2001
 +++ src/global.c	Sat Jan 26 22:15:50 2002
-@@ -1333,7 +1333,6 @@
-   
-   return NULL;
- }
--*/
+@@ -3096,7 +3096,7 @@
+   global.allowed_ports = NULL;
  
- char *strcasestr(char *str, char *sub)
- {
-@@ -1361,6 +1360,7 @@
- 
-   return NULL;
- }
-+*/
- 
- int idle_function(gpointer data ATTR_UNUSED)
- {
-@@ -3597,7 +3597,7 @@
    global.auto_save = NULL;
-   global.execs = NULL;
-   global.pings = NULL;
 -  global.ping_command = l_strdup("/bin/ping -c 3 $IP");
 +  global.ping_command = l_strdup("/sbin/ping -c 3 $IP");
  
