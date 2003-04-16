1d9f8ad40b0281e441c152c12dde9f80c91bc66d
Martin Blapp <mbr@FreeBSD.org>
2003-04-16 22:32:47 +0000

    Fix compilation on alpha.
    Submitted by:	Christian Weisgerber <naddy@freebsd.org>
    PR:		51066

--- converters/recode/files/patch-lib-error.c
+++ converters/recode/files/patch-lib-error.c
@@ -1,25 +1,23 @@
---- lib/error.c.orig	Mon Mar 10 10:57:22 2003
-+++ lib/error.c	Mon Mar 10 11:02:12 2003
-@@ -25,6 +25,18 @@
+--- lib/error.c.orig	Thu Aug  3 03:21:13 2000
++++ lib/error.c	Thu Apr 17 00:29:29 2003
+@@ -25,6 +25,16 @@
  # include <config.h>
  #endif
  
 +# ifdef __FreeBSD__
 +# include <sys/types.h>
 +# include <sys/exec.h>
++# include <sys/param.h>
 +# include <vm/vm.h>
 +# include <vm/vm_param.h>
 +# include <vm/pmap.h>
 +# include <vm/swap_pager.h>
-+# include <machine/param.h>
-+# include <machine/vmparam.h>
-+# include <machine/pmap.h>
 +# endif
 +
  #include <stdio.h>
  
  #if HAVE_VPRINTF || HAVE_DOPRNT || _LIBC
-@@ -80,7 +92,11 @@
+@@ -80,7 +90,11 @@
  
  /* The calling program should define program_name and set it to the
     name of the executing program.  */
