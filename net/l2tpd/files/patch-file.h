073a4e4697f16fa6122d46013b29d174af39c8df
ru <ru@FreeBSD.org>
2003-04-16 13:53:27 +0000

    When l2tpd(8) runs on a multi-homed machine, and you attempt to establish an L2TP VPN connection to its external IP address from the Windows XP box on your LAN, l2tpd(8) will reply (UDP) from its local IP address, and not external IP address.
    Implement the new global option "address" to aid in overcoming
    this problem.  Also helps in environments with complex NAT
    configurations.
    
    Bump PORTREVISION.
    
    Approved by:	sobomax

--- net/l2tpd/files/patch-file.h
+++ net/l2tpd/files/patch-file.h
@@ -2,7 +2,7 @@
 $FreeBSD$
 
 --- file.h.orig	Wed Apr 10 23:09:33 2002
-+++ file.h	Wed Oct 23 16:34:37 2002
++++ file.h	Wed Apr 16 16:07:19 2003
 @@ -49,10 +49,10 @@
  #define SENSE_ALLOW -1
  #define SENSE_DENY 0
@@ -18,3 +18,11 @@ $FreeBSD$
  #define DEFAULT_PID_FILE "/var/run/l2tpd.pid"
  
  /* Definition of an LNS */
+@@ -130,6 +130,7 @@
+ 
+ struct global
+ {
++    struct in_addr addr;	/* Address to listen on */
+     int port;                   /* Port number to listen to */
+     char authfile[STRLEN];      /* File containing authentication info */
+     char altauthfile[STRLEN];   /* File containing authentication info */
