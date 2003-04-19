625ff17f68bb93764151ba842a186907b3aad137
adamw <adamw@FreeBSD.org>
2003-04-19 18:34:23 +0000

    Add gxine, a GTK interface to xine. The interface is simple, but stable.
    PR:		51156
    Submitted by:	Grigori Goronzy <greg@chown.ath.cx>

---
+++ multimedia/gxine/files/patch-src::desktop_integration.c
@@ -0,0 +1,10 @@
+--- src/desktop_integration.c.orig	Sat Apr 19 02:23:31 2003
++++ src/desktop_integration.c	Sat Apr 19 02:23:39 2003
+@@ -30,7 +30,6 @@
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <dlfcn.h>
+-#include <alloca.h>
+ #include <string.h>
+ 
+ #include "globals.h"
---
+++ multimedia/gxine/files/patch-src::gtkxine.h
@@ -0,0 +1,10 @@
+--- src/gtkxine.h.orig	Sat Apr 19 02:24:55 2003
++++ src/gtkxine.h	Sat Apr 19 02:25:07 2003
+@@ -27,6 +27,7 @@
+ #include <X11/Xlib.h>
+ #include <X11/extensions/XShm.h>
+ #include <xine.h>
++#include "globals.h"
+ 
+ #ifdef __cplusplus
+ extern "C" {
