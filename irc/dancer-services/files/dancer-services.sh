fc37200bcd2540800b9b82ab45ea395717c31281
Akinori MUSHA <knu@FreeBSD.org>
2003-04-19 12:02:50 +0000

    Add dancer-services, the IRC services (nickserv, chanserv, etc.) for dancer-ircd.

---
+++ irc/dancer-services/files/dancer-services.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# $FreeBSD$
+
+case "$1" in
+start)
+  if [ -x %PREFIX%/bin/dancer-services ]; then
+    su -fm ircservices -c %PREFIX%/bin/dancer-services && echo ' dancer-services'
+  fi
+  ;;
+
+stop)
+  if [ -f /var/run/dancer-services.pid ]; then
+    kill `cat /var/run/dancer-services.pid`
+  else
+    # oh well
+    killall dancer-services
+  fi
+  ;;
+
+*)
+  echo "usage: $0 {start|stop}" >&2
+  exit 64
+  ;;
+esac
