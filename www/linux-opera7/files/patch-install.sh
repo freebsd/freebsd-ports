3bdc34c891b0ff64b886c4af5eef311398f7c222
Norikatsu Shigemura <nork@FreeBSD.org>
2003-04-19 12:55:18 +0000

    Fix following mistake (missed by nork).
    1) Java plug-in path aren't correct and out of date
    2) pkg-messages aren't there, which broke the Makefile.
    3) Two patches aren't there that make Makefile and pkg-plist
       aren't right.
    
    PR:		ports/51136, ports/51157
    Submitted by:	Mezz <reigncracks@hotmail.com> (maintainer)
    Reported by:	Stacey Roberts <stacey@vickiandstacey.com>
    Pointy hat to:	myself

---
+++ www/linux-opera7/files/patch-install.sh
@@ -0,0 +1,189 @@
+--- install.sh.orig	Sat Apr 12 01:07:58 2003
++++ install.sh	Sat Apr 12 01:17:23 2003
+@@ -721,20 +721,13 @@
+     case "${machine}:${os}" in
+ 	i[3456]86:Linux|i[3456]86:FreeBSD|i[3456]86:NetBSD|i[3456]86:OpenBSD)
+ 	    wrapper_plugin_paths="
+-    /usr/local/Acrobat[45]/Browsers/intellinux \\
+-    /usr/lib/Acrobat[45]/Browsers/intellinux \\
+-    /usr/X11R6/lib/Acrobat[45]/Browsers/intellinux \\
+-    /opt/Acrobat[45]/Browsers/intellinux \\
+-    /usr/Acrobat[45]/Browsers/intellinux \\
+-    /usr/j2se/jre/plugin/i386/ns4 \\
+-    /usr/java/jre1.4.0/plugin/i386/ns4 \\
+-    /usr/java/jre1.3.1/plugin/i386/ns4 \\
+-    /usr/lib/j2re1.3/plugin/i386/netscape4 \\
+-    /usr/local/jdk1.3.1/jre/plugin/i386/ns4 \\
+-    /usr/local/linux-jdk1.3.1/jre/plugin/i386/ns4 \\"
++    $prefix/lib/RealPlayer8/Plugins \\
++    $prefix/Acrobat[45]/Browsers/intellinux \\
++    $prefix/linux-sun-jdk1.[34].1/jre/plugin/i386/ns4 \\
++    $prefix/linux-blackdown-jdk1.[34].1/jre/plugin/i386/netscape4 \\"
+ 	    wrapper_ibmjava="
+-	    IBMJava2-14/jre \\
+-	    IBMJava2-131/jre \\"
++	    linux-ibm-jdk1.3.1/jre \\
++	    linux-ibm-jdk1.4.0/jre \\"
+ 	    wrapper_sunjava_machine="i386"
+ 	;;
+ 
+@@ -905,7 +898,7 @@
+ 
+ # Acrobat Reader
+ for BINDIR in \\
+-    /usr/local/Acrobat[45]/bin \\
++    $prefix/Acrobat[45]/bin \\
+     /usr/lib/Acrobat[45]/bin \\
+     /usr/X11R6/lib/Acrobat[45]/bin \\
+     /opt/Acrobat[45]/bin \\
+@@ -1106,7 +1099,7 @@
+ 	mkdir $mkdirv $mkdirp $share_dir/java/
+ 	chmod $chmodv 755 $share_dir/java
+ 	
+-	cp $cpv $cpf $cpp java/lc.jar java/opera.jar $share_dir/java/
++	cp $cpv $cpf $cpp java/opera.jar $share_dir/java/
+ 	generate_opera_policy
+ 	
+      # Plug-in files
+@@ -1119,8 +1112,9 @@
+         chmod $chmodv 755 $plugin_dir/operamotifwrapper $plugin_dir/operaplugincleaner $plugin_dir/libnpp.so
+ 
+      # System wide configuration files
+-	config_dir="/etc"
++	config_dir="$prefix/etc"
+ 	if can_write_to "$config_dir"; then
++if false; then # XXX
+ 	    echo
+ 	    echo "System wide configuration files:"
+ 	    echo "  $config_dir/opera6rc"
+@@ -1147,6 +1141,7 @@
+ 			;;
+ 		esac
+ 	    done
++fi # XXX
+ 	else
+ 	    echo
+ 	    echo "User \"${USERNAME}\" does not have write access to $config_dir"
+@@ -1230,22 +1225,22 @@
+ 
+     debug_msg 0 "in icons()"
+ 
+-    if test ! -d /usr/share/icons; then
+-      if test -w /usr/share; then
+-        mkdir $mkdirv $mkdirp /usr/share/icons/
+-	chmod $chmodv 755 /usr/share/icons
+-	cp $cpv $cpp $share_dir/images/opera.xpm /usr/share/icons/opera.xpm
++    if test ! -d %%X11PREFIX%%/share/icons; then
++      if test -w %%X11PREFIX%%/share; then
++        mkdir $mkdirv $mkdirp %%X11PREFIX%%/share/icons/
++	chmod $chmodv 755 %%X11PREFIX%%/share/icons
++	cp $cpv $cpp $share_dir/images/opera.xpm %%X11PREFIX%%/share/icons/opera.xpm
+       fi
+-    elif test -w /usr/share/icons; then cp $cpv $cpp $share_dir/images/opera.xpm /usr/share/icons/opera.xpm
++    elif test -w %%X11PREFIX%%/share/icons; then cp $cpv $cpp $share_dir/images/opera.xpm %%X11PREFIX%%/share/icons/opera.xpm
+     fi
+   
+-    if test ! -d /usr/share/pixmaps; then
+-      if test -w /usr/share; then
+-	mkdir $mkdirv $mkdirp /usr/share/pixmaps/
+-	chmod $chmodv 755 /usr/share/pixmaps
+-	cp $cpv $cpp $share_dir/images/opera.xpm /usr/share/pixmaps/opera.xpm
++    if test ! -d %%X11PREFIX%%/share/pixmaps; then
++      if test -w %%X11PREFIX%%/share; then
++	mkdir $mkdirv $mkdirp %%X11PREFIX%%/share/pixmaps/
++	chmod $chmodv 755 %%X11PREFIX%%/share/pixmaps
++	cp $cpv $cpp $share_dir/images/opera.xpm %%X11PREFIX%%/share/pixmaps/opera.xpm
+       fi
+-    elif test -w /usr/share/pixmaps/; then cp $cpv $cpp $share_dir/images/opera.xpm /usr/share/pixmaps/opera.xpm
++    elif test -w %%X11PREFIX%%/share/pixmaps/; then cp $cpv $cpp $share_dir/images/opera.xpm %%X11PREFIX%%/share/pixmaps/opera.xpm
+     fi
+   
+     if test ! -d /etc/X11/wmconfig/; then
+@@ -1311,36 +1306,36 @@
+       fi
+       # end /opt/gnome share
+ 
+-    elif test -d /usr/share/gnome/; then
++    elif test -d %%X11PREFIX%%/share/gnome/; then
+ 
+-        # /usr/share/gnome icon
+-        if test ! -d /usr/share/gnome/pixmaps/; then
+-	  if test -w /usr/share/gnome; then
+-	    mkdir $mkdirv $mkdirp /usr/share/gnome/pixmaps/
+-	    chmod $chmodv 755 /usr/share/gnome/pixmaps
+-	    cp $cpv $cpp $share_dir/images/opera.xpm /usr/share/gnome/pixmaps/opera.xpm
++        # %%X11PREFIX%%/share/gnome icon
++        if test ! -d %%X11PREFIX%%/share/gnome/pixmaps/; then
++	  if test -w %%X11PREFIX%%/share/gnome; then
++	    mkdir $mkdirv $mkdirp %%X11PREFIX%%/share/gnome/pixmaps/
++	    chmod $chmodv 755 %%X11PREFIX%%/share/gnome/pixmaps
++	    cp $cpv $cpp $share_dir/images/opera.xpm %%X11PREFIX%%/share/gnome/pixmaps/opera.xpm
+ 	  fi
+-	elif test -w /usr/share/gnome/pixmaps; then cp $cpv $cpp $share_dir/images/opera.xpm /usr/share/gnome/pixmaps/opera.xpm
++	elif test -w %%X11PREFIX%%/share/gnome/pixmaps; then cp $cpv $cpp $share_dir/images/opera.xpm %%X11PREFIX%%/share/gnome/pixmaps/opera.xpm
+ 	fi
+-	# end /usr/share/gnome icon
++	# end %%X11PREFIX%%/share/gnome icon
+ 
+-	# /usr/share/gnome link
+-	if test -d /usr/share/gnome/apps/; then
+-          if test -d /usr/share/gnome/apps/Internet/; then
+-            if test -w /usr/share/gnome/apps/Internet; then
+-              generate_desktop /usr/share/gnome/apps/Internet
++	# %%X11PREFIX%%/share/gnome link
++	if test -d %%X11PREFIX%%/share/gnome/apps/; then
++          if test -d %%X11PREFIX%%/share/gnome/apps/Internet/; then
++            if test -w %%X11PREFIX%%/share/gnome/apps/Internet; then
++              generate_desktop %%X11PREFIX%%/share/gnome/apps/Internet
+ 	    fi
+-          elif test -d /usr/share/gnome/apps/Networking/WWW/; then
+-            if test -w /usr/share/gnome/apps/Networking/WWW; then
+-	     generate_desktop /usr/share/gnome/apps/Networking/WWW
++          elif test -d %%X11PREFIX%%/share/gnome/apps/Networking/WWW/; then
++            if test -w %%X11PREFIX%%/share/gnome/apps/Networking/WWW; then
++	     generate_desktop %%X11PREFIX%%/share/gnome/apps/Networking/WWW
+ 	    fi
+-	  elif test -w /usr/share/gnome/apps; then
+-	    mkdir $mkdirv $mkdirp/usr/share/gnome/apps/Internet/
+-	    chmod $chmodv 755 /usr/share/gnome/apps/Internet
+-            generate_desktop /usr/share/gnome/apps/Internet
++	  elif test -w %%X11PREFIX%%/share/gnome/apps; then
++	    mkdir $mkdirv $mkdirp %%X11PREFIX%%/share/gnome/apps/Internet/
++	    chmod $chmodv 755 %%X11PREFIX%%/share/gnome/apps/Internet
++            generate_desktop %%X11PREFIX%%/share/gnome/apps/Internet
+           fi
+ 	fi
+-	# end /usr/share/gnome link
++	# end %%X11PREFIX%%/share/gnome link
+    fi
+    # Add ximian here
+ }
+@@ -1377,12 +1372,12 @@
+ 
+     fi  
+     
+-    if test -d /usr/share/applnk/Networking; then
+-      if test ! -d /usr/share/applnk/Networking/WWW/ -a -w /usr/share/applnk/Networking; then
+-	    mkdir $mkdirv $mkdirp /usr/share/applnk/Networking/WWW/
+-	    chmod $chmodv 755 /usr/share/applnk/Networking/WWW
++    if test -d %%X11PREFIX%%/share/applnk/Networking; then
++      if test ! -d %%X11PREFIX%%/share/applnk/Networking/WWW/ -a -w %%X11PREFIX%%/share/applnk/Networking; then
++	    mkdir $mkdirv $mkdirp %%X11PREFIX%%/share/applnk/Networking/WWW/
++	    chmod $chmodv 755 %%X11PREFIX%%/share/applnk/Networking/WWW
+       fi
+-      if test -w /usr/share/applnk/Networking/WWW; then generate_desktop /usr/share/applnk/Networking/WWW; fi
++      if test -w %%X11PREFIX%%/share/applnk/Networking/WWW; then generate_desktop %%X11PREFIX%%/share/applnk/Networking/WWW; fi
+     fi
+ }
+ 
+@@ -1406,8 +1401,8 @@
+       fi
+       if test -w /opt/kde/share/applnk/Internet; then generate_desktop /opt/kde/share/applnk/Internet; fi
+ 
+-    elif test -d /usr/share/applnk/Internet/ -a -w /usr/share/applnk/Internet; then
+-      generate_desktop /usr/share/applnk/Internet
++    elif test -d %%X11PREFIX%%/share/applnk/Internet/ -a -w %%X11PREFIX%%/share/applnk/Internet; then
++      generate_desktop %%X11PREFIX%%/share/applnk/Internet
+     fi
+ }
+ 
