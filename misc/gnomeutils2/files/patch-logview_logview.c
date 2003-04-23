47f8b8a259f29f379986d05fc21966a49c985455
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-04-23 18:58:32 +0000

    Fix a double-free bug in the logview application.  This would be very bad on non-FreeBSD systems, but phkmalloc should protect us.  This fix should take care of the abort crashes on -CURRENT.

--- misc/gnomeutils2/files/patch-logview_logview.c
+++ misc/gnomeutils2/files/patch-logview_logview.c
@@ -1,6 +1,6 @@
---- logview/logview.c.orig	Tue May  7 00:03:16 2002
-+++ logview/logview.c	Tue May  7 00:04:37 2002
-@@ -903,7 +903,7 @@
+--- logview/logview.c.orig	Sun Feb 16 15:34:59 2003
++++ logview/logview.c	Wed Apr 23 14:51:26 2003
+@@ -774,7 +774,7 @@
  
  	if ( ! found) {
  		g_snprintf (full_name, sizeof (full_name),
@@ -9,7 +9,7 @@
  		if (access (full_name, R_OK) == 0) {
  			found = TRUE;
  			g_free (cfg->regexp_db_path);
-@@ -926,7 +926,7 @@
+@@ -797,7 +797,7 @@
  
  	if ( ! found) {
  		g_snprintf (full_name, sizeof (full_name),
@@ -18,7 +18,7 @@
  		if (access (full_name, R_OK) == 0) {
  			found = TRUE;
  			g_free (cfg->descript_db_path);
-@@ -960,7 +960,7 @@
+@@ -831,7 +831,7 @@
  
  	if ( ! found) {
  		g_snprintf (full_name, sizeof (full_name),
@@ -27,3 +27,11 @@
  		if (access (full_name, R_OK) == 0) {
  			found = TRUE;
  			g_free (cfg->action_db_path);
+@@ -882,7 +882,6 @@
+ 		else
+ 			prefs->logfile = NULL;
+ 	}
+-	g_free (logfile);	
+ }
+ 
+ void SaveUserPrefs(UserPrefsStruct *prefs)
