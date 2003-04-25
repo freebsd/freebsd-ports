2de006395ad9ad10d668836e72e6f0e559dbf648
Jacques Vidrine <nectar@FreeBSD.org>
2003-04-25 00:07:20 +0000

    Add support for using nss_ldap in `compat' mode (i.e. as sources for the `passwd_compat' and `group_compat' databases).

--- net/nss_ldap/files/bsdnss.c
+++ net/nss_ldap/files/bsdnss.c
@@ -46,6 +46,18 @@ static ns_mtab methods[] = {
 { NSDB_PASSWD, "endpwent",   __nss_compat_setpwent,   _nss_ldap_setpwent },
 { NSDB_PASSWD, "setpwent",   __nss_compat_endpwent,   _nss_ldap_endpwent },
 
+{ NSDB_GROUP_COMPAT, "getgrnam_r", __nss_compat_getgrnam_r, _nss_ldap_getgrnam_r },
+{ NSDB_GROUP_COMPAT, "getgrgid_r", __nss_compat_getgrgid_r, _nss_ldap_getgrgid_r },
+{ NSDB_GROUP_COMPAT, "getgrent_r", __nss_compat_getgrent_r, _nss_ldap_getgrent_r },
+{ NSDB_GROUP_COMPAT, "endgrent",   __nss_compat_setgrent,   _nss_ldap_setgrent },
+{ NSDB_GROUP_COMPAT, "setgrent",   __nss_compat_endgrent,   _nss_ldap_endgrent },
+
+{ NSDB_PASSWD_COMPAT, "getpwnam_r", __nss_compat_getpwnam_r, _nss_ldap_getpwnam_r },
+{ NSDB_PASSWD_COMPAT, "getpwuid_r", __nss_compat_getpwuid_r, _nss_ldap_getpwuid_r },
+{ NSDB_PASSWD_COMPAT, "getpwent_r", __nss_compat_getpwent_r, _nss_ldap_getpwent_r },
+{ NSDB_PASSWD_COMPAT, "endpwent",   __nss_compat_setpwent,   _nss_ldap_setpwent },
+{ NSDB_PASSWD_COMPAT, "setpwent",   __nss_compat_endpwent,   _nss_ldap_endpwent },
+
 };
 
 
