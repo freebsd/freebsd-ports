4fe022b400964474609da7a25f7191e2d793df24
nork <nork@FreeBSD.org>
2003-04-26 22:28:40 +0000

    Update to 0.8.1.
    PR:		ports/51358
    Submitted by:	Ports Fury

---
+++ audio/muse/files/patch-shout::mp3.c
@@ -0,0 +1,10 @@
+--- shout/mp3.c.orig	Tue Mar 25 05:30:10 2003
++++ shout/mp3.c	Thu Apr 24 02:44:09 2003
+@@ -1,7 +1,6 @@
+ /* mp3.c: libshout MP3 format handler */
+ 
+ #include <stdio.h>
+-#include <stdint.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <config.h>
