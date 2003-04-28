7e05c9215178164981a8b09a789cf604c4c15e54
phk <phk@FreeBSD.org>
2003-04-28 19:43:06 +0000

    Update to version 2.17.1

--- emulators/hercules/files/patch-tapecopy.c
+++ emulators/hercules/files/patch-tapecopy.c
@@ -1,6 +1,6 @@
---- tapecopy.c.orig	Mon Sep  2 11:00:59 2002
-+++ tapecopy.c	Mon Sep  2 11:03:21 2002
-@@ -32,6 +32,7 @@
+--- tapecopy.c	Sat Feb  1 20:34:57 2003
++++ ../work/hercules-2.17.1/tapecopy.c	Mon Apr 28 21:21:15 2003
+@@ -42,6 +42,7 @@
  static BYTE hdrlbl[] = "\xC8\xC4\xD9";  /* EBCDIC characters "HDR"   */
  static BYTE eoflbl[] = "\xC5\xD6\xC6";  /* EBCDIC characters "EOF"   */
  static BYTE eovlbl[] = "\xC5\xD6\xE5";  /* EBCDIC characters "EOV"   */
@@ -8,7 +8,7 @@
  static struct mt_tape_info tapeinfo[] = MT_TAPE_INFO;
  static struct mt_tape_info densinfo[] = {
      {0x01, "NRZI (800 bpi)"},
-@@ -59,6 +60,7 @@
+@@ -69,6 +70,7 @@
      {0x8C, "EXB-8505 compressed"},
      {0x90, "EXB-8205 compressed"},
      {0, NULL}};
@@ -16,15 +16,15 @@
  static BYTE buf[65500];
  
  /*-------------------------------------------------------------------*/
-@@ -71,6 +73,7 @@
+@@ -76,6 +78,7 @@
  /*-------------------------------------------------------------------*/
  static void print_status (BYTE *devname, long stat)
  {
 +#ifdef GNU_MTIO_SUPPORT
-     printf ("%s status: %8.8lX", devname, stat);
+     printf ("HHCTC015I %s status: %8.8lX", devname, stat);
      if (GMT_EOF(stat)) printf (" EOF");
      if (GMT_BOT(stat)) printf (" BOT");
-@@ -84,6 +87,7 @@
+@@ -89,6 +92,7 @@
      if (GMT_D_800(stat)) printf (" 800");
      if (GMT_DR_OPEN(stat)) printf (" NOTAPE");
      printf ("\n");
@@ -32,7 +32,7 @@
  
  } /* end function print_status */
  
-@@ -104,10 +108,12 @@
+@@ -109,10 +113,12 @@
          return -1;
      }
  
@@ -45,7 +45,7 @@
  
      return 0;
  } /* end function print_status */
-@@ -188,6 +194,7 @@
+@@ -205,6 +211,7 @@
          exit (7);
      }
  
@@ -53,7 +53,7 @@
      /* Display tape status information */
      for (i = 0; tapeinfo[i].t_type != 0
                  && tapeinfo[i].t_type != stblk.mt_type; i++);
-@@ -215,6 +222,7 @@
+@@ -233,6 +240,7 @@
  
      /* Set the tape device to process variable length blocks */
      opblk.mt_op = MTSETBLK;
