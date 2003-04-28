393e6cce23cb88ac3e125223f5d5cb51f733f805
lioux <lioux@FreeBSD.org>
2003-04-28 05:33:26 +0000

    New port knutclient version 0.6.0: A visual client for UPS system NUT

---
+++ sysutils/knutclient/files/patch-knutclient::knutnet.h
@@ -0,0 +1,12 @@
+--- knutclient/knutnet.h.orig	Mon Apr 28 02:07:21 2003
++++ knutclient/knutnet.h	Mon Apr 28 02:07:28 2003
+@@ -28,8 +28,8 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+-#include <sys/socket.h>
+ #include <sys/types.h>
++#include <sys/socket.h>
+ #include <sys/time.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
