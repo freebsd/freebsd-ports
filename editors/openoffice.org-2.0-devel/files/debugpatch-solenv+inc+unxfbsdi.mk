da841ec5cae9ee0ec523d569e658d830cd91dd67
maho <maho@FreeBSD.org>
2003-04-28 07:09:58 +0000

    Fix debug patches, which were not correctly applied.
    Submitted by:	Hiromi Kimura <hiromi@tac.tsukuba.ac.jp>

--- editors/openoffice.org-2.0-devel/files/debugpatch-solenv+inc+unxfbsdi.mk
+++ editors/openoffice.org-2.0-devel/files/debugpatch-solenv+inc+unxfbsdi.mk
@@ -1,18 +1,18 @@
 --- ../solenv/inc/unxfbsdi.mk.orig	Mon Jul 22 00:40:56 2002
 +++ ../solenv/inc/unxfbsdi.mk	Mon Jul 22 00:42:02 2002
-@@ -25,12 +25,12 @@
- .ENDIF
+@@ -92,12 +92,12 @@
+ CDEFS+= $(PTHREAD_CFLAGS) -D_REENTRANT
  
- # flags for C and C++ Compiler
--CFLAGS+=-w -c $(INCLUDE) -I$(SOLARENV)/unxfbsdi/usr/include
-+CFLAGS+=-g -ggdb -w -c $(INCLUDE) -I$(SOLARENV)/unxfbsdi/usr/include
- CFLAGS+=-I/usr/X11R6/include
+ # flags for C and C++ Compile
+-CFLAGS+= -w -c $(INCLUDE)
++CFLAGS+= -g -ggdb -w -c $(INCLUDE)
+ CFLAGS+= -I/usr/X11R6/include
  
  # flags for the C++ Compiler
--CFLAGSCC=-pipe
--CFLAGSCXX=-pipe -fno-for-scope -fpermissive
-+CFLAGSCC=-g -ggdb -pipe
-+CFLAGSCXX=-g -ggdb -pipe -fno-for-scope -fpermissive
+-CFLAGSCC= -pipe
+-CFLAGSCXX= -pipe -fno-for-scope -fpermissive
++CFLAGSCC= -g -ggdb -pipe
++CFLAGSCXX= -g -ggdb -pipe -fno-for-scope -fpermissive
  
  # Flags for enabling exception handling
- CFLAGSEXCEPTIONS=-fexceptions
+ CFLAGSEXCEPTIONS= -fexceptions
--- editors/openoffice.org-vcltesttool/files/debugpatch-setup2+mow+source+loader+loader.c
+++ editors/openoffice.org-vcltesttool/files/debugpatch-setup2+mow+source+loader+loader.c
@@ -1,15 +1,15 @@
 --- ../setup2/mow/source/loader/loader.c.orig2	Thu Jun  6 11:23:17 2002
 +++ ../setup2/mow/source/loader/loader.c	Thu Jun  6 11:24:14 2002
-@@ -902,6 +902,7 @@
+@@ -1055,6 +1055,7 @@
  
  void KillSetupDir()
  {
 +#if 0
-     DIR* pDir = opendir( strTmpPath );
+     DIR* pDir;
      struct dirent* pFile;
  
-@@ -915,6 +916,10 @@
- 
+@@ -1074,6 +1075,10 @@
+     closedir( pDir );
      chdir( strInitPath );
      rmdir( strTmpPath );
 +#else
