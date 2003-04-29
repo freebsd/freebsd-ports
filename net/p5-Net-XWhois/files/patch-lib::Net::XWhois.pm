0d4bea03b0a87f2015802f86c5106e8dccd1b8b6
ache <ache@FreeBSD.org>
2003-04-29 06:15:55 +0000

    .org moved to whois.publicinterestregistry.net Misspelling: nonexisten "registrants" -> documented "registrant"

--- net/p5-Net-XWhois/files/patch-lib::Net::XWhois.pm
+++ net/p5-Net-XWhois/files/patch-lib::Net::XWhois.pm
@@ -1,12 +1,47 @@
---- lib/Net/XWhois.pm.orig	Sat Jul 14 11:30:30 2001
-+++ lib/Net/XWhois.pm	Tue Mar  5 15:18:05 2002
-@@ -124,6 +124,22 @@
+--- lib/Net/XWhois.pm.orig	Sun Oct  6 05:37:55 2002
++++ lib/Net/XWhois.pm	Tue Apr 29 09:54:06 2003
+@@ -16,6 +16,7 @@
+ # 
+ # Changes:
+ #
++#  03/08/2003  jmiller     Added new .org PIR registy and parser
+ #  08/05/2002  rwoodard    Merged in changes from XWhois discussion forum on
+ #                          sourceforge.net; made additional changes as needed
+ #                          to implement reverse lookups of IP addresses
+@@ -157,6 +158,26 @@
+    net_handle           => '\((NETBLK\S+)\)',
+    country              => '\n\s+(\S+)\n\n',
+  },
++
++ PIR => {
++  name            => 'Domain Name:(\S+)',
++  status          => 'Status:(.*?)\s*\n',
++  registrant      => 'Registrant (?:Name|Street1|Street2|City|Postal Code|Country|Email):(.*?)\s*\n',
++  nameservers     => 'Name Server:(\S+)',
++  contact_admin   => 'Admin (?:Name|Street1|Street2|City|Postal Code|Country|Email):(.*?)\s*\n',
++  contact_tech    => 'Tech (?:Name|Street1|Street2|City|Postal Code|Country|Email):(.*?)\s*\n',
++  contact_zone    => 'Admin (?:Name|Street1|Street2|City|Postal Code|Country|Email):(.*?)\s*\n',
++  contact_billing => 'Billing (?:Name|Street1|Street2|City|Postal Code|Country|Email):(.*?)\s*\n',
++  contact_emails  => '(?:Registrant|Admin|Billing|Tech) Email:(\S+\@\S+)',
++  contact_handles => '\(([^\W\d]+\d+)\)',
++  domain_handles  => '\((\S*?-DOM)\)',
++  org_handles     => '\((\S*?-ORG)\)',
++  not_registered  => 'NOT FOUND',
++#  forwardwhois    => 'Whois Server:(.*?)\s*\n',
++  registrar       => 'Referral URL:(.*?)\s*\n',
++  reg_date        => 'Created On:(.*?)\s*\n',
++  exp_date        => 'Expiration Date:(.*?)\s*\n',
++ },
+  
+  INTERNIC => {
+   name            => '[\n\r\f]+\s*[Dd]omain [Nn]ame[:\.]*\s+(\S+)', 
+@@ -241,6 +262,22 @@
    nameservers     => 'Name servers:[\s\n]+(\S+)[\s\n]+(\S+)',
   },
  
 + RIPN => {
 +  name            => 'domain:\s+(\S+)\n',
-+  registrants     => 'descr:\s+(.+?)\n',
++  registrant      => 'descr:\s+(.+?)\n',
 +  contact_admin   => 'admin-o:\s+(.*?)\s*\n',
 +  contact_tech    => 'tech-c:\s+(.*?)\s*\n',
 +  nameservers     => 'nserver:\s+(\S+)',
@@ -23,17 +58,31 @@
   NOMINET => { 
    name                => 'omain Name:\s+(\S+)',
    registrant          => 'egistered For:\s*(.*?)\n',
-@@ -202,6 +218,7 @@
+@@ -276,7 +313,7 @@
+ 
+  DENIC => { 
+   name            => 'domain:\s+(\S+)\n',
+-  registrants     => 'descr:\s+(.+?)\n',
++  registrant      => 'descr:\s+(.+?)\n',
+   contact_admin   => 'admin-c:\s+(.*?)\s*\n',
+   contact_tech    => 'tech-c:\s+(.*?)\s*\n',
+   contact_zone    => 'zone-c:\s+(.*?)\s*\n',
+@@ -339,7 +376,9 @@
+ );
  
  my %WHOIS_PARSER = (
++    'whois.publicinterestregistry.net' => 'PIR',
      'whois.ripe.net'            => 'RPSL',
 +    'whois.ripn.net'            => 'RIPN',
      'whois.nic.mil'             => 'INTERNIC',
      'whois.nic.ad.jp'           => 'JAPAN',
      'whois.domainz.net.nz'      => 'GENERIC',
-@@ -254,10 +271,10 @@
+@@ -397,12 +436,12 @@
+     'mt'  => 'whois.ripe.net',      'mx'  => 'whois.nic.mx',
+     'net' => 'whois.internic.net',  'nl'  => 'whois.ripe.net',
      'no'  => 'whois.norid.no',      'nz'  => 'whois.domainz.net.nz',
-     'org' => 'whois.internic.net',
+-    'org' => 'whois.internic.net',
++    'org' => 'whois.publicinterestregistry.net',
      'pl'  => 'whois.ripe.net',      'pt'  => 'whois.ripe.net',
 -    'ro'  => 'whois.ripe.net',      'ru'  => 'whois.ripe.net',
 +    'ro'  => 'whois.ripe.net',      'ru'  => 'whois.ripn.net',
