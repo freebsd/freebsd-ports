60ba85cfb80c34eadb1b67c1c880363c72ee1dc6
marcus <marcus@FreeBSD.org>
2003-05-02 03:18:31 +0000

    Update to 1.6.4.  This is a big update that has the potential of breaking ports that depend on guile.  As such I have tested most of them.  Here are my results:
    --KNOWN TO WORK--
    devel/gconf
    games/gnomegames
    games/gnomegames2
    devel/g-wrap
    x11-toolkits/guile-gtk
    x11-toolkits/guile-gnome
    math/guppi
    finance/gnucash
    deskutils/gnotime
    devel/libgtop
    devel/autogen
    games/gnurobots
    audio/snd
    math/drgenius
    net/serveez
    science/libctl
    x11-wm/scwm (works with patches to be sent to maintainer)
    --KNOWN NOT TO WORK (maintainer is working on update)--
    cad/geda
    cad/geda-symcheck
    cad/geda-utils
    cad/gnetlist
    cad/gschem
    cad/gwave
    cad/libgeda
    --UNKNOWN--
    mail/icqmail (marked IGNORE for other reasons)
    lang/gnustep-guile (could not build for other reasons; will notify maintainer)
    math/siag (did not test)
    science/mpb (did not test)
    
    PR:	43910

--- lang/guile/files/patch-libguile_load.c
+++ lang/guile/files/patch-libguile_load.c
@@ -1,11 +1,16 @@
---- libguile/load.c.orig	Tue Feb  4 21:05:24 2003
-+++ libguile/load.c	Tue Feb  4 21:19:39 2003
-@@ -239,6 +239,8 @@
-   path = scm_listify (scm_makfrom0str (SCM_SITE_DIR),
- 		      scm_makfrom0str (SCM_LIBRARY_DIR),
- 		      scm_makfrom0str (SCM_PKGDATA_DIR),
-+		      scm_makfrom0str ("%%X11BASE%%/share/guile"),
-+		      scm_makfrom0str ("%%LOCALBASE%%/share/guile"),
- 		      SCM_UNDEFINED);
+--- libguile/load.c.orig	Sat Dec  7 17:41:32 2002
++++ libguile/load.c	Wed Apr 30 22:55:23 2003
+@@ -247,9 +247,11 @@
+   SCM path = SCM_EOL;
+ 
+ #ifdef SCM_LIBRARY_DIR
+-  path = scm_list_3 (scm_makfrom0str (SCM_SITE_DIR),
++  path = scm_list_5 (scm_makfrom0str (SCM_SITE_DIR),
+ 		     scm_makfrom0str (SCM_LIBRARY_DIR),
+-		     scm_makfrom0str (SCM_PKGDATA_DIR));
++		     scm_makfrom0str (SCM_PKGDATA_DIR),
++		     scm_makfrom0str ("/usr/X11R6/share/guile"),
++		     scm_makfrom0str ("/usr/local%%/share/guile"));
  #endif /* SCM_LIBRARY_DIR */
  
+   path = scm_internal_parse_path (getenv ("GUILE_LOAD_PATH"), path);
