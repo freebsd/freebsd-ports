9a579bdae0e899f23105808a33749f4e6510a21c
maho <maho@FreeBSD.org>
2003-05-05 13:06:37 +0000

    A Three-Dimensional Structural Finite Element Program
    Submitted by	 : Pedro F. Giffuni <giffunip@yahoo.com>
    Almost writeen by: maho

---
+++ cad/calculix/files/patch-ccx_1.0+src+CalculiX.h
@@ -0,0 +1,11 @@
+--- ccx_1.0/src/CalculiX.h.orig	Mon Mar  3 00:35:58 2003
++++ ccx_1.0/src/CalculiX.h	Mon May  5 21:15:50 2003
+@@ -24,6 +24,8 @@
+ #define FORTRAN(A,B) A##_##B
+ #elif ARCH == HP
+ #define FORTRAN(A,B) A##B
++#else
++#define FORTRAN(A,B) A##_ B
+ #endif
+ 
+ #define NNEW(a,b) (a *)u_calloc((b),sizeof(a))
