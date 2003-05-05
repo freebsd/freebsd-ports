bc4b84868fd257488e9692237b697015bfe88df3
sumikawa <sumikawa@FreeBSD.org>
2003-05-05 14:48:25 +0000

    Fix build on -current.
    Partly Submitted by:	Christian Lackas <delta@lackas.net>
    Pointed out by:		kris

---
+++ palm/prc-tools/files/patch-gdb:strsignal.c
@@ -0,0 +1,11 @@
+--- ../gdb-5.0/libiberty/strsignal.c.orig	Mon May  3 07:28:54 1999
++++ ../gdb-5.0/libiberty/strsignal.c	Mon May  5 14:27:35 2003
+@@ -244,7 +244,7 @@
+ #else
+ 
+ #ifdef NSIG
+-static int sys_nsig = NSIG;
++static const int sys_nsig = NSIG;
+ #else
+ #ifdef _NSIG
+ static int sys_nsig = _NSIG;
