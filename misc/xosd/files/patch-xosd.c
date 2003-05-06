f4c8f0c8090dc924888d3ee9fcb8309d342ebfba
Hye-Shik Chang <perky@FreeBSD.org>
2003-05-06 12:40:24 +0000

    - Update to 2.2.1 [1] - Allow building WITHOUT_XMMS. [2]
    PR:		51494 [1]
    Submitted by:	Sebastian Yepes <Sebastian@mail.x123.info> [1]
    Suggested by:	Andy Fawcett <andy@athame.co.uk> [2]

--- misc/xosd/files/patch-xosd.c
+++ misc/xosd/files/patch-xosd.c
@@ -1,11 +1,11 @@
 --- src/libxosd/xosd.c.orig	Wed Feb 19 10:53:59 2003
 +++ src/libxosd/xosd.c	Mon Mar 31 14:00:23 2003
-@@ -278,7 +278,7 @@
+@@ -332,7 +332,7 @@
  
-   DEBUG("event thread started");
-   assert (osd);
--  usleep (500);
-+  usleep (100000);
+ 	DEBUG("event thread started");
+	assert (osd);
+-	usleep (500);
++	usleep (100000);
  
-   while (!osd->done) {
-     //DEBUG("checking window event");
+	while (!osd->done) {
+		//DEBUG("checking window event");
