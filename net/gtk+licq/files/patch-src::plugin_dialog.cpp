a4442963eb3cae79542cb7d8a9ec5a1cf5286fc9
kris <kris@FreeBSD.org>
2003-05-06 03:49:05 +0000

    As announced on 27 March 2003 in <20030328052350.GA18971@rot13.obsecurity.org>, remove these ports that have been broken for >4 months (in many cases much longer) with no sign of a fix.

--- net/gtk+licq/files/patch-src::plugin_dialog.cpp
+++
@@ -1,9 +0,0 @@
---- src/plugin_dialog.cpp.orig	Mon Mar 27 17:59:06 2000
-+++ src/plugin_dialog.cpp	Tue Sep 19 21:47:13 2000
-@@ -1,5 +1,6 @@
- #include "gtk-gui.h"
- 
-+#include <unistd.h>
- #include <sys/time.h>
- #include <stdio.h>
- #include <sys/types.h>
