b6c49a64ad3e4c37d6cb0ff39bc8cd8b2e50689b
Jun Kuriyama <kuriyama@FreeBSD.org>
2003-05-07 00:51:37 +0000

    (Hopefully) fix build on alpha.

--- sysutils/kcpuload/files/patch-kcpuproc.cpp
+++ sysutils/kcpuload/files/patch-kcpuproc.cpp
@@ -1,5 +1,5 @@
---- kcpuload/kcpuproc.cpp.orig	Wed May  7 09:07:19 2003
-+++ kcpuload/kcpuproc.cpp	Wed May  7 09:16:42 2003
+--- kcpuload/kcpuproc.cpp.orig	Fri Dec 20 10:48:06 2002
++++ kcpuload/kcpuproc.cpp	Wed May  7 09:33:48 2003
 @@ -18,8 +18,12 @@
  
  // BSD-specific includes.
@@ -14,3 +14,12 @@
  #include <sys/sysctl.h>
  #include <string.h>
  #include <kvm.h>
+@@ -119,7 +123,7 @@
+     static int oidCpuTime[CTL_MAXNAME + 2];
+     static size_t oidCpuTimeLen = sizeof(oidCpuTime);
+     long cpuTime[CPUSTATES];
+-    unsigned int cpuTimeLen = sizeof(cpuTime);
++    size_t cpuTimeLen = sizeof(cpuTime);
+     static char *name = "kern.cp_time";
+     static int initialized = 0;
+ #endif
