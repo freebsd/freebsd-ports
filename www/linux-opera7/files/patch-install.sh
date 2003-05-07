a2c9acb7ef75a172b3f62f36a9a400022fe2d271
Norikatsu Shigemura <nork@FreeBSD.org>
2003-05-07 09:56:30 +0000

    Bump the version for the plugin fixes:
    o Upgrade opera, the plugins left in the old directory
      still will work fine without require reinstall plugins.
    o Sun Java and Blackdown Java plugins are work ok now,
      they have been tested by maintainer.
    
    PR:		ports/51904
    Submitted by:	Mezz <reigncracks@hotmail.com> (maintainer)

--- www/linux-opera7/files/patch-install.sh
+++ www/linux-opera7/files/patch-install.sh
@@ -1,6 +1,14 @@
---- install.sh.orig	Mon May  5 18:19:51 2003
-+++ install.sh	Mon May  5 18:49:29 2003
-@@ -721,17 +721,9 @@
+--- install.sh.orig	Tue May  6 18:02:23 2003
++++ install.sh	Tue May  6 18:11:33 2003
+@@ -714,27 +714,17 @@
+ 
+     wrapper_opera_plugin_paths="    \"\${HOME}/.opera/plugins\" \\
+     ${plugin_dir} \\
+-    /usr/lib/opera/plugins \\
+-    /usr/local/lib/opera/plugins \\
+-    /opt/lib/opera/plugins \\"
++    %%LOCALBASE%%/share/opera/plugins \\"
+ 
      case "${machine}:${os}" in
  	i[3456]86:Linux|i[3456]86:FreeBSD|i[3456]86:NetBSD|i[3456]86:OpenBSD)
  	    wrapper_plugin_paths="
@@ -19,58 +27,75 @@
 +    %%LOCALBASE%%/linux-sun-jdk1.[34].1/jre/plugin/i386/ns4 \\
 +    %%LOCALBASE%%/linux-blackdown-jdk1.[34].1/jre/plugin/i386/netscape4 \\"
  	    wrapper_ibmjava="
- 	    IBMJava2-14/jre \\
- 	    IBMJava2-131/jre \\"
-@@ -781,7 +773,7 @@
+-	    IBMJava2-14/jre \\
+-	    IBMJava2-131/jre \\"
++	    linux-ibm-jdk1.3.1/jre \\
++	    linux-ibm-jdk1.4.0/jre \\"
+ 	    wrapper_sunjava_machine="i386"
+ 	;;
+ 
+@@ -781,15 +771,9 @@
  	;;
      esac
      wrapper_netscape_plugin_paths="
 -    /usr/lib/RealPlayer8/Plugins \\
+-    /usr/lib/realplay/plugins \\
+-    /usr/lib/RealPlayer8 \\
+-    /usr/lib/realplay \\
 +    %%LOCALBASE%%/lib/RealPlayer8/Plugins \\
-     /usr/lib/realplay/plugins \\
-     /usr/lib/RealPlayer8 \\
-     /usr/lib/realplay \\
-@@ -789,7 +781,7 @@
-     /opt/netscape/plugins \\
-     /usr/lib/netscape/plugins \\
-     /usr/local/netscape/plugins \\
+     \"\${HOME}/.netscape/plugins\" \\
+-    /opt/netscape/plugins \\
+-    /usr/lib/netscape/plugins \\
+-    /usr/local/netscape/plugins \\
 -    /usr/local/lib/netscape/plugins \\"
 +    %%LOCALBASE%%/lib/netscape-linux/plugins \\"
  
      wrapper_file="${wrapper_dir}/opera"
      
-@@ -862,8 +854,8 @@
- 	jre1.3.1 \\
- 	j2re1.3 \\
+@@ -841,26 +825,11 @@
+ 
+ if test ! \"\${OPERA_JAVA_DIR}\"; then
+ 
+-    PREFIXES=\"
+-	/usr
+-	/usr/java
+-	/usr/lib
+-	/usr/local
+-	/opt\"
++    PREFIXES=\"%%LOCALBASE%%\"
+ 
+     for SUNJAVA in \\
+-	j2re1.4.1_01 \\
+-	j2re1.4.1 \\
+-	j2re1.4.0_01 \\
+-	j2sdk1.4.0_01/jre \\
+-	j2re1.4.0 \\
+-	jre1.4.0 \\
+-	j2se/jre \\
+-	jre1.3.1_02 \\
+-	jre1.3.1_01 \\
+-	j2re1.3.1 \\
+-	jre1.3.1 \\
+-	j2re1.3 \\
++	linux-blackdown-jdk1.[34].1/jre \\
++	linux-sun-jdk1.[34].1/jre \\
  	; do
--	for PREFIX in \${PREFIXES}; do
--	    if test -f \"\${PREFIX}/\${SUNJAVA}/lib/${wrapper_sunjava_machine}/libjava.so\"; then OPERA_JAVA_DIR=\"\${PREFIX}/\${SUNJAVA}/lib/${wrapper_sunjava_machine}\" && break; fi
-+	for PREFIX in %%LOCALBASE%%; do
-+	    if test -f \"%%LOCALBASE%%/\${SUNJAVA}/lib/${wrapper_sunjava_machine}/libjava.so\"; then OPERA_JAVA_DIR=\"%%LOCALBASE%%/\${SUNJAVA}/lib/${wrapper_sunjava_machine}\" && break; fi
- 	done
- 	if test \"\${OPERA_JAVA_DIR}\"; then break; fi
-     done
-@@ -871,8 +863,8 @@
-     if test ! \"\${OPERA_JAVA_DIR}\"; then
- 	for IBMJAVA in \\${wrapper_ibmjava}
- 	    ; do
--	    for PREFIX in \${PREFIXES}; do
--		if test -f \"\${PREFIX}/\${IBMJAVA}/bin/libjava.so\"; then OPERA_JAVA_DIR=\"\${PREFIX}/\${IBMJAVA}/bin\" && break; fi
-+	    for PREFIX in %%LOCALBASE%%; do
-+		if test -f \"%%LOCALBASE%%/\${IBMJAVA}/bin/libjava.so\"; then OPERA_JAVA_DIR=\"%%LOCALBASE%%/\${IBMJAVA}/bin\" && break; fi
- 	    done
- 	if test \"\${OPERA_JAVA_DIR}\"; then break; fi
- 	done
-@@ -905,7 +897,7 @@
+ 	for PREFIX in \${PREFIXES}; do
+ 	    if test -f \"\${PREFIX}/\${SUNJAVA}/lib/${wrapper_sunjava_machine}/libjava.so\"; then OPERA_JAVA_DIR=\"\${PREFIX}/\${SUNJAVA}/lib/${wrapper_sunjava_machine}\" && break; fi
+@@ -905,11 +874,7 @@
  
  # Acrobat Reader
  for BINDIR in \\
 -    /usr/local/Acrobat[45]/bin \\
+-    /usr/lib/Acrobat[45]/bin \\
+-    /usr/X11R6/lib/Acrobat[45]/bin \\
+-    /opt/Acrobat[45]/bin \\
+-    /usr/Acrobat[45]/bin \\
 +    %%LOCALBASE%%/Acrobat[45]/bin \\
-     /usr/lib/Acrobat[45]/bin \\
-     /usr/X11R6/lib/Acrobat[45]/bin \\
-     /opt/Acrobat[45]/bin \\
-@@ -1106,7 +1098,7 @@
+     ; do
+     if test -d \${BINDIR} ; then PATH=\${PATH}:\${BINDIR}; fi
+ done
+@@ -1106,7 +1071,7 @@
  	mkdir $mkdirv $mkdirp $share_dir/java/
  	chmod $chmodv 755 $share_dir/java
  	
@@ -79,7 +104,7 @@
  	generate_opera_policy
  	
       # Plug-in files
-@@ -1119,8 +1111,9 @@
+@@ -1119,8 +1084,9 @@
          chmod $chmodv 755 $plugin_dir/operamotifwrapper $plugin_dir/operaplugincleaner $plugin_dir/libnpp.so
  
       # System wide configuration files
@@ -90,7 +115,7 @@
  	    echo
  	    echo "System wide configuration files:"
  	    echo "  $config_dir/opera6rc"
-@@ -1147,6 +1140,7 @@
+@@ -1147,6 +1113,7 @@
  			;;
  		esac
  	    done
@@ -98,7 +123,7 @@
  	else
  	    echo
  	    echo "User \"${USERNAME}\" does not have write access to $config_dir"
-@@ -1230,22 +1224,22 @@
+@@ -1230,22 +1197,22 @@
  
      debug_msg 0 "in icons()"
  
@@ -133,7 +158,7 @@
      fi
    
      if test ! -d /etc/X11/wmconfig/; then
-@@ -1311,36 +1305,36 @@
+@@ -1311,36 +1278,36 @@
        fi
        # end /opt/gnome share
  
@@ -182,7 +207,7 @@
 -	    chmod $chmodv 755 /usr/share/gnome/apps/Internet
 -            generate_desktop /usr/share/gnome/apps/Internet
 +	  elif test -w %%X11PREFIX%%/share/gnome/apps; then
-+	    mkdir $mkdirv $mkdirp%%X11PREFIX%%/share/gnome/apps/Internet/
++	    mkdir $mkdirv $mkdirp %%X11PREFIX%%/share/gnome/apps/Internet/
 +	    chmod $chmodv 755 %%X11PREFIX%%/share/gnome/apps/Internet
 +            generate_desktop %%X11PREFIX%%/share/gnome/apps/Internet
            fi
@@ -192,7 +217,7 @@
     fi
     # Add ximian here
  }
-@@ -1377,12 +1371,12 @@
+@@ -1377,12 +1344,12 @@
  
      fi  
      
@@ -210,7 +235,7 @@
      fi
  }
  
-@@ -1406,8 +1400,8 @@
+@@ -1406,8 +1373,8 @@
        fi
        if test -w /opt/kde/share/applnk/Internet; then generate_desktop /opt/kde/share/applnk/Internet; fi
  
