c23c7d4ba70479fa21ead6658ef8c33ce7f366d5
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-05-08 00:26:31 +0000

    Add gtksourceview, and editor component that adds syntax highlighting support to the default GtkTextView widget.

---
+++ editors/gtksourceview/files/patch-gtksourceview_gtksourceregex.h
@@ -0,0 +1,11 @@
+--- gtksourceview/gtksourceregex.h.orig	Wed May  7 20:04:08 2003
++++ gtksourceview/gtksourceregex.h	Wed May  7 20:05:43 2003
+@@ -20,7 +20,7 @@
+ #ifndef __GTK_SOURCE_REGEX_H__
+ #define __GTK_SOURCE_REGEX_H__
+ 
+-#include <regex.h>
++#include <gnuregex.h>
+ #include <glib/gtypes.h>
+ 
+ G_BEGIN_DECLS
