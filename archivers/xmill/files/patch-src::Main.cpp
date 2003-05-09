751000fc4323bb80f1546d65386a33d7bb875fb5
Max Khon <fjoe@FreeBSD.org>
2003-05-09 19:03:17 +0000

    New port: xmill
    An efficient compressor for XML.
    
    Submitted by:	Alexey Dokuchaev

---
+++ archivers/xmill/files/patch-src::Main.cpp
@@ -0,0 +1,27 @@
+--- src/Main.cpp.orig	Fri May  9 16:24:51 2003
++++ src/Main.cpp	Fri May  9 16:25:41 2003
+@@ -599,7 +599,7 @@
+       return;
+    }
+ 
+-   if(output.CreateFile((no_output==0) ? destfile : "")==0)
++   if(output.CreateFile((no_output==0) ? destfile : (char *)"")==0)
+    {
+       Error("Could not create output file '");
+       ErrorCont(destfile);
+@@ -816,7 +816,7 @@
+ 
+    globallabeldict.Init();
+ 
+-   if(output.CreateFile((no_output==0) ? destfile : "")==0)
++   if(output.CreateFile((no_output==0) ? destfile : (char *)"")==0)
+    {
+       Error("Could not create output file '");
+       ErrorCont(destfile);
+@@ -905,4 +905,4 @@
+ {
+    return malloc(size);
+ }
+-*/
+\ No newline at end of file
++*/
