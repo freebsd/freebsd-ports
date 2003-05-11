d7c82651f668e022a5677c2109b2a1465d412293
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-05-11 18:02:46 +0000

    upgrade to 0.1.7

---
+++ textproc/gmetadom/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Sun May 11 23:50:20 2003
++++ ltmain.sh	Sun May 11 23:50:40 2003
+@@ -4247,10 +4247,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
