38d90218232723ec62debce4a3d49e745b38b0b1
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2003-05-11 02:42:33 +0000

    Fix build on alpha ARCH
    Submitted by:	marcus

--- multimedia/gstreamer-plugins/files/patch-ffmpeg::simple_idct_alpha.c
+++ multimedia/gstreamer-plugins/files/patch-ffmpeg::simple_idct_alpha.c
@@ -1,13 +1,17 @@
---- gst-libs/ext/ffmpeg/ffmpeg/libavcodec/alpha/simple_idct_alpha.c.orig	Tue Mar 18 02:16:44 2003
-+++ gst-libs/ext/ffmpeg/ffmpeg/libavcodec/alpha/simple_idct_alpha.c	Wed Mar 19 01:10:35 2003
-@@ -27,6 +27,10 @@
+--- gst-libs/ext/ffmpeg/ffmpeg/libavcodec/alpha/simple_idct_alpha.c.orig	Fri Apr 25 22:06:13 2003
++++ gst-libs/ext/ffmpeg/ffmpeg/libavcodec/alpha/simple_idct_alpha.c	Fri Apr 25 23:19:39 2003
+@@ -24,8 +24,14 @@
+  *                     and Falk Hueffner <falk@debian.org>
+  */
+ 
++#include <sys/param.h>
++
  #include "asm.h"
  #include "../dsputil.h"
- 
-+#if defined(__FreeBSD__) && __FreeBSD_version < 500000
-+typedef long int int_fast32_t;
-+#endif
 +
++#if defined(__FreeBSD__) && __FreeBSD_version < 500000
++typedef __int32_t int_fast32_t;
++#endif
+ 
  extern void (*put_pixels_clamped_axp_p)(const DCTELEM *block, uint8_t *pixels,
                                          int line_size);
- extern void (*add_pixels_clamped_axp_p)(const DCTELEM *block, uint8_t *pixels, 
