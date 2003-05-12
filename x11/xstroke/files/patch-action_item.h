57fdefd302b62ef61f7ae117e3df4ca0006e46b6
anholt <anholt@FreeBSD.org>
2003-05-12 21:52:49 +0000

    Un-break after Xft and XFree86 4.3 changes.

---
+++ x11/xstroke/files/patch-action_item.h
@@ -0,0 +1,11 @@
+--- action_item.h.orig	Mon May 12 14:36:48 2003
++++ action_item.h	Mon May 12 14:36:59 2003
+@@ -18,7 +18,7 @@
+ 
+ #include <X11/X.h>
+ #define XK_MISCELLANY
+-#include <X11/keysymdef.h>
++#include <X11/keysym.h>
+ #include <X11/Xutil.h>
+ 
+ enum action_type
