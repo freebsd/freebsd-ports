1791f0066fd4f4318039217626afa5d9e6109362
Eric Anholt <anholt@FreeBSD.org>
2003-05-12 23:39:07 +0000

    Fix with new gcc by converting multiline string literals to concatenated strings.
    Reported by:	bento

--- sysutils/tree/files/patch-tree.c
+++ sysutils/tree/files/patch-tree.c
@@ -1,7 +1,20 @@
-$FreeBSD$
-
---- tree.c.orig	Mon Jan 29 22:38:30 2001
-+++ tree.c	Mon Dec 24 08:09:39 2001
+--- tree.c.orig	Mon May 12 16:44:19 2003
++++ tree.c	Mon May 12 16:44:24 2003
+@@ -22,10 +22,10 @@
+ #include <grp.h>
+ 
+ static char *version = "$Version: $ tree v1.3 (c) 1996 - 1999 by Steve Baker, Thomas Moore $";
+-static char *hversion= "tree v1.3 \251 1996 - 1999 by Steve Baker and Thomas Moore\
+-<BR>HTML output hacked and copyleft \251 1998 by Francesc Rocher\
+-<BR>This software is experimental. Send commends and/or
+-<BR>suggestions to <A HREF=\"mailto:rocher@econ.udg.es\">rocher@econ.udg.es</A>";
++static char *hversion= "tree v1.3 \251 1996 - 1999 by Steve Baker and Thomas Moore"
++"<BR>HTML output hacked and copyleft \251 1998 by Francesc Rocher"
++"<BR>This software is experimental. Send commends and/or\n"
++"<BR>suggestions to <A HREF=\"mailto:rocher@econ.udg.es\">rocher@econ.udg.es</A>";
+ 
+ #define scopy(x)	strcpy(xmalloc(strlen(x)+1),(x))
+ 
 @@ -417,7 +417,7 @@
        if (uflag && (gflag || sflag || Dflag)) fprintf(outfile,"%s",sp);
        if (gflag) fprintf(outfile,"%-8.8s",gidtoname((*dir)->gid));
@@ -11,3 +24,11 @@ $FreeBSD$
        if (sflag && Dflag) fprintf(outfile,"%s",sp);
        if (Dflag) fprintf(outfile,"%s",do_date((*dir)->mtime));
        if (pflag || sflag || uflag || gflag || Dflag) fprintf(outfile,"]%s%s",sp,sp);
+@@ -1022,6 +1022,7 @@
+ 	}
+       }
+     default:
++      ;
+     }
+   }
+ 
