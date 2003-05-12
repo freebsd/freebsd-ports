0fadf1e2a53a8a4d379c7fbac9f4ae0fa01caf84
anholt <anholt@FreeBSD.org>
2003-05-12 02:13:01 +0000

    Fix building with new gcc (multi-line string literals) and make the build actually respect CFLAGS.
    Reported by:	bento

---
+++ security/ddos_scan/files/patch-dds.c
@@ -0,0 +1,22 @@
+--- dds.c.orig	Sun May 11 18:44:07 2003
++++ dds.c	Sun May 11 18:48:34 2003
+@@ -12,8 +12,7 @@
+  * David Brumley <dbrumley@rtfm.stanford.edu>
+  */
+ 
+-#if YOU_HAVE_NOT_READ_THIS_YET
+-
++/*
+ This software should only be used in compliance with all applicable laws and
+ the policies and preferences of the owners of any networks, systems, or hosts
+ scanned with the software
+@@ -30,8 +29,7 @@
+ DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, TORT (INCLUDING NEGLIGENCE) OR STRICT LIABILITY, ARISING
+ OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.  
+-
+-#endif
++*/
+ 
+ #define VERSION "$Revision: 1.6 $"
+ 
