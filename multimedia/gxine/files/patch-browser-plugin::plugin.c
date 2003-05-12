cfed23cc713b937489d268f5693130df5991a5cf
ijliao <ijliao@FreeBSD.org>
2003-05-12 09:19:32 +0000

    make the browser plugin usable
    PR:		52004
    Submitted by:	maintainer

---
+++ multimedia/gxine/files/patch-browser-plugin::plugin.c
@@ -0,0 +1,10 @@
+--- browser-plugin/plugin.c.orig	Tue May  6 14:20:32 2003
++++ browser-plugin/plugin.c	Tue May  6 14:20:50 2003
+@@ -47,6 +47,7 @@
+ 
+ #include <stdio.h>
+ #include <math.h>
++#include <signal.h>
+ 
+ 
+ 
