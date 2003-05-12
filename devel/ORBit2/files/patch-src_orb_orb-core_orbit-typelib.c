d0de18de2059bcf9b1d18dd6564b9e3c810e54b9
marcus <marcus@FreeBSD.org>
2003-05-12 00:55:32 +0000

    Add some more module search paths.

---
+++ devel/ORBit2/files/patch-src_orb_orb-core_orbit-typelib.c
@@ -0,0 +1,11 @@
+--- src/orb/orb-core/orbit-typelib.c.orig	Sun May 11 20:37:14 2003
++++ src/orb/orb-core/orbit-typelib.c	Sun May 11 20:40:42 2003
+@@ -215,6 +215,8 @@
+ 	paths = g_ptr_array_sized_new (8);
+ 
+ 	g_ptr_array_add (paths, g_strdup (ORBIT_TYPELIB_DIR));
++	add_if_unique(paths, "%%X11BASE%%");
++	add_if_unique(paths, "%%LOCALBASE%%");
+ 
+ 	if ((path = g_getenv ("ORBIT_TYPELIB_PATH"))) {
+ 		char **strv;
