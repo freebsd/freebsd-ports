166a5bc2bc4e579da398abb78b67d7a94bd35f60
Edwin Groothuis <edwin@FreeBSD.org>
2003-05-12 09:00:53 +0000

    Upgrade to version 0.9.8

--- games/bomberclone/files/patch-src::configuration.c
+++ games/bomberclone/files/patch-src::configuration.c
@@ -1,20 +1,11 @@
---- src/configuration.c.orig	Mon Apr 28 06:56:46 2003
-+++ src/configuration.c	Thu May  8 23:27:11 2003
-@@ -5,6 +5,8 @@
- #ifdef _WIN32
- #include <winsock.h>
- #else
-+#include <sys/types.h>
-+#include <sys/time.h>
- #include <sys/socket.h>
- #endif
- 
-@@ -51,7 +53,7 @@
- 	gfx.bpp = 16;
+--- src/configuration.c.orig	Fri May  9 06:36:13 2003
++++ src/configuration.c	Mon May 12 18:46:54 2003
+@@ -48,7 +48,7 @@
+ 	gfx.random_tileset = 1;
      bman.fieldsize.x = 25;
      bman.fieldsize.y = 17;
--    sprintf (bman.datapath, "data/bomberclone.gfx");
-+    sprintf (bman.datapath, "@@LOCALBASE@@/share/bomberclone.gfx");
- };
- 
- int
+-    sprintf (bman.datapath, "data");
++    sprintf (bman.datapath, "@@LOCALBASE@@/share/bomberclone");
+     bman.fieldpath[0] = 0;
+ 	bman.random_map = 2;
+ 	init_map_tileset();
