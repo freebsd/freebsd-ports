d6ea489c9382b8b81c89dc90b9340bc9bd790005
Eric Anholt <anholt@FreeBSD.org>
2003-05-12 22:18:49 +0000

    Fix build with new gcc by converting multi-line string literal (copyright) to concatenated strings.

---
+++ mail/muttzilla/files/patch-mutzilla.c
@@ -0,0 +1,44 @@
+--- muttzilla.c.orig	Wed Jan 26 23:11:08 2000
++++ muttzilla.c	Mon May 12 15:22:48 2003
+@@ -26,23 +26,21 @@
+ */
+ 
+ char copyright[] =
+-
+-"
+-muttzilla  v0.40
+-
+-Copyright (C) 1999-2000 Brian D. Winters
+-
+-This library is free software; you can redistribute it and/or
+-modify it under the terms of the GNU Lesser General Public
+-License as published by the Free Software Foundation; either
+-version 2.1 of the License, or (at your option) any later version.
+-
+-This library is distributed in the hope that it will be useful,
+-but WITHOUT ANY WARRANTY; without even the implied warranty of
+-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-Lesser General Public License for more details.
+-
+-You should have received a copy of the GNU Lesser General Public
+-License along with this library; if not, write to the Free Software
+-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+-";
++"\n"
++"muttzilla  v0.40\n"
++"\n"
++"Copyright (C) 1999-2000 Brian D. Winters\n"
++"\n"
++"This library is free software; you can redistribute it and/or\n"
++"modify it under the terms of the GNU Lesser General Public\n"
++"License as published by the Free Software Foundation; either\n"
++"version 2.1 of the License, or (at your option) any later version.\n"
++"\n"
++"This library is distributed in the hope that it will be useful,\n"
++"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
++"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
++"Lesser General Public License for more details.\n"
++"\n"
++"You should have received a copy of the GNU Lesser General Public\n"
++"License along with this library; if not, write to the Free Software\n"
++"Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA\n";
