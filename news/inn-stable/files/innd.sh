e635aa96345e49003b0c7974667d8b21f791e70f
Dag-Erling Smørgrav <des@FreeBSD.org>
2003-05-13 14:43:32 +0000

    Update to 2.4.20030512.

--- news/inn-stable/files/innd.sh
+++ news/inn-stable/files/innd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if [ $# -eq 0 -o x$1 = xstart ]; then
-   if [ -x !!PREFIX!!/news/bin/rc.news -a -f !!PREFIX!!/news/db/history.pag ]; then
-      su news -c !!PREFIX!!/news/bin/rc.news && echo ' inn'
+   if [ -x !!PREFIX!!/news/bin/rc.news -a -f !!PREFIX!!/news/db/history.dir ]; then
+      su -fm news -c !!PREFIX!!/news/bin/rc.news && echo ' inn'
    fi
 fi
 if [ x$1 = xstop ]; then
-   !!PREFIX!!/news/bin/ctlinnd shutdown machine is going down
+      su -fm news -c '!!PREFIX!!/news/bin/rc.news stop'
 fi
