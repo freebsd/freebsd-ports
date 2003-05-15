95ca3a8f1f727f14ddb3124fbcafdccf40261b72
anholt <anholt@FreeBSD.org>
2003-05-15 20:16:27 +0000

    - Fix MASTER_SITES - Fix build on -current - Remove obsolete WWW: line
    PR:		ports/52137
    Submitted by:	Ports Fury

--- misc/gman/files/patch-gman.c
+++ misc/gman/files/patch-gman.c
@@ -1,20 +1,11 @@
---- gman.c.orig	Sat Mar 31 18:38:43 2001
-+++ gman.c	Sat Apr 14 05:10:14 2001
-@@ -83,7 +83,7 @@
- 	context = new AppContext();
- 	//	context->set_default_value("v_size",(void*)400);
- 	context->set_default_value("debuging","int",(void*)0);
--	context->set_default_value("man_paths","char*",(void*)"/usr/man:/usr/local/man:/usr/X11R6/man");
-+	context->set_default_value("man_paths","char*",(void*)"/usr/share/man:/usr/local/man:/usr/X11R6/man");
- 	context->set_default_value("display_section_policy","int",(void*)0);
- 	context->set_default_value("display_section","int",(void*)3);
- 	context->set_default_value("searching_mode","int",(void*)0);
-@@ -91,7 +91,7 @@
+--- gman.c.dist	Mon May 12 07:39:11 2003
++++ gman.c	Mon May 12 07:39:53 2003
+@@ -90,7 +90,7 @@
  	context->set_default_value("show_warning","int",(void*)0);
  	context->set_default_value("show_mode","int",(void*)0); // 0 = xterm, 1 = ghostview
- 	context->set_default_value("xterm_command","char*",(void*)"xterm"); // rxvt, Eterm also works
+ 	context->set_default_value("xterm_command","char*",(void*)"x-terminal-emulator");
 -	context->set_default_value("gv_command","char*",(void*)"gv");
 +	context->set_default_value("gv_command","char*",(void*)"ghostview");
- 	context->set_default_value("browser_command","char*",(void*)"netscape"); // kfm also works
+ 	context->set_default_value("browser_command","char*",(void*)"mozilla"); // kfm also works
  	context->set_default_value("cgi_host","char*",(void*)"localhost");
- 	context->set_default_value("cgi_location","char*",(void*)"/cgi-bin/gman/gman.pl");
+ 	context->set_default_value("cgi_location","char*",(void*)"/cgi-bin/gman.pl");
