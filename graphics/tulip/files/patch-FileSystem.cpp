2979d5f96aff817fdb8aad68fd0e45bd0192e36c
Pete Fritchman <petef@FreeBSD.org>
2003-05-17 01:56:02 +0000

    - update to 1.2.3 - sort pkg-plist
    PR:		51227
    Submitted by:	Ports Fury

--- graphics/tulip/files/patch-FileSystem.cpp
+++ graphics/tulip/files/patch-FileSystem.cpp
@@ -1,10 +1,11 @@
---- plugins/import/FileSystem.cpp.orig	Fri Jan 10 12:54:43 2003
-+++ plugins/import/FileSystem.cpp	Fri Jan 10 12:56:18 2003
+--- plugins/import/FileSystem.cpp.orig	Fri Nov  1 00:55:42 2002
++++ plugins/import/FileSystem.cpp	Sun Apr 20 01:10:32 2003
 @@ -1,8 +1,8 @@
 +#include <sys/types.h>
  #include <sys/timeb.h>
- #include<qfiledialog.h>
- #include "TulipPlugin.h"
+-#include<qfiledialog.h>
++#include <qfiledialog.h>
+ #include <tulip/TulipPlugin.h>
  #include <sys/stat.h>
 -#include <sys/types.h>
  #include <dirent.h>
