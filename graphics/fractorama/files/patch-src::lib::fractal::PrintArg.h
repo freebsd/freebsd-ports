51e5617a66a3ac1488c99be578abface7827a6d8
Pete Fritchman <petef@FreeBSD.org>
2003-05-18 22:39:55 +0000

    Update to 1.6.5.
    PR:		51512
    Submitted by:	Ports Fury

--- graphics/fractorama/files/patch-src::lib::fractal::PrintArg.h
+++ graphics/fractorama/files/patch-src::lib::fractal::PrintArg.h
@@ -1,6 +1,6 @@
---- lib/fractal/PrintArg.h.orig	Sat Dec  1 01:27:59 2001
-+++ lib/fractal/PrintArg.h	Sun Dec 22 07:00:54 2002
-@@ -1,9 +1,11 @@
+--- lib/fractal/PrintArg.h.orig	Sun Feb  2 08:45:00 2003
++++ lib/fractal/PrintArg.h	Mon Apr 28 04:26:13 2003
+@@ -1,10 +1,12 @@
  #ifndef _PRINT_ARG_H_
  #   define _PRINT_ARG_H_
  
@@ -9,6 +9,7 @@
 +
  class ComplexVariables;
  class DoubleVariables;
+ class ColorEnvironment;
 -class ostream;
  
  class PrintArg
