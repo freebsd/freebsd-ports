c4823e883be563337dde9dde23949cde6c3fce0f
Adam Weinberger <adamw@FreeBSD.org>
2003-05-18 09:43:17 +0000

    Fix build:
    Remove an ill-formed REINPLACE, and use a proper patch instead.
    
    Take an explicit "${WRKSRC}" out of a displayed message, and replace
    it with the value of ${WRKSRC} instead.
    
    Drop maintainership.

---
+++ games/mythserver/files/patch-users_new::main.c
@@ -0,0 +1,20 @@
+--- users_new/main.c.orig	Sun May 18 02:36:43 2003
++++ users_new/main.c	Sun May 18 02:37:47 2003
+@@ -97,16 +97,7 @@
+ 	NUMBER_OF_CLIENT_TYPES
+ };
+ 
+-#define CLIENT_DATA_HEADER \
+-	int socket; \
+-	long host; \
+-	short port; \
+-	short type; \
+-	char buffer[MAXIMUM_PACKET_LENGTH]; \
+-	short state; \
+-	void *next; \
+-	struct circular_queue incoming; \
+-	struct circular_queue outgoing;
++#define CLIENT_DATA_HEADER int socket; long host; short port; short type; char buffer[MAXIMUM_PACKET_LENGTH]; short state; void *next; struct circular_queue incoming; struct circular_queue outgoing;
+ 
+ struct client_data {
+ 	CLIENT_DATA_HEADER
