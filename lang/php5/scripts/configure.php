18ea1afefe75baf9e9d5a81ea4e2820433993faa
Norikatsu Shigemura <nork@FreeBSD.org>
2003-05-22 18:26:28 +0000

    Add completely new port system for PHP4 (4.3.2RC4).  Updated mod_php4 to 4.3.2RC4 release and added php4-cli and php4 ports, for apache module, command line interface, and both, respectively.  Too many changes (improvements, bug fixes and new features) to be listed here,
    PR:		ports/51945
    Submitted by:	Alex Dupre <sysadmin@alexdupre.com>

---
+++ lang/php5/scripts/configure.php
@@ -0,0 +1,102 @@
+#!/bin/sh
+# $FreeBSD$
+
+if [ -f ${WRKDIR}/Makefile.inc ]; then
+	exit
+fi
+
+tempfile=`/usr/bin/mktemp -t checklist`
+
+if [ "${BATCH}" ]; then
+	echo "${SEL_OPTIONS}" > $tempfile
+else
+	if [ -f "${OPTION_FILE}" ]; then
+		for i in `${CAT} ${OPTION_FILE}`
+		do
+			export $i
+		done
+	fi
+	/usr/bin/dialog --title "configuration options" --clear --checklist "\n\
+Please select desired options:" -1 -1 16 \
+BCMATH		"bc style precision math functions" ${WITH_BCMATH:-OFF} \
+BZIP2		"bzip2 library support" ${WITH_BZIP2:-OFF} \
+CALENDAR	"calendar conversion support" ${WITH_CALENDAR:-OFF} \
+CRACK		"crack support" ${WITH_CRACK:-OFF} \
+CTYPE		"ctype functions" ${WITH_CTYPE:-OFF} \
+CURL		"CURL support" ${WITH_CURL:-OFF} \
+DBASE		"dBase library support" ${WITH_DBASE:-OFF} \
+DBX		"dbx support" ${WITH_DBX:-OFF} \
+DOMXML		"DOM support" ${WITH_DOMXML:-OFF} \
+DOMXSLT		"DOM XSLT and EXSLT support (implies DOMXML)" ${WITH_DOMXSLT:-OFF} \
+EXIF		"EXIF support" ${WITH_EXIF:-OFF} \
+FILEPRO		"filePro support" ${WITH_FILEPRO:-OFF} \
+FTP		"FTP support" ${WITH_FTP:-OFF} \
+GD		"GD library support" ${WITH_GD:-OFF} \
+GDBM		"GDBM database support" ${WITH_GDBM:-OFF} \
+GETTEXT		"gettext library support" ${WITH_GETTEXT:-OFF} \
+GMP		"GNU MP support" ${WITH_GMP:-OFF} \
+HYPERWAVE	"Hyperwave support" ${WITH_HYPERWAVE:-OFF} \
+ICONV		"iconv support" ${WITH_ICONV:-OFF} \
+IMAP		"IMAP support" ${WITH_IMAP:-OFF} \
+INTERBASE	"Interbase 6 database support (Firebird)" ${WITH_INTERBASE:-OFF} \
+MBSTRING	"multibyte string support" ${WITH_MBSTRING:-OFF} \
+MCAL		"Modular Calendar Access Library support" ${WITH_MCAL:-OFF} \
+MCVE		"MCVE support" ${WITH_MCVE:-OFF} \
+MCRYPT		"Encryption support" ${WITH_MCRYPT:-OFF} \
+MHASH		"Crypto-hashing support" ${WITH_MHASH:-OFF} \
+MIME		"mime_magic support" ${WITH_MIME:-OFF} \
+MING		"ming shockwave flash support" ${WITH_MING:-OFF} \
+MYSQL		"MySQL database support" ${WITH_MYSQL:-OFF} \
+NCURSES		"ncurses support (CLI only)" ${WITH_NCURSES:-OFF} \
+OPENLDAP	"OpenLDAP support" ${WITH_OPENLDAP:-OFF} \
+OPENSSL		"OpenSSL support" ${WITH_OPENSSL:-OFF} \
+PCNTL		"pcntl support (CLI only)" ${WITH_PCNTL:-OFF} \
+PCRE		"Perl Compatible Regular Expression support" ${WITH_PCRE:-OFF} \
+PDFLIB		"PDFlib support" ${WITH_PDFLIB:-OFF} \
+POSIX		"POSIX-like functions" ${WITH_POSIX:-OFF} \
+POSTGRESQL	"PostgreSQL database support" ${WITH_POSTGRESQL:-OFF} \
+PSPELL		"pspell support" ${WITH_PSPELL:-OFF} \
+READLINE	"readline support (CLI only)" ${WITH_READLINE:-OFF} \
+RECODE		"recode support" ${WITH_RECODE:-OFF} \
+SESSION		"session support" ${WITH_SESSION:-OFF} \
+SHMOP		"shmop support" ${WITH_SHMOP:-OFF} \
+SNMP		"SNMP support" ${WITH_SNMP:-OFF} \
+SOCKETS		"sockets support" ${WITH_SOCKETS:-OFF} \
+SYBASEDB	"Sybase/MS-SQL database support (DB-lib)" ${WITH_SYBASEDB:-OFF} \
+SYBASECT	"Sybase/MS-SQL database support (CT-lib)" ${WITH_SYBASECT:-OFF} \
+SYSVSEM		"System V semaphore support" ${WITH_SYSVSEM:-OFF} \
+SYSVSHM		"System V shared memory support" ${WITH_SYSVSHM:-OFF} \
+TOKENIZER	"tokenizer support" ${WITH_TOKENIZER:-OFF} \
+UNIXODBC	"unixODBC support" ${WITH_UNIXODBC:-OFF} \
+WDDX		"WDDX support" ${WITH_WDDX:-OFF} \
+XML		"XML support" ${WITH_XML:-OFF} \
+XMLRPC		"XMLRPC-EPI support" ${WITH_XMLRPC:-OFF} \
+XSLT		"XSLT Sablotron support" ${WITH_XSLT:-OFF} \
+YAZ		"YAZ support (ANSI/NISO Z39.50)" ${WITH_YAZ:-OFF} \
+YP		"YP/NIS support" ${WITH_YP:-OFF} \
+ZIP		"ZIP support" ${WITH_ZIP:-OFF} \
+ZLIB		"ZLIB support" ${WITH_ZLIB:-OFF} \
+2> $tempfile
+fi
+
+retval=$?
+
+if [ -s $tempfile ]; then
+	set `${CAT} $tempfile | ${SED} 's|"||g'`
+fi
+rm -f $tempfile
+
+case $retval in
+	0)	if [ -z "$*" ]; then
+			echo "Nothing selected"
+		fi
+		;;
+	1)	echo "Cancel pressed."
+		exit 1
+		;;
+esac
+exec > ${WRKDIR}/Makefile.inc
+while [ $1 ]; do
+	echo "WITH_$1=yes";
+	shift
+done
---
+++ lang/php53/files/patch-pear::Makefile.frag
@@ -0,0 +1,22 @@
+--- pear/Makefile.frag.orig	Sat Apr 12 01:48:37 2003
++++ pear/Makefile.frag	Sat May 17 10:08:54 2003
+@@ -13,10 +13,13 @@
+ 
+ install-pear:
+ 	@echo "Installing PEAR environment:      $(INSTALL_ROOT)$(peardir)/"
+-	@if $(mkinstalldirs) $(INSTALL_ROOT)$(peardir); then \
+-		$(MAKE) -s install-pear-installer install-pear-packages; \
+-	else \
+-		cat $(srcdir)/install-pear.txt; \
+-		exit 5; \
+-	fi
++	@$(mkinstalldirs) $(INSTALL_ROOT)$(peardir)/bootstrap \
++		$(INSTALL_ROOT)$(peardir)/bootstrap/Archive \
++		$(INSTALL_ROOT)$(peardir)/bootstrap/Console
++	@cp $(srcdir)/PEAR.php $(srcdir)/System.php \
++		$(INSTALL_ROOT)$(peardir)/bootstrap
++	@cp $(srcdir)/Archive/*.php $(INSTALL_ROOT)$(peardir)/bootstrap/Archive
++	@cp $(srcdir)/Console/*.php $(INSTALL_ROOT)$(peardir)/bootstrap/Console
++	@cp -R $(srcdir)/OS $(INSTALL_ROOT)$(peardir)/bootstrap/OS
++	@cp -R $(srcdir)/PEAR $(INSTALL_ROOT)$(peardir)/bootstrap/PEAR
+ 
