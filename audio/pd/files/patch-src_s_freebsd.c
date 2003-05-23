4ef987ed223d197dc2efa5d21362f72f27f82fd0
Trevor Johnson <trevor@FreeBSD.org>
2003-05-23 01:29:04 +0000

    According to an unreliable source, when _POSIX_MEMLOCK is defined as -1, that means it is "never available."

--- audio/pd/files/patch-src_s_freebsd.c
+++ audio/pd/files/patch-src_s_freebsd.c
@@ -1,5 +1,7 @@
---- src/s_freebsd.c.orig	Sun Oct 20 02:49:11 2002
-+++ src/s_freebsd.c	Sun Oct 20 02:53:24 2002
+$FreeBSD$
+
+--- src/s_freebsd.c.orig	Fri May 23 00:52:34 2003
++++ src/s_freebsd.c	Fri May 23 01:01:59 2003
 @@ -32,7 +32,8 @@
   * 1311:forum::für::umläute:2001
   */
@@ -10,7 +12,18 @@
  
  #if (defined(ALSA01) || defined(ALSA99))
  #define ALSA
-@@ -865,7 +866,8 @@
+@@ -762,8 +762,10 @@
+ #endif
+ 
+ #ifdef _POSIX_MEMLOCK
++#if _POSIX_MEMLOCK != -1
+     if (mlockall(MCL_FUTURE) != -1) 
+     	fprintf(stderr, "memory locking enabled.\n");
++#endif
+ #endif
+ }
+ 
+@@ -865,7 +868,8 @@
      }
      else
      {
@@ -20,7 +33,7 @@
  	dev->d_bytespersamp = 2;
      }
      param = wantformat;
-@@ -2747,7 +2749,7 @@
+@@ -2747,7 +2751,7 @@
  
    /* use ctrlpanel to change, experiment, channels 1 */
  
