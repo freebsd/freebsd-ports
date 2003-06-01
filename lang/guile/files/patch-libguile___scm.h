733ff13c7b8280d6c67f77d176ce1b9c72f06002
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-06-01 20:39:23 +0000

    Correct a make patch error.

--- lang/guile/files/patch-libguile___scm.h
+++ lang/guile/files/patch-libguile___scm.h
@@ -1,12 +1,12 @@
---- libguile/__scm.h.orig	Thu May 22 22:07:27 2003
-+++ libguile/__scm.h	Thu May 22 21:03:18 2003
+--- libguile/__scm.h.orig	Sat Apr 12 14:16:08 2003
++++ libguile/__scm.h	Sun Jun  1 16:32:36 2003
 @@ -354,7 +354,8 @@
   * that all the state of the process is contained in the stack. 
   */
  
--#if (defined (sparc) || defined (__sparc__) || defined (__sparc))
+-#if defined (sparc) || defined (__sparc__) || defined (__sparc)
 +#if (defined (sparc) || defined (__sparc__) || defined (__sparc)) && \
-+	!defined(__FreeBSD__)
++    (!defined(__FreeBSD__))
  # define SCM_FLUSH_REGISTER_WINDOWS asm("ta 3")
  #else
  # define SCM_FLUSH_REGISTER_WINDOWS /* empty */
