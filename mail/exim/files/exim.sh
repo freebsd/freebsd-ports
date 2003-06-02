d21b983add41e7a9393a5d6929165db647b87d7e
sheldonh <sheldonh@FreeBSD.org>
2003-06-02 13:17:43 +0000

    * Update to exiscan-acl patch 08, which fixes wrong defaults for sophie   antivirus and adds clamd support.
    * Use the bz2 patch for exiscan-acl.
    
    * Add a CVS Id tag to exim.sh
      Add a reload command to exim.sh.
    
    Bump PORTREVISION accordingly.
    
    Submitted by:	Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- mail/exim/files/exim.sh
+++ mail/exim/files/exim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id$
+# $FreeBSD$
 #
 # Note that 'pidfile' may need to be changed if 'args' is altered; see
 # the description of the 'pid_file_path' Exim configuration option in
@@ -17,8 +17,11 @@ start)
 stop)
         kill `cat ${pidfile}` && echo -n ' exim'
         ;;
+reload)
+        kill -HUP `cat ${pidfile}` && echo -n ' exim'
+        ;;
 *)
-        echo "Usage: `basename $0` {start|stop}" >&2
+        echo "Usage: `basename $0` {start|stop|reload}" >&2
         exit 64
         ;;
 esac
