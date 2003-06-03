8ec396d052d830f33ac48f2daa8d6cc07981364d
will <will@FreeBSD.org>
2003-06-03 02:42:39 +0000

    Add hotkeys 0.5.7.1, a program that allows usage of special keys on internet/multimedia keyboards.
    PR:		52616
    Submitted by:	Andy Pavlo <amp0928@rit.edu>

---
+++ misc/hotkeys/files/patch-src-kbddef.h
@@ -0,0 +1,12 @@
+--- src/kbddef.h.orig	Mon Mar 19 01:51:24 2001
++++ src/kbddef.h	Sun May 11 16:01:17 2003
+@@ -55,6 +55,9 @@
+     myComputerKey,
+     favoritesKey,
+     calculatorKey,
++    messengerKey,
++    webcamKey,
++    mediaKey,
+     newsReaderKey,
+     iNewsKey,
+     rewindKey,
