506cd273a9eda613d37027b294879a69d8e7f813
mich <mich@FreeBSD.org>
2003-06-04 14:34:24 +0000

    Unbreak sendip build on -CURRENT.
    PR:		52584
    Submitted by:	maintainer
    Approved by:	roberto (mentor)

---
+++ net/sendip/files/patch-gnugetopt.c
@@ -0,0 +1,10 @@
+--- gnugetopt.c.orig	Thu May 22 08:35:25 2003
++++ gnugetopt.c	Thu May 22 08:35:37 2003
+@@ -45,6 +45,7 @@
+ #endif
+ 
+ #include <stdio.h>
++#include <string.h>
+ 
+ /* Comment out all this code if we are using the GNU C Library, and are not
+    actually compiling the library itself.  This code is part of the GNU C
