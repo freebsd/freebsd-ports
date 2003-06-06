3c4775003d3aa86d9843c293fbaa51a63a5829fc
Will Andrews <will@FreeBSD.org>
2003-06-06 05:20:17 +0000

    Update to 0.50.13.
    PR:		52968
    Submitted by:	maintainer

--- multimedia/dvdrip/files/patch-lib::Video::DVDRip::GUI::Project::TranscodeTab.pm
+++ multimedia/dvdrip/files/patch-lib::Video::DVDRip::GUI::Project::TranscodeTab.pm
@@ -1,11 +1,11 @@
---- lib/Video/DVDRip/GUI/Project/TranscodeTab.pm.orig	Fri Dec 20 12:15:47 2002
-+++ lib/Video/DVDRip/GUI/Project/TranscodeTab.pm	Fri Dec 20 12:16:11 2002
-@@ -218,7 +218,7 @@
+--- lib/Video/DVDRip/GUI/Project/TranscodeTab.pm.orig	Mon Jun  2 15:41:16 2003
++++ lib/Video/DVDRip/GUI/Project/TranscodeTab.pm	Mon Jun  2 16:04:28 2003
+@@ -1205,7 +1205,7 @@
+ 			->set_popdown_strings( "SVCD", "VCD" );
+ 	} else {
+ 		$widgets->{tc_video_codec_combo}
+-			->set_popdown_strings( "divx4","divx5","xvid","xvidcvs","ffmpeg","fame","af6" );
++			->set_popdown_strings( "xvid","ffmpeg","fame","af6" );
+ 	}
  
- 	$entry = Gtk::Combo->new;
- 	$entry->show;
--	$entry->set_popdown_strings ("SVCD","VCD","divx4","divx5","xvid","xvidcvs","ffmpeg","fame","af6");
-+	$entry->set_popdown_strings ("SVCD","VCD","xvid","ffmpeg","fame","af6");
- 	$entry->set_usize(80,undef);
- 	$hbox->pack_start($entry, 0, 1, 0);
- 
+ 	$widgets->{tc_video_codec}->set_text($title->tc_video_codec);
