d399afc00c26a06888bb5dd2aecd83b85d12e4ab
adamw <adamw@FreeBSD.org>
2003-06-06 03:18:22 +0000

    Add glMaze.
    glMaze is a multiplayer OpenGL maze game.  Up to six players hunt
    each other online in a real 3-D maze.

---
+++ games/glmaze/files/patch-shot.c
@@ -0,0 +1,10 @@
+--- shot.c.orig	Thu Jun  5 19:55:39 2003
++++ shot.c	Thu Jun  5 19:56:34 2003
+@@ -47,6 +47,7 @@
+   "/usr/games/glmaze/",
+   "/usr/local/games/glMaze/",
+   "/usr/local/games/glmaze/",
++  "%%PREFIX%%/share/glmaze/",
+   "./", 
+   ""
+ };
