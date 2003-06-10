01af5aadd9951ccae320bdc876f59613204b83b9
naddy <naddy@FreeBSD.org>
2003-06-10 16:24:30 +0000

    Update to 0.7.0.
    PR:		52577
    Submitted by:	Ports Fury

--- net-im/libyahoo2/files/patch-sample_client.c
+++ net-im/libyahoo2/files/patch-sample_client.c
@@ -1,6 +1,6 @@
---- src/sample_client.c.orig	Sat Apr 12 17:04:56 2003
-+++ src/sample_client.c	Sat Apr 12 17:05:25 2003
-@@ -51,7 +51,6 @@
+--- src/sample_client.c.orig	Sat May 17 03:33:06 2003
++++ src/sample_client.c	Wed May 21 08:38:45 2003
+@@ -52,7 +52,6 @@
  #include <yahoo2_callbacks.h>
  #include "yahoo_util.h"
  
@@ -8,3 +8,11 @@
  
  
  #define MAX_PREF_LEN 255
+@@ -186,7 +185,6 @@
+ 
+ FILE *popen(const char *command, const char *type);
+ int pclose(FILE *stream);
+-int gethostname(char *name, size_t len);
+ 
+ static char * get_local_addresses()
+ {
