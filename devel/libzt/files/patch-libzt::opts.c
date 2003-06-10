fbbfcc63c2fa792d18cb2e778ee4df279e520e24
Max Khon <fjoe@FreeBSD.org>
2003-06-10 09:27:41 +0000

    New port: devel/libzt
    Collection of utility code for C application/server
    development.
    
    PR:		53124
    Submitted by:	Kirill Ponomarew

---
+++ devel/libzt/files/patch-libzt::opts.c
@@ -0,0 +1,10 @@
+--- libzt/opts.c.orig	Tue Jun 10 01:23:36 2003
++++ libzt/opts.c	Tue Jun 10 01:24:00 2003
+@@ -14,6 +14,7 @@
+ 
+ #include <unistd.h>
+ #include <ctype.h>
++#include <getopt.h>
+ 
+ #include <libzt/zt.h>
+ #include <libzt/opts.h>
