257b51868831cfbd2171e5d0bc6ccbabc057c274
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-06-11 06:16:16 +0000

    Update to 1.4.0.  Note, this is now a GNOME 2 application.

--- mail/evolution/files/patch-camel_camel-mime-utils.c
+++ mail/evolution/files/patch-camel_camel-mime-utils.c
@@ -1,29 +1,11 @@
---- camel/camel-mime-utils.c.orig	Mon Oct  7 22:44:40 2002
-+++ camel/camel-mime-utils.c	Mon Dec  2 02:34:31 2002
-@@ -3387,6 +3387,8 @@
- 		return 0;
+--- camel/camel-mime-utils.c.orig	Fri May  3 15:47:41 2002
++++ camel/camel-mime-utils.c	Sun May  5 18:09:24 2002
+@@ -3388,6 +3388,8 @@
+ 		}
+ 		d(printf("named offset = %d\n", offset));
  	}
- 
-+	tm.tm_isdst = -1;
 +
- 	d(printf ("\ndecoding date '%s'\n", inptr));
++	tm.tm_isdst = -1;
  
- 	memset (&tm, 0, sizeof(tm));
-@@ -3739,8 +3741,6 @@
- 	/* X-Mailing-List: gnome-hackers */
- 	/* X-Mailing-List: <gnome-hackers> */
- 	{ "X-Mailing-List", "[ \t]*<?([^@>]+)@?([^ \n\t\r>]*)" },
--	/* X-Loop: gnome-hackers@gnome.org */
--	{ "X-Loop", "[ \t]*([^@]+)@?([^ \n\t\r>]*)" },
- 	/* List-Id: GNOME stuff <gnome-hackers.gnome.org> */
- 	/* List-Id: <gnome-hackers.gnome.org> */
- 	/* List-Id: <gnome-hackers> */
-@@ -3756,6 +3756,8 @@
- 	/* X-List: gnome-hackers */
- 	/* X-List: gnome-hackers@gnome.org */
- 	{ "X-List", "[ \t]*([^@]+)@?([^ \n\t\r>]*)" },	
-+	/* X-Loop: gnome-hackers@gnome.org */
-+	{ "X-Loop", "[ \t]*([^@]+)@?([^ \n\t\r>]*)" },
- 	/* Sender: owner-gnome-hackers@gnome.org */
- 	/* Sender: owner-gnome-hacekrs */
- 	{ "Sender", "[ \t]*owner-([^@]+)@?([^ @\n\t\r>]*)" },
+ 	t = mktime(&tm);
+ #if defined(HAVE_TIMEZONE)
