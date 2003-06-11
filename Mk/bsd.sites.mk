24d85a1a1772028e083f0cc371ac36f514913262
Trevor Johnson <trevor@FreeBSD.org>
2003-06-11 22:56:51 +0000

    Add ftp.meiji.ac.jp and ftp.ntnu.no to the TEX_CTAN list.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -576,9 +576,11 @@ MASTER_SITE_TEX_CTAN+=  \
 	ftp://ctan.unsw.edu.au/tex-archive/%SUBDIR%/ \
 	ftp://ftp.tex.ac.uk/tex-archive/%SUBDIR%/  \
 	ftp://ftp.kddlabs.co.jp/CTAN/%SUBDIR%/ \
+	ftp://ftp.meiji.ac.jp/pub/TeX/CTAN/%SUBDIR%/ \
 	ftp://ctan.tug.org/tex-archive/%SUBDIR%/ \
 	ftp://ftp.dante.de/tex-archive/%SUBDIR%/ \
 	ftp://gd.tuwien.ac.at/publishing/tex/CTAN/%SUBDIR%/ \
+	ftp://ftp.ntnu.no/pub/tex/CTAN/%SUBDIR%/ \
 	${MASTER_SITE_RINGSERVER:S,%SUBDIR%,text/CTAN/&,} \
 	ftp://ftp.chg.ru/pub/TeX/CTAN/%SUBDIR%/
 
