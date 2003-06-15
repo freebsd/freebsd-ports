b6c3bce36cb97432b6344b3c00d6f3cd6a156b5d
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-06-15 06:39:34 +0000

    Fix build on -CURRENT.
    Reported by:	Martin Klaffenboeck <martin.klaffenboeck@gmx.at>

---
+++ www/galeon2/files/patch-utils_gul-toolbar-bonobo-view.c
@@ -0,0 +1,10 @@
+--- utils/gul-toolbar-bonobo-view.c.orig	Sun Jun 15 02:34:00 2003
++++ utils/gul-toolbar-bonobo-view.c	Sun Jun 15 02:34:36 2003
+@@ -20,6 +20,7 @@
+ #include "config.h"
+ #endif
+ 
++#include <sys/types.h>
+ #include <libgnome/gnome-i18n.h>
+ #include "gul-gobject-misc.h"
+ #include "galeon-marshal.h"
