b95035a68731845d2ed086df7f54bfbbfe517ccf
Kris Kennaway <kris@FreeBSD.org>
2003-06-16 01:16:25 +0000

    Quiet compiler warnings on sparc64 by adding missing #includes

---
+++ x11-wm/pwm/files/patch-function.c
@@ -0,0 +1,10 @@
+--- function.c.orig	Sat Jul  1 04:21:29 2000
++++ function.c	Sun Jun 15 18:13:51 2003
+@@ -5,6 +5,7 @@
+  * See the included file LICENSE for details.
+  */
+ 
++#include <string.h>
+ #include "common.h"
+ #include "function.h"
+ #include "exec.h"
---
+++ x11-wm/pwm/files/patch-menudata.c
@@ -0,0 +1,10 @@
+--- menudata.c.orig	Fri May 19 14:40:29 2000
++++ menudata.c	Sun Jun 15 18:13:59 2003
+@@ -5,6 +5,7 @@
+  * See the included file LICENSE for details.
+  */
+ 
++#include <string.h>
+ #include "common.h"
+ #include "menu.h"
+ 
