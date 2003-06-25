c17abea2ccbd81046e60f50c2f1d50750281adc1
Erwin Lansing <erwin@FreeBSD.org>
2003-06-25 15:13:49 +0000

    Vendor update to 0.36
    -added support for different Ghostscript output devices (-dev)
    -updated Xpdf to 2.02 (security fix)
    -a couple of fixes and tweaks for better output
    -ported duplicate lines elimination from pdftotext (does not affect complex mode)
    -fixed bug which caused bold to spread from one sentence to the entire document
    -support for document outlines (patch by Nicolas Pitre)

---
+++ textproc/pdftohtml/files/patch-aconf.h
@@ -0,0 +1,11 @@
+--- aconf.h.orig	Wed Jun 25 00:41:27 2003
++++ aconf.h	Wed Jun 25 09:27:43 2003
+@@ -51,7 +51,7 @@
+ /* #undef HAVE_BSTRING_H */
+ #define HAVE_POPEN 1
+ #define HAVE_MKSTEMP 1
+-#define HAVE_MKSTEMPS 0
++#define HAVE_MKSTEMPS 1
+ /* #undef SELECT_TAKES_INT */
+ #define GHOSTSCRIPT "gs"
+ 
