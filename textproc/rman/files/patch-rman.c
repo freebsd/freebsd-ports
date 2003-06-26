fe7721a8d60c311613dbff4a34011ce59b13e313
Adam Weinberger <adamw@FreeBSD.org>
2003-06-26 01:39:16 +0000

    Utilize ${MASTER_SITE_SOURCEFORGE}, instead of attempting to download distfiles from an ftp space that doesn't exist.
    Also, fix patches on machines other than tg's ::)

--- textproc/rman/files/patch-rman.c
+++ textproc/rman/files/patch-rman.c
@@ -1,6 +1,6 @@
-diff -c /home/tg/work/FreeBSD/ports/local/rman/work/rman-3.1/rman.c.orig /home/tg/work/FreeBSD/ports/local/rman/work/rman-3.1/rman.c
---- /home/tg/work/FreeBSD/ports/local/rman/work/rman-3.1/rman.c.orig	Sun Mar 30 00:45:18 2003
-+++ /home/tg/work/FreeBSD/ports/local/rman/work/rman-3.1/rman.c	Mon May 12 18:59:41 2003
+diff -c rman.c.orig rman.c
+--- rman.c.orig	Sun Mar 30 00:45:18 2003
++++ rman.c	Mon May 12 18:59:41 2003
 @@ -3729,11 +3729,15 @@
  	   q=strchr(p, ' ');
  	   if (q!=NULL) *q++='\0';
