2f6c676c40217fd7db7bc9568031f0c0f3bd02c4
leeym <leeym@FreeBSD.org>
2003-06-26 09:43:59 +0000

    + update to gbfs-1.3 + add NTFS support + refine Makefile and gbfs.sh
    PR:		53731
    Submitted by:	Statue <statue@freebsd.sinica.edu.tw>

--- chinese/gbfs/files/gbfs.sh
+++ chinese/gbfs/files/gbfs.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
+	echo "$0: Cannot determine the PREFIX" >&2
+	exit 1
+fi
+
 case "$1" in
 start)
-	if [ -x @@PREFIX@@/modules/cd9660.ko ]; then
+	if [ -x ${PREFIX}/modules/cd9660.ko ]; then
 		echo -n ' GB-cd9660'
 		kldload /usr/local/modules/cd9660.ko
 	fi
-	if [ -x @@PREFIX@@/modules/msdos.ko ]; then
+	if [ -x ${PREFIX}/modules/msdos.ko ]; then
 		echo -n ' GB-msdos'
 		kldload /usr/local/modules/msdos.ko
 	fi
 	# msdosfs.ko instead of msdos.ko after FreeBSD 5.0-CURRENT
-	if [ -x @@PREFIX@@/modules/msdosfs.ko ]; then
+	if [ -x ${PREFIX}/modules/msdosfs.ko ]; then
 		echo -n ' GB-msdosfs'
 		kldload /usr/local/modules/msdosfs.ko
 	fi
-	if [ -x @@PREFIX@@/modules/ntfs.ko ]; then
+	if [ -x ${PREFIX}/modules/ntfs.ko ]; then
 		echo -n ' GB-ntfs'
 		kldload /usr/local/modules/ntfs.ko
 	fi
@@ -27,7 +32,7 @@ stop)
 	kldunload -v -n ntfs && echo -n ' GB-ntfs'
 	;;
 *)
-	echo "Usage: big5fs.sh {start|stop}" >&2
+	echo "Usage: gbfs.sh {start|stop}" >&2
 	exit 1
 	;;
 esac
