43824e954e8cd1125175bdfd70ed22d6f46fc5db
leeym <leeym@FreeBSD.org>
2003-06-26 17:07:24 +0000

    o assign maintainership o update to 2.3
    PR:		53737
    Submitted by:	Statue <statue@freebsd.sinica.edu.tw>

--- chinese/big5fs/files/big5fs.sh
+++ chinese/big5fs/files/big5fs.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
+	echo "$0: Cannot determine the PREFIX" >&2
+	exit 1
+fi
+
 case "$1" in
 start)
-	if [ -x @@PREFIX@@/modules/cd9660.ko ]; then
+	if [ -x ${PREFIX}/modules/cd9660.ko ]; then
 		echo -n ' Big5-cd9660'
 		kldload /usr/local/modules/cd9660.ko
 	fi
-	if [ -x @@PREFIX@@/modules/msdos.ko ]; then
+	if [ -x ${PREFIX}/modules/msdos.ko ]; then
 		echo -n ' Big5-msdos'
 		kldload /usr/local/modules/msdos.ko
 	fi
 	# msdosfs.ko instead of msdos.ko after FreeBSD 5.0-CURRENT
-	if [ -x @@PREFIX@@/modules/msdosfs.ko ]; then
+	if [ -x ${PREFIX}/modules/msdosfs.ko ]; then
 		echo -n ' Big5-msdosfs'
 		kldload /usr/local/modules/msdosfs.ko
 	fi
-	if [ -x @@PREFIX@@/modules/ntfs.ko ]; then
+	if [ -x ${PREFIX}/modules/ntfs.ko ]; then
 		echo -n ' Big5-ntfs'
 		kldload /usr/local/modules/ntfs.ko
 	fi
@@ -27,7 +32,7 @@ stop)
 	kldunload -v -n ntfs && echo -n ' Big5-ntfs'
 	;;
 *)
-	echo "Usage: big5fs.sh {start|stop}" >&2
+	echo "Usage: $0 {start|stop}" >&2
 	exit 1
 	;;
 esac
