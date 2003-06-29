7bc88ffcd4321d101c170ac4086222709724ac6e
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-06-29 17:38:52 +0000

    Fix build on -CURRENT by conditionally using va_copy() where available.

--- www/epiphany/files/patch-lib_ephy-node.c
+++ www/epiphany/files/patch-lib_ephy-node.c
@@ -1,17 +1,21 @@
---- lib/ephy-node.c.orig	Sat Jun 28 11:03:58 2003
-+++ lib/ephy-node.c	Sat Jun 28 11:04:22 2003
-@@ -115,12 +115,12 @@
+--- lib/ephy-node.c.orig	Mon Jun  9 19:02:09 2003
++++ lib/ephy-node.c	Sun Jun 29 02:01:59 2003
+@@ -115,12 +115,16 @@
  static void
  callback (long id, EphyNodeSignalData *data, gpointer *user_data)
  {
 -        va_list valist;
-+        _BSD_VA_LIST_ valist;
++	va_list valist;
  	EphyNodeSignalType type = GPOINTER_TO_INT (user_data[1]);
  
  	if (data->type != type) return;
  
 -        va_copy(valist, user_data[0]);
-+        valist = user_data[0];
++#if __FreeBSD_version >= 500040
++	va_copy(valist, user_data[0]);
++#else
++	valist = (va_list) user_data[0];
++#endif
  
  	switch (data->type)
  	{
