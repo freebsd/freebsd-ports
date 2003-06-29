8762b03b9b9f554d9cb7701bbee77f5b4e2b5648
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-06-29 20:35:20 +0000

    Update to 0.7, and update the WWW link.
    PR:		53905
    Submitted by:	maintainer

---
+++ devel/devhelp/files/patch-ltmain.sh
@@ -0,0 +1,51 @@
+--- ltmain.sh.orig	Sun Jun 29 11:51:47 2003
++++ ltmain.sh	Sun Jun 29 21:02:37 2003
+@@ -1280,7 +1280,7 @@
+ 	  esac
+ 	elif test "X$arg" = "X-lc_r"; then
+ 	 case $host in
+-	 *-*-openbsd* | *-*-freebsd*)
++	 *-*-openbsd* | *-*-freebsd4*)
+ 	   # Do not include libc_r directly, use -pthread flag.
+ 	   continue
+ 	   ;;
+@@ -1290,8 +1290,16 @@
+ 	continue
+ 	;;
+ 
++      -pthread)
++      compile_command="$compile_command -pthread"
++      finalize_command="$finalize_command -pthread"
++      compiler_flags="$compiler_flags -pthread"
++      continue
++      ;;
++
+       -module)
+ 	module=yes
++	build_old_libs=no
+ 	continue
+ 	;;
+ 
+@@ -3000,6 +3008,9 @@
+ 	    # problems, so we reset it completely
+ 	    verstring=
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    verstring="0.0"
+ 	    ;;
+@@ -5428,10 +5439,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
--- devel/devhelp/files/patch-src::dh-base.c
+++ devel/devhelp/files/patch-src::dh-base.c
@@ -1,10 +1,10 @@
---- src/dh-base.c.orig	Tue Jun  3 12:21:34 2003
-+++ src/dh-base.c	Tue Jun  3 12:23:04 2003
-@@ -155,6 +155,7 @@
+--- src/dh-base.c.orig	Sat Jun 28 22:16:34 2003
++++ src/dh-base.c	Sun Jun 29 13:40:23 2003
+@@ -222,6 +222,7 @@
  	
- 	/* Insert the books from default gtk-doc install path */
- 	base_add_books (base, DATADIR"/gtk-doc/html");
-+	base_add_books (base, DATADIR"/../doc"); // FreeBSD default doc path
+ 	/* Insert the books from default gtk-doc install path. */
+ 	base_add_books (base, DATADIR "/gtk-doc/html");
++	base_add_books (base, DATADIR "/../doc"); // FreeBSD default doc path
  	base_add_books (base, "/usr/share/gtk-doc/html");
- 	base_add_books (base, DATADIR"/devhelp/books");
- 	dir = g_strconcat (g_getenv ("HOME"), "/.devhelp/books", NULL);
+ 	base_add_books (base, DATADIR "/devhelp/books");
+ 	dir = g_build_filename (g_get_home_dir (), ".devhelp", "books", NULL);
