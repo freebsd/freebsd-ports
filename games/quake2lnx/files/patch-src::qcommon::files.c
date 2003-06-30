a08e361503a84a378de8c405b747a9d80e46bf60
osa <osa@FreeBSD.org>
2003-06-30 08:55:39 +0000

    Fix latest update
    Submitted by:	maintainer
    PR:		53893
    Approved by:	fjoe (mentor) (implicit)

--- games/quake2lnx/files/patch-src::qcommon::files.c
+++ games/quake2lnx/files/patch-src::qcommon::files.c
@@ -1,5 +1,5 @@
---- qcommon/files.c.orig	Wed Oct  9 03:54:25 2002
-+++ qcommon/files.c	Sat Jun 28 12:30:57 2003
+--- src/qcommon/files.c.orig	Wed Oct  9 03:54:25 2002
++++ src/qcommon/files.c	Sat Jun 28 12:30:57 2003
 @@ -898,7 +898,11 @@
  	// basedir <path>
  	// allows the game to run from outside the data tree
