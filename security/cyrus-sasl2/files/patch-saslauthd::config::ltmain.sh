21dc6dfcc7d2d46eea63fa6226a35b8ef0fab684
ume <ume@FreeBSD.org>
2003-06-30 16:51:00 +0000

    Update to 2.1.14.

--- security/cyrus-sasl2/files/patch-saslauthd::config::ltmain.sh
+++ security/cyrus-sasl2/files/patch-saslauthd::config::ltmain.sh
@@ -1,24 +1,8 @@
---- saslauthd/config/ltmain.sh.orig	Tue Dec  4 03:06:57 2001
-+++ saslauthd/config/ltmain.sh	Tue Apr 15 02:33:18 2003
-@@ -1861,7 +1861,7 @@
- 	  if test $? -eq 0 ; then
- 	    ldd_output=`ldd conftest`
- 	    for i in $deplibs; do
--	      name="`expr $i : '-l\(.*\)'`"
-+	      name="`expr X$i : 'X-l\(.*\)'`"
- 	      # If $name is empty we are operating on a -L argument.
- 	      if test "$name" != "" ; then
- 		libname=`eval \\$echo \"$libname_spec\"`
-@@ -1886,7 +1886,7 @@
- 	    # Error occured in the first compile.  Let's try to salvage the situation:
- 	    # Compile a seperate program for each library.
- 	    for i in $deplibs; do
--	      name="`expr $i : '-l\(.*\)'`"
-+	      name="`expr X$i : 'X-l\(.*\)'`"
- 	     # If $name is empty we are operating on a -L argument.
- 	      if test "$name" != "" ; then
- 		$rm conftest
-@@ -1926,7 +1926,7 @@
+Index: saslauthd/config/ltmain.sh
+diff -u saslauthd/config/ltmain.sh.orig saslauthd/config/ltmain.sh
+--- saslauthd/config/ltmain.sh.orig	Wed Apr 16 07:28:26 2003
++++ saslauthd/config/ltmain.sh	Tue Jul  1 01:24:38 2003
+@@ -1959,7 +1959,7 @@
  	  set dummy $deplibs_check_method
  	  file_magic_regex="`expr \"$deplibs_check_method\" : \"$2 \(.*\)\"`"
  	  for a_deplib in $deplibs; do
