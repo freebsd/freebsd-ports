1f8cd43b7357f32439b1fd1a826951ba69d51798
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-06-30 17:15:20 +0000

    Add alsaplayer, an audio player with pitch control and a GTK+ GUI.
    PR:		43142
    Submitted by:	Robin Schilham <co9@xs4all.nl> (based on)

---
+++ audio/alsaplayer/files/patch-input::sndfile::sndfile_engine.c
@@ -0,0 +1,10 @@
+--- input/sndfile/sndfile_engine.c-orig	Tue Jun 17 16:43:57 2003
++++ input/sndfile/sndfile_engine.c	Tue Jun 17 16:44:12 2003
+@@ -20,7 +20,6 @@
+  */
+ 
+ #include <stdio.h>
+-#include <alloca.h>
+ #include <stdlib.h>
+ #include <limits.h>
+ #include <string.h>
---
+++ audio/alsaplayer/files/patch-input_audiofile_audiofile_engine.c
@@ -0,0 +1,10 @@
+--- input/audiofile/audiofile_engine.c.orig	Sun Jun 29 21:24:22 2003
++++ input/audiofile/audiofile_engine.c	Sun Jun 29 21:24:26 2003
+@@ -18,7 +18,6 @@
+  */
+ 
+ #include <stdio.h>
+-#include <alloca.h>
+ #include <stdlib.h>
+ #include <limits.h>
+ #include <string.h>
