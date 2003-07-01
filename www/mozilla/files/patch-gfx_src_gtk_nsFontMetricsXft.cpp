ee2b6b93ef0bcc2cdcc97552d9edc02374eba512
marcus <marcus@FreeBSD.org>
2003-07-01 06:14:02 +0000

    * Update to 1.4 * Mark IGNORE on Alpha -STABLE

--- www/mozilla/files/patch-gfx_src_gtk_nsFontMetricsXft.cpp
+++ www/mozilla/files/patch-gfx_src_gtk_nsFontMetricsXft.cpp
@@ -1,6 +1,6 @@
---- gfx/src/gtk/nsFontMetricsXft.cpp.orig	Mon Mar 31 12:30:39 2003
-+++ gfx/src/gtk/nsFontMetricsXft.cpp	Mon Mar 31 12:30:58 2003
-@@ -84,6 +84,7 @@
+--- gfx/src/gtk/nsFontMetricsXft.cpp.orig	Thu May 29 19:10:54 2003
++++ gfx/src/gtk/nsFontMetricsXft.cpp	Mon Jun 30 23:11:39 2003
+@@ -90,6 +90,7 @@
      FcPattern *mPattern;
      FcPattern *mFontName;
      FcCharSet *mCharset;
@@ -8,43 +8,25 @@
  };
  
  struct MozXftLangGroup {
-@@ -375,7 +376,7 @@
-         PRInt32 end = mLoadedFonts.Count();
-         for (PRInt32 j = 0; j < end; ++j) {
-             font = (nsFontXft *)mLoadedFonts.ElementAt(j);
--            if (FcCharSetHasChar(font->mCharset, c)) {
-+            if (FcCharSetHasChar(font->mCharset, c) && font->GetXftFont()) {
-                 currFont = font;
-                 goto FoundFont; // for speed -- avoid "if" statement
-             }
-@@ -635,7 +636,7 @@
- 
-         for (PRInt32 j = 0, end = mLoadedFonts.Count(); j < end; ++j) {
-             font = (nsFontXft *)mLoadedFonts.ElementAt(j);
--            if (FcCharSetHasChar(font->mCharset, c)) {
-+            if (FcCharSetHasChar(font->mCharset, c) && font->GetXftFont()) {
-                 currFont = font;
-                 goto FoundFont; // for speed -- avoid "if" statement
-             }
-@@ -888,7 +889,7 @@
+@@ -774,7 +775,7 @@
      // font in our loaded list that supports the character
      for (PRInt32 i = 0, end = mLoadedFonts.Count(); i < end; ++i) {
          nsFontXft *font = (nsFontXft *)mLoadedFonts.ElementAt(i);
 -        if (FcCharSetHasChar(font->mCharset, aChar))
-+        if (FcCharSetHasChar(font->mCharset, aChar) && font->GetXftFont() != NULL)
++        if (FcCharSetHasChar(font->mCharset, aChar) && font->GetXftFont())
              return font;
      }
  
-@@ -1051,7 +1052,7 @@
- 
+@@ -1196,7 +1197,7 @@
          for (PRInt32 j = 0, end = mLoadedFonts.Count(); j < end; ++j) {
+             nsFontXft *font;
              font = (nsFontXft *)mLoadedFonts.ElementAt(j);
 -            if (FcCharSetHasChar(font->mCharset, c)) {
 +            if (FcCharSetHasChar(font->mCharset, c) && font->GetXftFont()) {
-                 currFont = font;
-                 goto FoundFont; // for speed -- avoid "if" statement
+                 foundFont = font;
+                 break;
              }
-@@ -1461,6 +1462,7 @@
+@@ -1566,6 +1567,7 @@
      FcPatternReference(mFontName);
  
      mXftFont = nsnull;
@@ -52,7 +34,7 @@
  
      // set up our charset
      mCharset = nsnull;
-@@ -1487,14 +1489,16 @@
+@@ -1592,7 +1594,7 @@
  XftFont *
  nsFontXft::GetXftFont(void)
  {
@@ -61,13 +43,15 @@
          FcPattern *pat = FcFontRenderPrepare(0, mPattern, mFontName);
          if (!pat)
              return nsnull;
+@@ -1611,8 +1613,10 @@
+             FcPatternDel(pat, FC_SPACING);
  
          mXftFont = XftFontOpenPattern(GDK_DISPLAY(), pat);
 -        if (!mXftFont)
-+        if (!mXftFont) { 
++        if (!mXftFont) {
              FcPatternDestroy(pat);
-+	    mNotXft = 1;
-+	}
++            mNotXft = 1;
++        }
      }
  
      return mXftFont;
--- www/mozilla/files/patch-nsprpub_pr_src_misc_prtime.c
+++ www/mozilla/files/patch-nsprpub_pr_src_misc_prtime.c
@@ -1,18 +1,11 @@
---- nsprpub/pr/src/misc/prtime.c.orig	Sun Mar 31 12:17:46 2002
-+++ nsprpub/pr/src/misc/prtime.c	Tue Dec 17 21:30:30 2002
-@@ -1666,12 +1666,13 @@
-  * values for these two fields.
+--- nsprpub/pr/src/misc/prtime.c.orig	Wed Feb 26 19:01:02 2003
++++ nsprpub/pr/src/misc/prtime.c	Mon Jun 30 23:18:29 2003
+@@ -1666,7 +1666,7 @@
   */
  
--#if defined(SUNOS4) || (__GLIBC__ >= 2) || defined(XP_BEOS)
-+#if defined(__FreeBSD__)|| defined(SUNOS4) \
-+		|| (__GLIBC__ >= 2) || defined(XP_BEOS)
-     if (mktime(&a) == -1) {
-         PR_snprintf(buf, buflen, "can't get timezone");
-         return 0;
-     }
--#endif
-+#endif /* check for __FreeBSD__, too <alane@freebsd.org> */
- 
-     return strftime(buf, buflen, fmt, &a);
- }
+ #if defined(SUNOS4) || (__GLIBC__ >= 2) || defined(XP_BEOS) \
+-        || defined(NETBSD)
++        || defined(NETBSD) || defined(FREEBSD)
+     a.tm_zone = NULL;
+     a.tm_gmtoff = tm->tm_params.tp_gmt_offset + tm->tm_params.tp_dst_offset;
+ #endif
