701cdee42ad3c86a059c1893ba4748a79698e63b
Daichi GOTO <daichi@FreeBSD.org>
2003-07-01 13:33:27 +0000

    update games/adgali: 0.1.0 --> 0.2.2
    PR:		53580
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

---
+++ games/adgali/files/patch-tools::agl_bfont.c
@@ -0,0 +1,10 @@
+--- tools/agl_bfont.c.orig	Sun May 11 02:44:58 2003
++++ tools/agl_bfont.c	Sat Jun 21 15:18:43 2003
+@@ -40,7 +40,6 @@
+ #include <sys/types.h>
+ #include <sys/time.h>
+ #include <unistd.h>
+-#include <getopt.h>
+ #include <string.h>
+ #include <ctype.h>
+ #include "../config.h"
