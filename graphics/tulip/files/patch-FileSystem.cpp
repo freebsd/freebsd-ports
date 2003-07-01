99acda5608e9e747e76974db882f6807990197fd
daichi <daichi@FreeBSD.org>
2003-07-01 06:14:29 +0000

    update graphics/tulip: 1.2.3 --> 1.2.4
    PR:		52697
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

--- graphics/tulip/files/patch-FileSystem.cpp
+++ graphics/tulip/files/patch-FileSystem.cpp
@@ -1,13 +1,12 @@
---- plugins/import/FileSystem.cpp.orig	Fri Nov  1 00:55:42 2002
-+++ plugins/import/FileSystem.cpp	Sun Apr 20 01:10:32 2003
+--- plugins/import/FileSystem.cpp.orig	Sat Dec  7 00:39:53 2002
++++ plugins/import/FileSystem.cpp	Mon May 26 01:24:45 2003
 @@ -1,8 +1,8 @@
 +#include <sys/types.h>
  #include <sys/timeb.h>
--#include<qfiledialog.h>
-+#include <qfiledialog.h>
+ #include <qfiledialog.h>
  #include <tulip/TulipPlugin.h>
  #include <sys/stat.h>
 -#include <sys/types.h>
  #include <dirent.h>
  
- struct FileSystem:public ImportModule
+ using namespace std;
---
+++ graphics/tulip/files/patch-IdLabels.cpp
@@ -0,0 +1,9 @@
+--- plugins/string/IdLabels.cpp.orig	Thu Apr 10 21:57:47 2003
++++ plugins/string/IdLabels.cpp	Mon May 26 10:27:11 2003
+@@ -1,5 +1,6 @@
+ //-*-c++-*-
+ #include <tulip/TulipPlugin.h>
++#include <cstdio>
+ #include <string>
+ using namespace std;
+ 
