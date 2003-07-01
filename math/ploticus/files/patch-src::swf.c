05284ee12724b8a28a93a21534cdb8191a12e1ae
Daichi GOTO <daichi@FreeBSD.org>
2003-07-01 07:25:59 +0000

    update math/ploticus:   -  Add graphics/ming to LIB_DEPENDS
    PR:		52932
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

---
+++ math/ploticus/files/patch-src::swf.c
@@ -0,0 +1,11 @@
+--- swf.c.orig	Tue May 20 02:55:58 2003
++++ swf.c	Wed Jun  4 09:41:53 2003
+@@ -80,7 +80,7 @@
+ static void swf_TX(char *s);
+ static void swf_LS(char *s);
+ static void swf_PO(char *s);
+-static void swf_FT(char *s);
++/* static void swf_FT(char *s); */
+ static void swf_TR(char *s);
+ 
+ /* ============================= */
