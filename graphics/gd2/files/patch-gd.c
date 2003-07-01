4e408d2956b4a2a482b2feed8cb2851a3f604595
Daichi GOTO <daichi@FreeBSD.org>
2003-07-01 05:03:52 +0000

    update graphics/gd2: 2.0.11 --> 2.0.12
    PR:		52235
    Submitted by:	Mathieu Arnold <m@absolight.net> (maintainer)

--- graphics/gd2/files/patch-gd.c
+++ graphics/gd2/files/patch-gd.c
@@ -1,12 +1,9 @@
---- gd.c	Thu Apr 25 14:15:15 2002
-+++ gd.c	Thu Apr 25 12:43:55 2002
-@@ -2211,7 +2230,17 @@
- }
- 
+--- gd.c.orig	Sat Mar 29 17:25:42 2003
++++ gd.c	Wed May 14 16:46:34 2003
+@@ -2696,6 +2696,16 @@
  void
--gdImagePolygon (gdImagePtr im, gdPointPtr p, int n, int c)
-+gdImagePolygon(gdImagePtr im, gdPointPtr p, int n, int c)
-+{
+ gdImagePolygon (gdImagePtr im, gdPointPtr p, int n, int c)
+ {
 +	if (!n) {
 +		return;
 +	}
@@ -16,25 +13,15 @@
 +
 +void
 +gdImageOpenPolygon (gdImagePtr im, gdPointPtr p, int n, int c)
- {
++{
    int i;
    int lx, ly;
-@@ -2221,7 +2250,6 @@
-     }
+   if (!n)
+@@ -2710,7 +2720,6 @@
+ 
    lx = p->x;
    ly = p->y;
 -  gdImageLine (im, lx, ly, p[n - 1].x, p[n - 1].y, c);
    for (i = 1; (i < n); i++)
      {
        p++;
-@@ -2461,8 +2489,8 @@
-     {
-       for (x = 0; (x < sx); x++)
- 	{
--	  p1 = im1->pixels[y][x];
--	  p2 = im2->pixels[y][x];
-+	  p1 = im1->trueColor ? gdImageTrueColorPixel(im1, x, y) : gdImagePalettePixel(im1, x, y);
-+	  p2 = im2->trueColor ? gdImageTrueColorPixel(im2, x, y) : gdImagePalettePixel(im2, x, y);
- 	  if (gdImageRed (im1, p1) != gdImageRed (im2, p2))
- 	    {
- 	      cmpStatus |= GD_CMP_COLOR + GD_CMP_IMAGE;
