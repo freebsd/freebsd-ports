3daa1a78aadc8353295e9bc0890cb8ade32b5bd6
Hajimu UMEMOTO <ume@FreeBSD.org>
2003-07-02 09:42:15 +0000

    Reduce warnings during build on 5-CURRENT.
    Submitted by:	Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- security/cyrus-sasl2/files/patch-config::ltmain.sh
+++ security/cyrus-sasl2/files/patch-config::ltmain.sh
@@ -1,7 +1,25 @@
 Index: config/ltmain.sh
 diff -u config/ltmain.sh.orig config/ltmain.sh
 --- config/ltmain.sh.orig	Thu Jun 12 09:32:43 2003
-+++ config/ltmain.sh	Tue Jul  1 01:07:11 2003
++++ config/ltmain.sh	Wed Jul  2 11:50:04 2003
+@@ -1901,7 +1901,7 @@
+ 		deplib_matches=`eval \\$echo \"$library_names_spec\"`
+ 		set dummy $deplib_matches
+ 		deplib_match=$2
+-		if test `expr "$ldd_output" : ".*$deplib_match"` -ne 0 ; then
++		if test `expr "X$ldd_output" : "X.*$deplib_match"` -ne 0 ; then
+ 		  newdeplibs="$newdeplibs $i"
+ 		else
+ 		  droppeddeps=yes
+@@ -1931,7 +1931,7 @@
+ 		  deplib_matches=`eval \\$echo \"$library_names_spec\"`
+ 		  set dummy $deplib_matches
+ 		  deplib_match=$2
+-		  if test `expr "$ldd_output" : ".*$deplib_match"` -ne 0 ; then
++		  if test `expr "X$ldd_output" : "X.*$deplib_match"` -ne 0 ; then
+ 		    newdeplibs="$newdeplibs $i"
+ 		  else
+ 		    droppeddeps=yes
 @@ -1959,7 +1959,7 @@
  	  set dummy $deplibs_check_method
  	  file_magic_regex="`expr \"$deplibs_check_method\" : \"$2 \(.*\)\"`"
