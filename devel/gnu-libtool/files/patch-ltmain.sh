aba51095cc77e2ae5dd655d80528e86562ee0439
Ade Lovett <ade@FreeBSD.org>
2003-07-02 18:26:53 +0000

    Upgrade to libtool 1.5 after repo-copy.

--- devel/gnu-libtool/files/patch-ltmain.sh
+++ devel/gnu-libtool/files/patch-ltmain.sh
@@ -1,11 +1,7 @@
---- ltmain.sh.orig	Fri Jun 27 19:46:40 2003
-+++ ltmain.sh	Fri Jun 27 19:46:52 2003
-@@ -1061,7 +1061,7 @@
- 	  esac
- 	 elif test "X$arg" = "X-lc_r"; then
- 	  case $host in
+--- ltmain.sh.orig	Wed Jul  2 16:56:39 2003
++++ ltmain.sh	Wed Jul  2 16:58:03 2003
+@@ -1279,3 +1279,3 @@
+ 	 case $host in
 -	 *-*-openbsd* | *-*-freebsd*)
 +	 *-*-openbsd* | *-*-freebsd4*)
- 	    # Do not include libc_r directly, use -pthread flag.
- 	    continue
- 	    ;;
+ 	   # Do not include libc_r directly, use -pthread flag.
