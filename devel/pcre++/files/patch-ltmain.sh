1c8a109e3a14e6e70809552767110885206ab726
Daichi GOTO <daichi@FreeBSD.org>
2003-07-02 08:45:59 +0000

    update devel/pcre++: 0.6 --> 0.9.2
    PR:		53963
    Submitted by:	Kirill Ponomarew <ponomarew@oberon.net>

---
+++ devel/pcre++/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Wed Jun 25 11:24:31 2003
++++ ltmain.sh	Wed Jun 25 11:25:27 2003
+@@ -4325,10 +4325,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
