0b3641c35d1cf8c3c0b58dec2162464d632a9a61
daichi <daichi@FreeBSD.org>
2003-07-03 12:26:16 +0000

    fix audio/ecawave: fix build error
    PR:		53231
    Submitted by:	Serge Gagnon <gagnon__s@videotron.ca>
    Pointed out by:	bento via kris

---
+++ audio/ecawave/files/patch-qewaveform.h
@@ -0,0 +1,10 @@
+--- src/qewaveform.h.orig	Wed Jun 11 18:38:05 2003
++++ src/qewaveform.h	Wed Jun 11 18:38:39 2003
+@@ -1,6 +1,7 @@
+ #ifndef INCLUDED_QEWAVEFORM_H
+ #define INCLUDED_QEWAVEFORM_H
+ 
++#include <sys/types.h>
+ #include <vector>
+ 
+ #include <qwidget.h>
