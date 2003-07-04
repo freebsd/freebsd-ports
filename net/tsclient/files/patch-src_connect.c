41a619593853a4c398782fe007f9b3bc9e5d71db
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-07-04 23:13:35 +0000

    Fix patching due to ^M's in the source files.

--- net/tsclient/files/patch-src_connect.c
+++ net/tsclient/files/patch-src_connect.c
@@ -1,28 +1,24 @@
---- src/connect.c.orig	Thu Jul  3 22:43:25 2003
-+++ src/connect.c	Fri Jul  4 21:06:08 2003
-@@ -1793,9 +1793,9 @@
+--- src/connect.c.orig	Thu Jul  3 16:43:25 2003
++++ src/connect.c	Fri Jul  4 19:08:41 2003
+@@ -1793,8 +1793,9 @@
        rdp_file_get_screen (rdp, main_window);
        rdp_file_save (rdp, filename);
        g_free (rdp);
--    }
++      g_free (filename);
+     }
 -    g_free (filename);
--  }
-+    }
-+/*    g_free (filename);*/
-+  }
++    /*g_free (filename);*/
+   }
    gtk_widget_destroy (GTK_WIDGET (save_selector));
    return;
- }
-@@ -1838,9 +1838,9 @@
+@@ -1838,8 +1839,9 @@
          rdp_file_set_screen (rdp, main_window);
        }
        g_free (rdp);
--    }
++      g_free (filename);
+     }
 -    g_free (filename);
--  }
-+    }
-+/*      g_free (filename);*/
-+  }
++    /*g_free (filename);*/
+   }
    gtk_widget_destroy (GTK_WIDGET (open_selector));
    return;
- }
