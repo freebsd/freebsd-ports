2ef4c71c730f5a98bde4beafbc3eca8c7bb8867b
oliver <oliver@FreeBSD.org>
2003-07-07 14:01:25 +0000

    - now *really* use libtool from ports instead of the included one - build -dynamic for better compatibility with libltdl - grap back-perl fixes from OpenLDAP CVS to let FreeBSD port users   test the perl backend - dynamically create pkg-plist in pre-install instead of post-install   to simplify things and guard against changes in bsd.port.mk - bump portrevision - miscellaneous style fixes:
    PR:		54177
    Submitted by:	maintainer

--- net/openldap22/files/patch-libraries::liblunicode::ucstr.c
+++ net/openldap22/files/patch-libraries::liblunicode::ucstr.c
@@ -1,11 +1,11 @@
 --- libraries/liblunicode/ucstr.c.orig	Fri Apr 11 03:57:10 2003
-+++ libraries/liblunicode/ucstr.c	Sat Jul  5 10:53:04 2003
-@@ -15,6 +15,8 @@
++++ libraries/liblunicode/ucstr.c	Sun Jul  6 02:50:32 2003
+@@ -10,7 +10,7 @@
+ #include <ac/string.h>
+ #include <ac/stdlib.h>
+ 
+-#include <lber.h>
++#include <lber_pvt.h>
+ 
  #include <ldap_utf8.h>
  #include <ldap_pvt_uc.h>
- 
-+#include "lber_pvt.h"
-+
- #define	malloc(x)	ber_memalloc_x(x,ctx)
- #define	realloc(x,y)	ber_memrealloc_x(x,y,ctx)
- #define	free(x)		ber_memfree_x(x,ctx)
