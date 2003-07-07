618af922c688569f9e385753bff5cb21d80efb13
tg <tg@FreeBSD.org>
2003-07-07 09:32:39 +0000

    Upgrade to 2.4.3.

--- math/py-scientific/files/patch-setup.py
+++ math/py-scientific/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Thu Mar 29 17:08:38 2001
-+++ setup.py	Mon Apr  2 18:50:48 2001
-@@ -11,7 +11,7 @@
- 
- 
- if netcdf_prefix is None:
--    for netcdf_prefix in ['/usr/local', '/usr']:
-+    for netcdf_prefix in [sys.prefix, '/usr']:
-         netcdf_include = os.path.join(netcdf_prefix, 'include')
-         netcdf_lib = os.path.join(netcdf_prefix, 'lib')
-         if os.path.exists(os.path.join(netcdf_include, 'netcdf.h')):
+--- setup.py~	Fri Jul  4 11:50:18 2003
++++ setup.py	Fri Jul  4 11:51:08 2003
+@@ -14,7 +14,7 @@
+     try:
+         netcdf_prefix=os.environ['NETCDF_PREFIX']
+     except KeyError:
+-        for netcdf_prefix in ['/usr/local', '/usr']:
++        for netcdf_prefix in [sys.prefix, '/usr']:
+             netcdf_include = os.path.join(netcdf_prefix, 'include')
+             netcdf_lib = os.path.join(netcdf_prefix, 'lib')
+             if os.path.exists(os.path.join(netcdf_include, 'netcdf.h')):
