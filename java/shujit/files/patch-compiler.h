582b8cc56ed070656eb6e43dad69337ebde11611
Dag-Erling Smørgrav <des@FreeBSD.org>
2003-07-08 08:48:57 +0000

    Unbreak and grab maintainership.

--- java/shujit/files/patch-compiler.h
+++ java/shujit/files/patch-compiler.h
@@ -12,3 +12,12 @@ $FreeBSD$
  #  ifndef _UINT16_T
  #    define _UINT16_T
  typedef unsigned short uint16_t;
+@@ -196,7 +196,7 @@
+ #    include <linux/version.h>	// for kernel version
+ #  elif defined(__FreeBSD__) || defined(__NetBSD__)
+ #    ifdef __FreeBSD__
+-#      if __FreeBSD__ >= 4 && JDK_VER < 12
++#      if __FreeBSD__ == 4 && JDK_VER < 12
+ 	// In this case, the 3rd arg. of sig. handler is not (sigcontext *).
+ #	 define SIGCONTEXT_T struct osigcontext
+ #      else
