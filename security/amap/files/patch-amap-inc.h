3ec6170e45e53213c4024d9253810048c7f7b4ff
Yen-Ming Lee <leeym@FreeBSD.org>
2003-07-08 06:23:24 +0000

    add missing patch files in my previous commit.
    Noticed by:	kris and bento

---
+++ security/amap/files/patch-amap-inc.h
@@ -0,0 +1,14 @@
+--- amap-inc.h.orig	Sun Jun 29 20:31:38 2003
++++ amap-inc.h	Sun Jun 29 20:31:50 2003
+@@ -12,10 +12,9 @@
+ #include <sys/stat.h>
+ #include <sys/types.h>
+ #include <sys/socket.h>
+-#include <arpa/inet.h>
+-#include <netdb.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
++#include <netdb.h>
+ #include <fcntl.h>
+ #include <time.h>
+ #include <errno.h>
