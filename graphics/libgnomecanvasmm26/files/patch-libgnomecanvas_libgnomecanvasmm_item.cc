2fc0b2687515aa8ce1bbf3e383f3ea6c3c5907dd
marcus <marcus@FreeBSD.org>
2003-07-09 21:50:09 +0000

    Fix build on bento.
    Reported by:	bento via kris

--- graphics/libgnomecanvasmm26/files/patch-libgnomecanvas_libgnomecanvasmm_item.cc
+++ graphics/libgnomecanvasmm26/files/patch-libgnomecanvas_libgnomecanvasmm_item.cc
@@ -1,11 +1,13 @@
---- libgnomecanvas/libgnomecanvasmm/item.cc.orig	Sat Jun 28 00:33:08 2003
-+++ libgnomecanvas/libgnomecanvasmm/item.cc	Sat Jun 28 00:34:20 2003
-@@ -45,7 +45,7 @@
+--- libgnomecanvas/libgnomecanvasmm/item.cc.orig	Wed Jul  9 17:36:39 2003
++++ libgnomecanvas/libgnomecanvasmm/item.cc	Wed Jul  9 17:44:53 2003
+@@ -44,8 +44,9 @@
+ void
  Item::item_construct(Group& group)
  {
++  va_list ap;
    gnome_canvas_item_construct(GNOME_CANVAS_ITEM(gobj()), group.gobj(),
 -                              0, 0);
-+                              0, (void*)0);
++                              0, ap);
  }
  
  void
