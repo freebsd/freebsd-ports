ca8ec2765bfcd7443e8f676050947cd46c3bc935
marcus <marcus@FreeBSD.org>
2003-07-12 05:53:09 +0000

    Update to 0.120.  Changelog available at: http://www.gnome.org/project/shownotes.php?group_id=584&release_id=2031
    PR:		54362
    Submitted by:	maintainer

--- net/tsclient/files/patch-src_connect.c
+++ net/tsclient/files/patch-src_connect.c
@@ -1,24 +1,20 @@
---- src/connect.c.orig	Thu Jul  3 16:43:25 2003
-+++ src/connect.c	Fri Jul  4 19:08:41 2003
-@@ -1793,8 +1793,9 @@
-       rdp_file_get_screen (rdp, main_window);
+--- src/connect.c.orig	Sat Jul 12 01:50:36 2003
++++ src/connect.c	Sat Jul 12 01:51:34 2003
+@@ -1739,7 +1739,7 @@
        rdp_file_save (rdp, filename);
        g_free (rdp);
-+      g_free (filename);
      }
 -    g_free (filename);
-+    /*g_free (filename);*/
++/*    g_free (filename);*/
    }
    gtk_widget_destroy (GTK_WIDGET (save_selector));
    return;
-@@ -1838,8 +1839,9 @@
-         rdp_file_set_screen (rdp, main_window);
+@@ -1784,7 +1784,7 @@
        }
        g_free (rdp);
-+      g_free (filename);
      }
 -    g_free (filename);
-+    /*g_free (filename);*/
++/*    g_free (filename);*/
    }
    gtk_widget_destroy (GTK_WIDGET (open_selector));
    return;
