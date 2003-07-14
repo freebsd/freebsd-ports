73e9df16f30aa62cae232c4ea9508d42871288d8
sf <sf@FreeBSD.org>
2003-07-14 02:36:32 +0000

    get rid of libgnugetopt dependency for -CURRENT, use USE_GETOPT_LONG instead. convert getopt_long_only() to getopt_long().

--- games/foobillard/files/patch-billard3d.c
+++ games/foobillard/files/patch-billard3d.c
@@ -1,15 +1,24 @@
---- src/billard3d.c.orig	Sat Jan 11 02:04:01 2003
-+++ src/billard3d.c	Sun Feb 16 16:39:59 2003
+--- src/billard3d.c.orig	Thu May  1 08:32:10 2003
++++ src/billard3d.c	Mon Jul  7 04:29:04 2003
 @@ -24,7 +24,7 @@
  #include <string.h>
  #include <math.h>
  #include <unistd.h>
 -#include <endian.h>
-+#include <machine/endian.h>
++#include <sys/endian.h>
  
  #ifndef USE_SDL
  #include <GL/glut.h>
-@@ -4094,7 +4094,7 @@
+@@ -4270,7 +4270,7 @@
+ 
+    /* config file */
+    load_config( &confv, &confc, argv, argc );
+-   while( ( act_option = getopt_long_only(confc, confv, "+", long_options, &option_index) ) >= 0){
++   while( ( act_option = getopt_long(confc, confv, "+", long_options, &option_index) ) >= 0){
+        process_option(act_option);
+    }
+    DPRINTF("main:rgstereo=%d",options_rgstereo_on);
+@@ -4359,7 +4359,7 @@
         fread( &ball_ball_snd.data[SOUND_NULLOFFS*2], 1, ball_ball_snd.len-SOUND_NULLOFFS*2*2 , f );
         fclose(f);
  
