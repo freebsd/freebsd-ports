f40260f3228d4de25d8c5b31eb787736f1ed4733
des <des@FreeBSD.org>
2003-07-14 12:00:47 +0000

    Upgrade to 0.8.1.

---
+++ lang/sbcl/files/patch-make.sh
@@ -0,0 +1,8 @@
+--- make.sh.orig	Tue May 27 13:47:51 2003
++++ make.sh	Tue May 27 13:47:55 2003
+@@ -121,5 +121,4 @@
+ sh make-target-1.sh || exit 1
+ sh make-host-2.sh   || exit 1
+ sh make-target-2.sh || exit 1
+-sh make-target-contrib.sh || exit 1
+ date
