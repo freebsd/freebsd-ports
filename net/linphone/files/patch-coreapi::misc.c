0dfe76074bbbdbf5c629a508e4a5790c9fb4eff4
Maxim Sobolev <sobomax@FreeBSD.org>
2003-07-14 18:01:46 +0000

    Fix auto-kill option.
    PR:		49733
    Submitted by:	Yoshiyuki NAKAMICHI <nakamiti-sendpr@emergence.jp>

--- net/linphone/files/patch-coreapi::misc.c
+++ net/linphone/files/patch-coreapi::misc.c
@@ -1,15 +1,23 @@
 
-$FreeBSD$
+	
 
---- coreapi/misc.c	2003/06/13 12:59:10	1.1
-+++ coreapi/misc.c	2003/06/13 12:59:39
-@@ -249,8 +249,7 @@
- 	hints.ai_family = PF_UNSPEC;
- 	hints.ai_flags = AI_NUMERICHOST;
- 	getaddrinfo(lc->net_conf.sel_if->ipaddr, NULL, &hints, &res);
--	if ( (strcmp(lc->net_conf.sel_if->ipaddr,"127.0.0.1")!=0)
--	   && !IN6_IS_ADDR_LINKLOCAL(res->ai_addr) && !IN6_IS_ADDR_LOOPBACK(res->ai_addr))
-+	if ( (strcmp(lc->net_conf.sel_if->ipaddr,"127.0.0.1")!=0))
- #else
- 	if (strcmp(lc->net_conf.sel_if->ip4addr,"127.0.0.1")!=0)
- #endif
+--- coreapi/misc.c.orig	Wed Mar 12 09:52:05 2003
++++ coreapi/misc.c	Wed Mar 12 09:52:39 2003
+@@ -87,7 +87,7 @@
+ 	pid_t pid;
+ 	
+ 	/* try to kill all artsd*/
+-	command=g_strdup_printf("ps -u %s |grep artsd",getenv("LOGNAME"));
++	command=g_strdup_printf("ps -c %s |grep artsd",getenv("LOGNAME"));
+ 	out=popen(command,"r");
+ 	if (out!=NULL)
+ 	{
+@@ -102,7 +102,7 @@
+ 	}
+ 	g_free(command);
+ 	/* do the same with esd*/
+-	command=g_strdup_printf("ps -u %s |grep esd",getenv("LOGNAME"));
++	command=g_strdup_printf("ps -c %s |grep esd",getenv("LOGNAME"));
+ 	out=popen(command,"r");
+ 	if (out!=NULL)
+ 	{
