09ed650beef5a853fc54e28130aca515eda2aa08
Edwin Groothuis <edwin@FreeBSD.org>
2003-07-16 05:27:36 +0000

    Remove compiler warnings from freenet6 port
    	When compiling the freenet6 port, some compiler warnings
    	are generated. By including string.h they are removed.
    
    PR:		ports/50217
    Submitted by:	root <macklobell@hotmail.com>

---
+++ net/freenet6/files/patch-log.c
@@ -0,0 +1,12 @@
+*** src/log.c.org	Sun Mar 23 16:41:11 2003
+--- src/log.c	Sun Mar 23 16:42:54 2003
+***************
+*** 32,37 ****
+--- 32,38 ----
+  #include <stdio.h>
+  #include <time.h>
+  #include <syslog.h>
++ #include <string.h>
+  
+  #define LOG_IT
+  
---
+++ net/freenet6/files/patch-xmltsp.c
@@ -0,0 +1,12 @@
+*** src/xmltsp.c.org	Sun Mar 23 16:41:06 2003
+--- src/xmltsp.c	Sun Mar 23 16:41:29 2003
+***************
+*** 29,34 ****
+--- 29,35 ----
+  */
+  #include <stdio.h>
+  #include <stdlib.h>
++ #include <string.h>
+  
+  #include "xmlparse.h"
+  
