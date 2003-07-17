2541f7b906d7e437a5bfcfb78f1ec25ae552affa
daichi <daichi@FreeBSD.org>
2003-07-17 05:45:26 +0000

    update textproc/gmetadom: fix build on 5.x
    PR:		54566
    Submitted by:	Charlie & <yinjieh@csie.nctu.edu.tw> (maintainer)

---
+++ textproc/gmetadom/files/patch-src::gdome_cpp_smart::test::main.cc
@@ -0,0 +1,10 @@
+--- src/gdome_cpp_smart/test/main.cc.orig	Thu Jul 17 11:35:05 2003
++++ src/gdome_cpp_smart/test/main.cc	Thu Jul 17 11:36:16 2003
+@@ -21,6 +21,7 @@
+  * or send an email to <luca.padovani@cs.unibo.it>
+  */
+ 
++#include <assert.h>
+ #include <config.h>
+ 
+ #include <gdome.h>
