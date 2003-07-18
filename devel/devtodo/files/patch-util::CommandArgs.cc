2b6afa6c1a249edee3dc8ef2ad53fab0db9c9503
Kris Kennaway <kris@FreeBSD.org>
2003-07-18 03:37:46 +0000

    Fix build with gcc3.3

---
+++ devel/devtodo/files/patch-util::CommandArgs.cc
@@ -0,0 +1,7 @@
+--- util/CommandArgs.cc.orig	Thu Jul 17 20:33:23 2003
++++ util/CommandArgs.cc	Thu Jul 17 20:33:31 2003
+@@ -1,3 +1,4 @@
++#include <assert.h>
+ #include "CommandArgs.h"
+ CommandArgs::CommandArgs() {
+ }
---
+++ devel/devtodo/files/patch-util::Regex.h
@@ -0,0 +1,10 @@
+--- util/Regex.h.orig	Thu Jul 17 20:31:31 2003
++++ util/Regex.h	Thu Jul 17 20:31:43 2003
+@@ -1,6 +1,7 @@
+ #ifndef CRASH_REGEX
+ #define CRASH_REGEX
+ 
++#include <assert.h>
+ #include <cstring>
+ #include <string>
+ #include <map>
