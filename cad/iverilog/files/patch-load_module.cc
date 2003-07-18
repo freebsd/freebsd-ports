7eaa42259842320157d3e7b296110f48b18d8ca6
Kris Kennaway <kris@FreeBSD.org>
2003-07-18 04:17:17 +0000

    Partial fix for builds with gcc 3.3.  This still blows up later on.

---
+++ cad/iverilog/files/patch-load_module.cc
@@ -0,0 +1,10 @@
+--- load_module.cc.orig	Thu Jul 17 20:49:38 2003
++++ load_module.cc	Thu Jul 17 20:49:51 2003
+@@ -24,6 +24,7 @@
+ # include  "util.h"
+ # include  "parse_api.h"
+ # include  "compiler.h"
++# include  <assert.h>
+ # include  <iostream>
+ # include  <map>
+ # include  <string>
