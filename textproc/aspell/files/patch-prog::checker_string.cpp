6a939ee0f044d4758b8198c636f66fc6fc5a26f8
kris <kris@FreeBSD.org>
2003-07-18 02:40:04 +0000

    Fix build on gcc 3.3

---
+++ textproc/aspell/files/patch-prog::checker_string.cpp
@@ -0,0 +1,10 @@
+--- prog/checker_string.cpp.orig	Thu Jul 17 19:39:02 2003
++++ prog/checker_string.cpp	Thu Jul 17 19:39:09 2003
+@@ -4,6 +4,7 @@
+ // license along with this library if you did not you can find
+ // it at http://www.gnu.org/.
+ 
++#include <assert.h>
+ #include "checker_string.hpp"
+ #include "speller.hpp"
+ #include "document_checker.hpp"
