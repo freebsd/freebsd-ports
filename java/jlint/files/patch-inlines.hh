b78a4c4130e48675de6ed757de8bb020e4ce900e
Kris Kennaway <kris@FreeBSD.org>
2003-07-18 04:20:16 +0000

    Partially fix the build with gcc 3.3.  This still blows up later in the build.

---
+++ java/jlint/files/patch-inlines.hh
@@ -0,0 +1,10 @@
+--- inlines.hh.orig	Thu Jul 17 21:18:47 2003
++++ inlines.hh	Thu Jul 17 21:18:53 2003
+@@ -1,5 +1,7 @@
+ // inline functions
+ 
++#include <assert.h>
++
+ #ifndef INLINES_HH
+ #define INLINES_HH
+ 
