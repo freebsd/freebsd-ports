dd0f93eead5b5dbfe86b1fc135fcf0ad20d55e3b
Sergey A. Osokin <osa@FreeBSD.org>
2003-07-18 09:20:54 +0000

    Fix build on -CURRENT (gcc 3.3)
    Submitted by:	Kirill Ponomarew <ponomarew@oberon.net>
    PR:		54607

---
+++ devel/kimwitu++/files/patch-src::Gen.boot::unpk.cc
@@ -0,0 +1,10 @@
+--- src/Gen.boot/unpk.cc.orig	Fri Jul 18 12:32:38 2003
++++ src/Gen.boot/unpk.cc	Fri Jul 18 12:33:58 2003
+@@ -17,6 +17,7 @@
+ #include <stdio.h>
+ #include <string>
+ #include <stdlib.h>
++#include <assert.h>
+ #include "k.h"
+ #include "unpk.h"
+ 
