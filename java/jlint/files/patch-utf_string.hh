0bd631cf26517617f6fc767c1ef4fcd454ae9534
kris <kris@FreeBSD.org>
2003-07-18 04:20:16 +0000

    Partially fix the build with gcc 3.3.  This still blows up later in the build.

---
+++ java/jlint/files/patch-utf_string.hh
@@ -0,0 +1,10 @@
+--- utf_string.hh.orig	Thu Jul 17 21:03:53 2003
++++ utf_string.hh	Thu Jul 17 21:04:03 2003
+@@ -1,6 +1,7 @@
+ #ifndef UTF_STRING_HH
+ #define UTF_STRING_HH
+ 
++#include <assert.h>
+ #include <string>
+ #include <stdio.h>
+ #include "functions.hh"
