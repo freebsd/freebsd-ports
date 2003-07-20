2e9b5a7a7a2411f556aa58f7148d2ba69c7b5c26
osa <osa@FreeBSD.org>
2003-07-20 12:33:00 +0000

    Fix build on -CURRENT (gcc-3.3)
    Submitted by:	Simon Barner <barner@in.tum.de>

---
+++ sysutils/lsmlib/files/patch-main.c
@@ -0,0 +1,11 @@
+--- main.c.orig	Sat Jul 19 06:04:26 2003
++++ main.c	Sat Jul 19 06:03:37 2003
+@@ -21,7 +21,7 @@
+ #define USAGE "usage: lsm [-h] [-o file] [dir] \n \
+ \n \
+ \th \t: print this help message\n \
+-\tv \t: print current version\n
++\tv \t: print current version\n\
+ \to file \t: use file as output \n\n\n\
+ "
+ 
