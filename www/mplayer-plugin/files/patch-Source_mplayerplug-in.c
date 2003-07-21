f5385ef668aaf5ff5c102433372a69ff299c6f7b
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-07-21 19:18:04 +0000

    Don't advertize that we support RealPlayer formats since there is a problem with mplayer handling certain .ram files.
    Submitted by:	Barney Wolff <barney@databus.com>

--- www/mplayer-plugin/files/patch-Source_mplayerplug-in.c
+++ www/mplayer-plugin/files/patch-Source_mplayerplug-in.c
@@ -1,5 +1,15 @@
---- Source/mplayerplug-in.c.orig	Sat Apr 12 15:07:47 2003
-+++ Source/mplayerplug-in.c	Sat Apr 12 15:08:56 2003
+--- Source/mplayerplug-in.c.orig	Thu May 22 22:38:28 2003
++++ Source/mplayerplug-in.c	Mon Jul 21 15:14:24 2003
+@@ -142,9 +142,6 @@
+ 		"audio/mpeg3:mp3:MPEG audio;"
+ 		"audio/x-mpeg3:mp3:MPEG audio;"
+ 		"audio/mp3:mp3:MPEG audio;"
+-		"audio/x-pn-realaudio:ram:RealAudio;"
+-		"audio/x-pn-realaudio-plugin:rpm:RealAudio;"
+-		"audio/x-realaudio:ra:RealAudio;"
+ 		"video/fli:fli,flc:FLI animation;"
+ 		"video/x-fli:fli,flc:FLI animation;"
+ 		"video/x-ms-asf-plugin:asf,asx:Windows Media;"
 @@ -262,7 +262,7 @@
  	}
  
