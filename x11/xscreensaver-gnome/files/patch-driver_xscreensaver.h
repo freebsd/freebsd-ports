b301680be13dc20cc4d4b44acbb0c3652b0772b0
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-07-22 04:31:28 +0000

    Update to 4.11.

---
+++ x11/xscreensaver-gnome/files/patch-driver_xscreensaver.h
@@ -0,0 +1,10 @@
+--- driver/xscreensaver.h.orig	Tue Jul 22 00:14:02 2003
++++ driver/xscreensaver.h	Tue Jul 22 00:14:10 2003
+@@ -19,6 +19,7 @@
+ 
+ #include <string.h>
+ #include <stdio.h>
++#include <signal.h>
+ 
+ #include "prefs.h"
+ 
