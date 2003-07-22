e56dd08276a4c0b989b4a98b8bc5379b6b3c2e6c
Daichi GOTO <daichi@FreeBSD.org>
2003-07-22 13:12:15 +0000

    update mail/pflogstats: 20030404 -> 20030721
    PR:		54641
    Submitted by:	Don Croyle <croyle@gelemna.org>
    Reviewed by:	A.J.Caines@halplant.com (maintainer)

--- mail/pflogstats/files/patch-sqwmstats.pl
+++ mail/pflogstats/files/patch-sqwmstats.pl
@@ -1,43 +1,8 @@
---- sqwmstats.pl.orig	Fri Apr  4 15:29:35 2003
-+++ sqwmstats.pl	Mon May 12 10:38:01 2003
+--- sqwmstats.pl.bak	Sat Jul 19 14:40:21 2003
++++ sqwmstats.pl	Sat Jul 19 14:52:04 2003
 @@ -1,4 +1,4 @@
--#!/usr/bin/perl -W
+-#!/usr/bin/perl -w
 +#!/usr/bin/perl
  
  ###
  # Project:     pflogstats
-@@ -32,28 +32,27 @@
- ###
- 
- 
--use warnings;
- use strict;
- 
- ## Name and version
--my $release = "0.03";
--our $progName = "sqwmstats.pl";
-+use vars qw{$release $progName};
-+$release = "0.03";
-+$progName = "sqwmstats.pl";
- 
- 
- ## Define global variables
- 
- # option handling
--our %options;
--our %opts;
-+use vars qw{%options %opts};
- 
- $options{'help|h|?'}  = \$opts{'help'};
- $options{"version"}  = \$opts{'version'};
- 
- # module hooks
--our %hooks;
-+use vars qw{%hooks};
- 
- # Number formats
--our %numberformat;
-+use vars qw{%numberformat};
- 
- ## Module loader
- # General
