d5978ce49da2c877f237affe6f86b0fe165dec35
seanc <seanc@FreeBSD.org>
2003-07-22 06:16:06 +0000

    Add memcached: a high-performance, distributed memory object caching system, generic in nature, but intended for use in speeding up dynamic web applications by alleviating database load.

---
+++ databases/memcached/files/patch-items.c
@@ -0,0 +1,11 @@
+--- items.c.orig	Mon Jul 21 22:57:51 2003
++++ items.c	Mon Jul 21 22:57:58 2003
+@@ -17,8 +17,6 @@
+ #include <errno.h>
+ #include <time.h>
+ #include <event.h>
+-#include <malloc.h>
+-#include <Judy.h>
+ #include <assert.h>
+ 
+ #include "memcached.h"
