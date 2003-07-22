4c878b2de3c591f42b407359ba508da86b4dc779
marcus <marcus@FreeBSD.org>
2003-07-22 03:51:15 +0000

    Include sed_inplace as a PATCH_DEPENDS rather than a BUILD_DEPENDS to fix patching on older FreeBSD systems (4.5 and earlier).
    PR:		54090
    Submitted by:	tobez

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1494,7 +1494,7 @@ USE_SUBMAKE=	yes
 .if defined(USE_REINPLACE)
 REINPLACE_ARGS?=	-i.bak
 .if ${OSVERSION} < 460101 || ( ${OSVERSION} >= 500000 && ${OSVERSION} < 500036 )
-BUILD_DEPENDS+=	${LOCALBASE}/bin/sed_inplace:${PORTSDIR}/textproc/sed_inplace
+PATCH_DEPENDS+=	${LOCALBASE}/bin/sed_inplace:${PORTSDIR}/textproc/sed_inplace
 REINPLACE_CMD?=	${LOCALBASE}/bin/sed_inplace ${REINPLACE_ARGS}
 .else
 REINPLACE_CMD?=	${SED} ${REINPLACE_ARGS}
