d2524a8b3057bdcd5a350eb1029cb1672258aad4
oliver <oliver@FreeBSD.org>
2003-07-23 14:35:27 +0000

    Add med 2.1.5, modélisation et Échanges de Données.
    PR:		54761
    Submitted by:	thierry@pompo.net

---
+++ french/med/files/patch-include::med_outils.h
@@ -0,0 +1,11 @@
+--- include/med_outils.h.orig	Tue Dec 10 19:05:22 2002
++++ include/med_outils.h	Wed Jul 23 11:37:29 2003
+@@ -111,7 +111,7 @@
+ #define MED_TAILLE_PROFILS 9 
+ 
+ /*Pour eviter le bug solaris*/
+-#include <malloc.h>
++#include <stdlib.h>
+ 
+ /* Interface des routines du composant tools */
+ #include <med_misc.h>
