6366ae8f6cdde2ea6786b66c3e818136fc950c7f
oliver <oliver@FreeBSD.org>
2003-07-23 18:11:10 +0000

    Assign MAINTAINER to Serge Gagnon <gagnon__s@videotron.ca> fix lex.c add a missed dependency
    PR:		ports/50967
    Submitted by:	Serge Gagnon <gagnon__s@videotron.ca>

--- ftp/ftptool/files/lex.c
+++ ftp/ftptool/files/lex.c
@@ -1,4 +1,5 @@
-# include "stdio.h"
+# include <stdio.h>
+# include <unistd.h>
 # define U(x) x
 # define NLSTATE yyprevious=YYNEWLINE
 # define BEGIN yybgin = yysvec + 1 +
@@ -17,7 +18,7 @@ int yyleng; extern char yytext[];
 int yymorfg;
 extern char *yysptr, yysbuf[];
 int yytchar;
-FILE *yyin = {stdin}, *yyout = {stdout};
+FILE *yyin = STDIN_FILENO, *yyout = STDOUT_FILENO;
 extern int yylineno;
 struct yysvf { 
 	struct yywork *yystoff;
