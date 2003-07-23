c755aa5f08ade861b7347b18753795dd9386c14a
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-07-23 05:40:42 +0000

    Update to 1.5a.

--- www/seamonkey/files/patch-nsprpub_pr_src_misc_prtime.c
+++ www/seamonkey/files/patch-nsprpub_pr_src_misc_prtime.c
@@ -1,11 +1,11 @@
 --- nsprpub/pr/src/misc/prtime.c.orig	Wed Feb 26 19:01:02 2003
-+++ nsprpub/pr/src/misc/prtime.c	Sun Apr  6 19:22:10 2003
++++ nsprpub/pr/src/misc/prtime.c	Mon Jun 30 23:18:29 2003
 @@ -1666,7 +1666,7 @@
   */
  
  #if defined(SUNOS4) || (__GLIBC__ >= 2) || defined(XP_BEOS) \
 -        || defined(NETBSD)
-+        || defined(NETBSD) || defined(__FreeBSD__)
++        || defined(NETBSD) || defined(FREEBSD)
      a.tm_zone = NULL;
      a.tm_gmtoff = tm->tm_params.tp_gmt_offset + tm->tm_params.tp_dst_offset;
  #endif
--- www/seamonkey2/files/patch-Double.cpp
+++ www/seamonkey2/files/patch-Double.cpp
@@ -1,11 +1,16 @@
---- extensions/transformiix/source/base/Double.cpp.orig	Wed May 21 04:42:05 2003
-+++ extensions/transformiix/source/base/Double.cpp	Wed May 21 04:42:24 2003
-@@ -51,7 +51,7 @@
+--- extensions/transformiix/source/base/Double.cpp.orig	Thu Jan 30 00:26:46 2003
++++ extensions/transformiix/source/base/Double.cpp	Tue Jun 24 23:30:10 2003
+@@ -51,10 +51,10 @@
  //A trick to handle IEEE floating point exceptions on FreeBSD - E.D.
  #ifdef __FreeBSD__
  #include <ieeefp.h>
 -#ifdef __alpha__
-+#if defined(__alpha__) || defined(__sparc64__)
- fp_except_t allmask = FP_X_INV|FP_X_OFL|FP_X_UFL|FP_X_DZ|FP_X_IMP;
- #else
+-fp_except_t allmask = FP_X_INV|FP_X_OFL|FP_X_UFL|FP_X_DZ|FP_X_IMP;
+-#else
++#if defined(__i386__)
  fp_except_t allmask = FP_X_INV|FP_X_OFL|FP_X_UFL|FP_X_DZ|FP_X_IMP|FP_X_DNML;
++#else
++fp_except_t allmask = FP_X_INV|FP_X_OFL|FP_X_UFL|FP_X_DZ|FP_X_IMP;
+ #endif
+ fp_except_t oldmask = fpsetmask(~allmask);
+ #endif
