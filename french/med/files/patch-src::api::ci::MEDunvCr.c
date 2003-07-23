98ad40ed215b7640f01a17188028db1b72f7720f
Oliver Lehmann <oliver@FreeBSD.org>
2003-07-23 14:35:27 +0000

    Add med 2.1.5, modélisation et Échanges de Données.
    PR:		54761
    Submitted by:	thierry@pompo.net

---
+++ french/med/files/patch-src::api::ci::MEDunvCr.c
@@ -0,0 +1,27 @@
+--- src/api/ci/MEDunvCr.c.orig	Tue Dec 10 16:48:14 2002
++++ src/api/ci/MEDunvCr.c	Fri Jun 27 16:10:48 2003
+@@ -34,6 +34,12 @@
+ #include <sys/time.h>
+ #endif
+ 
++#ifdef __FreeBSD__
++#include <sys/types.h>
++#include <unistd.h>
++#include <pwd.h>
++#endif
++
+ med_err 
+ MEDunvCr(med_idt fid, char *maa)
+ {
+@@ -78,7 +84,11 @@
+   nSize = strlen(nomu)-1;
+   if ( sprintf(&nomu[nSize]," %hu",tp.millitm) < 0 ) return -1;
+ #else
++# ifdef __FreeBSD__
++  if (getpwuid(geteuid()) == NULL) return -1;
++# else
+   if (cuserid(nomu) == (void*) NULL) return -1;
++# endif
+   strcat(nomu," ");
+   temps=time(&temps);
+   strcat(nomu,ctime(&temps));
