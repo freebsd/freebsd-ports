5d51b1f44e5ce2542c57165bf3cede911a8668a5
Kirill Ponomarev <krion@FreeBSD.org>
2003-07-23 08:49:01 +0000

    Port added with slight modification
    PR:		54510
    Submitted by:	Ying-Chieh Chen <yinjieh@csie.nctu.edu.tw>
    Approved by:	fjoe (mentor) (implicit)

---
+++ net-mgmt/ipaudit/files/patch-src::hash.h
@@ -0,0 +1,11 @@
+--- src/hash.h.orig	Tue Jul 15 16:53:16 2003
++++ src/hash.h	Tue Jul 15 16:53:49 2003
+@@ -1,6 +1,8 @@
+ #ifndef _HASH_H
+ #define _HASH_H
+ 
++#include <sys/types.h>
++
+ /*
+ ------------------------------------------------------------------------
+ Type Definitions
