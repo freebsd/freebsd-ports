968a91ee79bf95e8cc0a43f89f5a2207982b105e
Sergey A. Osokin <osa@FreeBSD.org>
2003-07-24 15:51:39 +0000

    Fix building under ${OSVERSION} >= 500113
    Submitted by:	Simon Barner <barner@in.tum.de>

---
+++ textproc/xmlpp/files/patch-src::xmltokenizer.cpp
@@ -0,0 +1,11 @@
+--- src/xmltokenizer.cpp.orig	Wed Jul 23 04:14:53 2003
++++ src/xmltokenizer.cpp	Wed Jul 23 04:29:18 2003
+@@ -15,7 +15,7 @@
+ // needed includes
+ #include "xmlpp.h"
+ #include "xmltokenizer.h"
+-
++#include <iostream>
+ 
+ // namespace declaration
+ namespace xmlpp {
---
+++ textproc/xmlpp/files/patch-test::nodetest.cpp
@@ -0,0 +1,10 @@
+--- test/nodetest.cpp.orig	Wed Jul 23 04:18:02 2003
++++ test/nodetest.cpp	Wed Jul 23 04:19:01 2003
+@@ -5,6 +5,7 @@
+ */
+ 
+ #include <fstream>
++#include <iostream>
+ #include "xmlpp.h"
+ 
+ using namespace xmlpp;
