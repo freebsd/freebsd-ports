f41f5ae9e6c29e68bf5dabac583c047f40b9ca0f
Oliver Lehmann <oliver@FreeBSD.org>
2003-07-25 23:45:06 +0000

    fix build on CURRENT

--- devel/wftk/files/patch-uta::blitters.cc
+++ devel/wftk/files/patch-uta::blitters.cc
@@ -1,11 +1,19 @@
---- uta/blitters.cc.orig	Wed May 14 05:23:15 2003
-+++ uta/blitters.cc	Wed May 14 15:39:28 2003
-@@ -28,6 +28,8 @@
+--- uta/blitters.cc.orig	Sat Oct 12 23:11:07 2002
++++ uta/blitters.cc	Sat Jul 26 01:16:18 2003
+@@ -20,6 +20,7 @@
+ 
+ #include "blitters.h"
+ #include <painter.h>
++#include <cassert>
+ 
+ namespace uta {
+ 
+@@ -27,6 +28,8 @@
+ #undef X86_ASSEMBLER
  #endif
  #if !defined(X86_ASSEMBLER)
- 
-+using namespace std;
 +
++using namespace std;
+ 
  /** Apply const gamma value.
      Blit just copies alpha value of the source to the destination surface.
-  */
