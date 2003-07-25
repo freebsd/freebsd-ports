4afb6df7c5225caa9542b093c5a7f9230931ac98
maho <maho@FreeBSD.org>
2003-07-25 02:48:05 +0000

    a) Forgot to update distinfo 52498 b) added LEGAL. author requests no distribution    except from his FTP site c) clean-ups for compiler optimization
    Submitted by:	Glenn Johnson <gjohnson@srrc.ars.usda.gov>
    Pointy hat to:	maho; partially :)

--- biology/tinker/files/patch-openend.f
+++ biology/tinker/files/patch-openend.f
@@ -1,6 +1,14 @@
---- openend.f.orig	Tue Dec 18 12:28:54 2001
-+++ openend.f	Sat Jul 19 12:30:57 2003
-@@ -33,7 +33,7 @@
+
+$FreeBSD$
+
+--- openend.f.orig	Fri Apr 18 13:31:51 2003
++++ openend.f	Mon Jul 21 13:36:11 2003
+@@ -29,11 +29,11 @@
+ c
+ c     standard Fortran 90, unavailable in some Fortran 77 compilers
+ c
+-      open (unit=iunit,file=name,status='old',position='append')
++c     open (unit=iunit,file=name,status='old',position='append')
  c
  c     common extension supported by many Fortran 77 compilers
  c
