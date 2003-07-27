2e2930bbff813314dfa9d9589e27431d97c7743b
Sheldon Hearn <sheldonh@FreeBSD.org>
2003-07-27 13:07:14 +0000

    Work around build failure on -current until maintainer has a chance to take a look.

---
+++ net/centericq/files/patch-Xml.h
@@ -0,0 +1,11 @@
+--- libicq2000-0.1/libicq2000/Xml.h.orig	Sun Jul 27 14:08:10 2003
++++ libicq2000-0.1/libicq2000/Xml.h	Sun Jul 27 14:08:26 2003
+@@ -24,6 +24,8 @@
+ #ifndef XML_H
+ #define XML_H
+ 
++using namespace std;
++
+ #include <string>
+ #include <list>
+ #include <ctype.h>
