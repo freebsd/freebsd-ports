7a46ce361471083044c777be7e4c6e84c7decf7f
Kirill Ponomarev <krion@FreeBSD.org>
2003-07-27 16:21:34 +0000

    - Update to version 0.7.3
    PR:		54905
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>
    Approved by:	fjoe (mentor) (implicit)

---
+++ multimedia/avinfo/files/patch-cmdline.c
@@ -0,0 +1,11 @@
+--- cmdline.c.orig	Mon Jun 16 18:05:02 2003
++++ cmdline.c	Sat Jul 26 02:30:59 2003
+@@ -167,7 +167,7 @@
+     					break;
+ 					}
+ 					if(*pointer=='?'){
+-						gets(buffer);
++						fgets(buffer,sizeof(buffer),stdin);
+ 						template[template_number++]=buffer;
+ 						buffer+=strlen(buffer);
+ 						pointer+=strlen(pointer)-1;
