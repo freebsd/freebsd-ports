3378745c51f136483f9224deb8b41b6c2a90e7bf
Kris Kennaway <kris@FreeBSD.org>
2003-07-27 09:44:00 +0000

    Add support for sparc64.
    Submitted by:	jake (based on)

--- x11-servers/XFree86-4-Server/files/patch-Pci.h
+++ x11-servers/XFree86-4-Server/files/patch-Pci.h
@@ -1,5 +1,5 @@
---- programs/Xserver/hw/xfree86/os-support/bus/Pci.h.orig	Tue Mar  4 16:42:16 2003
-+++ programs/Xserver/hw/xfree86/os-support/bus/Pci.h	Tue Mar  4 16:42:21 2003
+--- programs/Xserver/hw/xfree86/os-support/bus/Pci.h.orig	Mon Dec 23 07:37:26 2002
++++ programs/Xserver/hw/xfree86/os-support/bus/Pci.h	Sat Jul 26 21:34:12 2003
 @@ -230,6 +230,10 @@
  #  define ARCH_PCI_INIT linuxPciInit
  #  define INCLUDE_XF86_MAP_PCI_MEM
@@ -11,9 +11,19 @@
  # endif
  # define XF86SCANPCI_WRAPPER ia64ScanPCIWrapper
  #elif defined(__i386__)
-@@ -298,7 +302,11 @@
+@@ -291,14 +295,20 @@
+ # elif defined(sun)
+ #  define ARCH_PCI_INIT sparcPciInit
+ #  define INCLUDE_XF86_MAP_PCI_MEM
+-# elif defined(__OpenBSD__) && defined(__sparc64__)
++# elif (defined(__OpenBSD__) || defined(__FreeBSD__)) && defined(__sparc64__)
+ #  define  ARCH_PCI_INIT freebsdPciInit
+ #  define INCLUDE_XF86_MAP_PCI_MEM
+ #  define INCLUDE_XF86_NO_DOMAIN
  # endif
++# if !defined(__FreeBSD__)
  # define ARCH_PCI_PCI_BRIDGE sparcPciPciBridge
++# endif
  #elif defined(__x86_64__)
 -# define ARCH_PCI_INIT ix86PciInit
 +# if defined(__FreeBSD__)
