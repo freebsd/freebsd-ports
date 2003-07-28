0e09693564992295797ab3b1ee025ac52cee18af
daichi <daichi@FreeBSD.org>
2003-07-28 05:09:23 +0000

    fix archivers/makeself: makeself.sh fails
    PR:		54805
    Submitted by:	Andriy I Pylypenko <bamby@oranta.ua>

---
+++ archivers/makeself/files/patch-makeself.sh
@@ -0,0 +1,13 @@
+*** makeself.sh.orig	Thu Jul 24 11:42:36 2003
+--- makeself.sh	Thu Jul 24 11:42:45 2003
+***************
+*** 1,4 ****
+! #!/bin/sh
+  #
+  # Makeself version 2.1.x
+  #  by Stephane Peter <megastep@megastep.org>
+--- 1,4 ----
+! #!/usr/local/bin/bash
+  #
+  # Makeself version 2.1.x
+  #  by Stephane Peter <megastep@megastep.org>
