15d4dda6b5e57dfa63559941870774485ba96277
daichi <daichi@FreeBSD.org>
2003-07-28 05:32:09 +0000

    fix games/freebsd-games: Trivial fix to unbreak it on -CURRENT
    PR:		54873
    Submitted by:	Clement Laforet <sheepkiller@cultdeadsheep.org>

---
+++ games/freebsd-games/files/patch-sail::pl_1.c
@@ -0,0 +1,10 @@
+--- sail/pl_1.c.orig	Sat Jul 26 00:10:30 2003
++++ sail/pl_1.c	Sat Jul 26 00:10:42 2003
+@@ -40,6 +40,7 @@
+ #endif /* not lint */
+ 
+ #include "player.h"
++#include "misc.h"
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ 
