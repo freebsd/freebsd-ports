db6df99368bc4bf4585b37b5022fba55008ec756
Kelly Yancey <kbyanc@FreeBSD.org>
2003-07-29 19:33:58 +0000

    Add gcipher 1.0, a simple encryption tool.
    PR:		53738
    Submitted by:	Shannon -jj Behrens <jjinux@yahoo.com>

---
+++ security/gcipher/files/patch-Const.py
@@ -0,0 +1,9 @@
+diff -ur ../gcipher-1.0.orig/src/Const.py ./src/Const.py
+--- ../gcipher-1.0.orig/src/Const.py	Wed Jun 25 14:03:43 2003
++++ ./src/Const.py	Wed Jun 25 14:05:59 2003
+@@ -9,4 +9,4 @@
+ Read Help/Contents to learn how to use gcipher
+ as a command line filter or as a network proxy."""
+ AUTHORS = ["Shannon -jj Behrens <jjinux@yahoo.com>"]
+-GLADEDIR = "."
++GLADEDIR = "/usr/X11R6/share/gnome/gcipher/lib"
