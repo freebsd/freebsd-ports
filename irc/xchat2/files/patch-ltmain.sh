10eb4e207a932db64d0ed1f3d161f799f22d14ab
adamw <adamw@FreeBSD.org>
2003-07-31 18:01:41 +0000

    Update to 2.0.4.
    PR:		ports/55123
    Submitted by:	Sebastian Yepes F. <esn@x123.info>

--- irc/xchat2/files/patch-ltmain.sh
+++ irc/xchat2/files/patch-ltmain.sh
@@ -1,24 +1,24 @@
---- ltmain.sh.orig	Fri Apr 25 15:18:44 2003
-+++ ltmain.sh	Sun May 11 02:27:13 2003
-@@ -1061,7 +1061,7 @@
+--- ltmain.sh.old	Thu Jun 12 05:09:04 2003
++++ ltmain.sh	Thu Jul 31 19:03:06 2003
+@@ -1280,7 +1280,7 @@
  	  esac
- 	 elif test "X$arg" = "X-lc_r"; then
- 	  case $host in
+ 	elif test "X$arg" = "X-lc_r"; then
+ 	 case $host in
 -	 *-*-openbsd* | *-*-freebsd*)
 +	 *-*-openbsd* | *-*-freebsd4*)
- 	    # Do not include libc_r directly, use -pthread flag.
- 	    continue
- 	    ;;
-@@ -1071,8 +1071,16 @@
+ 	   # Do not include libc_r directly, use -pthread flag.
+ 	   continue
+ 	   ;;
+@@ -1290,8 +1290,16 @@
  	continue
  	;;
  
 +      -pthread)
-+      compile_command="$compile_command -pthread"
-+      finalize_command="$finalize_command -pthread"
-+      compiler_flags="$compiler_flags -pthread"
-+      continue
-+      ;;
++	compile_command="$compile_command -pthread"
++	finalize_command="$finalize_command -pthread"
++	compiler_flags="$compiler_flags -pthread"
++	continue
++	;;
 +
        -module)
  	module=yes
@@ -26,17 +26,24 @@
  	continue
  	;;
  
-@@ -2471,6 +2479,9 @@
- 	  *-*-openbsd* | *-*-freebsd*)
- 	    # Do not include libc due to us having libc/libc_r.
- 	    ;;
-+	  *-*-freebsd*)
-+	    # FreeBSD doesn't need this...
-+	    ;;
- 	  *)
- 	    # Add libc to deplibs on all other systems if necessary.
- 	    if test $build_libtool_need_lc = "yes"; then
-@@ -4280,10 +4291,12 @@
+@@ -4305,11 +4313,11 @@
+ 	  $show "(cd $output_objdir && $LTCC -c$no_builtin_flag$pic_flag_for_symtable \"$dlsyms\")"
+ 	  $run eval '(cd $output_objdir && $LTCC -c$no_builtin_flag$pic_flag_for_symtable "$dlsyms")' || exit $?
+ 
+-	  # Clean up the generated files.
+-	  $show "$rm $output_objdir/$dlsyms $nlist ${nlist}S ${nlist}T"
+-	  $run $rm "$output_objdir/$dlsyms" "$nlist" "${nlist}S" "${nlist}T"
+-
+-	  # Transform the symbol file into the correct name.
++	        # Clean up the generated files.
++	        $show "$rm $output_objdir/$dlsyms $nlist ${nlist}S ${nlist}T"
++	        $run $rm "$output_objdir/$dlsyms" "$nlist" "${nlist}S" "${nlist}T"
++               
++	        # Transform the symbol file into the correct name.
+ 	  compile_command=`$echo "X$compile_command" | $Xsed -e "s%@SYMFILE@%$output_objdir/${outputname}S.${objext}%"`
+ 	  finalize_command=`$echo "X$finalize_command" | $Xsed -e "s%@SYMFILE@%$output_objdir/${outputname}S.${objext}%"`
+ 	  ;;
+@@ -5428,10 +5436,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
