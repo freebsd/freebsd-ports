a449f03a82e19a36bc7d89fcad97db2f65d132bb
nectar <nectar@FreeBSD.org>
2003-07-31 12:19:53 +0000

    After a repo-copy from ports/mail/postfix, time warp back to 2003/01/08 and Postfix version 1.1.12.  Some folks haven't moved on to Postfix 2.x it seems.
    Requested by:	diviner17@yahoo.com

---
+++ mail/postfix1/files/patch-lmtp.h
@@ -0,0 +1,13 @@
+--- ./src/lmtp/lmtp.h.orig	Fri Jan 19 15:43:06 2001
++++ ./src/lmtp/lmtp.h	Sat Feb  3 14:01:09 2001
+@@ -12,8 +12,8 @@
+   * SASL library.
+   */
+ #ifdef USE_SASL_AUTH
+-#include <sasl.h>
+-#include <saslutil.h>
++#include <sasl1/sasl.h>
++#include <sasl1/saslutil.h>
+ #endif
+ 
+  /*
