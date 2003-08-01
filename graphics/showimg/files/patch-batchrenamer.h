6596be0508ef642b0a2f14277dbd33caeaaabcc7
Tilman Keskinoz <arved@FreeBSD.org>
2003-08-01 13:41:37 +0000

    Fix Build on STABLE.
    PR:		53665
    Submitted by:	Bartosz Fabianowski <bartosz@fabianowski.de>

---
+++ graphics/showimg/files/patch-batchrenamer.h
@@ -0,0 +1,12 @@
+--- showimg/batchrenamer.h.orig	Fri Aug  1 15:30:41 2003
++++ showimg/batchrenamer.h	Fri Aug  1 15:30:57 2003
+@@ -20,8 +20,8 @@
+ 
+ // OS includes
+ #include <stdio.h>
+-#include <utime.h>
+ #include <time.h>
++#include <utime.h>
+ #include <sys/types.h>
+ 
+ // QT includes
