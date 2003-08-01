b08d762b9b1694d657fc16ec20621b8eb661bffd
krion <krion@FreeBSD.org>
2003-08-01 18:18:17 +0000

    - Fix build on -current - Rearrange USE_GNOME
    PR:		55153
    Submitted by:	Ports Fury
    Approved by:	fjoe (mentor) (implicit)

---
+++ finance/gnomepm/files/patch-src::menu.c
@@ -0,0 +1,15 @@
+--- src/menu.c.orig	Sun Oct 29 10:19:49 2000
++++ src/menu.c	Fri Aug  1 23:40:03 2003
+@@ -86,9 +86,9 @@
+ 			    "(C)1999 Rodney Dawes",
+ 			    authors,
+ 			    /* other comments */
+-			    _("Portfolio management utility for GNOME\n
+-			    	Information provided by Yahoo!(C) Finance\n
+-					This program is not supported by Yahoo!(c)."),
++			    _("Portfolio management utility for GNOME\n"
++			    	"Information provided by Yahoo!(C) Finance\n"
++					"This program is not supported by Yahoo!(c)."),
+ 				NULL);
+   gtk_widget_show (about);
+   
