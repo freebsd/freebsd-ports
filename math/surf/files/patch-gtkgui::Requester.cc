c1299e3480f371b9bffad42a35446e5c863947d3
maho <maho@FreeBSD.org>
2003-08-02 02:21:02 +0000

    a) Fix build on gcc-3.3.x b) Update to 1.0.4
    Submitted by:	Simon Barner <barner@in.tum.de>

---
+++ math/surf/files/patch-gtkgui::Requester.cc
@@ -0,0 +1,11 @@
+--- gtkgui/Requester.cc.orig	Tue Jul 22 16:23:45 2003
++++ gtkgui/Requester.cc	Tue Jul 22 16:24:01 2003
+@@ -25,7 +25,7 @@
+ 
+ #include <assert.h>
+ #include <iostream.h>
+-#include <strstream.h>
++#include <strstream>
+ 
+ #include <gtk/gtk.h>
+ 
