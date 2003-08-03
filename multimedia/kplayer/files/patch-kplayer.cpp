8a49ba1a28b6738332f59010e937c1656073bef1
Stefan Eßer <se@FreeBSD.org>
2003-08-03 09:47:56 +0000

    Upgrade to kplayer-0.3.0

--- multimedia/kplayer/files/patch-kplayer.cpp
+++ multimedia/kplayer/files/patch-kplayer.cpp
@@ -1,18 +1,76 @@
---- kplayer/kplayer.cpp~	Wed Mar 26 12:14:07 2003
-+++ kplayer/kplayer.cpp	Fri Apr 11 09:35:05 2003
-@@ -824,6 +824,15 @@
-   kPlayerProcess() -> stop();
+--- kplayer/kplayer.cpp~	Sat Jul 26 15:49:07 2003
++++ kplayer/kplayer.cpp	Sun Aug  3 10:36:45 2003
+@@ -395,7 +395,7 @@
+   setSaturation (value);
+   if ( kPlayerProcess() -> length() > 0 )
+     sliderAction ("player_progress") -> slider() ->
+-      setPageStep (int (roundf (kPlayerProcess() -> length() * settings -> progressMarks())));
++      setPageStep (int (rintf (kPlayerProcess() -> length() * settings -> progressMarks())));
+   QApplication::postEvent (this, new QEvent (QEvent::LayoutHint));
+ //layout() -> activate();
+   recentFilesAction() -> setMaxItems (settings -> recentFileListSize());
+@@ -1192,7 +1192,7 @@
+   KPlayerSettings* settings = kPlayerSettings();
+   int seek = settings -> progressNormalSeekUnits() ? settings -> progressNormalSeek() :
+     kPlayerProcess() -> length() == 0 ? 10 :
+-    int (roundf (kPlayerProcess() -> length() * settings -> progressNormalSeek() / 100));
++    int (rintf (kPlayerProcess() -> length() * settings -> progressNormalSeek() / 100));
+   if ( seek == 0 )
+     seek = 1;
+   kPlayerProcess() -> relativeSeek (seek);
+@@ -1203,7 +1203,7 @@
+   KPlayerSettings* settings = kPlayerSettings();
+   int seek = settings -> progressFastSeekUnits() ? settings -> progressFastSeek() :
+     kPlayerProcess() -> length() == 0 ? 60 :
+-    int (roundf (kPlayerProcess() -> length() * settings -> progressFastSeek() / 100));
++    int (rintf (kPlayerProcess() -> length() * settings -> progressFastSeek() / 100));
+   if ( seek == 0 )
+     seek = 2;
+   kPlayerProcess() -> relativeSeek (seek);
+@@ -1214,7 +1214,7 @@
+   KPlayerSettings* settings = kPlayerSettings();
+   int seek = settings -> progressNormalSeekUnits() ? settings -> progressNormalSeek() :
+     kPlayerProcess() -> length() == 0 ? 10 :
+-    int (roundf (kPlayerProcess() -> length() * settings -> progressNormalSeek() / 100));
++    int (rintf (kPlayerProcess() -> length() * settings -> progressNormalSeek() / 100));
+   if ( seek == 0 )
+     seek = 1;
+   kPlayerProcess() -> relativeSeek (- seek);
+@@ -1225,7 +1225,7 @@
+   KPlayerSettings* settings = kPlayerSettings();
+   int seek = settings -> progressFastSeekUnits() ? settings -> progressFastSeek() :
+     kPlayerProcess() -> length() == 0 ? 60 :
+-    int (roundf (kPlayerProcess() -> length() * settings -> progressFastSeek() / 100));
++    int (rintf (kPlayerProcess() -> length() * settings -> progressFastSeek() / 100));
+   if ( seek == 0 )
+     seek = 2;
+   kPlayerProcess() -> relativeSeek (- seek);
+@@ -1586,7 +1586,7 @@
+     return;
+   kdDebug() << "Seek to " << progress << ": " << kPlayerProcess() -> position() << " => " << ((progress + 50) / 100) << " / " << kPlayerProcess() -> length() << "\n";
+   kPlayerProcess() -> absoluteSeek ((progress + 50) / 100);
+-/*int seek = int (roundf (float (progress) / 100 - kPlayerProcess() -> position()));
++/*int seek = int (rintf (float (progress) / 100 - kPlayerProcess() -> position()));
+   if ( seek == 0 )
+     return;
+   kPlayerProcess() -> relativeSeek (seek);*/
+@@ -1714,7 +1714,7 @@
+   int maxValue = slider -> maxValue();
+   if ( maxValue )
+   {
+-    int value = int (roundf (progress * 100));
++    int value = int (rintf (progress * 100));
+     if ( value > maxValue )
+       slider -> setMaxValue (value);
+     slider -> setValue (value);
+@@ -1757,8 +1757,8 @@
+   KPlayerSettings* settings = kPlayerSettings();
+   kdDebug() << "Info available. Detected length: " << kPlayerProcess() -> length() << "\n";
+   KPlayerSlider* slider = sliderAction ("player_progress") -> slider();
+-  slider -> setMaxValue (int (roundf (kPlayerProcess() -> length() * 100)));
+-  slider -> setPageStep (int (roundf (kPlayerProcess() -> length() * settings -> progressMarks())));
++  slider -> setMaxValue (int (rintf (kPlayerProcess() -> length() * 100)));
++  slider -> setPageStep (int (rintf (kPlayerProcess() -> length() * settings -> progressMarks())));
+   enablePlayerActions();
  }
  
-+#ifdef __FreeBSD__
-+int roundf(double number)
-+{
-+  if (number > 0.0)
-+    return (int)(number + 0.5);
-+  return (int)(number -0.5);
-+}
-+#endif
-+
- void KPlayer::onPlayerForward (void)
- {
-   int seek = kPlayerProcess() -> length() == 0 ? 10 :
