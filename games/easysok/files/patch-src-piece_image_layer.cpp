82e72125579696e423c95d04699129e50bd6f68f
krion <krion@FreeBSD.org>
2003-08-04 14:29:26 +0000

    Unbreak on -current
    Reported by:	kris via bento
    Approved by:	fjoe (mentor) (implicit)

---
+++ games/easysok/files/patch-src-piece_image_layer.cpp
@@ -0,0 +1,11 @@
+--- src/piece_image_layer.cpp.orig	Mon Aug  4 16:08:12 2003
++++ src/piece_image_layer.cpp	Mon Aug  4 16:08:27 2003
+@@ -21,7 +21,7 @@
+ #include "piece_image_layer.h"
+ 
+ #include <qdom.h>
+-
++#include <assert.h>
+ #include <kglobal.h>
+ #include <kstddirs.h>
+ 
