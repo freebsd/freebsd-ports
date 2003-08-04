e721a3bf1b399e93a5f57b61cd1dcc69a8f14709
Kirill Ponomarev <krion@FreeBSD.org>
2003-08-04 14:29:26 +0000

    Unbreak on -current
    Reported by:	kris via bento
    Approved by:	fjoe (mentor) (implicit)

---
+++ games/easysok/files/patch-src-pixmap_provider.cpp
@@ -0,0 +1,11 @@
+--- src/pixmap_provider.cpp.orig	Mon Aug  4 16:12:21 2003
++++ src/pixmap_provider.cpp	Mon Aug  4 16:12:33 2003
+@@ -21,7 +21,7 @@
+ #include "pixmap_provider.h"
+ 
+ #include <qwmatrix.h>
+-
++#include <assert.h>
+ #include <kglobal.h>
+ #include <kstddirs.h>
+ 
