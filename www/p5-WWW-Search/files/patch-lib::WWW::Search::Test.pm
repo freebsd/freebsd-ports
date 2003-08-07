1081ad36204ce5a296c866eaeb25ebb6ccb8e1a3
Erwin Lansing <erwin@FreeBSD.org>
2003-08-07 14:24:14 +0000

    Update to 2.42
    * lib/WWW/Search.pm (preprocess_results_page): BUGFIX bad return value
    * lib/WWW/Search/Test.pm (count_results): printResults output is more verbose
    * lib/WWW/Search.pm (parse_tree): new NOP stub function
    * lib/WWW/Search.pm (login): now returns flag for success/failure
    * lib/WWW/Search/Test.pm (find_websearch): new function
    * lib/WWW/Search/Test.pm (count_results): call login() before getting results
    * lib/WWW/Search.pm (cookie_jar): allow any flavor of HTTP::Cookies
    
    PR:		55342
    Submitted by:	Mathieu Arnold <m@absolight.net>

---
+++ www/p5-WWW-Search/files/patch-lib::WWW::Search::Test.pm
@@ -0,0 +1,11 @@
+--- lib/WWW/Search/Test.pm~	Tue Jul 15 05:02:28 2003
++++ lib/WWW/Search/Test.pm	Thu Aug  7 15:18:31 2003
+@@ -627,7 +627,7 @@
+       print $oResult->url, "\n";
+       foreach my $sField (qw( title description score change_date index_date size company location source ))
+         {
+-        print "  $sField==", $oResult->$sField, "==\n" if defined($oResult->$sField);
++        print "  $sField==", eval "$oResult->$sField", "==\n" if $oResult->can($sField);
+         } # foreach
+       } # foreach
+     } # if
