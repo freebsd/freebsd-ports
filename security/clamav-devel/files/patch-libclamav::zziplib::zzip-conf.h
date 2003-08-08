d988eb75a7486bece77df898f245e9a716ed917c
Yen-Ming Lee <leeym@FreeBSD.org>
2003-08-08 17:09:11 +0000

    The clamav-people do nightly snapshots now, hence the port must be updated every day or snapshots have to be preserved elsewhere, this patch changes the master_sites to a mirror which keeps the clamav-devel package for a longer time. And while we're at it update to the latest snapshot.
    PR:		55294
    Submitted by:	Rob Evers <rob@debank.tv>

---
+++ security/clamav-devel/files/patch-libclamav::zziplib::zzip-conf.h
@@ -0,0 +1,13 @@
+--- libclamav/zziplib/zzip-conf.h.orig	Mon Aug  4 06:25:30 2003
++++ libclamav/zziplib/zzip-conf.h	Thu Aug  7 03:48:35 2003
+@@ -29,6 +29,10 @@
+ # endif
+ #endif
+ 
++# ifdef ZZIP_HAVE_SYS_TYPES_H
++# include <sys/types.h>
++# endif
++
+ /* especially win32 platforms do not declare off_t so far - see zzip-msvc.h */
+ #ifndef _zzip_off_t
+ #ifdef   ZZIP_off_t
