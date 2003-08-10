78a30f0b1d7308eefa0f8534591fb14ff9579a83
krion <krion@FreeBSD.org>
2003-08-10 20:10:41 +0000

    Update to 0.8.0.p5
    The upgrade should fix the build problems with GCC 3.3 and
    issues with 16 bit colour depth.
    
    PR:		55352
    Submitted by:	maintainer

---
+++ multimedia/slideshow/files/patch-src::Page.cpp
@@ -0,0 +1,10 @@
+--- src/Page.cpp.orig	Mon Aug  4 17:22:29 2003
++++ src/Page.cpp	Mon Aug  4 17:22:51 2003
+@@ -28,6 +28,7 @@
+ #include "Namer.h"
+ #include "Macros.h"
+ #include "Presentation.h"
++#include <cassert>
+ #include <iostream>
+ 
+ using namespace std;
