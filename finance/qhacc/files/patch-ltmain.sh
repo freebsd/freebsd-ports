6731f010ce8f1411b6ddd4868ff7e5066cd5b52d
krion <krion@FreeBSD.org>
2003-08-12 18:04:27 +0000

    - Update to version 2.9
    PR:		55497
    Submitted by:	Ports Fury

---
+++ finance/qhacc/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Mar 13 01:25:30 2003
++++ ltmain.sh	Tue Aug 12 01:49:52 2003
+@@ -4210,10 +4210,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
