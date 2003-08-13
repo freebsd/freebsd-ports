d8a0d9ac8dffdf6b3a9a8dd0ead121d7e9cc2bef
arved <arved@FreeBSD.org>
2003-08-13 07:06:25 +0000

    Fix build on stable
    Reported by:	bento

---
+++ net-p2p/dctc-gui-qt/files/patch-hubwdgt.cc
@@ -0,0 +1,10 @@
+--- hubwdgt.cc.orig	Wed Aug 13 08:57:51 2003
++++ hubwdgt.cc	Wed Aug 13 08:59:05 2003
+@@ -29,6 +29,7 @@
+ #include <qfile.h>
+ #include <qdir.h>
+ #include <zlib.h>
++#include <sys/time.h>
+ #include "hubwdgt.h"
+ #include "debugdlg.h"
+ #include "searchdlg.h"
