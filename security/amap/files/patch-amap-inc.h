06a26b6083464fd7c89129c047039f38da1643c3
Sergey A. Osokin <osa@FreeBSD.org>
2003-08-13 12:54:55 +0000

    Update to 4.2 [1] Also: [2]  * respect to ${CC}  * save three bytes in pkg-plist (use %%DOCSDIR%%)  * save more bytes in Makefile (by remove refuse `/' and    optimize ${INSTALL_DATA} rules)
    Submitted by:	Yonatan@xpert.com <Yonatan@xpert.com> (maintainer) [1],
    		osa [2]
    PR:		55532

--- security/amap/files/patch-amap-inc.h
+++ security/amap/files/patch-amap-inc.h
@@ -1,10 +1,13 @@
---- amap-inc.h.orig	Tue Jul 29 14:26:51 2003
-+++ amap-inc.h	Tue Jul 29 14:27:05 2003
-@@ -13,7 +13,6 @@
+--- amap-inc.h.orig	Tue Aug  5 21:21:37 2003
++++ amap-inc.h	Wed Aug 13 11:54:03 2003
+@@ -12,9 +12,9 @@
  #include <sys/stat.h>
  #include <sys/types.h>
  #include <sys/socket.h>
 -#include <arpa/inet.h>
  #include <netdb.h>
  #include <netinet/in.h>
- #include <arpa/inet.h>
++#include <arpa/inet.h>
+ #include <fcntl.h>
+ #include <time.h>
+ #include <errno.h>
--- security/amap/files/patch-amap.h
+++ security/amap/files/patch-amap.h
@@ -1,5 +1,5 @@
---- amap.h.orig	Tue Jul 22 13:54:37 2003
-+++ amap.h	Tue Jul 29 14:06:27 2003
+--- amap.h.orig	Tue Aug  5 21:18:29 2003
++++ amap.h	Wed Aug 13 11:48:47 2003
 @@ -24,7 +24,7 @@
  #define AMAP_EMAIL     "amap-dev@thc.org"
  #define AMAP_RESOURCE  "www.thc.org"
@@ -7,5 +7,5 @@
 -#define AMAP_APPDEF_PATH	"/usr/local/bin"
 +#define AMAP_APPDEF_PATH	"PREFIX/bin"
  #define AMAP_BUFSIZE		1024	// standard buffer size
- #define AMAP_BUFSIZE_BIG	16348   // big standard buffer size
+ #define AMAP_BUFSIZE_BIG	65536   // big standard buffer size
  #define AMAP_REGEX_OPTIONS	( PCRE_MULTILINE | PCRE_CASELESS )
