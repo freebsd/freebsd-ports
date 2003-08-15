14fe54027c8219ee10e2f7f9622844cb9b448ed4
dinoex <dinoex@FreeBSD.org>
2003-08-15 18:44:27 +0000

    - Force binding with wish8.3, even if wish8.4 is installed. - install and build, even if no DISPLAY is running This will allow packages to be build on bento

--- irc/zircon/files/patch-unix.tcl
+++ irc/zircon/files/patch-unix.tcl
@@ -5,7 +5,7 @@
      package require -exact unixfile 1.18.[info tclversion]
  #
 -    global Type types Lib Bin CC LD Version Patchlevel Wish types cflags\
-+    global Type types Lib Bin CC LD Version Patchlevel IWish types cflags\
++    global Type types Lib Bin CC LD Version Patchlevel WishCmd types cflags\
        libs share SCflag GCC SLDflag CFlags TIDir v7 SED go NoGUI Xtn
  #
      set types {AIX Alpha BSDI FreeBSD HP-UX IRIX Linux OSF1 {SunOS 4} {SunOS 5} UNIX_SV}
@@ -14,7 +14,7 @@
  # Wish
  #
 -    makeEntry $w wish {Tcl/Tk Interpreter} Wish [incr i] getWish
-+    makeEntry $w wish {Tcl/Tk Interpreter} IWish [incr i] getWish
++    makeEntry $w wish {Tcl/Tk Interpreter} WishCmd [incr i] getWish
  #
  # Info
  #
@@ -24,13 +24,13 @@
  proc getWish {} {
 -    global Wish
 -    switch {} $Wish {set Wish [findWish]}
-+    global IWish
-+    switch {} $IWish {set IWish [findWish]}
++    global WishCmd
++    switch {} $WishCmd {set WishCmd [findWish]}
  }
  #
  proc getBin {} {
 -    global Bin env Lib Wish
-+    global Bin env Lib IWish
++    global Bin env Lib WishCmd
      switch {} $Bin {} default return
      foreach x [split $env(PATH) :] {
  	set z [file join $x zircon]
@@ -39,7 +39,7 @@
  	    if {![catch {open $z r} fd]} {
  		gets $fd wish
 -		set Wish [string range $wish 2 end]
-+		set IWish [string range $wish 2 end]
++		set WishCmd [string range $wish 2 end]
  		gets $fd lib
  		close $fd
  		set Lib [lindex $lib 2]	
@@ -57,7 +57,7 @@
      global env
      foreach x [split $env(PATH) :] {
 -	foreach y {wish8.3 wish8.2 wish8.1 wish8.0 wish4.2 wish4.1 wish wishx} {
-+	foreach y {wish8.4 wish8.3 wish8.2 wish8.1 wish8.0 wish4.2 wish4.1 wishx wishx} {
++	foreach y {%%WISH%%} {
  	    if {[file exists [set f [file join $x $y]]]} {
  		return $f
  	    }
