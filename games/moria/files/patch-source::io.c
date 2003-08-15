1b28bf39eeae0688637e2be230bc49226a400862
Greg Lewis <glewis@FreeBSD.org>
2003-08-15 14:28:23 +0000

    . Include <time.h> instead of doing a bogus declaration of time(3).  This   should get the alpha closer to compiling.
    Submitted by:	bento

--- games/moria/files/patch-source::io.c
+++ games/moria/files/patch-source::io.c
@@ -1,19 +1,30 @@
 $FreeBSD$
 
---- source/io.c	Thu Jul 21 19:47:26 1994
-+++ source/io.c	Tue Aug  5 09:59:14 2003
-@@ -13,6 +13,10 @@
+--- source/io.c.orig	Thu Jul 21 19:47:26 1994
++++ source/io.c	Fri Aug 15 07:42:02 2003
+@@ -13,6 +13,11 @@
  
  #include "config.h"
  
 +#ifdef __FreeBSD__
 +#include <sys/ioctl_compat.h>
++#include <time.h>
 +#endif
 +
  #ifdef HPUX
  #include <sys/bsdtty.h>
  #endif
-@@ -313,7 +317,7 @@
+@@ -245,7 +250,9 @@
+   struct ltchars lcbuf;
+   struct tchars cbuf;
+   int lbuf;
++#ifndef __FreeBSD__
+   long time();
++#endif
+ 
+   py.misc.male |= 2;
+   (void) ioctl(0, TIOCGETP, (char *)&tbuf);
+@@ -313,7 +320,7 @@
  #endif
  
    /* PC curses returns ERR */
@@ -22,7 +33,7 @@ $FreeBSD$
    if (initscr() == NULL)
  #else
    if (initscr() == ERR)
-@@ -331,7 +335,7 @@
+@@ -331,7 +338,7 @@
  #if defined(atarist) && defined(__GNUC__)
    (void) signal (SIGTSTP, (__Sigfunc)suspend);
  #else
@@ -31,7 +42,7 @@ $FreeBSD$
    (void) signal (SIGTSTP, (sig_t)suspend);
  #else
    (void) signal (SIGTSTP, suspend);
-@@ -757,7 +761,7 @@
+@@ -757,7 +764,7 @@
        msg_print("Fork failed. Try again.");
        return;
      }
