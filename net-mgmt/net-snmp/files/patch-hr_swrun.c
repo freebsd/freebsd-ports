66f257f4a9a823224969294c8b40987f02d36052
kuriyama <kuriyama@FreeBSD.org>
2003-08-16 08:53:52 +0000

    o Fix not to define "freebsd[234]" definition on freebsd5 environment. o Fix related patch to fit freebsd5 environment. o Implement tcp.tcpCurrEstab to return correct value.
    PR:		ports/54551
    Submitted by:	sub_0@netcabo.pt

--- net-mgmt/net-snmp/files/patch-hr_swrun.c
+++ net-mgmt/net-snmp/files/patch-hr_swrun.c
@@ -1,7 +1,7 @@
---- agent/mibgroup/host/hr_swrun.c.orig	Sat Apr 20 16:30:03 2002
-+++ agent/mibgroup/host/hr_swrun.c	Wed Apr 24 01:43:15 2002
-@@ -557,7 +557,11 @@
-         strcpy(string, proc_buf->p_user.u_comm);
+--- agent/mibgroup/host/hr_swrun.c.orig	Fri Dec 20 00:07:30 2002
++++ agent/mibgroup/host/hr_swrun.c	Sat Aug 16 17:10:54 2003
+@@ -561,7 +561,11 @@
+         string[ sizeof(string)-1 ] = 0;
  #endif
  #elif HAVE_KVM_GETPROCS
 +#if defined(freebsd5) && __FreeBSD_version >= 500014
@@ -12,7 +12,7 @@
  #elif defined(linux)
          sprintf(string, "/proc/%d/status", pid);
          if ((fp = fopen(string, "r")) == NULL)
-@@ -662,7 +666,11 @@
+@@ -666,7 +670,11 @@
          *cp1 = 0;
  #endif
  #elif HAVE_KVM_GETPROCS
@@ -24,7 +24,7 @@
  #elif defined(linux)
          sprintf(string, "/proc/%d/cmdline", pid);
          if ((fp = fopen(string, "r")) == NULL)
-@@ -853,7 +861,11 @@
+@@ -857,7 +865,11 @@
          }
  #else
  #if HAVE_KVM_GETPROCS
@@ -36,7 +36,7 @@
  #elif defined(dynix)
          switch (lowpsinfo.pr_state) {
  #elif defined(solaris2)
-@@ -948,9 +960,17 @@
+@@ -952,9 +964,17 @@
          long_return = proc_buf->p_utime * 100 + proc_buf->p_stime * 100;
  #endif
  #elif HAVE_KVM_GETPROCS
@@ -54,21 +54,23 @@
  #elif defined(linux)
          sprintf(string, "/proc/%d/stat", pid);
          if ((fp = fopen(string, "r")) == NULL)
-@@ -1022,6 +1042,13 @@
- #if defined(freebsd3) && !defined(darwin)
-         long_return =
-             proc_table[LowProcIndex].kp_eproc.e_vm.vm_map.size / 1024;
-+#elif defined(freebsd5) && __FreeBSD_version >= 500014
+@@ -1023,7 +1043,14 @@
+         long_return = proc_buf->p_swrss;
+ #endif
+ #elif HAVE_KVM_GETPROCS
+-#if defined(freebsd3) && !defined(darwin)
++#if defined(freebsd5) && __FreeBSD_version >= 500014
 +	    /* XXX
 +	    long_return = proc_table[LowProcIndex].ki_vmspace->vm_tsize +
 +			  proc_table[LowProcIndex].ki_vmspace->vm_ssize +
 +			  proc_table[LowProcIndex].ki_vmspace->vm_dsize;
 +	    long_return = long_return * (getpagesize() / 1024); */
 +	    long_return = 0;
++#elif defined(freebsd3) && !defined(darwin)
+         long_return =
+             proc_table[LowProcIndex].kp_eproc.e_vm.vm_map.size / 1024;
  #else
-         long_return = proc_table[LowProcIndex].kp_eproc.e_vm.vm_tsize +
-             proc_table[LowProcIndex].kp_eproc.e_vm.vm_ssize +
-@@ -1299,8 +1326,13 @@
+@@ -1303,8 +1330,13 @@
  #elif defined(solaris2)
          return proc_table[current_proc_entry++];
  #elif HAVE_KVM_GETPROCS
