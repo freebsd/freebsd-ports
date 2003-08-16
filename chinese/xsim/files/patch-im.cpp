b6b98651cd1a0cdd8c48622411dacc8252a3f1ba
kris <kris@FreeBSD.org>
2003-08-16 23:33:00 +0000

    Fix build with gcc 3.3.  This port does not respect CFLAGS/CXXFLAGS.

---
+++ chinese/xsim/files/patch-im.cpp
@@ -0,0 +1,10 @@
+--- xsim/im.cpp.orig	Sat Aug 16 16:31:04 2003
++++ xsim/im.cpp	Sat Aug 16 16:31:11 2003
+@@ -16,6 +16,7 @@
+ 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
++#include <cassert>
+ #include <dlfcn.h>
+ 
+ #include "im.h"
---
+++ chinese/xsim/files/patch-simpleim.cpp
@@ -0,0 +1,10 @@
+--- plugins/IMs/jianpin/simpleim.cpp.orig	Sat Aug 16 16:19:23 2003
++++ plugins/IMs/jianpin/simpleim.cpp	Sat Aug 16 16:19:34 2003
+@@ -16,6 +16,7 @@
+ 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ #include <X11/keysym.h>
++#include <cassert>
+ 
+ #include "simpleim.h"
+ #include "b2q.h"
