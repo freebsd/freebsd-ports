8f689d0538db4d9a9259e482aae01040feeb971e
krion <krion@FreeBSD.org>
2003-08-16 16:12:08 +0000

    - Update to 2.2
    Submitted by:	Kurt Jaeger <pi@complx.LF.net>, krion
    Approved by:	maintainer

--- net/lft/files/patch-lft.c
+++ net/lft/files/patch-lft.c
@@ -1,20 +1,56 @@
---- lft.c.orig	Wed Mar  5 16:16:09 2003
-+++ lft.c	Wed Mar  5 16:16:21 2003
-@@ -928,7 +928,7 @@
- 	//	return; /* not for us */
+--- lft.c.orig	Thu May  8 00:43:30 2003
++++ lft.c	Fri Aug 15 13:50:43 2003
+@@ -76,6 +76,12 @@
+ #ifdef BSD
+ #include <machine/limits.h>
+ #endif
++#ifdef BSD_IP_STACK
++#include <errno.h>
++#include <sys/ioctl.h>
++#include <net/bpf.h>
++#include <pcap-int.h>
++#endif
+ 
+ #ifdef sun
+ #include <limits.h>
+@@ -961,7 +967,7 @@
+       	return; * not for us */
  
  
--    if (noisy) printf ("ICMP %u\n", ntohl (tcp->th_seq));
-+    if (noisy) printf ("ICMP %lu\n", ntohl (tcp->th_seq));
+-    if (noisy) printf ("ICMP SEQ=%u received\n", ntohl (tcp->th_seq));
++    if (noisy) printf ("ICMP SEQ=%lu received\n", ntohl (tcp->th_seq));
      recv_packet (ntohl (tcp->th_seq) , orig_ip->ip_src,
  		 (icmp->icmp_type == ICMP_TIMXCEED) ? -2 : icmp->icmp_code);
      break;
-@@ -949,7 +949,7 @@
-       if (noisy) printf (" ACK ");
-     if (tcp->th_flags & TH_SYN)
-       if (noisy) printf (" SYN ");
--    if (noisy) printf (" seq %u ack %u ", ntohl (tcp->th_seq), ntohl (tcp->th_ack));
-+    if (noisy) printf (" seq %lu ack %lu ", ntohl (tcp->th_seq), ntohl (tcp->th_ack));
-     if (noisy) printf ("from %s\n", inet_ntoa (ip->ip_src));
+@@ -1046,6 +1052,9 @@
+ 	   "LFT version 2.2 2003/05/07 compiled for " HOST_SYSTEM_TYPE "\n\n"
+ 	   "    Compile-time options:\n\n"
+ #if defined(BSD_IP_STACK)
++	   "      + BSDSTACK\n"
++#endif
++#if defined(BSD)
+ 	   "      + BSD\n"
+ #endif
+ #if defined(linux)
+@@ -1111,6 +1120,7 @@
+   int use_fins = 0;
+   char *cp;
+   struct timeval tb;
++  unsigned int bpfimmflag;
  
- 	 //if (ntohl(tcp->th_ack) < seq_start || ntohl(tcp->th_ack) > seq_start + trace_packet_info_length + 1)
+   setbuf (stdout, NULL);
+ 
+@@ -1311,6 +1321,13 @@
+     fprintf (stderr, "%s\n", ebuf);
+     exit (1);
+   }
++#ifdef BSD_IP_STACK
++  bpfimmflag = 1;
++  if ( ioctl(pd->fd, BIOCIMMEDIATE, &bpfimmflag) < 0) {
++    fprintf(stderr, "BIOCIMMEDIATE: %s",
++    pcap_strerror(errno));
++  }
++#endif
+ #endif
+   init_address (hostname, pcap_dev);
+   open_sockets ();
