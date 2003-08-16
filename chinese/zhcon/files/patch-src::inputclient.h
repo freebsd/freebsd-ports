4c393eec307b23b896de9806a2acb7bafa4f7897
Kris Kennaway <kris@FreeBSD.org>
2003-08-16 23:37:25 +0000

    Fix build with gcc 3.3.

---
+++ chinese/zhcon/files/patch-src::inputclient.h
@@ -0,0 +1,10 @@
+--- src/inputclient.h.orig	Sat Aug 16 16:34:06 2003
++++ src/inputclient.h	Sat Aug 16 16:34:15 2003
+@@ -19,6 +19,7 @@
+ #ifndef INPUTCLIENT_H
+ #define INPUTCLIENT_H
+ 
++#include <cassert>
+ #include <string>
+ #include "console.h"
+ 
