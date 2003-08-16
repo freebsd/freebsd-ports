8cdaebb0773d3c767f6795aeaec2e9aa97930697
Kris Kennaway <kris@FreeBSD.org>
2003-08-16 23:33:00 +0000

    Fix build with gcc 3.3.  This port does not respect CFLAGS/CXXFLAGS.

---
+++ chinese/xsim/files/patch-pyphraser.cpp
@@ -0,0 +1,10 @@
+--- plugins/IMs/jianpin/pyphraser.cpp.orig	Sat Aug 16 16:21:19 2003
++++ plugins/IMs/jianpin/pyphraser.cpp	Sat Aug 16 16:21:30 2003
+@@ -16,6 +16,7 @@
+ 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
++#include <cassert>
+ #include "wstring.h"
+ #include "pyphraser.h"
+ #include "simpleimc.h"
---
+++ chinese/xsim/files/patch-wubiim.cpp
@@ -0,0 +1,10 @@
+--- plugins/IMs/wubi/wubiim.cpp.orig	Sat Aug 16 16:22:10 2003
++++ plugins/IMs/wubi/wubiim.cpp	Sat Aug 16 16:22:18 2003
+@@ -16,6 +16,7 @@
+ 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
++#include <cassert>
+ #include <X11/keysym.h>
+ 
+ #include "wubiim.h"
