24b6449f5829ee758537768ae8830738155f506c
Kirill Ponomarev <krion@FreeBSD.org>
2003-08-18 08:54:01 +0000

    - Update to version 2.9.1
    PR:		55686
    Submitted by:	Ports Fury

--- finance/qhacc/files/patch-ltmain.sh
+++ finance/qhacc/files/patch-ltmain.sh
@@ -1,5 +1,23 @@
 --- ltmain.sh.orig	Thu Mar 13 01:25:30 2003
-+++ ltmain.sh	Tue Aug 12 01:49:52 2003
++++ ltmain.sh	Mon Aug 18 02:14:24 2003
+@@ -1043,7 +1043,7 @@
+ 	    # These systems don't actually have a C library (as such)
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    test "X$arg" = "X-lc" && continue
+ 	    ;;
+@@ -2441,7 +2441,7 @@
+ 	  *-*-netbsd*)
+ 	    # Don't link with libc until the a.out ld.so is fixed.
+ 	    ;;
+-	  *-*-openbsd*)
++	  *-*-openbsd* | *-*-freebsd*)
+ 	    # Do not include libc due to us having libc/libc_r.
+ 	    ;;
+ 	  *)
 @@ -4210,10 +4210,12 @@
  	fi
  
