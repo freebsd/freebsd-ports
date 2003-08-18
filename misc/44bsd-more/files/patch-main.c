113c74438c2a00a58d7824903f12a633deca1413
Tim Vanderhoek <hoek@FreeBSD.org>
2003-08-18 03:03:22 +0000

    "Fixes for \ gcc33"
    PR:	ports/55288 (Volker Stolz)

---
+++ misc/44bsd-more/files/patch-main.c
@@ -0,0 +1,14 @@
+--- main.c.orig	Tue Aug  5 17:17:15 2003
++++ main.c	Tue Aug  5 17:21:34 2003
+@@ -33,9 +33,9 @@
+  */
+ 
+ #ifndef lint
+-char copyright[] =
++char copyright[] = \
+ "@(#) Copyright (c) 1988 Mark Nudleman.\n\
+-@(#) Copyright (c) 1988, 1993
++@(#) Copyright (c) 1988, 1993\
+ 	Regents of the University of California.  All rights reserved.\n";
+ #endif /* not lint */
+ 
