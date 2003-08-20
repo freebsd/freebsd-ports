d5777e546795093c6cb5dd7af672daa86452c100
leeym <leeym@FreeBSD.org>
2003-08-20 15:55:34 +0000

    catch up with news/tin (tin-1.6.1)
    Submitted by:	Jia-Shiun Li <jsli@mail2000.com.tw>

--- chinese/tin/files/patch-attrib.c
+++ chinese/tin/files/patch-attrib.c
@@ -1,14 +1,14 @@
---- src/attrib.c.orig	Thu Jan  9 10:55:14 2003
-+++ src/attrib.c	Thu Jan  9 10:56:45 2003
-@@ -626,6 +626,11 @@
- 	fprintf(fp, "delete_tmp_files=ON\n");
- 	fprintf(fp, "followup_to=poster\n\n");
- 
-+	fprintf(fp, _("# set charset to Big5 in all newsgroups\n"));
-+	fprintf(fp, "scope=*\n");
-+	fprintf(fp, "mm_network_charset=Big5\n");
-+	fprintf(fp, "undeclared_charset=Big5\n");
+--- src/attrib.c.orig	Wed Aug 20 19:19:52 2003
++++ src/attrib.c	Wed Aug 20 19:23:06 2003
+@@ -687,6 +687,11 @@
+ 		fprintf(fp, "post_proc_type=%d\n", POST_PROC_YES);
+ 		fprintf(fp, "delete_tmp_files=ON\n");
+ 		fprintf(fp, "followup_to=poster\n\n");
 +
- #if 0 /* FIXME */
- 	for_each_group(i) {
- 		group = &active[i];
++		fprintf(fp, _("# set charset to Big5 in all newsgroups\n"));
++		fprintf(fp, "scope=*\n");
++		fprintf(fp, "mm_network_charset=Big5\n");
++		printf(fp, "undeclared_charset=Big5\n");
+ 	} else {
+ 		char *ptr;
+ 		char buf[LEN];
--- chinese/tin/files/patch-init.c
+++ chinese/tin/files/patch-init.c
@@ -1,8 +1,8 @@
---- src/init.c.orig	Sun May 11 14:06:09 2003
-+++ src/init.c	Sun May 11 14:07:53 2003
-@@ -291,8 +291,8 @@
- 	2,		/* recent_time */
+--- src/init.c.orig	Sun Aug 10 09:37:40 2003
++++ src/init.c	Wed Aug 20 19:23:32 2003
+@@ -282,8 +282,8 @@
  	32,		/* groupname_max_length */
+ 	UUE_NO,	/* hide_uue */
  	KILL_UNREAD,		/* kill_level */
 -	MIME_ENCODING_7BIT,		/* mail_mime_encoding */
 -	MIME_ENCODING_7BIT,		/* post_mime_encoding */
@@ -11,7 +11,7 @@
  	POST_PROC_NO,			/* post_process */
  	REREAD_ACTIVE_FILE_SECS,	/* reread_active_file_secs */
  	1,		/* scroll_lines */
-@@ -339,7 +339,7 @@
+@@ -330,7 +330,7 @@
  	TRUE,		/* word_highlight */
  	0,		/* wrap_column */
  #ifdef HAVE_COLOR
@@ -20,12 +20,13 @@
  #endif /* HAVE_COLOR */
  	TRUE,		/* add_posted_to_filter */
  	TRUE,		/* advertising */
-@@ -369,11 +369,11 @@
+@@ -360,12 +360,12 @@
  #endif /* USE_INVERSE_HACK */
  	TRUE,		/* keep_dead_articles */
  	POSTED_FILE,	/* posted_articles_file */
 -	FALSE,		/* mail_8bit_header */
 +	TRUE,		/* mail_8bit_header */
+ 	FALSE,		/* mark_ignore_tags */
  	TRUE,		/* mark_saved_read */
  	TRUE,		/* pgdn_goto_next */
  	TRUE,		/* pos_first_unread */
