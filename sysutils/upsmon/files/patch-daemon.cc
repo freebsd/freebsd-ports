a1262455adc758ce6afa922fec939a79f47f5663
Erwin Lansing <erwin@FreeBSD.org>
2003-08-20 22:05:09 +0000

    Fix for 5.x
    PR:		55819
    Submitted by:	rwatson

---
+++ sysutils/upsmon/files/patch-daemon.cc
@@ -0,0 +1,10 @@
+--- daemon.cc.bak	Tue Aug  4 17:18:14 1998
++++ daemon.cc	Wed Aug 20 17:07:49 2003
+@@ -32,6 +32,7 @@
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
++#include <stdlib.h>
+ #include <unistd.h>
+ #include "stddef.h"
+ #include "daemon.h"
