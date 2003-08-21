9e444fc268bea56922f02ac82a2267b46a9dc360
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2003-08-21 11:19:17 +0000

    o Update to 1.4.3 o Unmark FORBIDDEN
    PR:		55471
    Submitted by:	Morten Rodal <morten@rodal.no>

--- net-p2p/xmule/files/patch-src::xmule.cpp
+++ net-p2p/xmule/files/patch-src::xmule.cpp
@@ -1,42 +1,20 @@
---- src/xmule.cpp.orig	Tue May 27 00:02:47 2003
-+++ src/xmule.cpp	Tue May 27 21:31:38 2003
-@@ -55,16 +55,6 @@
- //	ON_COMMAND(ID_HELP, CWinApp::OnHelp)
- //END_MESSAGE_MAP()
- 
--#include <sys/time.h>
--inline long GetTickCount()
--{	
--  struct timeval aika;
--  gettimeofday(&aika,NULL);
--  unsigned long secs=aika.tv_sec*1000;
--  secs+=(aika.tv_usec/1000);
--  return secs;
--}
--
- CxmuleApp::CxmuleApp() {
-   splashBmp=new wxBitmap((const char**)About_jpg);
+--- src/xmule.cpp.orig	Mon Aug 11 14:38:14 2003
++++ src/xmule.cpp	Mon Aug 11 14:38:27 2003
+@@ -763,7 +763,7 @@
+ 	delete[] xmulesig_path;
  }
-@@ -641,11 +631,13 @@
-     
- } //End Added By Bouc7
  
--
+-#if defined(__Linux__)
 +#if 0
  #include <execinfo.h>
-+#endif
+ #endif
  
- void CxmuleApp::OnFatalException()
- {
-+#if 0
+@@ -776,7 +776,7 @@
+     	theApp.clientudp->Destroy();
+ 
      // (stkn) create backtrace
+-#if defined(__Linux__)
++#if 0
      void * bt_array[100]; // 100 should be enough ?!?
      char ** bt_strings;
-@@ -666,6 +658,7 @@
-        fprintf(stderr, "[%d] %s\n", i, bt_strings[i]);
-        
-     free(bt_strings);
-+#endif
- }
- 
- #define wxGTK_WINDOW 1
+     int num_entries;
