07d2d4687bd693ad675a16b01e62f729ef6c3912
Hye-Shik Chang <perky@FreeBSD.org>
2003-08-21 05:04:32 +0000

    Update to 0.6.2

--- korean/gdick/files/setup.py
+++
@@ -1,7 +0,0 @@
-__version__ = "$FreeBSD$"
-from distutils.core import setup
-setup(	name = "gdick",
-	py_modules = ["YahooDict", "YahooView"],
-	scripts = ["gdick", "gdick_applet"],
-	data_files = [("share/gdick", ("gdick.glade",))],
-)
