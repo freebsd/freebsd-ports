6265984963be07d5929a32223b092ecd1aad8b42
bland <bland@FreeBSD.org>
2003-08-25 23:33:45 +0000

    Update to 1.3.7
    Approved by: marcus (mentor)

--- devel/orbitcpp/files/patch-orbitcpp::orb-cpp::orbitcpp_string_seq.h
+++ devel/orbitcpp/files/patch-orbitcpp::orb-cpp::orbitcpp_string_seq.h
@@ -1,27 +1,24 @@
---- orbitcpp/orb-cpp/orbitcpp_string_seq.h.orig	Wed Jul 23 10:57:57 2003
-+++ orbitcpp/orb-cpp/orbitcpp_string_seq.h	Wed Jul 23 10:58:52 2003
-@@ -35,7 +35,7 @@
-     class StringSeqBase: public SequenceBase <CORBA::String_mgr,
- 			 CORBA_char*, CORBA_sequence_CORBA_string>
-     {
--    protected:
-+    public:
- 	typedef CORBA::String_mgr           value_t;
- 	typedef CORBA_char*                 c_value_t;
- 	typedef CORBA_sequence_CORBA_string c_seq_t;
-@@ -90,6 +90,7 @@
+--- orbitcpp/orb-cpp/orbitcpp_string_seq.h.orig	Mon Aug 25 15:18:19 2003
++++ orbitcpp/orb-cpp/orbitcpp_string_seq.h	Mon Aug 25 15:45:43 2003
+@@ -90,8 +90,10 @@
  
      class StringUnboundedSeq: public StringSeqBase
      {
 +    public:
  	typedef StringSeqBase            Super;
  	
++	typedef Super::value_t  value_t;
  	typedef Super::size_t   size_t;
-@@ -145,6 +146,7 @@
+ 	typedef Super::index_t  index_t;
+ 	typedef Super::buffer_t buffer_t;
+@@ -145,8 +147,10 @@
      template<CORBA::ULong max>
      class StringBoundedSeq: public StringSeqBase
      {
 +    public:
  	typedef StringSeqBase   Super;
  
++	typedef Super::value_t  value_t;
  	typedef Super::size_t   size_t;
+ 	typedef Super::index_t  index_t;
+ 	typedef Super::buffer_t buffer_t;
