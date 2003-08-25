e04eb58e52ead4c604027524f1fccf9881551c1e
edwin <edwin@FreeBSD.org>
2003-08-25 12:13:36 +0000

    [update] www/suphp: update to 0.3 and fix a security leak introduced with previous patch
    	- update to 0.3
    	- add a workaround to don't use getpwuid to performs check for apache user (since 4.x
    	  doesn't support getpwuid_r).
    	- use %%DOCSDIR%%
    
    PR:		ports/55952
    Submitted by:	Clement Laforet <sheepkiller@cultdeadsheep.org>

--- www/suphp/files/patch-config.h
+++
@@ -1,9 +0,0 @@
---- config.h.orig	Wed Oct 23 22:16:50 2002
-+++ config.h	Wed Nov 13 16:30:35 2002
-@@ -24,5 +24,6 @@
- #define OPT_MIN_UID 100
- #define OPT_MIN_GID 100
- #define OPT_APACHE_USER "wwwrun"
-+#define OPT_APACHE_GROUP "wwwrun"
- #define OPT_PATH_TO_PHP "/usr/bin/php"
- #define OPT_LOGFILE "/opt/apache/var/logs/suphp_log"
