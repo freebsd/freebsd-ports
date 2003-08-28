a04969334177e0d1fdbf9969e0cb9f384d268520
Christian Weisgerber <naddy@FreeBSD.org>
2003-08-28 16:56:58 +0000

    Convert from varargs.h to stdarg.h in order to fix build on -CURRENT.

--- audio/xcdplayer/files/patch-debug.c
+++ audio/xcdplayer/files/patch-debug.c
@@ -2,8 +2,14 @@
 $FreeBSD$
 
 --- debug.c.orig	Tue Jan 12 19:59:35 1993
-+++ debug.c	Mon Aug 25 00:38:35 2003
-@@ -20,6 +20,10 @@
++++ debug.c	Mon Aug 25 01:36:43 2003
+@@ -15,28 +15,29 @@
+ 
+ # include <X11/Intrinsic.h>
+ 
+-# include <varargs.h>
++# include <stdarg.h>
+ # include <stdio.h>
  
  # include "debug.h"
  
@@ -13,13 +19,24 @@ $FreeBSD$
 +
  /* VARARGS */
  void
- debug_printf(va_alist)
-@@ -33,7 +37,7 @@
+-debug_printf(va_alist)
+-	va_dcl
++debug_printf(int flag, char *fmt, ...)
+ {
+ 	va_list		args;
+-	char		*fmt;
+ 
+-	va_start(args);
++	va_start(args, fmt);
+ 
  	/*
  	 * first arg is whether or not to print.
  	 */
 -	if ((va_arg(args, int) == 0) || (debug == False))
-+	if ((va_arg(args, int) == 0) || (app_data.debug == False))
++	if ((flag == 0) || (app_data.debug == False))
  		return;
  
- 	fmt = va_arg(args, char *);
+-	fmt = va_arg(args, char *);
+ 	vfprintf(stdout, fmt, args);
+ 
+ 	fflush(stdout);
