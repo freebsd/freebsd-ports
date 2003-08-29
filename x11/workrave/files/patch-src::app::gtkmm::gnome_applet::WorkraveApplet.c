0eac30808f6d443ae4131adb8da1dbcf904c1da8
Alexander Nedotsukov <bland@FreeBSD.org>
2003-08-29 17:04:31 +0000

    Update to 1.4.0
    Submitted by: maintainer (-STABLE patches)
    Approved by:  marcus (mentor)

--- x11/workrave/files/patch-src::app::gtkmm::gnome_applet::WorkraveApplet.c
+++ x11/workrave/files/patch-src::app::gtkmm::gnome_applet::WorkraveApplet.c
@@ -1,6 +1,6 @@
---- gui/gnome/WorkraveApplet.c.orig	Tue Mar 18 22:04:42 2003
-+++ gui/gnome/WorkraveApplet.c	Thu Jul 31 13:43:50 2003
-@@ -783,6 +783,7 @@
+--- src/app/gtkmm/gnome_applet/WorkraveApplet.c.orig	Wed Aug  6 23:01:06 2003
++++ src/app/gtkmm/gnome_applet/WorkraveApplet.c	Fri Aug 29 18:24:11 2003
+@@ -849,6 +849,7 @@
  {
    GdkPixbuf *pixbuf = NULL;
    GtkWidget *hbox = NULL;
@@ -8,12 +8,11 @@
    BonoboUIComponent *ui = NULL;
    
    // Create menus.
-@@ -797,7 +798,7 @@
+@@ -863,7 +864,6 @@
    bonobo_ui_component_add_listener(ui, "Quiet", mode_callback, NULL);
  
    // Eventbox
 -  GtkWidget *event_box = gtk_event_box_new();
-+//  GtkWidget *event_box = gtk_event_box_new();
    applet_control->event_box = event_box;
    gtk_widget_set_events(event_box, gtk_widget_get_events(event_box) | GDK_BUTTON_PRESS_MASK);
    gtk_widget_show(GTK_WIDGET(event_box));
