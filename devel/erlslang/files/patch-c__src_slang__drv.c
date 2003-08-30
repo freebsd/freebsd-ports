07be1cc368dba826cb0b5276d756f3df6c2a8c6e
olgeni <olgeni@FreeBSD.org>
2003-08-30 18:46:21 +0000

    Add erlslang, SLang binding for Erlang.
    Submitted by:	Chris Pressey <chris_pressey@yahoo.ca>

---
+++ devel/erlslang/files/patch-c__src_slang__drv.c
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- c_src/slang_drv.c.orig	Tue Dec 12 14:16:28 2000
++++ c_src/slang_drv.c	Sat Jul 19 12:28:11 2003
+@@ -1,6 +1,6 @@
+ 
+ #include <stdio.h>
+-#include "driver.h"
++#include "obsolete/driver.h"
+ #include <slang.h>
+ #include <signal.h> 
+ 
