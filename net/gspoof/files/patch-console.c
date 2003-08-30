5842295de36bdb145047df65cbf3d727716b5684
krion <krion@FreeBSD.org>
2003-08-30 17:04:34 +0000

    - Update to version 3.0
    PR:		56195
    Submitted by:	Ports Fury

--- net/gspoof/files/patch-console.c
+++ net/gspoof/files/patch-console.c
@@ -1,23 +1,23 @@
---- console.c.orig	Wed Sep 25 04:20:25 2002
-+++ console.c	Wed May  7 03:42:30 2003
-@@ -333,7 +333,11 @@
+--- console.c.orig	Sat Aug 30 00:16:50 2003
++++ console.c	Sat Aug 30 15:56:23 2003
+@@ -335,7 +335,11 @@
  		  fgets(value, 20, stdin);
  		  seq = atol(value);
  	       }
 +#ifdef __FreeBSD__
-+	     while (atoi(value) < 0 || atoi(value) > 4294970000);
++	     while (atoi(value) < 0 || strtoll(value, NULL, 10) > 4294970000);
 +#else
  	     while (atoi(value) < 0 || atoll(value) > 4294970000);
 +#endif
  	  }
  	else if (!strcmp ("3.5", cmd))
  	  {
-@@ -343,7 +347,11 @@
+@@ -345,7 +349,11 @@
  		  fgets(value, 20, stdin);
  		  ack = atol(value);
  	       }
 +#ifdef __FreeBSD__
-+	     while (atoi(value) < 0 || atoi(value) > 4294970000);
++	     while (atoi(value) < 0 || strtoll(value, NULL, 10) > 4294970000);
 +#else
  	     while (atoi(value) < 0 || atoll(value) > 4294970000);
 +#endif
