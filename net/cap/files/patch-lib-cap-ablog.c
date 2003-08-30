991df8ab845731139744befc391de0ca1704fdf8
krion <krion@FreeBSD.org>
2003-08-30 22:54:37 +0000

    - Fix build on -current   o varargs -> stdarg   o remove non-existant host from {MASTER,PATCH}_SITES:
    PR:		56214
    Submitted by:	Michael Edenfield <kutulu@kutulu.org>

--- net/cap/files/patch-lib-cap-ablog.c
+++ net/cap/files/patch-lib-cap-ablog.c
@@ -1,6 +1,6 @@
---- lib/cap/ablog.c.orig	Mon Feb  3 01:16:00 2003
-+++ lib/cap/ablog.c	Mon Feb  3 03:48:59 2003
-@@ -24,6 +24,7 @@
+--- lib/cap/ablog.c.orig	Sat Aug 30 18:19:18 2003
++++ lib/cap/ablog.c	Sat Aug 30 18:25:05 2003
+@@ -24,13 +24,14 @@
   *
  */
  
@@ -8,6 +8,14 @@
  #include <stdio.h>
  #include <sys/types.h>
  #include <sys/time.h>
+ #include <netat/appletalk.h>
+ 
+ #ifdef USEVPRINTF
+-# include <varargs.h>
++# include <stdarg.h>
+ #endif USEVPRINTF
+ #ifdef USETIMES
+ # include <time.h>
 @@ -66,8 +67,13 @@
   * This is something all machine should, but don't have :-)
   */
@@ -23,7 +31,20 @@
  
  #ifndef USEVPRINTF
  /* Bletch - gotta do it because pyramids don't work the other way */
-@@ -91,7 +97,7 @@
+@@ -79,19 +85,16 @@
+ char *fmt;
+ #else USEVPRINTF
+ /*VARARGS*/
+-logit(va_alist)
+-va_dcl
++logit(int level, char *fmt, ...)
+ #endif USEVPRINTF
+ {
+   static char *mytod();
+ #ifdef USEVPRINTF
+-  register char *fmt;
+   va_list args;
+-  int level;
  #endif USEVPRINTF
    int saveerr;
    extern int errno;
@@ -32,3 +53,14 @@
  #ifndef __FreeBSD__
    extern char *sys_errlist[];
  #endif
+@@ -101,9 +104,7 @@
+ 
+   saveerr = errno;
+ #ifdef USEVPRINTF
+-  va_start(args);
+-  level = va_arg(args, int);
+-  fmt = va_arg(args, char *);
++  va_start(args, fmt);
+ #endif USEVPRINTF
+ 
+   if (dlevel < (level & L_LVL))
