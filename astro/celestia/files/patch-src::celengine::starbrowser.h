15e1f930fd554969e32abeebe9c5976a31fab335
edwin <edwin@FreeBSD.org>
2003-08-31 03:58:53 +0000

    Partly unbreak astro/celestia due to C++ issues. Also applied PR ports/46149
    PR:		ports/46149
    Submitted by:	Brad Laue <brad@brad-x.com>
    Approved by:	maintainer timeout

---
+++ astro/celestia/files/patch-src::celengine::starbrowser.h
@@ -0,0 +1,11 @@
+--- src/celengine/starbrowser.h.orig	Sat Aug 30 20:38:54 2003
++++ src/celengine/starbrowser.h	Sat Aug 30 20:39:08 2003
+@@ -12,6 +12,8 @@
+ #ifndef _CELENGINE_STARBROWSER_H_
+ #define _CELENGINE_STARBROWSER_H_
+ 
++using namespace std;
++
+ #include "star.h"
+ #include "stardb.h"
+ #include "simulation.h"
