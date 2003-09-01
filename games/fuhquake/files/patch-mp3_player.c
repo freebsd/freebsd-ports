0aa5177174cf05ae7db462b9908ddabedbe7e974
osa <osa@FreeBSD.org>
2003-09-01 11:52:06 +0000

    Update port: - Fixes build on 5.x with XMMS support enabled; - Fixes path where to look for XMMS binary; - Allows fuhquake to be run from any directory per kris' suggestion; - Adds WITH_SHAREWARE_DATA knob.  Coupled with previous, this   makes fuhquake playable right after install, yet allows it to be   distributed on CDROM (since shareware data cannot be included,   one must explicitly define this knob); - Properly utilizes EXTRACT_ONLY in Makefile and DOCSDIR in pkg-plist; - Removes EXTRACT_BEFORE_ARGS from Makefile since it's no longer needed; - Tells user about WITHOUT_XMMS knob, when XMMS bits are found; - Fixes palette problems on 24-bit depth with x11-renderer; - Fixes DGA mouse behavior on higher mouse rates when GLX-rendered.
    Submitted by:	Alexey Dokuchaev <danfe@regency.nsu.ru>
    PR:		56085

--- games/fuhquake/files/patch-mp3_player.c
+++ games/fuhquake/files/patch-mp3_player.c
@@ -1,13 +1,26 @@
---- mp3_player.c.orig	Wed May 21 17:18:36 2003
-+++ mp3_player.c	Wed May 21 17:21:23 2003
-@@ -48,6 +48,7 @@
+--- mp3_player.c.orig	Mon Sep  1 18:18:40 2003
++++ mp3_player.c	Mon Sep  1 18:20:09 2003
+@@ -48,9 +48,10 @@
  
  #ifdef __XMMS__
  
 +#include <sys/signal.h>
  #include <sys/wait.h>
  
- cvar_t mp3_dir = {"mp3_xmms_dir", "/usr/local/bin"};
+-cvar_t mp3_dir = {"mp3_xmms_dir", "/usr/local/bin"};
++cvar_t mp3_dir = {"mp3_xmms_dir", "/usr/X11R6/bin"};
+ cvar_t mp3_xmms_session = {"mp3_xmms_session", "0"};
+ 
+ #endif
+@@ -288,7 +289,7 @@
+ #define XMMS_COMMAND(Name, Param)						\
+     void MP3_##Name##_f(void) {							\
+ 	   if (MP3_IsPlayerRunning()) {						\
+-		   xmms_remote_##Param##(XMMS_SESSION);			\
++		   xmms_remote_##Param(XMMS_SESSION);			\
+ 	   } else {											\
+ 		   Com_Printf("%s\n", mp3_notrunning_msg);		\
+ 	   }												\
 @@ -769,7 +770,7 @@
  
  #endif
