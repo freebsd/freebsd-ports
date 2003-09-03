1570ba744047133f1dfd6cc53c2c01a6ccd80bae
Dmitry Sivachenko <demon@FreeBSD.org>
2003-09-03 14:10:01 +0000

    New port: Network Load plugin for XFce4.

---
+++ sysutils/xfce4-netload-plugin/files/patch-panel-plugin-netload.c
@@ -0,0 +1,11 @@
+--- panel-plugin/netload.c.orig	Wed Sep  3 18:04:50 2003
++++ panel-plugin/netload.c	Wed Sep  3 18:05:13 2003
+@@ -43,7 +43,7 @@
+ 
+ /* Defaults */
+ #define DEFAULT_TEXT "Net"
+-#define DEFAULT_DEVICE "eth0"
++#define DEFAULT_DEVICE "fxp0"
+ 
+ #define INIT_MAX 4096
+ #define MINIMAL_MAX 1024
