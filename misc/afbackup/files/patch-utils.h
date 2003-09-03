c7570d4792cf3814ef8f78f48fbfd8dd736eaa8c
Kirill Ponomarev <krion@FreeBSD.org>
2003-09-03 09:40:19 +0000

    - Unbreak on -current
    PR:		54108
    Submitted by:	Alexander Marx <mad@madness.at>, maintainer timeout = 2 months

---
+++ misc/afbackup/files/patch-utils.h
@@ -0,0 +1,10 @@
+--- utils.h.orig     Sat Jun 28 02:00:49 2003
++++ utils.h    Sat Jun 28 02:04:23 2003
+@@ -10,6 +10,8 @@
+ #include <string.h>
+ #include <math.h>
+ #include <x_types.h>
++#include <time.h>
++#include <sys/types.h>
+
+ #define        NO_ERROR        0
