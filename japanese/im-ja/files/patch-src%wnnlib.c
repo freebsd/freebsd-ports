b6b409378e601f648e12ba0e4f66408ec78ee192
nectar <nectar@FreeBSD.org>
2003-09-03 00:13:57 +0000

    Add im-ja, a Japanese input module for GTK2.

---
+++ japanese/im-ja/files/patch-src%wnnlib.c
@@ -0,0 +1,12 @@
+*** src/wnnlib.c.orig	Tue Sep  2 15:42:48 2003
+--- src/wnnlib.c	Tue Sep  2 15:42:58 2003
+***************
+*** 585,590 ****
+--- 585,591 ----
+  #include	<string.h>
+  #include        <stdlib.h>
+  #include	<pwd.h>
++ #include	<unistd.h>
+  
+  #ifndef ENVDIR
+  #define ENVDIR	"/usr/share/wnn"
