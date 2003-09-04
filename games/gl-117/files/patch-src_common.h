14d4a1ea2d06059d43fc4fdd034d042a04dfc36a
Edwin Groothuis <edwin@FreeBSD.org>
2003-09-04 12:48:40 +0000

    [Maintainer Update] games/gl-117 0.9 -> 1.0
    	reshmeat blurb:
    	This release adds three more missions to the campaign. It
    	provides more realistic physics (optional), a new fighter,
    	some visual improvements, and adjustments concerning almost
    	everything. Some important bugfixes have been made.
    
    PR:		ports/55931
    Submitted by:	Erik Greenwald <erik@smluc.org>

---
+++ games/gl-117/files/patch-src_common.h
@@ -0,0 +1,38 @@
+--- src/common.h.orig	Fri Aug 22 06:07:09 2003
++++ src/common.h	Thu Sep  4 05:31:40 2003
+@@ -22,13 +22,13 @@
+ // APPLE path is different (thanks Minami)
+ #ifdef __APPLE__
+ #include <GLUT/glut.h>
+-#include "SDL/SDL.h"
+-#include "SDL/SDL_thread.h"
++#include "SDL.h"
++#include "SDL_thread.h"
+ #ifdef HAVE_SDL_MIXER
+-#include "SDL/SDL_mixer.h"
++#include "SDL_mixer.h"
+ #endif
+ #ifdef HAVE_SDL_NET
+-#include "SDL/SDL_net.h"
++#include "SDL_net.h"
+ #endif
+ #endif
+ #ifndef __APPLE__
+@@ -36,13 +36,13 @@
+ #include <GL/glut.h>
+ #else
+ #include <GL/glut.h>
+-#include "SDL/SDL.h"
+-#include "SDL/SDL_thread.h"
++#include "SDL.h"
++#include "SDL_thread.h"
+ #ifdef HAVE_SDL_MIXER
+-#include "SDL/SDL_mixer.h"
++#include "SDL_mixer.h"
+ #endif
+ #ifdef HAVE_SDL_NET
+-#include "SDL/SDL_net.h"
++#include "SDL_net.h"
+ #endif
+ #endif
+ #endif // __APPLE__
