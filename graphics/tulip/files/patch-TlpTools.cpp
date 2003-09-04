5d96d3984a34685da2e0f9eee95fdc6ed6f5e1fe
krion <krion@FreeBSD.org>
2003-09-04 16:59:17 +0000

    - Update to version 1.2.5
    PR:		56434
    Submitted by:	Ports Fury

--- graphics/tulip/files/patch-TlpTools.cpp
+++ graphics/tulip/files/patch-TlpTools.cpp
@@ -1,9 +1,9 @@
---- library/tulip/src/TlpTools.cpp.orig	Wed Apr  2 23:46:09 2003
-+++ library/tulip/src/TlpTools.cpp	Mon May 26 14:32:25 2003
-@@ -36,7 +36,7 @@
+--- library/tulip/src/TlpTools.cpp.orig	Tue Jul 29 20:59:26 2003
++++ library/tulip/src/TlpTools.cpp	Wed Sep  3 23:46:41 2003
+@@ -46,7 +46,7 @@
  }
  //=========================================================
- // accepts only file names ending with ".so"
+ // accepts only file names ending with ".so"; used by TemplateFactory.cxx for directory filtering
 -int __tulip_selectSO(const struct dirent *ent) {
 +int __tulip_selectSO(struct dirent *ent) {
    const char *SO = ".so";
