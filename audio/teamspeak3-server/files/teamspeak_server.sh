65e6d0e0006cadbffb50159cb91f4a64eafe7b3d
Edwin Groothuis <edwin@FreeBSD.org>
2003-09-05 04:46:06 +0000

    iMinor teamspeak_server port update
    	This patch by crtxreavr@trioptimum.com to prevent users
    	from read passwords out of the log file and database. Also
    	runs the server as the nobody user.
    
    PR:		ports/56464
    Submitted by:	Daniel O'Connor <darius@dons.net.au>

--- audio/teamspeak3-server/files/teamspeak_server.sh
+++ audio/teamspeak3-server/files/teamspeak_server.sh
@@ -9,18 +9,21 @@ PIDFILE=${TSHOME}/server.pid
 cd "$TSHOME"
 
 case "$1" in
-    start)
-        ./server_linux >/dev/null 2>&1
-        ;;
-    stop)
-        kill -TERM `cat "$PIDFILE"`
-        ;;
-    restart)
-        $0 stop && $0 start || exit 1
-        ;;
-    *)
-        echo "Usage: $0 {start|stop|restart}"
-        exit 1
+	start)
+		su -fm nobody -c './server_linux >/dev/null 2>&1' && echo -n ' teamspeak_server'
+		;;
+	stop)
+		kill -TERM `cat "$PIDFILE"`
+		;;
+	restart)
+		$0 stop
+		echo 5 second pause before restart. . .
+		sleep 5
+		$0 start
+		;;
+	*)
+	echo "Usage: $0 {start|stop|restart}"
+	exit 1
 esac
 exit 0
 
