e327f54df0d5cc68ce9c1f430b4c53c3a5059be5
Hajimu UMEMOTO <ume@FreeBSD.org>
2003-09-06 17:45:02 +0000

    Use USE_RC_SUBR.

---
+++ net/dtcpclient/files/dtcpclient.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+# PROVIDE: dtcpclient
+# REQUIRE: DAEMON
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
+#
+# NOTE for FreeBSD 5.0+:
+# If you want this script to start with the base rc scripts
+# move imapd.sh to /etc/rc.d/dtcpclient
+
+prefix=%%PREFIX%%
+
+# Define these imapd_* variables in one of these files:
+#	/etc/rc.conf
+#	/etc/rc.conf.local
+#	/etc/rc.conf.d/dtcpclient
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE
+#
+dtcpclient_enable="NO"				# Enable dtcpclient
+#dtcpclient_program="${prefix}/sbin/dtcpclient"	# Location of dtcpclient
+dtcpclient_server=""				# DTCP server name
+dtcpclient_username=""				# DTCP user name
+dtcpclient_flags="-t network -Dl"		# Flags to dtcpclient program
+
+. %%RC_SUBR%%
+
+name="dtcpclient"
+rcvar=`set_rcvar`
+command="${prefix}/sbin/${name}"
+pidfile="/var/run/${name}.pid"
+extra_commands="reload"
+
+load_rc_config $name
+flags="${dtcpclient_flags} -u ${dtcpclient_username} ${dtcpclient_server}"
+run_rc_command "$1"
