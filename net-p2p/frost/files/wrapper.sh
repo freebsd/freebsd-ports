a82b07dd2bc409e62c5f0023f42948fea3f102fa
lioux <lioux@FreeBSD.org>
2003-09-08 00:45:12 +0000

    Update to 1.2.0.6 (wot-071203-upd6)
    PR:		55249
    Submitted by:	Stefan Walter <sw@gegenunendlich.de>

--- net-p2p/frost/files/wrapper.sh
+++ net-p2p/frost/files/wrapper.sh
@@ -4,12 +4,19 @@ PREFIX="%%PREFIX%%"
 JAVAVM="%%JAVAVM%%"
 JAR_DIR="%%JAR_DIR%%"
 JAR_FILE="%%JAR_FILE%%"
+SHARE_DIR="%%SHARE_DIR%%"
 HOME_DIR=${HOME}/.frost
+CLASSPATH="${PREFIX}/${JAR_DIR}/${JAR_FILE}:${PREFIX}/${JAR_DIR}/fecImpl.jar:${PREFIX}/${JAR_DIR}/genChkImpl.jar:${PREFIX}/${JAR_DIR}/xercesImpl.jar:${PREFIX}/${JAR_DIR}/xml-apis.jar:${PREFIX}/${JAR_DIR}/xmlParserAPIs.jar:$CLASSPATH"
 
-if [ ! -d ${HOME_DIR} ]
+if [ ! -d ${HOME_DIR}/config ]
 then
-	mkdir -p ${HOME_DIR}
+	mkdir -p ${HOME_DIR}/config
+fi
+
+if [ ! -e ${HOME_DIR}/config/boards.xml.default ]
+then
+	cp ${PREFIX}/${SHARE_DIR}/boards.xml.default ${HOME_DIR}/config/
 fi
 
 cd ${HOME_DIR} &&
-	exec ${JAVAVM} -jar ${PREFIX}/${JAR_DIR}/${JAR_FILE} "${@}"
+	exec ${JAVAVM} -cp $CLASSPATH frost.frost "${@}"
