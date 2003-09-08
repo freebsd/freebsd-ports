1a33c780f70cf8af9cddd7214624594752c1e9f2
edwin <edwin@FreeBSD.org>
2003-09-08 06:58:00 +0000

    new-port: game, train control simulation
    	New port, game, a little simulation of a train dispatching station.
    
    	Can simulate real station with real schedules. Datafiles
    	downloadable from the home page.
    
    PR:		ports/45316
    Submitted by:	Guido Falsi <mad@madpilot.net>

---
+++ games/traindirector/files/patch-track.c
@@ -0,0 +1,11 @@
+--- track.c.orig	Sun Sep  7 23:43:06 2003
++++ track.c	Sun Sep  7 23:43:10 2003
+@@ -20,7 +20,7 @@
+ 
+ #include <stdio.h>
+ #include <string.h>
+-#include <malloc.h>
++#include <stdlib.h>
+ #include "trsim.h"
+ #include "ask.h"
+ 
---
+++ games/traindirector/files/patch-track1.c
@@ -0,0 +1,11 @@
+--- track1.c.orig	Sun Sep  7 23:43:12 2003
++++ track1.c	Sun Sep  7 23:43:16 2003
+@@ -20,7 +20,7 @@
+ 
+ #include <stdio.h>
+ #include <string.h>
+-#include <malloc.h>
++#include <stdlib.h>
+ #include "trsim.h"
+ #include "ask.h"
+ 
