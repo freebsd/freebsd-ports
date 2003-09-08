1622a642b366e1e34f23c36648942934ae421b9e
edwin <edwin@FreeBSD.org>
2003-09-08 09:55:03 +0000

    [patch] graphics/osgedit 0.3.0 -> 0.4.1
    	This port is broken on stable as mentioned in the named PR.
    	This patch fixes this problem by updating the port to the
    	latest version.
    
    PR:		ports/56548
    Submitted by:	Simon Barner <barner@in.tum.de>

---
+++ graphics/osgedit/files/patch-src::editable::entities::edstateset.cc
@@ -0,0 +1,8 @@
+--- src/editable/entities/edstateset.cc.orig	Sat Sep  6 14:05:01 2003
++++ src/editable/entities/edstateset.cc	Sat Sep  6 14:05:19 2003
+@@ -1,3 +1,5 @@
++#include <stdio.h>
++
+ #include <osg/Texture2D>
+ #include <osg/TextureCubeMap>
+ #include <editable/entityfactory.h>
---
+++ graphics/osgedit/files/patch-src::editable::view::gtkedmatrix.cc
@@ -0,0 +1,7 @@
+--- src/editable/view/gtkedmatrix.cc.orig	Sat Sep  6 14:09:46 2003
++++ src/editable/view/gtkedmatrix.cc	Sat Sep  6 14:10:06 2003
+@@ -1,3 +1,4 @@
++#include <stdio.h>
+ #include <iostream>
+ #include <gtk/gtkframe.h>
+ #include <gtk/gtktable.h>
