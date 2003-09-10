05952ca7403a1475b2ba2512065d22cb35fce3f2
Norikatsu Shigemura <nork@FreeBSD.org>
2003-09-10 16:28:36 +0000

    Update to 5.5.7-10. [1] o Removed two (now obsolate) libtool version patches   as ImageMagick now uses internal version system for   them starting from 0:0:0 (5.5.8 will become 0:1:0),   also MAJOR_VERSION/MINOR_VERSION/MICRO_VERSION   variables removed from the source. o Removed two other patches as now they are already   embedded to the source. o Updated plist to reflect separate Q8/Q16 folder   installations based on selection while installing.   Also Makefile modifed to reflect this.
    PR:		ports/55274 [1]
    Submitted by:	Erdener Gonenc <ergonenc@artemis.efes.net> [1]
    Pointed out by:	Rob Lahaye <lahaye@snu.ac.kr>

--- graphics/ImageMagick/files/patch-coders::jp2.c
+++ graphics/ImageMagick/files/patch-coders::jp2.c
@@ -1,8 +1,7 @@
 --- coders/jp2.c.orig	Sat Oct 12 23:41:32 2002
 +++ coders/jp2.c	Wed Jan 22 21:11:31 2003
-@@ -498,6 +498,15 @@
- }
- 
+@@ -639,5 +639,14 @@
+ 
  #if defined(HasJP2)
 +
 +#ifndef JAS_IMAGE_CM_GRAY
