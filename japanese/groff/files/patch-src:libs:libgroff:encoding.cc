569528d01789d264d27a3b54d628c2496703f142
OKAZAKI Tetsurou <okazaki@FreeBSD.org>
2003-09-10 12:31:48 +0000

    Update I18N patch.
    Obtained from:	debian
    
    Update `operating-system' to FreeBSD 4.9.
    Use %%DOCSDIR%% in pkg-plist.

--- japanese/groff/files/patch-src:libs:libgroff:encoding.cc
+++ japanese/groff/files/patch-src:libs:libgroff:encoding.cc
@@ -1,6 +1,6 @@
---- src/libs/libgroff/encoding.cc.bak	Tue Mar  4 20:26:04 2003
-+++ src/libs/libgroff/encoding.cc	Tue Mar  4 21:33:16 2003
-@@ -177,7 +177,8 @@
+--- src/libs/libgroff/encoding.cc.bak	Wed Sep 10 03:45:14 2003
++++ src/libs/libgroff/encoding.cc	Wed Sep 10 11:31:11 2003
+@@ -179,7 +179,8 @@
    inline wchar make_wchar(unsigned char c0, encoding_istream& eis) {
      wchar wc = 0;
      char inbuf[8], outbuf[8];
@@ -10,7 +10,7 @@
      size_t inbytesleft, outbytesleft;
      int i = 0;
      
-@@ -212,7 +213,8 @@
+@@ -214,7 +215,8 @@
  
    inline int put_wchar(wchar wc, encoding_ostream& eos) {
      char inbuf[4], outbuf[4];
@@ -20,13 +20,13 @@
      size_t inbytesleft, outbytesleft;
  
      if (!is_wchar_code(wc)) {
-@@ -377,6 +379,9 @@
+@@ -382,6 +384,9 @@
  #if HAVE_LANGINFO_CODESET
-   locale = nl_langinfo(CODESET);
- #endif
-+  if (strcmp(locale, "ja_JP.EUC") == 0 || strcmp(locale, "ja_JP.eucJP") == 0) {
-+	  locale = "EUC-JP";
+   charset = nl_langinfo(CODESET);
+ #else
++  if (strcmp(locale, "ja_JP.EUC") == 0) {
++    locale = "ja_JP.eucJP";
 +  }
-   select_input_encoding_handler(locale);
-   select_output_encoding_handler(locale);
- #endif
+   charset = strchr(locale, '.');
+   if (charset)
+     ++charset;
