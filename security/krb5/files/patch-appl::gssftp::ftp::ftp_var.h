615d60baa13f416d7abd3ad21ca215ba05e0cbb2
Cy Schubert <cy@FreeBSD.org>
2003-09-10 00:00:42 +0000

    Patch to fix compiles under -STABLE (RELENG_4).
    PR:		56169
    Submitted by:	Sergey Matveychuk <sem@ciam.ru>

--- security/krb5/files/patch-appl::gssftp::ftp::ftp_var.h
+++ security/krb5/files/patch-appl::gssftp::ftp::ftp_var.h
@@ -1,5 +1,5 @@
---- appl/gssftp/ftp/ftp_var.h.orig	Mon Jun 16 15:37:40 2003
-+++ appl/gssftp/ftp/ftp_var.h	Tue Jul 29 22:14:57 2003
+--- appl/gssftp/ftp/ftp_var.h.orig	Tue Jun 17 02:37:40 2003
++++ appl/gssftp/ftp/ftp_var.h	Sat Aug 30 05:30:44 2003
 @@ -33,6 +33,10 @@
   *	@(#)ftp_var.h	5.9 (Berkeley) 6/1/90
   */
@@ -11,3 +11,13 @@
  #ifdef _WIN32
  #include <windows.h>
  #include <winsock2.h>
+@@ -57,9 +61,7 @@
+ typedef void (*sig_t)(int);
+ typedef void sigtype;
+ #else
+-#define sig_t my_sig_t
+ #define sigtype krb5_sigtype
+-typedef sigtype (*sig_t)();
+ #endif
+ 
+ /*
