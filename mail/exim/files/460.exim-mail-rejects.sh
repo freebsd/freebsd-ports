55c1140415cbe3b10e15ca807f7ec5de81f6cb43
Sheldon Hearn <sheldonh@FreeBSD.org>
2003-09-10 13:49:09 +0000

    Bring the Exim port closer to use as an install-time sendmail replacement for FreeBSD:
    * Use an rc.subr start/stop script.
    * Include contributed periodic scripts.
    * Replace $PREFIX with the build-time value of PREFIX.
    * Generate a default configure file if it doesn't exist at install-time.
    * Remove the default configure file if it hasn't been modified at
      deinstall-time.
    * Install additional documentation.
    * Move creation and removal of /var/log/exim to pkg-plist.
    
    Bump PORTREVISION accordingly.
    
    This paves the way for ports/56458, which teaches sysinstall about Exim.
    Bug trhodes about that PR. :-)
    
    PR:		ports/56450
    Submitted by:	Oliver Eikemeier <eikemeier@fillmore-labs.com>

---
+++ mail/exim/files/460.exim-mail-rejects.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+# Check for rejected mail
+# Log lines that end with ' : IGNORE' will not be reported
+# 
+#   contributed by: Oliver Eikemeier <eikemeier@fillmore-labs.com>
+#
+
+# If there is a global system configuration file, suck it in.
+#
+if [ -r /etc/defaults/periodic.conf ]
+then
+    . /etc/defaults/periodic.conf
+    source_periodic_confs
+fi
+
+exim_status_mail_rejects_enable=${exim_status_mail_rejects_enable:-"YES"}
+exim_status_mail_rejects_logs=${exim_status_mail_rejects_logs:-2}
+exim_rejectlog=${exim_rejectlog:-"/var/log/exim/rejectlog"}
+
+case "$exim_status_mail_rejects_enable" in
+    [Yy][Ee][Ss])
+	if [ ! -d `dirname "$exim_rejectlog"` ]
+	then
+	    echo '$exim_status_mail_rejects_enable is set but' \
+		"`dirname "$exim_rejectlog"` doesn't exist"
+	    rc=2
+	elif [ "$exim_status_mail_rejects_logs" -le 0 ]
+	then
+	    echo '$exim_status_mail_rejects_enable is set but' \
+		'$exim_status_mail_rejects_logs is not greater than zero'
+	    rc=2
+	else
+	    echo
+	    echo Checking for rejected mail:
+
+	    start=`date -v-1d '+%Y-%m-%d'`
+	    n=$(($exim_status_mail_rejects_logs - 2))
+	    rc=$({
+		while [ $n -ge 0 ]
+		do
+		    if [ -f "$exim_rejectlog.$n" ]
+		    then
+			cat "$exim_rejectlog.$n"
+		    elif [ -f "$exim_rejectlog.$n.gz" ]
+		    then
+			zcat -fc "$exim_rejectlog.$n.gz"
+		    elif [ -f "$exim_rejectlog.$n.bz2" ]
+		    then
+			bzcat -fc "$exim_rejectlog.$n.bz2"
+		    fi
+		    n=$(($n - 1))
+		done
+		if [ -f "$exim_rejectlog" ]
+		then
+		    cat "$exim_rejectlog"
+		fi
+	    } |
+		grep -e "^$start" | grep -v ' : IGNORE$' | tee /dev/stderr | wc -l)
+	    [ $rc -gt 0 ] && rc=1
+	fi;;
+
+    *)  rc=0;;
+esac
+
+exit $rc
--- mail/exim/files/exim.sh
+++ mail/exim/files/exim.sh
@@ -2,29 +2,63 @@
 #
 # $FreeBSD$
 #
-# Note that 'pidfile' may need to be changed if 'args' is altered; see
-# the description of the 'pid_file_path' Exim configuration option in
-# the Exim Specification.
+
+# PROVIDE: mail
+# REQUIRE: LOGIN
+# KEYWORD: FreeBSD shutdown
+#	we make mail start late, so that things like .forward's are not
+#	processed until the system is fully operational
+
 #
-args='-bd -q30m'
-pidfile='/var/run/exim.pid'
+# Add the following lines to /etc/rc.conf to enable exim:
+#
+#exim_enable="YES"
+#
+# See exim(8) for flags
+#
 
-case "$1" in
-start)
-        [ -x /usr/local/sbin/exim ] && /usr/local/sbin/exim ${args} && \
-	    echo -n ' exim'
-        ;;
-stop)
-        kill `cat ${pidfile}` && echo -n ' exim'
-        ;;
-reload)
-        kill -HUP `cat ${pidfile}` && echo -n ' exim'
-        ;;
-*)
-        echo "Usage: `basename $0` {start|stop|reload}" >&2
-        exit 64
-        ;;
-esac
+. %%RC_SUBR%%
 
-exit 0
+name=exim
+rcvar=`set_rcvar`
 
+command=%%PREFIX%%/sbin/exim
+pidfile=/var/run/exim.pid
+required_dirs=/var/log/exim
+required_files=%%PREFIX%%/etc/exim/configure
+
+start_precmd=start_precmd
+stop_postcmd=stop_postcmd
+
+extra_commands="reload"
+
+start_precmd()
+{
+  case $sendmail_enable in
+  [Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|[Oo][Nn]|1)
+    warn "sendmail_enable should be set to NONE"
+    ;;
+  [Nn][Oo]|[Ff][Aa][Ll][Ss][Ee]|[Oo][Ff][Ff]|0)
+    case $sendmail_submit_enable in
+    [Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|[Oo][Nn]|1)
+      warn "sendmail_submit_enable should be set to NO"
+      ;;
+    esac
+    ;;
+  [Nn][Oo][Nn][Ee])
+    ;;
+  esac
+}
+
+stop_postcmd()
+{
+  rm -f $pidfile
+}
+
+# set defaults
+
+exim_enable=${exim_enable:-"NO"}
+exim_flags=${exim_flags:-"-bd -q30m"}
+
+load_rc_config $name
+run_rc_command "$1"
