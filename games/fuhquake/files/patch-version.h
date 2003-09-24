7f8f10d03356d5cbac60edbe5b54ee1253513fb1
Edwin Groothuis <edwin@FreeBSD.org>
2003-09-24 09:18:43 +0000

    Report our platform as FreeBSD, not Linnex
    	It would be nice to redefine QW_PLATFORM to ``FreeBSD''
    	instead of ``Linux'' (which is default), so to make FuhQuake
    	report real box type it's being run onto.
    
    PR:		ports/56990
    Submitted by:	Alexey Dokuchaev <danfe@regency.nsu.ru>

---
+++ games/fuhquake/files/patch-version.h
@@ -0,0 +1,11 @@
+--- version.h.orig	Thu Sep 18 23:36:30 2003
++++ version.h	Thu Sep 18 23:36:50 2003
+@@ -7,7 +7,7 @@
+ #ifdef _WIN32
+ #define QW_PLATFORM	"Win32"
+ #else 
+-#define QW_PLATFORM	"Linux"
++#define QW_PLATFORM	"FreeBSD"
+ #endif
+ 
+ #ifdef GLQUAKE
