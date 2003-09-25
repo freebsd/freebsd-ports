bb01a3bcad9d4a1bdd9b3db2170778582aa8daba
adamw <adamw@FreeBSD.org>
2003-09-25 06:01:21 +0000

    Add forgotten patch.
    Reminded by:	Mezz

---
+++ graphics/dia/files/patch-po_checktrans.py
@@ -0,0 +1,8 @@
+--- po-checktrans.py.orig	Wed Sep 24 21:32:42 2003
++++ po-checktrans.py	Wed Sep 24 21:33:03 2003
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python
++#!/usr/bin/env python
+ #
+ # This quick hack gives translation statistics (from the core translation
+ # files).
