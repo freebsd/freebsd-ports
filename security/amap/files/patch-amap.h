20e5fce88b0c5da60a211af9f729b41a59624825
krion <krion@FreeBSD.org>
2003-09-25 11:40:36 +0000

    - Update to version 4.3
    PR:		56860
    Submitted by:	maintainer

--- security/amap/files/patch-amap.h
+++ security/amap/files/patch-amap.h
@@ -1,11 +1,11 @@
---- amap.h.orig	Tue Aug  5 21:18:29 2003
-+++ amap.h	Wed Aug 13 11:48:47 2003
-@@ -24,7 +24,7 @@
- #define AMAP_EMAIL     "amap-dev@thc.org"
- #define AMAP_RESOURCE  "www.thc.org"
+--- amap.h.orig	Sun Sep 14 11:25:16 2003
++++ amap.h	Sun Sep 14 11:25:29 2003
+@@ -33,7 +33,7 @@
+  #endif
+ #endif
  
--#define AMAP_APPDEF_PATH	"/usr/local/bin"
-+#define AMAP_APPDEF_PATH	"PREFIX/share/amap"
+-#define AMAP_APPDEF_PATH	AMAP_PREFIX"/bin"
++#define AMAP_APPDEF_PATH	AMAP_PREFIX"/share/amap"
  #define AMAP_BUFSIZE		1024	// standard buffer size
  #define AMAP_BUFSIZE_BIG	65536   // big standard buffer size
- #define AMAP_REGEX_OPTIONS	( PCRE_MULTILINE | PCRE_CASELESS )
+ #define AMAP_REGEX_OPTIONS	( PCRE_MULTILINE | PCRE_CASELESS | PCRE_DOTALL )
