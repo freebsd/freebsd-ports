0a268929fa8de54659b458391b32f7c47bb62e0f
Kirill Ponomarev <krion@FreeBSD.org>
2003-09-25 10:05:05 +0000

    - Update to version 0.71
    PR:		56524
    Submitted by:	Ports Fury

--- devel/kaptain/files/patch-kaptain-tools.cpp
+++ devel/kaptain/files/patch-kaptain-tools.cpp
@@ -1,13 +1,9 @@
---- kaptain/tools.cpp.orig	Wed Aug 14 06:17:39 2002
-+++ kaptain/tools.cpp	Fri Dec 20 18:53:13 2002
-@@ -1,8 +1,10 @@
+--- kaptain/tools.cpp.orig	Wed Mar 12 01:38:12 2003
++++ kaptain/tools.cpp	Wed Sep  3 18:43:40 2003
+@@ -1,5 +1,6 @@
  #include <stdlib.h>
  #include <stdio.h>
 +#include <sys/types.h>
  #include <regex.h>
- #include "tools.h"
- 
-+using namespace std;
- 
- Error::Error()
- {
+ #include <iostream>
+ #include <iterator>
