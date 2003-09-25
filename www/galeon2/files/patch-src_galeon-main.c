8fa78a1278418ed349bd5e85ade24b9e02e22d48
adamw <adamw@FreeBSD.org>
2003-09-25 03:20:12 +0000

    Update to 1.3.9.
    Obtained from:	marcuscom

--- www/galeon2/files/patch-src_galeon-main.c
+++ www/galeon2/files/patch-src_galeon-main.c
@@ -1,11 +1,11 @@
---- src/galeon-main.c.orig	Sun Aug 17 14:59:02 2003
-+++ src/galeon-main.c	Sun Aug 17 15:00:25 2003
-@@ -126,7 +126,7 @@
- 	
+--- src/galeon-main.c.orig	Thu Sep 11 01:11:30 2003
++++ src/galeon-main.c	Thu Sep 11 01:12:31 2003
+@@ -130,7 +130,7 @@
  #ifdef ENABLE_NLS
  	/* Initialize the i18n stuff */
+ 	setlocale(LC_ALL, "");
 -	bindtextdomain(GETTEXT_PACKAGE, DATADIR "/locale");
 +	bindtextdomain(GETTEXT_PACKAGE, GNOMELOCALEDIR);
- 	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
  	textdomain(GETTEXT_PACKAGE);
  #endif
+ 	/* set the application name manually until gnome handles the
