eb50041e2c0b1ff8ff31ac11aab01af29b001748
Hye-Shik Chang <perky@FreeBSD.org>
2003-09-27 17:26:00 +0000

    Update to 1.18

--- print/py-reportlab/files/patch-setup.py
+++ print/py-reportlab/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Mon Dec 23 08:03:38 2002
-+++ setup.py	Mon Mar  3 16:00:37 2003
+--- setup.py.orig	Tue Apr 15 16:52:36 2003
++++ setup.py	Sun Sep 28 02:10:42 2003
 @@ -18,7 +18,7 @@
  
  pjoin = os.path.join
  
 -package_path = pjoin(package_home(distutils.__dict__), 'reportlab')
 +package_path = os.environ['PACKAGE_PATH']
- print package_path
  
  
+ # why oh why don't most setup scripts have a script handler?
