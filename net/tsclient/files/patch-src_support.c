ddf5c7396b0c2c619ca41e5734ca8138708b9bb7
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-09-28 22:29:56 +0000

    Update to 0.124.
    * Fixed more banner problems
    * Fixed bug in compact mode, now reads computer name properly again
    * Fixed bug in VNC code
    * Update translations
    
    PR:		57033
    Submitted by:	maintainer

--- net/tsclient/files/patch-src_support.c
+++
@@ -1,11 +0,0 @@
---- src/support.c.orig	Thu Jul  3 16:20:04 2003
-+++ src/support.c	Fri Jul  4 19:09:54 2003
-@@ -566,7 +566,7 @@
- 
-   } else {
-     // clean up and exit
--    g_free (cmd);
-+    /*g_free (cmd);*/
-     return 1;  
-   }
-   // clean up and exit
