3082facd997db193e1a43cc5fda64b52e9d35f61
marcus <marcus@FreeBSD.org>
2003-09-29 04:58:37 +0000

    Update to 0.91.

--- www/mplayer-plugin/files/patch-Source_mplayerplug-in.c
+++ www/mplayer-plugin/files/patch-Source_mplayerplug-in.c
@@ -1,21 +1,11 @@
---- Source/mplayerplug-in.c.orig	Thu May 22 22:38:28 2003
-+++ Source/mplayerplug-in.c	Mon Jul 21 15:14:24 2003
-@@ -142,9 +142,6 @@
- 		"audio/mpeg3:mp3:MPEG audio;"
- 		"audio/x-mpeg3:mp3:MPEG audio;"
- 		"audio/mp3:mp3:MPEG audio;"
--		"audio/x-pn-realaudio:ram:RealAudio;"
--		"audio/x-pn-realaudio-plugin:rpm:RealAudio;"
--		"audio/x-realaudio:ra:RealAudio;"
- 		"video/fli:fli,flc:FLI animation;"
- 		"video/x-fli:fli,flc:FLI animation;"
- 		"video/x-ms-asf-plugin:asf,asx:Windows Media;"
-@@ -262,7 +262,7 @@
- 	}
+--- Source/mplayerplug-in.c.orig	Wed Sep 10 10:34:15 2003
++++ Source/mplayerplug-in.c	Mon Sep 29 00:43:56 2003
+@@ -67,7 +67,7 @@
+     }
  
- 	if (config == NULL) {
--		config = fopen("/etc/mplayerplug-in.conf","r");
-+		config = fopen("%%PREFIX%%/etc/mplayerplug-in.conf","r");
- 	}
+     if (config == NULL) {
+-	config = fopen("/etc/mplayerplug-in.conf", "r");
++	config = fopen("%%PREFIX%%/etc/mplayerplug-in.conf", "r");
+     }
  
- 	DEBUG = 0;
+     if (config == NULL) {
