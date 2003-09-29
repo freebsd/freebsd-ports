c17a2b21f71549cdd47e3ae65715d8a3b0d5bf74
trevor <trevor@FreeBSD.org>
2003-09-29 11:35:54 +0000

    Update to 0.36-0.

--- audio/pd/files/patch-src_s_freebsd.c
+++ audio/pd/files/patch-src_s_freebsd.c
@@ -1,7 +1,7 @@
 $FreeBSD$
 
---- src/s_freebsd.c.orig	Fri May 23 00:52:34 2003
-+++ src/s_freebsd.c	Fri May 23 01:01:59 2003
+--- src/s_freebsd.c.orig	Sun Sep 28 15:48:40 2003
++++ src/s_freebsd.c	Sun Sep 28 15:48:41 2003
 @@ -32,7 +32,8 @@
   * 1311:forum::für::umläute:2001
   */
@@ -12,18 +12,19 @@ $FreeBSD$
  
  #if (defined(ALSA01) || defined(ALSA99))
  #define ALSA
-@@ -762,8 +762,10 @@
+@@ -765,9 +766,11 @@
  #endif
  
  #ifdef _POSIX_MEMLOCK
 +#if _POSIX_MEMLOCK != -1
      if (mlockall(MCL_FUTURE) != -1) 
      	fprintf(stderr, "memory locking enabled.\n");
-+#endif
  #endif
++#endif
  }
  
-@@ -865,7 +868,8 @@
+ /* ------------ linux-specific command-line flags -------------- */
+@@ -868,7 +871,8 @@
      }
      else
      {
@@ -33,7 +34,7 @@ $FreeBSD$
  	dev->d_bytespersamp = 2;
      }
      param = wantformat;
-@@ -2747,7 +2751,7 @@
+@@ -2750,7 +2754,7 @@
  
    /* use ctrlpanel to change, experiment, channels 1 */
  
