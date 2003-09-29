c24024662e7d76003bd7586cfb49740826f0dd67
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-09-29 17:20:54 +0000

    Update to 0.13.2.

--- deskutils/gtodo/files/patch-src::callback.c
+++ deskutils/gtodo/files/patch-src::callback.c
@@ -1,5 +1,5 @@
---- src/callback.c.orig	Mon Sep 29 01:45:56 2003
-+++ src/callback.c	Mon Sep 29 01:48:27 2003
+--- src/callback.c.orig	Mon Sep 29 10:26:50 2003
++++ src/callback.c	Mon Sep 29 10:29:36 2003
 @@ -26,8 +26,8 @@
  
      if(cur != root) 
@@ -10,7 +10,7 @@
  	gtk_list_store_remove(mw.list, &iter);
  	}
      else{
-@@ -167,8 +167,8 @@
+@@ -166,8 +166,8 @@
  	    if(xmlStrEqual(temp, (const xmlChar *)tm))
   		{
  		g_signal_handler_block(mw.option, shand);
@@ -20,13 +20,13 @@
  		read_categorys();
  		g_signal_handler_unblock(mw.option, shand);
  		cur = NULL;
-@@ -302,8 +302,8 @@
+@@ -301,8 +301,8 @@
      			cur = get_id_node(category, value);
  			if(cur != root) 
  				{
 -				xmlFreeNode(cur);
  				xmlUnlinkNode(cur);
 +				xmlFreeNode(cur);
- 				//gtk_list_store_remove(mw.list, &nextiter);
  				}
-     			else{
+     	    		else{
+ 				  g_print(_("item not found.. that can't be\n"));
