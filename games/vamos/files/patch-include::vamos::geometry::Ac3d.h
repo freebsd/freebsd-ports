3137898f9fff0ebb358661a52c6a192a0887b382
krion <krion@FreeBSD.org>
2003-10-03 15:43:11 +0000

    - Update to version 0.4.1
    PR:		57533
    Submitted by:	Ports Fury

---
+++ games/vamos/files/patch-include::vamos::geometry::Ac3d.h
@@ -0,0 +1,10 @@
+--- include/vamos/geometry/Ac3d.h.orig	Fri Sep 26 11:25:48 2003
++++ include/vamos/geometry/Ac3d.h	Fri Oct  3 18:09:20 2003
+@@ -25,6 +25,7 @@
+ 
+ #include <GL/gl.h>
+ 
++#include <fstream>
+ #include <string>
+ #include <vector>
+ 
