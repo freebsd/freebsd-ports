70996f8f1fe91c16f0d7ee0c539653b03719b209
dinoex <dinoex@FreeBSD.org>
2003-10-03 13:31:26 +0000

    - Upgrade to 20031001 - added RC-ng scripts - Fix build on CURENT PR:		57451 Submitted by:	rob@debank.tv, eikemeier@fillmore-labs.com, dinoex

--- security/clamav-devel/files/clamav-clamd.sh
+++ security/clamav-devel/files/clamav-clamd.sh
@@ -1,18 +1,53 @@
 #!/bin/sh
-PREFIX=%%PREFIX%%
+#
+# $FreeBSD$
+#
 
-case "$1" in
-start)
-	# remove old socket
-	rm -f /tmp/clamd
-	[ -x ${PREFIX}/sbin/clamd ] && ${PREFIX}/sbin/clamd > /dev/null && echo -n ' clamd'
-	;;
-stop)
-	killall 'clamd' && echo -n ' clamd'
-	;;
-*)
-	echo "Usage: ${0##*/} { start | stop }" >&2
-	;;
-esac
+# PROVIDE: clamd
+# REQUIRE: LOGIN
+# BEFORE: mail
+# KEYWORD: FreeBSD shutdown
 
-exit 0
+#
+# Add the following lines to /etc/rc.conf to enable clamd:
+#
+#clamav_clamd_enable="YES"
+#
+# See clamd(8) for flags
+#
+
+. %%RC_SUBR%%
+
+name=clamav_clamd
+rcvar=`set_rcvar`
+
+command=%%PREFIX%%/sbin/clamd
+pidfile=/var/run/clamav/clamd.pid
+required_dirs=%%DATADIR%%
+required_files=%%PREFIX%%/etc/clamav.conf
+
+start_precmd=start_precmd
+
+start_precmd()
+{
+	if [ -S "$clamd_socket" ]; then
+		warn "Stale socket $clamd_socket removed."
+		rm "$clamd_socket"
+	fi
+}
+
+stop_postcmd=stop_postcmd
+
+stop_postcmd()
+{
+  rm -f $pidfile
+}
+
+# set defaults
+
+clamav_clamd_enable=${clamav_clamd_enable:-"NO"}
+clamav_clamd_flags=${clamav_clamd_flags:-""}
+clamav_clamd_socket=${clamav_clamd_socket:-"%%CLAMAV_CLAMD_SOCKET%%"}
+
+load_rc_config $name
+run_rc_command "$1"
