68d84cf6349ad56c0d7d96b7aa5091390115f01d
krion <krion@FreeBSD.org>
2003-10-05 19:59:13 +0000

    - Update to version 2.0.12 - Approved by maintainer
    PR:		56180
    Submitted by:	Dan Larsson <ports@tyfon.net>

--- dns/noip/files/noip.sh
+++ dns/noip/files/noip.sh
@@ -2,14 +2,14 @@
 
 case "$1" in
     start)
-	if [ -x %%PREFIX%%/bin/noip -a -f %%PREFIX%%/etc/no-ip.conf ]; then
+	if [ -x %%PREFIX%%/bin/noip2 -a -f %%PREFIX%%/etc/no-ip2.conf ]; then
             echo -n ' noip';
-            su -m noip -c '%%PREFIX%%/bin/noip' 2> /dev/null > /dev/null
+            su -m noip -c '%%PREFIX%%/bin/noip2' 2> /dev/null > /dev/null
         fi
     ;;
     stop)
         echo -n ' noip';
-	killall noip
+	killall noip2
     ;;
     *)
 	echo "Usage: `basename $0` {start|stop}" >&2
