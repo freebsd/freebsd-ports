37df48703699a300d221f6b485339291cbe1a664
sumikawa <sumikawa@FreeBSD.org>
2003-10-05 13:13:25 +0000

    Make buildable.
    Approved by:	maintainer

---
+++ www/privoxy+ipv6/files/patch-src::addrlist.c
@@ -0,0 +1,13 @@
+--- src/addrlist.c-	Fri Oct  3 12:12:29 2003
++++ src/addrlist.c	Fri Oct  3 12:12:32 2003
+@@ -40,9 +40,8 @@
+  *
+  *********************************************************************/
+ 
+-#include "addrlist.h"
+ #include <sys/types.h>
+-#include <sys/socket.h>
++#include "addrlist.h"
+ #include <netdb.h>
+ #include <string.h>
+ 
