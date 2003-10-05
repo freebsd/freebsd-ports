0870f4e5af0ed9e24a83d1316af16c7f6a27691c
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-10-05 08:29:02 +0000

    * Update to 1.0.1 [1] * Revert maintainer to ports@ (previous maintainer no longer uses GNOME)
    PR:		56306
    Submitted by:	Jens Rehsack <rehsack@liwing.de> [1]

---
+++ x11-fm/gnome-commander/files/patch-src::gnome-cmd-connection.c
@@ -0,0 +1,10 @@
+--- src/gnome-cmd-connection.c.orig	Tue Aug 19 16:07:51 2003
++++ src/gnome-cmd-connection.c	Tue Aug 19 16:08:00 2003
+@@ -17,7 +17,6 @@
+     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */ 
+ #include <config.h>
+-#include <mntent.h>
+ #include "gnome-cmd-includes.h"
+ #include "gnome-cmd-connection-funcs.h"
+ #include "gnome-cmd-dir-funcs.h"
