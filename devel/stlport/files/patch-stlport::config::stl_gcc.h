4f51476e2a85dc5acf309faaaba681dfccd4b682
Alexander Leidinger <netchild@FreeBSD.org>
2003-10-05 08:29:55 +0000

    Fix stlport: ---snip--- The patch is tested (and works) on FreeBSD/i386 -stable and -current with GCC. Although there are no ICC-specific changes I verified that it doesn't break lang/stlport-icc. On FreeBSD/alpha and FreeBSD/sparc64 -current lang/stlport compiles with this patch however the exception handling test fails. On alpha this looks like a GCC-bug, on sparc64 it could be also a bug in FreeBSD however GCC is known to have bugs there. The patch has also a small fix for the Makefile of the port to allow concurrent buils with `make -jX`. The wchar-related part of the patch (which is also needed to fix compilation on -current) is obtained from the STLport CVS repository. ---snip---
    PR:		57267
    Submitted by:	Marius Strobl <marius@alchemy.franken.de>
    Approved by:	maintainer

--- devel/stlport/files/patch-stlport::config::stl_gcc.h
+++ devel/stlport/files/patch-stlport::config::stl_gcc.h
@@ -1,12 +1,41 @@
 --- stlport/config/stl_gcc.h.orig	Thu Jan 10 19:41:58 2002
 +++ stlport/config/stl_gcc.h	Thu Jun  6 23:15:40 2002
-@@ -250,8 +250,13 @@
+@@ -9,8 +9,14 @@
+ 
+ #   define _STLP_NO_MEMBER_TEMPLATE_KEYWORD
+ 
+-# if defined(__FreeBSD__) || defined (__hpux) || defined(__amigaos__)
++# if defined (__hpux) || defined(__amigaos__)
+ #  define _STLP_NO_WCHAR_T
++# elif defined(__FreeBSD__)
++# if (__FreeBSD_cc_version < 500005)
++# define _STLP_NO_WCHAR_T
++# else
++# define _STLP_FREEBSD_HAS_WFUNCS
++# endif /* __FreeBSD_cc_version < 500005 */
+ # endif
+ 
+ # if defined (__sun)
+@@ -77,7 +83,7 @@
+ 
+ # endif
+ 
+-#if defined (__CYGWIN__) || defined (__MINGW32__) || !(defined (_STLP_USE_GLIBC) || defined (__sun)) 
++#if defined (__CYGWIN__) || defined (__MINGW32__) || !(defined (_STLP_USE_GLIBC) || defined (_STLP_FREEBSD_HAS_WFUNCS) || defined (__sun))
+ #ifndef __MINGW32__
+ #   define _STLP_NO_NATIVE_MBSTATE_T      1
+ #endif
+@@ -250,8 +256,17 @@
  
  # if (__GNUC__ >= 3)
  
 +#ifdef __FreeBSD__
-+#  define _STLP_NATIVE_INCLUDE_PATH ../g++
-+#  define _STLP_NATIVE_OLD_STREAMS_INCLUDE_PATH ../g++/backward
++#  if (__FreeBSD_cc_version > 500005)
++#    define _STLP_NATIVE_INCLUDE_PATH ../__GNUC__.__GNUC_MINOR__
++#  else
++#    define _STLP_NATIVE_INCLUDE_PATH ../g++
++#    define _STLP_NATIVE_OLD_STREAMS_INCLUDE_PATH ../g++/backward
++#  endif
 +#else
  #  define _STLP_NATIVE_INCLUDE_PATH ../g++-v3
  #  define _STLP_NATIVE_OLD_STREAMS_INCLUDE_PATH ../g++-v3/backward
