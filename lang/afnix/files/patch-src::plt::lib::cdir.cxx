132a10e20bd7f3df94462aa178d1b0fe7e48bd07
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-10-07 08:51:44 +0000

    upgrade to 0.9.0

---
+++ lang/afnix/files/patch-src::plt::lib::cdir.cxx
@@ -0,0 +1,10 @@
+--- src/plt/lib/cdir.cxx.orig	Tue Oct  7 16:39:01 2003
++++ src/plt/lib/cdir.cxx	Tue Oct  7 16:39:21 2003
+@@ -14,6 +14,7 @@
+ // - copyright (c) 1999-2003 amaury darsch                                   -
+ // ---------------------------------------------------------------------------
+ 
++#include <stdio.h>
+ #include "cdir.hpp"
+ #include "cstr.hpp"
+ #include "cdir.hxx"
