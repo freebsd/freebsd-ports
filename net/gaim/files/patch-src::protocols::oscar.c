86f50bcc4dddf0396e050052eace89ddad69dcb6
marcus <marcus@FreeBSD.org>
2003-10-10 17:33:31 +0000

    * Update to 0.71 [1] * Add optional Mozilla NSS support
    Submitted by:	Matthew Luckie <mjl@luckie.org.nz> [1]

--- net/gaim/files/patch-src::protocols::oscar.c
+++ net/gaim/files/patch-src::protocols::oscar.c
@@ -1,19 +1,19 @@
---- src/protocols/oscar/oscar.c.orig	Thu Oct  2 15:40:55 2003
-+++ src/protocols/oscar/oscar.c	Thu Oct  2 15:59:38 2003
-@@ -5462,11 +5462,11 @@
- 			free(tstr);
+--- src/protocols/oscar/oscar.c.orig	Sat Oct 11 00:10:06 2003
++++ src/protocols/oscar/oscar.c	Sat Oct 11 00:13:51 2003
+@@ -5391,11 +5391,11 @@
+ 		}
  
- 			if (bi->ipaddr) {
--				char *tstr =  g_strdup_printf("%hhd.%hhd.%hhd.%hhd",
--								(bi->ipaddr & 0xff000000) >> 24,
--								(bi->ipaddr & 0x00ff0000) >> 16,
--								(bi->ipaddr & 0x0000ff00) >> 8,
--								(bi->ipaddr & 0x000000ff));
-+				char *tstr =  g_strdup_printf("%hhu.%hhu.%hhu.%hhu",
-+								(char)(bi->ipaddr >> 24),
-+								(char)(bi->ipaddr >> 16),
-+								(char)(bi->ipaddr >> 8),
-+								(char)bi->ipaddr);
- 				tmp = yay;
- 				yay = g_strconcat(tmp, _("<b>IP Address:</b> "), tstr, "\n", NULL);
- 				free(tmp);
+ 		if ((bi != NULL) && (bi->ipaddr)) {
+-			char *tstr =  g_strdup_printf("%hhd.%hhd.%hhd.%hhd",
+-							(bi->ipaddr & 0xff000000) >> 24,
+-							(bi->ipaddr & 0x00ff0000) >> 16,
+-							(bi->ipaddr & 0x0000ff00) >> 8,
+-							(bi->ipaddr & 0x000000ff));
++			char *tstr =  g_strdup_printf("%hhu.%hhu.%hhu.%hhu",
++							(char)(bi->ipaddr >> 24),
++							(char)(bi->ipaddr >> 16),
++							(char)(bi->ipaddr >> 8),
++							(char)bi->ipaddr);
+ 			tmp = ret;
+ 			ret = g_strconcat(tmp, _("<b>IP Address:</b> "), tstr, "\n", NULL);
+ 			g_free(tmp);
