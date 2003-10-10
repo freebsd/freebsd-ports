608baaf7e97d1df68d9c86e0cc26c6cfeef14ff9
marcus <marcus@FreeBSD.org>
2003-10-10 19:14:33 +0000

    Update to 2.8.2.

--- devel/ORBit2/files/patch-src_orb_orb-core_orbit-typelib.c
+++ devel/ORBit2/files/patch-src_orb_orb-core_orbit-typelib.c
@@ -4,8 +4,8 @@
  	paths = g_ptr_array_sized_new (8);
  
  	g_ptr_array_add (paths, g_strdup (ORBIT_TYPELIB_DIR));
-+	add_if_unique(paths, "%%X11BASE%%");
-+	add_if_unique(paths, "%%LOCALBASE%%");
++	add_if_unique(paths, "%%X11BASE%%", TRUE);
++	add_if_unique(paths, "%%LOCALBASE%%", TRUE);
  
  	if ((path = g_getenv ("ORBIT_TYPELIB_PATH"))) {
  		char **strv;
