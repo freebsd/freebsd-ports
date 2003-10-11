4cbead71540b96e0a376da9744b58cf433b9ce77
Kirill Ponomarev <krion@FreeBSD.org>
2003-10-11 14:44:21 +0000

    - Don't install useless *.la
    Submitted by:	ijliao

---
+++ devel/kyra/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Oct  9 11:44:49 2003
++++ ltmain.sh	Thu Oct  9 11:45:05 2003
+@@ -4210,10 +4210,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
