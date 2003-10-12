b27d7e16af5c515b110c439d6df716c135dc07b6
Dirk Meyer <dinoex@FreeBSD.org>
2003-10-12 10:58:50 +0000

    - use DOCSDIR and EXAMPLESDIR

--- mail/noattach/files/noattach.sh
+++ mail/noattach/files/noattach.sh
@@ -8,7 +8,7 @@ then
 fi
 case "$1" in
 start)
-	rm -f /var/run/noattach.pid
+	rm -f /var/run/noattach.pid /var/run/noattach
 	%%PREFIX%%/libexec/noattach -p local:/var/run/noattach &&
 	echo -n ' noattach'
 	;;
