056cdfbe3a134de4f0ca1a61fb3d1b150fd46c7a
krion <krion@FreeBSD.org>
2003-10-14 21:04:37 +0000

    - Fix build on -current

---
+++ editors/joe2/files/patch-vs.c
@@ -0,0 +1,11 @@
+--- vs.c.orig	Tue Oct 14 22:58:32 2003
++++ vs.c	Tue Oct 14 22:58:52 2003
+@@ -16,7 +16,7 @@
+ JOE; see the file COPYING.  If not, write to the Free Software Foundation, 
+ 675 Mass Ave, Cambridge, MA 02139, USA.  */ 
+ 
+-#include <varargs.h>
++#include <stdarg.h>
+ #include "config.h"
+ #include "zstr.h"
+ #include "blocks.h"
