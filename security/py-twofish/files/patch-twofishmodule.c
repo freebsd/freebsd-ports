45d5f592828e267dce7ad5c122e7ed86530faa39
edwin <edwin@FreeBSD.org>
2003-10-15 13:47:11 +0000

    unbreak port by adding a \n\ to a patchfile

---
+++ security/py-twofish/files/patch-twofishmodule.c
@@ -0,0 +1,11 @@
+--- twofishmodule.c.orig	Wed Oct 15 06:46:16 2003
++++ twofishmodule.c	Wed Oct 15 06:46:30 2003
+@@ -489,7 +489,7 @@
+ Functions:\n\
+ new() -- return a new twofish object.\n\
+ \n\
+-Special Objects: 
++Special Objects: \n\
+ \n\
+ TwoFishType -- type object for TwoFish objects. \n";
+ 
