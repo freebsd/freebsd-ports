8d997981d2ff13a8d1fec46fac6553b6a29e1274
krion <krion@FreeBSD.org>
2003-10-15 13:07:42 +0000

    - Fix build on -current
    Submitted by:	kris via bento

---
+++ graphics/qtutils/files/patch-src-usage_qtconv.c
@@ -0,0 +1,62 @@
+--- src/usage_qtconv.c.orig	Wed Oct 15 14:54:23 2003
++++ src/usage_qtconv.c	Wed Oct 15 14:56:10 2003
+@@ -1,36 +1,35 @@
+-char *usage_qtconv = "
++char *usage_qtconv = ""
+ 
+-     _________________________________________________________________
++     "_________________________________________________________________"
+ 
+-   Table of Contents
++   "Table of Contents"
+ 
+-   qtconv [option]...
++   "qtconv [option]..."
+ 
+-   -h, --help
+-          Print a list of options
++   "-h, --help"
++          "Print a list of options"
+ 
+-   -a, --audio=codec
+-          Set target audio codec
++   "-a, --audio=codec"
++          "Set target audio codec"
+ 
+-   -v, --video=codec
+-          Set target video codec
++   "-v, --video=codec"
++          "Set target video codec"
+ 
+-   -i, --input=path
+-          Set the input QuickTime file
++   "-i, --input=path"
++          "Set the input QuickTime file"
+ 
+-   -o, --output=path
+-          Set the output QuickTime file
++   "-o, --output=path"
++          "Set the output QuickTime file"
+ 
+-   -q, --quality=int
+-          Set the quality used for JPEG or MJPA encoding
++   "-q, --quality=int"
++          "Set the quality used for JPEG or MJPA encoding"
+ 
+-   -c, --cpus=int
+-          Set the number of CPUs to use for MJPA encoding
++   "-c, --cpus=int"
++          "Set the number of CPUs to use for MJPA encoding"
+ 
+-References
++"References"
+ 
+-   Visible links
+-   Hidden links:
+-   1. file://localhost/tmp/xmlto.UQmNyB/usage_wrapper_qtconv.proc#id2752075
+-   2. file://localhost/tmp/xmlto.UQmNyB/usage_wrapper_qtconv.proc#id2750574
+-";
++   "Visible links"
++   "Hidden links:"
++   "1. file://localhost/tmp/xmlto.UQmNyB/usage_wrapper_qtconv.proc#id2752075"
++   "2. file://localhost/tmp/xmlto.UQmNyB/usage_wrapper_qtconv.proc#id2750574";
---
+++ graphics/qtutils/files/patch-src-usage_qtsg.c
@@ -0,0 +1,47 @@
+--- src/usage_qtsg.c.orig	Sat Jun  8 12:39:00 2002
++++ src/usage_qtsg.c	Wed Oct 15 14:53:16 2003
+@@ -1,27 +1,26 @@
+-char *usage_qtsg = "
++char *usage_qtsg = ""
+ 
+-     _________________________________________________________________
++    "_________________________________________________________________"
+ 
+-   Table of Contents
++   "Table of Contents"
+ 
+-   qtsg [option]... image...
++   "qtsg [option]... image..."
+ 
+-   -o, --output=path
+-          The path to the QuickTime output
++   "-o, --output=path"
++          "The path to the QuickTime output"
+ 
+-   -a, --audio=path
+-          The path to the Ogg Vorbis encoded soundtrack
++   "-a, --audio=path"
++          "The path to the Ogg Vorbis encoded soundtrack"
+ 
+-   -c, --acodec=codec
+-          Audio codec to use in output
++   "-c, --acodec=codec"
++          "Audio codec to use in output"
+ 
+-   -r, --rate=rate
+-          Frame rate of output video
++   "-r, --rate=rate"
++          "Frame rate of output video"
+ 
+-References
++"References"
+ 
+-   Visible links
+-   Hidden links:
+-   1. file://localhost/tmp/xmlto.HiIPRu/usage_wrapper_qtsg.proc#id2752075
+-   2. file://localhost/tmp/xmlto.HiIPRu/usage_wrapper_qtsg.proc#id2750577
+-";
++   "Visible links"
++   "Hidden links:"
++   "1. file://localhost/tmp/xmlto.HiIPRu/usage_wrapper_qtsg.proc#id2752075"
++   "2. file://localhost/tmp/xmlto.HiIPRu/usage_wrapper_qtsg.proc#id2750577";
