bbd73ace1fdd533678d64a562613abc3a395389a
Kirill Ponomarev <krion@FreeBSD.org>
2003-10-15 13:07:42 +0000

    - Fix build on -current
    Submitted by:	kris via bento

---
+++ graphics/qtutils/files/patch-src-usage_qtsplit.c
@@ -0,0 +1,42 @@
+--- src/usage_qtsplit.c.orig	Wed Oct 15 14:57:09 2003
++++ src/usage_qtsplit.c	Wed Oct 15 14:58:40 2003
+@@ -1,24 +1,23 @@
+-char *usage_qtsplit = "
++char *usage_qtsplit = ""
+ 
+-     _________________________________________________________________
++     "_________________________________________________________________"
+ 
+-   Table of Contents
++   "Table of Contents"
+ 
+-   qtsg [option]...
++   "qtsg [option]..."
+ 
+-   -i, --input=path
+-          The path to the QuickTime input
++   "-i, --input=path"
++          "The path to the QuickTime input"
+ 
+-   -o, --output=path
+-          The path to the QuickTime output (should contain %d)
++   "-o, --output=path"
++          "The path to the QuickTime output (should contain %d)"
+ 
+-   -s, --size=int
+-          The maximum size of the split files
++   "-s, --size=int"
++          "The maximum size of the split files"
+ 
+-References
++"References"
+ 
+-   Visible links
+-   Hidden links:
+-   1. file://localhost/tmp/xmlto.Mjwffz/usage_wrapper_qtsplit.proc#id2754184
+-   2. file://localhost/tmp/xmlto.Mjwffz/usage_wrapper_qtsplit.proc#id2750586
+-";
++   "Visible links"
++   "Hidden links:"
++   "1. file://localhost/tmp/xmlto.Mjwffz/usage_wrapper_qtsplit.proc#id2754184"
++   "2. file://localhost/tmp/xmlto.Mjwffz/usage_wrapper_qtsplit.proc#id2750586";
