d4626f9b85370a80e3da9c9d6a8679914df0acc3
Edwin Groothuis <edwin@FreeBSD.org>
2003-10-17 12:41:12 +0000

    Upgrade to the latests isakmpd version.

--- security/isakmpd/files/patch-conf.c
+++
@@ -1,9 +0,0 @@
---- conf.c.orig	Mon Nov 18 06:58:04 2002
-+++ conf.c	Sat Feb  8 11:39:43 2003
-@@ -254,5 +254,5 @@
-       if (!section)
- 	{
--	  log_print ("conf_parse_line: %d: malloc (%d) failed", ln, i);
-+	  log_print ("conf_parse_line: %d: malloc (%ld) failed", ln, (long) i);
- 	  return;
- 	}
