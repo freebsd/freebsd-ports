7483b6f2298ab537631f9e825e7286a36d83d059
ijliao <ijliao@FreeBSD.org>
2003-10-17 17:01:02 +0000

    add linkchecker 1.9.3 Check HTML documents for broken links

---
+++ www/linkchecker/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	Sat Oct 18 00:44:51 2003
++++ setup.py	Sat Oct 18 00:45:09 2003
+@@ -197,7 +197,7 @@
+               'lconline/index.html', 'lconline/lc_cgi.html.en',
+               'lconline/lc_cgi.html.de', 'lconline/check.js',
+               'lc.cgi','lc.fcgi','lc.sz_fcgi','linkchecker.bat']),
+-         ('share/man/man1', ['linkchecker.1']),
++         ('man/man1', ['linkchecker.1']),
+       ],
+       classifiers = [
+         'Topic :: Internet :: WWW/HTTP :: Site Management :: Link Checking',
