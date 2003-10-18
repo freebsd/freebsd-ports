9185da2fbae6ee46a2894dbda5d581ee36213142
Kris Kennaway <kris@FreeBSD.org>
2003-10-18 22:09:25 +0000

    Remove the chimera2 port, which has not been in active development for many years, never made it out of alpha status and just doesn't appear to work with the modern WWW (crashed on the first page I tried it on).
    Submitted by:	maintainer

--- www/chimera2/files/protopng.h
+++
@@ -1,3 +0,0 @@
-/*  Eliminate a couple of errors from image/image.c for missing prototype */
-#include "image_format.h"
-void pngInit(void (*lineProc)(void *, int, int), void *closure, struct ifs_vector *ifsv);
