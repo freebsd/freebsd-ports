7feccd9870a48694a0a118f44e50bf07fc64d81f
Trevor Johnson <trevor@FreeBSD.org>
2003-10-18 21:45:26 +0000

    Update to 0.7 with patch-config_preprocess.pl updated by obraun. De-support GTK+ 1.2 (requested by marcus).  Trim COMMENT.

--- www/mozilla-firebird/files/patch-nsprpub-pr-src-pthreads-ptio.c
+++ www/mozilla-firebird/files/patch-nsprpub-pr-src-pthreads-ptio.c
@@ -1,24 +1,26 @@
---- nsprpub/pr/src/pthreads/ptio.c.orig	Tue Jul  8 23:37:46 2003
-+++ nsprpub/pr/src/pthreads/ptio.c	Wed Jul 23 00:53:15 2003
-@@ -3370,7 +3370,7 @@
- PR_EXTERN(PRStatus) _pr_push_ipv6toipv4_layer(PRFileDesc *fd);
- #if defined(_PR_INET6_PROBE)
- PR_EXTERN(PRBool) _pr_ipv6_is_present;
+$FreeBSD$
+
+--- nsprpub/pr/src/pthreads/ptio.c.orig	Mon Jul 14 21:59:47 2003
++++ nsprpub/pr/src/pthreads/ptio.c	Fri Oct 17 05:25:43 2003
+@@ -189,7 +189,7 @@
+ #endif
+ #endif
+ 
 -#ifdef DARWIN
 +#if defined(DARWIN) || defined(FREEBSD)
  static PRBool _pr_ipv6_v6only_on_by_default;
- #endif
- PR_IMPLEMENT(PRBool) _pr_test_ipv6_socket()
-@@ -3385,7 +3385,7 @@
-      */
-     osfd = socket(AF_INET6, SOCK_STREAM, 0);
-     if (osfd != -1) {
+ /* The IPV6_V6ONLY socket option is not defined on Mac OS X 10.1. */
+ #ifndef IPV6_V6ONLY
+@@ -1154,7 +1154,7 @@
+     _pr_stderr = pt_SetMethods(2, PR_DESC_FILE, PR_FALSE, PR_TRUE);
+     PR_ASSERT(_pr_stdin && _pr_stdout && _pr_stderr);
+ 
 -#ifdef DARWIN
 +#if defined(DARWIN) || defined(FREEBSD)
-         /* In Mac OS X v10.3 Panther Beta the IPV6_V6ONLY socket option
-          * is turned on by default, contrary to what RFC 3493, Section
-          * 5.3 says.  So we have to turn it off.  Find out whether we
-@@ -3452,7 +3452,7 @@
+     /* In Mac OS X v10.3 Panther Beta the IPV6_V6ONLY socket option
+      * is turned on by default, contrary to what RFC 3493, Section
+      * 5.3 says.  So we have to turn it off.  Find out whether we
+@@ -3462,7 +3462,7 @@
      if (osfd == -1) pt_MapError(_PR_MD_MAP_SOCKET_ERROR, errno);
      else
      {
