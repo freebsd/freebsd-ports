0a115a7a35cacc92880343c038e4ebba2e1d545e
krion <krion@FreeBSD.org>
2003-10-19 17:56:28 +0000

    Add briquolo 0.4.1, breakout clone with an OpenGL 3D representation.

---
+++ games/briquolo/files/patch-src-MOGL-MOGL_GenerateurParticule.h
@@ -0,0 +1,10 @@
+--- src/MOGL/MOGL_GenerateurParticule.h.orig	Sun Oct 19 19:40:24 2003
++++ src/MOGL/MOGL_GenerateurParticule.h	Sun Oct 19 19:40:54 2003
+@@ -22,6 +22,7 @@
+ #ifndef MOGL_GENERATEURPARTICULE
+ #define MOGL_GENERATEURPARTICULE
+ 
++#include <sys/types.h>
+ #include <list>
+ #include <sys/timeb.h>
+ 
