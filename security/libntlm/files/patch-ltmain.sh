753a066a45b2aeb0afe94e647ac56f3272821d9a
ijliao <ijliao@FreeBSD.org>
2003-10-19 10:07:29 +0000

    add libntlm 0.3.2 A library that implement Microsoft's NTLM authentication

---
+++ security/libntlm/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Sun Oct 19 17:38:52 2003
++++ ltmain.sh	Sun Oct 19 17:39:29 2003
+@@ -4325,10 +4325,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
