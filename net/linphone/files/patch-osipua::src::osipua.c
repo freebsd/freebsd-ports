a6b7f4005b9aeed9d62a90106532a29c68fb4212
edwin <edwin@FreeBSD.org>
2003-10-20 03:09:19 +0000

    Update port: net/linphone to 0.12.0 (fix ports/55665)
    	- Update to version 0.12.0
    
    PR:		ports/55872
    Submitted by:	KATO Tsuguru <tkato@prontomail.com>

--- net/linphone/files/patch-osipua::src::osipua.c
+++ net/linphone/files/patch-osipua::src::osipua.c
@@ -1,15 +1,15 @@
 
 $FreeBSD$
 
---- osipua/src/osipua.c	2003/06/13 12:25:43	1.1
-+++ osipua/src/osipua.c	2003/06/13 12:58:48
-@@ -595,8 +595,7 @@
- 		        if (ipaddr != NULL)
+--- osipua/src/osipua.c.orig	Sat Aug 16 01:30:51 2003
++++ osipua/src/osipua.c	Sat Aug 23 00:07:02 2003
+@@ -591,8 +591,7 @@
+ 			if (ipaddr != NULL)
  			{
- 			        osip_trace(OSIP_INFO1,("osip_ua_find 1: %s <> %s \n",ua->ua_ipaddr,ipaddr));
+ 				osip_trace(OSIP_INFO1,("osip_ua_find 1: %s <> %s \n",ua->ua_ipaddr,ipaddr));
 -				if ((strcasecmp(ua->ua_ipaddr,ipaddr)==0) || (strcasecmp("127.0.0.1",ipaddr)==0) ||
--				    IN6_IS_ADDR_LOOPBACK(res->ai_addr))
+-				   	IN6_IS_ADDR_LOOPBACK(res->ai_addr))
 +				if ((strcasecmp(ua->ua_ipaddr,ipaddr)==0) || (strcasecmp("127.0.0.1",ipaddr)==0))
  				{
- 				      ret_ua=ua;       /* ip addresses match */
+ 				   	 ret_ua=ua;       /* ip addresses match */
  				}
