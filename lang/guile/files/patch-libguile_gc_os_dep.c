bdec6bae5a3cdbf5ba94aa7dceb37106de872c85
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-10-20 05:32:13 +0000

    Fix build and execution of guile on ia64:
    *  Use continuation->backing_store to determine if we return for the first
       time from getcontext(). This avoids having to depend on non-POSIX semantics
       of the Linux implementation.
    *  Avoid using __libc_ia64_register_backing_store_base by hardcoding the base
       of the backing store. The kernel is changed to put the backing store at
       this address. It's not entirely clean, but I want to avoid adding all sorts
       of variables in our libc for this for now.
    *  Fix a genuine bug in the ia64 case: the ia64-specific fields are not
       initialized. On FreeBSD this means they have garbage (= 0xd0). Call
       memset() after allocation to make sure the contregs structure is properly
       cleared.  Since this may affect other architectures, this patch will be
       ia64 only.
    
    Submitted by:	marcel

--- lang/guile/files/patch-libguile_gc_os_dep.c
+++ lang/guile/files/patch-libguile_gc_os_dep.c
@@ -1,5 +1,5 @@
---- libguile/gc_os_dep.c.orig	Wed Apr 16 13:16:21 2003
-+++ libguile/gc_os_dep.c	Thu May 22 20:41:37 2003
+--- libguile/gc_os_dep.c.orig	Mon Oct 20 01:22:19 2003
++++ libguile/gc_os_dep.c	Mon Oct 20 01:22:03 2003
 @@ -93,6 +93,14 @@
  #    define OPENBSD
  #    define mach_type_known
@@ -15,11 +15,16 @@
  # if defined(__NetBSD__) && defined(__powerpc__)
  #    define POWERPC
  #    define NETBSD
-@@ -225,7 +233,11 @@
+@@ -225,7 +233,16 @@
  #    define ARM32
  #    define mach_type_known
  # endif
 -# if defined(__alpha) || defined(__alpha__)
++# if defined(__FreeBSD__) && defined(__ia64__)
++#    define FREEBSD
++#    define IA64
++#    define mach_type_known
++# endif
 +# if defined(__FreeBSD__) && defined(__alpha__)
 +#   define FREEBSD
 +#   define ALPHA
@@ -28,7 +33,7 @@
  #   define ALPHA
  #   if !defined(LINUX)
  #     define OSF1	/* a.k.a Digital Unix */
-@@ -1468,7 +1480,7 @@
+@@ -1468,7 +1485,7 @@
  #   include <setjmp.h>
  #endif
  
