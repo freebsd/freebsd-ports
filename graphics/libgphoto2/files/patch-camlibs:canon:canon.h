74fb31c2493cb6d778a7b8da6cab9cce4f492ee1
andreas <andreas@FreeBSD.org>
2003-10-20 17:49:40 +0000

    Fix to support Canon PowerShot G5
    Situation:
    - The PTP2 driver doesn't work for G5
    - The G3 driver can't be used a G5
    - So the canon native driver has been tought the G5's id etc ...
    
    - Bumped port revision, since the G5 PTP2 support was in
      test status and the G5 canon native driver support
      - fixes G5 PTP2 flaws and
      - is kind of new functionality
    
    These patches have been sent to the gphoto devel team and
    have been included into CVS today. Since yestarday 2.1.3
    has been releases this fix will show up in later versions of
    libgphoto.
    
    Note 1: when using gphoto --auto-detect unluckily the buggy PTP2 driver
    will be autodetected. You then have to choose manually the correct
    driver. Easiest way is to use digikam->Setup and to Choose driver called
    "Canon PowerShot G5 (normal mode)".
    
    Note 2: If you want to use digikam as non-root user, you have to tweak
    permissions of /dev/ugenX and /dev/ugenX.{1,2,3}
    
    I succeded by using the following commands which I put into /etc/rc.local,
    thanks to Poul-Henning Kamp:
    /sbin/devfs ruleset 10
    /sbin/devfs rule applyset
    /sbin/devfs rule add path ugen1* mode 666
    /sbin/devfs rule show
    
    A working solution for /etc/devfs.rules I didn't find yet.
    
    Approved by:	 portmgr@freebsd.org (Joe)

---
+++ graphics/libgphoto2/files/patch-camlibs:canon:canon.h
@@ -0,0 +1,18 @@
+--- camlibs/canon/canon.h.orig	Sun Aug 10 20:51:13 2003
++++ camlibs/canon/canon.h	Mon Oct 20 10:17:51 2003
+@@ -86,6 +86,7 @@
+  * @CANON_PS_S330: Digital IXUS 330
+  * @CANON_PS_S45: PowerShot S45
+  * @CANON_PS_G3: PowerShot G3
++ * @CANON_PS_G5: PowerShot G5
+  * @CANON_PS_S230: PowerShot S230, Digital IXUS v3
+  * @CANON_PS_A60: PowerShot A60
+  * @CANON_EOS_10D: EOS 10D
+@@ -123,6 +124,7 @@
+ 	CANON_PS_A300,
+ 	CANON_PS_S45,
+ 	CANON_PS_G3,
++	CANON_PS_G5,
+ 	CANON_PS_S50,
+ 	CANON_PS_S230,
+ 	CANON_EOS_10D,
