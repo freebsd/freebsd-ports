32fabfe90f665b95996e000683011077912986ec
osa <osa@FreeBSD.org>
2003-10-21 09:17:21 +0000

    Update to 0.6.1
    Submitted by:	Sergei Kolobov <sergei@kolobov.com> (maintainer)
    PR:		58323

--- net/ipsvd/files/patch-ipsvd_scan.c
+++
@@ -1,7 +0,0 @@
---- src/ipsvd_scan.c.orig       Wed Oct  1 00:58:34 2003
-+++ src/ipsvd_scan.c    Wed Oct  1 00:58:12 2003
-@@ -1,3 +1,4 @@
-+#include <sys/types.h>
- #include <netdb.h>
- #include <netinet/in.h>
- #include "scan.h"
