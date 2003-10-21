6b03e8627ddfd88f6db538aa9d001c1f04a3ef3c
krion <krion@FreeBSD.org>
2003-10-21 14:57:32 +0000

    - Fix build on -current, unbreak this port - Use DOCSDIR
    PR:		58114
    Submitted by:	maintainer

---
+++ games/jfk/files/patch-src-server-person_s.cc
@@ -0,0 +1,8 @@
+--- src/server/person_s.cc.orig	Tue Oct 21 16:50:02 2003
++++ src/server/person_s.cc	Tue Oct 21 16:50:15 2003
+@@ -1,4 +1,5 @@
+ #include <algorithm>
++#include <cassert>
+ #include <cstdlib>
+ #include <sstream>
+ #include <string>
