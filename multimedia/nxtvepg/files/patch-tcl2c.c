7d095337c48540cdd5275b9138981182fc68f05f
foxfair <foxfair@FreeBSD.org>
2003-10-22 03:06:21 +0000

    PR: Submitted by:	maintainer via ports@ list Reviewed by: Approved by: Obtained from: MFC after: Upgrade nxtvepg to 2.6.0, contains new features since last version:
    o Implemented reminders
    o Added weekday scale to the right of the programme list
    o Added new programme list attribute type "weekday color"
    o Added an expire time filter to the filter menu
    
    Some more feature improvements too, please refer to:
    
    	http://nxtvepg.sourceforge.net/changelog.html

--- multimedia/nxtvepg/files/patch-tcl2c.c
+++ multimedia/nxtvepg/files/patch-tcl2c.c
@@ -1,19 +1,19 @@
---- tcl2c.c.orig	Sun Nov  3 04:15:11 2002
-+++ tcl2c.c	Sun Oct  5 18:45:58 2003
-@@ -43,6 +43,7 @@
- 
+--- tcl2c.c.orig	Tue Sep 23 21:49:35 2003
++++ tcl2c.c	Tue Oct 21 13:14:35 2003
+@@ -50,6 +50,7 @@
  #include <stdlib.h>
+ #include <string.h>
  #include <stdio.h>
 +#include <time.h>
  #include <errno.h>
- 
- #define FALSE 0
-@@ -123,6 +124,8 @@
+ #include <time.h>
+ #include <ctype.h>
+@@ -429,6 +430,8 @@
      scriptName = malloc(fileNameLen + 1);
-     strncpy(outNameC, argv[1], fileNameLen - 4);
-     strncpy(outNameH, argv[1], fileNameLen - 4);
+     strncpy(outNameC, inFileName, fileNameLen - 4);
+     strncpy(outNameH, inFileName, fileNameLen - 4);
 +    outNameC[fileNameLen-4]=0;
 +    outNameH[fileNameLen-4]=0;
-     baseName = (char *)strrchr(argv[1], '/');
+     baseName = (char *)strrchr(inFileName, '/');
      if (baseName != NULL)
      {
