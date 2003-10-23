f7de8e7808492ab172896bd8c622dcba41927f59
peter <peter@FreeBSD.org>
2003-10-23 02:29:44 +0000

    Unbreak for gcc-3.3 where #include <varargs.h> is a fatal error.  varargs wasn't even used here....

---
+++ x11/xloadimage/files/patch-rlelib.c
@@ -0,0 +1,10 @@
+--- rlelib.c.base	Wed Oct 22 17:20:09 2003
++++ rlelib.c	Wed Oct 22 17:18:30 2003
+@@ -12,7 +12,6 @@
+ 
+ #include <stdio.h>
+ #include <math.h>
+-#include <varargs.h>
+ #include <ctype.h>
+ 
+ #include "image.h"		/* need ZFILE definition */
