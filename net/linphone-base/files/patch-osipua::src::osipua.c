e4076a018eb0ba76180365cd455470088ac8048e
Edwin Groothuis <edwin@FreeBSD.org>
2003-10-23 03:29:23 +0000

    Upgrade to version 0.12.0. This is a follow up of the upgrade and backout of the commit last tuesday.
    I have disabled IPv6 support since it causes linphone not to work
    properly. Informed author about it.

--- net/linphone-base/files/patch-osipua::src::osipua.c
+++ net/linphone-base/files/patch-osipua::src::osipua.c
@@ -1,15 +1,21 @@
-
-$FreeBSD$
-
---- osipua/src/osipua.c	2003/06/13 12:25:43	1.1
-+++ osipua/src/osipua.c	2003/06/13 12:58:48
-@@ -595,8 +595,7 @@
- 		        if (ipaddr != NULL)
+--- osipua/src/osipua.c.orig	Sat Aug 16 02:30:51 2003
++++ osipua/src/osipua.c	Thu Oct 23 12:56:06 2003
+@@ -591,8 +591,7 @@
+ 			if (ipaddr != NULL)
  			{
- 			        osip_trace(OSIP_INFO1,("osip_ua_find 1: %s <> %s \n",ua->ua_ipaddr,ipaddr));
+ 				osip_trace(OSIP_INFO1,("osip_ua_find 1: %s <> %s \n",ua->ua_ipaddr,ipaddr));
 -				if ((strcasecmp(ua->ua_ipaddr,ipaddr)==0) || (strcasecmp("127.0.0.1",ipaddr)==0) ||
--				    IN6_IS_ADDR_LOOPBACK(res->ai_addr))
+-				   	IN6_IS_ADDR_LOOPBACK(res->ai_addr))
 +				if ((strcasecmp(ua->ua_ipaddr,ipaddr)==0) || (strcasecmp("127.0.0.1",ipaddr)==0))
  				{
- 				      ret_ua=ua;       /* ip addresses match */
+ 				   	 ret_ua=ua;       /* ip addresses match */
  				}
+@@ -600,7 +599,7 @@
+ 			else ret_ua=ua;	/* we accept uncheckable host names... */
+ 			ret_ua=ua;
+ #else
+-			if (ip4addr!=NULL)
++			if (ipaddr!=NULL)
+ 			{
+ 				osip_trace(OSIP_INFO1,("osip_ua_find 1: %s <> %s \n",ua->ua_ip4addr,ipaddr));
+ 				if ((strcasecmp(ua->ua_ip4addr,ipaddr)==0) || (strcasecmp("127.0.0.1",ipaddr)==0))
--- net/linphone/files/patch-coreapi::misc.c
+++ net/linphone/files/patch-coreapi::misc.c
@@ -1,9 +1,9 @@
 
-	
+$FreeBSD$
 
---- coreapi/misc.c.orig	Wed Mar 12 09:52:05 2003
-+++ coreapi/misc.c	Wed Mar 12 09:52:39 2003
-@@ -87,7 +87,7 @@
+--- coreapi/misc.c.orig	Sat Aug  9 00:10:47 2003
++++ coreapi/misc.c	Sat Aug 23 00:08:50 2003
+@@ -93,7 +93,7 @@
  	pid_t pid;
  	
  	/* try to kill all artsd*/
@@ -12,9 +12,9 @@
  	out=popen(command,"r");
  	if (out!=NULL)
  	{
-@@ -102,7 +102,7 @@
- 	}
+@@ -123,7 +123,7 @@
  	g_free(command);
+ 	
  	/* do the same with esd*/
 -	command=g_strdup_printf("ps -u %s |grep esd",getenv("LOGNAME"));
 +	command=g_strdup_printf("ps -c %s |grep esd",getenv("LOGNAME"));
