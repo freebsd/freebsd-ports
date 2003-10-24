34d375cd090963ea945db8bcf9c3e1586ceb4648
krion <krion@FreeBSD.org>
2003-10-24 20:08:06 +0000

    - Update to version 0.1.15
    PR:		58382
    Submitted by:	Ports Fury

---
+++ graphics/vertex/files/patch-messages.h
@@ -0,0 +1,11 @@
+--- vertex/messages.h.orig	Wed Oct  1 06:44:17 2003
++++ vertex/messages.h	Tue Oct 21 23:29:45 2003
+@@ -22,7 +22,7 @@
+ \n\
+     [file] is the V3D model file to load on startup.\n\
+ \n\
+-    [options] can be any of the following:
++    [options] can be any of the following:\n\
+ \n\
+         --rcfile <file>         Load configuration from <file>.\n\
+         --config_file <file>    Same as --rcfile.\n\
