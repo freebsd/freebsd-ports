0ee244bb6013f0b34cf321d1a22df378bcf4636c
ijliao <ijliao@FreeBSD.org>
2003-10-24 02:01:39 +0000

    add jakelib2 2.0.0s A cross platform C++ class library with a Java-like API

---
+++ devel/jakelib2/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Oct 24 09:34:59 2003
++++ ltmain.sh	Fri Oct 24 09:35:12 2003
+@@ -4175,10 +4175,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
