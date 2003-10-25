5bc48ebfaaaefb6aa60c926f1669b8a5a0df8cf9
Edwin Groothuis <edwin@FreeBSD.org>
2003-10-25 12:06:30 +0000

    upgrade to version 1.0
    PR:		ports/58418
    Submitted by:	Volker Stolz <stolz@i2.informatik.rwth-aachen.de>

--- net/freenet6/files/patch-src-tspc.c
+++ net/freenet6/files/patch-src-tspc.c
@@ -1,21 +1,11 @@
---- src/tspc.c.orig	Tue Sep 11 23:41:05 2001
-+++ src/tspc.c	Thu Oct 11 13:27:31 2001
-@@ -178,7 +178,7 @@
+--- src/tspc.c.orig	Wed Sep 10 07:44:23 2003
++++ src/tspc.c	Thu Oct 23 11:13:55 2003
+@@ -205,7 +205,7 @@
  int Initialise(int argc, char *argv[], tConf *Conf)
  {
    tConf CmdLine;
 -  char *Templ = "template";
 +  char *Templ = "bin";
-   extern int ReadConfigFile(char *File, tConf *Conf);
  
    Conf->tsp_dir      = NULL;
-@@ -554,7 +554,7 @@
-   SetEnv("TSP_VERBOSE",           buf, 1);
-   SetEnv("TSP_HOME_DIR",          TspHomeDir, 1);
- 
--  snprintf(buf, sizeof buf, "%s%s%c%s.%s", ScriptInterpretor, ScriptDir, DirSeparator, Conf.template, ScriptExtension);
-+  snprintf(buf, sizeof buf, "%s%s%ctspc-%s.%s", ScriptInterpretor, ScriptDir, DirSeparator, Conf.template, ScriptExtension);
- 
-   Display(2, ELInfo, "SetUpInterface", "Executing configuration script.\n");
-   Display(2, ELInfo, "SetUpInterface", buf);
-
+   Conf->server       = "";
