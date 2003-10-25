a72a08e3e95b48139773c91ba2615ad5325c9c1e
knu <knu@FreeBSD.org>
2003-10-25 10:16:13 +0000

    Update to 0.5.1.
    Unmark BROKEN for 5.x.
    
    PR:		ports/58110
    Submitted by:	Ying-Chieh Chen <yinjieh@csie.nctu.edu.tw> (MAINTAINER)

--- x11-toolkits/gtkmathview/files/patch-src::T1_FontManager.cc
+++ x11-toolkits/gtkmathview/files/patch-src::T1_FontManager.cc
@@ -1,11 +1,13 @@
---- src/T1_FontManager.cc.orig	Mon Jul 14 08:30:07 2003
-+++ src/T1_FontManager.cc	Mon Jul 14 08:30:33 2003
-@@ -77,7 +77,7 @@
+--- src/T1_FontManager.cc.orig	Thu Jul 17 18:18:13 2003
++++ src/T1_FontManager.cc	Thu Oct 16 15:09:23 2003
+@@ -77,8 +77,8 @@
  int
  T1_FontManager::SearchT1FontId(const char* fileName) const
  {
+-  //int n = T1_GetNoFonts(); // version 5.0 of t1lib
 -  int n = T1_Get_no_fonts();
-+  int n = T1_GetNoFonts();
++  int n = T1_GetNoFonts(); // version 5.0 of t1lib
++  //int n = T1_Get_no_fonts();
    int i;
    for (i = 0; i < n && strcmp(fileName, T1_GetFontFileName(i)); i++) ;
  
