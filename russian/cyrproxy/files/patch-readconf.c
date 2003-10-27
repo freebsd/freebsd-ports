fb83bcb0cae025ad2be2e28f33ec0c8f0c3ee6f2
fjoe <fjoe@FreeBSD.org>
2003-10-27 21:01:34 +0000

    Fix build with gcc 3.3.1. Bump PORTREVISION.

--- russian/cyrproxy/files/patch-readconf.c
+++ russian/cyrproxy/files/patch-readconf.c
@@ -1,11 +1,11 @@
---- readconf.c.orig	Wed Sep  3 18:51:28 2003
-+++ readconf.c	Wed Sep  3 18:51:40 2003
+--- readconf.c.orig	Sat May  3 18:57:22 1997
++++ readconf.c	Tue Oct 28 02:46:38 2003
 @@ -648,7 +648,7 @@
  	  retval=NULL;							\
  	  while(list!=NULL)						\
  	    {								\
 -	      if(list->##fieldn == compval)				\
-+	      if((list->##fieldn) == compval)				\
++	      if((list->fieldn) == compval)				\
  		retval=list;						\
  	      list=list->chain;						\
  	    }								\
