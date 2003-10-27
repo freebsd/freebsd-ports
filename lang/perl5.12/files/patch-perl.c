1aee5817e8ee565d8efb60984a85c74ae48d025a
Anton Berezin <tobez@FreeBSD.org>
2003-10-27 19:48:03 +0000

    Update to 5.8.1.
    Also:
    
    Make suidperl optional (ENABLE_SUIDPERL knob).  Switch to perlmalloc by
    default, unless threaded perl is built, to improve performance.
    Modernize pkg-plist (switch to SITE_PERL where possible).  Update WWW.
    
    Many thanks to foxfair who prepared most of this update.

--- lang/perl5.12/files/patch-perl.c
+++ lang/perl5.12/files/patch-perl.c
@@ -1,58 +1,60 @@
 $FreeBSD$
 
---- perl.c.orig	Sun Jun  2 12:29:01 2002
-+++ perl.c	Sun Jun  2 12:30:14 2002
-@@ -3407,6 +3407,26 @@
- #endif /* VMS */
-     }
- 
+--- perl.c.orig	Mon Oct 27 20:32:18 2003
++++ perl.c	Mon Oct 27 20:33:57 2003
+@@ -3945,6 +3945,27 @@ S_init_perllib(pTHX)
+ /* Use the ~-expanded versions of APPLLIB (undocumented),
+     ARCHLIB PRIVLIB SITEARCH SITELIB VENDORARCH and VENDORLIB
+ */
 +#ifdef SITEARCH_EXP
 +    /* sitearch is always relative to sitelib on Windows for
 +     * DLL-based path intuition to work correctly */
 +#  if !defined(WIN32)
-+    incpush(SITEARCH_EXP, FALSE, FALSE);
++    incpush(SITEARCH_EXP, FALSE, FALSE, TRUE);
 +#  endif
 +#endif
 +
 +#ifdef SITELIB_EXP
 +#  if defined(WIN32)
-+    incpush(SITELIB_EXP, TRUE, FALSE);	/* this picks up sitearch as well */
++    /* this picks up sitearch as well */
++    incpush(SITELIB_EXP, TRUE, FALSE, TRUE);
 +#  else
-+    incpush(SITELIB_EXP, FALSE, FALSE);
++    incpush(SITELIB_EXP, FALSE, FALSE, TRUE);
 +#  endif
 +#endif
 +
 +#ifdef SITELIB_STEM /* Search for version-specific dirs below here */
-+    incpush(SITELIB_STEM, FALSE, TRUE);
++    incpush(SITELIB_STEM, FALSE, TRUE, TRUE);
 +#endif
 +
- /* Use the ~-expanded versions of APPLLIB (undocumented),
-     ARCHLIB PRIVLIB SITEARCH SITELIB VENDORARCH and VENDORLIB
- */
-@@ -3445,26 +3465,6 @@
-     incpush(PRIVLIB_EXP, TRUE, FALSE);
+ #ifdef APPLLIB_EXP
+     incpush(APPLLIB_EXP, TRUE, TRUE, TRUE);
+ #endif
+@@ -3980,27 +4001,6 @@ S_init_perllib(pTHX)
+     incpush(PRIVLIB_EXP, TRUE, FALSE, TRUE);
  #else
-     incpush(PRIVLIB_EXP, FALSE, FALSE);
+     incpush(PRIVLIB_EXP, FALSE, FALSE, TRUE);
 -#endif
 -
 -#ifdef SITEARCH_EXP
 -    /* sitearch is always relative to sitelib on Windows for
 -     * DLL-based path intuition to work correctly */
 -#  if !defined(WIN32)
--    incpush(SITEARCH_EXP, FALSE, FALSE);
+-    incpush(SITEARCH_EXP, FALSE, FALSE, TRUE);
 -#  endif
 -#endif
 -
 -#ifdef SITELIB_EXP
 -#  if defined(WIN32)
--    incpush(SITELIB_EXP, TRUE, FALSE);	/* this picks up sitearch as well */
+-    /* this picks up sitearch as well */
+-    incpush(SITELIB_EXP, TRUE, FALSE, TRUE);
 -#  else
--    incpush(SITELIB_EXP, FALSE, FALSE);
+-    incpush(SITELIB_EXP, FALSE, FALSE, TRUE);
 -#  endif
 -#endif
 -
 -#ifdef SITELIB_STEM /* Search for version-specific dirs below here */
--    incpush(SITELIB_STEM, FALSE, TRUE);
+-    incpush(SITELIB_STEM, FALSE, TRUE, TRUE);
  #endif
  
  #ifdef PERL_VENDORARCH_EXP
