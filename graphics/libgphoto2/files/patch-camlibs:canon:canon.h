bbe803a11e5cd38aeaa8c92bcc0c1884b69c565f
krion <krion@FreeBSD.org>
2003-10-28 22:14:07 +0000

    - Update to version 2.1.3
    PR:		58627
    Submitted by:	Ports Fury

--- graphics/libgphoto2/files/patch-camlibs:canon:canon.h
+++ graphics/libgphoto2/files/patch-camlibs:canon:canon.h
@@ -1,18 +1,18 @@
---- camlibs/canon/canon.h.orig	Sun Aug 10 20:51:13 2003
-+++ camlibs/canon/canon.h	Mon Oct 20 10:17:51 2003
-@@ -86,6 +86,7 @@
+--- camlibs/canon/canon.h.orig	Mon Oct 20 01:53:12 2003
++++ camlibs/canon/canon.h	Sun Oct 26 22:57:54 2003
+@@ -87,6 +87,7 @@
   * @CANON_PS_S330: Digital IXUS 330
   * @CANON_PS_S45: PowerShot S45
   * @CANON_PS_G3: PowerShot G3
-+ * @CANON_PS_G5: PowerShot G5
++ * @CANON_PS_G3: PowerShot G5
   * @CANON_PS_S230: PowerShot S230, Digital IXUS v3
   * @CANON_PS_A60: PowerShot A60
-  * @CANON_EOS_10D: EOS 10D
-@@ -123,6 +124,7 @@
- 	CANON_PS_A300,
+  * @CANON_MV630I: MV630i camcorder
+@@ -133,6 +134,7 @@
+ 	CANON_PS_S50,
  	CANON_PS_S45,
  	CANON_PS_G3,
 +	CANON_PS_G5,
- 	CANON_PS_S50,
  	CANON_PS_S230,
+ 	CANON_MV630I,
  	CANON_EOS_10D,
