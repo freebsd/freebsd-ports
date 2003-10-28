9a22c33feed3a0f14e0aaee7e5cb08e31742d027
foxfair <foxfair@FreeBSD.org>
2003-10-28 15:24:03 +0000

    PR:		58597 Submitted by:	Cheng-Lung Sung <clsung@dragon2.net> Approved by:	maintainer Fix build step in version 1.9.7

--- chinese/dictd/files/patch-index.c
+++ chinese/dictd/files/patch-index.c
@@ -1,36 +1,36 @@
---- index.c.orig	Sun Nov 24 01:24:10 2002
-+++ index.c	Sun Nov 24 01:24:21 2002
-@@ -91,6 +91,12 @@
+--- index.c.orig	Mon Mar 10 01:06:16 2003
++++ index.c	Mon Oct 27 22:10:18 2003
+@@ -103,6 +103,12 @@
  
     for (; *src; ++src) {
        c = * (const unsigned char *) src;
 +      if((c & 0x80) && *(src+1)) {
-+	 *dest++ = c;
-+	 *dest++ = *(src+1);
-+	 src++;
-+	 continue;
++        *dest++ = c;
++        *dest++ = *(src+1);
++        src++;
++        continue;
 +      }
  
        if (isspace( c )) {
           *dest++ = ' ';
-@@ -150,7 +156,7 @@
-     isspacealnumtab_allchars['\t'] = isspacealnumtab_allchars['\n'] = 0; /* special */
+@@ -184,7 +190,7 @@
  
-     for (i = 0; i <= UCHAR_MAX; i++){
--	if (islower (i) || (utf8_mode && i >= 0xC0))
-+	if (islower (i) || (utf8_mode && i >= 0xC0) || (i>=0x80))
- 	    chartab[charcount++] = i;
-     }
+    charcount = 0;
+    for (i = 0; i <= UCHAR_MAX; i++){
+-      if (islower (i) || (utf8_mode && i >= 0xC0))
++      if (islower (i) || (utf8_mode && i >= 0xC0) || (i>=0x80))
+ 	 chartab [charcount++] = i;
+    }
  
-@@ -317,6 +326,15 @@
+@@ -293,6 +299,15 @@
  
     /* FIXME.  Optimize this inner loop. */
     while (*word && start < end && *start != '\t') {
 +      if ((*start & 0x80) && start+1<end && start[1]!='\t') {
-+        if(start[0]!=word[0]) 
-+	  return ((unsigned)word[0]<(unsigned)start[0])?-2:1;
-+        if(start[1]!=word[1]) 
-+	  return ((unsigned)word[1]<(unsigned)start[1])?-2:1;
++        if(start[0]!=word[0])
++         return ((unsigned)word[0]<(unsigned)start[0])?-2:1;
++        if(start[1]!=word[1])
++         return ((unsigned)word[1]<(unsigned)start[1])?-2:1;
 +        word+=2;
 +        start+=2;
 +        continue;
@@ -38,7 +38,7 @@
        if (!dbindex -> isspacealnum[* (const unsigned char *) start]) {
  	 ++start;
  	 continue;
-@@ -359,6 +377,8 @@
+@@ -340,6 +355,8 @@
         *start != '\t' &&
         !dbindex -> isspacealnum[* (const unsigned char *) start])
     {
--- chinese/dictd/files/patch-libmaa::arg.c
+++ chinese/dictd/files/patch-libmaa::arg.c
@@ -1,12 +1,12 @@
---- libmaa/arg.c.orig	Wed Jul 17 05:21:04 2002
-+++ libmaa/arg.c	Wed Jul 17 05:13:50 2002
+--- libmaa/arg.c.orig	Sat Aug  3 03:43:15 2002
++++ libmaa/arg.c	Mon Oct 27 22:11:11 2003
 @@ -216,6 +216,9 @@
        ++pt;
  
     for (last = pt, len = 0; *pt; ++pt, ++len) {
-+     if((*pt & 0x80) && pt[1]) {
-+       ++pt, ++len;
-+     } else
++      if((*pt & 0x80) && pt[1]) {
++        ++pt, ++len;
++      } else
        switch (*pt) {
        case ' ':
        case '\t':
