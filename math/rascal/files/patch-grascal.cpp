b0c07be4fb4d342b4dc322a383e94bee96a964c5
krion <krion@FreeBSD.org>
2003-10-28 09:01:44 +0000

    - Fix build on -current - Utilize NOPORTDOCS
    PR:		58223
    Submitted by:	Ports Fury

--- math/rascal/files/patch-grascal.cpp
+++ math/rascal/files/patch-grascal.cpp
@@ -1,5 +1,14 @@
---- grascal.cpp.orig	Fri Jul 13 10:04:16 2001
-+++ grascal.cpp	Fri Jul 13 10:05:23 2001
+--- grascal.cpp.orig	Tue May 14 07:09:26 2002
++++ grascal.cpp	Sat Oct 18 14:51:09 2003
+@@ -76,7 +76,7 @@
+ 
+ int main(int argc,char **argv)
+ {
+-   Calterm a(string("grascal ")+string(VERSION),"/usr/local/bin/rascal","--shellcolour 4 --outputcolour 0");
++   Calterm a(string("grascal ")+string(VERSION),"%%PREFIX%%/bin/rascal","--shellcolour 4 --outputcolour 0");
+ 
+    if(exist("./rascal"))
+       a.setClientPath("./rascal");
 @@ -91,7 +91,7 @@
     a.addMenu(new MenuFileSelect("/File/_Load","<control>L","Load Rascal File",rascal_load));
     a.addMenu(new MenuFileSelect("/File/_Save","<control>S","Save Rascal File",rascal_save));
@@ -9,3 +18,12 @@
  
     a.addMenu(new MenuTop("/_Show"));
     a.addMenu(new MenuSimpleSend("/Show/_Variables","","variables;\n"));
+@@ -113,7 +113,7 @@
+    a.addMenu(new MenuSimpleSend("/Help/Licence","","licence;\n"));
+    a.addMenu(new MenuSimpleSend("/Help/Changelog","","help changes;\n"));
+    a.addMenu(new MenuSeparator("/Help/sep2"));
+-   a.addMenu(new MenuShowUrl("/Help/Manual","","file:/usr/local/doc/rascal/html/index.html"));
++   a.addMenu(new MenuShowUrl("/Help/Manual","","file:%%PREFIX%%/share/doc/rascal/html/index.html"));
+    a.addMenu(new MenuSeparator("/Help/sep1"));
+    a.addMenu(new MenuShowUrl("/Help/Rascal Homepage","","http://rascal.sourceforge.net/"));
+          
