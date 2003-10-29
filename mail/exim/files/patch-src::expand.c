df593bbd438a5d0cafaaf7e51e1a6765aa1bf07c
Sheldon Hearn <sheldonh@FreeBSD.org>
2003-10-29 17:11:47 +0000

    Fix patch path problem in previous delta. *blush*

--- mail/exim/files/patch-src::expand.c
+++ mail/exim/files/patch-src::expand.c
@@ -1,5 +1,5 @@
---- /var/tmp/usr/ports/mail/exim/work/exim-4.24/src/expand.c.orig	Mon Sep 22 10:29:56 2003
-+++ /var/tmp/usr/ports/mail/exim/work/exim-4.24/src/expand.c	Tue Oct 28 15:39:43 2003
+--- src/expand.c.orig	Mon Sep 22 10:29:56 2003
++++ src/expand.c	Tue Oct 28 15:39:43 2003
 @@ -4114,6 +4114,7 @@
  
  if (s == NULL) return -1;
