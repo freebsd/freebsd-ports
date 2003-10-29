c7eacd37bf9ed924378ebb2cc2f04a27bd2526f2
sheldonh <sheldonh@FreeBSD.org>
2003-10-29 11:56:02 +0000

    * Remove old saslauthd bugfix, included in 4.24. * Add bugfix for numeric overflow. * Update clamav installation instructions for changes to the clamav port.
    Bump PORTREVISION accordingly.
    
    Submitted by:	Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- mail/exim/files/patch-src::expand.c
+++ mail/exim/files/patch-src::expand.c
@@ -1,12 +1,10 @@
---- src/expand.c.orig	Mon Aug 18 14:52:54 2003
-+++ src/expand.c	Fri Aug 29 17:51:12 2003
-@@ -1462,6 +1462,9 @@
-   if (yield != NULL)
-     {
-     int rc;
-+
-+    if ( sub[2] == NULL ) sub[3] = NULL;
-+
-     rc = auth_call_saslauthd(sub[0], sub[1], sub[2], sub[3],
-       &expand_string_message);
-     if (rc == ERROR || rc == DEFER) return NULL;
+--- /var/tmp/usr/ports/mail/exim/work/exim-4.24/src/expand.c.orig	Mon Sep 22 10:29:56 2003
++++ /var/tmp/usr/ports/mail/exim/work/exim-4.24/src/expand.c	Tue Oct 28 15:39:43 2003
+@@ -4114,6 +4114,7 @@
+ 
+ if (s == NULL) return -1;
+ 
++errno = 0;
+ value = strtol(CS s, CSS &endptr, 0);
+ 
+ if (endptr == s)
