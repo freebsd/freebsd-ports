8d87a71c306d0c7335af7421c61d540b8110511d
Sergei Kolobov <sergei@FreeBSD.org>
2003-10-29 16:20:57 +0000

    Add fuse 0.6.1.1, Free Unix (Sinclair ZX-)Spectrum Emulator.
    fuse is a free version of a ZX Spectrum emulator. It emulates
    the 48K/128K/+2/+2A/+3 Speccy and Timex TC2048 machine, supports
    loading from .tzx files, has sound and kempston joystick emulation,
    and emulates various printers you could attach to the Spectrum.
    It supports the new RZX input recording file format.
    
    Author:	Philip Kendall <pak21-spectrum@srcf.ucam.org>
    WWW:	http://www.srcf.ucam.org/~pak21/spectrum/fuse.html
    
    PR:		58639
    Submitted by:	Alexey V. Antipovsky <kemm@in-line.ru>

---
+++ emulators/fuse/files/patch-osssound.c
@@ -0,0 +1,18 @@
+--- osssound.c.orig	Sat Sep 13 16:53:12 2003
++++ osssound.c	Tue Oct 28 17:19:06 2003
+@@ -107,6 +107,7 @@
+   return 1;
+   }
+ 
++#if 0
+ if(*freqptr>8250) frag++;
+ if(*freqptr>16500) frag++;
+ if(*freqptr>33000) frag++;
+@@ -118,6 +119,7 @@
+   close(soundfd);
+   return 1;
+   }
++#endif
+ 
+ return 0;	/* success */
+ }
