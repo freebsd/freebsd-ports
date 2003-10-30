43e9b2bdff9dcdd0927b847b5a2a958e42db8316
Hye-Shik Chang <perky@FreeBSD.org>
2003-10-30 08:56:06 +0000

    - Add a security/stability patch backported from main trunk. [1] - Utilize USE_AUTOCONF_VER rather than having a direct dependency.
    Submitted by:	Andrew Belashov <bel@orel.ru> [1]
    Approved by:	maintainer
    Obtained from:	Savannah

--- mail/spamass-milter/files/patch-spamass-milter.cpp
+++ mail/spamass-milter/files/patch-spamass-milter.cpp
@@ -1,5 +1,5 @@
---- spamass-milter.cpp.orig	Thu Jun 26 18:10:44 2003
-+++ spamass-milter.cpp	Wed Jul  2 14:12:25 2003
+--- spamass-milter.cpp.orig	Thu Jun 26 19:10:44 2003
++++ spamass-milter.cpp	Wed Oct 29 13:43:29 2003
 @@ -102,6 +102,10 @@
  #include <csignal>
  #include <string>
@@ -11,3 +11,14 @@
  
  #ifdef  __cplusplus
  extern "C" {
+@@ -350,6 +354,9 @@
+   string::size_type eoh2(assassin->d().find("\n\r\n"));
+   string::size_type eoh = ( eoh1 < eoh2 ? eoh1 : eoh2 );
+   string::size_type bob = assassin->d().find_first_not_of("\r\n", eoh);
++
++  if (bob == string::npos)
++  	bob = assassin->d().size();
+ 
+   update_or_insert(assassin, ctx, assassin->spam_flag(), &SpamAssassin::set_spam_flag, "X-Spam-Flag");
+   update_or_insert(assassin, ctx, assassin->spam_status(), &SpamAssassin::set_spam_status, "X-Spam-Status");
+
