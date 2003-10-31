2e49a1805d13d9edb9e886a03c2f927b5900deb7
marcus <marcus@FreeBSD.org>
2003-10-31 19:54:15 +0000

    Fix build with new -CURRENT if_xname change.
    Submitted by:	brooks

--- devel/libgtop2/files/patch-sysdeps_freebsd_netload.c
+++ devel/libgtop2/files/patch-sysdeps_freebsd_netload.c
@@ -8,6 +8,15 @@
  
      union {
  	struct ifaddr ifa;
+@@ -109,7 +109,7 @@
+ 			  sizeof (ifnet)) != sizeof (ifnet))
+ 		    glibtop_error_io_r (server, "kvm_read (ifnetaddr)");
+ 
+-#if defined(__FreeBSD__) || defined(__bsdi__)
++#if (defined(__FreeBSD__) && (__FreeBSD_version < 501113)) || defined(__bsdi__)
+ 	    if (kvm_read (server->machine.kd, (u_long) ifnet.if_name,
+ 			  tname, 16) != 16)
+ 		    glibtop_error_io_r (server, "kvm_read (if_name)");
 @@ -113,6 +114,9 @@
  	    if (kvm_read (server->machine.kd, (u_long) ifnet.if_name,
  			  tname, 16) != 16)
