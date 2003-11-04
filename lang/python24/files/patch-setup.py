0223d7cce347c990eb0499217aa63c6600a210fa
perky <perky@FreeBSD.org>
2003-11-04 18:00:37 +0000

    - Include pyexpat module in python base ports because python incorporated   expat 1.95.6 in its distribution from Python-2.3. - Bump PORTREVISION subsequently.
    Requested by:	Mike Brown <mike@skew.org>
    		Jeremy Kloth <jeremy.kloth@fourthought.com>

--- lang/python24/files/patch-setup.py
+++ lang/python24/files/patch-setup.py
@@ -1,14 +1,11 @@
-
-$FreeBSD$
-
---- setup.py.orig	Mon Sep 22 04:19:45 2003
-+++ setup.py	Wed Oct 29 22:47:51 2003
+--- setup.py.orig	Mon Sep 22 10:19:45 2003
++++ setup.py	Tue Nov  4 05:22:59 2003
 @@ -15,7 +15,7 @@
  from distutils.command.install_lib import install_lib
  
  # This global variable is used to hold the list of modules to be disabled.
 -disabled_module_list = []
-+disabled_module_list = ["_bsddb", "_tkinter", "gdbm", "mpz", "pyexpat"]
++disabled_module_list = ["_bsddb", "_tkinter", "gdbm", "mpz"]
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (at the front) if
