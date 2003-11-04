e9c6609e696160053bc687c7d3c7256fb11f9cb2
bland <bland@FreeBSD.org>
2003-11-04 06:55:16 +0000

    Kick off local gnome-support nonsense. All we need is libpopt. This also unscrew pkgconfig database.

--- devel/libgtop2/files/patch-src::daemon::gnuserv.c
+++ devel/libgtop2/files/patch-src::daemon::gnuserv.c
@@ -1,15 +1,24 @@
---- src/daemon/gnuserv.c.orig	Thu Nov 29 08:58:40 2001
-+++ src/daemon/gnuserv.c	Thu Nov 29 08:58:17 2001
-@@ -200,6 +200,12 @@
+--- src/daemon/gnuserv.c.orig	Mon Oct 20 01:54:32 2003
++++ src/daemon/gnuserv.c	Tue Nov  4 15:22:38 2003
+@@ -42,7 +42,7 @@
+ #include <glibtop/gnuserv.h>
  
- 	auth_data_len = atoi (buf);
+ #include <errno.h>
+-#include <popt-gnome.h>
++#include <popt.h>
  
-+	if (auth_data_len < 1 || auth_data_len > sizeof(buf)) {
-+	    syslog_message(LOG_WARNING,
-+			   "Invalid data length supplied by client");
+ #include "daemon.h"
+ 
+@@ -202,6 +202,12 @@
+ 	
+ 	if (auth_data_len < 1 || auth_data_len > sizeof(buf)) {
+ 	    syslog_message(LOG_WARNING, "Invalid data length supplied by client");
 +	    return FALSE;
 +	}
 +
- 	if (timed_read (fd, buf, auth_data_len, AUTH_TIMEOUT, 0) != auth_data_len)
++	if (auth_data_len < 1 || auth_data_len > sizeof(buf)) {
++	    syslog_message(LOG_WARNING,
++			   "Invalid data length supplied by client");
  	    return FALSE;
+ 	}
  
