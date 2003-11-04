1150c6620e4ca3e4276eadcd8a09bf310338e03e
Maho Nakata <maho@FreeBSD.org>
2003-11-04 12:30:41 +0000

    A small bug was found on the multithreaded version of spooles...
    PR:		58908
    Submitted by:	Pedro F. Giffuni <giffunip@yahoo.com>

---
+++ math/spooles/files/patch-MT+drivers+AllInOneMT.c
@@ -0,0 +1,11 @@
+--- MT/drivers/AllInOneMT.c.orig	Sat Aug  9 14:06:19 2003
++++ MT/drivers/AllInOneMT.c	Sat Aug  9 14:13:07 2003
+@@ -35,7 +35,7 @@
+ Chv             *rootchv ;
+ ChvManager      *chvmanager ;
+ double          droptol = 0.0, tau = 100. ;
+-double          cpus[10] ;
++double          cpus[11] ;	/* see MT/src/factor.c - Manfred Spraul */
+ DV              *cumopsDV ;
+ ETree           *frontETree ;
+ FrontMtx        *frontmtx ;
