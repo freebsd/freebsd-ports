cb275c7acb86c3afa0da29147a85db828fd047c5
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-11-05 05:04:16 +0000

    Don't reference memory after free'ing it.  This fixes a lockup and possible crash when exiting Galeon.
    Reported by:	Simon Barner <barner@in.tum.de>

--- www/galeon2/files/patch-src_galeon-shell.c
+++ www/galeon2/files/patch-src_galeon-shell.c
@@ -1,5 +1,5 @@
---- src/galeon-shell.c.orig	Tue Oct 28 01:07:00 2003
-+++ src/galeon-shell.c	Tue Oct 28 01:09:34 2003
+--- src/galeon-shell.c.orig	Mon Oct 13 17:56:13 2003
++++ src/galeon-shell.c	Tue Nov  4 23:07:54 2003
 @@ -186,6 +186,7 @@
  	if (gs->priv->automation_factory == NULL)
  	{
@@ -17,3 +17,21 @@
  					       "<tt>\"GNOME_Galeon_Automation.server\"</tt>");
  		hig_alert_set_secondary_printf (HIG_ALERT (dialog), "%s\n\n%s", text,
  			      _("Usually this is an indication of an incomplete installation. "
+@@ -283,8 +284,6 @@
+ 	gb_bookmark_set_check_save (gs->priv->bookmark_set);
+ 	g_object_unref (G_OBJECT (gs->priv->bookmark_set));
+ 	
+-        g_free (gs->priv);
+-
+         G_OBJECT_CLASS (parent_class)->finalize (object);
+ 
+ #ifdef DEBUG_MARCO
+@@ -295,6 +294,8 @@
+ 	{
+ 		bonobo_object_unref (gs->priv->automation_factory);
+ 	}
++
++        g_free (gs->priv);
+ 	
+ 	bonobo_main_quit ();
+ 
