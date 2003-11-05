6f3c154eab0aa81adef08051fd70a797d70cb878
fjoe <fjoe@FreeBSD.org>
2003-11-05 10:26:00 +0000

    Make this port use wxgtk 2.4.2.

---
+++ biology/L-Breeder/files/patch-LBreeder::MyTextFrame.h
@@ -0,0 +1,10 @@
+--- LBreeder/MyTextFrame.h.orig	Wed Nov  5 01:03:14 2003
++++ LBreeder/MyTextFrame.h	Wed Nov  5 01:03:22 2003
+@@ -7,6 +7,7 @@
+       TF_TEXT};
+ 
+ #include <wx/dialog.h>
++#include <wx/button.h>
+ #include <wx/textctrl.h>
+ #include <wx/layout.h>
+ 
