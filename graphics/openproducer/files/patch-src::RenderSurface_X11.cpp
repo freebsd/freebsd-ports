2d118bfc876ed56e5a257e2ed73f9110508ebaf3
James E. Housley <jeh@FreeBSD.org>
2003-11-06 12:52:46 +0000

    Update ports/graphics/openproducer to 0.8.4.2
    PR:		58162
    Submitted By:	MAINTAINER

---
+++ graphics/openproducer/files/patch-src::RenderSurface_X11.cpp
@@ -0,0 +1,11 @@
+--- src/RenderSurface_X11.cpp.orig	Fri Oct 17 19:14:39 2003
++++ src/RenderSurface_X11.cpp	Fri Oct 17 19:14:59 2003
+@@ -16,7 +16,7 @@
+ using namespace Producer; 
+ 
+ // HP, SGI and SUN don't implement glXGetProcAddressARB...
+-#if defined (__sgi) || defined (sun) || defined( __hpux )
++#if defined (__sgi) || defined (sun) || defined( __hpux ) || defined(__FreeBSD__)
+ #include <dlfcn.h>
+ void (*glXGetProcAddressARB(const GLubyte *procName))(void)
+ {
