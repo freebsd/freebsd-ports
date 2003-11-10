c96dcdc3dfd639ed2966c13b2e831ad81dd6d1d9
ijliao <ijliao@FreeBSD.org>
2003-11-10 08:29:22 +0000

    add libcwd 0.99.31 The C++ Debugging Support Library

---
+++ devel/libcwd/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Mon Nov 10 15:51:52 2003
++++ ltmain.sh	Mon Nov 10 15:52:02 2003
+@@ -4330,10 +4330,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
