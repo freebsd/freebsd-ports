2b421ac43a05af24139344012e77dc22bf899645
marcus <marcus@FreeBSD.org>
2003-11-10 19:28:34 +0000

    * Update to 0.132 * ChangeLog available at http://www.gnomepro.com/tsclient/ * Add knobs for optional vnc and Xnest (experimental) support * Update COMMENT * Add manpage * Fix pkg-plist to shutup portlint * Update pkg-descr
    PR:		59146
    Submitted by:	maintainer

--- net/tsclient/files/patch-src_connect.c
+++
@@ -1,20 +0,0 @@
---- src/connect.c.orig	Sat Jul 12 01:50:36 2003
-+++ src/connect.c	Sat Jul 12 01:51:34 2003
-@@ -1739,7 +1739,7 @@
-       rdp_file_save (rdp, filename);
-       g_free (rdp);
-     }
--    g_free (filename);
-+/*    g_free (filename);*/
-   }
-   gtk_widget_destroy (GTK_WIDGET (save_selector));
-   return;
-@@ -1784,7 +1784,7 @@
-       }
-       g_free (rdp);
-     }
--    g_free (filename);
-+/*    g_free (filename);*/
-   }
-   gtk_widget_destroy (GTK_WIDGET (open_selector));
-   return;
