ff7609bfb72a3e1bd6e4cd6320008e7779146d6a
netchild <netchild@FreeBSD.org>
2003-11-10 17:21:37 +0000

    Update to 0.24.
    Submitted by:	maintainer
    PR:		59096

--- deskutils/gdesklets/files/patch-libdesklets::CPU.py
+++ deskutils/gdesklets/files/patch-libdesklets::CPU.py
@@ -1,104 +1,59 @@
 # Many thanks to Alexander Leidinger <netchild@FreeBSD.org> for
 # help and create those patches.
 
---- libdesklets/CPU.py.orig	Fri Oct 17 22:47:02 2003
-+++ libdesklets/CPU.py	Fri Oct 17 22:53:05 2003
-@@ -1,5 +1,7 @@
+--- libdesklets/CPU.py	Thu Nov  6 14:50:47 2003
++++ libdesklets/CPU.py	Sun Nov  9 01:34:27 2003
+@@ -1,6 +1,8 @@
  import polling
+ import glibtop
  
 +import os
 +import libdesklets as lib
  
  #
  # TODO: support SMP
-@@ -21,8 +23,16 @@
- 
-         self.get_load = polling.wrap(self.__poll_load, 0.2)
- 
-+        platform = lib.sys.get_os()
-+
-         try:
--            fd = open("/proc/cpuinfo", "r")
-+            if platform == "FreeBSD":
-+                fd = os.popen("grep -3 CPU /var/run/dmesg.boot | head -7 | tail -4", "r")
-+            elif platform == "Linux":
-+                fd = open("/proc/cpuinfo", "r")
-+            else:
-+                print "Unknown OS, strange things may happen."
-+                return
-         except IOError, e:
-             import traceback; traceback.print_exc()
-             print e
-@@ -40,15 +50,24 @@
+@@ -35,20 +37,36 @@
  
      def __poll_cpu(self):
  
 -        import libdesklets as lib
 +        import re
-         arch = lib.sys.get_arch()
+ 
+-        fd = open("/proc/cpuinfo", "r")
 +        platform = lib.sys.get_os()
++
++        if platform == "FreeBSD":
++            fd = os.popen("grep -3 CPU /var/run/dmesg.boot | head -7 | tail -4", "r")
++        elif platform == "Linux":
++            fd = open("/proc/cpuinfo", "r")
++        else:
++            print "Unknown OS, strange things may happen."
++            return
+         lines = fd.readlines()
+         fd.close()
+ 
+         arch = lib.sys.get_arch()
          if (arch in ["i386", "i486", "i586", "i686"]):
--            fields = self.__lines[4].split()
+-            fields = lines[4].split()
 -            model_name = " ".join(fields[3:])
--            fields = self.__lines[6].split()
+-            fields = lines[6].split()
 -            cpu_mhz = fields[3]
--            fields = self.__lines[7].split()
+-            fields = lines[7].split()
 -            cpu_cache = " ".join(fields[3:5])
 +            if ("FreeBSD" == platform):
-+                m = re.search('^CPU: (.*) \(([0-9]+.*)-MHz [0-9]+-class CPU\)', self.__lines[0])
++                m = re.search('^CPU: (.*) \(([0-9]+.*)-MHz [0-9]+-class CPU\)', lines[0])
 +                model_name = m.group(1)           # or sysctl hw.model
 +                cpu_mhz = m.group(2)              # or sysctl hw.clockrate
 +                cpu_cache = " "                   # not available by default
 +            elif ("Linux" == platform):
-+                fields = self.__lines[4].split()
++                fields = lines[4].split()
 +                model_name = " ".join(fields[3:])
-+                fields = self.__lines[6].split()
++                fields = lines[6].split()
 +                cpu_mhz = fields[3]
-+                fields = self.__lines[7].split()
++                fields = lines[7].split()
 +                cpu_cache = " ".join(fields[3:5])
 +            else:
 +                pass
  
          elif (arch == "ppc"):
-             fields = self.__lines[0].split()
-@@ -68,17 +87,34 @@
- 
-     def __poll_load(self):
- 
--        fd = open("/proc/stat", "r")
-+        platform = lib.sys.get_os()
-+
-+        if ("FreeBSD" == platform):
-+            fd = os.popen("iostat -n 0", "r")
-+            line = 2
-+        elif ("Linux" == platform):
-+            fd = open("/proc/stat", "r")
-+            line = 0
-+        else:
-+            return
-         data = fd.read()
-         fd.close()
- 
-         data = data.splitlines()
--        fields = data[0].split()
-+        fields = data[line].split()
- 
--        u = float(fields[1])
--        s = float(fields[2])
--        n = float(fields[3])
--        i = float(fields[4])
-+        if ("FreeBSD" == platform):
-+            u = float(fields[2]) + float(fields[3])
-+            s = float(fields[4])
-+            n = float(fields[5])
-+            i = float(fields[6])
-+        elif ("Linux" == platform):
-+            u = float(fields[1])
-+            s = float(fields[2])
-+            n = float(fields[3])
-+            i = float(fields[4])
-+        else:
-+            pass
- 
-         total = ((u - self.__uT) + (s - self.__sT) + (n - self.__nT) +
-                 (i - self.__iT))
+             fields = lines[0].split()
--- deskutils/gdesklets/files/patch-libdesklets::Network.py
+++ deskutils/gdesklets/files/patch-libdesklets::Network.py
@@ -1,19 +1,20 @@
 # Many thanks to Alexander Leidinger <netchild@FreeBSD.org> for
 # help and create those patches.
 
---- libdesklets/Network.py.orig	Fri Oct 17 23:02:41 2003
-+++ libdesklets/Network.py	Fri Oct 17 23:24:47 2003
-@@ -1,7 +1,9 @@
+--- libdesklets/Network.py.orig	Sun Nov  9 00:52:39 2003
++++ libdesklets/Network.py	Sun Nov  9 00:53:05 2003
+@@ -1,8 +1,8 @@
  import polling
- 
- import commands
+ import glibtop
+-
 +import os
  import time
+-
 +import libdesklets as lib
  
  class Network:
  
-@@ -26,21 +28,42 @@
+@@ -27,21 +27,40 @@
  
      def __poll_devices(self):
  
@@ -34,7 +35,7 @@
 -            l = lines.strip()
 -            l = l.replace(":", " ")
 -            fields = l.split()
- 
+-
 -            if (fields[0] == "lo"):
 -                continue
 -            else:
@@ -57,92 +58,11 @@
 +
 +                if (fields[0] == "lo"):
 +                    continue
-+                else: # (fields[0].startswith("eth")):
++                else:
 +                    device = fields[0]
 +                    devices.append(device)
-+            #end for
 +        else:
 +            pass
  
          return devices
  
-@@ -48,14 +71,15 @@
- 
-     def __poll_ipaddr(self, dev):
- 
--        data = commands.getoutput("/sbin/ifconfig " + dev)
--        lines = data.splitlines()
--        for l in lines:
-+        fd = os.popen("/sbin/ifconfig " + dev, "r")
-+        data = fd.readlines()
-+        fd.close()
-+        for l in data:
-             l = l.strip()
-             fields = l.split()
- 
-             if (fields[0] == "inet"):
--                return fields[1].split(":")[1]
-+                return fields[1]
- 
-         return ("xxx.xxx.xxx.xxx")
- 
-@@ -63,6 +87,8 @@
- 
-     def __poll_in_out(self, dev):
- 
-+        platform = lib.sys.get_os()
-+
-         t = time.time()
-         interval = t - self.__time
-         self.__time = t
-@@ -73,24 +99,40 @@
-         speed_in = 0
-         speed_out = 0
- 
--        fd = open("/proc/net/dev", "r")
-+        if ("FreeBSD" == platform):
-+            fd = os.popen("netstat -b -I " + dev + " | grep Link", "r")
-+        elif ("Linux" == platform):
-+            fd = open("/proc/net/dev", "r")
-+        else:
-+            return (bytes_in, bytes_out, pack_in, pack_out, speed_in, speed_out)
-         data = fd.read()
-         fd.close()
-         lines = data.splitlines()
- 
-         # look for the device
-         found = 0
--        for l in lines:
--            l.strip()
--            l = l.replace(":", " ")
--            fields = l.split()
--            if (fields[0] == dev):
--                bytes_in, pack_in, bytes_out, pack_out = \
--                long(fields[1]), long(fields[2]), \
--                long(fields[9]), long(fields[10])
-+        if ("FreeBSD" == platform):
-+            for l in lines:
-                 found = 1
-+                fields = l.strip().split()
-+                bytes_in, pack_in, bytes_out, pack_out = \
-+                long(fields[6]), long(fields[4]), \
-+                long(fields[9]), long(fields[7])
-                 break
--        #end for
-+        elif ("Linux" == platform):
-+            for l in lines:
-+                l.strip()
-+                l = l.replace(":", " ")
-+                fields = l.split()
-+                if (fields[0] == dev):
-+                    bytes_in, pack_in, bytes_out, pack_out = \
-+                    long(fields[1]), long(fields[2]), \
-+                    long(fields[9]), long(fields[10])
-+                    found = 1
-+                    break
-+            #end for
-+        else:
-+            pass
- 
-         # warn if we didn't find the device
-         if (not found): print ("WARNING:: Device %(dev)s not found!") % vars()
