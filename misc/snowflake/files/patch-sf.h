ff283bd66d5a112bea98f901f4bfe72552a3794c
Kirill Ponomarev <krion@FreeBSD.org>
2003-11-10 19:31:11 +0000

    - Fix MASTER_SITES - Avoid some compiler warnings - Remove obsolete WWW: line - Bump PORTREVISION
    PR:		59129
    Submitted by:	Ports Fury

---
+++ misc/snowflake/files/patch-sf.h
@@ -0,0 +1,10 @@
+--- sf.h.orig	Mon Dec 21 08:18:55 1998
++++ sf.h	Sat Nov  8 21:20:24 2003
+@@ -17,6 +17,7 @@
+ 
+ typedef enum
+ {
++  c_VERSION          = 'V',
+   c_VIEW_GEOMETRY    = 'v',
+   c_CONTROL_GEOMETRY = 'c',
+   c_MINIMIZE         = 'm',
