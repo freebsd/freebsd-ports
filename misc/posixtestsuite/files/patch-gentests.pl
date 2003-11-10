d20b95972457889dd699b77d4547b23616b568d7
phantom <phantom@FreeBSD.org>
2003-11-10 09:49:38 +0000

    Add pseudo-port of POSIX Test Suite (http://posixtest.sourceforge.net). It does not builds/installs anything, and should be used as wrapper to run testsuite on FreeBSD.  See 'make help' output for usage.

---
+++ misc/posixtestsuite/files/patch-gentests.pl
@@ -0,0 +1,11 @@
+--- conformance/interfaces/sigaction/gentests.pl~	Mon Nov 10 11:37:15 2003
++++ conformance/interfaces/sigaction/gentests.pl	Mon Nov 10 11:43:01 2003
+@@ -11,7 +11,7 @@
+ my (@signals) = ("SIGABRT", "SIGALRM", "SIGBUS", "SIGCHLD", "SIGCONT", 
+ 		 "SIGFPE", "SIGHUP", "SIGILL", "SIGINT", "SIGPIPE",
+ 		 "SIGQUIT", "SIGSEGV", "SIGTERM", "SIGTSTP", "SIGTTIN",
+-		 "SIGTTOU", "SIGUSR1", "SIGUSR2", "SIGPOLL", "SIGPROF",
++		 "SIGTTOU", "SIGUSR1", "SIGUSR2", "SIGPROF",
+ 		 "SIGSYS", "SIGTRAP", "SIGURG", "SIGVTALRM", "SIGXCPU",
+ 		 "SIGXFSZ");
+ 
