51a9c8beaa7652f14e6769f5e02c414ea11dbea4
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2003-11-10 00:41:15 +0000

    0.9.2

---
+++ multimedia/ogle-gui/files/patch-src::callbacks.c
@@ -0,0 +1,11 @@
+--- src/callbacks.c.orig	Sun Nov  9 18:20:31 2003
++++ src/callbacks.c	Sun Nov  9 18:21:14 2003
+@@ -22,7 +22,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <time.h>
++#include <sys/time.h>
+ #include <gtk/gtk.h>
+ #include <ogle/dvdcontrol.h>
+ 
