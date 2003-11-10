554ce86c00882adf4548ca9e351ffa97fefc8786
Kirill Ponomarev <krion@FreeBSD.org>
2003-11-10 18:51:28 +0000

    - Update to version 0.4.2
    PR:		59126
    Submitted by:	Ports Fury

---
+++ games/vamos/files/patch-track::Road_Segment.cc
@@ -0,0 +1,10 @@
+--- track/Road_Segment.cc.orig	Mon Oct 27 10:15:10 2003
++++ track/Road_Segment.cc	Mon Nov 10 02:10:01 2003
+@@ -21,6 +21,7 @@
+ 
+ #include <GL/glut.h>
+ 
++#include <algorithm>
+ #include <cmath>
+ #include <cassert>
+ 
