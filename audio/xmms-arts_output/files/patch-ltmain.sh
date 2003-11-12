7d49db11e9874cd6288d9617f5aed907d30687a7
Max Khon <fjoe@FreeBSD.org>
2003-11-12 00:14:40 +0000

    Add one more XMMS aRts output plugin.

--- audio/xmms-arts_output/files/patch-ltmain.sh
+++ audio/xmms-arts_output/files/patch-ltmain.sh
@@ -1,5 +1,14 @@
 --- ltmain.sh.orig	Wed Aug  6 19:13:39 2003
 +++ ltmain.sh	Tue Nov 11 22:47:49 2003
+@@ -1075,7 +1075,7 @@
+ 	  esac
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	 *-*-openbsd* | *-*-freebsd*)
++	 *-*-openbsd*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
 @@ -1084,6 +1084,13 @@
  	deplibs="$deplibs $arg"
  	continue
