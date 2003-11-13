0fd7d1616f80e01a7b456113052bd15cbe89d917
ijliao <ijliao@FreeBSD.org>
2003-11-13 07:09:20 +0000

    add xmlrpc++ 0.7 A C++ implementation of the XML-RPC protocol

---
+++ net/xmlrpc++/files/patch-Validator.cpp
@@ -0,0 +1,11 @@
+--- test/Validator.cpp.orig	Thu Nov 13 14:47:39 2003
++++ test/Validator.cpp	Thu Nov 13 14:47:56 2003
+@@ -137,7 +137,7 @@
+   {
+     std::cerr << "ModerateSizeArrayCheck\n";
+     std::string s = params[0][0];
+-    s += params[0][params[0].size()-1];
++    s += (std::string)params[0][params[0].size()-1];
+     result = s;
+   }
+ } moderateSizeArrayCheck(&s);
