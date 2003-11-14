43d1d3804682e1135d856bce7a461f672a50b866
osa <osa@FreeBSD.org>
2003-11-14 15:10:05 +0000

    Add a new port. Tkabber is a free client for an instant messaging system called Jabber. It is written in Tcl/Tk and supports many features like support of unicode, ssl support, http proxy, file transfers and support of multi-user conference protocol.
    Submitted by:	Alexander Timoshenko <gonzo@univ.kiev.ua>
    PR:		58389

---
+++ net/tkabber/files/patch-tkabber.tcl
@@ -0,0 +1,10 @@
+--- tkabber.tcl.orig	Mon Jun  9 13:00:36 2003
++++ tkabber.tcl	Fri Oct 17 15:45:06 2003
+@@ -1,6 +1,6 @@
+ #!/bin/sh
+ # the next line restarts using the correct interpreter \
+-exec wish "$0" -name tkabber "$@"
++exec wish8.4 "$0" -name tkabber "$@"
+ 
+ # $Id: tkabber.tcl,v 1.159 2003/05/30 18:05:27 aleksey Exp $
+ 
