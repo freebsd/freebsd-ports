63a282cb80939157bb4e22c00f1bff5fa75eba59
Pav Lucistnik <pav@FreeBSD.org>
2003-11-14 23:14:00 +0000

    Fix build on 4.x
    PR:		ports/59294
    Submitted by:	Mezz <mezz7@cox.net> (maintainer)
    Approved by:	adamw (mentor)

---
+++ deskutils/gtodo/files/patch-src::main.c
@@ -0,0 +1,10 @@
+--- src/main.c.orig	Fri Nov 14 14:37:02 2003
++++ src/main.c	Fri Nov 14 14:37:09 2003
+@@ -1,7 +1,6 @@
+ #include <gtk/gtk.h>
+ #include <sys/stat.h>
+ #include <sys/types.h>
+-#include <getopt.h>
+ #include <gconf/gconf.h>
+ #include "main.h"
+ 
