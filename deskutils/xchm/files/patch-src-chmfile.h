e93712b5e6833b9ec274c412b7ff34808a172071
Pav Lucistnik <pav@FreeBSD.org>
2003-11-14 22:42:38 +0000

    Update to 0.8.8, major feature enhancement: - all the wxListBox widgets have been replaced by wxListCtrl controls - added searchable index support via new Index panel - bugfix: uppercase .HTM files are recognized to be text/html
    Approved by:	adamw (mentor)

---
+++ deskutils/xchm/files/patch-src-chmfile.h
@@ -0,0 +1,10 @@
+--- src/chmfile.h.orig	Wed Nov  5 19:37:01 2003
++++ src/chmfile.h	Fri Nov 14 22:11:24 2003
+@@ -23,6 +23,7 @@
+ #define __CHMFILE_H_
+ 
+ #include <chm_lib.h>
++#include <wx/filefn.h>
+ #include <wx/string.h>
+ #include <wx/hashmap.h>
+ #include <wx/font.h>
