86b6af553ce4ba9cc4916d21699c3bfaf10c716d
lofi <lofi@FreeBSD.org>
2003-11-16 19:10:44 +0000

    Add libpaper, a library providing routines for paper size management.

---
+++ print/libpaper/files/patch-lib::paper.c
@@ -0,0 +1,10 @@
+--- lib/paper.c.orig	Sun Nov 16 19:56:24 2003
++++ lib/paper.c	Sun Nov 16 19:56:29 2003
+@@ -14,7 +14,6 @@
+ #include <sys/stat.h>
+ 
+ #include <stdio.h>
+-#include <malloc.h>
+ #include <string.h>
+ #include <ctype.h>
+ 
