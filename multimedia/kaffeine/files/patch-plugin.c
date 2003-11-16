ca28edcb1b32bcde5a9c8a0582a9376da9d5465b
Tilman Keskinoz <arved@FreeBSD.org>
2003-11-16 16:39:59 +0000

    Update to 0.4

---
+++ multimedia/kaffeine/files/patch-plugin.c
@@ -0,0 +1,10 @@
+--- addons/mozilla-plugin/plugin.c.orig	Sun Nov 16 16:59:07 2003
++++ addons/mozilla-plugin/plugin.c	Sun Nov 16 16:59:46 2003
+@@ -63,6 +63,7 @@
+ 
+ #include <stdio.h>
+ #include <math.h>
++#include <signal.h>
+ 
+ 
+ 
