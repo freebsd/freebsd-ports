e14fcf0de1f74fe8880c4f0d0766ff254c3af632
pav <pav@FreeBSD.org>
2003-11-16 18:56:32 +0000

    - Update to 2.9 - Add knob to build with SDL instead of glut.
    PR:		ports/59319
    Submitted by:	Mezz <mezz7@cox.net>
    Approved by:	Thomas Vogt (maintainer)
    Approved by:	marcus (backup mentor)

--- games/foobillard/files/patch-billard3d.c
+++ games/foobillard/files/patch-billard3d.c
@@ -1,5 +1,5 @@
---- src/billard3d.c.orig	Thu May  1 08:32:10 2003
-+++ src/billard3d.c	Mon Jul  7 04:29:04 2003
+--- src/billard3d.c.orig	Sat Nov 15 21:10:47 2003
++++ src/billard3d.c	Sat Nov 15 21:12:13 2003
 @@ -24,7 +24,7 @@
  #include <string.h>
  #include <math.h>
@@ -9,16 +9,16 @@
  
  #ifndef USE_SDL
  #include <GL/glut.h>
-@@ -4270,7 +4270,7 @@
+@@ -4960,7 +4960,7 @@
  
     /* config file */
     load_config( &confv, &confc, argv, argc );
 -   while( ( act_option = getopt_long_only(confc, confv, "+", long_options, &option_index) ) >= 0){
 +   while( ( act_option = getopt_long(confc, confv, "+", long_options, &option_index) ) >= 0){
+        DPRINTF("processing option %d=%s\n",act_option,optarg);
         process_option(act_option);
     }
-    DPRINTF("main:rgstereo=%d",options_rgstereo_on);
-@@ -4359,7 +4359,7 @@
+@@ -5058,7 +5058,7 @@
         fread( &ball_ball_snd.data[SOUND_NULLOFFS*2], 1, ball_ball_snd.len-SOUND_NULLOFFS*2*2 , f );
         fclose(f);
  
--- games/foobillard/files/patch-sys_stuff.c
+++ games/foobillard/files/patch-sys_stuff.c
@@ -1,11 +1,11 @@
---- src/sys_stuff.c.orig	Sat Oct 26 20:47:23 2002
-+++ src/sys_stuff.c	Sat Oct 26 20:47:32 2002
+--- src/sys_stuff.c.orig	Sat Nov 15 21:24:23 2003
++++ src/sys_stuff.c	Sat Nov 15 21:25:20 2003
 @@ -5,7 +5,7 @@
  #ifndef USE_SDL
  #include <GL/glut.h>
  #else
 -#include <SDL/SDL.h>
-+#include <SDL11/SDL.h>
++#include <SDL.h>
  #include <GL/gl.h>
  #include <GL/glu.h>
  #endif
