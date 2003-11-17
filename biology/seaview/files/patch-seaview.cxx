1bd27d1435ed89265be40dca3633bca22e9e40cf
krion <krion@FreeBSD.org>
2003-11-17 13:36:41 +0000

    - Update to new version 2003.11.17 - Unbreak this port

--- biology/seaview/files/patch-seaview.cxx
+++ biology/seaview/files/patch-seaview.cxx
@@ -1,14 +1,11 @@
-
-$FreeBSD$
-
---- seaview.cxx.orig	Thu May 22 01:45:14 2003
-+++ seaview.cxx	Thu May 22 01:37:25 2003
+--- seaview.cxx.orig	Mon Nov 17 14:16:39 2003
++++ seaview.cxx	Mon Nov 17 14:17:13 2003
 @@ -164,6 +164,8 @@
  	"seaview.hlp", 
  #elif defined(__APPLE__)
  	NULL,
 +#elif defined(DEFAULT_HELPFILE)
-+	__XSTRING(DEFAULT_HELPFILE),
++       __XSTRING(DEFAULT_HELPFILE),
  #else
  	"seaview.help", 
  #endif
