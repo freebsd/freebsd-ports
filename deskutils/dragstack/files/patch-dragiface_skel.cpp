a35dd01fb4ff101714861686c108c3975c5e4c81
Kirill Ponomarev <krion@FreeBSD.org>
2003-11-17 09:20:07 +0000

    - Update to version 1.2
    PR:		59353
    Submitted by:	Markus Brueffer <brueffer@phoenix-systems.de> (maintainer)

---
+++ deskutils/dragstack/files/patch-dragiface_skel.cpp
@@ -0,0 +1,11 @@
+--- dragstack/dragiface_skel.cpp.orig	Sun Nov 16 13:37:49 2003
++++ dragstack/dragiface_skel.cpp	Sun Nov 16 13:38:21 2003
+@@ -6,7 +6,7 @@
+ **
+ *****************************************************************************/
+ 
+-#include "/home/dominik/Desktop/Programming/dragstack/dragstack/dragiface.h"
++#include "dragiface.h"
+ 
+ #include <kdatastream.h>
+ 
