c8a475f06cb6d1fdb7ef29834f8261cd6ff3b205
krion <krion@FreeBSD.org>
2003-11-19 11:17:41 +0000

    - unbreak on 5.x - respect PTHREAD_LIBS and CFLAGS - use REINPLACE_CMD instead of PERL - remove obsolete replacement
    PR:		58226
    Submitted by:	Markus Brueffer <brueffer@phoenix-systems.de>
    Approved by:	maintainer timeout

---
+++ ftp/kwebget/files/patch-kwebget.cpp
@@ -0,0 +1,11 @@
+--- kwebget/kwebget.cpp.orig	Sat Oct 18 18:26:29 2003
++++ kwebget/kwebget.cpp	Sat Oct 18 18:26:50 2003
+@@ -15,6 +15,8 @@
+  *                                                                         *
+  ***************************************************************************/
+ 
++#include <stdlib.h>
++
+ #include "kwebget.h"
+ 
+ #include "../pics/bg.xpm"
