24518616f6b51d7e249009c4b188cec2b9c6619b
krion <krion@FreeBSD.org>
2003-11-22 21:24:41 +0000

    - Update to version 0.12.1
    PR:		59583
    Submitted by:	Ports Fury

---
+++ net/linphone-base/files/patch-oRTP::ltmain.sh
@@ -0,0 +1,15 @@
+--- oRTP/ltmain.sh.orig	Thu Nov 20 19:42:06 2003
++++ oRTP/ltmain.sh	Fri Nov 21 13:13:05 2003
+@@ -5478,10 +5478,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
--- net/linphone/files/patch-osipua::src::osipua.c
+++ net/linphone/files/patch-osipua::src::osipua.c
@@ -1,5 +1,5 @@
---- osipua/src/osipua.c.orig	Sat Aug 16 02:30:51 2003
-+++ osipua/src/osipua.c	Thu Oct 23 12:56:06 2003
+--- osipua/src/osipua.c.orig	Mon Oct 20 17:55:05 2003
++++ osipua/src/osipua.c	Fri Nov 21 11:52:31 2003
 @@ -591,8 +591,7 @@
  			if (ipaddr != NULL)
  			{
@@ -10,12 +10,3 @@
  				{
  				   	 ret_ua=ua;       /* ip addresses match */
  				}
-@@ -600,7 +599,7 @@
- 			else ret_ua=ua;	/* we accept uncheckable host names... */
- 			ret_ua=ua;
- #else
--			if (ip4addr!=NULL)
-+			if (ipaddr!=NULL)
- 			{
- 				osip_trace(OSIP_INFO1,("osip_ua_find 1: %s <> %s \n",ua->ua_ip4addr,ipaddr));
- 				if ((strcasecmp(ua->ua_ip4addr,ipaddr)==0) || (strcasecmp("127.0.0.1",ipaddr)==0))
