7805d1362e2481f763df6fcb2e74bfdaa0ed3154
pav <pav@FreeBSD.org>
2003-11-22 22:32:00 +0000

    Squash is a learning ncurses music player written in C. It supports mp3, ogg and flac. Squash uses statistics to determine songs to play automatically. It garners this information through whether or not a song is skipped.
    PR:			ports/55275
    Submitted by:		Matthias Andree <matthias.andree@web.de>
    Based on shar by:	trevor
    Approved by:		adamw (mentor)

---
+++ audio/squash/files/patch-include::global.h
@@ -0,0 +1,13 @@
+$FreeBSD$
+
+--- include/global.h~ 	Mon Aug  4 16:43:16 2003
++++ include/global.h	Tue Aug  5 11:54:11 2003
+@@ -40,7 +40,7 @@
+ #include <ao/ao.h>
+ #include <math.h>
+ #include <stdarg.h>
+-#include <wait.h>
++#include <sys/wait.h>
+ #include <sys/mman.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
