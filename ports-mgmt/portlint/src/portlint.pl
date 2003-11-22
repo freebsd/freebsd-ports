0688779922ae28b9884a0e51dba86c896d55646e
marcus <marcus@FreeBSD.org>
2003-11-22 20:38:55 +0000

    Ack, I knew I forgot something.  Add a new -C argument that enables pedantic committer mode.  This is the identical to -A except it omits the new port checks.
    Requested by:	eik

--- ports-mgmt/portlint/src/portlint.pl
+++ ports-mgmt/portlint/src/portlint.pl
@@ -17,7 +17,7 @@
 # OpenBSD and NetBSD will be accepted.
 #
 # $FreeBSD$
-# $Id: portlint.pl,v 1.29 2003/11/22 20:24:40 marcus Exp $
+# $Id: portlint.pl,v 1.30 2003/11/22 20:37:23 marcus Exp $
 #
 
 use vars qw/ $opt_a $opt_A $opt_b $opt_c $opt_h $opt_t $opt_v $opt_M $opt_N $opt_B $opt_V /;
@@ -40,7 +40,7 @@ $portdir = '.';
 # version variables
 my $major = 2;
 my $minor = 5;
-my $micro = 2;
+my $micro = 3;
 
 sub l { '[{(]'; }
 sub r { '[)}]'; }
@@ -90,6 +90,7 @@ usage: $prog [-AabchvtN] [-M ENV] [-B#] [port_directory]
 	-a	additional check for scripts/* and pkg-*
 	-A	turn on all additional checks (equivalent to -abcNt)
 	-b	warn \$(VARIABLE)
+	-C	pedantic committer mode (equivalent to -abct)
 	-c	committer mode
 	-h	show summary of command line options
 	-v	verbose mode
@@ -107,16 +108,16 @@ sub version {
 	exit $major;
 }
 
-getopts('AabchtvB:M:NV');
+getopts('AabCchtvB:M:NV');
 
 &usage if $opt_h;
 &version if $opt_V;
-$extrafile = 1 if $opt_a || $opt_A;
-$parenwarn = 1 if $opt_b || $opt_A;
-$committer = 1 if $opt_c || $opt_A;
+$extrafile = 1 if $opt_a || $opt_A || $opt_C;
+$parenwarn = 1 if $opt_b || $opt_A || $opt_C;
+$committer = 1 if $opt_c || $opt_A || $opt_C;
 $verbose = 1 if $opt_v;
 $newport = 1 if $opt_N || $opt_A;
-$usetabs = 1 if $opt_t || $opt_A;
+$usetabs = 1 if $opt_t || $opt_A || $opt_C;
 $contblank = $opt_B if $opt_B;
 $makeenv = $opt_M if $opt_M;
 
