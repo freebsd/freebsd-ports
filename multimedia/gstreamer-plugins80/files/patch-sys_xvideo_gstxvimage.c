68dfb56a10766f42a940505095dfea309349cc0e
Joe Marcus Clarke <marcus@FreeBSD.org>
2003-11-24 01:26:53 +0000

    Fix build on Alpha.
    Reported by:	bento via kris
    Approved by:	portmgr (implicit)

--- multimedia/gstreamer-plugins80/files/patch-sys_xvideo_gstxvimage.c
+++ multimedia/gstreamer-plugins80/files/patch-sys_xvideo_gstxvimage.c
@@ -1,12 +1,21 @@
---- sys/xvideo/gstxvimage.c.orig	Mon Oct 13 15:28:54 2003
-+++ sys/xvideo/gstxvimage.c	Mon Oct 13 15:29:24 2003
-@@ -20,6 +20,9 @@
+--- sys/xvideo/gstxvimage.c.orig	Sun Nov 23 20:17:00 2003
++++ sys/xvideo/gstxvimage.c	Sun Nov 23 20:25:19 2003
+@@ -20,6 +20,10 @@
  #include "config.h"
  
  #include <errno.h>
++#include <sys/types.h>
 +#include <machine/param.h>
 +#include <sys/types.h>
 +#include <sys/ipc.h>
  #include <sys/shm.h> /* for SHM symbols */
  #include <gst/gst.h>
  /* gcc -ansi -pedantic on GNU/Linux causes warnings and errors
+@@ -34,7 +38,6 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <sys/types.h>
+ 
+ #if defined (HAVE_IPC_H) && defined (HAVE_SHM_H) && defined (HAVE_XSHM_H)
+ #define USE_SHM
