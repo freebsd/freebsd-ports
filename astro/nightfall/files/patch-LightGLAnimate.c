a9ee2334a31a2abf6529516d1ddf5e9a74973602
Kirill Ponomarev <krion@FreeBSD.org>
2003-11-25 06:04:10 +0000

    - Fix build on 5.x
    Noticed by:	bento via kris
    Approved by:	portmgr (kris)

---
+++ astro/nightfall/files/patch-LightGLAnimate.c
@@ -0,0 +1,10 @@
+--- LightGLAnimate.c.orig	Tue Nov 25 00:21:36 2003
++++ LightGLAnimate.c	Tue Nov 25 00:24:22 2003
+@@ -25,6 +25,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
++#include <sys/types.h>
+ #include "Light.h"
+ 
+ #ifdef  _WITH_OPENGL
