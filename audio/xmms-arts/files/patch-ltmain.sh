0f4ba387592396fc900dcacba671c4648d669fcf
fjoe <fjoe@FreeBSD.org>
2003-11-25 00:55:36 +0000

    Fix build on 5.x.
    Notice by:	kris
    Approved by:	kris

--- audio/xmms-arts/files/patch-ltmain.sh
+++ audio/xmms-arts/files/patch-ltmain.sh
@@ -1,16 +1,25 @@
---- ltmain.sh.orig	Wed Aug  6 19:13:39 2003
-+++ ltmain.sh	Tue Nov 11 22:47:49 2003
-@@ -1084,6 +1084,13 @@
+--- ltmain.sh.orig	Fri Sep  1 10:46:39 2000
++++ ltmain.sh	Tue Nov 25 06:49:01 2003
+@@ -1091,6 +1091,13 @@
  	deplibs="$deplibs $arg"
- 	continue
  	;;
-+
+ 
 +      -pthread)
 +        compile_command="$compile_command -pthread"
 +        finalize_command="$finalize_command -pthread"
 +        compiler_flags="$compiler_flags -pthread"
 +        continue
 +        ;;
- 
++
        -module)
  	module=yes
+ 	continue
+@@ -1926,7 +1933,7 @@
+ 	  set dummy $deplibs_check_method
+ 	  file_magic_regex="`expr \"$deplibs_check_method\" : \"$2 \(.*\)\"`"
+ 	  for a_deplib in $deplibs; do
+-	    name="`expr $a_deplib : '-l\(.*\)'`"
++	    name="`expr \( $a_deplib : '-l\(.*\)' \)`"
+ 	    # If $name is empty we are operating on a -L argument.
+ 	    if test "$name" != "" ; then
+ 	      libname=`eval \\$echo \"$libname_spec\"`
