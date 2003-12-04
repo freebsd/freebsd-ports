fc4c5d46916fbfec01d84ae1a4371b16e85c7a6e
Kirill Ponomarev <krion@FreeBSD.org>
2003-12-04 18:46:01 +0000

    - Update to version 0.6.6
    PR:		59644
    Submitted by:	Ports Fury

--- games/csmash/files/patch-MultiPlay.cpp
+++ games/csmash/files/patch-MultiPlay.cpp
@@ -1,11 +1,19 @@
---- MultiPlay.cpp.orig	Tue Nov 11 16:12:35 2003
-+++ MultiPlay.cpp	Tue Nov 11 16:10:24 2003
-@@ -35,7 +35,7 @@
+--- MultiPlay.cpp.orig	Thu Nov 20 02:05:02 2003
++++ MultiPlay.cpp	Mon Nov 24 09:34:22 2003
+@@ -37,13 +37,15 @@
+ #include <netinet/tcp.h>
+ #endif
  
- #if defined(WIN32) || defined(__FreeBSD__)
+-#if defined(WIN32) || defined(__FreeBSD__)
++#if defined(WIN32)
  
--typedef int socklen_t;		/* mimic Penguin's typedef */
-+//typedef int socklen_t;		/* mimic Penguin's typedef */
+ typedef int socklen_t;		/* mimic Penguin's typedef */
  
  #else	/* ! WIN32 */
+ 
++#if !defined(__FreeBSD__)
+ #define closesocket(FD) close(FD)
++#endif
+ 
+ #endif
  
