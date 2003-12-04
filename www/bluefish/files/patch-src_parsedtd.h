6d15c9b13cbfbbdf09593eae91503f2dfb01bf14
Pav Lucistnik <pav@FreeBSD.org>
2003-12-04 00:00:00 +0000

    - Fix build with gcc 3.3
    PR:		ports/59585
    Submitted by:	Koop Mast <einekoai@chello.nl>
    Approved by:	marcus (portmgr)

---
+++ www/bluefish/files/patch-src_parsedtd.h
@@ -0,0 +1,15 @@
+--- src/parsedtd.h.orig	Sat Nov 17 15:32:46 2001
++++ src/parsedtd.h	Sat Nov 22 14:58:35 2003
+@@ -135,10 +135,8 @@
+ #define DTD_HTML_4_01T "!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\""
+ #define DTD_XHTML_1_0 "!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0//EN\""
+ #define DTD_XHTML_1_0S "!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"DTD/xhtml1-strict.dtd\""
+-#define DTD_XHTML_1_0F "!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Frameset//EN\"
+-\"DTD/xhtml1-frameset.dtd\""
+-#define DTD_XHTML_1_0T "!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
+-\"DTD/xhtml1-transitional.dtd\""
++#define DTD_XHTML_1_0F "!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Frameset//EN\" \"DTD/xhtml1-frameset.dtd\""
++#define DTD_XHTML_1_0T "!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"DTD/xhtml1-transitional.dtd\""
+ #define DTD_XHTML_1_1 "!DOCTYPE html PUBLIC \"-//IETF//DTD XHTML 1.1//EN\""
+ 
+ extern SGML_doctype HTML_doctypes[];
