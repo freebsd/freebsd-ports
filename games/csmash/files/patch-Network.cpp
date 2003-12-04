99f46781e5365cb95cc91223235c3c469a69585a
krion <krion@FreeBSD.org>
2003-12-04 18:46:01 +0000

    - Update to version 0.6.6
    PR:		59644
    Submitted by:	Ports Fury

--- games/csmash/files/patch-Network.cpp
+++ games/csmash/files/patch-Network.cpp
@@ -1,20 +1,27 @@
---- Network.cpp.orig	Tue Nov 11 16:13:38 2003
-+++ Network.cpp	Tue Nov 11 16:16:03 2003
-@@ -38,7 +38,7 @@
+--- Network.cpp.orig	Sun Aug 17 20:43:03 2003
++++ Network.cpp	Mon Nov 24 10:35:27 2003
+@@ -36,13 +36,15 @@
+ #include <netinet/tcp.h>
+ #endif
  
- #if defined(WIN32) || defined(__FreeBSD__)
+-#if defined(WIN32) || defined(__FreeBSD__)
++#if defined(WIN32)
  
--typedef int socklen_t;		/* mimic Penguin's typedef */
-+//typedef int socklen_t;		/* mimic Penguin's typedef */
+ typedef int socklen_t;		/* mimic Penguin's typedef */
  
  #else	/* ! WIN32 */
  
-@@ -164,7 +164,7 @@
++#if !defined(__FreeBSD__)
+ #define closesocket(FD) close(FD)
++#endif
+ 
+ #endif
+ 
+@@ -169,7 +171,6 @@
  
    char *b = buf;
    long millitm;
 -  b = ReadLong( b, tb->time );
-+  b = ReadLong( b, (long)tb->time );
    b = ReadLong( b, millitm );
    tb->millitm = (unsigned short)millitm;
  
