5ec92313ab862f07611e3e9199a7c6bc99d55438
ijliao <ijliao@FreeBSD.org>
2003-12-05 00:48:27 +0000

    upgrade to 1.0.13

---
+++ devel/commoncpp/files/patch-config:ltmain.sh
@@ -0,0 +1,15 @@
+--- config/ltmain.sh.orig	Tue Dec  2 09:24:40 2003
++++ config/ltmain.sh	Tue Dec  2 09:24:52 2003
+@@ -5428,10 +5428,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
