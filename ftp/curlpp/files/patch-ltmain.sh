c6609ee04ff0c907003236b74d350c8ab112d897
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-12-05 01:01:58 +0000

    add curlpp 0.3.0 A C++ wrapper for libcurl
    PR:		59891

---
+++ ftp/curlpp/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Nov 27 11:22:47 2003
++++ ltmain.sh	Thu Nov 27 11:22:57 2003
+@@ -4344,10 +4344,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
