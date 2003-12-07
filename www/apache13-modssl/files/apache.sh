afe267dc21ba6ad536cee0fc93f8dc72c33e3242
Dirk Meyer <dinoex@FreeBSD.org>
2003-12-07 16:48:54 +0000

    - option WITH_APACHE_IPV6 PR:             42427 Submitted by:   sec@42.org
    - fix Usage

--- www/apache13-modssl/files/apache.sh
+++ www/apache13-modssl/files/apache.sh
@@ -8,7 +8,8 @@ stop)
 	[ -r /var/run/httpd.pid ] && %%PREFIX%%/sbin/apachectl stop > /dev/null && echo -n ' apache'
 	;;
 *)
-	echo "Usage: `basename $0` {start|stop}" >&2
+	echo "Usage: ${0##*/} { start | stop }" >&2
+	exit 64
 	;;
 esac
 
