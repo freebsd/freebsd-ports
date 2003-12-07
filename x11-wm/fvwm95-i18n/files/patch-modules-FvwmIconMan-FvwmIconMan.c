94b1d4c56132e49f72a7f7a36f848e1bcb5a6455
pav <pav@FreeBSD.org>
2003-12-07 21:10:01 +0000

    - Unbreak on 5.x (add patch from x11-wm/fvwm95)
    PR:		ports/60017
    Submitted by:	Volker Stolz <vs@foldr.org>

---
+++ x11-wm/fvwm95-i18n/files/patch-modules-FvwmIconMan-FvwmIconMan.c
@@ -0,0 +1,34 @@
+--- modules/FvwmIconMan/FvwmIconMan.c.orig	Thu Oct 30 12:53:01 2003
++++ modules/FvwmIconMan/FvwmIconMan.c	Thu Oct 30 13:10:36 2003
+@@ -315,6 +315,23 @@
+     return copy_string (s1, s2);
+ }
+ 
++#if __GNUC__ > 2
++#define SET_MANAGER(manager,field,value)                           \
++   do {                                                            \
++     int id = manager;                                             \
++     if (id == -1) {                                               \
++       for (id = 0; id < globals.num_managers; id++) {             \
++	 globals.managers[id].field = value;                     \
++       }                                                           \
++     }                                                             \
++     else if (id < globals.num_managers) {                         \
++       globals.managers[id].field = value;                       \
++     }                                                             \
++     else {                                                        \
++       ConsoleMessage ("Internal error in SET_MANAGER: %d\n", id); \
++     }                                                             \
++   } while (0)
++#else 
+ #define SET_MANAGER(manager,field,value)                           \
+    do {                                                            \
+      int id = manager;                                             \
+@@ -330,6 +347,7 @@
+        ConsoleMessage ("Internal error in SET_MANAGER: %d\n", id); \
+      }                                                             \
+    } while (0)
++#endif /* GCC 2 */
+ 
+ static void read_in_resources (char *file)
+ {
