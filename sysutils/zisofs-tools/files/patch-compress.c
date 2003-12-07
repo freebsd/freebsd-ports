2426daab19cfb0791c81cad87366ab9107f01182
pav <pav@FreeBSD.org>
2003-12-07 22:11:23 +0000

    Add zisofs-tools, a set of tools for zisofs filesystem, an extension of the ISO9660 filesystem that allows transparent compression of files, as supported by recent Linux kernels (2.4.14 or later)
    PR:		ports/60023
    Submitted by:	chip <chip-set@mail.ru>

---
+++ sysutils/zisofs-tools/files/patch-compress.c
@@ -0,0 +1,10 @@
+--- compress.c.orig	Mon Nov 24 13:26:57 2003
++++ compress.c	Mon Nov 24 13:27:06 2003
+@@ -13,6 +13,7 @@
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
++#include <time.h>
+ #include <utime.h>
+ #include <unistd.h>
+ #include <zlib.h>
