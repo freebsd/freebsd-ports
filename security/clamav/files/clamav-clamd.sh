71b93e612e86c1b84511567fc9b56892829fa50c
Alexander Leidinger <netchild@FreeBSD.org>
2003-12-08 14:06:49 +0000

    BEWARE: As rcoder(8) isn't present on FreeBSD 4 but clamd must be         be started before clamav-milter the startup-script for clamd         must alphabetically arranged before the startup-script for         clamav-milter. It was decided to use the clamd statup-scrtipt         from security/clamav-devel (clamav-clamd.sh), so users running         an older version of security/clamav have to change         'clamd_enable="YES"' to 'clamav_clamd_enable="YES"' in         /etc/rc.conf. On the other hand this changes makes it easier         to switch between security/clamav and security/clamav-devel.
    - Respect PTHREAD_CFLAGS.
    - Use USE_LIBTOOL_VER=14 as the libtool shipping with clamav 0.65
      erroneously links against libc _and_ libc_r on FreeBSD 4.
    - Don't link against libcipher on FreeBSD < 501100, clamav doesn't
      depend on it. The linker warnings suggesting that it is required
      were triggered by linking against both, libc and libc_r.
    - Unconditionally depend on math/libgmp4. Otherwise, if libgmp4 is
      already installed on a FreeBSD 4-STABLE system compilation of
      security/clamav fails, as the headers of libgmp4 are used but the
      base libgmp is used for linking.
    - Fix generation of clamav-milter.8 when building with WITH_MILTER
      (part of new files/patch-clamav-milter::Makefile.in).
    - Don't USE_GETOPT_LONG when compiling with WITH_MILTER, clamav
      uses its own getopt_long() regardless if a devel/libgnugetopt is
      installed or the version in the base of FreeBSD 5 is present.
    - Allow compilation of WITH_MILTER with mail/sendmail.
    - Install a startup-script (taken from security/clamav-devel with some
      fixes in comments) for clamav-milter when compiled with WITH_MILTER.
    - In pkg-descr remove the note that clamav-milter doesn't build on
      FreeBSD 4 with base gcc, it builds fine there. Mention that
      clamav-milter can be optionally used (taken from pkg-descr of
      security/clamav-devel).
    - Fix compilation when an older version of security/clamav is installed.
      If there's an older version of libclamav installed clamav 0.65 will
      link against it instead of the newly built one. This is not a problem
      if the installed version is compatible with the new one as libclamav
      gets linked dynamically and the installed version will be replaced
      with the new one. However, if the older version is incompatible
      linking of clamav 0.65 fails. This problem was discovered by
      Jan-Peter Koopmann Jan-Peter.Koopmann@seceidos.de and the fix
      (most of the new patches in the files directory) confirmed working.
    - In clamav-milter replace the path to the used sendmail executable with
      the location of the mailwrapper(8) (/usr/sbin/sendmail). Obtained
      from security/clamav-devel.
    
    PR:		59948
    Submitted by:	Marius Strobl <marius@alchemy.franken.de>
    Approved by:	maintainer

--- security/clamav/files/clamav-clamd.sh
+++ security/clamav/files/clamav-clamd.sh
@@ -11,14 +11,14 @@
 #
 # Add the following lines to /etc/rc.conf to enable clamd:
 #
-#clamd_enable="YES"
+#clamav_clamd_enable="YES"
 #
 # See clamd(8) for flags
 #
 
 . %%RC_SUBR%%
 
-name=clamd
+name=clamav_clamd
 rcvar=`set_rcvar`
 
 command=%%PREFIX%%/sbin/clamd
@@ -30,10 +30,10 @@ start_precmd=start_precmd
 
 start_precmd()
 {
-       if [ -S "$clamd_socket" ]; then
-       warn "Stale socket $clamd_socket removed."
-       rm "$clamd_socket"
-       fi
+	if [ -S "$clamd_socket" ]; then
+		warn "Stale socket $clamd_socket removed."
+		rm "$clamd_socket"
+	fi
 }
 
 stop_postcmd=stop_postcmd
@@ -45,9 +45,9 @@ stop_postcmd()
 
 # set defaults
 
-clamd_enable=${clamd_enable:-"NO"}
-clamd_flags=${clamd_flags:-""}
-clamd_socket=${clamd_socket:-"%%CLAMD_SOCKET%%"}
+clamav_clamd_enable=${clamav_clamd_enable:-"NO"}
+clamav_clamd_flags=${clamav_clamd_flags:-""}
+clamav_clamd_socket=${clamav_clamd_socket:-"%%CLAMAV_CLAMD_SOCKET%%"}
 
 load_rc_config $name
 run_rc_command "$1"
