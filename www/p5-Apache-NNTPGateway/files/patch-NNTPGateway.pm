a00c31de615bd3c79e6728f1521b9e8d70933826
Sergey Skvortsov <skv@FreeBSD.org>
2003-12-08 17:40:51 +0000

    Updated to 0.9

--- www/p5-Apache-NNTPGateway/files/patch-NNTPGateway.pm
+++ www/p5-Apache-NNTPGateway/files/patch-NNTPGateway.pm
@@ -1,19 +1,13 @@
-*** NNTPGateway.pm.orig	Mon Jan 15 18:13:44 2001
---- NNTPGateway.pm	Mon Jan 15 18:14:10 2001
-***************
-*** 99,105 ****
-  # MESSAGES, stuff that is printed on the users's screen...  
-  # The current default language: Should be an entry in Messages_Map &
-  # LANGS_OK
-! my $USR_LANG = 'fr';
-  # Allowed languages choice.
-  my %LANGS_OK = ( 'fr' => 1, 'en' => 1 );
-  # All messages that could be printed.
---- 99,105 ----
-  # MESSAGES, stuff that is printed on the users's screen...  
-  # The current default language: Should be an entry in Messages_Map &
-  # LANGS_OK
-! my $USR_LANG = 'en';
-  # Allowed languages choice.
-  my %LANGS_OK = ( 'fr' => 1, 'en' => 1 );
-  # All messages that could be printed.
+--- NNTPGateway.pm.orig	Mon Dec  8 20:42:03 2003
++++ NNTPGateway.pm	Mon Dec  8 20:42:19 2003
+@@ -118,8 +118,8 @@
+ 
+ # MESSAGES, stuff that is printed on the user's screen... The current default
+ # language: Should be an entry in Messages_Map & LANGS_OK
+-my $USR_LANG = 'fr';
+-#my $USR_LANG = 'en';
++#my $USR_LANG = 'fr';
++my $USR_LANG = 'en';
+ # Allowed languages choice.
+ my %LANGS_OK = ( 'fr' => 1, 'en' => 1 );
+ 
