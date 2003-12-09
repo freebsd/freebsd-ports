cf2811554e736a939085036e703f383de252ce25
marcus <marcus@FreeBSD.org>
2003-12-09 21:57:28 +0000

    Fix build on -STABLE.
    Reported by:	Mike Harding <mvh@ix.netcom.com>

---
+++ x11/gnomemag/files/patch-magnifier_zoom-region.c
@@ -0,0 +1,10 @@
+--- magnifier/zoom-region.c.orig	Tue Dec  9 16:54:22 2003
++++ magnifier/zoom-region.c	Tue Dec  9 16:56:18 2003
+@@ -20,6 +20,7 @@
+  * Boston, MA 02111-1307, USA.
+  */
+ 
++#include <sys/time.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <popt.h>
