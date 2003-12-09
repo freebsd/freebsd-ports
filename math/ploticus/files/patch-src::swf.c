468f16d221bb67db691008c26a06ec0e1e727a28
Kirill Ponomarev <krion@FreeBSD.org>
2003-12-09 19:52:19 +0000

    - Update to version 2.20
    PR:		60071
    Submitted by:	Ports Fury

--- math/ploticus/files/patch-src::swf.c
+++ math/ploticus/files/patch-src::swf.c
@@ -1,11 +1,11 @@
---- swf.c.orig	Tue May 20 02:55:58 2003
-+++ swf.c	Wed Jun  4 09:41:53 2003
-@@ -80,7 +80,7 @@
- static void swf_TX(char *s);
- static void swf_LS(char *s);
- static void swf_PO(char *s);
--static void swf_FT(char *s);
-+/* static void swf_FT(char *s); */
- static void swf_TR(char *s);
+--- swf.c.orig	Tue Oct 28 04:45:58 2003
++++ swf.c	Mon Dec  8 00:27:51 2003
+@@ -15,7 +15,7 @@
+ */
+ #ifndef NOSWF
  
- /* ============================= */
+-#include MAX_D_ROWS 1000
++#define MAX_D_ROWS 1000
+ 
+ #include <stdio.h>
+ #include <math.h>
