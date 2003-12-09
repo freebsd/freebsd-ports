9188d5103458118751ccdb6f0f4d0f4ba44fa0f5
Pav Lucistnik <pav@FreeBSD.org>
2003-12-09 00:50:45 +0000

    - On many systems, ttmkfdir was the only reason to install freetype1.   Here is ttmkfdir that uses freetype2 as a backend.
    PR:		ports/60055
    Submitted by:	Thierry Thomas <thierry@pompo.net>

---
+++ x11-fonts/ttmkfdir/files/patch-ttf.h
@@ -0,0 +1,11 @@
+--- ttf.h.orig	Wed Mar 13 19:16:38 2002
++++ ttf.h	Sat Dec  6 23:47:35 2003
+@@ -13,6 +13,8 @@
+ #include "util.h"
+ #include "encoding.h"
+ 
++using namespace std;
++
+ namespace ttf {
+ 
+     class Face {
