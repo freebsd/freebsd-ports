132aab81228c918cb24e62bfc2cc10176e001606
Pav Lucistnik <pav@FreeBSD.org>
2003-12-09 23:48:28 +0000

    - Update to 1.18i
    PR:		ports/60090
    Submitted by:	Guido Falsi <mad@madpilot.net> (maintainer)

--- games/traindirector/files/patch-run.c
+++ games/traindirector/files/patch-run.c
@@ -1,21 +1,23 @@
---- run.c.orig	Fri Jul 18 15:53:36 2003
-+++ run.c	Fri Jul 18 15:56:59 2003
-@@ -20,9 +20,13 @@
+--- run.c.orig	Tue Dec  9 22:02:20 2003
++++ run.c	Tue Dec  9 22:04:51 2003
+@@ -20,11 +20,15 @@
  
  #include <stdio.h>
  #include <string.h>
 -#include <malloc.h>
 +#include <stdlib.h>
+ #include <math.h>
  #include "ask.h"
  #include "trsim.h"
-+
+ 
 +#if (defined(__unix__) || defined(unix)) && !defined(USG)
 +#include <sys/param.h>
 +#endif
++
+ #define	HOMAN_030330
+ #define	MERGE_TRAINS 1
  
- int	assign_ok = 1;
- 
-@@ -56,7 +60,11 @@
+@@ -60,7 +64,11 @@
  {
  	if(i >= v->size) {
  	    printf("Bad index %d: only %d elements in vector!\n", i, v->size);
@@ -27,7 +29,7 @@
  	}
  	return v->ptr[i];
  }
-@@ -65,7 +73,11 @@
+@@ -69,7 +77,11 @@
  {
  	if(i >= v->size) {
  	    printf("Bad index %d: only %d elements in vector!\n", i, v->size);
