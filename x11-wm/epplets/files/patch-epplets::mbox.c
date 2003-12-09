5596b8898f1b94f833d0936018927297766c13df
Kirill Ponomarev <krion@FreeBSD.org>
2003-12-09 20:31:10 +0000

    - Update to version 0.7
    PR:		60076
    Submitted by:	Ports Fury

---
+++ x11-wm/epplets/files/patch-epplets::mbox.c
@@ -0,0 +1,10 @@
+--- epplets/mbox.c.orig	Tue Apr  9 05:39:38 2002
++++ epplets/mbox.c	Mon Dec  8 02:46:36 2003
+@@ -32,7 +32,6 @@
+ #include <string.h>
+ #include <time.h>
+ #include <utime.h>
+-#include <malloc.h>
+ #include <limits.h>
+ 
+ #define ISSPACE(c) isspace((unsigned char) c)
