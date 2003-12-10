10a6c5cc79b05f9bf2456e61fbef8a16974c7f88
arved <arved@FreeBSD.org>
2003-12-10 14:56:20 +0000

    Fix a bug leading to SIG11 in the child process
    Submitted by:	Herbert Straub
    Reported by:	Konstantin Nikonenko

--- news/newscache/files/patch-NewsCache.cc
+++ news/newscache/files/patch-NewsCache.cc
@@ -1,10 +1,19 @@
---- src/NewsCache.cc.orig      Sat Feb 23 18:06:38 2002
-+++ src/NewsCache.cc   Sat Feb 23 18:06:48 2002
-@@ -18,7 +18,6 @@
+--- src/NewsCache.cc.orig	Fri Oct 24 12:12:20 2003
++++ src/NewsCache.cc	Wed Dec 10 15:47:46 2003
+@@ -24,7 +24,6 @@
   */
  #include "config.h"
-
+ 
 -#include <crypt.h>
  #include <ctype.h>
  #include <unistd.h>
  #include <string.h>
+@@ -2024,7 +2023,7 @@
+ 		}
+ 
+ 		cmdp = clt.client_command_map.find(argv[0]);
+-		if (cmdp == end || !cmdp->second->func) {
++		if (cmdp == clt.client_command_map.end() || !cmdp->second->func) {
+ 			slog.p(Logger::Notice) << clt.client_logname
+ 			    << " unrecognized " << oreq << "\n";
+ 			(*clt.co) << "500 What?\r\n";
