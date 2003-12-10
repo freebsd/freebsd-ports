cc86e0a1997ee240f7e56cbd6daa88222819d7d3
ijliao <ijliao@FreeBSD.org>
2003-12-10 04:04:08 +0000

    add ulxmlrpcpp 1.2.4 beta 5 A ultra lightweight xml-rpc library written in C++

---
+++ net/ulxmlrpcpp/files/patch-config:ltmain.sh
@@ -0,0 +1,15 @@
+--- config/ltmain.sh.orig	Sun Dec  8 16:53:29 2002
++++ config/ltmain.sh	Wed Dec 10 11:43:13 2003
+@@ -4232,10 +4232,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
