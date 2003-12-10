9d952d5c992fbd1c44287fd3ffa0e9c3839c275d
pav <pav@FreeBSD.org>
2003-12-10 22:17:03 +0000

    - Replace our patch with more correct one obtained from author - Add faster master site
    PR:		ports/60118
    Submitted by:	Scott M. Likens <damm@yazzy.org> (maintainer)

--- irc/scrollz/files/patch-source-ircaux.c
+++ irc/scrollz/files/patch-source-ircaux.c
@@ -1,14 +1,30 @@
 --- source/ircaux.c.orig	Thu Oct  3 21:16:38 2002
-+++ source/ircaux.c	Tue Nov 18 15:58:26 2003
-@@ -869,9 +869,11 @@
- 		return -10;
- #else
++++ source/ircaux.c	Wed Dec 10 23:10:54 2003
+@@ -861,20 +861,19 @@
+ 	err = getaddrinfo(localhost, localport, &hintsx, &res0x);
+ 
+ 	if (err != 0)
+-#if defined(__linux__) && 0
+-		/*
+-		 * Due to bug in glibc implementation in getaddrinfo() we always
+-		 * return -10.   This will be fixed, soon
+-		 */
+-		return -10;
+-#else
  	{
-+#ifndef __FreeBSD__
++		# ifndef EAI_ADDRFAMILY
++		#  ifdef EAI_FAMILY
++		#   define EAI_ADDRFAMILY EAI_FAMILY
++		#  else
++		#   error "no EAI_ADDRFAMILY or EAI_FAMILY"
++		#  endif
++		# endif
  		if (err == EAI_ADDRFAMILY)
  			return -10;
  		else
-+#endif
  			return -2;
  	}
- #endif
+-#endif
+ 	err = -1;
+ 	for (resx = res0x; resx; resx = resx->ai_next)
+ 	{
