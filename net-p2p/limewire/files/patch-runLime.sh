2121b505eb8bb495bd97b1c93dc98c1e0a4dacf8
glewis <glewis@FreeBSD.org>
2003-12-11 17:22:46 +0000

    . Update to 3.6.15. . Set the maintainer to the submitter.
    PR:		59712
    Submitted by:	past@noc.ntua.gr

--- net-p2p/limewire/files/patch-runLime.sh
+++ net-p2p/limewire/files/patch-runLime.sh
@@ -1,15 +1,16 @@
---- runLime.sh.orig	Wed May 15 14:32:03 2002
-+++ runLime.sh	Tue Jul 16 22:25:34 2002
-@@ -7,8 +7,9 @@
+--- runLime.sh.orig	Mon Jun  9 22:45:57 2003
++++ runLime.sh	Wed Nov 26 15:30:04 2003
+@@ -7,8 +7,10 @@
  # work around some limitations in the JDK1.3.  For more information
  # see http://java.sun.com/j2se/1.3/relnotes.html#vm
  #
--CLASSPATH=".:collections.jar:xerces.jar:jl011.jar:MessagesBundles.jar"
+-CLASSPATH=".:collections.jar:xerces.jar:jl011.jar:MessagesBundles.jar:themes.jar:logicrypto.jar:GURL.jar"
 -export CLASSPATH
 +CLASSPATH="%%JAR_DIR%%"
  J2SE_PREEMPTCLOSE=1
  export J2SE_PREEMPTCLOSE
--java -jar RunLime.jar
+-java -jar LimeWire.jar
++cd ${CLASSPATH}
 +exec %%LOCALBASE%%/bin/javavm \
-+	-cp "${CLASSPATH}/collections.jar:${CLASSPATH}/xerces.jar:${CLASSPATH}/jl011.jar:${CLASSPATH}/MessagesBundles.jar" \
++	-cp "${CLASSPATH}/collections.jar:${CLASSPATH}/xerces.jar:${CLASSPATH}/jl011.jar:${CLASSPATH}/MessagesBundles.jar:${CLASSPATH}/themes.jar:${CLASSPATH}/logicrypto.jar:${CLASSPATH}/GURL.jar" \
 +	-jar ${CLASSPATH}/LimeWire.jar
