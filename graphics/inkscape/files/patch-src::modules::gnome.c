b8079eada6da1ff0845a870511c1bd35f903e596
bland <bland@FreeBSD.org>
2003-12-11 15:36:02 +0000

    - Enable GNOME printing support. - Fix PLIST.

---
+++ graphics/inkscape/files/patch-src::modules::gnome.c
@@ -0,0 +1,10 @@
+--- src/modules/gnome.c.orig	Thu Dec 11 23:52:25 2003
++++ src/modules/gnome.c	Thu Dec 11 23:52:44 2003
+@@ -31,6 +31,7 @@
+ #include <libgnomeprintui/gnome-print-dialog.h>
+ 
+ #include "helper/sp-intl.h"
++#include "helper/canvas-bpath.h"
+ #include "enums.h"
+ #include "document.h"
+ #include "style.h"
