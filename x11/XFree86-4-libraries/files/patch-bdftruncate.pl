d3adce9ee0ec9a363d613b8be121d70d0ed977ef
anholt <anholt@FreeBSD.org>
2003-12-12 22:55:31 +0000

    Use #!/usr/bin/env perl instead of #!/usr/bin/perl to increase the chances of success.

---
+++ x11/XFree86-4-libraries/files/patch-bdftruncate.pl
@@ -0,0 +1,13 @@
+Index: fonts/util/bdftruncate.pl
+===================================================================
+RCS file: /home/ncvs/xfree/xc/fonts/util/bdftruncate.pl,v
+retrieving revision 1.3
+diff -u -r1.3 bdftruncate.pl
+--- fonts/util/bdftruncate.pl	12 Oct 2002 16:06:42 -0000	1.3
++++ fonts/util/bdftruncate.pl	12 Dec 2003 22:33:24 -0000
+@@ -1,4 +1,4 @@
+-#!/usr/bin/perl
++#!/usr/bin/env perl
+ #
+ # bdftruncate.pl -- Markus Kuhn <mkuhn@acm.org>
+ #
