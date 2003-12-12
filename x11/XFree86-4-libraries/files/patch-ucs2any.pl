d9b1664b1839a9d0450c4c7ba2c352f5b3ae69ca
Eric Anholt <anholt@FreeBSD.org>
2003-12-12 22:55:31 +0000

    Use #!/usr/bin/env perl instead of #!/usr/bin/perl to increase the chances of success.

---
+++ x11/XFree86-4-libraries/files/patch-ucs2any.pl
@@ -0,0 +1,13 @@
+Index: fonts/util/ucs2any.pl
+===================================================================
+RCS file: /home/ncvs/xfree/xc/fonts/util/ucs2any.pl,v
+retrieving revision 1.5
+diff -u -r1.5 ucs2any.pl
+--- fonts/util/ucs2any.pl	12 Oct 2002 16:06:42 -0000	1.5
++++ fonts/util/ucs2any.pl	12 Dec 2003 22:25:18 -0000
+@@ -1,4 +1,4 @@
+-#!/usr/bin/perl
++#!/usr/bin/env perl
+ #
+ # ucs2any.pl -- Markus Kuhn <mkuhn@acm.org>
+ #
