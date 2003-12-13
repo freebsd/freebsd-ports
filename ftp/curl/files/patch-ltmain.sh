e317cb982e4becbcd37deee23ea7d37db9d58d97
Peter Pentchev <roam@FreeBSD.org>
2003-12-13 08:32:10 +0000

    Update to 7.10.8 and remove the BROKEN mark for 64-bit arches.
    PR:		60105
    Submitted by:	ijliao

---
+++ ftp/curl/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Wed Dec 10 15:20:29 2003
++++ ltmain.sh	Wed Dec 10 15:20:39 2003
+@@ -5425,10 +5425,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
