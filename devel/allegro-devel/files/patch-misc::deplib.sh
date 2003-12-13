0e2a89a8d7e2968e1e06296a837d8d56c1763bb5
pav <pav@FreeBSD.org>
2003-12-13 01:41:30 +0000

    - Update to 4.1.12 - Add knob to enable to arts sound driver - Use PORTSDOC - Use INFO
    PR:		ports/60193
    Submitted by:	Thierry Thomas <thierry@pompo.net>

--- devel/allegro-devel/files/patch-misc::deplib.sh
+++ devel/allegro-devel/files/patch-misc::deplib.sh
@@ -1,5 +1,5 @@
---- misc/deplib.sh.orig	Wed Oct 23 08:08:27 2002
-+++ misc/deplib.sh	Sun Nov 10 00:40:21 2002
+--- misc/deplib.sh.orig	Sun Nov  9 19:43:07 2003
++++ misc/deplib.sh	Thu Dec 11 22:32:16 2003
 @@ -10,7 +10,7 @@
      staticlib="lib${1}.a"
      staticobj="\$(${2}_OBJECTS)"
@@ -13,7 +13,7 @@
      echo "	rm -f \$@"
      # gf: This bit is obviously gcc-specific
      # eb: Yes, but the GNU C Compiler doesn't always spell 'gcc'
--    echo "	\$(CC) -shared -o \$@ ${shareobj} \$(LDFLAGS) -Wl,-h,lib${1}.so.\$(shared_major_minor) \$(LIBS)"
+-    echo "	\$(CC) -shared -o \$@ ${shareobj} ${3} \$(LDFLAGS) -Wl,-h,lib${1}.so.\$(shared_major_minor) \$(LIBS)"
 +    echo "	\$(CC) -shared -o \$@ ${shareobj} \$(LDFLAGS) -Wl,-h,lib${1}.so.\$(shared_version) \$(LIBS)"
      echo ""
      echo "\$(LIBDIR)/${unsharelib}: ${unshareobj}"
