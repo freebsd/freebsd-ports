7f5493fe8c870749fc8a6231e23c7500151ca822
pav <pav@FreeBSD.org>
2003-12-14 13:03:37 +0000

    - Update to 2.2.0
    PR:		ports/60210
    Submitted by:	Thierry Thomas <thierry@pompo.net>

--- graphics/freeglut/files/patch-progs::demos::Lorenz::lorenz.c
+++ graphics/freeglut/files/patch-progs::demos::Lorenz::lorenz.c
@@ -1,11 +1,11 @@
---- progs/demos/Lorenz/lorenz.c.orig	Thu Jul 10 13:46:34 2003
-+++ progs/demos/Lorenz/lorenz.c	Mon Sep 22 21:52:40 2003
+--- progs/demos/Lorenz/lorenz.c.orig	Thu Dec 11 02:32:08 2003
++++ progs/demos/Lorenz/lorenz.c	Sat Dec 13 10:19:26 2003
 @@ -32,7 +32,7 @@
  #include <string.h>
  #include <math.h>
  #include <time.h>
 -#include <GL/freeglut.h>
 +#include "../../../include/GL/freeglut.h"
- 
- 
- /************************************** Defined Constants ***************************************/
+ #ifdef WIN32
+ #include <crtdbg.h>  // DUMP MEMORY LEAKS
+ #endif
