d19a9c06ea86a5a7a55cc777eab235026262abaa
marcus <marcus@FreeBSD.org>
2003-12-14 16:47:57 +0000

    Update to 1.6b, and add Peter's amd64 patches from Firebird.  For a list of what is changed in 1.6b, checkout the release notes at:
    http://www.mozilla.org/releases/mozilla1.6b/

--- www/seamonkey2/files/patch-security::coreconf::FreeBSD.mk
+++ www/seamonkey2/files/patch-security::coreconf::FreeBSD.mk
@@ -1,9 +1,6 @@
-
-$FreeBSD$
-
---- security/coreconf/FreeBSD.mk.orig	Sat Mar  9 00:17:36 2002
-+++ security/coreconf/FreeBSD.mk	Tue Mar 12 17:19:15 2002
-@@ -35,9 +35,7 @@
+--- security/coreconf/FreeBSD.mk.orig	Wed Mar 26 20:17:25 2003
++++ security/coreconf/FreeBSD.mk	Sun Dec 14 11:38:37 2003
+@@ -35,16 +35,18 @@
  
  include $(CORE_DEPTH)/coreconf/UNIX.mk
  
@@ -14,7 +11,18 @@ $FreeBSD$
  RANLIB			= ranlib
  
  ifeq ($(OS_TEST),alpha)
-@@ -70,7 +68,7 @@
+ CPU_ARCH		= alpha
+ else
++ifeq ($(OS_TEST),amd64)
++CPU_ARCH		= amd64
++else
+ CPU_ARCH		= x86
+ endif
++endif
+ 
+ OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
+ 
+@@ -71,7 +73,7 @@
  DLL_SUFFIX		= so.1.0
  endif
  
