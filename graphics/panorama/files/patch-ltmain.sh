7f29f09f5694ade8d7e1a1f868a8b02fd711c0ac
Kirill Ponomarev <krion@FreeBSD.org>
2003-12-15 21:09:56 +0000

    - Fix LIB_DEPENDS - Support CFLAGS/CXXFLAGS properly - Fix pkg-plist
    PR:		60270
    Submitted by:	Ports Fury

---
+++ graphics/panorama/files/patch-ltmain.sh
@@ -0,0 +1,33 @@
+--- ltmain.sh.orig	Wed Feb 12 11:23:37 2003
++++ ltmain.sh	Fri Dec 12 14:34:27 2003
+@@ -944,6 +944,7 @@
+ 	;;
+ 
+       -avoid-version)
++	build_old_libs=no
+ 	avoid_version=yes
+ 	continue
+ 	;;
+@@ -2408,6 +2409,9 @@
+ 	  *-*-netbsd*)
+ 	    # Don't link with libc until the a.out ld.so is fixed.
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test $build_libtool_need_lc = "yes"; then
+@@ -4175,10 +4179,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
