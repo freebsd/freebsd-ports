ec8c0c1100071f2f7ee8a261741acba73251db1e
Sergei Kolobov <sergei@FreeBSD.org>
2003-12-15 16:39:43 +0000

    - Update from 2.0.0b9 to 2.0.0 - Add WITH_ZONECHECK_CGI knob
    While I'm here:
    - Bump PORTEPOCH
    - Re-sort pkg-plist
    
    PR:		59899
    Submitted by:	maintainer

--- dns/zonecheck/files/patch-installer.rb
+++ dns/zonecheck/files/patch-installer.rb
@@ -1,11 +1,32 @@
---- installer.rb.orig	Mon Oct 27 17:42:01 2003
-+++ installer.rb	Tue Nov  4 13:06:14 2003
-@@ -190,8 +190,6 @@
-     def rule_all
- 	inst_common ; patch_common 
- 	inst_cli
--	inst_cgi    ; patch_cgi
--	inst_doc
+--- installer.rb.orig	Mon Nov 17 18:59:32 2003
++++ installer.rb	Tue Dec  2 00:46:09 2003
+@@ -55,16 +55,19 @@
+ 	ENV['DOCDIR'    ] ||= "#{ENV['PREFIX']}/share/doc"
+ 	ENV['ETCDIR'    ] ||= "#{ENV['PREFIX']}/etc"
+ 	ENV['CGIDIR'    ] ||= "#{ENV['LIBEXEC']}/#{ENV['PROGNAME']}/cgi-bin"
++	ENV['WWWDIR'    ] ||= "#{ENV['PREFIX']}/www"
+ 
+ 	ENV['VERSION'	] ||= VERSION
+ 
+ 	@installdir    = "#{ENV['LIBEXEC']}/#{ENV['PROGNAME']}"
+ 	@confdir       = "#{ENV['ETCDIR']}/#{ENV['PROGNAME']}#{ENV['ETCDIST']}"
+ 	@zc            = "#{@installdir}/zc/zc.rb"
++	@wwwdir    = "#{ENV['WWWDIR']}/#{ENV['PROGNAME']}"
+ 
+ 	@ch_installdir = "#{ENV['CHROOT']}#{@installdir}"
+ 	@ch_confdir    = "#{ENV['CHROOT']}#{@confdir}"
+ 	@ch_zc         = "#{ENV['CHROOT']}#{@zc}"
++	@ch_wwwdir = "#{ENV['CHROOT']}#{@wwwdir}"
+ 
+ 	@verbose       = true
      end
-     def rule_cli
- 	inst_common ; patch_common
+@@ -185,7 +188,8 @@
+ 
+     def inst_cgi
+ 	puts "==> Installing HTML pages"
+-	cp_r "www", @ch_installdir,			:verbose => @verbose
++	mkdir_p	@ch_wwwdir,				:verbose => @verbose
++	cp_r "www", @ch_wwwdir,			:verbose => @verbose
+ 	puts
+ 
+ 	puts "==> Installing CGI"
