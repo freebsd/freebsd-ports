cd5ea7183cf112dfeca76758abceae121234d107
Kirill Ponomarev <krion@FreeBSD.org>
2003-12-17 16:20:30 +0000

    - Update to version 2.9.4
    PR:		60335
    Submitted by:	Ports Fury

---
+++ finance/qhacc/files/patch-lib::qhacc.cpp
@@ -0,0 +1,11 @@
+--- lib/qhacc.cpp.orig	Sun Sep 28 01:08:13 2003
++++ lib/qhacc.cpp	Sun Nov  2 10:53:38 2003
+@@ -31,7 +31,7 @@
+ #include "plugins.h"
+ #include "qhacc.moc"
+ 
+-#include <math.h>
++#include <cmath>
+ 
+ #include <qdir.h>
+ #include <qfile.h>
