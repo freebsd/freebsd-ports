e4789de0d903aa2669ad4435a6924e66b9b8d6fe
Kirill Ponomarev <krion@FreeBSD.org>
2003-12-17 15:19:51 +0000

    Add vpnc 0.2,
    VPNC - Client for Cisco 3000 VPN Concentrator
    
    A VPN client compatible with Cisco's EasyVPN equipment.
    Supports IPSec (ESP) with Mode Configuration and Xauth.
    Supports only shared-secret IPSec authentication, 3DES, MD5,
    and IP tunneling.  It runs entirely in userspace
    
    PR:		60283
    Submitted by:	Christian Lackas

---
+++ security/vpnc/files/patch-vpnc.c
@@ -0,0 +1,11 @@
+--- vpnc.c.orig	Tue Dec 16 00:25:01 2003
++++ vpnc.c	Tue Dec 16 00:25:33 2003
+@@ -2007,7 +2007,7 @@
+     else
+       read_config_file (argv[i], config, 0);
+ 
+-  read_config_file ("/etc/vpnc.conf", config, 1);
++  read_config_file ("/usr/local/etc/vpnc.conf", config, 1);
+ 
+   if (!config[CONFIG_IKE_DH])
+     config[CONFIG_IKE_DH] = "dh2";
