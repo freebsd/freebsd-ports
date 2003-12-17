8cc80b8c29e26056b6702fdee326d9b8503cf2f6
perky <perky@FreeBSD.org>
2003-12-17 01:26:48 +0000

    Update to 2.02

--- audio/py-xmms/files/patch-setup.py
+++ audio/py-xmms/files/patch-setup.py
@@ -1,38 +1,25 @@
---- setup.py.orig	Sun Nov  9 12:15:24 2003
-+++ setup.py	Mon Nov 10 20:55:24 2003
+--- setup.py.orig	Wed Nov 19 19:52:54 2003
++++ setup.py	Wed Dec 17 10:24:39 2003
 @@ -20,7 +20,7 @@
  # Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
  # MA 02111-1307, USA.
  
 -import os, string, sys, distutils.dist
-+import os, string, sys, re, distutils.dist
++import os, string, sys, distutils.dist, re
  from distutils.core import setup, Extension
  
  # Note:
-@@ -30,9 +30,21 @@
- #   run the Distutils from Python 2.1, you will get License: UNKNOWN. This
- #   problem does not appear with the versions included in Python 2.2 and 2.3.
+@@ -32,7 +32,8 @@
  
-+def patch4gcc3(fname):
-+    instr = 0
-+    cont = open(fname).readlines()
-+    of = open(fname, 'w')
-+    for l in cont:
-+        instr = (instr+len(re.findall(r'(^")|([^\\]")', l))) % 2
-+        print >>of, l[:-1] + ((not l.startswith('/*') and instr) and "\\n\\" or "")
-+if 'patch' in sys.argv:
-+    patch4gcc3('src/_xmmscontrolmodule.c')
-+    raise SystemExit
-+
  PACKAGE = "pyxmms"
- VERSION = "2.00"
+ VERSION = "2.02"
 -GLIB_CONFIG = "glib-config"
 +GLIB_CONFIG = os.environ['GLIB_CONFIG']
 +XMMS_CONFIG = os.environ['XMMS_CONFIG']
  
  
  def get_glib_config():
-@@ -70,13 +82,16 @@
+@@ -70,13 +71,16 @@
  def setup_args():
      """Craft appropriate arguments for distutils.setup."""
      (glib_include_dirs, glib_compile_args, glib_link_args) = get_glib_config()
@@ -51,7 +38,7 @@
                               extra_link_args=glib_link_args)]
  
      if sys.hexversion < 0x02020000:
-@@ -92,9 +107,9 @@
+@@ -92,9 +96,9 @@
          # This module requires Python >= 2.2
          ext_modules.append(Extension("xmms._xmmsconfig",
                                       ["src/_xmmsconfigmodule.c"],
