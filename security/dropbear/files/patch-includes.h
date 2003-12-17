3bbf940ccb14bb99b032be145692793a6539a363
krion <krion@FreeBSD.org>
2003-12-17 11:56:01 +0000

    - Update to version 0.39
    PR:		60296
    Submitted by:	maintainer

---
+++ security/dropbear/files/patch-includes.h
@@ -0,0 +1,11 @@
+--- includes.h.orig	Tue Dec 16 11:02:22 2003
++++ includes.h	Tue Dec 16 11:02:43 2003
+@@ -38,7 +38,7 @@
+ #include <sys/time.h>
+ #include <sys/un.h>
+ #include <sys/wait.h>
+-#include <sys/dir.h>
++#include <sys/dirent.h>
+ 
+ #include <stdio.h>
+ #include <errno.h>
