436b3eb2cf8bf983a7c63f68acfa913a0eccc871
clement <clement@FreeBSD.org>
2003-12-18 14:36:28 +0000

    - update pygopherd to 2.0.3 - preserve config file
    PR:             59984
    Approved by:    erwin (mentor)

--- net/pygopherd/files/patch-setup.py
+++ net/pygopherd/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Sun Sep  8 02:47:43 2002
-+++ setup.py	Sun Sep  8 02:48:06 2002
-@@ -34,7 +34,7 @@
+--- setup.py.orig	Sat Aug 23 16:31:00 2003
++++ setup.py	Fri Dec  5 21:24:43 2003
+@@ -33,7 +33,7 @@
        url = pygopherd.version.homepage,
        packages = ['pygopherd', 'pygopherd.handlers', 'pygopherd.protocols'],
        scripts = ['bin/pygopherd'],
 -      data_files = [ ('/etc/pygopherd', ['conf/pygopherd.conf',
-+      data_files = [ ('%%PREFIX%%/etc/pygopherd', ['conf/pygopherd.conf',
++      data_files = [ ('%%PREFIX%%/etc/pygopherd', ['conf/pygopherd.conf-dist',
                                           'conf/mime.types'] ) ],
        license = pygopherd.version.license
  )
