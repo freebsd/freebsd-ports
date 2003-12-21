8d25379ea590960d5f2e42270536f98328501b5c
Akinori MUSHA <knu@FreeBSD.org>
2003-12-21 18:02:42 +0000

    Update to 3.2.2.

--- devel/ruby-setup.rb/files/patch-ruby-setup.rb
+++ devel/ruby-setup.rb/files/patch-ruby-setup.rb
@@ -1,11 +1,11 @@
---- ruby-setup.rb.orig	Wed Aug 27 13:03:01 2003
-+++ ruby-setup.rb	Thu Aug 28 01:16:03 2003
-@@ -908,7 +908,7 @@
+--- ruby-setup.rb.orig	Tue Dec 16 18:45:36 2003
++++ ruby-setup.rb	Mon Dec 22 03:02:04 2003
+@@ -916,7 +916,7 @@
    @singleton = nil
  
    def ToplevelInstaller.instance
--    @singleton = new(File.dirname($0)) unless defined?(@singleton)
-+    @singleton = new(File.pwd) unless defined?(@singleton)
+-    @singleton ||= new(File.dirname($0))
++    @singleton ||= new(File.pwd)
      @singleton
    end
  
