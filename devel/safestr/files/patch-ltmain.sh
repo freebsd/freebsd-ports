874b2845cf268894333ee48f80c64b06272db3c5
pav <pav@FreeBSD.org>
2003-12-22 17:04:59 +0000

    Add safestr, a simple and safe string handling library for C.
    PR:		ports/56129
    Submitted by:	Robert Schlotterbeck <rws@suki.rs.tarrant.tx.us>

---
+++ devel/safestr/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Aug 29 13:31:36 2003
++++ ltmain.sh	Fri Aug 29 13:31:59 2003
+@@ -5425,10 +5425,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
