993568264873b6a73bfb9b06dc7db8587f76ef7c
netchild <netchild@FreeBSD.org>
2003-12-22 15:28:18 +0000

    Second part of the stlport-icc fix for 4.x. [1]
    This patch modifies some gcc parts too, but basically it makes the port
    respect CFLAGS. Because of bugs in combination with gcc 3.x and some
    optimization options the CFLAGS are hardcoded (better safe than sorry).
    
    I've tested the gcc part of the fix on 4-stable and two 5.x systems.
    
    Submitted by:	Marius Strobl <marius@alchemy.franken.de> [1]
    Approved by:	maintainer timeout (for devel/stlport)

--- devel/stlport/files/stlport::stl_icc.h
+++ devel/stlport/files/stlport::stl_icc.h
@@ -1,8 +1,38 @@
-/*
- * STLport configuration file
- * It is internal STLport header - DO NOT include it directly
- */
+// STLport configuration file
+// It is internal STLport header - DO NOT include it directly
+// A list of Intel compiler for Linux portion of STLport settings.
 
-#define	_STLP_NO_CSTD_FUNCTION_IMPORTS
+# define _STLP_LONG_LONG long long
 
-#define	_STLP_LONG_LONG long long
+// Edit relative path below (or put full path) to get native
+// compiler headers included. Default is "../include".
+// C headers may reside in different directory, so separate macro is provided.
+# if (__INTEL_COMPILER >= 800)
+
+# define _STLP_NATIVE_INCLUDE_PATH ../include/c++
+# define _STLP_NATIVE_C_INCLUDE_PATH ../include
+//# define _STLP_NATIVE_CPP_C_INCLUDE_PATH ../include
+
+# ifndef __GNUC__ 
+//  If GCC compatability is disabled (-no-gcc is specified) STD needs to be redefined.
+# define _STLP_REDEFINE_STD 1
+# endif /* __GNUC__ */
+
+//#define _STLP_HAS_NO_NEW_C_HEADERS 1
+
+# endif /* __INTEL_COMPILER >= 800 */
+
+#ifndef __FreeBSD__ // FreeBSD system headers use unsupported GCC extensions.
+# ifndef __GNUC__ 
+# define __GNUC__ 3
+# endif
+#endif
+
+# define _STLP_NO_NATIVE_WIDE_FUNCTIONS 1
+
+#ifdef __FreeBSD__
+# include <osreldate.h>
+# define _STLP_HAS_NATIVE_FLOAT_ABS
+# define _STLP_NO_CSTD_FUNCTION_IMPORTS
+# define _STLP_NO_LONG_DOUBLE
+#endif
