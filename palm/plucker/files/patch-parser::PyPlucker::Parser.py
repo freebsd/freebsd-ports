a8b46e5aaa977416746daf1e2e4e5da3ea4ed417
pav <pav@FreeBSD.org>
2003-12-25 20:11:46 +0000

    - Update to 1.6.1
    PR:		ports/60545
    Submitted by:	mt <mt@primats.org.ua> (maintainer)

---
+++ palm/plucker/files/patch-parser::PyPlucker::Parser.py
@@ -0,0 +1,8 @@
+--- parser/PyPlucker/Parser.py.orig	Wed Dec 24 16:05:12 2003
++++ parser/PyPlucker/Parser.py	Wed Dec 24 16:05:21 2003
+@@ -1,4 +1,5 @@
+ #!/usr/bin/env python
++# -*- coding: iso-8859-1 -*-
+ 
+ """
+ Parser.py   $Id: Parser.py,v 1.21 2003/02/18 01:29:38 chrish Exp $
