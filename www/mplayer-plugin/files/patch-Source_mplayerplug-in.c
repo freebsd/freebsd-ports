7c3bac609cd63e26b9a7e89c9ac17f495dee7731
marcus <marcus@FreeBSD.org>
2003-12-25 05:42:42 +0000

    Update to 1.0.

--- www/mplayer-plugin/files/patch-Source_mplayerplug-in.c
+++ www/mplayer-plugin/files/patch-Source_mplayerplug-in.c
@@ -1,14 +1,5 @@
 --- Source/mplayerplug-in.c.orig	Fri Oct  3 07:54:52 2003
 +++ Source/mplayerplug-in.c	Sun Oct  5 00:02:12 2003
-@@ -28,7 +28,7 @@
-  */
- 
- #include "mplayerplug-in.h"
--pthread_mutex_t playlist_mutex = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
-+pthread_mutex_t playlist_mutex;
- 
- // This routine is only called when the plugin library is newer than the pluginrc file
- 
 @@ -71,7 +71,7 @@
      }
  
@@ -27,22 +18,3 @@
  	}
  
  	if (customtypes != NULL) {
-@@ -274,6 +274,7 @@
-     char parse[1000];
-     char logfile[1000];
-     char *cp;
-+    pthread_mutexattr_t attr;
- 
-     if (instance == NULL)
- 	return NPERR_INVALID_INSTANCE_ERROR;
-@@ -281,6 +282,10 @@
-     instance->pdata = NPN_MemAlloc(sizeof(PluginInstance));
-     This = (PluginInstance *) instance->pdata;
-     InitPrivateData(instance);
-+    pthread_mutexattr_init(&attr);
-+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
-+    pthread_mutex_init(&playlist_mutex, &attr);
-+    pthread_mutexattr_destroy(&attr);
- 
-     DESTROYED = 0;
- 
