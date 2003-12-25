cfbdde31de41ab396f2a64b47cd518139c1a858e
lioux <lioux@FreeBSD.org>
2003-12-25 17:18:34 +0000

    o New port mute-net version 0.1: Peer-to-peer client with privacy   protection and easy search/download o This is a master port for upcoming both net/mute-net-gui and   net/mute-net-text ports

---
+++ net-p2p/mute-net/files/wrapper.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+PREFIX="%%PREFIX%%"
+PROGRAM="${0}"
+DIRNAME="${HOME}/.mute-net/%%INTERFACE%%"
+DIR_FILES="${DIRNAME}/files"
+DIR_SETTINGS_ORIGINAL="%%DATADIR%%/%%INTERFACE%%_settings"
+DIR_SETTINGS="${DIRNAME}/settings"
+
+if [ ! -d ${DIRNAME} ]
+then
+	mkdir -p ${DIRNAME}
+fi
+
+if [ ! -d ${DIR_FILES} ]
+then
+	mkdir -p ${DIR_FILES}
+fi
+
+if [ ! -d ${DIR_SETTINGS} ]
+then
+	mkdir -p "${DIR_SETTINGS}"
+	cp -R "${DIR_SETTINGS_ORIGINAL}/." "${DIR_SETTINGS}"
+fi
+
+if [ ! -h ${DIRNAME}/${PROGRAM##*/} ]
+then
+	ln -sf ${PREFIX}/bin/${PROGRAM##*/}-real ${DIRNAME}/${PROGRAM##*/}
+fi
+
+echo " ${*} " | grep " \-createdironly " ||
+{
+	cd ${DIRNAME} &&
+	echo Running under ${DIRNAME} &&
+	exec ./${PROGRAM##*/} "${@}" ||
+	echo PROBLEM. Cannot create directory ${DIRNAME}, make sure you have the proper permissions to create it and try again
+}
