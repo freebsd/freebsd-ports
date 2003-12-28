e761ad7f83772a7c1db4625d842d89d45fc6bd49
maho <maho@FreeBSD.org>
2003-12-28 06:01:19 +0000

    a) Update to 3.5.22 b) added Opteron support but my machine is very unstable :(

--- math/atlas-devel/files/patch-config.c
+++ math/atlas-devel/files/patch-config.c
@@ -1,15 +1,6 @@
---- config.c.orig	Mon Jun 23 07:58:35 2003
-+++ config.c	Tue Jul  1 12:32:49 2003
-@@ -708,7 +708,7 @@
-             else if (mach == IA64Itan || MachIsUS(mach) ||
-                      mach == Dec21164 || mach == Dec21264)
-             {
--               if (major == 3)
-+               if ((major == 2 && minor < 96) || (major == 3)) /* Nasty hack for FreeBSD/Alpha STABLE */
-                {
-                   strcpy(goodgcc, files[i]);
-                   return(0);
-@@ -1114,7 +1114,9 @@
+--- config.c.orig	Sun Dec 28 12:44:08 2003
++++ config.c	Sun Dec 28 12:54:09 2003
+@@ -1188,7 +1188,9 @@
     switch(OS)
     {
     case OSOSX:  /* don't know answer */
@@ -20,19 +11,30 @@
     case OSLinux:
        break;
     case OSSunOS:
-@@ -1252,6 +1254,11 @@
-                "-mcpu=ultrasparc -mtune=ultrasparc -fomit-frame-pointer -O3");
+@@ -1450,9 +1452,9 @@
+          if (THREADS && OS == OSFreeBSD)
+          {
+             if (which == CPF77)
+-               strcat(flag, " -pthread");
++               strcat(flag, " %%PTHREAD_LIBS%%");
+             else
+-               strcat(flag, " -pthread -D_REENTRANT -D_THREAD_SAFE");
++               strcat(flag, " %%PTHREAD_LIBS%%  %%PTHREAD_CFLAGS%%");
+          }
+          break;
+       case OSSunOS4:
+@@ -1984,8 +1986,8 @@
+       if (OS == OSFreeBSD)
+       {
+          if (strstr(comp, "cc"))
+-            strcat(flag, " -pthread -D_REENTRANT -D_THREAD_SAFE");
+-         else strcat(flag, " -pthread");
++            strcat(flag, " %%PTHREAD_LIBS%% %%PTHREAD_CFLAGS%%");
++         else strcat(flag, " %%PTHREAD_LIBS%%");
        }
-       if (OS == OSFreeBSD && F77) strcpy(F77, "f77");
-+      if (OS == OSFreeBSD && THREADS) { 
-+              strcat(F77FLAGS, "%%PTHREAD_CFLAGS%%");
-+              strcat(CCFLAGS, "%%PTHREAD_CFLAGS%%");
-+              strcat(MMFLAGS, "%%PTHREAD_CFLAGS%%");
-+     }
-       break;
-    case OSSunOS:
-       np = 3;
-@@ -2116,7 +2123,10 @@
+    }
+    return(*comp ? comp : NULL);
+@@ -2840,7 +2842,10 @@
           if (!CmndOneLine(targ, "sysctl hw.model", ln))
           {
              if (strstr(ln, "433au")) mach = Dec21164;
@@ -43,7 +45,7 @@
           }
           break;
        case LAIA64: /* don't know */
-@@ -2125,14 +2135,22 @@
+@@ -2849,14 +2854,22 @@
           if (!CmndOneLine(targ, "sysctl hw.model", ln))
           {
              if (strstr(ln, "Pentium Pro")) mach = IntPPRO;
@@ -58,20 +60,31 @@
 +            else if (strstr(ln, "Celeron(R)")) mach = IntPII;
              else if (strstr(ln, "Athlon")) mach = AmdAthlon;
              else if (strstr(ln, "AMD-K7")) mach = AmdAthlon;
-             else if (strstr(ln, "32 bit Hammer")) mach = AmdHammer32;
-             else if (strstr(ln, "64 bit Hammer")) mach = AmdHammer64;
+-            else if (strstr(ln, "32 bit Hammer")) mach = AmdHammer32;
+-            else if (strstr(ln, "64 bit Hammer")) mach = AmdHammer64;
++            else if (strstr(ln, "Opteron(tm)") && sizeof(void *)==4) mach = AmdHammer32;
++            else if (strstr(ln, "Opteron(tm)") && sizeof(void *)==8) mach = AmdHammer64;
              else if (strstr(ln, "Pentium/P55C")) mach = IntP5MMX; /* sent by */
              else if (strstr(ln, "Pentium")) mach=IntP5;       /* Nakata Maho */
 +            else mach = IntP5;
           }
           break;
        default:;
-@@ -3136,6 +3154,8 @@
+@@ -3939,6 +3952,8 @@
+       USEDEFL1 = !IsYes('y', "", "Tune the Level 1 BLAS?");
     }
-    if (USEWINF77) strcpy(F77, "$(BINdir)/winf77.exe");
  
 +   fprintf(fpout, "%s", ARCH);
 +   fclose(fpout);
     ATL_mprintf(2, fplog, stdout,"\nCreating make include file Make.%s\n", ARCH);
     sprintf(ln, "Make.%s", ARCH);
     fpout = fopen(ln, "w");
+@@ -4080,7 +4095,7 @@
+    if (THREADS)
+    {
+       fprintf(fpout, " -DATL_NCPU=%d", ncpu);
+-      if (OS == OSFreeBSD) fprintf(fpout, " -D_THREAD_SAFE -D_REENTRANT");
++      if (OS == OSFreeBSD) fprintf(fpout, " %%PTHREAD_CFLAGS%%");
+       if (OS == OSAIX) fprintf(fpout, " -DIBM_PT_ERROR");
+       if (OS == OSIRIX) fprintf(fpout, " -D_POSIX_C_SOURCE=199506L");
+    }
