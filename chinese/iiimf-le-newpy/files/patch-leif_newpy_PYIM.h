7874f190136393a609799dc33e0ed757e6ad48de
Edwin Groothuis <edwin@FreeBSD.org>
2003-12-28 11:43:36 +0000

    New port: iiimf-*
    	IIIMF stands for Internet/Intranet Input Method Framework.
    	IIIMF is designed to be the next generation of input method framework
    	which provides the following capabilities;
    
    	* Multiplatform, platform independent.
    	* Multlingual and Full UNICODE support, but satisfactory for native speakers.
    	* Windowing System Independent.
    	* Multiple language engines concurrently run.
    	* Multiuser.
    	* Distributed, lightweight clients and scalable server.
    	* Extensible in multiple means.
    	* Input method protocol efficient enough to be used over low-speed modem
    	  connection.
    	* Easy input method engine development with plugin API.
    	* Easy input method enabling with libiiimcf, even on console apps.
    	* Small core part to start from.
    
    	WWW: http://www.openi18n.org/subgroups/im/IIIMF/
    
    	- Kuang-che Wu
    	  kcwu@csie.org
    
    PR:		ports/60087
    Submitted by:	Kuang-che Wu <kcwu@csie.org>

---
+++ chinese/iiimf-le-newpy/files/patch-leif_newpy_PYIM.h
@@ -0,0 +1,10 @@
+--- newpy/PYIM.h.orig	Sat Dec  6 02:51:17 2003
++++ newpy/PYIM.h	Sat Dec  6 02:51:23 2003
+@@ -47,7 +47,6 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <malloc.h>
+ #include <string.h>
+ /*
+ #include <sys/types.h>
