f5e295be7a66b5eaac0bb47f808b3833a5dac6ea
edwin <edwin@FreeBSD.org>
2003-12-29 10:37:58 +0000

    Remove java/orion again, it was a duplicate of www/orion
    Gimme da pointy hat.

--- java/orion/files/orion.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ "x${JAVA_CMD}" = "x" ]; then
-    JAVA_CMD=%%JAVA_HOME%%/bin/java
-fi
-if [ "$1" = "help" ]; then
-    $0 -\? 
-fi
-cd %%ORION_HOME%% && \
-    ${JAVA_CMD} -jar orion.jar $@ 
