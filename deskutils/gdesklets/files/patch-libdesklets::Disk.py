00ed34b05ec055a51c59893ec8ae61ead02f337c
marcus <marcus@FreeBSD.org>
2003-12-30 22:34:22 +0000

    * Update to 0.25.1 * Change maintainer to Franz Klammer <klammer@webonaut.com>
    PR:		60742
    Submitted by:	Jeremy Messenger <mezz7@cox.net> (previous maintainer)

--- deskutils/gdesklets/files/patch-libdesklets::Disk.py
+++ deskutils/gdesklets/files/patch-libdesklets::Disk.py
@@ -2,12 +2,15 @@
 # Franz Klammer <klammer@webonaut.com> for help and create
 # those patches.
 #
-# Note that, this is for now. One of us still need to fix the
-# libgtop to correct read the partition so this patch can be
-# remove.
+# Note that, this is for now. It will be remove when the
+# libgtop is update to 2.6 or maybe I can ask gnome@freebsd.org
+# if they won't mind to add a patch in libgtop 2.4 to get this
+# patch remove.
+#
+# Joe has fixed it in the libgtop 2.5.x and isn't in ports yet.
 
---- libdesklets/Disk.py.orig	Mon Nov 17 13:59:20 2003
-+++ libdesklets/Disk.py	Mon Nov 17 14:05:34 2003
+--- libdesklets/Disk.py.orig	Tue Dec 30 13:29:57 2003
++++ libdesklets/Disk.py	Tue Dec 30 13:31:01 2003
 @@ -1,6 +1,6 @@
  import glibtop
  import polling
@@ -16,23 +19,22 @@
  
  class Disk:
  
-@@ -13,18 +13,22 @@
+@@ -14,18 +14,22 @@
  
      def __poll_partitions(self):
  
 -        data = glibtop.get_mountlist(0)
--        partitions = []
++        fd = os.popen("mount -p")
++        data = fd.readlines()
++        fd.close()
++
+         partitions = []
 -        for size, device, mpoint, fstype in data:
 -            if (fstype in ("ext2", "ext3", "msdos", "vfat", "ntfs", "hpfs"
 -                           "jfs", "reiserfs", "xfs", "qnx4", "adfs", "ffs",
 -                           "hfs", "befs", "bfs", "efs", "iso9660", "minix",
 -                           "sysv", "coda", "nfs", "udf", "ufs", "xiafs")):
 -                partitions.append((device, mpoint))
-+        fd = os.popen("mount -p")
-+        data = fd.readlines()
-+        fd.close()
-+
-+        partitions = []
 +        for lines in data:
 +           fields = lines.strip().replace("\t", " ").split()
 +           if (fields[2] in ("ext2", "ext3", "msdos", "vfat", "ntfs", "hpfs"
