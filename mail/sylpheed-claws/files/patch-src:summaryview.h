a90ca0f0b02c172cd10c0ad33c467880af503233
Alexander Leidinger <netchild@FreeBSD.org>
2003-12-30 14:59:16 +0000

    - fix build on 4.x [1][2][3] - remove unnecessary file [2]
    Submitted by:	Horio Shoichi <horio@bugsgrief.net> [3]
    Noticed by:	Horio Shoichi <horio@bugsgrief.net> [2]
    		Piotr Smyrak <piotr.smyrak@heron.pl> [1]
    		Filippo Natali <mail bounces, no such user> [1]

---
+++ mail/sylpheed-claws/files/patch-src:summaryview.h
@@ -0,0 +1,10 @@
+--- src/summaryview.h.orig	Tue Dec 30 15:28:37 2003
++++ src/summaryview.h	Tue Dec 30 15:29:00 2003
+@@ -20,6 +20,7 @@
+ #ifndef __SUMMARY_H__
+ #define __SUMMARY_H__
+ 
++#include <sys/types.h>
+ #include <regex.h>
+ 
+ #include <glib.h>
