25dc373b47363b8e74bac8e82078fe31d0684a57
Ying-Chieh Liao <ijliao@FreeBSD.org>
2003-12-31 03:14:57 +0000

    add codeworker 3.2 A parsing tool and a source code generator

---
+++ devel/codeworker/files/patch-CGRuntime.cpp
@@ -0,0 +1,13 @@
+--- CGRuntime.cpp.orig	Wed Dec 31 09:56:40 2003
++++ CGRuntime.cpp	Wed Dec 31 09:57:16 2003
+@@ -60,6 +60,10 @@
+ #	endif
+ #	include "UtlString.h" // for Debian/gcc 2.95.4
+ #endif
++
++#if defined(__FreeBSD__)
++	extern char **environ;
++#endif
+ 
+ 
+ #ifndef WIN32
---
+++ devel/codeworker/files/patch-UtlTimer.cpp
@@ -0,0 +1,10 @@
+--- UtlTimer.cpp.orig	Wed Dec 31 10:07:18 2003
++++ UtlTimer.cpp	Wed Dec 31 10:09:12 2003
+@@ -30,6 +30,7 @@
+ #	include "windows.h"
+ #	include "winbase.h"
+ #else
++#	include <time.h>
+ #	include <sys/timeb.h>
+ #endif
+ 
