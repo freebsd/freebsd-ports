8787572f90e5bd64c83c056052208e66c4a6165c
Edwin Groothuis <edwin@FreeBSD.org>
2004-01-03 07:37:48 +0000

    New port: x11-wm/wmconfig, X window manager menu generation tool
    	This is the port of Wmconfig, a X window manager menu
    	generation tool.
    
    PR:		ports/55672
    Submitted by:	Tommy Scheunemann <net@arrishq.org>

---
+++ x11-wm/wmconfig/files/patch-wmconfig.h
@@ -0,0 +1,5 @@
+--- wmconfig.h.orig	Sat Aug 17 16:42:47 2003
++++ wmconfig.h		Sun Aug 18 16:59:38 2003
+@@ -30 +30 @@
+-#define DEFAULT_SYS_CONFDIR	"/etc/X11/wmconfig"
++#define DEFAULT_SYS_CONFDIR	"/usr/local/etc/wmconfig"
