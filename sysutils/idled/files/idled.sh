0d620f2f4eae1a7042dd3ab57c9179d5a0a5c7a5
edwin <edwin@FreeBSD.org>
2004-01-03 06:59:22 +0000

    [PATCH] sysutils/idled 1.16
    	This patch adds rc_subr support to the idled port.
    
    PR:		ports/57383
    Submitted by:	Martin Matuska <martin@tradex.sk>
    Approved by: 	maintainer timeout

--- sysutils/idled/files/idled.sh
+++ sysutils/idled/files/idled.sh
@@ -1,19 +1,33 @@
 #!/bin/sh
-case "$1" in
-	stop)
-		killall -TERM idled
-		echo "idled stopped"
-		;;
-	restart)
-		killall -TERM idled
-		/usr/local/libexec/idled
-		echo "idled restarted"
-		;;
-	-h)
-		echo "Usage: `basename $0` { start | stop | restart }"
-		;;
-	*)				# includes start
-		/usr/local/libexec/idled
-		echo -n ' idled'
-		;;
-esac
+#
+# $FreeBSD$
+#
+
+# PROVIDE: idled
+# REQUIRE: DAEMON
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
+
+#
+# Add the following lines to /etc/rc.conf to enable idled:
+#
+#idled_enable="YES"
+#
+# See idled(8) for flags
+#
+
+. %%RC_SUBR%%
+
+name=idled
+rcvar=`set_rcvar`
+
+command=%%PREFIX%%/libexec/idled
+required_files=%%PREFIX%%/etc/idled.cf
+
+# set defaults
+
+idled_enable=${idled_enable:-"NO"}
+idled_flags=${idled_flags:-""}
+
+load_rc_config $name
+run_rc_command "$1"
