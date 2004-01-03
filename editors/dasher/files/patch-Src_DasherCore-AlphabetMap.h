020941c470435bb4c5bd0497119e9732b9fdf489
marcus <marcus@FreeBSD.org>
2004-01-03 08:17:19 +0000

    Merge diffs from my GNOME 2.5 repo.
    * Make this port build
    * Use USE_BZIP2
    * Point to MASTER_SITE_GNOME
    * Correct pkg-plist
    * Obey CFLAGS
    * Use USE_LIBTOOL to avoid threading problems on -CURRENT
    * Add additional categories

---
+++ editors/dasher/files/patch-Src_DasherCore-AlphabetMap.h
@@ -0,0 +1,10 @@
+--- Src/DasherCore/AlphabetMap.h.orig	Sun Dec 14 10:56:59 2003
++++ Src/DasherCore/AlphabetMap.h	Sun Dec 14 10:57:30 2003
+@@ -50,6 +50,7 @@
+ #define __AlphabetMap_h__
+ 
+ #include "../Common/MSVC_Unannoy.h"
++#include <sys/types.h>
+ #include <vector>
+ #include <string>
+ 
