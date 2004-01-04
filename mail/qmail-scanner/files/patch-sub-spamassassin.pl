1849c826fbe5987c949396dc451d71bd26e38dc0
sergei <sergei@FreeBSD.org>
2004-01-04 13:25:13 +0000

    - Fix broken detection of Japanese locales [1] - Update patches - Bump PORTREVISION
    PR:		60853 [1] (partly)
    Submitted by:	Fumihiko Kimura <jfkimura@yahoo.co.jp> [1],
    		maintainer

--- mail/qmail-scanner/files/patch-sub-spamassassin.pl
+++ mail/qmail-scanner/files/patch-sub-spamassassin.pl
@@ -1,6 +1,6 @@
 --- sub-spamassassin.pl.orig	Mon Sep 29 04:17:22 2003
-+++ sub-spamassassin.pl	Thu Dec 11 14:28:48 2003
-@@ -57,6 +57,7 @@ sub spamassassin {
++++ sub-spamassassin.pl	Sun Jan  4 16:17:55 2004
+@@ -57,6 +57,7 @@
      $tag_score .= "SA:1($sa_score/$sa_max):";
      $sa_comment = "Yes, hits=$sa_score required=$sa_max" if ($spamc_options =~ /\-c/);
      &debug("SA: yup, this smells like SPAM");
