15ae8ca223b0dffefa52ca9f3a0f58c3c09d3bf4
krion <krion@FreeBSD.org>
2004-01-05 16:54:48 +0000

    - Update to version 0.7.3
    PR:		60935
    Submitted by:	Ports Fury

--- net/libyahoo2/files/patch-sample_client.c
+++ net/libyahoo2/files/patch-sample_client.c
@@ -25,12 +25,3 @@
  		timestr[strlen(timestr) - 1] = '\0';
  
  		print_message(("[Offline message at %s from %s]: %s", 
-@@ -654,7 +652,7 @@
- }
- 
- void ext_yahoo_got_webcam_image(int id, const char *who,
--		unsigned char *image, unsigned int image_size, unsigned int real_size,
-+		const unsigned char *image, unsigned int image_size, unsigned int real_size,
- 		unsigned int timestamp)
- {
- 	static unsigned char *cur_image = NULL;
