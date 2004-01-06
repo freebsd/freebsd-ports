e7d05a7642ef3299fd2a7f182be1ae50e4bf6120
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-01-06 06:46:09 +0000

    add libparsifal 0.7.3 Lightweight XML Parser

---
+++ textproc/libparsifal/files/patch-conf:ltmain.sh
@@ -0,0 +1,15 @@
+--- conf/ltmain.sh.orig	Tue Jan  6 14:43:27 2004
++++ conf/ltmain.sh	Tue Jan  6 14:43:42 2004
+@@ -4247,10 +4247,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
