1439737317a182487e6a1cc7c646d38e5a9a92e7
clement <clement@FreeBSD.org>
2004-01-06 01:26:57 +0000

    Add mod_mya 3.0.1, allows basic authentication data to be stored in a MySQL database.
    PR:		58252
    Approved by:    erwin (mentor) (implicitly)

---
+++ www/mod_mya/files/patch-mod_mya.c
@@ -0,0 +1,10 @@
+--- mod_mya.c.orig	Sun Oct 19 16:41:56 2003
++++ mod_mya.c	Sun Oct 19 16:42:23 2003
+@@ -41,6 +41,7 @@
+ #include <stdlib.h>
+ #include <sys/types.h>
+ #include <unistd.h>
++#include <stddef.h>
+ 
+ #include <mysql.h>
+ 
