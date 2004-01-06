524a641616bccc3e90bff5857d42d0e47e153df4
pav <pav@FreeBSD.org>
2004-01-06 14:18:02 +0000

    - Update to 0.3.16
    PR:		ports/60711
    Submitted by:	ijliao

---
+++ security/gpgme/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Tue Dec 30 09:32:46 2003
++++ ltmain.sh	Tue Dec 30 09:32:59 2003
+@@ -4333,10 +4333,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
