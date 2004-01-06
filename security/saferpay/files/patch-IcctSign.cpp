877c83391dba61a1a04dc037e361aeaaf3ed1968
Martin Blapp <mbr@FreeBSD.org>
2004-01-06 14:55:43 +0000

    Upgrade to version 1.3.0

--- security/saferpay/files/patch-IcctSign.cpp
+++ security/saferpay/files/patch-IcctSign.cpp
@@ -1,5 +1,5 @@
---- saferpay/IcctSign.cpp.orig	Wed Feb 19 12:17:14 2003
-+++ saferpay/IcctSign.cpp	Wed Feb 19 12:19:52 2003
+--- saferpay/IcctSign.cpp.orig	Wed Sep 24 15:13:06 2003
++++ saferpay/IcctSign.cpp	Tue Jan  6 15:46:01 2004
 @@ -32,7 +32,11 @@
  	#define RSA_generate_key_S RSA_generate_key
  #endif
@@ -12,7 +12,7 @@
  					   ULONG cbMessage, /*[out]*/ UCHAR* pDigest, ULONG* pcbDigest)
  {
  	RSA *pPrivateKey = RSA_new();
-@@ -59,7 +63,11 @@
+@@ -64,7 +68,12 @@
  	return -1;
  }
  
@@ -21,42 +21,12 @@
 +#else
  HRESULT ICCT_Verify(UCHAR* pucPublicKey, int cbPublicKey, UCHAR* pMessage, ULONG cbMessage,
 +#endif
++
  					/*[in]*/ UCHAR* pDigest, ULONG cbDigest)
  {
  	RSA *pPublicKey = RSA_new();
-@@ -91,7 +99,11 @@
- 	return -1;
+@@ -255,3 +264,4 @@
+ 	}
  }
- 
-+#ifdef HAVE_SSLENGINE_H
-+HRESULT ICCT_CryptDigest(const UCHAR* pucPublicKey, int cbPublicKey,
-+#else
- HRESULT ICCT_CryptDigest(UCHAR* pucPublicKey, int cbPublicKey,
-+#endif
- 					UCHAR* pDigest, ULONG cbDigest, UCHAR * pOut, ULONG * cbOut)
- {
- 	RSA *pPublicKey = RSA_new();
-@@ -126,7 +138,11 @@
- 	return -1;
- }
- 
-+#ifdef HAVE_SSLENGINE_H
-+HRESULT ICCT_DecryptDigest(const UCHAR* pucPrivateKey, int cbPrivateKey,
-+#else
- HRESULT ICCT_DecryptDigest(UCHAR* pucPrivateKey, int cbPrivateKey,
-+#endif
- 					UCHAR * pIn, ULONG cbIn, UCHAR* pDigest, ULONG * cbDigest)
- {
- 	RSA *pPrivateKey = RSA_new();
-@@ -158,7 +174,11 @@
- 						  int* cbPrivateKey, UCHAR* Seed,
- 						  int cbSeed)
- {
-+#ifdef HAVE_SSLENGINE_H
-+	RSA *pPrivateKey = RSA_new_method((ENGINE *)RSA_PKCS1_SSLeay());
-+#else
- 	RSA *pPrivateKey = RSA_new_method(RSA_PKCS1_SSLeay());
-+#endif
- 
- 	UCHAR* pPubK = pucPublicKey;
- 	UCHAR* pPrivK = pucPrivateKey;
+ */
++
--- security/saferpay/files/patch-saferpay.mk
+++ security/saferpay/files/patch-saferpay.mk
@@ -1,25 +1,20 @@
---- saferpay/saferpay.mk.orig	Tue Feb 18 16:58:08 2003
-+++ saferpay/saferpay.mk	Tue Feb 18 16:58:50 2003
-@@ -7,18 +7,11 @@
- 
+--- saferpay/saferpay.mk.orig	Tue Jul 22 16:08:35 2003
++++ saferpay/saferpay.mk	Tue Jan  6 15:10:05 2004
+@@ -1,13 +1,11 @@
  ####### Compiler, tools and options
- 
--#CC	=	/project/gcc31/bin/gcc
--#CXX	=	/project/gcc31/bin/g++
--
 -CC	=	gcc
 -CXX	=	g++
--
+ 
 -CFLAGS	=	-pipe -fPIC -O2
 -CXXFLAGS=	-pipe -fPIC -O2
-+CFLAGS	+=	-fPIC
-+CXXFLAGS +=	-fPIC
-+LFLAGS	=	-fPIC -Lout
++CFLAGS		+=	-fPIC
++CXXFLAGS	+=	-fPIC
  INCPATH	=		
--#LINK	=	/project/gcc31/bin/g++
+ 
 -LINK	=	g++
--LFLAGS	=	-fPIC
-+LINK	=	$(CXX)
+-LFLAGS	=	-fPIC -ldl
++LINK	+=	$(CXX) -Lout
++LFLAGS	+=	-fPIC
  LIBS	=	-lidpapp
  MOC	=	$(QTDIR)/bin/moc
  
