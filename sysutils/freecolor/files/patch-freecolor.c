67b80dc124f932c0ac8381dee0d895283f5fdc0e
Pav Lucistnik <pav@FreeBSD.org>
2004-01-07 23:53:23 +0000

    Add freecolor, a 'free' replacement that displays free memory graphically as a bargraph. It supports the same options as 'free'.
    http://www.rkeene.org/oss/freecolor/
    
    PR:		ports/61038
    Submitted by:	Tim Bishop <tim@bishnet.net>

---
+++ sysutils/freecolor/files/patch-freecolor.c
@@ -0,0 +1,12 @@
+--- freecolor.c.bak	Wed Jan  7 20:04:01 2004
++++ freecolor.c	Wed Jan  7 20:04:13 2004
+@@ -62,9 +62,6 @@
+ #include <unistd.h>
+ #include <stdlib.h>
+ #include <stdio.h>
+-#ifndef NO_GETOPT
+-#include <getopt.h>
+-#endif
+ #define BARLEN 35
+ #define HEADERLEN 14
+ #define VERSION "0.8.3"
