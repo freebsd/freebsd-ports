869f514acf65c7bcca7ef282c174fba0cda70ce2
nobutaka <nobutaka@FreeBSD.org>
2004-01-07 14:16:51 +0000

    Update to 0.9.23.

---
+++ multimedia/xine/files/patch-src:common:utils.c
@@ -0,0 +1,10 @@
+--- src/common/utils.c.orig	Wed Jan  7 01:04:22 2004
++++ src/common/utils.c	Wed Jan  7 01:04:47 2004
+@@ -34,7 +34,6 @@
+ #include <sys/stat.h>
+ #include <sys/wait.h>
+ #include <sys/utsname.h>
+-#include <alloca.h>
+ 
+ #ifdef HAVE_X11
+ #include <X11/Xlib.h>
