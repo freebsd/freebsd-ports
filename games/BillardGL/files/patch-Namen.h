9d0a562fbb0aa4ada41846e893ed0301974bdafc
krion <krion@FreeBSD.org>
2004-01-07 08:44:10 +0000

    Add BillardGL 1.75, an OpenGL billiard simulator.
    PR:		60847
    Submitted by:	Igor Pokrovsky <tiamat@comset.net>

---
+++ games/BillardGL/files/patch-Namen.h
@@ -0,0 +1,11 @@
+--- Namen.h.orig	Sat Jan  3 16:33:42 2004
++++ Namen.h	Sat Jan  3 16:34:03 2004
+@@ -23,7 +23,7 @@
+ #define TUTORIAL 105
+ 
+ #ifndef _WIN32
+-#define PFAD "/usr/share/BillardGL/"
++#define PFAD "%%X11BASE%%/share/%%PORTNAME%%/"
+ #endif
+ 
+ #ifndef PFAD
