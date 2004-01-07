80f0a8d351ec1bae4180821bfc1b6992d5ab2eed
arved <arved@FreeBSD.org>
2004-01-07 15:26:07 +0000

    Add krosswordplayer, a krossword puzzle game for KDE.

---
+++ games/krosswordplayer/files/patch-krosswordplayer-libacrosslite.cpp
@@ -0,0 +1,10 @@
+--- krosswordplayer/libacrosslite.cpp.orig	Wed Jan  7 15:54:39 2004
++++ krosswordplayer/libacrosslite.cpp	Wed Jan  7 15:54:54 2004
+@@ -3,6 +3,7 @@
+ #include <sys/stat.h>
+ #include <fcntl.h>
+ #include <unistd.h>
++#include <stdio.h>
+ #include "libacrosslite.h"
+ 
+ 
