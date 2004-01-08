c06121e31cd7ab13978e1c5e2af2b7f51d888ce1
edwin <edwin@FreeBSD.org>
2004-01-08 02:17:34 +0000

    New port: japanese/multiskkserv
    	multiskkserv is a skk server which can search multiple
    	dictionaries.  This server uses cdb format as the dictionary
    	format.  cdb is the efficient and constant (i.e. cannot
    	add, delete, modify) database format by Dan J. Bernstein,
    	which I think is the appropriate format for the system-wide
    	constant dictionary storage.
    
    	This is very experimental.
    
    PR:		ports/60645
    Submitted by:	Takahiro Morishita <morishita@skywing.org>

---
+++ japanese/multiskkserv/files/multiskkserv.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+# PROVIDE: multiskkserv
+# REQUIRE: LOGIN
+# KEYWORD: FreeBSD
+
+#
+# Add the following lines to /etc/rc.conf to enable multiskkserv:
+#
+#multiskkserv_enable="YES"
+#
+# See `%%PREFIX%%/sbin/multiskkserv -h` for flags
+#
+
+. %%RC_SUBR%%
+
+name=multiskkserv
+rcvar=`set_rcvar`
+
+command=%%PREFIX%%/sbin/multiskkserv
+
+# set defaults
+
+multiskkserv_enable=${multiskkserv_enable:-"NO"}
+multiskkserv_flags=${multiskkserv_flags:-"%%PREFIX%%/%%SKKDIR%%/SKK-JISYO.%%SKKJISYO_TYPE%%.cdb"}
+
+load_rc_config ${name}
+run_rc_command "$1"
