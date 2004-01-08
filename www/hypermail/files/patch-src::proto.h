c89de17a9c1f3653c0344f56b954e58db1a22cd1
Pete Fritchman <petef@FreeBSD.org>
2004-01-08 06:48:49 +0000

    - update to 2.1.8 - use %%DOCSDIR%%

--- www/hypermail/files/patch-src::proto.h
+++ www/hypermail/files/patch-src::proto.h
@@ -1,11 +1,11 @@
---- src/proto.h.orig	Mon Dec 24 12:40:11 2001
-+++ src/proto.h	Mon Dec 24 12:40:19 2001
-@@ -88,7 +88,7 @@
+--- src/proto.h.orig	Thu Jul  3 18:00:18 2003
++++ src/proto.h	Thu Jan  8 00:27:25 2004
+@@ -94,7 +94,7 @@
+ char *PushString(struct Push *, const char *);
+ char *PushNString(struct Push *, const char *, int);
+ 
+-char *strcasestr (char *, const char *);
++// char *strcasestr (char *, const char *);
  char *strsav(const char *);
  char *strreplace(char *, char *);
  void strcpymax(char *, const char *, int);
--char *strcasestr(char *, const char *);
-+/* char *strcasestr(char *, const char *); */
- char *stripzone(char *);
- int numstrchr(char *, char);
- char *getvalue(char *);
