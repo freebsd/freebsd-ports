385201700b7e5ef62fec232b91724e9d5ac9fa73
Kirill Ponomarev <krion@FreeBSD.org>
2004-01-08 15:26:22 +0000

    - Update to version 0.13.2
    PR:		61073
    Submitted by:	Ports Fury

--- audio/darkice/files/patch-src_OssDspSource.cpp
+++ audio/darkice/files/patch-src_OssDspSource.cpp
@@ -1,11 +1,11 @@
---- src/OssDspSource.cpp.orig	Sun Jan 26 18:48:57 2003
-+++ src/OssDspSource.cpp	Mon Feb 10 21:40:43 2003
+--- src/OssDspSource.cpp.orig	Thu Feb 13 00:48:22 2003
++++ src/OssDspSource.cpp	Thu Jan  8 12:15:07 2004
 @@ -86,6 +86,13 @@
  #error need sys/soundcard.h
  #endif
  
-+#if defined(__FreeBSD__)
-+#if defined(__sparc__) || defined(__powerpc__)
++#if !defined(AFMT_S16_NE)
++#if defined(BYTE_ORDER) && (BYTE_ORDER == BIG_ENDIAN)
 +#define AFMT_S16_NE AFMT_S16_BE
 +#else
 +#define AFMT_S16_NE AFMT_S16_LE
