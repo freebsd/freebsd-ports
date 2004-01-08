dc0963790e9f4349640cf4af4fd61a512fb2f7b6
ijliao <ijliao@FreeBSD.org>
2004-01-08 07:10:20 +0000

    upgrade to 4.3.2

---
+++ graphics/opendx/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Jan  8 13:51:00 2004
++++ ltmain.sh	Thu Jan  8 13:51:11 2004
+@@ -5425,10 +5425,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
