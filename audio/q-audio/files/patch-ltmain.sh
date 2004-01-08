327d69cdcbe6086b3a7c0faf022ca146dfd88333
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-01-08 08:14:47 +0000

    add q-audio 1.2 Q-Audio -- digital audio interface for the Q language

---
+++ audio/q-audio/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Jan  8 15:57:49 2004
++++ ltmain.sh	Thu Jan  8 15:58:00 2004
+@@ -4210,10 +4210,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
