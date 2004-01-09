29b3a4fd119e490146e8f30b8c6ddc3bdfa6ed2b
pav <pav@FreeBSD.org>
2004-01-09 00:12:51 +0000

    - Update to 2.99.1
    Changes in software:
    - vendor's bugfixes
    - new FLUTE support
    - new dependencies in gdome-2 and glib20
    - properly install the shared library and headers
    - replace -pthread with $PTHREAD_LIBS
    - fix compilation warnings due to C++ headers in -current
    
    PR:		ports/61068
    Submitted by:	Panagiotis Astithas <past@noc.ntua.gr> (maintainer)

--- mbone/mcl/files/patch-mcl_osdep.h
+++ mbone/mcl/files/patch-mcl_osdep.h
@@ -1,24 +1,24 @@
---- src/common/mcl_osdep.h.orig	Tue Jul 22 19:37:39 2003
-+++ src/common/mcl_osdep.h	Wed Oct 15 16:11:46 2003
-@@ -30,7 +30,7 @@
- /*
+--- src/common/mcl_osdep.h.orig	Sun Dec  7 18:58:22 2003
++++ src/common/mcl_osdep.h	Sun Dec  7 19:00:39 2003
+@@ -44,7 +44,7 @@
   * bit field order is compiler/OS dependant
+  * With linux, see the above LINUX_XXX defines...
   */
--#if defined(LINUX) || defined(WIN32)
-+#if defined(LINUX) || defined(WIN32) || defined(FREEBSD)
+-#if defined(LINUX_I386) || defined(WIN32)
++#if defined(LINUX_I386) || defined(WIN32) || defined (FREEBSD)
  #define _BIT_FIELDS_LTOH
  #undef _BIT_FIELDS_HTOL
  
-@@ -44,7 +44,7 @@
+@@ -58,7 +58,7 @@
  /*
   * sighandler
   */
 -#if defined(LINUX)
-+#if defined(LINUX) || defined(FREEBSD)
++#if defined(LINUX) || defined (FREEBSD)
  	/* On Linux systems, signal handlers must be of __sighandler_t type */
  #define	sighandler_t	__sighandler_t
  
-@@ -63,9 +63,9 @@
+@@ -77,9 +77,9 @@
  #define u_int16_t	ushort_t	/* or uint16_t */
  #define u_int32_t	uint_t		/* or uint32_t */
  
