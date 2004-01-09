ba9510d9aa42a9cb4aaa4f83f64b3536fae48254
Pav Lucistnik <pav@FreeBSD.org>
2004-01-09 22:23:50 +0000

    - Update to 2.1.0
    PR:		ports/61141
    Submitted by:	Eyal Soha <esoha@attbi.com> (maintainer)

--- dns/noip/files/patch-noip2.c
+++ dns/noip/files/patch-noip2.c
@@ -1,15 +1,15 @@
---- noip2.c.orig	Sat May 24 00:55:14 2003
-+++ noip2.c	Fri Aug 29 10:50:06 2003
-@@ -141,7 +141,7 @@
+--- noip2.c.orig	Fri Jan  2 18:14:09 2004
++++ noip2.c	Thu Jan  8 14:45:57 2004
+@@ -153,7 +153,7 @@
+ #define CLIENT_IP_PORT		8245
  
- #define VERSION			"2.0.12"
- #define NOIP_NAME		"dynupdate.no-ip.com"
--#define USER_AGENT		"User-Agent: Linux DUC "VERSION
-+#define USER_AGENT		"User-Agent: FreeBSD DUC "VERSION
+ #define VERSION			"2.1"
+-#define USER_AGENT		"User-Agent: Linux-DUC/"VERSION
++#define USER_AGENT		"User-Agent: FreeBSD-DUC/"VERSION
  #define SETTING_SCRIPT		"settings.php?"
  #define USTRNG			"username="
  #define PWDSTRNG		"&pass="
-@@ -246,7 +246,7 @@
+@@ -258,7 +258,7 @@
  #define CMSG21	"Please select the Internet interface from this list.\n"
  #define CMSG22	"By typing the number associated with it."
  #define CMSG23	"Too many network devices.  Limit is %d"
@@ -18,7 +18,7 @@
  #define CMSG25	"Can't create config file (%s)"
  #define CMSG25a	"Re-run noip, adding '-c configfilename' as a parameter."
  #define CMSG26	"Can't rename config file (%s)"
-@@ -438,7 +438,7 @@
+@@ -450,7 +450,7 @@
          fprintf(stderr, "[ -d][ -D pid]");
  #endif                                                                          
  	fprintf(stderr, "[ -i addr][ -S][ -M][ -h]");
@@ -27,9 +27,12 @@
  	fprintf(stderr, "Options: -C               create configuration data\n");
  	fprintf(stderr, "         -F               force NAT off\n");
  	fprintf(stderr, "         -Y               select all hosts/groups\n");
-@@ -1884,12 +1884,10 @@
+@@ -1935,17 +1935,12 @@
+ 	dq = devs;     // point at name list
  	for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
  		if (ifa->ifa_addr->sa_family == AF_LINK) {
+-// FreeBSD doesn't define some of these!
+-#ifdef IFT_PFLOG
  			struct if_data *ifd = (struct if_data *) ifa->ifa_data;
 -			if (ifd->ifi_type == IFT_PFLOG
 -				 || ifd->ifi_type == IFT_PFSYNC
@@ -38,9 +41,11 @@
 -				 || ifd->ifi_type == IFT_OTHER
 -				 || ifd->ifi_type == IFT_GIF)
 +			if (ifd->ifi_type == IFT_OTHER
-+				|| ifd->ifi_type == IFT_GIF
-+				|| ifd->ifi_type == IFT_LOOP
-+				|| ifd->ifi_type == IFT_FAITH)
++				 || ifd->ifi_type == IFT_GIF
++				 || ifd->ifi_type == IFT_LOOP
++				 || ifd->ifi_type == IFT_FAITH)
  		 	   continue;
+-#endif
  			q = dq;     // add new name into list
  			p = ifa->ifa_name;
+ 			devnum++;
