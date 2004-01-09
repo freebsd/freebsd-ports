8012133efe8daad9f1d54fa206f000968f2566dd
lev <lev@FreeBSD.org>
2004-01-09 16:59:36 +0000

    Update to subversion-0.35.1   Use old (0.9.4) version of apr instead of recommended 0.9.5.   Return BIG MESSAGE about repository dump/restore cycle.
      Based on PR form Roman Neuhauser <neuhauser@bellavista.cz>
    
    PR:		ports/61013
    Submitted by:	Roman Neuhauser <neuhauser@bellavista.cz>

--- devel/subversion-freebsd/files/patch-ac-helpers::ltmain.sh
+++ devel/subversion-freebsd/files/patch-ac-helpers::ltmain.sh
@@ -9,17 +9,4 @@
  	    # Do not include libc_r directly, use -pthread flag.
  	    continue
  	    ;;
-@@ -1212,7 +1212,12 @@
- 	;;
- 
-       -Kthread | -mthreads | -mt | -pthread | -pthreads | -threads | -qthreaded | -kthread )
--        compiler_flags="$compiler_flags $arg"
-+        pthread=yes
-+	arg=$arg
-+	linker_flags="$linker_flags $arg"
-+	compiler_flags="$compiler_flags $arg"
-+	compile_command="$compile_command $arg"
-+	finalize_command="$finalize_command $arg"
- 	continue
- 	;;
- 
+
