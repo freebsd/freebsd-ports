2f672b1fd86e8cc2bd37a8cf362f9aa4f57e9fed
ijliao <ijliao@FreeBSD.org>
2004-01-14 02:29:01 +0000

    add ayam 1.6 3D modeling environment for the RenderMan interface
    PR:		61286
    Submitted by:	Igor Pokrovsky <tiamat@comset.net>

---
+++ graphics/ayam/files/patch-ayam.tcl
@@ -0,0 +1,11 @@
+--- tcl/ayam.tcl.orig	Mon Jan 12 16:05:21 2004
++++ tcl/ayam.tcl	Mon Jan 12 16:06:01 2004
+@@ -391,7 +391,7 @@
+  pady 3
+  failsafe 0
+  showsplash 1
+- aysplash "../lib/ayam-splash.gif"
++ aysplash "%%DATADIR%%/ayam-splash.gif"
+  filename ""
+  tmpfiles ""
+  ayamrc "~/.ayamrc"
