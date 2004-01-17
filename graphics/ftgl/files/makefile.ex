bf939979164c6bee44237058e5467a9f28005820
krion <krion@FreeBSD.org>
2004-01-17 09:03:29 +0000

    Add ftgl 2.04,
    FTGL is a free open source library to enable developers to use
    arbitrary fonts in their OpenGL applications. Unlike other
    OpenGL font libraries FTGL uses standard font file formats so
    doesn't need a preprocessing step to convert the high quality
    font data into a lesser quality, proprietary format. FTGL uses
    the Freetype (www.freetype.org) font library to open and
    'decode' the fonts. It then takes that output and stores it in
    a format most efficient for OpenGL rendering.
    
    PR:		61456
    Submitted by:	Igor Pokrovsky <tiamat@comset.net>

---
+++ graphics/ftgl/files/makefile.ex
@@ -0,0 +1,28 @@
+# Make sure, that you have graphics/libglut installed
+# in order to build example program
+
+CC=		%%CC%%
+CXX=		%%CXX%%
+RM=		%%RM%%
+CFLAGS=		%%CFLAGS%% -I. -I%%X11BASE%%/include -I%%PREFIX%%/include/ftgl
+CXXFLAGS=	%%CXXFLAGS%% -I. -I%%X11BASE%%/include -I%%PREFIX%%/include/ftgl
+LDFLAGS=	-L%%X11BASE%%/lib -L%%PREFIX%%/lib -lglut -lftgl
+
+FREETYPE_CFLAGS!=%%LOCALBASE%%/bin/freetype-config --cflags
+FREETYPE_LFLAGS!=%%LOCALBASE%%/bin/freetype-config --libs
+
+CFLAGS+=	${FREETYPE_CFLAGS}
+CXXFLAGS+=	${FREETYPE_CFLAGS}
+LDFLAGS+=	${FREETYPE_LFLAGS}
+
+FTGLDemo: tb.o trackball.o FTGLDemo.o
+	${CXX} $> -o $@ ${LDFLAGS}
+
+.cpp.o:
+	${CXX} -c ${CXXFLAGS} $*.cpp
+
+.c.o:
+	${CC} -c ${CFLAGS} $*.c
+
+clean:
+	${RM} -f FTGLDemo *.o
