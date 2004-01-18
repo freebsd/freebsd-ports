bbd6b461de427e009b934486187e7b36a5d6651c
krion <krion@FreeBSD.org>
2004-01-18 18:21:17 +0000

    - Update to version 1.03
    PR:		61515
    Submitted by:	maintainer

--- misc/p5-File-CounterFile/files/patch-CounterFile.pm
+++ misc/p5-File-CounterFile/files/patch-CounterFile.pm
@@ -1,5 +1,5 @@
---- CounterFile.pm.orig	Tue Jul 30 23:45:06 2002
-+++ CounterFile.pm	Sun Jul  6 08:08:46 2003
+--- CounterFile.pm.orig	Sun Jan 18 13:31:18 2004
++++ CounterFile.pm	Sun Jan 18 13:31:54 2004
 @@ -19,7 +19,7 @@
  $DEFAULT_INITIAL = 0;                  # default initial counter value
  
@@ -9,12 +9,12 @@
  
  # Experimental overloading.
  use overload ('++'     => \&inc,
-@@ -205,7 +205,7 @@
+@@ -210,7 +210,7 @@
  If the file name does not start with "/" or ".", then it is
  interpreted as a file relative to C<$File::CounterFile::DEFAULT_DIR>.
  The default value for this variable is initialized from the
--environment variable C<TMPDIR>, or F</usr/tmp> is no environment
-+environment variable C<TMPDIR>, or F</var/tmp> is no environment
+-environment variable C<TMPDIR>, or F</usr/tmp> if no environment
++environment variable C<TMPDIR>, or F</var/tmp> if no environment
  variable is defined.  You may want to assign a different value to this
  variable before creating counters.
  
