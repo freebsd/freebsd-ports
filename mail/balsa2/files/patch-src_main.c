0877d5f96596cf67f6df20090f4204f48e06f2fa
marcus <marcus@FreeBSD.org>
2004-01-19 07:04:57 +0000

    Update to 2.0.16.

--- mail/balsa2/files/patch-src_main.c
+++ mail/balsa2/files/patch-src_main.c
@@ -1,11 +1,11 @@
---- src/main.c.orig	Mon Sep 15 06:45:59 2003
-+++ src/main.c	Thu Oct  2 01:50:21 2003
-@@ -150,7 +150,7 @@
+--- src/main.c.orig	Sun Jan 18 06:39:47 2004
++++ src/main.c	Sun Jan 18 21:38:15 2004
+@@ -233,7 +233,7 @@
      gnome_program_init(PACKAGE, VERSION, LIBGNOMEUI_MODULE, argc, argv,
                         GNOME_PARAM_POPT_TABLE, options,
                         GNOME_PARAM_APP_PREFIX, BALSA_STD_PREFIX,
 -                       GNOME_PARAM_APP_DATADIR, BALSA_STD_PREFIX "/share",
 +                       GNOME_PARAM_APP_DATADIR, BALSA_STD_PREFIX "/share/gnome",
+ 		       GNOME_PARAM_HUMAN_READABLE_NAME, _("The Balsa E-Mail Client"),
                         NULL);
- }
  
