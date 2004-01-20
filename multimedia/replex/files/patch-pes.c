e638b9b3070aabe2aac00ac4897e75a99d159b9a
Dirk Meyer <dinoex@FreeBSD.org>
2004-01-20 04:45:05 +0000

    Replex was created to remultiplex transport stream (TS) data taken from a DVB source. The result is supposed to be a program stream (PS) that can be used to be burned to a DVD (with dvdauthor). Replex can also remultiplex other PSs and AVIs with MPEG2 content.
    WWW: http://www.metzlerbros.org/dvb/

---
+++ multimedia/replex/files/patch-pes.c
@@ -0,0 +1,10 @@
+--- pes.c.orig	Sun Dec 28 17:57:19 2003
++++ pes.c	Tue Jan 20 05:15:09 2004
+@@ -26,6 +26,7 @@
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
++#include <sys/types.h>
+ #include <netinet/in.h>
+ #include <string.h>
+ 
