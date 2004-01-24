b7b820b88661643127d2e6aabffd43aad2b123a8
pav <pav@FreeBSD.org>
2004-01-24 14:33:03 +0000

    - Fix build on 5.x

---
+++ games/clanbomber/files/patch-clanbomber-Controller_AI.cpp
@@ -0,0 +1,11 @@
+--- Controller_AI.cpp.orig	Wed Aug  8 23:46:14 2001
++++ Controller_AI.cpp	Sat Jan 24 15:07:58 2004
+@@ -19,6 +19,8 @@
+ 
+ //#define WITH_DEBUG
+ 
++#include <assert.h>
++
+ #include "ClanBomber.h"
+ #include "Controller_AI.h"
+ 
