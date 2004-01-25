4b9c33ef06514835fca0b176c9f7fbbd114bb647
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-01-25 14:25:59 +0000

    fix build on -stable
    Noticed by:	bento via kris

---
+++ devel/codeworker/files/patch-DtaPatternScript.h
@@ -0,0 +1,10 @@
+--- DtaPatternScript.h.orig	Sun Jan 25 21:36:50 2004
++++ DtaPatternScript.h	Sun Jan 25 21:40:17 2004
+@@ -24,6 +24,7 @@
+ 
+ #include "DtaScript.h"
+ #include <map>
++#include <memory>
+ 
+ namespace CodeWorker {
+ 	#ifndef EXECUTE_FUNCTION_TYPE
