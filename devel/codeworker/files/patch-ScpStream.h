3bc86728069f4f0abf310333baf71a3717bfb54d
ijliao <ijliao@FreeBSD.org>
2004-01-25 14:25:59 +0000

    fix build on -stable
    Noticed by:	bento via kris

---
+++ devel/codeworker/files/patch-ScpStream.h
@@ -0,0 +1,11 @@
+--- ScpStream.h.orig	Sun Jan 25 21:34:17 2004
++++ ScpStream.h	Sun Jan 25 21:34:40 2004
+@@ -34,6 +34,8 @@
+ #include <map>
+ #include <set>
+ 
++#include <ctime>
++
+ namespace CodeWorker {
+ 
+ #ifdef IN
