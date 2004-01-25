2b42db0479f8233e1ed088a07c43b59cb60f4546
Kirill Ponomarev <krion@FreeBSD.org>
2004-01-25 16:26:04 +0000

    - Update to version 0.9.0

---
+++ astro/libnova/files/patch-src::misc.c
@@ -0,0 +1,12 @@
+--- src/misc.c.orig	Sun Jan 25 17:18:32 2004
++++ src/misc.c	Sun Jan 25 17:18:42 2004
+@@ -44,9 +44,6 @@
+ #include <ctype.h>
+ #include "libnova.h"
+ 
+-#ifndef __APPLE__
+-#include <malloc.h>
+-#endif
+ 
+ /* local types and macros */
+ typedef int BOOL;
