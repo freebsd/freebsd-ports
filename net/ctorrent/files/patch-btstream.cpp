4625fd61032946330ab6c57251049868a3013375
Norikatsu Shigemura <nork@FreeBSD.org>
2004-01-26 08:47:44 +0000

    Fix build on alpha. Accordingly(by author), update distfiles. So, bump PORTREVISION instead of.
    Reported by:	bento via kris
    Submitted by:	Florent Thoumie <flz@xbsd.org> (maintainer)

--- net/ctorrent/files/patch-btstream.cpp
+++
@@ -1,7 +0,0 @@
---- btstream.cpp.orig	Fri Jan 16 00:01:00 2004
-+++ btstream.cpp	Sun Jan 18 02:42:47 2004
-@@ -1,3 +1,4 @@
-+#include <arpa/inet.h>
- #include "btstream.h"
- #include "msgencode.h"
- #include "btconfig.h"
--- net/ctorrent/files/patch-iplist.cpp
+++
@@ -1,7 +0,0 @@
---- iplist.cpp.orig	Sun Jan 11 05:51:45 2004
-+++ iplist.cpp	Sun Jan 18 02:43:29 2004
-@@ -1,3 +1,4 @@
-+#include <string.h>
- #include "iplist.h"
- 
- IpList IPQUEUE;
