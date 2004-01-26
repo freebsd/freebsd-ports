0960eec48deef335432709b99af24425aa82a9d6
nork <nork@FreeBSD.org>
2004-01-26 08:47:44 +0000

    Fix build on alpha. Accordingly(by author), update distfiles. So, bump PORTREVISION instead of.
    Reported by:	bento via kris
    Submitted by:	Florent Thoumie <flz@xbsd.org> (maintainer)

--- net/ctorrent/files/patch-httpencode.cpp
+++ net/ctorrent/files/patch-httpencode.cpp
@@ -1,14 +1,16 @@
---- httpencode.cpp.orig	Sun Jan 11 05:51:45 2004
-+++ httpencode.cpp	Sun Jan 18 02:43:16 2004
-@@ -10,6 +10,7 @@
+diff -u -r CTorrent-1.3r1/httpencode.cpp CTorrent-1.3r1.new/httpencode.cpp
+--- httpencode.cpp	Sat Jan 10 20:51:45 2004
++++ httpencode.cpp.new	Fri Jan 23 17:22:28 2004
+@@ -10,6 +10,8 @@
  /* NOTE: */
  /* LINUX/WINDOWS IMPLEMENT's STANDARD C LIBRARY NO strnstr FOUND */
  /* FUNCTION PROGRAMER: Siberiaic Sang */
-+#if 0
++
++#if defined(__win32__) || defined(__linux__)
  static char* strnstr(const char *haystack,const char *needle,size_t haystacklen)
  {
     char *p;
-@@ -30,6 +31,7 @@ static char* strnstr(const char *haystac
+@@ -30,6 +32,7 @@
    }
    return (char*) 0;
  }
