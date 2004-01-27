4ef07d396ea4911e4be5f529b38cf1676a6cb092
Kirill Ponomarev <krion@FreeBSD.org>
2004-01-27 15:58:52 +0000

    Add wmget 0.5.0,
    wmget is a dock app for the GNU Window Maker window manager
    which makes it more convenient to perform long downloads in the
    background.
    
    PR:		ports/61736
    Submitted by:	Ulrich Spoerlein <q@uni.de>

---
+++ ftp/wmget/files/patch-configure.c
@@ -0,0 +1,10 @@
+--- configure.c.orig	Mon Jan  5 18:16:22 2004
++++ configure.c	Mon Jan  5 18:16:32 2004
+@@ -30,7 +30,6 @@
+ */
+ 
+ #include <string.h>
+-#include <malloc.h>
+ #include <ctype.h>
+ #include <getopt.h>
+ #include <stdlib.h>
