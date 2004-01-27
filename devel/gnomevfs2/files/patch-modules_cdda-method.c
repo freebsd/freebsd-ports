7a392264285f3d755f07257c13a6f5fd2d7cc8ed
bland <bland@FreeBSD.org>
2004-01-27 15:55:15 +0000

    Update to 2.4.2

--- devel/gnomevfs2/files/patch-modules_cdda-method.c
+++ devel/gnomevfs2/files/patch-modules_cdda-method.c
@@ -1,7 +1,7 @@
---- modules/cdda-method.c.orig	Wed Jan 22 13:54:31 2003
-+++ modules/cdda-method.c	Wed Jan 22 13:54:33 2003
+--- modules/cdda-method.c.orig	Tue Jan 27 06:19:01 2004
++++ modules/cdda-method.c	Wed Jan 28 00:21:01 2004
 @@ -623,7 +623,11 @@
- 	// Check and see if we already have opened and stashed this drive
+ 	/* Check and see if we already have opened and stashed this drive */
  	if (!use_base) {
  		if (global_context != NULL) {
 +#ifdef Linux
@@ -14,13 +14,13 @@
  				gnome_vfs_file_info_copy (file_info, global_context->file_info);
 @@ -709,7 +713,11 @@
  	if (!use_base) {
- 		// Check for cache
+ 		/* Check for cache */
  		if (global_context != NULL) {
 +#ifdef Linux
  				if (strcmp (drive->cdda_device_name, global_context->drive->cdda_device_name) != 0) {
 +#elif defined(__FreeBSD__)
 +				if (strcmp (drive->dev->device_path, global_context->drive->dev->device_path) != 0) {
 +#endif
- 					//	Clear old cache
+ 					/*	Clear old cache */
  					cdda_context_free (global_context);
  					global_context = cdda_context_new (drive, uri);
