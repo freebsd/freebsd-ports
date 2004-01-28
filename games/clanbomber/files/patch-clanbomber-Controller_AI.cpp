62f861eb553dec55748aba98fd4d895a03f1c937
pav <pav@FreeBSD.org>
2004-01-28 22:47:45 +0000

    - Fix patching
    Submitted by:	bento via kris

--- games/clanbomber/files/patch-clanbomber-Controller_AI.cpp
+++ games/clanbomber/files/patch-clanbomber-Controller_AI.cpp
@@ -1,5 +1,5 @@
---- Controller_AI.cpp.orig	Wed Aug  8 23:46:14 2001
-+++ Controller_AI.cpp	Sat Jan 24 15:07:58 2004
+--- clanbomber/Controller_AI.cpp.orig	Wed Aug  8 23:46:14 2001
++++ clanbomber/Controller_AI.cpp	Sat Jan 24 15:07:58 2004
 @@ -19,6 +19,8 @@
  
  //#define WITH_DEBUG
