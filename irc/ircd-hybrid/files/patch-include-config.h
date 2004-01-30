202ccd570080fc7148daba5a86b3457f8dd2c0f6
Erwin Lansing <erwin@FreeBSD.org>
2004-01-30 07:09:26 +0000

    Fix BROKEN.
    ircd-hybrid now complies with hier(7) and runs. Good work, Brad!
    
    Submitted by:	maintainer

--- irc/ircd-hybrid/files/patch-include-config.h
+++ irc/ircd-hybrid/files/patch-include-config.h
@@ -1,24 +1,32 @@
---- include/config.h.ori	Wed Jan 28 01:57:26 2004
-+++ include/config.h	Wed Jan 28 02:03:47 2004
-@@ -93,8 +93,10 @@
+--- include/config.h.orig	Thu Feb  6 01:46:11 2003
++++ include/config.h	Thu Jan 29 20:16:13 2004
+@@ -90,7 +90,9 @@
+ /*#define IRCD_PREFIX     "IRCD$BASEDIR:"*/
+ #define DPATH           "IRCD$BASEDIR:"
+ #define BINPATH         "IRCD$BINDIR:"
++/*
  #define ETCPATH         "IRCD$CONFDIR:"
++*/
  #define LOGPATH         "IRCD$LOGDIR:"
  
-+/*
  #undef  MODPATH
- #undef  AUTOMODPATH
-+*/
- #undef  MSGPATH
- 
- #define SPATH   BINPATH "IRCD.EXE"              /* server executable */
-@@ -134,8 +136,10 @@
+@@ -134,13 +136,13 @@
  /* dirs */
  #define DPATH   IRCD_PREFIX                                                     
  #define BINPATH IRCD_PREFIX "/bin/"
-+/*
- #define MODPATH IRCD_PREFIX "/modules/"
- #define AUTOMODPATH IRCD_PREFIX "/modules/autoload/"
-+*/
- #define MSGPATH IRCD_PREFIX "/messages/"
- #define ETCPATH IRCD_PREFIX "/etc"
+-#define MODPATH IRCD_PREFIX "/modules/"
+-#define AUTOMODPATH IRCD_PREFIX "/modules/autoload/"
+-#define MSGPATH IRCD_PREFIX "/messages/"
+-#define ETCPATH IRCD_PREFIX "/etc"
++#define MODPATH IRCD_PREFIX "/share/ircd-hybrid-7.0/modules/"
++#define AUTOMODPATH IRCD_PREFIX "/share/ircd-hybrid-7.0/modules/autoload/"
++#define MSGPATH IRCD_PREFIX "/share/ircd-hybrid-7.0/messages/"
++#define ETCPATH IRCD_PREFIX "/etc/ircd-hybrid"
  #define LOGPATH IRCD_PREFIX "/logs"
+-#define UHPATH   IRCD_PREFIX "/help/users"
+-#define HPATH  IRCD_PREFIX "/help/opers"
++#define UHPATH   IRCD_PREFIX "/share/ircd-hybrid-7.0/help/users"
++#define HPATH  IRCD_PREFIX "/share/ircd-hybrid-7.0/help/opers"
+ 
+ /* files */
+ #define SPATH   BINPATH "/ircd"                 /* ircd executable */
