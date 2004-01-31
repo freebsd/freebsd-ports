0aeca78601c2e4a1e1c5151c494dfacb26102419
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-01-31 18:25:23 +0000

    Fix the build on Alpha -STABLE.
    Reported by:	bento via kris

--- devel/gnomebuild/files/patch-src_backends_libgbf_am_gbf-am-project.c
+++ devel/gnomebuild/files/patch-src_backends_libgbf_am_gbf-am-project.c
@@ -1,11 +1,11 @@
---- src/backends/libgbf_am/gbf-am-project.c.orig	Sun Oct 26 01:24:42 2003
-+++ src/backends/libgbf_am/gbf-am-project.c	Sun Oct 26 01:32:23 2003
+--- src/backends/libgbf_am/gbf-am-project.c.orig	Fri May  2 06:12:51 2003
++++ src/backends/libgbf_am/gbf-am-project.c	Sat Jan 31 13:22:04 2004
 @@ -1750,7 +1750,7 @@
  
  		switch (status) {
  		    case G_IO_STATUS_NORMAL:
 -			    DEBUG (g_message ("wrote %d bytes", bytes_written));
-+			    DEBUG (g_message ("wrote %zu bytes", bytes_written));
++			    DEBUG (g_message ("wrote %lu bytes", bytes_written));
  			    
  			    if (data->input.length < data->input.size) {
  				    /* don't remove the source */
