ed551081104da4dd69696f0b547cd1a139a06ea1
nork <nork@FreeBSD.org>
2004-02-01 18:14:46 +0000

    Update to 5.0.
    Requested by:	many people
    Sponsored by:	Ensure Technology Ltd.

--- net/DarwinStreamingServer/files/patch-WebAdmin::src::streamingadminserver.pl
+++ net/DarwinStreamingServer/files/patch-WebAdmin::src::streamingadminserver.pl
@@ -1,15 +1,6 @@
---- WebAdmin/src/streamingadminserver.pl.orig	Wed Apr 24 09:22:15 2002
-+++ WebAdmin/src/streamingadminserver.pl	Mon Sep 30 02:48:53 2002
-@@ -43,7 +43,7 @@
- }
- else
- {
--    eval "use Net::SSleay";
-+    eval "use Net::SSLeay";
- }
- $ssl_available = 0;
- if (!$@) {
-@@ -241,7 +241,7 @@
+--- WebAdmin/src/streamingadminserver.pl.orig	Tue Oct 28 09:43:24 2003
++++ WebAdmin/src/streamingadminserver.pl	Sun Jan 18 07:25:04 2004
+@@ -255,7 +255,7 @@ else {
  	  "gbrowse", "0",
  	  "ssl", "0",
  	  "crtfile", "/etc/streaming/streamingadminserver.pem",
@@ -18,12 +9,3 @@
  	  #"keypasswordfile", "",
  	  "qtssQTPasswd", "/usr/local/bin/qtpasswd",
  	  "qtssPlaylistBroadcaster", "/usr/local/bin/PlaylistBroadcaster",
-@@ -249,7 +249,7 @@
- 	  "helpurl", "http://helpqt.apple.com/dssWebAdminHelpR3/dssWebAdmin.help/DSSHelp.htm",
- 	  "qtssAdmin", "streamingadmin",
- 	  "cacheMessageFiles", "0",
--	  #"pidfile", "/var/streaming/streamingadminserver.pid"
-+	  "pidfile", "/var/run/streamingadminserver.pid"
- 	  );
- }
- 
