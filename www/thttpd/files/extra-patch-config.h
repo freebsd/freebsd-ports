4d952daecc0ae10094143c6d51a31983f2f53457
silby <silby@FreeBSD.org>
2004-02-01 04:51:54 +0000

    Enable sendfile support by default for 5.2+ systems, as we now have alc's improved sendfile and the ability to watch usage via netstat -m.

---
+++ www/thttpd/files/extra-patch-config.h
@@ -0,0 +1,9 @@
+--- config.h.old	Sat Jan 31 22:17:25 2004
++++ config.h	Sat Jan 31 22:17:38 2004
+@@ -380,4 +380,6 @@
+ */
+ #define MIN_WOULDBLOCK_DELAY 100L
+ 
++#define USE_SENDFILE
++
+ #endif /* _CONFIG_H_ */
