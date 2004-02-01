ec1ccec80a5db69858a776a30a257a80f2e031d2
pav <pav@FreeBSD.org>
2004-02-01 18:06:53 +0000

    - Update to 0.5.0
    PR:		ports/62159
    Submitted by:	Ports Fury

---
+++ graphics/osgedit/files/patch-src::editable::entities::edgeode.cc
@@ -0,0 +1,8 @@
+--- src/editable/entities/edgeode.cc.orig	Fri Oct 31 08:32:03 2003
++++ src/editable/entities/edgeode.cc	Sat Jan 31 12:44:22 2004
+@@ -1,3 +1,5 @@
++#include <stdio.h>
++
+ #include <editable/entityfactory.h>
+ #include "edgeometry.h"
+ #include "edgeode.h"
