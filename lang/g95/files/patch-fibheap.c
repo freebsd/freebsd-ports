20b4f7ed1a02029c895c5126a1b8b8eacac6700f
maho <maho@FreeBSD.org>
2004-02-01 23:20:23 +0000

    Add New port lang/g95 GNU Fortran 90/95 Compiler from GCC's tree-ssa-20020619-branch and still this is alpha stage. you can invoke Fortran by typing gfortran or g95,
    Note: gcc-3.5 will include this effort.

---
+++ lang/g95/files/patch-fibheap.c
@@ -0,0 +1,14 @@
+--- libiberty/fibheap.c.org	Sat Jan 31 14:51:11 2004
++++ libiberty/fibheap.c	Sat Jan 31 14:51:28 2004
+@@ -22,9 +22,9 @@
+ #ifdef HAVE_CONFIG_H
+ #include "config.h"
+ #endif
+-#ifdef HAVE_LIMITS_H
++//#ifdef HAVE_LIMITS_H
+ #include <limits.h>
+-#endif
++//#endif
+ #ifdef HAVE_STDLIB_H
+ #include <stdlib.h>
+ #endif
