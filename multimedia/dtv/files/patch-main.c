16f9b1e8c9fe9301c6dadf548686a408edc16194
Kirill Ponomarev <krion@FreeBSD.org>
2004-02-01 19:04:25 +0000

    - Fix build on AMD64 - Pet portlint

---
+++ multimedia/dtv/files/patch-main.c
@@ -0,0 +1,10 @@
+--- main.c.orig	Sun Feb  1 19:51:55 2004
++++ main.c	Sun Feb  1 19:52:11 2004
+@@ -11,6 +11,7 @@
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
+ #include <netdb.h>
++#include <sys/time.h>
+ 
+ Prototype int VerboseOpt;
+ 
