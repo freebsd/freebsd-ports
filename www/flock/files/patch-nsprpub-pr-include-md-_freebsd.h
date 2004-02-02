a49e622081fcd807fe50ce9b087d62c78045110f
marcel <marcel@FreeBSD.org>
2004-02-02 03:40:14 +0000

    Port ia64 support from www/mozilla.

--- www/flock/files/patch-nsprpub-pr-include-md-_freebsd.h
+++ www/flock/files/patch-nsprpub-pr-include-md-_freebsd.h
@@ -1,11 +1,13 @@
---- nsprpub/pr/include/md/_freebsd.h	Thu Jan 16 15:09:12 2003
-+++ nsprpub/pr/include/md/_freebsd.h	Sat Dec 13 13:02:49 2003
-@@ -48,6 +48,10 @@
+--- nsprpub/pr/include/md/_freebsd.h.orig	Thu Jan 16 15:09:12 2003
++++ nsprpub/pr/include/md/_freebsd.h	Sun Feb  1 15:21:56 2004
+@@ -48,6 +48,12 @@
  #define _PR_SI_ARCHITECTURE "x86"
  #elif defined(__alpha)
  #define _PR_SI_ARCHITECTURE "alpha"
 +#elif defined(__sparc__)
 +#define _PR_SI_ARCHITECTURE "sparc"
++#elif defined(__ia64__)
++#define _PR_SI_ARCHITECTURE "ia64"
 +#elif defined(__amd64__)
 +#define _PR_SI_ARCHITECTURE "amd64"
  #else
