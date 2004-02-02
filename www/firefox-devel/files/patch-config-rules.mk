fb325c73d69d382ee54ff03110ff89184c7a9403
Marcel Moolenaar <marcel@FreeBSD.org>
2004-02-02 03:40:14 +0000

    Port ia64 support from www/mozilla.

--- www/firefox-devel/files/patch-config-rules.mk
+++ www/firefox-devel/files/patch-config-rules.mk
@@ -1,10 +1,10 @@
---- config/rules.mk.orig	Mon May  5 21:04:55 2003
-+++ config/rules.mk	Tue May 27 01:33:11 2003
+--- config/rules.mk.orig	Tue Aug 26 13:28:47 2003
++++ config/rules.mk	Sun Feb  1 15:11:41 2004
 @@ -444,7 +444,11 @@
  
  ifeq ($(OS_ARCH),FreeBSD)
  ifdef IS_COMPONENT
-+ifneq (,$(filter alpha,$(OS_TEST)))
++ifneq (,$(filter alpha ia64,$(OS_TEST)))
 +EXTRA_DSO_LDOPTS += -Wl,-Bsymbolic -lc
 +else
  EXTRA_DSO_LDOPTS += -Wl,-Bsymbolic
