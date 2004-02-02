63e0320cf0369bba41316771a89e668c00ddac42
ijliao <ijliao@FreeBSD.org>
2004-02-02 07:49:25 +0000

    add wxmozilla 0.5.3 Embedding Mozilla in wxWindows

---
+++ x11-toolkits/wxmozilla/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Mon Feb  2 14:01:02 2004
++++ ltmain.sh	Mon Feb  2 14:01:19 2004
+@@ -4394,10 +4394,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
