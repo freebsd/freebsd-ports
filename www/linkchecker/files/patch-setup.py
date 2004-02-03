a523ac520343e3ceaef900638760d93550616583
Clement Laforet <clement@FreeBSD.org>
2004-02-03 09:54:23 +0000

    - Update to 1.12.0

--- www/linkchecker/files/patch-setup.py
+++ www/linkchecker/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	Wed Oct 22 11:25:31 2003
-+++ setup.py	Thu Nov 20 02:02:19 2003
-@@ -192,12 +192,12 @@
+--- setup.py.orig	Thu Jan 29 19:14:40 2004
++++ setup.py	Tue Feb  3 09:42:34 2004
+@@ -194,13 +194,13 @@
           ('share/locale/nl/LC_MESSAGES',
               ['share/locale/nl/LC_MESSAGES/linkcheck.mo']),
           ('share/linkchecker', ['linkcheckerrc']),
@@ -9,7 +9,8 @@
               ['lconline/leer.html.en', 'lconline/leer.html.de',
                'lconline/index.html', 'lconline/lc_cgi.html.en',
                'lconline/lc_cgi.html.de', 'lconline/check.js',
-               'lc.cgi','lc.fcgi','lc.sz_fcgi','linkchecker.bat']),
+               'lc.cgi', 'lc.fcgi', 'lc.sz_fcgi', 'linkchecker.bat',
+               'linkchecker-completion', 'linkcheck-cron.sh']),
 -         ('share/man/man1', ['linkchecker.1']),
 +         ('man/man1', ['linkchecker.1']),
        ],
