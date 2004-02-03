73247a12347fedd6ec5706373a6df34800ad0ef4
Max Khon <fjoe@FreeBSD.org>
2004-02-03 20:31:35 +0000

    Fix build on 64-bit platforms.

--- devel/cvsnt/files/patch-lockservice-server.cpp
+++ devel/cvsnt/files/patch-lockservice-server.cpp
@@ -1,5 +1,5 @@
---- lockservice/server.cpp.orig	Sun Jan 25 20:42:11 2004
-+++ lockservice/server.cpp	Sun Jan 25 20:42:29 2004
+--- lockservice/server.cpp.orig	Thu Mar 13 04:28:10 2003
++++ lockservice/server.cpp	Tue Feb  3 11:35:11 2004
 @@ -8,7 +8,10 @@
  #include <ws2tcpip.h>
  #define SOCKET_ERRNO WSAGetLastError()
@@ -11,3 +11,12 @@
  #include <netdb.h>
  #include <errno.h>
  #include <unistd.h>
+@@ -162,7 +165,7 @@
+ #ifdef _WIN32
+ 		int addrlen;
+ #else
+-		size_t addrlen;
++		socklen_t addrlen;
+ #endif
+  		int maxdesc = -1;
+ 
