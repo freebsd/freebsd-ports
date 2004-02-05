ba3455bcaeeac361a5db7e74146d92954c5b1bb7
Maxim Sobolev <sobomax@FreeBSD.org>
2004-02-05 19:57:37 +0000

    Update to 0.8.12.

--- net/ser/files/patch-modules::auth_radius::authrad_mod.c
+++ net/ser/files/patch-modules::auth_radius::authrad_mod.c
@@ -1,14 +1,14 @@
 
 $FreeBSD$
 
---- modules/auth_radius/authrad_mod.c.orig	Sat May  3 00:36:29 2003
-+++ modules/auth_radius/authrad_mod.c	Mon Sep  8 01:28:58 2003
+--- modules/auth_radius/authrad_mod.c	2004/02/05 19:49:04	1.1
++++ modules/auth_radius/authrad_mod.c	2004/02/05 19:49:21
 @@ -59,7 +59,7 @@
  /*
   * Module parameter variables
   */
 -char* radius_config = "/usr/local/etc/radiusclient/radiusclient.conf";
 +char* radius_config = (CFG_DIR "radiusclient.conf");
- int service_type = PW_SIP;
+ int service_type = PW_SIP_SESSION;
  
  
--- net/ser/files/patch-modules::tm::t_lookup.c
+++ net/ser/files/patch-modules::tm::t_lookup.c
@@ -1,25 +1,25 @@
 
 $FreeBSD$
 
---- modules/tm/t_lookup.c	2003/07/29 04:01:00	1.1
-+++ modules/tm/t_lookup.c	2003/07/29 04:05:47
-@@ -94,8 +94,8 @@
+--- modules/tm/t_lookup.c.orig	Thu Aug 21 22:33:58 2003
++++ modules/tm/t_lookup.c	Mon Dec 15 20:59:56 2003
+@@ -96,8 +96,8 @@
  #include "t_lookup.h"
  
  #define EQ_VIA_LEN(_via)\
 -	( (p_msg->via1->bsize-(p_msg->_via->name.s-(p_msg->_via->hdr.s+p_msg->_via->hdr.len)))==\
 -	  	(t_msg->via1->bsize-(t_msg->_via->name.s-(t_msg->_via->hdr.s+t_msg->_via->hdr.len))) )
-+	( (p_msg->_via->port_str.s + p_msg->_via->port_str.len - p_msg->_via->name.s)==\
-+		(t_msg->_via->port_str.s + t_msg->_via->port_str.len - t_msg->_via->name.s) )
++	( (p_msg->_via->port_str.len > 0 ? (p_msg->_via->port_str.s + p_msg->_via->port_str.len - p_msg->_via->name.s) : p_msg->_via->name.len) ==\
++		(t_msg->_via->port_str.len > 0 ? (t_msg->_via->port_str.s + t_msg->_via->port_str.len - t_msg->_via->name.s) : t_msg->_via->name.len) )
  
  
  
-@@ -113,7 +113,7 @@
+@@ -115,7 +115,7 @@
  #define EQ_VIA_STR(_via)\
  	( memcmp( t_msg->_via->name.s,\
  	 p_msg->_via->name.s,\
 -	 (t_msg->via1->bsize-(t_msg->_via->name.s-(t_msg->_via->hdr.s+t_msg->_via->hdr.len)))\
-+	 (t_msg->_via->port_str.s+t_msg->_via->port_str.len - t_msg->_via->name.s)\
++	 (t_msg->_via->port_str.len > 0 ? (t_msg->_via->port_str.s+t_msg->_via->port_str.len - t_msg->_via->name.s) : t_msg->_via->name.len) \
  	)==0 )
  
  
