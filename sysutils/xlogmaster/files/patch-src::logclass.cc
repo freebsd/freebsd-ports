05079e44280b32f0ad87246d3622a87caea6c6c2
Kirill Ponomarev <krion@FreeBSD.org>
2004-02-06 14:04:12 +0000

    - Fix build on -current - Utilize INFO
    PR:		ports/62434
    Submitted by:	Ports Fury

---
+++ sysutils/xlogmaster/files/patch-src::logclass.cc
@@ -0,0 +1,11 @@
+--- src/logclass.cc.orig	Thu Apr  1 01:00:29 1999
++++ src/logclass.cc	Thu Feb  5 21:39:55 2004
+@@ -37,6 +37,8 @@
+ #include "../import/import.h"
+ #include "logclass.H"
+ #include "extern.H"
++#include <iostream>
++using namespace std;
+ 
+ /*}}}*/
+ 
