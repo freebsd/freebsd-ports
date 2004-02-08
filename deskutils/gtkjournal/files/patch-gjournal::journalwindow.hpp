092fd95ac007480b630f58b163316a0f78f420b6
Adam Weinberger <adamw@FreeBSD.org>
2004-02-08 06:36:43 +0000

    Add gtkjournal.
    Gtk+ Journal is a Journal/Diary application written using the C++ bindings for
    Gtk+-2 (gtkmm). It uses SQlite to store your entries, and the OpenSSL
    implementations of the BlowFish and SHA-1 algorithms to keep your data safe.

---
+++ deskutils/gtkjournal/files/patch-gjournal::journalwindow.hpp
@@ -0,0 +1,10 @@
+--- gjournal/journalwindow.hpp.orig	Sun Feb  8 00:09:23 2004
++++ gjournal/journalwindow.hpp	Sun Feb  8 00:09:41 2004
+@@ -19,6 +19,7 @@
+ #ifndef JOURNAL_WINDOW_HPP
+ #define JOURNAL_WINDOW_HPP
+ 
++#include <libgen.h>
+ #include <gtkmm.h>
+ #include <iostream>
+ #include <map>
