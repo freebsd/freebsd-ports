4bd9a44945281398d2631dfea7ad6eb8aa2190a3
Tilman Keskinoz <arved@FreeBSD.org>
2004-02-09 20:08:28 +0000

    Fix a problem with KDE 3.2
    PR:		62558
    Submitted by:	Markus Brueffer <brueffer@phoenix-systems.de>

--- deskutils/superkaramba/files/patch-karamba.cpp
+++ deskutils/superkaramba/files/patch-karamba.cpp
@@ -1,5 +1,14 @@
---- src/karamba.cpp.orig	2003-11-07 01:15:20.000000000 -0500
-+++ src/karamba.cpp	2003-11-07 02:30:39.000000000 -0500
+--- src/karamba.cpp.orig	Sun Oct 19 22:02:45 2003
++++ src/karamba.cpp	Mon Feb  9 00:57:18 2004
+@@ -2243,7 +2243,7 @@
+ 
+ 
+     //Matthew Kay: set window type to "dock" (plays better with taskbar themes this way)
+-    KWin::setType(winId(), NET::Dock);
++    //KWin::setType(winId(), NET::Dock);
+ 
+     widgetMask = 0;
+     info = new NETWinInfo( qt_xdisplay(), winId(), qt_xrootwin(), NET::WMState );
 @@ -2662,6 +2662,26 @@
  
                  }
