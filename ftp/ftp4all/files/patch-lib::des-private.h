3ce4ee98a8addde9690843e522c882544493b66e
krion <krion@FreeBSD.org>
2004-02-09 18:48:54 +0000

    - Fix build on AMD64
    Approved by:	maintainer informed

--- ftp/ftp4all/files/patch-lib::des-private.h
+++ ftp/ftp4all/files/patch-lib::des-private.h
@@ -1,10 +1,13 @@
---- lib/des-private.h.orig	Tue May  6 07:48:47 2003
-+++ lib/des-private.h	Tue May  6 07:28:57 2003
-@@ -10,6 +10,9 @@
+--- lib/des-private.h.orig	Thu Sep 24 15:02:11 1998
++++ lib/des-private.h	Mon Feb  9 19:41:07 2004
+@@ -10,6 +10,12 @@
  #ifdef i386
  #define LITTLE_ENDIAN 1
  #endif
 +#ifdef __ia64
++#define LITTLE_ENDIAN 1
++#endif
++#ifdef __amd64
 +#define LITTLE_ENDIAN 1
 +#endif
  #ifdef sun386
