8b452d3dd2f6bc94d1601abcb218521c4c9bbb8b
bland <bland@FreeBSD.org>
2004-02-16 08:46:10 +0000

    Update to 0.37

---
+++ graphics/inkscape/files/patch-src::libnr::nr-matrix.cpp
@@ -0,0 +1,10 @@
+--- src/libnr/nr-matrix.cpp.org Sun Feb 15 09:45:06 2004
++++ src/libnr/nr-matrix.cpp     Sun Feb 15 09:44:32 2004
+@@ -9,6 +9,7 @@
+  * This code is in public domain
+  */
+ 
++#include <stdio.h>
+ #include "nr-matrix.h"
+ 
+ NRMatrix *
