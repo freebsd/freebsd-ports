eb96eec63fb0fa3531f07fd8ad79a52d52ef5d67
sergei <sergei@FreeBSD.org>
2004-02-18 12:25:09 +0000

    - add two patches from squid-cache.org - synchronize as much as possible with www/squid - add SQUID_{U,G}ID and SQUID_LANGUAGE tunables - utilize OPTIONS, PLIST_FILES, PLIST_DIRS, and USE_SIZE - add a patch to make the LDAP helper at least compile - add NO_LATEST_LINK and mark the port DEPRECATED - bump PORTREVISION
    PR:		ports/62443
    Submitted by:	maintainer

--- www/squid24/files/squid.sh
+++ www/squid24/files/squid.sh
@@ -1,26 +1,75 @@
 #!/bin/sh
+#
+# $FreeBSD: /tmp/pcvs/ports/www/squid24/files/Attic/squid.sh,v 1.4 2004-02-18 12:25:09 sergei Exp $
+#
+# PROVIDE: squid
+# REQUIRE: NETWORKING SERVERS
+# BEFORE: DAEMON
+# KEYWORD: FreeBSD
+# 
+# Note:
+# If you are running an rcNG-System (i.e. FreeBSD 5 and later or after
+# having installed the rc_subr-port on an earlier system) you must set
+# "squid_enable=YES" in either /etc/rc.conf, /etc/rc.conf.local or
+# /etc/rc.conf.d/squid to make this script actually do something. There
+# you can also set squid_chdir, squid_user, and squid_flags.
+#
+# Please see squid(8), rc.conf(5) and rc(8) for further details.
 
-PREFIX=%%PREFIX%%
+unset rcNG
+name="squid"
+command=%%PREFIX%%/sbin/squid
+extra_commands=reload
+reload_cmd="${command} -k reconfigure"
+stop_cmd="${command} -k shutdown"
+: ${squid_chdir:=%%PREFIX%%/squid/logs}
+: ${squid_user:=%%SQUID_UID%%}
+: ${squid_flags:="-D"}
+default_config=%%PREFIX%%/etc/squid/squid.conf
 
-case "$1" in
-start)
-	if [ -x ${PREFIX}/sbin/squid -a -f ${PREFIX}/etc/squid/squid.conf ]; then
-		(cd /${PREFIX}/squid/logs; ${PREFIX}/sbin/squid >/dev/null 2>&1 &) ; echo -n ' squid'
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr && rcNG=yes
+else
+	if [ -f %%PREFIX%%/etc/rc.subr ]; then
+		. %%PREFIX%%/etc/rc.subr && rcNG=yes
 	fi
-	;;
-stop)
-		${PREFIX}/sbin/squid -k shutdown 2>&1
-		# Uncomment this if you'd like the system to (attempt to
-		# wait for) squid to shut down cleanly
-		#echo "Sleeping for 45 seconds to allow squid to shutdown.."
-		#sleep 45 
-	;;
-restart)
-		${PREFIX}/sbin/squid -k reconfigure
-	;;
-*)
-	echo "Usage: `basename $0` {start|stop|restart}" >&2
-	;;
-esac
+fi
 
-exit 0
+if [ "${rcNG}" ]; then
+	rcvar=`set_rcvar`
+	load_rc_config ${name}
+	# check that squid's default configuration is present when
+	# squid_flags is not set. We assume that you specify at
+	# least the path to your non-default configuration with
+	# '-f /path/to/config.file' in squid_flags if you delete this file.
+	if [ -z "${squid_flags}" ]; then
+		required_files=${default_config}
+	fi
+	required_dirs=${squid_chdir}
+	run_rc_command "$1"
+else
+	case $1 in
+	start)
+		if [ -x "${command}" -a \
+		    \( -f "${default_config}" -o "${squid_flags}" \) ]; then
+			echo -n ' squid'
+			(cd ${squid_chdir} && exec su -fm ${squid_user} -c \
+			    "${command} ${squid_flags}")
+		fi
+		;;
+	stop)
+		if [ -x "${command}" ]; then
+			echo -n ' squid'
+			${stop_cmd}
+			while ps -xcU ${squid_user} | grep -q squid; do
+				sleep 2
+			done
+		fi
+		;;
+	*)
+		echo "usage: `basename $0` {start|stop}" >&2
+		exit 64
+		;;
+	esac
+	exit 0
+fi
