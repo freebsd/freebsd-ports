f2719275dee7a5cdaa1aa0879dc0247972c84822
Tilman Keskinoz <arved@FreeBSD.org>
2004-02-19 20:00:57 +0000

    Fix build on CURRENT

---
+++ graphics/landscape/files/patch-texture-texture_rough.cc
@@ -0,0 +1,11 @@
+--- texture/texture_rough.cc.orig	Thu Feb 19 20:48:54 2004
++++ texture/texture_rough.cc	Thu Feb 19 20:49:59 2004
+@@ -8,7 +8,7 @@
+ // $Id: texture_rough.cc,v 1.1 2003/10/11 18:11:00 jon Exp $
+ //
+ 
+-
++#include <assert.h>
+ #include "texture_rough.h"
+ 
+ 
