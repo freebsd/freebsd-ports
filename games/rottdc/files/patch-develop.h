10e472981d14aecf6e1e1fe902a3d80744d3b2e3
krion <krion@FreeBSD.org>
2004-02-19 08:56:29 +0000

    Add rottdc 1.0.2,
    This is a port of Apogee's 3D action title Rise of the Triad,
    originally released in 1994. This port duplicates the
    functionality of the original game on modern operating systems,
    including Linux, Win32, OSX and now FreeBSD. In order to run
    game you'll need original game data.
    
    PR:		ports/63049
    Submitted by:	Igor Pokrovsky <tiamat@comset.net>

---
+++ games/rottdc/files/patch-develop.h
@@ -0,0 +1,11 @@
+--- develop.h.orig	Wed Feb 18 09:00:17 2004
++++ develop.h	Wed Feb 18 09:01:08 2004
+@@ -41,7 +41,7 @@
+ 
+ // Make sure only one of the following are on at one time
+ #define SHAREWARE   0
+-#define SUPERROTT   0
++#define SUPERROTT   1
+ #define SITELICENSE 0
+ 
+ // cute little dopefish thing, only works with special patch?
