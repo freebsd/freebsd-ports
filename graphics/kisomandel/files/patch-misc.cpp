af8fca43d8e6f0ad2bb6817c359a01a78dc5efd4
Tilman Keskinoz <arved@FreeBSD.org>
2004-02-19 18:55:33 +0000

    Fix Build on 5.x Respect CFLAGS

---
+++ graphics/kisomandel/files/patch-misc.cpp
@@ -0,0 +1,10 @@
+--- kisomandel/misc.cpp.orig	Thu Feb 19 19:48:17 2004
++++ kisomandel/misc.cpp	Thu Feb 19 19:48:28 2004
+@@ -16,6 +16,7 @@
+  ***************************************************************************/
+ 
+ #include <math.h>
++#include <stdlib.h>
+ #include "kisomandel.h"
+ #include "misc.h"
+ #include "fractal.h"
