7f9dd2dac0309fe52842f1ffb1fb5d6abe0a8ca3
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-02-20 09:39:06 +0000

    upgrade to 1.1.1

---
+++ audio/ccaudio/files/patch-src:dtmf.cpp
@@ -0,0 +1,10 @@
+--- src/dtmf.cpp.orig	Fri Feb 20 17:23:13 2004
++++ src/dtmf.cpp	Fri Feb 20 17:23:19 2004
+@@ -30,6 +30,7 @@
+ #include <stdint.h>
+ #include <string.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <time.h>
+ #include <fcntl.h>
+ #include "audio.h"
