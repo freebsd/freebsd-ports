458bb8127b3e7c6c17086fb5b36a3146e46d8349
marcus <marcus@FreeBSD.org>
2004-02-21 20:46:54 +0000

    Make sure to link against libc_r on 5.X if requested to do so.  This should fix any DTP-related crash.
    Obtained from:	MarcusCom GNOME 2.5 repo

--- multimedia/gstreamer80/files/patch-ltmain.sh
+++ multimedia/gstreamer80/files/patch-ltmain.sh
@@ -1,6 +1,15 @@
---- ltmain.sh.orig	Fri Sep 13 15:38:11 2002
-+++ ltmain.sh	Fri Oct 11 01:44:24 2002
-@@ -1073,8 +1073,16 @@
+--- ltmain.sh.orig	Mon Oct 13 14:17:09 2003
++++ ltmain.sh	Sat Feb 21 15:44:32 2004
+@@ -1075,7 +1075,7 @@
+ 	  esac
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	 *-*-openbsd* | *-*-freebsd*)
++	 *-*-openbsd* | *-*-freebsd4*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -1085,8 +1085,16 @@
  	continue
  	;;
  
@@ -17,8 +26,8 @@
  	continue
  	;;
  
-@@ -2502,6 +2510,9 @@
- 	  *-*-openbsd*)
+@@ -2503,6 +2511,9 @@
+ 	  *-*-openbsd* | *-*-freebsd*)
  	    # Do not include libc due to us having libc/libc_r.
  	    ;;
 +	  *-*-freebsd*)
@@ -27,7 +36,7 @@
  	  *)
  	    # Add libc to deplibs on all other systems if necessary.
  	    if test $build_libtool_need_lc = "yes"; then
-@@ -4286,10 +4297,12 @@
+@@ -4330,10 +4341,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
