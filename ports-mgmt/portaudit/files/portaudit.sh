da981388e66d1427a30dd01703c6570d0fb821e4
eik <eik@FreeBSD.org>
2004-02-21 21:19:41 +0000

    update to 0.2: - new command line tool - new man page - reworked database update code, incorporating feedback from   Max Khon <fjoe>, Radim Kolar <hsn@netmag.cz> (PR 63066) and   Ion-Mihai Tetcu <itetcu@apropo.ro> (PR 62655)

--- ports-mgmt/portaudit/files/portaudit.sh
+++ ports-mgmt/portaudit/files/portaudit.sh
@@ -1,12 +1,38 @@
 #!/bin/sh
-
+#
+# Copyright (c) 2004 Oliver Eikemeier. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#  1. Redistributions of source code must retain the above copyright notice
+#     this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the author nor the names of its contributors may be
+#    used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # $FreeBSD$
 #
 
 # defaults
 daily_status_portaudit_enable="YES"
-portaudit_dir="%%DATABASEDIR%%"
 
 # If there is a global system configuration file, suck it in.
 #
@@ -17,8 +43,7 @@ then
 fi
 
 . %%DATADIR%%/portaudit.functions
-
-portaudit_file="${portaudit_dir}/auditfile.tbz"
+portaudit_confs
 
 rc=0
 case "$daily_status_portaudit_enable" in
@@ -27,58 +52,11 @@ case "$daily_status_portaudit_enable" in
 		echo "Checking for packages with security vulnerabilities:"
 		echo ""
 
-		if [ -z "${PKG_INFO}" ]; then
-			if [ -x /usr/local/sbin/pkg_info ]; then
-				PKG_INFO=/usr/local/sbin/pkg_info
-			else
-				PKG_INFO=/usr/sbin/pkg_info
-			fi
-		fi
-
-		if [ ! -x "${PKG_INFO}" ]; then
-			echo "${PKG_INFO} missing, please install port sysutils/pkg_install-devel"
-			exit 2
-		fi
-
-		PKG_INSTALL_VER=`${PKG_INFO} -qP 2>/dev/null`
-		if [ -z "${PKG_INSTALL_VER}" -o "${PKG_INSTALL_VER}" -lt 20040125 ]; then
-			echo "${PKG_INFO} is too old, please update port sysutils/pkg_install-devel"
-			exit 2
-		fi
-
-		if [ ! -f "${portaudit_file}" ]; then
-			echo "portaudit: database missing."
-			echo "           run fetchaudit to update."
-			exit 2
-		fi
-		if checksum_auditfile; then
-			echo "portaudit: corrupt database."
-			exit 2
-		fi
-		if checkexpiry_auditfile 14; then
-			echo "portaudit: database too old."
-			echo ""
+		if portaudit_prerequisites; then
+			audit_installed || rc=1
+		else
 			rc=2
 		fi
-
-		extract_auditfile | awk -F\| "
-			BEGIN { vul=0 }
-			/^(#|\$)/ { next }
-			{
-				cmd=\"${PKG_INFO} -E \\\"\" \$1 \"\\\"\"
-				while((cmd | getline pkg) > 0) {
-					vul++
-					print \"Affected package: \" pkg \"\\n\" \
-						\"Type of problem: \" \$3 \".\\n\" \
-						\"Reference: <\" \$2 \">\\n\"
-				}
-				close(cmd)
-			}
-			END {
-				print vul \" problem(s) in your installed packages found.\"
-				if (vul > 0) exit(1)
-			}
-		" || rc=1
 		;;
 	*)
 		;;
