3598c06157ff163bf1440b1820fef8f42ce1c53d
Kirill Ponomarev <krion@FreeBSD.org>
2004-02-22 23:35:12 +0000

    - Add patch to increase max temperature for modern hardware
    PR:		ports/61346
    Submitted by:	Andrew Thompson <andy@fud.org.nz>
    Approved by:	maintainer timeout

---
+++ sysutils/mbmon/files/patch-xmbmon.c
@@ -0,0 +1,11 @@
+--- xmbmon.c.orig	Mon Feb 23 00:32:24 2004
++++ xmbmon.c	Mon Feb 23 00:32:43 2004
+@@ -32,7 +32,7 @@
+ #define DEFAULT_CSEC        "20"
+ #define DEFAULT_WSEC      "1800"
+ #define DEFAULT_TMIN      "10.0"
+-#define DEFAULT_TMAX      "50.0"
++#define DEFAULT_TMAX      "90.0"
+ #define DEFAULT_TMINF     "50.0"	/* for Fahrenheit */
+ #define DEFAULT_TMAXF     "130.0"	/* for Fahrenheit */
+ #define DEFAULT_VMIN      "1.80"
