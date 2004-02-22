2fff16cf5b7071c7f9f4cd487516f4bb06184b59
nobutaka <nobutaka@FreeBSD.org>
2004-02-22 14:07:39 +0000

    1. Fix hangup problem with arts. 2. Fix build error on alpha. 3. SIZEify.
    PR:		ports/62249 [1], ports/62387 [2]
    Submitted by:	Fridtjof Busse <fbusse@gmx.de> [1],
    		Klaus Robert Suetterlin <krs@robert2.mpe-garching.mpg.de> [2]

--- multimedia/libxine/files/patch-src:video_out:video_out_xv.c
+++ multimedia/libxine/files/patch-src:video_out:video_out_xv.c
@@ -1,6 +1,6 @@
---- src/video_out/video_out_xv.c.orig	Sun Aug 10 00:46:38 2003
-+++ src/video_out/video_out_xv.c	Sun Aug 10 01:19:42 2003
-@@ -45,7 +45,11 @@
+--- src/video_out/video_out_xv.c.orig   Sun Dec 14 23:13:26 2003
++++ src/video_out/video_out_xv.c        Thu Feb  5 14:27:10 2004
+@@ -47,13 +47,17 @@
  #if defined (__SVR4) && defined (__sun)
  # include <sys/int_types.h>
  #else
@@ -12,4 +12,11 @@
 +# endif
  #endif
  
++#include <sys/types.h>
  #if defined(__FreeBSD__)
+ #include <machine/param.h>
+ #endif
+-#include <sys/types.h>
+ #include <sys/ipc.h>
+ #include <sys/shm.h>
+ #include <sys/time.h>
