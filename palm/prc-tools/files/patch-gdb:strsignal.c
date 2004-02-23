dafbda26d68decdceacafc70ad97cb30d60eee6a
nobutaka <nobutaka@FreeBSD.org>
2004-02-23 15:01:49 +0000

    1. Fix patch error. 2. Correct pkg-plist.
    PR:		ports/63149 [1]
    Submitted by:	Helge Oldach <ports-palm-prc-tools-2.3@oldach.net> [1]

--- palm/prc-tools/files/patch-gdb:strsignal.c
+++ palm/prc-tools/files/patch-gdb:strsignal.c
@@ -1,5 +1,5 @@
---- ../gdb-5.0/libiberty/strsignal.c.orig	Mon May  3 07:28:54 1999
-+++ ../gdb-5.0/libiberty/strsignal.c	Mon May  5 14:27:35 2003
+--- ../gdb-5.3/libiberty/strsignal.c.orig	Mon May  3 07:28:54 1999
++++ ../gdb-5.3/libiberty/strsignal.c	Mon May  5 14:27:35 2003
 @@ -244,7 +244,7 @@
  #else
  
