aae34ea9722f69dbc02ac840c9a392d6cc42844c
krion <krion@FreeBSD.org>
2004-02-26 15:09:25 +0000

    - Update to version 0.9.1
    PR:		ports/63367
    Submitted by:	maintainer

--- games/pydance/files/patch-dance.py
+++ games/pydance/files/patch-dance.py
@@ -1,16 +1,14 @@
---- /tmp/pydance/share/games/pydance/dance.py	Sat Oct 25 18:50:56 2003
-+++ dance.py	Sun Oct 26 18:44:40 2003
-@@ -11,6 +11,9 @@
+--- dance.py.orig	Sat Jan 31 23:12:01 2004
++++ dance.py	Sun Feb  8 16:35:46 2004
+@@ -22,6 +22,7 @@
+ import records
  
- import random, sys, os, copy
- 
+ import os
 +import time
-+# see comment about FreeBSD below. A better solution would be appreciated.
-+
- class BGmovie(pygame.sprite.Sprite):
-   def __init__ (self, filename):
-     pygame.sprite.Sprite.__init__(self)
-@@ -314,6 +317,10 @@
+ 
+ # A simple movie-playing sprite. It can only do MPEG1 though.
+ class BGMovie(pygame.sprite.Sprite):
+@@ -331,6 +332,10 @@
            break
        if songFailed:
          song.kill()
