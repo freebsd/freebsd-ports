322de53e18713eed9b751a3346cfe50fc721b4d0
Kirill Ponomarev <krion@FreeBSD.org>
2004-02-27 22:42:25 +0000

    - Update to version 0.18
    PR:		ports/63450
    Submitted by:	maintainer

--- devel/libowfat/files/patch-io_iob_prefetch.c
+++
@@ -1,9 +0,0 @@
---- io/iob_prefetch.c.orig	Sat Feb 21 17:56:29 2004
-+++ io/iob_prefetch.c	Sat Feb 21 17:56:49 2004
-@@ -1,5 +1,6 @@
- #include "iob_internal.h"
- #include <stdio.h>
-+#include <sys/types.h>
- #include <sys/mman.h>
- 
- void iob_prefetch(io_batch* b,uint64 bytes) {
