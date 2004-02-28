e5a3dc3dbde34309822f90ba5635f3e1419c70dc
maho <maho@FreeBSD.org>
2004-02-28 09:45:06 +0000

    Update to 3.6.0

--- math/atlas/files/patch-config.c
+++ math/atlas/files/patch-config.c
@@ -1,15 +1,6 @@
---- config.c.orig	Mon Jun 23 07:58:35 2003
-+++ config.c	Tue Jul  1 12:32:49 2003
-@@ -708,7 +708,7 @@
-             else if (mach == IA64Itan || MachIsUS(mach) ||
-                      mach == Dec21164 || mach == Dec21264)
-             {
--               if (major == 3)
-+               if ((major == 2 && minor < 96) || (major == 3)) /* Nasty hack for FreeBSD/Alpha STABLE */
-                {
-                   strcpy(goodgcc, files[i]);
-                   return(0);
-@@ -1114,7 +1114,9 @@
+--- config.c.org	Tue Jan 20 22:32:38 2004
++++ config.c	Tue Jan 20 22:32:54 2004
+@@ -1188,7 +1188,9 @@
     switch(OS)
     {
     case OSOSX:  /* don't know answer */
@@ -20,19 +11,51 @@
     case OSLinux:
        break;
     case OSSunOS:
-@@ -1252,6 +1254,11 @@
-                "-mcpu=ultrasparc -mtune=ultrasparc -fomit-frame-pointer -O3");
+@@ -1450,9 +1452,9 @@
+          if (THREADS && OS == OSFreeBSD)
+          {
+             if (which == CPF77)
+-               strcat(flag, " -pthread");
++               strcat(flag, " %%PTHREAD_LIBS%%");
+             else
+-               strcat(flag, " -pthread -D_REENTRANT -D_THREAD_SAFE");
++               strcat(flag, " %%PTHREAD_LIBS%%  %%PTHREAD_CFLAGS%%");
+          }
+          break;
+       case OSSunOS4:
+@@ -1984,8 +1986,8 @@
+       if (OS == OSFreeBSD)
+       {
+          if (strstr(comp, "cc"))
+-            strcat(flag, " -pthread -D_REENTRANT -D_THREAD_SAFE");
+-         else strcat(flag, " -pthread");
++            strcat(flag, " %%PTHREAD_LIBS%% %%PTHREAD_CFLAGS%%");
++         else strcat(flag, " %%PTHREAD_LIBS%%");
        }
-       if (OS == OSFreeBSD && F77) strcpy(F77, "f77");
-+      if (OS == OSFreeBSD && THREADS) { 
-+              strcat(F77FLAGS, "%%PTHREAD_CFLAGS%%");
-+              strcat(CCFLAGS, "%%PTHREAD_CFLAGS%%");
-+              strcat(MMFLAGS, "%%PTHREAD_CFLAGS%%");
-+     }
-       break;
-    case OSSunOS:
-       np = 3;
-@@ -2116,7 +2123,10 @@
+    }
+    return(*comp ? comp : NULL);
+@@ -2801,7 +2801,7 @@
+       else if (strstr(ln, "ia64")) la = LAIA64;
+       else if ( strstr(ln, "i686") || strstr(ln, "i586") ||
+                 strstr(ln, "i486") || strstr(ln, "i386") ||
+-                strstr(ln, "x86_64") ) la = LAX86;
++                strstr(ln, "x86_64") || strstr(ln, "amd64")  ) la = LAX86;
+    }
+    return(la);
+ }
+@@ -2835,12 +2837,22 @@
+          }
+          break;
+       case LASPARC: /* don't know */
++         if (!CmndOneLine(targ, "sysctl hw.model", ln))
++         {
++            if (strstr(ln, "UltraSparc-II")) mach = SunUS2;
++            if (strstr(ln, "UltraSparc-I")) mach = SunUS1;
++            if (strstr(ln, "UltraSparc")) mach = SunUSX;
++            else mach = SunUSX;
++         }
+          break;
+       case LAALPHA:
           if (!CmndOneLine(targ, "sysctl hw.model", ln))
           {
              if (strstr(ln, "433au")) mach = Dec21164;
@@ -43,35 +66,73 @@
           }
           break;
        case LAIA64: /* don't know */
-@@ -2125,14 +2135,22 @@
+@@ -2849,14 +2861,32 @@
           if (!CmndOneLine(targ, "sysctl hw.model", ln))
           {
              if (strstr(ln, "Pentium Pro")) mach = IntPPRO;
 +            else if (strstr(ln, "Pentium(R) Pro")) mach = IntPPRO;
 +            else if (strstr(ln, "Pentium 4")) mach = IntP4;
-+	    else if (strstr(ln, "Pentium(R) 4")) mach = IntP4;
++            else if (strstr(ln, "Pentium(R) 4")) mach = IntP4;
++            else if (strstr(ln, "Pentium(R) M")) mach = IntP4;
              else if (strstr(ln, "Pentium III")) mach = IntPIII;
+-            else if (strstr(ln, "Pentium II ")) mach = IntPII;
++            else if (strstr(ln, "Pentium III/Pentium III")) mach = IntPIII;
 +            else if (strstr(ln, "Pentium(R) III")) mach = IntPIII;
-             else if (strstr(ln, "Pentium II ")) mach = IntPII;
++            else if (strstr(ln, "Pentium II")) mach = IntPII;
++            else if (strstr(ln, "Pentium II/PentiumII")) mach = IntPII;
 +            else if (strstr(ln, "Pentium(R) II ")) mach = IntPII;
 +            else if (strstr(ln, "Celeron")) mach = IntPII;
 +            else if (strstr(ln, "Celeron(R)")) mach = IntPII;
++            else if (strstr(ln, "Opteron(tm)") && sizeof(void *)==4) mach = AmdHammer32;
++            else if (strstr(ln, "Opteron(tm)") && sizeof(void *)==8) mach = AmdHammer64;
++            else if (strstr(ln, "AMD Athlon(tm) 64") && sizeof(void *)==4) mach = AmdHammer32;
++            else if (strstr(ln, "AMD Athlon(tm) 64") && sizeof(void *)==8) mach = AmdHammer64;
++            else if (strstr(ln, "Athlon(tm)")) mach = AmdAthlon;
              else if (strstr(ln, "Athlon")) mach = AmdAthlon;
              else if (strstr(ln, "AMD-K7")) mach = AmdAthlon;
-             else if (strstr(ln, "32 bit Hammer")) mach = AmdHammer32;
-             else if (strstr(ln, "64 bit Hammer")) mach = AmdHammer64;
-             else if (strstr(ln, "Pentium/P55C")) mach = IntP5MMX; /* sent by */
-             else if (strstr(ln, "Pentium")) mach=IntP5;       /* Nakata Maho */
+-            else if (strstr(ln, "32 bit Hammer")) mach = AmdHammer32;
+-            else if (strstr(ln, "64 bit Hammer")) mach = AmdHammer64;
+-            else if (strstr(ln, "Pentium/P55C")) mach = IntP5MMX; /* sent by */
+-            else if (strstr(ln, "Pentium")) mach=IntP5;       /* Nakata Maho */
++            else if (strstr(ln, "AMD-K6(tm)")) mach = IntP5MMX;
++            else if (strstr(ln, "Pentium/P55C")) mach = IntP5MMX;
++            else if (strstr(ln, "Pentium/P54C")) mach=IntP5;
++            else if (strstr(ln, "Pentium")) mach=IntP5;
++            else if (strstr(ln, "VIA"))     mach=IntP5MMX;       
++            else if (strstr(ln, "Crusoe(tm)")) mach=IntP5MMX;       
 +            else mach = IntP5;
           }
           break;
        default:;
-@@ -3136,6 +3154,8 @@
+@@ -3641,8 +3671,8 @@
     }
-    if (USEWINF77) strcpy(F77, "$(BINdir)/winf77.exe");
- 
-+   fprintf(fpout, "%s", ARCH);
-+   fclose(fpout);
-    ATL_mprintf(2, fplog, stdout,"\nCreating make include file Make.%s\n", ARCH);
-    sprintf(ln, "Make.%s", ARCH);
-    fpout = fopen(ln, "w");
+    if (THREADS) /* add ncpu to ARCH */
+    {
+-      for (i=0; ARCH[i]; i++);
+-      sprintf(ARCH+i, "_%d", ncpu);
++//      for (i=0; ARCH[i]; i++); // do not add number of cpu for ARCHNAME
++//      sprintf(ARCH+i, "_%d", ncpu); // #cpu can be different in general
+    }
+    do
+    {
+@@ -4047,9 +4077,9 @@
+    if (mach == IA64Itan || mach == IA64Itan2 )
+       fprintf(fpout, " -DATL_MAXNREG=128");
+    if (ASMD != ASM_None) fprintf(fpout, " -DATL_%s", ASMNAM[ASMD]);
+-   if (mach == AmdHammer32 && (OS != OSWinNT && OS != OSWin9x))
++   if (mach == AmdHammer32 && (OS != OSWinNT && OS != OSWin9x && OS != OSFreeBSD))
+       fprintf(fpout, " -m32");
+-   else if (mach == AmdHammer64) fprintf(fpout, " -m64");
++   else if (mach == AmdHammer64 && (OS != OSFreeBSD)) fprintf(fpout, " -m64");
+    if (mach == IA64Itan2 && strstr(CC, "icc"))
+       fprintf(fpout, " -DATL_IntelIccBugs");
+    fprintf(fpout, "\n\n");
+@@ -4080,7 +4095,7 @@
+    if (THREADS)
+    {
+       fprintf(fpout, " -DATL_NCPU=%d", ncpu);
+-      if (OS == OSFreeBSD) fprintf(fpout, " -D_THREAD_SAFE -D_REENTRANT");
++      if (OS == OSFreeBSD) fprintf(fpout, " %%PTHREAD_CFLAGS%%");
+       if (OS == OSAIX) fprintf(fpout, " -DIBM_PT_ERROR");
+       if (OS == OSIRIX) fprintf(fpout, " -D_POSIX_C_SOURCE=199506L");
+    }
