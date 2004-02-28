23f02b0cbb5379656ee2b6bdc7f4de8564d33313
lioux <lioux@FreeBSD.org>
2004-02-28 05:41:35 +0000

    Update to 20040225 snapshot

--- net-p2p/frost/files/wrapper.sh
+++ net-p2p/frost/files/wrapper.sh
@@ -6,7 +6,7 @@ JAR_DIR="%%JAR_DIR%%"
 JAR_FILE="%%JAR_FILE%%"
 SHARE_DIR="%%SHARE_DIR%%"
 HOME_DIR=${HOME}/.frost
-CLASSPATH="${PREFIX}/${JAR_DIR}/${JAR_FILE}:${PREFIX}/${JAR_DIR}/fecImpl.jar:${PREFIX}/${JAR_DIR}/genChkImpl.jar:${PREFIX}/${JAR_DIR}/xercesImpl.jar:${PREFIX}/${JAR_DIR}/xml-apis.jar:${PREFIX}/${JAR_DIR}/xmlParserAPIs.jar:$CLASSPATH"
+CLASSPATH="${PREFIX}/${JAR_DIR}/BCastle.jar:${PREFIX}/${JAR_DIR}/fecImpl.jar:${PREFIX}/${JAR_DIR}/frost.jar:${PREFIX}/${JAR_DIR}/genChkImpl.jar:${PREFIX}/${JAR_DIR}/jocache.jar:${PREFIX}/${JAR_DIR}/mailapi.jar:${PREFIX}/${JAR_DIR}/skinlf.jar:${PREFIX}/${JAR_DIR}/skinlfFix.jar:${PREFIX}/${JAR_DIR}/smtp.jar:${PREFIX}/${JAR_DIR}/xercesImpl.jar:${PREFIX}/${JAR_DIR}/xml-apis.jar:$CLASSPATH"
 
 if [ ! -d ${HOME_DIR}/config ]
 then
@@ -16,6 +16,16 @@ fi
 if [ ! -e ${HOME_DIR}/config/boards.xml.default ]
 then
 	cp ${PREFIX}/${SHARE_DIR}/boards.xml.default ${HOME_DIR}/config/
+fi
+
+if [ ! -d ${HOME_DIR}/themes ]
+then
+	mkdir -p ${HOME_DIR}/themes
+fi
+
+if [ ! -e ${HOME_DIR}/themes/themepack.zip ]
+then
+	cp ${PREFIX}/${SHARE_DIR}/themepack.zip ${HOME_DIR}/themes/
 fi
 
 cd ${HOME_DIR} &&
