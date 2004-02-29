5111b833f4ad23c9efca9f355aede794f36b1e5b
Kirill Ponomarev <krion@FreeBSD.org>
2004-02-29 17:12:05 +0000

    - Update to version 0.22
    PR:		ports/63534
    Submitted by:	maintainer

--- net/grdesktop/files/patch-src::options.c
+++ net/grdesktop/files/patch-src::options.c
@@ -1,5 +1,5 @@
---- src/options.c.orig	Mon Feb  2 15:05:53 2004
-+++ src/options.c	Thu Feb 26 10:58:47 2004
+--- src/options.c	Tue Feb  3 09:49:48 2004
++++ src/options.c.new	Sun Feb 29 17:23:08 2004
 @@ -20,6 +20,7 @@
   */
  
@@ -8,29 +8,29 @@
  #include "options.h"
  
  void fillRdpProtocols() {
-@@ -275,6 +276,8 @@
- 
+@@ -276,6 +277,9 @@
  		item = g_slist_nth_data(values, i);
  		typ = gconf_entry_get_value(item);
+ 
 +		if (!typ)
 +			continue ;
- 
++
  		path = g_strsplit(gconf_entry_get_key(item), "/", 0);
  		while(path[y] != NULL)
-@@ -489,6 +492,7 @@
+ 			y++;
+@@ -487,6 +491,7 @@
  
  		while((entry = readdir(licence_dir)) != NULL) {
  			gchar *fullname = NULL;
-+			gchar *hostname;
++			gchar *hostname = &entry->d_name[strlen(prefix)];
  			struct stat sts;
  
  			fullname = g_strdup_printf("%s/%s", licence_path,
-@@ -502,7 +506,7 @@
+@@ -500,7 +505,6 @@
  			if(strlen(entry->d_name) <= strlen(prefix))
  				continue;
  
 -			gchar *hostname = &entry->d_name[strlen(prefix)];
-+			hostname = &entry->d_name[strlen(prefix)];
- 			licence_list = g_list_append(licence_list, hostname);
+ 			licence_list = g_list_append(licence_list, g_strdup(hostname));
  		}
  		closedir(licence_dir);
