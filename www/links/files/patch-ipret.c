f45052e2689b61e4936b986a791bf19007996ffe
Dmitry Sivachenko <demon@FreeBSD.org>
2004-03-01 06:54:07 +0000

    Remove leading '+'.  I committed corrupted patch.
    Submitted by:	Josh Elsasser <jre@vineyard.net>

--- www/links/files/patch-ipret.c
+++ www/links/files/patch-ipret.c
@@ -1,11 +1,11 @@
-+--- ipret.c.orig	Fri Aug 29 13:05:17 2003
-++++ ipret.c	Fri Feb 20 17:12:11 2004
-+@@ -411,7 +411,7 @@
-+ 		case VARIABLE:
-+ 			zrusit=(lns*)pom->argument;
-+ 			if(zrusit)
-+-			{	if(zrusit->value==CIntMETFUN)
-++			{	if(zrusit->value==CIntMETFUN&&zrusit->handler)
-+ 				{	delarg((abuf*)zrusit->handler,context);
-+ 					js_mem_free(zrusit); /* Kdyz zustane trcet na zasobniku
-+ 						* interni funkce nebo metoda, tak ji je potreba znicit, stejne
+--- ipret.c.orig	Fri Aug 29 13:05:17 2003
++++ ipret.c	Fri Feb 20 17:12:11 2004
+@@ -411,7 +411,7 @@
+ 		case VARIABLE:
+ 			zrusit=(lns*)pom->argument;
+ 			if(zrusit)
+-			{	if(zrusit->value==CIntMETFUN)
++			{	if(zrusit->value==CIntMETFUN&&zrusit->handler)
+ 				{	delarg((abuf*)zrusit->handler,context);
+ 					js_mem_free(zrusit); /* Kdyz zustane trcet na zasobniku
+ 						* interni funkce nebo metoda, tak ji je potreba znicit, stejne
