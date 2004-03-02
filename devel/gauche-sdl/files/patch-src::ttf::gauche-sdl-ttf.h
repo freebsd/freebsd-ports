506dc68f3f252a2c9ae97db02c2dbf515bee870d
pav <pav@FreeBSD.org>
2004-03-02 16:56:35 +0000

    Add gauche-sdl, SDL bindings for the gauche scheme environment
    PR:		ports/62892
    Submitted by:	Erik Greenwald <erik@smluc.org>

---
+++ devel/gauche-sdl/files/patch-src::ttf::gauche-sdl-ttf.h
@@ -0,0 +1,11 @@
+--- src/ttf/gauche-sdl-ttf.h.orig	Sun Feb 15 18:31:53 2004
++++ src/ttf/gauche-sdl-ttf.h	Sun Feb 15 18:32:10 2004
+@@ -20,7 +20,7 @@
+ 
+ #include "../gauche-sdl.h"
+ 
+-#include <SDL/SDL_ttf.h>
++#include <SDL_ttf.h>
+ 
+ /*****************************************************************************
+  * TTF
