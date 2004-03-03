4abe16dfa70e65345a14a6eb4470e10250caf32b
krion <krion@FreeBSD.org>
2004-03-03 13:38:28 +0000

    - Update to version 0.6 - Respect PTHREAD_LIBS
    PR:		ports/63688
    Submitted by:	maintainer

--- net/mad_fcl/files/patch-flute::fdt.c
+++ net/mad_fcl/files/patch-flute::fdt.c
@@ -1,23 +1,24 @@
---- flute/fdt.c.orig	Fri Feb 13 15:58:54 2004
-+++ flute/fdt.c	Fri Feb 13 15:58:54 2004
-@@ -113,14 +113,14 @@
- #ifdef WIN32
-                                 file->file_len = _atoi64(*(++atts));
- #else
--                                file->file_len = atoll(*(++atts));
-+                                file->file_len = strtoll(*(++atts), NULL, 10);
+--- flute/fdt.c.orig	Wed Mar  3 12:48:53 2004
++++ flute/fdt.c	Wed Mar  3 12:49:38 2004
+@@ -114,7 +114,7 @@
+ #ifdef WIN32     
+ 				file->file_len = _atoi64(*(++atts));
+ #else               
+-				file->file_len = atoll(*(++atts));				
++				file->file_len = strtoll(*(++atts), NULL, 10);				
  #endif
-                         }
- 			else if(!strcmp(*atts, "Transfer-Length")) {
+ 				if(file->toi_len == 0) {
+ 					file->toi_len = file->file_len;
+@@ -125,7 +125,7 @@
  #ifdef WIN32			  
  				file->toi_len = _atoi64(*(++atts));
  #else
 -				file->toi_len = atoll(*(++atts));
-+				file->toi_len = strtoll(*(++atts),NULL, 10);
++				file->toi_len = strtoll(*(++atts), NULL, 10);
  #endif 
  			}
  			else if(!strcmp(*atts, "Content-Type")) {
-@@ -179,7 +179,7 @@
+@@ -184,7 +184,7 @@
  #ifdef WIN32
  				fdt->expires = _atoi64(*(++atts));
  #else
