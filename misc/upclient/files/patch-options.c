e8ef4a0dcd8bac56a0bdcc55a001b2df2abe4cc1
Kirill Ponomarev <krion@FreeBSD.org>
2004-03-03 11:46:05 +0000

    - Fix MASTER_SITES - Add USE_RC_SUBR - Update pkg-descr - Use DOCSDIR
    PR:		ports/63668
    Submitted by:	maintainer

--- misc/upclient/files/patch-options.c
+++ misc/upclient/files/patch-options.c
@@ -1,5 +1,5 @@
---- src/options.c.orig	Sat Nov 30 03:27:15 2002
-+++ src/options.c	Wed Dec 18 01:43:00 2002
+--- src/options.c.orig	Tue May 27 09:03:43 2003
++++ src/options.c	Wed Mar  3 00:47:28 2004
 @@ -60,7 +60,7 @@
   * Minimum value is enforced. A warning is logged if greater than upper limit.
   */
@@ -9,3 +9,12 @@
  #define UPPER_INTERVAL   600    /* warning if exceeded */
  
  /* Macros */
+@@ -79,7 +79,7 @@
+  * These are the configuration options available.
+  */
+ char   cfg_authkey[AUTHKEY_REQUIRED_LEN + 1] = "";
+-char   cfg_upserver[UPSERVER_MAXLEN + 1] = "uptimes.wonko.com";
++char   cfg_upserver[UPSERVER_MAXLEN + 1] = "uptimes.hostingwired.com";
+ long int cfg_interval = DEFAULT_INTERVAL;
+ char   cfg_pidfile[PIDFILE_MAXLEN + 1] = PIDFILE;
+ char   cfg_proxyserver[PROXYSERVER_MAXLEN + 1] = "";
