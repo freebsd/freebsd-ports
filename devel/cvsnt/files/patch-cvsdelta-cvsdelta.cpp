91802da0e7f9a5e4d69a5c87dba72d850bdc50a9
Max Khon <fjoe@FreeBSD.org>
2004-03-03 19:32:05 +0000

    Update to 2.0.34.

--- devel/cvsnt/files/patch-cvsdelta-cvsdelta.cpp
+++ devel/cvsnt/files/patch-cvsdelta-cvsdelta.cpp
@@ -1,14 +1,6 @@
---- cvsdelta/cvsdelta.cpp.orig	Sun Feb 22 03:05:15 2004
-+++ cvsdelta/cvsdelta.cpp	Sun Feb 22 03:04:56 2004
-@@ -10,7 +10,6 @@
-  * the rest thrown together from a mixture of inspiration and google.
-  */
- 
--#include <malloc.h>
- #include <stdio.h>
- #include <assert.h>
- #include <string.h>
-@@ -20,6 +19,7 @@
+--- cvsdelta/cvsdelta.cpp.orig	Thu Mar  4 01:17:46 2004
++++ cvsdelta/cvsdelta.cpp	Thu Mar  4 01:18:01 2004
+@@ -23,6 +23,7 @@
  #if defined(_WIN32)
    #include <winsock2.h>
  #else
