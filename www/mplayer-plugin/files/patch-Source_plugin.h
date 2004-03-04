4264d7f45ca427b399522fe1b1a365fb0b6d405c
marcus <marcus@FreeBSD.org>
2004-03-04 06:36:21 +0000

    Update to 2.45.

--- www/mplayer-plugin/files/patch-Source_plugin.h
+++ www/mplayer-plugin/files/patch-Source_plugin.h
@@ -1,15 +1,15 @@
---- Source/plugin.h.orig	Sat Feb 14 08:00:01 2004
-+++ Source/plugin.h	Sat Feb 14 14:53:55 2004
-@@ -37,11 +37,8 @@
+--- Source/plugin.h.orig	Mon Mar  1 13:29:02 2004
++++ Source/plugin.h	Thu Mar  4 01:29:25 2004
+@@ -37,11 +37,9 @@
  
  #ifndef __PLUGIN_H__
  #define __PLUGIN_H__
 +#include <cmath>
  #define _XOPEN_SOURCE 500
--
+ 
 -#ifndef _GNU_SOURCE
 -#define _GNU_SOURCE
 -#endif
- 
- #include "pluginbase.h"
- #include "nsScriptablePeer.h"
+ #ifdef GECKOSDK_ENABLED
+ #include "mozilla-config.h"
+ #endif
