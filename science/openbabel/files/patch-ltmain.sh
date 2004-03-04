ef6167c2be7fc5801a427257e808590968c34600
Pav Lucistnik <pav@FreeBSD.org>
2004-03-04 13:03:10 +0000

    - Update to 1.100.2
    PR:		ports/63756
    Submitted by:	Ports Fury

---
+++ science/openbabel/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Tue Feb  3 02:46:45 2004
++++ ltmain.sh	Thu Mar  4 13:21:54 2004
+@@ -5528,10 +5528,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
