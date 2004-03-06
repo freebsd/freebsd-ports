0f1ef8c059756c2d3a20a9de99cac2c041900f03
pav <pav@FreeBSD.org>
2004-03-06 10:55:07 +0000

    - Unbreak on -CURRENT [1] - Add patch to add assert header [1] - Add patch to make assembly routine gcc3 friendly (obtained from Gentoo) [1] - Patch path to bash in two scripts - Remove USE_SIZE=yes it's default now and Portlint
    PR:		ports/63745
    Submitted by:	michael johnson <ahze@ahze.net>

---
+++ multimedia/mjpegtools/files/patch-mplex::systems.hh
@@ -0,0 +1,11 @@
+--- mplex/systems.hh.orig	Thu Mar  4 01:45:50 2004
++++ mplex/systems.hh	Thu Mar  4 01:46:04 2004
+@@ -4,6 +4,8 @@
+ #include <sys/param.h>
+ #include "inputstrm.hh"
+ 
++#include <cassert>
++
+ #include <vector>
+ 
+ using std::vector;
