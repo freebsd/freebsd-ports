f6e1c6e7b13cd206a0d2c8f0f04a75f21feb5fcf
Pav Lucistnik <pav@FreeBSD.org>
2004-03-06 15:06:59 +0000

    - "Drive read speed selection now works correctly."
    PR:		ports/63785
    Submitted by:	Igor Pokrovsky <tiamat@comset.net>
    Approved by:	maintainer

--- audio/xmms-cdread/files/patch-cdrombsd.h
+++ audio/xmms-cdread/files/patch-cdrombsd.h
@@ -1,14 +1,12 @@
---- cdrombsd.h.orig	Sat Aug 18 15:09:10 2001
-+++ cdrombsd.h	Thu Nov 20 19:24:05 2003
-@@ -5,15 +5,19 @@
+--- cdrombsd.h.orig	Sat Aug 18 14:09:10 2001
++++ cdrombsd.h	Thu Mar  4 09:55:32 2004
+@@ -5,15 +5,17 @@
  #ifndef CDROMBSD_H
  #define CDROMBSD_H
  #include <sys/param.h>
-+#if __FreeBSD_version >= 501106
 +#include <sys/cdrio.h>
 +#ifndef CD_FRAMESIZE_RAW
 +#define CD_FRAMESIZE_RAW 2352
-+#endif
 +#endif
  /*\
  |*| FreeBSD (?) specific code
@@ -25,7 +23,25 @@
  
  
  /*\
-@@ -84,6 +88,16 @@
+@@ -26,16 +28,14 @@
+ 	int fd;
+ 	fd = open(device, O_RDONLY|O_NONBLOCK);
+ 	if (fd < 0) return -1;
+-#ifdef CDROM_SELECT_SPEED
+ 	if (!(*flags & FLAG_FAIL_SPD) &&
+-	    (ioctl(fd, CDROM_SELECT_SPEED, cd_cfg.cdrom_speed) < 0)) {
++	    (ioctl(fd, CDRIOCREADSPEED, &cd_cfg.cdrom_speed) < 0)) {
+ 		if (errno == ENOTTY) {
+ 			close(fd);
+ 			return -1;
+ 		}
+ 		*flags |= FLAG_FAIL_SPD;
+ 	}
+-#endif
+ 	return fd;
+ }
+ 
+@@ -84,6 +84,16 @@
  |*|  from device cdfd, at position lba
  |*|  Return number of successfully read frames, -1 on error.
  \*/
@@ -42,7 +58,7 @@
  static int
  cdrom_read_audio(int cdfd, int lba, char *buf, int btw)
  {
-@@ -101,6 +115,7 @@
+@@ -101,6 +111,7 @@
  	} while (--rtr >= 0);
  	return -1;
  }
