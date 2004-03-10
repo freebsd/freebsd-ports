b94d25458360defb8c2bec6c1b17f10853d99dde
John Baldwin <jhb@FreeBSD.org>
2004-03-10 16:38:25 +0000

    libmath++ is a template based math library, written in C++, for symbolic and numeric calculus applications.
    WWW: http://www.surakware.net/projects/libmath++/index.xml

---
+++ math/libmath++/files/patch-ltmain.sh
@@ -0,0 +1,35 @@
+$FreeBSD: /tmp/pcvs/ports/math/libmath++/files/patch-ltmain.sh,v 1.1 2004-03-10 16:38:25 jhb Exp $
+
+--- ltmain.sh.orig	Thu Nov 22 05:43:58 2001
++++ ltmain.sh	Fri Dec  7 07:18:03 2001
+@@ -944,6 +944,7 @@
+ 	;;
+ 
+       -avoid-version)
++	build_old_libs=no
+ 	avoid_version=yes
+ 	continue
+ 	;;
+@@ -2408,6 +2409,9 @@
+ 	  *-*-netbsd*)
+ 	    # Don't link with libc until the a.out ld.so is fixed.
+ 	    ;;
++	  *-*-freebsd*)
++	    # FreeBSD doesn't need this...
++	    ;;
+ 	  *)
+ 	    # Add libc to deplibs on all other systems if necessary.
+ 	    if test $build_libtool_need_lc = "yes"; then
+@@ -4175,10 +4179,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
