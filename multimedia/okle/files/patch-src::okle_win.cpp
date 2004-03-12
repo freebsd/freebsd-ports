09f914f18d2105bfe876f15b8f9876609565e20f
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2004-03-12 01:59:38 +0000

    Update to 0.4

--- multimedia/okle/files/patch-src::okle_win.cpp
+++ multimedia/okle/files/patch-src::okle_win.cpp
@@ -1,6 +1,6 @@
---- src/okle_win.cpp.orig	Tue Feb 25 11:19:50 2003
-+++ src/okle_win.cpp	Sun Apr  6 01:50:55 2003
-@@ -34,6 +34,7 @@
+--- src/okle_win.cpp.orig	Sun Nov 23 09:26:07 2003
++++ src/okle_win.cpp	Thu Mar 11 22:41:26 2004
+@@ -37,6 +37,7 @@
  #include <kstandarddirs.h>
  #include <qtooltip.h>
  #include <dcopclient.h>
@@ -8,33 +8,21 @@
  
  extern "C" {
  #include <X11/Xlib.h>
-@@ -519,16 +520,16 @@
- void OkleWin::slotFileOpenDisc()
- {
-  conf->setGroup("DVD");
-- QString dvdp = conf->readEntry("dvdpath","/dev/dvd");
-+ QString dvdp = conf->readEntry("dvdpath","/dev/acd0c");
+@@ -737,14 +738,14 @@
+     return;
+   }
  
 - QFileInfo fi(dvdp);
 - if(!fi.exists())
 + KFileItem fi(KFileItem::Unknown, KFileItem::Unknown, dvdp);
 + if(!QFile::exists(dvdp))
    {
-    KMessageBox::error(0,QString(i18n("%1 does not exist!")).arg(dvdp),i18n("Error"));
+    KMessageBox::error(0,QString(i18n("%1 does not exist.\n\nEither set the correct DVD path in File -> Settings or create a symlink from your DVD device to %2.")).arg(dvdp).arg(dvdp),i18n("DVD device file not found"));
     return;
    }
  
 - if(!fi.isFile() && !fi.isSymLink())
 + if(!fi.isFile() && !fi.isLink())
    {
-    KMessageBox::error(0,QString(i18n("%1 exists but is not a file or symlink!")).arg(dvdp),i18n("Error"));
+    KMessageBox::error(0,QString(i18n("%1 exists but is not a file or symlink.\n\nMake sure that %2 really refers to your DVD device or change the DVD device path in File -> Settings.")).arg(dvdp).arg(dvdp),i18n("DVD device file not usable"));
     return;
-@@ -714,7 +715,7 @@
- {
-  PrefsWin* prefs = new PrefsWin(this);
-  conf->setGroup("DVD");
-- prefs->setDVDPath(conf->readEntry("dvdpath","/dev/dvd"));
-+ prefs->setDVDPath(conf->readEntry("dvdpath","/dev/acd0c"));
-  prefs->setRegion(conf->readNumEntry("region",1));
-  if(prefs->exec())
-   {
