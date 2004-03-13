baa8612f1ddaa3e387fadb28ca60660b05f7042a
Alexander Leidinger <netchild@FreeBSD.org>
2004-03-13 10:33:18 +0000

    - some base system headers know about icc now, don't override them   where appropriate [1] - make portlint happy [1] - sync icc7 and icc [1] - add linux_base as a patch depends for icc v8
    Submitted by:	Marius Strobl <marius@alchemy.franken.de> [1]
    Requested by:	maintainer [1]

--- lang/icc7/files/stdarg.h
+++ lang/icc7/files/stdarg.h
@@ -1,3 +1,5 @@
+#ifndef	__INTEL_COMPILER_STDARG_H_
+#define	__INTEL_COMPILER_STDARG_H_
 
 #include_next <stdarg.h>
 
@@ -7,3 +9,5 @@
 #define	va_copy(dest, src)	((dest) = (src))
 #endif
 #endif
+
+#endif /* !__INTEL_COMPILER_STDARG_H_ */
