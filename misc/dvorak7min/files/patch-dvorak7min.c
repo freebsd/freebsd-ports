ab398b6aa6aebc767b802f6538687d741257e227
Daichi GOTO <daichi@FreeBSD.org>
2004-03-15 15:55:31 +0000

    Update to 1.6.1
    PR:		ports/64295
    Submitted by:	Li-Lun Wang <llwang@infor.org> (maintainer)

--- misc/dvorak7min/files/patch-dvorak7min.c
+++ misc/dvorak7min/files/patch-dvorak7min.c
@@ -1,14 +1,10 @@
-
-$FreeBSD$
-
---- dvorak7min.c.orig	Wed Feb 12 23:15:04 2003
-+++ dvorak7min.c	Wed Feb 12 23:15:32 2003
-@@ -41,7 +41,7 @@
+--- dvorak7min.c.orig	Mon Mar 15 20:41:06 2004
++++ dvorak7min.c	Mon Mar 15 20:41:25 2004
+@@ -41,7 +41,6 @@
   *  
   */
  
 -#include <strings.h>
-+#include <string.h>
+ #include <string.h>
  #include <stdlib.h>
  #include <ctype.h>
- #include <time.h>
