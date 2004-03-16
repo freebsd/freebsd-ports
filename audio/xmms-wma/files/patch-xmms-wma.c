fb1a18c3c57d6249150b0f2b8720695f6c8202b6
Kirill Ponomarev <krion@FreeBSD.org>
2004-03-16 14:39:19 +0000

    - Update to version 0.1.3
    PR:		ports/64339
    Submitted by:	maintainer

--- audio/xmms-wma/files/patch-xmms-wma.c
+++ audio/xmms-wma/files/patch-xmms-wma.c
@@ -1,5 +1,5 @@
---- xmms-wma.c.orig	Thu Feb 26 12:26:21 2004
-+++ xmms-wma.c	Thu Feb 26 11:07:28 2004
+--- xmms-wma.c	Wed Mar  3 23:53:43 2004
++++ xmms-wma.c	Thu Mar 11 18:27:02 2004
 @@ -17,7 +17,7 @@
   *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
   */
