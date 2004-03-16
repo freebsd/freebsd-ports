65ad9741296eef4b2605e4276facc235b15dd2e2
krion <krion@FreeBSD.org>
2004-03-16 14:39:19 +0000

    - Update to version 0.1.3
    PR:		ports/64339
    Submitted by:	maintainer

--- audio/xmms-wma/files/patch-ffmpeg-strip-wma::wmadec.c
+++ audio/xmms-wma/files/patch-ffmpeg-strip-wma::wmadec.c
@@ -1,5 +1,5 @@
---- ffmpeg-strip-wma/wmadec.c.orig	Thu Feb 26 12:26:00 2004
-+++ ffmpeg-strip-wma/wmadec.c	Thu Feb 26 11:53:40 2004
+--- ffmpeg-strip-wma/wmadec.c	Mon Feb 23 03:46:59 2004
++++ ffmpeg-strip-wma/wmadec.c	Thu Mar 11 18:39:18 2004
 @@ -1155,7 +1155,7 @@
          iptr = s->frame_out[ch];
  
