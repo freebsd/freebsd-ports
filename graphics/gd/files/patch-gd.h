945c3b3d9382bf62c2a9a0d3b94583584a4cee95
Dirk Meyer <dinoex@FreeBSD.org>
2004-03-17 16:06:06 +0000

    - update to 2.0.22 - Fix comatibility ABI - add missing includes

--- graphics/gd/files/patch-gd.h
+++ graphics/gd/files/patch-gd.h
@@ -1,41 +1,57 @@
---- gd.h	Thu Jan 16 11:28:09 2003
-+++ gd.h	Mon Mar 24 16:26:14 2003
-@@ -209,4 +209,8 @@
-   gdImagePtr gdImageCreateFromPngSource (gdSourcePtr in);
+--- gd.h.orig	Thu Jan 15 18:42:10 2004
++++ gd.h	Wed Mar 17 16:52:30 2004
+@@ -273,6 +273,12 @@
+    /* Deprecated in favor of gdImageCreateFromPngCtx */
+    BGD_EXPORT gdImagePtr gdImageCreateFromPngSource (gdSourcePtr in);
  
-+  gdImagePtr gdImageCreateFromGif(FILE *fd);
-+  gdImagePtr gdImageCreateFromGifCtx(gdIOCtxPtr in);
-+  gdImagePtr gdImageCreateFromGifSource(gdSourcePtr in);
++#ifdef __FreeBSD__
++   BGD_EXPORT gdImagePtr gdImageCreateFromGif(FILE *fd);
++   BGD_EXPORT gdImagePtr gdImageCreateFromGifCtx(gdIOCtxPtr in);
++   BGD_EXPORT gdImagePtr gdImageCreateFromGifSource(gdSourcePtr in);
++#endif
 +
-   gdImagePtr gdImageCreateFromGd (FILE * in);
-   gdImagePtr gdImageCreateFromGdCtx (gdIOCtxPtr in);
-@@ -300,4 +304,5 @@
+    BGD_EXPORT gdImagePtr gdImageCreateFromGd (FILE * in);
+    BGD_EXPORT gdImagePtr gdImageCreateFromGdCtx (gdIOCtxPtr in);
+    BGD_EXPORT gdImagePtr gdImageCreateFromGdPtr (int size, void *data);
+@@ -397,6 +403,9 @@
+   gdPoint, *gdPointPtr;
  
-   void gdImagePolygon (gdImagePtr im, gdPointPtr p, int n, int c);
-+  void gdImageOpenPolygon(gdImagePtr im, gdPointPtr p, int n, int c);
-   void gdImageFilledPolygon (gdImagePtr im, gdPointPtr p, int n, int c);
+    BGD_EXPORT void gdImagePolygon (gdImagePtr im, gdPointPtr p, int n, int c);
++#ifdef __FreeBSD__
++   BGD_EXPORT void gdImageOpenPolygon(gdImagePtr im, gdPointPtr p, int n, int c);
++#endif
+    BGD_EXPORT void gdImageFilledPolygon (gdImagePtr im, gdPointPtr p, int n, int c);
  
-@@ -395,4 +400,13 @@
-   void *gdImageJpegPtr (gdImagePtr im, int *size, int quality);
+ /* These functions still work with truecolor images, 
+@@ -498,6 +507,17 @@
+ /* Best to free this memory with gdFree(), not free() */
+   BGD_EXPORT void *gdImageJpegPtr (gdImagePtr im, int *size, int quality);
  
-+  void gdImageLzw(gdImagePtr im, FILE *out);
-+  void* gdImageLzwPtr(gdImagePtr im, int *size);
-+  void gdImageLzwCtx(gdImagePtr im, gdIOCtxPtr out);
++#ifdef __FreeBSD__
++  BGD_EXPORT void gdImageLzw(gdImagePtr im, FILE *out);
++  BGD_EXPORT void* gdImageLzwPtr(gdImagePtr im, int *size);
++  BGD_EXPORT void gdImageLzwCtx(gdImagePtr im, gdIOCtxPtr out);
 +
-+  void gdImageBigGif(gdImagePtr im, FILE *out);
-+  void* gdImageBigGifPtr(gdImagePtr im, int *size);
-+  void gdImageBigGifCtx(gdImagePtr im, gdIOCtxPtr out);
++  BGD_EXPORT void gdImageBigGif(gdImagePtr im, FILE *out);
++  BGD_EXPORT void* gdImageBigGifPtr(gdImagePtr im, int *size);
++  BGD_EXPORT void gdImageBigGifCtx(gdImagePtr im, gdIOCtxPtr out);
++#endif
 +
 +
  /* A custom data sink. For backwards compatibility. Use
  	gdIOCtx instead. */
-@@ -408,4 +422,9 @@
+ /* The sink function must return -1 on error, otherwise the number
+@@ -511,6 +531,13 @@
+   gdSink, *gdSinkPtr;
  
-   void gdImagePngToSink (gdImagePtr im, gdSinkPtr out);
+    BGD_EXPORT void gdImagePngToSink (gdImagePtr im, gdSinkPtr out);
 +
-+  void gdImageGif(gdImagePtr im, FILE *out);
-+  void* gdImageGifPtr(gdImagePtr im, int *size);
-+  void gdImageGifCtx(gdImagePtr im, gdIOCtxPtr out);
-+  void gdImageGifToSink(gdImagePtr im, gdSinkPtr out);
++#ifdef __FreeBSD__
++   BGD_EXPORT void gdImageGif(gdImagePtr im, FILE *out);
++   BGD_EXPORT void* gdImageGifPtr(gdImagePtr im, int *size);
++   BGD_EXPORT void gdImageGifCtx(gdImagePtr im, gdIOCtxPtr out);
++   BGD_EXPORT void gdImageGifToSink(gdImagePtr im, gdSinkPtr out);
++#endif
  
-   void gdImageGd (gdImagePtr im, FILE * out);
+    BGD_EXPORT void gdImageGd (gdImagePtr im, FILE * out);
+    BGD_EXPORT void gdImageGd2 (gdImagePtr im, FILE * out, int cs, int fmt);
