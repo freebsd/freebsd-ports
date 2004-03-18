0fa869ed8bc50cf3f519a0caf24f59e42c4c9594
Tilman Keskinoz <arved@FreeBSD.org>
2004-03-18 14:07:03 +0000

    Don't depend on devel/pth
    Submitted by:	marcus

--- security/libgcrypt-devel/files/patch-ltmain.sh
+++ security/libgcrypt-devel/files/patch-ltmain.sh
@@ -1,23 +1,32 @@
---- scripts/ltmain.sh.orig	Wed Jul 17 15:42:41 2002
-+++ ltmain.sh	Wed Jul 17 15:47:36 2002
-@@ -1062,8 +1062,16 @@
+--- ltmain.sh.orig	Thu May 22 16:42:22 2003
++++ ltmain.sh	Tue May 27 12:12:52 2003
+@@ -1072,7 +1072,7 @@
+ 	  esac
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	 *-*-openbsd* | *-*-freebsd*)
++	 *-*-openbsd* | *-*-freebsd4*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -1082,8 +1082,16 @@
  	continue
  	;;
  
 +      -pthread)
-+        compile_command="$compile_command %%PTHREAD_LIBS%%"
-+	finalize_command="$finalize_command %%PTHREAD_LIBS%%"
-+	compiler_flags="$compiler_flags %%PTHREAD_LIBS%%"
-+	continue;
-+	;;	
++      compile_command="$compile_command -pthread"
++      finalize_command="$finalize_command -pthread"
++      compiler_flags="$compiler_flags -pthread"
++      continue
++      ;;
 +
        -module)
  	module=yes
-+	build_old_libs=yes
++	build_old_libs=no
  	continue
  	;;
  
-@@ -4247,10 +4255,12 @@
+@@ -4325,10 +4336,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
