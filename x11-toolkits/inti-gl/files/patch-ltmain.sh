cf416487eaf14441fc94d7cee2700bbffa9a42e7
ijliao <ijliao@FreeBSD.org>
2004-03-19 08:43:30 +0000

    add inti-gl 0.9 An Inti binding for GtkGLExt, the Open-GL extension to GTK+

---
+++ x11-toolkits/inti-gl/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Mar 19 14:14:27 2004
++++ ltmain.sh	Fri Mar 19 14:14:40 2004
+@@ -5425,10 +5425,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
