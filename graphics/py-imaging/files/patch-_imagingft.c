9a20ae510293fc1aeb0b39c0d51772c0374d6537
ijliao <ijliao@FreeBSD.org>
2004-03-23 05:48:42 +0000

    fix patch path

--- graphics/py-imaging/files/patch-_imagingft.c
+++ graphics/py-imaging/files/patch-_imagingft.c
@@ -1,5 +1,5 @@
---- _imagingft.c.orig	Mon Mar 22 23:27:33 2004
-+++ _imagingft.c	Mon Mar 22 23:28:03 2004
+--- ../_imagingft.c.orig	Mon Mar 22 23:27:33 2004
++++ ../_imagingft.c	Mon Mar 22 23:28:03 2004
 @@ -15,7 +15,8 @@
  #include "Python.h"
  #include "Imaging.h"
