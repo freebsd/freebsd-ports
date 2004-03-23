13b2a5a4b4c1f50198cd28109f1cf4228f8df54f
krion <krion@FreeBSD.org>
2004-03-23 08:40:12 +0000

    - Fix build on 5-x
    PR:		ports/64575
    Submitted by:	Matthew West <mwest@uct.ac.za>

---
+++ net/tvark/files/patch-main.c
@@ -0,0 +1,10 @@
+--- main.c.orig	Fri Nov 14 23:36:01 2003
++++ main.c	Mon Mar 22 18:19:28 2004
+@@ -37,7 +37,6 @@
+  *
+  */
+ 
+-#define _XOPEN_SOURCE 600
+ #define _BSD_SOURCE 1
+ 
+ #include <fcntl.h>
