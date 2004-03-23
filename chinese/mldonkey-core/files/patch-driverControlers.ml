4158deff8e0a5fb1b05e9627772607eca30c618e
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-03-23 08:26:49 +0000

    catch up mldonkey 2.5.14
    PR:		64604
    Submitted by:	maintainer

--- chinese/mldonkey-core/files/patch-driverControlers.ml
+++ chinese/mldonkey-core/files/patch-driverControlers.ml
@@ -1,6 +1,6 @@
---- src/daemon/driver/driverControlers.ml.orig	Mon Oct 13 02:41:30 2003
-+++ src/daemon/driver/driverControlers.ml	Wed Mar 10 19:11:04 2004
-@@ -291,6 +291,7 @@
+--- src/daemon/driver/driverControlers.ml.orig	Sun Mar  7 17:44:13 2004
++++ src/daemon/driver/driverControlers.ml	Tue Mar 23 11:31:09 2004
+@@ -319,6 +319,7 @@
    }
  
  
@@ -8,7 +8,7 @@
  let iac_will_naws = "\255\253\031"  
    
  let user_reader o telnet sock nread  = 
-@@ -320,7 +321,7 @@
+@@ -348,7 +349,7 @@
        
        let i = int_of_char c in
        telnet.telnet_iac <- false;
@@ -17,7 +17,7 @@
        
        if telnet.telnet_wait = 1 then begin
            Buffer.add_char telnet.telnet_buffer c;
-@@ -439,6 +440,7 @@
+@@ -469,6 +470,7 @@
          TcpBufferedSocket.set_closer sock user_closed;
          user_socks := sock :: !user_socks;
  
@@ -25,24 +25,22 @@
          TcpBufferedSocket.write_string sock iac_will_naws;
  
          before_telnet_output o sock;
-@@ -573,17 +575,17 @@
- let http_add_html_header buf = 
-   http_add_gen_header buf;
-   Buffer.add_string  buf "Pragma: no-cache\r\n";
--  Buffer.add_string  buf "Content-Type: text/html; charset=iso-8859-1\r\n";
-+  Buffer.add_string  buf "Content-Type: text/html; charset=big5\r\n";
-   Buffer.add_string  buf "\r\n"
+@@ -602,15 +604,15 @@
+ let http_add_html_header r = 
+   http_add_gen_header r;
+   add_reply_header r "Pragma" "no-cache";
+-  add_reply_header r "Content-Type" "text/html; charset=iso-8859-1"
++  add_reply_header r "Content-Type" "text/html; charset=big5"
  
- let http_add_css_header buf = 
-   http_add_gen_header buf;
--  Buffer.add_string  buf "Content-Type: text/css; charset=iso-8859-1\r\n";
-+  Buffer.add_string  buf "Content-Type: text/css; charset=big5\r\n";
-   Buffer.add_string  buf "\r\n"
+ let http_add_css_header r = 
+   http_add_gen_header r;
+-  add_reply_header r "Content-Type" "text/css; charset=iso-8859-1"
++  add_reply_header r "Content-Type" "text/css; charset=big5"
  
- let http_add_js_header buf =
-   http_add_gen_header buf;
--  Buffer.add_string  buf "Content-Type: text/javascript; charset=iso-8859-1\r\n";
-+  Buffer.add_string  buf "Content-Type: text/javascript; charset=big5\r\n";
-   Buffer.add_string  buf "\r\n"
- 
+ let http_add_js_header r =
+   http_add_gen_header r;
+-  add_reply_header  r "Content-Type" "text/javascript; charset=iso-8859-1"
++  add_reply_header  r "Content-Type" "text/javascript; charset=big5"
+   
  let any_ip = Ip.of_inet_addr Unix.inet_addr_any
+   
