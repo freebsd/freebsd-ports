45896ffb24247c1faf2ff1c8d934ed57f0faf0bd
eik <eik@FreeBSD.org>
2004-03-24 02:53:30 +0000

    workaround for changed soname versioning (OpenLDAP ITS#3016)
    Spotted by:	Andrzej Tobola <A.Tobola@wilk.2a.pl>

--- net/openldap24-server/files/patch-build::top.mk
+++ net/openldap24-server/files/patch-build::top.mk
@@ -1,11 +1,20 @@
---- build/top.mk.orig	Mon Apr  7 22:23:26 2003
-+++ build/top.mk	Mon Jul  7 05:35:52 2003
-@@ -192,6 +192,8 @@
+--- build/top.mk.orig	Wed Mar 17 21:15:31 2004
++++ build/top.mk	Wed Mar 24 03:37:31 2004
+@@ -68,7 +68,7 @@
+ 
+ LIBTOOL = @LIBTOOL@
+ LIBVERSION = @OPENLDAP_LIBVERSION@
+-LTVERSION = -release $(LIBVERSION)
++LTVERSION = -version-info $(LIBVERSION)
+ 
+ # libtool --only flag for libraries: platform specific
+ NT_LTONLY_LIB = # --only-$(BUILD_LIBS_DYNAMIC)
+@@ -190,6 +190,8 @@
+ 
  SLAPD_LIBS = @SLAPD_LIBS@ @SLAPD_PERL_LDFLAGS@ @SLAPD_SQL_LDFLAGS@ @SLAPD_SQL_LIBS@ @SLAPD_SLP_LIBS@
  SLURPD_LIBS = @SLURPD_LIBS@
- 
-+SLAPI_LIBS = @SLAPI_LIBS@
 +
++SLAPI_LIBS = @SLAPI_LIBS@
+ 
  # Our Defaults
  CC = $(AC_CC)
- DEFS = $(LDAP_INCPATH) $(XINCPATH) $(XDEFS) $(AC_DEFS) $(DEFINES)
