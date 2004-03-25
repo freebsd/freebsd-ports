3ee652d35fb0ad663eb2106ae0a71887b5863891
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-03-25 17:38:45 +0000

    Fix gaim's name resolution on 64-bit platforms.
    Submitted by:	Kenneth Culver <culverk@sweetdreamsracing.biz>

--- net/gaim/files/patch-src::proxy.c
+++ net/gaim/files/patch-src::proxy.c
@@ -1,19 +1,23 @@
-*** src/proxy.c.orig	Thu Jan 22 08:27:26 2004
---- src/proxy.c	Thu Jan 22 08:28:05 2004
-***************
-*** 974,980 ****
-  
-  	gaim_input_remove(phb->inpa);
-  
-! 	while ((nlc != 2) && (read(source, &inputline[pos++], 1) == 1)) {
-  		if (inputline[pos - 1] == '\n')
-  			nlc++;
-  		else if (inputline[pos - 1] != '\r')
---- 974,980 ----
-  
-  	gaim_input_remove(phb->inpa);
-  
-! 	while ((pos < sizeof(inputline)-1) && (nlc != 2) && (read(source, &inputline[pos++], 1) == 1)) {
-  		if (inputline[pos - 1] == '\n')
-  			nlc++;
-  		else if (inputline[pos - 1] != '\r')
+--- src/proxy.c.orig	Fri Jan  9 23:04:56 2004
++++ src/proxy.c	Thu Mar 25 12:37:20 2004
+@@ -388,7 +388,11 @@
+ 	int rc, err;
+ 	GSList *hosts = NULL;
+ 	struct sockaddr *addr = NULL;
++#if HAVE_GETADDRINFO
++	size_t addrlen;
++#else
+ 	socklen_t addrlen;
++#endif
+ 
+ 	gaim_debug(GAIM_DEBUG_INFO, "dns", "Host '%s' resolved\n", req->host);
+ 	gaim_input_remove(req->inpa);
+@@ -974,7 +978,7 @@
+ 
+ 	gaim_input_remove(phb->inpa);
+ 
+-	while ((nlc != 2) && (read(source, &inputline[pos++], 1) == 1)) {
++	while ((pos < sizeof(inputline)-1) && (nlc != 2) && (read(source, &inputline[pos++], 1) == 1)) {
+ 		if (inputline[pos - 1] == '\n')
+ 			nlc++;
+ 		else if (inputline[pos - 1] != '\r')
