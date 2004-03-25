4c1b9d2889067292686051ddfffdf5ea0fed4376
Akinori MUSHA <knu@FreeBSD.org>
2004-03-25 10:57:46 +0000

    Catch up with the master port's update to 4.2.0.

--- shells/zsh+euc_hack/files/patch-Src::Zle::compmatch.c
+++ shells/zsh+euc_hack/files/patch-Src::Zle::compmatch.c
@@ -1,6 +1,6 @@
---- Src/Zle/compmatch.c.orig	Tue Apr  3 20:25:13 2001
-+++ Src/Zle/compmatch.c	Thu May  9 07:59:05 2002
-@@ -1309,6 +1309,9 @@
+--- Src/Zle/compmatch.c.orig	Tue Feb 10 11:30:14 2004
++++ Src/Zle/compmatch.c	Thu Mar 25 19:51:03 2004
+@@ -1313,6 +1313,9 @@
      while (la && lb) {
  	if (*sa != *sb) {
  	    /* Different characters, try the matchers. */
@@ -10,7 +10,7 @@
  	    for (t = 0, ms = bmatchers; ms && !t; ms = ms->next) {
  		mp = ms->matcher;
  		if (mp && !mp->flags && mp->wlen > 0 && mp->llen > 0 &&
-@@ -1354,6 +1357,13 @@
+@@ -1358,6 +1361,13 @@
  	    }
  	    if (!t)
  		break;
@@ -24,10 +24,10 @@
  	} else {
  	    /* Same character, just take it. */
  	    if (rr <= 1) {
-@@ -1580,6 +1590,30 @@
- 	if (check_cmdata(md, sfx))
- 	    return ret;
- 
+@@ -1589,6 +1599,30 @@
+ 	 * characters, at this stage we still need the overall length
+ 	 * including Meta's as separate characters.
+ 	 */
 +#ifdef ZSH_EUC
 +	if (locale_is_euc) {
 +	    if (sfx)
@@ -54,4 +54,4 @@
 +#endif
  	for (l = 0, p = str, q = md->str;
  	     l < len && l < md->len && p[ind] == q[ind];
- 	     l++, p += add, q += add);
+ 	     l++, p += add, q += add) {}
