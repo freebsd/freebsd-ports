e3e9b999837bd6c16866224f1d4000d9d64a9e35
eik <eik@FreeBSD.org>
2004-03-25 01:46:54 +0000

    use included libtool

---
+++ net/openldap22-server/files/patch-build::ltmain.sh
@@ -0,0 +1,32 @@
+--- build/ltmain.sh.orig	Thu Jan  1 19:16:25 2004
++++ build/ltmain.sh	Thu Mar 25 02:19:32 2004
+@@ -1076,7 +1076,7 @@
+ 	  esac
+ 	 elif test "X$arg" = "X-lc_r"; then
+ 	  case $host in
+-	 *-*-openbsd* | *-*-freebsd*)
++	 *-*-openbsd* | *-*-freebsd4*)
+ 	    # Do not include libc_r directly, use -pthread flag.
+ 	    continue
+ 	    ;;
+@@ -4296,10 +4296,16 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
+-	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+-	instname="$dir/$name"i
+-	$show "$install_prog $instname $destdir/$name"
+-	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	+       case $host in
++	*-*-freebsd*)
++	  # Do not install the useless pseudo-library
++	  ;;
++	*)
++	  name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
++	  instname="$dir/$name"i
++	  $show "$install_prog $instname $destdir/$name"
++	  $run eval "$install_prog $instname $destdir/$name" || exit $?
++	esac
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
