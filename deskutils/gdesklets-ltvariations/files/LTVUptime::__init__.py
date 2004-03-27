824e196dde3ad78e6c32447399cd405ea65aebcb
pav <pav@FreeBSD.org>
2004-03-27 14:17:38 +0000

    - Replace Linuxism with FreeBSDism in uptime sensor (clock display). - Raise PORTREVISION.
    PR:		ports/64787
    Submitted by:	Tim Bishop <tim@bishnet.net> (maintainer)

--- deskutils/gdesklets-ltvariations/files/LTVUptime::__init__.py
+++ deskutils/gdesklets-ltvariations/files/LTVUptime::__init__.py
@@ -1,5 +1,5 @@
---- __init__.py.bak	Sun Mar 21 17:40:07 2004
-+++ __init__.py	Sun Mar 21 17:41:40 2004
+--- __init__.py.bak	Fri Mar 26 22:08:26 2004
++++ __init__.py	Fri Mar 26 22:08:49 2004
 @@ -21,7 +21,7 @@
  		self._set_config_type("update", TYPE_INT, int(interval))
  		self._set_config_type("format", TYPE_STRING, format)
@@ -9,3 +9,12 @@
  		self._set_config_type("kernel", TYPE_STRING, commands.getoutput("uname -s"))
  		self._set_config_type("version", TYPE_STRING, commands.getoutput("uname -r"))
  		self._set_config_type("processor", TYPE_STRING, commands.getoutput("uname -p"))
+@@ -63,7 +63,7 @@
+ 
+ 	def draw_uptime(self, data):
+ 		if self.__enable[ 0 ] != "0" :
+-			uptime = float( commands.getoutput("cat /proc/uptime").split()[0] )
++			uptime = time.time() - float(commands.getoutput("sysctl kern.boottime").split()[4][:-1])
+ 			d = int( uptime / 60 / 60 / 24 )
+ 			uptime = uptime - d * 60 * 60 * 24
+ 			h = int( uptime / 60 / 60 )
