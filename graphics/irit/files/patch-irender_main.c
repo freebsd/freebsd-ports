7495ef527a2483379c3590bc32954b18bafce57d
Pav Lucistnik <pav@FreeBSD.org>
2004-03-28 12:29:00 +0000

    - Fix build on != i386
    PR:		ports/64790
    Submitted by:	Igor Pokrovsky <tiamat@comset.net> (maintainer)
    Tested on:	Sledge

--- graphics/irit/files/patch-irender_main.c
+++ graphics/irit/files/patch-irender_main.c
@@ -1,15 +1,19 @@
---- irender/main.c.orig	Mon Feb  2 13:53:42 2004
-+++ irender/main.c	Mon Feb  2 14:05:03 2004
-@@ -13,7 +13,7 @@
+--- irender/main.c.orig	Thu Dec 25 18:34:39 2003
++++ irender/main.c	Fri Mar 26 08:51:39 2004
+@@ -12,9 +12,9 @@
+ #include "parser.h"
  #include "config.h"
  
- #ifdef __FreeBSD__
+-#ifdef __FreeBSD__
 -#include <floatingpoint.h>
+-#endif
++#if (defined(__FreeBSD__) && defined(__i386__))
 +#include <ieeefp.h>
- #endif
++#endif /* __FreeBSD__ && __i386__ */
  
  /* Contains all configuration options. Subject to change by config file and  */
-@@ -48,15 +48,15 @@
+ /* and command line parameters.                                              */
+@@ -48,16 +48,16 @@
  *****************************************************************************/
  int main(int argc, char *argv[])
  {
@@ -22,10 +26,24 @@
  	*MatrixFiles[] = {NULL, NULL};
      IRndrPtrType Rend;
      BoolType DoClipping;
-+
-+#ifdef __FreeBSD__
-+	fpsetmask(FP_X_INV | FP_X_DZ);
-+#endif /* __FreeBSD__ */
  
++#if (defined(__FreeBSD__) && defined(__i386__))
++	fpsetmask(FP_X_INV | FP_X_DZ);
++#endif /* __FreeBSD__ && __i386__ */
++
      DoClipping = TRUE;
      GlblLastTime = IritCPUTime(FALSE);
+     InitOptions();
+@@ -129,10 +129,10 @@
+     IRndrDestroy(Rend);
+     TakeTime("Z-Buffer dump");
+ 
+-#ifdef __FreeBSD__
++#if (defined(__FreeBSD__) && defined(__i386__))
+     fpresetsticky(FP_X_INV | FP_X_OFL | FP_X_UFL |
+ 		  FP_X_DZ | FP_X_IMP | FP_X_DNML);
+-#endif /* __FreeBSD__ */
++#endif /* __FreeBSD__ && __i386__ */
+ 
+     return 0;
+ }
