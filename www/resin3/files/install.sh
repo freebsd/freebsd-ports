7c5f2d9a14059567626004b92a7fa203cfffc1d7
Greg Lewis <glewis@FreeBSD.org>
2004-03-29 16:40:23 +0000

    . Upgrade to 3.0.7, fixes Apache support . GNU make is now required for compilation . USE_REINPLACE and REINPLACE_CMD instead of sed -i . make makesum to compute file size . config files in etc/resin3
    PR:		64844
    Submitted by:	Jean-Baptiste Quenot <jb.quenot@caraldi.com> (maintainer)

--- www/resin3/files/install.sh
+++ www/resin3/files/install.sh
@@ -3,29 +3,31 @@
 set -e
 
 warning() {
-    echo "Kept %%PREFIX%%/etc/%%APP_NAME%%.xml intact from previous installation, please diff"
-    echo "against %%APP_NAME%%.xml-dist"
+    echo "Kept %%PREFIX%%/etc/%%APP_NAME%%/resin.xml intact from previous installation, please diff"
+    echo "against %%PREFIX%%/etc/%%APP_NAME%%/resin.xml-dist for merging changes"
 }
 
 # Install config file only if none is already there
-if test -e %%PREFIX%%/etc/%%APP_NAME%%.xml && \
-    ! cmp -s %%WRKDIR%%/resin.conf %%PREFIX%%/etc/%%APP_NAME%%.xml ; then
+if test -e %%PREFIX%%/etc/%%APP_NAME%%/resin.xml && \
+    ! cmp -s %%WRKDIR%%/resin.conf %%PREFIX%%/etc/%%APP_NAME%%/resin.xml ; then
         echo
         echo "********************************************************************************"
         warning | fmt -w 80
         echo "********************************************************************************"
         echo
 else
-    install %%WRKDIR%%/resin.conf %%PREFIX%%/etc/%%APP_NAME%%.xml
-    echo Installed %%PREFIX%%/etc/%%APP_NAME%%.xml
+    test -d %%PREFIX%%/etc/%%APP_NAME%% || mkdir %%PREFIX%%/etc/%%APP_NAME%%
+    echo Created configuration directory %%PREFIX%%/etc/%%APP_NAME%%
+    install %%WRKDIR%%/resin.conf %%PREFIX%%/etc/%%APP_NAME%%/resin.xml
+    echo Installed %%PREFIX%%/etc/%%APP_NAME%%/resin.xml
 fi
 
 # Install new config file with '-dist' appended
-install %%WRKDIR%%/resin.conf %%PREFIX%%/etc/%%APP_NAME%%.xml-dist
-echo Installed %%PREFIX%%/etc/%%APP_NAME%%.xml-dist
+install %%WRKDIR%%/resin.conf %%PREFIX%%/etc/%%APP_NAME%%/resin.xml-dist
+echo Installed %%PREFIX%%/etc/%%APP_NAME%%/resin.xml-dist
 
-install %%WRKSRC%%/conf/app-default.xml %%PREFIX%%/etc/
-echo Installed %%PREFIX%%/etc/app-default.xml
+install %%WRKSRC%%/conf/app-default.xml %%PREFIX%%/etc/%%APP_NAME%%
+echo Installed %%PREFIX%%/etc/%%APP_NAME%%/app-default.xml
 
 install %%WRKDIR%%/resin.sh.in %%PREFIX%%/etc/rc.d/%%APP_NAME%%.sh
 chmod 755 %%PREFIX%%/etc/rc.d/%%APP_NAME%%.sh
@@ -39,13 +41,13 @@ echo Created installation directory %%APP_HOME%%
 
 list()
 {
-    for dir in %%COPYDIRS%% libexec ; do
+    for dir in %%COPYDIRS%% ; do
         ( cd %%WRKSRC%% ; find $dir )
     done
 }
 
 # Remove all empty dirs
-( cd %%WRKSRC%% && find %%COPYDIRS%% libexec -type d -empty -delete )
+( cd %%WRKSRC%% && find %%COPYDIRS%% -type d -empty -delete )
 
 echo Installing in %%APP_HOME%%
 list | xargs tar -C %%WRKSRC%% -cf- | tar -C %%APP_HOME%% -xpf-
