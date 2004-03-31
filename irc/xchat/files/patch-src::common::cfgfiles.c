648bc08b6a36e36acdb650f0fa0ca7bc01790df6
Eivind Eklund <eivind@FreeBSD.org>
2004-03-31 14:27:43 +0000

    Make the default config conform to RFC1122 (no public away messages).

---
+++ irc/xchat/files/patch-src::common::cfgfiles.c
@@ -0,0 +1,11 @@
+--- src/common/cfgfiles.c.orig	Mon Mar 29 15:46:01 2004
++++ src/common/cfgfiles.c	Mon Mar 29 15:46:20 2004
+@@ -600,7 +600,7 @@
+ 
+ 		prefs.autoresume = 1;
+ 		prefs.show_away_once = 1;
+-		prefs.show_away_message = 1;
++		prefs.show_away_message = 0;
+ 		prefs.indent_pixels = 80;
+ 		prefs.dialog_indent_pixels = 80;
+ 		prefs.indent_nicks = 1;
