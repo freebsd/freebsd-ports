793ebec7dba68e2c4490d839b6b321a23ce88c3a
marcus <marcus@FreeBSD.org>
2004-04-02 18:36:32 +0000

    Use an appropriate GLib type which should fix the build on both 32 and 64-bit platforms.
    Reported by:	Sven Esbjerg <esbjerg@sifira.dk>

--- graphics/swfdec/files/patch-player-swf_play.c
+++ graphics/swfdec/files/patch-player-swf_play.c
@@ -1,11 +1,11 @@
---- player/swf_play.c.orig	Mon Mar 29 11:43:50 2004
-+++ player/swf_play.c	Mon Mar 29 11:44:14 2004
+--- player/swf_play.c.orig	Thu Apr  3 23:11:57 2003
++++ player/swf_play.c	Fri Apr  2 13:34:32 2004
 @@ -390,7 +390,7 @@
  static gboolean input(GIOChannel *chan, GIOCondition cond, gpointer ignored)
  {
  	char *data;
 -	int bytes_read;
-+	long bytes_read;
++	gsize bytes_read;
  	GError *error = NULL;
  	int ret;
  
