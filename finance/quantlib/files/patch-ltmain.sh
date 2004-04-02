930af243ccdc9c9dca40adaa6edca5ee2c274f90
ijliao <ijliao@FreeBSD.org>
2004-04-02 03:27:02 +0000

    upgrade to 0.3.5

---
+++ finance/quantlib/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- config/ltmain.sh.orig	Fri Apr  2 10:15:05 2004
++++ config/ltmain.sh	Fri Apr  2 10:15:13 2004
+@@ -5528,10 +5528,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
