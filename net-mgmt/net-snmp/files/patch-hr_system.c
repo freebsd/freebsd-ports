608ab07e0e4e730b51f88da74102bc3c727d1232
kuriyama <kuriyama@FreeBSD.org>
2004-04-03 13:49:22 +0000

    Upgrade to 5.1.1.

--- net-mgmt/net-snmp/files/patch-hr_system.c
+++ net-mgmt/net-snmp/files/patch-hr_system.c
@@ -1,21 +1,11 @@
---- agent/mibgroup/host/hr_system.c.orig	Sat Mar  1 05:35:13 2003
-+++ agent/mibgroup/host/hr_system.c	Sun Feb  8 17:39:50 2004
-@@ -280,7 +280,6 @@
-     while ((utmp_p = getutent()) != NULL) {
+--- agent/mibgroup/host/hr_system.c.orig	Mon Feb 23 02:29:34 2004
++++ agent/mibgroup/host/hr_system.c	Sat Apr  3 00:00:24 2004
+@@ -596,6 +596,8 @@
  #ifndef UTMP_HAS_NO_TYPE
-         if (utmp_p->ut_type == USER_PROCESS) {
--#endif
-             /* This block of code fixes zombie user PIDs in the
-                utmp/utmpx file that would otherwise be counted as a
-                current user */
-@@ -291,6 +290,10 @@
-             }
-             ++total;
-         }
+         if (utmp_p->ut_type != USER_PROCESS)
+             continue;
 +#else
 +	if (*utmp_p->ut_name != '~')
-+            ++total;
-+#endif
-     }
-     endutent();
-     return total;
+ #endif
+ #ifndef UTMP_HAS_NO_PID
+             /* This block of code fixes zombie user PIDs in the
