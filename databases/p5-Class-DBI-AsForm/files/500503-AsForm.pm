2929a99bad579d593ba4e88f2f9eb14ba5e99134
Erwin Lansing <erwin@FreeBSD.org>
2004-04-04 21:11:42 +0000

    Add p5-Class-DBI-AsForm 2.2, produce HTML form elements for database columns.
    This module helps to generate HTML forms for creating new database
    rows or editing existing rows. It maps column names in a database
    table to HTML form elements which fit the schema. Large text fields
    are turned into textareas, and fields with a has-a relationship to
    other Class::DBI tables are turned into select drop-downs populated
    with objects from the joined class.
    
    WWW: http://search.cpan.org/dist/Class-DBI-AsForm/
    
    PR:		ports/65147
    Submitted by:	Lars Thegler <lars@thegler.dk>

---
+++ databases/p5-Class-DBI-AsForm/files/500503-AsForm.pm
@@ -0,0 +1,49 @@
+--- AsForm.pm.orig	Sat Apr  3 22:37:32 2004
++++ AsForm.pm	Sat Apr  3 22:37:38 2004
+@@ -1,16 +1,14 @@
+ package Class::DBI::AsForm;
+-use 5.006;
+ use strict;
+-use warnings;
+ use Class::DBI::Plugin::Type ();
+-our $OLD_STYLE = 0;
++use vars qw($OLD_STYLE);$OLD_STYLE = 0;
+ 
+ use HTML::Element;
+ require Exporter;
+-our @ISA = qw(Exporter);
+-our @EXPORT = qw( to_cgi to_field _to_textarea _to_textfield _to_select
++use vars qw(@ISA);@ISA = qw(Exporter);
++use vars qw(@EXPORT);@EXPORT = qw( to_cgi to_field _to_textarea _to_textfield _to_select
+ type_of );
+-our $VERSION = '2.2';
++use vars qw($VERSION);$VERSION = '2.2';
+ 
+ =head1 NAME
+ 
+@@ -100,14 +98,14 @@
+ sub _to_textarea {
+     my ($self, $col) = @_;
+     my $a = HTML::Element->new("textarea", name => $col);
+-    if (ref $self) { $a->push_content($self->$col) }
++    if (ref $self) { $a->push_content($self->$col()) }
+     $OLD_STYLE && return $a->as_HTML;
+     $a;
+ }
+ 
+ sub _to_textfield {
+     my ($self, $col) = @_;
+-    my $value = ref $self && $self->$col;
++    my $value = ref $self && $self->$col();
+     my $a = HTML::Element->new("input", type=> "text", name => $col);
+     $a->attr("value" => $value) if $value;
+     $OLD_STYLE && return $a->as_HTML;
+@@ -122,7 +120,7 @@
+     for (@objs) { 
+         my $sel = HTML::Element->new("option", value => $_->id);
+         $sel->attr("selected" => "selected") if ref $self 
+-                                                and eval { $_->id == $self->$col->id };
++                                                and eval { $_->id == $self->$col()->id };
+         $sel->push_content($_->stringify_self);
+         $a->push_content($sel);
+     }
