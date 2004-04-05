895f7e80dad6e9ae28ca8ac50106b0f95458d4c9
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2004-04-05 23:22:10 +0000

    Update to 1.8.0
    Submitted by:	Tom McLaughlin <tmclaugh@sdf.lonestar.org>

--- net/xmule/files/patch-src::otherfunctions.cpp
+++ net/xmule/files/patch-src::otherfunctions.cpp
@@ -1,11 +1,11 @@
---- src/otherfunctions.cpp.orig	Mon May 26 12:05:22 2003
-+++ src/otherfunctions.cpp	Mon May 26 12:05:37 2003
-@@ -546,7 +546,7 @@
+--- src/otherfunctions.cpp.orig	Mon Apr  5 19:58:46 2004
++++ src/otherfunctions.cpp	Mon Apr  5 19:59:45 2004
+@@ -662,7 +662,7 @@
  }
  
- #ifdef __FreeBSD__   
--long long atoll( char * s ){
-+long long atoll(const char * s){
- return  strtoll(s, (char **)NULL, 10);
+ #ifdef __FreeBSD__
+-long long atoll(char * s) {
++long long atoll(const char * s) {
+ 	return strtoll(s, (char **) NULL, 10);
  }
- #endif /* __FreeBSD__ */
+ 
