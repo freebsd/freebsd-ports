3c3fe1c67e542be540b24ad96c5365ad3ac44e25
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-04-05 19:06:26 +0000

    Fix a crash with GNOME 2.6.
    PR:		65222
    Submitted by:	maintainer

--- x11/gcursor/files/patch-src::gcursor.c
+++ x11/gcursor/files/patch-src::gcursor.c
@@ -1,5 +1,5 @@
---- src/gcursor.c.orig	Fri Mar 26 17:51:58 2004
-+++ src/gcursor.c	Fri Mar 26 17:55:16 2004
+--- src/gcursor.c.orig	Fri Mar 26 12:54:55 2004
++++ src/gcursor.c	Mon Apr  5 13:21:52 2004
 @@ -16,7 +16,7 @@
  GConfClient *client;
  XcursorImage * cursor = NULL;
@@ -20,3 +20,12 @@
  	"%s/.icons/"
  };	
  
+@@ -348,7 +346,7 @@
+ 				GTK_STOCK_DIALOG_QUESTION, GTK_ICON_SIZE_LARGE_TOOLBAR);
+ 	}
+ 
+-	if(animations[1].image->images != NULL && animations[1].image->nimage != 0)
++	if(animations[1].image != NULL && animations[1].image->nimage != 0)
+ 	{
+ 		pixbuf = get_pixbuf_from_cursor_image(animations[1].image->images[animations[1].pos]);
+ 		gtk_image_set_from_pixbuf(GTK_IMAGE(glade_xml_get_widget(main_window, "example_2")),
