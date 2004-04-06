0e1c425b35bead7a2a99e686b35ec72aef1461ee
ijliao <ijliao@FreeBSD.org>
2004-04-06 01:31:21 +0000

    add dprog 0.3.1 A language for specifying dynamic programming algorithms

---
+++ devel/dprog/files/patch-type_checking.hh
@@ -0,0 +1,10 @@
+--- type_checking.hh.orig	Tue Dec 30 17:41:32 2003
++++ type_checking.hh	Tue Dec 30 17:41:42 2003
+@@ -7,6 +7,7 @@
+ #include <set>
+ #include <iostream>
+ #include <typeinfo>
++#include <cassert>
+ 
+ namespace type_checking {
+ 
