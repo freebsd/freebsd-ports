4e92e6194fa0fd47c49314976c015ce655e12ccc
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-04-06 01:31:21 +0000

    add dprog 0.3.1 A language for specifying dynamic programming algorithms

---
+++ devel/dprog/files/patch-codegen_test.cc
@@ -0,0 +1,11 @@
+--- codegen_test.cc.orig	Tue Apr  6 08:57:15 2004
++++ codegen_test.cc	Tue Apr  6 08:57:29 2004
+@@ -2,6 +2,8 @@
+ #include "codegen.hh"
+ #include <iostream>
+ 
++#include <cassert>
++
+ using namespace std;
+ 
+ int
---
+++ devel/dprog/files/patch-cxx_dprog.hh
@@ -0,0 +1,10 @@
+--- cxx_dprog.hh.orig	Tue Apr  6 08:58:45 2004
++++ cxx_dprog.hh	Tue Apr  6 08:59:01 2004
+@@ -5,6 +5,7 @@
+ 
+ #include <vector>
+ #include <cstdarg>
++#include <cassert>
+ 
+ #include <iostream>
+ 
