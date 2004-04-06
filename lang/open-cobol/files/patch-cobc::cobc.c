adc606fcba90c48304a1b6dcb74ef9ef32684483
Kirill Ponomarev <krion@FreeBSD.org>
2004-04-06 21:02:44 +0000

    - Support USE_GETOPT_LONG - Support WITHOUT_NLS
    PR:		ports/65262
    Submitted by:	Ports Fury

---
+++ lang/open-cobol/files/patch-cobc::cobc.c
@@ -0,0 +1,11 @@
+--- cobc/cobc.c.orig	Thu Jun  5 09:02:15 2003
++++ cobc/cobc.c	Sun Apr  4 00:32:21 2004
+@@ -36,7 +36,7 @@
+ 
+ #include "cobc.h"
+ #include "tree.h"
+-#include "lib/getopt.h"
++#include <getopt.h>
+ 
+ 
+ /*
