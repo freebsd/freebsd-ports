e590e7b8cb63681f621ffdb14626051f897f5cb8
vanilla <vanilla@FreeBSD.org>
2004-04-07 04:13:35 +0000

    Fix auto-complete problem.
    Submitted by:	mhsin (msgin_at_mhsin_dot_org)

--- chinese/irssi/files/patch-src::fe-text::gui-readline.c
+++ chinese/irssi/files/patch-src::fe-text::gui-readline.c
@@ -1,5 +1,5 @@
 --- src/fe-text/gui-readline.c.orig	Thu Nov 27 01:30:03 2003
-+++ src/fe-text/gui-readline.c	Sun Dec 14 01:54:28 2003
++++ src/fe-text/gui-readline.c	Wed Apr  7 11:02:59 2004
 @@ -187,7 +187,12 @@
  			out[utf16_char_to_utf8(arr[i], out)] = '\0';
  			g_string_append(str, out);
@@ -13,4 +13,26 @@
 +				g_string_append_c(str, arr[i]);
  		}
  	}
+ 
+@@ -620,9 +625,7 @@
+ 	char *text, *line;
+ 	int pos;
+ 
+-	pos = gui_entry_get_pos(active_entry);
+-
+-        text = gui_entry_get_text(active_entry);
++        text = gui_entry_get_text_and_pos(active_entry, &pos);
+ 	line = word_complete(active_win, text, &pos, erase);
+ 	g_free(text);
+ 
+@@ -648,9 +651,7 @@
+ 	char *text, *line;
+ 	int pos;
+ 
+-	pos = gui_entry_get_pos(active_entry);
+-
+-        text = gui_entry_get_text(active_entry);
++        text = gui_entry_get_text_and_pos(active_entry, &pos);
+ 	line = auto_word_complete(text, &pos);
+ 	g_free(text);
  
