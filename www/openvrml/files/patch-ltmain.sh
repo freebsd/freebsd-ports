f850b2b7e3e9361e6d2c2fbe10a8192d2606216c
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-04-07 06:28:35 +0000

    upgrade to 0.14.3

---
+++ www/openvrml/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Wed Apr  7 12:00:09 2004
++++ ltmain.sh	Wed Apr  7 12:00:20 2004
+@@ -5428,10 +5428,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
