d3eb98a2dcd37db8315d41f406d0944f99066e14
Kirill Ponomarev <krion@FreeBSD.org>
2004-04-08 20:50:06 +0000

    - Update to version 0.80.2.a
    PR:		ports/65259
    Submitted by:	Ports Fury

---
+++ archivers/rvm/files/patch-estring.cc
@@ -0,0 +1,10 @@
+--- estring.cc.orig	Thu Mar 25 00:36:48 2004
++++ estring.cc	Sat Apr  3 21:20:18 2004
+@@ -4,6 +4,7 @@
+ #include <string>
+ #include <cstdio>
+ #include <cctype>
++#include <cassert>
+ 
+ #include "debug.h"
+ #include "types.h"
---
+++ archivers/rvm/files/patch-vaulter.cpp
@@ -0,0 +1,10 @@
+--- vaulter.cc.orig	Wed Mar 24 05:03:42 2004
++++ vaulter.cc	Sat Apr  3 21:24:37 2004
+@@ -4,6 +4,7 @@
+ #include <vector>
+ #include <map>
+ #include <string>
++#include <cassert>
+ 
+ #include "debug.h"
+ #include "error.h"
