2d56da8bcbc32a6bf0ff3ce067d4a763afe486b8
krion <krion@FreeBSD.org>
2004-04-08 18:28:21 +0000

    - Fix bug with avifile
    PR:		ports/65327
    Submitted by:	maintainer

---
+++ graphics/zphoto/files/patch-util.c
@@ -0,0 +1,10 @@
+--- util.c.orig	Sun Apr  4 02:11:57 2004
++++ util.c	Fri Apr  9 00:25:22 2004
+@@ -393,7 +393,6 @@
+ int
+ zphoto_video_file_p (const char *file_name)
+ {
+-    return 0;
+     /* 
+      * Check by its file name only (not its content). 
+      */
