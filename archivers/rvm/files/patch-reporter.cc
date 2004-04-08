5010de23bf3f39c87c2811fa9eeff3817b6e854e
krion <krion@FreeBSD.org>
2004-04-08 20:50:06 +0000

    - Update to version 0.80.2.a
    PR:		ports/65259
    Submitted by:	Ports Fury

---
+++ archivers/rvm/files/patch-reporter.cc
@@ -0,0 +1,10 @@
+--- reporter.cc.orig	Thu Mar 25 00:28:09 2004
++++ reporter.cc	Sat Apr  3 21:26:51 2004
+@@ -4,6 +4,7 @@
+ #include <string>
+ #include <vector>
+ #include <algorithm>
++#include <cassert>
+ 
+ #include "debug.h"
+ #include "error.h"
