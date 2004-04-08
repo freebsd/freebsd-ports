63bbdcda9cdc3947b3093a7e2e2c254560dbeb32
marcus <marcus@FreeBSD.org>
2004-04-08 18:36:39 +0000

    * Teach vte about posix_openpt() * Attempt to speed up terminal rendering a bit [1] * Don't leave children shells linger after logging out of GNOME [2]
    Submitted by:	Jean-Yves Lefort <jylefort@brutele.be> [2]
    Obtained from:	http://bugzilla.gnome.org/show_bug.cgi?id=122656 [1]

--- x11-toolkits/vte/files/patch-src_pty.c
+++ x11-toolkits/vte/files/patch-src_pty.c
@@ -1,5 +1,5 @@
---- src/pty.c.orig	Sat Apr 12 00:47:10 2003
-+++ src/pty.c	Sat Apr 12 00:47:34 2003
+--- src/pty.c.orig	Tue Jun  3 15:54:15 2003
++++ src/pty.c	Wed Apr  7 02:55:57 2004
 @@ -45,6 +45,7 @@
  #include "pty.h"
  
@@ -8,3 +8,12 @@
  #include "../gnome-pty-helper/gnome-pty.h"
  #endif
  
+@@ -709,6 +710,8 @@
+ #ifdef HAVE_GETPT
+ 	/* Call the system's function for allocating a pty. */
+ 	fd = getpt();
++#elif defined(HAVE_POSIX_OPENPT)
++	fd = posix_openpt(O_RDWR | O_NOCTTY);
+ #else
+ 	/* Try to allocate a pty by accessing the pty master multiplex. */
+ 	fd = open("/dev/ptmx", O_RDWR | O_NOCTTY);
