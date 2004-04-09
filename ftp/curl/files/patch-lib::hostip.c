bcc2c336a4d34a79120d96d7fc1ce48e0b72a149
Peter Pentchev <roam@FreeBSD.org>
2004-04-09 12:14:13 +0000

    Update to curl 7.11.1.

--- ftp/curl/files/patch-lib::hostip.c
+++ ftp/curl/files/patch-lib::hostip.c
@@ -1,16 +1,16 @@
---- lib/hostip.c.orig	Tue May 13 05:12:17 2003
-+++ lib/hostip.c	Sun Jun 15 18:44:13 2003
-@@ -497,11 +497,11 @@
+--- lib/hostip.c.orig	Fri Apr  9 14:28:07 2004
++++ lib/hostip.c	Fri Apr  9 14:29:12 2004
+@@ -887,11 +887,11 @@
  
    /* now, shrink the allocated buffer to the size we actually need, which
       most often is only a fraction of the original alloc */
--  newbuf=(char *)realloc(*buf, (int)bufptr-(int)(*buf));
-+  newbuf=(char *)realloc(*buf, (int)(bufptr-*buf));
+-  newbuf=(char *)realloc(*buf, (long)bufptr-(long)(*buf));
++  newbuf=(char *)realloc(*buf, (long)(bufptr-*buf));
  
    /* if the alloc moved, we need to adjust things again */
    if(newbuf != *buf)
--    hostcache_fixoffset((struct hostent*)newbuf, (int)newbuf-(int)*buf);
-+    hostcache_fixoffset((struct hostent*)newbuf, (int)(newbuf-*buf));
+-    hostcache_fixoffset((struct hostent*)newbuf, (long)newbuf-(long)*buf);
++    hostcache_fixoffset((struct hostent*)newbuf, (long)(newbuf-*buf));
  
    /* setup the return */
    *buf = newbuf;
