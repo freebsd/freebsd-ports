7ead418e9b515590dda33e738e0f5e34b36a712b
Maho Nakata <maho@FreeBSD.org>
2004-04-09 08:47:39 +0000

    update to 1.1.2RC (checkout cws_srx645_ooo112fix1, 2004/04/08 9pm JST) please see the release note http://download.openoffice.org/1.1.2rc/release_notes_1.1.2rc.html
    removed patches:
    a) patch-solenv+inc+unxfbsdi.mk
       http://www.openoffice.org/issues/show_bug.cgi?id=25500
    
    b) patch-sw+source+filter+ww8+ww8scan.cxx
       http://www.openoffice.org/issues/show_bug.cgi?id=23856
       http://www.openoffice.org/issues/show_bug.cgi?id=24123
       but actually fixed somewhere I cannot find
    
    added patch:
    c) patch-setup2+wrapper+install.sh
       http://www.openoffice.org/issues/show_bug.cgi?id=27638
    
    Note: simple test was done for FreeBSD 5.2.1-RELEASE

---
+++ editors/openoffice-1.1-devel/files/patch-setup2+wrapper+install.sh
@@ -0,0 +1,13 @@
+--- ../setup2/wrapper/install.sh.org	Fri Apr  9 17:29:34 2004
++++ ../setup2/wrapper/install.sh	Sat Jan 10 03:53:17 2004
+@@ -4,8 +4,8 @@
+ cd `dirname $0`
+ 
+ # Set the Defaults Here
+-oo_version="OpenOffice.org 1.1.1"
+-oo_home=OpenOffice.org1.1.1
++oo_version="OpenOffice.org 1.1.2"
++oo_home=OpenOffice.org1.1.2
+ oo_default_prefix=/usr/local
+ oo_default_netflg=-net
+ oo_default_interact=no
