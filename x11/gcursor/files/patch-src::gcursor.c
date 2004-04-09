085aa76e07a859f12252b8978209ad583dab25bd
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-04-09 03:48:04 +0000

    Update to 0.05.
    PR:		65342
    Submitted by:	maintainer

--- x11/gcursor/files/patch-src::gcursor.c
+++ x11/gcursor/files/patch-src::gcursor.c
@@ -1,31 +1,14 @@
---- src/gcursor.c.orig	Fri Mar 26 12:54:55 2004
-+++ src/gcursor.c	Mon Apr  5 13:21:52 2004
-@@ -16,7 +16,7 @@
- GConfClient *client;
- XcursorImage * cursor = NULL;
- char*current_theme = NULL;
--int path_index = 4;
-+int path_index = 2;
+--- src/gcursor.c.orig	Thu Apr  8 10:25:03 2004
++++ src/gcursor.c	Thu Apr  8 18:00:14 2004
+@@ -32,10 +32,7 @@
  
- typedef struct {
- 	XcursorImages *image;
-@@ -26,9 +26,7 @@
- animation_struct animations[4];
- 
- char *paths[4] = {
+ #warning move this into gconf
+ char *paths[] = {
 -	"/usr/share/cursors/xfree/",
+-	"/usr/X11R6/lib/X11/icons/",
 -	"/usr/local/share/icons/",
 -	"/usr/share/icons/",
 +	"%%X11BASE%%/lib/X11/icons/",
- 	"%s/.icons/"
+ 	"%s/.icons/",
+ 	NULL
  };	
- 
-@@ -348,7 +346,7 @@
- 				GTK_STOCK_DIALOG_QUESTION, GTK_ICON_SIZE_LARGE_TOOLBAR);
- 	}
- 
--	if(animations[1].image->images != NULL && animations[1].image->nimage != 0)
-+	if(animations[1].image != NULL && animations[1].image->nimage != 0)
- 	{
- 		pixbuf = get_pixbuf_from_cursor_image(animations[1].image->images[animations[1].pos]);
- 		gtk_image_set_from_pixbuf(GTK_IMAGE(glade_xml_get_widget(main_window, "example_2")),
