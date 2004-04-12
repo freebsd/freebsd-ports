4c10905f03e3af163cb874980e702c1d7fe8d6ec
bland <bland@FreeBSD.org>
2004-04-12 16:20:45 +0000

    - Update to 0.38.1 - Workaround internal compiller error on -STABLE [1] - More build patches incl. fix for build with antiqe STL on -STABLE
    Reported by:	Lee Harr <missive@hotmail.com> [1]

---
+++ graphics/inkscape/files/patch-src::livarot::PathOutline.cpp
@@ -0,0 +1,10 @@
+--- src/livarot/PathOutline.cpp.orig	Sat Apr 10 04:09:41 2004
++++ src/livarot/PathOutline.cpp	Sat Apr 10 04:34:35 2004
+@@ -9,6 +9,7 @@
+ #include "Path.h"
+ //#include "MyMath.h"
+ #include <math.h>
++#include <algorithm>
+ #include <libnr/nr-point-fns.h>
+ 
+ /*
