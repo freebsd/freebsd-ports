d35d0448e69214f86e11aa5a5c7bf7bada29e6f1
arved <arved@FreeBSD.org>
2004-04-13 19:06:16 +0000

    Fix build on amd64 by using va_copy. Use external libraries, where possible. Bump PORTREVISION.

---
+++ mail/lmtp2nntp/files/patch-ts.c
@@ -0,0 +1,14 @@
+--- ts.c.orig	Tue Apr 13 19:32:20 2004
++++ lib_tai/ts.c	Tue Apr 13 19:32:46 2004
+@@ -206,7 +206,11 @@
+ 
+     if (format == NULL)
+         return NULL;
++#ifdef __amd64__
++    va_copy(ap2,ap);
++#else
+     ap2 = ap;
++#endif
+     if ((n = ts_suite_mvxprintf(NULL, 0, format, ap)) == -1)
+         return NULL;
+     if ((buffer = (char *)malloc(n+1)) == NULL)
