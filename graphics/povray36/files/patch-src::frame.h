903f8a161a14fbab37ddfcc33ba71da95524af83
Maho Nakata <maho@FreeBSD.org>
2004-04-13 03:42:20 +0000

    Hope this fixes for Alpha 4-stable

--- graphics/povray36/files/patch-src::frame.h
+++ graphics/povray36/files/patch-src::frame.h
@@ -4,7 +4,7 @@
  class pov_istream_class;
  class pov_ostream_class;
  
-+#if defined(__LP64__) 
++#if defined(__LP64__) || defined(__alpha__)
 +typedef unsigned int u_int32 ;
 +#else
  typedef unsigned long u_int32 ;
@@ -13,7 +13,7 @@
  typedef unsigned char u_int8 ;
  typedef unsigned char byte ;
  
-+#if defined(__LP64__) 
++#if defined(__LP64__) || defined(__alpha__)
 +typedef signed int int32 ;
 +#else
  typedef signed long int32 ;
