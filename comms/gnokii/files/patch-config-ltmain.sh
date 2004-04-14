80df8d5a985aa8c92c067dd95ff53fbee6162739
Kirill Ponomarev <krion@FreeBSD.org>
2004-04-14 21:25:09 +0000

    - Update to version 0.6.1
    PR:		ports/65547
    Submitted by:	maintainer

--- comms/gnokii/files/patch-config-ltmain.sh
+++ comms/gnokii/files/patch-config-ltmain.sh
@@ -1,15 +1,15 @@
---- config/ltmain.sh.orig	Sat Feb 14 16:37:52 2004
-+++ config/ltmain.sh	Sat Feb 14 16:39:37 2004
-@@ -1061,7 +1061,11 @@
+--- config/ltmain.sh.orig	Wed Apr 14 20:10:18 2004
++++ config/ltmain.sh	Wed Apr 14 20:14:14 2004
+@@ -1356,7 +1356,11 @@
  	  esac
- 	 elif test "X$arg" = "X-lc_r"; then
- 	  case $host in
+ 	elif test "X$arg" = "X-lc_r"; then
+ 	 case $host in
 -	 *-*-openbsd* | *-*-freebsd*)
 +#	 *-*-openbsd* | *-*-freebsd*)
 +#	this situation is already managed by the ports tree for
 +#	freebsd, so leave it as is(-lc_r is actually needed for machines
 +#	built from freebsd-current source in a short life span :)
-+	  *-*-openbsd*)
- 	    # Do not include libc_r directly, use -pthread flag.
- 	    continue
- 	    ;;
++	 *-*-openbsd*)
+ 	   # Do not include libc_r directly, use -pthread flag.
+ 	   continue
+ 	   ;;
