b2ab9dc2593c4a82d4350e002a731f8074f870f5
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-04-14 09:27:54 +0000

    add flowdesigner 0.8.0 A free (GPL) "data flow oriented" development environment

---
+++ devel/flowdesigner/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Wed Jan 21 09:59:22 2004
++++ ltmain.sh	Wed Apr 14 16:50:20 2004
+@@ -5428,10 +5428,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
