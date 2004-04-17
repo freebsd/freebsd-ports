284534483a58ed250974a40b147943fca2f9f042
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-04-17 04:52:38 +0000

    * statfs.f_flags is a bitmap, so we need to do boolean operations, and not   simply test for equality * Fix a potential crash in many GNOME applications because the device number   for various file systems could not be obtained

--- devel/gnomevfs2/files/patch-libgnomevfs_gnome-vfs-unix-mounts.c
+++ devel/gnomevfs2/files/patch-libgnomevfs_gnome-vfs-unix-mounts.c
@@ -1,5 +1,27 @@
---- libgnomevfs/gnome-vfs-unix-mounts.c.orig	Mon Mar  8 22:29:57 2004
-+++ libgnomevfs/gnome-vfs-unix-mounts.c	Mon Mar  8 22:33:35 2004
+--- libgnomevfs/gnome-vfs-unix-mounts.c.orig	Mon Feb 16 06:13:27 2004
++++ libgnomevfs/gnome-vfs-unix-mounts.c	Sat Apr 17 00:45:01 2004
+@@ -141,9 +141,9 @@
+ 		FD_ZERO(&read_fds);
+ 		FD_SET(pipes[0], &read_fds);
+ 
+-		/* Wait max half a second */
+-		tv.tv_sec = 0;
+-		tv.tv_usec = 500;
++		/* Wait max one second */
++		tv.tv_sec = 1;
++		tv.tv_usec = 0;
+ 
+ 		res = select(pipes[0] + 1,
+ 			     &read_fds, NULL, NULL, &tv);
+@@ -428,7 +428,7 @@
+ 		mount_entry->mount_path = g_strdup (mntent[i].f_mntonname);
+ 		mount_entry->device_path = g_strdup (mntent[i].f_mntfromname);
+ 		mount_entry->filesystem_type = g_strdup (mntent[i].f_fstypename);
+-		if (mntent[i].f_flags == MNT_RDONLY) {
++		if (mntent[i].f_flags & MNT_RDONLY) {
+ 		    	mount_entry->is_read_only = TRUE;
+ 		}
+ 
 @@ -857,8 +857,9 @@
  
  #ifdef HAVE_SYS_SYSCTL_H
