8e7d83250bb0612ced9156352b7ef17ce75fbdc3
krion <krion@FreeBSD.org>
2004-04-17 06:22:03 +0000

    - Update to version 0.1.0 - Take maintainership

---
+++ security/gsasl/files/patch-lib::lt-main.sh
@@ -0,0 +1,15 @@
+--- lib/ltmain.sh.orig	Sat Apr 17 08:15:31 2004
++++ lib/ltmain.sh	Sat Apr 17 08:16:16 2004
+@@ -5528,10 +5528,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
