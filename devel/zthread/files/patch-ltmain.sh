a1cc7d6c25562370ff33c6ae6b24637f8f8a1e39
pav <pav@FreeBSD.org>
2004-04-18 15:40:54 +0000

    - Update to 2.3.1 [1] - Use gcc33 on 4.x because it provokes Internal compiler error with 2.95 [2]
    PR:		ports/65014 [1], ports/65688 [2]
    Submitted by:	ijliao [1], Stefan Grundmann <sg-sendpr@waset.de> [2]
    Approved by:	maintainer timeout (17 days)

---
+++ devel/zthread/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Apr  1 11:53:22 2004
++++ ltmain.sh	Thu Apr  1 11:53:33 2004
+@@ -4210,10 +4210,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
