5b0ebd971a4eb1ec3c10c93e368866e79bcf9785
knu <knu@FreeBSD.org>
2004-04-19 16:42:15 +0000

    Update to 1.0.35, unbreak for 5.x and USE_RC_SUBR.

--- irc/dancer-ircd/files/patch-config.h
+++ irc/dancer-ircd/files/patch-config.h
@@ -1,11 +1,11 @@
---- include/config.h.orig	Wed Sep  3 03:11:17 2003
-+++ include/config.h	Fri Nov 14 06:16:38 2003
-@@ -36,8 +36,8 @@
+--- include/config.h.orig	Sun Feb 15 02:11:26 2004
++++ include/config.h	Tue Apr 20 00:29:14 2004
+@@ -66,8 +66,8 @@
   * system limits. If you know what you are doing, increase them now
   */
  
--#define HARD_FDLIMIT_   4096 /* was 256 */
--#define INIT_MAXCLIENTS 4000 /* was 200 */
+-#define HARD_FDLIMIT_   256 /* was 256 */
+-#define INIT_MAXCLIENTS 200 /* was 200 */
 +#define HARD_FDLIMIT_   512 /* was 256 */
 +#define INIT_MAXCLIENTS 400 /* was 200 */
  
--- irc/dancer-ircd/files/patch-paths.c
+++ irc/dancer-ircd/files/patch-paths.c
@@ -1,6 +1,6 @@
---- src/paths.c.orig	Tue Jul  8 14:41:55 2003
-+++ src/paths.c	Fri Nov 14 05:26:16 2003
-@@ -64,20 +64,21 @@
+--- src/paths.c.orig	Sat Nov  8 01:28:44 2003
++++ src/paths.c	Tue Apr 20 00:33:19 2004
+@@ -64,20 +64,20 @@
   */
  
  #define DPATH    PREFIX "/"
@@ -13,26 +13,25 @@
 -#define HLBASE   "var/log/dancer-ircd/hash"
 -#define USERLOG  "var/log/dancer-ircd/user.log"
 -#define OPERLOG  "var/log/dancer-ircd/oper.log"
--#define PPATH    "var/run/dancer-ircd.pid"
+-#define PPATH    "var/run/dancer-ircd/dancer-ircd.pid"
 -#define KPATH    "var/lib/dancer-ircd/kline.conf"
 -#define DLPATH   "var/lib/dancer-ircd/dline.conf"
 -#define MXPATH   "var/lib/dancer-ircd/ircd.max"
 -#define DUMPPATH "var/lib/dancer-ircd/dump"
-+
-+#define SPATH    "@@prefix@@/bin/dancer-ircd"
-+#define CPATH    "@@prefix@@/etc/dancer-ircd/ircd.conf"
-+#define MPATH    "@@prefix@@/etc/dancer-ircd/motd"
-+#define HPATH    "@@prefix@@/etc/dancer-ircd/ohelp"
-+#define OPATH    "@@prefix@@/etc/dancer-ircd/omotd"
++#define SPATH    "/usr/local/sbin/dancer-ircd"
++#define CPATH    "/usr/local/etc/dancer-ircd/ircd.conf"
++#define MPATH    "/usr/local/etc/dancer-ircd/motd"
++#define HPATH    "/usr/local/etc/dancer-ircd/ohelp"
++#define OPATH    "/usr/local/etc/dancer-ircd/omotd"
 +#define LPATH    "/var/log/dancer-ircd/ircd.log"
 +#define HLBASE   "/var/log/dancer-ircd/hash"
 +#define USERLOG  "/var/log/dancer-ircd/user.log"
 +#define OPERLOG  "/var/log/dancer-ircd/oper.log"
 +#define PPATH    "/var/run/dancer-ircd/dancer-ircd.pid"
-+#define KPATH    "@@prefix@@/etc/dancer-ircd/kline.conf"
-+#define DLPATH   "@@prefix@@/etc/dancer-ircd/dline.conf"
-+#define MXPATH   "@@prefix@@/etc/dancer-ircd/ircd.max"
-+#define DUMPPATH "@@prefix@@/etc/dancer-ircd/dump"
++#define KPATH    "/usr/local/etc/dancer-ircd/kline.conf"
++#define DLPATH   "/usr/local/etc/dancer-ircd/dline.conf"
++#define MXPATH   "/usr/local/etc/dancer-ircd/ircd.max"
++#define DUMPPATH "/usr/local/etc/dancer-ircd/dump"
  
  const char *work_dir = DPATH;
  const char *config_file = CPATH;
