68bc0c42f70cccb740e1438c8a553fd70d4735d6
marcus <marcus@FreeBSD.org>
2004-04-20 06:22:06 +0000

    Update to 2.6.1.

--- archivers/file-roller/files/patch-src_fr-command-tar.c
+++ archivers/file-roller/files/patch-src_fr-command-tar.c
@@ -1,7 +1,7 @@
---- src/fr-command-tar.c.orig	Mon Mar  8 18:20:07 2004
-+++ src/fr-command-tar.c	Sun Mar 14 00:14:41 2004
-@@ -196,6 +196,11 @@
- 	return g_strndup (f_start + 1, f_end - f_start);
+--- src/fr-command-tar.c.orig	Mon Apr 19 15:10:23 2004
++++ src/fr-command-tar.c	Tue Apr 20 02:15:36 2004
+@@ -226,6 +226,11 @@
+ 	return g_strdup (f_start);
  }
  
 +static char *months[] = {
@@ -12,7 +12,7 @@
  
  static void
  process_line (char     *line, 
-@@ -204,28 +209,36 @@
+@@ -234,28 +239,37 @@
  	FileData    *fdata;
  	FRCommand   *comm = FR_COMMAND (data);
  	char       **fields;
@@ -29,6 +29,11 @@
  
 -	date_idx = get_index_from_pattern (line, "%n%n%n%n-%n%n-%n%n %n%n:%n%n");
 +	fields = split_line (line, 7);
++
++	fdata->size = atol (fields[2]);
++	for (i = 0; i < 12; i++) {
++		if (!strncmp(fields[3], months[i], 3)) break;
++	}
  
 -	field_size = get_prev_field (line, date_idx, 1);
 -	fdata->size = atol (field_size);
@@ -39,10 +44,6 @@
 -	fdata->modified = mktime_from_string (field_date, field_time);
 -	g_free (field_date);
 -	g_free (field_time);
-+	fdata->size = atol (fields[2]);
-+	for (i = 0; i < 12; i++) {
-+		if (!strncmp(fields[3], months[i], 3)) break;
-+	}
 +	tm.tm_mon = i;
 +	tm.tm_year = atoi (fields[6]) - 1900;
 +	tm.tm_mday = atoi (fields[4]);
@@ -57,12 +58,12 @@
  
  	/* Full path */
  
--	field_name = get_next_field (line, date_idx, 3);
+-	field_name = tar_get_last_field (line, date_idx, 3);
 +	field_name = get_last_field (line, 8);
  	fields = g_strsplit (field_name, " -> ", 2);
  
  	if (fields[1] == NULL) {
-@@ -244,7 +257,6 @@
+@@ -274,7 +288,6 @@
  	if (fields[1] != NULL)
  		fdata->link = g_strdup (fields[1]);
  	g_strfreev (fields);
