788035ae511f8ebccfc6dd53249a9ee5345e8b0f
glewis <glewis@FreeBSD.org>
2004-04-20 14:24:28 +0000

    . Sync with the nethack32 port:   . By default, install things in a location that won't conflict with the     other nethack ports.   . Build and install the recover utility so that users can recover their     game if it crashes (bump PORTREVISION for this). . Sync with the nethack34 port:   . Use PKGNAMESUFFIX to select the graphics type.   . Remove the COMMENT in the slave ports. . Take maintainership of nethack33 and nethack33-nox11. . Unbreak the nethack33-qt port.

---
+++ games/nethack33/files/patch-system.h
@@ -0,0 +1,26 @@
+$FreeBSD$
+
+--- include/system.h.orig	Mon Apr 19 23:27:53 2004
++++ include/system.h	Mon Apr 19 23:31:54 2004
+@@ -86,7 +86,7 @@
+ # endif
+ #else
+ E long lrand48();
+-E void srand48();
++E void FDECL(srand48, (long));
+ #endif /* BSD || ULTRIX || RANDOM */
+ 
+ #if !defined(BSD) || defined(ultrix)
+@@ -340,10 +340,10 @@
+ #if defined(BSD) && defined(ultrix)	/* i.e., old versions of Ultrix */
+ E void sleep();
+ #endif
+-#if defined(ULTRIX) || defined(SYSV)
++#if !defined(__FreeBSD__) && (defined(ULTRIX) || defined(SYSV))
+ E unsigned sleep();
+ #endif
+-#if defined(HPUX)
++#if defined(HPUX) || defined(__FreeBSD__)
+ E unsigned int FDECL(sleep, (unsigned int));
+ #endif
+ #ifdef VMS
