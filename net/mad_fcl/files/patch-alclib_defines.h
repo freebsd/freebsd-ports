31a5ceb87d88cf14f55e6fce3f085b665f16a931
krion <krion@FreeBSD.org>
2004-04-30 08:21:26 +0000

    - Update to version 0.8
    PR:		ports/66050
    Submitted by:	maintainer

---
+++ net/mad_fcl/files/patch-alclib_defines.h
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- alclib/defines.h.orig	Wed Apr 28 16:48:02 2004
++++ alclib/defines.h	Wed Apr 28 16:48:28 2004
+@@ -32,7 +32,7 @@
+ /*#define SSM*/
+ 
+ /* Use ZLIB comprsession library */
+-#define USE_ZLIB
++/*#define USE_ZLIB */
+ 
+ /* Use  OpenSSL library for MD5*/
+ #define USE_OPENSSL
