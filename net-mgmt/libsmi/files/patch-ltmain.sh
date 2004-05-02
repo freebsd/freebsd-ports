34381596027224f27b5239b93be7bf553d7b0f99
Pav Lucistnik <pav@FreeBSD.org>
2004-05-02 15:52:22 +0000

    - Update to 0.4.2
    PR:		ports/65605
    Submitted by:	ijliao
    Approved by:	maintainer timeout (2 weeks)

---
+++ net-mgmt/libsmi/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Apr 16 14:11:18 2004
++++ ltmain.sh	Fri Apr 16 14:11:29 2004
+@@ -4247,10 +4247,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
