6d9db9d43d55f818a4e3c86a18d9efd0d480b2ac
lofi <lofi@FreeBSD.org>
2004-05-03 19:19:53 +0000

    security/newpg -> security/gnupg-devel

--- security/gnupg-devel/files/patch-kbx_keybox-dump.c
+++ security/gnupg-devel/files/patch-kbx_keybox-dump.c
@@ -1,5 +1,5 @@
---- kbx/keybox-dump.c.orig	Mon Jul 22 10:33:48 2002
-+++ kbx/keybox-dump.c	Wed Jan 29 17:00:08 2003
+--- kbx/keybox-dump.c.orig	Mon May  3 18:38:00 2004
++++ kbx/keybox-dump.c	Mon May  3 18:38:21 2004
 @@ -24,6 +24,7 @@
  #include <string.h>
  #include <errno.h>
