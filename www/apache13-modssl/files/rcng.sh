8970412bd0de28534ce36f4022fcc224bb964d19
Dirk Meyer <dinoex@FreeBSD.org>
2004-05-03 04:27:23 +0000

    - rcNG-fy Submitted by:	nork
    WARNING installing the package will overwrite your "etc/rc.d/apache.sh"
    installing from port is safe
    
    - new option WITHOUT_RC_SUBR

---
+++ www/apache13-modssl/files/rcng.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# $FreeBSD$
+
+# PROVIDE: apache
+# REQUIRE: DAEMON
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
+
+# Define these apache_* variables in one of these files:
+#       /etc/rc.conf
+#       /etc/rc.conf.local
+#       /etc/rc.conf.d/apache
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE
+#
+apache_enable="NO"
+apache_flags="-DSSL"
+apache_pidfile="/var/run/httpd.pid"
+
+. %%RC_SUBR%%
+
+name="apache"
+rcvar=`set_rcvar`
+command="%%PREFIX%%/sbin/httpd"
+
+load_rc_config $name
+
+pidfile="${apache_pidfile}"
+
+run_rc_commmand "$1"
