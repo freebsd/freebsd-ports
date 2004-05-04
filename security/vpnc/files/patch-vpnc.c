183ab1ee113941574a7d018595a9804a51fc3349
Markus Brueffer <markus@FreeBSD.org>
2004-05-04 20:14:23 +0000

    Update to vpnc-0.2-rm+zomb-pre9
    PR:		ports/66202
    Submitted by:	Christian Lackas <delta@lackas.net> (maintainer)

--- security/vpnc/files/patch-vpnc.c
+++ security/vpnc/files/patch-vpnc.c
@@ -1,6 +1,28 @@
---- vpnc.c.orig	Sun Apr 25 02:46:23 2004
-+++ vpnc.c	Sun Apr 25 02:46:50 2004
-@@ -2036,8 +2036,8 @@
+--- vpnc.c.dist	Mon May  3 14:13:05 2004
++++ vpnc.c	Mon May  3 14:13:41 2004
+@@ -1527,10 +1527,10 @@
+ 	  reject = ISAKMP_N_ATTRIBUTES_NOT_SUPPORTED;
+ 	  break;
+ 	}
+-	strbuf = xallocc(a->u.lots.length)+1;
++	strbuf = xallocc(a->u.lots.length+1);
+ 	memcpy(strbuf, a->u.lots.data, a->u.lots.length);
+ 	addenv("CISCO_DEF_DOMAIN", strbuf);
+-	/*free(strbuf); free(): invalid pointer 0x80593f9! FIXME */
++	free(strbuf);
+ 	break;
+ 	
+       case ISAKMP_MODECFG_ATTRIB_CISCO_BANNER:
+@@ -1538,7 +1538,7 @@
+ 	  reject = ISAKMP_N_ATTRIBUTES_NOT_SUPPORTED;
+ 	  break;
+ 	}
+-	strbuf = xallocc(a->u.lots.length)+1;
++	strbuf = xallocc(a->u.lots.length+1);
+ 	memcpy(strbuf, a->u.lots.data, a->u.lots.length);
+ 	addenv("CISCO_BANNER", strbuf);
+ 	free(strbuf);
+@@ -2162,8 +2162,8 @@
      else
        read_config_file (argv[i], config, 0);
  
