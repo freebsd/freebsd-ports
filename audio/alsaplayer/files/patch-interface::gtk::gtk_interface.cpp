66b32c848df11e71f5d0b884c03bc23138cb3d94
pav <pav@FreeBSD.org>
2004-05-04 19:52:38 +0000

    - Fix build of gtk12 extension on gcc 3.3
    PR:		ports/66190
    Reported by:	User1001 <supraexpress@globaleyes.net>

---
+++ audio/alsaplayer/files/patch-interface::gtk::gtk_interface.cpp
@@ -0,0 +1,10 @@
+--- interface/gtk/gtk_interface.cpp.orig	Tue May  4 21:33:21 2004
++++ interface/gtk/gtk_interface.cpp	Tue May  4 21:43:54 2004
+@@ -16,6 +16,7 @@
+  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
+ 
++#include "assert.h"
+ #include "AlsaPlayer.h"
+ #include "config.h"
+ #include "prefs.h"
