2841ba43296bad7566864be53795fee434c33e84
Pav Lucistnik <pav@FreeBSD.org>
2004-05-04 19:52:38 +0000

    - Fix build of gtk12 extension on gcc 3.3
    PR:		ports/66190
    Reported by:	User1001 <supraexpress@globaleyes.net>

---
+++ audio/alsaplayer/files/patch-interface::gtk::PlaylistWindow.cpp
@@ -0,0 +1,10 @@
+--- interface/gtk/PlaylistWindow.cpp.orig	Tue May  4 21:33:21 2004
++++ interface/gtk/PlaylistWindow.cpp	Tue May  4 21:41:10 2004
+@@ -16,6 +16,7 @@
+  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */ 
+ 
++#include <assert.h>
+ #include <cstdio>
+ #include <cstdlib>
+ #include <cstring>
