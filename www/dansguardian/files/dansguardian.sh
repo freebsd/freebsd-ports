7c77a0b3c034aab072c418105a02f2a04e153cc5
pav <pav@FreeBSD.org>
2004-05-06 20:09:41 +0000

    - Update to 2.6.1-13 - Convert rc script to RCng
    PR:		ports/66333
    Submitted by:	Freddie Cash <fcash@sd73.bc.ca> (maintainer)

---
+++ www/dansguardian/files/dansguardian.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# $FreeBSD$
+
+# PROVIDE: dansguardian
+# REQUIRE: DAEMON squid
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
+
+# Define these dansguardian_* variables in one of these files:
+#       /etc/rc.conf
+#       /etc/rc.conf.local
+#       /etc/rc.conf.d/dansguardian
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE
+#
+dansguardian_enable="NO"
+dansguardian_flags=""
+dansguardian_pidfile="/var/run/dansguardian.pid"
+
+. %%RC_SUBR%%
+
+name="dansguardian"
+rcvar=`set_rcvar`
+command="%%PREFIX%%/sbin/dansguardian"
+
+load_rc_config $name
+
+pidfile="${dansguardian_pidfile}"
+
+run_rc_command "$1"
