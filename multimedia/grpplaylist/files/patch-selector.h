6b16d3a03829738407eb64033e44616e85713232
markus <markus@FreeBSD.org>
2004-05-07 12:06:06 +0000

    Add grpplaylist 0.1, a Noatun playlist plugin. It is a playlist inspired by ZInf, in which you can organize your multimedia files in groups based on the files meta information (Artist, Album, etc).

---
+++ multimedia/grpplaylist/files/patch-selector.h
@@ -0,0 +1,10 @@
+--- src/selector.h.orig	Fri May  7 13:31:17 2004
++++ src/selector.h	Fri May  7 13:36:18 2004
+@@ -161,6 +161,7 @@
+ friend class GrpListItemBase;
+ friend class GrpListItemFile;
+ friend class GrpListItemGroup;
++friend class GroupedSelector;
+ 
+ 	Q_OBJECT
+ 
