0f760ac9cebe6b722439f4006a79e3475895d8c1
anholt <anholt@FreeBSD.org>
2004-05-10 05:05:00 +0000

    New port: xorg-server taken from X.org release 6.7.0.
    Tested only on x86 so far due to problems installing dependencies on build
    cluster.  Based on XFree86-4-Server originally, but I decided the history
    wasn't worth keeping that much as a lot has been changed.

---
+++ x11-servers/xorg-server/files/patch-xkbout.c
@@ -0,0 +1,10 @@
+--- lib/xkbfile/xkbout.c.orig		Fri May  7 18:05:29 2004
++++ lib/xkbfile/xkbout.c		Fri May  7 18:05:29 2004
+@@ -54,7 +54,6 @@
+ #include "XKBsrv.h"
+ 
+ #include <X11/extensions/XKBgeom.h>
+-#include "extensions/XKBfile.h"
+ 
+ #endif
+ 
