b29c861b4d6162e0036419ab75e49458c90ce8b5
Eric Anholt <anholt@FreeBSD.org>
2004-05-10 05:05:00 +0000

    New port: xorg-server taken from X.org release 6.7.0.
    Tested only on x86 so far due to problems installing dependencies on build
    cluster.  Based on XFree86-4-Server originally, but I decided the history
    wasn't worth keeping that much as a lot has been changed.

---
+++ x11-servers/xorg-server/files/patch-xf86sym.c
@@ -0,0 +1,10 @@
+--- programs/Xserver/hw/xfree86/loader/xf86sym.c.orig	Fri May  7 18:51:51 2004
++++ programs/Xserver/hw/xfree86/loader/xf86sym.c	Fri May  7 18:52:02 2004
+@@ -63,7 +63,6 @@
+ #include "xf86.h"
+ #include "xf86Resources.h"
+ #include "xf86_OSproc.h"
+-#include "xf86Parser.h"
+ #include "xf86Config.h"
+ #ifdef XINPUT
+ # include "xf86Xinput.h"
