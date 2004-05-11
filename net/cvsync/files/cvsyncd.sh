a48cbfdd5b089ed30a317d2c42f85ebd6f463f7d
hrs <hrs@FreeBSD.org>
2004-05-11 12:13:26 +0000

    Add USE_RC_SUBR=yes and rcNG-style startup script.
    Approved by:	linimon (mentor)

---
+++ net/cvsync/files/cvsyncd.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+# PROVIDE: cvsyncd
+# REQUIRE: LOGIN
+# KEYWORD: FreeBSD
+#
+cvsyncd_enable="NO"
+cvsyncd_pidfile="/var/run/cvsyncd.pid"
+cvsyncd_conffile="%%PREFIX%%/etc/cvsyncd.conf"
+cvsyncd_flags=""
+
+. %%RC_SUBR%%
+
+name=cvsyncd
+rcvar=`set_rcvar`
+command=%%PREFIX%%/bin/${name}
+
+load_rc_config ${name}
+
+case "${cvsyncd_flags}" in
+*-p\ *)
+	echo "Warning: \$cvsyncd_flags includes -p option." \
+		"Please use \$cvsyncd_pidfile instead."
+	;;
+*)
+	cvsyncd_flags="-p ${cvsyncd_pidfile} ${cvsyncd_flags}" ;;
+esac
+
+case "${cvsyncd_flags}" in
+*-c\ *)
+	echo "Warning: \$cvsyncd_flags includes -c option." \
+		"Please use \$cvsyncd_conffile instead."
+	;;
+*)
+	cvsyncd_flags="-c ${cvsyncd_conffile} ${cvsyncd_flags}" ;;
+esac
+
+pidfile=${cvsyncd_pidfile}
+required_files=${cvsyncd_conffile}
+
+run_rc_command "$1"
+
