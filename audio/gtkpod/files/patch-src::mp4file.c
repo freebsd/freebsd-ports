0956ac21cc6eea59a73e59ae75930701304677bf
Pav Lucistnik <pav@FreeBSD.org>
2004-05-12 08:43:58 +0000

    - Add AAC support, optionally either with faad or mpeg4ip
    PR:		ports/66356
    Submitted by:	ume
    Approved by:	maintainer

--- audio/gtkpod/files/patch-src::mp4file.c
+++ audio/gtkpod/files/patch-src::mp4file.c
@@ -1,6 +1,19 @@
---- src/mp4file.c.old	Sat Jan 17 10:37:49 2004
-+++ src/mp4file.c	Tue Jan 20 21:33:20 2004
-@@ -342,7 +342,7 @@
+Index: src/mp4file.c
+diff -u -p src/mp4file.c.orig src/mp4file.c
+--- src/mp4file.c.orig	Sat Jan 17 18:37:49 2004
++++ src/mp4file.c	Fri May  7 15:20:24 2004
+@@ -109,6 +109,10 @@
+ 
+ #include "mp4.h"
+ 
++#ifndef UINT64_TO_DOUBLE
++#define UINT64_TO_DOUBLE(a) ((double)((int64_t)(a)))
++#endif
++
+ Track *file_get_mp4_info (gchar *mp4FileName)
+ {
+     Track *track = NULL;
+@@ -342,7 +346,7 @@ Track *file_get_mp4_info (gchar *name)
      return NULL;
  }
  
---
+++ audio/gtkpod/files/systems.h
@@ -0,0 +1,7 @@
+/* This file is included from faad2 version of mepg4ip.h. */
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HAVE_RINT
