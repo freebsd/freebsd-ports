cb0623abda371f47f50999a94a4a4e3baddc7f01
ume <ume@FreeBSD.org>
2004-05-15 17:37:42 +0000

    Update to 2.2.0. This version uses Pango to fix font problem with GTK+-2.4.0.

--- sysutils/gkrellm2/files/patch-server::gkrellmd.h
+++ sysutils/gkrellm2/files/patch-server::gkrellmd.h
@@ -1,14 +1,17 @@
 Index: server/gkrellmd.h
 diff -u server/gkrellmd.h.orig server/gkrellmd.h
---- server/gkrellmd.h.orig	Tue Aug 13 01:58:55 2002
-+++ server/gkrellmd.h	Thu Aug 29 18:47:14 2002
-@@ -107,7 +107,9 @@
- #define GKRELLMD_CONFIG     "gkrellmd.conf"
+--- server/gkrellmd.h.orig	Mon Apr 12 23:32:29 2004
++++ server/gkrellmd.h	Tue Apr 13 17:16:02 2004
+@@ -105,8 +105,12 @@
+ #endif
  
  #if !defined(WIN32)
 +#if !defined(GKRELLMD_SYS_ETC)
  #define	GKRELLMD_SYS_ETC	"/etc"
 +#endif
++#if 0
  #define GKRELLMD_LOCAL_ETC	"/usr/local/etc"
++#endif
  #endif
+ 
  
