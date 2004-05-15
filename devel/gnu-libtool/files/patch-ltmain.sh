6f316ce2e89f972a4f22e1b97e1dda5e9d58cefa
cvs2svn <cvs2svn@FreeBSD.org>
2004-05-15 15:49:17 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_4_10_0'.

--- devel/gnu-libtool/files/patch-ltmain.sh
+++
@@ -1,7 +0,0 @@
---- ltmain.sh.orig	Wed Jul  2 16:56:39 2003
-+++ ltmain.sh	Wed Jul  2 16:58:03 2003
-@@ -1279,3 +1279,3 @@
- 	 case $host in
--	 *-*-openbsd* | *-*-freebsd*)
-+	 *-*-openbsd* | *-*-freebsd4*)
- 	   # Do not include libc_r directly, use -pthread flag.
