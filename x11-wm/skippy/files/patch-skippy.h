45a84563ae1314e13c2ae615cd9a2c6662812e33
krion <krion@FreeBSD.org>
2004-05-16 16:56:43 +0000

    Add skippy 0.4.1,
    Skippy is best described as a full-screen task-switcher for
    X11. It tries to provide an alternative when taskbars or
    regular task-switchers aren't the most efficient way of
    switching tasks (like when you have a lot of applications open)
    When activated (currently only through a hotkey), it will
    arrange and scale snapshots of all windows on the current
    desktop and it'll let you pick a window using a mouse or a
    keyboard. Yes, this is also what expocity and Apple's Expose.
    
    PR:		ports/66681
    Submitted by:	michael johnson <ahze at ahze dot net>

---
+++ x11-wm/skippy/files/patch-skippy.h
@@ -0,0 +1,10 @@
+--- skippy.h.orig	Sat May 15 19:33:08 2004
++++ skippy.h	Sat May 15 19:33:44 2004
+@@ -20,6 +20,7 @@
+ #ifndef SKIPPY_H
+ #define SKIPPY_H
+ 
++#include <ft2build.h>
+ #include <X11/Xlib.h>
+ #include <X11/Xmd.h>
+ #include <X11/Xatom.h>
