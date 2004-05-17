0b2f5691307bc8288b481f4d16e11abc3965c312
Thierry Thomas <thierry@FreeBSD.org>
2004-05-17 05:29:19 +0000

    Unbreak on -CURRENT.
    Reported by:	Kris.

--- sysutils/xvidcap/files/patch-src::capture.c
+++ sysutils/xvidcap/files/patch-src::capture.c
@@ -1,5 +1,5 @@
 --- src/capture.c.orig	Sat Feb 14 21:14:20 2004
-+++ src/capture.c	Sun Apr 25 01:21:53 2004
++++ src/capture.c	Sat May 15 23:01:57 2004
 @@ -643,7 +643,11 @@
              (*job->close) (fp);
          else if (job->flags & FLG_SYNC) {
@@ -7,7 +7,7 @@
 +#ifdef HAVE_FDATASYNC
                  fdatasync(fileno(fp));
 +#else
-+                fsync(fileno(fp));
++                fsync(fileno((FILE *) fp));
 +#endif
          }
          
