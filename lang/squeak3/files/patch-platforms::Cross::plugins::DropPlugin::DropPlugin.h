cf5af8e2172eb46923d1cf296df896e54b8aa663
vs <vs@FreeBSD.org>
2004-05-17 06:58:12 +0000

    - Update to 3.6-3 - Change MAINTAINER to submitter
    PR:		ports/66618
    Submitted by:	Chris Burkert

---
+++ lang/squeak3/files/patch-platforms::Cross::plugins::DropPlugin::DropPlugin.h
@@ -0,0 +1,7 @@
+--- platforms/Cross/plugins/DropPlugin/DropPlugin.h.orig	Thu May 13 16:07:09 2004
++++ platforms/Cross/plugins/DropPlugin/DropPlugin.h	Thu May 13 16:05:52 2004
+@@ -10,3 +10,4 @@
+ int sqSecFileAccessCallback(void *);
+ void sqSetNumberOfDropFiles(int numberOfFiles);
+ void sqSetFileInformation(int dropIndex, void *dropFile);
++
