0eda2536298774fdd4c670a2385657f23c761138
Thierry Thomas <thierry@FreeBSD.org>
2004-05-19 21:31:52 +0000

    Upgrade to 0.93a.
    Warning: the installation directories have changed.

--- lang/gambas/files/patch-src::lib::db::sqlite::sqlitedataset.cpp
+++ lang/gambas/files/patch-src::lib::db::sqlite::sqlitedataset.cpp
@@ -1,5 +1,5 @@
---- ./src/lib/db/sqlite/sqlitedataset.cpp.orig	Fri Feb 27 22:53:21 2004
-+++ ./src/lib/db/sqlite/sqlitedataset.cpp	Thu Mar 25 10:33:48 2004
+--- src/lib/db/sqlite/sqlitedataset.cpp.orig	Fri Feb 27 22:53:21 2004
++++ src/lib/db/sqlite/sqlitedataset.cpp	Sun May 16 14:58:59 2004
 @@ -33,6 +33,9 @@
  #include <time.h>
  #include <iostream>
@@ -10,3 +10,15 @@
  
  #include "sqlitedataset.h"
  #include <unistd.h>
+@@ -148,7 +151,11 @@
+ 			    break; 
+ 		       case ft_Float:
+ 	 		    v.set_asString(reslt[i]);
++#if defined(__FreeBSD__) && !defined(_GLIBCPP_HAVE_STRTOF)
++	 		    v.set_asFloat((float)strtod(reslt[i], NULL));
++#else
+ 	 		    v.set_asFloat(strtof(reslt[i], NULL));
++#endif
+ 			    break; 
+ 		       case ft_Double:
+ 	 		    v.set_asString(reslt[i]);
