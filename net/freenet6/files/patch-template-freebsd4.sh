4cf5a993fdc2f8827aca8f30a5d09e6a15c93d8b
edwin <edwin@FreeBSD.org>
2004-05-22 02:14:02 +0000

    From submitter:     I've noticed that on some 'unsymmetric' locales/charsets, the     lower/uppercase conversion in the tspc-scripts fails on FreeBSD     5.x (caused by tr being changed to behave posixly correct). The     fix is easy, see attached patch.
    Submitted by:	Michael Nottebrock <michaelnottebrock@gmx.net>

--- net/freenet6/files/patch-template-freebsd4.sh
+++ net/freenet6/files/patch-template-freebsd4.sh
@@ -1,8 +1,17 @@
 --- template/freebsd4.sh.orig	Thu Oct 11 16:40:29 2001
 +++ template/freebsd4.sh	Thu Oct 11 16:40:48 2001
-@@ -110,6 +110,9 @@
+@@ -92,7 +92,7 @@
+ fi
+ 
+ #change to upper case
+-TSP_HOST_TYPE=`echo $TSP_HOST_TYPE | tr a-z A-Z`
++TSP_HOST_TYPE=`echo $TSP_HOST_TYPE | tr [:lower:] [:upper:]`
+
+ if [ X"${TSP_HOST_TYPE}" = X"HOST" ] || [ X"${TSP_HOST_TYPE}" = X"ROUTER" ]; then
+    #
+@@ -122,6 +122,9 @@
     # Delete any default IPv6 route first
-    Exec $route delete -inet6 default
+    ExecNoCheck $route delete -inet6 default
     Exec $route add -inet6 default  -interface $TSP_TUNNEL_INTERFACE
 +
 +   # Save interface for later
