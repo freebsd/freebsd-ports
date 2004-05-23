d11aea420e2f9f2c4abf648d6afd57d99829b746
krion <krion@FreeBSD.org>
2004-05-23 19:06:19 +0000

    - Update to version 0.4.1
    PR:		ports/67081
    Submitted by:	Ports Fury

---
+++ devel/eboxy/files/patch-eboxy::basewidgets::stringlist.cpp
@@ -0,0 +1,10 @@
+--- eboxy/basewidgets/stringlist.cpp.orig	Thu May 20 16:46:55 2004
++++ eboxy/basewidgets/stringlist.cpp	Thu May 20 20:56:53 2004
+@@ -18,6 +18,7 @@
+ #include <cstring>
+ #include <iostream>
+ #include <vector>
++#include <string>
+ 
+ #include "stringlist.h"
+ 
