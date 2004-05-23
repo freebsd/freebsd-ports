206b294339cb6961c5e8156f564eff4503905b04
Pav Lucistnik <pav@FreeBSD.org>
2004-05-23 11:40:52 +0000

    Add xtrlock, a minimal transparent X lock. Transparent means that all windows are still visible and updated on the locked session.
    PR:		ports/66964
    Submitted by:	Alexey Y. Mikhailov <karma@ez.pereslavl.ru>

---
+++ x11/xtrlock/files/patch-xtrlock.c
@@ -0,0 +1,16 @@
+*** xtrlock.c.orig	Sun Sep  1 08:46:29 2002
+--- xtrlock.c	Tue May 18 17:45:48 2004
+***************
+*** 31,41 ****
+  #include <grp.h>
+  #include <limits.h>
+  #include <string.h>
+- #include <crypt.h>
+  #include <unistd.h>
+  #include <math.h>
+  #include <ctype.h>
+- #include <values.h>
+  
+  #ifdef SHADOW_PWD
+  #include <shadow.h>
+--- 31,39 ----
