43b97c030dbecaba74c5777845ca2f1b51d57d4e
Pav Lucistnik <pav@FreeBSD.org>
2004-05-24 23:02:23 +0000

    - Update to 20040524 - Rename rc script freshclam.sh -> clamav-freshclam.sh for consistency   BEWARE if you're using freshclam daemon you must update your rc.conf! - Chmod 770 socket directory - Diff reduction against security/clamav port
    PR:		ports/67125
    Submitted by:	Rob Evers <rob@debank.tv> (maintainer)

--- security/clamav-devel/files/clamav-freshclam.sh
+++ security/clamav-devel/files/clamav-freshclam.sh
@@ -11,25 +11,27 @@
 #
 # Add the following lines to /etc/rc.conf to enable freshclam daemon:
 #
-#freshclam_enable"YES"
+#clamav_freshclam_enable="YES"
 #
 # See freshclam(1) for flags
 #
 
 . %%RC_SUBR%%
 
-name=freshclam
+name=clamav_freshclam
 rcvar=`set_rcvar`
 
 command=%%PREFIX%%/bin/freshclam
 required_dirs=%%DATADIR%%
 required_files=%%PREFIX%%/etc/freshclam.conf
 
-
 # set defaults
 
-freshclam_enable=${freshclam_enable:-"NO"}
-freshclam_flags=${freshclam_flags:-"--daemon"}
+clamav_freshclam_enable=${clamav_freshclam_enable:-"NO"}
+clamav_freshclam_flags=${clamav_freshclam_flags:-"--daemon-notify=%%PREFIX%%/etc/clamav.conf"}
 
 load_rc_config $name
+
+# add --daemon to any given arguments
+clamav_freshclam_flags="${clamav_freshclam_flags} --daemon"
 run_rc_command "$1"
