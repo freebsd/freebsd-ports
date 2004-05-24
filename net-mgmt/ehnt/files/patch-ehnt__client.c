2bcc01a77cd2bb9960ea172a5542ebf1e534e424
Kirill Ponomarev <krion@FreeBSD.org>
2004-05-24 09:06:37 +0000

    - turn off interactivity for pipe-oriented mode (colondump) - convert ASN table from static to alloc()ed to reduce memory   footprint - bump PORTREVISION
    PR:		ports/67117
    Submitted by:	maintainer

--- net-mgmt/ehnt/files/patch-ehnt__client.c
+++ net-mgmt/ehnt/files/patch-ehnt__client.c
@@ -2,8 +2,28 @@
 $FreeBSD$
 
 --- ehnt_client.c.orig	Thu Oct  4 22:18:29 2001
-+++ ehnt_client.c	Thu Nov 14 22:12:07 2002
-@@ -89,7 +89,7 @@
++++ ehnt_client.c	Mon May 24 11:59:17 2004
+@@ -69,7 +69,8 @@
+    printf("Warning: can't catch interrupt signal\n");
+  }
+  fflush(stdout);
+- set_term(SET_TERM_RAW);
++ if (e_cfg->mode != EM_COLONDUMP)
++   set_term(SET_TERM_RAW);
+  setbuf(stdout,NULL);
+ 
+  for ( ; ; ) {
+@@ -78,7 +79,8 @@
+    int maxfd=sockfd;
+    
+    FD_ZERO(&read_fds);
+-   FD_SET(STDIN_FILENO,&read_fds);
++   if (e_cfg->mode != EM_COLONDUMP)
++     FD_SET(STDIN_FILENO,&read_fds);
+    FD_SET(sockfd,&read_fds);
+ 
+    fflush(stdout);
+@@ -89,7 +91,7 @@
     if (FD_ISSET(sockfd,&read_fds)) {
       ehnt_client_processmessage(sockfd,routeraddr,my_ehnt_struct);
     }
