fbd0fe5998ba81ce1d5201f08664606dfe7c2f04
Pav Lucistnik <pav@FreeBSD.org>
2004-05-24 22:45:37 +0000

    - Drop unneeded dependencies on autotools - Detach from controlling terminal - Fix PID handling in rc script
    PR:		ports/66145, ports/66146, ports/66147
    Submitted by:	Helge Oldach <spamass-milter-may04@oldach.net>
    Approved by:	maintainer timeout (19 days)

--- mail/spamass-milter/files/patch-spamass-milter.cpp
+++ mail/spamass-milter/files/patch-spamass-milter.cpp
@@ -1,5 +1,5 @@
---- spamass-milter.cpp.orig	Thu Jun 26 19:10:44 2003
-+++ spamass-milter.cpp	Wed Oct 29 13:43:29 2003
+--- spamass-milter.cpp.orig	Thu Jun 26 17:10:44 2003
++++ spamass-milter.cpp	Tue May 25 00:29:58 2004
 @@ -102,6 +102,10 @@
  #include <csignal>
  #include <string>
@@ -11,7 +11,15 @@
  
  #ifdef  __cplusplus
  extern "C" {
-@@ -350,6 +354,9 @@
+@@ -273,6 +277,7 @@
+ 				exit(errno);
+ 				break;
+ 			case 0: /* Child */
++				setsid();
+ 				break;
+ 			default: /* Parent */
+ 				exit(0);
+@@ -350,6 +355,9 @@
    string::size_type eoh2(assassin->d().find("\n\r\n"));
    string::size_type eoh = ( eoh1 < eoh2 ? eoh1 : eoh2 );
    string::size_type bob = assassin->d().find_first_not_of("\r\n", eoh);
@@ -21,4 +29,3 @@
  
    update_or_insert(assassin, ctx, assassin->spam_flag(), &SpamAssassin::set_spam_flag, "X-Spam-Flag");
    update_or_insert(assassin, ctx, assassin->spam_status(), &SpamAssassin::set_spam_status, "X-Spam-Status");
-
