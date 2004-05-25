d295ad1c7ea5051e9389b2a50f10e90f1007c4bd
Pav Lucistnik <pav@FreeBSD.org>
2004-05-25 00:21:33 +0000

    - Update to .06
    PR:		ports/66787
    Submitted by:	Aaron Myles Landwehr <assroot@snaphat.com> (maintainer)

---
+++ sysutils/usrinfo/files/patch-usrinfo.cpp
@@ -0,0 +1,9 @@
+--- usrinfo.cpp.orig	Tue May 18 04:50:46 2004
++++ usrinfo.cpp	Tue May 25 02:18:09 2004
+@@ -147,4 +147,5 @@
+ 	usrtest(argv);
+ 	preftest(argv);
+ 	infodisplay(argv);
+-	return 0;//end}
++	return 0;//end
++}
