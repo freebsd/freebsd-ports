a51cdd3ebe4bddda5405f0b61ee36bb1a0e4223e
Oliver Eikemeier <eik@FreeBSD.org>
2004-05-27 11:29:07 +0000

    Update net/openldap22-* to 2.2.11 and follow the OpenLDAP soname change:   <http://www.openldap.org/lists/openldap-devel/200403/msg00101.html>
    While I'm not totally happy with their choice, I believe it is better to
    be in sync with the upstream library naming scheme.
    
    No objection from:	portmgr

--- net/openldap24-server/files/patch-build::top.mk
+++ net/openldap24-server/files/patch-build::top.mk
@@ -1,20 +1,11 @@
---- build/top.mk.orig	Wed Mar 24 22:51:22 2004
-+++ build/top.mk	Wed Mar 31 22:28:53 2004
-@@ -69,7 +69,7 @@
- LIBTOOL = @LIBTOOL@
- LIBRELEASE = @OPENLDAP_LIBRELEASE@
- LIBVERSION = @OPENLDAP_LIBVERSION@
--LTVERSION = -release $(LIBRELEASE) -version-info $(LIBVERSION)
-+LTVERSION = -version-info $(LIBVERSION)
- 
- # libtool --only flag for libraries: platform specific
- NT_LTONLY_LIB = # --only-$(BUILD_LIBS_DYNAMIC)
-@@ -191,6 +191,8 @@
- 
+--- build/top.mk.orig	Mon Apr 12 20:21:07 2004
++++ build/top.mk	Wed May  5 12:49:13 2004
+@@ -194,6 +194,8 @@
  SLAPD_LIBS = @SLAPD_LIBS@ @SLAPD_PERL_LDFLAGS@ @SLAPD_SQL_LDFLAGS@ @SLAPD_SQL_LIBS@ @SLAPD_SLP_LIBS@
  SLURPD_LIBS = @SLURPD_LIBS@
-+
-+SLAPI_LIBS = @SLAPI_LIBS@
  
++SLAPI_LIBS = @SLAPI_LIBS@
++
  # Our Defaults
  CC = $(AC_CC)
+ DEFS = $(LDAP_INCPATH) $(XINCPATH) $(XDEFS) $(AC_DEFS) $(DEFINES)
