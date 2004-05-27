8db03a458204cb2d3aa75190d5de963bf2680227
Akinori MUSHA <knu@FreeBSD.org>
2004-05-27 04:03:18 +0000

    Double MAXLAUNCHTABLE and MAXFUNCLINES.

--- shells/fd/files/patch-fd.h
+++ shells/fd/files/patch-fd.h
@@ -1,7 +1,17 @@
---- fd.h.orig	Wed Jul 10 00:00:00 2002
-+++ fd.h	Sun Aug 11 22:25:15 2002
-@@ -132,3 +132,3 @@
- #define	MAXLAUNCHTABLE	32
+--- fd.h.orig	Wed May 12 00:00:00 2004
++++ fd.h	Wed May 26 19:07:10 2004
+@@ -171,11 +171,11 @@
+  ****************************************************************/
+ #define	MAXBINDTABLE	256
+ #define	MAXMACROTABLE	64
+-#define	MAXLAUNCHTABLE	32
 -#define	MAXARCHIVETABLE	16
++#define	MAXLAUNCHTABLE	64
 +#define	MAXARCHIVETABLE	32
  #define	MAXALIASTABLE	256
+ #define	MAXFUNCTABLE	32
+-#define	MAXFUNCLINES	16
++#define	MAXFUNCLINES	32
+ #if	MSDOS
+ #define	MAXCOMMSTR	(128 - 2)
+ #endif
