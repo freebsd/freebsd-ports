c679f4759f06f3d7d52931971d58f1e01aa8b1c2
Kirill Ponomarev <krion@FreeBSD.org>
2004-05-27 08:26:39 +0000

    - Update to version .08
    PR:		ports/67243
    Submitted by:	maintainer

--- sysutils/usrinfo/files/patch-usrinfo.cpp
+++
@@ -1,9 +0,0 @@
---- usrinfo.cpp.orig	Wed May 26 04:32:17 2004
-+++ usrinfo.cpp	Wed May 26 09:17:15 2004
-@@ -157,4 +157,5 @@
- 	argtest(argc, argv);
- 	usrtest(argv);
- 	infodisplay(argv);
--	return 0;//die}
-+	return 0;//die
-+}
