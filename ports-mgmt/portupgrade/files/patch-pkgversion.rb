06db38c48a8be986f980db206e137ac8d7b2d987
knu <knu@FreeBSD.org>
2004-05-29 04:12:20 +0000

    Update to 20040529.
    overall:
      - Update Copyright years.
      - Apply massive message improvement. (I hope..)
    
    pkgtools.conf(5):
      - pkg_site_mirror(): Use pointyhat.FreeBSD.org instead of bento and
        beta.
    
    portupgrade(1):
      - Indicate some pieces of information on what is going on in the
        process title.
    
    portversion(1):
      - Make a generated script (with -c) accept additional arguments for
        portupgrade(1).

--- ports-mgmt/portupgrade/files/patch-pkgversion.rb
+++
@@ -1,9 +0,0 @@
---- lib/pkgversion.rb.orig	Mon Mar 29 07:35:38 2004
-+++ lib/pkgversion.rb	Mon Mar 29 07:36:10 2004
-@@ -62,4 +62,4 @@
-     # ignore padded 0's
--    n1.gsub!(/(^|\D)0+(\d)/, "\\1\\2")
--    n2.gsub!(/(^|\D)0+(\d)/, "\\1\\2")
-+    n1 = n1.gsub(/(^|\D)0+(\d)/, "\\1\\2")
-+    n2 = n2.gsub(/(^|\D)0+(\d)/, "\\1\\2")
- 
