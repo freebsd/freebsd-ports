462a956354d07f32b277a24c5d3fc55458218f65
thierry <thierry@FreeBSD.org>
2004-05-30 22:17:04 +0000

    Add gtk-qt-engine 0.3, a GTK-QT Theme Engine allowing GTK2 to use QT widget styles.
    PR:		63282
    Submitted by:	Danny Pansters <danny@ricin.com>

---
+++ x11-themes/gtk-qt-engine/files/patch-qt_theme_draw.c
@@ -0,0 +1,9 @@
+--- qt_theme_draw.c.orig        Mon Feb  9 19:50:12 2004
++++ qt_theme_draw.c     Tue Mar 30 02:09:08 2004
+@@ -1,5 +1,6 @@
+ #include <math.h>
+ #include <string.h>
++#include <gdk/gdk.h>
+ #include <gtk/gtkprogressbar.h>
+ #include <gtk/gtkrange.h>
+ #include <gtk/gtknotebook.h>
