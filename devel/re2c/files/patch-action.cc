9533c2887510307a633f74dc6bad9204d90ce6d9
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-06-01 09:41:36 +0000

    upgrade to 0.9.3

---
+++ devel/re2c/files/patch-action.cc
@@ -0,0 +1,10 @@
+--- actions.cc.orig	Tue Jun  1 17:31:44 2004
++++ actions.cc	Tue Jun  1 17:32:05 2004
+@@ -1,6 +1,7 @@
+ /* $Id: actions.cc,v 1.5 2004/05/13 02:58:17 nuffer Exp $ */
+ #include <time.h>
+ #include <string.h>
++#include <ctype.h>
+ #include <iostream>
+ #include <iomanip>
+ 
---
+++ devel/re2c/files/patch-substr.h
@@ -0,0 +1,10 @@
+--- substr.h.orig	Thu May 13 10:58:18 2004
++++ substr.h	Tue Jun  1 17:31:18 2004
+@@ -2,6 +2,7 @@
+ #ifndef _substr_h
+ #define _substr_h
+ 
++#include <sys/types.h>
+ #include <iostream>
+ #include "basics.h"
+ 
