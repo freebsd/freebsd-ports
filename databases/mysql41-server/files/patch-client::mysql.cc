9ae92de6d1fd20bd7aeb5a64c58bd1dee726f19d
Alex Dupre <ale@FreeBSD.org>
2004-06-02 07:24:45 +0000

    Improve the patch (this should work on 4.X, too).

--- databases/mysql41-server/files/patch-client::mysql.cc
+++ databases/mysql41-server/files/patch-client::mysql.cc
@@ -1,11 +1,11 @@
---- client/mysql.cc.orig	Mon May 31 18:16:27 2004
-+++ client/mysql.cc	Mon May 31 18:24:35 2004
+--- client/mysql.cc.orig	Wed Jun  2 09:11:24 2004
++++ client/mysql.cc	Wed Jun  2 09:13:19 2004
 @@ -294,7 +294,7 @@
   HIST_ENTRY is defined for libedit, but not for the real readline
   Need to redefine it for real readline to find it
  */
 -#if !defined(USE_LIBEDIT_INTERFACE)
-+#if defined(USE_LIBEDIT_INTERFACE)
++#if !defined(HIST_ENTRY)
  typedef struct _hist_entry {
    const char      *line;
    const char      *data;
