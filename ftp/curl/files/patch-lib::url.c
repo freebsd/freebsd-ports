64b64d5ff70dae7baae7d1c4985bfe485e310ca3
Peter Pentchev <roam@FreeBSD.org>
2004-06-03 22:41:40 +0000

    Update ftp/curl to 7.12.0 and bump the shared library version in all dependent ports.

--- ftp/curl/files/patch-lib::url.c
+++ ftp/curl/files/patch-lib::url.c
@@ -1,13 +1,13 @@
---- lib/url.c.old	Wed Nov 27 17:22:24 2002
-+++ lib/url.c	Wed Nov 27 17:23:29 2002
-@@ -288,6 +288,10 @@
-   
-   data->set.proxytype = CURLPROXY_HTTP; /* defaults to HTTP proxy */
+--- lib/url.c.orig	Wed Jun  2 14:45:18 2004
++++ lib/url.c	Wed Jun  2 14:45:40 2004
+@@ -329,6 +329,10 @@
+     data->set.httpauth = CURLAUTH_BASIC;  /* defaults to basic */
+     data->set.proxyauth = CURLAUTH_BASIC; /* defaults to basic */
  
 +#if defined(__FreeBSD_version)
-+  data->set.no_signal = TRUE; /* different handling of signals and threads */
++    data->set.no_signal = TRUE; /* different handling of signals and threads */
 +#endif /* __FreeBSD_version */
 +
-   /* create an array with connection data struct pointers */
-   data->state.numconnects = 5; /* hard-coded right now */
-   data->state.connects = (struct connectdata **)
+     /* create an array with connection data struct pointers */
+     data->state.numconnects = 5; /* hard-coded right now */
+     data->state.connects = (struct connectdata **)
