8774326ef7a8934d5549141dfe9fd9370c762448
Anders Nordby <anders@FreeBSD.org>
2004-06-03 13:07:56 +0000

    Preserve state/runtime information between (re-)starts.
    PR:		66451
    Submitted by:	TAOKA Fumiyoshi <fmysh@iijmio-mail.jp>

--- www/mod_throttle/files/patch-mod_throttle.c
+++ www/mod_throttle/files/patch-mod_throttle.c
@@ -11,3 +11,17 @@
  /***********************************************************************
   *** Pick one that best suits your system.
   ***********************************************************************/
+--- mod_throttle.c.orig Mon May 10 15:59:10 2004
++++ mod_throttle.c      Mon May 10 16:00:45 2004
+@@ -215,8 +215,8 @@
+ static const char dtype[] = "Directory";
+ static const char text_html[] = "text/html";
+ static const char text_plain[] = "text/plain";
+-static const char file_lock[] = "logs/mod_throttle.lock";
+-static const char file_runtime[] = "logs/mod_throttle.runtime";
++static const char file_lock[] = "/var/run/mod_throttle/mod_throttle.lock";
++static const char file_runtime[] = "/var/run/mod_throttle/mod_throttle.runtime";
+
+ static const char x_is_subrequest[] = "x-is-subrequest";
+ static const char request_handler[] = "request-handler";
+
