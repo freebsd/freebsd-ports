72b1f5a129d46996df886967c2f198c486a0f7e1
Anders Nordby <anders@FreeBSD.org>
2004-06-03 11:59:23 +0000

    Update to 1.1.1. DOCSDIRify.
    PR:		58635
    Submitted by:	clement

--- www/mod_auth_pam/files/patch-mod_auth_pam.c
+++ www/mod_auth_pam/files/patch-mod_auth_pam.c
@@ -1,15 +1,15 @@
---- mod_auth_pam.c.orig	Sun Feb 13 23:16:57 2000
-+++ mod_auth_pam.c	Mon May 17 11:13:59 2004
-@@ -113,7 +113,7 @@
+--- mod_auth_pam.c.orig	Thu Aug  8 13:47:33 2002
++++ mod_auth_pam.c	Thu Jun  3 11:40:47 2004
+@@ -147,7 +147,7 @@
  #define PAM_STRE_NEEDS_PAMH 1
- #define VERSION "1.0a"
+ #define VERSION "1.1.1"
  
 -module pam_auth_module;
 +module auth_pam_module;
  
  static const char
  *pam_servicename = "httpd",
-@@ -197,13 +197,11 @@
+@@ -231,13 +231,11 @@
  
  static command_rec auth_pam_cmds[] = {
    { "AuthFailDelay", (const char*(*)())auth_fail_delay, 0, OR_AUTHCFG, TAKE1,
@@ -25,7 +25,7 @@
    { "AuthPAM_Enabled", (const char*(*)())auth_enable, NULL, OR_AUTHCFG, FLAG,
      "on|off - determines if PAM authentication is enabled; default is on" },
    { 0 }
-@@ -362,7 +360,7 @@
+@@ -323,7 +321,7 @@
    /* mod_auth_pam specific */
    auth_pam_userinfo userinfo = { NULL, NULL };
    auth_pam_dir_config *conf = (auth_pam_dir_config*)
@@ -34,16 +34,16 @@
    /* PAM specific  */
    struct pam_conv conv_info = { &auth_pam_talker, (void*)&userinfo};
    pam_handle_t *pamh  = NULL;
-@@ -442,7 +440,7 @@
+@@ -402,7 +400,7 @@
+   register int i = 0;
    char method_restricted = 0, *line = 0, *word = 0;
-   table *groups = 0;
    auth_pam_dir_config *conf = (auth_pam_dir_config*)
 -    ap_get_module_config(r->per_dir_config, &pam_auth_module);
 +    ap_get_module_config(r->per_dir_config, &auth_pam_module);
+   struct passwd *pwent;
  
    /* check for allowed users/group */
-   const array_header *reqs_arr = ap_requires (r);
-@@ -505,7 +503,7 @@
+@@ -487,7 +485,7 @@
    return AUTH_REQUIRED;
  }
  
