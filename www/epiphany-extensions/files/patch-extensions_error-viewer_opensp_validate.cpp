2af841bb34b738c766bd99b3983bb9074234ad83
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-06-04 21:21:56 +0000

    Fix build with OpenSP on 4.X.
    Reported by:	J. W. Ballantine <jwb@homer.att.com>

---
+++ www/epiphany-extensions/files/patch-extensions_error-viewer_opensp_validate.cpp
@@ -0,0 +1,10 @@
+--- extensions/error-viewer/opensp/validate.cpp.orig	Fri Jun  4 17:19:33 2004
++++ extensions/error-viewer/opensp/validate.cpp	Fri Jun  4 17:19:44 2004
+@@ -35,6 +35,7 @@
+ #include <glib/gi18n-lib.h>
+ 
+ #include <iostream>
++#include <string>
+ 
+ using namespace std;
+ 
