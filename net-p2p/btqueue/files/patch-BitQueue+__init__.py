2c0f69dba8bc90f69f40ab8772a7aa17ba0e6c9b
Pav Lucistnik <pav@FreeBSD.org>
2004-06-05 12:54:40 +0000

    Add btqueue, a console-based BitTorrent client with queuing functionalities running on top of BitTornado.
    PR:		ports/67567
    Submitted by:	Stefan Walter <sw@gegenunendlich.de>

---
+++ net-p2p/btqueue/files/patch-BitQueue+__init__.py
@@ -0,0 +1,9 @@
+--- BitQueue/__init__.py.old	Fri Jun  4 12:22:41 2004
++++ BitQueue/__init__.py	Fri Jun  4 04:34:25 2004
+@@ -1,5 +1,5 @@
+ 
+-version = "Q-0.0.8 103 (BitQueue)"
++version = "Q-0.0.8 104 (BitQueue)"
+ 
+ version_short = version.split(' ')[0]
+ version_num = version.split(' ')[0].split('-')[1]
---
+++ net/btqueue/files/patch-btqueue.py
@@ -0,0 +1,11 @@
+--- btqueue.py.old	Fri Jun  4 12:23:14 2004
++++ btqueue.py	Fri Jun  4 04:36:41 2004
+@@ -6,7 +6,7 @@
+ #sys.path.insert(0,os.path.dirname(os.path.abspath(sys.argv[0])))
+ 
+ def usage():
+-    print 'abccmd.py [scheduler|crawler|query|add] [args...]'
++    print 'btqueue.py [scheduler|crawler|query|add] [args...]'
+     sys.exit()
+ 
+ if __name__ == '__main__':
