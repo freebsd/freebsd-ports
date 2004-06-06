f35ec7277cd648283a3550be810663598a153f7e
Thierry Thomas <thierry@FreeBSD.org>
2004-06-06 16:52:22 +0000

    - Update to version 3.11.r1; - Update project home page URL; - Unbreak on 5.x but keep BROKEN on 4.x; - Compile the examples; - Use EXAMPLESDIR and (NO)PORTDOCS.
    PR:		ports/67255
    Submitted by:	Stefan Walter

---
+++ x11-toolkits/py-kde/files/patch-configure.py
@@ -0,0 +1,11 @@
+--- configure.py.orig	Sat May  8 11:09:53 2004
++++ configure.py	Wed May 26 19:47:16 2004
+@@ -700,7 +700,7 @@
+             opt_concat = 1
+ 
+         elif opt == "-d":
+-            opt_pyqtmoddir = arg
++            opt_pykdemoddir = arg
+         elif opt == "-g":
+             opt_releasegil = 1
+ 
