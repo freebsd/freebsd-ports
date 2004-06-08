5de5450d9d86ca129d43b2534952b0e53de326ac
ale <ale@FreeBSD.org>
2004-06-08 13:56:25 +0000

    - Update to 5.0.0RC3 release - Remove PDFlib extension

--- lang/php5/scripts/configure.php
+++ lang/php5/scripts/configure.php
@@ -56,7 +56,6 @@ OPENSSL		"OpenSSL support" ${WITH_OPENSSL:-OFF} \
 ORACLE		"Oracle support" ${WITH_ORACLE:-OFF} \
 PCNTL		"pcntl support (CLI only)" ${WITH_PCNTL:-OFF} \
 PCRE		"Perl Compatible Regular Expression support" ${WITH_PCRE:-OFF} \
-PDFLIB		"PDFlib support" ${WITH_PDFLIB:-OFF} \
 POSIX		"POSIX-like functions" ${WITH_POSIX:-OFF} \
 POSTGRESQL	"PostgreSQL database support" ${WITH_POSTGRESQL:-OFF} \
 PSPELL		"pspell support" ${WITH_PSPELL:-OFF} \
