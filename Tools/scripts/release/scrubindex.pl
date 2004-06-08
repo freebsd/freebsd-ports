23cad417ec9b254e09b8c14b13d698d53b0025e5
Murray Stokely <murray@FreeBSD.org>
2004-06-08 21:57:01 +0000

    Modernize these two scripts a little bit to make it easier to deal with 5.X or 4.X package sets (i.e., INDEX vs INDEX-5 and tgz vs tbz)
    OKed by:    marcus

--- Tools/scripts/release/scrubindex.pl
+++ Tools/scripts/release/scrubindex.pl
@@ -5,6 +5,12 @@ die "$0 <pkgdir> <indexfile>\n" if ($#ARGV != 1);
 
 $xdep = 'XFree86-3\.3\.6_10';
 $pkgdir = shift(@ARGV);
+#$ext = 'tbz';
+$ext = 'tgz';
+
+print STDERR "scrubindex pkgdir: $pkgdir\n";
+
+my $cnt = 0;
 
 LOOP:
 while (<>) {
@@ -13,11 +19,13 @@ while (<>) {
 	s/\| /\|/g;
 
 	@f = split('\|');
-	next if (! -f "$pkgdir/$f[0].tgz");
+	next if (! -f "$pkgdir/$f[0].$ext");
 
 	foreach $dep (split(' ', $f[8])) {
-		next LOOP if (! -f "$pkgdir/$dep.tgz");
+		next LOOP if (! -f "$pkgdir/$dep.$ext");
 	}
 
+	$cnt++;
 	print;
 }
+print STDERR "$cnt lines copied from scrubindex.pl"
