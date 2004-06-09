86c01e98532b6e8d9f89cae3ab7b49bade5a84cd
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-06-09 06:52:06 +0000

    add beagle 2.1.3 An Evolutionary Computation Framework

---
+++ devel/beagle/files/patch-config:ltmain.sh
@@ -0,0 +1,15 @@
+--- config/ltmain.sh.orig	Wed Jun  9 14:13:42 2004
++++ config/ltmain.sh	Wed Jun  9 14:13:51 2004
+@@ -4330,10 +4330,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
