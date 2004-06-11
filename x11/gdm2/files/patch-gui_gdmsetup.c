b63682d265f545dbe7c56028f1d390d0311ef54c
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-06-11 19:53:54 +0000

    Update to 2.6.0.3.

--- x11/gdm2/files/patch-gui_gdmsetup.c
+++ x11/gdm2/files/patch-gui_gdmsetup.c
@@ -1,11 +1,11 @@
---- gui/gdmsetup.c.orig	Mon Apr 12 14:24:57 2004
-+++ gui/gdmsetup.c	Mon Apr 12 14:25:00 2004
-@@ -1962,7 +1962,7 @@
- 			success = TRUE;
- 
- 			/* HACK! */
--			cmd = g_strdup_printf ("%s -R root.root %s", chown, quoted);
-+			cmd = g_strdup_printf ("%s -R root:wheel %s", chown, quoted);
- 			system (cmd);
- 			g_free (cmd);
- 
+--- gui/gdmsetup.c.orig	Fri Jun  4 14:37:17 2004
++++ gui/gdmsetup.c	Fri Jun 11 15:42:28 2004
+@@ -1983,7 +1983,7 @@
+ 		/* HACK! */
+ 		argv[0] = chown;
+ 		argv[1] = "-R";
+-		argv[2] = "root:root";
++		argv[2] = "root:wheel";
+ 		argv[3] = quoted;
+ 		argv[4] = NULL;
+ 		simple_spawn_sync (argv);
