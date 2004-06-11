8a4881c4d880f88758f5b517354f1912a161d4f1
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-06-11 07:10:39 +0000

    add k3d 0.4.1.0 A free 3D modeling, animation, and rendering system

---
+++ graphics/k3d/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Jun 10 09:49:38 2004
++++ ltmain.sh	Thu Jun 10 09:49:53 2004
+@@ -4383,10 +4383,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
