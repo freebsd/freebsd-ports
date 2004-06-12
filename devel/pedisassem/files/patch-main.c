cf65af831e3c751e5c9fdbf616b0d9e19d33a812
Tilman Keskinoz <arved@FreeBSD.org>
2004-06-12 21:41:39 +0000

    Fix build with gcc34

---
+++ devel/pedisassem/files/patch-main.c
@@ -0,0 +1,56 @@
+--- main.c.orig	Wed Sep 12 13:00:18 2001
++++ main.c	Sat Jun 12 23:39:59 2004
+@@ -2146,7 +2146,7 @@
+                 if (getMap(pos)&0x05) orMap(pos, 0x10);
+                 /*----------*/popTrace();
+             }
+-        default:
++        default: break;
+     }
+     return 1;
+ }
+@@ -2246,7 +2246,7 @@
+             orMap(r, 0xE0);  
+             /*-----------*/popTrace();
+             break;
+-        default:
++        default: break;
+     }
+ }
+ 
+@@ -2306,7 +2306,7 @@
+         switch(i)
+         {
+             case  8: changeTo80Real(r,rmax);  break;
+-            default: 
++            default: break; 
+         }
+     }
+ }
+@@ -2970,7 +2970,7 @@
+             // the following code is move to above position....^^^^^.....
+             //if (ref<pos+4) { eraseUncertain(ref); break; }
+ 
+-            default:
++            default: break;
+         }
+     }
+  //
+@@ -3042,7 +3042,7 @@
+                           /*-----------*/popTrace();
+                      }
+                  }
+-            default:
++            default: break;
+         }
+     }
+ }
+@@ -3217,7 +3217,7 @@
+                  }
+              }
+              break;
+-        default:
++        default: break;
+     }
+     /*-----------*/popTrace();
+ }
---
+++ devel/pedisassem/files/patch-print.c
@@ -0,0 +1,29 @@
+--- print.c.bak	Fri Oct 13 09:27:24 2000
++++ print.c	Sat Jun 12 23:42:27 2004
+@@ -225,7 +225,7 @@
+         case 16: pr1ntf("word");  printCol+=4; break;
+         case 32: pr1ntf("dword"); printCol+=5; break;
+         case 64: pr1ntf("qword"); printCol+=5; break;
+-        default:
++        default: break;
+     }
+     prefix();
+ }
+@@ -430,7 +430,7 @@
+                      break;
+             case 16: print16case();
+                      break;
+-            default:
++            default: break;
+         }
+     }
+     else 
+@@ -449,7 +449,7 @@
+                      break;
+             case  5: print25case();
+                      break;
+-            default:
++            default: break;
+         }
+     }    
+ } /* end of nmonicprint() */
