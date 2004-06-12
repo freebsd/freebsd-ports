2dbf892c19a9a6eb7adf9a50db2bdbdcef5c72d0
Pav Lucistnik <pav@FreeBSD.org>
2004-06-12 15:09:21 +0000

    - Use OPTIONS - Fix dependencies
    PR:		ports/67817
    Submitted by:	Douglas K. Rand <rand@meridian-enviro.com> (maintainer)

---
+++ graphics/mapserver/files/patch-map.h
@@ -0,0 +1,11 @@
+--- map.h.orig	Mon Jun  7 16:12:02 2004
++++ map.h	Mon Jun  7 16:11:50 2004
+@@ -39,7 +39,7 @@
+ #endif
+ 
+ #ifdef USE_MING_FLASH
+-#include "ming.h"
++#include "ming/ming.h"
+ #endif
+ 
+ #include <sys/types.h> /* regular expression support */
