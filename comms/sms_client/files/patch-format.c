a4087d433017a5774533bbbe3f225e054eb20ae6
arved <arved@FreeBSD.org>
2004-06-12 20:24:01 +0000

    Fix build with gcc34

---
+++ comms/sms_client/files/patch-format.c
@@ -0,0 +1,10 @@
+--- src/common/format.c.orig	Sat Jun 12 22:16:16 2004
++++ src/common/format.c	Sat Jun 12 22:16:24 2004
+@@ -265,6 +265,7 @@
+ 					break;
+ 
+ 				default:
++					break;
+ 			}
+ 
+ 			fmt++;
