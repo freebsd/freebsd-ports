fb499032dc624363d5a58b550608e9801e0beccd
netchild <netchild@FreeBSD.org>
2004-06-13 15:01:27 +0000

    - Update to 1.1.9. Relevant parts from the release notes:   <snip>   Cdrdao changes:   - MMC-driver auto-detection. If no --driver option is specified,     cdrdao will try to autodetect an MMC-compliant device and use the     generic-mmc driver by default. (Denis Leroy)   - Included scsilib upgraded from cdrtools 2.01a31 (Denis Leroy)   - Added --eject option to the unlock command to eject the cd after     unlocking. (Eric Benson)   - New 'discid' command to display CDDB information. (Eric Benson,     Denis Leroy).   - Added --full-burn and --capacity options to extend lead-out to entire     disc, beyond 80 mins limit. (Vladimir Petrov)   GCDMaster changes (Denis Leroy) :   - Some bug fixes (manual driver configurations were not being saved     correctly).   cue2toc changes:   - Version 0.2 released. Complete rewrite, should now handle most of all     CUE files. (Matthias Czapla)   <snip> - Disable the use of mlockall(2) on FreeBSD 5, even after the last round   of fixes there are still issues. - Add a workaround for problems with gmake and certain locales on FreeBSD 5. - Use MASTER_SITE_SOURCEFORGE_EXTENDED. - Use ARCH instead of MACHINE_ARCH to please portlint >= 2.5.9.
    Submitted by:		marius
    On my TODO list since:	June 7 :-(

--- sysutils/cdrdao/files/patch-dao::dao.cc
+++ sysutils/cdrdao/files/patch-dao::dao.cc
@@ -1,15 +1,29 @@
 --- dao/dao.cc.orig	Thu Feb 12 02:13:31 2004
 +++ dao/dao.cc	Tue Mar  2 17:22:11 2004
-@@ -388,10 +388,10 @@
-         if (cdr->readBufferCapacity(&totalcap, &availcap)) {
-           writerFill = (int)((1.0 - ((double)availcap / (double)totalcap))
-                              * 100.0);
--          message(1, "Wrote %ld of %ld MB (Buffers %3d%% %3d%%).\n",
-+          message(1, "Wrote %ld of %ld MB (Buffers %3d%% %3d%%).\r",
-                   cnt >> 20, total >> 20, buffFill, writerFill);
-         } else {
--          message(1, "Wrote %ld of %ld MB (Buffer %3d%%).\n",
-+          message(1, "Wrote %ld of %ld MB (Buffer %3d%%).\r",
-                   cnt >> 20, total >> 20, buffFill);
-         }
-         lastMb = cntMb;
+@@ -703,7 +703,7 @@
+ 
+     setsid(); // detach from controlling terminal
+ 
+-#ifdef HAVE_MLOCKALL
++#if defined(HAVE_MLOCKALL) && !defined(__FreeBSD__)
+     if (geteuid() == 0) {
+       if (mlockall(MCL_CURRENT|MCL_FUTURE) != 0) {
+ 	message(-1, "Cannot lock memory pages: %s", strerror(errno));
+@@ -737,7 +737,7 @@
+     break;
+   }
+ 
+-#ifdef HAVE_MLOCKALL
++#if defined(HAVE_MLOCKALL) && !defined(__FreeBSD__)
+   if (geteuid() == 0) {
+     if (mlockall(MCL_CURRENT|MCL_FUTURE) != 0) {
+       message(-1, "Cannot lock memory pages: %s", strerror(errno));
+@@ -762,7 +762,7 @@
+     cdr->abortDao(); // abort writing process
+ 
+  fail:
+-#ifdef HAVE_MUNLOCKALL
++#if defined(HAVE_MUNLOCKALL) && !defined(__FreeBSD__)
+   munlockall();
+ #endif
+ 
