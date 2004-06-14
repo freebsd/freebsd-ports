22b2c108a2575d1bbe35fa65155d638a6a15d1a7
Volker Stolz <vs@FreeBSD.org>
2004-06-14 21:17:22 +0000

    Update to 1.4
    PR:		ports/67908
    Submitted by:	maintainer

--- mail/milter-greylist/files/patch-dump.h
+++ mail/milter-greylist/files/patch-dump.h
@@ -1,5 +1,5 @@
---- dump.h.orig	Tue May 25 11:39:49 2004
-+++ dump.h	Tue Jun  8 10:24:13 2004
+--- dump.h.orig	Tue Jun  8 16:47:47 2004
++++ dump.h	Sat Jun 12 10:47:56 2004
 @@ -46,7 +46,7 @@
  #include <arpa/inet.h>
  
