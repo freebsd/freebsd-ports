643a3e07da92388e2536cd76a3901cb3a8da820c
Bruce M Simpson <bms@FreeBSD.org>
2004-06-14 16:56:13 +0000

    Committed (with some fixups), thanks!
    PR:		ports/67327
    Submitted by:	Paul Chvostek

---
+++ net-mgmt/rotorouter/files/rotorouter.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# $Id$
+#
+
+# PROVIDE: rotorouter
+# REQUIRE: DAEMON
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
+#
+# Add the following lines to /etc/rc.conf to enable rotorouter:
+#
+#   rotorouter_enable="YES"
+#
+# See rotorouter(8) for flags.
+#
+
+. %%RC_SUBR%%
+
+name=rotorouter
+rcvar=`set_rcvar`
+
+command=%%PREFIX%%/sbin/${name}
+
+# set defaults
+
+rotorouter_enable=${rotorouter_enable:-"NO"}
+rotorouter_flags="${rotorouter_flags:--h %%PREFIX%%/etc/rotorouter.conf}"
+
+rotorouter_flags="${rotorouter_flags} &"
+
+load_rc_config $name
+run_rc_command "$1"
+
