376c2d472b0032d9ff4631d22f68eec198bd03e6
Kirill Ponomarev <krion@FreeBSD.org>
2004-06-14 18:15:00 +0000

    Update to version 1.3
    PR:		ports/67801
    Submitted by:	maintainer

--- games/gl-117/files/patch-src_common.h
+++ games/gl-117/files/patch-src_common.h
@@ -1,20 +1,20 @@
---- src/common.h.orig	Fri Aug 22 06:07:09 2003
-+++ src/common.h	Thu Sep  4 05:31:40 2003
+--- src/common.h.orig	Sun Jun 13 11:17:16 2004
++++ src/common.h	Sun Jun 13 11:17:43 2004
 @@ -22,13 +22,13 @@
  // APPLE path is different (thanks Minami)
  #ifdef __APPLE__
  #include <GLUT/glut.h>
 -#include "SDL/SDL.h"
 -#include "SDL/SDL_thread.h"
-+#include "SDL.h"
-+#include "SDL_thread.h"
++#include "SDL11/SDL.h"
++#include "SDL11/SDL_thread.h"
  #ifdef HAVE_SDL_MIXER
 -#include "SDL/SDL_mixer.h"
-+#include "SDL_mixer.h"
++#include "SDL11/SDL_mixer.h"
  #endif
  #ifdef HAVE_SDL_NET
 -#include "SDL/SDL_net.h"
-+#include "SDL_net.h"
++#include "SDL11/SDL_net.h"
  #endif
  #endif
  #ifndef __APPLE__
@@ -24,15 +24,15 @@
  #include <GL/glut.h>
 -#include "SDL/SDL.h"
 -#include "SDL/SDL_thread.h"
-+#include "SDL.h"
-+#include "SDL_thread.h"
++#include "SDL11/SDL.h"
++#include "SDL11/SDL_thread.h"
  #ifdef HAVE_SDL_MIXER
 -#include "SDL/SDL_mixer.h"
-+#include "SDL_mixer.h"
++#include "SDL11/SDL_mixer.h"
  #endif
  #ifdef HAVE_SDL_NET
 -#include "SDL/SDL_net.h"
-+#include "SDL_net.h"
++#include "SDL11/SDL_net.h"
  #endif
  #endif
  #endif // __APPLE__
