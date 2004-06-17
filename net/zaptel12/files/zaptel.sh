a468afc698e426244287164cf2f56aacce16bd99
Maxim Sobolev <sobomax@FreeBSD.org>
2004-06-17 17:01:34 +0000

    Update to 0.5, which brings in TDM400P support (only fxo is tested right now). Also features some fixes for existing X100P driver.

--- net/zaptel12/files/zaptel.sh
+++ net/zaptel12/files/zaptel.sh
@@ -7,11 +7,21 @@ case "$1" in
 	start)
 		/sbin/kldload ${LIBDIR}/zaptel.ko || exit 1
 		/sbin/kldload ${LIBDIR}/wcfxo.ko || exit 1
-		/usr/sbin/devinfo | /usr/bin/grep -q wcfxo && ${PREFIX}/bin/ztcfg 2> /dev/null
+		/sbin/kldload ${LIBDIR}/wcfxs.ko || exit 1
+		if /usr/sbin/devinfo | /usr/bin/grep -q wcfxo
+		then
+			${PREFIX}/bin/ztcfg 2> /dev/null
+		else
+			if /usr/sbin/devinfo | /usr/bin/grep -q wcfxs
+			then
+				${PREFIX}/bin/ztcfg 2> /dev/null
+			fi
+		fi
 		echo -n " zaptel"
 		;;
 
 	stop)
+		/sbin/kldunload wcfxs.ko || exit 1
 		/sbin/kldunload wcfxo.ko || exit 1
 		/sbin/kldunload zaptel.ko || exit 1
 		echo -n " zaptel"
