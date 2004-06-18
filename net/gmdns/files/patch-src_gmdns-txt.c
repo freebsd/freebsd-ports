4782b29ec0cc429fafdc7da7df28cc5cf03a0069
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-06-18 19:19:29 +0000

    Fix build on 64-bit platforms.
    Reported by:	pointyhat via kris

--- net/gmdns/files/patch-src_gmdns-txt.c
+++ net/gmdns/files/patch-src_gmdns-txt.c
@@ -1,6 +1,18 @@
---- src/gmdns-txt.c.orig	Sun Jun  6 01:27:19 2004
-+++ src/gmdns-txt.c	Sun Jun  6 01:29:38 2004
-@@ -106,6 +106,7 @@
+--- src/gmdns-txt.c.orig	Fri Jun  4 13:19:40 2004
++++ src/gmdns-txt.c	Fri Jun 18 15:19:04 2004
+@@ -78,7 +78,11 @@
+ 		}
+ 
+ 		if (pairlen+i > len) {
++#if GLIB_SIZEOF_LONG == 4
+ 			g_warning ("invalid record (pair claims to extend beyond the end of the record (%d + %d > len = %d))", pairlen, i, len);
++#else
++			g_warning ("invalid record (pair claims to extend beyond the end of the record (%d + %d > len = %ld))", pairlen, i, len);
++#endif
+ 			return hash;
+ 		}
+ 
+@@ -106,6 +110,7 @@
  			return hash;
  		}
  
