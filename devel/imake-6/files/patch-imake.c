80a5a8cf6ca47c3032bd1e3027162b7393d2f7fc
anholt <anholt@FreeBSD.org>
2004-06-19 05:36:38 +0000

    New port: imake-6: imake and other build utilities from X.Org.

---
+++ devel/imake-6/files/patch-imake.c
@@ -0,0 +1,22 @@
+--- imake/imake.c.orig	Tue Dec 17 04:21:38 2002
++++ imake/imake.c	Sat Jan  4 15:57:23 2003
+@@ -1384,6 +1384,19 @@
+ 	  name = &uts_name;
+       }
+ #endif
++# ifdef __FreeBSD__
++       /* Hack for FreeBSD ports "Cross Version" compiling system. */
++       {
++	 char *e;
++	 if ((e = getenv("OSREL")) != NULL && 
++	     strlen(name->sysname) + strlen(e) + 1 < SYS_NMLN) {
++	  strcpy(name->release, e);
++	  strcpy(name->version, name->sysname);
++	  strcat(name->version, " ");
++	  strcat(name->version, e);
++	 }
++       }
++# endif
+ 
+ #  if defined DEFAULT_OS_NAME
+ #   if defined CROSSCOMPILE
---
+++ devel/imake-6/files/patch-mkhtmlindex.pl
@@ -0,0 +1,13 @@
+Index: util/mkhtmlindex.pl
+===================================================================
+RCS file: /home/ncvs/xfree/xc/config/util/mkhtmlindex.pl,v
+retrieving revision 1.3
+diff -u -r1.3 mkhtmlindex.pl
+--- util/mkhtmlindex.pl	18 Mar 2001 14:49:01 -0000	1.3
++++ util/mkhtmlindex.pl	12 Dec 2003 22:19:08 -0000
+@@ -1,4 +1,4 @@
+-#!/usr/bin/perl
++#!/usr/bin/env perl
+ #
+ # $XFree86: xc/config/util/mkhtmlindex.pl,v 1.2 2001/03/15 19:02:31 dawes Exp $
+ #
