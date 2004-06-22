f5c1ab781ac178f95adc5ca58233996d7cbd67a8
Pav Lucistnik <pav@FreeBSD.org>
2004-06-22 19:18:41 +0000

    - Add rc script - Expand variables in pkg-message - Move users.sample to better place
    PR:		ports/68164
    Submitted by:	Roman Bogorodskiy <bogorodskiy@inbox.ru> (maintainer)

---
+++ ftp/wzdftpd/files/wzdftpd.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Start or stop wzdftpd
+# $FreeBSD$
+
+# PROVIDE: wzdftpd
+# REQUIRE: DAEMON
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
+#
+
+prefix=%%PREFIX%%
+
+wzdftpd_enable=${wzdftpd_enable:-"NO"}		# Enable wzdftpd
+wzdftpd_program="${prefix}/sbin/wzdftpd"	# Location of wzdftpd
+wzdftpd_flags=${gkrellmd_flags:-""}		# Flags to wzdftpd
+
+. %%RC_SUBR%%
+
+name="wzdftpd"
+rcvar=`set_rcvar`
+command="${prefix}/sbin/${name}"
+required_files="${prefix}/etc/wzd.cfg"
+
+load_rc_config $name
+run_rc_command "$1"
