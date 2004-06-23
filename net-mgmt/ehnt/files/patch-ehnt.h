79c3a398f469d146fb0eedbae6b08cb6a237bb65
Sergey A. Osokin <osa@FreeBSD.org>
2004-06-23 12:01:33 +0000

    Make small improvement: print packets per second in topmode.
    Original patch from:		Gleb Smirnoff <glebius@cell.sick.ru>
    Fixed patch from, approved by:	Dmitry Morozovsky <marck@rinet.ru> (maintainer)

--- net-mgmt/ehnt/files/patch-ehnt.h
+++ net-mgmt/ehnt/files/patch-ehnt.h
@@ -2,8 +2,16 @@
 $FreeBSD$
 
 --- ehnt.h.orig	Thu Oct  4 22:18:29 2001
-+++ ehnt.h	Thu May 20 16:07:24 2004
-@@ -93,7 +93,7 @@
++++ ehnt.h	Wed Jun 23 14:35:08 2004
+@@ -59,6 +59,7 @@
+ 
+ struct ehnt_stats {
+   double InBoundTotal, OutBoundTotal;
++  double InBoundTotalPkts, OutBoundTotalPkts;
+   double *InBoundSrcAS, *InBoundDstAS, *OutBoundSrcAS, *OutBoundDstAS;
+   double *InBoundProto, *OutBoundProto;
+   double *InBoundSrcTcpPort, *InBoundDstTcpPort, 
+@@ -93,7 +94,7 @@
  int ProcessFlow(struct flow_ver5_rec *, struct in_addr *, struct ehnt_struct *);
  int Init_ASN_Lookups(void);
  char * ASN_Lookup(int);
--- net-mgmt/ehnt/files/patch-ehnt__display.c
+++ net-mgmt/ehnt/files/patch-ehnt__display.c
@@ -2,8 +2,43 @@
 $FreeBSD$
 
 --- ehnt_display.c.orig	Thu Oct  4 22:18:29 2001
-+++ ehnt_display.c	Thu May 20 16:06:07 2004
-@@ -246,7 +246,7 @@
++++ ehnt_display.c	Wed Jun 23 14:35:08 2004
+@@ -110,7 +110,9 @@
+   ShowReportTimeHeader(stats);
+ 
+   if ( ! report_inout ) {
+-    printf (" Total %10.0fkbits/s\n",stats->InBoundTotal*8/((interval)*1024));
++    printf (" Total %10.0fkbits/s, %10.0fkpps\n",
++	stats->InBoundTotal*8/((interval)*1024),
++	stats->InBoundTotalPkts/((interval)*1024));
+     if ( e_cfg->topmode != ETM_PROTO ) {
+       printf ("Rank|-------------Source----------------| |---------------Dest----------------|\n");
+     }
+@@ -151,8 +153,9 @@
+       printf ("Rank|-------------Source----------------| |---------------Dest----------------|\n");
+     }
+     printf("%s",header);
+-    printf ("                          O   U   T   B   O   U   N   D  (total %.0fkbits/s)\n",
+-	    stats->OutBoundTotal*8/((interval)*1024));
++    printf ("                          O   U   T   B   O   U   N   D  (total %.0fkbits/s, %.0fkpps)\n",
++	    stats->OutBoundTotal*8/((interval)*1024),
++	    stats->OutBoundTotalPkts/((interval)*1024));
+ 
+     for (i=1; i<= (int)(e_cfg->screenlength - headersize)/2 + 1; i++) {
+       printf("%2d:",i);
+@@ -183,8 +186,9 @@
+       }
+       printf("\n");
+     }
+-    printf ("                            I   N   B   O   U   N   D    (total %.0fkbits/s)\n",
+-	    stats->InBoundTotal*8/((interval)*1000));
++    printf ("                            I   N   B   O   U   N   D    (total %.0fkbits/s, %.0fkpps)\n",
++	    stats->InBoundTotal*8/((interval)*1024),
++	    stats->InBoundTotalPkts/((interval)*1024));
+     for (i=1; i<= (int)(e_cfg->screenlength - headersize)/2; i++) {
+       printf("%2d:",i);
+       switch (e_cfg->topmode) {
+@@ -246,7 +250,7 @@
  
  static char str[100];
  
