aff3fdce4cf8f25ddfada80757d1a3148754e18c
adamw <adamw@FreeBSD.org>
2004-06-25 05:31:17 +0000

    Update to 0.5.0.

--- games/vamos/files/patch-geometry::Ac3d.cc
+++ games/vamos/files/patch-geometry::Ac3d.cc
@@ -1,6 +1,6 @@
---- geometry/Ac3d.cc.orig	Mon Sep 29 11:30:40 2003
-+++ geometry/Ac3d.cc	Fri Oct  3 18:35:35 2003
-@@ -26,6 +26,8 @@
+--- geometry/Ac3d.cc.orig	Fri Jun 25 01:10:29 2004
++++ geometry/Ac3d.cc	Fri Jun 25 01:11:59 2004
+@@ -27,6 +27,8 @@
  #include <cassert>
  #include <iostream>
  #include <cstdlib>
@@ -9,15 +9,15 @@
  
  using namespace Vamos_Geometry;
  
-@@ -256,7 +258,11 @@
-   m_normal [2] = 1.0;
+@@ -188,7 +190,11 @@
+ 	m_normal [2] = 1.0;
  
-   std::istringstream is (type_code);
+ 	std::istringstream is (figure_type_code);
 +#if defined(__GNUC__) && __GNUC__ < 3
-+  is.setf (std::ios::hex, std::ios::basefield);
++	is.setf (std::ios::hex, std::ios::basefield);
 +#else
-   is.setf (std::ios_base::hex, std::ios_base::basefield);
+ 	is.setf (std::ios_base::hex, std::ios_base::basefield);
 +#endif
-   int code;
-   is >> code;
+ 	int code;
+ 	is >> code;
  
