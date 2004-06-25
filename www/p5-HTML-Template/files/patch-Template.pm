6e907eb8c2dce50cd189e1a3992ca3ed7fc04bf8
kuriyama <kuriyama@FreeBSD.org>
2004-06-25 13:47:03 +0000

    Upgrade to 2.7.
    Submitted by:	Alex Kapranoff <kappa@rambler-co.ru>

--- www/p5-HTML-Template/files/patch-Template.pm
+++ www/p5-HTML-Template/files/patch-Template.pm
@@ -1,25 +1,12 @@
---- Template.pm.orig	Sat Feb  2 08:01:37 2002
-+++ Template.pm	Thu May 30 12:33:34 2002
-@@ -863,7 +863,9 @@
+--- Template.pm.orig	Fri Jun 18 21:42:06 2004
++++ Template.pm	Fri Jun 25 13:33:50 2004
+@@ -890,7 +890,9 @@
  use strict; # and no funny business, either.
  
  use Carp; # generate better errors with more context
 +BEGIN { unshift(@INC, "%%PERL_INC%%"); }
  use File::Spec; # generate paths that work on all platforms
 +shift(@INC);
+ use Digest::MD5 qw(md5_hex); # generate cache keys
  
  # define accessor constants used to improve readability of array
- # accesses into "objects".  I used to use 'use constant' but that
-@@ -2011,10 +2013,10 @@
- 	push(@ifstack, $cond);
- 	
-       } elsif ($which eq '/TMPL_IF' or $which eq '/TMPL_UNLESS') {
--	$options->{debug} and print STDERR "### HTML::Template Debug ###$fname : line $fcounter : $which end\n";
-+	$options->{debug} and print STDERR "### HTML::Template Debug ### $fname : line $fcounter : $which end\n";
- 	
- 	my $cond = pop(@ifstack);
--	die "HTML::Template->new() : found </${which}> with no matching <TMPL_IF> at $fname : line $fcounter." unless defined $cond;
-+	die "HTML::Template->new() : found <${which}> with no matching <TMPL_IF> at $fname : line $fcounter." unless defined $cond;
- 	if ($which eq '/TMPL_IF') {
- 	  die "HTML::Template->new() : found </TMPL_IF> incorrectly terminating a <TMPL_UNLESS> (use </TMPL_UNLESS>) at $fname : line $fcounter.\n" 
- 	    if ($cond->[HTML::Template::COND::WHICH] == HTML::Template::COND::WHICH_UNLESS);
