782e1008c7aa1a2d32cabec3b719c1378e097e65
krion <krion@FreeBSD.org>
2004-06-25 07:26:42 +0000

    Add hydrogen 0.8.2, an advanced drum machine.
    PR:		ports/68242
    Submitted by:	Jean-Yves Lefort <jylefort@brutele.be>

---
+++ audio/hydrogen/files/patch-lib::Object.h
@@ -0,0 +1,10 @@
+--- lib/Object.h.orig	Wed Jun 23 01:10:55 2004
++++ lib/Object.h	Wed Jun 23 01:11:45 2004
+@@ -31,6 +31,7 @@
+ #include <string>
+ using std::string;
+ using std::map;
++#include <qglobal.h>
+ 
+ 
+ class Object;
