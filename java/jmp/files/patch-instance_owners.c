e324002963e712f3b13d85e40bd1996d76cd9347
Greg Lewis <glewis@FreeBSD.org>
2004-06-25 04:32:08 +0000

    . Update to 0.42. [1] . Don't use jikes with this port (it breaks the build).
    PR:		68174 [1]
    Submitted by:	Ronald Klop <ronald@cs.vu.nl> (maintainer) [1]

--- java/jmp/files/patch-instance_owners.c
+++ java/jmp/files/patch-instance_owners.c
@@ -1,15 +1,14 @@
 $FreeBSD$
 
---- instance_owners.c	Tue May 13 20:28:40 2003
-+++ instance_owners.c	Tue May 13 20:28:11 2003
-@@ -11,6 +11,10 @@
- #include <jmp-config.h>
- #include <inttypes.h>
- 
+--- instance_owners.c.orig	Fri Jun 25 02:20:42 2004
++++ instance_owners.c	Fri Jun 25 02:28:35 2004
+@@ -16,0 +17,4 @@
 +#if __FreeBSD__ < 5
 +#define PRId32 "d"
 +#endif
 +
- enum {
-     INFO,
-     NUM_COLUMNS
+@@ -205,0 +210 @@
++    GtkWidget* menuitem;
+@@ -207 +212 @@
+-    GtkWidget* menuitem = gtk_menu_item_new_with_label (buf);
++    menuitem = gtk_menu_item_new_with_label (buf);
