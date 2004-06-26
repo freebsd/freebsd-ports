d16c96bd099c7c2ebba94d296660659798ef8a50
marcus <marcus@FreeBSD.org>
2004-06-26 03:28:37 +0000

    Add support for a mime database in ${X11BASE} as well as ${LOCALBASE}.
    Requested by:	pav

--- misc/shared-mime-info/files/patch-update-mime-database.c
+++ misc/shared-mime-info/files/patch-update-mime-database.c
@@ -1,5 +1,5 @@
---- update-mime-database.c.orig	Mon Oct 13 11:43:38 2003
-+++ update-mime-database.c	Mon Apr 12 22:45:01 2004
+--- update-mime-database.c.orig	Mon Oct 13 05:43:38 2003
++++ update-mime-database.c	Wed Jun 23 20:13:10 2004
 @@ -377,7 +377,8 @@
  		}
  	}
@@ -10,3 +10,12 @@
  }
  
  /* 'node' is a <mime-type> node from a source file, whose type is 'type'.
+@@ -1330,7 +1331,7 @@
+ 
+ 	env = getenv("XDG_DATA_DIRS");
+ 	if (!env)
+-		env = "/usr/local/share/:/usr/share/";
++		env = "/usr/local/share/:%%X11BASE%%/share/:/usr/share/";
+ 	dirs = g_strsplit(env, ":", 0);
+ 	g_return_if_fail(dirs != NULL);
+ 	for (n = 0; dirs[n]; n++)
