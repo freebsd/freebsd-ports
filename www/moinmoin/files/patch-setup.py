f1b46e3d268fa8c8a0e65e05dbcaa37ed573416f
eik <eik@FreeBSD.org>
2004-06-26 15:07:21 +0000

    Updating www/moinmoin to 1.2.2. MoinMoin 1.1 contains administrative group name privilege escalation vulnerability.   <http://www.freebsd.org/ports/portaudit/da9e6438-bfc0-11d8-b00e-000347a4fa7d.html>
    Fix UnpicklingError in MoinMoin/userp.py.
    
    PR:		68365
    Submitted by:	TAOKA Fumiyoshi <fmysh@iijmio-mail.jp>

--- www/moinmoin/files/patch-setup.py
+++ www/moinmoin/files/patch-setup.py
@@ -1,16 +1,16 @@
---- setup.py.orig    Sun Nov  9 16:00:45 2003
-+++ setup.py       Sun Dec  7 01:04:52 2003
-@@ -167,10 +167,10 @@
+--- setup.py.orig	Sun Jun  6 08:58:33 2004
++++ setup.py	Fri Jun 25 03:31:38 2004
+@@ -167,9 +167,11 @@
+     'scripts': moin_scripts,
+ 
      'data_files': [
-         ('share/moin/cgi-bin',
-             glob.glob('wiki/cgi-bin/*.cgi') +
--            glob.glob('wiki/cgi-bin/*.py')  ),
-+            glob.glob('wiki/cgi-bin/*.py.default')  ),
-         ('share/moin/data',
-             ['wiki/data/intermap.txt']),
--        ('share/moin/data/text',
-+        ('share/moin/data/text.default',
-             filter(pagefile_filter, glob.glob('wiki/data/text/*'))),
-         ('share/moin/data/backup', []),
-         ('share/moin/data/cache', []),
-
+-        ('share/moin/cgi-bin', filter(trash_filter, glob.glob('wiki/cgi-bin/*'))),
++        ('share/moin/cgi-bin', filter(trash_filter,
++            glob.glob('wiki/cgi-bin/*.cgi') +
++            glob.glob('wiki/cgi-bin/*.py.default'))),
+         ('share/moin/data', ['wiki/data/intermap.txt']),
+-        ('share/moin/data/text', filter(trash_filter, glob.glob('wiki/data/text/*'))),
++        ('share/moin/data/text.default', filter(trash_filter, glob.glob('wiki/data/text/*'))),
+         ('share/moin/data/plugin', ['wiki/data/plugin/__init__.py']),
+         ('share/moin/data/plugin/action', ['wiki/data/plugin/action/__init__.py']),
+         ('share/moin/data/plugin/macro', ['wiki/data/plugin/macro/__init__.py']),
