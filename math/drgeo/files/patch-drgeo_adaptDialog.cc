76a9fa4ba5b6f88ec54c1af915020dc97d70925a
Kirill Ponomarev <krion@FreeBSD.org>
2004-06-27 18:12:01 +0000

    Update to version 0.9.13
    PR:		ports/68387
    Submitted by:	Ports Fury

--- math/drgeo/files/patch-drgeo_adaptDialog.cc
+++ math/drgeo/files/patch-drgeo_adaptDialog.cc
@@ -1,10 +1,11 @@
---- drgeo_adaptDialog.cc.orig	Sat Jan 11 03:21:20 2003
-+++ drgeo_adaptDialog.cc	Sat Jan 11 03:21:27 2003
-@@ -24,7 +24,6 @@
- #include <stdio.h>
- #include <time.h>
- #include <unistd.h>
--#include <crypt.h>
- #include <string.h>
+--- drgeo_adaptDialog.cc.orig	Mon Jun  7 02:30:06 2004
++++ drgeo_adaptDialog.cc	Tue Jun  8 13:20:32 2004
+@@ -33,7 +33,7 @@
+ #include "drgeo_menu.h"
+ #include "drgenius_mdi.h"
  
- #include <glade/glade.h>
+-#ifndef G_PLATFORM_WIN32
++#if !defined(G_PLATFORM_WIN32) && !defined(__FreeBSD__)
+    #include <crypt.h>
+ #endif 
+ 
