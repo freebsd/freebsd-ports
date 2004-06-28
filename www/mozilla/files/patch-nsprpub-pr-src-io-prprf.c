6125fcff5fd4e44927009ee2cc3eaca9b0b7f7d6
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-06-28 02:18:58 +0000

    * Update to 1.7 * Build the chrome and component registries in a pkg-install script so that   the resulting paths are correct [1]
    For a list of all the changes in 1.7, see
    http://www.mozilla.org/releases/mozilla1.7.  This port features support for
    the GSSAPI extension as well as optional gnome-vfs support.
    
    PR:		65590 [1]
    Submitted by:	thierry [1]

--- www/mozilla/files/patch-nsprpub-pr-src-io-prprf.c
+++ www/mozilla/files/patch-nsprpub-pr-src-io-prprf.c
@@ -1,12 +1,14 @@
---- nsprpub/pr/src/io/prprf.c	Wed Feb 26 15:53:42 2003
-+++ nsprpub/pr/src/io/prprf.c	Thu Oct 16 22:50:40 2003
-@@ -52,5 +52,8 @@
+--- nsprpub/pr/src/io/prprf.c.orig	Mon Mar  8 22:18:19 2004
++++ nsprpub/pr/src/io/prprf.c	Fri Mar 19 15:26:55 2004
+@@ -51,7 +51,10 @@
+ ** Note: on some platforms va_list is defined as an array,
  ** and requires array notation.
  */
--#if (defined(LINUX) && defined(__powerpc__)) || \
+-#if (defined(LINUX) && defined(__x86_64__))
 +#if defined(__amd64__)
 +#include <stdarg.h>
 +#define VARARGS_ASSIGN(foo, bar) va_copy((foo), (bar))
-+#elif (defined(LINUX) && defined(__powerpc__)) || \
++#elif (defined(LINUX) && defined(__x86_64__))
+ #define VARARGS_ASSIGN(foo, bar) __va_copy((foo), (bar))
+ #elif (defined(LINUX) && defined(__powerpc__)) || \
      (defined(LINUX) && defined(__s390__)) || \
-     (defined(LINUX) && defined(__s390x__)) || \
