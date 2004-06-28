c73e47103c63e1d04e115e1832cc05c06df84a6e
vs <vs@FreeBSD.org>
2004-06-28 10:29:10 +0000

    Add qmrtg 0.5.6, a simple tool for monitoring and graphing qmail activity through MRTG.
    PR:		ports/67887
    Submitted by:	Mij <mij @ dev.publicshout.org>

---
+++ mail/qmrtg/files/patch-qmrtgsync.sh
@@ -0,0 +1,8 @@
+--- script/qmrtgsync.sh	Sat Jun 12 02:31:41 2004
++++ script/qmrtgsync.sh	Sat Jun 12 02:32:03 2004
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/usr/local/bin/bash
+ 
+ CONFFILE=/usr/local/etc/qmrtg.conf
+ QMRTG_DBPATH=/tmp/qmrtg
