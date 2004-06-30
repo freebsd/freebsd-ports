1a27e9da10ec2c3d7950f47181dd5bb8533c1373
krion <krion@FreeBSD.org>
2004-06-30 07:14:28 +0000

    Update to 2.9
    PR:		ports/68491
    Submitted by:	maintainer

---
+++ dns/dnsmasq/files/dnsmasq.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+#
+#
+# PROVIDE: dnsmasq
+# REQUIRE: DAEMON NETWORKING SERVERS
+# KEYWORD: FreeBSD
+#
+# Add the following line to /etc/rc.conf to enable dnsmasq:
+#
+# dnsmasq_enable="YES"
+#
+
+# override these variables in /etc/rc.conf
+dnsmasq_enable=NO
+
+
+. /etc/rc.subr
+
+name=dnsmasq
+rcvar=$(set_rcvar)
+
+command=/usr/local/sbin/${name}
+pidfile=/var/run/${name}.pid
+required_files=${dnsmasq_conf}
+#start_precmd=${name}_precmd
+
+load_rc_config ${name}
+run_rc_command "$1"
+
