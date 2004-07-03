22625887e97a175ab6680edfba0e0ad3d985fd47
marcus <marcus@FreeBSD.org>
2004-07-03 22:44:28 +0000

    Convert this port to use RCng.
    Submitted by:	kuriyama

--- net/netatalk-devel/files/netatalk.sh
+++ net/netatalk-devel/files/netatalk.sh
@@ -1,37 +1,53 @@
 #!/bin/sh
 #
-# $FreeBSD: /tmp/pcvs/ports/net/netatalk-devel/files/Attic/netatalk.sh,v 1.2 2004-06-06 02:25:46 marcus Exp $
+# $FreeBSD: /tmp/pcvs/ports/net/netatalk-devel/files/Attic/netatalk.sh,v 1.3 2004-07-03 22:44:28 marcus Exp $
+#
+# PROVIDE: netatalk
+# REQUIRE: DAEMON
+# KEYWORD: FreeBSD
 #
 # AppleTalk daemons. Make sure not to start atalkd in the background:
 # its data structures must have time to stablize before running the
 # other processes.
 #
-if ! test -x %%PREFIX%%/sbin/atalkd
-then
-#	exit 0
-fi
-HOSTNAME=`hostname|sed 's/\..*$//'`
-case $1 in
-start)
-	echo -n ' netatalk'
-	%%PREFIX%%/sbin/atalkd
-	%%PREFIX%%/bin/nbprgstr -p 4 ${HOSTNAME}:Workstation &
-	%%PREFIX%%/bin/nbprgstr -p 4 ${HOSTNAME}:netatalk &
-	%%PREFIX%%/sbin/papd
-	%%PREFIX%%/sbin/cnid_metad
-	%%PREFIX%%/sbin/afpd -s %%PREFIX%%/etc/AppleVolumes.system \
-		-f %%PREFIX%%/etc/AppleVolumes.default
-	[ -x %%PREFIX%%/sbin/timelord ] && %%PREFIX%%/sbin/timelord
-	;;
-stop)
-	killall timelord
-	killall afpd
-	killall cnid_metad
-	killall papd
-	killall atalkd
-	;;
-*)
-	echo "Usage: $0: [ start | stop ]" 2>&1
-	exit 65
-	;;
-esac
+
+netatalk_enable="NO"
+atalkd_enable="NO"
+papd_enable="NO"
+cind_metad_enable="NO"
+afpd_enable="NO"
+timelord_enable="NO"
+
+. %%RC_SUBR%%
+
+name=netatalk
+rcvar=`set_rcvar`
+hostname=`hostname -s`
+
+start_cmd=netatalk_start
+stop_cmd=netatalk_stop
+
+netatalk_start() {
+    checkyesno atalkd_enable && %%PREFIX%%/sbin/atalkd
+    checkyesno atalkd_enable && \
+    	%%PREFIX%%/bin/nbprgstr -p 4 ${hostname}:Workstation &
+    checkyesno atalkd_enable && \
+    	%%PREFIX%%/bin/nbprgstr -p 4 ${hostname}:netatalk &
+    checkyesno papd_enable && %%PREFIX%%/sbin/papd
+    checkyesno cind_metad_enable && %%PREFIX%%/sbin/cnid_metad
+    checkyesno afpd_enable && \
+    	%%PREFIX%%/sbin/afpd -s %%PREFIX%%/etc/AppleVolumes.system \
+	-f %%PREFIX%%/etc/AppleVolumes.default
+    checkyesno timelord_enable && %%PREFIX%%/sbin/timelord
+}
+
+netatalk_stop() {
+    checkyesno timelord_enable && killall timelord
+    checkyesno afpd_enable && killall afpd
+    checkyesno cind_metad_enable && killall cnid_metad
+    checkyesno papd_enable && killall papd
+    checkyesno atalkd_enable && killall atalkd
+}
+
+load_rc_config ${name}
+run_rc_command "$1"
