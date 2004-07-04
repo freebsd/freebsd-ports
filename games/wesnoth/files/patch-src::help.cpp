8e9094ed3125f0fe73590af9012a0d4c652bc2aa
mezz <mezz@FreeBSD.org>
2004-07-04 02:19:30 +0000

    Update to 0.7.11.

---
+++ games/wesnoth/files/patch-src::help.cpp
@@ -0,0 +1,10 @@
+--- src/help.cpp.orig	Sat Jul  3 20:44:17 2004
++++ src/help.cpp	Sat Jul  3 20:44:25 2004
+@@ -27,7 +27,6 @@
+ #include <cassert>
+ #include <algorithm>
+ #include <iostream>
+-#include <locale>
+ #include <queue>
+ #include <set>
+ #include <stack>
