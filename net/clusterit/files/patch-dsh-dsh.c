6f20070f0924dbeffa7f36572aaec0ec1d19b5a1
Dmitry Sivachenko <demon@FreeBSD.org>
2004-07-06 08:57:49 +0000

    Fix build on -current.

--- net/clusterit/files/patch-dsh-dsh.c
+++ net/clusterit/files/patch-dsh-dsh.c
@@ -1,18 +1,34 @@
---- dsh/dsh.c.orig	Fri Jul  2 12:50:52 2004
-+++ dsh/dsh.c	Fri Jul  2 12:51:08 2004
-@@ -76,6 +76,7 @@
+--- dsh/dsh.c.orig	Tue Jul  6 12:49:37 2004
++++ dsh/dsh.c	Tue Jul  6 12:53:51 2004
+@@ -35,6 +35,7 @@
+ #include <sys/types.h>
+ #include <sys/resource.h>
+ #include <sys/wait.h>
++#include <sys/param.h>
+ 
+ #include <errno.h>
+ #include <fcntl.h>
+@@ -76,6 +77,11 @@
  {
  	extern char	*optarg;
  	extern int	optind;
++#if __FreeBSD_version < 500000
 +	extern char	*malloc_options;
++#else
++	extern const char	*_malloc_options;
++#endif
  
  	int someflag, ch, i, fanout, showflag, fanflag;
  	char *p, *q, *group, *nodename, *username;
-@@ -93,6 +94,7 @@
+@@ -93,6 +99,11 @@
  	nodeptr = NULL;
  	nodelink = NULL;
  
++#if __FreeBSD_version < 500000
 +	malloc_options = "Z";
++#else
++	_malloc_options = "Z";
++#endif
  	rungroup = malloc(sizeof(char **) * GROUP_MALLOC);
  	if (rungroup == NULL)
  		bailout(__LINE__);
