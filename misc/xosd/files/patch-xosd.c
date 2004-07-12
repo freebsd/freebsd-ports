8e2c2a43b1c534dcd578148b44c336be26827118
Hye-Shik Chang <perky@FreeBSD.org>
2004-07-12 01:56:31 +0000

    Update to 2.2.8 and unmark BROKEN.
    PR:		68906
    Submitted by:	Roman Bogorodskiy <bogorodskiy@inbox.ru>

--- misc/xosd/files/patch-xosd.c
+++ misc/xosd/files/patch-xosd.c
@@ -1,11 +1,11 @@
---- src/libxosd/xosd.c.orig	Fri Sep 26 09:25:42 2003
-+++ src/libxosd/xosd.c	Sun Sep 28 01:30:28 2003
-@@ -377,7 +377,7 @@
+--- src/libxosd/xosd.c.orig	Mon Jul  5 02:29:06 2004
++++ src/libxosd/xosd.c	Sun Jul 11 06:30:47 2004
+@@ -432,7 +432,7 @@
+   FUNCTION_START;
+   DEBUG("event thread started");
+   assert(osd);
+-  usleep(500);
++  usleep(100000);
  
-   DEBUG ("event thread started");
-   assert (osd);
--  usleep (500);
-+  usleep (100000);
- 
-   while (!osd->done)
-     {
+   while (!osd->done) {
+     // DEBUG("checking window event");
