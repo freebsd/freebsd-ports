e2ca6bea49eef37b38c7d98b702dd449492b4b8e
Kirill Ponomarev <krion@FreeBSD.org>
2004-07-13 08:33:57 +0000

    Add cal3d 0.9.1,
    Cal3D is a skeletal based 3D character animation library
    written in C++ in a way that is both platform-independent and
    graphics API-independent.  It was originally designed to be
    used in a 3D client for Worldforge, but evolved into a
    stand-alone product which can be used in many different kinds
    of projects.
    
    Cal3D's essentials can be boiled down to 2 parts: the C++
    library and the exporter. The exporter is what you would use to
    take your characters (built in a 3D modeling package) and
    create the Cal3D-format files that the library knows how to
    load. The exporters are actually plug-ins for 3D modeling
    packages. This allows 3D artists to use the modeling tools that
    they're already comfortable with.
    
    The C++ library is what you would actually use in your
    application, whether it's a game or a VR application. The
    library provides methods to load your exported files, build
    characters, run animations, and access the data necessary to
    render them with 3D graphics.
    
    PR:		ports/68954
    Submitted by:	Stefan Walter <sw@gegenunendlich.de>

---
+++ graphics/cal3d-devel/files/patch-src_cal3d_buffersource.h
@@ -0,0 +1,11 @@
+--- src/cal3d/buffersource.h.orig	Tue Jul 13 10:03:53 2004
++++ src/cal3d/buffersource.h	Tue Jul 13 10:04:11 2004
+@@ -17,7 +17,7 @@
+ 
+ #include "cal3d/global.h"
+ #include "cal3d/datasource.h"
+-#include <istream>
++#include <istream.h>
+ 
+ /**
+  * CalBufferSource class.
---
+++ graphics/cal3d-devel/files/patch-src_cal3d_loader.h
@@ -0,0 +1,11 @@
+--- src/cal3d/loader.h.orig	Tue Jul 13 10:04:57 2004
++++ src/cal3d/loader.h	Tue Jul 13 10:05:09 2004
+@@ -17,7 +17,7 @@
+ 
+ 
+ #include <string>
+-#include <istream>
++#include <istream.h>
+ #include "cal3d/global.h"
+ #include "cal3d/datasource.h"
+ 
