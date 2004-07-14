4b1df695ff9e519fd56457f6765504cf9884586b
pav <pav@FreeBSD.org>
2004-07-14 12:52:56 +0000

    - Fix build on recent -CURRENT which got trunc()

--- devel/py-game/files/patch-src-draw.c
+++ devel/py-game/files/patch-src-draw.c
@@ -1,20 +1,14 @@
 --- src/draw.c.orig	Thu Oct 16 08:13:22 2003
-+++ src/draw.c	Mon Nov 17 03:47:49 2003
-@@ -26,17 +26,12 @@
++++ src/draw.c	Wed Jul 14 14:20:17 2004
+@@ -25,9 +25,9 @@
+  */
  #include "pygame.h"
  #include <math.h>
++#include <sys/param.h>
  
 -#ifdef _MSC_VER
 -#pragma warning (disable:4244)
--
++#if __FreeBSD_version < 502119
+ 
  float trunc(float d)
  {
-     if (d >= 0)
-         return floor(d);
-     return ceil(d);
- }
--
--#endif
- 
- #define FRAC(z) (z-trunc(z))
- #define INVFRAC(z) (1-(z-trunc(z)))
