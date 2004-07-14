d12909dc3eaaef80fb2671e3c2e39d3a2700749c
Pav Lucistnik <pav@FreeBSD.org>
2004-07-14 15:46:55 +0000

    - Unset USE_GCC=2.95 and add patch to fix build with gcc 3.x - Respect CFLAGS - Portlint
    PR:		ports/69022
    Submitted by:	Andrey Slusar <vasallia@ukr.net>

--- mail/mailsync/files/patch-mailsync.c
+++ mail/mailsync/files/patch-mailsync.c
@@ -1,6 +1,14 @@
 --- mailsync.c.orig	Thu Nov  7 12:05:46 2002
-+++ mailsync.c	Mon Feb  3 22:59:09 2003
-@@ -30,8 +30,8 @@
++++ mailsync.c	Wed Jul 14 03:52:22 2004
+@@ -20,6 +20,7 @@
+ extern int errno;               // Just in case
+ #include <sys/stat.h>           // Stat()
+ 
++#include <cassert>
+ #include <string>
+ #include <set>
+ #include <map>
+@@ -30,8 +31,8 @@
  using std::vector;
  using std::make_pair;
  
