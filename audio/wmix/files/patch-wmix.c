079a18698cc5899903d077689afd8e908c18422a
Volker Stolz <vs@FreeBSD.org>
2004-07-15 11:45:37 +0000

    - Update to 3.1 - Install documentation - Assign maintership to submitter
    PR:		ports/69056
    Submitted by:	Roman Bogorodskiy

---
+++ audio/wmix/files/patch-wmix.c
@@ -0,0 +1,10 @@
+--- wmix.c.orig	Wed Jul 14 18:15:17 2004
++++ wmix.c	Wed Jul 14 18:15:26 2004
+@@ -24,7 +24,6 @@
+ #include <string.h>
+ #include <signal.h>
+ #include <string.h>
+-#include <getopt.h>
+ #include <unistd.h>
+ 
+ #include <X11/X.h>
