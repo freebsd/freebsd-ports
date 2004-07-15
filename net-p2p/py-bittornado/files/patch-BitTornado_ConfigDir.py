8f8fb5fa36081ed10877e954a1f77ad2274ace33
Jeremy Messenger <mezz@FreeBSD.org>
2004-07-15 21:25:51 +0000

    Update to 0.3.7.
    PR:		ports/69094
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu> (maintainer)

--- net-p2p/py-bittornado/files/patch-BitTornado_ConfigDir.py
+++ net-p2p/py-bittornado/files/patch-BitTornado_ConfigDir.py
@@ -1,13 +1,13 @@
 
 $FreeBSD$
 
---- BitTornado/ConfigDir.py.orig	Tue May 25 09:55:49 2004
-+++ BitTornado/ConfigDir.py	Tue May 25 09:56:59 2004
-@@ -15,7 +15,7 @@
-     True = 1
-     False = 0
- 
--OLDICONPATH = os.path.abspath(os.path.dirname(os.path.realpath(sys.argv[0])))
+--- BitTornado/ConfigDir.py.orig	Sun Jul 11 04:57:30 2004
++++ BitTornado/ConfigDir.py	Thu Jul 15 12:03:50 2004
+@@ -19,7 +19,7 @@
+     realpath = os.path.realpath
+ except:
+     realpath = lambda x:x
+-OLDICONPATH = os.path.abspath(os.path.dirname(realpath(sys.argv[0])))
 +OLDICONPATH="%%PREFIX%%/share/BitTornado"
  
  DIRNAME = '.'+product_name
