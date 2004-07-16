37980855a665d79c2f64ce5ca1ced0c852a4a474
Sergey Matveychuk <sem@FreeBSD.org>
2004-07-16 03:19:43 +0000

    Update to 0.7
    PR:		ports/69099
    Submitted by:	Andrej Zverev <az@inec.ru>
    Approved by:	krion (mentor)

---
+++ databases/hk_classes/files/patch-hk_classes::hk_database.h
@@ -0,0 +1,10 @@
+--- hk_classes/hk_database.h.orig	Thu Jul 15 22:02:22 2004
++++ hk_classes/hk_database.h	Thu Jul 15 22:02:36 2004
+@@ -14,6 +14,7 @@
+ #include <list>
+ #include <vector>
+ #include <algorithm>
++#include <sstream>
+ #include "hk_presentation.h"
+ using namespace std;
+ 
