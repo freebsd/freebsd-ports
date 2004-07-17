e66b22a8851ece304130ede1b7642248785eb116
pav <pav@FreeBSD.org>
2004-07-17 12:26:09 +0000

    - Fix crasher in pure-pw
    PR:		ports/69098
    Submitted by:	Xin LI <delphij@frontfree.net>
    Approved by:	maintainer

--- ftp/pure-ftpd/files/patch-src_bsd-getopt_long.c
+++ ftp/pure-ftpd/files/patch-src_bsd-getopt_long.c
@@ -1,11 +1,21 @@
---- src/bsd-getopt_long.c.orig  Sun Jun 20 19:29:34 2004
-+++ src/bsd-getopt_long.c       Tue Jun 22 15:21:09 2004
-@@ -472,6 +472,8 @@
-                      *nargv[pure_optind + 1] != '-') {
-                      pure_optarg = nargv[++pure_optind];
-                  }
+--- src/bsd-getopt_long.c.orig	Mon Jun 21 01:29:56 2004
++++ src/bsd-getopt_long.c	Thu Jul 15 22:03:34 2004
+@@ -462,16 +462,8 @@
+                     fprintf(stderr, recargchar, optchar);
+                 pure_optopt = optchar;
+                 return BADARG;
+-            } else if (!(flags & FLAG_PERMUTE)) {
+-                /* 
+-                 * If permutation is disabled, we can accept an
+-                 * optional arg separated by whitespace so long
+-                 * as it does not start with a dash (-).
+-                 */
+-                 if (pure_optind + 1 < nargc && pure_optind + 1 > 0 &&
+-                     *nargv[pure_optind + 1] != '-') {
+-                     pure_optarg = nargv[++pure_optind];
+-                 }
 +            } else {
-+                    pure_optarg = nargv[pure_optind];       
++                pure_optarg = nargv[++pure_optind];
              }
          }
          pure_place = EMSG;
