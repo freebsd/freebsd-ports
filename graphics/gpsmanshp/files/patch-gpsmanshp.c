1316c7895c9931f2bade0a72ea0a0b455339a778
jhay <jhay@FreeBSD.org>
2004-07-18 19:16:03 +0000

    Initial import of gpsmanshp 1.2
    Provides Tcl/Tk shapelib file support for gpsman.

---
+++ graphics/gpsmanshp/files/patch-gpsmanshp.c
@@ -0,0 +1,11 @@
+--- gpsmanshp.c.org	Mon Jan 26 14:10:32 2004
++++ gpsmanshp.c	Wed Jul  7 20:53:40 2004
+@@ -65,7 +65,7 @@
+ 
+ #include <stdlib.h>
+ #include <string.h>
+-#include <libshp/shapefil.h>
++#include <shapefil.h>
+ #include <tcl.h>
+ 
+ #define VERSION "1.1"
