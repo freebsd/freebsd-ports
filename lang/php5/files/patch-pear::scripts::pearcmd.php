af7ee2a6ac8dc18d5a1df76a8d9e06e4a1657ed8
Alex Dupre <ale@FreeBSD.org>
2004-07-19 07:05:36 +0000

    First step of the PHP mega-commit: - remove extensions and PEAR from the PHP base ports - add more OPTIONS to PHP base ports

--- lang/php5/files/patch-pear::scripts::pearcmd.php
+++ lang/php5/files/patch-pear::scripts::pearcmd.php
@@ -1,18 +1,20 @@
---- pear/scripts/pearcmd.php.orig	Fri May 16 17:31:37 2003
-+++ pear/scripts/pearcmd.php	Fri May 16 17:36:10 2003
+--- pear/scripts/pearcmd.php.orig	Sun Jun  6 17:52:41 2004
++++ pear/scripts/pearcmd.php	Mon Jul 12 16:22:50 2004
 @@ -1,3 +1,4 @@
-+#!%%PREFIX%%/bin/php -n -dsafe_mode=0 -doutput_buffering=1
++#!%%PREFIX%%/bin/php -n -q -dsafe_mode=0 -doutput_buffering=1
  <?php
  //
  // +----------------------------------------------------------------------+
-@@ -24,9 +25,7 @@
+@@ -24,9 +25,9 @@
  /**
   * @nodep Gtk
   */
 -if ('@include_path@' != '@'.'include_path'.'@') {
 -    ini_set('include_path', '@include_path@');
 -}
++dl('pcre.so');
++dl('xml.so');
 +ini_set('include_path', '%%PREFIX%%/share/pear:%%PREFIX%%/share/pear/bootstrap');
  ini_set('allow_url_fopen', true);
- set_time_limit(0);
- ob_implicit_flush(true);
+ if (!ini_get('safe_mode')) {
+     set_time_limit(0);
