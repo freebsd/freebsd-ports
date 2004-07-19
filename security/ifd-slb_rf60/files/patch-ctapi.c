bb900f8ce57b2df1671b39999652934e5b2764d4
edwin <edwin@FreeBSD.org>
2004-07-19 06:04:16 +0000

    New port: devel/slb_rf60
    	This is a pcsc-lite driver for the Schlumberger Reflex 62/64
    	serial smartcard reader
    
    	This port should be filed under "security/ifd-slb_rf60"
    	because there are already some other pcsc-lite drivers
    	follow this convention.
    
    	I also made a few minor changes because of portlint warnings,
    	so please apply this patch and rename the directory to
    	"ifd-slb_rf60" before committing.
    
    PR:		ports/63120
    Submitted by:	Toni Andjelkovic <toni@soth.at>

---
+++ security/ifd-slb_rf60/files/patch-ctapi.c
@@ -0,0 +1,38 @@
+--- ctapi.c.bak	Fri Feb 20 05:40:24 2004
++++ ctapi.c	Fri Feb 20 12:32:47 2004
+@@ -29,7 +29,7 @@
+     #endif
+ 
+     #ifndef SUN_SPARC
+-     BretVal = IO_InitializePort(9600,8,'E',"/dev/ttyS0");
++     BretVal = IO_InitializePort(9600,8,'E',"/dev/cuaa0");
+     #endif
+ 
+     break;
+@@ -41,7 +41,7 @@
+     #endif
+ 
+     #ifndef SUN_SPARC
+-     BretVal = IO_InitializePort(9600,8,'E',"/dev/ttyS1");
++     BretVal = IO_InitializePort(9600,8,'E',"/dev/cuaa1");
+     #endif
+ 
+     break;
+@@ -53,7 +53,7 @@
+     #endif
+ 
+     #ifndef SUN_SPARC
+-     BretVal = IO_InitializePort(9600,8,'E',"/dev/ttyS2");
++     BretVal = IO_InitializePort(9600,8,'E',"/dev/cuaa2");
+     #endif
+ 
+     break;
+@@ -65,7 +65,7 @@
+     #endif
+ 
+     #ifndef SUN_SPARC
+-     BretVal = IO_InitializePort(9600,8,'E',"/dev/ttyS3");
++     BretVal = IO_InitializePort(9600,8,'E',"/dev/cuaa3");
+     #endif
+ 
+     break;
---
+++ security/ifd-slb_rf60/files/patch-serial.h
@@ -0,0 +1,10 @@
+--- serial.h.bak	Fri Feb 20 07:26:31 2004
++++ serial.h	Fri Feb 20 07:26:45 2004
+@@ -65,6 +65,6 @@
+ 	void
+ );
+ 
+-#define DATA_DUMP_FILE "dump"
++/* #define DATA_DUMP_FILE "dump" */
+ 
+ #endif
