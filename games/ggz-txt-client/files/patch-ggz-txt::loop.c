974048e9660e355fa58a22e2d0f4fb881336a167
Kirill Ponomarev <krion@FreeBSD.org>
2004-07-19 18:02:38 +0000

    Update to version 0.0.9
    PR:		ports/69295
    Submitted by:	Ports Fury

---
+++ games/ggz-txt-client/files/patch-ggz-txt::loop.c
@@ -0,0 +1,10 @@
+--- ggz-txt/loop.c.orig	Mon May 17 23:20:01 2004
++++ ggz-txt/loop.c	Sun Jul 18 20:25:18 2004
+@@ -32,6 +32,7 @@
+ #include <ggz.h>
+ #include <unistd.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <sys/time.h>
+ #include <sys/types.h>
+ 
