2f8ad8c8d03ac9d6efd1a131600f2144a0ed4390
adamw <adamw@FreeBSD.org>
2004-07-20 00:36:24 +0000

    Fix build with mozilla-devel. All the work for this was done by marcus.

--- www/epiphany-extensions/files/patch-extensions_popup-blocker_mozilla_mozilla-helpers.cpp
+++ www/epiphany-extensions/files/patch-extensions_popup-blocker_mozilla_mozilla-helpers.cpp
@@ -1,5 +1,5 @@
---- extensions/popup-blocker/mozilla/mozilla-helpers.cpp.orig	Wed Jul  7 15:55:52 2004
-+++ extensions/popup-blocker/mozilla/mozilla-helpers.cpp	Wed Jul  7 15:57:58 2004
+--- extensions/popup-blocker/mozilla/mozilla-helpers.cpp.orig	Mon Mar 15 17:07:05 2004
++++ extensions/popup-blocker/mozilla/mozilla-helpers.cpp	Mon Jul 19 20:33:33 2004
 @@ -65,12 +65,16 @@
  	nsCOMPtr<nsPIDOMWindow> piWin(do_QueryInterface (domWindow, &rv));
  	NS_ENSURE_SUCCESS (rv, NULL);
@@ -17,3 +17,15 @@
  	NS_ENSURE_SUCCESS (rv, NULL);
  
  	nsCOMPtr<nsIDOMEventTarget> target;
+@@ -181,7 +185,11 @@
+ 	g_free (script);
+ 
+ 	PRBool isUndefined;
++#if MOZILLA_SNAPSHOT < 18
+ 	nsAutoString ret;
++#else
++	nsAString *ret;
++#endif
+ 	context->EvaluateString (aScript, nsnull, nsnull, nsnull, 0,
+ 				 nsnull, ret, &isUndefined);
+ 
