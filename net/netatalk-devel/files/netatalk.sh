59dec445d1542b0ba1c40e3389c7a4e0153dab0f
marcus <marcus@FreeBSD.org>
2004-07-21 19:56:30 +0000

    * Properly setup rc.d default macros [1] * Correct a typo (cind_metad_enable should be cnid_metad_enable)
    Reminded by:	eik [1]

--- net/netatalk-devel/files/netatalk.sh
+++ net/netatalk-devel/files/netatalk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: /tmp/pcvs/ports/net/netatalk-devel/files/Attic/netatalk.sh,v 1.3 2004-07-03 22:44:28 marcus Exp $
+# $FreeBSD: /tmp/pcvs/ports/net/netatalk-devel/files/Attic/netatalk.sh,v 1.4 2004-07-21 19:56:30 marcus Exp $
 #
 # PROVIDE: netatalk
 # REQUIRE: DAEMON
@@ -10,13 +10,6 @@
 # its data structures must have time to stablize before running the
 # other processes.
 #
-
-netatalk_enable="NO"
-atalkd_enable="NO"
-papd_enable="NO"
-cind_metad_enable="NO"
-afpd_enable="NO"
-timelord_enable="NO"
 
 . %%RC_SUBR%%
 
@@ -34,7 +27,7 @@ netatalk_start() {
     checkyesno atalkd_enable && \
     	%%PREFIX%%/bin/nbprgstr -p 4 ${hostname}:netatalk &
     checkyesno papd_enable && %%PREFIX%%/sbin/papd
-    checkyesno cind_metad_enable && %%PREFIX%%/sbin/cnid_metad
+    checkyesno cnid_metad_enable && %%PREFIX%%/sbin/cnid_metad
     checkyesno afpd_enable && \
     	%%PREFIX%%/sbin/afpd -s %%PREFIX%%/etc/AppleVolumes.system \
 	-f %%PREFIX%%/etc/AppleVolumes.default
@@ -44,10 +37,17 @@ netatalk_start() {
 netatalk_stop() {
     checkyesno timelord_enable && killall timelord
     checkyesno afpd_enable && killall afpd
-    checkyesno cind_metad_enable && killall cnid_metad
+    checkyesno cnid_metad_enable && killall cnid_metad
     checkyesno papd_enable && killall papd
     checkyesno atalkd_enable && killall atalkd
 }
+
+[ -z "$netatalk_enable" ]	&& netatalk_enable="NO"
+[ -z "$atalkd_enable" ]		&& atalkd_enable="NO"
+[ -z "$papd_enable" ]		&& papd_enable="NO"
+[ -z "$cnid_metad_enable" ]	&& cnid_metad_enable="NO"
+[ -z "$afpd_enable" ]		&& afpd_enable="NO"
+[ -z "$timelord_enable" ]	&& timelord_enable="NO"
 
 load_rc_config ${name}
 run_rc_command "$1"
