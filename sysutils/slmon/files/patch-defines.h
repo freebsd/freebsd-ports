76fb6bb2b87bcf841b9b107133413158923464c5
Volker Stolz <vs@FreeBSD.org>
2004-07-21 12:21:16 +0000

    Patch for libgtop2, unDEPRECATE.

--- sysutils/slmon/files/patch-defines.h
+++ sysutils/slmon/files/patch-defines.h
@@ -1,6 +1,6 @@
---- defines.h.orig	Tue Nov  6 14:33:50 2001
-+++ defines.h	Tue Nov  6 14:34:20 2001
-@@ -17,6 +17,7 @@
+--- defines.h.orig	Wed Sep 11 19:44:03 2002
++++ defines.h	Wed Jul 21 14:12:16 2004
+@@ -21,6 +21,7 @@
  #ifndef SLMON_DEFINES_H
  #define SLMON_DEFINES_H
  
@@ -8,3 +8,11 @@
  #include <stdlib.h>
  #include <time.h>
  #include <string.h>
+@@ -47,7 +48,6 @@
+ #include <glibtop/procmap.h>
+ #include <glibtop/mountlist.h>
+ #include <glibtop/fsusage.h>
+-#include <glibtop/xmalloc.h>
+ 
+ #ifndef PROFILE_COUNT
+ #define PROFILE_COUNT   1000
