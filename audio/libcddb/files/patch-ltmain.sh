b846af5b2f887076a18db9bf69278c570a8ac6ec
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-07-22 01:10:30 +0000

    upgrade to 0.9.5
    PR:		69393
    Submitted by:	maintainer

---
+++ audio/libcddb/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Wed Jul 21 15:43:15 2004
++++ ltmain.sh	Wed Jul 21 15:43:45 2004
+@@ -5608,10 +5608,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
