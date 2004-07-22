a50e71b628cb74200cfbaa30b186c9c8d3303798
eik <eik@FreeBSD.org>
2004-07-22 12:15:28 +0000

    update of net/spread and net/spread-j to version 3.17.2
    PR:		69095
    Submitted by:	Joshua Goodall <joshua@roughtrade.net> (maintainer)

--- net/spread/files/patch-auth-ip.c
+++ net/spread/files/patch-auth-ip.c
@@ -1,17 +1,17 @@
---- auth-ip.c	Sun Sep 22 12:56:52 2002
-+++ auth-ip.c	Tue Aug  5 21:27:40 2003
+--- auth-ip.c	Fri Mar  5 11:32:46 2004
++++ auth-ip.c	Thu Jul 15 21:38:09 2004
 @@ -90,12 +90,9 @@
      /* load spread.access_ip file */
      if (NULL != (fp = fopen(file_name,"r")) )
-         Alarm( PRINT, "ip_init: using file: %s\n", file_name);
+         Alarmp( SPLOG_INFO, ACM, "ip_init: using file: %s\n", file_name);
 -    if (fp == NULL) 
 -        if (NULL != (fp = fopen("./spread.access_ip", "r")) )
--            Alarm( PRINT, "ip_init: using file: ./spread.access_ip\n");
+-            Alarmp( SPLOG_INFO, ACM, "ip_init: using file: ./spread.access_ip\n");
      if (fp == NULL)
 -        if (NULL != (fp = fopen("/etc/spread.access_ip", "r")) )
--            Alarm( PRINT, "ip_init: using file: /etc/spread.access_ip\n");
+-            Alarmp( SPLOG_INFO, ACM, "ip_init: using file: /etc/spread.access_ip\n");
 +        if (NULL != (fp = fopen(SPREAD_ETCDIR "/spread.access_ip", "r")) )
-+            Alarm( PRINT, "ip_init: using file: " SPREAD_ETCDIR "/spread.access_ip\n");
++            Alarmp( SPLOG_INFO, ACM, "ip_init: using file: " SPREAD_ETCDIR "/spread.access_ip\n");
      if (fp == NULL)
      {
-         Alarm( PRINT, "ip_init: IP access control file not found.\nIf you are using IP based access controls, please make sure the file exists.\n");
+         Alarmp( SPLOG_ERROR, ACM, "ip_init: IP access control file not found.\nIf you are using IP based access controls, please make sure the file exists.\n");
