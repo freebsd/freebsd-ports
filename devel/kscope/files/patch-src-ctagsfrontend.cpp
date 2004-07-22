6b1db8924f48f2f5bee5d048c0e3066eeed8987a
Mark Linimon <linimon@FreeBSD.org>
2004-07-22 05:58:30 +0000

    Update to 0.7.
    PR:		ports/68874
    Submitted by:	Frank Mayhar <frank at exit dot com> (maintainer)

--- devel/kscope/files/patch-src-ctagsfrontend.cpp
+++ devel/kscope/files/patch-src-ctagsfrontend.cpp
@@ -1,11 +1,11 @@
---- /homes/fmayhar/kscope-0.5/src/ctagsfrontend.cpp	Thu Dec 18 20:58:30 2003
-+++ src/ctagsfrontend.cpp	Wed Apr 21 13:01:45 2004
+--- src/ctagsfrontend.cpp.orig	Fri Jul  9 11:03:47 2004
++++ src/ctagsfrontend.cpp	Fri Jul  9 11:04:02 2004
 @@ -81,7 +81,7 @@
  	QFileInfo fi(sPath);
  
  	if (!fi.exists() || !fi.isFile() || !fi.isExecutable() ||
--		fi.fileName() != "ctags") {
-+		fi.fileName() != "exctags") {
+-		fi.fileName().left(5) != "ctags") {
++		fi.fileName().left(7) != "exctags") {
  		KMessageBox::error(0, i18n("Ctags cannot be found in the given "
  			"path"));
  		return false;
