0bf587a1d16b4667487fd6053014ce2f25151228
Oliver Eikemeier <eik@FreeBSD.org>
2004-07-22 12:15:28 +0000

    update of net/spread and net/spread-j to version 3.17.2
    PR:		69095
    Submitted by:	Joshua Goodall <joshua@roughtrade.net> (maintainer)

--- net/spread/files/patch-auth-pword.c
+++ net/spread/files/patch-auth-pword.c
@@ -1,17 +1,17 @@
---- auth-pword.c	Sun Sep 22 12:56:52 2002
-+++ auth-pword.c	Tue Aug  5 21:28:50 2003
+--- auth-pword.c	Fri Mar  5 11:32:46 2004
++++ auth-pword.c	Thu Jul 15 21:40:16 2004
 @@ -118,12 +118,9 @@
      /* load spread.access_ip file */
      if (NULL != (fp = fopen(file_name,"r")) )
-         Alarm( PRINT, "pword_init: using file: %s\n", file_name);
+         Alarmp( SPLOG_INFO, ACM, "pword_init: using file: %s\n", file_name);
 -    if (fp == NULL) 
 -        if (NULL != (fp = fopen("./spread.access_pword", "r")) )
--            Alarm( PRINT, "pword_init: using file: ./spread.access_pword\n");
+-            Alarmp( SPLOG_INFO,ACM, "pword_init: using file: ./spread.access_pword\n");
      if (fp == NULL)
 -        if (NULL != (fp = fopen("/etc/spread.access_pword", "r")) )
--            Alarm( PRINT, "pword_init: using file: /etc/spread.access_pword\n");
+-            Alarmp( SPLOG_INFO, ACM, "pword_init: using file: /etc/spread.access_pword\n");
 +        if (NULL != (fp = fopen(SPREAD_ETCDIR "/spread.access_pword", "r")) )
-+            Alarm( PRINT, "pword_init: using file: " SPREAD_ETCDIR "/spread.access_pword\n");
++            Alarmp( SPLOG_INFO, ACM, "pword_init: using file: " SPREAD_ETCDIR "/spread.access_pword\n");
      if (fp == NULL)
-         Alarm( EXIT, "pword_init: error opening config file %s in any of the standard locations. Please make sure the file exists\n", file_name);
+         Alarmp( SPLOG_FATAL, ACM, "pword_init: error opening config file %s in any of the standard locations. Please make sure the file exists\n", file_name);
  
