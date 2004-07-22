52e0b644922f9e7aa69f886b56285126d12529a7
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-07-22 04:19:47 +0000

    upgrade to 0.9.3 grab maintainership
    PR:		69405
    Submitted by:	Michael Johnson <ahze@ahze.net>

---
+++ multimedia/libquicktime/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Wed Jul 21 23:06:52 2004
++++ ltmain.sh	Wed Jul 21 23:07:10 2004
+@@ -5428,10 +5428,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
