ec6a2c4c040fccd0feb8fa7cd02738486122afdf
Kirill Ponomarev <krion@FreeBSD.org>
2004-07-23 07:18:36 +0000

    Update to 1.2.1
    PR:		ports/69418
    Submitted by:	Evgueni V. Gavrilov <aquatique-ports@rambler.ru>
    Approved by:	maintainer

--- net-p2p/lopster/files/patch-src::global.c
+++ net-p2p/lopster/files/patch-src::global.c
@@ -1,11 +1,11 @@
---- src/global.c.orig	Sun Nov  4 13:30:19 2001
-+++ src/global.c	Sat Jan 26 22:15:50 2002
-@@ -3096,7 +3096,7 @@
+--- src/global.c.orig	Sat Jul  3 19:19:19 2004
++++ src/global.c	Thu Jul 22 14:03:01 2004
+@@ -3588,7 +3588,7 @@
    global.allowed_ports = NULL;
  
    global.auto_save = NULL;
--  global.ping_command = l_strdup("/bin/ping -c 3 $IP");
-+  global.ping_command = l_strdup("/sbin/ping -c 3 $IP");
+-  global.ping_command = g_strdup("/bin/ping -c 3 $IP");
++  global.ping_command = g_strdup("/sbin/ping -c 3 $IP");
  
    global.browse_width[0] = 369;
    global.browse_width[1] = 80;
