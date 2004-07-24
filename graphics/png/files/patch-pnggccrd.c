61782ca833c91c6a724f62cf655965ea7b0ef8d7
Andrey A. Chernov <ache@FreeBSD.org>
2004-07-24 10:12:36 +0000

    Fix compiling with gcc 3.4
    Submitted by:   Andreas Kohn <andreas@syndrom23.de>

--- graphics/png/files/patch-pnggccrd.c
+++ graphics/png/files/patch-pnggccrd.c
@@ -1,5 +1,5 @@
---- pnggccrd.c.orig	Thu Dec  4 15:52:22 2003
-+++ pnggccrd.c	Thu Dec  4 16:00:18 2003
+--- pnggccrd.c.old	Thu Oct  3 15:32:28 2002
++++ pnggccrd.c	Sat Jul 24 14:07:13 2004
 @@ -1767,7 +1767,8 @@
  
                          : "1" (sptr),      // esi      // input regs
@@ -31,3 +31,25 @@
  
  #if 0  /* %mm0, ..., %mm3 not supported by gcc 2.7.2.3 or egcs 1.1 */
                             : "%mm0", "%mm1"               // clobber list
+@@ -4529,8 +4533,7 @@
+       : "0" (bpp),              // eax    // input regs
+         "1" (row)               // edi
+ 
+-      : "%ebx", "%ecx", "%edx"            // clobber list
+-      , "%esi"
++      : "%esi", "%ecx", "%edx"            // clobber list
+ 
+ #if 0  /* MMX regs (%mm0, etc.) not supported by gcc 2.7.2.3 or egcs 1.1 */
+       , "%mm0", "%mm1", "%mm2", "%mm3"
+@@ -5034,7 +5037,10 @@
+         "1" (prev_row),         // esi
+         "2" (row)               // edi
+ 
+-      : "%eax", "%ebx", "%ecx"            // clobber list (no input regs!)
++      : "%eax", "%ecx"            // clobber list (no input regs!)
++#ifndef __PIC__
++      , "%ebx"
++#endif
+ 
+ #if 0  /* MMX regs (%mm0, etc.) not supported by gcc 2.7.2.3 or egcs 1.1 */
+       , "%mm0", "%mm1", "%mm2", "%mm3"
