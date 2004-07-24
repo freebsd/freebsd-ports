1a787fbfd5abdc8a564596ecb978a571343009c9
marcus <marcus@FreeBSD.org>
2004-07-24 19:53:33 +0000

    Hack the default stack size for threads created with g_thread_create() to be 256 KB instead of the default (64 KB on platforms other than ia64, and 256 KB on ia64).
    This fixes some stack overflows seen in applications such as
    gnome-cups-manager.

---
+++ devel/glib20/files/patch-glib_gthread.h
@@ -0,0 +1,11 @@
+--- gthread.h.orig	Sat Jul 24 15:31:32 2004
++++ gthread.h	Sat Jul 24 15:32:10 2004
+@@ -202,7 +202,7 @@
+ #define g_thread_yield()              G_THREAD_CF (thread_yield, (void)0, ())
+ 
+ #define g_thread_create(func, data, joinable, error)			\
+-  (g_thread_create_full (func, data, 0, joinable, FALSE, 		\
++  (g_thread_create_full (func, data, 1024*256, joinable, FALSE, 		\
+                          G_THREAD_PRIORITY_NORMAL, error))
+ 
+ GThread* g_thread_create_full  (GThreadFunc            func,
