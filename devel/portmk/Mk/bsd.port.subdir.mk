d003f0073df58446fb82d97153316305a8fc843b
Oliver Eikemeier <eik@FreeBSD.org>
2004-07-24 08:58:50 +0000

    sync with latest b.p.m

--- devel/portmk/Mk/bsd.port.subdir.mk
+++ devel/portmk/Mk/bsd.port.subdir.mk
@@ -1,8 +1,3 @@
 # $FreeBSD$
 
-# local customization of the ports tree
-.if exists(${.CURDIR}/Makefile.local)
-.include "${.CURDIR}/Makefile.local"
-.endif
-
 .include "${PORTSDIR}/Mk/bsd.port.subdir.mk"
