3484ad47dfd8f279bf0e58f3c0ebbf4d65b2c39c
adamw <adamw@FreeBSD.org>
2004-07-25 07:21:34 +0000

    Fix build on 4.x.

--- www/seamonkey2/files/extra-patch-nsSVGLibartGlyphMetricsFT.cpp
+++ www/seamonkey2/files/extra-patch-nsSVGLibartGlyphMetricsFT.cpp
@@ -1,10 +1,9 @@
---- layout/svg/renderer/src/libart/nsSVGLibartGlyphMetricsFT.cpp.orig	Thu Feb 26 02:13:35 2004
-+++ layout/svg/renderer/src/libart/nsSVGLibartGlyphMetricsFT.cpp	Thu Feb 26 03:27:56 2004
-@@ -56,6 +57,13 @@
+--- layout/svg/renderer/src/libart/nsSVGLibartGlyphMetricsFT.cpp.orig	Sat Jul 24 23:59:16 2004
++++ layout/svg/renderer/src/libart/nsSVGLibartGlyphMetricsFT.cpp	Sun Jul 25 00:04:20 2004
+@@ -56,6 +56,12 @@
  #include "nsArray.h"
  #include "nsDataHashtable.h"
  
-+
 +#ifdef HAVE_CPP_2BYTE_WCHAR_T
 +  typedef nsDependentString nsLiteralString;
 +#else
@@ -14,30 +13,30 @@
  /**
   * \addtogroup libart_renderer Libart Rendering Engine
   * @{
-@@ -112,7 +120,7 @@
+@@ -112,7 +118,7 @@
    nsCOMPtr<nsISVGGlyphMetricsSource> mSource;
      
  public:
--  static nsDataHashtable<nsStringHashKey,nsDependentString*> sFontAliases;  
-+  static nsDataHashtable<nsStringHashKey,nsLiteralString*> sFontAliases;  
+-  static nsDataHashtable<nsStringHashKey,const nsDependentString*> sFontAliases;  
++  static nsDataHashtable<nsStringHashKey,const nsLiteralString*> sFontAliases;  
  };
  
  /** @} */
-@@ -120,7 +128,7 @@
+@@ -120,7 +126,7 @@
  //----------------------------------------------------------------------
  // nsSVGLibartGlyphMetricsFT implementation:
  
--nsDataHashtable<nsStringHashKey,nsDependentString*>
-+nsDataHashtable<nsStringHashKey,nsLiteralString*>
+-nsDataHashtable<nsStringHashKey,const nsDependentString*>
++nsDataHashtable<nsStringHashKey,const nsLiteralString*>
  nsSVGLibartGlyphMetricsFT::sFontAliases;
  
  
-@@ -406,7 +414,7 @@
+@@ -406,7 +412,7 @@
    }
    else {
      // try alias if there is one:
--    nsDependentString *alias = nsnull;
-+    nsLiteralString *alias = nsnull;
+-    const nsDependentString *alias = nsnull;
++    const nsLiteralString *alias = nsnull;
      nsSVGLibartGlyphMetricsFT::sFontAliases.Get(NS_ConvertUTF8toUCS2(family_name),
                                                  &alias);
      if (alias) {
