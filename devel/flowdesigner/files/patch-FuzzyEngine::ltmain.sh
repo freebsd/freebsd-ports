d3398573462d0cafd4b9b833b510852a0527121d
krion <krion@FreeBSD.org>
2004-07-25 16:23:44 +0000

    Update to version 0.8.1
    PR:		ports/69581
    Submitted by:	Ports Fury

---
+++ devel/flowdesigner/files/patch-FuzzyEngine::ltmain.sh
@@ -0,0 +1,15 @@
+--- FuzzyEngine/ltmain.sh.orig	Mon May 31 21:01:56 2004
++++ FuzzyEngine/ltmain.sh	Sat Jul 24 12:08:43 2004
+@@ -5550,10 +5550,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
