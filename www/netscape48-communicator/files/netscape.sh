3f64c3f68a51638b77375eb04c4735167ab3cb08
vs <vs@FreeBSD.org>
2004-07-25 10:37:34 +0000

    Add path to Acroread5-plugin for auto-detection
    PR:		ports/33320
    Submitted by:	John Merryweather Cooper
    Approved by:	maintainer timeout

--- www/netscape48-communicator/files/netscape.sh
+++ www/netscape48-communicator/files/netscape.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export MOZILLA_HOME; MOZILLA_HOME=${MOZILLA_HOME:=@PREFIX@/@NSUBDIR@}
-export NPX_PLUGIN_PATH; NPX_PLUGIN_PATH=${MOZILLA_HOME}/plugins
+export NPX_PLUGIN_PATH; NPX_PLUGIN_PATH=${MOZILLA_HOME}/plugins:@LOCALBASE@/Acrobat5/Browsers/intellinux/
 export CLASSPATH ; CLASSPATH=.:$MOZILLA_HOME
 export XCMSDB; XCMSDB=/dev/null
 ulimit -c 0
