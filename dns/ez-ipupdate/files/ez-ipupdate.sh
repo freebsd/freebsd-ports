8bf20a51e315f83676ab8a3844f17bc2ef9163bc
se <se@FreeBSD.org>
2004-07-26 23:13:03 +0000

    Install startup script with  expanded.

--- dns/ez-ipupdate/files/ez-ipupdate.sh
+++ dns/ez-ipupdate/files/ez-ipupdate.sh
@@ -13,22 +13,18 @@
 #ez_ipupdate_enable="YES"
 #
 
-. /etc/rc.subr
+. %%RC_SUBR%%
 
-if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/${0##*/}\$"); then
-    echo "$0: Cannot determine the PREFIX" >&2
-    exit 1
-fi
-
-name="ez_ipupdate"
+name=ez_ipupdate
 rcvar=`set_rcvar`
-command="$PREFIX/bin/ez-ipupdate"
-pidfile="/var/run/ez-ipupdate.pid"
-required_files="$PREFIX/etc/ez-ipupdate.conf"
+
+command=%%PREFIX%%/bin/ez-ipupdate
+pidfile=/var/run/ez-ipupdate.pid
+required_files=%%PREFIX%%/etc/ez-ipupdate.conf
 sig_stop="QUIT"
 
 ez_ipupdate_enable="NO"
-ez_ipupdate_flags="-c $PREFIX/etc/ez-ipupdate.conf -d -F $pidfile"
+ez_ipupdate_flags="-c %%PREFIX%%/etc/ez-ipupdate.conf -d -F $pidfile"
 
 load_rc_config $name
 run_rc_command "$1"
