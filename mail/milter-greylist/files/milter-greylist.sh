a0662a341d5569e08ff5214f1bc6f5ca8e3e1918
Pav Lucistnik <pav@FreeBSD.org>
2004-07-29 20:07:03 +0000

    - Update rc script to conform latest localpkg changes
    PR:		ports/69698
    Submitted by:	Cyril Guibourg <aragorn+ports@teaser.fr> (maintainer)

--- mail/milter-greylist/files/milter-greylist.sh
+++ mail/milter-greylist/files/milter-greylist.sh
@@ -8,18 +8,18 @@
 
 # Define these miltergreylist_* variables in one of these files:
 #       /etc/rc.conf
-#       /etc/rc.conf.miltergreylist
+#       /etc/rc.conf.local
 #       /etc/rc.conf.d/miltergreylist
 #
 # DO NOT CHANGE THESE DEFAULT VALUES HERE
 #
-miltergreylist_enable="NO"
-miltergreylist_runas="smmsp"
-miltergreylist_pidfile="/var/run/milter-greylist.pid"
-miltergreylist_sockfile="/var/milter-greylist/milter-greylist.sock"
-miltergreylist_cfgfile="%%PREFIX%%/etc/mail/greylist.conf"
-miltergreylist_flags="-P $miltergreylist_pidfile -f $miltergreylist_cfgfile
- -p $miltergreylist_sockfile -u $miltergreylist_runas"
+miltergreylist_enable=${miltergreylist_enable-"NO"}
+miltergreylist_runas=${miltergreylist_runas-"smmsp"}
+miltergreylist_pidfile=${miltergreylist_pidfile-"/var/run/milter-greylist.pid"}
+miltergreylist_sockfile=${miltergreylist_sockfile-"/var/milter-greylist/milter-greylist.sock"}
+miltergreylist_cfgfile=${miltergreylist_cfgfile-"%%PREFIX%%/etc/mail/greylist.conf"}
+miltergreylist_flags=${miltergreylist_flags-"-P $miltergreylist_pidfile \
+-f $miltergreylist_cfgfile -p $miltergreylist_sockfile -u $miltergreylist_runas"}
 
 . %%RC_SUBR%%
 
