24fca8096f00d48fe182fe89b6e5d8cc51261a31
ijliao <ijliao@FreeBSD.org>
2004-07-29 02:08:47 +0000

    fix msn 100% cpu loading problem
    Obtained from:	centericq mailing list
    Approved by:	maintainer

--- net/centericq/files/patch-msnhook.cc
+++ net/centericq/files/patch-msnhook.cc
@@ -1,6 +1,16 @@
---- src/hooks/msnhook.cc.orig	Sun Mar  7 21:44:41 2004
-+++ src/hooks/msnhook.cc	Fri Apr  2 10:23:03 2004
-@@ -408,11 +408,11 @@
+--- src/hooks/msnhook.cc.orig	Thu Jul  1 03:42:16 2004
++++ src/hooks/msnhook.cc	Thu Jul 29 10:02:57 2004
+@@ -155,6 +155,9 @@
+     int hsock = 0;
+     MSN::Connection *c;
+ 
++    FD_ZERO(&rs);
++    FD_ZERO(&ws);
++    FD_ZERO(&es);
+     getsockets(rs, ws, es, hsock);
+     tv.tv_sec = tv.tv_usec = 0;
+ 
+@@ -451,11 +454,11 @@
  
  void msnhook::checkfriendly(icqcontact *c, const string friendlynick, bool forcefetch) {
      string oldnick = c->getnick();
