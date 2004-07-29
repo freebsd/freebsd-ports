633f76fd42e1f2bbc375ed3133f643e8b87458fa
Pav Lucistnik <pav@FreeBSD.org>
2004-07-29 20:28:27 +0000

    - Update to 1.7b - Switch to sasl2
    PR:		ports/69697
    Submitted by:	Palle Girgensohn <girgen@pingpong.net>

--- databases/cyrus-imspd/files/patch-imsp::dispatch.h
+++ databases/cyrus-imspd/files/patch-imsp::dispatch.h
@@ -1,6 +1,3 @@
-
-$FreeBSD$
-
 --- imsp/dispatch.h	2003/04/03 13:38:05	1.1
 +++ imsp/dispatch.h	2003/04/03 13:38:26
 @@ -42,7 +42,7 @@
@@ -10,5 +7,5 @@ $FreeBSD$
 -#define MAX_BUF 4096
 +#define MAX_BUF (4096 * 1024)
  
- #include <sasl.h>
+ #include <sasl/sasl.h>
  
