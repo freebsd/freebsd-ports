48a4a592c9c53a9c48598f21665212645ccf610d
Greg Lewis <glewis@FreeBSD.org>
2004-07-30 16:49:12 +0000

    . Update to 2.3.3.22, a version written this century.  It could do with   some more work to make it easier to run out of the box (in terms of   configuration files) if anyone is interested.

--- games/tinymux/files/patch-src-htab.c
+++
@@ -1,9 +0,0 @@
---- htab.c.orig	Tue Oct 28 01:03:30 2003
-+++ htab.c	Tue Oct 28 01:03:38 2003
-@@ -805,4 +805,4 @@
- 	return -1;
- }
- 
--#endif STANDALONE
-\ No newline at end of file
-+#endif /* STANDALONE */
