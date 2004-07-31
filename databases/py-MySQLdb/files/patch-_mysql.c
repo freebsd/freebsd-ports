1928b42c5faaab304ea19846bbf7d42ee5b4be4e
Clement Laforet <clement@FreeBSD.org>
2004-07-31 20:13:16 +0000

    - Fix build with MySQL 4.1.3
    Reported by: 	crappy, thierry
    Submitted by:	thierry
    Obtained from:	http://sourceforge.net/tracker/index.php?func=detail&aid=998151&group_id=22307&atid=374934

---
+++ databases/py-MySQLdb/files/patch-_mysql.c
@@ -0,0 +1,21 @@
+Index: MySQLdb/_mysql.c
+===================================================================
+RCS file: /cvsroot/mysql-python/MySQLdb/_mysql.c,v
+retrieving revision 1.49
+diff -u -r1.49 _mysql.c
+--- _mysql.c	7 Jun 2004 03:44:38 -0000	1.49
++++ _mysql.c	26 Jul 2004 16:58:29 -0000
+@@ -1542,7 +1542,13 @@
+ 	if (!PyArg_ParseTuple(args, "")) return NULL;
+ 	check_connection(self);
+ 	Py_BEGIN_ALLOW_THREADS
++
++#if MYSQL_VERSION_ID >= 040103
++   r = mysql_shutdown(&(self->connection),
++                      SHUTDOWN_DEFAULT);
++#else
+ 	r = mysql_shutdown(&(self->connection));
++#endif
+ 	Py_END_ALLOW_THREADS
+ 	if (r) return _mysql_Exception(self);
+ 	Py_INCREF(Py_None);
