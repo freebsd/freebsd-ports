42717d2b3309aaa62cdb280c545f14926be4a437
Pav Lucistnik <pav@FreeBSD.org>
2004-07-31 15:16:58 +0000

    - Fix build with gcc-3.4
    PR:		ports/69833
    Submitted by:	Jake Hamby <jhamby@anobject.com>

---
+++ mail/thunderbird-esr/files/patch-xpinstall-src-nsSoftwareUpdate.cpp
@@ -0,0 +1,11 @@
+--- xpinstall/src/nsSoftwareUpdate.cpp.orig	Sat May 22 01:02:23 2004
++++ xpinstall/src/nsSoftwareUpdate.cpp	Thu Jul 29 22:48:34 2004
+@@ -512,7 +512,7 @@
+                                          nsSoftwareUpdate::GetInstance)
+ NS_GENERIC_FACTORY_CONSTRUCTOR(nsInstallTrigger)
+ NS_GENERIC_FACTORY_CONSTRUCTOR(nsInstallVersion)
+-NS_GENERIC_FACTORY_CONSTRUCTOR(nsXPInstallManager);
++NS_GENERIC_FACTORY_CONSTRUCTOR(nsXPInstallManager)
+ NS_GENERIC_FACTORY_CONSTRUCTOR(nsSoftwareUpdateNameSet)
+ 
+ //----------------------------------------------------------------------
