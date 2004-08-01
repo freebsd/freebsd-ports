9fc953aaa19d70826960bc3daba1091a6c4fd212
thierry <thierry@FreeBSD.org>
2004-08-01 22:06:39 +0000

    Upgrade Grisbi to 0.5.1.

--- finance/grisbi/files/patch-src::include.h
+++
@@ -1,8 +0,0 @@
---- src/include.h.orig	Wed May 12 22:47:18 2004
-+++ src/include.h	Sat May 29 12:29:36 2004
-@@ -26,5 +26,4 @@
- #define G_BROKEN_FILENAMES 1
- #include <gtk/gtktext.h>
- #include <gdk/gdkkeysyms.h>
--#include <popt.h>
- 
