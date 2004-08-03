3020645a7c15b55dbed8c20d7917906d69b5a489
Jun Kuriyama <kuriyama@FreeBSD.org>
2004-08-03 07:15:43 +0000

    - Unbreak with gcc-3.4.x.
    Submitted by:	marcel

---
+++ textproc/opensp/files/patch-RangeMap.h
@@ -0,0 +1,10 @@
+--- include/RangeMap.h.orig	Sun Aug  1 19:34:55 2004
++++ include/RangeMap.h	Sun Aug  1 19:26:43 2004
+@@ -9,6 +9,7 @@
+ #include "ISet.h"
+ #include "types.h"
+ #include <stddef.h>
++#include "constant.h"
+ 
+ #ifdef SP_NAMESPACE
+ namespace SP_NAMESPACE {
