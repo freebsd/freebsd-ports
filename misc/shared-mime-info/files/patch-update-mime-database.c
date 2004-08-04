d29d2613e421d23a7bb48fc2dc0c47644bd2e788
marcus <marcus@FreeBSD.org>
2004-08-04 18:33:45 +0000

    Sync the MIME search directories from gnome-vfs.  This fixes some prioritization issues with custom MIME types.

--- misc/shared-mime-info/files/patch-update-mime-database.c
+++ misc/shared-mime-info/files/patch-update-mime-database.c
@@ -15,7 +15,7 @@
  	env = getenv("XDG_DATA_DIRS");
  	if (!env)
 -		env = "/usr/local/share/:/usr/share/";
-+		env = "/usr/local/share/:%%X11BASE%%/share/:/usr/share/";
++		env = "%%X11BASE%%/share/gnome/:/usr/local/share/gnome/:%%X11BASE%%/share/:/usr/local/share/:/usr/share/";
  	dirs = g_strsplit(env, ":", 0);
  	g_return_if_fail(dirs != NULL);
  	for (n = 0; dirs[n]; n++)
--- x11-toolkits/gtk20/files/patch-gtk_xdgmime_xdgmime.c
+++ x11-toolkits/gtk20/files/patch-gtk_xdgmime_xdgmime.c
@@ -5,7 +5,7 @@
        xdg_data_dirs = getenv ("XDG_DATA_DIRS");
        if (xdg_data_dirs == NULL)
 -	xdg_data_dirs = "/usr/local/share/:/usr/share/";
-+	xdg_data_dirs = "/usr/local/share/:%%X11BASE%%/share/:/usr/share/";
++	xdg_data_dirs = "%%X11BASE%%/share/gnome/:/usr/local/share/gnome/:%%X11BASE%%/share/:/usr/local/share/:/usr/share/";
  
        ptr = xdg_data_dirs;
  
