4cbde22db8be6f7babd58c4ea1412d06ffaffc28
arved <arved@FreeBSD.org>
2004-08-06 16:24:03 +0000

    Fix build with gcc 3.4

---
+++ archivers/fastjar/files/patch-jargrep.c
@@ -0,0 +1,10 @@
+--- jargrep.c.orig	Fri Aug  6 18:22:38 2004
++++ jargrep.c	Fri Aug  6 18:23:10 2004
+@@ -573,6 +573,7 @@
+ 					break;
+ 				case 2:
+ 					/* fall through continue */
++					break;
+ 				}
+ 			}
+ 		} while(floop);
