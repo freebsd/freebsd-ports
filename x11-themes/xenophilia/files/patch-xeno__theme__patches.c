e6fb357b19ac485f12ec3bed79d6406c04c9f370
Kirill Ponomarev <krion@FreeBSD.org>
2004-08-06 21:27:36 +0000

    Fix build with gcc-3.4.2
    PR:		ports/70078
    Submitted by:	Christian Hiris <4711@chello.at>

---
+++ x11-themes/xenophilia/files/patch-xeno__theme__patches.c
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- xeno_theme_patches.c.orig	Fri Aug  6 23:26:02 2004
++++ xeno_theme_patches.c	Fri Aug  6 23:26:14 2004
+@@ -632,7 +632,6 @@
+ 				spacing = -1;
+ 				extra = -1;
+ 			}
+-		  nope:
+ 		}
+ 		dims->min_length = size;
+ 	}
