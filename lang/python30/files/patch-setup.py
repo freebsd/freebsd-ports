511678e7c67a83ed3763c1ea4a6ad4013589a56b
perky <perky@FreeBSD.org>
2004-08-07 06:36:57 +0000

    - Update to 2.4a2 - Add IPV6 option [1]
    PR:		69950 [1]
    Submitted by:	Marcus Grando <marcus@corp.grupos.com.br> [1]

--- lang/python30/files/patch-setup.py
+++ lang/python30/files/patch-setup.py
@@ -9,15 +9,6 @@
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (at the front) if
-@@ -778,7 +778,7 @@
-             # Linux-specific modules
-             exts.append( Extension('linuxaudiodev', ['linuxaudiodev.c']) )
- 
--        if platform in ('linux2', 'freebsd4'):
-+        if platform in ('linux2', 'freebsd4', 'freebsd5'):
-             exts.append( Extension('ossaudiodev', ['ossaudiodev.c']) )
- 
-         if platform == 'sunos5':
 @@ -1160,8 +1160,7 @@
            ext_modules=[Extension('struct', ['structmodule.c'])],
  
