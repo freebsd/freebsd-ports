631deef91135ae92207e0c98a609fa0304625580
Kirill Ponomarev <krion@FreeBSD.org>
2004-08-07 06:57:35 +0000

    Fix build with gcc3.4
    PR:		ports/70102
    Submitted by:	Andrey Slusar <vasallia@ukr.net>

---
+++ x11-wm/amaterus/files/patch-src::wmlabel.c
@@ -0,0 +1,10 @@
+--- src/wmlabel.c.old	Thu Aug  5 05:53:44 2004
++++ src/wmlabel.c	Thu Aug  5 05:53:59 2004
+@@ -42,6 +42,7 @@
+ 	    break;
+ 
+ 	default:
++	    break;
+ 	}
+ 	list = list->next;
+     }
