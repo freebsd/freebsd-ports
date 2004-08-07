d0ee6c37962a73cee17fafb3d9f672d771fc79bd
krion <krion@FreeBSD.org>
2004-08-07 06:59:53 +0000

    Fix statistic read and write.
    PR:		ports/70104
    Submitted by:	maintainer

---
+++ comms/bforce-kst/files/patch-bfstat.c
@@ -0,0 +1,10 @@
+--- bfutil/bfstat.c.orig	Thu Nov 21 23:23:00 2002
++++ bfutil/bfstat.c	Sat Aug  7 02:20:35 2004
+@@ -17,6 +17,7 @@
+ #include "logger.h"
+ #include "util.h"
+ #include "outbound.h"
++#include "session.h"
+ 
+ /*
+  * Command line options storage structure
