7398eb9f932728fa0782c1785a8ecc31b74eab72
Kirill Ponomarev <krion@FreeBSD.org>
2004-08-08 17:13:17 +0000

    Fix build with gcc-3.4 Support CC/CFLAGS properly
    PR:		ports/70171
    Submitted by:	Ports Fury

---
+++ misc/wmx10/files/patch-wmx.10.c
@@ -0,0 +1,10 @@
+--- wmx10.c.orig	Fri Dec 24 12:11:19 1999
++++ wmx10.c	Fri Aug  6 17:40:57 2004
+@@ -429,6 +429,7 @@
+ 		  updateNumDisplay(lampNum);
+ 		  break;
+ 	      default:
++		  ;
+ 	      }
+ 	  }
+       case ButtonRelease:
