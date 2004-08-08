8bb7296b9c0b25022cd2f1272587e7578a527e9d
Kirill Ponomarev <krion@FreeBSD.org>
2004-08-08 17:09:43 +0000

    Fix build with gcc-3.4
    PR:		ports/70168
    Submitted by:	Ports Fury

---
+++ games/xconq/files/patch-tcltk::tkmain.c
@@ -0,0 +1,12 @@
+--- tcltk/tkmain.c.orig	Tue Dec  5 02:51:35 2000
++++ tcltk/tkmain.c	Thu Aug  5 21:24:26 2004
+@@ -1380,7 +1380,9 @@
+ 	    )
+ 	  return TRUE;
+       case 1:
++	  ;
+       default:
++	  ;
+     }
+     return FALSE;
+ }
