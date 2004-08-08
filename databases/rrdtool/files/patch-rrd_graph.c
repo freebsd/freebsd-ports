99584da08b8138e84971e4cf8be6e1dd2830c60b
demon <demon@FreeBSD.org>
2004-08-08 14:25:31 +0000

    Update to version 1.0.49

--- databases/rrdtool/files/patch-rrd_graph.c
+++ databases/rrdtool/files/patch-rrd_graph.c
@@ -1,11 +1,11 @@
---- src/rrd_graph.c.orig	Fri May  7 15:10:16 2004
-+++ src/rrd_graph.c	Fri May  7 15:10:34 2004
+--- src/rrd_graph.c.orig	Sun Aug  8 14:58:13 2004
++++ src/rrd_graph.c	Sun Aug  8 18:19:06 2004
 @@ -2966,7 +2966,7 @@
  
  	
  	opt = getopt_long(argc, argv, 
--			  "s:e:x:y:v:w:h:iu:l:rb:oc:t:f:a:zgjFYAMXL:S:RB:O:U:",
-+			  "s:e:x:y:v:w:h:iu:l:rb:oc:t:f:a:zgjFYNAMX:L:S:RB:O:U:",
+-			  "s:e:x:y:v:w:h:iu:l:rb:oc:t:f:a:zgjFYAMX:L:S:RB:O:U:",
++			  "s:e:x:y:v:w:h:iu:l:rb:oc:t:f:a:zgjFYAMNX:L:S:RB:O:U:",
  			  long_options, &option_index);
  
  	if (opt == EOF)
