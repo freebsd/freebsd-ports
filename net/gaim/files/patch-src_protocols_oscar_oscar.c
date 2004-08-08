1a32a03d68f0a474688f5dfe2dac82775aac9454
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-08-08 21:36:53 +0000

    Update to 0.81.  For a list of what's changed, see http://gaim.sourceforge.net/ChangeLog.
    Submitted by:	Matthew Luckie <mjl@luckie.org.nz>

--- net/gaim/files/patch-src_protocols_oscar_oscar.c
+++ net/gaim/files/patch-src_protocols_oscar_oscar.c
@@ -1,6 +1,6 @@
---- src/protocols/oscar/oscar.c.orig	Tue Jun  1 18:12:11 2004
-+++ src/protocols/oscar/oscar.c	Tue Jun  1 18:12:58 2004
-@@ -3127,7 +3127,7 @@
+--- src/protocols/oscar/oscar.c.orig	Sun Aug  8 01:32:27 2004
++++ src/protocols/oscar/oscar.c	Sun Aug  8 01:40:47 2004
+@@ -3136,7 +3136,7 @@
  	 * Note: There *may* be some clients which send messages as HTML formatted -
  	 *       they need to be special-cased somehow.
  	 */
@@ -9,3 +9,29 @@
  		/* being recevied by ICQ from ICQ - escape HTML so it is displayed as sent */
  		gchar *tmp2 = gaim_escape_html(tmp);
  		g_free(tmp);
+@@ -3473,6 +3473,9 @@
+ 	gchar **msg1, **msg2;
+ 	GError *err = NULL;
+ 	int i, numtoks;
++#ifdef HAVE_TM_GMTOFF
++	struct tm tm;
++#endif
+ 
+ 	if (!args->type || !args->msg || !args->uin)
+ 		return 1;
+@@ -3507,8 +3510,15 @@
+ 
+ 				if (t) { /* This is an offline message */
+ 					/* The timestamp is UTC-ish, so we need to get the offset */
++#ifdef HAVE_TM_GMTOFF
++					localtime_r(&t, &tm);
++					t += tm.tm_gmtoff;
++#else
++#ifdef HAVE_TIMEZONE
+ 					tzset();
+ 					t -= timezone;
++#endif
++#endif
+ 					serv_got_im(gc, uin, tmp, 0, t);
+ 				} else { /* This is a message from MacICQ/Miranda */
+ 					serv_got_im(gc, uin, tmp, 0, time(NULL));
