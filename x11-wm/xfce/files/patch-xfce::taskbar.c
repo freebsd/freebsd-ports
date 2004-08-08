67cbe5a98f410e39817db00a655e5e20ab86c91e
oliver <oliver@FreeBSD.org>
2004-08-08 16:43:58 +0000

    fix build with gcc 3.4.2 Bump PORTREVISION
    PR:		ports/70103
    Submitted By:	Andrey Slusar <vasallia@ukr.net>

---
+++ x11-wm/xfce/files/patch-xfce::taskbar.c
@@ -0,0 +1,10 @@
+--- xfce/taskbar.c.old	Fri Aug  6 23:00:40 2004
++++ xfce/taskbar.c	Fri Aug  6 23:01:03 2004
+@@ -1697,6 +1697,7 @@
+     fprintf(f,"\tSortByUnsorted\n");
+     break;
+   default:
++    break;
+   } 
+ }
+ 
