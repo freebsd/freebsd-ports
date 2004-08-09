8d30865672c6ca7e5455a2504bb833c32f66bba0
krion <krion@FreeBSD.org>
2004-08-09 08:18:49 +0000

    Add pft 1.2,
    libPJL- Printer Job Language library A library and a
    command-line tool to use PJL- HP Printer Job Language
    
    PR:		ports/69382
    Submitted by:	Yonatan <Yonatan@xpert.com>

---
+++ security/pft/files/patch-commands.h
@@ -0,0 +1,8 @@
+--- commands.h.orig	Thu Aug  5 13:31:46 2004
++++ commands.h	Thu Aug  5 13:31:54 2004
+@@ -31,4 +31,4 @@
+ #define PJLFS_FILE			1
+ 
+ 
+-#endif __COMMANDS_H__
++#endif // __COMMANDS_H__
---
+++ security/pft/files/patch-fxstrings.h
@@ -0,0 +1,8 @@
+--- fxstrings.h.orig	Thu Aug  5 13:34:15 2004
++++ fxstrings.h	Thu Aug  5 13:34:26 2004
+@@ -64,4 +64,4 @@
+ };
+ 
+ 
+-#endif __FXSTRINGS_H__
++#endif // __FXSTRINGS_H__
