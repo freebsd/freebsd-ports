d2b5596b162b30bdfb3600d06efec20a6cd7748a
Pav Lucistnik <pav@FreeBSD.org>
2004-08-11 22:01:59 +0000

    - move MASTER_SITES to standalone ports - autogenerate plists - tweak install a bit
    PR:		ports/70285
    Submitted by:	Tom McLaughlin <tmclaugh@sdf.lonestar.org> (maintainer)

--- x11-themes/gnome-icons/bsd.gnome-icons.mk
+++ x11-themes/gnome-icons/bsd.gnome-icons.mk
@@ -9,8 +9,6 @@
 # lioux@.
 
 PKGNAMEPREFIX=	gnome-icons-
-MASTER_SITES?=	${MASTER_SITE_GNOME}
-MASTER_SITE_SUBDIR?=	teams/art.gnome.org/themes/icon
 
 NO_BUILD=	yes
 USE_SIZE=	yes
@@ -19,12 +17,23 @@ REASON=		Themes may contain artwork not done by the author. \
 		Keep FreeBSD safe if theme author violated copyrights.
 
 USE_X_PREFIX=	yes
+PLIST=		${WRKDIR}/plist
+
+pre-install:
+	@${RM} -rf ${PLIST}
+
+	@cd ${WRKDIR} && ${FIND} * ! -type d | ${SORT} >> ${PLIST}; \
+	${SED} -i "" -e "s:^:share/icons/:" ${PLIST}; \
+	${FIND} * -type d ! -empty | ${SORT} -r | \
+		${SED} -e "s:^:@dirrm share/icons/:" | \
+		${GREP} / >> ${PLIST}; \
+	${ECHO} "@unexec /bin/rmdir %D/share/icons 2> /dev/null || \
+		/usr/bin/true" >> ${PLIST}
 
 do-install:
-	${MKDIR} ${PREFIX}/share/icons/${PORTNAME}
-	cd ${WRKSRC} && ${FIND} . -type d ! -empty \
+	cd ${WRKDIR} && ${FIND} * -type d ! -empty \
 		-exec ${MKDIR} -m 0755 \
-		${PREFIX}/share/icons/${PORTNAME}/"{}" \;
-	cd ${WRKSRC} && ${FIND} . -type f \
-		-exec ${INSTALL_DATA} ${WRKSRC}/"{}" \
-		${PREFIX}/share/icons/${PORTNAME}/"{}" \;
+		${PREFIX}/share/icons/"{}" \;
+	cd ${WRKDIR} && ${FIND} * ! -type d ! -name 'plist' \
+		-exec ${INSTALL_DATA} ${WRKDIR}/"{}" \
+		${PREFIX}/share/icons/"{}" \;
