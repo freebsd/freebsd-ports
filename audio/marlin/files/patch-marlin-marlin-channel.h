6208594758a2f7b7eb622d611aca70f47719b197
Pav Lucistnik <pav@FreeBSD.org>
2004-08-11 20:54:17 +0000

    Add marlin, an audio editor similar to Audacity, with Gstreamer backend. It can open files, record from microphone and rip from AudioCD.

---
+++ audio/marlin/files/patch-marlin-marlin-channel.h
@@ -0,0 +1,10 @@
+--- marlin/marlin-channel.h.orig	Sat Oct 11 01:02:22 2003
++++ marlin/marlin-channel.h	Sun Nov 16 23:53:22 2003
+@@ -22,6 +22,7 @@
+ #ifndef __MARLIN_CHANNEL_H__
+ #define __MARLIN_CHANNEL_H__
+ 
++#include <sys/types.h>
+ #include <glib.h>
+ 
+ #include <marlin/marlin-types.h>
