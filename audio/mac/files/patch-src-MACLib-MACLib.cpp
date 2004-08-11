f3248b633d8ed080c6acadf9f4fe53c9a7fc491e
pav <pav@FreeBSD.org>
2004-08-11 07:52:12 +0000

    Add mac, Monkey's Audio Codec which was recently released in source code. It contain a library and an executable compressor/decompressor of .ape files.
    Does not build under 4.x, we hit the wall with missing wcstol(). Working
    patches would be welcomed.
    
    PR:		ports/70153
    Submitted by:	Martin Dieringer <Martin.Dieringer@t-online.de>

---
+++ audio/mac/files/patch-src-MACLib-MACLib.cpp
@@ -0,0 +1,20 @@
+--- src/MACLib/MACLib.cpp.orig	Wed May 12 08:56:22 2004
++++ src/MACLib/MACLib.cpp	Mon Aug  9 20:27:19 2004
+@@ -62,7 +62,7 @@
+         pExtension--;
+ 
+     // take the appropriate action (based on the extension)
+-    if (wcsicmp(pExtension, L".apl") == 0)
++    if (wcscmp(pExtension, L".apl") == 0)
+     {
+         // "link" file (.apl linked large APE file)
+         CAPELink APELink(pFilename);
+@@ -72,7 +72,7 @@
+             nStartBlock = APELink.GetStartBlock(); nFinishBlock = APELink.GetFinishBlock();
+         }
+     }
+-    else if ((wcsicmp(pExtension, L".mac") == 0) || (wcsicmp(pExtension, L".ape") == 0))
++    else if ((wcscmp(pExtension, L".mac") == 0) || (wcscmp(pExtension, L".ape") == 0))
+     {
+         // plain .ape file
+         pAPEInfo = new CAPEInfo(&nErrorCode, pFilename);
