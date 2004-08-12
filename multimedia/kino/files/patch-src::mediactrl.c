3f0ba7f965a9e4b7133e6540f8ce8d706ea5a440
Pierre Beyssac <pb@FreeBSD.org>
2004-08-12 23:38:04 +0000

    Update to 0.7.3.

--- multimedia/kino/files/patch-src::mediactrl.c
+++ multimedia/kino/files/patch-src::mediactrl.c
@@ -1,9 +1,9 @@
---- src/mediactrl.c.orig	Mon Mar 15 23:21:57 2004
-+++ src/mediactrl.c	Tue Apr 13 11:10:03 2004
-@@ -18,14 +18,14 @@
- #include <sys/ioctl.h>
+--- src/mediactrl.c.orig	Tue Jul 13 02:46:15 2004
++++ src/mediactrl.c	Fri Aug 13 01:26:56 2004
+@@ -19,14 +19,14 @@
  #include <sys/types.h>
  #include <sys/stat.h>
+ #include <sys/time.h>
 -#include <asm/types.h>
 +//#include <asm/types.h>
  #include <fcntl.h>
@@ -18,7 +18,16 @@
  #include "mediactrl.h"
  
  
-@@ -119,6 +119,7 @@
+@@ -101,7 +101,7 @@
+ 	Griffin PowerMate
+ */
+ static struct media_ctrl_key mc_powermate_keys[] = {
+-	{ BTN_0, "Button", MEDIA_CTRL_B1 },
++	{ 0xdead, "Button", MEDIA_CTRL_B1 },
+ 	{ 0, NULL, 0 }
+ };
+ 
+@@ -126,6 +126,7 @@
  
  void translate_contour_hid_event(struct media_ctrl *ctrl, struct input_event *ev, struct media_ctrl_event *me) 
  {
@@ -26,7 +35,7 @@
  	
  	int lv, cv;
  	
-@@ -176,10 +177,12 @@
+@@ -183,10 +184,12 @@
  		
  	}
  	
@@ -39,7 +48,7 @@
  	me->type = 0;
  	
  	// printf("Translate %02x %02x\n", ev->type, ev->code );
-@@ -220,6 +223,7 @@
+@@ -227,6 +230,7 @@
  		//printf("Key: %04x %02x: %s\n", ev->code, ev->value, key->name);
  		
  	}
@@ -47,7 +56,7 @@
  }
  
  struct media_ctrl_device supported_devices[] = {
-@@ -264,8 +268,9 @@
+@@ -273,8 +277,9 @@
  	
  	if ( me->type  == MEDIA_CTRL_EVENT_JOG ) {
  		struct timeval timev;
@@ -58,7 +67,7 @@
  		if ( now < ctrl->last_jog_time + 40000 ) {
  			//printf("*** Fast Jog %02d %05d ***\n", me->value, now - ctrl->last_jog_time);
  			ctrl->jogrel = me->value;
-@@ -285,6 +290,7 @@
+@@ -294,6 +299,7 @@
  
  int probe_device(struct media_ctrl *mc)
  {
@@ -66,7 +75,7 @@
  	short devinfo[4];
  	int i = 0;
  	  
-@@ -312,6 +318,7 @@
+@@ -321,6 +327,7 @@
  	
  	} while ( supported_devices[++i].vendor != 0 );
  			
