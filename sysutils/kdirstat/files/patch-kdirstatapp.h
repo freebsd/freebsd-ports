9d829e2089134dd0cf13b95dab9ebb61df2c99d0
markus <markus@FreeBSD.org>
2004-08-12 12:39:34 +0000

    Fix build with gcc 3.4.2
    PR:		ports/70077
    Submitted by:	Christian Hiris <4711@chello.at>

---
+++ sysutils/kdirstat/files/patch-kdirstatapp.h
@@ -0,0 +1,10 @@
+--- kdirstat/kdirstatapp.h.orig	Tue Aug 26 22:47:47 2003
++++ kdirstat/kdirstatapp.h	Fri Aug  6 14:33:36 2004
+@@ -46,7 +46,7 @@
+     class KSettingsDialog;
+     class KTreemapView;
+     class KTreemapTile;
+-};
++}
+ 
+ using namespace KDirStat;
