b45207534f07eb7c813498681685467341d13336
Markus Brueffer <markus@FreeBSD.org>
2004-08-12 12:41:48 +0000

    Fix build with gcc 3.4.2
    PR:		ports/70071
    Submitted by:	Christian Hiris <4711@chello.at>

---
+++ sysutils/kkeyled/files/patch-ledcfgdia.h
@@ -0,0 +1,13 @@
+--- kkeyled/ledcfgdia.h.orig	Wed Feb 18 20:48:46 2004
++++ kkeyled/ledcfgdia.h	Fri Aug  6 13:19:04 2004
+@@ -92,10 +92,6 @@
+     QPushButton* CancelButton;
+ 
+ protected:
+-    ;
+-    ;
+-    ;
+-
+     QVBoxLayout* ledcfgdiaLayout;
+     QVBoxLayout* mainFrameLayout;
+     QVBoxLayout* Frame27Layout;
