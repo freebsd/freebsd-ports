00af95661f5474d68cfc0312ebc0bc7aa73e7534
Volker Stolz <vs@FreeBSD.org>
2004-08-13 14:15:25 +0000

    - Add WITHOUT_NESSUS_GTK knob. - Upgrade rc.d script to the rc-ng style; bump PORTREVISION for that. - Add CONFLICTS.
    PR:		ports/69878
    Submitted by:	Udo Schweigert (maintainer)

--- security/nessus-devel/files/nessusd.sh
+++ security/nessus-devel/files/nessusd.sh
@@ -1,28 +1,37 @@
-#! /bin/sh
-#
+#!/bin/sh
+
+# Start or stop nessusd
 # $FreeBSD$
+
+# PROVIDE: nessusd
+# REQUIRE: DAEMON
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
 #
+# NOTE for FreeBSD 5.0+:
+# If you want this script to start with the base rc scripts
+# move nessusd.sh to /etc/rc.d/nessusd
 
-umask 022
-arg=${1:-start}
+prefix=%%PREFIX%%
 
-nessusd_bin=%%PREFIX%%/sbin/nessusd
-nessus_arg="-D"
-kill_arg=nessusd
+# Define these nessusd_* variables in one of these files:
+#	/etc/rc.conf
+#	/etc/rc.conf.local
+#	/etc/rc.conf.d/nessusd
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE
+#
+[ -z "$nessusd_enable" ] && nessusd_enable="NO"	# Disable nessusd
+#nessusd_program="${prefix}/sbin/nessusd"	# Location of nessusd
+nessusd_flags="-D"				# Flags to nessusd program
 
-echo -n " nessusd"
+. %%RC_SUBR%%
 
-case $arg in
-start)
-	test -x $nessusd_bin || exit 1
-	$nessusd_bin $nessus_arg
-	;;
-stop)
-	killall $kill_arg > /dev/null 2>&1
-	;;
-restart)
-	killall $kill_arg > /dev/null 2>&1
-	test -x $nessusd_bin || exit 1
-	$nessusd_bin $nessus_arg
-	;;
-esac
+name="nessusd"
+rcvar=`set_rcvar`
+command="${prefix}/sbin/nessusd"
+pidfile="/usr/local/var/nessus/nessusd.pid"
+required_files="${prefix}/etc/nessus/nessusd.conf"
+
+load_rc_config $name
+run_rc_command "$1"
