9bd793da4988a9488661241c398465f6ae6ceda2
krion <krion@FreeBSD.org>
2004-08-14 10:45:15 +0000

    Restore break.
    Submitted by:	Andrey Slusar <anray@inet.ua>

--- x11/xxkb/files/patch-resource.c
+++ x11/xxkb/files/patch-resource.c
@@ -1,13 +1,12 @@
 
 $FreeBSD$
 
---- resource.c.orig	Sat Nov 30 14:34:12 2002
-+++ resource.c	Sat Aug 14 08:45:34 2004
-@@ -382,8 +382,7 @@
-     break;
+--- resource.c.orig	Sat Nov 30 15:34:12 2002
++++ resource.c	Sat Aug 14 11:48:57 2004
+@@ -383,7 +383,7 @@
    case XpmNoMemory:
      printf("No memory for open xpm file: %s\n", name);
--    break;
+     break;
 -  default:
 +  default: break;
    }
