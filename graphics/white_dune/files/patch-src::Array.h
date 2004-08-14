6b542a31bfb6326b3e4c5e03320ee5686c34ca57
arved <arved@FreeBSD.org>
2004-08-14 10:31:50 +0000

    Fix build with gcc 3.4

---
+++ graphics/white_dune/files/patch-src::Array.h
@@ -0,0 +1,11 @@
+--- src/Array.h.orig	Sat Aug 14 12:22:02 2004
++++ src/Array.h	Sat Aug 14 12:22:40 2004
+@@ -19,6 +19,8 @@
+  * Cambridge, MA 02139, USA.
+  */
+ 
++#include <cassert>
++
+ #ifndef _ARRAY_H
+ #define _ARRAY_H
+ 
