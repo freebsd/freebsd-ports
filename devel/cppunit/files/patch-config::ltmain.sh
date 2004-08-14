42e444b819b9ddd08c8405610e2de724b0b92169
ijliao <ijliao@FreeBSD.org>
2004-08-14 08:19:02 +0000

    upgrade to 1.10.2
    PR:		68759
    Approved by:	maintainer

---
+++ devel/cppunit/files/patch-config::ltmain.sh
@@ -0,0 +1,15 @@
+--- config/ltmain.sh.orig	Wed Jul  7 16:52:56 2004
++++ config/ltmain.sh	Wed Jul  7 16:53:13 2004
+@@ -5425,10 +5425,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
