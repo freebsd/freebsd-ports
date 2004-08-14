d2d9f64a6843496b9f45168f669bf4e32df28edf
Tilman Keskinoz <arved@FreeBSD.org>
2004-08-14 09:48:27 +0000

    Fix build with gcc 3.4
    Reported by:	pointyhat
    
    Mark IGNORE on amd64, crahes on startup.
    Probably broken on other 64bit archs too, because of a lot of LP64-warnings.

---
+++ ftp/IglooFTP/files/patch-ftp_tree.h
@@ -0,0 +1,15 @@
+--- src/ftp_tree.h.orig	Sat Aug 14 11:40:27 2004
++++ ftp_tree.h	Sat Aug 14 11:41:17 2004
+@@ -321,10 +321,12 @@
+ 	  break;
+ 
+ 	default:
++	  break;
+ 	}
+       break;
+ 
+     default:
++      break;
+     }
+   return FALSE;
+ }
