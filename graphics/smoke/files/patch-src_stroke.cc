901ef871836e6e12fb7503724c95364b4e3aa37b
krion <krion@FreeBSD.org>
2004-08-14 17:34:42 +0000

    Fix build with gcc-3.4
    PR:		ports/70456
    Submitted by:	maintainer

---
+++ graphics/smoke/files/patch-src_stroke.cc
@@ -0,0 +1,10 @@
+--- src/stroke.cc.orig	Sat Aug 14 21:16:17 2004
++++ src/stroke.cc	Sat Aug 14 21:16:27 2004
+@@ -24,6 +24,7 @@
+ #include "store.h"
+ #include "segments.h"
+ 
++#include <cmath>
+ 
+ 
+ // StrokeStyle
