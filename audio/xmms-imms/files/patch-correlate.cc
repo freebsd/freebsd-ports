e19fd86d99df811ac6eda81902600a47a53baf49
Brian Feldman <green@FreeBSD.org>
2004-08-14 17:03:58 +0000

    Update to IMMS 1.1 and fix for newer GCC.

---
+++ audio/xmms-imms/files/patch-correlate.cc
@@ -0,0 +1,9 @@
+--- correlate.cc.orig	Thu Apr 15 16:15:24 2004
++++ correlate.cc	Sat Aug 14 11:15:59 2004
+@@ -1,5 +1,6 @@
+ #include <assert.h>
+ #include <iostream>
++#include <math.h>
+ 
+ #include "correlate.h"
+ #include "strmanip.h"
--- audio/xmms-imms/files/patch-plugin.h
+++ audio/xmms-imms/files/patch-plugin.h
@@ -1,14 +1,14 @@
---- plugin.h.orig	Wed Feb 11 17:44:30 2004
-+++ plugin.h	Wed Feb 11 17:45:06 2004
+--- plugin.h.orig	Thu Apr 15 16:15:24 2004
++++ plugin.h	Sat Aug 14 11:08:55 2004
 @@ -1,7 +1,11 @@
  #ifndef __PLUGIN_H
  #define __PLUGIN_H
  
-+#if HAVE_STDINT_H
++#ifdef HAVE_STDIN
  #include <stdint.h>
 +#else
 +#include <sys/types.h>
 +#endif
+ #include "immsconf.h"
  
  #ifdef __cplusplus
- extern "C" {
