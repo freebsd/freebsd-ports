39b9a98c25b660a81df03d9d3b27d6f7c7673b5a
Markus Brueffer <markus@FreeBSD.org>
2004-08-14 00:01:06 +0000

    - Fix build with gcc 3.4 [1] - Fix build on amd64
    Reported by:	pointyhat via kris [1]

---
+++ graphics/kimagemapeditor/files/patch-kimearea.cpp
@@ -0,0 +1,11 @@
+--- src/kimearea.cpp.orig	Sat Aug 14 01:41:52 2004
++++ src/kimearea.cpp	Sat Aug 14 01:42:17 2004
+@@ -440,7 +440,7 @@
+    }
+   p.setRasterOp(Qt::XorROP);
+ 	
+-};
++}
+ 
+ QRect* Area::onSelectionPoint(const QPoint & p, double zoom) const
+ {
