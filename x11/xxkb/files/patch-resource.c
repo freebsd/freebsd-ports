30cd14fe160cd443e2d07dc55a515ccf9f52726a
Kirill Ponomarev <krion@FreeBSD.org>
2004-08-14 06:50:05 +0000

    Handle 'default' in switch more neatly.
    Submitted by:	Andrey Slusar <anray@inet.ua>

--- x11/xxkb/files/patch-resource.c
+++ x11/xxkb/files/patch-resource.c
@@ -1,13 +1,15 @@
 
 $FreeBSD$
 
---- resource.c.orig	Fri Aug 13 22:34:08 2004
-+++ resource.c	Fri Aug 13 22:34:19 2004
-@@ -383,7 +383,6 @@
+--- resource.c.orig	Sat Nov 30 14:34:12 2002
++++ resource.c	Sat Aug 14 08:45:34 2004
+@@ -382,8 +382,7 @@
+     break;
    case XpmNoMemory:
      printf("No memory for open xpm file: %s\n", name);
-     break;
+-    break;
 -  default:
++  default: break;
    }
  }
  
