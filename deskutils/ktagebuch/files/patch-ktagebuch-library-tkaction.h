918c1b5dc252bd39df8b34d8b2944e929ab8e2bf
markus <markus@FreeBSD.org>
2004-08-15 17:52:24 +0000

    - Fix build with gcc 3.4 [1] - Fix build on amd64
    Noticed by:	pointyhat [1]

---
+++ deskutils/ktagebuch/files/patch-ktagebuch-library-tkaction.h
@@ -0,0 +1,11 @@
+--- ktagebuch/library/tkaction.h.orig	Sun Aug 15 19:28:28 2004
++++ ktagebuch/library/tkaction.h	Sun Aug 15 19:32:10 2004
+@@ -24,7 +24,7 @@
+ 
+ namespace TK {
+   enum IconMode { IconOnly, IconAndText, TextOnly };
+-};
++}
+ 
+ class TKToolBarButton;
+ class TKComboBox;
