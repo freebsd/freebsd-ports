3414aca09d6225d239d4f5c288c5ca6a975c72e5
markus <markus@FreeBSD.org>
2004-08-15 18:18:41 +0000

    Fix build with gcc 3.4
    Noticed by:	pointyhat

---
+++ deskutils/kickpim/files/patch-src-kpcontactevent.cpp
@@ -0,0 +1,11 @@
+--- src/kpcontactevent.cpp.orig	Sun Aug 15 20:05:05 2004
++++ src/kpcontactevent.cpp	Sun Aug 15 20:05:18 2004
+@@ -24,7 +24,7 @@
+   m_text    = "";
+   m_date    = QDate(25,61,61); // invalid
+   m_contact = 0;
+-};
++}
+ 
+ KPContactEvent::~KPContactEvent()
+ {
