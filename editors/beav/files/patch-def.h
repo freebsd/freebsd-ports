02129dc3c032f9339bd0f05b8a960e67c506a0aa
Oliver Lehmann <oliver@FreeBSD.org>
2004-08-15 01:55:40 +0000

    update to 1.40-17 remove BROKEN

---
+++ editors/beav/files/patch-def.h
@@ -0,0 +1,12 @@
+--- def.h.orig	Tue Jul 13 11:05:00 2004
++++ def.h	Sun Aug 15 03:51:52 2004
+@@ -566,9 +566,6 @@
+ /*
+  * Standard I/O.
+  */
+-extern char *malloc ();
+-extern char *strcpy ();
+-extern char *strcat ();
+ 
+ #ifndef NOPROTO
+ #include "prototyp.h"
