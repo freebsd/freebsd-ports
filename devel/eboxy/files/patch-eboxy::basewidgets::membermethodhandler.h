26531d46d8c931db6142f6157479428e44bd35fc
Tilman Keskinoz <arved@FreeBSD.org>
2004-08-15 21:06:28 +0000

    Fix build on CURRENT

---
+++ devel/eboxy/files/patch-eboxy::basewidgets::membermethodhandler.h
@@ -0,0 +1,11 @@
+--- eboxy/basewidgets/membermethodhandler.h.orig	Sun Aug 15 20:21:31 2004
++++ eboxy/basewidgets/membermethodhandler.h	Sun Aug 15 23:00:54 2004
+@@ -19,6 +19,8 @@
+ #ifndef INCLUDED__MEMBERMETHODHANDLER_H
+ #define INCLUDED__MEMBERMETHODHANDLER_H
+ 
++using namespace std;
++
+ #include "methodhandler.h"
+ #include "variant.h"
+ 
