37f8349dd4cd306514041ab0cb5e5aad823ca2cc
Tilman Keskinoz <arved@FreeBSD.org>
2004-08-15 14:40:58 +0000

    Fix build with gcc 3.4

---
+++ security/nbaudit/files/patch-nmb.c
@@ -0,0 +1,10 @@
+--- nmb.c.orig	Sun Jun 13 21:08:40 2004
++++ nmb.c	Sun Jun 13 21:09:17 2004
+@@ -40,7 +40,6 @@
+ #endif /* SCANNER */
+ 
+ int Debug1(char *, ...);
+-int Debug1();
+ 
+ int num_good_sends=0;
+ int  num_good_receives=0;
