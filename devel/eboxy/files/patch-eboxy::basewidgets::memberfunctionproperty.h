f2864b71f1bce49646a79a0cff67f591a4c26628
arved <arved@FreeBSD.org>
2004-08-15 21:06:28 +0000

    Fix build on CURRENT

---
+++ devel/eboxy/files/patch-eboxy::basewidgets::memberfunctionproperty.h
@@ -0,0 +1,10 @@
+--- eboxy/basewidgets/memberfunctionproperty.h.orig	Sun Aug 15 23:02:26 2004
++++ eboxy/basewidgets/memberfunctionproperty.h	Sun Aug 15 23:02:36 2004
+@@ -19,6 +19,7 @@
+ #ifndef INCLUDED__MEMBERFUNCTIONPROPERTY_H
+ #define INCLUDED__MEMBERFUNCTIONPROPERTY_H
+ 
++using namespace std;
+ #include "customproperty.h"
+ #include "variant.h"
+ 
