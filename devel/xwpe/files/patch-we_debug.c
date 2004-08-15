3e5088fe275488c63ac058c0a855bcf509404a25
arved <arved@FreeBSD.org>
2004-08-15 17:23:10 +0000

    Fix build with gcc 3.4

---
+++ devel/xwpe/files/patch-we_debug.c
@@ -0,0 +1,10 @@
+--- we_debug.c.orig	Sun Aug 15 19:14:55 2004
++++ we_debug.c	Sun Aug 15 19:16:13 2004
+@@ -52,7 +52,6 @@
+ extern char *att_no;
+ extern char *e_tmp_dir;
+ 
+-char *tparm();
+ char *tgoto();
+ #ifdef DEFTPUTS
+ int tputs();
