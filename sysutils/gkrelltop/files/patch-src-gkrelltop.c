99bf496f590cc2eaaace6d569ecbf81ff91ae086
Pav Lucistnik <pav@FreeBSD.org>
2004-08-16 08:40:00 +0000

    - Update to 2.2.4
    PR:		ports/70496
    Submitted by:	David Gardner <david@pinko.net> (maintainer)

---
+++ sysutils/gkrelltop/files/patch-src-gkrelltop.c
@@ -0,0 +1,11 @@
+--- gkrelltop.c	Sat May 29 00:11:48 2004
++++ gkrelltop.c	Mon Aug  2 19:03:22 2004
+@@ -414,7 +414,7 @@
+ 
+     /* -- About -- */
+     about_text = g_strdup_printf(
+-      "gkrelltop version 2.2.3\n"
++      "gkrelltop version 2.2.4\n"
+       "GKrellM Top Plugin\n"
+       "Copyright (c) 2002,2003 Adi Zaimi\n"
+       "zaimi-at-pegasus.rutgers.edu\n"
