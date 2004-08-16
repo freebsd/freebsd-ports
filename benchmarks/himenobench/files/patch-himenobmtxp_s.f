b45a3f075807de64415b4919850d060fc4d986b3
Maho Nakata <maho@FreeBSD.org>
2004-08-16 08:14:12 +0000

    Add Himeno Benchmark. this suite was made by HIMENO, Ryutaro, for evaluation of performance of the calculation of incompressible flow analysis. This program solves Poisson equation by Jacobi's iterative method which have many loops

---
+++ benchmarks/himenobench/files/patch-himenobmtxp_s.f
@@ -0,0 +1,10 @@
+--- himenobmtxp_s.f.orig	Mon Aug 16 15:39:44 2004
++++ himenobmtxp_s.f	Mon Aug 16 15:39:54 2004
+@@ -39,7 +39,6 @@
+ C "use portlib" statement on the next line is for Visual fortran
+ C to use UNIX libraries. Please remove it if your system is UNIX.
+ C -------------------
+-      use portlib
+       IMPLICIT REAL*4(a-h,o-z)
+ C
+       PARAMETER (mimax=513,mjmax=257,mkmax=257)
---
+++ benchmarks/himenobench/files/patch-himenobmtxps.c
@@ -0,0 +1,18 @@
+--- himenobmtxps.c~	Thu Feb 21 09:27:59 2002
++++ himenobmtxps.c	Mon Aug 16 16:13:42 2004
+@@ -37,6 +37,7 @@
+ ********************************************************************/
+ 
+ #include <stdio.h>
++#include <sys/time.h>
+ 
+ #ifdef SSMALL
+ #define MIMAX            33
+@@ -245,7 +246,6 @@
+ double
+ second()
+ {
+-#include <sys/time.h>
+ 
+   struct timeval tm;
+   double t ;
