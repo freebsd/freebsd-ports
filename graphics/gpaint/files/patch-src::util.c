e2e527f0c252d9d04a0a43f191cac7c93e945886
arved <arved@FreeBSD.org>
2004-08-16 14:12:59 +0000

    Fix build with gcc 3.4

---
+++ graphics/gpaint/files/patch-src::util.c
@@ -0,0 +1,20 @@
+--- src/util.c.orig	Mon Aug 16 16:09:49 2004
++++ src/util.c	Mon Aug 16 16:10:57 2004
+@@ -995,7 +995,7 @@
+       break;
+                 
+    default:
+-      
++      break;
+    }
+    ibuf->mx = x;
+    ibuf->my = y;
+@@ -1147,7 +1147,7 @@
+       break;
+          
+    default:
+-      
++      break;
+    }
+    ibuf->lx = INT_MIN;
+    ibuf->ly = INT_MIN;
