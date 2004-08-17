9becd45701c40c24c25d14ad9580bbf136ea50e0
Sergey A. Osokin <osa@FreeBSD.org>
2004-08-17 11:54:09 +0000

    Update nVidia binary drivers to most recent released version, 6113. Features:  * Support for the latest NVIDIA GPUs including GeForce 6800 Series  * Improved interaction with -CURRENT's new threading libraries.
    It also brings some more general fixes to the port:
     * Utilizes PORTDOCS
     * New knobs added
     * pkg-plist fixed
    
    Special note for -CURRENT users: nVidia driver now depends
    on mem.ko kernel module.
    
    Submitted by:	Alexey Dokuchaev <danfe@regency.nsu.ru> (maintainer)
    PR:		70561

--- x11/nvidia-driver/files/patch-src::nvidia_pci.c
+++ x11/nvidia-driver/files/patch-src::nvidia_pci.c
@@ -1,21 +1,12 @@
---- src/nvidia_pci.c.orig	Thu May 29 02:51:52 2003
-+++ src/nvidia_pci.c	Fri Oct  3 01:19:55 2003
-@@ -33,6 +33,18 @@
-     if (vendor != NVIDIA_VENDORID || device < 0x0020)
-         return ENXIO;
+--- src/nvidia_pci.c.orig	Mon Aug 16 17:40:56 2004
++++ src/nvidia_pci.c	Mon Aug 16 17:41:20 2004
+@@ -218,6 +218,9 @@
+ };
  
-+    /* Exclude nForce MCP devices from detection */
-+    if (device >= 0x01A4 && device <= 0x01ef)
-+    	return ENXIO;
-+    	
-+    /* Exclude nForce2 MCP2 devices from detection */
-+    if (device >= 0x0060 && device <= 0x006e)
-+    	return ENXIO;
-+
-+    /* Exclude nForce3 MCP3 devices from detection */
-+    if (device >= 0x00d4 && device <= 0x00da)
-+    	return ENXIO;
-+    
-     if (rm_get_device_name(device, NV_DEVICE_NAME_LENGTH, name)
-             != RM_OK) {
-         strcpy(name, "Unknown");
+ DRIVER_MODULE(nvidia, pci, nvidia_pci_driver, nvidia_devclass, nvidia_modevent, 0);
++#if __FreeBSD_version > 502126
++MODULE_DEPEND(nvidia, mem, 1, 1, 1);
++#endif
+ 
+ #ifdef NV_SUPPORT_OS_AGP
+ MODULE_DEPEND(nvidia, agp, 1, 1, 1);
