b52b39c34f1e7af765656d8c4be3c750d9a773a8
Tilman Keskinoz <arved@FreeBSD.org>
2004-08-17 13:58:21 +0000

    Fix build with gcc 3.4

---
+++ graphics/diacanvas/files/patch-diamagnifytool.c
@@ -0,0 +1,10 @@
+--- diacanvas/diamagnifytool.c.orig	Tue Aug 17 15:52:45 2004
++++ diacanvas/diamagnifytool.c	Tue Aug 17 15:53:00 2004
+@@ -118,6 +118,7 @@
+ 	}
+       break;
+     default:
++	break;
+     } /* switch */
+   return FALSE;
+ }
