e178c6c76eff58a38c6f7f1f971cb309994ed7d8
mi <mi@FreeBSD.org>
2004-08-17 16:52:19 +0000

    Don't require a particular version of libexpat. Use sh(1)'s `echo *' instead of spawning off ls(1) in pkg_check.sh.
    Approved by:	maintainer

--- security/vxquery/files/pkg_check.sh
+++ security/vxquery/files/pkg_check.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
-LS="/bin/ls"
 VXQUERY="vxquery"
 PORTSDIR="${PORTSDIR:-/usr/ports}"
 PKGDIR="${PKGDIR:-/var/db/pkg}"
@@ -7,4 +6,5 @@ FORMAT="${FORMAT:-text}"
 VULN_XML="${VULN_XML:-${PORTSDIR}/security/vuxml/vuln.xml}"
 
 set -e
-(cd "$PKGDIR" && "${LS}") | "${VXQUERY}" -f - -t "${FORMAT}" "${VULN_XML}"
+cd "$PKGDIR"
+echo * | "${VXQUERY}" -f - -t "${FORMAT}" "${VULN_XML}"
