a68477189c817f667aa2280dac85953e6649f3e1
pav <pav@FreeBSD.org>
2004-08-17 08:51:19 +0000

    - Update to 1.25 - Give maintainership to submitter
    PR:		ports/70558
    Submitted by:	Jonathan Lennox <lennox@cs.columbia.edu>
    Approved by:	old maintainer

--- textproc/xml2rfc/files/patch-xml2rfc.tcl
+++ textproc/xml2rfc/files/patch-xml2rfc.tcl
@@ -1,15 +1,13 @@
 
 $FreeBSD$
 
---- xml2rfc.tcl.orig	Mon Jun 10 04:32:34 2002
-+++ xml2rfc.tcl	Thu Jul 25 21:33:22 2002
-@@ -1,7 +1,7 @@
+--- xml2rfc.tcl.orig	Mon Aug 16 17:55:38 2004
++++ xml2rfc.tcl	Mon Aug 16 17:55:45 2004
+@@ -1,6 +1,6 @@
  #!/bin/sh
  # the next line restarts using the correct interpreter \
--if   test ! -z "$DISPLAY"; then exec wish "$0" "$0" "$@"; \
--elif test ! -z "$1";       then exec tclsh "$0" "$0" "$@"; \
-+if   test ! -z "$DISPLAY"; then exec wish8.3 "$0" "$0" "$@"; \
-+elif test ! -z "$1";       then exec tclsh8.3 "$0" "$0" "$@"; \
- else                            echo "usage: $0 filename" >&2; exit 1; fi
+-exec tclsh "$0" "$0" "$@"
++exec tclsh8.3 "$0" "$0" "$@"
  
  
+ # NOTE FROM CLIVE
