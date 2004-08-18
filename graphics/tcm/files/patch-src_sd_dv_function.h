f9f17e9dbded41712b7cd080c8d19e2de1d4d542
mich <mich@FreeBSD.org>
2004-08-18 15:59:36 +0000

    o Fix build with gcc 3.4.x o Switch to REINPLACE_CMD o Tested on CURRENT-amd64 / CURRENT-i386 / 5.2.1REL-i386
    Tested by	mux

---
+++ graphics/tcm/files/patch-src_sd_dv_function.h
@@ -0,0 +1,16 @@
+
+$FreeBSD$
+
+--- src/sd/dv/function.h.orig	Wed Aug 18 16:02:05 2004
++++ src/sd/dv/function.h	Wed Aug 18 16:02:38 2004
+@@ -19,8 +19,8 @@
+ // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ // 02111-1307, USA.
+ //-----------------------------------------------------------------------------
+-#ifndef _FUNCTION_H
+-#define _FUNCTION_H
++#ifndef _FUNCTION_CLASS_H
++#define _FUNCTION_CLASS_H
+ 
+ #include "c1edge.h"
+ class ERGraph;
