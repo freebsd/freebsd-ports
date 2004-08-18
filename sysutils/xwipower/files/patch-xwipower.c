a02334c9b93e3131ddcebdd3392310675f4c1778
Munechika SUMIKAWA <sumikawa@FreeBSD.org>
2004-08-18 01:27:34 +0000

    Fix compilation problem with gcc-3.4 on -current.

---
+++ sysutils/xwipower/files/patch-xwipower.c
@@ -0,0 +1,10 @@
+--- xwipower.c~	Mon Aug 16 13:03:01 2004
++++ xwipower.c	Mon Aug 16 13:05:34 2004
+@@ -277,7 +277,6 @@
+         case 'a':
+             mode = ANTENNA_MODE;
+ 			break;
+-        default:
+ 		}
+     }
+ 
