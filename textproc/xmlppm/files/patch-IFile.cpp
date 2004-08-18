ca004b53c65ba92a073303a730024c27b2646268
Kirill Ponomarev <krion@FreeBSD.org>
2004-08-18 18:34:33 +0000

    Update to version 0.98.1
    PR:		ports/70635
    Submitted by:	Ports Fury

--- textproc/xmlppm/files/patch-IFile.cpp
+++ textproc/xmlppm/files/patch-IFile.cpp
@@ -1,11 +1,11 @@
---- IFile.cpp.orig	Wed Jul 23 04:32:54 2003
-+++ IFile.cpp	Wed Jul 23 04:35:43 2003
-@@ -100,7 +100,7 @@
-   while(insz > 0) {
-     size_t result;
-     outsz = BUFFSIZE;
--    result = iconv(ifile->iconv, &(char*)inptr, &insz, &outptr, &outsz);
-+    result = iconv(ifile->iconv, &inptr, &insz, &outptr, &outsz);
-     total += fwrite(outbuf, sizeof(char), BUFFSIZE-outsz, ifile->file);
-     if(result == (size_t)-1 && errno != E2BIG) {
-       return total;
+--- IFile.cpp.orig	Thu Jun 10 04:16:13 2004
++++ IFile.cpp	Tue Aug 17 23:21:02 2004
+@@ -73,7 +73,7 @@
+    writing the output.  Then flush the file.  */
+ void ifflush(IFILE* ifile) {
+   static char outbuf[BUFFSIZE];
+-  char* inptr = ifile->buf;
++  const char* inptr = ifile->buf;
+   char* outptr = outbuf;
+   size_t insz = ifile->bufsiz;
+   size_t outsz;
