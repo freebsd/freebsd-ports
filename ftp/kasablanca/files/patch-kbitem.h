61c87cf5c818e2c050fef6ee027bc26b72858de7
Markus Brueffer <markus@FreeBSD.org>
2004-08-18 13:45:53 +0000

    Update to version 0.4.0.1

---
+++ ftp/kasablanca/files/patch-kbitem.h
@@ -0,0 +1,10 @@
+--- src/kbitem.h.orig	Wed Aug 18 15:07:56 2004
++++ src/kbitem.h	Wed Aug 18 15:08:22 2004
+@@ -12,6 +12,7 @@
+ #ifndef KBITEM_H
+ #define KBITEM_H
+ 
++#include <fcntl.h>
+ #include <qlistview.h>
+ 
+ class KbFileInfo;
