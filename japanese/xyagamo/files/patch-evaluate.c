f3a5653065eba724065bc4b40373d552ffa7db90
arved <arved@FreeBSD.org>
2004-08-19 13:01:49 +0000

    Fix build with gcc 3.4

---
+++ japanese/xyagamo/files/patch-evaluate.c
@@ -0,0 +1,10 @@
+--- src/evaluate.c.orig	Thu Aug 19 14:59:23 2004
++++ src/evaluate.c	Thu Aug 19 15:00:10 2004
+@@ -378,6 +378,7 @@
+         }
+       }
+     done:
++      ;
+     }
+   }
+   if (number_of_singletons == 0) {
