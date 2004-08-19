a9ea5a05c6bb7b51b6a851fafa54c527e661c8db
Hiroki Sato <hrs@FreeBSD.org>
2004-08-19 17:38:43 +0000

    - Update to 5.94a. - Add odvips/udvips support. - Fix pkg-descr. - Fix kpathsea searching order.  This (Japanese version   of dvipsk) now uses $TEXMF/web2c-ptex/texmf.cnf first.
    Approved by:	linimon (mentor)

---
+++ japanese/dvipsk/files/patch-texk::make::paths.mk
@@ -0,0 +1,11 @@
+--- make/paths.mk.orig	Sat Jun 12 12:13:14 2004
++++ make/paths.mk	Sat Jun 12 12:13:04 2004
+@@ -61,7 +61,7 @@
+ dvilj_latex2e_macrodir = $(texinputdir)/latex/dvilj
+ 
+ # mktex.cnf, texmf.cnf, etc.
+-web2cdir = $(texmf)/web2c
++web2cdir = $(texmf)/web2c-ptex
+ 
+ # The top-level font directory.
+ fontdir = $(texmf)/fonts
