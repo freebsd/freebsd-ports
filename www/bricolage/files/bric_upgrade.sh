57cdfc5052d648a98c04dc8d85dc678dfca824c2
Pav Lucistnik <pav@FreeBSD.org>
2004-08-20 23:33:39 +0000

    - Update to 1.8.1
    PR:		ports/70677
    Submitted by:	Rod Taylor <ports@rbt.ca> (maintainer)

---
+++ www/bricolage/files/bric_upgrade.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+BRICUPGRADE=%%PREFIX%%
+NEWVERSION=%%PORTVERSION%%
+
+# Ensure the version exists
+if [ "X" = "X${NEWVERSION}" ]
+then
+	echo "New version not supplied (port bug)"
+	exit 1
+fi
+
+# Ensure the old version exists at the end of the argument list
+OLDVERSION=`echo "$*" | sed -e "s/^.* \([^ ]*\)/\1/"`
+VTEST=`echo "$OLDVERSION" | sed -e "s/^1\.8\.[01]$/OKAY/"`
+if [ "${VTEST}" != "OKAY" ]
+then
+	echo "The final argument should be the old version of the port, in the format."
+	echo "	$0 [options] <version number>"
+	exit 1
+fi
+
+HAS_VERSION=`grep ${OLDVERSION} ${BRICUPGRADE}/versions.txt`
+if [ "${HAS_VERSION}" = "" ]
+then
+	echo "Version ${OLDVERSION} could not be found in the ${BRICUPGRADE}/versions.txt file"
+	echo "There are no components to upgrade"
+	exit 0
+fi
+
+# Now try to upgrade it
+for ver in `grep -A 1000 ${OLDVERSION} ${BRICUPGRADE}/versions.txt | tail -n +2`
+do
+	for file in `ls ${BRICUPGRADE}/${ver}`
+	do
+		${BRICUPGRADE}/${ver}/${file} $@ # Run with our args
+	done
+done
+
+echo "Upgrade complete"
+exit 0
