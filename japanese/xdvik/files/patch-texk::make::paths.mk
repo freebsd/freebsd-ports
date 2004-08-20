bc948412ab0c35c722a1091e0246c1a8251219d1
Hiroki Sato <hrs@FreeBSD.org>
2004-08-20 16:28:49 +0000

    - Repocopy from japanese/xdvik-vflib to japanese/xdvik because   it no longer depends on VFlib.
    - Fix kpathsea searching order.  This (Japanese version
      of xdvik) now uses $TEXMF/web2c-ptex/texmf.cnf first.
    
    - Some style changes.

---
+++ japanese/xdvik/files/patch-texk::make::paths.mk
@@ -0,0 +1,11 @@
+--- xdvik-22.40w/texk/make/paths.mk.orig	Sat Jun 12 12:13:14 2004
++++ xdvik-22.40w/texk/make/paths.mk	Sat Jun 12 12:13:04 2004
+@@ -61,7 +61,7 @@
+ dvilj_latex2e_macrodir = $(texinputdir)/latex/dvilj
+ 
+ # mktex.cnf, texmf.cnf, etc.
+-web2cdir = $(texmf)/web2c
++web2cdir = $(texmf)/web2c-ptex
+ 
+ # The top-level font directory.
+ fontdir = $(texmf)/fonts
