abe67dbda0463c83a9cfe698cbc22bad8b518ebf
Pav Lucistnik <pav@FreeBSD.org>
2004-08-21 18:58:43 +0000

    - Fix build with gcc 3.4 - Please portlint
    PR:		ports/70439
    Submitted by:	Greg J. <xcas@cox.net> (maintainer)

---
+++ x11-wm/kahakai/files/patch-Screen.cc
@@ -0,0 +1,10 @@
+--- src/Screen.cc.orig	Sat Feb  7 17:50:59 2004
++++ src/Screen.cc	Fri Aug 13 20:27:46 2004
+@@ -63,6 +63,7 @@
+ #include <sstream>
+ #include <iostream>
+ #include <iomanip>
++#include <cerrno>
+ 
+ using std::cout;
+ using std::cerr;
