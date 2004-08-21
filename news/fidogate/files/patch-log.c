f8db9748f917383d4ad9c75d12ad89200a2d03a1
dinoex <dinoex@FreeBSD.org>
2004-08-21 09:14:16 +0000

    - Security Fix Reported by:	Niels Heinen Obtained from:	Martin Junius

--- news/fidogate/files/patch-log.c
+++ news/fidogate/files/patch-log.c
@@ -1,5 +1,5 @@
 --- src/common/log.c.orig	Sun Feb 16 16:38:56 2003
-+++ src/common/log.c	Wed Nov 19 16:24:20 2003
++++ src/common/log.c	Sat Aug 21 11:10:09 2004
 @@ -69,8 +69,8 @@
  char *strerror(int errnum)
  {
@@ -10,3 +10,14 @@
      extern char *sys_errlist[];
  # endif
  #endif
+@@ -280,8 +280,10 @@
+     
+     BUF_COPY(logprog, name);
+ 
++#if 0 /**NOT NEEDED AND SECURITY RISK**/
+     if( (p = getenv("LOGFILE")) )
+        log_file(p);
+     if( (p = getenv("FIDOGATE_LOGFILE")) )
+        log_file(p);
++#endif
+ }
