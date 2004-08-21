9e149cf78b8b35922553594af075410557fecf04
markus <markus@FreeBSD.org>
2004-08-21 08:54:37 +0000

    Fix build with gcc 3.4

---
+++ games/kslide/files/patch-puz_app.cpp
@@ -0,0 +1,29 @@
+--- kslide/puz_app.cpp.orig	Sat Aug 21 10:49:41 2004
++++ kslide/puz_app.cpp	Sat Aug 21 10:50:07 2004
+@@ -110,16 +110,16 @@
+   }
+ }
+ 
+-void KSlideApp::setModeCl3() { setMode(0); };
+-void KSlideApp::setModeCl4() { setMode(1); };
+-void KSlideApp::setModeCl5() { setMode(2); };
+-void KSlideApp::setModeCl6() { setMode(3); };
+-void KSlideApp::setModeCo()  { setMode(4); };
+-void KSlideApp::setModeCr()  { setMode(5); };
+-void KSlideApp::setModeDi()  { setMode(6); };
+-void KSlideApp::setModeIs()  { setMode(7); };
+-void KSlideApp::setModeTr()  { setMode(8); };
+-void KSlideApp::setModeNo()  { setMode(9); };
++void KSlideApp::setModeCl3() { setMode(0); }
++void KSlideApp::setModeCl4() { setMode(1); }
++void KSlideApp::setModeCl5() { setMode(2); }
++void KSlideApp::setModeCl6() { setMode(3); }
++void KSlideApp::setModeCo()  { setMode(4); }
++void KSlideApp::setModeCr()  { setMode(5); }
++void KSlideApp::setModeDi()  { setMode(6); }
++void KSlideApp::setModeIs()  { setMode(7); }
++void KSlideApp::setModeTr()  { setMode(8); }
++void KSlideApp::setModeNo()  { setMode(9); }
+ 
+ void KSlideApp::scramblingStarted()
+ {
