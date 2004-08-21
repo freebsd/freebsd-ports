84b05e1603d2173491cab6d79fc8e7b83c24bdfc
arved <arved@FreeBSD.org>
2004-08-21 15:40:36 +0000

    Fix build with gcc 3.4

---
+++ editors/thoteditor/files/patch-UIcss.c
@@ -0,0 +1,10 @@
+--- thot/UIcss.c.orig	Sat Aug 21 17:34:06 2004
++++ ../thot/UIcss.c	Sat Aug 21 17:34:27 2004
+@@ -1117,6 +1117,7 @@
+ #ifdef DEBUG_CSS
+ 	    fprintf (stderr, "CSSCallbackHandle: undefined button value.\n");
+ #endif
++            break;
+ 	  }
+       break;
+ 
