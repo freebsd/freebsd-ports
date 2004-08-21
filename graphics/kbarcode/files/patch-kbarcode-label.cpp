97ab542de301c7f0755e8cfd20ed46ab70253cc9
markus <markus@FreeBSD.org>
2004-08-21 09:16:46 +0000

    Fix build with gcc 3.4

---
+++ graphics/kbarcode/files/patch-kbarcode-label.cpp
@@ -0,0 +1,9 @@
+--- kbarcode/label.cpp.orig	Sat Aug 21 11:06:41 2004
++++ kbarcode/label.cpp	Sat Aug 21 11:06:53 2004
+@@ -369,5 +369,5 @@
+         return d->getId();
+ 
+     return -1;
+-};
++}
+ 
