dbeb78eda8b50dd124f411b98d6813d2c52305c8
pav <pav@FreeBSD.org>
2004-08-21 16:34:57 +0000

    Add vmd, a molecular visualization program for displaying, animating, and analyzing large biomolecular systems using 3-D graphics and built-in scripting.
    PR:		ports/70509
    Submitted by:	Stephen Montgomery-Smith <stephen@math.missouri.edu>

---
+++ science/vmd/files/patch-AtomParser.C
@@ -0,0 +1,11 @@
+--- src/AtomParser.C-xxx	Fri Dec  5 13:21:53 2003
++++ src/AtomParser.C	Thu Aug 12 00:24:00 2004
+@@ -109,7 +109,7 @@
+ #define	YYCONST
+ #endif
+ 
+-#if !defined(_MSC_VER) && !defined(ARCH_MACOSX)
++#if !defined(_MSC_VER) && !defined(ARCH_MACOSX) && !defined(ARCH_FREEBSD)
+ #include <values.h>
+ #endif
+ 
