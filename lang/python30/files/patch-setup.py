c2605509ebe75f6595d4ea386ebb9becd0359208
perky <perky@FreeBSD.org>
2004-08-21 01:02:12 +0000

    - Add support for FreeBSD 6. - Drop FreeBSD 2 support. - Fix build on -stable.
    Spotted by:	marcus, kris
    Obtained from:	Python CVS

--- lang/python30/files/patch-setup.py
+++ lang/python30/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	Sun Jun 27 07:29:42 2004
-+++ setup.py	Thu Jul  8 23:55:04 2004
+--- setup.py.orig	Fri Jul 23 11:50:10 2004
++++ setup.py	Sat Aug 21 09:04:58 2004
 @@ -15,7 +15,7 @@
  from distutils.command.install_lib import install_lib
  
@@ -9,7 +9,16 @@
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (at the front) if
-@@ -1160,8 +1160,7 @@
+@@ -782,7 +782,7 @@
+             # Linux-specific modules
+             exts.append( Extension('linuxaudiodev', ['linuxaudiodev.c']) )
+ 
+-        if platform in ('linux2', 'freebsd4', 'freebsd5'):
++        if platform in ('linux2', 'freebsd4', 'freebsd5', 'freebsd6'):
+             exts.append( Extension('ossaudiodev', ['ossaudiodev.c']) )
+ 
+         if platform == 'sunos5':
+@@ -1164,8 +1164,7 @@
            ext_modules=[Extension('struct', ['structmodule.c'])],
  
            # Scripts to install
