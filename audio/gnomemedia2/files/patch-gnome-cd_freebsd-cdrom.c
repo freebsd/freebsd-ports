0ed4bdec98e0a566b9519db4e9f6c2c3331603aa
marcus <marcus@FreeBSD.org>
2004-08-22 05:07:55 +0000

    Properly initialize the length struct member for the previous patch that updates track length status.

--- audio/gnomemedia2/files/patch-gnome-cd_freebsd-cdrom.c
+++ audio/gnomemedia2/files/patch-gnome-cd_freebsd-cdrom.c
@@ -1,6 +1,15 @@
---- gnome-cd/freebsd-cdrom.c.orig	Sun Aug 22 00:42:31 2004
-+++ gnome-cd/freebsd-cdrom.c	Sun Aug 22 00:48:36 2004
-@@ -903,6 +903,11 @@
+--- gnome-cd/freebsd-cdrom.c.orig	Mon Jan  5 23:12:49 2004
++++ gnome-cd/freebsd-cdrom.c	Sun Aug 22 01:06:01 2004
+@@ -896,6 +896,8 @@
+ 
+ 	ASSIGN_MSF (realstatus->relative, blank_msf);
+ 	ASSIGN_MSF (realstatus->absolute, blank_msf);
++	ASSIGN_MSF (realstatus->length, blank_msf);
++
+ 	realstatus->track = 1;
+ 	switch (subchnl.data->header.audio_status) {
+ 	case CD_AS_PLAY_IN_PROGRESS:
+@@ -903,6 +905,11 @@
  		ASSIGN_MSF (realstatus->relative, subchnl.data->what.position.reladdr.msf);
  		ASSIGN_MSF (realstatus->absolute, subchnl.data->what.position.absaddr.msf);
  		realstatus->track = subchnl.data->what.position.track_number;
@@ -12,7 +21,7 @@
  		break;
  
  	case CD_AS_PLAY_PAUSED:
-@@ -910,6 +915,11 @@
+@@ -910,6 +917,11 @@
  		ASSIGN_MSF (realstatus->relative, subchnl.data->what.position.reladdr.msf);
  		ASSIGN_MSF (realstatus->absolute, subchnl.data->what.position.absaddr.msf);
  		realstatus->track = subchnl.data->what.position.track_number;
@@ -24,7 +33,7 @@
  		break;
  
  	case CD_AS_PLAY_COMPLETED:
-@@ -917,6 +927,11 @@
+@@ -917,6 +929,11 @@
  		ASSIGN_MSF (realstatus->relative, subchnl.data->what.position.reladdr.msf);
  		ASSIGN_MSF (realstatus->absolute, subchnl.data->what.position.absaddr.msf);
  		realstatus->track = subchnl.data->what.position.track_number;
