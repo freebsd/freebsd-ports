ce0b5caa77bcb643b9429cd7f93c911a66ce66e4
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-08-22 22:05:58 +0000

    Fix build with gcc-3.4.2.
    Reported by:	pointyhat

---
+++ math/guppi/files/patch-src_libguppipilot_guppi-view-interval.c
@@ -0,0 +1,10 @@
+--- src/libguppipilot/guppi-view-interval.c.orig	Sun Aug 22 18:02:50 2004
++++ src/libguppipilot/guppi-view-interval.c	Sun Aug 22 18:03:02 2004
+@@ -227,6 +227,7 @@
+ #endif
+ 
+   default:
++    ;
+   }
+ 
+   return TRUE;
