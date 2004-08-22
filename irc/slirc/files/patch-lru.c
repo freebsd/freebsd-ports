45f810649fb8edb0ee3ac5f86216b804e11c2273
krion <krion@FreeBSD.org>
2004-08-22 15:16:30 +0000

    Fix build with gcc-3.4

--- irc/slirc/files/patch-lru.c
+++ irc/slirc/files/patch-lru.c
@@ -1,5 +1,8 @@
---- lru.c.orig	Sun Aug 22 20:14:28 1999
-+++ lru.c	Sun Jan  7 02:37:54 2001
+
+$FreeBSD$
+
+--- lru.c.orig	Sun Aug 22 13:14:28 1999
++++ lru.c	Sun Aug 22 17:15:12 2004
 @@ -253,8 +253,9 @@
  	if (el) {
  		do {
@@ -12,3 +15,11 @@
  			if (ix + len > SLtt_Screen_Cols - 2)
  				break;
  			if (ix) {
+@@ -283,6 +284,7 @@
+ 					Col = ColLRUmesg;
+ 					break;
+ 				case 'm':
++				break;
+ 				}
+ 				SLsmg_set_color(Col);
+ 			}
