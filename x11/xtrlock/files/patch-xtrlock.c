f44b4ed398f82d6e5fe095818e63c008041f034f
krion <krion@FreeBSD.org>
2004-08-22 16:10:38 +0000

    Fix build with gcc-3.4

--- x11/xtrlock/files/patch-xtrlock.c
+++ x11/xtrlock/files/patch-xtrlock.c
@@ -1,16 +1,26 @@
-*** xtrlock.c.orig	Sun Sep  1 08:46:29 2002
---- xtrlock.c	Tue May 18 17:45:48 2004
-***************
-*** 31,41 ****
-  #include <grp.h>
-  #include <limits.h>
-  #include <string.h>
-- #include <crypt.h>
-  #include <unistd.h>
-  #include <math.h>
-  #include <ctype.h>
-- #include <values.h>
-  
-  #ifdef SHADOW_PWD
-  #include <shadow.h>
---- 31,39 ----
+
+$FreeBSD$
+
+--- xtrlock.c.orig	Sun Aug 22 18:08:41 2004
++++ xtrlock.c	Sun Aug 22 18:08:58 2004
+@@ -31,11 +31,9 @@
+ #include <grp.h>
+ #include <limits.h>
+ #include <string.h>
+-#include <crypt.h>
+ #include <unistd.h>
+ #include <math.h>
+ #include <ctype.h>
+-#include <values.h>
+ 
+ #ifdef SHADOW_PWD
+ #include <shadow.h>
+@@ -203,7 +201,7 @@
+         break;
+       }
+       break;
+-    default:
++    default: break;
+     }
+   }
+  loop_x:
