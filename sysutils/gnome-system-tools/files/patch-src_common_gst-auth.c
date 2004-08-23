2306868ad4eca0320c00c534f6f92f2a86569c96
marcus <marcus@FreeBSD.org>
2004-08-23 17:05:12 +0000

    * Add FreeBSD 6.X support * Try a slightly different patch to preserve the locale when authenticating

--- sysutils/gnome-system-tools/files/patch-src_common_gst-auth.c
+++ sysutils/gnome-system-tools/files/patch-src_common_gst-auth.c
@@ -1,27 +1,26 @@
---- src/common/gst-auth.c.orig	Sat Aug 21 01:01:33 2004
-+++ src/common/gst-auth.c	Sat Aug 21 01:06:24 2004
-@@ -300,6 +300,7 @@
+--- src/common/gst-auth.c.orig	Sun Aug  1 20:04:13 2004
++++ src/common/gst-auth.c	Mon Aug 23 13:00:40 2004
+@@ -300,14 +300,16 @@
  	lc_all      = getenv ("LC_ALL");
  	lang        = getenv ("LANG");
  
-+#ifndef __FreeBSD__
++	g_string_append (command, "env ");
++
  	if (lc_all)
- 		g_string_append_printf (command, "export LC_ALL=\"%s\" && ", lc_all);
- 	else if (lc_messages)
-@@ -308,6 +309,16 @@
- 		g_string_append_printf (command, "export LANG=\"%s\" && ", lang);
- 	else if (language)
- 		g_string_append_printf (command, "export LANGUAGE=\"%s\" && ", language);
-+#else
-+	if (lc_all)
-+		g_string_append_printf (command, "env LC_ALL=\"%s\" ", lc_all);
-+	else if (lc_messages)
-+		g_string_append_printf (command, "env LC_MESSAGES=\"%s\" ", lc_messages);
-+	else if (lang)
-+		g_string_append_printf (command, "env LANG=\"%s\" ", lang);
-+	else if (language)
-+		g_string_append_printf (command, "env LANGUAGE=\"%s\" ", language);
-+#endif
+-		g_string_append_printf (command, "export LC_ALL=\"%s\" && ", lc_all);
+-	else if (lc_messages)
+-		g_string_append_printf (command, "export LC_MESSAGES=\"%s\" && ", lc_messages);
+-	else if (lang)
+-		g_string_append_printf (command, "export LANG=\"%s\" && ", lang);
+-	else if (language)
+-		g_string_append_printf (command, "export LANGUAGE=\"%s\" && ", language);
++		g_string_append_printf (command, "LC_ALL=\"%s\" ", lc_all);
++	if (lc_messages)
++		g_string_append_printf (command, "LC_MESSAGES=\"%s\" ", lc_messages);
++	if (lang)
++		g_string_append_printf (command, "LANG=\"%s\" ", lang);
++	if (language)
++		g_string_append_printf (command, "LANGUAGE=\"%s\" ", language);
  }
  
  void
