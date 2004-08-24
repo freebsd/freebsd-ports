e1a73c88d22df2373c55017ccf9b564d5e385499
mezz <mezz@FreeBSD.org>
2004-08-24 07:34:13 +0000

    Update to 0.25.1.
    PR:		ports/70807
    Submitted by:	Tom McLaughlin <tmclaugh@sdf.lonestar.org>

--- net/straw/files/patch-src::lib::LookupManager.py
+++ net/straw/files/patch-src::lib::LookupManager.py
@@ -1,14 +1,14 @@
---- src/lib/LookupManager.py.orig	Thu Nov 13 23:37:03 2003
-+++ src/lib/LookupManager.py	Thu Nov 13 23:38:28 2003
-@@ -6,9 +6,9 @@
- from error import log
+--- src/lib/LookupManager.py.orig	Thu Jul 15 15:15:58 2004
++++ src/lib/LookupManager.py	Mon Aug 16 22:20:25 2004
+@@ -5,9 +5,9 @@
+ import Config
  try:
      import adns, ADNS
 -    _have_adns = True
 +    _have_adns = False
  except ImportError:
--    log("No ADNS library found, using synchronous name lookups.")
-+    #log("No ADNS library found, using synchronous name lookups.")
+-    error.log("No ADNS library found, using synchronous name lookups.")
++#    error.log("No ADNS library found, using synchronous name lookups.")
      _have_adns = False
  
  MAX_TIMES = 3
