01e6779790ff4aa9198fe0abbbeea5950a379fce
Kirill Ponomarev <krion@FreeBSD.org>
2004-08-25 09:17:55 +0000

    Use RCNG
    PR:		ports/70862
    Submitted by:	maintainer

--- misc/proxyper/files/proxyper.sh
+++ misc/proxyper/files/proxyper.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
+#
+# $FreeBSD$
+#
+# PROVIDE: proxyper
+# REQUIRE: LOGIN
+# KEYWORD: FreeBSD shutdown
 
-dir="CHANGETHIS"
+proxyper_enable=${proxyper_enable:-"NO"}
 
-case "$1" in
-start)
+dir="%%BINDIR%%"
+
+. %%RC_SUBR%%
+
+name="proxyper"
+rcvar=`set_rcvar`
+
+required_files=${dir}/${name}.ini
+
+start_cmd="${name}_start"
+stop_cmd="${name}_stop"
+
+proxyper_start()
+{
 	echo -n " proxyper" 
-	su -m nobody -c "$dir/proxyper" 2>/dev/null >/dev/null &
-	;;
-stop)
-	kill `cat $dir/rc5desproxy.pid` && echo -n " proxyper"
-	;;
-*)
-	echo "Usage: `basename $0` {start|stop}" >&2
-	exit 64
-	;;
-esac
+	su -m nobody -c "${dir}/proxyper" >/dev/null 2>&1 &
+}
 
-exit 0
+proxyper_stop()
+{
+	kill `cat ${dir}/rc5desproxy.pid`
+	echo -n " proxyper" 
+}
+
+load_rc_config $name
+run_rc_command "$1"
