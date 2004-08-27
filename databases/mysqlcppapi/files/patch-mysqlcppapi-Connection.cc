a47d0e4abd8514186854dc182e3b8665f274a506
Sergey Matveychuk <sem@FreeBSD.org>
2004-08-27 18:40:15 +0000

    A fork of mysql++ C++ API for MySQL
    PR:		ports/70050
    Submitted by:	Jie Gao <gaoj@cpsc.ucalgary.ca>

---
+++ databases/mysqlcppapi/files/patch-mysqlcppapi-Connection.cc
@@ -0,0 +1,16 @@
+--- mysqlcppapi/Connection.cc.orig	Wed Aug  4 23:15:58 2004
++++ mysqlcppapi/Connection.cc	Wed Aug  4 23:18:13 2004
+@@ -211,9 +211,11 @@
+ bool Connection::shutdown ()
+ {
+   check_connection_is_open();
+-  
++#if  ( MYSQL_VERSION_ID == 40103 ) || ( MYSQL_VERSION_ID == 50001 )
++  bool suc = !(mysql_shutdown(m_sharedptr_connection.obj(),SHUTDOWN_DEFAULT));
++#else
+   bool suc = !(mysql_shutdown(m_sharedptr_connection.obj()));
+-
++#endif
+   if (!suc)
+     throw ex_BadQuery(error());
+ 
