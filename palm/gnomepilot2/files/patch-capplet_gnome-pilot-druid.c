0ff5763fb7bc1f85cb2053815d97be93f4e4680d
marcus <marcus@FreeBSD.org>
2004-08-27 05:09:42 +0000

    Update to 2.0.11.

--- palm/gnomepilot2/files/patch-capplet_gnome-pilot-druid.c
+++
@@ -1,10 +0,0 @@
---- capplet/gnome-pilot-druid.c.orig	Mon Aug  2 20:12:33 2004
-+++ capplet/gnome-pilot-druid.c	Mon Aug  2 20:12:44 2004
-@@ -181,6 +181,7 @@
- 	init_widgets (gpd);
- 
-  error:
-+	return;
- }
- 
- 
--- palm/gnomepilot2/files/patch-capplet_gnome-pilot-pdialog.c
+++
@@ -1,10 +0,0 @@
---- capplet/gnome-pilot-pdialog.c.orig	Mon Aug  2 20:11:37 2004
-+++ capplet/gnome-pilot-pdialog.c	Mon Aug  2 20:11:43 2004
-@@ -144,6 +144,7 @@
- 	}
- 
-  error:
-+	return;
- }
- 
- 
