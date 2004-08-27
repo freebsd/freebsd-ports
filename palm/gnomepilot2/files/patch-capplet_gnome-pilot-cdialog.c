7dc5f75888be5c2753f0f9e9a072e793ec632752
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-08-27 05:09:42 +0000

    Update to 2.0.11.

--- palm/gnomepilot2/files/patch-capplet_gnome-pilot-cdialog.c
+++
@@ -1,10 +0,0 @@
---- capplet/gnome-pilot-cdialog.c.orig	Mon Aug  2 20:12:19 2004
-+++ capplet/gnome-pilot-cdialog.c	Mon Aug  2 20:12:25 2004
-@@ -123,6 +123,7 @@
- 	gnome_dialog_close_hides (GNOME_DIALOG (priv->dialog), TRUE);
- 	
-  error:
-+	return;
- }
- 
- 
--- palm/gnomepilot2/files/patch-capplet_gnome-pilot-ddialog.c
+++
@@ -1,11 +0,0 @@
---- capplet/gnome-pilot-ddialog.c.orig	Tue Apr 22 23:12:36 2003
-+++ capplet/gnome-pilot-ddialog.c	Thu Aug  5 14:38:01 2004
-@@ -123,6 +123,7 @@
- 	}
- 
-  error:
-+	return;
- }
- 
- 
-
