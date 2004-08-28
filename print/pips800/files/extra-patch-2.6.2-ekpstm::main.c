60206ddf46f33a69209470e29558d2351980015b
ume <ume@FreeBSD.org>
2004-08-28 21:46:11 +0000

    more for PX-G900.

---
+++ print/pips800/files/extra-patch-2.6.2-ekpstm::main.c
@@ -0,0 +1,12 @@
+Index: ekpstm/main.c
+diff -u -p ekpstm/main.c.orig ekpstm/main.c
+--- ekpstm/main.c.orig	Thu Jul 31 15:01:39 2003
++++ ekpstm/main.c	Tue Aug 24 19:41:40 2004
+@@ -69,6 +69,7 @@ main (int argc, char *argv[])
+           break;
+ 	  
+         default:
++          break;
+         }
+     }
+   
