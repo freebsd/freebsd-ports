af23ca47e9890fa00fbec2df21324c6b4e25fee8
Mark Linimon <linimon@FreeBSD.org>
2004-08-28 05:50:11 +0000

    Update to 0.8.0 and assign maintainership.
    PR:		ports/70895
    Submitted by:	Sergio Mangialardi <sergio at softshark dot org>

--- devel/py-mx-experimental/files/patch-mxEXPERIMENTAL.py
+++ devel/py-mx-experimental/files/patch-mxEXPERIMENTAL.py
@@ -1,15 +1,15 @@
---- mxEXPERIMENTAL.py.orig	Mon Nov 26 12:44:48 2001
-+++ mxEXPERIMENTAL.py	Mon Nov 26 12:45:52 2001
-@@ -140,7 +140,7 @@
+--- mxEXPERIMENTAL.py.orig	Sat Aug 21 20:34:49 2004
++++ mxEXPERIMENTAL.py	Sat Aug 21 20:38:15 2004
+@@ -133,7 +133,7 @@
  
              mx_Extension('mx.Number.mxNumber.mxNumber',
                           ['mx/Number/mxNumber/mxNumber.c'],
--                         include_dirs=['mx/Number/mxNumber']),
-+                         include_dirs=['mx/Number/mxNumber', 'LIBGMPH']),
- 
-             ]
- else:
-@@ -160,7 +160,6 @@
+-                         include_dirs=['mx/Number/mxNumber'],
++                         include_dirs=['mx/Number/mxNumber', 'LIBGMPH'],
+                          libraries=['gmp'],
+                          optional_libraries=[('mpfr', ['gmp.h', 'mpfr.h']),
+                                              ]),
+@@ -156,7 +156,6 @@
      'mx/Number/README',
      'mx/Number/mxNumber/mxNumber.h',
      'mx/Number/mxNumber/mxh.h',
