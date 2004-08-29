b3cb95d13e3806637bab77f698b7364b804a5e9f
perky <perky@FreeBSD.org>
2004-08-29 16:41:14 +0000

    Update to 1.0.0 and assign maintainership to submitter.
    PR:		71018
    Submitted by:	June-Yen Huang <jihuang@gate.sinica.edu.tw>

--- devel/cvs2svn/files/setup.py
+++
@@ -1,8 +0,0 @@
-__version__ = '$FreeBSD$'
-from distutils.core import setup
-setup(	name = 'cvs2svn',
-	version = '0.0.938',
-	packages = ['rcsparse'],
-	scripts = ['cvs2svn', 'profile-cvs2svn',
-		   'verify-cvs2svn', 'dump-db', 'show-db'],
-)
