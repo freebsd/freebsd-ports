3bbb9c4567531218e09e7334da07dc8bffac9090
Pav Lucistnik <pav@FreeBSD.org>
2004-08-29 18:28:06 +0000

    Add gkrellmoon2, a GKrellM 2 plugin showing a moon phaze.
    PR:		ports/70937
    Submitted by:	Jan Srzednicki <w@wrzask.pl>

---
+++ astro/gkrellmoon2/files/patch-CalcEphem.c
@@ -0,0 +1,10 @@
+--- CalcEphem.c.orig	Thu Aug 15 20:41:03 2002
++++ CalcEphem.c	Thu Aug 15 20:41:23 2002
+@@ -8,6 +8,7 @@
+ #ifdef HAVE_CONFIG_H
+ #include <config.h>
+ #endif
++#include <string.h>
+ 
+ #include "CalcEphem.h"
+ #include "Moon.h"
