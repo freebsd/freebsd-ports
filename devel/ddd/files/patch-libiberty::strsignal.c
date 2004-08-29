5068ccfb52c4ee4a8d0d326579a4a19933102409
pav <pav@FreeBSD.org>
2004-08-29 20:17:24 +0000

    - Update to 3.3.9 - Grant maintainership to submitter
    PR:		ports/70894
    Submitted by:	Sergio Mangialardi <sergio@softshark.org>

--- devel/ddd/files/patch-libiberty::strsignal.c
+++ devel/ddd/files/patch-libiberty::strsignal.c
@@ -1,5 +1,5 @@
---- libiberty/strsignal.c.orig	Wed Oct 22 14:46:55 2003
-+++ libiberty/strsignal.c	Fri Feb 27 13:33:02 2004
+--- libiberty/strsignal.c.orig	Thu Aug 19 20:58:24 2004
++++ libiberty/strsignal.c	Thu Aug 19 20:59:24 2004
 @@ -241,6 +241,7 @@
     same name, it differs from other implementations in that it is dynamically
     initialized rather than statically initialized. */
@@ -8,11 +8,12 @@
  #ifndef HAVE_SYS_SIGLIST
  
  static int sys_nsig;
-@@ -257,6 +258,7 @@
- #endif
+@@ -258,7 +259,7 @@
  extern const char * const sys_siglist[];
  
-+#endif
  #endif
+-
++#endif
  
+ /*
  
