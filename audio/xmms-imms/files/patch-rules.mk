c1cec095666745b87149595056975a3e7edeeda1
green <green@FreeBSD.org>
2004-09-01 01:26:20 +0000

    Update to IMMS version 1.2a.  The functionality for classification has been split back out so this is a General, not a Visualization, XMMS plug-in. The sox program is now used to scale music for classification, and in general things should be far faster using more optimal SQL with SQLite 3. Since analysis is not integrated with XMMS playback directly, instead of "demand-classification" the analyze program can be run beforehand and you will immediately start off with smoother transitions between musical genres.

--- audio/xmms-imms/files/patch-rules.mk
+++ audio/xmms-imms/files/patch-rules.mk
@@ -1,11 +1,11 @@
---- rules.mk.orig	Sun Jan 25 16:55:21 2004
-+++ rules.mk	Wed Feb 11 18:43:22 2004
-@@ -57,7 +57,7 @@
- 	$(warning Use 'make install-user' to install for the current user only.)
+--- rules.mk.orig	Wed Aug 18 01:56:28 2004
++++ rules.mk	Tue Aug 31 16:13:51 2004
+@@ -26,7 +26,7 @@
+ endif
  
- install-system: libimms.so
--	${INSTALL_PROGRAM} libimms.so ${DESTDIR}`xmms-config --visualization-plugin-dir`
-+	${INSTALL_PROGRAM} libimms.so `xmms-config --visualization-plugin-dir`
+ define installprogs
+-    ${INSTALL_PROGRAM} analyzer immsremote immstool ${PREFIX}
++    ${INSTALL_PROGRAM} analyzer immsremote immstool ${PREFIX}/bin
+ endef
  
- user-message:
- 	$(warning Defaulting to installing for current user only.)
+ system-message:
