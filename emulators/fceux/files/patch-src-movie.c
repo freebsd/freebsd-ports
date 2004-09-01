50571a7624d0b58eda7f0d2cf4c775a14d16e3c3
krion <krion@FreeBSD.org>
2004-09-01 07:59:07 +0000

    Update to 0.98.12
    PR:		ports/71197
    Submitted by:	maintainer

--- emulators/fceux/files/patch-src-movie.c
+++ emulators/fceux/files/patch-src-movie.c
@@ -1,5 +1,5 @@
---- src/movie.c.orig	Sat Aug 14 07:13:51 2004
-+++ src/movie.c	Sat Aug 14 07:15:58 2004
+--- src/movie.c.orig	Tue Aug 31 07:35:21 2004
++++ src/movie.c	Tue Aug 31 07:35:57 2004
 @@ -154,7 +154,7 @@
  
  void FCEUMOV_AddJoy(uint8 *js)
@@ -9,10 +9,10 @@
  
   if(!current) return;	/* Not playback nor recording. */
  
-@@ -190,7 +190,7 @@
-    tmp >>= 5;
+@@ -191,7 +191,7 @@
     tmp &= 0x3;
     ti=0;
+ 
 -   int tmpfix = tmp;
 +   tmpfix = tmp;
     while(tmp--) { nextts |= fgetc(slots[-1 - current]) << (ti * 8); ti++; }
