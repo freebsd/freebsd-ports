93ad5c3f67962054387e1f5ebfd8523480238c6a
Volker Stolz <vs@FreeBSD.org>
2004-09-01 08:00:14 +0000

    - Update to 1.8.2 - RUN_DEPEND on timidity
    PR:		ports/71207
    Submitted by:	Igor Pokrovsky (maintainer)

--- games/deng/files/patch-Src_dsSDLMixer_driver__sdlmixer.c
+++ games/deng/files/patch-Src_dsSDLMixer_driver__sdlmixer.c
@@ -1,10 +1,10 @@
---- Src/dsSDLMixer/driver_sdlmixer.c.orig	Thu Aug 26 19:07:44 2004
-+++ Src/dsSDLMixer/driver_sdlmixer.c	Thu Aug 26 19:11:15 2004
-@@ -117,7 +117,7 @@
+--- Src/dsSDLMixer/driver_sdlmixer.c.orig	Tue Aug 31 21:53:06 2004
++++ Src/dsSDLMixer/driver_sdlmixer.c	Tue Aug 31 21:53:21 2004
+@@ -130,7 +130,7 @@
  		return false;
  	}
  
--	if(Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024))
+-	if(Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 2048))
 +	if(Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 4096))
  	{
  		Error();
