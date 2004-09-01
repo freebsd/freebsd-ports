94cbdc57429f24c30d892a0aae103d9bf1e3e0d6
ijliao <ijliao@FreeBSD.org>
2004-09-01 08:37:04 +0000

    add classpath 0.10 A GNU project to create free core class libraries

---
+++ java/classpath/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Wed Sep  1 15:46:52 2004
++++ ltmain.sh	Wed Sep  1 15:47:02 2004
+@@ -5550,10 +5550,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
