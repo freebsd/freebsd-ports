d2d87f21b33a530fa2605d1c912373b99f3b9adb
Edwin Groothuis <edwin@FreeBSD.org>
2004-09-02 04:14:21 +0000

    [MAINTAINER UPDATE] sysutils/est
        Update port to version 0.6.  This adds a sysctl "hw.est_verbose"
        which controls whether frequency changes are logged, and converts
        the rc.d script to use the rcNG framework.
    
        Note that the module will now only load if 'est_enable="YES"'
        appears in rc.conf, rc.conf.local, or rc.conf.d/est.
    
    PR:		ports/71269
    Submitted by:	Colin Percival <cperciva@daemonology.net>

--- sysutils/est/files/est.sh
+++ sysutils/est/files/est.sh
@@ -1,20 +1,44 @@
 #!/bin/sh
+#
+# $FreeBSD$
+#
 
-kmoddir=@@PREFIX@@/modules
-kmod=est.ko
+# PROVIDE: est
+# REQUIRE: abi
+# BEFORE: securelevel
+# KEYWORD: FreeBSD
 
-case "$1" in
-start)
-	if [ -x $kmoddir/$kmod ]; then
-	    echo -n ' est'
-	    /sbin/kldload $kmoddir/$kmod
+# Define est_* variables in one of these files:
+#	/etc/rc.conf
+#	/etc/rc.conf.local
+#	/etc/rc.conf.d/est
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE
+#
+est_enable="NO"
+est_verbose="NO"
+
+. %%RC_SUBR%%
+
+name="est"
+rcvar=`set_rcvar`
+
+start_cmd="est_start"
+stop_cmd="est_stop"
+
+load_rc_config $name
+
+kmoddir="%%PREFIX%%/modules"
+
+est_start () {
+	/sbin/kldload ${kmoddir}/est.ko
+	if checkyesno est_verbose; then
+		sysctl hw.est_verbose=1
 	fi
-	;;
-stop)
-	/sbin/kldunload $kmod && echo -n ' est'
-	;;
-*)
-	echo "Usage: `basename $0` {start|stop}" >&2
-	exit 64
-	;;
-esac
+}
+
+est_stop () {
+	/sbin/kldunload $name
+}
+
+run_rc_command "$1"
