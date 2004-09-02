0d3af27c5f7e14d9798f9136bb56936bab422ce3
arved <arved@FreeBSD.org>
2004-09-02 16:18:13 +0000

    Fix build with gcc 3.4
    PR:		71206
    Submitted by:	Eric van Gyzen <vangyzen@stat.duke.edu>

---
+++ math/jags/files/patch-src-lib-model-Model.cc
@@ -0,0 +1,19 @@
+--- src/lib/model/Model.cc.orig	Tue Aug 31 13:22:19 2004
++++ src/lib/model/Model.cc	Tue Aug 31 13:22:28 2004
+@@ -12,14 +12,14 @@
+ #include <sarray/RightRangeIterator.h>
+ #include <sarray/LeftRangeIterator.h>
+ 
+-#include <Rmath.h>
+-
+ #include <iostream>
+ #include <fstream>
+ #include <sstream>
+ #include <set>
+ #include <stdexcept>
+ #include <string>
++
++#include <Rmath.h>
+ 
+ using std::vector;
+ using std::list;
