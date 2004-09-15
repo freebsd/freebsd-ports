df8a93b6568c73776ec9404b4bb28281b4140fd5
Alexey Dokuchaev <danfe@FreeBSD.org>
2004-09-15 17:19:10 +0000

    Unbreak the build with recent GCC.
    Reported by:	pointyhat.freebsd.org
    Approved by:	portmgr (marcus), fjoe (mentor, implicit)

---
+++ astro/nova/files/patch-src::vega::grid.cc
@@ -0,0 +1,10 @@
+--- src/vega/grid.cc.orig	Wed Sep 15 16:21:00 2004
++++ src/vega/grid.cc	Wed Sep 15 16:21:47 2004
+@@ -15,6 +15,7 @@
+ // Copyright Liam Girdwood 2003
+ 
+ 
++#include <math.h>
+ #include "grid.hh"
+ 
+ namespace Vega
