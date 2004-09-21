2838bc4361dfc9f54f4a005ed4ff03bdc4b5dac4
ale <ale@FreeBSD.org>
2004-09-21 21:09:31 +0000

    Update to 4.0.21 release: - Fix mysqlhotcopy insecure temporary file creation - Fix bug in remote connections caused by libwrap [1]
    PR:		ports/70889 [1]
    Submitted by:	Andreas Haakh <andreas.haakh@ib-haakh.de> [1]
    Patch by:	peadar [1]
    Approved by:	portmgr (marcus)

--- databases/mysql40-server/files/patch-sql::mysqld.cc
+++ databases/mysql40-server/files/patch-sql::mysqld.cc
@@ -1,12 +1,19 @@
---- sql/mysqld.cc.orig	Wed Feb  5 01:55:03 2003
-+++ sql/mysqld.cc	Wed Feb  5 01:57:06 2003
-@@ -104,7 +104,8 @@
+--- sql/mysqld.cc.orig	Tue Sep  7 00:29:37 2004
++++ sql/mysqld.cc	Mon Sep 20 08:37:21 2004
+@@ -107,15 +107,7 @@
  int allow_severity = LOG_INFO;
  int deny_severity = LOG_WARNING;
  
 -#ifdef __STDC__
-+#include <osreldate.h>
-+#if defined(__STDC__) && __FreeBSD_version < 500000
- #define my_fromhost(A)	   fromhost(A)
- #define my_hosts_access(A) hosts_access(A)
- #define my_eval_client(A)  eval_client(A)
+-#define my_fromhost(A)	   fromhost(A)
+-#define my_hosts_access(A) hosts_access(A)
+-#define my_eval_client(A)  eval_client(A)
+-#else
+-#define my_fromhost(A)	   fromhost()
+-#define my_hosts_access(A) hosts_access()
+-#define my_eval_client(A)  eval_client()
+-#endif
++#include "mylibwrap.h"
+ #endif /* HAVE_LIBWRAP */
+ 
+ #ifdef HAVE_SYS_MMAN_H
