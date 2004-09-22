5a1e6a8f8fb25f631c5c1d445a2a913a64ac9fc6
Alex Dupre <ale@FreeBSD.org>
2004-09-22 22:22:32 +0000

    Update to 4.1.15 release: - Fix a buffer overflow in prepared statements API - Fix bug in remote connections caused by libwrap [1]
    Patch by:       peadar [1]
    Approved by:    portmgr (marcus)

--- databases/mysql41-server/files/patch-sql::mysqld.cc
+++ databases/mysql41-server/files/patch-sql::mysqld.cc
@@ -1,12 +1,19 @@
---- sql/mysqld.cc.orig	Wed Feb  5 01:55:03 2003
-+++ sql/mysqld.cc	Wed Feb  5 01:57:06 2003
-@@ -104,7 +104,8 @@
+--- sql/mysqld.cc.orig	Thu Sep 16 23:47:47 2004
++++ sql/mysqld.cc	Tue Sep 21 23:56:49 2004
+@@ -115,15 +115,7 @@
  int allow_severity = LOG_INFO;
  int deny_severity = LOG_WARNING;
  
 -#ifdef __STDC__
-+#include <osreldate.h>
-+#if defined(__STDC__) && __FreeBSD_version < 500000
- #define my_fromhost(A)	   fromhost(A)
- #define my_hosts_access(A) hosts_access(A)
- #define my_eval_client(A)  eval_client(A)
+-#define my_fromhost(A)	   fromhost(A)
+-#define my_hosts_access(A) hosts_access(A)
+-#define my_eval_client(A)  eval_client(A)
+-#else
+-#define my_fromhost(A)	   fromhost()
+-#define my_hosts_access(A) hosts_access()
+-#define my_eval_client(A)  eval_client()
+-#endif /* __STDC__ */
++#include "mylibwrap.h"
+ #endif /* HAVE_LIBWRAP */
+ 
+ #ifdef HAVE_SYS_MMAN_H
