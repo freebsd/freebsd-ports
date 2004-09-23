942f1d54f01187fbcb5f7724b2170a9199612b03
Will Andrews <will@FreeBSD.org>
2004-09-23 02:07:36 +0000

    Update to 1.16 to fix distfile fetch & support reload rc.d command.
    PR:		71901
    Submitted by:	MAINTAINER
    Approved by:	portmgr (self)

--- mail/postgrey/files/postgrey.sh
+++ mail/postgrey/files/postgrey.sh
@@ -23,6 +23,7 @@ rcvar=`set_rcvar`
 command=%%PREFIX%%/sbin/postgrey
 command_interpreter=/usr/bin/perl
 required_dirs=/var/db/postgrey
+extra_commands=reload
 
 stop_postcmd=stop_postcmd
 
@@ -38,6 +39,7 @@ postgrey_pidfile=${postgrey_pidfile:-"/var/run/postgrey.pid"}
 postgrey_flags=${postgrey_flags:-"--pidfile=${postgrey_pidfile} \
 	--inet=10023 -d --user=postgrey --dbdir=/var/db/postgrey \
 	--whitelist-clients=%%PREFIX%%/etc/postfix/postgrey_whitelist_clients \
+	--whitelist-clients=%%PREFIX%%/etc/postfix/postgrey_whitelist_clients.local \
 	--whitelist-recipients=%%PREFIX%%/etc/postfix/postgrey_whitelist_recipients"}
 
 pidfile="${postgrey_pidfile}"
