1d7281f7a1f6ed33d3665fc3b662fd0ea695b5f0
eik <eik@FreeBSD.org>
2004-10-09 10:16:33 +0000

    Fix process substitution on systems without fdescfs (FreeBSD 5.x, 6.x)
    While I'm here, use the official patchset instead of the custom ones
    and reflect the port revision in the version number.
    
    Submitted by:	Reinhard Speyerer <rspmn@arcor.de>
    Approved by:	portmgr (krion)

--- shells/bash/files/patch-subst.c
+++ shells/bash/files/patch-subst.c
@@ -1,16 +1,16 @@
 #
 # Fix broken array expansion
 #
-# http://lists.gnu.org/archive/html/bug-bash/2004-08/msg00192.html
+# http://lists.gnu.org/archive/html/bug-bash/2004-09/msg00097.html
 #
---- subst.c.orig	Sun Jul  4 13:56:13 2004
-+++ subst.c	Thu Aug 12 13:36:17 2004
-@@ -4891,7 +4891,7 @@
-   if (*e1p < 0)		/* negative offsets count from end */
-     *e1p += len;
- 
--  if (*e1p >= len || *e1p < 0)
-+  if (*e1p > len || *e1p < 0)
-     return (-1);
- 
- #if defined (ARRAY_VARS)
+--- subst.c.orig	Thu Aug 12 13:36:17 2004
++++ subst.c	Thu Sep  9 11:54:22 2004
+@@ -4900,7 +4900,7 @@
+     case VT_ARRAYVAR:
+       a = (ARRAY *)value;
+       /* For arrays, the first value deals with array indices. */
+-      len = array_max_index (a);	/* arrays index from 0 to n - 1 */
++      len = array_max_index (a) + 1;	/* arrays index from 0 to n - 1 */
+       break;
+ #endif
+     }
