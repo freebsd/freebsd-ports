89486ee4a68fa34c54643ce456fc591a094194cf
eik <eik@FreeBSD.org>
2004-10-11 23:48:25 +0000

    - update to Exim 4.43 and exiscan 28 - add support for the SA-Exim local_scan function   + http://marc.merlins.org/linux/exim/sa.html - new options WITH_SA_EXIM, WITH_AUTH_SASL, WITH_RADIUS_TYPE
    - fix 150.exim-tidydb.sh when Exim is installed, but not run [1]
    
    Submitted by:	Brian Somers <brian@Awfulhak.org> [1]

--- mail/exim/files/150.exim-tidydb.sh
+++ mail/exim/files/150.exim-tidydb.sh
@@ -14,17 +14,18 @@ if [ -r /etc/defaults/periodic.conf ]; then
     source_periodic_confs
 fi
 
-exim_tidydb_enable=${exim_tidydb_enable:-"YES"}
-exim_tidydb=${exim_tidydb:-"/usr/local/sbin/exim_tidydb"}
-exim_dbdir=${exim_dbdir:-"/var/spool/exim"}
+: ${exim_tidydb_enable="YES"}
+: ${exim_tidydb="%%PREFIX%%/sbin/exim_tidydb"}
+: ${exim_dbdir="/var/spool/exim"}
 
 case "$exim_tidydb_enable" in
     [Yy][Ee][Ss])
 	echo ""
 	echo "Tidying Exim hints databases:"
 	for db in "$exim_dbdir"/db/*.lockfile; do
-	    echo ""
-	    "$exim_tidydb" "$exim_dbdir" `basename $db .lockfile`
+	    [ "$db" = "$exim_dbdir/db/*.lockfile" ] && continue
+	    echo
+	    "$exim_tidydb" "$exim_dbdir" `basename "$db" .lockfile`
 	done
 	rc=1;;
 
