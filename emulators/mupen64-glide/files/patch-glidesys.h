6a2753b940fb71400b6b4db0d2304804dabfb1dc
Dirk Meyer <dinoex@FreeBSD.org>
2004-10-12 04:58:19 +0000

    Glide64 graphics plugin for Mupen64 - Mupen64 is a highly portable Nintendo 64 emulator. It has been developed on/for Linux originally but has already been ported successfully to other operating systems. The program can easily be ported to all operating systems supported by the SDL library.
    In its current state, the emulator is highly compatible and uses a plugin
    system. With the correct plugins ("correct" can be computer dependent), it
    can achieve nearly perfect graphics and sound in many games.
    
    WWW: http://mupen64.emulation64.com/
    
    PR:		71895
    Submitted by:	Travis Poppe

---
+++ emulators/mupen64-glide/files/patch-glidesys.h
@@ -0,0 +1,11 @@
+--- wrapper/glidesys.h.orig	Sun Jun 13 23:58:12 2004
++++ wrapper/glidesys.h	Thu Aug 12 17:14:11 2004
+@@ -96,7 +96,7 @@
+ #endif
+ 
+ /* Check for OS */
+-#if defined(__IRIX__) || defined(__sparc__) || defined(__linux__)
++#if defined(__IRIX__) || defined(__sparc__) || defined(__linux__) || defined(__FreeBSD__)
+ #  define GLIDE_OS        GLIDE_OS_UNIX
+ #elif defined(__DOS__)
+ #  define GLIDE_OS        GLIDE_OS_DOS32
