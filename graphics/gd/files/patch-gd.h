126c2636098a1dab1463471a89a74d5331ecc34c
dinoex <dinoex@FreeBSD.org>
2004-10-12 05:17:13 +0000

    - update to 2.0.28 drop patch-gdft.c, as it won't apply after 2.0.25

--- graphics/gd/files/patch-gd.h
+++ graphics/gd/files/patch-gd.h
@@ -1,19 +1,17 @@
---- gd.h.orig	Tue May 25 13:05:50 2004
-+++ gd.h	Wed May 26 09:30:23 2004
-@@ -283,6 +283,12 @@
+--- gd.h.orig	Sat Jul 17 20:02:38 2004
++++ gd.h	Wed Aug 25 13:38:59 2004
+@@ -278,6 +278,10 @@
     /* Deprecated in favor of gdImageCreateFromPngCtx */
  BGD_DECLARE(gdImagePtr) gdImageCreateFromPngSource (gdSourcePtr in);
  
 +#ifdef __FreeBSD__
-+BGD_DECLARE(gdImagePtr) gdImageCreateFromGif(FILE *fd);
-+BGD_DECLARE(gdImagePtr) gdImageCreateFromGifCtx(gdIOCtxPtr in);
 +BGD_DECLARE(gdImagePtr) gdImageCreateFromGifSource(gdSourcePtr in);
 +#endif
 +
  BGD_DECLARE(gdImagePtr) gdImageCreateFromGd (FILE * in);
  BGD_DECLARE(gdImagePtr) gdImageCreateFromGdCtx (gdIOCtxPtr in);
  BGD_DECLARE(gdImagePtr) gdImageCreateFromGdPtr (int size, void *data);
-@@ -411,6 +417,9 @@
+@@ -407,6 +411,9 @@
    gdPoint, *gdPointPtr;
  
  BGD_DECLARE(void) gdImagePolygon (gdImagePtr im, gdPointPtr p, int n, int c);
@@ -23,7 +21,7 @@
  BGD_DECLARE(void) gdImageFilledPolygon (gdImagePtr im, gdPointPtr p, int n, int c);
  
  /* These functions still work with truecolor images, 
-@@ -512,6 +521,16 @@
+@@ -517,6 +524,16 @@
  /* Best to free this memory with gdFree(), not free() */
  BGD_DECLARE(void *) gdImageJpegPtr (gdImagePtr im, int *size, int quality);
  
@@ -40,15 +38,12 @@
  /* A custom data sink. For backwards compatibility. Use
  	gdIOCtx instead. */
  /* The sink function must return -1 on error, otherwise the number
-@@ -525,6 +544,13 @@
+@@ -530,6 +547,10 @@
    gdSink, *gdSinkPtr;
  
  BGD_DECLARE(void) gdImagePngToSink (gdImagePtr im, gdSinkPtr out);
 +
 +#ifdef __FreeBSD__
-+BGD_DECLARE(void) gdImageGif(gdImagePtr im, FILE *out);
-+BGD_DECLARE(void *) gdImageGifPtr(gdImagePtr im, int *size);
-+BGD_DECLARE(void) gdImageGifCtx(gdImagePtr im, gdIOCtxPtr out);
 +BGD_DECLARE(void) gdImageGifToSink(gdImagePtr im, gdSinkPtr out);
 +#endif
  
