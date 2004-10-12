a78799ff3565f900a505905c454e335905b6e4d1
dinoex <dinoex@FreeBSD.org>
2004-10-12 04:58:19 +0000

    Glide64 graphics plugin for Mupen64 - Mupen64 is a highly portable Nintendo 64 emulator. It has been developed on/for Linux originally but has already been ported successfully to other operating systems. The program can easily be ported to all operating systems supported by the SDL library.
    In its current state, the emulator is highly compatible and uses a plugin
    system. With the correct plugins ("correct" can be computer dependent), it
    can achieve nearly perfect graphics and sound in many games.
    
    WWW: http://mupen64.emulation64.com/
    
    PR:		71895
    Submitted by:	Travis Poppe

---
+++ emulators/mupen64-glide/files/extra-patch-main.cpp
@@ -0,0 +1,41 @@
+--- wrapper/main.cpp.orig	Fri Jul  2 21:57:48 2004
++++ wrapper/main.cpp	Fri Aug 13 21:21:15 2004
+@@ -3,7 +3,7 @@
+ #include <stdarg.h>
+ #include <string.h>
+ #include <unistd.h>
+-#include <SDL/SDL.h>
++#include <SDL.h>
+ #endif // _WIN32
+ #include "glide.h"
+ #include <stdlib.h>
+@@ -20,9 +20,13 @@
+ #endif // _WIN32
+ 
+ 
++#ifndef __FreeBSD__
+ PFNGLACTIVETEXTUREARBPROC glActiveTextureARB;
++#endif
+ PFNGLBLENDFUNCSEPARATEEXTPROC glBlendFuncSeparateEXT;
++#ifndef __FreeBSD__
+ PFNGLMULTITEXCOORD2FARBPROC glMultiTexCoord2fARB;
++#endif
+ PFNGLFOGCOORDFPROC glFogCoordfEXT;
+ #ifdef _WIN32
+ PFNWGLGETEXTENSIONSSTRINGARBPROC wglGetExtensionsStringARB;
+@@ -501,6 +505,7 @@
+ 	if (isExtensionSupported("GL_ARB_texture_mirrored_repeat") == FALSE)
+ 		display_warning((unsigned char*)"Your video card doesn't support GL_ARB_texture_mirrored_repeat extension");
+ 
++#ifndef __FreeBSD__
+ #ifdef _WIN32
+ 	glActiveTextureARB = (PFNGLACTIVETEXTUREARBPROC)wglGetProcAddress("glActiveTextureARB");
+ 	glMultiTexCoord2fARB = (PFNGLMULTITEXCOORD2FARBPROC)wglGetProcAddress("glMultiTexCoord2fARB");
+@@ -508,6 +513,7 @@
+    glActiveTextureARB = (PFNGLACTIVETEXTUREARBPROC)SDL_GL_GetProcAddress("glActiveTextureARB");
+    glMultiTexCoord2fARB = (PFNGLMULTITEXCOORD2FARBPROC)SDL_GL_GetProcAddress("glMultiTexCoord2fARB");
+ #endif // _WIN32
++#endif
+ 	glGetIntegerv(GL_MAX_TEXTURE_UNITS_ARB, &nbTextureUnits);
+ 	if (nbTextureUnits == 1) display_warning((unsigned char*)"You need a video card that has at least 2 texture units");
+    
---
+++ emulators/mupen64-glide/files/extra-patch-main.h
@@ -0,0 +1,13 @@
+--- wrapper/main.h.orig	Sun Jun 20 21:08:48 2004
++++ wrapper/main.h	Fri Aug 13 21:17:22 2004
+@@ -16,8 +16,10 @@
+ void updateCombinera(int i);
+ 
+ //#ifdef _WIN32
++#ifndef __FreeBSD__
+ extern PFNGLACTIVETEXTUREARBPROC glActiveTextureARB;
+ extern PFNGLMULTITEXCOORD2FARBPROC glMultiTexCoord2fARB;
++#endif
+ extern PFNGLBLENDFUNCSEPARATEEXTPROC glBlendFuncSeparateEXT;
+ extern PFNGLFOGCOORDFPROC glFogCoordfEXT;
+ //#endif // _WIN32
