b847c16670504718544a0b591ac8988333b241d0
krion <krion@FreeBSD.org>
2004-10-12 12:59:39 +0000

    Update to version 2004.10.04
    PR:		ports/72531
    Submitted by:	Ports Fury

---
+++ math/fxt/files/patch-bmat::bitmatprint.cc
@@ -0,0 +1,10 @@
+--- bmat/bitmatprint.cc.orig	Mon Dec 22 01:05:42 2003
++++ bmat/bitmatprint.cc	Wed Oct  6 22:28:38 2004
+@@ -2,6 +2,7 @@
+ 
+ //#include "bits/printbin.h"
+ #include "fxtiomanip.h"
++#include "fxttypes.h"
+ 
+ 
+ void
---
+++ math/fxt/files/patch-bpol::bitpolprint.cc
@@ -0,0 +1,10 @@
+--- bpol/bitpolprint.cc.orig	Wed Apr 14 01:51:29 2004
++++ bpol/bitpolprint.cc	Wed Oct  6 23:18:23 2004
+@@ -2,6 +2,7 @@
+ #include "bits/bitsperlong.h"
+ 
+ #include "fxtio.h"
++#include "fxttypes.h"
+ 
+ 
+ void
