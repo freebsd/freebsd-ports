f31ee36890ee875b8f875c6f0985c8e5d17668c7
dinoex <dinoex@FreeBSD.org>
2004-10-12 05:03:32 +0000

    Rice Daedalus graphics plugin for Mupen64 - Mupen64 is a highly portable Nintendo 64 emulator. It has been developed on/for Linux originally but has already been ported successfully to other operating systems. The program can easily be ported to all operating systems supported by the SDL library.
    In its current state, the emulator is highly compatible and uses a plugin
    system. With the correct plugins ("correct" can be computer dependent), it
    can achieve nearly perfect graphics and sound in many games.
    
    WWW: http://mupen64.emulation64.com/
    
    PR:		71895
    Submitted by:	Travis Poppe

---
+++ emulators/mupen64-rice/files/patch-unique.h
@@ -0,0 +1,10 @@
+--- unique.h.orig	Tue Aug 26 00:25:58 2003
++++ unique.h	Fri Aug 13 21:51:55 2004
+@@ -18,6 +18,7 @@
+ 
+ */
+ 
++#include <assert.h>
+ #ifndef __UNIQUE_H__
+ #define __UNIQUE_H__
+ 
