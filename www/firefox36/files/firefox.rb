2d0cba2617626af7a15f6e5b2ad9919721fddbe5
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-10-12 00:04:40 +0000

    * Update to 1.0.1.p (aka 0.10.1, aka 1.0PR) * Add support for installing the new Firefox brand icon by defining   WITH_NEW_ICON [1]
    For all that's new, check out http://www.mozilla.org/products/firefox/releases/
    
    PR:		71781 [1]
    Submitted by:	Radek Kozlowski <radek@raadradd.com> [1]

--- www/firefox36/files/firefox.rb
+++ www/firefox36/files/firefox.rb
@@ -33,7 +33,7 @@
   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
   POSSIBILITY OF SUCH DAMAGE.
 
-  $Id: firebird.rb,v 1.2 2003/05/31 17:24:19 marcus Exp $
+  $Id: firefox.rb,v 1.1 2004/09/30 06:27:21 marcus Exp $
 
   2002-12-16 Alan Eldridge <alane@geeksrus.net>
 
