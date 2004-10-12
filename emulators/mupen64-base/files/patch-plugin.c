d57538b2e09c609c3f8d9fe83b820c602d0b41df
Dirk Meyer <dinoex@FreeBSD.org>
2004-10-12 04:53:51 +0000

    Mupen64 base - Mupen64 is a highly portable Nintendo 64 emulator. It has been developed on/for Linux originally but has already been ported successfully to other operating systems. The program can easily be ported to all operating systems supported by the SDL library.
    In its current state, the emulator is highly compatible and uses a plugin
    system. With the correct plugins ("correct" can be computer dependent), it
    can achieve nearly perfect graphics and sound in many games.
    
    WWW: http://mupen64.emulation64.com/
    
    PR:		71895
    Submitted by:	Travis Poppe

---
+++ emulators/mupen64-base/files/patch-plugin.c
@@ -0,0 +1,10 @@
+--- main/plugin.c.orig	Mon Jul  5 22:37:09 2004
++++ main/plugin.c	Tue Aug 10 22:39:28 2004
+@@ -33,6 +33,7 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
++#include <limits.h>
+ 
+ #include "winlnxdefs.h"
+ #include "plugin.h"
