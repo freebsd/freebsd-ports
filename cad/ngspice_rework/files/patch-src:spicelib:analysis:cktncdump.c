ee1253831db97939e3512f376dc8c00c78fc5778
Yen-Ming Lee <leeym@FreeBSD.org>
2004-10-13 01:30:33 +0000

    - upgrade to ngspice_rework-15 - utilize INFO - add USE_LIBTOOL_VER?=15 to eliminate *.la files - make this port portlint clean
    PR:		72008
    Submitted by:	leeym
    Reviewed by:	maintainer
    Approved by:	maintainer

---
+++ cad/ngspice_rework/files/patch-src:spicelib:analysis:cktncdump.c
@@ -0,0 +1,12 @@
+--- src/spicelib/analysis/cktncdump.c.orig	Sun Sep 26 19:16:43 2004
++++ src/spicelib/analysis/cktncdump.c	Sun Sep 26 19:16:54 2004
+@@ -4,7 +4,8 @@
+ **********/
+ 
+ #include "ngspice.h"
+-#include "cktdefs.h"#include "suffix.h"
++#include "cktdefs.h"
++#include "suffix.h"
+ 
+ void
+ CKTncDump(ckt) 
