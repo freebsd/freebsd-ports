a73e5a1a8641541d0966bd9327a64b2f07185ffb
Clement Laforet <clement@FreeBSD.org>
2004-10-13 08:11:52 +0000

    - Update to 1.13.5

--- www/linkchecker/files/patch-setup.py
+++ www/linkchecker/files/patch-setup.py
@@ -1,16 +1,16 @@
---- setup.py.orig	Thu Jan 29 19:14:40 2004
-+++ setup.py	Tue Feb  3 09:42:34 2004
-@@ -194,13 +194,13 @@
-          ('share/locale/nl/LC_MESSAGES',
-              ['share/locale/nl/LC_MESSAGES/linkcheck.mo']),
-          ('share/linkchecker', ['linkcheckerrc']),
+--- setup.py.orig	Mon Sep 20 11:05:17 2004
++++ setup.py	Wed Oct 13 09:54:32 2004
+@@ -234,13 +234,13 @@
+              ['share/locale/nl/LC_MESSAGES/linkchecker.mo']),
+          ('share/linkchecker', ['config/linkcheckerrc',
+                                 'config/logging.conf', ]),
 -         ('share/linkchecker/examples',
 +         ('share/examples/linkchecker',
-              ['lconline/leer.html.en', 'lconline/leer.html.de',
-               'lconline/index.html', 'lconline/lc_cgi.html.en',
-               'lconline/lc_cgi.html.de', 'lconline/check.js',
-               'lc.cgi', 'lc.fcgi', 'lc.sz_fcgi', 'linkchecker.bat',
-               'linkchecker-completion', 'linkcheck-cron.sh']),
+              ['cgi/lconline/leer.html.en', 'cgi/lconline/leer.html.de',
+               'cgi/lconline/index.html', 'cgi/lconline/lc_cgi.html.en',
+               'cgi/lconline/lc_cgi.html.de', 'cgi/lconline/check.js',
+               'cgi/lc.cgi', 'cgi/lc.fcgi',
+               'config/linkchecker-completion', 'config/linkcheck-cron.sh']),
 -         ('share/man/man1', ['linkchecker.1']),
 +         ('man/man1', ['linkchecker.1']),
        ],
