1f54f2ad0a31eee87335461da1d8a4e7c9bc3d71
marcus <marcus@FreeBSD.org>
2004-10-13 05:41:57 +0000

    Fix the build on 4.X.

--- x11-fm/nautilus2/files/patch-components-image_properties-nautilus-image-properties-view.c
+++ x11-fm/nautilus2/files/patch-components-image_properties-nautilus-image-properties-view.c
@@ -1,11 +1,17 @@
---- components/image_properties/nautilus-image-properties-view.c.orig	Thu Sep 30 15:41:33 2004
-+++ components/image_properties/nautilus-image-properties-view.c	Thu Sep 30 15:43:11 2004
-@@ -130,7 +130,8 @@
+--- components/image_properties/nautilus-image-properties-view.c.orig	Tue Mar  9 06:23:37 2004
++++ components/image_properties/nautilus-image-properties-view.c	Wed Oct 13 01:38:33 2004
+@@ -124,13 +124,14 @@
+ exif_content_callback (ExifContent *content, gpointer data)
+ {
+ 	struct ExifAttribute *attribute;
++        char value[1024];
+ 
+ 	attribute = (struct ExifAttribute *)data;
+ 	if (attribute->found) {
  		return;
  	}
  
 -        attribute->value = g_strdup (exif_content_get_value (content, attribute->tag));
-+        char value[1024];
 +        attribute->value = g_strdup (exif_content_get_value (content, attribute->tag, value, sizeof(value)));
  	if (attribute->value != NULL) {
  		attribute->found = TRUE;
