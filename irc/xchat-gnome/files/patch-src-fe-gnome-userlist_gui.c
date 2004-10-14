dbc16cb5ef5b5840f1fa9627a13be2beb1fd299f
Koop Mast <kwm@FreeBSD.org>
2004-10-14 17:52:47 +0000

    Update to 0.2.
    Approved by:	pav (mentor)

--- irc/xchat-gnome/files/patch-src-fe-gnome-userlist_gui.c
+++ irc/xchat-gnome/files/patch-src-fe-gnome-userlist_gui.c
@@ -1,18 +1,19 @@
---- src/fe-gnome/userlist_gui.c.orig	Sun Apr 25 01:14:29 2004
-+++ src/fe-gnome/userlist_gui.c	Wed May  5 00:16:02 2004
-@@ -64,13 +64,14 @@
- 	}
+--- src/fe-gnome/userlist_gui.c.orig	Wed Oct 13 10:27:32 2004
++++ src/fe-gnome/userlist_gui.c	Wed Oct 13 10:28:46 2004
+@@ -82,6 +82,7 @@
+ {
+ 	GtkTreePath *path;
+ 	GtkTreeSelection *select;
++	struct User *u;
+ 	if (!event)
+ 		return FALSE;
  
- 	if(event->button == 3) {
-+		struct User *u;
- 		if(gtk_tree_view_get_path_at_pos(GTK_TREE_VIEW(view), event->x, event->y, &path, 0, 0, 0)) {
- 			select = gtk_tree_view_get_selection(GTK_TREE_VIEW(view));
- 			gtk_tree_selection_unselect_all(select);
- 			gtk_tree_selection_select_path(select, path);
- 			gtk_tree_path_free(path);
+@@ -105,7 +106,7 @@
+ 			gtk_tree_selection_select_path (select, path);
+ 			gtk_tree_path_free (path);
  		}
--		struct User *u = userlist_get_selected();
-+		u = userlist_get_selected();
- 		if(u != NULL)
- 			userlist_context(view, u);
+-		struct User *u = userlist_get_selected ();
++		u = userlist_get_selected ();
+ 		if (u != NULL)
+ 			userlist_context (view, u);
  		return TRUE;
