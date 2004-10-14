5cfe4ba8cab4ba9931abde6954d7a0a3f08be261
Clive Lin <clive@FreeBSD.org>
2004-10-14 10:16:14 +0000

    Add milter-spamc, a bridge between spamc from SpamAssassin and sendmail. The libsnert stuff mentioned in the PR is not addressed in this commit, since it is not a run time dependency, and is strictly coupled with milter-spamc itself.
    PR:		ports/71486
    Submitted by:	Michael O. Boev <mike@tric.tomsk.gov.ru>

---
+++ mail/milter-spamc/files/milter-spamc.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+# PROVIDE: %%NAME%%
+# REQUIRE: syslogd
+# BEFORE:  sendmail
+# KEYWORD: FreeBSD shutdown
+
+#
+# Add the following lines to /etc/rc.conf to enable milter-spamc:
+#
+#%%NAME%%_enable="YES"
+#
+# See %%PREFIX%%/share/doc/milter-spamc/index.shtml for flags
+#
+
+. %%RC_SUBR%%
+
+name=%%NAME%%
+rcvar=`set_rcvar`
+
+# set defaults
+%%NAME%%_enable=${%%NAME%%_enable:-"NO"}
+%%NAME%%_chdir="/var/spool/milter-spamc"
+%%NAME%%_pidfile=${%%NAME%%_pidfile:-"$%%NAME%%_chdir/pid"}
+%%NAME%%_flags=${%%NAME%%_flags:-"-f /etc/mail/sendmail.cf unix:$%%NAME%%_chdir/socket"}
+
+command=%%PREFIX%%/sbin/milter-spamc
+required_dirs="$%%NAME%%_chdir"
+
+start_cmd=start_cmd
+start_cmd()
+{
+    if [ -s "$%%NAME%%_chdir/mutex" ]; then
+	ipcrm -s `cat $%%NAME%%_chdir/mutex`
+    fi
+    rm -f $%%NAME%%_chdir/{mutex,socket} $%%NAME%%_pidfile
+    cd $%%NAME%%_chdir && $command $%%NAME%%_flags >/dev/null &
+}
+
+load_rc_config $name
+run_rc_command "$1"
