f606a08cc946dfaa0020bec53034b735920dd430
kwm <kwm@FreeBSD.org>
2004-10-14 17:52:47 +0000

    Update to 0.2.
    Approved by:	pav (mentor)

--- irc/xchat-gnome/files/patch-src-fe-gnome-channel_list.c
+++ irc/xchat-gnome/files/patch-src-fe-gnome-channel_list.c
@@ -1,19 +1,19 @@
---- src/fe-gnome/channel_list.c.orig	Sun Apr 25 01:14:31 2004
-+++ src/fe-gnome/channel_list.c	Wed May  5 00:02:05 2004
-@@ -98,6 +98,7 @@
+--- src/fe-gnome/channel_list.c.orig	Wed Oct 13 10:27:41 2004
++++ src/fe-gnome/channel_list.c	Wed Oct 13 10:28:10 2004
+@@ -118,6 +118,7 @@
  	GtkSizeGroup *group;
  	GtkTreeSelection *select;
  	int width, height;
 +	gchar *title;
  
- 	if(sess == NULL)
+ 	if (sess == NULL)
  		return;
-@@ -122,7 +123,7 @@
+@@ -142,7 +143,7 @@
  	}
  
- 	widget = glade_xml_get_widget(win->xml, "window 1");
--	gchar *title = g_strdup_printf("%s Channel List", sess->server->networkname);
-+	title = g_strdup_printf("%s Channel List", sess->server->networkname);
- 	gtk_window_set_title(GTK_WINDOW(widget), title);
- 	g_free(title);
- 	g_signal_connect(G_OBJECT(widget), "delete-event", G_CALLBACK(chanlist_delete), win);
+ 	widget = glade_xml_get_widget (win->xml, "window 1");
+-	gchar *title = g_strdup_printf ("%s Channel List", get_network (sess));
++	title = g_strdup_printf ("%s Channel List", get_network (sess));
+ 	gtk_window_set_title (GTK_WINDOW (widget), title);
+ 	g_free (title);
+ 	g_signal_connect (G_OBJECT (widget), "delete-event", G_CALLBACK (chanlist_delete), win);
