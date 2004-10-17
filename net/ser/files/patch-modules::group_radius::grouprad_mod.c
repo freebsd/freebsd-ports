7db5b3a33ff2376453c5c3613aaf923401dbf56e
Maxim Sobolev <sobomax@FreeBSD.org>
2004-10-17 19:22:13 +0000

    Update to 0.8.14.

--- net/ser/files/patch-modules::group_radius::grouprad_mod.c
+++ net/ser/files/patch-modules::group_radius::grouprad_mod.c
@@ -1,14 +1,14 @@
 
 $FreeBSD$
 
---- modules/group_radius/grouprad_mod.c	2003/07/06 18:04:54	1.1
-+++ modules/group_radius/grouprad_mod.c	2003/07/06 18:05:41
-@@ -52,7 +52,7 @@
+--- modules/group_radius/grouprad_mod.c.orig	Mon Jul 19 01:56:24 2004
++++ modules/group_radius/grouprad_mod.c	Tue Jul 27 21:01:37 2004
+@@ -57,7 +57,7 @@
  /*
   * Module parameter variables
   */
--char* radius_config = "/usr/local/etc/radiusclient/radiusclient.conf";
-+char* radius_config = (CFG_DIR "radiusclient.conf");
+-static char* radius_config = "/usr/local/etc/radiusclient/radiusclient.conf";
++static char* radius_config = (CFG_DIR "radiusclient.conf");
  int use_domain = 1;  /* By default we use domain */
  
  
