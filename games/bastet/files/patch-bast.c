f3b0d98ca7344c949daf698ed1198eecb7412ce3
Michael Nottebrock <lofi@FreeBSD.org>
2004-10-21 18:57:58 +0000

    Add bastet, an ncurses Tetris clone which is harder than every other Tetris. Bastet stands for "bastard tetris" and uses a special algorithm designed to choose the worst brick possible.

---
+++ games/bastet/files/patch-bast.c
@@ -0,0 +1,11 @@
+--- bast.c.orig	Thu Oct 21 20:32:29 2004
++++ bast.c	Thu Oct 21 20:32:39 2004
+@@ -1,7 +1,7 @@
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <ncurses.h>
+-#include <termio.h>
++#include <termios.h>
+ #include <assert.h>
+ #include <unistd.h>
+ #include <string.h>
