5b02391395e8e4b98a710424148301806b979663
ijliao <ijliao@FreeBSD.org>
2004-10-22 01:09:20 +0000

    upgrade to 0.3.5

---
+++ misc/chmlib/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Oct 22 09:03:41 2004
++++ ltmain.sh	Fri Oct 22 09:03:58 2004
+@@ -5550,10 +5550,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
