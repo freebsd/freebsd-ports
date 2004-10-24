4fa3f58c5b12d7f576786b317b657527eca45dbe
Lars Thegler <lth@FreeBSD.org>
2004-10-24 19:59:22 +0000

    - Modify patch-collection to fit release

--- databases/p5-SQL-Translator/files/5.005_03-patch-lib::SQL::Translator::Producer::XML::SQLFairy.pm
+++ databases/p5-SQL-Translator/files/5.005_03-patch-lib::SQL::Translator::Producer::XML::SQLFairy.pm
@@ -1,23 +1,17 @@
---- lib/SQL/Translator/Producer/XML/SQLFairy.pm.orig	Sun May  9 15:11:42 2004
-+++ lib/SQL/Translator/Producer/XML/SQLFairy.pm	Sun May  9 15:13:45 2004
-@@ -224,9 +224,9 @@
-         # Use array to ensure consistant (ie not hash) ordering of attribs
-         # The order comes from the meths list passes in.
-         my @attr = map {
--            my $val = $obj->$_;
-+            my $val = $obj->$_();
-             ($_ => ref($val) eq 'ARRAY' ? join(', ', @$val) : $val);
--        } grep { defined $obj->$_ } @meths;
-+        } grep { defined $obj->$_() } @meths;
-         $empty_tag ? $xml->emptyTag( [ $Namespace => $tag ], @attr )
-                    : $xml->startTag( [ $Namespace => $tag ], @attr );
-     }
-@@ -247,7 +247,7 @@
-     my $emit_empty            = $PArgs->{'emit_empty_tags'};
- 
-     for my $method ( @methods ) {
--        my $val = $obj->$method;
-+        my $val = $obj->$method();
-         debug "        ".ref($obj)."->$method=",
-               (defined $val ? "'$val'" : "<UNDEF>");
-         next unless $emit_empty || defined $val;
+--- lib/SQL/Translator/Producer/XML/SQLFairy.pm.orig	Fri Sep  3 16:27:14 2004
++++ lib/SQL/Translator/Producer/XML/SQLFairy.pm	Sun Oct 24 17:00:03 2004
+@@ -349,11 +349,11 @@
+     # The order comes from the meths list passed in.
+     my @tags;
+     my @attr;
+-    foreach ( grep { defined $obj->$_ } @meths ) {
++    foreach ( grep { defined $obj->$_() } @meths ) {
+         my $what = m/$elements_re/ ? \@tags : \@attr;
+         my $val = $_ eq 'extra'
+-            ? { $obj->$_ }
+-            : $obj->$_;
++            ? { $obj->$_() }
++            : $obj->$_();
+         $val = ref $val eq 'ARRAY' ? join(',', @$val) : $val;
+         push @$what, $_ => $val;
+     };
