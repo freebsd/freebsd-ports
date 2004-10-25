bdaf1f5fc27fd4c1954b0b08e4d876e46961ecc3
maho <maho@FreeBSD.org>
2004-10-25 12:39:25 +0000

    1) update to mileston56 2) builds, but broken (bison1875d is required, setup fails, etc.)

--- editors/openoffice.org-vcltesttool/files/patch-psprint+source+helper+strhelper.cxx
+++ editors/openoffice.org-vcltesttool/files/patch-psprint+source+helper+strhelper.cxx
@@ -6,8 +6,8 @@ Index: source/helper/strhelper.cxx
 RCS file: /cvs/gsl/psprint/source/helper/strhelper.cxx,v
 retrieving revision 1.6
 diff -u -r1.6 strhelper.cxx
---- ../psprint/source/helper/strhelper.cxx	30 Mar 2004 13:48:06 -0000	1.6
-+++ ../psprint/source/helper/strhelper.cxx	13 Apr 2004 22:26:11 -0000
+--- psprint/source/helper/strhelper.cxx	30 Mar 2004 13:48:06 -0000	1.6
++++ psprint/source/helper/strhelper.cxx	13 Apr 2004 22:26:11 -0000
 @@ -67,6 +67,10 @@
  #include <cmath> // for isnan
  #include <cstring>
