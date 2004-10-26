fd6de580ef7c1fa86cf814beaea6bc04685d0262
Hiroki Sato <hrs@FreeBSD.org>
2004-10-26 09:31:23 +0000

    - Update the Japanese patch to 1.6a. - Remove dependency on tetex-modesw in print/tex-texmflocal. - Use updmap.cfg to generate font map files.  updmap-ptex, a   wrapper script of updmap(1) for pTeX support has been added. - $TEXCONFIG has been set to $TEXMF/dvips{-ptex,}// by default.

--- japanese/dvipsk/files/patch-texk::make::paths.mk
+++ japanese/dvipsk/files/patch-texk::make::paths.mk
@@ -1,5 +1,5 @@
---- make/paths.mk.orig	Sat Jun 12 12:13:14 2004
-+++ make/paths.mk	Sat Jun 12 12:13:04 2004
+--- make/paths.mk.orig	Wed Nov 13 05:54:37 2002
++++ make/paths.mk	Fri Sep 17 13:22:23 2004
 @@ -61,7 +61,7 @@
  dvilj_latex2e_macrodir = $(texinputdir)/latex/dvilj
  
@@ -9,3 +9,12 @@
  
  # The top-level font directory.
  fontdir = $(texmf)/fonts
+@@ -80,7 +80,7 @@
+ fontnamedir = $(texmf)/fontname
+ 
+ # For dvips configuration files, psfonts.map, etc.
+-dvipsdir = $(texmf)/dvips
++dvipsdir = $(texmf)/dvips-ptex
+ 
+ # For dvips .pro files, gsftopk's render.ps, etc.
+ psheaderdir = $(dvipsdir)
