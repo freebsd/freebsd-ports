3ee406a0ebfc14eda43137df5ee5a3d4c91dec29
dinoex <dinoex@FreeBSD.org>
2004-10-26 21:32:19 +0000

    - Bugfix update to 0.9.7e - md5 verfied with website

--- security/openssl/files/patch-apps-dgst.c
+++ security/openssl/files/patch-apps-dgst.c
@@ -1,11 +1,11 @@
---- apps/dgst.c.orig	Sat Dec 27 15:40:56 2003
-+++ apps/dgst.c	Wed Mar 17 13:59:05 2004
-@@ -349,7 +349,7 @@
+--- apps/dgst.c.orig	Thu Aug  5 20:10:46 2004
++++ apps/dgst.c	Tue Oct 26 07:02:30 2004
+@@ -363,7 +363,7 @@
  				{
- 				size_t len = strlen(name)+strlen(argv[i])+5;
+ 				size_t len = strlen(name)+strlen(argv[i])+(hmac_key ? 5 : 0)+5;
  				tmp=tofree=OPENSSL_malloc(len);
--				BIO_snprintf(tmp,len,"%s(%s)= ",name,argv[i]);
-+				BIO_snprintf(tmp,len,"%s(%s) = ",name,argv[i]);
+-				BIO_snprintf(tmp,len,"%s%s(%s)= ",
++				BIO_snprintf(tmp,len,"%s%s(%s) = ",
+ 							 hmac_key ? "HMAC-" : "",name,argv[i]);
  				}
  			else
- 				tmp="";
