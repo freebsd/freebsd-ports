8eef976b46e4e04315ac872845c1470c7595339c
glewis <glewis@FreeBSD.org>
2004-10-26 16:51:29 +0000

    . Update to 0.44.
    PR:		72959
    Submitted by:	Ronald Klop <ronald@cs.vu.nl> (maintainer)

--- java/jmp/files/patch-instance_owners.c
+++ java/jmp/files/patch-instance_owners.c
@@ -1,10 +1,15 @@
---- /tmp/instance_owners.c	Wed Aug 18 20:23:20 2004
-+++ instance_owners.c	Wed Aug 18 20:24:24 2004
-@@ -16,0 +17,4 @@
-+#if __FreeBSD__ < 5
+$FreeBSD$
+
+--- instance_owners.c.orig	Wed Jul 21 12:09:53 2004
++++ instance_owners.c	Wed Oct 20 02:01:44 2004
+@@ -14,6 +14,10 @@
+ #include <jmp-config.h>
+ #include <inttypes.h>
+ 
++#ifndef PRId32
 +#define PRId32 "d"
 +#endif
 +
-@@ -207 +211 @@
--    omenu = gtk_menu_new ();
-+    menuitem = gtk_menu_item_new_with_label (buf);
+ enum {
+     INFO,
+     OO_PARENT,
