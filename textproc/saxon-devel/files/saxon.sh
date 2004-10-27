5d0afe735502da3a6d26a5bf2cfa98a36ddff9c0
Herve Quiroz <hq@FreeBSD.org>
2004-10-27 16:50:43 +0000

    - Update to 8.1.1 - New launcher script 'saxon-xquery' to perform XQuery queries - PREFIX -> LOCALBASE in launcher script - (CP ; CHOWN) -> CPIO - Register run dependency on Xerces-J - Dynamic plist - More relevant homepage URL in pkg-descr - Take maintainership

--- textproc/saxon-devel/files/saxon.sh
+++ textproc/saxon-devel/files/saxon.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 #
-# $FreeBSD: /tmp/pcvs/ports/textproc/saxon-devel/files/Attic/saxon.sh,v 1.2 2004-07-17 08:19:44 glewis Exp $
+# $FreeBSD: /tmp/pcvs/ports/textproc/saxon-devel/files/Attic/saxon.sh,v 1.3 2004-10-27 16:50:43 hq Exp $
 
-export CLASSPATH=`%%PREFIX%%/bin/classpath`
-JAVAVM=%%JAVAVM%% javavm -jar %%JAVAJARDIR%%/saxon8.jar $@
+IAM=`basename "$0"`
+
+if [ "${IAM}" = "saxon-xquery" ]
+then
+	LAUNCHER_CLASS="net.sf.saxon.Query"
+else
+	LAUNCHER_CLASS="net.sf.saxon.Transform"
+fi
+
+JAVAVM=%%JAVAVM%% %%LOCALBASE%%/bin/javavm -classpath "`%%LOCALBASE%%/bin/classpath`" ${LAUNCHER_CLASS} $@
