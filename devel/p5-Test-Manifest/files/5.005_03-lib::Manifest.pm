de95ea51df78a3327c8818732612d7dd54ae241f
Dmitry Sivachenko <demon@FreeBSD.org>
2004-10-27 12:28:35 +0000

    Update to version 1.11.

--- devel/p5-Test-Manifest/files/5.005_03-lib::Manifest.pm
+++ devel/p5-Test-Manifest/files/5.005_03-lib::Manifest.pm
@@ -1,33 +1,35 @@
---- lib/Manifest.pm.orig	Fri Feb 20 20:11:18 2004
-+++ lib/Manifest.pm	Sun Jun  6 16:35:27 2004
-@@ -102,17 +102,17 @@
+--- lib/Manifest.pm.orig	Sun Sep 26 03:42:46 2004
++++ lib/Manifest.pm	Wed Oct 27 16:25:38 2004
+@@ -135,11 +135,11 @@
  sub get_t_files()
  	{
  	carp( "$Manifest does not exist!" ) unless -e $Manifest;
 -	return unless open my( $fh ), $Manifest;
 +	return unless open FH, $Manifest;
- 	
+ 
  	my @tests = ();
- 	
+ 
 -	while( <$fh> )
 +	while( <FH> )
  		{
  		chomp;
+ 		s/^\s+|\s+$//g;
+@@ -147,7 +147,7 @@
  		carp( "test file begins with t/ [$_]" ) if m|^t/|;
- 		push @tests, "t/$_" if -e "t/$_";
+ 		push @tests, catfile( "t", $_ ) if -e catfile( "t", $_ );
  		}
 -	close $fh;
 +	close FH;
- 		
+ 
  	return wantarray ? @tests : join " ", @tests;
  	}
-@@ -131,16 +131,16 @@
+@@ -166,16 +166,16 @@
  sub make_test_manifest()
  	{
  	carp( "t/ directory does not exist!" ) unless -d "t";
 -	return unless open my( $fh ), "> $Manifest";
 +	return unless open FH, "> $Manifest";
- 	
+ 
  	my $count = 0;
  	while( my $file = glob("t/*.t") )
  		{
@@ -38,6 +40,6 @@
  		}
 -	close $fh;
 +	close FH;
- 	
+ 
  	return $count;
  	}
