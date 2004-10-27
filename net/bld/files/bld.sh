1240a58212657ebf68069f71b9f359482a5dcb14
Anton Berezin <tobez@FreeBSD.org>
2004-10-27 19:54:46 +0000

    Add bld 0.2.4, a blacklisting daemon.
    PR:		72598
    Submitted by:	ob

---
+++ net/bld/files/bld.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# PROVIDE: bld
+# REQUIRE: LOGIN
+# BEFORE: mail
+# KEYWORD: FreeBSD shutdown
+
+#
+# Add the following lines to /etc/rc.conf to enable bld:
+#
+#bld_enable="YES"
+#
+# See bld(8) for flags
+#
+
+. %%RC_SUBR%%
+
+name=bld
+rcvar=`set_rcvar`
+
+command=%%PREFIX%%/sbin/bld
+required_dirs=/var/run/bld
+
+# set defaults
+
+bld_enable=${bld_enable:-"NO"}
+bld_flags=${bld_flags:-""}
+
+load_rc_config $name
+run_rc_command "$1"
