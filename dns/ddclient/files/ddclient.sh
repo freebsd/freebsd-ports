f3a0d1bb65756f7d8a4c6107804e3ccfb18819f6
Pav Lucistnik <pav@FreeBSD.org>
2004-10-28 00:04:57 +0000

    - Update to 3.6.4 - Take maintainership - rcng script
    PR:		ports/73059
    Submitted by:	Radek Kozlowski <radek@raadradd.com>

---
+++ dns/ddclient/files/ddclient.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+# PROVIDE: ddclient
+# REQUIRE: LOGIN
+# KEYWORD: FreeBSD shutdown
+
+#
+# Add the following lines to /etc/rc.conf to enable ddclient:
+#
+#ddclient_enable="YES"
+#
+ddclient_enable=${ddclient_enable-"NO"}
+ddclient_flags=${ddclient_flags-"-daemon 300"}
+
+. %%RC_SUBR%%
+
+name="ddclient"
+rcvar=`set_rcvar`
+command="%%PREFIX%%/sbin/${name}"
+required_files="%%PREFIX%%/etc/${name}.conf"
+
+load_rc_config ${name}
+
+run_rc_command "$1"
