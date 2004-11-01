e90dc04b9170d22b5cad4268885200f8f9ae26df
Kirill Ponomarev <krion@FreeBSD.org>
2004-11-01 17:20:51 +0000

    Update to version 1.9.2
    PR:		ports/73343
    Submitted by:	Ports Fury

--- audio/extace/files/patch-src::input.c
+++ audio/extace/files/patch-src::input.c
@@ -1,20 +1,11 @@
---- src/input.c.orig	Sat Nov 29 14:05:28 2003
-+++ src/input.c	Sun Nov 30 13:24:14 2003
-@@ -15,7 +15,7 @@
-  * No warranty is made or implied. You use this program at your own risk.
-  */
+--- src/input.c.orig	Mon Aug 23 23:35:00 2004
++++ src/input.c	Sun Oct 31 00:44:56 2004
+@@ -52,8 +52,12 @@
+ } handles[MAX_HANDLES];
  
--#include <asm/errno.h>
-+#include <errno.h>
- #include <config.h>
- #include <enums.h>
- #include <fcntl.h>
-@@ -76,8 +76,12 @@
- static gchar channel_numbers[MAX_STR][10];  /* string containing integers */
- #endif
- GtkWidget *errbox;
+ static GtkWidget *errbox;
 +GtkWidget *label;
- int errorbox_up;
+ static int errorbox_up =0;
  gint tag;		/* Used by gdk_input_* */
 +gint timeo;
 +gint res;
@@ -22,15 +13,15 @@
  
  /*--- globals to this file */
  
-@@ -151,7 +155,6 @@
- 
+@@ -295,7 +299,6 @@
+ 	
  	/* The rest is error handling */
  
 -	GtkWidget *label;
  	if (errorbox_up)
  		return(-1); /* ERROR window already onscreen */
  	errbox = gtk_window_new(GTK_WINDOW_DIALOG);
-@@ -380,9 +383,9 @@
+@@ -536,9 +539,9 @@
  	struct pollfd ufds;
  	ufds.fd = source;
  	ufds.events = POLLIN;
@@ -41,5 +32,5 @@
 +	res = -1;
 +	to_get = 0;
       
- 	/* reset data ring buffer */
- 	ring_pos=0;
+ 	/* adjust position in ring buffer to be on first channel */
+ 	ring_pos -= ring_pos%ring_channels;
