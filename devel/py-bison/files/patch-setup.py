3268686344a8d6a2c584d616bc65f2b69d8a58a8
ijliao <ijliao@FreeBSD.org>
2004-11-02 03:30:50 +0000

    add freebsd6 support
    Reported by:	pointyhat via kris

--- devel/py-bison/files/patch-setup.py
+++ devel/py-bison/files/patch-setup.py
@@ -1,6 +1,6 @@
 --- setup.py.orig	Sat Apr 24 10:05:41 2004
-+++ setup.py	Mon Jul 26 14:20:35 2004
-@@ -24,6 +24,16 @@
++++ setup.py	Tue Nov  2 11:26:45 2004
+@@ -24,6 +24,21 @@
      extra_link_args = []
      bison2pyscript = 'utils/bison2py'
      bisondynlibModule = "src/c/bisondynlib-linux.c"
@@ -10,6 +10,11 @@
 +    bison2pyscript = 'utils/bison2py'
 +    bisondynlibModule = "src/c/bisondynlib-linux.c"
 +elif sys.platform == 'freebsd5':
++    libs = []
++    extra_link_args = []
++    bison2pyscript = 'utils/bison2py'
++    bisondynlibModule = "src/c/bisondynlib-linux.c"
++elif sys.platform == 'freebsd6':
 +    libs = []
 +    extra_link_args = []
 +    bison2pyscript = 'utils/bison2py'
