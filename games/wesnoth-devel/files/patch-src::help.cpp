dba927cbee4113528e894eb6ab524953a4ab8dc2
mezz <mezz@FreeBSD.org>
2004-11-02 06:34:03 +0000

    - Update to 0.8.7. - Add an offical url of distfiles in case when sourceforge.net is down or   having the problem with mirror. - Remove SDL_ttf dependency, they imported SDL_ttf into this tarball. They   said that they fixed memory corruption errors on font rendering in their   imported SDL_ttf. I guess, I will let Wesnoth to use own SDL_ttf for now.

--- games/wesnoth-devel/files/patch-src::help.cpp
+++ games/wesnoth-devel/files/patch-src::help.cpp
@@ -1,11 +1,11 @@
---- src/help.cpp.orig	Tue Oct  5 23:47:06 2004
-+++ src/help.cpp	Tue Oct  5 23:47:19 2004
-@@ -29,7 +29,7 @@
- #include <cassert>
+--- src/help.cpp.orig	Mon Nov  1 22:25:36 2004
++++ src/help.cpp	Mon Nov  1 22:25:48 2004
+@@ -31,7 +31,7 @@
  #include <algorithm>
  #include <iostream>
+ #include <list>
 -#include <locale>
 +#include <locale.h>
+ #include <map>
  #include <queue>
  #include <set>
- #include <stack>
