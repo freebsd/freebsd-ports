1384f373c564af64ed7f789ddaa950a1e645b368
hq <hq@FreeBSD.org>
2004-11-03 12:39:23 +0000

    - Update to 3.0 [1] - Unbreak on 5.x [1] - Use PORTDOCS - Some minor cosmetic improvements
    Submitted by:	Roman Kennke <roman@ontographics.com> [1]

---
+++ java/jlint/files/patch-types.hh
@@ -0,0 +1,12 @@
+$FreeBSD$
+
+--- types.hh.orig	Fri Apr  4 20:29:43 2003
++++ types.hh	Thu Oct 28 14:24:56 2004
+@@ -31,7 +31,6 @@
+ #endif
+ #endif
+ 
+-#define bool  int
+ #define true  1
+ #define false 0
+ 
