3a52300e23f895202d4d112338e40ba8ae1236e0
Clement Laforet <clement@FreeBSD.org>
2004-11-04 11:53:41 +0000

    - Fix previous patch :-)
    Huge pointy hat to: me
    Noticed by:	Meno Abels <meno.abels@adviser.com>

--- www/apache20/files/patch-modules:experimental:util_ldap_cache_mgr.c
+++ www/apache20/files/patch-modules:experimental:util_ldap_cache_mgr.c
@@ -1,20 +1,20 @@
---- modules/experimental/util_ldap_cache_mgr.c.orig	Thu Sep 23 18:55:37 2004
-+++ modules/experimental/util_ldap_cache_mgr.c	Sat Oct 30 21:37:48 2004
+--- modules/experimental/util_ldap_cache_mgr.c       Thu Sep 23 18:55:37 2004
++++ modules/experimental/util_ldap_cache_mgr.c    Wed Nov  3 06:17:59 2004
 @@ -173,7 +173,7 @@
  void util_ald_cache_purge(util_ald_cache_t *cache)
  {
      unsigned long i;
 -    util_cache_node_t *p, *q;
-+    util_cache_node_t *p, *q, *pp;
++    util_cache_node_t *p, *q, **pp;
      apr_time_t t;
- 
+
      if (!cache)
 @@ -184,7 +184,8 @@
      cache->numpurges++;
- 
+
      for (i=0; i < cache->size; ++i) {
 -        p = cache->nodes[i];
-+        p = cache->nodes + i;
++        pp = cache->nodes + i;
 +        p = *pp;
          while (p != NULL) {
              if (p->add_time < cache->marktime) {
@@ -34,7 +34,7 @@
 +                 p = *pp;            }
          }
      }
- 
+
 @@ -686,7 +688,9 @@
                  default:
                      break;
