85c26d1b89fbb23c801e7723bc8f88ce142d51ed
lev <lev@FreeBSD.org>
2004-11-05 15:42:05 +0000

    (1) Update to 1.1.1 and supports `devel/apr' of version 1.0.0   (2) Add installation of usefuls scripts and tools into `share/subversion'   (3) Prepare for JavaHL bindings, but `devel/apr' have problems with threads on STABLE now.
    PR:		73135, 72551, 52797, 72665.
    Submitted by:	Many people, especially bmah@

--- devel/subversion-devel/files/patch-ac-helpers::ltmain.sh
+++ devel/subversion-devel/files/patch-ac-helpers::ltmain.sh
@@ -1,6 +1,6 @@
---- ac-helpers/ltmain.sh.orig	Thu Oct 23 17:15:47 2003
-+++ ac-helpers/ltmain.sh	Tue Nov 11 15:04:17 2003
-@@ -1072,7 +1072,7 @@
+--- ac-helpers/ltmain.sh.orig	2004-10-22 22:13:13.000000000 +0400
++++ ac-helpers/ltmain.sh	2004-11-05 11:30:39.328125000 +0300
+@@ -1061,7 +1061,7 @@
  	  esac
  	 elif test "X$arg" = "X-lc_r"; then
  	  case $host in
@@ -9,4 +9,3 @@
  	    # Do not include libc_r directly, use -pthread flag.
  	    continue
  	    ;;
-
