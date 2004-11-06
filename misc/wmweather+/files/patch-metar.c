48447ccc5694cd85b8720d9ecf35b700d25bf931
pav <pav@FreeBSD.org>
2004-11-06 12:00:12 +0000

    - Fix rounding/truncating functions on FreeBSD 4.x - Simplify dependencies
    Submitted by:	Harald Wille <harald.wille@students.jku.at> (maintainer)

--- misc/wmweather+/files/patch-metar.c
+++ misc/wmweather+/files/patch-metar.c
@@ -1,11 +1,15 @@
---- metar.orig	Sun Sep 22 22:00:53 2002
-+++ metar.c	Wed Jul 28 11:44:50 2004
-@@ -36,6 +36,9 @@
+--- metar.c.orig	Fri May 14 04:55:39 2004
++++ metar.c	Fri Nov  5 19:54:07 2004
+@@ -36,6 +36,12 @@
  #include <string.h>
  #include <ctype.h>
  #include <math.h>
-+#ifndef NAN
++
++#include <sys/param.h>
++#if (__FreeBSD_version < 500000)
 +#define NAN (0.0/0.0)
 +#endif
++
  #include <sys/stat.h>
  #include <sys/wait.h>
+ 
