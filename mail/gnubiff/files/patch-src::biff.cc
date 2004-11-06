50e95cac551c42045ecdaa464b9e03b3dee258ed
Pav Lucistnik <pav@FreeBSD.org>
2004-11-06 14:44:41 +0000

    - Update to 2.0.2
    PR:		ports/73037
    Submitted by:	Tim Bishop <tim@bishnet.net> (maintainer)

---
+++ mail/gnubiff/files/patch-src::biff.cc
@@ -0,0 +1,10 @@
+--- src/biff.cc.bak	Sat Nov  6 13:49:41 2004
++++ src/biff.cc	Sat Nov  6 13:50:06 2004
+@@ -32,6 +32,7 @@
+ #include <fstream>
+ #include <sstream>
+ #include <map>
++#include <unistd.h>
+ #include <fcntl.h>
+ #include "biff.h"
+ #include "ui-preferences.h"
