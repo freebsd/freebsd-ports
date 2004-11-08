1e72144daecbca5bc55621c7ff269223f299b5a6
Pete Fritchman <petef@FreeBSD.org>
2004-11-08 00:34:19 +0000

    * update to 0.1.18 * use %%EXAMPLESDIR%%

--- devel/devtodo/files/patch-util::CommandArgs.cc
+++
@@ -1,7 +0,0 @@
---- util/CommandArgs.cc.orig	Thu Jul 17 20:33:23 2003
-+++ util/CommandArgs.cc	Thu Jul 17 20:33:31 2003
-@@ -1,3 +1,4 @@
-+#include <assert.h>
- #include "CommandArgs.h"
- CommandArgs::CommandArgs() {
- }
