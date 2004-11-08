14a1da27f45c47f56c03f100467c4145b22cc27f
Michael Nottebrock <lofi@FreeBSD.org>
2004-11-08 00:34:12 +0000

    Update to KDE 3.3.1

--- graphics/kdegraphics4/files/patch-kpdf_xpdf_Catalog.cc
+++ graphics/kdegraphics4/files/patch-kpdf_xpdf_Catalog.cc
@@ -1,30 +1,37 @@
---- kpdf/xpdf/Catalog.cc	20 Aug 2003 21:25:12 -0000	1.3
-+++ kpdf/xpdf/Catalog.cc	18 Oct 2004 20:12:09 -0000
-@@ -61,10 +61,16 @@ Catalog::Catalog(XRef *xrefA) {
+Index: kpdf/xpdf/Catalog.cc
+===================================================================
+RCS file: /home/kde/kdegraphics/kpdf/xpdf/Catalog.cc,v
+retrieving revision 1.3.4.1
+diff -u -5 -d -p -r1.3.4.1 Catalog.cc
+--- kpdf/xpdf/Catalog.cc	2 Sep 2004 21:30:18 -0000	1.3.4.1
++++ kpdf/xpdf/Catalog.cc	12 Oct 2004 21:15:38 -0000
+@@ -62,10 +62,19 @@ Catalog::Catalog(XRef *xrefA) {
  	  obj.getTypeName());
      goto err3;
    }
-   pagesSize = numPages0 = obj.getInt();
+   pagesSize = numPages0 = (int)obj.getNum();
    obj.free();
++  // The gcc doesnt optimize this away, so this check is ok,
++  // even if it looks like a pagesSize != pagesSize check
 +  if (pagesSize*sizeof(Page *)/sizeof(Page *) != pagesSize ||
 +      pagesSize*sizeof(Ref)/sizeof(Ref) != pagesSize) {
 +    error(-1, "Invalid 'pagesSize'");
 +    ok = gFalse;
 +    return;
 +  }
++
    pages = (Page **)gmalloc(pagesSize * sizeof(Page *));
    pageRefs = (Ref *)gmalloc(pagesSize * sizeof(Ref));
    for (i = 0; i < pagesSize; ++i) {
      pages[i] = NULL;
      pageRefs[i].num = -1;
-@@ -188,10 +194,15 @@ int Catalog::readPageTree(Dict *pagesDic
+@@ -189,10 +198,14 @@ int Catalog::readPageTree(Dict *pagesDic
  	++start;
  	goto err3;
        }
        if (start >= pagesSize) {
  	pagesSize += 32;
-+        if (pagesSize*sizeof(Page *)/sizeof(Page *) != pagesSize ||
-+            pagesSize*sizeof(Ref)/sizeof(Ref) != pagesSize) {
++        if (pagesSize*sizeof(Page *)/sizeof(Page *) != pagesSize) {
 +          error(-1, "Invalid 'pagesSize' parameter.");
 +          goto err3;
 +        }
--- x11/kde4-baseapps/files/patch-kdm-kfrontend-genkdmconf.c
+++ x11/kde4-baseapps/files/patch-kdm-kfrontend-genkdmconf.c
@@ -20,12 +20,14 @@
  "\n"
  #ifdef _AIX
  "if [ -f /usr/lib/X11/xdm/sessreg ]; then\n"
-@@ -668,7 +668,7 @@
+@@ -667,9 +667,7 @@ static const char def_session[] = 
+ "  */csh|*/tcsh)\n"
  "    # [t]cshrc is always sourced automatically.\n"
  "    # Note that sourcing csh.login after .cshrc is non-standard.\n"
- "    set -a\n"
--"    eval `$SHELL -c 'if (-f /etc/csh.login) source /etc/csh.login; if (-f ~/.login) source ~/.login; /bin/sh -c set | egrep -v \"^(BASH_VERSINFO|EUID|PPID|UID|_)=\"'`\n"
-+"    eval `$SHELL -c 'if (-f /etc/csh.login) source /etc/csh.login >& /dev/null; if (-f ~/.login) source ~/.login >& /dev/null; /usr/bin/env | egrep -v \"^(TERM|SHLVL)=\"'`\n"
- "    set +a\n"
+-"    set -a\n"
+-"    eval `$SHELL -c 'if (-f /etc/csh.login) source /etc/csh.login > /dev/null; if (-f ~/.login) source ~/.login > /dev/null; /bin/sh -c set | egrep -v \"^(BASH_VERSINFO|EUID|PPID|UID|GROUPS|SHELLOPTS|_)=\"'`\n"
+-"    set +a\n"
++"    eval `$SHELL -c 'if (-f /etc/csh.login) source /etc/csh.login > /dev/null; if (-f ~/.login) source ~/.login > /dev/null; /bin/sh -c export'`\n"
  "    ;;\n"
  "  *) # Plain sh, ksh, and anything we don't know.\n"
+ "    [ -f /etc/profile ] && . /etc/profile\n"
