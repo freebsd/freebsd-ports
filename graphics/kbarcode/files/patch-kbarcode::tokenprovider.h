8f138b6174f6abbdf35decd665645be345697f9c
Kirill Ponomarev <krion@FreeBSD.org>
2004-11-09 22:06:44 +0000

    Update to version 1.8.1
    PR:		ports/73731
    Submitted by:	Ports Fury

---
+++ graphics/kbarcode/files/patch-kbarcode::tokenprovider.h
@@ -0,0 +1,11 @@
+--- kbarcode/tokenprovider.h.orig	Wed Jun 30 17:18:40 2004
++++ kbarcode/tokenprovider.h	Mon Aug 23 16:23:32 2004
+@@ -18,6 +18,8 @@
+ #ifndef TOKENPROVIDER_H
+ #define TOKENPROVIDER_H
+ 
++#include <time.h>
++
+ #include <qstring.h>
+ #include <qregexp.h>
+ 
