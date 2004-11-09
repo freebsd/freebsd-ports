84c134a487f0a040d20b03247f1976417c5cc8ac
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-11-09 00:51:03 +0000

    Fix the build on 4.X.
    Reported by:	pointyhat via kris
    Approved by:	maintainer

---
+++ sysutils/libgksuui/files/patch-libgksuui_gksuui-convenience.c
@@ -0,0 +1,14 @@
+--- libgksuui/gksuui-convenience.c.orig	Tue Nov  9 02:19:01 2004
++++ libgksuui/gksuui-convenience.c	Tue Nov  9 02:21:28 2004
+@@ -20,9 +20,9 @@
+ 
+ #include <stdio.h>
+ #include <string.h>
+-#include <sys/select.h>
+-#include <sys/time.h>
+ #include <sys/types.h>
++#include <sys/time.h>
++#include <sys/select.h>
+ #include <unistd.h>
+ 
+ #include <gtk/gtk.h>
