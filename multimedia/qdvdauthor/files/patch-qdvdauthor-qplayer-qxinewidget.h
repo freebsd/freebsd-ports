b17cc1cb1f81559e55e8be5f5742e40c8721b9fc
Volker Stolz <vs@FreeBSD.org>
2004-11-09 08:15:10 +0000

    Update to 0.0.8: - 'qxine' is now spelled 'qplayer' - Get rid of pkg-plist
    PR:		ports/73407
    Submitted by:	maintainer

---
+++ multimedia/qdvdauthor/files/patch-qdvdauthor-qplayer-qxinewidget.h
@@ -0,0 +1,10 @@
+--- qdvdauthor/qplayer/qxinewidget.h.orig	Fri Nov  5 14:56:16 2004
++++ qdvdauthor/qplayer/qxinewidget.h	Fri Nov  5 14:56:28 2004
+@@ -31,6 +31,7 @@
+ #include <qtimer.h>
+ #include <qevent.h>
+ #include <qthread.h>
++#include <pthread.h>
+ 
+ #include <xine.h>
+ 
