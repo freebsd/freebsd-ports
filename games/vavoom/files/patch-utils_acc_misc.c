91f04f6f0c366dc280c861a7a10e1a97e75183fd
vs <vs@FreeBSD.org>
2004-11-10 16:00:00 +0000

    Update to 1.15.3 Sound and music support is still very poor.
    PR:		ports/73432
    Submitted by:	maintainer

---
+++ games/vavoom/files/patch-utils_acc_misc.c
@@ -0,0 +1,11 @@
+--- utils/acc/misc.c.orig
++++ utils/acc/misc.c
+@@ -12,7 +12,7 @@
+ #else
+ #include <fcntl.h>
+ #include <stdlib.h>
+-#ifndef __linux__
++#if !defined(__linux__) && !defined(__FreeBSD__)
+ #include <io.h>
+ #endif
+ #include <sys/stat.h>
