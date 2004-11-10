5ce6698fd43e11d99adce5d623620cbdfd294701
Tilman Keskinoz <arved@FreeBSD.org>
2004-11-10 21:06:49 +0000

    Update to 0.6
    PR:		73752
    Submitted by:	Andrew Thompson <andy@fud.org.nz>

--- net/pfflowd/files/patch-pfflowd.c
+++ net/pfflowd/files/patch-pfflowd.c
@@ -1,6 +1,6 @@
---- pfflowd.c	Thu May  6 16:06:45 2004
-+++ pfflowd.c	Tue Jun 15 09:40:46 2004
-@@ -43,7 +43,11 @@
+--- pfflowd.c.orig	Tue Sep  7 00:25:57 2004
++++ pfflowd.c	Wed Nov 10 13:46:53 2004
+@@ -40,7 +40,11 @@
  #include <syslog.h>
  #include <time.h>
  #include <unistd.h>
@@ -9,49 +9,37 @@
 +#else
  #include <util.h>
 +#endif
+ #include <netdb.h>
+ #include "pfflowd.h"
  
- #define	PROGNAME		"pfflowd"
- #define	PROGVER			"0.5"
-@@ -189,6 +193,9 @@
- parse_hostport(const char *s, struct sockaddr_in *addr)
+@@ -115,9 +119,7 @@
+ /* Signal handlers */
+ static void sighand_exit(int signum)
  {
- 	char *host, *port;
-+#if defined(__FreeBSD__)
-+	int val;
-+#endif
+-	struct syslog_data sd = SYSLOG_DATA_INIT;
+-
+-	syslog_r(LOG_INFO, &sd, "%s exiting on signal %d", PROGNAME, signum);
++	syslog(LOG_INFO, "%s exiting on signal %d", PROGNAME, signum);
  
- 	if ((host = strdup(s)) == NULL) {
- 		fprintf(stderr, "Out of memory\n");
-@@ -201,12 +208,22 @@
- 	}
- 	*(port - 1) = '\0';
- 	addr->sin_family = AF_INET;
-+#if defined(__FreeBSD__)
-+	val = atoi(port);
-+	if (val <= 0 || val >= 65536) {
-+		fprintf(stderr, "Invalid -n port.\n");
-+		usage();
-+		exit(1);
-+	}
-+	addr->sin_port = (in_port_t)val;
-+#else
- 	addr->sin_port = atoi(port);
- 	if (addr->sin_port <= 0 || addr->sin_port >= 65536) {
- 		fprintf(stderr, "Invalid -n port.\n");
- 		usage();
- 		exit(1);
- 	}
-+#endif
- 	addr->sin_port = htons(addr->sin_port);
- 	if (inet_aton(host, &addr->sin_addr) == 0) {
- 		fprintf(stderr, "Invalid -n host.\n");
-@@ -425,7 +442,9 @@
+ 	_exit(0);
+ }
+@@ -366,7 +368,9 @@
  				strlcat(dst_s, pbuf, sizeof(dst_s));
  			}
  
 +#ifndef OLD_PFSYNC
- 			syslog(LOG_DEBUG, "IFACE %s\n", st->ifname); 
+ 			syslog(LOG_DEBUG, "IFACE %s", st[i].ifname); 
 +#endif
- 			syslog(LOG_DEBUG, "GWY %s\n", rt_s); 
+ 			syslog(LOG_DEBUG, "GWY %s", rt_s); 
  			syslog(LOG_DEBUG, "FLOW proto %d direction %d", 
- 			    st->proto, st->direction);
+ 			    st[i].proto, st[i].direction);
+@@ -538,7 +542,9 @@
+ 				strlcat(dst_s, pbuf, sizeof(dst_s));
+ 			}
+ 
++#ifndef OLD_PFSYNC
+ 			syslog(LOG_DEBUG, "IFACE %s", st[i].ifname); 
++#endif
+ 			syslog(LOG_DEBUG, "GWY %s", rt_s); 
+ 			syslog(LOG_DEBUG, "FLOW proto %d direction %d", 
+ 			    st[i].proto, st[i].direction);
