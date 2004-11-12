1ec5e3c20fe760a04e2ebafe9cefa4f0c5086604
Michael Nottebrock <lofi@FreeBSD.org>
2004-11-12 02:45:40 +0000

    Fix some more environment nits in the kdm configuration templates, and default to shutdown -p now for halting instead of shutdown -h now.
    No PORTREVISION bump since manual regeneration of kdm configuration would
    be required anyway.
    
    PR: 73844
    Submitted by: Jose M Rodriguez <josemi@freebsd.jazztel.es>

--- x11/kdebase4-runtime/files/patch-kdm-kfrontend-genkdmconf.c
+++ x11/kdebase4-runtime/files/patch-kdm-kfrontend-genkdmconf.c
@@ -1,5 +1,5 @@
---- kdm/kfrontend/genkdmconf.c.orig	Thu Aug  5 20:57:04 2004
-+++ kdm/kfrontend/genkdmconf.c	Wed Sep 15 18:18:11 2004
+--- kdm/kfrontend/genkdmconf.c.orig	Sun Oct  3 09:11:07 2004
++++ kdm/kfrontend/genkdmconf.c	Thu Nov 11 20:47:01 2004
 @@ -578,7 +578,7 @@
  "# by Xsetup usually.\n"
  "# This is not required if you use PAM with the pam_console module.\n"
@@ -20,14 +20,12 @@
  "\n"
  #ifdef _AIX
  "if [ -f /usr/lib/X11/xdm/sessreg ]; then\n"
-@@ -667,9 +667,7 @@ static const char def_session[] = 
- "  */csh|*/tcsh)\n"
+@@ -668,7 +668,7 @@
  "    # [t]cshrc is always sourced automatically.\n"
  "    # Note that sourcing csh.login after .cshrc is non-standard.\n"
--"    set -a\n"
+ "    set -a\n"
 -"    eval `$SHELL -c 'if (-f /etc/csh.login) source /etc/csh.login > /dev/null; if (-f ~/.login) source ~/.login > /dev/null; /bin/sh -c set | egrep -v \"^(BASH_VERSINFO|EUID|PPID|UID|GROUPS|SHELLOPTS|_)=\"'`\n"
--"    set +a\n"
-+"    eval `$SHELL -c 'if (-f /etc/csh.login) source /etc/csh.login > /dev/null; if (-f ~/.login) source ~/.login > /dev/null; /bin/sh -c export'`\n"
++"    eval `$SHELL -c 'if (-f /etc/csh.login) source /etc/csh.login >& /dev/null; if (-f ~/.login) source ~/.login >& /dev/null; printenv | egrep -v \"^(TERM|SHLVL)=\"'`\n"
+ "    set +a\n"
  "    ;;\n"
  "  *) # Plain sh, ksh, and anything we don't know.\n"
- "    [ -f /etc/profile ] && . /etc/profile\n"
