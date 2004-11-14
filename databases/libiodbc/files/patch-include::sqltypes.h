70c8543f9faefb9a2586aa9235677ce8628a7689
krion <krion@FreeBSD.org>
2004-11-14 09:25:35 +0000

    Update to version 3.52.1
    PR:		ports/73901
    Submitted by:	Ports Fury

--- databases/libiodbc/files/patch-include::sqltypes.h
+++ databases/libiodbc/files/patch-include::sqltypes.h
@@ -1,10 +1,10 @@
---- include/sqltypes.h.orig	Fri Mar 26 08:33:24 2004
-+++ include/sqltypes.h	Fri Mar 26 08:33:37 2004
-@@ -409,6 +409,7 @@
+--- include/sqltypes.h.orig	Wed Sep 29 19:14:06 2004
++++ include/sqltypes.h	Sat Nov 13 15:26:42 2004
+@@ -387,6 +387,7 @@
  #  if defined(_WCHAR_T)			|| \
        defined(_WCHAR_T_DEFINED)		|| \
-       defined(_WCHAR_T_DECLARED)	|| \
-+      defined(__FreeBSD__)		|| \
+       defined(_WCHAR_T_DECLARED)        || \
++      defined(__FreeBSD__)              || \
        defined(_BSD_WCHAR_T_DEFINED_)
  typedef wchar_t SQLWCHAR;
  #  else
