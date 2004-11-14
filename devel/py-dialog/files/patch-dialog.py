45a45258762c30675fb5f79d956278c6f3aad798
sobomax <sobomax@FreeBSD.org>
2004-11-14 16:44:50 +0000

    Add two missed options recognised by cdialog(1).

--- devel/py-dialog/files/patch-dialog.py
+++ devel/py-dialog/files/patch-dialog.py
@@ -1,9 +1,18 @@
 
 $FreeBSD$
 
---- dialog.py
+--- dialog.py.orig
 +++ dialog.py
-@@ -524,7 +524,7 @@
+@@ -214,6 +214,8 @@
+     "create_rc": lambda file: ("--create-rc", file),
+     "defaultno": lambda enable: _simple_option("--defaultno", enable),
+     "default_item": lambda string: ("--default-item", string),
++    "exit_label": lambda string: ("--exit-label", string),
++    "extra_button": lambda enable: _simple_option("--extra-button", enable),
+     "help": lambda enable: _simple_option("--help", enable),
+     "help_button": lambda enable: _simple_option("--help-button", enable),
+     "help_label": lambda string: ("--help-label", string),
+@@ -524,7 +526,7 @@
  
      """
  
