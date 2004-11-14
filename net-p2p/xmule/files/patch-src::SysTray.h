3c0da30d471ea3258c9f68efa4b38ed6c9ed223f
lioux <lioux@FreeBSD.org>
2004-11-14 23:59:23 +0000

    Update to 1.9.4b

---
+++ net-p2p/xmule/files/patch-src::SysTray.h
@@ -0,0 +1,10 @@
+--- src/SysTray.h.orig	Sun Nov 14 19:45:30 2004
++++ src/SysTray.h	Sun Nov 14 19:45:00 2004
+@@ -31,6 +31,7 @@
+ #include <locale.h>
+ #include <config.h>
+ #include <sys/ioctl.h>
++#include <sys/socket.h>
+ #include <net/if.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
