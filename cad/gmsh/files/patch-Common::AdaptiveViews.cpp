687e3ec491dd5fff7068adf19faf8cc033f145de
krion <krion@FreeBSD.org>
2004-11-14 09:44:19 +0000

    Update to version 1.56.2
    PR:		ports/73899
    Submitted by:	Ports Fury

---
+++ cad/gmsh/files/patch-Common::AdaptiveViews.cpp
@@ -0,0 +1,10 @@
+--- Common/AdaptiveViews.cpp.orig	Thu Nov 11 07:45:16 2004
++++ Common/AdaptiveViews.cpp	Thu Nov 11 16:11:29 2004
+@@ -23,6 +23,7 @@
+ 				       
+ #include <stdio.h>
+ #include <math.h>
++#include <time.h>
+ #include <list>
+ #include <set>
+ #include "Views.h"
