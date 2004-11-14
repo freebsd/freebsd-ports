9090ecc39a019a1b7eccec16d15a753427d5c174
ahze <ahze@FreeBSD.org>
2004-11-14 17:21:11 +0000

    Update to 1.3b
    Approved by:	pav (co mentor)

--- multimedia/dvdstyler/files/patch-src::MenuBox.cpp
+++ multimedia/dvdstyler/files/patch-src::MenuBox.cpp
@@ -1,38 +1,38 @@
---- src/MenuBox.cpp.orig	Fri Aug  6 19:32:13 2004
-+++ src/MenuBox.cpp	Fri Aug  6 19:41:00 2004
-@@ -15,7 +15,7 @@
- #include "MPEG.h"
+--- src/MenuBox.cpp.orig	Fri Nov 12 13:27:47 2004
++++ src/MenuBox.cpp	Sat Nov 13 20:49:08 2004
+@@ -16,7 +16,7 @@
  #include <wxVillaLib/ImageProc.h>
  #include <wxVillaLib/utils.h>
+ #include <wxXML/xml.h>
 -#include "math.h"
 +#include <math.h>
  #include <wx/dnd.h>
  #include <wx/utils.h>
+ #include <wx/mstream.h>
+@@ -404,11 +404,11 @@
  
-@@ -374,11 +374,11 @@
- 
- void MenuBox::MoveObject(int& x, int& y)
+ bool MenuBox::MoveObject(int& x, int& y)
  {
 -  x = (int) round(x/m_scale);
 -  y = (int) round(y/m_scale);
 +  x = rint(x/m_scale);
 +  y = rint(y/m_scale);
-   MoveObjectInt(x,y);
+   bool res = MoveObjectInt(x,y);
 -  x = (int) round(x*m_scale);
 -  y = (int) round(y*m_scale);
 +  x = rint(x*m_scale);
 +  y = rint(y*m_scale);
+   return res;
  }
  
- void MenuBox::MoveObjectInt(int& x, int& y)
-@@ -411,8 +411,8 @@
+@@ -444,8 +444,8 @@
  
- void MenuBox::ResizeObject(int x, int y, TransformType transformType)
+ bool MenuBox::ResizeObject(int x, int y, TransformType transformType)
  {
 -  x = (int) round(x/m_scale);
 -  y = (int) round(y/m_scale);
 +  x = rint(x/m_scale);
 +  y = rint(y/m_scale);
-   ResizeObjectInt(x, y, transformType);
+   return ResizeObjectInt(x, y, transformType);
  }
  
