038400a51b80c63b851dcc1eb1c512337685f9b6
Pav Lucistnik <pav@FreeBSD.org>
2004-11-16 13:48:43 +0000

    - Update to 7.1rc4
    PR:		ports/73966
    Submitted by:	Evgueni V. Gavrilov <aquatique@rusunix.org> (maintainer)

--- russian/ircd-hybrid/files/patch-src-packet.c
+++ russian/ircd-hybrid/files/patch-src-packet.c
@@ -1,11 +1,11 @@
---- src/packet.c.orig	Mon Jun 21 09:43:24 2004
-+++ src/packet.c	Mon Jun 21 09:44:15 2004
-@@ -467,7 +467,7 @@
+--- src/packet.c.orig	Mon Nov 15 14:41:32 2004
++++ src/packet.c	Mon Nov 15 14:42:04 2004
+@@ -531,7 +531,7 @@
    parse_client_queued(client_p);
  
    /* Check to make sure we're not flooding */
 -  if (IsPerson(client_p) &&
 +  if (!(IsServer(client_p) || IsHandshake(client_p) || IsConnecting(client_p)) &&
-       (dbuf_length(&client_p->localClient->buf_recvq) >
-        (unsigned int)ConfigFileEntry.client_flood))
+       (linebuf_alloclen(&client_p->localClient->buf_recvq) >
+        ConfigFileEntry.client_flood))
    {
