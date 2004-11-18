649220a8ac8595921f80f47e0af7dd809ba167e1
krion <krion@FreeBSD.org>
2004-11-18 20:22:06 +0000

    Update to version 0.5.12
    PR:		ports/74078
    Submitted by:	Ports Fury

--- editors/manedit/files/patch-maneditop.c
+++ editors/manedit/files/patch-maneditop.c
@@ -1,14 +1,14 @@
 
 $FreeBSD$
 
---- maneditop.c	2001/07/17 15:14:39	1.1
-+++ maneditop.c	2001/07/17 15:16:23
-@@ -242,9 +242,7 @@
- 		/* Not exist, so need to create a new one. */
+--- maneditop.c.orig	Tue Nov 16 13:09:17 2004
++++ maneditop.c	Tue Nov 16 19:27:06 2004
+@@ -236,9 +236,7 @@
+ 		/* Not exist, so need to create a new one */
  		rmkdir(
  		    tmp_dir_rtn,
 -		    S_IRUSR | S_IWUSR | S_IXUSR |
--                    S_IRGRP | S_IXGRP |
+-		    S_IRGRP | S_IXGRP |
 -		    S_IROTH | S_IXOTH
 +		    S_IRUSR | S_IWUSR | S_IXUSR
  		);
