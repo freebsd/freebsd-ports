c491a23bf7f5fd593cd19690a162d94e5eab4edf
Markus Brueffer <markus@FreeBSD.org>
2004-11-19 15:45:47 +0000

    - Add patch to make xmmsctrl.h compile with c++ (through 3rd party apps)   and gcc 3.4 - Bump PORTREVISION
    Approved by:	maintainer

---
+++ multimedia/xmms/files/patch-libxmms-xmmsctrl.h
@@ -0,0 +1,11 @@
+--- libxmms/xmmsctrl.h.orig	Fri Nov 19 13:49:06 2004
++++ libxmms/xmmsctrl.h	Fri Nov 19 13:49:14 2004
+@@ -85,7 +85,7 @@
+ 
+ 	
+ #ifdef __cplusplus
+-};
++}
+ #endif
+ 
+ /* Deprecated APIs */
