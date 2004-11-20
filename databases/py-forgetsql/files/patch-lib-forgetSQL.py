7ec0a5a30fafd14a2046f3ee5078feb19a8cd5e9
Anders Nordby <anders@FreeBSD.org>
2004-11-20 17:07:29 +0000

    Add forgetSQL, a Python module for object-oriented access to SQL databases. Supports MySQL and PostgreSQL currently.
    Sponsored by:	University of Tromsø

---
+++ databases/py-forgetsql/files/patch-lib-forgetSQL.py
@@ -0,0 +1,8 @@
+--- lib/forgetSQL.py.orig	Sat Nov 20 16:59:07 2004
++++ lib/forgetSQL.py	Sat Nov 20 16:59:39 2004
+@@ -1,4 +1,5 @@
+ #!/usr/bin/env python
++# -*- coding: ISO-8859-1 -*-
+ 
+ __version__ = "0.5.1"
+ 
