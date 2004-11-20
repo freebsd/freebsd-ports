cbe4c7a47a5793d26ce051ae757bb19365df0075
Anders Nordby <anders@FreeBSD.org>
2004-11-20 17:16:38 +0000

    Set encoding for forgetHTML.py to prevent warnings.

---
+++ www/py-forgethtml/files/patch-lib-forgetHTML.py
@@ -0,0 +1,8 @@
+--- lib/forgetHTML.py.orig	Sat Nov 20 17:16:38 2004
++++ lib/forgetHTML.py	Sat Nov 20 17:16:54 2004
+@@ -1,4 +1,5 @@
+ # $Id: forgetHTML.py,v 1.8 2004/09/13 00:48:42 stain Exp $
++# -*- coding: ISO-8859-1 -*-
+ #
+ # (c) Stian Soiland <stain@nvg.org> 2001-2003
+ # Licence: LGPL
