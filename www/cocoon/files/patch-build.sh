52899536945d5d81f8e90f414c6dd399af78a2a9
Herve Quiroz <hq@FreeBSD.org>
2004-11-20 16:53:26 +0000

    - Update to 2.1.6 - Use CPIO and INSTALL_SCRIPT - When ant build fails, return non-null error code in build.sh - Added WITH_SAMPLES and WITH_SOURCES options
    PR:		74139
    Submitted by:	Jean-Baptiste Quenot <jb.quenot@caraldi.com>

---
+++ www/cocoon/files/patch-build.sh
@@ -0,0 +1,8 @@
+--- build.sh.orig	Thu Nov  4 13:14:57 2004
++++ build.sh	Thu Nov  4 13:15:01 2004
+@@ -1,4 +1,4 @@
+-#!/bin/sh
++#!/bin/sh -e
+ 
+ #  Copyright 1999-2004 The Apache Software Foundation
+ #
