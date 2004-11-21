0cc10aba9ddbbe95272624a8846af246927ae35f
Thierry Thomas <thierry@FreeBSD.org>
2004-11-21 22:02:28 +0000

    Upgrade to 1.0-RC3.

--- lang/gambas/files/patch-src::lib::db::sqlite::sqlitedataset.cpp
+++ lang/gambas/files/patch-src::lib::db::sqlite::sqlitedataset.cpp
@@ -1,24 +1,12 @@
---- src/lib/db/sqlite/sqlitedataset.cpp.orig	Fri Feb 27 22:53:21 2004
-+++ src/lib/db/sqlite/sqlitedataset.cpp	Sun May 16 14:58:59 2004
-@@ -33,6 +33,9 @@
- #include <time.h>
+--- src/lib/db/sqlite/sqlitedataset.cpp.orig	Sat Nov 20 13:52:09 2004
++++ src/lib/db/sqlite/sqlitedataset.cpp	Sun Nov 21 21:14:37 2004
+@@ -34,6 +34,9 @@
  #include <iostream>
  #include <string.h>
+ #include <math.h>
 +#if defined(__FreeBSD__)
 +#  include <publib.h>
 +#endif
  
  #include "sqlitedataset.h"
  #include <unistd.h>
-@@ -148,7 +151,11 @@
- 			    break; 
- 		       case ft_Float:
- 	 		    v.set_asString(reslt[i]);
-+#if defined(__FreeBSD__) && !defined(_GLIBCPP_HAVE_STRTOF)
-+	 		    v.set_asFloat((float)strtod(reslt[i], NULL));
-+#else
- 	 		    v.set_asFloat(strtof(reslt[i], NULL));
-+#endif
- 			    break; 
- 		       case ft_Double:
- 	 		    v.set_asString(reslt[i]);
