ac4020ae920e9058794d7d231c60fd0a48f25275
Thierry Thomas <thierry@FreeBSD.org>
2004-11-21 20:58:15 +0000

    Upgrade to 4.4.
    Note: high order Finite Element package NGSolve is included.

--- cad/netgen/files/patch-ngtcltk::ngappinit.cpp
+++ cad/netgen/files/patch-ngtcltk::ngappinit.cpp
@@ -1,5 +1,5 @@
---- ngtcltk/ngappinit.cpp.orig	Tue Feb 10 10:01:01 2004
-+++ ngtcltk/ngappinit.cpp	Wed May  5 19:51:54 2004
+--- ngtcltk/ngappinit.cpp.orig	Wed Nov 17 10:43:35 2004
++++ ngtcltk/ngappinit.cpp	Wed Nov 17 21:57:41 2004
 @@ -3,10 +3,12 @@
    This file is a modification of tkAppInit.c from the Tcl/Tk package
  */
@@ -23,46 +23,14 @@
  int *tclDummyMathPtr = (int *) matherr;
  
  
-@@ -169,20 +171,22 @@
- 
+@@ -194,8 +196,9 @@
    // For adding an application, parse the file here,
    // and call the init-procedure below
--  // #define DEMOAPP
+   // #define DEMOAPP
 +#define DEMOAPP
  #ifdef DEMOAPP  
--  //  Tcl_EvalFile (myinterp, "demoapp/demoapp.tcl");
+-  Tcl_EvalFile (myinterp, "demoapp/demoapp.tcl");
 +  Tcl_EvalFile (myinterp, "%%DATADIR%%/demoapp.tcl");
  #endif
  
-+// #define METIS
-+
  #ifdef ADDON
-   Tcl_EvalFile (myinterp, "addon/addon.tcl");
- #endif
- 
-   // start event-loop
-   Tk_MainLoop();
--  Tcl_DeleteInterp (myinterp); 
-+  Tcl_DeleteInterp (myinterp);
- 
--  return 0;		
-+  return 0;
- }
- 
- 
-@@ -268,6 +272,7 @@
-     // #define NGSOLVE
-     // #undef NGSOLVE
- 
-+/*
- #ifdef NGSOLVE
-     extern int NGSolve_Init (Tcl_Interp * interp);
-     if (NGSolve_Init(interp) == TCL_ERROR) 
-@@ -277,6 +282,7 @@
- 	return TCL_ERROR;
-       }
- #endif
-+*/
- 
-     Tcl_StaticPackage(interp, "Tk", Tk_Init, 0);
-     
