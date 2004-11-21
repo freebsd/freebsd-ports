77715780c277f3ee0546a18d68d098ae8f19d13a
Pav Lucistnik <pav@FreeBSD.org>
2004-11-21 04:40:58 +0000

    - Update to 1.2.95
    PR:		ports/74108
    Submitted by:	Andrey Pavlenko <lance@merlin.net.ua> (maintainer)

---
+++ net/gnu-radius/files/radiusd.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# PROVIDE radiusd
+# REQUIRE: NETWORKING SERVERS
+# BEFORE: DAEMON
+# KEYWORD: FreeBSD shutdown
+
+#
+# Add the following lines to /etc/rc.conf to enable radiusd:
+#
+# radiusd_enable="YES"
+#
+
+. %%RC_SUBR%%
+
+name=radiusd
+rcvar=`set_rcvar`
+
+command=%%PREFIX%%/sbin/radiusd
+radiusd_config=${radiusd_config:-"%%PREFIX%%/etc/raddb/config"}
+required_files=${radiusd_config}
+pidfile=/var/run/radiusd.pid
+
+radiusd_enable=${radiusd_enable:-"NO"}
+
+load_rc_config $name
+run_rc_command "$1"
