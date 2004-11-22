abbdc8775296ce96af3d534470d465c874216cff
Joe Marcus Clarke <marcus@FreeBSD.org>
2004-11-22 04:16:25 +0000

    * Instead of using Xvfb, and evil black magic to create the extensions   repository, use the hidden "-register" option instead.  This has the   advantage of not requiring an X DISPLAY and not modifying any   preferences. * Add i18n support to the Thunderbird startup script [1] * Link nss with -Bsymbolic to avoid symbol conflicts with OpenSSL.  This   should help alleviate the CUPS printing problem
    PR:		73839 [1]
    Submitted by:	Jose M Rodriguez <josemi@freebsd.jazztel.es> [1]

--- mail/thunderbird/files/patch-security-coreconf-FreeBSD.mk
+++ mail/thunderbird/files/patch-security-coreconf-FreeBSD.mk
@@ -1,12 +1,28 @@
---- security/coreconf/FreeBSD.mk	Thu Oct 16 23:03:47 2003
-+++ security/coreconf/FreeBSD.mk	Thu Oct 16 22:53:11 2003
-@@ -44,5 +44,9 @@
+--- security/coreconf/FreeBSD.mk.orig	Wed Mar 26 20:17:25 2003
++++ security/coreconf/FreeBSD.mk	Sun Nov 21 22:59:49 2004
+@@ -43,8 +43,12 @@
+ ifeq ($(OS_TEST),alpha)
  CPU_ARCH		= alpha
  else
 +ifeq ($(OS_TEST),amd64)
 +CPU_ARCH		= amd64
 +else
  CPU_ARCH		= x86
+ endif
 +endif
+ 
+ OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
+ 
+@@ -71,7 +75,11 @@
+ DLL_SUFFIX		= so.1.0
  endif
  
+-MKSHLIB			= $(CC) $(DSO_LDOPTS)
++ifneq (,$(filter alpha ia64,$(OS_TEST)))
++MKSHLIB			= $(CC) -Wl,-Bsymbolic -lc $(DSO_LDOPTS) -o $@
++else
++MKSHLIB			= $(CC) -Wl,-Bsymbolic $(DSO_LDOPTS) -o $@
++endif
+ ifdef MAPFILE
+ # Add LD options to restrict exported symbols to those in the map file
+ endif
