e41aff52a7913563dcb30f2b47a42bc22bb45989
ale <ale@FreeBSD.org>
2004-11-24 14:16:13 +0000

    Update to 3.1 release.

--- www/pserv/files/patch-main.h
+++ www/pserv/files/patch-main.h
@@ -1,24 +1,21 @@
---- sources/main.h.orig	Mon Aug 30 13:19:03 2004
-+++ sources/main.h	Wed Sep 15 15:24:32 2004
-@@ -18,14 +18,16 @@
- 
- 
+--- sources/main.h.orig	Wed Nov 24 11:12:35 2004
++++ sources/main.h	Wed Nov 24 14:58:06 2004
+@@ -20,12 +20,14 @@
  /* --- CPP parsing options --- */
--#define PRINTF_DEBUG            /* enable this to print some debugging messages */
-+#undef PRINTF_DEBUG            /* enable this to print some debugging messages */
+ #undef PRINTF_DEBUG           /* enable this to print some debugging messages */
  #undef ON_THE_FLY_CONVERSION /* enable this for line ending conversion */
 +#define LOG                     /* enable logging to file */
  #define BRAIN_DEAD_CAST         /* if your compiler is brainwashed and does not cast standard types.h structures */
  #define FORKING_SERVER          /* enables to fork for every request */
  #define AUTO_INDEX              /* enables auto-index of directories */
  #define SOCKADDR_REUSE          /* enables reuse oth the socket (quick relaunch) */
- #define VOID_SIG_HANDLERS       /* enables different signal functíon types */
- #define ADDR_LEN_TYPE_INT       /* in accept() if addrlen is int */
+ #define VOID_SIG_HANDLERS       /* enables different signal function types */
+ #undef ADDR_LEN_TYPE_INT       /* in accept() if addrlen is int */
 +#define PHP                     /* enables transparent PHP support */
  
- 
  /* --- Configure options --- */
-@@ -37,18 +39,19 @@
+ #define CONFIG_FILE_NAME    "pserv.conf"  /* name of the configuration file, concatenated to DEFAULT_CONFIG_LOCATION */
+@@ -36,18 +38,19 @@
  #define MIME_TYPE_DEFAULT   "application/octet-stream"
  
  /* configuration file location */
@@ -29,7 +26,7 @@
 -#define DEFAULT_PORT	    	2000
 +#define DEFAULT_PORT	    	80
  #define DEFAULT_MAX_CHILDREN	5
--#define DEFAULT_DOCS_LOCATION	"/home/multix/public_html"
+-#define DEFAULT_DOCS_LOCATION	"/usr/people/multix/public_html"
 +#define DEFAULT_DOCS_LOCATION	"/usr/local/www/data"
  #define DEFAULT_FILE_NAME   	"index.html"
  #define DEFAULT_SEC_TO	    	1
@@ -37,14 +34,14 @@
 -#define DEFAULT_LOG_FILE    	"/home/multix/pserv/pserv.log"
 -#define DEFAULT_MIME_FILE   	"/home/multix/pserv/mime_types.dat"
 -#define DEFAULT_CGI_ROOT    	"/home/multix/public_html/cgi-bin"
-+#define DEFAULT_LOG_FILE    	"/var/log/pserv.log"
-+#define DEFAULT_MIME_FILE   	"/usr/local/etc/mime.types"
-+#define DEFAULT_PHP_FILE        "/usr/local/bin/php"
-+#define DEFAULT_CGI_ROOT    	"/usr/local/www/cgi-bin"
++#define DEFAULT_LOG_FILE	"/var/log/pserv.log"
++#define DEFAULT_MIME_FILE	"/usr/local/etc/mime.types"
++#define DEFAULT_PHP_FILE	"/usr/local/bin/php"
++#define DEFAULT_CGI_ROOT	"/usr/local/www/cgi-bin"
  #define DEFAULT_SERVER_NAME 	"localhost"
  
  /* amount of connections queued in listening */
-@@ -122,8 +125,10 @@
+@@ -120,8 +123,10 @@
  #define ADDRESS_LEN 	    	16
  #define METHOD_LEN  	    	16
  #define PROTOCOL_LEN	    	16
@@ -55,13 +52,13 @@
  
  /* ------ Structures ------- */
  /* we use +1 to be safe with handling the end-of-string */
-@@ -141,7 +146,9 @@
-     char protocolVersion[PROTOCOL_LEN+1];
-     int keepAlive;
-     char userAgent[USER_AGENT_LEN+1];
-+    char cookie[MAX_COOKIE_LEN+1];
+@@ -139,7 +144,9 @@
+     char     protocolVersion[PROTOCOL_LEN+1];
+     int      keepAlive;
+     char     userAgent[USER_AGENT_LEN+1];
++    char     cookie[MAX_COOKIE_LEN+1];
      long int contentLength;
-+    char contentType[CONTENT_TYPE_LEN+1];
-     char rest[BUFFER_SIZE+1];
++    char     contentType[CONTENT_TYPE_LEN+1];
+     char     rest[BUFFER_SIZE+1];
  };
  
