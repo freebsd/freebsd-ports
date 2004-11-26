8cf0fa314837d32c03cdb359aa18f8f2de20062a
marcus <marcus@FreeBSD.org>
2004-11-26 22:16:16 +0000

    Add gnome-user-share, an application that lets GNOME users share a personal, public file space via WebDAV.  The resulting share will automatically be visible in Nautilus.

---
+++ www/gnome-user-share/files/patch-file-share-properties.c
@@ -0,0 +1,10 @@
+--- file-share-properties.c.orig	Fri Nov 26 14:53:54 2004
++++ file-share-properties.c	Fri Nov 26 14:54:07 2004
+@@ -1,6 +1,7 @@
+ #include "config.h"
+ 
+ #include <string.h>
++#include <stdio.h>
+ 
+ #include <gtk/gtk.h>
+ #include <glib/gi18n.h>
