481092f374e60f6ee6da787f8162fbeaca7b56cf
krion <krion@FreeBSD.org>
2004-11-27 12:01:23 +0000

    Fix build
    PR:		ports/74428
    Submitted by:	Ports Fury

---
+++ x11-fm/FSViewer/files/patch-src::FSUtils.c
@@ -0,0 +1,12 @@
+*** src/FSUtils.c.orig	Sun Dec 15 06:40:13 2002
+--- src/FSUtils.c	Sat Nov 27 20:44:50 2004
+***************
+*** 30,35 ****
+--- 30,36 ----
+  #include <kvm.h>
+  #include <limits.h>
+  #include <osreldate.h>
++ #include <sys/param.h>
+  #include <sys/conf.h>
+  #include <sys/dkstat.h>
+  /* #include <sys/rlist.h> */
