777bd29d0651c0bf0a968bab5e552ab75bb05c8e
Oliver Eikemeier <eik@FreeBSD.org>
2004-11-30 18:10:45 +0000

    update to version 3.77

--- security/nmap/files/patch-nmapfe::nmapfe_sig.c
+++ security/nmap/files/patch-nmapfe::nmapfe_sig.c
@@ -1,6 +1,6 @@
 --- nmapfe/nmapfe_sig.c.orig	Mon Oct  6 12:59:49 2003
 +++ nmapfe/nmapfe_sig.c	Mon Oct  6 13:00:06 2003
-@@ -412,7 +412,7 @@
+@@ -425,7 +425,7 @@
    if (size > command_size)
      command = realloc(command, size);
  
