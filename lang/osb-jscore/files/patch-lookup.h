99b15d4247ea4e1c5b03cdbc39d7ae0722c7b137
ahze <ahze@FreeBSD.org>
2004-11-30 21:49:48 +0000

    Add osb-jscore
    JavaScriptCore is part of Gtk-Wecore.
    
    JavaScriptCore is javascript interpreter that OSB uses. It is based on
    Apple's WebCore JavaScriptCore code, which is in turn based on KDE kjs
    code.

---
+++ lang/osb-jscore/files/patch-lookup.h
@@ -0,0 +1,10 @@
+--- kjs/lookup.h.orig	2004/09/23 08:27:54
++++ kjs/lookup.h	2004/11/29 06:28:35
+@@ -25,6 +25,7 @@
+ 
+ #include "identifier.h"
+ #include "value.h"
++#include "object.h"
+ #include <stdio.h>
+ 
+ namespace KJS {
