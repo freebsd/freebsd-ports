2bbe6b652108b44a504b7563159d5ea2ca474b84
perky <perky@FreeBSD.org>
2004-12-01 15:04:05 +0000

    - Update lang/python to 2.4 final release. - Finish repocopy lang/python -> lang/python23 for Python 2.3.4

--- lang/python24/files/patch-Python::ceval.c
+++ lang/python24/files/patch-Python::ceval.c
@@ -1,14 +1,16 @@
---- Python/ceval.c.orig	Sat Dec 20 15:13:53 2003
-+++ Python/ceval.c	Sat Dec 20 15:15:46 2003
-@@ -496,7 +496,10 @@
+--- Python/ceval.c.orig	Wed Nov 24 03:06:08 2004
++++ Python/ceval.c	Wed Dec  1 22:29:37 2004
+@@ -414,8 +414,11 @@
  
  /* The interpreter's recursion limit */
  
 -static int recursion_limit = 1000;
+-int _Py_CheckRecursionLimit = 1000;
 +#ifndef PYTHON_DEFAULT_RECURSION_LIMIT
 +#define PYTHON_DEFAULT_RECURSION_LIMIT 1000
 +#endif
 +static int recursion_limit = PYTHON_DEFAULT_RECURSION_LIMIT;
++int _Py_CheckRecursionLimit = PYTHON_DEFAULT_RECURSION_LIMIT;
  
  int
  Py_GetRecursionLimit(void)
