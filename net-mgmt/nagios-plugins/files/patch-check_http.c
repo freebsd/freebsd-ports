f6bf46596618c78c67a3f9e8ec37be8f1aabaf4c
Tilman Keskinoz <arved@FreeBSD.org>
2004-12-01 22:19:44 +0000

    Update nagios-plugins port to 1.4.0.alpha1. Fix compilation with radiusclient 0.4.7.
    PR:		74446
    Submitted by:	Blaz Zupan <blaz@si.FreeBSD.org>

--- net-mgmt/nagios-plugins/files/patch-check_http.c
+++ net-mgmt/nagios-plugins/files/patch-check_http.c
@@ -1,12 +1,20 @@
---- plugins/check_http.c.orig	Mon Jun 30 11:56:08 2003
-+++ plugins/check_http.c	Fri Dec 19 23:03:52 2003
-@@ -198,3 +198,3 @@
+--- plugins/check_http.c.orig	Mon Sep 15 07:03:47 2003
++++ plugins/check_http.c	Sat Nov 27 12:32:09 2004
+@@ -83,7 +83,7 @@
+ #define HTTP_URL "/"
+ #define CRLF "\r\n"
  
 -char timestamp[17] = "";
 +char timestamp[256] = "";
  int specify_port = FALSE;
-@@ -975,3 +975,3 @@
+ int server_port = HTTP_PORT;
+ char server_port_text[6] = "";
+@@ -1010,7 +1010,7 @@
+ 
+ 	days_left = (mktime (&stamp) - time (NULL)) / 86400;
  	snprintf
--		(timestamp, 16, "%02d/%02d/%04d %02d:%02d",
+-		(timestamp, 17, "%02d/%02d/%04d %02d:%02d",
 +		(timestamp, sizeof(timestamp), "%02d/%02d/%04d %02d:%02d",
  		 stamp.tm_mon + 1,
+ 		 stamp.tm_mday, stamp.tm_year + 1900, stamp.tm_hour, stamp.tm_min);
+ 
