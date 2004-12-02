33ca128b59bd325b7635b913fcaea098a44b7469
ahze <ahze@FreeBSD.org>
2004-12-02 08:32:02 +0000

    Fix build with latest mozilla 1.8a5

---
+++ www/kazehakase/files/patch-kz-mozembed.cpp
@@ -0,0 +1,11 @@
+--- src/mozilla/kz-mozembed.cpp.orig	Thu Dec  2 03:08:25 2004
++++ src/mozilla/kz-mozembed.cpp	Thu Dec  2 03:08:35 2004
+@@ -64,7 +64,7 @@
+ #include <docshell/nsIDocShellTreeItem.h>
+ #include <docshell/nsIDocShellTreeOwner.h>
+ #include <nsIPresShell.h>
+-#include <nsIPresContext.h>
++// #include <nsIPresContext.h>
+ #include <nsIDOMNodeList.h>
+ #include <nsIDOMWindow.h>
+ #include <nsISelection.h>
