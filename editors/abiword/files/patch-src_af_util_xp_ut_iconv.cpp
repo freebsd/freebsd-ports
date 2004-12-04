394b18022b6bdcd4606c2065a2c4f9824afa63d0
mezz <mezz@FreeBSD.org>
2004-12-04 21:46:46 +0000

    -Move editors/AbiWord2 -> editors/abiword -Update to 2.2.1, see the detail:
    	http://www.abisource.com/release-notes/2.2.1.phtml

--- editors/abiword/files/patch-src_af_util_xp_ut_iconv.cpp
+++ editors/abiword/files/patch-src_af_util_xp_ut_iconv.cpp
@@ -1,13 +1,11 @@
---- src/af/util/xp/ut_iconv.cpp.orig	Thu Jul 17 15:12:55 2003
-+++ src/af/util/xp/ut_iconv.cpp	Thu Jul 17 15:16:40 2003
-@@ -295,8 +295,8 @@
-     return (size_t)-1;
+--- src/af/util/xp/ut_iconv.cpp.orig	Fri Jan  2 16:00:33 2004
++++ src/af/util/xp/ut_iconv.cpp	Fri Jan  2 16:08:03 2004
+@@ -67,7 +67,7 @@
+ #if defined (WIN32) || defined(__QNXNTO__) || defined(__CYGWIN__) ||  \
+ (defined (__MACH__) && defined (__APPLE__)) || \
+ (defined(TARGET_OS_MAC) && TARGET_OS_MAC) || \
+-defined(__BEOS__) || defined (__AIX__) || \
++defined(__BEOS__) || defined (__AIX__) || defined (__FreeBSD__) || \
+ (defined(__linux__) && defined(__powerpc__) && (__GLIBC__ <= 2) && (__GLIBC_MINOR__ <= 1))
  
- 
--  ICONV_CONST char ** buf = const_cast<ICONV_CONST char**>(inbuf);
--  return iconv( cd, buf, inbytesleft, outbuf, outbytesleft );
-+  //ICONV_CONST char ** buf = const_cast<ICONV_CONST char**>(inbuf);
-+  return iconv( cd, inbuf, inbytesleft, outbuf, outbytesleft );
- }
- 
- int  UT_iconv_close( UT_iconv_t cd )
+ #define ICONV_CONST const
