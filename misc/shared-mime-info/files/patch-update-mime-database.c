0d3eb9c34a1ea7abd6a490aa2f9cdc655adfcf44
marcus <marcus@FreeBSD.org>
2004-12-04 21:10:03 +0000

    Fix a potential overflow leading to incorrect MIME detection on 64-bit and bigendian platforms.  See
    https://bugs.freedesktop.org/show_bug.cgi?id=1506
    https://bugs.freedesktop.org/show_bug.cgi?id=1507
    
    For more details.
    
    Reported by:	Sean McNeil <sean@mcneil.com>
    Obtained from:	FreeDesktop CVS

--- misc/shared-mime-info/files/patch-update-mime-database.c
+++ misc/shared-mime-info/files/patch-update-mime-database.c
@@ -1,5 +1,5 @@
 --- update-mime-database.c.orig	Mon Oct 13 05:43:38 2003
-+++ update-mime-database.c	Wed Jun 23 20:13:10 2004
++++ update-mime-database.c	Sat Dec  4 15:52:53 2004
 @@ -377,7 +377,8 @@
  		}
  	}
@@ -10,7 +10,85 @@
  }
  
  /* 'node' is a <mime-type> node from a source file, whose type is 'type'.
-@@ -1330,7 +1331,7 @@
+@@ -809,7 +810,13 @@
+ 	unsigned long value;
+ 	int b;
+ 
+-	value = strtol(in, &end, 0);
++	value = strtoul(in, &end, 0);
++	if (errno == ERANGE) {
++		g_set_error(error, MIME_ERROR, 0,
++			"Number out-of-range (%s should fit in %d bytes)",
++			in, bytes);
++		return;
++	}
+ 	if (*end != '\0')
+ 	{
+ 		g_set_error(error, MIME_ERROR, 0, "Value is not a number");
+@@ -834,9 +841,16 @@
+ 	if (in_mask)
+ 	{
+ 		int b;
+-		long mask;
+-		
+-		mask = strtol(in_mask, &end, 0);
++		unsigned long mask;
++
++		mask = strtoul(in_mask, &end, 0);
++		if (errno == ERANGE) {
++			g_set_error(error, MIME_ERROR, 0,
++				"Mask out-of-range (%s should fit in %d bytes)",
++				in_mask, bytes);
++			return;
++		}
++
+ 		if (*end != '\0')
+ 		{
+ 			g_set_error(error, MIME_ERROR, 0,
+@@ -847,7 +861,7 @@
+ 		out_mask = g_new(char, bytes);
+ 		for (b = 0; b < bytes; b++)
+ 		{
+-			int shift = (bytes - b - 1) * 8;
++			int shift = (big_endian ? (bytes - b - 1) : b) * 8;
+ 			out_mask[b] = (mask >> shift) & 0xff;
+ 		}
+ 	}
+@@ -991,11 +1005,32 @@
+ 	}
+ 
+ 	match->range_start = strtol(offset, &end, 10);
++	if (errno == ERANGE) {
++		char *number;
++		number = g_strndup(offset, end-offset);
++		g_set_error(error, MIME_ERROR, 0,
++			"Number out-of-range (%s should fit in 4 bytes)",
++			number);
++		g_free(number);
++		return;
++	}
+ 	if (*end == ':' && end[1] && match->range_start >= 0)
+ 	{
+ 		int last;
++		char *begin;
++
++		begin = end + 1;
++		last = strtol(begin, &end, 10);
++		if (errno == ERANGE) {
++			char *number;
++			number = g_strndup(begin, end-begin);
++			g_set_error(error, MIME_ERROR, 0,
++				"Number out-of-range (%s should fit in 8 bytes)",
++				number);
++			g_free(number);
++			return;
++		}
+ 
+-		last = strtol(end + 1, &end, 10);
+ 		if (*end == '\0' && last >= match->range_start)
+ 			match->range_length = last - match->range_start + 1;
+ 		else
+@@ -1330,7 +1365,7 @@
  
  	env = getenv("XDG_DATA_DIRS");
  	if (!env)
