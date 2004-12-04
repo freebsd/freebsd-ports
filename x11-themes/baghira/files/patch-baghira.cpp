c9f4ca4e26182cc4c6ba3aa724e28027ddd4e8e6
Tilman Keskinoz <arved@FreeBSD.org>
2004-12-04 18:22:09 +0000

    Update to 0.6
    PR:		73974
    Submitted by:	Miguel Mendez <flynn@energyhq.es.eu.org>

--- x11-themes/baghira/files/patch-baghira.cpp
+++ x11-themes/baghira/files/patch-baghira.cpp
@@ -1,11 +1,11 @@
---- style/baghira.cpp.orig	Tue Aug 31 00:07:49 2004
-+++ style/baghira.cpp	Thu Sep  2 12:57:28 2004
-@@ -1643,7 +1643,7 @@
- 				const QPushButton *btn = ( const QPushButton* ) widget;
- 				int x, y, w, h;
- 				r.rect( &x, &y, &w, &h );
--				bool highlighted = btn->isOn() || btn->isDown() || btn->hasFocus() || btn == highlightWidget;
-+				bool highlighted = btn->isOn() || btn->isDown() || btn->hasFocus() || btn == ( const QPushButton* ) highlightWidget;
- 
- 				if ( btn->isOn() || btn->isDown() ) {
- 					flags |= Style_Sunken;
+--- style/baghira.cpp.orig	Thu Nov 11 05:55:20 2004
++++ style/baghira.cpp	Fri Nov 19 21:21:49 2004
+@@ -1597,7 +1597,7 @@
+                 const QPushButton *btn = ( const QPushButton* ) widget;
+                 int x, y, w, h;
+                 r.rect( &x, &y, &w, &h );
+-                bool highlighted = btn->isOn() || btn->isDown() || btn->hasFocus() || btn == highlightWidget;
++                bool highlighted = btn->isOn() || btn->isDown() || btn->hasFocus() || btn == ( const QPushButton* ) highlightWidget;
+     
+                 if ( btn->isOn() || btn->isDown() )
+                     {
