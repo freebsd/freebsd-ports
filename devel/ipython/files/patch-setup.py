87aae6254e2e3b03d3cbb7d9f9d7366f4a0b8e5a
Volker Stolz <vs@FreeBSD.org>
2004-12-07 11:52:52 +0000

    Update to 0.6.5
    PR:		ports/74575
    Submitted by:	maintainer

--- devel/ipython/files/patch-setup.py
+++ devel/ipython/files/patch-setup.py
@@ -1,11 +1,13 @@
---- setup.py.orig	Thu Nov 20 10:26:08 2003
-+++ setup.py	Thu Nov 20 10:26:55 2003
-@@ -85,7 +85,7 @@
+--- setup.py.orig	Wed Dec  1 07:32:32 2004
++++ setup.py	Wed Dec  1 16:31:21 2004
+@@ -99,8 +99,8 @@
+ 
  # I can't find how to make distutils create a nested dir. structure, so
  # in the meantime do it manually. Butt ugly.
- docdirbase  = 'share/doc/IPython'
+-docdirbase  = 'share/doc/ipython-%s' % version
 -manpagebase = 'share/man/man1'
++docdirbase  = 'share/doc/ipython'
 +manpagebase = 'man/man1'
- docfiles    = filter(isfile, glob('doc/*[!~|.lyx|.sh|.1|.1.gz]'))
- examfiles   = filter(isfile, glob('doc/examples/*.py'))
- manfiles    = filter(isfile, glob('doc/manual/*.html')) + \
+ 
+ # We only need to exclude from this things NOT already excluded in the
+ # MANIFEST.in file.
