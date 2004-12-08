10f7c0937c756c0eb69c5caa590ee7fd4af1f7df
lioux <lioux@FreeBSD.org>
2004-12-08 01:36:10 +0000

    New port codeville version 0.1.9: An "anarchic" control version system without unnecessary re-merges

---
+++ devel/codeville/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	Tue Dec  7 23:17:53 2004
++++ setup.py	Tue Dec  7 23:18:37 2004
+@@ -43,6 +43,5 @@
+     packages = ["Codeville"],
+     ext_modules = plat_ext,
+ 
+-    scripts = scripts,
+-    data_files = data_files
++    scripts = scripts
+     )
