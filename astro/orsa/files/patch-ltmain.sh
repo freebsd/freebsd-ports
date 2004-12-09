a486995bdac6d9220078dd4f56e32d469c7bf05b
Ying-Chieh Liao <ijliao@FreeBSD.org>
2004-12-09 06:52:51 +0000

    add orsa 0.6.2 Orbit Reconstruction, Simulation and Analysis

---
+++ astro/orsa/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Dec  9 14:11:14 2004
++++ ltmain.sh	Thu Dec  9 14:11:27 2004
+@@ -4342,10 +4342,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
