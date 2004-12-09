ebfe720af5e65c7dc8323b6c154e06f7437e36a0
pav <pav@FreeBSD.org>
2004-12-09 01:05:13 +0000

    - Update to 5.3a and unmark BROKEN
    PR:		ports/74768
    Submitted by:	Diane Bruce <db@db.net> (maintainer)

---
+++ comms/qsstv/files/patch-qsstv_configfile.cpp
@@ -0,0 +1,10 @@
+--- qsstv/configfile.cpp.orig	Sun Feb  8 19:18:32 2004
++++ qsstv/configfile.cpp	Thu Dec  9 01:42:36 2004
+@@ -17,6 +17,7 @@
+ #include "configfile.h"
+ #include "config.h"
+ #include <stdlib.h>
++#include <unistd.h>
+ #include <qtextstream.h>
+ #include "localtypes.h"
+ 
