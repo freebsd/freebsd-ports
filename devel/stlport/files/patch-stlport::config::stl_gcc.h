45463d77745ddc748692a9ea80cbb9ec9a610fdd
Sergey Matveychuk <sem@FreeBSD.org>
2004-12-12 23:32:32 +0000

    - Utilize a new gcc feature
    PR:		ports/74550
    Submitted by:	Sepherosa Ziehau <sepherosa(at)softhome.net>

--- devel/stlport/files/patch-stlport::config::stl_gcc.h
+++ devel/stlport/files/patch-stlport::config::stl_gcc.h
@@ -1,14 +1,13 @@
 --- stlport/config/stl_gcc.h.orig	Sun Nov  2 16:59:11 2003
-+++ stlport/config/stl_gcc.h	Mon Nov  8 19:15:42 2004
-@@ -7,11 +7,21 @@
++++ stlport/config/stl_gcc.h	Mon Nov 29 18:42:05 2004
+@@ -7,11 +7,20 @@
  # define _STLP_USE_GLIBC
  #endif
  
-+#if defined(__FreeBSD__) && (__FreeBSD_cc_version >= 530001)
-+# define _STLP_DONT_USE_NESTED_TCLASS_THROUGHT_TPARAM	1
++#if !defined(__FreeBSD__) || (defined(__FreeBSD__) && (__FreeBSD_cc_version < 530001))
+ #   define _STLP_NO_MEMBER_TEMPLATE_KEYWORD
 +#endif
 +
- #   define _STLP_NO_MEMBER_TEMPLATE_KEYWORD
  
 -# if defined(__FreeBSD__) || defined (__hpux) || defined(__amigaos__) || ( defined(__OS2__) && defined(__EMX__) )
 +#if defined (__hpux) || defined(__amigaos__) || ( defined(__OS2__) && defined(__EMX__) )
@@ -24,7 +23,7 @@
  
  #ifdef __USLC__
  # include <config/stl_sco.h>
-@@ -81,7 +91,7 @@
+@@ -81,7 +90,7 @@
  
  # endif
  
@@ -33,7 +32,7 @@
  #ifndef __MINGW32__
  #   define _STLP_NO_NATIVE_MBSTATE_T      1
  #endif
-@@ -267,7 +277,7 @@
+@@ -267,7 +276,7 @@
  #   define _STLP_NATIVE_INCLUDE_PATH ../g++-v3
  #   define _STLP_NATIVE_OLD_STREAMS_INCLUDE_PATH ../g++-v3/backward
  #  else
