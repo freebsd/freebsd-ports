671feaf99aeb50fce7c584d980e6e47de9b3ca3f
krion <krion@FreeBSD.org>
2004-12-13 10:07:13 +0000

    Use RC_SUBR. Cleanups for net/libnids dependency.
    PR:		ports/75009
    Submitted by:	maintainer

---
+++ security/scanlogd/files/scanlogd.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Start or stop scanlogd
+# $FreeBSD$
+
+# PROVIDE: scanlogd
+# REQUIRE: DAEMON
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
+#
+
+prefix=%%PREFIX%%
+
+scanlogd_enable=${scanlogd_enable:-"NO"}	# Enable scanlogd
+scanlogd_program="${prefix}/sbin/scanlogd"	# Location of scanlogd
+scanlogd_flags=${scanlogd_flags:-""}		# Flags to scanlogd
+
+. %%RC_SUBR%%
+
+name="scanlogd"
+rcvar=`set_rcvar`
+command="${prefix}/sbin/${name}"
+required_files=""
+
+load_rc_config $name
+run_rc_command "$1"
