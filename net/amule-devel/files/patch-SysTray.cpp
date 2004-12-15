ad93e1744d92397e89fe16e02a3e307d45fec972
Edwin Groothuis <edwin@FreeBSD.org>
2004-12-15 07:13:05 +0000

    [NEW PORT] net/amule-devel (aMule 2.0.0rc7)
    	This port is designed to manage the "unstable" branch of
    	aMule, currently aMule 2.0.0rc7.
    
    PR:		ports/73502
    Submitted by:	Ganael LAPLANCHE <ganael.laplanche@martymac.com>

--- net/amule-devel/files/patch-SysTray.cpp
+++ net/amule-devel/files/patch-SysTray.cpp
@@ -1,12 +1,15 @@
---- src/SysTray.cpp.orig	Tue Jul  6 07:32:56 2004
-+++ src/SysTray.cpp	Tue Jul  6 07:33:44 2004
-@@ -23,8 +23,8 @@
- #include "sys.h"
- #include <libintl.h>
- #include <clocale>		// Needed for setlocale(3)
--#include <net/if.h>		// Needed for struct ifreq
- #include <sys/socket.h>		//
-+#include <net/if.h>		// Needed for struct ifreq
- #include <netinet/in.h>		// Needed for inet_ntoa
- #include <arpa/inet.h>		//
- #include <sys/ioctl.h>		// Needed for SIOCGIFADDR
+--- src/SysTray.cpp.orig	Sat Oct 23 05:53:20 2004
++++ src/SysTray.cpp	Sat Oct 23 05:55:40 2004
+@@ -24,6 +24,12 @@
+ 		#include <sys/types.h>
+ 	#endif /* __BSD__ */
+ 
++#if defined(__FreeBSD__) // for FreeBSD 4.x
++        #if __FreeBSD__ < 5
++                #include <sys/types.h>
++        #endif
++#endif
++
+ 	#include <cstddef>			// Needed for NULL. Must be BEFORE gtk/gdk headers!
+ 	#include <sys/socket.h>		//
+ 	#include <netinet/in.h>		// Needed for inet_ntoa
