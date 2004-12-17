a0e6867f3b0d01d9fe9794e42195a248387c7de0
Kirill Ponomarev <krion@FreeBSD.org>
2004-12-17 10:28:28 +0000

    Update to 0.9.2
    PR:		ports/75164
    Submitted by:	maintainer

--- devel/freebase/files/patch-freebase:bin:install.rb
+++ devel/freebase/files/patch-freebase:bin:install.rb
@@ -1,15 +1,19 @@
---- bin/install.rb.orig	Tue Aug 24 18:05:55 2004
-+++ bin/install.rb	Tue Aug 24 18:22:07 2004
-@@ -10,12 +10,7 @@
+--- bin/install.rb.orig	Mon Mar 24 07:10:57 2003
++++ bin/install.rb	Thu Dec 16 20:42:15 2004
+@@ -8,14 +8,9 @@
+ 
+ $srcdir = CONFIG["srcdir"]
  $version = CONFIG["MAJOR"]+"."+CONFIG["MINOR"]
- $libdir = File.join(CONFIG["libdir"], "ruby", $version)
- $archdir = File.join($libdir, CONFIG["arch"])
+-$libdir = File.join(CONFIG["libdir"], "ruby", $version)
+-$archdir = File.join($libdir, CONFIG["arch"])
 -$site_libdir = $:.find {|x| x =~ /site_ruby$/}
 -if !$site_libdir
 -  $site_libdir = File.join($libdir, "site_ruby")
 -elsif $site_libdir !~ Regexp.quote($version)
 -  $site_libdir = File.join($site_libdir, $version)
 -end
++$libdir = CONFIG["rubylibdir"]
++$archdir = CONFIG["archdir"]
 +$site_libdir = CONFIG["sitelibdir"]
  
  def install_rb(srcdir = nil)
