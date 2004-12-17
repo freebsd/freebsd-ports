a7f3a324eefe0f7a751eb0f862b29a9b30e4ea9b
David E. O'Brien <obrien@FreeBSD.org>
2004-12-17 19:35:59 +0000

    Support -pthreads for 5.0-5.2.1.

--- lang/gcc-ooo/files/patch-freebsd-spec.h
+++ lang/gcc-ooo/files/patch-freebsd-spec.h
@@ -1,13 +1,22 @@
 --- gcc/config/freebsd-spec.h.orig	Tue Mar  2 14:34:55 2004
-+++ gcc/config/freebsd-spec.h	Thu Dec 16 10:19:11 2004
-@@ -132,8 +132,8 @@
- #if FBSD_MAJOR >= 5
- #define FBSD_LIB_SPEC "							\
-   %{!shared:								\
--    %{!pg: %{pthread:-lc_r} -lc}					\
--    %{pg:  %{pthread:-lc_r_p} -lc_p}					\
-+    %{!pg: %{pthread:-lpthread} -lc}					\
-+    %{pg:  %{pthread:-lpthread} -lc_p}					\
++++ gcc/config/freebsd-spec.h	Fri Dec 17 11:22:26 2004
+@@ -130,11 +130,19 @@
    }"
  #else
+ #if FBSD_MAJOR >= 5
++#if __FreeBSD_version < 502102 /* upto FreeBSD 5.2.1 */
  #define FBSD_LIB_SPEC "							\
+   %{!shared:								\
+     %{!pg: %{pthread:-lc_r} -lc}					\
+     %{pg:  %{pthread:-lc_r_p} -lc_p}					\
+   }"
++#else
++#define FBSD_LIB_SPEC "							\
++  %{!shared:								\
++    %{!pg: %{pthread:-lpthread} -lc}					\
++    %{pg:  %{pthread:-lpthread} -lc_p}					\
++  }"
++#endif	/* deal with FreeBSD 5.0 - 5.2.1 */
+ #else
+ #define FBSD_LIB_SPEC "							\
+   %{!shared:								\
