c200d3526f85c67adc4e39d3fbe2cae83f992ed0
vs <vs@FreeBSD.org>
2004-12-17 14:44:51 +0000

    Update to 0.63
    PR:		ports/74303
    Submitted by:	Miguel Mendez
    Approved by:	maintainer timeout

--- emulators/dosbox/files/patch-src::dos::cdrom_image.cpp
+++ emulators/dosbox/files/patch-src::dos::cdrom_image.cpp
@@ -1,20 +1,11 @@
---- src/dos/cdrom_image.cpp.orig	Mon Oct 25 22:00:14 2004
-+++ src/dos/cdrom_image.cpp	Mon Oct 25 22:00:52 2004
+--- src/dos/cdrom_image.cpp.orig	Tue Oct  5 21:55:03 2004
++++ src/dos/cdrom_image.cpp	Tue Nov 23 21:11:08 2004
 @@ -23,7 +23,7 @@
  #include <cstdio>
  #include <fstream>
  #include <iostream>
--#include <limits>
-+/* #include <limits> */
+-#include <limits.h>
++/* #include <limits.h> */
  #include <sstream>
  #include <vector>
  #include <sys/stat.h>
-@@ -572,7 +572,7 @@
- 	if (stat(filename.c_str(), &test) == 0) return true;
- 	
- 	// check if file with path relative to cue file exists
--#if not defined(WIN32)
-+#if !defined(WIN32)
- 	string tmpstr(pathname + "/" + filename);
- 	if (stat(tmpstr.c_str(), &test) == 0) {
- 		filename = tmpstr;
