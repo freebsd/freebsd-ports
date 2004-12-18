3abbd129b5f058a337ac5d718133084dd9206414
ahze <ahze@FreeBSD.org>
2004-12-18 07:21:43 +0000

    Fix path to glade file
    Submitted by:	Joel Diaz <joeldiaz@bellsouth.net>

--- audio/tunesbrowser/files/patch-main.c
+++ audio/tunesbrowser/files/patch-main.c
@@ -1,11 +1,10 @@
---- main.c.orig	Tue Jul 27 17:40:25 2004
-+++ main.c	Tue Jul 27 17:40:49 2004
+--- main.c.orig	Tue Apr 13 03:17:50 2004
++++ main.c	Sat Dec 18 00:55:53 2004
 @@ -46,7 +46,7 @@
  
      gtk_init(&argc, &argv);
  
 -    xml = glade_xml_new(XSTR(UIDIR) "/tunesbrowser.glade", NULL, NULL);
-+    xml = glade_xml_new(XSTR(UIDIR) "/tunesbrowser/tunesbrowser.glade", NULL, NULL);
++    xml = glade_xml_new(XSTR(UIDIR) "/gnome/tunesbrowser/tunesbrowser.glade", NULL, NULL);
  
      glade_xml_signal_autoconnect(xml);
- 
