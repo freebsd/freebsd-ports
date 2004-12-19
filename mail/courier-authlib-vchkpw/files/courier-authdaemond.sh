c16332dab7b02117be3c4acaa6e30ecc3845b910
Oliver Lehmann <oliver@FreeBSD.org>
2004-12-19 11:25:28 +0000

    Add courier-authlib
    <quote>
    Copies of this library code used to exist in other tarballs: Courier,
    Courier-IMAP, and SqWebMail. Building and installing any of these packages
    would've automatically installed this authentication code.
    
    The authentication library is now a separate, standalone package. This
    authentication library must now be installed, separately, before upgrading
    to the following builds (or if installing them for the first time):
    Courier 0.48, Courier-IMAP 4.0, and SqWebMail 5.0. See NEWS for more information.
    </quote>
    
    http://www.courier-mta.org/authlib/

---
+++ mail/courier-authlib-vchkpw/files/courier-authdaemond.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# An rc.subr-style startup script for courier-authdaemond service.
+
+# PROVIDE: courier_authdaemond
+# REQUIRE: LOGIN
+# KEYWORD: FreeBSD shutdown
+
+# Define these courier_authdaemond_* variables in one of these files:
+# 	/etc/rc.conf
+# 	/etc/rc.conf.local
+# 	/etc/rc.conf.d/courier_authdaemond
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE
+
+courier_authdaemond_enable=${courier_authdaemond_enable-"NO"}
+
+. %%RC_SUBR%%
+
+name="courier_authdaemond"
+rcvar=`set_rcvar`
+command="%%PREFIX%%/sbin/authdaemond"
+
+start_cmd="authdaemond_cmd start"
+stop_cmd="authdaemond_cmd stop"
+restart_cmd="authdaemond_cmd stop && authdaemond_cmd start"
+
+load_rc_config $name
+
+authdaemond_cmd () {
+	case $1 in
+	start)
+		echo "Starting ${name}."
+		${command} start
+		;;
+	stop)
+		echo "Stopping ${name}."
+		${command} stop
+		;;
+	esac
+}
+
+run_rc_command "$1"
+
