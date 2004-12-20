adad5c6122d3fbe5a9095c541e91d09806ef584c
matusita <matusita@FreeBSD.org>
2004-12-20 14:35:32 +0000

    Update to 0.22.2, with minor updates including: 	- Add WITH_POPFILE_XMLRPC option for XMLRPC module dependency. 	- Add WITH_POPFILE_SOCKS, but it's just a placeholder... 	  (there's no IO::Socket::Socks module in our ports) 	- Minor comment cleanup. 	- Update patch-Bayes.pm for 0.22.2 (actually comments were changed).

--- mail/popfile/files/patch-Bayes.pm
+++ mail/popfile/files/patch-Bayes.pm
@@ -1,11 +1,11 @@
---- Classifier/Bayes.pm.dist	Fri Sep  3 07:16:08 2004
-+++ Classifier/Bayes.pm	Sun Oct 24 03:41:07 2004
-@@ -211,7 +211,7 @@
-     # it resolves to the full path to the database named in the
-     # database parameter above.
+--- Classifier/Bayes.pm.orig	Wed Dec 15 19:48:24 2004
++++ Classifier/Bayes.pm	Mon Dec 20 23:27:16 2004
+@@ -218,7 +218,7 @@
+     # $dbname can be used within it and it resolves to the full path
+     # to the database named in the database parameter above.
  
 -    $self->config_( 'dbconnect', 'dbi:SQLite:dbname=$dbname' );
 +    $self->config_( 'dbconnect', 'dbi:SQLite2:dbname=$dbname' );
      $self->config_( 'dbuser', '' ); $self->config_( 'dbauth', '' );
  
-     # No default unclassified weight is the number of times more sure POPFile
+     # SQLite 1.05+ have some problems we are resolving.  This lets us
