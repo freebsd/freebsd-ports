45be54d2bed0536f77d1de528989aba697eb01ef
Trevor Johnson <trevor@FreeBSD.org>
2004-12-20 22:41:30 +0000

    Remove dead site alpha.crl.dec.com.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -670,7 +670,6 @@ MASTER_SITE_REDHAT_LINUX+= \
 	ftp://ftp.kddlabs.co.jp/pub/Linux/packages/RedHat/redhat/linux/%SUBDIR%/ \
 	ftp://ftp.cse.cuhk.edu.hk/pub2/redhat/%SUBDIR%/ \
 	ftp://ftp2.compaq.com/pub/linux/RedHat/7.2-alpha/updates/rpms/alpha/ \
-	ftp://alpha.crl.dec.com/pub/linux/redhat/7.2-alpha/updates/rpms/alpha/ \
 	ftp://ftp.rz.uni-karlsruhe.de/pub/mirror/ftp.redhat.de/pub/redhat/updates/7.2/en/os/i386/
 
 #
