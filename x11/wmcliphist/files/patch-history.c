bdb5cb9f4af1211af10bca9a3fb0cf110393384b
Patrick Li <pat@FreeBSD.org>
2004-12-20 17:57:37 +0000

    Fix loading history on sparc64
    PR:		ports/66074
    Submitted by:	Andrew Belashov <bel@orel.ru>

--- x11/wmcliphist/files/patch-history.c
+++ x11/wmcliphist/files/patch-history.c
@@ -1,8 +1,35 @@
---- history.c.orig	Thu Jun  5 19:11:21 2003
-+++ history.c	Thu Jun  5 19:11:17 2003
+--- history.c.orig	Sun Aug 24 16:59:37 2003
++++ history.c	Mon Dec 20 12:33:42 2004
 @@ -1,3 +1,5 @@
 +#include <sys/types.h>
 +#include <sys/stat.h>
  #include <wmcliphist.h>
  
  
+@@ -182,7 +184,7 @@
+ history_load()
+ {
+ 	gchar		*buf;
+-	gint		len;
++	size_t		len;
+ 	gint		ver;
+ 	FILE		*f;
+ 	gchar		*fname;
+@@ -214,7 +216,7 @@
+ 	
+ 	while (!feof(f)) {
+ 
+-		if (fread(&len, sizeof(gint), 1, f) != 1)
++		if (fread(&len, sizeof(size_t), 1, f) != 1)
+ 			break;
+ 
+ 		if (num_items == num_items_to_keep) {
+@@ -299,7 +301,7 @@
+ 	list_node = g_list_last(history_items);
+ 	while (list_node) {
+ 		hist_item = (HISTORY_ITEM *)list_node->data;
+-		if (fwrite(&hist_item->content_len, sizeof(gint), 1, f) != 1) {
++		if (fwrite(&hist_item->content_len, sizeof(size_t), 1, f) != 1) {
+ 			tmp_errno = E_WRITE;
+ 			break;
+ 		}
