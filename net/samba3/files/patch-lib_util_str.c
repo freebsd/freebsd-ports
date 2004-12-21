92d47ccb5dcefdb8a7f18ee99b536ad89b910e07
Sergey Matveychuk <sem@FreeBSD.org>
2004-12-21 12:24:03 +0000

    - Update to 3.0.10
    PR:		ports/75168
    Submitted by:	Joerg Pulz <Joerg.Pulz(at)frm2.tum.de>
    Reworked by:	maintainer

--- net/samba3/files/patch-lib_util_str.c
+++ net/samba3/files/patch-lib_util_str.c
@@ -1,13 +1,13 @@
---- lib/util_str.c.orig	Tue Apr 20 22:42:55 2004
-+++ lib/util_str.c	Fri May 14 02:02:30 2004
-@@ -1980,10 +1980,16 @@
+--- lib/util_str.c.orig	Wed Dec 15 15:33:12 2004
++++ lib/util_str.c	Fri Dec 17 02:15:34 2004
+@@ -2008,10 +2008,16 @@
  {
  	int bits = 0;
  	int char_count = 0;
 -	size_t out_cnt = 0;
 -	size_t len = data.length;
 -	size_t output_len = data.length * 2;
--	char *result = malloc(output_len); /* get us plenty of space */
+-	char *result = SMB_MALLOC(output_len); /* get us plenty of space */
 +	size_t out_cnt, len, output_len;
 +	char *result;
 +
@@ -17,7 +17,7 @@
 +	out_cnt = 0;
 +	len = data.length;
 +	output_len = data.length * 2;
-+	result = malloc(output_len); /* get us plenty of space */
++	result = SMB_MALLOC(output_len); /* get us plenty of space */
  
  	while (len-- && out_cnt < (data.length * 2) - 5) {
  		int c = (unsigned char) *(data.data++);
