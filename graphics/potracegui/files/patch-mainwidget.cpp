06697563defdec1ad2a8995b2f6f20a7b2d1a916
Markus Brueffer <markus@FreeBSD.org>
2004-12-22 17:46:02 +0000

    - Update to version 1.2 - Pet portlint

---
+++ graphics/potracegui/files/patch-mainwidget.cpp
@@ -0,0 +1,11 @@
+--- src/mainwidget.cpp.orig	Wed Dec 22 18:36:50 2004
++++ src/mainwidget.cpp	Wed Dec 22 18:38:09 2004
+@@ -44,6 +44,8 @@
+ #include <kpushbutton.h>
+ #include <kmessagebox.h>
+ 
++#include <unistd.h>
++
+ #include <fstream>
+ using namespace std;
+ 
