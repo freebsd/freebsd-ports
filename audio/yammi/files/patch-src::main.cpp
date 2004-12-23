0d3f7688e7304ed0e59bb63627bfb9cf66c6b906
lesi <lesi@FreeBSD.org>
2004-12-23 04:52:33 +0000

    Update audio/yammi to version 1.2-rc
    PR:		75079
    Submitted by:	Arjan van Leeuwen (maintainer)

---
+++ audio/yammi/files/patch-src::main.cpp
@@ -0,0 +1,12 @@
+ --- src/main.cpp.orig	Fri Dec 17 19:44:35 2004
+ +++ src/main.cpp	Fri Dec 17 19:44:59 2004
+ @@ -17,7 +17,7 @@
+  
+  #include "yammigui.h"
+  
+ -
+ +#include <time.h>
+  
+  static KCmdLineOptions options[] = { 
+  			{ "d", 0, 0 },
+ 
