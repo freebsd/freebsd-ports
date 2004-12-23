65440fcd9bcb20a1d534ae744432598de80568e0
anholt <anholt@FreeBSD.org>
2004-12-23 01:35:49 +0000

    Update X.Org ports to 6.8.1, and the DRI port to use Mesa 6.2.  Because Mesa 6.2 DRI drivers are incompatible with the old libGL in XFree86, they have been repocopied to graphics/xfree86-dri.  Also note that with this commit the html manpages are going away, and with it the runtime dependency on perl for the imake port.
    Release notes at: http://www.x.org/X11R6.8.1/RELNOTES.txt
    
    Props to:	kris (multiple cluster runs and sorting through logs)
    		lesi (fixing all the issues in those logs, and more)

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1571,14 +1571,15 @@ PKG_IGNORE_DEPENDS?=		'^XFree86-3\.'
 
 .else
 
-.if defined(USE_IMAKE)
-RUN_DEPENDS+=			mkhtmlindex:${X_IMAKE_PORT}
-.endif
 .if defined(USE_XPM) || defined(USE_GL)
 USE_XLIB=			yes
 .endif
 
+.if ${X_WINDOW_SYSTEM:L} == xorg
+XAWVER=				8
+.else
 XAWVER=				7
+.endif
 PKG_IGNORE_DEPENDS?=		'this_port_does_not_exist'
 
 .endif
@@ -1772,7 +1773,6 @@ NONEXISTENT?=	/nonexistent
 # Miscellaneous overridable commands:
 GMAKE?=			gmake
 XMKMF?=			xmkmf -a
-MKHTMLINDEX?=		${X11BASE}/bin/mkhtmlindex
 .if exists(/sbin/md5)
 MD5?=			/sbin/md5
 .elif exists(/bin/md5)
@@ -2638,16 +2638,6 @@ INFO_PATH?=	share/info
 INFO_PATH?=	info
 .endif
 
-.if ${X_WINDOW_SYSTEM:L} == xfree86-3
-XFREE86_HTML_MAN=	no
-.else
-.if defined(USE_IMAKE)
-XFREE86_HTML_MAN?=	yes
-.else
-XFREE86_HTML_MAN?=	no
-.endif
-.endif
-
 DOCSDIR?=	${PREFIX}/share/doc/${PORTNAME}
 EXAMPLESDIR?=	${PREFIX}/share/examples/${PORTNAME}
 DATADIR?=	${PREFIX}/share/${PORTNAME}
@@ -3284,9 +3274,6 @@ do-install:
 	@(cd ${INSTALL_WRKSRC} && ${SETENV} ${MAKE_ENV} ${GMAKE} ${MAKE_FLAGS} ${MAKEFILE} ${MAKE_ARGS} ${INSTALL_TARGET})
 .if defined(USE_IMAKE) && !defined(NO_INSTALL_MANPAGES)
 	@(cd ${INSTALL_WRKSRC} && ${SETENV} ${MAKE_ENV} ${GMAKE} ${MAKE_FLAGS} ${MAKEFILE} ${MAKE_ARGS} install.man)
-.if ${XFREE86_HTML_MAN:L} == yes
-	@${MKHTMLINDEX} ${PREFIX}/lib/X11/doc/html
-.endif
 .endif
 .else # !defined(USE_GMAKE)
 .if defined(PERL_MODBUILD)
@@ -3295,9 +3282,6 @@ do-install:
 	@(cd ${INSTALL_WRKSRC} && ${SETENV} ${MAKE_ENV} ${MAKE} ${MAKE_FLAGS} ${MAKEFILE} ${MAKE_ARGS} ${INSTALL_TARGET})
 .if defined(USE_IMAKE) && !defined(NO_INSTALL_MANPAGES)
 	@(cd ${INSTALL_WRKSRC} && ${SETENV} ${MAKE_ENV} ${MAKE} ${MAKE_FLAGS} ${MAKEFILE} ${MAKE_ARGS} install.man)
-.if ${XFREE86_HTML_MAN:L} == yes
-	@${MKHTMLINDEX} ${PREFIX}/lib/X11/doc/html
-.endif
 .endif
 .endif
 .endif
@@ -4712,21 +4696,6 @@ generate-plist:
 	@for i in $$(${ECHO_CMD} ${__MANPAGES} ${_TMLINKS:M${_PREFIX}*:S|^${_PREFIX}/||} ' ' | ${SED} -E -e 's|man([1-9ln])/([^/ ]+) |cat\1/\2 |g'); do \
 		${ECHO_CMD} "@unexec rm -f %D/$${i%.gz} %D/$${i%.gz}.gz" >> ${TMPPLIST}; \
 	done
-.if ${XFREE86_HTML_MAN:L} == "yes"
-.for mansect in 1 2 3 4 5 6 7 8 9 L N
-.for man in ${MAN${mansect}}
-	@${ECHO_CMD} lib/X11/doc/html/${man}.html >> ${TMPPLIST}
-.endfor
-.endfor
-	@${ECHO_CMD} "@unexec %D/bin/mkhtmlindex %D/lib/X11/doc/html" >> ${TMPPLIST}
-	@${ECHO_CMD} "@exec %D/bin/mkhtmlindex %D/lib/X11/doc/html" >> ${TMPPLIST}
-.if defined(MLINKS)
-	@${ECHO_CMD} ${MLINKS} | ${AWK} \
-	'{ for (i=1; i<=NF; i++) { \
-		if (i % 2 == 0) { printf "lib/X11/doc/html/%s.html\n", $$i } \
-	} }' >> ${TMPPLIST}
-.endif
-.endif
 .endfor
 	@if [ -f ${PLIST} ]; then \
 		${SED} ${PLIST_SUB:S/$/!g/:S/^/ -e s!%%/:S/=/%%!/} ${PLIST} >> ${TMPPLIST}; \
