4c99597992b71933a65671ad046abc9ff128bf5d
thierry <thierry@FreeBSD.org>
2004-12-26 10:22:58 +0000

    Upgrade to 3.0.
    Horde Version 3 differs from the 2.x releases in many ways, including
    the following:
    	* Full support for groups and arbitrary permissions.
    	* Completely rewritten, fully RFC-compliant MIME system with many
    	  new MIME viewers.
    	* Many i18n improvements, including full charset support for the whole
    	  framework.
    	* User customizable portal page.
    	* Theme support.
    	* Global personal categories and category colors.
    	* Access (shortcut) keys.
    	* Dynamically updating tree menu for applications, including Mozilla
    	  sidebar support.
    	* XML-based application configuration through a graphical user interface.
    	* Forms creation and validation API.
    	* Template engine.
    	* Crypt API with support for PGP/GPG and S/MIME.
    	* XML-RPC and SOAP server and client, experimental SyncML support.
    	* Improved notification system.
    	* Many new APIs like Compress, Image, PDF, History, iCalendar, SyncML,
    	  CLI, Version control, NLS, Timer, and SVG.
    	* and many more...
    
    This is a resurrection of the previous www/horde, upgraded from www/horde2.
    
    PR:		ports/75434
    Submitted by:	/me

---
+++ www/horde/files/patch-po_translation.php
@@ -0,0 +1,8 @@
+--- po/translation.php.orig	Tue Jan 21 16:05:36 2003
++++ po/translation.php	Wed Jan 29 23:00:27 2003
+@@ -1,4 +1,4 @@
+-#!/usr/bin/php -q
++#!/usr/local/bin/php -q
+ <?php
+ /**
+  * Translation helper application for the Horde framework.
