0c587e779cfe6692c4fe6b1f791c529a2ac795b3
pav <pav@FreeBSD.org>
2004-12-28 15:14:21 +0000

    - Workaround for people who have mpeg4ip installed poluting system with   another copy of sdp.h header
    PR:		ports/75572
    Submitted by:	Guido Falsi <mad@madpilot.net> (maintainer)

---
+++ comms/obexapp/files/patch-main.c
@@ -0,0 +1,11 @@
+--- main.c.orig	Tue Dec 28 15:23:30 2004
++++ main.c	Tue Dec 28 15:23:48 2004
+@@ -36,7 +36,7 @@
+ #include <libgen.h>
+ #include <limits.h>
+ #include <obex.h>
+-#include <sdp.h>
++#include </usr/include/sdp.h>
+ #include <signal.h>
+ #include <stdarg.h>
+ #include <stdio.h>
---
+++ comms/obexapp/files/patch-sdp.c
@@ -0,0 +1,11 @@
+--- sdp.c.orig	Tue Dec 28 15:24:24 2004
++++ sdp.c	Tue Dec 28 15:24:42 2004
+@@ -31,7 +31,7 @@
+ 
+ #include <bluetooth.h>
+ #include <errno.h>
+-#include <sdp.h>
++#include </usr/include/sdp.h>
+ #include <stdio.h>
+ 
+ #undef	PROTOCOL_DESCRIPTOR_LIST_BUFFER_SIZE
