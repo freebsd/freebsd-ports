00cc0614086999e89aa6f699018d151292f1b77a
Jun Kuriyama <kuriyama@FreeBSD.org>
2004-12-29 13:39:02 +0000

    o Apply ports/73114 (1)     - Update to 20020426a.     - Update to bsd.java.mk 2.0     - Register dependencies on textproc/xp and textproc/xml4j     - Honor hier(7): install docs in DOCSDIR, samples in EXAMPLESDIR     - Honor NOPORTDOCS     - Single script with symlinks     - Use 'classpath' from java/javavmwrapper o Release maintainership.
    PR:		ports/73114
    Submitted by:	hq (1)

--- textproc/xt/files/xt-xml4j.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# $FreeBSD$
-
-export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xerces.jar:${CLASSPATH}
-
-javavm -Dcom.jclark.xsl.sax.parser=org.apache.xerces.parsers.SAXParser com.jclark.xsl.sax.Driver $*
--- textproc/xt/files/xt-xp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# $FreeBSD$
-
-export CLASSPATH=%%PREFIX%%/share/java/classes/xt.jar:%%PREFIX%%/share/java/classes/xp-0.5.jar:%%PREFIX%%/share/java/classes/sax.jar:${CLASSPATH}
-
-javavm com.jclark.xsl.sax.Driver $*
