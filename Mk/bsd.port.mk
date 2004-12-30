e35e7da51b8f6bac5858574905dd92237dcee079
kris <kris@FreeBSD.org>
2004-12-30 23:56:13 +0000

    Rework MTREE_FILE handling from last commit:
    * Users may specify a nonstandard PREFIX to install somewhere.  If they
      are trying to use an USE_X_PREFIX=yes port, install with BSD.x11*.dist.
      Otherwise, use BSD.local.dist.  Ports that deliberately set a nonstandard
      PREFIX must also set NO_MTREE to avoid spamming their hierarchy with an
      inappropriate MTREE specfile.
    
    Pointed out by:	krion

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1884,20 +1884,18 @@ EXTRACT_CMD?=			${GZIP_CMD}
 
 # Figure out where the local mtree file is
 .if !defined(MTREE_FILE) && !defined(NO_MTREE)
-.if ${PREFIX} == ${X11BASE}
+.if ${PREFIX} == ${X11BASE} || defined(USE_X_PREFIX)
+# User may have specified non-standard PREFIX for installing a port that
+# uses X
 .if ${X_WINDOW_SYSTEM:L} == xfree86-3
 MTREE_FILE=	/etc/mtree/BSD.x11.dist
 .else
 MTREE_FILE=	/etc/mtree/BSD.x11-4.dist
 .endif
-.else
-.if ${PREFIX} == ${LOCALBASE}
-MTREE_FILE=	/etc/mtree/BSD.local.dist
 .elif ${PREFIX} == /usr
 MTREE_FILE=	/etc/mtree/BSD.usr.dist
 .else
-NO_MTREE=	yes	# Can't figure out prefix, assume nonstandard
-.endif
+MTREE_FILE=	/etc/mtree/BSD.local.dist
 .endif
 .endif
 MTREE_CMD?=	/usr/sbin/mtree
