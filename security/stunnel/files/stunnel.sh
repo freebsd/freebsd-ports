197e247a539039c8aa675505d26791aa2a7c97fc
roam <roam@FreeBSD.org>
2004-12-30 15:39:26 +0000

    Okay then, purge the last reference to MySQL in the startup script...

--- security/stunnel/files/stunnel.sh
+++ security/stunnel/files/stunnel.sh
@@ -9,7 +9,7 @@
 # KEYWORD: shutdown
 
 #
-# Add the following line to /etc/rc.conf to enable mysql:
+# Add some of the following variables to /etc/rc.conf to configure stunnel:
 # stunnel_enable (bool):	Set to "NO" by default.
 #				Set it to "YES" to enable stunnel.
 # stunnel_config (str):		Default "!!PREFIX!!/etc/stunnel/stunnel.conf"
