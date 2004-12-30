8935bd10dbfb4d72d94d184702856f30c8eb5b08
nobutaka <nobutaka@FreeBSD.org>
2004-12-30 00:39:19 +0000

    Update to 1.0. (At last!)

--- multimedia/libxine/files/patch-src:libffmpeg:libavcodec:h263.c
+++ multimedia/libxine/files/patch-src:libffmpeg:libavcodec:h263.c
@@ -1,17 +1,15 @@
---- src/libffmpeg/libavcodec/h263.c.orig	Sun May 30 19:24:19 2004
-+++ src/libffmpeg/libavcodec/h263.c	Sun Aug  1 12:52:11 2004
-@@ -66,8 +66,8 @@
+--- src/libffmpeg/libavcodec/h263.c.orig	Sun Sep 12 21:17:13 2004
++++ src/libffmpeg/libavcodec/h263.c	Mon Nov 15 01:05:07 2004
+@@ -63,7 +63,7 @@
  static int h263p_decode_umotion(MpegEncContext * s, int pred);
  static int h263_decode_block(MpegEncContext * s, DCTELEM * block,
                               int n, int coded);
--static inline int mpeg4_decode_dc(MpegEncContext * s, int n, int *dir_ptr);
 -static inline int mpeg4_decode_block(MpegEncContext * s, DCTELEM * block,
-+static int mpeg4_decode_dc(MpegEncContext * s, int n, int *dir_ptr);
 +static int mpeg4_decode_block(MpegEncContext * s, DCTELEM * block,
                                int n, int coded, int intra, int rvlc);
  static int h263_pred_dc(MpegEncContext * s, int n, uint16_t **dc_val_ptr);
  #ifdef CONFIG_ENCODERS
-@@ -4548,7 +4548,7 @@
+@@ -3279,7 +3279,7 @@
   * @param dir_ptr the prediction direction will be stored here
   * @return the quantized dc
   */
@@ -20,7 +18,7 @@
  {
      int level, code;
  
-@@ -4593,7 +4593,7 @@
+@@ -3629,7 +3629,7 @@
   * decodes a block.
   * @return <0 if an error occured
   */
