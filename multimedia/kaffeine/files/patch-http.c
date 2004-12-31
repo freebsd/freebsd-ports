a57e85ca8cc4339dd19eadae3d66a4ab7761ce5f
Tilman Keskinoz <arved@FreeBSD.org>
2004-12-31 14:00:40 +0000

    Update to 0.5 Chase xine Shared Library version

--- multimedia/kaffeine/files/patch-http.c
+++ multimedia/kaffeine/files/patch-http.c
@@ -1,10 +1,34 @@
---- kaffeine/http.c.orig	Sat Mar 27 23:09:53 2004
-+++ kaffeine/http.c	Sat Mar 27 23:15:12 2004
+--- kaffeine/player-parts/http.c.orig	Wed Dec  8 15:07:06 2004
++++ kaffeine/player-parts/http.c	Fri Dec 31 14:57:16 2004
 @@ -48,6 +48,7 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <sys/types.h>
 +#include <netinet/in.h>
  #include <sys/socket.h>
+ #include <netinet/in.h>
  #include <netdb.h>
- #include <unistd.h>
+@@ -56,6 +57,11 @@
+ #include <string.h>
+ #include <ctype.h>
+ #include <errno.h>
++#ifdef HAVE_STDINT_H
++#include <stdint.h>
++#else
++#include <inttypes.h>
++#endif
+ 
+ #include "http.h"
+ 
+@@ -514,7 +520,11 @@
+ 	}
+       } else {
+ 	if (this->contentlength == 0) {
++#ifdef HAVE_STDINT_H
+ 	  intmax_t contentlength;
++#else
++	  int64_t contentlength;
++#endif
+ 	  
+ 	  if ( strlen(this->buf)<(BUFSIZE-20) && sscanf(this->buf, "Content-Length: %Ld", &contentlength) == 1) {
+ 	    printf ("http: content length = %Ld bytes\n", contentlength);
