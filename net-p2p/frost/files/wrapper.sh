f1ad311c5b51459f59afd8f57b6c5a2322ec8de7
lioux <lioux@FreeBSD.org>
2004-12-31 11:51:12 +0000

    Update to snapshot 20041202
    Submitted by:	Peter Thoenen <eol1@yahoo.com>

--- net-p2p/frost/files/wrapper.sh
+++ net-p2p/frost/files/wrapper.sh
@@ -9,7 +9,7 @@ JAR_DIR="%%JAR_DIR%%"
 JAR_FILE="%%JAR_FILE%%"
 DATADIR="%%DATADIR%%"
 HOME_DIR=${HOME}/.frost
-CLASSPATH="${PREFIX}/${JAR_DIR}/BCastle.jar:${PREFIX}/${JAR_DIR}/fecImpl.jar:${PREFIX}/${JAR_DIR}/frost.jar:${PREFIX}/${JAR_DIR}/genChkImpl.jar:${PREFIX}/${JAR_DIR}/jocache.jar:${PREFIX}/${JAR_DIR}/mailapi.jar:${PREFIX}/${JAR_DIR}/skinlf.jar:${PREFIX}/${JAR_DIR}/skinlfFix.jar:${PREFIX}/${JAR_DIR}/smtp.jar:${PREFIX}/${JAR_DIR}/xercesImpl.jar:${PREFIX}/${JAR_DIR}/xml-apis.jar:$CLASSPATH"
+CLASSPATH="${PREFIX}/${JAR_DIR}/BCastle.jar:${PREFIX}/${JAR_DIR}/fec-native.jar:${PREFIX}/${JAR_DIR}/fecImpl.jar:${PREFIX}/${JAR_DIR}/frost.jar:${PREFIX}/${JAR_DIR}/genChkImpl.jar:${PREFIX}/${JAR_DIR}/jocache.jar:${PREFIX}/${JAR_DIR}/mailapi.jar:${PREFIX}/${JAR_DIR}/skinlf.jar:${PREFIX}/${JAR_DIR}/skinlfFix.jar:${PREFIX}/${JAR_DIR}/smtp.jar:${PREFIX}/${JAR_DIR}/xercesImpl.jar:${PREFIX}/${JAR_DIR}/xml-apis.jar"
 
 if [ ! -d ${HOME_DIR}/config ]
 then
@@ -25,6 +25,8 @@ then
 fi
 
 ln -sf ${PREFIX}/${DATADIR}/themepack.zip ${HOME_DIR}/themes/themepack.zip
+ln -sf ${PREFIX}/${DATADIR}/aquathemepack.zip \
+	${HOME_DIR}/themes/aquathemepack.zip
 
 cd ${HOME_DIR} &&
-	exec ${JAVAVM} -cp $CLASSPATH frost.frost "${@}"
+	exec ${JAVAVM} -cp $CLASSPATH frost.Frost "${@}"
