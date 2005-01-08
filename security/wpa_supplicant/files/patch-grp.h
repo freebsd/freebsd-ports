a375fcd47eec545b18438d1e2d2d2ece6e232b7d
brooks <brooks@FreeBSD.org>
2005-01-08 19:06:48 +0000

    - Spell wpa_supplicant with two 'p's in PORTNAME - Remove now unnecessicary DISTNAME - Add net to CATEGORIES - Ditch the pkg-plist file in favor of PLIST_FILES, there were only   three lines in the plist - Make the patch files relative to WRKSRC so we can drop PATCH_ARGS - Install some documentation - Bump PORTREVISION for new docs

--- security/wpa_supplicant/files/patch-grp.h
+++ security/wpa_supplicant/files/patch-grp.h
@@ -1,6 +1,5 @@
-diff -ru work/wpa_supplicant-0.3.0/eap.c work.sav/wpa_supplicant-0.3.0/eap.c
---- work/wpa_supplicant-0.3.0/eap.c	Sun Dec  5 16:28:25 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap.c	Sun Dec 12 15:53:27 2004
+--- eap.c	Sun Dec  5 16:28:25 2004
++++ eap.c	Sun Dec 12 15:53:27 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -9,9 +8,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap.c work.sav/wpa_supplicant-0.3.0/eap.c
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_aka.c work.sav/wpa_supplicant-0.3.0/eap_aka.c
---- work/wpa_supplicant-0.3.0/eap_aka.c	Sat Nov 27 10:29:04 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_aka.c	Sun Dec 12 15:51:53 2004
+--- eap_aka.c	Sat Nov 27 10:29:04 2004
++++ eap_aka.c	Sun Dec 12 15:51:53 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -20,9 +18,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_aka.c work.sav/wpa_supplicant-0.3.0/eap_a
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_gtc.c work.sav/wpa_supplicant-0.3.0/eap_gtc.c
---- work/wpa_supplicant-0.3.0/eap_gtc.c	Sat Nov 27 10:29:04 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_gtc.c	Sun Dec 12 15:50:50 2004
+--- eap_gtc.c	Sat Nov 27 10:29:04 2004
++++ eap_gtc.c	Sun Dec 12 15:50:50 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <netinet/in.h>
@@ -31,9 +28,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_gtc.c work.sav/wpa_supplicant-0.3.0/eap_g
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_leap.c work.sav/wpa_supplicant-0.3.0/eap_leap.c
---- work/wpa_supplicant-0.3.0/eap_leap.c	Sat Nov 27 10:29:04 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_leap.c	Sun Dec 12 15:51:40 2004
+--- eap_leap.c	Sat Nov 27 10:29:04 2004
++++ eap_leap.c	Sun Dec 12 15:51:40 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -42,9 +38,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_leap.c work.sav/wpa_supplicant-0.3.0/eap_
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_md5.c work.sav/wpa_supplicant-0.3.0/eap_md5.c
---- work/wpa_supplicant-0.3.0/eap_md5.c	Sat Dec  4 08:53:24 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_md5.c	Sun Dec 12 15:50:32 2004
+--- eap_md5.c	Sat Dec  4 08:53:24 2004
++++ eap_md5.c	Sun Dec 12 15:50:32 2004
 @@ -15,6 +15,7 @@
  #include <stdlib.h>
  #include <stdio.h>
@@ -53,9 +48,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_md5.c work.sav/wpa_supplicant-0.3.0/eap_m
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_mschapv2.c work.sav/wpa_supplicant-0.3.0/eap_mschapv2.c
---- work/wpa_supplicant-0.3.0/eap_mschapv2.c	Sun Dec  5 16:28:25 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_mschapv2.c	Sun Dec 12 15:50:42 2004
+--- eap_mschapv2.c	Sun Dec  5 16:28:25 2004
++++ eap_mschapv2.c	Sun Dec 12 15:50:42 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -64,9 +58,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_mschapv2.c work.sav/wpa_supplicant-0.3.0/
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_otp.c work.sav/wpa_supplicant-0.3.0/eap_otp.c
---- work/wpa_supplicant-0.3.0/eap_otp.c	Sat Nov 27 10:29:04 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_otp.c	Sun Dec 12 15:51:01 2004
+--- eap_otp.c	Sat Nov 27 10:29:04 2004
++++ eap_otp.c	Sun Dec 12 15:51:01 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <netinet/in.h>
@@ -75,9 +68,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_otp.c work.sav/wpa_supplicant-0.3.0/eap_o
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_peap.c work.sav/wpa_supplicant-0.3.0/eap_peap.c
---- work/wpa_supplicant-0.3.0/eap_peap.c	Sun Dec  5 16:28:25 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_peap.c	Sun Dec 12 15:50:15 2004
+--- eap_peap.c	Sun Dec  5 16:28:25 2004
++++ eap_peap.c	Sun Dec 12 15:50:15 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -86,9 +78,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_peap.c work.sav/wpa_supplicant-0.3.0/eap_
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_psk.c work.sav/wpa_supplicant-0.3.0/eap_psk.c
---- work/wpa_supplicant-0.3.0/eap_psk.c	Sat Nov 27 10:29:04 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_psk.c	Sun Dec 12 15:51:47 2004
+--- eap_psk.c	Sat Nov 27 10:29:04 2004
++++ eap_psk.c	Sun Dec 12 15:51:47 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -97,9 +88,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_psk.c work.sav/wpa_supplicant-0.3.0/eap_p
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_sim.c work.sav/wpa_supplicant-0.3.0/eap_sim.c
---- work/wpa_supplicant-0.3.0/eap_sim.c	Sat Nov 27 10:29:04 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_sim.c	Sun Dec 12 15:51:08 2004
+--- eap_sim.c	Sat Nov 27 10:29:04 2004
++++ eap_sim.c	Sun Dec 12 15:51:08 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -108,9 +98,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_sim.c work.sav/wpa_supplicant-0.3.0/eap_s
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_tls.c work.sav/wpa_supplicant-0.3.0/eap_tls.c
---- work/wpa_supplicant-0.3.0/eap_tls.c	Sat Nov 27 10:29:04 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_tls.c	Sun Dec 12 15:49:28 2004
+--- eap_tls.c	Sat Nov 27 10:29:04 2004
++++ eap_tls.c	Sun Dec 12 15:49:28 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -119,9 +108,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_tls.c work.sav/wpa_supplicant-0.3.0/eap_t
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_tls_common.c work.sav/wpa_supplicant-0.3.0/eap_tls_common.c
---- work/wpa_supplicant-0.3.0/eap_tls_common.c	Wed Oct  6 21:25:17 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_tls_common.c	Sun Dec 12 15:53:38 2004
+--- eap_tls_common.c	Wed Oct  6 21:25:17 2004
++++ eap_tls_common.c	Sun Dec 12 15:53:38 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -130,9 +118,8 @@ diff -ru work/wpa_supplicant-0.3.0/eap_tls_common.c work.sav/wpa_supplicant-0.3.
  
  #include "common.h"
  #include "eap_i.h"
-diff -ru work/wpa_supplicant-0.3.0/eap_ttls.c work.sav/wpa_supplicant-0.3.0/eap_ttls.c
---- work/wpa_supplicant-0.3.0/eap_ttls.c	Sat Nov 27 10:29:04 2004
-+++ work.sav/wpa_supplicant-0.3.0/eap_ttls.c	Sun Dec 12 15:50:25 2004
+--- eap_ttls.c	Sat Nov 27 10:29:04 2004
++++ eap_ttls.c	Sun Dec 12 15:50:25 2004
 @@ -16,6 +16,7 @@
  #include <stdio.h>
  #include <string.h>
