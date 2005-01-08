37bfaf52b3f8dbc59e190231e65e83e736524a8e
Kirill Ponomarev <krion@FreeBSD.org>
2005-01-08 10:58:55 +0000

    Add option "-H" - horizontal offset (e.g. osdclock -H 100).
    PR:		ports/75924
    Submitted by:	maintainer

--- x11-clocks/osdclock/files/patch-osd_clock.c
+++ x11-clocks/osdclock/files/patch-osd_clock.c
@@ -1,6 +1,36 @@
---- osd_clock.c.orig	Thu Mar 29 19:10:24 2001
-+++ osd_clock.c	Fri Aug  6 08:05:45 2004
-@@ -109,7 +109,15 @@
+--- osd_clock.c.orig	Sun Apr  8 02:50:01 2001
++++ osd_clock.c	Tue Jan  4 16:27:20 2005
+@@ -57,8 +57,9 @@
+   int offset = 0;
+   int shadow = 2;
+   int interval = 1;
+-
+-  while ((c = getopt_long(argc ,argv,"f:c:d:F:i:s:o:tbh",
++  int horizontal_offset = 0;
++  
++  while ((c = getopt_long(argc ,argv,"f:c:d:F:i:s:o:tbH:h",
+ 			  long_options, NULL)) != -1)
+   {
+     switch(c)
+@@ -90,6 +91,9 @@
+       case 'b':
+ 	pos = XOSD_bottom;
+ 	break;
++      case 'H':
++	horizontal_offset = atoi(optarg);
++	break;
+       case 'h':
+ 	printf("USAGE: %s [-flag args]\n"
+ 		"\t-f\tfully qualified font.  default: fixed\n"
+@@ -102,6 +106,7 @@
+ 		"\t-d\tDelay (time the clock stays on screen when it's updated)\n"
+ 	        "\t\tin seconds\n"
+ 		"\t-i\tInterval (time between updates) in seconds\n"
++		"\t-H\thorizontal offset. default: 0\n"
+ 		"\t-h\tthis help message\n",
+ 		argv[0]);
+ 	return EXIT_SUCCESS;
+@@ -109,7 +114,16 @@
      }
    }
    
@@ -12,13 +42,14 @@
 +  xosd_set_timeout(osd, delay);
 +  xosd_set_pos(osd, pos);
 +  xosd_set_vertical_offset(osd, offset);
++  xosd_set_horizontal_offset(osd, horizontal_offset);
 +  xosd_set_shadow_offset(osd, shadow);
 +  
    if (!osd)
    {
      fprintf (stderr, "Error initializing osd\n");
-@@ -127,11 +135,12 @@
-     output = malloc(255 * sizeof(char));
+@@ -126,11 +140,12 @@
+    
      strftime(output, 255, format, localtime(&curr_time));
  
 -    xosd_display (osd, 1, XOSD_string, output);
