2a4160bde7e60080b45604ce5a5d0bbed1bfb66c
fjoe <fjoe@FreeBSD.org>
2005-01-08 11:16:50 +0000

    Update to 1.25.
    PR:		72472

--- archivers/upx/files/patch-conf.h
+++ archivers/upx/files/patch-conf.h
@@ -1,14 +1,5 @@
 --- conf.h.orig	Wed Jan 28 02:49:57 2004
 +++ conf.h	Tue Feb 17 06:24:41 2004
-@@ -109,7 +109,7 @@
- #    define UPX_E_OK      UCL_E_OK
- #    define UPX_E_ERROR   UCL_E_ERROR
- #    define UPX_E_OUT_OF_MEMORY UCL_E_OUT_OF_MEMORY
--#    define __UPX_ENTRY   __UCL_ENTRY
-+#    define __UPX_ENTRY
- #  endif
- #endif
- #if defined(WITH_NRV)
 @@ -518,6 +518,7 @@
  #define UPX_F_BVMLINUZ_i386     16
  #define UPX_F_ELKS_8086         17
