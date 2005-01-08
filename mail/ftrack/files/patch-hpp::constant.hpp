32ed1b6782b60f6a4d31b3d2a46debb8a723b131
Yen-Ming Lee <leeym@FreeBSD.org>
2005-01-08 15:08:19 +0000

    - assign new MAINTAINER - update to ftrack-3.1
    PR:		75926
    Submitted by:	forsite@spamtest.ru

--- mail/ftrack/files/patch-hpp::constant.hpp
+++ mail/ftrack/files/patch-hpp::constant.hpp
@@ -1,11 +1,20 @@
---- hpp/constant.hpp.orig  Wed Sep 20 14:46:48 2000
-+++ hpp/constant.hpp   Wed Sep 20 14:47:01 2000
-@@ -30,7 +30,7 @@
- #define PATHDELIMS     "/"
- #define PATHDELIMC     '/'
- #endif
--#define FVersion       "3.01"OsType
-+#define FVersion       "3.01"
- #define RevisionHI     3
- #define RevisionLO     1
+--- hpp/constant.hpp.orig	Fri Jan  7 21:15:51 2005
++++ hpp/constant.hpp	Fri Jan  7 21:17:02 2005
+@@ -21,7 +21,7 @@
+ #   ifdef __LINUX__
+ #      define OsType      "/LNX"
+ #   elif defined(__FreeBSD__)
+-#      define OsType      "/FBSD"
++#      define OsType      "/BSD"
+ #   elif defined(sun)
+ #      define OsType      "/SUN"
+ #   elif defined(_AIX)
+@@ -38,7 +38,7 @@
  #define FTrackProductLo 0xff
+ #define FTrackProductHi 0x0c
+ #define Copyright      "(C) 1998-2002 Fyodor Ustinov (2:5020/79)"
+-#define DefaultConfig  "ftrack.cfg"
++#define DefaultConfig  "/usr/local/etc/ftrack/ftrack.cfg"
+ 
+ #ifdef UNIX
+ #define MsgExtension   ".msg"
