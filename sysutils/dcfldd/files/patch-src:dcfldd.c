7fae5250af1d01475944405356cdce850136334c
nobutaka <nobutaka@FreeBSD.org>
2005-01-09 18:14:24 +0000

    Add dcfldd, an enhanced dd tool with MD5 hashing capability.

---
+++ sysutils/dcfldd/files/patch-src:dcfldd.c
@@ -0,0 +1,11 @@
+--- src/dcfldd.c.orig	Mon Jan 10 00:36:19 2005
++++ src/dcfldd.c	Mon Jan 10 00:36:45 2005
+@@ -342,7 +342,7 @@
+   hashlog=FILE         send MD5 hash output to FILE instead of stderr\n\
+   status=[on|off]      display a continual status message on stderr\n\
+                          default state is \"on\"\n\
+-  sizeprobe=[if|of]    determine the size of the input or output file
++  sizeprobe=[if|of]    determine the size of the input or output file\n\
+                          for use with status messages. (this option\n\
+                          gives you a percentage indicator)\n\
+                          WARNING: Read the manual before using this\n\
