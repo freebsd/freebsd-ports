032a43b97300d18d1a7a2abd8474422d165af056
nobutaka <nobutaka@FreeBSD.org>
2005-01-09 12:01:24 +0000

    Fix compile error on amd64.

--- devel/boehm-gc/files/patch-include-private-gcconfig.h
+++ devel/boehm-gc/files/patch-include-private-gcconfig.h
@@ -1,5 +1,5 @@
---- include/private/gcconfig.h.orig	Wed Dec 29 22:30:21 2004
-+++ include/private/gcconfig.h	Wed Dec 29 22:30:39 2004
+--- include/private/gcconfig.h.orig	Sat Dec 18 09:20:16 2004
++++ include/private/gcconfig.h	Mon Jan  3 19:49:44 2005
 @@ -328,6 +328,10 @@
  #    define X86_64
  #    define mach_type_known
@@ -11,16 +11,19 @@
  # if defined(FREEBSD) && defined(__sparc__)
  #    define SPARC
  #    define mach_type_known
-@@ -1942,6 +1946,12 @@
+@@ -1939,6 +1943,15 @@
+ #	ifdef __ELF__
+ #	    define DYNAMIC_LOADING
+ #	endif
++#	define HEURISTIC2
++	extern char etext[];
++#	define SEARCH_FOR_DATA_START
++#   endif
++#   ifdef FREEBSD
++#       define OS_TYPE "FREEBSD"
++#       ifdef __ELF__
++#           define DYNAMIC_LOADING
++#       endif
  #	define HEURISTIC2
  	extern char etext[];
  #	define SEARCH_FOR_DATA_START
-+#   endif
-+#   ifdef FREEBSD
-+#     define OS_TYPE "FREEBSD"
-+#     ifdef __ELF__
-+#         define DYNAMIC_LOADING
-+#     endif
- #   endif
- # endif
- 
