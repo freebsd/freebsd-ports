6f4e8a61d220b5fc4f1e62b6d013d59b733d7149
edwin <edwin@FreeBSD.org>
2005-01-09 23:58:25 +0000

    update games/scummvm-tools to 0.7.0
    	sync with scummvm
    
    PR:		ports/75799
    Submitted by:	Miguel Mendez <flynn@energyhq.es.eu.org>

--- games/scummvm-tools/files/patch-simon1descr.c
+++ games/scummvm-tools/files/patch-simon1descr.c
@@ -1,5 +1,5 @@
---- simon1decr.c.orig	Tue May 27 16:34:14 2003
-+++ simon1decr.c	Tue May 27 16:34:34 2003
+--- simon1decr.c.orig	Mon Apr 12 00:10:19 2004
++++ simon1decr.c	Tue Jan  4 15:36:39 2005
 @@ -26,14 +26,9 @@
  
  #define EndGetM32(a)  ((((a)[0])<<24)|(((a)[1])<<16)|(((a)[2])<<8)|((a)[3]))
@@ -8,7 +8,7 @@
 -  if (!bits--) { s -= 4; if (s < src) return 0; bb=EndGetM32(s); bits=31; } \
 -  (var) = bb & 1; bb >>= 1; \
 -} while (0)
-+#define SD_GETBIT(var) do { if (!bits--) { s -= 4; if (s < src) return 0; bb=EndGetM32(s); bits=31; }  (var) = bb & 1; bb >>= 1; } while (0)
++#define SD_GETBIT(var) do { if (!bits--) { s -= 4; if (s < src) return 0; bb=EndGetM32(s); bits=31; } (var) = bb & 1; bb >>= 1; } while (0)
  
 -#define SD_GETBITS(var, nbits) do { \
 -  bc=(nbits); (var)=0; while (bc--) {(var)<<=1; SD_GETBIT(bit); (var)|=bit; } \
