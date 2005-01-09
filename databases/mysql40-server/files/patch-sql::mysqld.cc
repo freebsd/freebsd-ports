1e5de7912b8e81a783fe3cb82c240ec3877d9191
Alex Dupre <ale@FreeBSD.org>
2005-01-09 13:20:47 +0000

    Update to 4.0.23a release [1]: - Use MASTER_SITES_MYSQL - Add WITHOUT_INNODB knob [2] - Fix tcp wrappers (hopefully definitely!)
    PR:		ports/75598 [1]
    Submitted by:	Marcus Grando <marcus@corp.grupos.com.br> [1],
    		sobomax [2]

--- databases/mysql40-server/files/patch-sql::mysqld.cc
+++ databases/mysql40-server/files/patch-sql::mysqld.cc
@@ -1,6 +1,15 @@
---- sql/mysqld.cc.orig	Tue Sep  7 00:29:37 2004
-+++ sql/mysqld.cc	Mon Sep 20 08:37:21 2004
-@@ -107,15 +107,7 @@
+--- sql/mysqld.cc.orig	Sat Dec 18 18:25:05 2004
++++ sql/mysqld.cc	Sun Jan  9 12:43:21 2005
+@@ -99,7 +99,7 @@
+ #endif /* __WIN__ */
+ 
+ #ifdef HAVE_LIBWRAP
+-#include <tcpd.h>
++#include "mytcpd.h"
+ #include <syslog.h>
+ #ifdef NEED_SYS_SYSLOG_H
+ #include <sys/syslog.h>
+@@ -107,15 +107,6 @@
  int allow_severity = LOG_INFO;
  int deny_severity = LOG_WARNING;
  
@@ -13,7 +22,26 @@
 -#define my_hosts_access(A) hosts_access()
 -#define my_eval_client(A)  eval_client()
 -#endif
-+#include "mylibwrap.h"
  #endif /* HAVE_LIBWRAP */
  
  #ifdef HAVE_SYS_MMAN_H
+@@ -3240,8 +3231,8 @@
+ 	struct request_info req;
+ 	signal(SIGCHLD, SIG_DFL);
+ 	request_init(&req, RQ_DAEMON, libwrapName, RQ_FILE, new_sock, NULL);
+-	my_fromhost(&req);
+-	if (!my_hosts_access(&req))
++	fromhost(&req);
++	if (!hosts_access(&req))
+ 	{
+ 	  /*
+ 	    This may be stupid but refuse() includes an exit(0)
+@@ -3249,7 +3240,7 @@
+ 	    clean_exit() - same stupid thing ...
+ 	  */
+ 	  syslog(deny_severity, "refused connect from %s",
+-		 my_eval_client(&req));
++		 eval_client(&req));
+ 
+ 	  /*
+ 	    C++ sucks (the gibberish in front just translates the supplied
