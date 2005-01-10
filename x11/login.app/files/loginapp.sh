46d68af243921ac116ede3f273f894d38d0a0b1b
Edwin Groothuis <edwin@FreeBSD.org>
2005-01-10 10:51:29 +0000

    x11/login.app : update and take maintainership
    	This updates x11/login.app from 2.0.0.a7_1 to 2.1.1 and
    	transmits maintainership from ports@freebsd.org to me.
    
    PR:		ports/76010
    Submitted by:   Tobias Roth <ports@fsck.ch>

---
+++ x11/login.app/files/loginapp.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+# PROVIDE: login_app
+# REQUIRE: LOGIN
+
+# Add the following line to /etc/rc.conf to enable login.app:
+#
+# login_app_enable="YES"
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE
+
+login_app_enable=${login_app_enable-"NO"}
+
+. /etc/rc.subr
+
+name="login_app"
+rcvar=`set_rcvar`
+command="%%X11BASE%%/GNUstep/Apps/Login.app/Login"
+
+load_rc_config $name
+run_rc_command "$1"
