882df49ad90473155e9ab9fa311909976ef07979
Edwin Groothuis <edwin@FreeBSD.org>
2005-01-12 22:13:43 +0000

    INTAINER] Update port: databases/py-MySQLdb to 1.0.1
    	Update port: databases/py-MySQLdb to 1.0.1
    
    	- Change to 1.0.1
    	- Rediff patch-_mysql.c
    	- Install more docs
    
    PR:		ports/75866
    Submitted by:	Marcus Grando <marcus@corp.grupos.com.br>

--- databases/py-MySQLdb/files/patch-_mysql.c
+++ databases/py-MySQLdb/files/patch-_mysql.c
@@ -1,18 +1,11 @@
-Index: MySQLdb/_mysql.c
-===================================================================
-RCS file: /cvsroot/mysql-python/MySQLdb/_mysql.c,v
-retrieving revision 1.49
-diff -u -r1.49 _mysql.c
---- _mysql.c	7 Jun 2004 03:44:38 -0000	1.49
-+++ _mysql.c	26 Jul 2004 16:58:29 -0000
-@@ -1542,7 +1542,13 @@
+--- _mysql.c.orig	Sat Jan  1 15:40:13 2005
++++ _mysql.c	Wed Jan  5 18:35:18 2005
+@@ -1542,7 +1542,11 @@
  	if (!PyArg_ParseTuple(args, "")) return NULL;
  	check_connection(self);
  	Py_BEGIN_ALLOW_THREADS
-+
-+#if MYSQL_VERSION_ID >= 40103 && MYSQL_VERSION_ID < 50000
-+   r = mysql_shutdown(&(self->connection),
-+                      SHUTDOWN_DEFAULT);
++#if MYSQL_VERSION_ID >= 40103
++	r = mysql_shutdown(&(self->connection), SHUTDOWN_DEFAULT);
 +#else
  	r = mysql_shutdown(&(self->connection));
 +#endif
