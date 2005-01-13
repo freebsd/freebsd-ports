8148ffac3e75a40f89bd70ab52b54d7b1fbfd602
hq <hq@FreeBSD.org>
2005-01-13 01:55:13 +0000

    - Update to 3.2.6 - Update to bsd.java.mk 2.0 - rcNGify startup - Fix table creation for autoincrement feature in Postgresql - Add new "-config" option to jboss3ctl for JBoss3 startup config - Dynamic plist - Many other improvements (similar to recent changes in java/jboss4)
    PR:		76125
    Submitted by:	maintainer

--- java/jboss3/files/startup.sh
+++ java/jboss3/files/startup.sh
@@ -1,35 +1,58 @@
 #!/bin/sh
-# -*- mode: Fundamental; tab-width: 4; -*-
-# ex:ts=4
 #
 # %%APP_TITLE%% startup script.
 #
-# $FreeBSD: /tmp/pcvs/ports/java/jboss3/files/Attic/startup.sh,v 1.1 2004-06-21 16:42:59 vanilla Exp $
+# $FreeBSD: /tmp/pcvs/ports/java/jboss3/files/Attic/startup.sh,v 1.2 2005-01-13 01:55:13 hq Exp $
 #
 
+# PROVIDE: %%APP_SHORTNAME%%
+# REQUIRE: NETWORKING SERVERS
 
-# Set some variables
-MYSELF=`basename $0`
-JAVA_OPTS="%%JAVA_OPTS%%"
+# Add the following lines to /etc/rc.conf to enable %%APP_SHORTNAME%%:
+# %%APP_SHORTNAME%%_enable (bool):		Set to "NO" by default.
+#				Set it to "YES" to enable %%APP_SHORTNAME%%
+# %%APP_SHORTNAME%%_flags (str):		Set to "-server" by default.
+#				Extra JVM flags.
+# %%APP_SHORTNAME%%_config (str):		Set to "" by default
+#				JBoss server config, eg {all|default|minimal|standard}
+#
+. %%RC_SUBR%%
 
-case "$1" in
-	start)
-		echo -n ' '
-		truncate -s 0 %%PID_FILE%%
-		chown %%USER%%:%%GROUP%% %%PID_FILE%%
-		chmod 600 %%PID_FILE%%
-		su -f -m %%USER%% -c "exec %%CONTROL_SCRIPT%% ${JAVA_OPTS} start" >/dev/null && echo -n '%%APP_SHORTNAME%%'
-		;;
-	stop)
-		echo -n ' '
-		chown %%USER%%:%%GROUP%% %%PID_FILE%%
-		chmod 600 %%PID_FILE%%
-		su -f -m %%USER%% -c "exec %%CONTROL_SCRIPT%% stop" >/dev/null 2>&1 ; echo -n '%%APP_SHORTNAME%%'
-		;;
-	*)
-		echo ""
-		echo "Usage: ${MYSELF} { start | stop }"
-		echo ""
-		exit 64
-		;;
-esac
+name="%%APP_SHORTNAME%%"
+rcvar=`set_rcvar`
+
+start_cmd="%%APP_SHORTNAME%%_start"
+restart_cmd="%%APP_SHORTNAME%%_restart"
+pidfile="%%PID_FILE%%"
+procname="%%JAVA%%"
+
+[ -z "$%%APP_SHORTNAME%%_enable" ]	&& %%APP_SHORTNAME%%_enable="NO"
+[ -z "$%%APP_SHORTNAME%%_flags" ]	&& %%APP_SHORTNAME%%_flags="-server"
+[ -z "$%%APP_SHORTNAME%%_config" ]	&& %%APP_SHORTNAME%%_config=""
+
+%%APP_SHORTNAME%%_start ()
+{
+	checkyesno %%APP_SHORTNAME%%_enable &&
+	{
+		if [ ! -f ${pidfile} ]
+		then
+			touch ${pidfile}
+			chown %%USER%%:%%GROUP%% ${pidfile}
+		fi
+
+		%%CONTROL_SCRIPT%% -q ${%%APP_SHORTNAME%%_flags} ${%%APP_SHORTNAME%%_configflag} ${%%APP_SHORTNAME%%_config} start &&
+		echo -n " %%APP_SHORTNAME%%"
+	}
+}
+
+%%APP_SHORTNAME%%_restart ()
+{
+	checkyesno %%APP_SHORTNAME%%_enable &&
+		%%CONTROL_SCRIPT%% -q ${%%APP_SHORTNAME%%_flags} ${%%APP_SHORTNAME%%_configflag} ${%%APP_SHORTNAME%%_config} restart
+}
+
+load_rc_config $name
+
+[ -n "${%%APP_SHORTNAME%%_config}" ] && %%APP_SHORTNAME%%_configflag="-config"
+
+run_rc_command "$1"
