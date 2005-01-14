9f7c072fd9186df5b8b696a5dcb1fc298b606dd0
adamw <adamw@FreeBSD.org>
2005-01-14 19:25:56 +0000

    Extra super OMFG-I'd-be-screwed-without-you thanks to marcus for the sleuthing here.
    - Stop libgnomesu from causing 4.x machines to catch fire and break
      all your dishes.
    
    - Don't install stuff into /etc/pam.d by default.
    
    - Install example "gnomesu-pam"s, and include a pkg-message explaining
      what to do with them (HINT: copy the appropriate one into /etc/pam.d
      and name it gnomesu-pam).
    
    - Fix a double-free.
    
    With these fixes, the gnomeu binary in libgnomesu provides a much more
    comprehensive GNOME su frontend, and the Great Red Eye of Adamwdor is
    now turned towards sysutils/gnomesu.

--- security/libgnomesu/files/patch-pam-backend_pam.c
+++ security/libgnomesu/files/patch-pam-backend_pam.c
@@ -1,5 +1,5 @@
---- pam-backend/pam.c.orig	Thu Jan 13 23:43:53 2005
-+++ pam-backend/pam.c	Thu Jan 13 23:43:59 2005
+--- pam-backend/pam.c.orig	Wed Jan 12 07:16:27 2005
++++ pam-backend/pam.c	Fri Jan 14 14:14:42 2005
 @@ -17,7 +17,7 @@
   */
  
@@ -9,3 +9,11 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <sys/types.h>
+@@ -276,7 +276,6 @@
+ 		#ifdef HAVE_SETFSUID
+ 		setfsuid (pw->pw_uid);
+ 		#endif /* HAVE_SETFSUID */
+-		change_identity (pw);
+ 		initgroups (pw->pw_name, pw->pw_gid);
+ 		setgid (pw->pw_gid);
+ 		setuid (pw->pw_uid);
