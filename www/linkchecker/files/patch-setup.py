7723b852ff8d4599069f2247a36a05c707948a92
Clement Laforet <clement@FreeBSD.org>
2005-01-15 15:37:19 +0000

    - Update to 2.1

--- www/linkchecker/files/patch-setup.py
+++ www/linkchecker/files/patch-setup.py
@@ -1,18 +1,24 @@
---- setup.py.orig	Mon Sep 20 11:05:17 2004
-+++ setup.py	Wed Oct 13 09:54:32 2004
-@@ -234,13 +234,13 @@
+--- setup.py.orig	Tue Jan 11 13:04:34 2005
++++ setup.py	Sat Jan 15 16:24:38 2005
+@@ -242,7 +242,7 @@
               ['share/locale/nl/LC_MESSAGES/linkchecker.mo']),
-          ('share/linkchecker', ['config/linkcheckerrc',
-                                 'config/logging.conf', ]),
+          ('share/linkchecker',
+              ['config/linkcheckerrc', 'config/logging.conf', ]),
 -         ('share/linkchecker/examples',
 +         ('share/examples/linkchecker',
               ['cgi/lconline/leer.html.en', 'cgi/lconline/leer.html.de',
                'cgi/lconline/index.html', 'cgi/lconline/lc_cgi.html.en',
                'cgi/lconline/lc_cgi.html.de', 'cgi/lconline/check.js',
-               'cgi/lc.cgi', 'cgi/lc.fcgi',
-               'config/linkchecker-completion', 'config/linkcheck-cron.sh']),
--         ('share/man/man1', ['linkchecker.1']),
-+         ('man/man1', ['linkchecker.1']),
-       ],
-       classifiers = [
-         'Topic :: Internet :: WWW/HTTP :: Site Management :: Link Checking',
+@@ -250,10 +250,8 @@
+       ]
+ 
+ if os.name == 'posix':
+-    data_files.append(('share/man/man1', ['doc/en/linkchecker.1']))
+-    data_files.append(('share/man/de/man1', ['doc/de/linkchecker.1']))
+-    data_files.append(('share/man/fr/man1', ['doc/fr/linkchecker.1']))
+-    data_files.append(('share/linkchecker/examples',
++    data_files.append(('man/man1', ['doc/en/linkchecker.1']))
++    data_files.append(('share/examples/linkchecker',
+               ['config/linkchecker-completion', 'config/linkcheck-cron.sh']))
+ elif os.name == 'nt':
+     data_files.append(('share/linkchecker/doc',
