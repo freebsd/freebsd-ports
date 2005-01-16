f2d3e07ebc5fc0ae5fb9ac9d7902e52b32119dff
adamw <adamw@FreeBSD.org>
2005-01-16 23:47:38 +0000

    Update to 1.8a6. They merged a bunch of FreeBSD changes. Neat.

--- www/mozilla-devel/files/patch-nsprpub-pr-include-md-_pth.h
+++ www/mozilla-devel/files/patch-nsprpub-pr-include-md-_pth.h
@@ -1,11 +1,11 @@
---- nsprpub/pr/include/md/_pth.h.orig	Mon Jul 26 07:23:42 2004
-+++ nsprpub/pr/include/md/_pth.h	Mon Jul 26 07:18:37 2004
-@@ -90,7 +90,7 @@
- #define _PT_PTHREAD_MUTEXATTR_INIT        pthread_mutexattr_init
- #define _PT_PTHREAD_MUTEXATTR_DESTROY     pthread_mutexattr_destroy
- #define _PT_PTHREAD_MUTEX_INIT(m, a)      pthread_mutex_init(&(m), &(a))
+--- nsprpub/pr/include/md/_pth.h.orig	Sun Jan 16 16:09:10 2005
++++ nsprpub/pr/include/md/_pth.h	Sun Jan 16 16:10:06 2005
+@@ -84,7 +84,7 @@
+ #define _PT_PTHREAD_MUTEXATTR_DESTROY(x)  /* */
+ #define _PT_PTHREAD_MUTEX_INIT(m, a)      (memset(&(m), 0, sizeof(m)), \
+                                       pthread_mutex_init(&(m), NULL))
 -#define _PT_PTHREAD_MUTEX_IS_LOCKED(m)    (EBUSY == pthread_mutex_trylock(&(m)))
 +#define _PT_PTHREAD_MUTEX_IS_LOCKED(m)    (0 != pthread_mutex_trylock(&(m)))
- #if defined(DARWIN)
  #define _PT_PTHREAD_CONDATTR_INIT(x)      0
- #else
+ #define _PT_PTHREAD_CONDATTR_DESTROY(x)   /* */
+ #define _PT_PTHREAD_COND_INIT(m, a)       (memset(&(m), 0, sizeof(m)), \
