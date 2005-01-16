44ae66555ccbcc517c31a44ed2d2591953d86f25
Akinori MUSHA <knu@FreeBSD.org>
2005-01-16 18:41:01 +0000

    Update to 0.9.0.

--- databases/rubygem-sqlite3-ruby/files/patch-api.rb
+++ databases/rubygem-sqlite3-ruby/files/patch-api.rb
@@ -1,9 +1,9 @@
---- lib/sqlite3/driver/dl/api.rb.orig	Wed Dec 22 13:45:15 2004
-+++ lib/sqlite3/driver/dl/api.rb	Sun Dec 26 06:03:27 2004
-@@ -38,7 +38,7 @@
-     extend ::DL::Importable
- 
+--- lib/sqlite3/driver/dl/api.rb.orig	Thu Dec 23 19:37:37 2004
++++ lib/sqlite3/driver/dl/api.rb	Mon Jan 17 03:30:10 2005
+@@ -40,7 +40,7 @@
      library_name = case RUBY_PLATFORM.downcase
+       when /darwin/
+         "libsqlite3.dylib"
 -      when /linux/
 +      when /linux/, /freebsd|netbsd|openbsd|dragonfly/
          "libsqlite3.so"
