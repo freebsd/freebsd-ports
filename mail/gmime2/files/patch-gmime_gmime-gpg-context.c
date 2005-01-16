241bcee99b91095a16211af61cd0bea25ab6e0dd
Kirill Ponomarev <krion@FreeBSD.org>
2005-01-16 16:21:55 +0000

    Update to 2.1.10

---
+++ mail/gmime2/files/patch-gmime_gmime-gpg-context.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- gmime/gmime-gpg-context.c.orig
++++ gmime/gmime-gpg-context.c
+@@ -25,8 +25,6 @@
+ #include <config.h>
+ #endif
+ 
+-#define _POSIX_SOURCE
+-
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
