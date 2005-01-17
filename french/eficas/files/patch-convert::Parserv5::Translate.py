6fb9d48695d2246a1f5aa5692e1e8200e8d0877d
thierry <thierry@FreeBSD.org>
2005-01-17 21:09:10 +0000

    Upgrade to 1.7.0.

---
+++ french/eficas/files/patch-convert::Parserv5::Translate.py
@@ -0,0 +1,10 @@
+--- convert/Parserv5/Translate.py.orig	Fri Dec 10 14:07:22 2004
++++ convert/Parserv5/Translate.py	Mon Jan 10 23:18:03 2005
+@@ -20,7 +20,6 @@
+ # ======================================================================
+ 
+ #!/bin/env python -d
+-#!/tools/net/app/Python-1.5.2/bin/python1.5
+ 
+ """Translate - a first attempt at parsing my little language
+ 
