1177ff25dc070023cfccc518982dc9cdddb95b2f
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-01-18 06:17:48 +0000

    Update to 1.4.2.

---
+++ math/gnumeric/files/patch-src_tools_solver_lp_solve_commonlib.c
@@ -0,0 +1,8 @@
+--- src/tools/solver/lp_solve/commonlib.c.orig	Tue Jan 18 00:16:36 2005
++++ src/tools/solver/lp_solve/commonlib.c	Tue Jan 18 00:19:24 2005
+@@ -1,4 +1,5 @@
+ 
++#include <gnumeric-config.h>
+ #include <sys/types.h>
+ 
+ #ifdef INTEGERTIME
