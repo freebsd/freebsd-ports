c46261ec96b0f69f61f413722662ad6826609a62
clement <clement@FreeBSD.org>
2005-01-18 22:04:02 +0000

    - Update to 1.1

--- dns/adns/files/patch-src-parse.c
+++ dns/adns/files/patch-src-parse.c
@@ -1,11 +1,11 @@
---- src/parse.c.orig	Wed Aug 11 18:19:45 2004
-+++ src/parse.c	Wed Aug 11 18:20:19 2004
-@@ -111,7 +111,7 @@
+--- src/parse.c.orig	Tue Jan 18 22:48:32 2005
++++ src/parse.c	Tue Jan 18 22:48:56 2005
+@@ -113,7 +113,7 @@
  }
  
  adns_status adns__parse_domain(adns_state ads, int serv, adns_query qu,
 -			       vbuf *vb, adns_queryflags flags,
 +			       vbuf *vb, parsedomain_flags flags,
- 			       const byte *dgram, int dglen, int *cbyte_io, int max) {
+ 			       const byte *dgram, int dglen, int *cbyte_io,
+ 			       int max) {
    findlabel_state fls;
-   
