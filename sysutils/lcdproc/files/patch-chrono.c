e5199888109866127f7e76a1ad281c9503ca392b
pav <pav@FreeBSD.org>
2005-01-19 19:35:14 +0000

    - Update to 0.4.5
    PR:		ports/76433
    Submitted by:	Lewis Thompson <purple@fajita.org>

--- sysutils/lcdproc/files/patch-chrono.c
+++ sysutils/lcdproc/files/patch-chrono.c
@@ -1,5 +1,5 @@
---- clients/lcdproc/chrono.c.orig	Sat Dec 29 06:52:39 2001
-+++ clients/lcdproc/chrono.c	Tue May  6 05:07:49 2003
+--- clients/lcdproc/chrono.c.orig	Tue Jan 18 19:57:44 2005
++++ clients/lcdproc/chrono.c	Tue Jan 18 19:59:30 2005
 @@ -62,6 +62,8 @@
  #include <sys/dkstat.h>
  #endif
