0676490164c84c128350c029c970910a46738666
edwin <edwin@FreeBSD.org>
2005-01-19 12:04:29 +0000

    update port: games/bzflag to version 2.0.0
    	Update games/bzflag port to version 2.0.0.  It might be
    	useful to have some people test v2 long enough to confirm
    	that people prefer it to v1.x.  If the users prefer, it
    	might be better to create a new port games/bzflag2, but I
    	will wait for feedback which indicates this.
    
    PR:		ports/76454
    Submitted by:	Charles Swiger <chuck@pkix.net>

--- games/bzflag/files/patch-src::bzadmin::StdBothUI.cxx
+++ games/bzflag/files/patch-src::bzadmin::StdBothUI.cxx
@@ -1,5 +1,5 @@
---- src/bzadmin/StdBothUI.cxx.orig	Tue Dec  9 16:38:14 2003
-+++ src/bzadmin/StdBothUI.cxx	Thu Dec 11 01:25:29 2003
+--- src/bzadmin/StdBothUI.cxx.orig	Tue Jan 18 18:28:44 2005
++++ src/bzadmin/StdBothUI.cxx	Wed Jan 19 00:05:11 2005
 @@ -20,6 +20,7 @@
  #include <ws2tcpip.h>
  #include <wincon.h>
