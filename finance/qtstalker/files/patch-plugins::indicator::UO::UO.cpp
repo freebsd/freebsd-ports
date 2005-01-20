04bf85f65bf6f9e81c7d265c7ca3edf22e9cb04a
lioux <lioux@FreeBSD.org>
2005-01-20 03:21:32 +0000

    New port qtstalker version 0.28: Commodity and stock market charting and technical analysis

---
+++ finance/qtstalker/files/patch-plugins::indicator::UO::UO.cpp
@@ -0,0 +1,10 @@
+--- plugins/indicator/UO/UO.cpp.orig	Wed Jan 19 03:40:55 2005
++++ plugins/indicator/UO/UO.cpp	Wed Jan 19 03:41:05 2005
+@@ -21,6 +21,7 @@
+ 
+ #include "UO.h"
+ #include "PrefDialog.h"
++#include <math.h>
+ #include <qdict.h>
+ #include <qobject.h>
+ 
