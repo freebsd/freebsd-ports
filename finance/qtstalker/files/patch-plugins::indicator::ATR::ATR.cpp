f921c98ebe8ae2d6cffcf610cc6de782676f7634
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2005-01-20 03:21:32 +0000

    New port qtstalker version 0.28: Commodity and stock market charting and technical analysis

---
+++ finance/qtstalker/files/patch-plugins::indicator::ATR::ATR.cpp
@@ -0,0 +1,10 @@
+--- plugins/indicator/ATR/ATR.cpp.orig	Wed Jan 19 03:26:19 2005
++++ plugins/indicator/ATR/ATR.cpp	Wed Jan 19 03:27:15 2005
+@@ -21,6 +21,7 @@
+ 
+ #include "ATR.h"
+ #include "PrefDialog.h"
++#include <math.h>
+ #include <qdict.h>
+ #include <qobject.h>
+ 
---
+++ finance/qtstalker/files/patch-plugins::indicator::BB::BB.cpp
@@ -0,0 +1,10 @@
+--- plugins/indicator/BB/BB.cpp.orig	Wed Jan 19 03:28:14 2005
++++ plugins/indicator/BB/BB.cpp	Wed Jan 19 03:28:24 2005
+@@ -21,6 +21,7 @@
+ 
+ #include "BB.h"
+ #include "PrefDialog.h"
++#include <math.h>
+ #include <qdict.h>
+ #include <qobject.h>
+ 
