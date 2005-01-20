e2b9995a76433f56bd82b43a589779229b32768a
Maho Nakata <maho@FreeBSD.org>
2005-01-20 04:44:07 +0000

    Update to 1.2
    PR:		75503
    Submitted by:	"Pedro F. Giffuni" <giffunip@yahoo.com>

--- cad/calculix/files/patch-cgx_VER+src+cgx.h
+++ cad/calculix/files/patch-cgx_VER+src+cgx.h
@@ -1,13 +1,13 @@
---- cgx_1.1/src/cgx.h.orig	Tue Aug  5 10:08:37 2003
-+++ cgx_1.1/src/cgx.h	Thu Aug  7 00:37:38 2003
+--- cgx_1.2/src/cgx.h.orig	Sat Jul 17 17:17:41 2004
++++ cgx_1.2/src/cgx.h	Thu Dec 23 15:32:58 2004
 @@ -59,8 +59,8 @@
  #define     MILLISECONDS_PER_PERIOD 1200 
  
  /* default location of the help files, please insert the actual one */
--#define     HELPFILE        {"/usr/local/CalculiX/cgx_1.1/doc/cgx/cgx.html",\
--   "/usr/local/CalculiX/ccx_1.1/doc/ccx/ccx.html"}
+-#define     HELPFILE        {"/usr/local/CalculiX/cgx_1.2/doc/cgx/cgx.html",\
+-    "/usr/local/CalculiX/ccx_1.2/doc/ccx/ccx.html"}
 +#define     HELPFILE        {"%%DOCSDIR%%/cgx/cgx.html",\
-+   "%%DOCSDIR%%/ccx/ccx.html"}
++    "%%DOCSDIR%%/ccx/ccx.html"}
  
  /* html browser, change if necessary */
- #define     BROWSER         {"netscape"}
+ /* #define     BROWSER         {"netscape"} */
