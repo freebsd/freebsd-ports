5ffba37b9f2da699615b931329af3bdc6c311b4e
maho <maho@FreeBSD.org>
2005-01-20 04:45:45 +0000

    forgot to add some files...

---
+++ cad/calculix/files/patch-cgx_VER+src+uselibSNL.cpp
@@ -0,0 +1,10 @@
+--- cgx_1.2/src/uselibSNL.cpp.orig	Sat Dec 25 01:43:08 2004
++++ cgx_1.2/src/uselibSNL.cpp	Sat Dec 25 01:47:02 2004
+@@ -1,6 +1,6 @@
+ #include <extUtil.h>
+ #include <snlCtrlPoint.h>
+-#include <iostream.h>
++#include <iostream>
+ 
+ #include "uselibSNL.h"
+ #include "snlKnotVector.h"
