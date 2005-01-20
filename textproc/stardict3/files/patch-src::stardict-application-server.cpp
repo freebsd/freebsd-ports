016c3f8b5dee4cec75be6b52572a5416a39c5372
pav <pav@FreeBSD.org>
2005-01-20 19:20:46 +0000

    - Update to 2.4.4
    PR:		ports/76367
    Submitted by:	Xin LI <delphij@freebsd.org> (maintainer)

---
+++ textproc/stardict3/files/patch-src::stardict-application-server.cpp
@@ -0,0 +1,8 @@
+--- src/stardict-application-server.cpp.orig	Tue Sep 23 18:19:48 2003
++++ src/stardict-application-server.cpp	Tue Jan 18 02:25:54 2005
+@@ -117,4 +117,4 @@
+         StardictApplicationServer,                    
+         GNOME_Stardict_Application, 
+         BONOBO_TYPE_OBJECT,           
+-        stardict_application_server);
++        stardict_application_server)
