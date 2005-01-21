525ac9acd4d8f412e71cbfb7fd26d6cdb7a4a519
krion <krion@FreeBSD.org>
2005-01-21 22:45:35 +0000

    Fix build on 4.x

--- irc/undernet-ircu/files/patch-ircd_ircd__log.c
+++ irc/undernet-ircu/files/patch-ircd_ircd__log.c
@@ -3,12 +3,23 @@ $FreeBSD$
 
 --- ircd/ircd_log.c.orig
 +++ ircd/ircd_log.c
-@@ -395,7 +395,7 @@
+@@ -49,6 +49,7 @@
+ #include <syslog.h>
+ #include <time.h>
+ #include <unistd.h>
++#include <osreldate.h>
+ 
+ #define LOG_BUFSIZE 2048 
+ 
+@@ -395,7 +396,11 @@
  
    /* Build the basic log string */
    vd.vd_format = fmt;
--  vd.vd_args = vl;
++  #if __FreeBSD_version > 500000 && defined(__amd64__)
 +  va_copy(vd.vd_args, vl);
++  #else
+   vd.vd_args = vl;
++  #endif
  
    /* save the length for writev */
    /* Log format: "SYSTEM [SEVERITY]: log message" */
