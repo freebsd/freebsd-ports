0d252eea07c8a02b08a98c9320479a9404662a94
Kirill Ponomarev <krion@FreeBSD.org>
2005-01-21 22:45:35 +0000

    Fix build on 4.x

--- irc/undernet-ircu/files/patch-ircd_send.c
+++ irc/undernet-ircu/files/patch-ircd_send.c
@@ -3,12 +3,24 @@ $FreeBSD$
 
 --- ircd/send.c.orig
 +++ ircd/send.c
-@@ -697,7 +697,7 @@
+@@ -44,7 +44,7 @@
+ #include <assert.h>
+ #include <stdio.h>
+ #include <string.h>
+-
++#include <osreldate.h>
+ 
+ static int sentalong[MAXCONNECTIONS];
+ static int sentalong_marker;
+@@ -697,7 +697,11 @@
     * this is ok...
     */
    vd.vd_format = pattern;
--  vd.vd_args = vl;
++  #if __FreeBSD_version > 500000 && defined(__amd64__)
 +  va_copy(vd.vd_args, vl);
++  #else
+   vd.vd_args = vl;
++  #endif
    mb = msgq_make(0, ":%s " MSG_NOTICE " * :*** Notice -- %v", cli_name(&me),
  		 &vd);
  
