342bff0997a780a1124f6421d6871fb4f0a36cfa
Pav Lucistnik <pav@FreeBSD.org>
2005-01-24 16:44:20 +0000

    - Update to 2.18
    PR:		ports/76531
    Submitted by:	"Choe, Cheng-Dae" <whitekid@gmail.com>

--- devel/bugzilla/files/patch-checksetup.pl
+++ devel/bugzilla/files/patch-checksetup.pl
@@ -1,11 +1,11 @@
---- checksetup.pl~	Fri Apr 16 02:48:51 2004
-+++ checksetup.pl	Thu Jul 15 23:37:09 2004
-@@ -399,7 +399,7 @@
- # and you cannot set this up any other way. YOU HAVE BEEN WARNED.
- # If you set this to anything besides "", you will need to run checksetup.pl
- # as root, or as a user who is a member of the specified group.
--$webservergroup = "apache";
-+$webservergroup = "nogroup";
- ');
+--- checksetup.pl.orig	Fri Jan 14 10:51:36 2005
++++ checksetup.pl	Mon Jan 24 17:39:29 2005
+@@ -616,7 +616,7 @@
  
- 
+ my $webservergroup_default;
+ if ($^O !~ /MSWin32/i) {
+-    $webservergroup_default = 'apache';
++    $webservergroup_default = 'nogroup';
+ } else {
+     $webservergroup_default = '';
+ }
