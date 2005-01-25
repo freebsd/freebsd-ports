c9c1429103f327fd2341a73f4483bbe13b5d34cf
Jacques Vidrine <nectar@FreeBSD.org>
2005-01-25 13:55:06 +0000

    The set-user-ID binary zhcon normally reads it's user-specified configuration file as root.  Drop privileges before opening the file to prevent a local user from reading arbitrary files.
    Reported by:	Erik SjÃ¶lund
    Obtained from:	Debian

---
+++ chinese/zhcon/files/patch-src::configfile.cpp
@@ -0,0 +1,21 @@
+--- src/configfile.cpp.orig	Tue Jan 25 07:38:59 2005
++++ src/configfile.cpp	Tue Jan 25 07:41:19 2005
+@@ -19,13 +19,18 @@
+ #include <stdexcept>
+ #include <fstream>
+ #include <cstdlib>
++#include <sys/types.h>
++#include <unistd.h>
+ #include "configfile.h"
+ 
+ ConfigFile::ConfigFile(const char *fn) {
++    uid_t euid = geteuid();
++    setuid(getuid());
+     ifstream in(fn);
+     if (!in)
+         throw runtime_error("Could not open config file!");
+     ParseFile(in);
++    setuid(euid);
+ }
+ 
+ ConfigFile::~ConfigFile() {}
