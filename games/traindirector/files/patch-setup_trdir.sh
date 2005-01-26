6d9f7b430bc8a1451c06059d5addafd32ccb3aea
Kevin Lo <kevlo@FreeBSD.org>
2005-01-26 04:22:06 +0000

    Update to 1.19c
    PR: ports/76662
    Submitted by: MAINTAINER

--- games/traindirector/files/patch-setup_trdir.sh
+++ games/traindirector/files/patch-setup_trdir.sh
@@ -1,5 +1,5 @@
---- setup_trdir.sh.orig	Wed Feb 18 15:03:49 2004
-+++ setup_trdir.sh	Wed Feb 18 15:10:49 2004
+--- setup_trdir.sh.orig	Tue Jan 25 13:44:55 2005
++++ setup_trdir.sh	Tue Jan 25 13:45:30 2005
 @@ -6,9 +6,9 @@
  # edit the folowing line
  #
@@ -21,7 +21,7 @@
  ICONDIR="$TDHOMEDIR/tdicons"
  LOCALEDIR="$TDHOMEDIR/locales"
  if ! test -d $TDHOMEDIR  ; then
-@@ -37,7 +37,7 @@
+@@ -38,7 +38,7 @@
  
  case $CHOIX in
      "2") FLNAME=".traindir.it"
@@ -30,21 +30,26 @@
           STRING='locale .it';;
  	 	 
      "3") FLNAME=".traindir.fr"
-@@ -54,9 +54,7 @@
-     cp -f $SAMPLE/traindir/locales/$FLNAME $HOME/.traindir/
+@@ -56,12 +56,10 @@
+ if ! test -d $HOME/.traindir ; then
+     mkdir $HOME/.traindir
  fi
- 
+-    echo $STRING > $HOME/.traindir/.traindir.ini
+-    cp -f $SAMPLE/traindir/locales/$FLNAME $HOME/.traindir/
+-
 -cp -f $SAMPLE/traindir/* $TDHOMEDIR
 -cp -f $SAMPLE/traindir/tdicons/*.*  $ICONDIR 
--
++echo $STRING > $HOME/.traindir/.traindir.ini
++cp -f -R $SAMPLE/traindir/locales/$FLNAME $HOME/.traindir/
+ 
 +cp -f -R $SAMPLE/traindir/* $TDHOMEDIR
  
  echo
  echo   "Creating startup script......"
-@@ -64,11 +62,13 @@
+@@ -69,12 +67,13 @@
  echo   "#!/bin/sh" > $HOME/traindir.sh
  echo   "# Startup file for traindirector" >> $HOME/traindir.sh
- echo   "#" >> $HOME/sstart-traindir
+ echo   "#" >> $HOME/traindir.sh
 -echo   "TDHOME=\"$HOME/traindir\"" >> $HOME/traindir.sh
 +echo   "TDHOME=\"$HOME/.traindir\"" >> $HOME/traindir.sh
  echo   "export TDHOME" >> $HOME/traindir.sh
@@ -54,7 +59,7 @@
  chmod 0775 $HOME/traindir.sh
  echo "Installation complete"
 -echo "You can run ~/traindir.sh from a X console"
-\ No newline at end of file
+-
 +echo "a script file 'traindir.sh' has been created in the current"
 +echo "directory. You can launch it to start the simulation, or move it"
 +echo "to your user's bin directory if you want."
