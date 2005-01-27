e02c7f2ab778d995551e14340951d982402fb899
Kirill Ponomarev <krion@FreeBSD.org>
2005-01-27 18:22:37 +0000

    Fix build on 4.x

---
+++ games/highmoon/files/patch-galaxy.cpp
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- galaxy.cpp.orig
++++ galaxy.cpp
+@@ -24,7 +24,7 @@
+  ******************************************************************************************/
+ 
+ #include <cmath>
+-
++#include <time.h>
+ #include "galaxy.hpp"
+ #include "sound.hpp"
+ 
