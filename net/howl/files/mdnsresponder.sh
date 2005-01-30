aa536795f49eda163aa51af9a44daf5a5c1e72c8
marcus <marcus@FreeBSD.org>
2005-01-30 04:03:36 +0000

    * Update to 0.9.10 * Add an rcNG script for starting mDNSResponder * Assume maintainership
    Approved by:	Paul Dlug <paul@aps.org> (previous maintainer)

---
+++ net/howl/files/mdnsresponder.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+# PROVIDE: mdnsresponder
+# REQUIRE: DAEMON
+# KEYWORD: FreeBSD
+#
+# Howl's mDNSResponder, a Zeroconf (Rendezvous) service advertisement daemon.
+#
+
+mdnsresponder_enable=${mdnsresponder_enable-"NO"}
+mdnsresponder_flags=${mdnsresponder_flags-""}
+
+. %%RC_SUBR%%
+
+name=mdnsresponder
+rcvar=`set_rcvar`
+
+start_cmd=mdnsresponder_start
+stop_cmd=mdnsresponder_stop
+
+mdnsresponder_start() {
+    checkyesno mdnsresponder_enable && echo "Starting mDNSResponder." && \
+    	%%PREFIX%%/bin/mDNSResponder ${mdnsresponder_flags}
+}
+
+mdnsresponder_stop() {
+    checkyesno mdnsresponder_enable && echo "Stopping mDNSResponder." && \
+    	killall mDNSResponder
+}
+
+load_rc_config ${name}
+run_rc_command "$1"
