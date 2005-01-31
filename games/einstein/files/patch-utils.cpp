ce29958bb466fe1eb99417dc6f6bd15911880204
Edwin Groothuis <edwin@FreeBSD.org>
2005-01-31 21:24:15 +0000

    [games/einstein] : compile fix
        I tried to install the Einstein game just for the hack of it,
        but it doesn't compile without the attached (trivial) patch.
    
    Submitted by:	Rene Ladan <r.c.ladan@student.tue.nl>

---
+++ games/einstein/files/patch-utils.cpp
@@ -0,0 +1,10 @@
+--- utils.cpp.orig	Tue Feb  1 08:23:17 2005
++++ utils.cpp	Tue Feb  1 08:23:27 2005
+@@ -9,6 +9,7 @@
+ //#endif
+ 
+ #include <fstream>
++#include <math.h>
+ 
+ #include "utils.h"
+ #include "main.h"
