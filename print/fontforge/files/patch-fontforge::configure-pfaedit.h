a8ea377876e90e5ff1476c0b0851d8c37da502d8
thierry <thierry@FreeBSD.org>
2005-01-31 22:07:31 +0000

    Upgrade to 20050117.
    PR:		ports/76844
    Submitted by:	maintainer.

--- print/fontforge/files/patch-fontforge::configure-pfaedit.h
+++ print/fontforge/files/patch-fontforge::configure-pfaedit.h
@@ -1,6 +1,15 @@
---- fontforge/configure-pfaedit.h.orig	Tue May 13 03:20:08 2003
-+++ fontforge/configure-pfaedit.h	Tue May 13 10:24:58 2003
-@@ -31,7 +31,7 @@
+--- fontforge/configure-pfaedit.h.orig	Sat Jan  8 11:11:45 2005
++++ fontforge/configure-pfaedit.h	Thu Jan 20 16:53:29 2005
+@@ -73,7 +73,7 @@
+ /* Sometimes I want to create a glyph which is a word, and this command       */
+ /*  makes that easy                                                           */
+ /*									      */
+-/* #define FONTFORGE_CONFIG_PASTEAFTER					      */
++#define FONTFORGE_CONFIG_PASTEAFTER
+ /*									      */
+ /* The paste after command pastes the contents of the clipboard into the      */
+ /*  current glyph, translates that by the advance width of the current glyph  */
+@@ -84,7 +84,7 @@
  /*  don't include code for it by default, but if you do want it simply define */
  /*  the following macro							      */
  /*									      */
@@ -9,7 +18,7 @@
  /*									      */
  /* The tile path command uses the contents of the clipboard as a tile which is*/
  /*  applied to any selected paths, replacing them. It could be used to make a */
-@@ -42,7 +42,7 @@
+@@ -190,7 +190,7 @@
  /*  routine. I'm not sure that it's very useful though. It can be used to do  */
  /*  perspective transformations and such				      */
  /*									      */
