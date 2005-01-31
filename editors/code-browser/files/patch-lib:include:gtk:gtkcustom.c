1a8eb50f7324b99d0edaadc93282af33abc1c10c
ijliao <ijliao@FreeBSD.org>
2005-01-31 09:08:59 +0000

    upgrade to 1.3

---
+++ editors/code-browser/files/patch-lib:include:gtk:gtkcustom.c
@@ -0,0 +1,17 @@
+--- lib/include/gtk/gtkcustom.c.orig	Mon Jan 31 17:05:27 2005
++++ lib/include/gtk/gtkcustom.c	Mon Jan 31 17:05:43 2005
+@@ -131,11 +131,11 @@
+ ///////////////////////////////////////////////////////////////////////////////
+ static void gtk_custom_add (GtkContainer *container, GtkWidget *widget)
+ {
+-	g_return_if_fail (GTK_IS_CUSTOM (container));
+-	g_return_if_fail (GTK_IS_WIDGET (widget));
+-	
+ 	GtkCustom* custom;
+ 	GtkCustomChild *child_info;
++	
++	g_return_if_fail (GTK_IS_CUSTOM (container));
++	g_return_if_fail (GTK_IS_WIDGET (widget));
+ 	
+ 	custom = GTK_CUSTOM (container);
+ 	
