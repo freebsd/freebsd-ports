03b6a79c500283f82533fb39471b7e0efa7c093d
Maho Nakata <maho@FreeBSD.org>
2005-02-01 07:07:06 +0000

    Fix a rare case problem (sometimes dumps core)
    Submitted by:	"Pedro F. Giffuni" <giffunip@yahoo.com>

--- graphics/urt/files/patch-lib::rle_global.c
+++ graphics/urt/files/patch-lib::rle_global.c
@@ -1,13 +1,11 @@
---- lib/rle_global.c.orig	Wed Jan 29 03:23:03 1992
-+++ lib/rle_global.c	Fri Dec 27 23:20:42 2002
-@@ -76,8 +76,10 @@
+--- lib/rle_global.c.orig	Tue Jan 28 18:23:03 1992
++++ lib/rle_global.c	Sun Jan 30 15:19:36 2005
+@@ -76,7 +76,7 @@
      8,				/* cmaplen (log2 of length of color map) */
      NULL,			/* pointer to color map */
      NULL,			/* pointer to comment strings */
-+#if 0
-     stdout,			/* output file */
+-    stdout,			/* output file */
++    0,				/* stdout, output file */
      { 7 },			/* RGB channels only */
-+#endif
      0L,				/* Can't free name and file fields. */
      "Urt",			/* Default "program name". */
-     "no file",			/* No file name given. */
