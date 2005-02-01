02b01044b8d58a9f07f9a7b63bda7ccd7253a874
vs <vs@FreeBSD.org>
2005-02-01 11:12:05 +0000

    After a crash jabberd refuses to run if a pidfile exists. Check if a process with the pid contained in jabber.pid is running and removes the file if not.
    PR:		ports/75824
    Submitted by:	oss-freebsd-ports AT technorama DOT net

--- net/jabber/files/jabberd.sh
+++ net/jabber/files/jabberd.sh
@@ -19,8 +19,19 @@ cd ${RUNDIR} || exit
 
 case ${1:-start} in
 start)
-    su -f -m ${USER} -c "jabberd -B -h ${HOSTNAME} -c ${PREFIX}/etc/jabber.xml" ;;
+    if [ -f jabber.pid ]; then
+        pid=`cat jabber.pid`
+        if [ ! -z "$pid" ] && kill -0 -- "$pid"; then
+            echo "A pidfile already exists at the specified location."
+            echo "Check to ensure another copy of the server is not running, or remove the existing file."
+            exit 1
+        fi
 
+        rm -f ${RUNDIR}/jabber.pid;
+    fi
+
+    su -f -m ${USER} -c "jabberd -B -h ${HOSTNAME} -c ${PREFIX}/etc/jabber.xml"
+    ;;
 stop)
     killall -SIGKILL -u ${USER} jabberd;
     rm -f ${RUNDIR}/jabber.pid;
