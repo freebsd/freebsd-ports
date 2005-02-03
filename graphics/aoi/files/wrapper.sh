677401496ad7e540c2738be5164c6f34d777e58e
Herve Quiroz <hq@FreeBSD.org>
2005-02-03 00:51:59 +0000

    - Update to bsd.java.mk 2.0 - Improve launcher shell script (switch to javavmwrapper 2.0) - Dynamic plist - Install in DATADIR=${JAVASHAREDIR}/${PORTNAME} - Bump PORTREVISION

--- graphics/aoi/files/wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PREFIX="%%PREFIX%%"
-JAVAVM="%%JAVAVM%%"
-JAR_DIR="%%JAR_DIR%%"
-JAR_FILE="%%JAR_FILE%%"
-
-( cd ${PREFIX}/${JAR_DIR} && \
-	${JAVAVM} -jar ${PREFIX}/${JAR_DIR}/${JAR_FILE} "${@}" )
