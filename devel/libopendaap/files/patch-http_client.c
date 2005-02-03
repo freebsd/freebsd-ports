bf1e132fa6b77756e4bbf3e096594f8529fa8a68
Michael Johnson <ahze@FreeBSD.org>
2005-02-03 02:37:56 +0000

    - Update to 0.3.0

--- devel/libopendaap/files/patch-http_client.c
+++ devel/libopendaap/files/patch-http_client.c
@@ -1,10 +1,10 @@
---- http_client.c.orig	Wed Aug  4 20:11:25 2004
-+++ http_client.c	Wed Aug  4 20:12:06 2004
+--- http_client.c.orig	Fri Dec 17 00:27:43 2004
++++ http_client.c	Wed Feb  2 21:33:28 2005
 @@ -37,6 +37,7 @@
  # include <sys/socket.h>
  # include <sys/select.h>
  # include <netdb.h>
 +# include <netinet/in.h>
+ # include <fcntl.h>
  #elif defined(SYSTEM_WIN32)
  # include <winsock2.h>
- # include <ws2tcpip.h>
