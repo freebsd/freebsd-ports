cfae8997c751d279d25bba42dca968a7dc32e776
Mikhail Teterin <mi@FreeBSD.org>
2005-02-04 00:45:31 +0000

    Fix warnings. Default to ssh. Don't insist, that the remote hostname resolves -- it may be an entry from ssh's config file.
    Use ftruncate -- we have it. Bump PORTREVISION.
    
    Skimmed over by:	peter (past maintainer)

--- net/rdist6/files/patch-defs.h
+++ net/rdist6/files/patch-defs.h
@@ -1,6 +1,34 @@
---- include/defs.h.orig	Mon Nov  9 20:23:54 1998
-+++ include/defs.h	Fri Nov  3 02:39:45 2000
-@@ -379,7 +379,9 @@
+--- include/defs.h	Mon Nov  9 23:23:54 1998
++++ include/defs.h	Tue Jan 18 15:41:40 2005
+@@ -17,10 +17,9 @@
+ /*
+  * POSIX settings
+  */
+-#if	defined(_POSIX_SOURCE)
+ #include <unistd.h>
+ #include <stdlib.h>
+-#endif	/* _POSIX_SOURCE */
++#include <libgen.h>
+ #include <stdio.h>
+ #include <ctype.h>
+ #include <errno.h>
+@@ -357,7 +355,6 @@
+ extern void			complain();
+ extern void			docmds();
+ extern void			finish();
+-extern void			log();
+ extern void			logmsg();
+ extern void			lostconn();
+ extern void			markassigned();
+@@ -366,7 +363,6 @@
+ extern void			runcmdspecial();
+ extern void			runcommand();
+ extern void			server();
+-extern void			setprogname();
+ extern void			sighandler();
+ extern void			waitup();
+ struct namelist		       *expand();
+@@ -379,7 +375,9 @@
  extern void			error(char *, ...);
  extern void			fatalerr(char *, ...);
  extern void			message(int, char *, ...);
