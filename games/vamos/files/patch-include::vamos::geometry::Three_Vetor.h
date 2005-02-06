8257a5226e58cac66b4b944dc9599bb66aa8ff37
pav <pav@FreeBSD.org>
2005-02-06 23:30:41 +0000

    - Update to 0.5.3
    PR:		ports/77136
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ games/vamos/files/patch-include::vamos::geometry::Three_Vetor.h
@@ -0,0 +1,10 @@
+--- include/vamos/geometry/Three_Vector.h.orig	Thu Feb  3 12:17:54 2005
++++ include/vamos/geometry/Three_Vector.h	Sat Feb  5 20:11:47 2005
+@@ -19,6 +19,7 @@
+ #define _THREE_VECTOR_H_
+ 
+ #include <iostream>
++#include <cmath>
+ 
+ // A three-element vector.  Useful for representing physical quantites
+ // like position, veocity or force.
