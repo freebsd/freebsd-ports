40ac89c3e6ff9dedb97c337a952d62705104212d
pav <pav@FreeBSD.org>
2005-02-08 23:18:00 +0000

    Libmpeg3 supports advanced editing and manipulation of MPEG streams. Libmpeg3 provides a uniform front end for a large number of the MPEG formats used in HDTV broadcasting.
    It decodes:
    * MPEG-1 Layer II Audio
    * MPEG-1 Layer III Audio
    * MPEG-2 Layer III Audio
    * MPEG-1 program streams
    * MPEG-2 program streams
    * MPEG-2 transport streams
    * AC3 Audio
    * MPEG-2 Video
    * MPEG-1 Video
    * IFO files
    * VOB files
    
    PR:		ports/77175
    Submitted by:	Igor Pokrovsky <ip@doom.homeunix.org>

---
+++ multimedia/libmpeg3/files/patch-a52dec-0.7.3_include_config.h
@@ -0,0 +1,11 @@
+--- a52dec-0.7.3/include/config.h.orig
++++ a52dec-0.7.3/include/config.h
+@@ -17,7 +17,7 @@
+ /* #undef HAVE_IO_H */
+ 
+ /* Define if you have the `memalign' function. */
+-#define HAVE_MEMALIGN 1
++/*#define HAVE_MEMALIGN 1*/
+ 
+ /* Define if you have the <memory.h> header file. */
+ #define HAVE_MEMORY_H 1
