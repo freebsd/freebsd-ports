5f2dbec2db3a2616c3dc160cda6de00b44946f3c
Volker Stolz <vs@FreeBSD.org>
2005-02-09 11:14:39 +0000

    Add nfsshell, a userland NFS command tool.
    PR:		ports/77153
    Submitted by:	johans@stack.nl

---
+++ net/nfsshell/files/patch-nfs.c
@@ -0,0 +1,10 @@
+--- nfs.c.orig	Sat Feb  5 23:17:18 2005
++++ nfs.c	Sat Feb  5 23:17:09 2005
+@@ -50,7 +50,6 @@
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
+ #include <sys/time.h>
+-#include <sys/sysmacros.h>
+ #include "mount.h"
+ #include "nfs_prot.h"
+ #include <netinet/in_systm.h>
