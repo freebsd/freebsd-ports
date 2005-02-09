b2edfa4f1401261c248f73a48ec3b2935f6e9dcb
ahze <ahze@FreeBSD.org>
2005-02-09 04:20:10 +0000

    - add support for mozilla 1.8a6.

--- www/kazehakase/files/patch-kz-mozembed.cpp
+++ www/kazehakase/files/patch-kz-mozembed.cpp
@@ -1,11 +1,14 @@
---- src/mozilla/kz-mozembed.cpp.orig	Thu Dec  2 03:08:25 2004
-+++ src/mozilla/kz-mozembed.cpp	Thu Dec  2 03:08:35 2004
-@@ -64,7 +64,7 @@
+--- src/mozilla/kz-mozembed.cpp.orig	Tue Feb  8 22:01:07 2005
++++ src/mozilla/kz-mozembed.cpp	Tue Feb  8 22:01:23 2005
+@@ -64,7 +64,11 @@
  #include <docshell/nsIDocShellTreeItem.h>
  #include <docshell/nsIDocShellTreeOwner.h>
  #include <nsIPresShell.h>
--#include <nsIPresContext.h>
-+// #include <nsIPresContext.h>
++#if MOZILLA_SNAPSHOT > 16
++#include <nsPresContext.h>
++#else
+ #include <nsIPresContext.h>
++#endif
  #include <nsIDOMNodeList.h>
  #include <nsIDOMWindow.h>
  #include <nsISelection.h>
