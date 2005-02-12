03027c74da60f3f4d5de73d634ca6003447da42e
thierry <thierry@FreeBSD.org>
2005-02-12 21:11:07 +0000

    Unbreak devel/monotone on FreeBSD-5.3.
    Bump PORTREVISION.
    
    PR:		ports/76730
    Submitted by:	Mark A. Wicks and maintainer
    Obtained from:	Boost CVS repository.

--- devel/boost/files/patch-boost::config::platform:bsd.hpp
+++ devel/boost/files/patch-boost::config::platform:bsd.hpp
@@ -1,25 +1,14 @@
---- boost/config/platform/bsd.hpp.orig	Fri Feb 27 14:13:00 2004
-+++ boost/config/platform/bsd.hpp	Wed Dec 15 16:01:21 2004
-@@ -39,9 +39,20 @@
- #endif
- 
+--- boost/config/platform/bsd.hpp.orig	Sat Feb  5 12:53:03 2005
++++ boost/config/platform/bsd.hpp	Sat Feb  5 12:53:46 2005
+@@ -41,8 +41,9 @@
  //
--// No wide character support in the BSD header files:
-+// Enable wide character support on FreeBSD 5 and above
+ // No wide character support in the BSD header files:
  //
 -#define BOOST_NO_CWCHAR
-+#if defined(__FreeBSD__) && (__FreeBSD__ >= 5)
-+#  undef BOOST_NO_CWCHAR
-+#  undef BOOST_NO_SWPRINTF
-+#  undef BOOST_NO_CWCTYPE
-+#  undef BOOST_DEDUCED_TYPENAME
-+#else
+-
++#if !(defined(__FreeBSD__) && (__FreeBSD__ >= 5))
 +#  define BOOST_NO_CWCHAR
-+#  define BOOST_NO_SWPRINTF
-+#  define BOOST_NO_CWCTYPE
 +#endif
-+ 
-+#define BOOST_NO_INCLASS_MEMBER_INITIALIZATION
- 
  //
  // The BSD <ctype.h> has macros only, no functions:
+ //
