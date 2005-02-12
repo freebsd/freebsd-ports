7d242e995e301074981fcf940e33ecf3643a799a
thierry <thierry@FreeBSD.org>
2005-02-12 21:11:43 +0000

    Upgrade to 0.16 and unbreak on FreeBSD-5.3.
    PR:		ports/76738
    Submitted by:	maintainer.

--- devel/monotone/files/patch-sanity.hh
+++ devel/monotone/files/patch-sanity.hh
@@ -1,6 +1,6 @@
---- sanity.hh.orig	Wed Jan  7 13:11:48 2004
-+++ sanity.hh	Sun Mar 21 22:49:59 2004
-@@ -58,7 +58,11 @@
+--- sanity.hh.orig	Fri Dec 31 03:16:54 2004
++++ sanity.hh	Wed Jan 26 22:20:17 2005
+@@ -64,7 +64,12 @@
  extern sanity global_sanity;
  
  // F is for when you want to build a boost formatter
@@ -9,6 +9,7 @@
 +#else
 +#define F(str) boost::format((str))
 +#endif
++
  
  // L is for logging, you can log all you want
- #define L(fmt) global_sanity.log(fmt)
+ #define L(fmt) global_sanity.log(fmt, __FILE__, __LINE__)
