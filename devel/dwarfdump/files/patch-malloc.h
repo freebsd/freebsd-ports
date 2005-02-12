ce7ac1a8347b59a3d3eea8fe1aecd28a069584b4
Joerg Wunsch <joerg@FreeBSD.org>
2005-02-12 22:12:15 +0000

    dwarfdump is a utility to display DWARF debugging information in ELF files.  Needs libdwarf (and libelf in turn).

---
+++ devel/dwarfdump/files/patch-malloc.h
@@ -0,0 +1,10 @@
+--- makename.c.orig	Mon Nov 22 19:20:33 2004
++++ makename.c	Sat Feb 12 22:56:10 2005
+@@ -45,7 +45,6 @@
+ */
+ 
+ #include <stdio.h>
+-#include <malloc.h>
+ #include <string.h>
+ #include <stdlib.h>
+ #include "makename.h"
