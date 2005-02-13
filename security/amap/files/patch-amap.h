8ed9d18d8f48e522eeb22e69c4031e1a9a52f088
Yen-Ming Lee <leeym@FreeBSD.org>
2005-02-13 18:51:36 +0000

    - update to amap-4.8
    PR:		77451
    Submitted by:	Yonatan <onatan at gmail.com>

--- security/amap/files/patch-amap.h
+++ security/amap/files/patch-amap.h
@@ -1,10 +1,10 @@
---- amap.h.orig	Sun Sep 14 11:25:16 2003
-+++ amap.h	Sun Sep 14 11:25:29 2003
+--- amap.h.orig	Tue Feb  1 20:07:14 2005
++++ amap.h	Sun Feb 13 14:28:05 2005
 @@ -33,7 +33,7 @@
   #endif
  #endif
  
--#define AMAP_APPDEF_PATH	AMAP_PREFIX"/bin"
+-#define AMAP_APPDEF_PATH	AMAP_PREFIX"/etc"
 +#define AMAP_APPDEF_PATH	AMAP_PREFIX"/share/amap"
  #define AMAP_BUFSIZE		1024	// standard buffer size
  #define AMAP_BUFSIZE_BIG	65536   // big standard buffer size
