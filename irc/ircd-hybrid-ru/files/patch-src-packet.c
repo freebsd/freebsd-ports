ea28b3e28a0448825df8ee12c1f5b334d74e9801
Kirill Ponomarev <krion@FreeBSD.org>
2005-02-13 10:26:10 +0000

    Update to version 7.0rc12
    PR:		ports/77441
    Submitted by:	maintainer

--- irc/ircd-hybrid-ru/files/patch-src-packet.c
+++ irc/ircd-hybrid-ru/files/patch-src-packet.c
@@ -1,9 +1,9 @@
---- src/packet.c.orig	Mon Nov 15 14:41:32 2004
-+++ src/packet.c	Mon Nov 15 14:42:04 2004
-@@ -531,7 +531,7 @@
-   parse_client_queued(client_p);
+--- src/packet.c.orig	Tue Nov 16 22:29:48 2004
++++ src/packet.c	Sun Feb 13 12:33:49 2005
+@@ -532,7 +532,7 @@
  
    /* Check to make sure we're not flooding */
+ 
 -  if (IsPerson(client_p) &&
 +  if (!(IsServer(client_p) || IsHandshake(client_p) || IsConnecting(client_p)) &&
        (linebuf_alloclen(&client_p->localClient->buf_recvq) >
