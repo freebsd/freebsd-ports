5815e58c6e9a8a371e65b6c08d617def5cc41148
Adam Weinberger <adamw@FreeBSD.org>
2005-02-14 17:15:40 +0000

    Update to 2.8.3.

--- games/gnomegames2/files/patch-iagno_othello.c
+++ games/gnomegames2/files/patch-iagno_othello.c
@@ -1,6 +1,6 @@
---- iagno/othello.c.orig	Thu May 20 17:20:21 2004
-+++ iagno/othello.c	Thu May 20 17:21:11 2004
-@@ -544,7 +544,7 @@
+--- iagno/othello.c.orig	Mon Feb 14 12:07:12 2005
++++ iagno/othello.c	Mon Feb 14 12:07:47 2005
+@@ -551,7 +551,7 @@
  					memcpy (tboard, board, sizeof (gint8) * 8 * 8);
  					move_board(tboard, i, j, me, 0);
  	
@@ -9,16 +9,7 @@
  					tmp_move = find_best_move(tboard, not_me, ply_depth-1,
  						-pass_thresh, -use_thresh, 0, 0);
  
-@@ -559,7 +559,7 @@
- 					}
- 				}
- 		
--		// check if we have to pass
-+		/* check if we have to pass */
- 		if (best_x == 8 && best_y == 8) {
- 			tmp_move = find_best_move(board, not_me, ply_depth-1,
- 				-pass_thresh, -use_thresh, 0, 0);
-@@ -802,14 +802,14 @@
+@@ -808,14 +808,14 @@
  				found_free = TRUE;
  		}
  		
@@ -32,7 +23,7 @@
 -	// if the game is finished, return the actual score rather than
 -	// a huristic
 +	/* if the game is finished, return the actual score rather than
-+	 * a huristic */
++	   a huristic */
  	if (!found_free)
  		return (actual_score);
  	
