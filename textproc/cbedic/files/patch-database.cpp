355d4ab26e58d76590cf8a09cdbff2db72ba06f9
roam <roam@FreeBSD.org>
2005-02-15 23:17:45 +0000

    Snatch kbedic from the jaws of Death in the nick of time...  Okay, okay - the truth is, this is a *long* overdue update to kbedic to use the QT 3 libraries instead of the dead-and-gone QT 2 ones.  This does indeed result in some duplication of installed dictionaries, since kbedic no longer uses textproc/bedic-data, but I'll take care of this later.
    Break cbedic out into a full-blown port - it's a different version from
    kbedic now.  While I'm here, take maintainership of both ports.
    
    Submitted by:	arved (a long, long time ago)

--- textproc/cbedic/files/patch-database.cpp
+++ textproc/cbedic/files/patch-database.cpp
@@ -1,5 +1,5 @@
---- src/database.cpp.old	Fri Dec 12 03:18:26 2003
-+++ src/database.cpp	Fri Dec 12 03:18:39 2003
+--- src/database.cpp.orig	Sat Aug  9 19:23:43 2003
++++ src/database.cpp	Sat Aug  9 19:24:03 2003
 @@ -106,7 +106,7 @@
  // Open data file and set some variables
  // Return true if success, false if failed
