9492d5c576e7fcaf087b9b6971548cc47e4cf1ee
vs <vs@FreeBSD.org>
2005-02-16 14:28:08 +0000

    - Chase master site - Cleanup Makefile a bit (me)
    PR:		ports/77552
    Submitted by:	"Jonatan B" (maintainer)

---
+++ security/dcetest/files/patch-dcetest.c
@@ -0,0 +1,11 @@
+--- dcetest.c.orig	Tue Feb 15 11:32:36 2005
++++ dcetest.c	Tue Feb 15 11:31:48 2005
+@@ -359,7 +359,7 @@
+   int tint;
+   char annotation[EPT_MAX_ANNOTATION_SIZE+1];
+   int floor, floors;
+-  int address_type;
++  int address_type=0;
+ 
+   char tmp_address[200];
+   char tmp_address2[200];
