636327204cdbd4122f9154690f00e9125895a093
Michael Johnson <ahze@FreeBSD.org>
2005-02-16 05:52:43 +0000

    - Update to 0.3.0 - Add WITHOUT_GLITZ OPTION [1] - Add WITHOUT_X11 knob
    Requested by:	debugger via irc [1]

--- graphics/cairo/files/patch-src_cairo_font.c
+++ graphics/cairo/files/patch-src_cairo_font.c
@@ -1,27 +1,15 @@
---- src/cairo_font.c.orig	Tue Nov  9 21:51:24 2004
-+++ src/cairo_font.c	Tue Nov  9 21:53:06 2004
-@@ -54,9 +54,9 @@
- static unsigned long
- _font_cache_hash (void *cache, void *key)
- {
-+    unsigned long hash;
-     cairo_font_cache_key_t *in;
-     in = (cairo_font_cache_key_t *) key;
--    unsigned long hash;
- 
-     /* 1607 and 1451 are just a couple random primes. */
-     hash = _cairo_hash_string (in->family);
-@@ -86,11 +86,12 @@
+--- src/cairo_font.c.orig	Thu Jan 13 09:37:40 2005
++++ src/cairo_font.c	Wed Feb 16 00:50:47 2005
+@@ -85,9 +85,11 @@
  			  void *key,
  			  void **return_value)
  {
-+    const struct cairo_font_backend *backend; 
+-    const cairo_font_backend_t *backend = CAIRO_FONT_BACKEND_DEFAULT;
++    const cairo_font_backend_t *backend;
      cairo_font_cache_key_t *k;
      cairo_font_cache_entry_t *entry;
++
++    backend = CAIRO_FONT_BACKEND_DEFAULT;
      k = (cairo_font_cache_key_t *) key;
  
--    const struct cairo_font_backend *backend = CAIRO_FONT_BACKEND_DEFAULT;
-+    backend = CAIRO_FONT_BACKEND_DEFAULT;
- 
      /* XXX: The current freetype backend may return NULL, (for example
-      * if no fonts are installed), but I would like to guarantee that
