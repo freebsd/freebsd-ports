9cfb643162e8690eef288525a15bfd3305935c3c
Alfred Perlstein <alfred@FreeBSD.org>
2005-02-16 08:13:33 +0000

    Update to 98.
    PR: 77514

--- net/mDNSResponder/files/patch-mDNSShared::dns_sd.h
+++ net/mDNSResponder/files/patch-mDNSShared::dns_sd.h
@@ -1,14 +1,10 @@
---- mDNSShared/dns_sd.h.old	Fri Mar 19 15:38:48 2004
-+++ mDNSShared/dns_sd.h	Fri Mar 19 15:38:58 2004
-@@ -46,7 +46,11 @@
+--- mDNSShared/dns_sd.h.orig	Sat Dec 18 04:14:23 2004
++++ mDNSShared/dns_sd.h	Mon Feb 14 13:52:41 2005
+@@ -39,6 +39,7 @@
+ #define DNSSD_API
+ #endif
  
++#include <osreldate.h>
+ #if defined(__FreeBSD_version) && (__FreeBSD_version < 500000)
+ /* stdint.h does not exist on FreeBSD 4.x; its types are defined in sys/types.h instead */
  #include <sys/types.h>
- #include <sys/socket.h>
-+#if defined(__FreeBSD__) && __FreeBSD_version >= 500000
- #include <stdint.h>
-+#elif !defined(__FreeBSD__)
-+#include <stdint.h>
-+#endif
- #include <netinet/in.h>
- 
- 
