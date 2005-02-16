1668f0adeb02c4d443a8d09be541da7c35c05df4
thierry <thierry@FreeBSD.org>
2005-02-16 21:42:16 +0000

    Upgrade to TORCS-1.2.3.
    Remark: it is currently broken on FreeBSD-4.11.

---
+++ games/torcs/files/patch-src::libs::raceengineclient::raceinit.cpp
@@ -0,0 +1,11 @@
+--- src/libs/raceengineclient/raceinit.cpp.orig	Mon Feb 14 23:33:57 2005
++++ src/libs/raceengineclient/raceinit.cpp	Mon Feb 14 23:38:17 2005
+@@ -34,6 +34,8 @@
+ #include <racescreens.h>
+ #include <robottools.h>
+ 
++#include <publib.h>
++
+ #include "raceengine.h"
+ #include "racemain.h"
+ #include "racestate.h"
---
+++ games/torcs/files/patch-src::libs::tgfclient::screen.cpp
@@ -0,0 +1,11 @@
+--- src/libs/tgfclient/screen.cpp.orig	Mon Feb 14 23:09:32 2005
++++ src/libs/tgfclient/screen.cpp	Mon Feb 14 23:22:49 2005
+@@ -36,6 +36,8 @@
+ #include <process.h>
+ #endif /* WIN32 */
+ 
++#include <publib.h>
++
+ #include <tgfclient.h>
+ #include "gui.h"
+ #include "fg_gm.h"
