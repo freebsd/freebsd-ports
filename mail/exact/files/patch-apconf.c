8ec660dae71854839f7a6eb559f9034cfa405c3f
Kirill Ponomarev <krion@FreeBSD.org>
2005-02-19 11:16:46 +0000

    Update to version 1.41
    PR:		ports/77724
    Submitted by:	Sam Lawrance <boris at brooknet.com.au>

---
+++ mail/exact/files/patch-apconf.c
@@ -0,0 +1,10 @@
+--- apconf.c.orig	Sat Feb 19 21:05:40 2005
++++ apconf.c	Sat Feb 19 21:05:54 2005
+@@ -6,6 +6,7 @@
+  */
+ 
+ #include <stdio.h>
++#include <sys/types.h>
+ #include <regex.h>
+ #include <string.h>
+ #include <stdlib.h>
