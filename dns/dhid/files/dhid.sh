9f5981cd69875f2d70c6dab97a5a15aa4788f09b
Pav Lucistnik <pav@FreeBSD.org>
2005-02-19 20:42:39 +0000

    - Update to 5.2
    PR:		ports/77127
    Submitted by:	Thomas Melzer <tmelzer@tomesoft.de>
    Approved by:	maintainer timeout (14 days)

--- dns/dhid/files/dhid.sh
+++ dns/dhid/files/dhid.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
     echo "$0: Cannot determine the PREFIX" >&2
@@ -7,7 +8,7 @@ fi
 
 case "$1" in
 start)
-	[ -x "${PREFIX}/sbin/dhid" -a -f "${PREFIX}/etc/dhid.conf" ] && ${PREFIX}/sbin/dhid > /dev/null && echo -n ' dhid'
+	[ -x "${PREFIX}/sbin/dhid" -a -f "${PREFIX}/etc/dhid.conf" ] && su -m dhis -c ${PREFIX}/sbin/dhid > /dev/null && echo -n ' dhid'
 	;;
 stop)
 	killall dhid && echo -n ' dhid'
