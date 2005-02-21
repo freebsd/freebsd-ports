d05127ae033eaf0af8c7f64108294c4673c64514
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-02-21 21:46:36 +0000

    Update to 2.8.2.

--- sysutils/gnome-control-center/files/patch-capplets_default-applications_gnome-default-applications-properies-structs.c
+++ sysutils/gnome-control-center/files/patch-capplets_default-applications_gnome-default-applications-properies-structs.c
@@ -1,20 +1,14 @@
---- capplets/default-applications/gnome-default-applications-properties-structs.c.orig	Mon Dec 13 22:37:25 2004
-+++ capplets/default-applications/gnome-default-applications-properties-structs.c	Mon Dec 13 22:45:16 2004
-@@ -9,11 +9,8 @@
-         { N_("Epiphany"), 		"epiphany",	"epiphany %s",		FALSE, FALSE },
-         { N_("Galeon"), 		"galeon",	"galeon %s",		FALSE, FALSE },
-         { N_("Encompass"), 		"encompass",	"encompass %s",		FALSE, FALSE },
--        { N_("Firebird/FireFox"),  "mozilla-firebird",  "mozilla-firebird %s",  FALSE, FALSE },
--        { N_("Firebird/FireFox"),  "mozilla-firefox",	"mozilla-firebird %s",  FALSE, FALSE },
--        { N_("Firebird/FireFox"),  	"firefox",	"mozilla-firebird %s",	FALSE, FALSE },
--        { N_("Mozilla/Netscape 6"), 	"mozilla-1.6",	"mozilla-1.6 %s",	FALSE, FALSE },
--        { N_("Mozilla/Netscape 6"), 	"mozilla",	"mozilla %s",		FALSE, FALSE },
-+        { N_("FireFox"),  		"firefox",	"firefox %s",		FALSE, FALSE },
-+        { N_("Mozilla"), 		"mozilla",	"mozilla %s",		FALSE, FALSE },
+--- capplets/default-applications/gnome-default-applications-properties-structs.c.orig	Sun Feb 20 16:18:03 2005
++++ capplets/default-applications/gnome-default-applications-properties-structs.c	Mon Feb 21 15:52:04 2005
+@@ -12,6 +12,7 @@
+         { N_("Firebird/FireFox"),  "mozilla-firebird",  "mozilla-firebird %s",  FALSE, FALSE },
+         { N_("Firebird/FireFox"),  "mozilla-firefox",	"mozilla-firebird %s",  FALSE, FALSE },
+         { N_("Firebird/FireFox"),  	"firefox",	"mozilla-firefox %s",	FALSE, FALSE },
++        { N_("Firebird/FireFox"),  	"firefox",	"firefox %s",	FALSE, FALSE },
+         { N_("Mozilla/Netscape 6"), 	"mozilla-1.6",	"mozilla-1.6 %s",	FALSE, FALSE },
+         { N_("Mozilla/Netscape 6"), 	"mozilla",	"mozilla %s",		FALSE, FALSE },
          { N_("Netscape Communicator"), 	"netscape",	"netscape %s",		FALSE, FALSE },
-         { N_("Konqueror"), 		"konqueror",	"konqueror %s",		FALSE, FALSE },
-         { N_("W3M Text Browser"),	"w3n",		"w3m %s",		TRUE,  FALSE },
-@@ -40,6 +37,7 @@
+@@ -40,6 +41,7 @@
  	{ N_("Balsa"),        			"balsa",		"balsa -m %s",		FALSE, FALSE },
  	{ N_("KMail"),        			"kmail",		"kmail %s",		FALSE, FALSE },
  	{ N_("Mozilla Mail"), 			"mozilla",		"mozilla -mail %s",	FALSE, FALSE},
