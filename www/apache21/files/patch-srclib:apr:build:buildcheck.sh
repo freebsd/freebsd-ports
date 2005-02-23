be52772461db2b45f8b3af03d8991ae0f5590050
clement <clement@FreeBSD.org>
2005-02-23 10:59:45 +0000

    - Update to 2.1.3
    If you can't fetch the distfile, fetch http://httpd.apache.org/dev/dist/httpd-2.1.3-alpha.tar.bz2
    into ${PORTSDIR}/distfiles/apache21

--- www/apache21/files/patch-srclib:apr:build:buildcheck.sh
+++ www/apache21/files/patch-srclib:apr:build:buildcheck.sh
@@ -1,9 +1,9 @@
---- srclib/apr/build/buildcheck.sh.orig	Sun Jun  6 23:19:19 2004
-+++ srclib/apr/build/buildcheck.sh	Sat Dec 11 16:57:18 2004
+--- srclib/apr/build/buildcheck.sh.orig	Fri Jan 14 11:16:31 2005
++++ srclib/apr/build/buildcheck.sh	Wed Feb 23 11:16:25 2005
 @@ -38,7 +38,7 @@
  # output is multiline from 1.5 onwards
  
- # Require libtool 1.3.3 or newer
+ # Require libtool 1.4 or newer
 -libtool=`build/PrintPath glibtool libtool libtool15 libtool14`
 +libtool=${LIBTOOL}
  lt_pversion=`$libtool --version 2>/dev/null|sed -e 's/([^)]*)//g;s/^[^0-9]*//;s/[- ].*//g;q'`
