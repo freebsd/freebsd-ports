7a0d5170276ec84775b1c05cdc7d68c34d34ad08
Hajimu UMEMOTO <ume@FreeBSD.org>
2005-02-23 15:39:12 +0000

    update Debian patch to egg_4.0.6+0.20041122cvs-2.

--- editors/tamago-emacs21/files/patch-egg::canna.el
+++ editors/tamago-emacs21/files/patch-egg::canna.el
@@ -1,7 +1,16 @@
 Index: egg/canna.el
 diff -u egg/canna.el.orig egg/canna.el
---- egg/canna.el.orig	Wed Feb 23 21:57:47 2005
-+++ egg/canna.el	Wed Feb 23 21:58:47 2005
+--- egg/canna.el.orig	Thu Feb 24 00:16:28 2005
++++ egg/canna.el	Thu Feb 24 00:27:33 2005
+@@ -53,7 +53,7 @@
+   "Group Name on CANNA server"
+   :group 'canna :type 'string)
+ 
+-(defcustom egg-canna-icanna-path "icanna"
++(defcustom egg-canna-icanna-path "egg-helper"
+   "path of canna unix domain connection helper program"
+   :group 'canna :type 'file)
+ 
 @@ -360,13 +360,12 @@
  		      (setq proc (make-network-process :name proc-name :buffer buf :host host :service port :family family))
  		    ((error quit)))
