21343c86198a6a42ed4c6624c52794c2884a8077
ume <ume@FreeBSD.org>
2005-02-24 11:43:48 +0000

    update Debian patch to egg_4.0.6+0.20041122cvs-3.

--- editors/tamago/files/patch-egg::canna.el
+++ editors/tamago/files/patch-egg::canna.el
@@ -11,23 +11,3 @@ diff -u egg/canna.el.orig egg/canna.el
    "path of canna unix domain connection helper program"
    :group 'canna :type 'file)
  
-@@ -360,13 +360,12 @@
- 		      (setq proc (make-network-process :name proc-name :buffer buf :host host :service port :family family))
- 		    ((error quit)))
- 		; for old emacs (<= 21.3) bellow
--		(setq proc
--		      (cond ((string-match "^unix/" hostname)
--			     (let ((process-connection-type nil))
--			       (start-process proc-name buf egg-canna-icanna-path)))
--			    (condition-case nil
--				(open-network-stream proc-name buf hostname port)
--			      (error nil))))))
-+		(if (string-match "^unix/" hostname)
-+		    (let ((process-connection-type nil))
-+		      (setq proc (start-process proc-name buf egg-canna-icanna-path)))
-+		  (condition-case nil
-+		      (setq proc (open-network-stream proc-name buf hostname port))
-+		    (error quit)))))
- 	    (when (processp proc)
- 	      (process-kill-without-query proc)
- 	      (set-process-coding-system proc 'binary 'binary)
