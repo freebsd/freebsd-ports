71920aeb2b15ac44461a3991e892b485547c48e4
Maho Nakata <maho@FreeBSD.org>
2005-02-24 13:21:51 +0000

    update to 1.9m80

---
+++ editors/openoffice.org-3-RC/files/patch-chart2+source+controller+makefile.mk
@@ -0,0 +1,18 @@
+#iZ 43297
+
+Index: chart2/source/controller/makefile.mk
+===================================================================
+RCS file: /cvs/graphics/chart2/source/controller/makefile.mk,v
+retrieving revision 1.5
+diff -u -r1.5 makefile.mk
+--- chart2/source/controller/makefile.mk	18 May 2004 18:34:37 -0000	1.5
++++ chart2/source/controller/makefile.mk	23 Feb 2005 10:06:48 -0000
+@@ -143,7 +143,7 @@
+ 	$(SRS)$/chcdialogs.srs
+ 
+ RESLIB1NAME=	$(TARGET)
+-RESLIB1IMAGES=$(PRJ)$/res
++RESLIB1IMAGES=$(PRJ)$
+ RESLIB1SRSFILES=$(RESLIB1LIST)
+ RESLIB1DEPN=$(RESLIB1LIST)
+ 
