293df9e15e1d4b65394b379dea6ce6333574dc23
krion <krion@FreeBSD.org>
2005-02-25 14:02:40 +0000

    Update to version 1.4.6
    PR:		ports/77977
    Submitted by:	Ports Fury

--- x11-toolkits/fox-devel/files/patch-src::FXFile.cpp
+++ x11-toolkits/fox-devel/files/patch-src::FXFile.cpp
@@ -1,6 +1,15 @@
---- src/FXFile.cpp.orig	Fri Apr 16 23:25:33 2004
-+++ src/FXFile.cpp	Sat May 22 13:13:48 2004
-@@ -159,7 +159,7 @@
+--- src/FXFile.cpp.orig	Sat Feb  5 13:10:50 2005
++++ src/FXFile.cpp	Wed Feb  9 17:40:15 2005
+@@ -102,7 +102,7 @@
+ // Get current user name
+ FXString FXFile::getCurrentUserName(){
+ #ifndef WIN32
+-#ifdef FOX_THREAD_SAFE
++#if defined(FOX_THREAD_SAFE) && !defined(__FreeBSD__)
+   struct passwd pwdresult,*pwd;
+   char buffer[1024];
+   if(getpwuid_r(geteuid(),&pwdresult,buffer,sizeof(buffer),&pwd)==0 && pwd) return pwd->pw_name;
+@@ -180,7 +180,7 @@
  // Get home directory for a given user
  FXString FXFile::getUserDirectory(const FXString& user){
  #ifndef WIN32
@@ -9,7 +18,7 @@
    struct passwd pwdresult,*pwd;
    char buffer[1024];
    if(user.empty()){
-@@ -1032,7 +1032,7 @@
+@@ -1059,7 +1059,7 @@
  FXString FXFile::owner(FXuint uid){
    FXchar result[64];
  #ifndef WIN32
@@ -18,7 +27,7 @@
    struct passwd pwdresult,*pwd;
    char buffer[1024];
    if(getpwuid_r(uid,&pwdresult,buffer,sizeof(buffer),&pwd)==0 && pwd) return pwd->pw_name;
-@@ -1050,7 +1050,7 @@
+@@ -1077,7 +1077,7 @@
  FXString FXFile::group(FXuint gid){
    FXchar result[64];
  #ifndef WIN32
@@ -27,3 +36,12 @@
    ::group grpresult;
    ::group *grp;
    char buffer[1024];
+@@ -1435,7 +1435,7 @@
+ // Convert file time to string as per strftime format
+ FXString FXFile::time(const FXchar *format,FXTime filetime){
+ #ifndef WIN32
+-#ifdef FOX_THREAD_SAFE
++#if defined(FOX_THREAD_SAFE) && !defined(__FreeBSD__)
+   time_t tmp=(time_t)FXMAX(filetime,0);
+   struct tm tmresult;
+   FXchar buffer[512];
