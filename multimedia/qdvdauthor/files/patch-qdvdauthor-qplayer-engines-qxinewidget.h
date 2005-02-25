d13ca8fee420c45ad7d9aa448bc99392f3528771
Pav Lucistnik <pav@FreeBSD.org>
2005-02-25 23:52:10 +0000

    - Update to 0.0.9
    PR:		ports/78002
    Submitted by:	Phil Oleson <oz@nixil.net> (maintainer)

---
+++ multimedia/qdvdauthor/files/patch-qdvdauthor-qplayer-engines-qxinewidget.h
@@ -0,0 +1,10 @@
+--- qdvdauthor/qplayer/engines/qxinewidget.h.orig	Wed Feb 16 02:14:23 2005
++++ qdvdauthor/qplayer/engines/qxinewidget.h	Wed Feb 16 02:15:10 2005
+@@ -31,6 +31,7 @@
+ #include <qtimer.h>
+ #include <qevent.h>
+ #include <qthread.h>
++#include <pthread.h>
+ 
+ #include <xine.h>
+ 
