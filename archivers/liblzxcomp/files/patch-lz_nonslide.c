30612612af0beecd3ef3bce84cdb0c0e6f058728
Sergey Skvortsov <skv@FreeBSD.org>
2005-02-27 18:31:48 +0000

    Add liblzxcomp 20020618, LZX compression library.

---
+++ archivers/liblzxcomp/files/patch-lz_nonslide.c
@@ -0,0 +1,10 @@
+--- lz_nonslide.c.orig	Sun Feb 27 21:08:11 2005
++++ lz_nonslide.c	Sun Feb 27 21:08:47 2005
+@@ -19,6 +19,7 @@
+ /* 
+  * Document here
+  */
++#include <sys/types.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <assert.h>
