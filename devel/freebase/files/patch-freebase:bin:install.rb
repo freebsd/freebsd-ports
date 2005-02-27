bdcd4927a8a7da807050d8066cdfc95ccc1f003a
Volker Stolz <vs@FreeBSD.org>
2005-02-27 17:47:50 +0000

    Update to 0.9.3
    PR:		ports/77864
    Submitted by:	maintainer

--- devel/freebase/files/patch-freebase:bin:install.rb
+++ devel/freebase/files/patch-freebase:bin:install.rb
@@ -1,8 +1,40 @@
 --- bin/install.rb.orig	Mon Mar 24 07:10:57 2003
-+++ bin/install.rb	Thu Dec 16 20:42:15 2004
-@@ -8,14 +8,9 @@
++++ bin/install.rb	Mon Feb 21 18:38:00 2005
+@@ -1,21 +1,40 @@
+ require 'rbconfig'
+ require 'find'
+ require 'ftools'
++require 'getoptlong'
++
++def usage( exit_code = 1 )
++	printf "Usage: #{ARGV[0]} [--path <PATH>]\n"
++	exit( exit_code )
++end
  
- $srcdir = CONFIG["srcdir"]
+ Dir.chdir ".." if Dir.pwd =~ /bin.?$/
+ 
++opts = GetoptLong.new( ["--prefix", "-p", GetoptLong::REQUIRED_ARGUMENT ] )
++$my_prefix = nil
++opts.each { |opt, arg|
++	case opt
++	when "--prefix"
++		$my_prefix = arg
++	else
++		usage()
++	end
++}
++
++def prefixize( str )
++	$my_prefix ?
++		str.gsub( Regexp::compile( "^#{Regexp::escape($prefix)}" ), $my_prefix ) :
++		str
++end
++
+ include Config
+ 
+-$srcdir = CONFIG["srcdir"]
++$prefix = CONFIG["prefix"]
++$srcdir = prefixize( CONFIG["srcdir"] )
  $version = CONFIG["MAJOR"]+"."+CONFIG["MINOR"]
 -$libdir = File.join(CONFIG["libdir"], "ruby", $version)
 -$archdir = File.join($libdir, CONFIG["arch"])
@@ -12,9 +44,9 @@
 -elsif $site_libdir !~ Regexp.quote($version)
 -  $site_libdir = File.join($site_libdir, $version)
 -end
-+$libdir = CONFIG["rubylibdir"]
-+$archdir = CONFIG["archdir"]
-+$site_libdir = CONFIG["sitelibdir"]
++$libdir = prefixize( CONFIG["rubylibdir"] )
++$archdir = prefixize( CONFIG["archdir"] )
++$site_libdir = prefixize( CONFIG["sitelibdir"] )
  
  def install_rb(srcdir = nil)
    libdir = "lib"
