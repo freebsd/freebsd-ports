daf4cc81e4d055f630943b557402293b79680953
Michael Johnson <ahze@FreeBSD.org>
2005-02-28 22:16:10 +0000

    - split multimedia/gstreamer-plugins in to individual ports per   gstreamer plugin (52 new ports). - Chase changes to ports that used USE_GNOME=gstreamerplugisn    to use new USE_GSTREAMER= macro, Bump PORTREVISION's
    --
    
    Examples of new USE_GSTREAMER macro:
    
     USE_GSTREAMER=        dvd lame flac
     .include <bsd.port.mk>
    
     If you want to use USE_GSTREAMER after <bsd.port.pre.mk>
     you must follow one of the examples listed below
    
           WANT_GSTREAMER= yes
           .include <bsd.port.pre.mk>
           .if defined(WITH_VORBIS)
           USE_GSTREAMER+= vorbis
           .endif
    
     or
           USE_GSTREAMER=  yes
           .include <bsd.port.pre.mk>
           .if defined(WITH_FAAD)
           USE_GSTREAMER+= faad
           .endif
    
    Note: USE_GSTREAMER=yes will always add a dependency to
                   ports/multimedia/gstreamer-plugins
    
    --
    
    Thanks to krion for runing this on pointyhat
    
    Thanks to kwm for helping with this, and many bug fixes , and updates.

--- multimedia/gstreamer-plugins80/files/patch-gst_tta_gstttaparse.c
+++ multimedia/gstreamer-plugins80/files/patch-gst_tta_gstttaparse.c
@@ -1,6 +1,6 @@
---- gst/tta/gstttaparse.c.orig	Sun Jan  9 13:59:02 2005
-+++ gst/tta/gstttaparse.c	Sun Jan  9 14:02:25 2005
-@@ -162,11 +162,13 @@
+--- gst/tta/gstttaparse.c.orig	Sat Jan  8 13:36:26 2005
++++ gst/tta/gstttaparse.c	Sat Jan  8 13:37:08 2005
+@@ -162,11 +162,14 @@
      case GST_EVENT_SEEK:
      {
        if (GST_EVENT_SEEK_FORMAT (event) == GST_FORMAT_TIME) {
@@ -9,9 +9,10 @@
 -        guint64 time = GST_EVENT_SEEK_OFFSET (event);
 -        guint64 seek_frame = time / (FRAME_TIME * 1000000000);
 -        guint64 seekpos = ttaparse->index[seek_frame].pos;
-+        guint64 time;
-+        guint64 seek_frame;
-+        guint64 seekpos;
++	guint64 time;
++	guint64 seek_frame;
++	guint64 seekpos;
++        GST_DEBUG_OBJECT (ttaparse, "got seek event");
 +        time = GST_EVENT_SEEK_OFFSET (event);
 +        seek_frame = time / (FRAME_TIME * 1000000000);
 +        seekpos = ttaparse->index[seek_frame].pos;
