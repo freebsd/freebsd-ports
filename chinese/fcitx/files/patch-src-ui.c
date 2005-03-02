c2574fcb07cce4647c17254af6522f0cfb786d50
Cheng-Lung Sung <clsung@FreeBSD.org>
2005-03-02 03:21:58 +0000

    - update to 3.1 - new file: files/patch-xpm-Makefile.in
    PR:		ports/78267
    Submitted by:	Dryice Liu (maintainer)

--- chinese/fcitx/files/patch-src-ui.c
+++ chinese/fcitx/files/patch-src-ui.c
@@ -1,29 +1,29 @@
---- src/ui.c.orig	Wed Oct 27 23:19:07 2004
-+++ src/ui.c	Wed Oct 27 23:20:59 2004
-@@ -339,7 +339,7 @@
+--- src/ui.c.orig	Wed Feb 23 03:34:39 2005
++++ src/ui.c	Wed Mar  2 09:53:26 2005
+@@ -361,7 +361,7 @@
      l2 = 99;
      ps = str1;
  
 -    l1 = iconv (convUTF8, &str, &l1, &ps, &l2);
 +    l1 = iconv (convUTF8, (const char**) &str, &l1, &ps, &l2);
      *ps = '\0';
-     XftTextExtentsUtf8 (dpy, font, (FcChar8*)str1, strlen (str1), &extents);
-     if ( font!=xftFont)
-@@ -364,7 +364,7 @@
+     XftTextExtentsUtf8 (dpy, font, (FcChar8 *) str1, strlen (str1), &extents);
+     if (font == xftMainWindowFont)
+@@ -386,7 +386,7 @@
      ps2 = str2;
      ps1 = str1;
  
 -    l1 = iconv (convUTF8, &ps1, &l1, &ps2, &l2);
-+    l1 = iconv (convUTF8, (const char**)&ps1, &l1, &ps2, &l2);
-     *ps2='\0';
++    l1 = iconv (convUTF8, (const char**) &ps1, &l1, &ps2, &l2);
+     *ps2 = '\0';
  
-     XftTextExtentsUtf8 (dpy, font, (FcChar8 *)str2, strlen (str2), &extents);
-@@ -418,7 +418,7 @@
+     XftTextExtentsUtf8 (dpy, font, (FcChar8 *) str2, strlen (str2), &extents);
+@@ -439,7 +439,7 @@
+     l1 = strlen (str);
      l2 = 99;
      ps = strOutput;
- 
--    l1 = iconv (convUTF8, (char **) (&str), &l1, &ps, &l2);
-+    l1 = iconv (convUTF8, (const char **) (&str), &l1, &ps, &l2);
-     *ps='\0';
+-    l1 = iconv (convUTF8, &str, &l1, &ps, &l2);
++    l1 = iconv (convUTF8, (const char**) &str, &l1, &ps, &l2);
+     *ps = '\0';
  
      renderColor.red = color.red;
