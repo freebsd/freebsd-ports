9b0d5fc1eca05b759c64c3c0b6eade90577cd265
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2005-03-02 15:05:55 +0000

    Fix problems with AltGr-Modifier on several keyboards.
    PR:		ports/78209
    Submitted by:	Kay Lehmann <kay_lehmann@web.de>

---
+++ editors/emacs22/files/patch-src:xterm.h
@@ -0,0 +1,11 @@
+--- src/xterm.h.orig	Mon Feb 28 13:53:40 2005
++++ src/xterm.h	Mon Feb 28 13:53:57 2005
+@@ -21,8 +21,8 @@
+ 
+ #include <X11/Xlib.h>
+ #include <X11/cursorfont.h>
+-#include <X11/Xutil.h>
+ #include <X11/keysym.h>
++#include <X11/Xutil.h>
+ #include <X11/Xatom.h>
+ #include <X11/Xresource.h>
