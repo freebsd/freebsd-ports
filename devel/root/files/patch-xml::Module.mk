fc320151326a93d0d6da4e879db5a8a5a9ecc765
pav <pav@FreeBSD.org>
2005-03-02 22:17:53 +0000

    - Update to 4.02.00 - Unbreak on 5.x - Break on 4.x - Break on amd64
    PR:		ports/78043
    Submitted by:	Alexander S. Usov <usov@KVI.nl>
    Approved by:	maintainer

---
+++ devel/root/files/patch-xml::Module.mk
@@ -0,0 +1,8 @@
+--- xml/Module.mk.orig	Tue Feb 22 17:32:18 2005
++++ xml/Module.mk	Tue Feb 22 17:32:42 2005
+@@ -67,4 +67,4 @@
+ 
+ ##### extra rules ######
+ $(XMLO): %.o: %.cxx
+-	$(CXX) $(OPT) $(CXXFLAGS) -I$(XMLINCDIR) -o $@ -c $<
++	$(CXX) $(OPT) $(CXXFLAGS) -I$(XMLINCDIR) -I/usr/local/include -o $@ -c $<
