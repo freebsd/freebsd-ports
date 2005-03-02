5bfdf13569bc129447b3be3d28ee95affdc2cf56
pav <pav@FreeBSD.org>
2005-03-02 22:54:55 +0000

    - Update to 2.16.8 - Unbreak
    PR:		ports/78195
    Submitted by:	TAKATSU Tomonari <tota@rtfm.jp> (maintainer)

---
+++ japanese/bugzilla/files/patch-checksetup.pl
@@ -0,0 +1,11 @@
+--- checksetup.pl~	Fri Apr 16 02:48:51 2004
++++ checksetup.pl	Thu Jul 15 23:37:09 2004
+@@ -399,7 +399,7 @@
+ # and you cannot set this up any other way. YOU HAVE BEEN WARNED.
+ # If you set this to anything besides "", you will need to run checksetup.pl
+ # as root, or as a user who is a member of the specified group.
+-$webservergroup = "apache";
++$webservergroup = "www";
+ ');
+ 
+ 
