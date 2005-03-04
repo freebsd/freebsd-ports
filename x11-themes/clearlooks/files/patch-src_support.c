79dede27b0fc8a9dc75d286da27fc2b33e129b4d
Michael Johnson <ahze@FreeBSD.org>
2005-03-04 01:23:57 +0000

    - Update to 0.4

--- x11-themes/clearlooks/files/patch-src_support.c
+++ x11-themes/clearlooks/files/patch-src_support.c
@@ -1,7 +1,7 @@
---- src/support.c.orig	Sat Feb 19 06:50:22 2005
-+++ src/support.c	Sun Feb 27 01:44:57 2005
-@@ -503,8 +503,9 @@
- static void gtk_treeview_get_header_index (GtkWidget *tv, GtkWidget *header,
+--- src/support.c.orig	Wed Mar  2 13:09:43 2005
++++ src/support.c	Thu Mar  3 20:05:51 2005
+@@ -507,8 +507,9 @@
+ void gtk_treeview_get_header_index (GtkWidget *tv, GtkWidget *header,
                                             gint *column_index, gint *columns)
  {
 +	GList *list;
@@ -11,3 +11,25 @@
  
  	do
  	{
+@@ -627,16 +628,17 @@
+                 int x, int y, int width, int height,
+                 GdkColor *left_color, GdkColor *right_color)
+ {
++	int i;
++        GdkColor col;
++        int dr, dg, db;
++        GdkGCValues old_values;
++
+ 	if ( left_color == NULL || right_color == NULL )
+ 	{
+ 		gdk_draw_rectangle (drawable, gc, TRUE, x, y, width, height);
+ 		return;
+ 	}
+ 
+-	int i;
+-	GdkColor col;
+-	int dr, dg, db;
+-	GdkGCValues old_values;
+ 
+ 	gdk_gc_get_values (gc, &old_values);
+ 	
