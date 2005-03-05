f513051768f07568c388c7ef1f4b8387318c94d8
thierry <thierry@FreeBSD.org>
2005-03-05 22:09:18 +0000

    Upgrade to to 2.3-20050304.
    PR:		ports/78447
    Submitted by:	Marcus Grando (maintainer)

---
+++ mail/postfix-current/files/patch-src::global::mail_version.h
@@ -0,0 +1,11 @@
+--- src/global/mail_version.h.orig	Sat Mar  5 11:50:13 2005
++++ src/global/mail_version.h	Sat Mar  5 11:50:23 2005
+@@ -21,7 +21,7 @@
+   * release date only.
+   */
+ #define MAIL_RELEASE_DATE	"20050304"
+-#define MAIL_VERSION_NUMBER	"2.2"
++#define MAIL_VERSION_NUMBER	"2.3"
+ 
+ #define VAR_MAIL_VERSION	"mail_version"
+ #ifdef SNAPSHOT
