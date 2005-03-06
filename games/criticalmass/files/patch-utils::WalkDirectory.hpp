6524edf83cbdb3b8e9643c9cba5664cfddfa8759
Pav Lucistnik <pav@FreeBSD.org>
2005-03-06 21:34:43 +0000

    - Update to 0.9.10
    PR:		ports/78499
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ games/criticalmass/files/patch-utils::WalkDirectory.hpp
@@ -0,0 +1,10 @@
+--- utils/WalkDirectory.hpp.orig	Sun Dec 19 15:18:06 2004
++++ utils/WalkDirectory.hpp	Sun Mar  6 08:49:51 2005
+@@ -16,6 +16,7 @@
+ #define _WalkDirectory_hpp_
+ 
+ #include <iostream>
++#include <string>
+ using namespace std;
+ 
+ #ifdef VCPP
