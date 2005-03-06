1ea3dfc1be205dfda389cc967c251af4a17e9e44
Pav Lucistnik <pav@FreeBSD.org>
2005-03-06 23:32:23 +0000

    - Update to RC2A snapshot of 20050302.1715
    PR:		ports/78418
    Submitted by:	Ion-Mihai "IOnut" Tetcu <itetcu@people.tecnik93.com> (maintainer)

--- mail/dspam-devel/files/patch-dspam.c
+++ mail/dspam-devel/files/patch-dspam.c
@@ -20,47 +20,4 @@
 +//      }  // end of FreeBSD patch
  
        if (!strcmp(_ds_pref_val(PTX, "showFactors"), "on")) {
- 
-@@ -2594,7 +2595,7 @@
- int embed_signature(DSPAM_CTX *CTX, AGENT_CTX *ATX, agent_pref_t PTX) {
-   struct nt_node *node_nt;
-   struct nt_c c_nt;
--/*  char toplevel_boundary[128] = { 0 }; */
-+  char toplevel_boundary[128] = { 0 };
-   struct _ds_message_block *block;
-   int i = 0;
- 
-@@ -2613,13 +2614,13 @@
-   if (block->media_subtype == MST_SIGNED)
-     return embed_signed(CTX, ATX, PTX);
- 
--/*
-+
-   if (block->media_type == MT_MULTIPART && block->terminating_boundary != NULL)
-   {
-     strlcpy(toplevel_boundary, block->terminating_boundary,
-             sizeof(toplevel_boundary));
-   }
--*/
-+
- 
-   while (node_nt != NULL)
-   {
-@@ -2635,16 +2636,7 @@
-         && (block->media_type == MT_TEXT
-             || (block->boundary == NULL && i == 0
-                 && block->media_type != MT_MULTIPART))
--
--        /* The part is one of the top-level parts */ 
--        && (
--/*toplevel_boundary[0] == 0 || */
--           (block->terminating_boundary 
--/*            && 
--            !strncmp(block->terminating_boundary, toplevel_boundary,
--             strlen(toplevel_boundary))   
--*/
--           )))
-+        && (toplevel_boundary[0] == 0 || block->terminating_boundary))
-     {
-       int is_attachment = 0;
-       struct _ds_header_field *field;
+
