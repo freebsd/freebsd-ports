00a4f7cfdd08a6b89921a05775e367d2ec9d8a0d
vs <vs@FreeBSD.org>
2005-03-08 12:11:30 +0000

    Unbreak (on 4.x) by picking up 'stderr' from stdio.h

---
+++ graphics/gtkcairo/files/patch-gtkcairo::gdkcairo.c
@@ -0,0 +1,10 @@
+--- gtkcairo/gdkcairo.c.orig	Tue Mar  8 13:06:20 2005
++++ gtkcairo/gdkcairo.c	Tue Mar  8 13:07:11 2005
+@@ -18,6 +18,7 @@
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+  */
+ 
++#include <stdio.h>
+ #include <gdk/gdkx.h>
+ #include "gdkcairo.h"
+ 
