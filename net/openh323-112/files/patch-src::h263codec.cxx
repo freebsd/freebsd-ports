19189c90238974eb2bed2c243627b28b0dfae867
Volker Stolz <vs@FreeBSD.org>
2005-03-08 14:14:10 +0000

    Fix breakage in the presence of ffmpeg
    PR:		ports/76931
    Patch by:	Jan Stocker

--- net/openh323-112/files/patch-src::h263codec.cxx
+++ net/openh323-112/files/patch-src::h263codec.cxx
@@ -1,7 +1,5 @@
-Index: src/h263codec.cxx
-diff -u src/h263codec.cxx.orig src/h263codec.cxx
---- src/h263codec.cxx.orig	Fri Jun  6 15:32:08 2003
-+++ src/h263codec.cxx	Sat Nov 22 23:17:45 2003
+--- src/h263codec.cxx.orig	Fri Jun  6 08:32:08 2003
++++ src/h263codec.cxx	Sat Feb 19 11:19:36 2005
 @@ -109,7 +109,7 @@
  #include "rtp.h"
  
@@ -11,3 +9,12 @@ diff -u src/h263codec.cxx.orig src/h263codec.cxx
  };
  
  
+@@ -836,7 +836,7 @@
+ }
+ 
+ 
+-void H323_H263Codec::RtpCallback(void *data, int size, int /*packetNumber*/)
++void H323_H263Codec::RtpCallback(AVCodecContext* /*s*/, void *data, int size, int /*packetNumber*/)
+ {
+   encodedPackets.AppendH263Packet(data, size);
+ }
