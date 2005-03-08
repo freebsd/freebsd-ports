e057bad313c54711d8d921be00baa7f74371fb93
Simon Barner <barner@FreeBSD.org>
2005-03-08 16:21:34 +0000

    - Update to 0.8.10 - Fix fetch - Mark broken on FreeBSD < 500039 (needs wchar support)
    PR:		ports/77815
    Submitted by:	Sam Lawrance <boris@brooknet.com.au>
    Approved by:	arved (mentor)

--- devel/libmba/files/patch-src::shellout.c
+++ devel/libmba/files/patch-src::shellout.c
@@ -1,11 +1,19 @@
---- src/shellout.c.orig	Mon Mar 10 09:10:10 2003
-+++ src/shellout.c	Tue Apr 15 12:46:00 2003
-@@ -27,7 +27,7 @@
+--- src/shellout.c.orig	Fri May 28 23:52:03 2004
++++ src/shellout.c	Tue Mar  8 16:56:58 2005
+@@ -27,12 +27,15 @@
  #include <string.h>
  #include <termios.h>
  #include <unistd.h>
 -#include <pty.h>
-+/* #include <pty.h> */
  #include <errno.h>
  #include <sys/types.h>
++#include <sys/ioctl.h>
  #include <sys/wait.h>
+ #include <sys/time.h>
++#include <sys/select.h>
+ #include <signal.h>
++#include <termios.h>
++#include <libutil.h>
+ #include "mba/msgno.h"
+ #include "mba/text.h"
+ #include "mba/shellout.h"
