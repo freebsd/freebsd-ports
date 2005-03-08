57bceb91413cd3f273098664d22093631117246f
edwin <edwin@FreeBSD.org>
2005-03-08 11:29:47 +0000

    Extract patch in patch and use that patch to patch instead of patching with a patch which doesn't patch the file to patch.
    Noticed by: Dmitry Morozovsky <marck@FreeBSD.org>
    Pointy hat: me!

--- games/einstein/files/patch-random.cpp
+++ games/einstein/files/patch-random.cpp
@@ -1,14 +1,8 @@
---- /dev/null	1 Jan 1970 00:00:00 -0000
-+++ files/patch-random.cpp	7 Mar 2005 16:00:23 -0000
-@@ -0,0 +1,11 @@
+--- random.cpp.orig
++++ random.cpp
+@@ -1,3 +1,5 @@
++#include "sys/time.h"
 +
-+$FreeBSD$
-+
-+--- random.cpp.orig
-++++ random.cpp
-+@@ -1,3 +1,5 @@
-++#include "sys/time.h"
-++
-+ #include "random.h"
-+ #include "utils.h"
-+
+ #include "random.h"
+ #include "utils.h"
+
