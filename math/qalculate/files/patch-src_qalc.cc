92fb09df2b0525d049e6579f3ee44df2f6577d69
Michael Johnson <ahze@FreeBSD.org>
2005-03-11 22:20:47 +0000

    - Update to 0.7.2
    PR:		ports/78696
    Submitted by:	Sergey Akifyev <asa@agava.com> (maintainer)

---
+++ math/qalculate/files/patch-src_qalc.cc
@@ -0,0 +1,11 @@
+--- src/qalc.cc.orig	Fri Mar 11 14:50:18 2005
++++ src/qalc.cc	Fri Mar 11 14:50:31 2005
+@@ -16,7 +16,7 @@
+ #include <time.h>
+ #include <pthread.h>
+ #include <dirent.h>
+-#include <malloc.h>
++#include <stdlib.h>
+ #include <stdio.h>
+ #include <vector>
+ #include <glib.h>
