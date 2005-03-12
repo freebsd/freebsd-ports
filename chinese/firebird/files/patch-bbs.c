591e3b8a1aa6958d5144e9304b44777d7e6227f6
vanilla <vanilla@FreeBSD.org>
2005-03-12 03:04:48 +0000

    s/varargs.h/stdarg.h/g
    Submitted by:	chinsan <chinsan@mail2000.com.tw>
    Approved by:	maintainer.

---
+++ chinese/firebird/files/patch-bbs.c
@@ -0,0 +1,12 @@
+--- src/bbs.c.orig	Tue Mar  8 02:34:20 2005
++++ src/bbs.c	Tue Mar  8 02:33:11 2005
+@@ -25,8 +25,7 @@
+ 
+ #ifndef lint
+ static const char copyright[] =
+-"@(#) Copyright (c) 1999,2000\n
+-	Firebird BBS Project. All rights reserved. http://www.firebird.org.tw\n";
++"@(#) Copyright (c) 1999,2000\n	Firebird BBS Project. All rights reserved. http://www.firebird.org.tw\n";
+ #endif
+ #include "bbs.h"
+ #include <time.h>
---
+++ chinese/firebird/files/patch-paging.c
@@ -0,0 +1,11 @@
+--- src/SO/paging.c.orig	Fri Sep 22 23:14:03 2000
++++ src/SO/paging.c	Tue Mar  8 03:24:43 2005
+@@ -351,7 +351,7 @@
+ 		case '0':
+ 			return 0;
+ 		default:
+-
++			return 0;
+ 		}
+ 	}
+ 
