a2bc1c2c4c0a84265208dc47cf38798043f21256
Maho Nakata <maho@FreeBSD.org>
2005-03-12 16:20:30 +0000

    update to 1.9m84

--- editors/openoffice.org-2.0/files/patch-extensions+source+config+ldap+makefile.mk
+++ editors/openoffice.org-2.0/files/patch-extensions+source+config+ldap+makefile.mk
@@ -1,19 +1,20 @@
 o llber50 is missing
 o http://www.openoffice.org/issues/show_bug.cgi?id=43037
 
-Index: extensions/source/config/ldap/makefile.mk
+Index: makefile.mk
 ===================================================================
 RCS file: /cvs/util/extensions/source/config/ldap/makefile.mk,v
-retrieving revision 1.5
-diff -u -r1.5 makefile.mk
---- extensions/source/config/ldap/makefile.mk	18 Jan 2005 15:18:56 -0000	1.5
-+++ extensions/source/config/ldap/makefile.mk	17 Feb 2005 23:21:16 -0000
-@@ -117,7 +117,7 @@
-         $(SALHELPERLIB) \
+retrieving revision 1.4.24.2
+diff -u -u -r1.4.24.2 makefile.mk
+--- extensions/source/config/ldap/makefile.mk	27 Jan 2005 19:26:59 -0000	1.4.24.2
++++ extensions/source/config/ldap/makefile.mk	18 Feb 2005 08:21:16 -0000
+@@ -114,6 +114,9 @@
          $(SALLIB)
  .IF "$(OS)"=="FREEBSD"
--SHL1STDLIBS+=-lcompat
-+SHL1STDLIBS+=-lcompat -llber50
+ SHL1STDLIBS+=-lcompat
++.IF "$(WITH_OPENLDAP)" != "YES"
++SHL1STDLIBS+=-llber50
++.ENDIF
  .ENDIF
  
  DEF1NAME=$(SHL1TARGET)
