228d29bd0628b5021fab6ff082bd1ac8bbdd3779
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-03-12 11:09:11 +0000

    Update to 0.7.6.
    PR:		78697
    Submitted by:	maintainer

--- security/seahorse/files/patch-agent_seahorse-agent.c
+++ security/seahorse/files/patch-agent_seahorse-agent.c
@@ -1,6 +1,27 @@
---- agent/seahorse-agent.c.orig	Mon Jan 31 11:14:02 2005
-+++ agent/seahorse-agent.c	Mon Jan 31 11:14:48 2005
-@@ -309,7 +309,6 @@
+--- agent/seahorse-agent.c.orig	Sun Feb 13 19:43:53 2005
++++ agent/seahorse-agent.c	Fri Mar 11 22:20:26 2005
+@@ -265,7 +265,6 @@
+     int ch = 0;
+     const char *sockname;
+ 
+-    secmem_init (65536);
+ 
+     /* We need to drop privileges completely for security */
+ #if defined(HAVE_SETRESUID) && defined(HAVE_SETRESGID)
+@@ -273,10 +272,12 @@
+     /* Not in header files for all OSs, even where present */
+     int setresuid(uid_t ruid, uid_t euid, uid_t suid);
+     int setresgid(gid_t rgid, gid_t egid, gid_t sgid);
++    secmem_init (65536);
+   
+     if (setresuid (getuid (), getuid (), getuid ()) == -1 ||
+         setresgid (getgid (), getgid (), getgid ()) == -1)
+ #else
++    secmem_init (65536);
+     if (setuid (getuid ()) == -1 || setgid (getgid ()) == -1)
+ #endif
+         err (1, _("couldn't drop privileges properly"));
+@@ -320,7 +321,6 @@
       * All functions after this point have to print messages 
       * nicely and not just called exit() 
       */
@@ -8,7 +29,7 @@
  
      /* Handle some signals */
      signal (SIGINT, on_quit);
-@@ -322,6 +321,8 @@
+@@ -333,6 +333,8 @@
      /* Initialize our sub systems */
      seahorse_agent_actions_init ();
      seahorse_agent_cache_init ();
