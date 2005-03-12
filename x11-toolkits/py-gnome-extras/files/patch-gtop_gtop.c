0e9806a48fef1d089265bef4404a64ed6878b42a
marcus <marcus@FreeBSD.org>
2005-03-12 11:18:20 +0000

    Add py-gnome-extras, a set of Python bindings for additional GNOME components previously wrapped by py-gnome2.  These include libgtop2, gtkspell2, totem, mozilla, and nautilus-cd-burner.

---
+++ x11-toolkits/py-gnome-extras/files/patch-gtop_gtop.c
@@ -0,0 +1,10 @@
+--- gtop/gtop.c.orig	Fri Jan 28 14:09:00 2005
++++ gtop/gtop.c	Fri Jan 28 14:09:18 2005
+@@ -43,6 +43,7 @@
+ 
+ #include <sys/types.h>
+ #include <sys/socket.h>
++#include <netinet/in.h>
+ #include <arpa/inet.h>
+ 
+ 
