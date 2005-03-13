c4d86504397afcc47b5b034bd346381482c44c9f
vanilla <vanilla@FreeBSD.org>
2005-03-13 17:30:30 +0000

    Upgrade to 0.8.8.
    Approved by:	maintainer

--- chinese/gcin-devel/files/patch-im-srv.c
+++ chinese/gcin-devel/files/patch-im-srv.c
@@ -1,11 +1,14 @@
 --- im-srv/im-srv.c.orig	Mon Feb 14 10:18:04 2005
-+++ im-srv/im-srv.c	Wed Mar  2 02:49:58 2005
-@@ -121,7 +121,7 @@
++++ im-srv/im-srv.c	Fri Mar 11 01:43:51 2005
+@@ -121,7 +121,11 @@
    char sock_path[128];
    get_gcin_im_srv_sock_path(sock_path, sizeof(sock_path));
    strcpy(serv_addr.sun_path, sock_path);
--  servlen = strlen(serv_addr.sun_path) + sizeof(serv_addr.sun_family);
-+  servlen = strlen(serv_addr.sun_path) + sizeof(serv_addr.sun_family) + 1;
++#ifdef	SUN_LEN
++  servlen = SUN_LEN (&serv_addr);
++#else
+   servlen = strlen(serv_addr.sun_path) + sizeof(serv_addr.sun_family);
++#endif
  
    dbg("-- %s\n",serv_addr.sun_path);
    struct stat st;
