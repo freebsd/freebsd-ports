be10a1ceecab423d076ffa144580029e7797f954
Kirill Ponomarev <krion@FreeBSD.org>
2005-03-13 09:35:39 +0000

    Update to version 1.2.2
    PR:		ports/78740
    Submitted by:	Ports Fury

---
+++ mail/renattach/files/patch-src::renattach.c
@@ -0,0 +1,11 @@
+--- src/renattach.c.orig	Wed Oct  6 13:22:24 2004
++++ src/renattach.c	Mon Mar  7 23:36:42 2005
+@@ -26,7 +26,7 @@
+ #include "renattach.h"
+ #include "strings-en.h"
+ #include "utility.h"
+-#include "getopt.h"
++#include <getopt.h>
+ #include <memory.h>
+ #include <stdio.h>
+ #include <stdlib.h>
