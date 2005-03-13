3b081a37ddcc8de272f5a17891c64ac78fd8e36c
krion <krion@FreeBSD.org>
2005-03-13 09:29:48 +0000

    Update to version 0.7.1
    PR:		ports/78738
    Submitted by:	Ports Fury

--- mail/filtermail/files/patch-src::preferences.cc
+++ mail/filtermail/files/patch-src::preferences.cc
@@ -1,7 +1,7 @@
---- src/preferences.cc.orig	Sun Jan 25 21:56:46 2004
-+++ src/preferences.cc	Fri Mar 19 23:16:27 2004
-@@ -34,7 +34,7 @@
- #include "rcfile.hh"
+--- src/preferences.cc.orig	Sat Nov 20 23:39:15 2004
++++ src/preferences.cc	Mon Mar  7 22:58:20 2005
+@@ -35,7 +35,7 @@
+ 
  extern "C"
  {
 -#include <wordexp.h>
@@ -9,43 +9,43 @@
  #include <sys/types.h>
  #include <regex.h>
  }
-@@ -230,15 +230,15 @@
-   void Preferences :: set_headers_file (const char* name)
-   {
-     // Expand the given file name.
--    wordexp_t result;
--    if (wordexp (name, &result, 0) == 0)
--      headers_file_name = result.we_wordv[0];
-+    glob_t result;
-+    if (glob (name, 0, NULL, &result) == 0)
-+      headers_file_name = result.gl_pathv[0];
-     else
-       {
- 	ERROR_MSG(_("Invalid file name: ") + (string)name + ".");
- 	exit (-1);
-       }
--    wordfree (&result);
-+    globfree (&result);
-   }
+@@ -233,15 +233,15 @@
+ void Preferences :: set_headers_file (const char* name)
+ {
+   // Expand the given file name.
+-  wordexp_t result;
+-  if (wordexp (name, &result, 0) == 0)
+-    headers_file_name = result.we_wordv[0];
++  glob_t result;
++  if (glob (name, 0, NULL, &result) == 0)
++    headers_file_name = result.gl_pathv[0];
+   else
+     {
+       ERROR_MSG(_("Invalid file name: ") + (string)name + ".");
+       exit (-1);
+     }
+-  wordfree (&result);
++  globfree (&result);
+ }
  
-   string Preferences :: headers_file (void)
-@@ -256,15 +256,15 @@
-     // command line.
-     if (log_file_name.length () == 0)
-       {
--	wordexp_t result;
--	if (wordexp (name, &result, 0) == 0)
--	  log_file_name = result.we_wordv[0];
-+	glob_t result;
-+	if (glob (name, 0, NULL, &result) == 0)
-+	  log_file_name = result.gl_pathv[0];
- 	else
- 	  {
- 	    ERROR_MSG(_("Invalid file name: ") + (string)name + ".");
- 	    exit (-1);
- 	  }
--	wordfree (&result);
-+	globfree (&result);
-       }
-   }
+ string Preferences :: headers_file (void)
+@@ -259,15 +259,15 @@
+   // command line.
+   if (log_file_name.length () == 0)
+     {
+-      wordexp_t result;
+-      if (wordexp (name, &result, 0) == 0)
+-	log_file_name = result.we_wordv[0];
++      glob_t result;
++      if (glob (name, 0, NULL, &result) == 0)
++	log_file_name = result.gl_pathv[0];
+       else
+ 	{
+ 	  ERROR_MSG(_("Invalid file name: ") + (string)name + ".");
+ 	  exit (-1);
+ 	}
+-      wordfree (&result);
++      globfree (&result);
+     }
+ }
  
