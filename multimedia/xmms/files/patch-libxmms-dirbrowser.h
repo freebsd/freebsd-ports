a99de201d6e49b3b917d8d8cdb2351626f57d0f3
flz <flz@FreeBSD.org>
2005-03-14 18:22:31 +0000

    - Added patches to enable 3rd party C++ apps to compile with gcc 3.4.
    PR:		ports/78831
    Submitted by:	Espen Skoglund <esk@ira.uka.de> (maintainer)

---
+++ multimedia/xmms/files/patch-libxmms-dirbrowser.h
@@ -0,0 +1,11 @@
+--- libxmms/dirbrowser.h.orig	Mon Mar 14 18:08:23 2005
++++ libxmms/dirbrowser.h	Mon Mar 14 18:08:30 2005
+@@ -27,7 +27,7 @@
+ GtkWidget *xmms_create_dir_browser(gchar * title, gchar * current_path, GtkSelectionMode mode, void (*handler) (gchar *));
+ 
+ #ifdef __cplusplus
+-};
++}
+ #endif
+ 
+ 
---
+++ multimedia/xmms/files/patch-libxmms-titlestring.h
@@ -0,0 +1,11 @@
+--- libxmms/titlestring.h.orig	Mon Mar 14 18:05:21 2005
++++ libxmms/titlestring.h	Mon Mar 14 18:05:31 2005
+@@ -80,7 +80,7 @@
+ GtkWidget* xmms_titlestring_descriptions(char* tags, int rows);
+ 
+ #ifdef __cplusplus
+-};
++}
+ #endif
+ 
+ #endif /* !XMMS_TITLESTRING_H */
