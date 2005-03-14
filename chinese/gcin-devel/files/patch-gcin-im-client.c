ff1dcebb0794755a3cf62387c26d34acade16606
Vanilla I. Shu <vanilla@FreeBSD.org>
2005-03-14 16:29:51 +0000

    s/setgrp/setpgrp/, bump version.
    Submitted by:	chinsan <chinsan@mail2000.com.tw>
    Approved by:	maintainer

--- chinese/gcin-devel/files/patch-gcin-im-client.c
+++ chinese/gcin-devel/files/patch-gcin-im-client.c
@@ -1,14 +1,15 @@
 --- im-client/gcin-im-client.c.orig	Sat Feb 26 10:39:12 2005
-+++ im-client/gcin-im-client.c	Wed Mar  2 02:54:55 2005
++++ im-client/gcin-im-client.c	Tue Mar 15 00:15:47 2005
 @@ -68,11 +68,7 @@
        save_old_sigaction_single(SIGCHLD, &ori_act);
  
        if ((pid=fork())==0) {
 -#if 	FREEBSD
-         setgrp();
+-        setgrp();
 -#else
 -        setpgrp();
 -#endif
++        setpgrp(0, 0);
          execl(execbin, "gcin", NULL);
        } else {
          sleep(1);
