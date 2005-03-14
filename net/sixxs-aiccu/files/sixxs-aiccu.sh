b5ce33b72e5b83e156fa15a759ad9e545b5e6412
vs <vs@FreeBSD.org>
2005-03-14 17:47:08 +0000

    Add sixxs-aiccu 20050131, sixXS IPv6 TIC+ tunnel broker heartbeat client.
    This is the TIC+ heartbeart client for the public dynamic-IPv4
    IPv6 tunnel beta test from the SixXS tunnel service provider.
    
    WWW: http://www.sixxs.net/tools/aiccu/
    
    PR:		ports/71462
    Submitted by:	Meno Abels <meno.abels AT adviser.com>

---
+++ net/sixxs-aiccu/files/sixxs-aiccu.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+# PROVIDE: sixxs-aiccu
+# REQUIRE: NETWORKING
+# KEYWORD: FreeBSD
+
+#
+# Add the following lines to /etc/rc.conf to enable sixxs-aiccu:
+#
+#sixxs_aiccu_enable="YES"
+#
+
+. %%RC_SUBR%%
+
+name=sixxs_aiccu
+rcvar=`set_rcvar`
+
+command="%%PREFIX%%/sbin/sixxs-aiccu start"
+command_args=%%PREFIX%%/etc/aiccu.conf
+stop_cmd="%%PREFIX%%/sbin/sixxs-aiccu stop"
+required_files=%%PREFIX%%/etc/aiccu.conf
+
+# set default
+sixxs_aiccu_enable=${sixxs_aiccu_enable:-"NO"}
+
+load_rc_config ${name}
+run_rc_command "$1"
