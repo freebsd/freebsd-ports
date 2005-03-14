5dba860e88d01c5800fa7d93dd84016280d0a593
Florent Thoumie <flz@FreeBSD.org>
2005-03-14 18:22:31 +0000

    - Added patches to enable 3rd party C++ apps to compile with gcc 3.4.
    PR:		ports/78831
    Submitted by:	Espen Skoglund <esk@ira.uka.de> (maintainer)

---
+++ multimedia/xmms/files/patch-libxmms-configfile.h
@@ -0,0 +1,11 @@
+--- libxmms/configfile.h.orig	Mon Mar 14 18:06:38 2005
++++ libxmms/configfile.h	Mon Mar 14 18:06:55 2005
+@@ -66,7 +66,7 @@
+ void xmms_cfg_remove_key(ConfigFile * cfg, gchar * section, gchar * key);
+ 
+ #ifdef __cplusplus
+-};
++}
+ #endif
+ 
+ #endif
