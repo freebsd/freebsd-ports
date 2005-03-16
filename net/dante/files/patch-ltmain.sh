ba7eeaf25f38ef6da29af9f53ce6f1ca30e18217
Anders Nordby <anders@FreeBSD.org>
2005-03-16 21:14:26 +0000

    Update to 1.1.15.

--- net/dante/files/patch-ltmain.sh
+++ net/dante/files/patch-ltmain.sh
@@ -1,47 +1,6 @@
---- ltmain.sh.orig	Sun Feb 20 11:57:03 2005
-+++ ltmain.sh	Sun Feb 20 12:04:33 2005
-@@ -1052,14 +1052,14 @@
- 	    # These systems don't actually have a C library (as such)
- 	    test "X$arg" = "X-lc" && continue
- 	    ;;
--	  *-*-openbsd*)
-+	  *-*-openbsd* | *-*-freebsd4*)
- 	    # Do not include libc due to us having libc/libc_r.
- 	    test "X$arg" = "X-lc" && continue
- 	    ;;
- 	  esac
- 	 elif test "X$arg" = "X-lc_r"; then
- 	  case $host in
--	  *-*-openbsd*)
-+	  *-*-openbsd* | *-*-freebsd4*)
- 	    # Do not include libc_r directly, use -pthread flag.
- 	    continue
- 	    ;;
-@@ -1069,6 +1069,13 @@
- 	continue
- 	;;
- 
-+      -pthread)
-+	compile_command="$compile_command -pthread"
-+	finalize_command="$finalize_command -pthread"
-+	compiler_flags="$compiler_flags -pthread"
-+	continue
-+	;;
-+
-       -module)
- 	module=yes
- 	continue
-@@ -2453,6 +2460,9 @@
- 	  *-*-openbsd*)
- 	    # Do not include libc due to us having libc/libc_r.
- 	    ;;
-+	  *-*-freebsd*)
-+	    # FreeBSD doesn't need this...
-+	    ;;
- 	  *)
- 	    # Add libc to deplibs on all other systems if necessary.
- 	    if test $build_libtool_need_lc = "yes"; then
-@@ -4219,10 +4229,12 @@
+--- ltmain.sh.orig	Wed Mar 16 21:57:42 2005
++++ ltmain.sh	Wed Mar 16 21:59:55 2005
+@@ -5540,10 +5540,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
