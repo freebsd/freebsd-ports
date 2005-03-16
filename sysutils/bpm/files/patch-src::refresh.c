6b11a1e44d5aba79eddaea34f978c43319d66c34
vs <vs@FreeBSD.org>
2005-03-16 13:20:55 +0000

    Add patch to work on systems newer than 500036
    PR:		ports/74916
    Submitted by:	Joel Diaz
    Approved by:	maintainer

--- sysutils/bpm/files/patch-src::refresh.c
+++ sysutils/bpm/files/patch-src::refresh.c
@@ -1,10 +1,12 @@
---- src/refresh.c.orig	Tue Aug 24 02:40:08 2004
-+++ src/refresh.c	Tue Oct 12 12:31:32 2004
-@@ -62,7 +62,11 @@
+--- src/refresh.c.orig	Mon Aug 23 22:40:08 2004
++++ src/refresh.c	Fri Dec 10 08:58:03 2004
+@@ -62,7 +62,13 @@
  __RCSID("$Id: refresh.c,v 1.32 2004/08/24 02:40:08 sethk Exp $");
  
  /* XXX use libinstall/lib.h */
-+#if __FreeBSD_version >= 500036
++#if __FreeBSD_version >= 600000
++#define	INDEX_FN    PORTS_BASE "/INDEX-6"
++#elif __FreeBSD_version >= 500036
 +#define	INDEX_FN    PORTS_BASE "/INDEX-5"
 +#else
  #define	INDEX_FN    PORTS_BASE "/INDEX"
@@ -12,7 +14,7 @@
  #define INDEX_MAXLN 4096
  #define DB_DIR	    "/var/db/pkg"
  #define ITER_BYTES  (128 * 1024)
-@@ -183,10 +187,12 @@
+@@ -183,10 +189,12 @@
  	if (*sp == '|')
  	    ++i;
  
@@ -27,7 +29,7 @@
  #endif /* DEBUG */
  	return FALSE;
      }
-@@ -195,12 +201,9 @@
+@@ -195,12 +203,9 @@
  
      for (i = 0; i < PFIELD_NFIELD; ++i)
      {
@@ -43,7 +45,7 @@
  
  	switch (i)
  	{
-@@ -320,7 +323,6 @@
+@@ -320,7 +325,6 @@
  
  	if (_refresh_index_parse(ln, portdp, rdp) != TRUE)
  	{
