265e2a37edcd45c71ea0f78e9b0f99ec9ed6acf0
Ying-Chieh Liao <ijliao@FreeBSD.org>
2005-03-18 03:09:41 +0000

    add libpeak 0.1.0 Parallel Events and Asynchronous tasKing Library

---
+++ devel/libpeak/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Mar 18 10:55:56 2005
++++ ltmain.sh	Fri Mar 18 10:56:06 2005
+@@ -5580,10 +5580,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
