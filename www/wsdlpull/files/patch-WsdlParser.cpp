8faa8492cc2d26a6479b0f302c806679d1735105
ijliao <ijliao@FreeBSD.org>
2005-03-18 04:19:55 +0000

    add wsdlpull 1.6.1 C++ WSDL Parser

---
+++ www/wsdlpull/files/patch-WsdlParser.cpp
@@ -0,0 +1,46 @@
+--- src/wsdlparser/WsdlParser.cpp.orig	Fri Mar 18 11:50:47 2005
++++ src/wsdlparser/WsdlParser.cpp	Fri Mar 18 11:52:37 2005
+@@ -343,11 +343,11 @@
+         string tag = xParser->getName();
+         switch (event_type)
+         {
+-            case xParser->START_DOCUMENT:
++            case XmlPullParser::START_DOCUMENT:
+                 if (m_State != START)
+                     error("Syntax error at the start");
+                 break;
+-            case xParser->START_TAG:
++            case XmlPullParser::START_TAG:
+                 if (xParser->getNamespace() != wsdlUri
+                     &&xParser->getNamespace() != SchemaUri)
+                     m_State = EXTENSIBILITY;
+@@ -403,7 +403,7 @@
+                 else
+                     error("Unknown Tag " + tag);
+                 break;
+-            case xParser->END_TAG:
++            case XmlPullParser::END_TAG:
+                 if (tag == "definitions")
+                     m_State = END;
+ 
+@@ -424,14 +424,14 @@
+                         return peek(lookahead);   //get the next tag
+                 }
+                 break;
+-            case xParser->TEXT:
+-            case xParser->ENTITY_REF:
+-            case xParser->COMMENT:
+-            case xParser->PROCESSING_INSTRUCTION:
+-            case xParser->CDSECT:
++            case XmlPullParser::TEXT:
++            case XmlPullParser::ENTITY_REF:
++            case XmlPullParser::COMMENT:
++            case XmlPullParser::PROCESSING_INSTRUCTION:
++            case XmlPullParser::CDSECT:
+                 xParser->getText();
+                 break;
+-            case xParser->DOCDECL:
++            case XmlPullParser::DOCDECL:
+                 error("Doc Declaration ??");
+                 break;
+             default:
---
+++ www/wsdlpull/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Mar 18 11:34:55 2005
++++ ltmain.sh	Fri Mar 18 11:35:05 2005
+@@ -5527,10 +5527,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
