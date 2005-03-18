5c91585b596d3d1cfa3a2e83583d80b98fd1b02d
Sergey Matveychuk <sem@FreeBSD.org>
2005-03-18 08:03:53 +0000

    - Update to 21.5.b20 "cilantro". - Fixed option WITH_GTK(bug, reported by Palle Girgensohn <girgen@pingpong.net>)
    PR:		ports/78960
    Submitted by:	maintainer

--- Mk/bsd.emacs.mk
+++ Mk/bsd.emacs.mk
@@ -1,5 +1,5 @@
 #
-#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.39 2005-02-26 06:21:34 sem Exp $
+#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.40 2005-03-18 08:03:53 sem Exp $
 #
 #	bsd.emacs.mk - 19990829 Shigeyuki Fukushima.
 #
@@ -183,7 +183,7 @@ PLIST?=                 ${PKGDIR}/pkg-plist.${EMACS_PORT_NAME}
 # XEmacs-21 development version with Mule
 .elif (${EMACS_PORT_NAME} == "xemacs-devel-mule")
 EMACS_NAME=		xemacs
-EMACS_VER=		21.5-b19
+EMACS_VER=		21.5-b20
 EMACS_MAJOR_VER=	21
 EMACS_LIBDIR?=		lib/${EMACS_NAME}
 EMACS_LIBDIR_WITH_VER?=	lib/${EMACS_NAME}-${EMACS_VER}
