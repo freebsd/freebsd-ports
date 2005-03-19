092324072dc928b97ba712e3cd787be0715308e2
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2005-03-19 16:44:02 +0000

    Fix build on powerpc.
    PR:		ports/78577
    Submitted by:	Martin Minkus <diskiller@diablo.diskiller.net>

--- devel/boehm-gc/files/patch-include-private-gcconfig.h
+++ devel/boehm-gc/files/patch-include-private-gcconfig.h
@@ -1,6 +1,6 @@
 --- include/private/gcconfig.h.orig	Sat Dec 18 09:20:16 2004
-+++ include/private/gcconfig.h	Mon Jan  3 19:49:44 2005
-@@ -328,6 +328,10 @@
++++ include/private/gcconfig.h	Sun Mar 20 01:37:33 2005
+@@ -328,10 +328,18 @@
  #    define X86_64
  #    define mach_type_known
  # endif
@@ -11,7 +11,33 @@
  # if defined(FREEBSD) && defined(__sparc__)
  #    define SPARC
  #    define mach_type_known
-@@ -1939,6 +1943,15 @@
+-#endif
++# endif
++# if defined(FREEBSD) && defined(__powerpc__)
++#    define POWERPC
++#    define mach_type_known
++# endif
+ # if defined(bsdi) && (defined(i386) || defined(__i386__))
+ #    define I386
+ #    define BSDI
+@@ -811,6 +819,16 @@
+ #     define DATASTART GC_data_start
+ #     define DYNAMIC_LOADING
+ #   endif
++#   ifdef FREEBSD
++#   define ALIGNMENT 4
++#       define OS_TYPE "FREEBSD"
++#       ifdef __ELF__
++#           define DYNAMIC_LOADING
++#       endif
++#	define HEURISTIC2
++	extern char etext[];
++#	define SEARCH_FOR_DATA_START
++#   endif
+ #   ifdef NOSYS
+ #     define ALIGNMENT 4
+ #     define OS_TYPE "NOSYS"
+@@ -1939,6 +1957,15 @@
  #	ifdef __ELF__
  #	    define DYNAMIC_LOADING
  #	endif
