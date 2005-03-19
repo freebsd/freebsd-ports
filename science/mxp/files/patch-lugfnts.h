c1cdfbce71cc7d298197f0765e28e179b8614d40
maho <maho@FreeBSD.org>
2005-03-19 10:54:36 +0000

    Add mxp (Mandelbrot explorer). Mxp is an X application for computing and exploring Mandelbrot sets.  Features of mxp include:         - zoom and un-zoom         - dynamic resizing of drawing window         - setup save/load         - asynchronous image generation (buttons always work)         - GIF output         - animation         - nine color schemes         - color rotation         - color change options         - detailed statistics
    WWW:  http://www.ibiblio.org/pub/Linux/apps/math/fractals/

---
+++ science/mxp/files/patch-lugfnts.h
@@ -0,0 +1,16 @@
+--- lug/lugfnts.h.old	Sat Aug 18 13:33:46 2001
++++ lug/lugfnts.h	Sat Feb 12 16:30:29 2005
+@@ -1057,13 +1057,6 @@
+ );
+ 
+ extern int
+-isnumber(
+-#ifdef USE_PROTOTYPES
+-        char *
+-#endif
+-);
+-
+-extern int
+ Uncompress(
+ #ifdef USE_PROTOTYPES
+         char *,
