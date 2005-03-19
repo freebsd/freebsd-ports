140e9686980b9e8206def9a366e9fb7dfe90dd7e
Maho Nakata <maho@FreeBSD.org>
2005-03-19 10:28:48 +0000

    Update to 1.3
    PR:		78958
    Submitted by:	Pedro F. Giffuni <giffunip@asme.org>

--- cad/calculix/files/patch-cgx_VER+src+uselibSNL.cpp
+++ cad/calculix/files/patch-cgx_VER+src+uselibSNL.cpp
@@ -1,5 +1,5 @@
---- cgx_1.2/src/uselibSNL.cpp.orig	Sat Dec 25 01:43:08 2004
-+++ cgx_1.2/src/uselibSNL.cpp	Sat Dec 25 01:47:02 2004
+--- cgx_1.3/src/uselibSNL.cpp.orig	Sat Dec 25 01:43:08 2004
++++ cgx_1.3/src/uselibSNL.cpp	Sat Dec 25 01:47:02 2004
 @@ -1,6 +1,6 @@
  #include <extUtil.h>
  #include <snlCtrlPoint.h>
