82b08fc83b5b6da27bd342d89ddaa0f715da6456
maho <maho@FreeBSD.org>
2005-03-19 10:28:48 +0000

    Update to 1.3
    PR:		78958
    Submitted by:	Pedro F. Giffuni <giffunip@asme.org>

--- cad/calculix/files/patch-ccx_VER+src+CalculiX.h
+++ cad/calculix/files/patch-ccx_VER+src+CalculiX.h
@@ -1,5 +1,5 @@
---- ccx_1.2/src/CalculiX.h.orig	Mon Mar  3 00:35:58 2003
-+++ ccx_1.2/src/CalculiX.h	Mon May  5 21:15:50 2003
+--- ccx_1.3/src/CalculiX.h.orig	Mon Mar  3 00:35:58 2003
++++ ccx_1.3/src/CalculiX.h	Mon May  5 21:15:50 2003
 @@ -24,6 +24,8 @@
  #define FORTRAN(A,B) A##_##B
  #elif ARCH == HP
--- cad/calculix/files/patch-cgx_VER+src+cgx.h
+++ cad/calculix/files/patch-cgx_VER+src+cgx.h
@@ -1,13 +1,13 @@
---- cgx_1.2/src/cgx.h.orig	Sat Jul 17 17:17:41 2004
-+++ cgx_1.2/src/cgx.h	Thu Dec 23 15:32:58 2004
-@@ -59,8 +59,8 @@
+--- cgx_1.3/src/cgx.h.orig	Fri Feb 25 20:00:16 2005
++++ cgx_1.3/src/cgx.h	Sat Feb 26 20:45:51 2005
+@@ -63,8 +63,8 @@
  #define     MILLISECONDS_PER_PERIOD 1200 
  
  /* default location of the help files, please insert the actual one */
--#define     HELPFILE        {"/usr/local/CalculiX/cgx_1.2/doc/cgx/cgx.html",\
--    "/usr/local/CalculiX/ccx_1.2/doc/ccx/ccx.html"}
+-#define     HELPFILE        {"/usr/local/CalculiX/cgx_1.3/doc/cgx/cgx.html",\
+-    "/usr/local/CalculiX/ccx_1.3/doc/ccx/ccx.html"} 
 +#define     HELPFILE        {"%%DOCSDIR%%/cgx/cgx.html",\
-+    "%%DOCSDIR%%/ccx/ccx.html"}
++    "%%DOCSDIR%%/ccx/ccx.html"} 
  
  /* html browser, change if necessary */
  /* #define     BROWSER         {"netscape"} */
