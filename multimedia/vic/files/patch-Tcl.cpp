a5b76d8073559a26479098c53be639d16682a046
mi <mi@FreeBSD.org>
2005-03-20 23:13:45 +0000

    Upgrade devel/uclmmbase and mbone/vic to their latest respected versions. Use BSD's bsd.lib.mk to build uclmmbase and avoid building (most of the) functionality, that is present in our base libraries (btree, md5, etc.)
    In vic stop using PERL, use Tcl/Tk-8.4, respect CC and CFLAGS and some
    other improvements. vic is thus unbroken on 5.x
    
    Per fenner's (much) earlier e-mail, grab maintainership for the time
    being.
    
    PR:		ports/75066 (obsoleted)
    Approved by:	fenner (implicitly)

--- multimedia/vic/files/patch-Tcl.cpp
+++ multimedia/vic/files/patch-Tcl.cpp
@@ -1,5 +1,5 @@
---- Tcl.cpp.org	Mon May  7 15:55:17 2001
-+++ Tcl.cpp	Mon May  7 15:55:34 2001
+--- Tcl.cpp	Thu Sep  9 08:21:53 1999
++++ Tcl.cpp	Thu Sep 23 18:59:59 2004
 @@ -66,6 +66,7 @@
  {
  	instance_.tcl_ = tcl;
@@ -8,3 +8,12 @@
  }
  
  void Tcl::evalc(const char* s)
+@@ -214,7 +215,7 @@
+ 	}
+ }
+ 
+-int TclObject::callback(ClientData cd, Tcl_Interp*, int ac, char** av)
++int TclObject::callback(ClientData cd, Tcl_Interp*, int ac, const char** av)
+ {
+ 	TclObject* tc = (TclObject*)cd;
+ 	return (tc->command(ac, (const char*const*)av));
