7cd341218d611966bc3af58ebea46a62b5179f15
Mikhail Teterin <mi@FreeBSD.org>
2005-03-20 23:13:45 +0000

    Upgrade devel/uclmmbase and mbone/vic to their latest respected versions. Use BSD's bsd.lib.mk to build uclmmbase and avoid building (most of the) functionality, that is present in our base libraries (btree, md5, etc.)
    In vic stop using PERL, use Tcl/Tk-8.4, respect CC and CFLAGS and some
    other improvements. vic is thus unbroken on 5.x
    
    Per fenner's (much) earlier e-mail, grab maintainership for the time
    being.
    
    PR:		ports/75066 (obsoleted)
    Approved by:	fenner (implicitly)

--- multimedia/vic/files/patch-net-ipv6.cpp
+++ multimedia/vic/files/patch-net-ipv6.cpp
@@ -1,29 +1,10 @@
---- net/net-ipv6.cpp.org	Mon Feb  4 17:51:46 2002
-+++ net/net-ipv6.cpp	Mon Feb  4 17:52:08 2002
-@@ -277,7 +277,7 @@
-   p->sin6_family = AF_INET6;
-   int len = sizeof(*p), result = 0;
+--- net/net-ipv6.cpp	Thu Apr 24 10:53:26 2003
++++ net/net-ipv6.cpp	Thu Sep 23 19:08:53 2004
+@@ -59,7 +59,6 @@
+ #include "config.h"
+ #include "net.h"
+ #include "vic_tcl.h"
+-#include "inet_ntop.h"
  
--  if ((result = getsockname(ssock_, (struct sockaddr *)p, &len)) < 0) {
-+  if ((result = getsockname(ssock_, (struct sockaddr *)p, (socklen_t *)&len)) < 0) {
-     perror("getsockname");
-     p->sin6_addr = in6addr_any;
-     p->sin6_port = 0;
-@@ -364,7 +364,7 @@
- 		 * with bated breath.
- 		 */
- #if defined(__FreeBSD__)
--                struct oipv6_mreq mr;
-+                struct ipv6_mreq mr;
- #else
-                 struct ipv6_mreq mr;
- #endif
-@@ -522,7 +522,7 @@
- 	sockaddr_in6 sfrom;
- 	int fromlen = sizeof(sfrom);
- 	int cc = ::recvfrom(fd, (char*)buf, len, 0,
--			    (sockaddr*)&sfrom, &fromlen);
-+			    (sockaddr*)&sfrom, (socklen_t *)&fromlen);
- 	if (cc < 0) {
- 		if (errno != EWOULDBLOCK)
- 			perror("recvfrom");
+ #include "inet6.h"
+ #include "net-addr.h"
