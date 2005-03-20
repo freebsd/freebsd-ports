d5d7f1c6ff586f3679254cb96720a1159f444c34
marcus <marcus@FreeBSD.org>
2005-03-20 09:48:45 +0000

    Fix cdparanoia support so that gnome-cd can play CDs even when the CD-ROM isn't wired to the soundcard.

--- audio/gnomemedia2/files/patch-gnome-cd_gst-cdparanoia-cdrom.c
+++ audio/gnomemedia2/files/patch-gnome-cd_gst-cdparanoia-cdrom.c
@@ -1,6 +1,23 @@
---- gnome-cd/gst-cdparanoia-cdrom.c.orig	Sat Mar 12 00:09:14 2005
-+++ gnome-cd/gst-cdparanoia-cdrom.c	Sat Mar 12 00:19:15 2005
-@@ -1443,15 +1443,18 @@
+--- gnome-cd/gst-cdparanoia-cdrom.c.orig	Mon Feb  7 12:39:31 2005
++++ gnome-cd/gst-cdparanoia-cdrom.c	Sun Mar 20 04:42:32 2005
+@@ -1213,6 +1213,16 @@
+ 		*status = NULL;
+ 		return FALSE;
+ 	}
++#else
++	if (ioctl (cdrom->fd, CDIOREADTOCHEADER, priv->tochdr) < 0) {
++		realstatus->cd = GNOME_CDROM_STATUS_NO_DISC;
++		realstatus->audio = GNOME_CDROM_AUDIO_NOTHING;
++		realstatus->track = -1;
++
++		gst_cdparanoia_cdrom_close (lcd);
++		return TRUE;
++	}
++	realstatus->cd = GNOME_CDROM_STATUS_OK;
+ #endif
+ 
+ 	/* Get the volume */
+@@ -1443,15 +1453,18 @@
  		return FALSE;
  	}
  
@@ -22,7 +39,7 @@
  	if (ioctl (fd, CDIOCGETVOL, 0) < 0) {
  #else
  	if (ioctl (fd, CDROM_GET_CAPABILITY, 0) < 0) {
-@@ -1463,6 +1466,7 @@
+@@ -1463,6 +1476,7 @@
  	}
  
  	close (fd);
