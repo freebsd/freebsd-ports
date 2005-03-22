e523e55957cf8dec36590574ec161e7a86d23c64
adamw <adamw@FreeBSD.org>
2005-03-22 04:27:50 +0000

    Fix build on 4.X.
    Approved by:	portmgr (marcus)

---
+++ emulators/snes9express/files/patch-s9xskin.h
@@ -0,0 +1,10 @@
+--- s9xskin.h.orig	Mon Mar 21 23:22:23 2005
++++ s9xskin.h	Mon Mar 21 23:22:34 2005
+@@ -8,6 +8,7 @@
+ #include <cstdio>
+ #include <cstdlib>
+ #include <iostream>
++#include <sys/types.h>
+ #include <netinet/in.h>
+ #include <string>
+ #include <map>
