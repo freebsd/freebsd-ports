38ef784cb8d53d31f8bb30fde2fd7c1244b8354e
Thierry Thomas <thierry@FreeBSD.org>
2005-04-02 20:14:05 +0000

    Upgrade to 1.2.2.

---
+++ cad/astk-serveur/files/patch-lib::as_config.py
@@ -0,0 +1,13 @@
+--- lib/as_config.py.orig	Mon Feb 14 09:23:49 2005
++++ lib/as_config.py	Thu Mar 24 10:08:09 2005
+@@ -66,8 +66,8 @@
+          'SRCMAT'          : ['materiau'],
+          'SRCHIST'         : ['histor'],
+          # name of "binaries" (as results of a make)
+-         'BIN_NODBG'       : ['asteru.exe'],
+-         'BIN_DBG'         : ['asterd.exe'],
++         'BIN_NODBG'       : ['asteru'],
++         'BIN_DBG'         : ['asterd'],
+          'BINCMDE'         : ['commande'],
+          'BINELE'          : ['elements'],
+          'BINPICKLED'      : ['cata_ele.pickled'],
