b19271a4acaec1730e8390b3d86faefc08e44649
marcus <marcus@FreeBSD.org>
2005-04-02 19:11:44 +0000

    Update to 0.10.5.

--- accessibility/gnopernicus/files/patch-srcore_srbrl.c
+++ accessibility/gnopernicus/files/patch-srcore_srbrl.c
@@ -1,39 +1,38 @@
---- srcore/srbrl.c.orig	Wed Jul 14 20:51:31 2004
-+++ srcore/srbrl.c	Wed Jul 14 20:55:57 2004
-@@ -723,6 +723,12 @@
- static void
+--- srcore/srbrl.c.orig	Wed Mar 23 04:02:10 2005
++++ srcore/srbrl.c	Wed Mar 23 17:32:38 2005
+@@ -763,6 +763,11 @@
  src_braille_load_values (SRCBraille *braille)
  {
+     gint default_port = DEFAULT_BRAILLE_PORT_NO;
 +    gint default_pos_sensor;
 +    gint default_opt_sensor;
 +    gchar *default_braille_transaltion_table;
 +    gchar *default_braille_style;
 +    gchar *default_braille_cursor_style;
-+
+ 
      sru_assert (braille);
      
-     if (!braille->device)
-@@ -749,7 +755,7 @@
- 	}					  
+@@ -786,7 +791,7 @@
+     	SET_BRAILLE_CONFIG_DATA (BRAILLE_PORT_NO, CFGT_INT, &braille->port);
      }    
      
 -    gchar *default_braille_transaltion_table = DEFAULT_BRAILLE_TRANSLATION;
-+    *default_braille_transaltion_table = DEFAULT_BRAILLE_TRANSLATION;
++    default_braille_transaltion_table = DEFAULT_BRAILLE_TRANSLATION;
      GET_BRAILLE_CONFIG_DATA_WITH_DEFAULT (BRAILLE_TRANSLATION, CFGT_STRING, 
  					  &braille->translation_table, 
  					  default_braille_transaltion_table);		
-@@ -761,22 +767,22 @@
+@@ -798,22 +803,22 @@
      }
  	
      
 -    gchar *default_braille_style = DEFAULT_BRAILLE_STYLE;
-+    *default_braille_style = DEFAULT_BRAILLE_STYLE;
++    default_braille_style = DEFAULT_BRAILLE_STYLE;
      GET_BRAILLE_CONFIG_DATA_WITH_DEFAULT (BRAILLE_STYLE, CFGT_STRING, 
  					  &braille->style, 
  					  default_braille_style);
  					  
 -    gchar *default_braille_cursor_style = DEFAULT_BRAILLE_CURSOR_STYLE;
-+    *default_braille_cursor_style = DEFAULT_BRAILLE_CURSOR_STYLE;
++    default_braille_cursor_style = DEFAULT_BRAILLE_CURSOR_STYLE;
      GET_BRAILLE_CONFIG_DATA_WITH_DEFAULT (BRAILLE_CURSOR_STYLE, CFGT_STRING, 
  					  &braille->cursor_style, 
  					  default_braille_cursor_style);
