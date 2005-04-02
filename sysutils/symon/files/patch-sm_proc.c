13168f3ad12ff9cb5c4b0e8151e09702fef1a7a7
pav <pav@FreeBSD.org>
2005-04-02 09:09:33 +0000

    - Fix build on -CURRENT
    Submitted by:	maintainer

--- sysutils/symon/files/patch-sm_proc.c
+++ sysutils/symon/files/patch-sm_proc.c
@@ -5,9 +5,9 @@
  		   if (kvm_read(proc_kd, (unsigned long)pp->ki_paddr, &pproc,
  				  sizeof(pproc)) == sizeof(pproc)) {
 +#ifdef HAS_RUSAGE_EXT
-+			cpu_uticks += pproc.p_rux.p_uticks;  /* user */
-+			cpu_sticks += pproc.p_rux.p_sticks;  /* sys  */
-+			cpu_iticks += pproc.p_rux.p_iticks;  /* int  */
++			cpu_uticks += pproc.p_rux.rux_uticks;  /* user */
++			cpu_sticks += pproc.p_rux.rux_sticks;  /* sys  */
++			cpu_iticks += pproc.p_rux.rux_iticks;  /* int  */
 +#else
  			cpu_uticks += pproc.p_uticks;  /* user */
  			cpu_sticks += pproc.p_sticks;  /* sys  */
