7956aac99219e6e3c1834ec6a2bae77495f3ade3
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2005-04-02 20:01:30 +0000

    Update to 4.0.1

--- net/py-bittorrent/files/extra-nogui-patch-setup.py
+++ net/py-bittorrent/files/extra-nogui-patch-setup.py
@@ -1,18 +1,16 @@
 --- setup.py.orig	Sat Mar 12 10:01:45 2005
 +++ setup.py	Sat Mar 12 10:14:21 2005
-@@ -19,9 +19,9 @@
+@@ -19,7 +19,7 @@
  
  import glob
  
--scripts = ["btdownloadgui.py", "btdownloadcurses.py", "btdownloadheadless.py", 
+-scripts = ["btdownloadgui.py", "btdownloadcurses.py", "btdownloadheadless.py",
 -           "btmaketorrentgui.py", "btmaketorrent.py",
-+scripts = ["btdownloadcurses.py", "btdownloadheadless.py", 
++scripts = ["btdownloadcurses.py", "btdownloadheadless.py",
 +           "btmaketorrent.py",
             "btlaunchmany.py", "btlaunchmanycurses.py", 
-            "bttrack.py", "btreannounce.py", "btrename.py", "btshowmetainfo.py",
-            "bttest.py"]
-
-@@ -28,4 +28,3 @@
+            "bttrack.py", "btreannounce.py", "btrename.py", "btshowmetainfo.py"]
+@@ -27,4 +27,3 @@
  img_root, doc_root = BitTorrent.calc_unix_dirs()
  
 -data_files = [ (img_root        , glob.glob('images/*png')+['images/bittorrent.ico',]),
