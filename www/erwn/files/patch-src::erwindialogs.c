65589606f72668872c18f67071a40f56a980e319
Florent Thoumie <flz@FreeBSD.org>
2005-04-05 07:31:20 +0000

    - Update to 0.8.
    PR:		ports/79485
    Submitted by:	Ports Fury

--- www/erwn/files/patch-src::erwindialogs.c
+++ www/erwn/files/patch-src::erwindialogs.c
@@ -1,13 +1,13 @@
---- src/erwindialogs.c.orig	Sun Jul 11 17:39:47 2004
-+++ src/erwindialogs.c	Sun Jul 11 17:40:13 2004
+--- src/erwindialogs.c.orig	Thu Jan 20 05:59:44 2005
++++ src/erwindialogs.c	Thu Mar 31 18:52:42 2005
 @@ -125,9 +125,9 @@
  {
  	GtkWidget *dialog;
- 	GtkFileFilter *filter_all = gtk_file_filter_new ();
-+	GtkFileFilter *filter_html = gtk_file_filter_new ();
- 	gtk_file_filter_add_pattern (filter_all, "*");
- 	gtk_file_filter_set_name(filter_all,"All Files");
--	GtkFileFilter *filter_html = gtk_file_filter_new ();
- 	gtk_file_filter_add_pattern (filter_html, "*html");
- 	gtk_file_filter_set_name(filter_html,"HTML Files");
+ 	GtkFileFilter *filter_all = gtk_file_filter_new();
++	GtkFileFilter *filter_html = gtk_file_filter_new();
+ 	gtk_file_filter_add_pattern(filter_all, "*");
+ 	gtk_file_filter_set_name(filter_all, "All Files");
+-	GtkFileFilter *filter_html = gtk_file_filter_new();
+ 	gtk_file_filter_add_pattern(filter_html, "*html");
+ 	gtk_file_filter_set_name(filter_html, "HTML Files");
  
