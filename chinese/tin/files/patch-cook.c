702ad6d5133dcb938349e7666349b6db880acc26
leeym <leeym@FreeBSD.org>
2005-04-05 17:33:36 +0000

    - update patch and unbreak this port - take over the maintainership
    PR:		part of 78990
    Submitted by:	leeym
    Approved by:	ex-maintainer

--- chinese/tin/files/patch-cook.c
+++ chinese/tin/files/patch-cook.c
@@ -1,15 +1,16 @@
---- src/cook.c.orig	Tue Aug 26 20:34:07 2003
-+++ src/cook.c	Wed Oct  8 15:05:05 2003
-@@ -719,7 +719,12 @@
+--- src/cook.c.orig	Wed Dec  1 18:48:37 2004
++++ src/cook.c	Fri Mar 18 23:34:58 2005
+@@ -408,8 +408,13 @@
+ 		if (!(line && strlen(line)))
  			break;	/* premature end of file, file error etc. */
  
- 		/* convert network to local charset, tex2iso, iso2asc etc. */
 +#ifndef CHARSET_CONVERSION
- 		process_charsets(&line, &max_line_len, get_param(part->params, "charset"), tinrc.mm_local_charset, CURR_GROUP.attribute->tex2iso_conv && art->tex2iso);
+ 		/* convert network to local charset, tex2iso, iso2asc etc. */
+ 		process_charsets(&line, &max_line_len, get_param(part->params, "charset"), tinrc.mm_local_charset, curr_group->attribute->tex2iso_conv && art->tex2iso);
 +#else
 +		// force to use undeclared_charset first
-+		process_charsets(&line, &max_line_len, (CURR_GROUP.attribute->undeclared_charset) ? (CURR_GROUP.attribute->undeclared_charset) : get_param(part->params, "charset"), tinrc.mm_local_charset, CURR_GROUP.attribute->tex2iso_conv && art->tex2iso);
++		process_charsets(&line, &max_line_len, (curr_group->attribute->undeclared_charset) ? (curr_group->attribute->undeclared_charset) : get_param(part->params, "charset"), tinrc.mm_local_charset, curr_group->attribute->tex2iso_conv && art->tex2iso);
 +#endif /* !CHARSET_CONVERSION */
  
- 		len = (int) strlen(line);
- 
+ #if defined(MULTIBYTE_ABLE) && !defined(NO_LOCALE)
+ 		if (IS_LOCAL_CHARSET("UTF-8"))
