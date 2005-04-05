289f44141c7c342641721cc5c0d7ce15b48dc16e
Kirill Ponomarev <krion@FreeBSD.org>
2005-04-05 09:51:56 +0000

    Update to version 3.3.0
    PR:		ports/79469
    Submitted by:	Ports Fury

--- devel/libassa/files/patch-tests:ttcp_assa.cpp
+++ devel/libassa/files/patch-tests:ttcp_assa.cpp
@@ -1,5 +1,5 @@
---- tests/ttcp_assa.cpp.orig	Wed Dec 24 17:39:58 2003
-+++ tests/ttcp_assa.cpp	Wed Dec 24 17:41:13 2003
+--- tests/ttcp_assa.cpp.orig	Fri Jan  7 10:46:12 2005
++++ tests/ttcp_assa.cpp	Tue Mar 22 16:32:30 2005
 @@ -91,7 +91,7 @@
  IPv4Socket* sock = 0;		// Communication socket
  
@@ -9,16 +9,7 @@
  
  int domain, fromlen;
  int fd;				/* fd of network socket */
-@@ -384,7 +384,7 @@
- 	}
- #else
- 	if (getpeername(fd, (struct sockaddr*) &peer, 
--			(int*) &peerlen) < 0) {
-+			&peerlen) < 0) {
- 	    err("getpeername");
- 	}
- #endif /* defined(_POSIX_SOURCE) */
-@@ -783,7 +783,7 @@
+@@ -775,7 +775,7 @@
  Nread( int fd_, void* buf_, int count )
  {
      struct sockaddr_in from;
@@ -27,12 +18,3 @@
      register int cnt;
  
      if( udp )  
-@@ -795,7 +795,7 @@
- #else  // _POSIX_SOURCE
- 	cnt = recvfrom( fd_, (char*) buf_, count, 0, 
- 			(struct sockaddr*) &from, 
--			(int*) &len );
-+			&len );
- #endif
- 	numCalls++;
-     } else {
