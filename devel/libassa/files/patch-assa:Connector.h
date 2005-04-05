d36fe07d2c46d8a5e8e8cef142d09cc562257cde
krion <krion@FreeBSD.org>
2005-04-05 09:51:56 +0000

    Update to version 3.3.0
    PR:		ports/79469
    Submitted by:	Ports Fury

--- devel/libassa/files/patch-assa:Connector.h
+++ devel/libassa/files/patch-assa:Connector.h
@@ -1,20 +1,11 @@
---- assa/Connector.h.orig	Wed Dec 24 17:32:49 2003
-+++ assa/Connector.h	Wed Dec 24 17:34:16 2003
-@@ -407,7 +407,7 @@
+--- assa/Connector.h.orig	Tue Mar  1 20:40:39 2005
++++ assa/Connector.h	Tue Mar 22 20:13:07 2005
+@@ -406,7 +406,7 @@
  	int error;
  	int ret;
  	error = ret = errno = 0;
 -	size_t n = sizeof (error);
 +	socklen_t n = sizeof (error);
  
- 	/* Always remove IO handler first. */
- 	m_reactor->removeHandler (this, WRITE_EVENT);	
-@@ -415,7 +415,7 @@
- #if defined(linux)
- 	ret = getsockopt (m_fd, SOL_SOCKET, SO_ERROR, (void*)&error, &n);
- #else  // Solaris 2.6 
--	ret = getsockopt (m_fd, SOL_SOCKET, SO_ERROR, (char*)&error, (int*)&n);
-+	ret = getsockopt (m_fd, SOL_SOCKET, SO_ERROR, (char*)&error, &n);
- #endif
- 
- 	if (ret == 0) {
+ 	/** Always remove IO handler first. 
+ 	 */
