a34a6612a4828b74e011af1f52e6084d7e3c1d8d
Roman Bogorodskiy <novel@FreeBSD.org>
2005-04-07 16:52:56 +0000

    Fix segfault when there is no swap.

--- sysutils/torsmo/files/patch-torsmo.c
+++ sysutils/torsmo/files/patch-torsmo.c
@@ -1,5 +1,5 @@
 --- torsmo.c.orig	Wed Dec 22 01:14:46 2004
-+++ torsmo.c	Wed Dec 22 21:49:27 2004
++++ torsmo.c	Thu Apr  7 20:51:01 2005
 @@ -272,7 +272,7 @@
        snprintf(buf, 255, "%.1fM", m);
    }
@@ -18,7 +18,17 @@
      }
      OBJ(freq) {
        snprintf(p, n, "%s", get_freq());
-@@ -2223,8 +2223,8 @@
+@@ -1126,7 +1126,8 @@
+       human_readable(cur->swapmax*1024, p);
+     }
+     OBJ(swapperc) {
+-      snprintf(p, 255, "%*u", pad_percents, (cur->swap*100) / cur->swapmax);
++      snprintf(p, 255, "%*u", pad_percents, 
++		cur->swapmax ? (cur->swap*100) / cur->swapmax : 0);
+     }
+     OBJ(swapbar) {
+       new_bar(p, obj->data.pair.a, obj->data.pair.b,
+@@ -2223,8 +2224,8 @@
  
    /* handle other command line arguments */
  
@@ -29,7 +39,7 @@
    while (1) {
      int c = getopt(argc, argv, getopt_string);
      if(c == -1) break;
-@@ -2319,8 +2319,7 @@
+@@ -2319,8 +2320,7 @@
  
      default:
        fprintf(stderr, "torsmo: forked to background, pid is %d\n", ret);
