075a3f6516290eb95579968617b0c27dbafeba6a
Vanilla I. Shu <vanilla@FreeBSD.org>
2005-04-07 15:28:14 +0000

    Upgrade to 0.9.0.
    Submitted by:	chinsan <chinsan_at_mail2000.com.tw>
    Approved by:	maintainer

--- chinese/gcin/files/patch-im-srv.c
+++ chinese/gcin/files/patch-im-srv.c
@@ -1,10 +1,11 @@
---- im-srv/im-srv.c.orig	Mon Feb 14 10:18:04 2005
-+++ im-srv/im-srv.c	Fri Mar 11 01:43:51 2005
-@@ -121,7 +121,11 @@
+--- im-srv/im-srv.c.orig	Thu Apr  7 09:11:24 2005
++++ im-srv/im-srv.c	Thu Apr  7 09:11:01 2005
+@@ -121,7 +121,12 @@
    char sock_path[128];
    get_gcin_im_srv_sock_path(sock_path, sizeof(sock_path));
    strcpy(serv_addr.sun_path, sock_path);
-+#ifdef	SUN_LEN
++
++#ifdef SUN_LEN
 +  servlen = SUN_LEN (&serv_addr);
 +#else
    servlen = strlen(serv_addr.sun_path) + sizeof(serv_addr.sun_family);
