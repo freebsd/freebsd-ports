f7c3e8940639b97a7a83ed81d5d2aeb272821cb4
vs <vs@FreeBSD.org>
2005-04-07 14:55:46 +0000

    - Unbreak licq-osd: Update to 1.3.0 from main licq-distribution     and use already available infrastructure from net/licq - Reset maintainer
    PR:		ports/78559
    Approved by:	maintainer

---
+++ net/licq-osd/files/patch-src::licq_osd.conf.h
@@ -0,0 +1,11 @@
+--- src/licq_osd.conf.h.orig	Mon Mar  7 19:24:09 2005
++++ src/licq_osd.conf.h	Mon Mar  7 19:27:17 2005
+@@ -4,7 +4,7 @@
+ "# you can get the available fonts for your machine from xfontsel\n"
+ "# i prefer this one - but it is not available everywhere\n"
+ "# Font=-*-lucida-*-r-*-*-24-*-*-*-*-*-iso8859-15\n"
+-"Font=-*-*-*-*-*-*-24-*-*-*-*-*-*-*\n"
++"Font=-*-lucida-*-*-*-*-24-*-*-*-*-*-iso8859-15\n"
+ "\n"
+ "# how long should a message be displayed\n"
+ "Timeout=5\n"
