c00280bf37916f676a4b90ed27fdbc79e94bbe6c
vanilla <vanilla@FreeBSD.org>
2005-04-07 15:28:14 +0000

    Upgrade to 0.9.0.
    Submitted by:	chinsan <chinsan_at_mail2000.com.tw>
    Approved by:	maintainer

--- chinese/gcin/files/patch-gcin-im-client.c
+++ chinese/gcin/files/patch-gcin-im-client.c
@@ -1,15 +1,11 @@
---- im-client/gcin-im-client.c.orig	Sat Feb 26 10:39:12 2005
-+++ im-client/gcin-im-client.c	Tue Mar 15 00:15:47 2005
-@@ -68,11 +68,7 @@
-       save_old_sigaction_single(SIGCHLD, &ori_act);
- 
-       if ((pid=fork())==0) {
--#if 	FREEBSD
--        setgrp();
--#else
+--- im-client/gcin-im-client.c.orig	Thu Apr  7 09:18:13 2005
++++ im-client/gcin-im-client.c	Thu Apr  7 09:18:27 2005
+@@ -71,7 +71,7 @@
+ #if 	FREEBSD
+         setgrp();
+ #else
 -        setpgrp();
--#endif
 +        setpgrp(0, 0);
+ #endif
          execl(execbin, "gcin", NULL);
        } else {
-         sleep(1);
