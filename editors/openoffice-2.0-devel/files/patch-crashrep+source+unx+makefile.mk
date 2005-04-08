c33c6a0a967fbe18e7105b19816bd94e49f82a2a
maho <maho@FreeBSD.org>
2005-04-08 01:27:27 +0000

    Update to m91

---
+++ editors/openoffice-2.0-devel/files/patch-crashrep+source+unx+makefile.mk
@@ -0,0 +1,19 @@
+Index: crashrep/source/unx/makefile.mk
+===================================================================
+RCS file: /cvs/porting/crashrep/source/unx/makefile.mk,v
+retrieving revision 1.14
+diff -u -r1.14 makefile.mk
+--- crashrep/source/unx/makefile.mk	8 Mar 2005 16:24:14 -0000	1.14
++++ crashrep/source/unx/makefile.mk	28 Mar 2005 08:11:57 -0000
+@@ -82,7 +82,10 @@
+ APP1TARGET=$(TARGET)
+ APP1OBJS=$(OBJFILES)
+ 
+-APP1STDLIBS=$(DYNAMIC) -lXext -lX11 -ldl -lnsl
++APP1STDLIBS=$(DYNAMIC) -lXext -lX11
++.IF "$(OS)" != "FREEBSD"
++APP1STDLIBS+=-ldl -lnsl
++.ENDIF
+ .IF "$(OS)" == "SOLARIS"
+ APP1STDLIBS+=-lsocket
+ .ENDIF
---
+++ editors/openoffice.org-2/files/patch-canvas+inc+canvas+canvastools.hxx
@@ -0,0 +1,21 @@
+
+Index: canvas/inc/canvas/canvastools.hxx
+===================================================================
+RCS file: /cvs/gsl/canvas/inc/canvas/canvastools.hxx,v
+retrieving revision 1.7
+diff -u -r1.7 canvastools.hxx
+--- canvas/inc/canvas/canvastools.hxx	30 Mar 2005 07:34:18 -0000	1.7
++++ canvas/inc/canvas/canvastools.hxx	6 Apr 2005 01:22:58 -0000
+@@ -133,9 +133,9 @@
+ 		 */
+ 
+ 		// mickey's math tricks...
+-		inline unsigned int powerof2( unsigned int c ) { return 0x1 << c; }
+-		inline unsigned int mask( unsigned int c ) { return ((unsigned int)(-1)) / (powerof2(powerof2(c)) + 1); }
+-		inline unsigned int count( unsigned int x, unsigned int c ) { return ((x) & mask(c)) + (((x) >> (powerof2(c))) & mask(c)); }
++		inline unsigned int powerof2_ooo( unsigned int c ) { return 0x1 << c; }
++		inline unsigned int mask( unsigned int c ) { return ((unsigned int)(-1)) / (powerof2_ooo(powerof2_ooo(c)) + 1); }
++		inline unsigned int count( unsigned int x, unsigned int c ) { return ((x) & mask(c)) + (((x) >> (powerof2_ooo(c))) & mask(c)); }
+ 		template<typename T>
+ 		inline unsigned int bitcount( T c ) {
+ 			unsigned int nByteIndex = 0;
