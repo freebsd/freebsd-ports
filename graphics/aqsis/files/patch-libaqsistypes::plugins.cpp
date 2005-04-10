549e1e49716d2998540237a08a83cd06a1c50a94
Yen-Ming Lee <leeym@FreeBSD.org>
2005-04-10 14:41:30 +0000

    - Update ports/graphics/aqsis to 1.0.0
    PR:		79741
    Submitted by:	Ying-Chieh Chen <yinjieh@csie.nctu.edu.tw>

--- graphics/aqsis/files/patch-libaqsistypes::plugins.cpp
+++ graphics/aqsis/files/patch-libaqsistypes::plugins.cpp
@@ -1,11 +1,11 @@
---- libaqsistypes/plugins.cpp.orig	Fri Jul 23 23:50:39 2004
-+++ libaqsistypes/plugins.cpp	Tue Aug 17 14:10:36 2004
-@@ -150,7 +150,7 @@
+--- libaqsistypes/plugins.cpp.orig	Sun Apr 10 08:35:35 2005
++++ libaqsistypes/plugins.cpp	Sun Apr 10 08:35:58 2005
+@@ -153,7 +153,7 @@
      // Free the buffer.
      LocalFree( lpMsgBuf );
  #elif not defined AQSIS_SYSTEM_MACOSX
 -    char* error = dlerror();
 +    const char* error = dlerror();
-     if( dlerror )
+     if( error )
  	    errorlog = error;
  #endif
