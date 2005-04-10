0aea459e322e1456e0335cd3aab8dd95def52cfe
marcus <marcus@FreeBSD.org>
2005-04-10 17:36:01 +0000

    * Fallback to polling for files not supported by kqueue * Do not send an ENDEXISTS event on NOTE_REVOKE * Handle kevent aggregation if multiple event types are requested * Mimic FAM's behavior of never following symbolic links * Add exhaustive error handling * Remove all the legacy thread-safe code since gamin is now single-threaded
    PR:		79680
    Submitted by:	Jean-Yves Lefort <jylefort@brutele.be>

--- devel/gamin/files/patch-server_gam_kqueue.h
+++ devel/gamin/files/patch-server_gam_kqueue.h
@@ -1,19 +1,11 @@
---- server/gam_kqueue.h.orig	Sat Mar 26 18:32:19 2005
-+++ server/gam_kqueue.h	Sat Mar 26 18:36:11 2005
-@@ -0,0 +1,24 @@
-+
+--- server/gam_kqueue.h.orig	Fri Apr  8 14:50:41 2005
++++ server/gam_kqueue.h	Fri Apr  8 14:51:01 2005
+@@ -0,0 +1,16 @@
 +#ifndef __MD_KQUEUE_H__
 +#define __MD_KQUEUE_H__
 +
 +#include <glib.h>
-+#include "gam_poll.h"
 +#include "gam_subscription.h"
-+
-+#define VN_NOTE_MOST	(NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | \
-+			 NOTE_ATTRIB | NOTE_LINK | NOTE_RENAME | \
-+			 NOTE_REVOKE)
-+#define VN_NOTE_ALL	VN_NOTE_MOST
-+#define VN_NOTE_CHANGED	(NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK)
 +
 +G_BEGIN_DECLS
 +
