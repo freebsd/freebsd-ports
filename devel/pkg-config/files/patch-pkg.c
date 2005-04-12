aed30476b31d77bc911adef5534988ee37c2acb0
adamw <adamw@FreeBSD.org>
2005-04-12 18:12:12 +0000

    Update to 0.17.
    It's now significantly easier to change the pkg-config search path.

--- devel/pkg-config/files/patch-pkg.c
+++ devel/pkg-config/files/patch-pkg.c
@@ -1,6 +1,6 @@
---- pkg.c.orig	Thu Jan 16 16:07:09 2003
-+++ pkg.c	Wed Jan 22 18:11:41 2003
-@@ -113,7 +113,7 @@
+--- pkg.c.orig	Tue Apr 12 07:08:13 2005
++++ pkg.c	Tue Apr 12 13:55:42 2005
+@@ -128,7 +128,7 @@ name_ends_in_uninstalled (const char *st
   * locations, ignoring duplicates
   */
  static void
@@ -9,24 +9,13 @@
  {
    DIR *dir;
    struct dirent *dent;
-@@ -133,7 +133,8 @@
+@@ -148,7 +148,8 @@ scan_dir (const char *dirname)
    free (dirname_copy);
    if (!dir)
      {
 -      debug_spew ("Cannot open directory '%s' in package search path: %s\n",
 +      if (!quiet)
-+          debug_spew ("Cannot open directory '%s' in package search path: %s\n",
++        debug_spew ("Cannot open directory '%s' in package search path: %s\n",
                    dirname, g_strerror (errno));
        return;
      }
-@@ -202,7 +203,9 @@
-       path_positions = g_hash_table_new (g_str_hash, g_str_equal);
-       
-       g_slist_foreach (search_dirs, (GFunc)scan_dir, NULL);
--      scan_dir (pkglibdir);
-+      scan_dir (pkglibdir, (void *)0);
-+      scan_dir (XPKGLIBDIR, (void *)1);
-+      scan_dir (LPKGLIBDIR, (void *)1);
-     }
- }
- 
