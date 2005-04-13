3d65073d8a5489a61308743842bea661035bb07e
marcus <marcus@FreeBSD.org>
2005-04-13 20:07:11 +0000

    * Use gnometarget * Fix an infinite loop when duplicate components' libs are requested [1]
    Obtained from:	Freedesktop.org CVS [1]

--- devel/pkgconfig/files/patch-pkg.c
+++ devel/pkgconfig/files/patch-pkg.c
@@ -1,21 +1,20 @@
---- pkg.c.orig	Tue Apr 12 07:08:13 2005
-+++ pkg.c	Tue Apr 12 13:55:42 2005
-@@ -128,7 +128,7 @@ name_ends_in_uninstalled (const char *st
-  * locations, ignoring duplicates
-  */
- static void
--scan_dir (const char *dirname)
-+scan_dir (const char *dirname, void *quiet)
- {
-   DIR *dir;
-   struct dirent *dent;
-@@ -148,7 +148,8 @@ scan_dir (const char *dirname)
-   free (dirname_copy);
-   if (!dir)
+--- pkg.c.orig	Wed Apr 13 16:04:58 2005
++++ pkg.c	Wed Apr 13 16:05:33 2005
+@@ -1024,7 +1024,7 @@
+     tmp = pkgs;
+     while (tmp != NULL)
      {
--      debug_spew ("Cannot open directory '%s' in package search path: %s\n",
-+      if (!quiet)
-+        debug_spew ("Cannot open directory '%s' in package search path: %s\n",
-                   dirname, g_strerror (errno));
-       return;
+-      dups_list = g_slist_concat (dups_list, get_l_libs(tmp->data));
++      dups_list = g_slist_concat (dups_list, g_slist_copy(get_l_libs(tmp->data)));
+       tmp = tmp->next;
      }
+ 
+@@ -1067,7 +1067,7 @@
+     tmp = pkgs;
+     while (tmp != NULL)
+     {
+-      dups_list = g_slist_concat (dups_list, get_L_libs(tmp->data));
++      dups_list = g_slist_concat (dups_list, g_slist_copy(get_L_libs(tmp->data)));
+       tmp = tmp->next;
+     }
+ 
