dfbcaf7a5df55b858af8b4970da82c8d5a551a41
pav <pav@FreeBSD.org>
2005-04-13 08:34:10 +0000

    - Update to 2.0 - Require python 2.3+
    PR:		ports/79851
    Submitted by:	Dryice Liu <dryice@liu.com.cn> (maintainer)

---
+++ www/quixote/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	Tue Apr 12 02:41:32 2005
++++ setup.py	Wed Apr 13 15:23:43 2005
+@@ -29,7 +29,7 @@
+ 
+       'package_dir': {'quixote':os.curdir},
+       'packages': ['quixote',  'quixote.demo', 'quixote.form',
+-                   'quixote.server'],
++                   'quixote.server', 'quixote.html', 'quixote.ptl', 'quixote.form1'],
+ 
+       'ext_modules': [],
+ 
--- www/quixote/scripts/compile_ptl.py
+++ www/quixote/scripts/compile_ptl.py
@@ -1,5 +1,5 @@
 import sys
-from quixote import ptl_compile
+from quixote.ptl import ptl_compile
 
 for srcfile in sys.argv[1:]:
   ptl_compile.compile(srcfile, srcfile + 'c')
