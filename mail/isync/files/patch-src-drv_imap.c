a2e47116a640640dcb2a49b190da2e954302be2d
Florent Thoumie <flz@FreeBSD.org>
2005-04-15 17:48:23 +0000

    - Update to 1.0.1.
    PR:		ports/79975
    Submitted by:	Ports Fury

---
+++ mail/isync/files/patch-src-drv_imap.c
@@ -0,0 +1,10 @@
+--- src/drv_imap.c.orig	Mon Mar 28 19:41:04 2005
++++ src/drv_imap.c	Sun Apr 10 13:00:50 2005
+@@ -26,6 +26,7 @@
+ 
+ #include <assert.h>
+ #include <unistd.h>
++#include <limits.h>
+ #include <sys/mman.h>
+ #include <sys/time.h>
+ #include <stdlib.h>nistd.h>
