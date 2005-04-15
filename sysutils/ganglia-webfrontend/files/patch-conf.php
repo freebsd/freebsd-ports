ba2f18c6ae32796951f856f41b965a8a5559bf1e
Brooks Davis <brooks@FreeBSD.org>
2005-04-15 22:24:50 +0000

    Upgrade to 3.0.1 (mostly just includes fixes we previously installed via patches).
    Submitted by:	Michael Fuckner <michael at fuckner dot net>
    PR:		ports/79770

--- sysutils/ganglia-webfrontend/files/patch-conf.php
+++ sysutils/ganglia-webfrontend/files/patch-conf.php
@@ -3,20 +3,7 @@ $FreeBSD$
 
 --- conf.php.orig
 +++ conf.php
-@@ -3,11 +3,7 @@
- #
- # Gmetad-webfrontend version. Used to check for updates.
- #
--$majorversion = 2;
--$minorversion = 5;
--$microversion = 7;
--
--$ganglia_version = "$majorversion.$minorversion.$microversion";
-+include_once "./version.php";
- 
- #
- # The name of the directory in "./templates" which contains the
-@@ -22,12 +18,12 @@
+@@ -18,12 +18,12 @@
  #
  
  # Where gmetad stores the rrd archives.
