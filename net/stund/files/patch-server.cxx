538b496d6b522c65d55d8296cf7c2b3ffc447a12
Maxim Sobolev <sobomax@FreeBSD.org>
2005-04-17 10:49:07 +0000

    Update to 0.94.

--- net/stund/files/patch-server.cxx
+++ net/stund/files/patch-server.cxx
@@ -1,23 +1,51 @@
 
 $FreeBSD$
 
---- server.cxx	2004/06/10 15:25:17	1.1
-+++ server.cxx	2004/06/10 15:26:05
-@@ -172,7 +172,7 @@
- #if defined(WIN32)
-    int pid=0;
- 
+--- server.cxx.orig
++++ server.cxx
+@@ -32,7 +32,7 @@
+         << "  -a sets the secondary IP" << endl
+         << "  -p sets the primary port and defaults to 3478" << endl
+         << "  -o sets the secondary port and defaults to 3479" << endl
+-        << "  -b makes the program run in the backgroud" << endl
++        << "  -b makes the program run in the background" << endl
+         << "  -m sets up a STERN server starting at port m" << endl
+         << "  -v runs in verbose mode" << endl
+       // in makefile too
+@@ -188,30 +188,14 @@
+       //exit(1);
+    }
+    
+-#if defined(WIN32)
+-   int pid=0;
+-
 -   if ( background )
-+   if ( !background )
-    {
-       cerr << "The -b background option does not work in windows" << endl;
-       exit(-1);
-@@ -180,7 +180,7 @@
- #else
-    pid_t pid=0;
- 
+-   {
+-      cerr << "The -b background option does not work in windows" << endl;
+-      exit(-1);
+-   }
+-#else
+-   pid_t pid=0;
+-
 -   if ( background )
-+   if ( !background )
-    {
-       pid_t pid = fork();
+-   {
+-      pid = fork();
+-
+-      if (pid < 0)
+-      {
+-         cerr << "fork: unable to fork" << endl;
+-         exit(-1);
+-      }
++   if (background) {
++       if (daemon(0,0) < 0) {
++           cerr << "demon() call failed" << endl;
++           exit(1);
++       }
+    }
+-#endif
  
+-   if (pid == 0) //child or not using background
++   if (1) //child or not using background
+    {
+       StunServerInfo info;
+       bool ok = stunInitServer(info, myAddr, altAddr, myMediaPort, verbose);
---
+++ net/stund/files/patch-stun.cxx
@@ -0,0 +1,64 @@
+
+$FreeBSD$
+
+--- stun.cxx.orig
++++ stun.cxx
+@@ -648,55 +648,11 @@
+ stunRand()
+ {
+    // return 32 bits of random stuff
+-   assert( sizeof(int) == 4 );
+    static bool init=false;
+    if ( !init )
+-   { 
+-      init = true;
+-		
+-      UInt64 tick;
+-		
+-#if defined(WIN32) 
+-      volatile unsigned int lowtick=0,hightick=0;
+-      __asm
+-         {
+-            rdtsc 
+-               mov lowtick, eax
+-               mov hightick, edx
+-               }
+-      tick = hightick;
+-      tick <<= 32;
+-      tick |= lowtick;
+-#elif defined(__GNUC__) && ( defined(__i686__) || defined(__i386__) )
+-      asm("rdtsc" : "=A" (tick));
+-#elif defined (__SUNPRO_CC) || defined( __sparc__ )	
+-      tick = gethrtime();
+-#elif defined(__MACH__) 
+-      int fd=open("/dev/random",O_RDONLY);
+-      read(fd,&tick,sizeof(tick));
+-      closesocket(fd);
+-#else
+-#     error Need some way to seed the random number generator 
+-#endif 
+-      int seed = int(tick);
+-#ifdef WIN32
+-      srand(seed);
+-#else
+-      srandom(seed);
+-#endif
+-   }
+-	
+-#ifdef WIN32
+-   assert( RAND_MAX == 0x7fff );
+-   int r1 = rand();
+-   int r2 = rand();
+-	
+-   int ret = (r1<<16) + r2;
+-	
+-   return ret;
+-#else
+-   return random(); 
+-#endif
++      srandomdev();
++   /* random() is described as returning 0...2**31-1 */
++   return 0xffffffff & ( ( random() << 31 ) | random() );
+ }
+ 
+ 
