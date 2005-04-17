ff8cbab99a5ae2865bf422a41b8dab51b6550b0e
Maxim Sobolev <sobomax@FreeBSD.org>
2005-04-17 07:41:56 +0000

    Update to the latest snapshot of the driver. This improves existing support for X100 and TDM400P and adds support for TE410P, TE405P and TE110P.
    Also this version include QuadBRI/OctoBRI support.
    
    Sponsored by:	LF.net (quadBRI/octoBRI support), PBXpress Inc. (all the rest)

--- net/zaptel12/files/zaptel.sh
+++ net/zaptel12/files/zaptel.sh
@@ -5,68 +5,24 @@ LIBDIR=${PREFIX}/lib/zaptel
 
 case "$1" in
 	start)
+		/sbin/kldload ${LIBDIR}/qozap.ko || exit 1
 		/sbin/kldload ${LIBDIR}/zaptel.ko || exit 1
 		/sbin/kldload ${LIBDIR}/wcfxo.ko || exit 1
 		/sbin/kldload ${LIBDIR}/wcfxs.ko || exit 1
-		if [ ! -d /dev/zap ]
-		then
-			mkdir -p /dev/zap || exit 1
-		fi
-		if [ ! -c /dev/zap/channel ]
-		then
-			/sbin/mknod /dev/zap/channel c 196 254 || exit 1
-		fi
-		if [ ! -c /dev/zap/ctl ]
-		then
-			/sbin/mknod /dev/zap/ctl c 196 0 || exit 1
-		fi
-		if [ ! -c /dev/zap/pseudo ]
-		then
-			/sbin/mknod /dev/zap/pseudo c 196 255 || exit 1
-		fi
-		if [ ! -c /dev/zap/timer ]
-		then
-			/sbin/mknod /dev/zap/timer c 196 253 || exit 1
-		fi
-		z=" zaptel"
-		if /usr/sbin/pciconf -l | /usr/bin/grep -q ^wcfxo
-		then
-			for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-			do
-				if /usr/sbin/pciconf -l | /usr/bin/grep -q ^wcfxo${i}
-				then
-					if [ ! -c /dev/wcfxo${i} ]
-					then
-						/sbin/mknod /dev/wcfxo${i} c 197 ${i} || exit 1
-					fi
-				fi
-			done
-			${PREFIX}/bin/ztcfg 2> /dev/null
-		else
-			if /usr/sbin/pciconf -l | /usr/bin/grep -q ^wcfxs
-			then
-				for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-				do
-					if /usr/sbin/pciconf -l | /usr/bin/grep -q ^wcfxs${i}
-					then
-						if [ ! -c /dev/wcfxs${i} ]
-						then
-							/sbin/mknod /dev/wcfxo${i} c 198 ${i} || exit 1
-						fi
-					fi
-				done
-				${PREFIX}/bin/ztcfg 2> /dev/null
-			else
-				z=""
-			fi
-		fi
+		/sbin/kldload ${LIBDIR}/wct1xxp.ko || exit 1
+		/sbin/kldload ${LIBDIR}/wct4xxp.ko || exit 1
+		/sbin/kldload ${LIBDIR}/wcte11xp.ko || exit 1
 		echo -n ${z}
 		;;
 
 	stop)
+		/sbin/kldunload qozap.ko || exit 1
 		/sbin/kldunload wcfxs.ko || exit 1
 		/sbin/kldunload wcfxo.ko || exit 1
 		/sbin/kldunload zaptel.ko || exit 1
+		/sbin/kldunload wct1xxp.ko || exit 1
+		/sbin/kldunload wct4xxp.ko || exit 1
+		/sbin/kldunload wcte11xp.ko || exit 1
 		echo -n " zaptel"
 		;;
 
