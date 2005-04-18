224324eb5756b946b6b8c80f2c68cb02618c530c
pav <pav@FreeBSD.org>
2005-04-18 21:03:41 +0000

    - Point to tcl/tk includes [1] - Fix packing
    PR:		ports/79914 [1]
    Submitted by:	George V. Kouryachy <frbrgeorge@gmail.com>
    
    The port is still quite broken.

--- math/py-matplotlib/files/patch-setupext.py
+++ math/py-matplotlib/files/patch-setupext.py
@@ -2,7 +2,8 @@
 +++ setupext.py	Tue Apr 12 16:10:28 2005
 @@ -308,7 +308,7 @@
      o = find_tcltk() # todo: try/except
-     module.include_dirs.extend([o.tcl_inc, o.tk_inc])
+-    module.include_dirs.extend([o.tcl_inc, o.tk_inc])
++    module.include_dirs.extend(["/usr/local/include/tcl8.4", "/usr/local/include/tk8.4"])
      module.library_dirs.extend([o.tcl_lib, o.tk_lib])
 -    module.libraries.extend(['tk'+o.tkv, 'tcl'+o.tkv])
 +    module.libraries.extend(['tk84', 'tcl84'])
