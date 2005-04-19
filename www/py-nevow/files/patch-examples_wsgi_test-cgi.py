2811eac9a19a7afe7226e6bf5f024ad30c701795
pav <pav@FreeBSD.org>
2005-04-19 22:02:47 +0000

    Nevow is a next-generation web application templating system, based on the ideas developed in the Twisted Woven package. Its main focus is on separating the HTML template from both the business logic and the display logic, while allowing the programmer to write pure Python code as much as possible. It separates your code into 'data' and 'render' functions, a simplified implementation of traditional MVC. It has various parts which can be used individually or as a whole, integrated web solution.
    PR:		ports/80049
    Submitted by:	"Choe, Cheng-Dae" <whitekid@gmail.com>

---
+++ www/py-nevow/files/patch-examples_wsgi_test-cgi.py
@@ -0,0 +1,11 @@
+
+$FreeBSD$
+
+--- examples/wsgi/test-cgi.py.orig
++++ examples/wsgi/test-cgi.py
+@@ -1,4 +1,4 @@
+-/usr/bin/python
++#!/usr/bin/python
+ # You should modify 'prefix' variable
+ 
+ import os,sys
