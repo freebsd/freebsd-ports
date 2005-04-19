b44f841ace6226d6f0ebeef30fb2f7c6a9353d6e
Max Khon <fjoe@FreeBSD.org>
2005-04-19 19:03:52 +0000

    Update to latest stable release 2.5.01.1927.

--- devel/cvsnt/files/patch-protocols-gserver.c
+++ devel/cvsnt/files/patch-protocols-gserver.c
@@ -1,5 +1,5 @@
---- protocols/gserver.c.orig	Sun Feb 20 16:35:47 2005
-+++ protocols/gserver.c	Sun Feb 20 16:35:55 2005
+--- protocols/gserver.c.orig	Fri Mar 18 20:56:35 2005
++++ protocols/gserver.c	Wed Apr 20 01:49:15 2005
 @@ -43,12 +43,13 @@
  
  #ifdef HAVE_GSSAPI_H
@@ -8,7 +8,7 @@
 +#else
  #ifdef HAVE_GSSAPI_GSSAPI_H
  #include <gssapi/gssapi.h>
- #endif
+ #endif /* freebsd needs 'else' but this breaks on other platforms so is incorrect for the main distribution.  Fix manually if building on that platform */
  #ifdef HAVE_GSSAPI_GSSAPI_GENERIC_H
  #include <gssapi/gssapi_generic.h>
 +#endif
