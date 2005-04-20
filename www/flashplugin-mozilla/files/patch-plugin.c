d33a82612f117f9b7cdd286a445290f42ded0ebe
pav <pav@FreeBSD.org>
2005-04-20 12:13:04 +0000

    - Update to 0.4.13
    PR:		ports/80057
    Submitted by:	Li-Lun Wang (Leland Wang) <llwang@infor.org> (maintainer)

--- www/flashplugin-mozilla/files/patch-plugin.c
+++ www/flashplugin-mozilla/files/patch-plugin.c
@@ -1,20 +1,30 @@
-*** plugin.c.old	Mon Jun 12 10:24:09 2000
---- plugin.c	Mon Sep 30 00:48:40 2002
-***************
-*** 193,198 ****
---- 193,207 ----
-  			XSync(This->dpy,False);
-  			XFreePixmap(This->dpy, This->canvas);
-  			shmdt(This->segInfo.shmaddr);
-+ #ifndef linux
-+ 			{
-+ 				struct shmid_ds buf;
-+ 
-+ 			if (shmctl(This->segInfo.shmid, IPC_RMID, &buf) < 0) {
-+ 				perror("shmctl");
-+ 				}
-+ 			}
-+ #endif
-  
-  			FlashClose(This->fh);
-  			This->fh = 0;
+--- plugin.c.orig	Wed Jan 19 19:30:52 2005
++++ plugin.c	Wed Apr 20 14:10:14 2005
+@@ -194,6 +194,15 @@
+ 			XSync(This->dpy,False);
+ 			XFreePixmap(This->dpy, This->canvas);
+ 			shmdt(This->segInfo.shmaddr);
++#ifndef linux
++			{
++				struct shmid_ds buf;
++
++			if (shmctl(This->segInfo.shmid, IPC_RMID, &buf) < 0) {
++				perror("shmctl");
++				}
++			}
++#endif
+ 
+ 			FlashClose(This->fh);
+ 			This->fh = 0;
+@@ -257,10 +266,10 @@
+ static void
+ flashWakeUp(XtPointer client_data, XtIntervalId *id)
+ {
+-	pthread_mutex_lock(&synchro);
+ 	PluginInstance* This;
+ 	long cmd;
+ 	long wakeUp;
++	pthread_mutex_lock(&synchro);
+ 
+ 	This = (PluginInstance*)client_data;
+ 
