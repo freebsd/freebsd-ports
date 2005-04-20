384c2bae3b94d68ec15491eba4dc6c00acfb93ea
Tilman Keskinoz <arved@FreeBSD.org>
2005-04-20 12:42:06 +0000

    pcsc-lite: - Upgrade to 1.2.9.b.7 - Use libtool15 - make hier(7)-compliant by moving the pcsc/drivers to lib/pcsc/drivers - Compile with USB support per default. Use WITHOUT_USB to disable usb   support - Chase libusb shared library version (Forgotten by leeym)
    Based on:	PR 76068
    Reviewed by:	maintainer, sem (earlier patch)
    
    ifd-devkit:
    - Chase pcsclite sharedlibrary version
    
    ifd-gempc410:
    - Unbreak by updating to 0.9.3
    - Move library to lib/pcsc/drivers
    - Chase pcsclite sharedlibrary version
      NOTE: This was not tested, I don't own the specific hardware
    
    ifd-gpr400:
    - Chase pcsclite sharedlibrary version
    - Fix build with new pcsc-lite
    - Chase new pcsclite drivers location
    
    ifd-slb_rf60:
    - Chase pcsclite sharedlibrary version
    
    ifd-test:
    - Chase pcsclite sharedlibrary version
    - Fix build with new pcsclite
    
    libchipcard:
    - Update to 0.9.1[1]
    - Chase pcsclite shared library version
    - Fix build with new pcsclite
    
    [1]PR:			78815
    [1]Submitted by:	lofi
    
    opensc:
    - Chase pcsclite shared library version
    - Fix build on CURRENT
    
    opensc-esteid
    - Chase pcsclite shared library version
    - Fix build with new pcsclite
    
    p5-PCSC-Card:
    - Update to 1.4.2
    - Chase pcsclite shared library version
    - Fix build with new pcsclite
    
    ruby-pcsc-lite
    - Mark broken, does not compile with new pcsc-lite
      Maintainer will work on new version
    
    Submitted by:	maintainer

--- security/ifd-gpr400/files/patch-gpr400_ifd.c
+++ security/ifd-gpr400/files/patch-gpr400_ifd.c
@@ -1,5 +1,5 @@
---- gpr400_ifd.c.orig	Wed Aug 22 03:21:43 2001
-+++ gpr400_ifd.c	Sun Sep  8 18:10:47 2002
+--- gpr400_ifd.c.orig	Wed Aug 22 04:21:43 2001
++++ gpr400_ifd.c	Mon Apr 11 21:09:23 2005
 @@ -77,7 +77,7 @@
  	printf("IFDHCreateChannel:\n");
  #endif
@@ -9,3 +9,20 @@
          if(gpr_fd < 0){
              return IFD_COMMUNICATION_ERROR;
          }
+@@ -597,16 +597,6 @@
+      RxLength should be zero on error.
+  *
+  **/
+-RESPONSECODE IFDHControl ( DWORD Lun, PUCHAR TxBuffer, 
+-			 DWORD TxLength, PUCHAR RxBuffer, 
+-			 PDWORD RxLength ) 
+-{
+-#ifdef PCSC_DEBUG
+-	printf("IFDHControl:\n");
+-#endif
+-    return IFD_NOT_SUPPORTED;
+-
+-}/* IFDHControl */
+ 
+ /**
+  * check if there is a smartcard in the reader
