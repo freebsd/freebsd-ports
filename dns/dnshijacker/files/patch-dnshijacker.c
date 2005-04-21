693ba532be688c6d13c90224e198506975e31860
Jean-Yves Lefort <jylefort@FreeBSD.org>
2005-04-21 13:29:42 +0000

    - Fix the checksum of spoofed packets - Honor NOPORTDOCS
    PR:		ports/80161
    Submitted by:	David Thiel <lx@redundancy.redundancy.org> (maintainer)
    Approved by:	adamw (mentor)

---
+++ dns/dnshijacker/files/patch-dnshijacker.c
@@ -0,0 +1,11 @@
+--- dnshijacker.c.orig	Thu Apr 21 00:40:59 2005
++++ dnshijacker.c	Thu Apr 21 00:41:29 2005
+@@ -733,7 +733,7 @@
+      *  source and destination are swapped here because we are spoofing a reply
+      */
+ 
+-    libnet_build_ip(LIBNET_UDP_H + LIBNET_DNS_H,
++    libnet_build_ip(packet_size - LIBNET_IP_H,
+             0,                                   /* ip tos              */
+             0,                                   /* ip id               */
+             0,                                   /* fragmentation bits  */
