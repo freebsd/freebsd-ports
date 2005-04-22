7470f79cfd18ecc0df58dbacba79743c4afc2df0
Pav Lucistnik <pav@FreeBSD.org>
2005-04-22 20:57:12 +0000

    - Change path to match frontpage-rtr binaries
    PR:		ports/76013
    Submitted by:	Joe Rhett <jrhett@meer.net>
    Approved by:	maintainer timeout (1 month)

--- www/mod_frontpage/files/patch-fpstatic.c
+++ www/mod_frontpage/files/patch-fpstatic.c
@@ -1,9 +1,12 @@
---- fpstatic.c.orig	Tue Feb  5 16:16:46 2002
-+++ fpstatic.c	Sun Apr 28 13:33:01 2002
-@@ -435,6 +435,24 @@
+--- fpstatic.c.orig	Tue Mar 12 22:07:07 2002
++++ fpstatic.c	Fri Apr 22 22:55:33 2005
+@@ -433,7 +433,25 @@
+ 		} else if ((pos = strstr(uri, ".gif")) != NULL) {
+ 			pos = strrchr((char *)uri, '/');
  			snprintf((char *)fnbuf, sizeof(fnbuf), "%s%s%s%s%s",
- 			    FPBASE, _EXES, _VTI_ADM, _IMAGES, pos);
- 		/*
+-			    FPBASE, _EXES, _VTI_ADM, _IMAGES, pos);
++			    FPBASE, _EXES, _VTI_BIN, _IMAGES, pos);
++		/*
 +		 * Check to see if the user is changing the password,
 +		 * which has a URL like _vti_bin/_vti_aut/ passwd.htm.
 +		 * It's different from other ".htm" files because it
@@ -21,7 +24,6 @@
 +			pos = strrchr((char *)uri, '/');
 +			snprintf((char *)fnbuf, sizeof(fnbuf), "%s%s%c%04d%s",
 +     			    FPBASE, _ADMIN, '/', Lcid, pos);
-+		/*
+ 		/*
  		 * If we still have .htm or .css, we end up
  		 * with the admin dir. If we match, we calculate
- 		 * the Lcid from the string.
