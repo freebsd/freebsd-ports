dae67fa48da00c3dd0a2f1a366f297ebc3200682
Jeremy Messenger <mezz@FreeBSD.org>
2005-04-22 21:50:11 +0000

    Update to 2.10.3.

--- archivers/file-roller/files/patch-src_fr-command-tar.c
+++ archivers/file-roller/files/patch-src_fr-command-tar.c
@@ -1,11 +1,13 @@
---- src/fr-command-tar.c.orig	Sun Feb 20 17:17:15 2005
-+++ src/fr-command-tar.c	Sun Feb 20 17:19:07 2005
-@@ -219,7 +219,7 @@
- 	/* In solaris gtar is present under /usr/sfw/bin */
- 
- 	prev_path = g_getenv ("PATH");
--	temp = g_strdup_printf ("PATH=%s:%s", prev_path, "/usr/sfw/bin");
-+	temp = g_strdup_printf ("PATH=%s:%s:%s", "%%LOCALBASE%%/bin", prev_path, "/usr/sfw/bin");
- 	putenv (temp);
- 
- 	path = g_find_program_in_path ("gtar");
+--- src/fr-command-tar.c.orig	Fri Apr 22 13:11:56 2005
++++ src/fr-command-tar.c	Fri Apr 22 13:16:00 2005
+@@ -221,6 +221,10 @@
+ 	if (g_file_test ("/usr/sfw/bin/gtar", G_FILE_TEST_IS_EXECUTABLE)) {
+ 		command = g_strdup ("/usr/sfw/bin/gtar");
+ 	}
++#elif defined(__FreeBSD__)
++	if (g_file_test ("%%LOCALBASE%%/bin/gtar", G_FILE_TEST_IS_EXECUTABLE)) {
++		command = g_strdup ("%%LOCALBASE%%/bin/gtar");
++	}
+ #endif
+ 	if (command != NULL)
+ 		fr_process_begin_command (comm->process, command);
