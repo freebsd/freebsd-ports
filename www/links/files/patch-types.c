c0b4d1f37c8700b5337ff17baa2634bbc4b09040
Dmitry Sivachenko <demon@FreeBSD.org>
2005-04-22 07:24:26 +0000

    Update to version 2.1.p17.
    PR:		79199
    Submitted by:	Renato Botelho <freebsd@galle.com.br>

--- www/links/files/patch-types.c
+++ www/links/files/patch-types.c
@@ -1,12 +1,12 @@
---- types.c.orig	Sat Mar  1 19:06:39 2003
-+++ types.c	Sat Mar  1 19:07:45 2003
-@@ -705,7 +705,8 @@
+--- types.c.orig	Thu Mar 24 10:36:47 2005
++++ types.c	Thu Mar 24 10:38:12 2005
+@@ -796,7 +796,8 @@
  		if (*ct == '.') ext = ct + 1;
  		else if (dir_sep(*ct)) ext = NULL;
- 	if (ext) while (ext[extl] && !dir_sep(ext[extl]) && !end_of_dir(ext[extl])) extl++;
+ 	if (ext) while (ext[extl] && !dir_sep(ext[extl]) && !end_of_dir(url, ext[extl])) extl++;
 -	if ((extl == 3 && !casecmp(ext, "htm", 3)) ||
 +	if (force_html ||
 +	    (extl == 3 && !casecmp(ext, "htm", 3)) ||
  	    (extl == 4 && !casecmp(ext, "html", 4))) return stracpy("text/html");
- 	foreach(e, extensions) if (is_in_list(e->ext, ext, extl)) return stracpy(e->ct);
- 
+ 	foreach(e, extensions) {
+ 		unsigned char *fname = NULL;
