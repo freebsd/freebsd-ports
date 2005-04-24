5e3c948c93f1ee3c1e90d580c6a2c87443960f24
markus <markus@FreeBSD.org>
2005-04-24 10:43:59 +0000

    Unbreak by fixing the build with gcc 3.4
    No objections by: maintainer

---
+++ sysutils/barry/files/patch-portlistview.cc
@@ -0,0 +1,11 @@
+--- src/portlistview.cc.orig	Sun Apr 24 02:22:33 2005
++++ src/portlistview.cc	Sun Apr 24 02:22:41 2005
+@@ -42,7 +42,7 @@
+ 			QListViewItem *m_oldItem;
+ 			QListViewItem *m_newItem;
+ 	};
+-};
++}
+ 
+ using namespace Barry;
+ 
