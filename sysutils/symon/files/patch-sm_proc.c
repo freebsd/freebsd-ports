42be62beba4ae1a5f4aec2788c0ca0c7e8c173fb
Volker Stolz <vs@FreeBSD.org>
2005-04-26 14:32:53 +0000

    - Update to 2.71 - Add rcNG scripts, donated by J. Martin Petersen (jmp at alvorlig dot dk) - Add WITHOUT_SYMON/WITHOUT_SYMUX flags
    PR:		ports/80339
    Submitted by:	maintainer

--- sysutils/symon/files/patch-sm_proc.c
+++ sysutils/symon/files/patch-sm_proc.c
@@ -1,18 +1,15 @@
---- platform/FreeBSD/sm_proc.c.orig	Thu Mar 10 09:29:00 2005
-+++ platform/FreeBSD/sm_proc.c	Thu Mar 10 09:27:52 2005
-@@ -181,9 +181,15 @@
- 	      if (proc_kd) {
- 		   if (kvm_read(proc_kd, (unsigned long)pp->ki_paddr, &pproc,
- 				  sizeof(pproc)) == sizeof(pproc)) {
-+#ifdef HAS_RUSAGE_EXT
-+			cpu_uticks += pproc.p_rux.rux_uticks;  /* user */
-+			cpu_sticks += pproc.p_rux.rux_sticks;  /* sys  */
-+			cpu_iticks += pproc.p_rux.rux_iticks;  /* int  */
-+#else
- 			cpu_uticks += pproc.p_uticks;  /* user */
- 			cpu_sticks += pproc.p_sticks;  /* sys  */
- 			cpu_iticks += pproc.p_iticks;  /* int  */
-+#endif
- 		   } else {
- 			warning("while reading kvm: %s", kvm_geterr(proc_kd));
- 		   }
+--- platform/FreeBSD/sm_proc.c.orig	Mon Apr 25 17:34:28 2005
++++ platform/FreeBSD/sm_proc.c	Mon Apr 25 17:34:43 2005
+@@ -182,9 +182,9 @@
+ 		if (kvm_read(proc_kd, (unsigned long)pp->ki_paddr, &pproc,
+ 			     sizeof(pproc)) == sizeof(pproc)) {
+ #ifdef HAS_RUSAGE_EXT
+-		    cpu_uticks += pproc.p_rux.p_uticks;  /* user */
+-		    cpu_sticks += pproc.p_rux.p_sticks;  /* sys  */
+-		    cpu_iticks += pproc.p_rux.p_iticks;  /* int  */
++		    cpu_uticks += pproc.p_rux.rux_uticks;  /* user */
++		    cpu_sticks += pproc.p_rux.rux_sticks;  /* sys  */
++		    cpu_iticks += pproc.p_rux.rux_iticks;  /* int  */
+ #else
+ 		    cpu_uticks += pproc.p_uticks;  /* user */
+ 		    cpu_sticks += pproc.p_sticks;  /* sys  */
