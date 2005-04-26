d00065c3e14339c1f5c45ca90f8a359f1596047b
Sam Lawrance <lawrance@FreeBSD.org>
2005-04-26 12:45:31 +0000

    Update the entertaining gnofract4d to version 2.6. - contain the installation to its own DATADIR, rather than spreading   bits everywhere
    PR:		ports/76868
    Submitted by:	Edwin Young <edwin@bathysphere.org> (author)
    Approved by:	clement (mentor)

--- graphics/gnofract4d/files/patch-src::image.cpp
+++
@@ -1,9 +0,0 @@
---- src/image.cpp.orig	Mon Mar 17 15:00:25 2003
-+++ src/image.cpp	Sun Nov  2 23:13:40 2003
-@@ -1,4 +1,5 @@
--#include <stdlib.h>
-+#include <cassert>
-+#include <cstdlib>
- 
- #include "image.h"
- #include "iterFunc.h"
