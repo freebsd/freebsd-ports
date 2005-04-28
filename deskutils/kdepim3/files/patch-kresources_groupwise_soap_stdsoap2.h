17f8b586d7c7cf6bceb638f450700de450da62b5
Michael Nottebrock <lofi@FreeBSD.org>
2005-04-28 17:28:46 +0000

    Fix build on alpha.
    Obtained from: KDE CVS

---
+++ deskutils/kdepim3/files/patch-kresources_groupwise_soap_stdsoap2.h
@@ -0,0 +1,14 @@
+--- kresources/groupwise/soap/stdsoap2.h	2004/12/03 17:15:43	1.2
++++ kresources/groupwise/soap/stdsoap2.h	2005/04/28 13:00:14	1.2.2.2
+@@ -163,10 +163,6 @@
+ # define HP_UX
+ #endif
+ 
+-#ifdef __alpha
+-# define TRU64 
+-#endif
+-
+ #ifdef __MVS__
+ # define OS390
+ #endif
+
