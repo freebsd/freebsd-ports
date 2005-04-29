f664976049a14b066bc0b164695ba87fed867627
jylefort <jylefort@FreeBSD.org>
2005-04-29 04:25:00 +0000

    - Update to 0.3.4 - Replace some patches by ${REINPLACE_CMD} - Fix the packing list - Depend on freetype - Fix build on 5.X
    PR:		ports/80375
    Submitted by:	maintainer

---
+++ games/adonthell/files/patch-src_map__event__handler.cc
@@ -0,0 +1,13 @@
+--- src/map_event_handler.cc.orig	Fri Apr 29 05:30:43 2005
++++ src/map_event_handler.cc	Fri Apr 29 05:30:44 2005
+@@ -19,9 +19,9 @@
+  * @brief Implements the map_event_handler class.
+  */
+ 
+-#include <algorithm>
+ #include "map_event.h"
+ #include "map_event_handler.h"
++#include <algorithm>
+ 
+ 
+ // See whether a matching event is registered and execute the
