bc16950b05a9b8f3597f9b6d1863901d4a346920
Vanilla I. Shu <vanilla@FreeBSD.org>
2005-04-29 13:58:52 +0000

    - fix setpgrp bug - update to 0.9.2
    PR:		ports/80398
    Submitted by:	chinsan <chinsan_at_mail2000.com.tw>
    Approved by:	maintainer

--- chinese/gcin/files/patch-gcin-im-client.c
+++ chinese/gcin/files/patch-gcin-im-client.c
@@ -1,11 +1,11 @@
---- im-client/gcin-im-client.c.orig	Thu Apr  7 09:18:13 2005
-+++ im-client/gcin-im-client.c	Thu Apr  7 09:18:27 2005
+--- im-client/gcin-im-client.c.orig	Fri Mar 25 16:52:27 2005
++++ im-client/gcin-im-client.c	Sat Apr 23 21:43:56 2005
 @@ -71,7 +71,7 @@
  #if 	FREEBSD
          setgrp();
  #else
 -        setpgrp();
-+        setpgrp(0, 0);
++        setpgrp(0, getpid());
  #endif
          execl(execbin, "gcin", NULL);
        } else {
