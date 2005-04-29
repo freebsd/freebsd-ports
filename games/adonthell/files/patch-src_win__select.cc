751c5ce4733f974dfb7bf394dc37c1fb3760ca4f
Jean-Yves Lefort <jylefort@FreeBSD.org>
2005-04-29 04:25:00 +0000

    - Update to 0.3.4 - Replace some patches by ${REINPLACE_CMD} - Fix the packing list - Depend on freetype - Fix build on 5.X
    PR:		ports/80375
    Submitted by:	maintainer

---
+++ games/adonthell/files/patch-src_win__select.cc
@@ -0,0 +1,13 @@
+--- src/win_select.cc.orig	Fri Apr 29 05:39:00 2005
++++ src/win_select.cc	Fri Apr 29 05:39:04 2005
+@@ -12,9 +12,9 @@
+    See the COPYING file for more details
+ */
+ 
++#include "audio.h"
+ #include "win_select.h"
+ 
+-#include "audio.h"
+ 
+ win_select::win_select()
+ {
