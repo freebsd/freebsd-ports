4f5868bcf21f0c59e8fff2e2bd7446966fce5fd2
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2005-04-30 06:41:10 +0000

    Fix build error on 4-stable. [*] Move macro definitions from extra-patch-config.h.in to patch-src:xine-engine:xine_internal.h.
    Submitted by:	glewis [*]

--- multimedia/libxine/files/patch-src:xine-engine:xine_internal.h
+++ multimedia/libxine/files/patch-src:xine-engine:xine_internal.h
@@ -1,6 +1,6 @@
---- src/xine-engine/xine_internal.h.orig	Thu Dec 30 11:02:18 2004
-+++ src/xine-engine/xine_internal.h	Thu Dec 30 11:20:56 2004
-@@ -70,6 +70,42 @@
+--- src/xine-engine/xine_internal.h.orig	Tue Apr 26 17:09:12 2005
++++ src/xine-engine/xine_internal.h	Sat Apr 30 14:16:33 2005
+@@ -70,6 +70,77 @@
  #  include <xine/info_helper.h>
  #endif
  
@@ -12,9 +12,37 @@
 +#define INT8_MAX	0x7f
 +#endif
 +
++#ifndef INT16_MAX
++#define INT16_MAX 0x7fff
++#endif
++
++#ifndef INT16_MIN
++#define INT16_MIN (-0x7fff-1)
++#endif
++
++#ifndef INT32_MAX
++#define INT32_MAX 0x7fffffff
++#endif
++
++#ifndef INT32_MIN
++#define INT32_MIN (-0x7fffffff-1)
++#endif
++
 +#ifndef PRIiMAX
 +#define PRIiMAX	"lld"
 +typedef long long intmax_t;
++#endif
++
++#ifndef PRId64
++#if defined(__alpha__) || defined(__amd64__) || defined(__ia64__) || defined(__sparc64__)
++#define PRId64 "ld"
++#else if defined(__i386__) || defined(__powerpc__)
++#define PRId64 "lld"
++#endif
++#endif
++
++#ifndef PRIdMAX
++#define PRIdMAX	"lld"
 +#endif
 +
 +#ifndef	SCNdMAX
@@ -37,9 +65,16 @@
 +#define	PRIXMAX	"llX"
 +#endif
 +
++#ifndef PRIxMAX
++#define	PRIxMAX	"llx"
++#endif
++
 +#ifndef UINT64_C
 +#define	UINT64_C(c)	(c ## ULL)
 +#endif
++
++
++
  
  #define XINE_MAX_EVENT_LISTENERS         50
  #define XINE_MAX_EVENT_TYPES             100
