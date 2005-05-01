0df69780320818289a96d66f85b3ed95fbc8ff2b
nork <nork@FreeBSD.org>
2005-05-01 07:39:11 +0000

    o Update to 0.7.0. [1] o Remove hard samba dependency. [2]
    PR:		ports/80486
    Submitted by:	Jung-uk Kim <jkim@niksun.com> [1]
    		Juergen Lock <nox@jelal.kn-bremen.de> (maintainer) [1]
    Suggested by:	des [2]

--- emulators/qemu-devel/files/patch-target-sparc::op_helper.c
+++
@@ -1,8 +0,0 @@
---- qemu/target-sparc/op_helper.c.orig	Mon Oct 11 02:46:24 2004
-+++ qemu/target-sparc/op_helper.c	Thu Oct 28 23:28:57 2004
-@@ -1,5 +1,4 @@
- #include <math.h>
--#include <fenv.h>
- #include "exec.h"
- 
- #ifdef USE_INT_TO_FLOAT_HELPERS
