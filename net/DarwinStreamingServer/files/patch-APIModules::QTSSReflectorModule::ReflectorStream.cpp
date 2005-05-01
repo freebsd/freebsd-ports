6be179d0a0526e6d2a29bb7b9a1d9259d2b6f056
nork <nork@FreeBSD.org>
2005-05-01 11:21:20 +0000

    o Change INADDR_ANY to the IP address specified.   DSS uses the IP addess where it is bind to and not the   IP addess of the default interface. o Use the correct port number and not a generated one. o Bump PORTREVISION, accordingly.
    These are important for Broadcaster behind a NAT.
    
    Submitted by:	Denis Ahrens <denis@berlin.ccc.de>
    References::	http://lists.apple.com/archives/streaming-server-dev/2004/May/msg00000.html
    Sponsored by:	Ensure Technology Ltd.

--- net/DarwinStreamingServer/files/patch-APIModules::QTSSReflectorModule::ReflectorStream.cpp
+++ net/DarwinStreamingServer/files/patch-APIModules::QTSSReflectorModule::ReflectorStream.cpp
@@ -1,5 +1,5 @@
 --- APIModules/QTSSReflectorModule/ReflectorStream.cpp.orig	Thu Jun 10 09:03:34 2004
-+++ APIModules/QTSSReflectorModule/ReflectorStream.cpp	Fri Jan 21 10:23:11 2005
++++ APIModules/QTSSReflectorModule/ReflectorStream.cpp	Sun May  1 19:53:44 2005
 @@ -179,7 +179,7 @@
      theRRWriter++;
      *theRRWriter = htonl(theSsrc);
@@ -9,3 +9,26 @@
      theRRWriter++;
      *theRRWriter = htonl(0);
      theRRWriter++;
+@@ -381,11 +381,11 @@
+     // specified by the source info, we can use that to demultiplex separate broadcasts on
+     // the same port. If the src IP addr is 0, we cannot do this and must dedicate 1 port per
+     // broadcast
+-    fSockets = sSocketPool.GetUDPSocketPair(INADDR_ANY, fStreamInfo.fPort, fStreamInfo.fSrcIPAddr, 0);
++    fSockets = sSocketPool.GetUDPSocketPair(fStreamInfo.fDestIPAddr, fStreamInfo.fPort, fStreamInfo.fSrcIPAddr, 0);
+     if ((fSockets == NULL) && fStreamInfo.fSetupToReceive)
+     {
+         fStreamInfo.fPort = 0;
+-        fSockets = sSocketPool.GetUDPSocketPair(INADDR_ANY, fStreamInfo.fPort, fStreamInfo.fSrcIPAddr, 0);
++        fSockets = sSocketPool.GetUDPSocketPair(fStreamInfo.fDestIPAddr, fStreamInfo.fPort, fStreamInfo.fSrcIPAddr, 0);
+     }
+     if (fSockets == NULL)
+         return QTSSModuleUtils::SendErrorResponse(inRequest, qtssServerInternal,
+@@ -1388,7 +1388,7 @@
+         {   
+             // Check to see if we need to set the remote RTCP address
+             // for this stream. This will be necessary if the source is unicast.
+-            if ((theRemoteAddr != 0) && (theSender->fStream->fDestRTCPAddr == 0))
++            if ((theRemoteAddr != 0) && (theSender->fStream->fDestRTCPAddr == 0) && (thePacket->IsRTCP()))
+             {
+                 // If the source is multicast, this shouldn't be necessary
+                 Assert(!SocketUtils::IsMulticastIPAddr(theSender->fStream->fStreamInfo.fDestIPAddr));
