e244b1d6cecb6584cf815eedaecfebdf752962a0
Vanilla I. Shu <vanilla@FreeBSD.org>
2005-05-01 00:42:58 +0000

    add gb2312 font support.
    PR:		ports/80482
    Submitted by:	maintainer

--- chinese/kon2/files/patch-lib::coding.c
+++ chinese/kon2/files/patch-lib::coding.c
@@ -1,20 +1,24 @@
---- lib/coding.c.orig	Sat May  3 01:39:46 2003
-+++ lib/coding.c	Sat May  3 01:41:08 2003
-@@ -43,10 +43,10 @@
+--- lib/coding.c.orig	Fri Jan 24 15:08:40 1997
++++ lib/coding.c	Sun May  1 01:53:52 2005
+@@ -120,7 +120,7 @@
  
- static u_int GB2312(u_char ch1, u_char ch2)
- {
--    if (ch1 > 0x29)
--	return(((ch1 - 0x27) * 94 + ch2 - 0x21) << 5);
-+    if (ch1 < 0x80)
-+	return(((ch1 - 0x81) * 191 + ch2 - 0x40) << 5);
-     else
--	return(((ch1 - 0x21) * 94 + ch2 - 0x21) << 5);
-+	return(((ch1 - 0x81) * 191 + 63 + ch2 - 0x80) << 5);
- }
- 
- static u_int BIG5(u_char ch1, u_char ch2)
-@@ -135,7 +135,7 @@
+ struct fontRegs fDRegs[] = {
+     /* DF_GB2312 */
+-    {  GB2312, 0,  "GB2312.1980-0", NULL, 0, 0, 'A', 0, 0},
++    {  FldGB2312, 0,  "GB2312.1980-0", NULL, 0, 0, 'A', 0, 0},
+     /* DF_JISX0208 */
+     {JISX0208, 0,"JISX0208.1983-0", NULL, 0, 0, 'B', 0, 0},
+     /* DF_KSC5601 */
+@@ -128,19 +128,19 @@
+     /* DF_JISX0212 */
+     {JISX0208, 0,       "JISX0212", NULL, 0, 0, 'D', 0, 0},
+     /* DF_BIG5_0 */
+-    {    BIG5, 0,     "BIG5.HKU-0", NULL, 0, 0, '0', 0, 0},
++    {    BIG5, 0,         "BIG5-0", NULL, 0, 0, '0', 0, 0},
+     /* DF_BIG5_1 */
+-    {    BIG5, 0,     "BIG5.HKU-0", NULL, 0, 0, '1', 0, 0},
++    {    BIG5, 0,         "BIG5-0", NULL, 0, 0, '1', 0, 0},
+     {    NULL, 0,             NULL, NULL, 0, 0,   0, 0, 0}
  };
  
  struct fontLoaderRegs fldDRegs[] = {
@@ -23,3 +27,10 @@
      {FldJISX0208, 0x7424},
      { FldKSC5601, 0x7D7E},
      {FldJISX0208, 0x7424},
+-    {       BIG5,      0},
+-    {       BIG5,      0},
++    {       BIG5, 0xfefe},
++    {       BIG5, 0xfefe},
+     {       NULL,      0}
+ };
+ 
