4a427c034d0a752f99e44f3f94490fb0e819cdf0
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-05-03 05:54:15 +0000

    Update to 2.3.1.

--- mail/balsa/files/patch-libbalsa_imap_imap-handle.c
+++ mail/balsa/files/patch-libbalsa_imap_imap-handle.c
@@ -1,13 +1,7 @@
---- libbalsa/imap/imap-handle.c.orig	Sun Feb 20 13:54:21 2005
-+++ libbalsa/imap/imap-handle.c	Sat Mar 19 14:40:45 2005
-@@ -18,10 +18,13 @@
-  */
- #include "config.h"
+--- libbalsa/imap/imap-handle.c.orig	Sun May  1 17:37:58 2005
++++ libbalsa/imap/imap-handle.c	Tue May  3 01:39:13 2005
+@@ -23,6 +23,7 @@
  
-+#ifndef __FreeBSD__
- #define _POSIX_SOURCE 1
-+#endif
- #define _BSD_SOURCE   1
  #include <sys/types.h>
  #include <sys/socket.h>
 +#include <netinet/in.h>
