950120851f5bd0ec098219fc0b52589c27ac1b49
Pav Lucistnik <pav@FreeBSD.org>
2005-05-05 21:05:12 +0000

    - Update to 3.5.0.20050428.1100
    PR:		ports/80443
    Submitted by:	Ion-Mihai "IOnut" Tetcu <itetcu@people.tecnik93.com> (maintainer)

--- mail/dspam-devel/files/patch-dspam.c
+++ mail/dspam-devel/files/patch-dspam.c
@@ -1,23 +1,23 @@
---- src/dspam.c.dist	Sun Feb 20 22:42:47 2005
-+++ src/dspam.c	Sun Feb 20 22:46:22 2005
-@@ -2543,8 +2543,9 @@
-           LOG (LOG_CRIT, ERROR_MEM_ALLOC);
-       }
- 
--      if (CTX->result == DSR_ISSPAM)
--      {
-+// FreeBSD patch: put the user in all emails, extremely usefull for scripts
-+//      if (CTX->result == DSR_ISSPAM)
-+//      {
-         snprintf(data, sizeof(data), "X-DSPAM-User: %s", CTX->username);
-         head = _ds_create_header_field(data);
-         if (head != NULL)
-@@ -2556,7 +2557,7 @@
+--- src/dspam.c.dist	Wed Apr 27 14:45:51 2005
++++ src/dspam.c	Wed Apr 27 14:47:50 2005
+@@ -2966,8 +2966,9 @@
+             LOG (LOG_CRIT, ERR_MEM_ALLOC);
          }
-         else
-           LOG (LOG_CRIT, ERROR_MEM_ALLOC);
--      }
-+//      }  // end of FreeBSD patch
  
-       if (!strcmp(_ds_pref_val(PTX, "showFactors"), "on")) {
-
+-        if (CTX->result == DSR_ISSPAM)
+-        {
++// FreeBSD port patch: put the user in all emails, extremely usefull for scripts
++//        if (CTX->result == DSR_ISSPAM)
++//        {
+           snprintf(data, sizeof(data), "X-DSPAM-User: %s", CTX->username);
+           head = _ds_create_header_field(data);
+           if (head != NULL)
+@@ -2979,7 +2980,7 @@
+           }
+           else
+             LOG (LOG_CRIT, ERR_MEM_ALLOC);
+-        }
++//        } // // end of FreeBSD port patch
+ 
+         if (!strcmp(_ds_pref_val(ATX->PTX, "showFactors"), "on")) {
+ 
