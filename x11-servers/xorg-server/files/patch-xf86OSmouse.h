940df5f29e79856590962a780e345bf19783f03d
lesi <lesi@FreeBSD.org>
2005-05-09 07:22:57 +0000

    Bump max number of buttons supported by mouse driver to 16 so Logitech MediaPlay mouse works. (Xorg Bugzilla #2389)
    PR:		ports/80744
    Submitted by:	cy

---
+++ x11-servers/xorg-server/files/patch-xf86OSmouse.h
@@ -0,0 +1,11 @@
+--- programs/Xserver/hw/xfree86/os-support/xf86OSmouse.h.orig	Sat Jul 24 10:35:39 2004
++++ programs/Xserver/hw/xfree86/os-support/xf86OSmouse.h	Sat May  7 20:47:45 2005
+@@ -281,7 +281,7 @@
+ /* Generalize for other axes. */
+ #define MSE_NOAXISMAP	MSE_NOZMAP
+ 
+-#define MSE_MAXBUTTONS	12
++#define MSE_MAXBUTTONS	16
+ #define MSE_DFLTBUTTONS	 3
+ 
+ #endif /* _XF86OSMOUSE_H_ */
