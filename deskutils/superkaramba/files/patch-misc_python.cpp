f03b2f3ce05cd40243f7fc0f6aec3536e03f038f
Markus Brueffer <markus@FreeBSD.org>
2005-05-10 22:29:44 +0000

    - Fix a bug in the Python interface for getting the IP address - Bump PORTREVISION
    Reported by:	Midian of BSDForen.de

--- deskutils/superkaramba/files/patch-misc_python.cpp
+++ deskutils/superkaramba/files/patch-misc_python.cpp
@@ -1,6 +1,6 @@
---- src/misc_python.cpp.orig	Fri Nov 19 13:31:33 2004
-+++ src/misc_python.cpp	Fri Nov 19 13:37:12 2004
-@@ -380,6 +380,9 @@
+--- src/misc_python.cpp.orig	Wed Apr 20 00:55:42 2005
++++ src/misc_python.cpp	Wed May 11 00:05:48 2005
+@@ -385,6 +385,9 @@
  #include <sys/ioctl.h>
  #include <net/if.h>
  #include <arpa/inet.h>
@@ -10,3 +10,12 @@
  
  /* now a method we need to expose to Python */
  QString getIp(char *device_name)
+@@ -435,7 +438,7 @@
+     /* devptr points into an array of ifreq structs. */
+     devptr = &ifc_conf.ifc_req[i];
+ 
+-    if (devptr->ifr_addr.sa_family != AF_INET)
++    if (ioctl(sd, SIOCGIFADDR, devptr) < 0 || devptr->ifr_addr.sa_family != AF_INET)
+     continue;
+ 
+     if (ioctl(sd,SIOCGIFFLAGS,devptr) < 0)
