c58a90975c60ecdacbaa1639511dfc10fef87230
pav <pav@FreeBSD.org>
2005-05-11 15:04:48 +0000

    - Update to 1.0.0beta3
    PR:		ports/80884
    Submitted by:	Hendrik Scholz <hendrik@scholz.net> (maintainer)

--- multimedia/transcode/files/patch-libvo_yuv2rgv.c
+++ multimedia/transcode/files/patch-libvo_yuv2rgv.c
@@ -1,11 +1,11 @@
---- libvo/yuv2rgb.c.orig	Wed Feb 23 05:08:14 2005
-+++ libvo/yuv2rgb.c	Wed Feb 23 05:09:12 2005
-@@ -71,7 +71,7 @@
- void yuv2rgb_init (int bpp, int mode) 
+--- libvo/yuv2rgb.c.orig	Sun May  8 16:57:56 2005
++++ libvo/yuv2rgb.c	Sun May  8 16:58:18 2005
+@@ -72,7 +72,7 @@
  {
      yuv2rgb = NULL;
--#if defined(ARCH_X86) || defined(ARCH_X86_64)
-+#if defined(ARCH_X86)
+ #ifdef HAVE_MMX
+-# if defined(ARCH_X86) || defined(ARCH_X86_64)
++# if defined(ARCH_X86)
      if ((yuv2rgb == NULL) && (vo_mm_accel & MM_ACCEL_X86_MMXEXT)) {
  	yuv2rgb = yuv2rgb_init_mmxext (bpp, mode);
  	if (yuv2rgb != NULL)
