7e9756663ee91f61e896ac8468857e4e2b6f17ee
tg <tg@FreeBSD.org>
2005-05-11 11:06:08 +0000

    Upgrade to 0.6.8.

---
+++ games/freedoko/files/patch-constants.h
@@ -0,0 +1,12 @@
+$FreeBSD$
+
+--- constants.h~	Mon Apr 11 09:20:09 2005
++++ constants.h	Tue May 10 18:26:28 2005
+@@ -43,6 +43,7 @@
+ #include <fstream>
+ #include <sstream>
+ #include <algorithm>
++#include <cmath>
+ 
+ using std::cout;
+ using std::cerr;
