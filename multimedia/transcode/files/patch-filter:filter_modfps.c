f025a0b0f326d78f5d210c4ef249a044830efc7c
Pav Lucistnik <pav@FreeBSD.org>
2005-05-11 15:04:48 +0000

    - Update to 1.0.0beta3
    PR:		ports/80884
    Submitted by:	Hendrik Scholz <hendrik@scholz.net> (maintainer)

--- multimedia/transcode/files/patch-filter:filter_modfps.c
+++ multimedia/transcode/files/patch-filter:filter_modfps.c
@@ -1,16 +1,17 @@
---- filter/filter_modfps.c.old	Sat Aug 21 13:11:20 2004
-+++ filter/filter_modfps.c	Sat Aug 21 13:13:33 2004
-@@ -77,6 +77,13 @@
- #include "framebuffer.h"
- #include "optstr.h"
+--- filter/filter_modfps.c.orig	Sun May  8 16:55:54 2005
++++ filter/filter_modfps.c	Sun May  8 16:57:01 2005
+@@ -62,7 +62,14 @@
+ //#define DEBUG 1
  
-+#if !defined(HAVE_LRINTF)
+ // FIXME
++#if !defined(HAV_LRINTF)
 +static inline long int lrint(double x)
 +{
-+    return (long)(rint(x));
++	return (long)(rint(x));
 +}
++#else
+ extern long int lrint(double x);
 +#endif
-+
+ 
  static int show_results=0;
  
- /*-------------------------------------------------
