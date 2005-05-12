eecce1bc93e1544335518399607fc16a5db769c4
alfred <alfred@FreeBSD.org>
2005-05-12 17:48:44 +0000

    Perl module for authenticating talking to Paypal.

---
+++ security/p5-Business-PayPal-EWP/files/patch-EWP.pm
@@ -0,0 +1,10 @@
+--- lib/Business/PayPal/EWP.pm.old	Thu May 12 10:42:19 2005
++++ lib/Business/PayPal/EWP.pm	Thu May 12 10:42:39 2005
+@@ -65,6 +65,7 @@
+ 	goto END;
+     }
+     Net::SSLeay::BIO_free($bio);
++    $bio = undef;
+     # Reformat
+     $formdata=~s/,/\n/g;
+     # Encrypt and sign
---
+++ security/p5-Business-PayPal-EWP/files/patch-EWP.xs
@@ -0,0 +1,13 @@
+--- EWP.xs.old	Thu May 12 10:40:14 2005
++++ EWP.xs	Thu May 12 10:41:55 2005
+@@ -126,8 +126,10 @@
+ 		PKCS7_free(p7);
+ 	if (bio)
+ 		BIO_free_all(bio);
++#if 0
+ 	if (memBio)
+ 		BIO_free_all(memBio);
++#endif
+ 	if (p7bio)
+ 		BIO_free_all(p7bio);
+ 	if (pkey)
