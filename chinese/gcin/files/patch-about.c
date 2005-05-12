0e1b51041af0dff83e76ecfcef6b04dfa74894da
vanilla <vanilla@FreeBSD.org>
2005-05-12 06:51:20 +0000

    Upgrade to 0.9.5.
    PR:		ports/80923
    Submitted by:	chinsan <chinsan@mail2000.com.tw>
    Approved by:	maintainer

--- chinese/gcin/files/patch-about.c
+++ chinese/gcin/files/patch-about.c
@@ -1,5 +1,5 @@
---- about.c.orig	Thu May  5 11:32:39 2005
-+++ about.c	Thu May  5 11:41:34 2005
+--- about.c.orig	Sat Feb 26 21:42:00 2005
++++ about.c	Thu May 12 04:02:17 2005
 @@ -69,10 +69,16 @@
      gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 3);
  
@@ -10,7 +10,7 @@
 -    GtkWidget *label_sf = gtk_label_new ("http://www.csie.nctu.edu.tw/~cp76/gcin");
 +    GtkWidget *label_sf = gtk_label_new ("gcin WWW:\nhttp://www.csie.nctu.edu.tw/~cp76/gcin");
 +    gtk_misc_set_alignment (GTK_MISC (label_sf), 0, 0);
-+    GtkWidget *label_FreeBSD = gtk_label_new ("FeeeBack:\nhttp://cle.linux.org.tw/gcin/");
++    GtkWidget *label_FreeBSD = gtk_label_new ("Feedback:\nhttp://cle.linux.org.tw/gcin/");
 +    gtk_misc_set_alignment (GTK_MISC (label_FreeBSD), 0, 0);
      gtk_box_pack_start(GTK_BOX(vbox), label_sf, FALSE, FALSE, 0);
 +    gtk_box_pack_start(GTK_BOX(vbox), vseparator, FALSE, FALSE, 3);
