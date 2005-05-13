9d515501f75b8ca3efc5db3f947dfd93433dcc12
jylefort <jylefort@FreeBSD.org>
2005-05-13 08:16:40 +0000

    - Update to 0.85 [1] - Fix syntax of "extern C" wrappers in .h files [2] - Use SUB_FILES facility - Polish the Makefile
    PR:		ports/80947
    Submitted by:	maintainer [1], Milan Obuch <ports-bugs@dino.sk> [2]

---
+++ security/clamav/files/patch-libclamav_clamav.h
@@ -0,0 +1,11 @@
+--- libclamav/clamav.h.orig	Fri May 13 09:11:42 2005
++++ libclamav/clamav.h	Fri May 13 09:11:46 2005
+@@ -221,7 +221,7 @@
+ extern const char *cl_perror(int clerror); /* deprecated */
+ 
+ #ifdef __cplusplus
+-};
++}
+ #endif
+  
+ #endif
---
+++ security/clamav/files/patch-libclamav_mspack_mspack.h
@@ -0,0 +1,11 @@
+--- libclamav/mspack/mspack.h.orig	Fri May 13 09:10:53 2005
++++ libclamav/mspack/mspack.h	Fri May 13 09:10:57 2005
+@@ -1488,7 +1488,7 @@
+ };
+ 
+ #ifdef __cplusplus
+-};
++}
+ #endif
+ 
+ #endif
