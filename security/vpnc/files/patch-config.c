fe38222fcb46b65e89e21199f57dfa6477850304
krion <krion@FreeBSD.org>
2005-05-14 12:00:31 +0000

    Update to 0.3.3
    PR:		ports/81008
    Submitted by:	maintainer

--- security/vpnc/files/patch-config.c
+++ security/vpnc/files/patch-config.c
@@ -1,13 +1,13 @@
---- config.c.dist	Wed Nov 17 15:07:24 2004
-+++ config.c	Wed Nov 17 15:07:33 2004
-@@ -479,8 +479,8 @@
- 		}
+--- config.c.orig	Sat May 14 13:20:11 2005
++++ config.c	Sat May 14 13:20:51 2005
+@@ -520,8 +520,8 @@
  	}
  
--	read_config_file("/etc/vpnc/default.conf", config, 1);
--	read_config_file("/etc/vpnc.conf", config, 1);
-+	read_config_file("%%PREFIX%%/etc/vpnc/default.conf", config, 1);
-+	read_config_file("%%PREFIX%%/etc/vpnc.conf", config, 1);
+ 	if (!got_conffile) {
+-		read_config_file("/etc/vpnc/default.conf", config, 1);
+-		read_config_file("/etc/vpnc.conf", config, 1);
++		read_config_file("%%PREFIX%%/etc/vpnc/default.conf", config, 1);
++		read_config_file("%%PREFIX%%/etc/vpnc.conf", config, 1);
+ 	}
  
  	if (!print_config) {
- 		for (i = 0; config_names[i].name != NULL; i++)
