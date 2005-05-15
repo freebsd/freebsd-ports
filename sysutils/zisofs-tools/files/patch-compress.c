3e8c2e9328db01ec0d3a27ca6832e8e6b5dc6d7a
novel <novel@FreeBSD.org>
2005-05-15 18:29:01 +0000

    Update to 1.0.6.
    PR:		81046
    Submitted by:	Andrej Zverev <az@inec.ru>
    Approved by:	maintainer

--- sysutils/zisofs-tools/files/patch-compress.c
+++ sysutils/zisofs-tools/files/patch-compress.c
@@ -1,10 +1,11 @@
---- compress.c.orig	Mon Nov 24 13:26:57 2003
-+++ compress.c	Mon Nov 24 13:27:06 2003
-@@ -13,6 +13,7 @@
+--- compress.c.orig	Tue Jul 31 07:20:52 2001
++++ compress.c	Fri May 13 21:17:02 2005
+@@ -13,7 +13,7 @@
  
  #include <stdlib.h>
  #include <stdio.h>
+-#include <utime.h>
 +#include <time.h>
- #include <utime.h>
  #include <unistd.h>
  #include <zlib.h>
+ #include "mkzftree.h"
