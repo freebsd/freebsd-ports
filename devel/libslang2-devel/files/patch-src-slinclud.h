cb52e251b35f004079fe23762265a54dfc92450e
novel <novel@FreeBSD.org>
2005-05-16 17:13:02 +0000

    Add devel/libslang2 - a multi-platform programmer's library designed to allow a developer to create robust multi-platform software.
    PR:		81110
    Submitted by:	Renato Botelho <freebsd@galle.com.br>

---
+++ devel/libslang2-devel/files/patch-src-slinclud.h
@@ -0,0 +1,11 @@
+--- src/slinclud.h.orig	Mon May 16 16:11:14 2005
++++ src/slinclud.h	Mon May 16 16:11:48 2005
+@@ -20,7 +20,7 @@
+ # include <unistd.h>
+ #endif
+ 
+-#ifdef HAVE_MALLOC_H
++#if 0
+ # include <malloc.h>
+ #endif
+ 
