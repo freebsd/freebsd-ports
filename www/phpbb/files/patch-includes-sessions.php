2bebbc676d7f2efefba8b088bda6b2fbadf90de3
Jean-Yves Lefort <jylefort@FreeBSD.org>
2005-05-16 08:32:03 +0000

    - Update to 2.0.15 [1] - Use SUB_FILES=pkg-message
    PR:		ports/81060 [1]
    Submitted by:	maintainer [1]

--- www/phpbb/files/patch-includes-sessions.php
+++ www/phpbb/files/patch-includes-sessions.php
@@ -1,15 +1,15 @@
---- includes/sessions.php.orig	Sun Jul 20 17:42:23 2003
-+++ includes/sessions.php	Thu May  6 12:02:56 2004
-@@ -147,7 +147,7 @@
+--- includes/sessions.php.orig	Sat May 14 05:04:00 2005
++++ includes/sessions.php	Sun May 15 18:53:48 2005
+@@ -167,7 +167,7 @@
  		$sql = "INSERT INTO " . SESSIONS_TABLE . "
- 			(session_id, session_user_id, session_start, session_time, session_ip, session_page, session_logged_in)
- 			VALUES ('$session_id', $user_id, $current_time, $current_time, '$user_ip', $page_id, $login)";
+ 			(session_id, session_user_id, session_start, session_time, session_ip, session_page, session_logged_in, session_admin)
+ 			VALUES ('$session_id', $user_id, $current_time, $current_time, '$user_ip', $page_id, $login, $admin)";
 -		if ( !$db->sql_query($sql) )
 +		if ( $user_id != ANONYMOUS && !$db->sql_query($sql) )
  		{
  			message_die(CRITICAL_ERROR, 'Error creating new session', '', __LINE__, __FILE__, $sql);
  		}
-@@ -380,4 +380,4 @@
+@@ -421,4 +421,4 @@
  	return $url;
  }
  
