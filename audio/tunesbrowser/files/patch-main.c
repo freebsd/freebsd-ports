6d710f6c174dee4e68db044261279a8ac2000d26
Michael Johnson <ahze@FreeBSD.org>
2005-05-16 18:34:08 +0000

    - Update to 0.3.0 [1] - Add WITH_FAAD to OPTIONS for use of   aac/mp4 audio.
    PR:		ports/81118 [1]
    Submitted by:	Ports Fury [1]

--- audio/tunesbrowser/files/patch-main.c
+++ audio/tunesbrowser/files/patch-main.c
@@ -1,29 +1,6 @@
---- main.c.orig	Mon Jan  3 21:58:29 2005
-+++ main.c	Wed Feb  2 21:56:11 2005
-@@ -38,7 +38,7 @@
- {
-     fprintf(stderr, "Oops! TunesBrowser has crashed. Sorry about that!\n");
-     fprintf(stderr, "This probably won't be of any use unless you feel like debugging,\n");
--    fprintf(stderr, "but the crash occured because of %p being bad.\n", siginfo->si_ptr);
-+    fprintf(stderr, "but the crash occured because of %p being bad.\n", siginfo->si_value.sigval_ptr);
- #if 0
-     fprintf(stderr, "\n\nRaised SIGSTOP. You can now attach a debugger.\n");
-     fprintf(stderr, "Attach to PID %i\n", getpid());
-@@ -66,11 +66,11 @@
-     int ret;
- 
-     sa.sa_handler = (void*)sighandler_sigsegv;
--    sa.sa_flags = SA_NOMASK | SA_SIGINFO;
-+    sa.sa_flags = SA_NODEFER | SA_SIGINFO;
-     ret = sigaction(SIGSEGV, &sa, NULL);
- 
-     sa.sa_handler = (void*)sighandler_sigpipe;
--    sa.sa_flags = SA_NOMASK | SA_SIGINFO;
-+    sa.sa_flags = SA_NODEFER | SA_SIGINFO;
-     ret = sigaction(SIGPIPE, &sa, NULL);
- }
- 
-@@ -94,7 +94,7 @@
+--- main.c.orig	Mon May 16 14:27:54 2005
++++ main.c	Mon May 16 14:28:12 2005
+@@ -129,7 +129,7 @@
  
      gtk_init(&argc, &argv);
  
