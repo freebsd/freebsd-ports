adb7a3e545ec8940ba933bc5f1836d51245d44c9
Pav Lucistnik <pav@FreeBSD.org>
2005-05-17 19:08:10 +0000

    New port: math/pecl-big_int
    Functions for calculations with arbitrary length integers and bitsets
    
    Functions from this package are useful for number theory
    applications. For example, in two-keys cryptography.
    
    PR:		ports/81115
    Submitted by:	Antonio Carlos Venancio Junior <antonio@php.net>

---
+++ math/pecl-big_int/files/patch-libbig_int-include-big_int.h
@@ -0,0 +1,14 @@
+--- libbig_int/include/big_int.h.orig	Wed May 11 16:11:09 2005
++++ libbig_int/include/big_int.h	Tue May 17 21:01:26 2005
+@@ -77,9 +77,9 @@
+ #endif /* end of BIG_INT_DIGIT_SIZE */
+ #else
+ /*
+-    for ANSI C99 compliant comilers, which have stdint.h header
++    for ANSI C99 compliant compilers, which have inttypes.h header
+ */
+-#include <stdint.h>
++#include <inttypes.h>
+ #if (BIG_INT_DIGIT_SIZE == 32)
+ typedef uint32_t big_int_word;
+ typedef uint64_t big_int_dword;
