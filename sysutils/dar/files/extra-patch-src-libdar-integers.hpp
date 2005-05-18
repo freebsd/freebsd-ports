fe7d1b0a94c44370b2e5f2bbfec0b626e47cd784
pav <pav@FreeBSD.org>
2005-05-18 07:54:36 +0000

    - Fix: Headers of dar use some functions of gettext without including the   necessary header libintl.h.  This prevents kdar from getting compiled.
    PR:		ports/81003
    Submitted by:	Kay Lehmann <kay_lehmann@web.de>
    Approved by:	Edson Brandi <ebrandi@fugspbr.org> (maintainer)

---
+++ sysutils/dar/files/extra-patch-src-libdar-integers.hpp
@@ -0,0 +1,10 @@
+--- src/libdar/integers.hpp.orig	Sat Apr 16 11:38:51 2005
++++ src/libdar/integers.hpp	Sat Apr 16 11:39:27 2005
+@@ -31,6 +31,7 @@
+ #define INTEGERS_HPP
+ 
+ #include "../my_config.h"
++#include <libintl.h>
+ 
+ #ifndef OS_BITS
+ 
