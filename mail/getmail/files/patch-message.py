fa999c30c3052fc3debd61c6b853b36117fc3146
Roman Bogorodskiy <novel@FreeBSD.org>
2005-05-19 04:28:16 +0000

    Update to 4.3.9.
    PR:		81211
    Submitted by:	Linh Pham (maintainer)

--- mail/getmail/files/patch-message.py
+++ mail/getmail/files/patch-message.py
@@ -1,43 +1,19 @@
---- getmailcore/message.py.orig	Mon Feb 21 15:57:08 2005
-+++ getmailcore/message.py	Mon Feb 21 16:04:17 2005
-@@ -7,6 +7,7 @@
-     'Message',
- ]
- 
-+import sys
- import os
- import time
+--- getmailcore/message.py.orig	Wed May 18 09:28:26 2005
++++ getmailcore/message.py	Wed May 18 10:41:19 2005
+@@ -12,6 +12,7 @@
  import cStringIO
-@@ -86,20 +87,28 @@
-         # of filters, etc, which should be saner.
-         if fromlines:
-             try:
--                self.__msg = email.message_from_string(os.linesep.join(
--                    fromlines), strict=False)
-+                if sys.version_info < (2, 4):
-+                    self.__msg = email.message_from_string(os.linesep.join(fromlines), strict=False)
-+                else:
-+                    self.__msg = email.message_from_string(os.linesep.join(fromlines))
-             except email.Errors.MessageError, o:
-                 self.__msg = corrupt_message(o, fromlines=fromlines)
-             self.__raw = os.linesep.join(fromlines)
-         elif fromstring:
-             try:
--                self.__msg = email.message_from_string(fromstring, strict=False)
-+                if sys.version_info < (2, 4):
-+                    self.__msg = email.message_from_string(fromstring, strict=False)
-+                else:
-+                    self.__msg = email.message_from_string(fromstring)
-             except email.Errors.MessageError, o:
-                 self.__msg = corrupt_message(o, fromstring=fromstring)
-             self.__raw = fromstring
-         elif fromfile:
-             try:
--                self.__msg = email.message_from_file(fromfile, strict=False)
-+                if sys.version_info < (2, 4):
-+                    self.__msg = email.message_from_file(fromfile, strict=False)
-+                else:
-+                    self.__msg = email.message_from_file(fromfile)
-             except email.Errors.MessageError, o:
-                 # Shouldn't happen
-                 self.__msg = corrupt_message(o, fromstring=fromfile.read())
+ import email
+ import email.Errors
++import email.Parser
+ import email.Utils
+ from email.Generator import Generator
+ 
+@@ -79,7 +80,7 @@
+         self.received_from = None
+         self.received_with = None
+         self.__raw = None
+-        parser = email.Parser.HeaderParser(strict=False)
++        parser = email.Parser.HeaderParser()
+         
+         # Message is instantiated with fromlines for POP3, fromstring for
+         # IMAP (both of which can be badly-corrupted or invalid, i.e. spam,
