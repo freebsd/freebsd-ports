ad20a731c92a224b77850cc6ec57ede539812b6b
barner <barner@FreeBSD.org>
2005-05-19 09:53:43 +0000

    - Update to 0.74
    Submitted by:	Marcus Grando <marcus (a) corp.grupos.com.br>
    PR:		ports/81178

--- databases/tinycdb/files/patch-cdb.c
+++ databases/tinycdb/files/patch-cdb.c
@@ -1,11 +1,11 @@
---- cdb.c.orig	Mon Dec 10 06:18:02 2001
-+++ cdb.c	Mon Mar  3 08:38:57 2003
-@@ -144,7 +144,7 @@
+--- cdb.c.orig	Tue May 17 17:42:12 2005
++++ cdb.c	Tue May 17 17:42:28 2005
+@@ -143,7 +143,7 @@
        if (printf(mode == 'd' ? "+%u,%u:" : "+%u:", klen, vlen) < 0) return -1;
      if (fcpy(f, stdout, klen, &pos, eod) != 0) return -1;
      if (mode == 'd')
 -      if (!fputs(flags & F_MAP ? " " : "->", stdout))
 +      if (fputs(flags & F_MAP ? " " : "->", stdout) == EOF)
- 	return -1;
+         return -1;
      if (fcpy(f, mode == 'd' ? stdout : NULL, vlen, &pos, eod) != 0)
        return -1;
