1865ace86ca32a22ac0443198a95d4da1dda48a1
Pav Lucistnik <pav@FreeBSD.org>
2005-05-20 18:26:05 +0000

    - Update to 4.3.1
    PR:		ports/80904
    Submitted by:	Maxim Tuliuk <mt@primats.org.ua> (maintainer)

--- net-mgmt/ipplan/files/patch-config.php
+++ net-mgmt/ipplan/files/patch-config.php
@@ -1,20 +1,29 @@
---- config.php.org	Sat Nov 20 12:07:00 2004
-+++ config.php	Thu Dec  2 13:47:54 2004
-@@ -96,7 +96,7 @@
+--- config.php.orig	Wed May 11 00:26:32 2005
++++ config.php	Wed May 11 19:30:43 2005
+@@ -89,7 +89,7 @@
+ // see either httpd.conf User directive or ps -ef to see which user
+ // apache runs under, then set mode 700 on directory and change owner
+ // to this user
+-define("UPLOADDIRECTORY", "/var/spool/ipplanuploads");
++define("UPLOADDIRECTORY", "/var/spool/ipplan/uploads");
+ 
+ // default country code ("" for none)
+ define("DEFAULTCOUNTRY", "US");
+@@ -102,7 +102,7 @@
  // not work if php is running in safemode. probing and scanning may 
  // also be against policy for your site!
- //define("NMAP", "");
--define("NMAP", "/usr/bin/nmap");
-+define("NMAP", "/usr/local/bin/nmap");
+ define("NMAP", "");
+-//define("NMAP", "/usr/bin/nmap");
++//define("NMAP", "/usr/local/bin/nmap");
  
  //-------------------------START OF REGISTRAR---------------------------
  
-@@ -137,7 +137,7 @@
+@@ -145,7 +145,7 @@
  //   this will usually be the user under which Apache runs. If the 
  // permissions are not correct, the files will be created in the
  // system temp directory, usually /tmp
 -define("DNSEXPORTPATH", "/tmp/dns/");
-+define("DNSEXPORTPATH", "/tmp/ipplan-dns/");
- //-------------------------START OF LANGUAGES---------------------------
++define("DNSEXPORTPATH", "/var/spool/ipplan/exportdns/");
  
- // which languages are supported by ipplan - see TRANSLATIONS to 
+ // dnsslaveonly is the default setting for when creating new zones. If your
+ // dns server only serves as a slave for most of your zones, it's helpful
