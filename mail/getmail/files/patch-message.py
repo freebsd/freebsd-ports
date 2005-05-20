8f55758337d5c82181122af1b41649019235cc2f
Pav Lucistnik <pav@FreeBSD.org>
2005-05-20 14:06:53 +0000

    - Update to 4.3.10
    PR:		ports/81269
    Submitted by:	Linh Pham <question+fbsdports@closedsrc.org> (maintainer)

--- mail/getmail/files/patch-message.py
+++ mail/getmail/files/patch-message.py
@@ -1,13 +1,5 @@
 --- getmailcore/message.py.orig	Wed May 18 09:28:26 2005
 +++ getmailcore/message.py	Wed May 18 10:41:19 2005
-@@ -12,6 +12,7 @@
- import cStringIO
- import email
- import email.Errors
-+import email.Parser
- import email.Utils
- from email.Generator import Generator
- 
 @@ -79,7 +80,7 @@
          self.received_from = None
          self.received_with = None
