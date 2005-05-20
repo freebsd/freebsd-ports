58640ec2058474e6533a1ec512bcbcc5553f884c
Pav Lucistnik <pav@FreeBSD.org>
2005-05-20 16:26:20 +0000

    - Update to 0.95
    PR:		ports/81276
    Submitted by:	Radek Kozlowski <radek@raadradd.com> (maintainer)

---
+++ ftp/gwget/files/patch-src_gwget_data.c
@@ -0,0 +1,10 @@
+--- src/gwget_data.c.orig	Thu May 19 19:02:00 2005
++++ src/gwget_data.c	Thu May 19 19:02:38 2005
+@@ -31,6 +31,7 @@
+ #include "main_window_cb.h"
+ #include "utils.h"
+ #include "systray.h"
++#include <netinet/in.h>
+ 
+ #include <config.h>
+ 
