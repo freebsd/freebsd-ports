f9da77b6d0e1e3db9ac691d950ce9d316107c908
oliver <oliver@FreeBSD.org>
2005-05-21 07:23:55 +0000

    add a 3rd header-include case - that makes it compile on my amd64 system but... it still coredumpes, so ONLY_FOR_ARCH stays like it is right now

--- multimedia/xawtv/files/patch-libng::plugins::drv0-bsd.c
+++ multimedia/xawtv/files/patch-libng::plugins::drv0-bsd.c
@@ -1,5 +1,20 @@
 --- libng/plugins/drv0-bsd.c.orig	Wed Feb 11 23:13:04 2004
 +++ libng/plugins/drv0-bsd.c	Wed Feb 11 23:13:10 2004
+@@ -23,10 +23,12 @@
+ 
+ #ifdef HAVE_DEV_IC_BT8XX_H
+ # include <dev/ic/bt8xx.h>
+-#endif
+-#ifdef HAVE_MACHINE_IOCTL_BT848_H
++#elif defined(HAVE_MACHINE_IOCTL_BT848_H)
+ # include <machine/ioctl_bt848.h>
+ # include <machine/ioctl_meteor.h>
++#else
++# include <dev/bktr/ioctl_bt848.h>
++# include <dev/bktr/ioctl_meteor.h>
+ #endif
+ 
+ #include "grab-ng.h"
 @@ -599,7 +599,15 @@
  
  static int bsd_tuned(void *handle)
