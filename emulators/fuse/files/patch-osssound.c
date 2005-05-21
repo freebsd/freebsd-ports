2333f925c60c949c9a155492bd228d99e20ef25c
Jean-Yves Lefort <jylefort@FreeBSD.org>
2005-05-21 13:48:32 +0000

    Update to 0.7.0
    PR:		ports/81064
    Submitted by:	Miguel Mendez <flynn@energyhq.es.eu.org>
    Approved by:	maintainer

--- emulators/fuse/files/patch-osssound.c
+++ emulators/fuse/files/patch-osssound.c
@@ -1,5 +1,5 @@
---- osssound.c.orig	Sat Sep 13 16:53:12 2003
-+++ osssound.c	Tue Oct 28 17:19:06 2003
+--- sound/osssound.c.orig	Sat Sep 13 16:53:12 2003
++++ sound/osssound.c	Tue Oct 28 17:19:06 2003
 @@ -107,6 +107,7 @@
    return 1;
    }
