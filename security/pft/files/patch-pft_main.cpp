1799ee7036d2271441ad5397cf61fe2a9b1bd460
jylefort <jylefort@FreeBSD.org>
2005-05-22 17:35:42 +0000

    - Update to 1.3 [1] - Respect ${CXX} and ${CXXFLAGS} - Refactor pkg-plist as PLIST_FILES
    PR:		ports/81357
    Submitted by:	maintainer

---
+++ security/pft/files/patch-pft_main.cpp
@@ -0,0 +1,10 @@
+--- pft/main.cpp.orig	Tue Jan  4 13:01:00 2005
++++ pft/main.cpp	Sun May 22 19:22:22 2005
+@@ -28,6 +28,7 @@
+ #include <sys/types.h>			// open(), close(), write()
+ #include <sys/stat.h>
+ #include <fcntl.h>
++#include <string.h>
+ #endif //UNIX
+ 
+ #include "fxstrings.h"
