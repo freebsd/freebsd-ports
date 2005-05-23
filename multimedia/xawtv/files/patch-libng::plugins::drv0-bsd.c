bf76f2fafc1cf39b567afab62f72bac5fa45168b
Oliver Lehmann <oliver@FreeBSD.org>
2005-05-23 15:08:49 +0000

    - add amd64 to ONLY_FOR_ARCHS - fix build for amd64 - fix a bug on amd64 which caused xawtv to segfault on startup

--- multimedia/xawtv/files/patch-libng::plugins::drv0-bsd.c
+++ multimedia/xawtv/files/patch-libng::plugins::drv0-bsd.c
@@ -1,5 +1,5 @@
---- libng/plugins/drv0-bsd.c.orig	Wed Feb 11 23:13:04 2004
-+++ libng/plugins/drv0-bsd.c	Wed Feb 11 23:13:10 2004
+--- libng/plugins/drv0-bsd.c.orig	Wed Mar 12 17:45:56 2003
++++ libng/plugins/drv0-bsd.c	Mon May 23 17:01:27 2005
 @@ -23,10 +23,12 @@
  
  #ifdef HAVE_DEV_IC_BT8XX_H
@@ -15,7 +15,20 @@
  #endif
  
  #include "grab-ng.h"
-@@ -599,7 +599,15 @@
+@@ -502,10 +504,11 @@
+ static int bsd_read_attr(struct ng_attribute *attr)
+ {
+     struct bsd_handle *h = attr->handle;
+-    int arg, get, set, i;
++    int get, set, i;
++    long arg;
+     int value = -1;
+ 
+     switch (attr->id) {
+     case ATTR_ID_NORM:
+ 	if (-1 != xioctl(h->fd,BT848GFMT,&arg))
+ 	    for (i = 0; i < sizeof(norms_map)/sizeof(int); i++)
+@@ -599,7 +605,15 @@
  
  static int bsd_tuned(void *handle)
  {
