34388d6e0bb12565604781ef3a9ee2e3deff0836
oliver <oliver@FreeBSD.org>
2005-05-23 16:16:15 +0000

    - mark as broken if WITH_MMX is defined and ARCH is != i386 - fix some more 64 bit issues [i]
    Provided by:	Gerd Knorr <kraxel@bytesex.org> [1]

--- multimedia/xawtv/files/patch-libng::plugins::drv0-bsd.c
+++ multimedia/xawtv/files/patch-libng::plugins::drv0-bsd.c
@@ -1,5 +1,5 @@
 --- libng/plugins/drv0-bsd.c.orig	Wed Mar 12 17:45:56 2003
-+++ libng/plugins/drv0-bsd.c	Mon May 23 17:01:27 2005
++++ libng/plugins/drv0-bsd.c	Mon May 23 18:03:01 2005
 @@ -23,10 +23,12 @@
  
  #ifdef HAVE_DEV_IC_BT8XX_H
@@ -15,7 +15,34 @@
  #endif
  
  #include "grab-ng.h"
-@@ -502,10 +504,11 @@
+@@ -127,7 +129,7 @@
+     {  3, "CSVIDEO"      },
+     { -1, NULL }
+ };
+-static int inputs_map[] = {
++static long inputs_map[] = {
+     METEOR_INPUT_DEV1,
+     METEOR_INPUT_DEV0,
+     METEOR_INPUT_DEV_SVIDEO,
+@@ -144,7 +146,7 @@
+     {  6, "RSVD"      },
+     { -1, NULL }
+ };
+-static int norms_map[] = {
++static long norms_map[] = {
+     BT848_IFORM_F_NTSCM,
+     BT848_IFORM_F_NTSCJ,
+     BT848_IFORM_F_PALBDGHI,
+@@ -160,7 +162,7 @@
+     {  2, "Intern"      },
+     { -1, NULL }
+ };
+-static int audio_map[] = {
++static long audio_map[] = {
+     AUDIO_TUNER,
+     AUDIO_EXTERN,
+     AUDIO_INTERN,
+@@ -502,19 +504,20 @@
  static int bsd_read_attr(struct ng_attribute *attr)
  {
      struct bsd_handle *h = attr->handle;
@@ -27,8 +54,38 @@
      switch (attr->id) {
      case ATTR_ID_NORM:
  	if (-1 != xioctl(h->fd,BT848GFMT,&arg))
- 	    for (i = 0; i < sizeof(norms_map)/sizeof(int); i++)
-@@ -599,7 +605,15 @@
+-	    for (i = 0; i < sizeof(norms_map)/sizeof(int); i++)
++	    for (i = 0; i < sizeof(norms_map)/sizeof(*norms_map); i++)
+ 		if (arg == norms_map[i])
+ 		    value = i;
+ 	break;
+     case ATTR_ID_INPUT:
+ 	if (-1 != xioctl(h->fd,METEORGINPUT,&arg))
+-	    for (i = 0; i < sizeof(inputs_map)/sizeof(int); i++)
++	    for (i = 0; i < sizeof(inputs_map)/sizeof(*inputs_map); i++)
+ 		if (arg == inputs_map[i])
+ 		    value = i;
+ 	break;
+@@ -532,7 +535,7 @@
+ 	break;
+     case ATTR_ID_COUNT+1: /* AUDIO */
+ 	if (-1 != xioctl(h->tfd, BT848_GAUDIO, &arg))
+-	    for (i = 0; i < sizeof(audio_map)/sizeof(int); i++)
++	    for (i = 0; i < sizeof(audio_map)/sizeof(*audio_map); i++)
+ 		if (arg == audio_map[i])
+ 		    value = i;
+ 	break;
+@@ -545,7 +548,8 @@
+ static void bsd_write_attr(struct ng_attribute *attr, int value)
+ {
+     struct bsd_handle *h = attr->handle;
+-    int arg, get, set;
++    int get, set;
++    long arg;
+ 
+     switch (attr->id) {
+     case ATTR_ID_NORM:
+@@ -599,7 +603,15 @@
  
  static int bsd_tuned(void *handle)
  {
