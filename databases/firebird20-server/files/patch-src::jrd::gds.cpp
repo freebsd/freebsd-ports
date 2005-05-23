764ca147b69de46e09f7e78d0ca536205dbce9e1
Sergey Skvortsov <skv@FreeBSD.org>
2005-05-23 11:13:23 +0000

    Unbreak firebird-client (firebird-server seems to be still broken):
    * fix patch-src::jrd::gds.cpp
    * use "libdata/firebird" for *.msg
    * install all *.msg files (not only firebird.msg)
    * fix pkg-plist
    * use more strict permissions
    * use INSTALL_DATA instead of INSTALL_PROGRAM to install shared libs and similar files
    * clean Makefile
    * update RELNOTES
    
    PR:		ports/81363
    Submitted by:	skv
    Approved by:	maintainer

--- databases/firebird20-server/files/patch-src::jrd::gds.cpp
+++ databases/firebird20-server/files/patch-src::jrd::gds.cpp
@@ -1,12 +1,12 @@
---- src/jrd/gds.cpp.orig	Fri May 20 16:14:19 2005
-+++ src/jrd/gds.cpp	Fri May 20 16:31:25 2005
-@@ -1736,8 +1736,7 @@
- 			gds__prefix(ib_prefix_msg, "");
+--- src/jrd/gds.cpp.orig	Sun May 22 18:52:41 2005
++++ src/jrd/gds.cpp	Sun May 22 18:55:44 2005
+@@ -1732,8 +1732,8 @@
+ 
+ 	if (ib_prefix_msg == NULL) {
+ 		if (!(ib_prefix_msg = getenv(FB_MSG_ENV))) {
++			strncpy(ib_prefix_msg_val, "%%LIBDATADIR%%", MAXPATHLEN);
+ 			ib_prefix_msg = ib_prefix_msg_val;
+-			gds__prefix(ib_prefix_msg, "");
  		}
  		else {
--			strcat(ib_prefix_msg_val, ib_prefix_msg);
--			ib_prefix_msg = ib_prefix_msg_val;
-+			strcpy(ib_prefix_msg, "%%DATADIR%%");
- 		}
- 	}
- 	strcat(string, ib_prefix_msg);
+ 			strcat(ib_prefix_msg_val, ib_prefix_msg);
