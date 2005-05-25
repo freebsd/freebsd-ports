81368dd220b130cced6c9d36b4df8816f949523f
Jean-Yves Lefort <jylefort@FreeBSD.org>
2005-05-25 09:21:12 +0000

    [1]:   - Update to 1.7   - Submitter takes maintainership
    Moreover:
      - Refactor pkg-plist as PLIST_FILES/PORTDOCS
      - Replace the Makefile patch by USE_GMAKE and post-patch
    
    PR:		ports/80917 [1]
    Submitted by:	Erik Greenwald <erik@smluc.org> (new maintainer) [1]

---
+++ devel/picprog/files/patch-picport.cc
@@ -0,0 +1,11 @@
+--- picport.cc.orig	Thu Apr 29 06:08:10 2004
++++ picport.cc	Wed May 25 11:09:06 2005
+@@ -40,7 +40,7 @@
+ #include <unistd.h>
+ #include <termios.h>
+ #include <sysexits.h>
+-#include <string.h>
++#include <string>
+ #include <sched.h>
+ 
+ #include "picport.h"
