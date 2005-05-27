3f9322712a9781e51c0c4cca36b85318a164413d
Patrick Li <pat@FreeBSD.org>
2005-05-27 21:38:42 +0000

    Update to 0.2.1 (anyone want to make this DATADIR safe?)
    PR:		ports/81510
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ graphics/qcomicbook/files/patch-src-imgarchivesink.cpp
@@ -0,0 +1,10 @@
+--- src/imgarchivesink.cpp.orig	Mon May 16 00:16:43 2005
++++ src/imgarchivesink.cpp	Fri May 20 00:41:12 2005
+@@ -18,6 +18,7 @@
+ #include <qfileinfo.h>
+ #include <qfile.h>
+ #include <stdlib.h>
++#include <unistd.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ 
