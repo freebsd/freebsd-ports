5b78f79730d4bf46b32d1914aa0e109329c45c89
clsung <clsung@FreeBSD.org>
2005-05-27 06:53:16 +0000

    - update to 1.0.1 - bug fix, see   http://www.plogworld.net/blog.php/plog_development_journal/   2005/05/24/plog_101_released

--- www/plog/files/patch-wizard.php
+++ www/plog/files/patch-wizard.php
@@ -1,12 +1,12 @@
---- wizard.php.orig	Sun Apr 17 08:56:58 2005
-+++ wizard.php	Sun Apr 17 08:58:10 2005
-@@ -1414,6 +1414,9 @@
+--- wizard.php.orig	Wed May 25 01:20:42 2005
++++ wizard.php	Fri May 27 14:40:20 2005
+@@ -1419,6 +1419,9 @@
                      }
                  }
-                 
-+		// dirty hack, ignore duplicate table/key/column and upgrade anyway
-+		$errors = false;
-+
+ 
++                // dirty hack, ignore duplicate table/key/column and upgrade anyway
++                $errors = false;
++ 
                  if( !$errors )
                      $message .= "Changes to table <strong>$tableId</strong> executed successfully.<br/>";
                  else {
