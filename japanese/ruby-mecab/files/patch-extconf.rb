5815c5beafb9c19a5d2e2169be94d805fb412d22
kuriyama <kuriyama@FreeBSD.org>
2005-05-28 12:58:56 +0000

    - Upgrade to 0.81. - Unbreak on some conditions. - Update WWW: line.
    No response from:	maintainer

--- japanese/ruby-mecab/files/patch-extconf.rb
+++ japanese/ruby-mecab/files/patch-extconf.rb
@@ -1,19 +1,11 @@
---- extconf.rb.orig	Fri Dec 12 09:36:59 2003
-+++ extconf.rb	Fri Dec 12 09:45:19 2003
-@@ -1,12 +1,12 @@
- require 'mkmf'
- 
--mecab_config = with_config ('mecab-config', 'mecab-config')
-+mecab_config = with_config('mecab-config', 'mecab-config')
+--- extconf.rb.orig	Sun Mar 20 22:19:10 2005
++++ extconf.rb	Mon May  2 02:03:15 2005
+@@ -3,7 +3,7 @@
+ mecab_config = with_config('mecab-config', 'mecab-config')
  use_mecab_config = enable_config('mecab-config')
  
 -`mecab-config --libs-only-l`.chomp.split.each { | lib |
--  have_library (lib)
 +`#{mecab_config} --libs-only-l`.chomp.split.each{ | lib |
-+  have_library(lib)
+   have_library(lib)
  }
  
- $CFLAGS += ' ' + `#{mecab_config} --cflags`.chomp
- 
--have_header ('mecab.h') && create_makefile('MeCab')
-+have_header('mecab.h') && create_makefile('MeCab')
