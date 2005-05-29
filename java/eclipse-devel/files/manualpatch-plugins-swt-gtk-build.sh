a40f6b4f7752009bd5f5063eb01d1fec681314dd
Norikatsu Shigemura <nork@FreeBSD.org>
2005-05-29 03:41:42 +0000

    o Update to 3.1M7. [1] o Add LATEST_LINK. [2]
    PR:		ports/81076 [1]
    Submitted by:	Alex Varju <freebsd-ports@varju.ca> [1]
    Pointed out by:	krisbot via kris [2]
    Tested by:	java@ [1]

--- java/eclipse-devel/files/manualpatch-plugins-swt-gtk-build.sh
+++ java/eclipse-devel/files/manualpatch-plugins-swt-gtk-build.sh
@@ -1,6 +1,6 @@
---- plugins/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh.orig	Fri Apr  1 13:56:58 2005
-+++ plugins/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh	Sat Apr  9 16:27:29 2005
-@@ -110,6 +110,36 @@
+--- plugins/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh.orig	Fri May 13 11:37:09 2005
++++ plugins/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh	Sat May 14 21:02:00 2005
+@@ -100,6 +100,33 @@
  				;;	
  		esac
  		;;
@@ -8,9 +8,6 @@
 +		CC=gcc
 +		LD=gcc
 +		XTEST_LIB_PATH=$X11BASE/lib
-+		KDE_LIB_PATH=$PREFIX/lib
-+		KDE_INCLUDE_PATH=$PREFIX/include
-+		QT_HOME=$X11BASE
 +		GECKO_I=${X11BASE}/include/${BROWSER}
 +		GECKO_INCLUDES="-I${GECKO_I} -I${GECKO_I}/nspr -I${GECKO_I}/xpcom -I${GECKO_I}/string -I${GECKO_I}/embed_base -I${GECKO_I}/embedstring"
 +		GECKO_L=${X11BASE}/lib/${BROWSER}
@@ -19,13 +16,13 @@
 +			"amd64")
 +				AWT_LIB_PATH=$JAVA_HOME/jre/lib/amd64
 +				SWT_PTR_CFLAGS=-DSWT_PTR_SIZE_64
-+				OUTPUT_DIR=../../../org.eclipse.swt.gtk64/os/freebsd/amd64
++				OUTPUT_DIR=../../../org.eclipse.swt.gtk.freebsd.amd64
 +				makefile="make_freebsd.mak"
 +				echo "Building FreeBSD GTK AMD64 version of SWT"
 +				;;
 +			i?86)
 +				AWT_LIB_PATH=$JAVA_HOME/jre/lib/i386
-+				OUTPUT_DIR=../../../org.eclipse.swt.gtk/os/freebsd/x86
++				OUTPUT_DIR=../../../org.eclipse.swt.gtk.freebsd.x86
 +				makefile="make_freebsd.mak"
 +				echo "Building FreeBSD GTK x86 version of SWT"
 +				;;
@@ -37,18 +34,9 @@
  	"SunOS")
  		CC=gcc
  		LD=gcc
-@@ -138,4 +168,13 @@
+@@ -128,4 +155,4 @@
  
- export CC LD JAVA_HOME QT_HOME AWT_LIB_PATH XTEST_LIB_PATH GECKO_SDK GECKO_INCLUDES GECKO_LIBS SWT_PTR_CFLAGS CDE_HOME KDE_LIB_PATH KDE_INCLUDE_PATH OUTPUT_DIR
+ export CC LD JAVA_HOME AWT_LIB_PATH XTEST_LIB_PATH GECKO_SDK GECKO_INCLUDES GECKO_LIBS SWT_PTR_CFLAGS CDE_HOME OUTPUT_DIR
  
 -make -f $makefile ${1} ${2} ${3} ${4}
 +gmake -f $makefile ${1} ${2} ${3} ${4}
-+
-+build_kde=`pkg_info -xc kdebase | grep "no packages match"`
-+if [ "$build_kde" != "" ]; then
-+  echo "Building FreeBSD version of KDE DLL."
-+  gmake -f $makefile make_kde
-+fi
-+
-+cp -p library/xpcom* .
-+gmake -f $makefile make_mozilla
--- java/eclipse-devel/files/patch-features-launchers-library-eclipseShm.c
+++ java/eclipse-devel/files/patch-features-launchers-library-eclipseShm.c
@@ -1,5 +1,5 @@
---- features/org.eclipse.launchers/library/eclipseShm.c.orig	Fri Apr  1 13:55:58 2005
-+++ features/org.eclipse.launchers/library/eclipseShm.c	Sun Apr  3 11:34:02 2005
+--- features/org.eclipse.platform.launchers/library/eclipseShm.c.orig	Fri Apr  1 13:55:58 2005
++++ features/org.eclipse.platform.launchers/library/eclipseShm.c	Sun Apr  3 11:34:02 2005
 @@ -182,11 +182,13 @@
  
  #else /* Unix like platforms */
