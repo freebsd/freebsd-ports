fe7c3763a8b7ed577796ebb2ad9ecd3867e74384
fjoe <fjoe@FreeBSD.org>
2005-05-30 14:58:42 +0000

    - Fix the build: Use wxgtk2-2.4-config instead of wx-config. - Bump PORTREVISION.

--- x11-toolkits/ruby-wx/files/patch-extconf.rb
+++ x11-toolkits/ruby-wx/files/patch-extconf.rb
@@ -1,15 +1,19 @@
---- extconf.rb.orig	Wed Nov 17 15:56:22 2004
-+++ extconf.rb	Fri Feb 11 00:42:43 2005
+--- extconf.rb.orig	Wed Nov 17 20:56:22 2004
++++ extconf.rb	Mon May 30 21:50:18 2005
 @@ -32,13 +32,16 @@
  end
  
  
 -if /linux/ =~ RUBY_PLATFORM  or /i386-freebsd/ =~ RUBY_PLATFORM
+-    CONFIG['CC'] = `wx-config --cxx`
+-    CONFIG['LDSHARED'].gsub!("gcc",`wx-config --cxx`.strip)
+-    $CFLAGS += " `wx-config --cxxflags`"
+-    $LDFLAGS += " `wx-config --libs` -Wl,--version-script,./version-script "
 +if /linux/ =~ RUBY_PLATFORM  or /freebsd/ =~ RUBY_PLATFORM
-     CONFIG['CC'] = `wx-config --cxx`
-     CONFIG['LDSHARED'].gsub!("gcc",`wx-config --cxx`.strip)
-     $CFLAGS += " `wx-config --cxxflags`"
-     $LDFLAGS += " `wx-config --libs` -Wl,--version-script,./version-script "
++    CONFIG['CC'] = `wxgtk2-2.4-config --cxx`
++    CONFIG['LDSHARED'].gsub!("gcc",`wxgtk2-2.4-config --cxx`.strip)
++    $CFLAGS += " `wxgtk2-2.4-config --cxxflags`"
++    $LDFLAGS += " `wxgtk2-2.4-config --libs` -Wl,--version-script,./version-script "
      ["htmlprocessor.o", "htmlwindow.o", "htmleasyprinting.o"].each do |str|
          $objs.delete(str)
 +    end
