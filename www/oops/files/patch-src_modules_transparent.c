10f2eb8709222d003e467b5f74dd8d1fb4577b40
pav <pav@FreeBSD.org>
2005-05-30 21:20:39 +0000

    - One more fix for IPL_NAT - Convert to OPTIONS
    PR:		ports/81623
    Submitted by:	markm (maintainer)

--- www/oops/files/patch-src_modules_transparent.c
+++ www/oops/files/patch-src_modules_transparent.c
@@ -12,3 +12,12 @@ $FreeBSD$
  	    if (natfd < 0) {
  		my_xlog(OOPS_LOG_HTTP|OOPS_LOG_DBG|OOPS_LOG_SEVERE, "transparent/redir(): NAT open failed: %m\n");
  		goto notdone;
+@@ -270,7 +270,7 @@
+ 	natLookup.nl_outip = rq->client_sa.sin_addr;
+ 	natLookup.nl_flags = IPN_TCP;
+ 	if (natfd < 0) {
+-	    natfd = open(IPL_NAT, O_RDONLY, 0);
++	    natfd = open(IPNAT_NAME, O_RDONLY, 0);
+ 	    if (natfd < 0) {
+ 		my_xlog(OOPS_LOG_HTTP|OOPS_LOG_DBG|OOPS_LOG_SEVERE, "transparent/redir(): NAT open failed: %m\n");
+ 		goto notdone;
