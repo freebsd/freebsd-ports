5c3a0901a353d083d7d4ded8b3c47855e064809f
Florent Thoumie <flz@FreeBSD.org>
2005-05-31 07:53:04 +0000

    - Fix distinfo. - Fix patch-install.sh.
    PR:		ports/81691
    Submitted by:	maintainer

--- french/alphabet_sounds_fr/files/patch-install.sh
+++ french/alphabet_sounds_fr/files/patch-install.sh
@@ -1,34 +1,19 @@
---- install.sh.orig	Mon Jun 28 13:44:23 2004
-+++ install.sh	Sun Aug 22 17:16:36 2004
-@@ -1,8 +1,8 @@
+--- install.sh.orig	Sun Mar 20 06:29:18 2005
++++ install.sh	Mon May 30 20:58:56 2005
+@@ -1,5 +1,5 @@
 -#!/bin/bash 
--# install plugin in the standard childsplay path  
-+#!/bin/sh
-+# install plugin in the standard childsplay path
++#!/bin/sh 
+ # install plugin in the standard childsplay path  
  
  PREFIX=/usr/local
--DESTDIR=$PREFIX/games/childsplay
-+DESTDIR=$PREFIX/share/childsplay
- 
- ###########################################################
- #    DO NOT EDIT BEHIND THIS POINT
-@@ -11,7 +11,7 @@
- 
- # Dependency, which version of childsplay at least
- DEPEN=0.71
-- 
-+
- set -e
- CWD=`pwd`
- 
-@@ -34,23 +34,23 @@
+ DESTDIR=$PREFIX/share/childsplay
+@@ -34,24 +34,8 @@
      echo "exit"
      exit 1
  fi
--   
-+
- echo -e "\n The path to install the soundfiles in is "
- echo " $DESTDIR."
+    
+-echo -e "\n The path to install the soundfiles in is "
+-echo " $DESTDIR."
 -echo " If you have installed childsplay in a other place, you can"
 -echo " give the full path to childsplay, otherwise just hit enter"
 -echo " Hit enter to install in $DESTDIR, or give the full path"
@@ -43,20 +28,7 @@
 -        exit 1
 -    fi
 -fi
-+#echo " If you have installed childsplay in a other place, you can"
-+#echo " give the full path to childsplay, otherwise just hit enter"
-+#echo " Hit enter to install in $DESTDIR, or give the full path"
-+#read path
-+#if [ $path ];then
-+#    if [ -e $path/install.py ];then
-+#        DESTDIR=path
-+#    else
-+#        echo "*********** WARNING *************"
-+#        echo " $path does not exists or is not the childsplay directory"
-+#        echo " exit"
-+#        exit 1
-+#    fi
-+#fi
  echo " Installing in $DESTDIR/Data/AlphabetSounds"
  
  echo " Copy sound files"
+ cp -rf $CWD/AlphabetSounds $DESTDIR/Data/
