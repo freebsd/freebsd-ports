18706049a02f68b9ebc53ad354f80db5bb85ba6d
kwm <kwm@FreeBSD.org>
2005-06-03 07:34:45 +0000

    Fix the build on 4.x by removing a ^M.
    Reported by:	Kent Stewart <kstewart AT owt dot com>

---
+++ audio/libmpcdec/files/patch-src_mpc_decoder.c
@@ -0,0 +1,11 @@
+--- src/mpc_decoder.c.orig	Fri Jun  3 09:29:30 2005
++++ src/mpc_decoder.c	Fri Jun  3 09:29:49 2005
+@@ -486,7 +486,7 @@
+ 
+ #endif
+ #else
+-#define MPC_MULTIPLY_SCF(CcVal, SCF_idx) \
++#define MPC_MULTIPLY_SCF(CcVal, SCF_idx) \
+     MPC_MULTIPLY(CcVal, d->SCF[SCF_idx])
+ #endif
+     // requantization and scaling of subband-samples
