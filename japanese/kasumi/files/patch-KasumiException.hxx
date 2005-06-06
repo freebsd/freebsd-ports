b4341947d5502b770c9c1ac829b3ad433daa14ff
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2005-06-06 16:15:50 +0000

    Fix build error on 4-stable.
    PR:		ports/81511
    Submitted by:	Ports Fury

---
+++ japanese/kasumi/files/patch-KasumiException.hxx
@@ -0,0 +1,11 @@
+--- KasumiException.hxx.orig	Fri Jan  7 23:18:09 2005
++++ KasumiException.hxx	Sun May 22 19:58:45 2005
+@@ -5,6 +5,8 @@
+ #include "config.h"
+ #endif
+ 
++#include <string>
++
+ using namespace std;
+ 
+ class KasumiDicExaminationException{
