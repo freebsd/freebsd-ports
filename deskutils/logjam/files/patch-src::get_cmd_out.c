7e4d3b822f2804e9b9a73700c973e484bfd7b640
Roman Bogorodskiy <novel@FreeBSD.org>
2005-06-06 06:24:55 +0000

    Update to 4.4.1.
    PR:		81925
    Submitted by:	novel
    Approved by:	Mike <mike.patterson@unb.ca> (maintainer)

--- deskutils/logjam/files/patch-src::get_cmd_out.c
+++
@@ -1,9 +0,0 @@
---- src/get_cmd_out.c.orig  Sun May  9 16:29:56 2004
-+++ src/get_cmd_out.c       Sun May  9 16:30:55 2004
-@@ -12,6 +12,7 @@
- #include <unistd.h>
- #include <errno.h>
- #include <sys/types.h>
-+#include <signal.h>
- #include <sys/wait.h>
- #include "util.h"
