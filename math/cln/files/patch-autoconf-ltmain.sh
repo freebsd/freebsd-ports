9140c088322aad38941a7d74b40c7db9621baa11
pav <pav@FreeBSD.org>
2005-06-07 08:39:31 +0000

    - Revert recent libtool damage, unbreak on 4.X
    PR:		ports/81958
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>
    Pointy hat to:	oliver

---
+++ math/cln/files/patch-autoconf-ltmain.sh
@@ -0,0 +1,15 @@
+--- autoconf/ltmain.sh.orig	Tue Sep  7 04:46:35 2004
++++ autoconf/ltmain.sh	Sat Jun  4 16:54:55 2005
+@@ -5547,10 +5547,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
