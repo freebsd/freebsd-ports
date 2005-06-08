82c47554cbaf1f62ee1bb14d05aa51443f15fc0b
arved <arved@FreeBSD.org>
2005-06-08 16:51:58 +0000

    Oops, forgot a )
    Reported by:	maintainer

--- devel/ice/files/patch-src::icecpp::config.h
+++ devel/ice/files/patch-src::icecpp::config.h
@@ -5,7 +5,7 @@
  #if defined(_WIN32)
  #   define WCHAR_TYPE_SIZE 2
 -#elif (defined(__linux) || defined(__FreeBSD__)) && (defined(__i386) || defined(__x86_64) || defined(__sparc)) \
-+#elif (defined(__linux) || defined(__FreeBSD__)) && (defined(__i386) || defined(__x86_64) || defined(__sparc) || defined(__alpha__) || defined(__powerpc__) \
++#elif (defined(__linux) || defined(__FreeBSD__)) && (defined(__i386) || defined(__x86_64) || defined(__sparc) || defined(__alpha__) || defined(__powerpc__)) \
        || defined (__sun) || defined(__hpux) || defined(__APPLE__) || defined(_AIX)
  #   define WCHAR_TYPE_SIZE 4
  #else
