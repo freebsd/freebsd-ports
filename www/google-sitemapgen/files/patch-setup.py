1b06f82c8cb92befc4af750598ae089c7dc4eb20
Sergey Skvortsov <skv@FreeBSD.org>
2005-06-09 09:00:30 +0000

    Add google-sitemapgen 1.0, script to analyze web server and generate Google Sitemap files.

---
+++ www/google-sitemapgen/files/patch-setup.py
@@ -0,0 +1,8 @@
+--- setup.py.orig	Thu May 26 23:53:25 2005
++++ setup.py	Thu Jun  9 12:39:45 2005
+@@ -9,4 +9,5 @@
+       author='Google Inc.',
+       author_email='opensource@google.com',
+       url='http://sourceforge.net/projects/goog-sitemap_gen/',
++      py_modules = [ 'sitemap_gen' ]
+      )
