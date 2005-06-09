719b8f186ebddfdee6a860cfab2fc355361a3604
krion <krion@FreeBSD.org>
2005-06-09 20:41:09 +0000

    Fix build with MySQL 4.1
    Provided by:	ale

--- databases/mysqlcc/files/patch-shared-src-CMySQL.cpp
+++ databases/mysqlcc/files/patch-shared-src-CMySQL.cpp
@@ -1,12 +1,15 @@
---- shared/src/CMySQL.cpp.orig	Sun Dec 14 00:10:44 2003
-+++ shared/src/CMySQL.cpp	Sun Dec 26 14:24:43 2004
+
+$FreeBSD$
+
+--- shared/src/CMySQL.cpp.orig
++++ shared/src/CMySQL.cpp
 @@ -450,8 +450,12 @@
    
    if (!isConnected())
      return false;
 -  
 +
-+#if  ( MYSQL_VERSION_ID == 40103 ) || ( MYSQL_VERSION_ID == 50001 ) || ( MYSQL_VERSION_ID == 40107 )
++#if  ( MYSQL_VERSION_ID > 40100 ) || ( MYSQL_VERSION_ID == 50001 ) || ( MYSQL_VERSION_ID == 40107 )
 +  if (mysql_shutdown(mysql,SHUTDOWN_DEFAULT) != 0)
 +#else
    if (mysql_shutdown(mysql) != 0)
