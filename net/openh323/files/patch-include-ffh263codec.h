8dcfd3da34900ca743948f825061df44fea4ea27
Pav Lucistnik <pav@FreeBSD.org>
2005-06-10 21:30:34 +0000

    - Update to 1.12.2
    PR:		ports/82086
    Submitted by:	Steve Ames <steve@energistic.com>

---
+++ net/openh323/files/patch-include-ffh263codec.h
@@ -0,0 +1,12 @@
+*** include/ffh263codec.h.orig	Mon Aug  4 00:03:16 2003
+--- include/ffh263codec.h	Mon Jun  6 14:54:31 2005
+***************
+*** 96,101 ****
+--- 96,102 ----
+  struct AVCodec;
+  struct AVCodecContext;
+  struct AVFrame;
++ struct AVCodecParserContext;
+   
+  ///////////////////////////////////////////////////////////////////////////////
+  
