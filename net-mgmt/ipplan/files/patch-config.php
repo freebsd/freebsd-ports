01fa7954a6de9097e16ff57e6d947c0b775fa563
lawrance <lawrance@FreeBSD.org>
2005-06-10 14:01:49 +0000

    Updated to 4.3.2
    PR:		ports/82113
    Submitted by:	Maxim Tuliuk <mt@zoo.top.net.ua> (maintainer)

--- net-mgmt/ipplan/files/patch-config.php
+++ net-mgmt/ipplan/files/patch-config.php
@@ -1,6 +1,6 @@
---- config.php.orig	Wed May 11 00:26:32 2005
-+++ config.php	Wed May 11 19:30:43 2005
-@@ -89,7 +89,7 @@
+--- config.php.orig	Sun Jun  5 09:53:19 2005
++++ config.php	Fri Jun 10 15:28:16 2005
+@@ -96,7 +96,7 @@
  // see either httpd.conf User directive or ps -ef to see which user
  // apache runs under, then set mode 700 on directory and change owner
  // to this user
@@ -9,16 +9,16 @@
  
  // default country code ("" for none)
  define("DEFAULTCOUNTRY", "US");
-@@ -102,7 +102,7 @@
+@@ -109,7 +109,7 @@
  // not work if php is running in safemode. probing and scanning may 
  // also be against policy for your site!
- define("NMAP", "");
--//define("NMAP", "/usr/bin/nmap");
-+//define("NMAP", "/usr/local/bin/nmap");
+ //define("NMAP", "");
+-define("NMAP", "/usr/bin/nmap");
++define("NMAP", "/usr/local/bin/nmap");
  
  //-------------------------START OF REGISTRAR---------------------------
  
-@@ -145,7 +145,7 @@
+@@ -152,7 +152,7 @@
  //   this will usually be the user under which Apache runs. If the 
  // permissions are not correct, the files will be created in the
  // system temp directory, usually /tmp
