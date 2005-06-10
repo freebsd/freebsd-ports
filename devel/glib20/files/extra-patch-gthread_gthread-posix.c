598c7fa20689cc7aacc1a7d5a17fe2e0e986d50c
Koop Mast <kwm@FreeBSD.org>
2005-06-10 20:45:32 +0000

    Fix patching
    Noticed by:	krion

--- devel/glib20/files/extra-patch-gthread_gthread-posix.c
+++ devel/glib20/files/extra-patch-gthread_gthread-posix.c
@@ -1,5 +1,5 @@
---- gthread/gthread-posix.c.orig	Mon Nov  1 13:47:12 2004
-+++ gthread/gthread-posix.c	Wed Mar  9 14:21:20 2005
+--- gthread/gthread-posix.c.orig	Thu Jun  9 17:27:13 2005
++++ gthread/gthread-posix.c	Fri Jun 10 22:38:37 2005
 @@ -115,7 +115,11 @@
  # define PRIORITY_NORMAL_VALUE    priority_normal_value
  #endif /* POSIX_MIN_PRIORITY && POSIX_MAX_PRIORITY */
@@ -12,21 +12,21 @@
  
  #define G_MUTEX_SIZE (sizeof (pthread_mutex_t))
  
-@@ -307,8 +312,16 @@
-   if (stack_size)
-     {
+@@ -309,8 +313,16 @@
        stack_size = MAX (g_thread_min_stack_size, stack_size);
--      posix_check_cmd (pthread_attr_setstacksize (&attr, stack_size));
+       /* No error check here, because some systems can't do it and
+        * we simply don't want threads to fail because of that. */
+-      pthread_attr_setstacksize (&attr, stack_size);
      }
-+  else
++    else
 +    {
 +      if (sizeof(void *) == 8)
-+	g_thread_default_stack_size = G_THREAD_STACK_SIZE64;
++      g_thread_default_stack_size = G_THREAD_STACK_SIZE64;
 +      else
-+	g_thread_default_stack_size = G_THREAD_STACK_SIZE32;
++      g_thread_default_stack_size = G_THREAD_STACK_SIZE32;
 +      stack_size = MAX (g_thread_min_stack_size, g_thread_default_stack_size);
 +    }
-+  posix_check_cmd (pthread_attr_setstacksize (&attr, stack_size));
++  pthread_attr_setstacksize (&attr, stack_size);
  #endif /* HAVE_PTHREAD_ATTR_SETSTACKSIZE */
  
  #ifdef PTHREAD_SCOPE_SYSTEM
