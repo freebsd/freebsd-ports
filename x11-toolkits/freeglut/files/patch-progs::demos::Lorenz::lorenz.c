4f7b2f0b6bcdb43df87f727e8e839cbc2a51a2a9
thierry <thierry@FreeBSD.org>
2005-06-11 19:57:46 +0000

    Upgrade to 2.4.0.

--- x11-toolkits/freeglut/files/patch-progs::demos::Lorenz::lorenz.c
+++ x11-toolkits/freeglut/files/patch-progs::demos::Lorenz::lorenz.c
@@ -1,11 +1,11 @@
---- progs/demos/Lorenz/lorenz.c.orig	Thu Dec 11 02:32:08 2003
-+++ progs/demos/Lorenz/lorenz.c	Sat Dec 13 10:19:26 2003
-@@ -32,7 +32,7 @@
+--- progs/demos/Lorenz/lorenz.c.orig	Mon Jan  3 11:24:15 2005
++++ progs/demos/Lorenz/lorenz.c	Sat Jun 11 14:06:07 2005
+@@ -31,7 +31,7 @@
+ #include <stdlib.h>
  #include <string.h>
  #include <math.h>
- #include <time.h>
 -#include <GL/freeglut.h>
 +#include "../../../include/GL/freeglut.h"
  #ifdef WIN32
- #include <crtdbg.h>  // DUMP MEMORY LEAKS
- #endif
+ /* DUMP MEMORY LEAKS */
+ #include <crtdbg.h>
