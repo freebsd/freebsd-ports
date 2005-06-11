d6afa0cff7cf571f77de7b3bf63b025bc8eed667
Jean-Yves Lefort <jylefort@FreeBSD.org>
2005-06-11 20:31:11 +0000

    Add xmms-openspc.
    XMMS input plugin for SPC format support.
    
    WWW: http://www.xmms.org/plugins.php?details=225
    
    PR:		ports/82139
    Submitted by:	Emanuel Haupt <ehaupt@critical.ch>

---
+++ audio/xmms-openspc/files/patch-src_id666.c
@@ -0,0 +1,10 @@
+--- src/id666.c.orig	Sat Jun 11 14:13:07 2005
++++ src/id666.c	Sat Jun 11 14:13:27 2005
+@@ -100,6 +100,7 @@
+ 					break;
+ 
+ 			default:	/* Who knows. Ignore it. */
++					break;
+ 		}
+ 	}
+ 	
