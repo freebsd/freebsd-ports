d7d20d0add0d1388293f1236056a6f5540292080
Dirk Meyer <dinoex@FreeBSD.org>
2005-06-11 07:29:15 +0000

    - update to 0.2.8 - use /var/crash - don't issue SMFIS_TEMPFAIL in testmode - fix gcc error on FreeBSD 5.x: sid-filter.c: In function `sid_marid_check': sid-filter.c:1145: error: label at end of compound statement

--- mail/sid-milter/files/patch-sid-filter.c
+++ mail/sid-milter/files/patch-sid-filter.c
@@ -1,109 +1,15 @@
---- sid-filter/sid-filter.c.orig	Fri Dec  3 01:06:22 2004
-+++ sid-filter/sid-filter.c	Sat Dec 18 22:08:31 2004
-@@ -857,7 +857,7 @@
- 				if (dolog)
- 				{
- 					syslog(LOG_ERR,
--					       "%s DNS reply garbled (%d %s)",
-+					       "%s DNS reply garbled expand1 (%d %s)",
- 					       sic->ctx_jobid, type, dd);
- 				}
+--- sid-filter/sid-filter.c.orig	Wed Apr 27 00:16:26 2005
++++ sid-filter/sid-filter.c	Sat Jun 11 09:22:24 2005
+@@ -1140,8 +1140,6 @@
+ 					}
+ 					break;
  
-@@ -876,8 +876,11 @@
- 			if (dolog)
- 			{
- 				syslog(LOG_ERR,
--				       "%s DNS reply garbled (%d %s)",
-+				       "%s DNS reply garbled type1 (%d %s)",
- 				       sic->ctx_jobid, type, dd);
-+				syslog(LOG_ERR,
-+				       "rtype=%d, type=%d, class=%d, expected=%d",
-+				       rtype, type, class, (int)C_IN);
+-				  default:
+-					/* NOTREACHED */
+ 				}
  			}
  
- 			return -1;
-@@ -905,7 +908,7 @@
- 				if (dolog)
- 				{
- 					syslog(LOG_ERR,
--					       "%s DNS reply garbled (%d %s)",
-+					       "%s DNS reply garbled expand2 (%d %s)",
- 					       sic->ctx_jobid, type, dd);
- 				}
- 
-@@ -918,7 +921,7 @@
- 				if (dolog)
- 				{
- 					syslog(LOG_ERR,
--					       "%s DNS reply garbled (%d %s)",
-+					       "%s DNS reply garbled cp1 (%d %s)",
- 					       sic->ctx_jobid, type, dd);
- 				}
- 
-@@ -932,7 +935,7 @@
- 				if (dolog)
- 				{
- 					syslog(LOG_ERR,
--					       "%s DNS reply garbled (%d %s)",
-+					       "%s DNS reply garbled type2 (%d %s)",
- 					       sic->ctx_jobid, type, dd);
- 				}
- 
-@@ -948,7 +951,7 @@
- 				if (dolog)
- 				{
- 					syslog(LOG_ERR,
--					       "%s DNS reply garbled (%d %s)",
-+					       "%s DNS reply garbled cp2 (%d %s)",
- 					       sic->ctx_jobid, type, dd);
- 				}
- 
-@@ -961,7 +964,7 @@
- 				if (dolog)
- 				{
- 					syslog(LOG_ERR,
--					       "%s DNS reply garbled (%d %s)",
-+					       "%s DNS reply garbled cp3 (%d %s)",
- 					       sic->ctx_jobid, type, dd);
- 				}
- 
-@@ -999,7 +1002,7 @@
- 					if (dolog)
- 					{
- 						syslog(LOG_ERR,
--						       "%s DNS reply garbled (%d %s)",
-+						       "%s DNS reply garbled mx (%d %s)",
- 						       sic->ctx_jobid, type,
- 						       dd);
- 					}
-@@ -1018,7 +1021,7 @@
- 					if (dolog)
- 					{
- 						syslog(LOG_ERR,
--						       "%s DNS reply garbled (%d %s)",
-+						       "%s DNS reply garbled mx expand (%d %s)",
- 						       sic->ctx_jobid, type,
- 						       dd);
- 					}
-@@ -1047,7 +1050,7 @@
- 					if (dolog)
- 					{
- 						syslog(LOG_ERR,
--						       "%s DNS reply garbled (%d %s)",
-+						       "%s DNS reply garbled ptr (%d %s)",
- 						       sic->ctx_jobid, type,
- 						       dd);
- 					}
-@@ -1073,7 +1076,7 @@
- 		/* sort MX list */
- 		for (n = 0; n < nmx; n++)
- 		{
--			if (prec[n] > prec[n + 1])
-+			if ((n + 1 < nmx) && prec[n] > prec[n + 1])
- 			{
- 				m = prec[n];
- 				prec[n] = prec[n + 1];
-@@ -1772,7 +1775,7 @@
+@@ -1865,7 +1863,7 @@
  		}
  
  		sid_msgcleanup(ctx);
@@ -112,7 +18,7 @@
  	}
  
  	/* construct the status header's content */
-@@ -1801,7 +1804,7 @@
+@@ -1895,7 +1893,7 @@
  			}
  
  			sid_msgcleanup(ctx);
