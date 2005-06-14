7e0c96c0ee6f670e739ca0ca9b3959470c7e0fa6
thomas <thomas@FreeBSD.org>
2005-06-14 10:25:24 +0000

    There used to be a macro IPL_NAT in ipfilter that was defined to be the same as IPNAT_NAME. This macro has been removed with the import of ipfilter 4, so we now need to use IPNAT_NAME directly.
    Fixes build on -CURRENT.
    
    Reviewed by:	roberto

--- net/proxy-suite/files/patch-com-socket.c
+++ net/proxy-suite/files/patch-com-socket.c
@@ -1,5 +1,5 @@
---- common/com-socket.c	Tue Mar 30 12:31:32 2004
-+++ common/com-socket.c	Mon Dec 20 17:33:39 2004
+--- common/com-socket.c.dist	Tue Mar 30 10:31:32 2004
++++ common/com-socket.c	Tue Jun 14 09:26:44 2005
 @@ -84,6 +84,9 @@
  #if defined(HAVE_STROPTS_H)
  #   include <stropts.h>
@@ -10,3 +10,15 @@
  #if defined(HAVE_SYS_CONF_H)
  #   include <sys/conf.h>
  #endif
+@@ -2132,9 +2135,9 @@
+ 	/*
+ 	** BSD ipnat table lookup
+ 	*/
+-	if ((nat_fd = open(IPL_NAT, O_RDONLY, 0)) < 0) {
++	if ((nat_fd = open(IPNAT_NAME, O_RDONLY, 0)) < 0) {
+ 		syslog_error("can't open ipnat device '%.*s'",
+-		             MAX_PATH_SIZE, IPL_NAT);
++		             MAX_PATH_SIZE, IPNAT_NAME);
+ 		return -1;
+ 	}
+ 
