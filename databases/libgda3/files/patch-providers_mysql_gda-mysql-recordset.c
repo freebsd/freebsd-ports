21ba433221969c26b43a303834d2f9dc81af9a76
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-06-15 03:26:23 +0000

    Update to 1.2.2.

--- databases/libgda3/files/patch-providers_mysql_gda-mysql-recordset.c
+++ databases/libgda3/files/patch-providers_mysql_gda-mysql-recordset.c
@@ -1,13 +1,11 @@
---- providers/mysql/gda-mysql-recordset.c.orig	Thu Jul 22 10:40:31 2004
-+++ providers/mysql/gda-mysql-recordset.c	Thu Jul 22 10:42:51 2004
-@@ -67,8 +67,8 @@
- 	case FIELD_TYPE_LONGLONG :
- 	case FIELD_TYPE_INT24 :
- 		if (is_unsigned)
--			gda_value_set_biguint (gda_value, atoll (value));
--		gda_value_set_bigint (gda_value, atoll (value));
-+			gda_value_set_biguint (gda_value, strtoll (value, &value, 10));
-+		gda_value_set_bigint (gda_value, strtoll (value, &value, 10));
+--- providers/mysql/gda-mysql-recordset.c.orig	Wed May 18 03:10:04 2005
++++ providers/mysql/gda-mysql-recordset.c	Tue Jun 14 23:12:25 2005
+@@ -71,7 +71,7 @@ fill_gda_value (GdaValue *gda_value, enu
+ 		if (is_unsigned) {
+ 			gda_value_set_biguint (gda_value, strtoull (value, NULL, 0));
+ 		} else {
+-			gda_value_set_bigint (gda_value, atoll (value));
++			gda_value_set_bigint (gda_value, strtoll (value, NULL, 10));
+ 		}
  		break;
  	case FIELD_TYPE_SHORT :
- 		if (is_unsigned)
