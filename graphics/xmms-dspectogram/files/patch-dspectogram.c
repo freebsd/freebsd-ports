32e06537aff7300452253d13e5ad06fc8a75429c
Jean-Yves Lefort <jylefort@FreeBSD.org>
2005-06-16 22:41:17 +0000

    Add xmms-dspectogram.
    Dual Spectogram is a dual spectral histogram for XMMS.
    
    WWW: http://www.shell.linux.se/bm/index.php?page=xmmsplugin
    
    PR:		ports/82301
    Submitted by:	Alejandro Pulver <alejandro@varnet.biz>

---
+++ graphics/xmms-dspectogram/files/patch-dspectogram.c
@@ -0,0 +1,10 @@
+--- dspectogram.c.orig	Sun Nov 10 16:01:05 2002
++++ dspectogram.c	Wed Jun 15 19:09:24 2005
+@@ -11,6 +11,7 @@
+ #include <math.h>
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <xmms/plugin.h>
+ #include <xmms/configfile.h>
+ #include <unistd.h>
