9996e951793b2c3ed034b35045943931053fb155
Eric Anholt <anholt@FreeBSD.org>
2005-06-16 19:06:08 +0000

    New port: swfdec-plugin is a Mozilla plugin for rendering flash (.swf) animations using the gstreamer framework and the swfdec library.  It should currently play Flash 4 files, and Flash 5 support is under development.

---
+++ www/swfdec-plugin/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Wed Nov 24 17:28:43 2004
++++ ltmain.sh	Wed Nov 24 17:28:55 2004
+@@ -5551,10 +5551,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
