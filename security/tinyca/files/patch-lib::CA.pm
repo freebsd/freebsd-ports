ab62d3b05552959d0970df0a1e23500be28d40e2
novel <novel@FreeBSD.org>
2005-06-16 15:51:40 +0000

    Update to 0.7.0.
    PR:		82322
    Submitted by:	Janos Mohacsi (maintainer)

--- security/tinyca/files/patch-lib::CA.pm
+++ security/tinyca/files/patch-lib::CA.pm
@@ -1,11 +1,14 @@
---- lib/CA.pm.orig	Tue Jul  1 00:33:18 2003
-+++ lib/CA.pm	Tue Jul 15 17:05:35 2003
-@@ -328,7 +328,7 @@
+
+$FreeBSD$
+
+--- lib/CA.pm.orig
++++ lib/CA.pm
+@@ -883,7 +883,7 @@
     };
  
     # create configuration file
 -   my $in  = $self->{'init'}->{'templatedir'}."/openssl.cnf";
 +   my $in  = $self->{'init'}->{'templatedir'}."/tinyca.cnf";
-    my $out = $self->{$name}->{'dir'}."/openssl.cnf";
+    my $out = $self->{$opts->{'name'}}->{'dir'}."/openssl.cnf";
  
     open(IN, "<$in") || do {
