06e15bfd81afc51dde07477bc5643bb26eff4021
Ade Lovett <ade@FreeBSD.org>
2005-06-17 17:12:00 +0000

    Update to 1.5.18 More robust -pthread hack obtained from dfports.
    Tested with:	full build of x11/gnome2 and x11/kde3

--- devel/libtool15/files/patch-ltmain.sh
+++ devel/libtool15/files/patch-ltmain.sh
@@ -1,17 +1,10 @@
---- ltmain.sh.orig	Sun Sep 19 08:34:44 2004
-+++ ltmain.sh	Mon Mar  7 12:53:53 2005
-@@ -1477,7 +1477,7 @@
- 	  esac
- 	elif test "X$arg" = "X-lc_r"; then
- 	 case $host in
--	 *-*-openbsd* | *-*-freebsd*)
-+	 *-*-openbsd* | *-*-freebsd4*)
- 	   # Do not include libc_r directly, use -pthread flag.
- 	   continue
- 	   ;;
-@@ -1494,6 +1494,12 @@
- 
-       -module)
+--- ltmain.sh.orig	Mon May 16 02:39:29 2005
++++ ltmain.sh	Thu Jun 16 19:42:32 2005
+@@ -1525,2 +1525,3 @@
+ 	finalize_command="$finalize_command $arg"
++	deplibs="$deplibs $arg"
+ 	continue
+@@ -1530,2 +1531,8 @@
  	module=yes
 +	case $host in
 +	*-*-freebsd*)
@@ -20,56 +13,39 @@
 +	  ;;
 +	esac
  	continue
- 	;;
- 
-@@ -1977,6 +1983,7 @@
- 	    finalize_deplibs="$deplib $finalize_deplibs"
- 	  else
- 	    deplibs="$deplib $deplibs"
-+	    test "$linkmode" = lib && newdependency_libs="$deplib $newdependency_libs"
+@@ -2015,2 +2022,25 @@
  	  fi
++
++	  case $linkmode in
++	  lib)
++	    deplibs="$deplib $deplibs"
++	    test "$pass" = conv && continue
++	    newdependency_libs="$deplib $newdependency_libs"
++	    ;;
++	  prog)
++	    if test "$pass" = conv; then
++	      deplibs="$deplib $deplibs"
++	      continue
++	    fi
++	    if test "$pass" = scan; then
++	      deplibs="$deplib $deplibs"
++	    else
++	      compile_deplibs="$deplib $compile_deplibs"
++	      finalize_deplibs="$deplib $finalize_deplibs"
++	    fi
++	    ;;
++	  *)
++	    ;;
++	  esac # linkmode
++
  	  continue
- 	  ;;
-@@ -3084,7 +3091,7 @@
- 
- 	# Check that each of the things are valid numbers.
- 	case $current in
--	0 | [1-9] | [1-9][0-9] | [1-9][0-9][0-9]) ;;
-+	0|[1-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-9][0-9][0-9][0-9][0-9]) ;;	
- 	*)
- 	  $echo "$modename: CURRENT \`$current' is not a nonnegative integer" 1>&2
- 	  $echo "$modename: \`$vinfo' is not valid version information" 1>&2
-@@ -3093,7 +3100,7 @@
- 	esac
- 
- 	case $revision in
--	0 | [1-9] | [1-9][0-9] | [1-9][0-9][0-9]) ;;
-+	0|[1-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-9][0-9][0-9][0-9][0-9]) ;;
- 	*)
- 	  $echo "$modename: REVISION \`$revision' is not a nonnegative integer" 1>&2
- 	  $echo "$modename: \`$vinfo' is not valid version information" 1>&2
-@@ -3102,7 +3109,7 @@
- 	esac
- 
- 	case $age in
--	0 | [1-9] | [1-9][0-9] | [1-9][0-9][0-9]) ;;
-+	0|[1-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-9][0-9][0-9][0-9][0-9]) ;;
- 	*)
- 	  $echo "$modename: AGE \`$age' is not a nonnegative integer" 1>&2
- 	  $echo "$modename: \`$vinfo' is not valid version information" 1>&2
-@@ -4442,6 +4449,9 @@
- 	  compile_command=`$echo "X$compile_command" | $Xsed -e "s%@SYMFILE@%$output_objdir/${outputname}S.${objext}%"`
- 	  finalize_command=`$echo "X$finalize_command" | $Xsed -e "s%@SYMFILE@%$output_objdir/${outputname}S.${objext}%"`
+@@ -4498,2 +4528,5 @@
  	  ;;
 +	*-*-freebsd*)
 +	  # FreeBSD doesn't need this...
 +	  ;;
  	*)
- 	  $echo "$modename: unknown suffix for \`$dlsyms'" 1>&2
- 	  exit $EXIT_FAILURE
-@@ -5527,10 +5537,17 @@
- 	fi
- 
+@@ -5627,6 +5660,13 @@
  	# Install the pseudo-library for information purposes.
 -	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
 -	instname="$dir/$name"i
@@ -87,5 +63,3 @@
 +	  ;;
 +	esac
  
- 	# Maybe install the static library, too.
- 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
