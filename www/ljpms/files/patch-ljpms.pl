947417b0ded1ff43b84b251b4a4ad0f44a536175
Andrey A. Chernov <ache@FreeBSD.org>
2005-06-18 18:30:04 +0000

    Remove unneded lines

--- www/ljpms/files/patch-ljpms.pl
+++ www/ljpms/files/patch-ljpms.pl
@@ -1,11 +1,5 @@
 --- ljpms.pl.orig	Mon Jan  3 15:19:08 2005
 +++ ljpms.pl	Sat Jun 18 21:59:45 2005
-@@ -1,4 +1,4 @@
--#!perl
-+#!/usr/local/bin/perl
- 
- # CVS: $Id: ljpms.pl,v 2.5 2005/01/03 12:19:08 sasha Exp $
- # Author: Alexander Nikolaev <sasha_nikolaev@yahoo.com>
 @@ -84,7 +84,7 @@
  exit(usage('invalidmode')) unless ($modes{$mode} || $mode =~ /^\d+$/);
  exit (usage('noexport')) unless -d LOCALDIR . "$suser/export";
