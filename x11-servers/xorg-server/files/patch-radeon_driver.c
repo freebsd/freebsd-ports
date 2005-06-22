fb3b2e00340f4584e384a9092dc3c0fd412416af
anholt <anholt@FreeBSD.org>
2005-06-22 23:12:58 +0000

    [1] Fix the Chipset lines written by xorgcfg for 3dfx cards. [2] Add the PCI ID for Radeon X600, 0x5B62 to a place that was missed.
    PR:		[1] ports/32121
    Submitted by:	[1] alane
    		[2] Vladimir Kushnir <vkushnir@i.kiev.ua>

--- x11-servers/xorg-server/files/patch-radeon_driver.c
+++ x11-servers/xorg-server/files/patch-radeon_driver.c
@@ -1,5 +1,13 @@
 --- programs/Xserver/hw/xfree86/drivers/ati/radeon_driver.c.orig	Thu Feb 17 13:15:22 2005
 +++ programs/Xserver/hw/xfree86/drivers/ati/radeon_driver.c	Thu Feb 17 13:21:09 2005
+@@ -2501,6 +2501,7 @@
+     case PCI_CHIP_RV370_5464:
+         info->IsMobility = TRUE;
+     case PCI_CHIP_RV370_5B60:
++    case PCI_CHIP_RV370_5B62:
+     case PCI_CHIP_RV370_5B64:
+     case PCI_CHIP_RV370_5B65:
+         info->ChipFamily = CHIP_FAMILY_RV380;
 @@ -7159,7 +7159,6 @@
  	RADEONDoAdjustFrame(pScrn, x, y, FALSE);
      }
