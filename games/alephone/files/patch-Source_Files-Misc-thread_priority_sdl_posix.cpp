8f4488de12b6e2d6d22454e305bd733854e58b65
oliver <oliver@FreeBSD.org>
2005-06-23 03:14:26 +0000

    The file to patch is in DOS format (NL+CR) but the patch didn't addressed that. Fixing the patch makes it apply...
    Pointed out by:	Colin Worthy <cworthy@myrealbox.com>

--- games/alephone/files/patch-Source_Files-Misc-thread_priority_sdl_posix.cpp
+++ games/alephone/files/patch-Source_Files-Misc-thread_priority_sdl_posix.cpp
@@ -1,5 +1,5 @@
---- Source_Files/Misc/thread_priority_sdl_posix.cpp.orig	Wed Mar 23 10:41:58 2005
-+++ Source_Files/Misc/thread_priority_sdl_posix.cpp	Wed Mar 23 10:42:10 2005
+--- Source_Files/Misc/thread_priority_sdl_posix.cpp.orig	Fri Aug 29 03:31:10 2003
++++ Source_Files/Misc/thread_priority_sdl_posix.cpp	Thu Jun 23 05:11:11 2005
 @@ -12,7 +12,7 @@
  #if defined(TARGET_API_MAC_CARBON) && __MACH__
  #include <SDL/SDL_Thread.h>
