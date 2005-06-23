5085b27be4eb735437f97089c680cc33d324210a
sem <sem@FreeBSD.org>
2005-06-23 06:35:35 +0000

    - Update to 2.0.8

---
+++ sysutils/hdup/files/patch-src-prototype.h
@@ -0,0 +1,11 @@
+--- src/prototype.h.orig	Thu Jun 23 10:17:28 2005
++++ src/prototype.h	Thu Jun 23 10:17:40 2005
+@@ -82,7 +82,7 @@
+ char * setup_splitcmd(phost_t);
+ char * hdup_makedate(int , int , int , int );
+ time_t hdup_file_time(char *);
+-int isblank(int c);
++/*int isblank(int c);*/
+ /* 
+  * walker.c 
+  */
