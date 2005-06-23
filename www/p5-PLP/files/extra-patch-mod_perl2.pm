c5a87a661104eb366f40de09fdb2d0f93ab89040
Sergey Matveychuk <sem@FreeBSD.org>
2005-06-23 09:01:47 +0000

    - Reviving with mod_perl2 new API - Drop maintainership
    PR:		ports/82457
    Submitted by:	maintainer

--- www/p5-PLP/files/extra-patch-mod_perl2.pm
+++ www/p5-PLP/files/extra-patch-mod_perl2.pm
@@ -1,12 +1,13 @@
---- PLP.pm.orig	Fri Oct 18 15:47:07 2002
-+++ PLP.pm	Tue Jun  1 15:49:07 2004
-@@ -131,7 +131,12 @@
+--- PLP.pm.orig	Wed Jun 22 04:02:50 2005
++++ PLP.pm	Wed Jun 22 04:02:52 2005
+@@ -131,7 +131,13 @@
  
  # This is the mod_perl handler.
  sub handler {
 -    require Apache::Constants;
 +    if($ENV{'mod_perl2_compat'} eq 'on') {
-+	    use Apache::Const -compile => qw(OK);
++	    require Apache2::Const;
++	    import Apache2::Const -compile => qw(OK);
 +    }
 +    else {
 +	require Apache::Constants;
@@ -14,13 +15,13 @@
      clean();
      if (my $ret = mod_perl_init($_[0])) {
  	return $ret;
-@@ -139,7 +144,12 @@
+@@ -139,7 +145,12 @@
      #S start($_[0]);
      start();
      no strict 'subs';
 -    return Apache::Constants::OK();
 +    if($ENV{'mod_perl2_compat'} eq 'on') {
-+	    return Apache::Const::OK;
++	    return Apache2::Const::OK;
 +    }
 +    else {
 +	return Apache::Constants::OK();
@@ -28,13 +29,13 @@
  }
  
  # mod_perl initializer: returns 0 on success, Apache error code on failure
-@@ -151,10 +161,20 @@
+@@ -151,10 +162,20 @@
      $ENV{PLP_FILENAME} = my $filename = $r->filename;
      
      unless (-f $filename) {
 -	return Apache::Constants::NOT_FOUND();
 +	if($ENV{'mod_perl2_compat'} eq 'on') {
-+		return Apache::Const::NOT_FOUND();
++		return Apache2::Const::NOT_FOUND;
 +	}
 +	else {
 +		return Apache::Constants::NOT_FOUND();
@@ -43,7 +44,7 @@
      unless (-r _) {
 -	return Apache::Constants::FORBIDDEN();
 +	if($ENV{'mod_perl2_compat'} eq 'on') {
-+		return Apache::Const::FORBIDDEN();
++		return Apache2::Const::FORBIDDEN;
 +	}
 +	else {
 +		return Apache::Constants::FORBIDDEN();
