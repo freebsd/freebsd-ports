29d4cb9bc0ada6d0abe3fe32e7cb9a444cfd4b38
nobutaka <nobutaka@FreeBSD.org>
2005-06-26 17:24:27 +0000

    Update to 6.5.
    Submitted by:	lioux

--- devel/boehm-gc/files/patch-os_dep.c
+++ devel/boehm-gc/files/patch-os_dep.c
@@ -1,9 +1,9 @@
---- os_dep.c.orig	Sat Dec 18 10:42:19 2004
-+++ os_dep.c	Wed Dec 29 22:48:03 2004
+--- os_dep.c.orig	Thu May 19 20:48:49 2005
++++ os_dep.c	Fri Jun 17 21:28:07 2005
 @@ -699,7 +699,7 @@
      || defined(HURD) || defined(NETBSD)
  	static struct sigaction old_segv_act;
- #	if defined(_sigargs) /* !Irix6.x */ || defined(HPUX) \
+ #	if defined(IRIX5) || defined(HPUX) \
 -	|| defined(HURD) || defined(NETBSD)
 +	|| defined(HURD) || defined(NETBSD) || defined(FREEBSD)
  	    static struct sigaction old_bus_act;
@@ -18,10 +18,10 @@
  	  struct sigaction	act;
  
  	  act.sa_handler	= h;
-@@ -734,7 +734,7 @@
+@@ -736,7 +736,7 @@
  #	  else
  	        (void) sigaction(SIGSEGV, &act, &old_segv_act);
- #		if defined(IRIX5) && defined(_sigargs) /* Irix 5.x, not 6.x */ \
+ #		if defined(IRIX5) \
 -		   || defined(HPUX) || defined(HURD) || defined(NETBSD)
 +		   || defined(HPUX) || defined(HURD) || defined(NETBSD) || defined(FREEBSD)
  		    /* Under Irix 5.x or HP/UX, we may get SIGBUS.	*/
