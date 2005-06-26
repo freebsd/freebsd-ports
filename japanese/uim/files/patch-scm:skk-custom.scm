ef48641145fc2606ccae4cf73f96a33bc99d29a5
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2005-06-26 19:42:42 +0000

    Update to 0.4.7.

--- japanese/uim/files/patch-scm:skk-custom.scm
+++ japanese/uim/files/patch-scm:skk-custom.scm
@@ -1,12 +1,12 @@
---- scm/skk-custom.scm.orig	Tue May 17 19:06:47 2005
-+++ scm/skk-custom.scm	Tue May 17 19:13:48 2005
-@@ -168,8 +168,7 @@
- ;; dictionary
- ;;
+--- scm/skk-custom.scm.orig	Tue Jun 14 05:46:11 2005
++++ scm/skk-custom.scm	Mon Jun 27 02:29:34 2005
+@@ -338,8 +338,7 @@
+ 		 (lambda ()
+ 		   skk-use-skkserv?))
  
 -(define-custom 'skk-dic-file-name (string-append (sys-datadir)
 -						 "/skk/SKK-JISYO.L")
 +(define-custom 'skk-dic-file-name "%%LOCALBASE%%/share/skk/SKK-JISYO.L"
-   '(skk dictionary)
+   '(skk-dict)
    '(pathname)
    (_ "Dictionary file")
