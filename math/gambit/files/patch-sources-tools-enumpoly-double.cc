0b93e602c0bffb6dd51ec0050941e1892e6df168
ahze <ahze@FreeBSD.org>
2005-07-05 05:14:34 +0000

    - Update to 0.2005.06.13
    PR:		ports/82943
    Submitted by:	Ports Fury

---
+++ math/gambit/files/patch-sources-tools-enumpoly-double.cc
@@ -0,0 +1,10 @@
+--- sources/tools/enumpoly/double.cc.orig	Thu Jun  9 02:47:34 2005
++++ sources/tools/enumpoly/double.cc	Sun Jul  3 23:39:27 2005
+@@ -28,6 +28,7 @@
+ #pragma implementation
+ #endif
+ 
++#include <stdio.h>
+ #include <math.h>
+ #include <float.h>
+ #include <assert.h>
