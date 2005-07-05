8166ce463f140bec9e8d7cbdeb2ab802fcf5cffb
Michael Nottebrock <lofi@FreeBSD.org>
2005-07-05 20:55:18 +0000

    Update to KOffice 1.4.0a.
    Note that 'a' does *not* stand for 'alpha', but denotes a short-notice
    bugfix release made after KOffice 1.4.0.

---
+++ editors/calligra/files/extrapatch-kexi-main-keximainwindowimpl.cpp
@@ -0,0 +1,13 @@
+--- kexi/main/keximainwindowimpl.cpp.orig	Thu Jun 30 17:28:58 2005
++++ kexi/main/keximainwindowimpl.cpp	Thu Jun 30 17:29:14 2005
+@@ -106,8 +106,8 @@
+ //#define PROPEDITOR_VISIBILITY_CHANGES
+ 
+ //temporary fix to manage layout
+-#include "ksplitter.h"
+-#define KDOCKWIDGET_P 1
++// #include "ksplitter.h"
++// #define KDOCKWIDGET_P 1
+ 
+ //#ifndef KEXI_NO_MIGRATION
+ //#include "migration/importwizard.h"
