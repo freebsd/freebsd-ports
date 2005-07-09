439173703661a0082f764cdca427dc830944de5d
ahze <ahze@FreeBSD.org>
2005-07-09 00:53:50 +0000

    - Update to 2.0.0

---
+++ graphics/exifprobe/files/patch-print.c
@@ -0,0 +1,34 @@
+--- print.c.orig	Mon Jul  4 17:59:19 2005
++++ print.c	Fri Jul  8 20:49:36 2005
+@@ -168,6 +168,7 @@
+                 chpr += printf(":MM");
+                 break;
+             default:
++		break;
+         }
+         status = 0;
+         break;
+@@ -182,6 +183,7 @@
+                 chpr += printf(":MM");
+                 break;
+             default:
++		break;
+         }
+         status = 0;
+         break;
+@@ -196,6 +198,7 @@
+                 chpr += printf(":MM");
+                 break;
+             default:
++		break;
+         }
+         status = 0;
+         break;
+@@ -210,6 +213,7 @@
+                 chpr += printf(":MM");
+                 break;
+             default:
++		break;
+         }
+         status = 0;
+         break;
