8a6844c8d9dee81c1e5decb68aa473a149bdab3c
Norikatsu Shigemura <nork@FreeBSD.org>
2005-07-10 12:11:52 +0000

    Update to 5.5.
    Informed by:	many people
    Sponsored by:	Ensure Technology Ltd.

--- net/DarwinStreamingServer/files/patch-WebAdmin::src::streamingadminserver.pl
+++ net/DarwinStreamingServer/files/patch-WebAdmin::src::streamingadminserver.pl
@@ -1,15 +1,6 @@
---- WebAdmin/src/streamingadminserver.pl.orig	Thu Aug 28 08:21:55 2003
-+++ WebAdmin/src/streamingadminserver.pl	Sat Jan 22 09:55:56 2005
-@@ -40,7 +40,7 @@
- }
- else
- {
--    eval "use Net::SSleay";
-+    eval "use Net::SSLeay";
- }
- $ssl_available = 0;
- if (!$@) {
-@@ -248,7 +248,7 @@
+--- WebAdmin/src/streamingadminserver.pl.orig	Thu Jul  7 07:57:00 2005
++++ WebAdmin/src/streamingadminserver.pl	Sun Jul 10 15:18:05 2005
+@@ -255,7 +255,7 @@
  	  "gbrowse", "0",
  	  "ssl", "0",
  	  "crtfile", "/etc/streaming/streamingadminserver.pem",
