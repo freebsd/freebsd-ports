b60f12bee52f378f79ba334f6dd3b7c515ce404f
pav <pav@FreeBSD.org>
2005-07-11 21:14:08 +0000

    - Update to 1.3.0.2
    PR:		ports/83228
    Submitted by:	Kevin Brunelle <kruptos@mlinux.org> (maintainer)

--- net-im/gaim-bangexec/files/patch-bangexec.c
+++ net-im/gaim-bangexec/files/patch-bangexec.c
@@ -1,6 +1,6 @@
---- bangexec.c.orig	Mon May  9 07:02:52 2005
-+++ bangexec.c	Mon May  9 07:03:30 2005
-@@ -287,9 +287,10 @@
+--- bangexec.c.orig	Sun Jul 10 13:08:21 2005
++++ bangexec.c	Sun Jul 10 13:08:37 2005
+@@ -297,9 +297,10 @@
  		return -1;
      }else if (pid){		//Parent
  		int rv;
@@ -11,4 +11,4 @@
 +		flags = fcntl (p[0], F_GETFL, 0);
  		fcntl (p[0], F_SETFL, flags | O_NONBLOCK);
  		
- 		rv=read_nonblock(p[0],strtoul(BANGEXEC_MAXLEN,NULL,10)-chrs,strtoul(BANGEXEC_TIMEOUT,NULL,10),inp);	
+ 		rv=read_nonblock(p[0],maxlen-chrs,strtoul(BANGEXEC_TIMEOUT,NULL,10),inp);	
