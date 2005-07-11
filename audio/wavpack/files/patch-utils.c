6e0b6311e5fb441e6c793a49a3a56d19a9952841
kwm <kwm@FreeBSD.org>
2005-07-11 11:29:42 +0000

    Fix build on 4.x.
    Noticed by:	pointyhat via kris and Ingmar Gebert <ingmar dot gebert At uni-rostock dot de> (ports/83227)

---
+++ audio/wavpack/files/patch-utils.c
@@ -0,0 +1,19 @@
+--- utils.c.orig	Sun Mar 20 08:16:38 2005
++++ utils.c	Mon Jul 11 13:25:37 2005
+@@ -613,6 +613,7 @@
+     MultiByteToWideChar (CP_ACP, 0, string, -1, temp, max_chars + 1);
+     WideCharToUTF8 (temp, (uchar *) string, len);
+ #else
++    iconv_t converter;
+     char *temp = malloc (len);
+ //  memset(temp, 0, len);
+     char *outp = temp;
+@@ -624,7 +625,7 @@
+ 
+     memset(temp, 0, len);
+     old_locale = setlocale (LC_CTYPE, "");
+-    iconv_t converter = iconv_open ("UTF-8", "");
++    converter = iconv_open ("UTF-8", "");
+     err = iconv (converter, &inp, &insize, &outp, &outsize);
+     iconv_close (converter);
+     setlocale (LC_CTYPE, old_locale);
