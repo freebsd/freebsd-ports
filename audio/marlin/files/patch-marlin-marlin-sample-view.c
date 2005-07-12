2da96b6ee15878c8a53b5626902261099b71dd88
pav <pav@FreeBSD.org>
2005-07-12 22:54:06 +0000

    - Update to 0.8

--- audio/marlin/files/patch-marlin-marlin-sample-view.c
+++ audio/marlin/files/patch-marlin-marlin-sample-view.c
@@ -1,11 +1,12 @@
---- marlin/marlin-sample-view.c.orig	Sun Aug  8 00:41:40 2004
-+++ marlin/marlin-sample-view.c	Wed Aug 11 20:01:45 2004
-@@ -157,6 +157,9 @@
+--- marlin/marlin-sample-view.c.orig	Mon Jul 11 21:28:23 2005
++++ marlin/marlin-sample-view.c	Wed Jul 13 00:36:31 2005
+@@ -177,6 +177,10 @@
  			 guint64 new_position,
  			 gboolean extend_selection);
  
 +void marlin_sample_view_vzoom_in (MarlinSampleView *view);
 +void marlin_sample_view_vzoom_out (MarlinSampleView *view);
++void scroll_to (MarlinSampleView *view, int x);
 +
  /* Stuff to register the coverage enum */
  #define MARLIN_TYPE_COVERAGE (coverage_get_type ())
