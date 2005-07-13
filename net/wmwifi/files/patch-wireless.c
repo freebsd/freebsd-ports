4e37af4595848d4dc6af1fa00f5d868c3cd36b24
Pav Lucistnik <pav@FreeBSD.org>
2005-07-13 21:35:27 +0000

    - The code walking the interface list will issue the SIOCGIFMEDIA ioctl to   every interface. This ioctl will fail on if_fwe and if_plip (and probably   others). Therefore make the failing ioctl() non-fatal and issue a warning   only.
    PR:		ports/83381
    Submitted by:	Ulrich Spoerlein <q@galgenberg.net> (maintainer)

--- net/wmwifi/files/patch-wireless.c
+++ net/wmwifi/files/patch-wireless.c
@@ -1,5 +1,5 @@
---- src/wireless.c.orig	Sat Jul  2 14:41:48 2005
-+++ src/wireless.c	Sat Jul  2 14:41:53 2005
+--- src/wireless.c.orig	Tue Mar  9 20:39:17 2004
++++ src/wireless.c	Mon Jul 11 10:57:43 2005
 @@ -143,6 +143,7 @@
  
  	max = get_max_ifs();
@@ -8,3 +8,28 @@
  
  	if (old > max)
  		old = max; /* just be sure to not be out of bounds */
+@@ -193,9 +194,11 @@
+ 
+ 		if (ioctl(s, SIOCGIFMEDIA, (caddr_t) &ifmr) < 0)
+ 		{
+-			perror("ioctl");
++			char str[128];
++			snprintf(str, 128, "ioctl for interface %s failed", ifmr.ifm_name);
+ 			close(s);
+-			exit(1);
++			warn(str);
++			continue;
+ 		}
+ 		close(s);
+ 		/* we cannot monitor interfaces in hostap mode, so just
+@@ -207,6 +210,10 @@
+ 			found++;
+ 		}
+ 	}
++
++	if (!found)
++	  return -1;
++
+ 	return index;
+ }
+ /* how many interfaces do we have? this includes non-wireless! */
