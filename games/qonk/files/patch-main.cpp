e56a38c7b619477551ff845192c9fda3f3c7e08e
Pav Lucistnik <pav@FreeBSD.org>
2005-07-13 11:46:09 +0000

    Qonk is a small game I wrote to learn some SDL basics. The game is a small build-and-conquer strategy game with very simple rules. A complete game only lasts for a few minutes and can be a fun break away from work or whatever you're doing.
    PR:		ports/83293
    Submitted by:	Alejandro Pulver <alejandro@varnet.biz>

---
+++ games/qonk/files/patch-main.cpp
@@ -0,0 +1,10 @@
+--- main.cpp.orig	Wed Jul 13 13:40:12 2005
++++ main.cpp	Wed Jul 13 13:41:13 2005
+@@ -1,5 +1,7 @@
+ // Copyright 2005 by Anthony Liekens anthony@liekens.net
+ 
++#include <time.h>
++
+ #include "coordinate.h"
+ #include "stars.h"
+ #include "planets.h"
