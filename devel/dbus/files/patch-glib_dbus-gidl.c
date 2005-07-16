80f2510833f0beb3750c93788b753858a66e7f3f
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-07-16 07:40:01 +0000

    Update to 0.35, and hopefully fix cmsgcred alignment on non-i386 platforms.

--- devel/dbus/files/patch-glib_dbus-gidl.c
+++ devel/dbus/files/patch-glib_dbus-gidl.c
@@ -1,6 +1,6 @@
---- glib/dbus-gidl.c.orig	Fri Apr  8 22:38:35 2005
-+++ glib/dbus-gidl.c	Fri Apr  8 22:37:53 2005
-@@ -661,8 +661,8 @@ property_info_unref (PropertyInfo *info)
+--- glib/dbus-gidl.c.orig	Sat Jul  9 13:52:52 2005
++++ glib/dbus-gidl.c	Sat Jul 16 02:13:02 2005
+@@ -662,8 +662,8 @@ property_info_unref (PropertyInfo *info)
    info->base.refcount -= 1;
    if (info->base.refcount == 0)
      {
@@ -10,13 +10,13 @@
      }
  }
  
-@@ -717,8 +717,8 @@ arg_info_unref (ArgInfo *info)
-   info->base.refcount -= 1;
+@@ -722,8 +722,8 @@ arg_info_unref (ArgInfo *info)
    if (info->base.refcount == 0)
      {
+       g_hash_table_destroy (info->annotations);
 -      base_info_free (info);
        g_free (info->type);
 +      base_info_free (info);
      }
  }
- const char*
+ 
---
+++ devel/dbus/files/patch-python_dbus_h_wrapper.h
@@ -0,0 +1,7 @@
+--- python/dbus_h_wrapper.h.orig	Sat Jul 16 03:30:40 2005
++++ python/dbus_h_wrapper.h	Sat Jul 16 03:28:47 2005
+@@ -1,3 +1,4 @@
+ #define DBUS_API_SUBJECT_TO_CHANGE 1
++#define lint 1
+ #include <dbus/dbus.h>
+ 
