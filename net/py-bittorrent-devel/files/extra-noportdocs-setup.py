45c7bf3e02e5d877c09669ba035d6f8e8fc2d52b
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2005-07-17 21:30:34 +0000

    Update to 4.1.3

--- net/py-bittorrent-devel/files/extra-noportdocs-setup.py
+++ net/py-bittorrent-devel/files/extra-noportdocs-setup.py
@@ -1,7 +1,7 @@
---- setup.py.orig	Sun May 29 23:44:29 2005
-+++ setup.py	Sun May 29 23:47:33 2005
+--- setup.py.orig	Sun Jul 17 18:07:07 2005
++++ setup.py	Sun Jul 17 18:07:41 2005
 @@ -40,4 +40,0 @@
--               (doc_root        , ['credits.txt', 'LICENSE.txt',
--                                   'README.txt', 'redirdonate.html',
--                                   'TRACKERLESS.txt',
+-               (doc_root        , ['credits.txt', 'credits-l10n.txt',
+-                                   'LICENSE.txt', 'README.txt',
+-                                   'TRACKERLESS.txt', 'redirdonate.html',
 -                                   ]       ),
