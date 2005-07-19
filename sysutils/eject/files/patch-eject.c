0f0ffd53a9cbb737759611471a0c0eb95cfcc77b
Pav Lucistnik <pav@FreeBSD.org>
2005-07-19 20:48:26 +0000

    - Teach eject to recognize full device node pathnames too
    PR:		ports/83719
    Submitted by:	Stefan Sperling <stsp@stsp.in-berlin.de>
    Approved by:	Sylvio Cesar <sylvioc@gmail.com> (maintainer)

--- sysutils/eject/files/patch-eject.c
+++ sysutils/eject/files/patch-eject.c
@@ -1,11 +1,49 @@
---- eject.c.orig	Tue May  3 14:59:19 2005
-+++ eject.c	Tue May  3 15:01:30 2005
-@@ -133,7 +133,7 @@
+--- eject.c.orig	Tue Jul 19 13:43:37 2005
++++ eject.c	Tue Jul 19 14:14:24 2005
+@@ -122,7 +122,7 @@
+ }
+ 
+ /*
+- *  check device is exists.
++ *  check whether device exists.
+  */
+ 
+ int
+@@ -133,8 +133,14 @@
      int sts;
      struct stat sb;
  
 -    if (asprintf(device, "/dev/%sc", name) == -1)
-+    if (asprintf(device, "/dev/%s", name) == -1)
- 	return sts;
+-	return sts;
++    if (strncmp("/dev/", name, strlen("/dev/")) == 0) {
++	    if (asprintf(device, "%s", name) == -1)
++		return sts;
++    }
++    else { 
++	    if (asprintf(device, "/dev/%s", name) == -1)
++		return sts;
++    }
      if (vflag || nflag) {
  	printf("%s: using device %s\n", program, device);
+     }
+@@ -174,11 +180,16 @@
+     /* get proper mount information into the list */
+     len = strlen(name);
+     for (n = 0; n < mnts; n++) {
+-	p = rindex(mntbuf[n].f_mntfromname, '/');
+-	if (p == NULL) {
+-	    continue;
++	if (strncmp("/dev/", name, strlen("/dev/")) == 0) 
++		p = mntbuf[n].f_mntfromname;
++	else {
++		p = rindex(mntbuf[n].f_mntfromname, '/');
++		if (p == NULL)
++		    continue;
++		++p;
+ 	}
+-	for (i = 0, ++p, q = name; *p != '\0' && *q != '\0'; ++i, ++p, ++q) {
++
++	for (i = 0, p, q = name; *p != '\0' && *q != '\0'; ++i, ++p, ++q) {
+ 	    if (*p != *q) {
+ 		break;
+ 	    }
