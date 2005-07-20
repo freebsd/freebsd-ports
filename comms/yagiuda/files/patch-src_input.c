fa81b9ef027b71e84d957257a72d23d7b733c0e8
pav <pav@FreeBSD.org>
2005-07-20 14:58:46 +0000

    The Yagi-Uda project is a set of programmes for designing, and optimising Yagi-Uda antennas, with 2 or more elements, such as shown below.  Additionally, a very simple program called dipole works with just a dipole, but its functionality is limited.
    PR:		ports/83737
    Submitted by:	Diane Bruce <db@db.net>

---
+++ comms/yagiuda/files/patch-src_input.c
@@ -0,0 +1,10 @@
+--- src/input.c.orig	Tue Jul 19 12:24:11 2005
++++ src/input.c	Tue Jul 19 12:24:18 2005
+@@ -16,7 +16,6 @@
+ */
+
+ #include <stdio.h>
+-#include <malloc.h>
+ #include <math.h>
+ #include "nrutil.h"
+ #include "yagi.h"
---
+++ comms/yagiuda/files/patch-src_output.c
@@ -0,0 +1,10 @@
+--- src/output.c.orig	Tue Jul 19 12:24:27 2005
++++ src/output.c	Tue Jul 19 12:24:35 2005
+@@ -11,7 +11,6 @@
+ #ifdef HAVE_STDLIB_H
+
+ #endif
+-#include <malloc.h>
+ #include <math.h>
+ #include <string.h>
+ #include <sys/types.h>
