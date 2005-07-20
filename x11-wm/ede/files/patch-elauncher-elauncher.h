7721fdd2d6d804276c8f814cd88e863d36986ad3
sem <sem@FreeBSD.org>
2005-07-20 14:25:09 +0000

    - Update to 1.0.3

---
+++ x11-wm/ede/files/patch-elauncher-elauncher.h
@@ -0,0 +1,10 @@
+--- elauncher/elauncher.h.orig	Wed Jul 20 11:30:44 2005
++++ elauncher/elauncher.h	Wed Jul 20 11:32:49 2005
+@@ -13,6 +13,7 @@
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <errno.h>
++#include <signal.h>
+ 
+ #include <efltk/Fl.h>
+ #include <efltk/Fl_Locale.h>
---
+++ x11-wm/ede/files/patch-evolume-prefs.cpp
@@ -0,0 +1,11 @@
+--- evolume/prefs.cpp.orig	Wed Jul 20 12:01:37 2005
++++ evolume/prefs.cpp	Wed Jul 20 12:01:44 2005
+@@ -4,7 +4,7 @@
+ // See COPYING for details.
+ 
+ #include "prefs.h"
+-#include <malloc.h>
++//#include <malloc.h>
+ #include <efltk/Fl_Item.h>
+ #include <efltk/filename.h>
+ #include <efltk/Fl_Locale.h>
