dad64a05648d191ac358802c3c92e52427bb645f
Pav Lucistnik <pav@FreeBSD.org>
2005-07-20 08:34:35 +0000

    - Update to 1.6
    PR:		ports/83751
    Submitted by:	Vsevolod Stakhov <vsevolod@highsecure.ru>

---
+++ devel/ply/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	Wed Jul 20 01:27:21 2005
++++ setup.py	Wed Jul 20 01:27:31 2005
+@@ -17,7 +17,7 @@
+ 
+ PLY is extremely easy to use and provides very extensive error checking. 
+ """,
+-            licence="""Lesser GPL (LGPL)""",
++            license="""Lesser GPL (LGPL)""",
+             version = "1.3.1",
+             author = "David Beazley",
+             author_email = "beazley@cs.uchicago.edu",
