d811de6426fe9561fb84fe449a69eae2e8cc8158
Eric Anholt <anholt@FreeBSD.org>
2005-07-20 09:35:05 +0000

    Update to a 20050719 snapshot of Mesa, DRM, and now r300 CVS trunk.  The r300 support includes all Radeons that I have heard of so far.  Requires xorg-server-snap and DRM from r300 CVS (5.x-7.x DRM insufficient).  The r300 DRM will be committed to -current soon and hopefully merged to 6.x/5.x.  Previous r300 support in 6.x/5.x just results in hangs due to improper command submission, among other problems.

---
+++ graphics/dri-devel/files/patch-r300_context.c
@@ -0,0 +1,11 @@
+--- src/mesa/drivers/dri/r300/r300_context.c.orig	Wed Jul 20 01:01:19 2005
++++ src/mesa/drivers/dri/r300/r300_context.c	Wed Jul 20 01:01:23 2005
+@@ -66,7 +66,7 @@
+ int future_hw_tcl_on=0;
+ int hw_tcl_on=0;
+ 
+-#if 1
++#if 0
+ #define need_GL_ARB_multisample
+ #define need_GL_ARB_texture_compression
+ #define need_GL_EXT_blend_minmax
