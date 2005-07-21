30bf002499f661bff0551ab8daa00fc72075d156
pav <pav@FreeBSD.org>
2005-07-21 15:07:44 +0000

    - Move .pid file to /var/run
    PR:		ports/83089
    Submitted by:	Radim Kolar <hsn@netmag.cz>
    Approved by:	maintainer timeout (14 days)

--- irc/ircd-hybrid/files/ircd-hybrid.sh
+++ irc/ircd-hybrid/files/ircd-hybrid.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
+PIDFILE=/var/run/ircd.pid
+
 if [ $# -eq 0 -o x$1 = xstart ]; then
   if [ -x %PREFIX%/bin/ircd ]; then
+    touch $PIDFILE 
+    chown ircd $PIDFILE  
     su -fm ircd -c %PREFIX%/bin/ircd && echo ' ircd'
   fi
 fi
 if [ x$1 = xstop ]; then
-  if [ -f %PREFIX%/etc/ircd.pid ]; then
-    kill `cat %PREFIX%/etc/ircd.pid`
+  if [ -s $PIDFILE ]; then
+    kill `cat $PIDFILE`
   else
     # oh well
     killall ircd
   fi
+  rm -f $PIDFILE
 fi
