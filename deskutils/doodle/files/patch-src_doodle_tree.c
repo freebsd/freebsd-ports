98aaf7c65c2ebe4483aeff8a77737952044a0e51
Jean-Yves Lefort <jylefort@FreeBSD.org>
2005-07-22 13:58:01 +0000

    Fix build on 4.x
    Reported by:	pointyhat via kris

--- deskutils/doodle/files/patch-src_doodle_tree.c
+++ deskutils/doodle/files/patch-src_doodle_tree.c
@@ -1,6 +1,14 @@
---- src/doodle/tree.c.orig	Sun Jun 26 15:13:51 2005
-+++ src/doodle/tree.c	Sun Jun 26 15:14:00 2005
-@@ -1815,7 +1815,7 @@
+--- src/doodle/tree.c.orig	Wed Jan  5 23:24:27 2005
++++ src/doodle/tree.c	Fri Jul 22 15:48:57 2005
+@@ -38,6 +38,7 @@
+ 
+ #include "config.h"
+ #include "doodle.h"
++#include <sys/types.h>
+ #include <netinet/in.h>
+ #include <stdarg.h>
+ #include <stdlib.h>
+@@ -1815,7 +1816,7 @@
      /* make certain that the dirty marker is on
         disk... */
      flush_buffer(ret->fd);
