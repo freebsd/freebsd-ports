a1e2dbedc32bf55344927bb5322d186320384cf9
ahze <ahze@FreeBSD.org>
2005-07-24 03:27:08 +0000

    - Update to 2.0.1
    PR:		79460
    Submitted by:	Ports Fury

--- audio/glame/files/patch-swapfile.c
+++ audio/glame/files/patch-swapfile.c
@@ -1,13 +1,16 @@
---- src/swapfile/swapfile_fs.c.orig	Fri Dec  6 07:35:22 2002
-+++ src/swapfile/swapfile_fs.c	Fri Dec  6 07:41:02 2002
-@@ -44,6 +44,10 @@
+--- src/swapfile/swapfile_fs.c.orig	Sat Oct 23 22:09:29 2004
++++ src/swapfile/swapfile_fs.c	Tue Mar 29 23:11:17 2005
+@@ -44,8 +44,12 @@
  #define DEBUG 1
  #endif
  
+-#include <sys/stat.h>
 +#if __FreeBSD__ >= 5
 +#define __BSD_VISIBLE	1
 +#endif
 +
- #include <sys/stat.h>
  #include <sys/types.h>
++#include <sys/stat.h>
  #include <pthread.h>
+ #include <fcntl.h>
+ #include <dirent.h>
