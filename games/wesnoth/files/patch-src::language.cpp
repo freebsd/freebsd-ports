4eb61265bbd7b9efae7aea36029370f98f7a89b3
mezz <mezz@FreeBSD.org>
2005-07-25 19:43:15 +0000

    Update to 0.9.4, see ChangeLog for detail:
    	http://changelog.wesnoth.org/

--- games/wesnoth/files/patch-src::language.cpp
+++ games/wesnoth/files/patch-src::language.cpp
@@ -1,17 +1,15 @@
---- src/language.cpp.orig	Tue Jun  7 19:36:19 2005
-+++ src/language.cpp	Tue Jun  7 19:36:39 2005
-@@ -124,12 +124,12 @@
- static void wesnoth_setlocale(int category, std::string const &slocale)
- {
- 	char const *locale = slocale.c_str();
+--- src/language.cpp.orig	Mon Jul 25 13:44:10 2005
++++ src/language.cpp	Mon Jul 25 13:45:47 2005
+@@ -133,12 +133,10 @@
+ 	unsetenv ("LANGUAGE"); // void so no return value to check
+ #endif
+ 
 -#ifdef __BEOS__
-+
  	if(setenv ("LANG", locale, 1) == -1)
  		std::cerr << "setenv LANG failed: " << strerror(errno);
  	if(setenv ("LC_ALL", locale, 1) == -1)
  		std::cerr << "setenv LC_ALL failed: " << strerror(errno);
 -#endif
-+
  #ifdef __APPLE__
  	if(setenv ("LANGUAGE", locale, 1) == -1)
  		std::cerr << "setenv LANGUAGE failed: " << strerror(errno);
