e4e5677161b44c24595a5705aa6c257a74f1d954
Jeremy Messenger <mezz@FreeBSD.org>
2005-07-25 07:21:29 +0000

    -Update to 1.0.20050524, they never bump version so add date in it on our own. -Remove the BROKEN, DEPRECATED and EXPIRATION_DATE.
    PR:		ports/83938
    Submitted by:	TAOKA Fumiyoshi <fmysh@iijmio-mail.jp>

--- math/superlu_mt/files/patch-SRC+qrnzcnt.c
+++
@@ -1,8 +0,0 @@
---- SRC/qrnzcnt.c.orig	Thu May  8 22:47:27 2003
-+++ SRC/qrnzcnt.c	Thu May  8 22:47:47 2003
-@@ -1,4 +1,4 @@
--#include <malloc.h>
-+#include <stdlib.h>
- #include "pdsp_defs.h"
- 
- #define EMPTY    (-1)
