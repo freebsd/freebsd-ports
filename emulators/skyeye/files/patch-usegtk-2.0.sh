5bfeafa00352905be81f9aa7418e3e1e30bfb78b
pav <pav@FreeBSD.org>
2005-07-28 13:20:05 +0000

    SkyEye is an Open Source Software Project. The goal of SkyEye is to provide an integrated simulation environment in Linux and Windows. SkyEye environment simulates typical Embedded Computer Systems (Now it supports Atmel AT91 board based on ARM7TDMI CPU, board based on ARM720T CPU, board based on StrongARM (SA1100/SA1110). You can run some Embedded Operation System such as ARM Linux, uClinux, uc/O
    PR:		ports/83770
    Submitted by:	Ruan Wei <iamayan@gmail.com>

---
+++ emulators/skyeye/files/patch-usegtk-2.0.sh
@@ -0,0 +1,9 @@
+--- usegtk-2.0.sh.orig	Wed Jul 20 13:28:03 2005
++++ usegtk-2.0.sh	Wed Jul 20 13:25:21 2005
+@@ -1,5 +1,5 @@
+ #! /bin/sh
+ cp gdb/Makefile.in.gtk-2.0 gdb/Makefile.in
+ cp sim/arm/Makefile.in.gtk-2.0 sim/arm/Makefile.in
+-cp sim/arm/configure.gtk-2.0 sim/arm/configure
++cp sim/arm/configure.gtk2.0 sim/arm/configure
+ 	 
