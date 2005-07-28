3227fb2e983d750365cfccf5f13f461f76047eea
Dirk Meyer <dinoex@FreeBSD.org>
2005-07-28 05:08:45 +0000

    - drop vulnerability check

--- Mk/bsd.openssl.mk
+++ Mk/bsd.openssl.mk
@@ -2,7 +2,7 @@
 # Date created:		31 May 2002
 # Whom:			dinoex
 #
-# $FreeBSD: /tmp/pcvs/ports/Mk/bsd.openssl.mk,v 1.21 2005-04-15 04:35:37 dinoex Exp $
+# $FreeBSD: /tmp/pcvs/ports/Mk/bsd.openssl.mk,v 1.22 2005-07-28 05:08:45 dinoex Exp $
 #
 # Use of 'USE_OPENSSL=yes' includes this Makefile after bsd.ports.pre.mk
 #
@@ -42,26 +42,14 @@ WITH_OPENSSL_BASE=yes
 WITH_OPENSSL_PORT=yes
 .endif
 
-#	if no preference was set, check for an up to date base version
+#	if no preference was set, check for an installed base version
 #	but give an installed port preference over it.
 .if	!defined(WITH_OPENSSL_BASE) && \
 	!defined(WITH_OPENSSL_BETA) && \
 	!defined(WITH_OPENSSL_PORT) && \
 	!exists(${LOCALBASE}/lib/libcrypto.so) && \
 	exists(/usr/include/openssl/opensslv.h)
-#	Security: version in base must be 0.9.7d or have fixes
-#	http://www.freebsd.org/cgi/cvsweb.cgi/src/crypto/openssl/crypto/opensslv.h
-OPENSSLVER!=	${AWK} '/OPENSSL_VERSION_TEXT/ { sub(/-fips/, ""); print $$4; exit }' \
-		/usr/include/openssl/opensslv.h
-# check for safe versions in the base
-.if ${OPENSSLVER} != "0.9.7a-p1" && ${OPENSSLVER} != "0.9.7c-p1" && ${OPENSSLVER} != "0.9.7d" && ${OPENSSLVER} != "0.9.7e" && ${OPENSSLVER} != "0.9.7f" && ${OPENSSLVER} != "0.9.7g"
-check-depends::
-	@${ECHO_CMD} "Dependency warning: used OpenSSL version contains known vulnerabilities"
-	@${ECHO_CMD} "Please update or define either WITH_OPENSSL_BASE or WITH_OPENSSL_PORT"
-	@${FALSE}
-.else
 WITH_OPENSSL_BASE=yes
-.endif
 .endif
 
 .if defined(WITH_OPENSSL_BASE)
