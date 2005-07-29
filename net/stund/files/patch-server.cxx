d4e73d4e3fd8725bc60c25343f2b35cb45db77b8
sobomax <sobomax@FreeBSD.org>
2005-07-29 12:36:30 +0000

    o Add support for generating pidfile;
    o newrc startup script;
    
    o bump PORTREVISION.

--- net/stund/files/patch-server.cxx
+++ net/stund/files/patch-server.cxx
@@ -3,7 +3,22 @@ $FreeBSD$
 
 --- server.cxx.orig
 +++ server.cxx
-@@ -32,7 +32,7 @@
+@@ -6,10 +6,14 @@
+ #ifndef WIN32
+ #include <sys/time.h>
+ #include <sys/types.h>
++#include <sys/file.h>
++#include <fcntl.h>
+ #include <unistd.h>
+ #include <netinet/in.h>
+ #include <errno.h>
+ #include <stdlib.h>
++#include <err.h>
++#include <stdio.h>
+ #endif
+ 
+ #include "udp.h"
+@@ -32,13 +36,16 @@
          << "  -a sets the secondary IP" << endl
          << "  -p sets the primary port and defaults to 3478" << endl
          << "  -o sets the secondary port and defaults to 3479" << endl
@@ -12,7 +27,39 @@ $FreeBSD$
          << "  -m sets up a STERN server starting at port m" << endl
          << "  -v runs in verbose mode" << endl
        // in makefile too
-@@ -188,30 +188,14 @@
+         << endl;
+ }
+ 
++#define DEFAULT_PIDFILE "/var/run/stund.pid"
++
++int pf = -1;
+ 
+ int
+ main(int argc, char* argv[])
+@@ -63,6 +70,7 @@
+    int myPort = 0;
+    int altPort = 0;
+    int myMediaPort = 0;
++   const char *pidfile = DEFAULT_PIDFILE;
+    
+    UInt32 interfaces[10];
+    int numInterfaces = stunFindLocalInterfaces(interfaces,10);
+@@ -135,6 +143,14 @@
+          }
+          myMediaPort = UInt16(strtol( argv[arg], NULL, 10));
+       }
++      else if( !strcmp( argv[arg], "-i" ) ) {
++         ++arg;
++         if( argc <= arg ) {
++            usage();
++            exit(-1);
++         }
++         pidfile = argv[arg];
++      }
+       else
+       {
+          usage();
+@@ -188,30 +204,30 @@
        //exit(1);
     }
     
@@ -41,6 +88,22 @@ $FreeBSD$
 +           cerr << "daemon() call failed" << endl;
 +           exit(1);
 +       }
++       // Create pidfile
++       pf = open( pidfile, O_RDWR|O_CREAT, 0644 );
++       if( pf == -1 )
++          err( 1, "open(pidfile)" );
++       if( flock( pf, LOCK_EX | LOCK_NB ) == -1 ) {
++          if( errno == EWOULDBLOCK )
++             errx( 1, "pidfile is locked by another process" );
++          err( 1, "flock(pidfile)" );
++       }
++       if( ftruncate( pf, 0 ) == -1 )
++          err( 1, "ftruncate(pidfile)" );
++       char buf[20];
++       ssize_t blen = snprintf( buf, sizeof buf, "%d\n", (int) getpid() );
++       if( write( pf, buf, blen ) != blen )
++          err( 1, "write(pidfile)" );
++       // Don't close pf as we hold lock on it
     }
 -#endif
  
