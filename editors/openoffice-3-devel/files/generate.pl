49793a210812b6ff51627d6fba1f25328488659f
maho <maho@FreeBSD.org>
2005-07-29 03:06:52 +0000

    more robust generation of script

--- editors/openoffice-3-devel/files/generate.pl
+++ editors/openoffice-3-devel/files/generate.pl
@@ -5,7 +5,7 @@
 # $FreeBSD$
 
 print "#!/bin/csh\n";
-print "/usr/bin/time -h make WITH_CCACHE=yes package package-rename solver sdk deinstall languagepack >& log.en\n";
+print "/usr/bin/time -h make WITH_CCACHE=yes deinstall clean package package-rename solver sdk deinstall languagepack >& log.en\n";
 print "rm work/.configure* work/.build* \n";
 print "/usr/bin/time -h make ALL_LOCALIZED_LANGS=yes >& log.all\n";
 
@@ -15,7 +15,7 @@ while(<FILE>){
 @tmp2=split ('"',$tmp[3]);
 if ( $tmp[0] eq ".if" && $tmp[1] eq "\${LOCALIZED_LANG}" ) { $LANG=$tmp2[1]; 
 	print "make TWEAK_L10N=yes LOCALIZED_LANG=$LANG pre-everything\n";
-	print "/usr/bin/time -h make LOCALIZED_LANG=$LANG WITH_CCACHE=yes languagepack package package-rename deinstall >& log.$LANG\n";
+	print "/usr/bin/time -h make LOCALIZED_LANG=$LANG WITH_CCACHE=yes deinstall languagepack package package-rename deinstall >& log.$LANG\n";
 	}
 }
 close FILE;
--- editors/openoffice.org-1.0/files/generate.pl
+++ editors/openoffice.org-1.0/files/generate.pl
@@ -2,10 +2,10 @@
 
 # generate full build shell script for OpenOffice.org
 # Whom:         Maho Nakata <maho@FreeBSD.org>
-# $FreeBSD: /tmp/pcvs/ports/editors/openoffice.org-1.0/files/Attic/generate.pl,v 1.1 2005-07-29 02:16:38 maho Exp $
+# $FreeBSD: /tmp/pcvs/ports/editors/openoffice.org-1.0/files/Attic/generate.pl,v 1.2 2005-07-29 03:06:51 maho Exp $
 
 print "#!/bin/csh\n";
-print "/usr/bin/time make WITH_CCACHE=yes package package-rename solver sdk deinstall clean >& log.en\n";
+print "/usr/bin/time make WITH_CCACHE=yes deinstall clean package package-rename solver sdk deinstall clean >& log.en\n";
 print "/usr/bin/time make ALL_LOCALIZED_LANGS=yes >& log.all\n";
 
 open ( FILE,  "< Makefile.localized") ;
@@ -14,7 +14,7 @@ while(<FILE>){
 @tmp2=split ('"',$tmp[3]);
 if ( $tmp[1] eq "\${LOCALIZED_LANG}") { $LANG=$tmp2[1]; 
 	print "make TWEAK_L10N=yes LOCALIZED_LANG=$LANG pre-everything\n";
-	print "/usr/bin/time make LOCALIZED_LANG=$LANG  WITH_CCACHE=yes package package-rename deinstall >& log.$LANG\n";
+	print "/usr/bin/time make LOCALIZED_LANG=$LANG WITH_CCACHE=yes deinstall package package-rename deinstall >& log.$LANG\n";
 	}
 }
 close FILE;
