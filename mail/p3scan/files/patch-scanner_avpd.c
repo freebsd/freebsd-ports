63d2ed26a7f159ac0ff87b3c80f7682c06f383d0
edwin <edwin@FreeBSD.org>
2005-07-29 00:42:28 +0000

    update mail/p3scan from 1.0.2 to 2.1
    PR:		ports/84137
    Submitted by:	dawnshade <h-k@mail.ru>

--- mail/p3scan/files/patch-scanner_avpd.c
+++ mail/p3scan/files/patch-scanner_avpd.c
@@ -1,5 +1,5 @@
---- scanner_avpd.c.orig	Thu Dec  4 02:58:25 2003
-+++ scanner_avpd.c	Wed Feb 18 12:50:42 2004
+--- scanner_avpd.c.orig      Wed Jan  5 20:53:04 2005
++++ scanner_avpd.c   Tue Jul 26 12:44:34 2005
 @@ -39,12 +39,12 @@
  #include <time.h>
  #include <sys/time.h>
@@ -14,12 +14,3 @@
  
  #include "p3scan.h"
  
-@@ -81,7 +81,7 @@
-    if (avp_fd!=-1 && connected==-1){
-       do_log(LOG_DEBUG, "Trying to connect to socket");
-       if (connect(avp_fd, (struct sockaddr *)(&avp_socket),
--      sizeof(avp_socket.sun_family) + strlen(NodeCtl)) >= 0){
-+      	SUN_LEN(&avp_socket)) >= 0){
-          connected=1;
-          do_log(LOG_DEBUG, "avp_socket_connect connected to kavdaemon");
-          return 0;
