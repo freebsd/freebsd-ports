0be0fb477cb7000852c24a6cf6bba1c9bf3abd7e
Norikatsu Shigemura <nork@FreeBSD.org>
2005-07-31 05:26:16 +0000

    Update to 3.1. o java/eclipse-devel is IGNOREd now until 3.2+. o change MAINTAINERship to java@ on java/eclipse-devel. [1]
    PR:		ports/84252
    Submitted by:	Panagiotis Astithas <past@ebs.gr>
    Requested by:	devel/eclipse-devel's maintainer [1]
    Tested by:	java@

---
+++ java/eclipse/files/manualpatch-plugins-swt-gtk-build.sh
@@ -0,0 +1,42 @@
+--- plugins/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh.orig	Fri May 13 11:37:09 2005
++++ plugins/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh	Sat May 14 21:02:00 2005
+@@ -100,6 +100,33 @@
+ 				;;	
+ 		esac
+ 		;;
++	"FreeBSD")
++		CC=gcc
++		LD=gcc
++		XTEST_LIB_PATH=$X11BASE/lib
++		GECKO_I=${X11BASE}/include/${BROWSER}
++		GECKO_INCLUDES="-I${GECKO_I} -I${GECKO_I}/nspr -I${GECKO_I}/xpcom -I${GECKO_I}/string -I${GECKO_I}/embed_base -I${GECKO_I}/embedstring"
++		GECKO_L=${X11BASE}/lib/${BROWSER}
++		GECKO_LIBS="-L${GECKO_L} -lxpcom -lnspr4 -lplds4 -lplc4 -lgtkembedmoz"
++		case $MODEL in
++			"amd64")
++				AWT_LIB_PATH=$JAVA_HOME/jre/lib/amd64
++				SWT_PTR_CFLAGS=-DSWT_PTR_SIZE_64
++				OUTPUT_DIR=../../../org.eclipse.swt.gtk.freebsd.amd64
++				makefile="make_freebsd.mak"
++				echo "Building FreeBSD GTK AMD64 version of SWT"
++				;;
++			"i386")
++				AWT_LIB_PATH=$JAVA_HOME/jre/lib/i386
++				OUTPUT_DIR=../../../org.eclipse.swt.gtk.freebsd.x86
++				makefile="make_freebsd.mak"
++				echo "Building FreeBSD GTK x86 version of SWT"
++				;;
++			*)
++				echo "*** Unknown MODEL <${MODEL}>"
++				;;	
++		esac
++		;;
+ 	"SunOS")
+ 		CC=gcc
+ 		LD=gcc
+@@ -128,4 +155,4 @@
+ 
+ export CC LD JAVA_HOME AWT_LIB_PATH XTEST_LIB_PATH GECKO_SDK GECKO_INCLUDES GECKO_LIBS SWT_PTR_CFLAGS CDE_HOME OUTPUT_DIR
+ 
+-make -f $makefile ${1} ${2} ${3} ${4}
++gmake -f $makefile ${1} ${2} ${3} ${4}
---
+++ java/eclipse/files/patch-features-launchers-library-eclipseShm.c
@@ -0,0 +1,17 @@
+--- features/org.eclipse.platform.launchers/library/eclipseShm.c.orig	Fri Apr  1 13:55:58 2005
++++ features/org.eclipse.platform.launchers/library/eclipseShm.c	Sun Apr  3 11:34:02 2005
+@@ -182,11 +182,13 @@
+ 
+ #else /* Unix like platforms */
+ 
++#include <machine/param.h>
++#include <sys/types.h>
++#include <sys/ipc.h>
+ #include <sys/shm.h>
+ #include <string.h>
+ #include <stdlib.h>
+ #include <stdio.h>
+-#include <sys/types.h>
+ #include <unistd.h>
+ 
+ int createSharedData(char** id, int size) {
