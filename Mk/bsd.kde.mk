ecc191066f62f2a3d850fba9401f792e1f815eb2
Michael Nottebrock <lofi@FreeBSD.org>
2005-07-31 22:46:35 +0000

    Update to KDE 3.4.2 / KOffice 1.4.1

--- Mk/bsd.kde.mk
+++ Mk/bsd.kde.mk
@@ -117,7 +117,7 @@ CONFIGURE_ENV+=	MOC="${MOC}" CPPFLAGS="${CPPFLAGS} ${QTCPPFLAGS}" LIBS="${QTCFGL
 
 # Yeah, it's namespace pollution, but this is really the best place for this
 # stuff. Arts does NOT use it anymore.
-KDE_VERSION=		3.4.1
+KDE_VERSION=		3.4.2
 KDE_ORIGVER=	${KDE_VERSION}
 KDE_PREFIX?=	${LOCALBASE}
 
