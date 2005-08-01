eb764110b9ef6b266286d6219ec879b074970147
Norikatsu Shigemura <nork@FreeBSD.org>
2005-08-01 08:08:27 +0000

    Update to 0.3.0.
    PR:		ports/82814
    Submitted by:	Jun Mukai <mukai@jmuk.org> (maintainer)
    Reviewed by:	vs
    Approved by	maintainer no response

---
+++ japanese/scim-skk/files/patch-src-scim_skk_prefs.h
@@ -0,0 +1,11 @@
+--- src/scim_skk_prefs.h.orig	Wed Jun 29 14:31:26 2005
++++ src/scim_skk_prefs.h	Sat Jul 30 19:26:45 2005
+@@ -73,7 +73,7 @@
+ #define SCIM_SKK_CONFIG_UPCASE_KEY_DEFAULT           "Control+u"
+ #define SCIM_SKK_CONFIG_SELECTION_STYLE_DEFAULT      "Qwerty"
+ 
+-#define SCIM_SKK_CONFIG_SYSDICT_DEFAULT          "/usr/share/skk/SKK-JISYO.L"
++#define SCIM_SKK_CONFIG_SYSDICT_DEFAULT          "LOCALBASE/share/skk/SKK-JISYO.L"
+ #define SCIM_SKK_CONFIG_USERDICT_DEFAULT         ".skk-scim-jisyo"
+ #define SCIM_SKK_CONFIG_CANDVEC_SIZE_DEFAULT      4
+ #define SCIM_SKK_CONFIG_ANNOT_VIEW_DEFAULT    true
