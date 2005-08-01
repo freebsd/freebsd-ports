9ccdf092b7b5105f6a3e4501c4c4a6a88e279889
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-08-01 02:22:03 +0000

    Change the guint64 size type to a long.  While this will result in LARGE attachment size truncation in the display, this is more portable. See http://bugzilla.gnome.org/show_bug.cgi?id=312105 for more details.

--- mail/balsa/files/patch-src_sendmsg-window.c
+++ mail/balsa/files/patch-src_sendmsg-window.c
@@ -1,11 +1,11 @@
---- src/sendmsg-window.c.orig	Sun Jul 24 12:51:49 2005
-+++ src/sendmsg-window.c	Sun Jul 24 12:52:57 2005
+--- src/sendmsg-window.c.orig	Fri Jun 24 04:36:25 2005
++++ src/sendmsg-window.c	Sun Jul 31 22:10:14 2005
 @@ -2009,7 +2009,7 @@ add_attachment(BalsaSendmsg * bsmsg, gch
  		       ATTACH_ICON_COLUMN, pixbuf,
  		       ATTACH_TYPE_COLUMN, content_type,
  		       ATTACH_MODE_COLUMN, attach_data->mode,
 -		       ATTACH_SIZE_COLUMN, attach_stat.st_size,
-+		       ATTACH_SIZE_COLUMN, (guint64) attach_stat.st_size,
++		       ATTACH_SIZE_COLUMN, (glong) attach_stat.st_size,
  		       ATTACH_DESC_COLUMN, utf8name,
  		       -1);
      g_object_unref(attach_data);
@@ -14,16 +14,25 @@
  {
      gint mode;
 -    gint size;
-+    guint64 size;
++    glong size;
      gchar *sstr;
  
      gtk_tree_model_get(model, iter, ATTACH_MODE_COLUMN, &mode,
+@@ -2674,7 +2674,7 @@ render_attach_size(GtkTreeViewColumn *co
+     else if (size > 1200)
+ 	sstr = g_strdup_printf("%.2fkB", (gfloat)size / (gfloat)1024);
+     else
+-	sstr = g_strdup_printf("%dB", size);
++	sstr = g_strdup_printf("%ldB", size);
+     g_object_set(cell, "text", sstr, NULL);
+ }
+ 
 @@ -2780,7 +2780,7 @@ create_info_pane(BalsaSendmsg * bsmsg, S
  			       GDK_TYPE_PIXBUF,
  			       G_TYPE_STRING,
  			       G_TYPE_INT,
 -			       G_TYPE_INT,
-+			       G_TYPE_UINT64,
++			       G_TYPE_LONG,
  			       G_TYPE_STRING);
  
      bsmsg->attachments[1] = gtk_tree_view_new_with_model(GTK_TREE_MODEL(store));
