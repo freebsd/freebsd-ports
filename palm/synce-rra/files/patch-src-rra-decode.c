0259650dd4815530fc121d21576e202b317338e1
Sam Lawrance <lawrance@FreeBSD.org>
2005-08-01 17:27:28 +0000

    Update to 0.9.1
    PR:		ports/84335
    Submitted by:	Andrej Zverev <az@inec.ru>

--- palm/synce-rra/files/patch-src-rra-decode.c
+++ palm/synce-rra/files/patch-src-rra-decode.c
@@ -1,11 +1,11 @@
---- src/rra-decode.c.orig	Thu Mar 11 22:13:04 2004
-+++ src/rra-decode.c	Thu Mar 11 22:13:26 2004
-@@ -218,7 +218,7 @@
+--- src/rra-decode.c.orig	Thu May 12 16:57:53 2005
++++ src/rra-decode.c	Fri Jul 29 19:26:36 2005
+@@ -202,7 +202,7 @@
  					time_t unix_time = filetime_to_unix_time(&propvals[i].val.filetime);
- 					char* time_str = ctime(&unix_time);
+ 					char* time_str = asctime(gmtime(&unix_time));
  					time_str[strlen(time_str)-1] = '\0'; /* remove trailing newline */
 -					printf("%s  (%lu)", time_str, unix_time);
 +					printf("%s  (%lu)", time_str, (long unsigned)unix_time);
-           db_dump(&propvals[i].val.filetime, sizeof(FILETIME));
-         }
- 				break;
+ #else
+           TIME_FIELDS time_fields;
+           time_fields_from_filetime(&propvals[i].val.filetime, &time_fields);
