e8c511790e0936b7a85c8c2584588dfb1715d6c2
sem <sem@FreeBSD.org>
2005-08-01 14:49:21 +0000

    - Fix build on 4.x
    PR:		ports/84404
    Submitted by:	Ports Fury

---
+++ graphics/libqglviewer/files/patch-VRender-Vector2.cpp
@@ -0,0 +1,10 @@
+--- VRender/Vector2.cpp.orig	Thu Jun 30 02:06:00 2005
++++ VRender/Vector2.cpp	Tue Jul  5 12:53:56 2005
+@@ -44,6 +44,7 @@
+ 
+ *****************************************************************************/
+ 
++#include <vector>
+ #include "Vector2.h"
+ #include "Vector3.h"
+ #include "Functions.h"
---
+++ graphics/libqglviewer/files/patch-VRender-Vector3.cpp
@@ -0,0 +1,10 @@
+--- VRender/Vector3.cpp.orig	Thu Jun 30 02:06:00 2005
++++ VRender/Vector3.cpp	Tue Jul  5 12:53:29 2005
+@@ -45,6 +45,7 @@
+ *****************************************************************************/
+ 
+ #include <iostream>
++#include <vector>
+ #include "Vector3.h"
+ #include "NVector3.h"
+ #include "Functions.h"
