d2c12aa0df5c61e68381b63cb3d6b7a27cb74a4a
Pav Lucistnik <pav@FreeBSD.org>
2005-08-11 13:20:12 +0000

    - Unbreak xview-clients on FreeBSD 5.X and newer
    Reported by:	krismail
    Approved by:	portmgr (kris)

--- x11-toolkits/xview/files/patch-clients+olwm+gettext.c
+++ x11-toolkits/xview/files/patch-clients+olwm+gettext.c
@@ -1,13 +1,16 @@
---- clients/olwm/gettext.c.orig	Fri Oct 17 11:24:49 2003
-+++ clients/olwm/gettext.c	Fri Oct 17 11:30:14 2003
-@@ -20,7 +20,10 @@
+--- clients/olwm/gettext.c.orig	Thu Aug 11 13:59:59 2005
++++ clients/olwm/gettext.c	Thu Aug 11 14:05:12 2005
+@@ -16,11 +16,13 @@
+ #endif
+ 
+ char *malloc(), *strdup();
+-char * dgettext();
++char * dgettext(char *domain_name, char *msg_id);
  char *_gettext();
  char *in_path();
  char *fgets(), *getenv();
--caddr_t mmap(), calloc();
-+caddr_t calloc();
 +#ifndef __FreeBSD__
-+caddr_t mmap();
+ caddr_t mmap(), calloc();
 +#endif
  
  static struct domain_binding *firstbind=0, *lastbind=0;
