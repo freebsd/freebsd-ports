35aca968c7c636be2cdc047c8a877edfd29ff8c0
ale <ale@FreeBSD.org>
2005-08-28 18:46:42 +0000

    Update to 5.0.11 release.
    PR:		ports/85403
    Submitted by:	Marcus Grando <marcus@corp.grupos.com.br>

--- databases/mysql54-server/files/patch-sql::mysqld.cc
+++ databases/mysql54-server/files/patch-sql::mysqld.cc
@@ -1,6 +1,6 @@
---- sql/mysqld.cc.orig	Tue Dec 14 13:40:36 2004
-+++ sql/mysqld.cc	Mon Jan 10 00:28:52 2005
-@@ -128,7 +128,7 @@
+--- sql/mysqld.cc.orig	Sat Aug  6 03:39:41 2005
++++ sql/mysqld.cc	Sun Aug 28 20:41:38 2005
+@@ -121,7 +121,7 @@
  #endif /* __WIN__ */
  
  #ifdef HAVE_LIBWRAP
@@ -9,3 +9,12 @@
  #include <syslog.h>
  #ifdef NEED_SYS_SYSLOG_H
  #include <sys/syslog.h>
+@@ -3177,7 +3177,7 @@
+   init_ssl();
+ 
+ #ifdef HAVE_LIBWRAP
+-  libwrapName= my_progname+dirname_length(my_progname);
++  libwrapName= (char *) my_progname+dirname_length(my_progname);
+   openlog(libwrapName, LOG_PID, LOG_AUTH);
+ #endif
+ 
