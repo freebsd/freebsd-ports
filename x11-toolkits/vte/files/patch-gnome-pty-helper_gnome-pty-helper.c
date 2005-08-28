632401dedfce3fd596453c1be6f2f992a4081ff4
Jeremy Messenger <mezz@FreeBSD.org>
2005-08-28 17:30:37 +0000

    Update to 0.11.15.

--- x11-toolkits/vte/files/patch-gnome-pty-helper_gnome-pty-helper.c
+++ x11-toolkits/vte/files/patch-gnome-pty-helper_gnome-pty-helper.c
@@ -1,19 +1,26 @@
---- gnome-pty-helper/gnome-pty-helper.c.orig	Wed Nov 27 13:40:01 2002
-+++ gnome-pty-helper/gnome-pty-helper.c	Wed Nov 27 13:40:42 2002
-@@ -42,6 +42,7 @@
- #include <unistd.h>
- #include <string.h>
- #include <signal.h>
-+#include <sys/param.h>
- #include <fcntl.h>
- #include <termios.h>
- #include <errno.h>
-@@ -93,7 +94,7 @@
- #endif
+--- gnome-pty-helper/gnome-pty-helper.c.orig	Sat Aug 13 07:55:19 2005
++++ gnome-pty-helper/gnome-pty-helper.c	Wed Aug 24 19:03:08 2005
+@@ -95,12 +95,12 @@ static pty_info *pty_list;
  #endif /* CMSG_DATA */
  
--#define CONTROLLEN (sizeof (struct cmsghdr)  + sizeof (int))
-+#define CONTROLLEN (sizeof (struct cmsghdr)  + sizeof (long))
- 
  static struct cmsghdr *cmptr;
+-static int CONTROLLEN;
++static socklen_t CONTROLLEN;
  
+ static int
+ init_msg_pass ()
+ {
+-	CONTROLLEN = (CMSG_DATA (cmptr) - (unsigned char *)cmptr) + sizeof(int);
++	CONTROLLEN = CMSG_SPACE (sizeof (int));
+ 	cmptr = malloc (CONTROLLEN);
+ 
+ 	if (cmptr)
+@@ -128,7 +128,7 @@ pass_fd (int client_fd, int fd)
+ 
+ 	cmptr->cmsg_level = SOL_SOCKET;
+ 	cmptr->cmsg_type  = SCM_RIGHTS;
+-	cmptr->cmsg_len   = CONTROLLEN;
++	cmptr->cmsg_len   = CMSG_LEN (sizeof (fd));
+ 	*(int *)CMSG_DATA (cmptr) = fd;
+ 
+ 	if (sendmsg (client_fd, &msg, 0) != 1)
