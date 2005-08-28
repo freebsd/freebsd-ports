74037487e3ba65a9a636e63819f119b41d31383c
maho <maho@FreeBSD.org>
2005-08-28 08:00:11 +0000

    Fix build for 7-CURRENT

--- lang/gcc-ooo/files/patch-freebsd-spec.h
+++ lang/gcc-ooo/files/patch-freebsd-spec.h
@@ -1,6 +1,17 @@
---- gcc/config/freebsd-spec.h.orig	Tue Mar  2 14:34:55 2004
-+++ gcc/config/freebsd-spec.h	Fri Dec 17 11:22:26 2004
-@@ -130,11 +130,19 @@
+--- gcc/config/freebsd-spec.h.orig	Wed Mar  3 07:34:55 2004
++++ gcc/config/freebsd-spec.h	Sat Aug 13 18:47:14 2005
+@@ -51,7 +51,9 @@
+ #define FBSD_TARGET_OS_CPP_BUILTINS()					\
+   do									\
+     {									\
+-	if (FBSD_MAJOR == 6)						\
++	if (FBSD_MAJOR == 7)						\
++	  builtin_define ("__FreeBSD__=7");			       	\
++	else if (FBSD_MAJOR == 6)	       				\
+ 	  builtin_define ("__FreeBSD__=6");			       	\
+ 	else if (FBSD_MAJOR == 5)	       				\
+ 	  builtin_define ("__FreeBSD__=5");			       	\
+@@ -130,11 +132,19 @@
    }"
  #else
  #if FBSD_MAJOR >= 5
