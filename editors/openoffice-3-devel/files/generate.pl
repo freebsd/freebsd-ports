bbcbc8e055af5e56eef63144840b175957866c0a
maho <maho@FreeBSD.org>
2005-08-28 08:41:59 +0000

    1. update to m126 2. set LC_ALL, LANG as c when building [1] 3. better generate.pl 4. Partial fix for 4.11-RELEASE [2] 5. do not pass CPUFLAGS 6. Partial improvement for WITHOUT_JAVA
    PR:		84786
    Submitted by:	[1] achix mantzix
                    [2] Hiromi Kimura and Yury Tarasievich

--- editors/openoffice-3-devel/files/generate.pl
+++ editors/openoffice-3-devel/files/generate.pl
@@ -5,7 +5,8 @@
 # $FreeBSD$
 
 print "#!/bin/csh\n";
-print "/usr/bin/time -h make WITH_CCACHE=yes deinstall clean package package-rename solver sdk deinstall languagepack >& log.en\n";
+print "make deinstall clean\n";
+print "/usr/bin/time -h make WITH_CCACHE=yes install package package-rename solver sdk ure languagepack deinstall >& log.en\n";
 print "rm work/.configure* work/.build* \n";
 print "/usr/bin/time -h make ALL_LOCALIZED_LANGS=yes >& log.all\n";
 
@@ -15,7 +16,7 @@ while(<FILE>){
 @tmp2=split ('"',$tmp[3]);
 if ( $tmp[0] eq ".if" && $tmp[1] eq "\${LOCALIZED_LANG}" ) { $LANG=$tmp2[1]; 
 	print "make TWEAK_L10N=yes LOCALIZED_LANG=$LANG pre-everything\n";
-	print "/usr/bin/time -h make LOCALIZED_LANG=$LANG WITH_CCACHE=yes deinstall languagepack package package-rename deinstall >& log.$LANG\n";
+	print "/usr/bin/time -h make LOCALIZED_LANG=$LANG WITH_CCACHE=yes languagepack package package-rename deinstall >& log.$LANG\n";
 	}
 }
 close FILE;
