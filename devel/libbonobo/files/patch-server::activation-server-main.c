e8233f521c6357fdcad644447cbc61b2b6568acd
mezz <mezz@FreeBSD.org>
2005-08-28 17:47:42 +0000

    Update to 2.10.1.

--- devel/libbonobo/files/patch-server::activation-server-main.c
+++ devel/libbonobo/files/patch-server::activation-server-main.c
@@ -1,11 +1,11 @@
---- activation-server/activation-server-main.c.orig	Fri Oct 18 06:15:12 2002
-+++ activation-server/activation-server-main.c	Sat Nov  9 01:50:36 2002
-@@ -127,7 +127,7 @@
+--- activation-server/activation-server-main.c.orig	Fri Apr 15 20:37:07 2005
++++ activation-server/activation-server-main.c	Fri Aug 19 16:27:59 2005
+@@ -197,7 +197,7 @@ build_src_dir (void)
                          g_string_append (gnome_od_source_dir,
                                           gnome_dirs[i]);
                          g_string_append (gnome_od_source_dir,
--                                         "/lib/bonobo/servers:");
-+                                         "/libdata/bonobo/servers:");
+-                                         "/lib/bonobo/servers" G_SEARCHPATH_SEPARATOR_S);
++                                         "/libdata/bonobo/servers" G_SEARCHPATH_SEPARATOR_S);
                  }
                  g_strfreev (gnome_dirs);
-                 g_string_append_c (real_od_source_dir, ':');
+                 g_string_append (real_od_source_dir,
