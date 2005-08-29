5048c326fce2e684cc32ac6ecb2a461668afa909
ahze <ahze@FreeBSD.org>
2005-08-29 02:38:14 +0000

    - Update to 1.4.3
    PR:		ports/85374
    Submitted by:	Ports Fury

--- audio/mhwaveedit/files/patch-src-filetypes.c
+++ audio/mhwaveedit/files/patch-src-filetypes.c
@@ -1,5 +1,5 @@
---- src/filetypes.c.orig	Wed Jul 13 07:44:46 2005
-+++ src/filetypes.c	Thu Jul 14 01:59:03 2005
+--- src/filetypes.c.orig	Sat Aug 13 02:01:54 2005
++++ src/filetypes.c	Sat Aug 13 15:45:17 2005
 @@ -97,15 +97,6 @@
       return FALSE;
  }
@@ -16,7 +16,7 @@
  static void register_file_type(gchar *name, gchar *ext, 
  			       gboolean (*typecheck)(gchar *filename),
  			       Chunk *(*load)(gchar *filename, 
-@@ -922,7 +913,7 @@
+@@ -927,7 +918,7 @@
       b = pipe_dialog_send_chunk(y,c,FALSE,dither_mode,bar);
       g_free(c);     
       if (x != NULL) gtk_object_sink(GTK_OBJECT(x));
@@ -25,7 +25,7 @@
       if (b || !file_exists(filename)) {
  	  *fatal = TRUE;
  	  return TRUE;
-@@ -980,7 +971,7 @@
+@@ -985,7 +976,7 @@
  	       gtk_object_sink(GTK_OBJECT(x));
  	  }
       }
@@ -34,3 +34,40 @@
       if (b || !file_exists(filename)) {
  	  *fatal = TRUE;
  	  return TRUE;
+@@ -999,7 +990,11 @@
+ static Chunk *try_mplayer(gchar *filename, int dither_mode, StatusBar *bar)
+ {
+      gchar *c,*d;
+-     char *tempname;
++     gchar *tempname;
++     gchar *argv[] = { "sh", "-c", 
++		      "mplayer -quiet -noconsolecontrols "
++		      "-ao \"pcm:file=$OUTFILE\" -vc dummy -vo null "
++		      "\"$INFILE\"", NULL };
+      Chunk *x;
+      if (!program_exists("mplayer")) return NULL;
+      tempname = get_temp_filename(0);
+@@ -1008,20 +1003,16 @@
+      if (xputenv(c)) { g_free(d); g_free(c); g_free(tempname); return NULL; }
+      if (xputenv(d)) { 
+ 	  g_free(d); 
+-	  if (!xunsetenv("OUTFILE"))
++	  if (!xputenv("OUTFILE"))
+ 	       g_free(c);
+ 	  g_free(tempname);
+ 	  return NULL; 
+      }
+-     char *argv[] = { "sh", "-c", 
+-		      "mplayer -quiet -noconsolecontrols "
+-		      "-ao \"pcm:file=$OUTFILE\" -vc dummy -vo null "
+-		      "\"$INFILE\"", NULL };
+ 
+      x = run_decoder(filename,tempname,"sh",argv,dither_mode,bar);
+ 
+-     if (!xunsetenv("OUTFILE")) g_free(c);
+-     if (!xunsetenv("INFILE")) g_free(d);
++     if (!xputenv("OUTFILE")) g_free(c);
++     if (!xputenv("INFILE")) g_free(d);
+      g_free(tempname);
+      
+      return x;
