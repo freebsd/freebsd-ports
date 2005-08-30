43badb79776ed890bee69ef534c04656e95db0c8
pav <pav@FreeBSD.org>
2005-08-30 19:03:11 +0000

    - Fix build on FreeBSD 7
    PR:		ports/85496
    Submitted by:	Jonathan <onatan@gmail.com> (maintainer)

--- sysutils/memdump/files/patch-memdump.c
+++ sysutils/memdump/files/patch-memdump.c
@@ -186,7 +186,7 @@
  
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
 -	|| defined(FREEBSD5) \
-+	|| defined(FREEBSD5) || defined(FREEBSD6) \
++	|| defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
  	|| defined(OPENBSD2) || defined(OPENBSD3) \
  	|| defined(BSDI2) || defined(BSDI3) || defined(BSDI4)
  #include <sys/param.h>
@@ -195,7 +195,7 @@
  
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
 -	|| defined(FREEBSD5) \
-+	|| defined(FREEBSD5) || defined(FREEBSD6) \
++	|| defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
  	|| defined(OPENBSD2) || defined(OPENBSD3)
      int     name[] = {CTL_HW, HW_PHYSMEM};
      size_t  len;
