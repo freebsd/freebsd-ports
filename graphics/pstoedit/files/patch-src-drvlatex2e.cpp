d3997839a5817883b2834ca1f97ee62a8aa4ba22
Ying-Chieh Liao <ijliao@FreeBSD.org>
2005-08-30 03:05:40 +0000

    upgrade to 3.41
    PR:		85381
    Submitted by:	Ports Fury

---
+++ graphics/pstoedit/files/patch-src-drvlatex2e.cpp
@@ -0,0 +1,30 @@
+--- src/drvlatex2e.cpp.pre_tr	Sat Jun 18 22:35:42 2005
++++ src/drvlatex2e.cpp	Tue Aug 23 00:30:01 2005
+@@ -284,27 +284,6 @@
+ 	const char * cp = textinfo.thetext.value();
+ 	//buffer << textinfo.thetext.value() 
+ 
+-#if 0
+-// characters to be handled specially	
+-% -> \%
+-# -> \#
+-{ -> \{
+-} -> \}
+-_ -> \_
+-& -> \&
+-$ -> \$  
+-	Tritt auch manchmal mit veränderter Bedeutung als $$ auf: --> \$\$
+-
+-\ -> \textbackslash
+-^ -> \textasciicircum
+-~ -> \textasciitilde
+-
+-Zusätzlich machen folgende Zeichen oft Probleme:
+-
+-" -> \textquotedblright
+-?`-> \textquestiondown
+-!`-> \textexclamdown
+-#endif
+ 	while (cp && *cp) {
+ 		if ((*cp == '%') ||
+ 			(*cp == '#') ||
