405d6277bea4b5adf79539f5430d98d1a3dd637b
ahze <ahze@FreeBSD.org>
2005-08-30 01:10:02 +0000

    - Update to 0.35.r1
    PR:		ports/85456
    Submitted by:	Alejandro Pulver <alejandro@varnet.biz> (maintainer)

--- emulators/generator-cbiere/files/patch-hdr_generator.h
+++ emulators/generator-cbiere/files/patch-hdr_generator.h
@@ -1,6 +1,6 @@
---- hdr/generator.h.orig	Mon Jan  3 19:07:55 2005
-+++ hdr/generator.h	Thu Jun 16 22:56:43 2005
-@@ -257,14 +257,22 @@
+--- hdr/generator.h.orig	Sun Aug 28 14:44:26 2005
++++ hdr/generator.h	Sun Aug 28 22:33:12 2005
+@@ -257,17 +257,26 @@
  #include "registers.h"
  #endif
  
@@ -9,18 +9,22 @@
 +#endif
 +
  #if defined(linux)
-   #include <byteswap.h>
-   #define SWAP16(x) bswap_16((x))
-   #define SWAP32(x) bswap_32((x))
+ 
+ #include <byteswap.h>
+ #define SWAP16(x) bswap_16((x))
+ #define SWAP32(x) bswap_32((x))
+ 
 -#elif defined(__FreeBSD__)
-+#elif defined(__FreeBSD__) && __FreeBSD_version >= 500000
-   #include <sys/endian.h>
-   #define SWAP16(x) bswap16((x))
-   #define SWAP32(x) bswap32((x))
++#elif defined(__FreeBSD__) && __FreeBSD_version >=500000
+ 
+ #include <sys/endian.h>
+ #define SWAP16(x) bswap16((x))
+ #define SWAP32(x) bswap32((x))
++
 +#elif defined(__FreeBSD__)
-+  #include <sys/endian.h>
-+  #define SWAP16(x) (be16toh(x))
-+  #define SWAP32(x) (be32toh(x))
++#include <sys/endian.h>
++#define SWAP16(x) (be16toh(x))
++#define SWAP32(x) (be32toh(x))
+ 
  #elif defined(__NetBSD__)
-   #include <sys/bswap.h>
-   #define SWAP16(x) bswap16((x))
+ 
