43f45c024ab327dc72d463ac07ba3f517fb41cbe
jylefort <jylefort@FreeBSD.org>
2005-08-31 00:54:05 +0000

    Add gnome-colorscheme.
    GNOME Colorscheme is a color scheme builder for the GNOME desktop. It is
    useful for web creation as well as room painting. It supports 6 various types
    of color schemes: Complements, Split Complements, Triads, Tetrads, Analogous,
    and Monochromatic.
    
    The user can lighten/darken the whole colorscheme or increase/decrease its
    saturation.
    
    WWW: http://home.gna.org/colorscheme/
    
    PR:		ports/85458
    Submitted by:	Piotr Smyrak <piotr.smyrak@heron.pl>

---
+++ graphics/agave/files/patch-src_main.cc
@@ -0,0 +1,10 @@
+--- src/main.cc.orig	Tue Aug 30 02:25:47 2005
++++ src/main.cc	Tue Aug 30 02:26:18 2005
+@@ -33,6 +33,7 @@
+ #include <libgnomeui/gnome-ui-init.h>
+ 
+ #include "gcs-mainwindow.h"
++#include "gcs-i18n.h"
+ 
+ namespace gcs
+ {
