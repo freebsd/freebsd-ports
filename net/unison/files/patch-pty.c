7b7b3e85fd1c1855037652339fb7e1b314d1a5f6
pav <pav@FreeBSD.org>
2005-08-31 11:37:57 +0000

    - Update to 2.13.16
    PR:		ports/85350
    Submitted by:	NAKAMURA Takeshi via
    		Dan Pelleg <daniel+unison@pelleg.org> (maintainer)

--- net/unison/files/patch-pty.c
+++ net/unison/files/patch-pty.c
@@ -1,13 +1,22 @@
---- pty.c.orig	Sun Jan 23 19:55:28 2005
-+++ pty.c	Sun Jan 23 19:56:28 2005
-@@ -32,6 +32,10 @@
- #ifdef __APPLE__
- #include <util.h>          // openpty
+--- pty.c.orig	Mon May 30 15:15:43 2005
++++ pty.c	Sat Aug 27 15:47:55 2005
+@@ -21,6 +21,7 @@
  #endif
-+#ifdef __FreeBSD__
-+#include <sys/types.h>
-+#include <libutil.h>
-+#endif
  
+ #ifdef __FreeBSD__
++#include <sys/types.h>
+ #include <libutil.h>
+ #define HAS_OPENPTY 1
+ #endif
+@@ -40,9 +41,10 @@
  /* c_openpty: unit -> (int * Unix.file_descr) */
  CAMLprim value c_openpty() {
+   int master,slave;
++  value pair;
+   if (openpty(&master,&slave,NULL,NULL,NULL) < 0)
+     uerror("openpty", (value) 0);
+-  value pair = alloc_tuple(2);
++  pair = alloc_tuple(2);
+   Store_field(pair,0,Val_int(master));
+   Store_field(pair,1,Val_int(slave));
+   return pair;
