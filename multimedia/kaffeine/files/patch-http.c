b280ba8740d47ac985385004ff973aeffe786963
arved <arved@FreeBSD.org>
2005-08-31 16:23:34 +0000

    Update to 0.7 - a gstreamer based part was added. - Detailed Changelog at: http://kaffeine.sourceforge.net/

--- multimedia/kaffeine/files/patch-http.c
+++ multimedia/kaffeine/files/patch-http.c
@@ -1,34 +1,22 @@
---- kaffeine/player-parts/http.c.orig	Wed Dec  8 15:07:06 2004
-+++ kaffeine/player-parts/http.c	Fri Dec 31 14:57:16 2004
-@@ -48,6 +48,7 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <sys/types.h>
-+#include <netinet/in.h>
- #include <sys/socket.h>
- #include <netinet/in.h>
- #include <netdb.h>
-@@ -56,6 +57,11 @@
- #include <string.h>
- #include <ctype.h>
- #include <errno.h>
-+#ifdef HAVE_STDINT_H
-+#include <stdint.h>
-+#else
-+#include <inttypes.h>
-+#endif
- 
- #include "http.h"
- 
-@@ -514,7 +520,11 @@
+--- kaffeine/player-parts/http.c.orig	Wed Aug 31 16:16:57 2005
++++ kaffeine/player-parts/http.c	Wed Aug 31 16:23:33 2005
+@@ -492,10 +492,19 @@
  	}
        } else {
  	if (this->contentlength == 0) {
 +#ifdef HAVE_STDINT_H
  	  intmax_t contentlength;
+ 
+ 	  if ( strlen(this->buf)<(BUFSIZE-20) && sscanf(this->buf, "Content-Length: %" PRIdMAX, &contentlength) == 1) {
++
+ 	    printf ("http: content length = %" PRIdMAX " bytes\n", contentlength);
 +#else
 +	  int64_t contentlength;
++
++	  if ( strlen(this->buf)<(BUFSIZE-20) && sscanf(this->buf, "Content-Length: %Ld", &contentlength) == 1) {
++	    printf ("http: content length = %Ld bytes\n", contentlength);
 +#endif
- 	  
- 	  if ( strlen(this->buf)<(BUFSIZE-20) && sscanf(this->buf, "Content-Length: %Ld", &contentlength) == 1) {
- 	    printf ("http: content length = %Ld bytes\n", contentlength);
++
+ 	    this->contentlength = (off_t)contentlength;
+ 	  }
+         }
