89621463ecc452ee6e7721ebfa020ba9dc622b87
kuriyama <kuriyama@FreeBSD.org>
2005-09-01 09:22:02 +0000

    - Unbreak on 4.x by using "use vars" instead of "our". - Add forgotten $PKGNAMEPREFIX.
    Reported by:	pointyhat via kris

---
+++ math/p5-Math-Vec/files/patch-Vec.pm
@@ -0,0 +1,10 @@
+--- Vec.pm.orig	Thu Sep  1 18:16:13 2005
++++ Vec.pm	Thu Sep  1 18:16:43 2005
+@@ -1,5 +1,6 @@
+ package Math::Vec;
+-our $VERSION   = '0.02';
++use vars qw($VERSION);
++$VERSION   = '0.02';
+ 
+ =pod
+ 
