509cdc26e44774af89493655cb89aa054bedb9d5
Renato Botelho <garga@FreeBSD.org>
2005-09-01 19:24:36 +0000

    - Update to 4.2p1
    PR:		ports/85578
    Submitted by:	Marcus Grando <marcus@corp.grupos.com.br>

--- security/openssh-portable/files/patch-auth1.c
+++ security/openssh-portable/files/patch-auth1.c
@@ -1,5 +1,5 @@
---- auth1.c.orig	Tue Feb  8 11:52:48 2005
-+++ auth1.c	Sat Mar 19 21:34:47 2005
+--- auth1.c.orig	Sun Jul 17 04:26:44 2005
++++ auth1.c	Thu Sep  1 11:23:35 2005
 @@ -26,6 +26,7 @@
  #include "uidswap.h"
  #include "monitor_wrap.h"
@@ -8,10 +8,10 @@
  
  /* import */
  extern ServerOptions options;
-@@ -71,6 +72,15 @@
- 	u_int dlen;
- 	u_int ulen;
- 	int prev, type = 0;
+@@ -220,6 +221,15 @@
+ 	char info[1024];
+ 	int prev = 0, type = 0;
+ 	const struct AuthMethod1 *meth;
 +#ifdef HAVE_LOGIN_CAP
 +	login_cap_t *lc;
 +#endif /* HAVE_LOGIN_CAP */
@@ -24,38 +24,37 @@
  
  	debug("Attempting authentication for %s%.100s.",
  	    authctxt->valid ? "" : "invalid user ", authctxt->user);
-@@ -219,6 +229,34 @@
- 			logit("Unknown message during authentication: type %d", type);
- 			break;
+@@ -270,6 +280,33 @@
+ 			    "type %d", type);
+ 			goto skip;
  		}
 +
 +#ifdef HAVE_LOGIN_CAP
 +		if (authctxt->pw != NULL) {
-+		  lc = login_getpwclass(authctxt->pw);
-+		  if (lc == NULL)
-+			lc = login_getclassbyname(NULL, authctxt->pw);
-+		  if (!auth_hostok(lc, from_host, from_ip)) {
-+			logit("Denied connection for %.200s from %.200s [%.200s].",
-+		      authctxt->pw->pw_name, from_host, from_ip);
-+			packet_disconnect("Sorry, you are not allowed to connect.");
-+		  }
-+		  if (!auth_timeok(lc, time(NULL))) {
-+			logit("LOGIN %.200s REFUSED (TIME) FROM %.200s",
-+		      authctxt->pw->pw_name, from_host);
-+			packet_disconnect("Logins not available right now.");
-+		  }
-+		  login_close(lc);
-+		  lc = NULL;
++			lc = login_getpwclass(authctxt->pw);
++			if (lc == NULL)
++				lc = login_getclassbyname(NULL, authctxt->pw);
++			if (!auth_hostok(lc, from_host, from_ip)) {
++				logit("Denied connection for %.200s from %.200s [%.200s].",
++				       authctxt->pw->pw_name, from_host, from_ip);
++				packet_disconnect("Sorry, you are not allowed to connect.");
++			}
++			if (!auth_timeok(lc, time(NULL))) {
++				logit("LOGIN %.200s REFUSED (TIME) FROM %.200s",
++				       authctxt->pw->pw_name, from_host);
++				packet_disconnect("Logins not available right now.");
++			}
++			login_close(lc);
++			lc = NULL;
 +		}
 +#endif  /* HAVE_LOGIN_CAP */
-+#ifdef LOGIN_ACCESS
++#ifdef LOGIN_ACCESS                                                       
 +		if (authctxt->pw != NULL && !login_access(authctxt->pw->pw_name, from_host)) {
-+		  logit("Denied connection for %.200s from %.200s [%.200s].",
-+		      authctxt->pw->pw_name, from_host, from_ip);
-+		  packet_disconnect("Sorry, you are not allowed to connect.");
++			logit("Denied connection for %.200s from %.200s [%.200s].",
++			       authctxt->pw->pw_name, from_host, from_ip);
++			packet_disconnect("Sorry, you are not allowed to connect.");
 +		}
 +#endif /* LOGIN_ACCESS */
-+
- #ifdef BSD_AUTH
- 		if (authctxt->as) {
- 			auth_close(authctxt->as);
+ 
+ 		if (!*(meth->enabled)) {
+ 			verbose("%s authentication disabled.", meth->name);
