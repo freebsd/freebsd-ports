737b634f8d0a3829217e85fde6a80e3f1865e895
sem <sem@FreeBSD.org>
2005-09-02 12:55:55 +0000

    - One more patch to fix things from my last commit - Bump PORTREVISION
    Submitted by:	maintainer

--- net-mgmt/nagios-plugins/files/patch-check_tcp.c
+++ net-mgmt/nagios-plugins/files/patch-check_tcp.c
@@ -1,5 +1,5 @@
---- plugins/check_tcp.c.orig	Sun Jun  5 21:43:58 2005
-+++ plugins/check_tcp.c	Thu Sep  1 12:16:31 2005
+--- plugins/check_tcp.c.orig	Sun Jun  5 19:43:58 2005
++++ plugins/check_tcp.c	Mon Aug 15 21:05:57 2005
 @@ -179,7 +179,7 @@
  		SEND = "<stream:stream to=\'host\' xmlns=\'jabber:client\' xmlns:stream=\'http://etherx.jabber.org/streams\'>\n";
  		EXPECT = "<?xml version=\'1.0\'?><stream:stream xmlns:stream=\'http://etherx.jabber.org/streams\'";
