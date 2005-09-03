40c5f8c6f6859125bf9f086dbf54bd3479c285e5
Alexander Leidinger <netchild@FreeBSD.org>
2005-09-03 09:57:32 +0000

    There's another place where a fix for 5.x and 4.x is needed.
    Bump PORTREVISION, pointyhat doesn't seems to have picked up the
    previous fix in the most recent run, it still displays the old error.
    
    Noticed by:	c0rn@gazeta.pl

--- mail/claws-mail/files/patch-src_etpan_imap-thread.c
+++ mail/claws-mail/files/patch-src_etpan_imap-thread.c
@@ -1,7 +1,10 @@
---- src/etpan/imap-thread.c.orig	Sat Jul 30 11:57:41 2005
-+++ src/etpan/imap-thread.c	Sat Jul 30 11:58:06 2005
-@@ -8,6 +8,7 @@
+--- src/etpan/imap-thread.c.orig	Fri Jul 22 13:30:23 2005
++++ src/etpan/imap-thread.c	Sat Sep  3 11:05:25 2005
+@@ -6,8 +6,10 @@
+ 
+ #include "imap-thread.h"
  #include <imap.h>
++#include <pthread.h>
  #include <sys/types.h>
  #include <sys/stat.h>
 +#include <sys/socket.h>
