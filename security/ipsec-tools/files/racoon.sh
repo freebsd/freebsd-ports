9eebd2f1314e10cd7dc029b410a93d3817bd17f9
Vsevolod Stakhov <vsevolod@FreeBSD.org>
2005-09-05 14:13:42 +0000

    Add IPSec tools port - the new "official" version of racoon, is the only one which is maintained and have lots of new features.
    PR:		85544
    Submitted by:	VANHULLEBUS Yvan <vanhu@netasq.com>
    Approved by:	perky (mentor)

---
+++ security/ipsec-tools/files/racoon.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Start or stop racoon
+# $FreeBSD: /tmp/pcvs/ports/security/ipsec-tools/files/Attic/racoon.sh,v 1.1 2005-09-05 14:13:42 vsevolod Exp $
+
+# PROVIDE: racoon
+# REQUIRE: DAEMON
+# BEFORE: LOGIN
+# KEYWORD: FreeBSD shutdown
+#
+# NOTE for FreeBSD 5.0+:
+# If you want this script to start with the base rc scripts
+# move racoon.sh to /etc/rc.d/racoon
+
+prefix=%%PREFIX%%
+
+# Define these racoon_* variables in one of these files:
+#	/etc/rc.conf
+#	/etc/rc.conf.local
+#	/etc/rc.conf.d/racoon
+#
+# DO NOT CHANGE THESE DEFAULT VALUES HERE
+#
+[ -z "$racoon_enable" ] && racoon_enable="YES"	# Enable racoon
+#racoon_program="${prefix}/sbin/racoon"		# Location of racoon
+#racoon_flags=""				# Flags to racoon program
+
+. %%RC_SUBR%%
+
+name="racoon"
+rcvar=`set_rcvar`
+command="${prefix}/sbin/racoon"
+pidfile="/var/run/racoon.pid"
+required_files="${prefix}/etc/racoon/racoon.conf"
+stop_postcmd="racoon_poststop"
+
+racoon_poststop() {
+	/bin/rm -f ${pidfile}
+}
+
+load_rc_config $name
+run_rc_command "$1"
