fe361b3f4bb50238db87e7e65fb0d41114e19001
marcus <marcus@FreeBSD.org>
2005-09-12 06:37:45 +0000

    Fix build with new libsmbclient, and bump PORTREVISION due to ABI change.

--- devel/gnomevfs2/files/patch-modules_smb-method.c
+++ devel/gnomevfs2/files/patch-modules_smb-method.c
@@ -1,6 +1,15 @@
---- modules/smb-method.c.orig	Tue Jan 11 18:24:53 2005
-+++ modules/smb-method.c	Tue Jan 11 18:25:03 2005
-@@ -1345,7 +1345,7 @@
+--- modules/smb-method.c.orig	Wed Mar  2 18:44:11 2005
++++ modules/smb-method.c	Mon Sep 12 02:36:28 2005
+@@ -1425,7 +1425,7 @@ do_close (GnomeVFSMethod *method,
+ 
+ 		/* Important: perform_authentication leaves and re-enters the lock! */
+ 		while (perform_authentication (&actx) > 0) {
+-			r = smb_context->close (smb_context, handle->file);
++			r = smb_context->close_fn (smb_context, handle->file);
+ 			actx.res = (r >= 0) ? GNOME_VFS_OK : gnome_vfs_result_from_errno ();
+ 		}
+ 
+@@ -1446,7 +1446,7 @@ do_read (GnomeVFSMethod *method,
  	 GnomeVFSContext *context)
  {
  	FileHandle *handle = (FileHandle *)method_handle;
