1aed20573f2c4584ca1fa77c67448ad27b943537
lofi <lofi@FreeBSD.org>
2005-09-12 14:36:17 +0000

    Update to 0.9.12

---
+++ security/libksba/files/patch-src_asn1-func.c
@@ -0,0 +1,11 @@
+--- src/asn1-func.c.orig	Mon Sep 12 16:13:20 2005
++++ src/asn1-func.c	Mon Sep 12 16:13:28 2005
+@@ -26,8 +26,6 @@
+ #include <ctype.h>
+ #include <assert.h>
+ 
+-#include <alloca.h>
+-
+ #include "ksba.h"
+ #include "asn1-func.h"
+ #include "util.h"
