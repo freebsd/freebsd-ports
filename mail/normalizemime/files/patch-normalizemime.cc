d3b1a26146dec49a585fd900e7f11530a3b54d2d
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2005-09-13 16:53:25 +0000

    o Improve previous fix for building on OSVERSION < 500000: simple   patch instead of USE_GCC=3.4+ o Bump PORTREVISION due to dependency change

--- mail/normalizemime/files/patch-normalizemime.cc
+++ mail/normalizemime/files/patch-normalizemime.cc
@@ -1,6 +1,15 @@
---- normalizemime.cc.orig	Tue Aug 30 21:31:02 2005
-+++ normalizemime.cc	Tue Aug 30 21:33:08 2005
-@@ -498,7 +498,7 @@
+--- normalizemime.cc.orig	Tue Sep 13 00:03:52 2005
++++ normalizemime.cc	Tue Sep 13 00:04:18 2005
+@@ -58,6 +58,8 @@
+ #include <map>
+ #include <string>
+ 
++#include <sys/types.h>
++
+ #include <regex.h>
+ #include <assert.h>
+ #include <ctype.h>
+@@ -498,7 +500,7 @@
    char *obuf = buf;
    memcpy (icopy, message.c_str(), in_left);
    message.erase ();
