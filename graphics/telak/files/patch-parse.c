108960574aded64a7314d0ffdf9cef9768b62fb5
Jean-Yves Lefort <jylefort@FreeBSD.org>
2005-09-13 23:15:46 +0000

    Add telak.
    Telak is a small tool to draw local or remote pictures on your root window.
    This is very useful if you want to have RRD graphs drawn above your wallpaper.
    
    WWW: http://julien.danjou.info/telak.html
    
    PR:		ports/86069
    Submitted by:	Emanuel Haupt <ehaupt@critical.ch>

---
+++ graphics/telak/files/patch-parse.c
@@ -0,0 +1,10 @@
+--- parse.c.orig	Tue Sep 13 22:05:22 2005
++++ parse.c	Tue Sep 13 22:05:39 2005
+@@ -17,6 +17,7 @@
+ #include <sys/types.h>
+ #include <pwd.h>
+ #include <time.h>
++#include <sys/time.h>
+ 
+ #include "telak.h"
+ #include "image.h"
