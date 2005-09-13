7cced2806283b6127aa937cb41b524c668088500
Pav Lucistnik <pav@FreeBSD.org>
2005-09-13 16:04:44 +0000

    - Update to 0.0.20050512
    PR:		ports/85362
    Submitted by:	Erik Greenwald <erik@smluc.org> (maintainer)

---
+++ graphics/bugle/files/patch-budgie_budgie.cpp
@@ -0,0 +1,10 @@
+--- budgie/budgie.cpp.orig	Tue Sep 13 10:56:30 2005
++++ budgie/budgie.cpp	Tue Sep 13 10:56:36 2005
+@@ -25,7 +25,6 @@
+ #include "tree.h"
+ #include "treeutils.h"
+ #include "budgie.h"
+-#include "getopt.h"
+ #include <iostream>
+ #include <fstream>
+ #include <sstream>
