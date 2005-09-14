3bbf18e543ef3f256d80f07d9af1c05731bc72ed
Pav Lucistnik <pav@FreeBSD.org>
2005-09-14 17:44:45 +0000

    - Update to 1.7.1 - Take maintainership
    PR:		ports/86104
    Submitted by:	Marcus von Appen <mva@sysfault.org>

--- devel/py-game/files/patch-config_unix.py
+++ devel/py-game/files/patch-config_unix.py
@@ -1,25 +1,39 @@
-
-$FreeBSD$
-
---- config_unix.py.orig	Sat Mar 23 10:23:12 2002
-+++ config_unix.py	Tue May 28 12:58:39 2002
-@@ -113,7 +113,7 @@
+--- config_unix.py.orig	Wed Aug 10 04:45:11 2005
++++ config_unix.py	Wed Sep 14 09:12:44 2005
+@@ -4,7 +4,7 @@
+ from glob import glob
+ from distutils.sysconfig import get_python_inc
  
- sdl_lib_name = 'SDL'
- if sys.platform.find('bsd') != -1:
--    sdl_lib_name = 'SDL-1.2'
-+    sdl_lib_name = 'SDL-1.1'
+-configcommand = os.environ.get('SDL_CONFIG', 'sdl-config',)
++configcommand = os.environ.get('SDL_CONFIG', 'sdl11-config',)
+ configcommand = configcommand + ' --version --cflags --libs'
+ localbase = os.environ.get('LOCALBASE', '')
  
- 
+@@ -52,6 +52,9 @@
+                     self.cflags += f + ' '
+                 elif f[:3] == '-Wl':
+                     self.cflags += '-Xlinker ' + f + ' '
++	    if self.name == 'SDL':
++		inc = '-I' + os.environ.get ('X11BASE', '') + '/include'
++		self.cflags = inc + ' ' + self.cflags
+         except:
+             print 'WARNING: "%s" failed!' % command    
+             self.found = 0
+@@ -137,7 +140,7 @@
  def main():
-@@ -132,8 +132,8 @@
+     print '\nHunting dependencies...'
+     DEPS = [
+-        DependencyProg('SDL', 'SDL_CONFIG', 'sdl-config', '1.2', 'sdl'),
++        DependencyProg('SDL', 'SDL_CONFIG', 'sdl11-config', '1.2', 'sdl'),
+         Dependency('FONT', 'SDL_ttf.h', 'libSDL_ttf.so', 'SDL_ttf'),
+         Dependency('IMAGE', 'SDL_image.h', 'libSDL_image.so', 'SDL_image'),
+         Dependency('MIXER', 'SDL_mixer.h', 'libSDL_mixer.so', 'SDL_mixer'),
+@@ -146,7 +149,7 @@
+     ]
+ 
+     if not DEPS[0].found:
+-        print 'Unable to run "sdl-config". Please make sure a development version of SDL is installed.'
++        print 'Unable to run "sdl11-config". Please make sure a development version of SDL is installed.'
          raise SystemExit
  
- 
--    if localbase: #unneeded?
--        incdirs = [localbase + '/include/SDL']
-+    if localbase: #unneeded? nah, needed on FreeBSD!
-+        incdirs = [localbase + '/include/SDL11', localbase + '/include/smpeg']
-         libdirs = [localbase + '/lib']
-     else:
-         incdirs = []
+     if localbase:
