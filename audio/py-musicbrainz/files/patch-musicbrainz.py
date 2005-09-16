99369ba169b1bd0bc35fe05011b88702c3dce679
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2005-09-16 22:17:13 +0000

    Fix build on FreeBSD 4.x branch
    Submitted by:	pointyhat (kris)

--- audio/py-musicbrainz/files/patch-musicbrainz.py
+++ audio/py-musicbrainz/files/patch-musicbrainz.py
@@ -1,6 +1,6 @@
 --- musicbrainz.py.orig	Tue Aug 17 03:44:32 2004
-+++ musicbrainz.py	Sat Aug 20 13:57:38 2005
-@@ -35,7 +35,13 @@
++++ musicbrainz.py	Fri Sep 16 19:14:15 2005
+@@ -35,7 +35,16 @@
          if sys.platform == "linux2":
             lib = "libmusicbrainz.so.4"
          else:
@@ -11,7 +11,10 @@
 +                if sys.platform == "freebsd6":
 +                   lib = "libmusicbrainz.so.4"
 +                else:
-+                    raise MusicBrainzError, "Unknown platform: " + sys.platform 
++                   if sys.platform == "freebsd4":
++                      lib = "libmusicbrainz.so.4"
++                   else:
++                      raise MusicBrainzError, "Unknown platform: " + sys.platform 
    
      path = "/usr/lib/" + lib
      if os.access(path, os.F_OK):
