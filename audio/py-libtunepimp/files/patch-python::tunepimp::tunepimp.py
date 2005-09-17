5a3210b445d6dd1ecc684f281f197422adbbd2c0
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2005-09-17 01:45:52 +0000

    Fix build on FreeBSD 4.x branch

--- audio/py-libtunepimp/files/patch-python::tunepimp::tunepimp.py
+++ audio/py-libtunepimp/files/patch-python::tunepimp::tunepimp.py
@@ -1,6 +1,6 @@
---- python/tunepimp/tunepimp.py.orig	Sat Aug 20 13:53:49 2005
-+++ python/tunepimp/tunepimp.py	Sat Aug 20 13:54:58 2005
-@@ -137,7 +137,13 @@
+--- python/tunepimp/tunepimp.py.orig	Sat Jul  2 22:13:08 2005
++++ python/tunepimp/tunepimp.py	Fri Sep 16 22:44:39 2005
+@@ -137,7 +137,16 @@
          if sys.platform == "linux2":
             lib = "libtunepimp.so.3"
          else:
@@ -11,7 +11,10 @@
 +                if sys.platform == "freebsd6":
 +                   lib = "libtunepimp.so.3"
 +                else:
-+                    raise TunePimpError, "Unknown platform: " + sys.platform 
++                    if sys.platform == "freebsd4":
++                       lib = "libtunepimp.so.3"
++                    else:
++                        raise TunePimpError, "Unknown platform: " + sys.platform 
  
      pathList = [ ]
      ldLibPath = os.environ.get('LD_LIBRARY_PATH')
