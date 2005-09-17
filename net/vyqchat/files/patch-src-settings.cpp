05facefee14cd649e7cbd35ea8d3b89cfedbe668
Pav Lucistnik <pav@FreeBSD.org>
2005-09-17 22:40:23 +0000

    VyQChat is a real-time, text-based, serverless chat program dedicated to LANs, that runs on Linux using Qt/X11 library. It is almost 100% compatible with Vypress Chat(TM) for Windows. It allows you to chat with friends on public or private channels, send and recieve messages etc. The GUI is meant to be user-friendly and lets you to do most things with mouse. There is also optional sound support. VyQChat has been written in C++ and requires Qt library, version 3.x.
    PR:		ports/86257
    Submitted by:	Victor Semionov <semionov@mail.bg>

---
+++ net/vyqchat/files/patch-src-settings.cpp
@@ -0,0 +1,12 @@
+--- src/settings.cpp    Mon Jul 18 21:54:08 2005
++++ src/settings.cpp    Sun Aug  7 15:59:58 2005
+@@ -14,8 +14,8 @@
+ #include "settings.h"
+ #include <unistd.h>
+ #include <sys/ioctl.h>
+-#include <net/if.h>
+ #include <sys/socket.h>
++#include <net/if.h>
+ #include <sys/types.h>
+ #include <arpa/inet.h>
+ #include <stdlib.h>
