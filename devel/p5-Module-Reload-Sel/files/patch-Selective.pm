9dd55cf6f58073f71551d19a22e31914ba8af440
Lars Thegler <lth@FreeBSD.org>
2005-09-17 17:45:30 +0000

    Add p5-Module-Reload-Sel 1.02, reload perl library files when updated on disk.
    PR:		ports/85126
    Submitted by:	simond@irrelevant.org

---
+++ devel/p5-Module-Reload-Sel/files/patch-Selective.pm
@@ -0,0 +1,11 @@
+--- Selective.pm.orig	Wed May  8 01:45:28 2002
++++ Selective.pm	Mon Apr 25 14:48:59 2005
+@@ -412,8 +412,6 @@
+ 
+ =cut
+ 
+-{};	## Get emacs to indent correctly. Sigh.
+-
+ use Data::Dumper;
+ 
+ BEGIN
