f1edb0759bceb5c9ed1bbe49efafb98f3474b91b
Pav Lucistnik <pav@FreeBSD.org>
2005-09-17 20:20:37 +0000

    - Fix build on -CURRENT
    PR:		ports/86271
    Submitted by:	Ports Fury

--- www/cherokee-devel/files/patch-cherokee-fdpoll-kqueue.c
+++ www/cherokee-devel/files/patch-cherokee-fdpoll-kqueue.c
@@ -1,6 +1,14 @@
 --- cherokee/fdpoll-kqueue.c.orig	Fri May 27 02:42:27 2005
-+++ cherokee/fdpoll-kqueue.c	Mon Jul 11 15:41:35 2005
-@@ -92,7 +92,7 @@
++++ cherokee/fdpoll-kqueue.c	Fri Sep 16 00:03:37 2005
+@@ -27,6 +27,7 @@
+ #include "fdpoll-protected.h"
+ 
+ #include <stdio.h>
++#include <sys/types.h>
+ #include <sys/event.h>
+ #include <sys/time.h>
+ #include <unistd.h>
+@@ -92,7 +93,7 @@
  		 */
  		index = fdp->nchanges;
  		if ( index > FDPOLL(fdp)->nfiles) {
