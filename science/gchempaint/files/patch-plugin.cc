882b503c4c8423c75c7b0acdcea07b2eb155341b
Pav Lucistnik <pav@FreeBSD.org>
2005-09-19 15:56:49 +0000

    - Update to 0.6.0

---
+++ science/gchempaint/files/patch-plugin.cc
@@ -0,0 +1,10 @@
+--- plugins/templates/plugin.cc.orig	Mon Aug 15 12:43:14 2005
++++ plugins/templates/plugin.cc	Mon Sep 19 14:20:05 2005
+@@ -29,7 +29,6 @@
+ #include "lib/application.h"
+ #include "templatetool.h"
+ #include "templatetree.h"
+-#include <mcheck.h>
+ #include <sys/stat.h>
+ 
+ gcpTemplatesPlugin plugin;
