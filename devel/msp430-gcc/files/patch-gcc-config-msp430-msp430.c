8590021a592bc0826e657e952cd87ac53e91654f
lev <lev@FreeBSD.org>
2005-09-19 13:39:41 +0000

    Latest gcc 3.4.4, latest msp430 patches.

---
+++ devel/msp430-gcc/files/patch-gcc-config-msp430-msp430.c
@@ -0,0 +1,10 @@
+--- gcc/config/msp430/msp430.c.orig	Mon Sep 19 17:03:53 2005
++++ gcc/config/msp430/msp430.c	Mon Sep 19 17:04:11 2005
+@@ -149,6 +149,7 @@
+   MSP430_ISA_14 = 14,
+   MSP430_ISA_15 = 15,
+   MSP430_ISA_16 = 16,
++  MSP430_ISA_210 = 210,
+   MSP430_ISA_31 = 31,
+   MSP430_ISA_32 = 32,
+   MSP430_ISA_33 = 33,
