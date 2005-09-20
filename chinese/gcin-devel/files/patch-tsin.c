e72b2deda5410a8f9de3ecae00a8940256780085
Vanilla I. Shu <vanilla@FreeBSD.org>
2005-09-20 16:13:36 +0000

    Upgrade to 1.0.7.
    PR:		ports/86373
    Submitted by:	chinsan

---
+++ chinese/gcin-devel/files/patch-tsin.c
@@ -0,0 +1,11 @@
+--- tsin.c.orig	Tue Sep 20 22:26:31 2005
++++ tsin.c	Tue Sep 20 23:01:14 2005
+@@ -469,7 +469,7 @@
+ }
+ 
+ #define PH_SHIFT_N (tsin_buffer_size - 1)
+-void compact_win0_x();
++//void compact_win0_x();
+ 
+ static void shift_ins()
+ {
