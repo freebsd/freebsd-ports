8895e3ce8a41733c4f83bba684baa7bd2a54fe3d
nobutaka <nobutaka@FreeBSD.org>
2005-09-20 16:44:33 +0000

    Update to 1.1.0. Use internal liba52 and libmad. Add WITHOUT_* knob. [*]
    PR:		ports/84743 [*]
    Submitted by:	Carlos A M dos Santos <casantos@urisan.tche.br> [*]

---
+++ multimedia/libxine/files/patch-src:post:deinterlace:plugins:tomsmocomp:tomsmocompmacros.h
@@ -0,0 +1,11 @@
+--- src/post/deinterlace/plugins/tomsmocomp/tomsmocompmacros.h.orig	Sat Sep 17 01:51:42 2005
++++ src/post/deinterlace/plugins/tomsmocomp/tomsmocompmacros.h	Sat Sep 17 01:52:41 2005
+@@ -1,7 +1,6 @@
+-#include <malloc.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <math.h>
+-#include <malloc.h>
+ 
+ #define USE_FOR_DSCALER
+ 
