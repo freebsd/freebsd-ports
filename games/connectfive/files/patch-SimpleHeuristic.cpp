9c63fe7e9f01aacb50e0f09e3d1a3123783450c8
barner <barner@FreeBSD.org>
2005-09-21 07:53:20 +0000

    Add connectfive 1.0.2, place five pieces in a row on a 3d board.
    PR:		86351
    Submitted by:	Dmitry Marakasov <amdmi3@mail.ru>

---
+++ games/connectfive/files/patch-SimpleHeuristic.cpp
@@ -0,0 +1,10 @@
+--- SimpleHeuristic.cpp.orig	Thu Jan  1 03:21:53 2004
++++ SimpleHeuristic.cpp	Wed Sep 21 09:30:36 2005
+@@ -31,6 +31,7 @@
+ 
+ 
+ // Header Files #############################################################
++#include <stdio.h>
+ #include "SimpleHeuristic.h"
+ 
+ // Macros ###################################################################
---
+++ games/connectfive/files/patch-glfont2.h
@@ -0,0 +1,13 @@
+--- glfont2.h.orig	Sat Dec 20 10:26:21 2003
++++ glfont2.h	Wed Sep 21 09:34:38 2005
+@@ -8,7 +8,9 @@
+ #ifndef GLFONT2_H
+ #define GLFONT2_H
+ 
+-//#include <pair.h>
++#if defined(__GNUC__) && (__GNUC__ < 3)
++#include <pair.h>
++#endif
+ #include <string>
+ #include <GL/gl.h>
+ #include <GL/glu.h>
