f66dcf1ebc4dbaa618860fad47760db86e26fd70
Tilman Keskinoz <arved@FreeBSD.org>
2005-09-21 18:26:09 +0000

    - Chase libxine shared library version - Fix compile on amd64

--- multimedia/gxine/files/patch-src::spidermonkey::jsprf.c
+++ multimedia/gxine/files/patch-src::spidermonkey::jsprf.c
@@ -1,11 +1,16 @@
---- src/spidermonkey/jsprf.c.orig	Sat Apr 19 02:11:15 2003
-+++ src/spidermonkey/jsprf.c	Sat Apr 19 02:12:27 2003
-@@ -57,7 +57,7 @@
+--- src/spidermonkey/jsprf.c.orig	Fri Mar  7 02:14:11 2003
++++ src/spidermonkey/jsprf.c	Wed Sep 21 20:20:45 2005
+@@ -57,7 +57,12 @@
  #define VARARGS_ASSIGN(foo, bar)	(foo) = (bar)
  #endif
  */
 -#define VARARGS_ASSIGN(foo, bar) __va_copy(foo, bar)
-+#define VARARGS_ASSIGN(foo, bar)        (foo) = (bar)
++#include <osreldate.h>
++#if __FreeBSD_version >= 500000
++#define VARARGS_ASSIGN(foo, bar) va_copy(foo, bar)
++#else
++#define VARARGS_ASSIGN(foo, bar)	(foo) = (bar)
++#endif
  
  /*
  ** WARNING: This code may *NOT* call JS_LOG (because JS_LOG calls it)
