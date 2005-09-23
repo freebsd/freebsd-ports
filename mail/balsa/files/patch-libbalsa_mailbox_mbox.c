ac37a6408f410d0444ed577d05628027acab5afd
marcus <marcus@FreeBSD.org>
2005-09-23 00:42:39 +0000

    Update to 2.3.5.

--- mail/balsa/files/patch-libbalsa_mailbox_mbox.c
+++ mail/balsa/files/patch-libbalsa_mailbox_mbox.c
@@ -1,19 +1,19 @@
---- libbalsa/mailbox_mbox.c.orig	Wed Nov 24 21:05:03 2004
-+++ libbalsa/mailbox_mbox.c	Wed Nov 24 21:06:54 2004
-@@ -27,6 +27,7 @@
- #include <libgnome/libgnome.h>
+--- libbalsa/mailbox_mbox.c.orig	Tue Sep 20 13:01:57 2005
++++ libbalsa/mailbox_mbox.c	Thu Sep 22 20:32:25 2005
+@@ -26,6 +26,7 @@
+ 
  #include <gmime/gmime-stream-fs.h>
  
 +#include <sys/stat.h>
  #include <stdlib.h>
  #include <unistd.h>
  #include <fcntl.h>
-@@ -195,7 +196,7 @@
+@@ -215,7 +216,7 @@ libbalsa_mailbox_mbox_create(const gchar
  	if(!create)
  	    return(-1);
  
 -	if ((fd = creat(path, S_IRUSR | S_IWUSR)) == -1) {
 +	if ((fd = open(path, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR)) == -1) {
- 	    g_warning("An error:\n%s\n occured while trying to "
+ 	    g_warning("An error:\n%s\n occurred while trying to "
  		      "create the mailbox \"%s\"\n",
  		      strerror(errno), path);
