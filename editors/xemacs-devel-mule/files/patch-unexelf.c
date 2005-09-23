83910b5577c72a208098f25bb8171de8b952fffc
Sergey Matveychuk <sem@FreeBSD.org>
2005-09-23 07:47:23 +0000

    - Try to fix build on amd64
    Submitted by:	maintainer

--- editors/xemacs-devel-mule/files/patch-unexelf.c
+++ editors/xemacs-devel-mule/files/patch-unexelf.c
@@ -1,6 +1,6 @@
---- src/unexelf.c.orig	Fri Jul 22 23:34:12 2005
-+++ src/unexelf.c	Fri Jul 22 23:34:42 2005
-@@ -477,7 +477,7 @@
+--- src/unexelf.c.orig	Mon Jul  4 16:11:27 2005
++++ src/unexelf.c	Thu Sep 22 22:38:19 2005
+@@ -478,7 +478,7 @@
  # include <sys/exec_elf.h>
  #endif
  
@@ -9,3 +9,14 @@
  # ifdef __STDC__ 
  #  define ElfW(type)   Elf64_##type 
  # else 
+@@ -565,8 +565,8 @@
+  *
+  */
+ int
+-unexec (Extbyte *new_name, Extbyte *old_name, unsigned int UNUSED (data_start),
+-	unsigned int UNUSED (bss_start), unsigned int UNUSED (entry_address))
++unexec (Extbyte *new_name, Extbyte *old_name, uintptr_t UNUSED (data_start),
++	uintptr_t UNUSED (bss_start), uintptr_t UNUSED (entry_address))
+ {
+   int new_file, old_file, new_file_size;
+ 
