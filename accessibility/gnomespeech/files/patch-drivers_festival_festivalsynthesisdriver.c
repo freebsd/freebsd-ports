aa20bbcdbb8145f2f5293ca408d42bfb741ebf74
marcus <marcus@FreeBSD.org>
2005-09-30 03:22:34 +0000

    Update to 0.3.8.

--- accessibility/gnomespeech/files/patch-drivers_festival_festivalsynthesisdriver.c
+++ accessibility/gnomespeech/files/patch-drivers_festival_festivalsynthesisdriver.c
@@ -1,21 +1,10 @@
---- drivers/festival/festivalsynthesisdriver.c.orig	Tue Jan 11 14:48:51 2005
-+++ drivers/festival/festivalsynthesisdriver.c	Tue Jan 11 14:50:22 2005
-@@ -33,6 +33,8 @@
+--- drivers/festival/festivalsynthesisdriver.c.orig	Thu Sep 29 23:19:28 2005
++++ drivers/festival/festivalsynthesisdriver.c	Thu Sep 29 23:19:41 2005
+@@ -32,6 +32,7 @@
+ #include <unistd.h>
  #include <stdlib.h>
+ #include <sys/socket.h>
++#include <netinet/in.h>
  #include <libbonobo.h>
  #include <netdb.h>
-+#include <sys/socket.h>
-+#include <netinet/in.h>
  #include "festivalsynthesisdriver.h"
- #include "festivalspeaker.h"
- 
-@@ -591,8 +593,8 @@
- static void
- voice_list_free (GSList *l)
- {
--	g_assert (l);
- 	GSList *tmp = l;
-+	g_assert (l);
- 	
- 	while (tmp)
- 	{
