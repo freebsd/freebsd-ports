23bbd8ccd7f79f2af691d56ac1dcbbae8419d390
pav <pav@FreeBSD.org>
2005-10-03 22:25:17 +0000

    - Fix runtime crash [1] - Update WWW - Style
    PR:		ports/86365 [1]
    Submitted by:	Fred Wheeler <fred.s.wheeler@gmail.com>

--- audio/vsound/files/patch-vsound.c
+++ audio/vsound/files/patch-vsound.c
@@ -1,6 +1,6 @@
---- vsound.c.orig	Tue Apr 13 16:52:15 2004
-+++ vsound.c	Tue Apr 13 17:01:30 2004
-@@ -70,11 +70,11 @@
+--- vsound.c.orig	Sat Jul 10 23:29:33 2004
++++ vsound.c	Tue Oct  4 00:20:25 2005
+@@ -73,11 +73,11 @@
  /*------------------------------------------------------------------------------
  ** Macros to handle big/little endian issues.
  */
@@ -14,3 +14,12 @@
  	#if (__BYTE_ORDER == __LITTLE_ENDIAN)
  		#define	CPU_IS_LITTLE_ENDIAN		1
  		#define	CPU_IS_BIG_ENDIAN			0
+@@ -210,7 +210,7 @@
+ 	dsp_init () ;
+ 
+ 	va_start (args, flags) ;
+-	mode = va_arg (args, mode_t) ;
++	mode = va_arg (args, int) ;
+ 	va_end (args) ;
+ 
+ 	if (strcmp (pathname, "/dev/dsp"))
