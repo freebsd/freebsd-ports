2b2741c31b195e85bc69c5f73df00805025ce4b6
Oliver Lehmann <oliver@FreeBSD.org>
2005-10-04 05:27:15 +0000

    fix build for 4

---
+++ devel/mercator/files/patch-Merecator-iround.h
@@ -0,0 +1,19 @@
+--- Mercator/iround.h.orig	Tue Oct  4 01:25:06 2005
++++ Mercator/iround.h	Tue Oct  4 01:31:36 2005
+@@ -9,6 +9,16 @@
+ #error iround.h must be included after config.h
+ #endif
+ 
++#if defined(__FreeBSD__)
++#include <osreldate.h>
++#if __FreeBSD_version < 500000
++extern "C" {
++extern float fabsf __P((float));
++extern float rintf __P((float));
++}
++#endif
++#endif
++
+ #ifdef HAVE_LRINTF
+     #define I_ROUND(x) (::lrintf(x)) 
+ #elif defined(HAVE_RINTF)
