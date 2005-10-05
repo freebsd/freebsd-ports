60a2232a024e0f9df1dce32c3889007580df231d
Bill Fenner <fenner@FreeBSD.org>
2005-10-05 16:44:35 +0000

    MASTER_SITE_SOURCEFORGE:  Remove aleraon, unc and voxel.  They are missing about 30%   of the files we're trying to fetch, and prdownloads.sf.net   doesn't currently offer them as mirrors.  Remove mesh, even though prdownloads.sf.net currently offers   it as a mirror.  It is missing about 30% of the files we're   trying to fetch.  Add umn and jaist, sites that prdownloads.sf.net currently   offers that have most of the files that we're trying to fetch.  Don't add internap; even though prdownloads.sf.net currently   offers it, it's missing about 30% of the files we're trying   to fetch.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -957,7 +957,7 @@ MASTER_SITE_SAVANNAH+= \
 .endif
 
 .if !defined(IGNORE_MASTER_SITE_SOURCEFORGE)
-.for mirror in heanet aleron nchc voxel optusnet kent unc ufpr surfnet mesh
+.for mirror in heanet nchc optusnet kent ufpr surfnet jaist umn
 MASTER_SITE_SOURCEFORGE+= \
 	http://${mirror}.dl.sourceforge.net/sourceforge/%SUBDIR%/
 .endfor
