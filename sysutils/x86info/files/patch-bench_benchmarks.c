ffba9de11af0db8c5f775ee245df4871e0953068
Emanuel Haupt <ehaupt@FreeBSD.org>
2005-10-05 14:40:25 +0000

    Update sysutils/x86info to 1.16
    PR:		86912
    Submitted by:	Erik Greenwald <erik@smluc.org> (maintainer)
    
    Approved by:	novel (mentor)

--- sysutils/x86info/files/patch-bench_benchmarks.c
+++ sysutils/x86info/files/patch-bench_benchmarks.c
@@ -1,21 +1,17 @@
---- bench/benchmarks.c.orig	Sun Aug 24 11:04:51 2003
-+++ bench/benchmarks.c	Sun Aug 24 11:08:26 2003
-@@ -1,7 +1,8 @@
+--- bench/benchmarks.c.orig	Tue Oct  4 19:22:20 2005
++++ bench/benchmarks.c	Tue Oct  4 19:22:40 2005
+@@ -1,6 +1,5 @@
  #include <time.h>
  #include <sys/time.h>
 -#include <asm/unistd.h>
-+#include <sys/unistd.h>
  #include <sys/stat.h>
-+#include <sys/syscall.h>
  #include <stdio.h>
  #include "../x86info.h"
- #include "bench.h"
-@@ -13,7 +14,7 @@
+@@ -13,7 +12,6 @@
  	if (show_bench != 1)
  		return;
  
 -	TIME(asm volatile("int $0x80" :"=a" (ret) :"0" (__NR_getppid)), "int 0x80");
-+	TIME(asm volatile("int $0x80" :"=a" (ret) :"0" (SYS_getppid)), "int 0x80");
  	TIME(asm volatile("cpuid": : :"ax", "dx", "cx", "bx"), "cpuid");
  
  	TIME(asm volatile("addl $1,0(%esp)"), "addl");
--- sysutils/x86info/files/patch-x86info.h
+++ sysutils/x86info/files/patch-x86info.h
@@ -1,22 +1,11 @@
---- x86info.h.orig	Sun Aug 24 10:59:27 2003
-+++ x86info.h	Sun Aug 24 11:00:47 2003
-@@ -1,15 +1,10 @@
+--- x86info.h.orig	Tue Oct  4 19:13:58 2005
++++ x86info.h	Tue Oct  4 19:14:04 2005
+@@ -1,7 +1,7 @@
  #ifndef _X86INFO_H
  #define _X86INFO_H
  
--#ifdef linux
 -#include <linux/types.h>
--typedef unsigned char u8;
--typedef unsigned short u16;
--typedef unsigned int u32;
--#else
--#include <machine/types.h>
--#define __u32 int
--#endif /* linux */
 +#include <sys/types.h>
-+typedef u_int8_t u8;
-+typedef u_int16_t u16;
-+typedef u_int32_t u32;
- 
- #define VENDOR_AMD	1
- #define VENDOR_CENTAUR 2
+ typedef unsigned char u8;
+ typedef unsigned short u16;
+ typedef unsigned int u32;
