46c9cedbaaba14bfbede04dab21b7d0e612e25ff
Edwin Groothuis <edwin@FreeBSD.org>
2005-10-05 02:19:42 +0000

    Upgrade to version 1.1.7 No version bumps neither for other ports since nobody else is using this one.

---
+++ devel/kxl/files/patch-src__KXL.h
@@ -0,0 +1,10 @@
+--- src/KXL.h.orig	Wed Oct  5 12:14:12 2005
++++ src/KXL.h	Wed Oct  5 12:14:18 2005
+@@ -4,7 +4,6 @@
+ #include <stdio.h>
+ #include <X11/Xlib.h>
+ #include <X11/Xutil.h>
+-#include <linux/joystick.h>
+ 
+ 
+ //================================================================
