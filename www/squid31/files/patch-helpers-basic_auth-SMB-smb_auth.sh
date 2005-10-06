c5799da9bebaee9b7dca61fb8578fd4b6d0a46ad
Marcus Alves Grando <mnag@FreeBSD.org>
2005-10-06 18:47:55 +0000

    Fix smb_auth helper
    PR:		86850
    Pointed by:	Dean M. Phillips <dmphilli@gmail.com>
    Submitted by:	Thomas-Martin Seck <tmseck@netcologne.de> (maintainer)

--- www/squid31/files/patch-helpers-basic_auth-SMB-smb_auth.sh
+++ www/squid31/files/patch-helpers-basic_auth-SMB-smb_auth.sh
@@ -6,7 +6,7 @@
   # along with this program; if not, write to the Free Software
   # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
   
-+ SAMBAPREFIX=${SAMBAPREFIX:-/usr/local/bin}
++ SAMBAPREFIX=${SAMBAPREFIX:-%%LOCALBASE%%}
 + 
   read DOMAINNAME
   read PASSTHROUGH
