600c447d1ee77366da70ae38f9bab0b80bf4b8ff
pav <pav@FreeBSD.org>
2005-10-08 21:32:20 +0000

    - Fix handling of & sign, it need to be escaped for XML storage
    PR:		ports/85240
    Submitted by:	Maurice Castro <maurice@sphinx.clari.net.au>
    Approved by:	maintainer timeout (anders, 17 days)
    
    - While here, fix runtime crash on 64bit platforms by explicitly declaring
      getenv() (via including <stdlib.h> header)

---
+++ security/pwman/files/patch-src-pwman.h
@@ -0,0 +1,10 @@
+--- src/pwman.h.orig	Mon Oct 14 18:10:36 2002
++++ src/pwman.h	Sat Oct  8 23:21:23 2005
+@@ -26,6 +26,7 @@
+ #include <string.h>
+ #include <config.h>
+ #include <time.h>
++#include <stdlib.h>
+ #include <stdarg.h>
+ 
+ #define CONF_FILE 	".pwmanrc" 
