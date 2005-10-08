d366fd7b79e4dcf1b934cbb8cb139dcc1c311d77
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2005-10-08 14:09:03 +0000

    Update to 0.1.10.

--- multimedia/libdvdnav/files/patch-src:vm.c
+++ multimedia/libdvdnav/files/patch-src:vm.c
@@ -1,5 +1,5 @@
---- src/vm.c.orig	Fri Feb 20 00:49:57 2004
-+++ src/vm.c	Fri Feb 20 00:54:31 2004
+--- src/vm/vm.c.orig	Fri Feb 20 00:49:57 2004
++++ src/vm/vm.c	Fri Feb 20 00:54:31 2004
 @@ -47,6 +47,10 @@
  #define lseek64 lseek
  #endif /* _MSC_VER */
