6e058c4a085f4d0fdaeb8615f4ca0ad2d21b85de
mnag <mnag@FreeBSD.org>
2005-10-09 00:17:17 +0000

    Soya 3D is a very high level 3D engine for Python. Soya aims at being to 3D what Python is to programming : fast to learn, easy to use, while keeping good performances
    PR:		87008
    Submitted by:	Jose Alonso Cardenas Marquez <acardenas@bsd.org.pe>

---
+++ graphics/py-soya3d/files/patch-_ode.c
@@ -0,0 +1,13 @@
+--- _ode.c.old	Wed Oct  5 02:50:09 2005
++++ _ode.c	Wed Oct  5 02:50:18 2005
+@@ -10,8 +10,8 @@
+ #include "math.h"
+ #include "GL/glew.h"
+ #include "ode/ode.h"
+-#include "SDL/SDL_endian.h"
+-#include "SDL/SDL.h"
++#include "SDL11/SDL_endian.h"
++#include "SDL11/SDL.h"
+ #include "chunk.h"
+ #include "matrix.h"
+ #include "cal3d_wrapper.h"
---
+++ graphics/py-soya3d/files/patch-_soya.c
@@ -0,0 +1,13 @@
+--- _soya.c.old	Wed Oct  5 00:23:48 2005
++++ _soya.c	Wed Oct  5 00:24:03 2005
+@@ -6,8 +6,8 @@
+   #define PY_LONG_LONG LONG_LONG
+ #endif
+ #include "GL/glew.h"
+-#include "SDL/SDL_endian.h"
+-#include "SDL/SDL.h"
++#include "SDL11/SDL_endian.h"
++#include "SDL11/SDL.h"
+ #include "chunk.h"
+ #include "matrix.h"
+ #include "cal3d_wrapper.h"
