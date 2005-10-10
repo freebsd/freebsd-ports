296570727573428bf290ffda367063a070c79f60
perky <perky@FreeBSD.org>
2005-10-10 14:44:10 +0000

    Update to 4.3.3

--- databases/py-bsddb3/files/patch-setup.py
+++ databases/py-bsddb3/files/patch-setup.py
@@ -1,17 +1,17 @@
---- setup.py.orig	Mon Feb  3 11:28:52 2003
-+++ setup.py	Mon Mar  3 16:22:31 2003
-@@ -73,8 +73,8 @@
+--- setup.py.orig	Fri Jun  3 16:27:21 2005
++++ setup.py	Mon Oct 10 22:43:16 2005
+@@ -197,8 +197,8 @@
          incdir = os.path.join(BERKELEYDB_DIR, 'include')
      if not libdir:
          libdir = os.path.join(BERKELEYDB_DIR, 'lib')
 -    if not '-ldb' in LIBS:
--        libname = ['db']
+-        libname = [dblib]
 +    if not '-l%%DBVERSION%%' in LIBS:
 +        libname = ['%%DBVERSION%%']
      else:
+         if debug: print "LIBS already contains '-ldb' not adding our own", "'-l"+dblib+"'"
          libname = []
-     utils = []
-@@ -96,7 +96,7 @@
+@@ -221,7 +221,7 @@
      if not status and results.find('libdb.') >= 0:
          static = 1
  
