7fb89ec5292d59abc2816c53394aa861d947d6b8
lioux <lioux@FreeBSD.org>
2005-10-10 06:18:31 +0000

    o Update to Freenet Stable build 5105 released 20050930 snapshot o Bump PORTREVISION since build version did not change o Add a rc.d startup script [1]
    PR:		86023 [1]
    Requested by:	Evgeniy <dushistov@gmail.com> [1]

---
+++ net-p2p/freenet05/files/freenet.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+# Under a BSDL license. Copyright 2005. Mario S F Ferreira <lioux@FreeBSD.org>
+
+# PROVIDE: freenet
+# REQUIRE: NETWORKING
+# KEYWORD: FreeBSD shutdown
+
+#
+# Add the following lines to /etc/rc.conf to enable freenet:
+#
+#freenet_enable="YES"
+#freenet_user=""
+#
+
+. /etc/rc.subr
+
+name="freenet"
+rcvar=`set_rcvar`
+command="%%PREFIX%%/bin/${name}"
+extra_commands="configure"
+
+freenet_check_vars()
+{
+  if [ -z "${freenet_user}" ]; then
+    freenet_user=$(whoami)
+  fi
+
+  if [ "x${freenet_user}" = "xroot" ]; then
+    err 1 "You have to set freenet_user to a non-root user for security reasons"
+  fi
+}
+
+configure_cmd="configure_cmd"
+configure_cmd()
+{
+  freenet_check_vars
+  su -l ${freenet_user} -c "${command} -c"
+}
+
+start_cmd="start_cmd"
+start_cmd()
+{
+  freenet_check_vars
+  su -l ${freenet_user} -c "${command} -i"
+}
+
+stop_cmd="stop_cmd"
+stop_cmd()
+{
+  freenet_check_vars
+  su -l ${freenet_user} -c "${command} -o"
+}
+
+status_cmd="status_cmd"
+status_cmd()
+{
+  echo "Can't check ${name} status"
+}
+
+load_rc_config "${name}"
+: ${freenet_enable="NO"}
+: ${freenet_user=""}
+
+run_rc_command "$1"
