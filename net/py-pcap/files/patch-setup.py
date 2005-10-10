6f57b6443bb22e561278defd7387724658e4a4c7
Hye-Shik Chang <perky@FreeBSD.org>
2005-10-10 14:46:26 +0000

    Update to 0.5

--- net/py-pcap/files/patch-setup.py
+++ net/py-pcap/files/patch-setup.py
@@ -1,11 +1,23 @@
---- setup.py.orig	Mon Dec 20 01:50:10 2004
-+++ setup.py	Mon Dec 20 01:50:19 2004
-@@ -21,7 +21,7 @@
- #    self.run_command('make_clean')
- 
- class pcap_build_ext(build_ext):
+--- setup.py.orig	Mon Jul  4 08:14:31 2005
++++ setup.py	Mon Oct 10 23:40:45 2005
+@@ -75,7 +75,7 @@
+               new_sources.append(source)
+       return new_sources
+     
 -  def swig_sources(self, sources):
 +  def swig_sources(self, sources, ext=None):
    
        """Walk the list of source files in 'sources', looking for SWIG
        interface (.i) files.  Run SWIG on all that are found, and
+@@ -121,11 +121,6 @@
+           self.announce("swigging %s to %s" % (source, target))
+           self.spawn(swig_cmd + ["-o", target, source])
+ 
+-          self.announce('doc-ifying swig-generated source file %s' % target)
+-          self.spawn(['./build-tools/docify.py', target])
+-
+-          self.announce('doc-ifying swig-generated shadow class file %s' % 'pcap.py')
+-          self.spawn(['./build-tools/docify-shadow.py', 'pcap.py'])
+   
+       return new_sources
+ 
