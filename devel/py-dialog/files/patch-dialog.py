4383013be9263450eb6f81ae8a392730e5fdfdd3
Maxim Sobolev <sobomax@FreeBSD.org>
2005-10-11 20:55:44 +0000

    Add missed yes_label and no_label modifiers. Bump PORTREVISION.

--- devel/py-dialog/files/patch-dialog.py
+++ devel/py-dialog/files/patch-dialog.py
@@ -3,16 +3,17 @@ $FreeBSD$
 
 --- dialog.py.orig
 +++ dialog.py
-@@ -214,6 +214,8 @@
+@@ -214,6 +214,9 @@
      "create_rc": lambda file: ("--create-rc", file),
      "defaultno": lambda enable: _simple_option("--defaultno", enable),
      "default_item": lambda string: ("--default-item", string),
-+    "exit_label": lambda string: ("--exit-label", string),
++    "yes_label": lambda string: ("--yes-label", string),
++    "no_label": lambda string: ("--no-label", string),
 +    "extra_button": lambda enable: _simple_option("--extra-button", enable),
      "help": lambda enable: _simple_option("--help", enable),
      "help_button": lambda enable: _simple_option("--help-button", enable),
      "help_label": lambda string: ("--help-label", string),
-@@ -524,7 +526,7 @@
+@@ -524,7 +527,7 @@
  
      """
  
