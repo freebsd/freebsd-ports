24dbb421ad1c47a27eecd7e42a98bbb0bcedb42c
edwin <edwin@FreeBSD.org>
2005-10-14 10:05:58 +0000

    Fix build of astro/mymoon

--- astro/mymoon/files/patch-mymoon.c
+++ astro/mymoon/files/patch-mymoon.c
@@ -1,23 +1,36 @@
---- mymoon.c.orig	Fri Jul 18 09:25:58 2003
-+++ mymoon.c	Fri Jul 18 09:26:35 2003
-@@ -122,14 +122,12 @@
- getmaxyx(stdscr,row,cols);
+--- mymoon.c.orig	Wed Jul 16 21:13:55 2003
++++ mymoon.c	Fri Oct 14 19:55:31 2005
+@@ -1,4 +1,4 @@
+-#include <libnova.h>
++#include <libnova/libnova.h>
+ #include <curses.h>
+ #include <time.h>
+ #include <stdlib.h>
+@@ -59,7 +59,7 @@
+ memset(&date3, 0, sizeof(date3));
  
- mvprintw(row/4,0,
--"
--        Moon's situation at: %d/%d/%d %d:%d:%d
--        Age: +- %d days
--        Rise: %d/%d/%d %d:%d:%d
--        Set: %d/%d/%d %d:%d:%d
--        Illuminated: %.4f%%
--        Moon->Earth distance: %.3f Km\n\n
--",
-+        "Moon's situation at: %d/%d/%d %d:%d:%d\n"
-+        "Age: +- %d days\n"
-+        "Rise: %d/%d/%d %d:%d:%d\n"
-+        "Set: %d/%d/%d %d:%d:%d\n"
-+        "Illuminated: %.4f%%\n"
-+        "Moon->Earth distance: %.3f Km\n\n",
- date3.days,date3.months,date3.years,date3.hours,date3.minutes,(int)date3.seconds,(int)age,
- date1.days,date1.months,date1.years,date1.hours,date1.minutes,(int)date1.seconds,
- date2.days,date2.months,date2.years,date2.hours,date2.minutes,(int)date2.seconds,light,dist);
+ while (1) {
+-julian=get_julian_from_sys();
++julian=ln_get_julian_from_sys();
+ age=moonphase();
+ 
+ 
+@@ -67,12 +67,12 @@
+ observer.lng=latitude;
+ observer.lat=longitude;
+ } else {observer.lng=45.12;observer.lat=8.42;}
+-light=100*get_lunar_disk(julian);
+-dist=get_lunar_earth_dist(julian);
+-get_lunar_rst(julian,&observer,&rst);
+-get_date(rst.rise,&date1);
+-get_date(rst.set,&date2);
+-get_date(julian,&date3);
++light=100*ln_get_lunar_disk(julian);
++dist=ln_get_lunar_earth_dist(julian);
++ln_get_lunar_rst(julian,&observer,&rst);
++ln_get_date(rst.rise,&date1);
++ln_get_date(rst.set,&date2);
++ln_get_date(julian,&date3);
+ getmaxyx(stdscr,row,cols);
+ grapher(dist,row,date1,date2,date3,age,light);
+ napms(update * 1000);
