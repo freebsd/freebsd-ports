0ed9c293b7a8ee7487b24a77c487bf218528c4c8
Marcus Alves Grando <mnag@FreeBSD.org>
2005-10-15 23:39:50 +0000

    Plazma is an xmms visualization plugin, it was coded with the SDL library.
    It runs on Linux, FreeBSD and of the majority of Unix.
    
    It displays effects of rotation of grids, wave radial, oscilloscope and
    spectrum analyzer in agreement with the music.
    
    WWW: http://membres.lycos.fr/diablolight/
    
    PR:		87490
    Submitted by:	Alejandro Pulver <alejandro@varnet.biz>

---
+++ graphics/xmms-plazma/files/patch-plazma.c
@@ -0,0 +1,13 @@
+--- plazma.c.orig	Thu Oct 25 14:58:03 2001
++++ plazma.c	Sat Oct 15 15:11:05 2005
+@@ -9,8 +9,8 @@
+ 
+ #include <gtk/gtk.h>
+ #include <math.h>
+-#include <SDL/SDL.h>
+-#include <SDL/SDL_thread.h>
++#include <SDL.h>
++#include <SDL_thread.h>
+ #include <xmms/plugin.h>
+ #include <xmms/util.h>
+ #include <stdio.h>
