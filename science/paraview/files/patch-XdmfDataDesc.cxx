65d488f4b3f0e90dfe5abc3307d39698c5f05934
Eric Anholt <anholt@FreeBSD.org>
2005-10-16 03:44:52 +0000

    Update to 2.2.1 and start respecting CFLAGS.  Note that this update does not fix the libtiff vulnerability.

--- science/paraview/files/patch-XdmfDataDesc.cxx
+++ science/paraview/files/patch-XdmfDataDesc.cxx
@@ -1,15 +1,6 @@
---- Utilities/Xdmf/libsrc/XdmfDataDesc.cxx.orig	Tue Jun  7 16:06:38 2005
-+++ Utilities/Xdmf/libsrc/XdmfDataDesc.cxx	Tue Jun  7 16:06:40 2005
-@@ -313,7 +313,7 @@
- 
- XdmfInt32
- XdmfDataDesc::SelectCoordinates(  XdmfInt64 NumberOfElements, XdmfInt64 *Coordinates ){
--hssize_t  *HCoordinates;
-+hsize_t  *HCoordinates;
- XdmfInt32  status;
- XdmfInt64  i, rank = this->Rank;
- hssize_t  Length = NumberOfElements * rank;
-@@ -326,14 +326,14 @@
+--- Utilities/Xdmf/libsrc/XdmfDataDesc.cxx.orig	Sat Oct 15 13:58:21 2005
++++ Utilities/Xdmf/libsrc/XdmfDataDesc.cxx	Sat Oct 15 13:58:36 2005
+@@ -334,7 +334,7 @@
  
  this->SelectionType = XDMF_COORDINATES;
  XdmfDebug(" Selecting " << (int)NElements << " elements" );
@@ -18,11 +9,3 @@
  for( i = 0 ; i < Length ; i++ ){
    HCoordinates[i] = Coordinates[i];
    }
- status = H5Sselect_elements( this->DataSpace,
-         H5S_SELECT_SET,
-          NElements,
--         ( const hssize_t **)HCoordinates);
-+         ( const hsize_t **)HCoordinates);
- if( status < 0 ) {
-   return( XDMF_FAIL );
- }
