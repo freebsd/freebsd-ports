960abb9bd58e38f046a88011bec4b7e28d921f0c
trevor <trevor@FreeBSD.org>
2005-10-17 06:17:44 +0000

    Update to 1.95 beta (obsoletes PRs 49056, 51900 and 58653).

--- audio/festival/files/patch-speech_tools_voxware.cc
+++ audio/festival/files/patch-speech_tools_voxware.cc
@@ -1,8 +1,6 @@
-$FreeBSD$
-
---- speech_tools/audio/voxware.cc.orig	Tue Sep 28 16:38:22 1999
-+++ speech_tools/audio/voxware.cc	Wed Oct 29 08:06:38 2003
-@@ -61,7 +61,7 @@
+--- speech_tools/audio/voxware.cc.orig	Fri Apr 30 16:56:49 2004
++++ speech_tools/audio/voxware.cc	Sun Oct 16 12:43:12 2005
+@@ -61,11 +61,12 @@
  #include "EST_error.h"
  
  #ifdef SUPPORT_FREEBSD16
@@ -11,3 +9,8 @@ $FreeBSD$
  #include <fcntl.h>
  int freebsd16_supported = TRUE;
  int linux16_supported = FALSE;
+ static char *aud_sys_name = "FreeBSD";
++static int stereo_only = 0;
+ #endif
+ 
+ #ifdef SUPPORT_LINUX16
