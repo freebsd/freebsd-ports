f33cd063bc7acd0ff3033f34e43521d69e6a3255
Vanilla I. Shu <vanilla@FreeBSD.org>
2005-10-18 09:38:53 +0000

    Add 2 patches for new ecore.
    Submitted by:	Laurent Debacker <debackerl@gmail com>

---
+++ graphics/esmart/files/patch-src_lib_esmart_trans_x11_Esmart_Trans_X11.h
@@ -0,0 +1,12 @@
+--- src/lib/esmart_trans_x11/Esmart_Trans_X11.h.orig	Wed Nov 10 23:18:11 2004
++++ src/lib/esmart_trans_x11/Esmart_Trans_X11.h	Tue Oct 18 17:30:01 2005
+@@ -1,7 +1,9 @@
+ #ifndef _ESMART_TRANS_X11_H
+ #define _ESMART_TRANS_X11_H
++#include <stdio.h>
+ #include <Evas.h>
+ #include <Ecore_X.h>
++#include <Ecore_Data.h>
+ 
+ #ifdef __cplusplus 
+ extern "C" {
