5f0de834c3f75dc2198a1242c88c03716faf77fe
Edwin Groothuis <edwin@FreeBSD.org>
2005-10-18 03:18:54 +0000

    Well, believe it or not, but there is another game development SDK:
        flatzebra - A generic game engine for 2D double-buffering animation
    
        WWW: http://www3.sympatico.ca/sarrazip/dev/
    
    Needed for the upgrade of games/afternoonstalker

---
+++ devel/flatzebra/files/patch-src__flatzebra__SoundMixer.h
@@ -0,0 +1,11 @@
+--- src/flatzebra/SoundMixer.h.orig	Tue Oct 18 12:13:19 2005
++++ src/flatzebra/SoundMixer.h	Tue Oct 18 12:13:24 2005
+@@ -23,7 +23,7 @@
+ #ifndef _H_SoundMixer
+ #define _H_SoundMixer
+ 
+-#include <SDL/SDL_mixer.h>
++#include <SDL11/SDL_mixer.h>
+ 
+ #include <string>
+ 
