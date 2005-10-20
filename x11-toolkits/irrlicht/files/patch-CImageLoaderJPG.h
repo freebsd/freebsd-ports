f3827aab25cd38819678cdf696bd564b4e8db56e
Simon Barner <barner@FreeBSD.org>
2005-10-20 00:29:37 +0000

    - Update to 0.12.0 [1] - Explicitely link against libz, libpng, libjpeg - Remove @'s in do-install: target, so one sees what's going on
    Reminded by:	Edwin's version nagger [1]

--- x11-toolkits/irrlicht/files/patch-CImageLoaderJPG.h
+++ x11-toolkits/irrlicht/files/patch-CImageLoaderJPG.h
@@ -1,13 +1,18 @@
---- CImageLoaderJPG.h.orig	Sun Sep 19 00:48:11 2004
-+++ CImageLoaderJPG.h	Sun Sep 19 00:53:05 2004
-@@ -12,8 +12,8 @@
+--- CImageLoaderJPG.h.orig	Wed Oct 19 17:24:14 2005
++++ CImageLoaderJPG.h	Wed Oct 19 17:26:32 2005
+@@ -12,12 +12,13 @@
  #include "IrrCompileConfig.h"
  #ifdef _IRR_COMPILE_WITH_LIBJPEG_
  extern "C" {
--#include "jpeglib/jconfig.h"
--#include "jpeglib/jpeglib.h"
-+#include "jconfig.h"
-+#include "jpeglib.h"
+-	#include "jpeglib/jconfig.h"
+ 	#ifndef _IRR_USE_NON_SYSTEM_JPEG_LIB_
++	#include <jconfig.h>
+ 	#include <jpeglib.h> // use system lib
+ 	#else  // _IRR_USE_NON_SYSTEM_JPEG_LIB_
++	#include "jpeglib/jconfig.h"
+ 	#include "jpeglib/jpeglib.h" // use irrlicht jpeglib
+-#endif // _IRR_USE_NON_SYSTEM_JPEG_LIB_
++	#endif // _IRR_USE_NON_SYSTEM_JPEG_LIB_
  }
- #endif
+ #endif // _IRR_COMPILE_WITH_LIBJPEG_
  
