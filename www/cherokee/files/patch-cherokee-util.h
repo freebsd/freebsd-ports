fb61f3d3c348b1624dbb5a8d5cf151288a839cff
pav <pav@FreeBSD.org>
2005-10-21 19:49:10 +0000

    - Update to 0.4.27
    PR:		ports/87744
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- www/cherokee/files/patch-cherokee-util.h
+++ www/cherokee/files/patch-cherokee-util.h
@@ -1,27 +1,17 @@
---- cherokee/util.h.orig	Tue Sep 20 01:44:20 2005
-+++ cherokee/util.h	Tue Sep 20 11:56:57 2005
-@@ -30,13 +30,21 @@
- #define CHEROKEE_UTIL_H
+--- cherokee/util.h.orig	Sat Oct 15 19:31:05 2005
++++ cherokee/util.h	Thu Oct 20 00:59:40 2005
+@@ -31,6 +31,14 @@
  
  #include <cherokee/common.h>
-+#include <time.h>
-+#include <dirent.h>
+ 
++#ifdef HAVE_SYS_TYPES_H
 +#include <sys/types.h>
++#endif
 +
 +#ifdef HAVE_SYS_SOCKET_H
 +# include <sys/socket.h>
 +#endif
 +
-+#ifdef HAVE_ARPA_INET_H
-+# include <arpa/inet.h>
-+#endif
- 
  #ifdef HAVE_NETINET_IN_H
  # include <netinet/in.h>
  #endif
--
--#include <time.h>
--#include <dirent.h>
- 
- #include <cherokee/buffer.h>
- 
