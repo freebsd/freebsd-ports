b8b928f9a9c0edc18718ca3fa60456ee730302ed
naddy <naddy@FreeBSD.org>
2005-10-22 20:19:49 +0000

    Update to 1.3.
    Obtained from:	Steven Mestdagh <steven.mestdagh@esat.kuleuven.be> (OpenBSD port)

---
+++ games/xsoldier/files/patch-enemyshot.c
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- enemyshot.c.orig
++++ enemyshot.c
+@@ -11,6 +11,7 @@
+ #include <X11/xpm.h>
+ */
+ 
++#include <stdlib.h>
+ #include "image.h"
+ #include "xsoldier.h"
+ #include "manage.h"
