4e8711aa04a6316209d982cef13012acc6f17582
Thomas Quinot <thomas@FreeBSD.org>
2005-10-22 21:32:48 +0000

    Update to upstream release 1.9.2.3.
    Reviewed by:	roberto

--- net/proxy-suite/files/patch-com-socket.c
+++ net/proxy-suite/files/patch-com-socket.c
@@ -1,15 +1,5 @@
 --- common/com-socket.c.dist	Tue Mar 30 10:31:32 2004
 +++ common/com-socket.c	Tue Jun 14 09:26:44 2005
-@@ -84,6 +84,9 @@
- #if defined(HAVE_STROPTS_H)
- #   include <stropts.h>
- #endif
-+#if defined(HAVE_SYS_PARAM_H)
-+#   include <sys/param.h>
-+#endif
- #if defined(HAVE_SYS_CONF_H)
- #   include <sys/conf.h>
- #endif
 @@ -2132,9 +2135,9 @@
  	/*
  	** BSD ipnat table lookup
