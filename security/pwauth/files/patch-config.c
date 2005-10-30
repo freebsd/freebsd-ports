7eeae06ea41ccdb94e7223252a8b77579cba08f5
Clement Laforet <clement@FreeBSD.org>
2005-10-30 12:01:22 +0000

    - Update to 2.3.1 - Avoid patching Makefile [1]
    Submitted by:		vs [1]

--- security/pwauth/files/patch-config.c
+++ security/pwauth/files/patch-config.c
@@ -1,34 +1,45 @@
---- config.h.orig	Thu Oct 14 10:18:19 2004
-+++ config.h	Thu Oct 14 10:17:07 2004
-@@ -93,11 +93,11 @@
+--- config.h.orig	Wed Sep 29 01:07:18 2004
++++ config.h	Sun Oct 30 13:49:52 2005
+@@ -106,13 +106,13 @@
  
  /* #define SHADOW_NONE		/**/
- /* #define SHADOW_BSD		/**/
--#define SHADOW_SUN		/**/
-+/* #define SHADOW_SUN		/**/
+ /* #define SHADOW_BSD		/* FreeBSD, NetBSD, OpenBSD, BSDI */
+-#define SHADOW_SUN		/* Linux, Solaris, IRIX */
++/* #define SHADOW_SUN		/* Linux, Solaris, IRIX */
  /* #define SHADOW_JFH		/**/
  /* #define SHADOW_MDW		/**/
- /* #define SHADOW_AIX		/**/
--/* #define PAM			/**/
-+#define PAM			/**/
- /* #define PAM_SOLARIS_26	/**/
+ /* #define SHADOW_AIX		/* AIX */
+ /* #define SHADOW_HPUX		/* HPUX ? */
+ 
+-/* #define PAM			/* Linux PAM or OpenPAM*/
++   #define PAM			/* Linux PAM or OpenPAM*/
+ /* #define PAM_SOLARIS		/* PAM on Solaris other than 2.6 */
+ /* #define PAM_SOLARIS_26	/* PAM on Solaris 2.6 */
+ /* #define LOGIN_CONF_OPENBSD	/**/
+@@ -193,7 +193,7 @@
+  */
+ 
+ #define UNIX_LASTLOG		/**/
+-#define HAVE_LASTLOG_H		/**/
++//#define HAVE_LASTLOG_H		/**/
  
  
-@@ -145,7 +145,7 @@
+ /* If NOLOGIN_FILE is defined to the full path name of a file, then the
+@@ -238,7 +238,7 @@
   * last).
   */
  
--#define SERVER_UIDS 99		/* user "nobody" */
-+#define SERVER_UIDS %%UIDS%%	/* user "nobody" */
+-#define SERVER_UIDS 72		/* user "nobody" */
++#define SERVER_UIDS %%UIDS%%	/* user "www" */
  
  
  /* If MIN_UNIX_UID is defined to an integer, logins with uid numbers less than
-@@ -156,7 +156,7 @@
+@@ -250,7 +250,7 @@
   * given value will be accepted).
   */
  
 -#define MIN_UNIX_UID 500	/**/
-+#define MIN_UNIX_UID %%MINUID%%	/**/
++#define MIN_UNIX_UID 1000	/**/
  
  
- /* On failed authentications, pwauth will sleep for SLEEP_TIME seconds, using
+ /* If IGNORE_CASE is defined, the login given is checked in two different
