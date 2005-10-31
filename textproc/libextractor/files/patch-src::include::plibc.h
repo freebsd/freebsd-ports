e1c00bcd2e263a870ff5d4efd365eac81d4e9ab9
kevlo <kevlo@FreeBSD.org>
2005-10-31 02:07:46 +0000

    Update to 0.5.6a
    PR: ports/88074
    Submitted by: Vasil Dimov <vd@datamax.bg>

--- textproc/libextractor/files/patch-src::include::plibc.h
+++ textproc/libextractor/files/patch-src::include::plibc.h
@@ -1,34 +1,6 @@
---- src/include/plibc.h.orig	Tue Oct  4 23:37:40 2005
-+++ src/include/plibc.h	Tue Oct  4 23:41:22 2005
-@@ -451,20 +451,20 @@
-  #define STRERROR(i) strerror(i)
-  #define READLINK(p, b, s) readlink(p, b, s)
-  #define LSTAT(p, b) lstat(p, b)
-- #define PRINTF(f, ...) printf(f , __VA_ARGS__)
-- #define FPRINTF(fil, fmt, ...) fprintf(fil, fmt, __VA_ARGS__)
-+ #define PRINTF(f, ARGS...) printf(f , ##ARGS)
-+ #define FPRINTF(fil, fmt, ARGS...) fprintf(fil, fmt, ##ARGS)
-  #define VPRINTF(f, a) vprintf(f, a)
-  #define VFPRINTF(s, f, a) vfprintf(s, f, a)
-  #define VSPRINTF(d, f, a) vsprintf(d, f, a)
-  #define VSNPRINTF(str, size, fmt, a) vsnprintf(str, size, fmt, a)
-- #define _REAL_SNPRINTF(str, size, fmt, ...) snprintf(str, size, fmt, __VA_ARGS__)
-- #define SPRINTF(d, f, ...) sprintf(d, f, __VA_ARGS__)
-+ #define _REAL_SNPRINTF(str, size, fmt, ARGS...) snprintf(str, size, fmt, ##ARGS)
-+ #define SPRINTF(d, f, ARGS...) sprintf(d, f, ##ARGS)
-  #define VSSCANF(s, f, a) vsscanf(s, f, a)
-- #define SSCANF(s, f, ...) sscanf(s, f, __VA_ARGS__)
-+ #define SSCANF(s, f, ARGS...) sscanf(s, f, ##ARGS)
-  #define VFSCANF(s, f, a) vfscanf(s, f, a)
-  #define VSCANF(f, a) vscanf(f, a)
-- #define SCANF(f, ...) scanf(f, __VA_ARGS__)
-- #define FSCANF(s, f, ...) fscanf(s, f, __VA_ARGS__)
-+ #define SCANF(f, ARGS...) scanf(f, ##ARGS)
-+ #define FSCANF(s, f, ARGS...) fscanf(s, f, ##ARGS)
-  #define ACCEPT(s, a, l) accept(s, a, l)
-  #define BIND(s, n, l) bind(s, n, l)
-  #define CONNECT(s, n, l) connect(s, n, l)
-@@ -515,20 +515,20 @@
+--- src/include/plibc.h.orig	Wed Oct 26 18:55:22 2005
++++ src/include/plibc.h	Wed Oct 26 19:04:33 2005
+@@ -506,20 +506,20 @@
   #define STRERROR(i) _win_strerror(i)
   #define READLINK(p, b, s) _win_readlink(p, b, s)
   #define LSTAT(p, b) _win_lstat(p, b)
@@ -53,6 +25,6 @@
 - #define FSCANF(s, f, ...) _win_fscanf(s, f, __VA_ARGS__)
 + #define SCANF(f, ARGS...) _win_scanf(f, ##ARGS)
 + #define FSCANF(s, f, ARGS...) _win_fscanf(s, f, ##ARGS)
+  #define WAITPID(p, s, o) _win_waitpid(p, s, o)
   #define ACCEPT(s, a, l) _win_accept(s, a, l)
   #define BIND(s, n, l) _win_bind(s, n, l)
-  #define CONNECT(s, n, l) _win_connect(s, n, l)
