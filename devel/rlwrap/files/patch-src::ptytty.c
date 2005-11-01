b1aa05c46f1d48c46fbdeaa2288befd1837dda27
Kirill Ponomarev <krion@FreeBSD.org>
2005-11-01 10:03:23 +0000

    Update to 0.24
    PR:		ports/88300
    Submitted by:	maintainer

--- devel/rlwrap/files/patch-src::ptytty.c
+++ devel/rlwrap/files/patch-src::ptytty.c
@@ -1,11 +1,11 @@
---- src/ptytty.c.orig	Thu Jan 22 18:16:16 2004
-+++ src/ptytty.c	Mon Feb  2 15:12:59 2004
+--- src/ptytty.c.orig	Mon Oct 31 16:01:11 2005
++++ src/ptytty.c	Mon Oct 31 16:01:57 2005
 @@ -41,7 +41,7 @@
  #ifdef HAVE_SYS_IOCTL_H
  # include <sys/ioctl.h>
  #endif
 -#if defined(PTYS_ARE_PTMX) && !defined(__CYGWIN32__)
 +#if defined(PTYS_ARE_PTMX) && !defined(__CYGWIN32__) && !defined(__FreeBSD__)
- # include <sys/stropts.h>      /* for I_PUSH */
+ # include <sys/stropts.h>	/* for I_PUSH */
  #endif
  
