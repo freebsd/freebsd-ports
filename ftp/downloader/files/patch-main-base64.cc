af07a7c34525e1d85e8073a8c974e6ec3d2c557a
Marcus Alves Grando <mnag@FreeBSD.org>
2005-11-01 20:17:30 +0000

    Update to 2.5.6
    PR:		88343
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- ftp/downloader/files/patch-main-base64.cc
+++ ftp/downloader/files/patch-main-base64.cc
@@ -1,9 +1,10 @@
---- main/base64.cc.orig	Fri Sep 16 13:32:16 2005
-+++ main/base64.cc	Fri Sep 16 23:03:23 2005
-@@ -13,6 +13,7 @@
+--- main/base64.cc.orig	Sun Oct 30 22:02:27 2005
++++ main/base64.cc	Mon Oct 31 04:14:40 2005
+@@ -13,7 +13,7 @@
  #include "base64.h"
  #include <string.h>
  #include <stdio.h>
+-#include <stdint.h>
 +#include <inttypes.h>
  
  char Table64[64]={  'A','B','C','D','E','F','G','H',
