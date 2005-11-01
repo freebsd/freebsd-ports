793940018d27697ae3be661bf7aca3ce463a2ec5
pav <pav@FreeBSD.org>
2005-11-01 20:18:18 +0000

    - Update to 1.9.2

--- games/bygfoot/files/patch-src-league.c
+++ games/bygfoot/files/patch-src-league.c
@@ -1,17 +1,17 @@
---- src/league.c.orig	Sat Sep 10 11:30:49 2005
-+++ src/league.c	Sat Oct  8 00:09:34 2005
-@@ -423,6 +423,7 @@
+--- src/league.c.orig	Sun Oct 16 11:53:36 2005
++++ src/league.c	Tue Nov  1 20:26:35 2005
+@@ -457,6 +457,7 @@
      {
  	dest_sids = misc_separate_strings(
- 	    g_array_index(elements, PromRelElement, i).dest_sid->str);
+ 	    g_array_index(elements, PromRelElement, i).dest_sid);
 +	{
  	gint dest_idcs_int[dest_sids->len];
  	gint dest_idcs_order[dest_sids->len];
  
-@@ -447,6 +448,7 @@
+@@ -481,6 +482,7 @@
  	}
  
- 	free_g_string_array(&dest_sids);
+ 	free_gchar_array(&dest_sids);
 +	}
      }
  }
--- games/bygfoot/files/patch-src-lg_commentary.c
+++ games/bygfoot/files/patch-src-lg_commentary.c
@@ -1,6 +1,6 @@
---- src/lg_commentary.c.orig	Sat Sep 10 11:30:49 2005
-+++ src/lg_commentary.c	Sat Oct  8 00:06:21 2005
-@@ -48,6 +48,7 @@
+--- src/lg_commentary.c.orig	Sun Oct 16 11:53:36 2005
++++ src/lg_commentary.c	Tue Nov  1 19:34:50 2005
+@@ -49,6 +49,7 @@
      else
  	commentaries = &lg_commentary[unit->event.type];
  
@@ -8,15 +8,15 @@
      gint order[(*commentaries)->len];
      lg_commentary_get_order(*commentaries, order);
  
-@@ -89,6 +90,7 @@
- 	    g_free(token_rep[i]);
- 	    token_rep[i] = NULL;
- 	}
+@@ -87,6 +88,7 @@
+ 	   i != option_int("string_lg_commentary_token_team_layer0", &lg_tokens) &&
+ 	   i != option_int("string_lg_commentary_token_team_layer1", &lg_tokens))
+ 	    free_gchar_ptr(token_rep[i]);
 +    }
  }
  
  /** Check whether the commentary conditions are fulfilled and whether
-@@ -310,6 +312,7 @@
+@@ -305,6 +307,7 @@
  	order[i] = -1;
      }
  
@@ -24,7 +24,7 @@
      gint permutation[priority_sum];
  
      math_generate_permutation(permutation, 1, priority_sum);
-@@ -330,6 +333,7 @@
+@@ -325,6 +328,7 @@
  
  		break;
  	    }
