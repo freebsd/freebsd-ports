8a150f3bbc0b2c7b4babdef43e72564f0acaa536
edwin <edwin@FreeBSD.org>
2005-11-02 05:25:44 +0000

    update audio/gramofile to 1.6P+debian patches
    	update to 1.6P (with perl interface), add debian patches,
    	MASTER_SITE moved
    
    PR:		ports/88199
    Submitted by:	Juergen Lock <nox@jelal.kn-bremen.de>

---
+++ audio/gramofile/files/post-patch-reclp_main.c
@@ -0,0 +1,10 @@
+Index: reclp_main.c
+@@ -18,7 +18,7 @@
+ #include <curses.h>
+ #endif
+ 
+-#define BRECCMD "/usr/lib/gramofile/brec_gramo"
++#define BRECCMD "brec_gramo"
+ 
+ void
+ record_from_lp (char *startdir)
