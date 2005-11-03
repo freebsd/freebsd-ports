4577bfeee448e5c7a70bbc268e90af1cfe8d5450
Vsevolod Stakhov <vsevolod@FreeBSD.org>
2005-11-03 13:43:00 +0000

    - Fix for the exilog.sh rc script to make "status" and "restart" command work correctly
    PR:		87972
    Submitted by:	Alex Samorukov <samm@os2.kiev.ua>

--- mail/exilog/files/exilog.sh
+++ mail/exilog/files/exilog.sh
@@ -5,6 +5,8 @@
 # Add the following lines to /etc/rc.conf to enable exilog agent:
 #
 #exilog_enable="YES"
+#
+# also uncomment  'use_pretty_names' => 'no' in exilog.conf file
 
 . %%RC_SUBR%%
 
@@ -14,17 +16,10 @@ rcvar=`set_rcvar`
 command=%%PREFIX%%/sbin/exilog_agent.pl
 pidfile=/var/run/exilog.pid
 required_files=%%PREFIX%%/etc/exilog.conf
+command_interpreter=/usr/bin/perl
 
 # read settings, set default values
 load_rc_config $name
 : ${exilog_enable="NO"}
 
-case $1 in
-	stop)
-		kill `cat $pidfile`
-		rm -f $pidfile
-	;;
-	*)
-		run_rc_command "$1"
-	;;
-esac
+run_rc_command "$1"
