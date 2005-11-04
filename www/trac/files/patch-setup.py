742becd0dfb819a10a20e123d4506aaec9fcee0b
Renato Botelho <garga@FreeBSD.org>
2005-11-04 12:30:10 +0000

    - Update to 0.9 - Pass maintainership to submitter
    PR:		ports/88453
    Submitted by:	Rui Lopes <rgl@ruilopes.com>
    Approved by:	maintainer

--- www/trac/files/patch-setup.py
+++ www/trac/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Tue Mar  1 23:02:01 2005
-+++ setup.py	Tue Mar  1 23:02:40 2005
-@@ -202,7 +202,7 @@
-       data_files=[(_p('share/trac/templates'), glob('templates/*')),
+--- setup.py.orig	Thu Nov  3 11:44:28 2005
++++ setup.py	Thu Nov  3 11:45:01 2005
+@@ -225,7 +225,7 @@
                    (_p('share/trac/htdocs'), glob(_p('htdocs/*.*')) + [_p('htdocs/README')]),
                    (_p('share/trac/htdocs/css'), glob(_p('htdocs/css/*'))),
+                   (_p('share/trac/htdocs/js'), glob(_p('htdocs/js/*'))),
 -                  (_p('share/man/man1'), glob(_p('scripts/*.1'))),
 +                  (_p('man/man1'), glob(_p('scripts/*.1'))),
-                   (_p('share/trac/wiki-default'), glob(_p('wiki-default/[A-Z]*')))],
+                   (_p('share/trac/wiki-default'), glob(_p('wiki-default/[A-Z]*'))),
+                   (_p('share/trac/wiki-macros'), glob(_p('wiki-macros/*.py')))],
        scripts=[_p('scripts/trac-admin'),
-                _p('scripts/tracd'),
