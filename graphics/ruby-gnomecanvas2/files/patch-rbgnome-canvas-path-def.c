fe817611db6a2c0766533b593f6c69244d0dbf49
Jeremy Messenger <mezz@FreeBSD.org>
2005-11-05 05:35:10 +0000

    - Update to 0.14.0, see release note for details:
    	http://ruby-gnome2.sourceforge.jp/hiki.cgi?News_20051016_1
    
    - Two new ports have been added, ruby-cairo and ruby-gtkmozembed.
    - databases/ruby-libgda has been mark as BROKEN, because it still needs to
      catch up w/ new libgda-2.0, so remove it from ruby-gnome2-all.

--- graphics/ruby-gnomecanvas2/files/patch-rbgnome-canvas-path-def.c
+++ graphics/ruby-gnomecanvas2/files/patch-rbgnome-canvas-path-def.c
@@ -1,12 +1,11 @@
---- src/rbgnome-canvas-path-def.c.orig	Sun Jul 31 12:39:54 2005
-+++ src/rbgnome-canvas-path-def.c	Fri Sep 16 21:16:00 2005
-@@ -82,8 +82,9 @@
+--- src/rbgnome-canvas-path-def.c.orig	Sun Oct 16 20:51:48 2005
++++ src/rbgnome-canvas-path-def.c	Sun Oct 16 20:53:08 2005
+@@ -72,7 +72,8 @@
      if (NIL_P(obj)) {
          path = gnome_canvas_path_def_new();
      } else if (TYPE(obj) == T_FIXNUM) {
-+	gint length;
-         path = gnome_canvas_path_def_new_sized(NUM2INT(obj));
 -        gint length = NUM2INT(obj);
++        gint length;
 +        length = NUM2INT(obj);
          if (length > 0) {
              path = gnome_canvas_path_def_new_sized(length);
