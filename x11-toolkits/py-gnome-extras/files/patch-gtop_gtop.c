658c3a1f53f61ceb23f1c62d355011fadeadee3e
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-11-05 06:26:18 +0000

    Update to 2.12.0.

--- x11-toolkits/py-gnome-extras/files/patch-gtop_gtop.c
+++ x11-toolkits/py-gnome-extras/files/patch-gtop_gtop.c
@@ -1,6 +1,6 @@
---- gtop/gtop.c.orig	Fri Jan 28 14:09:00 2005
-+++ gtop/gtop.c	Fri Jan 28 14:09:18 2005
-@@ -43,6 +43,7 @@
+--- gtop/gtop.c.orig	Wed May 18 00:54:59 2005
++++ gtop/gtop.c	Wed May 18 00:54:12 2005
+@@ -44,6 +44,7 @@
  
  #include <sys/types.h>
  #include <sys/socket.h>
@@ -8,3 +8,13 @@
  #include <arpa/inet.h>
  
  
+@@ -1316,8 +1317,8 @@ build_siglist(void)
+ 
+ 	for(i = 0; i < len; ++i)
+ 	{
+-		p = &glibtop_sys_siglist[i];
+ 		PyObject* d;
++		p = &glibtop_sys_siglist[i];
+ 
+ 		d = PyDict_New();
+ 		my_dict_add_and_decref(d, "number", PyI_L(p->number));
