c72f84013cb9e585cbaee067fda0302346a92a3a
Michael Nottebrock <lofi@FreeBSD.org>
2005-11-05 07:48:01 +0000

    Update gpgme to 1.0.3 and chase shared library version bump.

---
+++ security/gpgme/files/patch-gpgme-ath-pthread-compat.c
@@ -0,0 +1,11 @@
+--- gpgme/ath-pth-compat.c.orig	Tue Nov  1 10:25:41 2005
++++ gpgme/ath-pth-compat.c	Tue Nov  1 10:25:55 2005
+@@ -20,7 +20,7 @@
+ 
+ #include <stdlib.h>
+ #include <errno.h>
+-#include <pth.h>
++#include <pth/pth.h>
+ 
+ #include "ath.h"
+ 
