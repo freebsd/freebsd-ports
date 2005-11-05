08e42535493cb844771474e9802231f24b050039
kwm <kwm@FreeBSD.org>
2005-11-05 12:16:51 +0000

    Revert 2.2.2 -> 2.2.1 downgrade which was included in the gnome 2.12 update. Bump portrevision to chase glib/gtk update.

--- deskutils/gnotime/files/patch-src::main.c
+++ deskutils/gnotime/files/patch-src::main.c
@@ -1,6 +1,6 @@
---- src/main.c.orig	Sun May 23 16:08:47 2004
-+++ src/main.c	Thu Jul  8 00:25:04 2004
-@@ -299,6 +299,7 @@
+--- src/main.c.orig	Fri Sep  2 01:02:57 2005
++++ src/main.c	Fri Oct 21 11:49:42 2005
+@@ -301,6 +301,7 @@
  	char * xml_filepath;
  	gboolean read_is_ok;
  	char *errmsg, *qmsg;
@@ -8,7 +8,7 @@
  
  	xml_filepath = resolve_old_path (config_data_url);
  
-@@ -347,7 +348,6 @@
+@@ -349,7 +350,6 @@
  			_("Do you want to continue?"),
  			NULL);
  
@@ -16,14 +16,14 @@
  	mb = gtk_message_dialog_new (NULL,
  	         GTK_DIALOG_MODAL,
  	         GTK_MESSAGE_ERROR,
-@@ -398,13 +398,13 @@
+@@ -400,13 +400,13 @@
  	{
  		const char *fp;
  		char *errmsg, *qmsg;
 +		GtkWidget *mb;
  		fp = gtt_get_config_filepath();
  		errmsg = gtt_err_to_string (conf_errcode, fp);
- 		qmsg = g_strconcat (errmsg, 
+ 		qmsg = g_strconcat (errmsg,
  			_("Shall I setup a new configuration?"),
  			NULL);
  
@@ -31,8 +31,8 @@
  		mb = gtk_message_dialog_new (NULL,
  		         GTK_DIALOG_MODAL,
  		         GTK_MESSAGE_ERROR,
-@@ -719,11 +719,7 @@
- int 
+@@ -721,11 +721,7 @@
+ int
  main(int argc, char *argv[])
  {
 -#if defined (HAVE_DECL_WNOHANG) || defined (HAVE_DECL_SA_NOCLDWAIT)
@@ -44,7 +44,7 @@
  	static char *geometry_string = NULL;
  	static const struct poptOption geo_options[] =
  	{
-@@ -734,6 +730,11 @@
+@@ -736,6 +732,11 @@
  		{NULL, '\0', 0, NULL, 0}
  	};
  
@@ -53,10 +53,10 @@
 +	memset(&reapchildren, 0, sizeof reapchildren);
 +#endif /*  WNOHANG/SA_NOCLDWAIT */
 +
- 	gnome_program_init(PACKAGE, VERSION, LIBGNOMEUI_MODULE, argc, argv, 
- 		                   GNOME_PARAM_POPT_TABLE, geo_options, 
+ 	gnome_program_init(PACKAGE, VERSION, LIBGNOMEUI_MODULE, argc, argv,
+ 		                   GNOME_PARAM_POPT_TABLE, geo_options,
  		                   GNOME_PROGRAM_STANDARD_PROPERTIES, NULL);
-@@ -745,7 +746,7 @@
+@@ -747,7 +748,7 @@
  	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
  	textdomain(GETTEXT_PACKAGE);
  
