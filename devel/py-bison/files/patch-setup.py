52b440f519cc7facb541bab5a11bad0c551f992d
arved <arved@FreeBSD.org>
2005-11-05 17:24:29 +0000

    Fix build on CURRENT

--- devel/py-bison/files/patch-setup.py
+++ devel/py-bison/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	Sat Apr 24 10:05:41 2004
-+++ setup.py	Tue Nov  2 11:26:45 2004
-@@ -24,6 +24,21 @@
+--- setup.py.orig	Sat Apr 24 04:05:41 2004
++++ setup.py	Sat Nov  5 18:22:47 2005
+@@ -24,6 +24,26 @@
      extra_link_args = []
      bison2pyscript = 'utils/bison2py'
      bisondynlibModule = "src/c/bisondynlib-linux.c"
@@ -15,6 +15,11 @@
 +    bison2pyscript = 'utils/bison2py'
 +    bisondynlibModule = "src/c/bisondynlib-linux.c"
 +elif sys.platform == 'freebsd6':
++    libs = []
++    extra_link_args = []
++    bison2pyscript = 'utils/bison2py'
++    bisondynlibModule = "src/c/bisondynlib-linux.c"
++elif sys.platform == 'freebsd7':
 +    libs = []
 +    extra_link_args = []
 +    bison2pyscript = 'utils/bison2py'
