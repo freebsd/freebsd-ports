debf196a65e3e4f9b5339fbddf079932f21328bc
Michael Nottebrock <lofi@FreeBSD.org>
2005-11-05 15:55:24 +0000

    Remove KDE2/QT1 cruft.
    Submitted by: arved

--- Mk/bsd.kde.mk
+++ Mk/bsd.kde.mk
@@ -13,19 +13,13 @@ KDE_MAINTAINER=		will@FreeBSD.org
 # XXX: Write HAVE_ definitions sometime.
 
 # USE_QT_VER		- Says that the port uses the Qt toolkit.  Possible values:
-#					  1 or 3; each specify the major version of Qt to use.
+#					  3; each specify the major version of Qt to use.
 # USE_KDELIBS_VER	- Says that the port uses KDE libraries.  Possible values:
 #					  3 specifies the major version of KDE to use.
 #					  This implies USE_QT of the appropriate version.
 # USE_KDEBASE_VER	- Says that the port uses the KDE base.  Possible values:
 #					  3 specifies the major version of KDE to use.
 #					  This implies USE_KDELIBS of the appropriate version.
-
-#
-# WARNING!  ACHTUNG!  DANGER WILL ROBINSON!
-# DO NOT USE USE_QT_VER=1 UNLESS YOU WILL NOT BE NEEDING ANY ASSISTANCE
-# WHATSOEVER FROM THE MAINTAINER OF THIS FILE!
-#
 
 # tagged MASTER_SITE_KDE_kde
 kmaster=				${MASTER_SITE_KDE:S@%/@%/:kde@g}
@@ -45,9 +39,6 @@ USE_KDELIBS_VER=CVS
 # kdebase 3.x common stuff
 LIB_DEPENDS+=	kfontinst:${PORTSDIR}/x11/kdebase3
 USE_KDELIBS_VER=3
-.else
-# kdebase 2.x common stuff -- DEFAULT
-USE_KDELIBS_VER=2
 .endif # ${USE_KDEBASE_VER} == 3
 .endif # defined(USE_KDEBASE_VER)
 
@@ -84,18 +75,7 @@ BROKEN=			"Unknown value in USE_KDELIBS_VER"
 # End of USE_KDELIBS_VER section
 
 # USE_QT_VER section
-# Qt 1.x common stuff
-.if ${USE_QT_VER} == 1
-LIB_DEPENDS+=  qt1.3:${PORTSDIR}/x11-toolkits/qt145
-MOC?=                  ${X11BASE}/bin/moc1
-.if defined(PREFIX)
-QTDIR=                 ${PREFIX}
-.else
-QTDIR=                 ${X11BASE}
-.endif # defined(PREFIX)
-CONFIGURE_ENV+=        MOC="${MOC}" QTDIR="${QTDIR}"
-
-.elif ${USE_QT_VER} == CVS
+.if ${USE_QT_VER} == CVS
 
 KDE_CVS_PREFIX?=	${LOCALBASE}/kde-cvs
 QT_CVS_PREFIX?=		${X11BASE}/qt-cvs
