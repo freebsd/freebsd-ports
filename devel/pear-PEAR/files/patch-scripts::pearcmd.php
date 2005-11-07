7e5e17d7bbfa9cf14f69c7715358ccfe26f26ded
thierry <thierry@FreeBSD.org>
2005-11-07 18:01:35 +0000

    Upgrade to 1.4.4 and fix a security problem.
    Changelog at <http://pear.php.net/package/PEAR/download/All>.
    
    PR:		88572
    Approved by:	maintainer
    Security:	VuXML 44e5f5bd-4d76-11da-bf37-000fb586ba73

--- devel/pear-PEAR/files/patch-scripts::pearcmd.php
+++ devel/pear-PEAR/files/patch-scripts::pearcmd.php
@@ -1,20 +1,19 @@
---- scripts/pearcmd.php.orig	Wed Oct 27 02:58:21 2004
-+++ scripts/pearcmd.php	Thu Dec 16 17:30:19 2004
+--- scripts/pearcmd.php.orig	Sun Nov  6 21:17:11 2005
++++ scripts/pearcmd.php	Sun Nov  6 21:19:20 2005
 @@ -1,3 +1,4 @@
 +#!@php_bin@ -nq
  <?php
  //
  // +----------------------------------------------------------------------+
-@@ -24,8 +25,11 @@
+@@ -29,8 +30,10 @@
  /**
   * @nodep Gtk
   */
 +dl('pcre.so');
 +dl('xml.so');
-+
  if ('@include_path@' != '@'.'include_path'.'@') {
 -    ini_set('include_path', '@include_path@');
 +    ini_set('include_path', '@include_path@:@include_path@/bootstrap');
- }
- ini_set('allow_url_fopen', true);
- if (!ini_get('safe_mode')) {
+     $raw = false;
+ } else {
+     // this is a raw, uninstalled pear, either a cvs checkout, or php distro
