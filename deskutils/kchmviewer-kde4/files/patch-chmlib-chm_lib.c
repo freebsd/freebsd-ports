9f093e7a155b4d1a4b0011bf7d02d0f295783604
markus <markus@FreeBSD.org>
2005-11-08 17:15:14 +0000

    Update to version 1.1

--- deskutils/kchmviewer-kde4/files/patch-chmlib-chm_lib.c
+++ deskutils/kchmviewer-kde4/files/patch-chmlib-chm_lib.c
@@ -1,11 +1,11 @@
---- chmlib-0.35/chm_lib.c.orig	Thu Jun 30 19:24:09 2005
-+++ chmlib-0.35/chm_lib.c	Tue Aug  9 20:22:47 2005
-@@ -160,7 +160,7 @@
- /* Sparc        */
- /* MIPS         */
- /* PPC          */
--#elif __i386__ || __sun || __sgi || __ppc__ || __x86_64__
-+#elif __i386__ || __sun || __sgi || __ppc__ || __x86_64__ || __ia64__ || __alpha__ || __sparc64__
+--- chmlib/chm_lib.c.orig	Tue Nov  8 16:18:06 2005
++++ chmlib/chm_lib.c	Tue Nov  8 16:20:05 2005
+@@ -171,7 +171,7 @@
+ 
+ /* x86-64 */
+ /* Note that these may be appropriate for other 64-bit machines. */
+-#elif __x86_64__
++#elif __x86_64__ || __ia64__ || __alpha__ || __sparc64__
  typedef unsigned char           UChar;
  typedef short                   Int16;
  typedef unsigned short          UInt16;
