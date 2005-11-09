972562de16199704c111680e78e428a3f7a14c99
Pav Lucistnik <pav@FreeBSD.org>
2005-11-09 20:40:59 +0000

    This is a new port of bazaar, a C implementation of GNU Arch (based on tla) from Canonical. The difference between this and bazaar-ng is that this is the "mainline" version of bazaar, beeing faster and more stable, while bazaar-ng is the somwhat future platform. I thought it would be nice to have this in ports as well, this is a very good implemenation that is easy to use, much easier than TLA.
    PR:		ports/88476
    Submitted by:	Ulf Lilleengen <lulf@kerneled.org>

---
+++ devel/baz/files/patch-pfs-signatures.c
@@ -0,0 +1,11 @@
+--- src/baz/libarch/pfs-signatures.c.old	Wed Nov  2 21:46:56 2005
++++ src/baz/libarch/pfs-signatures.c	Wed Nov  2 21:47:09 2005
+@@ -29,7 +29,7 @@
+ #include "libarch/namespace.h"
+ #include "libarch/archives.h"
+ #include "libarch/cached-archive.h"
+-#include <gpgme.h>
++#include <gpgme/gpgme.h>
+ #include "libarch/pfs-signatures.h"
+ 
+ 
