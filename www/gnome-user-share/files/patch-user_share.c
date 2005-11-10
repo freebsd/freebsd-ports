f1879535139a1d05322fb4c326800b76c68fc450
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-11-10 18:42:07 +0000

    Update to 0.7.

--- www/gnome-user-share/files/patch-user_share.c
+++ www/gnome-user-share/files/patch-user_share.c
@@ -1,7 +1,7 @@
---- user_share.c.orig	Sat Dec  4 04:34:24 2004
-+++ user_share.c	Sat Dec  4 04:35:05 2004
-@@ -103,8 +103,8 @@
- 	    gpointer     callback_data)
+--- user_share.c.orig	Tue Nov  8 10:41:50 2005
++++ user_share.c	Thu Nov 10 13:38:07 2005
+@@ -252,8 +252,8 @@ howl_input (GIOChannel  *io_channel,
+ 			gpointer     callback_data)
  {
  	sw_discovery session;
 -	session = callback_data;
@@ -10,3 +10,11 @@
  
  	if (sw_discovery_salt (session, &salt) == SW_OKAY) {
  		sw_salt_lock (salt);
+@@ -301,7 +301,6 @@ publish_service (int port)
+ 								   /* TODO: should be u=guest */
+ 								   /* text */ (unsigned char *) "", 0,
+ 								   publish_reply, NULL, &published_id);
+-    g_free (share_name);
+     if (result != SW_OKAY) {
+ 		return FALSE;
+     }
