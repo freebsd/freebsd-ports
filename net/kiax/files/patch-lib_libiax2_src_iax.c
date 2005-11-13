de3cf7cd5008ad18c8d1e7cfd71d20608ee89294
Sam Lawrance <lawrance@FreeBSD.org>
2005-11-13 13:39:01 +0000

    Another build fix, this time for 5.x.
    Submitted by:	maintainer

--- net/kiax/files/patch-lib_libiax2_src_iax.c
+++ net/kiax/files/patch-lib_libiax2_src_iax.c
@@ -1,6 +1,6 @@
---- lib/libiax2/src/iax.c.orig	Sat Nov 12 17:29:58 2005
-+++ lib/libiax2/src/iax.c	Sat Nov 12 17:31:06 2005
-@@ -53,9 +53,11 @@
+--- lib/libiax2/src/iax.c.orig	Sun Nov 13 13:59:36 2005
++++ lib/libiax2/src/iax.c	Sun Nov 13 14:21:46 2005
+@@ -53,11 +53,13 @@
  #include <time.h>
  
  #ifndef MACOSX
@@ -8,7 +8,24 @@
  #include <malloc.h>
  #ifndef SOLARIS
  #include <error.h>
+ #endif
+ #endif
++#endif
+ 
+ #endif
+ 
+@@ -85,10 +87,14 @@
+ #ifdef MACOSX
+ #define IAX_SOCKOPTS MSG_DONTWAIT
+ #else
++#ifdef __FreeBSD__
++#define IAX_SOCKOPTS MSG_DONTWAIT
++#else
+ #ifdef SOLARIS
+ #define IAX_SOCKOPTS MSG_DONTWAIT
+ #else  /* Linux and others */
+ #define IAX_SOCKOPTS MSG_DONTWAIT | MSG_NOSIGNAL
 +#endif
  #endif
  #endif
- 
+ #endif
