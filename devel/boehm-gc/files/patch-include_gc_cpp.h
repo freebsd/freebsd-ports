bf119152698348778ee5a2a15eda8721114f4036
nobutaka <nobutaka@FreeBSD.org>
2005-11-13 13:28:25 +0000

    Remove redundant gc:: from gc_cpp.h for gcc41.
    Submitted by:	"Arno J. Klaassen" <arno@heho.snv.jussieu.fr>

--- devel/boehm-gc/files/patch-include_gc_cpp.h
+++ devel/boehm-gc/files/patch-include_gc_cpp.h
@@ -1,6 +1,15 @@
---- include/gc_cpp.h.orig	Thu Oct 14 12:19:56 2004
-+++ include/gc_cpp.h	Thu Oct 14 12:20:15 2004
-@@ -276,7 +276,7 @@
+--- include/gc_cpp.h.orig	Fri Oct 22 07:47:45 2004
++++ include/gc_cpp.h	Sun Nov 13 22:19:07 2005
+@@ -180,7 +180,7 @@
+     inline void* operator new[]( size_t size, void *p );
+     inline void operator delete[]( void* obj );
+ #   ifdef GC_PLACEMENT_DELETE
+-      inline void gc::operator delete[]( void*, void* );
++      inline void operator delete[]( void*, void* );
+ #   endif
+ #endif /* GC_OPERATOR_NEW_ARRAY */
+     };    
+@@ -281,7 +281,7 @@
      else
          return GC_MALLOC_UNCOLLECTABLE( size );}
  
@@ -9,7 +18,7 @@
      return p;}
  
  inline void gc::operator delete( void* obj ) {
-@@ -294,7 +294,7 @@
+@@ -299,7 +299,7 @@
  inline void* gc::operator new[]( size_t size, GCPlacement gcp ) {
      return gc::operator new( size, gcp );}
  
