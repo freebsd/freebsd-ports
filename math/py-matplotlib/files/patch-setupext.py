11ba587b4bd23119ed381fd22f0575a04df7ea9a
Tilman Keskinoz <arved@FreeBSD.org>
2005-11-13 15:35:04 +0000

    Fix build on FreeBSD7

--- math/py-matplotlib/files/patch-setupext.py
+++ math/py-matplotlib/files/patch-setupext.py
@@ -1,6 +1,18 @@
---- setupext.py.orig	Tue Apr 12 16:09:32 2005
-+++ setupext.py	Tue Apr 12 16:10:28 2005
-@@ -308,7 +308,7 @@
+--- setupext.py.orig	Tue Mar 29 19:25:37 2005
++++ setupext.py	Sat Nov  5 20:26:49 2005
+@@ -38,7 +38,8 @@
+     'darwin' : ['/usr/local', '/usr', '/sw', '/usr/X11R6'],
+     'freebsd4' : ['/usr/local', '/usr'],
+     'freebsd5' : ['/usr/local', '/usr'],
+-    'freebsd6' : ['/usr/local', '/usr'],    
++    'freebsd6' : ['/usr/local', '/usr'],
++    'freebsd7' : ['/usr/local', '/usr'],
+     'sunos5' : [os.getenv('MPLIB_BASE') or '/usr/local',],
+ }
+ 
+@@ -306,9 +307,9 @@
+ 
+     # you're still here? ok we'll try it this way
      o = find_tcltk() # todo: try/except
 -    module.include_dirs.extend([o.tcl_inc, o.tk_inc])
 +    module.include_dirs.extend(["/usr/local/include/tcl8.4", "/usr/local/include/tk8.4"])
