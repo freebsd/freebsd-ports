67b8cfeeef1992497579b2585c64a9ce0154f979
Sam Lawrance <lawrance@FreeBSD.org>
2005-11-14 05:17:09 +0000

    Update to 2005-11-12 snapshot.
    PR:		ports/88952
    Submitted by:	Juergen Lock <nox@jelal.kn-bremen.de> (maintainer)

--- emulators/qemu/files/patch-vl.c
+++ emulators/qemu/files/patch-vl.c
@@ -1,4 +1,5 @@
-Index: qemu/vl.c
+--- vl.c.orig	Mon Nov 14 15:55:56 2005
++++ vl.c	Mon Nov 14 15:57:25 2005
 @@ -40,6 +40,10 @@
  #include <sys/socket.h>
  #include <netinet/in.h>
@@ -10,7 +11,7 @@ Index: qemu/vl.c
  #ifdef _BSD
  #include <sys/stat.h>
  #ifndef __APPLE__
-@@ -1280,7 +1284,7 @@
+@@ -1363,7 +1367,7 @@
      return chr;
  }
  
@@ -19,3 +20,19 @@ Index: qemu/vl.c
  CharDriverState *qemu_chr_open_pty(void)
  {
      char slave_name[1024];
+@@ -1509,6 +1513,7 @@
+     return chr;
+ }
+ 
++#if defined(__linux__)
+ static int pp_ioctl(CharDriverState *chr, int cmd, void *arg)
+ {
+     int fd = (int)chr->opaque;
+@@ -1571,6 +1576,7 @@
+     chr->chr_ioctl = pp_ioctl;
+     return chr;
+ }
++#endif
+ 
+ #else
+ CharDriverState *qemu_chr_open_pty(void)
