666c1c9ec1518bf6b0a4ea49998d8678f077c87d
az <az@FreeBSD.org>
2005-11-15 10:22:58 +0000

    - Add net/hping-devel This is the devel branch of hping: it's the same, but with a TCL scripting feature.
    PR:		ports/88552
    Submitted by:	Nicolas Jombart

---
+++ net/hping-devel/files/patch-bytesex.h
@@ -0,0 +1,18 @@
+
+$FreeBSD: /tmp/pcvs/ports/net/hping-devel/files/patch-bytesex.h,v 1.1 2005-11-15 10:22:58 az Exp $
+
+--- bytesex.h.orig	Tue Sep 14 15:22:45 2004
++++ bytesex.h	Tue Sep 14 15:21:50 2004
+@@ -8,10 +8,12 @@
+ #define ARS_BYTESEX_H
+ 
+ #if 	defined(__i386__) \
++	|| defined(__ia64__) \
+ 	|| defined(__alpha__) \
+ 	|| (defined(__mips__) && (defined(MIPSEL) || defined (__MIPSEL__)))
+ #define BYTE_ORDER_LITTLE_ENDIAN
+ #elif 	defined(__mc68000__) \
++	|| defined (__amd64__) \
+ 	|| defined (__sparc__) \
+ 	|| defined (__sparc) \
+ 	|| defined (__PPC__) \
