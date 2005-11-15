081b17449bd91bcac5390cd0d8a63ed3914df51d
edwin <edwin@FreeBSD.org>
2005-11-15 06:46:57 +0000

    Remove full path in patch

--- net-im/kmess/files/patch-kmess_network_msnconnection.h
+++ net-im/kmess/files/patch-kmess_network_msnconnection.h
@@ -1,5 +1,5 @@
---- /work/a/ports/net-im/kmess/work/kmess-1.4.2/kmess/network/msnconnection.h.orig	Tue Nov 15 04:03:10 2005
-+++ /work/a/ports/net-im/kmess/work/kmess-1.4.2/kmess/network/msnconnection.h	Tue Nov 15 04:04:04 2005
+--- kmess/network/msnconnection.h.orig	Tue Nov 15 04:03:10 2005
++++ kmess/network/msnconnection.h	Tue Nov 15 04:04:04 2005
 @@ -29,6 +29,7 @@
  
  #include <stdlib.h>
