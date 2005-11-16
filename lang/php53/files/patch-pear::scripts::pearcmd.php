2b1dc0524614c6e4aa6ad94e8c9dad452e06e853
ale <ale@FreeBSD.org>
2005-11-16 08:33:09 +0000

    - Fix pear pkg-plist - Increase pear memory limit (requested by amd64)

--- lang/php53/files/patch-pear::scripts::pearcmd.php
+++ lang/php53/files/patch-pear::scripts::pearcmd.php
@@ -1,11 +1,11 @@
---- pear/scripts/pearcmd.php.orig	Sun Jun  6 17:52:41 2004
-+++ pear/scripts/pearcmd.php	Mon Jul 12 16:22:50 2004
+--- pear/scripts/pearcmd.php.orig	Mon Mar 28 18:46:06 2005
++++ pear/scripts/pearcmd.php	Wed Nov 16 09:30:03 2005
 @@ -1,3 +1,4 @@
 +#!%%PREFIX%%/bin/php -nq
  <?php
  //
  // +----------------------------------------------------------------------+
-@@ -24,9 +25,9 @@
+@@ -24,9 +25,10 @@
  /**
   * @nodep Gtk
   */
@@ -15,6 +15,7 @@
 +dl('pcre.so');
 +dl('xml.so');
 +ini_set('include_path', '%%PREFIX%%/share/pear:%%PREFIX%%/share/pear/bootstrap');
++ini_set('memory_limit', '16M');
  ini_set('allow_url_fopen', true);
  if (!ini_get('safe_mode')) {
-     set_time_limit(0);
+     @set_time_limit(0);
