11ca6a3ce7ae52ca4e47e90a7fd03acf18bd8bae
jmz <jmz@FreeBSD.org>
2005-11-17 10:28:21 +0000

    Upgrade to version 3.9.9.

---
+++ emulators/mtools/files/patch-sysincludes.h
@@ -0,0 +1,10 @@
+--- sysincludes.h~	Wed May  1 12:17:51 2002
++++ sysincludes.h	Wed Nov 16 23:57:42 2005
+@@ -239,7 +239,6 @@
+ #endif
+ 
+ #ifdef HAVE_MALLOC_H
+-# include <malloc.h>
+ #endif
+ 
+ #ifdef HAVE_SIGNAL_H
