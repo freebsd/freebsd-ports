d59271e6a508215d93687799c01119ae0b800a4e
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-11-19 04:34:52 +0000

    Add avahi, another mDNS, Zeroconf, Bonjour, Redezvous, etc. implementation ...but with a twist.  Avahi is a D-BUS based mDNS solution from Freedesktop.org.
    Avahi provides Service discovery on a local network -- this means that you
    can plug your laptop or computer into a network and instantly be able to view
    other people who you can chat with, find printers to print to or find files
    being shared. This kind of technology is already found in MacOS X
    (branded 'Rendezvous', 'Bonjour' and sometimes 'ZeroConf') and is very
    convenient.
    
    WWW: http://www.freedesktop.org/Software/Avahi
    
    A lot of the ground work for this port was provided by ahze.

---
+++ net/avahi/files/avahi-dnsconfd.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+# PROVIDE: avahi_dnsconfd
+# REQUIRE: DAEMON
+# KEYWORD: FreeBSD
+#
+# avahi-dnsconfd connects to a running avahi-daemon and runs  the script
+# %%PREFIX%%/etc/avahi/dnsconf.action for each unicast DNS server that
+# is announced on the local LAN. This is useful for configuring unicast
+# DNS servers in a DHCP-like fashion with mDNS.
+#
+
+avahi_dnsconfd_enable=${avahi_dnsconfd_enable-"NO"}
+avahi_dnsconfd_flags=${avahi_dnsconfd_flags-"-D"}
+
+. %%RC_SUBR%%
+
+name=avahi_dnsconfd
+rcvar=`set_rcvar`
+
+start_cmd=avahi_dnsconfd_start
+stop_cmd=avahi_dnsconfd_stop
+
+avahi_dnsconfd_bin=%%PREFIX%%/sbin/avahi-daemon
+
+avahi_dnsconfd_start() {
+    checkyesno avahi_dnsconfd_enable && echo "Starting avahi-dnsconfd." && \
+    	${avahi_dnsconfd_bin} ${avahi_dnsconfd_flags}
+}
+
+avahi_dnsconfd_stop() {
+    checkyesno avahi_dnsconfd_enable && echo "Stopping avahi-dnsconfd." && \
+    	${avahi_dnsconfd_bin} -k
+}
+
+load_rc_config ${name}
+run_rc_command "$1"
