5312fdb36e1e8ad82660b0e84ebdb9a61204ca10
edwin <edwin@FreeBSD.org>
2005-11-25 03:43:05 +0000

    New port: games/shootingstar
    	The Red-Shirted-Guy served happily in the police forces for
    	many years, until he got kicked out because he used too
    	much violence.  He started drinking. After couple months
    	his twin brother, the Green-Shirted-Guy, thought that Red's
    	life wasn't very glamourous, so Green asked Red if he wanted
    	to collaborate and start killing bad guys. They both thought
    	it was a good idea and so they started their agency, The
    	Shooting Stars. It was finally the time to take law to their
    	own hands.
    
    	WWW: http://www.2ndpoint.fi/ss
    
    PR:		ports/89516
    Submitted by:	Dmitry Marakasov <amdmi3@mail.ru>

---
+++ games/shootingstar/files/patch-src--engine--cTextureFont.cpp
@@ -0,0 +1,10 @@
+--- src/engine/cTextureFont.cpp.orig	Sun Jul  6 22:31:10 2003
++++ src/engine/cTextureFont.cpp	Sun Nov 20 02:33:54 2005
+@@ -31,6 +31,7 @@
+ #include <stdio.h>
+ #include <GL/gl.h>
+ #include <GL/glu.h>
++#include <math.h>
+ #include "cTextureManager.hpp"
+ #include "Debug.hpp"
+ //------------------------------------------------------------------------------
