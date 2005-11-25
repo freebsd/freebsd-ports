e00906edc023936cac84df6b19fdaeb7fee8b97b
vs <vs@FreeBSD.org>
2005-11-25 15:28:37 +0000

    - Change the name of the ports database file (INDEX.db) to    $(basename ${INDEXFILE}).db, e.g. INDEX-6.db. (requested by kris) - Add SHA256.
    PR:		ports/89520
    Submitted by:	maintainer

--- sysutils/portupgrade/files/patch-lib_portsdb.rb
+++ sysutils/portupgrade/files/patch-lib_portsdb.rb
@@ -1,5 +1,18 @@
---- lib/portsdb.rb.orig	Tue Aug  2 10:24:50 2005
-+++ lib/portsdb.rb	Tue Aug  2 10:25:17 2005
+Index: lib/portsdb.rb
+===================================================================
+--- lib/portsdb.rb	(.../vendor/portupgrade/trunk/lib/portsdb.rb)	(revision 2)
++++ lib/portsdb.rb	(.../branches/ports/lib/portsdb.rb)	(revision 41)
+@@ -194,8 +194,8 @@
+   def db_dir=(new_db_dir)
+     @db_dir = new_db_dir || ENV['PORTS_DBDIR'] || ports_dir
+ 
+-    @db_file = File.join(@db_dir, 'INDEX.db')
+-    @db_filebase = @db_file.sub(/\.db$/, '')
++    @db_filebase = File.join(@db_dir, File.basename(index_file()))
++    @db_file = @db_filebase + '.db'
+ 
+     close_db
+ 
 @@ -846,7 +846,7 @@
  
    def all_depends_list(origin, before_args = nil, after_args = nil)
