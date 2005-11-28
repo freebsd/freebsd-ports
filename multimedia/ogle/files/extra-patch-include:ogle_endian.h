2ccd2eb1b3dd8fe6fed547387971cc49647e34f4
lioux <lioux@FreeBSD.org>
2005-11-28 22:56:51 +0000

    o If ${OSVERSION} >= 500000 use bswap16(9)/bswap32(9) instead   of be16toh(9)/be32toh(9). o Bump PORTREVISION

--- multimedia/ogle/files/extra-patch-include:ogle_endian.h
+++ multimedia/ogle/files/extra-patch-include:ogle_endian.h
@@ -1,15 +1,23 @@
---- include/ogle_endian.h.orig	Wed Mar  5 23:26:06 2003
-+++ include/ogle_endian.h	Wed Mar  5 23:28:37 2003
-@@ -45,6 +45,13 @@
+--- include/ogle_endian.h.orig	Sun Apr 13 12:43:23 2003
++++ include/ogle_endian.h	Mon Nov 28 16:46:40 2005
+@@ -48,8 +48,21 @@
+ #  define FROM_BE_32(x) (swap32(x))
  #elif defined(HAVE_SYS_ENDIAN_H) && defined(__FreeBSD__) && __FreeBSD_version >= 470000
  #  include <sys/endian.h>
++#  if (__FreeBSD_version >= 500000)
++#   define FROM_BE_16(x) (bswap16(x))
++#   define FROM_BE_32(x) (bswap32(x))
++#  else
+ #  define FROM_BE_16(x) (be16toh(x))
  #  define FROM_BE_32(x) (be32toh(x))
++#  endif
 +#elif defined(__FreeBSD__)
 +#  define _KERNEL
 +#  define I486_CPU     /* Will crash unless 486+ */
 +#  include <machine/endian.h>
 +#  undef _KERNEL
 +#  undef I486_CPU
++#  define FROM_BE_16(x) (ntohs(x))
 +#  define FROM_BE_32(x) (ntohl(x))
  #else
  #  warning "No accelerated byte swap found. Using slow c version."
