68dd82d20468417680a63eff8e8b2892b95baad0
marcus <marcus@FreeBSD.org>
2005-11-28 19:12:18 +0000

    Update to 0.9.2.

---
+++ audio/rhythmbox-devel/files/patch-podcast_rb-podcast-parse.h
@@ -0,0 +1,10 @@
+--- podcast/rb-podcast-parse.h.orig	Mon Nov 28 13:51:28 2005
++++ podcast/rb-podcast-parse.h	Mon Nov 28 13:51:05 2005
+@@ -22,6 +22,7 @@
+ #ifndef RB_PODCAST_PARSE_H
+ #define RB_PODCAST_PARSE_H
+ 
++#include <inttypes.h>
+ #include <glib.h>
+ 
+ typedef struct
