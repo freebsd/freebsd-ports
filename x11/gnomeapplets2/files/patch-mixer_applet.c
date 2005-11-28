61bf362a8180041b081223a16f64a6983206a8a8
marcus <marcus@FreeBSD.org>
2005-11-28 06:30:45 +0000

    Update to 2.12.2.

--- x11/gnomeapplets2/files/patch-mixer_applet.c
+++ x11/gnomeapplets2/files/patch-mixer_applet.c
@@ -1,11 +1,11 @@
---- mixer/applet.c.orig	Tue Oct  4 00:15:40 2005
-+++ mixer/applet.c	Tue Oct  4 00:16:09 2005
+--- mixer/applet.c.orig	Mon Nov  7 09:53:52 2005
++++ mixer/applet.c	Mon Nov 28 01:16:50 2005
 @@ -953,7 +953,7 @@ gnome_volume_applet_adjust_volume (GstMi
  
    volumes = g_new (gint, track->num_channels);
    for (n = 0; n < track->num_channels; n++)
--    volumes[n] = lrintf (volume);
-+    volumes[n] = (int)rint (volume);
+-    volumes[n] = lrint (volume);
++    volumes[n] = (int) rint (volume);
    gst_mixer_set_volume (mixer, track, volumes);
    g_free (volumes);
  }
