1d5cf4bb401ab91defec5dfbc8a9f054c6970512
Maxim Sobolev <sobomax@FreeBSD.org>
2005-11-28 11:03:42 +0000

    Update to 0.8.16.

--- devel/pychecker/files/patch-setup.py
+++ devel/pychecker/files/patch-setup.py
@@ -1,10 +1,13 @@
---- setup.py.orig	Mon Apr 25 15:50:35 2005
-+++ setup.py	Mon Apr 25 15:50:42 2005
-@@ -78,7 +78,6 @@ fairly infrequent."""
-           author_email          = "pychecker@metaslash.com",
-           url                   = "http://pychecker.sourceforge.net/",
-           packages              = [ 'pychecker' ],
--          data_files            = [ (install_dir, DOC_FILES) ],
-           scripts               = [ LOCAL_SCRIPT, ],
-           # classifiers was added in Python 2.3, but needed for PyPI
-           #classifiers           = CLASSIFIERS,
+
+$FreeBSD$
+
+--- setup.py
++++ setup.py
+@@ -187,7 +187,6 @@
+         'url'              : "http://pychecker.sourceforge.net/",
+         'packages'         : [ 'pychecker', ],
+         'scripts'          : [ "pychecker" ],   # note: will be replaced by customized action
+-        'data_files'       : [ ( "pychecker", DATA_FILES, ) ], 
+         'long_description' : LONG_DESCRIPTION,
+         'cmdclass'         : CUSTOMIZED_ACTIONS, 
+       }
