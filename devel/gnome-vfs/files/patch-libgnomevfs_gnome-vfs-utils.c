13aeece2e7556c5cf79ce41326da1b6b9c2d65f0
Joe Marcus Clarke <marcus@FreeBSD.org>
2005-11-29 04:06:43 +0000

    Update to 2.12.2.

--- devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-utils.c
+++ devel/gnome-vfs/files/patch-libgnomevfs_gnome-vfs-utils.c
@@ -8,19 +8,3 @@
  						  "pixmaps",
  						  relative_filename,
  						  NULL);
-@@ -1991,9 +1992,14 @@ _gnome_vfs_uri_resolve_all_symlinks_uri 
- 			resolved_uri = gnome_vfs_uri_resolve_relative (new_uri,
- 								       info->symlink_name);
- 			if (*p != 0) {
-+				gnome_vfs_uri_unref (new_uri);
-+
-+				new_uri = gnome_vfs_uri_append_string (resolved_uri, p);
-+
- 				gnome_vfs_uri_unref (uri);
--				uri = gnome_vfs_uri_append_string (resolved_uri, p);
- 				gnome_vfs_uri_unref (resolved_uri);
-+
-+				uri = gnome_vfs_uri_ref (new_uri);
- 			} else {
- 				gnome_vfs_uri_unref (uri);
- 				uri = resolved_uri;
