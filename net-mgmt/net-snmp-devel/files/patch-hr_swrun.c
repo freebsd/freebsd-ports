8bad2f31f7d330400081a24f5d9367f6f6885aef
kuriyama <kuriyama@FreeBSD.org>
2005-11-30 05:22:40 +0000

    - Upgrade to 5.2.2. - Return more appropriate values for hrSWRunType (1)
    PR:		ports/86572 (1)
    Submitted by:	wollman (1)

--- net-mgmt/net-snmp-devel/files/patch-hr_swrun.c
+++ net-mgmt/net-snmp-devel/files/patch-hr_swrun.c
@@ -1,5 +1,5 @@
---- agent/mibgroup/host/hr_swrun.c.orig	Wed Jun  9 05:56:03 2004
-+++ agent/mibgroup/host/hr_swrun.c	Fri Jun 25 10:56:38 2004
+--- agent/mibgroup/host/hr_swrun.c.orig	Thu Oct 20 04:32:20 2005
++++ agent/mibgroup/host/hr_swrun.c	Wed Nov 30 12:50:12 2005
 @@ -585,7 +585,7 @@
          string[ sizeof(string)-1 ] = 0;
  #endif
@@ -9,7 +9,7 @@
          strcpy(string, proc_table[LowProcIndex].ki_comm);
      #else
          strcpy(string, proc_table[LowProcIndex].kp_proc.p_comm);
-@@ -694,7 +694,7 @@
+@@ -698,7 +698,7 @@
          *cp1 = 0;
  #endif
  #elif HAVE_KVM_GETPROCS
@@ -18,7 +18,32 @@
          strcpy(string, proc_table[LowProcIndex].ki_comm);
      #else
          strcpy(string, proc_table[LowProcIndex].kp_proc.p_comm);
-@@ -889,7 +889,7 @@
+@@ -868,8 +868,23 @@
+         if (pid < PID_MAXSYS)
+             long_return = 2;    /* operatingSystem */
+         else
+-#endif
+             long_return = 4;    /* application */
++#elif HAVE_KVM_GETPROCS
++    #if defined(freebsd5) && __FreeBSD_version >= 500014
++        if (proc_table[LowProcIndex].ki_flag & P_SYSTEM) {
++            if (proc_table[LowProcIndex].ki_pri.pri_class == PRI_ITHD)
++                long_return = 3;/* deviceDriver */
++            else
++                long_return = 2;/* operatingSystem */
++        } else
++            long_return = 4;    /* application */
++#else
++        if (proc_table[LowProcIndex].kp_proc.p_flag & P_SYSTEM)
++            long_return = 2;    /* operatingSystem */
++        else
++            long_return = 4;    /* application */
++#endif
++#endif
+         return (u_char *) & long_return;
+     case HRSWRUN_STATUS:
+ #if defined(cygwin)
+@@ -901,7 +916,7 @@
          }
  #else
  #if HAVE_KVM_GETPROCS
@@ -27,7 +52,7 @@
          switch (proc_table[LowProcIndex].ki_stat) {
      #else
          switch (proc_table[LowProcIndex].kp_proc.p_stat) {
-@@ -988,7 +988,13 @@
+@@ -1000,7 +1015,13 @@
          long_return = proc_buf->p_utime * 100 + proc_buf->p_stime * 100;
  #endif
  #elif HAVE_KVM_GETPROCS
@@ -42,23 +67,7 @@
          long_return = proc_table[LowProcIndex].ki_runtime / 100000;
      #else
          long_return = proc_table[LowProcIndex].kp_proc.p_uticks +
-@@ -1063,7 +1069,14 @@
-         long_return = proc_buf->p_swrss;
- #endif
- #elif HAVE_KVM_GETPROCS
--#if defined(freebsd3) && !defined(darwin)
-+#if defined(freebsd5) && __FreeBSD_version >= 500014
-+	    /* XXX
-+	    long_return = proc_table[LowProcIndex].ki_vmspace->vm_tsize +
-+			  proc_table[LowProcIndex].ki_vmspace->vm_ssize +
-+			  proc_table[LowProcIndex].ki_vmspace->vm_dsize;
-+	    long_return = long_return * (getpagesize() / 1024); */
-+	    long_return = 0;
-+#elif defined(freebsd3) && !defined(darwin)
-         long_return =
-     #if defined(freebsd5)
-             proc_table[LowProcIndex].ki_size / 1024;
-@@ -1349,7 +1362,7 @@
+@@ -1365,7 +1386,7 @@
  #elif defined(solaris2)
          return proc_table[current_proc_entry++];
  #elif HAVE_KVM_GETPROCS
--- net-mgmt/net-snmp53/files/patch-snmp_agent.c
+++ net-mgmt/net-snmp53/files/patch-snmp_agent.c
@@ -1,10 +1,10 @@
---- agent/snmp_agent.c.orig	Tue Mar 22 11:11:51 2005
-+++ agent/snmp_agent.c	Tue Mar 22 11:12:29 2005
-@@ -825,6 +825,7 @@
-             return 0;
-         }
-     } else {
-+	SNMP_FREE(addr_string);
-         if (hosts_ctl("snmpd", STRING_UNKNOWN, STRING_UNKNOWN, STRING_UNKNOWN)){
-             snmp_log(allow_severity, "Connection from <UNKNOWN>\n");
+--- agent/snmp_agent.c.orig	Thu Mar 17 17:17:43 2005
++++ agent/snmp_agent.c	Wed Nov 30 12:23:41 2005
+@@ -839,6 +839,7 @@
+             ;
+         else if (hosts_ctl("snmpd", STRING_UNKNOWN, STRING_UNKNOWN, STRING_UNKNOWN)){
+             snmp_log(allow_severity, "Connection from <UNKNOWN> (%s)\n", addr_string);
++            SNMP_FREE(addr_string);
              addr_string = strdup("<UNKNOWN>");
+         } else {
+             snmp_log(deny_severity, "Connection from <UNKNOWN> (%s) REFUSED\n", addr_string);
