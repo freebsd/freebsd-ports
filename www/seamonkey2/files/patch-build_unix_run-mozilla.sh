ffd91b8d7915efb1198d02bcaebe8981ce7b912d
ahze <ahze@FreeBSD.org>
2005-11-30 00:44:58 +0000

    - Add seamonkey
    This is the current release of the Seamonkey open source web browser. It should
    be fully compliant with all W3C standards, including HTML, CSS, XML, XSL,
    JavaScript, MathML, SSL encryption, SVG and RDF.  Also supports Java with
    the use of the FreeBSD native Java plug-in.
    
    This is the latest bleeding edge release.  Beware that you'll probably find
    bugs here, so if you value stability, try www/mozilla.
    
    WWW: http://www.mozilla.org/projects/seamonkey/
    
    Repocopied by:	marcus

--- www/seamonkey2/files/patch-build_unix_run-mozilla.sh
+++ www/seamonkey2/files/patch-build_unix_run-mozilla.sh
@@ -1,15 +1,15 @@
---- build/unix/run-mozilla.sh.orig	Mon Dec 15 12:27:28 2003
-+++ build/unix/run-mozilla.sh	Wed Feb 11 14:24:17 2004
-@@ -355,7 +355,7 @@
- fi
- ##
- ## Set LD_LIBRARY_PATH
--LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
-+LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%PREFIX%%/lib/browser_linux_plugins:%%PREFIX%%/lib/browser_plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
- if [ -n "$LD_LIBRARYN32_PATH" ]
+--- build/unix/run-mozilla.sh.orig	Mon Oct 17 13:21:17 2005
++++ build/unix/run-mozilla.sh	Thu Nov 10 22:01:41 2005
+@@ -333,7 +333,7 @@
+ ## LD_LIBRARY_PATH because $ORIGIN doesn't work on a symbolic link.
+ if [ `uname -s` != "SunOS" -o -h "$MOZ_PROGRAM" ]
  then
- 	LD_LIBRARYN32_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARYN32_PATH+":$LD_LIBRARYN32_PATH"}
-@@ -442,6 +442,9 @@
+-	LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
++	LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%PREFIX%%/lib/browser_plugins:%%PREFIX%%/lib/browser_linux_plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
+ fi 
+ 
+ if [ -n "$LD_LIBRARYN32_PATH" ]
+@@ -422,6 +422,9 @@
  #
  export MOZILLA_FIVE_HOME LD_LIBRARY_PATH
  export SHLIB_PATH LIBPATH LIBRARY_PATH ADDON_PATH DYLD_LIBRARY_PATH
