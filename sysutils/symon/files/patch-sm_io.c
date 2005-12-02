6f45d20c0d95b5f9b6939ece8d3ec4fc7f58048a
Marcus Alves Grando <mnag@FreeBSD.org>
2005-12-02 16:19:10 +0000

    Update to 2.72 Add SHA256 portlint(1)
    PR:		89836
    Submitted by:	Ulrich Spoerlein <q@galgenberg.net> (maintainer)

--- sysutils/symon/files/patch-sm_io.c
+++ sysutils/symon/files/patch-sm_io.c
@@ -1,6 +1,6 @@
---- platform/FreeBSD/sm_io.c.orig	Mon Jul 25 12:08:47 2005
-+++ platform/FreeBSD/sm_io.c	Mon Jul 25 12:12:27 2005
-@@ -81,7 +81,7 @@
+--- platform/FreeBSD/sm_io.c.orig	Sat Nov 12 20:16:41 2005
++++ platform/FreeBSD/sm_io.c	Sat Nov 12 20:17:10 2005
+@@ -80,7 +80,7 @@
  void
  gets_io()
  {
@@ -9,7 +9,7 @@
      io_numdevs = devstat_getnumdevs(NULL);
  #else
      io_numdevs = getnumdevs();
-@@ -98,7 +98,7 @@
+@@ -97,7 +97,7 @@
      /* clear the devinfo struct, as getdevs expects it to be all zeroes */
      bzero(io_stats.dinfo, sizeof(struct devinfo));
  
@@ -18,12 +18,12 @@
      devstat_getdevs(NULL, &io_stats);
  #else
      getdevs(&io_stats);
-@@ -118,7 +118,7 @@
- 	    strlen(ds->device_name) < strlen(dev) &&
- 	    isdigit(dev[strlen(ds->device_name)]) &&
- 	    atoi(&dev[strlen(ds->device_name)]) == ds->unit_number) {
+@@ -117,7 +117,7 @@
+ 	    strlen(ds->device_name) < strlen(st->arg) &&
+ 	    isdigit(st->arg[strlen(ds->device_name)]) &&
+ 	    atoi(&st->arg[strlen(ds->device_name)]) == ds->unit_number) {
 -#if DEVSTAT_USER_API_VER == 5
 +#if DEVSTAT_USER_API_VER == 5 || DEVSTAT_USER_API_VER == 6
- 	    return snpack(symon_buf, maxlen, dev, MT_IO2,
+ 	    return snpack(symon_buf, maxlen, st->arg, MT_IO2,
  			  ds->operations[DEVSTAT_READ],
  			  ds->operations[DEVSTAT_WRITE],
