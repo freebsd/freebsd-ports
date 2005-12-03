d97c6f0567b5c61afbedd46abd27be805303a03f
Clement Laforet <clement@FreeBSD.org>
2005-12-03 22:02:58 +0000

    - Add apache 2.2.0   It's a temporary layout, I need more time to find the best.   note that ${PREFIX}/www/(data|errors|cgi)(-dist) disappeared in favor of   ${PREFIX}/www/apache22

--- www/apache22/files/apache.sh
+++ www/apache22/files/apache.sh
@@ -1,66 +1,66 @@
 #!/bin/sh
 #
-# $FreeBSD: /tmp/pcvs/ports/www/apache22/files/Attic/apache.sh,v 1.8 2005-01-11 13:45:05 clement Exp $
+# $FreeBSD: /tmp/pcvs/ports/www/apache22/files/Attic/apache.sh,v 1.9 2005-12-03 22:02:58 clement Exp $
 #
 
-# PROVIDE: apache21
+# PROVIDE: apache22
 # REQUIRE: NETWORKING SERVERS
 # BEFORE: DAEMON
 # KEYWORD: FreeBSD shutdown
 
 #
-# Add the following lines to /etc/rc.conf to enable apache21:
-# apache21_enable (bool):      Set to "NO" by default.
-#                             Set it to "YES" to enable apache21
-# apache21ssl_enable (bool):   Set to "NO" by default.
+# Add the following lines to /etc/rc.conf to enable apache22:
+# apache22_enable (bool):      Set to "NO" by default.
+#                             Set it to "YES" to enable apache22
+# apache22ssl_enable (bool):   Set to "NO" by default.
 #                             Set it to "YES" to start apache with SSL   
 #                             (if <IfDefined SSL> exists in httpd.conf)
-# apache21limits_enable (bool):Set to "NO" by default.
+# apache22limits_enable (bool):Set to "NO" by default.
 #                             Set it to yes to run `limits $limits_args`
 #                             just before apache starts.
-# apache21_flags (str):        Set to "" by default.
+# apache22_flags (str):        Set to "" by default.
 #                             Extra flags passed to start command.
-# apache21limits_args (str):   Default to "-e -C daemon"
+# apache22limits_args (str):   Default to "-e -C daemon"
 #                             Arguments of pre-start limits run.
 #
 . %%RC_SUBR%%
 
-name="apache21"
+name="apache22"
 rcvar=`set_rcvar`
 
-start_precmd="apache21_precmd"
-restart_precmd="apache21_checkconfig"
-reload_precmd="apache21_checkconfig"
+start_precmd="apache22_precmd"
+restart_precmd="apache22_checkconfig"
+reload_precmd="apache22_checkconfig"
 command="%%PREFIX%%/sbin/httpd"
 pidfile="/var/run/httpd.pid"
-required_files=%%PREFIX%%/etc/apache21/httpd.conf
+required_files=%%PREFIX%%/etc/apache22/httpd.conf
 
-[ -z "$apache21_enable" ]       && apache21_enable="NO"
-[ -z "$apache21ssl_enable" ]    && apache21ssl_enable="NO"
-[ -z "$apache21_flags" ]        && apache21_flags=""
-[ -z "$apache21limits_enable" ] && apache21limits_enable="NO"
-[ -z "$apache21limits_args" ]   && apache21limits_args="-e -C daemon"
+[ -z "$apache22_enable" ]       && apache22_enable="NO"
+[ -z "$apache22ssl_enable" ]    && apache22ssl_enable="NO"
+[ -z "$apache22_flags" ]        && apache22_flags=""
+[ -z "$apache22limits_enable" ] && apache22limits_enable="NO"
+[ -z "$apache22limits_args" ]   && apache22limits_args="-e -C daemon"
 
 load_rc_config $name
 
-checkyesno apache21ssl_enable && \
-			apache21_flags="-DSSL $apache21_flags"
+checkyesno apache22ssl_enable && \
+			apache22_flags="-DSSL $apache22_flags"
 
-apache21_checkconfig()
+apache22_checkconfig()
 {
-	echo "Performing sanity check on apache21 configuration:"
-	${command} ${apache21_flags} -t
+	echo "Performing sanity check on apache22 configuration:"
+	${command} ${apache22_flags} -t
 }
 
-apache21_precmd() 
+apache22_precmd() 
 {
 	if test -f %%PREFIX%%/sbin/envvars
 	then
 		. %%PREFIX%%/sbin/envvars
 	fi
-	if checkyesno apache21limits_enable
+	if checkyesno apache22limits_enable
 	then
-		eval `/usr/bin/limits ${apache21limits_args}` 2>/dev/null
+		eval `/usr/bin/limits ${apache22limits_args}` 2>/dev/null
 	else
 		return 0
         fi
