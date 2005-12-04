8e6ecba4d16ddb8c6e68608e737e36eb80758348
clement <clement@FreeBSD.org>
2005-12-04 00:18:15 +0000

    - prepare removal of www/apache2 in favor of www/apache20 for naming   consistency - add entries in UPDATING (for apache22 too)
    PR:		ports/78119
    Repocopied by:	marcus

--- Mk/bsd.apache.mk
+++ Mk/bsd.apache.mk
@@ -34,7 +34,7 @@ AP_PORT_IS_MODULE=	YES
 #### for backward compatibility
 .elif ${USE_APACHE:L} == yes
 .   if defined(WITH_APACHE2)
-APACHE_PORT?=	www/apache2
+APACHE_PORT?=	www/apache20
 .   else
 APACHE_PORT?=	www/apache13
 .   endif
@@ -293,9 +293,7 @@ IGNORE?=	PREFIX must be egal to APXS_PREFIX.
 AP_BUILDEXT=	la
 PLIST_SUB+=	APACHEMODDIR="libexec/apache2" \
 			APACHEINCLUDEDIR="include/apache2"
-# XXX We postpone www/apache2 => www/apache20 migration
-#APACHE_PORT=	www/apache${APACHE_VERSION}
-APACHE_PORT=	www/apache2
+APACHE_PORT=	www/apache${APACHE_VERSION}
 .elif ${APACHE_VERSION} >= "21"
 AP_BUILDEXT=	la
 PLIST_SUB+=	APACHEMODDIR="libexec/apache${APACHE_VERSION}" \
@@ -307,6 +305,7 @@ PLIST_SUB+=	APACHEMODDIR="libexec/apache" \
 			APACHEINCLUDEDIR="include/apache"
 APACHE_PORT?= www/apache13
 .endif
+
 .for VAR in ${OVERRIDABLE_VARS}
 .  if defined(AP${APACHE_VERSION}_${VAR})
 ${VAR} =${AP${APACHE_VERSION}_${VAR}}
