7b0635736dee1806311bb53a3f8dd1528cdf7ce4
clement <clement@FreeBSD.org>
2005-12-04 14:05:14 +0000

    - Fix quoting error in BROKEN_WITH_MYSQL IGNORE message
    Approved by:	portmgr (self)

--- Mk/bsd.database.mk
+++ Mk/bsd.database.mk
@@ -96,7 +96,7 @@ BROKEN=	MySQL versions mismatch: mysql${_MYSQL_VER}-client is installed and want
 .if defined(BROKEN_WITH_MYSQL)
 .	for VER in ${BROKEN_WITH_MYSQL}
 .		if (${MYSQL_VER} == "${VER}")
-IGNORE=		"Doesn't work with MySQL version : ${MYSQL_VER} (Doesn't support MySQL ${BROKEN_WITH_MYSQL})"
+IGNORE=		Doesn't work with MySQL version : ${MYSQL_VER} (Doesn't support MySQL ${BROKEN_WITH_MYSQL})
 .		endif
 .	endfor
 .endif # BROKEN_WITH_MYSQL
