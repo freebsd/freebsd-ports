deab2a8c19bb96e055dde6cd0b06e644333096a3
ehaupt <ehaupt@FreeBSD.org>
2005-12-04 01:20:10 +0000

    - Update to 0.1.10 - Remove ONLY_FOR_ARCHS - Add SHA256
    PR:		89899
    Submitted by:	Dmitry Marakasov <amdmi3@mail.ru>

---
+++ games/xmoto/files/patch-src-VCommon.h
@@ -0,0 +1,12 @@
+--- src/VCommon.h.orig	Sun Dec  4 01:53:03 2005
++++ src/VCommon.h	Sun Dec  4 01:53:35 2005
+@@ -69,8 +69,7 @@
+ 
+ #include <string>
+ #include <vector>
+-#include <ostream>
+-#include <istream>
++#include <iostream>
+ 
+ #include <stdio.h>
+ #include <math.h>
