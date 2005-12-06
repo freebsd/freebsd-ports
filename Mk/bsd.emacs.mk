65b721ad38403a519e72e873d0f5bcb12a0a51bd
Renato Botelho <garga@FreeBSD.org>
2005-12-06 16:17:12 +0000

    - Update editors/xemacs [1] and editors/xemacs21-mule [2] to 21.4.18
    PR:		ports/90011 [1], ports/90010 [2]
    Submitted by:	Andrey Slusar <anrays@gmail.com> [1] [2]

--- Mk/bsd.emacs.mk
+++ Mk/bsd.emacs.mk
@@ -1,5 +1,5 @@
 #
-#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.45 2005-10-27 17:09:48 sem Exp $
+#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.46 2005-12-06 16:17:12 garga Exp $
 #
 #	bsd.emacs.mk - 19990829 Shigeyuki Fukushima.
 #
@@ -132,7 +132,7 @@ PLIST?=                 ${PKGDIR}/pkg-plist.${EMACS_PORT_NAME}
 # XEmacs-21.x
 .elif (${EMACS_PORT_NAME} == "xemacs21")
 EMACS_NAME=		xemacs
-EMACS_VER=		21.4.17
+EMACS_VER=		21.4.18
 EMACS_MAJOR_VER=	21
 EMACS_LIBDIR?=		lib/${EMACS_NAME}
 EMACS_LIBDIR_WITH_VER?=	lib/${EMACS_NAME}-${EMACS_VER}
@@ -149,7 +149,7 @@ PLIST?=                 ${PKGDIR}/pkg-plist.${EMACS_PORT_NAME}
 # XEmacs-21.x with Mule
 .elif (${EMACS_PORT_NAME} == "xemacs21-mule")
 EMACS_NAME=		xemacs
-EMACS_VER=		21.4.17
+EMACS_VER=		21.4.18
 EMACS_MAJOR_VER=	21
 EMACS_LIBDIR?=		lib/${EMACS_NAME}
 EMACS_LIBDIR_WITH_VER?=	lib/${EMACS_NAME}-${EMACS_VER}
