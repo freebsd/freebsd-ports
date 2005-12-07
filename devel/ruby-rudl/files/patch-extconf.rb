61de7286de6460b35850f92c1c3c177bbd0379e9
garga <garga@FreeBSD.org>
2005-12-07 13:45:33 +0000

    - Update to 0.8 - Pass maintainership to submitter
    PR:		ports/90053
    Submitted by:	Erik Greenwald <erik@smluc.org>

--- devel/ruby-rudl/files/patch-extconf.rb
+++ devel/ruby-rudl/files/patch-extconf.rb
@@ -1,31 +1,11 @@
---- extconf.rb.orig	Sat Sep 27 07:59:45 2003
-+++ extconf.rb	Mon Apr 19 04:07:30 2004
-@@ -52,14 +52,14 @@
- windows=mswin32||cygwin
- unix=!windows
- 
--$CFLAGS+="-funroll-loops " if !mswin32
-+$CFLAGS+=" -funroll-loops " if !mswin32
- 
--$CFLAGS+="-Wall " if unix||cygwin
--$CPPFLAGS+="-GX " if mswin32
-+$CFLAGS+=" -Wall " if unix||cygwin
-+$CPPFLAGS+=" -GX " if mswin32
- 
- if unix
--	$CFLAGS += `#{sdl_config} --cflags`.chomp
--	$LDFLAGS += `#{sdl_config} --libs`.chomp
-+	$CFLAGS += " " + `#{sdl_config} --cflags`.chomp
-+	$LDFLAGS += " " + `#{sdl_config} --libs`.chomp
- end
- 
- puts '* Checking for optional files'
-@@ -99,7 +99,7 @@
+--- extconf.rb.orig	Wed Dec  7 11:12:50 2005
++++ extconf.rb	Wed Dec  7 11:14:10 2005
+@@ -105,7 +105,7 @@
  
  puts '* Checking for required files'
  puts ' - SDL from http://www.libsdl.org/download-1.2.html'
 -if have_library('SDL', 'SDL_Quit') and have_header('SDL.h')
 +if have_func('SDL_Quit') and have_header('SDL.h')
- 	create_makefile('RUDL')
- 	puts '* Done! You may now run make.'
+     create_makefile('RUDL')
+     puts '* Done! You may now run make.'
  else
