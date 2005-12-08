b5b643ff6e90b52cb70cf4b9e531d30b165b47d3
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2005-12-08 17:25:46 +0000

    Update to 4.3.1

--- net/py-bittorrent-devel/files/patch-BitTorrent::defaultargs.py
+++ net/py-bittorrent-devel/files/patch-BitTorrent::defaultargs.py
@@ -1,11 +1,11 @@
---- BitTorrent/defaultargs.py.orig	Tue May 24 03:01:06 2005
-+++ BitTorrent/defaultargs.py	Sun May 29 22:48:48 2005
-@@ -110,7 +110,7 @@
-      _("do not connect to several peers that have the same IP address")),
-     ('peer_socket_tos', 8,
-      _("if nonzero, set the TOS option for peer connections to this value")),
--    ('bad_libc_workaround', MYFALSE,
-+    ('bad_libc_workaround', MYTRUE,
-      _("enable workaround for a bug in BSD libc that makes file reads very slow.")),
-     ('tracker_proxy', '',
-      _("address of HTTP proxy to use for tracker connections")),
+--- BitTorrent/defaultargs.py.orig	Thu Dec  8 14:52:02 2005
++++ BitTorrent/defaultargs.py	Thu Dec  8 14:52:22 2005
+@@ -33,6 +33,8 @@
+ if os.name == 'posix':
+     if os.uname()[0] in ['Darwin']:
+         BAD_LIBC_WORKAROUND_DEFAULT = MYTRUE
++    if os.uname()[0] in ['FreeBSD']:
++        BAD_LIBC_WORKAROUND_DEFAULT = MYTRUE
+     
+ from BitTorrent import languages
+ 
