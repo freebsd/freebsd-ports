0a7f5f45415a0a3680c38136568ff844bc25074d
Thierry Thomas <thierry@FreeBSD.org>
2005-12-08 23:30:13 +0000

    Add Kicad, a software for the creation of electronic schematic diagrams and printed circuit board artwork.
    Kicad is a set of four softwares and a project manager:
    
        * Eeschema: Schematic entry.
        * Pcbnew: Board editor.
        * Gerbview: GERBER viewer (photoplotter documents).
        * Cvpcb: footprint selector for components used in the circuit design.
        * Kicad: project manager.

---
+++ cad/kicad-devel/files/patch-include__fctsys.h
@@ -0,0 +1,17 @@
+--- ./include/fctsys.h.orig	Tue Mar 22 15:30:28 2005
++++ ./include/fctsys.h	Thu Dec  1 21:50:58 2005
+@@ -18,8 +18,12 @@
+ #endif
+ 
+ #include <stdio.h>
+-#ifndef __DARWIN__
+-#include <malloc.h>		// MacOSX (DARWIN): malloc() and free() are in stdlib.h
++#ifdef __FreeBSD__
++#include <stdlib.h>
++#else
++# ifndef __DARWIN__
++# include <malloc.h>		// MacOSX (DARWIN): malloc() and free() are in stdlib.h
++# endif
+ #endif
+ #include <time.h>
+ #include <math.h>
---
+++ cad/kicad/files/patch-pcbnew__class_module.cpp
@@ -0,0 +1,11 @@
+--- ./pcbnew/class_module.cpp.orig	Fri Sep  9 12:22:06 2005
++++ ./pcbnew/class_module.cpp	Thu Dec  1 23:15:31 2005
+@@ -1017,7 +1017,7 @@
+ 	pos += 14;
+ 	if ( flag )	// Affichage date de modification (utile en Module Editor)
+ 		{
+-		strcpy(Line, ctime(&m_LastEdit_Time));
++		strcpy(Line, ctime((time_t*) &m_LastEdit_Time));
+ 		strtok(Line," \n\r");
+ 		strcpy( bufcar, strtok(NULL," \n\r") ); strcat(bufcar," ");
+ 		strcat( bufcar, strtok(NULL," \n\r") ); strcat(bufcar,", ");
