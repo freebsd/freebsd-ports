1a1d7f44bebfee6bbe2af9562d491e4898bd1ed5
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2005-12-10 21:05:06 +0000

    Update to 1.1.1.

--- multimedia/libxine/files/patch-src:libreal:audio_decoder.c
+++ multimedia/libxine/files/patch-src:libreal:audio_decoder.c
@@ -1,5 +1,5 @@
---- src/libreal/audio_decoder.c.orig	Thu Dec 16 22:59:10 2004
-+++ src/libreal/audio_decoder.c	Thu Dec 30 08:55:14 2004
+--- src/libreal/audio_decoder.c.orig	Sun Oct 23 07:09:47 2005
++++ src/libreal/audio_decoder.c	Sun Dec 11 02:49:37 2005
 @@ -48,6 +48,14 @@
  #include "buffer.h"
  #include "xineutils.h"
@@ -15,7 +15,7 @@
  typedef struct {
    audio_decoder_class_t   decoder_class;
  
-@@ -709,22 +717,12 @@
+@@ -709,24 +717,12 @@
  
    /* try some auto-detection */
  
@@ -33,13 +33,15 @@
 -    default_real_codec_path = "/usr/lib64/RealPlayer8/Codecs";
 -  if (!stat ("/usr/lib64/RealPlayer9/users/Real/Codecs/drv3.so.6.0", &s)) 
 -    default_real_codec_path = "/usr/lib64/RealPlayer9/users/Real/Codecs";
+-  if (!stat ("/usr/lib/codecs/drv3.so.6.0", &s)) 
+-    default_real_codec_path = "/usr/lib/codecs";
 -  if (!stat ("/usr/lib/win32/drv3.so.6.0", &s)) 
 -    default_real_codec_path = "/usr/lib/win32";
-+  if (!stat ("/usr/local/lib/RealPlayer8/Codecs/drv3.so.6.0", &s)) 
++  if (!stat ("/usr/local/lib/RealPlayer8/Codecs/drv3.so.6.0", &s))
 +    default_real_codec_path = "/usr/local/lib/RealPlayer8/Codecs";
-+  if (!stat ("/usr/local/lib/RealPlayer9/users/Real/Codecs/drv3.so.6.0", &s)) 
++  if (!stat ("/usr/local/lib/RealPlayer9/users/Real/Codecs/drv3.so.6.0", &s))
 +    default_real_codec_path = "/usr/local/lib/RealPlayer9/users/Real/Codecs";
-+  if (!stat ("/usr/local/lib/win32/drv3.so.6.0", &s)) 
++  if (!stat ("/usr/local/lib/win32/drv3.so.6.0", &s))
 +    default_real_codec_path = "/usr/local/lib/win32";
    
    real_codec_path = config->register_string (config, "decoder.external.real_codecs_path", 
--- multimedia/libxine/files/patch-src:libreal:xine_decoder.c
+++ multimedia/libxine/files/patch-src:libreal:xine_decoder.c
@@ -1,5 +1,5 @@
---- src/libreal/xine_decoder.c.orig	Thu Dec 16 22:59:10 2004
-+++ src/libreal/xine_decoder.c	Thu Dec 30 08:56:38 2004
+--- src/libreal/xine_decoder.c.orig	Thu Oct 27 23:58:56 2005
++++ src/libreal/xine_decoder.c	Sun Dec 11 02:52:23 2005
 @@ -47,6 +47,14 @@
  #include "buffer.h"
  #include "xineutils.h"
@@ -15,7 +15,7 @@
  typedef struct {
    video_decoder_class_t   decoder_class;
  
-@@ -589,22 +597,12 @@
+@@ -589,25 +597,13 @@
  
    /* try some auto-detection */
  
@@ -33,14 +33,18 @@
 -    default_real_codec_path = "/usr/lib64/RealPlayer8/Codecs";
 -  if (!stat ("/usr/lib64/RealPlayer9/users/Real/Codecs/drv3.so.6.0", &s)) 
 -    default_real_codec_path = "/usr/lib64/RealPlayer9/users/Real/Codecs";
+-  if (!stat ("/usr/lib/codecs/drv3.so.6.0", &s)) 
+-    default_real_codec_path = "/usr/lib/codecs";
 -  if (!stat ("/usr/lib/win32/drv3.so.6.0", &s)) 
 -    default_real_codec_path = "/usr/lib/win32";
-+  if (!stat ("/usr/local/lib/RealPlayer8/Codecs/drv3.so.6.0", &s)) 
+-  
++  if (!stat ("/usr/local/lib/RealPlayer8/Codecs/drv3.so.6.0", &s))
 +    default_real_codec_path = "/usr/local/lib/RealPlayer8/Codecs";
-+  if (!stat ("/usr/local/lib/RealPlayer9/users/Real/Codecs/drv3.so.6.0", &s)) 
++  if (!stat ("/usr/local/lib/RealPlayer9/users/Real/Codecs/drv3.so.6.0", &s))
 +    default_real_codec_path = "/usr/local/lib/RealPlayer9/users/Real/Codecs";
-+  if (!stat ("/usr/local/lib/win32/drv3.so.6.0", &s)) 
++  if (!stat ("/usr/local/lib/win32/drv3.so.6.0", &s))
 +    default_real_codec_path = "/usr/local/lib/win32";
-   
++
    real_codec_path = config->register_string (config, "decoder.external.real_codecs_path", 
  					     default_real_codec_path,
+ 					     _("path to RealPlayer codecs"),
