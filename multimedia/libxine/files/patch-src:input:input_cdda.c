4b489759bfbcdbf269b2524cbcf8863088270094
nobutaka <nobutaka@FreeBSD.org>
2005-12-10 21:05:06 +0000

    Update to 1.1.1.

--- multimedia/libxine/files/patch-src:input:input_cdda.c
+++ multimedia/libxine/files/patch-src:input:input_cdda.c
@@ -1,11 +1,11 @@
---- src/input/input_cdda.c.orig	Mon Oct 10 00:51:19 2005
-+++ src/input/input_cdda.c	Mon Oct 10 00:52:58 2005
-@@ -1473,7 +1473,7 @@
-     return;
-   }
-   else {
--    fprintf(fd, filecontent);
-+    fprintf(fd, "%s", filecontent);
-     fclose(fd);
-   }
-   
+--- src/input/input_cdda.c.orig	Sun Dec 11 03:34:00 2005
++++ src/input/input_cdda.c	Sun Dec 11 03:34:07 2005
+@@ -32,7 +32,7 @@
+ #include <unistd.h>
+ 
+ #include <sys/types.h>
+-#ifdef SYS_PARAM_H
++#ifdef HAVE_SYS_PARAM_H
+ #include <sys/param.h>
+ #endif
+ #ifdef HAVE_DIRENT_H
--- multimedia/libxine/files/patch-src:post:deinterlace:plugins:tomsmocomp:tomsmocompmacros.h
+++ multimedia/libxine/files/patch-src:post:deinterlace:plugins:tomsmocomp:tomsmocompmacros.h
@@ -1,11 +1,12 @@
---- src/post/deinterlace/plugins/tomsmocomp/tomsmocompmacros.h.orig	Sat Sep 17 01:51:42 2005
-+++ src/post/deinterlace/plugins/tomsmocomp/tomsmocompmacros.h	Sat Sep 17 01:52:41 2005
-@@ -1,7 +1,6 @@
--#include <malloc.h>
-+#include <stdlib.h>
+--- src/post/deinterlace/plugins/tomsmocomp/tomsmocompmacros.h.orig	Sat Jul 30 03:14:40 2005
++++ src/post/deinterlace/plugins/tomsmocomp/tomsmocompmacros.h	Sun Dec 11 02:54:16 2005
+@@ -1,8 +1,6 @@
  #include <string.h>
  #include <math.h>
+-#ifdef HAVE_MALLOC_H
 -#include <malloc.h>
+-#endif
++#include <stdlib.h>
  
  #define USE_FOR_DSCALER
  
