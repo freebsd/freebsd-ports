648792b7e06241008474f030d74e884d6808fc88
edwin <edwin@FreeBSD.org>
2005-12-12 21:03:34 +0000

    Update port: games/vavoom 1.17 -> 1.18
    PR:		ports/90280
    Submitted by:	Igor Pokrovsky <ip@doom.homeunix.org>

--- games/vavoom/files/patch-source_sys__sdl.cpp
+++ games/vavoom/files/patch-source_sys__sdl.cpp
@@ -1,57 +1,11 @@
---- source/sys_sdl.cpp.orig
-+++ source/sys_sdl.cpp
-@@ -61,8 +61,8 @@
+--- source/sys_sdl.cpp.orig	Mon Dec 12 14:39:33 2005
++++ source/sys_sdl.cpp	Mon Dec 12 14:39:52 2005
+@@ -380,7 +380,7 @@
+ 			char *out = buf;
+ 			size_t inbytes = 1;
+ 			size_t outbytes = 8;
+-			iconv(cd, &in, &inbytes, &out, &outbytes);
++			iconv(cd, (const char**)&in, &inbytes, &out, &outbytes);
+ 			fwrite(buf, 1, out - buf, stdout);
+ 		}
  
- // PUBLIC DATA DEFINITIONS -------------------------------------------------
- 
--jmp_buf __Context::Env;
--const char* __Context::ErrToThrow;
-+//jmp_buf __Context::Env;
-+//const char* __Context::ErrToThrow;
- 
- // PRIVATE DATA DEFINITIONS ------------------------------------------------
- 
-@@ -733,33 +733,33 @@
- 	switch (s)
- 	{
- 	case SIGABRT:
--		__Context::ErrToThrow = "Aborted";
-+		throw VavoomError("Aborted");
- 		break;
- 	case SIGFPE:
--		__Context::ErrToThrow = "Floating Point Exception";
-+		throw VavoomError("Floating Point Exception");
- 		break;
- 	case SIGILL:
--		__Context::ErrToThrow = "Illegal Instruction";
-+		throw VavoomError("Illegal Instruction");
- 		break;
- 	case SIGSEGV:
--		__Context::ErrToThrow = "Segmentation Violation";
-+		throw VavoomError("Segmentation Violation");
- 		break;
- 	case SIGTERM:
--		__Context::ErrToThrow = "Terminated";
-+		throw VavoomError("Terminated");
- 		break;
- 	case SIGINT:
--		__Context::ErrToThrow = "Interrupted by User";
-+		throw VavoomError("Interrupted by User");
- 		break;
- 	case SIGKILL:
--		__Context::ErrToThrow = "Killed";
-+		throw VavoomError("Killed");
- 		break;
- 	case SIGQUIT:
--		__Context::ErrToThrow = "Quited";
-+		throw VavoomError("Quited");
- 		break;
- 	default:
--		__Context::ErrToThrow = "Terminated by signal";
-+		throw VavoomError("Terminated by signal");
- 	}
--	longjmp(__Context::Env, 1);
-+//	longjmp(__Context::Env, 1);
- }
- 
- //==========================================================================
