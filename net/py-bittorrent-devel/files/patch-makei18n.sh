5e7b851de027a6c8d917f316ca46d8cbb24c0111
lioux <lioux@FreeBSD.org>
2005-12-12 05:10:05 +0000

    Update to 4.3.2

--- net/py-bittorrent-devel/files/patch-makei18n.sh
+++
@@ -1,9 +0,0 @@
---- makei18n.sh.orig	Sun Nov 20 23:17:39 2005
-+++ makei18n.sh	Sun Nov 20 23:18:34 2005
-@@ -1,5 +1,5 @@
- APP_NAME="bittorrent"
--LANGUAGES=`python language_codes.py`
-+LANGUAGES=`cd po; ls *.po | sed -e 's/\.po//'`
- MESSAGES_PO="messages.pot"
- 
- rm -f $APP_NAME.lis
