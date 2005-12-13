7eae006de27d0c182ddeee66fc90b703aa95d285
Jeremy Messenger <mezz@FreeBSD.org>
2005-12-13 07:04:11 +0000

    Update to 2.1.
    Reminded by:	edwin's "FreeBSD Ports Version Check" script

--- x11/libxklavier/files/patch-libxklavier_xklavier_xmm.c
+++ x11/libxklavier/files/patch-libxklavier_xklavier_xmm.c
@@ -1,15 +1,12 @@
---- libxklavier/xklavier_xmm.c.orig	Tue Nov 30 02:17:37 2004
-+++ libxklavier/xklavier_xmm.c	Tue Nov 30 02:17:51 2004
-@@ -66,10 +66,11 @@
- const XmmSwitchOptionPtr _XklXmmGetCurrentShortcut()
+--- libxklavier/xklavier_xmm.c.orig	Tue Dec 13 00:58:22 2005
++++ libxklavier/xklavier_xmm.c	Tue Dec 13 00:58:47 2005
+@@ -66,7 +66,8 @@
+ XmmSwitchOptionPtr _XklXmmGetCurrentShortcut( void )
  {
    const char* optionName = _XklXmmGetCurrentShortcutOptionName();
+-  XmmSwitchOptionPtr switchOption = allSwitchOptions;
 +  XmmSwitchOptionPtr switchOption;
++  switchOption = allSwitchOptions;
    XklDebug( 150, "Configured switch option: [%s]\n", optionName );
    if( optionName == NULL )
      return NULL;
--  XmmSwitchOptionPtr switchOption = allSwitchOptions;
-+  switchOption = allSwitchOptions;
-   while( switchOption->optionName != NULL )
-   {
-     if( !strcmp( switchOption->optionName, optionName ) )
