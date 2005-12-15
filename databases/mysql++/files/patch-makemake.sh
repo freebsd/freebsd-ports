2698cde1d9797bdb8114604068ccdc750135c7e2
mnag <mnag@FreeBSD.org>
2005-12-15 18:41:41 +0000

    * databases/mysql++ Update to 2.0.7 Add SHA256 Add CONFLICTS
    * databases/mysql++1 [1]
    Add SHA256
    Add CONFLICTS
    Add NO_LATEST_LINK
    
    * databases/Makefile
    Add mysql++1
    
    PR:		89935
    Submitted by:	maintainer
    Repocopy by:	marcus [1]

---
+++ databases/mysql++/files/patch-makemake.sh
@@ -0,0 +1,8 @@
+--- makemake.sh.orig	Wed Nov  2 21:03:09 2005
++++ makemake.sh	Sat Dec 10 15:07:58 2005
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/usr/local/bin/bash
+ 
+ # Display usage message
+ function usage() {
