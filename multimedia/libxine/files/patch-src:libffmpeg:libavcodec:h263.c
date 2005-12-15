a26d950f79583d951342f1e7c5f81286554b5fb5
nobutaka <nobutaka@FreeBSD.org>
2005-12-15 17:19:47 +0000

    Fix build error on 5-stable.
    PR:		ports/90329
    Submitted by:	Heino Tiedemann <rotkaps_spam_trap@gmx.de>

--- multimedia/libxine/files/patch-src:libffmpeg:libavcodec:h263.c
+++ multimedia/libxine/files/patch-src:libffmpeg:libavcodec:h263.c
@@ -1,5 +1,5 @@
---- src/libffmpeg/libavcodec/h263.c.orig	Tue Dec 13 22:39:59 2005
-+++ src/libffmpeg/libavcodec/h263.c	Tue Dec 13 22:41:08 2005
+--- src/libffmpeg/libavcodec/h263.c.orig	Fri Dec 16 00:57:15 2005
++++ src/libffmpeg/libavcodec/h263.c	Fri Dec 16 01:00:23 2005
 @@ -66,8 +66,8 @@
  static int h263p_decode_umotion(MpegEncContext * s, int pred);
  static int h263_decode_block(MpegEncContext * s, DCTELEM * block,
@@ -11,3 +11,21 @@
                                int n, int coded, int intra, int rvlc);
  static int mpeg4_get_block_length(MpegEncContext * s, DCTELEM * block, int n, int intra_dc, 
                                 uint8_t *scan_table);
+@@ -4682,7 +4682,7 @@
+  * @param dir_ptr the prediction direction will be stored here
+  * @return the quantized dc
+  */
+-static inline int mpeg4_decode_dc(MpegEncContext * s, int n, int *dir_ptr)
++static int mpeg4_decode_dc(MpegEncContext * s, int n, int *dir_ptr)
+ {
+     int level, code;
+ 
+@@ -4727,7 +4727,7 @@
+  * decodes a block.
+  * @return <0 if an error occured
+  */
+-static inline int mpeg4_decode_block(MpegEncContext * s, DCTELEM * block,
++static int mpeg4_decode_block(MpegEncContext * s, DCTELEM * block,
+                               int n, int coded, int intra, int rvlc)
+ {
+     int level, i, last, run;
