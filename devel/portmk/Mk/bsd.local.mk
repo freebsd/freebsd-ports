1d5fc60b5efad3ea115943b0c05e7fe7399cf9a7
Mark Linimon <linimon@FreeBSD.org>
2005-12-16 02:11:14 +0000

    A sandbox for local modifications to bsd.port.mk.  This little bit of framework is designed so that such things as FreshPorts, portsmon, and the distfile survey can more easily maintain local changes without having to re-merge them into their copies of bsd.port.mk all the time.

---
+++ devel/portmk/Mk/bsd.local.mk
@@ -0,0 +1,33 @@
+# -*- mode: Makefile; tab-width: 4; -*-
+# ex: ts=4
+#
+# bsd.local.mk - Sandbox for local modification to ports framework.
+#
+# Created by: Mark Linimon <linimon@FreeBSD.org>
+#
+# $FreeBSD$
+#
+
+.if !defined(_POSTMKINCLUDED) && !defined(Local_Pre_Include)
+
+Local_Pre_Include=		bsd.local.mk
+
+#
+# here is where any code that needs to run at bsd.port.pre.mk inclusion
+# time should live.
+#
+
+.endif # !defined(_POSTMKINCLUDED) && !defined(Local_Pre_Include)
+
+.if defined(_POSTMKINCLUDED) && !defined(Local_Post_Include)
+
+Local_Post_Include=	bsd.local.mk
+
+#
+# here is where any code that needs to run at bsd.port.pre.mk inclusion
+# time should live.
+#
+
+.include "${DEVELPORTSDIR}/Mk/bsd.portsmon.mk"
+ 
+.endif # defined(_POSTMKINCLUDED) && !defined(Local_Post_Include)
