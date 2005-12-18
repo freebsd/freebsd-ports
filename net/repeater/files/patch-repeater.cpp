eaddfc38fdb0fc4547fdd808f81a5e0b1bc95b35
Yen-Ming Lee <leeym@FreeBSD.org>
2005-12-18 02:09:32 +0000

    Add repeater-0.08, the UltraVNC repeater which support both mode I and II.
    There are two modes available with the UltraVNC repeater:
    - mode I is used for UltraVNC Server and Viewer both in normal mode,
    - mode II works with the server in listening mode.
    
    WWW: http://ultravnc.sourceforge.net/addons/repeater.html

---
+++ net/repeater/files/patch-repeater.cpp
@@ -0,0 +1,10 @@
+--- repeater.cpp.orig	Wed Dec 14 14:49:03 2005
++++ repeater.cpp	Wed Dec 14 14:49:16 2005
+@@ -28,6 +28,7 @@
+ // Linux port (C) 2005 Jari Korhonen, jarit1.korhonen@dnainternet.net
+ // ///////////////////////////////////////////////////////////////////////////
+ 
++#include <%%LIMITS_H%%>
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <sys/stat.h>
---
+++ net/repeater/files/repeater.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# PROVIDE: repeater
+# REQUIRE: NETWORKING SERVERS
+# BEFORE: DAEMON
+# KEYWORD: FreeBSD shutdown
+
+#
+# Add the following lines to /etc/rc.conf to enable repeater:
+#
+# repeater_enable="YES"
+#
+
+. /etc/rc.subr
+
+name=repeater
+rcvar=`set_rcvar`
+
+load_rc_config $name
+
+repeater_enable=${repeater_enable:-"NO"}
+repeater_server_port=${repeater_server_port:-5500}
+repeater_viewer_port=${repeater_viewer_port:-5900}
+repeater_log=${repeater_log:-"/var/run/repeater.log"}
+command=/usr/local/sbin/repeater
+procname=repeater
+start_precmd="echo Starting ${procname}."
+start_cmd="daemon ${procname} ${repeater_viewer_port} ${repeater_server_port} > ${repeater_log} 2>&1"
+
+run_rc_command "$1"
