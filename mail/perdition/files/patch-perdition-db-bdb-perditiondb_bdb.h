ee8c2571761eb43f0fa778ffd98905b7625a468f
clement <clement@FreeBSD.org>
2005-12-19 10:06:19 +0000

    - Update perdition to 1.17 - Pass maintainership of perdition and libvanessa* to submitter
    Submitted by:		Thomas Vogt <thomas@bsdunix.ch>
    Also submitted by:	TAOKA Fumiyoshi <fmysh@iijmio-mail.jp> [1]
    PR:			ports/83305 [1]

--- mail/perdition/files/patch-perdition-db-bdb-perditiondb_bdb.h
+++ mail/perdition/files/patch-perdition-db-bdb-perditiondb_bdb.h
@@ -1,5 +1,5 @@
---- perdition/db/bdb/perditiondb_bdb.h.orig	Sun Jun 15 18:38:52 2003
-+++ perdition/db/bdb/perditiondb_bdb.h	Sun Jun 15 18:39:05 2003
+--- perdition/db/bdb/perditiondb_bdb.h.orig	Tue Dec  6 15:58:20 2005
++++ perdition/db/bdb/perditiondb_bdb.h	Tue Dec  6 15:58:34 2005
 @@ -29,7 +29,7 @@
  #include <unistd.h>
  #include <stdlib.h>
