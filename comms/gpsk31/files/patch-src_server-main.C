2a76b90808753b313cbd6eeaa4e17a7df8f85a89
erwin <erwin@FreeBSD.org>
2005-12-21 20:51:09 +0000

    - Update to 0.3
    New version new mastersite new maincoder
    
    PR:		90592
    Submitted by:	Soeren Straarup <xride@x12.dk> (maintainer)

--- comms/gpsk31/files/patch-src_server-main.C
+++ comms/gpsk31/files/patch-src_server-main.C
@@ -1,10 +1,10 @@
---- src/server-main.C.orig	Sun May  8 13:28:03 2005
-+++ src/server-main.C	Sun May  8 13:28:18 2005
-@@ -19,6 +19,7 @@
-  *   thread and from user interface thread (==interface functions)
+--- src/server-main.C.orig	Wed Nov  2 09:54:12 2005
++++ src/server-main.C	Sun Dec 18 18:37:29 2005
+@@ -20,7 +20,6 @@
   */
  
-+#include <sys/types.h>
- #include <sys/mman.h>
- #include "config.h"
+ 
+-#include <sys/mman.h>
  #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
