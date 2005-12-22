4bce12b760a734d0aa563d7ab17f42cd092fd9d9
lioux <lioux@FreeBSD.org>
2005-12-22 17:19:16 +0000

    New port ta2as version 0.8: TASM to AT&T asm syntax converter (GNU AS)

---
+++ converters/ta2as/files/patch-ta2as.c
@@ -0,0 +1,10 @@
+--- ta2as.c.orig	Thu Dec 22 14:30:47 2005
++++ ta2as.c	Thu Dec 22 14:29:17 2005
+@@ -5,6 +5,7 @@
+ #include "ta2as.h"
+ #include <stdlib.h>
+ #include <string.h>
++#include <strings.h>
+ 
+ char rarray[38][4]=
+ {
