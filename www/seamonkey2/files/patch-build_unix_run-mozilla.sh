2530ce59ff0749ac57c559b5c4370b7610994330
ahze <ahze@FreeBSD.org>
2005-12-23 00:04:18 +0000

    - Update to 1.0b - Enable <canvas> support
    Release notes:	http://www.mozilla.org/projects/seamonkey/releases/seamonkey1.0b

--- www/seamonkey2/files/patch-build_unix_run-mozilla.sh
+++ www/seamonkey2/files/patch-build_unix_run-mozilla.sh
@@ -1,15 +1,15 @@
---- build/unix/run-mozilla.sh.orig	Mon Oct 17 13:21:17 2005
-+++ build/unix/run-mozilla.sh	Thu Nov 10 22:01:41 2005
-@@ -333,7 +333,7 @@
- ## LD_LIBRARY_PATH because $ORIGIN doesn't work on a symbolic link.
- if [ `uname -s` != "SunOS" -o -h "$MOZ_PROGRAM" ]
- then
--	LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
-+	LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%PREFIX%%/lib/browser_plugins:%%PREFIX%%/lib/browser_linux_plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
- fi 
- 
+--- build/unix/run-mozilla.sh.orig	Sat Oct  1 01:36:47 2005
++++ build/unix/run-mozilla.sh	Thu Dec 22 17:51:10 2005
+@@ -328,7 +328,7 @@
+ fi
+ ##
+ ## Set LD_LIBRARY_PATH
+-LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
++LD_LIBRARY_PATH=${MOZ_DIST_BIN}:%%PREFIX%%/lib/browser_plugins:%%PREFIX%%/lib/browser_linux_plugins:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
  if [ -n "$LD_LIBRARYN32_PATH" ]
-@@ -422,6 +422,9 @@
+ then
+ 	LD_LIBRARYN32_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARYN32_PATH+":$LD_LIBRARYN32_PATH"}
+@@ -415,6 +415,9 @@
  #
  export MOZILLA_FIVE_HOME LD_LIBRARY_PATH
  export SHLIB_PATH LIBPATH LIBRARY_PATH ADDON_PATH DYLD_LIBRARY_PATH
