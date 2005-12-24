19351dbafcb33666ce6c1e55c6354de826e574dd
Ade Lovett <ade@FreeBSD.org>
2005-12-24 20:15:41 +0000

    Fix pthread handling
    Submitted by:	marcus

--- devel/libtool15/files/patch-ltmain.sh
+++ devel/libtool15/files/patch-ltmain.sh
@@ -1,10 +1,14 @@
---- ltmain.sh.orig	Mon May 16 02:39:29 2005
-+++ ltmain.sh	Thu Jun 16 19:42:32 2005
-@@ -1525,2 +1525,3 @@
+--- ltmain.sh.orig	Sun Dec 18 17:14:54 2005
++++ ltmain.sh	Sat Dec 24 12:22:07 2005
+@@ -1604,11 +1604,18 @@
+ 	compiler_flags="$compiler_flags $arg"
+ 	compile_command="$compile_command $arg"
  	finalize_command="$finalize_command $arg"
 +	deplibs="$deplibs $arg"
  	continue
-@@ -1530,2 +1531,8 @@
+ 	;;
+ 
+       -module)
  	module=yes
 +	case $host in
 +	*-*-freebsd*)
@@ -13,7 +17,11 @@
 +	  ;;
 +	esac
  	continue
-@@ -2015,2 +2022,25 @@
+ 	;;
+ 
+@@ -2101,6 +2108,29 @@
+ 	  else
+ 	    compiler_flags="$compiler_flags $deplib"
  	  fi
 +
 +	  case $linkmode in
@@ -39,13 +47,21 @@
 +	  esac # linkmode
 +
  	  continue
-@@ -4498,2 +4528,5 @@
+ 	  ;;
+ 	-l*)
+@@ -4705,6 +4735,9 @@
+             ;;
+           esac
  	  ;;
 +	*-*-freebsd*)
 +	  # FreeBSD doesn't need this...
 +	  ;;
  	*)
-@@ -5627,6 +5660,13 @@
+ 	  $echo "$modename: unknown suffix for \`$dlsyms'" 1>&2
+ 	  exit $EXIT_FAILURE
+@@ -5995,10 +6028,17 @@
+ 	fi
+ 
  	# Install the pseudo-library for information purposes.
 -	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
 -	instname="$dir/$name"i
@@ -63,3 +79,5 @@
 +	  ;;
 +	esac
  
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
