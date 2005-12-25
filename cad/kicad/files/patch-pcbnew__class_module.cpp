42b14a6224caaa0545b069006e2d1630637ec385
thierry <thierry@FreeBSD.org>
2005-12-25 18:08:35 +0000

    Upgrade to 2005-12-22.
    Changelog at <http://iut-tice.ujf-grenoble.fr/cao/news.txt>.

--- cad/kicad/files/patch-pcbnew__class_module.cpp
+++ cad/kicad/files/patch-pcbnew__class_module.cpp
@@ -1,9 +1,9 @@
---- ./pcbnew/class_module.cpp.orig	Fri Sep  9 12:22:06 2005
-+++ ./pcbnew/class_module.cpp	Thu Dec  1 23:15:31 2005
-@@ -1017,7 +1017,7 @@
+--- ./pcbnew/class_module.cpp.orig	Mon Dec  5 09:47:56 2005
++++ ./pcbnew/class_module.cpp	Sun Dec 25 16:55:30 2005
+@@ -1020,7 +1020,7 @@
  	pos += 14;
  	if ( flag )	// Affichage date de modification (utile en Module Editor)
- 		{
+ 	{
 -		strcpy(Line, ctime(&m_LastEdit_Time));
 +		strcpy(Line, ctime((time_t*) &m_LastEdit_Time));
  		strtok(Line," \n\r");
