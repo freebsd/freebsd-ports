574798d51421a87beade73d9e5a040aebcb637d5
Alex Dupre <ale@FreeBSD.org>
2005-12-25 18:31:58 +0000

    Fix plist when CATEGORY has a subcategory.

--- devel/pear/bsd.pear.mk
+++ devel/pear/bsd.pear.mk
@@ -130,6 +130,7 @@ do-generate-plist:
 	if [ -n "${EXAMPLES}" ]; then echo "%%PORTDOCS%%@dirrm ${LEXAMPLESDIR}"; fi; \
 	echo "@dirrm ${LPKGREGDIR}"; \
 	if [ -n "${CATEGORY}" ]; then echo "@unexec rmdir %D/${LINSTDIR} 2> /dev/null || true"; fi; \
+	if [ -n "${CATEGORY:M*/*}" ]; then echo "@unexec rmdir %D/${LINSTDIR:H} 2> /dev/null || true"; fi; \
 	echo "@unexec rmdir %D/${LPKGREGDIR:H} 2> /dev/null || true"; \
 	echo "@unexec rmdir %D/${LDOCSDIR:H} 2> /dev/null || true"; \
 	echo "@unexec rmdir %D/${LEXAMPLESDIR:H} 2> /dev/null || true"; \
