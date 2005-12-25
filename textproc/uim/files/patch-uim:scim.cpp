a2f49f641523de8502f3d95f0919aa5b7e3aea25
nobutaka <nobutaka@FreeBSD.org>
2005-12-25 05:47:34 +0000

    - Update to 1.0.1. - Use OPTIONS. - Make dependencies on anthy and m17nlib optional.

--- textproc/uim/files/patch-uim:scim.cpp
+++ textproc/uim/files/patch-uim:scim.cpp
@@ -1,6 +1,14 @@
---- uim/scim.cpp.orig	Wed Sep 21 00:27:36 2005
-+++ uim/scim.cpp	Wed Sep 21 00:29:33 2005
-@@ -175,7 +175,7 @@
+--- uim/scim.cpp.orig	Wed Dec  7 21:39:32 2005
++++ uim/scim.cpp	Sat Dec 24 04:13:02 2005
+@@ -43,7 +43,6 @@
+ #include "uim.h"
+ #include "uim-scm.h"
+ #include "uim-compat-scm.h"
+-#include "context.h"
+ 
+ using namespace scim;
+ 
+@@ -175,7 +174,7 @@
              return uim_scm_f();
          }
  
