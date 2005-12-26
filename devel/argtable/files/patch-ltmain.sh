3da9887623d761d7caa410041fe18a86c943edf8
ijliao <ijliao@FreeBSD.org>
2005-12-26 01:33:20 +0000

    upgrade to 2.5

---
+++ devel/argtable/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Mon Dec 26 09:27:12 2005
++++ ltmain.sh	Mon Dec 26 09:27:43 2005
+@@ -5551,10 +5551,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
