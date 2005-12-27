701efadf6ce14cf6191c5ee380277a50e9f5c645
pav <pav@FreeBSD.org>
2005-12-27 02:40:49 +0000

    - Update to 1.2.0
    PR:		ports/90933
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- net/linphone-base/files/patch-coreapi::linphonecore.c
+++ net/linphone-base/files/patch-coreapi::linphonecore.c
@@ -1,6 +1,6 @@
---- coreapi/linphonecore.c.orig	Thu Mar 17 01:16:55 2005
-+++ coreapi/linphonecore.c	Wed Mar 23 13:17:30 2005
-@@ -34,9 +34,7 @@
+--- coreapi/linphonecore.c.orig	Thu Dec  8 02:37:31 2005
++++ coreapi/linphonecore.c	Tue Dec 20 13:24:10 2005
+@@ -36,9 +36,7 @@
  #include <arpa/inet.h>
  #include <time.h>
  
@@ -10,3 +10,17 @@
  
  #include "enum.h"
  
+@@ -1225,12 +1223,12 @@
+ {
+     char *real_url=NULL;
+     osip_to_t *real_parsed_url=NULL;
++    LinphoneCall *call=lc->call;
+     if (!linphone_core_interpret_url(lc,url,&real_url,&real_parsed_url)){
+         /* bad url */
+         return -1;
+     }
+ 
+-    LinphoneCall *call=lc->call;
+     if (call==NULL){
+         g_warning("No established call to refer.");
+         return -1;
