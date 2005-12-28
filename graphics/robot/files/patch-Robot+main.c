329383b49524108d86c642503dd52caa665d3989
pav <pav@FreeBSD.org>
2005-12-28 16:09:24 +0000

    - Update to a new patchlevel (sources were checked)

--- graphics/robot/files/patch-Robot+main.c
+++ graphics/robot/files/patch-Robot+main.c
@@ -1,6 +1,6 @@
---- Robot/main.c.orig	Sat Oct 11 13:14:19 2003
-+++ Robot/main.c	Sat Oct 11 13:22:51 2003
-@@ -539,20 +539,19 @@
+--- Robot/main.c.orig	Tue May  3 17:14:51 2005
++++ Robot/main.c	Wed Dec 28 17:02:15 2005
+@@ -561,19 +561,19 @@
  			 || streq(argv[i], "--help") ){ 
  			printf("Robot version %-1.2f patch  %d\n\n",
  				VERSION, PATCHLEVEL);
@@ -14,22 +14,22 @@
 --v		   (print version number and quit)\n\
 --help/-h           (prints this information and quits)\n\
 -(optional command file name)\n\n\
-+			printf("robot (generic-tool-options)\n");
-+printf("-arraysize [size of arrays, default is %d]\n");
-+printf("-plotscale [scale factor]\n");
-+printf("-portrait\n");
-+printf("-landscape         (default)\n");
-+printf("-notol             (not Open-look - adds some extra quit buttons)\n");
-+printf("-stdin             (reads commands from standard input instead of GUI)\n");
-+printf("-v		   (print version number and quit)\n");
-+printf("-help/-h           (prints this information and quits)\n");
-+printf("(optional command file name)\n\n");
- 
 -For more information see help available in the program under\n\
 -the \"About\" option or visit:\n\
 -%s\n", msize, WEB_HELP);
-+printf("For more information see help available in the program under\n");
-+printf("the \"About\" option or visit:\n\%s\n", msize, WEB_HELP);
++			printf("robot (generic-tool-options)\n");
++			printf("-arraysize [size of arrays, default is %d]\n");
++			printf("-plotscale [scale factor]\n");
++			printf("-portrait\n");
++			printf("-landscape         (default)\n");
++			printf("-notol             (not Open-look - adds some extra quit buttons)\n");
++			printf("-stdin             (reads commands from standard input instead of GUI)\n");
++			printf("-v		   (print version number and quit)\n");
++			printf("-help/-h           (prints this information and quits)\n");
++			printf("(optional command file name)\n\n");
++			printf("For more information see help available in the program under\n");
++			printf("the \"About\" option or visit:\n");
++			printf("%s\n", msize, WEB_HELP);
  			fstop_();
  		}
  
