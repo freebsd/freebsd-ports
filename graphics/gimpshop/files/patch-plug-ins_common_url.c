90969ea0fdaca5ae7e5dc1910d0a0dd59a6fa0d3
adamw <adamw@FreeBSD.org>
2005-12-28 19:35:45 +0000

    Update to 2.2.10. Works with GNOME 2.15 too!

--- graphics/gimpshop/files/patch-plug-ins_common_url.c
+++ graphics/gimpshop/files/patch-plug-ins_common_url.c
@@ -1,13 +1,17 @@
---- plug-ins/common/url.c.orig	Mon Mar 14 05:00:40 2005
-+++ plug-ins/common/url.c	Sun Apr 10 11:45:50 2005
-@@ -186,8 +186,8 @@
+--- plug-ins/common/url.c.orig	Wed Dec 28 13:56:48 2005
++++ plug-ins/common/url.c	Wed Dec 28 13:58:33 2005
+@@ -186,10 +186,10 @@
        putenv ("LANG=C");
  #endif
  
--      execlp ("wget", "wget", "-e", "server-response=off", "-T", TIMEOUT,
+-      execlp ("wget",
+-              "wget", "-v", "-e", "server-response=off", "-T", TIMEOUT,
 -              filename, "-O", tmpname, NULL);
-+      execlp ("fetch", "fetch", "-T", TIMEOUT, filename, "-p", "-o", 
-+              tmpname, NULL);
-       g_message ("exec() failed: wget: %s", g_strerror (errno));
+-      g_message ("exec() failed: wget: %s", g_strerror (errno));
++      execlp ("fetch",
++              "fetch", "-T", TIMEOUT,
++              filename, "-p", "-o", tmpname, NULL);
++      g_message ("exec() failed: fetch: %s", g_strerror (errno));
        g_free (tmpname);
        _exit (127);
+     }
