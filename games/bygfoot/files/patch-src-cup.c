7783b0d444aaec3aceea808557a3ef9b3c9c6fa3
pav <pav@FreeBSD.org>
2005-12-29 21:13:42 +0000

    - Update to 1.9.4

--- games/bygfoot/files/patch-src-cup.c
+++ games/bygfoot/files/patch-src-cup.c
@@ -1,6 +1,6 @@
---- src/cup.c.orig	Sat Sep 10 11:30:49 2005
-+++ src/cup.c	Sat Oct  8 00:02:13 2005
-@@ -269,6 +269,7 @@
+--- src/cup.c.orig	Thu Dec 22 10:44:34 2005
++++ src/cup.c	Thu Dec 29 21:32:04 2005
+@@ -307,6 +307,7 @@
  	    start = ct->start_idx - 1;
  	    end = ct->end_idx - start;
  
@@ -8,7 +8,7 @@
  	    gint order[end];
  	    for(j=0;j<end;j++)
  		order[j] = j + start;
-@@ -301,6 +302,7 @@
+@@ -340,6 +341,7 @@
  			break;
  		}
  	    }
@@ -16,33 +16,33 @@
  	}
  
  	if(ct->number_of_teams != -1 &&
-@@ -322,6 +324,7 @@
- 		main_exit_program(EXIT_CHOOSE_TEAM_ERROR, NULL);
- 	    }
+@@ -359,6 +361,7 @@
+ 				  ct->sid, lig(0).teams->len, 
+ 				  ct->number_of_teams, cup->name);
  
 +	    {
  	    gint permutation[lig(0).teams->len];
  	    math_generate_permutation(permutation, 0, lig(0).teams->len - 1);
  
-@@ -349,6 +352,7 @@
- 			  ct->sid->str, number_of_teams, ct->number_of_teams, cup->name->str, cup->group);
- 		main_exit_program(EXIT_CHOOSE_TEAM_ERROR, NULL);
- 	    }
+@@ -385,6 +388,7 @@
+ 				  "cup_load_choose_team (2): not enough teams found in league 0 for chooseteam %s (%d; required: %d) in cup %s (group %d)\n",
+ 				  ct->sid, number_of_teams, 
+ 				  ct->number_of_teams, cup->name, cup->group);
 +	    }
  	}
  	else
  	{
-@@ -431,6 +435,7 @@
- 
-     g_array_free(leagues, TRUE);
+@@ -475,6 +479,7 @@
+ 	return;
+     }
      
 +    {
      gint permutation[teams_local->len];
      for(j=0;j<teams_local->len;j++)
  	permutation[j] = j;
-@@ -488,6 +493,7 @@
+@@ -532,6 +537,7 @@
+ 	    g_array_remove_index(teams_local, j);
  	
-     free_g_string_array(&sids);
      free_teams_array(&teams_local, FALSE);
 +    }
  }
