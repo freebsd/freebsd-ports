13ca38ce4853bad199f817e409485a14a635dbe1
Edwin Groothuis <edwin@FreeBSD.org>
2005-12-29 05:44:05 +0000

    japanese/lipsf cannot be built
    	building japanese/lipsf causes core dump on amd64.
    	this is because of the lack of inclusion of header file.
    
    PR:		ports/90960
    Submitted by:	Tsurutani Naoki <turutani@scphys.kyoto-u.ac.jp>

---
+++ japanese/lipsf/files/patch-mkmfsed.c
@@ -0,0 +1,10 @@
+--- mkmfsed.c.orig      Tue May 28 00:00:00 2002
++++ mkmfsed.c   Tue Dec 27 14:51:35 2005
+@@ -5,6 +5,7 @@
+ */
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include "machine.h"
+ 		
+ #ifdef NOVOID
