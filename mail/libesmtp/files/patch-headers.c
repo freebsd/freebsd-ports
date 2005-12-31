e8900feb9880d5ff5b5cea36f37cf5060d8d82ce
mnag <mnag@FreeBSD.org>
2005-12-31 02:28:24 +0000

    Update to 1.0.4 [1] Add OPTIONS to disable OPENSSL and enable DEBUG
    PR:		91109 [1]
    Submitted by:	Babak Farrokhi <babak@farrokhi.net> [1]

---
+++ mail/libesmtp/files/patch-headers.c
@@ -0,0 +1,10 @@
+--- headers.c.orig	Thu Dec 15 18:52:01 2005
++++ headers.c	Fri Dec 30 23:51:37 2005
+@@ -33,6 +33,7 @@
+ #include <unistd.h>
+ #include <time.h>
+ #include <errno.h>
++#include <sys/time.h>
+ 
+ #include <missing.h>
+ 
