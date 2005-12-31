92eaeee9b4825e756cad08a3219d67a291d5a1f4
Emanuel Haupt <ehaupt@FreeBSD.org>
2005-12-31 04:28:08 +0000

    - Update to 1.6.40 - Be more picky about whats PORTDOCS and what not - Moved john.conf from ${DATADIR} to ${PREFIX}/etc - Preserve existing/changed config file on install/deinstall - Remove files/patch-john.conf

--- security/john/files/patch-params.h
+++ security/john/files/patch-params.h
@@ -1,6 +1,6 @@
---- params.h.orig	Mon Feb 23 17:07:38 2004
-+++ params.h	Sun Apr 18 19:16:14 2004
-@@ -22,12 +22,12 @@
+--- params.h.orig	Sat Dec 31 04:21:22 2005
++++ params.h	Sat Dec 31 04:25:13 2005
+@@ -22,15 +22,15 @@
   * will probably want to set this to 1 for their builds of John.
   */
  #ifndef JOHN_SYSTEMWIDE
@@ -9,30 +9,33 @@
  #endif
  
  #if JOHN_SYSTEMWIDE
+ #ifndef JOHN_SYSTEMWIDE_EXEC
 -#define JOHN_SYSTEMWIDE_EXEC		"/usr/libexec/john"
--#define JOHN_SYSTEMWIDE_HOME		"/usr/share/john"
 +#define JOHN_SYSTEMWIDE_EXEC		"%%PREFIX%%/bin/john"
-+#define JOHN_SYSTEMWIDE_HOME		"%%PREFIX%%/share/john"
+ #endif
+ #ifndef JOHN_SYSTEMWIDE_HOME
+-#define JOHN_SYSTEMWIDE_HOME		"/usr/share/john"
++#define JOHN_SYSTEMWIDE_HOME		"%%DATADIR%%"
+ #endif
  #define JOHN_PRIVATE_HOME		"~/.john"
  #endif
- 
-@@ -67,8 +67,8 @@
+@@ -74,8 +74,8 @@
  /*
   * File names.
   */
 -#define CFG_FULL_NAME			"$JOHN/john.conf"
 -#define CFG_ALT_NAME			"$JOHN/john.ini"
-+#define CFG_FULL_NAME		"%%PREFIX%%/share/john/john.conf"
-+#define CFG_ALT_NAME		"%%PREFIX%%/share/john/john.ini"
++#define CFG_FULL_NAME			"%%PREFIX%%/etc/john.conf"
++#define CFG_ALT_NAME			"%%PREFIX%%/etc/john.ini"
  #if JOHN_SYSTEMWIDE
  #define CFG_PRIVATE_FULL_NAME		JOHN_PRIVATE_HOME "/john.conf"
  #define CFG_PRIVATE_ALT_NAME		JOHN_PRIVATE_HOME "/john.ini"
-@@ -82,7 +82,7 @@
+@@ -89,7 +89,7 @@
  #endif
  #define LOG_SUFFIX			".log"
  #define RECOVERY_SUFFIX			".rec"
 -#define WORDLIST_NAME			"$JOHN/password.lst"
-+#define WORDLIST_NAME		"%%PREFIX%%/share/john/password.lst"
++#define WORDLIST_NAME			"%%DATADIR%%/password.lst"
  
  /*
   * Configuration file section names.
