73810d4c5bf1d9576f30ae3cd17fc28e7a8c63a5
Edwin Groothuis <edwin@FreeBSD.org>
2006-01-02 05:24:46 +0000

    fix building: - support wish8.4 - supoprt iwidgets4.0.1

--- net/smm++/files/patch-itk.sh
+++ net/smm++/files/patch-itk.sh
@@ -5,7 +5,7 @@
  
  # all possible itkwish executables 
 -itkarray="itkwish3.1 itkwish3 itkwish30 itkwish3.0 itkwish" 
-+itkarray="itkwish3.1 itkwish3 itkwish30 itkwish3.0 itkwish wish8.3 wish8.2"
++itkarray="itkwish3.1 itkwish3 itkwish30 itkwish3.0 itkwish wish8.4 wish8.3 wish8.2"
  
  set $itkarray 
  while [ ! -z "$1" ]; do 
