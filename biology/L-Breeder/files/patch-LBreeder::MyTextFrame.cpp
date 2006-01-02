8871985be6f70555d8bd59000d2b2ef74ecdc67e
edwin <edwin@FreeBSD.org>
2006-01-02 21:29:30 +0000

    possible fix to unbreak biology/L-Breeder
    	L-Breeder is currently marked unfetchable:
    	http://people.freebsd.org/~fenner/portsurvey/biology.html#L-Breeder
    
    	So I tried to investigate the problem and I'm now submitting
    	my homework so it doesn't get lost.
    
    	This is what I noticed:
    
    	i) L-Breeder upstream sources have "stagnated for some time,
    	but now a new simplified version is available" (sic, from
    	the web site)
    
    	So, the fetch issue is resolved. And the pkg-descr is also
    	updated to reflect the new home page. However:
    
    	ii) L-Breeder is now called LBreeder. I have changed PORTNAME
    	to reflect this, although I don't know if this is the right
    	thing to do. Should the port be moved to biology/LBreeder?
    	Or perhaps just leaving PORTNAME pointing to the old name
    	and then work inside the Makefile to point the build/install
    	to the correct name?
    
    	iii) the sources have been reorganized so I have to update
    	the port's Makefile to reflect this. The attached patch
    	works for me under FreeBSD-4.11 (although it's possible
    	that I may have missed a dependency if I already have a
    	required library installed in my box. Sorry, I don't have
    	a tinderbox-like setup to test builds)
    
    	iv) the program does not have a version number, nor did I
    	find one by looking inside the sources. To avoid changing
    	too much the port I just left PORTVERSION at 1.0 and bumped
    	PORTREVISION to 6
    
    	Finally, do note that I am not a user of LBreeder, so I
    	cannot test the program myself. I just noticed that by
    	calling the executable I get a black X screen, that I can
    	quit by pressing 'q' as documented in Readme.txt, but I
    	can't tell myself if this is the expected behaviour. Nor
    	do I know how to use the example files. Caveat emptor.
    
    PR:		ports/90073
    Submitted by:	Fernan Aguero <fernan@iib.unsam.edu.ar>
    Approved by:	maintainer timeout

--- biology/L-Breeder/files/patch-LBreeder::MyTextFrame.cpp
+++
@@ -1,9 +0,0 @@
---- LBreeder/MyTextFrame.cpp.orig	Wed Aug 16 22:08:11 2000
-+++ LBreeder/MyTextFrame.cpp	Wed Nov  5 01:03:54 2003
-@@ -1,5 +1,5 @@
--#include "wx/filedlg.h"
- #include "MyTextFrame.h"
-+#include "wx/filedlg.h"
- #include "Ltypes.h" //for debugging messages
- 
- BEGIN_EVENT_TABLE(MyTextFrame, wxDialog)
