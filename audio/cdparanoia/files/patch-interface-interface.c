76fda451b5fa9a570cd6c4970c14788e4232bf9a
Markus Brueffer <markus@FreeBSD.org>
2006-01-07 13:34:49 +0000

    Fix potential mini-memleak

--- audio/cdparanoia/files/patch-interface-interface.c
+++ audio/cdparanoia/files/patch-interface-interface.c
@@ -1,17 +1,10 @@
-Index: interface/interface.c
-===================================================================
-RCS file: /home/cvs/cdparanoia/interface/interface.c,v
-retrieving revision 1.1.1.1
-retrieving revision 1.3
-diff -u -r1.1.1.1 -r1.3
---- interface/interface.c	2003/01/05 09:46:26	1.1.1.1
-+++ interface/interface.c	2003/01/06 21:26:23	1.3
-@@ -29,12 +29,25 @@
-       d->enable_cdda(d,0);
+--- interface/interface.c.orig	Thu Apr 20 00:41:04 2000
++++ interface/interface.c	Sat Jan  7 14:31:19 2006
+@@ -30,11 +30,24 @@
  
      _clean_messages(d);
-+#ifdef Linux
      if(d->cdda_device_name)free(d->cdda_device_name);
++#ifdef Linux
      if(d->ioctl_device_name)free(d->ioctl_device_name);
      if(d->drive_model)free(d->drive_model);
      if(d->cdda_fd!=-1)close(d->cdda_fd);
