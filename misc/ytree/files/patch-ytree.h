982428a538be2406fc4cb242d4e51822f2d463ee
Jimmy Olgeni <olgeni@FreeBSD.org>
2006-01-07 10:35:53 +0000

    Upgrade to version 1.86.

---
+++ misc/ytree/files/patch-ytree.h
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- ytree.h.orig
++++ ytree.h
+@@ -14,6 +14,7 @@
+ #include <stdio.h>
+ #include <ctype.h>
+ #include <math.h>
++#include <locale.h>
+ 
+ #ifdef XCURSES
+ #include <xcurses.h>
