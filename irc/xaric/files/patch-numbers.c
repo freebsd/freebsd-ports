7dfb23b93d6e69bea81a577dfa3c35a209603d45
Edwin Groothuis <edwin@FreeBSD.org>
2006-01-08 11:32:35 +0000

    Make irc/xaric buildable on 4.x and 6.x.

--- irc/xaric/files/patch-numbers.c
+++ irc/xaric/files/patch-numbers.c
@@ -1,11 +1,19 @@
---- numbers.c.orig	Sun Jan  8 07:57:54 2006
-+++ numbers.c	Sun Jan  8 07:58:02 2006
-@@ -986,7 +986,7 @@
+--- numbers.c.orig	Wed Nov 30 11:06:26 2005
++++ numbers.c	Sun Jan  8 22:22:04 2006
+@@ -980,14 +980,14 @@
+ 
+ 	case 329:		/* #define CREATION_TIME 329 */
+ 	    {
+-		time_t tme;
++		unsigned long tme;
+ 		char *this_sucks;
+ 		struct channel *chan = NULL;
  
  		if (!ArgList[1] || !*ArgList[1])
  		    break;
--		sscanf(ArgList[1], "%lu", &tme);
-+		sscanf(ArgList[1], "%u", &tme);
- 		this_sucks = ctime(&tme);
+ 		sscanf(ArgList[1], "%lu", &tme);
+-		this_sucks = ctime(&tme);
++		this_sucks = ctime((time_t *)&tme);
  		this_sucks[strlen(this_sucks) - 1] = '\0';
  
+ 		message_from(ArgList[0], LOG_CRAP);
