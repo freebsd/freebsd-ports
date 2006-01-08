6e02c65f4b99e973b1dab0645ed8a79b19b9c628
erwin <erwin@FreeBSD.org>
2006-01-08 17:37:25 +0000

    Update to 2.8.3
    PR:		91515
    Submitted by:	Christian Laursen <xi@borderworlds.dk> (maintainer)

--- net/nbd-server/files/patch-nbd-server.c
+++ net/nbd-server/files/patch-nbd-server.c
@@ -1,7 +1,7 @@
-diff -urN nbd-2.8.2.orig/nbd-server.c nbd-2.8.2/nbd-server.c
---- nbd-2.8.2.orig/nbd-server.c	Wed Nov  9 22:38:44 2005
-+++ nbd-server.c	Thu Dec 22 16:04:47 2005
-@@ -363,11 +363,11 @@
+diff -urN nbd-2.8.3.orig/nbd-server.c nbd-2.8.3/nbd-server.c
+--- nbd-2.8.3.orig/nbd-server.c	Thu Dec 22 21:37:20 2005
++++ nbd-server.c	Sun Jan  8 16:45:30 2006
+@@ -363,12 +363,12 @@
   * is severely wrong)
   **/
  void sigchld_handler(int s) {
@@ -9,18 +9,10 @@ diff -urN nbd-2.8.2.orig/nbd-server.c nbd-2.8.2/nbd-server.c
 +        int status;
  	int* i;
  	pid_t pid;
+ 	int done=0;
  
--	while((pid=wait(status)) > 0) {
-+	while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+-	while(!done && (pid=wait(status)) > 0) {
++	while(!done && (pid=wait(&status)) > 0) {
  		if(WIFEXITED(status)) {
  			msg3(LOG_INFO, "Child exited with %d", WEXITSTATUS(status));
- 		}
-@@ -684,7 +684,7 @@
- 
- 		if (request.magic != htonl(NBD_REQUEST_MAGIC))
- 			err("Not enough magic.");
--		if (len > BUFSIZE)
-+		if (len > (BUFSIZE-sizeof(struct nbd_reply)))
- 			err("Request too big!");
- #ifdef DODBG
- 		printf("%s from %Lu (%Lu) len %d, ", request.type ? "WRITE" :
+ 			msg3(LOG_INFO, "pid is %d", pid);
