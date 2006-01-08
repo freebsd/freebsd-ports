6ed17593f36c201df3d4164af15401d658f1b8ab
Edwin Groothuis <edwin@FreeBSD.org>
2006-01-08 21:48:57 +0000

    [NEW PORT] x11/slim - a login manager derived from login.app
    	SLiM is a desktop-independent graphical login manager for
    	X11, derived from Login.app by Per Liden.
    
    	It aims to be light and simple, although completely
    	configurable through themes and an option file; is suitable
    	for machines on which remote login functionalities are not
    	needed.
    
    	Author: Simone Rota and Johannes Winkelmann
    	WWW: http://slim.berlios.de/
    
    PR:		ports/91511
    Submitted by:	Tobias Roth <ports@fsck.ch>

---
+++ x11/slim/files/patch-panel.cpp
@@ -0,0 +1,10 @@
+--- panel.cpp.orig	Thu May 19 22:46:22 2005
++++ panel.cpp	Thu May 19 22:47:12 2005
+@@ -10,6 +10,7 @@
+ */
+ 
+ #include <sstream>
++#include <libgen.h>
+ #include "panel.h"
+ 
+ using namespace std;
