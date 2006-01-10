eaf079bb204b41d8b1e1dd1df21c23c710d35fe3
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2006-01-10 00:54:23 +0000

    o Fix build on FreeBSD 4.x branch o Some code tidying up through better patching
    PR:		91485
    Submitted by:	Tod McQuillin <devin@spamcop.net>

--- sysutils/vobcopy/files/patch-vobcopy.h
+++ sysutils/vobcopy/files/patch-vobcopy.h
@@ -1,6 +1,23 @@
---- vobcopy.h.orig	Fri Jan  6 23:22:11 2006
-+++ vobcopy.h	Fri Jan  6 23:34:11 2006
-@@ -67,24 +67,33 @@
+--- vobcopy.h.orig	Wed Dec  7 21:32:10 2005
++++ vobcopy.h	Sun Jan  8 15:26:43 2006
+@@ -19,7 +19,6 @@
+ 
+ #if ( defined( __unix__ ) || defined( unix )) && !defined( USG )
+ #include <sys/param.h>
+-#else
+ #endif
+ 
+ #if defined( __GNUC__ ) && \
+@@ -32,6 +31,8 @@
+ /* by some bugreport:*/
+ #if !( defined( BSD ) && ( BSD >= 199306 ) ) && !defined( sun )
+ #include <stdint.h>
++#else
++#include <inttypes.h>
+ #endif
+ 
+ /*for/from play_title.c*/
+@@ -67,24 +68,23 @@
  /* //////////  *BSD //////////  */
  #if ( defined( BSD ) && ( BSD >= 199306 ) )
  
@@ -9,11 +26,7 @@
 -#include <sys/mount.h>
 -#define USE_STATFS 1
 +#  if ( defined( __NetBSD__) && ( __NetBSD_Version__ < 200040000 ) ) || \
-+      ( defined( __FreeBSD__) && \
-+	( ((__FreeBSD_version >= 460102 ) && (__FreeBSD_version < 500000)) || \
-+	  ( __FreeBSD_version >= 500040) \
-+	) \
-+      )
++      ( defined( __FreeBSD__) && ( __FreeBSD_version >= 500040 ) )
 +#include <sys/statvfs.h>
  
  #  else
@@ -25,19 +38,15 @@
  #  endif
  
 -#  if defined(NetBSD)
+-
+-#include <sys/param.h>
 +#  if defined(__NetBSD__) || defined(__FreeBSD__)
- 
- #include <sys/param.h>
  
  #define USE_GETMNTINFO
  
 -#    if ( __NetBSD_Version__ < 200040000 )
 +#    if (defined( __NetBSD__ ) && ( __NetBSD_Version__ < 200040000 )) || \
-+        ( defined( __FreeBSD__) && \
-+	  ( ((__FreeBSD_version >= 460102 ) && (__FreeBSD_version < 500000)) || \
-+	    ( __FreeBSD_version >= 500040) \
-+	  ) \
-+	)
++        (defined( __FreeBSD__ ))
  
  #include <sys/mount.h>
  #define USE_STATFS_FOR_DEV
