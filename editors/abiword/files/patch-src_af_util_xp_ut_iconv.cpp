75bdefb5cede7dc5cc5fdbeb9ebdd949a0805737
mezz <mezz@FreeBSD.org>
2006-01-10 07:01:47 +0000

    Update to 2.4.2, see the release notes and changelog for details.
    	http://www.abisource.com/release-notes/2.4.2.phtml
    	http://www.abisource.com/changelogs/2.4.2.phtml

--- editors/abiword/files/patch-src_af_util_xp_ut_iconv.cpp
+++ editors/abiword/files/patch-src_af_util_xp_ut_iconv.cpp
@@ -1,11 +1,11 @@
---- src/af/util/xp/ut_iconv.cpp.orig	Wed Jul 20 22:24:55 2005
-+++ src/af/util/xp/ut_iconv.cpp	Wed Jul 20 22:25:23 2005
+--- src/af/util/xp/ut_iconv.cpp.orig	Tue Jan 10 00:18:13 2006
++++ src/af/util/xp/ut_iconv.cpp	Tue Jan 10 00:18:31 2006
 @@ -67,7 +67,7 @@
  #if defined (WIN32) || defined(__QNXNTO__) || defined(__CYGWIN__) ||  \
  (defined (__MACH__) && defined (__APPLE__)) || \
  (defined(TARGET_OS_MAC) && TARGET_OS_MAC) || \
--defined (__AIX__) || \
-+defined (__AIX__) || defined (__FreeBSD__) || \
+-defined (__AIX__) || defined(__OpenBSD__) || \
++defined (__AIX__) || defined(__OpenBSD__) || defined (__FreeBSD__) || \
  (defined(__linux__) && defined(__powerpc__) && (__GLIBC__ <= 2) && (__GLIBC_MINOR__ <= 1))
  
  #define ICONV_CONST const
