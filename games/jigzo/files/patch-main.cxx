be78365887a8d38e3e381de446ce6657cd31a1c2
Jean-Yves Lefort <jylefort@FreeBSD.org>
2006-01-11 11:15:28 +0000

    Add glpuzzle.
    glpuzzle is a jigsaw puzzle game. Choose from 12 puzzles of various
    difficulties (4 to 25 pieces), and move the pieces around to create
    the desired picture. glpuzzle uses OpenGL for smooth movement and
    anti-aliased display.
    
    WWW: http://www.iua.upf.es/~mdeboer/resorama/glpuzzle/
    
    PR:		ports/91619
    Submitted by:	Dmitry Marakasov <amdmi3@mail.ru>

---
+++ games/jigzo/files/patch-main.cxx
@@ -0,0 +1,10 @@
+--- main.cxx.orig	Wed Jan 11 04:42:54 2006
++++ main.cxx	Wed Jan 11 04:43:04 2006
+@@ -24,6 +24,7 @@
+  * 
+  */
+ 
++#include <unistd.h>
+ #include <cstdio>
+ #include <cstdlib>
+ #include <cstring>
