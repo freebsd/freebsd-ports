db0c0639410466c8d63f6a72a83838007fab3745
pav <pav@FreeBSD.org>
2006-01-14 13:04:49 +0000

    - Unbreak on FreeBSD 7.X - Simplify

--- audio/py-musicbrainz/files/patch-musicbrainz.py
+++ audio/py-musicbrainz/files/patch-musicbrainz.py
@@ -1,20 +1,19 @@
---- musicbrainz.py.orig	Tue Aug 17 03:44:32 2004
-+++ musicbrainz.py	Fri Sep 16 19:14:15 2005
-@@ -35,7 +35,16 @@
-         if sys.platform == "linux2":
-            lib = "libmusicbrainz.so.4"
-         else:
+--- musicbrainz.py.orig	Tue Aug 17 08:44:54 2004
++++ musicbrainz.py	Sat Jan 14 14:04:01 2006
+@@ -28,15 +28,7 @@
+ Error = MusicBrainzError
+ 
+ def findLibrary():
+-    
+-    if sys.platform == "darwin":
+-        lib = "libmusicbrainz.4.dylib"
+-    else:
+-        if sys.platform == "linux2":
+-           lib = "libmusicbrainz.so.4"
+-        else:
 -           raise MusicBrainzError, "Unknown platform: " + sys.platform 
-+            if sys.platform == "freebsd5":
-+               lib = "libmusicbrainz.so.4"
-+            else:
-+                if sys.platform == "freebsd6":
-+                   lib = "libmusicbrainz.so.4"
-+                else:
-+                   if sys.platform == "freebsd4":
-+                      lib = "libmusicbrainz.so.4"
-+                   else:
-+                      raise MusicBrainzError, "Unknown platform: " + sys.platform 
-   
+-  
++    lib = "libmusicbrainz.so.4"
      path = "/usr/lib/" + lib
      if os.access(path, os.F_OK):
+        return path;
