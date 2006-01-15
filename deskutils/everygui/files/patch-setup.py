c8c3fd94f92d26b50025749d641d0f1f3f8a6477
Pav Lucistnik <pav@FreeBSD.org>
2006-01-15 14:35:22 +0000

    EveryGUI provides Chameleon, which is a dynamic GUI for virtually any tool or OS command, and Designer which is a visual environment for editing config files that determine Chameleon's different behaviors for each tool/command.
    WWW: http://everygui.sourceforge.net/
    
    PR:		ports/91746, ports/91747
    Submitted by:	Remington <mrl0lz@gmail.com>

---
+++ deskutils/everygui/files/patch-setup.py
@@ -0,0 +1,19 @@
+--- setup.py.orig	Tue Jan 10 09:59:47 2006
++++ setup.py	Sun Jan 15 11:46:49 2006
+@@ -8,11 +8,6 @@
+ from distutils.core import setup
+ import glob
+ 
+-try:
+-	import gtk
+-except:
+-	sys.exit("pyGTK version 2.6 or greater is required to run everygui. http://www.pygtk.org")
+-
+ 
+ setup(
+ 	name='everygui',
+@@ -29,4 +24,3 @@
+ 			("share/everygui/egconfig", glob.glob("egconfig/*.egd"))]
+ 	)
+ 
+-os.chmod("/usr/share/everygui/egconfig", 0777)
