32c40c631647bdb7f46954402857d85a0ece48d1
Pav Lucistnik <pav@FreeBSD.org>
2006-01-15 23:56:27 +0000

    - After repocopy, add development version of nfdump at 1.5
    PR:		ports/91725
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu> (maintainer)
    Repocopied by:	marcus

---
+++ net-mgmt/nfdump-devel/files/patch-ft2nfdump.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- ft2nfdump.c.orig
++++ ft2nfdump.c
+@@ -54,7 +54,7 @@
+ 
+ #include <string.h>
+ #include <errno.h>
+-#include "ftbuild.h"
++/*#include "ftbuild.h" */
+ 
+ #include <sys/stat.h>
+ 
