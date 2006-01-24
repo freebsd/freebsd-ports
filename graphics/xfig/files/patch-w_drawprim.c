526d8a29396d66ee79c02f4094b3dbe4dd5fcc23
pav <pav@FreeBSD.org>
2006-01-24 15:11:08 +0000

    - Fix runtime; new_string prototype was not defined
    PR:		ports/92223
    Submitted by:	Steven G. Kargl <kargl@troutmask.apl.washington.edu>

---
+++ graphics/xfig/files/patch-w_drawprim.c
@@ -0,0 +1,10 @@
+--- w_drawprim.c.orig   Mon Jan 23 11:45:22 2006
++++ w_drawprim.c        Mon Jan 23 11:45:40 2006
+@@ -35,6 +35,7 @@
+ #include "paintop.h"
+ #include "mode.h"
+ #include "object.h"
++#include "u_create.h"
+ #include "u_fonts.h"
+ #include "w_canvas.h"
+ #include "w_drawprim.h"
