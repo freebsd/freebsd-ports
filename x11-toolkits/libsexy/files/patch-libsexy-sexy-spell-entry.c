f20193c85a4777b6cacf6e7dabcebede1281b2fd
Pav Lucistnik <pav@FreeBSD.org>
2006-01-24 11:40:46 +0000

    - Update to 0.1.5
    PR:		ports/92231
    Submitted by:	Andreas Kohn <andreas@syndrom23.de> (maintainer)

--- x11-toolkits/libsexy/files/patch-libsexy-sexy-spell-entry.c
+++ x11-toolkits/libsexy/files/patch-libsexy-sexy-spell-entry.c
@@ -1,37 +1,48 @@
---- libsexy/sexy-spell-entry.c.orig	Fri Oct 14 20:20:45 2005
-+++ libsexy/sexy-spell-entry.c	Sun Nov 13 13:21:06 2005
-@@ -799,12 +799,14 @@
+--- libsexy/sexy-spell-entry.c.orig	Wed Nov 16 04:45:57 2005
++++ libsexy/sexy-spell-entry.c	Thu Jan 12 00:57:57 2006
+@@ -880,10 +880,10 @@
+ static gchar *
+ get_lang_from_dict(struct EnchantDict *dict)
+ {
++	gchar *lang;
+ 	if (!have_enchant)
+ 		return NULL;
+ 
+-	gchar *lang;
+ 	enchant_dict_describe(dict, get_lang_from_dict_cb, &lang);
+ 	return lang;
+ }
+@@ -938,14 +938,14 @@
  GSList *
  sexy_spell_entry_get_languages(const SexySpellEntry *entry)
  {
-+#ifdef HAVE_ENCHANT_BROKER_LIST_DICTS
 +	GSList *langs = NULL;
-+#endif
 +
  	g_return_val_if_fail(entry != NULL, NULL);
  	g_return_val_if_fail(SEXY_IS_SPELL_ENTRY(entry), NULL);
  
- #ifdef HAVE_ENCHANT_BROKER_LIST_DICTS
+ 	if (enchant_broker_list_dicts == NULL)
+ 		return NULL;
+ 
 -	GSList *langs = NULL;
 -
  	if (!entry->priv->broker)
  		return NULL;
  
-@@ -866,13 +868,15 @@
+@@ -1002,14 +1002,14 @@
  gboolean
  sexy_spell_entry_activate_language(SexySpellEntry *entry, const gchar *lang, GError **error)
  {
-+#ifdef HAVE_ENCHANT
 +	gboolean ret;
-+#endif
 +
  	g_return_val_if_fail(entry != NULL, FALSE);
  	g_return_val_if_fail(SEXY_IS_SPELL_ENTRY(entry), FALSE);
  	g_return_val_if_fail(lang != NULL && lang != '\0', FALSE);
  
- #ifdef HAVE_ENCHANT
--	gboolean ret;
+ 	if (!have_enchant)
+ 		return FALSE;
 -
+-	gboolean ret;
+ 
  	if (error)
  		g_return_val_if_fail(*error == NULL, FALSE);
- 
