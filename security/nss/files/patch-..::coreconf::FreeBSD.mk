a2f06bf70dd6d48126b5a1c5a4a4a1fd06fe2a2c
marcus <marcus@FreeBSD.org>
2006-01-24 23:37:03 +0000

    Update to 3.11.
    PR:		92017
    Submitted by:	mi

--- security/nss/files/patch-..::coreconf::FreeBSD.mk
+++ security/nss/files/patch-..::coreconf::FreeBSD.mk
@@ -1,7 +1,6 @@
---- ../coreconf/FreeBSD.mk.orig	Wed Mar 26 20:17:25 2003
-+++ ../coreconf/FreeBSD.mk	Sat Apr 17 21:25:24 2004
-@@ -35,9 +35,9 @@
- 
+--- ../coreconf/FreeBSD.mk	Fri Sep 16 13:09:23 2005
++++ ../coreconf/FreeBSD.mk	Wed Jan 18 18:30:48 2006
+@@ -38,7 +38,7 @@
  include $(CORE_DEPTH)/coreconf/UNIX.mk
  
 -DEFAULT_COMPILER	= gcc
@@ -12,27 +11,27 @@
 +CCC			= $(CXX)
  RANLIB			= ranlib
  
- ifeq ($(OS_TEST),alpha)
-@@ -47,6 +47,8 @@
- endif
+@@ -50,6 +50,12 @@
  
  OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
 +OS_LIBS			= $(BSD_LDOPTS)
 +OPTIMIZER		=
  
++ifeq ($(OS_TEST),sparc64)
  DSO_CFLAGS		= -fPIC
++else
++DSO_CFLAGS		= -fpic
++endif
  DSO_LDOPTS		= -shared -Wl,-soname -Wl,$(notdir $@)
-@@ -58,7 +60,7 @@
- USE_PTHREADS		= 1
+ 
+@@ -61,5 +67,5 @@
  DEFINES			+= -D_THREAD_SAFE -D_REENTRANT
  OS_LIBS			+= -pthread
 -DSO_LDOPTS		+= -pthread
 +DSO_LDOPTS		+= $(BSD_LDOPTS)
  endif
  
- ARCH			= freebsd
-@@ -66,12 +68,16 @@
- MOZ_OBJFORMAT		:= $(shell test -x /usr/bin/objformat && /usr/bin/objformat || echo aout)
+@@ -69,10 +75,14 @@
  
  ifeq ($(MOZ_OBJFORMAT),elf)
 -DLL_SUFFIX		= so
@@ -49,4 +48,8 @@
 +endif
  ifdef MAPFILE
  # Add LD options to restrict exported symbols to those in the map file
- endif
+@@ -84,2 +94,4 @@
+ 
+ INCLUDES		+= -I/usr/X11R6/include
++USE_SYSTEM_ZLIB		= 1
++ZLIB_LIBS		= -lz
--- security/nss/files/patch-..::coreconf::command.mk
+++ security/nss/files/patch-..::coreconf::command.mk
@@ -1,14 +1,11 @@
-
 $FreeBSD$
 
---- ../coreconf/command.mk	2002/08/08 17:20:41	1.1
-+++ ../coreconf/command.mk	2002/08/08 17:20:54
-@@ -42,7 +42,7 @@
+--- ../coreconf/command.mk	Mon Oct 10 19:46:12 2005
++++ ../coreconf/command.mk	Wed Jan 18 17:23:28 2006
+@@ -46,5 +46,5 @@
  LINK_DLL      = $(LINK) $(OS_DLLFLAGS) $(DLLFLAGS)
  LINK_EXE      = $(LINK) $(OS_LFLAGS) $(LFLAGS)
- NFSPWD        = $(NSINSTALL_DIR)/nfspwd
 -CFLAGS        = $(OPTIMIZER) $(OS_CFLAGS) $(XP_DEFINE) $(DEFINES) $(INCLUDES) \
 +CFLAGS        += $(OPTIMIZER) $(OS_CFLAGS) $(XP_DEFINE) $(DEFINES) $(INCLUDES) \
  		$(XCFLAGS)
  RANLIB        = echo
- TAR           = /bin/tar
