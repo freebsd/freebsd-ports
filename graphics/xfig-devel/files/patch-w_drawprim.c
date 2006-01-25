a7d8b07d33b6bc603c532df940708ea2ece3fd1b
lesi <lesi@FreeBSD.org>
2006-01-25 20:35:19 +0000

    Add a patch to include a missing header. This fixes coredump when trying to insert a picture.
    PR:		ports/92222
    Submitted by:	Steven G. Kargl ( kargl at troutmask.apl.washington.edu )

---
+++ graphics/xfig-devel/files/patch-w_drawprim.c
@@ -0,0 +1,10 @@
+--- w_drawprim.c.orig	Tue Jan 24 23:42:51 2006
++++ w_drawprim.c	Tue Jan 24 23:43:11 2006
+@@ -35,6 +35,7 @@
+ #include "paintop.h"
+ #include "mode.h"
+ #include "object.h"
++#include "u_create.h"
+ #include "u_fonts.h"
+ #include "w_canvas.h"
+ #include "w_drawprim.h"
