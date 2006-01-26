94521905eef4547c296dde96afa8dd254cbd14bf
Pav Lucistnik <pav@FreeBSD.org>
2006-01-26 09:43:48 +0000

    - Unbreak with darts 0.3
    PR:		ports/92288
    Submitted by:	UMENO Takashi <umeno@rr.iij4u.or.jp>

--- japanese/chasen-base/files/patch-dartsdic.cpp
+++ japanese/chasen-base/files/patch-dartsdic.cpp
@@ -1,5 +1,14 @@
---- lib/dartsdic.cpp.orig	Sat Aug 14 12:10:21 2004
-+++ lib/dartsdic.cpp	Sat Aug 14 12:10:26 2004
+--- lib/dartsdic.cpp.orig	Thu Jul 31 01:06:57 2003
++++ lib/dartsdic.cpp	Wed Jan 25 09:30:23 2006
+@@ -68,7 +68,7 @@
+ 
+     da = (darts_t*)cha_malloc(sizeof(darts_t));
+     da->da_mmap = cha_mmap_file(daname);
+-    darts->setArray(cha_mmap_map(da->da_mmap));
++    darts->set_array(cha_mmap_map(da->da_mmap));
+     da->da = darts;
+     da->lex_mmap = cha_mmap_file(lexname);
+     da->dat_mmap = cha_mmap_file(datname);
 @@ -177,7 +177,7 @@
  	    lex_indices.push_back(i->second);
  	}
