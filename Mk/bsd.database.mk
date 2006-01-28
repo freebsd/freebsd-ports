c98d5db64eaa713d38fbd00c991639518b648330
Vsevolod Stakhov <vsevolod@FreeBSD.org>
2006-01-28 07:20:00 +0000

    Replace BROKEN with IGNORE.
    PR:		92445
    Submitted by:	linimon

--- Mk/bsd.database.mk
+++ Mk/bsd.database.mk
@@ -25,7 +25,7 @@ Database_Include_MAINTAINER=	vsevolod@FreeBSD.org
 #				  Default: 41.
 # WANT_MYSQL_VER
 #				- Maintainer can set an arbitrary version of MySQL by using it.
-# BROKEN_WITH_MYSQL
+# IGNORE_WITH_MYSQL
 #				- This variable can be defined if the ports doesn't support
 #				  one or more version of MySQL.
 # MYSQL_VER		- Internal variable for MySQL version.
@@ -43,7 +43,7 @@ Database_Include_MAINTAINER=	vsevolod@FreeBSD.org
 # WANT_PGSQL_VER
 #				- Maintainer can set an arbitrary version of PostgreSQL by
 #				  using it.
-# BROKEN_WITH_PGSQL
+# IGNORE_WITH_PGSQL
 #				- This variable can be defined if the ports doesn't support
 #				  one or more versions of PostgreSQL.
 ##
@@ -72,7 +72,7 @@ _MYSQL_VER!=	${LOCALBASE}/bin/mysql --version | ${SED} -e 's/.*Distrib \([0-9]\)
 
 .if defined(WANT_MYSQL_VER)
 .if defined(WITH_MYSQL_VER) && ${WITH_MYSQL_VER} != ${WANT_MYSQL_VER}
-BROKEN=		The port wants mysql${WANT_MYSQL_VER}-client and you try to install mysql${WITH_MYSQL_VER}-client.
+IGNORE=		The port wants mysql${WANT_MYSQL_VER}-client and you try to install mysql${WITH_MYSQL_VER}-client.
 .endif
 MYSQL_VER=	${WANT_MYSQL_VER}
 .elif defined(WITH_MYSQL_VER)
@@ -87,19 +87,19 @@ MYSQL_VER=	${DEFAULT_MYSQL_VER}
 
 .if defined(_MYSQL_VER)
 .if ${_MYSQL_VER} != ${MYSQL_VER}
-BROKEN=	MySQL versions mismatch: mysql${_MYSQL_VER}-client is installed and wanted version is mysql${MYSQL_VER}-client
+IGNORE=	MySQL versions mismatch: mysql${_MYSQL_VER}-client is installed and wanted version is mysql${MYSQL_VER}-client
 .endif
 .endif
 
 # And now we are checking if we can use it
 .if defined(MYSQL${MYSQL_VER}_LIBVER)
-.if defined(BROKEN_WITH_MYSQL)
-.	for VER in ${BROKEN_WITH_MYSQL}
+.if defined(IGNORE_WITH_MYSQL)
+.	for VER in ${IGNORE_WITH_MYSQL}
 .		if (${MYSQL_VER} == "${VER}")
-IGNORE=		Doesn't work with MySQL version : ${MYSQL_VER} (Doesn't support MySQL ${BROKEN_WITH_MYSQL})
+IGNORE=		Doesn't work with MySQL version : ${MYSQL_VER} (Doesn't support MySQL ${IGNORE_WITH_MYSQL})
 .		endif
 .	endfor
-.endif # BROKEN_WITH_MYSQL
+.endif # IGNORE_WITH_MYSQL
 LIB_DEPENDS+=	mysqlclient.${MYSQL${MYSQL_VER}_LIBVER}:${PORTSDIR}/databases/mysql${MYSQL_VER}-client
 .else
 IGNORE=		Unknown MySQL version: ${MYSQL_VER}
@@ -119,7 +119,7 @@ _PGSQL_VER!=	${LOCALBASE}/bin/pg_config --version | ${SED} -n 's/PostgreSQL[^0-9
 .endif
 
 .if defined(WANT_PGSQL_VER) && defined(_PGSQL_VER) && ${WANT_PGSQL_VER} != ${_PGSQL_VER}
-BROKEN=		the port wants postgresql${WANT_PGSQL_VER}-client but you have postgresql${_PGSQL_VER}-client installed
+IGNORE=		the port wants postgresql${WANT_PGSQL_VER}-client but you have postgresql${_PGSQL_VER}-client installed
 .endif
 
 .if defined(_PGSQL_VER)
@@ -132,13 +132,13 @@ PGSQL_VER=	${DEFAULT_PGSQL_VER}
 
 # And now we are checking if we can use it
 .if defined(PGSQL${PGSQL_VER}_LIBVER)
-.if defined(BROKEN_WITH_PGSQL)
-.	for VER in ${BROKEN_WITH_PGSQL}
+.if defined(IGNORE_WITH_PGSQL)
+.	for VER in ${IGNORE_WITH_PGSQL}
 .		if (${PGSQL_VER} == "${VER}")
-IGNORE=		Does not work with postgresql${PGSQL_VER}-client PostgresSQL (${BROKEN_WITH_PGSQL} not supported)
+IGNORE=		Does not work with postgresql${PGSQL_VER}-client PostgresSQL (${IGNORE_WITH_PGSQL} not supported)
 .		endif
 .	endfor
-.endif # BROKEN_WITH_PGSQL
+.endif # IGNORE_WITH_PGSQL
 LIB_DEPENDS+=	pq.${PGSQL${PGSQL_VER}_LIBVER}:${PORTSDIR}/databases/postgresql${PGSQL_VER}-client
 .else
 IGNORE=		Unknown PostgreSQL version: ${PGSQL_VER}
