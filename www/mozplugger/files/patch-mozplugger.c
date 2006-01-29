767545bd18caed570f9a932b000dff3a15c9019d
pav <pav@FreeBSD.org>
2006-01-29 12:20:56 +0000

    - Update to 1.7.3
    PR:		ports/92442
    Submitted by:	Loren M. Lang <lorenl@alzatex.com> (maintainer)

--- www/mozplugger/files/patch-mozplugger.c
+++ www/mozplugger/files/patch-mozplugger.c
@@ -1,6 +1,6 @@
---- mozplugger.c.orig	Sun Apr 17 05:19:59 2005
-+++ mozplugger.c	Fri Jun  3 20:55:32 2005
-@@ -568,6 +568,9 @@
+--- mozplugger.c.orig	Mon Aug 29 14:08:04 2005
++++ mozplugger.c	Fri Jan 27 17:04:36 2006
+@@ -572,6 +572,9 @@
  	  if (cb(fname, data)) return 1;
       }
  
@@ -10,20 +10,3 @@
       snprintf(fname, sizeof(fname), "/etc/%s", basename);
       if (cb(fname, data)) return 1;
  
-@@ -1208,6 +1211,8 @@
-  *****************************************************************************/
- NPError NPP_SetWindow(NPP instance, NPWindow* window)
- {
-+     XSetWindowAttributes attrib;
-+
-      D("SetWindow\n");
- 
-      if (!instance)
-@@ -1239,7 +1244,6 @@
- 	  D("Writing WIN 0x%x to fd %d\n", window->window, THIS->fd);
- 	  write(THIS->fd, (char *)window, sizeof(*window));
- 
--	  XSetWindowAttributes attrib;
- 	  attrib.override_redirect = True;
- 	  XChangeWindowAttributes(THIS->display, (Window)THIS->windata.window,
- 				  CWOverrideRedirect, &attrib);
