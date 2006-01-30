9dcb98adfc2bd4674bbd6842d3d6d437dd29b349
pav <pav@FreeBSD.org>
2006-01-30 21:36:28 +0000

    Populate newly create net-p2p category with these ports:
      ftp/jigdo
      net/amule1
      net/amule2
      net/apollon
      net/azureus
      net/bnbt
      net/btpeer
      net/btqueue
      net/cdonkey
      net/ctorrent
      net/dcd
      net/dclib
      net/dctc
      net/dctc-gui
      net/dctc-gui-qt
      net/edonkey-gui-gtk
      net/edonkey-gui-gtk-urlslave
      net/fcptools
      net/fidelio
      net/freenet
      net/frost
      net/giftcurs
      net/giftoxic
      net/giftui
      net/gift
      net/gift-fasttrack
      net/gift-gnutella
      net/gift-openft
      net/gkrellm-gift
      net/gnewtellium
      net/gnome-btdownload
      net/gnunet
      net/gtkhx
      net/gtk-gnutella
      net/gtorrentviewer
      net/hagelslag
      net/hx
      net/i2p
      net/javadc
      net/kmldonkey
      net/ktorrent
      net/libbt
      net/liberator
      net/libfreenet
      net/libpdtp
      net/libtorrent
      net/limewire
      net/linux-agsatellite
      net/linux-edonkey-core
      net/linux-edonkey-server
      net/linux-jigdo
      net/linux-overnet-core
      net/minder
      net/mldonkey
      net/mldonkey-core
      net/mldonkey-core-devel
      net/mldonkey-devel
      net/mldonkey-gui
      net/mldonkey-gui-devel
      net/mldonkey-perlreactor
      net/mldonkey-sancho
      net/mldonkey-serverspy
      net/mldonkey-urlslave
      net/mutella
      net/mute-net
      net/mute-net-gui
      net/mute-net-text
      net/napshare
      net/nicotine
      net/opendchub
      net/peercast
      net/phex
      net/pyslsk
      net/py-bittornado
      net/py-bittornado-core
      net/py-bittorrent
      net/py-bittorrent-core
      net/py-bittorrent-core-devel
      net/py-bittorrent-devel
      net/py-fngrab
      net/py-kenosis
      net/py-kenosis-bittorrent
      net/py-py2play
      net/p5-pdonkey
      net/qtella
      net/qtorrent
      net/rtorrent
      net/squall
      net/torrentflux
      net/torrentsniff
      net/trackerbt
      net/transmission
      net/valknut
      net/verlihub
      net/verlihub-plugins
      net/xmule
      net/xnap
      misc/ed2k
      misc/edonkey-tool-hash
      misc/linux-edonkey-tool-recovermet
    
    Repocopies by:	marcus

--- net/py-bittorrent/files/extra-noportdocs-setup.py
+++
@@ -1,8 +0,0 @@
---- setup.py.orig	Mon Nov 21 00:10:24 2005
-+++ setup.py	Mon Nov 21 00:10:33 2005
-@@ -72,5 +72,0 @@
--               (doc_root        , ['credits.txt', 'credits-l10n.txt',
--                                   'LICENSE.txt', 'README.txt',
--                                   'TRACKERLESS.txt', 'redirdonate.html',
--
--                                   ]       ),
