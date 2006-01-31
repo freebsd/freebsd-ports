3cd345f14fbd25b2fac040f92b8e517e91b35edd
Simon Barner <barner@FreeBSD.org>
2006-01-31 21:28:38 +0000

    - Update to version 3.52.4 [1] - Use less greedy CONFLICTS
    Submitted by:	Ports fury
    PR:		ports/92612

--- databases/libiodbc/files/patch-include::sqltypes.h
+++ databases/libiodbc/files/patch-include::sqltypes.h
@@ -1,10 +1,10 @@
---- include/sqltypes.h.orig	Wed Sep 29 19:14:06 2004
-+++ include/sqltypes.h	Sat Nov 13 15:26:42 2004
-@@ -387,6 +387,7 @@
- #  if defined(_WCHAR_T)			|| \
+--- include/sqltypes.h.orig	Thu Jan 26 18:50:59 2006
++++ include/sqltypes.h	Tue Jan 31 13:45:45 2006
+@@ -398,6 +398,7 @@
+       defined(_WCHAR_T)			|| \
        defined(_WCHAR_T_DEFINED)		|| \
        defined(_WCHAR_T_DECLARED)        || \
 +      defined(__FreeBSD__)              || \
-       defined(_BSD_WCHAR_T_DEFINED_)
+       defined(_BSD_WCHAR_T_DEFINED_)    || \
+       defined(_BSD_WCHAR_T_)
  typedef wchar_t SQLWCHAR;
- #  else
