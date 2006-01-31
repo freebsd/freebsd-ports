87af7b6f01b8ab9f24f1403ae05645dc59689220
barner <barner@FreeBSD.org>
2006-01-31 21:28:38 +0000

    - Update to version 3.52.4 [1] - Use less greedy CONFLICTS
    Submitted by:	Ports fury
    PR:		ports/92612

--- databases/libiodbc/files/patch-iodbcinst__unicode.c
+++ databases/libiodbc/files/patch-iodbcinst__unicode.c
@@ -1,14 +1,14 @@
---- iodbc/unicode.c.orig	Tue Aug 30 22:24:12 2005
-+++ iodbc/unicode.c	Thu Nov 10 13:35:19 2005
-@@ -192,6 +192,7 @@
+--- iodbcinst/unicode.c.orig	Mon Jan 23 19:30:26 2006
++++ iodbcinst/unicode.c	Tue Jan 31 13:51:36 2006
+@@ -197,6 +197,7 @@
  #if !defined(HAVE_TOWLOWER)
  #if (defined(__APPLE__) && !defined (_LP64)) || defined (macintosh)
  #include <Carbon/Carbon.h>
 +#endif
  wchar_t towlower(wchar_t wc)
  {
- #ifdef __APPLE__
-@@ -213,7 +214,6 @@
+ #if defined (__APPLE__) && !defined (NO_FRAMEWORKS)
+@@ -218,7 +219,6 @@
    return wc;
  #endif
  }
