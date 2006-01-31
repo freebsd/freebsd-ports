fe200ce608847779e1b1c99b8980458628cecdc1
Joe Marcus Clarke <marcus@FreeBSD.org>
2006-01-31 02:46:29 +0000

    Adjust the order of variable declarations to fix a problem getting the signal strength from Aironet cards.

--- net/gnome-netstatus/files/patch-src_netstatus-sysdeps.c
+++ net/gnome-netstatus/files/patch-src_netstatus-sysdeps.c
@@ -1,5 +1,19 @@
---- src/netstatus-sysdeps.c.orig	Mon Jun 27 14:29:53 2005
-+++ src/netstatus-sysdeps.c	Mon Jun 27 14:30:18 2005
+--- src/netstatus-sysdeps.c.orig	Mon Mar 14 09:34:06 2005
++++ src/netstatus-sysdeps.c	Mon Jan 30 21:44:49 2006
+@@ -430,11 +430,11 @@ static inline char *
+ get_an_data (const char *iface,
+ 	     int        *signal_strength)
+ {
++  struct an_ltv_status *sts;
++  struct an_req         areq;
+ #ifdef AN_RID_RSSI_MAP
+   struct an_ltv_rssi_map  an_rssimap;
+ #endif
+-  struct an_req         areq;
+-  struct an_ltv_status *sts;
+   int                   level;
+   char                 *error = NULL;
+   gboolean              rssimap_valid = FALSE;
 @@ -548,6 +548,8 @@ netstatus_sysdeps_read_iface_wireless_de
        g_strncasecmp (iface, "wi",   2) &&
        g_strncasecmp (iface, "ath",  3) &&
