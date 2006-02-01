0bdafa9ce292c464890affc3c2c0b381cb30ec81
Jean-Yves Lefort <jylefort@FreeBSD.org>
2006-02-01 20:05:08 +0000

    - Split into two ports: mbmon (tty) and xmbmon (X) - Remove -s from LDFLAGS (do not unconditionally strip, let   ${INSTALL_PROGRAM} do it) - Use OPTIONS - Decouple manpage installation from NOPORTDOCS - xmbmon:     - use X prefix     - install xmbmon.resources into the appropriate location     - do not require a font package: use fixed rather than Helvetica [1] - Take maintainership
    [1]:
    PR:		ports/92600
    Submitted by:	Daniel Berlin <mail@daniel-berlin.de>

--- sysutils/xmbmon/files/patch-xmbmon.c
+++ sysutils/xmbmon/files/patch-xmbmon.c
@@ -1,6 +1,17 @@
---- xmbmon.c.orig	Mon Feb 23 00:32:24 2004
-+++ xmbmon.c	Mon Feb 23 00:32:43 2004
-@@ -32,7 +32,7 @@
+--- xmbmon.c.orig	Thu Aug 12 06:37:03 2004
++++ xmbmon.c	Wed Feb  1 20:44:54 2006
+@@ -23,16 +23,16 @@
+ #define RES_NAME   "XMBmon"
+ 
+ #define DEFAULT_GEOMETRY  "100x140"
+-#define DEFAULT_FONT      "-adobe-helvetica-bold-r-*-*-10-*-*-*-*-*-*-*"
++#define DEFAULT_FONT      "fixed"
+ 
+-#define DEFAULT_LBFONT    "-adobe-helvetica-bold-r-*-*-12-*-*-*-*-*-*-*"
++#define DEFAULT_LBFONT    "fixed"
+ #define DEFAULT_LBCOLOR   "black"
+ 
+ #define DEFAULT_COUNT        "4"
  #define DEFAULT_CSEC        "20"
  #define DEFAULT_WSEC      "1800"
  #define DEFAULT_TMIN      "10.0"
