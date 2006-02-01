b8bd6b9c1258457c7fe8541ad2d5bb8318b714cb
Renato Botelho <garga@FreeBSD.org>
2006-02-01 11:46:17 +0000

    - Update to 1.3.1
    PR:		ports/92674
    Submitted by:	maintainer

--- lang/see/files/patch-libsee_dtoa__config.h
+++ lang/see/files/patch-libsee_dtoa__config.h
@@ -1,10 +1,10 @@
---- libsee/dtoa_config.h.orig	Mon Apr 25 18:19:54 2005
-+++ libsee/dtoa_config.h	Mon Apr 25 18:23:28 2005
+--- libsee/dtoa_config.h.orig	Wed Jan 18 13:51:50 2006
++++ libsee/dtoa_config.h	Tue Jan 31 16:52:54 2006
 @@ -12,7 +12,7 @@
  
  #include <see/type.h>
  
--#if defined(__i386__)
+-#if defined(__i386__) || defined(__amd64__)
 +#if defined(__i386__) || defined(__amd64__) || defined(__ia64__) || defined(__alpha__)
  #   define IEEE_8087
  #endif
