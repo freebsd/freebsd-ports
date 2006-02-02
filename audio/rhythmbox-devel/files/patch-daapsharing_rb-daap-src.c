8c60030e5aaebc6363a232f155c4bb95042fc56d
marcus <marcus@FreeBSD.org>
2006-02-02 07:58:16 +0000

    Update to 0.9.3.

--- audio/rhythmbox-devel/files/patch-daapsharing_rb-daap-src.c
+++ audio/rhythmbox-devel/files/patch-daapsharing_rb-daap-src.c
@@ -1,27 +1,26 @@
---- daapsharing/rb-daap-src.c.orig	Sun Oct 16 05:50:21 2005
-+++ daapsharing/rb-daap-src.c	Mon Oct 17 10:30:20 2005
+--- daapsharing/rb-daap-src.c.orig	Fri Jan 27 21:11:13 2006
++++ daapsharing/rb-daap-src.c	Thu Feb  2 02:07:48 2006
 @@ -34,6 +34,7 @@
  #include <sys/ioctl.h>
  #include <netdb.h>
  #include <unistd.h>
 +#include <signal.h>
+ #include <ctype.h>
  
- #include <libgnome/gnome-i18n.h>
- #include <gst/gst.h>
-@@ -580,7 +581,15 @@ gst_tcp_socket_write (int socket, const 
+ #include <libsoup/soup-headers.h>
+@@ -435,7 +436,14 @@ rb_daap_src_write (RBDAAPSrc *src, const
  	size_t bytes_written = 0;
  
  	while (bytes_written < count) {
--		ssize_t wrote = send (socket, buf + bytes_written, count - bytes_written, MSG_NOSIGNAL);
+-		ssize_t wrote = send (src->sock_fd, buf + bytes_written, count - bytes_written, MSG_NOSIGNAL);
 +		ssize_t wrote;
 +#ifdef MSG_NOSIGNAL
-+		wrote = send (socket, buf + bytes_written, count - bytes_written, MSG_NOSIGNAL);
++		wrote = send (src->sock_fd, buf + bytes_written, count - bytes_written, MSG_NOSIGNAL);
 +#else
 +		signal (SIGPIPE, SIG_IGN);
-+		wrote = send (socket, buf + bytes_written, count - bytes_written, 0);
++		wrote = send (src->sock_fd, buf + bytes_written, count - bytes_written, 0);
 +		signal (SIGPIPE, SIG_DFL);
 +#endif
-+
  
- 		if (wrote <= 0) {
- 			return bytes_written;
+ 		if (wrote < 0) {
+ 			GST_WARNING ("error while writing: %s", g_strerror (errno));
