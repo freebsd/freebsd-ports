3e03d77e1bff1b038e3d816ead3e05881fb143cc
Greg Lewis <glewis@FreeBSD.org>
2006-02-02 22:02:01 +0000

    . Fix the build on amd64 (although some native libraries are missing). . Fix a crash when trying to execute a programme via the "Run" or "Debug"   menu items.
    PR:		91825
    Submitted by:	Panagiotis Astithas <past@ebs.gr>

--- java/eclipse-cdt/files/patch-cdt.core-openpty.c
+++ java/eclipse-cdt/files/patch-cdt.core-openpty.c
@@ -1,5 +1,5 @@
---- eclipse/plugins/org.eclipse.cdt.source.freebsd.gtk.x86_3.0.1/src/org.eclipse.cdt.core.freebsd_3.0.1/library/openpty.c.orig	Sat Dec  3 01:10:16 2005
-+++ eclipse/plugins/org.eclipse.cdt.source.freebsd.gtk.x86_3.0.1/src/org.eclipse.cdt.core.freebsd_3.0.1/library/openpty.c	Sat Dec  3 01:13:46 2005
+--- eclipse/plugins/org.eclipse.cdt.source.freebsd.gtk.x86_3.0.1/src/org.eclipse.cdt.core.freebsd_3.0.1/library/openpty.c.orig	Wed Jan 11 22:04:49 2006
++++ eclipse/plugins/org.eclipse.cdt.source.freebsd.gtk.x86_3.0.1/src/org.eclipse.cdt.core.freebsd_3.0.1/library/openpty.c	Wed Jan 11 22:52:35 2006
 @@ -20,7 +20,6 @@
  #include <grp.h>
  
@@ -8,3 +8,12 @@
  
  /**
   * This is taken from R. W. Stevens book.
+@@ -84,7 +83,7 @@
+ 	char *ptr;
+ 
+ 	strcpy(pts_name, "/dev/ptmx");
+-	fdm = getpt();
++	fdm = posix_openpt(O_RDWR);
+ 	if (fdm < 0)
+ 		return -1;
+ 	if (grantpt(fdm) < 0) { /* grant access to slave */
