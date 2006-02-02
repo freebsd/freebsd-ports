003f037e94eb127e55dae29b3444661d71865f22
Michael Johnson <ahze@FreeBSD.org>
2006-02-02 13:16:54 +0000

    - Update to 1.5.0.1

--- www/firefox36/files/patch-config-mkdepend-imakemdep.h
+++ www/firefox36/files/patch-config-mkdepend-imakemdep.h
@@ -1,6 +1,3 @@
-$FreeBSD$
-  $MCom: ports/www/firefox/files/patch-config-mkdepend-imakemdep.h,v 1.7 2005/11/11 00:15:48 ahze Exp $
-
 --- config/mkdepend/imakemdep.h	Fri Sep  4 21:22:25 1998
 +++ config/mkdepend/imakemdep.h	Thu Oct 16 22:43:25 2003
 @@ -278,4 +278,7 @@
