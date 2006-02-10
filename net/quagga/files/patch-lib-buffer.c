ae13e8f3ddea02f423f306e70f75671a9a745ab6
sem <sem@FreeBSD.org>
2006-02-10 12:45:36 +0000

    - Fix a crash when vtysh was executed. - Remove useless target (install is default) in depends
    PR:		ports/93094
    Reported by:	TANABE Hiroyasu <h@cosat.com>, John <freebsd@moe.co.uk>
    Submitted by:	maintainer

--- net/quagga/files/patch-lib-buffer.c
+++ net/quagga/files/patch-lib-buffer.c
@@ -5,7 +5,7 @@
  
    /* Actual data stream (variable length). */
 -  unsigned char data[];  /* real dimension is buffer->size */
-+  unsigned char *data;  /* real dimension is buffer->size */
++  unsigned char data[0];  /* real dimension is buffer->size */
  };
  
  /* It should always be true that: 0 <= sp <= cp <= size */
