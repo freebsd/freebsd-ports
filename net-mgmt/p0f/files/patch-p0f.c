ff44181d999295f9f9fc672ff121daad7bd208ee
lawrance <lawrance@FreeBSD.org>
2006-02-12 04:59:07 +0000

    Update to 2.0.4. - Submitter takes maintainership - Remove stale patches
    PR:		ports/93206
    Submitted by:	Babak Farrokhi <babak@farrokhi.net>

--- net-mgmt/p0f/files/patch-p0f.c
+++ net-mgmt/p0f/files/patch-p0f.c
@@ -1,20 +1,6 @@
-$FreeBSD$
-Add loopback support for FreeBSD.
-author:  Radim Kolar
-
---- p0f.c.orig	Tue Feb 17 21:11:51 2004
-+++ p0f.c	Tue Feb 17 21:10:48 2004
-@@ -161,7 +161,8 @@
- 
-   switch(type) {
- 
--    case DLT_NULL:
-+    case DLT_NULL: header_len=4; break;
-+
-     case DLT_SLIP:
-     case DLT_RAW:  break;
- 
-@@ -1199,11 +1200,15 @@
+--- p0f.c.orig	Sun Jul 11 02:17:57 2004
++++ p0f.c	Sun Feb 12 01:44:36 2006
+@@ -1238,11 +1238,15 @@
    /* Whoops, IP header ends past end_ptr */
    if ((_u8*)(iph + 1) > end_ptr) return;
  
@@ -32,7 +18,7 @@ author:  Radim Kolar
    /* If the declared length is shorter than the snapshot (etherleak
       or such), truncate this bad boy. */
  
-@@ -1590,7 +1595,7 @@
+@@ -1632,7 +1636,7 @@
      if (!use_iface) use_iface=pcap_lookupdev(errbuf);
  #endif /* ^WIN32 */
  
