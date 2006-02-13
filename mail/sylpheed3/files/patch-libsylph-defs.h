9d703508e62686fe7a20df66129be15e18e93107
oliver <oliver@FreeBSD.org>
2006-02-13 21:02:50 +0000

    update to 2.2.0 most Makefile changes and patch-libsylph-defs.h where taken from sylpheed2-devel

--- mail/sylpheed3/files/patch-libsylph-defs.h
+++ mail/sylpheed3/files/patch-libsylph-defs.h
@@ -1,17 +1,17 @@
---- src/defs.h.orig	Mon Sep  3 01:07:32 2001
-+++ src/defs.h	Mon Sep  3 04:50:35 2001
-@@ -58,8 +58,8 @@
- #define MARK_VERSION		2
- 
- #define DEFAULT_SIGNATURE	".signature"
+--- libsylph/defs.h.orig	Tue Sep 13 16:55:49 2005
++++ libsylph/defs.h	Mon Sep 19 03:23:15 2005
+@@ -81,8 +81,8 @@
+ #else
+ #  define DEFAULT_SIGNATURE	".signature"
+ #endif
 -#define DEFAULT_INC_PATH	"/usr/bin/mh/inc"
 -#define DEFAULT_INC_PROGRAM	"inc"
 +#define DEFAULT_INC_PATH	""
 +#define DEFAULT_INC_PROGRAM	""
  /* #define DEFAULT_INC_PATH	"/usr/bin/imget" */
  /* #define DEFAULT_INC_PROGRAM	"imget" */
- #define DEFAULT_SENDMAIL_CMD	"/usr/sbin/sendmail -t"
-@@ -67,7 +67,7 @@
+ #define DEFAULT_SENDMAIL_CMD	"/usr/sbin/sendmail -t -i"
+@@ -91,7 +91,7 @@
  #ifdef _PATH_MAILDIR
  #  define DEFAULT_SPOOL_PATH	_PATH_MAILDIR
  #else
