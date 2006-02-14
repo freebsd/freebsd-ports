448c84014965ef859440a392c5064e61f83401ea
marcus <marcus@FreeBSD.org>
2006-02-14 17:46:36 +0000

    Fix build on 4.X.
    PR:		93271

--- devel/gamin/files/patch-tests_testing.c
+++ devel/gamin/files/patch-tests_testing.c
@@ -1,6 +1,19 @@
---- tests/testing.c.orig	Thu Oct 27 12:51:03 2005
-+++ tests/testing.c	Tue Feb  7 08:10:40 2006
-@@ -486,9 +486,9 @@
+--- tests/testing.c.orig	Thu Oct 27 10:51:03 2005
++++ tests/testing.c	Tue Feb 14 17:46:01 2006
+@@ -376,11 +376,11 @@ processCommand(char *line, int no)
+         }
+         printf("chmod %s to %s\n", arg, arg2);
+     } else if (!strcmp(command, "chown")) {
++	struct stat sb;
+         if (args != 3) {
+             fprintf(stderr, "chown line %d: lacks path and owner\n", no);
+             return (-1);
+         }
+-		struct stat sb;
+ 		if (!lstat (arg, &sb)) {
+ 			ret = (S_ISLNK (sb.st_mode)) ?
+ 				lchown(arg, strtol(arg2, NULL, 10), -1) :
+@@ -486,9 +486,9 @@ processCommand(char *line, int no)
              return (-1);
          }
          /*
