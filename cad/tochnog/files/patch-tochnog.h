fb8a2cc23708dfed35c7176f728787a4eace5794
Maho Nakata <maho@FreeBSD.org>
2006-02-15 01:26:28 +0000

    build cleanup of cad/tochnog again
    PR:		92782
    Submitted by:	Pedro F. Giffuni <giffunip@asme.org>

--- cad/tochnog/files/patch-tochnog.h
+++ cad/tochnog/files/patch-tochnog.h
@@ -1,13 +1,12 @@
 --- tochnog.h.orig	Sun Feb 11 05:05:27 2001
 +++ tochnog.h	Fri Feb  3 15:15:51 2006
-@@ -17,12 +17,17 @@
+@@ -17,12 +17,16 @@
      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
  
 -#include <iostream.h>
 +#include <iostream>
 +// C++ headers
-+#include <iostream>
 +#include <fstream>
 +#include <iomanip>
 +using namespace std;
