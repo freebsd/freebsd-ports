cf00299a27e4efb0f4013e3feac77bbff2a87f72
barner <barner@FreeBSD.org>
2006-02-15 17:42:26 +0000

    - Fix build on 7-current by avoiding conflict in si_band macro, that   is already defined in the sys/signal.h header
    Submitted by:	Niklas Sorensson <nik@cs.chalmers.se>
    Prodded by:	kris via pointyhat

--- devel/valgrind/files/patch-include_vg_kerneliface.h
+++ devel/valgrind/files/patch-include_vg_kerneliface.h
@@ -1,6 +1,17 @@
---- include/vg_kerneliface.h.orig	Mon Sep 19 21:20:22 2005
-+++ include/vg_kerneliface.h	Mon Sep 19 21:22:41 2005
-@@ -975,6 +975,8 @@
+--- include/vg_kerneliface.h.orig	Fri Jul 16 19:35:22 2004
++++ include/vg_kerneliface.h	Wed Feb 15 18:24:26 2006
+@@ -177,6 +177,10 @@
+ 
+ #ifdef __FreeBSD__
+ 
++#ifdef si_band
++#undef si_band
++#endif
++
+ typedef
+    struct {
+       Int si_signo;
+@@ -975,6 +979,8 @@
  	unsigned long long rlim_max;
  } vki_rlimit;
  
