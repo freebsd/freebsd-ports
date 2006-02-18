dd5bb0ca3163bad7eed9372291d7a1739f763a4f
Marius Strobl <marius@FreeBSD.org>
2006-02-18 15:53:48 +0000

    o Update to 2.01.01a07. Noteworthy changes since 2.01.01a04 are:   - With 2.01.01a05 a new tool btcflash for flashing the firmware of     BTC DRW1008 DVD+/-RW drives was added.   - With 2.01.01a06 `readcd -scanbus` was finally fixed (the "fix" in     2.01.01a05 did not work). Thus the respective ports patch was     removed.   - With 2.01.01a07 my changes to make the direct ATAPI transport     (cooked_ioctl interface) of cdda2wav work again with FreeBSD     after ata(4) was GEOM'ifed and the CDIOCREADAUDIO ioctl removed     were finally integrated, but unfortunately a modified, buggy     version. Thus the respective ports patches were removed and the     bug corrected.
      For the complete list of changes since 2.01.01a04 see
      ftp://ftp.berlios.de/pub/cdrecord/alpha/AN-2.01.01a0[5-7]
    
    o Make the direct ATAPI transport of cdda2wav work again with FreeBSD
      after device major numbers were abandoned.

--- sysutils/cdrtools-devel/files/patch-cdda2wav::mycdrom.h
+++ sysutils/cdrtools-devel/files/patch-cdda2wav::mycdrom.h
@@ -1,24 +1,21 @@
---- cdda2wav/mycdrom.h	Wed Sep  4 14:07:16 2002
-+++ cdda2wav/mycdrom.h	Sat Jan 17 18:52:08 2004
-@@ -4,6 +4,12 @@
- #else
- # if defined HAVE_SYS_CDIO_H
- #  include <sys/cdio.h>
-+#  if defined(__FreeBSD__) && __FreeBSD__ >= 2
-+#   include <osreldate.h>
-+#   if __FreeBSD_version >= 400014
-+#    include <sys/cdrio.h>
-+#   endif
-+#  endif
- 
- #  if (defined (__sun) && defined (SVR4))
- #   if 0
-@@ -53,7 +59,7 @@
+--- cdda2wav/mycdrom.h  Wed Sep  4 14:07:16 2002
++++ cdda2wav/mycdrom.h  Sat Jan 17 18:52:08 2004
+@@ -59,9 +59,16 @@
  #   endif /* if 0 */
  #  else /* not Sun SVR4 */
- #   if defined __FreeBSD__ || defined __NetBSD__ || defined __OpenBSD__
--#    if !defined CDIOCREADAUDIO
-+#    if (!defined(__FreeBSD__) && !defined(CDIOCREADAUDIO)) || __FreeBSD_version < 228000
- #     undef HAVE_IOCTL_INTERFACE
+ #   if defined __FreeBSD__ || defined __NetBSD__ || defined __OpenBSD__ || defined __DragonFly__
+-#    if (defined(__FreeBSD__) && __FreeBSD_version < 228000) || !defined(CDIOCREADAUDIO)
+-#     undef HAVE_IOCTL_INTERFACE
++#    if defined __FreeBSD__  && !defined __DragonFly__ 
++#     if __FreeBSD_version < 228000
++#      undef HAVE_IOCTL_INTERFACE
++#     endif
  #    else
++#     if !defined CDIOCREADAUDIO 
++#      undef HAVE_IOCTL_INTERFACE
++#     endif
++#    endif
++#    if defined HAVE_IOCTL_INTERFACE 
  
+ #define CDROM_LBA	CD_LBA_FORMAT
+ #define CDROM_MSF	CD_MSF_FORMAT
