2d23ec8113ff8fa3287fbe4ec20d98222921bcd9
marius <marius@FreeBSD.org>
2006-02-18 15:53:48 +0000

    o Update to 2.01.01a07. Noteworthy changes since 2.01.01a04 are:   - With 2.01.01a05 a new tool btcflash for flashing the firmware of     BTC DRW1008 DVD+/-RW drives was added.   - With 2.01.01a06 `readcd -scanbus` was finally fixed (the "fix" in     2.01.01a05 did not work). Thus the respective ports patch was     removed.   - With 2.01.01a07 my changes to make the direct ATAPI transport     (cooked_ioctl interface) of cdda2wav work again with FreeBSD     after ata(4) was GEOM'ifed and the CDIOCREADAUDIO ioctl removed     were finally integrated, but unfortunately a modified, buggy     version. Thus the respective ports patches were removed and the     bug corrected.
      For the complete list of changes since 2.01.01a04 see
      ftp://ftp.berlios.de/pub/cdrecord/alpha/AN-2.01.01a0[5-7]
    
    o Make the direct ATAPI transport of cdda2wav work again with FreeBSD
      after device major numbers were abandoned.

--- sysutils/cdrtools-devel/files/patch-cdda2wav::interface.c
+++ sysutils/cdrtools-devel/files/patch-cdda2wav::interface.c
@@ -1,19 +1,13 @@
 --- cdda2wav/interface.c	Mon Dec 29 11:28:09 2003
 +++ cdda2wav/interface.c	Sun Jan 18 01:35:50 2004
-@@ -444,7 +444,15 @@
+@@ -452,6 +452,10 @@
      default:			/* for example ATAPI cds */
  #else
- #if defined (__FreeBSD__)
--    case 117:
-+#if __FreeBSD_version >= 501113
-+    case 4:	/* GEOM */
-+	if (memcmp(pdev_name, "/dev/acd", sizeof("/dev/acd") - 1) != 0) {
-+	    fprintf(stderr, "%s is not an ATAPI CDROM device\n", pdev_name);
-+	    exit(SYNTAX_ERROR);
-+	}
+ #if defined (__FreeBSD__) || defined(__DragonFly__)
++#if __FreeBSD_version >= 600021
++    case 0:	/* majors abandoned */
 +	/* FALLTHROUGH */
 +#endif
-+    case 117:	/* pre-GEOM atapi cd */
- 	if (!S_ISCHR(statstruct->st_mode)) {
- 	    fprintf(stderr, "%s is not a char device\n",pdev_name);
- 	    exit(SYNTAX_ERROR);
+ #if __FreeBSD_version >= 501113
+     case 4:	/* GEOM */
+ 	/* FALLTHROUGH */
