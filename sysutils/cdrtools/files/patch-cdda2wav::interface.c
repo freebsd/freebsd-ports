957d478a920ea4420d7e0595df562f19cf4825dd
Marius Strobl <marius@FreeBSD.org>
2006-02-18 15:55:40 +0000

    - Replace the fix to make `readcd -scanbus` work which was added in   2.01_2 with the "official" version from cdrtools 2.01.01a06. - Patch i386-freebsd-cc.rul to allow compilation with CC set to a   compiler other than the default (as long as it doesn't include a   path component, which requires considerably changes to make work). - Make the direct ATAPI transport of cdda2wav work again with FreeBSD   after device major numbers were abandoned; bump PORTREVISION for   this.
    Approved by:	netchild

--- sysutils/cdrtools/files/patch-cdda2wav::interface.c
+++ sysutils/cdrtools/files/patch-cdda2wav::interface.c
@@ -1,16 +1,16 @@
 --- cdda2wav/interface.c	Mon Dec 29 11:28:09 2003
 +++ cdda2wav/interface.c	Sun Jan 18 01:35:50 2004
-@@ -444,7 +444,15 @@
+@@ -445,7 +445,15 @@
      default:			/* for example ATAPI cds */
  #else
  #if defined (__FreeBSD__)
 -    case 117:
++#if __FreeBSD_version >= 600021
++    case 0:	/* majors abandoned */
++	/* FALLTHROUGH */
++#endif
 +#if __FreeBSD_version >= 501113
 +    case 4:	/* GEOM */
-+	if (memcmp(pdev_name, "/dev/acd", sizeof("/dev/acd") - 1) != 0) {
-+	    fprintf(stderr, "%s is not an ATAPI CDROM device\n", pdev_name);
-+	    exit(SYNTAX_ERROR);
-+	}
 +	/* FALLTHROUGH */
 +#endif
 +    case 117:	/* pre-GEOM atapi cd */
--- sysutils/cdrtools/files/patch-readcd::readcd.c
+++ sysutils/cdrtools/files/patch-readcd::readcd.c
@@ -1,31 +1,63 @@
 --- readcd/readcd.c.original	Sun Jan 15 17:14:03 2006
 +++ readcd/readcd.c	Sun Jan 15 17:24:47 2006
-@@ -370,6 +370,8 @@
- 			exit(err);
+@@ -253,9 +256,9 @@
+ 	int	fcount;
+ 	int	cac;
+ 	char	* const *cav;
+-	int	scsibus	= 0;
+-	int	target	= 0;
+-	int	lun	= 0;
++	int	scsibus	= -1;
++	int	target	= -1;
++	int	lun	= -1;
+ 	int	silent	= 0;
+ 	int	verbose	= 0;
+ 	int	kdebug	= 0;
+@@ -335,20 +338,22 @@
+ 				usage(EX_BAD);
+ 				/* NOTREACHED */
+ 			}
+-		} else {
+-			scsibus = 0;
  		}
- 	} else {
-+                char	errstr[80];
-+
- 		if (scsibus == -1 && target >= 0 && lun >= 0)
- 			scsibus = 0;
- 
-@@ -378,8 +380,17 @@
- 		scgp->kdebug = kdebug;
- 
- 		scg_settarget(scgp, scsibus, target, lun);
--		if (scg__open(scgp, NULL) <= 0)
--			comerr("Cannot open SCSI driver.\n");
-+
-+		scgp = scg_open(dev, errstr, sizeof(errstr), 0, 0);
-+
-+		if (scgp == NULL) { 
-+		         errmsg("%s%sCannot open SCSI driver.\n", errstr, errstr[0]?". ":"");
-+		         errmsgno(EX_BAD, "For possible targets try 'readcd -scanbus'.%s\n",
-+                                                geteuid() ? " Make sure you are root.":"");
-+			 errmsgno(EX_BAD, "For possible transport specifiers try 'readcd dev=help'.\n");
-+                        exit(EX_BAD);
-+		}
-+
+ 		cac--;
+ 		cav++;
  	}
- 	scgp->silent = silent;
- 	scgp->verbose = verbose;
+ /*error("dev: '%s'\n", dev);*/
+-
+ 	if (!scanbus)
+ 		cdr_defaults(&dev, NULL, NULL, NULL);
+ 	if (debug) {
+ 		printf("dev: '%s'\n", dev);
+ 	}
+-	if (dev) {
++	if (!scanbus && dev == NULL &&
++	    scsibus == -1 && (target == -1 || lun == -1)) {
++		errmsgno(EX_BAD, "No SCSI device specified.\n");
++		usage(EX_BAD);
++	}
++	if (dev || scanbus) {
+ 		char	errstr[80];
+ 
+ 		/*
+@@ -357,8 +362,9 @@
+ 		 * remote routines that are located inside libscg.
+ 		 */
+ 		scg_remote();
+-		if ((strncmp(dev, "HELP", 4) == 0) ||
+-		    (strncmp(dev, "help", 4) == 0)) {
++		if (dev != NULL &&
++		    ((strncmp(dev, "HELP", 4) == 0) ||
++		    (strncmp(dev, "help", 4) == 0))) {
+ 			scg_help(stderr);
+ 			exit(0);
+ 		}
+@@ -1629,7 +1656,7 @@
+ 		end = scgp->cap->c_baddr + 1;
+ 	scgp->silent--;
+ 
+-	if (end <= 0 || isrange || (askrange && scg_yes("Ignore disk size? ")))
++	if ((end <= 0 && isrange) || (askrange && scg_yes("Ignore disk size? ")))
+ 		end = 10000000;	/* Hack to read empty (e.g. blank=fast) disks */
+ 
+ 	if (parmp) {
