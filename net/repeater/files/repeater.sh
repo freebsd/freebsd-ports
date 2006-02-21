28e553f8105336e05bdb170e40e472c9c0c74f65
leeym <leeym@FreeBSD.org>
2006-02-21 16:07:07 +0000

    - fix procname problem in start script

--- net/repeater/files/repeater.sh
+++ net/repeater/files/repeater.sh
@@ -23,8 +23,7 @@ repeater_server_port=${repeater_server_port:-5500}
 repeater_viewer_port=${repeater_viewer_port:-5900}
 repeater_log=${repeater_log:-"/var/run/repeater.log"}
 command=/usr/local/sbin/repeater
-procname=repeater
-start_precmd="echo Starting ${procname}."
+start_precmd="echo Starting ${name}."
 start_cmd="daemon ${command} ${repeater_viewer_port} ${repeater_server_port} > ${repeater_log} 2>&1"
 
 run_rc_command "$1"
