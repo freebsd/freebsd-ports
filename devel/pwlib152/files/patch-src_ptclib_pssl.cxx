59d8d20da2c6c68c007a4b2bfea39166e2c151a9
tmclaugh <tmclaugh@FreeBSD.org>
2006-02-22 22:19:01 +0000

    Fix patch when when openssl-beta is not used.
    Approved by:	maintainer

--- devel/pwlib152/files/patch-src_ptclib_pssl.cxx
+++ devel/pwlib152/files/patch-src_ptclib_pssl.cxx
@@ -1,5 +1,5 @@
 --- src/ptclib/pssl.cxx.orig	Wed Apr 16 04:00:19 2003
-+++ src/ptclib/pssl.cxx	Wed Feb 15 23:54:42 2006
++++ src/ptclib/pssl.cxx	Wed Feb 22 09:24:08 2006
 @@ -285,14 +285,22 @@
  
  PSSLPrivateKey::PSSLPrivateKey(const BYTE * keyData, PINDEX keySize)
@@ -46,13 +46,15 @@
  }
  
  
-@@ -477,7 +493,8 @@
+@@ -477,7 +493,11 @@
    PBase64::Decode(certStr, certData);
    if (certData.GetSize() > 0) {
      const BYTE * certPtr = certData;
--    certificate = d2i_X509(NULL, (unsigned char **)&certPtr, certData.GetSize());
-+//    certificate = d2i_X509(NULL, (unsigned char **)&certPtr, certData.GetSize());
++#if (OPENSSL_VERSION_NUMBER < 0x0090801f)
+     certificate = d2i_X509(NULL, (unsigned char **)&certPtr, certData.GetSize());
++#else
 +    certificate = d2i_X509(NULL, (const unsigned char **)&certPtr, certData.GetSize());
++#endif
    }
    else
      certificate = NULL;
