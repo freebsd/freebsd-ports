2b80f5825b15db8f894367c7d01531a7072249a6
Renato Botelho <garga@FreeBSD.org>
2006-03-02 20:57:22 +0000

    - Update to 0.7 - Change it to run as jabber and not more as root, which is an unecessary   security risk [1]
    PR:		ports/93991 [1]
    Submitted by:	Neil Darlow <neil@darlow.co.uk> [1]
    Approved by:	portmgr (erwing)

--- net-im/jabber-pyicq/files/patch-src_main.py
+++ net-im/jabber-pyicq/files/patch-src_main.py
@@ -1,24 +1,16 @@
---- src/main.py.orig	Tue Jun  7 09:54:58 2005
-+++ src/main.py	Tue Jun  7 09:56:26 2005
+--- src/main.py.orig	Sat Feb 11 01:14:59 2006
++++ src/main.py	Thu Mar  2 17:01:07 2006
 @@ -1,3 +1,4 @@
 +#!%%PYTHON_CMD%%
- # Copyright 2004 James Bunton <james@delx.cjb.net>
+ # Copyrigh 2004-2005 Daniel Henninger <jadestorm@nc.rr.com>
  # Licensed for distribution under the GPL version 2, check COPYING for details
  
-@@ -13,15 +14,9 @@
- 	print("You are using version %s of Python, at least 2.3 is required." % (sys.version[:3]))
- 	os._exit(0)
+@@ -34,7 +35,7 @@
  
--name = "PyICQt"
--exe = os.path.realpath(sys.executable)
--if (exe.find("python") >= 0):
--	print("Restarting with process name %s..." % (name))
--	os.execv(exe, [name, sys.argv[0]]+sys.argv[1:])
--
  import config
  import xmlconfig
 -conffile = "config.xml"
 +conffile = "%%PREFIX%%/etc/jabber-pyicq.xml"
  options = {}
- opts, args = getopt.getopt(sys.argv[1:], "c:o:dDl:h", ["config=", "option=", "debug", "Debug", "log=", "help"])
- for o, v in opts:
+ daemonizeme = False
+ opts, args = getopt.getopt(sys.argv[1:], "bc:o:dDgtl:h", ["background", "config=", "option=", "debug", "Debug", "garbage", "traceback", "log=", "help"])
