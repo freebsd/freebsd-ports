3189b60a36dc30fcc715dd7e706427a5d1302c19
pav <pav@FreeBSD.org>
2006-03-11 10:12:57 +0000

    - Unbreak on 5.X
    Submitted by:	Kay Lehmann <kay_lehmann@web.de>
    Approved by:	portmgr (marcus)

---
+++ x11-toolkits/v/files/patch-includex-v-v_defs.h
@@ -0,0 +1,10 @@
+--- includex/v/v_defs.h.orig	Mon Apr 28 19:16:56 2003
++++ includex/v/v_defs.h	Thu Mar  9 19:03:12 2006
+@@ -310,6 +310,7 @@
+ #define notChk 0
+ #define noSub 0
+ #define notUsed 0
++#define notUsedVoid
+ #define noIcon 0
+ 
+     // standard menu definitions
