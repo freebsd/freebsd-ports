a68542090892971571f52955d98f36ec11ec4e6a
tg <tg@FreeBSD.org>
2006-03-15 08:23:17 +0000

    Upgrade to 2.4.9.
    PR:		93808
    Submitted by:	"Pedro F. Giffuni" <giffunip at asme dot org>

--- math/py-scientific/files/patch-setup.py
+++ math/py-scientific/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py~	Fri Jul  4 11:50:18 2003
-+++ setup.py	Fri Jul  4 11:51:08 2003
+--- setup.py.orig	Mon Oct 18 08:34:00 2004
++++ setup.py	Fri Feb 24 15:33:58 2006
 @@ -14,7 +14,7 @@
      try:
          netcdf_prefix=os.environ['NETCDF_PREFIX']
      except KeyError:
--        for netcdf_prefix in ['/usr/local', '/usr']:
-+        for netcdf_prefix in [sys.prefix, '/usr']:
+-        for netcdf_prefix in ['/usr/local', '/usr', '/sw']:
++        for netcdf_prefix in [sys.prefix, '/usr', '/sw']:
              netcdf_include = os.path.join(netcdf_prefix, 'include')
              netcdf_lib = os.path.join(netcdf_prefix, 'lib')
              if os.path.exists(os.path.join(netcdf_include, 'netcdf.h')):
