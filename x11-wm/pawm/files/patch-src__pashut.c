64156f45847fbe982d70b4b1fb592f4cb499cf96
Pav Lucistnik <pav@FreeBSD.org>
2006-03-16 10:32:54 +0000

    - Update to 2.2.0
    PR:		ports/94507
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ x11-wm/pawm/files/patch-src__pashut.c
@@ -0,0 +1,10 @@
+--- src/pashut.c.orig	Sat Feb 25 00:55:09 2006
++++ src/pashut.c	Sun Feb 26 03:15:23 2006
+@@ -24,6 +24,7 @@
+ #include "pabar.h"
+ #include "pafont.h"
+ #include "pashut.h"
++#include "signals.h"
+ 
+ 
+ static void shut_all(void *);
