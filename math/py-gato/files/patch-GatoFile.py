7dd43cc19a274de12546f8232ab91c668c77dd78
pav <pav@FreeBSD.org>
2006-03-16 10:43:34 +0000

    - Update to 0.98Q
    PR:		ports/94230
    Submitted by:	Jean-Francois BOEUF <jfb@minet.net>

---
+++ math/py-gato/files/patch-GatoFile.py
@@ -0,0 +1,8 @@
+--- GatoFile.py	Wed Mar  8 10:47:35 2006
++++ GatoFile.py	Wed Mar  8 10:47:47 2006
+@@ -1,4 +1,4 @@
+-#!/usr/bin/env python2.3
++#!/usr/bin/env python
+ ################################################################################
+ #
+ #       This file is part of Gato (Graph Animation Toolbox) 
