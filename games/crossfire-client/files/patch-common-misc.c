ee2ad2d25ce3e5416be9c5ba42de5bd6ec6e1c77
Pav Lucistnik <pav@FreeBSD.org>
2006-03-16 18:31:43 +0000

    - Update to 1.8.0 - SDL is no longer optional
    PR:		ports/92748
    Submitted by:	Mark Daniel Reidel <mark@alu.reidel.info> (maintainer)

--- games/crossfire-client/files/patch-common-misc.c
+++ games/crossfire-client/files/patch-common-misc.c
@@ -1,6 +1,6 @@
---- common/misc.c.orig	Sat May  8 15:31:29 2004
-+++ common/misc.c	Sat May  8 15:31:49 2004
-@@ -237,13 +237,13 @@
+--- common/misc.c.orig	Wed Feb  1 19:15:57 2006
++++ common/misc.c	Wed Feb  1 19:16:21 2006
+@@ -237,11 +237,11 @@
  #ifndef WIN32
      ChildProcess* cp=FirstChild;
      ChildProcess* last=NULL;
@@ -9,9 +9,7 @@
          if (!cp)
              return; /*no child to monitor*/
          if (waitpid(cp->pid,NULL,WNOHANG)){
+-            ChildProcess* next;
+ 
              /*pid is dead*/
              LOG(LOG_INFO,"common::monitorChilds","Child %s died. Removing and closing pipes",cp->name?cp->name:"UNKNOWN");
--            ChildProcess* next;
-             if (cp==LastChild)
-                 LastChild=last;
-             next=cp->next;
