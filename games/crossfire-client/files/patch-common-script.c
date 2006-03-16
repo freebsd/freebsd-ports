ab061a28959cb7864293642fd3952ea8655b94a2
pav <pav@FreeBSD.org>
2006-03-16 18:31:43 +0000

    - Update to 1.8.0 - SDL is no longer optional
    PR:		ports/92748
    Submitted by:	Mark Daniel Reidel <mark@alu.reidel.info> (maintainer)

---
+++ games/crossfire-client/files/patch-common-script.c
@@ -0,0 +1,10 @@
+--- common/script.c.orig	Wed Feb  1 19:14:36 2006
++++ common/script.c	Wed Feb  1 19:14:51 2006
+@@ -120,6 +120,7 @@
+ #include <sys/types.h>
+ #include <sys/socket.h>
+ #include <sys/wait.h>
++#include <sys/signal.h>
+ #include <signal.h>
+ #endif
+ 
