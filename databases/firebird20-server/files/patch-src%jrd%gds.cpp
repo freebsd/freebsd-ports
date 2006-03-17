0cb37d406d4db1d2af47fd699f8a6cb5171e04ae
skv <skv@FreeBSD.org>
2006-03-17 10:23:32 +0000

    Update to 1.5.3
    * move "intl/" and "UDF/" from "/var/db/firebird"
      into "libexec/firebird/"
    
    * add support for amd64 (UNSTABLE)

--- databases/firebird20-server/files/patch-src%jrd%gds.cpp
+++ databases/firebird20-server/files/patch-src%jrd%gds.cpp
@@ -1,11 +1,11 @@
---- src/jrd/gds.cpp.orig	Mon Nov 22 08:11:11 2004
-+++ src/jrd/gds.cpp	Wed Jul 27 13:10:40 2005
-@@ -1733,7 +1733,7 @@
- 	if (ib_prefix_msg == NULL) {
- 		if (!(ib_prefix_msg = getenv(FB_MSG_ENV))) {
- 			ib_prefix_msg = ib_prefix_msg_val;
--			gds__prefix(ib_prefix_msg, "");
-+			strlcpy(ib_prefix_msg_val, FB_MSG_PREFIX, MAXPATHLEN);
+--- src/jrd/gds.cpp.orig	Mon Jan 23 15:04:44 2006
++++ src/jrd/gds.cpp	Mon Jan 23 15:07:20 2006
+@@ -3697,7 +3697,7 @@
+ 		Firebird::string msgPrefix(getenv(FB_MSG_ENV) ? getenv(FB_MSG_ENV) : "");
+ 		if (msgPrefix.length() == 0) 
+ 		{
+-			msgPrefix = prefix;
++			msgPrefix = FB_MSG_PREFIX;
  		}
- 		else {
- 			strcat(ib_prefix_msg_val, ib_prefix_msg);
+ 		copyTo(msgPrefix, ib_prefix_msg_val, sizeof(ib_prefix_msg_val));
+ 		ib_prefix_msg = ib_prefix_msg_val;
