81a436466d1cb47f49c6e69a2c4ab3013d7241ac
garga <garga@FreeBSD.org>
2006-03-20 16:38:51 +0000

    - Update to 0.7a

--- net-im/jabber-pyicq/files/patch-src_main.py
+++ net-im/jabber-pyicq/files/patch-src_main.py
@@ -1,16 +1,16 @@
---- src/main.py.orig	Sat Feb 11 01:14:59 2006
-+++ src/main.py	Thu Mar  2 17:01:07 2006
+--- src/main.py.orig	Mon Mar  6 21:58:50 2006
++++ src/main.py	Mon Mar 20 13:34:50 2006
 @@ -1,3 +1,4 @@
 +#!%%PYTHON_CMD%%
- # Copyrigh 2004-2005 Daniel Henninger <jadestorm@nc.rr.com>
+ # Copyrigh 2004-2006 Daniel Henninger <jadestorm@nc.rr.com>
  # Licensed for distribution under the GPL version 2, check COPYING for details
  
-@@ -34,7 +35,7 @@
+@@ -25,7 +26,7 @@
  
  import config
  import xmlconfig
 -conffile = "config.xml"
 +conffile = "%%PREFIX%%/etc/jabber-pyicq.xml"
+ profilelog = None
  options = {}
  daemonizeme = False
- opts, args = getopt.getopt(sys.argv[1:], "bc:o:dDgtl:h", ["background", "config=", "option=", "debug", "Debug", "garbage", "traceback", "log=", "help"])
