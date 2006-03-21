48e4827f7fad9cc50c446b23410942493ce3c76a
ale <ale@FreeBSD.org>
2006-03-21 15:06:15 +0000

    - Add CCACHE_CACHE_COMPILER patch - Improve examples - Add two new scripts to enhance buildworld/buildkernel
    ccache may now be used safely and effectively to build the FreeBSD source tree.
    Follow the pkg-message tips to exploit it.
    
    Approved by:	maintainer

--- devel/ccache/files/patch-ccache.c
+++ devel/ccache/files/patch-ccache.c
@@ -1,12 +1,14 @@
---- ccache.c.orig	Mon Sep 13 06:38:30 2004
-+++ ccache.c	Tue Nov 15 14:59:46 2005
-@@ -331,8 +331,10 @@
+--- ccache.c.orig	2004/09/13 10:19:06	1.96
++++ ccache.c	2005/11/24 21:54:09	1.98
+@@ -331,8 +331,12 @@
  		hash_string(str_basename(args->argv[0]));
  	}
  
 -	hash_int(st.st_size);
 -	hash_int(st.st_mtime);
-+	if (!getenv("CCACHE_NOHASH_SIZE_MTIME")) {
++	if (getenv("CCACHE_HASH_COMPILER")) {
++		hash_file(args->argv[0]);
++	} else if (!getenv("CCACHE_NOHASH_SIZE_MTIME")) {
 +		hash_int(st.st_size);
 +		hash_int(st.st_mtime);
 +	}
