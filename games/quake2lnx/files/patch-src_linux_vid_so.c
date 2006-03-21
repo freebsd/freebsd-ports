29a8fa28da04515849e638601e0efb706c2d5c40
Alexey Dokuchaev <danfe@FreeBSD.org>
2006-03-21 16:24:32 +0000

    Further work on my beloved port:
    - Rework the way we handle BASEDIR/LIBDIR, keeping ability to set basedir
      from command line.  Only ref_*.so files path left hardcoded
    - Tiny fix for dedicated server build (Makefile typo)
    - Most importantly, help Quake II always find libGL.so (no more LD_PRELOAD
      magic).  Bump PORTREVISION for that [*]
    - Restore my nifty comments in pkg-plist
    
    References:	http://utah-glx.sourceforge.net/faq.html#AEN262 [*]

--- games/quake2lnx/files/patch-src_linux_vid_so.c
+++ games/quake2lnx/files/patch-src_linux_vid_so.c
@@ -1,5 +1,5 @@
---- src/linux/vid_so.c.orig	Fri Sep 24 19:11:44 2004
-+++ src/linux/vid_so.c	Tue Sep 27 19:28:29 2005
+--- src/linux/vid_so.c.orig	Tue Mar 21 19:43:33 2006
++++ src/linux/vid_so.c	Tue Mar 21 19:47:12 2006
 @@ -216,7 +216,6 @@
  	refimport_t	ri;
  	GetRefAPI_t	GetRefAPI;
@@ -15,7 +15,7 @@
 -	path = Cvar_Get ("basedir", ".", CVAR_NOSET)->string;
 -
 -	snprintf (fn, MAX_OSPATH, "%s/%s", path, name );
-+	snprintf (fn, MAX_OSPATH, LIBDIR"/%s", name );
++	snprintf (fn, MAX_OSPATH, "%%LIBDIR%%/%s", name );
  	
  	if (stat(fn, &st) == -1) {
  		Com_Printf( "LoadLibrary(\"%s\") failed: %s\n", name, strerror(errno));
@@ -28,7 +28,7 @@
  
 -	path = Cvar_Get ("basedir", ".", CVAR_NOSET)->string;
 -	snprintf (fn, MAX_OSPATH, "%s/ref_%s.so", path, ref );
-+	snprintf (fn, MAX_OSPATH, LIBDIR"/ref_%s.so", ref );
++	snprintf (fn, MAX_OSPATH, "%%LIBDIR%%/ref_%s.so", ref );
  	
  	if (stat(fn, &st) == 0)
  		return true;
