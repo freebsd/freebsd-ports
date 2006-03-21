28c8a406d2458a75ef614dbc60ac4a5daf01b9df
lofi <lofi@FreeBSD.org>
2006-03-21 20:29:51 +0000

    Update to 3.3.6

--- x11-toolkits/qt33/files/patch-src_tools-qsettings.cpp
+++ x11-toolkits/qt33/files/patch-src_tools-qsettings.cpp
@@ -1,15 +1,14 @@
 --- src/tools/qsettings.cpp.orig	Fri Sep  2 14:42:53 2005
-+++ src/tools/qsettings.cpp	Tue Jan 31 10:23:14 2006
++++ src/tools/qsettings.cpp	Mon Mar 20 18:36:27 2006
 @@ -500,7 +500,12 @@
      } );
  #endif // Q_OS_TEMP
  #else
--    defPath = qInstallPathSysconf();
 +  if (getenv( "TMPDIR" ) != 0) {
 +    defPath = QString( getenv( "TMPDIR" ) );
 +  }
 +  else {
-+    defPath= "/tmp";
+     defPath = qInstallPathSysconf();
 +  }
  #endif
      QDir dir(appSettings);
