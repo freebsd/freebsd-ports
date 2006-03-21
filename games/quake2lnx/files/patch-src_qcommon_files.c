57d4c6530df87034d20861081f386847fe353c43
danfe <danfe@FreeBSD.org>
2006-03-21 16:24:32 +0000

    Further work on my beloved port:
    - Rework the way we handle BASEDIR/LIBDIR, keeping ability to set basedir
      from command line.  Only ref_*.so files path left hardcoded
    - Tiny fix for dedicated server build (Makefile typo)
    - Most importantly, help Quake II always find libGL.so (no more LD_PRELOAD
      magic).  Bump PORTREVISION for that [*]
    - Restore my nifty comments in pkg-plist
    
    References:	http://utah-glx.sourceforge.net/faq.html#AEN262 [*]

--- games/quake2lnx/files/patch-src_qcommon_files.c
+++ games/quake2lnx/files/patch-src_qcommon_files.c
@@ -1,12 +1,11 @@
---- src/qcommon/files.c.orig	Sun Jan  2 00:38:12 2005
-+++ src/qcommon/files.c	Tue Sep 27 18:25:41 2005
-@@ -912,7 +912,8 @@
- 	//
+--- src/qcommon/files.c.orig	Tue Mar 21 19:36:01 2006
++++ src/qcommon/files.c	Tue Mar 21 19:41:27 2006
+@@ -913,6 +913,8 @@
  	// add baseq2 to search path
  	//
--	FS_AddGameDirectory (va("%s/"BASEDIRNAME, fs_basedir->string) );
-+	FS_AddGameDirectory(LIBDIR"/"BASEDIRNAME);
-+	FS_AddGameDirectory(DATADIR"/"BASEDIRNAME);
+ 	FS_AddGameDirectory (va("%s/"BASEDIRNAME, fs_basedir->string) );
++	FS_AddGameDirectory ("%%LIBDIR%%/"BASEDIRNAME);
++	FS_AddGameDirectory ("%%DATADIR%%/"BASEDIRNAME);
  
  	//
  	// then add a '.quake2/baseq2' directory in home directory by default
