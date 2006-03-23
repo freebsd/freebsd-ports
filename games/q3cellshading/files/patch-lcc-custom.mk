96fe98e1c6bc0301781b0dd7dbfeef3e3f137f43
cvs2svn <cvs2svn@FreeBSD.org>
2006-03-23 22:24:03 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_5_5_0'.

--- games/q3cellshading/files/patch-lcc-custom.mk
+++
@@ -1,6 +0,0 @@
-diff -ruN ./lcc/custom.mk ../quake3-1.32b-BUILT/./lcc/custom.mk
---- ./lcc/custom.mk	Wed Oct 17 23:53:10 2001
-+++ ../quake3-1.32b-BUILT/./lcc/custom.mk	Mon Aug 22 20:50:29 2005
-@@ -1 +1 @@
--BUILDDIR=/tmp
-+BUILDDIR=./build
--- textproc/linux-f10-aspell/pkg-plist.es
+++
@@ -1,6 +0,0 @@
-usr/lib/aspell/es.multi
-usr/lib/aspell/es.rws
-usr/lib/aspell/español.alias
-usr/lib/aspell/esponol.alias
-usr/lib/aspell/spanish.alias
-usr/share/aspell/es.dat
