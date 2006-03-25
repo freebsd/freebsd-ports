558937276c52703c32d1c89f11afde6147410719
Joe Marcus Clarke <marcus@FreeBSD.org>
2006-03-25 04:07:34 +0000

    Update to 1.9.102.

--- databases/libgda2/files/patch-libgda__gda-value.c
+++ databases/libgda2/files/patch-libgda__gda-value.c
@@ -1,6 +1,6 @@
---- ../../work-/libgda-1.9.100/libgda/gda-value.c	Tue Nov  8 14:56:44 2005
-+++ libgda/gda-value.c	Tue Nov  8 14:57:29 2005
-@@ -1263,8 +1263,8 @@
+--- libgda/gda-value.c.orig	Fri Mar 17 11:08:32 2006
++++ libgda/gda-value.c	Fri Mar 24 22:31:12 2006
+@@ -1281,8 +1281,8 @@ gda_value_is_null (GdaValue *value)
  gboolean
  gda_value_is_number (GdaValue *value)
  {
@@ -10,15 +10,3 @@
  	
  	type = GDA_VALUE_TYPE(value);
  	switch (type) {
-@@ -1437,10 +1437,10 @@
- void
- gda_value_set_blob (GdaValue *value, const GdaBlob *val)
- {
-+	GdaBlob *blob;
- 	g_return_if_fail (value);
- 	g_return_if_fail (GDA_IS_BLOB (val));
- 	
--	GdaBlob *blob;
- 	l_g_value_unset (value);
- 	g_value_init (value, G_VALUE_TYPE_BLOB);
- 	
