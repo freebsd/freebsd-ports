ffead0f84ed692c795204d6bbbdd72fee6950cad
Pav Lucistnik <pav@FreeBSD.org>
2006-03-26 14:46:38 +0000

    - Update to 0.9
    PR:		ports/94922
    Submitted by:	Philip Jenvey <pjenvey@groovie.org> (maintainer)

--- news/hellanzb/files/patch-nodocs-setup.py
+++ news/hellanzb/files/patch-nodocs-setup.py
@@ -1,8 +1,8 @@
---- setup.py.bak	Thu Jun 30 14:49:48 2005
-+++ setup.py	Thu Jun 30 14:50:22 2005
-@@ -35,8 +35,7 @@
- 
-         packages = [ 'Hellanzb', 'Hellanzb.NZBLeecher', 'Hellanzb.HellaXMLRPC' ],
+--- setup.py.bak	Thu Feb  9 20:21:21 2006
++++ setup.py	Thu Feb  9 20:21:58 2006
+@@ -36,8 +36,7 @@
+         packages = [ 'Hellanzb', 'Hellanzb.NZBLeecher', 'Hellanzb.HellaXMLRPC',
+                      'Hellanzb.external', 'Hellanzb.external.elementtree' ],
          scripts = [ 'hellanzb.py' ],
 -        data_files = [ ( 'etc', [ 'etc/hellanzb.conf.sample' ] ),
 -                       ( 'share/doc/hellanzb', [ 'CHANGELOG', 'CREDITS', 'README', 'LICENSE' ] ) ],
