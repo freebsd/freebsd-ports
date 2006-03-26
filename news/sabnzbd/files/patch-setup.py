0d05f40e15b1af862f30f49f1c969f5312cf9f28
Pav Lucistnik <pav@FreeBSD.org>
2006-03-26 14:50:58 +0000

    - Update to 0.1.8.1
    PR:		ports/94924
    Submitted by:	Daniel <daniel@netwalk.org> (maintainer)

--- news/sabnzbd/files/patch-setup.py
+++ news/sabnzbd/files/patch-setup.py
@@ -1,18 +1,18 @@
-*** setup.py.orig	Sat Mar  4 13:16:01 2006
---- setup.py	Sat Mar 18 21:59:59 2006
+*** setup.py.orig	Fri Mar 24 23:26:24 2006
+--- setup.py	Fri Mar 24 23:27:22 2006
 ***************
 *** 15,20 ****
         license = 'Academic Free License 2.1',
-!       data_files = [('/usr/share/doc/SABnzbd-' + sabnzbd.__version__,
-!                              ['SABnzbd.ini.sample', 'README.txt', 'LICENSE.txt',
-                                'TODO.txt', 'CHANGELOG.txt', 'UPGRADE.txt']),
-!                     ('/usr/share/doc/SABnzbd-' + sabnzbd.__version__ + '/templates',
+!       data_files = [('share/doc/SABnzbd-' + sabnzbd.__version__,
+!                      ['SABnzbd.ini.sample', 'README.txt', 'LICENSE.txt',
+!                       'TODO.txt', 'CHANGELOG.txt', 'UPGRADE.txt']),
+!                     ('share/SABnzbd-' + sabnzbd.__version__ + '/templates',
                                ['templates/default.css', 'templates/history.tmpl',
 --- 15,21 ----
         license = 'Academic Free License 2.1',
 !       data_files = [('share/doc/SABnzbd',
 !                              ['README.txt', 'LICENSE.txt',
-                                'TODO.txt', 'CHANGELOG.txt', 'UPGRADE.txt']),
+!                               'TODO.txt', 'CHANGELOG.txt', 'UPGRADE.txt']),
 !                     ('share/SABnzbd',['SABnzbd.ini.sample']),
 !                     ('share/SABnzbd' + '/templates',
                                ['templates/default.css', 'templates/history.tmpl',
