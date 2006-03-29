f7b51c1887a20843f44c32c2994d1c1fac5fe90b
pav <pav@FreeBSD.org>
2006-03-29 06:55:17 +0000

    - Update to 2.1.99   Even if this is a development release, authors prefer it over 2.1.6
    PR:		ports/94429
    Submitted by:	Pedro F. Giffuni <giffunip@asme.org>

--- graphics/libgphoto2/files/patch-libgphoto2_port-serial-unix.c
+++ graphics/libgphoto2/files/patch-libgphoto2_port-serial-unix.c
@@ -1,9 +1,9 @@
---- libgphoto2_port/serial/unix.c.orig	Wed Jan 18 18:06:35 2006
-+++ libgphoto2_port/serial/unix.c	Wed Jan 18 18:08:39 2006
+--- libgphoto2_port/serial/unix.c.orig	Mon May  2 14:36:52 2005
++++ libgphoto2_port/serial/unix.c	Mon Mar 13 16:35:59 2006
 @@ -108,7 +108,11 @@
  
  /* FreeBSD */
- #if defined(__FreeBSD__)
+ #ifdef __FreeBSD__
 +#if __FreeBSD_version < 600000
  #define GP_PORT_SERIAL_PREFIX   "/dev/cuaa%x"
 +#else
