a9c1b4482c12fb51d0ec874b1a55f4fd2d95ad59
ijliao <ijliao@FreeBSD.org>
2006-03-31 03:43:05 +0000

    upgrade to 2.2
    PR:		95044
    Submitted by:	maintainer

--- security/denyhosts/files/patch-setup.py
+++ security/denyhosts/files/patch-setup.py
@@ -3,24 +3,25 @@ $FreeBSD$
 
 --- setup.py.orig
 +++ setup.py
-@@ -7,7 +7,7 @@
+@@ -8,7 +8,7 @@
  from glob import glob
  
  
 -libpath = "/usr/share/denyhosts"
 +libpath = "%%PREFIX%%/share/denyhosts"
+ scriptspath = "%s/scripts" % libpath
+ pluginspath = "%s/plugins" % libpath
  
- #########################################################################
- 
-@@ -24,10 +24,7 @@
+@@ -26,11 +26,8 @@
        data_files=[(libpath, glob("denyhosts.cfg-dist")),
-                   (libpath, glob("denyhosts-daemon-initscript")),
                    (libpath, glob("setup.py")),
--                  (libpath, glob("daemon-control-dist")),
+                   (libpath, glob("daemon-control-dist")),
 -                  (libpath, glob("CHANGELOG.txt")),
 -                  (libpath, glob("README.txt")),
+                   (scriptspath, glob("scripts/*")),
+-                  (pluginspath, glob("plugins/*")),
 -                  (libpath, glob("LICENSE.txt"))],
-+                  (libpath, glob("daemon-control-dist"))],
-       license="GPL",
++                  (pluginspath, glob("plugins/*"))],
+       license="GPL v2",
        ##extra_path='denyhosts',
        long_description="""
