000699efe2596e7bb51e0e2dd84ee0d328576484
nork <nork@FreeBSD.org>
2006-04-01 12:08:47 +0000

    Fix build on gcc 4.1.
    PR:		ports/95136
    Submitted by:	Ron MacNeil <macro@hotmail.com>

---
+++ graphics/OpenEXR/files/patch-exrmaketiled::Image.h
@@ -0,0 +1,11 @@
+--- exrmaketiled/Image.h.orig	Fri Jun  4 13:11:23 2004
++++ exrmaketiled/Image.h	Sat Apr  1 20:36:11 2006
+@@ -50,6 +50,8 @@
+ #include <map>
+ 
+ 
++class Image;
++
+ class ImageChannel
+ {
+   public:
