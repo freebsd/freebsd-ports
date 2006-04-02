cb1ceae374e1e0ff218869f506085d78250d708d
pav <pav@FreeBSD.org>
2006-04-02 14:26:46 +0000

    - Update to 1.02 - Take maintainership
    PR:		ports/93293
    Submitted by:	Anders Troback <freebsd@troback.com>

--- net-mgmt/bigsister/files/patch-tools::bb_start.pl
+++ net-mgmt/bigsister/files/patch-tools::bb_start.pl
@@ -1,20 +1,11 @@
---- tools/bb_start.pl.orig	Mon Aug 11 21:29:38 2003
-+++ tools/bb_start.pl	Sat Feb 28 17:28:29 2004
-@@ -161,7 +161,7 @@
+--- tools/bb_start.pl.org	Thu Feb  9 11:28:48 2006
++++ tools/bb_start.pl	Thu Feb  9 11:29:11 2006
+@@ -178,7 +178,7 @@
  
  sub proclist {
      my @result;
 -    foreach my $cmd ( "ps cax", "ps -e", "ps -ef" ) {
-+    foreach my $cmd ( "/bin/ps ax" ) {
++    foreach my $cmd ( "ps ax" ) {
  	open( CMD, "$cmd 2>/dev/null|" );
  	@result = ();
  	while( <CMD> ) {
-@@ -197,7 +197,7 @@
- 	    exec( $cmd );
- 	}
- 	else {
--	    exec( "su", "-", "$user", "-c", $cmd );
-+	    exec( "/usr/bin/su", "-m", "$user", "-c", $cmd );
- 	}
- 	exit(1);
-     }
