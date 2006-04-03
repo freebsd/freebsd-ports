2d5d5cd17c081123910b0f6f49598c16dada4f38
markus <markus@FreeBSD.org>
2006-04-03 14:08:55 +0000

    - Update to version 2.0.0 - remove runtime dependency to graphics/barcode as kbarcode supports several   backends and has a runtime detection with advice for the user
    PR:		ports/95112
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- graphics/kbarcode/files/patch-kbarcode::zplutils.cpp
+++ graphics/kbarcode/files/patch-kbarcode::zplutils.cpp
@@ -1,11 +1,11 @@
---- kbarcode/zplutils.cpp.orig	Wed Nov  5 20:26:46 2003
-+++ kbarcode/zplutils.cpp	Fri Dec 26 10:22:53 2003
-@@ -307,7 +307,7 @@
-     setField( "<ESC>E3<CAN>" );  // choose format number 3
+--- kbarcode/zplutils.cpp.orig	Mon Mar 21 00:31:21 2005
++++ kbarcode/zplutils.cpp	Thu Mar 16 12:27:36 2006
+@@ -256,7 +256,7 @@
+     ipl += field( "<ESC>E3<CAN>" );  // choose format number 3
  
-     for( unsigned int i = 0; i < values.count(); i++ )
--        setField( values[i] + ( i != values.count() - 1 ? "<CR>" : QString::null ) );
-+        setField( values[i] + ( i != values.count() - 1 ? QString("<CR>") : QString::null ) );
-     
+     for( unsigned int i = 0; i < m_values.count(); i++ )
+-        ipl += field( m_values[i] + ( i != m_values.count() - 1 ? "<CR>" : QString::null ) );
++        ipl += field( m_values[i] + ( i != m_values.count() - 1 ? QString("<CR>") : QString::null ) );
+ 
      // end actual data
-     setField( "<ETB>" );
+     ipl += field( "<ETB>" );
