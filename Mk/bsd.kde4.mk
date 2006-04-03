88e34b465fd68e9f2229e22a75b3c675de552576
Michael Nottebrock <lofi@FreeBSD.org>
2006-04-03 09:34:22 +0000

    Set TMPDIR for all users of uic in order to avoid spamming files outside WRKDIR.

--- Mk/bsd.kde4.mk
+++ Mk/bsd.kde4.mk
@@ -114,6 +114,11 @@ QTCPPFLAGS+=	-I${LOCALBASE}/include -I${PREFIX}/include \
 				-I${QT_PREFIX}/include -D_GETOPT_H
 QTCFGLIBS+=		-Wl,-export-dynamic -L${LOCALBASE}/lib -L${X11BASE}/lib -ljpeg \
 				-L${QT_PREFIX}/lib
+.if defined(PACKAGE_BUILDING)
+TMPDIR?=	/tmp
+MAKE_ENV+=	TMPDIR=${TMPDIR}
+.endif
+
 .if !defined(QT_NONSTANDARD)
 CONFIGURE_ARGS+=--with-qt-includes=${QT_PREFIX}/include \
 				--with-qt-libraries=${QT_PREFIX}/lib \
