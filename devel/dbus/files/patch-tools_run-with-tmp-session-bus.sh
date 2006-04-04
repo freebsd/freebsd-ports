729769e0e5f223fdf5ac0f0d3d3825226b29c1c3
bland <bland@FreeBSD.org>
2006-04-04 12:31:16 +0000

    - Drop bash build dependency. [1] - Remove obsolete USE_REINPLACE.
    Submitted by:	Jean-Baptiste Quenot <jbq@caraldi.com> [1]

--- devel/dbus/files/patch-tools_run-with-tmp-session-bus.sh
+++ devel/dbus/files/patch-tools_run-with-tmp-session-bus.sh
@@ -1,8 +1,15 @@
---- tools/run-with-tmp-session-bus.sh.orig	Sat Apr  2 14:52:51 2005
-+++ tools/run-with-tmp-session-bus.sh	Sat Apr  2 14:53:05 2005
-@@ -1,4 +1,4 @@
+--- tools/run-with-tmp-session-bus.sh.orig	Fri Mar 18 02:48:29 2005
++++ tools/run-with-tmp-session-bus.sh	Tue Apr  4 20:40:12 2006
+@@ -1,10 +1,10 @@
 -#! /bin/bash
-+#! %%LOCALBASE%%/bin/bash
++#! /bin/sh
  
  SCRIPTNAME=$0
  WRAPPED_SCRIPT=$1
+ shift
+ 
+-function die() 
++die() 
+ {
+     if ! test -z "$DBUS_SESSION_BUS_PID" ; then
+         echo "killing message bus "$DBUS_SESSION_BUS_PID >&2
