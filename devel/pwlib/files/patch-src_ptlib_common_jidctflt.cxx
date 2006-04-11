bb752d917e52b2862fad24ae2de85365093e4377
bland <bland@FreeBSD.org>
2006-04-11 03:13:26 +0000

    Unbreak i686+ builds. Do not use bogus asm optimized code block.
    PR:	95400

--- devel/pwlib/files/patch-src_ptlib_common_jidctflt.cxx
+++ devel/pwlib/files/patch-src_ptlib_common_jidctflt.cxx
@@ -1,11 +1,12 @@
 --- src/ptlib/common/jidctflt.cxx.orig	Wed Apr  5 07:08:00 2006
 +++ src/ptlib/common/jidctflt.cxx	Wed Apr  5 07:08:08 2006
-@@ -80,7 +80,7 @@
+@@ -80,7 +80,8 @@
  
  #define DEQUANTIZE(coef,quantval)  (((FAST_FLOAT) (coef)) * (quantval))
  
 -#if defined(__GNUC__) && (defined(__i686__) || defined(__x86_64__))
-+#if defined(__GNUC__) && defined(__i686__)
- 
++/* asm doesn't compile properly... avoid using */
++#if 0 
+
  static inline unsigned char descale_and_clamp(int x, int shift)
  {
