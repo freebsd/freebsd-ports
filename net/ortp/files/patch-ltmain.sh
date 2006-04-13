e8be24295386f019f43fbf04beebb1a389e614fc
Maxim Sobolev <sobomax@FreeBSD.org>
2006-04-13 03:54:16 +0000

    Update to 0.9.1.
    Submitted by:	Yi-Huan Chan

---
+++ net/ortp/files/patch-ltmain.sh
@@ -0,0 +1,16 @@
+
+$FreeBSD$
+
+--- ltmain.sh
++++ ltmain.sh
+@@ -6005,8 +6005,8 @@
+ 	# Install the pseudo-library for information purposes.
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+-	$show "$install_prog $instname $destdir/$name"
+-	$run eval "$install_prog $instname $destdir/$name" || exit $?
++#	$show "$install_prog $instname $destdir/$name"
++#	$run eval "$install_prog $instname $destdir/$name" || exit $?
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
