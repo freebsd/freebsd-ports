a876c24d164bcce9991a3e9035c8489605a74d7c
sem <sem@FreeBSD.org>
2006-04-15 18:41:33 +0000

    - Fix df(1) output in query_unix_partitions.pl - Fix path for ping in ping.pl - Rename a patch to a common scheme - Bump PORTREVISION
    Submitted by:	Giraudet Olivier <giraudeo@altern.org>

---
+++ net-mgmt/cacti/files/patch-scripts-ping.pl
@@ -0,0 +1,10 @@
+--- scripts/ping.pl.orig	Sat Apr 15 22:26:44 2006
++++ scripts/ping.pl	Sat Apr 15 22:27:16 2006
+@@ -1,6 +1,6 @@
+ #!/usr/bin/perl 
+ 
+-open(PROCESS, "ping -c 1 $ARGV[0] | grep icmp_seq |");
++open(PROCESS, "/sbin/ping -c 1 $ARGV[0] | grep icmp_seq |");
+ $ping = <PROCESS>;
+ close(PROCESS);
+ $ping =~ m/(.*time=)(.*) (ms|usec)/; 
