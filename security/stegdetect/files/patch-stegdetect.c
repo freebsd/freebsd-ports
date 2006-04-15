807c03af91a8f04507abf7f9358bdcb1c5fdbf70
pav <pav@FreeBSD.org>
2006-04-15 15:43:17 +0000

    - Update to 0.6 - Take maintainership
    PR:		ports/95793
    Submitted by:	Rainer Alves <rainer.alves@gmail.com>

--- security/stegdetect/files/patch-stegdetect.c
+++ security/stegdetect/files/patch-stegdetect.c
@@ -1,29 +1,29 @@
---- stegdetect.c	Sat Jan 26 23:51:16 2002
-+++ /tmp/stegdetect.c.diff	Wed Dec 29 03:32:38 2004
-@@ -1198,7 +1198,7 @@
- 		flag = 1;
- 		strlcat(outbuf, " f5(***)", sizeof(outbuf));
- 
+--- stegdetect.c.orig	Sat Apr 15 00:14:05 2006
++++ stegdetect.c	Sat Apr 15 00:14:40 2006
+@@ -1227,7 +1227,7 @@
+ 			strlcat(outbuf, quality(tmp, stars), sizeof(outbuf));
+ 			flag = 1;
+ 		}
 -	no_f5:
 +	no_f5:;
+ 	a_wasted_var = 0;
  	}
  
- 	if (scans & FLAG_DOINVIS) {
-@@ -1237,7 +1237,7 @@
+@@ -1267,7 +1267,7 @@
  			strlcat(outbuf, tmp, sizeof(outbuf));
  		}
  		
 -	no_invisiblesecrets:
 +	no_invisiblesecrets:;
+ 	a_wasted_var = 0;
  	}
  
- 	if ((scans & FLAG_CHECKHDRS)) {
-@@ -1301,7 +1301,7 @@
+@@ -1332,7 +1332,7 @@
  		}
  
  		free(dcts);
 -	jsteg_error:
 +	jsteg_error:;
+ 	a_wasted_var = 0;
  	}
  
- 	if ((scans & FLAG_DOOUTGUESS) && prepare_normal(&dcts, &bits) != -1) {
