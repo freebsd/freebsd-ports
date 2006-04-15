f107c73104df0028fbb83553c5a44317f8c910e1
arved <arved@FreeBSD.org>
2006-04-15 19:39:37 +0000

    Update to 1.5.1
    PR:		95817
    Submitted by:	KATO Tsuguru

--- games/qgo/files/patch-src__mainwin.cpp
+++ games/qgo/files/patch-src__mainwin.cpp
@@ -1,15 +1,16 @@
---- src/mainwin.cpp.orig	Thu Nov 24 08:38:33 2005
-+++ src/mainwin.cpp	Wed Jan 18 03:09:52 2006
-@@ -1604,10 +1604,10 @@
-       //set the params of "who command"
- 	if ((whoBox1->currentItem() >1)  || (whoBox2->currentItem() >1))
-         {
--		wparam.append(whoBox1->currentItem()==1 ? "9p" : whoBox1->currentText());
-+		wparam.append(whoBox1->currentItem()==1 ? QString("9p") : whoBox1->currentText());
- 		if ((whoBox1->currentItem())  && (whoBox2->currentItem()))
- 			wparam.append("-");
--		wparam.append(whoBox2->currentItem()==1 ? "9p" : whoBox2->currentText());
-+		wparam.append(whoBox2->currentItem()==1 ? QString("9p") : whoBox2->currentText());
-          } 
- 	else if ((whoBox1->currentItem())  || (whoBox2->currentItem()))
-         	wparam.append("1p-9p");
+--- src/mainwin.cpp.orig	Mon Mar 27 06:37:41 2006
++++ src/mainwin.cpp	Wed Apr 12 13:36:58 2006
+@@ -1589,11 +1589,11 @@
+       			//set the params of "who command"
+ 			if ((whoBox1->currentItem() >1)  || (whoBox2->currentItem() >1))
+         		{
+-				wparam.append(whoBox1->currentItem()==1 ? "9p" : whoBox1->currentText());
++				wparam.append(whoBox1->currentItem()==1 ? QString("9p") : whoBox1->currentText());
+ 				if ((whoBox1->currentItem())  && (whoBox2->currentItem()))
+ 					wparam.append("-");
+ 
+-				wparam.append(whoBox2->currentItem()==1 ? "9p" : whoBox2->currentText());
++				wparam.append(whoBox2->currentItem()==1 ? QString("9p") : whoBox2->currentText());
+          		} 
+ 			else if ((whoBox1->currentItem())  || (whoBox2->currentItem()))
+         			wparam.append("1p-9p");
