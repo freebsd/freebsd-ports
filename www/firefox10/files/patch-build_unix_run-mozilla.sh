54b43fce0db4e371cf2aed9d86fe4c46d0b3f0f2
ahze <ahze@FreeBSD.org>
2006-04-16 23:57:45 +0000

    Firefox is an award winning, free, open-source web browser for many platforms and is based on the Mozilla codebase. It is small, fast and easy to use, and offers many advanced features:  o Popup Blocking  o Tabbed Browsing  o Live Bookmarks (ie. RSS)  o Extensions  o Themes  o FastFind  o Improved Security
    Repo-copied from:	www/firefox
    Repo-copied by:		marcus

--- www/firefox10/files/patch-build_unix_run-mozilla.sh
+++ www/firefox10/files/patch-build_unix_run-mozilla.sh
@@ -5,7 +5,7 @@
  ##
  ## Set LD_LIBRARY_PATH
 -LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
-+LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%PREFIX%%/lib/browser_plugins:%%PREFIX%%/lib/browser_linux_plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
++LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%PREFIX%%/lib/browser_plugins:%%PREFIX%%/lib/browser_linux_plugins:{MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
  if [ -n "$LD_LIBRARYN32_PATH" ]
  then
  	LD_LIBRARYN32_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARYN32_PATH+":$LD_LIBRARYN32_PATH"}
