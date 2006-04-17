e15f608393e54b0714fff4d1fd6da22b310c74c1
Alejandro Pulver <alepulver@FreeBSD.org>
2006-04-17 20:06:04 +0000

    - Update to version 0.6.11. - Change MAINTAINER address to acardenas@bsd.org.pe. - Add USE_X_PREFIX. - Add USE_GETOPT_LONG and a patch to make it build on 4.X. - Remove USE_REINPLACE. - Remove RESTRICTED and add pkg-message.in. Now the Neo-Geo BIOS is not   packaged with the port, so packages can be generated now. The pkg-message.in   file contains instructions about how to install the BIOS. - Fix OPTIONS handling. - Add message to pkg-plist about files that weren't removed.
    PR:		ports/94023
    Submitted by:	Jose Alonso Cardenas Marquez <acardenas@bsd.org.pe>
    Approved by:	garga (mentor)

---
+++ emulators/gngeo/files/extra-patch-src__emu.h
@@ -0,0 +1,11 @@
+--- src/emu.h.orig	Wed Feb  8 17:21:55 2006
++++ src/emu.h	Fri Apr 14 12:52:00 2006
+@@ -24,7 +24,7 @@
+ #endif
+ 
+ #include <string.h>
+-#include <stdint.h>
++#include <inttypes.h>
+ #include "SDL.h"
+ #include "SDL_types.h"
+ #define Uintptr long
