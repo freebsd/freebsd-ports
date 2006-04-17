cc5f4bac257c462471132dce2edbc0daf1db2038
Volker Stolz <vs@FreeBSD.org>
2006-04-17 11:06:55 +0000

    Fix build on 4.x
    PR:		ports/95906
    Submitted by:	Jeffrey H. Johnson

---
+++ audio/wavpack/files/patch-wavpack.c
@@ -0,0 +1,22 @@
+
+$FreeBSD$
+
+--- wavpack.c.orig
++++ wavpack.c
+@@ -1604,6 +1604,7 @@
+     MultiByteToWideChar (CP_ACP, 0, string, -1, temp, max_chars + 1);
+     WideCharToUTF8 (temp, (uchar *) string, len);
+ #else
++    iconv_t converter;
+     char *temp = malloc (len);
+     char *outp = temp;
+     char *inp = string;
+@@ -1614,7 +1615,7 @@
+ 
+     memset(temp, 0, len);
+     old_locale = setlocale (LC_CTYPE, "");
+-    iconv_t converter = iconv_open ("UTF-8", "");
++    converter = iconv_open ("UTF-8", "");
+     err = iconv (converter, &inp, &insize, &outp, &outsize);
+     iconv_close (converter);
+     setlocale (LC_CTYPE, old_locale);
