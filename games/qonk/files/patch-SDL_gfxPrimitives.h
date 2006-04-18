5e7922eca8669c0bf00604cb9e3839b69039e40c
Alejandro Pulver <alepulver@FreeBSD.org>
2006-04-18 18:01:39 +0000

    - Update to version 0.0.2beta1. - Remove USE_REINPLACE.
    Approved by:	garga (mentor)

---
+++ games/qonk/files/patch-SDL_gfxPrimitives.h
@@ -0,0 +1,11 @@
+--- SDL_gfxPrimitives.h.orig	Sat Aug  6 08:53:38 2005
++++ SDL_gfxPrimitives.h	Thu Apr 13 23:20:20 2006
+@@ -14,7 +14,7 @@
+ #define M_PI	3.141592654
+ #endif
+ 
+-#include <SDL/SDL.h>
++#include <SDL.h>
+ 
+ /* Set up for C function definitions, even when using C++ */
+ #ifdef __cplusplus
