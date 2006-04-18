7f8b30ed7271caa6afa2e6ccfe40f59392aee1c1
sem <sem@FreeBSD.org>
2006-04-18 17:35:29 +0000

    MySQL Query Browser is a database querying tool that combines the simplicity of a Web-browser-like interface with powerful features like multiple result sets on tab sheets, query history, storing query "bookmarks", editing and comparing resultsets, SQL script debugging, and more.
    WWW: http://www.mysql.com/products/tools/query-browser/
    
    PR:		ports/95530
    Submitted by:	Rainer Alves <rainer.alves@gmail.com>

---
+++ databases/mysql-query-browser/files/patch-MCrontab.h
@@ -0,0 +1,13 @@
+--- mysql-gui-common/source/linux/MCrontab.h.orig	Sat Apr  8 21:17:27 2006
++++ mysql-gui-common/source/linux/MCrontab.h	Sat Apr  8 21:18:01 2006
+@@ -41,8 +41,8 @@
+   public:
+     bool find_entry_by_comment(const std::string &comment, Entry &entry);
+ 
+-    bool MCrontab::remove_entry_with_comment(const std::string &comment,
+-                                             const std::string &command="");
++    bool remove_entry_with_comment(const std::string &comment,
++                                   const std::string &command="");
+     
+     void add_entry(Entry &entry, const std::string &comment);
+ 
---
+++ databases/mysql-query-browser/files/patch-MQBookmarkBrowser.cc
@@ -0,0 +1,12 @@
+--- mysql-query-browser/source/linux/MQBookmarkBrowser.cc.orig	Sat Apr  8 21:26:35 2006
++++ mysql-query-browser/source/linux/MQBookmarkBrowser.cc	Sat Apr  8 21:27:52 2006
+@@ -257,7 +257,8 @@
+   {
+     for (unsigned int i=0; i < _menu.items().size(); i++)
+       _menu.items()[i].set_sensitive(false);
+-    _menu.items()[3].set_sensitive(true); // New Folder
++    if( _menu.items().size() > 3 )
++      _menu.items()[3].set_sensitive(true); // New Folder
+   }
+ }
+ 
