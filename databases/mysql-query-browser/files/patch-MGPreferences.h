44e1a837db51799ef23068699f366991334a3494
Sergey Matveychuk <sem@FreeBSD.org>
2006-04-18 17:35:29 +0000

    MySQL Query Browser is a database querying tool that combines the simplicity of a Web-browser-like interface with powerful features like multiple result sets on tab sheets, query history, storing query "bookmarks", editing and comparing resultsets, SQL script debugging, and more.
    WWW: http://www.mysql.com/products/tools/query-browser/
    
    PR:		ports/95530
    Submitted by:	Rainer Alves <rainer.alves@gmail.com>

---
+++ databases/mysql-query-browser/files/patch-MGPreferences.h
@@ -0,0 +1,11 @@
+--- mysql-gui-common/source/linux/MGPreferences.h.orig	Sat Apr  8 21:21:45 2006
++++ mysql-gui-common/source/linux/MGPreferences.h	Sat Apr  8 21:22:58 2006
+@@ -40,7 +40,7 @@
+     virtual MYX_APPLICATION_OPTIONS *prepare_options()= 0;
+ 
+     const char *find_value(MYX_OPTION_GROUP *group, const char *name);
+-    std::list<const char *>MGOptions::find_values(MYX_OPTION_GROUP *group, const char *name);
++    std::list<const char *> find_values(MYX_OPTION_GROUP *group, const char *name);
+ 
+     void add_group(MYX_APPLICATION_OPTIONS *options, const char *name,
+                    const std::list<ValuePair> &values);
---
+++ databases/mysql-query-browser/files/patch-regcomp.c
@@ -0,0 +1,21 @@
+--- mysql-query-browser/source/linux/gtksourceview/gtksourceview/gnu-regex/regcomp.c.orig	Sun Apr  9 00:37:59 2006
++++ mysql-query-browser/source/linux/gtksourceview/gtksourceview/gnu-regex/regcomp.c	Sun Apr  9 00:39:42 2006
+@@ -451,18 +451,6 @@
+   if (ret == REG_ERPAREN)
+     ret = REG_EPAREN;
+ 
+-  /* We have already checked preg->fastmap != NULL.  */
+-  if (BE (ret == REG_NOERROR, 1))
+-    /* Compute the fastmap now, since regexec cannot modify the pattern
+-       buffer.  This function nevers fails in this implementation.  */
+-    (void) re_compile_fastmap (preg);
+-  else
+-    {
+-      /* Some error occurred while compiling the expression.  */
+-      re_free (preg->fastmap);
+-      preg->fastmap = NULL;
+-    }
+-
+   return (int) ret;
+ }
+ #ifdef _LIBC
