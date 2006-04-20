fbbe602b0704cb4e8864246e6933f1a3bf46e731
marcus <marcus@FreeBSD.org>
2006-04-20 16:13:25 +0000

    Fix the Python console plug-in.
    Obtained from:	MarcusCom CVS

---
+++ audio/rhythmbox-devel/files/patch-plugins_pythonconsole_pythonconsole.py
@@ -0,0 +1,10 @@
+--- plugins/pythonconsole/pythonconsole.py.orig	Wed Apr 19 17:24:43 2006
++++ plugins/pythonconsole/pythonconsole.py	Wed Apr 19 17:24:53 2006
+@@ -31,6 +31,7 @@
+ import gobject
+ import gtk
+ import pango
++import rhythmdb
+ from gettext import gettext as _
+ import rb
+ 
