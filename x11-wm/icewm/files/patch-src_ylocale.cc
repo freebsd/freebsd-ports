ff84bb313ff577215fec8cb3ab68904fbe18b19c
sem <sem@FreeBSD.org>
2006-04-20 05:42:42 +0000

    - Update to version 1.2.26
    PR:		ports/96012
    Submitted by:	Ports Fury

--- x11-wm/icewm/files/patch-src_ylocale.cc
+++ x11-wm/icewm/files/patch-src_ylocale.cc
@@ -1,7 +1,7 @@
---- src/ylocale.cc.orig	Wed Apr 21 05:06:34 2004
-+++ src/ylocale.cc	Sun Apr 25 19:40:07 2004
-@@ -149,7 +149,8 @@
- 	return NULL;
+--- src/ylocale.cc.orig	Mon Apr 17 01:12:31 2006
++++ src/ylocale.cc	Mon Apr 17 16:19:46 2006
+@@ -149,10 +149,11 @@
+         return NULL;
  
      YUChar * uStr(new YUChar[lLen + 1]);
 -    char * inbuf((char *) lStr), * outbuf((char *) uStr);
@@ -9,4 +9,8 @@
 +    char * outbuf((char *) uStr);
      size_t inlen(lLen), outlen(4 * lLen);
  
-     if (0 > (int) iconv(instance->toUnicode, &inbuf, &inlen, &outbuf, &outlen))
+-    if (0 > (int) iconv(instance->toUnicode, const_cast<char **>(&inbuf), &inlen, &outbuf, &outlen))
++    if (0 > (int) iconv(instance->toUnicode, &inbuf, &inlen, &outbuf, &outlen))
+         warn(_("Invalid multibyte string \"%s\": %s"), lStr, strerror(errno));
+ 
+     *((YUChar *) outbuf) = 0;
