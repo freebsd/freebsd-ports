f2be70c6f4cf33de96bfc8a694e93b2d1714f79c
Vasil Dimov <vd@FreeBSD.org>
2006-04-21 14:31:05 +0000

    Update from 0.69 to 0.70
    PR:		ports/95298
    Submitted by:	Antonio Querubin <tony@lava.netg>

--- net/mtr/files/patch-dns.c
+++ net/mtr/files/patch-dns.c
@@ -1,31 +1,10 @@
-Index: dns.c
-diff -u -p dns.c.orig dns.c
---- dns.c.orig	Tue Jan 11 17:32:42 2005
-+++ dns.c	Thu Feb  3 19:25:28 2005
-@@ -437,7 +437,7 @@ char *strlongip(ip_t * ip)
- }
- 
- 
--int longipstr(char *s, ip_t *dst)
-+int longipstr(char *s, ip_t *dst, int af)
- {
+--- dns.c      2006-03-22 19:45:58.000000000 -1000
++++ dns.c      2006-03-25 11:41:31.000000000 -1000
+@@ -1357,7 +1357,7 @@
  #ifdef ENABLE_IPV6
-   return inet_pton( af, s, dst );
-@@ -488,7 +488,7 @@ void dns_open(void)
- 	    strerror(errno));
-     exit(-1);
-   }
--  longipstr( "127.0.0.1", &localhost );
-+  longipstr( "127.0.0.1", &localhost, AF_INET );
-   aseed = time(NULL) ^ (time(NULL) << 3) ^ (dword)getpid();
-   for (i = 0;i < BashSize;i++) {
-     idbash[i] = NULL;
-@@ -1228,7 +1228,7 @@ void dns_ack(void)
- 	if ( addrcmp( (void *) &(_res.nsaddr_list[i].sin_addr),
- 		      (void *) &(from4->sin_addr), (int) AF_INET ) == 0 ||
- 	     addrcmp( (void *) &(_res.nsaddr_list[i].sin_addr),
--		      (void *) &unspec_addr, (int) AF_INET ) != 0 )	/* 0.0.0.0 replies as 127.0.0.1 */
-+		      (void *) &unspec_addr, (int) AF_INET ) == 0 )	/* 0.0.0.0 replies as 127.0.0.1 */
- 	  break;
-     } else
-       for (i = 0;i < _res.nscount;i++)
+ /* Returns an ip6.arpa character string. */
+ void addr2ip6arpa( ip_t * ip, char * buf ) {
+-  char * p = (char *) ip;
++  unsigned char * p = (unsigned char *) ip;
+   char * b = buf;
+   int i;
