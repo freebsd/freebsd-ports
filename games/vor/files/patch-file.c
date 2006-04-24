257276719d5c5b4df35e79b3878ccb96436f1474
vs <vs@FreeBSD.org>
2006-04-24 13:10:41 +0000

    Update to 0.4
    PR:		ports/94983
    Submitted by:	maintainer

--- games/vor/files/patch-file.c
+++ games/vor/files/patch-file.c
@@ -1,26 +1,13 @@
---- file.c.orig	Wed Sep  7 05:00:47 2005
-+++ file.c	Wed Sep  7 05:01:01 2005
-@@ -58,20 +58,19 @@
- find_data_dir(void)
+--- file.c.orig	Sun Mar 26 06:52:35 2006
++++ file.c	Mon Mar 27 02:30:51 2006
+@@ -101,9 +101,9 @@
  {
  	int i;
--	char *data_options[3] = {
--		"./data",
-+	char *data_options[2] = {
+ 	char *data_options[3] = {
++		DATA_PREFIX,
  		getenv("VOR_DATA"),
- 		DATA_PREFIX
+ 		"data",
+-		DATA_PREFIX
  	};
  
--	for(i=0; i<3; i++) {
-+	for(i=0; i<2; i++) {
- 		if(!data_options[i]) continue;
- 		g_data_dir = strdup(data_options[i]);
- 		if(is_dir(g_data_dir)) return true;
- 	}
- 
- 	fprintf(stderr, "Can't find VoR data! Tried:\n");
--	for(i=0; i<3; i++) {
-+	for(i=0; i<2; i++) {
- 		fprintf(stderr, "\t%s\n", data_options[i]);
- 	}
- 	return false;
+ 	for(i=0; i<3; i++) {
