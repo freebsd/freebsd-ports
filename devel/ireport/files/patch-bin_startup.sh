4d6ac7dbb640f5d41605f8d846a47f6e9c32fb4a
Thierry Thomas <thierry@FreeBSD.org>
2006-04-28 22:26:16 +0000

    Upgrade to iReport 1.2.2.
    Changelog at
    <http://ireport.sourceforge.net/index.php?p_module=page&pageid=33>

--- devel/ireport/files/patch-bin_startup.sh
+++ devel/ireport/files/patch-bin_startup.sh
@@ -1,9 +1,11 @@
---- bin/startup.sh.orig	Fri Apr  7 03:38:58 2006
-+++ bin/startup.sh	Sat Apr  8 22:40:32 2006
-@@ -1,30 +1,6 @@
+--- bin/startup.sh.orig	Wed Apr 26 07:35:37 2006
++++ bin/startup.sh	Sat Apr 29 00:04:02 2006
+@@ -1,30 +1,12 @@
  #! /bin/sh
  
--
++IREPORT_CLASSPATH="%%CLSSPTH%%:%%DATADIR%%/fonts"
++IREPORT_HOME="$HOME/ireport"
+ 
 -# add the libraries to the IREPORT_CLASSPATH.
 -# EXEDIR is the directory where this executable is.
 -EXEDIR=${0%/*}
@@ -27,9 +29,13 @@
 -  fi
 -done
 -
--IREPORT_CLASSPATH="${EXEDIR}/../classes":${EXEDIR}/../fonts:$IREPORT_CLASSPATH
+-IREPORT_CLASSPATH="${EXEDIR}/../classes":"${EXEDIR}/../fonts":$IREPORT_CLASSPATH
 -IREPORT_HOME="${EXEDIR}/.."
-+IREPORT_CLASSPATH="%%CLSSPTH%%:%%DATADIR%%/fonts"
-+IREPORT_HOME="%%DATADIR%%"
- 
+-
++# Since iReport-1.2.2, $IREPORT_HOME must be writable
++/bin/mkdir -p $IREPORT_HOME/lib
++/bin/rm $IREPORT_HOME/plugins $IREPORT_HOME/templates 2>/dev/null || /usr/bin/true
++/bin/ln -sf %%DATADIR%%/plugins $IREPORT_HOME/plugins
++/bin/ln -sf %%DATADIR%%/templates $IREPORT_HOME/templates
++cd $IREPORT_HOME
  java -classpath "$IREPORT_CLASSPATH:$CLASSPATH" -Direport.home=$IREPORT_HOME it.businesslogic.ireport.gui.MainFrame "$@"
