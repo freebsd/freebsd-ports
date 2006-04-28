18b1f5c205ae69981bce479187884cd09b9a323e
ijliao <ijliao@FreeBSD.org>
2006-04-28 02:36:32 +0000

    upgrade to 2.1.1
    PR:		96434
    Submitted by:	maintainer

--- databases/mysql++/files/patch-makemake.sh
+++
@@ -1,8 +0,0 @@
---- makemake.sh.orig	Wed Nov  2 21:03:09 2005
-+++ makemake.sh	Sat Dec 10 15:07:58 2005
-@@ -1,4 +1,4 @@
--#!/bin/bash
-+#!/usr/local/bin/bash
- 
- # Display usage message
- function usage() {
