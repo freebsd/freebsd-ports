ffb98eed056a4e0786d4472e510b678cd224a2c6
erwin <erwin@FreeBSD.org>
2006-04-28 13:36:02 +0000

    - Update to 0.0.13 - Mark BROKEN on 4.x
    PR:		96179
    Submitted by:	Ying-Chieh Chen <yinjieh@csie.nctu.edu.tw> (maintainer)

--- games/libggz/files/patch-src-easysock.c
+++ games/libggz/files/patch-src-easysock.c
@@ -1,12 +1,12 @@
---- src/easysock.c.orig	Mon Sep 19 01:28:42 2005
-+++ src/easysock.c	Fri Oct  7 12:45:11 2005
-@@ -49,8 +49,8 @@
+--- src/easysock.c.orig	Fri Mar 17 01:35:39 2006
++++ src/easysock.c	Sun Apr  9 20:50:42 2006
+@@ -58,8 +58,8 @@
+ #include <sys/socket.h>
  #include <sys/uio.h>
  #include <sys/un.h>
- #include <sys/param.h>
 -#include <arpa/inet.h>
  #include <netinet/in.h>
 +#include <arpa/inet.h>
  #include <netdb.h>
+ #endif
  
- #ifdef HAVE_WINSOCK2_H
