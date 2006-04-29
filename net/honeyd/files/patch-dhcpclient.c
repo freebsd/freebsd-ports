b6de0020508225b134a0d5f0875499f13f4fcb37
Pav Lucistnik <pav@FreeBSD.org>
2006-04-29 12:33:12 +0000

    - Update to 1.5a - Take over maintaining
    PR:		ports/96318
    Submitted by:	Wesley Shields <wxs@csh.rit.edu>
    Approved by:	Yann Berthier <yb@bashibuzuk.net> (maintainer)

--- net/honeyd/files/patch-dhcpclient.c
+++ net/honeyd/files/patch-dhcpclient.c
@@ -1,6 +1,6 @@
---- dhcpclient.c.orig	Thu Apr 14 16:07:26 2005
-+++ dhcpclient.c	Thu Apr 14 15:24:19 2005
-@@ -236,8 +236,8 @@
+--- dhcpclient.c.orig	Thu Apr 20 08:23:52 2006
++++ dhcpclient.c	Thu Apr 20 08:24:15 2006
+@@ -211,8 +211,8 @@
  {
  	struct dhcpclient_req *req = tmpl->dhcp_req;
  	struct dhcp_msg *msg = (struct dhcp_msg *)buf;
@@ -11,23 +11,3 @@
  	short replyreq = 0, ack = 0, done = 0;
  	struct netconf nc;
  	struct addr *which = NULL, ipmask;
-@@ -472,7 +472,8 @@
- 
- 	memset(buf, 0, sizeof(buf));
- 
--	eth = (struct eth_hdr *)p = buf;
-+	p = buf;
-+	eth = (struct eth_hdr *)p;
- 	eth_pack_hdr(eth, ETH_ADDR_BROADCAST, req->ea, ETH_TYPE_IP);
- 
- 	restlen -= ETH_HDR_LEN;
-@@ -524,7 +525,8 @@
- 
- 	memset(buf, 0, sizeof(buf));
- 
--	eth = (struct eth_hdr *)p = buf;
-+	p = buf;
-+	eth = (struct eth_hdr *)p;
- 	eth_pack_hdr(eth, req->server_ea, req->ea, ETH_TYPE_IP);
- 
- 	restlen -= ETH_HDR_LEN;
