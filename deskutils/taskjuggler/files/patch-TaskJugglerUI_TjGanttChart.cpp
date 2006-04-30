d3a51e7d3646d07d7f30c9a3bb33e38d12b410c2
lofi <lofi@FreeBSD.org>
2006-04-30 00:47:57 +0000

    Unbreak, update to 2.2.0 and reset MAINTAINER.

---
+++ deskutils/taskjuggler/files/patch-TaskJugglerUI_TjGanttChart.cpp
@@ -0,0 +1,11 @@
+--- TjGanttChart.cpp.orig	Sun Apr 30 01:56:22 2006
++++ TjGanttChart.cpp	Sun Apr 30 02:01:40 2006
+@@ -994,7 +994,7 @@
+         {
+             const Task* t = static_cast<const Task*>
+                 ((*it)->getCoreAttributes());
+-            drawTask(t, false);
++            drawTask(t, (const Resource*)false);
+ 
+             drawDependencies(t, collisionDetector);
+         }
