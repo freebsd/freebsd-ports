a74c53744c2977e2f92a45794fc2ae6722cac04b
Joe Marcus Clarke <marcus@FreeBSD.org>
2006-04-30 01:23:20 +0000

    Remove evolution-caldav now that it is part of Evolution.

--- www/evolution-caldav/files/patch-eplugin.mk
+++
@@ -1,5 +0,0 @@
---- eplugin.mk.orig	Wed Aug 31 17:21:09 2005
-+++ eplugin.mk	Wed Aug 31 17:22:10 2005
-@@ -0,0 +1,2 @@
-+%.eplug: %.eplug.in
-+	sed -e 's|\@PLUGINDIR\@|$(eplugindir)|;s|\@LOCALEDIR\@|$(localedir)|;s|\@GETTEXT_PACKAGE\@|$(GETTEXT_PACKAGE)|' $< > $@
