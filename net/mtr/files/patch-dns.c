20afb377f3216d1d2e0a59de69eba7b701898e96
Erwin Lansing <erwin@FreeBSD.org>
2006-05-04 18:51:42 +0000

    - Update to 0.71 - Hand maintainership to submitter
    PR:		96736
    Submitted by:	Rainer Alves <rainer.alves@gmail.com>

--- net/mtr/files/patch-dns.c
+++ net/mtr/files/patch-dns.c
@@ -1,6 +1,20 @@
---- dns.c      2006-03-22 19:45:58.000000000 -1000
-+++ dns.c      2006-03-25 11:41:31.000000000 -1000
-@@ -1357,7 +1357,7 @@
+--- dns.c.orig	Thu Mar 23 02:45:58 2006
++++ dns.c	Wed May  3 18:48:01 2006
+@@ -48,11 +48,13 @@
+ #include "dns.h"
+ #include "net.h"
+ 
++/*
+ #ifdef NO_STRERROR
+ extern int sys_nerr;
+ extern char *sys_errlist[];
+ #define strerror(errno) (((errno) >= 0 && (errno) < sys_nerr) ? sys_errlist[errno] : "unlisted error")
+ #endif
++*/
+ 
+ /*  Hmm, it seems Irix requires this  */
+ extern int errno;
+@@ -1357,7 +1359,7 @@
  #ifdef ENABLE_IPV6
  /* Returns an ip6.arpa character string. */
  void addr2ip6arpa( ip_t * ip, char * buf ) {
@@ -8,3 +22,4 @@
 +  unsigned char * p = (unsigned char *) ip;
    char * b = buf;
    int i;
+ 
