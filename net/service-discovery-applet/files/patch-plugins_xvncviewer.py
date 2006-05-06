e55c73caffc1ff0d764eca30c8faa2969492c320
marcus <marcus@FreeBSD.org>
2006-05-06 23:09:26 +0000

    Add service-discovery-applet, a GNOME applet for viewing available network services advertised via mDNS (aka Bonjour, Zeroconf, Rendezvous).

---
+++ net/service-discovery-applet/files/patch-plugins_xvncviewer.py
@@ -0,0 +1,17 @@
+--- plugins/xvncviewer.py.orig	Sat May  6 15:19:12 2006
++++ plugins/xvncviewer.py	Sat May  6 15:20:13 2006
+@@ -19,11 +19,11 @@ class plugin_xvncviewer:
+     def __init__(self):
+         self.service_type = ["_rfb._tcp"]
+         self.author = "Sebastien Estienne"
+-        self.description = "Start a VNC connections with xvncviewer"
++        self.description = "Start a VNC connections with vncviewer"
+ 
+     def connect(self, use_host_names, name, stype, hostname, address, port, txts):
+-        print "connecting using xvncviewer"
+-	cmdline = ["xvncviewer", "-xrm", "*passwordDialog:true", hostname ]
++        print "connecting using vncviewer"
++	cmdline = ["vncviewer", "-xrm", "*passwordDialog:true",]
+ 	print cmdline
+ 	pid = subprocess.Popen(cmdline, close_fds=True).pid
+ 
