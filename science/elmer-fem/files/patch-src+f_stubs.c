226c0473043935620cb24a281964dff5dd35b688
thierry <thierry@FreeBSD.org>
2006-05-06 06:20:28 +0000

    Fix on FreeBSD-4.
    Submitted by:	Pedro F. Giffuni <giffunip (at) asme.org> (maintainer)

---
+++ science/elmer-fem/files/patch-src+f_stubs.c
@@ -0,0 +1,12 @@
+--- src/f_stubs.c.orig	Mon Aug 29 05:07:45 2005
++++ src/f_stubs.c	Thu May  4 22:56:30 2006
+@@ -13,8 +13,8 @@
+ 
+ #else 
+ 
+-#include <sys/resource.h>
+ #include <sys/time.h>
++#include <sys/resource.h>
+ #include <sys/times.h>
+ #include <sys/param.h>
+ 
