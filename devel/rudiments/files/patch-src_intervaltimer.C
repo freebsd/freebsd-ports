ab160982a980dbecb8419d5c765acc4a4ede4c0b
mnag <mnag@FreeBSD.org>
2006-05-06 02:42:34 +0000

    - Update to 0.29 - Take maintainership - Fix package creation on 4.x
    PR:		96735
    Submitted by:	Rainer Alves <rainer.alves___gmail.com>

--- devel/rudiments/files/patch-src_intervaltimer.C
+++ devel/rudiments/files/patch-src_intervaltimer.C
@@ -1,11 +1,11 @@
---- src/intervaltimer.C.orig	Tue Jan 31 20:22:04 2006
-+++ src/intervaltimer.C	Tue Jan 31 20:22:26 2006
-@@ -111,7 +111,7 @@
- 	// Some systems define tv_usec as something other than long so we need
- 	// to get a long and then let it auto-cast during assignment below.
+--- src/intervaltimer.C.orig	Sat Sep 24 00:31:57 2005
++++ src/intervaltimer.C	Wed May  3 18:08:55 2006
+@@ -134,7 +134,7 @@
+ 	// assignment below.
+ 	long	sec;
  	long	usec;
--	bool	retval=getTimeRemaining(&(tv->tv_sec),&usec);
-+	bool	retval=getTimeRemaining((long*)(&tv->tv_sec),&usec);
+-	bool	retval=getTimeRemaining(&sec,&usec);
++	bool	retval=getTimeRemaining((long*)(&sec),&usec);
+ 	tv->tv_sec=sec;
  	tv->tv_usec=usec;
  	return retval;
- }
