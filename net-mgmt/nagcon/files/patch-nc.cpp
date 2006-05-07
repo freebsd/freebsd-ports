b8392b2dae8c0ffed7a79acd0699ff9a7a98b0f0
Pav Lucistnik <pav@FreeBSD.org>
2006-05-07 11:58:57 +0000

    - Update to 0.0.14
    PR:		ports/96895
    Submitted by:	Douglas K. Rand <rand@meridian-enviro.com> (maintainer)

--- net-mgmt/nagcon/files/patch-nc.cpp
+++ net-mgmt/nagcon/files/patch-nc.cpp
@@ -1,20 +1,29 @@
---- ./nc.cpp.orig	Tue Mar 28 20:58:16 2006
-+++ ./nc.cpp	Wed Mar 29 20:39:15 2006
-@@ -182,7 +182,7 @@
- 	version();
+--- nc.cpp.orig	Tue May  2 09:29:37 2006
++++ nc.cpp	Sat May  6 21:42:59 2006
+@@ -258,7 +258,7 @@
  
  	printf("-f file   what file to monitor (usuallly:\n");
+ 	printf("-F host:port   connect to a host for retrieving the status.log information\n");
 -	printf("          /usr/local/nagios/var/status.log, look for status_file in\n");
-+	printf("          /var/spool/nagios/status.dat, look for status_file in\n");
++	printf("          /var/spool/nagios/status.log, look for status_file in\n");
  	printf("          the nagios.cfg file\n");
  	printf("-i x      check interval (in seconds)\n");
  	printf("-a        list also the services for hosts that are down\n");
-@@ -208,7 +208,7 @@
+@@ -288,7 +288,7 @@
  	WINDOW *win;
  	int sw;
  
--	char *statuslog = "/usr/local/nagios/var/status.log";
-+	char *statuslog = "/var/spool/nagios/status.dat";
+-	char *status_log = "/usr/local/nagios/var/status.log";
++	char *status_log = "/var/spool/nagios/status.log";
  	int interval = 5;
  	char list_all_problems = 0;
  	char always_notify = 0;
+@@ -390,7 +390,7 @@
+ 		wattroff(win, COLOR_PAIR(MY_DRAW));
+ 
+ 		if (file_mode == 0)	/* file */
+-			fd = open64(status_log, O_RDONLY);
++			fd = open(status_log, O_RDONLY);
+ 		else
+ 			fd = connect_to(status_log);
+ 
