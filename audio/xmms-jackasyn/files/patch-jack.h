b95b1de74eca2e3b1e823ff3071b17abdcd82112
pav <pav@FreeBSD.org>
2006-05-08 16:07:31 +0000

    This plugin enables xmms to playback sound via the JACK audio server. The plugin is actually a port of the xmms OSS sound ouput plugin to JACK, using libjackasyn.
    WWW:	http://gige.xdv.org/libjackasyn/xmms.php
    
    PR:		ports/94292
    Submitted by:	Jose Alonso Cardenas Marquez <acardenas@bsd.org.pe>

---
+++ audio/xmms-jackasyn/files/patch-jack.h
@@ -0,0 +1,11 @@
+--- jack.h	Thu Apr  6 12:12:30 2006
++++ jack.h	Thu Apr  6 12:13:02 2006
+@@ -18,6 +18,8 @@
+ #ifndef JACK_H
+ #define JACK_H
+ 
++#define HAVE_NEWPCM
++
+ #include "config.h"
+ 
+ #include <gtk/gtk.h>
