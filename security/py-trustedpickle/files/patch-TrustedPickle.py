bdcd0201a64f7e8240473919ac13e2ff043061ce
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2006-05-09 11:46:02 +0000

    TrustedPickle is a Python module that can save most any arbitrary Python object in a signed pickle file. There are two big differences between this module and the standard pickle module. First, TrustedPickle can pickle a module, but the standard pickle module cannot. Second, TrustedPickle includes a signature that can verify the data's origin before the data is unpickled.
    WWW: http://trustedpickle.sourceforge.net/index.html
    
    PR:		ports/96691
    Submitted by:	Alexander Botero-Lowry <alex@foxybanana.com>
    Approved by:	lawrance (mentor)

---
+++ security/py-trustedpickle/files/patch-TrustedPickle.py
@@ -0,0 +1,10 @@
+--- TrustedPickle.py.orig	Tue May  2 14:20:14 2006
++++ TrustedPickle.py	Tue May  2 14:20:24 2006
+@@ -82,6 +82,7 @@
+ Low-level functions in this module you should not need to call:
+ 
+ Hash(): used in signing a string
++"""
+ 
+ import cPickle
+ import getpass
