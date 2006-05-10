026fd8408313a929c055f36a81f0c79d186aa948
Andrew Pantyukhin <sat@FreeBSD.org>
2006-05-10 19:08:09 +0000

    - Update to 0.98.1 - Fix epiphany extension
    PR:		ports/96665
    Submitted by:	sat
    Approved by:	osa (co-mentor)

---
+++ ftp/gwget/files/patch-src__gwget_data.c
@@ -0,0 +1,10 @@
+--- ./src/gwget_data.c.orig	Tue Dec  6 19:54:41 2005
++++ ./src/gwget_data.c	Tue May  2 17:55:45 2006
+@@ -35,6 +35,7 @@
+ #include "main_window_cb.h"
+ #include "utils.h"
+ #include "systray.h"
++#include <netinet/in.h>
+ 
+ 
+ 
