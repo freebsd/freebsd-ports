77d336f91edb09537809ad20582986d351bcf7c9
mnag <mnag@FreeBSD.org>
2006-05-10 19:16:19 +0000

    - Update to 6.1 - portlint(1)
    PR:		97100
    Submitted by:	maintainer

--- sysutils/dvd+rw-tools/files/patch-mp.h
+++ sysutils/dvd+rw-tools/files/patch-mp.h
@@ -1,12 +1,12 @@
---- mp.h.orig	Mon Jan 16 11:34:56 2006
-+++ mp.h	Thu Jan 19 20:22:30 2006
-@@ -39,7 +39,9 @@
-   pthread_attr_t attr;
+--- mp.h.orig	Wed May 10 16:01:28 2006
++++ mp.h	Wed May 10 16:06:04 2006
+@@ -40,7 +40,9 @@
  
      if (pthread_attr_init(&attr)==0 &&
-+#if	!defined(__FreeBSD__) || __FreeBSD__ >= 5
+ 	pthread_attr_setstacksize(&attr,64*1024)==0 &&
++#if !defined(__FreeBSD__) || __FreeBSD_version >= 500000
  	pthread_attr_setscope(&attr,PTHREAD_SCOPE_SYSTEM)==0 &&
-+#endif	/* FreeBSD 4 does not support PTHREAD_SCOPE_SYSTEM */
++#endif /* FreeBSD 4 does not support PTHREAD_SCOPE_SYSTEM */
  	pthread_create(&h,&attr,(void *(*)(void *))func,arg)==0 )
  	return (void *)h;
  
