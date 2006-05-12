f24dffda8261b4e16ed7940e866c5a594c0f766b
Yen-Ming Lee <leeym@FreeBSD.org>
2006-05-12 14:50:28 +0000

    - add patch for missing require
    PR:		97171
    Submitted by:	Jui-Nan Lin <jnlin at csie.nctu.edu.tw>

---
+++ mail/openwebmail/files/patch-openwebmail.pl
@@ -0,0 +1,10 @@
+--- openwebmail.pl.orig	Fri May 12 22:08:14 2006
++++ openwebmail.pl	Fri May 12 22:08:38 2006
+@@ -41,6 +41,7 @@
+ require "modules/datetime.pl";
+ require "modules/lang.pl";
+ require "modules/mime.pl";
++require "modules/htmltext.pl";
+ require "auth/auth.pl";
+ require "quota/quota.pl";
+ require "shares/ow-shared.pl";
