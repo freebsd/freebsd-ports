aeab8ffd5514a3c83645903c897debcb74ce9cd3
lawrance <lawrance@FreeBSD.org>
2006-05-15 06:53:11 +0000

    Patch pptpctrl.c so that it does not pass bogus parameters to ppp. Bump PORTREVISION for the change above.
    PR:		ports/96386
    Submitted by:	Stefan Bethke <stb@lassitu.de>
    Approved by:	olmi@rentech.ru (maintainer, timeout 2 weeks)

--- net/poptop/files/patch-pptpctrl.c
+++ net/poptop/files/patch-pptpctrl.c
@@ -1,7 +1,15 @@
---- pptpctrl.c.orig	Fri Oct 21 16:23:58 2005
-+++ pptpctrl.c	Fri Oct 21 16:24:28 2005
-@@ -770,7 +770,7 @@
- 
+--- pptpctrl.c.orig	Mon Jan 24 23:04:13 2005
++++ pptpctrl.c	Sat May  6 12:48:55 2006
+@@ -742,7 +742,6 @@
+ 		sprintf(pppInterfaceIPs, "%s:%s", pppaddrs[0], pppaddrs[1]);
+ 		pppd_argv[an++] = pppInterfaceIPs;
+ 	}
+-#endif
+
+         if (!noipparam) {
+                  pppd_argv[an++] = "ipparam";
+@@ -751,10 +750,11 @@
+
          if (pptp_logwtmp) {
                   pppd_argv[an++] = "plugin";
 -                 pppd_argv[an++] = "/usr/lib/pptpd/pptpd-logwtmp.so";
@@ -9,3 +17,7 @@
                   pppd_argv[an++] = "pptpd-original-ip";
                   pppd_argv[an++] = inet_ntoa(inetaddrs[1]);
          }
++#endif
+
+ 	/* argv arrays must always be NULL terminated */
+ 	pppd_argv[an++] = NULL;
