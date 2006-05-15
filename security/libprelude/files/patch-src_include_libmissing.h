5ac3691c5b188be326f5e6fc24a445b082e94ff4
Renato Botelho <garga@FreeBSD.org>
2006-05-15 12:40:38 +0000

    Fix build on 4.x
    Submitted by:	maintainer and author by email

---
+++ security/libprelude/files/patch-src_include_libmissing.h
@@ -0,0 +1,12 @@
+--- src/include/libmissing.h.orig	Mon May 15 09:10:13 2006
++++ src/include/libmissing.h	Mon May 15 09:09:54 2006
+@@ -35,8 +35,8 @@
+ #include <stdbool.h>
+ #include <stdlib.h>
+ #include <string.h>
+-#include <sys/socket.h>
+ #include <sys/types.h>
++#include <sys/socket.h>
+ #include <time.h>
+ #include "ftw_.h"
+ #include "getaddrinfo.h"
