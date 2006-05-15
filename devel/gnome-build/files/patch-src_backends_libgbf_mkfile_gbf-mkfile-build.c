0ae7373e551b3d70fe459f338a3fb336af9d82c6
marcus <marcus@FreeBSD.org>
2006-05-15 22:15:45 +0000

    Update to 0.1.3.

---
+++ devel/gnome-build/files/patch-src_backends_libgbf_mkfile_gbf-mkfile-build.c
@@ -0,0 +1,11 @@
+--- src/backends/libgbf_mkfile/gbf-mkfile-build.c.orig	Mon May 15 18:06:23 2006
++++ src/backends/libgbf_mkfile/gbf-mkfile-build.c	Mon May 15 18:07:32 2006
+@@ -32,7 +32,7 @@
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <string.h>
+-#include <regex.h>
+ #include "gbf-i18n.h"
+ #include "gbf-mkfile-build.h"
++#include <gnuregex.h>
+ 
