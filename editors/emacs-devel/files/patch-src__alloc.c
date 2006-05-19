8079cc10736bec10a71c3a32f1aad393ef363297
anray <anray@FreeBSD.org>
2006-05-19 18:41:29 +0000

    - Update to 22.0.50.20060502 - Change CONFIGURE_TARGET to ${MACHINE_ARCH}-freebsd. - Assign maintainership to Giorgos Keramidas <keramida@ceid.upatras.gr> [1]
    Submitted by:	keramida (by email)
    Created by:	nobutaka, keramida
    Approved by:	maintainer (by email) [1]

--- editors/emacs-devel/files/patch-src__alloc.c
+++ editors/emacs-devel/files/patch-src__alloc.c
@@ -1,6 +1,8 @@
---- src/alloc.c.orig	Sat Apr  1 23:41:43 2006
-+++ src/alloc.c	Sat Apr  1 23:42:32 2006
-@@ -4528,7 +4528,11 @@
+Index: src/alloc.c
+===================================================================
+--- src/alloc.c	(.../branches/gnu/emacs)	(revision 15)
++++ src/alloc.c	(.../trunk/emacs)	(revision 15)
+@@ -4542,8 +4542,12 @@
       needed on ia64 too.  See mach_dep.c, where it also says inline
       assembler doesn't work with relevant proprietary compilers.  */
  #ifdef sparc
@@ -8,7 +10,8 @@
 +  asm ("flushw");
 +#else
    asm ("ta 3");
-+#endif
  #endif
++#endif
  
    /* Save registers that we need to see on the stack.  We need to see
+      registers used to hold register variables and registers used to
