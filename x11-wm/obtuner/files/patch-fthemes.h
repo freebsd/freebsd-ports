36e022249527bb60e5388b0e99611c659026bc09
novel <novel@FreeBSD.org>
2006-05-22 12:55:32 +0000

    Fix build on 4.x.
    Reported by:	pointyhat via kris

--- x11-wm/obtuner/files/patch-fthemes.h
+++ x11-wm/obtuner/files/patch-fthemes.h
@@ -1,10 +1,14 @@
---- fthemes.h.orig	Tue May  9 09:31:44 2006
-+++ fthemes.h	Tue May  9 09:31:58 2006
-@@ -6,6 +6,7 @@
- #include "value.h"
+--- fthemes.h.orig	Wed Sep 14 08:53:37 2005
++++ fthemes.h	Mon May 22 12:21:54 2006
+@@ -7,6 +7,11 @@
  #include "rc.h"
  #include <gtk/gtk.h>
-+#include <sys/select.h>
  
++#include <sys/types.h>
++#include <sys/time.h>
++#include <unistd.h>
++#include <sys/select.h>
++
  namespace Themes {
      GtkWidget *face(Rc &rc);
+ };
