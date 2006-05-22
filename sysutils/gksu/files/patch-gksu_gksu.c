c62da368e2975f428b7e31d15c1b4ff4d61898c9
Pav Lucistnik <pav@FreeBSD.org>
2006-05-22 21:33:26 +0000

    - Update to 1.3.7
    PR:		ports/97608
    Submitted by:	Boris B. Samorodov <bsam@ipt.ru> (maintainer)

--- sysutils/gksu/files/patch-gksu_gksu.c
+++ sysutils/gksu/files/patch-gksu_gksu.c
@@ -1,6 +1,6 @@
---- gksu/gksu.c.orig	Wed Oct 26 14:50:43 2005
-+++ gksu/gksu.c	Tue Nov 15 00:28:41 2005
-@@ -499,6 +499,7 @@
+--- gksu/gksu.c.orig	Wed Mar 29 06:18:28 2006
++++ gksu/gksu.c	Mon May 22 16:29:36 2006
+@@ -513,6 +513,7 @@
  
  pid_t test_lock(const char* fname)
  {
@@ -8,7 +8,7 @@
     int FD = open(fname, 0);
     if(FD < 0) {
        if(errno == ENOENT) {
-@@ -509,7 +510,6 @@
+@@ -523,7 +524,6 @@
  	 return(-1);
        }
     }
@@ -16,7 +16,7 @@
     fl.l_type = F_WRLCK;
     fl.l_whence = SEEK_SET;
     fl.l_start = 0;
-@@ -529,6 +529,7 @@
+@@ -543,6 +543,7 @@
  
  int get_lock(const char *File)
  {
@@ -24,9 +24,9 @@
     int FD = open(File,O_RDWR | O_CREAT | O_TRUNC,0640);
     if (FD < 0)
     {
-@@ -546,13 +547,13 @@
+@@ -560,13 +561,13 @@
     fcntl(FD,F_SETFD, FD_CLOEXEC);
-       
+ 
     // Aquire a write lock
 -   struct flock fl;
     fl.l_type = F_WRLCK;
@@ -39,16 +39,16 @@
        if (errno == ENOLCK)
        {
  	 g_warning(_("Not using locking for nfs mounted lock file %s"), File);
-@@ -561,7 +562,7 @@
- 	 return dup(0);       // Need something for the caller to close	 
-       }      
-       
+@@ -575,7 +576,7 @@
+ 	 return dup(0);       // Need something for the caller to close
+       }
+ 
 -      int Tmp = errno;
 +      Tmp = errno;
        close(FD);
        errno = Tmp;
        return -1;
-@@ -759,6 +760,7 @@
+@@ -773,6 +774,7 @@
    GtkWidget *dialog = GTK_WIDGET(data);
  
    int retvalue = 0;
@@ -56,7 +56,7 @@
  
    if (prompt)
      gksuui_dialog_set_prompt (GKSUUI_DIALOG(dialog), prompt);
-@@ -782,7 +784,6 @@
+@@ -796,7 +798,6 @@
        g_free (msg);
      }
  
@@ -64,15 +64,15 @@
    if (grab)
      lock = grab_keyboard_and_mouse (dialog);
    retvalue = gtk_dialog_run (GTK_DIALOG(dialog));
-@@ -1206,6 +1207,7 @@
+@@ -1220,6 +1221,7 @@
  
    if (print_pass)
      {
 +      int lock = 0;
        if (!gksuui_dialog_get_message (GKSUUI_DIALOG(dialog)))
  	{
- 	  gchar *msg = 
-@@ -1216,7 +1218,6 @@
+ 	  gchar *msg =
+@@ -1230,7 +1232,6 @@
  	  g_free (msg);
  	}
  
