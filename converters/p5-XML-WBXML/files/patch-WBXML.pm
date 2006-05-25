54a95807827bd3ce6d9760147447f6399aeb4efe
aaron <aaron@FreeBSD.org>
2006-05-25 21:51:54 +0000

    - Tweaked source to allow use under Perl 5.6
    Approved by:	tobez (implicit)

---
+++ converters/p5-XML-WBXML/files/patch-WBXML.pm
@@ -0,0 +1,10 @@
+--- lib/XML/WBXML.pm	Thu Aug 11 20:55:31 2005
++++ lib/XML/WBXML.pm	Thu May 25 15:47:21 2006
+@@ -1,6 +1,6 @@
+ package XML::WBXML;
+ 
+-use 5.008003;
++use 5.00600;
+ use strict;
+ use warnings;
+ 
