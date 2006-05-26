1dc45df688eed83be0233b94fd65bd8a406d2eb8
Thierry Thomas <thierry@FreeBSD.org>
2006-05-26 12:37:08 +0000

    Integrate patch written by Andreas Wiese <bzimage (at) gmx.net> from BSD Crew Dresden, which ports the previously linux-only CPU plugin to FreeBSD.
    PR:		ports/97575
    Submitted by:	Tobias Roth <ports (at) fsck.ch> (maintainer)

---
+++ x11/fbpanel/files/patch-plugin.h
@@ -0,0 +1,11 @@
+--- plugin.h~	Tue May 16 19:22:49 2006
++++ plugin.h	Tue May 16 19:00:09 2006
+@@ -61,7 +61,7 @@
+ #define STATIC_SPACE
+ #define STATIC_ICONS
+ #define STATIC_DESKNO
++#define STATIC_CPU
+ #endif
+-
+ 
+ #endif
