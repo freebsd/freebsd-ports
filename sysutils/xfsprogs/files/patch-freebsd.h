3a2b3b32e5d84dd88d5c3bfeffdd6760081ca077
rodrigc <rodrigc@FreeBSD.org>
2006-05-27 12:50:44 +0000

    - Bump PORTREVISION. - Fix endian macros on FreeBSD, so that mkfs.xfs does not produce   a scrambled superblock.
    Submitted by:	Nathan Scott <nathans at sgi dot com >

--- sysutils/xfsprogs/files/patch-freebsd.h
+++ sysutils/xfsprogs/files/patch-freebsd.h
@@ -1,9 +1,25 @@
---- include/freebsd.h.orig	Tue Feb  7 21:14:51 2006
-+++ include/freebsd.h	Tue Feb  7 21:15:07 2006
-@@ -39,15 +39,6 @@
+--- include/freebsd.h.orig	Mon Jan 16 22:46:48 2006
++++ include/freebsd.h	Sat May 27 03:35:21 2006
+@@ -27,7 +27,13 @@
+ #include <paths.h>
+ #include <uuid.h>
+ 
+-#include <machine/endian.h>
++#include <sys/endian.h>
++#define __BYTE_ORDER    BYTE_ORDER
++#define __BIG_ENDIAN    BIG_ENDIAN
++#define __LITTLE_ENDIAN LITTLE_ENDIAN
++#define __swab16(x)	bswap16(x)
++#define __swab32(x)	bswap32(x)
++#define __swab64(x)	bswap64(x)
+ 
+ /* FreeBSD file API is 64-bit aware */
+ #define	fstat64		fstat
+@@ -38,15 +44,6 @@
+ #define	pread64		pread
  #define	fdatasync	fsync
  #define memalign(a,size)	valloc(size)
- 
+-
 -typedef u_int8_t	__u8;
 -typedef int8_t		__s8;
 -typedef u_int16_t	__u16;
@@ -12,7 +28,6 @@
 -typedef int32_t		__s32;
 -typedef u_int64_t	__u64;
 -typedef int64_t		__s64;
--
+ 
  #define constpp	char * const *
  
- #define EFSCORRUPTED	990	/* Filesystem is corrupted */
