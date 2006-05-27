5184b3d6f6dbb40e03fd00539f1963858075fde1
anray <anray@FreeBSD.org>
2006-05-27 16:31:57 +0000

    Update to 1.250
    Submitted by:	maintainer via email

--- net/nss_ldap/files/patch-ldap-nss.c
+++ net/nss_ldap/files/patch-ldap-nss.c
@@ -1,11 +1,11 @@
---- ldap-nss.c.orig	Sat Mar 25 00:26:47 2006
-+++ ldap-nss.c	Sat Mar 25 00:26:53 2006
-@@ -68,7 +68,7 @@
+--- ldap-nss.c.orig	Sat May 27 16:23:40 2006
++++ ldap-nss.c	Sat May 27 16:23:52 2006
+@@ -69,7 +69,7 @@
  #endif
  
  /* Try to handle systems with both SASL libraries installed */
 -#if defined(HAVE_SASL_SASL_H) && defined(HAVE_SASL_AUXPROP_REQUEST)
-+#if defined(HAVE_SASL_SASL_H)
++#if defined(HAVE_SASL_SASL_H) 
  #include <sasl/sasl.h>
  #elif defined(HAVE_SASL_H)
  #include <sasl.h>
