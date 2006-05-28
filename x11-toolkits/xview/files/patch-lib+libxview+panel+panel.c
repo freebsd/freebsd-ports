853e9efdda4d317226f85b3042843a24bcebb2d0
Thierry Thomas <thierry@FreeBSD.org>
2006-05-28 20:23:16 +0000

    Fix two bad patches.
    Submitted by:	Pedro F. Giffuni <giffunip (at) asme.org>

--- x11-toolkits/xview/files/patch-lib+libxview+panel+panel.c
+++ x11-toolkits/xview/files/patch-lib+libxview+panel+panel.c
@@ -1,10 +1,10 @@
---- ./lib/libxview/panel/panel.c.orig	Tue Jun 29 07:16:57 1993
-+++ ./lib/libxview/panel/panel.c	Sat Apr  1 18:25:27 2000
-@@ -27,6 +27,7 @@ Pkg_private int panel_view_init();
+--- lib/libxview/panel/panel.c.orig	Sat Mar 25 23:52:08 2006
++++ lib/libxview/panel/panel.c	Sun Mar 26 00:19:28 2006
+@@ -27,6 +27,7 @@
  Pkg_private int panel_destroy();
  
  static int      panel_layout();
-+static int	panel_unregister_view();
++static int	panel_unregister_view(Panel_info *panel, Xv_Window view);
  
  Xv_private_data Defaults_pairs xv_kbd_cmds_value_pairs[4];
  
