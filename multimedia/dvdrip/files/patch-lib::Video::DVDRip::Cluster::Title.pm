aaf6a178619c3d8941766ca639123b71ae1ccd32
mnag <mnag@FreeBSD.org>
2006-05-30 12:40:26 +0000

    - Update to 0.52.7 - Take maintainer - portlint(1)
    PR:		95859
    Submitted by:	Rainer Alves <rainer.alves___gmail.com>

--- multimedia/dvdrip/files/patch-lib::Video::DVDRip::Cluster::Title.pm
+++ multimedia/dvdrip/files/patch-lib::Video::DVDRip::Cluster::Title.pm
@@ -1,26 +1,13 @@
---- lib/Video/DVDRip/Cluster/Title.pm.orig	Sat Apr 10 11:18:03 2004
-+++ lib/Video/DVDRip/Cluster/Title.pm		Sun Jan  9 17:41:11 2005
-@@ -277,10 +277,10 @@
- 	} else {
- 		$command .=
- 			"dr_exec avimerge".
--			" -i $avi_file".
- 			" -p $audio_file".
- 			" -a $target_nr".
--			" -o $avi_file.merged &&".
-+			" -o $avi_file.merged ".
-+			" -i $avi_file &&".
- 			" mv $avi_file.merged $target_file &&".
- 			" rm $audio_file &&".
- 			" echo DVDRIP_SUCCESS";
-@@ -312,10 +312,9 @@
+--- lib/Video/DVDRip/Cluster/Title.pm.orig	Sun Aug 14 18:22:29 2005
++++ lib/Video/DVDRip/Cluster/Title.pm	Sun Apr 16 04:44:24 2006
+@@ -313,10 +313,10 @@
  
  	my $command =
  		"mkdir -m 0775 -p '$audio_video_psu_dir' && ".
 -		"${nice}dr_exec avimerge -i $avi_chunks_dir/*".
 -		" -o $audio_video_psu_file ";
--	
 +		"${nice}dr_exec avimerge -o $audio_video_psu_file";
+ 	
  	$command .= " -p $audio_psu_file " if not $self->is_ogg;
 +	$command .= " -i $avi_chunks_dir/*";
  
