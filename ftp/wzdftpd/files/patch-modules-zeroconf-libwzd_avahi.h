1b46b08cc9b0d692b588823b2ef7c90c73b438c6
novel <novel@FreeBSD.org>
2006-05-31 15:19:27 +0000

    Update to 0.7.2.
    PR:		98212 (based on)
    Submitted by:	Jui-Nan Lin <jnlin@csie.nctu.edu.tw>

---
+++ ftp/wzdftpd/files/patch-modules-zeroconf-libwzd_avahi.h
@@ -0,0 +1,10 @@
+--- modules/zeroconf/libwzd_avahi.h.orig	Wed May 31 14:55:16 2006
++++ modules/zeroconf/libwzd_avahi.h	Wed May 31 14:56:17 2006
+@@ -29,6 +29,7 @@
+ #include <stdlib.h>
+ #include <assert.h>
+ #include <string.h>
++#include <pthread.h>
+ 
+ #include <libwzd-core/wzd_structs.h>
+ #include <libwzd-core/wzd_log.h>
