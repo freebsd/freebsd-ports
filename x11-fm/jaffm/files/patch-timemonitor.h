0f9ad0b0e9f324de9d8f04bf9a879794754c7fd0
Emanuel Haupt <ehaupt@FreeBSD.org>
2006-05-31 01:09:54 +0000

    Fix build on 4.x
    PR:		97572
    Submitted by:	Shaun Amott <shaun@inerd.com> (maintainer)

---
+++ x11-fm/jaffm/files/patch-timemonitor.h
@@ -0,0 +1,9 @@
+--- timemonitor.h.orig	Mon Sep 22 14:54:23 2003
++++ timemonitor.h	Sun May 21 19:57:14 2006
+@@ -1,5 +1,6 @@
+ #ifndef TIMEMONITOR_H
+ #define TIMEMONITOR_H
++#include <time.h>
+ #include <string>
+ #include <vector>
+ 
