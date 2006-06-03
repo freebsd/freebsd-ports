7ebe8d6d9cb4ab697d3c74172558151fdcb6c122
Vanilla I. Shu <vanilla@FreeBSD.org>
2006-06-03 10:12:02 +0000

    Fix pkg-plist, set NOT_FOR_ARCHS to amd64.
    PR:		ports/95479
    Submitted by:	Jimmy F.Klarke <iss.jfk@gmail.dot.com>

--- chinese/firebird/files/patch-screen.c
+++ chinese/firebird/files/patch-screen.c
@@ -1,5 +1,5 @@
 --- src/screen.c.orig	Sat Jan 15 09:45:29 2000
-+++ src/screen.c	Tue Mar  8 03:06:59 2005
++++ src/screen.c	Sat Jun  3 17:58:30 2006
 @@ -27,7 +27,7 @@
  #include "screen.h"
  #include "edit.h"
@@ -9,7 +9,7 @@
  
  extern char clearbuf[];
  extern char cleolbuf[];
-@@ -538,14 +538,13 @@
+@@ -538,15 +538,12 @@
  int     dec[] = {1000000000, 100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1};
  
  void
@@ -19,11 +19,11 @@
  {
  	va_list ap;
 -	register char *fmt;
-+	register char;
  	char   *bp;
  	register int i, count, hd, indx;
 -	va_start(ap);
+-	fmt = va_arg(ap, char *);
 +	va_start(ap, fmt);
- 	fmt = va_arg(ap, char *);
  	while (*fmt != '\0') {
  		if (*fmt == '%') {
+ 			int     sgn = 1;
