8b3c5fa1fc44b02a1def338214bed21c98ba051c
erwin <erwin@FreeBSD.org>
2006-06-05 16:42:08 +0000

    Update to 2.1
    PR:		98419
    Submitted by:	miwi, with fixes from maintainer
    Approved by:	maintainer

--- dns/dualserver/files/extra-patch-dualserver.cpp
+++ dns/dualserver/files/extra-patch-dualserver.cpp
@@ -1,66 +1,59 @@
---- dualserver.cpp.orig	Sun Apr 30 15:32:37 2006
-+++ dualserver.cpp	Sun Apr 30 15:36:39 2006
-@@ -463,8 +463,8 @@
+--- dualserver.cpp.orig	Mon Jun  5 16:08:44 2006
++++ dualserver.cpp	Mon Jun  5 16:09:14 2006
+@@ -501,15 +501,15 @@
  	if (!req->rtype)
  	{
  		req->dnsp.header.ancount = 0;
 -		req->dnsp.header.rcode = 3;
 -		req->dnsp.header.ra = 0;
-+                req->dnsp.header.flags.rcode = 3;
-+                req->dnsp.header.flags.ra = 0;
++		req->dnsp.header.flags.rcode = 3;
++		req->dnsp.header.flags.ra = 0;
  	}
  
- 	if (cfig->servers[0])
-@@ -478,8 +478,8 @@
- 		req->dnsp.header.adcount = 0;
- 	}
- 
+ 	req->dnsp.header.nscount = 0;
+ 	req->dnsp.header.adcount = 0;
 -	req->dnsp.header.qr = 1;
 -	req->dnsp.header.aa = 1;
-+        req->dnsp.header.flags.qr = 1;
-+        req->dnsp.header.flags.aa = 1;
- 
+-
++	req->dnsp.header.flags.qr = 1;
++	req->dnsp.header.flags.aa = 1;
++	
  	char *raw = req->dnsp.data;
  
-@@ -649,7 +649,7 @@
- 	if (!cfig->dns[0])
- 		getdnserv(cfig);
+ 	for (int i = 1; i <= ntohs(req->dnsp.header.qdcount); i++)
+@@ -701,7 +701,7 @@
+ 	time_t currentTime = time(NULL);
+ 	req->lease = cfig->lease;
  
 -	if (ntohs(req->dnsp.header.qdcount) == 1 && ntohs(req->dnsp.header.opcode) == 0)
-+        if (ntohs(req->dnsp.header.qdcount) == 1 && ntohs(req->dnsp.header.flags.opcode) == 0)
++	if (ntohs(req->dnsp.header.qdcount) == 1 && ntohs(req->dnsp.header.flags.opcode) == 0)
  	{
  		char *raw = req->dnsp.data;
- 		fQu(req->query, &req->dnsp, raw);
-@@ -853,7 +853,7 @@
- 
- WORD fdnmess(data5 *req, data2 *cfig)
- {
--	if (!req->dnsp.header.rd)
-+        if (!req->dnsp.header.flags.rd)
- 		return 0;
- 
- 	if (!cfig->dns[0])
-@@ -923,10 +923,10 @@
- 	else
- 	{
- 		req->dnsp.header.ancount = 0;
--		req->dnsp.header.rcode = 3;
--		req->dnsp.header.ra = 0;
--		req->dnsp.header.qr = 1;
--		req->dnsp.header.aa = 0;
-+                req->dnsp.header.flags.rcode = 3;
-+                req->dnsp.header.flags.ra = 0;
-+                req->dnsp.header.flags.qr = 1;
-+                req->dnsp.header.flags.aa = 0;
- 		char *dp = req->dnsp.data;
- 		dp += pQu(dp, req->query);
- 		dp += pShort(dp, DNS_TYPE_A);
-@@ -1106,7 +1106,7 @@
+ 		raw += fQu(req->query, &req->dnsp, raw);
+@@ -721,7 +721,7 @@
+ 		{
+ 			if (!checkLocal(req, cfig))
+ 			{
+-				if (!req->dnsp.header.rd)
++				if (!req->dnsp.header.flags.rd)
+ 					return genMess(req, cfig);
+ 				else
+ 					return 0;
+@@ -1127,7 +1127,7 @@
  	if (!found)
  		req->bytes = 0;
  
 -	req->dnsp.header.aa = 0;
-+        req->dnsp.header.flags.aa = 0;
++	req->dnsp.header.flags.aa = 0;
  	return req->bytes;
  }
  
+@@ -2216,7 +2216,7 @@
+ 
+ 			mess.header.xid = 6;
+ 			mess.header.qdcount = htons(1);
+-			mess.header.rd = 1;
++			mess.header.flags.rd = 1;
+ 			char *raw = mess.data;
+ 			raw += pQu(raw, "1.0.0.127.in-addr.arpa");
+ 			raw += pShort(raw, DNS_TYPE_PTR);
