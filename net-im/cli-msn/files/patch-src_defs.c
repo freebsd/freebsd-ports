6be9bd3ef5030993eeac69b91b53b9f0dec24667
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2006-06-06 17:47:19 +0000

    CLI-MSN is a UNIX Command Line implementation of the MSN Messenger Client, coded purely in C with the ability to work in almost any UNIX/Linux variation. The desire for the creation of CLI-MSN came when little or no command-line based messengers were found to be available that implemented MSN Messenger.
    The client itself accepts commands based on that of an IRC clients nature
    (/cmd). Currently in the process of working out the file transfer.
    
    WWW:	http://mor-pah.net/
    
    PR:		ports/95186
    Submitted by:	Jose Alonso Cardenas Marquez <acardenas@bsd.org.pe>
    Approved by:	lawrance (mentor, implicit)

---
+++ net-im/cli-msn/files/patch-src_defs.c
@@ -0,0 +1,10 @@
+--- src/headers/defs.h	Wed Jan  4 10:52:17 2006
++++ src/headers/defs.h	Fri Mar 31 20:29:12 2006
+@@ -1,7 +1,6 @@
+ #define VERSION "CLI-MSN-2.2"
+ #define MAX_LINE_LENGTH 255                     /* Config File line length */
+ #define MAX_NO_LINES 4                          /* Config File No. Lines */
+-#define CONF_LOCATION "msn.conf"                /* Default locality of msn.conf */
+ #define MAX_LIST_SIZE 150
+ #define MAX_ACCOUNT_LEN 130
+ #define MAX_NICK_LEN 130
