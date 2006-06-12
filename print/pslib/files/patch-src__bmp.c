684d2a999569adea601f5be8345b44d3d22ea067
ijliao <ijliao@FreeBSD.org>
2006-06-12 01:43:27 +0000

    upgrade to 0.2.7
    PR:		98828
    Submitted by:	Ports Fury

---
+++ print/pslib/files/patch-src__bmp.c
@@ -0,0 +1,31 @@
+--- src/bmp.c.orig	Mon Apr 24 23:25:31 2006
++++ src/bmp.c	Thu Jun  8 18:59:00 2006
+@@ -41,10 +41,28 @@
+ #include <fcntl.h>
+ #include <unistd.h>
+ 
++#if (defined(__unix__) || defined(unix)) && !defined(USG)
++#include <sys/param.h>
++#endif
++
++#if defined(__FreeBSD__) && __FreeBSD_version >= 470000
++#include <sys/endian.h>
++
++#if __FreeBSD_version >= 500000
++#define BMPSwabShort(x) bswap16(x)
++#define BMPSwabLong(x) bswap32(x)
++#else
++#define BMPSwabShort(x) (be16toh(x))
++#define BMPSwabLong(x) (be32toh(x))
++#endif
++#endif
++
++#ifdef linux
+ #include <byteswap.h>
+ 
+ #define BMPSwabShort(x) *x = bswap_16 (*x)
+ #define BMPSwabLong(x) *x = bswap_32 (*x)
++#endif
+ 
+ #include <inttypes.h>
+ 
