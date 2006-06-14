3b7843e6f84a55db753dbe036bab217a23a18c4f
Michael Nottebrock <lofi@FreeBSD.org>
2006-06-14 18:53:15 +0000

    - Use shutdown -p now by default as kdm HALT_CMD. - Fix KDE Bug 128663 (double-free in kpager) - Fix KDE Bug 128757 (regression media:/ kioslave)
    PR: ports/98918
    
    Security: http://www.kde.org/info/security/advisory-20060614-1.txt
    Security: CVE-2006-2449

--- x11/kde4-workspace/files/patch-kioslave_media_mediamanager-fstabbackend.cpp
+++ x11/kde4-workspace/files/patch-kioslave_media_mediamanager-fstabbackend.cpp
@@ -1,5 +1,5 @@
---- kioslave/media/mediamanager/fstabbackend.cpp	Mon May 22 20:12:42 2006
-+++ kioslave/media/mediamanager/fstabbackend.cpp	Fri Mar 17 11:17:33 2006
+--- kioslave/media/mediamanager/fstabbackend.cpp	2006/03/23 09:19:38	521680
++++ kioslave/media/mediamanager/fstabbackend.cpp	2006/06/14 09:11:46	551306
 @@ -144,7 +144,7 @@
  
  void FstabBackend::handleMtabChange(bool allowNotification)
@@ -9,22 +9,42 @@
  	KMountPoint::List mtab = KMountPoint::currentMountPoints();
  
  	KMountPoint::List::iterator it = mtab.begin();
-@@ -158,14 +158,6 @@
- 
- 		if ( ::inExclusionPattern(*it, m_networkSharesOnly) ) continue;
- 
--		/* Did we know this already before ? If yes, then
--		   nothing has changed, do not stat the mount point. Avoids
--		   hang if network shares are stalling */
--		QString mtabEntry = dev + "*" + mp + "*" + fs;
+@@ -162,18 +162,19 @@
+ 		   nothing has changed, do not stat the mount point. Avoids
+ 		   hang if network shares are stalling */
+ 		QString mtabEntry = dev + "*" + mp + "*" + fs;
 -		bool isOldEntry = m_mtabEntries.contains(mtabEntry);
 -		new_mtabEntries+=mtabEntry;
 -		if (isOldEntry) continue;
--
++		if(m_mtabEntries.contains(mtabEntry)) {
++		        new_mtabIds += m_mtabEntries[mtabEntry];
++			continue;
++		}
+ 
  		QString id = generateId(dev, mp);
  		new_mtabIds+=id;
++		m_mtabEntries[mtabEntry] = id;
  
-@@ -226,7 +218,6 @@
+ 		if ( !m_mtabIds.contains(id) && m_fstabIds.contains(id) )
+ 		{
+ 			QString mime, icon, label;
+ 			guess(dev, mp, fs, true, mime, icon, label);
+-
+ 			m_mediaList.changeMediumState(id, true, false,
+ 			                              mime, icon, label);
+ 		}
+@@ -211,6 +212,10 @@
+ 			QString mp = medium->mountPoint();
+ 			QString fs = medium->fsType();
+ 
++
++			QString mtabEntry = dev + "*" + mp + "*" + fs;
++			m_mtabEntries.remove(mtabEntry);
++
+ 			QString mime, icon, label;
+ 			guess(dev, mp, fs, false, mime, icon, label);
+ 
+@@ -226,7 +231,6 @@
  	}
  
  	m_mtabIds = new_mtabIds;
