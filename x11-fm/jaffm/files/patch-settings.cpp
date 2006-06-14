6056cd2507dd6b20e6e776c66ea3a2427620b547
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2006-06-14 09:05:50 +0000

    - Update to 1.4 - Remove now redundant patches
    PR:		ports/98919
    Submitted by:	maintainer

--- x11-fm/jaffm/files/patch-settings.cpp
+++
@@ -1,8 +0,0 @@
---- settings.cpp.orig	Sun Mar 27 14:36:58 2005
-+++ settings.cpp	Sun May 21 19:56:07 2006
-@@ -1,3 +1,5 @@
-+#include <stdio.h>
-+
- #include "settings.h"
- #include "filehandler.h"
- 
