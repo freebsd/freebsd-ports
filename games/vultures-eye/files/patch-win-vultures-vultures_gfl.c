9d53c831559443ea94ea207f152611ef89382d16
Pav Lucistnik <pav@FreeBSD.org>
2006-06-16 22:19:03 +0000

    - Drop dependency on sdl_image
    PR:		ports/99004
    Submitted by:	Li-Lun Wang (Leland Wang) <llwang@infor.org> (maintainer)

---
+++ games/vultures-eye/files/patch-win-vultures-vultures_gfl.c
@@ -0,0 +1,11 @@
+--- win/vultures/vultures_gfl.c.orig	Tue Jun  6 21:23:28 2006
++++ win/vultures/vultures_gfl.c	Sat Jun 17 00:12:45 2006
+@@ -11,7 +11,7 @@
+ #endif
+ 
+ #include <SDL.h>
+-#include <SDL_image.h>
++/* #include <SDL_image.h> */
+ 
+ #include "png.h"
+ 
