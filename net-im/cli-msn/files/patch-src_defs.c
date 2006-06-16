39ae0f2585b76bd9cc2623ed76998eb48726bfa5
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2006-06-16 08:09:40 +0000

    - Update to 2.3
    Changes:
    - Added panels system
    - Completed Invite and multi conversation system
    
    PR:		98968
    Submitted by:	maintainer

--- net-im/cli-msn/files/patch-src_defs.c
+++ net-im/cli-msn/files/patch-src_defs.c
@@ -1,9 +1,9 @@
 --- src/headers/defs.h	Wed Jan  4 10:52:17 2006
 +++ src/headers/defs.h	Fri Mar 31 20:29:12 2006
 @@ -1,7 +1,6 @@
- #define VERSION "CLI-MSN-2.2"
+ #define VERSION "CLI-MSN-2.3"
  #define MAX_LINE_LENGTH 255                     /* Config File line length */
- #define MAX_NO_LINES 4                          /* Config File No. Lines */
+ #define MAX_NO_LINES 5                          /* Config File No. Lines */
 -#define CONF_LOCATION "msn.conf"                /* Default locality of msn.conf */
  #define MAX_LIST_SIZE 150
  #define MAX_ACCOUNT_LEN 130
