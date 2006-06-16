f121cfd31f4fb5774f42f1d89705938b0d6aa583
Maxim Sobolev <sobomax@FreeBSD.org>
2006-06-16 23:34:22 +0000

    Update to 0.0.2.p26.

--- comms/spandsp/files/patch-src-spandsp-dc_restore.h
+++ comms/spandsp/files/patch-src-spandsp-dc_restore.h
@@ -1,14 +1,16 @@
---- src/spandsp/dc_restore.h.orig	Tue Jan 18 15:05:48 2005
-+++ src/spandsp/dc_restore.h	Thu Jun 30 22:35:33 2005
-@@ -85,6 +85,11 @@
+
+$FreeBSD$
+
+--- src/spandsp/dc_restore.h.orig
++++ src/spandsp/dc_restore.h
+@@ -109,5 +109,10 @@
  }
- /*- End of function --------------------------------------------------------*/
+ #endif
  
 +#ifndef INT16_MAX
 +#define INT16_MAX 0x7fff
 +#define INT16_MIN (-0x7fff-1)
 +#endif
 +
- static inline int16_t saturate(int32_t amp)
- {
-     if (amp > INT16_MAX)
+ #endif
+ /*- End of file ------------------------------------------------------------*/
