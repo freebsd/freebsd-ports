65506bcd284c4e6ab8677f5a67bd4f57e9b809e5
Florent Thoumie <flz@FreeBSD.org>
2006-06-20 11:36:20 +0000

    - Update to 0.43.3 [1]. - Stop patching source to be C89 friendly and use USE_GCC=3.2+ instead.
    Submitted by:	ultragames93 <ultragames93@free.fr> [1]

--- emulators/raine/files/patch-source::alleg::blit.c
+++ emulators/raine/files/patch-source::alleg::blit.c
@@ -1,6 +1,6 @@
 --- source/alleg/blit.c.orig	Mon Jan 17 12:37:21 2005
 +++ source/alleg/blit.c	Mon Jan 17 12:38:10 2005
-@@ -1,6 +1,8 @@
+@@ -1,1 +1,1 @@
  #ifdef RAINE_WIN32
  #include <allegro.h>
  #include <winalleg.h>
--- emulators/raine/files/patch-source_bezel.c
+++ emulators/raine/files/patch-source_bezel.c
@@ -1,6 +1,6 @@
 --- source/bezel.c.orig	Thu Nov 17 18:24:59 2005
 +++ source/bezel.c	Thu Nov 17 18:49:28 2005
-@@ -18,7 +18,7 @@
+@@ -18,1 +18,1 @@
  #define __USE_ISOC99 1
  #include <math.h>
  
@@ -9,20 +9,3 @@
  // djgpp seems to have specificities for rounding...
  // why on earth non one implements cleanly such basic function ???
  #define round rint
-@@ -296,6 +296,7 @@
-     int w = bezel_width, h = bezel_height;
-     int multw =1, multh = 1;
-     int xoff2,yoff2,destx,desty,xxx,yyy;
-+    BITMAP *scaled_bezel;
- 
-     if (!w || !h) {
- 	    // if wants_switch_res = 0, then the bezel is not initalised when arriving here...
-@@ -316,7 +317,7 @@
- 
-     w *= multw; h *= multh;
-     print_debug("bezel: create bitmap %dx%d\n",w,h);
--    BITMAP *scaled_bezel = create_bitmap_ex(bitmap_color_depth(bezel_bitmap),w,h);
-+    scaled_bezel = create_bitmap_ex(bitmap_color_depth(bezel_bitmap),w,h);
-     stretch_blit(bezel_bitmap,scaled_bezel,0,0,bezel_bitmap->w-1,bezel_bitmap->h-1,0,0,w,h);
- 
-     if (rotate_screen == 3 || rotate_screen == 1) { // 270
