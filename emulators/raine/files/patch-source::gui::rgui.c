ff2c05b3ee81457e985a57ea98b6eb05f668a5b3
flz <flz@FreeBSD.org>
2006-06-20 11:36:20 +0000

    - Update to 0.43.3 [1]. - Stop patching source to be C89 friendly and use USE_GCC=3.2+ instead.
    Submitted by:	ultragames93 <ultragames93@free.fr> [1]

--- emulators/raine/files/patch-source::gui::rgui.c
+++ emulators/raine/files/patch-source::gui::rgui.c
@@ -1,6 +1,6 @@
 --- source/gui/rgui.c.orig	Mon Jan 17 12:07:21 2005
 +++ source/gui/rgui.c	Mon Jan 17 12:23:46 2005
-@@ -9,6 +9,10 @@
+@@ -9,1 +9,1 @@
  #include <winalleg.h>
  #endif
 
@@ -11,22 +11,3 @@
  #include "raine.h"
  #include "rgui.h"
  #include "rguiproc.h"
-@@ -1507,6 +1511,10 @@
- void dialog_oxygen(void)
- {
-
-+#ifdef RAINE_UNIX
-+  struct timeval timeout;
-+#endif
-+
- #ifdef BUFFER_GUI
-    static int z,z1,z2,z3;
-    int ta;
-@@ -1560,7 +1568,6 @@
-   // its sleep function has a 1ms resolution instead of 1us. Too bad...
-    Sleep(1000/60);
- #elif defined(RAINE_UNIX)
--  struct timeval timeout;
-   timeout.tv_sec = 0;
-   timeout.tv_usec = 1000000/60;
-   select(0, NULL, NULL, NULL, &timeout);
