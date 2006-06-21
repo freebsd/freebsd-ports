2440f22a83d39b71451ad9de5f8ca76d5cbd1cbe
Norikatsu Shigemura <nork@FreeBSD.org>
2006-06-21 06:54:43 +0000

    o Set stty min & vtime. o Bump PORTREVISION, accordingly.
    PR:		ports/92184
    Submitted by:	TOMITA Yoshinori <yoshint@flab.fujitsu.co.jp>

--- japanese/kterm/files/patch-main.c
+++ japanese/kterm/files/patch-main.c
@@ -1,12 +1,30 @@
---- main.c.orig	Tue Dec 20 22:31:20 2005
-+++ main.c	Tue Dec 20 22:35:22 2005
-@@ -237,7 +237,9 @@
- #endif
- #else /* } !SYSV { */			/* BSD systems */
- #ifndef linux
+--- main.c.orig	Wed Jun 21 15:49:06 2006
++++ main.c	Wed Jun 21 15:50:17 2006
+@@ -210,7 +210,9 @@
+ #undef FIOCLEX
+ #undef FIONCLEX
+ #define setpgrp2 setpgrp
 +#ifndef USE_POSIX_TERMIOS
  #include <sgtty.h>
 +#endif
- #endif
  #include <sys/resource.h>
- #define HAS_UTMP_UT_HOST
+ #endif
+ #ifdef sco
+@@ -1355,6 +1357,8 @@
+ 	d_tio.c_cc[VDISCARD] = CFLUSH;
+ 	d_tio.c_cc[VWERASE] = CWERASE;
+ 	d_tio.c_cc[VLNEXT] = CLNEXT;
++	d_tio.c_cc[VMIN] = 1;
++	d_tio.c_cc[VTIME] = 0;
+ #endif /* } */
+ #ifdef TIOCSLTC /* { */
+         d_ltc.t_suspc = CSUSP;		/* t_suspc */
+@@ -1403,6 +1407,8 @@
+ 	d_tio.c_cc[VQUIT] = CQUIT;		/* '^\'	*/
+     	d_tio.c_cc[VEOF] = CEOF;		/* '^D'	*/
+ 	d_tio.c_cc[VEOL] = CEOL;		/* '^@'	*/
++	d_tio.c_cc[VMIN] = 1;
++	d_tio.c_cc[VTIME] = 0;
+ #ifdef VSWTCH
+ 	d_tio.c_cc[VSWTCH] = CSWTCH;            /* usually '^Z' */
+ #endif
