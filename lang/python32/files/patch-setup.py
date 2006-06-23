06295f92854b2ca080077ea2a6bdee69f2c44429
Hye-Shik Chang <perky@FreeBSD.org>
2006-06-23 03:09:16 +0000

    Update python-devel to 2.5b1.

--- lang/python32/files/patch-setup.py
+++ lang/python32/files/patch-setup.py
@@ -1,16 +1,16 @@
---- setup.py.orig      Tue Aug 31 10:37:25 2004
-+++ setup.py   Fri Sep  3 10:42:53 2004
+--- setup.py.orig	Tue Jun  6 08:38:06 2006
++++ setup.py	Fri Jun 23 11:02:35 2006
 @@ -15,7 +15,7 @@
  from distutils.command.install_lib import install_lib
  
  # This global variable is used to hold the list of modules to be disabled.
 -disabled_module_list = []
-+disabled_module_list = ["_bsddb", "_tkinter", "gdbm", "mpz"]
++disabled_module_list = ["_bsddb", "_sqlite3", "_tkinter", "gdbm", "mpz"]
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (at the front) if
-@@ -1144,8 +1144,7 @@
-           ext_modules=[Extension('struct', ['structmodule.c'])],
+@@ -1470,8 +1470,7 @@
+           ext_modules=[Extension('_struct', ['_struct.c'])],
  
            # Scripts to install
 -          scripts = ['Tools/scripts/pydoc', 'Tools/scripts/idle',
