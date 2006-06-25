a2f525b0fa6b853b276dfe11a28743775f856bb3
Jeremy Messenger <mezz@FreeBSD.org>
2006-06-25 02:25:58 +0000

    HomeBank is the free software you have always wanted to manage your personal accounts at home. The main concept is to be light, simple and very easy to use. It brings you many features that allows you to analyze your finances in adetailed way instantly and dynamically with powerfull report tools based on filtering and graphical charts.
    Furthermore HomeBank benefits now for more than 10 years of users
    experiences and feedbacks as its development started in 1995 on Amiga
    computers. It is now available on Amiga, GNU/Linux, and will probably be
    soon available for Microsoft Windows and MacOS X systems, as GTK+ exists
    on it.
    
    WWW: http://homebank.free.fr/
    
    --
    
    Keep in mind, this is an alpha quality apps. (Homebank 3.2 alpha). I only
    test it a little so far.

---
+++ finance/homebank/files/patch-src_homebank.h
@@ -0,0 +1,10 @@
+--- src/homebank.h.orig	Sat Jun 24 20:53:39 2006
++++ src/homebank.h	Sat Jun 24 20:54:12 2006
+@@ -37,6 +37,7 @@
+ #include "data_access.h"
+ #include "widgets.h"
+ #include "misc.h"
++#include "prototypes.h"
+ 
+ #include "gtkdateentry.h"
+ #include "gtkchart.h"
