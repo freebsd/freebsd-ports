64cc00e0850ec1e5cf8bd794caf964678fc3aa78
Daichi GOTO <daichi@FreeBSD.org>
2006-06-29 12:39:00 +0000

    sysutils/psgconf: update to 3.2
    PR:		ports/98300
    Submitted by:	maintainer

--- sysutils/psgconf/files/patch-PSGConf.pm
+++ sysutils/psgconf/files/patch-PSGConf.pm
@@ -1,26 +1,15 @@
---- ./lib/PSGConf.pm.orig	Mon Jun 21 21:17:31 2004
-+++ ./lib/PSGConf.pm	Mon Jun 21 21:17:48 2004
-@@ -34,8 +34,8 @@
+--- ./lib/PSGConf.pm.orig	Wed May  3 08:55:41 2006
++++ ./lib/PSGConf.pm	Thu May  4 10:21:24 2006
+@@ -34,7 +34,7 @@
  my %defaults = (
  	config_dir		=> '/usr/local/share/psgconf/config',
- 	files_dir		=> '/usr/local/share/psgconf/files',
--	config_file		=> '/etc/psg.conf',
+ 	files_dir			=> '/usr/local/share/psgconf/files',
 -	modules_file		=> '/etc/psgconf_modules',
-+	config_file		=> '/usr/local/etc/psg.conf',
 +	modules_file		=> '/usr/local/etc/psgconf_modules',
  	tmpdir			=> "/var/tmp/psgconf.$$",
  	verbose			=> 0,
  	do_fix			=> 0,
-@@ -713,7 +713,7 @@
- =item config_file
- 
- Full path to B<psgconf> config file (if applicable).  Default is
--F</etc/psg.conf>.
-+F</usr/local/etc/psg.conf>.
- 
- =item files_dir
- 
-@@ -723,7 +723,7 @@
+@@ -678,7 +678,7 @@
  =item modules_file
  
  Full path to B<psgconf> modules file.  Default is
