1c0722ff51345b7b03900dce1f7e390e8deb65c1
Thierry Thomas <thierry@FreeBSD.org>
2006-07-04 21:54:35 +0000

    Teach about TCL / TK v. 8.4.13 and unbreak.

--- cad/netgen/files/patch-togl::togl.cpp
+++ cad/netgen/files/patch-togl::togl.cpp
@@ -1,12 +1,14 @@
 --- togl/togl.cpp.orig	Wed Sep 29 19:48:06 2004
-+++ togl/togl.cpp	Sun Jul 17 22:22:17 2005
-@@ -140,12 +140,16 @@
++++ togl/togl.cpp	Tue Jul  4 23:05:23 2006
+@@ -140,12 +140,18 @@
  #  include "tkInt8.4p4.h"
  #elif TK_MAJOR_VERSION==8 && TK_MINOR_VERSION==4 && TK_RELEASE_SERIAL==6
  #  include "tkInt8.4p6.h"
 +#elif TK_MAJOR_VERSION==8 && TK_MINOR_VERSION==4 && TK_RELEASE_SERIAL==7
 +#  include "tkInt8.4p6.h"
 +#elif TK_MAJOR_VERSION==8 && TK_MINOR_VERSION==4 && TK_RELEASE_SERIAL==11
++#  include "tkInt8.4p6.h"
++#elif TK_MAJOR_VERSION==8 && TK_MINOR_VERSION==4 && TK_RELEASE_SERIAL==13
 +#  include "tkInt8.4p6.h"
  #else
 -   Sorry, you will have to edit togl.c to include the right tkInt.h file
