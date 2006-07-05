68230e7cc9df5634f476e0af72f1fd81e189bcc4
anray <anray@FreeBSD.org>
2006-07-05 11:43:12 +0000

    Update to 22.0.50.20060703
    Submitted by:	keramida(maintainer)

--- editors/emacs-devel/files/patch-src__alloc.c
+++ editors/emacs-devel/files/patch-src__alloc.c
@@ -1,8 +1,6 @@
-Index: src/alloc.c
-===================================================================
---- src/alloc.c	(.../branches/gnu/emacs)	(revision 15)
-+++ src/alloc.c	(.../trunk/emacs)	(revision 15)
-@@ -4542,8 +4542,12 @@
+--- src/alloc.c.orig	Tue Jul 04 16:55:30 2006 +0300
++++ src/alloc.c	Tue Jul 04 16:58:01 2006 +0300
+@@ -4542,7 +4542,11 @@ mark_stack ()
       needed on ia64 too.  See mach_dep.c, where it also says inline
       assembler doesn't work with relevant proprietary compilers.  */
  #ifdef sparc
@@ -10,8 +8,7 @@ Index: src/alloc.c
 +  asm ("flushw");
 +#else
    asm ("ta 3");
- #endif
 +#endif
+ #endif
  
    /* Save registers that we need to see on the stack.  We need to see
-      registers used to hold register variables and registers used to
