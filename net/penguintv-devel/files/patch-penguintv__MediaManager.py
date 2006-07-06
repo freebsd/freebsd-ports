cad0a40eecd7994a4485f91dc2309f730c956eb0
Edwin Groothuis <edwin@FreeBSD.org>
2006-07-06 02:43:57 +0000

    First add files, then commit them

---
+++ net/penguintv-devel/files/patch-penguintv__MediaManager.py
@@ -0,0 +1,11 @@
+--- penguintv/MediaManager.py.orig	Thu Jul  6 12:41:43 2006
++++ penguintv/MediaManager.py	Thu Jul  6 12:41:47 2006
+@@ -18,7 +18,7 @@
+ import gnome
+ import gnomevfs
+ 
+-from penguintv import DOWNLOAD_ERROR, DOWNLOAD_PROGRESS, DOWNLOAD_WARNING, DOWNLOAD_QUEUED
++#from penguintv import DOWNLOAD_ERROR, DOWNLOAD_PROGRESS, DOWNLOAD_WARNING, DOWNLOAD_QUEUED
+ 
+ FINISHED=0
+ FINISHED_AND_PLAY=1
