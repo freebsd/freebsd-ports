76fd2429e085d1e9dec9fe0ec9719551323b3e12
itetcu <itetcu@FreeBSD.org>
2006-07-06 18:03:52 +0000

    cdcat is a powerful QT based tool for creating fully searcheable offline catalogs of the contents of any arbitrary media. Primarily it is most useful for cataloging CDs, DVDs, and other such removeable media. The catalogs can be quickly searched (including across multiple catalogs) with regular expressions, exported as CSV or HTML files, sorted, and statistical information gathered.
    WWW: http://cdcat.sourceforge.net/
    
    PR:		ports/96828
    Submitted by:	Aren Tyr <aren.tyr at gawab.com>

---
+++ deskutils/cdcat/files/patch-src-recode.cpp
@@ -0,0 +1,19 @@
+*** ./recode.cpp	Mon Aug 22 18:03:00 2005
+--- ../../cdcat.bsd/src/recode.cpp	Sun Apr 30 19:47:51 2006
+***************
+*** 12,18 ****
+  #include <string.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+! #include <malloc.h>
+  #include <ctype.h>
+  
+  char *recode2(const char *c)
+--- 12,18 ----
+  #include <string.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+! /* #include <malloc.h> */
+  #include <ctype.h>
+  
+  char *recode2(const char *c)
---
+++ deskutils/cdcat/files/patch-src-tparser.cpp
@@ -0,0 +1,20 @@
+*** ./tparser.cpp	Mon Sep 19 21:52:00 2005
+--- ../../cdcat.bsd/src/tparser.cpp	Sun Apr 30 19:56:55 2006
+***************
+*** 27,33 ****
+   */
+  //#include <iostream.h>
+  //#include <stdio.h>
+! #include <malloc.h>
+  #include <qstring.h>
+  
+  #include "tparser.h"
+--- 27,34 ----
+   */
+  //#include <iostream.h>
+  //#include <stdio.h>
+! /*#include <malloc.h> */
+! #include <stdlib.h>
+  #include <qstring.h>
+  
+  #include "tparser.h"
