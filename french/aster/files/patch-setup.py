fbf1b0649f76fbd2004768687fd736e204b6498f
thierry <thierry@FreeBSD.org>
2006-07-06 19:44:19 +0000

    Upgrade to 8.3.0-1.

--- french/aster/files/patch-setup.py
+++ french/aster/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Thu Jun 30 16:35:15 2005
-+++ setup.py	Fri Jul  1 20:46:30 2005
-@@ -260,6 +260,8 @@
-    # 1.3.2. ----- convert uname value to Code_Aster terminology...
-    if sys.platform == 'linux2':
-       cfg['IFDEF']='P_LINUX'
+--- setup.py.orig	Wed Jun 28 17:31:56 2006
++++ setup.py	Sat Jul  1 20:00:55 2006
+@@ -275,6 +275,8 @@
+          cfg['IFDEF']='LINUX64'
+       else:
+          cfg['IFDEF']='P_LINUX'
 +   elif sys.platform[:7] == 'freebsd':
 +      cfg['IFDEF']='FREEBSD'
-    elif sys.platform == 'x86_64':
-       cfg['IFDEF']='x86_64'
+    # elif sys.platform == 'win32':
+    #    ???
     elif sys.platform[:4] == 'osf1':
