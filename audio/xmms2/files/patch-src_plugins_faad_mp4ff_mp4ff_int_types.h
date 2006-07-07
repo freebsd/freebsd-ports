0613ab6cf0127a3c3e290ef2116db5382042fbeb
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2006-07-07 08:20:24 +0000

    - Update to 0.2.d.5 - Use the OPTIONS framework - Use bsd.scons.mk
    Many patches removed because we merged them upstream into XMMS2 HEAD
    
    Checkout the release notes at:
       http://wiki.xmms2.xmms.se/index.php/Release:DrEvil
    
    PR:		ports/99864
    Submitted by:	Alexander Botero-Lowry <alex@foxybanana.com> (maintainer)

---
+++ audio/xmms2/files/patch-src_plugins_faad_mp4ff_mp4ff_int_types.h
@@ -0,0 +1,11 @@
+--- src/plugins/faad/mp4ff/mp4ff_int_types.h.orig	Fri Jul  7 03:00:50 2006
++++ src/plugins/faad/mp4ff/mp4ff_int_types.h	Fri Jul  7 03:01:16 2006
+@@ -15,7 +15,7 @@
+ 
+ #else
+ 
+-#include <stdint.h>
++#include <inttypes.h>
+ 
+ #endif
+ 
