4d62f393a90a63bc259041d3737691297eb017b5
rafan <rafan@FreeBSD.org>
2006-07-08 14:20:39 +0000

    - Update to 0.9.0
    Approved by:	delphij (mentor, implicit)

--- devel/flowdesigner/files/patch-data-flow_src_SocketStream.cc
+++ devel/flowdesigner/files/patch-data-flow_src_SocketStream.cc
@@ -1,6 +1,6 @@
---- data-flow/src/SocketStream.cc.orig	Sun Apr 24 15:16:43 2005
-+++ data-flow/src/SocketStream.cc	Sun Apr 24 15:16:32 2005
-@@ -190,12 +190,12 @@
+--- data-flow/src/SocketStream.cc.orig	Wed Sep 21 20:21:25 2005
++++ data-flow/src/SocketStream.cc	Sat Jul  8 13:36:23 2006
+@@ -194,12 +194,12 @@
  
    size_t packet_len = 0;
    unsigned int flags = 0;
@@ -9,9 +9,9 @@
  
    switch (m_type) {
    case BROADCAST_TYPE:
- 
+     #ifndef __CYGWIN__
 -    packet_len = recvfrom(m_read_socket, (char*)packet, size, 0, (sockaddr*) &m_read_addr, &addr_len);
 +    packet_len = recvfrom(m_read_socket, (char*)packet, size, 0, (sockaddr*) &m_read_addr, (socklen_t *)&addr_len);
- 
-     if ((int) packet_len < 0) {
-       if (errno == EAGAIN) {
+     #else
+     #warning CYGWIN not yet supported for broadcast network_socket
+     #endif
