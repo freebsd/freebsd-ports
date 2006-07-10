03b8c0d7c7f2dcec45b95626e28cbe6190001471
linimon <linimon@FreeBSD.org>
2006-07-10 05:29:04 +0000

    Bring this up to what is currently being tested on the cluster, including ports/98105, ports/98565 (both forgotten last time), and ports/99370, a bugfix for USE_LDCONFIG with on-default prefix.

--- ports-mgmt/portmk/Mk/bsd.port.mk
+++ ports-mgmt/portmk/Mk/bsd.port.mk
@@ -215,6 +215,8 @@ FreeBSD_MAINTAINER=	portmgr@FreeBSD.org
 # NOT_FOR_ARCHS_REASON
 # NOT_FOR_ARCHS_REASON_${ARCH}
 #				- Reason why it's not for ${NOT_FOR_ARCHS}s
+# IA32_BINARY_PORT	- Set this instead of ONLY_FOR_ARCHS if the given port
+#				  fetches and installs compiled i386 binaries.
 #
 # Dependency checking.  Use these if your port requires another port
 # not in the list below.  (Default: empty.)
@@ -1062,6 +1064,7 @@ SH?=		/bin/sh
 SORT?=		/usr/bin/sort
 STRIP_CMD?=	/usr/bin/strip
 SU_CMD?=	/usr/bin/su root -c
+SYSCTL?=	/sbin/sysctl
 TAIL?=		/usr/bin/tail
 TEST?=		test				# Shell builtin
 TR?=		LANG=C /usr/bin/tr
@@ -1103,7 +1106,7 @@ OSREL!=	${UNAME} -r | ${SED} -e 's/[-(].*//'
 
 # Get __FreeBSD_version
 .if !defined(OSVERSION)
-OSVERSION!=	/sbin/sysctl -n kern.osreldate
+OSVERSION!=	${SYSCTL} -n kern.osreldate
 .endif
 
 # Get the object format.
@@ -1119,6 +1122,29 @@ MASTER_PORT?=${MASTERDIR:C/[^\/]+\/\.\.\///:C/[^\/]+\/\.\.\///:C/^.*\/([^\/]+\/[
 .else
 SLAVE_PORT?=	no
 MASTER_PORT?=
+.endif
+
+# Check the compatibility layer for amd64/ia64
+
+.if ${ARCH} == "amd64" || ${ARCH} =="ia64"
+.if exists(/usr/lib32)
+HAVE_COMPAT_IA32_LIBS?=  YES
+.endif
+.if !defined(HAVE_COMPAT_IA32_KERN)
+HAVE_COMPAT_IA32_KERN!= if ${SYSCTL} -a compat.ia32.maxvmem >/dev/null 2>&1; then echo YES; fi
+.endif
+.endif
+
+.if defined(IA32_BINARY_PORT) && ${ARCH} != "i386"
+.if ${ARCH} == "amd64" || ${ARCH} == "ia64"
+.if !defined(HAVE_COMPAT_IA32_KERN)
+IGNORE= you need a kernel with compiled-in IA32 compatibility to use this port.
+.elif !defined(HAVE_COMPAT_IA32_LIBS)
+IGNORE= you need the 32-bit libraries installed under /usr/lib32 to use this port.
+.endif
+.else
+IGNORE= you have to use i386 (or compatible) platform to use this port.
+.endif
 .endif
 
 # If they exist, include Makefile.inc, then architecture/operating
@@ -2832,7 +2858,7 @@ CONFIGURE_FAIL_MESSAGE?=	"Please report the problem to ${MAINTAINER} [maintainer
 .if defined(GNU_CONFIGURE)
 # Maximum command line length
 .if !defined(CONFIGURE_MAX_CMD_LEN)
-CONFIGURE_MAX_CMD_LEN!=	/sbin/sysctl -n kern.argmax
+CONFIGURE_MAX_CMD_LEN!=	${SYSCTL} -n kern.argmax
 .endif
 CONFIGURE_ARGS+=	--prefix=${PREFIX} ${CONFIGURE_TARGET}
 CONFIGURE_ENV+=		lt_cv_sys_max_cmd_len=${CONFIGURE_MAX_CMD_LEN}
@@ -3817,7 +3843,7 @@ install-ldconfig-file:
 .if ${USE_LDCONFIG:L} != "${PREFIX}/lib"
 	@${ECHO_MSG} "===>   Installing ldconfig configuration file"
 .if defined(NO_LDCONFIG_MTREE)
-	@${MKDIR} ${LDCONFIG_DIR}
+	@${MKDIR} ${PREFIX}/${LDCONFIG_DIR}
 .endif
 	@${ECHO_CMD} ${USE_LDCONFIG} | ${TR} ' ' '\n' \
 		> ${PREFIX}/${LDCONFIG_DIR}/${UNIQUENAME}
@@ -5825,3 +5851,4 @@ install-desktop-entries:
 
 .endif
 # End of post-makefile section.
+
