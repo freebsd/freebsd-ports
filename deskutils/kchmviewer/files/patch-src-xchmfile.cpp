0089ffeda5c2714dd230f35a14184f6893f84018
Markus Brueffer <markus@FreeBSD.org>
2006-07-10 10:12:54 +0000

    Update to version 2.6

--- deskutils/kchmviewer/files/patch-src-xchmfile.cpp
+++ deskutils/kchmviewer/files/patch-src-xchmfile.cpp
@@ -1,6 +1,6 @@
---- src/xchmfile.cpp.orig	Mon Jul 25 12:27:20 2005
-+++ src/xchmfile.cpp	Mon Jul 25 12:28:16 2005
-@@ -1189,7 +1189,7 @@
+--- src/xchmfile.cpp.orig	Mon Jul 10 11:44:50 2006
++++ src/xchmfile.cpp	Mon Jul 10 11:45:49 2006
+@@ -1278,7 +1278,7 @@
  		FIXENDIAN32(off_url);
  
  		if ( RetrieveObject ( &m_chmURLSTR, buf, off_url + 8, sizeof(buf) - 1) == 0 )
