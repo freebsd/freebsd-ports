b606ddbc6ba8d23a61a94e0097684cf26a2c8069
itetcu <itetcu@FreeBSD.org>
2006-07-12 19:44:58 +0000

    Update to 1.0.r2
    PR:		ports/99805
    Submitted by:	maintainer

--- multimedia/tunapie/files/patch-src-TunerFrame1.py
+++ multimedia/tunapie/files/patch-src-TunerFrame1.py
@@ -1,18 +1,18 @@
 $FreeBSD$
---- src/TunerFrame1.py	Tue May  9 23:17:31 2006
-+++ src/TunerFrame1.py.port	Wed May 10 11:38:29 2006
-@@ -352,10 +352,10 @@
+--- src/TunerFrame1.py	Sun Jul  2 20:16:55 2006
++++ src/TunerFrame1.py.port	Tue Jul  4 15:07:01 2006
+@@ -422,10 +422,10 @@
          for name in self.favname:
              self.favorites.Append(name)
          self.name=[]
 -        self.mediaplayer='/usr/bin/xmms'
--        self.videoplayer='/usr/bin/mplayer -nojoystick'
+-        self.videoplayer='/usr/bin/totem'
 -        self.term='/usr/bin/x-terminal-emulator'
 -        self.ripper='/usr/bin/streamripper -t -u winamp2 -o never'
 +        self.mediaplayer='%%X11BASE%%/bin/xmms'
-+        self.videoplayer='%%LOCALBASE%%/bin/mplayer -nojoystick'
++        self.videoplayer='%%X11BASE%%/bin/xine'
 +        self.term='%%X11BASE%%/bin/xterm'
 +        self.ripper='%%LOCALBASE%%/bin/streamripper -t -u winamp2 -o never'
          self.rdir=self.home+'/.tunapie/streamripper'
          self.shoutice=1 #0 is icecast server 1 is shoutcast
-         self.mediaplayer,self.videoplayer,self.term,self.ripper,self.rdir,self.shoutice=\
+         self.maxd=100
--- multimedia/tunapie/files/patch-src-TunerPrefs.py
+++ multimedia/tunapie/files/patch-src-TunerPrefs.py
@@ -1,7 +1,7 @@
 $FreeBSD$
---- src/TunerPrefs.py	Wed May 24 23:10:23 2006
-+++ src/TunerPrefs.py.port	Sat May 27 10:59:16 2006
-@@ -74,7 +74,7 @@
+--- src/TunerPrefs.py	Sun Jul  2 10:49:31 2006
++++ src/TunerPrefs.py.port	Tue Jul  4 15:11:29 2006
+@@ -77,7 +77,7 @@
  
          self.audiopath = wx.TextCtrl(id=wxID_PREFERENCESAUDIOPATH,
                name='audiopath', parent=self, pos=wx.Point(160, 16),
@@ -10,16 +10,16 @@ $FreeBSD$
          self.audiopath.SetFont(wx.Font(7, 77, wx.NORMAL, wx.NORMAL, False,
                u'Sans'))
          self.audiopath.SetBackgroundColour(wx.Colour(255, 253, 239))
-@@ -82,7 +82,7 @@
+@@ -85,7 +85,7 @@
  
          self.videopath = wx.TextCtrl(id=wxID_PREFERENCESVIDEOPATH,
                name='videopath', parent=self, pos=wx.Point(160, 48),
 -              size=wx.Size(208, 22), style=0, value='/usr/bin/mplayer')
-+              size=wx.Size(208, 22), style=0, value='%%LOCALBASE%%/bin/mplayer')
++              size=wx.Size(208, 22), style=0, value='%%X11BASE%%/bin/xine')
          self.videopath.SetFont(wx.Font(7, 77, wx.NORMAL, wx.NORMAL, False,
                u'Sans'))
          self.videopath.SetBackgroundColour(wx.Colour(255, 253, 239))
-@@ -110,7 +110,7 @@
+@@ -113,7 +113,7 @@
          self.termpath = wx.TextCtrl(id=wxID_PREFERENCESTERMPATH,
                name='termpath', parent=self, pos=wx.Point(160, 80),
                size=wx.Size(208, 22), style=0,
@@ -28,7 +28,7 @@ $FreeBSD$
          self.termpath.SetFont(wx.Font(7, wx.SWISS, wx.NORMAL, wx.NORMAL, False,
                u'Sans'))
          self.termpath.SetBackgroundColour(wx.Colour(255, 253, 239))
-@@ -118,7 +118,7 @@
+@@ -121,7 +121,7 @@
  
          self.rippath = wx.TextCtrl(id=wxID_PREFERENCESRIPPATH, name='rippath',
                parent=self, pos=wx.Point(160, 112), size=wx.Size(208, 22),
