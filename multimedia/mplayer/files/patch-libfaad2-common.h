0a8c12711850fb24f66348512f8b0ea8c7635ea5
itetcu <itetcu@FreeBSD.org>
2006-07-12 01:53:07 +0000

    - Update to the latest mplayer release, 1.0pre8. - OPTIONify - Due to some changes in the official developent tree, some of our local patches became obsolete and where removed.
    PR:		ports/99871
    Submitted by:	maintainer

--- multimedia/mplayer/files/patch-libfaad2-common.h
+++ multimedia/mplayer/files/patch-libfaad2-common.h
@@ -1,13 +1,14 @@
---- libfaad2/common.h.orig	Thu Mar 10 15:33:19 2005
-+++ libfaad2/common.h	Thu Mar 10 15:34:38 2005
-@@ -328,7 +328,9 @@
+--- libfaad2/common.h.orig	Thu Jun 15 10:39:27 2006
++++ libfaad2/common.h	Thu Jun 15 10:40:18 2006
+@@ -318,8 +318,9 @@
+       *y2 = MUL_F(x2, c1) - MUL_F(x1, c2);
    }
  
- 
--  #if defined(_WIN32) && !defined(__MINGW32__)
+-
+-  #if defined(_WIN32) && !defined(__MINGW32__) && !defined(HAVE_LRINTF)
 +  #if defined(HAVE_LRINTF)
 +    #define HAS_LRINTF
-+  #elif defined(_WIN32) && !defined(__MINGW32__)
++  #elif defined(_WIN32) && !defined(__MINGW32__) && !defined(HAVE_LRINTF)
      #define HAS_LRINTF
      static INLINE int lrintf(float f)
      {
