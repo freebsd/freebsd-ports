b024ce7969809bac1da4a58488075805f4937cc8
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2006-07-12 17:30:51 +0000

    - update archivers/libunrar port to 3.60.b6 - new added patch fix bug which can cause a crash on unpacking broken rar file using RarOpenEX function (the problem also was reported to the vendor)
    PR:		ports/100143
    Submitted by:	maintainer

--- archivers/libunrar/files/patch-dll.cpp
+++ archivers/libunrar/files/patch-dll.cpp
@@ -1,16 +1,23 @@
 diff -ruN unrar.orig/dll.cpp unrar/dll.cpp
---- dll.cpp.orig       2005-08-03 20:34:30.000000000 +0400
-+++ dll.cpp    2006-01-11 12:13:33.000000000 +0300
-@@ -271,8 +271,10 @@
++++ dll.cpp
+@@ -59,8 +59,8 @@
+     }
+     if (!Data->Arc.IsArchive(false))
+     {
+-      delete Data;
+       r->OpenResult=Data->Cmd.DllError!=0 ? Data->Cmd.DllError:ERAR_BAD_ARCHIVE;
++      delete Data;
+       return(NULL);
+     }
+     r->Flags=Data->Arc.NewMhd.Flags;
+@@ -272,7 +272,9 @@
        strcpy(Data->Cmd.Command,Operation==RAR_EXTRACT ? "X":"T");
        Data->Cmd.Test=Operation!=RAR_EXTRACT;
        bool Repeat=false;
 -      Data->Extract.ExtractCurrentFile(&Data->Cmd,Data->Arc,Data->HeaderSize,Repeat);
--
 +      
 +      if(Data->Extract.ExtractCurrentFile(&Data->Cmd,Data->Arc,Data->HeaderSize,Repeat) == false)
 +        return (ERAR_UNKNOWN);
-+
+ 
        while (Data->Arc.ReadHeader()!=0 && Data->Arc.GetHeaderType()==NEWSUB_HEAD)
        {
-         Data->Extract.ExtractCurrentFile(&Data->Cmd,Data->Arc,Data->HeaderSize,Repeat);
