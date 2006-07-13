917b8f06f02f0e69e678719db6702411695658e5
pav <pav@FreeBSD.org>
2006-07-13 08:50:42 +0000

    - Update to 0.2.2
    PR:		ports/100193
    Submitted by:	Daniel <daniel@netwalk.org> (maintainer)

--- news/sabnzbd/files/patch-setup.py
+++ news/sabnzbd/files/patch-setup.py
@@ -1,18 +1,25 @@
-*** setup.py.orig	Fri Mar 24 23:26:24 2006
---- setup.py	Fri Mar 24 23:27:22 2006
-***************
-*** 15,20 ****
-        license = 'Academic Free License 2.1',
-!       data_files = [('share/doc/SABnzbd-' + sabnzbd.__version__,
-!                      ['SABnzbd.ini.sample', 'README.txt', 'LICENSE.txt',
-!                       'TODO.txt', 'CHANGELOG.txt', 'UPGRADE.txt']),
-!                     ('share/SABnzbd-' + sabnzbd.__version__ + '/templates',
-                               ['templates/default.css', 'templates/history.tmpl',
---- 15,21 ----
-        license = 'Academic Free License 2.1',
-!       data_files = [('share/doc/SABnzbd',
-!                              ['README.txt', 'LICENSE.txt',
-!                               'TODO.txt', 'CHANGELOG.txt', 'UPGRADE.txt']),
-!                     ('share/SABnzbd',['SABnzbd.ini.sample']),
-!                     ('share/SABnzbd' + '/templates',
-                               ['templates/default.css', 'templates/history.tmpl',
+--- setup.py.orig	Wed Jul 12 09:55:04 2006
++++ setup.py	Wed Jul 12 20:44:42 2006
+@@ -13,10 +13,11 @@
+       packages = ['sabnzbd', 'sabnzbd.utils', 'sabnzbd.utils.multiauth'],
+       platforms = ['posix'],
+       license = 'GNU General Public License 2 (GPL2)',
+-      data_files = [('share/doc/SABnzbd-' + sabnzbd.__version__,
+-                     ['SABnzbd.ini.sample', 'README.txt', 'LICENSE.txt',
+-                      'TODO.txt', 'CHANGELOG.txt', 'UPGRADE.txt']),
+-                    ('share/SABnzbd-' + sabnzbd.__version__ + '/templates',
++      data_files = [('share/doc/SABnzbd',
++                             ['README.txt', 'LICENSE.txt',
++                              'TODO.txt', 'CHANGELOG.txt', 'UPGRADE.txt']),
++                    ('share/SABnzbd',['SABnzbd.ini.sample']),
++                    ('share/SABnzbd' + '/templates',
+                              ['templates/default.css', 'templates/history.tmpl',
+                               'templates/main.tmpl',
+ 			      'templates/connection_info.tmpl',
+@@ -30,5 +31,5 @@
+                               'templates/config_scheduling.tmpl',
+                               'templates/config_rss.tmpl',
+                               'templates/static/placeholder.txt']),
+-                    ('share/SABnzbd-' + sabnzbd.__version__ + '/templates/static',
++                    ('share/SABnzbd/templates/static',
+                               ['templates/static/placeholder.txt'])])
