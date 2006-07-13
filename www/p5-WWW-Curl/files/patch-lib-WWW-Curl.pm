20d9aaddea64c4212722579990c8019d43652394
roam <roam@FreeBSD.org>
2006-07-13 14:22:26 +0000

    Update to WWW-Curl 3.02, mostly based on the PR, but with some additional modifications.
    PR:		98560
    Submitted by:	Rong-En Fan <rafan@infor.org>

---
+++ www/p5-WWW-Curl/files/patch-lib-WWW-Curl.pm
@@ -0,0 +1,11 @@
+--- lib/WWW/Curl.pm.orig	Thu Jul 13 15:04:35 2006
++++ lib/WWW/Curl.pm	Thu Jul 13 15:04:18 2006
+@@ -35,7 +35,7 @@
+ Version 3.02 adds some backwards compatibility for scripts still using
+ 'WWW::Curl::easy' names.
+ 
+-Version 3.00 adds WWW::Curl::Multi interface, and a new module names
++Version 3.00 adds WWW::Curl::Multi interface, and new module names
+ following perl conventions (WWW::Curl::Easy rather than WWW::Curl::easy),
+ by Sebastian Riedel <sri@cpan.org>
+ 
