63575abc7b8f108b7e0d7999bc0b96d9871d817d
itetcu <itetcu@FreeBSD.org>
2006-07-14 21:53:59 +0000

    - Add QT frontend support - Add support for Type1 fonts via T1 library - Add NLS support (French localization) - Fix GCC 4.1 compatiblity - Fix comment - Add OPTIONS - Enable builds with optimized CFLAGS - Fix patchname to make portlint happy - Pass maintainership to submitter
    Added file(s):
    - files/patch-Makefile.in
    - files/patch-src_graph_graphqtdlg.h
    - files/patch-src_graph_graphunixx11.c
    - files/patch-src_include_global.h
    
    Removed file(s):
    - files/patch-src::graph::graphunixx11.c
    
    PR:		ports/100285
    Submitted by:	Stanislav Sedov <ssedov@mbsd.msk.ru>

---
+++ cad/electric/files/patch-src_graph_graphqtdlg.h
@@ -0,0 +1,10 @@
+--- src/graph/graphqtdlg.h.orig	Fri Jul 14 00:33:51 2006
++++ src/graph/graphqtdlg.h	Fri Jul 14 00:34:01 2006
+@@ -59,6 +59,7 @@
+ class QSignalMapper;
+ class EScrollField;
+ class EDialogPrivate;
++class EDialog;
+ 
+ class EDialogPrivate: public QDialog
+ {
