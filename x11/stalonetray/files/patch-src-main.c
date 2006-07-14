b2b5fc6c94c9cb6d02279f1f771cf745d61fbf06
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2006-07-14 08:09:19 +0000

    Update to 0.4
    PR:		ports/100273
    Submitted by:	maintainer

--- x11/stalonetray/files/patch-src-main.c
+++ x11/stalonetray/files/patch-src-main.c
@@ -1,6 +1,16 @@
---- src/main.c.orig	Thu Apr  6 14:16:26 2006
-+++ src/main.c	Thu Apr  6 14:16:06 2006
-@@ -423,16 +423,20 @@
+--- src/main.c.orig	Thu Jul 13 19:06:09 2006
++++ src/main.c	Thu Jul 13 19:07:23 2006
+@@ -131,7 +131,9 @@
+ 			tray_data.xsh.width, tray_data.xsh.height,
+ 			tray_data.xsh.x, tray_data.xsh.y));
+ 
++#ifdef DEBUG
+ 	forall_icons(&print_icon_data);	
++#endif
+ 	DBG(3, ("===================================\n"));
+ 	trace_mode = 0;
+ }
+@@ -455,16 +457,20 @@
  					unlayout_icon(ti); /* or hide ? */
  					return;
  				}
@@ -9,14 +19,14 @@
 +#endif
  				update_positions();
  				tray_set_constraints();
- 				DBG(4, "icon position were updated\n");
+ 				DBG(4, ("icon position were updated\n"));
  			} else {
  				hide_window(ti);
  			}
 +#ifdef DEBUG
- 			DBG(4, "====================\n");
+ 			DBG(4, ("====================\n"));
  			forall_icons(&print_icon_data);
- 			DBG(4, "====================\n");
+ 			DBG(4, ("====================\n"));
 +#endif
  		}
  	}
