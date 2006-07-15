7ef853c3e00054e91afe85411eeb318725bd97c5
rafan <rafan@FreeBSD.org>
2006-07-15 00:10:53 +0000

    - Update to 1.5.3 - Pass maintainership to submitter - Reformat pkg-descr - Workaround for sword 1.5.8 (LIB_DEPENDS -> {RUN,BUILD}_DEPENDS)
    PR:		ports/100213
    Submitted by	Thomas Abthorpe <thomas goodking.ca>

--- misc/bibletime/files/patch-bibletime::backend::cswordbackend.cpp
+++ misc/bibletime/files/patch-bibletime::backend::cswordbackend.cpp
@@ -1,11 +1,9 @@
---- bibletime/backend/cswordbackend.cpp	Thu Jan 29 01:37:40 2004
-+++ bibletime/backend/cswordbackend.cpp	Sat Aug 21 15:12:25 2004
-@@ -66,7 +67,7 @@
- }
+--- bibletime/backend/cswordbackend.cpp.orig	Sat Sep 10 04:19:40 2005
++++ bibletime/backend/cswordbackend.cpp	Thu Jul 13 08:58:04 2006
+@@ -1,3 +1,6 @@
++//
++// please send this patch- to the attic.
++//
+ /********* Read the file LICENSE for license details. *********/
  
- CSwordBackend::CSwordBackend(const QString& path)
--	: sword::SWMgr((!path.isEmpty() ? path.local8Bit() : 0), false, new sword::EncodingFilterMgr( sword::ENC_UTF8 ))
-+        : sword::SWMgr((!path.isEmpty() ? (const char*)path.local8Bit() : 0), false, new sword::EncodingFilterMgr( sword::ENC_UTF8 ))
- {
-   qWarning("backend constructed with a path argument! %s", path.latin1());
- 	m_displays.entry = 0;
+ //BibleTime includes
