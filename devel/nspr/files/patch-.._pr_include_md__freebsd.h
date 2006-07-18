f04a5323108e56ae81918e28aa34240caa70cb20
marcus <marcus@FreeBSD.org>
2006-07-18 13:25:44 +0000

    Chase the recent getproto*_r additions to RELENG_6.

--- devel/nspr/files/patch-.._pr_include_md__freebsd.h
+++ devel/nspr/files/patch-.._pr_include_md__freebsd.h
@@ -4,7 +4,7 @@
  #define _PR_IPV6_V6ONLY_PROBE
  #endif
  
-+#if __FreeBSD_version >= 700016
++#if (__FreeBSD_version >= 700016) || (__FreeBSD_version < 700000 && __FreeBSD_version >= 601103)
 +#define _PR_HAVE_GETPROTO_R
 +#define _PR_HAVE_5_ARG_GETPROTO_R
 +#endif
