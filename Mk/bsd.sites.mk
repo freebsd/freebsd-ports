1f562ae1c03fb8cc54d8f4e09aad4bcaf72b8c46
Jean Milanez Melo <jmelo@FreeBSD.org>
2006-07-21 16:29:48 +0000

    - Remove CPAN bad sites: 	* ftp://ftp.is.co.za/programming/perl/CPAN/modules/by-module/ 	* ftp://ftp.ucr.ac.cr/pub/Unix/CPAN/modules/by-module/

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -729,10 +729,8 @@ MASTER_SITE_PERL_CPAN+=	\
 	ftp://ftp.mirrorservice.org/sites/ftp.funet.fi/pub/languages/perl/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://csociety-ftp.ecn.purdue.edu/pub/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.isu.net.sa/pub/CPAN/modules/by-module/%SUBDIR%/ \
-	ftp://ftp.ucr.ac.cr/pub/Unix/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.cs.colorado.edu/pub/perl/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://cpan.pop-mg.com.br/pub/CPAN/modules/by-module/%SUBDIR%/ \
-	ftp://ftp.is.co.za/programming/perl/CPAN/modules/by-module/%SUBDIR%/ \
 	http://at.cpan.org/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.chg.ru/pub/lang/perl/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.auckland.ac.nz/pub/perl/CPAN/modules/by-module/%SUBDIR%/
