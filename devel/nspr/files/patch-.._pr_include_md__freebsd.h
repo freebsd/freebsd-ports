35412b878ea687fa50fb79c93d08e95c4b9fa7e1
Joe Marcus Clarke <marcus@FreeBSD.org>
2006-07-23 17:21:55 +0000

    Add additional definitions now that we have gethost_r functions and hour normal gethost functions are thread-safe.  Also, wrap thread-safe definitions around a check for _PR_PTHREADS.
    Reported by:	Sean C. Farley <sean-freebsd@farley.org>

--- devel/nspr/files/patch-.._pr_include_md__freebsd.h
+++ devel/nspr/files/patch-.._pr_include_md__freebsd.h
@@ -1,12 +1,17 @@
---- ../pr/include/md/_freebsd.h.orig	Sat Apr 22 17:14:55 2006
-+++ ../pr/include/md/_freebsd.h	Sat Apr 22 17:14:47 2006
-@@ -106,6 +106,11 @@
+--- ../pr/include/md/_freebsd.h.orig	Mon Nov 22 16:24:53 2004
++++ ../pr/include/md/_freebsd.h	Sun Jul 23 13:15:29 2006
+@@ -106,6 +106,16 @@
  #define _PR_IPV6_V6ONLY_PROBE
  #endif
  
 +#if (__FreeBSD_version >= 700016) || (__FreeBSD_version < 700000 && __FreeBSD_version >= 601103)
++#if defined(_PR_PTHREADS)
 +#define _PR_HAVE_GETPROTO_R
 +#define _PR_HAVE_5_ARG_GETPROTO_R
++#define _PR_HAVE_GETHOST_R
++#define _PR_HAVE_GETHOST_R_INT
++#define _PR_HAVE_THREADSAFE_GETHOST
++#endif
 +#endif
 +
  #define USE_SETJMP
