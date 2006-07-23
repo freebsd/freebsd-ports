4c345ed272a9daa65c8ea4789ba74e77ddb7cd4b
Alejandro Pulver <alepulver@FreeBSD.org>
2006-07-23 18:56:26 +0000

    - Bump PORTREVISION. - Use Makefile for building instead of scons, and remove Perl dependency. - Use pkg-plist. - Add OPTIONS. Allow to build without X11, threaded version, etc. - Fix building on non-i386 and FreeBSD 4.X. - Respect CFLAGS when optimizations are enabled. - Add patch to fix 2 security vulnerabilities [1].
    PR:		ports/98295
    Submitted by:	alepulver (myself)
    Approved by:	maintainer (timeout)
    Obtained from:	q3icculus (http://icculus.org/quake3/) [1]
    Security:	CVE-2006-2082 [1]

--- games/cleanq3/files/patch-code-qcommon-common.c
+++ games/cleanq3/files/patch-code-qcommon-common.c
@@ -1,15 +1,15 @@
---- code/qcommon/common.c.orig	Mon Aug 15 23:10:07 2005
-+++ code/qcommon/common.c	Wed Nov 23 10:53:29 2005
+--- ./code/qcommon/common.c.orig	Wed May 31 20:52:07 2006
++++ ./code/qcommon/common.c	Wed May 31 20:52:09 2006
 @@ -24,15 +24,8 @@
  #include "../game/q_shared.h"
  #include "qcommon.h"
  #include <setjmp.h>
 -#ifdef __linux__
-+#include <sys/types.h>
- #include <netinet/in.h>
+-#include <netinet/in.h>
 -#else
 -#if defined(MACOS_X)
--#include <netinet/in.h>
++#include <sys/types.h>
+ #include <netinet/in.h>
 -#else
 -#include <winsock.h>
 -#endif
@@ -17,3 +17,12 @@
  
  int demo_protocols[] =
  { 66, 67, 68, 0 };
+@@ -2367,7 +2360,7 @@
+ 	// cvar and command buffer management
+ 	Com_ParseCommandLine( commandLine );
+ 
+-//	Swap_Init ();
++	Swap_Init ();
+ 	Cbuf_Init ();
+ 
+ 	Com_InitZoneMemory();
