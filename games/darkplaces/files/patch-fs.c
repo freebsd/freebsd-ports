ae6b84d43aebcb25da9b72030743453a211789ad
alepulver <alepulver@FreeBSD.org>
2006-07-24 03:19:09 +0000

    - Update to version 20060606. - Clean-up.

--- games/darkplaces/files/patch-fs.c
+++ games/darkplaces/files/patch-fs.c
@@ -1,10 +1,10 @@
---- fs.c.orig	Tue Aug  2 23:18:34 2005
-+++ fs.c	Sun Jan 15 17:38:11 2006
-@@ -933,7 +933,7 @@
+--- fs.c.orig	Sun May 28 18:26:08 2006
++++ fs.c	Sun Jul 23 16:31:17 2006
+@@ -1061,7 +1061,7 @@
  
  	fs_mempool = Mem_AllocPool("file management", 0, NULL);
  
--	strcpy(fs_basedir, ".");
+-	strcpy(fs_basedir, "");
 +	strcpy(fs_basedir, Q1DIR);
  	strcpy(fs_gamedir, "");
  
