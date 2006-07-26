2d2159ce84fe4012056aeb14d300e9d88d7b7aec
sobomax <sobomax@FreeBSD.org>
2006-07-26 01:21:25 +0000

    Add support for 7-CURRENT.

--- net-mgmt/net-snmp/files/patch-ucd_snmp.h
+++ net-mgmt/net-snmp/files/patch-ucd_snmp.h
@@ -1,11 +1,16 @@
---- agent/mibgroup/ucd_snmp.h.orig	Mon Oct 25 21:36:55 2004
-+++ agent/mibgroup/ucd_snmp.h	Mon Oct 25 21:37:20 2004
-@@ -39,6 +39,8 @@
+
+$FreeBSD$
+
+--- agent/mibgroup/ucd_snmp.h.orig
++++ agent/mibgroup/ucd_snmp.h
+@@ -39,6 +39,10 @@
  config_arch_require(freebsd4, ucd-snmp/memory_freebsd2)
  config_arch_require(freebsd5, ucd-snmp/vmstat_freebsd2)
  config_arch_require(freebsd5, ucd-snmp/memory_freebsd2)
 +config_arch_require(freebsd6, ucd-snmp/vmstat_freebsd2)
 +config_arch_require(freebsd6, ucd-snmp/memory_freebsd2)
++config_arch_require(freebsd7, ucd-snmp/vmstat_freebsd2)
++config_arch_require(freebsd7, ucd-snmp/memory_freebsd2)
  config_arch_require(netbsd1, ucd-snmp/vmstat_netbsd1)
  config_arch_require(netbsd1, ucd-snmp/memory_netbsd1)
- config_arch_require(openbsd2, ucd-snmp/vmstat_netbsd1)
+ config_arch_require(netbsd, ucd-snmp/vmstat_netbsd1)
