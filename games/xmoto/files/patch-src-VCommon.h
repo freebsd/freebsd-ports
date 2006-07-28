1a99c9e2ff023545d2515a351042bdcdcd7dd74d
Emanuel Haupt <ehaupt@FreeBSD.org>
2006-07-28 15:10:38 +0000

    Update to 0.2.0
    PR:		100979
    Submitted by:	Dmitry Marakasov <amdmi3@mail.ru> (maintainer)

--- games/xmoto/files/patch-src-VCommon.h
+++ games/xmoto/files/patch-src-VCommon.h
@@ -1,9 +1,9 @@
---- src/VCommon.h.orig	Sun Dec  4 01:53:03 2005
-+++ src/VCommon.h	Sun Dec  4 01:53:35 2005
-@@ -69,8 +69,7 @@
- 
+--- src/VCommon.h.orig	Thu Jul 20 21:51:42 2006
++++ src/VCommon.h	Fri Jul 28 15:59:16 2006
+@@ -62,8 +62,7 @@
  #include <string>
  #include <vector>
+ #include <queue>
 -#include <ostream>
 -#include <istream>
 +#include <iostream>
