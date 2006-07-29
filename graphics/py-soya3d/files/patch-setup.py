fdb3b005ec9a111d47cbf1ef62d9720653620cf8
acm <acm@FreeBSD.org>
2006-07-29 16:36:25 +0000

    - Update to 0.12 - Remove obsolete patch files. Now, i'm using ${REINPLACE_CMD} for it.
    Approved by:	garga (mentor)

--- graphics/py-soya3d/files/patch-setup.py
+++ graphics/py-soya3d/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py	Wed Jun 14 14:28:09 2006
-+++ setup.py	Wed Jun 14 14:30:13 2006
-@@ -27,19 +27,19 @@
+--- setup.py.orig	Tue Jul 18 15:14:24 2006
++++ setup.py	Thu Jul 27 20:21:36 2006
+@@ -32,21 +32,22 @@
  INCDIR = [
  	"ode-0.5/include",
  	"/usr/include",
@@ -10,28 +10,27 @@
 +	"%%X11BASE%%/include",
  	"/usr/include/freetype2",
 -	"/usr/local/include/freetype2",
--	"/usr/include/cal3d",
 +	"%%LOCALBASE%%/include/freetype2",
-+	"%%LOCALBASE%%/cal3d",
- 	"/usr/local/include/cal3d",
- 	"/sw/include", # For Mac OS X
+ 	"/usr/include/cal3d",
+-	"/usr/local/include/cal3d",
++	"%%LOCALBASE%%/include/SDL11",
++	"%%LOCALBASE%%/include/cal3d",
+ 	"/sw/include", # For Mac OS X "fink"
+     "/opt/local/include", # For Mac OS X "darwin port"
  	]
  LIBDIR = [
  	"ode-0.5/lib",
  	"/usr/lib",
 -	"/usr/local/lib",
--	"/usr/X11R6/lib",
 +	"%%LOCALBASE%%/lib",
+ 	"/opt/local/lib", # For Mac OS X "darwin port"
+-	"/usr/X11R6/lib",
 +	"%%X11BASE%%/lib",
  	"/sw/lib/", # For Mac OS X
  	]
  
-@@ -65,10 +65,10 @@
- from config import *
- 
- if sys.platform[:3] == "win":
--	LIBS = ["m", "glew32", "SDL", "SDL_mixer", "freetype", "cal3d", "stdc++"]
-+	LIBS = ["m", "glew32", "SDL-1.1", "SDL_mixer", "freetype", "cal3d", "stdc++"]
+@@ -80,7 +81,7 @@
+ 	LIBS = ["m", "glew32", "SDL", "freetype", "cal3d", "stdc++"]
  else:
  	#LIBS = ["m", "GLEW", "GL", "GLU", "SDL", "SDL_mixer", "freetype", "cal3d", "stdc++"]
 -	LIBS = ["m", "GLEW", "SDL", "freetype", "cal3d", "stdc++"]
@@ -39,7 +38,7 @@
  
  SOYA_PYREX_SOURCES  = ["_soya.pyx", "matrix.c", "chunk.c" ]
  SOYA_C_SOURCES      = ["_soya.c"  , "matrix.c", "chunk.c" ]
-@@ -99,14 +99,14 @@
+@@ -133,14 +134,14 @@
  			print "ODE and OPCODE have already been compiled; if you want to recompile them do:  cd %s ; make clean" % ODE_DIR
  		else:
  			print "Building ODE and OPCODE from %s" % ODE_DIR
@@ -51,7 +50,7 @@
 +			do("cd %s ; gmake" % ODE_DIR)
  			print "ODE and OPCODE built successfully !"
  			
- 	elif "sdist" in sys.argv:
+ 	elif SDISTING:
  		# Clean ODE, to remove configuration files and binaries
 -		do("cd %s ; make clean" % ODE_DIR)
 +		do("cd %s ; gmake clean" % ODE_DIR)
