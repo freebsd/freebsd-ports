94dad380df580bd1ee2ce5c7d1432d05691a069d
miwi <miwi@FreeBSD.org>
2006-07-29 19:34:03 +0000

    - Pass maintainership to submitter - Update to 2006.07.08
    PR:		ports/100809
    Submitted by:	Stanislav Sedov <ssedov(at)mbsd.msk.ru>
    Approved by:	krion (mentor)

---
+++ cad/gnucap/files/patch-modelgen_md.h
@@ -0,0 +1,11 @@
+--- modelgen/md.h.orig	Tue Jul 25 19:24:12 2006
++++ modelgen/md.h	Tue Jul 25 19:24:17 2006
+@@ -32,7 +32,7 @@
+ #include <cfloat>
+ #include <cmath>
+ #include <climits>
+-#include <limits>
++#include <limits.h>
+ #include <cstdio>
+ #include <cerrno>
+ #include <csetjmp>
