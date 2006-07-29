3988ed0e39e961f3e910d9ec8f3fec261ff8778b
Martin Wilke <miwi@FreeBSD.org>
2006-07-29 19:34:03 +0000

    - Pass maintainership to submitter - Update to 2006.07.08
    PR:		ports/100809
    Submitted by:	Stanislav Sedov <ssedov(at)mbsd.msk.ru>
    Approved by:	krion (mentor)

---
+++ cad/gnucap/files/patch-src_md.h
@@ -0,0 +1,11 @@
+--- src/md.h.orig	Tue Jul 25 19:24:01 2006
++++ src/md.h	Tue Jul 25 19:24:07 2006
+@@ -32,7 +32,7 @@
+ #include <cfloat>
+ #include <cmath>
+ #include <climits>
+-#include <limits>
++#include <limits.h>
+ #include <cstdio>
+ #include <cerrno>
+ #include <csetjmp>
