05abfcafad41b97e180315d73f2c39d414db80e7
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2006-08-01 10:59:41 +0000

    - Update to 16b
    Approved by:	garga (mentor)

--- emulators/xgngeo/files/patch-data-py_configfile.py
+++ emulators/xgngeo/files/patch-data-py_configfile.py
@@ -1,32 +1,11 @@
---- data/py/configfile.py	Thu Jul 14 13:48:58 2005
-+++ data/py/configfile.py	Fri Apr 14 01:25:46 2006
-@@ -33,7 +33,7 @@
- as the `gngeorc'."""
- 
- 	def __init__(self):
--		self.xgngeoconfPath = "data/xgngeo.conf" #Path to XGngeo config file.
-+		self.xgngeoconfPath =  os.path.join(os.getenv("HOME"),".gngeo/xgngeo.conf") #Path to XGngeo config file.
- 		self.gngeoDir = os.path.expanduser("~/.gngeo") #Gngeo local configuration directory.
- 		self.gngeorcPath = os.path.join(self.gngeoDir,"gngeorc") #Path to Gngeo config file.
- 
-@@ -42,9 +42,9 @@
- 		and XGngeo configuration files."""
- 		return { #Gngeo default.
- 			#Path
+--- data/py/configfile.py	Fri Jul 14 07:30:15 2006
++++ data/py/configfile.py	Tue Aug  1 00:01:53 2006
+@@ -90,7 +90,7 @@
+ 			"68kclock":0,
+ 			"z80clock":0,
+ 			#Other thing section.
 -			"libglpath":"/usr/lib/libGL.so",
 +			"libglpath":"%%X11BASE%%/lib/libGL.so",
- 			"rompath": os.path.join(os.getenv("HOME"),"..."),
--			"romrc":"/usr/local/share/gngeo/romrc",
-+			"romrc":"%%X11BASE%%/share/gngeo/romrc",
- 			#Graphic
- 			"blitter":"soft",
- 			"effect":"none",
-@@ -74,7 +74,7 @@
- 			"previewimages":"false",
- 			"previewimagedir":"data/previewimages/",
- 			"rominfos":"true",
--			"rominfoxml":"data/rominfos.xml",
-+			"rominfoxml":"%%DATADIR%%/rominfos.xml",
- 			"showavailableromsonly":"true"
- 			}
- 
+ 			"sleepidle":"true",
+ 			"bench":"false",
+ 			},{ #XGngeo default.
