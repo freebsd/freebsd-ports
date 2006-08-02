0ae474e559eebe156fcd3fa22687e912fcc400b4
mich <mich@FreeBSD.org>
2006-08-02 13:01:59 +0000

    - update to 0.8.5
    PR:		ports/97940
    Submitted by:	Eric Kjeldergaard <kjelderg@gmail.com>

--- net/kiax/files/patch-lib_libiax2_src_iax.c
+++ net/kiax/files/patch-lib_libiax2_src_iax.c
@@ -1,31 +1,27 @@
---- lib/libiax2/src/iax.c.orig	Sun Nov 13 13:59:36 2005
-+++ lib/libiax2/src/iax.c	Sun Nov 13 14:21:46 2005
-@@ -53,11 +53,13 @@
+--- lib/libiax2/src/iax.c.orig	Thu May 25 18:42:18 2006
++++ lib/libiax2/src/iax.c	Thu May 25 18:44:26 2006
+@@ -59,14 +59,12 @@ void gettimeofday(struct timeval *tv, vo
+ #include <arpa/inet.h>
  #include <time.h>
  
- #ifndef MACOSX
-+#ifndef __FreeBSD__
+-#ifndef MACOSX
++#if !defined(MACOSX) && !defined(__FreeBSD__)
  #include <malloc.h>
  #ifndef SOLARIS
+-#if !defined(__NetBSD__) && !defined(__FreeBSD__)
  #include <error.h>
  #endif
  #endif
-+#endif
+-#endif
+  
+  #endif
  
- #endif
- 
-@@ -85,10 +87,14 @@
- #ifdef MACOSX
+@@ -97,7 +95,7 @@ void gettimeofday(struct timeval *tv, vo
+ #ifdef SOLARIS
  #define IAX_SOCKOPTS MSG_DONTWAIT
  #else
-+#ifdef __FreeBSD__
-+#define IAX_SOCKOPTS MSG_DONTWAIT
-+#else
- #ifdef SOLARIS
+-#ifdef __NetBSD__
++#if defined(__NetBSD__) || defined(__FreeBSD__)
  #define IAX_SOCKOPTS MSG_DONTWAIT
  #else  /* Linux and others */
  #define IAX_SOCKOPTS MSG_DONTWAIT | MSG_NOSIGNAL
-+#endif
- #endif
- #endif
- #endif
