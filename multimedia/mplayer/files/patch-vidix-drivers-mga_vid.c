b008ba91a1b2c00b9268560ab1ac23780bd813b2
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2006-08-04 18:19:49 +0000

    Apply some user-requested changes to the mplayer port: o Have mga xvidix driver autodetect memory, reported by Andre Albsmeier o Remove PKGSUFFIX stuff, instead port name is always "mplayer" o Use win32 real player codecs if the user prohibits linux-realplayer interaction
    PR:		ports/101224
    Submitted by:	Thomas E. Zander (maintainer)

--- multimedia/mplayer/files/patch-vidix-drivers-mga_vid.c
+++ multimedia/mplayer/files/patch-vidix-drivers-mga_vid.c
@@ -1,5 +1,14 @@
---- vidix/drivers/mga_vid.c.orig	Thu Sep  2 13:34:25 2004
-+++ vidix/drivers/mga_vid.c	Tue Apr 12 17:51:55 2005
+--- vidix/drivers/mga_vid.c.orig	Sun Jun 11 20:35:43 2006
++++ vidix/drivers/mga_vid.c	Mon Jul 31 22:48:55 2006
+@@ -43,7 +43,7 @@
+ 
+ #define MGA_VSYNC_POS 2
+ 
+-#undef MGA_PCICONFIG_MEMDETECT
++#define MGA_PCICONFIG_MEMDETECT
+ 
+ #define MGA_DEFAULT_FRAMES 4
+ 
 @@ -1218,8 +1218,7 @@
  	    {
  		if ((lst[i].command & PCI_COMMAND_IO) == 0)
