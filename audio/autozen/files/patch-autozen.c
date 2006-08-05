861950440005d51674365839562116816528f41f
Martin Wilke <miwi@FreeBSD.org>
2006-08-05 08:05:36 +0000

    - Update to 2.1 - Pass maintainer to submitter
    PR:		ports/101049
    Submitted by:	Stanislav Sedov <ssedov(at)mbsd.msk.ru>
    Approved by:	krion (mentor)

---
+++ audio/autozen/files/patch-autozen.c
@@ -0,0 +1,11 @@
+--- autozen.c.orig	Sun Jul 30 15:29:57 2006
++++ autozen.c	Sun Jul 30 15:30:05 2006
+@@ -113,7 +113,7 @@
+ //
+ /////////////////////////////////
+ 
+-#define SAMPLE_RATE 8000
++#define SAMPLE_RATE 11025
+ 
+ #define MAX_HARMONICS 10
+ #define DEFAULT_HARMONICS 3
