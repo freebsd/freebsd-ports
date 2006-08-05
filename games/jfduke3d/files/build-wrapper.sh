21f7d3fb407e001a6d6be7ec93b336d05cf3cd19
Alejandro Pulver <alepulver@FreeBSD.org>
2006-08-05 16:12:20 +0000

    - Update to version 20051009. - Add patch for sound and joystick support taken from icculus.org/duke3d. - Integrate with games/duke3d-data. - Remove wrapper scripts (use programs' search path routines). - Remove CONFLICTS with nonexistent (removed) port games/duke3d. - Move notes in files/pkg-message.in to pkg-descr.
    PR:		ports/101279
    Submitted by:	alepulver (myself)
    Approved by:	maintainer

--- games/jfduke3d/files/build-wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Needed to make symlinks/shortcuts work.
-# Wrap build (editor) binary
-cd %%DUKEDIR%%
-./build $*
-exit $?
--- games/jfduke3d/files/wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Needed to make symlinks/shortcuts work.
-# Wrap duke3d binary
-cd %%DUKEDIR%%
-./duke3d $*
-exit $?
