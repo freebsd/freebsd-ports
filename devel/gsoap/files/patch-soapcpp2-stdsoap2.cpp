09391edc3b9d43e267d4705d3081a855de0884a5
Sergey Matveychuk <sem@FreeBSD.org>
2006-08-05 08:31:04 +0000

    - FD_SETSIZE is unsigned in FreeBSD. It caused problem when it's compared   with failed socket descriptor (-1).
    PR:		ports/101352
    Submitted by:	Simun Mikecin <numisemis@yahoo.com>

--- devel/gsoap/files/patch-soapcpp2-stdsoap2.cpp
+++ devel/gsoap/files/patch-soapcpp2-stdsoap2.cpp
@@ -1,6 +1,6 @@
---- soapcpp2/stdsoap2.cpp.orig	Sat May 20 00:33:35 2006
-+++ soapcpp2/stdsoap2.cpp	Sat May 20 00:58:56 2006
-@@ -3251,7 +3251,7 @@
+--- soapcpp2/stdsoap2.cpp.orig	Mon Jun 26 23:36:38 2006
++++ soapcpp2/stdsoap2.cpp	Sat Aug  5 12:18:51 2006
+@@ -3282,7 +3282,7 @@
      soap->errnum = h_errno;
    }
  #elif defined(HAVE_GETHOSTBYNAME_R)
@@ -9,3 +9,12 @@
  #elif defined(VXWORKS)
    /* If the DNS resolver library resolvLib has been configured in the vxWorks
     * image, a query for the host IP address is sent to the DNS server, if the
+@@ -3755,7 +3755,7 @@
+             X509V3_EXT_METHOD *meth = X509V3_EXT_get(ext);
+             if (!meth)
+               break;
+-            val = meth->i2v(meth, meth->d2i(NULL, (unsigned char**)&ext->value->data, ext->value->length), NULL);
++            val = meth->i2v(meth, meth->d2i(NULL, (const unsigned char**)&ext->value->data, ext->value->length), NULL);
+             for (j = 0; j < sk_CONF_VALUE_num(val); j++)
+             { CONF_VALUE *nval = sk_CONF_VALUE_value(val, j);
+               if (nval && !strcmp(nval->name, "DNS") && !strcmp(nval->value, host))
