3c44e41d2b88d689a8c0dd4d255a9a8c251d80a7
pav <pav@FreeBSD.org>
2006-08-06 19:43:47 +0000

    - Update to 5.0.24
    Approved by:	ale's vacation

--- databases/mysql50-server/files/patch-sql::mysqld.cc
+++ databases/mysql50-server/files/patch-sql::mysqld.cc
@@ -1,14 +1,5 @@
 --- sql/mysqld.cc.orig	Sat Aug  6 03:39:41 2005
 +++ sql/mysqld.cc	Sun Aug 28 20:41:38 2005
-@@ -121,7 +121,7 @@
- #endif /* __WIN__ */
- 
- #ifdef HAVE_LIBWRAP
--#include <tcpd.h>
-+#include "mytcpd.h"
- #include <syslog.h>
- #ifdef NEED_SYS_SYSLOG_H
- #include <sys/syslog.h>
 @@ -3177,7 +3177,7 @@
    init_ssl();
  
