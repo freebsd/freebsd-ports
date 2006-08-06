a1957c18862380d6ae6e86f1b1cdfbad7cd7949e
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2006-08-06 09:32:49 +0000

    Update to 6.6 to compleate the repo-copy
    PR:		ports/100162
    Submitted by:	Babak Farrokhi (maintainer)
    Repocopy by:	marcus

--- www/awstats-devel/files/patch-tools-awstats_updateall.pl
+++ www/awstats-devel/files/patch-tools-awstats_updateall.pl
@@ -1,13 +1,13 @@
---- tools/awstats_updateall.pl.orig	Mon Jun  7 08:27:26 2004
-+++ tools/awstats_updateall.pl	Thu Sep 22 01:30:15 2005
-@@ -121,8 +121,8 @@
+--- tools/awstats_updateall.pl.orig	Thu May 25 03:59:24 2006
++++ tools/awstats_updateall.pl	Sun Jul 16 01:14:54 2006
+@@ -128,8 +128,8 @@
  	# Check if AWSTATS prog is found
  	my $AwstatsFound=0;
  	if (-s "$Awstats") { $AwstatsFound=1; }
 -	elsif (-s "/usr/local/awstats/wwwroot/cgi-bin/awstats.pl") {
 -		$Awstats="/usr/local/awstats/wwwroot/cgi-bin/awstats.pl";
-+	elsif (-s "%%PREFIX%%/www/awstats/cgi-bin/awstats.pl") {
-+		$Awstats="%%PREFIX%%/www/awstats/cgi-bin/awstats.pl";
++	elsif (-s "%%PREFIX%%/awstats/wwwroot/cgi-bin/awstats.pl") {
++		$Awstats="%%PREFIX%%/awstats/wwwroot/cgi-bin/awstats.pl";
  		$AwstatsFound=1;
  	}
  	if (! $AwstatsFound) {
