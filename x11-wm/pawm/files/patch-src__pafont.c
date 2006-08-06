216afac79f87baa2dbe90f8b40413f2fca626ae1
Martin Wilke <miwi@FreeBSD.org>
2006-08-06 08:33:45 +0000

    - Update to 2.2.7
    PR:		ports/101197
    Submitted by:	KATO Tsuguru <tkato432(at)yahoo.com>
    Approved by:	markus (co mentor)

--- x11-wm/pawm/files/patch-src__pafont.c
+++ x11-wm/pawm/files/patch-src__pafont.c
@@ -1,5 +1,5 @@
---- src/pafont.c.orig	Thu Jun 29 20:30:15 2006
-+++ src/pafont.c	Fri Jun 30 23:49:19 2006
+--- src/pafont.c.orig	Tue Jul 25 05:49:39 2006
++++ src/pafont.c	Fri Jul 28 16:28:48 2006
 @@ -44,7 +44,7 @@
  }
  
@@ -9,3 +9,21 @@
  {
  	PAFont xlfdfont;
  	char fontname[20];
+@@ -86,7 +86,7 @@
+ {
+ 	ASSERT(xlfdfont);
+ 
+-	XSetForeground(xlfdfont->display, xgc, xfldfont->color);
++	XSetForeground(xlfdfont->display, xgc, xlfdfont->color);
+ 	/* X and Y coordinates mark the top left corner of the text box,
+ 	 * so we need to sum the ascent value to y to make the shitty old
+ 	 * X font system draw the text at the right place */
+@@ -149,7 +149,7 @@
+ 	XColor ecolor, scolor;
+ 
+ 	XAllocNamedColor(xlfdfont->display, COLORMAP, name, &scolor, &ecolor);
+-	xfldfont->color=scolor.pixel;
++	xlfdfont->color=scolor.pixel;
+ }
+ 
+ #else
