b52d7dc34c3c0e7bdd24a860306a68cd1df8fa8a
Tilman Keskinoz <arved@FreeBSD.org>
2006-08-06 22:05:59 +0000

    Update to 1.4.0
    PR:		100101
    Submitted by:	chinsan <chinsan.tw@gmail.com>
    Approved by:	maintainer timeout (3.5 weeks)

--- x11/fbdesk/files/patch-src-FbTk-FbPixmap.cc
+++ x11/fbdesk/files/patch-src-FbTk-FbPixmap.cc
@@ -1,10 +1,10 @@
---- src/FbTk/FbPixmap.cc.orig	Mon Sep 20 12:26:19 2004
-+++ src/FbTk/FbPixmap.cc	Fri Oct 15 18:08:16 2004
-@@ -28,6 +28,7 @@
+--- src/FbTk/FbPixmap.cc.orig	Wed Jul 12 02:27:17 2006
++++ src/FbTk/FbPixmap.cc	Wed Jul 12 02:27:47 2006
+@@ -29,6 +29,7 @@
  #include <X11/Xutil.h>
  #include <X11/Xatom.h>
  #include <iostream>
 +#include <stdio.h>
- using namespace std;
+ #include <string>
  
- namespace FbTk {
+ using namespace std;
