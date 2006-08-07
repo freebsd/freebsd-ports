e4ceadae5397bbfb4b12424e54841c336f4ae20b
miwi <miwi@FreeBSD.org>
2006-08-07 09:02:45 +0000

    - Fix build on 4.x
    Submitted by:	pointyhat via kris
    Approved by:	krion (mentor)

---
+++ archivers/advancecomp/files/patch-Portable.h
@@ -0,0 +1,11 @@
+--- 7z/Portable.h.org	Mon Aug  7 10:54:35 2006
++++ 7z/Portable.h	Mon Aug  7 10:54:55 2006
+@@ -2,7 +2,7 @@
+ #define __PORTABLE_H
+ 
+ #include <string.h>
+-#include <stdint.h>
++#include <inttypes.h>
+ 
+ typedef signed char INT8;
+ typedef unsigned char UINT8;
