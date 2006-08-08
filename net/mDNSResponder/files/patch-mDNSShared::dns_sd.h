b61fa3d0ca509f8f31cf054315ce176f1e24291d
itetcu <itetcu@FreeBSD.org>
2006-08-08 14:29:19 +0000

    Fix patching.

--- net/mDNSResponder/files/patch-mDNSShared::dns_sd.h
+++ net/mDNSResponder/files/patch-mDNSShared::dns_sd.h
@@ -1,10 +1,10 @@
---- mDNSShared/dns_sd.h.orig	Tue Dec 20 22:24:58 2005
-+++ mDNSShared/dns_sd.h	Fri Mar 10 14:41:32 2006
-@@ -42,6 +42,7 @@
+--- ./mDNSShared/dns_sd.h.orig	Tue Aug  8 17:27:30 2006
++++ ./mDNSShared/dns_sd.h	Tue Aug  8 17:27:52 2006
+@@ -39,6 +39,7 @@
+ #define DNSSD_API
  #endif
  
- /* stdint.h does not exist on FreeBSD 4.x; its types are defined in sys/types.h instead */
 +#include <osreldate.h>
  #if defined(__FreeBSD_version) && (__FreeBSD_version < 500000)
+ /* stdint.h does not exist on FreeBSD 4.x; its types are defined in sys/types.h instead */
  #include <sys/types.h>
- 
