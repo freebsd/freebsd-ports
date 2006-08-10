cd5b22ddfb3d9310ac2320f21b06fe84cc7a0306
simon <simon@FreeBSD.org>
2006-08-10 20:33:38 +0000

    Fix compile with OpenSSL 0.9.8b from -CURRENT.
    Problem reported by:	kris

--- security/fressh/files/patch-crypto::ssh_crypto_openssl.c
+++ security/fressh/files/patch-crypto::ssh_crypto_openssl.c
@@ -1,13 +1,16 @@
 
 $FreeBSD$
 
---- crypto/ssh_crypto_openssl.c	2001/04/18 16:00:03	1.1
-+++ crypto/ssh_crypto_openssl.c	2001/04/18 16:00:24
-@@ -44,6 +44,7 @@
+--- crypto/ssh_crypto_openssl.c.orig
++++ crypto/ssh_crypto_openssl.c
+@@ -44,8 +44,10 @@
  #include <stdlib.h>
  #include <errno.h>
  #include <string.h>
 +#include <sys/types.h>
  
  #include <openssl/rand.h>
++#include <openssl/sha.h>
+ 
+ #include <openssl/opensslv.h>
  
