ef1d007c6ada8bbd1fc390daa35cb6a219b52ec3
simon <simon@FreeBSD.org>
2006-08-13 15:34:46 +0000

    Fix compile with OpenSSL 0.9.8b from -CURRENT.
    Note the fix wrt. OPENSSL_VERSION_NUMBER isn't optimal, but the best way
    since base system versions of OpenSSL 0.9.7 don't define OPENSSL_THREADS
    even though threads should be supported.
    
    Problem reported by:	pointyhat via kris
    Approved by:		anordby (maintainer)

--- www/pound/files/patch-pound.h
+++ www/pound/files/patch-pound.h
@@ -1,13 +1,15 @@
---- pound.h.orig	Wed Jul 23 04:48:13 2003
-+++ pound.h	Wed Jul 23 04:48:49 2003
-@@ -163,10 +163,6 @@
+
+$FreeBSD$
+
+--- pound.h.orig
++++ pound.h
+@@ -178,7 +178,8 @@
+ #if HAVE_OPENSSL_SSL_H
  #define OPENSSL_THREAD_DEFINES
  #include    <openssl/ssl.h>
- #if OPENSSL_VERSION_NUMBER >= 0x00907000L
--#ifndef OPENSSL_THREADS
--#error  "Pound requires OpenSSL with thread support"
--#endif
--#else
- #ifndef THREADS
+-#if OPENSSL_VERSION_NUMBER >= 0x00907000L
++#include    <openssl/opensslconf.h>
++#if OPENSSL_VERSION_NUMBER > 0x0090705FL
+ #ifndef OPENSSL_THREADS
  #error  "Pound requires OpenSSL with thread support"
  #endif
