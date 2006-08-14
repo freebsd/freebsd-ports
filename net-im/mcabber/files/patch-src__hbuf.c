e6465687420cfc54246ebb12f0fe00e9c25e052e
sat <sat@FreeBSD.org>
2006-08-14 06:22:02 +0000

    - Update to 0.8.0 - Complete UTF-8 support - Work around the no-stream-header bug [1]
    PR:		ports/101335 [1]
    Submitted by:	thompsa [1]

--- net-im/mcabber/files/patch-src__hbuf.c
+++ net-im/mcabber/files/patch-src__hbuf.c
@@ -1,6 +1,6 @@
---- ./src/hbuf.c.orig	Thu Jun 22 10:30:34 2006
-+++ ./src/hbuf.c	Thu Jun 22 10:31:06 2006
-@@ -253,6 +253,7 @@
+--- ./src/hbuf.c.orig	Sun Aug 13 23:01:59 2006
++++ ./src/hbuf.c	Mon Aug 14 10:15:38 2006
+@@ -252,6 +252,7 @@
    hbuf_block *blk;
    guchar last_persist_prefixflags = 0;
    GList *last_persist;  // last persistent flags
@@ -8,7 +8,7 @@
    hbb_line **array, **array_elt;
  
    // To be able to correctly highlight multi-line messages,
-@@ -273,7 +274,6 @@
+@@ -272,7 +273,6 @@
  
    for (i = 0 ; i < n ; i++) {
      if (hbuf) {
