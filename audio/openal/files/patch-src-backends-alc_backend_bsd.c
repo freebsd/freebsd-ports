3200099820bf30d9432a2ce0d68de1258885a4c9
Jean-Yves Lefort <jylefort@FreeBSD.org>
2006-08-16 13:27:39 +0000

    Allow the user to customize the audio buffer size.
    PR:		ports/101960
    Submitted by:	jylefort
    Approved by:	maintainer

--- audio/openal/files/patch-src-backends-alc_backend_bsd.c
+++ audio/openal/files/patch-src-backends-alc_backend_bsd.c
@@ -1,5 +1,5 @@
---- src/backends/alc_backend_bsd.c.orig	Sat Apr 22 12:25:15 2006
-+++ src/backends/alc_backend_bsd.c	Sat Apr 22 12:26:15 2006
+--- src/backends/alc_backend_bsd.c.orig	Thu Jan  5 16:11:20 2006
++++ src/backends/alc_backend_bsd.c	Sun Aug 13 07:39:16 2006
 @@ -9,7 +9,10 @@
   */
  #include "al_siteconfig.h"
@@ -11,49 +11,55 @@
  #include <assert.h>
  #include <fcntl.h>
  #include <sys/soundcard.h>
-@@ -113,7 +114,6 @@
+@@ -113,7 +116,6 @@
   */
  static void *grab_write_native(void) {
-	const char *dsppath = "/dev/dsp";
+ 	const char *dsppath = "/dev/dsp";
 -	int divisor = _alSpot(_AL_DEF_BUFSIZ) | (2<<16);
-
-	dsp_fd = open(dsppath, O_WRONLY | O_NONBLOCK);
-
-@@ -126,10 +126,6 @@
-		perror("fcntl");
-	}
-
+ 
+ 	dsp_fd = open(dsppath, O_WRONLY | O_NONBLOCK);
+ 
+@@ -126,10 +128,6 @@
+ 		perror("fcntl");
+ 	}
+ 
 -	if(ioctl(dsp_fd, SNDCTL_DSP_SETFRAGMENT, &divisor) < 0) {
 -		perror("ioctl SETFRAGMENT");
 -	}
 -
-	FD_ZERO(&dsp_fd_set);
-	FD_SET(dsp_fd, &dsp_fd_set);
-
-@@ -318,10 +314,11 @@
+ 	FD_ZERO(&dsp_fd_set);
+ 	FD_SET(dsp_fd, &dsp_fd_set);
+ 
+@@ -318,10 +316,12 @@
  }
-
+ 
  static ALboolean set_write_native(UNUSED(void *handle),
 -				  UNUSED(unsigned int *bufsiz),
 +				  unsigned int *bufsiz,
-				  ALenum *fmt,
-				  unsigned int *speed) {
-	ALuint channels = _alGetChannelsFromFormat(*fmt);
+ 				  ALenum *fmt,
+ 				  unsigned int *speed) {
+ 	ALuint channels = _alGetChannelsFromFormat(*fmt);
 +	int divisor;
-
-	if(dsp_fd < 0) {
-		return AL_FALSE;
-@@ -363,6 +360,23 @@
-
-
-	*fmt = BSD2ALFMT(*fmt, channels);
++	char *custom_bufsiz;
+ 
+ 	if(dsp_fd < 0) {
+ 		return AL_FALSE;
+@@ -363,6 +363,28 @@
+ 
+ 
+ 	*fmt = BSD2ALFMT(*fmt, channels);
 +
-+	if(ioctl(dsp_fd, SNDCTL_DSP_GETBLKSIZE, bufsiz) < 0) {
++	custom_bufsiz = getenv("OPENAL_BUFFER_SIZE");
++	if (custom_bufsiz != NULL)
++		*bufsiz = atoi(custom_bufsiz);
++	else {
++		if(ioctl(dsp_fd, SNDCTL_DSP_GETBLKSIZE, bufsiz) < 0) {
 +#ifdef DEBUG_MAXIMUS
-+		fprintf(stderr, "blksize %d\n", *bufsiz);
-+		perror("get_devsp blksize ioctl");
++			fprintf(stderr, "blksize %d\n", *bufsiz);
++			perror("get_devsp blksize ioctl");
 +#endif
-+		return AL_FALSE;
++			return AL_FALSE;
++		}
 +	}
 +
 +	divisor = _alSpot(*bufsiz) | (2<<16);
@@ -64,6 +70,6 @@
 +#endif
 +		return AL_FALSE;
 +	}
-
-	return AL_TRUE;
+ 
+ 	return AL_TRUE;
  }
