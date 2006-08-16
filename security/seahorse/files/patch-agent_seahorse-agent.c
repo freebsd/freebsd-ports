2471c3a638d9c55dde3edf60725e17d7c43dd98e
marcus <marcus@FreeBSD.org>
2006-08-16 06:23:18 +0000

    Update to 0.8.2.

--- security/seahorse/files/patch-agent_seahorse-agent.c
+++ security/seahorse/files/patch-agent_seahorse-agent.c
@@ -1,14 +1,14 @@
---- agent/seahorse-agent.c.orig	Thu May 19 00:15:36 2005
-+++ agent/seahorse-agent.c	Thu Jul 28 01:32:39 2005
-@@ -273,18 +273,18 @@ main (int argc, char **argv)
-     const char *sockname;
- 	GnomeProgram *program = NULL;
- 	
+--- agent/seahorse-agent.c.orig	Sun Aug 13 14:50:35 2006
++++ agent/seahorse-agent.c	Wed Aug 16 02:08:05 2006
+@@ -280,7 +280,6 @@ main (int argc, char **argv)
+     GnomeProgram *program = NULL;
+     GnomeClient *client = NULL;
+     
 -    secmem_init (65536);
--
+ 
      /* We need to drop privileges completely for security */
  #if defined(HAVE_SETRESUID) && defined(HAVE_SETRESGID)
- 
+@@ -288,10 +287,12 @@ main (int argc, char **argv)
      /* Not in header files for all OSs, even where present */
      int setresuid(uid_t ruid, uid_t euid, uid_t suid);
      int setresgid(gid_t rgid, gid_t egid, gid_t sgid);
@@ -21,7 +21,7 @@
      if (setuid (getuid ()) == -1 || setgid (getgid ()) == -1)
  #endif
          err (1, _("couldn't drop privileges properly"));
-@@ -301,7 +301,6 @@ main (int argc, char **argv)
+@@ -313,7 +314,6 @@ main (int argc, char **argv)
       * All functions after this point have to print messages 
       * nicely and not just called exit() 
       */
@@ -29,7 +29,7 @@
  
      /* Handle some signals */
      signal (SIGINT, on_quit);
-@@ -314,6 +313,8 @@ main (int argc, char **argv)
+@@ -329,6 +329,8 @@ main (int argc, char **argv)
      /* Initialize our sub systems */
      seahorse_agent_actions_init ();
      seahorse_agent_cache_init ();
