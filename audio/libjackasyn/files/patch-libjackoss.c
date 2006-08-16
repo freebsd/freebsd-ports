9a92101b30cfe636404be94a9cbc532dd2c98369
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2006-08-16 10:37:03 +0000

    - Update to 0.13 - Remove broken on 4.x
    Approved by:	garga (mentor)

--- audio/libjackasyn/files/patch-libjackoss.c
+++ audio/libjackasyn/files/patch-libjackoss.c
@@ -1,6 +1,17 @@
---- libjackoss.c	Mon Mar  6 07:19:23 2006
-+++ libjackoss.c	Mon Mar  6 12:25:46 2006
-@@ -117,7 +117,7 @@
+--- libjackoss.c	Mon Mar  6 08:17:57 2006
++++ libjackoss.c	Tue Aug 15 17:36:31 2006
+@@ -32,7 +32,9 @@
+ #define REAL_LIBC ((void *) -1L)
+ #endif
+ 
+-
++#if defined (__FreeBSD__)
++#define O_LARGEFILE 0
++#endif
+ 
+ #if 1
+ int (*_select)(int n, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
+@@ -119,7 +121,7 @@
    }
  
    va_start (args, flags);
@@ -9,7 +20,16 @@
    va_end (args);
  
    if ((strncmp(name,"/dev/dsp",8) && strncmp(name,"/dev/audio",8))
-@@ -367,10 +367,10 @@
+@@ -198,7 +200,7 @@
+         DEBUG(fprintf(stderr,"%s\n",__FUNCTION__));
+         if (oflag & O_CREAT) {
+                 va_start(args, oflag);
+-                mode = va_arg(args, mode_t);
++                mode = va_arg(args, int);
+                 va_end(args);
+         }
+         return jackoss_open(file, oflag | O_LARGEFILE, mode);
+@@ -386,10 +388,10 @@
      UNIMPLEMENTED( SNDCTL_DSP_MAPOUTBUF);
      UNIMPLEMENTED( SNDCTL_DSP_SETSYNCRO);
      UNIMPLEMENTED( SNDCTL_DSP_SETDUPLEX);
