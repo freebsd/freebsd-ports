366f49bd481c60d0d8a923c1cc84adc7a7c56d41
dinoex <dinoex@FreeBSD.org>
2006-08-17 04:59:00 +0000

    the autoconfig-version of lha maintained as seperate project
    Suggested by:	Shigekazu Kimura

---
+++ archivers/lha-ac/files/patch-lha.h
@@ -0,0 +1,10 @@
+--- src/lha.h.orig	Sun Sep 21 15:58:52 2003
++++ src/lha.h	Thu Sep 23 07:10:33 2004
+@@ -16,6 +16,7 @@
+ #endif
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <errno.h>
+ #include <ctype.h>
+ #include <sys/types.h>
