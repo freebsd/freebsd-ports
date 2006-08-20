96add768b87264137da2daf4270c11abac0a2fd4
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2006-08-20 15:11:09 +0000

    - Integrate games/warsow with games/warsow-data - Fixes some problems with games/linux-warsow and games/warsow-data, now   we can install games/warsow and games/linux-warsow at the same place.
    Submitted by:	me
    Approved by:	maintainer timeout (16 days), garga (mentor)

--- games/warsow/files/patch-linux+sys_linux.c
+++ games/warsow/files/patch-linux+sys_linux.c
@@ -1,29 +1,20 @@
---- linux/sys_linux.c.orig	Thu May 11 22:18:54 2006
-+++ linux/sys_linux.c	Thu Jun  1 15:44:44 2006
-@@ -434,7 +392,7 @@
- 			APIfunc = GetGameAPI;
+--- linux/sys_linux.c	Sat Aug 19 21:04:32 2006
++++ linux/sys_linux.c	Sat Aug 19 21:04:55 2006
+@@ -380,7 +380,7 @@
+ 
+ #if defined __FreeBSD__
+ #if defined __i386__
+-#define ARCH "freebsd_i386"
++#define ARCH "i386"
+ 
+ #ifdef NDEBUG
+ 	const char *debugdir = "freebsd_releasei386";
+@@ -389,7 +389,7 @@
  #endif
- 			lib = &game_library;
--			libname = "game_" ARCH ".so";
-+			libname = "game.so";
- 			apifuncname = "GetGameAPI";
- 			break;
- 		}
-@@ -445,7 +403,7 @@
- 			APIfunc = GetCGameAPI;
- #endif
- 			lib = &cgame_library;
--			libname = "cgame_" ARCH ".so";
-+			libname = "cgame.so";
- 			apifuncname = "GetCGameAPI";
- 			break;
- 		}
-@@ -456,7 +414,7 @@
- 			APIfunc = GetUIAPI;
- #endif
- 			lib = &ui_library;
--			libname = "ui_" ARCH ".so";
-+			libname = "ui.so";
- 			apifuncname = "GetUIAPI";
- 			break;
- 		}
+ 
+ #elif defined __x86_64__
+-#define ARCH "xfreebsd_86_64"
++#define ARCH "x86_64"
+ #ifdef NDEBUG
+ 	const char *debugdir = "releasex86_64";
+ #else
