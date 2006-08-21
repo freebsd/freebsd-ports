0977976ddedb384080ee47a64ff1269a4f793e19
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2006-08-21 16:07:03 +0000

    - Fix build problem, it isn't linking appropriately to openal lib. - Add patch_py-openal.c. This port is using DEPRECATED functions of openal.
    Approved by:	garga (mentor)

--- audio/py-openal/files/patch-setup.py
+++ audio/py-openal/files/patch-setup.py
@@ -1,27 +1,29 @@
 --- setup.py	Sun Apr 30 10:23:44 2006
-+++ setup.py	Wed May 10 09:25:22 2006
-@@ -1,13 +1,13 @@
++++ setup.py	Mon Aug 21 00:25:58 2006
+@@ -1,14 +1,14 @@
  import os
  from distutils.core import setup, Extension
  
 -LIBDIRS = ["/usr/lib", "/usr/local/lib"]
-+LIBDIRS = ["/usr/lib", "/usr/local/lib", "%%PREFIX%%/lib"]
++LIBDIRS = ["/usr/lib", "/usr/local/lib", "/usr/local/lib"]
  LIBS    = ["openal", "alut"]
  
  try:
    openal_version = os.popen("openal-config --version").read()
    if openal_version[0] == "0":
 -    LIBDIRS = ["/usr/lib", "/usr/local/lib"]
-+    LIBDIRS = ["/usr/lib", "/usr/local/lib", "%%PREFIX%%/lib"]
-     LIBS    = ["openal"]
+-    LIBS    = ["openal"]
++    LIBDIRS = ["/usr/lib", "/usr/local/lib", "/usr/local/lib"]
++    LIBS    = ["openal","alut"]
      
  except:
+   print "Warning: error while running 'openal-config --version', using default values..."
 @@ -38,7 +38,7 @@
    
    ext_modules  = [Extension("_openal",
                              ["py_openal.c"],
 -                            ["/usr/include", "/usr/local/include"],
-+                            ["/usr/include", "/usr/local/include", "%%PREFIX%%/include"],
++                            ["/usr/include", "/usr/local/include", "/usr/local/include"],
                              [("HAVE_CONFIG_H", None)],
                              None,
                              LIBDIRS,
