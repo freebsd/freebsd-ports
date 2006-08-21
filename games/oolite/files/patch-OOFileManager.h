7ec9254833e76bf6967c4d8b824505101eb7a1bd
rafan <rafan@FreeBSD.org>
2006-08-21 15:21:15 +0000

    - Update to 1.65
    PR:		ports/100857
    Submitted by:	Denis Shaposhnikov <dsh at vlink.ru>
    Approved by:	Andriy Gapon <avg at icyb.net.ua> (maintainer)

--- games/oolite/files/patch-OOFileManager.h
+++ games/oolite/files/patch-OOFileManager.h
@@ -1,5 +1,5 @@
---- OOFileManager.h.orig	Mon Oct 17 16:42:31 2005
-+++ OOFileManager.h	Mon Oct 17 16:43:14 2005
+--- src/Core/OOFileManager.h.orig	Mon Oct 17 16:42:31 2005
++++ src/Core/OOFileManager.h	Mon Oct 17 16:43:14 2005
 @@ -16,7 +16,7 @@
  //
  #import <Foundation/Foundation.h>
--- games/oolite/files/patch-main.m
+++ games/oolite/files/patch-main.m
@@ -1,14 +1,20 @@
---- main.m.orig	Fri Oct 14 00:32:51 2005
-+++ main.m	Wed Oct 19 14:08:21 2005
-@@ -23,6 +23,11 @@
+--- src/SDL/main.m.orig	Tue Jul 18 21:34:19 2006
++++ src/SDL/main.m	Thu Aug 17 11:03:44 2006
+@@ -1,5 +1,3 @@
+-#import <AppKit/NSApplication.h>
+-
+ #ifdef GNUSTEP
+ #import <Foundation/NSAutoreleasePool.h>
+ #import <Foundation/NSString.h>
+@@ -25,6 +23,11 @@
+ 	// Need this because we're not using the default run loop's autorelease
  	// pool.
  	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
- 
++
 +	NSString *gstepdir = [NSHomeDirectory() stringByAppendingPathComponent:@"GNUstep"];
 +	NSString *libdir   = [gstepdir stringByAppendingPathComponent:@"Library"];
 +	[[NSFileManager defaultManager] createDirectoryAtPath:gstepdir attributes:nil];
 +	[[NSFileManager defaultManager] createDirectoryAtPath:libdir attributes:nil];
-+
+ 
  	// dajt: allocate and set the NSApplication delegate manually because not
  	// using NIB to do this
- 	controller = [[GameController alloc] init];
