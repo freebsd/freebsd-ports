d3ae086cc4924b15655a840784d4f862c0130cc2
Rong-En Fan <rafan@FreeBSD.org>
2006-08-21 04:30:49 +0000

    - Update to 0.1.14 - Replace the build system to hand-written configure/Makefile - Mark it as BROKEN on 4.x: does not compile
    PR:		ports/101961
    Submitted by:	Anish Mistry <amistry at am-productions.biz>
    Approved by:	Filippo Natali" <filippo.natali at gmail.com> (maintainer)

---
+++ net/iaxmodem/files/patch-lib_spandsp_src_spandsp_t4.h
@@ -0,0 +1,11 @@
+--- lib/spandsp/src/spandsp/t4.h.orig	Mon Jun 12 15:54:34 2006
++++ lib/spandsp/src/spandsp/t4.h	Thu Jun 15 14:17:20 2006
+@@ -28,6 +28,8 @@
+ 
+ /*! \file */
+ 
++#include <time.h>
++
+ #if !defined(_T4_H_)
+ #define _T4_H_
+ 
