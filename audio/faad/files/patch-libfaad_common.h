cfb3d4c3ee89044494764dad022b073d5f87fd8f
Michael Johnson <ahze@FreeBSD.org>
2006-08-24 23:33:51 +0000

    - Update to 2.5

--- audio/faad/files/patch-libfaad_common.h
+++ audio/faad/files/patch-libfaad_common.h
@@ -1,16 +1,18 @@
---- libfaad/common.h.orig	Fri Feb  6 13:55:24 2004
-+++ libfaad/common.h	Wed Jan 19 01:52:32 2005
-@@ -308,6 +308,7 @@
-         return i;
-     }
-   #elif (defined(__i386__) && defined(__GNUC__))
-+    #ifndef HAVE_LRINTF
+--- libfaad/common.h.orig	Thu Aug 24 16:50:30 2006
++++ libfaad/common.h	Thu Aug 24 16:51:20 2006
+@@ -310,6 +310,7 @@
+ 
+ 
+   #if defined(_WIN32) && !defined(__MINGW32__)
++   #ifndef HAVE_LRINTF
      #define HAS_LRINTF
-     // from http://www.stereopsis.com/FPU.html
      static INLINE int lrintf(float f)
-@@ -320,6 +321,7 @@
+     {
+@@ -335,6 +336,7 @@
              : "m" (f));
          return i;
      }
 +    #endif /* HAVE_LRINTF */
    #endif
+ 
+ 
