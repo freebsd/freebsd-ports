4461f7b04eea8ae4e628270f1c7a5aa66e251e01
miwi <miwi@FreeBSD.org>
2006-08-28 12:29:14 +0000

    - Update to 1.4.9
    PR:		ports/102580
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- audio/mhwaveedit/files/patch-src-filetypes.c
+++ audio/mhwaveedit/files/patch-src-filetypes.c
@@ -1,40 +1,6 @@
---- src/filetypes.c.orig	Sat Jan 14 22:39:51 2006
-+++ src/filetypes.c	Sun Jan 29 12:48:53 2006
-@@ -104,15 +104,6 @@
-      return FALSE;
- }
- 
--static gboolean xunsetenv(char *varname)
--{
--     if (unsetenv(varname) != 0) {
--	  console_message(_("unsetenv failed!"));
--	  return TRUE;
--     }
--     return FALSE;
--}
--
- static struct file_type *register_file_type
- (gchar *name, gchar *ext, gboolean lossy,
-  gboolean (*typecheck)(gchar *filename), 
-@@ -971,7 +962,7 @@
-      b = pipe_dialog_send_chunk(y,c,FALSE,dither_mode,bar);
-      g_free(c);     
-      if (x != NULL) gtk_object_sink(GTK_OBJECT(x));
--     if (!xunsetenv("OUTFILE")) g_free(d);
-+     if (!xputenv("OUTFILE")) g_free(d);
-      if (b || !file_exists(filename)) {
- 	  *fatal = TRUE;
- 	  return TRUE;
-@@ -1240,7 +1231,7 @@
- 	       gtk_object_sink(GTK_OBJECT(x));
- 	  }
-      }
--     if (!xunsetenv("OUTFILE")) g_free(c);
-+     if (!xputenv("OUTFILE")) g_free(c);
-      if (b || !file_exists(filename)) {
- 	  *fatal = TRUE;
- 	  return TRUE;
-@@ -1254,7 +1245,11 @@
+--- src/filetypes.c.orig	Tue Jul 25 05:06:59 2006
++++ src/filetypes.c	Tue Aug 22 15:52:56 2006
+@@ -1258,7 +1258,11 @@
  static Chunk *try_mplayer(gchar *filename, int dither_mode, StatusBar *bar)
  {
       gchar *c,*d;
@@ -47,13 +13,7 @@
       Chunk *x;
       if (!program_exists("mplayer")) return NULL;
       tempname = get_temp_filename(0);
-@@ -1263,20 +1258,16 @@
-      if (xputenv(c)) { g_free(d); g_free(c); g_free(tempname); return NULL; }
-      if (xputenv(d)) { 
- 	  g_free(d); 
--	  if (!xunsetenv("OUTFILE"))
-+	  if (!xputenv("OUTFILE"))
- 	       g_free(c);
+@@ -1272,10 +1276,6 @@
  	  g_free(tempname);
  	  return NULL; 
       }
@@ -64,10 +24,3 @@
  
       x = run_decoder(filename,tempname,"sh",argv,dither_mode,bar);
  
--     if (!xunsetenv("OUTFILE")) g_free(c);
--     if (!xunsetenv("INFILE")) g_free(d);
-+     if (!xputenv("OUTFILE")) g_free(c);
-+     if (!xputenv("INFILE")) g_free(d);
-      g_free(tempname);
-      
-      return x;
