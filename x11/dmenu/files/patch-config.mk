c39d9bc85a11ec40fba3a64e58e7ed2d3344e1c3
Pav Lucistnik <pav@FreeBSD.org>
2006-08-28 08:17:37 +0000

    dmenu is a minimalistic X11 menu. It reads a newline separated list of items from stdin and shows them as a menu on the top of the screen. When the user selects one item or types any text and presses Enter, his choice is printed to stdout.
    dmenu was developed as an addition to the dwm window manager, but can be used
    in any X11-environment.
    
    WWW: http://10kloc.org/dwm/
    
    PR:		ports/101573
    Submitted by:	Jeroen Schot <schot@a-eskwadraat.nl>

---
+++ x11/dmenu/files/patch-config.mk
@@ -0,0 +1,30 @@
+--- config.mk.orig	Mon Aug  7 13:34:51 2006
++++ config.mk	Mon Aug  7 13:35:50 2006
+@@ -4,21 +4,21 @@
+ # Customize below to fit your system
+ 
+ # paths
+-PREFIX = /usr/local
+-MANPREFIX = ${PREFIX}/share/man
++PREFIX ?= /usr/local
++MANPREFIX = ${PREFIX}/man
+ 
+-X11INC = /usr/X11R6/include
+-X11LIB = /usr/X11R6/lib
++X11INC = $(X11BASE)/include
++X11LIB = $(X11BASE)/lib
+ 
+ # includes and libs
+ INCS = -I/usr/lib -I${X11INC}
+ LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
+ 
+ # flags
+-CFLAGS = -Os ${INCS} -DVERSION=\"${VERSION}\"
++CFLAGS+= ${INCS} -DVERSION=\"${VERSION}\"
+ LDFLAGS = ${LIBS}
+ #CFLAGS = -g -Wall -O2 ${INCS} -DVERSION=\"${VERSION}\"
+ #LDFLAGS = -g ${LIBS}
+ 
+ # compiler
+-CC = cc
++CC?= cc
