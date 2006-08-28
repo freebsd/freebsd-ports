28ba660cdf538eb2eb1839398f70347121a4dc05
miwi <miwi@FreeBSD.org>
2006-08-28 11:56:05 +0000

    - Update to version 2.2.8
    PR:		ports/102568
    Submitted by:	KATO Tsuguru <tkato432(at)yahoo.com>

--- x11-wm/pawm/files/patch-src__pafont.c
+++ x11-wm/pawm/files/patch-src__pafont.c
@@ -1,14 +1,5 @@
 --- src/pafont.c.orig	Tue Jul 25 05:49:39 2006
 +++ src/pafont.c	Fri Jul 28 16:28:48 2006
-@@ -44,7 +44,7 @@
- }
- 
- 
--PAFont pafont_create(Display *dpy, const char* id)
-+PAFont pafont_create(Display *dpy, Visual *visual, const char* id)
- {
- 	PAFont xlfdfont;
- 	char fontname[20];
 @@ -86,7 +86,7 @@
  {
  	ASSERT(xlfdfont);
