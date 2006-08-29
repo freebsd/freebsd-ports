fa2d81e6f9fd1bd4c98fc8310f6a26be0560ae40
Dirk Meyer <dinoex@FreeBSD.org>
2006-08-29 04:14:27 +0000

    - fix for child reaping Submitted by:	Scott Hazen Mueller

--- news/nntp/files/patch-server-netaux.c
+++ news/nntp/files/patch-server-netaux.c
@@ -1,11 +1,24 @@
---- server/netaux.c.orig	Tue Jun  4 21:22:30 2002
-+++ server/netaux.c	Tue Jun  4 21:23:10 2002
-@@ -261,7 +261,7 @@
+--- server/netaux.c.orig	Tue Nov  1 07:08:56 1994
++++ server/netaux.c	Sun Aug 27 21:52:42 2006
+@@ -20,6 +20,9 @@
+ #else
+ #include <sys/time.h>
+ #endif
++#ifdef BSD_44
++#include <sys/resource.h>
++#endif
  
+ /*
+  * read_again -- (maybe) read in the active file again,
+@@ -262,7 +265,11 @@
  reaper()
  {
--#ifndef USG
-+#if !defined(USG) && !defined(BSD_44)
+ #ifndef USG
++#ifdef BSD_44
++	int status;
++#else
  	union wait	status;
++#endif
  
  	while (wait3(&status, WNOHANG, (struct rusage *)0) > 0)
+ 		;
