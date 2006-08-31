a08eac1dadb840a3d32fb5f9c2b3415dd6032da7
Pav Lucistnik <pav@FreeBSD.org>
2006-08-31 18:04:20 +0000

    - Update to 1.0.4.r1
    PR:		ports/102713
    Submitted by:	Timur I. Bakeyev <timur@gnu.org> (maintainer)

---
+++ net/samba4wins/files/patch-torture_config.mk
@@ -0,0 +1,11 @@
+--- torture/config.mk.orig	Mon Aug 28 23:34:14 2006
++++ torture/config.mk	Mon Aug 28 23:34:27 2006
+@@ -198,7 +198,7 @@
+ 
+ #################################
+ # Start BINARY smbtorture
+-[BINARY::smbtorture]
++[BINARY::smbtorture4]
+ INSTALLDIR = BINDIR
+ PRIVATE_PROTO_HEADER = torture.h
+ OBJ_FILES = \
