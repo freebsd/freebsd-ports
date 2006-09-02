db0cd25d6aa8b0d61ee2520a87ef4f4078fa6ce8
maho <maho@FreeBSD.org>
2006-09-02 12:12:44 +0000

    * Install more header files. * get rid of parts that link against libcompat * bump PORTREVISION

---
+++ math/lp_solve/files/patch-shared+commonlib.c
@@ -0,0 +1,27 @@
+--- shared/commonlib.c.orig	Sat Mar 18 07:16:42 2006
++++ shared/commonlib.c	Sat Sep  2 21:01:39 2006
+@@ -4,7 +4,7 @@
+ #ifdef INTEGERTIME
+ # include <time.h>
+ #else
+-# include <sys/timeb.h>
++# include <sys/time.h>
+ #endif
+ 
+ #include <stdlib.h>
+@@ -673,10 +673,12 @@
+ #elif defined CLOCKTIME
+   return((double)clock()/CLOCKS_PER_SEC /* CLK_TCK */);
+ #else
+-  struct timeb buf;
++  struct timeval tv;
++  struct timezone tz;
++
++  gettimeofday(&tv, &tz);
++  return((double)tv.tv_sec+((double)tv.tv_usec)/1000.0);
+ 
+-  ftime(&buf);
+-  return((double)buf.time+((double) buf.millitm)/1000.0);
+ #endif
+ }
+ 
