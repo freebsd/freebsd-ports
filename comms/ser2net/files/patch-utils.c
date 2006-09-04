93e7abf2c3880fa19d6b8cfe5ee376adbe38517f
Andrew Pantyukhin <sat@FreeBSD.org>
2006-09-04 09:26:10 +0000

    Add port comms/ser2net:
    This is ser2net, a program for allowing network connections to serial
    ports.  See the man page for information about using the program.
    
    Note that ser2net supports RFC 2217 (remote control of serial port
    parameters), but you must have a complient client.  The only one I
    know if is kermit (http://www.columbia.edu/kermit).
    
    WWW: http://www.sourceforge.net/projects/ser2net/
    
    Submitted by:	Devon H. O'Dell <devon.odell@coyotepoint.com>

---
+++ comms/ser2net/files/patch-utils.c
@@ -0,0 +1,10 @@
+--- utils.c.old	Sun Sep  3 16:39:48 2006
++++ utils.c	Sun Sep  3 16:39:56 2006
+@@ -21,6 +21,7 @@
+ 
+ #include <string.h>
+ #include <netdb.h>
++#include <sys/types.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
