e7e2d61aa8232da1e7163fc28b811da299820a30
bsam <bsam@FreeBSD.org>
2006-09-07 11:58:44 +0000

    - update to newest version 1.9; - fix plist; - some cosmetic changes; - change the authours email address to a new one; - pass maintainership to the submitter.
    PR:		102870
    Submitted by:	Marcus von Appen <mva at sysfault.org>

--- devel/py-ply/files/patch-setup.py
+++ devel/py-ply/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Wed Jul 20 01:27:21 2005
-+++ setup.py	Wed Jul 20 01:27:31 2005
-@@ -17,7 +17,7 @@
+--- setup.py.orig	Mon Sep  4 23:19:42 2006
++++ setup.py	Mon Sep  4 23:19:49 2006
+@@ -16,7 +16,7 @@
  
  PLY is extremely easy to use and provides very extensive error checking. 
  """,
 -            licence="""Lesser GPL (LGPL)""",
 +            license="""Lesser GPL (LGPL)""",
-             version = "1.3.1",
+             version = "1.9",
              author = "David Beazley",
-             author_email = "beazley@cs.uchicago.edu",
+             author_email = "dave@dabeaz.com",
