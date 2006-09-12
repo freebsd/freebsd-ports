1e4b64bae76e2ae7854c7b0c0d78695c635044a2
lev <lev@FreeBSD.org>
2006-09-12 12:46:30 +0000

    (1) Update to 1.4.0 release   (2) Migrate book to stable version 1.2   (3) Add images to HTML book   (4) Add dependency to gfind (findutils) for asvn   (5) Improve asvn script
    PR:		ports/99177 [4],[5]
    Submitted by:	David Robillard <david.robillard@gmail.com> [3]

--- devel/subversion-freebsd/files/patch-build--ltmain.sh
+++ devel/subversion-freebsd/files/patch-build--ltmain.sh
@@ -1,5 +1,5 @@
---- ac-helpers/ltmain.sh.orig	Fri May 20 21:41:54 2005
-+++ ac-helpers/ltmain.sh	Wed May 25 14:38:21 2005
+--- build/ltmain.sh.orig	Fri May 20 21:41:54 2005
++++ build/ltmain.sh	Wed May 25 14:38:21 2005
 @@ -1492,7 +1492,7 @@
  	    # These systems don't actually have a C library (as such)
  	    test "X$arg" = "X-lc" && continue
