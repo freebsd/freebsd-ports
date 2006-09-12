1233c00f46b3156a84e8370551b0fac79bd3f11a
Mathieu Arnold <mat@FreeBSD.org>
2006-09-12 13:29:00 +0000

    Update to 1.13

--- databases/p5-DBD-SQLite/files/patch-dbdimp.h
+++ databases/p5-DBD-SQLite/files/patch-dbdimp.h
@@ -1,10 +1,9 @@
---- dbdimp.h.orig	Sat Sep 11 00:24:28 2004
-+++ dbdimp.h	Mon Dec 27 16:37:25 2004
-@@ -99,6 +99,10 @@
- #define newUTF8SVpv newSVpv
+--- dbdimp.h.orig	Fri Sep  8 06:50:50 2006
++++ dbdimp.h	Tue Sep 12 14:00:36 2006
+@@ -105,6 +105,9 @@
  #define newUTF8SVpvn newSVpvn
  #define SvUTF8_on(a) (a)
-+#define SvUTF8_off(a) (a)
+ #define SvUTF8_off(a) (a)
 +#ifndef get_sv
 +#define get_sv perl_get_sv
 +#endif
