c59bcf0778784e70f28592d24c3bdfdc04331b5d
Martin Wilke <miwi@FreeBSD.org>
2006-09-14 06:47:58 +0000

    - Update to 1.0.2
    PR:		ports/101755
    Submitted by:	Matthew West <mwest(at)uct.ac.za>
    Approved by:	Maintainer timeout

--- net/silc-server/files/silcd.sh
+++ net/silc-server/files/silcd.sh
@@ -1,15 +1,41 @@
 #!/bin/sh
+#
+# $FreeBSD$
+#
+# PROVIDE: silcd
+# REQUIRE: NETWORKING
+#
+# Add the following line to /etc/rc.conf[.local] to enable silcd:
+#
+# silcd_enable="YES"
+#
 
-case "$1" in
-start)
-	[ -x %%PREFIX%%/sbin/silcd ] && %%PREFIX%%/sbin/silcd > /dev/null && echo -n ' silcd'
-	;;
-stop)
-	[ -r /var/run/silcd.pid ] && /bin/kill `cat /var/run/silcd.pid` > /dev/null && echo -n ' silcd'
-	;;
-*)
-	echo "Usage: `basename $0` {start|stop}" >&2
-	;;
-esac
+silcd_enable=${silcd_enable-"NO"}
 
-exit 0
+. %%RC_SUBR%%
+
+name=silcd
+rcvar=`set_rcvar`
+
+command=%%PREFIX%%/sbin/${name}
+pidfile=/var/run/${name}.pid
+
+extra_commands="keygen"
+keygen_cmd="silcd_keygen"
+start_precmd="silcd_precmd"
+
+silcd_keygen()
+{
+	umask 077
+	${command} -C %%PREFIX%%/etc/silc
+}
+
+silcd_precmd()
+{
+	if [ ! -f %%PREFIX%%/etc/silc/silcd.prv -a ! -f %%PREFIX%%/etc/silc/silcd.pub ]; then
+		run_rc_command keygen
+	fi
+}
+
+load_rc_config ${name}
+run_rc_command "$1"
