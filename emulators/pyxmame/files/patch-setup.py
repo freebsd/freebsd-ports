704a7edcafc87dd737738c1d20aa3bd504830610
acm <acm@FreeBSD.org>
2006-09-17 15:51:22 +0000

    - Update to 0.4.0
    Approved by:	garga (mentor,implicit)

--- emulators/pyxmame/files/patch-setup.py
+++ emulators/pyxmame/files/patch-setup.py
@@ -1,24 +1,21 @@
---- setup.py	Tue Aug  1 02:32:46 2006
-+++ setup.py	Tue Aug  1 02:33:30 2006
-@@ -9,7 +9,7 @@
-       url='http://pegueroles.cat/pyxmame',
-       long_description='Allows you search, browse and launch games from a GTK interface.Looks good and its very responsive even with a lot of roms.',
+--- setup.py	Sat Sep 16 23:03:56 2006
++++ setup.py	Sat Sep 16 23:05:37 2006
+@@ -16,16 +16,16 @@
+   It uses a list of sets of images to show the game image.
+ """,
        license='GNU GPL',
 -      platforms='linux',
 +      platforms='FreeBSD',
        packages=['pyxmame'],
        scripts=[
-                'scripts/pyxmame',
-@@ -18,10 +18,10 @@
+                'scripts/pyxmame'
                 ],
        data_files=[
                    ('share/pixmaps/pyxmame',["pixmaps/penguin-revolt-128.png"]),
--                  ('share/games/pyxmame',["config/catver.ini"]),
 -                  ('share/games/pyxmame/flyers',''),
 -                  ('share/games/pyxmame/roms',''),
 -                  ('share/games/pyxmame/snaps',''),
 -                  ('/etc/pyxmame',["config/pyxmamerc"])
-+                  ('share/pyxmame',["config/catver.ini"]),
 +                  ('share/pyxmame/flyers',''),
 +                  ('share/pyxmame/roms',''),
 +                  ('share/pyxmame/snaps',''),
