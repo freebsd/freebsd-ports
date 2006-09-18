c5c9b472b1870aea96e32d4cc506bb003dd3cf9e
rafan <rafan@FreeBSD.org>
2006-09-18 13:18:55 +0000

    - Fix test on amd64   Note that it still fails due to the multiuser patch. However, I think   this PR already improves PPerl on amd64 a lot. So, I decide to commit   it first. - Correct CFLAGS passed to cc (patch-Makefile.PL)
    These patches were sent to the author via CPAN RT.
    
    PR:		ports/103282
    Submitted by:	Vivek Khera <vivek at khera.org>

--- devel/p5-PPerl/files/patch-main.c
+++ devel/p5-PPerl/files/patch-main.c
@@ -1,10 +1,25 @@
 --- main.c.orig	Wed Mar  3 01:06:15 2004
-+++ main.c	Mon Dec 13 18:22:53 2004
++++ main.c	Fri Sep 15 16:48:22 2006
+@@ -105,11 +105,11 @@
+             newval = atoi(pArg);
+             if (newval > 0) prefork = newval;
+         }
+-        else if (!strncmp(pArg, "--logfile", 7) ) {
++        else if (!strncmp(pArg, "--logfile", 9) ) {
+             int newval;
+             char *filename;
+-            if (pArg[7] == '=') /* --logfile=.... */
+-              pArg += 13;
++            if (pArg[9] == '=') /* --logfile=.... */
++              pArg += 10;
+             else
+               pArg = argv[++i];
+             
 @@ -223,6 +223,8 @@
      /* strict C compilers can't/won't do char foo[variant]; */
      char *fullpath = my_malloc(path_max);
      int i = 0;
-+    char euid[sizeof(uid_t)+2];
++    char euid[sizeof(uid_t)+10+2];
 +    sprintf(euid, "_%d", geteuid());
  
      if (realpath(scriptname, fullpath) == NULL) {
