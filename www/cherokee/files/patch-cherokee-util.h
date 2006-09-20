acbc095fcc4368acf03ef240a817ecca3d940227
Martin Wilke <miwi@FreeBSD.org>
2006-09-20 10:33:33 +0000

    - Update to 0.5.5
    PR:		ports/103228
    Submitted by:	chinsan <chinsan.tw@gmail.com>

--- www/cherokee/files/patch-cherokee-util.h
+++ www/cherokee/files/patch-cherokee-util.h
@@ -1,5 +1,5 @@
---- cherokee/util.h.orig	Sat Oct 15 19:31:05 2005
-+++ cherokee/util.h	Thu Oct 20 00:59:40 2005
+--- cherokee/util.h.orig	Wed Sep 13 22:13:07 2006
++++ cherokee/util.h	Wed Sep 13 22:15:20 2006
 @@ -31,6 +31,14 @@
  
  #include <cherokee/common.h>
@@ -12,6 +12,6 @@
 +# include <sys/socket.h>
 +#endif
 +
- #ifdef HAVE_NETINET_IN_H
- # include <netinet/in.h>
- #endif
+ #include <time.h>
+ #include <dirent.h>
+ 
