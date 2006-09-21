c5ed4911be1508c001a46a904a3ad413451864fc
Oliver Braun <obraun@FreeBSD.org>
2006-09-21 21:13:42 +0000

    Fix build on 4-STABLE.

--- sysutils/multitail/files/extra-patch-cv.c
+++ sysutils/multitail/files/extra-patch-cv.c
@@ -1,6 +1,14 @@
---- cv.c.orig	Tue Apr 18 15:54:34 2006
-+++ cv.c	Sun Apr 30 18:20:44 2006
-@@ -81,6 +81,7 @@
+--- cv.c.orig	Fri Sep  8 08:38:28 2006
++++ cv.c	Thu Sep 21 21:09:36 2006
+@@ -105,6 +105,7 @@
+ 
+ 		case CONVTYPE_EPOCHTODATE:
+ 			{
++				#define atoll(a) strtoll((a), (char **)NULL, 10)
+ 				char *new_str = epoch_to_str((time_t)atoll(what));
+ 
+ 				if (new_str)
+@@ -131,6 +132,7 @@
  
  		case CONVTYPE_DECTOHEX:
  			{
