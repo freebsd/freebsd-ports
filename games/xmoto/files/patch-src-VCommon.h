b2003d2b473f40728c67c9a05d3a2770e57e6aba
Cheng-Lung Sung <clsung@FreeBSD.org>
2006-09-22 00:40:04 +0000

    - Update to 0.2.1 - X-Moto 0.2.1 2006-09-14 -------------------------------
    * X-Moto is no more ALPHA !
    * ppc computer can now read and write x86 replays (patch by Dave Vasilevsky)
    * upload highscores from the game
    * webrooms configuration
    * Man pages (from debian package)
    * desktop files and icon for kde/gnome (by SnowBear)
    * -nowww option to force xmoto to not connect on the web
    * -testTheme option to test your themes
    * new functions for scripted levels
    * grip parameter for blocks (to make ice blocks for example)
    * auto-adaptation of the camera (depending on the gravity/wind)
    * uglyer ugly mode (for slower computer ; press F1 to switch ugly mode)
    * translations [french] (for unix only)
    * collision between body and sprites
    * level preview animation before playing (patch by Frederic Huard)
    * Some minor improvements
    * Several bugs fixed.
    
    PR:		ports/103394
    Submitted by:	maintainer (Dmitry Marakasov)

--- games/xmoto/files/patch-src-VCommon.h
+++ games/xmoto/files/patch-src-VCommon.h
@@ -1,12 +1,12 @@
---- src/VCommon.h.orig	Thu Jul 20 21:51:42 2006
-+++ src/VCommon.h	Fri Jul 28 15:59:16 2006
-@@ -62,8 +62,7 @@
+--- src/VCommon.h.orig	Tue Sep 12 22:29:01 2006
++++ src/VCommon.h	Sat Sep 16 17:45:59 2006
+@@ -70,8 +70,7 @@
  #include <string>
  #include <vector>
  #include <queue>
 -#include <ostream>
 -#include <istream>
 +#include <iostream>
+ #include <sstream>
  
  #include <stdio.h>
- #include <math.h>
