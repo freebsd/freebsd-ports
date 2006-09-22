f450c9190f62c229973316b5edb013499f7585d0
arved <arved@FreeBSD.org>
2006-09-22 15:06:23 +0000

    Add missing dependency on libdnet
    PR:		102272
    Submitted by:	David Adam <zanchey@ucc.gu.uwa.edu.au>
    Approved by:	maintainer timeout (1 month)
    
    While here fix a segfault in usage() (*sigh*)

--- net-mgmt/arpscan/files/patch-arpscan.c
+++ net-mgmt/arpscan/files/patch-arpscan.c
@@ -1,6 +1,16 @@
---- arpscan.c.orig	Mon Aug 18 16:54:25 2003
-+++ arpscan.c	Mon Mar 22 23:05:40 2004
-@@ -17,6 +17,13 @@
+--- arpscan.c.orig	Mon Aug 18 22:54:25 2003
++++ arpscan.c	Fri Sep 22 16:55:48 2006
+@@ -8,7 +8,9 @@
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
++#include <err.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
+ 
+@@ -17,6 +19,13 @@
  
  #define PCAP_TIMEO 20
  
@@ -14,3 +24,12 @@
  struct	ether_arp {
  	struct	 arp_hdr ea_hdr;
  	u_int8_t arp_sha[ETH_ADDR_LEN];
+@@ -36,7 +45,7 @@
+ 	fprintf(stderr, "\teg)\n");
+ 	fprintf(stderr, "\t    %s 172.16.1.11\n", __progname);
+ 	fprintf(stderr, "\t    %s 192.168.1.100-192.168.1.200\n", __progname);
+-	fprintf(stderr, "\t    %s 172.16.1.41/29\n");
++	fprintf(stderr, "\t    %s 172.16.1.41/29\n", __progname);
+ 	fprintf(stderr, "\n");
+ 	exit(1);
+ }
