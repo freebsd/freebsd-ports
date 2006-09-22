424d45ee0709648bb7fd498d97be8ae8bf9ac23b
Ying-Chieh Liao <ijliao@FreeBSD.org>
2006-09-22 07:45:58 +0000

    upgrade to 0.12.1 take maintainership
    PR:		103211
    Submitted by:	Alex Samorukov <samm@os2.kiev.ua>

---
+++ palm/pilot-link/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Fri Sep 22 15:15:14 2006
++++ ltmain.sh	Fri Sep 22 15:15:24 2006
+@@ -6003,10 +6003,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
