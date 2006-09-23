e591844a5192b3601dd3843c97f9c103e80ca9bc
clsung <clsung@FreeBSD.org>
2006-09-23 03:19:54 +0000

    - Update to 1.52
    PR:		ports/103467
    Submitted by:	Gea-Suan Lin <gslin_AT_gslin dot org>

--- textproc/p5-Number-Format/files/patch-Format.pm
+++ textproc/p5-Number-Format/files/patch-Format.pm
@@ -1,13 +1,11 @@
---- Format.pm.orig	Thu Apr 27 00:47:37 2006
-+++ Format.pm	Thu Apr 27 21:59:09 2006
-@@ -643,8 +643,8 @@
-     $decimal .= '0'x($precision - length $decimal);
+--- Format.pm.orig	Fri Sep 22 08:43:28 2006
++++ Format.pm	Fri Sep 22 08:43:35 2006
+@@ -644,7 +644,7 @@
  
      # Combine it all back together and return it.
--    $self->{int_curr_symbol} =~ s/\s+$/ /;
+     $self->{int_curr_symbol} =~ s/\s*$/ /;
 -    my $result = ($self->{int_curr_symbol} .
-+    $self->{int_curr_symbol} =~ s/\s+$//;
-+    my $result = ($self->{int_curr_symbol} . ' ' .
++    my $result = ($self->{int_curr_symbol} . ' '
                    ($precision ?
                     join($self->{mon_decimal_point}, $integer, $decimal) :
                     $integer));
