3c80bae4859349a24eadec1ec05ec1dea8bd2f1a
arved <arved@FreeBSD.org>
2006-09-24 16:11:02 +0000

    Fix build with gcc 3.4.6
    Reported by:	pointyhat

---
+++ security/steghide/files/patch-src-MHashPP.cc
@@ -0,0 +1,10 @@
+--- src/MHashPP.cc.orig	Sun Sep 24 17:48:31 2006
++++ src/MHashPP.cc	Sun Sep 24 17:56:27 2006
+@@ -21,6 +21,7 @@
+ #include <cstdlib>
+ #include <string>
+ 
++#define _Bool bool
+ #include <mhash.h>
+ 
+ #include "BitString.h"
