02529500d1d5ba9e319c4f03614dc7a07bf9d7a4
miwi <miwi@FreeBSD.org>
2006-09-24 21:36:03 +0000

    - Update to 0.30
    PR:		ports/103524
    Submitted by:	Lapo Luchini <lapo(at)lapo.it> (maintainer)

--- devel/monotone/files/patch-monotone.cc
+++ devel/monotone/files/patch-monotone.cc
@@ -1,13 +1,13 @@
---- monotone.cc.orig    Fri Sep 30 11:50:05 2005
-+++ monotone.cc Fri Oct 14 11:31:14 2005
-@@ -238,8 +238,10 @@
-   // go-go gadget i18n
- 
-   setlocale(LC_ALL, "");
+--- monotone.cc.orig	Sun Sep 17 11:56:38 2006
++++ monotone.cc	Sat Sep 23 18:42:05 2006
+@@ -188,8 +188,10 @@
+   if (!init)
+     {
+       setlocale(LC_ALL, "");
 +#ifdef ENABLE_NLS
-   bindtextdomain(PACKAGE, LOCALEDIR);
-   textdomain(PACKAGE);
+       bindtextdomain(PACKAGE, LOCALEDIR);
+       textdomain(PACKAGE);
 +#endif
- 
- 
-   // we want to catch any early informative_failures due to charset
+       init = 1;
+     }
+ }
