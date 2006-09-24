4a43e6024f6cbd7ec57b63909b7654e6b6cd12ea
Pav Lucistnik <pav@FreeBSD.org>
2006-09-24 12:25:57 +0000

    - Update to 0.98.1
    PR:		ports/103528
    Submitted by:	Rainer Alves <rainer.alves@gmail.com> (maintainer)

--- multimedia/dvdrip/files/patch-lib__Video__DVDRip__FilterList.pm
+++ multimedia/dvdrip/files/patch-lib__Video__DVDRip__FilterList.pm
@@ -1,11 +1,11 @@
---- lib/Video/DVDRip/FilterList.pm.orig	Sat Jul 23 03:00:00 2005
-+++ lib/Video/DVDRip/FilterList.pm	Sat Mar 25 18:09:24 2006
-@@ -211,7 +211,7 @@
- 	print STDERR "[filterlist] (re)scanning transcode's module path $modpath...\n";
- 	
- 	my @filter_names = grep !/^(pv|preview)$/,
--			   map  {m!/filter_([^/]+)\.so$!}
-+			   map  {m!/filter_([^/]+)\.so$/!}
- 			   glob ("$modpath/filter_*");
+--- lib/Video/DVDRip/FilterList.pm.orig	Fri Sep 22 16:05:34 2006
++++ lib/Video/DVDRip/FilterList.pm	Fri Sep 22 16:06:07 2006
+@@ -220,7 +220,7 @@
+         "[filterlist] (re)scanning transcode's module path $modpath...\n";
  
- 	my %filters;
+     my @filter_names = grep !/^(pv|preview)$/,
+-        map {m!/filter_([^/]+)\.so$!} glob("$modpath/filter_*");
++        map {m!/filter_([^/]+)\.so$/!} glob("$modpath/filter_*");
+ 
+     my %filters;
+     foreach my $filter_name (@filter_names) {
