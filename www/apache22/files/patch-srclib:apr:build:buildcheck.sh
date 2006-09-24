9f63b13398e0098ca4989e4c2c18065b6f8ab7f5
Clement Laforet <clement@FreeBSD.org>
2006-09-24 16:16:40 +0000

    - House keeping (update to current standards: USE_RC_SUBR, USE_LDCONFIG,etc.) - Remove python dependency unless apr_dbd_mysql is built [1]
    Required by:		many [1]

--- www/apache22/files/patch-srclib:apr:build:buildcheck.sh
+++ www/apache22/files/patch-srclib:apr:build:buildcheck.sh
@@ -1,6 +1,22 @@
---- srclib/apr/build/buildcheck.sh.orig	Fri Jan 14 11:16:31 2005
-+++ srclib/apr/build/buildcheck.sh	Wed Feb 23 11:16:25 2005
-@@ -38,7 +38,7 @@
+--- srclib/apr/build/buildcheck.sh.orig	Fri Jun 17 00:53:05 2005
++++ srclib/apr/build/buildcheck.sh	Sun Sep 24 15:18:38 2006
+@@ -2,6 +2,7 @@
+ 
+ echo "buildconf: checking installation..."
+ 
++if [ "x${FULLBUILD}" = "xon" ] ; then
+ # any python
+ python=`build/PrintPath python`
+ if test -z "$python"; then
+@@ -13,6 +14,7 @@
+ py_version=`python -c 'import sys; print sys.version' 2>&1|sed 's/ .*//;q'`
+ echo "buildconf: python version $py_version (ok)"
+ fi
++fi
+ 
+ # autoconf 2.50 or newer
+ ac_version=`${AUTOCONF:-autoconf} --version 2>/dev/null|sed -e 's/^[^0-9]*//;s/[a-z]* *$//;q'`
+@@ -38,7 +40,7 @@
  # output is multiline from 1.5 onwards
  
  # Require libtool 1.4 or newer
