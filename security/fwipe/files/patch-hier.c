a45c483301e07cf1864712894e78974ab2c9b20f
miwi <miwi@FreeBSD.org>
2006-09-26 18:09:39 +0000

    - Fix build problems [1] - Fix permissions [2]
    Submitted by:	pointyhat via kris [1]
                    miwi [2]
    Approved by:	David Thiel <lx(at)redundancy.redundancy.org> (maintainer)

--- security/fwipe/files/patch-hier.c
+++ security/fwipe/files/patch-hier.c
@@ -1,10 +1,22 @@
---- hier.c.orig	Fri Sep 22 07:11:47 2006
-+++ hier.c	Fri Sep 22 07:12:15 2006
-@@ -10,11 +10,7 @@
+--- hier.c.orig	Sat Apr 14 04:53:05 2001
++++ hier.c	Tue Sep 26 08:56:49 2006
+@@ -2,19 +2,15 @@
  
-   d(auto_home,"man",-1,-1,02755);
-   d(auto_home,"man/man1",-1,-1,02755);
+ void hier()
+ {
+-  h(auto_home,-1,-1,02755);
+-  d(auto_home,"bin",-1,-1,02755);
++  h(auto_home,-1,-1,0755);
++  d(auto_home,"bin",-1,-1,0755);
+   c(auto_home,"bin","fwipe0",-1,-1,0755);
+   c(auto_home,"bin","echo0",-1,-1,0755);
+   c(auto_home,"bin","fwipe",-1,-1,0755);
+ 
+-  d(auto_home,"man",-1,-1,02755);
+-  d(auto_home,"man/man1",-1,-1,02755);
 -  d(auto_home,"man/cat1",-1,-1,02755);
++  d(auto_home,"man",-1,-1,0755);
++  d(auto_home,"man/man1",-1,-1,0755);
    c(auto_home,"man/man1","fwipe0.1",-1,-1,0644);
 -  c(auto_home,"man/cat1","fwipe0.0",-1,-1,0644);
    c(auto_home,"man/man1","echo0.1",-1,-1,0644);
