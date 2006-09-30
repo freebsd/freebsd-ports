3179f20ddcf853d7a1fdad6cbf7611b89d750542
Yen-Ming Lee <leeym@FreeBSD.org>
2006-09-30 03:10:46 +0000

    - utilize USR_RC_SUBR for new start script - handle version-dependent patch better - bump PORTREVISION

--- net/repeater/files/patch-repeater.cpp
+++ net/repeater/files/patch-repeater.cpp
@@ -1,10 +1,16 @@
---- repeater.cpp.orig	Wed Dec 14 14:49:03 2005
-+++ repeater.cpp	Wed Dec 14 14:49:16 2005
-@@ -28,6 +28,7 @@
+--- repeater.cpp.orig	Wed Oct 19 03:12:05 2005
++++ repeater.cpp	Sat Sep 30 10:01:56 2006
+@@ -28,6 +28,13 @@
  // Linux port (C) 2005 Jari Korhonen, jarit1.korhonen@dnainternet.net
  // ///////////////////////////////////////////////////////////////////////////
  
-+#include <%%LIMITS_H%%>
++#if defined(__FreeBSD__)
++#if __FreeBSD__ < 5
++#include <machine/limits.h>
++#else
++#include <sys/limits.h>
++#endif
++#endif	/* __FreeBSD__ */
  #include <sys/types.h>
  #include <sys/wait.h>
  #include <sys/stat.h>
