8d9f8bdd9956363c39b9e87db4874b8760d7e40b
Vasil Dimov <vd@FreeBSD.org>
2006-10-02 14:00:45 +0000

    Remove support code for old linux distros
    PR:		ports/103901
    Submitted by:	vd
    Approved by:	netchild

--- Mk/bsd.linux-rpm.mk
+++ Mk/bsd.linux-rpm.mk
@@ -1,7 +1,7 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: /tmp/pcvs/ports/Mk/bsd.linux-rpm.mk,v 1.9 2006-07-30 22:34:30 sat Exp $
+# $FreeBSD: /tmp/pcvs/ports/Mk/bsd.linux-rpm.mk,v 1.10 2006-10-02 14:00:45 vd Exp $
 #
 
 # Variables:
@@ -109,15 +109,9 @@ BRANDELF_FILES?=
 
 .    if ${USE_LINUX} == "fc4" || ${USE_LINUX:L} == "yes"
 _LINUX_BASE_SUFFIX=		fc4
-.    elif ${USE_LINUX} == "debian"
-_LINUX_BASE_SUFFIX=		debian
-.    elif ${USE_LINUX} == "fc3"
-_LINUX_BASE_SUFFIX=		fc3
-.    elif ${USE_LINUX} == "8"
-_LINUX_BASE_SUFFIX=		8
 .    else
 # other linux_base ports do not provide a pkg-plist file
-IGNORE=					uses AUTOMATIC_PLIST with an unsupported USE_LINUX, \"${USE_LINUX}\". Supported values are \"yes\", \"8\", \"debian\", \"fc3\" and \"fc4\"
+IGNORE=					uses AUTOMATIC_PLIST with an unsupported USE_LINUX, \"${USE_LINUX}\". Supported values are \"yes\" and \"fc4\"
 .    endif
 
 PLIST?=					${WRKDIR}/.PLIST.linux-rpm
