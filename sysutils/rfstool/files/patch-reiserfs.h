ff13fdabb5470addebbffa38303fa56390c304e3
stas <stas@FreeBSD.org>
2006-10-02 13:34:33 +0000

    - Honour CC and CXX - Fix compiling with gcc 4.1
    Approved by:	sem (mentor)

---
+++ sysutils/rfstool/files/patch-reiserfs.h
@@ -0,0 +1,11 @@
+--- reiserfs.h.orig	Mon Oct  2 13:58:35 2006
++++ reiserfs.h	Mon Oct  2 13:58:39 2006
+@@ -268,7 +268,7 @@
+         {
+         }
+ 
+-        ReiserFsBlock::~ReiserFsBlock()
++        ~ReiserFsBlock()
+         {
+             delete m_lpbMemory;
+         }
