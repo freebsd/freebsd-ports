1f5891fc475e72700a7a44eb40c6fc597ddcb0ff
vd <vd@FreeBSD.org>
2006-10-05 17:09:55 +0000

    Upgrade:
    * net/coda6_server and net/coda6_client from 6.0.7 to 6.1.1 and thus
      un-DEPRECATE them
    * devel/rvm from 1.10 to 1.13
    * devel/lwp from 1.12 to 2.3
    * devel/rpc2 from 1.28 to 2.2
    
    These four are tightly related so I upgrade them in one.. well.. changeset

--- net/coda6_server/files/patch-pioctl.h
+++ net/coda6_server/files/patch-pioctl.h
@@ -1,11 +1,11 @@
---- coda-src/kerndep/pioctl.h.orig	Thu Aug 26 17:23:04 2004
-+++ coda-src/kerndep/pioctl.h	Tue Nov 16 22:04:33 2004
-@@ -104,7 +104,7 @@
+--- coda-src/kerndep/pioctl.h.orig	Thu Oct  5 11:54:04 2006
++++ coda-src/kerndep/pioctl.h	Thu Oct  5 11:54:20 2006
+@@ -81,7 +81,7 @@
  /* unpacking macros */
  #ifndef _IOC_NR
  
--#if defined(__CYGWIN32__) || defined(__NetBSD__)
-+#if defined(__CYGWIN32__) || defined(__NetBSD__) || defined(__FreeBSD__)
+-#if defined(__NetBSD__)
++#if defined(__NetBSD__) || defined(__FreeBSD__)
  #define _IOC_TYPEMASK	0xff
  #define _IOC_TYPESHIFT	8
  #define _IOC_NRMASK	0xff
