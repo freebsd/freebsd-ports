422318ef21c53797f9f2efc03fd5c8b32848ba1c
Martin Wilke <miwi@FreeBSD.org>
2006-10-06 20:33:53 +0000

    - Fix build with gcc41
    PR:		ports/104054
    Submitted by:	trasz <trasz@pin.if.uz.zgora.pl>

--- games/xevil/files/patch-cmn::locator.h
+++ games/xevil/files/patch-cmn::locator.h
@@ -1,5 +1,5 @@
---- cmn/locator.h.dist	Sat Mar 22 20:35:30 2003
-+++ cmn/locator.h	Sat Oct 25 21:42:46 2003
+--- cmn/locator.h.orig	Sat Mar 22 20:35:30 2003
++++ cmn/locator.h	Fri Oct  6 13:44:36 2006
 @@ -47,7 +47,8 @@
  
  
@@ -10,3 +10,18 @@
  
  #include "utils.h"
  #include "coord.h"
+@@ -108,12 +109,12 @@
+ 
+   
+   // Only valid if mapped || flash.
+-  OLgridEntry *gridEntry; 
++  class OLgridEntry *gridEntry; 
+   // Set by OLgridEntry::insert.  Not nec. valid.
+   GLoc gloc; 
+ #if X11
+   // Only valid if mapped || flash.
+-  OLshadowEntry *shadowEntry; 
++  class OLshadowEntry *shadowEntry; 
+   // Set by OLshadowEntry::insert.  Not nec. valid.
+   GLoc shadowGloc; 
+ #endif
--- games/xevil/files/patch-cmn::utils.cpp
+++ games/xevil/files/patch-cmn::utils.cpp
@@ -1,5 +1,5 @@
 --- cmn/utils.cpp.orig	Wed Mar 19 10:05:12 2003
-+++ cmn/utils.cpp	Sat Oct 25 21:45:15 2003
++++ cmn/utils.cpp	Fri Oct  6 13:45:47 2006
 @@ -30,16 +30,17 @@
  
  // Include Files
@@ -24,3 +24,12 @@
  #endif
  
  #ifdef WIN32
+@@ -761,7 +762,7 @@
+ 
+ 
+ private:
+-  Bucket* HashTable::_get(int &index,void* key);
++  Bucket* _get(int &index,void* key);
+   /* MODIFIES: index */
+   /* EFFECTS: Internal helper function.  Return the Bucket containing key
+      or NULL if not found.  Set index to the bucket list for key whether
