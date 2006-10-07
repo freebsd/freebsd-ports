c098c9538dea1bb069c031c7b6cadd06a9258990
dinoex <dinoex@FreeBSD.org>
2006-10-07 18:53:56 +0000

    - fix build for gcc41

--- net/radiusd-cistron/files/patch-radiusd.c
+++ net/radiusd-cistron/files/patch-radiusd.c
@@ -1,5 +1,21 @@
 --- src/radiusd.c.orig	Wed Jul  2 11:39:03 2003
-+++ src/radiusd.c	Fri Jul  9 15:57:46 2004
++++ src/radiusd.c	Sat Oct  7 20:51:28 2006
+@@ -46,6 +46,7 @@
+ char			*radacct_dir;
+ int			log_stripped_names;
+ int 			cache_passwd = 0;
++int			radius_pid;
+ int			use_dbm = 0;
+ int			use_wtmp = 1;
+ int			use_utmp = 1;
+@@ -72,7 +73,6 @@
+ static int		acctfd;
+ static int		spawn_flag;
+ static int		acct_pid;
+-static int		radius_pid;
+ static int		need_reload = 0;
+ static time_t		start_time;
+ static AUTH_REQ		*first_request;
 @@ -612,6 +612,7 @@
          if ((cl = client_find(authreq->ipaddr)) == NULL) {
                  log(L_ERR, "packet from unknown client/host: %s",
