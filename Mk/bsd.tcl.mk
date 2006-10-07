cf96917c27dd6e779753fc85eb5463db2a75bc70
Martin Wilke <miwi@FreeBSD.org>
2006-10-07 10:24:57 +0000

    - The white space police is back in town! - Reformat comments to match other bsd.x.mk files
    PR:		ports/104024
    Submitted by:	Edwin Groothuis <edwin(at)mavetju.org>
    Sponsored by:   FreeBSD Bug-a-thon #1

--- Mk/bsd.tcl.mk
+++ Mk/bsd.tcl.mk
@@ -9,37 +9,40 @@
 Tcl_Pre_Include=		bsd.tcl.mk
 Tcl_Include_MAINTAINER=	ports@FreeBSD.org
 
-# USE_TCL:		Depend on tcl to run. In case of incompatible APIs of different 
-#				TCL versions the version can be specified directly. If version
-#				is not specified (USE_TCL=yes) then the latest version is
-#				used (8.4 currently).
-#				Available values are: 84, 83, 82, 81 and 80.
+# USE_TCL		- Depend on tcl to run. In case of incompatible APIs of
+#				  different TCL versions the version can be specified directly.
+#				  If version is not specified (USE_TCL=yes) then the latest
+#				  version is used (8.4 currently).
+#				  Available values are: 84, 83, 82, 81 and 80.
 #
-# USE_TK:		Depend on tk to run. In case of incompatible APIs of different
-#				TK versions the version can be specified directly. If version
-#				is not specified (USE_TK=yes) then the latest version is
-#				used (8.4 currently).
-#				Available values are: 84, 83, 82, 81 and 80.
+# USE_TK		- Depend on tk to run. In case of incompatible APIs of different
+#				  TK versions the version can be specified directly. If version
+#				  is not specified (USE_TK=yes) then the latest version is
+#				  used (8.4 currently).
+#				  Available values are: 84, 83, 82, 81 and 80.
 ##
-# TCL_LIBDIR:		Path where tcl libraries can be found
+# TCL_LIBDIR	- Path where tcl libraries can be found
 #
-# TCL_INCLUDEDIR: 	Path where tcl C headers can be found
+# TCL_INCLUDEDIR
+#				- Path where tcl C headers can be found
 ##
-# TK_LIBDIR:		Path where tk libraries can be found
+# TK_LIBDIR		- Path where tk libraries can be found
 #
-# TK_INCLUDEDIR: 	Path where tk C headers can be found
+# TK_INCLUDEDIR - Path where tk C headers can be found
 ##
-# TCLSH:		Path to tclsh executable respecting tcl version
+# TCLSH			- Path to tclsh executable respecting tcl version
 #
-# WISH:			Path to wish executable respecting tk version
+# WISH			- Path to wish executable respecting tk version
 ##
-# PATCH_TCL_SCRIPTS: 	List of tcl scripts that need to be patched to replace
-# 				tclsh calls with tclsh${TK_VER} calls. Also note that
-# 				post-patch target is used.
+# PATCH_TCL_SCRIPTS
+#				- List of tcl scripts that need to be patched to replace
+# 				  tclsh calls with tclsh${TK_VER} calls. Also note that
+# 				  post-patch target is used.
 #
-# PATCH_TK_SCRIPTS: 	List of tcl scripts that need to be patched to replace
-# 				wish calls with wish${TK_VER} calls. Also note that
-# 				post-patch target is used.
+# PATCH_TK_SCRIPTS
+#				- List of tcl scripts that need to be patched to replace
+# 				  wish calls with wish${TK_VER} calls. Also note that
+# 				  post-patch target is used.
 
 .if defined(USE_TCL) || defined(USE_TCL_BUILD)
 
