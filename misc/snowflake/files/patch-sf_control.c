73feb8f41620f657d41cf638a36e15207e74c9c5
Martin Wilke <miwi@FreeBSD.org>
2006-10-07 15:24:09 +0000

    - Fix build with gcc41
    PR:		ports/104110
    Submitted by:	trasz <trasz@pin.if.uz.zgora.pl>

--- misc/snowflake/files/patch-sf_control.c
+++ misc/snowflake/files/patch-sf_control.c
@@ -1,6 +1,17 @@
---- sf_control.c.orig	Mon Dec 21 08:18:55 1998
-+++ sf_control.c	Sat Nov  8 21:20:24 2003
-@@ -266,7 +266,7 @@
+--- sf_control.c.orig	Mon Dec 21 00:18:55 1998
++++ sf_control.c	Sat Oct  7 16:20:33 2006
+@@ -15,9 +15,8 @@
+ #include <sys/shm.h>
+ #include <sys/sem.h>
+ 
+-#include "sf.h"
+-
+ #include "getopt.h"
++#include "sf.h"
+ 
+ /* for iconify_window(): */
+ #include <X11/Xlib.h>
+@@ -266,7 +265,7 @@
  static char        xpm_filespec[MAX_FILESPEC+1]    = { 0 };
  
  static char        design_filespec[MAX_FILESPEC+1] = { 0 };
