a2082a523b410b2f5f3033c8d799c7a4136f8faf
Dirk Meyer <dinoex@FreeBSD.org>
2006-10-08 08:44:44 +0000

    - make gcc41 happy

--- misc/estic/files/patch-coll.h
+++ misc/estic/files/patch-coll.h
@@ -1,19 +1,26 @@
---- spunk/coll.h.orig	Sat Feb 26 17:44:32 2000
-+++ spunk/coll.h	Sat Feb 26 17:45:58 2000
+--- spunk/coll.h.orig	Wed Dec 18 23:42:14 1996
++++ spunk/coll.h	Sun Oct  8 10:19:16 2006
+@@ -24,7 +24,7 @@
+ 
+ 
+ #include <stdlib.h>
+-#include <iostream.h>
++#include <iostream>
+ 
+ #include "check.h"
+ #include "object.h"
 @@ -33,8 +33,8 @@
  
  
  
 -static const coIndexError	= 1;		// Index out of range
 -static const coOverflowError	= 2;		// Collection overflow
-+static const int coIndexError		= 1;	// Index out of range
-+static const int coOverflowError	= 2;	// Collection overflow
++static const int coIndexError		= 1;		// Index out of range
++static const int coOverflowError	= 2;		// Collection overflow
  
  
  
---- spunk/coll.h.orig	Thu Dec 23 20:58:57 2004
-+++ spunk/coll.h	Thu Dec 23 20:50:11 2004
-@@ -418,15 +418,15 @@
+@@ -479,15 +479,15 @@
  
      // Duplicates allowed. Do a linear search.
      // (Hint: Search returns the first of all entrys with the same key)
@@ -32,7 +39,7 @@
  
      // Item not found
      return -1;
-@@ -461,7 +461,7 @@
+@@ -522,7 +522,7 @@
  {
      // do a binary search
      int First = 0;
@@ -41,7 +48,7 @@
      int Current;
      int Result;
      int S = 0;
-@@ -472,7 +472,7 @@
+@@ -533,7 +533,7 @@
  	Current = (Last + First) / 2;
  
  	// Do a compare
@@ -50,7 +57,7 @@
  	if (Result < 0) {
  	    First = Current + 1;
  	} else {
-@@ -516,7 +516,7 @@
+@@ -577,7 +577,7 @@
      int I;
      if (Search (Key, I) != 0) {
  	// We found the key, I is the index
