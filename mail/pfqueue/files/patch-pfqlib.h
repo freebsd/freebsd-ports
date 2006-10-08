5b33357908d8395ec08baaeda9983850f43ed53c
Marcus Alves Grando <mnag@FreeBSD.org>
2006-10-08 02:45:58 +0000

    - Update to 0.5.3 - portlint(1)

---
+++ mail/pfqueue/files/patch-pfqlib.h
@@ -0,0 +1,10 @@
+--- pfqlib.h.orig	Sat Oct  7 23:40:07 2006
++++ pfqlib.h	Sat Oct  7 23:40:42 2006
+@@ -7,6 +7,7 @@
+ #define __PFQLIB_H
+ 
+ #include <time.h>
++#include <sys/types.h>
+ #include <regex.h>
+ #include <sys/types.h>
+ #include "pfregex.h"
