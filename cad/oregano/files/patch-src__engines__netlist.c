bdc6a2c0df550060d6dc21f50f360f70bddf29df
Alejandro Pulver <alepulver@FreeBSD.org>
2006-10-09 00:35:08 +0000

    - Update to version 0.60.0.
    PR:		ports/104170
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- cad/oregano/files/patch-src__engines__netlist.c
+++ cad/oregano/files/patch-src__engines__netlist.c
@@ -1,14 +1,13 @@
---- src/simulate.c.orig	Wed May 24 14:16:30 2006
-+++ src/simulate.c	Wed May 24 21:39:29 2006
-@@ -188,10 +188,10 @@
+--- src/engines/netlist.c.orig	Sat Sep 30 13:47:23 2006
++++ src/engines/netlist.c	Sat Sep 30 21:26:42 2006
+@@ -628,9 +628,9 @@
  	GList *parts;
  	GList *p;
  	gchar *prop, *type, *ac;
 -	parts = node_store_get_parts (store);
  	GString *out;
  	gchar *ret;
- 
 +	parts = node_store_get_parts (store);
+ 
  	out = g_string_new ("");
  
- 	for(p=parts; p; p = p->next) {
