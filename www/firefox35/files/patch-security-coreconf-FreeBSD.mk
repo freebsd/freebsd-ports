bb0030cf997d4640b23a509fef80a4eb946935b7
ahze <ahze@FreeBSD.org>
2006-10-09 00:04:49 +0000

    - Update to 2.0rc2

--- www/firefox35/files/patch-security-coreconf-FreeBSD.mk
+++ www/firefox35/files/patch-security-coreconf-FreeBSD.mk
@@ -1,6 +1,6 @@
---- security/coreconf/FreeBSD.mk.orig	Wed Mar 26 20:17:25 2003
-+++ security/coreconf/FreeBSD.mk	Sun Nov 21 22:59:49 2004
-@@ -43,8 +43,12 @@
+--- security/coreconf/FreeBSD.mk.orig	Mon Sep 25 18:26:23 2006
++++ security/coreconf/FreeBSD.mk	Mon Sep 25 18:27:03 2006
+@@ -45,8 +45,12 @@
  ifeq ($(OS_TEST),alpha)
  CPU_ARCH		= alpha
  else
@@ -13,16 +13,12 @@
  
  OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
  
-@@ -71,7 +75,11 @@
+@@ -73,7 +77,7 @@
  DLL_SUFFIX		= so.1.0
  endif
  
 -MKSHLIB			= $(CC) $(DSO_LDOPTS)
-+ifneq (,$(filter alpha ia64,$(OS_TEST)))
 +MKSHLIB			= $(CC) -Wl,-Bsymbolic -lc $(DSO_LDOPTS) -o $@
-+else
-+MKSHLIB			= $(CC) -Wl,-Bsymbolic $(DSO_LDOPTS) -o $@
-+endif
  ifdef MAPFILE
- # Add LD options to restrict exported symbols to those in the map file
+ 	MKSHLIB += -Wl,--version-script,$(MAPFILE)
  endif
