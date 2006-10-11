960e3b3430c66abf425eaf7477a2681522cf0d27
Xin LI <delphij@FreeBSD.org>
2006-10-11 16:20:45 +0000

    Fix build with Python 2.5 by adding encoding information on two files, as requested by pep-0263.
    Approved by:	portmgr/maintainer (linimon)

---
+++ dns/py-dns/files/patch-DNS-Lib.py
@@ -0,0 +1,7 @@
+--- DNS/Lib.py.origWed Oct 11 05:45:08 2006
++++ DNS/Lib.pyWed Oct 11 05:46:19 2006
+@@ -1,3 +1,4 @@
++# This Python file uses the following encoding: iso8859-1
+ """
+  $Id: Lib.py,v 1.11 2002/03/19 13:05:02 anthonybaxter Exp $
+ 
