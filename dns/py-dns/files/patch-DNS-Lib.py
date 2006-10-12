c32ac55c8b8bd18aeeb78feb8b60533e1433e66f
delphij <delphij@FreeBSD.org>
2006-10-12 10:52:26 +0000

    Oops, it seems that the tabs were being mangled while I copy and paste them on my MacBook, so fix it.
    Pointy hat to:	delphij
    Approved by:	portmgr (implicit)

--- dns/py-dns/files/patch-DNS-Lib.py
+++ dns/py-dns/files/patch-DNS-Lib.py
@@ -1,5 +1,5 @@
---- DNS/Lib.py.origWed Oct 11 05:45:08 2006
-+++ DNS/Lib.pyWed Oct 11 05:46:19 2006
+--- DNS/Lib.py.orig	Wed Oct 11 05:45:08 2006
++++ DNS/Lib.py	Wed Oct 11 05:46:19 2006
 @@ -1,3 +1,4 @@
 +# This Python file uses the following encoding: iso8859-1
  """
--- dns/py-dns/files/patch-DNS-Type.py
+++ dns/py-dns/files/patch-DNS-Type.py
@@ -1,5 +1,5 @@
---- DNS/Type.py.origWed Oct 11 05:44:59 2006
-+++ DNS/Type.pyWed Oct 11 05:46:26 2006
+--- DNS/Type.py.orig	Wed Oct 11 05:44:59 2006
++++ DNS/Type.py	Wed Oct 11 05:46:26 2006
 @@ -1,3 +1,4 @@
 +# This Python file uses the following encoding: iso8859-1
  """
