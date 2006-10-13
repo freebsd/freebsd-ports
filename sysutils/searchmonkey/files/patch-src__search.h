44769616e58acbe266e1eb480e8af3c48a438e2e
Andrew Pantyukhin <sat@FreeBSD.org>
2006-10-13 16:52:18 +0000

    - Fix build by including a missing header (pthread.h)
    Approved by:	portmgr (marcus)

--- sysutils/searchmonkey/files/patch-src__search.h
+++
@@ -1,7 +0,0 @@
---- ./src/search.h.orig	Wed Aug 30 17:40:29 2006
-+++ ./src/search.h	Wed Aug 30 17:40:40 2006
-@@ -1,3 +1,4 @@
-+#include <sys/types.h>
- #include <gtk/gtk.h>
- #include <regex.h>
- 
