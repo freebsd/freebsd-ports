7de963409943bba48b4b88127da49d13cc8d67a6
Joe Marcus Clarke <marcus@FreeBSD.org>
2006-10-14 09:13:38 +0000

    Add akamaru, a physics engine prototype.
    Akamaru is a simple, but fun, physics engine prototype. It's named after the
    super awesome ninja dog, Akamaru, from the Naruto anime and based on the
    article Advanced Character Physics by Thomas Jakobsen on Verlet integration.
    It contains a very cool dock, called kiba.
    
    Submitted by:	Phillip Neumann <pneumann@gmail.com>
    Approved by:	portmgr (implicit, kris)

---
+++ deskutils/akamaru/files/patch-populate-dock.sh
@@ -0,0 +1,27 @@
+--- populate-dock.sh.orig	Sun Jul 16 01:37:05 2006
++++ populate-dock.sh	Wed Oct 11 22:29:57 2006
+@@ -3,18 +3,16 @@
+ # Simple shell script to populate the dock by setting up some random
+ # standard applications.
+ 
+-make install-schemas
++GAPPs=/usr/local/share/gnome/applications
++APPs=/usr/local/share/applications
+ 
+ while read n file; do
+     key=/apps/kiba/launchers/$n/file
+     gconftool-2 --set $key --type string $file
+     gconftool-2 --apply-schema /schemas/apps/kiba/launchers/file $key
+ done <<EOF
+-    0 /usr/share/applications/gnome-terminal.desktop
+-    1 /usr/share/applications/gimp-2.2.desktop
+-    2 /usr/share/applications/epiphany.desktop
+-    3 /usr/share/applications/gaim.desktop
+-    4 /usr/share/applications/gnome-cd.desktop
+-    5 /usr/share/applications/background.desktop
+-    6 /usr/share/applications/gnome-file-roller.desktop
++    0 $GAPPs/gnome-terminal.desktop
++    1 $GAPPs/gedit.desktop
++    2 $APPs/firefox.desktop
++    3 $GAPPs/evolution-2.8.desktop
+ EOF
