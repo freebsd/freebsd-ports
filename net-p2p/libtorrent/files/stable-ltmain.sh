dcaeabed38a9aac164ee79c004e98bbbd697e5f8
Florent Thoumie <flz@FreeBSD.org>
2006-10-26 20:11:45 +0000

    - Update libtorrent to 0.10.3.
    Approved by:	portmgr (erwin)

--- net-p2p/libtorrent/files/stable-ltmain.sh
+++ net-p2p/libtorrent/files/stable-ltmain.sh
@@ -1,19 +1,11 @@
---- ltmain.sh.orig
-+++ ltmain.sh
-@@ -976,6 +976,7 @@
- 	;;
- 
-       -avoid-version)
-+	build_old_libs=no
- 	avoid_version=yes
- 	continue
- 	;;
-@@ -4330,10 +4331,12 @@
+--- ltmain.sh.orig	Wed Jul 12 10:35:32 2006
++++ ltmain.sh	Wed Jul 12 10:36:46 2006
+@@ -2279,10 +2279,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
 +	if /usr/bin/false; then
- 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	name=`$ECHO "X$file" | $Xsed -e 's%^.*/%%'`
  	instname="$dir/$name"i
  	$show "$install_prog $instname $destdir/$name"
  	$run eval "$install_prog $instname $destdir/$name" || exit $?
@@ -21,3 +13,11 @@
  
  	# Maybe install the static library, too.
  	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
+@@ -2896,6 +2898,7 @@
+ 	;;
+ 
+       -avoid-version)
++	build_old_libs=no
+ 	avoid_version=yes
+ 	continue
+ 	;;
