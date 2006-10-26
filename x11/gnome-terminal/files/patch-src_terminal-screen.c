2e8b169f4520747b7ba89fdfaf967cee5e4ec71c
jylefort <jylefort@FreeBSD.org>
2006-10-26 08:14:09 +0000

    Fix the obnoxious tab-related resize bugs.
    PR:		ports/104662
    Submitted by:	Thomas Vogt <thomas@bsdunix.ch>
    Approved by:	portmgr (marcus)

--- x11/gnome-terminal/files/patch-src_terminal-screen.c
+++ x11/gnome-terminal/files/patch-src_terminal-screen.c
@@ -1,6 +1,31 @@
---- src/terminal-screen.c.orig	Wed May 17 17:02:36 2006
-+++ src/terminal-screen.c	Thu May 18 15:30:27 2006
-@@ -278,22 +278,22 @@ terminal_screen_init (TerminalScreen *sc
+--- src/terminal-screen.c.orig	Wed Oct 18 15:50:28 2006
++++ src/terminal-screen.c	Wed Oct 18 15:49:42 2006
+@@ -244,6 +244,10 @@
+       GtkAllocation *allocation)
+ {
+   GtkWidget *child;
++  GtkAllocation old_allocation;
++
++  old_allocation.width = widget->allocation.width;
++  old_allocation.height = widget->allocation.height;
+ 
+   widget->allocation = *allocation;
+ 
+@@ -251,6 +255,13 @@
+   g_assert (child != NULL);
+ 
+   gtk_widget_size_allocate (child, allocation);
++
++  if (old_allocation.width != allocation->width ||
++      old_allocation.height != allocation->height)
++    {
++      GtkWidget *term = TERMINAL_SCREEN (widget)->priv->term;
++      gtk_widget_queue_resize_no_redraw (term);
++    }
+ }
+ 
+ static void
+@@ -278,22 +289,22 @@
  #define URLPATH   "/[" PATHCHARS "]*[^]'.}>) \t\r\n,\\\"]"
  
    terminal_widget_match_add (screen->priv->term,
@@ -30,4 +55,28 @@
 +			     "@[" HOSTCHARS ".]+(:[0-9]+)?[[:>:]]", FLAVOR_AS_IS);
  
    terminal_screen_setup_dnd (screen);
+ 
+@@ -548,7 +559,6 @@
+        */
+       terminal_screen_update_scrollbar (screen);
+       terminal_window_update_icon (screen->priv->window);
+-      terminal_window_update_geometry (screen->priv->window);
+     }
+   
+   if (GTK_WIDGET_REALIZED (screen->priv->term))
+@@ -909,7 +919,6 @@
+   
+   terminal_widget_set_allow_bold (term,
+                                   terminal_profile_get_allow_bold (profile));
+-  terminal_window_set_size (screen->priv->window, screen, TRUE);
+ }
+ 
+ static void
+@@ -1019,6 +1028,7 @@
+ GtkWidget*
+ terminal_screen_get_widget (TerminalScreen *screen)
+ {
++  if (screen == NULL) return NULL;
+   return screen->priv->term;
+ }
  
