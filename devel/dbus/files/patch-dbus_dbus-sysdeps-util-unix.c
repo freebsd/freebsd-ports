7b1e0fc98007f16cd31f7a64a3e8f5d4d54b5fae
Joe Marcus Clarke <marcus@FreeBSD.org>
2006-10-27 02:05:30 +0000

    Add a missing #include to correct a problem where dbus would refuse to launch if /usr/local or /usr/X11R6 were NFS mounts.  The error seen would be:
    Failed to start message bus: Can't calculate buffer size when reading directory
    
    Approved by:	portmgr (implicit)

--- devel/dbus/files/patch-dbus_dbus-sysdeps-util-unix.c
+++ devel/dbus/files/patch-dbus_dbus-sysdeps-util-unix.c
@@ -1,6 +1,14 @@
---- dbus/dbus-sysdeps-util-unix.c.orig	Sat Oct  7 12:33:47 2006
-+++ dbus/dbus-sysdeps-util-unix.c	Sat Oct  7 12:34:13 2006
-@@ -502,7 +502,7 @@ static dbus_bool_t
+--- dbus/dbus-sysdeps-util-unix.c.orig	Thu Oct 26 22:02:29 2006
++++ dbus/dbus-sysdeps-util-unix.c	Thu Oct 26 22:02:52 2006
+@@ -42,6 +42,7 @@
+ #include <sys/socket.h>
+ #include <dirent.h>
+ #include <sys/un.h>
++#include <limits.h>
+ 
+ #ifndef O_BINARY
+ #define O_BINARY 0
+@@ -502,7 +503,7 @@ static dbus_bool_t
  dirent_buf_size(DIR * dirp, size_t *size)
  {
   long name_max;
