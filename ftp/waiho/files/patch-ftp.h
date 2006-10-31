7f2617c58e65e384038437ebe5bfe32cf50d7159
dinoex <dinoex@FreeBSD.org>
2006-10-31 05:46:22 +0000

    Waiho is a simple FTP client for GNUstep, designed to be easy to use. While it's far from finished, it's already quite useable.
    WWW: http://www.roard.com/waiho/

---
+++ ftp/waiho/files/patch-ftp.h
@@ -0,0 +1,10 @@
+--- ftp.h.orig	Thu Aug  1 18:27:55 2002
++++ ftp.h	Sat Oct 21 21:09:28 2006
+@@ -25,6 +25,7 @@
+ 
+ #include <errno.h>
+ #include <netdb.h>
++#include <time.h>
+ 
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
