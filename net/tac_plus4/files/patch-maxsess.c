7bc99fa31618c58e4d93529894904fb9a28de24b
marcus <marcus@FreeBSD.org>
2006-10-31 02:42:33 +0000

    * Update to F4.0.4.13 * Configure tac_plus to run as the user tacacs after starting up [1] * Convert to USE_RC_SUBR
    Requested by:	Ryan Steinmetz <rpsfa@rit.edu>

--- net/tac_plus4/files/patch-maxsess.c
+++ net/tac_plus4/files/patch-maxsess.c
@@ -1,29 +1,14 @@
-Using tacacs I found that ckfinger() function from maxsess.c module
-returns wrong count of current sessions for users with "maxsess"
-parameter established in tac_plus.conf. It happens if Cisco access 
-server works with IOS v 12.x.
-On the other hand ckfinger() works well with IOS v 11.x
-
-Here are patches for both maxsess.c and port's Makefile to fix 
-this problem (but it is just workaround, ckfinger() should be 
-fully rewritten).
-
-Best regards,
-Sergey E. Levov (serg@informika.ru)
-
-*** maxsess.c.orig	Fri Jan 19 17:16:46 2001
---- maxsess.c	Fri Jan 19 17:25:51 2001
-***************
-*** 470,476 ****
---- 470,480 ----
-  	}
-  	/* Extract username, up to 10 chars wide, starting at char 13 */
-  	nmlen = 0;
-+ #if (TAC_IOS_VERSION == 11)
-  	name = p + 13;
-+ #else
-+ 	name = p + 15;
-+ #endif		
-  	for (i = 0; *name && !isspace(*name) && (i < 10); i++) {
-  	    nmbuf[nmlen++] = *name++;
-  	}
+--- maxsess.c.orig	Tue Jul 18 13:53:34 2006
++++ maxsess.c	Sun Oct 29 02:52:16 2006
+@@ -464,7 +464,11 @@ ckfinger(char *user, char *nas, struct i
+ 	}
+ 	/* Extract username, up to 10 chars wide, starting at char 13 */
+ 	nmlen = 0;
++#if (TAC_IOS_VERSION == 11)
+ 	name = p + 13;
++#else
++	name = p + 15;
++#endif
+ 	for (i = 0; *name && !isspace((int) *name) && (i < 10); i++) {
+ 	    nmbuf[nmlen++] = *name++;
+ 	}
