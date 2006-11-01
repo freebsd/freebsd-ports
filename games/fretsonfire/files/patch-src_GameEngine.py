61c3c7b866bb31161dcdfc54b41df8a450ccba68
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2006-11-01 07:46:37 +0000

    - Update to 1.0.263 - Remove pkg-install scripts - See the full changelog at:   http://louhi.kempele.fi/~skyostil/uv/fretsonfire/fretsonfire-1-0-263.blog/

--- games/fretsonfire/files/patch-src_GameEngine.py
+++ games/fretsonfire/files/patch-src_GameEngine.py
@@ -1,11 +1,11 @@
---- src/GameEngine.py	Tue Oct  3 09:25:05 2006
-+++ src/GameEngine.py	Tue Oct  3 09:25:33 2006
-@@ -46,7 +46,7 @@
- Config.define("engine", "tickrate",     float, 1.0)
- Config.define("game",   "uploadscores", bool,  False, text = "Upload Highscores",    options = {False: "No", True: "Yes"})
+--- src/GameEngine.py	Wed Nov  1 01:00:46 2006
++++ src/GameEngine.py	Wed Nov  1 01:01:06 2006
+@@ -49,7 +49,7 @@
+ Config.define("game",   "uploadscores", bool,  False, text = _("Upload Highscores"),    options = {False: _("No"), True: _("Yes")})
  Config.define("game",   "uploadurl",    str,   "http://kempele.fi/~skyostil/python/fretsonfire/upload")
--Config.define("video",  "fullscreen",   bool,  True,  text = "Fullscreen Mode",      options = {False: "No", True: "Yes"})
-+Config.define("video",  "fullscreen",   bool,  False,  text = "Fullscreen Mode",      options = {False: "No", True: "Yes"})
- Config.define("video",  "multisamples", int,   4,     text = "Antialiasing Quality", options = {0: "None", 2: "2x", 4: "4x", 6: "6x", 8: "8x"})
+ Config.define("game",   "leftymode",    bool,  False, text = _("Lefty mode"),           options = {False: _("No"), True: _("Yes")})
+-Config.define("video",  "fullscreen",   bool,  True,  text = _("Fullscreen Mode"),      options = {False: _("No"), True: _("Yes")})
++Config.define("video",  "fullscreen",   bool,  False,  text = _("Fullscreen Mode"),      options = {False: _("No"), True: _("Yes")})
+ Config.define("video",  "multisamples", int,   4,     text = _("Antialiasing Quality"), options = {0: _("None"), 2: _("2x"), 4: _("4x"), 6: _("6x"), 8: _("8x")})
  Config.define("video",  "resolution",   str,   "640x480")
- Config.define("video",  "fps",          int,   80,    text = "Frames per Second", options = dict([(n, n) for n in range(0, 120)]))
+ Config.define("video",  "fps",          int,   80,    text = _("Frames per Second"), options = dict([(n, n) for n in range(0, 120)]))
