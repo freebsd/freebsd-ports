3617e760969f3a15aa092bc5f81848061dc84801
Michael Johnson <ahze@FreeBSD.org>
2006-11-03 22:10:18 +0000

    Fix x264

--- multimedia/mplayer/files/patch-libmpcodecs_ve_x264.c
+++ multimedia/mplayer/files/patch-libmpcodecs_ve_x264.c
@@ -1,5 +1,5 @@
---- libmpcodecs/ve_x264.c.orig	Sun Jun 11 20:35:41 2006
-+++ libmpcodecs/ve_x264.c	Sun Aug 20 12:11:22 2006
+--- libmpcodecs/ve_x264.c.orig	Sun Jun 11 18:35:41 2006
++++ libmpcodecs/ve_x264.c	Fri Nov  3 17:05:22 2006
 @@ -208,7 +208,7 @@
  };
  
@@ -9,17 +9,19 @@
      int i;
      if (!str) return 0;
      for (i = 0; i < length; i++) {
-@@ -247,7 +247,10 @@
+@@ -247,8 +247,11 @@
      mod->param.i_deblocking_filter_beta = deblockbeta;
      mod->param.b_cabac = cabac;
  
 +    mod->param.rc.i_rc_method = X264_RC_CQP;
      mod->param.rc.i_qp_constant = qp_constant;
+-    mod->param.rc.i_rf_constant = rf_constant;
 +    if(rf_constant > 0)
 +        mod->param.rc.i_rc_method = X264_RC_CRF;
-     mod->param.rc.i_rf_constant = rf_constant;
++    mod->param.rc.f_rf_constant = rf_constant;
      if(qp_min > qp_constant)
          qp_min = qp_constant;
+     if(qp_max < qp_constant)
 @@ -275,7 +278,7 @@
                     "VBV requires both vbv_maxrate and vbv_bufsize.\n");
              return 0;
