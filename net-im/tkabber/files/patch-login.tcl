38312622b8a015fc256c5af3572e1555185f1b9a
Rong-En Fan <rafan@FreeBSD.org>
2006-11-05 12:38:04 +0000

    - Update to 0.9.9
    PR:		ports/105172
    Submitted by:	Toni Schmidbauer

--- net-im/tkabber/files/patch-login.tcl
+++ net-im/tkabber/files/patch-login.tcl
@@ -1,15 +1,18 @@
---- login.tcl.orig	Tue Aug  3 14:35:36 2004
-+++ login.tcl	Tue Aug  3 14:35:54 2004
-@@ -36,10 +36,10 @@
- custom::defvar loginconf(usedigest) 1 \
-     [::msgcat::mc "Use hashed password transmission."] \
-     -group Login -type boolean
--custom::defvar loginconf(resource) "tkabber" \
-+custom::defvar loginconf(resource) "TKabber" \
-     [::msgcat::mc "Resource."] \
+--- login.tcl.orig      Tue Oct 31 13:08:19 2006
++++ login.tcl   Tue Oct 31 13:08:45 2006
+@@ -46,13 +46,13 @@
+ custom::defvar loginconf(user) "" \
+     [::msgcat::mc "User name."] \
      -group Login -type string
 -custom::defvar loginconf(server) "localhost" \
 +custom::defvar loginconf(server) "jabber.ru" \
      [::msgcat::mc "Server name."] \
      -group Login -type string
- custom::defvar loginconf(port) "5222" \
+ custom::defvar loginconf(password) "" \
+     [::msgcat::mc "Password."] \
+     -group Login -type password
+-custom::defvar loginconf(resource) "tkabber" \
++custom::defvar loginconf(resource) "TKabber" \
+     [::msgcat::mc "Resource."] \
+     -group Login -type string
+ custom::defvar loginconf(priority) "8" \
