a23c2dee38c0db3c3f942c778300861a69379e89
Martin Wilke <miwi@FreeBSD.org>
2006-11-07 14:29:04 +0000

    - Update to 0.72
    PR:		ports/104206
    Submitted by:	Li-Wen Hsu <lwhsu@lwhsu.org>
    Approved by:	maintainer timeout (this commit is approved by clsung on irc)

--- net/mtr/files/patch-dns.c
+++ net/mtr/files/patch-dns.c
@@ -1,5 +1,5 @@
---- dns.c.orig	Thu Mar 23 02:45:58 2006
-+++ dns.c	Wed May  3 18:48:01 2006
+--- dns.c.orig	Mon Oct  9 16:11:26 2006
++++ dns.c	Mon Oct  9 16:11:46 2006
 @@ -48,11 +48,13 @@
  #include "dns.h"
  #include "net.h"
@@ -14,12 +14,3 @@
  
  /*  Hmm, it seems Irix requires this  */
  extern int errno;
-@@ -1357,7 +1359,7 @@
- #ifdef ENABLE_IPV6
- /* Returns an ip6.arpa character string. */
- void addr2ip6arpa( ip_t * ip, char * buf ) {
--  char * p = (char *) ip;
-+  unsigned char * p = (unsigned char *) ip;
-   char * b = buf;
-   int i;
- 
