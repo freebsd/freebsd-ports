3172941d906378863a8ccb2df0290d592dbd5fd0
miwi <miwi@FreeBSD.org>
2006-11-08 19:33:45 +0000

    Initially UProxy has been designed to allow playing Unreal Tournament matches over Internet on computers connected by a LAN sharing a dial up connection, but it could be used to play other games (such as Quake) and by every program using a UDP protocol where clients don't have to bind a fixed port (not only games!).
    WWW: http://www.geocities.com/SiliconValley/Vista/8155/uproxy/
    
    PR:		ports/104697
    Submitted by:	Alexander Logvinov <ports@logvinov.com>

---
+++ net/uproxy/files/patch-uproxy.c
@@ -0,0 +1,24 @@
+--- uproxy.c.orig	Thu Aug 10 11:05:10 2006
++++ uproxy.c	Fri Oct 20 13:17:01 2006
+@@ -25,11 +25,11 @@
+     #include <winsock.h>
+ #else
+     #include <sys/ioctl.h>
++    #include <sys/types.h>
+     #include <sys/socket.h>
+     #include <netinet/in.h>
+     #include <arpa/inet.h>
+     #include <sys/time.h>
+-    #include <sys/types.h>
+     #include <unistd.h>
+     #include <stdlib.h>
+ #endif
+@@ -50,7 +50,7 @@
+ 
+ SOCKET ProxySocket;
+ struct sockaddr_in ProxySAddr, ClientSAddr, TargetSAddr;
+-struct sockaddr AnySAddr={AF_INET, {INADDR_ANY}};
++struct sockaddr AnySAddr={AF_INET, INADDR_ANY};
+ struct ClientList
+ {
+     SOCKET sock;
