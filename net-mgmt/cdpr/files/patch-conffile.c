83c3b662a56a8041bee15b69668eb4b1b2897747
Michael Landin <mich@FreeBSD.org>
2006-11-08 14:04:29 +0000

    - fix build failure on 4.x
    Notified by:	pointyhat

---
+++ net-mgmt/cdpr/files/patch-conffile.c
@@ -0,0 +1,22 @@
+
+$FreeBSD$
+
+--- conffile.c.orig
++++ conffile.c
+@@ -57,6 +57,7 @@
+ 	struct hostent *h;
+ 	char *addy;
+ 	int port;
++	char *tport;
+ 
+ 	if (ip && url)
+ 	{
+@@ -70,7 +71,7 @@
+ 		else
+ 		{
+ 			strtok(ip,":");
+-			char *tport = strtok(NULL,":");
++			tport = strtok(NULL,":");
+ 			port = atoi(tport);
+ 		}
+ /*
