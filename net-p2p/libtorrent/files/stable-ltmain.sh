7bbcef2155f401e508af345cc6df460da1bd7dc0
Florent Thoumie <flz@FreeBSD.org>
2006-11-08 12:10:46 +0000

    Update libtorrent and libtorrent-devel to 0.10.4.
    PR:		ports/105230
    Submitted by:	Gea-Suan Lin <gslin@gslin.org>

--- net-p2p/libtorrent/files/stable-ltmain.sh
+++ net-p2p/libtorrent/files/stable-ltmain.sh
@@ -1,23 +1,10 @@
---- ltmain.sh.orig	Wed Jul 12 10:35:32 2006
-+++ ltmain.sh	Wed Jul 12 10:36:46 2006
-@@ -2279,10 +2279,12 @@
- 	fi
- 
- 	# Install the pseudo-library for information purposes.
-+	if /usr/bin/false; then
- 	name=`$ECHO "X$file" | $Xsed -e 's%^.*/%%'`
- 	instname="$dir/$name"i
- 	$show "$install_prog $instname $destdir/$name"
- 	$run eval "$install_prog $instname $destdir/$name" || exit $?
-+	fi
- 
- 	# Maybe install the static library, too.
- 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
-@@ -2896,6 +2898,7 @@
+--- ltmain.sh.orig	Fri Oct 27 22:47:02 2006
++++ ltmain.sh	Tue Nov  7 10:33:04 2006
+@@ -1256,6 +1256,7 @@
  	;;
  
        -avoid-version)
-+	build_old_libs=no
++      	build_old_libs=no
  	avoid_version=yes
  	continue
  	;;
