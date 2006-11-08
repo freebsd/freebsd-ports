a1d553eed9c03ad12a805f04948baa01fff3ba2a
marcus <marcus@FreeBSD.org>
2006-11-08 05:46:33 +0000

    Update to 2.16.1.1.

--- games/gnome-games/files/patch-aisleriot_cscmi.c
+++ games/gnome-games/files/patch-aisleriot_cscmi.c
@@ -1,22 +1,12 @@
---- aisleriot/cscmi.c.orig	Wed Oct 18 17:51:31 2006
-+++ aisleriot/cscmi.c	Wed Oct 18 17:51:13 2006
-@@ -603,8 +603,8 @@ cscmi_drag_valid(int slot_id, GList *car
- 			    cscmi_catch_handler,
- 			    NULL);
+--- aisleriot/cscmi.c.orig	Tue Nov  7 17:49:43 2006
++++ aisleriot/cscmi.c	Tue Nov  7 17:49:53 2006
+@@ -637,8 +637,8 @@ cscmi_drop_cards (int start_slot, GList 
+ 			    cscmi_call_lambda,
+ 			    call_data, cscmi_catch_handler, NULL);
    retval = call_data->retval;
 -  g_free (call_data);
    scm_gc_unprotect_object (call_data->arg2);
 +  g_free (call_data);
  
-   return SCM_NFALSEP(retval);
- }
-@@ -698,8 +698,8 @@ cscmi_drop_valid(int start_slot, GList *
- 			    cscmi_catch_handler,
- 			    NULL);
-   retval = call_data->retval;
--  g_free (call_data);
-   scm_gc_unprotect_object (call_data->arg2);
-+  g_free (call_data);
- 
-   return SCM_NFALSEP(retval);
+   return SCM_NFALSEP (retval);
  }
