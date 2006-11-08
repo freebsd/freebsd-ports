7a6d1b993095b36db06d9d3d8409aaacba9e75cb
Frank J. Laszlo <laszlof@FreeBSD.org>
2006-11-08 12:55:59 +0000

    Update to latest version.
    PR:		ports/105258
    Submitted by:	Nicola Vitale <nivit@email.it> (maintainer)
    Approved by:	flz (mentor)

--- math/ump/files/patch-src-main.cpp
+++ math/ump/files/patch-src-main.cpp
@@ -1,11 +1,11 @@
---- src/main.cpp	Sun Jun 25 12:48:31 2006
-+++ src/main.cpp.port	Mon Jul 10 14:08:33 2006
-@@ -682,7 +682,7 @@
+--- src/main.cpp	Mon Oct  2 11:05:33 2006
++++ src/main.cpp.port	Tue Nov  7 22:17:00 2006
+@@ -695,7 +695,7 @@
  
  		setup_file.replace_escape_sequences = true;
  
 -		try{  setup_file.read( DATA_DIR "/ump.conf" );  }
 +		try{  setup_file.read( "%%PREFIX%%/etc/ump.conf" );  }
- 		catch(...) { }
+ 		catch(...) { std_settings = false; }
  
  		snprintf(filename,100,"%s/.ump",getenv("HOME"));
