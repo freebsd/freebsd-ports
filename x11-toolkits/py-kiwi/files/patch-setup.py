89ec18534bbf91a942f408858b15f06e8b83870d
alexbl <alexbl@FreeBSD.org>
2006-11-09 09:59:56 +0000

    - Update to 1.9.11
    PR:		105301
    Submitted by:	Nicola Vitale <nivit@email.it>

--- x11-toolkits/py-kiwi/files/patch-setup.py
+++ x11-toolkits/py-kiwi/files/patch-setup.py
@@ -1,13 +1,15 @@
-$FreeBSD$
---- setup.py	Tue Apr 25 17:09:29 2006
-+++ setup.py.port	Tue Jun 20 13:35:48 2006
-@@ -34,9 +34,6 @@
+--- setup.py	Fri Sep 15 18:39:09 2006
++++ setup.py.port	Wed Nov  8 21:41:48 2006
+@@ -34,12 +34,6 @@
                               'kiwiwidgets', '*.png')),
                    (get_site_packages_dir('gazpacho', 'widgets'),
                     listfiles('gazpacho-plugin', 'kiwiwidgets.py')),
 -                  ('share/doc/kiwi',
 -                   ('AUTHORS', 'ChangeLog', 'NEWS', 'README')),
--                  ('$sysconfdir/kiwi', ['setup.py']),
+-                  ('share/doc/kiwi/howto',
+-                   listfiles('doc/howto/', '*')),
+-                  ('share/doc/kiwi/api',
+-                   listfiles('doc/api/', '*')),
                    ],
        scripts=['bin/kiwi-i18n',
                 'bin/kiwi-ui-test'],
