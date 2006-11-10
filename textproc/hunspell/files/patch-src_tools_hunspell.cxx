803ba73ff0167acb9ffd8157172977d879f6f611
miwi <miwi@FreeBSD.org>
2006-11-10 14:11:20 +0000

    - Update to 1.1.4
    PR:		ports/105350
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu> (maintainer)

--- textproc/hunspell/files/patch-src_tools_hunspell.cxx
+++ textproc/hunspell/files/patch-src_tools_hunspell.cxx
@@ -1,13 +1,14 @@
---- hunspell.cxx.orig	Wed Dec 17 18:50:49 2003
-+++ hunspell.cxx	Thu May 20 15:54:49 2004
-@@ -49,8 +49,8 @@
+
+$FreeBSD$
+
+--- src/tools/hunspell.cxx.orig
++++ src/tools/hunspell.cxx
+@@ -47,7 +47,7 @@
  #include "manparser.hxx"
  #include "firstparser.hxx"
  
 -#define LIBDIR "/usr/share/myspell/"
--#define LIBDIR2 "/usr/share/hunmorph/"
 +#define LIBDIR "%%PREFIX%%/share/myspell/"
-+#define LIBDIR2 "%%PREFIX%%/share/hunmorph/"
  #define HOME getenv("HOME")
  #define DICBASENAME ".hunspell_"
  #define LOGFILE "/tmp/hunspell.log"
