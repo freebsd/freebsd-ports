89717616acd8d3d3e0326d644d6789af102929a3
novel <novel@FreeBSD.org>
2006-11-12 13:52:38 +0000

    Update to 1.4.3.

--- sysutils/conky/files/patch-src-freebsd.c
+++ sysutils/conky/files/patch-src-freebsd.c
@@ -1,11 +1,11 @@
---- src/freebsd.c.orig	Sun Mar 26 01:47:50 2006
-+++ src/freebsd.c	Wed May 24 07:57:35 2006
-@@ -508,7 +508,7 @@
+--- src/freebsd.c.orig	Sun Nov 12 09:23:59 2006
++++ src/freebsd.c	Sun Nov 12 09:24:20 2006
+@@ -290,7 +290,7 @@
+ 	size_t len = sizeof (cp_time);
  
- 	if (GETSYSCTL("dev.cpu.0.freq", freq) == 0)
- 		snprintf(p_client_buffer, client_buffer_size,
--				p_format, freq/divisor);
-+				p_format, (float)freq/(float)divisor);
- 	else
- 		snprintf(p_client_buffer, client_buffer_size, p_format, 0.0f);
- }
+ 	/* add check for !info.cpu_usage since that mem is freed on a SIGUSR1 */
+-	if ((cpu_setup == 0)) || (!info.cpu_usage) {
++	if ((cpu_setup == 0) || (!info.cpu_usage)) {
+ 		get_cpu_count();
+ 		cpu_setup = 1;
+ 	}
