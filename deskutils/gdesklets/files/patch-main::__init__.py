279641f48ab71291b797599186ff0dbd1cb05604
miwi <miwi@FreeBSD.org>
2006-11-15 19:48:26 +0000

    - Update to 0.35.4
    PR:		ports/105522
    Submitted by:	Franz Klammer <klammer@webonaut.com> (maintainer)

--- deskutils/gdesklets/files/patch-main::__init__.py
+++ deskutils/gdesklets/files/patch-main::__init__.py
@@ -1,11 +1,11 @@
---- main/__init__.py.orig	Mon Feb 28 12:11:37 2005
-+++ main/__init__.py	Mon Feb 28 12:12:18 2005
-@@ -20,7 +20,7 @@
-     USERHOME = os.path.join(os.environ["APPDATA"], "gDesklets")
- else:
-     USERHOME = os.path.join(os.path.expanduser("~"), ".gdesklets")
+--- main/__init__.py.orig	Thu Oct  5 16:14:40 2006
++++ main/__init__.py	Tue Nov 14 13:38:57 2006
+@@ -7,7 +7,7 @@
+ # gDesklets home paths
+ HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+ USERHOME = os.path.join(os.path.expanduser("~"), ".gdesklets")
 -OLDHOME = os.path.join(HOME, os.pardir, os.pardir, "share", "gdesklets")
 +OLDHOME = os.path.join(HOME, os.pardir, os.pardir, "share", "gnome", "gdesklets")
  
- 
  # we need the DISPLAY variable
+ try:
