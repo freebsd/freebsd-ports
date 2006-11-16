e67a216b3eb141e288238ed47cb05cc934fcdca8
Shaun Amott <shaun@FreeBSD.org>
2006-11-16 02:43:44 +0000

    Fix port - it compiled, but the daemon failed to start completely.
    Reported by:	Beech Rintoul <beech@alaskaparadise.com> (maintainer)

--- www/cherokee/files/patch-cherokee-connection.c
+++ www/cherokee/files/patch-cherokee-connection.c
@@ -1,33 +1,30 @@
---- cherokee/connection.c.orig	Wed Sep 20 21:17:53 2006
-+++ cherokee/connection.c	Wed Sep 20 21:17:53 2006
-@@ -33,6 +33,12 @@
+--- cherokee/connection.c.orig	Wed Aug 30 17:12:13 2006
++++ cherokee/connection.c	Thu Nov 16 00:41:26 2006
+@@ -33,6 +33,9 @@
  #include <time.h>
  #include <sys/types.h>
  
-+#undef fr_atoll
-+#define fr_atoll(nptr) strtoll(nptr, (char **)NULL, 10)
-+
 +#include <stdlib.h>
 +#include <limits.h>
 +
  #ifdef HAVE_PWD_H
  # include <pwd.h>
  #endif
-@@ -1210,7 +1216,7 @@
+@@ -1210,7 +1213,7 @@
  	}
  	tmp[num_len] = '\0';
  	if (num_len != 0) {
 -		cnt->range_start = atoll (tmp);
-+		cnt->range_start = fr_atoll (tmp);
++		cnt->range_start = strtoll(tmp, (char **)NULL, 10);
  		if (cnt->range_start < 0) {
  			return ret_error;
  		}
-@@ -1236,7 +1242,7 @@
+@@ -1236,7 +1239,7 @@
  			num_len++;
  		}
  		tmp[num_len] = '\0';
 -		cnt->range_end = atoll (tmp);
-+		cnt->range_end = fr_atoll (tmp);
++		cnt->range_end = strtoll(tmp, (char **)NULL, 10);
  		if (cnt->range_end < 1){
  			return ret_error;
  		}
