78ef42e5a26dc0ead13b8344d82c299b972296d3
nork <nork@FreeBSD.org>
2006-11-19 14:05:55 +0000

    o Remove extra AtCfg_freebsd. o Bump PORTREVISION, accordingly.
    Approved by:	kuriyama

--- sysutils/p5-Schedule-At/files/patch-At.pm
+++ sysutils/p5-Schedule-At/files/patch-At.pm
@@ -1,5 +1,5 @@
---- At.pm.orig	Mon Mar 25 20:52:01 2002
-+++ At.pm	Tue May 14 18:15:55 2002
+--- At.pm.orig	Tue Sep 13 16:11:36 2005
++++ At.pm	Sun Nov 19 21:58:16 2006
 @@ -55,6 +55,7 @@
  		print ATCMD "$TAGID$params{TAG}\n" if $params{TAG};
  		print ATCMD $params{COMMAND};
@@ -8,37 +8,3 @@
  	}
  
  	0;
-@@ -258,6 +259,7 @@
- Jose A. Rodriguez (josear@ac.upc.es)
- 
- =cut
-+#' for Emacs face
- 
- ###############################################################################
- # OS dependent code
-@@ -327,6 +329,25 @@
- 		my @fields = split("\t", $_[0]);
- 		($fields[0], substr($fields[1], 0, 16)) 
- 	};
-+}
-+
-+sub AtCfg_freebsd {
-+	$AT{'add'} = 'at %TIME% 2> /dev/null';
-+	$AT{'addFile'} = 'at -f %FILE% %TIME% 2> /dev/null';
-+	$AT{'timeFormat'} = '%HOUR%:%MINS% %MONTH%/%DAY%/%YEAR%';
-+	$AT{'remove'} = 'atrm %JOBID%';
-+	$AT{'getJobs'} = 'atq';
-+	$AT{'headings'} = ['Date'];
-+	$AT{'getCommand'} = 'at -c %JOBID% |';
-+	$AT{'parseJobList'} = 
-+	  sub {
-+	    my @a = split(/\s+/, $_[0]);
-+	    if ($_[0] =~ /^\d\d/) {
-+	      return ($a[4], "$a[0] $a[1]");
-+	    } elsif (defined $a[8]) {
-+	      return ($a[8], "$a[0] $a[1] $a[2] $a[3] $a[4] $a[5]");
-+	    }
-+	  };
- }
- 
- sub AtCfg_aix {
