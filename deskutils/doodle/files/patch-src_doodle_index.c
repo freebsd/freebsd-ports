125760f6cabb2b7dc3c0b8bb63fbc4b9b65d2e50
Martin Wilke <miwi@FreeBSD.org>
2006-11-20 22:03:24 +0000

    - Update to 0.6.5
    PR:		ports/105695
    Submitted by:	Thomas Abthorpe

--- deskutils/doodle/files/patch-src_doodle_index.c
+++ deskutils/doodle/files/patch-src_doodle_index.c
@@ -1,6 +1,14 @@
---- src/doodle/index.c.orig	Tue Sep  6 16:15:31 2005
-+++ src/doodle/index.c	Tue Sep  6 16:15:53 2005
-@@ -54,7 +54,7 @@
+--- src/doodle/index.c.orig	Wed May  3 05:21:21 2006
++++ src/doodle/index.c	Mon Nov 20 11:06:49 2006
+@@ -33,6 +33,7 @@
+ #include "helper2.h"
+ #include "gettext.h"
+ #include "doodle.h"
++#include <signal.h>
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <unistd.h>
+@@ -327,7 +328,7 @@
      char * cpos;
      size_t slen;
  
