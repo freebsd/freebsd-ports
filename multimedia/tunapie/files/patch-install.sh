11fb80b76fb3ac7f48ccd000111d5a4cac55d361
Martin Wilke <miwi@FreeBSD.org>
2006-11-23 11:54:39 +0000

    - Update to 1.3
    PR:		ports/105769
    Submitted by:	nivit@email.it (Nicola Vitale) (maintainer)

--- multimedia/tunapie/files/patch-install.sh
+++ multimedia/tunapie/files/patch-install.sh
@@ -1,20 +1,17 @@
---- install.sh	Sat Nov 11 21:19:00 2006
-+++ install.sh.port	Sun Nov 12 23:10:26 2006
+--- install.sh	Sun Nov 19 13:50:27 2006
++++ install.sh.port	Thu Nov 23 10:12:43 2006
 @@ -1,11 +1,9 @@
  #!/bin/sh
 -if [ "$1" = "--adult" ]; then echo '1'>/etc/tunapie.config 
 -else echo '0'>/etc/tunapie.config
-+if [ "$1" = "--adult" ]; then echo '1'>%%PREFIX%%/etc/tunapie.config.default
-+else echo '0'>%%PREFIX%%/etc/tunapie.config.default
++if [ "$1" = "--adult" ]; then echo '1'>/usr/local/etc/tunapie.config.default
++else echo '0'>/usr/local/etc/tunapie.config.default
  fi
  python src/compile
--cp tunapie /usr/local/bin
+ cp tunapie /usr/local/bin
 -rm -rf /usr/local/share/tunapie
 -mkdir /usr/local/share/tunapie
 -cp src/*.py src/*.pyc src/*.jpg /usr/local/share/tunapie
--cp tunapie.1 /usr/local/man/man1
--cp tunapie.desktop /usr/local/share/applications
-+cp tunapie %%PREFIX%%/bin
-+cp src/*.py src/*.pyc src/*.jpg %%DATADIR%%
-+cp tunapie.1 %%MANPREFIX%%/man/man1
-+cp tunapie.desktop %%DESKTOPDIR%%
++cp src/*.py src/*.pyc src/*.ico /usr/local/share/tunapie
+ cp tunapie.1 /usr/local/man/man1
+ cp tunapie.desktop /usr/local/share/applications
