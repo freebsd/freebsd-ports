3c4e8d4a80fb2eb56efaabd2983ff20544e20703
Rong-En Fan <rafan@FreeBSD.org>
2006-11-26 18:12:11 +0000

    - Repsect X11BASE. This should fix chinese/*ttf build failure with   non-standard X11BASE.
    Reported by:	pointyhat

--- chinese/ttfm/files/patch-ttfm.sh
+++ chinese/ttfm/files/patch-ttfm.sh
@@ -1,5 +1,5 @@
---- ttfm.sh.orig	Wed Jan 31 17:34:46 2001
-+++ ttfm.sh	Sat Feb 19 09:14:56 2005
+--- ./ttfm.sh.orig	Wed Jan 31 17:34:46 2001
++++ ./ttfm.sh	Mon Nov 27 02:03:54 2006
 @@ -258,7 +258,7 @@
  	# Call script with --remove
  	for i in "$INSTALLSCRIPT"; do
