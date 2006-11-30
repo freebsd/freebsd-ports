82e36da73630e231cdc6c4fb27922b959c52b1a3
skv <skv@FreeBSD.org>
2006-11-30 13:32:15 +0000

    Update to 1.2.0
    Changes:	http://code.sixapart.com/svn/memcached/tags/1.2.0/ChangeLog
    PR:		ports/103523
    Submitted by:	skv
    Approved by:	maintainer timeout (68 days)

--- databases/memcached/files/patch-memcached.c
+++ databases/memcached/files/patch-memcached.c
@@ -1,12 +1,12 @@
---- memcached.c.orig	Mon Jul  3 13:04:31 2006
-+++ memcached.c	Mon Jul  3 13:04:40 2006
-@@ -1146,9 +1146,7 @@
-     setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &flags, sizeof(flags));
-     setsockopt(sfd, SOL_SOCKET, SO_KEEPALIVE, &flags, sizeof(flags));
-     setsockopt(sfd, SOL_SOCKET, SO_LINGER, &ling, sizeof(ling));
--#if !defined(TCP_NOPUSH)
-     setsockopt(sfd, IPPROTO_TCP, TCP_NODELAY, &flags, sizeof(flags));
--#endif
+--- memcached.c.orig	Sat Sep  9 21:10:21 2006
++++ memcached.c	Sat Sep 23 21:33:25 2006
+@@ -1706,7 +1706,9 @@
+     } else {
+         setsockopt(sfd, SOL_SOCKET, SO_KEEPALIVE, &flags, sizeof(flags));
+         setsockopt(sfd, SOL_SOCKET, SO_LINGER, &ling, sizeof(ling));
++#if !defined(TCP_NOPUSH)
+         setsockopt(sfd, IPPROTO_TCP, TCP_NODELAY, &flags, sizeof(flags));
++#endif
+     }
  
-     /* 
-      * the memset call clears nonstandard fields in some impementations
+     /*
