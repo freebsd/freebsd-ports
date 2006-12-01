5955ee8c6f9612f195d665ccc889cf273a800c5d
Tilman Keskinoz <arved@FreeBSD.org>
2006-12-01 18:59:36 +0000

    Update to 0.8.3

---
+++ multimedia/kaffeine/files/patch-kaffeine-src-kaffeine.cpp
@@ -0,0 +1,10 @@
+--- kaffeine/src/kaffeine.cpp.orig	Fri Dec  1 19:17:37 2006
++++ kaffeine/src/kaffeine.cpp	Fri Dec  1 19:18:01 2006
+@@ -26,6 +26,7 @@
+ #include "kaffeine.h"
+ 
+ #include <dcopref.h>
++#include <kapplication.h>
+ #include <kkeydialog.h>
+ #include <kfiledialog.h>
+ #include <kdirselectdialog.h>
