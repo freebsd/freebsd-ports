8a32a46bd76c78ca7d42486558a5bd722001de69
miwi <miwi@FreeBSD.org>
2006-12-01 07:49:16 +0000

    - Unbreak on 5.X
    PR:		ports/106110
    Submitted by:	Peter Johnson <johnson.peter@gmail.com>

--- devel/clint/files/patch-python.h
+++ devel/clint/files/patch-python.h
@@ -1,6 +1,29 @@
---- lib/python/python.h.orig	Fri Sep  6 21:53:18 2002
-+++ lib/python/python.h	Fri Sep  6 21:56:38 2002
-@@ -12,13 +12,17 @@
+--- lib/python/python.h.orig	Mon Apr 16 12:53:21 2001
++++ lib/python/python.h	Thu Nov 30 22:25:14 2006
+@@ -3,6 +3,22 @@
+ 
+ #pragma interface
+ 
++#include <ctype.h>
++#undef isalnum
++#undef isalpha
++#undef iscntrl
++#undef isdigit
++#undef isgraph
++#undef islower
++#undef isprint
++#undef ispunct
++#undef isspace
++#undef isupper
++#undef isxdigit
++#undef tolower
++#undef toupper
++#include <iostream>
++
+ #include <Python.h>
+ #include <string>
+ #include <iterator>
+@@ -12,13 +28,17 @@
  #include "debug.h"
  
  // I hate #define macros, seee if I can remove this
@@ -11,13 +34,13 @@
 -#  define STANDARD_LIBRARY_HAS_ITERATOR_TRAITS 0
 +#  undef STANDARD_LIBRARY_HAS_RANDOM_ACCESS_ITERATOR
 +#  define STANDARD_LIBRARY_HAS_RANDOM_ACCESS_ITERATOR 0
++#endif
++
++#if defined (__GNUG__) && (__GNUG__ >= 3)
++#  undef STANDARD_LIBRARY_HAS_RANDOM_ACCESS_ITERATOR
  #endif
  
 -#if STANDARD_LIBRARY_HAS_ITERATOR_TRAITS
-+#if defined (__GNUG__) && (__GNUG__ >= 3)
-+#  undef STANDARD_LIBRARY_HAS_RANDOM_ACCESS_ITERATOR
-+#endif
-+
 +#if STANDARD_LIBRARY_HAS_RANDOM_ACCESS_ITERATOR
  #  define random_access_iterator_parent(itemtype) std::random_access_iterator<itemtype, int>
  #else
