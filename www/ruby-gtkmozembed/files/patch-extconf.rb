9377b98f8f2bdace6bb294861a8e6f32bffd1172
Stanislav Sedov <stas@FreeBSD.org>
2006-12-02 18:34:38 +0000

    - Update to current snapshot (should fix compatiblity problems with the new   gnome) - PREFIX-cleaness
    Approved by:	mezz (maintainer, implicitly)

--- www/ruby-gtkmozembed/files/patch-extconf.rb
+++ www/ruby-gtkmozembed/files/patch-extconf.rb
@@ -1,10 +1,10 @@
---- extconf.rb.orig	Sun Jul  2 19:15:05 2006
-+++ extconf.rb	Sun Jul  2 19:19:25 2006
-@@ -23,8 +23,7 @@
- PKGConfig.have_package(PACKAGE_ID)
+--- extconf.rb.orig	Fri Dec  1 02:00:22 2006
++++ extconf.rb	Fri Dec  1 03:37:15 2006
+@@ -40,8 +40,7 @@
+ PKGConfig.have_package(package_id)
  setup_win32(PACKAGE_NAME)
  
--mozpath = PKGConfig.libs_only_L(PACKAGE_ID)
+-mozpath = PKGConfig.libs_only_L(package_id)
 -mozpath.strip!.sub!(/^-L/, "")
 +mozpath = "%%GECKO_PATH%%"
  
