1d60c19c6ba5faff82fd4d00b066daea5be1be7f
stas <stas@FreeBSD.org>
2006-12-04 20:33:04 +0000

    - Fix an another cgi library vulnerability - Bump portrevision
    PR:		ports/106287
    Reported by:	UEDA Hiroyuki <bsdmad@gmail.com>
    Obtained from:	ruby cvs

--- lang/ruby18/files/patch-lib_cgi.rb
+++ lang/ruby18/files/patch-lib_cgi.rb
@@ -1,6 +1,23 @@
---- lib/cgi.rb      2005-10-06 19:01:22.000000000 -0600
-+++ lib/cgi.rb      2006-09-22 16:38:08.000000000 -0600
-@@ -1017,7 +1017,7 @@
+--- lib/cgi.rb.orig	Tue Aug 22 13:38:19 2006
++++ lib/cgi.rb	Mon Dec  4 23:22:42 2006
+@@ -967,6 +967,7 @@
+     def read_multipart(boundary, content_length)
+       params = Hash.new([])
+       boundary = "--" + boundary
++      quoted_boundary = Regexp.quote(boundary, "n")
+       buf = ""
+       bufsize = 10 * 1024
+       boundary_end=""
+@@ -998,7 +999,7 @@
+         end
+         body.binmode if defined? body.binmode
+ 
+-        until head and /#{boundary}(?:#{EOL}|--)/n.match(buf)
++        until head and /#{quoted_boundary}(?:#{EOL}|--)/n.match(buf)
+ 
+           if (not head) and /#{EOL}#{EOL}/n.match(buf)
+             buf = buf.sub(/\A((?:.|\n)*?#{EOL})#{EOL}/n) do
+@@ -1018,14 +1019,14 @@
                else
                  stdinput.read(content_length)
                end
@@ -9,3 +26,11 @@
              raise EOFError, "bad content body"
            end
            buf.concat(c)
+           content_length -= c.size
+         end
+ 
+-        buf = buf.sub(/\A((?:.|\n)*?)(?:[\r\n]{1,2})?#{boundary}([\r\n]{1,2}|--)/n) do
++        buf = buf.sub(/\A((?:.|\n)*?)(?:[\r\n]{1,2})?#{quoted_boundary}([\r\n]{1,2}|--)/n) do
+           body.print $1
+           if "--" == $2
+             content_length = -1
