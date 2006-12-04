f93d5b8ac648a4dd252f0ffdc017a9ab5c0882a7
miwi <miwi@FreeBSD.org>
2006-12-04 23:50:54 +0000

    liblo is an implementation of the Open Sound Control protocol for POSIX systems.
    WWW:	http://liblo.sourceforge.net/
    
    PR:		ports/106353
    Submitted by:	trasz <trasz at pin.if.uz.zgora.pl>

---
+++ audio/liblo/files/patch-src-lo_types_internal.h
@@ -0,0 +1,10 @@
+--- src/lo_types_internal.h.orig	Mon Dec  4 23:29:22 2006
++++ src/lo_types_internal.h	Mon Dec  4 23:29:32 2006
+@@ -9,6 +9,7 @@
+ #endif
+ 
+ #include <pthread.h>
++#include <sys/socket.h>
+ 
+ #include "lo/lo_osc_types.h"
+ 
