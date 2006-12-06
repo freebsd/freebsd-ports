330da12dad95e4a3874d3183faeb73aac9595b89
Pav Lucistnik <pav@FreeBSD.org>
2006-12-06 14:20:13 +0000

    - Update to 1.8.2
    PR:		ports/106268
    Submitted by:	Gerrit Beine <gerrit.beine@gmx.de> (maintainer)

--- www/mediawiki115/files/patch-index.php
+++ www/mediawiki115/files/patch-index.php
@@ -1,10 +1,10 @@
---- config/index.php.orig	Tue Jun  6 11:04:04 2006
-+++ config/index.php	Sat Jul  1 13:27:04 2006
-@@ -340,7 +340,7 @@
- $diff3locations = array("/usr/bin", "/opt/csw/bin", "/usr/gnu/bin", "/usr/sfw/bin") + explode($sep, getenv("PATH"));
- $diff3names = array("gdiff3", "diff3", "diff3.exe");
+--- config/index.php.orig	Sat Oct 14 02:06:36 2006
++++ config/index.php	Sun Dec  3 16:18:18 2006
+@@ -434,7 +434,7 @@
+ $diff3locations = array( "/usr/bin", "/usr/local/bin", "/opt/csw/bin", "/usr/gnu/bin", "/usr/sfw/bin" ) + explode( $sep, getenv( "PATH" ) );
+ $diff3names = array( "gdiff3", "diff3", "diff3.exe" );
  
--$diff3versioninfo = array('$1 --version 2>&1', 'diff3 (GNU diffutils)');
+-$diff3versioninfo = array( '$1 --version 2>&1', 'diff3 (GNU diffutils)' );
 +$diff3versioninfo = array('$1 --version 2>&1', 'diff3 - GNU diffutils');
  foreach ($diff3locations as $loc) {
  	$exe = locate_executable($loc, $diff3names, $diff3versioninfo);
