33d836ef4601f1dff6411db002f3a6ceab79c74d
glewis <glewis@FreeBSD.org>
2006-12-08 22:36:16 +0000

    . Replace some hardwired /usr/X11R6 instances with X11BASE. [1] . Small Makefile restructure to keep related variables in one place. . Trim the ancient bootstrapping options. . Add ipv6 to CATEGORIES if its enabled. [2]
    PR:		106129 [2]
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu> [2]
    Pointed out by:	pointyhat (via kris) [1]

---
+++ java/jdk14/files/patch-j2se::awt::fontpath.c
@@ -0,0 +1,19 @@
+$FreeBSD$
+
+--- ../../j2se/src/solaris/native/sun/awt/fontpath.c.orig	Fri Dec  8 11:59:25 2006
++++ ../../j2se/src/solaris/native/sun/awt/fontpath.c	Fri Dec  8 12:00:06 2006
+@@ -495,10 +495,10 @@
+     char *ptr;
+ 
+ #if defined(__linux__) || defined(_ALLBSD_SOURCE)
+-    ptr = "/usr/X11R6/lib/X11/fonts/Type1:"
+-          "/usr/X11R6/lib/X11/fonts/TrueType:"
+-          "/usr/X11R6/lib/X11/fonts/tt:"
+-          "/usr/share/fonts/ja/TrueType";
++    ptr = "%%X11BASE%%/lib/X11/fonts/Type1:"
++          "%%X11BASE%%/lib/X11/fonts/TrueType:"
++          "%%X11BASE%%/lib/X11/fonts/tt:"
++          "%%X11BASE%%/fonts/ja/TrueType";
+ #elif defined( HEADLESS )
+     ptr = getSolarisFontLocations(env, (int) (noType1==JNI_TRUE));
+ #elif SCAN_FONT_DIRS
