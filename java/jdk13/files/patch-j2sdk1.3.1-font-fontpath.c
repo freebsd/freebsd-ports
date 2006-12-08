010e6bbbe376c3965947f3dd468ed36dc8afb5b7
glewis <glewis@FreeBSD.org>
2006-12-08 19:39:45 +0000

    . Replace some hardwired /usr/X11R6 instances with X11BASE. [1] . Replace one hardwired /usr/local instance with LOCALBASE.  This wasn't   a problem since the port Makefile overrode that variable anyway. . Small Makefile restructure to keep related variables in one place.
    Pointed out by:	pointyhat (via kris) [1]

---
+++ java/jdk13/files/patch-j2sdk1.3.1-font-fontpath.c
@@ -0,0 +1,13 @@
+$FreeBSD$
+
+--- ../src/solaris/native/sun/awt/font/fontpath.c.orig	Fri Dec  8 10:52:19 2006
++++ ../src/solaris/native/sun/awt/font/fontpath.c	Fri Dec  8 10:54:02 2006
+@@ -426,7 +426,7 @@
+ JNIEXPORT jstring JNICALL Java_sun_awt_font_NativeFontWrapper_getFontPath(JNIEnv *env, jclass obj, jboolean noType1)
+ {
+ #ifndef __solaris__
+-   char *ptr   = "/usr/X11R6/lib/X11/fonts/Type1:/usr/X11R6/lib/X11/fonts/TrueType:/usr/X11R6/lib/X11/fonts/tt";
++   char *ptr   = "%%X11BASE%%/lib/X11/fonts/Type1:%%X11BASE%%/lib/X11/fonts/TrueType:%%X11BASE%%/lib/X11/fonts/tt";
+ #else
+     char *ptr   = getSolarisFontLocations((int) (noType1==JNI_TRUE));
+ #endif
