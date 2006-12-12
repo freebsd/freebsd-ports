7d1fb0b9c416d200873468f8ae72f40968a67ff5
miwi <miwi@FreeBSD.org>
2006-12-12 20:44:23 +0000

    - Update to 0.30 - Update MASTER_SITES
    PR:		ports/106600
    Submitted by:	MURAOKA Daisuke <raia@muj.biglobe.ne.jp>

--- textproc/p5-Search-Saryer/files/patch-Saryer.pm
+++ textproc/p5-Search-Saryer/files/patch-Saryer.pm
@@ -1,24 +1,11 @@
---- Saryer.pm.orig	Mon Dec 18 14:10:08 2000
-+++ Saryer.pm	Tue Oct  8 23:04:36 2002
-@@ -13,10 +13,10 @@
+--- Saryer.pm.orig	Mon Jul 11 02:19:01 2005
++++ Saryer.pm	Fri Jun 23 13:38:04 2006
+@@ -13,7 +13,7 @@
  
  =head1 SYNOPSIS
  
--  my $saryer = Search::Saryer::new(filename->'search.txt');
-+  my $saryer = Search::Saryer::new(filename=>'search.txt');
+-  my $saryer = new Search::Saryer(filename->'search.txt');
++  my $saryer = new Search::Saryer(filename=>'search.txt');
+   print $saryer->grep($pattern)
  
-   if (defined $saryer->search($pattern)) {
--    while (defined ($line = saryer->get_next_line())) {
-+    while (defined ($line = $saryer->get_next_line())) {
-       print $line;
-     }
-   }
-@@ -27,7 +27,7 @@
- 
- =head2 new
- 
--  $saryer->new('filename'->'file.txt')
-+  $saryer->new('filename'=>'file.txt')
- 
- Create saryer object.
- If creation process was failed, it returns undef.
+ =head1 CONSTRUCTOR
