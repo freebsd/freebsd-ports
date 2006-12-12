939a3e770a4f4b7c5ad5ff457cf1876c9b2acf7c
vd <vd@FreeBSD.org>
2006-12-12 16:06:42 +0000

    Remove expired ports: 2006-12-01 print/latex-beamer: is already included in the teTeX distribution 2006-12-01 print/latex-bibtopic: is already included in the teTeX distribution 2006-12-01 print/latex-bibunits: is already included in the teTeX distribution 2006-12-01 print/latex-booktabs: is already included in the teTeX distribution 2006-12-01 print/latex-index: is already included in the teTeX distribution 2006-12-01 print/latex-layouts: is already included in the teTeX distribution 2006-12-01 print/latex-pgf: is already included in the teTeX distribution 2006-12-01 print/latex-subfig: is already included in the teTeX distribution 2006-12-01 print/latex-xcolor: is already included in the teTeX distribution 2006-12-01 print/latex-lineno: is already included in the teTeX distribution 2006-12-01 print/latex-pict2e: is already included in the teTeX distribution 2007-05-31 multimedia/gstreamer-plugins-mpeg2enc80: Obsolete version, use gstreamer 0.10 instead 2006-12-01 lang/gcc30: "This port is no longer in use by anything in the ports collection and will be removed in the future.  Use a later release instead." 2006-12-01 lang/egcs: "This port is no longer in use by anything in the ports collection and will be removed in the future.  Use a later release of gcc instead" 2006-12-01 graphics/php4-ming: doesn't work with new version of ming library 2006-12-01 graphics/dvipng: is already included in the teTeX distribution

--- lang/egcs/files/patch-strerror.c
+++
@@ -1,7 +0,0 @@
---- libiberty/strerror.c.orig	Fri May 15 16:42:41 1998
-+++ libiberty/strerror.c	Sun Nov 24 14:25:18 2002
-@@ -465,2 +465,2 @@
--extern int sys_nerr;
--extern char *sys_errlist[];
-+/* extern int sys_nerr;		declared just fine by stdio.h, thank you very much */
-+/* extern char *sys_errlist[];	declared just fine by stdio.h, thank you very much */
