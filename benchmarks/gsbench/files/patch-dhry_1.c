067ff7d4114fef0fa3fcfc6cc83a7a6e4a1e0e8f
dinoex <dinoex@FreeBSD.org>
2006-12-14 20:16:43 +0000

    This is GSBench for GNUstep. It started as a simple conversion of NXBench for NEXTSTEP. I rewrote the entire application for version 0.5 in order to have an open architecture which allows the integration of other benchmarks by using bundles.
    WWW: http://www.nice.ch/~phip/softcorner.html

---
+++ benchmarks/gsbench/files/patch-dhry_1.c
@@ -0,0 +1,10 @@
+--- Bundles/Dhry/dhry_1.c.orig	Sun Jan 14 11:47:31 2001
++++ Bundles/Dhry/dhry_1.c	Thu Dec 14 16:59:23 2006
+@@ -66,7 +66,6 @@
+ int             Arr_2_Glob [50] [50];
+ 
+ extern char     *malloc ();
+-Enumeration     Func_1 ();
+   /* forward declaration necessary since Enumeration may not simply be int */
+ 
+ #ifndef REG
