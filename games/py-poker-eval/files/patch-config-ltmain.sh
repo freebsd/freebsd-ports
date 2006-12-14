313393dcf06efb233f4af460f37cf937138c9a0f
ijliao <ijliao@FreeBSD.org>
2006-12-14 16:00:46 +0000

    add py-poker-eval Python adaptor for the poker-eval toolkit

---
+++ games/py-poker-eval/files/patch-config-ltmain.sh
@@ -0,0 +1,15 @@
+--- config/ltmain.sh.orig	Thu Dec 14 22:00:03 2006
++++ config/ltmain.sh	Thu Dec 14 22:00:20 2006
+@@ -6003,10 +6003,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
