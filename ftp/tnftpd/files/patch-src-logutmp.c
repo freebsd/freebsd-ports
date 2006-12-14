ae012243f604b5221ab942fa7e17bd677a0e7cc1
Martin Wilke <miwi@FreeBSD.org>
2006-12-14 12:34:40 +0000

    - Update to 20061204
    PR:		ports/106700
    Submitted by:	Rudolf Cejka <cejkar@fit.vutbr.cz> (maintainer)

--- ftp/tnftpd/files/patch-src-logutmp.c
+++ ftp/tnftpd/files/patch-src-logutmp.c
@@ -1,5 +1,9 @@
---- src/logutmp.c-orig	Thu Jan 29 15:43:57 2004
-+++ src/logutmp.c	Thu Jan 29 15:44:33 2004
+
+FreeBSD does not have header file util.h, fortunately it is not needed.
+Add header file time.h for time().
+
+--- src/logutmp.c.orig	Wed Dec 13 13:16:58 2006
++++ src/logutmp.c	Wed Dec 13 13:17:18 2006
 @@ -59,13 +59,13 @@
  #include <stdio.h>
  #include <stdlib.h>
