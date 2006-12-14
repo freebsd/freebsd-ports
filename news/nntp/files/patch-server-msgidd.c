869e891ce87e204cc85e656289b64bb9257b56d9
dinoex <dinoex@FreeBSD.org>
2006-12-14 09:56:00 +0000

    - fix build with gcc41 again, new snapshots are more pedantic

--- news/nntp/files/patch-server-msgidd.c
+++ news/nntp/files/patch-server-msgidd.c
@@ -1,5 +1,5 @@
 --- server/msgidd.c.orig	Fri Dec  9 03:53:01 1994
-+++ server/msgidd.c	Mon Nov  4 05:51:18 2002
++++ server/msgidd.c	Thu Dec 14 10:48:45 2006
 @@ -34,8 +34,10 @@
  #define dprintf (void)
  #endif
@@ -11,3 +11,12 @@
  
  int log = 0, flags = 0;
  time_t hold_time = MAX_AGE * 60;
+@@ -171,7 +173,7 @@
+     alarm(ALARM_TIME);
+ }
+ 
+-SIGRET onpipe();
++static SIGRET onpipe();
+ 
+ main(argc, argv)
+     int argc;
