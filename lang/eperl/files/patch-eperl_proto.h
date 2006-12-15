0064e479e311d04d0f1630260e6003c9273204a8
Martin Wilke <miwi@FreeBSD.org>
2006-12-15 07:51:51 +0000

    - Fix gcc 4.X build
    PR:		ports/106743
    Submitted by:	trasz

---
+++ lang/eperl/files/patch-eperl_proto.h
@@ -0,0 +1,11 @@
+--- eperl_proto.h.orig	Fri Dec 15 01:27:39 2006
++++ eperl_proto.h	Fri Dec 15 01:27:51 2006
+@@ -35,6 +35,8 @@
+ #ifndef EPERL_PROTO_H
+ #define EPERL_PROTO_H 1
+ 
++#include "eperl_getopt.h"
++
+ /*_BEGIN_PROTO_*/
+ 
+ /* eperl_main.c */
