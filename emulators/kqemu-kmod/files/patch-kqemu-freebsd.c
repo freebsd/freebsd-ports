d7aa9009e069d74f3493c676e3d2fc82438bee6d
Martin Wilke <miwi@FreeBSD.org>
2006-12-15 08:38:11 +0000

    - Add fix for -current - Add rcscript
    PR:		ports/106728
    Submitted by:	Juergen Lock <nox@jelal.kn-bremen.de> (maintainer)

--- emulators/kqemu-kmod/files/patch-kqemu-freebsd.c
+++ emulators/kqemu-kmod/files/patch-kqemu-freebsd.c
@@ -7,3 +7,14 @@ Index: kqemu-freebsd.c
  static struct kqemu_global_state *kqemu_gs = NULL;
  
  #if __FreeBSD_version < 500000
+@@ -321,6 +321,9 @@
+ #if __FreeBSD_version >= 500000
+     dev->si_drv1 = NULL;
+     TAILQ_REMOVE(&kqemuhead, ks, kqemu_ent);
++#if __FreeBSD_version >= 700024
++    dev_relthread(dev); 
++#endif
+     destroy_dev(dev);
+ #endif
+     free(ks, M_KQEMU);
+
