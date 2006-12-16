4c17b9e11c4050e56537bb923bdee91e474bbcc1
Pav Lucistnik <pav@FreeBSD.org>
2006-12-16 14:05:00 +0000

    - Fix build with gcc41

---
+++ mail/popular/files/patch-src-pdeliver.c
@@ -0,0 +1,11 @@
+--- src/pdeliver.c.orig	Mon Apr 30 14:20:34 2001
++++ src/pdeliver.c	Sat Dec 16 15:01:12 2006
+@@ -33,7 +33,7 @@
+ #include "popular.h"
+ 
+ 
+-static int debug;
++int debug;
+ 
+ 
+ /*****************************************************************************
