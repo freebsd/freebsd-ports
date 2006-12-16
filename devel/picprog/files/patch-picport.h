4ce690f76d86c32dddb6d231c8724950cb416011
Martin Wilke <miwi@FreeBSD.org>
2006-12-16 09:28:22 +0000

    - Fix build with gcc4.X
    PR:		ports/106792
    Submitted by:	Erik Greenwald <erik@smluc.org> (maintainer)

--- devel/picprog/files/patch-picport.h
+++ devel/picprog/files/patch-picport.h
@@ -1,13 +1,22 @@
---- picport.h.orig	Thu Apr 29 00:09:38 2004
-+++ picport.h	Fri Jul  8 21:46:07 2005
-@@ -59,6 +59,10 @@
+--- picport.h.orig	Wed Jul 14 01:24:34 2004
++++ picport.h	Fri Dec 15 16:43:35 2006
+@@ -45,7 +45,7 @@
+   int W[16];
+ 
+   int modembits;
+-  void picport::set_clock_data (int rts, int dtr);
++  void set_clock_data (int rts, int dtr);
+ 
+   void p_out (int b);
+   int p_in ();
+@@ -58,6 +58,10 @@
+   static int use_nanosleep;
  
  public:
- 
++
 +  static int t_on;
 +  static int t_off;
 +  static int t_edge;
-+
+ 
    static void delay (long ns);
  
-   enum commands {
