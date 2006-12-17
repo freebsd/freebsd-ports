63e2d3a742fc8648a84a21bef0fe694d021856dc
Alex Dupre <ale@FreeBSD.org>
2006-12-17 09:07:01 +0000

    Update to 0.9.0-rc2 release.

--- graphics/digikam-kde4/files/patch-digikam_kioslave_digikamdates.cpp
+++ graphics/digikam-kde4/files/patch-digikam_kioslave_digikamdates.cpp
@@ -9,14 +9,3 @@
  
              Q_LLONG     imageid;
              QString     name;
---- digikam/kioslave/digikamdates.cpp.orig	Mon Apr 24 17:00:27 2006
-+++ digikam/kioslave/digikamdates.cpp	Mon Apr 24 17:01:39 2006
-@@ -248,7 +248,7 @@ void kio_digikamdates::special(const QBy
-                 *os << dirid;
-                 *os << name;
-                 *os << date;
--                *os << stbuf.st_size;
-+                *os << static_cast<size_t>(stbuf.st_size);
-                 *os << dims;
- 
-                 count++;
