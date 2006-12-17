45cd42af07840eec8971c98dd8d12815b196aaf0
alepulver <alepulver@FreeBSD.org>
2006-12-17 19:24:20 +0000

    - Fix building with GCC 4.x.
    Reported by:	pointyhat

--- games/quakeforge/files/patch-nq__source__sys_unix.c
+++ games/quakeforge/files/patch-nq__source__sys_unix.c
@@ -1,7 +1,7 @@
---- nq/source/sys_unix.c.orig
-+++ nq/source/sys_unix.c
+--- ./nq/source/sys_unix.c.orig	Wed Jan 15 12:31:28 2003
++++ ./nq/source/sys_unix.c	Sun Dec 17 15:50:06 2006
 @@ -69,6 +69,9 @@
-        fcntl (0, F_SETFL, fcntl (0, F_GETFL, 0) & ~O_NONBLOCK);
+ 	fcntl (0, F_SETFL, fcntl (0, F_GETFL, 0) & ~O_NONBLOCK);
  }
  
 +extern void Key_Progs_Init (progs_t *pr);
