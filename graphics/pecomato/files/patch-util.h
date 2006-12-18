b16ca97dfe47d8e821f0a716f72659079211f67b
ehaupt <ehaupt@FreeBSD.org>
2006-12-18 22:46:30 +0000

    This port now builds with gcc 4.0+
    Notified by:	kris

---
+++ graphics/pecomato/files/patch-util.h
@@ -0,0 +1,10 @@
+--- util.h.orig	Mon Dec 18 20:22:56 2006
++++ util.h	Mon Dec 18 20:23:03 2006
+@@ -8,6 +8,7 @@
+ 
+ #ifndef _util_h_
+ #define _util_h_
++#define _SSIZE_T_DECLARED
+ 
+ 
+ #include <stdio.h>
