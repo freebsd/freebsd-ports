f99571444e63375dccf889e7e33512bf28ad2679
Sergey Matveychuk <sem@FreeBSD.org>
2006-12-19 09:02:06 +0000

    - Update to 2.7.9a

--- devel/gsoap/files/patch-soapcpp2-stdsoap2.c
+++ devel/gsoap/files/patch-soapcpp2-stdsoap2.c
@@ -1,20 +1,14 @@
---- soapcpp2/stdsoap2.c.orig	Mon Jun 26 21:36:38 2006
-+++ soapcpp2/stdsoap2.c	Fri Sep 29 14:22:51 2006
-@@ -3282,7 +3282,7 @@
+--- soapcpp2/stdsoap2.c.orig	Tue Dec 19 11:28:08 2006
++++ soapcpp2/stdsoap2.c	Tue Dec 19 11:36:34 2006
+@@ -3335,7 +3335,11 @@
      soap->errnum = h_errno;
    }
  #elif defined(HAVE_GETHOSTBYNAME_R)
--  host = gethostbyname_r(addr, &hostent, soap->buf, SOAP_BUFLEN, &soap->errnum);
++#if defined(__FreeBSD__)
 +  gethostbyname_r(addr, &hostent, soap->buf, SOAP_BUFLEN, &host, &soap->errnum);
++#else
+   host = gethostbyname_r(addr, &hostent, soap->buf, SOAP_BUFLEN, &soap->errnum);
++#endif
  #elif defined(VXWORKS)
    /* If the DNS resolver library resolvLib has been configured in the vxWorks
     * image, a query for the host IP address is sent to the DNS server, if the
-@@ -7616,7 +7616,7 @@
-         break;
-     }
-     if (!tp)
--    { tp = (struct soap_attribute*)SOAP_MALLOC(soap, sizeof(struct soap_attribute) + strlen(soap->tmpbuf));
-+    { tp = (struct soap_attribute*)SOAP_MALLOC(soap, sizeof(struct soap_attribute) + strlen(soap->tmpbuf) + 1);
-       if (!tp)
-         return soap->error = SOAP_EOM;
-       strcpy(tp->name, soap->tmpbuf);
