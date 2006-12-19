cac2a8bd1984e03cc7b6be93bff4d2076905f0b9
Nicola Vitale <nivit@FreeBSD.org>
2006-12-19 10:42:47 +0000

    - Updated to 1.3.1.1 - Fixed paths with %%MANPREFIX%%, %%DATADIR%%, etc. - Substituted USE_COMPS=python_run with WX_COMPS=python:run   (because it did not install x11-toolkits/py-wxPython*)
    Approved by:	alexbl (mentor)

--- multimedia/tunapie/files/patch-install.sh
+++ multimedia/tunapie/files/patch-install.sh
@@ -1,17 +1,20 @@
---- install.sh	Sun Nov 19 13:50:27 2006
-+++ install.sh.port	Thu Nov 23 10:12:43 2006
+--- install.sh	Tue Dec 12 12:51:22 2006
++++ install.sh.port	Fri Dec 15 18:24:54 2006
 @@ -1,11 +1,9 @@
  #!/bin/sh
 -if [ "$1" = "--adult" ]; then echo '1'>/etc/tunapie.config 
 -else echo '0'>/etc/tunapie.config
-+if [ "$1" = "--adult" ]; then echo '1'>/usr/local/etc/tunapie.config.default
-+else echo '0'>/usr/local/etc/tunapie.config.default
++if [ "$1" = "--adult" ]; then echo '1'>%%LOCALBASE%%/etc/tunapie.config.default
++else echo '0'>%%LOCALBASE%%/etc/tunapie.config.default
  fi
  python src/compile
- cp tunapie /usr/local/bin
+-cp tunapie /usr/local/bin
 -rm -rf /usr/local/share/tunapie
 -mkdir /usr/local/share/tunapie
--cp src/*.py src/*.pyc src/*.jpg /usr/local/share/tunapie
-+cp src/*.py src/*.pyc src/*.ico /usr/local/share/tunapie
- cp tunapie.1 /usr/local/man/man1
- cp tunapie.desktop /usr/local/share/applications
+-cp src/*.py src/*.pyc /usr/local/share/tunapie
+-cp tunapie.1 /usr/local/man/man1
+-cp tunapie.desktop /usr/local/share/applications
++cp tunapie %%PREFIX%%/bin
++cp src/*.py src/*.pyc %%DATADIR%%
++cp tunapie.1 %%MANPREFIX%%/man/man1
++cp tunapie.desktop %%DESKTOPDIR%%
