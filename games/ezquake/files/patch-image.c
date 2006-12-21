ae9cc160755a76b57134c2be2903812cc399be38
Alejandro Pulver <alepulver@FreeBSD.org>
2006-12-21 13:32:40 +0000

    - Update to version 1754.

--- games/ezquake/files/patch-image.c
+++ games/ezquake/files/patch-image.c
@@ -1,19 +1,10 @@
---- ./image.c.orig	Sat Apr  8 10:15:01 2006
-+++ ./image.c	Tue Aug 29 20:59:12 2006
+--- image.c.orig	Thu Dec 14 10:04:06 2006
++++ image.c	Tue Dec 19 19:00:49 2006
 @@ -20,6 +20,7 @@
- 	$Id: image.c,v 1.20 2006/04/08 13:15:01 disconn3ct Exp $
- */
- 
+     $Id: image.c,v 1.33 2006/12/14 13:04:06 disconn3ct Exp $
+ */
+ 
 +#include <dlfcn.h>
- #include "quakedef.h"
- 
- 
-@@ -1271,7 +1272,7 @@
- 
- static QLIB_HANDLETYPE_T jpeg_handle = NULL;
- 
--#define qjpeg_create_compress(cinfo) \
-+#define qjpeg_create_compress(cinfo) \
-     qjpeg_CreateCompress((cinfo), JPEG_LIB_VERSION, (size_t) sizeof(struct jpeg_compress_struct))
- 
- static struct jpeg_error_mgr *(*qjpeg_std_error)(struct jpeg_error_mgr *);
+ #include "quakedef.h"
+ 
+ 
--- games/ezquake/files/patch-snd_linux.c
+++ games/ezquake/files/patch-snd_linux.c
@@ -1,17 +1,15 @@
---- ./snd_linux.c.orig	Fri Sep  9 08:00:27 2005
-+++ ./snd_linux.c	Tue Aug 29 20:59:12 2006
-@@ -6,7 +6,6 @@
+--- snd_linux.c.orig	Tue May 16 06:40:16 2006
++++ snd_linux.c	Tue Dec 19 19:22:24 2006
+@@ -21,19 +21,14 @@
  
- #include "quakedef.h" 
+ #include "quakedef.h"
  
--qbool SNDDMA_ALSA;
- int snd_inited;
+-static qbool SNDDMA_ALSA = true;
  // Note: The functions here keep track of if the sound system is inited.
- //       They perform checks so that the real functions are only called if
-@@ -15,13 +14,9 @@
+ // They perform checks so that the real functions are only called if appropriate.
+ 
  
  // Prototypes
- 
 -qbool SNDDMA_Init_ALSA(void);
  qbool SNDDMA_Init_OSS(void);
 -int SNDDMA_GetDMAPos_ALSA(void);
@@ -19,72 +17,61 @@
 -void SNDDMA_Shutdown_ALSA(void);
  void SNDDMA_Shutdown_OSS(void);
 -void SNDDMA_Submit_ALSA(void);
- void SNDDMA_Submit_OSS(void);
  
  
-@@ -31,30 +26,8 @@
+ // Main functions
+@@ -41,49 +36,24 @@
  {
-     int retval;
+ 	int retval;
  
--    // Give user the option to force OSS...
--    if(COM_CheckParm("-noalsa")) // || Cvar_VariableValue("s_noalsa") )
--    {
--        // User wants us to use OSS...
--        Com_Printf("sound: Using OSS at user's request...\n");
--        retval = SNDDMA_Init_OSS();
--    }
--    else
--    {
--        // Try ALSA first...
--        Com_Printf("sound: Attempting to initialise ALSA...\n");
--        retval = SNDDMA_Init_ALSA();
--        if( retval )
--        {
--            SNDDMA_ALSA = true;
--        }
--        else
--        {
--            // Fall back to OSS...
--            SNDDMA_ALSA = false;
--            Com_Printf("sound: Falling back to OSS...\n");
--            retval = SNDDMA_Init_OSS();
--        }
--    }
-+    Com_Printf("sound: Initializing OSS...\n");
-+    retval = SNDDMA_Init_OSS();
+-	// Give user the option to force OSS...
+-	if (Cvar_VariableValue("s_noalsa")) {
+-		// User wants us to use OSS...
+-		SNDDMA_ALSA = false;
+-		Com_Printf("sound: Using OSS at user's request...\n");
+-		retval = SNDDMA_Init_OSS();
+-	} else {
+-		// Try ALSA first...
+-		Com_Printf("sound: Attempting to initialise ALSA...\n");
+-		retval = SNDDMA_Init_ALSA();
+-		if (retval) {
+-			SNDDMA_ALSA = true;
+-		} else {
+-			// Fall back to OSS...
+-			SNDDMA_ALSA = false;
+-			Com_Printf("sound: Falling back to OSS...\n");
+-			retval = SNDDMA_Init_OSS();
+-		}
+-	}
++	Com_Printf("sound: Initializing OSS...\n");
++	retval = SNDDMA_Init_OSS();
  
-     snd_inited = retval;
-     return retval;
-@@ -64,10 +37,7 @@
+ 	return retval;
+ }
+ 
+ int SNDDMA_GetDMAPos(void)
  {
-     if( snd_inited )
-     {
--        if( SNDDMA_ALSA )
--	    return SNDDMA_GetDMAPos_ALSA();
--        else
--	    return SNDDMA_GetDMAPos_OSS();
+-	if (SNDDMA_ALSA)
+-		return SNDDMA_GetDMAPos_ALSA();
+-	else
+-		return SNDDMA_GetDMAPos_OSS();
 +	return SNDDMA_GetDMAPos_OSS();
-     }
-     else
-         return 0;
-@@ -77,10 +47,7 @@
- {
-     if (snd_inited)
-     {
--        if( SNDDMA_ALSA )
--            SNDDMA_Shutdown_ALSA();
--        else
--            SNDDMA_Shutdown_OSS();
-+        SNDDMA_Shutdown_OSS();
+ }
  
-         snd_inited = 0;
-     }
-@@ -97,8 +64,6 @@
+ void SNDDMA_Shutdown(void)
  {
-     if( snd_inited )
-     {
--        if( SNDDMA_ALSA )
--            SNDDMA_Submit_ALSA();
-         // OSS doesn't use this so no need to call it.
-     }
+-	if (SNDDMA_ALSA)
+-		SNDDMA_Shutdown_ALSA();
+-	else
+-		SNDDMA_Shutdown_OSS();
++	SNDDMA_Shutdown_OSS();
+ }
+ 
+ //Send sound to device if buffer isn't really the dma buffer
+ void SNDDMA_Submit(void)
+ {
+-	if (SNDDMA_ALSA)
+-		SNDDMA_Submit_ALSA();
+-		// OSS doesn't use this so no need to call it.
++	// OSS doesn't use this so no need to call it.
  }
