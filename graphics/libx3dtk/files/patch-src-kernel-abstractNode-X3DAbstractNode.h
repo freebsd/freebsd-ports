b82f026927dde6d5a41a3fec6fe795c9cbc4a6d8
miwi <miwi@FreeBSD.org>
2006-12-21 19:33:08 +0000

    - Fix build with gcc 4.1
    PR:		ports/106834
    Submitted by:	German Tischler <tanis@gaspode.franken.de> (maintainer)

--- graphics/libx3dtk/files/patch-src-kernel-abstractNode-X3DAbstractNode.h
+++ graphics/libx3dtk/files/patch-src-kernel-abstractNode-X3DAbstractNode.h
@@ -1,5 +1,5 @@
-*** include/X3DTK/private/X3DAbstractNode.h.orig	Fri Aug 27 13:18:33 2004
---- include/X3DTK/private/X3DAbstractNode.h	Sat Dec 16 15:42:01 2006
+*** src/kernel/abstractNode/X3DAbstractNode.h.orig	Sat Dec 16 16:20:51 2006
+--- src/kernel/abstractNode/X3DAbstractNode.h	Sat Dec 16 16:21:07 2006
 ***************
 *** 102,108 ****
     inline void defineTypeName(const SFString &name, const SFString &component = SFString(""), const SFString &sceneGraph = SFString("")) {defineTempTypeName(this, name, component, sceneGraph);};
@@ -17,22 +17,3 @@
     /// Defines an attribute.
     template<class T, class V>
     inline void defineAttribute(const SFString &name, V T:: *member, const V &init);
-***************
-*** 132,137 ****
---- 132,147 ----
-    /// Removes a parent.
-    bool removeParent(const SFNode &N);
-  };
-+ 
-+ inline void X3DAbstractNode_addParentToChild(SFNode parent, SFNode child) {
-+   X3DAbstractNode::addParentToChild(parent,child);
-+ }
-+ inline void X3DAbstractNode_removeParentFromChild(SFNode parent, SFNode child) {
-+   X3DAbstractNode::removeParentFromChild(parent,child);
-+ }
-+ inline SFType * X3DAbstractNode_getType(X3DAbstractNode * node) {
-+   return node->getType();
-+ }
-  
-  }
-  
