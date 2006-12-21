d31df7648ad3562fe25dee7f0b8b994092dcfbcf
arved <arved@FreeBSD.org>
2006-12-21 16:06:50 +0000

    Fix build with gcc41

---
+++ x11-toolkits/fl_editor/files/patch-fltk-wstring.h
@@ -0,0 +1,11 @@
+--- fltk/wstring.h.orig	Thu Dec 21 17:03:29 2006
++++ fltk/wstring.h	Thu Dec 21 17:03:41 2006
+@@ -88,7 +88,7 @@
+ 		long     operator>(const char *str) const { return (strcmp(buffer,str) > 0); };
+ 		long     operator==(const char *str) const  { return (strcmp(buffer,str) == 0); };
+         wString& operator=(const char *str)	{ SSet(str);  return (*this); };
+-        RCLDLL wString& wString::operator=(const wString& str) { SSet(str.buffer); return (*this); }
++        RCLDLL wString& operator=(const wString& str) { SSet(str.buffer); return (*this); }
+         wString& operator=(const unsigned char *str)    { SSet((const char *)str); return *this; };
+ 		operator const char *() const       { return Get(); };
+ 		wString& ToUpper()	{ strupr(buffer);  return *this; };
