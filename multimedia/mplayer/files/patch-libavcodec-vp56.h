ce4c8148a4f56e83bd1af879ac2225141f275aab
Rong-En Fan <rafan@FreeBSD.org>
2006-12-22 01:07:53 +0000

    - Fix build on 4.x
    PR:		ports/106990
    Submitted by:	KATO Tsuguru <tkato432 at yahoo.com>
    Approved by:	Thomas E. Zander <riggs at rrr.de> (maintainer)

---
+++ multimedia/mplayer/files/patch-libavcodec-vp56.h
@@ -0,0 +1,11 @@
+--- libavcodec/vp56.h.orig	Mon Oct 23 07:32:34 2006
++++ libavcodec/vp56.h	Wed Dec 13 03:03:41 2006
+@@ -24,7 +24,7 @@
+ #ifndef VP56_H
+ #define VP56_H
+ 
+-#include <stdint.h>
++#include <inttypes.h>
+ 
+ #include "vp56data.h"
+ #include "dsputil.h"
---
+++ multimedia/mplayer/files/patch-libmpcodecs-vf_ass.c
@@ -0,0 +1,11 @@
+--- libmpcodecs/vf_ass.c.orig	Mon Oct 23 07:32:24 2006
++++ libmpcodecs/vf_ass.c	Wed Dec 13 03:04:26 2006
+@@ -23,7 +23,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+-#include <stdint.h>
++#include <inttypes.h>
+ #include <assert.h>
+ 
+ #include "config.h"
