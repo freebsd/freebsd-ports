ea97799905439558b725505b0b6d03c8e62f1daa
ahze <ahze@FreeBSD.org>
2006-12-22 22:37:41 +0000

    Revive gstreamer-plugins-jack

--- Mk/bsd.gstreamer.mk
+++ Mk/bsd.gstreamer.mk
@@ -62,8 +62,8 @@ GST80_SHLIB_VERSION=	1
 #
 _USE_GSTREAMER_ALL=	a52dec aalib annodex bad bz2 cairo cdaudio cdparanoia dts \
 			dv dvd esound faac faad ffmpeg flac gconf gnomevfs \
-			gnonlin good gsm hal ivorbis jpeg ladspa lame libcaca \
-			libmms libpng libvisual mad mpeg2dec \
+			gnonlin good gsm hal ivorbis jack jpeg ladspa lame \
+			libcaca libmms libpng libvisual mad mpeg2dec \
 			musepack neon ogg pango pulse python sdl shout2 sidplay \
 			speex theora ugly vorbis wavpack xvid
 _USE_GSTREAMER80_ALL=	a52dec aalib artsd audiofile cairo cdaudio cdio cdparanoia \
