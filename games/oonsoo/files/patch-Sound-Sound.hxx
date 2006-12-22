1236ad793f65f920950b1df033270240830b889d
Tilman Keskinoz <arved@FreeBSD.org>
2006-12-22 16:36:23 +0000

    Fix build with gcc41

---
+++ games/oonsoo/files/patch-Sound-Sound.hxx
@@ -0,0 +1,11 @@
+--- Sound/Sound.hxx.orig	Fri Dec 22 17:29:08 2006
++++ Sound/Sound.hxx	Fri Dec 22 17:30:21 2006
+@@ -31,7 +31,7 @@
+ 
+   public:
+     // Constructor
+-    Sound::Sound(SampleCollection* sampleCollection);
++    Sound(SampleCollection* sampleCollection);
+ 
+     // Destructor 
+     virtual ~Sound();
---
+++ games/oonsoo/files/patch-UI-Sprite.hxx
@@ -0,0 +1,11 @@
+--- UI/Sprite.hxx.orig	Fri Dec 22 17:31:58 2006
++++ UI/Sprite.hxx	Fri Dec 22 17:32:28 2006
+@@ -26,7 +26,7 @@
+  
+   public:
+     // Constructor
+-    Sprite::Sprite(unsigned char* spriteData);
++    Sprite(unsigned char* spriteData);
+ 
+     // Destructor 
+     virtual ~Sprite();
