b112d78ac073a3630d525a7a84eef0fb8a1757da
Tilman Keskinoz <arved@FreeBSD.org>
2006-12-22 15:21:58 +0000

    Fix build with gcc41

--- sysutils/ndir/files/patch-src-File-Date-StringUtils-Format.h
+++ sysutils/ndir/files/patch-src-File-Date-StringUtils-Format.h
@@ -1,5 +1,14 @@
---- src/File-Date-StringUtils/Format.h.orig	Sat Apr 10 01:41:38 2004
-+++ src/File-Date-StringUtils/Format.h	Mon Sep 26 16:09:59 2005
+--- src/File-Date-StringUtils/Format.h.orig	Fri Apr  9 23:41:38 2004
++++ src/File-Date-StringUtils/Format.h	Fri Dec 22 16:20:15 2006
+@@ -53,7 +53,7 @@
+ private:
+ 
+    template<typename T>
+-   void DecimalFormat::format_internal( char* buffer, T value, std::string typeString);
++   void format_internal( char* buffer, T value, std::string typeString);
+    
+ 
+    
 @@ -65,6 +65,6 @@
      text-containing string to be output on a terminal 
     */
