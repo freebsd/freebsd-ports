43c148549f2fcb4b9b4ae5bbc55601272b0c3540
ahze <ahze@FreeBSD.org>
2006-12-22 23:31:33 +0000

    Add support for gstreamer-plugins-spc

---
+++ multimedia/gstreamer-plugins-bad/files/patch-ext-spc-gstspc.h
@@ -0,0 +1,11 @@
+--- ext/spc/gstspc.h.orig	Fri Dec 22 18:28:46 2006
++++ ext/spc/gstspc.h	Fri Dec 22 18:28:15 2006
+@@ -22,7 +22,7 @@
+ 
+ #include <gst/gst.h>
+ 
+-#include <openspc.h>
++#include <OpenSPC.h>
+ 
+ G_BEGIN_DECLS
+ 
