be19a6192582f5bb0579ec089ac5c0e01273d0ad
Dmitry Sivachenko <demon@FreeBSD.org>
2006-12-23 14:11:38 +0000

    Update to 1.1.3.
    PR:		106653
    Submitted by:	Ying-Chieh Liao <ijliao@csie.nctu.edu.tw>

---
+++ devel/libmatheval/files/patch-config-ltmain.sh
@@ -0,0 +1,15 @@
+--- config/ltmain.sh.orig	Wed Dec 13 11:54:39 2006
++++ config/ltmain.sh	Wed Dec 13 11:54:52 2006
+@@ -5659,10 +5659,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
