efab6f09ca42af252aa61f52c4de91aa46d116f4
thierry <thierry@FreeBSD.org>
2006-12-26 09:34:33 +0000

    Upgrade to 9.0.0-1.

--- french/aster/files/patch-setup.py
+++ french/aster/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Wed Jun 28 17:31:56 2006
-+++ setup.py	Sat Jul  1 20:00:55 2006
-@@ -275,6 +275,8 @@
-          cfg['IFDEF']='LINUX64'
+--- setup.py.orig	Fri Dec 22 17:56:58 2006
++++ setup.py	Sun Dec 24 11:00:55 2006
+@@ -278,6 +278,8 @@
+          cfg['IFDEF'] = 'LINUX64'
        else:
-          cfg['IFDEF']='P_LINUX'
+          cfg['IFDEF'] = 'LINUX'
 +   elif sys.platform[:7] == 'freebsd':
 +      cfg['IFDEF']='FREEBSD'
-    # elif sys.platform == 'win32':
-    #    ???
     elif sys.platform[:4] == 'osf1':
+       cfg['IFDEF']='TRU64'
+    elif sys.platform == 'sunos5':
