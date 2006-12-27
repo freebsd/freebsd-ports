974a1c0f367a5a5e9c34e4631a94bfcfd9894a2b
ehaupt <ehaupt@FreeBSD.org>
2006-12-27 20:50:29 +0000

    - Update to 1.0.3 - Take maintainer-ship

--- mail/isync/files/patch-src-drv_imap.c
+++ mail/isync/files/patch-src-drv_imap.c
@@ -1,5 +1,5 @@
---- src/drv_imap.c.orig	Mon Mar 28 19:41:04 2005
-+++ src/drv_imap.c	Sun Apr 10 13:00:50 2005
+--- ./src/drv_imap.c.orig	Wed Nov  1 08:15:09 2006
++++ ./src/drv_imap.c	Wed Dec 27 21:43:01 2006
 @@ -26,6 +26,7 @@
  
  #include <assert.h>
@@ -7,4 +7,14 @@
 +#include <limits.h>
  #include <sys/mman.h>
  #include <sys/time.h>
- #include <stdlib.h>nistd.h>
+ #include <stdlib.h>
+@@ -1243,9 +1244,7 @@
+ 	} else {
+ 		memset( &addr, 0, sizeof(addr) );
+ 		addr.sin_port = htons( srvc->port ? srvc->port :
+-#ifdef HAVE_LIBSSL
+ 		                       srvc->use_imaps ? 993 :
+-#endif
+ 		                       143 );
+ 		addr.sin_family = AF_INET;
+ 
