dbbde9798476e37e659a348030d8840142337b98
miwi <miwi@FreeBSD.org>
2007-01-03 17:17:43 +0000

    - Fix build with amd64
    PR:		107478
    Submitted by:	Daniel Roethlisberger <daniel@roe.ch> (maintainer)

--- security/nmap/files/patch-output.cc
+++ security/nmap/files/patch-output.cc
@@ -1,12 +1,20 @@
 --- output.cc.orig	Fri Dec  8 04:01:19 2006
-+++ output.cc		Mon Dec 18 15:50:13 2006
-@@ -742,8 +742,8 @@
++++ output.cc	Wed Jan  3 17:10:43 2007
+@@ -109,6 +109,7 @@
+ #include "NmapOutputTable.h"
+ #include "MACLookup.h"
+ 
++#include <sys/param.h>
+ #include <string>
+ 
+ /* Workaround for lack of namespace std on HP-UX 11.00 */
+@@ -742,8 +743,8 @@
    case LOG_MACHINE:
    case LOG_SKID:
    case LOG_XML:
 -#ifdef WIN32
 -	  apcopy = ap;
-+#if defined(WIN32) || (defined(FREEBSD) && (OSVERSION < 500000))
++#if defined(WIN32) || (defined(FREEBSD) && (__FreeBSD_version < 500000))
 +    apcopy = ap;
  #else
      va_copy(apcopy, ap); /* Needed in case we need to so a second vnsprintf */
