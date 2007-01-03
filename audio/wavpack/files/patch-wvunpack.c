004280ba11aff30043710ac112eccd78c5ba5c4f
Jeremy Messenger <mezz@FreeBSD.org>
2007-01-03 02:19:22 +0000

    -Update to 4.40.0. -Use USE_DOS2UNIX and get rid of ^M in our files/patch-*. -INSTALLS_SHLIB -> USE_LDCONFIG.

--- audio/wavpack/files/patch-wvunpack.c
+++ audio/wavpack/files/patch-wvunpack.c
@@ -1,15 +1,15 @@
---- wvunpack.c.orig	Thu Apr  6 05:48:33 2006
-+++ wvunpack.c	Mon May 15 03:42:14 2006
-@@ -1040,6 +1040,8 @@
- static UTF8ToAnsi (char *string, int len)
+--- cli/wvunpack.c.orig	Thu Apr  6 05:48:33 2006
++++ cli/wvunpack.c	Mon May 15 03:42:14 2006
+@@ -1374,6 +1374,8 @@
+ static void UTF8ToAnsi (char *string, int len)
  {
-     int max_chars = strlen (string);
+     int max_chars = (int) strlen (string);
 +
 +    iconv_t converter;
  #if defined (WIN32)
      unsigned short *temp = malloc ((max_chars + 1) * 2);
      int act_chars = UTF8ToWideChar (string, temp);
-@@ -1066,7 +1068,7 @@
+@@ -1400,7 +1402,7 @@
  
      memset(temp, 0, len);
      old_locale = setlocale (LC_CTYPE, "");
