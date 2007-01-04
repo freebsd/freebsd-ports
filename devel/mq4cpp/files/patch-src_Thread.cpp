337ae3e17f8397fd7c919b7bc179cf70395b34d2
vanilla <vanilla@FreeBSD.org>
2007-01-04 09:17:03 +0000

    Add mq4cpp, it's Message queue for c++.

---
+++ devel/mq4cpp/files/patch-src_Thread.cpp
@@ -0,0 +1,11 @@
+--- src/Thread.cpp.orig	Thu Jan  4 12:31:04 2007
++++ src/Thread.cpp	Thu Jan  4 12:31:22 2007
+@@ -76,7 +76,7 @@
+ #ifdef WIN32
+ 	return GetCurrentThreadId();
+ #else
+-	return pthread_self();
++	return (unsigned long)pthread_self();
+ #endif
+ }
+ 
