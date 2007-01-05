874c88d048265580ae01e9d7ef3243a600b5d18e
Brooks Davis <brooks@FreeBSD.org>
2007-01-05 21:32:19 +0000

    Upgrade drupal to 4.7.5 fixing a couple security issues. Upgrade drupal-pubcookie and drupal-textile to the 4.7 versions.
    Submitted by:	Nick Hilliard <nick at foobar dot org> (upgrade to 4.7.4)
    Security:	vid:3d8d3548-9d02-11db-a541-000ae42e9b93

--- www/drupal5/bsd.drupal.mk
+++ www/drupal5/bsd.drupal.mk
@@ -12,18 +12,19 @@ DOCSDIR?=	${DRUPAL_DOCSDIR}
 .endif
 
 .if defined(DRUPAL_MODULE)
-DRUPAL_VERSION?=	4.6.0
-DRUPAL_MODDIR?=	${DRUPAL_BASE}/modules
+DRUPAL_VERSION?=	4.7.0
+.if defined(DRUPAL_MODSUBDIR)
+DRUPAL_MODDIR?= ${DRUPAL_BASE}/modules/${DRUPAL_MODSUBDIR}
+.else 
+DRUPAL_MODDIR?= ${DRUPAL_BASE}/modules
+.endif
 DOCSDIR?=	${DRUPAL_DOCSDIR}/${PORTNAME}
 
-# We have to mirror the distfiles because they are automaticly
-# regenerated snapshots that change size even when nothing changes in the
-# files themselves so they will generally be MASTER_SITE_LOCAL
-MASTER_SITES?=	${MASTER_SITE_LOCAL}
+MASTER_SITES?=	http://ftp.osuosl.org/pub/drupal/files/projects/
 PKGNAMEPREFIX=	drupal-
 DIST_SUBDIR=	drupal
 
-RUN_DEPENDS=	${LOCALBASE}/${DRUPAL_BASE}/index.php:${PORTSDIR}/www/drupal
+RUN_DEPENDS+=	${LOCALBASE}/${DRUPAL_BASE}/index.php:${PORTSDIR}/www/drupal
 
 NO_BUILD?=	yes
 
@@ -42,6 +43,9 @@ PLIST_SUB+=	DRUPAL_MODDIR=${DRUPAL_MODDIR}
 PLIST_FILES+=	${MODULE_FILES:C|^|%%DRUPAL_MODDIR%%/|}
 PLIST_FILES+=	${MODULE_CONF_FILES:C|^|%%DRUPAL_MODDIR%%/|:C|$|-dist|}
 PLIST_DIRS+=	${MODULE_DIRS:C|^|%%DRUPAL_MODDIR%%/|}
+.if defined(DRUPAL_MODSUBDIR)
+PLIST_DIRS+=	${DRUPAL_MODDIR}
+.endif
 .if !defined(NOPORTDOCS)
 PLIST_FILES+=	${DOC_FILES:C|^|%%DOCSDIR%%/|}
 PLIST_DIRS+=	${DOC_DIRS:C|^|%%DOCSDIR%%/|}
@@ -51,15 +55,19 @@ PLIST_DIRS+=	%%DOCSDIR%%
 do-install:
 .if defined(MODULE_CONF_DIRS)
 	@${MKDIR} ${MODULE_CONF_DIRS:C|^|${PREFIX}/${DRUPAL_MODDIR}/|}
+	${CHOWN} ${WWWOWN}:${WWWGRP} ${MODULE_CONF_DIRS:C|^|${PREFIX}/${DRUPAL_MODDIR}/|}
 .endif
 .if defined(MODULE_DIRS)
 	@${MKDIR} ${MODULE_DIRS:C|^|${PREFIX}/${DRUPAL_MODDIR}/|}
+	${CHOWN} ${WWWOWN}:${WWWGRP} ${MODULE_DIRS:C|^|${PREFIX}/${DRUPAL_MODDIR}/|}
 .endif
 .for f in ${MODULE_FILES}
 	@${INSTALL_DATA} ${WRKSRC}/${f} ${PREFIX}/${DRUPAL_MODDIR}/${f}
+	${CHOWN} ${WWWOWN}:${WWWGRP} ${PREFIX}/${DRUPAL_MODDIR}/${f}
 .endfor
 .for f in ${MODULE_CONF_FILES}
 	@${INSTALL_DATA} ${WRKSRC}/${f} ${PREFIX}/${DRUPAL_MODDIR}/${f}-dist
+	${CHOWN} ${WWWOWN}:${WWWGRP} ${PREFIX}/${DRUPAL_MODDIR}/${f}-dist
 .endfor
 .if !defined(NOPORTDOCS)
 	@${MKDIR} ${DOCSDIR} ${DOC_DIRS:C|^|${DOCSDIR}/|}
