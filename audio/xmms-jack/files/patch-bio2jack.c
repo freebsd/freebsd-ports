66e089fb6abadfbb3f2ee5053d61f3d508832d01
Martin Wilke <miwi@FreeBSD.org>
2007-01-05 08:02:23 +0000

    - Update to 0.17
    PR:		ports/106448
    Submitted by:	trasz <trasz@pin.if.uz.zgora.pl>
    Approved by:	maintainer timeout

--- audio/xmms-jack/files/patch-bio2jack.c
+++ audio/xmms-jack/files/patch-bio2jack.c
@@ -1,19 +1,16 @@
---- bio2jack.c.orig	Thu Apr  8 10:03:08 2004
-+++ bio2jack.c	Thu Apr  8 10:07:28 2004
-@@ -585,6 +585,7 @@
-       /* and transition to STOPPED */
-       if(this->state == RESET)
-       {
-+		wave_header_t *wh;
-           this->written_client_bytes    = 0;
-           this->played_client_bytes     = 0;  /* number of the clients bytes that jack has played */
+--- bio2jack.c.orig	Sat Jun 17 18:58:16 2006
++++ bio2jack.c	Thu Dec  7 12:51:17 2006
+@@ -2512,11 +2512,12 @@
+   {
+     drv = &outDev[x];
  
-@@ -598,7 +599,7 @@
-           /* free up all of the buffers of audio that are queued */
-           /* NOTE: this needs to be done inside of the callback because */
-           /*  the callback could be using any of these buffers */
--          wave_header_t *wh = this->pPlayPtr;
-+		wh = this->pPlayPtr;
-           while(wh)
-           {
-               wh = wh->pNext;
++    memset(drv, 0, sizeof(jack_driver_t));
++
+     pthread_mutex_init(&drv->mutex, NULL);
+ 
+     getDriver(x);
+ 
+-    memset(drv, 0, sizeof(jack_driver_t));
+     drv->volumeEffectType = linear;
+     drv->deviceID = x;
+ 
