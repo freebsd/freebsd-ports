b1243ce085fd528cec54e3084000da455fa101d8
clsung <clsung@FreeBSD.org>
2007-01-07 14:38:30 +0000

    This is a library for converting Unicode strings to numbers. Standard functions like strtoul and strtod do this for numbers written in the usual Western number system using the Indo-Arabic numerals, but they do not handle other number systems. The main functions take as input a UTF-32 Unicode string and compute the corresponding unsigned integer.
    WWW:    http://billposer.org/Software/libuninum.html

---
+++ devel/libuninum/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Sun Jan 22 03:36:15 2006
++++ ltmain.sh	Sun Jan  7 22:28:13 2007
+@@ -5995,10 +5995,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
