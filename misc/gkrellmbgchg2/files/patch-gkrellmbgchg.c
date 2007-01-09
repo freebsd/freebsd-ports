d8e7a70be8dbc95bc40b05c49539281fc4cc1b9e
Martin Wilke <miwi@FreeBSD.org>
2007-01-09 22:26:12 +0000

    - Update to 0.1.7
    PR:		107632
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- misc/gkrellmbgchg2/files/patch-gkrellmbgchg.c
+++ misc/gkrellmbgchg2/files/patch-gkrellmbgchg.c
@@ -1,6 +1,6 @@
---- gkrellmbgchg.c.orig	Wed Apr 26 17:41:54 2006
-+++ gkrellmbgchg.c	Wed Apr 26 17:54:17 2006
-@@ -114,7 +114,9 @@
+--- gkrellmbgchg.c.orig	Thu Jun 29 05:19:13 2006
++++ gkrellmbgchg.c	Sat Jan  6 02:06:10 2007
+@@ -117,7 +117,9 @@
  {
  	FILE *file;
  	gchar *tmp;
@@ -11,7 +11,7 @@
  	if( filename ) {
  		if( !strncmp( filename, "~/", MIN(2, strlen(filename)) ) )
  			tmp = g_strdup_printf( "%s/%s", g_get_home_dir(), filename+2 );
-@@ -125,7 +127,6 @@
+@@ -128,7 +130,6 @@
  
  #if !defined(WIN32)
  	/* don't load if no force and not modified */
@@ -19,21 +19,3 @@
  	if( stat( tmp, &buf ) == -1 ) {
  		/* something went wrong, we just don't care what for now */
  		GKBG_debug("stat: error on `%s'", tmp);
-@@ -223,6 +224,9 @@
- 	gint num=1;
- 	FILE *idb_file;
- 	struct idb_entry *idb_e;
-+#if !defined(WIN32)
-+	struct stat buf;
-+#endif
- 
- 	if((idb_file = open_imagelist( bgmon.idb, force )) == NULL) return 1;
- 
-@@ -261,7 +265,6 @@
- #if !defined(WIN32)
- 		/* ignore the file if we stat returns -1 (error) and the user wants
- 		 * us to */
--		struct stat buf;
- 		if( bgmon.ignore && (stat( tmp, &buf ) == -1) ) {
- 			GKBG_debug( "ignoring `%s'", tmp );
- 		} else {
