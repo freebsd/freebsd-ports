39c24110db57e9d0bb64e09f51da5b4061c663ff
Maho Nakata <maho@FreeBSD.org>
2007-01-09 05:52:07 +0000

    Migrated to gfortran42. install shared libs as well.

--- math/taucs/files/patch-config+FreeBSD.mk
+++ math/taucs/files/patch-config+FreeBSD.mk
@@ -1,35 +1,42 @@
 --- config/FreeBSD.mk.orig	Fri Aug 29 00:13:38 2003
-+++ config/FreeBSD.mk	Mon Mar 13 11:38:31 2006
-@@ -9,15 +9,15 @@
++++ config/FreeBSD.mk	Tue Jan  9 14:18:49 2007
+@@ -8,16 +8,16 @@
+ PATHSEP=/
  DEFFLG=-D
  
- FC        ?= f77
+-FC        ?= f77
 -FFLAGS    += -Os -fno-second-underscore
-+FFLAGS    += -fno-second-underscore
++FC        = %%FC%%
++FFLAGS    += -fno-second-underscore %%FFLAGS%%
  FOUTFLG   =-o 
  
- #CC        = cc
+-#CC        = cc
 -CFLAGS    += -Os -D_POSIX_C_SOURCE=199506L -fPIC
-+CFLAGS    += -D_POSIX_C_SOURCE=199506L -fPIC
++CC        = %%CC%%
++CFLAGS    += -D_POSIX_C_SOURCE=199506L %%CFLAGS%%
  COUTFLG   = -o
  
- LD        = $(CC) 
+-LD        = $(CC) 
 -LDFLAGS   = $(CFLAGS) -static
-+LDFLAGS   = $(CFLAGS)
++LD        = %%FC%%
++LDFLAGS   = -L%%LOCALBASE%%/lib -L./
  LOUTFLG   = $(COUTFLG)
  
  AR        = ar cr
-@@ -26,10 +26,10 @@
+@@ -26,12 +26,12 @@
  RANLIB    = ranlib
  RM        = rm -rf
  
 -LIBBLAS   = -L/usr/local/lib -lf77blas -lcblas -latlas -lg2c
 -LIBLAPACK = -L/usr/local/lib -llapack
-+LIBBLAS   = -L${LOCALBASE}/lib -lf77blas -lcblas -latlas -lg2c
-+LIBLAPACK = -L${LOCALBASE}/lib -llapack
++LIBBLAS   = %%BLAS%%
++LIBLAPACK = %%LAPACK%%
  
 -LIBMETIS  = -L/usr/local/lib -lmetis 
-+LIBMETIS  = -L${LOCALBASE}/lib -lmetis 
++LIBMETIS  = -lmetis 
  
- LIBF77 = -lg2c  
+-LIBF77 = -lg2c  
++LIBF77 = 
  #compat is required for ftime()
+ LIBC   = -lm -lcompat 
+ 
