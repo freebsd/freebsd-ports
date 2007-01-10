7f87d651847db945a0a502326e23514fe314bd5b
Stanislav Sedov <stas@FreeBSD.org>
2007-01-10 19:33:49 +0000

    - Fix for FreeBSD 4.x
    Reported by:	kris

---
+++ devel/fga/files/patch-graph_gen.cpp
@@ -0,0 +1,10 @@
+--- graph_gen.cpp.orig	Sat Nov 25 20:51:17 2006
++++ graph_gen.cpp	Wed Jan 10 22:31:44 2007
+@@ -5,6 +5,7 @@
+ 
+ #include <fstream>
+ #include <iostream>
++#include <stdlib.h>
+ #include <ctime>
+ 
+ using namespace std;
