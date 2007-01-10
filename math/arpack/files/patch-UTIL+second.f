06288f91266003621519d898efa81db6d72b1816
maho <maho@FreeBSD.org>
2007-01-10 14:14:44 +0000

    * gfortran42 has "INTRINSIC etime"   http://icl.cs.utk.edu/lapack-forum/archives/lapack/msg00193.html * Bump port revision

---
+++ math/arpack/files/patch-UTIL+second.f
@@ -0,0 +1,10 @@
+--- UTIL/second.f~	Tue Oct  3 03:56:44 1995
++++ UTIL/second.f	Wed Jan 10 23:06:17 2007
+@@ -21,7 +21,6 @@
+ *     ..
+ *     .. External Functions ..
+       REAL               ETIME
+-      EXTERNAL           ETIME
+ *     ..
+ *     .. Executable Statements ..
+ *
