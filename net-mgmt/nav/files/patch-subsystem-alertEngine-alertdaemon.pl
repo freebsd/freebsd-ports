3fef709305c4916a610e5b9585aae5c0ba3f8075
des <des@FreeBSD.org>
2007-01-15 10:54:00 +0000

    Upgrade to 3.1.0.  Note that some run-time dependencies may be missing; these will be added as soon as I can track them down properly (probably along with an upgrade to 3.1.1)
    This update incorporates contributions from Dru Lavigne and Dmitriy
    Kirhlarov, and was sponsored by the University of Tromsø (uit.no).

--- net-mgmt/nav/files/patch-subsystem-alertEngine-alertdaemon.pl
+++ net-mgmt/nav/files/patch-subsystem-alertEngine-alertdaemon.pl
@@ -1,5 +1,5 @@
---- subsystem/alertEngine/alertdaemon.pl.orig	Tue Feb 15 14:05:23 2005
-+++ subsystem/alertEngine/alertdaemon.pl	Tue Feb 15 14:07:58 2005
+--- subsystem/alertEngine/alertdaemon.pl.orig	Fri Sep  9 10:52:00 2005
++++ subsystem/alertEngine/alertdaemon.pl	Mon Aug 21 10:16:23 2006
 @@ -37,9 +37,16 @@
  use IO::Handle;
  
@@ -17,15 +17,16 @@
  ####################################################
  ## Engine 
  ####################################################
-@@ -66,6 +73,7 @@
+@@ -66,7 +73,7 @@
+ 		die "Cannot open pidfile";
  	    my ($pid, $tid) = split / /, <pid_file>;
  	    close(pid_file);
- 	    open ps_file, "ps -e | grep $pid | wc -l|";
+-	    open ps_file, "ps -e | grep $pid | wc -l|";
 +	    open ps_file, "$pscmd | grep $pid | wc -l|";
  	    my $count=<ps_file>;
  	    close(ps_file);
  	    if($count==0) {
-@@ -144,7 +152,7 @@
+@@ -145,7 +152,7 @@
  			die "Cannot open pidfile";
  		my ($pid, $tid) = split / /, <pid_file>;
  		close(pid_file);
@@ -34,7 +35,7 @@
  		my $count=<ps_file>;
  		close(ps_file);
  		if($count==0) {
-@@ -179,7 +187,7 @@
+@@ -180,7 +187,7 @@
  	    die "Cannot open pidfile";
  	my ($pid, $tid) = split / /, <pid_file>;
  	close(pid_file);
