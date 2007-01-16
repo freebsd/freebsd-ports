23c23b1ea1d4fc11b69a30bb8aa20e31324414ce
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2007-01-16 10:28:15 +0000

    - update to 0.16 - don't delete user-modified .conf file on deinstall - OPTIONify
    PR:		ports/107651
    Submitted by:	Milan Obuch (maintainer)

--- mail/courier-pythonfilter/files/patch-filters__comeagain.py
+++ mail/courier-pythonfilter/files/patch-filters__comeagain.py
@@ -1,11 +1,23 @@
---- filters/comeagain.py.orig  Mon Dec 13 09:26:13 2004
-+++ filters/comeagain.py       Thu Jul 27 08:34:25 2006
-@@ -27,7 +27,7 @@
-
- # Keep a dictionary of sender/recipient pairs that we've seen before
- _sendersLock = thread.allocate_lock()
--_sendersDir = '/var/state/pythonfilter'
-+_sendersDir = '/var/spool/courier/pythonfilter'
- try:
-     _senders = anydbm.open(_sendersDir + '/correspondents', 'c')
- except:
+--- filters/comeagain.py.orig	Tue Jan  2 22:02:12 2007
++++ filters/comeagain.py	Sun Jan  7 12:35:20 2007
+@@ -89,11 +89,7 @@
+     if foundAll:
+         return ''
+     else:
+-        return('421-Please send the message again.\n'
+-               '421-This is not an indication of a problem:  We require\n'
+-               '421-that any new sender retry their delivery as proof that\n'
+-               '421-they are not spamware or virusware.\n'
+-               '421 Thank you.')
++        return('421 Please send the message again to proof you are not spamware or virusware.')
+ 
+ 
+ if __name__ == '__main__':
+@@ -103,6 +99,6 @@
+     # recipient.  Run this script with the name of that file as an
+     # argument, and it'll validate that email address.
+     if not sys.argv[1:]:
+-        print 'Use:  comeagain.py <control file>'
++        print 'Use: comeagain.py <control file>'
+         sys.exit(1)
+     print doFilter('', sys.argv[1:])
