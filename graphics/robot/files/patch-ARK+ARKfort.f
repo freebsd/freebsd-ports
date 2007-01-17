65bde25fb337b8bff8e41815504f376c5368b204
Maho Nakata <maho@FreeBSD.org>
2007-01-17 21:27:07 +0000

    * Migrate to gfortran. * Bump port revision.

---
+++ graphics/robot/files/patch-ARK+ARKfort.f
@@ -0,0 +1,11 @@
+--- ARK/ARKfort.f~	Wed May  4 00:14:51 2005
++++ ARK/ARKfort.f	Wed Jan 17 18:01:35 2007
+@@ -998,7 +998,7 @@
+ 	character*(10) startext
+ 	integer nway, nchar
+ 	logical hase
+-	integer i, epos, fnn, len, ic
++	integer i, epos, fnn, len, ic, add
+ 
+ 	integer ie, ibigE, ispace
+ 
