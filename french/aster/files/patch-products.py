196304bcc19a10c22bff038c7850fa2c3b827ca9
maho <maho@FreeBSD.org>
2007-01-17 07:51:39 +0000

    * Migrate to gfortran. * Bump port revision.
    Approved by:  portmgr(kris)

--- french/aster/files/patch-products.py
+++ french/aster/files/patch-products.py
@@ -1,6 +1,6 @@
---- products.py.orig	Wed Jun 28 15:53:05 2006
-+++ products.py	Mon Jul 24 16:14:55 2006
-@@ -966,16 +966,18 @@
+--- products.py.orig	Wed Jan  3 01:13:15 2007
++++ products.py	Wed Jan 17 15:14:05 2007
+@@ -833,16 +833,18 @@
             'HOME_PYTHON', 'PYTHON_EXE', 'PYTHONLIB', 'PYMODULES_PREFIX',
             'HOME_MUMPS', 'HOME_ZMAT', 'HOME_MPI',
             'HOME_MED', 'HOME_HDF', 'HOME_CRPCRS',
@@ -24,13 +24,13 @@
 +           'F90INCLUDE',
             'NOBUILD', ],
     )
-    cfg['OTHERLIB'] = cfg.get('OTHERLIB', '')
-@@ -1015,6 +1017,18 @@
+    cfg['ENV_SH']   = cfg.get('ENV_SH', '')
+@@ -885,6 +887,18 @@
        cxxlibs.extend(['stdc++', 'supc++'])
-       zmat_platform='Linux4'
+       zmat_platform='Linux'
        mpilibs.extend(['mpich'])
 +   elif cfg['IFDEF']=='FREEBSD':
-+      opt['MATH_LIST']  = [cfg['LAPACKLIB'], cfg['BLASLIB'], 'g2c']
++      opt['MATH_LIST']  = [cfg['LAPACKLIB'], cfg['BLASLIB'], cfg['ATLASLIB'], 'gfortran']
 +      opt['SYSLIB']     = cfg['SYSLIB']
 +      opt['LDFLAGS']    = cfg['LDFLAGS']
 +      opt['CFLAGS_DBG'] = cfg['CFLAGS_DBG']
@@ -44,7 +44,7 @@
     elif cfg['IFDEF'] == 'LINUX64':
        opt['MATH_LIST']  = ['lapack', 'blas', 'g2c']
        opt['SYSLIB']     = '-Wl,--allow-multiple-definition -Wl,--export-dynamic -lieee -ldl -lpthread -lutil -lm'
-@@ -1066,8 +1080,8 @@
+@@ -939,8 +953,8 @@
        opt['FINCLUDE']   = ''
  
     # ----- F90
@@ -55,7 +55,7 @@
     opt['F90INCLUDE']=''
  
     # ----- check for MED and HDF5 libraries, and HDF5 includes
-@@ -1100,11 +1114,14 @@
+@@ -973,11 +987,14 @@
  
     # ----- MUMPS
     if cfg['HOME_MUMPS'] != '':
@@ -75,7 +75,7 @@
        opt['F90INCLUDE'] += ' -I%s' % os.path.join(cfg['ASTER_ROOT'],cfg['ASTER_VERSION'],'bibf90','include_mumps')
     else:
        opt['MUMPSLIB'] = ''
-@@ -1160,7 +1177,7 @@
+@@ -1033,7 +1050,7 @@
           ftools.findlib_and_set(cfg, 'SCOTCHLIB', lib,
              kargs['libdirs'], cfg['HOME_SCOTCH'],
              err=True, append=True)
