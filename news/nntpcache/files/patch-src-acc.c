65952a8739438625147ebc25a9e93422b17afcef
Yen-Ming Lee <leeym@FreeBSD.org>
2007-01-17 13:38:12 +0000

    - fix a bug to handle duplicate spam.filter in nntpcache.access correctly.   (patch sent back to authors. fix it first before next release)

--- news/nntpcache/files/patch-src-acc.c
+++ news/nntpcache/files/patch-src-acc.c
@@ -1,5 +1,13 @@
 --- src/acc.c.orig	Sat Feb 19 12:16:21 2000
 +++ src/acc.c	Tue Sep 13 23:00:32 2005
+@@ -147,6 +147,7 @@
+ 			if (strEq(fli->name, fi))
+ 			{
+ 				fc->filter = fli->filter;
++				fc->name = fli->name;
+ 				goto cont;
+ 			}
+ 		}
 @@ -195,7 +195,7 @@
  					loge (("bad filter header/scope %s in file %s: %s", scope, fi, buf));
  					Exit(1);
