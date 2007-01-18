5d1049fad2500c4de62cf3b06a99125f99d2d08f
Michael Landin <mich@FreeBSD.org>
2007-01-18 12:52:52 +0000

    - add patch so amarok can understand FreeBSD MSDOS FS.
    Submitted by:	Daniel O'Connor <doconnor@gsoft.com.au>

--- audio/amarok-kde4/files/patch-amarok_src_mediadevice_generic_genericmediadevice.cpp
+++ audio/amarok-kde4/files/patch-amarok_src_mediadevice_generic_genericmediadevice.cpp
@@ -3,7 +3,22 @@ $FreeBSD$
 
 --- amarok/src/mediadevice/generic/genericmediadevice.cpp.orig
 +++ amarok/src/mediadevice/generic/genericmediadevice.cpp
-@@ -406,7 +406,7 @@
+@@ -415,7 +415,13 @@
+         if( m_medium.mountPoint() == (*mountiter)->mountPoint() )
+             m_medium.setFsType( (*mountiter)->mountType() );
+     }
+-    m_actuallyVfat = m_medium.fsType() == "vfat" ? true : false;
++    m_actuallyVfat = m_medium.fsType() == 
++#ifdef __FreeBSD__
++"msdosfs"
++#else
++"vfat"
++#endif
++ ? true : false;
+     m_connected = true;
+     m_transferDir = m_medium.mountPoint();
+     m_initialFile = new GenericMediaFile( 0, m_medium.mountPoint(), this );
+@@ -457,7 +463,7 @@
      debug() << "Renaming: " << src << " to: " << dst << endl;
  
      //do we want a progress dialog?  If so, set last false to true
