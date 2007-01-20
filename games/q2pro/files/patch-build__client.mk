3dd3f4fc83f964754f5a4f04a07ce9f6d36758f8
alepulver <alepulver@FreeBSD.org>
2007-01-20 16:08:29 +0000

    - Disable x86 assembly code on non-i386. - Add X86_ASM option.
    Reported by:	pointyhat

--- games/q2pro/files/patch-build__client.mk
+++ games/q2pro/files/patch-build__client.mk
@@ -1,10 +1,16 @@
---- ./build/client.mk.orig	Mon Dec 25 17:07:02 2006
-+++ ./build/client.mk	Mon Dec 25 17:07:02 2006
-@@ -42,7 +42,7 @@
+--- build/client.mk.orig	Sat Jan 20 12:40:15 2007
++++ build/client.mk	Sat Jan 20 12:40:20 2007
+@@ -41,8 +41,13 @@
+ # linux
  #
  SRCFILES_LINUX = sys_unix.c snd_linux.c cd_linux.c vid_sdl.c in_linux.c
++ifdef USE_X86_ASM
  ASMFILES_LINUX = snd_mixa.s
 -LDFLAGS_LINUX += -lz -ldl -lSDL
++else
++SRCFILES_LINUX += snd_mix.c
++CFLAGS_LINUX += -DC_ONLY
++endif
 +LDFLAGS_LINUX += -lz `sdl-config --libs`
  
  include ../../post.mk
