7f1fdfe509539dd38648434861d1ff9f1ce9b8f9
mezz <mezz@FreeBSD.org>
2007-01-23 19:58:43 +0000

    Update to 0.102.28.20061216, a SVN version. A new port, ghostess, needs this SVN version.
    PR:		ports/108114
    Submitted by:	trasz <trasz@pin.if.uz.zgora.pl>

--- audio/jack/files/patch-libjack-midiport.c
+++ audio/jack/files/patch-libjack-midiport.c
@@ -1,9 +1,9 @@
---- libjack/midiport.c.orig	Wed Dec  6 08:05:17 2006
-+++ libjack/midiport.c	Wed Dec  6 08:05:56 2006
-@@ -72,7 +72,7 @@
+--- libjack/midiport.c.orig	Sat Dec 16 13:50:17 2006
++++ libjack/midiport.c	Fri Jan 19 00:25:32 2007
+@@ -77,7 +77,7 @@
  		(jack_midi_port_info_private_t *) port_buffer;
  	
- 	if (event_idx >= info->info.event_count)
+ 	if (event_idx >= info->event_count)
 -		return ENODATA;
 +		return ENOMSG;
  	
