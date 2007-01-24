e9f205222ad644e2ee8148e9c1c57c414d14f8c3
Martin Wilke <miwi@FreeBSD.org>
2007-01-24 20:39:06 +0000

    Ghostess is a a graphical DSSI host, based on jack-dssi-host, but capable of saving and restoring plugin configuration, as well as specifying MIDI channels and layering synths. ghostess includes three MIDI drivers: an ALSA sequencer MIDI driver, a (clumsy but functional) CoreMIDI driver (which allows ghostess to be used on Mac OS X), and a JACK MIDI driver for use with the MIDI transport in recent versions (>=0.102.27) of JACK. ghostess also comes with a universal DSSI GUI, which attempts to provide GUI services for any DSSI or LADSPA plugin, and may be used with any DSSI host.
    WWW:	http://home.jps.net/~musound/
    
    PR:		ports/108131
    Submitted by:	trasz <trasz at pin.if.uz.zgora.pl>

---
+++ audio/ghostess/files/patch-src-ghostess.c
@@ -0,0 +1,10 @@
+--- src/ghostess.c.orig	Sat Jan 13 19:36:01 2007
++++ src/ghostess.c	Sat Jan 13 19:36:18 2007
+@@ -56,6 +56,7 @@
+ #include <dirent.h>
+ #include <pthread.h>
+ #include <math.h>
++#include <errno.h>
+ 
+ #include <gtk/gtk.h>
+ 
