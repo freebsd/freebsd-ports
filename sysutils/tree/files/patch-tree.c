80f89efd9c3d829a9e51727c1c9695fa3aa54277
Vasil Dimov <vd@FreeBSD.org>
2007-01-25 17:02:16 +0000

    Upgrade sysutils/tree from 1.5.0 to 1.5.1
    New version reported by:	shaun (via portscout)

--- sysutils/tree/files/patch-tree.c
+++ sysutils/tree/files/patch-tree.c
@@ -1,10 +1,23 @@
---- tree.c.orig	Mon Aug 16 02:07:21 2004
-+++ tree.c	Thu Oct 21 11:51:36 2004
-@@ -189,7 +189,6 @@
+--- tree.c.orig	Tue Jan  9 23:07:16 2007
++++ tree.c	Thu Jan 25 18:58:28 2007
+@@ -17,7 +17,6 @@
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  */
+ 
+-#include <features.h>
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <time.h>
+@@ -178,12 +177,6 @@
+ char *sLevel, *curdir, *outfilename = NULL;
  FILE *outfile;
  int *dirs, maxdirs;
- 
+-
+-#ifdef CYGWIN
+-extern int MB_CUR_MAX;
+-#else
 -extern size_t MB_CUR_MAX;
+-#endif
  
  int main(int argc, char **argv)
  {
