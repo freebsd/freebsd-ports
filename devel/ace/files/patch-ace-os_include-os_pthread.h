163fb4cae7301fd7addea54015c489d38d77dc75
sem <sem@FreeBSD.org>
2007-01-28 14:46:35 +0000

    - Update to 5.5.2. - Turn off a stupid test of std::bad_alloc exception. [1]
    Requested by:	kris

--- devel/ace/files/patch-ace-os_include-os_pthread.h
+++ devel/ace/files/patch-ace-os_include-os_pthread.h
@@ -1,11 +1,14 @@
---- ace/os_include/os_pthread.h.orig	Sun Dec  3 21:47:36 2006
-+++ ace/os_include/os_pthread.h	Sun Dec  3 21:51:19 2006
-@@ -345,7 +345,7 @@
+--- ace/os_include/os_pthread.h.orig	Tue May 30 16:15:53 2006
++++ ace/os_include/os_pthread.h	Sun Jan 28 13:50:01 2007
+@@ -339,7 +339,11 @@
  #    undef THR_DAEMON
  
  #    define THR_BOUND               0x00000001
--#    if defined (CHORUS)
-+#    if defined (CHORUS) || (__FreeBSD__)
- #      define THR_NEW_LWP             0x00000000
- #    else
- #      define THR_NEW_LWP             0x00000002
++# if defined (__FreeBSD__)
++#    define THR_NEW_LWP             0x00000000
++# else
+ #    define THR_NEW_LWP             0x00000002
++# endif
+ #    define THR_DETACHED            0x00000040
+ #    define THR_SUSPENDED           0x00000080
+ #    define THR_DAEMON              0x00000100
