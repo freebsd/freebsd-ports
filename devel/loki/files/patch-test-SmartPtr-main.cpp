5f7e11dd4222204cbaffb1b1ccae884d45ded612
Pav Lucistnik <pav@FreeBSD.org>
2007-01-28 01:38:11 +0000

    - Update to 0.1.5
    PR:		ports/105211
    Submitted by:	Charming Sun <snnn119@gmail.com> (maintainer)

---
+++ devel/loki/files/patch-test-SmartPtr-main.cpp
@@ -0,0 +1,11 @@
+--- test/SmartPtr/main.cpp.orig	Mon Nov  6 20:25:41 2006
++++ test/SmartPtr/main.cpp	Mon Nov  6 20:26:02 2006
+@@ -998,7 +998,7 @@
+ 
+ // ----------------------------------------------------------------------------
+ 
+-int main( unsigned int , const char * [] )
++int main(  int , const char * [] )
+ {
+ 
+     DoRefLinkTests();
