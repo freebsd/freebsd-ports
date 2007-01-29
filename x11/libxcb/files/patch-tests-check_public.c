63ab3f195d0b2cc7a3c91fdf5c3293e1eafc969e
Martin Wilke <miwi@FreeBSD.org>
2007-01-29 11:18:46 +0000

    - Update to 1.0
    PR:		108494
    Submitted by:	Ashish Shukla<wahjava@gmail.com> (maintainer)

--- x11/libxcb/files/patch-tests-check_public.c
+++ x11/libxcb/files/patch-tests-check_public.c
@@ -1,6 +1,18 @@
---- tests/check_public.c.orig	Wed Aug 23 02:49:33 2006
-+++ tests/check_public.c	Wed Aug 23 02:49:33 2006
-@@ -97,7 +97,7 @@
+--- tests/check_public.c.orig	Wed Jan 24 15:16:59 2007
++++ tests/check_public.c	Wed Jan 24 15:22:33 2007
+@@ -70,7 +70,10 @@
+ 		else if(test_type == TEST_ENVIRONMENT)
+ 		{
+ 			argument = 0;
+-			setenv("DISPLAY", name, 1);
++			if(name == NULL)
++				unsetenv("DISPLAY");
++			else
++				setenv("DISPLAY", name, 1);
+ 		}
+ 
+ 		got_host = (char *) -1;
+@@ -148,7 +151,7 @@
  
  START_TEST(parse_display_negative)
  {
