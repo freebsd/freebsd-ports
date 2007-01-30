2b455a70ba1fc813769b64872e736d337c4a0d93
anray <anray@FreeBSD.org>
2007-01-30 20:09:12 +0000

    Add Samsung SCH-I730 support.

--- palm/uppc-kmod/files/patch-uppc.c
+++ palm/uppc-kmod/files/patch-uppc.c
@@ -1,6 +1,15 @@
 --- uppc.c.orig	Sun Sep 12 05:39:44 2004
-+++ uppc.c	Wed Jan 24 02:31:02 2007
-@@ -93,6 +93,10 @@
++++ uppc.c	Thu Jan 25 23:40:56 2007
+@@ -62,6 +62,8 @@
+ #define USB_PRODUCT_TOSHIBA_E330	0x0707
+ #define USB_PRODUCT_TOSHIBA_E740	0x0706
+ 
++#define USB_PRODUCT_SAMSUNG_I730	0x6662
++
+ #include <sys/cdefs.h>
+ 
+ #include <sys/param.h>
+@@ -93,6 +95,10 @@
  
  #include <dev/usb/ucomvar.h>
  
@@ -11,3 +20,11 @@
  static int uppcrtvendor = 0;
  static int uppcrtproduct = 0;
  
+@@ -166,6 +172,7 @@
+ 	{{ USB_VENDOR_TOSHIBA, USB_PRODUCT_TOSHIBA_UNK }, 0},
+ 	{{ USB_VENDOR_TOSHIBA, USB_PRODUCT_TOSHIBA_E330 }, 0},
+ 	{{ USB_VENDOR_TOSHIBA, USB_PRODUCT_TOSHIBA_E740 }, 0},
++	{{ USB_VENDOR_SAMSUNG, USB_PRODUCT_SAMSUNG_I730 }, 0},
+ };
+ 
+ /* The ucom_softc structure plus anything else we might need. */
