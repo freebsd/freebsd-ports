888e2ab6b11e31b6f66815a9074c5415f4cca157
garga <garga@FreeBSD.org>
2007-01-31 17:09:02 +0000

    - RBL2SMTPD patch was not logging URL reference
    Reported by:	Patrick Tracanelli <eksffa@freebsdbrasil.com.br>

--- mail/qmail/files/extra-patch-spamcontrol-qmail-smtpd.c
+++ mail/qmail/files/extra-patch-spamcontrol-qmail-smtpd.c
@@ -1,25 +1,34 @@
---- qmail-smtpd.c.orig	Fri Jun 30 15:41:00 2006
-+++ qmail-smtpd.c	Fri Jun 30 15:44:49 2006
-@@ -129,6 +129,18 @@
+--- qmail-smtpd.c.orig	Wed Jan 31 13:03:47 2007
++++ qmail-smtpd.c	Wed Jan 31 13:22:36 2007
+@@ -129,6 +129,27 @@
    logs(s1,s2,s3,s4,s5,s6,s7);
    return;
    }
 +void err_rbl(s1,s2,s3,s4,s5,s6,s7) char *s1, *s2, *s3, *s4, *s5, *s6, *s7; {
++  char *rblmatch;
 +  stralloc rblmessage = {0};
++  stralloc new_s1 = {0};
++
++  rblmatch = env_get("RBLMATCH");
 +
 +  if (!stralloc_copys(&rblmessage,"553 sorry, your IP is listed on a RBL list ")) die_nomem(); 
-+  if (!stralloc_cats(&rblmessage,env_get("RBLMATCH"))) die_nomem(); 
++  if (!stralloc_cats(&rblmessage,rblmatch)) die_nomem(); 
 +  if (!stralloc_cats(&rblmessage," (#5.7.1)\r\n")) die_nomem(); 
 +  if (!stralloc_0(&rblmessage)) die_nomem(); 
 +
++  if (!stralloc_copys(&new_s1, s1)) die_nomem();
++  if (!stralloc_cats(&new_s1, " ")) die_nomem();
++  if (!stralloc_cats(&new_s1, rblmatch)) die_nomem();
++  if (!stralloc_0(&new_s1)) die_nomem(); 
++
 +  out(rblmessage.s); 
-+  logs(s1,s2,s3,s4,s5,s6,s7);
++  logs(new_s1.s,s2,s3,s4,s5,s6,s7);
 +  return;
 +  }
  void err_brcptto(s1,s2,s3,s4,s5,s6,s7) char *s1, *s2, *s3, *s4, *s5, *s6, *s7; {
    out("550 sorry, your envelope recipient is in my badrcptto list (#5.7.1)\r\n");
    logs(s1,s2,s3,s4,s5,s6,s7);
-@@ -270,6 +282,8 @@
+@@ -270,6 +291,8 @@
  
  char *auth;
  char *reqauth;
@@ -28,7 +37,7 @@
  int smtpauth = 0;
  int seenauth = 0;
  
-@@ -388,6 +402,9 @@
+@@ -388,6 +411,9 @@
    if(env_get("UCSPITLS")) starttls = 1;
    if(starttls) reqttls = env_get("REQUIRETLS");
  
@@ -38,7 +47,7 @@
  #ifdef RELAYMAILFROM
    if (!relayclient) {
      relaymailfromok = control_readfile(&relaymailfrom,"control/relaymailfrom",0);
-@@ -876,6 +893,10 @@
+@@ -876,6 +902,10 @@
        flagerrcpts++;
        return; 
      } 
