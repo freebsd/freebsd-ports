43fa3c207594eef898716f01ac9f9682694976b1
gabor <gabor@FreeBSD.org>
2007-02-02 14:13:13 +0000

    This AOLserver module performs SHA1 hashes.
    WWW: http://www.aolserver.com/
    
    - Martin Matuska
    martin@matuska.org
    
    PR:		ports/105781
    Submitted by:	Martin Matuska <martin@matuska.org>
    Approved by:	erwin (mentor)

---
+++ security/aolserver-nssha1/files/patch-nssha1-nssha1.c
@@ -0,0 +1,12 @@
+--- ../nssha1-0.1/nssha1.c.orig	Mon Sep 30 02:25:04 2002
++++ ../nssha1-0.1/nssha1.c	Thu Jul 28 14:47:01 2005
+@@ -136,9 +136,6 @@
+     return NS_OK;
+ }
+ 
+-typedef unsigned int u_int32_t;
+-typedef unsigned char u_int8_t;
+-
+ /*** FROM sha.h: ***/
+ 
+ /*
