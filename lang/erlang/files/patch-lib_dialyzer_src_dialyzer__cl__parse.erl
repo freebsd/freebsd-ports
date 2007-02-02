39a9b99bbcb5e463b8a763beeff4760d3b678599
Jimmy Olgeni <olgeni@FreeBSD.org>
2007-02-02 13:07:42 +0000

    Upgrade to version R11B-3.

--- lang/erlang/files/patch-lib_dialyzer_src_dialyzer__cl__parse.erl
+++ lang/erlang/files/patch-lib_dialyzer_src_dialyzer__cl__parse.erl
@@ -3,13 +3,12 @@ $FreeBSD$
 
 --- lib/dialyzer/src/dialyzer_cl_parse.erl.orig
 +++ lib/dialyzer/src/dialyzer_cl_parse.erl
-@@ -155,8 +155,7 @@
-   put(dialyzer_options_from, byte_code),
-   put(dialyzer_options_libs, ?DEFAULT_LIBS),
+@@ -159,7 +159,7 @@
+   throw({dialyzer_cl_parse_error, Msg}).
  
--  InitPltDir = filename:join([code:lib_dir(dialyzer),
--			      "plt","dialyzer_init_plt"]),
-+  InitPltDir = filename:join([os:getenv ("HOME"), ".dialyzer_init_plt"]),
-   put(dialyzer_init_plt, InitPltDir),
-   put(dialyzer_warnings, []).
- 
+ init() ->
+-  InitPlt = filename:join([code:lib_dir(dialyzer), "plt","dialyzer_init_plt"]),
++  InitPlt = filename:join([os:getenv ("HOME"), "plt","dialyzer_init_plt"]),
+   put(dialyzer_init_plt, InitPlt),
+   put(dialyzer_only_check_init_plt, false),
+   put(dialyzer_options_gui, true),
