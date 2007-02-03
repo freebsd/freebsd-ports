6b6cf2700c18ec98fdd5f3a9466447dee5d3acd4
nox <nox@FreeBSD.org>
2007-02-03 22:15:28 +0000

    Update to 2007-02-03 snapshot:
    - Fix `could not open hard disk image '/dev/ad0'' (e.g.) when open(2)
    fails with EPERM instead of EACCES for ro devices.
    - PIIX4 support, by Aurelien Jarno.
    - Gallileo GT64xxx support, by Aurelien Jarno.
    - MIPS Malta system and devices support, by Aurelien Jarno and Stefan Weil.
    - ARM ELF loader.
    - Improved console handling, thanks Stefan Weil.
    - Disable Malta floppy controller for now, by Aurelien Jarno.
    - Big endian support for Gallileo, by Aurelien Jarno.
    - Add more ATAPI CDROM DMA support, by Juergen Keil.
    - Add ARM Angel semihosting to system emulation.
    - Don't resume guest when gdb connection terminates and -S specified.
    - mips: Implementing dmfc/dmtc.
    - Fix DMA timeouts on FreeBSD, by Carlo Marcelo Arenas Belon.
    - New multiple snapshot support for VMDK, by Igor Lvovsky.
    - Add support for 82371FB (Step A1) and Improved support for 82371SB
    (Function 1), by Carlo Marcelo Arenas Belon.
    - Accept -help.
    - Accept --foo as an alias for -foo.
    - Add nodelay option for TCP character devices.
    - Use standard character device interface for gdbstub.
    - GDB hosted syscalls.
    - Upgrade the apic version_id, by Don Laor.
    - Save some vm space for the regular program loading zone, by
    Pierre d'Herbemont
    - script=no for the TUN/TAP net option, by Jean-Christian de Rivaz
    - Sparc arm/mips/sparc register patch, by Martin Bochnig.
    - PIIX4 SMBus host, EEPROM device emulation, by Ed Swierk.
    - sem* and msg* for qemu, by Kirill Shutemov.
    - And some more bugfixes.
    
    Approved by:	miwi (mentor, implicit)

--- emulators/qemu/files/patch-cpu-exec.c
+++ emulators/qemu/files/patch-cpu-exec.c
@@ -1,14 +1,13 @@
---- qemu/cpu-exec.c.orig	Wed Jan 31 16:58:03 2007
-+++ qemu/cpu-exec.c	Wed Jan 31 17:08:11 2007
+Index: qemu/cpu-exec.c
 @@ -226,9 +226,9 @@
  
  int cpu_exec(CPUState *env1)
  {
--    int saved_T0, saved_T1;
-+    long saved_T0, saved_T1;
+-    target_ulong saved_T0, saved_T1;
++    unsigned long saved_T0, saved_T1;
  #if defined(reg_T2)
--    int saved_T2;
-+    long saved_T2;
+-    target_ulong saved_T2;
++    unsigned long saved_T2;
  #endif
      CPUState *saved_env;
  #if defined(TARGET_I386)
