7de9bf4d1708e3a6e41c30529d63fedd31df5a4d
Alejandro Pulver <alepulver@FreeBSD.org>
2007-02-04 22:25:00 +0000

    - Fix building on amd64 and probably ia64.
    Reported by:	pointyhat

---
+++ games/quake2-lights/files/patch-lights__src__g_items.c
@@ -0,0 +1,10 @@
+--- ./lights/src/g_items.c.orig	Sun Feb  4 16:20:11 2007
++++ ./lights/src/g_items.c	Sun Feb  4 16:20:32 2007
+@@ -1794,6 +1794,7 @@
+ 		60,                   /*ATTILA respwan after 60 secs*/ 
+ 		NULL,
+ 		0,
++		0,
+ 		NULL,
+ 		0,
+ /* precache */ "hover/hovidle1.wav items/damage.wav items/damage2.wav items/damage3.wav"
