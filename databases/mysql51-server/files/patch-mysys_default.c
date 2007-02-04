d19c3823ea68c78e4fab7339abd9baa5d57c5022
ale <ale@FreeBSD.org>
2007-02-04 09:32:18 +0000

    Update to 5.1.15 release.

--- databases/mysql51-server/files/patch-mysys_default.c
+++ databases/mysql51-server/files/patch-mysys_default.c
@@ -1,6 +1,6 @@
---- mysys/default.c.orig	Thu May 26 15:26:56 2005
-+++ mysys/default.c	Fri Jun  3 17:16:59 2005
-@@ -517,7 +517,7 @@
+--- mysys/default.c.orig	Tue Jan 30 09:34:13 2007
++++ mysys/default.c	Sat Feb  3 17:12:31 2007
+@@ -608,7 +608,7 @@
    {
      MY_STAT stat_info;
      if (!my_stat(name,&stat_info,MYF(0)))
@@ -9,11 +9,12 @@
      /*
        Ignore world-writable regular files.
        This is mainly done to protect us to not read a file created by
-@@ -867,9 +867,8 @@
-     *ptr++= env;
- #endif
+@@ -1013,9 +1013,9 @@
+ #else
    *ptr++= "/etc/";
+   *ptr++= "/etc/mysql/";
 +  *ptr++= "%%PREFIX%%/etc/";
++  *ptr++= "%%PREFIX%%/etc/mysql/";
  #endif
 -  if ((env= getenv(STRINGIFY_ARG(DEFAULT_HOME_ENV))))
 -    *ptr++= env;
