4be9bd4f717167bf739efe076e86a81feee53125
Rong-En Fan <rafan@FreeBSD.org>
2007-02-05 18:33:41 +0000

    - Fix build on amd64 and gcc4
    PR:		ports/108720
    Submitted by:	Stephen Montgomery-Smith <stephen at math.missouri.edu> (maintainer)

--- x11/accessx/files/patch-Access.C
+++ x11/accessx/files/patch-Access.C
@@ -1,5 +1,5 @@
---- Access.C.orig	Mon Dec 16 20:35:51 2002
-+++ Access.C	Mon Dec 16 20:36:29 2002
+--- Access.C.orig	Tue Mar 13 23:39:55 2001
++++ Access.C	Tue Jan 30 22:18:00 2007
 @@ -21,6 +21,8 @@
  #include <X11/XKBlib.h>
  #include "Access.h"
@@ -9,3 +9,21 @@
  //
  // Access()
  //
+@@ -152,7 +154,7 @@
+     //Get the state of the keyboard.
+ 
+     XkbDescPtr xkb = XkbGetMap(display, 0, XkbUseCoreKbd);
+-    if ((int)xkb == BadAlloc || xkb == NULL)
++    if (xkb == NULL)
+       {
+ 	return 5; /*AccessKeyboardQueryFailure;*/
+       }
+@@ -261,7 +263,7 @@
+     //Get the state of the keyboard.
+ 
+     XkbDescPtr xkb = XkbGetMap(display, 0, XkbUseCoreKbd);
+-    if ((int)xkb == BadAlloc || xkb == NULL)
++    if (xkb == NULL)
+       {
+ 	return 5; /*AccessKeyboardQueryFailure;*/
+       }
