e75ff3b34bf309d48503e7b181f65759abe86a5e
Oliver Lehmann <oliver@FreeBSD.org>
2007-02-06 20:39:07 +0000

    propper style to fit into Mk/ directory where I hope it will go verry soon

--- x11-wm/xfce4/bsd.xfce.mk
+++ x11-wm/xfce4/bsd.xfce.mk
@@ -1,3 +1,20 @@
+#
+# $FreeBSD$
+#
+
+.if !defined(_POSTMKINCLUDED) && !defined(Xfce_Pre_Include)
+
+Xfce_Pre_Include=		bsd.xfce.mk
+Xfce_Include_MAINTAINER=	oliver@FreeBSD.org
+
+# This file contains some variable definitions that are supposed to
+# make your life easier when dealing with ports related to the Xfce
+# desktop environment. It's automatically included when USE_XFCE
+# is defined in the ports' makefile.
+
+_USE_XFCE_ALL=			configenv libexo libgui libutil libmcs mcsmanager panel \
+				thunar wm xfdev
+
 MASTER_SITE_SUBDIR?=		xfce-4.4.0
 
 configenv_CONFIGURE_ENV=	CPPFLAGS="${CPPFLAGS} -I${LOCALBASE}/include -L${LOCALBASE}/lib"
@@ -22,9 +39,23 @@ wm_RUN_DEPENDS=			xfwm4:${PORTSDIR}/x11-wm/xfce4-wm
 
 xfdev_RUN_DEPENDS=		xfce4-dev-tools:${PORTSDIR}/devel/xfce4-dev-tools
 
+.endif
+
+.if defined(_POSTMKINCLUDED) && !defined(Xfce_Post_Include)
+
+Xfce_Post_Include=		bsd.xfce.mk
+
 .for component in ${USE_XFCE}
 BUILD_DEPENDS+=	${${component}_BUILD_DEPENDS}
 LIB_DEPENDS+=	${${component}_LIB_DEPENDS}
 RUN_DEPENDS+=	${${component}_RUN_DEPENDS}
 CONFIGURE_ENV+=	${${component}_CONFIGURE_ENV}
 .endfor
+
+.for component in ${USE_XFCE}
+. if ${_USE_XFCE_ALL:M${component}}==""
+IGNORE=	cannot install: Unknown component ${component}
+. endif
+.endfor
+
+.endif
