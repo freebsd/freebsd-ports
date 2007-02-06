c8369be915cec631d38643b13050bc0854f81326
Juergen Lock <nox@FreeBSD.org>
2007-02-06 20:46:29 +0000

    Update to 1.3.0pre10:
    - Changed license to GPL version 2
    - x86_64 full virtualization support
    
    Approved by:	miwi (mentor, implicit)

--- emulators/kqemu-kmod-devel/files/patch-kqemu-freebsd.c
+++ emulators/kqemu-kmod-devel/files/patch-kqemu-freebsd.c
@@ -1,12 +1,4 @@
 Index: kqemu-freebsd.c
-@@ -236,6 +236,7 @@
- };
- 
- static int kqemu_ref_count = 0;
-+static int max_locked_pages;
- static struct kqemu_global_state *kqemu_gs = NULL;
- 
- #if __FreeBSD_version < 500000
 @@ -321,6 +321,9 @@
  #if __FreeBSD_version >= 500000
      dev->si_drv1 = NULL;
