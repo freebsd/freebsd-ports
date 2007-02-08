c935806fc170242a5a175dd76dfd9c60a0653c7b
Martin Wilke <miwi@FreeBSD.org>
2007-02-08 22:35:00 +0000

    - Update to 1.9001
    PR:		108869
    Submitted by:	Martin Matuska <martin@matuska.org>

--- graphics/jasper/files/patch-src::libjasper::jpc::jpc_cs.c
+++ graphics/jasper/files/patch-src::libjasper::jpc::jpc_cs.c
@@ -1,14 +1,11 @@
-
-$FreeBSD$
-
---- src/libjasper/jpc/jpc_cs.c	2003/05/07 16:24:56	1.1
-+++ src/libjasper/jpc/jpc_cs.c	2003/05/07 16:25:31
-@@ -337,7 +337,7 @@
+--- src/libjasper/jpc/jpc_cs.c.orig	Fri Jan 19 21:43:07 2007
++++ src/libjasper/jpc/jpc_cs.c	Wed Feb  7 11:05:57 2007
+@@ -288,7 +288,7 @@
  			jpc_ms_dump(ms, stderr);
  		}
  
 -		if (JAS_CAST(ulong, jas_stream_tell(tmpstream)) != ms->len) {
 +		if (JAS_CAST(unsigned long, jas_stream_tell(tmpstream)) != ms->len) {
- 			fprintf(stderr,
- 			  "warning: trailing garbage in marker segment (%ld bytes)\n",
+ 			jas_eprintf("warning: trailing garbage in marker segment (%ld bytes)\n",
  			  ms->len - jas_stream_tell(tmpstream));
+ 		}
