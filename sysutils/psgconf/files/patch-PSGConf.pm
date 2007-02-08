29231548a8f6c931b083bf1ac9b05e34f6d17032
bsam <bsam@FreeBSD.org>
2007-02-08 21:02:44 +0000

    The patch was totally rewritten by the maintainer: . update to the new version 3.2.3; . fix package building/installing/deinstalling; . remove BROKEN.
    PR:		107803
    Submitted by:	bsam (me)
    Approved by:	pirjyk (maintainer)

--- sysutils/psgconf/files/patch-PSGConf.pm
+++ sysutils/psgconf/files/patch-PSGConf.pm
@@ -1,15 +1,33 @@
---- ./lib/PSGConf.pm.orig	Wed May  3 08:55:41 2006
-+++ ./lib/PSGConf.pm	Thu May  4 10:21:24 2006
-@@ -34,7 +34,7 @@
+--- ./lib/PSGConf.pm.orig	Sat Sep 23 13:44:35 2006
++++ ./lib/PSGConf.pm	Thu Jan 11 11:15:45 2007
+@@ -14,6 +14,7 @@
+ 
+ use strict;
+ 
++use File::Basename;
+ use File::Copy;
+ use File::Path;
+ use File::stat;
+@@ -34,8 +35,8 @@
  my %defaults = (
  	config_dir		=> '/usr/local/share/psgconf/config',
  	files_dir			=> '/usr/local/share/psgconf/files',
 -	modules_file		=> '/etc/psgconf_modules',
+-	tmpdir			=> "/var/tmp/$0.$$",
 +	modules_file		=> '/usr/local/etc/psgconf_modules',
- 	tmpdir			=> "/var/tmp/psgconf.$$",
++	tmpdir			=> '/var/tmp/' . basename $0 . ".$$",
  	verbose			=> 0,
  	do_fix			=> 0,
-@@ -678,7 +678,7 @@
+ 	restart_daemons	=> 1,
+@@ -62,6 +63,7 @@
+ 
+ 	### Set from the environment variables if 
+ 	### not set on the command line.
++	$0 = basename $0;
+ 	$self->{tmpdir} = $ENV{'PSGCONF_TMPDIR'} . "/$0.$$"
+ 		if ( !defined $self->{tmpdir} && 
+ 			defined $ENV{'PSGCONF_TMPDIR'} && 
+@@ -677,7 +679,7 @@
  =item modules_file
  
  Full path to B<psgconf> modules file.  Default is
