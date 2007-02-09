1a563330eba37a33d45d7ef3edbf29c798574e1c
nork <nork@FreeBSD.org>
2007-02-09 19:10:52 +0000

    Fix wrong handling command line arguments.
    Submitted by:	Kouhei Sutou <kou@cozmixng.org>

--- net/DarwinStreamingServer/files/patch-MP3Broadcaster::BroadcasterMain.cpp
+++ net/DarwinStreamingServer/files/patch-MP3Broadcaster::BroadcasterMain.cpp
@@ -1,5 +1,23 @@
 --- MP3Broadcaster/BroadcasterMain.cpp.orig	Sat Mar 12 08:24:54 2005
-+++ MP3Broadcaster/BroadcasterMain.cpp	Sun Aug 13 00:37:12 2006
++++ MP3Broadcaster/BroadcasterMain.cpp	Sat Feb 10 03:54:31 2007
+@@ -85,7 +85,7 @@
+     char* config = NULL;
+     char* playList = NULL;
+     char* workingDir = NULL;
+-    char ch;
++    int ch;
+     bool preflight = false;
+     bool checkMP3s = false;
+     char*   errorlog = NULL;
+@@ -112,7 +112,7 @@
+         ::exit(0);
+     }
+ 
+-    while ( (ch = getopt(argc,argv, "vdixXa:p:c:l:e:w:")) != EOF ) // opt: means requires option
++    while ( (ch = getopt(argc,argv, "vdixXa:p:c:l:e:w:")) != -1 ) // opt: means requires option
+     {
+         switch(ch)
+         {
 @@ -216,7 +216,7 @@
  
  struct sigaction act;
