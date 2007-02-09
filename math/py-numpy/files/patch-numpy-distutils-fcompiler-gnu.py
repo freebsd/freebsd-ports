8c1ef3292fd261803d56707c5591fa4ef0e13567
Diane Bruce <db@FreeBSD.org>
2007-02-09 18:54:54 +0000

    - finish g77 to gfortran conversion - change maintainer address to FreeBSD.org one - fold in changes suggested by [1]
    PR:             107022, 108807 (based on) [1]
    Submitted by:   thinker@heaven.branda.to [1]
    Approved by: ehaupt (mentor)

--- math/py-numpy/files/patch-numpy-distutils-fcompiler-gnu.py
+++ math/py-numpy/files/patch-numpy-distutils-fcompiler-gnu.py
@@ -1,5 +1,14 @@
---- numpy/distutils/fcompiler/gnu.py~	Mon Oct  9 16:46:59 2006
-+++ numpy/distutils/fcompiler/gnu.py	Wed Jan 17 12:26:12 2007
+--- numpy/distutils/fcompiler/gnu.py.orig	Mon Oct  9 03:46:59 2006
++++ numpy/distutils/fcompiler/gnu.py	Thu Jan 25 21:48:15 2007
+@@ -22,7 +22,7 @@
+     #         GNU Fortran 0.5.25 20010319 (prerelease)
+     # Redhat: GNU Fortran (GCC 3.2.2 20030222 (Red Hat Linux 3.2.2-5)) 3.2.2 20030222 (Red Hat Linux 3.2.2-5)
+ 
+-    for fc_exe in map(find_executable,['g77','f77']):
++    for fc_exe in map(find_executable,['gfortran', 'gfortran41', 'gfortran42', 'g77','f77']):
+         if os.path.isfile(fc_exe):
+             break
+     executables = {
 @@ -247,7 +247,7 @@
      # OS X: GNU Fortran 95 (GCC) 4.1.0
      #       GNU Fortran 95 (GCC) 4.2.0 20060218 (experimental)
--- math/py-numpy/files/patch-numpy-distutils-system_info.py
+++ math/py-numpy/files/patch-numpy-distutils-system_info.py
@@ -1,5 +1,5 @@
 --- numpy/distutils/system_info.py.orig	Tue Oct  3 00:35:22 2006
-+++ numpy/distutils/system_info.py	Sat Dec  9 22:51:47 2006
++++ numpy/distutils/system_info.py	Sun Feb  4 19:46:15 2007
 @@ -860,6 +860,8 @@
          atlas_1 = None
          for d in lib_dirs:
@@ -9,3 +9,22 @@
              lapack_atlas = self.check_libs2(d,['lapack_atlas'],[])
              if atlas is not None:
                  lib_dirs2 = [d] + self.combine_paths(d,['atlas*','ATLAS*'])
+@@ -935,6 +937,7 @@
+             else:
+                 info['language'] = 'f77'
+ 
++        info['language'] = 'f77'
+         self.set_info(**info)
+ 
+ class atlas_blas_info(atlas_info):
+@@ -1188,8 +1191,9 @@
+         atlas_version = None
+         need_lapack = 0
+         need_blas = 0
++	lapack_type = self.cp.get(self.section, 'lapack_type').strip()
+         info = {}
+-        if atlas_info:
++        if lapack_type == 'atlas' and atlas_info:
+             version_info = atlas_info.copy()
+             atlas_version = get_atlas_version(**version_info)
+             if not atlas_info.has_key('define_macros'):
