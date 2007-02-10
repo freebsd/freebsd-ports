8e9c2a931a30aba26a293057cab2640e05e83e1d
nivit <nivit@FreeBSD.org>
2007-02-10 21:44:34 +0000

    - Updated to 0.12.1
    PR:		ports/109005
    Submitted by:	CHAO Shin <quakelee at cn.freebsd.org>
    Approved by:	alexbl (mentor, implicit)

--- astro/libnova/files/patch-src__utility.c
+++ astro/libnova/files/patch-src__utility.c
@@ -1,5 +1,5 @@
---- src/utility.c.orig	Sun Oct  9 11:04:48 2005
-+++ src/utility.c	Sun Oct  9 11:04:56 2005
+--- src/utility.c.orig	Wed Jan 17 08:16:02 2007
++++ src/utility.c	Sat Feb 10 12:08:36 2007
 @@ -45,10 +45,6 @@
  #include <ctype.h>
  #include <libnova/utility.h>
@@ -8,6 +8,6 @@
 -#include <malloc.h>
 -#endif
 -
- #ifndef WIN32
- #include "config.h"
- #else
+ /* Include unistd.h only if not on a Win32 platform */
+ /* Include Win32 Headers sys/types.h and sys/timeb.h if on Win32 */
+ #ifndef __WIN32__
