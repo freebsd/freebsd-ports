2ffb3b3a259e591c0406927b6191550a9f02e538
Joe Marcus Clarke <marcus@FreeBSD.org>
2007-02-12 06:47:39 +0000

    Fix an undefined symbol error in the Groupwise plugin that can lead to a configuration failure.
    PR:		108964
    Submitted by:	Mark Evenson<mark.evenson@gmx.at>

---
+++ mail/evolution/files/patch-plugins_groupwise-features_junk-settings.c
@@ -0,0 +1,10 @@
+--- plugins/groupwise-features/junk-settings.c.orig	Mon Feb 12 01:44:51 2007
++++ plugins/groupwise-features/junk-settings.c	Mon Feb 12 01:45:01 2007
+@@ -25,6 +25,7 @@
+ #include <glade/glade.h>
+ #include "junk-settings.h"
+ #include <glib/gmain.h>
++#include <glib/gi18n-lib.h>
+ #include <gtk/gtktreemodel.h>
+ #include <gtk/gtkliststore.h>
+ #include <gtk/gtktreeselection.h>
