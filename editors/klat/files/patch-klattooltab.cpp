fee07cba285e15d73361c04de73657bf53cd0ee9
miwi <miwi@FreeBSD.org>
2007-02-12 12:29:28 +0000

    A LaTeX source editor for KDE.
    WWW: http://research.edm.uhasselt.be/jori/klat/
    
    PR:		ports/109080
    Submitted by:	laddassm

---
+++ editors/klat/files/patch-klattooltab.cpp
@@ -0,0 +1,8 @@
+--- src/klattooltab.cpp.orig	Jun 29 00:00:00 2004
++++ src/klattooltab.cpp	Jun 29 00:01:00 2006
+@@ -73,4 +73,4 @@
+ void KlatToolTab::leaveEvent(QEvent* event)
+ {
+   emit signalLeaveEvent();
+-};
++}
