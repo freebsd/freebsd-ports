ece2e2c2011abbbda47c065043cb2830e3c5b82d
Joe Marcus Clarke <marcus@FreeBSD.org>
2007-02-13 22:19:23 +0000

    Update to 2.12.1.

--- net/gnome-netstatus/files/patch-src_netstatus-sysdeps.c
+++ net/gnome-netstatus/files/patch-src_netstatus-sysdeps.c
@@ -1,5 +1,5 @@
---- src/netstatus-sysdeps.c.orig	Mon Mar 14 09:34:06 2005
-+++ src/netstatus-sysdeps.c	Mon Jan 30 21:44:49 2006
+--- src/netstatus-sysdeps.c.orig	Tue Feb 13 04:39:19 2007
++++ src/netstatus-sysdeps.c	Tue Feb 13 17:16:29 2007
 @@ -430,11 +430,11 @@ static inline char *
  get_an_data (const char *iface,
  	     int        *signal_strength)
@@ -14,6 +14,39 @@
    int                   level;
    char                 *error = NULL;
    gboolean              rssimap_valid = FALSE;
+@@ -486,11 +486,11 @@ get_wi_data (const char *iface,
+   level = (int) wreq.wi_val[1];
+ 
+ #ifdef WI_RID_READ_APS
+-  if (signal_strength <= 0)
++  if (*signal_strength <= 0)
+     {
+       /* we fail to get signal strength by usual means, try another way */
+       static time_t   last_scan;
+-      static long int cached;
++      static int cached;
+       time_t          now;
+ 
+       now = time (NULL);
+@@ -510,15 +510,15 @@ get_wi_data (const char *iface,
+           if (nstations > 0)
+ 	    {
+               w = (struct wi_apinfo *)(((char *) &wreq.wi_val) + sizeof (int));
+-              signal_strength = (long int) w->signal;
++              level = w->signal;
+             }
+ 
+-            cached = signal_strength;
++            cached = level;
+             last_scan = now;
+           }
+         else
+ 	  {
+-            signal_strength = cached;
++            level = cached;
+           }
+     }
+ #endif
 @@ -548,6 +548,8 @@ netstatus_sysdeps_read_iface_wireless_de
        g_strncasecmp (iface, "wi",   2) &&
        g_strncasecmp (iface, "ath",  3) &&
