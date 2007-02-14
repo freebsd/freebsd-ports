b0c97aa4e28f56d83e49b5e4d17349eef45277c1
delphij <delphij@FreeBSD.org>
2007-02-14 06:07:29 +0000

    Update to Python 2.4.4.
    Approved by:	maintainer timeout, alexbl (python@)
    Tested by:	pointyhat
    PR:		ports/105901

--- lang/python24/files/patch-setup.py
+++ lang/python24/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	Mon Oct  9 22:08:07 2006
-+++ setup.py	Mon Oct  9 22:08:42 2006
+--- setup.py.orig	Mon Nov 27 03:33:44 2006
++++ setup.py	Mon Nov 27 03:35:40 2006
 @@ -15,7 +15,7 @@
  from distutils.command.install_lib import install_lib
  
@@ -9,7 +9,7 @@
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (at the front) if
-@@ -425,7 +425,7 @@
+@@ -442,7 +442,7 @@
          if self.compiler.find_library_file(lib_dirs, 'readline'):
              readline_libs = ['readline']
              if self.compiler.find_library_file(lib_dirs,
@@ -18,12 +18,12 @@
                  readline_libs.append('ncursesw')
              elif self.compiler.find_library_file(lib_dirs,
                                                   'ncurses'):
-@@ -681,7 +681,7 @@
- 
+@@ -728,7 +728,7 @@
          # Curses support, requiring the System V version of curses, often
          # provided by the ncurses library.
+         panel_library = 'panel'
 -        if (self.compiler.find_library_file(lib_dirs, 'ncursesw')):
 +        if (self.compiler.find_library_file(lib_dirs, 'xxxncursesw')):
              curses_libs = ['ncursesw']
-             exts.append( Extension('_curses', ['_cursesmodule.c'],
-                                    libraries = curses_libs) )
+             # Bug 1464056: If _curses.so links with ncursesw,
+             # _curses_panel.so must link with panelw.
