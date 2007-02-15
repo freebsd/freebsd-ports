a933b342e07410032c5e19d8e98f9e4319c92c14
Martin Wilke <miwi@FreeBSD.org>
2007-02-15 11:44:54 +0000

    - Fix build
    PR:		109184
    Submitted by:	Stephen Montgomery-Smith <stephen@cauchy.math.missouri.edu> (maintainer)

---
+++ x11/accessx/files/patch-Access.h
@@ -0,0 +1,11 @@
+--- Access.h-orig	Sun Nov 17 19:34:23 2002
++++ Access.h	Sun Nov 17 19:34:34 2002
+@@ -20,7 +20,7 @@
+  *
+  */
+ 
+-#include <iostream.h>
++#include <iostream>
+ #include <math.h>
+ 
+ inline char* onoroff(int val)
