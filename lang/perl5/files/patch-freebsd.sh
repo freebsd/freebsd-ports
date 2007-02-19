24cd6a80f0d1eb6f0bff44533944e0d8b5e8c544
tobez <tobez@FreeBSD.org>
2007-02-19 20:17:09 +0000

    Objformat fallout fix.

--- lang/perl5/files/patch-freebsd.sh
+++ lang/perl5/files/patch-freebsd.sh
@@ -1,5 +1,5 @@
---- hints/freebsd.sh.orig	Mon Jan 31 22:08:10 2005
-+++ hints/freebsd.sh	Mon Jan 31 22:10:17 2005
+--- hints/freebsd.sh.orig	Wed Oct 15 22:26:16 2003
++++ hints/freebsd.sh	Mon Feb 19 21:16:03 2007
 @@ -101,7 +101,14 @@ case "$osvers" in
  	    "") usemymalloc='n'
  	        ;;
@@ -15,3 +15,27 @@
  	;;
  esac
  
+@@ -116,17 +123,17 @@ case "$osvers" in
+ 
+ *)
+         objformat=`/usr/bin/objformat`
+-        if [ x$objformat = xelf ]; then
+-            libpth="/usr/lib /usr/local/lib"
+-            glibpth="/usr/lib /usr/local/lib"
+-            ldflags="-Wl,-E "
+-            lddlflags="-shared "
+-        else
++        if [ x$objformat = xaout ]; then
+             if [ -e /usr/lib/aout ]; then
+                 libpth="/usr/lib/aout /usr/local/lib /usr/lib"
+                 glibpth="/usr/lib/aout /usr/local/lib /usr/lib"
+             fi
+             lddlflags='-Bshareable'
++        else
++            libpth="/usr/lib /usr/local/lib"
++            glibpth="/usr/lib /usr/local/lib"
++            ldflags="-Wl,-E "
++            lddlflags="-shared "
+         fi
+         cccdlflags='-DPIC -fPIC'
+         ;;
