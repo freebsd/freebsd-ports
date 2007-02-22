fc8d446dbdb18281a55c72bb9af66b6203e23734
Alexander Botero-Lowry <alexbl@FreeBSD.org>
2007-02-22 04:21:12 +0000

    - update to 0.12
    Submitted by:	Philip Jenvey (maintainer, via irc)

--- news/hellanzb/files/patch-nodocs-setup.py
+++ news/hellanzb/files/patch-nodocs-setup.py
@@ -1,12 +1,12 @@
---- setup.py.bak	2006-03-26 22:14:40.000000000 -0800
-+++ setup.py	2006-03-26 22:15:05.000000000 -0800
-@@ -40,8 +40,7 @@
+--- setup.py.orig	Mon Feb 19 18:12:55 2007
++++ setup.py	Wed Feb 21 16:34:28 2007
+@@ -38,8 +38,7 @@
          packages = [ 'Hellanzb', 'Hellanzb.NZBLeecher', 'Hellanzb.HellaXMLRPC',
                       'Hellanzb.external', 'Hellanzb.external.elementtree' ],
          scripts = [ 'hellanzb.py' ],
 -        data_files = [ ( 'etc', [ 'etc/hellanzb.conf.sample' ] ),
 -                       ( 'share/doc/hellanzb', [ 'CHANGELOG', 'CREDITS', 'README', 'LICENSE' ] ) ],
 +        data_files = [ ( 'etc', [ 'etc/hellanzb.conf.sample' ] ), ],
- 
-         # py2app options
+         )
+     py2app_options = dict(
          app = [ 'hellanzb.py' ],
