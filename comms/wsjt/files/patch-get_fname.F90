bc31aba5aff9ad920b4c93f34886b9966640bb2d
Diane Bruce <db@FreeBSD.org>
2007-02-25 02:37:44 +0000

    - fixed Makefile to depend on gcc42 not gfortran - regenerated configure to use gcc42 gfortran42 with py-numpy - fixed botch with missing gmtime_r_ (already fixed upstream) - fixed wsjt to use py-numpy instead of deprecated py-numeric - fixed to use portaudio v19 already in ports - fixed to run on amd due to above - maho wants WANT_FORTRAN knob in gfortran ports
    Pointy hat to: pav,maho
    Approved by: ehaupt (mentor)

---
+++ comms/wsjt/files/patch-get_fname.F90
@@ -0,0 +1,19 @@
+--- get_fname.F90.orig	Mon Dec 11 10:38:21 2006
++++ get_fname.F90	Mon Dec 11 10:38:42 2006
+@@ -5,6 +5,7 @@
+   use dfport
+ #endif
+ 
++  external gmtime_r
+   character hiscall*12,fname*24,tag*7
+   integer ntime
+   integer trperiod
+@@ -13,7 +14,7 @@
+   n1=ntime
+   n2=(n1+2)/trperiod
+   n3=n2*trperiod
+-  call gmtime(n3,it)
++  call gmtime_r(n3,it)
+   it(5)=it(5)+1
+   it(6)=mod(it(6),100)
+   write(fname,1000) (it(j),j=6,1,-1)
