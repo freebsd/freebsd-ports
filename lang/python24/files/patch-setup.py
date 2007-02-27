7563f5f145828b96696fefcd0af65a4697645e39
rafan <rafan@FreeBSD.org>
2007-02-27 19:00:23 +0000

    - Force setup.py picks up readline and ncurses from base. This fixes build   when devel/ncurses installed. - Similar to python24, don't pick up ncursesw in python25. This results   in both ncurses are linked into _curses.so
    Tested by:	krion
    Approved by:	alexbl (python@)

--- lang/python24/files/patch-setup.py
+++ lang/python24/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	Mon Nov 27 03:33:44 2006
-+++ setup.py	Mon Nov 27 03:35:40 2006
+--- setup.py.orig	Tue Feb 27 20:10:53 2007
++++ setup.py	Tue Feb 27 20:15:23 2007
 @@ -15,7 +15,7 @@
  from distutils.command.install_lib import install_lib
  
@@ -18,6 +18,15 @@
                  readline_libs.append('ncursesw')
              elif self.compiler.find_library_file(lib_dirs,
                                                   'ncurses'):
+@@ -465,7 +465,7 @@
+                 readline_extra_link_args = ()
+ 
+             exts.append( Extension('readline', ['readline.c'],
+-                                   library_dirs=['/usr/lib/termcap'],
++                                   library_dirs=['/usr/lib', '/usr/lib/termcap'],
+                                    extra_link_args=readline_extra_link_args,
+                                    libraries=readline_libs) )
+         if platform not in ['mac']:
 @@ -728,7 +728,7 @@
          # Curses support, requiring the System V version of curses, often
          # provided by the ncurses library.
@@ -27,3 +36,19 @@
              curses_libs = ['ncursesw']
              # Bug 1464056: If _curses.so links with ncursesw,
              # _curses_panel.so must link with panelw.
+@@ -738,6 +738,7 @@
+         elif (self.compiler.find_library_file(lib_dirs, 'ncurses')):
+             curses_libs = ['ncurses']
+             exts.append( Extension('_curses', ['_cursesmodule.c'],
++                                   library_dirs = ['/usr/lib'],
+                                    libraries = curses_libs) )
+         elif (self.compiler.find_library_file(lib_dirs, 'curses')
+               and platform != 'darwin'):
+@@ -757,6 +758,7 @@
+         if (module_enabled(exts, '_curses') and
+             self.compiler.find_library_file(lib_dirs, panel_library)):
+             exts.append( Extension('_curses_panel', ['_curses_panel.c'],
++                                   library_dirs = ['/usr/lib'],
+                                    libraries = [panel_library] + curses_libs) )
+ 
+ 
