ec9c55f34b00ca163e92c0303eb81f2c0acb5bc3
Rong-En Fan <rafan@FreeBSD.org>
2007-02-27 19:00:23 +0000

    - Force setup.py picks up readline and ncurses from base. This fixes build   when devel/ncurses installed. - Similar to python24, don't pick up ncursesw in python25. This results   in both ncurses are linked into _curses.so
    Tested by:	krion
    Approved by:	alexbl (python@)

--- lang/python31/files/patch-setup.py
+++ lang/python31/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	Thu Aug 10 08:42:18 2006
-+++ setup.py	Thu Oct  5 13:26:43 2006
+--- setup.py.orig	Thu Aug 10 07:42:18 2006
++++ setup.py	Tue Feb 27 20:27:07 2007
 @@ -15,7 +15,7 @@
  from distutils.command.install_lib import install_lib
  
@@ -9,6 +9,24 @@
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (at the front) if
+@@ -488,7 +488,7 @@
+ 
+             readline_libs = ['readline']
+             if self.compiler.find_library_file(lib_dirs,
+-                                                 'ncursesw'):
++                                                 'xxxncursesw'):
+                 readline_libs.append('ncursesw')
+             elif self.compiler.find_library_file(lib_dirs,
+                                                  'ncurses'):
+@@ -500,7 +500,7 @@
+                                                'termcap'):
+                 readline_libs.append('termcap')
+             exts.append( Extension('readline', ['readline.c'],
+-                                   library_dirs=['/usr/lib/termcap'],
++                                   library_dirs=['/usr/lib', '/usr/lib/termcap'],
+                                    extra_link_args=readline_extra_link_args,
+                                    libraries=readline_libs) )
+         if platform not in ['mac']:
 @@ -590,6 +590,8 @@
              # OpenSSL doesn't do these until 0.9.8 so we'll bring our own hash
              exts.append( Extension('_sha256', ['sha256module.c']) )
@@ -27,7 +45,23 @@
              curses_libs = ['ncursesw']
              # Bug 1464056: If _curses.so links with ncursesw,
              # _curses_panel.so must link with panelw.
-@@ -1498,8 +1500,7 @@
+@@ -913,6 +915,7 @@
+         elif (self.compiler.find_library_file(lib_dirs, 'ncurses')):
+             curses_libs = ['ncurses']
+             exts.append( Extension('_curses', ['_cursesmodule.c'],
++                                   library_dirs = ['/usr/lib'],
+                                    libraries = curses_libs) )
+         elif (self.compiler.find_library_file(lib_dirs, 'curses')
+               and platform != 'darwin'):
+@@ -932,6 +935,7 @@
+         if (module_enabled(exts, '_curses') and
+             self.compiler.find_library_file(lib_dirs, panel_library)):
+             exts.append( Extension('_curses_panel', ['_curses_panel.c'],
++                                   library_dirs = ['/usr/lib'],
+                                    libraries = [panel_library] + curses_libs) )
+ 
+ 
+@@ -1515,8 +1519,7 @@
            ext_modules=[Extension('_struct', ['_struct.c'])],
  
            # Scripts to install
