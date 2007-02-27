7a7e1f3a94bd11ce2541813a9294297273c2928d
maho <maho@FreeBSD.org>
2007-02-27 02:54:26 +0000

    1. Update to 1.6 and make fetchable[1] 2. WITH_ATLAS knobs works.
    Submitted by:	Pedro F. Giffuni and fenner[1]

--- cad/calculix/files/patch-cgx_VER+src+uselibSNL.cpp
+++ cad/calculix/files/patch-cgx_VER+src+uselibSNL.cpp
@@ -1,5 +1,5 @@
---- cgx_1.5/src/uselibSNL.cpp.orig	Sun Feb 19 22:06:07 2006
-+++ cgx_1.5/src/uselibSNL.cpp	Sun Feb 19 22:06:38 2006
+--- cgx_1.6/src/uselibSNL.cpp.orig	Sun Feb 19 22:06:07 2006
++++ cgx_1.6/src/uselibSNL.cpp	Sun Feb 19 22:06:38 2006
 @@ -26,7 +26,7 @@
  
  #include <extUtil.h>
