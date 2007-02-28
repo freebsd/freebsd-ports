512c546b626c81f8d41fe3a7e421c11ddf93f855
kuriyama <kuriyama@FreeBSD.org>
2007-02-28 01:17:16 +0000

    - Upgrade to 5.3.1 [1]. - Add $snmpd_flush_cache knob [2]. - Add conflict with net-snmp-4.* [3].
    PR:	ports/97673 [1] (but mostly rewritten), ports/106036 [2],
    	ports/103462 [3]
    Submitted by:	Rick van der Zwet <rick@wzoeterwoude.net> [1],
    	"Brian A. Seklecki"<bseklecki@collaborativefusion.com> [2],
    	Mykola Dzham <freebsd@levsha.org.ua> [3]

--- net-mgmt/net-snmp53/files/patch-ucd_snmp.h
+++ net-mgmt/net-snmp53/files/patch-ucd_snmp.h
@@ -1,14 +1,9 @@
-
-$FreeBSD$
-
---- agent/mibgroup/ucd_snmp.h.orig
-+++ agent/mibgroup/ucd_snmp.h
-@@ -39,6 +39,10 @@
- config_arch_require(freebsd4, ucd-snmp/memory_freebsd2)
- config_arch_require(freebsd5, ucd-snmp/vmstat_freebsd2)
+--- agent/mibgroup/ucd_snmp.h.orig	Tue Feb 27 20:44:04 2007
++++ agent/mibgroup/ucd_snmp.h	Tue Feb 27 20:44:20 2007
+@@ -41,6 +41,8 @@
  config_arch_require(freebsd5, ucd-snmp/memory_freebsd2)
-+config_arch_require(freebsd6, ucd-snmp/vmstat_freebsd2)
-+config_arch_require(freebsd6, ucd-snmp/memory_freebsd2)
+ config_arch_require(freebsd6, ucd-snmp/vmstat_freebsd2)
+ config_arch_require(freebsd6, ucd-snmp/memory_freebsd2)
 +config_arch_require(freebsd7, ucd-snmp/vmstat_freebsd2)
 +config_arch_require(freebsd7, ucd-snmp/memory_freebsd2)
  config_arch_require(netbsd1, ucd-snmp/vmstat_netbsd1)
