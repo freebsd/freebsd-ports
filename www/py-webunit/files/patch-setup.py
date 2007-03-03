835f147acd08c79c2cfdbb9dc6a5233c2bfda058
clsung <clsung@FreeBSD.org>
2007-03-03 17:34:01 +0000

    Add py-webunit 1.3.8, unit test your websites with code that acts like a web browser.
    PR:		ports/109777
    Submitted by:	Li-Wen Hsu <lwhsu at lwhsu.org>

---
+++ www/py-webunit/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	Sat Mar  3 03:16:17 2007
++++ setup.py	Sat Mar  3 03:16:26 2007
+@@ -34,7 +34,7 @@
+     author_email = "richard@mechanicalcat.net",
+     url = 'http://mechanicalcat.net/tech/webunit/',
+     download_url = 'http://mechanicalcat.net/tech/webunit/webunit-%s.tar.gz'%__version__,
+-    packages = ['webunit', 'demo'],
++    packages = ['webunit'],
+     classifiers = [
+         'Development Status :: 5 - Production/Stable',
+         'Environment :: Console',
