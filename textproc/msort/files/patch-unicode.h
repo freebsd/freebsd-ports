c7c417202985958ed7dfaf5e38993768d5f956b0
Cheng-Lung Sung <clsung@FreeBSD.org>
2007-03-06 07:27:03 +0000

    - Update to 8.38

--- textproc/msort/files/patch-unicode.h
+++
@@ -1,7 +0,0 @@
---- unicode.h.orig	Mon Jan  8 03:33:05 2007
-+++ unicode.h	Mon Jan 22 09:29:08 2007
-@@ -1,3 +1,4 @@
-+typedef unsigned short     UTF16;
- typedef unsigned long	UTF32;	/* at least 32 bits */
- typedef unsigned char	UTF8;	/* 8 bits */
- typedef unsigned char	Boolean; /* 0 or 1 */
