56c7b36b176f910671346529061f34f6532ed2b2
mezz <mezz@FreeBSD.org>
2007-03-07 19:28:28 +0000

    Update to 0.4.99.1 as in Deluge 0.5 Release Candidate 1.

--- net-p2p/deluge05/files/patch-src_delugegtk.py
+++ net-p2p/deluge05/files/patch-src_delugegtk.py
@@ -1,35 +1,15 @@
-Index: /trunk/src/delugegtk.py
-===================================================================
---- src/delugegtk.py (revision 288)
-+++ src/delugegtk.py (revision 289)
-@@ -695,6 +695,5 @@
- 				self.wtree.get_widget("toolbutton_pause").set_stock_id(gtk.STOCK_MEDIA_PAUSE)
- 		except KeyError:
--			pass	
--		self.saved_peer_info = None
-+			pass
- 		
- 
-@@ -744,8 +743,5 @@
- 			unique_id = self.get_selected_torrent()
- 			
--			self.saved_peer_info = self.manager.get_torrent_peer_info(unique_id)
--			
--			
--			new_peer_info = self.saved_peer_info
-+			new_peer_info = self.manager.get_torrent_peer_info(unique_id)
- 			
- 			new_ips = {}
-@@ -782,4 +778,12 @@
- 											dcommon.frate(peer["download_speed"]), 
- 											dcommon.frate(peer["upload_speed"])])
-+			#print new_ips
-+			#print curr_ips
-+			#print new_peer_info
-+			del new_peer_info
-+			del new_ips
-+			del curr_ips
-+			
-+			
- 								
- 		elif tab == 2: #File List
+--- src/delugegtk.py.orig	Tue Mar  6 20:46:00 2007
++++ src/delugegtk.py	Tue Mar  6 22:42:58 2007
+@@ -33,9 +33,9 @@
+ 	def __init__(self):
+ 		APP = 'deluge'
+ 		DIR = os.path.join(dcommon.INSTALL_PREFIX, 'share', 'locale')
+-		locale.setlocale(locale.LC_ALL, '')
+-		locale.bindtextdomain(APP, DIR)
+-		locale.textdomain(APP)
++#		locale.setlocale(locale.LC_ALL, '')
++#		locale.bindtextdomain(APP, DIR)
++#		locale.textdomain(APP)
+ 		gettext.bindtextdomain(APP, DIR)
+ 		gettext.textdomain(APP)
+ 		gettext.install(APP, DIR)
