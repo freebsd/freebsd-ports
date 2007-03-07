bd3a31d0bdc3b8df629b2bc7cc974ee672c27898
Martin Wilke <miwi@FreeBSD.org>
2007-03-07 17:15:43 +0000

    - Fix build with amd64
    Thanks to:       Martin Matuska for access of amd64 TB.

--- net/sipxportlib/files/patch-src_os_OsSysLog.cpp
+++ net/sipxportlib/files/patch-src_os_OsSysLog.cpp
@@ -1,5 +1,5 @@
 --- src/os/OsSysLog.cpp.orig	Tue Jul 25 17:05:55 2006
-+++ src/os/OsSysLog.cpp	Tue Feb  6 08:07:54 2007
++++ src/os/OsSysLog.cpp	Wed Feb 14 09:54:52 2007
 @@ -39,6 +39,9 @@
  #if defined(_WIN32)
       // Windows va_arg function does not take a const 
@@ -10,12 +10,14 @@
  #elif defined(__pingtel_on_posix__)
       // Posix va_arg function takes a const
  #    define OS_VA_ARG_CONST const
-@@ -333,7 +336,7 @@
+@@ -333,7 +336,9 @@
        {
           UtlString logData;
           UtlString logEntry;
 -         myvsprintf(logData, format, ap) ;
-+         myvsprintf(logData, format, (OS_VA_ARG_CONST va_list)ap) ;
++         OS_VA_ARG_CONST va_list *myap;
++         myap = (OS_VA_ARG_CONST va_list *)&ap;
++         myvsprintf(logData, format, *myap) ;
           logData = escape(logData) ;
  
           OsTime timeNow;
