49b9a5a788922caf7c61b9ff158d109ea836eb76
Martin Wilke <miwi@FreeBSD.org>
2007-03-13 09:57:36 +0000

    - Update to 0.90.0 - Fix depends on devel/py-pytz, it has been packed as an egg - USE_PYTHON=yes, there is no python2.2 in ports tree - Remove 4.x stuff - Use USE_TCL, USE_TK, and some variables in bsd.tcl.mk - Adjust EXAMPLESDIR - Use static pkg-plist
    PR:             110184
    Submitted by:   Li-Wen Hsu <lwhsu@lwhsu.org>
    Approved by:    maintainer

--- math/py-matplotlib/files/patch-setupext.py
+++ math/py-matplotlib/files/patch-setupext.py
@@ -1,24 +1,28 @@
---- setupext.py.orig	Tue Mar 29 19:25:37 2005
-+++ setupext.py	Sat Nov  5 20:26:49 2005
-@@ -38,7 +38,8 @@
-     'darwin' : ['/usr/local', '/usr', '/sw', '/usr/X11R6'],
-     'freebsd4' : ['/usr/local', '/usr'],
-     'freebsd5' : ['/usr/local', '/usr'],
--    'freebsd6' : ['/usr/local', '/usr'],    
-+    'freebsd6' : ['/usr/local', '/usr'],
-+    'freebsd7' : ['/usr/local', '/usr'],
+--- setupext.py.orig	Sun Mar 11 16:20:42 2007
++++ setupext.py	Sun Mar 11 16:22:14 2007
+@@ -50,9 +50,10 @@
+     'cygwin' : ['/usr/local', '/usr',],
+     'darwin' : ['/sw/lib/freetype2', '/sw/lib/freetype219', '/usr/local',
+                 '/usr', '/sw'],
+-    'freebsd4' : ['/usr/local', '/usr'],
+-    'freebsd5' : ['/usr/local', '/usr'],
+-    'freebsd6' : ['/usr/local', '/usr'],
++    'freebsd4' : ['%%LOCALBASE%%', '/usr'],
++    'freebsd5' : ['%%LOCALBASE%%', '/usr'],
++    'freebsd6' : ['%%LOCALBASE%%', '/usr'],
++    'freebsd7' : ['%%LOCALBASE%%', '/usr'],
      'sunos5' : [os.getenv('MPLIB_BASE') or '/usr/local',],
- }
- 
-@@ -306,9 +307,9 @@
+     'gnukfreebsd5' : ['/usr/local', '/usr'],
+     'gnukfreebsd6' : ['/usr/local', '/usr'],
+@@ -455,9 +456,9 @@
  
      # you're still here? ok we'll try it this way
      o = find_tcltk() # todo: try/except
 -    module.include_dirs.extend([o.tcl_inc, o.tk_inc])
-+    module.include_dirs.extend(["/usr/local/include/tcl8.4", "/usr/local/include/tk8.4"])
++    module.include_dirs.extend(["%%TCL_INCLUDEDIR%%", "%%TK_INCLUDEDIR%%"])
      module.library_dirs.extend([o.tcl_lib, o.tk_lib])
 -    module.libraries.extend(['tk'+o.tkv, 'tcl'+o.tkv])
-+    module.libraries.extend(['tk84', 'tcl84'])
++    module.libraries.extend(['tk%%USE_TK%%', 'tcl%%USE_TCL%%'])
  
  
  def add_windowing_flags(module):
