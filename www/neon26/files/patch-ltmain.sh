de7020af32b8546e31c7891c14f28c898ffc74f8
Lev A. Serebryakov <lev@FreeBSD.org>
2007-03-16 10:03:11 +0000

    (1) Update `www/neon' to 0.26.3   (2) Bump port revisions (and dependencies) on all ports, using libneon   (3) Mark `devel/bazaar' broken, it doesn't support new neon version.       Approved by maintainer.   (4) Mark `www/neonpp' broken, it doesn't support new neon version.       Maintainer timeout.

--- www/neon26/files/patch-ltmain.sh
+++ www/neon26/files/patch-ltmain.sh
@@ -1,27 +1,10 @@
---- ltmain.sh.orig	Wed Jan 23 11:38:40 2002
-+++ ltmain.sh	Sun Feb  3 01:44:17 2002
-@@ -1037,8 +1037,16 @@
- 	continue
- 	;;
- 
-+      -pthread)
-+	compile_command="$compile_command -pthread"
-+	finalize_command="$finalize_command -pthread"
-+	compiler_flags="$compiler_flags -pthread"
-+	continue
-+	;;
-+
-       -module)
- 	module=yes
-+	build_old_libs=no
- 	continue
- 	;;
- 
-@@ -4179,10 +4190,12 @@
+--- ltmain.sh.orig	Wed Feb  7 15:52:17 2007
++++ ltmain.sh	Wed Feb  7 15:52:58 2007
+@@ -6038,10 +6038,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
-+	if /usr/bin/false; then
++	if /usr/bin/false ; then
  	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
  	instname="$dir/$name"i
  	$show "$install_prog $instname $destdir/$name"
