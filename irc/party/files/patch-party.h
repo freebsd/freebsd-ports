44533aa15477935114aa5f40386d8e8ecde276ef
miwi <miwi@FreeBSD.org>
2007-03-16 09:31:58 +0000

    - Remove usage of sgtty.h
    PR:             110354
    Submitted by:   Ed Schouten <ed@fxq.nl>

--- irc/party/files/patch-party.h
+++ irc/party/files/patch-party.h
@@ -1,5 +1,14 @@
 --- party.h.orig	Wed Oct 15 18:31:34 2003
 +++ party.h	Wed Oct 15 18:31:58 2003
+@@ -225,7 +225,7 @@
+ 
+ #ifdef BSD
+ #define L_FLOCK         /* Use flock() to lock files */
+-#if defined(bsdi) || defined(linux)
++#if defined(bsdi) || defined(linux) || defined(__FreeBSD__)
+ #define F_TERMIOS       /* Use tcsetattr() to set modes */
+ #else
+ #define F_STTY          /* Use stty() to set modes */
 @@ -464,7 +464,7 @@
  #ifdef BSD
  int susp();
