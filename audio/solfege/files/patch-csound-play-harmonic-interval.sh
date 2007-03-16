e0a573da5995fda5ef37b2461410c7353d1b3131
nox <nox@FreeBSD.org>
2007-03-16 19:45:33 +0000

    - Fix csound interval script to use /bin/sh instead of /bin/bash - bump PORTREVISION
    Approved by:	miwi (mentor, implicit)

---
+++ audio/solfege/files/patch-csound-play-harmonic-interval.sh
@@ -0,0 +1,7 @@
+Index: lesson-files/bin/csound-play-harmonic-interval.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/bin/sh
+ 
+ # This file is free software; as a special exception the author gives
+ # unlimited permission to copy and/or distribute it, with or without
