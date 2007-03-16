c59457f776b3970d342011633fa48aa342db30f2
miwi <miwi@FreeBSD.org>
2007-03-16 21:05:59 +0000

    The heyu port makes use of <sgtty.h>, though it supports <termios.h> when -DPOSIX is turned on. Using <sgtty.h> is bad, because it breaks on kernels without COMPAT_43TTY
    PR:             110360
    Submitted by:   Ed Schouten <ed@fxq.nl>
    Approved by:    maintainer

---
+++ misc/heyu/files/patch-tty.c
@@ -0,0 +1,16 @@
+--- tty.c	Sun Mar 30 22:57:59 2003
++++ tty.c	Thu Mar 15 22:48:59 2007
+@@ -86,11 +86,11 @@
+ #include <sgtty.h>
+ struct sgttyb oldsb, newsb;
+ #else
+-#ifndef POSIX
+-#include <termio.h>
+ #ifndef NCC
+ #define NCC NCCS
+ #endif
++#ifndef POSIX
++#include <termio.h>
+ struct termio oldsb, newsb;
+ #else
+ #include <termios.h>
