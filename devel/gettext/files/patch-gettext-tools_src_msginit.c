4f4710c4c010cb62fad8007e4844294e2485b814
ade <ade@FreeBSD.org>
2007-03-17 22:37:26 +0000

    Update to 0.16.1
    Many thanks to all those that tested this update, particularly the
    FreeBSD GNOME team.

--- devel/gettext/files/patch-gettext-tools_src_msginit.c
+++ devel/gettext/files/patch-gettext-tools_src_msginit.c
@@ -1,38 +1,22 @@
---- gettext-tools/src/msginit.c.orig	Fri May 20 00:14:54 2005
-+++ gettext-tools/src/msginit.c	Fri May 20 00:15:16 2005
-@@ -950,7 +950,7 @@
- 
-   gettextlibdir = getenv ("GETTEXTLIBDIR");
+--- gettext-tools/src/msginit.c.orig	Mon Nov 27 09:02:08 2006
++++ gettext-tools/src/msginit.c	Sun Dec  3 18:03:46 2006
+@@ -820,3 +820,3 @@
    if (gettextlibdir == NULL || gettextlibdir[0] == '\0')
 -    gettextlibdir = relocate (LIBDIR "/gettext");
 +    gettextlibdir = relocate (LIBDIR "/gettext-tools");
  
-   prog = concatenated_pathname (gettextlibdir, "project-id", NULL);
- 
-@@ -1016,7 +1016,7 @@
- 
-   gettextlibdir = getenv ("GETTEXTLIBDIR");
+@@ -887,3 +887,3 @@
    if (gettextlibdir == NULL || gettextlibdir[0] == '\0')
 -    gettextlibdir = relocate (LIBDIR "/gettext");
 +    gettextlibdir = relocate (LIBDIR "/gettext-tools");
  
-   prog = concatenated_pathname (gettextlibdir, "project-id", NULL);
- 
-@@ -1165,7 +1165,7 @@
- static const char *
- get_user_email ()
+@@ -1037,3 +1037,3 @@
  {
 -  const char *prog = relocate (LIBDIR "/gettext/user-email");
 +  const char *prog = relocate (LIBDIR "/gettext-tools/user-email");
    char *argv[4];
-   pid_t child;
-   int fd[1];
-@@ -1261,7 +1261,7 @@
-   argv[0] = "/bin/sh";
-   argv[1] = (char *) prog;
+@@ -1134,3 +1134,3 @@
    argv[2] = (char *) relocate (PROJECTSDIR);
 -  argv[3] = (char *) relocate (LIBDIR "/gettext");
 +  argv[3] = (char *) relocate (LIBDIR "/gettext-tools");
    argv[4] = (char *) catalogname;
-   argv[5] = (char *) language;
-   argv[6] = NULL;
