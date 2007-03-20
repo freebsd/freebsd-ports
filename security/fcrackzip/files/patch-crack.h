251f9c5e768cf51a6b90c9462104f9ddf34042c1
Stefan Walter <stefan@FreeBSD.org>
2007-03-20 21:02:06 +0000

    Fix build with gcc 4.x.
    Reported by:	kris/pointyhat

---
+++ security/fcrackzip/files/patch-crack.h
@@ -0,0 +1,10 @@
+--- crack.h.orig	Tue Mar 20 21:56:11 2007
++++ crack.h	Tue Mar 20 21:56:20 2007
+@@ -24,7 +24,6 @@
+ extern u8 bf_last;
+ 
+ extern int verbosity;
+-extern int use_unzip;
+ 
+ #define FILE_SIZE	12
+ #define CRC_SIZE	2
