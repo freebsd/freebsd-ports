e66308c1a0698ee3ff4204a62ff9d8833f31afeb
miwi <miwi@FreeBSD.org>
2007-03-21 09:59:20 +0000

    - Respect %%PREFIX%% - Bump PORTREVISION
    PR:             110466
    Submitted by:   Helge Oldach <ports-vpnc-mar07@oldach.net> (maintainer)

--- security/vpnc/files/patch-config.c
+++ security/vpnc/files/patch-config.c
@@ -15,6 +15,15 @@
  }
  
  static const char *config_def_vendor(void)
+@@ -485,7 +485,7 @@
+ {
+ 	char *realname;
+ 	
+-	asprintf(&realname, "%s%s%s", index(name, '/') ? "" : "/etc/vpnc/", name, add_dot_conf ? ".conf" : "");
++	asprintf(&realname, "%s%s%s", index(name, '/') ? "" : "%%PREFIX%%/etc/vpnc/", name, add_dot_conf ? ".conf" : "");
+ 	return realname;
+ }
+ 
 @@ -701,8 +701,8 @@
  	}
  	
