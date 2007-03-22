c10103e9afd629cc6757a785acd962ddca5e4853
ijliao <ijliao@FreeBSD.org>
2007-03-22 09:37:55 +0000

    add libruin 0.1.4 Renderer for User Interfaces in Ncurses

---
+++ devel/libruin/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Mar 22 17:12:36 2007
++++ ltmain.sh	Thu Mar 22 17:12:52 2007
+@@ -5551,10 +5551,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
