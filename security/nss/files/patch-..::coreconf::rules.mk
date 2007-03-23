6e4d727cf15dca684daccc96e97f4a2e2c28f6e0
ahze <ahze@FreeBSD.org>
2007-03-23 14:33:23 +0000

    Update to 3.11.5

--- security/nss/files/patch-..::coreconf::rules.mk
+++ security/nss/files/patch-..::coreconf::rules.mk
@@ -1,10 +1,10 @@
---- ../coreconf/rules.mk.orig	Thu Apr  3 16:48:00 2003
-+++ ../coreconf/rules.mk	Fri Apr 11 01:01:50 2003
-@@ -110,6 +110,7 @@
+--- ../coreconf/rules.mk.orig	Sat Jan  6 00:48:54 2007
++++ ../coreconf/rules.mk	Fri Mar 23 10:15:46 2007
+@@ -114,6 +114,7 @@
  endif
  ifdef SHARED_LIBRARY
  	$(INSTALL) -m 775 $(SHARED_LIBRARY) $(SOURCE_LIB_DIR)
 +	ln -sf $(notdir $(SHARED_LIBRARY)) $(SOURCE_LIB_DIR)/$(notdir $(SHARED_LIBRARY:.so.1=.so))
- endif
- ifdef IMPORT_LIBRARY
- 	$(INSTALL) -m 775 $(IMPORT_LIBRARY) $(SOURCE_LIB_DIR)
+ ifdef MOZ_DEBUG_SYMBOLS
+ ifeq (,$(filter-out _WIN%,$(NS_USE_GCC)_$(OS_TARGET)))
+ 	$(INSTALL) -m 644 $(SHARED_LIBRARY:$(DLL_SUFFIX)=pdb) $(SOURCE_LIB_DIR)
