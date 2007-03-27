ca47f9b854d89bb51ee80c24128bac99b4fcbff2
Martin Wilke <miwi@FreeBSD.org>
2007-03-27 12:49:02 +0000

    - Update to 0.3-pre6
    PR:             110863
    Submitted by:   Anderson S. Ferreira <anderson@cnpm.embrapa.br> (maintainer)

--- security/kovpn/files/patch-src-openvpnmanager.cpp
+++ security/kovpn/files/patch-src-openvpnmanager.cpp
@@ -1,6 +1,6 @@
---- src/openvpnmanager.cpp	Fri Mar 31 21:56:53 2006
-+++ src/openvpnmanager.cpp	Tue Apr 11 16:27:47 2006
-@@ -159,7 +159,7 @@
+--- src/openvpnmanager.cpp	Mon Mar 26 10:43:56 2007
++++ src/openvpnmanager.cpp	Mon Mar 26 10:47:06 2007
+@@ -192,7 +192,7 @@
     mConnectionWanted = false;
     mForce = false;
     command( "hold on\n" ); /* OpenVPN does imediatelly reconnect without the hold release. */
