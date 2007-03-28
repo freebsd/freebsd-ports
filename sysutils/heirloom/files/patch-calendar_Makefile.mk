0e85be7c96c1042c58cedc8017ceb896f884142e
miwi <miwi@FreeBSD.org>
2007-03-28 21:54:41 +0000

    - Update to 070227
    PR:             110986
    Submitted by:   trasz

--- sysutils/heirloom/files/patch-calendar_Makefile.mk
+++ sysutils/heirloom/files/patch-calendar_Makefile.mk
@@ -1,15 +1,11 @@
---- ./calendar/Makefile.mk.orig	Sat May  1 18:58:02 2004
-+++ ./calendar/Makefile.mk	Thu Jul 27 00:52:02 2006
-@@ -1,10 +1,10 @@
+--- calendar/Makefile.mk.orig	Wed May 24 16:43:22 2006
++++ calendar/Makefile.mk	Wed Mar 28 10:58:09 2007
+@@ -1,7 +1,7 @@
  all: calendar calprog
  
  calendar: calendar.sh
--	echo '#!$(SHELL)' | cat - calendar.sh | sed ' \
-+	echo '#!$(SHELL)' | cat - calendar.sh | sed " \
- 		s,@DEFBIN@,$(DEFBIN),g; \
- 		s,@SV3BIN@,$(SV3BIN),g; \
--		s,@DEFLIB@,$(DEFLIB),g' >calendar
-+		s,@DEFLIB@,$(DEFLIB),g" >calendar
+-	echo '#!$(SHELL)' | cat - calendar.sh | sed ' s,@DEFBIN@,$(DEFBIN),g; s,@SV3BIN@,$(SV3BIN),g; s,@DEFLIB@,$(DEFLIB),g' >calendar
++	echo '#!$(SHELL)' | cat - calendar.sh | sed " s,@DEFBIN@,$(DEFBIN),g; s,@SV3BIN@,$(SV3BIN),g; s,@DEFLIB@,$(DEFLIB),g" >calendar
  	chmod 755 calendar
  
  calprog: calprog.o
--- sysutils/heirloom/files/patch-spell_Makefile.mk
+++ sysutils/heirloom/files/patch-spell_Makefile.mk
@@ -1,26 +1,16 @@
---- ./spell/Makefile.mk.orig	Sun Jul 17 13:24:27 2005
-+++ ./spell/Makefile.mk	Thu Jul 27 01:00:46 2006
-@@ -36,19 +36,19 @@
+--- spell/Makefile.mk.orig	Wed May 24 16:46:54 2006
++++ spell/Makefile.mk	Wed Mar 28 11:04:59 2007
+@@ -36,11 +36,11 @@
  		hlista hlistb hstop hashmake hashcheck
  
  spell: spell.sh
--	echo '#!$(SHELL)' | cat - spell.sh | sed ' \
-+	echo '#!$(SHELL)' | cat - spell.sh | sed " \
- 		s,@DEFBIN@,$(DEFBIN),g; \
- 		s,@SV3BIN@,$(SV3BIN),g; \
- 		s,@DEFLIB@,$(DEFLIB),g; \
--		s,@SPELLHIST@,$(SPELLHIST),g' >spell
-+		s,@SPELLHIST@,$(SPELLHIST),g" >spell
+-	echo '#!$(SHELL)' | cat - spell.sh | sed ' s,@DEFBIN@,$(DEFBIN),g; s,@SV3BIN@,$(SV3BIN),g; s,@DEFLIB@,$(DEFLIB),g; s,@SPELLHIST@,$(SPELLHIST),g' >spell
++	echo '#!$(SHELL)' | cat - spell.sh | sed " s,@DEFBIN@,$(DEFBIN),g; s,@SV3BIN@,$(SV3BIN),g; s,@DEFLIB@,$(DEFLIB),g; s,@SPELLHIST@,$(SPELLHIST),g" >spell
  	chmod 755 spell
  
  compress: compress.sh
--	echo '#!$(SHELL)' | cat - compress.sh | sed ' \
-+	echo '#!$(SHELL)' | cat - compress.sh | sed " \
- 		s,@DEFBIN@,$(DEFBIN),g; \
- 		s,@SV3BIN@,$(SV3BIN),g; \
- 		s,@DEFLIB@,$(DEFLIB),g; \
--		s,@SPELLHIST@,$(SPELLHIST),g' >compress
-+		s,@SPELLHIST@,$(SPELLHIST),g" >compress
+-	echo '#!$(SHELL)' | cat - compress.sh | sed ' s,@DEFBIN@,$(DEFBIN),g; s,@SV3BIN@,$(SV3BIN),g; s,@DEFLIB@,$(DEFLIB),g; s,@SPELLHIST@,$(SPELLHIST),g' >compress
++	echo '#!$(SHELL)' | cat - compress.sh | sed " s,@DEFBIN@,$(DEFBIN),g; s,@SV3BIN@,$(SV3BIN),g; s,@DEFLIB@,$(DEFLIB),g; s,@SPELLHIST@,$(SPELLHIST),g" >compress
  	chmod 755 compress
  
  spellprog: spellprog.o hash.o hashlook.o huff.o
