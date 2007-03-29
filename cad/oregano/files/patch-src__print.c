eb09611248b082e6311a59ac2b29bf55b65e7249
Martin Wilke <miwi@FreeBSD.org>
2007-03-29 09:35:49 +0000

    - Fix build
    PR:             11102
    Submitted by:   trasz

---
+++ cad/oregano/files/patch-src__print.c
@@ -0,0 +1,10 @@
+--- src/print.c.orig	Thu Mar 29 10:30:23 2007
++++ src/print.c	Thu Mar 29 10:30:26 2007
+@@ -31,6 +31,7 @@
+ #include <math.h>
+ #include <gnome.h>
+ #include <libgnomeprint/gnome-print.h>
++#include <libgnomeprint/gnome-print-paper.h>
+ #include "schematic.h"
+ #include "schematic-view.h"
+ #include "sheet.h"
