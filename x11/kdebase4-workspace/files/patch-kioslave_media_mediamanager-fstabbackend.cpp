1d98c9fedcdc24fbfd178a783c881afc5c24a136
lofi <lofi@FreeBSD.org>
2007-03-29 00:31:22 +0000

    Disable polling of smb filesystems listed in fstab.

--- x11/kdebase4-workspace/files/patch-kioslave_media_mediamanager-fstabbackend.cpp
+++ x11/kdebase4-workspace/files/patch-kioslave_media_mediamanager-fstabbackend.cpp
@@ -1,8 +1,10 @@
---- kioslave/media/mediamanager/fstabbackend.cpp.orig	Fri Feb  9 04:11:01 2007
-+++ kioslave/media/mediamanager/fstabbackend.cpp	Fri Feb  9 04:11:10 2007
-@@ -154,7 +154,6 @@
+--- kioslave/media/mediamanager/fstabbackend.cpp.orig	Sun Oct  1 19:31:54 2006
++++ kioslave/media/mediamanager/fstabbackend.cpp	Thu Mar 29 02:19:18 2007
+@@ -152,9 +152,7 @@
+ 	  // We might want to display only network shares
+ 	  // since HAL doesn't handle them
  	  || ( networkSharesOnly
- 	    && mount->mountType().find( "smb" ) == -1
+-	    && mount->mountType().find( "smb" ) == -1
  	    && mount->mountType().find( "cifs" ) == -1
 -	    && mount->mountType().find( "nfs" ) == -1
  	     )
