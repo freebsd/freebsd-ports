6f14c4537470258b5b66bcc416e8f78633695bbc
Martin Wilke <miwi@FreeBSD.org>
2007-03-29 13:20:59 +0000

    - Update to 0.9.9
    PR:		110947
    Submitted by:	trasz

--- audio/cheesetracker/files/patch-common::plugins::effects::custom::chorus_effect.cpp
+++ audio/cheesetracker/files/patch-common::plugins::effects::custom::chorus_effect.cpp
@@ -1,19 +1,11 @@
---- common/plugins/effects/custom/chorus_effect.cpp.orig	Sun Oct 26 05:07:38 2003
-+++ common/plugins/effects/custom/chorus_effect.cpp	Thu Oct 30 12:00:04 2003
-@@ -10,10 +10,16 @@
+--- common/plugins/effects/custom/chorus_effect.cpp.orig	Fri Apr 23 05:21:14 2004
++++ common/plugins/effects/custom/chorus_effect.cpp	Tue Mar 27 20:51:01 2007
+@@ -10,7 +10,7 @@
  //
  //
  #include "chorus_effect.h"
+-
 +#include <math.h>
  
- 
- #define FRACTIONAL_BITS 13
  #define MIX_CALCULATE_INCREMENT_INTERVAL 8
-+
-+#if defined(__FreeBSD__)
-+  #define lrint(flt)	((int) (flt))
-+  #define lrintf(flt)	((int) (flt))
-+#endif
  
- 
- inline static double get_msecs_from_samples(double p_samples,double p_mixfreq) {
