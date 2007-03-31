14782b4f28808203e04c0a31506699c0686f253a
Alejandro Pulver <alepulver@FreeBSD.org>
2007-03-31 02:32:23 +0000

    - Fix Makefile patch. - Remove part of patch-server__sv_main.c which was causing a "Bad challenge"   error when connecting to a password protected server [1].
    Reported by:	"Kyle Hunter" <keletmaster@gmail.com> [1]

--- games/alienarena/files/patch-server__sv_main.c
+++ games/alienarena/files/patch-server__sv_main.c
@@ -1,6 +1,6 @@
---- ./server/sv_main.c.orig	Tue May 16 15:20:10 2006
-+++ ./server/sv_main.c	Tue May 16 15:20:10 2006
-@@ -445,8 +445,9 @@
+--- server/sv_main.c.orig	Fri Mar 30 23:15:20 2007
++++ server/sv_main.c	Fri Mar 30 23:16:36 2007
+@@ -450,8 +450,9 @@
  		return;
  	}
  
@@ -12,20 +12,7 @@
  
  	//check it is not overflowed, save enough bytes for /ip/111.222.333.444:55555
  	if (strlen(userinfo) + 25 >= sizeof(userinfo)-1)
-@@ -502,8 +503,11 @@
- 		{
- 			if (NET_CompareBaseAdr (net_from, svs.challenges[i].adr))
- 			{
--				if (challenge == svs.challenges[i].challenge)
-+				// sku - ignore zero challenges
-+				if( svs.challenges[i].challenge && challenge == svs.challenges[i].challenge ) {
-+					svs.challenges[i].challenge = 0;
- 					break;		// good
-+				}
- 				Netchan_OutOfBandPrint (NS_SERVER, adr, "print\nBad challenge.\n");
- 				return;
- 			}
-@@ -588,12 +592,6 @@
+@@ -595,12 +596,6 @@
  			Netchan_OutOfBandPrint (NS_SERVER, adr, "print\nConnection refused.\n" );
  		Com_DPrintf ("Game rejected a connection.\n");
  		return;
