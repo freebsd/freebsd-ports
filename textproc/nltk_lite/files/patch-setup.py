e1ba073adc927f0823f0764bf366ad7bb621e193
clsung <clsung@FreeBSD.org>
2007-04-02 07:56:00 +0000

    - Update to 0.7.3 - use py-yaml in ports tree instead of bundle one.

---
+++ textproc/nltk_lite/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	Sun Jan 14 14:54:14 2007
++++ setup.py	Sun Feb 25 03:03:35 2007
+@@ -36,6 +36,6 @@
+                 'nltk_lite.misc', 'nltk_lite.model', 'nltk_lite.cluster',
+                 'nltk_lite.semantics', 'nltk_lite.wordnet', 'nltk_lite.etree',
+                 'nltk_lite.contrib', 'nltk_lite.contrib.toolbox',
+-                'nltk_lite.contrib.fst', 'yaml'
++                'nltk_lite.contrib.fst'
+                 ],
+     )
