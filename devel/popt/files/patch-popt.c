66c222db396f1c58f6014f940e6a292a1954c16e
sat <sat@FreeBSD.org>
2007-04-04 08:33:59 +0000

    - Remove qualifiers on some functions' return types - This tweak pacifies gcc 4.x for ports depending   on popt
    Reported by:	pointyhat via kris
    Discussed with:	rafan

--- devel/popt/files/patch-popt.c
+++ devel/popt/files/patch-popt.c
@@ -1,11 +1,8 @@
-
-$FreeBSD$
-
---- popt.c	2001/01/31 09:50:38	1.1
-+++ popt.c	2001/01/31 09:51:08
-@@ -5,6 +5,9 @@
- #include "system.h"
- 
+--- ./popt.c.orig	Thu Aug 22 18:17:44 2002
++++ ./popt.c	Wed Apr  4 12:25:14 2007
+@@ -14,6 +14,9 @@
+ #include <float.h>
+ #endif
  #include <math.h>
 +#ifdef __FreeBSD__
 +#include <machine/float.h>
@@ -13,3 +10,12 @@ $FreeBSD$
  
  #include "findme.h"
  #include "poptint.h"
+@@ -1153,7 +1156,7 @@
+     /*@=nullderef@*/
+ }
+ 
+-const char *const poptStrerror(const int error)
++const char * poptStrerror(const int error)
+ {
+     switch (error) {
+       case POPT_ERROR_NOARG:
