2ec15bd3a734c8a0478e20ad2843a4a1311dc1ab
Andrew Pantyukhin <sat@FreeBSD.org>
2007-04-04 08:33:59 +0000

    - Remove qualifiers on some functions' return types - This tweak pacifies gcc 4.x for ports depending   on popt
    Reported by:	pointyhat via kris
    Discussed with:	rafan

--- devel/popt/files/patch-test3.c
+++ devel/popt/files/patch-test3.c
@@ -1,5 +1,5 @@
---- test3.c.orig	Wed Aug 18 23:57:09 2004
-+++ test3.c	Thu Aug 19 00:01:27 2004
+--- ./test3.c.orig	Sat Jul 20 23:04:08 2002
++++ ./test3.c	Wed Apr  4 12:24:18 2007
 @@ -9,7 +9,7 @@
  int main (int argc, char **argv) {
      char *out;
