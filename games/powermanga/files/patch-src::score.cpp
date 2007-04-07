8ed7e7476e3842875ce173b297c8d4638013613c
Rong-En Fan <rafan@FreeBSD.org>
2007-04-07 11:19:17 +0000

    - Update to 0.80 - Move to LOCALBASE

--- games/powermanga/files/patch-src::score.cpp
+++ games/powermanga/files/patch-src::score.cpp
@@ -1,11 +1,11 @@
---- src/score.cpp.orig	Sat Jan 22 01:59:45 2005
-+++ src/score.cpp	Sat Jan 22 01:59:56 2005
-@@ -449,7 +449,7 @@
-     }
-     close(_iHandle);
-   }
--  if(_pMemoire)
+--- src/score.cpp.orig	Mon Jan 31 17:44:33 2005
++++ src/score.cpp	Sat Apr  7 18:00:49 2007
+@@ -749,7 +749,7 @@
+   //##############################################################
+   // copy data file into memory structure
+   //##############################################################
+-  if (_pMemoire)
 +  if(_pMemoire && sDescriptionFichier.st_size > 0)
-   {
-     char                   *_pBufferC = _pMemoire;
-     int                    *_pBufferL;
+     {
+       char *_pBufferC = _pMemoire;
+       Sint32 *_pBufferL;
