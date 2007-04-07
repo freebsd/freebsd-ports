7858d12efca7a8d0fbaf89ae599af5be80b26e64
Rong-En Fan <rafan@FreeBSD.org>
2007-04-07 15:49:51 +0000

    - Update ggz ports to 0.0.14 - Introduce bsd.ggz.mk to simplify all ggz ports - Remove 4.x support
    Approved by:	 Ying-Chieh Chen (maintainer)

---
+++ games/libggz/bsd.ggz.mk
@@ -0,0 +1,29 @@
+# $FreeBSD$
+
+# common settings
+
+GGZ_VERSION=	0.0.14
+
+MASTER_SITES=	\
+	http://mirrors.dotsrc.org/ggzgamingzone/ggz/%SUBDIR%/ \
+	http://ftp.ggzgamingzone.org/pub/ggz/%SUBDIR%/
+MASTER_SITE_SUBDIR=	${PORTVERSION}
+DIST_SUBDIR=	ggz
+
+# components section
+
+_USE_GGZ_ALL=	core client
+
+core_LIB_DEPENDS=	ggz.5:${PORTSDIR}/games/libggz
+
+client_LIB_DEPENDS=	ggzcore.9:${PORTSDIR}/games/ggz-client-libs
+
+.for component in ${USE_GGZ}
+LIB_DEPENDS+=	${${component}_LIB_DEPENDS}
+. endfor
+
+.for component in ${USE_GGZ}
+. if ${_USE_GGZ_ALL:M${component}}==""
+IGNORE=	cannot install: Unknown component ${component}
+. endif
+.endfor
