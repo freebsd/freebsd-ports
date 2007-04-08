23f442e8fe1bd4bfb7ebb1f5af392095af512e2d
Maxim Sobolev <sobomax@FreeBSD.org>
2007-04-08 18:07:23 +0000

    Fix on ia64.
    Poked by:	Kris's unbildable ports reminder

---
+++ net/ortp/files/patch-src::stun.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/stun.c
++++ src/stun.c
+@@ -768,7 +768,7 @@
+ 	read(fd,&tick,sizeof(tick));
+ 	closesocket(fd);
+       }
+-#elif defined(__linux) 
++#elif defined(__linux) || defined (__FreeBSD__) 
+       {
+  	fd_set fdSet;
+ 	int maxFd=0;
