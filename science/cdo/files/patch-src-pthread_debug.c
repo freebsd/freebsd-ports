c59b817325ed239dbd4b0937302bb8e58517efec
lwhsu <lwhsu@FreeBSD.org>
2007-04-10 03:09:55 +0000

    - Update to 1.0.7
    PR:		111346
    Submitted by:	Sunpoet Po-Chuan Hsieh <sunpoet AT sunpoet.net> (maintainer)
    Approved by:	clsung (mentor)

--- science/cdo/files/patch-src-pthread_debug.c
+++ science/cdo/files/patch-src-pthread_debug.c
@@ -1,10 +1,10 @@
 --- src/pthread_debug.c.orig	Mon Dec  4 16:11:40 2006
-+++ src/pthread_debug.c	Mon Dec 18 10:01:00 2006
++++ src/pthread_debug.c	Sat Apr  7 01:12:05 2007
 @@ -77,6 +77,7 @@
  }
  
  
-+#if __FreeBSD_version >= 600000
++#if defined (PTHREAD_MUTEXATTR_CONDATTR)
  void print_pthread_mutexattr(const char *caller,  pthread_mutexattr_t *m_attr)
  {
    int protocol, kind, pshared;
--- science/cdo/files/patch-src-pthread_debug.h
+++ science/cdo/files/patch-src-pthread_debug.h
@@ -1,10 +1,10 @@
 --- src/pthread_debug.h.orig	Wed Feb 15 21:22:38 2006
-+++ src/pthread_debug.h	Mon Dec 18 10:01:00 2006
++++ src/pthread_debug.h	Sat Apr  7 01:13:25 2007
 @@ -15,8 +15,10 @@
  void Pthread_cond_wait(const char *caller, pthread_cond_t *cond, pthread_mutex_t *mutex);
  
  void print_pthread_attr(const char *caller, pthread_attr_t *attr);
-+#if __FreeBSD_version >= 600000
++#if defined (PTHREAD_MUTEXATTR_CONDATTR)
  void print_pthread_mutexattr(const char *caller,  pthread_mutexattr_t *m_attr);
  void print_pthread_condattr(const char *caller, pthread_condattr_t *c_attr);
 +#endif
