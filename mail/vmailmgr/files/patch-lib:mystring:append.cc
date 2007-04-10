53cea4891329676c187124f87812374b1a4a678c
miwi <miwi@FreeBSD.org>
2007-04-10 17:40:51 +0000

    2007-03-27 emulators/kmamerun: Project was abandoned 4 years ago and expects an old version of XMAME, please use other frontends instead (like gxmame) 2007-03-28 graphics/hobbes-icons-xpm: Archaic port 2007-04-10 japanese/firefox-ja: Incomplete pkg-plist 2007-04-10 japanese/lookup-xemacs: Does not install 2007-04-10 lang/linux-hla: Does not compile 2007-04-10 mail/vmailmgr: Incomplete pkg-plist 2007-04-10 multimedia/qvamps: Touches filesystem prior to 'make install' 2007-03-10 net-mgmt/sting: Broken on all supported versions of FreeBSD 2007-04-10 net-mgmt/tas: Incomplete pkg-plist 2007-04-10 net-p2p/verlihub-plugins: Does not configure, it needs at least verlihub 1.0 2007-04-10 news/inn-stable: Fails to patch 2007-04-10 palm/malsync: Does not build with new pilot-link 2007-04-10 russian/elm.language: Leaves behind files on deinstall 2007-04-10 russian/pine.language: Leaves behind config file on deinstall 2007-04-01 science/py-scipy03: Replaced by py-scipy 2007-04-10 security/php4-cryptopp: Does not compile

--- mail/vmailmgr/files/patch-lib:mystring:append.cc
+++
@@ -1,8 +0,0 @@
---- lib/mystring/append.cc.orig	Mon Feb  2 14:41:33 2004
-+++ lib/mystring/append.cc	Mon Feb  2 14:41:52 2004
-@@ -1,3 +1,5 @@
-+#include <string.h>
-+
- #include "mystring.h"
- #include "trace.h"
- 
