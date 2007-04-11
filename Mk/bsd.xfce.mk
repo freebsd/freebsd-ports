9646c0a68d582c7a1f79179b81c340ae472fe135
Oliver Lehmann <oliver@FreeBSD.org>
2007-04-11 16:26:53 +0000

    update xfce to 4.4.1 bump PORTREVISION where needed

--- Mk/bsd.xfce.mk
+++ Mk/bsd.xfce.mk
@@ -15,27 +15,33 @@ Xfce_Include_MAINTAINER=	oliver@FreeBSD.org
 _USE_XFCE_ALL=			configenv libexo libgui libutil libmcs mcsmanager panel \
 				thunar wm xfdev
 
-MASTER_SITE_SUBDIR?=		xfce-4.4.0
+MASTER_SITE_SUBDIR?=		xfce-4.4.1
 
 configenv_CONFIGURE_ENV=	CPPFLAGS="${CPPFLAGS} -I${LOCALBASE}/include -L${LOCALBASE}/lib"
 
-libexo_LIB_DEPENDS=		exo-0.3:${PORTSDIR}/x11/libexo
+libexo_BUILD_DEPENDS=		libexo>=0.3.2:${PORTSDIR}/x11/libexo
+libexo_RUN_DEPENDS=		libexo>=0.3.2:${PORTSDIR}/x11/libexo
 
-libgui_LIB_DEPENDS=		xfcegui4.6:${PORTSDIR}/x11-toolkits/libxfce4gui
+libgui_BUILD_DEPENDS=		libxfce4gui>=4.4.1:${PORTSDIR}/x11-toolkits/libxfce4gui
+libgui_RUN_DEPENDS=		libxfce4gui>=4.4.1:${PORTSDIR}/x11-toolkits/libxfce4gui
 
-libutil_LIB_DEPENDS=		xfce4util.4:${PORTSDIR}/x11/libxfce4util
+libutil_BUILD_DEPENDS=		libxfce4util>=4.4.1:${PORTSDIR}/x11/libxfce4util
+libutil_RUN_DEPENDS=		libxfce4util>=4.4.1:${PORTSDIR}/x11/libxfce4util
 
-libmcs_LIB_DEPENDS=		xfce4mcs-manager.3:${PORTSDIR}/x11/libxfce4mcs
+libmcs_BUILD_DEPENDS=		libxfce4mcs>=4.4.1:${PORTSDIR}/x11/libxfce4mcs
+libmcs_RUN_DEPENDS=		libxfce4mcs>=4.4.1:${PORTSDIR}/x11/libxfce4mcs
 
-mcsmanager_BUILD_DEPENDS=	xfce-mcs-manager:${PORTSDIR}/sysutils/xfce4-mcs-manager
-mcsmanager_RUN_DEPENDS=		xfce-mcs-manager:${PORTSDIR}/sysutils/xfce4-mcs-manager
+mcsmanager_BUILD_DEPENDS=	xfce4-mcs-manager>=4.4.1:${PORTSDIR}/sysutils/xfce4-mcs-manager
+mcsmanager_RUN_DEPENDS=		xfce4-mcs-manager>=4.4.1:${PORTSDIR}/sysutils/xfce4-mcs-manager
 
-panel_LIB_DEPENDS=		xfce4panel.2:${PORTSDIR}/x11-wm/xfce4-panel
+panel_BUILD_DEPENDS=		xfce4-panel>=4.4.1:${PORTSDIR}/x11-wm/xfce4-panel
+panel_RUN_DEPENDS=		xfce4-panel>=4.4.1:${PORTSDIR}/x11-wm/xfce4-panel
 
-thunar_LIB_DEPENDS=		thunar-vfs-1.4:${PORTSDIR}/x11-fm/thunar
+thunar_BUILD_DEPENDS=		Thunar>=0.8.0:${PORTSDIR}/x11-fm/thunar
+thunar_RUN_DEPENDS=		Thunar>=0.8.0:${PORTSDIR}/x11-fm/thunar
 
-wm_BUILD_DEPENDS=		xfwm4:${PORTSDIR}/x11-wm/xfce4-wm
-wm_RUN_DEPENDS=			xfwm4:${PORTSDIR}/x11-wm/xfce4-wm
+wm_BUILD_DEPENDS=		xfce4-wm>=4.4.1:${PORTSDIR}/x11-wm/xfce4-wm
+wm_RUN_DEPENDS=			xfce4-wm>=4.4.1:${PORTSDIR}/x11-wm/xfce4-wm
 
 xfdev_RUN_DEPENDS=		xfce4-dev-tools:${PORTSDIR}/devel/xfce4-dev-tools
 
