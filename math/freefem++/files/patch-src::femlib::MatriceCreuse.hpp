af13b69d8d9ec8747687dac163d37ea46690398b
thierry <thierry@FreeBSD.org>
2007-04-18 22:04:29 +0000

    Upgrade to 2.16-0.

--- math/freefem++/files/patch-src::femlib::MatriceCreuse.hpp
+++ math/freefem++/files/patch-src::femlib::MatriceCreuse.hpp
@@ -1,11 +1,23 @@
---- ./src/femlib/MatriceCreuse.hpp.orig	Fri Jun 25 10:56:36 2004
-+++ ./src/femlib/MatriceCreuse.hpp	Thu Jul  8 00:19:27 2004
-@@ -10,7 +10,7 @@
+--- ./src/femlib/MatriceCreuse.hpp.orig	Mon Apr 16 15:29:19 2007
++++ ./src/femlib/MatriceCreuse.hpp	Wed Apr 18 22:10:17 2007
+@@ -10,15 +10,19 @@
  #include <umfpack.h>
  #else
  #ifdef HAVE_UMFPACK_UMFPACK_H
 -#include <umfpack/umfpack.h>
 +#include <UMFPACK/umfpack.h>
  #else
+ #ifdef HAVE_BIG_UMFPACK_UMFPACK_H
+ #include <UMFPACK/umfpack.h>
+ #else
++#ifdef HAVE_UFSPARSE_UMFPACK_H
++#include <ufsparse/umfpack.h>
++#else
  
    // Defaults to a local version of the UMFPACK headers
+ #include "../../download/include/umfpack.h"
+ 
++#endif // HAVE_UFSPARSE_UMFPACK_H
+ #endif // HAVE_BIG_UMFPACK_UMFPACK_H
+ #endif // HAVE_UMFPACK_UMFPACK_H
+ #endif // HAVE_UMFPACK_H
