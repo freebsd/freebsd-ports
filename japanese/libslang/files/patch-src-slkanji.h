7da6a5b029ac26a77ded559cdd3d1bb3ea531a13
rafan <rafan@FreeBSD.org>
2007-04-20 19:02:11 +0000

    - Fix build with gcc4 - Replace deprecated variable: INSTALLS_SHLIB - Pet portlint(1)
    PR:		ports/111283
    Submitted by:	rafan
    Approved by:	maintainer timeout (14 days)

---
+++ japanese/libslang/files/patch-src-slkanji.h
@@ -0,0 +1,10 @@
+--- src/slkanji.h.orig	Fri Apr  6 03:48:00 2007
++++ src/slkanji.h	Fri Apr  6 03:48:03 2007
+@@ -48,7 +48,6 @@
+ #define NULL 0
+ #endif
+ 
+-extern char *Kcode[];
+ extern int	kSLfiAuto, SKanaToDKana;
+ extern int	kSLcode;
+ extern int	kSLfile_code, kSLinput_code, kSLdisplay_code, kSLsystem_code;
