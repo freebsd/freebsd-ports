4bdc7dc16f2609af674250986ac8ba0c5949480b
mezz <mezz@FreeBSD.org>
2007-04-23 21:25:44 +0000

    Change this patch to a better fix, bump the PORTREVISION.
    GNOME Bugzilla:	http://bugzilla.gnome.org/show_bug.cgi?id=432387
    Submitted by:	Charles Kerr (developer)
    Tested by:	pav

--- news/pan/files/patch-pan_data_article.cc
+++ news/pan/files/patch-pan_data_article.cc
@@ -1,11 +1,12 @@
---- pan/data/article.cc.orig	Sun Apr 22 15:17:04 2007
-+++ pan/data/article.cc	Sun Apr 22 15:17:44 2007
-@@ -153,7 +153,7 @@
+--- pan/data/article.cc.orig	Mon Apr 23 16:21:51 2007
++++ pan/data/article.cc	Mon Apr 23 16:23:29 2007
+@@ -153,7 +153,8 @@
  
    kc = &key.back();
    mc = &mid.back();
 -  ke = key.str + 1 + std::max (int(b), (int)minlen - UCHAR_MAX);
-+  ke = key.str + 1 + std::max ((char)b, (char)(minlen - UCHAR_MAX));
++  ke = std::max (key.str + b + 1,
++                 key.str + key.len - UCHAR_MAX);
    while (kc>ke)
      if (*kc-- != *mc--) { ++kc; break; }
    const size_t e (&key.back() - kc);
