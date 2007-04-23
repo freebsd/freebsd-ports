361cdc31486c92a06e013055b59b6ff86a480b47
clsung <clsung@FreeBSD.org>
2007-04-23 01:44:44 +0000

    - Fix build error in 7-CURRENT
    Noted by:	kris

--- sysutils/fusefs-funionfs/files/patch-funionfs.h
+++ sysutils/fusefs-funionfs/files/patch-funionfs.h
@@ -1,9 +1,10 @@
---- funionfs.h.orig	Mon Feb 26 21:06:15 2007
-+++ funionfs.h	Mon Feb 26 21:06:40 2007
-@@ -2,6 +2,8 @@
+--- funionfs.h.orig	Fri Jan 19 06:35:52 2007
++++ funionfs.h	Mon Apr 23 09:39:54 2007
+@@ -21,6 +21,9 @@
  #ifndef _FUNIONFS_H
  #define _FUNIONFS_H
  
++#include <limits.h>
 +#include <pthread.h>
 +
  // use the new API
