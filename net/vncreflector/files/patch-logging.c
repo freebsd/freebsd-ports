b5c3d34125d3e07a5f0356b6cf597b5fe8889478
Maxim Sobolev <sobomax@FreeBSD.org>
2007-04-24 05:57:15 +0000

    Remove bogus hunk.

--- net/vncreflector/files/patch-logging.c
+++ net/vncreflector/files/patch-logging.c
@@ -3,15 +3,6 @@ $FreeBSD$
 
 --- logging.c
 +++ logging.c
-@@ -10,7 +10,7 @@
-  * This software was authored by Constantin Kaplinsky <const@ce.cctpu.edu.ru>
-  * and sponsored by HorizonLive.com, Inc.
-  *
-- * $Id: logging.c,v 1.1 2007/04/24 05:32:17 root Exp $
-+ * $Id: logging.c,v 1.5 2003/04/21 17:20:35 const Exp $
-  * Logging implementation
-  */
- 
 @@ -138,8 +138,6 @@
    char time_buf[32];
    char level_char = ' ';
