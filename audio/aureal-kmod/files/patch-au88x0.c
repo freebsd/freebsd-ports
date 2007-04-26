2f5821aeb0b974fa410fd39be19e866ab12f76c4
Andrey Slusar <anray@FreeBSD.org>
2007-04-26 19:35:52 +0000

    Fix build on 7.0-CURRENT

--- audio/aureal-kmod/files/patch-au88x0.c
+++ audio/aureal-kmod/files/patch-au88x0.c
@@ -1,8 +1,5 @@
-
-$FreeBSD$
-
---- au88x0.c.orig	Fri May 10 10:32:55 2002
-+++ au88x0.c	Sun Aug  7 18:14:57 2005
+--- au88x0.c.orig	Fri May 10 04:32:55 2002
++++ au88x0.c	Thu Apr 26 22:18:49 2007
 @@ -29,10 +29,16 @@
   */
  
@@ -21,16 +18,20 @@ $FreeBSD$
  #include <sys/queue.h>
  
  SND_DECLARE_FILE("$FreeBSD$");
-@@ -573,7 +579,7 @@
+@@ -573,7 +579,11 @@
  	ch->channel = c;
  	ch->buffer = b;
  	ch->run = 0;
 -	if (sndbuf_alloc(ch->buffer, au->parent_dmat, AU_BUFFSIZE) == -1) {
-+	if (sndbuf_alloc(ch->buffer, au->parent_dmat, AU_BUFFSIZE) != 0) {
++	if (sndbuf_alloc(ch->buffer, au->parent_dmat,
++#if __FreeBSD_version >= 700037
++					0,
++#endif
++					AU_BUFFSIZE) != 0) {
  		printf("sndbuf_alloc failed\n");
  		return NULL;
  	}
-@@ -768,7 +774,7 @@
+@@ -768,7 +778,7 @@
  	}
  
  	if (s) device_set_desc(dev, s);
@@ -39,7 +40,7 @@ $FreeBSD$
  }
  
  static int
-@@ -802,7 +808,7 @@
+@@ -802,7 +812,7 @@
  	data = pci_read_config(dev, PCIR_COMMAND, 2);
  
  	for (i = 0; i < 3; i++) {
@@ -48,7 +49,7 @@ $FreeBSD$
  		au->regtype[i] = SYS_RES_MEMORY;
  		au->reg[i] = bus_alloc_resource(dev, au->regtype[i], &au->regid[i], 0, ~0, 1, RF_ACTIVE);
  		if (!au->reg[i]) {
-@@ -852,7 +858,11 @@
+@@ -852,7 +862,11 @@
  		/*highaddr*/BUS_SPACE_MAXADDR,
  		/*filter*/NULL, /*filterarg*/NULL,
  		/*maxsize*/AU_BUFFSIZE, /*nsegments*/1, /*maxsegz*/0x3ffff,
@@ -61,7 +62,7 @@ $FreeBSD$
  		device_printf(dev, "unable to create dma tag\n");
  		goto bad;
  	}
-@@ -942,6 +952,10 @@
+@@ -942,6 +956,10 @@
  
  
  DRIVER_MODULE(snd_au88x0, pci, au_driver, pcm_devclass, 0, 0);
