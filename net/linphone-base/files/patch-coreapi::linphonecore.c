699f03a5c1bc1c2a118533d4070ad3a5e1076374
Li-Wen Hsu <lwhsu@FreeBSD.org>
2007-04-28 12:50:11 +0000

    - Update to version 1.7.1
    PR:		112137
    Submitted by:	KATO Tsuguru <tkato432 AT yahoo.com>
    Approved by:	clsung (mentor, implicitly)

--- net/linphone-base/files/patch-coreapi::linphonecore.c
+++ net/linphone-base/files/patch-coreapi::linphonecore.c
@@ -12,13 +12,3 @@
  
  
  static const char *liblinphone_version=LIBLINPHONE_VERSION;
-@@ -1179,7 +1177,9 @@
- 
- void linphone_core_init_media_streams(LinphoneCore *lc){
- 	lc->audiostream=audio_stream_new(linphone_core_get_audio_port(lc),linphone_core_ipv6_enabled(lc));
-+#ifdef VIDEO_ENABLED
- 	lc->videostream=video_stream_new(linphone_core_get_video_port(lc),linphone_core_ipv6_enabled(lc));
-+#endif
- }
- 
- void linphone_core_start_media_streams(LinphoneCore *lc, LinphoneCall *call){
