e3dbd47bf1e725dab67805e6039f6353dbe5e1a7
Hiroki Sato <hrs@FreeBSD.org>
2007-04-28 18:55:05 +0000

    KLayout is a Qt-based GDS2 viewer.

---
+++ cad/klayout/files/patch-build.sh
@@ -0,0 +1,17 @@
+--- build.sh.orig	Sat Apr 28 23:13:29 2007
++++ build.sh	Sat Apr 28 23:13:40 2007
+@@ -61,12 +61,12 @@
+   exit 1
+ fi
+ 
+-if [ ! -r $QTDIR/bin/moc ]; then
++if [ ! -r $QTDIR/bin/moc4 ]; then
+   echo "*** ERROR: unable to locate Qt MOC in $QTDIR/bin"
+   exit 1
+ fi
+ 
+-if [ ! -r $QTDIR/bin/uic ]; then
++if [ ! -r $QTDIR/bin/uic4 ]; then
+   echo "*** ERROR: unable to locate Qt UIC in $QTDIR/bin"
+   exit 1
+ fi
