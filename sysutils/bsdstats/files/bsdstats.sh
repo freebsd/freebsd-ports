b5b0f7f9b52ca786c9040093163ea0f033f27024
scrappy <scrappy@FreeBSD.org>
2007-04-28 23:54:03 +0000

    forgot to cvs add the start up file ...

---
+++ sysutils/bsdstats/files/bsdstats.sh
@@ -0,0 +1,30 @@
+#!/bin/sh 
+#
+# $FreeBSD: /tmp/pcvs/ports/sysutils/bsdstats/files/Attic/bsdstats.sh,v 1.1 2007-04-28 23:54:03 scrappy Exp $
+#
+
+# PROVIDE: bsdstats
+# REQUIRE: LOGIN
+# KEYWORD: shutdown
+
+# bsdstats is disabled by default, if you have configuration file
+#
+# Add the following line to /etc/rc.conf to enable bsdstats:
+#
+#bsdstats_enable="YES"
+#
+
+. /etc/rc.subr
+
+load_rc_config bsdstats
+
+name=bsdstats
+rcvar=`set_rcvar`
+
+command=/usr/local/etc/periodic/monthly/300.statistics
+
+# default to disable
+bsdstats_enable=${bsdstats_enable:-"NO"}
+bsdstats_flags=${bsdstats_flags:-"-nodelay"}
+
+run_rc_command "$1"
