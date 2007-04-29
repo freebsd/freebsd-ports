94b9f329251a3bff0dd4c27f90bedd1f3acce8b4
Hiroki Sato <hrs@FreeBSD.org>
2007-04-29 07:20:02 +0000

    GDT (graphics data text) format translator written in C/C++ that converts a binary gdsii file to a text format that is compact and easy to parse.

---
+++ cad/gdt/files/patch-gdsStream.h
@@ -0,0 +1,12 @@
+--- gdsStream.h.orig	Sun Apr 29 15:45:42 2007
++++ gdsStream.h	Sun Apr 29 15:45:54 2007
+@@ -3,8 +3,7 @@
+ #define _gdsStream__
+ 
+ #include <stdio.h>
+-#include <malloc.h>
+-#include <stdio.h>
++#include <stdlib.h>
+ #include <time.h>
+ #include <kvstypes.h>
+ 
