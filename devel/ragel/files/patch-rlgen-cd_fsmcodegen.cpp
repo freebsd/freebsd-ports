4c37f88095d962eb50e980b00ba4946e25b58cb8
shaun <shaun@FreeBSD.org>
2007-04-29 23:45:29 +0000

    Upgrade to 5.20.

--- devel/ragel/files/patch-rlgen-cd_fsmcodegen.cpp
+++ devel/ragel/files/patch-rlgen-cd_fsmcodegen.cpp
@@ -1,5 +1,5 @@
---- rlcodegen/fsmcodegen.cpp.orig	Wed Dec 21 15:15:28 2005
-+++ rlcodegen/fsmcodegen.cpp	Mon Dec 26 13:36:08 2005
+--- rlgen-cd/fsmcodegen.cpp.orig	Wed Dec 21 15:15:28 2005
++++ rlgen-cd/fsmcodegen.cpp	Mon Dec 26 13:36:08 2005
 @@ -25,6 +25,7 @@
  #include "fsmcodegen.h"
  #include "redfsm.h"
