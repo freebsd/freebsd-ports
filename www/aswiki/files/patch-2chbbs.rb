dd41871c8741a4c74e65add25e0625a9a09f7c36
Makoto Matsushita <matusita@FreeBSD.org>
2007-04-29 13:14:01 +0000

    Yet another ruby-1.8.6's broken backward compatibility fix.
      I dunno why, but array data cannot be (implicitly) converted to string
      data, so covert the array data explicitly with 'to_s'.
    
    Bump PORTREVISION as usual.
    
    Please note that I'll report all the patches to the aswiki author iff
    I've confirmed that aswiki works well on ruby-1.8.6.

--- www/aswiki/files/patch-2chbbs.rb
+++ www/aswiki/files/patch-2chbbs.rb
@@ -1,5 +1,14 @@
 --- plugin/2chbbs.rb.dist	Wed Jan  8 11:01:21 2003
-+++ plugin/2chbbs.rb	Sat Apr 28 13:41:24 2007
++++ plugin/2chbbs.rb	Sun Apr 29 22:05:37 2007
+@@ -24,7 +24,7 @@
+       t = Time.now
+       time = t.strftime("%Y/%m/%d (#{weekstr[t.wday]}) %R")
+       data = "\n#{number}: #{from}: #{name} #{date}: #{time} \n\n" +
+-	session['textdata'] + "\n"
++	session['textdata'].to_s + "\n"
+       file = @repository.load(pname)
+       file[session['begin'].to_i-1, 0 ] = data
+       file[session['begin'].to_i] = "#2chbbs #{number}\n"
 @@ -40,7 +40,7 @@
        session['number'] = av[1].to_i
        @data = {
