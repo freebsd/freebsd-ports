4ba786e49ded8d06cd62b603daf9951de5f88f1d
Joe Marcus Clarke <marcus@FreeBSD.org>
2007-04-30 00:20:33 +0000

    * Update to 0.9.5 * Respect PTHREAD_{CFLAGS,LIBS} * Make sure pulse-dependent applications are linked against PTHREAD_LIBS [1] * Add missing pulse and pulse-access user and groups
    Reported by:	Romain Tartiere <romain@blogreen.org> [1]

---
+++ audio/pulseaudio/files/patch-src_modules_oss-util.c
@@ -0,0 +1,25 @@
+--- src/modules/oss-util.c.orig	Sun Apr 29 19:53:31 2007
++++ src/modules/oss-util.c	Sun Apr 29 19:55:24 2007
+@@ -40,6 +40,22 @@
+ 
+ #include "oss-util.h"
+ 
++#ifndef DSP_CAP_COPROC
++#define DSP_CAP_COPROC 0x00000800
++#endif
++#ifndef DSP_CAP_TRIGGER
++#define DSP_CAP_TRIGGER 0x00001000
++#endif
++#ifndef DSP_CAP_MMAP
++#define DSP_CAP_MMAP 0x00002000
++#endif
++#ifndef DSP_CAP_MULTI
++#define DSP_CAP_MULTI 0x00004000
++#endif
++#ifndef DSP_CAP_BIND
++#define DSP_CAP_BIND  0x00008000
++#endif
++
+ int pa_oss_open(const char *device, int *mode, int* pcaps) {
+     int fd = -1;
+     int caps;
