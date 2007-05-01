21eb97072eceb4e0fc6a2713b5cc0e48a60fe580
Cheng-Lung Sung <clsung@FreeBSD.org>
2007-05-01 15:21:00 +0000

    - Update to 0.7.4

--- textproc/nltk_lite/files/patch-setup.py
+++ textproc/nltk_lite/files/patch-setup.py
@@ -1,10 +1,11 @@
---- setup.py.orig	Sun Jan 14 14:54:14 2007
-+++ setup.py	Sun Feb 25 03:03:35 2007
-@@ -36,6 +36,6 @@
+--- setup.py.orig	Fri Apr 13 08:29:37 2007
++++ setup.py	Tue May  1 23:16:52 2007
+@@ -36,7 +36,7 @@
                  'nltk_lite.misc', 'nltk_lite.model', 'nltk_lite.cluster',
                  'nltk_lite.semantics', 'nltk_lite.wordnet', 'nltk_lite.etree',
                  'nltk_lite.contrib', 'nltk_lite.contrib.toolbox',
--                'nltk_lite.contrib.fst', 'yaml'
-+                'nltk_lite.contrib.fst'
-                 ],
-     )
+-                'nltk_lite.contrib.fst', 'yaml',
++                'nltk_lite.contrib.fst',
+                 'nltk_lite.contrib.classifier', 'nltk_lite.contrib.classifier.exceptions',
+                 'nltk_lite.contrib.classifier_tests',
+                 'nltk_lite.contrib.dependency'
