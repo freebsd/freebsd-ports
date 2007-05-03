d8df2d7a499bccbefb7a3524c4cd6529fb0b04fe
alepulver <alepulver@FreeBSD.org>
2007-05-03 21:07:14 +0000

    - Update to version 4.3.1.

--- devel/allegro-devel/files/patch-src__i386__istretch.c
+++ devel/allegro-devel/files/patch-src__i386__istretch.c
@@ -1,13 +1,11 @@
---- src/i386/istretch.c.orig	Mon Jul 24 09:10:50 2006
-+++ src/i386/istretch.c	Wed Aug  9 19:03:27 2006
-@@ -33,9 +33,8 @@
+--- ./src/i386/istretch.c.orig	Sat Apr 21 00:54:32 2007
++++ ./src/i386/istretch.c	Thu May  3 16:04:38 2007
+@@ -37,7 +37,7 @@
  
  
  #ifdef HAVE_MPROTECT
 -   #include <sys/types.h>
 +   #include <sys/param.h>
     #include <sys/mman.h>
--   #include <sys/user.h>
  #endif     /* ifdef HAVE_MPROTECT */
- 
  
