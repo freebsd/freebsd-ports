0bb1cf4426656350817e6e1b1df2c3c896866e41
alepulver <alepulver@FreeBSD.org>
2007-05-04 00:55:30 +0000

    TorrentVolve is a cross-platform PHP-driven web-based BitTorrent client. It focuses on speed and reliability, while also providing a full feature set. It includes a user management system, a Torrent file manager, and a configurable Torrent downloader.
    WWW: http://torrentvolve.sourceforge.net/

---
+++ net-p2p/torrentvolve/files/patch-site__lib__torrentmodules__tvAzureusBridge__tvTelnetClient__client.php
@@ -0,0 +1,16 @@
+--- ./site/lib/torrentmodules/tvAzureusBridge/tvTelnetClient/client.php.orig	Tue Jan  2 07:14:20 2007
++++ ./site/lib/torrentmodules/tvAzureusBridge/tvTelnetClient/client.php	Sun Apr 22 18:57:45 2007
+@@ -74,10 +74,10 @@
+ 	public function startLocalServer() {
+ 
+ 		$path = $this->appPath . '../azureusServer';
+-		$command = 'java -Dazureus.config.path="' . $path . '" -jar "' . $path . '/Azureus2.jar" --ui=telnet';
++		$command = 'env JAVA_VERSION=1.5+ java -Dazureus.config.path="' . $path . '" -jar "' . $path . '/Azureus2.jar" --ui=telnet';
+ 
+ 		if (strtoupper(substr(PHP_OS, 0, 3) == 'WIN')) popen('start /D"' . $path . '" ' . $command, 'w');
+ 		else exec("$command > /dev/null 2>&1 &");
+ 	}
+ }
+-?>
+\ No newline at end of file
++?>
