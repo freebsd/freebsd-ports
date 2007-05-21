663be9585a7f8a3b2b56ea5f84cdf67a3847c0c4
Martin Wilke <miwi@FreeBSD.org>
2007-05-21 20:23:57 +0000

    - Update port to 1.4.10:    * Some security fixes (see below)    * Small enhancements    * A collection of bugfixes and stability enhancements
    PR:		112548
    Submitted by:	Simon Dick <simond@irrelevant.org> (maintainer)
    Approved by:	portmgr (marcus)
    Security:	http://www.vuxml.org/freebsd/0e575ed3-0764-11dc-a80b-0016179b2dd5.html

--- mail/squirrelmail/files/patch-config-config_default.php
+++ mail/squirrelmail/files/patch-config-config_default.php
@@ -1,19 +1,19 @@
---- config/config_default.php.orig	Wed Jun  2 10:49:41 2004
-+++ config/config_default.php	Wed Jun  2 10:50:21 2004
-@@ -442,7 +442,7 @@
-  *   $data_dir = SM_PATH . 'data/';
+--- config/config_default.php.orig	Wed May  9 16:48:26 2007
++++ config/config_default.php	Wed May  9 16:49:20 2007
+@@ -464,7 +464,7 @@
+  *
   * @global string $data_dir
   */
--$data_dir = SM_PATH . 'data/';
+-$data_dir = '/var/local/squirrelmail/data/';
 +$data_dir = '/var/spool/squirrelmail/pref/';
  
  /**
   * Attachments directory
-@@ -460,7 +460,7 @@
+@@ -482,7 +482,7 @@
   *    + It should probably be another directory than data_dir.
   * @global string $attachment_dir
   */
--$attachment_dir = $data_dir;
+-$attachment_dir = '/var/local/squirrelmail/attach/';
 +$attachment_dir = '/var/spool/squirrelmail/attach/';
  
  /**
