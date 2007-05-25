528ca953aa07e9b6c5bfa5ac32308a0739279733
edwin <edwin@FreeBSD.org>
2007-05-25 11:56:59 +0000

    Update for port net/asterisk-app-ldap
    	Update for port net/asterisk-app-ldap. The new version runs
    	now with asterisk 1.4.
    
    Also modified PORTNAME to asterisk-app-ldap and introduced DISTNAME
    
    PR:		ports/111774
    Submitted by:	Matthias Fechner <idefix@fechner.net>

--- net/asterisk-app-ldap/files/patch-cflags.sh
+++ net/asterisk-app-ldap/files/patch-cflags.sh
@@ -1,19 +1,14 @@
---- cflags.sh.orig	Sun Jul 30 12:23:23 2006
-+++ cflags.sh	Wed Jul 19 10:09:51 2006
-@@ -1,13 +1,13 @@
- #!/bin/sh
+--- cflags.sh.orig	Tue Apr 17 14:11:19 2007
++++ cflags.sh	Tue Apr 17 14:13:40 2007
+@@ -11,6 +11,11 @@ else
+   INCDIR=/usr/include/asterisk
+ fi
  
--CFLAGS="-g -Wall -D_REENTRANT -D_GNU_SOURCE -fPIC"
-+CFLAGS="-g -Wall -D_REENTRANT -D_GNU_SOURCE -fPIC -I/usr/local/include -L/usr/local/lib"
- 
--CHANNEL_H=/usr/include/asterisk/channel.h
-+CHANNEL_H=/usr/local/include/asterisk/channel.h
++if [ ${OSARCH} = "FreeBSD" ]; then
++  INCDIR=/usr/local/include/asterisk
++  CFLAGS="${CFLAGS} -I/usr/local/include -L/usr/local/lib"
++fi
++
+ CHANNEL_H=${INCDIR}/channel.h
  if [ "`grep 'struct ast_callerid cid' ${CHANNEL_H}`" != "" ]; then
      CFLAGS="${CFLAGS} -DCHANNEL_HAS_CID"
- fi
- 
--CONFIG_H=/usr/include/asterisk/config.h
-+CONFIG_H=/usr/local/include/asterisk/config.h
- if [ "`grep 'ast_config_load' ${CONFIG_H}`" != "" ]; then
-     CFLAGS="${CFLAGS} -DNEW_CONFIG"
- fi
