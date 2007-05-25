65a88c7afe5f503ec32b75ef878ace2d57263cd8
dinoex <dinoex@FreeBSD.org>
2007-05-25 04:39:43 +0000

    - fix missing includes

---
+++ devel/projectmanager/files/patch-ProjectCreator.m
@@ -0,0 +1,10 @@
+--- ProjectCreator.m.orig	Sun Jul  9 16:11:43 2006
++++ ProjectCreator.m	Mon Apr 30 10:53:41 2007
+@@ -27,6 +27,7 @@
+ #import <Foundation/NSUserDefaults.h>
+ #import <Foundation/NSNotification.h>
+ #import <Foundation/NSError.h>
++#import <Foundation/NSProcessInfo.h>
+ 
+ #import <AppKit/NSOpenPanel.h>
+ #import <AppKit/NSView.h>
---
+++ devel/projectmanager/files/patch-ProjectDocument.m
@@ -0,0 +1,10 @@
+--- ProjectDocument.m.orig	Sun Jul  9 16:11:43 2006
++++ ProjectDocument.m	Mon Apr 30 10:54:26 2007
+@@ -28,6 +28,7 @@
+ #import <Foundation/NSDictionary.h>
+ #import <Foundation/NSUserDefaults.h>
+ #import <Foundation/NSProcessInfo.h>
++#import <Foundation/NSError.h>
+ 
+ #import <AppKit/NSWorkspace.h>
+ #import <AppKit/NSDocumentController.h>
