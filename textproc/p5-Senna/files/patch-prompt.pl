9bcc6f9ecf2517d74d951e8e3254401069c5f3e5
kuriyama <kuriyama@FreeBSD.org>
2007-05-26 12:30:32 +0000

    - Upgrade to 0.51.

--- textproc/p5-Senna/files/patch-prompt.pl
+++ textproc/p5-Senna/files/patch-prompt.pl
@@ -1,11 +1,11 @@
---- tools/prompt.pl.orig	Wed Aug 30 14:26:45 2006
-+++ tools/prompt.pl	Wed Aug 30 14:27:13 2006
-@@ -16,7 +16,7 @@
+--- tools/prompt.pl.orig	Sat Mar 17 01:47:32 2007
++++ tools/prompt.pl	Tue May  1 20:41:18 2007
+@@ -17,7 +17,7 @@
      "\n",
      "Proceed ? [n] ";
  
--my $tmp = <STDIN>;
-+my $tmp = $interactive ? <STDIN> : "yes";
+-$tmp = <STDIN>;
++$tmp = $interactive ? <STDIN> : "yes";
  chomp $tmp;
  if ($tmp !~ /^y(?:es)?$/) {
      exit 1;
