98f2cd60446be053e6add870f24dfde0c77103c3
krion <krion@FreeBSD.org>
2007-05-26 20:11:11 +0000

    Fix build with gcc4.2

--- net-im/centericq/files/patch-src_icqdialogs.cc
+++ net-im/centericq/files/patch-src_icqdialogs.cc
@@ -1,5 +1,8 @@
---- src/icqdialogs.cc	Fri Aug 18 04:42:04 2006
-+++ src/icqdialogs.cc.new	Fri Aug 18 04:44:07 2006
+
+$FreeBSD$
+
+--- src/icqdialogs.cc.orig
++++ src/icqdialogs.cc
 @@ -210,17 +210,17 @@
      switch(subj) {
  	case fsuser:
@@ -21,3 +24,48 @@
  	    break;
      }
  
+@@ -894,7 +894,7 @@
+     i = m.open();
+     m.close();
+ 
+-    if(i) f = (unsigned short) ((int) m.getref(i-1));
++    if(i) f = (unsigned short) ((intptr_t) m.getref(i-1));
+ }
+ 
+ void icqface::selectlanguage(unsigned short &f) {
+@@ -946,7 +946,7 @@
+     int i = m.open();
+     m.close();
+ 
+-    if(i) f = (imgender) ((int) m.getref(i-1));
++    if(i) f = (imgender) ((intptr_t) m.getref(i-1));
+ }
+ 
+ void icqface::selectagerange(ICQ2000::AgeRange &r) {
+@@ -967,7 +967,7 @@
+     int i = m.open();
+     m.close();
+ 
+-    if(i) r = (ICQ2000::AgeRange) ((int) m.getref(i-1));
++    if(i) r = (ICQ2000::AgeRange) ((intptr_t) m.getref(i-1));
+ }
+ 
+ bool icqface::edit(string &txt, const string &header) {
+@@ -1256,7 +1256,7 @@
+ 
+ 	void *p;
+ 	finished = !db.open(n, b, &p);
+-	i = (int) p;
++	i = (intptr_t) p;
+ 
+ 	if(!finished)
+ 	switch(b) {
+@@ -1750,7 +1750,7 @@
+ 	    it = transfers.end();
+ 
+ 	    if(!db.gettree()->isnode(db.gettree()->getid(np-1)))
+-		it = transfers.begin() + (int) db.getmenu()->getref(n-1);
++		it = transfers.begin() + (intptr_t) db.getmenu()->getref(n-1);
+ 
+ 	    switch(b) {
+ 		case 0:
--- net-im/centericq/files/patch-src_icqface.cc
+++ net-im/centericq/files/patch-src_icqface.cc
@@ -1,5 +1,53 @@
---- src/icqface.cc.orig	Wed Aug  9 10:43:40 2006
-+++ src/icqface.cc	Wed Aug  9 10:44:14 2006
+
+$FreeBSD$
+
+--- src/icqface.cc.orig
++++ src/icqface.cc
+@@ -401,7 +401,7 @@
+ 
+     m.scale();
+     m.idle = &menuidle;
+-    i = (int) m.getref(m.open()-1);
++    i = (intptr_t) m.getref(m.open()-1);
+     m.close();
+ 
+     if(i) lastr = i;
+@@ -450,7 +450,7 @@
+ 
+     if(i) {
+ 	lastitem = i-1;
+-	r = (int) m.getref(lastitem);
++	r = (intptr_t) m.getref(lastitem);
+     }
+ 
+     return r;
+@@ -513,7 +513,7 @@
+ 	    g = 0;
+ 	}
+ 	
+-	if((unsigned int) c < 100) c = 0;
++	if((intptr_t) c < 100) c = 0;
+ 
+ 	if(i) {
+ 	    switch(action = extk) {
+@@ -1484,7 +1484,7 @@
+ 	m.scale();
+ 
+ 	i = m.open();
+-	choice = (int) m.getref(i-1);
++	choice = (intptr_t) m.getref(i-1);
+ 
+ 	m.close();
+     }
+@@ -1539,7 +1539,7 @@
+ 	m.close();
+ 
+ 	if(r = i) {
+-	    st = (imstatus) ((int) m.getref(i-1));
++	    st = (imstatus) ((intptr_t) m.getref(i-1));
+ 	}
+     }
+ 
 @@ -2003,7 +2003,7 @@
  	    mvaddchnstr(i-1, sizeWArea.x1+1, logline, sizeWArea.x2-sizeWArea.x1);
  	}
@@ -9,3 +57,12 @@
  
  	if(text.size() > sizeWArea.x2-sizeWArea.x1-2) text.resize(sizeWArea.x2-sizeWArea.x1-2);
  	mvhline(LINES-3, sizeWArea.x1+2, ' ', sizeWArea.x2-sizeWArea.x1-2);
+@@ -2132,7 +2132,7 @@
+ 			    c = (icqcontact *) cm->getref(i);
+ 			}
+ 
+-			if((unsigned int) c > 100) {
++			if((intptr_t) c > 100) {
+ 			    string current = c->getdispnick();
+ 			    len = current.size();
+ 			    if(len > nick.size()) len = nick.size();
