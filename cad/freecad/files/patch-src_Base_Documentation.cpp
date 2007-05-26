15cbed5e11c43b326b5d7bd7ddb68861ab3f752d
Thierry Thomas <thierry@FreeBSD.org>
2007-05-26 19:26:10 +0000

    FreeCAD is an OpenSource CAD/CAE, based on OpenCasCade, QT and Python.
    It features some key concepts like macro recording, workbenches, ability to run
    as a server and dynamically loadable application extensions and it is designed
    to be platform independent.
    
    Warning: FreeCAD is still in ALPHA state and not in shape for end user usage!
    
    <http://juergen-riegel.net/FreeCAD/Docu/>
    
    Suggested by:	Pedro F. Giffuni <giffunip (at) yahoo.com>

---
+++ cad/freecad/files/patch-src_Base_Documentation.cpp
@@ -0,0 +1,16 @@
+--- ./src/Base/Documentation.cpp.orig	Sat Feb 24 11:44:54 2007
++++ ./src/Base/Documentation.cpp	Sun Apr 22 18:46:43 2007
+@@ -44,6 +44,13 @@
+ #		undef  _POSIX_C_SOURCE
+ #	endif // (re-)defined in pyconfig.h
+ #	include <Python.h>
++#	undef isspace
++#	undef isupper
++#	undef islower
++#	undef isalpha
++#	undef isalnum
++#	undef toupper
++#	undef tolower
+ #	include <iostream>
+ #	include <fstream>
+ #	include <fcntl.h>
