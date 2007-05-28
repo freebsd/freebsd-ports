188b5626b415f8e21cd169cd0fe26369d54ce944
dinoex <dinoex@FreeBSD.org>
2007-05-28 09:06:36 +0000

    - fix build with gcc 4.2

---
+++ net/crescendo/files/patch-tellbox.c
@@ -0,0 +1,10 @@
+--- src/tellbox.c.orig	Thu Nov 11 05:40:04 1999
++++ src/tellbox.c	Mon May 28 10:53:50 2007
+@@ -11,7 +11,6 @@
+ #include "tellbox.h"
+ 
+ GtkWidget *boxMsg;
+-GtkWidget *txtOutput;
+ GtkWidget *cmbStupid;
+ 	GtkWidget *txtTarget;
+ GtkWidget *menu, *option_menu;
