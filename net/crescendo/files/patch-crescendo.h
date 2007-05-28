65bf93ecda9dfe38c6a50386c20d5882235071c2
Dirk Meyer <dinoex@FreeBSD.org>
2007-05-28 09:06:36 +0000

    - fix build with gcc 4.2

---
+++ net/crescendo/files/patch-crescendo.h
@@ -0,0 +1,10 @@
+--- src/crescendo.h.orig	Fri Nov 12 08:46:53 1999
++++ src/crescendo.h	Mon May 28 10:51:07 2007
+@@ -18,7 +18,6 @@
+ extern GtkWidget *vpane;
+ extern GtkWidget *vscrollbar;
+ extern GtkWidget *entInput;
+-extern GtkWidget *winNotepad;
+ extern GtkWidget *WinWorld;
+ 
+ /* testing the CrescPref struct */
