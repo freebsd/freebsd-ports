c33a4eedb33e8a47f1eb7db983f40c6cc5384b61
Martin Wilke <miwi@FreeBSD.org>
2007-05-29 15:51:32 +0000

    - Fix build error with gcc4.2 [1] - Fix panic on Head [2]
    PR:		112387 / 112989
    Submitted by:	Artem Naluzhny<tut@nhamon.com.ua> [1]
                    Anish Mistry <amistry@am-productions.biz> (maintainer) [2]

--- sysutils/fusefs-kmod/files/patch-fuse_module_fuse.h
+++ sysutils/fusefs-kmod/files/patch-fuse_module_fuse.h
@@ -1,15 +1,23 @@
---- fuse_module/fuse.h.orig	Fri Feb 10 17:31:53 2006
-+++ fuse_module/fuse.h	Sun Jul 16 14:43:20 2006
-@@ -8,7 +8,7 @@
+--- fuse_module/fuse.h.orig	Thu May  3 13:26:25 2007
++++ fuse_module/fuse.h	Thu May  3 13:26:03 2007
+@@ -8,7 +8,15 @@
  
  #ifndef USE_OLD_CLONEHANDLER_API
  #if __FreeBSD_version < 600034 || ( __FreeBSD_version >= 700000 && __FreeBSD_version < 700002 )
 -#define USE_OLD_CLONEHANDLER_API 1
 +#define USE_OLD_CLONEHANDLER_API
++#endif
++#endif
++
++#ifndef NEW_VNODES_ADJUSTED_MANUALLY
++#if __FreeBSD_version >= 700034
++#define NEW_VNODES_ADJUSTED_MANUALLY 1
++#else
++#define NEW_VNODES_ADJUSTED_MANUALLY 0
  #endif
  #endif
  
-@@ -19,10 +19,10 @@
+@@ -19,10 +27,10 @@
   */
  #if FUSE_KERNELABI_GEQ(7, 3)
  #ifndef FUSE_HAS_ACCESS
@@ -22,7 +30,7 @@
  #endif
  #endif
  
-@@ -75,7 +75,7 @@
+@@ -75,7 +83,7 @@
  	size_t len;     /* To keep track of size of the data pushed into base, =< len, of course */
  };
  
@@ -31,7 +39,7 @@
  #ifndef FUSE_MAX_STORED_FREE_TICKETS
  #define FUSE_MAX_STORED_FREE_TICKETS 0
  #endif
-@@ -251,7 +251,7 @@
+@@ -251,7 +259,7 @@
  	int flags;
  	LIST_HEAD(, fuse_filehandle) fh_head;
  	int fh_counter;
@@ -40,7 +48,7 @@
  	struct componentname *germcnp;
  #endif
  };
-@@ -260,6 +260,9 @@
+@@ -260,6 +268,9 @@
  
  /* Debug related stuff */
  
@@ -50,7 +58,7 @@
  #if DEBUGTOLOG
  #define dprintf(args ...)  log(LOG_DEBUG, args)
  #else
-@@ -267,6 +270,9 @@
+@@ -267,6 +278,9 @@
  #endif
  
  #define DEBLABEL "[fuse-debug] "
@@ -60,7 +68,7 @@
  #if     _DEBUG
  #ifndef _DEBUG2G
  #define _DEBUG2G 1
-@@ -280,6 +286,9 @@
+@@ -280,6 +294,9 @@
  #define DEBUG(args ...)
  #endif
  
@@ -70,7 +78,7 @@
  #if     _DEBUG2G
  #ifndef _DEBUG3G
  #define _DEBUG3G 1
-@@ -290,6 +299,9 @@
+@@ -290,6 +307,9 @@
  #define DEBUG2G(args ...)
  #endif
  
@@ -80,7 +88,7 @@
  #if     _DEBUG3G
  #define DEBUG3G(args, ...)							\
  	printf(DEBLABEL "%s:%d: " args, __func__, __LINE__, ## __VA_ARGS__)
-@@ -297,13 +309,19 @@
+@@ -297,13 +317,19 @@
  #define DEBUG3G(args ...)
  #endif
  
@@ -101,7 +109,7 @@
  #define fuprintf(args...) \
  	uprintf("[kern] " args)
  #else
-@@ -319,7 +337,7 @@
+@@ -319,7 +345,7 @@
  void fprettyprint(struct fuse_iov *fiov, size_t dlen);
  #endif
  
