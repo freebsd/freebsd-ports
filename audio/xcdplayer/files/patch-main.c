19a1a1e2e687755e58592bd4306eaa24fc46a38d
Tilman Keskinoz <arved@FreeBSD.org>
2007-05-30 17:22:06 +0000

    Fix build with gcc4

--- audio/xcdplayer/files/patch-main.c
+++ audio/xcdplayer/files/patch-main.c
@@ -1,20 +1,24 @@
-
-$FreeBSD$
-
 --- main.c.orig	Tue Jan 12 19:59:36 1993
-+++ main.c	Mon Aug 25 00:38:35 2003
-@@ -23,7 +23,9 @@
++++ main.c	Wed May 30 19:19:53 2007
+@@ -23,12 +23,15 @@
  
  Widget		top_form;
  
 -void
 +extern AppData app_data;
 +
++static void	chk_debug();
++
 +int
  main(argc, argv)
  	int		argc;
  	char		**argv;
-@@ -66,7 +68,7 @@
+ {
+-	static void	chk_debug();
+ 
+ 	chk_debug(argc, argv);
+ 
+@@ -66,7 +69,7 @@
  	/* ugly hack */
  	for (i = 1; i < argc; i++) {
  		if (strcmp(argv[i], "-debug") == 0) {
