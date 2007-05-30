c0c54fbe75137011056cc2e71f5105f4818972da
Tilman Keskinoz <arved@FreeBSD.org>
2007-05-30 20:17:19 +0000

    Fix build with gcc4

---
+++ misc/videotext/files/patch-hotlist.h
@@ -0,0 +1,10 @@
+--- hotlist.h.orig	Wed May 30 22:15:43 2007
++++ hotlist.h	Wed May 30 22:15:54 2007
+@@ -31,7 +31,6 @@
+ #define HL_SEARCHREQ   (1 << 15)
+ 
+ 
+-extern int hotlist_count;
+ 
+ 
+ void hotlist_init(void);
