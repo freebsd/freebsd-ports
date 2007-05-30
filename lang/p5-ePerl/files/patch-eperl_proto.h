4aaedc83cd52bb876d3f3de5b24a01409aee959d
Tilman Keskinoz <arved@FreeBSD.org>
2007-05-30 17:15:47 +0000

    Fix build with gcc4

--- lang/p5-ePerl/files/patch-eperl_proto.h
+++ lang/p5-ePerl/files/patch-eperl_proto.h
@@ -1,6 +1,14 @@
---- eperl_proto.h.orig	Thu Nov 21 21:26:44 2002
-+++ eperl_proto.h	Thu Nov 21 21:26:50 2002
-@@ -79,7 +79,7 @@
+--- eperl_proto.h.orig	Fri Jul 10 09:52:24 1998
++++ eperl_proto.h	Wed May 30 19:13:42 2007
+@@ -36,6 +36,7 @@
+ #define EPERL_PROTO_H 1
+ 
+ /*_BEGIN_PROTO_*/
++#include "eperl_getopt.h"
+ 
+ /* eperl_main.c */
+ extern int mode;
+@@ -79,7 +80,7 @@
  extern char *ePerl_Efwrite(char *cpBuf, int nBuf, int cNum, char *cpOut);
  extern char *ePerl_Cfwrite(char *cpBuf, int nBuf, int cNum, char *cpOut);
  extern char *strnchr(char *buf, char chr, int n);
