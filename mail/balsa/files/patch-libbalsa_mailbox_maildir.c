269bbbf524b47bd7dec7e9312d1ff67fef82adab
marcus <marcus@FreeBSD.org>
2007-05-31 05:52:02 +0000

    Update to 2.3.16.

--- mail/balsa/files/patch-libbalsa_mailbox_maildir.c
+++ mail/balsa/files/patch-libbalsa_mailbox_maildir.c
@@ -1,5 +1,5 @@
---- libbalsa/mailbox_maildir.c.orig	Tue Dec 27 04:44:40 2005
-+++ libbalsa/mailbox_maildir.c	Thu Dec 29 01:52:57 2005
+--- libbalsa/mailbox_maildir.c.orig	Sun May  6 13:57:53 2007
++++ libbalsa/mailbox_maildir.c	Thu May 31 01:06:37 2007
 @@ -25,6 +25,8 @@
  #define _XOPEN_SOURCE          500
  #define _XOPEN_SOURCE_EXTENDED 1
@@ -9,25 +9,25 @@
  #include <unistd.h>
  #include <fcntl.h>
  #include <errno.h>
-@@ -50,7 +52,7 @@ struct message_info {
+@@ -49,7 +51,7 @@ struct message_info {
      /* The message's order when parsing; needed for saving the message
       * tree in a form that will match the msgnos when the mailbox is
       * reopened. */
 -    guint fileno;
 +    guint mi_fileno;
  };
- #define REAL_FLAGS(flags) (flags & LIBBALSA_MESSAGE_FLAGS_REAL)
+ #define REAL_FLAGS(flags) ((flags) & LIBBALSA_MESSAGE_FLAGS_REAL)
  #define FLAGS_REALLY_DIFFER(orig_flags, flags) \
-@@ -194,7 +196,7 @@ libbalsa_mailbox_maildir_class_init(LibB
+@@ -174,7 +176,7 @@ libbalsa_mailbox_maildir_class_init(LibB
+     libbalsa_mailbox_local_class->check_files  = lbm_maildir_check_files;
      libbalsa_mailbox_local_class->set_path     = lbm_maildir_set_path;
      libbalsa_mailbox_local_class->remove_files = lbm_maildir_remove_files;
-     libbalsa_mailbox_local_class->load_message = lbm_maildir_load_message;
 -    libbalsa_mailbox_local_class->fileno       = lbm_maildir_fileno;
 +    libbalsa_mailbox_local_class->sti_fileno       = lbm_maildir_fileno;
+     libbalsa_mailbox_local_class->get_info     = lbm_maildir_get_info;
  }
  
- static void
-@@ -408,7 +410,7 @@ static LibBalsaMessageFlag parse_filenam
+@@ -389,7 +391,7 @@ static LibBalsaMessageFlag parse_filenam
  }
  
  static void lbm_maildir_parse(LibBalsaMailboxMaildir * mdir,
@@ -36,10 +36,10 @@
  {
      gchar *path;
      GDir *dir;
-@@ -463,12 +465,12 @@ static void lbm_maildir_parse(LibBalsaMa
+@@ -442,12 +444,12 @@ static void lbm_maildir_parse(LibBalsaMa
  	    msg_info->key=key;
  	    msg_info->filename=g_strdup(filename);
- 	    msg_info->flags = msg_info->orig_flags = flags;
+ 	    msg_info->local_info.flags = msg_info->orig_flags = flags;
 -	    msg_info->fileno = 0;
 +	    msg_info->mi_fileno = 0;
  	}
@@ -52,7 +52,7 @@
      }
      g_dir_close(dir);
  }
-@@ -476,16 +478,16 @@ static void lbm_maildir_parse(LibBalsaMa
+@@ -455,16 +457,16 @@ static void lbm_maildir_parse(LibBalsaMa
  static void
  lbm_maildir_parse_subdirs(LibBalsaMailboxMaildir * mdir)
  {
@@ -73,7 +73,7 @@
  }
  
  static gboolean
-@@ -927,7 +929,7 @@ lbm_maildir_fileno(LibBalsaMailboxLocal 
+@@ -864,7 +866,7 @@ lbm_maildir_fileno(LibBalsaMailboxLocal 
      msg_info =
          message_info_from_msgno((LibBalsaMailboxMaildir *) local, msgno);
  
@@ -81,4 +81,4 @@
 +    return msg_info->mi_fileno;
  }
  
- /* Called with mailbox locked. */
+ static LibBalsaMailboxLocalMessageInfo *
