ca5701d9157a70e5e088d852a7783783b81dafc1
ale <ale@FreeBSD.org>
2007-05-31 06:19:26 +0000

    Update to 1.07 release.
    Submitted by:	Oliver <olli@secnetix.de>
    Approved by:	maintainer

--- sysutils/cpdup/files/patch-cpdup.c
+++ sysutils/cpdup/files/patch-cpdup.c
@@ -1,17 +1,17 @@
 
 $FreeBSD$
 
---- cpdup.c.orig
-+++ cpdup.c
-@@ -321,6 +321,11 @@
+--- cpdup.c.orig	Thu Sep 21 06:09:28 2006
++++ cpdup.c	Wed May 30 17:27:55 2007
+@@ -463,6 +463,11 @@
  
-     if (lstat(spath, &st1) != 0)
+     if (hc_lstat(&SrcHost, spath, &st1) != 0)
  	return(0);
 +#ifdef SF_SNAPSHOT
-+    /* skip snapshot files: we cannot handle files larger than file system */
++    /* skip snapshot files because they're sparse and _huge_ */
 +    if (st1.st_flags & SF_SNAPSHOT)
-+	 return(0);
++	return(0);
 +#endif
      st2.st_mode = 0;	/* in case lstat fails */
      st2.st_flags = 0;	/* in case lstat fails */
-     if (dpath && lstat(dpath, &st2) == 0)
+     if (dpath && hc_lstat(&DstHost, dpath, &st2) == 0)
