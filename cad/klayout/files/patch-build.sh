e4464bbf1d3c5cf69591dd6ca3b120de9b65ccae
hrs <hrs@FreeBSD.org>
2007-06-02 05:47:57 +0000

    Add a patch for build failure when $DISPLAY is not set.
    Spotted by:	pointyhat

--- cad/klayout/files/patch-build.sh
+++ cad/klayout/files/patch-build.sh
@@ -1,5 +1,5 @@
---- build.sh.orig	Sat Apr 28 23:13:29 2007
-+++ build.sh	Sat Apr 28 23:13:40 2007
+--- build.sh.orig	Tue Mar  6 06:58:29 2007
++++ build.sh	Sat Jun  2 13:57:50 2007
 @@ -61,12 +61,12 @@
    exit 1
  fi
@@ -15,3 +15,20 @@
    echo "*** ERROR: unable to locate Qt UIC in $QTDIR/bin"
    exit 1
  fi
+@@ -107,11 +107,11 @@
+ 
+ echo "Build done."
+ echo "Final binary is ready in ./$BIN/$EXEC_NAME."
+-echo "To run it, you may need to add the following to your LD_LIBRARY_PATH:"
+-echo "$QTDIR/lib"
++#echo "To run it, you may need to add the following to your LD_LIBRARY_PATH:"
++#echo "$QTDIR/lib"
+ 
+-LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
+-./$BIN/$EXEC_NAME -v
+-./$BIN/$EXEC_NAME -h
++#LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
++#./$BIN/$EXEC_NAME -v
++#./$BIN/$EXEC_NAME -h
+ 
+ 
