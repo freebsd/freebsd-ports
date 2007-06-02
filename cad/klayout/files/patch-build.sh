5328ce0f2868cefc4b9522c050819248e0f33820
Hiroki Sato <hrs@FreeBSD.org>
2007-06-02 08:36:21 +0000

    Update to 0.10.  Changes include:
    - OASIS files can be read as well now. The reader automatically
      determines the kind of file. OASIS layer names are supported. Due to
      the complexity of the OASIS specification, or more precisely the
      effort required to test a reader for meeting the specification, OASIS
      support currently is regarded to be in "beta" state.
    
    - Layers can now be organized hierarchically. Layers can be grouped
      which allows to control visibility and other properties for the group
      as a whole. Layers can be removed from the layer view list or new
      layer views can be created.
    
    - The layout can be transformed (rotated, shifted, mirrored) now. This
      transformation is specified in the layer panel as a part of the layer
      source specification. An optional transformation can be applied per
      layer or layer group. This way for example, different layouts can be
      aligned over each other.
    
    - Shape properties now are supported. Each shape may carry optional
      properties that are read from GDS or OASIS files. A property selector
      can be applied per layer view, so that a layer only shows these shapes
      that match the given property selection.
    
    - As an experimental feature, the inverse layout tree can be
      visualised by allowing the minimum hierarchy level to go into the
      negative range. The effect of this is, that a cell is shown in the
      context of all of it's direct parents, if the first hierarchy level is
      set to -1. If set to -2, the cell is shown in the context of all of
      it's parents and grandparents and so on. Since there is no particular
      optimiziation for this feature, performance may be poor, if a cell
      must be painted in a huge number of contexts. In addition, the context
      displayed does not participate in selection or ruler snapping.

--- cad/klayout/files/patch-build.sh
+++ cad/klayout/files/patch-build.sh
@@ -1,6 +1,6 @@
---- build.sh.orig	Tue Mar  6 06:58:29 2007
-+++ build.sh	Sat Jun  2 13:57:50 2007
-@@ -61,12 +61,12 @@
+--- build.sh.orig	Tue May  8 04:41:18 2007
++++ build.sh	Sat Jun  2 16:43:04 2007
+@@ -77,12 +77,12 @@
    exit 1
  fi
  
@@ -15,20 +15,21 @@
    echo "*** ERROR: unable to locate Qt UIC in $QTDIR/bin"
    exit 1
  fi
-@@ -107,11 +107,11 @@
+@@ -153,12 +153,12 @@
+   echo "Build done."
+   echo ""
+   echo "Final binary is ready in $BIN/$EXEC_NAME."
+-  echo "To run it, you may need to add the following to your LD_LIBRARY_PATH:"
+-  echo "$QTDIR/lib"
++  #echo "To run it, you may need to add the following to your LD_LIBRARY_PATH:"
++  #echo "$QTDIR/lib"
+   
+-  LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
+-  $BIN/$EXEC_NAME -v
+-  $BIN/$EXEC_NAME -h
++  #LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
++  #$BIN/$EXEC_NAME -v
++  #$BIN/$EXEC_NAME -h
  
- echo "Build done."
- echo "Final binary is ready in ./$BIN/$EXEC_NAME."
--echo "To run it, you may need to add the following to your LD_LIBRARY_PATH:"
--echo "$QTDIR/lib"
-+#echo "To run it, you may need to add the following to your LD_LIBRARY_PATH:"
-+#echo "$QTDIR/lib"
- 
--LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
--./$BIN/$EXEC_NAME -v
--./$BIN/$EXEC_NAME -h
-+#LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
-+#./$BIN/$EXEC_NAME -v
-+#./$BIN/$EXEC_NAME -h
- 
+ else 
  
