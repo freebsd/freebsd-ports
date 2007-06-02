a40ce7783f5acbbff6be608f4eb50f0974d80b11
Greg Lewis <glewis@FreeBSD.org>
2007-06-02 21:18:27 +0000

    . Make it so we include Xm/XmIm.h rather than defining our own prototype.   Fixes a build error due to the prototype being defined not matching   the actual function.
    Pointed out by:	pointyhat (via kris)

---
+++ java/jdk13/files/patch-j2sdk1.3.1-awt-awt_motif21.c
@@ -0,0 +1,13 @@
+$FreeBSD$
+
+--- ../src/solaris/native/sun/awt/awt_motif21.c	14 Jan 2005 10:35:32 -0000	1.6
++++ ../src/solaris/native/sun/awt/awt_motif21.c	8 May 2007 22:05:37 -0000
+@@ -15,7 +15,7 @@
+ #include "awt_p.h"
+ #include "awt_Component.h"
+ 
+-#if (XmVersion == 2001)
++#if (XmVersion >= 2001)
+ #include <Xm/XmIm.h>
+ #else
+ #define XmPER_SHELL 0
