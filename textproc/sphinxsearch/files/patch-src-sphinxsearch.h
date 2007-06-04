61a5ff520a2230bbe3d741ee0ab66840ebf1a0a8
Martin Wilke <miwi@FreeBSD.org>
2007-06-04 07:24:48 +0000

    - Fix few mistakes in rc.d script
    PR:		113135
    Submitted by:	Matthew Seaman <m.seaman@infracaninophile.co.uk> (maintainer)

--- textproc/sphinxsearch/files/patch-src-sphinxsearch.h
+++ textproc/sphinxsearch/files/patch-src-sphinxsearch.h
@@ -1,11 +1,13 @@
---- src/sphinxstd.h.orig	Sun Apr 15 20:16:11 2007
-+++ src/sphinxstd.h	Sun Apr 15 20:17:25 2007
-@@ -57,7 +57,7 @@
+--- src/sphinxstd.h.orig	Tue May 29 19:50:47 2007
++++ src/sphinxstd.h	Tue May 29 19:52:23 2007
+@@ -57,9 +57,7 @@
  #error "Internal 64-bit integer macros already defined."
  #endif
  
 -#if __STDC_VERSION__>=199901L || __STDC_VERSION>=199901L || defined(_STDINT_H) || defined(_STDINT_H_)
-+#if __STDC_VERSION__>=199901L || __STDC_VERSION>=199901L || defined(_STDINT_H) || defined(_STDINT_H_) || defined(_SYS_STDINT_H_)
- #include <stdint.h>
- #else // no stdint.h
+-#include <stdint.h>
+-#else // no stdint.h
++#if !HAVE_STDINT_H
  
+ #if defined(_MSC_VER)
+ typedef __int64 int64_t;
