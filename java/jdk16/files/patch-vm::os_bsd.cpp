01790521d1f58b3814db40f9bcc340faf54e9c9f
Greg Lewis <glewis@FreeBSD.org>
2007-06-09 05:14:56 +0000

    . Update to patchset 5, based on the 1.5.0_11 JRL source code.

--- java/jdk16/files/patch-vm::os_bsd.cpp
+++ java/jdk16/files/patch-vm::os_bsd.cpp
@@ -1,13 +1,13 @@
 $FreeBSD$
 
---- ../../hotspot/src/os/bsd/vm/os_bsd.cpp.orig	Tue Jan 27 17:54:28 2004
-+++ ../../hotspot/src/os/bsd/vm/os_bsd.cpp	Tue Jan 27 17:55:21 2004
-@@ -333,7 +333,7 @@
- #define malloc(n) (char*)NEW_C_HEAP_ARRAY(char, (n))
+--- ../../hotspot/src/os/bsd/vm/os_bsd.cpp	Sun Jun  3 18:46:31 2007
++++ ../../hotspot/src/os/bsd/vm/os_bsd.cpp.orig	Sun Jun  3 18:47:28 2007
+@@ -499,7 +499,7 @@
  #define getenv(n) ::getenv(n)
  
+ #ifndef DEFAULT_LD_LIBRARY_PATH
 -#define DEFAULT_LD_LIBRARY_PATH "/usr/lib" /* See ld.so.1(1) */
 +#define DEFAULT_LD_LIBRARY_PATH "/usr/lib:%%LOCALBASE%%/lib" /* See ld.so.1(1) */
+ #endif
  #define EXTENSIONS_DIR "/lib/ext"
  #define ENDORSED_DIR "/lib/endorsed"
- 
