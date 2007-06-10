f7e0c13d6b672da7e28046815297ee68b289e710
sat <sat@FreeBSD.org>
2007-06-10 19:07:43 +0000

    Add port deskutils/py-vobject:
    Vobject parses iCalendar and vCard files into Python data structures,
    decoding the relevant encodings. Also serializes vobject data structures
    to iCalendar, vCard, or (expirementally) hCalendar unicode strings.
    
    WWW: http://vobject.skyhouseconsulting.com/
    Author: Jeffrey Harris <jeffrey@osafoundation.org>

---
+++ deskutils/py-vobject/files/patch-setup.py
@@ -0,0 +1,16 @@
+--- ./setup.py.orig	Thu Feb  8 05:43:56 2007
++++ ./setup.py	Sun Jun 10 22:36:12 2007
+@@ -14,12 +14,9 @@
+ 
+ """
+ 
+-from ez_setup import use_setuptools
+-use_setuptools()
+ 
+-from setuptools import setup, find_packages
+ 
+-#from distutils.core import setup
++from distutils.core import setup
+ 
+ # Metadata
+ PACKAGE_NAME = "vobject"
