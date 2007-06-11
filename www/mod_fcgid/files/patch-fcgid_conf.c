baf057091ff9f5b2e62c364c74f610d99d8e37da
mm <mm@FreeBSD.org>
2007-06-11 12:07:54 +0000

    - update to 2.1
    PR:		ports/113486
    Submitted by:	Josh Tolbert <hemi@puresimplicity.net> (maintainer)
    Approved by:	garga (mentor)

--- www/mod_fcgid/files/patch-fcgid_conf.c
+++ www/mod_fcgid/files/patch-fcgid_conf.c
@@ -5,7 +5,9 @@
  #define DEFAULT_ZOMBIE_SCAN_INTERVAL 3
  #define DEFAULT_PROC_LIFETIME (60*60)
 -#define DEFAULT_SOCKET_PREFIX "logs/fcgidsock"
+-#define DEFAULT_SHM_PATH "logs/fcgid_shm"
 +#define DEFAULT_SOCKET_PREFIX "/var/run/fcgidsock"
++#define DEFAULT_SHM_PATH "/var/run/fcgid_shm"
  #define DEFAULT_SPAWNSOCRE_UPLIMIT 10
  #define DEFAULT_SPAWN_SCORE 1
  #define DEFAULT_TERMINATION_SCORE 2
