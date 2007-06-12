5f0ddc845f22f9b3ac62526179041c70c76907ad
mezz <mezz@FreeBSD.org>
2007-06-12 07:11:07 +0000

    Update to 0.5.1 and took a patch from rblibtorrent/files/patch-socket_ops.hpp.

--- net-p2p/deluge/files/patch-setup.py
+++ net-p2p/deluge/files/patch-setup.py
@@ -1,27 +1,35 @@
---- setup.py.orig	Tue Mar  6 21:43:47 2007
-+++ setup.py	Tue Mar  6 21:45:44 2007
-@@ -51,7 +51,7 @@
- #
+--- setup.py.orig	Sat Jun  9 12:16:40 2007
++++ setup.py	Sat Jun  9 12:19:31 2007
+@@ -100,7 +100,7 @@
+ 	else:
+ 		boosttype = 'mt'
+ 	removals = ['-g', '-DNDEBUG', '-O2', '-Wstrict-prototypes']
+-        additions = ['-DNDEBUG', '-O2']
++        additions = ['-DNDEBUG']
  
- removals = ['-g', '-DNDEBUG', '-O2', '-Wstrict-prototypes']
--additions = ['-DNDEBUG', '-O2']
-+additions = ['-DNDEBUG']
+ 	if python_version == '2.5':
+ 		cv_opt = sysconfig.get_config_vars()["CFLAGS"]
+@@ -131,19 +131,21 @@
+ # Ubuntu possible.
+ if boosttype == "nomt":
+ 	librariestype = ['boost_filesystem', 'boost_date_time',
+-			'boost_thread', 'z', 'pthread', 'ssl']
++			'boost_thread', 'z', 'ssl']
+ 	print 'Libraries nomt' 
+ elif boosttype == "mt":
+ 	librariestype = ['boost_filesystem-mt', 'boost_date_time-mt',
+-			'boost_thread-mt', 'z', 'pthread', 'ssl']
++			'boost_thread-mt', 'z', 'ssl']
+ 	print 'Libraries mt'
  
- if pythonVersion == '2.5':
- 	cv_opt = sysconfig.get_config_vars()["CFLAGS"]
-@@ -85,12 +85,14 @@
  deluge_core = Extension('deluge_core',
                      include_dirs = ['./libtorrent', './libtorrent/include', 
                      			'./libtorrent/include/libtorrent', 
--                    			'/usr/include/python' + pythonVersion],
+-                    			'/usr/include/python' + python_version],
 +                    			'%%LOCALBASE%%/include'],
 +                    library_dirs = ['%%LOCALBASE%%/lib'],
-                     libraries = ['boost_filesystem', 'boost_date_time',
- 					'boost_program_options', 'boost_regex',
- 					'boost_serialization', 'boost_thread', 
--					'z', 'pthread'],
-+					'z'],
-                     extra_compile_args = ["-Wno-missing-braces"],
+ 		    libraries = librariestype,
+                     extra_compile_args = EXTRA_COMPILE_ARGS,
 +                    extra_link_args = ['%%PTHREAD_LIBS%%'],
                      sources = ['src/deluge_core.cpp',
  					'libtorrent/src/alert.cpp',
