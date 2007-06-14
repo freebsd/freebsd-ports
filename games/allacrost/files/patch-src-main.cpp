8d0b1d1c57a7836ca504297baa673b58bfa27be4
Pav Lucistnik <pav@FreeBSD.org>
2007-06-14 20:26:58 +0000

    - Update to 0.2.0
    PR:		ports/113571
    Submitted by:	Dmitry Marakasov <amdmi3@amdmi3.ru> (maintainer)

--- games/allacrost/files/patch-src-main.cpp
+++ games/allacrost/files/patch-src-main.cpp
@@ -1,10 +1,10 @@
---- src/main.cpp.orig	Mon Sep 11 00:31:22 2006
-+++ src/main.cpp	Wed Oct  4 03:11:59 2006
-@@ -84,6 +84,7 @@
- 
- // Every great game begins with a single function :)
- int32 main(int32 argc, char *argv[]) {
+--- src/main.cpp.orig	Thu Jun  7 00:18:13 2007
++++ src/main.cpp	Thu Jun  7 01:18:11 2007
+@@ -96,6 +96,7 @@
+ ***
+ **/
+ bool InitializeEngine() {
 +	chdir(DATADIR);
- 	// When the program exits, first the QuitAllacrost() function, followed by SDL_Quit()
- 	atexit(SDL_Quit);
- 	atexit(QuitAllacrost);
+ 	// Initialize SDL. The video, audio, and joystick subsystems are initialized elsewhere.
+ 	if (SDL_Init(SDL_INIT_TIMER) != 0) {
+ 		cerr << "MAIN ERROR: Unable to initialize SDL: " << SDL_GetError() << endl;
