e7c0db2522e7745a82736d42d4b078f64a92a523
Pav Lucistnik <pav@FreeBSD.org>
2007-06-16 16:42:59 +0000

    - Update to 3.0.2
    PR:		ports/113744
    Submitted by:	Nils Vogels <nivo+kw+ports.bfa274@is-root.com>

--- irc/ctrlproxy/files/patch-src__network.h
+++ irc/ctrlproxy/files/patch-src__network.h
@@ -1,13 +1,14 @@
---- src/network.h.orig	Fri Jan  5 06:33:07 2007
-+++ src/network.h	Fri Jan  5 23:29:33 2007
-@@ -30,7 +30,10 @@
+--- src/network.h.orig	Sun Apr 15 14:17:58 2007
++++ src/network.h	Sat Jun 16 11:46:55 2007
+@@ -30,8 +30,11 @@
  #endif
  
  #include "state.h"
 +#include <sys/types.h>
+ #include "isupport.h"
  #include <sys/socket.h>
 +#include <netinet/in.h>
 +#include <arpa/inet.h>
  
- struct global;
- struct network;
+ #define DEFAULT_NETWORK_CHARSET NULL
+ 
