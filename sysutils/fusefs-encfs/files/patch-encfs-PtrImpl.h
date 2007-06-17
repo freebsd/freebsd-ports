8de72e58aa65590f92bb36b1d340c481d99bbd76
Edward Tomasz Napierala <trasz@FreeBSD.org>
2007-06-17 21:43:29 +0000

    - Fix buld with gcc42.
    Submitted by:	pointyhat via kris
    Approved by:	miwi (mentor)

--- sysutils/fusefs-encfs/files/patch-encfs-PtrImpl.h
+++ sysutils/fusefs-encfs/files/patch-encfs-PtrImpl.h
@@ -1,12 +1,16 @@
---- encfs/PtrImpl.h.orig	Fri Apr 15 11:34:22 2005
-+++ encfs/PtrImpl.h	Thu Jan 18 22:34:52 2007
-@@ -26,6 +26,16 @@
+--- encfs/PtrImpl.h.orig	Wed Feb 14 22:36:56 2007
++++ encfs/PtrImpl.h	Thu Jun 14 18:04:10 2007
+@@ -26,6 +26,20 @@
  
  #include "OpaqueValue.h"
  
 +#include "config.h"
 +#if defined(HAVE_ATOMIC_GCC) || defined(HAVE_ATOMIC_GCC_PRIVATE)
++#if (__GNUC__ >= 4 && __GNUC_MINOR__ >= 2)
++#include <ext/atomicity.h>
++#else
 +#include <bits/atomicity.h>
++#endif
 +#define HAVE_ATOMIC_FUNCS
 +#endif
 +
@@ -17,7 +21,7 @@
  namespace rel
  {
  
-@@ -55,7 +65,12 @@
+@@ -55,7 +69,12 @@
  	virtual void destroy() =0;
  
      protected:
