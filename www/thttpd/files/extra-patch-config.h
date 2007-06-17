cf564f8ab533efdd6e8a8d3eb1e2ee27ee987196
anders <anders@FreeBSD.org>
2007-06-17 19:38:09 +0000

    Nit-pick: regenerate config.h patch.

--- www/thttpd/files/extra-patch-config.h
+++ www/thttpd/files/extra-patch-config.h
@@ -1,6 +1,6 @@
---- config.h.old	Sat Jan 31 22:17:25 2004
-+++ config.h	Sat Jan 31 22:17:38 2004
-@@ -380,4 +380,6 @@
+--- config.h.orig	Wed Jun 29 19:53:18 2005
++++ config.h	Sun Jun 17 20:05:51 2007
+@@ -395,4 +395,6 @@
  */
  #define MIN_WOULDBLOCK_DELAY 100L
  
