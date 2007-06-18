8e6d14db95d21851c262db4304b3bc32141b27da
danfe <danfe@FreeBSD.org>
2007-06-18 13:38:39 +0000

    - Fix the build with recent GCC [1] - Utilize DATADIR macro in pkg-plist - Canonicalize and reindent Makefile header - Improve on port description
    Reported by:	pointyhat (logs) [1]

--- games/rockdodger/files/patch-main.c
+++ games/rockdodger/files/patch-main.c
@@ -3,6 +3,23 @@ $FreeBSD$
 
 --- main.c.orig
 +++ main.c
+@@ -206,7 +206,6 @@
+ struct bangdots bdot[MAX_BANG_DOTS], *bdotptr=bdot;
+ struct spacedot sdot[MAX_SPACE_DOTS];
+ 
+-struct greeble greeb[MAX_GREEBLES];
+ struct greeble {
+     int active;
+     float x,y; // When landed, these represent an offset from the host rock
+@@ -214,6 +213,8 @@
+     int landed;
+     int boredom; // Goes up while landed
+ };
++
++struct greeble greeb[MAX_GREEBLES];
+ // }}}
+ 
+ void init_greeblies() {
 @@ -459,7 +459,7 @@
      else {
  	char s[1024];
