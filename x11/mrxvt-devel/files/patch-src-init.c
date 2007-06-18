6b7b5af14083eddea51072e5a01e93c7c64ba6ee
miwi <miwi@FreeBSD.org>
2007-06-18 16:14:38 +0000

    - Add a application icon to mrxvt. (by Stanislav Mekhanoshin <rampitec AT gmail.com> ) - Bump PORTREVISION
    PR:		113782
    Submitted by:	Lars Engels <lars.engels@0x20.net> (maintainer)

--- x11/mrxvt-devel/files/patch-src-init.c
+++ x11/mrxvt-devel/files/patch-src-init.c
@@ -1,7 +1,5 @@
-Index: src/init.c
-===================================================================
---- src/init.c	(revision 195)
-+++ src/init.c	(working copy)
+--- src/init.c.orig	Sat Jun 16 23:26:00 2007
++++ src/init.c	Sat Jun 16 23:26:19 2007
 @@ -41,7 +41,7 @@
  # define DEBUG_X
  #else
@@ -11,3 +9,22 @@ Index: src/init.c
  #endif
  
  #if DEBUG_LEVEL
+@@ -3369,6 +3369,18 @@
+     wm_hint.initial_state = ISSET_OPTION(r, Opt_iconic) ? IconicState
+ 			    : NormalState;
+     wm_hint.window_group = r->TermWin.parent;
++
++    /* window icon hint */
++    if( r->h->rs[Rs_appIcon] ) {
++        XpmReadFileToPixmap(r->Xdisplay, r->TermWin.parent, r->h->rs[Rs_appIcon],
++            &r->h->appIcon, &r->h->appIconMask,0);
++    }
++    if( r->h->appIcon != None &&  r->h->appIconMask != None ) {
++        wm_hint.icon_pixmap = r->h->appIcon;
++        wm_hint.icon_mask = r->h->appIconMask;
++        wm_hint.flags |= IconPixmapHint | IconMaskHint;
++    }
++    
+     /* class hints */
+     class_hint.res_name = (char*) r->h->rs[Rs_name];
+     class_hint.res_class = (char*) APL_CLASS;
