0a1305413788a57344a47b461b2e3725dab7e223
Alexey Dokuchaev <danfe@FreeBSD.org>
2007-06-18 14:26:43 +0000

    - Fix the build with latest GCC [1] - Small nits in pkg-descr - Clean up Makefile
    Reported by:	pointyhat (logs) [1]

--- games/rottdc/files/patch-rt__in.c
+++ games/rottdc/files/patch-rt__in.c
@@ -1,5 +1,18 @@
 --- rt_in.c.orig	Wed Feb 18 08:30:12 2004
 +++ rt_in.c	Wed Feb 18 08:54:20 2004
+@@ -155,9 +155,9 @@
+ //
+ //****************************************************************************]
+ 
+-static KeyboardDef KbdDefs = {0x1d,0x38,0x47,0x48,0x49,0x4b,0x4d,0x4f,0x50,0x51};
+-static JoystickDef JoyDefs[MaxJoys];
+-static ControlType Controls[MAXPLAYERS];
++KeyboardDef KbdDefs = {0x1d,0x38,0x47,0x48,0x49,0x4b,0x4d,0x4f,0x50,0x51};
++JoystickDef JoyDefs[MaxJoys];
++ControlType Controls[MAXPLAYERS];
+ 
+ 
+ static boolean  IN_Started;
 @@ -665,14 +665,16 @@
  
     {
