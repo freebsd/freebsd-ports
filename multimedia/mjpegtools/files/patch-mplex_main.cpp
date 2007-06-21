b4095513d667d3bd5519cef6b6bee494e0976b9d
edwin <edwin@FreeBSD.org>
2007-06-21 01:35:39 +0000

    [patch] multimedia/mjpegtools - update to 1.9rc1
    	The current mjpegtools (mpeg2enc for example) is dumping core way
    	too often to be able to do useful media conversions.
    
    	This version has been running on my system for two weeks now and
    	it works much better.
    
    portrevision bump for multimedia/lives multimedia/mjpegtools-yuvfilters
    multimedia/y4mscaler since they have LIB_DEPENDS on this port. Other
    ports having RUN_DEPENDS on this port are not bumped.
    
    PR:		ports/113878
    Submitted by:	Edwin Groothuis <edwin@mavetju.org>
    Approved by:	Michael Johnson <ahze@ahze.net>

--- multimedia/mjpegtools/files/patch-mplex_main.cpp
+++ multimedia/mjpegtools/files/patch-mplex_main.cpp
@@ -1,5 +1,5 @@
---- mplex/main.cpp.orig	Thu Dec  9 21:13:26 2004
-+++ mplex/main.cpp	Mon Jul 18 21:11:07 2005
+--- mplex/main.cpp.orig	Wed Jun 20 13:40:39 2007
++++ mplex/main.cpp	Wed Jun 20 13:41:49 2007
 @@ -38,6 +38,7 @@
  #endif
  #include <ctype.h>
@@ -8,43 +8,47 @@
  #include "cpu_accel.h"
  #include "mjpeg_types.h"
  #include "mjpeg_logging.h"
-@@ -107,6 +108,13 @@
- 		mjpeg_error_exit1( "Could not open for writing: %s", cur_filename );
- 	}
- 
-+	{
-+		int	flags;
-+
-+		(void)fcntl(fileno(strm), F_GETFL, &flags);
-+		(void)fcntl(fileno(strm), F_SETFL, flags & ~O_NONBLOCK);
-+	}
-+
- 	return 0;
- }
- 
-@@ -144,6 +152,12 @@
+@@ -107,6 +108,14 @@
  	{
  		mjpeg_error_exit1( "Could not open for writing: %s", cur_filename );
  	}
-+	{
-+		int	flags;
++       {
++               int     flags;
 +
-+		(void)fcntl(fileno(strm), F_GETFL, &flags);
-+		(void)fcntl(fileno(strm), F_SETFL, flags & ~O_NONBLOCK);
-+	}
++               (void)fcntl(fileno(strm), F_GETFL, &flags);
++               (void)fcntl(fileno(strm), F_SETFL, flags & ~O_NONBLOCK);
++       }
++
++
+ 
+ 	return 0;
+ }
+@@ -143,6 +152,13 @@
+ 		mjpeg_error_exit1( "Could not open for writing: %s", cur_filename );
+ 	}
+     segment_len = 0;
++       {
++               int     flags;
++
++               (void)fcntl(fileno(strm), F_GETFL, &flags);
++               (void)fcntl(fileno(strm), F_SETFL, flags & ~O_NONBLOCK);
++       }
++
  }
  
  void
-@@ -200,6 +214,14 @@
+@@ -200,6 +216,14 @@
  	{
  		mjpeg_error_exit1( "Unable to open file %s for reading.", bs_filename);
  	}
 +
-+	{
-+		int	flags;
++       {
++               int     flags;
 +
-+		(void)fcntl(fileno(fileh), F_GETFL, &flags);
-+		(void)fcntl(fileno(fileh), F_SETFL, flags & ~O_NONBLOCK);
-+	}
++               (void)fcntl(fileno(fileh), F_GETFL, &flags);
++               (void)fcntl(fileno(fileh), F_SETFL, flags & ~O_NONBLOCK);
++       }
 +
  	filename = strcpy( new char[strlen(bs_filename)+1], bs_filename );
+     streamname = filename;
+ 
