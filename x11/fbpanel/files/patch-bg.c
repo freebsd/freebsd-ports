b3191f0f34003d164b3c18cba04e34d8920ca867
rafan <rafan@FreeBSD.org>
2007-06-22 06:34:08 +0000

    - Update to 4.9
    PR:		ports/113916
    Submitted by:	Tobias Roth <ports at fsck.ch> (maintainer)

--- x11/fbpanel/files/patch-bg.c
+++ x11/fbpanel/files/patch-bg.c
@@ -1,9 +1,9 @@
---- bg.c~	Sat Dec 18 17:41:41 2004
-+++ bg.c	Sat Dec 18 17:43:04 2004
-@@ -196,7 +196,7 @@
-     ENTER;
+--- bg.c.orig	Tue Apr 24 15:45:43 2007
++++ bg.c	Wed Jun 20 17:02:10 2007
+@@ -237,7 +237,7 @@
      win =  GDK_WINDOW_XWINDOW(widget->window); 
-     if (!XGetGeometry(bg->dpy, win, &dummy, &x, &y, &width, &height, &border, &depth)) {
+     if (!XGetGeometry(bg->dpy, win, &dummy, &x, &y, &width, &height, &border,
+               &depth)) {
 -        DBG2("XGetGeometry failed\n");
 +        DBG("XGetGeometry failed\n");
          RET(NULL);
