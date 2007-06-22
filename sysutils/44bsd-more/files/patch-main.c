d6a6f8552aaee8bda1b0a5217f6fc05f757d3f14
Xin LI <delphij@FreeBSD.org>
2007-06-22 06:53:11 +0000

    Unbreak for gcc 4.2.
    Approved by:	maintainer timeout
    PR:		ports/112847

--- sysutils/44bsd-more/files/patch-main.c
+++ sysutils/44bsd-more/files/patch-main.c
@@ -1,5 +1,5 @@
---- main.c.orig	Tue Aug  5 17:17:15 2003
-+++ main.c	Tue Aug  5 17:21:34 2003
+--- ./main.c.orig	2007-06-22 14:48:44.602177005 +0800
++++ ./main.c	2007-06-22 14:48:44.620758150 +0800
 @@ -33,9 +33,9 @@
   */
  
@@ -12,3 +12,20 @@
  	Regents of the University of California.  All rights reserved.\n";
  #endif /* not lint */
  
+@@ -68,6 +68,8 @@
+ #include "less.h"
+ #include "pathnames.h"
+ 
++static int str_read(void *, char *, size_t);
++
+ int	ispipe;
+ char	*current_file, *previous_file, *current_name, *next_name;
+ int	any_display;
+@@ -410,7 +412,6 @@
+ 	char *c;
+ 	int readrc();
+ 	int savederrno;
+-	static int str_read();
+ 
+ 	/* The default builtin rc file */
+ 	if ((c = getenv("HOME")) &&
