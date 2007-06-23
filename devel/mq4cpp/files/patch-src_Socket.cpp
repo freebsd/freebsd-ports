3ad4df44d5e95f1e0787c00ca699d7075e34bfee
Vanilla I. Shu <vanilla@FreeBSD.org>
2007-06-23 02:03:36 +0000

    Upgrade to 1.15.

---
+++ devel/mq4cpp/files/patch-src_Socket.cpp
@@ -0,0 +1,20 @@
+--- src/Socket.cpp.orig	Sat Jun 23 09:55:17 2007
++++ src/Socket.cpp	Sat Jun 23 09:58:16 2007
+@@ -156,7 +156,7 @@
+   } while  (IFRSIZE <= ifc.ifc_len);
+ 
+   ifr = ifc.ifc_req;
+-  for(; (ifr - ifc.ifc_req) * sizeof(ifreq) < static_cast<ulong>(ifc.ifc_len); ++ifr)
++  for(; (ifr - ifc.ifc_req) * sizeof(ifreq) < static_cast<unsigned long>(ifc.ifc_len); ++ifr)
+   {
+     if (ifr->ifr_addr.sa_data == (ifr+1)->ifr_addr.sa_data) continue;  // duplicate, skip it
+ 
+@@ -168,7 +168,7 @@
+     string ip = inet_ntoa(inaddrr(ifr_addr.sa_data));
+ 
+     // ------ get HW_ADDRESS ------
+-    if (ioctl(sockfd, SIOCGIFHWADDR, ifr) != 0) continue;  // failed to get mac, skip it
++    if (ioctl(sockfd, SIOCGIFMAC, ifr) != 0) continue;  // failed to get mac, skip it
+ 
+ 	string mac=string((char*)ifr->ifr_addr.sa_data,MAC_ADDRESS_CHAR_LEN);
+ 
--- devel/mq4cpp/files/patch-src_Thread.cpp
+++ devel/mq4cpp/files/patch-src_Thread.cpp
@@ -1,6 +1,6 @@
---- src/Thread.cpp.orig	Thu Jan  4 12:31:04 2007
-+++ src/Thread.cpp	Thu Jan  4 12:31:22 2007
-@@ -76,7 +76,7 @@
+--- src/Thread.cpp.orig	Sat Mar 17 10:20:15 2007
++++ src/Thread.cpp	Sat Jun 23 09:54:56 2007
+@@ -78,7 +78,7 @@
  #ifdef WIN32
  	return GetCurrentThreadId();
  #else
@@ -9,3 +9,15 @@
  #endif
  }
  
+@@ -277,10 +277,10 @@
+ 	if(SetThreadAffinityMask(m_hThread,mask)==0)
+ 		throw ThreadException("Failed to set affinity");
+ #else
++#ifdef HAVE_PTHREAD_SETAFFINITY_NP 
+ 	cpu_set_t cpuset;
+ 	CPU_ZERO(&cpuset);
+ 	CPU_SET(cpu, &cpuset);
+-#ifdef HAVE_PTHREAD_SETAFFINITY_NP 
+ #ifndef P2_PTHREAD_SETAFFINITY
+ 	if(pthread_setaffinity_np(m_hThread, sizeof(cpuset), &cpuset)!=0)
+ 		throw ThreadException("Failed to set affinity");
