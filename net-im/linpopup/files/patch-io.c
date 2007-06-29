163e8381a3911d254f2441e18103496e56c9c525
Martin Wilke <miwi@FreeBSD.org>
2007-06-29 10:40:05 +0000

    - Fix build with gcc4 - Support SUB_FILES
    PR:		114077
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ net-im/linpopup/files/patch-io.c
@@ -0,0 +1,10 @@
+--- io.c.orig	Tue Mar 14 11:45:05 2000
++++ io.c	Sun Jun 24 00:08:35 2007
+@@ -21,6 +21,7 @@
+ 
+ #include <unistd.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include "protos.h"
+ 
