937976889d7f66ba8ad1e4400203e8ac9e40f052
Beech Rintoul <beech@FreeBSD.org>
2007-06-30 20:11:39 +0000

    - Update to 0.2.7
    PR:		ports/114089
    Submitted by:	Phillip Neumann <pneumann@gmail.com> (maintainer)
    Approved by:	sat (mentor)

--- deskutils/gimmie/files/patch-gimmie-gimmie_util.py
+++ deskutils/gimmie/files/patch-gimmie-gimmie_util.py
@@ -1,6 +1,16 @@
---- gimmie/gimmie_util.py.orig	Tue Feb  6 19:54:52 2007
-+++ gimmie/gimmie_util.py	Tue Feb  6 19:55:07 2007
-@@ -625,7 +625,6 @@
+--- gimmie/gimmie_util.py.orig	Fri Jun  8 05:55:28 2007
++++ gimmie/gimmie_util.py	Sat Jun  9 17:21:25 2007
+@@ -57,7 +57,8 @@
+                 monitor_type = gnomevfs.MONITOR_DIRECTORY
+             else:
+                 monitor_type = gnomevfs.MONITOR_FILE
+-            self.monitor = gnomevfs.monitor_add(self.path, monitor_type, self._queue_event)
++            if os.path.exists(self.path):
++                self.monitor = gnomevfs.monitor_add(self.path, monitor_type, self._queue_event)
+ 
+     def _clear_timeout(self, info_uri):
+         try:
+@@ -736,7 +737,6 @@
      DEFAULT_BOOKMARKS = [
          ("firefox.desktop", "application/x-desktop", "gimmie.gimmie_applications.DesktopFileItem"),
          ("nautilus.desktop", "application/x-desktop", "gimmie.gimmie_applications.DesktopFileItem"),
