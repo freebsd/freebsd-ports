7d34a82ca9cffcc6b5c377767bfa04ab80d255ca
pav <pav@FreeBSD.org>
2007-07-02 16:37:03 +0000

    - Fix build with gcc42
    PR:		ports/112687
    Submitted by:	Gavin Atkinson <gavin.atkinson@ury.york.ac.uk> (maintainer)

---
+++ net-p2p/mutella/files/patch-gnushare.h
@@ -0,0 +1,10 @@
+--- mutella/gnushare.h	Fri Nov 15 20:22:57 2002
++++ gnushare.h.new	Tue May 15 15:47:57 2007
+@@ -26,6 +26,7 @@
+ #include "mthread.h"
+ 
+ class MGnuDirector;
++class MShareThread;
+ 
+ class MGnuShare  
+ {
