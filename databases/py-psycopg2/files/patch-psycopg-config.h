1995c66e009861e3f75fecea80ff4bab988ad89e
Martin Wilke <miwi@FreeBSD.org>
2007-07-02 15:23:00 +0000

    - Update to 2.0.6 - Respect NOPORTEXAMPLES - Drop FreeBSD 4.X gruft

--- databases/py-psycopg2/files/patch-psycopg-config.h
+++ databases/py-psycopg2/files/patch-psycopg-config.h
@@ -1,11 +1,11 @@
---- psycopg/config.h.orig	Thu Jan 18 01:23:39 2007
-+++ psycopg/config.h	Thu Jan 18 01:24:57 2007
-@@ -102,7 +102,7 @@
+--- psycopg/config.h.orig	2007-04-11 12:12:37.000000000 +0000
++++ psycopg/config.h	2007-07-02 14:41:35.000000000 +0000
+@@ -113,7 +113,7 @@
  #define inline
  #endif
  
--#if defined(__FreeBSD__) || defined(_WIN32) || defined(__sun__)
-+#if defined(_WIN32) || defined(__sun__)
+-#if defined(__FreeBSD__) || (defined(_WIN32) && !defined(__GNUC__)) || defined(__sun__)
++#if (defined(_WIN32) && !defined(__GNUC__)) || defined(__sun__)
  /* what's this, we have no round function either? */
  static double round(double num)
  {
