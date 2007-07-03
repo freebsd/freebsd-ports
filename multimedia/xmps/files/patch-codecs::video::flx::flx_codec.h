5560133eb246c036ed05e9e011c46631b98b5275
Alexey Dokuchaev <danfe@FreeBSD.org>
2007-07-03 17:03:31 +0000

    - Fix the build with GCC 4.2 [1] - Try to pacify portlint(1) - Usual minor Makefile cleanups
    Reported by:	pointyhat (logs) [1]

--- multimedia/xmps/files/patch-codecs::video::flx::flx_codec.h
+++ multimedia/xmps/files/patch-codecs::video::flx::flx_codec.h
@@ -3,6 +3,17 @@ $FreeBSD$
 
 --- codecs/video/flx/flx_codec.h.orig	Wed Nov  1 20:46:14 2000
 +++ codecs/video/flx/flx_codec.h	Sun Feb  4 22:55:34 2001
+@@ -33,8 +33,8 @@
+ #include "config.h"
+ #endif
+ 
+-#define ReadU16(tmp1, tmp2) (Uint16)*(tmp1) = ((Uint8)*(tmp2+1)<<8)+(Uint8)*(tmp2);
+-#define ReadU32(tmp1, tmp2) (Uint32)*(tmp1) = (((((((Uint8)*(tmp2+3)<<8)+((Uint8)*(tmp2+2)))<<8)+((Uint8)*(tmp2+1)))<<8)+(Uint8)*(tmp2));
++#define ReadU16(tmp1, tmp2) *(tmp1) = (Uint16)((Uint8)*(tmp2+1)<<8)+(Uint8)*(tmp2)
++#define ReadU32(tmp1, tmp2) *(tmp1) = (Uint32)(((((((Uint8)*(tmp2+3)<<8)+((Uint8)*(tmp2+2)))<<8)+((Uint8)*(tmp2+1)))<<8)+(Uint8)*(tmp2))
+ 
+ /*
+  * includes : local
 @@ -52,7 +52,7 @@
  #include <stdlib.h>
  #include <string.h>
