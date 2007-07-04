9321d16f8541747a76eec0751402490ca29b617f
marcus <marcus@FreeBSD.org>
2007-07-04 18:03:24 +0000

    Fix the build with GCC 4.2.
    Reported by:	pointyhat via pav

---
+++ www/gtkhtml/files/patch-src_htmlcluealigned.c
@@ -0,0 +1,11 @@
+--- src/htmlcluealigned.c.orig	Wed Jul  4 14:01:42 2007
++++ src/htmlcluealigned.c	Wed Jul  4 14:01:58 2007
+@@ -26,7 +26,7 @@
+ #define ALIGN_BORDER 0
+ 
+ 
+-static HTMLClueAlignedClass html_cluealigned_class;
++HTMLClueAlignedClass html_cluealigned_class;
+ static HTMLClueClass *parent_class = NULL;
+ 
+ 
