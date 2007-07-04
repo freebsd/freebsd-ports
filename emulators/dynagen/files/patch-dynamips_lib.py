8554787dd2d9c3a4ceebdc5dc903a4967c4be227
Edward Tomasz Napierala <trasz@FreeBSD.org>
2007-07-04 11:07:56 +0000

    - Update to 0.9.3.
    PR:		ports/113807
    Submitted by:	Pavel I Volkov <pol@opk.ru>
    Approved by:	miwi (mentor)

--- emulators/dynagen/files/patch-dynamips_lib.py
+++ emulators/dynagen/files/patch-dynamips_lib.py
@@ -1,15 +1,6 @@
 --- dynamips_lib.py.orig	Sun Feb 18 17:28:09 2007
 +++ dynamips_lib.py	Mon Feb 26 21:05:00 2007
-@@ -820,7 +820,7 @@
-         self.__mmap = True
-         self.__state = 'stopped'
-         self.__ghost_status = 0
--        self._sparsemem = 0
-+        self.__sparsemem = 0
-         self.__idlemax = 1500
-         self.__idlesleep = 30
- 
-@@ -1295,7 +1295,11 @@
+@@ -1411,7 +1411,11 @@
              flag = '1'
          else:
              flag = '0'
