9d9abbeb50c828a57f2d0d1f703a5300ede70d84
barner <barner@FreeBSD.org>
2007-07-04 15:11:34 +0000

    Fix build with boost 1.34.0.

---
+++ devel/monotone/files/patch-numeric_vocab.hh
@@ -0,0 +1,10 @@
+--- numeric_vocab.hh.orig	Mon May  7 16:13:44 2007
++++ numeric_vocab.hh	Sat Jun 30 22:53:12 2007
+@@ -11,6 +11,7 @@
+ // PURPOSE.
+ 
+ #include <cstddef>
++#include <climits>
+ #include <limits>
+ 
+ #include "mt-stdint.h"
