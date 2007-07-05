b3388023a91de8a7b549de2e40e5f5de263035bf
demon <demon@FreeBSD.org>
2007-07-05 09:15:41 +0000

    Update to 2.1-pre28
    Based on:
    PR:		109332
    Submitted by:	rafan

--- www/links/files/patch-types.c
+++ www/links/files/patch-types.c
@@ -1,9 +1,9 @@
---- types.c.orig	Thu Mar 24 10:36:47 2005
-+++ types.c	Thu Mar 24 10:38:12 2005
-@@ -796,7 +796,8 @@
- 		if (*ct == '.') ext = ct + 1;
- 		else if (dir_sep(*ct)) ext = NULL;
- 	if (ext) while (ext[extl] && !dir_sep(ext[extl]) && !end_of_dir(url, ext[extl])) extl++;
+--- types.c.orig	Thu Jul  5 13:10:37 2007
++++ types.c	Thu Jul  5 13:12:07 2007
+@@ -807,7 +807,8 @@
+ 			ext = NULL;
+ 		}
+ 	if (ext) while (ext[extl] && ext[extl] != '.' && !dir_sep(ext[extl]) && !end_of_dir(url, ext[extl])) extl++;
 -	if ((extl == 3 && !casecmp(ext, "htm", 3)) ||
 +	if (force_html ||
 +	    (extl == 3 && !casecmp(ext, "htm", 3)) ||
