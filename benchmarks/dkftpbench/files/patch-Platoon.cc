52ef7d812ed7f2dd57d3421e7c05881209d070da
Roman Bogorodskiy <novel@FreeBSD.org>
2007-07-08 05:22:54 +0000

    Fix build with gcc 4.2.
    Reported by:	pointyhat via pav

---
+++ benchmarks/dkftpbench/files/patch-Platoon.cc
@@ -0,0 +1,8 @@
+--- Platoon.cc.orig	2007-07-08 09:20:15.000000000 +0400
++++ Platoon.cc	2007-07-08 09:20:35.000000000 +0400
+@@ -1,3 +1,5 @@
++#include <assert.h>
++
+ #include "Platoon.h"
+ #include "dprint.h"
+ 
