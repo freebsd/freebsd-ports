727976a1cbcb485f4125cb78f0a6b7870636c2e4
Simon Barner <barner@FreeBSD.org>
2007-07-08 09:18:00 +0000

    Fix build with boost 1.34.0 (#include different file).
    Approved by:	silence from alepulver (maintainer)

---
+++ games/galaxyhack/files/patch-SetupBattle.cpp
@@ -0,0 +1,11 @@
+--- SetupBattle.cpp.orig	Fri Jul  6 01:44:52 2007
++++ SetupBattle.cpp	Fri Jul  6 01:52:29 2007
+@@ -18,7 +18,7 @@
+ #include "PreBattle.h"
+ 
+ #include <sstream>
+-#include <boost/filesystem/exception.hpp>
++#include <boost/filesystem/path.hpp>
+ 
+ using std::stringstream;
+ using std::getline;
