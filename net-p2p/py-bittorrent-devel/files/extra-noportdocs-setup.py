2c686b352e2d0ca0813b6f81a7c724fda04474c8
Pav Lucistnik <pav@FreeBSD.org>
2007-07-10 21:37:24 +0000

    - Non-devel port have much newer version, and since there is no visible   development branch, remove
    PR:		ports/114223
    Submitted by:	Wesley Shields <wxs@atarininja.org>

--- net-p2p/py-bittorrent-devel/files/extra-noportdocs-setup.py
+++
@@ -1,6 +0,0 @@
---- setup.py.orig	Sat Jun 24 16:55:50 2006
-+++ setup.py	Sat Jun 24 16:57:02 2006
-@@ -69,3 +69,0 @@
--    (doc_root, ['credits.txt', 'credits-l10n.txt', 'LICENSE.txt', 'README.txt',
--
--                'INSTALL.unix.txt'] + extra_docs),
