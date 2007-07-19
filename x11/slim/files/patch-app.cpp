f5c2855dd24235aa28d83edd5f658ad397ccfe50
rafan <rafan@FreeBSD.org>
2007-07-19 16:09:00 +0000

    - Update to 1.3.0 [1] - Respect PREFIX [1] - Remove 4.x check [1] - Allow slim starting from /etc/ttys [2]
    PR:		ports/114708 [1], ports/114366 [2]
    Submitted by:	Tobias Roth <ports at fsck.ch> (maintainer) [1],
    		Nikos Ntarmos <ntarmos at ceid.upatras.gr> [2]
    Approved by:	maintainer [2]

--- x11/slim/files/patch-app.cpp
+++ x11/slim/files/patch-app.cpp
@@ -1,34 +1,16 @@
---- app.cpp.orig	Fri Nov 24 16:36:58 2006
-+++ app.cpp	Sun Nov 26 23:14:35 2006
-@@ -560,13 +560,6 @@ int App::StartServer() {
-         } else if (pos == 0) {
-             server[argc++] = args+pos;
-         }
--        if (server[argc-1][0] == 'v' && server[argc-1][1] == 't') {
--            bool ok = false;
--            Cfg::string2int(server[argc-1]+2, &ok);
--            if (ok) {
--                hasVtSet = true;
--            }
--        }
-         ++pos;
+--- app.cpp.orig	Sun Jul 15 16:09:28 2007
++++ app.cpp	Wed Jul 18 20:28:27 2007
+@@ -135,6 +135,7 @@
+     ServerPID = -1;
+     testing = false;
+     mcookie = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
++    daemonmode = false;
+     Dpy = NULL;
  
-         if (argc+1 >= MAX_XSERVER_ARGS) {
-@@ -576,8 +569,19 @@ int App::StartServer() {
-             break;
-         }
+     // Parse command line
+@@ -858,7 +859,7 @@
      }
-+
-+    for (int i=0; i<argc; i++) {
-+        if (server[i][0] == 'v' && server[i][1] == 't') {
-+            bool ok = false;
-+            Cfg::string2int(server[i]+2, &ok);
-+            if (ok) {
-+                hasVtSet = true;
-+            }
-+        }
-+    }
-+
+ 
      if (!hasVtSet && daemonmode) {
 -        server[argc++] = "vt07";
 +        server[argc++] = "vt09";
