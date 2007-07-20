263b5419ca27501b10519be30a05d0a6a1144cc9
sat <sat@FreeBSD.org>
2007-07-20 19:50:42 +0000

    - Update to 0.2.3

--- graphics/gimmage/files/patch-src__FileManager.cpp
+++ graphics/gimmage/files/patch-src__FileManager.cpp
@@ -1,15 +1,10 @@
---- ./src/FileManager.cpp.orig	Sun Jun 10 01:55:51 2007
-+++ ./src/FileManager.cpp	Sun Jun 10 01:56:13 2007
-@@ -20,10 +20,12 @@
- // gimmage: FileManager.cpp
+--- ./src/FileManager.cpp.orig	2007-07-20 23:42:55.000000000 +0400
++++ ./src/FileManager.cpp	2007-07-20 23:43:03.000000000 +0400
+@@ -23,6 +23,7 @@
+ 
  #include "FileManager.h"
  #include "defines.h"
-+#define get_current_dir_name() getcwd(NULL, PATH_MAX) 
++#define get_current_dir_name() getcwd(NULL, PATH_MAX)
  
  #include "../config.h"
  
- extern "C" {
-+#include <libintl.h>
- #include <string.h>
- #include <unistd.h>
- }
