fbac201cb7e199bfee97dbbe3e633c0d62c246df
pav <pav@FreeBSD.org>
2007-07-20 12:41:02 +0000

    - The port was patching /usr/lib --> /usr/X11R6/lib and then replacing all   occurrences of X11BASE to PREFIX. It's a lot cleaner to remove all prefix   changing patches and directly change /usr/lib to $PREFIX/lib with reinplace.
    PR:		ports/114507
    Submitted by:	Pedro F. Giffuni <giffunip@tutopia.com>

--- x11-toolkits/xview/files/patch-clients+olwm+screen.c
+++ x11-toolkits/xview/files/patch-clients+olwm+screen.c
@@ -1,5 +1,5 @@
 --- clients/olwm/screen.c.orig	Tue Jun 29 00:11:56 1993
-+++ clients/olwm/screen.c	Fri Oct 17 15:18:34 2003
++++ clients/olwm/screen.c	Sat Jun 30 19:34:43 2007
 @@ -111,6 +111,12 @@
  static XrmQuark stippledRubberBandsCQ;
  static XrmQuark stippledRubberBandsIQ;
@@ -21,21 +21,3 @@
  		    		return True;
  				/*NOTREACHED*/
  		    		break;
-@@ -460,7 +467,7 @@
-  * 	Construct bitmap search path as follows:
-  *		$OPENWINHOME/etc/workspace/patterns
-  *		$OPENWINHOME/include/X11/include/bitmaps
-- *		/usr/X11/include/X11/include/bitmaps
-+ *		/usr/X11R6/include/X11/include/bitmaps
-  *
-  * REMIND: this should be cleaned up so that it doesn't use a fixed-size 
-  * array.
-@@ -483,7 +490,7 @@
- 	(void)sprintf(bmPath, "%s/include/X11/bitmaps",owHome);
- 	bitmapSearchPath[i++] = MemNewString(bmPath);
- 
--	bitmapSearchPath[i++] = MemNewString("/usr/X11/include/X11/bitmaps");
-+	bitmapSearchPath[i++] = MemNewString("/usr/X11R6/include/X11/bitmaps");
- 
- 	bitmapSearchPath[i] = (char *)NULL;
- }
