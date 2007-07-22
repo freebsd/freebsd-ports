c9efa388404b8345fc8709555d1d23198b11f85e
se <se@FreeBSD.org>
2007-07-22 19:30:35 +0000

    Fix path to file to patch, I had accidently committed the patch with an absolute path that was correct only on my development system.

--- security/libfwbuilder/files/patch-IPAddress.h
+++ security/libfwbuilder/files/patch-IPAddress.h
@@ -1,5 +1,5 @@
---- /usr/ports/security/libfwbuilder/work/libfwbuilder-2.1.12/src/fwbuilder/IPAddress.h~	2006-08-18 05:59:32.000000000 +0200
-+++ /usr/ports/security/libfwbuilder/work/libfwbuilder-2.1.12/src/fwbuilder/IPAddress.h	2007-07-14 17:09:06.406044356 +0200
+--- src/fwbuilder/IPAddress.h~	2006-08-18 05:59:32.000000000 +0200
++++ src/fwbuilder/IPAddress.h	2007-07-14 17:09:06.406044356 +0200
 @@ -47,6 +47,7 @@
  {
  
