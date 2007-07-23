552f179d46fcf513955d85b099ceb19625463455
Thierry Thomas <thierry@FreeBSD.org>
2007-07-23 22:00:52 +0000

    - Upgrade to 2.0.8;   Changelog at <http://www.geuz.org/gmsh/doc/VERSIONS>
    - Now we are allowed to package Gmsh built with Tetgen: thanks to Pedro
      F. Giffuni, the author granted the FreeBSD project to redistribute it.
      However, redistribution of Triangle has not yet been granted, then:
      . build the default package with Tetgen and Triangle;
      . set NO_CDROM if Triangle is not disabled;
      . explain the conditions in post-installation message.
    
    Suggested by:	Pedro F. Giffuni <giffunip (at) yahoo.com>

---
+++ cad/gmsh/files/patch-Post_AdaptiveViews.cpp
@@ -0,0 +1,10 @@
+--- ./Post/AdaptiveViews.cpp.orig	Mon Jul  9 15:54:37 2007
++++ ./Post/AdaptiveViews.cpp	Mon Jul 23 22:42:06 2007
+@@ -20,6 +20,7 @@
+ 
+ #include <stdio.h>
+ #include <math.h>
++#include <time.h>
+ #include <list>
+ #include <set>
+ #include "AdaptiveViews.h"
