dc3bf5696040fe9987f2bcc4b50c8660ef936955
Alejandro Pulver <alepulver@FreeBSD.org>
2007-07-24 15:24:03 +0000

    - Fix building on 64-bit architectures with GCC 4.x.
    Reported by:	pav

--- games/airrox/files/patch-visual.cpp
+++ games/airrox/files/patch-visual.cpp
@@ -1,5 +1,73 @@
---- ./visual.cpp.orig	Tue Mar 27 14:57:10 2007
-+++ ./visual.cpp	Tue Mar 27 15:10:12 2007
+--- visual.cpp.orig	Fri Oct  8 09:22:08 2004
++++ visual.cpp	Sun Jul 22 14:04:48 2007
+@@ -37,15 +37,15 @@
+ Visual::Visual(Jogo *j, int telaLargura, int telaAltura, bool telaCheia, string telaTitulo) {
+ 	jogo = j;
+ 	
+-	this->font = (int)GLUT_BITMAP_8_BY_13;
++	this->font = (long)GLUT_BITMAP_8_BY_13;
+ 	this->telaLargura = telaLargura;
+ 	this->telaAltura = telaAltura;
+ 	this->telaTitulo = telaTitulo.c_str();
+ 
+-	Load3DS (&mesa,"../meshs/mesa.3ds");
+-	Load3DS(&mallet,"../meshs/mallet.3ds");
+-	Load3DS(&puck,"../meshs/puck.3ds");
+-	Load3DS(&placar,"../meshs/placar.3ds");
++	Load3DS (&mesa,"/usr/local/share/airrox/meshs/mesa.3ds");
++	Load3DS(&mallet,"/usr/local/share/airrox/meshs/mallet.3ds");
++	Load3DS(&puck,"/usr/local/share/airrox/meshs/puck.3ds");
++	Load3DS(&placar,"/usr/local/share/airrox/meshs/placar.3ds");
+ 
+ 	criarJanela();	
+ 	setarVideo(telaLargura, telaAltura, telaCheia);
+@@ -96,22 +96,22 @@
+ 	glEnable(GL_LIGHT0);
+ 	
+ 	/*TODO criar uma variavel Path de texturas e meshs*/
+-	mesa.id_texture=LoadBitmap("../texturas/mesa.bmp");
+-	mallet.id_texture=LoadBitmap("../texturas/mallet.bmp");
+-    	puck.id_texture=LoadBitmap("../texturas/puck.bmp");
+-	placar.id_texture=LoadBitmap("../texturas/placar.bmp");
++	mesa.id_texture=LoadBitmap("/usr/local/share/airrox/texturas/mesa.bmp");
++	mallet.id_texture=LoadBitmap("/usr/local/share/airrox/texturas/mallet.bmp");
++    	puck.id_texture=LoadBitmap("/usr/local/share/airrox/texturas/puck.bmp");
++	placar.id_texture=LoadBitmap("/usr/local/share/airrox/texturas/placar.bmp");
+ 	
+-	display[0]=LoadBitmap("../texturas/display0.bmp");
+-	display[1]=LoadBitmap("../texturas/display1.bmp");
+-	display[2]=LoadBitmap("../texturas/display2.bmp");
+-	display[3]=LoadBitmap("../texturas/display3.bmp");
+-	display[4]=LoadBitmap("../texturas/display4.bmp");
+-	display[5]=LoadBitmap("../texturas/display5.bmp");
+-	display[6]=LoadBitmap("../texturas/display6.bmp");
+-	display[7]=LoadBitmap("../texturas/display7.bmp");
+-	//display[8]=LoadBitmap("../texturas/display8.bmp");
+-	//display[9]=LoadBitmap("../texturas/display9.bmp");
+-	logomesa=LoadBitmap("../texturas/logomesa.bmp");
++	display[0]=LoadBitmap("/usr/local/share/airrox/texturas/display0.bmp");
++	display[1]=LoadBitmap("/usr/local/share/airrox/texturas/display1.bmp");
++	display[2]=LoadBitmap("/usr/local/share/airrox/texturas/display2.bmp");
++	display[3]=LoadBitmap("/usr/local/share/airrox/texturas/display3.bmp");
++	display[4]=LoadBitmap("/usr/local/share/airrox/texturas/display4.bmp");
++	display[5]=LoadBitmap("/usr/local/share/airrox/texturas/display5.bmp");
++	display[6]=LoadBitmap("/usr/local/share/airrox/texturas/display6.bmp");
++	display[7]=LoadBitmap("/usr/local/share/airrox/texturas/display7.bmp");
++	//display[8]=LoadBitmap("/usr/local/share/airrox/texturas/display8.bmp");
++	//display[9]=LoadBitmap("/usr/local/share/airrox/texturas/display9.bmp");
++	logomesa=LoadBitmap("/usr/local/share/airrox/texturas/logomesa.bmp");
+ 
+ 	/*TODO Criar metodo pra fazer isso abaixo */
+ 	if (mesa.id_texture==-1) {
+@@ -165,7 +165,7 @@
+ 
+ 	//telaBpp = telaInfo->vfmt->BitsPerPixel;
+ 
+-	SDL_WM_SetIcon(SDL_LoadBMP("../icones/airrox.bmp"), NULL);
++	SDL_WM_SetIcon(SDL_LoadBMP("/usr/local/share/airrox/icones/airrox.bmp"), NULL);
+ 	SDL_WM_SetCaption(this->telaTitulo.c_str(),NULL);
+ 
+ 	return 1;
 @@ -219,7 +219,7 @@
  	return 1;
  }
--- games/airrox/files/patch-visual.h
+++ games/airrox/files/patch-visual.h
@@ -1,5 +1,14 @@
---- ./visual.h.orig	Tue Mar 27 14:57:10 2007
-+++ ./visual.h	Tue Mar 27 15:00:26 2007
+--- visual.h.orig	Fri Oct  8 09:22:08 2004
++++ visual.h	Sun Jul 22 14:04:49 2007
+@@ -46,7 +46,7 @@
+ 
+ 	public:		
+ 		GLuint listaMesa;
+-		int font;
++		long font;
+ 		int telaLargura;
+ 		int telaAltura;
+ 		int telaCheia;
 @@ -70,7 +70,7 @@
  		int setarTelaCheia(void);
  		int aumentarResolucao(void);
