bf9eea6946bff7159249728eb716222021d0f5e7
miwi <miwi@FreeBSD.org>
2007-07-24 09:48:42 +0000

    - Update to 7.6.2 - Pass maintainership to submitter
    PR:		114845
    Submitted by:	Thomas Abthorpe <thomas@goodking.ca>

---
+++ misc/kcd/files/patch-version.cc
@@ -0,0 +1,8 @@
+--- version.cc.orig	Sat Jun 16 08:25:50 2007
++++ version.cc	Mon Jul 23 20:03:44 2007
+@@ -1,4 +1,4 @@
+-//bin/true; VERSION="7.6.2"; return 0
++//usr/bin/true; VERSION="7.6.2"; return 0
+ // Above magic is for configure to get version number
+ // Generate automatically from scripts/versions.cc.src
+ char	version[] = "7.6.2";
