b6948d866feb46add587613097d9061b4c3c0999
Martin Wilke <miwi@FreeBSD.org>
2007-07-25 09:31:31 +0000

    - Fix segfaults at start up
    PR:		114795
    Submitted by:	Max Brazhnikov <makc@issp.ac.ru> (maintainer)

---
+++ games/glaxium/files/patch-scene.cpp
@@ -0,0 +1,11 @@
+--- ./scene.cpp.orig	Thu Oct 10 16:57:58 2002
++++ ./scene.cpp	Sat Jul 21 17:54:40 2007
+@@ -2361,7 +2361,7 @@
+ 	oldTimer = newTimer;
+ 	globalList->move()	;
+ #ifndef PI
+-#define PI=3.151592653589793
++#define PI=3.141592653589793
+ #endif
+ 	benchFrames++;
+ 
