71d0efc3ff320fa1bec4d66a639e6a51c675b6a8
miwi <miwi@FreeBSD.org>
2007-07-25 09:31:31 +0000

    - Fix segfaults at start up
    PR:		114795
    Submitted by:	Max Brazhnikov <makc@issp.ac.ru> (maintainer)

---
+++ games/glaxium/files/patch-variables.cpp
@@ -0,0 +1,10 @@
+--- ./variables.cpp.orig	Fri Oct 18 01:53:00 2002
++++ ./variables.cpp	Sat Jul 21 17:51:10 2007
+@@ -453,6 +453,7 @@
+ 	static char strReturn[1024];
+ 	strcpy(strReturn, getenv("HOME"));
+ 	strcat(strReturn, "~/.glaxiumrc");
++	return strReturn;
+ #endif
+ }
+ 
