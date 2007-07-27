ef5eac6fc1101a6e41c791d62003cbc8c3c6c238
Martin Wilke <miwi@FreeBSD.org>
2007-07-27 16:39:56 +0000

    - Fix build with gcc 4.2
    PR:		114745
    Submitted by:	Matthias Sund <m.sund@arcor.de> (maintainer)

---
+++ devel/upp/files/patch-uppsrc-Core-Core.h
@@ -0,0 +1,19 @@
+--- uppsrc/Core/Core.h.orig	Thu Jul 19 19:57:53 2007
++++ uppsrc/Core/Core.h	Thu Jul 19 19:59:16 2007
+@@ -300,8 +300,16 @@
+ #include <string>
+ 
+ #ifdef PLATFORM_POSIX
++#ifdef COMPILER_GCC
++#if __GNUC__ > 3
++#include <ext/atomicity.h>
++#else
++#include <bits/atomicity.h>
++#endif
++#else
+ #include <bits/atomicity.h>
+ #endif
++#endif
+ 
+ // fix MSC8 beta problem....
+ #ifdef COMPILER_MSC
