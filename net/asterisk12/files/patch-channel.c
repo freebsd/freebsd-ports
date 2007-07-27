e8ebf11a7fb69956138393121767e79206c3c6ca
Maxim Sobolev <sobomax@FreeBSD.org>
2007-07-27 00:28:45 +0000

    Fix a critical bug in the channel code, which can result in a thread going into infinite loop on hangup. This bug is already fixed in 1.4.x.
    Bump PORTREVISION.
    
    Sponsored by: Sippy Software, Inc. ( http://www.sippysoft.com/ )

--- net/asterisk12/files/patch-channel.c
+++ net/asterisk12/files/patch-channel.c
@@ -12,3 +12,16 @@ $FreeBSD$
  #endif /* __linux__ */
  #ifndef ZT_TIMERPING
  #error "You need newer zaptel!  Please svn update zaptel"
+@@ -1564,6 +1564,12 @@
+ 			if (!havewhen)
+ 				time(&now);
+ 			diff = c[x]->whentohangup - now;
++			if (diff < 1) {
++				/* Should already be hungup */
++				c[x]->_softhangup |= AST_SOFTHANGUP_TIMEOUT;
++				ast_mutex_unlock(&c[x]->lock);
++				return c[x];
++			}
+ 			if (!havewhen || (diff < whentohangup)) {
+ 				havewhen++;
+ 				whentohangup = diff;
