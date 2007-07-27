cf24da0ea70fd8fd684751e4d3cdb248f7edbd41
Maxim Sobolev <sobomax@FreeBSD.org>
2007-07-27 02:41:10 +0000

    Update to 1.4.9.
    Sponsored by: Sippy Software, Inc. ( http://www.sippysoft.com/ )
    
    Enable SQLite support.
    
    PR:		114720
    Submitted by:	VipeR

--- net/asterisk16/files/patch-channels::chan_skinny.c
+++ net/asterisk16/files/patch-channels::chan_skinny.c
@@ -3,12 +3,12 @@ $FreeBSD$
 
 --- channels/chan_skinny.c.orig
 +++ channels/chan_skinny.c
-@@ -99,7 +99,7 @@
+@@ -107,7 +107,7 @@
  #define htolel(x) (x)
  #define htoles(x) (x)
  #else
--#if defined(SOLARIS) || defined(__Darwin__) || defined(__NetBSD__)
-+#if defined(SOLARIS) || defined(__Darwin__) || defined(__NetBSD__) || defined(__FreeBSD__)
+-#if defined(SOLARIS) || defined(__Darwin__) || defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(SOLARIS) || defined(__Darwin__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__)
  #define __bswap_16(x) \
-      ((((x) & 0xff00) >> 8) | \
-       (((x) & 0x00ff) << 8))
+ 	((((x) & 0xff00) >> 8) | \
+ 	 (((x) & 0x00ff) << 8))
