deec8960b0ad1fefeeeb648719581d1b9f8b608b
mezz <mezz@FreeBSD.org>
2007-07-27 16:03:03 +0000

    Update to 2.14.8 and get rid of X11BASE.

--- devel/ORBit2/files/patch-src_orb_orb-core_orbit-typelib.c
+++ devel/ORBit2/files/patch-src_orb_orb-core_orbit-typelib.c
@@ -1,10 +1,9 @@
---- src/orb/orb-core/orbit-typelib.c.orig	Fri Mar  5 23:42:33 2004
-+++ src/orb/orb-core/orbit-typelib.c	Fri Mar  5 23:43:00 2004
-@@ -218,6 +218,8 @@
+--- src/orb/orb-core/orbit-typelib.c.orig	2007-07-27 10:58:37.000000000 -0500
++++ src/orb/orb-core/orbit-typelib.c	2007-07-27 10:58:41.000000000 -0500
+@@ -218,6 +218,7 @@
  	paths = g_ptr_array_sized_new (8);
  
  	g_ptr_array_add (paths, g_strdup (ORBIT_TYPELIB_DIR));
-+	add_if_unique(paths, "%%X11BASE%%", TRUE);
 +	add_if_unique(paths, "%%LOCALBASE%%", TRUE);
  
  	if ((path = g_getenv ("ORBIT_TYPELIB_PATH"))) {
