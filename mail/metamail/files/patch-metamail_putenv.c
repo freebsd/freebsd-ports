a4f7a538a2ac20ba360d6f9a751341e8bf593c56
Pav Lucistnik <pav@FreeBSD.org>
2007-07-29 01:30:33 +0000

    - Fix build on -CURRENT (untested)
    PR:		ports/114599
    Submitted by:	Jean-Francois Dockes <jean-francois.dockes@wanadoo.fr> (maintainer)

--- mail/metamail/files/patch-metamail_putenv.c
+++ mail/metamail/files/patch-metamail_putenv.c
@@ -1,5 +1,5 @@
 --- metamail/putenv.c.orig	Tue Oct 12 17:17:43 1993
-+++ metamail/putenv.c	Mon Dec 18 11:46:22 2006
++++ metamail/putenv.c	Sat Jul 14 17:35:44 2007
 @@ -17,6 +17,8 @@
  Cambridge, MA 02139, USA.  */
  
@@ -9,3 +9,15 @@
  #include <sys/types.h>
  #include <errno.h>
  #ifdef STDC_HEADERS
+@@ -48,7 +50,11 @@
+ /* Put STRING, which is of the form "NAME=VALUE", in the environment.  */
+ int
+ putenv (string)
++#if defined __FreeBSD__ && __FreeBSD__ <= 6
+      const char *string;
++#else
++     char *string;
++#endif
+ {
+   char *name_end = index (string, '=');
+   register size_t size;
