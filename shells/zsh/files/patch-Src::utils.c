cf7b6f9465917ef9b06b4dac0b4295e84564d1a8
Renato Botelho <garga@FreeBSD.org>
2007-07-30 16:21:42 +0000

    - Update to 4.3.4 - Pet a bit portlint - OPTION'ify - Add PCRE support OPTION off by default - Add more MASTER_SITES - Remove ?= from MAINTAINER and COMMENT since there are no more slave ports - Remove CONFLICTS since conflicting ports doesn't exist anymore - Fix iconv detection and add USE_ICONV=yes - Use PORTDOCS and PORTEXAMPLES and cleanup pkg-plist - Respect NOPORTEXAMPLES - Add csup, portsnap and portlint Completions, it was already added to zsh cvs   but add it here using a patch while new version don't come
    Reviewed by:	sergei@
    Approved by:	sergei@ (maintainer)

--- shells/zsh/files/patch-Src::utils.c
+++ shells/zsh/files/patch-Src::utils.c
@@ -1,8 +1,23 @@
---- Src/utils.c.orig	Wed Dec  4 19:39:01 2002
-+++ Src/utils.c	Wed Dec  4 19:39:18 2002
-@@ -1093,4 +1093,5 @@
-  * is unique, for use as a temporary file.      */
-  
+--- Src/utils.c.orig	Tue Apr 17 10:57:15 2007
++++ Src/utils.c	Wed Jul 25 15:13:58 2007
+@@ -1092,6 +1092,7 @@
+ time_t lastmailcheck;
+ 
+ /* the last time we checked the people in the WATCH variable */
 +extern char *_mktemp(char *);
+ 
  /**/
- mod_export char *
+ time_t lastwatch;
+@@ -4569,11 +4570,11 @@
+     int count;
+ #else
+     unsigned int wval;
++    size_t count;
+ # if defined(HAVE_NL_LANGINFO) && defined(CODESET) && defined(HAVE_ICONV)
+     iconv_t cd;
+     char inbuf[4];
+     size_t inbytes, outbytes;
+-    size_t count;
+ # endif
+ #endif
+ 
