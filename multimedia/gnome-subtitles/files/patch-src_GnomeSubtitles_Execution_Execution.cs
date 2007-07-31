7d76a5a461cbf2a36009f133d2c1362f34a23ffa
Rong-En Fan <rafan@FreeBSD.org>
2007-07-31 02:31:17 +0000

    - Update to 0.6
    PR:		ports/114752
    Submitted by:	Marcin Simonides <marcin at studio4plus.com> (maintainer)

--- multimedia/gnome-subtitles/files/patch-src_GnomeSubtitles_Execution_Execution.cs
+++ multimedia/gnome-subtitles/files/patch-src_GnomeSubtitles_Execution_Execution.cs
@@ -1,12 +1,12 @@
---- src/GnomeSubtitles/Execution/Execution.cs.bak	Thu Jun  7 20:49:51 2007
-+++ src/GnomeSubtitles/Execution/Execution.cs	Thu Jun  7 20:51:10 2007
-@@ -97,8 +97,7 @@
- 	public string SystemHelpDir {
- 		get {
- 			return Path.Combine(SystemShareDir,
--				Path.Combine("gnome",
--				Path.Combine("help", applicationID)));
-+				Path.Combine("help", applicationID));
+--- src/GnomeSubtitles/Execution/Execution.cs.orig	Thu Jun 21 22:53:01 2007
++++ src/GnomeSubtitles/Execution/Execution.cs	Thu Jul 12 17:32:18 2007
+@@ -107,8 +107,7 @@
+ 				return path;
+ 			else {
+ 				return Path.Combine(SystemShareDir,
+-							Path.Combine("gnome",
+-							Path.Combine("help", applicationID)));
++							Path.Combine("help", applicationID));
+ 			}
  		}
  	}
- 	
