2e0e66b898d5a46f3b0e9b0abb6b52197786dac1
rafan <rafan@FreeBSD.org>
2007-08-01 11:52:45 +0000

    - Update to 2.3 beta 1
    PR:		ports/115060
    Submitted by:	Yinghong.Liu <relaxbsd at gmail.com> (maintainer)

--- games/lianliankan/files/patch-src_pak_pak.c
+++ games/lianliankan/files/patch-src_pak_pak.c
@@ -1,59 +1,29 @@
---- src/pak/pak.c.orig	Sun Oct 23 04:36:59 2005
-+++ src/pak/pak.c	Thu May 25 00:54:16 2006
-@@ -249,11 +249,7 @@
- 
-     /* Set the position in the PAK file for this file to be written at */
- 	/* If not GNU system,this should be Pos = Current->dwOffset */
--#ifdef WIN32
--	Pos = Current->dwOffset;
--#else
+--- src/pak/pak.c.orig	2007-07-30 17:17:44.000000000 +0800
++++ src/pak/pak.c	2007-07-30 17:19:23.000000000 +0800
+@@ -253,7 +253,7 @@
+ #ifdef WIN32
+ 	Pos = Current->dwOffset;
+ #else
 -    Pos.__pos = Current->dwOffset; 
--#endif
-+    Pos = Current->dwOffset;
++    Pos = Current->dwOffset; 
+ #endif
      fsetpos( PAKStream, &Pos );
  
-     /* Read in the file a byte at a time, encrypt it and write to PAK */
-@@ -410,11 +406,7 @@
-     /* Set the offset in PAK to the beginning of this particular file
-        to be extracted */
- 	/* If not GNU system,this should be Pos = Current->dwOffset */
--#ifdef WIN32
--	Pos = Current->dwOffset;
--#else
+@@ -418,7 +418,7 @@
+ #ifdef WIN32
+ 	Pos = Current->dwOffset;
+ #else
 -    Pos.__pos = Current->dwOffset; 
--#endif
-+    Pos = Current->dwOffset;
++    Pos = Current->dwOffset; 
+ #endif
      fsetpos( ReadStream, &Pos );
  
-     /* Loop through the file by it's size and read from the PAK
-@@ -453,6 +445,9 @@
-   fpos_t Pos;       /* Offset in to file */
-   guchar  Buffer = 0;    /* A read buffer */
-   guint32 Count;
-+
-+  gchar tmpstr[40];
-+
-   /* Declare a temporary file table node to work from */
-   struct sFileTableEntry* Current;
- 
-@@ -476,7 +471,6 @@
-   if( !ReadStream )
-     return FALSE;
-   /* Attempt to open the write stream for creating the file */
--  gchar tmpstr[40];
-   sprintf(tmpstr,"/tmp/llk_%s",Current->szFileName);
-   WriteStream = fopen( tmpstr, "wb" );
-   if( !WriteStream )
-@@ -488,11 +482,7 @@
-   /* Set the offset in PAK to the beginning of this particular file
-      to be extracted */
- 	/* If not GNU system,this should be Pos = Current->dwOffset */
--#ifdef WIN32
--	Pos = Current->dwOffset;
--#else
+@@ -497,7 +497,7 @@
+ #ifdef WIN32
+ 	Pos = Current->dwOffset;
+ #else
 -    Pos.__pos = Current->dwOffset; 
--#endif
-+  Pos = Current->dwOffset;
++    Pos = Current->dwOffset; 
+ #endif
    fsetpos( ReadStream, &Pos );
  
-   /* Loop through the file by it's size and read from the PAK
