d315e2cf042efb7d0e3c3fb192a82b852a79a258
delphij <delphij@FreeBSD.org>
2007-08-02 05:42:21 +0000

    Allow depending on Berkeley DB 4.5

--- Mk/bsd.database.mk
+++ Mk/bsd.database.mk
@@ -196,7 +196,7 @@ CONFIGURE_ENV+=	CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}"
 
 .if defined(USE_BDB)
 
-_DB_PORTS=	2 3 40 41 42 43 44 3+ 40+ 41+ 42+ 43+ 44+
+_DB_PORTS=	2 3 40 41 42 43 44 45 3+ 40+ 41+ 42+ 43+ 44+ 45+
 # Dependence lines for different db versions
 db2_DEPENDS=	db2.0:${PORTSDIR}/databases/db2
 db3_DEPENDS=	db3.3:${PORTSDIR}/databases/db3
@@ -205,6 +205,7 @@ db41_DEPENDS=	db41.1:${PORTSDIR}/databases/db41
 db42_DEPENDS=	db-4.2.2:${PORTSDIR}/databases/db42
 db43_DEPENDS=	db-4.3.0:${PORTSDIR}/databases/db43
 db44_DEPENDS=	db-4.4.0:${PORTSDIR}/databases/db44
+db45_DEPENDS=	db-4.5.0:${PORTSDIR}/databases/db45
 # Detect db versions by finding some files
 db3_FIND=	${LOCALBASE}/include/db3/db.h
 db40_FIND=	${LOCALBASE}/include/db4/db.h
@@ -212,13 +213,16 @@ db41_FIND=	${LOCALBASE}/include/db41/db.h
 db42_FIND=	${LOCALBASE}/include/db42/db.h
 db43_FIND=	${LOCALBASE}/include/db43/db.h
 db44_FIND=	${LOCALBASE}/include/db44/db.h
+db45_FIND=	${LOCALBASE}/include/db45/db.h
 
 # For specifying [3, 40, 41, ..]+
-_DB_3P=		3 40 41 42 43 44
-_DB_40P=	40 41 42 43 44
-_DB_41P=	41 42 43 44
-_DB_42P=	42 43 44
-_DB_43P=	43 44
+_DB_3P=		3 ${_DB_40P}
+_DB_40P=	40 ${_DB_41P}
+_DB_41P=	41 ${_DB_42P}
+_DB_42P=	42 ${_DB_43P}
+_DB_43P=	43 ${_DB_44P}
+_DB_44P=	44 ${_DB_45P}
+_DB_45P=	45
 
 # Override the global WITH_BDB_VER with the
 # port specific <UNIQUENAME>_WITH_BDB_VER
@@ -342,6 +346,10 @@ BDB_LIB_DIR=		${LOCALBASE}/lib/db43
 BDB_LIB_NAME=		db-4.4
 BDB_LIB_CXX_NAME=	db_cxx-4.4
 BDB_LIB_DIR=		${LOCALBASE}/lib/db44
+.  elif ${_BDB_VER} == 45
+BDB_LIB_NAME=		db-4.5
+BDB_LIB_CXX_NAME=	db_cxx-4.5
+BDB_LIB_DIR=		${LOCALBASE}/lib/db45
 .  endif
 BDB_LIB_NAME?=		db${_BDB_VER}
 BDB_LIB_CXX_NAME?=	db${_BDB_VER}_cxx
