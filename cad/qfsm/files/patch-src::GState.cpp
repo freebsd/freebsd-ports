5ddce0f3614b1e50d11be33437ef9eee692fb5c0
Tilman Keskinoz <arved@FreeBSD.org>
2007-08-02 12:30:34 +0000

    Fix build on 64bit platforms

--- cad/qfsm/files/patch-src::GState.cpp
+++ cad/qfsm/files/patch-src::GState.cpp
@@ -1,5 +1,5 @@
---- src/GState.cpp.orig	Sat Mar  6 22:41:05 2004
-+++ src/GState.cpp	Wed Mar 17 00:07:34 2004
+--- src/GState.cpp.orig	2004-11-21 14:48:41.000000000 +0100
++++ src/GState.cpp	2007-08-02 14:07:00.000000000 +0200
 @@ -16,7 +16,7 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
@@ -9,3 +9,29 @@
  
  #include <qpen.h>
  #include <qbrush.h>
+@@ -196,9 +196,9 @@
+     t->getCPoint2(c2x, c2y);
+     info = t->getInfo();
+ 
+-    qDebug("Adresse:     %d", (int)t);
+-    qDebug("Start:       %d", (int)t->getStart());
+-    qDebug("Ende:        %d", (int)t->getEnd());
++    qDebug("Adresse:     %d", (intptr_t)t);
++    qDebug("Start:       %d", (intptr_t)t->getStart());
++    qDebug("Ende:        %d", (intptr_t)t->getEnd());
+     qDebug("Inputs:      %s", info->getInputsStrBin(/*numin*/).latin1());
+     qDebug("Outputs:     %s", info->getOutputsStrBin(/*numout*/).latin1());
+     qDebug(" ");
+@@ -215,9 +215,9 @@
+     t->getCPoint2(c2x, c2y);
+     info = t->getInfo();
+ 
+-    qDebug("Adresse:     %d", (int)t);
+-    qDebug("Start:       %d", (int)t->getStart());
+-    qDebug("Ende:        %d", (int)t->getEnd());
++    qDebug("Adresse:     %d", (intptr_t)t);
++    qDebug("Start:       %d", (intptr_t)t->getStart());
++    qDebug("Ende:        %d", (intptr_t)t->getEnd());
+     qDebug("Inputs:      %s", info->getInputsStrBin(/*numin*/).latin1());
+     qDebug("Outputs:     %s", info->getOutputsStrBin(/*numout*/).latin1());
+     qDebug(" ");
