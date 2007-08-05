67475a5aee1bcd0b639384827e0bef454395a109
Martin Wilke <miwi@FreeBSD.org>
2007-08-05 08:37:36 +0000

    - Update to 2.0.1 - Use WWWDIR
    Approved by:	maintainer implicit

--- www/eventum/files/patch-misc_monitor.php
+++ www/eventum/files/patch-misc_monitor.php
@@ -1,8 +1,11 @@
---- misc/monitor.php.orig	Thu Mar 30 21:13:13 2006
-+++ misc/monitor.php	Sat Jun 17 14:53:41 2006
+
+$FreeBSD$
+
+--- misc/monitor.php.orig
++++ misc/monitor.php
 @@ -39,17 +39,17 @@
  $required_files = array(
-     APP_PATH . 'config.inc.php' => array(
+     APP_PATH . 'config/config.php' => array(
          'check_owner'      => true,
 -        'owner'            => 'apache',
 +        'owner'            => 'www',
@@ -12,7 +15,7 @@
          'check_permission' => true,
          'permission'       => 755,
      ),
-     APP_PATH . 'setup.conf.php' => array(
+     APP_PATH . 'config/setup.php' => array(
          'check_owner'      => true,
 -        'owner'            => 'apache',
 +        'owner'            => 'www',
@@ -22,10 +25,3 @@
          'check_permission' => true,
          'permission'       => 750,
          'check_filesize'   => true,
-@@ -60,4 +60,4 @@
- Monitor::checkDatabase();
- Monitor::checkMailQueue();
- Monitor::checkIRCBot();
--?>
-\ No newline at end of file
-+?>
