7637ff99a2a5b2e787fe74744ddd0f047dc22e67
Martin Wilke <miwi@FreeBSD.org>
2007-08-05 22:20:35 +0000

    - Fix build with gcc 4.2
    PR:		115227
    Submitted by:	Adam McDougall <mcdouga9@egr.msu.edu> (maintainer)

---
+++ sysutils/gcombust/files/patch-src-optimize_usage.h
@@ -0,0 +1,10 @@
+--- src/optimize_usage.h	1999-12-20 15:23:18.000000000 -0500
++++ src/optimize_usage.h	2007-08-05 15:47:54.000000000 -0400
+@@ -16,7 +16,6 @@
+ ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+ 
+-extern GtkWidget *opt_try_harder;
+ extern long int opt_cdsize;
+ 
+ void open_optimize_size_win(GtkWidget *, gpointer);
---
+++ sysutils/gcombust/files/patch-src-rip_audio.h
@@ -0,0 +1,7 @@
+--- src/rip_audio.h	2000-12-11 13:26:41.000000000 -0500
++++ src/rip_audio.h	2007-08-05 15:48:02.000000000 -0400
+@@ -20,4 +20,3 @@
+ 
+ extern char *cdda2wav_path, *cdda_device_path, *cdparanoia_path;
+ extern char *def_rip_path;
+-extern GtkWidget *rip_path_entry;
