4ce223c888acc6069eb93b334c011493486de006
mezz <mezz@FreeBSD.org>
2007-08-05 23:27:42 +0000

    - Update to 0.5.4. - Add a patch to enable compact allocation by default. It doesn't work very   if it is disable, because of UFS2.

--- net-p2p/deluge05/files/patch-setup.py
+++ net-p2p/deluge05/files/patch-setup.py
@@ -1,43 +1,41 @@
---- setup.py.orig	Thu Jul  5 22:49:16 2007
-+++ setup.py	Thu Jul  5 22:55:47 2007
-@@ -105,7 +105,7 @@
+--- setup.py.orig	2007-08-05 13:03:56.000000000 -0500
++++ setup.py	2007-08-05 13:06:12.000000000 -0500
+@@ -101,13 +101,13 @@
+                 "-DHAVE_INCLUDE_LIBTORRENT_ASIO_SSL_STREAM_HPP=1", 
+                 "-DHAVE_INCLUDE_LIBTORRENT_ASIO_IP_TCP_HPP=1", 
+                 "-DHAVE_PTHREAD=1", "-DTORRENT_USE_OPENSSL=1", "-DHAVE_SSL=1", 
+-                "-DNDEBUG=1", "-O2"]
++                "-DNDEBUG=1"]
+     if ARCH == "x64":
+         EXTRA_COMPILE_ARGS.append("-DAMD64")
  
- 	includedirs = ['./libtorrent', './libtorrent/include', 
+     includedirs = ['./libtorrent', './libtorrent/include', 
                       './libtorrent/include/libtorrent', 
 -                     '/usr/include/python' + python_version]
 +                     '%%LOCALBASE%%/include']
  
- 	if OS == "linux":
- 		if os.WEXITSTATUS(os.system('grep -iq "Debian GNU/Linux 4.0\|Ubuntu 7.04\|Ubuntu 6.06\|Ubuntu 6.10\|Fedora Core release 6\|openSUSE 10.2\|Mandriva Linux release 2007.1\|Fedora release 7" /etc/issue')) == 0:
-@@ -117,7 +117,7 @@
- 	else:
- 		boosttype = 'mt'
- 	removals = ['-g', '-Wstrict-prototypes']
--        additions = ['-DNDEBUG', '-O2']
-+        additions = ['-DNDEBUG']
- 
- 	if python_version == '2.5':
- 		cv_opt = sysconfig.get_config_vars()["CFLAGS"]
-@@ -152,11 +152,11 @@
+     if OS == "linux":
+         if os.WEXITSTATUS(os.system('grep -iq "Debian GNU/Linux 4.0\|Ubuntu 7.04\|Ubuntu 6.06\|Ubuntu 6.10\|Fedora Core release 6\|openSUSE 10.2\|Mandriva Linux release 2007.1\|Fedora release 7\|BLAG release 60001\|Yellow Dog Linux release 5.0 (Phoenix)\|CentOS release 5 (Final)" /etc/issue')) == 0:
+@@ -150,11 +150,11 @@
  # Ubuntu possible.
  if boosttype == "nomt":
- 	librariestype = ['boost_filesystem', 'boost_date_time',
--			'boost_thread', 'z', 'pthread', 'ssl']
-+			'boost_thread', 'z', 'ssl']
- 	print 'Libraries nomt' 
+     librariestype = ['boost_filesystem', 'boost_date_time',
+-            'boost_thread', 'z', 'pthread', 'ssl']
++            'boost_thread', 'z', 'ssl']
+     print 'Libraries nomt' 
  elif boosttype == "mt":
- 	librariestype = ['boost_filesystem-mt', 'boost_date_time-mt',
--			'boost_thread-mt', 'z', 'pthread', 'ssl']
-+			'boost_thread-mt', 'z', 'ssl']
- 	print 'Libraries mt'
+     librariestype = ['boost_filesystem-mt', 'boost_date_time-mt',
+-            'boost_thread-mt', 'z', 'pthread', 'ssl']
++            'boost_thread-mt', 'z', 'ssl']
+     print 'Libraries mt'
  
  def fetchCpp():
-@@ -175,8 +175,10 @@
+@@ -173,8 +173,10 @@
  
  deluge_core = Extension('deluge_core',
                      include_dirs = includedirs,
 +                    library_dirs = ['%%LOCALBASE%%/lib'],
- 		    libraries = librariestype,
+             libraries = librariestype,
                      extra_compile_args = EXTRA_COMPILE_ARGS,
 +                    extra_link_args = ['%%PTHREAD_LIBS%%'],
                      sources = sources)
