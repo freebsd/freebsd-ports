32264e1d6ba360c3a93a7d8d1c76c65b7efa6e66
mezz <mezz@FreeBSD.org>
2007-08-07 16:37:10 +0000

    -Update to 0.80. -Fix assertion bug in torrent.c::fileByteCompleted(). [1]
    Obtained from:	its SVN [1]

--- net-p2p/transmission-cli/files/patch-version.sh
+++ net-p2p/transmission-cli/files/patch-version.sh
@@ -1,11 +1,11 @@
---- version.sh.orig	Wed Apr 18 11:24:12 2007
-+++ version.sh	Wed Apr 18 11:24:21 2007
-@@ -48,9 +48,4 @@
+--- version.sh.orig	2007-08-07 10:44:05.000000000 -0500
++++ version.sh	2007-08-07 10:44:41.000000000 -0500
+@@ -52,9 +52,4 @@
  EOF
  replace_if_differs libtransmission/version.h.new libtransmission/version.h
  
 -# Generate Info.plist from Info.plist.in
--sed -e "s/%%BUNDLE_VERSION%%/$REV/" -e "s/%%SHORT_VERSION_STRING%%/$STRING/" \
+-sed -e "s/%%BUNDLE_VERSION%%/$SVN_REVISION/" -e "s/%%SHORT_VERSION_STRING%%/$USERAGENT_PREFIX/" \
 -        < macosx/Info.plist.in > macosx/Info.plist.new
 -replace_if_differs macosx/Info.plist.new macosx/Info.plist
 -
