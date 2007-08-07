124ba4c952a3e4423b9e969802c69bc90a68f5de
Dirk Meyer <dinoex@FreeBSD.org>
2007-08-07 10:27:30 +0000

    - update to 1.7

--- mail/milter-regex/files/patch-milter-regex.c
+++ mail/milter-regex/files/patch-milter-regex.c
@@ -1,14 +1,14 @@
---- milter-regex.c.orig	Sun Mar  6 12:42:53 2005
-+++ milter-regex.c	Tue Feb 20 17:33:46 2007
-@@ -57,6 +57,7 @@
+--- milter-regex.c.orig	Sat Aug  4 00:11:48 2007
++++ milter-regex.c	Tue Aug  7 10:20:43 2007
+@@ -60,6 +60,7 @@
  
  static const char	*rule_file_name = "/etc/milter-regex.conf";
  static int		 debug = 0;
 +static int		 quiet = 0;
+ static pthread_mutex_t	 mutex;
  
  struct context {
- 	struct ruleset	*rs;
-@@ -88,6 +89,7 @@
+@@ -93,6 +94,7 @@
  
  #define USER		"_milter-regex"
  #define OCONN		"unix:/var/spool/milter-regex/sock"
@@ -16,7 +16,7 @@
  #define RCODE_REJECT	"554"
  #define RCODE_TEMPFAIL	"451"
  #define XCODE_REJECT	"5.7.1"
-@@ -520,6 +522,9 @@
+@@ -611,6 +613,9 @@
  	va_list ap;
  	char msg[8192];
  
@@ -26,7 +26,7 @@
  	va_start(ap, fmt);
  	if (context != NULL)
  		snprintf(msg, sizeof(msg), "%s: ", context->host_addr);
-@@ -556,14 +561,18 @@
+@@ -647,14 +652,18 @@
  {
  	int ch;
  	const char *oconn = OCONN;
@@ -46,7 +46,7 @@
  		switch (ch) {
  		case 'c':
  			rule_file_name = optarg;
-@@ -574,6 +583,12 @@
+@@ -665,6 +674,12 @@
  		case 'p':
  			oconn = optarg;
  			break;
@@ -59,7 +59,7 @@
  		case 'u':
  			user = optarg;
  			break;
-@@ -638,9 +653,22 @@
+@@ -734,9 +749,22 @@
  		fprintf(stderr, "daemon: %s\n", strerror(errno));
  		goto done;
  	}
