d3a56eb657e8e6a49ed0867ad5a7c501719356f1
vanilla <vanilla@FreeBSD.org>
2007-08-09 09:19:33 +0000

    Fix build on current/amd64.
    Submitted by:	kris@ & pav@ via email.

---
+++ devel/mq4cpp/files/patch-include_Trace.h
@@ -0,0 +1,11 @@
+--- include/Trace.h.orig	Sat Mar 17 01:42:59 2007
++++ include/Trace.h	Thu Aug  9 17:17:25 2007
+@@ -92,7 +92,7 @@
+ 	}
+ #else
+ #define TRACE(a) \
+-	cout << __FILE__ << "(" << __LINE__ << ") #" << (int)pthread_self() << ": " << a << endl;
++	cout << __FILE__ << "(" << __LINE__ << ") #" << pthread_self() << ": " << a << endl;
+ #endif
+ #else
+ #define TRACE(a) ;
