ef4ba19fd1549ad4bd35146662b2b48152155f8a
thierry <thierry@FreeBSD.org>
2007-08-10 17:47:20 +0000

    - Upgrade to 1.4.1   Changelog at <http://www.opencfd.co.uk/openfoam/version1.4.1.html>
    - OpenMPI is used by default, unless WITH_MPICH or WITH_LAM is set.

--- science/openfoam/files/patch-src_dynamicMesh_fvMeshDist..._fvMeshDistributeTemplates.C
+++ science/openfoam/files/patch-src_dynamicMesh_fvMeshDist..._fvMeshDistributeTemplates.C
@@ -1,14 +1,14 @@
---- src/dynamicMesh/fvMeshDistribute/fvMeshDistributeTemplates.C.orig	Tue Mar 20 21:35:42 2007
-+++ src/dynamicMesh/fvMeshDistribute/fvMeshDistributeTemplates.C	Mon Apr 16 23:09:38 2007
-@@ -307,8 +307,10 @@
+--- src/dynamicMesh/fvMeshDistribute/fvMeshDistributeTemplates.C.orig	Tue Jun 26 18:53:30 2007
++++ src/dynamicMesh/fvMeshDistribute/fvMeshDistributeTemplates.C	Sun Aug  5 18:01:13 2007
+@@ -295,8 +295,10 @@
  
          // Send all fieldNames. This has to be exactly the same set as is
          // being received!
 +
 +	// FreeBSD: following patch only for gcc-3.4, to be removed with gcc-4.2
-         const fldType& fld =
--            subsetter.baseMesh().lookupObject<fldType>(fieldNames[i]);
-+            subsetter.baseMesh().template lookupObject<fldType>(fieldNames[i]);
+         const GeoField& fld =
+-            subsetter.baseMesh().lookupObject<GeoField>(fieldNames[i]);
++            subsetter.baseMesh().template lookupObject<GeoField>(fieldNames[i]);
  
-         tmp<fldType> tsubfld = subsetter.interpolate(fld);
+         tmp<GeoField> tsubfld = subsetter.interpolate(fld);
  
