9978fc5fa6a1e598b4ace31951fb59c410abb635
Rong-En Fan <rafan@FreeBSD.org>
2007-08-11 09:42:40 +0000

    - Update to 0.0.1r20070801 - Fix build after autotools changes
    PR:		ports/115229
    Submitted by:	Phillip Neumann <pneumann at gmail.com> (maintainer)

---
+++ www/asterisk-gui/files/patch-bootstrap.sh
@@ -0,0 +1,13 @@
+--- bootstrap.sh.orig	Wed Aug  1 18:59:07 2007
++++ bootstrap.sh	Wed Aug  1 19:00:28 2007
+@@ -14,8 +14,8 @@
+ 
+ uname -s | grep -q FreeBSD
+ if [ $? = 0 ] ; then	# FreeBSD case
+-	MY_AC_VER=259
+-	MY_AM_VER=19
++	MY_AC_VER=-2.59
++	MY_AM_VER=-1.9
+ else	# linux case
+ 	MY_AC_VER=
+ 	MY_AM_VER=
