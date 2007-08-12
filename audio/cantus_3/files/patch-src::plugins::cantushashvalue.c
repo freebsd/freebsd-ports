d3a7151351d7cc24e87ac36ad4e79d58bb0053b9
Tilman Keskinoz <arved@FreeBSD.org>
2007-08-12 18:01:22 +0000

    Fix build on 64bit platforms

---
+++ audio/cantus_3/files/patch-src::plugins::cantushashvalue.c
@@ -0,0 +1,29 @@
+--- src/plugins/cantushashvalue.c.orig	2007-08-03 14:31:22.000000000 +0200
++++ src/plugins/cantushashvalue.c	2007-08-03 14:35:10.000000000 +0200
+@@ -81,7 +81,7 @@
+   g_assert(hvalue != NULL);
+   switch (type) {
+   case G_TYPE_INT:
+-    value_set_int(hvalue, (gint)value);
++    value_set_int(hvalue, (gint64)value);
+     break;
+   
+   case G_TYPE_CHAR:
+@@ -90,7 +90,7 @@
+     break;
+   
+   case G_TYPE_BOOLEAN:
+-    value_set_bool(hvalue, (gboolean)value);
++    value_set_bool(hvalue, (gint64)value);
+     break;
+   
+   case G_TYPE_POINTER:
+@@ -157,7 +157,7 @@
+   if (hvalue->type != G_TYPE_BOOLEAN)
+     g_warning("G_TYPE_BOOLEAN value requested from non-G_TYPE_BOOLEAN"
+               " container.\n");
+-  return (gboolean)hvalue->value;
++  return (gint64) hvalue->value;
+ }
+ 
+ 
---
+++ audio/cantus_3/files/patch-src::ui_gtk2::gui_preferences.cc
@@ -0,0 +1,11 @@
+--- src/ui-gtk2/gui_preferences.cc.orig	2007-08-12 19:50:46.000000000 +0200
++++ src/ui-gtk2/gui_preferences.cc	2007-08-12 19:51:17.000000000 +0200
+@@ -81,7 +81,7 @@
+   }
+   Gtk::CheckButton *check = dynamic_cast<Gtk::CheckButton*>(widget);
+   if (check) {
+-    check->set_active((gboolean)value);
++    check->set_active(value);
+     return TRUE;
+   }
+   return TRUE;
