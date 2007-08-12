35a37c03f15fd82eabc9af7a9c0b3c5636f19b8d
rafan <rafan@FreeBSD.org>
2007-08-12 11:07:41 +0000

    - Update to 2.0 - Use macro from bsd.sites.mk - Remove 4.x support
    PR:		ports/115332
    Submitted by:	Dmitry Marakasov <amdmi3 at amdmi3.ru> (maintainer)

--- games/untahris/files/patch-intface.cpp
+++ games/untahris/files/patch-intface.cpp
@@ -1,9 +1,18 @@
---- intface.cpp.orig	Mon Sep  4 19:16:15 2006
-+++ intface.cpp	Fri Sep  8 06:57:45 2006
-@@ -517,7 +517,7 @@
-       thruster.pixel[y][x] = 'P';
+--- intface.cpp.orig	Wed May  2 02:41:32 2007
++++ intface.cpp	Thu Aug  9 03:04:09 2007
+@@ -45,7 +45,7 @@
+     SDL_Quit();
      }
-   
+ 
+-  if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 8000) != 0)
++  if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 8192) != 0)
+     printf("Failed to initialize audio.\n");
+   else {
+     Mix_AllocateChannels(16);
+@@ -653,7 +653,7 @@
+   Ic(icBazooka).fillWith("0", d.Res);
+   Ic(icBazooka).gradFill(d.Res*4/8-1, 0, d.Res*4/8, d.Res*3/4, 0x80FFFFFF, 0xFFFFFFFF);
+ 
 -  char buf[64];
 +  char buf[1024];
    sprintf(buf, "graph%d.unt", d.Res);
