e3546b66bb26b09a5c5cfed8807bbcd74a2fc415
mezz <mezz@FreeBSD.org>
2007-08-14 16:36:18 +0000

    The tarball has been rerolled again. Somehow I have lost the second tarball to compare with the third tarball, so I have no idea what kind of change. I have asked in the forum[1] and the developer has replied. He said that he does not remember what kind of change in there, but he promise me that it's safe. Since nobody knew, so I am going to bump the PORTREVISION in case.
    The developer said that he will post in the news next time if he reroll the
    tarball to give us the headup the reason of reroll. I think it's fair enough.
    
    [1] http://forum.deluge-torrent.org/viewtopic.php?f=8&t=309
    
    Reported by:	krismail

--- net-p2p/deluge/files/patch-setup.py
+++ net-p2p/deluge/files/patch-setup.py
@@ -1,14 +1,13 @@
---- setup.py.orig	2007-08-11 22:13:02.000000000 -0500
-+++ setup.py	2007-08-11 22:16:17.000000000 -0500
-@@ -102,13 +102,12 @@
+--- setup.py.orig	2007-08-14 11:14:32.000000000 -0500
++++ setup.py	2007-08-14 11:16:15.000000000 -0500
+@@ -101,13 +101,13 @@
+                 "-DHAVE_INCLUDE_LIBTORRENT_ASIO_SSL_STREAM_HPP=1", 
                  "-DHAVE_INCLUDE_LIBTORRENT_ASIO_IP_TCP_HPP=1", 
                  "-DHAVE_PTHREAD=1", "-DTORRENT_USE_OPENSSL=1", "-DHAVE_SSL=1", 
-                 "-DNDEBUG=1"]
--# Done through debian/rules, as all arches are not handled
--#    if ARCH == "x64":
--#        EXTRA_COMPILE_ARGS.append("-DAMD64")
-+    if ARCH == "x64":
-+        EXTRA_COMPILE_ARGS.append("-DAMD64")
+-                "-DNDEBUG=1", "-O2"]
++                "-DNDEBUG=1"]
+     if ARCH == "x64":
+         EXTRA_COMPILE_ARGS.append("-DAMD64")
  
      includedirs = ['./libtorrent', './libtorrent/include', 
                       './libtorrent/include/libtorrent', 
@@ -17,7 +16,7 @@
  
      if OS == "linux":
          if os.WEXITSTATUS(os.system('grep -iq "Debian GNU/Linux 4.0\|Ubuntu 7.04\|Ubuntu 6.06\|Ubuntu 6.10\|Fedora Core release 6\|openSUSE 10.2\|Mandriva Linux release 2007.1\|Fedora release 7\|BLAG release 60001\|Yellow Dog Linux release 5.0 (Phoenix)\|CentOS release 5 (Final)" /etc/issue')) == 0:
-@@ -154,11 +153,11 @@
+@@ -150,11 +150,11 @@
  # Ubuntu possible.
  if boosttype == "nomt":
      librariestype = ['boost_filesystem', 'boost_date_time',
@@ -31,7 +30,7 @@
      print 'Libraries mt'
  
  def fetchCpp():
-@@ -177,8 +176,10 @@
+@@ -173,8 +173,10 @@
  
  deluge_core = Extension('deluge_core',
                      include_dirs = includedirs,
