9439d5d01ff6b3b0c70555f762d6f43a8576b9de
acm <acm@FreeBSD.org>
2007-08-19 18:58:55 +0000

    - Update to 0.13.2 - Remove obsolete patch files - Add devel/ode like a dependency

--- graphics/py-soya3d/files/patch-setup.py
+++ graphics/py-soya3d/files/patch-setup.py
@@ -1,29 +1,10 @@
---- setup.py	Mon Aug 21 00:22:53 2006
-+++ setup.py	Mon Aug 21 00:24:52 2006
-@@ -99,6 +100,7 @@
- 	if USE_OPENAL:
- 		print "Sound support (with OpenAL) enabled..."
+--- setup.py.orig	Sun May 13 10:54:49 2007
++++ setup.py	Fri Aug 17 16:32:16 2007
+@@ -129,6 +129,7 @@
+ 		DEFINES.append(('SOYA_MACOSX',1))
+ 	else:
  		LIBS.append("openal")
 +		LIBS.append("alut")
- 		CONFIG_PXD_FILE.write("""include "sound/al.pxd"\n""")
- 		CONFIG_PYX_FILE.write("""include "sound/sound.pyx"\n""")
- 	else:
-@@ -133,14 +135,14 @@
- 			print "ODE and OPCODE have already been compiled; if you want to recompile them do:  cd %s ; make clean" % ODE_DIR
- 		else:
- 			print "Building ODE and OPCODE from %s" % ODE_DIR
--			do("cd %s ; make clean" % ODE_DIR)
--			do("cd %s ; make configure" % ODE_DIR)
--			do("cd %s ; make" % ODE_DIR)
-+			do("cd %s ; gmake clean" % ODE_DIR)
-+			do("cd %s ; gmake configure" % ODE_DIR)
-+			do("cd %s ; gmake" % ODE_DIR)
- 			print "ODE and OPCODE built successfully !"
- 			
- 	elif SDISTING:
- 		# Clean ODE, to remove configuration files and binaries
--		do("cd %s ; make clean" % ODE_DIR)
-+		do("cd %s ; gmake clean" % ODE_DIR)
- 	
- 	
- if HAVE_PYREX:
+ 
+ 
+ if "darwin" in sys.platform:
