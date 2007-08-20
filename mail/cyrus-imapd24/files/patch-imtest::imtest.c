78e8d615d345687231fa3b696512bbe9d2500607
ume <ume@FreeBSD.org>
2007-08-20 15:21:49 +0000

    Update to 2.3.9.
    Upgrading from 2.3.8
    
      * The default value of the allowplaintext option has been changed to
        disabled (0). If you need to allow cleartext passwords on the wire,
        then you will have to explicitly enable the allowplaintext option
        in imapd.conf.

--- mail/cyrus-imapd24/files/patch-imtest::imtest.c
+++ mail/cyrus-imapd24/files/patch-imtest::imtest.c
@@ -2,14 +2,6 @@ Index: imtest/imtest.c
 diff -u imtest/imtest.c.orig imtest/imtest.c
 --- imtest/imtest.c.orig	Sun Jul 13 05:10:56 2003
 +++ imtest/imtest.c	Fri Jul 18 00:52:29 2003
-@@ -51,6 +51,7 @@
- #include <sys/stat.h>
- #include <fcntl.h>
- 
-+#include <limits.h>
- #include <stdlib.h>
- #include <stdio.h>
- #include <string.h>
 @@ -419,8 +420,8 @@
  }
  
