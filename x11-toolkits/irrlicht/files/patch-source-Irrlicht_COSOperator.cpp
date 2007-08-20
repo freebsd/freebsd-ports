eecf6133e97eb5ff06dee17321190443aef5be6d
acm <acm@FreeBSD.org>
2007-08-20 07:02:28 +0000

    - Update to 1.3.1

--- x11-toolkits/irrlicht/files/patch-source-Irrlicht_COSOperator.cpp
+++ x11-toolkits/irrlicht/files/patch-source-Irrlicht_COSOperator.cpp
@@ -1,20 +1,24 @@
---- source/Irrlicht/COSOperator.cpp	Tue Mar 20 09:46:39 2007
-+++ source/Irrlicht/COSOperator.cpp	Tue Mar 20 09:50:49 2007
-@@ -13,6 +13,8 @@
- 
- #ifdef LINUX
+--- source/Irrlicht/COSOperator.cpp	Sun Aug 19 20:14:42 2007
++++ source/Irrlicht/COSOperator.cpp	Sun Aug 19 20:19:35 2007
+@@ -10,12 +10,9 @@
+ #else
+ #include <string.h>
  #include <unistd.h>
-+#include <sys/types.h>
-+#include <sys/sysctl.h>
+-#ifdef MACOSX
+-#include "OSXClipboard.h"
+ #include <sys/types.h>
+ #include <sys/sysctl.h>
  #endif
+-#endif
  
- #ifdef MACOSX
-@@ -167,7 +169,7 @@
+ namespace irr
+ {
+@@ -156,7 +153,7 @@
  	_IRR_IMPLEMENT_MANAGED_MARSHALLING_BUGFIX;
  	return true;
  
--#elif defined(LINUX) // || defined(MACOSX)
-+#elif defined(LINUX) && !defined(__FreeBSD__) // || defined(MACOSX)
+-#elif defined(_IRR_POSIX_API_) // || defined(MACOSX)
++#elif defined(_IRR_POSIX_API_) && !defined(__FreeBSD__)// || defined(MACOSX)
          long ps = sysconf(_SC_PAGESIZE);
          long pp = sysconf(_SC_PHYS_PAGES);
          long ap = sysconf(_SC_AVPHYS_PAGES);
