d1ae94c9500f63a9c1956bab306b09b328511558
Roman Bogorodskiy <novel@FreeBSD.org>
2007-08-21 18:31:58 +0000

    Fix build on i386.
    PR:		115642
    Submitted by:	Barbara <barbara.xxx1975@libero.it>

--- sysutils/conky/files/patch-src-freebsd.c
+++ sysutils/conky/files/patch-src-freebsd.c
@@ -1,5 +1,5 @@
 --- src/freebsd.c.orig	2007-08-05 08:48:13.000000000 +0400
-+++ src/freebsd.c	2007-08-19 23:15:54.000000000 +0400
++++ src/freebsd.c	2007-08-21 22:22:37.000000000 +0400
 @@ -43,6 +43,14 @@
  #define	FREEBSD_DEBUG
  #endif
@@ -15,3 +15,12 @@
  inline void proc_find_top(struct process **cpu, struct process **mem);
  
  u_int64_t diskio_prev = 0;
+@@ -832,7 +840,7 @@
+ 	int fd;
+ 	struct apm_info info;
+ 
+-	out = (char *)calloc(16, sizeof (char));
++	(char *)out = (char *)calloc(16, sizeof (char));
+ 
+ 	fd = open(APMDEV, O_RDONLY);
+ 	if (fd < 0) {
