05369f86c8cfb7d0cf6a4df0140e3f9398bf85d4
rafan <rafan@FreeBSD.org>
2007-08-23 14:52:53 +0000

    - Update to 2.0.3
    PR:		ports/115724
    Submitted by:	Ivan Lago <ivan.lago at ifom-ieo-campus.it> (maintainer)

--- sysutils/syslog-ng2/files/patch-syslog-ng.h
+++ sysutils/syslog-ng2/files/patch-syslog-ng.h
@@ -1,15 +1,16 @@
---- src/syslog-ng.h.orig	Wed Nov  8 14:45:18 2006
-+++ src/syslog-ng.h	Wed Nov  8 14:45:44 2006
-@@ -38,10 +38,10 @@
+--- src/syslog-ng.h.orig	Wed Aug 22 15:50:25 2007
++++ src/syslog-ng.h	Wed Aug 22 15:55:18 2007
+@@ -38,11 +38,11 @@
  
  #if ENABLE_DEBUG
  #define PATH_SYSLOG_NG_CONF     "syslog-ng.conf"
 -#define PATH_PIDFILE            "syslog-ng.pid"
 +#define PATH_PIDFILE            "syslog.pid"
+ #define PATH_PERSIST_CONFIG     "syslog-ng.persist"
  #else
  #define PATH_SYSLOG_NG_CONF     PATH_SYSCONFDIR "/syslog-ng.conf"
 -#define PATH_PIDFILE            "/var/run/syslog-ng.pid"
 +#define PATH_PIDFILE            "/var/run/syslog.pid"
+ #define PATH_PERSIST_CONFIG     PATH_LOCALSTATEDIR "/syslog-ng.persist"
  #endif
  
- #define LOG_PRIORITY_LISTEN 100
