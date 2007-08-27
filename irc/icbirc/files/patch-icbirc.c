1313b55d195f75bc59c75559da567699c9efd428
hrs <hrs@FreeBSD.org>
2007-08-27 20:14:33 +0000

    Update to 1.8.

--- irc/icbirc/files/patch-icbirc.c
+++ irc/icbirc/files/patch-icbirc.c
@@ -1,22 +1,13 @@
-
-$FreeBSD$
-
---- icbirc.c.orig	Wed Aug 20 23:55:59 2003
-+++ icbirc.c	Sat Feb 14 23:09:01 2004
-@@ -318,12 +318,12 @@
- done:
- 	if (server_fd >= 0)
- 		close(server_fd);
--	printf("(%u seconds, %lu:%lu bytes)\n",
--	    time(NULL) - t, bytes_out, bytes_in);
-+	printf("(%lu seconds, %lu:%lu bytes)\n",
-+	    (long)time(NULL) - t, bytes_out, bytes_in);
+--- icbirc.c.orig	Fri Nov 19 06:14:28 2004
++++ icbirc.c	Tue Aug 28 05:12:05 2007
+@@ -322,8 +322,8 @@
+ 	    (unsigned long)(time(NULL) - t), bytes_out, bytes_in);
  	if (terminate_client)
  		irc_send_notice(client_fd, "Closing connection "
 -		    "(%u seconds, %lu:%lu bytes)",
 -		    time(NULL) - t, bytes_out, bytes_in);
 +		    "(%lu seconds, %lu:%lu bytes)",
-+		    (long)time(NULL) - t, bytes_out, bytes_in);
++		     (unsigned long)(time(NULL) - t), bytes_out, bytes_in);
  }
  
  int
