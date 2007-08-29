99075ec56e405f1c5459e39ea853d56f52c46eda
clsung <clsung@FreeBSD.org>
2007-08-29 07:54:00 +0000

    - Upgrade to 0.90
    PR:		ports/115916
    Submitted by:	maintainer (Matthew D. Fuller)

--- devel/bzrtools/files/patch-shelf.py
+++ devel/bzrtools/files/patch-shelf.py
@@ -1,11 +1,11 @@
---- shelf.py.bak	Tue Feb 27 16:17:33 2007
-+++ shelf.py	Tue Feb 27 16:40:17 2007
+--- shelf.py.orig	Tue Aug 14 19:01:41 2007
++++ shelf.py	Mon Aug 20 17:16:40 2007
 @@ -250,7 +250,7 @@
          if reverse:
              args.append('-R')
          if dry_run:
 -            args.append('--dry-run')
 +            args.append('--check')
-             stdout = stderr = subprocess.PIPE
+             stderr = subprocess.PIPE
          else:
-             stdout = stderr = None
+             stderr = None
