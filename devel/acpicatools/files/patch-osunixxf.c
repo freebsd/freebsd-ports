ed7c21710ab26c6a64dc7dc2128a20986e480ef9
Tilman Keskinoz <arved@FreeBSD.org>
2007-09-02 14:11:33 +0000

    Fix build on amd64/ia64
    PR:		115897
    Submitted by:	Artem Naluzhny <tut@nhamon.com.ua>

--- devel/acpicatools/files/patch-osunixxf.c
+++ devel/acpicatools/files/patch-osunixxf.c
@@ -1,5 +1,32 @@
---- acpica-unix-20030523/osunixxf.c.orig	Sat May 24 09:11:02 2003
-+++ acpica-unix-20030523/osunixxf.c	Thu Jun 12 17:03:26 2003
+--- acpica-unix-20030523/osunixxf.c.orig	2003-05-24 03:11:02.000000000 +0300
++++ acpica-unix-20030523/osunixxf.c	2007-08-28 15:57:49.000000000 +0300
+@@ -452,7 +452,7 @@
+ ACPI_STATUS
+ AcpiOsMapMemory (
+     ACPI_PHYSICAL_ADDRESS   where,
+-    UINT32                  length,
++    ACPI_SIZE               length,
+     void                    **there)
+ {
+     *there = (void *) (UINT32) where;
+@@ -478,7 +478,7 @@
+ void
+ AcpiOsUnmapMemory (
+     void                    *where,
+-    UINT32                  length)
++    ACPI_SIZE               length)
+ {
+ 
+     return;
+@@ -499,7 +499,7 @@
+ 
+ void *
+ AcpiOsAllocate (
+-    UINT32                  size)
++    ACPI_SIZE               size)
+ {
+     void                    *Mem;
+ 
 @@ -815,7 +815,7 @@
      UINT32                  milliseconds)
  {
