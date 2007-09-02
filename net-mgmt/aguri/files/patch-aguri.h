1b360cbdbcbbbdc828e579aaadc10c1f2f3b3fb9
Tilman Keskinoz <arved@FreeBSD.org>
2007-09-02 13:55:44 +0000

    Fix build with gcc42
    PR:		115791
    Submitted by:	Hung-Yi Chen <gaod@hychen.org>
    Approved by:	Yann Berthier <yb@bashibuzuk.net>
    
    While here replace USE_XLIB with USE_XORG

---
+++ net-mgmt/aguri/files/patch-aguri.h
@@ -0,0 +1,10 @@
+--- aguri.h.orig        2003-03-12 22:09:41.000000000 +0800
++++ aguri.h     2007-08-25 02:33:28.000000000 +0800
+@@ -32,7 +32,6 @@
+ #define        AGURI_VERSION           "0.7"
+
+ extern char *fmt_string;
+-extern int read_count;
+ extern struct timeval start_time, end_time;
+ extern int debug;
+ extern int time_slot;
