2c37001fc9589b03abe941a8be677e90cdcee780
marcus <marcus@FreeBSD.org>
2007-09-03 22:50:59 +0000

    Fix a really stupid mistake on my part, and correct a crash when opening files over NFS.
    PR:		115969
    Submitted by:	Oleg Sharoiko <os@rsu.ru>

--- devel/anjuta/files/patch-plugins_document-manager_anjuta-docman.c
+++ devel/anjuta/files/patch-plugins_document-manager_anjuta-docman.c
@@ -1,6 +1,6 @@
---- plugins/document-manager/anjuta-docman.c.orig	Sun Jul  8 03:12:10 2007
-+++ plugins/document-manager/anjuta-docman.c	Sun Jul  8 03:20:22 2007
-@@ -902,6 +902,7 @@ anjuta_docman_goto_file_line_mark (Anjut
+--- plugins/document-manager/anjuta-docman.c.orig	Thu Jun  7 14:56:53 2007
++++ plugins/document-manager/anjuta-docman.c	Fri Aug 31 16:53:19 2007
+@@ -902,6 +902,7 @@
  	const gchar *linenum;
  	glong lineno;
  	gboolean is_local_uri;
@@ -8,19 +8,22 @@
  	gchar *normalized_path = NULL;
  	
  	IAnjutaEditor *te;
-@@ -926,9 +927,9 @@ anjuta_docman_goto_file_line_mark (Anjut
+@@ -926,9 +927,11 @@
  	/* Get the normalized file path for comparision */
  	is_local_uri = gnome_vfs_uri_is_local (vfs_uri);
  	if (is_local_uri)
 -		normalized_path = realpath (gnome_vfs_uri_get_path (vfs_uri), NULL);
-+		normalized_path = realpath (gnome_vfs_uri_get_path (vfs_uri), normalized_path_buf);
- 	if (normalized_path == NULL)
+-	if (normalized_path == NULL)
 -		normalized_path = g_strdup (uri);
-+		(void) strlcpy (normalized_path, uri, PATH_MAX);
++		normalized_path = realpath (gnome_vfs_uri_get_path (vfs_uri), normalized_path_buf);
++	if (normalized_path == NULL) {
++		(void) strlcpy (normalized_path_buf, uri, PATH_MAX);
++		normalized_path = normalized_path_buf;
++	}
  	
  	gnome_vfs_uri_unref (vfs_uri);
  	/* g_free(filename); */
-@@ -942,6 +943,7 @@ anjuta_docman_goto_file_line_mark (Anjut
+@@ -942,6 +945,7 @@
  		AnjutaDocmanPage *page;
  		gboolean te_is_local_uri;
  		gchar *te_uri;
@@ -28,19 +31,22 @@
  		gchar *te_normalized_path = NULL;
  		
  		page = (AnjutaDocmanPage *) node->data;
-@@ -959,9 +961,9 @@ anjuta_docman_goto_file_line_mark (Anjut
+@@ -959,9 +963,11 @@
  		te_is_local_uri = gnome_vfs_uri_is_local (vfs_uri);
  		if (te_is_local_uri)
  			te_normalized_path = realpath (gnome_vfs_uri_get_path (vfs_uri),
 -										   NULL);
-+										   te_normalized_path_buf);
- 		if (te_normalized_path == NULL)
+-		if (te_normalized_path == NULL)
 -			te_normalized_path = g_strdup (te_uri);
-+			(void) strlcpy (te_normalized_path, te_uri, PATH_MAX);
++										   te_normalized_path_buf);
++		if (te_normalized_path == NULL) {
++			(void) strlcpy (te_normalized_path_buf, te_uri, PATH_MAX);
++			te_normalized_path = te_normalized_path_buf;
++		}
  		gnome_vfs_uri_unref (vfs_uri);
  		
  		if (strcmp (normalized_path, te_normalized_path) == 0)
-@@ -982,12 +984,9 @@ anjuta_docman_goto_file_line_mark (Anjut
+@@ -982,12 +988,9 @@
  			an_file_history_push (te_uri, lineno);
  			g_free (uri);
  			g_free (te_uri);
@@ -53,7 +59,7 @@
  		node = g_list_next (node);
  	}
  	te = anjuta_docman_add_editor (docman, uri, NULL);
-@@ -1006,7 +1005,6 @@ anjuta_docman_goto_file_line_mark (Anjut
+@@ -1006,7 +1009,6 @@
  		}
  	}
  	g_free (uri);
