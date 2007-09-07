9ec4e33ebcdf915799466d7659da064fa701ca30
Rong-En Fan <rafan@FreeBSD.org>
2007-09-07 07:33:12 +0000

    - Add missing file for 1.3.0 update

---
+++ graphics/lib3ds/files/patch-ltmain.sh
@@ -0,0 +1,15 @@
+--- ltmain.sh.orig	Thu Aug 23 10:23:45 2007
++++ ltmain.sh	Thu Aug 23 10:23:58 2007
+@@ -6038,10 +6038,12 @@
+ 	fi
+ 
+ 	# Install the pseudo-library for information purposes.
++	if /usr/bin/false ; then
+ 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	instname="$dir/$name"i
+ 	$show "$install_prog $instname $destdir/$name"
+ 	$run eval "$install_prog $instname $destdir/$name" || exit $?
++	fi
+ 
+ 	# Maybe install the static library, too.
+ 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
