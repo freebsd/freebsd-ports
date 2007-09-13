1a3e25e4f91104d94e1e510910e49a65ef83a4bd
miwi <miwi@FreeBSD.org>
2007-09-13 18:15:58 +0000

    - Update to PyQt-4.3
    Submitted by:	Danny Pansters <danny@ricin.com> (maintainer via privat mail)

---
+++ devel/py-qt4-core/files/Qt.py
@@ -0,0 +1,49 @@
+# Replacement for the composite module PyQt4.Qt for FreeBSD
+# Provides namespace for all installed PyQt4 modules by importing them
+
+from PyQt4.QtCore import *
+
+try:
+    from PyQt4.QtGui import *
+except ImportError:
+    pass
+try:
+    from PyQt4.QtXml import *
+except ImportError:
+    pass
+try:
+    from PyQt4.QtAssistant import *
+except ImportError:
+    pass
+try:
+    from PyQt4.QtDesigner import *
+except ImportError:
+    pass
+try:
+    from PyQt4.QtNetwork import *
+except ImportError:
+    pass
+try:
+    from PyQt4.QtOpenGL import *
+except ImportError:
+    pass
+try:
+    from PyQt4.QtScript import *
+except ImportError:
+    pass
+try:
+    from PyQt4.QtSql import *
+except ImportError:
+    pass
+try:
+    from PyQt4.QtSvg import *
+except ImportError:
+    pass
+try:
+    from PyQt4.QtTest import *
+except ImportError:
+    pass
+try:
+    from PyQt4.Qsci import *
+except:
+    pass
