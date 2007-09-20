51e5794fa02c75828ed051b1bed41bed81e877fd
Martin Wilke <miwi@FreeBSD.org>
2007-09-20 02:10:56 +0000

    - Update to 0.2.8-RC1
    PR:		116344
    Submitted by:	Pavel I Volkov <pol@opk.ru> (maintainer)

--- emulators/dynamips-devel/files/patch-dev_vtty.c
+++ emulators/dynamips-devel/files/patch-dev_vtty.c
@@ -1,5 +1,5 @@
---- dev_vtty.c.orig	Fri Apr  6 16:44:07 2007
-+++ dev_vtty.c	Fri Apr  6 19:43:37 2007
+--- dev_vtty.c.orig	Fri Sep  7 12:03:11 2007
++++ dev_vtty.c	Fri Sep 14 10:24:02 2007
 @@ -24,6 +24,8 @@
  #include <fcntl.h>
  #include <errno.h>
@@ -13,21 +13,18 @@
  #include "dev_c2600.h"
  #include "dev_vtty.h"
  
-+extern char hypervisor_ip_address[];
++extern char *hypervisor_ip_address;
 +
  /* VTTY list */
  static pthread_mutex_t vtty_list_mutex = PTHREAD_MUTEX_INITIALIZER;
  static vtty_t *vtty_list = NULL;
-@@ -132,7 +136,11 @@
+@@ -132,7 +136,8 @@
  
     memset(&serv,0,sizeof(serv));
     serv.sin_family = AF_INET;
 -   serv.sin_addr.s_addr = htonl(INADDR_ANY);
-+   if (strlen(hypervisor_ip_address) != 0 &&
-+	inet_addr(hypervisor_ip_address) != INADDR_NONE)
-+		serv.sin_addr.s_addr = inet_addr(hypervisor_ip_address);
-+   else
-+	serv.sin_addr.s_addr = htonl(INADDR_ANY);
++   if (hypervisor_ip_address && strlen(hypervisor_ip_address))
++      serv.sin_addr.s_addr = inet_addr(hypervisor_ip_address);
     serv.sin_port = htons(vtty->tcp_port);
  
     if (bind(vtty->accept_fd,(struct sockaddr *)&serv,sizeof(serv)) < 0) {
