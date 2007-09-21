91ffafb1a0560b9a8021b742c9141a15ba77a5c0
maho <maho@FreeBSD.org>
2007-09-21 23:38:52 +0000

    Update to 1.7.

--- cad/calculix/files/patch-cgx_VER+src+cgx.h
+++ cad/calculix/files/patch-cgx_VER+src+cgx.h
@@ -1,11 +1,11 @@
---- cgx_1.6/src/cgx.h.orig	Sun Feb 19 22:07:26 2006
-+++ cgx_1.6/src/cgx.h	Sun Feb 19 22:09:50 2006
+--- cgx_1.7/src/cgx.h.orig	Sun Feb 19 22:07:26 2006
++++ cgx_1.7/src/cgx.h	Sun Feb 19 22:09:50 2006
 @@ -86,8 +86,8 @@
                               "/yaprod/cae/application/CalculiX/doc/ccx/ccx.html"}
    #endif
  #else
--  #define     HELPFILE        {"/usr/local/CalculiX/cgx_1.6/doc/cgx/cgx.html",\
--    "/usr/local/CalculiX/ccx_1.6/doc/ccx/ccx.html"}
+-  #define     HELPFILE        {"/usr/local/CalculiX/cgx_1.7/doc/cgx/cgx.html",\
+-    "/usr/local/CalculiX/ccx_1.7/doc/ccx/ccx.html"}
 +  #define     HELPFILE        {"%%DOCSDIR%%/cgx/cgx.html",\
 +    "%%DOCSDIR%%/ccx/ccx.html"}
  #endif
--- cad/calculix/files/patch-cgx_VER+src+uselibSNL.cpp
+++ cad/calculix/files/patch-cgx_VER+src+uselibSNL.cpp
@@ -1,5 +1,5 @@
---- cgx_1.6/src/uselibSNL.cpp.orig	Mon Feb 26 17:01:57 2007
-+++ cgx_1.6/src/uselibSNL.cpp	Sun Mar 25 14:27:21 2007
+--- cgx_1.7/src/uselibSNL.cpp.orig	Mon Feb 26 17:01:57 2007
++++ cgx_1.7/src/uselibSNL.cpp	Sun Mar 25 14:27:21 2007
 @@ -27,7 +27,7 @@
  #include <extUtil.h>
  #include "uselibSNL.h"
