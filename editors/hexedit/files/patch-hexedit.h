8b9e57c1de8d1876e5a861214e7d63d42e695c23
edwin <edwin@FreeBSD.org>
2007-09-24 12:32:54 +0000

    [PATCH] update editors/hexedit to 1.2.12
    	This patch updates editors/hexedit to 1.2.12. Additionally
    	included is a patch (patch-allowwhitespaces) which allows
    	a hex search pattern to contain whitespaces (e.g. A1 B2 C3
    	instead of A1B2C3).
    
    PR:		ports/101078
    Submitted by:	Andre Albsmeier <Andre.Albsmeier@siemens.com>
    Approved by:	maintainer timeout

---
+++ editors/hexedit/files/patch-hexedit.h
@@ -0,0 +1,10 @@
+--- hexedit.h.ORI	Tue Nov  8 17:16:47 2005
++++ hexedit.h	Sat Jul 29 15:23:36 2006
+@@ -5,6 +5,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
++#include <sys/param.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #if HAVE_FCNTL_H
