fc22f851b1f5281ddb8bc6037d1fadfe69880dff
Rong-En Fan <rafan@FreeBSD.org>
2007-09-25 08:53:49 +0000

    - When fltk is not installed, scripts/check-fltk-threads.sh misbehaves   due to not checking existence of fltk-config before using it. Fix that   by checking existence before using.
    PR:             ports/116623
    Submitted by:   bgruber <knightbg at yahoo.com>
    Approved by:    Martin Tournoij <carpetsmoker at xs4all.nl> (maintainer)

--- audio/csound/scripts/check-fltk-threads.sh
+++ audio/csound/scripts/check-fltk-threads.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ -z "$(fltk-config --ldflags | grep pthread)" ]; then
-	echo "FLTK does not have threading support enabled.e"
+if [ -x ${FLTKBASE}/bin/fltk-config ] && [ -z "$(fltk-config --ldflags | grep pthread)" ]; then
+	echo "FLTK does not have threading support enabled."
 	echo "Please remove the x11-toolkits/fltk port first and recompile csound again"
 	echo "The csound port will automatically install the x11-toolkits/fltk-threads port."
 	exit 1
