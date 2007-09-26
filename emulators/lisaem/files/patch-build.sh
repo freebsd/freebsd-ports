07eb9ce15a1e5aa74d788c6d9bbed31afcd78219
Edward Tomasz Napierala <trasz@FreeBSD.org>
2007-09-26 15:40:05 +0000

    - Update to 1.2.0.

--- emulators/lisaem/files/patch-build.sh
+++ emulators/lisaem/files/patch-build.sh
@@ -1,17 +1,17 @@
---- build.sh.orig	Thu Jul  5 14:49:15 2007
-+++ build.sh	Thu Jul  5 14:51:42 2007
-@@ -422,8 +422,8 @@
+--- build.sh.orig	2007-07-10 05:23:23.000000000 +0200
++++ build.sh	2007-09-25 08:25:06.000000000 +0200
+@@ -426,8 +426,8 @@
  then
- 
-  WXCONFIGFLAGS=`wx-config  --cppflags`
+  # many thanks to David Cecchin for finding the unicode issues fixed below.
+  WXCONFIGFLAGS=`wx-config  --cppflags --unicode=no `
 - CFLAGS="-I. -I../include -I../cpu68k -I../wxui $WXCONFIGFLAGS $WITHOPTIMIZE $WITHDEBUG"
 - CXXFLAGS="-I. -I../include -I../cpu68k -I../wxui $WXCONFIGFLAGS $WITHOPTIMIZE $WITHDEBUG"
 + CFLAGS+=" -I. -I../include -I../cpu68k -I../wxui $WXCONFIGFLAGS"
 + CXXFLAGS+=" -I. -I../include -I../cpu68k -I../wxui $WXCONFIGFLAGS"
-  LINKOPTS="`wx-config $STATIC  --libs --linkdeps --cppflags`"
+  LINKOPTS="`wx-config $STATIC  --unicode=no --libs --linkdeps --cppflags`"
   [ -z "$CC" ] && CC=gcc
   [ -z "$CXX" ] && CXX=g++
-@@ -759,7 +759,7 @@
+@@ -768,7 +768,7 @@
  #echo $CXX $GCCSTATIC  $WITHDEBUG -o lisaem  $LIST ../generator/libgenerator.a ../cpu68k/lib68k.a $LINKOPTS $SYSLIBS
  if [ -z "$WITHWXUI" ]
  then
