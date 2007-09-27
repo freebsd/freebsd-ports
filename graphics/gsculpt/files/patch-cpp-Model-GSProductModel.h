3da80328d9e2a5229baa54f51f4a4f0ed93f72cd
alepulver <alepulver@FreeBSD.org>
2007-09-27 19:07:05 +0000

    - Update to 0.99.46.2. - Use SF macro in MASTER_SITES [1]. - Fix building on non-i386 instead of adding NOT_FOR_ARCHS [1].
    PR:		ports/115138
    Submitted by:	Martin Tournoij <carpetsmoker@xs4all.nl> (maintainer)
    Reworked by:	alepulver (myself) [1]

---
+++ graphics/gsculpt/files/patch-cpp-Model-GSProductModel.h
@@ -0,0 +1,18 @@
+--- cpp/Model/GSProductModel.h.orig	Sun May  6 08:28:39 2007
++++ cpp/Model/GSProductModel.h	Thu Jul 12 23:09:13 2007
+@@ -10,7 +10,6 @@
+ 
+ #include <string>
+ 
+-#include <boost/python.hpp>
+ 
+ #include <Util/UniqueID.h>
+ 
+@@ -26,6 +25,7 @@
+ #include <Model/VisualPlane.h>
+ 
+ #include <Product/GSProduct.h>
++#include <boost/python.hpp>
+ 
+ 
+ 
