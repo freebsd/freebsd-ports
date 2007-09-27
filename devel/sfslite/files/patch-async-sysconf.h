fb695bd164a8ef92f5cae593ab8c7d7fd2758fa6
Alejandro Pulver <alepulver@FreeBSD.org>
2007-09-27 19:53:54 +0000

    - Update to version 1.2.2.
    PR:		ports/115952
    Submitted by:	ports@okws.org (maintainer)

--- devel/sfslite/files/patch-async-sysconf.h
+++ devel/sfslite/files/patch-async-sysconf.h
@@ -1,21 +1,25 @@
---- async/sysconf.h.orig	2005-11-01 16:20:33.000000000 +0000
-+++ async/sysconf.h	2007-07-12 22:38:10.000000000 +0000
-@@ -494,11 +494,12 @@
- #endif /* HAVE_MEMORY_H */
+--- async/sysconf.h.orig	Thu Aug 30 16:57:16 2007
++++ async/sysconf.h	Fri Sep 14 01:46:21 2007
+@@ -495,15 +495,16 @@
  #include <dmalloc.h>
+ 
  #undef memcpy
 +#undef memmove
  #undef xfree
+ 
+ #if DMALLOC_VERSION_MAJOR < 5  || \
+      (DMALLOC_VERSION_MAJOR == 5 && DMALLOC_VERSION_MINOR < 5)
+ 
  #define memcpy(to, from, len) \
 -  _dmalloc_memcpy((char *) (to), (const char *) (from), len)
 +  _dmalloc_memcpy(__FILE__, __LINE__, (char *) (to), (const char *) (from), len)
  #define memmove(to, from, len) \
 -  _dmalloc_bcopy((const char *) (from), (char *) (to), len)
 +  _dmalloc_bcopy(__FILE__, __LINE__, (const char *) (from), (char *) (to), len)
- /* Work around Dmalloc's misunderstanding of free's definition */
  
- #if DMALLOC_VERSION_MAJOR >= 5
-@@ -508,7 +509,7 @@
+ #endif /* version < 5.5 */
+ 
+@@ -515,7 +516,7 @@
    dmalloc_malloc (f, l, s, DMALLOC_FUNC_MALLOC, 0, 0)
  #define _xfree_leap(f, l, p) dmalloc_free (f, l, p, DMALLOC_FUNC_FREE)
  #undef xstrdup 
