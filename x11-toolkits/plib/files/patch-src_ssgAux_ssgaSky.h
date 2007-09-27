fce59cb5998e85a38277e4ee553a5ba9b60cbc51
Rong-En Fan <rafan@FreeBSD.org>
2007-09-27 04:10:40 +0000

    - Fix build with gcc 4.x
    PR:		ports/111284
    Submitted by:	rafan
    Approved by:	maintainer timeout (5 months)

--- x11-toolkits/plib/files/patch-src_ssgAux_ssgaSky.h
+++ x11-toolkits/plib/files/patch-src_ssgAux_ssgaSky.h
@@ -1,3 +1,23 @@
+--- src/ssgAux/ssgaSky.h.orig	Fri Apr  6 03:58:33 2007
++++ src/ssgAux/ssgaSky.h	Fri Apr  6 03:58:45 2007
+@@ -104,7 +104,7 @@
+   ssgaCelestialBodyList ( int init = 3 )
+ 	  : ssgSimpleList ( sizeof(ssgaCelestialBody*), init ) { }
+ 
+-  ssgaCelestialBodyList::~ssgaCelestialBodyList () { removeAll(); }
++  ~ssgaCelestialBodyList () { removeAll(); }
+ 
+   int getNum (void) { return total ; }
+ 
+@@ -192,7 +192,7 @@
+   ssgaCloudLayerList ( int init = 3 )
+ 	  : ssgSimpleList ( sizeof(ssgaCloudLayer*), init ) { }
+ 
+-  ssgaCloudLayerList::~ssgaCloudLayerList () { removeAll(); }
++  ~ssgaCloudLayerList () { removeAll(); }
+ 
+   int getNum (void) { return total ; }
+ 
 --- src/ssgAux/ssgaSky.h.orig	Tue May 22 20:11:39 2007
 +++ src/ssgAux/ssgaSky.h	Tue May 22 20:13:21 2007
 @@ -104,7 +104,7 @@
