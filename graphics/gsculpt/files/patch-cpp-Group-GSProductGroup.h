83ae30d70cc1baf6c1201488229695c9bed33726
Alejandro Pulver <alepulver@FreeBSD.org>
2007-09-27 19:07:05 +0000

    - Update to 0.99.46.2. - Use SF macro in MASTER_SITES [1]. - Fix building on non-i386 instead of adding NOT_FOR_ARCHS [1].
    PR:		ports/115138
    Submitted by:	Martin Tournoij <carpetsmoker@xs4all.nl> (maintainer)
    Reworked by:	alepulver (myself) [1]

---
+++ graphics/gsculpt/files/patch-cpp-Group-GSProductGroup.h
@@ -0,0 +1,17 @@
+--- cpp/Group/GSProductGroup.h.orig	Sun May  6 11:48:08 2007
++++ cpp/Group/GSProductGroup.h	Fri Jul 13 00:53:37 2007
+@@ -8,13 +8,13 @@
+ #ifndef GSPRODUCTGROUP_H__
+ #define GSPRODUCTGROUP_H__
+ 
+-#include <boost/python.hpp>
+ 
+ #include <Util/Array.h>
+ 
+ #include <Model/GSProductModel.h>
+ 
+ #include <Graphics/Region.h>
++#include <boost/python.hpp>
+ 
+ 
+ 
