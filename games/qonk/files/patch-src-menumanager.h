05f58c48255752ee1c110dc66dcd7cd366bdeb46
Alejandro Pulver <alepulver@FreeBSD.org>
2007-09-29 00:48:34 +0000

    - Update to 0.3.1. - Improve the port.
    PR:		ports/116500
    Submitted by:	Dmitry Marakasov <amdmi3@amdmi3.ru>

---
+++ games/qonk/files/patch-src-menumanager.h
@@ -0,0 +1,11 @@
+--- src/menumanager.h.orig	Thu Sep 20 22:01:32 2007
++++ src/menumanager.h	Thu Sep 20 21:58:13 2007
+@@ -7,6 +7,8 @@
+ #ifndef MENUMANAGER_H
+ #define MENUMANAGER_H
+ 
++#include "ui/menuaction.h"
++
+ class Main;
+ 
+ class ControlOptions;
