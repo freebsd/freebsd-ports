0e1a9d5048b2c3b1088346c70fb6d333a973c9dd
rafan <rafan@FreeBSD.org>
2007-10-01 14:35:18 +0000

    - Fix patch. For unknown reason, I put the same patch hunk twice.
    Noticed by:	linimon

--- x11-toolkits/plib/files/patch-src_ssgAux_ssgaSky.h
+++ x11-toolkits/plib/files/patch-src_ssgAux_ssgaSky.h
@@ -18,23 +18,3 @@
  
    int getNum (void) { return total ; }
  
---- src/ssgAux/ssgaSky.h.orig	Tue May 22 20:11:39 2007
-+++ src/ssgAux/ssgaSky.h	Tue May 22 20:13:21 2007
-@@ -104,7 +104,7 @@
-   ssgaCelestialBodyList ( int init = 3 )
- 	  : ssgSimpleList ( sizeof(ssgaCelestialBody*), init ) { }
- 
--  ssgaCelestialBodyList::~ssgaCelestialBodyList () { removeAll(); }
-+  ~ssgaCelestialBodyList () { removeAll(); }
- 
-   int getNum (void) { return total ; }
- 
-@@ -192,7 +192,7 @@
-   ssgaCloudLayerList ( int init = 3 )
- 	  : ssgSimpleList ( sizeof(ssgaCloudLayer*), init ) { }
- 
--  ssgaCloudLayerList::~ssgaCloudLayerList () { removeAll(); }
-+  ~ssgaCloudLayerList () { removeAll(); }
- 
-   int getNum (void) { return total ; }
- 
