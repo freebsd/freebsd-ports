2ecfe11aadc11e6e8ac172fd9fdb93f4b0a0d314
Joe Marcus Clarke <marcus@FreeBSD.org>
2007-10-01 18:28:29 +0000

    Fix one more problem with CUPS 1.3.  I missed this in the original PR.
    Reported by:	Scot Hetzel <swhetzel@gmail.com>

--- x11-toolkits/gtk20/files/patch-modules_printbackends_cups_gtkcupsutils.c
+++ x11-toolkits/gtk20/files/patch-modules_printbackends_cups_gtkcupsutils.c
@@ -1,5 +1,5 @@
---- modules/printbackends/cups/gtkcupsutils.c.orig	2007-09-30 18:40:09.000000000 -0400
-+++ modules/printbackends/cups/gtkcupsutils.c	2007-09-30 18:40:45.000000000 -0400
+--- modules/printbackends/cups/gtkcupsutils.c.orig	2007-07-16 15:45:18.000000000 -0400
++++ modules/printbackends/cups/gtkcupsutils.c	2007-10-01 14:27:17.000000000 -0400
 @@ -627,9 +627,7 @@ _post_send (GtkCupsRequest *request)
    httpClearFields(request->http);
    httpSetField(request->http, HTTP_FIELD_CONTENT_LENGTH, length);
@@ -10,4 +10,15 @@
 +  httpSetField (request->http, HTTP_FIELD_AUTHORIZATION, httpGetAuthString (request->http));
  
    if (httpPost(request->http, request->resource))
+     {
+@@ -966,9 +964,7 @@ _get_send (GtkCupsRequest *request)
+     }
+ 
+   httpClearFields(request->http);
+-#ifdef HAVE_HTTP_AUTHSTRING
+-  httpSetField(request->http, HTTP_FIELD_AUTHORIZATION, request->http->authstring);
+-#endif
++  httpSetField(request->http, HTTP_FIELD_AUTHORIZATION, httpGetAuthString (request->http));
+ 
+   if (httpGet(request->http, request->resource))
      {
