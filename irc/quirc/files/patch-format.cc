14326f5ba487a0bdb1d31c0497abe2c02332894f
Kevin Lo <kevlo@FreeBSD.org>
2007-10-02 15:00:14 +0000

    - Fix build with gcc4. - Drop maintainer.

--- irc/quirc/files/patch-format.cc
+++ irc/quirc/files/patch-format.cc
@@ -1,8 +1,15 @@
---- format.cc.orig	Mon Jan 12 18:32:01 2004
-+++ format.cc	Tue Feb  3 23:19:28 2004
+--- format.cc.orig	2004-01-12 17:32:01.000000000 +0800
++++ format.cc	2007-10-02 22:46:34.000000000 +0800
 @@ -1,4 +1,5 @@
  #include <stdarg.h>
 +#include <sys/types.h>
  
  #include "quirc.h"
  #include "hash.h"
+@@ -405,5 +406,5 @@
+ }
+ 
+ int fexists(const char *type) {
+-  return (int)hash_get(&formats,type);
++  return (int)*hash_get(&formats,type);
+ }
