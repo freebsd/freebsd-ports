b3950a7bca3d7e11baa383ed5cc3d9d9d0222cfc
Oliver Lehmann <oliver@FreeBSD.org>
2007-10-04 14:57:29 +0000

    fix build of courier-authlib-vchkpw

--- security/courier-authlib/files/patch-authvchkpw.c
+++ security/courier-authlib/files/patch-authvchkpw.c
@@ -1,12 +1,17 @@
---- authvchkpw.c.orig	Thu May 31 07:04:07 2007
-+++ authvchkpw.c	Thu May 31 07:03:44 2007
-@@ -28,6 +28,9 @@
-         int (*callback)(struct authinfo *, void *),
-                         void *arg);
- 
-+static int auth_vchkpw_login(const char *service, char *authdata,
-+        int (*callback_func)(struct authinfo *, void *), void *callback_arg);
-+
+--- authvchkpw.c.orig	Thu Aug 30 03:31:13 2007
++++ authvchkpw.c	Thu Oct  4 14:59:36 2007
+@@ -1,4 +1,4 @@
+-i/*
++/*
+ ** Copyright 1998 - 2007 Double Precision, Inc.  See COPYING for
+ ** distribution information.
+ */
+@@ -31,7 +31,7 @@
  extern FILE *authvchkpw_file(const char *, const char *);
  
+ static int auth_vchkpw_login(const char *service, char *authdata,
+-	int (*callback_func)(struct authinfo *, void *), void *callback_arg)
++	int (*callback_func)(struct authinfo *, void *), void *callback_arg);
+ 
  struct callback_info {
+ 	const char *pass;
