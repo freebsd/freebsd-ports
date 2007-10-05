df5d3508e83124654bf5a4097ee02d76f873f536
Martin Wilke <miwi@FreeBSD.org>
2007-10-05 21:53:51 +0000

    - Update 0.45
    PR:		116922
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- cad/qfsm/files/patch-src::Draw.cpp
+++ cad/qfsm/files/patch-src::Draw.cpp
@@ -1,11 +1,13 @@
---- src/Draw.cpp.orig	Sun Feb 29 21:38:23 2004
-+++ src/Draw.cpp	Tue Mar 16 23:46:29 2004
-@@ -16,7 +16,7 @@
+--- src/Draw.cpp.orig	Fri Aug 17 05:07:21 2007
++++ src/Draw.cpp	Wed Oct  3 14:30:19 2007
+@@ -16,8 +16,8 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
  
 -#include <math.h>
+-#include <float.h>
 +#include <cmath>
++#include <cfloat>
  #include <qnamespace.h>
- #include <qlist.h>
+ #include <qptrlist.h>
  #include <qpainter.h>
