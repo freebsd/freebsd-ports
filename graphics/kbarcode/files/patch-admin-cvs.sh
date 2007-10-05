e010dcffa8316ae538813dec588048161ddac7f8
miwi <miwi@FreeBSD.org>
2007-10-05 21:55:08 +0000

    - Update to 2.0.6
    PR:		116926
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ graphics/kbarcode/files/patch-admin-cvs.sh
@@ -0,0 +1,69 @@
+--- admin/cvs.sh.orig	Sat Sep 29 17:52:39 2007
++++ admin/cvs.sh	Mon Oct  1 03:23:11 2007
+@@ -29,66 +29,9 @@
+ 
+ check_autotool_versions()
+ {
+-required_autoconf_version="2.53 or newer"
+ AUTOCONF_VERSION=`$AUTOCONF --version | head -n 1`
+-case $AUTOCONF_VERSION in
+-  Autoconf*2.5* | autoconf*2.5* | autoconf*2.6* ) : ;;
+-  "" )
+-    echo "*** AUTOCONF NOT FOUND!."
+-    echo "*** KDE requires autoconf $required_autoconf_version"
+-    exit 1
+-    ;;
+-  * )
+-    echo "*** YOU'RE USING $AUTOCONF_VERSION."
+-    echo "*** KDE requires autoconf $required_autoconf_version"
+-    exit 1
+-    ;;
+-esac
+-
+ AUTOHEADER_VERSION=`$AUTOHEADER --version | head -n 1`
+-case $AUTOHEADER_VERSION in
+-  Autoconf*2.5* | autoheader*2.5* | autoheader*2.6* ) : ;;
+-  "" )
+-    echo "*** AUTOHEADER NOT FOUND!."
+-    echo "*** KDE requires autoheader $required_autoconf_version"
+-    exit 1
+-    ;;
+-  * )
+-    echo "*** YOU'RE USING $AUTOHEADER_VERSION."
+-    echo "*** KDE requires autoheader $required_autoconf_version"
+-    exit 1
+-    ;;
+-esac
+-
+ AUTOMAKE_STRING=`$AUTOMAKE --version | head -n 1`
+-required_automake_version="1.6.1 or newer"
+-case $AUTOMAKE_STRING in
+-  automake*1.5d* | automake*1.5* | automake*1.5-* )
+-    echo "*** YOU'RE USING $AUTOMAKE_STRING."
+-    echo "*** KDE requires automake $required_automake_version"
+-    exit 1
+-    ;;
+-  automake*1.6.* | automake*1.7* | automake*1.8* | automake*1.9*)
+-    echo "*** $AUTOMAKE_STRING found."
+-    UNSERMAKE=no
+-    ;;
+-  "" )
+-    echo "*** AUTOMAKE NOT FOUND!."
+-    echo "*** KDE requires automake $required_automake_version"
+-    exit 1
+-    ;;
+-  *unsermake* ) :
+-    echo "*** YOU'RE USING UNSERMAKE."
+-    echo "*** GOOD LUCK!! :)"
+-    UNSERMAKE=unsermake
+-    ;;
+-  * )
+-    echo "*** YOU'RE USING $AUTOMAKE_STRING."
+-    echo "*** KDE requires automake $required_automake_version"
+-    exit 1
+-    ;;
+-esac
+-unset required_automake_version
+ }
+ 
+ cvs()
