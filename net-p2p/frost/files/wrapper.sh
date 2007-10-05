52e04355c12aa3bf93b40930790ee2ae10d378a9
miwi <miwi@FreeBSD.org>
2007-10-05 21:54:47 +0000

    - Update to 1.2.0.20070719 - Pass maintainership to the submitter
    PR:		116843
    Submitted by:	Peter Thoenen <peter.thoenen@yahoo.com>

--- net-p2p/frost/files/wrapper.sh
+++ net-p2p/frost/files/wrapper.sh
@@ -9,7 +9,7 @@ JAR_DIR="%%JAR_DIR%%"
 JAR_FILE="%%JAR_FILE%%"
 DATADIR="%%DATADIR%%"
 HOME_DIR=${HOME}/.frost
-CLASSPATH="${PREFIX}/${JAR_DIR}/BCastle.jar:${PREFIX}/${JAR_DIR}/fec-native.jar:${PREFIX}/${JAR_DIR}/fecImpl.jar:${PREFIX}/${JAR_DIR}/frost.jar:${PREFIX}/${JAR_DIR}/genChkImpl.jar:${PREFIX}/${JAR_DIR}/jocache.jar:${PREFIX}/${JAR_DIR}/mailapi.jar:${PREFIX}/${JAR_DIR}/skinlf.jar:${PREFIX}/${JAR_DIR}/skinlfFix.jar:${PREFIX}/${JAR_DIR}/smtp.jar:${PREFIX}/${JAR_DIR}/xercesImpl.jar:${PREFIX}/${JAR_DIR}/xml-apis.jar"
+CLASSPATH="${PREFIX}/${JAR_DIR}/BCastle.jar:${PREFIX}/${JAR_DIR}/datechooser.jar:${PREFIX}/${JAR_DIR}/fec-native.jar:${PREFIX}/${JAR_DIR}/fecImpl.jar:${PREFIX}/${JAR_DIR}/frost.jar:${PREFIX}/${JAR_DIR}/genChkImpl.jar:${PREFIX}/${JAR_DIR}/gnu-regexp-1.1.4.jar:${PREFIX}/${JAR_DIR}/joda-time.jar:${PREFIX}/${JAR_DIR}/perst15.jar:${PREFIX}/${JAR_DIR}/skinlf.jar:${PREFIX}/${JAR_DIR}/skinlfFix.jar:${PREFIX}/${JAR_DIR}/mckoidb.jar:${PREFIX}/${JAR_DIR}/xercesImpl.jar:${PREFIX}/${JAR_DIR}/xml-apis.jar"
 
 if [ ! -d ${HOME_DIR}/config ]
 then
@@ -22,9 +22,16 @@ ln -sf ${PREFIX}/${DATADIR}/boards.xml.default \
 if [ ! -d ${HOME_DIR}/themes ]
 then
 	mkdir -p ${HOME_DIR}/themes
+	ln -sf ${PREFIX}/${DATADIR}/themepack.zip ${HOME_DIR}/themes/themepack.zip
 fi
 
-ln -sf ${PREFIX}/${DATADIR}/themepack.zip ${HOME_DIR}/themes/themepack.zip
+if [ ! -d ${HOME_DIR}/store ]
+then
+	mkdir -p ${HOME_DIR}/store
+	ln -sf ${PREFIX}/${DATADIR}/applayerdb.conf \
+		${HOME_DIR}/store/applayerdb.conf
+fi
+
 ln -sf ${PREFIX}/${DATADIR}/aquathemepack.zip \
 	${HOME_DIR}/themes/aquathemepack.zip
 
