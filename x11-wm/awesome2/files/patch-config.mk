818cb17827b342d7a7b2c0020958be25408e0ec3
Rong-En Fan <rafan@FreeBSD.org>
2007-10-06 11:37:58 +0000

    - Update to 1.3
    PR:		ports/116964
    Submitted by:	Nikos Ntarmos <ntarmos at ceid.upatras.gr> (maintainer)

--- x11-wm/awesome2/files/patch-config.mk
+++ x11-wm/awesome2/files/patch-config.mk
@@ -1,7 +1,7 @@
 --- config.mk.orig	2007-09-26 11:41:29.000000000 +0300
 +++ config.mk	2007-09-26 11:42:35.000000000 +0300
 @@ -7,19 +7,19 @@
- LAYOUTS = layouts/tile.c layouts/floating.c
+ LAYOUTS = layouts/tile.c layouts/floating.c layouts/max.c
  
  # paths
 -PREFIX = /usr/local
@@ -15,8 +15,8 @@
 +X11LIB = ${X11BASE}/lib
  
  # includes and libs
- INCS = -I. -I/usr/include -I${X11INC} `pkg-config --cflags libconfig`
- LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 `pkg-config --libs libconfig` -lXext -lXrandr -lXinerama
+ INCS = -I. -I/usr/include -I${X11INC} `pkg-config --cflags libconfig xft`
+ LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 `pkg-config --libs libconfig xft` -lXext -lXrandr -lXinerama
  
  # flags
 -CFLAGS = -fgnu89-inline -std=gnu99 -ggdb3 -pipe -Wall -Wextra -W -Wchar-subscripts -Wundef -Wshadow -Wcast-align -Wwrite-strings -Wsign-compare -Wunused -Wuninitialized -Winit-self -Wpointer-arith -Wredundant-decls -Wno-format-zero-length -Wmissing-prototypes -Wmissing-format-attribute -Wmissing-noreturn -O3 ${INCS} -DVERSION=\"${VERSION}\"
