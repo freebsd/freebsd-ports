4b13a8e3e38709227830468b16460eb392118818
laszlof <laszlof@FreeBSD.org>
2007-10-07 21:51:57 +0000

    FIx bug reported by submitter remove checks for readline 4.2+, we do not support 4.x anymore bump PORTREVISION
    PR:		ports/116690
    Submitted by:	Martin Kammerhofer <dada@pluto.tugraz.at>

--- devel/rlwrap/files/patch-src::ptytty.c
+++ devel/rlwrap/files/patch-src::ptytty.c
@@ -1,5 +1,5 @@
---- src/ptytty.c.orig	Mon Oct 31 16:01:11 2005
-+++ src/ptytty.c	Mon Oct 31 16:01:57 2005
+--- src/ptytty.c.orig	2005-10-17 09:36:46.000000000 +0200
++++ src/ptytty.c	2007-09-27 14:12:26.000000000 +0200
 @@ -41,7 +41,7 @@
  #ifdef HAVE_SYS_IOCTL_H
  # include <sys/ioctl.h>
@@ -9,3 +9,28 @@
  # include <sys/stropts.h>	/* for I_PUSH */
  #endif
  
+@@ -159,11 +159,19 @@
+     char pty_name[] = "/dev/pty??";
+     char tty_name[] = "/dev/tty??";
+ 
+-# ifndef PTYCHAR1
+-#  define PTYCHAR1	"pqrstuvwxyz"
+-# endif
+-# ifndef PTYCHAR2
+-#  define PTYCHAR2	"0123456789abcdef"
++# ifdef __FreeBSD__
++/* Auto-detected pty names do not work well with devfs! */
++#  undef  PTYCHAR1
++#  define PTYCHAR1	"pqrsPQRS"
++#  undef  PTYCHAR2
++#  define PTYCHAR2	"0123456789abcdefghijklmnopqrstuv"
++# else
++#  ifndef PTYCHAR1
++#   define PTYCHAR1	"pqrstuvwxyz"
++#  endif
++#  ifndef PTYCHAR2
++#   define PTYCHAR2	"0123456789abcdef"
++#  endif
+ # endif
+     for (c1 = PTYCHAR1; *c1; c1++) {
+       pty_name[(sizeof(pty_name) - 3)] =
