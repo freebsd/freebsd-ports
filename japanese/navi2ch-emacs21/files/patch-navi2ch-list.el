21040c8c51120ca8546e048d9b0d2eac78f9bf56
Vasil Dimov <vd@FreeBSD.org>
2007-10-14 09:58:22 +0000

    * Copy the contents of the master port japanese/navi2ch-emacs20 to the slave port   japanese/navi2ch-emacs21 because the former is deprecated and going to be removed   soon. Adjust it to point to emacs21 by default.
    * Adjust the slave port japanese/navi2ch-xemacs21-mule to point to the newly
      created master port japanese/navi2ch-emacs21.

---
+++ japanese/navi2ch-emacs21/files/patch-navi2ch-list.el
@@ -0,0 +1,11 @@
+--- navi2ch-list.el.orig	Sun Aug  8 00:03:09 2004
++++ navi2ch-list.el	Wed Sep  7 01:44:31 2005
+@@ -85,7 +85,7 @@
+     (nil " " navi2ch-list-board-name-face)))
+ 
+ (defconst navi2ch-list-bbstable-default-url
+-  "http://www.ff.iij4u.or.jp/~ch2/bbsmenu.html")
++  "http://menu.2ch.net/bbsmenu.html")
+ 
+ ;; add hook
+ (add-hook 'navi2ch-save-status-hook 'navi2ch-list-save-info)
