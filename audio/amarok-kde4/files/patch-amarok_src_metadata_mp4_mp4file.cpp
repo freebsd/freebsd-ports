acef1b8b398acf8b52d74db90771118bb24c89c8
mich <mich@FreeBSD.org>
2007-10-14 18:58:37 +0000

    - fix breakage from lastest mpeg4ip-libmp4v2
    PR:		ports/116972
    Submitted by:	Armin Pirkovitsch <a.pirko@inode.at>

--- audio/amarok-kde4/files/patch-amarok_src_metadata_mp4_mp4file.cpp
+++ audio/amarok-kde4/files/patch-amarok_src_metadata_mp4_mp4file.cpp
@@ -3,7 +3,7 @@ $FreeBSD$
 
 --- amarok/src/metadata/mp4/mp4file.cpp.orig
 +++ amarok/src/metadata/mp4/mp4file.cpp
-@@ -23,9 +23,8 @@
+@@ -27,9 +27,8 @@
  
  #include "mp4tag.h"
  #include <tfile.h>
@@ -14,3 +14,16 @@ $FreeBSD$
  
  #define MP4V2_HAS_WRITE_BUG 1
  
+@@ -156,10 +155,8 @@
+     }
+ #endif
+ 
+-    if(!MP4Close(handle))
+-    {
+-        fprintf(stderr, "close failed\n");
+-    }
++    MP4Close(handle);
++    handle=NULL;
+ 
+     mp4file = MP4Read(name());
+     if(mp4file == MP4_INVALID_FILE_HANDLE)
