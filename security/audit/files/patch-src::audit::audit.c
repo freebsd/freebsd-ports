cb8ea2b0ad058eb8920a8cea4aa35aa864d036a8
johans <johans@FreeBSD.org>
2007-10-17 08:35:03 +0000

    - Update patchfiles to match latest release (Nov 2001) - Fix MASTER_SITES (adding local mirror)
    The old master sites referenced distinct distfiles with the same filename.
    Primary site carries the latest version which includes minor bugfixes.
    Patches in previous commit were broken as they matched the older release.

--- security/audit/files/patch-src::audit::audit.c
+++ security/audit/files/patch-src::audit::audit.c
@@ -1,6 +1,6 @@
---- src/audit/audit.c.orig	Thu Nov  4 07:24:04 2004
-+++ src/audit/audit.c	Thu Nov  4 07:25:17 2004
-@@ -143,6 +143,7 @@
+--- src/audit/audit.c.orig	2001-10-06 01:14:58.000000000 +0200
++++ src/audit/audit.c	2007-10-17 09:41:34.000000000 +0200
+@@ -147,6 +147,7 @@
  	case SIGABRT:
  		fatal(-1, "");
  	default:
