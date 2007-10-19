7e809bb657dbdb17c4c361c362710d9cae040b0c
Motoyuki Konno <motoyuki@FreeBSD.org>
2007-10-19 04:02:08 +0000

    Update patch file and unbreak.

--- japanese/mh/files/patch-uip:vmh.c
+++ japanese/mh/files/patch-uip:vmh.c
@@ -1,16 +1,13 @@
---- uip/vmh.c.old	Tue Feb  9 20:30:00 1999
-+++ uip/vmh.c	Wed Jul 10 15:40:52 2002
-@@ -103,6 +103,16 @@
+--- uip/vmh.c.orig	2001-04-21 20:16:49 +0900
++++ uip/vmh.c	2007-10-19 12:51:52 +0900
+@@ -116,6 +116,13 @@
  
  #define	XYZ			/* XXX */
  
 +#if defined(__FreeBSD__) && __FreeBSD__ >= 5
 +struct ldat
 +{
-+	NCURSES_CH_T	*text;
-+	NCURSES_SIZE_T	firstchar;
-+	NCURSES_SIZE_T	lastchar;
-+	NCURSES_SIZE_T	oldindex;
++	unsigned long	*text;
 +};
 +#endif
 +
