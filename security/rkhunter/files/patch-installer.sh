70272da1322631c60450b7374c0925c57495c391
Martin Wilke <miwi@FreeBSD.org>
2007-10-20 23:51:21 +0000

    - Update to 1.3.0
    PR:		117269
    Submitted by:	Philippe Audeoud <jadawin@tuxaco.net>
    Approved by:	maintainer implicit

--- security/rkhunter/files/patch-installer.sh
+++ security/rkhunter/files/patch-installer.sh
@@ -1,68 +1,28 @@
---- installer.sh.orig	Mon Apr 25 13:24:56 2005
-+++ installer.sh	Fri Apr 28 21:44:41 2006
-@@ -119,15 +119,13 @@
- overwrite:programs_good.dat:/db/programs_good.dat:Database%%Program%%versions
- overwrite:defaulthashes.dat:/db/defaulthashes.dat:Database%%Default%%file%%hashes
- overwrite:md5blacklist.dat:/db/md5blacklist.dat:Database%%MD5%%blacklisted%%files
--overwrite:CHANGELOG:/docs/CHANGELOG:Changelog
--overwrite:README:/docs/README:Readme%%and%%FAQ
--overwrite:WISHLIST:/docs/WISHLIST:Wishlist%%and%%TODO
- "
+--- installer.sh.orig	Wed Oct 17 13:38:52 2007
++++ installer.sh	Wed Oct 17 13:40:59 2007
+@@ -399,7 +399,7 @@
  
- # Prefix: INSTALLDIR
- INSTALLFILES2="
--nooverwrite:rkhunter.conf:/usr/local/etc/rkhunter.conf:RK%%Hunter%%configuration%%file
--overwrite:rkhunter:/usr/local/bin/rkhunter:RK%%Hunter%%binary
-+nooverwrite:rkhunter.conf:${INSTALLDIR}/etc/rkhunter.conf:RK%%Hunter%%configuration%%file
-+overwrite:rkhunter.conf:${INSTALLDIR}/etc/rkhunter.conf.sample:RK%%Hunter%%sample%%configuration%%file
-+overwrite:rkhunter:${INSTALLDIR}/bin/rkhunter:RK%%Hunter%%binary
- "
+ # Perl will be found in Rkhunter itself.
  
- # Create directories (only if they do not exist)
-@@ -136,9 +134,7 @@
- ${INSTALLDIR}/etc
- ${INSTALLDIR}/bin
- ${INSTALLDIR}/lib/rkhunter/db
--${INSTALLDIR}/lib/rkhunter/docs
- ${INSTALLDIR}/lib/rkhunter/scripts
--${INSTALLDIR}/lib/rkhunter/tmp
- /usr/local/etc
- /usr/local/bin
- "
-@@ -397,9 +393,6 @@
- #################################################################################
+-RKHINST_DIRS="$RKHINST_DOC_DIR $RKHINST_MAN_DIR $RKHINST_ETC_DIR $RKHINST_BIN_DIR"
++RKHINST_DIRS="$RKHINST_ETC_DIR $RKHINST_BIN_DIR"
+ RKHINST_DIRS_EXCEP="$RKHINST_SCRIPT_DIR $RKHINST_DB_DIR $RKHINST_TMP_DIR $RKHINST_LANG_DIR"
  
- 
--# Clean active window
--clear
--
- echo "${INSTALLER_NAME} ${INSTALLER_VERSION} (${INSTALLER_COPYRIGHT})"
- echo $ECHOOPT "---------------"
  echo "Starting installation/update"
-@@ -534,11 +527,14 @@
+@@ -526,11 +526,11 @@
  done
  
- # Installation dir to configuration file
--INSTALLDIRCHECK=`cat /usr/local/etc/rkhunter.conf | grep "INSTALLDIR="`
-+echo "" >> ${INSTALLDIR}/etc/rkhunter.conf.sample
-+echo "INSTALLDIR=${INSTALLDIR}" >> ${INSTALLDIR}/etc/rkhunter.conf.sample
-+
-+INSTALLDIRCHECK=`cat ${INSTALLDIR}/etc/rkhunter.conf | grep "INSTALLDIR="`
- if [ "${INSTALLDIRCHECK}" = "" ]
-   then
--    echo "" >> /usr/local/etc/rkhunter.conf
--    echo "INSTALLDIR=${INSTALLDIR}" >> /usr/local/etc/rkhunter.conf
-+    echo "" >> ${INSTALLDIR}/etc/rkhunter.conf
-+    echo "INSTALLDIR=${INSTALLDIR}" >> ${INSTALLDIR}/etc/rkhunter.conf
-     echo "Configuration updated with installation path (${INSTALLDIR})"
-   else
-     echo "Configuration already updated."
-@@ -548,7 +544,7 @@
- then
- 	echo ""
- 	echo $E "$t17"
--	echo "$t18 (/usr/local/bin/rkhunter)"
-+	echo "$t18 (${INSTALLDIR}/rkhunter)"
- else
- 	echo ""
- 	echo $E "$t19"
+ # Application documents
+-for FILE in ${RKHINST_DOC_FILES}; do
+-	echo $N " Installing ${FILE}: "
+-	cp -f ./files/"${FILE}" "${RKHINST_DOC_DIR}"; retValChk
+-	chmod "${RKHINST_MODE_RWR}" "${RKHINST_DOC_DIR}/${FILE}"
+-done
++#for FILE in ${RKHINST_DOC_FILES}; do
++#	echo $N " Installing ${FILE}: "
++#	cp -f ./files/"${FILE}" "${RKHINST_DOC_DIR}"; retValChk
++#	chmod "${RKHINST_MODE_RWR}" "${RKHINST_DOC_DIR}/${FILE}"
++#done
+ 
+ # Language support files
+ echo $N " Installing language support files: "
