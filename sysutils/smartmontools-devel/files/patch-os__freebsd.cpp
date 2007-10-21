3ad8074073785a4359c7f5c56ff4154af7348ccc
pav <pav@FreeBSD.org>
2007-10-21 16:18:51 +0000

    - Allow smartctl to interact with SCSI /dev/pass devices, thus enabling it to   work with RAID controllers that expose disks via these devices.
    Submitted by:	scottl

--- sysutils/smartmontools-devel/files/patch-os__freebsd.cpp
+++ sysutils/smartmontools-devel/files/patch-os__freebsd.cpp
@@ -1,9 +1,26 @@
-
-$FreeBSD$
-
---- os_freebsd.cpp.orig
-+++ os_freebsd.cpp
-@@ -1008,7 +1008,7 @@
+--- os_freebsd.cpp.orig	2006-09-17 05:17:53.000000000 +0200
++++ os_freebsd.cpp	2007-10-21 18:09:02.000000000 +0200
+@@ -873,6 +873,7 @@
+ static const char * fbsd_dev_prefix = "/dev/";
+ static const char * fbsd_dev_ata_disk_prefix = "ad";
+ static const char * fbsd_dev_scsi_disk_plus = "da";
++static const char * fbsd_dev_scsi_pass = "pass";
+ static const char * fbsd_dev_scsi_tape1 = "sa";
+ static const char * fbsd_dev_scsi_tape2 = "nsa";
+ static const char * fbsd_dev_scsi_tape3 = "esa";
+@@ -908,6 +909,11 @@
+     return CONTROLLER_ATA;
+   }
+   
++  // form /dev/pass* or pass*
++  if (!strncmp(fbsd_dev_scsi_pass, dev_name,
++               strlen(fbsd_dev_scsi_pass)))
++    goto handlescsi;
++
+   // form /dev/da* or da*
+   if (!strncmp(fbsd_dev_scsi_disk_plus, dev_name,
+                strlen(fbsd_dev_scsi_disk_plus)))
+@@ -1008,7 +1014,7 @@
    // to first list. Turn on NOCHECK for second call. This results in no
    // error if no more matches found, however it does append the actual
    // pattern to the list of paths....
