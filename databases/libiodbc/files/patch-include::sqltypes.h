608f0e9b187d09b7354957cf2ed06caaf28a51ee
miwi <miwi@FreeBSD.org>
2007-10-23 23:07:09 +0000

    - Update to 3.52.6
    PR:		117333
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- databases/libiodbc/files/patch-include::sqltypes.h
+++ databases/libiodbc/files/patch-include::sqltypes.h
@@ -1,10 +1,10 @@
---- include/sqltypes.h.orig	Thu Jan 26 18:50:59 2006
-+++ include/sqltypes.h	Tue Jan 31 13:45:45 2006
-@@ -398,6 +398,7 @@
-       defined(_WCHAR_T)			|| \
+--- include/sqltypes.h.orig	Sun Oct  7 22:44:49 2007
++++ include/sqltypes.h	Sat Oct 20 01:06:36 2007
+@@ -399,6 +399,7 @@
        defined(_WCHAR_T_DEFINED)		|| \
-       defined(_WCHAR_T_DECLARED)        || \
-+      defined(__FreeBSD__)              || \
-       defined(_BSD_WCHAR_T_DEFINED_)    || \
-       defined(_BSD_WCHAR_T_)
- typedef wchar_t SQLWCHAR;
+       defined(_WCHAR_T_DEFINED_)	|| \
+       defined(_WCHAR_T_DECLARED)	|| \
++      defined(__FreeBSD__)		|| \
+       defined(_BSD_WCHAR_T_DEFINED_)	|| \
+       defined(_BSD_WCHAR_T_)		|| \
+       defined(_BSD_CT_RUNE_T_)
