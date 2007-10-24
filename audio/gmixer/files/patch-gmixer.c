8785c2b13c849ba2091f26581867a5e34ff176ed
miwi <miwi@FreeBSD.org>
2007-10-24 12:27:13 +0000

    - Fix build with gcc 4.2
    PR:		117406
    Submitted by:	Pietro Cerutti <gahr@gahr.ch> (maintainer)

--- audio/gmixer/files/patch-gmixer.c
+++ audio/gmixer/files/patch-gmixer.c
@@ -1,7 +1,19 @@
---- patch-ac begins here ---
---- gmixer.c	2002/05/26 05:09:08	1.1
-+++ gmixer.c	2002/05/26 05:21:05
-@@ -378,6 +378,27 @@
+--- gmixer.c.orig	1999-02-22 17:11:10.000000000 +0100
++++ gmixer.c	2007-10-24 13:30:08.000000000 +0200
+@@ -1,10 +1,11 @@
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <strings.h>
+ #include <fcntl.h>
+ #include <unistd.h>
+ #include <sys/ioctl.h>
+-#include <linux/soundcard.h>
++#include <sys/soundcard.h>
+ #include <gtk/gtk.h>
+ #include "icons/gmixer.xpm"
+ #include "icons/mix_logo.xpm"
+@@ -378,6 +379,27 @@
  	exit(0);
  }
  
@@ -29,7 +41,7 @@
  int main (int argc, char *argv[])
  {
  	char *homedir;
-@@ -385,6 +406,7 @@
+@@ -385,6 +407,7 @@
  	char *default_dev = "/dev/mixer";
  	int i;
  	g_print ("gmixer 0.98 (22.2.1999)\nCopyleft (C) 1998,1999 - Sergey Kiselev <sergey@junior.technion.ac.il>\n");
