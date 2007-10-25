eae5225fa305116140c119c918522e5fbb7ed1f3
mezz <mezz@FreeBSD.org>
2007-10-25 22:37:33 +0000

    Update to 0.5.6.

--- net-p2p/deluge/files/patch-src___init__.py
+++ net-p2p/deluge/files/patch-src___init__.py
@@ -1,15 +1,27 @@
---- src/__init__.py.orig	2007-08-05 13:08:25.000000000 -0500
-+++ src/__init__.py	2007-08-05 13:08:40.000000000 -0500
-@@ -6,9 +6,9 @@
+--- src/__init__.py.orig	2007-10-25 17:19:53.000000000 -0500
++++ src/__init__.py	2007-10-25 17:21:29.000000000 -0500
+@@ -37,15 +37,15 @@
  
  APP = 'deluge'
- DIR = os.path.join(INSTALL_PREFIX, 'share', 'locale')
--locale.setlocale(locale.LC_ALL, '')
--locale.bindtextdomain(APP, DIR)
--locale.textdomain(APP)
-+#locale.setlocale(locale.LC_ALL, '')
-+#locale.bindtextdomain(APP, DIR)
-+#locale.textdomain(APP)
+ DIR = os.path.join(common.INSTALL_PREFIX, 'share', 'locale')
+-if not common.windows_check(): 
+-    locale.setlocale(locale.LC_MESSAGES, '')
+-    locale.bindtextdomain(APP, DIR)
+-    locale.textdomain(APP)
+-else:
+-    import gtk.glade
+-    locale.setlocale(locale.LC_ALL, '')
+-    gtk.glade.bindtextdomain(APP, DIR)
+-    gtk.glade.textdomain(APP)
++#if not common.windows_check(): 
++#    locale.setlocale(locale.LC_MESSAGES, '')
++#    locale.bindtextdomain(APP, DIR)
++#    locale.textdomain(APP)
++#else:
++#    import gtk.glade
++#    locale.setlocale(locale.LC_ALL, '')
++#    gtk.glade.bindtextdomain(APP, DIR)
++#    gtk.glade.textdomain(APP)
  gettext.bindtextdomain(APP, DIR)
  gettext.textdomain(APP)
  gettext.install(APP, DIR)
--- net-p2p/deluge05/files/patch-setup.py
+++ net-p2p/deluge05/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2007-08-14 11:14:32.000000000 -0500
-+++ setup.py	2007-08-14 11:16:15.000000000 -0500
-@@ -101,13 +101,13 @@
+--- setup.py.orig	2007-10-25 16:49:47.000000000 -0500
++++ setup.py	2007-10-25 16:51:47.000000000 -0500
+@@ -103,13 +103,13 @@
                  "-DHAVE_INCLUDE_LIBTORRENT_ASIO_SSL_STREAM_HPP=1", 
                  "-DHAVE_INCLUDE_LIBTORRENT_ASIO_IP_TCP_HPP=1", 
                  "-DHAVE_PTHREAD=1", "-DTORRENT_USE_OPENSSL=1", "-DHAVE_SSL=1", 
@@ -16,28 +16,28 @@
  
      if OS == "linux":
          if os.WEXITSTATUS(os.system('grep -iq "Debian GNU/Linux 4.0\|Ubuntu 7.04\|Ubuntu 6.06\|Ubuntu 6.10\|Fedora Core release 6\|openSUSE 10.2\|Mandriva Linux release 2007.1\|Fedora release 7\|BLAG release 60001\|Yellow Dog Linux release 5.0 (Phoenix)\|CentOS release 5 (Final)" /etc/issue')) == 0:
-@@ -150,11 +150,11 @@
- # Ubuntu possible.
- if boosttype == "nomt":
-     librariestype = ['boost_filesystem', 'boost_date_time',
+@@ -164,11 +164,11 @@
+ if not OS == "win":
+     if boosttype == "nomt":
+         librariestype = ['boost_filesystem', 'boost_date_time',
 -            'boost_thread', 'z', 'pthread', 'ssl']
 +            'boost_thread', 'z', 'ssl']
-     print 'Libraries nomt' 
- elif boosttype == "mt":
-     librariestype = ['boost_filesystem-mt', 'boost_date_time-mt',
+         print 'Libraries nomt' 
+     elif boosttype == "mt":
+         librariestype = ['boost_filesystem-mt', 'boost_date_time-mt',
 -            'boost_thread-mt', 'z', 'pthread', 'ssl']
 +            'boost_thread-mt', 'z', 'ssl']
-     print 'Libraries mt'
- 
- def fetchCpp():
-@@ -173,8 +173,10 @@
- 
- deluge_core = Extension('deluge_core',
+         print 'Libraries mt'
+ else:
+         librariestype = ['boost_filesystem-mt', 'boost_date_time-mt',
+@@ -189,8 +189,10 @@
+     sources.remove('libtorrent/src/file_win.cpp')
+     deluge_core = Extension('deluge_core',
                      include_dirs = includedirs,
 +                    library_dirs = ['%%LOCALBASE%%/lib'],
-             libraries = librariestype,
+                     libraries = librariestype,
                      extra_compile_args = EXTRA_COMPILE_ARGS,
 +                    extra_link_args = ['%%PTHREAD_LIBS%%'],
                      sources = sources)
- # Thanks to Iain Nicol for code to save the location for installed prefix
- # At runtime, we need to know where we installed the data to.
+ else:
+     sources.remove('libtorrent\\src\\file.cpp')
