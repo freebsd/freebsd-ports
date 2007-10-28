6e31d07b8aded715d97218a510d3396e9ff54155
Marius Strobl <marius@FreeBSD.org>
2007-10-28 21:24:39 +0000

    Base on pcutils 2.2.8 so it gets the fixes and enhancements originally done as local patches in pciutils-2.2.6 (see sysutils/pciutils/Makefile 1.10).
    Approved by:	maintainer, miwi

--- devel/libpci/files/pci.h
+++ devel/libpci/files/pci.h
@@ -1,12 +1,15 @@
 --- lib/pci.h.orig	Thu Oct 30 15:57:58 2003
 +++ lib/pci.h	Thu Oct 30 16:04:23 2003
-@@ -11,8 +11,7 @@
+@@ -9,9 +9,9 @@
  #ifndef _PCI_LIB_H
  #define _PCI_LIB_H
  
 -#include "config.h"
 -#include "header.h"
-+#define OS_FREEBSD
+-#include "types.h"
++#include <pci/config.h>
++#include <pci/header.h>
++#include <pci/types.h>
  
- /*
-  *	Types
+ #define PCI_LIB_VERSION 0x020204
+ 
