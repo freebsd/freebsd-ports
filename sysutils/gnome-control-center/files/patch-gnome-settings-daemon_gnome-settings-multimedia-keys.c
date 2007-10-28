afa4f2e9d5f49b6e29841f71ce197ce131a75688
Michael Johnson <ahze@FreeBSD.org>
2007-10-28 17:46:20 +0000

    Bring back part of patch I accidentally removed in last commit

--- sysutils/gnome-control-center/files/patch-gnome-settings-daemon_gnome-settings-multimedia-keys.c
+++ sysutils/gnome-control-center/files/patch-gnome-settings-daemon_gnome-settings-multimedia-keys.c
@@ -1,5 +1,5 @@
---- gnome-settings-daemon/gnome-settings-multimedia-keys.c.orig	2007-10-28 13:37:40.000000000 -0400
-+++ gnome-settings-daemon/gnome-settings-multimedia-keys.c	2007-10-28 13:38:23.000000000 -0400
+--- gnome-settings-daemon/gnome-settings-multimedia-keys.c.orig	2007-09-18 05:32:43.000000000 -0400
++++ gnome-settings-daemon/gnome-settings-multimedia-keys.c	2007-10-28 13:45:28.000000000 -0400
 @@ -610,8 +610,11 @@
  	if ((command != NULL) && (strcmp (command, "") != 0))
  		execute (acme, command, FALSE, FALSE);
@@ -13,3 +13,12 @@
  	g_free (command);
  }
  
+@@ -708,7 +711,7 @@
+ 		do_mail_action (acme);
+ 		break;
+ 	case SLEEP_KEY:
+-		do_sleep_action ("apm", "xset dpms force off");
++		do_sleep_action ("zzz", "xset dpms force off");
+ 		break;
+ 	case SCREENSAVER_KEY:
+ 		if ((cmd = g_find_program_in_path ("gnome-screensaver-command")))
