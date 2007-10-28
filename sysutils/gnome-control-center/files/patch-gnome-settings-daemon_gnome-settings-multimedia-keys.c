07b623ef2d4e012ec962a7a11e0e596a0136fb9a
ahze <ahze@FreeBSD.org>
2007-10-28 17:41:37 +0000

    For multimedia-key-bindings use 'cdcontrol' instead of 'eject' to eject cds

--- sysutils/gnome-control-center/files/patch-gnome-settings-daemon_gnome-settings-multimedia-keys.c
+++ sysutils/gnome-control-center/files/patch-gnome-settings-daemon_gnome-settings-multimedia-keys.c
@@ -1,11 +1,15 @@
---- gnome-settings-daemon/gnome-settings-multimedia-keys.c.orig	Wed Jul 27 13:52:13 2005
-+++ gnome-settings-daemon/gnome-settings-multimedia-keys.c	Wed Jul 27 13:52:32 2005
-@@ -828,7 +828,7 @@
- 		do_mail_action (acme);
- 		break;
- 	case SLEEP_KEY:
--		do_sleep_action ("apm", "xset dpms force off");
-+		do_sleep_action ("zzz", "xset dpms force off");
- 		break;
- 	case SCREENSAVER_KEY:
- 		if ((cmd = g_find_program_in_path ("gnome-screensaver-command")))
+--- gnome-settings-daemon/gnome-settings-multimedia-keys.c.orig	2007-10-28 13:37:40.000000000 -0400
++++ gnome-settings-daemon/gnome-settings-multimedia-keys.c	2007-10-28 13:38:23.000000000 -0400
+@@ -610,8 +610,11 @@
+ 	if ((command != NULL) && (strcmp (command, "") != 0))
+ 		execute (acme, command, FALSE, FALSE);
+ 	else
++#if __FreeBSD__
++		execute (acme, "cdcontrol eject", FALSE, FALSE);
++#else
+ 		execute (acme, "eject -T", FALSE, FALSE);
+-
++#endif
+ 	g_free (command);
+ }
+ 
