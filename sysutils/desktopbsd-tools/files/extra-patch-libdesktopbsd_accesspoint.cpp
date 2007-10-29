a3016d829887eaac7c844473e4429a98e026b646
lme <lme@FreeBSD.org>
2007-10-29 21:03:57 +0000

    - Fix build
    PR:		ports/116313
    Submitted by:	Alex "lissyara" Keda <admin (AT) lissyara.su>
    		Peter Hofer <ph (AT) desktopbsd.net> via mail
    Approved by:	miwi (mentor)

--- sysutils/desktopbsd-tools/files/extra-patch-libdesktopbsd_accesspoint.cpp
+++ sysutils/desktopbsd-tools/files/extra-patch-libdesktopbsd_accesspoint.cpp
@@ -41,7 +41,7 @@ Index: libdesktopbsd/accesspoint.cpp
 + * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 + * SUCH DAMAGE.
 + *
-+ * $FreeBSD: /tmp/pcvs/ports/sysutils/desktopbsd-tools/files/Attic/patch-libdesktopbsd_accesspoint.cpp,v 1.1 2007-09-07 20:02:13 lme Exp $
++ * $FreeBSD: /tmp/pcvs/ports/sysutils/desktopbsd-tools/files/Attic/extra-patch-libdesktopbsd_accesspoint.cpp,v 1.1 2007-10-29 21:03:57 lme Exp $
 + */
  
 +/*-
