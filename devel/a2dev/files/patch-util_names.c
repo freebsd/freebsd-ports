3ff2ad9f7829c8a6390af6beeb32e85cee188c74
miwi <miwi@FreeBSD.org>
2007-10-30 10:54:00 +0000

    - Unbreak - Fix build with gcc 4.2 - pet portlint
    PR:		117648 (based on)
    Submitted by:	Pietro Cerutti <gahr@gahr.ch>

---
+++ devel/a2dev/files/patch-util_names.c
@@ -0,0 +1,10 @@
+--- util/names.c.orig	2007-10-30 11:34:51.000000000 +0100
++++ util/names.c	2007-10-30 11:40:14.000000000 +0100
+@@ -6,6 +6,7 @@
+  */
+ 
+ #include <stdio.h>
++#include <string.h>
+ #include "util.h"
+ #include "error.h"
+ #include "hash.h"
