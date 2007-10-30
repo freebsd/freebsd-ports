4f98af6d861f2104255f8d14ea1c536643141533
Stanislav Sedov <stas@FreeBSD.org>
2007-10-30 11:24:59 +0000

    - Update ruby to the latest 111 patchversion.   It's a bugfix release.
    Thanks to:	Alexander Logvinov <ports@logvinov.com> for initial patchset.

--- lang/ruby18/files/patch-ext_tk_lib_tkextlib_tcllib_tablelist.rb
+++
@@ -1,9 +0,0 @@
---- ext/tk/lib/tkextlib/tcllib/tablelist.rb.orig	Tue Feb 13 02:01:19 2007
-+++ ext/tk/lib/tkextlib/tcllib/tablelist.rb	Fri May 25 20:11:52 2007
-@@ -23,5 +23,5 @@
-   # TkPackage.require('Tablelist', '4.2')
-   TkPackage.require('Tablelist')
- 
--  requrie 'tkextlib/tcllib/tablelist_core'
-+  require 'tkextlib/tcllib/tablelist_core'
- end
