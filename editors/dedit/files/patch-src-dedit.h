2bd5751ba3450319d9821cbaedaabc48bb5729e4
Martin Wilke <miwi@FreeBSD.org>
2007-11-05 10:16:11 +0000

    - Fix build with gcc 4.2
    PR:		117801
    Submitted by:	Pietro Cerutti <gahr@gahr.ch>
    Approved by:	portmgr (pav)

---
+++ editors/dedit/files/patch-src-dedit.h
@@ -0,0 +1,10 @@
+--- src/dedit.h.orig	2007-11-04 00:35:59.000000000 +0100
++++ src/dedit.h	2007-11-04 00:36:08.000000000 +0100
+@@ -30,7 +30,6 @@
+ extern GnomeMDI *DEmdi;
+ 
+ extern gchar *print_cmd;
+-extern gboolean restarted;
+ extern const struct poptOption options[];
+ extern gint fs_code;
+ extern GtkWidget *file_sel;
