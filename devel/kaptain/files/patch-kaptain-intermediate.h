4d5f7f839688e3da50f800d987acb3f7a1a27c81
Martin Wilke <miwi@FreeBSD.org>
2007-11-05 10:04:24 +0000

    - Fix build with gcc 4.2 - Use SF macro
    PR:		117760
    Submitted by:	pietro cerutti <gahr@gahr.ch>
    Approved by:	portmgr (pav)

---
+++ devel/kaptain/files/patch-kaptain-intermediate.h
@@ -0,0 +1,10 @@
+--- kaptain/intermediate.h.orig	2004-09-11 16:07:54.000000000 +0200
++++ kaptain/intermediate.h	2007-11-02 11:23:46.000000000 +0100
+@@ -7,6 +7,7 @@
+ using namespace std;
+ 
+ class Grammar;
++class Kaptain;
+ class Rule;
+ class Conjunction;
+ class Parameter;
