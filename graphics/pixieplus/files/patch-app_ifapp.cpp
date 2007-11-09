66780dc8056fddf2f4eafe92e812a54b33134809
mi <mi@FreeBSD.org>
2007-11-09 02:40:12 +0000

    Attempt to fix this port, which seems to break every time ImageMagick is updated by switching it to use GraphicsMagick instead.
    This removes the need for "private" headers and promises a less eventful
    future.
    
    Don't install mimetype-descriptions, which conflict with those installed
    by kdelibs and otherwise improve plist. Build in parallel using our own
    make (no evident need for gmake).
    
    The port may be retired soon, as no new development happened on it since
    early 2005.
    
    Approved by:	portmgr	(linimon)

--- graphics/pixieplus/files/patch-app_ifapp.cpp
+++ graphics/pixieplus/files/patch-app_ifapp.cpp
@@ -1,25 +1,42 @@
---- app/ifapp.cpp.orig	Sat Sep 18 22:08:01 2004
-+++ app/ifapp.cpp	Sat Sep 18 22:10:56 2004
-@@ -33,18 +33,18 @@
- 
+--- app/ifapp.cpp	2004-05-22 10:02:26.000000000 -0400
++++ app/ifapp.cpp	2007-11-08 08:51:37.000000000 -0500
+@@ -34,6 +34,7 @@
  // ImageMagick message and progress stubs
  extern "C"{
 -    unsigned int magickMonitor(const char *msg, const off_t value,
 -                               const long long unsigned int span, ExceptionInfo *)
-+    MagickBooleanType magickMonitor(const char *msg, const MagickOffsetType value,
-+                               const MagickSizeType span, ExceptionInfo *)
++    static unsigned int
++    magickMonitor(const char *msg, const magick_int64_t value,
++                  const magick_uint64_t span, ExceptionInfo *)
      {
          if(!appPtr)
--            return(true);
-+            return((MagickBooleanType)true);
-         int val = (int)((((float)value)/((float)span))*100.0);
-         if(appPtr->magickMessageProgress())
-             appPtr->magickMessageProgress()->setValue(val);
-         if(appPtr->magickMessageLabel())
-             appPtr->magickMessageLabel()->setText(msg);
-         appPtr->processEvents();
--        return(true);
-+        return((MagickBooleanType)true);
+@@ -48,6 +49,7 @@
      }
  
-     void magickWarning(const ExceptionType /*warning*/, const char *reason,
+-    void magickWarning(const ExceptionType /*warning*/, const char *reason,
+-                       const char *description)
++    static void
++    magickWarning(const ExceptionType /*warning*/, const char *reason,
++                  const char *description)
+     {
+         QString str(reason);
+@@ -58,6 +60,7 @@
+     }
+ 
+-    void magickError(const ExceptionType /*error*/, const char *reason,
+-                     const char *description)
++    static void
++    magickError(const ExceptionType /*error*/, const char *reason,
++                const char *description)
+     {
+         QString str(reason);
+@@ -68,6 +71,7 @@
+     }
+ 
+-    void magickFatalError(const ExceptionType /*error*/, const char *reason,
+-                          const char *description)
++    static void
++    magickFatalError(const ExceptionType /*error*/, const char *reason,
++                     const char *description)
+     {
+         QString str(reason);
