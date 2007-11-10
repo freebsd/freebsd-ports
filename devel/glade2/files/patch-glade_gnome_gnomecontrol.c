01b3c3bbcf9277330c638f7fd479a109fe7b6287
Joe Marcus Clarke <marcus@FreeBSD.org>
2007-11-10 06:32:19 +0000

    Fix the build with GNOME 2.20.
    Approved by:	portmgr (implicit)

---
+++ devel/glade2/files/patch-glade_gnome_gnomecontrol.c
@@ -0,0 +1,13 @@
+--- glade/gnome/gnomecontrol.c.orig	2003-10-30 13:31:20.000000000 -0500
++++ glade/gnome/gnomecontrol.c	2007-11-10 01:31:32.000000000 -0500
+@@ -16,8 +16,8 @@
+  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
+ 
+-#define BONOBO_DISABLE_DEPRECATED
+-#define BONOBO_UI_DISABLE_DEPRECATED
++/*#define BONOBO_DISABLE_DEPRECATED
++#define BONOBO_UI_DISABLE_DEPRECATED*/
+ 
+ /*
+  * Glade only shows Bonobo controls which have a 'glade:show' attribute set
