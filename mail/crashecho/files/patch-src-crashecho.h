48d409fc2a50b5e8e04ba7688506d303cc11b202
miwi <miwi@FreeBSD.org>
2007-11-11 14:46:58 +0000

    - Fix build with gcc 4.2
    PR:		117917
    Submitted by:	Philippe Audeoud <jadawin@tuxaco.net>
    Approved by:	portmgr (erwin)

---
+++ mail/crashecho/files/patch-src-crashecho.h
@@ -0,0 +1,10 @@
+--- src/crashecho.h.orig	2007-11-08 09:24:38.000000000 +0000
++++ src/crashecho.h	2007-11-08 09:24:46.000000000 +0000
+@@ -82,7 +82,6 @@
+ 
+ extern ulong DayStatsWritten;
+ 
+-extern struct Nodelist AvailNodelists[];
+ extern struct Messagebase AvailMessagebases[];
+ 
+ extern struct Config config;
