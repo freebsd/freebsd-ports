168fa1dd5e7e5507a1a97cab8950cecf65cccbfd
Max Khon <fjoe@FreeBSD.org>
2007-11-15 08:33:17 +0000

    Fix gcc 4.2 build.
    Approved by:	portmgr (pav), MAINTAINER (danfe)

--- games/fuhquake/files/patch-cl_main.c
+++ games/fuhquake/files/patch-cl_main.c
@@ -1,5 +1,5 @@
---- cl_main.c.orig	Wed Jan  7 00:39:44 2004
-+++ cl_main.c	Sun Jan 22 22:49:30 2006
+--- cl_main.c.orig	2004-01-07 00:39:44.000000000 +0600
++++ cl_main.c	2007-11-15 00:01:57.000000000 +0600
 @@ -44,6 +44,10 @@
  #include "mp3_player.h"
  
@@ -11,6 +11,15 @@
  #include <netdb.h>		
  #endif
  
+@@ -380,7 +384,7 @@
+ 	Q_strncpyz(address, Cmd_Argv(1), sizeof(address));
+ 	if (s = strchr(address, ':'))
+ 		*s = 0;
+-	if (((int) addr.s_addr = inet_addr(address)) == INADDR_NONE) {
++	if ((addr.s_addr = inet_addr(address)) == INADDR_NONE) {
+ 		//forward lookup
+ 		if (!(h = gethostbyname(address))) {
+ 			Com_Printf("Couldn't resolve %s\n", address);
 @@ -850,19 +854,16 @@
  		Sys_Error ("Couldn't load gfx/colormap.lmp");
  	FMod_CheckModel("gfx/colormap.lmp", host_colormap, com_filesize); 
