15ed97a44cc5b2075744c509c5fd7db30872ad2b
se <se@FreeBSD.org>
2007-11-15 13:27:23 +0000

    Unbreak port: use functional interface to locate encoder/decoder functions instead of references to global function pointers (which are no longer exported by ffmpeg).
    Approved by:	portmgr (linimon)

--- multimedia/dvbcut/files/patch-main.cpp
+++ multimedia/dvbcut/files/patch-main.cpp
@@ -1,5 +1,5 @@
 --- src/main.cpp~	2007-04-13 19:36:26 +0200
-+++ src/main.cpp	2007-07-17 01:10:36 +0200
++++ src/main.cpp	2007-11-15 09:19:25 +0100
 @@ -27,7 +27,9 @@
  #endif // HAVE_LIB_AO
  
@@ -10,3 +10,11 @@
  #include <qimage.h>
  #include <qsettings.h>
  #include "dvbcut.h"
+@@ -76,6 +78,7 @@
+       else
+         idxfilename=mpgfilename+".idx";
+ 
++    av_register_all();
+     std::string errormessage;
+     mpgfile *mpg=mpgfile::open(mpgfilename,&errormessage);
+ 
