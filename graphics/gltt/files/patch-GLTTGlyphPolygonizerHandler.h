035f39a84a94fb4abff4948e8175cacda58e9afb
Martin Wilke <miwi@FreeBSD.org>
2007-11-21 14:56:44 +0000

    - Fix build with gcc 4.2
    PR:		118084
    Submitted by:	Pietro Cerutti <gahr@gahr.ch>
    Approved by:	portmgr (pav)

---
+++ graphics/gltt/files/patch-GLTTGlyphPolygonizerHandler.h
@@ -0,0 +1,10 @@
+--- GLTTGlyphPolygonizerHandler.h.orig	2007-11-16 11:59:00.000000000 +0100
++++ GLTTGlyphPolygonizerHandler.h	2007-11-16 11:59:58.000000000 +0100
+@@ -28,6 +28,7 @@
+ #include "FTGlyphVectorizer.h"
+ #endif
+ 
++class GLTTGlyphPolygonizer;
+ /////////////////////////////////////////////////////////////////////////////
+ 
+ // Mmh, this class name is a bit long... /SR
