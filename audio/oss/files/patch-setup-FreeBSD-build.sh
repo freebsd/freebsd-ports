f26ec97c5a9aa1e471bdf7487478271d09d89480
Jung-uk Kim <jkim@FreeBSD.org>
2007-12-11 20:19:07 +0000

    Update to build 1009.

--- audio/oss/files/patch-setup-FreeBSD-build.sh
+++ audio/oss/files/patch-setup-FreeBSD-build.sh
@@ -1,6 +1,6 @@
 --- setup/FreeBSD/build.sh.orig	2007-10-25 17:48:59.000000000 -0400
 +++ setup/FreeBSD/build.sh	2007-10-25 17:59:18.000000000 -0400
-@@ -23,11 +23,11 @@
+@@ -23,7 +23,7 @@
  mkdir -p prototype/usr/man/man1
  mkdir -p prototype/usr/man/man8
  
@@ -9,8 +9,3 @@
  
  cp -r $SRCDIR/setup/FreeBSD/oss prototype/usr/lib/
  cp $SRCDIR/kernel/OS/FreeBSD/wrapper/bsddefs.h prototype/usr/lib/oss/build/
--cp $SRCDIR/include/ossddk/oss_exports.h prototype/usr/lib/oss/build/
-+cp $SRCDIR/kernel/framework/include/ossddk/oss_exports.h prototype/usr/lib/oss/build/
- 
- cp $SRCDIR/include/soundcard.h prototype/usr/lib/oss/include/sys/
- #cp $SRCDIR/lib/libOSSlib/midiparser.h prototype/usr/lib/oss/include/
