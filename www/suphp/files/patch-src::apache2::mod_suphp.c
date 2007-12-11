c28f2b3cca2651203a00c50c179535838f975943
Jeremy Chadwick <koitsu@FreeBSD.org>
2007-12-11 20:38:58 +0000

    Upgrade to suPHP 0.6.2.  This upgrade is quite major; it introduces numerous features necessary for hosting, adds the use of a configuration file (vs. compile-time settings) for configuration details, and is presently supported by the software author.
    Changes to httpd.conf are required for this to work, otherwise PHP
    content is offered for download and not parsed.  pkg-message contains
    full migration details, but here's the simple version:
    
    suPHP 0.5.2                            suPHP 0.6.2
    ===========                            ===========
    AddType application/x-httpd-php .php   AddType x-httpd-php .php
    AddHandler x-httpd-php .php            suPHP_AddHandler x-httpd-php
    
    Tested on RELENG_6 and RELENG_7, using Apache 2.0 and 2.2, with the
    help of the PR submitter.
    
    Testing for Apache 1.3 was not performed, thus the port requires the
    use of 2.0 or later.  suPHP 0.6.2 does support Apache 1.3, but I'll
    need someone running 1.3 to work with me on fixing the port to support
    it, if desired.
    
    PR:		ports/116281
    Submitted by:	Vladimir Korkodinov <viper@perm.raid.ru>

--- www/suphp/files/patch-src::apache2::mod_suphp.c
+++ www/suphp/files/patch-src::apache2::mod_suphp.c
@@ -1,58 +1,13 @@
---- src/apache2/mod_suphp.c.orig	Tue Jul 13 04:43:41 2004
-+++ src/apache2/mod_suphp.c	Thu Mar  2 20:14:04 2006
-@@ -56,7 +56,7 @@
-         return -1;
-        
-     rv = apr_bucket_read(b, &bucket_data, &bucket_data_len, APR_BLOCK_READ);
--    if (!APR_STATUS_IS_SUCCESS(rv) || (bucket_data_len == 0))
-+    if (!(rv == APR_SUCCESS) || (bucket_data_len == 0))
-     {
-         return 0;
-     }
-@@ -463,7 +463,13 @@
-     
-     if (((rv = apr_procattr_create(&procattr, p)) != APR_SUCCESS)
-         || ((rv = apr_procattr_io_set(procattr, APR_CHILD_BLOCK, APR_CHILD_BLOCK, APR_CHILD_BLOCK)) != APR_SUCCESS)
-+
-+/* XXX: Pending review by both the Apache APR folks, as well as the suPHP folks.  See thread:
-+ * http://lists.marsching.biz/pipermail/suphp/2004-July/000756.html
-+ */
-+/*
-         || ((rv = apr_procattr_dir_set(procattr, ap_make_dirstr_parent(r->pool, r->filename))) != APR_SUCCESS)
-+*/
-     
-     /* set resource limits */
+--- src/apache2/mod_suphp.c.orig	2006-11-05 16:57:12.000000000 -0800
++++ src/apache2/mod_suphp.c	2007-11-11 03:15:50.000000000 -0800
+@@ -321,8 +321,8 @@
+     AP_INIT_TAKE2("suPHP_UserGroup", suphp_handle_cmd_user_group, NULL, RSRC_CONF | ACCESS_CONF,
+                   "User and group scripts shall be run as"),
+ #endif
+-    AP_INIT_ITERATE("suPHP_AddHandler", suphp_handle_cmd_add_handler, NULL, ACCESS_CONF, "Tells mod_suphp to handle these MIME-types"),
+-    AP_INIT_ITERATE("suPHP_RemoveHandler", suphp_handle_cmd_remove_handler, NULL, ACCESS_CONF, "Tells mod_suphp not to handle these MIME-types"),
++    AP_INIT_ITERATE("suPHP_AddHandler", suphp_handle_cmd_add_handler, NULL, RSRC_CONF | ACCESS_CONF, "Tells mod_suphp to handle these MIME-types"),
++    AP_INIT_ITERATE("suPHP_RemoveHandler", suphp_handle_cmd_remove_handler, NULL, RSRC_CONF | ACCESS_CONF, "Tells mod_suphp not to handle these MIME-types"),
+     {NULL}
+ };
  
-@@ -524,7 +530,9 @@
-             return rv;
-         }
-         
--        APR_BRIGADE_FOREACH(bucket, bb)
-+ 	for (bucket = APR_BRIGADE_FIRST(bb);
-+ 	     bucket != APR_BRIGADE_SENTINEL(bb);
-+ 	     bucket = APR_BUCKET_NEXT(bucket))
-         {
-             const char *data;
-             apr_size_t len;
-@@ -595,7 +603,9 @@
-             
-             const char *buf;
-             apr_size_t blen;
--            APR_BRIGADE_FOREACH(b, bb)
-+ 	    for (b = APR_BRIGADE_FIRST(bb);
-+ 		 b != APR_BRIGADE_SENTINEL(bb);
-+ 		 b = APR_BUCKET_NEXT(b))
-             {
-                 if (APR_BUCKET_IS_EOS(b))
-                     break;
-@@ -616,7 +626,9 @@
-             /* empty brigade (script output) */
-             const char *buf;
-             apr_size_t blen;
--            APR_BRIGADE_FOREACH(b, bb)
-+ 	    for (b = APR_BRIGADE_FIRST(bb);
-+ 		 b != APR_BRIGADE_SENTINEL(bb);
-+ 		 b = APR_BUCKET_NEXT(b))
-             {
-                 if (APR_BUCKET_IS_EOS(b))
-                     break;
