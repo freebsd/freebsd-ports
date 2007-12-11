a74febde07aa3857620d5270567aa3561a47cbe3
Jeremy Messenger <mezz@FreeBSD.org>
2007-12-11 19:22:08 +0000

    Update to 0.5.7.1.

--- net-p2p/deluge05/files/patch-setup.py
+++ net-p2p/deluge05/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	2007-10-25 16:49:47.000000000 -0500
-+++ setup.py	2007-10-25 16:51:47.000000000 -0500
+--- setup.py.orig	2007-11-28 11:49:38.000000000 -0600
++++ setup.py	2007-11-28 11:53:44.000000000 -0600
 @@ -103,13 +103,13 @@
                  "-DHAVE_INCLUDE_LIBTORRENT_ASIO_SSL_STREAM_HPP=1", 
                  "-DHAVE_INCLUDE_LIBTORRENT_ASIO_IP_TCP_HPP=1", 
@@ -15,22 +15,17 @@
 +                     '%%LOCALBASE%%/include']
  
      if OS == "linux":
-         if os.WEXITSTATUS(os.system('grep -iq "Debian GNU/Linux 4.0\|Ubuntu 7.04\|Ubuntu 6.06\|Ubuntu 6.10\|Fedora Core release 6\|openSUSE 10.2\|Mandriva Linux release 2007.1\|Fedora release 7\|BLAG release 60001\|Yellow Dog Linux release 5.0 (Phoenix)\|CentOS release 5 (Final)" /etc/issue')) == 0:
-@@ -164,11 +164,11 @@
- if not OS == "win":
-     if boosttype == "nomt":
-         librariestype = ['boost_filesystem', 'boost_date_time',
--            'boost_thread', 'z', 'pthread', 'ssl']
-+            'boost_thread', 'z', 'ssl']
-         print 'Libraries nomt' 
-     elif boosttype == "mt":
-         librariestype = ['boost_filesystem-mt', 'boost_date_time-mt',
--            'boost_thread-mt', 'z', 'pthread', 'ssl']
-+            'boost_thread-mt', 'z', 'ssl']
-         print 'Libraries mt'
- else:
-         librariestype = ['boost_filesystem-mt', 'boost_date_time-mt',
-@@ -189,8 +189,10 @@
+         if os.path.exists(os.path.join(sysconfig.get_config_vars()['LIBDIR'], \
+@@ -148,7 +148,7 @@
+         boost_thread = "boost_thread-mt"
+ 
+     librariestype = [boost_filesystem, boost_date_time,
+-            boost_thread, 'z', 'pthread', 'ssl']
++            boost_thread, 'z', 'ssl']
+     
+     removals = ['-g', '-Wstrict-prototypes']
+ 
+@@ -198,8 +198,10 @@
      sources.remove('libtorrent/src/file_win.cpp')
      deluge_core = Extension('deluge_core',
                      include_dirs = includedirs,
