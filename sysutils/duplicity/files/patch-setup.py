db41cdc518e227b628d7d4d8f0532fc805139a3e
Vasil Dimov <vd@FreeBSD.org>
2007-12-12 15:02:19 +0000

    Fix packing list problems in sysutils/duplicity: the man pages were installed in share/man/ instead of man/
    Pointyhat to:	vd, Peter Schuller <peter.schuller@infidyne.com>

--- sysutils/duplicity/files/patch-setup.py
+++ sysutils/duplicity/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	Wed Nov 28 19:09:07 2007
-+++ setup.py	Thu Nov 29 20:00:05 2007
+--- setup.py.orig	2007-12-07 17:28:51.000000000 +0200
++++ setup.py	2007-12-12 14:51:55.000000000 +0200
 @@ -21,6 +21,10 @@
  
  incdir_list = libdir_list = None 
@@ -11,12 +11,14 @@
  if os.name == 'posix':
  	LIBRSYNC_DIR = os.environ.get('LIBRSYNC_DIR', '')
  	args = sys.argv[:]
-@@ -49,7 +53,7 @@
+@@ -47,9 +51,6 @@
+ 							   include_dirs=incdir_list,
+ 							   library_dirs=libdir_list,
  							   libraries=["rsync"])],
- 	  scripts = ['rdiffdir', 'duplicity'],
- 	  data_files = [('share/man/man1', ['duplicity.1', 'rdiffdir.1']),
+-	  scripts = ['rdiffdir', 'duplicity'],
+-	  data_files = [('share/man/man1', ['duplicity.1', 'rdiffdir.1']),
 -					('share/doc/duplicity-%s' % version_string,
-+					('share/doc/duplicity',
- 					 ['COPYING', 'README', 'CHANGELOG'])])
+-					 ['COPYING', 'README', 'CHANGELOG'])])
++	  scripts = ['rdiffdir', 'duplicity'])
  
  
