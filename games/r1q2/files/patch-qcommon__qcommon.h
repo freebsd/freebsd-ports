d01ce3880ed6101bf18da26ba5383b48d944b1ae
Alejandro Pulver <alepulver@FreeBSD.org>
2007-12-12 05:18:19 +0000

    - Update to version 7431.

--- games/r1q2/files/patch-qcommon__qcommon.h
+++ games/r1q2/files/patch-qcommon__qcommon.h
@@ -1,5 +1,5 @@
---- ./qcommon/qcommon.h.orig	Sat Sep 16 16:16:40 2006
-+++ ./qcommon/qcommon.h	Sat Dec 16 17:28:37 2006
+--- qcommon/qcommon.h.orig	Mon Nov 19 03:50:37 2007
++++ qcommon/qcommon.h	Mon Nov 19 04:03:27 2007
 @@ -86,7 +86,8 @@
  
  #elif defined __FreeBSD__
@@ -10,3 +10,13 @@
  
  	#ifdef __i386__
  		#define CPUSTRING "i386"
+@@ -1079,7 +1080,8 @@
+ 	TAGMALLOC_CMDBANS,
+ 	TAGMALLOC_REDBLACK,
+ 	TAGMALLOC_LRCON,
+-#ifdef ANTICHEAT
++//#ifdef ANTICHEAT
++#if 1 // always used in sv_main.c, when building client and ANTICHEAT is 0
+ 	TAGMALLOC_ANTICHEAT,
+ #endif
+ 	TAGMALLOC_MAX_TAGS
