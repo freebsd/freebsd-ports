20934e9167adc3cd3cb0e06222c9113a410de41c
Martin Wilke <miwi@FreeBSD.org>
2007-12-12 18:58:57 +0000

    - Update to 2.0
    PR:		118560
    Submitted by:	Nikos Ntarmos <ntarmos@ceid.upatras.gr> (maintainer)

--- x11-wm/awesome/files/patch-config.mk
+++ x11-wm/awesome/files/patch-config.mk
@@ -1,28 +1,28 @@
---- config.mk.orig	2007-09-26 11:41:29.000000000 +0300
-+++ config.mk	2007-09-26 11:42:35.000000000 +0300
-@@ -7,19 +7,19 @@
+--- config.mk.o	2007-11-18 06:20:55.000000000 +0200
++++ config.mk	2007-11-18 06:23:43.000000000 +0200
+@@ -8,20 +8,18 @@
  LAYOUTS = layouts/tile.c layouts/floating.c layouts/max.c
  
  # paths
 -PREFIX = /usr/local
 -MANPREFIX = ${PREFIX}/share/man
-+PREFIX?= /usr/local
-+MANPREFIX = ${PREFIX}/man
  
 -X11INC = /usr/include/X11
 -X11LIB = /usr/lib/X11
-+X11INC = ${X11BASE}/include
-+X11LIB = ${X11BASE}/lib
++X11INC = ${LOCALBASE}/include
++X11LIB = ${LOCALBASE}/lib
  
  # includes and libs
- INCS = -I. -I/usr/include -I${X11INC} `pkg-config --cflags libconfig xft`
- LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 `pkg-config --libs libconfig xft` -lXext -lXrandr -lXinerama
+ INCS = -I. -I/usr/include -I${X11INC} `pkg-config --cflags libconfuse xft cairo`
+ LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 `pkg-config --libs libconfuse xft cairo` -lXext -lXrandr -lXinerama
  
  # flags
--CFLAGS = -fgnu89-inline -std=gnu99 -ggdb3 -pipe -Wall -Wextra -W -Wchar-subscripts -Wundef -Wshadow -Wcast-align -Wwrite-strings -Wsign-compare -Wunused -Wuninitialized -Winit-self -Wpointer-arith -Wredundant-decls -Wno-format-zero-length -Wmissing-prototypes -Wmissing-format-attribute -Wmissing-noreturn -O3 ${INCS} -DVERSION=\"${VERSION}\"
+-CFLAGS = -fgnu89-inline -std=gnu99 -ggdb3 -pipe -Wall -Wextra -W -Wchar-subscripts -Wundef -Wshadow -Wcast-align -Wwrite-strings -Wsign-compare -Wunused -Wuninitialized -Winit-self -Wpointer-arith -Wredundant-decls -Wno-format-zero-length -Wmissing-prototypes -Wmissing-format-attribute -Wmissing-noreturn -O3 ${INCS} -DVERSION=\"${VERSION}\" -DRELEASE=\"${RELEASE}\"
 -LDFLAGS = -ggdb3 ${LIBS}
-+CFLAGS+= -pipe -Wall -W -Wchar-subscripts -Wshadow -Wcast-align -Wwrite-strings -Wunused -Wuninitialized -Wpointer-arith -Wredundant-decls -Wmissing-prototypes ${INCS} -DVERSION=\"${VERSION}\"
+-CLIENTLDFLAGS = -ggdb3
++CFLAGS+= -pipe -Wall -W -Wchar-subscripts -Wshadow -Wcast-align -Wwrite-strings -Wunused -Wuninitialized -Wpointer-arith -Wredundant-decls -Wmissing-prototypes ${INCS} -DVERSION=\"${VERSION}\" -DRELEASE=\"${RELEASE}\"
 +LDFLAGS+= ${LIBS}
++CLIENTLDFLAGS+= ${LIBS}
  
  # compiler and linker
 -CC = cc
