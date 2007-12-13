c0d6bb3bb6c9171000b95cfdcd636a12b4c64935
Pav Lucistnik <pav@FreeBSD.org>
2007-12-13 23:14:42 +0000

    - Update to 0.3.2
    PR:		ports/117885
    Submitted by:	Anish Mistry <amistry@am-productions.biz>

--- net/iaxmodem/files/patch-lib_spandsp_src_spandsp_t4.h
+++ net/iaxmodem/files/patch-lib_spandsp_src_spandsp_t4.h
@@ -1,11 +1,11 @@
---- lib/spandsp/src/spandsp/t4.h.orig	Mon Jun 12 15:54:34 2006
-+++ lib/spandsp/src/spandsp/t4.h	Thu Jun 15 14:17:20 2006
-@@ -28,6 +28,8 @@
+--- lib/spandsp/src/spandsp/t4.h.orig	2007-10-05 20:00:19.000000000 -0400
++++ lib/spandsp/src/spandsp/t4.h	2007-11-06 23:57:56.000000000 -0500
+@@ -27,6 +27,8 @@
  
  /*! \file */
  
 +#include <time.h>
 +
- #if !defined(_T4_H_)
- #define _T4_H_
+ #if !defined(_SPANDSP_T4_H_)
+ #define _SPANDSP_T4_H_
  
