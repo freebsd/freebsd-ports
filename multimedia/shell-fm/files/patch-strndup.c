a18278ebc07fbb3589e039900e4d76274997d6dd
Pav Lucistnik <pav@FreeBSD.org>
2007-12-14 22:09:58 +0000

    - Update to 0.4
    PR:		ports/118138
    Submitted by:	Nikos Ntarmos <ntarmos@ceid.upatras.gr> (maintainer)

--- multimedia/shell-fm/files/patch-strndup.c
+++ multimedia/shell-fm/files/patch-strndup.c
@@ -1,6 +1,6 @@
 --- src/strndup.c.orig	Sat Dec 16 04:21:22 2006
 +++ src/strndup.c	Sat Dec 16 04:20:48 2006
-@@ -0,0 +1,14 @@
+@@ -0,0 +1,17 @@
 +#include "strndup.h"
 +#include <string.h>
 +#include <stdlib.h>
@@ -9,7 +9,10 @@
 +
 +char* strndup(const char*  src, size_t len) {
 +	char* _tmpStr = (char*)malloc(len + 1);
-+	strncpy(_tmpStr, src, len);
++	if (_tmpStr) {
++		strncpy(_tmpStr, src, len);
++		_tmpStr[len] = 0;
++	}
 +	return _tmpStr;
 +}
 +
@@ -31,41 +34,41 @@
 +
 --- src/Makefile.am.orig	Sat Dec 16 04:18:32 2006
 +++ src/Makefile.am	Sat Dec 16 04:18:41 2006
-@@ -6,6 +6,7 @@
+@@ -169,6 +169,7 @@
+ SUBDIRS = include
  INCLUDES = -I$(srcdir)/include
- 
  shell_fm_SOURCES = \
 +strndup.c \
  autoban.c \
  bookmark.c \
  getln.c \
---- src/radio.c.orig	Sat Dec 16 04:25:00 2006
-+++ src/radio.c	Sat Dec 16 02:07:30 2006
-@@ -35,6 +35,7 @@
- #include "settings.h"
- #include "rl_completion.h"
- #include "strarray.h"
-+#include "strndup.h"
- 
- #define LASTFM_URL_PREFIX "lastfm://"
- #define LASTFM_URL_MIN_LENGTH 6 // shortest possible: 'user/a'
---- src/strarray.c.orig	Sat Dec 16 04:25:13 2006
-+++ src/strarray.c	Sat Dec 16 02:07:39 2006
-@@ -22,6 +22,7 @@
- 
- #include "compatibility.h"
- #include "strarray.h"
-+#include "strndup.h"
- 
- void strarray_init (strarray_t *sa) {
- 	sa->strings = NULL;
 --- src/tag.c.orig	Sat Dec 16 04:25:15 2006
 +++ src/tag.c	Sat Dec 16 02:07:49 2006
-@@ -23,6 +23,7 @@
+@@ -22,6 +22,7 @@
  #include "split.h"
  #include "interface.h"
- #include "rl_completion.h"
+ #include "completion.h"
++#include "strndup.h"
+ #include "md5.h"
+ #include "feeds.h"
+
+--- src/feeds.c.o	2007-11-19 18:31:19.000000000 +0200
++++ src/feeds.c	2007-11-19 18:31:43.000000000 +0200
+@@ -8,6 +8,7 @@
+ #include "http.h"
+ #include "feeds.h"
+ #include "tag.h"
++#include "strndup.h"
+
+ char ** neighbors(const char * user) {
+	char * encoded = NULL, feed[128], ** names = NULL;
+--- src/playlist.c.o	2007-11-19 18:31:15.000000000 +0200
++++ src/playlist.c	2007-11-19 18:31:31.000000000 +0200
+@@ -18,6 +18,7 @@
+ #include "http.h"
+ #include "settings.h"
+ #include "service.h"
 +#include "strndup.h"
  
- static char ** getPopularTags(char, struct hash);
- static char * getExistingTags(char, struct hash);
+ #include "playlist.h"
+ #include "globals.h"
