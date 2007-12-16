7db15e411792e88b8d49c2c81dfd35f2b9f9bacd
Marcelo Araujo <araujo@FreeBSD.org>
2007-12-16 13:20:08 +0000

    - Update to 2.2.
    PR:		ports/117014
    Submitted by:	Sergey Prikhodko <sergey@network-asp.biz>
    Approved by:	Josh Tolbert <hemi@puresimplicity.net> (maintainer),
    		stas (mentor, implicit)

--- www/mod_fcgid/files/patch-fcgid_conf.c
+++ www/mod_fcgid/files/patch-fcgid_conf.c
@@ -1,6 +1,6 @@
---- fcgid_conf.c.orig	Mon Jul 26 07:08:03 2004
-+++ fcgid_conf.c	Fri Aug 13 05:04:44 2004
-@@ -15,7 +15,7 @@
+--- fcgid_conf.c.orig	2007-07-31 04:09:20.000000000 -0500
++++ fcgid_conf.c	2007-10-08 08:17:12.000000000 -0500
+@@ -17,8 +17,8 @@
  #define DEFAULT_ERROR_SCAN_INTERVAL 3
  #define DEFAULT_ZOMBIE_SCAN_INTERVAL 3
  #define DEFAULT_PROC_LIFETIME (60*60)
