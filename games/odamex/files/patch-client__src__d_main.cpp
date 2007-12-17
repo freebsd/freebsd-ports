ed021df9c9ba98f79aa9e853ea56b22cf7af4c3f
Alejandro Pulver <alepulver@FreeBSD.org>
2007-12-17 18:16:37 +0000

    Odamex is a free and open source port for the classic first-person-shooter Doom. Odamex's goal is to emulate the feel of and retain many aspects of the original Doom executables while offering a broader expanse of security features, personal configuration, gameplay options, and editing features. Odamex can run on a wide range of operating systems and hardware, so players should be able to play on almost any platform. Features:
    * The popular ZDoom 1.22 core engine and CSDoom 0.62 core netcode.
    * Compatability with many major operating systems, including Windows, Linux,
      FreeBSD and Mac OSX.
    * Core gameplay modeled on the original doom2.exe.
    * Streamlined WAD loading, allowing the server and clients to load WAD files
      on the fly without needing to restart the client or server.
    * Compatability with Boom, MBF and CTF Standard maps.
    * Deathmatch, Cooperative, Team Deathmatch and CTF gametypes.
    * Jumping, Mouselook and other non-standard features available as server-side
      options.
    * Comprehensive cheat and exploit countermeasures.
    * An open source code base licensed under the GPL, available for anyone to
      examine, compile, or modify to their liking.
    
    WWW:	http://odamex.net/

---
+++ games/odamex/files/patch-client__src__d_main.cpp
@@ -0,0 +1,12 @@
+--- ./client/src/d_main.cpp.orig	Sun Nov  4 12:21:20 2007
++++ ./client/src/d_main.cpp	Thu Dec  6 13:58:25 2007
+@@ -727,6 +727,9 @@
+ 	AddSearchDir(dirs, Args.CheckValue("-waddir"), separator);
+ 	AddSearchDir(dirs, getenv("-DOOMWADDIR"), separator);
+ 	AddSearchDir(dirs, getenv("-DOOMWADPATH"), separator);
++	AddSearchDir(dirs, "%%DATADIR%%", separator);
++	AddSearchDir(dirs, "%%DMDIR%%", separator);
++
+ 
+ 	dirs.push_back(startdir);
+ 	dirs.push_back(progdir);
