311264a24fd8c37f9ac75b7d73c911f61e8c4e71
Maho Nakata <maho@FreeBSD.org>
2007-12-19 22:47:57 +0000

    Update to 3.8.0.
    Only verified on i386 and amd64 FreeBSD 7BETA.
    
    Discussions with: bf <bf2006a@yahoo.com>, truckman
    and Tijl Coosemans <tijl@ulyssis.org>.
    
    PR: 118270 and 118271

--- math/atlas/files/patch-tune+sysinfo+L1CacheSize.c
+++ math/atlas/files/patch-tune+sysinfo+L1CacheSize.c
@@ -1,5 +1,5 @@
---- tune/sysinfo/L1CacheSize.c	Mon Dec 22 23:11:53 2003
-+++ ../../L1CacheSize.c	Tue Jun 28 13:37:10 2005
+--- tune/sysinfo/L1CacheSize.c.orig	Tue Dec 19 06:48:02 2006
++++ tune/sysinfo/L1CacheSize.c	Wed Dec 27 12:43:59 2006
 @@ -31,7 +31,7 @@
  #include <stdio.h>
  #include <stdlib.h>
@@ -9,12 +9,12 @@
  
  #ifndef time00
     #define time00 ATL_cputime
-@@ -134,7 +134,7 @@
+@@ -267,7 +267,7 @@
  
  main(int nargs, char *args[])
  {
--   int L1Size, tmp, MaxSize=64, correct=1;
-+   int L1Size, tmp, MaxSize=256, correct=1;
+-   int L1Size, MaxSize=64, correct=1;
++   int L1Size, MaxSize=256, correct=1;
     FILE *L1f;
  
     if (nargs > 2)
