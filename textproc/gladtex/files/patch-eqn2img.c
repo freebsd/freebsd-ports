85127e0d2718010020f0c0704bcc664ac8859b5b
Nicola Vitale <nivit@FreeBSD.org>
2007-12-19 22:26:11 +0000

    - Update to 1.1 - Remove files/patch-gladtex

--- textproc/gladtex/files/patch-eqn2img.c
+++ textproc/gladtex/files/patch-eqn2img.c
@@ -1,21 +1,12 @@
 $FreeBSD$
---- eqn2img.c	Tue Nov  5 18:10:13 2002
-+++ eqn2img.c.port	Sat Feb  4 21:29:22 2006
-@@ -675,7 +675,7 @@
+--- eqn2img.c	2007-10-05 17:51:48.000000000 +0200
++++ eqn2img.c.port	2007-12-19 14:08:52.000000000 +0100
+@@ -691,7 +691,7 @@
      fprintf(stderr, " -> ps");
  
    cmd = NEW(char, 2*strlen(basename) + 35);
--  sprintf(cmd, "dvips -E -o %s.ps %s.dvi &> /dev/null", basename, basename);
-+  sprintf(cmd, "dvips -q -E -o %s.ps %s.dvi", basename, basename);
+-  sprintf(cmd, "dvips -E -o %s.ps %s.dvi > /dev/null 2>&1", basename, basename);
++  sprintf(cmd, "dvips -q -E -o %s.ps %s.dvi > /dev/null 2>&1", basename, basename);
    if(system(cmd)) {
      fprintf(stderr, "\nError running dvips\n");
      return -1;
-@@ -714,7 +714,7 @@
- 	  basename, dpi*supersample, xsize, ysize, basename);
-   gs = popen(cmd, "w");
-   fprintf(gs, "%i neg %i neg translate\n", xoffset, yoffset);
--  fclose(gs);
-+  pclose(gs);
-   sprintf(cmd, "%s.ps", basename);
-   unlink(cmd);
-   free(cmd);
