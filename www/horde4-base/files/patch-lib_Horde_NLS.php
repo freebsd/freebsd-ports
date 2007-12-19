d4f17c6e1a9a4af46bc40cefe30f13c2ccbbd37a
Beech Rintoul <beech@FreeBSD.org>
2007-12-19 00:04:50 +0000

    - Fix bug "isspace broken for UTF-8 locales."   Causes Japanese characters to display improperly.
    PR:		ports/116363
    Submitted by:	Hiromi Kimura <hiromi@tac.tsukuba.ac.jp>
    Approved by:	linimon (mentor)

---
+++ www/horde4-base/files/patch-lib_Horde_NLS.php
@@ -0,0 +1,13 @@
+--- lib/Horde/NLS.php.orig	2007-09-29 07:22:46.000000000 -0800
++++ lib/Horde/NLS.php	2007-12-18 11:16:03.000000000 -0900
+@@ -119,6 +119,10 @@
+                 setlocale(LC_ALL, $lang_charset);
+             }
+         }
++        /* avoid FreeBSD issapce(3) bug */
++        if(NLS::getCharset() == "UTF-8"){
++            setlocale(LC_CTYPE,"C");
++        }
+         @putenv('LANG=' . $lang_charset);
+         @putenv('LANGUAGE=' . $lang_charset);
+     }
