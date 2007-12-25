1cc05d7f9cb0adf026983fdd90deaa1ab5c99c42
acm <acm@FreeBSD.org>
2007-12-25 03:11:39 +0000

    - Update to 1.4
    Submitted by:	Luke Dean <LukeD___ at _pobox.com> via email

--- x11-toolkits/irrlicht/files/patch-source-Irrlicht_COSOperator.cpp
+++ x11-toolkits/irrlicht/files/patch-source-Irrlicht_COSOperator.cpp
@@ -1,5 +1,5 @@
---- source/Irrlicht/COSOperator.cpp	Sun Aug 19 20:14:42 2007
-+++ source/Irrlicht/COSOperator.cpp	Sun Aug 19 20:19:35 2007
+--- source/Irrlicht/COSOperator.cpp	2007-12-24 20:31:04.000000000 -0500
++++ source/Irrlicht/COSOperator.cpp	2007-12-24 20:37:13.000000000 -0500
 @@ -10,12 +10,9 @@
  #else
  #include <string.h>
@@ -13,12 +13,12 @@
  
  namespace irr
  {
-@@ -156,7 +153,7 @@
+@@ -154,7 +151,7 @@
  	_IRR_IMPLEMENT_MANAGED_MARSHALLING_BUGFIX;
  	return true;
  
--#elif defined(_IRR_POSIX_API_) // || defined(MACOSX)
-+#elif defined(_IRR_POSIX_API_) && !defined(__FreeBSD__)// || defined(MACOSX)
+-#elif defined(_IRR_POSIX_API_)
++#elif defined(_IRR_POSIX_API_) && !defined(__FreeBSD__)
+ #if defined(_SC_PHYS_PAGES) && defined(_SC_AVPHYS_PAGES)
          long ps = sysconf(_SC_PAGESIZE);
          long pp = sysconf(_SC_PHYS_PAGES);
-         long ap = sysconf(_SC_AVPHYS_PAGES);
