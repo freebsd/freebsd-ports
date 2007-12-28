50a1d981644b5f35747f70488d78ec4b5359a108
nivit <nivit@FreeBSD.org>
2007-12-28 13:43:52 +0000

    - Forgot to update files/patch-*

--- multimedia/tunapie/files/patch-src-tuner.py
+++ multimedia/tunapie/files/patch-src-tuner.py
@@ -1,6 +1,5 @@
-$FreeBSD$
---- src/tuner.py	Sun Apr 15 13:04:12 2007
-+++ src/tuner.py.port	Wed Apr 18 23:23:51 2007
+--- src/tuner.py	2007-12-12 04:40:38.000000000 +0100
++++ src/tuner.py.port	2007-12-22 22:50:00.000000000 +0100
 @@ -296,7 +296,7 @@
      popen2.Popen4(browser+' '+url)
  
