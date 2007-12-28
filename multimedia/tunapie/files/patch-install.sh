dce735ae755017dcd223ea594d62b7f5b110f6ff
Nicola Vitale <nivit@FreeBSD.org>
2007-12-28 13:43:52 +0000

    - Forgot to update files/patch-*

--- multimedia/tunapie/files/patch-install.sh
+++ multimedia/tunapie/files/patch-install.sh
@@ -1,27 +1,28 @@
-$FreeBSD$
---- install.sh	Tue Apr 10 23:54:02 2007
-+++ install.sh.port	Wed Apr 11 09:15:43 2007
-@@ -1,14 +1,12 @@
+--- install.sh	2007-12-12 04:40:29.000000000 +0100
++++ install.sh.port	2007-12-22 22:55:49.000000000 +0100
+@@ -1,14 +1,13 @@
  #!/bin/sh
 -if [ "$1" = "--adult" ]; then echo '1'>/etc/tunapie.config 
 -else echo '0'>/etc/tunapie.config
 +if [ "$1" = "--adult" ]; then echo '1'>%%LOCALBASE%%/etc/tunapie.config.default
 +else echo '0'>%%LOCALBASE%%/etc/tunapie.config.default
  fi
- python src/compile
+-python src/compile
 -cp tunapie /usr/local/bin
 -rm -rf /usr/local/share/tunapie
 -mkdir /usr/local/share/tunapie
--cp src/*.py src/*.pyc /usr/local/share/tunapie
+-cp src/*.py src/*.pyc src/*.png /usr/local/share/tunapie
 -mkdir -p /usr/local/share/pixmaps
 -cp src/tplogo.xpm /usr/local/share/pixmaps/
 -mkdir -p /usr/local/man/man1
 -cp tunapie.1 /usr/local/man/man1
 -cp tunapie.desktop /usr/local/share/applications
++%%PYTHON_CMD%% %%PYTHON_LIBDIR%%/compileall.py src
 +cp tunapie %%PREFIX%%/bin
 +mkdir %%DATADIR%%
-+cp src/*.py src/*.pyc %%DATADIR%%
++cp src/*.py src/*.pyc src/*.png %%DATADIR%%
 +mkdir -p %%DATADIR%%/pixmaps
 +cp src/tplogo.xpm %%DATADIR%%/pixmaps/
++ln -s %%DATADIR%%/pixmaps/tplogo.xpm %%PREFIX%%/share/pixmaps/tplogo.xpm
 +cp tunapie.1 %%MANPREFIX%%/man/man1
 +cp tunapie.desktop %%DESKTOPDIR%%
