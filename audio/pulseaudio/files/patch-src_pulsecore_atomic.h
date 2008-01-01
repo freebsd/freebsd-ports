26aae04230485d74da0f2efde9ab170bd9186596
Joe Marcus Clarke <marcus@FreeBSD.org>
2008-01-01 22:35:54 +0000

    Update to 0.9.8.

--- audio/pulseaudio/files/patch-src_pulsecore_atomic.h
+++ audio/pulseaudio/files/patch-src_pulsecore_atomic.h
@@ -1,98 +1,91 @@
---- src/pulsecore/atomic.h.orig	Sun May 27 16:38:18 2007
-+++ src/pulsecore/atomic.h	Sun Jul  8 20:29:53 2007
-@@ -24,7 +24,9 @@
-   USA.
- ***/
+--- src/pulsecore/atomic.h.orig	2007-10-28 15:13:53.000000000 -0400
++++ src/pulsecore/atomic.h	2008-01-01 17:20:19.000000000 -0500
+@@ -106,6 +106,88 @@ static inline int pa_atomic_ptr_cmpxchg(
+     return __sync_bool_compare_and_swap(&a->value, (long) old_p, (long) new_p);
+ }
  
--#include <atomic_ops.h>
++#elif defined(__FreeBSD__)
++
 +#include <sys/cdefs.h>
 +#include <sys/types.h>
 +#include <machine/atomic.h>
- 
- /* atomic_ops guarantees us that sizeof(AO_t) == sizeof(void*).
-  *
-@@ -32,7 +34,7 @@
-  * however very likely. */
- 
- typedef struct pa_atomic_int {
--    volatile AO_t value;
-+    volatile unsigned long value;
- } pa_atomic_int_t;
- 
- #define PA_ATOMIC_INIT(v) { .value = (v) }
-@@ -42,47 +44,64 @@ typedef struct pa_atomic_int {
-  * suffixes to the function names */
- 
- static inline int pa_atomic_load(const pa_atomic_int_t *a) {
--    return (int) AO_load_full((AO_t*) &a->value);
-+    return (int) atomic_load_acq_int((unsigned int *) &a->value);
- }
- 
- static inline void pa_atomic_store(pa_atomic_int_t *a, int i) {
--    AO_store_full(&a->value, (AO_t) i);
-+    atomic_store_rel_int((unsigned int *) &a->value, i);
- }
- 
- static inline int pa_atomic_add(pa_atomic_int_t *a, int i) {
--    return AO_fetch_and_add_full(&a->value, (AO_t) i);
-+    atomic_add_int((unsigned int *) &a->value, i);
-+    return (int) a->value;
- }
- 
- static inline int pa_atomic_sub(pa_atomic_int_t *a, int i) {
--    return AO_fetch_and_add_full(&a->value, (AO_t) -i);
-+    atomic_subtract_int((unsigned int *) &a->value, i);
-+    return (int) a->value;
- }
- 
- static inline int pa_atomic_inc(pa_atomic_int_t *a) {
--    return AO_fetch_and_add1_full(&a->value);
-+    atomic_add_int((unsigned int *) &a->value, 1);
-+    return (int) a->value;
- }
- 
- static inline int pa_atomic_dec(pa_atomic_int_t *a) {
--    return AO_fetch_and_sub1_full(&a->value);
-+    atomic_subtract_int((unsigned int *) &a->value, 1);
-+    return (int) a->value;
- }
- 
- static inline int pa_atomic_cmpxchg(pa_atomic_int_t *a, int old_i, int new_i) {
--    return AO_compare_and_swap_full(&a->value, old_i, new_i);
-+    atomic_cmpset_int((unsigned int *) &a->value, old_i, new_i);
-+    return (int) a->value;
- }
- 
- typedef struct pa_atomic_ptr {
--    volatile AO_t value;
-+    volatile unsigned long value;
- } pa_atomic_ptr_t;
- 
- static inline void* pa_atomic_ptr_load(const pa_atomic_ptr_t *a) {
--    return (void*) AO_load_full((AO_t*) &a->value);
++
++typedef struct pa_atomic {
++	volatile unsigned long value;
++} pa_atomic_t;
++
++#define PA_ATOMIC_INIT(v) { .value = (v) }
++
++static inline int pa_atomic_load(const pa_atomic_t *a) {
++	return (int) atomic_load_acq_int((unsigned int *) &a->value);
++}
++
++static inline void pa_atomic_store(pa_atomic_t *a, int i) {
++	atomic_store_rel_int((unsigned int *) &a->value, i);
++}
++
++static inline int pa_atomic_add(pa_atomic_t *a, int i) {
++	int r;
++	r = pa_atomic_load(a);
++	atomic_add_int((unsigned int *) &a->value, i);
++	return r;
++}
++
++static inline int pa_atomic_sub(pa_atomic_t *a, int i) {
++	int r;
++	r = pa_atomic_load(a);
++	atomic_subtract_int((unsigned int *) &a->value, i);
++	return r;
++}
++
++static inline int pa_atomic_inc(pa_atomic_t *a) {
++	int r;
++	r = pa_atomic_load(a);
++	atomic_add_int((unsigned int *) &a->value, 1);
++	return r;
++}
++
++static inline int pa_atomic_dec(pa_atomic_t *a) {
++	int r;
++	r = pa_atomic_load(a);
++	atomic_subtract_int((unsigned int *) &a->value, 1);
++	return r;
++}
++
++static inline int pa_atomic_cmpxchg(pa_atomic_t *a, int old_i, int new_i) {
++	return atomic_cmpset_int((unsigned int *) &a->value, old_i, new_i);
++}
++
++typedef struct pa_atomic_ptr {
++	volatile unsigned long value;
++} pa_atomic_ptr_t;
++
++#define PA_ATOMIC_PTR_INIT(v) { .value = (unsigned long) (v) }
++
++static inline void* pa_atomic_ptr_load(const pa_atomic_ptr_t *a) {
 +#ifdef atomic_load_acq_64
-+    return (void*) atomic_load_acq_ptr((unsigned long *) &a->value);
++	return (void*) atomic_load_acq_ptr((unsigned long *) &a->value);
 +#else
-+    return (void*) atomic_load_acq_ptr((unsigned int *) &a->value);
++	return (void*) atomic_load_acq_ptr((unsigned int *) &a->value);
 +#endif
- }
- 
- static inline void pa_atomic_ptr_store(pa_atomic_ptr_t *a, void *p) {
--    AO_store_full(&a->value, (AO_t) p);
++}
++
++static inline void pa_atomic_ptr_store(pa_atomic_ptr_t *a, void *p) {
 +#ifdef atomic_load_acq_64
-+    atomic_store_rel_ptr(&a->value, (unsigned long) p);
++	atomic_store_rel_ptr(&a->value, (unsigned long) p);
 +#else
-+    atomic_store_rel_ptr((unsigned int *) &a->value, (unsigned int) p);
++	atomic_store_rel_ptr((unsigned int *) &a->value, (unsigned int) p);
 +#endif
- }
- 
- static inline int pa_atomic_ptr_cmpxchg(pa_atomic_ptr_t *a, void *old_p, void* new_p) {
--    return AO_compare_and_swap_full(&a->value, (AO_t) old_p, (AO_t) new_p);
++}
++
++static inline int pa_atomic_ptr_cmpxchg(pa_atomic_ptr_t *a, void *old_p, void* new_p) {
 +#ifdef atomic_load_acq_64
-+    return atomic_cmpset_ptr(&a->value, (unsigned long) old_p, (unsigned long) new_p);
++	return atomic_cmpset_ptr(&a->value, (unsigned long) old_p, (unsigned long) new_p);
 +#else
-+    return atomic_cmpset_ptr((unsigned int *) &a->value, (unsigned int) old_p, (unsigned int) new_p);
++	return atomic_cmpset_ptr((unsigned int *) &a->value, (unsigned int) old_p, (unsigned int) new_p);
 +#endif
- }
++}
++
+ #elif defined(__GNUC__) && (defined(__amd64__) || defined(__x86_64__))
  
- #endif
+ #error "The native atomic operations implementation for AMD64 has not been tested. libatomic_ops is known to not work properly on AMD64 and your gcc version is too old for the gcc-builtin atomic ops support. You have three options now: make the native atomic operations implementation for AMD64 work, fix libatomic_ops, or upgrade your GCC."
--- audio/pulseaudio/files/patch-src_pulsecore_shm.c
+++ audio/pulseaudio/files/patch-src_pulsecore_shm.c
@@ -1,10 +1,11 @@
---- src/pulsecore/shm.c.orig	Thu May 17 11:57:56 2007
-+++ src/pulsecore/shm.c	Thu May 17 11:58:10 2007
-@@ -50,7 +50,7 @@
- #define MAX_SHM_SIZE (1024*1024*20)
+--- src/pulsecore/shm.c.orig	2007-10-28 15:13:53.000000000 -0400
++++ src/pulsecore/shm.c	2008-01-01 16:19:19.000000000 -0500
+@@ -80,7 +80,7 @@ struct shm_marker {
+ };
  
  static char *segment_name(char *fn, size_t l, unsigned id) {
--    snprintf(fn, l, "/pulse-shm-%u", id);
-+    snprintf(fn, l, "/tmp/pulse-shm-%u", id);
+-    pa_snprintf(fn, l, "/pulse-shm-%u", id);
++    pa_snprintf(fn, l, "/tmp/pulse-shm-%u", id);
      return fn;
  }
+ 
