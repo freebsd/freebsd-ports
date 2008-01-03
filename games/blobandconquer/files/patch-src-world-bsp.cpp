00315a33c8929dd5cd107a611d29af9b88ea1152
miwi <miwi@FreeBSD.org>
2008-01-03 21:03:27 +0000

    - Fix build on sparc64
    PR:		119071
    Submitted by:	Dmitry Marakasov <amdmi3@amdmi3.ru> (maintainer)

---
+++ games/blobandconquer/files/patch-src-world-bsp.cpp
@@ -0,0 +1,11 @@
+--- src/world/bsp.cpp.orig	2007-05-22 00:26:13.000000000 +0400
++++ src/world/bsp.cpp	2007-12-31 19:23:29.000000000 +0300
+@@ -22,6 +22,8 @@
+ 
+ #define TESSELATION 10
+ 
++float swapBytes(float f);
++
+ TextureSpec *getTextureSpec(char *basename)
+ {
+ 	// ignore this texture
