4fbde95c552009bd5d715f0e76a81293378e081e
Alejandro Pulver <alepulver@FreeBSD.org>
2008-01-04 17:23:44 +0000

    - Update to version 1.8.2. - Bump PORTEPOCH. - Remove many upstream already integrated patches.

--- games/ezquake/files/patch-snd_dma.c
+++ games/ezquake/files/patch-snd_dma.c
@@ -1,25 +1,11 @@
---- snd_dma.c.orig	Fri Dec 22 02:00:07 2006
-+++ snd_dma.c	Fri Dec 22 02:00:22 2006
-@@ -90,11 +90,11 @@
- cvar_t s_swapstereo = {"s_swapstereo", "0", CVAR_ARCHIVE};
- 
- cvar_t s_khz = {"s_khz", "11", CVAR_ARCHIVE};
--#ifdef __linux__
-+#ifdef __unix__
- cvar_t s_noalsa = {"s_noalsa", "0"};
- // ALSA only -->
+--- snd_dma.c.orig	Fri Jan  4 07:05:02 2008
++++ snd_dma.c	Fri Jan  4 07:06:53 2008
+@@ -94,7 +94,7 @@
+ cvar_t s_khz = {"s_khz", "11", CVAR_ARCHIVE, OnChange_s_khz};
+ #ifdef __FreeBSD__
  cvar_t s_stereo = {"s_stereo", "1"};
--cvar_t s_device = {"s_device", "default"};
+-cvar_t s_device = {"s_device", "/dev/dsp0.0"};
 +cvar_t s_device = {"s_device", "/dev/dsp"};
  cvar_t s_bits = {"s_bits", "16"};
- // <-- ALSA only
  #endif
-@@ -185,7 +185,7 @@
- 	Cvar_Register(&s_show);
- 	Cvar_Register(&s_mixahead);
- 	Cvar_Register(&s_swapstereo);
--#ifdef __linux__
-+#ifdef __unix__
- 	Cvar_Register(&s_noalsa);
- 	Cvar_Register(&s_stereo);
- 	Cvar_Register(&s_device);
+ 
