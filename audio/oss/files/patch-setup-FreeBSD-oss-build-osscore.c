777c45b0c8d0b8c315d040b716933aabf806bd57
jkim <jkim@FreeBSD.org>
2008-01-04 23:25:14 +0000

    Update to BSD-licensed build 1012.
    http://www.opensound.com/press/2008/oss-bsd.txt
    http://www.4front-tech.com/forum/viewtopic.php?t=2406

--- audio/oss/files/patch-setup-FreeBSD-oss-build-osscore.c
+++ audio/oss/files/patch-setup-FreeBSD-oss-build-osscore.c
@@ -1,19 +1,19 @@
---- setup/FreeBSD/oss/build/osscore.c.orig	2007-12-11 15:01:24.000000000 -0500
-+++ setup/FreeBSD/oss/build/osscore.c	2007-12-11 15:06:33.000000000 -0500
-@@ -163,9 +163,13 @@
+--- setup/FreeBSD/oss/build/osscore.c.orig	2008-01-04 17:53:49.000000000 -0500
++++ setup/FreeBSD/oss/build/osscore.c	2008-01-04 17:55:26.000000000 -0500
+@@ -163,12 +163,12 @@
        return -EIO;
      }
  
 -  intr->irq == bus_setup_intr (osdev->dip, intr->irqres,
--			       INTR_TYPE_AV | INTR_MPSAFE, ossintr, intr,
--			       &(intr->cookie));
+-			       INTR_TYPE_AV | INTR_MPSAFE,
 +  intr->irq = bus_setup_intr (osdev->dip, intr->irqres,
 +			      INTR_TYPE_AV | INTR_MPSAFE,
-+#if __FreeBSD_version >= 700031
+ #if __FreeBSD_version >= 700031
+-			       NULL,
 +			      NULL,
-+#endif
-+			      ossintr, intr,
-+			      &(intr->cookie));
+ #endif
+-			       ossintr, intr, &(intr->cookie));
++			      ossintr, intr, &(intr->cookie));
  
    nintrs++;
  
