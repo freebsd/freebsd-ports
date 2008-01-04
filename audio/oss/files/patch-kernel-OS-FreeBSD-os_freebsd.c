36606728a87054204f4f1aa49a42f7be0adb77a2
Jung-uk Kim <jkim@FreeBSD.org>
2008-01-04 23:25:14 +0000

    Update to BSD-licensed build 1012.
    http://www.opensound.com/press/2008/oss-bsd.txt
    http://www.4front-tech.com/forum/viewtopic.php?t=2406

--- audio/oss/files/patch-kernel-OS-FreeBSD-os_freebsd.c
+++ audio/oss/files/patch-kernel-OS-FreeBSD-os_freebsd.c
@@ -1,6 +1,6 @@
---- kernel/OS/FreeBSD/os_freebsd.c.orig	2007-11-12 15:40:51.000000000 -0500
-+++ kernel/OS/FreeBSD/os_freebsd.c	2007-11-12 15:42:12.000000000 -0500
-@@ -28,6 +28,7 @@
+--- kernel/OS/FreeBSD/os_freebsd.c.orig	2008-01-04 17:47:28.000000000 -0500
++++ kernel/OS/FreeBSD/os_freebsd.c	2008-01-04 17:47:30.000000000 -0500
+@@ -21,6 +21,7 @@
  #include <sys/proc.h>
  #include <sys/sx.h>
  #include <sys/mman.h>
@@ -8,12 +8,3 @@
  #include <fs/devfs/devfs.h>
  #include <sys/poll.h>
  
-@@ -153,7 +154,7 @@
-     return 0;
- 
-   wq->flags = 0;
--  flag = msleep (wq, *mutex, PRIBIO | PCATCH, "oss", ticks);
-+  flag = msleep_spin (wq, *mutex, "oss", ticks);
- 
-   if (flag == EWOULDBLOCK)	/* Timeout */
-     {
