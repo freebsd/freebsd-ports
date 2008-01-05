e5ba908fdba45969b9518eb614ec41f887af572f
bsam <bsam@FreeBSD.org>
2008-01-05 14:26:54 +0000

    Update the port to a new version 0.9.5.2.
    Approved by:	Robert Gogolok <gogo at cs.uni-sb.de> (maintainer by email)

---
+++ sysutils/py-bcfg2/files/patch-setup.py
@@ -0,0 +1,13 @@
+--- setup.py.orig	2007-07-20 04:25:34.000000000 +0400
++++ setup.py	2008-01-05 01:08:35.361671089 +0300
+@@ -3,8 +3,8 @@
+ from distutils.core import setup
+ from glob import glob
+ 
+-setup(name="Bcfg2.Server",
+-      version="0.9.5pre1",
++setup(name="bcfg2",
++      version="0.9.5.2",
+       description="Bcfg2 Server",
+       author="Narayan Desai",
+       author_email="desai@mcs.anl.gov",
