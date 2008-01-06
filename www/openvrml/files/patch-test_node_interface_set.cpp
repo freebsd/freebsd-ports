d275f59502e96499bd4ae93a04fbc6cb1b1d2471
Thierry Thomas <thierry@FreeBSD.org>
2008-01-06 10:22:07 +0000

    Upgrade to 0.17.0.
    Note: since 0.17.0, OpenVRML is placed under the terms of version 3 of
    the LGPL and GPL licenses.
    The breakdown of how these licenses are applied to the different parts
    of OpenVRML does not change: libopenvrml and libopenvrml-gl remains
    available under the LGPL, while openvrml-xembed, openvrml-player, the
    Mozilla plug-in, and the example programs remain available under the
    GPL.
    
    Changelog at <http://sourceforge.net/forum/forum.php?forum_id=766275>.

--- www/openvrml/files/patch-test_node_interface_set.cpp
+++
@@ -1,8 +0,0 @@
---- tests/node_interface_set.cpp.orig	Sun Jul  8 23:24:35 2007
-+++ tests/node_interface_set.cpp	Sun Jul  8 23:23:24 2007
-@@ -1,3 +1,5 @@
-+# define BOOST_TEST_DYN_LINK
-+# define BOOST_TEST_MAIN
- # include <boost/test/unit_test.hpp>
- # include <openvrml/node.h>
- 
