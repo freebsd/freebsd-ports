c6b7c810bacb7dfb0c79e3801d2935339660f126
db <db@FreeBSD.org>
2008-01-08 18:23:26 +0000

    - Update to 3.1.1

--- comms/gnuradio/files/patch-gr-wxgui_src_python_stdgui.py
+++ comms/gnuradio/files/patch-gr-wxgui_src_python_stdgui.py
@@ -1,5 +1,5 @@
---- gr-wxgui/src/python/stdgui.py.orig	Sat Feb 24 10:05:41 2007
-+++ gr-wxgui/src/python/stdgui.py	Sat Feb 24 10:06:13 2007
+--- gr-wxgui/src/python/stdgui.py.orig	2007-11-05 19:54:57.000000000 -0500
++++ gr-wxgui/src/python/stdgui.py	2008-01-03 13:20:37.000000000 -0500
 @@ -21,6 +21,8 @@
  
  '''A simple wx gui for GNU Radio applications'''
