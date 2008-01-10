7d228a626b661c04ec2b0a1c7f93556236678fa9
Boris Samorodov <bsam@FreeBSD.org>
2008-01-10 19:49:49 +0000

    Update to last bugfix release 0.9.5.3.
    PR:		119473
    Submitted by:	Robert Gogolok <gogo at cs.uni-sb.de> (maintainer)

--- sysutils/py-bcfg2/files/patch-setup.py
+++ sysutils/py-bcfg2/files/patch-setup.py
@@ -1,13 +1,11 @@
---- setup.py.orig	2007-07-20 04:25:34.000000000 +0400
-+++ setup.py	2008-01-05 01:08:35.361671089 +0300
-@@ -3,8 +3,8 @@
+--- setup.py.orig	Wed Jan  9 00:30:05 2008
++++ setup.py	Wed Jan  9 00:30:31 2008
+@@ -3,7 +3,7 @@
  from distutils.core import setup
  from glob import glob
  
 -setup(name="Bcfg2.Server",
--      version="0.9.5pre1",
 +setup(name="bcfg2",
-+      version="0.9.5.2",
+       version="0.9.5.3",
        description="Bcfg2 Server",
        author="Narayan Desai",
-       author_email="desai@mcs.anl.gov",
