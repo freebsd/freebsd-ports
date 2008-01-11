588c273e3d4917a5568991d873f947d24cb9ddaf
Florent Thoumie <flz@FreeBSD.org>
2008-01-11 12:49:29 +0000

    - Update port to 0.2.901. - Fix build.
    Submitted by:	jkim

--- x11-drivers/xf86-video-openchrome/files/patch-libxvmc_viaXvMCPriv.h
+++
@@ -1,6 +0,0 @@
---- libxvmc/viaXvMCPriv.h.orig	Wed Jan 25 15:56:20 2006
-+++ libxvmc/viaXvMCPriv.h	Sun Nov 25 09:43:03 2007
-@@ -31,2 +31,3 @@
- #include <stdlib.h>
-+#include <pthread.h>		/* For FreeBSD 5.x */
- #include <X11/Xutil.h>
