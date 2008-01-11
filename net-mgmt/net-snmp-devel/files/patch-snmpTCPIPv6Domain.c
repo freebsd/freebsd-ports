79474c7e0603ff0158c36d98d902634058133d19
kuriyama <kuriyama@FreeBSD.org>
2008-01-11 23:17:32 +0000

    - Fix IPv6 handling for libwrap (1). - Show IPv4 port number as unsigned instead of signed in /var/log/snmpd.log. - Treat physmem as u_long, not int.  This fixes hrMmemorySize over 4GB.
    Submitted by:	ume (1)
    Pointy hat to:	kuriyama (1)

--- net-mgmt/net-snmp-devel/files/patch-snmpTCPIPv6Domain.c
+++ net-mgmt/net-snmp-devel/files/patch-snmpTCPIPv6Domain.c
@@ -1,12 +1,14 @@
---- snmplib/snmpTCPIPv6Domain.c.orig	Sat Oct 16 03:52:29 2004
-+++ snmplib/snmpTCPIPv6Domain.c	Mon Oct 25 09:22:06 2004
-@@ -95,13 +95,21 @@
+Index: snmplib/snmpTCPIPv6Domain.c
+diff -u -p snmplib/snmpTCPIPv6Domain.c.orig snmplib/snmpTCPIPv6Domain.c
+--- snmplib/snmpTCPIPv6Domain.c.orig	Fri Sep 16 19:30:49 2005
++++ snmplib/snmpTCPIPv6Domain.c	Fri Jan 11 13:40:09 2008
+@@ -96,13 +96,24 @@ netsnmp_tcp6_fmtaddr(netsnmp_transport *
      if (to == NULL) {
          return strdup("TCP/IPv6: unknown");
      } else {
 -        char addr[INET6_ADDRSTRLEN];
 -        char tmp[INET6_ADDRSTRLEN + 8];
-+        char tmp[NI_MAXHOST];
++	char addr[NI_MAXHOST], tmp[NI_MAXHOST + NI_MAXSERV + 12];
  
 -        sprintf(tmp, "TCP/IPv6: [%s]:%hd",
 -                inet_ntop(AF_INET6, (void *) &(to->sin6_addr), addr,
@@ -20,10 +22,13 @@
 +#ifndef NI_WITHSCOPEID
 +#define NI_WITHSCOPEID 0
 +#endif
-+	if (getnameinfo(to, sizeof(struct sockaddr_in6), tmp, sizeof(tmp),
-+			NULL, 0, NI_NUMERICHOST | NI_WITHSCOPEID)) {
++	if (getnameinfo((struct sockaddr *)to, sizeof(struct sockaddr_in6),
++			addr, sizeof(addr), NULL, 0,
++			NI_NUMERICHOST | NI_WITHSCOPEID)) {
 +	    return strdup("UDP/IPv6: unknown");
 +	}
++	snprintf(tmp, sizeof(tmp), "TCP/IPv6: [%s]:%hd", addr,
++		 ntohs(to->sin6_port));
 +	return strdup(tmp);
      }
  }
