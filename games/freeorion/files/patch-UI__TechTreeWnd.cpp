92e55a7773cd434a6e19e7bd56fdb094b7fcc597
alepulver <alepulver@FreeBSD.org>
2008-01-11 02:07:43 +0000

    - Fix local patch that was producing a crash. - Bump PORTREVISION.

--- games/freeorion/files/patch-UI__TechTreeWnd.cpp
+++ games/freeorion/files/patch-UI__TechTreeWnd.cpp
@@ -1,13 +1,14 @@
 --- ./UI/TechTreeWnd.cpp.orig	Fri Aug 10 21:45:52 2007
-+++ ./UI/TechTreeWnd.cpp	Sat Sep  8 15:48:32 2007
-@@ -55,6 +55,10 @@
-     const double MIN_SCALE = 0.1073741824;  // = 1.0/(1.25)^10
-     const double MAX_SCALE = 1.0;
++++ ./UI/TechTreeWnd.cpp	Thu Jan 10 23:14:08 2008
+@@ -20,6 +20,11 @@
+ #include <boost/format.hpp>
+ #include <algorithm>
  
-+    const double PI = 3.141594;
++#define PI M_PI
 +
-+    double PS2INCH(double x) { return PS2INCH(x); }
-+
-     pointf Bezier(pointf* patch, double t)
-     {
-         pointf temp[6][6];
++#define POINTS_PER_INCH	72
++#define POINTS(f_inch)	(ROUND((f_inch)*POINTS_PER_INCH))
++#define PS2INCH(ps)		((ps)/(double)POINTS_PER_INCH)
+ 
+ namespace {
+     // command-line options
