9bd60fbb22e8db21865d22c11575cddd69878940
Martin Wilke <miwi@FreeBSD.org>
2008-01-12 16:44:45 +0000

    Despite the name, it has nothing to do with apricots. It's a game where you fly a little plane around the screen and shoot things and drop bombs on enemy targets, and it's meant to be quick and fun.
    There can be up to 6 planes, at most two human-controlled. All
    others will be computer-controlled. Network support is in to do
    list.
    
    You must shoot at another's planes, destroy others' base and try
    to keep alive. You may return to your base to refill your ammo. The
    scenery is randomly generated.
    
    WWW: http://www.fishies.org.uk/apricots.html
    
    PR:		ports/119439
    Submitted by:	Dmitry Marakasov <amdmi3 at amdmi3.ru>

---
+++ games/apricots/files/patch-sampleio.cpp
@@ -0,0 +1,19 @@
+--- apricots/sampleio.cpp.orig	2003-08-06 03:21:22.000000000 +0400
++++ apricots/sampleio.cpp	2008-01-08 02:12:27.000000000 +0300
+@@ -81,16 +81,10 @@
+     ALsizei freq;
+     ALboolean fileok;
+     // Evil OpenAL portability fix done here
+-#ifdef _WIN32
+     ALenum format;
+     ALboolean trash;
+     alutLoadWAVFile(filenames[i],&format,&data,&filelen,&freq,&trash);
+     fileok = (alGetError() == AL_NO_ERROR);
+-#else
+-    ALsizei format;
+-    ALsizei trash;
+-    fileok = alutLoadWAV(filenames[i],&data,&format,&filelen,&trash,&freq);
+-#endif
+     if (!fileok){
+       cerr << "sampleio: could not open " << filenames[i] << endl;
+       exit(1);
