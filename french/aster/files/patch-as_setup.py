e4b2b93d5e5a4cf2a6c76705b60b6f94e1e10c14
thierry <thierry@FreeBSD.org>
2008-01-12 16:28:40 +0000

    Upgrade Code_Aster to 9.2.0-2.

--- french/aster/files/patch-as_setup.py
+++ french/aster/files/patch-as_setup.py
@@ -1,6 +1,17 @@
---- as_setup.py.orig	Wed Jun 28 18:04:25 2006
-+++ as_setup.py	Sun Sep 24 12:23:18 2006
-@@ -1170,7 +1170,10 @@
+--- as_setup.py.orig	2007-12-21 17:08:17.000000000 +0100
++++ as_setup.py	2008-01-08 22:32:14.000000000 +0100
+@@ -397,6 +397,10 @@
+                traceback.print_exc()
+                self.exit_code=iret
+                raise SetupExtractError, _('error during extracting archive %s') % archive
++            # Insert FreeBSD patches here
++            self._print('Handle gfortran C-like pre-processor directives')
++            os.rename(os.path.join(self.content, 'bibf90/utilitai/traceb.f'), os.path.join(self.content, 'bibf90/utilitai/traceb.F'))
++            # End of FreeBSD patches
+ 
+       if iextr_as:
+          iret=0
+@@ -1216,7 +1220,10 @@
        from socket import gethostname, gethostbyaddr
        if host==None:
           host = gethostname()
