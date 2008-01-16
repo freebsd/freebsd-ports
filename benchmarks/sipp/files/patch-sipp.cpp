05f80dd7bd455b98df8b85df927438d40af6e0fd
araujo <araujo@FreeBSD.org>
2008-01-16 22:43:01 +0000

    - Update to 3.0.
    PR:		ports/119698
    Submitted by:	Artem Naluzhnyy <tut@nhamon.com.ua> (maintainer)
    Approved by:	itetcu (mentor, implicit)

---
+++ benchmarks/sipp/files/patch-sipp.cpp
@@ -0,0 +1,11 @@
+--- sipp.cpp.orig	2007-11-28 17:09:53.000000000 +0200
++++ sipp.cpp	2008-01-15 22:57:14.000000000 +0200
+@@ -3627,7 +3627,7 @@
+ 	  }
+ 	  exit(EXIT_OTHER);
+ 	case SIPP_OPTION_VERSION:
+-	  printf("\n SIPp v2.0"
++	  printf("\n SIPp v3.0"
+ #ifdef _USE_OPENSSL
+ 	      "-TLS"
+ #endif
---
+++ benchmarks/sipp/files/patch-sipp.hpp
@@ -0,0 +1,11 @@
+--- sipp.hpp.orig	2007-11-28 12:33:23.000000000 +0200
++++ sipp.hpp	2008-01-15 22:57:46.000000000 +0200
+@@ -93,7 +93,7 @@
+ 
+ /************************** Constants **************************/
+ 
+-#define SIPP_VERSION               20071128
++#define SIPP_VERSION               20071204
+ #define T_UDP                      0
+ #define T_TCP                      1
+ #define T_TLS                      2
