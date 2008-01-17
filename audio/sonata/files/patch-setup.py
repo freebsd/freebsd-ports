9a6ee9c3b4047ffa9f6ea5b6f2245817dae39758
Nicola Vitale <nivit@FreeBSD.org>
2008-01-17 09:50:43 +0000

    - Update to 1.4

--- audio/sonata/files/patch-setup.py
+++ audio/sonata/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py	2007-10-30 02:41:45.000000000 +0100
-+++ setup.py.port	2008-01-10 14:01:07.000000000 +0100
-@@ -43,6 +43,29 @@
+--- setup.py	2008-01-16 05:25:41.000000000 +0100
++++ setup.py.port	2008-01-16 23:09:45.000000000 +0100
+@@ -43,6 +43,30 @@
  	print "generating", mofile
  	os.system("msgfmt %s -o %s" % (pofile, mofile))
  
 +port_data_files = [ ('share/applications', ['sonata.desktop']),
-+                    ('share/pixmaps', ['pixmaps/sonata.png', 'pixmaps/sonata_large.png', 'pixmaps/sonatacd.png', 'pixmaps/sonatacd_large.png', 'pixmaps/sonata-artist.png', 'pixmaps/sonata-album.png', 'pixmaps/sonata-stock_volume-mute.png', 'pixmaps/sonata-stock_volume-min.png', 'pixmaps/sonata-stock_volume-med.png', 'pixmaps/sonata-stock_volume-max.png', 'pixmaps/sonata_pause.png', 'pixmaps/sonata_play.png', 'pixmaps/sonata_disconnect.png']),
++		    ('share/pixmaps', ['pixmaps/sonata.png', 'pixmaps/sonata_large.png', 'pixmaps/sonatacd.png', 'pixmaps/sonatacd_large.png', 'pixmaps/sonata-artist.png', 'pixmaps/sonata-album.png', 'pixmaps/sonata-stock_volume-mute.png', 'pixmaps/sonata-stock_volume-min.png', 'pixmaps/sonata-stock_volume-med.png', 'pixmaps/sonata-stock_volume-max.png', 'pixmaps/sonata_pause.png', 'pixmaps/sonata_play.png', 'pixmaps/sonata_disconnect.png']),
 +                    ('share/locale/de/LC_MESSAGES', ['mo/de/sonata.mo']),
 +                    ('share/locale/pl/LC_MESSAGES', ['mo/pl/sonata.mo']),
 +                    ('share/locale/ru/LC_MESSAGES', ['mo/ru/sonata.mo']),
@@ -19,25 +19,26 @@
 +                    ('share/locale/cs/LC_MESSAGES', ['mo/cs/sonata.mo']),
 +                    ('share/locale/da/LC_MESSAGES', ['mo/da/sonata.mo']),
 +                    ('share/locale/pt_BR/LC_MESSAGES', ['mo/pt_BR/sonata.mo']),
++                    ('share/locale/be@latin/LC_MESSAGES', ['mo/be@latin/sonata.mo']),
 +                    ('share/locale/uk/LC_MESSAGES', ['mo/uk/sonata.mo'])]
 +
 +if not os.getenv("NOPORTDOCS"):
-+	port_data_files.append(('/usr/local/share/doc/sonata', ['README']))
++	port_data_files.append(('%%DOCSDIR%%', ['README']))
 +
 +if not os.getenv("NO_INSTALL_MANPAGES"):
 +	port_data_files.append(('man/man1', ['sonata.1']))
 +
  setup(name='Sonata',
-         version='1.3',
+         version='1.4',
          description='GTK+ client for the Music Player Daemon (MPD).',
-@@ -65,24 +88,7 @@
+@@ -65,27 +89,9 @@
          extra_link_args=capture("pkg-config --libs gtk+-2.0 pygtk-2.0").split()
           ),],
          scripts = ['sonata'],
 -        data_files=[('share/sonata', ['README', 'CHANGELOG', 'TODO', 'TRANSLATORS']),
 -                    ('share/applications', ['sonata.desktop']),
 -                    ('share/pixmaps', ['pixmaps/sonata.png', 'pixmaps/sonata_large.png', 'pixmaps/sonatacd.png', 'pixmaps/sonatacd_large.png', 'pixmaps/sonata-artist.png', 'pixmaps/sonata-album.png', 'pixmaps/sonata-stock_volume-mute.png', 'pixmaps/sonata-stock_volume-min.png', 'pixmaps/sonata-stock_volume-med.png', 'pixmaps/sonata-stock_volume-max.png', 'pixmaps/sonata_pause.png', 'pixmaps/sonata_play.png', 'pixmaps/sonata_disconnect.png']),
--                    ('man/man1', ['sonata.1']),
+-                    ('share/man/man1', ['sonata.1']),
 -                    ('share/locale/de/LC_MESSAGES', ['mo/de/sonata.mo']),
 -                    ('share/locale/pl/LC_MESSAGES', ['mo/pl/sonata.mo']),
 -                    ('share/locale/ru/LC_MESSAGES', ['mo/ru/sonata.mo']),
@@ -51,8 +52,13 @@
 -                    ('share/locale/cs/LC_MESSAGES', ['mo/cs/sonata.mo']),
 -                    ('share/locale/da/LC_MESSAGES', ['mo/da/sonata.mo']),
 -                    ('share/locale/pt_BR/LC_MESSAGES', ['mo/pt_BR/sonata.mo']),
+-                    ('share/locale/be@latin/LC_MESSAGES', ['mo/be@latin/sonata.mo']),
 -                    ('share/locale/uk/LC_MESSAGES', ['mo/uk/sonata.mo'])],
+-        )
+-
 +	data_files = port_data_files,
-         )
- 
++	)
++	
  # Cleanup (remove /build, /mo, and *.pyc files:
+ print "Cleaning up..."
+ try:
