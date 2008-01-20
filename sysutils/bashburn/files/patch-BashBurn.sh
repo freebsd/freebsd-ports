6704901561305af92c986e32633b104912ab4c5d
Marcelo Araujo <araujo@FreeBSD.org>
2008-01-20 04:43:41 +0000

    - Update to 2.1.2.
    PR:		ports/119757
    Submitted by:	Felippe de Meirelles Motta <lippemail@gmail.com>
    Reworked by:	Martin Tournoil <carpetsmoker@xs4all.nl> (maintainer)
    Approved by:	stas (mentor, implicit)

--- sysutils/bashburn/files/patch-BashBurn.sh
+++ sysutils/bashburn/files/patch-BashBurn.sh
@@ -1,24 +1,15 @@
---- BashBurn.sh.orig	2007-07-18 21:48:55.000000000 +0200
-+++ BashBurn.sh	2007-08-24 14:58:30.000000000 +0200
-@@ -31,18 +31,13 @@
+--- BashBurn.sh.orig	2007-11-09 19:02:46.000000000 +0100
++++ BashBurn.sh	2008-01-17 23:17:18.000000000 +0100
+@@ -31,7 +31,11 @@
  # This is run at first
  init_bashburn()
  {
 -	if [[ -r "${BBCONFFILE}" ]]; then	# If /etc/bashburnrc exists, read in the vars.
-+	if [[ ! -r "${BBCONFFILE}" ]]; then
-+		cp ${BBCONFFILE}.sample ${BBCONFFILE}
-+	fi
- 		export BBROOTDIR="$( cat $BBCONFFILE | grep -v '^#' | grep BBROOTDIR:  | cut -d ":" -f 2- | sed -e "s/^[[:blank:]]//g")"
++ 	if [[ ! -f "${BBCONFFILE}" ]]; then
++ 		cp ${BBCONFFILE}.sample ${BBCONFFILE}
++ 	fi
++
++ 	if [[ -r "${BBCONFFILE}" ]]; then
+ 		# Next line a tip from paddor
+ 		export BBROOTDIR="$( sed -n 's/^[[:blank:]]*BBROOTDIR:[[:blank:]]*\(.*\)/\1/p' < $BBCONFFILE)"
  		source ${BBROOTDIR}/misc/commands.idx
- 		source ${BBROOTDIR}/misc/variables.idx
- 		export BBTEMPMOUNTDIR="${BBBURNDIR}/mnt"
--	else
--		echo "There is no '${BBCONFFILE}'" 1>&2		
--		echo "That is the file where BashBurn stores its settings."
--		echo "You must run the BashBurn installation script first as root to install"
--		echo "all needed files. Do that now and then start 'bashburn' again."
--		exit 1
--	fi
- 	
- 	# Read in the language file
- 	source ${BBROOTDIR}/lang/${BBLANG}/BashBurn.lang
