f00cf79f17d39b13029076cc789389668f5b1ff5
miwi <miwi@FreeBSD.org>
2008-01-20 21:57:05 +0000

    - Update to 1.0a
    PR:		119798
    Submitted by:	peter.schuller@infidyne.com (maintainer)

--- devel/py-boto/files/patch-setup.py
+++ devel/py-boto/files/patch-setup.py
@@ -1,12 +1,12 @@
-# Prevent installation of test.py in PREFIX/bin, which would easily
-# conflict with other software.
---- setup.py.orig	Thu Aug 23 19:01:03 2007
-+++ setup.py	Thu Aug 23 19:01:22 2007
-@@ -37,7 +37,7 @@
+--- setup.py.orig	2008-01-19 10:06:45.253272917 +0100
++++ setup.py	2008-01-19 10:07:03.283255219 +0100
+@@ -36,8 +36,8 @@
+       author_email = "mitch@garnaat.com",
        url = "http://code.google.com/p/boto/",
-       packages = [ 'boto', 'boto.sqs', 'boto.s3', 'boto.ec2',
-                    'boto.mturk', 'tests'],
+       packages = [ 'boto', 'boto.sqs', 'boto.s3', 'boto.ec2', 'boto.sdb',
+-                   'boto.mturk', 'boto.contrib', 'tests'],
 -      scripts=['test.py'],
++                   'boto.mturk', 'boto.contrib' ],
 +      scripts=[],
        license = 'MIT',
        platforms = 'Posix; MacOS X; Windows',
