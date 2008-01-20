8f7854ccc168815ca0155423e831d66fbf0c5743
Martin Wilke <miwi@FreeBSD.org>
2008-01-20 21:55:39 +0000

    - Update to 1.10 - Respect PORTDOCS - Respect PORTEXAMPLES
    PR:		119234
    Submitted by:	Gerrit Beine <gerrit.beine@gmx.de> (maintainer)

--- deskutils/vym/files/patch-linkablemapobj.cpp
+++
@@ -1,8 +0,0 @@
---- linkablemapobj.cpp.orig	Fri Jul  8 09:24:42 2005
-+++ linkablemapobj.cpp	Sat Aug  6 17:22:48 2005
-@@ -1,3 +1,5 @@
-+#include <math.h>
-+
- #include "linkablemapobj.h"
- #include "branchobj.h"
- #include "mapeditor.h"
