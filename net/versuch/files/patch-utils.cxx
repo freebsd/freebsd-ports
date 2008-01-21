5970d5a7ea266fecf39f45f5d2d5e98c175bfe62
miwi <miwi@FreeBSD.org>
2008-01-21 11:41:58 +0000

    - Fix build with gcc4.2
    PR:		119707
    Submitted by:	Philippe Audeoud <jadawin@tuxaco.net>

---
+++ net/versuch/files/patch-utils.cxx
@@ -0,0 +1,12 @@
+--- Utils.cxx.orig	2008-01-15 16:01:46.000000000 +0000
++++ Utils.cxx	2008-01-15 16:02:07.000000000 +0000
+@@ -10,7 +10,8 @@
+ #include "Utils.h"
+ 
+ #include <fstream>
+-
++#include <assert.h>
++
+ void wtrim(string& str) {
+  const char* sp_chars = " \t\r\n";
+  unsigned int fst_notsp = str.find_first_not_of(sp_chars);
