9f80c4ac6c9ecc51e83662b28f5c421716c8ad2c
Nicola Vitale <nivit@FreeBSD.org>
2008-01-25 17:37:57 +0000

    PyChess is a gnome-inspired chess client written in Python.
    Features
    
        * CECP and UCI Engine support
        * Free on-line play on the FICS chess servers
        * Read and writes the PGN, EPD and FEN chess file formats
        * Built-in Python based engine
        * Undo and pause functions
        * Board and piece animation
        * Drag and drop
        * Tabbed interface
        * Hints and spy arrows
        * Opening book side panel using sqlite
        * Scoreplot side panel
        * "Enter game" in pgn dialog
        * Internationalised or figure pieces in notation
        * Optional sounds
        * Legal move highlighting
        * A nice and accessible look
    
    WWW:	http://pychess.googlepages.com/

---
+++ games/py-pychess/files/patch-setup.py
@@ -0,0 +1,12 @@
+--- setup.py	2008-01-18 13:57:53.000000000 +0100
++++ setup.py.port	2008-01-23 19:03:05.000000000 +0100
+@@ -58,7 +58,8 @@
+ DATA_FILES += [('share/gtksourceview-1.0/language-specs', ['gtksourceview-1.0/language-specs/pgn.lang'])]
+ 
+ # Manpages
+-DATA_FILES += [('share/man/man1', ['manpages/pychess.1.gz'])]
++if not os.getenv('NO_INSTALL_MANPAGES'):
++    DATA_FILES += [('man/man1', ['manpages/pychess.1.gz'])]
+ 
+ # Language
+ pofile = "LC_MESSAGES/pychess"
