a6834817f917d6614b059199b64b259e0ff514b9
Xin LI <delphij@FreeBSD.org>
2008-01-26 21:33:52 +0000

    Add a patch to allow stardict2 to build on gcc 4.x.
    Submitted by:	Hu Dong <hoodng at hotmail dot com>
    PR:		ports/119947

--- textproc/stardict3/files/patch-src::mainwin.h
+++ textproc/stardict3/files/patch-src::mainwin.h
@@ -1,5 +1,5 @@
---- src/mainwin.h.orig	Tue Sep 23 18:19:44 2003
-+++ src/mainwin.h	Tue Jan 18 02:23:04 2005
+--- ./src/mainwin.h.orig	2006-06-29 22:29:34.000000000 -0700
++++ ./src/mainwin.h	2008-01-26 13:26:47.532687040 -0800
 @@ -22,7 +22,7 @@
  	TEXT_WIN_PATTERN_NOT_FOUND,
  	TEXT_WIN_TIPS, // when the word entry is empty,it will show tip
@@ -8,4 +8,22 @@
 +	TEXT_WIN_TREEDICT
  };
  
- enum ListWinListWordType
+ enum ListWinListWordType {
+@@ -68,8 +68,6 @@
+ 	static void on_main_menu_about_activate(GtkMenuItem *menuitem, TopWin *oTopWin);
+ 	static void on_main_menu_quit_activate(GtkMenuItem *menuitem, TopWin *oTopWin);
+ 
+-	static void ClipboardReceivedCallback(GtkClipboard *clipboard, const gchar *text, gpointer data);
+-
+ 	void LoadHistory(void);
+ 	void SaveHistory(void);
+ public:
+@@ -79,6 +77,8 @@
+ 	TopWin();
+ 	~TopWin();
+ 
++	static void ClipboardReceivedCallback(GtkClipboard *clipboard, const gchar *text, gpointer data);
++
+ 	void Create(GtkWidget *vbox);
+ 	void Destroy(void);
+ 	void SetText(const gchar *word, bool notify=true);
