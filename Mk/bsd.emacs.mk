be9c1e9f59d2f9a89ef2cc44826418733ff683da
Andrey Slusar <anray@FreeBSD.org>
2008-01-27 14:26:42 +0000

    - Update editors/emacs-devel to 23.0.50.20080101 snapshot.
    PR:		ports/119290
    Submitted by:	keramida(maintainer)

--- Mk/bsd.emacs.mk
+++ Mk/bsd.emacs.mk
@@ -1,5 +1,5 @@
 #
-#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.70 2007-12-13 19:25:09 anray Exp $
+#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.71 2008-01-27 14:26:42 anray Exp $
 #
 #	bsd.emacs.mk - 19990829 Shigeyuki Fukushima.
 #
@@ -100,8 +100,8 @@ PLIST?=			${PKGDIR}/pkg-plist.${EMACS_PORT_NAME}
 # Emacs-22.x (development version)
 .elif (${EMACS_PORT_NAME} == "emacs-devel")
 EMACS_NAME=		emacs
-EMACS_VER=		22.0.99
-EMACS_MAJOR_VER=	22
+EMACS_VER=		23.0.50
+EMACS_MAJOR_VER=	23
 EMACS_LIBDIR?=		share/${EMACS_NAME}
 EMACS_LIBDIR_WITH_VER?=	share/${EMACS_NAME}/${EMACS_VER}
 EMACS_PORTSDIR=		${PORTSDIR}/editors/emacs-devel
