f936a879f944f14c6a4c13271443c5cfd68aaa82
Pav Lucistnik <pav@FreeBSD.org>
2008-01-28 21:10:18 +0000

    - Update to 0.8.6

--- science/gchemutils/files/patch-gcu-formula.cc
+++ science/gchemutils/files/patch-gcu-formula.cc
@@ -20,12 +20,3 @@
  using namespace gcu;
  
  parse_error::parse_error(const string& __arg, int start, int length)
-@@ -448,7 +461,7 @@
- 			atom_weight = Element::GetElement ((*i).first)->GetWeight (atom_prec);
- 			if (atom_prec == 0)
- 				m_Artificial = true;
--			delta += pow10 (-atom_prec) * (*i).second;
-+			delta += pow (10.0, -atom_prec) * (*i).second;
- 			m_Weight += atom_weight * (*i).second;
- 		}
- 		m_WeightPrec = (int) ceil (-log10 (delta) - 1e-5);
