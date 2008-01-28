214a9538860617a755d56629cbe01f7eff686787
dryice <dryice@FreeBSD.org>
2008-01-28 09:19:17 +0000

    The peak.util.extremes module provides a production-quality implementation of the Min and Max objects from PEP 326.
    PR:		ports/119915
    Submitted by:	alexander smishlajev <alex@tycobka.lv>

---
+++ devel/py-extremes/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	Wed Nov 21 14:25:46 2007
++++ setup.py	Tue Jan 22 11:28:34 2008
+@@ -1,8 +1,6 @@
+ #!/usr/bin/env python
+ """Distutils setup file"""
+ 
+-import ez_setup
+-ez_setup.use_setuptools()
+ from setuptools import setup
+ 
+ # Metadata
