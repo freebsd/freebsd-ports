70d6518eaa0bf66416c901a1f7becfe13467b199
danfe <danfe@FreeBSD.org>
2008-01-29 16:03:18 +0000

    - Fix "Couldn't create network handler" error [1] - Revise manual page for correctness and completeness - Reinstate the `-y' (nroff) flag - Drop gmake(1) dependency, builds with BSD make(1) - Tweak port description and Makefile markup and syntax - Pet portlint(1)
    Mentioned in PR:	ports/119680 [1]
    Patches obtained from:	Debian

--- net/ssldump/files/patch-ssl_ssldecode.c
+++ net/ssldump/files/patch-ssl_ssldecode.c
@@ -1,10 +1,20 @@
---- sssl/ssldecode.c.orig	Sat Aug 17 05:33:17 2002
+--- ssl/ssldecode.c.orig	Sat Aug 17 05:33:17 2002
 +++ ssl/ssldecode.c	Fri Jan 20 13:09:49 2006
-@@ -50,6 +50,7 @@
- #ifdef OPENSSL
+@@ -51,6 +51,7 @@
  #include <openssl/ssl.h>
  #include <openssl/hmac.h>
-+#include <openssl/md5.h>
  #include <openssl/evp.h>
++#include <openssl/md5.h>
  #include <openssl/x509v3.h>
  #endif
+ #include "ssldecode.h"
+@@ -131,7 +132,8 @@
+     ssl_decode_ctx *d=0;
+     int r,_status;
+     
+-    SSLeay_add_all_algorithms();
++    SSL_library_init();
++    OpenSSL_add_all_algorithms();
+     if(!(d=(ssl_decode_ctx *)malloc(sizeof(ssl_decode_ctx))))
+       ABORT(R_NO_MEMORY);
+     if(!(d->ssl_ctx=SSL_CTX_new(SSLv23_server_method())))
