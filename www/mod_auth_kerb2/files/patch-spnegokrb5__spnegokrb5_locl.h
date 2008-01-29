22ba3c9aea4cf42e094267a0639b9bbc43debc74
Clement Laforet <clement@FreeBSD.org>
2008-01-29 20:24:46 +0000

    - This time, really add the patch - Fix build on >= 7.0
    Spotted by:		erwin

---
+++ www/mod_auth_kerb2/files/patch-spnegokrb5__spnegokrb5_locl.h
@@ -0,0 +1,10 @@
+--- spnegokrb5/spnegokrb5_locl.h.orig	2008-01-27 20:59:03.000000000 +0000
++++ spnegokrb5/spnegokrb5_locl.h	2008-01-27 20:59:19.000000000 +0000
+@@ -1,6 +1,7 @@
+ #include <stdlib.h>
+ #include <errno.h>
+ #include <string.h>
++#include <sys/types.h>
+ 
+ #include "config.h"
+ 
