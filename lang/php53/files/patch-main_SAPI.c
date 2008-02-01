8e15a70d4f51d6e53bd921bfe351d77bfe2084b7
Alex Dupre <ale@FreeBSD.org>
2008-02-01 22:53:34 +0000

    Fix a rare crash with apache because of uninitialized variable.
    PR:		ports/120204
    Submitted by:	danger

--- lang/php53/files/patch-main_SAPI.c
+++ lang/php53/files/patch-main_SAPI.c
@@ -1,6 +1,22 @@
---- main/SAPI.c.orig	Sun Jan  1 13:50:17 2006
-+++ main/SAPI.c	Wed May  3 09:38:09 2006
-@@ -602,7 +602,7 @@
+--- main/SAPI.c.orig	2007-05-25 11:20:01.000000000 +0200
++++ main/SAPI.c	2008-02-01 23:48:51.000000000 +0100
+@@ -301,6 +301,7 @@
+ 
+ 	/* SG(sapi_headers).http_response_code = 200; */ 
+ 	SG(sapi_headers).http_status_line = NULL;
++	SG(sapi_headers).mimetype = NULL;
+ 	SG(read_post_bytes) = 0;
+ 	SG(request_info).post_data = NULL;
+ 	SG(request_info).raw_post_data = NULL;
+@@ -340,6 +341,7 @@
+ 	SG(sapi_headers).http_response_code = 200;
+ 	*/
+ 	SG(sapi_headers).http_status_line = NULL;
++	SG(sapi_headers).mimetype = NULL;
+ 	SG(headers_sent) = 0;
+ 	SG(read_post_bytes) = 0;
+ 	SG(request_info).post_data = NULL;
+@@ -604,7 +606,7 @@
  					ptr++;
  					len--;
  				}
@@ -9,7 +25,7 @@
  				if(!strncmp(ptr, "image/", sizeof("image/")-1)) {
  					zend_alter_ini_entry("zlib.output_compression", sizeof("zlib.output_compression"), "0", sizeof("0") - 1, PHP_INI_USER, PHP_INI_STAGE_RUNTIME);
  				}
-@@ -756,7 +756,7 @@
+@@ -758,7 +760,7 @@
  		return SUCCESS;
  	}
  
