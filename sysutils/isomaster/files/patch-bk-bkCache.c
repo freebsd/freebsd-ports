89e410fb12f020760f535c22c0c391a7ca6b7285
Martin Wilke <miwi@FreeBSD.org>
2008-02-02 11:39:00 +0000

    - Update to 1.3
    PR:		119971
    Submitted by:	"Beat Gaetzi" <beat@chruetertee.ch>
    Approved by:	maintainer implicit

---
+++ sysutils/isomaster/files/patch-bk-bkCache.c
@@ -0,0 +1,11 @@
+--- bk/bkCache.c.orig	Thu Jan 24 22:54:28 2008
++++ bk/bkCache.c	Thu Jan 24 22:54:47 2008
+@@ -21,8 +21,6 @@
+ 
+ #include <string.h>
+ #include <stdio.h>
+-#include <sys/timeb.h>
+-#include <sys/types.h>
+ #include <sys/stat.h>
+ 
+ #include "bkInternal.h"
