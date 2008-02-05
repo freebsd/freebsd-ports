85a8143a415984724b5198a05286c755fa01a94b
Martin Wilke <miwi@FreeBSD.org>
2008-02-05 13:35:26 +0000

    - Use termios.h instead of sgtty.h
    PR:		120050
    Submitted by:	Ed Schouten <ed@fxq.nl>

--- net-mgmt/annextools/files/patch-erpcd::erpcd.c
+++ net-mgmt/annextools/files/patch-erpcd::erpcd.c
@@ -1,5 +1,14 @@
 --- erpcd/erpcd.c.orig	Sat Jan 31 17:38:06 1998
 +++ erpcd/erpcd.c	Thu Sep 18 20:10:32 2003
+@@ -36,7 +36,7 @@
+ #include <sys/param.h>
+ #include <sys/wait.h>
+ #include <netinet/in.h>
+-#include <sgtty.h>
++#include <sys/ioctl.h>
+ #include <netdb.h>
+ #else
+ #include <time.h>
 @@ -2108,11 +2108,7 @@
  {
      int savederrno = errno;
