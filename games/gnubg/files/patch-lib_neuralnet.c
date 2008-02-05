55f1a97cc227cbd016f63ae22a8bb30b8fc1f1da
miwi <miwi@FreeBSD.org>
2008-02-05 19:15:43 +0000

    - Update to 0.15
    PR:		119557
    Submitted by:	Pietro Cerutti <gahr@gahr.ch> (maintainer)

--- games/gnubg/files/patch-lib_neuralnet.c
+++ games/gnubg/files/patch-lib_neuralnet.c
@@ -1,46 +1,40 @@
---- lib/neuralnet.c.orig	2007-11-25 22:55:20.000000000 +0100
-+++ lib/neuralnet.c	2007-11-25 23:00:22.000000000 +0100
-@@ -384,15 +384,17 @@
+--- lib/neuralnet.c.orig	2008-01-14 23:59:11.000000000 +0100
++++ lib/neuralnet.c	2008-01-15 00:05:27.000000000 +0100
+@@ -30,6 +30,10 @@
+ #include <time.h>
+ #include <stdlib.h>
  
-     return 0;
- }
--extern void *NeuralNetCreateDirect( neuralnet *pnn, void *p ) {
-+extern void *NeuralNetCreateDirect( neuralnet *pnn, void *v_p ) {
-+   int *p = v_p;
-+   float *fp = v_p;
-  
-    pnn->cInput = *( ( (int *) p )++ );
-    pnn->cHidden = *( ( (int *) p )++ );
-    pnn->cOutput = *( ( (int *) p )++ );
-    pnn->nTrained = *( ( (int *) p )++ );
-    pnn->fDirect = TRUE;
--   pnn->rBetaHidden = *( ( (float *) p )++ );
--   pnn->rBetaOutput = *( ( (float *) p )++ );
-+   pnn->rBetaHidden = *( ( fp )++ );
-+   pnn->rBetaOutput = *( ( fp )++ );
-  
-    if( pnn->cInput < 1 || pnn->cHidden < 1 || pnn->cOutput < 1 ||
-      pnn->nTrained < 0 || pnn->rBetaHidden <= 0.0 ||
-@@ -402,14 +404,14 @@
-      return NULL;
-    }
-  
--   pnn->arHiddenWeight = p;
--   ( (float *) p ) += pnn->cInput * pnn->cHidden;
--   pnn->arOutputWeight = p;
--   ( (float *) p ) += pnn->cHidden * pnn->cOutput;
--   pnn->arHiddenThreshold = p;
--   ( (float *) p ) += pnn->cHidden;
--   pnn->arOutputThreshold = p;
--   ( (float *) p ) += pnn->cOutput;
-+   pnn->arHiddenWeight = fp;
-+   fp += pnn->cInput * pnn->cHidden;
-+   pnn->arOutputWeight = fp;
-+   fp += pnn->cHidden * pnn->cOutput;
-+   pnn->arHiddenThreshold = fp;
-+   fp += pnn->cHidden;
-+   pnn->arOutputThreshold = fp;
-+   fp += pnn->cOutput;
++#ifdef __FreeBSD__
++#include <sys/types.h>
++#endif
++
+ #include "neuralnet.h"
+ #include "sse.h"
  
-    pnn->savedBase = malloc( pnn->cHidden * sizeof( float ) ); 
-    pnn->savedIBase = malloc( pnn->cInput * sizeof( float ) ); 
+@@ -960,7 +964,17 @@
+     if ( 0 != error ) result = 0;
+     return result;
+ 
+-#else
++#endif
++
++#ifdef __FreeBSD__
++
++#include <sys/sysctl.h>
++    size_t length = sizeof( result );
++    int error = sysctlbyname("hw.instruction_sse", &result, &length, NULL, 0);
++    if ( 0 != error) result = 0;
++    return result;
++
++#endif
+ 
+ 	asm (
+ 		/* Check if cpuid is supported (can bit 21 of flags be changed) */
+@@ -1011,7 +1025,6 @@
+ "4:"
+ 
+ 			: "=b"(result) : : "%eax", "%ecx", "%edx");
+-#endif
+ 
+ 	switch (result)
+ 	{
