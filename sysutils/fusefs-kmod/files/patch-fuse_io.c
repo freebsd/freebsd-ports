e2e64877ff7e3b95b4ac12bd2e4620bda3fb0ae9
Roman Bogorodskiy <novel@FreeBSD.org>
2008-02-08 19:44:43 +0000

    - Fix build on 8-CURRENT. It was broken because of vn_lock/VOP_LOCK changes. - Pet portlint, while here.
    Approved by:		maintainer timeout (more than 2 weeks)
    Related PR:		119764

--- sysutils/fusefs-kmod/files/patch-fuse_io.c
+++ sysutils/fusefs-kmod/files/patch-fuse_io.c
@@ -1,6 +1,18 @@
---- fuse_module/fuse_io.c.orig	Tue Jun 19 09:35:22 2007
-+++ fuse_module/fuse_io.c	Thu Dec 13 02:46:05 2007
-@@ -148,7 +148,7 @@
+--- fuse_module/fuse_io.c.orig	2007-06-19 16:35:22.000000000 +0400
++++ fuse_module/fuse_io.c	2008-01-20 20:47:05.000000000 +0300
+@@ -139,7 +139,11 @@
+ 	struct vnode *vp = fp->f_vnode;
+ 	int err = 0;
+ 
++#if __FreeBSD_version > 800009
++	vn_lock(vp, LK_EXCLUSIVE | LK_RETRY);
++#else
+ 	vn_lock(vp, LK_EXCLUSIVE | LK_RETRY, td);
++#endif
+ 
+ 	ASSERT_VOP_LOCKED__FH(vp);
+ 	if (_file_is_bad(fp) || ! _file_is_fat(fp)) {
+@@ -148,7 +152,7 @@
  	}
  
  	if (uio->uio_resid == 0)
@@ -9,3 +21,15 @@
  
  	if (uio->uio_rw == UIO_WRITE && fp->f_flag & O_APPEND) {
  		if ((err = VOP_GETATTR(vp, &va, cred, td)))
+@@ -166,7 +170,11 @@
+ 	fp->f_nextoff = uio->uio_offset;
+ 
+ out:
++#if __FreeBSD_version > 800009
++	VOP_UNLOCK(vp, 0);
++#else
+ 	VOP_UNLOCK(vp, 0, td);
++#endif
+ 
+ 	DEBUG("leaving with %d\n", err);
+ 	return (err);
