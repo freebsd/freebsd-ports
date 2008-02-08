44cadac72f7d990408748d9942bf30d5c4fee547
garga <garga@FreeBSD.org>
2008-02-08 14:55:55 +0000

    - Update to 0.11.3 - Change WWW tto point to new project's site

--- net-im/jabber-pymsn/files/patch-src_main.py
+++ net-im/jabber-pymsn/files/patch-src_main.py
@@ -1,10 +1,10 @@
---- src/main.py.orig	Wed Oct 18 01:40:55 2006
-+++ src/main.py	Sun Jan 14 19:47:46 2007
-@@ -390,7 +390,10 @@
+--- src/main.py.orig	2008-02-08 11:55:07.000000000 -0200
++++ src/main.py	2008-02-08 12:43:07.000000000 -0200
+@@ -379,7 +379,10 @@
  	# Set SIGHUP to reload the config file & close & open debug file
  	signal.signal(signal.SIGHUP, SIGHUPstuff)
  	# Load some scripts for PID and daemonising
--	from twisted.scripts import twistd
+-	from twisted.scripts import _twistd_unix as twistd
 +	try:
 +		from twisted.scripts import _twistd_unix as twistd
 +	except:
