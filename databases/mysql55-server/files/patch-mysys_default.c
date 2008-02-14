02460675495bd1a07f7ad6b2a3b694d8495cca4e
Alex Dupre <ale@FreeBSD.org>
2008-02-14 10:49:46 +0000

    Update to 5.1.23 release.

--- databases/mysql55-server/files/patch-mysys_default.c
+++ databases/mysql55-server/files/patch-mysys_default.c
@@ -1,6 +1,6 @@
---- mysys/default.c.orig	Tue Jan 30 09:34:13 2007
-+++ mysys/default.c	Sat Feb  3 17:12:31 2007
-@@ -608,7 +608,7 @@
+--- mysys/default.c.orig	2008-01-29 22:41:43.000000000 +0100
++++ mysys/default.c	2008-02-14 11:03:21.000000000 +0100
+@@ -623,7 +623,7 @@
    {
      MY_STAT stat_info;
      if (!my_stat(name,&stat_info,MYF(0)))
@@ -9,15 +9,12 @@
      /*
        Ignore world-writable regular files.
        This is mainly done to protect us to not read a file created by
-@@ -1013,9 +1013,9 @@
- #else
-   *ptr++= "/etc/";
-   *ptr++= "/etc/mysql/";
-+  *ptr++= "%%PREFIX%%/etc/";
-+  *ptr++= "%%PREFIX%%/etc/mysql/";
- #endif
--  if ((env= getenv(STRINGIFY_ARG(DEFAULT_HOME_ENV))))
--    *ptr++= env;
-   *ptr++= "";			/* Place for defaults_extra_file */
- #if !defined(__WIN__) && !defined(__NETWARE__)
-   *ptr++= "~/";;
+@@ -1102,6 +1102,8 @@
+   bzero((char *) default_directories, sizeof(default_directories));
+   ADD_DIRECTORY("/etc/");
+   ADD_DIRECTORY("/etc/mysql/");
++  ADD_DIRECTORY("%%PREFIX%%/etc/");
++  ADD_DIRECTORY("%%PREFIX%%/etc/mysql/");
+ #ifdef DEFAULT_SYSCONFDIR
+   if (DEFAULT_SYSCONFDIR != "")
+     ADD_DIRECTORY(DEFAULT_SYSCONFDIR);
