77843f9a69d0cf2231cbf4bc3ec3ff59c1e5c33f
miwi <miwi@FreeBSD.org>
2008-02-16 09:55:39 +0000

    - Update to 0.42
    PR:		120345
    Submitted by:	Anderson S. Ferreira <anderson@cnpm.embrapa.br> (maintainer)

--- security/klamav/files/patch-src__klamd__scanner.h
+++
@@ -1,9 +0,0 @@
---- src/klamd/scanner.h.orig	2007-12-19 08:34:32.000000000 -0200
-+++ src/klamd/scanner.h	2007-12-19 08:34:52.000000000 -0200
-@@ -33,4 +33,6 @@
- 
- int checksymlink(const char *path);
- 
-+extern int cli_gentempfd(const char *dir, char **name, int *fd);
-+
- #endif
