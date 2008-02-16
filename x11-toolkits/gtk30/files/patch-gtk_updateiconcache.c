ab8be875bc863704e79308432ad6a74ea02bc372
Joe Marcus Clarke <marcus@FreeBSD.org>
2008-02-16 16:36:43 +0000

    Restore the behavior of unlinking invalid icon cache files to prevent leftovers.
    reported by:	pointyhat via pav

---
+++ x11-toolkits/gtk30/files/patch-gtk_updateiconcache.c
@@ -0,0 +1,11 @@
+--- gtk/updateiconcache.c.orig	2008-02-16 11:33:25.000000000 -0500
++++ gtk/updateiconcache.c	2008-02-16 11:33:33.000000000 -0500
+@@ -1477,7 +1477,7 @@ build_cache (const gchar *path)
+   if (!validate_file (tmp_cache_path))
+     {
+       g_printerr (_("The generated cache was invalid.\n"));
+-      //g_unlink (tmp_cache_path);
++      g_unlink (tmp_cache_path);
+       exit (1);
+     }
+ 
