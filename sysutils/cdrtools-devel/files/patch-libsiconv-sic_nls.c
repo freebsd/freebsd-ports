4ba25cb640e4e591c325be51ecd8ae76bf5fe099
Marius Strobl <marius@FreeBSD.org>
2008-02-18 21:48:57 +0000

    Fix `mkisofs -input-charset`: - The configure script adds dependencies on libiconv and libintl when   found so set USE_GETTEXT and USE_ICONV accordingly. If these libraries   are not yet installed, these dependencies can be turned off by defining   WITHOUT_NLS. There's no easy way to force these dependencies off as   the configure script doesn't offer such a knob and modifying its tests   is not allowed. - When setting USE_GETTEXT and USE_ICONV, also add OSDEFS to the MAKE_ENV   as a hack to pass -I${LOCALBASE}/include as CPPFLAGS so the installed   iconv.h is found. - Install the charset conversion tables. - Set ${INS_BASE} to ${LOCALBASE} in DEFAULTS/Defaults.freebsd so the   path constructed by libsiconv and mkisofs points to the installed   conversion tables. - Fix a bug that caused the 'iconv:' prefix not to be stripped from the   implied '-output-charset' when the conversion tables are not found or   '-input-charset' is used with the 'iconv:' prefix.
    Submitted by:	jkim
    Approved by:	netchild

---
+++ sysutils/cdrtools-devel/files/patch-libsiconv-sic_nls.c
@@ -0,0 +1,45 @@
+###########################################################################
+# This patch was contributed by Jung-uk Kim <jkim@FreeBSD.org>.
+###########################################################################
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# See the file CDDL.Schily.txt in this distribution for details.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file CDDL.Schily.txt from this distribution.
+###########################################################################
+# The file CDDL.Schily.txt can be found in the original cdrtools tarball,
+# which is mirrored at ftp://ftp.freebsd.org/pub/FreeBSD/distfiles/.
+###########################################################################
+
+--- libsiconv/sic_nls.c.orig	2007-11-27 16:49:03.000000000 -0500
++++ libsiconv/sic_nls.c	2008-02-09 03:13:23.000000000 -0500
+@@ -422,12 +422,15 @@
+ 	siconvt_t	*sp;
+ 	iconv_t		to;
+ 	iconv_t		from;
+-	char		*nm = sip->sic_name;
++	char		*nm;
+ 
+ 	if ((sp = (siconvt_t *)malloc(sizeof (siconvt_t)))
+ 							== NULL) {
+ 		return ((siconvt_t *)NULL);
+ 	}
++	nm = sip->sic_name;
++	if (strncmp("iconv:", sip->sic_name, 6) == 0)
++		nm = &sip->sic_name[6];
+ 	if ((from = iconv_open("UCS-2BE", nm)) == (iconv_t)-1) {
+ 		free(sp);
+ 		return ((siconvt_t *)NULL);
+@@ -437,7 +440,7 @@
+ 		iconv_close(from);
+ 		return ((siconvt_t *)NULL);
+ 	}
+-	sp->sic_name = nm;	/* Allow to compare name pointers */
++	sp->sic_name = sip->sic_name;	/* Allow to compare name pointers */
+ 	sip->sic_uni2cs = NULL;
+ 	sip->sic_cs2uni = NULL;
+ 	sip->sic_cd2uni = from;
