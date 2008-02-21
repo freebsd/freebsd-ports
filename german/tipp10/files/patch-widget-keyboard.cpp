56f43f58788198be1b4a42fff5e527f04119bc8c
miwi <miwi@FreeBSD.org>
2008-02-21 11:51:10 +0000

    TIPP10 is a free 10 finger touch typing trainer, published under the GPL.
    The highlight: TIPP10 works intelligent. The dictation reacts on
    individual errors of the user. Keys, which are wrongly typed often,
    will be dictated more often.
    
    Visual help, various keyboard layouts, numerous settings and the
    possibility to create own exercise dictates are easing the learning
    process.
    
    Main features:
    - Intelligent exercise lessons
    - Free and own lessons
    - Scrolling text and virtual keyboard for help
    - Extensive customization
    - Performance appraisal
    - Support of alternate keyboard layouts
    - Multi platform
    
    WWW:www.tipp10.de
    
    PR:		ports/119609
    Submitted by:	Heiner <h.eichmann at gmx.de>

---
+++ german/tipp10/files/patch-widget-keyboard.cpp
@@ -0,0 +1,12 @@
+--- widget/keyboard.cpp.orig	Tue Nov 27 22:06:05 2007
++++ widget/keyboard.cpp	Tue Nov 27 22:07:15 2007
+@@ -293,7 +293,8 @@
+ 		// Emit the help text of current key(s)
+ 		emit statusRefreshed(statusText);
+     	// Call the paint event (refresh the keyboard)
+-		repaint();
++		if (keyColoring || keyColoringStart)
++			repaint();
+ 	}
+ }
+ 
