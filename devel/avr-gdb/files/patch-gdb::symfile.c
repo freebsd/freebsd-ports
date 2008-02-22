7f61a871cc1ba595d4eb464d8ec5d6b399d402c9
joerg <joerg@FreeBSD.org>
2008-02-22 08:49:27 +0000

    Unescape an HTML escape within this patch.  It's been within the context part only so all it did was causing fuzz when patching, thus no PORTREVISION bump needed.

--- devel/avr-gdb/files/patch-gdb::symfile.c
+++ devel/avr-gdb/files/patch-gdb::symfile.c
@@ -7,7 +7,7 @@ diff -u -p -p -r1.143 symfile.c
 +++ symfile.c	19 Oct 2004 20:07:58 -0000
 @@ -1405,7 +1405,7 @@ load_section_callback (bfd *abfd, asecti
  	  struct cleanup *old_chain;
- 	  CORE_ADDR lma = bfd_section_lma (abfd, asec) + args-&gt;load_offset;
+ 	  CORE_ADDR lma = bfd_section_lma (abfd, asec) + args->load_offset;
  	  bfd_size_type block_size;
 -	  int err;
 +	  int err = 0;
