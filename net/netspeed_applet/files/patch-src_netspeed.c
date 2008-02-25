a86e950bb523aed67a5599835c23d7adec56b7a9
marcus <marcus@FreeBSD.org>
2008-02-25 19:30:30 +0000

    Update to 0.14.

--- net/netspeed_applet/files/patch-src_netspeed.c
+++ net/netspeed_applet/files/patch-src_netspeed.c
@@ -1,5 +1,5 @@
---- src/netspeed.c.orig	Mon May  1 19:36:46 2006
-+++ src/netspeed.c	Mon May  1 19:42:28 2006
+--- src/netspeed.c.orig	2006-09-28 05:54:52.000000000 -0400
++++ src/netspeed.c	2008-02-25 14:24:13.000000000 -0500
 @@ -23,6 +23,7 @@
  #include <panel-applet.h>
  #include <panel-applet-gconf.h>
@@ -8,45 +8,21 @@
  #include "backend.h"
  #include "netspeed.h"
  
-@@ -391,7 +392,7 @@
- 
- 	devices = get_available_devices();
- 	for (tmp = devices; tmp; tmp = g_list_next(tmp)) {
--		if (strcmp(tmp->data, "lo") == 0) continue;
-+		if (strcmp(tmp->data, "lo0") == 0) continue;
- 		if (strncmp(tmp->data, "dummy", strlen("dummy")) == 0) continue;
- 		info = get_device_info(tmp->data);
- 		if (info.running) {
-@@ -1344,6 +1345,16 @@
- 	if (strcmp (iid, "OAFIID:GNOME_NetspeedApplet"))
- 		return FALSE;
- 
-+    /*
-+     * Work around a design flaw in libgtop: force an initialisation
-+     * of the server, otherwise glibtop_get_netlist() will find that
-+     * the server is not needed and glibtop_get_netload() (which needs
-+     * the server) will fail.
-+     *
-+     * Jean-Yves Lefort <jylefort@FreeBSD.org> 20060501
-+     */
-+    glibtop_init();
-+
-     icon_theme = gtk_icon_theme_get_default();
-     gtk_icon_theme_append_search_path(icon_theme, DATADIR"/pixmaps/"PACKAGE);
- 	
-@@ -1432,13 +1443,13 @@
+@@ -1493,7 +1494,7 @@ netspeed_applet_factory(PanelApplet *app
  		GList *ptr, *devices = get_available_devices();
  		ptr = devices;
  		while (ptr) { 
--			if (!g_str_equal(ptr->data, "lo"))
-+			if (!g_str_equal(ptr->data, "lo0"))
- 				applet->devinfo = get_device_info(ptr->data);
- 			ptr = g_list_next(ptr);
- 		}
+-			if (!g_str_equal(ptr->data, "lo")) {
++			if (!g_str_equal(ptr->data, "lo0")) {
+ 				get_device_info(ptr->data, &applet->devinfo);
+ 				break;
+ 			}
+@@ -1502,7 +1503,7 @@ netspeed_applet_factory(PanelApplet *app
  		free_devices_list(devices);		
  	}
--	if (!applet->devinfo.name) applet->devinfo = get_device_info("lo");	
-+	if (!applet->devinfo.name) applet->devinfo = get_device_info("lo0");	
+ 	if (!applet->devinfo.name)
+-		get_device_info("lo", &applet->devinfo);	
++		get_device_info("lo0", &applet->devinfo);	
  	applet->device_has_changed = TRUE;	
  	
  	applet->tooltips = gtk_tooltips_new();
