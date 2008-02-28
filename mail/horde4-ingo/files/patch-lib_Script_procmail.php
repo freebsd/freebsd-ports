b561b83a37818befed3c7854bd868d6d8566021e
Beech Rintoul <beech@FreeBSD.org>
2008-02-28 20:30:40 +0000

    - Update to 1.2-rc2 - Many changes, please see the following:   http://www.horde.org/ingo/docs/?f=CHANGES.html

--- mail/horde4-ingo/files/patch-lib_Script_procmail.php
+++ mail/horde4-ingo/files/patch-lib_Script_procmail.php
@@ -1,22 +1,21 @@
---- lib/Script/procmail.php.orig	Tue Jan 31 20:00:25 2006
-+++ lib/Script/procmail.php	Thu Aug 17 14:52:45 2006
-@@ -427,9 +427,16 @@
+--- lib/Script/procmail.php.orig	2007-08-21 17:51:56.000000000 -0800
++++ lib/Script/procmail.php	2007-12-04 00:37:57.000000000 -0900
+@@ -434,9 +434,15 @@
              $this->_action[] = '{';
              foreach ($params['action-value']['addresses'] as $address) {
                  if (!empty($address)) {
--                    $this->_action[] = '  FILEDATE=`ls -lcn --time-style=+%s \'.vacation.' . $address
--                                       . '\' | awk \'{ print $6 + ('
--                                       . $days * 86400 . ') }\'`';
-+/***** This bit does not work with FreeBSD.
-+ *                   $this->_action[] = '  FILEDATE=`ls -lcn --time-style=+%s \'.vacation.' . $address
-+ *                                      . '\' | awk \'{ print $6 + ('
-+ *                                      . $days * 86400 . ') }\'`';
-+ *
-+ */
+-                    $this->_action[] = '  FILEDATE=`test -f \'.vacation.' . $address . '\' && '
+-                        . 'ls -lcn --time-style=+%s \'.vacation.' . $address . '\' | '
+-                        . 'awk \'{ print $6 + (' . $days * 86400 . ') }\'`';
++/***** This bit does not work with FreeBSD
++*                    $this->_action[] = '  FILEDATE=`test -f \'.vacation.' . $address . '\' && '
++*                        . 'ls -lcn --time-style=+%s \'.vacation.' . $address . '\' | '
++*                        . 'awk \'{ print $6 + (' . $days * 86400 . ') }\'`';
++*/
 +                    $this->_action[] = '  FDATE=`ls -lcnT \'.vacation.' . $address
 +                                       . '\' 2> /dev/null | awk \'{ print $6, $7, $8, $9; }\'`';
 +                    $this->_action[] = '  FILEDATE=`date -j -f "%b %d %H:%M:%S %Y" "$FDATE" +%s '
 +                                       . ' | awk \'{ print $1 + (' . $days * 86400 . ') }\'`';
                      $this->_action[] = '  DATE=`date +%s`';
-                     $this->_action[] = '  DUMMY=`test $FILEDATE -le $DATE'
-                                        .' && rm \'.vacation.' . $address .'\'`';
+                     $this->_action[] = '  DUMMY=`test -f \'.vacation.' . $address . '\' && '
+                         . 'test $FILEDATE -le $DATE && '
