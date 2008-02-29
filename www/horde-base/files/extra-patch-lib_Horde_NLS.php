8c9bc467342328494f8f4d389732eeccff452a87
beech <beech@FreeBSD.org>
2008-02-29 19:00:49 +0000

    - Fix NLS.php patch - Bump portrevision

--- www/horde-base/files/extra-patch-lib_Horde_NLS.php
+++ www/horde-base/files/extra-patch-lib_Horde_NLS.php
@@ -1,9 +1,10 @@
---- lib/Horde/NLS.php.orig	2007-09-29 07:22:46.000000000 -0800
-+++ lib/Horde/NLS.php	2007-12-18 11:16:03.000000000 -0900
-@@ -119,6 +119,10 @@
+--- lib/Horde/NLS.php.orig	2008-01-22 10:08:06.000000000 -0900
++++ lib/Horde/NLS.php	2008-02-29 09:50:02.000000000 -0900
+@@ -132,7 +132,10 @@
                  setlocale(LC_ALL, $lang_charset);
              }
          }
+-
 +	/* avoid FreeBSD issapce(3) bug */
 +	if(NLS::getCharset() == "UTF-8"){
 +		setlocale(LC_CTYPE,"C");
