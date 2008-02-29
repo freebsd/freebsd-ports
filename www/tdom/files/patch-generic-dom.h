880201478b5621e50cae5c7b1281b20d7aef74ff
mm <mm@FreeBSD.org>
2008-02-29 11:17:57 +0000

    - Update to 0.8.2
    Suggested by:	mi
    Patches by:	mi

---
+++ www/tdom/files/patch-generic-dom.h
@@ -0,0 +1,8 @@
+--- ../generic/dom.h	2007-08-08 11:52:38.000000000 -0400
++++ ../generic/dom.h	2008-02-14 14:27:46.000000000 -0500
+@@ -41,5 +41,4 @@
+ #include <expat.h>
+ #include <utf8conv.h>
+-#include <domalloc.h>
+ 
+ /*
