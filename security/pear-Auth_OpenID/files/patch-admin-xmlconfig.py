8f317d31a5cde42d059a0bf725f45a4e481d6616
mm <mm@FreeBSD.org>
2008-03-04 10:43:08 +0000

    This is version 2 of PHP OpenID using the PEAR framework.
    The PHP OpenID library lets you enable OpenID authentication on sites built
    using PHP. It features the OpenID consumer, Store implementations, and an
    OpenID server.
    
    WWW:    http://openidenabled.com/php-openid/

---
+++ security/pear-Auth_OpenID/files/patch-admin-xmlconfig.py
@@ -0,0 +1,11 @@
+--- admin/xmlconfig.py.orig	2008-02-20 23:13:48.000000000 +0100
++++ admin/xmlconfig.py	2008-03-03 16:39:09.376694098 +0100
+@@ -48,7 +48,7 @@
+ # The HTTP package base URI.  This is the place on the web where the
+ # PEAR-installable tarballs will live, and this (plus the package
+ # tarball name) will be the URL that users pass to "pear install".
+-package_base_uri = 'http://www.openidenabled.com/resources/downloads/php-openid/pear/'
++package_base_uri = 'http://openidenabled.com/files/php-openid/packages/'
+ 
+ # The release stability.  Maybe this should be a commandline parameter
+ # since it might differ from release to release.
