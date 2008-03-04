92a542447883fcf69e1b6dc87c4b7b34482d6316
trasz <trasz@FreeBSD.org>
2008-03-04 12:18:18 +0000

    - Update to 2.7.1.

--- audio/ripperx/files/patch-src-config_rw.c
+++ audio/ripperx/files/patch-src-config_rw.c
@@ -1,14 +1,14 @@
---- src/config_rw.c.orig	Wed Jan  3 20:37:27 2007
-+++ src/config_rw.c	Wed Jan  3 20:38:12 2007
-@@ -84,9 +84,9 @@
+--- src/config_rw.c.orig	2008-01-28 06:00:09.000000000 +0100
++++ src/config_rw.c	2008-02-21 19:01:31.000000000 +0100
+@@ -87,9 +87,9 @@
  	{ "Encoder::Plugin", config.encoder.plugin,
  		STRING, 0, "ripperX_plugin-lame" },
  	{ "CdPlayer::Play_command", config.cd_player.play_command,
--		STRING, 0, "cdplay play %" },
+-		STRING, 0, "cdplay %" },
 +		STRING, 0, "cdcontrol play %" },
  	{ "CdPlayer::Stop_command", config.cd_player.stop_command,
--		STRING, 0, "cdplay stop" },
+-		STRING, 0, "cdstop" },
 +		STRING, 0, "cdcontrol stop" },
  	{ "WavPlayer::Command", config.wav_player.command,
- 		STRING, 0, "splay %" },
+ 		STRING, 0, "play %" },
  	{ "Mp3Player::Command", config.mp3_player.command,
