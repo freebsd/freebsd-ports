a8e38ad645da7ae2709ec851c75e69c3408b8c43
alepulver <alepulver@FreeBSD.org>
2008-03-04 18:17:45 +0000

    - Enable use of mmap() instead of malloc() in the x86 VM. - Fix building the amd64 VM (MAP_ANONYMOUS -> MAP_ANON) [1].
    Reported by:	Dominic Fandrey <kamikaze@bsdforen.de> [1]

---
+++ games/iourbanterror/files/patch-code__qcommon__vm_x86_64.c
@@ -0,0 +1,11 @@
+--- ./code/qcommon/vm_x86_64.c.orig	2007-10-09 02:47:22.000000000 -0300
++++ ./code/qcommon/vm_x86_64.c	2008-03-04 10:45:08.000000000 -0300
+@@ -534,7 +534,7 @@
+ 	{
+ 		compiledOfs = assembler_get_code_size();
+ 		vm->codeLength = compiledOfs;
+-		vm->codeBase = mmap(NULL, compiledOfs, PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
++		vm->codeBase = mmap(NULL, compiledOfs, PROT_WRITE, MAP_SHARED|MAP_ANON, -1, 0);
+ 		if(vm->codeBase == (void*)-1)
+ 			Com_Error(ERR_DROP, "VM_CompileX86: can't mmap memory");
+ 
