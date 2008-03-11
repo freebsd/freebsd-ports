20cbd026a81e215e7f3cf469bf0bc19ca90181f9
alepulver <alepulver@FreeBSD.org>
2008-03-11 22:48:42 +0000

    Ratmen is a simple program for displaying a menu in a window. On selection is may either run a program or simply write something on standard out. It is inspired by, and is similar in function to, 9menu and ratmenu but is muchly improved. It includes: User settings in X resources, long and short option names (using getopts), scrollable menus (if they do not fit on screen), both mouse and keyboard support, exiting on unfocus, and a decent manpage.
    WWW:	http://www.update.uu.se/~zrajm/programs/

---
+++ x11-wm/ratmen/files/patch-ratmen.c
@@ -0,0 +1,10 @@
+--- ./ratmen.c.orig	2007-10-25 09:13:39.000000000 -0300
++++ ./ratmen.c	2008-02-20 17:08:21.000000000 -0300
+@@ -128,7 +128,6 @@
+ 
+ 
+ /* function prototypes */
+-int  strcasecmp(char*, char*);                 /* string comparison */
+ void ask_wm_for_delete(void);
+ void reap(int);
+ void redraw_snazzy(int, int, int);
