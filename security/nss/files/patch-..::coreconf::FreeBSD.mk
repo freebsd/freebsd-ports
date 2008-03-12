2b2b793e2f1f32ff1223df555ee1351a2e2e2201
Michael Johnson <ahze@FreeBSD.org>
2008-03-12 13:12:02 +0000

    Update to 3.11.9

--- security/nss/files/patch-..::coreconf::FreeBSD.mk
+++ security/nss/files/patch-..::coreconf::FreeBSD.mk
@@ -1,5 +1,5 @@
---- ../coreconf/FreeBSD.mk.orig	2007-10-31 14:02:28.000000000 -0500
-+++ ../coreconf/FreeBSD.mk	2007-10-31 14:05:56.000000000 -0500
+--- ../coreconf/FreeBSD.mk.orig	Sat Jan 21 03:36:11 2006
++++ ../coreconf/FreeBSD.mk	Tue Jan 30 21:13:41 2007
 @@ -37,9 +37,9 @@
  
  include $(CORE_DEPTH)/coreconf/UNIX.mk
@@ -13,22 +13,7 @@
  RANLIB			= ranlib
  
  ifeq ($(OS_TEST),alpha)
-@@ -49,8 +49,14 @@
- endif
- 
- OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
-+OS_LIBS			= $(BSD_LDOPTS)
-+OPTIMIZER		=
- 
-+ifeq ($(OS_TEST),sparc64)
- DSO_CFLAGS		= -fPIC
-+else
-+DSO_CFLAGS		= -fpic
-+endif
- DSO_LDOPTS		= -shared -Wl,-soname -Wl,$(notdir $@)
- 
- #
-@@ -60,20 +66,19 @@
+@@ -60,20 +66,18 @@
  USE_PTHREADS		= 1
  DEFINES			+= -D_THREAD_SAFE -D_REENTRANT
  OS_LIBS			+= -pthread
@@ -49,15 +34,14 @@
 -DLL_SUFFIX		= so.1.0
 +MKSHLIB			= $(CC) -Wl,-Bsymbolic $(DSO_LDOPTS)
  endif
- 
+-
 -MKSHLIB			= $(CC) $(DSO_LDOPTS)
  ifdef MAPFILE
  	MKSHLIB += -Wl,--version-script,$(MAPFILE)
  endif
-@@ -82,4 +87,5 @@
- 
+@@ -83,3 +87,5 @@
  G++INCLUDES		= -I/usr/include/g++
  
--INCLUDES		+= -I/usr/X11R6/include
+ INCLUDES		+= -I/usr/X11R6/include
 +USE_SYSTEM_ZLIB		= 1
 +ZLIB_LIBS		= -lz
