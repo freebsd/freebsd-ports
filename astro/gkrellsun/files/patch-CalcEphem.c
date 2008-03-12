6f66aaa795e337cae49a436c19467053acc11ff2
gahr <gahr@FreeBSD.org>
2008-03-12 12:21:01 +0000

    - Remove useless USE_X_PREFIX variable - Fix a "missing include" warning
    Approved by:	miwi (mentor)

--- astro/gkrellsun/files/patch-CalcEphem.c
+++ astro/gkrellsun/files/patch-CalcEphem.c
@@ -1,9 +1,14 @@
-
-$FreeBSD$
-
---- CalcEphem.c.orig	Thu Aug 15 15:34:37 2002
-+++ CalcEphem.c	Thu Aug 15 15:35:00 2002
-@@ -239,7 +239,7 @@
+--- CalcEphem.c.orig	2001-11-07 01:34:50.000000000 +0100
++++ CalcEphem.c	2008-03-12 13:14:01.000000000 +0100
+@@ -8,6 +8,7 @@
+ #include <config.h>
+ #endif
+ 
++#include <string.h>
+ #include <time.h>
+ #include <X11/X.h>
+ 
+@@ -239,7 +240,7 @@
      double UTRise, UTSet;
      double LTRise, LTSet;
      struct tm *LocalTime;
