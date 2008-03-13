d13d6b6d3e38ba707568b5d2b192b253cb23825f
Li-Wen Hsu <lwhsu@FreeBSD.org>
2008-03-13 09:31:15 +0000

    - Add a patch for cleanup of temp directories upon exit - Move PLIST into Makefile - Bump PORTREVISION
    PR:		ports/119783
    Submitted by:	scf (maintainer)

--- devel/cscope/files/patch-main.c
+++ devel/cscope/files/patch-main.c
@@ -1,6 +1,6 @@
---- src/main.c.orig	Sat Sep 30 01:13:00 2006
-+++ src/main.c	Thu Oct  5 15:21:30 2006
-@@ -359,7 +359,7 @@ cscope: TMPDIR to a valid directory\n");
+--- src/main.c.orig	2006-09-30 03:13:00.000000000 -0500
++++ src/main.c	2008-01-18 15:01:59.000000000 -0600
+@@ -359,7 +359,7 @@
      /* create the temporary file names */
      orig_umask = umask(S_IRWXG|S_IRWXO);
      pid = getpid();
@@ -9,7 +9,7 @@
      if(mkdir(tempdirpv,S_IRWXU)) {
  	fprintf(stderr, "\
  cscope: Could not create private temp dir %s\n",
-@@ -368,8 +368,13 @@ cscope: Could not create private temp di
+@@ -368,8 +368,13 @@
      }
      umask(orig_umask);
  
@@ -25,3 +25,11 @@
  
      /* if running in the foreground */
      if (signal(SIGINT, SIG_IGN) != SIG_IGN) {
+@@ -379,6 +384,7 @@
+     }
+     /* cleanup on the hangup signal */
+     signal(SIGHUP, myexit);
++    signal(SIGTERM, myexit);
+ 
+     /* if the database path is relative and it can't be created */
+     if (reffile[0] != '/' && access(".", WRITE) != 0) {
