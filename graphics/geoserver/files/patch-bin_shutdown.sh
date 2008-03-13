2928b67a8530af93f78436d8cfc05f34869ce1c2
gahr <gahr@FreeBSD.org>
2008-03-13 22:43:04 +0000

    GeoServer is a GIS server. It is open source and it allows users to view and edit geographic data.  GeoServer is a fully functional WFS-T and WMS server that follows the OGC specifications. GeoServer is a tool you can display maps on web pages, where the user can zoom and pan around. And it is used in conjunction with clients such as OpenLayers, Google Earth, UDig, GVSig and others.
    WWW: http://docs.codehaus.org/display/GEOS/Home
    
    PR:		121586
    Submitted by:	Wen heping <wenheping@gmail.com>
    Approved by:	miwi (mentor)

---
+++ graphics/geoserver/files/patch-bin_shutdown.sh
@@ -0,0 +1,29 @@
+--- bin/shutdown.sh.orig	2008-03-11 17:53:00.000000000 +0100
++++ bin/shutdown.sh	2008-03-11 17:53:22.000000000 +0100
+@@ -5,20 +5,6 @@
+ # $Id: shutdown.sh 7290 2007-07-23 18:57:27Z jdeolive $
+ # -----------------------------------------------------------------------------
+ 
+-# Make sure prerequisite environment variables are set
+-if [ -z "$JAVA_HOME" ]; then
+-  echo "The JAVA_HOME environment variable is not defined"
+-  echo "This environment variable is needed to run this program"
+-  exit 1
+-fi
+-if [ ! -r "$JAVA_HOME"/bin/java ]; then
+-  echo "The JAVA_HOME environment variable is not defined correctly"
+-  echo "This environment variable is needed to run this program"
+-  exit 1
+-fi
+-# Set standard commands for invoking Java.
+-_RUNJAVA="$JAVA_HOME"/bin/java
+-
+ if [ -z $GEOSERVER_HOME ]; then
+   #If GEOSERVER_HOME not set then guess a few locations before giving
+   # up and demanding user set it.
+@@ -52,4 +38,4 @@
+ fi
+ 
+ cd "$GEOSERVER_HOME"
+-exec "$_RUNJAVA" -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar --stop
++exec java -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar --stop
---
+++ graphics/geoserver/files/patch-bin_startup.sh
@@ -0,0 +1,29 @@
+--- bin/startup.sh.orig	2008-03-11 17:52:11.000000000 +0100
++++ bin/startup.sh	2008-03-11 17:52:51.000000000 +0100
+@@ -5,20 +5,6 @@
+ # $Id: startup.sh 7290 2007-07-23 18:57:27Z jdeolive $
+ # -----------------------------------------------------------------------------
+ 
+-# Make sure prerequisite environment variables are set
+-if [ -z "$JAVA_HOME" ]; then
+-  echo "The JAVA_HOME environment variable is not defined"
+-  echo "This environment variable is needed to run this program"
+-  exit 1
+-fi
+-if [ ! -r "$JAVA_HOME"/bin/java ]; then
+-  echo "The JAVA_HOME environment variable is not defined correctly"
+-  echo "This environment variable is needed to run this program"
+-  exit 1
+-fi
+-# Set standard commands for invoking Java.
+-_RUNJAVA="$JAVA_HOME"/bin/java
+-
+ if [ -z $GEOSERVER_HOME ]; then
+   #If GEOSERVER_HOME not set then guess a few locations before giving
+   # up and demanding user set it.
+@@ -66,4 +52,4 @@
+ echo "GEOSERVER DATA DIR is $GEOSERVER_DATA_DIR"
+ #added headless to true by default, if this messes anyone up let the list
+ #know and we can change it back, but it seems like it won't hurt -ch
+-exec "$_RUNJAVA" -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar 
++exec java -DGEOSERVER_DATA_DIR=$GEOSERVER_DATA_DIR -Djava.awt.headless=true -DSTOP.PORT=8079 -DSTOP.KEY=geoserver -jar start.jar
