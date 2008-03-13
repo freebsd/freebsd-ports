e92ded7b66e157fc5f2d4e8a14845c1fe82dfbd9
miwi <miwi@FreeBSD.org>
2008-03-13 20:56:55 +0000

    - Update to 0.9.0 - USE SF macro
    PR:		121617
    Submitted by:	Ports Fury

--- finance/libofx/files/patch-lib_ofx_preproc.cpp
+++ finance/libofx/files/patch-lib_ofx_preproc.cpp
@@ -1,9 +1,9 @@
 
 $FreeBSD$
 
---- lib/ofx_preproc.cpp.orig	Sat Nov 23 23:09:24 2002
-+++ lib/ofx_preproc.cpp	Sat Apr 12 22:10:56 2003
-@@ -22,6 +22,7 @@
+--- lib/ofx_preproc.cpp.orig	Mon Nov 12 15:07:48 2007
++++ lib/ofx_preproc.cpp	Thu Jan 17 02:39:31 2008
+@@ -23,6 +23,7 @@
  #include <stdlib.h>
  #include <stdio.h>
  #include <string>
@@ -11,3 +11,12 @@ $FreeBSD$
  #include "ParserEventGeneratorKit.h"
  #include "libofx.h"
  #include "messages.hh"
+@@ -185,7 +186,7 @@
+ 	    memset(iconv_buffer,0,READ_BUFFER_SIZE);
+ 	    size_t inbytesleft = strlen(s_buffer.c_str());
+ 	    size_t outbytesleft = READ_BUFFER_SIZE;
+-#ifdef OS_WIN32
++#if defined(OS_WIN32) || defined(OS_FREEBSD)
+ 	    const char * inchar = (const char *)s_buffer.c_str();
+ #else
+ 	    char * inchar = (char *)s_buffer.c_str();
