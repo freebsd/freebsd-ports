ae8997725cb244f4aacbdc01c58c5c4040321e80
Martin Wilke <miwi@FreeBSD.org>
2008-03-14 19:07:40 +0000

    - Update master sites - Remove outdated files - Quiet some warnings
    PR:		121445
    Submitted by:	Jonathan Hanna <jhanna@shaw.ca> (maintainer)

---
+++ japanese/edict/files/patch-exjdxgen.c
@@ -0,0 +1,11 @@
+--- exjdxgen.c.orig	1998-05-24 08:43:37.000000000 -0700
++++ exjdxgen.c	2008-03-06 11:39:23.000000000 -0800
+@@ -22,7 +22,7 @@
+ #include <sys/stat.h>
+ 
+ #include <stdio.h>
+-/*#include <stdlib.h>*/
++#include <stdlib.h>
+ #include <ctype.h>
+ #include <string.h>
+ #include "xjdic.h"
---
+++ japanese/edict/files/patch-xjdxgen.c
@@ -0,0 +1,11 @@
+--- xjdxgen.c.orig	1998-09-10 20:04:21.000000000 -0700
++++ xjdxgen.c	2008-03-06 11:40:41.000000000 -0800
+@@ -23,7 +23,7 @@
+ #include <sys/stat.h>
+ 
+ #include <stdio.h>
+-/*#include <stdlib.h>*/
++#include <stdlib.h>
+ #include <ctype.h>
+ #include <string.h>
+ #include "xjdic.h"
