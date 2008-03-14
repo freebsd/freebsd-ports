dfe528607254f374e4c04d3f2567794389196005
Pietro Cerutti <gahr@FreeBSD.org>
2008-03-14 14:57:14 +0000

    - Update to 1.18 - Adopt maintainship
    Approved by:	miwi (mentor)

--- devel/dparser/files/patch-parse.c
+++ devel/dparser/files/patch-parse.c
@@ -17,20 +17,6 @@
  		     isy.start, stack_depth(&isy));
    stack_free(&psx); stack_free(&psy); stack_free(&isx); stack_free(&isy);
    return r;
-@@ -949,12 +950,12 @@
- static int
- cmp_greedyness(PNode *x, PNode *y) {
-   VecPNode pvx, pvy;
-+  int ix = 0, iy = 0;
-   vec_clear(&pvx); vec_clear(&pvy); 
-   get_unshared_pnodes(x, y, &pvx, &pvy);
-   set_to_vec(&pvx); set_to_vec(&pvy);
-   qsort(pvx.v, pvx.n, sizeof(PNode *), greedycmp);
-   qsort(pvy.v, pvy.n, sizeof(PNode *), greedycmp);
--  int ix = 0, iy = 0;
-   while (1) {
-     if (pvx.n <= ix || pvy.n <= iy)
-       return 0;
 @@ -1202,8 +1203,8 @@
  static void
  set_add_znode_hash(VecZNode *v, ZNode *z) {
