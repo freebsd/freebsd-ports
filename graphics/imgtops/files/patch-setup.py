708860990dc89497d6c3d2540b455651eff7f6b1
Rong-En Fan <rafan@FreeBSD.org>
2008-03-17 06:38:41 +0000

    Command-line utilities for converting bitmage images (JPEG, PNG, GIF, Targa, BMP, etc.) to and from encapsulated postscript. Special care is taken to minimize the size of output files without reducing image quality.
    WWW: http://imgtops.sourceforge.net
    
    PR:		ports/121775
    Submitted by:	Timothy Bourke <timbob at bigpond.com>

---
+++ graphics/imgtops/files/patch-setup.py
@@ -0,0 +1,39 @@
+--- setup.py.orig	2003-09-05 12:25:18.000000000 +0800
++++ setup.py	2008-03-17 12:42:43.000000000 +0800
+@@ -1,6 +1,6 @@
+ #!/usr/bin/env python
+ 
+-import sys
++import sys, os
+ 
+ if sys.hexversion < 0x02020000:
+     print
+@@ -46,7 +46,7 @@
+     print
+     print '   see http://www.pythonware.com/products/pil/'
+     print
+-    if not get_yesno( 'Do you want to continue installing? [Y/n] ', 1 ):
++    if True:
+         print
+         print 'Installation cancelled.'
+         print
+@@ -59,7 +59,7 @@
+ print 'To use "epstoimg" you will need the Ghostscript interpreter'
+ print 'installed.  (see http://www.ghostscript.com/)'
+ print
+-if get_yesno( 'Do you want to install epstoimg? [Y/n] ', 1 ):
++if True:
+     if sys.platform == 'win32':
+         default = '+gswin32c.exe'
+     else:
+@@ -68,7 +68,9 @@
+     print
+     print 'Enter the full pathname of the Ghostscript interpreter,'
+     print 'or "+foo" to search the PATH for "foo" at runtime:'
+-    gs = raw_input( '[%s] ' % (default,) )
++    # gs = raw_input( '[%s] ' % (default,) )
++    gs = os.environ['PREFIX'] + '/bin/gs'
++    print "ghostscript: " + gs
+     if gs == '':
+         gs = default
+ 
