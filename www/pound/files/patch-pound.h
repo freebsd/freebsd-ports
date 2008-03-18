e9bf795d35077cbbad726fd1b87326321cc00e58
anders <anders@FreeBSD.org>
2008-03-18 21:12:38 +0000

    Update to 2.4.

--- www/pound/files/patch-pound.h
+++ www/pound/files/patch-pound.h
@@ -1,12 +1,9 @@
-
-$FreeBSD$
-
---- pound.h.orig
-+++ pound.h
-@@ -178,7 +178,8 @@
- #if HAVE_OPENSSL_SSL_H
- #define OPENSSL_THREAD_DEFINES
+--- pound.h.orig	2008-02-11 12:53:52.000000000 +0100
++++ pound.h	2008-03-18 21:58:27.000000000 +0100
+@@ -132,7 +132,8 @@
  #include    <openssl/ssl.h>
+ #include    <openssl/lhash.h>
+ #include    <openssl/err.h>
 -#if OPENSSL_VERSION_NUMBER >= 0x00907000L
 +#include    <openssl/opensslconf.h>
 +#if OPENSSL_VERSION_NUMBER > 0x0090705FL
