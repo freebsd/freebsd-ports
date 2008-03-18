452a63391ebadfd62085c942f21f00b4e141ed9a
jadawin <jadawin@FreeBSD.org>
2008-03-18 13:13:06 +0000

    sgwi is a web interface to SQLGrey written in PHP. It allows you to edit the white- and blacklists as well as the current state of the greylist.
    WWW:	http://www.vanheusden.com/sgwi/
    
    PR:		ports/121630
    Submitted by:	Lukasz Wasikowski <lukasz at wasikowski.net>
    Approved by:	tabthorpe (mentor)

---
+++ mail/sgwi/files/patch-copyright.php
@@ -0,0 +1,8 @@
+--- copyright.inc.php.orig	2008-03-11 22:23:21.000000000 +0100
++++ copyright.inc.php	2008-03-11 22:23:33.000000000 +0100
+@@ -1,4 +1,4 @@
+ <TR HEIGHT=64><TD>
+ <HR>
+-<FONT SIZE=-1><A HREF="http://www.vanheusden.com/sgwi/" TARGET="_new">SQLGrey webinterface v0.6</A> by <A HREF="http://www.vanheusden.com/feedbackform.php?subject=SQLGrey%20webinterface" TARGET="_new">folkert@vanheusden.com</A>
++<FONT SIZE=-1><A HREF="http://www.vanheusden.com/sgwi/" TARGET="_new">SQLGrey webinterface v0.7</A> by <A HREF="http://www.vanheusden.com/feedbackform.php?subject=SQLGrey%20webinterface" TARGET="_new">folkert@vanheusden.com</A>
+ </TD></TR>
