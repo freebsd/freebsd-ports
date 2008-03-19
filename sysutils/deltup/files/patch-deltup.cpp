491d76d567a68d953ad44b0c60fd3052fa659242
Rong-En Fan <rafan@FreeBSD.org>
2008-03-19 13:27:48 +0000

    - Update to 0.4.4
    PR:		ports/121517
    Submitted by:	Yarodin <yarodin at gmail.com>
    Approved by:	novel (maintainer, implicit)

--- sysutils/deltup/files/patch-deltup.cpp
+++ sysutils/deltup/files/patch-deltup.cpp
@@ -1,19 +1,19 @@
---- deltup.cpp.orig	Fri May 27 18:53:49 2005
-+++ deltup.cpp	Fri May 27 18:55:24 2005
-@@ -748,14 +748,14 @@
- void applyPatchfile(char *fname) {
+--- deltup.cpp.orig	2007-07-13 09:22:20.000000000 +0600
++++ deltup.cpp		2008-03-08 23:18:08.000000000 +0500
+@@ -520,14 +520,14 @@
    IStream *f = new IFStream(fname);
    Injectable_IStream f2(*f);
--  if (((IFStream*)f)->bad()) {fprintf(stderr, "file is missing: %s\n", fname); return;}
-+  if (((IFStream*)f)->bad()) {fprintf(stderr, "file is missing: %s\n", fname); exit(1);}
-   int type = determine_filetype(f2);
+   if (((IFStream*)f)->bad()) {
+-    fprintf(stderr, "file is missing: %s\n", fname.c_str()); return;}
++    fprintf(stderr, "file is missing: %s\n", fname.c_str()); exit(1);}
+   unsigned type = determine_filetype(f2);
    delete f;
    switch (type) {
      case GZIP: f = new GZ_IFStream(fname); break;
      case BZIP2: f = new BZ_IFStream(fname); break;
      case DTU: f = new IFStream(fname); break;
--    case UNKNOWN_FMT: fprintf(stderr, "cannot read file %s\n", fname); return;
-+    case UNKNOWN_FMT: fprintf(stderr, "cannot read file %s\n", fname); exit(1);
+-    case UNKNOWN_FMT: fprintf(stderr, "cannot read file %s\n", fname.c_str()); return;
++    case UNKNOWN_FMT: fprintf(stderr, "cannot read file %s\n", fname.c_str()); exit(1);
      case TARBALL :
        f = new IFStream(fname);
        unsigned zero_count;
