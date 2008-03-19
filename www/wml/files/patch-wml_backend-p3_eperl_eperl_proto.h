1835548d4820ee3e9d6118e49a8ff56fd999a915
Pietro Cerutti <gahr@FreeBSD.org>
2008-03-19 00:28:37 +0000

    - Update to 2.1a2
    Approved by:	miwi (mentor)

---
+++ www/wml/files/patch-wml_backend-p3_eperl_eperl_proto.h
@@ -0,0 +1,11 @@
+--- wml_backend/p3_eperl/eperl_proto.h.orig	2008-03-19 00:22:05.000000000 +0100
++++ wml_backend/p3_eperl/eperl_proto.h	2008-03-19 00:22:54.000000000 +0100
+@@ -35,6 +35,8 @@
+ #ifndef EPERL_PROTO_H
+ #define EPERL_PROTO_H 1
+ 
++#include "eperl_getopt.h"
++
+ /*_BEGIN_PROTO_*/
+ 
+ /* eperl_main.c */
