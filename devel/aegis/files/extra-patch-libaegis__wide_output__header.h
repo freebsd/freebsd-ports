a9155381f580ab9a288d4076bfa05c338e8c798b
Alejandro Pulver <alepulver@FreeBSD.org>
2008-03-23 18:49:30 +0000

    - Forgot to add patch referenced in previous commit.

---
+++ devel/aegis/files/extra-patch-libaegis__wide_output__header.h
@@ -0,0 +1,10 @@
+--- ./libaegis/wide_output/header.h.orig	2008-03-09 20:08:17.000000000 -0200
++++ ./libaegis/wide_output/header.h	2008-03-22 15:46:10.000000000 -0300
+@@ -22,6 +22,7 @@
+ 
+ #include <common/wstring.h>
+ #include <libaegis/wide_output.h>
++#include <time.h>
+ 
+ /**
+   * The wide_output_header class is used to add page headers to the output.
