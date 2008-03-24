6a9bfea48193555f1b31cfd16320a24337ea9ec2
Stanislav Sedov <stas@FreeBSD.org>
2008-03-24 19:15:51 +0000

    - Get rid of damn gems requirements - Fix supplied example - Bump PORTREVISION.

---
+++ devel/ruby-sexp/files/patch-sexp.rb
@@ -0,0 +1,11 @@
+--- sexp.rb.orig	2008-03-24 22:09:19.424592832 +0300
++++ sexp.rb	2008-03-24 22:09:25.775019936 +0300
+@@ -1,7 +1,6 @@
+ #!/usr/bin/ruby
+ 
+-require 'rubygems'
+-require_gem 'multi'
++require 'multi'
+ require 'smulti'
+ 
+ module SExpressionParser
---
+++ devel/ruby-sexp/files/patch-sexp_example.rb
@@ -0,0 +1,10 @@
+--- sexp_example.rb.orig	2008-03-24 22:13:35.290466969 +0300
++++ sexp_example.rb	2008-03-24 22:13:38.648162757 +0300
+@@ -1,6 +1,6 @@
+ #!/usr/bin/ruby
+ 
+-require 'sexpr'
++require 'sexp'
+ require 'pp'
+ 
+ pp "abc".parse_sexp
