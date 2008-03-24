fe5b0bf363cda054747dc3c3f0685765dbbfff24
Michael Johnson <ahze@FreeBSD.org>
2008-03-24 15:46:51 +0000

    Update to 0.3.4

--- net/gtk-vnc/files/patch-src_gvnc.c
+++ net/gtk-vnc/files/patch-src_gvnc.c
@@ -1,5 +1,5 @@
---- src/gvnc.c.orig	2007-12-16 13:59:46.000000000 -0500
-+++ src/gvnc.c	2007-12-16 14:01:54.000000000 -0500
+--- src/gvnc.c.orig	2008-02-22 09:33:46.000000000 -0500
++++ src/gvnc.c	2008-03-24 11:38:08.000000000 -0400
 @@ -16,7 +16,7 @@
  
  #include <netdb.h>
@@ -9,23 +9,11 @@
  #include <unistd.h>
  #include <stdio.h>
  #include <errno.h>
-@@ -26,7 +26,19 @@
+@@ -26,7 +26,6 @@
  #include <string.h>
  #include <sys/types.h>
  #include <sys/stat.h>
-+#ifdef __FreeBSD__
-+#include <sys/endian.h>
-+
-+#define __BYTE_ORDER            _BYTE_ORDER
-+#define __LITTLE_ENDIAN         _LITTLE_ENDIAN
-+#define __BIG_ENDIAN            _BIG_ENDIAN
-+
-+#define bswap_16                bswap16
-+#define bswap_32                bswap32
-+#define bswap_64                bswap64
-+#else
- #include <endian.h>
-+#endif
+-#include <endian.h>
  
  #include "coroutine.h"
  #include "d3des.h"
