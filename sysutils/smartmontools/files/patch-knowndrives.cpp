0bb59eff20e87ae0b4a31c5be901ef1b2d88eb25
Pav Lucistnik <pav@FreeBSD.org>
2008-03-25 15:39:33 +0000

    - Update to 5.38 - Mark the CISS patch as broken, it wasn't update to 5.38

--- sysutils/smartmontools/files/patch-knowndrives.cpp
+++ sysutils/smartmontools/files/patch-knowndrives.cpp
@@ -1,14 +1,11 @@
-
-$FreeBSD$
-
---- knowndrives.cpp.orig
-+++ knowndrives.cpp
-@@ -1031,7 +1031,7 @@
+--- knowndrives.cpp.orig	2008-02-02 19:10:48.000000000 +0100
++++ knowndrives.cpp	2008-03-14 13:35:59.000000000 +0100
+@@ -1155,7 +1155,7 @@
      NULL, NULL, NULL, NULL
    },
-   { "Western Digital Caviar SE (Serial ATA) family",
--    "^WDC WD((4|8|12|16|20|25|32)00JD|(12|16|20|25|30|32)00JS|1600AAJS)-.*$",
-+    "^WDC WD((4|8|12|16|20|25|32)00[JP]D|(12|16|20|25|30|32)00JS|1600AAJS)-.*$",
+   { "Western Digital Caviar SE Serial ATA family",
+-    "^WDC WD((4|8|12|16|20|25|32|40)00(JD|KD))-.*$",
++    "^WDC WD((4|8|12|16|20|25|32|40)00(JD|KD|PD))-.*$",
      ".*",
      NULL, NULL, NULL, NULL
    },
--- sysutils/smartmontools/files/patch-os__freebsd.cpp
+++ sysutils/smartmontools/files/patch-os__freebsd.cpp
@@ -1,6 +1,14 @@
---- os_freebsd.cpp.orig	2006-09-17 05:17:53.000000000 +0200
-+++ os_freebsd.cpp	2007-10-21 18:09:02.000000000 +0200
-@@ -873,6 +873,7 @@
+--- os_freebsd.cpp.orig	2008-03-04 23:09:47.000000000 +0100
++++ os_freebsd.cpp	2008-03-14 13:52:37.000000000 +0100
+@@ -272,7 +272,6 @@
+ }
+ 
+ int highpoint_command_interface(__unused int fd, __unused smart_command_set command, __unused int select, __unused char *data) {
+-{
+   return -1;
+ }
+ 
+@@ -924,6 +923,7 @@
  static const char * fbsd_dev_prefix = "/dev/";
  static const char * fbsd_dev_ata_disk_prefix = "ad";
  static const char * fbsd_dev_scsi_disk_plus = "da";
@@ -8,7 +16,7 @@
  static const char * fbsd_dev_scsi_tape1 = "sa";
  static const char * fbsd_dev_scsi_tape2 = "nsa";
  static const char * fbsd_dev_scsi_tape3 = "esa";
-@@ -908,6 +909,11 @@
+@@ -960,6 +960,11 @@
      return CONTROLLER_ATA;
    }
    
@@ -20,12 +28,3 @@
    // form /dev/da* or da*
    if (!strncmp(fbsd_dev_scsi_disk_plus, dev_name,
                 strlen(fbsd_dev_scsi_disk_plus)))
-@@ -1008,7 +1014,7 @@
-   // to first list. Turn on NOCHECK for second call. This results in no
-   // error if no more matches found, however it does append the actual
-   // pattern to the list of paths....
--  if ((retglob=glob(pattern1, GLOB_ERR, NULL, &globbuf)) ||
-+  if ((retglob=glob(pattern1, GLOB_ERR|GLOB_NOCHECK, NULL, &globbuf)) ||
-       (retglob=glob(pattern2, GLOB_ERR|GLOB_APPEND|GLOB_NOCHECK,NULL,&globbuf))) {
-      int retval = -1;
-     // glob failed
