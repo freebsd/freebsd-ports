2c22e82a2b3d6f76e4be7b40d0aba4fa8428dcd1
marcus <marcus@FreeBSD.org>
2008-03-26 02:58:52 +0000

    Fix build on 6.X.
    Reported by:	pointyhat via pav

--- net-im/telepathy-salut/files/patch-lib_gibber_gibber-resolver.c
+++ net-im/telepathy-salut/files/patch-lib_gibber_gibber-resolver.c
@@ -1,5 +1,5 @@
---- lib/gibber/gibber-resolver.c.orig	2008-03-19 22:59:19.000000000 -0400
-+++ lib/gibber/gibber-resolver.c	2008-03-19 23:00:18.000000000 -0400
+--- lib/gibber/gibber-resolver.c.orig	2008-01-30 08:52:52.000000000 -0500
++++ lib/gibber/gibber-resolver.c	2008-03-25 22:58:24.000000000 -0400
 @@ -720,7 +720,9 @@ gibber_resolver_gai_error_to_g_error (in
  
    switch (error) {
@@ -10,7 +10,7 @@
      case EAI_SOCKTYPE:
      case EAI_FAMILY:
      case EAI_SERVICE:
-@@ -731,7 +733,9 @@ gibber_resolver_gai_error_to_g_error (in
+@@ -731,13 +733,17 @@ gibber_resolver_gai_error_to_g_error (in
        code = GIBBER_RESOLVER_ERROR_RESOLVE_TEMPORARY_FAILURE;
        break;
      case EAI_FAIL:
@@ -20,3 +20,11 @@
      case EAI_NONAME:
        code = GIBBER_RESOLVER_ERROR_RESOLVE_FAILURE;
        break;
+ 
+     case EAI_MEMORY:
++#ifdef EAI_OVERFLOW
+     case EAI_OVERFLOW:
++#endif
+       code = GIBBER_RESOLVER_ERROR_MEMORY;
+       break;
+ 
