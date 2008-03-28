1daa29bd81bc6f5973236a53852d99a109693c70
Joe Marcus Clarke <marcus@FreeBSD.org>
2008-03-28 22:34:59 +0000

    Fix an undefined symbol on g_assert when linking to this library.
    Reported by:	pointyhat via pav

---
+++ devel/goffice1/files/patch-goffice_utils_go-regression.c
@@ -0,0 +1,10 @@
+--- goffice/utils/go-regression.c.orig	2008-03-28 18:30:40.000000000 -0400
++++ goffice/utils/go-regression.c	2008-03-28 18:30:52.000000000 -0400
+@@ -14,6 +14,7 @@
+ 
+ #include <glib/gmem.h>
+ #include <glib/gmessages.h>
++#include <glib/gtestutils.h>
+ 
+ #include <math.h>
+ #include <string.h>
