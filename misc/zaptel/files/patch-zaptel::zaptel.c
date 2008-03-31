da2c34f4f81fe69527b289448e69480bd14bbb2f
Pav Lucistnik <pav@FreeBSD.org>
2008-03-31 13:46:47 +0000

    - Fix build on 8-CURRENT
    PR:		ports/117879, ports/118061
    Submitted by:	Philip M. Gollucci <pgollucci@p6m7g8.com>, erwin
    Approved by:	maintainer timeout (sobomax; 4 months)

--- misc/zaptel/files/patch-zaptel::zaptel.c
+++ misc/zaptel/files/patch-zaptel::zaptel.c
@@ -1,9 +1,21 @@
 
 $FreeBSD$
 
---- zaptel/zaptel.c.orig
-+++ zaptel/zaptel.c
-@@ -396,7 +396,7 @@
+--- zaptel/zaptel.c.orig	2007-01-26 09:51:48.000000000 +0100
++++ zaptel/zaptel.c	2008-03-31 15:44:58.000000000 +0200
+@@ -254,7 +254,11 @@
+         while (1) {
+                 if (kthread_must_exit) {
+                         wakeup(cookie);
++#if (__FreeBSD_version >= 800002)
++                        kthread_exit();
++#else
+                         kthread_exit(0);
++#endif
+                 }
+ 
+                 ZAP_LOCK(toselwakeup_lock);
+@@ -396,7 +400,7 @@
  
  int schluffen(void *q)
  {
@@ -12,7 +24,7 @@ $FreeBSD$
  	switch(rc)
  	{
  		case EINTR:
-@@ -675,7 +675,11 @@
+@@ -675,7 +679,11 @@
  	/* Free dev_info, if exist */
  	if(dev->si_drv2) free(dev->si_drv2, M_ZAP);
  	dev->si_drv2 = NULL;
@@ -24,3 +36,15 @@ $FreeBSD$
  	return res;
  }
  
+@@ -1254,7 +1262,11 @@
+ 		ZAP_LOCK_INIT(toselwakeup_lock, "toselwakeup_lock");
+ 
+ 	        kthread_must_exit = 0;
++#if (__FreeBSD_version >= 800002)
++		if (kproc_create(selwakeup_thread_handler, (void *)toselwakeup, &kthread, 0, 0, "zt_selwakeup_thread"))
++#else
+ 		if (kthread_create(selwakeup_thread_handler, (void *)toselwakeup, &kthread, 0, 0, "zt_selwakeup_thread"))
++#endif
+ 		{
+ 			printf("Failed to create kthread\n");
+ 		}
