7222d6c7a59ec938e01d69e39e2c82313ae9e1c9
Pav Lucistnik <pav@FreeBSD.org>
2008-03-31 14:26:08 +0000

    - Update to 4.8
    PR:		ports/120369
    Submitted by:	Beat Gaetzi <beat@chruetertee.ch>
    Approved by:	maintainer timeout (clement; 7 weeks)

--- www/linkchecker/files/nls-patch-setup.py
+++ www/linkchecker/files/nls-patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py	Wed Dec 27 12:28:29 2006
-+++ setup.py	Wed Dec 27 12:28:04 2006
-@@ -500,6 +500,8 @@
- 
+--- setup.py.orig	2008-02-07 21:27:59.000000000 +0100
++++ setup.py	2008-02-07 21:28:56.000000000 +0100
+@@ -501,6 +501,8 @@
  if os.name == 'posix':
      data_files.append(('man/man1', ['doc/en/linkchecker.1']))
+     data_files.append(('man/man5', ['doc/en/linkcheckerrc.5']))
 +    data_files.append(('man/de/man1', ['doc/de/linkchecker.1']))
-+    data_files.append(('man/fr/man1', ['doc/fr/linkchecker.1']))
++    data_files.append(('man/de/man5', ['doc/de/linkcheckerrc.5']))
      data_files.append(('share/examples/linkchecker',
                ['config/linkchecker-completion',
                 'doc/examples/check_blacklist.sh',
--- www/linkchecker/files/patch-setup.py
+++ www/linkchecker/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	Tue Nov 21 17:49:07 2006
-+++ setup.py	Wed Dec 27 12:28:29 2006
+--- setup.py.orig	2008-02-07 21:27:02.000000000 +0100
++++ setup.py	2008-02-07 21:27:42.000000000 +0100
 @@ -471,7 +471,6 @@
      # windows does not have unistd.h
      define_macros.append(('YY_NO_UNISTD_H', None))
@@ -17,15 +17,17 @@
               ['cgi-bin/lconline/leer.html.en',
                'cgi-bin/lconline/leer.html.de',
                'cgi-bin/lconline/index.html',
-@@ -500,10 +499,8 @@
+@@ -500,11 +499,9 @@
        ]
  
  if os.name == 'posix':
 -    data_files.append(('share/man/man1', ['doc/en/linkchecker.1']))
+-    data_files.append(('share/man/man5', ['doc/en/linkcheckerrc.5']))
 -    data_files.append(('share/man/de/man1', ['doc/de/linkchecker.1']))
--    data_files.append(('share/man/fr/man1', ['doc/fr/linkchecker.1']))
+-    data_files.append(('share/man/de/man5', ['doc/de/linkcheckerrc.5']))
 -    data_files.append(('share/linkchecker/examples',
 +    data_files.append(('man/man1', ['doc/en/linkchecker.1']))
++    data_files.append(('man/man5', ['doc/en/linkcheckerrc.5']))
 +    data_files.append(('share/examples/linkchecker',
                ['config/linkchecker-completion',
                 'doc/examples/check_blacklist.sh',
