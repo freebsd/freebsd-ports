5a7ec0c5a1dd1eeb361ff88ee213024745e28dbf
Max Khon <fjoe@FreeBSD.org>
2008-03-31 20:44:30 +0000

    New port: dmucs
    DMUCS is a system that allows a group of users to share a compilation farm.
    Each compilation request from each user will be sent to the fastest available
    machine, every time. The system has these fine qualities:
    * Supports multiple users compiling simultaneously, and scales well to handle
    the new loads.
    * Supports multiple operating systems in the compilation farm.
    * Uses all processors of a multi-processor compilation host.
    * Makes best use of compilation hosts with widely differing CPU speeds.
    * Guarantees that a compilation host will not be overloaded by compilations.
    * Takes into account the load on a host caused by non-compilation tasks.
    * Supports the dynamic addition and removal of hosts to the compilation farm.
    * Works with distcc, which need not be altered in any way.
    
    WWW:    http://dmucs.sourceforge.net/

---
+++ devel/dmucs/files/patch-COSMIC-error.c
@@ -0,0 +1,10 @@
+--- COSMIC/error.c.orig	2007-11-26 02:32:21.000000000 +0600
++++ COSMIC/error.c	2007-11-26 02:32:39.000000000 +0600
+@@ -1,6 +1,7 @@
+ /* error.c: this file contains an error function
+  */
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <ctype.h>
+ #include "sockets.h"
+ 
