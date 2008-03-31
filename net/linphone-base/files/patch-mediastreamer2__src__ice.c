681404d43da9c3b055ed0ba3d898d13acaa948c4
Martin Wilke <miwi@FreeBSD.org>
2008-03-31 20:33:54 +0000

    - Update to 2.1.1
    PR:		121089
    Submitted by:	Ports Fury

--- net/linphone-base/files/patch-mediastreamer2__src__ice.c
+++ net/linphone-base/files/patch-mediastreamer2__src__ice.c
@@ -1,10 +1,12 @@
---- mediastreamer2/src/ice.c.orig	Fri Jan  5 06:42:59 2007
-+++ mediastreamer2/src/ice.c	Sun Feb 25 03:37:44 2007
-@@ -18,6 +18,7 @@
+--- mediastreamer2/src/ice.c.orig	2007-10-18 22:05:10.000000000 +0200
++++ mediastreamer2/src/ice.c	2008-03-31 10:12:57.000000000 +0200
+@@ -18,9 +18,7 @@
  */
  
  #if !defined(WIN32) && !defined(_WIN32_WCE)
-+#include <sys/types.h>
+-#ifdef __APPLE__
+ #include <sys/types.h>
+-#endif
  #include <sys/socket.h>
  #include <netdb.h>
  #endif
