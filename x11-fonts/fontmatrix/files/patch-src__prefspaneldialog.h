c5a1e6885b69b11816143205a58416e66318fc8a
pav <pav@FreeBSD.org>
2008-04-06 22:30:35 +0000

    - Update to 0.4.1
    PR:		ports/122084
    Submitted by:	Max Brazhnikov <makc@issp.ac.ru> (maintainer)

---
+++ x11-fonts/fontmatrix/files/patch-src__prefspaneldialog.h
@@ -0,0 +1,11 @@
+--- ./src/prefspaneldialog.h.orig	2008-04-06 13:47:45.000000000 +0400
++++ ./src/prefspaneldialog.h	2008-04-07 01:12:12.000000000 +0400
+@@ -12,7 +12,7 @@
+ #ifndef PREFSPANELDIALOG_H
+ #define PREFSPANELDIALOG_H
+ 
+-#include <qdialog.h>
++#include <QDialog>
+ #include <ui_prefs_panel.h>
+ 
+ 
