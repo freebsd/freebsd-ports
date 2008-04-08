8b4d0cce83ce6362a907a74554a83cee13c2d546
Pietro Cerutti <gahr@FreeBSD.org>
2008-04-08 15:57:29 +0000

    - Fix build on sparc64 and amd64
    Approved by:	maintainer (timeout), miwi (mentor)

--- net/relayd/files/patch-ctl-relayctl.c
+++ net/relayd/files/patch-ctl-relayctl.c
@@ -1,5 +1,5 @@
---- relayctl/relayctl.c.orig	Thu Feb 22 14:59:13 2007
-+++ relayctl/relayctl.c	Fri Jun  8 13:00:53 2007
+--- relayctl/relayctl.c.orig	2007-12-20 21:15:43.000000000 +0100
++++ relayctl/relayctl.c	2008-03-20 17:15:14.000000000 +0100
 @@ -20,6 +20,7 @@
   */
  
@@ -8,7 +8,7 @@
  #include <sys/socket.h>
  #include <sys/queue.h>
  #include <sys/un.h>
-@@ -286,7 +287,7 @@
+@@ -297,7 +298,7 @@
  	imn = monitor_lookup(imsg->hdr.type);
  	printf("%s: imsg type %u len %u peerid %u pid %d\n", imn->name,
  	    imsg->hdr.type, imsg->hdr.len, imsg->hdr.peerid, imsg->hdr.pid);
@@ -17,3 +17,12 @@
  	if (imn->type == -1)
  		done = 1;
  	if (imn->func != NULL)
+@@ -508,7 +509,7 @@
+ 	printf("\t%8s\ttotal: %llu sessions\n"
+ 	    "\t%8s\tlast: %u/%us %u/h %u/d sessions\n"
+ 	    "\t%8s\taverage: %u/%us %u/h %u/d sessions\n",
+-	    "", crs.cnt,
++	    "", (long long unsigned)crs.cnt,
+ 	    "", crs.last, crs.interval,
+ 	    crs.last_hour, crs.last_day,
+ 	    "", crs.avg, crs.interval,
