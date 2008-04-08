1318c2cf76bf9fefd96c21c31eb0d54a032d6a07
Pietro Cerutti <gahr@FreeBSD.org>
2008-04-08 18:40:31 +0000

    - Update to 0.8.1 - Remove NLS since port doesn't install NLS files anymore - Put only installed file in PLIST_FILES
    Approved by:	miwi (mentor)

--- mail/filtermail/files/patch-src::preferences.cc
+++ mail/filtermail/files/patch-src::preferences.cc
@@ -1,6 +1,14 @@
---- src/preferences.cc.orig	Sat Nov 20 23:39:15 2004
-+++ src/preferences.cc	Mon Mar  7 22:58:20 2005
-@@ -35,7 +35,7 @@
+--- src/preferences.cc.orig	2007-01-22 23:36:04.000000000 +0100
++++ src/preferences.cc	2008-04-08 17:00:17.000000000 +0200
+@@ -24,6 +24,7 @@
+ #include <vector>
+ #include <stdexcept>
+ #include <cstdio>
++#include <wordexp.h>
+ #include "preferences.hh"
+ #include "filter.hh"
+ #include "filtermail.hh"
+@@ -34,7 +35,7 @@
  
  extern "C"
  {
@@ -9,43 +17,3 @@
  #include <sys/types.h>
  #include <regex.h>
  }
-@@ -233,15 +233,15 @@
- void Preferences :: set_headers_file (const char* name)
- {
-   // Expand the given file name.
--  wordexp_t result;
--  if (wordexp (name, &result, 0) == 0)
--    headers_file_name = result.we_wordv[0];
-+  glob_t result;
-+  if (glob (name, 0, NULL, &result) == 0)
-+    headers_file_name = result.gl_pathv[0];
-   else
-     {
-       ERROR_MSG(_("Invalid file name: ") + (string)name + ".");
-       exit (-1);
-     }
--  wordfree (&result);
-+  globfree (&result);
- }
- 
- string Preferences :: headers_file (void)
-@@ -259,15 +259,15 @@
-   // command line.
-   if (log_file_name.length () == 0)
-     {
--      wordexp_t result;
--      if (wordexp (name, &result, 0) == 0)
--	log_file_name = result.we_wordv[0];
-+      glob_t result;
-+      if (glob (name, 0, NULL, &result) == 0)
-+	log_file_name = result.gl_pathv[0];
-       else
- 	{
- 	  ERROR_MSG(_("Invalid file name: ") + (string)name + ".");
- 	  exit (-1);
- 	}
--      wordfree (&result);
-+      globfree (&result);
-     }
- }
- 
