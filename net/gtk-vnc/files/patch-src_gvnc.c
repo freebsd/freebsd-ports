f0f9061cd81c3e29ede1ad4847df4c6c4e2de440
Michael Johnson <ahze@FreeBSD.org>
2008-04-08 00:42:48 +0000

    Update to 0.3.5

--- net/gtk-vnc/files/patch-src_gvnc.c
+++ net/gtk-vnc/files/patch-src_gvnc.c
@@ -1,5 +1,5 @@
---- src/gvnc.c.orig	2008-02-22 09:33:46.000000000 -0500
-+++ src/gvnc.c	2008-03-24 11:38:08.000000000 -0400
+--- src/gvnc.c.orig	2008-04-06 18:35:22.000000000 -0400
++++ src/gvnc.c	2008-04-07 20:37:54.647022975 -0400
 @@ -16,7 +16,7 @@
  
  #include <netdb.h>
@@ -9,11 +9,3 @@
  #include <unistd.h>
  #include <stdio.h>
  #include <errno.h>
-@@ -26,7 +26,6 @@
- #include <string.h>
- #include <sys/types.h>
- #include <sys/stat.h>
--#include <endian.h>
- 
- #include "coroutine.h"
- #include "d3des.h"
