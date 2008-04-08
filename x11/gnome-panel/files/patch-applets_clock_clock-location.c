23e77943bf10299781fd417cda724a5b2648c12e
Joe Marcus Clarke <marcus@FreeBSD.org>
2008-04-08 04:19:46 +0000

    Update to 2.22.1.

--- x11/gnome-panel/files/patch-applets_clock_clock-location.c
+++ x11/gnome-panel/files/patch-applets_clock_clock-location.c
@@ -1,8 +1,8 @@
---- applets/clock/clock-location.c.orig	2008-03-22 21:14:45.000000000 -0400
-+++ applets/clock/clock-location.c	2008-03-23 02:11:34.000000000 -0400
-@@ -28,6 +28,8 @@
+--- applets/clock/clock-location.c.orig	2008-04-07 17:17:51.000000000 -0400
++++ applets/clock/clock-location.c	2008-04-07 22:54:31.000000000 -0400
+@@ -27,6 +27,8 @@
+ #include "clock-marshallers.h"
  #include "set-timezone.h"
- #include "gweather-xml.h"
  
 +#define daylight 1
 +
@@ -17,15 +17,7 @@
  
  #define PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), CLOCK_LOCATION_TYPE, ClockLocationPrivate))
  
-@@ -271,7 +274,6 @@ parse_etc_sysconfig_clock (void)
- 			}
- 		}
- 	}
--
- out:
- 	if (lines)
- 		g_strfreev (lines);
-@@ -298,7 +300,11 @@ zone_from_etc_sysconfig_clock (void)
+@@ -299,7 +302,11 @@ zone_from_etc_sysconfig_clock (void)
  
  		parse_etc_sysconfig_clock ();
  		
@@ -37,8 +29,8 @@
  
  		monitor = g_file_monitor_file (file, G_FILE_MONITOR_NONE,
  					       NULL, NULL);
-@@ -632,10 +638,17 @@ gboolean
- clock_location_is_current (ClockLocation *loc)
+@@ -642,12 +649,18 @@ gboolean
+ clock_location_is_current_timezone (ClockLocation *loc)
  {
          ClockLocationPrivate *priv = PRIVATE (loc);
 -	const char *zone;
@@ -47,13 +39,16 @@
  
 -	if ((zone = zone_from_etc_sysconfig_clock ()))
 +	zones = clock_zonetable_new (SYSTEM_ZONEINFODIR);
-+
 +	if ((zone = clock_location_guess_zone (zones))) {
 +		g_object_unref (zones);
  		return strcmp (zone, priv->timezone) == 0;
+-	else
+-		return clock_location_get_offset (loc) == 0;
 +	}
 +
 +	g_object_unref (zones);
- 
- 	return clock_location_get_offset (loc) == 0;
++
++	return clock_location_get_offset (loc) == 0;
  }
+ 
+ gboolean
