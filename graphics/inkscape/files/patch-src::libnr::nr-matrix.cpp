2966117f74d205e3c71094a66865a4b42b7ab438
mezz <mezz@FreeBSD.org>
2008-04-09 19:29:03 +0000

    -Update to 0.46. [1] -Remove USE_GCC=3.4+ and X11BASE.
    PR:		ports/122386 [1]
    Submitted by:	Robert Andersson <streambag@streambag.se> [1]

--- graphics/inkscape/files/patch-src::libnr::nr-matrix.cpp
+++ graphics/inkscape/files/patch-src::libnr::nr-matrix.cpp
@@ -1,10 +1,10 @@
---- src/libnr/nr-matrix.cpp.org Sun Feb 15 09:45:06 2004
-+++ src/libnr/nr-matrix.cpp     Sun Feb 15 09:44:32 2004
-@@ -9,6 +9,7 @@
+--- src/libnr/nr-matrix.cpp.org	2008-04-02 22:01:32.000000000 +0200
++++ src/libnr/nr-matrix.cpp	2008-04-02 22:01:54.000000000 +0200
+@@ -11,6 +11,7 @@
   * This code is in public domain
   */
  
 +#include <cstdio>
+ #include <cstdlib>
  #include "nr-matrix.h"
  
- NRMatrix *
