d7496304f1caaf4115437ed6ab4367fc9089b7a7
Pav Lucistnik <pav@FreeBSD.org>
2008-04-11 13:44:56 +0000

    - Update to 2.1pre33
    PR:		ports/121886
    Submitted by:	bf <bf2006a@yahoo.com>
    Approved by:	maintainer timeout (demon; 3 weeks)

--- www/links/files/patch-default.c
+++ www/links/files/patch-default.c
@@ -1,11 +1,11 @@
---- default.c.orig	Tue Feb 11 21:54:38 2003
-+++ default.c	Sat Mar  1 16:03:31 2003
-@@ -300,7 +300,7 @@
+--- default.c.orig	2008-03-06 22:43:25.117856165 -0500
++++ default.c	2008-03-06 22:44:12.412560939 -0500
+@@ -400,7 +400,7 @@
  	get_system_name();
  	links_home = get_home(&first_use);
  	if (!links_home) {
 -		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have $HOME variable set correctly and that you have write permission to your home directory.\n\007");
-+		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have set the $HOME variable correctly and that you have write permission to your home directory.\n");
++		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have $HOME variable set correctly and that you have write permission to your home directory.\n");
  		sleep(3);
  		return;
  	}
