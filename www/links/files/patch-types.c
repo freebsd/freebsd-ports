403bd2afd00993eec8b1f8db6936b506a3221302
pav <pav@FreeBSD.org>
2008-04-11 13:44:56 +0000

    - Update to 2.1pre33
    PR:		ports/121886
    Submitted by:	bf <bf2006a@yahoo.com>
    Approved by:	maintainer timeout (demon; 3 weeks)

--- www/links/files/patch-types.c
+++ www/links/files/patch-types.c
@@ -1,11 +1,11 @@
---- types.c.orig	Thu Jul  5 13:10:37 2007
-+++ types.c	Thu Jul  5 13:12:07 2007
-@@ -807,7 +807,8 @@
+--- types.c.orig	2008-03-19 18:18:18.614955994 -0400
++++ types.c	2008-03-19 18:21:18.345245179 -0400
+@@ -820,7 +820,8 @@
  			ext = NULL;
  		}
  	if (ext) while (ext[extl] && ext[extl] != '.' && !dir_sep(ext[extl]) && !end_of_dir(url, ext[extl])) extl++;
 -	if ((extl == 3 && !casecmp(ext, "htm", 3)) ||
-+	if (force_html ||
++	if (force_html || 
 +	    (extl == 3 && !casecmp(ext, "htm", 3)) ||
  	    (extl == 4 && !casecmp(ext, "html", 4))) return stracpy("text/html");
  	foreach(e, extensions) {
