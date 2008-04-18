d2b0ae9f3ae50d3698beeed4a72cb3cd4f537a4c
clsung <clsung@FreeBSD.org>
2008-04-18 07:55:42 +0000

    - I forgot to add patch - bump PORTREVISION

---
+++ net/coda6_server/files/patch-coda-src-venus-worker.cc
@@ -0,0 +1,24 @@
+--- coda-src/venus/worker.cc.orig	2007-12-18 01:19:57.000000000 +0800
++++ coda-src/venus/worker.cc	2008-04-18 14:07:24.000000000 +0800
+@@ -397,17 +397,17 @@
+ 	    md[0].iov_base = (char *)"fstype"; md[0].iov_len = sizeof("fstype");
+ 	    md[1].iov_base = (char *)"coda";   md[1].iov_len = sizeof("coda");
+ 	    md[2].iov_base = (char *)"fspath"; md[2].iov_len = sizeof("fspath");
+-	    md[3].iov_base = venusRoot;        md[3].iov_len = strlen(venusRoot) + 1;
++	    md[3].iov_base = (char *)venusRoot;        md[3].iov_len = strlen((char *)venusRoot) + 1;
+ 	    md[4].iov_base = (char *)"from";   md[4].iov_len = sizeof("from");
+-	    md[5].iov_base = kernDevice;       md[5].iov_len = strlen(kernDevice) + 1;
++	    md[5].iov_base = (char *)kernDevice;       md[5].iov_len = strlen((char *)kernDevice) + 1;
+ 	    error = nmount(md, 6, 0);
+ 	}
+ #endif
+ 			
+ 	if (error < 0)
+-	    error = mount("coda", venusRoot, 0, kernDevice);
++	    error = mount("coda", (char *)venusRoot, 0, (char *)kernDevice);
+ 	if (error < 0)
+-	    error = mount("cfs", venusRoot, 0, kernDevice);
++	    error = mount("cfs", (char *)venusRoot, 0, (char *)kernDevice);
+ #if defined(__FreeBSD__) && !defined(__FreeBSD_version)
+ #define MOUNT_CFS 19
+ 	if (error < 0)
