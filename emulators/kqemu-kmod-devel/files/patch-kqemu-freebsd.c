d4679191a5f74f87674e4ac93257034408e066df
Juergen Lock <nox@FreeBSD.org>
2008-04-19 15:04:56 +0000

    - Try and fix for real the issue of exiting qemu processes sometimes   stuck in devdrn - Remove the -DSMP magic, it didn't help the amd64 SMP panics and was   useless anyway (the panics still exist, the only good workaround I've   found so far is forcing qemu onto cpu 1 and that is only possible on   HEAD using the new cpuset feature) - Only bother with the -DKSE magic for OSVERSIONs where it matters - Bump PORTREVISION

--- emulators/kqemu-kmod-devel/files/patch-kqemu-freebsd.c
+++ emulators/kqemu-kmod-devel/files/patch-kqemu-freebsd.c
@@ -17,13 +17,33 @@ Index: kqemu-freebsd.c
      return SIGPENDING(curthread);
  }
  #endif
-@@ -320,6 +328,9 @@
+@@ -320,8 +328,15 @@
  #if __FreeBSD_version >= 500000
      dev->si_drv1 = NULL;
      TAILQ_REMOVE(&kqemuhead, ks, kqemu_ent);
++#if __FreeBSD_version >= 700051
++    destroy_dev_sched(dev);
++#else
 +#if __FreeBSD_version >= 700024
 +    dev_relthread(dev); 
 +#endif
      destroy_dev(dev);
  #endif
++#endif
      free(ks, M_KQEMU);
+     --kqemu_ref_count;
+ }
+@@ -500,7 +515,13 @@
+ 	while ((ks = TAILQ_FIRST(&kqemuhead)) != NULL) {
+ 	    kqemu_destroy(ks);
+ 	}
++#if __FreeBSD_version >= 700051
++	drain_dev_clone_events();
++#endif
+ 	clone_cleanup(&kqemuclones);
++#if __FreeBSD_version >= 700051
++	destroy_dev_drain(&kqemu_cdevsw);
++#endif
+ #endif
+         kqemu_global_delete(kqemu_gs);
+         kqemu_gs = NULL;
