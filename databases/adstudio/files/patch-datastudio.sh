1fd6dfcef4af03faad25b38faf5b44cecd77badb
Thierry Thomas <thierry@FreeBSD.org>
2008-04-20 15:20:35 +0000

    - Upgrade to v. 6.5.9.
    - Release maintainership.
    
    Important note: the free version (as in beer) has been removed from
    Aquafold's site; this one is only free for qualified OSS developers.
    
    Submitted by:	Mikhail Teterin <mi+kde (at) aldan.algebra.com>

--- databases/adstudio/files/patch-datastudio.sh
+++ databases/adstudio/files/patch-datastudio.sh
@@ -1,4 +1,4 @@
---- datastudio.sh.orig	Wed Aug  4 15:15:16 2004
+--- datastudio.sh	Wed Aug  4 15:15:16 2004
 +++ datastudio.sh	Fri Feb 11 16:08:59 2005
 @@ -1,7 +1,7 @@
  #!/bin/sh
@@ -9,9 +9,3 @@
  
  CLASSES=$ADS_HOME/lib/datastudio.jar
  CLASSES=$ADS_HOME/lib/oracle.jar:$CLASSES
-@@ -20,4 +20,4 @@
- CLASSES=$ADS_HOME/lib/mail.jar:$CLASSES
- CLASSES=$ADS_HOME/lib/activation.jar:$CLASSES
- 
--java -cp $CLASSES com.aquafold.datastudio.DataStudio
-+java -cp $CLASSES com.aquafold.datastudio.DataStudio &
