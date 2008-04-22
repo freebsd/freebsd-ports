8181c0311f1622a13255223a09674326c82f14d5
Xin LI <delphij@FreeBSD.org>
2008-04-22 05:32:58 +0000

    Mask FP_X_DNML not only on i386, but also on amd64.  This fixes mysterious crashes of thunderbird with SIGFPE, one of symptom of it is that this prevents enigmail-thunderbird from being able to be used on recent FreeBSD versions with thunderbird.
    PR:		ports/122975
    Approved by:	maintainer (marcus as gnome@)

--- mail/thunderbird3/files/patch-Double.cpp
+++ mail/thunderbird3/files/patch-Double.cpp
@@ -1,20 +1,20 @@
---- extensions/transformiix/source/base/Double.cpp.orig	Thu Jan 30 09:26:46 2003
-+++ extensions/transformiix/source/base/Double.cpp	Sun Nov 16 01:46:42 2003
-@@ -51,10 +51,10 @@
+--- extensions/transformiix/source/base/Double.cpp.orig	2006-06-22 12:13:00.000000000 -0700
++++ extensions/transformiix/source/base/Double.cpp	2008-04-21 14:04:37.540570448 -0700
+@@ -52,10 +52,10 @@
  //A trick to handle IEEE floating point exceptions on FreeBSD - E.D.
  #ifdef __FreeBSD__
  #include <ieeefp.h>
 -#ifdef __alpha__
 -fp_except_t allmask = FP_X_INV|FP_X_OFL|FP_X_UFL|FP_X_DZ|FP_X_IMP;
 -#else
-+#if defined(__i386__)
++#if defined(__i386__) || defined(__amd64__)
  fp_except_t allmask = FP_X_INV|FP_X_OFL|FP_X_UFL|FP_X_DZ|FP_X_IMP|FP_X_DNML;
 +#else
 +fp_except_t allmask = FP_X_INV|FP_X_OFL|FP_X_UFL|FP_X_DZ|FP_X_IMP;
  #endif
  fp_except_t oldmask = fpsetmask(~allmask);
  #endif
-@@ -75,22 +75,31 @@
+@@ -115,22 +115,31 @@
  #define TX_DOUBLE_HI32_EXPMASK   0x7ff00000
  #define TX_DOUBLE_HI32_MANTMASK  0x000fffff
  
