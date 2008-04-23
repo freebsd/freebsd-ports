882e9808677e3848be3ad8290fb959f756630b6e
Beech Rintoul <beech@FreeBSD.org>
2008-04-23 04:07:11 +0000

    - Update ircd-ratbox-devel port to latest beta testing release of ircd-ratbox-3.0.0, added sqlite3 as   dependancy, updated rc.d script to add 'conftest' option.
    Changes include:
    o Optional SSL support to enable encrypted connections between clients
    and servers, as well as server to server links.
    o Add support for SSL only channels, channel mode +S.
    o Bundled sqlite3 for handling and storing k/x/d lines.
    o Support for global CIDR limits.
    o Added adminwall allowing admins to broadcast messages to each other.
    o Creation of new library archive 'libratbox'.
    o Support for force nick changes.
    o New ssld and bandb processes for SSL connections and ban checking.
    
    PR:		ports/122994
    Submitted by:	Lewis <moggie@elasticmind.net> (maintainer)

--- irc/ircd-ratbox-devel/files/patch-include_config.h
+++ irc/ircd-ratbox-devel/files/patch-include_config.h
@@ -1,19 +1,22 @@
---- include/config.h.orig	Thu Jan 20 11:04:40 2005
-+++ include/config.h	Fri Jan 27 02:25:37 2006
-@@ -56,6 +56,7 @@
+--- ./include/config.h.orig	Wed Sep  5 20:08:21 2007
++++ ./include/config.h	Tue Apr 22 02:20:19 2008
+@@ -54,15 +54,17 @@
  #define LOGPATH LOG_DIR
  #define UHPATH   HELP_DIR "/users"
  #define HPATH  HELP_DIR "/opers"
-+#define RUNPATH "%%RUNDIR%%"
++#define RUNPATH	"/var/run/ircd-ratbox"
++#define DBPTH "/var/db/ircd-ratbox"
  
  /* files */
  #define SPATH    BINPATH "/ircd"	/* ircd executable */
-@@ -68,7 +69,7 @@
- #define RPATH    ETCPATH "/ircd.rsa"	/* ircd rsa private keyfile */
+ #define SLPATH   BINPATH "/servlink"	/* servlink executable */
+ #define CPATH    ETCPATH "/ircd.conf"	/* ircd.conf file */
+-#define DBPATH   ETCPATH "/ban.db"
++#define DBPATH   DBPTH "/ban.db"
  #define MPATH    ETCPATH "/ircd.motd"	/* MOTD file */
  #define LPATH    LOGPATH "/ircd.log"	/* ircd logfile */
 -#define PPATH    ETCPATH "/ircd.pid"	/* pid file */
 +#define PPATH    RUNPATH "/ircd.pid"	/* pid file */
  #define OPATH    ETCPATH "/opers.motd"	/* oper MOTD file */
  
- /* IGNORE_BOGUS_TS
+ /* TS6_ONLY
