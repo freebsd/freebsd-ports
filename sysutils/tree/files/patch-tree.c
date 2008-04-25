46398f6ab80f3ca69e9899a9a11e6707cff328b2
Thomas Abthorpe <tabthorpe@FreeBSD.org>
2008-04-25 19:46:13 +0000

    - Fix for CURRENT - Bump PORTREVISION - Pass maintainership to submiiter
    Submitted by:	Guy Brand <gb isis.u-strasbg.fr> (via private email)

--- sysutils/tree/files/patch-tree.c
+++ sysutils/tree/files/patch-tree.c
@@ -8,6 +8,15 @@
  #include <stdlib.h>
  #include <stdio.h>
  #include <time.h>
+@@ -148,7 +147,7 @@
+ int findino(ino_t, dev_t);
+ void *xmalloc(size_t), *xrealloc(void *, size_t);
+ void listdir(char *, int *, int *, u_long, dev_t), usage(int);
+-void parse_dir_colors(), printit(unsigned char *), free_dir(struct _info **), indent();
++void parse_dir_colors(), printit(char *), free_dir(struct _info **), indent();
+ void saveino(ino_t, dev_t);
+ char **split(char *, char *, int *);
+ char *gidtoname(int), *uidtoname(int), *do_date(time_t);
 @@ -178,12 +177,6 @@
  char *sLevel, *curdir, *outfilename = NULL;
  FILE *outfile;
@@ -21,3 +30,33 @@
  
  int main(int argc, char **argv)
  {
+@@ -622,7 +615,7 @@
+ #ifdef __USE_FILE_OFFSET64
+     if (inodeflag) sprintf(path," %7lld",(*dir)->inode);
+ #else
+-    if (inodeflag) sprintf(path," %7ld",(*dir)->inode);
++    if (inodeflag) sprintf(path," %7i",(*dir)->inode);
+ #endif
+     if (devflag) sprintf(path+strlen(path), " %3d", (int)(*dir)->dev);
+ #ifdef __EMX__
+@@ -1041,8 +1034,10 @@
+   } else {
+     if (Hflag) fprintf(outfile,"<br>\t\t\t\t   ");
+     for(i=0;dirs[i];i++) {
++      if( dirs[i] <= 0 )
++          break;
+       fprintf(outfile,"%s ",
+-	      dirs[i+1] ? (dirs[i]==1 ? linedraw->vert     : (Hflag? "&nbsp;&nbsp;&nbsp;" : "   ") )
++	      dirs[i+1] > 0 ? (dirs[i]==1 ? linedraw->vert     : (Hflag? "&nbsp;&nbsp;&nbsp;" : "   ") )
+ 			: (dirs[i]==1 ? linedraw->vert_left:linedraw->corner));
+     }
+   }
+@@ -1180,7 +1175,7 @@
+   return t->name;
+ }
+ 
+-void printit(unsigned char *s)
++void printit(char *s)
+ {
+   int c;
+ 
