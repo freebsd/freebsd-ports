0fb128ceb2b655226ae1d70f13f847eee2ce7c01
pav <pav@FreeBSD.org>
2008-04-25 17:22:39 +0000

    Free crossword game artificial intelligence and analysis tool that rivals the best players in the world!
    You will need to set up a game board with the bonus squares scrabble has.
    See http://web.mit.edu/jasonkb/www/quackle/example_report.html for a
    reminder of which squares are which, and recreate this board to have
    exactly the same rules as Scrabble (TM)
    
    WWW: http://web.mit.edu/jasonkb/www/quackle/
    
    PR:		ports/122810
    Submitted by:	Jonathan Shoemaker <shoemaker@bluebottle.com>

---
+++ games/quackle/files/patch-fixedstring.h
@@ -0,0 +1,10 @@
+--- fixedstring.h.orig	2008-04-15 20:02:38.000000000 -0600
++++ fixedstring.h	2008-04-15 20:02:55.000000000 -0600
+@@ -18,6 +18,7 @@
+  *  02110-1301  USA
+  */
+ 
++#include <cassert>
+ #ifndef QUACKLE_FIXEDSTRING_H
+ #define QUACKLE_FIXEDSTRING_H
+ 
