412d21545695829379637fa82fd4b09315b2e3ec
miwi <miwi@FreeBSD.org>
2008-04-26 14:48:34 +0000

    - Update to 1.5
    PR:		123086
    Submitted by:	Ports Fury

--- mail/smtptrapd/files/patch-smtptrapd.c
+++ mail/smtptrapd/files/patch-smtptrapd.c
@@ -1,17 +1,17 @@
---- smtptrapd.c.orig	Sun Mar 20 00:01:06 2005
-+++ smtptrapd.c	Sun Mar 20 00:01:36 2005
+--- smtptrapd.c.orig	2007-12-07 12:09:31.000000000 +0900
++++ smtptrapd.c	2008-02-01 00:11:37.000000000 +0900
 @@ -27,9 +27,9 @@
  #include <sys/time.h>
  #include <sys/types.h>
  #include <unistd.h>
-+#include <netinet/in.h>
- #include <arpa/inet.h>
+-#include <arpa/inet.h>
  #include <sys/socket.h>
--#include <netinet/in.h>
+ #include <netinet/in.h>
++#include <arpa/inet.h>
  #include <errno.h>
  #include <string.h>
  #include <sysexits.h>
-@@ -491,7 +491,11 @@
+@@ -544,7 +544,11 @@
  	/* background the process */
  	if (fork()) exit(0);
  	if (fork()) exit(0);
