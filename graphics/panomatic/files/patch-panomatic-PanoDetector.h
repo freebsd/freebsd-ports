cdd73bb9c473de2752feaf1af1c5216c321cd631
Martin Wilke <miwi@FreeBSD.org>
2008-04-26 20:24:03 +0000

    Pan-o-matic is a tool that automates the creation of control points in Hugin.
    WWW: http://aorlinsk2.free.fr/panomatic/
    
    PR:		ports/121859
    Submitted by:	Iouri V. Ivliev <ii@any.com.ru>

---
+++ graphics/panomatic/files/patch-panomatic-PanoDetector.h
@@ -0,0 +1,22 @@
+--- panomatic/PanoDetector.h.orig	2008-03-10 21:17:33.000000000 +0100
++++ panomatic/PanoDetector.h	2008-04-26 20:20:01.000000000 +0200
+@@ -84,8 +84,8 @@
+ 	inline bool getLinearMatch() const { return _linearMatch; }
+ 	inline int  getLinearMatchLen() const { return _linearMatchLen; }
+ 
+-	inline bool	getDownscale() const { return _downscale; }
+-    inline void setDownscale(bool iDown) { _downscale = iDown; }
++	inline float    getScale() const { return _scale; }
++    inline void setScale(float iScale) { _scale = iScale; }
+ 
+ 	
+ 	inline void addFile(const std::string& iFile) { _files.push_back(iFile); }
+@@ -126,7 +126,7 @@
+ 
+ 	bool					_test;
+ 	int						_cores;
+-    bool                    _downscale;
++    float                    _scale;
+ 	
+ 	// list of files
+ 	FileNameList_t			_files;
---
+++ graphics/panomatic/files/patch-panomatic-TestCode.h
@@ -0,0 +1,11 @@
+--- panomatic/TestCode.h.orig	2008-03-05 22:12:18.000000000 +0100
++++ panomatic/TestCode.h	2008-04-26 20:21:56.000000000 +0200
+@@ -29,7 +29,7 @@
+ {
+ public:
+ 	static void drawRansacMatches(std::string& i1, std::string& i2, 
+-						PointMatchVector_t& iOK, PointMatchVector_t& iNOK, Ransac& iRansac, bool iHalf);
++						PointMatchVector_t& iOK, PointMatchVector_t& iNOK, Ransac& iRansac, float iScale);
+ 
+ 
+ 
