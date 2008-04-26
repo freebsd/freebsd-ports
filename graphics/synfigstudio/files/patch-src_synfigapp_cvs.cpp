c7e31569b9d6f0680df0bf3980dffeb731ab3630
Martin Wilke <miwi@FreeBSD.org>
2008-04-26 23:44:52 +0000

    - Update to 0.61.08
    PR:		121798
    Submitted by:	Ports Fury

--- graphics/synfigstudio/files/patch-src_synfigapp_cvs.cpp
+++ graphics/synfigstudio/files/patch-src_synfigapp_cvs.cpp
@@ -1,24 +1,11 @@
---- src/synfigapp/cvs.cpp.orig	Sun Jun 24 17:35:08 2007
-+++ src/synfigapp/cvs.cpp	Sun Jun 24 17:38:54 2007
-@@ -39,6 +39,8 @@
- 
- #include <sys/types.h>
- #include <sys/stat.h>
-+#include <pthread.h>
-+#include <fcntl.h>
- //#include <unistd.h>
- 
- #include <cassert>
-@@ -60,10 +62,10 @@
+--- src/synfigapp/cvs.cpp.orig	2007-11-13 18:13:07.000000000 +0900
++++ src/synfigapp/cvs.cpp	2008-03-14 19:39:08.000000000 +0900
+@@ -62,7 +62,7 @@
  #define HAVE_STRPTIME
  #endif
  
 -#ifdef __APPLE__
-+//#ifdef __APPLE__
++#if defined(__APPLE__) || defined(__FreeBSD__)
  time_t _daylight_() { time_t t(time(0)); return localtime(&t)->tm_gmtoff; }
  #define daylight _daylight_()
--#endif
-+//#endif
- 
- /* === G L O B A L S ======================================================= */
- 
+ #endif
