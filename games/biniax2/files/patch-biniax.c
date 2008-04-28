0059c76a1c1dc2b83592a4e291abc24cad8e584a
Martin Wilke <miwi@FreeBSD.org>
2008-04-28 08:48:10 +0000

    Biniax-2 is original and entertaining game. Takes a minute to learn and gives you hours and hours of gameplay. Discover the different Biniax-2 faces - for action heroes or careful strategists.  Biniax-2 has three game modes (two singleplayer and one multiplayer), hall of fame, dynamic music and nice cartoon look.
    WWW: http://biniax.com/index2.html
    
    PR:		ports/122895
    Submitted by:	Dmitry Marakasov <amdmi3 at amdmi3.ru>

---
+++ games/biniax2/files/patch-biniax.c
@@ -0,0 +1,18 @@
+--- biniax.c.orig	2008-04-08 09:25:14.000000000 +0400
++++ biniax.c	2008-04-16 22:33:02.000000000 +0400
+@@ -89,6 +89,15 @@
+ 
+ int main( int argc, char *argv[] )
+ {
++	if (chdir(getenv("HOME")) != 0)
++		err(1, "cannot chdir to $HOME");
++
++	if (mkdir(".biniax2", 0755) != 0 && errno != EEXIST)
++		err(1, "cannot mkdir $HOME/.biniax2");
++
++	if (chdir(".biniax2") != 0)
++		err(1, "cannot chdir to $HOME/.biniax2");
++
+ 	BNX_BOOL		bquit		= BNX_FALSE;
+ 	BNX_INT16		enterState	= cStateMainMenu;
+ 	BNX_INT16		nmenu		= 0;
