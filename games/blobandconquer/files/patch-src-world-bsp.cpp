830abf1747a3f8e14b0b61e684021d2942c7e030
miwi <miwi@FreeBSD.org>
2008-04-29 12:50:32 +0000

    - Update to 0.92-1
    PR:		123182
    Submitted by:	Dmitry Marakasov <amdmi3@amdmi3.ru> (maintainer)

--- games/blobandconquer/files/patch-src-world-bsp.cpp
+++ games/blobandconquer/files/patch-src-world-bsp.cpp
@@ -1,11 +1,11 @@
---- src/world/bsp.cpp.orig	2007-05-22 00:26:13.000000000 +0400
-+++ src/world/bsp.cpp	2007-12-31 19:23:29.000000000 +0300
+--- src/world/bsp.cpp.orig	2008-04-25 19:38:11.000000000 +0400
++++ src/world/bsp.cpp	2008-04-29 00:26:31.000000000 +0400
 @@ -22,6 +22,8 @@
  
  #define TESSELATION 10
  
 +float swapBytes(float f);
 +
- TextureSpec *getTextureSpec(char *basename)
+ TextureSpec *getTextureSpec(const char *basename)
  {
  	// ignore this texture
