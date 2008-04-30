3073c1b10f7ff8cbd5240a0dc86c8d7d30ad3b48
Edwin Groothuis <edwin@FreeBSD.org>
2008-04-30 12:02:35 +0000

    Update to 3.8.1

--- math/it++/files/patch-include_base_machdep.h
+++ math/it++/files/patch-include_base_machdep.h
@@ -1,5 +1,5 @@
---- include/base/machdep.h.orig	Sat Feb 19 17:57:11 2005
-+++ include/base/machdep.h	Sat Feb 19 17:57:22 2005
+--- include/base/machdep.h.orig	2005-01-17 09:02:20.000000000 +0000
++++ include/base/machdep.h	2008-04-30 11:49:04.000000000 +0000
 @@ -24,6 +24,7 @@
  #ifndef __machdep_h
  #define __machdep_h
@@ -8,14 +8,14 @@
  #include "itconfig.h"
  
  namespace itpp {
-@@ -71,9 +72,9 @@ namespace itpp {
+@@ -71,9 +72,9 @@
    inline void  little_endian(it_s8 x, it_s8 &y) { y = x; }
    inline void  little_endian(it_u8 x, it_u8 &y) { y = x; }
  
 -#if defined(sparc)
 +#if _BYTE_ORDER == _LITTLE_ENDIAN
  #define __LITTLE_ENDIAN__
--#elif defined(i386) || defined(_M_IX86) || defined(alpha) || defined(vms)
+-#elif defined(i386) || defined(_M_IX86) || defined(_x86_64) || defined(alpha) || defined(vms)
 +#elif _BYTE_ORDER == _BIG_ENDIAN
  #define __BIG_ENDIAN__
  #endif
