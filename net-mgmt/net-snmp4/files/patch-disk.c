2ed8f5d65fdf6fbddb28f24f2651e939d375e70b
edwin <edwin@FreeBSD.org>
2008-04-30 11:45:55 +0000

    Update to 4.2.7

--- net-mgmt/net-snmp4/files/patch-disk.c
+++ net-mgmt/net-snmp4/files/patch-disk.c
@@ -1,17 +1,26 @@
---- agent/mibgroup/ucd-snmp/disk.c.orig	Wed Apr  3 07:57:57 2002
-+++ agent/mibgroup/ucd-snmp/disk.c	Thu Aug 29 12:11:17 2002
-@@ -340,7 +340,7 @@
+--- agent/mibgroup/ucd-snmp/disk.c.orig	2003-05-14 10:17:46.000000000 +0000
++++ agent/mibgroup/ucd-snmp/disk.c	2008-04-30 06:01:57.000000000 +0000
+@@ -346,7 +346,7 @@
    double totalblks, free, used, avail, availblks;
  #else
    static long avail;
--#ifdef STRUCT_STATVFS_HAS_F_FILES
-+#if defined(HAVE_UFS_FFS_FS_H) || defined(STRUCT_STATVFS_HAS_F_FILES)
+-#if defined(STRUCT_STATVFS_HAS_F_FILES) || defined(STRUCT_STATFS_HAS_F_FILES)
++#if defined(STRUCT_STATVFS_HAS_F_FILES) || defined(STRUCT_STATFS_HAS_F_FILES) || defined(HAVE_UFS_FFS_FS_H)
    int percent_inode;
  #endif
  #endif
-@@ -424,6 +424,11 @@
+@@ -425,7 +425,7 @@
+   iserror = (disks[disknum].minimumspace >= 0 ?
+              avail < disks[disknum].minimumspace :
+              100-percent <= disks[disknum].minpercent) ? 1 : 0;
+-#if defined(STRUCT_STATVFS_HAS_F_FILES) || defined(STRUCT_STATFS_HAS_F_AVAIL)
++#if defined(STRUCT_STATVFS_HAS_F_FILES) || defined(STRUCT_STATFS_HAS_F_AVAIL) || defined(HAVE_UFS_FFS_FS_H)
+   percent_inode = vfs.f_favail <= 0 ? 100 :
      (int) ((double) (vfs.f_files - vfs.f_ffree) /
             (double) (vfs.f_files - (vfs.f_ffree - vfs.f_favail)) * 100.0 + 0.5);
+@@ -436,6 +436,11 @@
+            (double) (vfs.f_files) * 100.0 + 0.5);
+ #endif
  #endif
 +#if defined(HAVE_UFS_FFS_FS_H) && !defined(STRUCT_STATVFS_HAS_F_FILES)
 +  percent_inode = vfs.f_files == 0 ? 100.0 :
@@ -21,12 +30,12 @@
    switch (vp->magic) {
      case DISKTOTAL:
        long_ret = vfs.f_blocks * (vfs.f_bsize / 1024);
-@@ -444,7 +449,7 @@
+@@ -456,7 +461,7 @@
      case DISKPERCENT:
        long_ret = percent;
        return ((u_char *) (&long_ret));
--#ifdef STRUCT_STATVFS_HAS_F_FILES
-+#if defined(HAVE_UFS_FFS_FS_H) || defined(STRUCT_STATVFS_HAS_F_FILES)
+-#if defined(STRUCT_STATVFS_HAS_F_FILES) || defined(STRUCT_STATFS_HAS_F_FILES)
++#if defined(STRUCT_STATVFS_HAS_F_FILES) || defined(STRUCT_STATFS_HAS_F_FILES) || defined(HAVE_UFS_FFS_FS_H)
      case DISKPERCENTNODE:
        long_ret = percent_inode;
        return ((u_char *) (&long_ret));
--- net-mgmt/net-snmp4/files/patch-hr_swrun.c
+++ net-mgmt/net-snmp4/files/patch-hr_swrun.c
@@ -1,84 +1,47 @@
---- agent/mibgroup/host/hr_swrun.c.orig	Sat Feb 16 09:41:19 2002
-+++ agent/mibgroup/host/hr_swrun.c	Sat Feb 16 23:41:05 2002
-@@ -543,7 +543,11 @@
- 	    strcpy(string, proc_buf->p_user.u_comm);
+--- agent/mibgroup/host/hr_swrun.c.orig	2006-06-06 08:11:59.000000000 +0000
++++ agent/mibgroup/host/hr_swrun.c	2008-04-30 11:30:47.000000000 +0000
+@@ -549,7 +549,7 @@
+             string[ sizeof(string)-1 ] = 0;
  #endif
  #elif HAVE_KVM_GETPROCS
-+#if defined(freebsd5) && __FreeBSD_version >= 500014
-+            strcpy(string, proc_table[LowProcIndex].ki_comm);
-+#else
+-    #if defined(freebsd5)
++    #if defined(freebsd5) && __FreeBSD_version >= 500014
+             strcpy(string, proc_table[LowProcIndex].ki_comm);
+     #else
              strcpy(string, proc_table[LowProcIndex].kp_proc.p_comm);
-+#endif
- #elif defined(linux)
- 	    sprintf( string, "/proc/%d/status", pid );
- 	    if ((fp = fopen( string, "r")) == NULL) return NULL;
-@@ -638,7 +642,11 @@
+@@ -648,7 +648,7 @@
  	    *cp1 = 0;
  #endif
  #elif HAVE_KVM_GETPROCS
-+#if defined(freebsd5) && __FreeBSD_version >= 500014
-+            strcpy(string, proc_table[LowProcIndex].ki_comm);
-+#else
+-    #if defined(freebsd5)
++    #if defined(freebsd5) && __FreeBSD_version >= 500014
+             strcpy(string, proc_table[LowProcIndex].ki_comm);
+     #else
              strcpy(string, proc_table[LowProcIndex].kp_proc.p_comm);
-+#endif
- #elif defined(linux)
- 	    sprintf( string, "/proc/%d/cmdline", pid );
- 	    if ((fp = fopen( string, "r")) == NULL) return NULL;
-@@ -810,7 +818,11 @@
+@@ -827,7 +827,7 @@
  	    }
  #else
  #if HAVE_KVM_GETPROCS
-+#if defined(freebsd5) && __FreeBSD_version >= 500014
-+	    switch ( proc_table[LowProcIndex].ki_stat ) {
-+#else
- 	    switch ( proc_table[LowProcIndex].kp_proc.p_stat ) {
-+#endif
- #elif defined(dynix)
- 	    switch ( lowpsinfo.pr_state ) {
- #elif defined(solaris2)
-@@ -906,9 +918,17 @@
+-    #if defined(freebsd5)
++    #if defined(freebsd5) && __FreeBSD_version >= 500014
+             switch ( proc_table[LowProcIndex].ki_stat ) {
+     #else
+             switch ( proc_table[LowProcIndex].kp_proc.p_stat ) {
+@@ -927,7 +927,7 @@
  	    		  proc_buf->p_stime*100;
  #endif
  #elif HAVE_KVM_GETPROCS
-+#if defined(freebsd5) && __FreeBSD_version >= 500014
-+	    /* XXX: Accessing ki_paddr causes sig10 ...
-+	    long_return = proc_table[LowProcIndex].ki_paddr->p_uticks +
-+	    		  proc_table[LowProcIndex].ki_paddr->p_sticks +
-+	    		  proc_table[LowProcIndex].ki_paddr->p_iticks; */
-+	    long_return = 0;
-+#else
+-    #if defined(freebsd5)
++    #if defined(freebsd5) && __FreeBSD_version >= 500014
+ 	    long_return = proc_table[LowProcIndex].ki_runtime / 100000;
+     #else
  	    long_return = proc_table[LowProcIndex].kp_proc.p_uticks +
- 	    		  proc_table[LowProcIndex].kp_proc.p_sticks +
- 	    		  proc_table[LowProcIndex].kp_proc.p_iticks;
-+#endif
- #elif defined(linux)
- 	    sprintf( string, "/proc/%d/stat", pid );
- 	    if ((fp = fopen( string, "r")) == NULL) return NULL;
-@@ -976,6 +996,13 @@
- #elif HAVE_KVM_GETPROCS
- #if defined(freebsd3) && !defined(darwin)
- 	    long_return = proc_table[LowProcIndex].kp_eproc.e_vm.vm_map.size/1024;
-+#elif defined(freebsd5) && __FreeBSD_version >= 500014
-+	    /* XXX
-+	    long_return = proc_table[LowProcIndex].ki_vmspace->vm_tsize +
-+			  proc_table[LowProcIndex].ki_vmspace->vm_ssize +
-+			  proc_table[LowProcIndex].ki_vmspace->vm_dsize;
-+	    long_return = long_return * (getpagesize() / 1024); */
-+	    long_return = 0;
- #else
- 	    long_return = proc_table[LowProcIndex].kp_eproc.e_vm.vm_tsize +
- 			  proc_table[LowProcIndex].kp_eproc.e_vm.vm_ssize +
-@@ -1237,8 +1264,13 @@
+@@ -1266,7 +1266,7 @@
  #elif defined(solaris2)
  	return proc_table[current_proc_entry++];
  #elif HAVE_KVM_GETPROCS
-+#if defined(freebsd5) && __FreeBSD_version >= 500014
-+	if ( proc_table[current_proc_entry].ki_stat != 0 )
-+	    return proc_table[current_proc_entry++].ki_pid;
-+#else
- 	if ( proc_table[current_proc_entry].kp_proc.p_stat != 0 )
- 	    return proc_table[current_proc_entry++].kp_proc.p_pid;
-+#endif
- #else
- 	if ( proc_table[current_proc_entry].p_stat != 0 )
- 	    return proc_table[current_proc_entry++].p_pid;
+-    #if defined(freebsd5)
++    #if defined(freebsd5) && __FreeBSD_version >= 500014
+ 	if ( proc_table[current_proc_entry].ki_stat != 0 )
+ 	    return proc_table[current_proc_entry++].ki_pid;
+     #else
