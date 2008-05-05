5e139cfb527fd1b9be56a6206f44b99864fcc9f4
hrs <hrs@FreeBSD.org>
2008-05-05 16:08:43 +0000

    Update to 0.14.  Changes since version 0.10 include:
     * Several RBA extensions: i.e. alternative methods replacing
       different 'new' variants by one 'new'. Markers now can be
       filled.
    
     * Fixes for the bugs mentioned in the issues list.
    
     * OASIS and GDS writing capabilities. Menu functions are
       provided to save a layout and single cells as well as RBA
       extensions that allow to write a layout. See Saving a layout
       or parts of it for details.
    
     * Rulers and markers are drawn with a thin halo in background
       color to enhance their visibility. This can be switched off in
       the setup dialog if this is not desired. The following
       screenshots illustrate the differences:
    
     * A couple of RBA extensions: API's for shape properties, rulers
       and geometry selections.
    
     * A bugfix for the OASIS reader (sometimes crashing the reader
       on CBLOCK-compressed input)
    
     * A function to descend into a cell in the hierarchy but keeping
       the context, which is shown in dimmed colors. This function
       can be found in the "Display" menu and is called "Descend". To
       use this function, select a shape or instance inside the cell
       into which to descend. "Descend" then enters the next cell
       along the path that leads to the selected shape or instance.
    
     * The mouse wheel can be used to zoom in and out.
    
     * Shift+right mouse button pressed now allows to "drag" layout
       in a "Google maps" fashion.
    
     * Copy & paste now is available for layers as well.
    
     * A ruby console is provided where ruby commands can be executed
       interactively.
    
     * The way how the selection is displayed can be configured
       now (color, line width and vertex size).
    
     * The interrupt signal now is enabled again even if Ruby is
       activated (Ctrl+C now works again).
    
     * Support for arbitrary angles on cell instances.
    
     * A bugfix for the OASIS reader (CTRAPEZOIDS have not been read
       in some cases). The OASIS reader now uses shape arrays to
       acchieve are smaller memory footprint in some cases.
    
     * Multiple transformations are now allowed on layer source
       expressions (see Transforming views and property selectors for
       a detailed description).
    
     * Rulers now are configurable to a large extent. Multiple ruler
       templates may be defined from which a certain ruler type can
       be choosen. Some of the new ruler styles are no
       longer "rulers" but rather "annotations". See Ruler properties
       for a description of that feature.
    
     * Last, but not least: KLayout now can be scripted to some
       extend using Ruby as the scripting language. For more
       information about this feature, see RBA introduction.
    
     * Some bugs were fixed in the OASIS implementation. CBLOCK
       compression now is supported. Forward cell name
       references (numerical ID is used before being associated with
       a string) are supported as now. File global properties are now
       read correctly. A bug reading certain CTRAPEZOID objects was
       fixed.
       (Still, forward references are not allowed for text/property
       strings or property names).
    
     * A layout properties dialog has been added that shows basic
       properties of the layout such as top cells, layers and others.
    
     * The "Select Cell" dialog has been enhanced with the capability
       to hide and show multiple cells in the cell list.
    
     * A "zoom fit" mouse gesture was implemented: moving the mouse
       up and right with the right mouse button pressed now fits the
       current cell into the window. Moving up and left still zooms
       out.
    
     * A bug in the GDS2 reader, dropping polygon points in rare
       cases, was fixed

--- cad/klayout/files/patch-build.sh
+++ cad/klayout/files/patch-build.sh
@@ -1,33 +1,32 @@
---- build.sh.orig	Tue May  8 04:41:18 2007
-+++ build.sh	Sat Jun  2 16:43:04 2007
-@@ -77,12 +77,12 @@
+--- build.sh.orig	Fri Nov  9 04:31:36 2007
++++ build.sh	Sun Jan  6 18:57:53 2008
+@@ -126,11 +126,11 @@
    exit 1
  fi
  
--if [ ! -r $QTDIR/bin/moc ]; then
-+if [ ! -r $QTDIR/bin/moc-qt4 ]; then
-   echo "*** ERROR: unable to locate Qt MOC in $QTDIR/bin"
+-if [ ! -r $QTBIN/moc ]; then
++if [ ! -r $QTBIN/moc-qt4 ]; then
+   echo "*** ERROR: unable to locate Qt MOC in $QTBIN"
    exit 1
  fi
- 
--if [ ! -r $QTDIR/bin/uic ]; then
-+if [ ! -r $QTDIR/bin/uic-qt4 ]; then
-   echo "*** ERROR: unable to locate Qt UIC in $QTDIR/bin"
+-if [ ! -r $QTBIN/uic ]; then
++if [ ! -r $QTBIN/uic-qt4 ]; then
+   echo "*** ERROR: unable to locate Qt UIC in $QTBIN"
    exit 1
  fi
-@@ -153,12 +153,12 @@
+@@ -231,12 +231,12 @@
    echo "Build done."
    echo ""
    echo "Final binary is ready in $BIN/$EXEC_NAME."
 -  echo "To run it, you may need to add the following to your LD_LIBRARY_PATH:"
--  echo "$QTDIR/lib"
+-  echo "$QTLIB"
 +  #echo "To run it, you may need to add the following to your LD_LIBRARY_PATH:"
-+  #echo "$QTDIR/lib"
++  #echo "$QTLIB"
    
--  LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
+-  LD_LIBRARY_PATH=$QTLIB:$LD_LIBRARY_PATH
 -  $BIN/$EXEC_NAME -v
 -  $BIN/$EXEC_NAME -h
-+  #LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
++  #LD_LIBRARY_PATH=$QTLIB:$LD_LIBRARY_PATH
 +  #$BIN/$EXEC_NAME -v
 +  #$BIN/$EXEC_NAME -h
  
