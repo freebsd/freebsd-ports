0bc90c5bf91d969ae05bb2269c9872d36609cc20
Dirk Meyer <dinoex@FreeBSD.org>
2008-05-05 13:49:19 +0000

    - fix build for option WITH_GNUSTEP_DEVEL

---
+++ science/paje/files/patch-NSArray+Additions.m
@@ -0,0 +1,10 @@
+--- General/NSArray+Additions.m.orig	2006-01-24 14:10:07.000000000 +0100
++++ General/NSArray+Additions.m	2008-05-03 08:13:11.000000000 +0200
+@@ -19,6 +19,7 @@
+ */
+ 
+ #include "NSArray+Additions.h"
++#include "Foundation/NSEnumerator.h"
+ #include "Macros.h"
+ 
+ // Enumerator of objects in a range of indexes of an array
