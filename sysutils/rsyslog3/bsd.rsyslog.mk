573607809d6f3c935bde2343039c06b5b8bab141
sat <sat@FreeBSD.org>
2008-05-06 15:25:01 +0000

    Add rsyslog 3.x to ports:
    Rsyslog is an enhanced multi-threaded syslogd supporting, among
    others, syslog/tcp, permitted sender lists, filtering on any message
    part, and fine grain output format control.  Its advanced features
    make it suitable for enterprise-class, encryption protected syslog
    relay chains while at the same time being very easy to setup for the
    novice user.
    
    The following features are supported through modules: logging to
    MySQL, PostgreSQL, any libdbi-supported backend; SNMP trap sender, GSS
    API, RELP.
    
    WWW: http://www.rsyslog.com/

---
+++ sysutils/rsyslog3/bsd.rsyslog.mk
@@ -0,0 +1,31 @@
+PORTNAME?=	rsyslog
+PORTVERSION?=	3.17.2
+CATEGORIES?=	sysutils
+MASTER_SITES?=	CENKES http://download.rsyslog.com/rsyslog/
+MAINTAINER?=	infofarmer@FreeBSD.org
+
+CONFLICTS=	rsyslog-2.[0-9]*
+CPPFLAGS+=	-I${LOCALBASE}/include
+LDFLAGS+=	-L${LOCALBASE}/lib
+GNU_CONFIGURE=	yes
+CONFIGURE_ENV+=	CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}"
+
+.ifdef MNAME
+PKGNAMESUFFIX?=	-${MNAME}
+CONFIGURE_ARGS+=	--enable-${MNAME:S|gssapi|gssapi-krb5|} \
+			--disable-rsyslogd --disable-klog
+RUN_DEPENDS=	rsyslog>=3:${PORTSDIR}/sysutils/rsyslog3
+PLIST_FILES=	${MTYPES:S|^|lib/rsyslog/|:S|$|${MNAME}.so|}
+PLIST=	""
+DESCR?=		${.CURDIR}/../rsyslog3/pkg-descr
+MD5_FILE?=	${.CURDIR}/../rsyslog3/distinfo
+
+do-install:
+	@${INSTALL} -d ${PREFIX}/lib/rsyslog/
+.for _T in ${MTYPES}
+	@${INSTALL_PROGRAM} \
+		${WRKSRC}/plugins/${_T}${MNAME}/.libs/${_T}${MNAME}.so \
+		${PREFIX}/lib/rsyslog/
+.endfor
+	@${ECHO_CMD} '@unexec rmdir %D/lib/rsyslog 2>/dev/null || true' >> ${TMPPLIST}
+.endif # def MNAME
