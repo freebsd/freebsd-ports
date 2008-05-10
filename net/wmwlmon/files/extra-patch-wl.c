abb59559178f81753e10f8dd6b92db13d3b41670
itetcu <itetcu@FreeBSD.org>
2008-05-10 09:00:09 +0000

    - fix build with gcc 4.2 [1] - fix build on 7 after wi_req removal (remove support for wi(4)) [2] - unBROKEN [1] - unDEPRECIATE [1] - use REINPALCE_CMD
    PR:		ports/123408 [1], [2] by itetcu@ (me) based on [1]
    Submitted by:	Luke Dean <luked@pobox.com> [1]

---
+++ net/wmwlmon/files/extra-patch-wl.c
@@ -0,0 +1,20 @@
+--- ./wl.c.orig 2006-05-10 06:57:57.000000000 +0300
++++ ./wl.c      2008-05-10 11:03:57.000000000 +0300
+@@ -283,6 +284,7 @@
+ int
+ get_wi_signal(const char *interface)
+ {
++#if __FreeBSD__ > 600100 && __FreeBSD__ < 700047 /* wi support is broken on 7 because of struct wi_req removal*/
+ 	int		s;
+ 	struct ifreq	ifr;
+ 	struct wi_req	wreq;
+@@ -319,6 +321,9 @@
+ #ifdef __FreeBSD__
+ 	return (wreq.wi_val[1]);
+ #endif
++#else
++	return -1;
++#endif
+ }
+ 
+ /*
