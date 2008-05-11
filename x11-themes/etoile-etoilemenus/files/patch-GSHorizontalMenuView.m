9ec35d7a1b51a5c33a566e08e4bfaea7937e0bcf
dinoex <dinoex@FreeBSD.org>
2008-05-11 13:21:59 +0000

    - update to etoile 0.2

---
+++ x11-themes/etoile-etoilemenus/files/patch-GSHorizontalMenuView.m
@@ -0,0 +1,10 @@
+--- Bundles/EtoileWildMenus/GSHorizontalMenuView.m.orig	2006-12-04 23:08:17.000000000 +0100
++++ Bundles/EtoileWildMenus/GSHorizontalMenuView.m	2008-05-03 09:23:03.000000000 +0200
+@@ -23,6 +23,7 @@
+ #include "GSHorizontalMenuView.h"
+ #include <Foundation/Foundation.h>
+ 
++#import <AppKit/NSAttributedString.h>
+ #import <AppKit/NSParagraphStyle.h>
+ #import <AppKit/NSImage.h>
+ #import <AppKit/NSColor.h>
