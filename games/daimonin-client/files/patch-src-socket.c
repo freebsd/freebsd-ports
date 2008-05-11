88a77db9053dc72699a02efcb14bc9b94f04ab11
pav <pav@FreeBSD.org>
2008-05-11 13:52:05 +0000

    - Update to 0.9.7.1
    PR:		ports/123589
    Submitted by:	Aleksandr Stankevic <alex@braske.net>

--- games/daimonin-client/files/patch-src-socket.c
+++ games/daimonin-client/files/patch-src-socket.c
@@ -1,11 +1,13 @@
---- ../../src/socket.c.orig	Sun May 20 02:10:22 2007
-+++ ../../src/socket.c	Thu Jun  7 01:09:08 2007
-@@ -706,6 +706,8 @@
+--- ../../src/socket.c.orig	2008-05-10 13:14:59.000000000 +0300
++++ ../../src/socket.c	2008-05-10 13:15:04.000000000 +0300
+@@ -706,7 +706,9 @@
      start_timer = SDL_GetTicks();
      while (connect(*socket_temp, (struct sockaddr *) &insock, sizeof(insock)) == -1)
      {
+-        SDL_Delay(3);
 +	// EINPROGRESS leads to EALREADY over few iterations
 +	if (errno == EALREADY) break;
-         SDL_Delay(3);
++        SDL_Delay(30);
          /* timeout.... without connect will REALLY hang a long time */
          if (start_timer + SOCKET_TIMEOUT_MS < SDL_GetTicks())
+         {
