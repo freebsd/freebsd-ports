3063e83bf1cd369fb5f119c67b2ab7bc3b8d741c
miwi <miwi@FreeBSD.org>
2008-05-18 00:02:03 +0000

    The F-1 Spirit remake is mostly identical to the original Konami classic F-1 Spirit game in that you first have to clear tracks in a top-down view with stock cars, rally cars and so on by accumulating points given for finishing in the first places until you actually get to play the Formula 1 tracks.
    Otherwise there are quite some enhancements, namely improved graphics,
    improved music, more realistic physics, real shape shown in the
    maps, camera rotation and zoom, split screen for up to four players,
    (online) highscore tables and replay saving.
    
    WWW: http://www.braingames.getput.com/f1spirit/default.asp
    
    PR:		ports/123686
    Submitted by:	Dmitry Marakasov <amdmi3 at amdmi3.ru>

---
+++ games/f1spirit-remake/files/patch-sources-CPlayer.cpp
@@ -0,0 +1,42 @@
+--- sources/CPlayer.cpp.orig	2008-05-17 02:59:23.000000000 +0400
++++ sources/CPlayer.cpp	2008-05-17 03:01:31.000000000 +0400
+@@ -204,7 +204,8 @@
+ 		struct tm *today;
+ 
+ 		gettimeofday(&ttime, NULL);
+-		today=localtime(&(ttime.tv_sec));
++		time_t tmp = (time_t)ttime.tv_sec;
++		today=localtime(&tmp);
+ 		
+ 		position_date[i].year=today->tm_year;
+ 		position_date[i].month=today->tm_mon;
+@@ -231,7 +232,8 @@
+ 		struct tm *today;
+ 
+ 		gettimeofday(&ttime, NULL);
+-		today=localtime(&(ttime.tv_sec));
++		time_t tmp = (time_t)ttime.tv_sec;
++		today=localtime(&tmp);
+ 		
+ 		time_date[i].year=today->tm_year;
+ 		time_date[i].month=today->tm_mon;
+@@ -258,7 +260,8 @@
+ 		struct tm *today;
+ 
+ 		gettimeofday(&ttime, NULL);
+-		today=localtime(&(ttime.tv_sec));
++		time_t tmp = (time_t)ttime.tv_sec;
++		today=localtime(&tmp);
+ 		
+ 		lap_date[i].year=today->tm_year;
+ 		lap_date[i].month=today->tm_mon;
+@@ -285,7 +288,8 @@
+ 		struct tm *today;
+ 
+ 		gettimeofday(&ttime, NULL);
+-		today=localtime(&(ttime.tv_sec));
++		time_t tmp = (time_t)ttime.tv_sec;
++		today=localtime(&tmp);
+ 		
+ 		position_date[i].year=today->tm_year;
+ 		position_date[i].month=today->tm_mon;
