78fe2f47ebeae34915fd3c0aa4297025fbfe085a
Dmitry Sivachenko <demon@FreeBSD.org>
2008-05-19 15:18:46 +0000

    Update to 0.6.
    PR:		123370
    Submitted by:	leeym

--- devel/cgilib/files/patch-cgi.c
+++ devel/cgilib/files/patch-cgi.c
@@ -1,11 +1,33 @@
---- cgi.c.orig	Sat Aug 21 01:14:07 1999
-+++ cgi.c	Sun May  8 22:42:30 2005
-@@ -31,7 +31,7 @@
- #include <unistd.h>
- #include <string.h>
- #include <ctype.h>
--#include <malloc.h>
-+#include <stdlib.h>
- #include <cgi.h>
+--- cgi.c.orig	2008-05-03 09:51:51.000000000 -0700
++++ cgi.c	2008-05-03 09:52:40.000000000 -0700
+@@ -227,6 +227,30 @@
+     return strdup (template);
+ }
  
- int cgiDebugLevel = 0;
++
++size_t
++strnlen(const char *s, size_t len)
++{
++    size_t i;
++
++    for(i = 0; i < len && s[i]; i++)
++	;
++    return i;
++}
++
++char *
++strndup(const char *old, size_t sz)
++{
++    size_t len = strnlen (old, sz);
++    char *t    = malloc(len + 1);
++
++    if (t != NULL) {
++	memcpy (t, old, len);
++	t[len] = '\0';
++    }
++    return t;
++}
++
+ /* cgiReadMultipart()
+  *
+  * Decode multipart/form-data
