43a4530c32ccb249eea7e38e2168740384ea324e
Jeremy Messenger <mezz@FreeBSD.org>
2008-05-19 23:15:41 +0000

    Update to 0.5.9.1.

--- net-p2p/deluge05/files/patch-setup.py
+++ net-p2p/deluge05/files/patch-setup.py
@@ -1,14 +1,15 @@
---- setup.py.orig	2008-05-01 11:22:01.000000000 -0500
-+++ setup.py	2008-05-01 11:24:16.000000000 -0500
-@@ -103,14 +103,14 @@
+--- setup.py.orig	2008-05-19 17:26:22.000000000 -0500
++++ setup.py	2008-05-19 17:28:27.000000000 -0500
+@@ -103,13 +103,13 @@
                  "-DHAVE_INCLUDE_LIBTORRENT_ASIO_SSL_STREAM_HPP=1", 
                  "-DHAVE_INCLUDE_LIBTORRENT_ASIO_IP_TCP_HPP=1", 
                  "-DHAVE_PTHREAD=1", "-DTORRENT_USE_OPENSSL=1", "-DHAVE_SSL=1", 
--                "-DNDEBUG=1", '-O2']
+-                "-DNDEBUG=1", "-O2"]
+-    if ARCH == "x64":
+-        EXTRA_COMPILE_ARGS.append("-DAMD64")
 +                "-DNDEBUG=1"]
- # Done through debian/rules, as all arches are not handled
- #    if ARCH == "x64":
- #        EXTRA_COMPILE_ARGS.append("-DAMD64")
++#    if ARCH == "x64":
++#        EXTRA_COMPILE_ARGS.append("-DAMD64")
  
      includedirs = ['./libtorrent', './libtorrent/include', 
                       './libtorrent/include/libtorrent', 
@@ -17,7 +18,7 @@
  
      if OS == "linux":
          if os.path.exists(os.path.join(sysconfig.get_config_vars()['LIBDIR'], \
-@@ -149,7 +149,7 @@
+@@ -148,7 +148,7 @@
          boost_thread = "boost_thread-mt"
  
      librariestype = [boost_filesystem, boost_date_time,
@@ -26,7 +27,7 @@
  
      if os == "linux":
          librariestype += ['rt']
-@@ -210,8 +210,10 @@
+@@ -205,8 +205,10 @@
      sources.remove('libtorrent/src/file_win.cpp')
      deluge_core = Extension('deluge_core',
                      include_dirs = includedirs,
