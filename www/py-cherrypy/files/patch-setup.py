47d137ef88f4761e86f84e10daf776a8d518cbe7
nivit <nivit@FreeBSD.org>
2008-05-26 13:54:53 +0000

    - Update to 3.0.3 - Add a secondary download site

--- www/py-cherrypy/files/patch-setup.py
+++ www/py-cherrypy/files/patch-setup.py
@@ -1,14 +1,14 @@
---- setup.py	2007-08-06 02:07:32.000000000 +0200
-+++ setup.py.port	2008-01-26 17:46:16.000000000 +0100
-@@ -38,24 +38,38 @@
+--- setup.py	2008-01-13 22:38:39.000000000 +0100
++++ setup.py.port	2008-05-25 12:04:24.000000000 +0200
+@@ -38,24 +38,39 @@
  cp_license="BSD"
  packages=[
      "cherrypy", "cherrypy.lib",
 -    "cherrypy.tutorial", "cherrypy.test",
--    "cherrypy.wsgiserver",
-+    "cherrypy.test", "cherrypy.wsgiserver",
++    "cherrypy.test",
+     "cherrypy.wsgiserver",
  ]
- download_url="http://download.cherrypy.org/cherrypy/3.0.2/"
+ download_url="http://download.cherrypy.org/cherrypy/3.0.3/"
  data_files=[
 -    ('cherrypy/tutorial',
 -        [
@@ -18,6 +18,7 @@
 -            'cherrypy/tutorial/custom_error.html',
 -        ]
 -    ),
++
      ('cherrypy', ['cherrypy/favicon.ico',]),
      ('cherrypy/test', ['cherrypy/test/style.css',]),
      ('cherrypy/test/static', ['cherrypy/test/static/index.html',
@@ -38,14 +39,13 @@
 +    data_files.append(
 +	('%%DOCSDIR%%',
 +	    [
-+		'cherrypy/tutorial/README.txt',
++        	'cherrypy/tutorial/README.txt',
 +        	'cherrypy/tutorial/pdf_file.pdf',
 +        	'cherrypy/tutorial/custom_error.html',
 +	    ]
 +	),
 +    )
 +
-+    	    
  ###############################################################################
  # end arguments for setup
  ###############################################################################
