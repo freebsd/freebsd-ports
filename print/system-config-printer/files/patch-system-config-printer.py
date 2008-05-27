6d71ee2cda9e65e9852015100b908d87a1b239e8
Michael Johnson <ahze@FreeBSD.org>
2008-05-27 06:31:37 +0000

    Update to 0.9.93

--- print/system-config-printer/files/patch-system-config-printer.py
+++ print/system-config-printer/files/patch-system-config-printer.py
@@ -1,15 +1,6 @@
---- system-config-printer.py.orig	2008-04-03 11:54:02.000000000 -0400
-+++ system-config-printer.py	2008-04-05 10:02:07.950114470 -0400
-@@ -77,7 +77,7 @@
- import gettext
- gettext.textdomain (domain)
- gtk.glade.bindtextdomain (domain)
--pkgdata = '/usr/share/' + domain
-+pkgdata = '%%PREFIX%%/share/' + domain
- iconpath = pkgdata + '/icons/'
- glade_file = pkgdata + '/' + domain + '.glade'
- sys.path.append (pkgdata)
-@@ -2855,7 +2855,7 @@
+--- system-config-printer.py.orig	2008-05-23 04:57:31.000000000 -0700
++++ system-config-printer.py	2008-05-26 23:25:11.159502896 -0700
+@@ -3098,7 +3098,7 @@
          # Try to get make and model via SNMP
          if host:
              os.environ["HOST"] = host
@@ -18,7 +9,7 @@
              debugprint (host + ": " + cmd)
              p = os.popen(cmd, 'r')
              output = p.read ().strip ()
-@@ -3931,7 +3931,7 @@
+@@ -4370,7 +4370,7 @@
                  os.environ["PPD"] = filename
                  # We want this to be in the current natural language,
                  # so we intentionally don't set LC_ALL=C here.
@@ -27,7 +18,7 @@
                  output = p.readlines ()
                  p.close ()
                  err += reduce (lambda x, y: x + y, output)
-@@ -4212,7 +4212,7 @@
+@@ -4655,7 +4655,7 @@
          (pkgs, exes) = cupshelpers.missingPackagesAndExecutables (ppd)
          if len (pkgs) > 0 or len (exes) > 0:
              # We didn't find a necessary executable.  Complain.
