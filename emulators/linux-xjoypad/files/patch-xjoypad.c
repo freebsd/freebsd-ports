ffaeb5805d8a2c2b53602ad58eba46b2125c20bc
edwin <edwin@FreeBSD.org>
2008-05-28 12:58:56 +0000

    New Port: emulators/xjoypad
    	xjoypad allows an USB joypad (probably a USB Joystick, too)
    	to be used with games on linux (probably other unix-like
    	operating systems too).  This is done by mapping the joypad
    	events to X keyboard events which are sent to the window
    	currently having the focus. Therefore only keyboard-controlled
    	games are currently supported by xjoypad. xjoypad was written
    	to add joypad support to the playstation emulator epsxe.
    
    PR:		ports/95990
    Submitted by:	Timothy Beyer <beyert@cs.ucr.edu>

---
+++ emulators/linux-xjoypad/files/patch-xjoypad.c
@@ -0,0 +1,10 @@
+--- xjoypad.c.orig	2008-04-20 20:07:58.000000000 -0700
++++ xjoypad.c	2008-04-20 20:11:19.000000000 -0700
+@@ -20,6 +20,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
