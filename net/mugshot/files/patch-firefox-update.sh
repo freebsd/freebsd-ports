c8dfddbe4c04b9812d6bdd8fb7405f7854799400
miwi <miwi@FreeBSD.org>
2008-05-30 21:52:00 +0000

    - Update to 124079
    PR:		124079
    Submitted by:	Ashish Shukla <wahjava@gmail.com> (maintainer)

--- net/mugshot/files/patch-firefox-update.sh
+++ net/mugshot/files/patch-firefox-update.sh
@@ -1,30 +1,30 @@
---- firefox-update.sh.orig	Sun Dec 24 22:31:37 2006
-+++ firefox-update.sh	Sun Dec 24 22:34:38 2006
-@@ -6,7 +6,7 @@
- # Firefox is installed into versioned directories in /usr/lib[64]/firefox
- #
+--- firefox-update.sh.pristine	2008-04-27 22:42:09.000000000 +0530
++++ firefox-update.sh	2008-04-27 22:46:58.000000000 +0530
+@@ -8,7 +8,7 @@
+ 
+ EXT_ID=firefox@mugshot.org
  if [ "$1" = "install" ] ; then
 -    for libdir in /usr/lib /usr/lib64 ; do
 +	libdir=%%PREFIX%%/lib
  	# Add symlinks to any firefox directory that looks like it is part of a
  	# currently installed package
- 	for d in $libdir/firefox* ; do
-@@ -19,9 +19,8 @@
- 		ln -s $target $link
- 	    fi
- 	done
+ 	for d in $libdir/firefox* $libdir/iceweasel*; do
+@@ -25,9 +25,8 @@
+         if [ -d $libdir/mozilla/extensions -a ! -L "$link" ]; then
+             ln -s $libdir/mugshot/firefox "$link"
+         fi
 -    done
  elif [ "$1" = "remove" ] ; then
 -    for libdir in /usr/lib /usr/lib64 ; do
 +	libdir=%%PREFIX%%/lib
  	# Remove any symlinks we've created into any firefox directory
- 	for d in $libdir/firefox* ; do
- 	    if [ d = "$libdir/firefox*" ] ; then
-@@ -32,7 +31,6 @@
+ 	for d in $libdir/firefox* $libdir/iceweasel*; do
+ 	    if [ "$d" = "$libdir/firefox*" -o "$d" = "$libdir/iceweasel*" ] ; then
+@@ -38,7 +37,6 @@
  		rm $link
  	    fi
  	done
 -     done
- else
-     echo "Usage firefox-update.sh [install/remove]"
- fi
+      if [ -d $libdir/mozilla/extensions ]; then
+         rm $libdir/mozilla/extensions/$EXT_ID
+      fi
