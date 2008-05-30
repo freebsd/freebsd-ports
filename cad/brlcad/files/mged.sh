1993093df1e1a758f7da30c2cb7cc557e2af8dc5
miwi <miwi@FreeBSD.org>
2008-05-30 13:49:18 +0000

    - Update to 7.12.4
    PR:		124105
    Submitted by:	Erik Greenwald <erik@smluc.org> (maintainer)

--- cad/brlcad/files/mged.sh
+++ cad/brlcad/files/mged.sh
@@ -7,5 +7,7 @@ BRLCAD_ROOT=%%BRLCAD_ROOT%%
 export BRLCAD_ROOT
 export PATH=%%BRLCAD_ROOT%%/bin:$PATH
 export MANPATH=%%BRLCAD_ROOT%%/man:$MANPATH
+export ITCL_PATH=%%BRLCAD_ROOT%%/lib/itcl3.4
+export ITK_PATH=%%BRLCAD_ROOT%%/lib/itk3.4
 
 exec %%BRLCAD_ROOT%%/bin/mged $@
