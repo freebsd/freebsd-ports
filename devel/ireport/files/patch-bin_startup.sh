bd41946dfc82899a0b6474c49d360967fbdbcaf7
thierry <thierry@FreeBSD.org>
2008-06-01 13:53:06 +0000

    Upgrade to iReport-3.0.0.
    Changelog at
    <https://sourceforge.net/project/shownotes.php?release_id=603069>.

--- devel/ireport/files/patch-bin_startup.sh
+++ devel/ireport/files/patch-bin_startup.sh
@@ -1,5 +1,5 @@
---- bin/startup.sh.orig	Thu Nov 16 02:59:28 2006
-+++ bin/startup.sh	Thu Nov 16 21:15:54 2006
+--- bin/startup.sh.orig	2008-05-30 08:34:03.000000000 +0200
++++ bin/startup.sh	2008-06-01 15:44:53.000000000 +0200
 @@ -1,33 +1,13 @@
  #! /bin/sh
  
@@ -16,9 +16,7 @@
 -    IREPORT_CLASSPATH="$i":$IREPORT_CLASSPATH
 -  fi
 -done
-+IREPORT_CLASSPATH="%%CLSSPTH%%:%%DATADIR%%/fonts"
-+IREPORT_HOME="$HOME/ireport"
- 
+-
 -DIRLIBS=${EXEDIR}/../lib/*.zip
 -for i in ${DIRLIBS}
 -do
@@ -34,12 +32,13 @@
 -IREPORT_HOME=$(pwd)
 -cd bin
 -#echo $IREPORT_HOME
--
--java -classpath "$IREPORT_CLASSPATH:$CLASSPATH" -Direport.home=$IREPORT_HOME -Djava.security.policy=$IREPORT_HOME/policy.all it.businesslogic.ireport.gui.MainFrame "$@"
++IREPORT_CLASSPATH="%%CLSSPTH%%:%%DATADIR%%/fonts"
++IREPORT_HOME="$HOME/ireport"
+ 
 +# Since iReport-1.2.2, $IREPORT_HOME must be writable
 +/bin/mkdir -p $IREPORT_HOME/lib
 +/bin/rm $IREPORT_HOME/plugins $IREPORT_HOME/templates 2>/dev/null || /usr/bin/true
 +/bin/ln -sf %%DATADIR%%/plugins $IREPORT_HOME/plugins
 +/bin/ln -sf %%DATADIR%%/templates $IREPORT_HOME/templates
 +cd $IREPORT_HOME
-+java -classpath "$IREPORT_CLASSPATH:$CLASSPATH" -Direport.home=$IREPORT_HOME it.businesslogic.ireport.gui.MainFrame "$@"
+ java -classpath "$IREPORT_CLASSPATH:$CLASSPATH" -Direport.home=$IREPORT_HOME -Djava.security.policy=$IREPORT_HOME/policy.all it.businesslogic.ireport.gui.MainFrame "$@"
