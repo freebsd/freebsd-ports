f9ae02780603c5858007f53c8403076bb974aa18
miwi <miwi@FreeBSD.org>
2008-06-03 08:20:59 +0000

    - Update to 3.0a
    PR:		124029
    Submitted by:	Alexander Logvinov <ports@logvinov.com> (maintainer)

--- games/foobillard/files/patch-sys_stuff.c
+++ games/foobillard/files/patch-sys_stuff.c
@@ -1,11 +1,13 @@
---- src/sys_stuff.c.orig	Sat Nov 15 21:24:23 2003
-+++ src/sys_stuff.c	Sat Nov 15 21:25:20 2003
-@@ -5,7 +5,7 @@
+--- src/sys_stuff.c.orig	2008-05-27 21:00:17.000000000 +1000
++++ src/sys_stuff.c	2008-05-27 21:00:25.000000000 +1000
+@@ -7,8 +7,8 @@
  #ifndef USE_SDL
  #include <GL/glut.h>
  #else
 -#include <SDL/SDL.h>
+-#include <SDL/SDL_syswm.h>
 +#include <SDL.h>
++#include <SDL_syswm.h>
  #include <GL/gl.h>
  #include <GL/glu.h>
  #endif
