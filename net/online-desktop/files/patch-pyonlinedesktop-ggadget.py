4e486db9b3b99f8922708e16da7f1f829da6f14a
Martin Wilke <miwi@FreeBSD.org>
2008-06-03 08:16:14 +0000

    The goal of the GNOME Online Desktop is to adapt the desktop to become the perfect window for online applications like GMail, Photobucket, Facebook, EBay, Wikipedia, and countless others that user and developer momentum is shifting towards.
    WWW:	http://live.gnome.org/OnlineDesktop
    
    PR:		ports/124198
    Submitted by:	Ashish Shukla <wahjava at gmail.com>

---
+++ net/online-desktop/files/patch-pyonlinedesktop-ggadget.py
@@ -0,0 +1,8 @@
+--- pyonlinedesktop/ggadget.py~	2008-04-23 23:06:43.000000000 +0530
++++ pyonlinedesktop/ggadget.py	2008-05-28 21:28:47.000000000 +0530
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python
++#!/usr/bin/env python
+ 
+ import os,sys,re,urllib,urllib2,logging,webbrowser,tempfile,shutil
+ import cookielib
