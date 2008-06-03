9a1f2c86fd80c9cd8b06b11408195371e43c588b
Pav Lucistnik <pav@FreeBSD.org>
2008-06-03 22:03:31 +0000

    - Update to 1.5.1
    Submitted by:	adamw
    Approved by:	maintainer (novel)

--- sysutils/conky-awesome/files/patch-src_conky.c
+++ sysutils/conky-awesome/files/patch-src_conky.c
@@ -1,11 +1,11 @@
---- src/conky.c.orig	2008-05-24 12:23:40.000000000 -0400
-+++ src/conky.c	2008-05-24 12:24:07.000000000 -0400
-@@ -5798,7 +5798,7 @@
- 	int added;
+--- src/conky.c.orig	2008-05-27 00:43:05.000000000 -0400
++++ src/conky.c	2008-05-27 00:43:36.000000000 -0400
+@@ -6513,7 +6513,7 @@ static void draw_string(const char *s)
+ 
  	width_of_s = get_string_width(s);
  	if (out_to_console) {
 -		printf("%s\n", s);
 +		printf("%s\n\n", s);
- 		fflush(stdout);   /* output immediately, don't buffer */
+ 		fflush(stdout);	/* output immediately, don't buffer */
  	}
- 	memset(tmpstring1,0,TEXT_BUFFER_SIZE);
+ 	memset(tmpstring1, 0, text_buffer_size);
