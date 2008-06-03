6e64d57700071a212c09649ea08a6b63d8170ec5
Pietro Cerutti <gahr@FreeBSD.org>
2008-06-03 23:09:26 +0000

    - Update to 0.10 - Add support for Pulseaudio soundserver via the OPTIONS framework - Respect WITHOUT_NLS knob - Take maintainship

---
+++ audio/wavbreaker/files/patch-src_toc.c
@@ -0,0 +1,10 @@
+--- src/toc.c.orig	2008-06-04 00:20:25.000000000 +0200
++++ src/toc.c	2008-06-04 00:20:36.000000000 +0200
+@@ -19,6 +19,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
++#include <sys/types.h>
+ 
+ #include "wavbreaker.h"
+ #include "sample.h"
