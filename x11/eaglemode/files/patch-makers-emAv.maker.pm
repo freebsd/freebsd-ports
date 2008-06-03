96d9801526d3d13a5991bf73f030d669ae014e94
Martin Wilke <miwi@FreeBSD.org>
2008-06-03 12:29:18 +0000

    - Update to 0.70.1
    PR:		124216
    Submitted by:	maintainer

--- x11/eaglemode/files/patch-makers-emAv.maker.pm
+++ x11/eaglemode/files/patch-makers-emAv.maker.pm
@@ -1,10 +1,11 @@
---- makers/emAv.maker.pm.orig	2008-04-09 14:53:52.000000000 +0400
-+++ makers/emAv.maker.pm	2008-05-04 20:03:19.000000000 +0400
-@@ -89,7 +89,7 @@
+--- makers/emAv.maker.pm.orig	2008-05-23 08:42:14.000000000 +0400
++++ makers/emAv.maker.pm	2008-06-02 19:16:54.000000000 +0400
+@@ -89,8 +89,7 @@
  		$options{'xine-lib-dir'} ne '' ? (
  			"--lib-search-dir", $options{'xine-lib-dir'}
  		) : (),
 -		"--link"          , "xine",
+-		"--link"          , "pthread",
 +		"--link"          , "xine ".(`xine-config --libs | tr -d '\\n'`),
  		"--type"          , "cexe",
  		"--name"          , "emAvServerProc_xine",
