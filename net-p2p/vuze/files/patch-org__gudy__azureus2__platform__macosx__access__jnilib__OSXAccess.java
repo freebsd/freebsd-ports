1a0091ad932bd207aa189b0a35a46d79e428339f
Martin Wilke <miwi@FreeBSD.org>
2008-06-03 17:47:55 +0000

    - Fix build
    PR:		124217
    Reported by:	Hardy Schumacher <hardy.schumacher@amd.com>
    Patched by:	maintainer

--- net-p2p/vuze/files/patch-org__gudy__azureus2__platform__macosx__access__jnilib__OSXAccess.java
+++ net-p2p/vuze/files/patch-org__gudy__azureus2__platform__macosx__access__jnilib__OSXAccess.java
@@ -1,11 +1,20 @@
---- org/gudy/azureus2/platform/macosx/access/jnilib/OSXAccess.java.orig	2007-03-07 16:49:08.000000000 +0100
-+++ org/gudy/azureus2/platform/macosx/access/jnilib/OSXAccess.java	2008-05-30 16:19:10.000000000 +0200
+--- org/gudy/azureus2/platform/macosx/access/jnilib/OSXAccess.java.orig	2008-06-03 11:20:58.000000000 -0400
++++ org/gudy/azureus2/platform/macosx/access/jnilib/OSXAccess.java	2008-06-03 11:23:02.000000000 -0400
 @@ -19,7 +19,7 @@
   */
  package org.gudy.azureus2.platform.macosx.access.jnilib;
  
 -import org.eclipse.swt.internal.carbon.AEDesc;
-+//import org.eclipse.swt.internal.carbon.AEDesc;
++// import org.eclipse.swt.internal.carbon.AEDesc;
  
  import org.gudy.azureus2.core3.util.*;
+ 
+@@ -42,7 +42,7 @@
+ 		}
+ 	}
+ 
+-	public static final native int AEGetParamDesc(int theAppleEvent, int theAEKeyword, int desiredType, AEDesc result);
++//	public static final native int AEGetParamDesc(int theAppleEvent, int theAEKeyword, int desiredType, AEDesc result);
+ 
+ 	public static final native String getVersion();
  
--- net-p2p/vuze/files/patch-org__gudy__azureus2__platform__unix__ScriptBeforeStartup.java
+++ net-p2p/vuze/files/patch-org__gudy__azureus2__platform__unix__ScriptBeforeStartup.java
@@ -1,5 +1,5 @@
---- org/gudy/azureus2/platform/unix/ScriptBeforeStartup.java.orig	2007-12-06 16:43:38.000000000 +0100
-+++ org/gudy/azureus2/platform/unix/ScriptBeforeStartup.java	2008-05-30 16:21:32.000000000 +0200
+--- org/gudy/azureus2/platform/unix/ScriptBeforeStartup.java.orig	2008-06-03 11:25:03.000000000 -0400
++++ org/gudy/azureus2/platform/unix/ScriptBeforeStartup.java	2008-06-03 11:27:27.000000000 -0400
 @@ -139,6 +139,7 @@
  						return false;
  					}
