fac155206a07933c5ee43952e5b703ba11a6a140
pav <pav@FreeBSD.org>
2008-06-04 12:53:48 +0000

    - Update to 1.5.0.53.38
    PR:		ports/123759
    Submitted by:	Jose Garcia Juanino <jjuanino@gmail.com>

--- databases/sqldeveloper/files/patch-sqldeveloper.sh
+++ databases/sqldeveloper/files/patch-sqldeveloper.sh
@@ -1,6 +1,6 @@
---- sqldeveloper.sh.orig	Tue Jan 15 22:18:13 2008
-+++ sqldeveloper.sh	Tue Jan 15 22:18:30 2008
+--- sqldeveloper.sh.orig	2008-04-21 08:33:48.000000000 +0200
++++ sqldeveloper.sh	2008-05-15 12:02:13.000000000 +0200
 @@ -1,2 +1,2 @@
  #!/bin/bash
 -cd "`dirname $0`"/sqldeveloper/bin && bash sqldeveloper $*
-+cd %DATADIR%/sqldeveloper/bin && sh sqldeveloper $*
++cd %DATADIR%/sqldeveloper/bin && JAVA_HOME=%JAVA_HOME% bash sqldeveloper $*
