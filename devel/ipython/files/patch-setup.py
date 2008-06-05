fcce10809db98ee6dc5b7e8b88e5cab4e41db57a
dryice <dryice@FreeBSD.org>
2008-06-05 12:21:14 +0000

    - update to 0.8.4 - fix plist with NOPORTDOCS set, thanks itetcu@ for pointing out

--- devel/ipython/files/patch-setup.py
+++ devel/ipython/files/patch-setup.py
@@ -1,12 +1,10 @@
---- setup.py.orig	Thu Jan 12 12:45:02 2006
-+++ setup.py	Thu Jan 12 12:45:29 2006
-@@ -99,8 +99,8 @@
- # Note that http://www.redbrick.dcu.ie/~noel/distutils.html, ex. 2/3, contain
+--- setup.py.orig	2008-06-01 06:31:42.000000000 +0800
++++ setup.py	2008-06-05 16:05:08.000000000 +0800
+@@ -107,7 +107,7 @@
  # information on how to do this more cleanly once python 2.4 can be assumed.
  # Thanks to Noel for the tip.
--docdirbase  = 'share/doc/ipython-%s' % version
+ docdirbase  = 'share/doc/ipython'
 -manpagebase = 'share/man/man1'
-+docdirbase  = 'share/doc/ipython'
 +manpagebase = 'man/man1'
  
  # We only need to exclude from this things NOT already excluded in the
