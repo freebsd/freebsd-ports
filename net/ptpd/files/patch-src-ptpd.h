0a66c5e1a5cf654817bcf2f172ff13fd3b762304
Wesley Shields <wxs@FreeBSD.org>
2008-06-05 01:44:43 +0000

    This is a port of ptpd, the Precision Time Protocol, which is an implementation of IEEE 1588.
    http://ptpd.sourceforge.net/
    
    Submitted by:	gnn (private mail/IRC)
    Approved by:	garga (mentor, implicit)

---
+++ net/ptpd/files/patch-src-ptpd.h
@@ -0,0 +1,10 @@
+--- ptpd.h	Tue Mar 27 18:14:20 2007
++++ ptpd.h	Thu Feb 28 14:03:43 2008
+@@ -4,6 +4,7 @@
+ #define PTPD_H
+ 
+ #include "constants.h"
++#include "limits.h"
+ #include "dep/constants_dep.h"
+ #include "dep/datatypes_dep.h"
+ #include "datatypes.h"
