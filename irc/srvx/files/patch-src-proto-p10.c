120b11f528a47b1008a952622a996d1fbf2d6fb1
miwi <miwi@FreeBSD.org>
2008-06-05 23:40:15 +0000

    - Update to 1.4.1
    PR:		124314
    Submitted by:	Ports Fury

--- irc/srvx/files/patch-src-proto-p10.c
+++ irc/srvx/files/patch-src-proto-p10.c
@@ -1,19 +1,19 @@
---- src/proto-p10.c.old	Tue Feb 22 05:11:21 2005
-+++ src/proto-p10.c	Mon Mar 14 22:21:12 2005
-@@ -367,10 +367,10 @@
+--- src/proto-p10.c.orig	2008-05-29 03:46:01.000000000 +0900
++++ src/proto-p10.c	2008-05-29 03:48:41.000000000 +0900
+@@ -369,10 +369,10 @@
      if (srv == self) {
          /* The +s, ignored by Run's ircu, means "service" to Undernet's ircu */
-         putsock(P10_SERVER " %s %d %li %li J10 %s%s +s :%s",
+         putsock(P10_SERVER " %s %d %li %li J10 %s%s +s6 :%s",
 -                srv->name, srv->hops+1, srv->boot, srv->link, srv->numeric, extranum, srv->description);
 +                srv->name, srv->hops+1, (long)srv->boot, (long)srv->link, srv->numeric, extranum, srv->description);
      } else {
-         putsock("%s " P10_SERVER " %s %d %li %li %c10 %s%s +s :%s",
+         putsock("%s " P10_SERVER " %s %d %li %li %c10 %s%s +s6 :%s",
 -                self->numeric, srv->name, srv->hops+1, srv->boot, srv->link, (srv->self_burst ? 'J' : 'P'), srv->numeric, extranum, srv->description);
 +                self->numeric, srv->name, srv->hops+1, (long)srv->boot, (long)srv->link, (srv->self_burst ? 'J' : 'P'), srv->numeric, extranum, srv->description);
      }
  }
  
-@@ -408,10 +408,10 @@
+@@ -485,10 +485,10 @@
  
          /* we don't need to put the + in modes because it's in the format string. */
          putsock("%s " P10_NICK " %s %d %li %s %s +%s %s %s :%s",
@@ -26,7 +26,7 @@
      }
  }
  
-@@ -545,7 +545,7 @@
+@@ -637,7 +637,7 @@
  irc_gline(struct server *srv, struct gline *gline)
  {
      putsock("%s " P10_GLINE " %s +%s %ld :%s",
@@ -35,7 +35,7 @@
  }
  
  void
-@@ -680,9 +680,9 @@
+@@ -772,9 +772,9 @@
  {
      if (what->members.used == 1) {
          putsock("%s " P10_CREATE " %s %lu",
