11a4980acbe5fdd43a115aaf1193aa48a7ca7327
Maxim Sobolev <sobomax@FreeBSD.org>
2008-06-10 08:30:35 +0000

    o Update to 1.4.20.1;
    o add opt-in support for the iLBC codec;
    
    o move another extra patch into opt-in section.

--- net/asterisk/files/patch-channels::chan_sip.c
+++ net/asterisk/files/patch-channels::chan_sip.c
@@ -1,6 +1,6 @@
---- channels/chan_sip.c.orig	2008-03-18 16:42:59.000000000 +0200
-+++ channels/chan_sip.c	2008-03-18 17:08:34.000000000 +0200
-@@ -495,7 +495,7 @@
+--- channels/chan_sip.c.orig	2008-06-10 00:46:46.000000000 -0700
++++ channels/chan_sip.c	2008-06-10 00:46:46.000000000 -0700
+@@ -496,7 +496,7 @@
  #define DEFAULT_MOHINTERPRET    "default"
  #define DEFAULT_MOHSUGGEST      ""
  #define DEFAULT_VMEXTEN 	"asterisk"
@@ -9,46 +9,3 @@
  #define DEFAULT_NOTIFYMIME 	"application/simple-message-summary"
  #define DEFAULT_MWITIME 	10
  #define DEFAULT_ALLOWGUEST	TRUE
-@@ -15873,6 +15881,9 @@
- 	char *ext, *host;
- 	char tmp[256];
- 	char *dest = data;
-+	char *secret = NULL;
-+	char *md5secret = NULL;
-+	char *authname = NULL;
- 
- 	oldformat = format;
- 	if (!(format &= ((AST_FORMAT_MAX_AUDIO << 1) - 1))) {
-@@ -15903,6 +15914,17 @@
- 	if (host) {
- 		*host++ = '\0';
- 		ext = tmp;
-+		secret = strchr(ext, ':');
-+		if (secret) {
-+			*secret++ = '\0';
-+			md5secret = strchr(secret, ':');
-+			if (md5secret) {
-+				*md5secret++ = '\0';
-+				authname = strchr(md5secret, ':');
-+				if (authname)
-+					*authname++ = '\0';
-+			}
-+		}
- 	} else {
- 		ext = strchr(tmp, '/');
- 		if (ext) 
-@@ -15933,6 +15955,14 @@
- 		ast_string_field_set(p, username, ext);
- 		ast_string_field_free(p, fullcontact);
- 	}
-+	if (secret && !ast_strlen_zero(secret))
-+		ast_string_field_set(p, peersecret, secret);
-+
-+	if (md5secret && !ast_strlen_zero(md5secret))
-+		ast_string_field_set(p, peermd5secret, md5secret);
-+
-+	if (authname && !ast_strlen_zero(authname))
-+		ast_string_field_set(p, authname, authname);
- #if 0
- 	printf("Setting up to call extension '%s' at '%s'\n", ext ? ext : "<none>", host);
- #endif
