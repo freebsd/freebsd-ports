56ce4e7447fd09433fefd2113505ffe6a8c612dc
Thierry Thomas <thierry@FreeBSD.org>
2008-06-14 14:49:50 +0000

    Upgrade to Miro 1.2.4.
    Release notes at:
    <https://develop.participatoryculture.org/trac/democracy/wiki/1.2ReleaseNotes>

--- multimedia/miro/files/patch-platform_gtk-x11_setup.py
+++ multimedia/miro/files/patch-platform_gtk-x11_setup.py
@@ -1,6 +1,6 @@
---- platform/gtk-x11/setup.py.orig	2008-01-10 20:55:46.000000000 +0100
-+++ platform/gtk-x11/setup.py	2008-01-11 21:26:05.000000000 +0100
-@@ -46,6 +46,7 @@
+--- platform/gtk-x11/setup.py.orig	2008-05-28 16:05:13.000000000 +0200
++++ platform/gtk-x11/setup.py	2008-06-14 15:40:32.000000000 +0200
+@@ -61,6 +61,7 @@
  ###############################################################################
  
  BOOST_LIB = 'boost_python'
@@ -8,7 +8,7 @@
  
  USE_XINE_HACK = True #use_xine_hack_default()
  
-@@ -200,6 +201,9 @@
+@@ -222,6 +223,9 @@
              options_dict['library_dirs'].append(rest)
          elif prefix == '-l':
              options_dict['libraries'].append(rest)
@@ -18,7 +18,7 @@
          else:
              options_dict['extra_compile_args'].append(comp)
      return options_dict
-@@ -209,6 +213,7 @@
+@@ -231,6 +235,7 @@
      Extension("miro.fasttypes", 
          sources = [os.path.join(portable_dir, 'fasttypes.cpp')],
          libraries = [BOOST_LIB],
@@ -26,11 +26,15 @@
      )
  
  ##### The libtorrent extension ####
-@@ -219,17 +224,17 @@
-     packages = getCommandOutput("pkg-config --list-all")
- except RuntimeError, error:
+@@ -243,21 +248,17 @@
      sys.exit("Package config error:\n%s" % (error,))
--if re.search("^xulrunner-xpcom", packages, re.MULTILINE):
+ 
+ xulrunner19 = False
+-if re.search("^libxul", packages, re.MULTILINE):
+-    xulrunner19 = True
+-    xpcom = 'libxul'
+-    gtkmozembed = 'libxul'
+-elif re.search("^xulrunner-xpcom", packages, re.MULTILINE):
 -    xpcom = 'xulrunner-xpcom'
 -    gtkmozembed = 'xulrunner-gtkmozembed'
 +if re.search("^firefox-xpcom", packages, re.MULTILINE):
@@ -46,21 +50,21 @@
 -    xpcom = 'firefox-xpcom'
 -    gtkmozembed = 'firefox-gtkmozembed'
  else:
--    sys.exit("Can't find xulrunner-xpcom, mozilla-xpcom or firefox-xpcom")
+-    sys.exit("Can't find libxul, xulrunner-xpcom, mozilla-xpcom or firefox-xpcom")
 +    sys.exit("Can't find seamonkey-xpcom, mozilla-xpcom or firefox-xpcom")
  
- # do this so that it doesn't execute when doing python setup.py clean
  if not "clean" in sys.argv:
-@@ -341,7 +346,7 @@
+     # build a miro script that wraps the miro.real script with an LD_LIBRARY_PATH
+@@ -383,7 +384,7 @@
+ # add the desktop file, icons, mime data, and man page.
  
- # do this so that it doesn't execute when doing python setup.py clean
  if not "clean" in sys.argv:
 -    rv = os.system ("gcc %s -o %s `pkg-config --libs --cflags gdk-pixbuf-2.0 glib-2.0 libxine`" % (os.path.join(platform_dir, "xine/xine_extractor.c"), os.path.join(platform_dir, "xine/xine_extractor")))
 +    rv = os.system ("gcc %s -o %s `pkg-config --libs --cflags gdk-pixbuf-2.0 glib-2.0 libxine` %%PTHREAD_LIBS%%" % (os.path.join(platform_dir, "xine/xine_extractor.c"), os.path.join(platform_dir, "xine/xine_extractor")))
  
      if rv != 0:
          raise RuntimeError("xine_extractor compilation failed.  Possibly missing libxine, gdk-pixbuf-2.0, or glib-2.0.")
-@@ -349,11 +354,11 @@
+@@ -391,11 +392,11 @@
  data_files += [
      ('/usr/share/pixmaps', 
       glob(os.path.join(platform_dir, 'miro-*.png'))),
