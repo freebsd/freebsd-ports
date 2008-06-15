4e9eee81e4d0a81ad282d2205d2d78499d248bbc
Li-Wen Hsu <lwhsu@FreeBSD.org>
2008-06-15 00:46:00 +0000

    - Update to version 3.11.0
    PR:		ports/124584
    Submitted by:	KATO Tsuguru <tkato432 AT yahoo.com>

--- devel/drpython/files/patch-setup.py
+++ devel/drpython/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	Sat Jul 23 16:04:31 2005
-+++ setup.py	Sat Jul 23 16:04:40 2005
+--- setup.py.orig	2008-02-25 16:48:04.000000000 +0900
++++ setup.py	2008-06-13 02:15:11.000000000 +0900
 @@ -93,6 +93,5 @@
- 	license = 'GPL',
- 	packages=[ MY_NAME ],
- 	package_dir={ MY_NAME : '.' },
--	package_data={ MY_NAME : DATA },
--	scripts=['postinst.py'],
-+	package_data={ MY_NAME : DATA }
+     license = 'GPL',
+     packages=[ MY_NAME ],
+     package_dir={ MY_NAME : '.' },
+-    package_data={ MY_NAME : DATA },
+-    scripts=['postinst.py'],
++    package_data={ MY_NAME : DATA }
  )
