69e1a0cb61e526ce1df033348e75f7135ec980d5
Thierry Thomas <thierry@FreeBSD.org>
2008-06-15 20:17:03 +0000

    Upgrade to 2.6, with some cleaning.
    PR:		ports/123532
    Submitted by:	Vitaly Magerya <vmagerya (at) gmail.com>

--- graphics/glfw/files/patch-compile.sh
+++ graphics/glfw/files/patch-compile.sh
@@ -1,121 +1,73 @@
---- compile.sh.orig	2004-07-25 00:36:46.000000000 +0400
-+++ compile.sh	2008-03-20 05:20:39.000000000 +0300
-@@ -72,10 +72,10 @@
- ##########################################################################
- if [ "x$force_gcc" = xyes ]; then
-   CC=gcc
--else
--  CC=${CC-cc}
-+#else
-+#  CC=${CC-cc}
+--- compile.sh.orig	2007-07-01 11:46:46.000000000 +0200
++++ compile.sh	2008-06-14 23:22:27.000000000 +0200
+@@ -63,7 +63,7 @@
+   CC=cc
  fi
+ 
 -CFLAGS=
 +#CFLAGS=
  LFLAGS=
+ LDFLAGS=
  INCS=
- LIBS="-lGL -lX11"
-@@ -99,10 +99,10 @@
-  INCS="-I/usr/X11/include"
-  echo " X11 libraries location: /usr/X11/lib" 1>&6
- # X11R6 in /usr/X11R6/lib ?
--elif [ -r "/usr/X11R6/lib" ]; then
-- LFLAGS="$LFLAGS -L/usr/X11R6/lib"
-- INCS="-I/usr/X11R6/include"
-- echo " X11 libraries location: /usr/X11R6/lib" 1>&6
-+elif [ -r "${LOCALBASE}/lib" ]; then
-+ LFLAGS="$LFLAGS -L${LOCALBASE}/lib"
-+ INCS="-I${LOCALBASE}/include"
-+ echo " X11 libraries location: ${LOCALBASE}/lib" 1>&6
- # X11R5 in /usr/X11R5/lib ?
- elif [ -r "/usr/X11R5/lib" ]; then
-  LFLAGS="$LFLAGS -L/usr/X11R5/lib"
-@@ -145,9 +145,9 @@
+@@ -164,10 +164,10 @@
+ fi
  rm -f conftest*
  
- echo " Using GNU C: ""$use_gcc" 1>&6
+-echo " Using GNU C: ""$use_gcc" 1>&6
 -if [ "x$use_gcc" = xyes ]; then
 -  CC=gcc
 -fi
++#echo " Using GNU C: ""$use_gcc" 1>&6
 +#if [ "x$use_gcc" = xyes ]; then
 +#  CC=gcc
 +#fi
  echo " " 1>&6
  
  
-@@ -169,6 +169,8 @@
- int main() {; return 0;}
+@@ -254,11 +254,11 @@
  EOF
  
-+CFLAGS="${CFLAGS} -I${LOCALBASE}/include"
-+
- if { (eval echo $config_script: \"$compile\") 1>&5; (eval $compile) 2>&5; }; then
-   rm -rf conftest*
-   has_xf86vm=yes
-@@ -200,18 +202,10 @@
- 
- # Try -lpthread (most systems)
+ # Try -pthread (most systems)
+-CFLAGS_THREAD="-pthread"
++CFLAGS_THREAD="$PTHREAD_CFLAGS"
+ CFLAGS_OLD="$CFLAGS"
+ CFLAGS="$CFLAGS $CFLAGS_THREAD"
  LIBS_OLD="$LIBS"
--LIBS="$LIBS -lpthread"
--if { (eval echo $config_script: \"$link\") 1>&5; (eval $link) 2>&5; }; then
--  rm -rf conftest*
--  has_pthread=yes
--else
--  echo "$config_script: failed program was:" >&5
--  cat conftest.c >&5
--fi
+-LIBS="$LIBS -pthread"
++LIBS="$LIBS ${PTHREAD_LIBS}"
+ if { (eval echo $config_script: \"$link\") 1>&5; (eval $link) 2>&5; }; then
+   rm -rf conftest*
+   has_pthread=yes
+@@ -267,20 +267,6 @@
+   cat conftest.c >&5
+ fi
  
- # Try -pthread (e.g. FreeBSD)
+-# Try -lpthread 
+-if [ "x$has_pthread" = xno ]; then
+-  CFLAGS_THREAD="-D_REENTRANT"
+-  CFLAGS="$CFLAGS_OLD $CFLAGS_THREAD" 
+-  LIBS="$LIBS_OLD -lpthread"
+-  if { (eval echo $config_script: \"$link\") 1>&5; (eval $link) 2>&5; }; then
+-    rm -rf conftest*
+-    has_pthread=yes
+-  else
+-    echo "$config_script: failed program was:" >&5
+-    cat conftest.c >&5
+-  fi
+-fi
+-
+ # Try -lsocket (e.g. QNX)
  if [ "x$has_pthread" = xno ]; then
--  LIBS="$LIBS_OLD -pthread"
-+  LIBS="$LIBS_OLD ${PTHREAD_LIBS}"
-   if { (eval echo $config_script: \"$link\") 1>&5; (eval $link) 2>&5; }; then
-     rm -rf conftest*
-     has_pthread=yes
-@@ -369,24 +363,24 @@
- echo "$config_script: Checking for sysconf support" >&5
- has_sysconf=no
- 
--cat > conftest.c <<EOF
-+#cat > conftest.c <<EOF
- #include <unistd.h>
- #ifndef _SC_NPROCESSORS_ONLN
- #ifndef _SC_NPROC_ONLN
- #error Neither _SC_NPROCESSORS_ONLN nor _SC_NPROC_ONLN available
- #endif
- #endif
--int main() {long x=sysconf(_SC_ARG_MAX); return 0; }
--EOF
-+#int main() {long x=sysconf(_SC_ARG_MAX); return 0; }
-+#EOF
- 
--if { (eval echo $config_script: \"$link\") 1>&5; (eval $link) 2>&5; }; then
--  rm -rf conftest*
--  has_sysconf=yes
--else
--  echo "$config_script: failed program was:" >&5
--  cat conftest.c >&5
--fi
--rm -f conftest*
-+#if { (eval echo $config_script: \"$link\") 1>&5; (eval $link) 2>&5; }; then
-+#  rm -rf conftest*
-+#  has_sysconf=yes
-+#else
-+#  echo "$config_script: failed program was:" >&5
-+#  cat conftest.c >&5
-+#fi
-+#rm -f conftest*
- 
- echo " sysconf support: ""$has_sysconf" 1>&6
- if [ "x$has_sysconf" = xyes ]; then
-@@ -429,9 +423,9 @@
+   CFLAGS="$CFLAGS_OLD" 
+@@ -534,9 +520,9 @@
  # Post fixups
  ##########################################################################
  if [ "x$use_gcc" = xyes ]; then
 -  CFLAGS_SPEED="-c -I. -I.. $CFLAGS -O3 -ffast-math -Wall"
-+  CFLAGS_SPEED="-c -I. -I.. $CFLAGS -Wall"
++  CFLAGS_SPEED="-c -I. -I.. $CFLAGS -ffast-math -Wall"
    CFLAGS="-c -I. -I.. $CFLAGS -Os -Wall"
 -  CFLAGS_LINK="$INCS -O3 -ffast-math -Wall"
-+  CFLAGS_LINK="$INCS -Wall"
++  CFLAGS_LINK="$INCS -ffast-math -Wall"
  else
    CFLAGS_SPEED="-c -I. -I.. $CFLAGS -O"
    CFLAGS="-c -I. -I.. $CFLAGS -O"
