de252ee2779dfa3edf429aaa22a172aa23cc8601
edwin <edwin@FreeBSD.org>
2008-06-18 11:16:09 +0000

    Change request: comms/cutecom
    	Updates to comms/cutecomm:
    	1. Update source version from 0.14.1 to 0.14.2
    	2. Add #include <sys/params.h> so that __FreeBSD_version is detected correctly.
    	3. Add USB-serial devices /dev/cuaU0 and /dev/cuaU1 to default device list.
    	4. Make default device sensitive to OS version. (cuad for 6 and later)
    
    PR:		ports/122804
    Submitted by:	Jason Bacon <jwbacon@tds.net>
    Approved by:	maintainer timeout

--- comms/cutecom/files/patch-qcppdialogimpl.cpp
+++ comms/cutecom/files/patch-qcppdialogimpl.cpp
@@ -1,14 +1,22 @@
---- qcppdialogimpl.cpp.orig	Thu Jun  9 21:43:44 2005
-+++ qcppdialogimpl.cpp	Thu Jun  9 23:26:47 2005
-@@ -217,11 +217,23 @@
+--- qcppdialogimpl.cpp.orig	2008-03-10 17:53:45.000000000 -0500
++++ qcppdialogimpl.cpp	2008-04-15 17:43:27.000000000 -0500
+@@ -49,6 +49,7 @@
+ #include <sys/time.h>
+ #include <sys/types.h>
+ #include <sys/select.h>
++#include <sys/param.h>
+ #include <fcntl.h>
+ 
+ void millisleep(int ms)
+@@ -234,11 +235,27 @@
     bool entryFound=false;
     QStringList devices=settings.readListEntry("/cutecom/AllDevices", &entryFound);
     if (!entryFound)
 +#ifdef __FreeBSD__
 +#if __FreeBSD_version < 600000
-+      devices<<"/dev/cuaa0"<<"/dev/cuaa1"<<"/dev/cuaa2"<<"/dev/cuaa3";
++      devices<<"/dev/cuaa0"<<"/dev/cuaa1"<<"/dev/cuaa2"<<"/dev/cuaa3"<<"/dev/cuaU0"<<"/dev/cuaU1";
 +#else
-+      devices<<"/dev/cuad0"<<"/dev/cuad1"<<"/dev/cuad2"<<"/dev/cuad3";
++      devices<<"/dev/cuad0"<<"/dev/cuad1"<<"/dev/cuad2"<<"/dev/cuad3"<<"/dev/cuaU0"<<"/dev/cuaU1";
 +#endif
 +#else
        devices<<"/dev/ttyS0"<<"/dev/ttyS1"<<"/dev/ttyS2"<<"/dev/ttyS3";
@@ -17,14 +25,18 @@
     m_deviceCb->insertStringList(devices);
  
 +#ifdef __FreeBSD__
++#if __FreeBSD_version < 600000
 +   m_deviceCb->setCurrentText(settings.readEntry("/cutecom/CurrentDevice", "/dev/cuaa0"));
++#else
++   m_deviceCb->setCurrentText(settings.readEntry("/cutecom/CurrentDevice", "/dev/cuad0"));
++#endif
 +#else
     m_deviceCb->setCurrentText(settings.readEntry("/cutecom/CurrentDevice", "/dev/ttyS0"));
 +#endif
  
     QStringList history=settings.readListEntry("/cutecom/History");
  
-@@ -326,7 +338,7 @@
+@@ -350,7 +367,7 @@
        m_sz->addArgument("sh");
        m_sz->addArgument("-c");
  //      QString tmp=QString("sx -vv \"")+filename+"\" < "+m_deviceCb->currentText()+" > "+m_deviceCb->currentText();
@@ -33,7 +45,7 @@
        if (m_protoPb->currentText()=="XModem")
           tmp+="--xmodem ";
        else if (m_protoPb->currentText()=="YModem")
-@@ -445,7 +457,7 @@
+@@ -472,7 +489,7 @@
  
  void QCPPDialogImpl::sendDone()
  {
@@ -42,7 +54,7 @@
  }
  
  bool QCPPDialogImpl::eventFilter(QObject* watched, QEvent *e)
-@@ -885,15 +897,21 @@
+@@ -915,15 +932,21 @@
     case 230400:
        _baud=B230400;
        break;
