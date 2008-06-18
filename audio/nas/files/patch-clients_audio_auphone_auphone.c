7b1bef4ffa4dd664b0da6a79b82beaf0a6eb3b9d
Edwin Groothuis <edwin@FreeBSD.org>
2008-06-18 08:48:58 +0000

    [PATCH] audio/nas: update to 1.9.1
    	* Update to 1.9.1 which is claimed to fix many race conditions
    	  and nasd segfaults (I experience those quite often with
    	  1.8) (I haven't yet used new nasd for enough time to prove
    	  that)
    	* Use SF macro, USE_XORG (so this port should be ready to
    	  ports/122340) and make this port respect CC/CXX
    	* Adapt all old patches to the new code
    
    PR:		ports/123684
    Submitted by:	Dmitry Marakasov <amdmi3@amdmi3.ru>
    Approved by:	maintainer timeout

--- audio/nas/files/patch-clients_audio_auphone_auphone.c
+++ audio/nas/files/patch-clients_audio_auphone_auphone.c
@@ -1,61 +1,72 @@
---- clients/audio/auphone/auphone.c	Sat Jul 20 21:13:30 2002
-+++ clients/audio/auphone/auphone.c	Mon Oct 10 11:32:46 2005
-@@ -28,4 +28,5 @@
+--- clients/audio/auphone/auphone.c.orig	2007-06-16 23:03:12.000000000 +0400
++++ clients/audio/auphone/auphone.c	2008-05-14 22:44:05.000000000 +0400
+@@ -27,6 +27,7 @@
+  * $NCDId: @(#)auphone.c,v 1.10 1996/05/07 20:13:12 greg Exp $
   */
  
 +#include <inttypes.h>
  #include <stdio.h>
  #include <stdlib.h>
-@@ -218,7 +219,7 @@
+ 
+@@ -219,13 +220,12 @@
+                *ringoutData,
                 *busyData;
  
 -static int
 +static intptr_t
- fatalError(message, arg)
--char           *message,
-+const char     *message,
-                *arg;
+ fatalError(const char *message, const char *arg)
  {
-@@ -226,5 +227,4 @@
+     fprintf(stderr, message, arg);
      fprintf(stderr, "\n");
      exit(1);
 -    return 0;
  }
  
-@@ -240,5 +240,5 @@
+ static void
+@@ -237,7 +237,7 @@
+     {
  	g->bufSize = e->num_bytes;
  
 -	if (!(g->buf = (char *) malloc(g->bufSize)))
 +	if (!(g->buf = malloc(g->bufSize)))
- 	    fatalError("malloc failed");
+ 	    fatalError("malloc failed", NULL);
      }
-@@ -252,6 +252,6 @@
+     else
+@@ -249,8 +249,8 @@
+ 
  	g->bufSize += e->num_bytes;
  
 -	if (!(g->buf = (char *) realloc(g->buf, g->bufSize)))
--	    fatalError("malloc failed");
+-	    fatalError("malloc failed", NULL);
 +	if (!(g->buf = realloc(g->buf, g->bufSize)))
-+	    fatalError("realloc failed");
++	    fatalError("remalloc failed", NULL);
      }
  
-@@ -918,5 +918,5 @@
+     n = AuReadElement(g->local.aud, e->flow, e->element_num, e->num_bytes,
+@@ -871,7 +871,7 @@
+ adjustVolume(Widget w, XtPointer gp, XtPointer volp)
  {
      GlobalDataPtr   g = (GlobalDataPtr)gp;
 -    int             vol = (int)volp;
 +    intptr_t        vol = (intptr_t)volp;
      AuElementParameters parms;
  
-@@ -940,5 +940,5 @@
+     if (!g->local.volFlow)
+@@ -890,7 +890,7 @@
+ adjustInputGain(Widget w, XtPointer gp, XtPointer gainp)
  {
      GlobalDataPtr   g = (GlobalDataPtr)gp;
 -    int             gain = (int)gainp;
 +    intptr_t        gain = (intptr_t)gainp;
      ServerPtr       l = &g->local;
      AuDeviceAttributes da;
-@@ -1129,5 +1129,5 @@
+ 
+@@ -1065,7 +1065,7 @@
+ 	if (!(s = SoundOpenFileForReading(ring)))
  	    fatalError("Can't open ring %s", ring);
  
 -	if (!(chunk = (char *) malloc(CHUNK_SIZE)))
 +	if (!(chunk = malloc(CHUNK_SIZE)))
- 	    fatalError("malloc failed");
+ 	    fatalError("malloc failed", NULL);
  
+ 	sprintf(buf, "%s %d", RINGIN, volume);
--- audio/nas/files/patch-server_dda_voxware_config.c
+++ audio/nas/files/patch-server_dda_voxware_config.c
@@ -1,97 +1,133 @@
---- server/dda/voxware/config.c	Fri Jul 12 00:44:16 2002
-+++ server/dda/voxware/config.c	Mon Oct 10 10:25:22 2005
-@@ -6,4 +6,5 @@
+--- server/dda/voxware/config.c.orig	2007-06-16 23:03:22.000000000 +0400
++++ server/dda/voxware/config.c	2008-05-14 22:49:23.000000000 +0400
+@@ -5,6 +5,7 @@
+  */
  
  #include <fcntl.h>
 +#include <inttypes.h>
  #include "nasconf.h"
  #include "config.h"
-@@ -27,5 +28,5 @@
-     {
+ #include "aulog.h"
+@@ -27,7 +28,7 @@
+ 
+     switch (token) {
      case CONF_SET_SECTION:
--      num = (int) value;
-+      num = (intptr_t) value;
+-        num = (int) value;
++        num = (intptr_t) value;
  
-       if (num == INPUTSECTION)
-@@ -40,10 +41,10 @@
+         if (num == INPUTSECTION) {      /* we're in the input section */
+             confStat = &sndStatIn;
+@@ -37,12 +38,12 @@
+         break;
  
-     case FORCERATE :
--      num = (int) value;
-+      num = (intptr_t) value;
-       confStat->forceRate = num ;
-       break;
+     case FORCERATE:
+-        num = (int) value;
++        num = (intptr_t) value;
+         confStat->forceRate = num;
+         break;
  
-     case GAIN :
--      num = (int) value;
-+      num = (intptr_t) value;
-       /* the default is 50, so if it's just out of range, don't
- 	 reset it */
-@@ -56,10 +57,10 @@
+     case GAINSCALE:
+-        num = (int) value;
++        num = (intptr_t) value;
+         if (num < 0 || num > 100)
+             osLogMsg("config: gain scaling must be within the range 0-100\n");
+         else
+@@ -50,7 +51,7 @@
+         break;
  
-     case AUTOOPEN :
--      num = (int) value;
-+      num = (intptr_t) value;
-       confStat->autoOpen = num;
-       break;
+     case GAIN:
+-        num = (int) value;
++        num = (intptr_t) value;
+         /* the default is 50, so if it's just out of range, don't
+            reset it */
+         if (num < 0 || num > 100)
+@@ -61,12 +62,12 @@
+         break;
  
-     case READWRITE :
--      num = (int) value;
-+      num = (intptr_t) value;
-       if (confStat == &sndStatIn) {
- 	confStat->howToOpen = (num ? O_RDWR : O_RDONLY);
-@@ -85,5 +86,5 @@
+     case AUTOOPEN:
+-        num = (int) value;
++        num = (intptr_t) value;
+         confStat->autoOpen = num;
+         break;
+ 
+     case READWRITE:
+-        num = (int) value;
++        num = (intptr_t) value;
+         if (confStat == &sndStatIn) {
+             confStat->howToOpen = (num ? O_RDWR : O_RDONLY);
+         } else {
+@@ -89,7 +90,7 @@
+         break;
  
      case WORDSIZE:
--      num = (int) value;
-+      num = (intptr_t) value;
+-        num = (int) value;
++        num = (intptr_t) value;
  
-       if (num != 8 && num != 16) 
-@@ -97,5 +98,5 @@
+         if (num != 8 && num != 16) {
+             osLogMsg("config: Wordsize (%d) not 8 or 16, setting to 8\n",
+@@ -100,7 +101,7 @@
+         break;
  
      case FRAGSIZE:
--      num = (int) value;
-+      num = (intptr_t) value;
+-        num = (int) value;
++        num = (intptr_t) value;
  
-       {
-@@ -122,5 +123,5 @@
+         {
+             int i, j, k;
+@@ -125,7 +126,7 @@
+         break;
  
      case MINFRAGS:
--      num = (int) value;
-+      num = (intptr_t) value;
+-        num = (int) value;
++        num = (intptr_t) value;
  
-       if (num < 2 || num > 32) 
-@@ -137,5 +138,5 @@
+         if (num < 2 || num > 32) {
+             osLogMsg("config: Minfrags out of range - setting to 2\n");
+@@ -138,7 +139,7 @@
+         break;
  
      case MAXFRAGS:
--      num = (int) value;
-+      num = (intptr_t) value;
+-        num = (int) value;
++        num = (intptr_t) value;
  
-       if (num < 2 || num > 32) 
-@@ -152,5 +153,5 @@
+         if (num < 2 || num > 32) {
+             osLogMsg("config: Maxfrags out of range - setting to 32\n");
+@@ -151,7 +152,7 @@
+         break;
  
      case NUMCHANS:
--      num = (int) value;
-+      num = (intptr_t) value;
+-        num = (int) value;
++        num = (intptr_t) value;
  
-       if (num != 1 && num != 2) 
-@@ -164,5 +165,5 @@
+         if (num != 1 && num != 2) {
+             osLogMsg("config: Number of channels wrong, setting to 1\n");
+@@ -161,25 +162,25 @@
+         break;
  
      case MAXRATE:
--      num = (int) value;
-+      num = (intptr_t) value;
+-        num = (int) value;
++        num = (intptr_t) value;
  
-       confStat->maxSampleRate = num;
-@@ -170,5 +171,5 @@
+         confStat->maxSampleRate = num;
+         break;
  
      case MINRATE:
--      num = (int) value;
-+      num = (intptr_t) value;
+-        num = (int) value;
++        num = (intptr_t) value;
  
-       confStat->minSampleRate = num;
-@@ -176,5 +177,5 @@
+         confStat->minSampleRate = num;
+         break;
  
      case MIXERINIT:
--      num = (int) value;
-+      num = (intptr_t) value;
+-        num = (int) value;
++        num = (intptr_t) value;
  
-       VOXMixerInit = num;
+         VOXMixerInit = num;
+         break;
+ 
+     case REINITMIXER:
+-        num = (int) value;
++        num = (intptr_t) value;
+ 
+         VOXReInitMixer = num;
+         break;
