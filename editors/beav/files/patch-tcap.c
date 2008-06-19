556374bb28977fd2ef0b900bdee0887a7bd45bc1
Ed Schouten <ed@FreeBSD.org>
2008-06-19 08:07:27 +0000

    Make editors/beav use termios instead of sgtty.
    The editors/beav port got broken on -CURRENT by the removal of the sgtty
    programming interface. Patch this port to use termios. The source code
    already contained support for termios, but was mistakenly covered by
    #ifdef __GLIBC__.
    
    Approved by:	philip (mentor)

--- editors/beav/files/patch-tcap.c
+++ editors/beav/files/patch-tcap.c
@@ -1,11 +1,11 @@
---- tcap.c.orig	Sat Dec 13 17:34:59 1997
-+++ tcap.c	Thu Mar 21 03:53:02 2002
-@@ -39,7 +39,7 @@
+--- tcap.c
++++ tcap.c
+@@ -89,7 +89,7 @@
+ 
  
  #ifdef BSD
- #include <sys/ioctl.h>
--struct winsize ttysize;
-+struct ttysize ttysize;
- #endif /* BSD */
- #ifdef ULTRIX
- struct winsize ttysize;
+-#ifdef ULTRIX
++#if 1
+     if (ioctl (0, TIOCGWINSZ, &ttysize) == 0
+ 	&& ttysize.ws_row > 0)
+     {
