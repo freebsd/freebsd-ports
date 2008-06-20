2470f8d6e1d5f87386a4f18a88d54da15658892a
miwi <miwi@FreeBSD.org>
2008-06-20 14:36:28 +0000

    - Update to 0.11.0
    PR:		124759
    Submitted by:	Pavel I Volkov <pol@opk.ru> (maintainer)

--- emulators/dynagen/files/patch-dynamips_lib.py
+++ emulators/dynagen/files/patch-dynamips_lib.py
@@ -1,15 +1,8 @@
---- dynamips_lib.py.orig	Sun Feb 18 17:28:09 2007
-+++ dynamips_lib.py	Mon Feb 26 21:05:00 2007
-@@ -1411,7 +1411,11 @@
-             flag = '1'
-         else:
-             flag = '0'
--        send(self.__d, 'vm set_sparse_mem %s %s' % (self.__name, flag))
-+
-+	# Workaround for "*** Error: Unknown command 'set_sparse_mem'" message
-+	# with dynamips-0.2.5.
-+	if flag == 1:
-+        	send(self.__d, 'vm set_sparse_mem %s %s' % (self.__name, flag))
+--- dynamips_lib.py.orig	2008-06-11 14:52:21.000000000 +0400
++++ dynamips_lib.py	2008-06-11 14:52:36.000000000 +0400
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python
++#!/usr/local/bin/python
+ # -*- coding: utf-8 -*-
  
-     def __getsparsemem(self):
-         """ Returns the sparsemem
+ """
