d4915061129169ae584584e61add7e3721eb99cb
alepulver <alepulver@FreeBSD.org>
2008-06-24 02:01:39 +0000

    - Update to version 0.13.
    PR:		ports/124858
    Approved by:	johan@stromnet.se (maintainer)

--- net-p2p/rblibtorrent/files/patch-socket_ops.hpp
+++ net-p2p/rblibtorrent/files/patch-socket_ops.hpp
@@ -1,10 +1,11 @@
---- include/libtorrent/asio/detail/socket_ops.hpp      Wed May 16 00:35:55 2007
-+++ include/libtorrent/asio/detail/socket_ops.hpp.orig Wed May 16 00:35:45 2007
-66c66
-< #if defined(__MACH__) && defined(__APPLE__)
----
-> #if defined(__MACH__) && defined(__APPLE__) || defined(__FreeBSD__)
-298c298
-< #elif defined(__MACH__) && defined(__APPLE__)
----
-> #elif defined(__MACH__) && defined(__APPLE__) || defined(__FreeBSD__) 
+--- include/libtorrent/asio/detail/socket_ops.hpp.orig	Wed Aug  8 03:43:42 2007
++++ include/libtorrent/asio/detail/socket_ops.hpp	Mon Sep 10 12:46:17 2007
+@@ -715,7 +715,7 @@
+   if (error)
+     ec = translate_netdb_error(error);
+   return retval;
+-#elif defined(__MACH__) && defined(__APPLE__)
++#elif defined(__MACH__) && defined(__APPLE__) || defined(__FreeBSD__) 
+   (void)(buffer);
+   (void)(buflength);
+   int error = 0;
