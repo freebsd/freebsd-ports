f538b91d4aa96eab0eb184ad5ab61735d4dfff0d
Dirk Meyer <dinoex@FreeBSD.org>
2008-06-24 19:13:01 +0000

    - fix build for gnstep-base 1.15

---
+++ deskutils/helpviewer/files/patch-mainWindowController.m
@@ -0,0 +1,10 @@
+--- mainWindowController.m.orig	2003-04-05 08:47:02.000000000 +0200
++++ mainWindowController.m	2008-06-16 20:36:29.000000000 +0200
+@@ -17,6 +17,7 @@
+     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+ 
++#include "GNUstepBase/GNUstep.h"
+ #include "mainWindowController.h"
+ 
+ @implementation MainWindowController
