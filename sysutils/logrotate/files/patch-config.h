3d9eefbfc155d6faeb0767e7c8718984fb11c204
jadawin <jadawin@FreeBSD.org>
2008-06-24 20:19:15 +0000

    - Update to 3.7.7 - Update MASTER_SITE - Remove some patches
    PR:		ports/124889
    Submitted by:	Balazs Nagy <js iksz.hu> (maintainer)
    Approved by:	mentor (implicit)

--- sysutils/logrotate/files/patch-config.h
+++ sysutils/logrotate/files/patch-config.h
@@ -1,15 +1,15 @@
-diff -ruN logrotate-3.7-orig/config.h logrotate-3.7/config.h
---- logrotate-3.7-orig/config.h	Mon Sep 22 21:11:12 2003
-+++ logrotate-3.7.1/config.h	Mon May 24 08:05:29 2004
+diff -ruN logrotate-3.7.7-orig/config.h logrotate-3.7.7/config.h
+--- logrotate-3.7.7-orig/config.h	Sat Nov 12 19:07:56 2005
++++ logrotate-3.7.7/config.h	Sun Jun 22 22:02:29 2008
 @@ -16,6 +16,13 @@
-     #define STATEFILE "/var/log/logrotate.status"
+ #define STATEFILE "/var/log/logrotate.status"
  #endif
  
 +#ifdef __FreeBSD__
 +    #define DEFAULT_MAIL_COMMAND "/usr/bin/mailx -s"
 +    #define COMPRESS_COMMAND "/usr/bin/gzip"
 +    #define UNCOMPRESS_COMMAND "/usr/bin/gunzip"
-+    #define STATEFILE "/var/log/logrotate.status"
++    #define STATEFILE "/var/run/logrotate.status"
 +#endif
 +
  /*
