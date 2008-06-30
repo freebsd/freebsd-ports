c10e57835c9dff22202a09a52e735925ef91e20d
Beech Rintoul <beech@FreeBSD.org>
2008-06-30 01:27:54 +0000

    - Update to 1.2 (release) - Bump PORTEPOCH

--- mail/horde4-ingo/files/patch-lib_Script_procmail.php
+++ mail/horde4-ingo/files/patch-lib_Script_procmail.php
@@ -1,21 +1,12 @@
---- lib/Script/procmail.php.orig	2007-08-21 17:51:56.000000000 -0800
-+++ lib/Script/procmail.php	2007-12-04 00:37:57.000000000 -0900
-@@ -434,9 +434,15 @@
+--- lib/Script/procmail.php.orig	2008-05-05 09:22:21.000000000 -0800
++++ lib/Script/procmail.php	2008-06-29 16:56:12.000000000 -0800
+@@ -470,9 +470,6 @@
              $this->_action[] = '{';
              foreach ($params['action-value']['addresses'] as $address) {
                  if (!empty($address)) {
--                    $this->_action[] = '  FILEDATE=`test -f \'.vacation.' . $address . '\' && '
--                        . 'ls -lcn --time-style=+%s \'.vacation.' . $address . '\' | '
+-                    $this->_action[] = '  FILEDATE=`test -f ${VACATION_DIR:-.}/\'.vacation.' . $address . '\' && '
+-                        . $this->_params['ls'] . ' -lcn --time-style=+%s ${VACATION_DIR:-.}/\'.vacation.' . $address . '\' | '
 -                        . 'awk \'{ print $6 + (' . $days * 86400 . ') }\'`';
-+/***** This bit does not work with FreeBSD
-+*                    $this->_action[] = '  FILEDATE=`test -f \'.vacation.' . $address . '\' && '
-+*                        . 'ls -lcn --time-style=+%s \'.vacation.' . $address . '\' | '
-+*                        . 'awk \'{ print $6 + (' . $days * 86400 . ') }\'`';
-+*/
-+                    $this->_action[] = '  FDATE=`ls -lcnT \'.vacation.' . $address
-+                                       . '\' 2> /dev/null | awk \'{ print $6, $7, $8, $9; }\'`';
-+                    $this->_action[] = '  FILEDATE=`date -j -f "%b %d %H:%M:%S %Y" "$FDATE" +%s '
-+                                       . ' | awk \'{ print $1 + (' . $days * 86400 . ') }\'`';
-                     $this->_action[] = '  DATE=`date +%s`';
-                     $this->_action[] = '  DUMMY=`test -f \'.vacation.' . $address . '\' && '
+                     $this->_action[] = '  DATE=`' . $this->_params['date'] . ' +%s`';
+                     $this->_action[] = '  DUMMY=`test -f ${VACATION_DIR:-.}/\'.vacation.' . $address . '\' && '
                          . 'test $FILEDATE -le $DATE && '
