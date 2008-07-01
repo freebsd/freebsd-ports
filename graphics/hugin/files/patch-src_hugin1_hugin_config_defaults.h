e685bc621184362b5b3b4e24c730b69fa0d64629
vd <vd@FreeBSD.org>
2008-07-01 14:47:01 +0000

    Upgrade graphics/hugin from 0.7_beta4 to 0.7.r3135 (SVN revision 3135).
    This version is not archived by the authors, so this is a
    snapshot of the SVN repository as of revision 3135.
    
    The archive is put on the freebsd.org cluster.

--- graphics/hugin/files/patch-src_hugin1_hugin_config_defaults.h
+++ graphics/hugin/files/patch-src_hugin1_hugin_config_defaults.h
@@ -1,11 +1,11 @@
---- src/include/hugin/config_defaults.h.orig	2008-06-15 18:26:33.000000000 +0300
-+++ src/include/hugin/config_defaults.h	2008-06-15 18:27:33.000000000 +0300
-@@ -150,8 +150,8 @@
+--- src/hugin1/hugin/config_defaults.h.orig	2008-06-20 17:21:28.000000000 +0300
++++ src/hugin1/hugin/config_defaults.h	2008-06-20 17:22:40.000000000 +0300
+@@ -160,8 +160,8 @@
  #define HUGIN_APKOLOR_EXE                     ""
  #define HUGIN_APKOLOR_ARGS                    ""
  
--#define HUGIN_APSIFT_EXE                      "autopano-complete.sh"
--#define HUGIN_APSIFT_ARGS                     "--output %o --points %p %i"
+-#define HUGIN_APSIFT_EXE                      "autopano-sift-c"
+-#define HUGIN_APSIFT_ARGS                     "--maxmatches %p %o %i"
 +#define HUGIN_APSIFT_EXE                      "autopano-complete"
 +#define HUGIN_APSIFT_ARGS                     "-o %o -p %p %i"
  #define HUGIN_STITCHER_RUN_EDITOR             0l
