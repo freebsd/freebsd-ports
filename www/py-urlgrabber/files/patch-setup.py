0a211c575dc25c766b57a4493946e2d83fdbdc43
nivit <nivit@FreeBSD.org>
2008-07-03 15:47:27 +0000

    - Fix patch target when NOPORTDOCS is set (issue reported by "QA Tindy" via itetcu - Use ${PYTHON_PKGNAMEPREFIX} inside DOCSDIR (so bump PORTREVISION) - Use os.environ.has_key() instead of os.getenv() inside setup.py

--- www/py-urlgrabber/files/patch-setup.py
+++ www/py-urlgrabber/files/patch-setup.py
@@ -1,18 +1,26 @@
 --- setup.py	2005-03-08 23:53:20.000000000 +0100
-+++ setup.py.port	2007-09-22 17:12:58.000000000 +0200
-@@ -1,4 +1,5 @@
++++ setup.py.port	2008-07-03 17:30:05.000000000 +0200
+@@ -1,3 +1,4 @@
++
  # urlgrabber distutils setup
-+import os
  import re as _re
  import urlgrabber as _urlgrabber
- 
-@@ -15,7 +16,8 @@
+@@ -15,8 +16,6 @@
  packages = ['urlgrabber']
  package_dir = {'urlgrabber':'urlgrabber'}
  scripts = ['scripts/urlgrabber']
 -data_files = [('share/doc/' + name + '-' + version,
-+if not os.getenv('NOPORTDOCS'):
-+    data_files = [('%%DOCSDIR%%',
-                ['README','LICENSE', 'TODO', 'ChangeLog'])]
+-               ['README','LICENSE', 'TODO', 'ChangeLog'])]
  options = { 'clean' : { 'all' : 1 } }
  classifiers = [
+         'Development Status :: 4 - Beta',
+@@ -41,5 +40,9 @@
+     #print '%-20s -> %s' % (k, config[k])
+     if k.startswith('_'): del config[k]
+ 
++  import os
++  if not os.environ.has_key('NOPORTDOCS'):
++    config.setdefault('data_files', [('%%DOCSDIR%%', ['README','LICENSE', 'TODO', 'ChangeLog'])])
++
+   from distutils.core import setup
+   setup(**config)
