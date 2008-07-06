7066ba6c210fc2fe2c58e5c1eaefb5ccd970f5a5
marcus <marcus@FreeBSD.org>
2008-07-06 19:48:31 +0000

    Update to 1.4.0.

--- net-im/loudmouth/files/patch-loudmouth_lm-sock.c
+++ net-im/loudmouth/files/patch-loudmouth_lm-sock.c
@@ -1,14 +1,13 @@
---- loudmouth/lm-sock.c.orig	Mon Apr 16 23:37:43 2007
-+++ loudmouth/lm-sock.c	Mon Apr 16 23:38:08 2007
-@@ -273,9 +273,11 @@
- 	case EAI_FAIL:     
- 		return _("The nameserver encountered errors "
- 			 "looking up this address");
-+#ifdef EAI_NODATA
- 	case EAI_NODATA:   
- 		return _("The remote host exists but no address "
- 			 "is available");
-+#endif
- 	case EAI_NONAME:   
- 		return _("The remote address is unknown");
- 	case EAI_FAMILY:
+--- loudmouth/lm-sock.c.orig	2008-07-06 15:41:02.000000000 -0400
++++ loudmouth/lm-sock.c	2008-07-06 15:43:01.000000000 -0400
+@@ -27,8 +27,10 @@
+ #include <errno.h>
+ #include <string.h>
+ #include <unistd.h>
++#include <sys/types.h>
+ #include <sys/socket.h>
+ #include <fcntl.h>
++#include <netinet/in_systm.h>
+ #include <netinet/in.h>
+ #include <netinet/ip.h>
+ #include <netinet/tcp.h>
