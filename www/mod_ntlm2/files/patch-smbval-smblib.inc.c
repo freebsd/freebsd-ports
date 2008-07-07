de324ac57e8bdf4c42ef54dd0782e4383c4a3520
Martin Wilke <miwi@FreeBSD.org>
2008-07-07 18:40:03 +0000

    - Unbreak - Fix build with gcc 4.2
    PR:		125207
    Submitted by:	Alex Keda <admin@lissyara.su> (maintainer)

--- www/mod_ntlm2/files/patch-smbval-smblib.inc.c
+++ www/mod_ntlm2/files/patch-smbval-smblib.inc.c
@@ -1,11 +1,25 @@
---- smbval/smblib.inc.c.orig	Wed Sep 26 12:42:22 2007
-+++ smbval/smblib.inc.c	Wed Sep 26 12:43:08 2007
-@@ -20,7 +20,7 @@
+--- smbval/smblib.inc.c.orig	2008-07-03 01:44:33.000000000 +0400
++++ smbval/smblib.inc.c	2008-07-03 01:45:18.000000000 +0400
+@@ -20,10 +20,10 @@
   * with this program; if not, write to the Free Software Foundation, Inc., 
   * 675 Mass Ave, Cambridge, MA 02139, USA. */
  #include <stdio.h>
 -#include <malloc.h>
 +#include <stdlib.h>
  
- static int SMBlib_errno;
- static int SMBlib_SMB_Error;
+-static int SMBlib_errno;
+-static int SMBlib_SMB_Error;
++int SMBlib_errno;
++int SMBlib_SMB_Error;
+ #define SMBLIB_ERRNO
+ #define uchar unsigned char
+ #include "smblib-priv.h"
+@@ -32,7 +32,7 @@
+ 
+ #include <signal.h>
+ 
+-static SMB_State_Types SMBlib_State;
++SMB_State_Types SMBlib_State;
+ 
+ /* Initialize the SMBlib package     */
+ static int 
