89609f6483fb807e09b8cb53495db62693daa419
Martin Wilke <miwi@FreeBSD.org>
2008-07-07 17:50:08 +0000

    - Update to 0.4.0
    PR:		125347
    Submitted by:	Daniel Bretoi <daniel@netwalk.org> (maintainer)

--- news/sabnzbdplus/files/patch-SABnzbd.py
+++ news/sabnzbdplus/files/patch-SABnzbd.py
@@ -1,11 +1,35 @@
---- SABnzbd.py.bak	2008-01-05 11:52:46.000000000 -0800
-+++ SABnzbd.py	2008-01-05 11:57:12.000000000 -0800
-@@ -492,7 +492,7 @@
-         web_dir = DEF_STDINTF
-     cfg['misc']['web_dir'] = web_dir
+--- SABnzbd.py	2008-07-04 12:31:58.000000000 -0700
++++ SABnzbd.py.bak	2008-07-06 12:33:06.000000000 -0700
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python -OO
++#!/usr/local/bin/python2.5 -OO
+ # Copyright 2008 The SABnzbd-Team <team@sabnzbd.org>
+ #
+ # This program is free software; you can redistribute it and/or
+@@ -164,7 +164,7 @@
+         print "fork() failed"
+         sys.exit(1)
  
+-    os.chdir(sabnzbd.DIR_PROG)
++    os.chdir(%%PREFIX%%)
+     os.setsid()
+     # Make sure I can read my own files and shut out others
+     prev= os.umask(0)
+@@ -301,7 +301,7 @@
+     sabnzbd.MY_FULLNAME = os.path.normpath(os.path.abspath(sys.argv[0]))
+     sabnzbd.MY_NAME = os.path.basename(sabnzbd.MY_FULLNAME)
+     sabnzbd.DIR_PROG = os.path.dirname(sabnzbd.MY_FULLNAME)
 -    sabnzbd.DIR_INTERFACES = real_path(sabnzbd.DIR_PROG, DEF_INTERFACES)
 +    sabnzbd.DIR_INTERFACES = real_path(%%PREFIX%%, DEF_INTERFACES)
-     web_dir = real_path(sabnzbd.DIR_INTERFACES, web_dir)
-     web_main = real_path(web_dir, DEF_MAIN_TMPL)
-     logging.info("Web dir is %s", web_dir)
+ 
+     # Need console logging for SABnzbd.py and SABnzbd-console.exe
+     consoleLogging = (not hasattr(sys, "frozen")) or (sabnzbd.MY_NAME.lower().find('-console') > 0)
+@@ -684,7 +684,7 @@
+ 
+     log_dir = dir_setup(cfg, 'log_dir', sabnzbd.DIR_LCLDATA, DEF_LOG_DIR)
+ 
+-    os.chdir(sabnzbd.DIR_PROG)
++    os.chdir(%%PREFIX%%)
+ 
+     web_dir  = Web_Template('web_dir',  DEF_STDINTF,  web_dir)
+     web_dir2 = Web_Template('web_dir2', '', web_dir2)
