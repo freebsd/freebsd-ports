79410fae2ca48616681bef2ab1287121966e6ee5
mezz <mezz@FreeBSD.org>
2008-07-08 20:31:10 +0000

    Update to 2.0.1.

--- textproc/exempi/files/patch-source_XMPFiles_FormatSupport_EndianUtils.hpp
+++ textproc/exempi/files/patch-source_XMPFiles_FormatSupport_EndianUtils.hpp
@@ -1,11 +1,11 @@
---- source/XMPFiles/FormatSupport/EndianUtils.hpp.orig	2008-02-26 11:51:10.000000000 -0500
-+++ source/XMPFiles/FormatSupport/EndianUtils.hpp	2008-02-26 11:51:17.000000000 -0500
-@@ -38,7 +38,7 @@
- 		#endif
- 	#endif
- #elif XMP_UNIXBuild
--#include <endian.h>
-+#include <machine/endian.h>
- 	#if BYTE_ORDER == BIG_ENDIAN
- 		#define kBigEndianHost 1
- 	#elif BYTE_ORDER == LITTLE_ENDIAN
+--- source/XMPFiles/FormatSupport/EndianUtils.hpp.orig	2008-07-08 15:22:01.000000000 -0500
++++ source/XMPFiles/FormatSupport/EndianUtils.hpp	2008-07-08 15:22:35.000000000 -0500
+@@ -57,7 +57,7 @@
+ #    define kBigEndianHost 1
+ #  endif
+ # else
+-#  include <endian.h>
++#  include <machine/endian.h>
+ #  if BYTE_ORDER == BIG_ENDIAN
+ #   define kBigEndianHost 1
+ #  elif BYTE_ORDER == LITTLE_ENDIAN
