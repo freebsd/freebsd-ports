54b840b37a44ced448a8ad0f47740ce08278f643
Juergen Lock <nox@FreeBSD.org>
2008-07-08 18:42:09 +0000

    - Update to 20080620 svn snapshot, which uses the new kqemu-kmod-devel   port that now also works for the 32 bit "qemu" executable on amd64   (if you build with the KQEMU knob on, you have to deinstall the old   kqemu-kmod first if it is installed) - Add an ALL_TARGETS knob that, if turned off, omits the remaining dyngen   targets, eliminating the need for gcc 3.4 (everything but ppc and sh4   has been converted to tcg in this snapshot) - Add tcg fixes for amd64 guests on i386 hosts (two of three have been   committed to qemu svn in the meantime) [1] - Update the pkg-message about kqemu on amd64, and add a note about using   nfs with slirp, the latter [2] - Homepage now at http://bellard.org/qemu/ - update links
    Submitted by:	nox [1]
    Submitted by:	joerg [2]

--- emulators/qemu-devel/files/patch-cpu-exec.c
+++ emulators/qemu-devel/files/patch-cpu-exec.c
@@ -1,29 +1,27 @@
---- qemu.orig/cpu-exec.c	Mon Jan 14 11:11:02 2008
-+++ qemu/cpu-exec.c	Thu Jan 17 23:03:00 2008
-@@ -449,16 +449,18 @@ int cpu_exec(CPUState *env1)
-                         (env->eflags & IF_MASK || env->hflags & HF_HIF_MASK) &&
-                         !(env->hflags & HF_INHIBIT_IRQ_MASK)) {
-                         int intno;
--                        svm_check_intercept(SVM_EXIT_INTR);
-                         env->interrupt_request &= ~(CPU_INTERRUPT_HARD | CPU_INTERRUPT_VIRQ);
-                         intno = cpu_get_pic_interrupt(env);
--                        if (loglevel & CPU_LOG_TB_IN_ASM) {
--                            fprintf(logfile, "Servicing hardware INT=0x%02x\n", intno);
--                        }
--                        do_interrupt(intno, 0, 0, 0, 1);
--                        /* ensure that no TB jump will be modified as
--                           the program flow was changed */
--                        BREAK_CHAIN;
-+	                     if(intno>=0) {
-+                            svm_check_intercept(SVM_EXIT_INTR);
-+                            if (loglevel & CPU_LOG_TB_IN_ASM) {
-+                                fprintf(logfile, "Servicing hardware INT=0x%02x\n", intno);
-+                            }
-+                            do_interrupt(intno, 0, 0, 0, 1);
-+                           /* ensure that no TB jump will be modified as
-+                              the program flow was changed */
-+                            BREAK_CHAIN;
-+                        }
+Index: qemu/cpu-exec.c
+@@ -394,16 +394,18 @@
+                                      (env->eflags & IF_MASK && 
+                                       !(env->hflags & HF_INHIBIT_IRQ_MASK))))) {
+                             int intno;
+-                            svm_check_intercept(SVM_EXIT_INTR);
+                             env->interrupt_request &= ~(CPU_INTERRUPT_HARD | CPU_INTERRUPT_VIRQ);
+                             intno = cpu_get_pic_interrupt(env);
+-                            if (loglevel & CPU_LOG_TB_IN_ASM) {
+-                                fprintf(logfile, "Servicing hardware INT=0x%02x\n", intno);
++                            if (intno>=0) {
++                                svm_check_intercept(SVM_EXIT_INTR);
++                                if (loglevel & CPU_LOG_TB_IN_ASM) {
++                                    fprintf(logfile, "Servicing hardware INT=0x%02x\n", intno);
++                                }
++                                do_interrupt(intno, 0, 0, 0, 1);
++                                /* ensure that no TB jump will be modified as
++                                   the program flow was changed */
++                                next_tb = 0;
+                             }
+-                            do_interrupt(intno, 0, 0, 0, 1);
+-                            /* ensure that no TB jump will be modified as
+-                               the program flow was changed */
+-                            next_tb = 0;
  #if !defined(CONFIG_USER_ONLY)
-                     } else if ((interrupt_request & CPU_INTERRUPT_VIRQ) &&
-                         (env->eflags & IF_MASK) && !(env->hflags & HF_INHIBIT_IRQ_MASK)) {
+                         } else if ((interrupt_request & CPU_INTERRUPT_VIRQ) &&
+                                    (env->eflags & IF_MASK) && 
