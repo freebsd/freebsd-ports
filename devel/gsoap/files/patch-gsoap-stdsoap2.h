c1a337e4429a8cdf8d8fc11b22ddfc4305fc7cb1
Sergey Matveychuk <sem@FreeBSD.org>
2008-07-09 17:57:46 +0000

    - Update to 2.7.10
    PR:		ports/125259
    Submitted by:	Bjorn Konig <bkoenig at alpha-tierchen.de>

---
+++ devel/gsoap/files/patch-gsoap-stdsoap2.h
@@ -0,0 +1,12 @@
+--- gsoap/stdsoap2.h.orig	2008-01-27 22:02:36.000000000 +0100
++++ gsoap/stdsoap2.h	2008-07-04 12:35:55.000000000 +0200
+@@ -292,9 +292,6 @@
+ #  define HAVE_STRRCHR
+ #  define HAVE_STRTOD
+ #  define HAVE_SSCANF
+-#  define HAVE_STRTOD_L
+-#  define HAVE_SSCANF_L
+-#  define HAVE_SPRINTF_L
+ #  define HAVE_STRTOL
+ #  define HAVE_STRTOUL
+ #  define HAVE_STRTOLL
