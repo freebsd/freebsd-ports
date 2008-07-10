6ca23cbd64556715734a87979218c0ce0f78ce75
Akinori MUSHA <knu@FreeBSD.org>
2008-07-10 11:10:57 +0000

    Update to 2.09i.

--- shells/fd/files/patch-fd.h
+++ shells/fd/files/patch-fd.h
@@ -1,17 +1,17 @@
---- fd.h.orig	Tue Aug 10 00:00:00 2004
-+++ fd.h	Fri Aug 20 10:01:22 2004
-@@ -159,11 +159,11 @@
+--- fd.h.orig	2008-04-19 00:00:00.000000000 +0900
++++ fd.h	2008-07-10 20:00:12.000000000 +0900
+@@ -183,11 +183,11 @@
   ****************************************************************/
- #define	MAXBINDTABLE	256
- #define	MAXMACROTABLE	64
--#define	MAXLAUNCHTABLE	32
--#define	MAXARCHIVETABLE	16
-+#define	MAXLAUNCHTABLE	64
-+#define	MAXARCHIVETABLE	32
- #define	MAXALIASTABLE	256
- #define	MAXFUNCTABLE	32
--#define	MAXFUNCLINES	16
-+#define	MAXFUNCLINES	32
+ #define	MAXBINDTABLE		256
+ #define	MAXMACROTABLE		64
+-#define	MAXLAUNCHTABLE		32
+-#define	MAXARCHIVETABLE		16
++#define	MAXLAUNCHTABLE		64
++#define	MAXARCHIVETABLE		32
+ #define	MAXALIASTABLE		256
+ #define	MAXFUNCTABLE		32
+-#define	MAXFUNCLINES		16
++#define	MAXFUNCLINES		32
  #if	MSDOS
- #define	MAXCOMMSTR	(128 - 2)
+ #define	MAXCOMMSTR		(128 - 2)
  #endif
