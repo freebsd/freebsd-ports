2449b5dd35cd5506f43abd35af19efd35f07112c
miwi <miwi@FreeBSD.org>
2008-07-10 23:33:43 +0000

    - Update to 2.18
    PR:		125381
    Submitted by:	Xavier Beaudouin <kiwi@oav.net> (maintainer)

--- net-mgmt/routers2/files/patch-routers2.cgi.pl
+++ net-mgmt/routers2/files/patch-routers2.cgi.pl
@@ -1,22 +1,24 @@
---- routers2.cgi.pl.old	Mon Dec 11 15:57:57 2006
-+++ routers2.cgi.pl	Mon Dec 11 16:01:18 2006
-@@ -1,9 +1,4 @@
+--- routers2.cgi.pl.orig	2008-06-09 07:18:11.000000000 +0200
++++ routers2.cgi.pl	2008-07-07 22:43:34.000000000 +0200
+@@ -1,9 +1,9 @@
 -#!/usr/bin/perl 
 -# DONT FORGET TO CHANGE THE ABOVE PATH TO MATCH YOUR PERL LOCATION! 
--# vim:ts=4
--##############################################################################
--# To use Speedycgi, you need to change the first line to this:
++#!%%LOCALBASE%%/bin/perl
++# DONT FORGET TO CHANGE THE ABOVE PATH TO MATCH YOUR PERL LOCATION!
+ # vim:ts=4
+ ##############################################################################
+ # To use Speedycgi, you need to change the first line to this:
 -##!/usr/bin/speedy -- -M20 -t3600 -gnone -r500
-+#!%%LOCALBASE%%/bin/speedy -- -M20 -t3600 -gnone -r500
++###!%%LOCALBASE%%/bin/speedy -- -M20 -t3600 -gnone -r500
  # and then set the CACHE global variable (below) to 1.
  # To use mod_perl, you should be able to put the script directly into the
  # mod_perl directory and it will work.  This is not very thoroughly tested 
-@@ -30,7 +25,7 @@
- use POSIX qw(tzset);  # For timezone support
+@@ -35,7 +35,7 @@
+ #use Data::Dumper;     # For persistent caching
  ##CONFIG#START################################################################
  # You MUST set this to the location of the configuration file!
 -my ($conffile) = '/u01/etc/routers2.conf';
-+my ($conffile) = '%%LOCALBASE%%/etc/routers2.conf';
++my ($conffile) = '/usr/local/etc/routers2.conf';
  ##############################################################################
  # Are we running in speedycgi or mod_perl mode?  Can we cache configs?
  # If you set this to 1 when you dont have speedycgi or mod_perl, it will
