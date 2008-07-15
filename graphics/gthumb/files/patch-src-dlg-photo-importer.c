b9e3371a4b8232048ede6f7dee2cf2b9f6ddace7
pav <pav@FreeBSD.org>
2008-07-15 20:23:20 +0000

    - Fix build after gphoto upgrade

---
+++ graphics/gthumb/files/patch-src-dlg-photo-importer.c
@@ -0,0 +1,10 @@
+--- src/dlg-photo-importer.c.orig	2008-01-01 12:13:17.000000000 +0100
++++ src/dlg-photo-importer.c	2008-07-15 22:02:41.000000000 +0200
+@@ -28,6 +28,7 @@
+ #include <string.h>
+ #include <strings.h>
+ #include <time.h>
++#include <unistd.h>
+ 
+ #include <glib/gi18n.h>
+ #include <gtk/gtk.h>
