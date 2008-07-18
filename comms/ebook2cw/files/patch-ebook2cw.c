19ede5f99a733dbb49cb8771cd0f3072535ec1a6
db <db@FreeBSD.org>
2008-07-18 14:57:43 +0000

    - ebook2cw converts text into cw as an mp3 file, useful for learning cw.

---
+++ comms/ebook2cw/files/patch-ebook2cw.c
@@ -0,0 +1,11 @@
+--- ebook2cw.c.orig	2008-07-17 21:28:33.000000000 -0400
++++ ebook2cw.c	2008-07-17 21:28:45.000000000 -0400
+@@ -20,7 +20,7 @@
+ 
+ */
+ 
+-#include "lame/lame.h"
++#include <lame/lame.h>
+ #include <math.h>
+ #include <stdio.h>
+ #include <string.h>
