683ad272183443d5979aa749c068060527d38a15
Beech Rintoul <beech@FreeBSD.org>
2008-07-18 00:46:11 +0000

    - Unbreak on >7.x - Fix build - Bump portrevision - Pass maintainership to submitter
    PR:		ports/125622
    Submitted by:	Ashish Shukla <wahjava@gmail.com>

---
+++ irc/irssi-fish/files/patch-base64.c
@@ -0,0 +1,12 @@
+--- base64.c.orig	2005-01-17 14:19:18.000000000 -0900
++++ base64.c	2008-07-14 21:41:47.000000000 -0800
+@@ -1,6 +1,9 @@
+ /*
+     Public Base64 conversion tables
+ */
++
++#include <string.h>
++
+ unsigned char B64ABC[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+ unsigned char b64buf[256];
+ 
---
+++ irc/irssi-fish/files/patch-blowfish.c
@@ -0,0 +1,9 @@
+--- blowfish.c.orig	2005-01-16 14:59:16.000000000 -0900
++++ blowfish.c	2008-07-14 21:46:38.000000000 -0800
+@@ -1,4 +1,6 @@
+ #include "blowfish.h"
++#include <string.h>
++#include <stdlib.h>
+ 
+ /* #define S(x,i) (bf_S[i][x.w.byte##i]) */
+ #define S0(x) (bf_S[0][x.w.byte0])
