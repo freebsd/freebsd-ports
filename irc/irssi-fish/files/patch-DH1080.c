525b2f0b852a8277aee519193a895ae5d1865a61
beech <beech@FreeBSD.org>
2008-07-18 00:46:11 +0000

    - Unbreak on >7.x - Fix build - Bump portrevision - Pass maintainership to submitter
    PR:		ports/125622
    Submitted by:	Ashish Shukla <wahjava@gmail.com>

---
+++ irc/irssi-fish/files/patch-DH1080.c
@@ -0,0 +1,10 @@
+--- DH1080.c.orig	2005-01-16 14:59:10.000000000 -0900
++++ DH1080.c	2008-07-14 21:26:25.000000000 -0800
+@@ -13,6 +13,7 @@
+ 
+ #include <stdio.h>
+ #include <time.h>
++#include <string.h>
+ #include "DH1080.h"
+ 
+ // ### new sophie-germain 1080bit prime number ###
---
+++ irc/irssi-fish/files/patch-SHA-256.c
@@ -0,0 +1,10 @@
+--- SHA-256.c.orig	2005-01-16 14:51:34.000000000 -0900
++++ SHA-256.c	2008-07-14 21:34:42.000000000 -0800
+@@ -12,6 +12,7 @@
+  * */
+ 
+ #include <stdio.h>
++#include <string.h>
+ 
+ typedef struct {
+     unsigned int state[8], length, curlen;
