2e1aa70b3463734133201983c3252bc1d062e11c
rafan <rafan@FreeBSD.org>
2008-07-19 14:10:37 +0000

    - Update to 1.5.1.54.40   http://www.oracle.com/technology/products/database/sql_developer/files/relnotes_v151.html - MySQL dependency is removed due to databases/mysql-connector-odbc is updated   to 5.1 and this port only supports 5.0 (see pkg-message for details)
    PR:		ports/125724
    Submitted by:	JosÃ© GarcÃ­a Juanino <jjuanino at gmail.com> (maintainer)

--- databases/sqldeveloper/files/patch-sqldeveloper.sh
+++ databases/sqldeveloper/files/patch-sqldeveloper.sh
@@ -1,6 +1,6 @@
---- sqldeveloper.sh.orig	2008-04-21 08:33:48.000000000 +0200
-+++ sqldeveloper.sh	2008-05-15 12:02:13.000000000 +0200
+--- sqldeveloper.sh.orig	2008-07-16 21:07:00.000000000 +0200
++++ sqldeveloper.sh	2008-07-16 21:10:20.000000000 +0200
 @@ -1,2 +1,2 @@
  #!/bin/bash
 -cd "`dirname $0`"/sqldeveloper/bin && bash sqldeveloper $*
-+cd %DATADIR%/sqldeveloper/bin && JAVA_HOME=%JAVA_HOME% bash sqldeveloper $*
++cd %DATADIR%/sqldeveloper/bin && JAVA_HOME=%JAVA_HOME% exec bash sqldeveloper $*
