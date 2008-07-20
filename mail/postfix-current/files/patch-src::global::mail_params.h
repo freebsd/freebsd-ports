105531a56ac1c75c7197b94d5aac5bf21d939003
mnag <mnag@FreeBSD.org>
2008-07-20 23:38:19 +0000

    - Update to 2.6-20080629 - Change default data_directory parameter to /var/db/postfix [1]
    PR:		125133 [1]
    Submitted by:	Sahil Tandon <sahil__tandon.net> [1]

--- mail/postfix-current/files/patch-src::global::mail_params.h
+++ mail/postfix-current/files/patch-src::global::mail_params.h
@@ -1,6 +1,6 @@
---- src/global/mail_params.h.orig	Fri Feb 11 21:53:21 2005
-+++ src/global/mail_params.h	Sat Mar  5 11:53:42 2005
-@@ -64,7 +64,7 @@
+--- src/global/mail_params.h.orig	2008-04-28 20:47:26.000000000 -0300
++++ src/global/mail_params.h	2008-07-20 20:21:07.000000000 -0300
+@@ -67,7 +67,7 @@
  extern gid_t var_owner_gid;
  
  #define VAR_SGID_GROUP		"setgid_group"
@@ -9,7 +9,7 @@
  extern char *var_sgid_group;
  extern gid_t var_sgid_gid;
  
-@@ -221,7 +221,7 @@
+@@ -241,7 +241,7 @@
    */
  #define VAR_DAEMON_DIR		"daemon_directory"
  #ifndef DEF_DAEMON_DIR
@@ -18,7 +18,16 @@
  #endif
  extern char *var_daemon_dir;
  
-@@ -250,7 +250,7 @@
+@@ -265,7 +265,7 @@
+   */
+ #define VAR_DATA_DIR		"data_directory"
+ #ifndef DEF_DATA_DIR
+-#define DEF_DATA_DIR		"/var/lib/postfix"
++#define DEF_DATA_DIR		"/var/db/postfix"
+ #endif
+ extern char *var_data_dir;
+ 
+@@ -279,7 +279,7 @@
    */
  #define VAR_CONFIG_DIR		"config_directory"
  #ifndef DEF_CONFIG_DIR
