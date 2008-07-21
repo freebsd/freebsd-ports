38b69bf45012acf034f61e46b686ff72ddfc0be5
rafan <rafan@FreeBSD.org>
2008-07-21 04:47:49 +0000

    - Update to 1.85 (share lib version bumped)
    PR:		ports/125435
    Submitted by:	Yi-Jheng Lin <yzlin at cs.nctu.edu.tw>
    Approved by:	Amatus <c11h15no2 at yahoo.com> (maintainer)

--- lang/guile/files/patch-libguile_eval.c
+++ lang/guile/files/patch-libguile_eval.c
@@ -1,11 +1,11 @@
---- libguile/eval.c	2008-02-23 18:14:28.083264040 -0600
-+++ libguile/eval.c	2008-02-23 18:20:55.081183329 -0600
-@@ -40,6 +40,8 @@
- /* This blob per the Autoconf manual (under "Particular Functions"). */
- #if HAVE_ALLOCA_H
- # include <alloca.h>
-+#elif defined __FreeBSD__
-+# include <stdlib.h>
- #elif defined __GNUC__
- # define alloca __builtin_alloca
- #elif defined _AIX
+--- libguile/eval.c	2008-07-09 14:44:29.000000000 +0800
++++ libguile/eval.c	2008-07-09 14:45:45.000000000 +0800
+@@ -42,6 +42,8 @@
+ #ifndef alloca
+ # if HAVE_ALLOCA_H
+ #  include <alloca.h>
++# elif defined __FreeBSD__
++#  include <stdlib.h>
+ # elif defined __GNUC__
+ #  define alloca __builtin_alloca
+ # elif defined _AIX
