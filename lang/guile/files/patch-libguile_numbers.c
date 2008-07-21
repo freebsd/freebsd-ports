e9c1a8c826b30368b045802db85d5822dd5f2626
Rong-En Fan <rafan@FreeBSD.org>
2008-07-21 04:47:49 +0000

    - Update to 1.85 (share lib version bumped)
    PR:		ports/125435
    Submitted by:	Yi-Jheng Lin <yzlin at cs.nctu.edu.tw>
    Approved by:	Amatus <c11h15no2 at yahoo.com> (maintainer)

--- lang/guile/files/patch-libguile_numbers.c
+++ lang/guile/files/patch-libguile_numbers.c
@@ -1,11 +1,11 @@
---- libguile/numbers.c	2008-02-23 15:41:35.231371691 -0600
-+++ libguile/numbers.c	2008-02-23 15:44:52.401368437 -0600
-@@ -170,7 +170,7 @@
- #endif
+--- libguile/numbers.c	2008-07-09 14:50:22.000000000 +0800
++++ libguile/numbers.c	2008-07-09 14:53:05.000000000 +0800
+@@ -163,7 +163,7 @@
+ }
  
- /* Convert a C "complex double" to an SCM value. */
+ #if defined (GUILE_I)
 -#if HAVE_COMPLEX_DOUBLE
 +#if HAVE_COMPLEX_DOUBLE && (HAVE_CLOG || HAVE_CLOG10 || HAVE_CEXP || HAVE_USABLE_CSQRT) && defined (SCM_COMPLEX_VALUE)
- static SCM
- scm_from_complex_double (complex double z)
- {
+ 
+ /* For an SCM object Z which is a complex number (ie. satisfies
+    SCM_COMPLEXP), return its value as a C level "complex double". */
