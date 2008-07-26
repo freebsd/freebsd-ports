f3a7c1088b10040c86480b4f096412f813b099a8
Michael Johnson <ahze@FreeBSD.org>
2008-07-26 18:39:26 +0000

    Update to 1.0.4

--- print/system-config-printer/files/patch-system-config-printer.py
+++ print/system-config-printer/files/patch-system-config-printer.py
@@ -1,24 +1,24 @@
---- system-config-printer.py.orig	2008-05-23 04:57:31.000000000 -0700
-+++ system-config-printer.py	2008-05-26 23:25:11.159502896 -0700
-@@ -3098,7 +3098,7 @@
+--- system-config-printer.py.orig	2008-07-10 10:31:58.000000000 -0500
++++ system-config-printer.py	2008-07-26 13:26:03.412705522 -0500
+@@ -3389,7 +3389,7 @@
          # Try to get make and model via SNMP
          if host:
              os.environ["HOST"] = host
--            cmd = '/usr/lib/cups/backend/snmp "${HOST}" 2>/dev/null'
-+            cmd = '%%LOCALBASE%%/libexec/cups/backend/snmp "${HOST}" 2>/dev/null'
+-            cmd = '/usr/lib/cups/backend/snmp "${HOST}"'
++            cmd = '%%LOCALBASE%%/lib/cups/backend/snmp "${HOST}"'
              debugprint (host + ": " + cmd)
-             p = os.popen(cmd, 'r')
-             output = p.read ().strip ()
-@@ -4370,7 +4370,7 @@
-                 os.environ["PPD"] = filename
-                 # We want this to be in the current natural language,
-                 # so we intentionally don't set LC_ALL=C here.
--                p = os.popen ('/usr/bin/cupstestppd -rvv "$PPD"', 'r')
-+                p = os.popen ('%%LOCALBASE%%/bin/cupstestppd -rvv "$PPD"', 'r')
-                 output = p.readlines ()
-                 p.close ()
-                 err += reduce (lambda x, y: x + y, output)
-@@ -4655,7 +4655,7 @@
+             stdout = None
+             try:
+@@ -4693,7 +4693,7 @@
+                 try:
+                     # We want this to be in the current natural language,
+                     # so we intentionally don't set LC_ALL=C here.
+-                    p = subprocess.Popen (['/usr/bin/cupstestppd',
++                    p = subprocess.Popen (['%%LOCALBASE%%/bin/cupstestppd',
+                                            '-rvv', filename],
+                                           stdin=file("/dev/null"),
+                                           stdout=subprocess.PIPE,
+@@ -4984,7 +4984,7 @@
          (pkgs, exes) = cupshelpers.missingPackagesAndExecutables (ppd)
          if len (pkgs) > 0 or len (exes) > 0:
              # We didn't find a necessary executable.  Complain.
