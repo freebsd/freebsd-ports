ed86f41155edabfc5d4ba2ec297b64837bf91955
Beech Rintoul <beech@FreeBSD.org>
2008-07-26 04:31:12 +0000

    Google App Engine Oil (GAEO) is an open-source web framework running on Google App Engine.
    Google App Engine Oil (GAEO) enables the web development on
    Google AppEngine quick and less configurations. GAEO enhances
    Google AppEngine SDK with new features such as:
        - Well-formed URL Routing
        - Enhanced Request Handler (Controller)
        - Enhanced Object-Relation Mapping (ORM)
    
    PR:		ports/125923
    Submitted by:	Yi-Jheng Lin <yzlin at cs.nctu.edu.tw>

---
+++ www/gaeo/files/patch-bin_gaeo.py
@@ -0,0 +1,11 @@
+--- bin/gaeo.py.orig	2008-07-24 15:24:37.000000000 +0800
++++ bin/gaeo.py	2008-07-24 15:25:07.000000000 +0800
+@@ -196,7 +196,7 @@
+     create_file(os.path.join(project_home, 'favicon.ico'), [])
+ 
+     # copy GAEO directory
+-    copytree(os.path.join(os.path.dirname(__file__), '..', 'gaeo'), os.path.join(project_home, 'gaeo'))
++    copytree(os.path.join(os.path.dirname(__file__), '%%DATADIR%%', 'gaeo'), os.path.join(project_home, 'gaeo'))
+ 
+     # create the eclipse project file
+     if create_eclipse_proj:
