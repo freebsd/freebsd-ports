805341278a66d62d9346743ec7b4ebd814d94309
Christian Weisgerber <naddy@FreeBSD.org>
2008-07-27 20:06:34 +0000

    Reorder include files to allow building with -CURRENT gcc (4.2).

---
+++ news/trn4/files/patch-rt-page.c
@@ -0,0 +1,17 @@
+
+$FreeBSD$
+
+--- rt-page.c.orig
++++ rt-page.c
+@@ -14,10 +14,10 @@
+ #include "datasrc.h"
+ #include "nntp.h"
+ #include "trn.h"
+-#include "opt.h"
+ #include "env.h"
+ #include "util.h"
+ #include "util2.h"
++#include "opt.h"
+ #include "only.h"
+ #include "addng.h"
+ #include "rcln.h"
