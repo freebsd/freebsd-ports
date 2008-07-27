3678e19fcc26b69b7cb37c0bbe6e354688f43dcd
naddy <naddy@FreeBSD.org>
2008-07-27 20:06:34 +0000

    Reorder include files to allow building with -CURRENT gcc (4.2).

---
+++ news/trn4/files/patch-datasrc.c
@@ -0,0 +1,16 @@
+
+$FreeBSD$
+
+--- datasrc.c.orig
++++ datasrc.c
+@@ -12,9 +12,9 @@
+ #include "nntpclient.h"
+ #include "term.h"
+ #include "env.h"
+-#include "opt.h"
+ #include "util.h"
+ #include "util2.h"
++#include "opt.h"
+ #include "intrp.h"
+ #include "init.h"
+ #include "rcstuff.h"
