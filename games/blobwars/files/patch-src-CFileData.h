7ddee40f4075e8260add51515a4ea16931dc19b1
amdmi3 <amdmi3@FreeBSD.org>
2008-07-28 14:39:44 +0000

    - Update to 1.08-1
    PR:		ports/125517
    Approved by:	miwi (mentor), maintainer timeout

--- games/blobwars/files/patch-src-CFileData.h
+++
@@ -1,9 +0,0 @@
---- src/CFileData.h.orig	2005-04-25 18:25:38.000000000 +0000
-+++ src/CFileData.h	2007-10-31 02:26:03.000000000 +0000
-@@ -30,5 +30,5 @@
- 	FileData();
- 	
- 	void swapEndians();
--	void FileData::set(char *filename, Uint32 fSize, Uint32 cSize, Uint32 location);
-+	void set(char *filename, Uint32 fSize, Uint32 cSize, Uint32 location);
- };
