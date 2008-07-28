7568375226281ab416adb62d3c7f5696c274637e
lioux <lioux@FreeBSD.org>
2008-07-28 13:47:27 +0000

    Update to 1.19

--- mail/normalizemime/files/patch-normalizemime.cc
+++ mail/normalizemime/files/patch-normalizemime.cc
@@ -1,15 +1,15 @@
---- normalizemime.cc.orig	Tue Sep 13 00:03:52 2005
-+++ normalizemime.cc	Tue Sep 13 00:04:18 2005
+--- normalizemime.cc.orig	2008-07-28 10:36:10.000000000 -0300
++++ normalizemime.cc	2008-07-28 10:37:16.000000000 -0300
 @@ -58,6 +58,8 @@
  #include <map>
  #include <string>
  
 +#include <sys/types.h>
 +
+ #include <string.h>
+ #include <stdlib.h>
  #include <regex.h>
- #include <assert.h>
- #include <ctype.h>
-@@ -498,7 +500,7 @@
+@@ -500,7 +502,7 @@
    char *obuf = buf;
    memcpy (icopy, message.c_str(), in_left);
    message.erase ();
@@ -18,7 +18,7 @@
  
    while (in_left > 0) {
      if ((size_t)(-1) == iconv (ic, &ibuf, &in_left, &obuf, &out_left)) {
-@@ -1491,6 +1493,7 @@
+@@ -1493,6 +1495,7 @@
        "X-Spam-",           // Added by SpamAssasin for example
        "X-CRM114-",         // Added by CRM114
        "X-Virus-",          // Added by ClamAV
