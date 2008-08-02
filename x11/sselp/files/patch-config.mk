c24bcf0fc66338b519517225e2793564f3e5cec5
miwi <miwi@FreeBSD.org>
2008-08-02 22:11:34 +0000

    - Update to 0.2
    PR:		126102
    Submitted by:	Julien Laffaye <kimelto@gmail.com> (maintainer)

--- x11/sselp/files/patch-config.mk
+++ x11/sselp/files/patch-config.mk
@@ -1,32 +1,31 @@
---- config.mk.orig 2008-07-19 13:20:31.000000000 +0200
-+++ config.mk  2008-07-19 14:18:14.000000000 +0200
-@@ -4,22 +4,22 @@
+--- config.mk.orig	2008-07-30 19:11:59.000000000 +0200
++++ config.mk	2008-07-30 19:13:21.000000000 +0200
+@@ -4,20 +4,20 @@
  # Customize below to fit your system
-
+ 
  # paths
 -PREFIX = /usr/local
 -MANPREFIX = ${PREFIX}/share/man
 +PREFIX ?= /usr/local
 +MANPREFIX = ${PREFIX}/man
-
+ 
 -X11INC = /usr/X11R6/include
 -X11LIB = /usr/X11R6/lib
 +X11INC = $(LOCALBASE)/include
 +X11LIB = $(LOCALBASE)/lib
-
+ 
  # includes and libs
  INCS = -I. -I/usr/include -I${X11INC}
  LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
-
+ 
  # flags
--CFLAGS = -Os ${INCS} -DVERSION=\"${VERSION}\"
--LDFLAGS = ${LIBS}
-+CFLAGS += -Os ${INCS} -DVERSION=\"${VERSION}\"
-+LDFLAGS += ${LIBS}
- #CFLAGS = -g -Wall -O2 ${INCS} -DVERSION=\"${VERSION}\"
- #LDFLAGS = -g ${LIBS}
-
+-CPPFLAGS = -DVERSION=\"${VERSION}\"
+-CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+-LDFLAGS = -s ${LIBS}
++CPPFLAGS += -DVERSION=\"${VERSION}\"
++CFLAGS += -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
++LDFLAGS += -s ${LIBS}
+ 
  # compiler and linker
 -CC = cc
 +CC ?= cc
- LD = ${CC}
