bb5a901564514e90561d3e5a92cf2e4c88490f2e
Dmitry Marakasov <amdmi3@FreeBSD.org>
2008-08-03 19:58:45 +0000

    - Improve serial device handling - Add mirror - Bump PORTREVISION
    Approved by:	miwi (mentor)

--- graphics/lprof-devel/files/patch-src-argyll-spectro-unixio.c
+++ graphics/lprof-devel/files/patch-src-argyll-spectro-unixio.c
@@ -1,5 +1,5 @@
 --- src/argyll/spectro/unixio.c.orig	2008-02-20 00:02:24.000000000 +0300
-+++ src/argyll/spectro/unixio.c	2008-05-14 06:16:33.000000000 +0400
++++ src/argyll/spectro/unixio.c	2008-08-03 21:47:17.000000000 +0400
 @@ -585,9 +585,8 @@
  				break;
  
@@ -7,21 +7,8 @@
 -			    (   strncmp(de->d_name, "ttyS", 4) == 0
 -			     && de->d_name[4] >= '0' && de->d_name[4] <= '9')
 -			 || (   strncmp(de->d_name, "ttyUSB", 5) == 0)
-+			    (   strncmp(de->d_name, "ttyd", 4) == 0 || strncmp(de->d_name, "ttyU", 4) == 0)
-+			     && de->d_name[4] >= '0' && de->d_name[4] <= '9' && de->d_name[5] == '\0'
++			    (strncmp(de->d_name, "cuad", 4) == 0 || strncmp(de->d_name, "cuaU", 4) == 0) &&
++			    strstr(de->d_name, ".init") == 0 && strstr(de->d_name, ".lock") == 0
  			))
  				continue;
  
-@@ -598,12 +597,6 @@
- 			strcpy(dpath, dirn);
- 			strcat(dpath, de->d_name);
- 
--			if ((fd = open(dpath, O_RDWR | O_NOCTTY )) < 0) {
--				free(dpath);
--				continue;
--			}
--			close(fd);
--
- 			/* Add the path to the list */
- 			if (p->paths == NULL) {
- 				if ((p->paths = (icompath **)calloc(sizeof(icompath *), 1 + 1)) == NULL) {
