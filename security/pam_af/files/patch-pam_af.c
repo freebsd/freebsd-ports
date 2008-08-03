484f070a21e76b782f80c9882524e1d04866cd23
Stanislav Sedov <stas@FreeBSD.org>
2008-08-03 08:06:15 +0000

    - OpenPAM seesm to defined _OPENPAM instead of OPENPAM in older   releases. Make autodetection work on both.

--- security/pam_af/files/patch-pam_af.c
+++ security/pam_af/files/patch-pam_af.c
@@ -1,11 +1,11 @@
---- pam_af.c.orig	2008-08-02 20:34:30.000000000 +0400
-+++ pam_af.c	2008-08-02 20:34:36.000000000 +0400
+--- pam_af.c.orig	2006-11-07 03:05:53.000000000 +0300
++++ pam_af.c	2008-08-03 12:03:49.000000000 +0400
 @@ -53,7 +53,7 @@
  
  #include <security/pam_appl.h>
  #include <security/pam_modules.h>
 -#ifdef _OPENPAM
-+#ifdef OPENPAM
++#if defined(OPENPAM) || defined(_OPENPAM)
  # include <security/openpam.h>
  # include <security/pam_mod_misc.h>
  #endif
