52f909195b30939df578181a71be3b5fd85a7104
stas <stas@FreeBSD.org>
2008-08-03 08:06:15 +0000

    - OpenPAM seesm to defined _OPENPAM instead of OPENPAM in older   releases. Make autodetection work on both.

--- security/pam_af/files/patch-common_pam_af.h
+++ security/pam_af/files/patch-common_pam_af.h
@@ -1,11 +1,11 @@
---- common/pam_af.h.orig	2008-08-02 20:32:55.000000000 +0400
-+++ common/pam_af.h	2008-08-02 20:33:08.000000000 +0400
+--- common/pam_af.h.orig	2005-10-16 19:55:28.000000000 +0400
++++ common/pam_af.h	2008-08-03 12:00:10.000000000 +0400
 @@ -37,7 +37,7 @@
  #define CFGDB "/etc/pam_af.conf"
  #define CFGDB_PERM (S_IRUSR | S_IWUSR)
  
 -#ifdef _OPENPAM
-+#ifdef OPENPAM
++#if defined(OPENPAM) || defined(_OPENPAM)
  # define PAM_AF_LOG(...) \
  	PAM_LOG(__VA_ARGS__)
  # define PAM_AF_LOGERR(...) \
--- security/pam_af/files/patch-common_subr.c
+++ security/pam_af/files/patch-common_subr.c
@@ -1,11 +1,11 @@
---- common/subr.c.orig	2008-08-02 20:33:51.000000000 +0400
-+++ common/subr.c	2008-08-02 20:34:25.000000000 +0400
+--- common/subr.c.orig	2006-11-07 03:22:37.000000000 +0300
++++ common/subr.c	2008-08-03 12:00:42.000000000 +0400
 @@ -66,7 +66,7 @@
  # define LOGERR(...) warnx(__VA_ARGS__)
  #else /* !PAM_AF_DEFS */
  # include <security/pam_appl.h>
 -# ifdef _OPENPAM
-+# ifdef OPENPAM
++# if defined(OPENPAM) || defined(_OPENPAM)
  #  include <security/pam_mod_misc.h>
  #  include <security/openpam.h>
  # endif
