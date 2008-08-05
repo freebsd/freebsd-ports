098a2242e41fb0edc656835ec414e6911de3f34b
Martin Wilke <miwi@FreeBSD.org>
2008-08-05 00:20:35 +0000

    - Update to 4.4.1
    Reported by:	ionbot

--- devel/qmake4/files/qconfig.cpp
+++ devel/qmake4/files/qconfig.cpp
@@ -5,15 +5,15 @@
  * at configure time.
  */
 #define QT_CONFIGURE_LICENSEE "Open Source";
-#define QT_CONFIGURE_LICENSED_PRODUCTS "OpenSourceDesktop";
+#define QT_CONFIGURE_LICENSED_PRODUCTS "OpenSource";
 #define QT_CONFIGURE_PREFIX_PATH "/usr/local";
 #define QT_CONFIGURE_DOCUMENTATION_PATH "/usr/local/share/doc/qt4";
-#define QT_CONFIGURE_HEADERS_PATH "/usr/local/include";
-#define QT_CONFIGURE_LIBRARIES_PATH "/usr/local/lib";
+#define QT_CONFIGURE_HEADERS_PATH "/usr/local/include/qt4";
+#define QT_CONFIGURE_LIBRARIES_PATH "/usr/local/lib/qt4";
 #define QT_CONFIGURE_BINARIES_PATH "/usr/local/bin";
 #define QT_CONFIGURE_PLUGINS_PATH "/usr/local/lib/qt4/plugins";
 #define QT_CONFIGURE_DATA_PATH "/usr/local/share/qt4";
 #define QT_CONFIGURE_TRANSLATIONS_PATH "/usr/local/share/qt4/translations";
 #define QT_CONFIGURE_SETTINGS_PATH "/usr/local/etc/xdg";
-#define QT_CONFIGURE_EXAMPLES_PATH "/usr/local/share/qt4/examples";
-#define QT_CONFIGURE_DEMOS_PATH "/usr/local/share/qt4/demos/";
+#define QT_CONFIGURE_EXAMPLES_PATH "/usr/local/share/examples/qt4/examples";
+#define QT_CONFIGURE_DEMOS_PATH "/usr/local/share/examples/qt4/demos/";
