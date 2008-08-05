ca600e1e5594540d88060af4a4ac3eaa1307fed8
Dirk Meyer <dinoex@FreeBSD.org>
2008-08-05 19:33:16 +0000

    - add patches for WEBALIZER_LANG=ukrainian

---
+++ www/webalizer/files/uk-lang.h
@@ -0,0 +1,11 @@
+--- lang.h.orig	Thu May 17 19:47:24 2001
++++ lang.h	Thu May 17 16:42:11 2001
+@@ -124,6 +124,8 @@
+ extern char *msg_mtot_ms ;
+ extern char *msg_mtot_th ;
+ extern char *msg_mtot_tf ;
++extern char *msg_mtot_tp ;
++extern char *msg_mtot_tv ;
+ extern char *msg_mtot_tx ;
+ extern char *msg_mtot_us ;
+ extern char *msg_mtot_ur ;
