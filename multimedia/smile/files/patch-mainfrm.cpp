0185e94371236946ad2f33c03acc3657dd55bc40
amdmi3 <amdmi3@FreeBSD.org>
2008-08-06 00:15:22 +0000

    - Update to 0.6.1
    PR:		ports/126288
    Submitted by:	Diego Depaoli <trebestie@gmail.com> (maintainer)
    Appeoved by:	miwi (mentor implicit)

--- multimedia/smile/files/patch-mainfrm.cpp
+++ multimedia/smile/files/patch-mainfrm.cpp
@@ -1,14 +1,14 @@
---- mainfrm.cpp	2008-07-06 23:36:59.000000000 +0200
-+++ mainfrm.cpp.new	2008-07-06 23:38:27.000000000 +0200
-@@ -815,9 +815,9 @@
- 			QString program = "cp";
- 			QStringList arguments;
+--- mainfrm.cpp	2008-08-04 10:44:08.000000000 +0200
++++ mainfrm.cpp.new	2008-08-04 12:02:17.000000000 +0200
+@@ -1284,9 +1284,9 @@
+ 				arguments << tempfolder->text();
+ 			#else
+ 				program = "cp";
+-				arguments << "-r";
++				arguments << "-R";
+ 				arguments << "-f";
+-				arguments << QApplication::applicationDirPath() + "/BIB_ManSlide/";
++				arguments << QApplication::applicationDirPath() + "/BIB_ManSlide";
+ 				arguments << tempfolder->text();
+ 			#endif
  			ispreview = 4;
--			arguments << "-r";
-+			arguments << "-R";
- 			arguments << "-f";
--			arguments << QApplication::applicationDirPath() + "/BIB_ManSlide/";
-+			arguments << QApplication::applicationDirPath() + "/BIB_ManSlide";
- 			arguments << tempfolder->text();
- 			myprocess_9.start( program, arguments );
- 			//
