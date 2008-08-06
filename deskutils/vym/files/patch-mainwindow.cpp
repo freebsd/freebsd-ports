5aee71c3b3e79a35fd73a6a2bef48bbdb4292924
Martin Wilke <miwi@FreeBSD.org>
2008-08-06 07:57:06 +0000

    - Update to 1.12.0
    PR:		126222
    Submitted by:	Gerrit Beine <gerrit.beine@gmx.de> (maintainer)

--- deskutils/vym/files/patch-mainwindow.cpp
+++ deskutils/vym/files/patch-mainwindow.cpp
@@ -1,19 +1,10 @@
---- mainwindow.cpp.orig	Tue Sep  4 15:53:33 2007
-+++ mainwindow.cpp	Tue Jan  1 20:35:11 2008
-@@ -140,7 +140,7 @@
- 			#if defined(Q_OS_MACX)
- 				s=settings.value (p,"/usr/bin/open").toString();
- 			#else
--				s=settings.value (p,"mozilla");
-+				s=settings.value (p,"mozilla").toString();
- 			#endif
- 		#endif
- 		settings.setValue( p,s);
-@@ -3637,6 +3637,7 @@
+--- mainwindow.cpp.orig	2008-05-05 15:46:42.000000000 +0200
++++ mainwindow.cpp	2008-08-03 20:13:53.000000000 +0200
+@@ -3715,6 +3715,7 @@
  	searchList << "doc/tex";	// Easy testing working on vym.tex
  	searchList << "/usr/share/doc/vym";	// Debian
  	searchList << "/usr/share/doc/packages";// Knoppix
-+	searchList << "/usr/local/share/doc/vym"; //FreeBSD
++	searchList << "/usr/local/share/doc/vym";// FreeBSD
  
  	bool found=false;
  	QFile docfile;
