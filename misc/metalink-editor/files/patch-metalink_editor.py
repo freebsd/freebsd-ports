a5cb7429c989a39b48340f12e1d075461c076968
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2008-08-11 03:31:16 +0000

    New port metalink-editor version 1.1.0: GUI editor to create and edit metalinks

---
+++ misc/metalink-editor/files/patch-metalink_editor.py
@@ -0,0 +1,21 @@
+--- metalink_editor.py.orig	2007-07-29 05:58:54.000000000 -0300
++++ metalink_editor.py	2008-08-10 08:40:49.000000000 -0300
+@@ -18,7 +18,8 @@
+ 
+ ####import wxversion
+ ####wxversion.ensureMinimal("2.6")
+-import wx, metalink, sys, os.path
++import wx, sys, os.path
++from Metalink_editor import metalink
+ 
+ current_version = "1.1.0"
+ 
+@@ -28,7 +29,7 @@
+ 
+ # Just a simple hack... (used to locate the icon)
+ try:
+-    data_path = os.path.dirname(sys.argv[0])
++    data_path = os.path.dirname("%%DATADIR%%/metalink_small.png")
+     if data_path != "" and data_path != ".":
+         print "Data path:", data_path
+ except:
