12bd6d8ddb197e0b7910b7c63fc3ac8a023a46e5
dinoex <dinoex@FreeBSD.org>
2008-08-11 15:37:18 +0000

    - fix build for gnustep-gui > 0.13

---
+++ x11-themes/etoile-etoilemenus/files/patch-NSMenuView+Hackery.m
@@ -0,0 +1,10 @@
+--- Bundles/EtoileWildMenus/NSMenuView+Hackery.m.orig	2007-08-03 16:46:33.000000000 +0200
++++ Bundles/EtoileWildMenus/NSMenuView+Hackery.m	2008-08-11 10:28:16.000000000 +0200
+@@ -24,6 +24,7 @@
+ #include "NSMenuView+Hackery.h"
+ 
+ #import <AppKit/NSApplication.h>
++#import <AppKit/NSEvent.h>
+ #import <AppKit/PSOperators.h>
+ 
+ #import "EtoileMenuTitleView.h"
