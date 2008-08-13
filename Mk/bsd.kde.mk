ad383f3b4227100399807106948cf552d502eb12
miwi <miwi@FreeBSD.org>
2008-08-13 09:14:11 +0000

    - Fix dependency checks so that KDE3 apps pull in the correct versions   of kdelibs3 and kdebase3, when they only currently have KDE4 installed   (this is because kde4 also installs libs with same name, so check for unique files)
    Thanks to:	Andy Fawcett/Max Brazhnikov

--- Mk/bsd.kde.mk
+++ Mk/bsd.kde.mk
@@ -34,11 +34,11 @@ MASTER_SITE_KDE_kde=	${kmaster:S@%SUBDIR%/@${ksub}/@g}
 # USE_KDEBASE_VER section
 .if defined(USE_KDEBASE_VER)
 .if ${USE_KDEBASE_VER} == CVS
-LIB_DEPENDS+=	kfontinst:${PORTSDIR}/x11/kdebase
+LIB_DEPENDS+=	kfontinst.0:${PORTSDIR}/x11/kdebase
 USE_KDELIBS_VER=CVS
 .elif ${USE_KDEBASE_VER} == 3
 # kdebase 3.x common stuff
-LIB_DEPENDS+=	kfontinst:${PORTSDIR}/x11/kdebase3
+LIB_DEPENDS+=	kfontinst.0:${PORTSDIR}/x11/kdebase3
 USE_KDELIBS_VER=3
 .endif # ${USE_KDEBASE_VER} == 3
 .endif # defined(USE_KDEBASE_VER)
@@ -64,12 +64,12 @@ CONFIGURE_ARGS+=--build=${MACHINE_ARCH}-portbld-freebsd${OSREL} \
 .endif
 
 .if ${USE_KDELIBS_VER} == CVS
-LIB_DEPENDS+=	kimproxy:${PORTSDIR}/x11/kdelibs
+LIB_DEPENDS+=	kimproxy.0:${PORTSDIR}/x11/kdelibs
 USE_QT_VER=		CVS
 PREFIX=			${KDE_CVS_PREFIX}
 .elif ${USE_KDELIBS_VER} == 3
 # kdelibs 3.x common stuff
-LIB_DEPENDS+=	kimproxy:${PORTSDIR}/x11/kdelibs3
+LIB_DEPENDS+=	kimproxy.0:${PORTSDIR}/x11/kdelibs3
 USE_QT_VER=		3
 PREFIX=			${KDE_PREFIX}
 .else
