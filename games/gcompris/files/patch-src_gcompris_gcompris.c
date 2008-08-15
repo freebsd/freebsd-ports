ce85d9b96f2c7022538e9274879701a59be210fe
Joe Marcus Clarke <marcus@FreeBSD.org>
2008-08-15 06:16:12 +0000

    Update to 8.4.6.

--- games/gcompris/files/patch-src_gcompris_gcompris.c
+++ games/gcompris/files/patch-src_gcompris_gcompris.c
@@ -1,13 +1,13 @@
---- src/gcompris/gcompris.c.orig	2008-08-04 14:21:07.000000000 -0400
-+++ src/gcompris/gcompris.c	2008-08-04 14:21:23.000000000 -0400
+--- src/gcompris/gcompris.c.orig	2008-07-04 17:35:40.000000000 -0400
++++ src/gcompris/gcompris.c	2008-08-15 02:05:47.000000000 -0400
 @@ -1127,11 +1127,11 @@ static void load_properties ()
  #ifdef NSBUNDLE
    exec_prefix = gcompris_nsbundle_resource ();
  #else
--  exec_prefix = gbr_find_exe_dir("");
+-  exec_prefix = gbr_find_exe_dir(NULL);
 +  exec_prefix = gbr_find_exe_dir("%%PREFIX%%");
  #endif
-   g_warning("exec_prefix %s\n", exec_prefix);
+   g_warning("exec_prefix %s\n", (exec_prefix==NULL ? "NONE" : exec_prefix));
  
 -  prefix_dir = gbr_find_prefix(NULL);
 +  prefix_dir = gbr_find_prefix("%%PREFIX%%");
