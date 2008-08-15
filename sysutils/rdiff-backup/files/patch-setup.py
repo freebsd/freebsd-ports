4ab8e99ab21c1b4a56718aad1f851970dad96fd6
vd <vd@FreeBSD.org>
2008-08-15 12:23:00 +0000

    * Copy sysutils/rdiff-backup to sysutils/rdiff-backup10 * Upgrade sysutils/rdiff-backup from 1.0.5 to 1.2.0 * Minor changes in sysutils/rdiff-backup-devel in CONFLICTS and pkg-descr
    PR:		ports/126313, ports/126314, ports/126315 (based on)
    Submitted by:	Peter Schuller <peter.schuller@infidyne.com>

--- sysutils/rdiff-backup/files/patch-setup.py
+++ sysutils/rdiff-backup/files/patch-setup.py
@@ -1,8 +1,8 @@
---- setup.py.orig	Thu Jun 26 15:27:30 2003
-+++ setup.py	Thu Jul  3 15:09:19 2003
-@@ -41,6 +41,10 @@
- 		if '-lrsync' in LIBS:
- 			libname = []
+--- setup.py.orig	2008-08-06 21:24:28.756047734 +0200
++++ setup.py	2008-08-06 21:24:18.122056264 +0200
+@@ -58,6 +58,10 @@
+ 				'zipfile': None
+ 			})
  
 +LOCALBASE = os.getenv("LOCALBASE", "/usr/local")
 +include_dirs = ["%s/include" % LOCALBASE]
@@ -11,20 +11,21 @@
  setup(name="rdiff-backup",
  	  version=version_string,
  	  description="Local/remote mirroring+incremental backup",
-@@ -51,12 +55,9 @@
+@@ -68,14 +72,8 @@
  	  ext_modules = [Extension("rdiff_backup.C", ["cmodule.c"]),
  					 Extension("rdiff_backup._librsync",
  							   ["_librsyncmodule.c"],
 -							   include_dirs=incdir_list,
 -							   library_dirs=libdir_list,
--							   libraries=libname,
--							   extra_link_args=lflags_arg)],
-+							   libraries=["rsync"],
-+                                                           include_dirs=include_dirs,
-+                                                           library_dirs=library_dirs)],
- 	  scripts = ['rdiff-backup'],
--	  data_files = [('share/man/man1', ['rdiff-backup.1']),
++							   include_dirs=include_dirs,
++							   library_dirs=library_dirs,
+ 							   libraries=libname,
+ 							   extra_link_args=lflags_arg)],
+-	  scripts = ['rdiff-backup', 'rdiff-backup-statistics'],
+-	  data_files = [('share/man/man1', ['rdiff-backup.1',
+-										'rdiff-backup-statistics.1']),
 -					('share/doc/rdiff-backup-%s' % (version_string,),
--					 ['CHANGELOG', 'COPYING', 'README', 'FAQ.html'])])
-+)
- 
+-					 ['CHANGELOG', 'COPYING', 'README', 'FAQ.html'])],
+-					**extra_options)
+-
++	  scripts = ['rdiff-backup', 'rdiff-backup-statistics'])
