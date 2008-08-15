718c5ef4c574df41050ce31a372df6bcd9ddef61
Edwin Groothuis <edwin@FreeBSD.org>
2008-08-15 00:42:08 +0000

    [MAINTAINER] lang/polyml: 5.1 -> 5.2
    PR:		ports/126068
    Submitted by:	Timothy Bourke <timbob@bigpond.com>

--- lang/polyml/files/patch-libpolyml-sighandler.cpp
+++ lang/polyml/files/patch-libpolyml-sighandler.cpp
@@ -1,6 +1,6 @@
---- libpolyml/sighandler.cpp.orig	2008-04-05 16:03:20.000000000 +1100
-+++ libpolyml/sighandler.cpp	2008-04-05 16:03:29.000000000 +1100
-@@ -381,7 +381,7 @@
+--- libpolyml/sighandler.cpp.orig	2008-02-28 00:20:51.000000000 +1100
++++ libpolyml/sighandler.cpp	2008-07-27 21:26:41.000000000 +1000
+@@ -357,7 +357,7 @@
      struct sigaltstack ex_stack;
  #endif
      memset(&ex_stack, 0, sizeof(ex_stack));
@@ -9,3 +9,12 @@
      ex_stack.ss_size  = SIGSTKSZ;
      ex_stack.ss_flags = 0; /* not SS_DISABLE */
      int sigaltstack_result = sigaltstack(&ex_stack, NULL);
+@@ -526,7 +526,7 @@
+     pthread_attr_init(&attrs);
+     pthread_attr_setdetachstate(&attrs, PTHREAD_CREATE_DETACHED);
+ #ifdef PTHREAD_STACK_MIN
+-    pthread_attr_setstacksize(&attrs, PTHREAD_STACK_MIN); // Only small stack.
++    pthread_attr_setstacksize(&attrs, PTHREAD_STACK_MIN + 100); // Only small stack.
+ #endif
+     pthread_create(&detectionThreadId, &attrs, SignalDetectionThread, 0);
+     pthread_attr_destroy(&attrs);
