ce9845cb74d6afb60fd5bfc6005da7cf4f191ddd
marcus <marcus@FreeBSD.org>
2008-08-16 19:04:25 +0000

    Update to 0.16.

--- devel/py-xdg/files/patch-xdg::BaseDirectory.py
+++ devel/py-xdg/files/patch-xdg::BaseDirectory.py
@@ -1,11 +1,18 @@
---- xdg/BaseDirectory.py.orig	Wed Mar 23 00:08:43 2005
-+++ xdg/BaseDirectory.py	Wed Mar 23 00:09:35 2005
-@@ -33,7 +33,7 @@
- 			os.path.join(_home, '.local', 'share'))
+--- xdg/BaseDirectory.py.orig	2008-03-02 08:56:26.000000000 -0500
++++ xdg/BaseDirectory.py	2008-08-16 14:56:01.000000000 -0400
+@@ -33,13 +33,13 @@ xdg_data_home = os.environ.get('XDG_DATA
+             os.path.join(_home, '.local', 'share'))
  
  xdg_data_dirs = [xdg_data_home] + \
--	os.environ.get('XDG_DATA_DIRS', '/usr/local/share:/usr/share').split(':')
-+	os.environ.get('XDG_DATA_DIRS', '%%LOCALBASE%%/share').split(':')
+-    os.environ.get('XDG_DATA_DIRS', '/usr/local/share:/usr/share').split(':')
++    os.environ.get('XDG_DATA_DIRS', '%%LOCALBASE%%/share').split(':')
  
  xdg_config_home = os.environ.get('XDG_CONFIG_HOME',
- 			os.path.join(_home, '.config'))
+             os.path.join(_home, '.config'))
+ 
+ xdg_config_dirs = [xdg_config_home] + \
+-    os.environ.get('XDG_CONFIG_DIRS', '/etc/xdg').split(':')
++    os.environ.get('XDG_CONFIG_DIRS', '%%LOCALBASE%%/etc/xdg').split(':')
+ 
+ xdg_cache_home = os.environ.get('XDG_CACHE_HOME',
+             os.path.join(_home, '.cache'))
--- devel/py-xdg/files/patch-xdg_Menu.py
+++ devel/py-xdg/files/patch-xdg_Menu.py
@@ -1,13 +1,12 @@
---- xdg/Menu.py.orig	Thu Aug 11 07:37:48 2005
-+++ xdg/Menu.py	Sat Sep  2 05:10:19 2006
-@@ -494,6 +494,10 @@
- 	if not filename:
- 		filename = __getFileName("applications.menu")
+--- xdg/Menu.py.orig	2008-06-02 05:29:42.000000000 -0400
++++ xdg/Menu.py	2008-08-16 14:59:55.000000000 -0400
+@@ -495,6 +495,9 @@ def parse(filename=None):
+         filename = __getFileName("applications.menu")
  
-+	# use gnome if no filename given
-+	if not filename:
-+		filename = __getFileName("gnome-applications.menu")
+     if not filename:
++	filename = __getFilename("gnome-applications.menu")
 +
- 	if not filename:
- 		raise ParsingError('File not found', "/etc/xdg/menus/applications.menu")
++    if not filename:
+         raise ParsingError('File not found', "/etc/xdg/menus/applications.menu")
  
+     # check if it is a .menu file
