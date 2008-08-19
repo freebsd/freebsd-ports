d9aa6f5324e7b3537527baa181f944e80044b30c
Adam Weinberger <adamw@FreeBSD.org>
2008-08-19 23:10:55 +0000

    Re-roll the -awesome patch for the conky-1.6.1 release.

--- sysutils/conky-awesome/files/patch-src_conky.c
+++ sysutils/conky-awesome/files/patch-src_conky.c
@@ -1,9 +1,9 @@
---- src/conky.c.orig	2008-05-27 00:43:05.000000000 -0400
-+++ src/conky.c	2008-05-27 00:43:36.000000000 -0400
-@@ -6513,7 +6513,7 @@ static void draw_string(const char *s)
+--- src/conky.c.orig	2008-08-19 16:02:53.000000000 -0700
++++ src/conky.c	2008-08-19 16:03:36.000000000 -0700
+@@ -6672,7 +6672,7 @@ static void draw_string(const char *s)
  
  	width_of_s = get_string_width(s);
- 	if (out_to_console) {
+ 	if ((output_methods & TO_STDOUT) && draw_mode == FG) {
 -		printf("%s\n", s);
 +		printf("%s\n\n", s);
  		fflush(stdout);	/* output immediately, don't buffer */
