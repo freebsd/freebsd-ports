a0b98dcc4df939cb06e3c91a0d1547ca8a17e072
amdmi3 <amdmi3@FreeBSD.org>
2008-08-21 19:25:33 +0000

    flvtool++ is a tool for hinting and manipulating the metadata of FLV files. It was originally created for Facebook's Video project (http://facebook.com/video/) for fast video hinting. It is loosely based on the Ruby FLVTool2, but is written in C++ for performance reasons.
    WWW:	http://mirror.facebook.com/facebook/flvtool++/
    
    PR:		ports/126336
    Submitted by:	Gea-Suan Lin <gslin at gslin dot org>

---
+++ multimedia/flvtool++/files/patch-AMFData.h
@@ -0,0 +1,10 @@
+--- AMFData.h.orig	2008-08-07 21:59:28.000000000 +0800
++++ AMFData.h	2008-08-07 21:59:34.000000000 +0800
+@@ -8,7 +8,6 @@
+ #pragma once
+ 
+ #include "common.h"
+-#include <byteswap.h>
+ #include <float.h>
+ #include <math.h>
+ #include <netinet/in.h>
