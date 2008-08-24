f2b4b28bb2712dd709b74897220f0cf0d9865644
Jun Kuriyama <kuriyama@FreeBSD.org>
2008-08-24 12:24:47 +0000

    - Upgrade to 0.11.1.ja1 and catch up to www/trac port. - default_charset in mimeview was changed to cp932 (previously I   patched from iso-8859-15 to utf-8).
    PR:		ports/126088 (no patch!)
    Submitted by:	nrg milk <nrgmilk@gmail.com>

--- japanese/trac/files/patch-notification.py
+++ japanese/trac/files/patch-notification.py
@@ -1,28 +1,28 @@
---- trac/notification.py.orig	Sat Dec  9 19:06:15 2006
-+++ trac/notification.py	Tue Mar  6 16:31:37 2007
-@@ -218,6 +218,7 @@
+--- trac/notification.py.orig	2008-08-17 14:28:38.000000000 +0900
++++ trac/notification.py	2008-08-24 18:30:45.000000000 +0900
+@@ -248,6 +248,7 @@
      def format_header(self, key, name, email=None):
          from email.Header import Header
          maxlength = MAXHEADERLEN-(len(key)+2)
-+	header = None
++        header = None
          # Do not sent ridiculous short headers
          if maxlength < 10:
-             raise TracError, "Header length is too short"
-@@ -225,6 +226,13 @@
+             raise TracError(_("Header length is too short"))
+@@ -255,6 +256,13 @@
              tmp = name.encode('ascii')
              header = Header(tmp, 'ascii', maxlinelen=maxlength)
          except UnicodeEncodeError:
-+	  if self.env.config.get('notification', 'mime_encoding').lower() == 'none':
-+	    try:
-+		tmp = name.encode('iso-2022-jp')
-+		header = Header(tmp, 'iso-2022-jp', maxlinelen=maxlength)
-+	    except UnicodeEncodeError:
-+		pass
-+	  if not header:
++          if self.env.config.get('notification', 'mime_encoding').lower() == 'none':
++            try:
++              tmp = name.encode('iso-2022-jp')
++              header = Header(tmp, 'iso-2022-jp', maxlinelen=maxlength)
++            except UnicodeEncodeError:
++              pass
++          if not header:
              header = Header(name, self._charset, maxlinelen=maxlength)
          if not email:
              return header
-@@ -343,12 +351,17 @@
+@@ -386,12 +394,17 @@
                  headers['To'] = ', '.join(toaddrs)
          if pcc:
              headers['Cc'] = ', '.join(pcc)
@@ -33,11 +33,11 @@
              try:
                  dummy = body.encode('ascii')
              except UnicodeDecodeError:
-+	      try:
-+		dummy = unicode(body, 'utf8');
-+		body = dummy.encode('iso-2022-jp')
-+		self._charset.output_charset = 'iso-2022-jp'
-+	      except UnicodeDecodeError:
-                 raise TracError, "Ticket contains non-Ascii chars. " \
-                                  "Please change encoding setting"
++             try:
++              dummy = unicode(body, 'utf8')
++              body = dummy.encode('iso-2022-jp')
++              self._charset.output_charset = 'iso-2022-jp'
++             except UnicodeDecodeError:
+                 raise TracError(_("Ticket contains non-ASCII chars. " \
+                                   "Please change encoding setting"))
          msg = MIMEText(body, 'plain')
