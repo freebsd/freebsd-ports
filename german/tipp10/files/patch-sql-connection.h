3ba5b8b6b5f6b3029e2d4ca4a287f2214d1fff5e
rafan <rafan@FreeBSD.org>
2008-08-24 08:55:09 +0000

    - Fix readonly database problem - Bump PORTREVISION
    PR:		ports/126767
    Submitted by:	Heiner <h.eichmann at gmx.de> (maintainer)

---
+++ german/tipp10/files/patch-sql-connection.h
@@ -0,0 +1,18 @@
+--- ./sql/connection.h.orig	2007-10-02 20:22:22.000000000 +0800
++++ ./sql/connection.h	2008-08-24 16:25:30.000000000 +0800
+@@ -170,6 +170,7 @@
+ 				// -> copy database to user home dir
+ 				QFile file(QCoreApplication::applicationDirPath() + "/" + dbNameTemplate);
+ 				if (file.copy(dbPath)) {
++ 					QFile::setPermissions(dbPath, QFile::permissions(dbPath) | QFile::WriteUser);
+ 					dbExist = true;
+ 				} else {
+ 					ErrorMessage *errorMessage = new ErrorMessage();
+@@ -201,6 +202,7 @@
+ 				dir.cd("tipp10");
+ 				QFile file(dbPath);
+ 				if (file.copy(dir.path() + "/" + dbNameUser)) {
++ 					QFile::setPermissions(dir.path() + "/" + dbNameUser, QFile::permissions(dir.path() + "/" + dbNameUser) | QFile::WriteUser);
+ 					dbPath = dir.path() + "/" + dbNameUser;
+ 				} else {
+ 					ErrorMessage *errorMessage = new ErrorMessage();
