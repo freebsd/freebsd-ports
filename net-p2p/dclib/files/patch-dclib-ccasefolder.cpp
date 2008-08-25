2243ede0af95528c4066b88e93d2dffe8e9c9a82
Pietro Cerutti <gahr@FreeBSD.org>
2008-08-25 23:08:31 +0000

    - Update to 0.3.19

---
+++ net-p2p/dclib/files/patch-dclib-ccasefolder.cpp
@@ -0,0 +1,11 @@
+--- dclib/ccasefolder.cpp.orig	2008-08-12 01:03:30.000000000 +0200
++++ dclib/ccasefolder.cpp	2008-08-12 01:04:03.000000000 +0200
+@@ -22,7 +22,7 @@
+ #endif
+ 
+ #ifndef ICONV_CONST
+-#define ICONV_CONST
++#define ICONV_CONST const
+ #endif
+ 
+ /*
