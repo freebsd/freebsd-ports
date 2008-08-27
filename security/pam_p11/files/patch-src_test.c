0928cb018d8a965fca4516d8275d86a5859e5a19
Alex Dupre <ale@FreeBSD.org>
2008-08-27 16:02:44 +0000

    Update to 0.1.5 release.

--- security/pam_p11/files/patch-src_test.c
+++ security/pam_p11/files/patch-src_test.c
@@ -1,11 +1,10 @@
---- src/test.c.orig	Thu Jun  1 15:13:06 2006
-+++ src/test.c	Thu Jun  1 15:13:15 2006
-@@ -2,7 +2,7 @@
+--- src/test.c.orig	2008-08-26 12:37:37.000000000 +0200
++++ src/test.c	2008-08-26 12:37:45.000000000 +0200
+@@ -2,7 +2,6 @@
  #include <stdlib.h>
  
  #include <security/pam_appl.h>
 -#include <security/pam_misc.h>
-+#include <security/pam_modules.h>
+ #include <security/pam_modules.h>
  
  int main(int argc, char **argv)
- {
