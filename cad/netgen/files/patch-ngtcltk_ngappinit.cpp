00ba7d8af48180e63aebd5970ade45b4534f6b72
rafan <rafan@FreeBSD.org>
2008-09-01 15:21:14 +0000

    - Fix build by comment out some code according to README.INSTALL

--- cad/netgen/files/patch-ngtcltk_ngappinit.cpp
+++ cad/netgen/files/patch-ngtcltk_ngappinit.cpp
@@ -1,5 +1,5 @@
---- ngtcltk/ngappinit.cpp.orig	Wed Nov 17 10:43:35 2004
-+++ ngtcltk/ngappinit.cpp	Wed Nov 17 21:57:41 2004
+--- ngtcltk/ngappinit.cpp.orig	2004-11-17 17:43:35.000000000 +0800
++++ ngtcltk/ngappinit.cpp	2008-09-01 22:40:22.000000000 +0800
 @@ -3,10 +3,12 @@
    This file is a modification of tkAppInit.c from the Tcl/Tk package
  */
@@ -14,7 +14,7 @@
  
  namespace netgen
  {
-@@ -26,7 +28,5 @@
+@@ -26,8 +28,6 @@
   * The following variable is a special hack that is needed in order for
   * Sun shared libraries to be used for Tcl.
   */
@@ -22,7 +22,8 @@
 -int *tclDummyMathPtr = (int *) matherr;
  
  
-@@ -194,8 +196,9 @@
+ /*
+@@ -194,8 +194,9 @@
    // For adding an application, parse the file here,
    // and call the init-procedure below
    // #define DEMOAPP
@@ -33,3 +34,12 @@
  #endif
  
  #ifdef ADDON
+@@ -316,7 +317,7 @@
+   // #undef NGSOLVE
+ 
+ 
+-#ifdef NGSOLVE
++#if 0
+ 
+   extern int NGSolve_Init (Tcl_Interp * interp);
+   if (NGSolve_Init(interp) == TCL_ERROR) 
