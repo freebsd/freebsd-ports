3ab82e7420c35b294e6a42ae8ad0847cbb03ab8a
lippe <lippe@FreeBSD.org>
2008-09-02 21:55:40 +0000

    - Update to 2.3. - Move pkg-message to FILESDIR.
    PR:		ports/127025
    Submitted by:	Douglas William Thrift <douglas@douglasthrift.net>
    Approved by:	Douglas William Thrift <douglas@douglasthrift.net> (maintainer)

--- www/mod_wsgi/files/patch-mod_wsgi.c
+++ www/mod_wsgi/files/patch-mod_wsgi.c
@@ -1,20 +1,13 @@
---- mod_wsgi.c.orig	2008-06-18 16:35:40.000000000 +0200
-+++ mod_wsgi.c	2008-07-19 19:41:18.000000000 +0200
-@@ -7771,6 +7771,9 @@
-      * is restricted, need to truncate display name if too long.
-      */
+Index: mod_wsgi.c
+===================================================================
+--- mod_wsgi.c	(revision 1007)
++++ mod_wsgi.c	(revision 1008)
+@@ -245,7 +245,7 @@
+ /* Version and module information. */
  
-+#ifdef __FreeBSD__
-+    setproctitle(display_name);
-+#else
-     argv0 = (char*)wsgi_server->process->argv[0];
+ #define MOD_WSGI_MAJORVERSION_NUMBER 2
+-#define MOD_WSGI_MINORVERSION_NUMBER 2
++#define MOD_WSGI_MINORVERSION_NUMBER 3
+ #define MOD_WSGI_VERSION_STRING "2.3"
  
-     dlen = strlen(argv0);
-@@ -7782,6 +7785,7 @@
-         memcpy(argv0, display_name, slen);
-     else
-         memcpy(argv0, display_name, dlen);
-+#endif
- }
- 
- static void wsgi_setup_access(WSGIDaemonProcess *daemon)
+ #if AP_SERVER_MAJORVERSION_NUMBER < 2
