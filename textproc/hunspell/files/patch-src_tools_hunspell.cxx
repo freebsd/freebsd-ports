fac8ce0a26951026374565d51ee8c2633f7e438e
Mikhail Teterin <mi@FreeBSD.org>
2008-09-03 18:54:24 +0000

    Upgrade from 1.2.2b to 1.2.7.
    Install header-files, which make the port's shared library usable by
    other applications -- such as, most importantly, OpenOffice.
    
    While here, ensure, the already installed -lintl is used, and configure
    in the use of readline too.
    
    Rephrase COMMENT to ease the confusion, that the port is only of
    interest to Hungarian-speakers. It is not -- the software is an improved
    version of myspell and can work with any dictionary.
    
    Approved by:	maintainer

--- textproc/hunspell/files/patch-src_tools_hunspell.cxx
+++ textproc/hunspell/files/patch-src_tools_hunspell.cxx
@@ -1,16 +1,14 @@
-
-$FreeBSD$
-
---- src/tools/hunspell.cxx.orig
-+++ src/tools/hunspell.cxx
-@@ -59,16 +59,12 @@
- #include "manparser.hxx"
- #include "firstparser.hxx"
- 
--#define LIBDIR "/usr/share/hunspell:/usr/share/myspell"
-+#define LIBDIR "%%PREFIX%%/share/hunspell:%%PREFIX%%/share/myspell"
- #define USEROOODIR ".openoffice.org2/user/wordbook"
--#define OOODIR "/opt/openoffice.org2.3/share/dict/ooo:" \
+--- src/tools/hunspell.cxx	2008-08-15 06:06:33.000000000 -0400
++++ src/tools/hunspell.cxx	2008-08-26 06:24:36.000000000 -0400
+@@ -70,18 +70,12 @@
+     ".openoffice.org2/user/wordbook:" \
+     ".openoffice.org2.0/user/wordbook"
+ #define OOODIR \
+-    "/opt/openoffice.org/basis3.0/share/dict/ooo:" \
+-    "/usr/lib/openoffice.org/basis3.0/share/dict/ooo:" \
+-    "/opt/openoffice.org2.4/share/dict/ooo:" \
+-    "/usr/lib/openoffice.org2.4/share/dict/ooo:" \
+-    "/opt/openoffice.org2.3/share/dict/ooo:" \
 -    "/usr/lib/openoffice.org2.3/share/dict/ooo:" \
 -    "/opt/openoffice.org2.2/share/dict/ooo:" \
 -    "/usr/lib/openoffice.org2.2/share/dict/ooo:" \
@@ -18,10 +16,12 @@ $FreeBSD$
 -    "/usr/lib/openoffice.org2.1/share/dict/ooo:" \
 -    "/opt/openoffice.org2.0/share/dict/ooo:" \
 -    "/usr/lib/openoffice.org2.0/share/dict/ooo"
-+#define OOODIR "%%PREFIX%%/openoffice.org-2.3/share/dict/ooo:" \
-+    "%%PREFIX%%/openoffice.org-2.2/share/dict/ooo:" \
-+    "%%PREFIX%%/openoffice.org-2.1/share/dict/ooo:" \
-+    "%%PREFIX%%/openoffice.org2.0/share/dict/ooo"
++	"%%PREFIX%%/openoffice.org-DEV300_m29/share/dict/ooo:"	\
++	"%%PREFIX%%/openoffice.org-3.0.0.b2/share/dict/ooo:"	\
++	"%%PREFIX%%/openoffice.org-2.4/share/dict/ooo:"	\
++	"%%PREFIX%%/openoffice.org-2.2/share/dict/ooo:"	\
++	"%%PREFIX%%/openoffice.org-2.1/share/dict/ooo:"	\
++	"%%PREFIX%%/openoffice.org2.0/share/dict/ooo"
  #define HOME getenv("HOME")
  #define DICBASENAME ".hunspell_"
  #define LOGFILE "/tmp/hunspell.log"
