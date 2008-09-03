dd683bc92a4ada3e65baaa9711c1f0d9ed7fead4
amdmi3 <amdmi3@FreeBSD.org>
2008-09-03 01:45:10 +0000

    - Fix a typo on this port that enables it to build again. - Label NOT_FOR AMD64 since it coredumps. - Sync the patches with those on CVS. - No functional change.
    PR:		126037
    Submitted by:	"Pedro F. Giffuni" <pfgshield-freebsd at yahoo dot com>

--- graphics/geomview/files/patch-streampool.h
+++ graphics/geomview/files/patch-streampool.h
@@ -1,11 +1,11 @@
---- src/lib/oogl/refcomm/streampool.h.orig	Mon Aug 12 17:15:10 2002
-+++ src/lib/oogl/refcomm/streampool.h	Mon Aug 12 17:15:15 2002
-@@ -29,7 +29,7 @@
- #include "config.h"
- #endif
- 
--#ifdef AIX
-+#if AIX
- # include <sys/select.h>  /* RS-6000 needs this */
- #endif
- 
+--- src/lib/oogl/refcomm/streampool.h	2006/07/14 17:47:21	1.4
++++ src/lib/oogl/refcomm/streampool.h	2006/07/15 19:06:00	1.5
+@@ -116,7 +116,7 @@
+ extern IOBFILE *PoolInputFile(Pool *);
+ extern FILE *PoolOutputFile(Pool *);
+ extern void  PoolDoReread(Pool *);
+-extern void PoolClose(register Pool *p);
++extern void PoolClose(Pool *p);
+ extern int   PoolOType(Pool *, int otype);
+ extern void  PoolSetOType(Pool *, int otype);
+ extern Pool *PoolByName(char *name);
