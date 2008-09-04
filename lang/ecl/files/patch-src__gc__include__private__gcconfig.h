e7d168334baa6d2f1b23cccb25cdcd985b6222a0
pav <pav@FreeBSD.org>
2008-09-04 20:07:50 +0000

    - Update to 0.9l
    PR:		ports/126920
    Submitted by:	fulvio ciriaco <oivulf@gmail.com>

--- lang/ecl/files/patch-src__gc__include__private__gcconfig.h
+++ lang/ecl/files/patch-src__gc__include__private__gcconfig.h
@@ -1,6 +1,6 @@
---- ./src/gc/include/private/gcconfig.h.orig	Fri Aug 12 13:26:42 2005
-+++ ./src/gc/include/private/gcconfig.h	Sat Jun 24 22:58:57 2006
-@@ -318,10 +318,6 @@
+--- src/gc/include/private/gcconfig.h.orig	2007-03-28 09:59:40.000000000 +0200
++++ src/gc/include/private/gcconfig.h	2008-08-27 16:27:54.000000000 +0200
+@@ -330,10 +330,6 @@
  #   define OPENBSD
  #   define mach_type_known
  # endif
@@ -11,7 +11,7 @@
  # if defined(__NetBSD__) && (defined(i386) || defined(__i386__))
  #   define I386
  #   define mach_type_known
-@@ -330,10 +326,24 @@
+@@ -342,10 +338,24 @@
  #    define X86_64
  #    define mach_type_known
  # endif
@@ -40,7 +40,7 @@
  # if defined(bsdi) && (defined(i386) || defined(__i386__))
  #    define I386
  #    define BSDI
-@@ -979,6 +989,9 @@
+@@ -1017,6 +1027,9 @@
  #	ifdef __ELF__
  #	    define DYNAMIC_LOADING
  #	endif
@@ -50,32 +50,10 @@
  	extern char etext[];
  	extern char edata[];
  	extern char end[];
-@@ -1215,15 +1228,15 @@
+@@ -2009,6 +2022,23 @@
  #   endif
  #   ifdef FREEBSD
  #	define OS_TYPE "FREEBSD"
--#	ifndef GC_FREEBSD_THREADS
--#	    define MPROTECT_VDB
--#	endif
- #	define SIG_SUSPEND SIGUSR1
- #	define SIG_THR_RESTART SIGUSR2
- #	define FREEBSD_STACKBOTTOM
- #	ifdef __ELF__
- #	    define DYNAMIC_LOADING
- #	endif
-+#	ifndef GC_FREEBSD_THREADS
-+#	    define MPROTECT_VDB
-+#	endif
- 	extern char etext[];
- 	extern char * GC_FreeBSDGetDataStart();
- #	define DATASTART GC_FreeBSDGetDataStart(0x1000, &etext)
-@@ -1926,6 +1939,23 @@
- #	    define PREFETCH(x) __builtin_prefetch((x), 0, 0)
- #	    define PREFETCH_FOR_WRITE(x) __builtin_prefetch((x), 1)
- #	endif
-+#   endif
-+#   ifdef FREEBSD
-+#	define OS_TYPE "FREEBSD"
 +#	define SIG_SUSPEND SIGUSR1
 +#	define SIG_THR_RESTART SIGUSR2
 +#	define FREEBSD_STACKBOTTOM
@@ -90,6 +68,9 @@
 +#	define DATAEND (GC_find_limit (DATASTART, TRUE))
 +#	define DATASTART2 ((ptr_t)(&edata))
 +#	define DATAEND2 ((ptr_t)(&end))
- #   endif
- #   ifdef NETBSD
- #	define OS_TYPE "NETBSD"
++#   endif
++#   ifdef FREEBSD
++#	define OS_TYPE "FREEBSD"
+ #	ifndef GC_FREEBSD_THREADS
+ #	    define MPROTECT_VDB
+ #	endif
