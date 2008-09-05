d6dc2addd21df5c03c224921af6ff1c18f89e667
Martin Wilke <miwi@FreeBSD.org>
2008-09-05 14:16:32 +0000

    GUI program to semi-automatically download album cover images for a music collection. It will work through a specified music directory, and for each subdirectory will download a set of corresponding (best guess) album covers. The naming format of the subdirectories can be configured, as can the location to save the results, such as .desktop-entry files, folder.jpg files, mp3 tags, etc.
    WWW: http://www.unrealvoodoo.org/hiteck/projects/albumart/
    
    PR:		ports/126883
    Submitted by:	Timothy Bourke <timbob at bigpond.com>

---
+++ audio/albumart/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2008-08-27 21:01:10.000000000 +1000
++++ setup.py	2008-08-27 21:01:24.000000000 +1000
+@@ -51,7 +51,6 @@
+ 			'lib/albumart/yahoo/search/webservices.py',
+ 			'lib/albumart/yahoo/search/version.py',
+ 		]),
+-		('share/doc/albumart', ['README', 'TODO', 'CHANGELOG']),
+ 	]
+ )
+ 
