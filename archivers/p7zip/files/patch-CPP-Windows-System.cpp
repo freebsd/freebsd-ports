24d75a994446fc7d15c70f6fb39261164d749511
Juergen Lock <nox@FreeBSD.org>
2008-09-07 11:16:48 +0000

    Update to 4.58
    PR:		ports/127159
    Submitted by:	chinsan

--- archivers/p7zip/files/patch-CPP-Windows-System.cpp
+++ archivers/p7zip/files/patch-CPP-Windows-System.cpp
@@ -1,10 +1,11 @@
-Index: CPP/Windows/System.cpp
-@@ -35,7 +35,7 @@
- 		#elif defined (__FreeBSD__)
+--- CPP/Windows/System.cpp.orig	2008-09-07 02:29:34.000000000 +0800
++++ CPP/Windows/System.cpp	2008-09-07 02:30:23.000000000 +0800
+@@ -44,7 +44,7 @@
+ 		#elif defined (__FreeBSD__) || defined (__FreeBSD_kernel__)
  		UInt32 GetNumberOfProcessors() {
- 		  		int nbcpu = 1;
+ 		  	int nbcpu = 1;
 -			size_t value;
-+			int value;
++		  	int value;
  			size_t len = sizeof(value);
  			if (sysctlbyname("hw.ncpu", &value, &len, NULL, 0) == 0)
  				nbcpu = value;
