64c76d957fa5ec16865d05d26da8f39700ffd4e2
marcus <marcus@FreeBSD.org>
2008-09-08 02:25:14 +0000

    Update to 0.2.40.

--- audio/esound/files/patch-audio_oss.c
+++ audio/esound/files/patch-audio_oss.c
@@ -1,21 +1,15 @@
---- audio_oss.c.orig	2008-07-15 12:00:03.000000000 -0400
-+++ audio_oss.c	2008-07-17 13:56:25.000000000 -0400
-@@ -1,4 +1,5 @@
- #include "config.h"
-+#include <errno.h>
+--- audio_oss.c.orig	2008-07-29 14:07:50.000000000 -0400
++++ audio_oss.c	2008-09-07 22:22:15.000000000 -0400
+@@ -12,8 +13,14 @@
  
- #ifdef HAVE_MACHINE_SOUNDCARD_H
- #  include <machine/soundcard.h>
-@@ -10,9 +11,14 @@
- #  endif
- #endif
+ #include <errno.h>
  
 +#if defined(__sparc__) || defined(__powerpc__)
 +#define AFMT_S16_NE AFMT_S16_BE
 +#else
 +#define AFMT_S16_NE AFMT_S16_LE
 +#endif
- 
++
  /* FreeBSD uses a different identifier? what other BSDs? */
 -#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
 +#ifndef SNDCTL_DSP_SETDUPLEX
