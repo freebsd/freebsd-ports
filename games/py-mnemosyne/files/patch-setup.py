d3560e2560989ab085f8e8457bed2f19a2e9f674
Beech Rintoul <beech@FreeBSD.org>
2008-09-12 07:16:11 +0000

    - Patch setup.py to not install files during build - Unbreak
    Reported by:	pointyhat via pav
    Approved by:	portmgr (erwin)

--- games/py-mnemosyne/files/patch-setup.py
+++ games/py-mnemosyne/files/patch-setup.py
@@ -1,13 +1,12 @@
---- setup.py.orig	2007-12-27 10:09:54.000000000 -0900
-+++ setup.py	2008-01-14 08:59:26.000000000 -0900
-@@ -147,8 +147,8 @@
+--- setup.py.orig	2008-08-23 08:05:54.000000000 -0800
++++ setup.py	2008-09-11 20:19:12.000000000 -0800
+@@ -151,8 +151,7 @@
  else:
      base_path = os.path.join(sys.exec_prefix, "lib", "python"+sys.version[:3],
                               "site-packages","mnemosyne")
 -    data_files = [('/usr/share/applications', ['mnemosyne.desktop']),
 -                  ('/usr/share/icons', ['pixmaps/mnemosyne.png'])]
-+    data_files = [('/usr/local/share/applications', ['mnemosyne.desktop']),
-+                  ('/usr/local/share/icons', ['pixmaps/mnemosyne.png'])]
++    data_files = []
    
  pixmap_path = os.path.join(base_path, "pixmaps")
  util_path   = os.path.join(base_path, "util")
