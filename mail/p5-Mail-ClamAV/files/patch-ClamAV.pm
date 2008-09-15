7f573ee7c6f939615e0b7d3a7a5bd27d10730508
garga <garga@FreeBSD.org>
2008-09-15 19:56:38 +0000

    - Update security/clamav to 0.94 [1] and fix a remote DoS [2] - Chase libclamav version bump on all dependant ports - Bump necessary PORTREVISIONS - Fix some BROKEN messages from ports that were already broken with clamav-0.93 - Mark security/klamav as BROKEN since it doesn't build with clamav-0.94
    PR:		ports/127122 [1], ports/127310 [2]
    Submitted by:	Gary Palmer <freebsd-gnats@in-addr.com> [1], delphij [2]
    Approved by:	portmgr (pav)

--- mail/p5-Mail-ClamAV/files/patch-ClamAV.pm
+++ mail/p5-Mail-ClamAV/files/patch-ClamAV.pm
@@ -1,17 +1,26 @@
---- ./ClamAV.pm.orig	2007-02-20 05:42:16.000000000 -0800
-+++ ./ClamAV.pm	2008-05-02 17:22:26.382239887 -0700
-@@ -73,10 +73,6 @@
+--- ClamAV.pm.orig	2007-02-20 11:42:16.000000000 -0200
++++ ClamAV.pm	2008-09-13 12:02:25.000000000 -0300
+@@ -58,7 +58,6 @@
+     CL_EACCES
+     CL_ENULLARG
+     CL_ETMPFILE
+-    CL_EFSYNC
+     CL_EMEM
+     CL_EOPEN
+     CL_EMALFDB
+@@ -71,11 +70,6 @@
+     CL_EIO
+     CL_EFORMAT
      CL_ESUPPORT
-     CL_ELOCKDB
- 
+-    CL_ELOCKDB
+-
 -    CL_ENCINIT
 -    CL_ENCLOAD
 -    CL_ENCIO
--
+ 
      CL_SCAN_RAW
      CL_SCAN_ARCHIVE
-     CL_SCAN_MAIL
-@@ -88,7 +84,6 @@
+@@ -88,7 +82,6 @@
      CL_SCAN_MAILURL
      CL_SCAN_BLOCKMAX
      CL_SCAN_ALGORITHMIC
@@ -19,7 +28,7 @@
      CL_SCAN_PHISHING_BLOCKSSL
  
      CL_SCAN_PHISHING_BLOCKCLOAK
-@@ -169,12 +164,9 @@
+@@ -169,12 +162,9 @@
      VERSION  => $VERSION,
      PREFIX   => 'clamav_perl_',
      NAME     => "Mail::ClamAV",
@@ -35,7 +44,7 @@
  use Inline C => <<'END_OF_C';
  #include <stdio.h>
  #include <string.h>
-@@ -210,12 +202,10 @@
+@@ -210,12 +200,10 @@
  
      /* set defaults for limits */
      c->limits.maxreclevel = 5;
@@ -48,7 +57,7 @@
      c->limits.archivememlim = 1;
  
      if (S_ISDIR(st.st_mode)) {
-@@ -290,19 +280,6 @@
+@@ -290,19 +278,6 @@
      return SvClam(self)->limits.maxreclevel;
  }
  
@@ -68,7 +77,7 @@
  int clamav_perl_maxfiles(SV *self, ...)
  {
      Inline_Stack_Vars;
-@@ -329,19 +306,6 @@
+@@ -329,19 +304,6 @@
      return SvClam(self)->limits.maxfilesize;
  }
  
@@ -88,21 +97,31 @@
  int clamav_perl_archivememlim(SV *self, ...)
  {
      Inline_Stack_Vars;
-@@ -496,13 +460,7 @@
+@@ -481,7 +443,6 @@
+     if (strEQ("CL_EACCES", name)) return CL_EACCES;
+     if (strEQ("CL_ENULLARG", name)) return CL_ENULLARG;
+     if (strEQ("CL_ETMPFILE", name)) return CL_ETMPFILE;
+-    if (strEQ("CL_EFSYNC", name)) return CL_EFSYNC;
+     if (strEQ("CL_EMEM", name)) return CL_EMEM;
+     if (strEQ("CL_EOPEN", name)) return CL_EOPEN;
+     if (strEQ("CL_EMALFDB", name)) return CL_EMALFDB;
+@@ -494,15 +455,8 @@
+     if (strEQ("CL_EIO", name)) return CL_EIO;
+     if (strEQ("CL_EFORMAT", name)) return CL_EFORMAT;
      if (strEQ("CL_ESUPPORT", name)) return CL_ESUPPORT;
-     if (strEQ("CL_ELOCKDB", name)) return CL_ELOCKDB;
- 
+-    if (strEQ("CL_ELOCKDB", name)) return CL_ELOCKDB;
+-
 -    /* NodalCore */
 -    if (strEQ("CL_ENCINIT", name)) return CL_ENCINIT;
 -    if (strEQ("CL_ENCLOAD", name)) return CL_ENCLOAD;
 -    if (strEQ("CL_ENCIO", name)) return CL_ENCIO;
--
+ 
      /* db options */
 -    if (strEQ("CL_DB_NCORE", name)) return CL_DB_NCORE;
      if (strEQ("CL_DB_PHISHING", name)) return CL_DB_PHISHING;
      if (strEQ("CL_DB_ACONLY", name)) return CL_DB_ACONLY;
      if (strEQ("CL_DB_PHISHING_URLS", name)) return CL_DB_PHISHING_URLS;
-@@ -522,7 +480,6 @@
+@@ -522,7 +476,6 @@
      if (strEQ("CL_SCAN_MAILURL", name)) return CL_SCAN_MAILURL;
      if (strEQ("CL_SCAN_BLOCKMAX", name)) return CL_SCAN_BLOCKMAX;
      if (strEQ("CL_SCAN_ALGORITHMIC", name)) return CL_SCAN_ALGORITHMIC;
@@ -110,7 +129,7 @@
      if (strEQ("CL_SCAN_PHISHING_BLOCKSSL", name)) return CL_SCAN_PHISHING_BLOCKSSL;
      if (strEQ("CL_SCAN_PHISHING_BLOCKCLOAK", name)) return CL_SCAN_PHISHING_BLOCKCLOAK;
      if (strEQ("CL_SCAN_ELF", name)) return CL_SCAN_ELF;
-@@ -602,11 +559,9 @@
+@@ -602,11 +555,9 @@
  
      # Set some limits (only applies to scan())
      $c->maxreclevel(4);
@@ -122,7 +141,7 @@
  
      # Scan a filehandle (scandesc in clamav)
      # scan(FileHandle or path, Bitfield of options)
-@@ -701,10 +656,6 @@
+@@ -701,10 +652,6 @@
  
  Enable algorithmic detection of viruses. 
  
@@ -133,7 +152,29 @@
  =item CL_SCAN_PHISHING_BLOCKSSL
  
  Phishing module: always block SSL mismatches in URLs. 
-@@ -890,10 +841,6 @@
+@@ -793,10 +740,6 @@
+ 
+ tmpfile() failed 
+ 
+-=item CL_EFSYNC
+-
+-fsync() failed 
+-
+ =item CL_EMEM
+ 
+ memory allocation error 
+@@ -845,10 +788,6 @@
+ 
+ not supported data format 
+ 
+-=item CL_ELOCKDB
+-
+-can't lock DB directory 
+-
+ =item CL_ENCINIT
+ 
+ NodalCore initialization failed 
+@@ -890,10 +829,6 @@
  
  Sets the maximum recursion level into archives [default 5].
  
@@ -144,7 +185,7 @@
  =item maxfiles
  
  Maximum number of files that will be scanned [default 1000]. A value of zero
-@@ -904,12 +851,6 @@
+@@ -904,12 +839,6 @@
  Maximum file size that will be scanned in bytes [default 10M]. A value of zero
  disables the check.
  
