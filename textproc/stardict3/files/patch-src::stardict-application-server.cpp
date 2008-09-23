c68f5cbbe3dbe72f026e2d869d6e2830d77c266f
edwin <edwin@FreeBSD.org>
2008-09-23 02:35:09 +0000

    [new port] Add textproc/stardict3
    	Add new port textproc/stardict3 (update stardict-2.x to
    	stardict-3.x).
    
    	StarDict is a Cross-Platform and international dictionary
    	written in Gtk2.  It has powerful features such as "Glob-style
    	pattern matching," "Scan selection word," "Fuzzy query,"
    	etc.
    
    	WWW: http://stardict.sourceforge.net/
    
    PR:		ports/125924
    Submitted by:	"Eric L. Chen" <d9364104@mail.nchu.edu.tw>

--- textproc/stardict3/files/patch-src::stardict-application-server.cpp
+++ textproc/stardict3/files/patch-src::stardict-application-server.cpp
@@ -1,6 +1,6 @@
 --- src/stardict-application-server.cpp.orig	Tue Sep 23 18:19:48 2003
 +++ src/stardict-application-server.cpp	Tue Jan 18 02:25:54 2005
-@@ -117,4 +117,4 @@
+@@ -119,4 +119,4 @@
          StardictApplicationServer,                    
          GNOME_Stardict_Application, 
          BONOBO_TYPE_OBJECT,           
