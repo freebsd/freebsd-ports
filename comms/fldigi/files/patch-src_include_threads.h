89dbce833f4bcda2ff8c3b4d9012315ee2b99fdc
db <db@FreeBSD.org>
2008-09-23 14:01:11 +0000

    - Update to 3.03

--- comms/fldigi/files/patch-src_include_threads.h
+++ comms/fldigi/files/patch-src_include_threads.h
@@ -1,5 +1,5 @@
---- src/include/threads.h.orig	2008-08-23 12:34:24.000000000 -0400
-+++ src/include/threads.h	2008-08-23 12:32:50.000000000 -0400
+--- src/include/threads.h.orig	2008-09-14 09:26:15.000000000 -0400
++++ src/include/threads.h	2008-09-23 08:50:48.000000000 -0400
 @@ -46,6 +46,7 @@
  #include <config.h>
  
@@ -8,7 +8,7 @@
  
  typedef pthread_t			Fl_Thread;
  typedef pthread_mutex_t		Fl_Mutex;
-@@ -87,7 +88,8 @@
+@@ -88,7 +89,8 @@
  	extern pthread_key_t thread_id_;
  #	define CREATE_THREAD_ID() pthread_key_create(&thread_id_, 0);
  #	define SET_THREAD_ID(x)   pthread_setspecific(thread_id_, (void *)(x))
