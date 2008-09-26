aa66cc641fef5ca99509ba3317d89bbce424cf39
Martin Matuska <mm@FreeBSD.org>
2008-09-26 08:34:46 +0000

    - Update to 1.09
    PR:		ports/127630
    Submitted by:	mm
    Approved by:	Gianmarco Giovannelli <gmarco@gufi.org> (maintainer)

--- sysutils/cpdup/files/patch-cpdup.c
+++ sysutils/cpdup/files/patch-cpdup.c
@@ -1,16 +1,13 @@
-
-$FreeBSD$
-
---- cpdup.c.orig	Thu Sep 21 06:09:28 2006
-+++ cpdup.c	Wed May 30 17:27:55 2007
-@@ -463,6 +463,11 @@
- 
-     if (hc_lstat(&SrcHost, spath, &st1) != 0)
- 	return(0);
+--- cpdup.c.orig	2008-04-14 07:40:51.000000000 +0200
++++ cpdup.c	2008-09-25 12:44:04.011824830 +0200
+@@ -634,6 +634,11 @@
+ 	r = 0;
+ 	goto done;
+     }
 +#ifdef SF_SNAPSHOT
 +    /* skip snapshot files because they're sparse and _huge_ */
 +    if (st1.st_flags & SF_SNAPSHOT)
-+	return(0);
++       return(0);
 +#endif
      st2.st_mode = 0;	/* in case lstat fails */
      st2.st_flags = 0;	/* in case lstat fails */
