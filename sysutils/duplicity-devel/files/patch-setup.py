d58a0dd9b0b2a13c916d6e2898dca142f7903f3e
vd <vd@FreeBSD.org>
2008-09-28 19:28:11 +0000

    Upgrade sysutils/duplicity from 0.4.12 to 0.5.02
    ChangeLog:
    http://www.nongnu.org/duplicity/CHANGELOG
    
    PR:		ports/127161 (with small fix)
    Submitted by:	peter.schuller@infidyne.com

--- sysutils/duplicity-devel/files/patch-setup.py
+++ sysutils/duplicity-devel/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	2007-12-07 17:28:51.000000000 +0200
-+++ setup.py	2007-12-12 14:51:55.000000000 +0200
+--- setup.py.orig	2008-09-21 17:21:44.000000000 +0300
++++ setup.py	2008-09-28 22:24:48.000000000 +0300
 @@ -21,6 +21,10 @@
  
  incdir_list = libdir_list = None 
@@ -9,16 +9,16 @@
 +libdir_list = ['%s/lib/' % LOCALBASE]
 +
  if os.name == 'posix':
- 	LIBRSYNC_DIR = os.environ.get('LIBRSYNC_DIR', '')
- 	args = sys.argv[:]
-@@ -47,9 +51,6 @@
- 							   include_dirs=incdir_list,
- 							   library_dirs=libdir_list,
- 							   libraries=["rsync"])],
--	  scripts = ['rdiffdir', 'duplicity'],
--	  data_files = [('share/man/man1', ['duplicity.1', 'rdiffdir.1']),
--					('share/doc/duplicity-%s' % version_string,
--					 ['COPYING', 'README', 'CHANGELOG'])])
-+	  scripts = ['rdiffdir', 'duplicity'])
+     LIBRSYNC_DIR = os.environ.get('LIBRSYNC_DIR', '')
+     args = sys.argv[:]
+@@ -48,9 +52,6 @@
+                                include_dirs=incdir_list,
+                                library_dirs=libdir_list,
+                                libraries=["rsync"])],
+-      scripts = ['rdiffdir', 'duplicity'],
+-      data_files = [('share/man/man1', ['duplicity.1', 'rdiffdir.1']),
+-                    ('share/doc/duplicity-%s' % version_string,
+-                     ['COPYING', 'README', 'CHANGELOG'])])
++      scripts = ['rdiffdir', 'duplicity'])
  
  
