94e670b38eb48e24b707342df4af99459dbc54d9
marcus <marcus@FreeBSD.org>
2008-09-29 04:33:08 +0000

    Update to 0.9.12.

--- audio/pulseaudio/files/patch-src_pulsecore_core-util.c
+++ audio/pulseaudio/files/patch-src_pulsecore_core-util.c
@@ -1,5 +1,5 @@
---- src/pulsecore/core-util.c.orig	2008-06-21 13:25:57.000000000 -0400
-+++ src/pulsecore/core-util.c	2008-07-26 17:01:01.000000000 -0400
+--- src/pulsecore/core-util.c.orig	2008-09-08 19:33:30.000000000 -0400
++++ src/pulsecore/core-util.c	2008-09-28 19:50:24.000000000 -0400
 @@ -37,6 +37,7 @@
  #include <time.h>
  #include <ctype.h>
@@ -8,15 +8,15 @@
  #include <sys/stat.h>
  #include <sys/time.h>
  #include <dirent.h>
-@@ -548,6 +549,7 @@ int pa_make_realtime(int rtprio) {
- #else
+@@ -567,6 +568,7 @@ int pa_make_realtime(int rtprio) {
+     errno = ENOTSUP;
      return -1;
  #endif
 +    ;
  }
  
  /* This is merely used for giving the user a hint. This is not correct
-@@ -1036,22 +1038,22 @@ int pa_check_in_group(gid_t g) {
+@@ -1131,22 +1133,22 @@ int pa_check_in_group(gid_t g) {
    (advisory on UNIX, mandatory on Windows) */
  int pa_lock_fd(int fd, int b) {
  #ifdef F_SETLKW
@@ -25,11 +25,11 @@
  
      /* Try a R/W lock first */
  
--    flock.l_type = b ? F_WRLCK : F_UNLCK;
+-    flock.l_type = (short) (b ? F_WRLCK : F_UNLCK);
 -    flock.l_whence = SEEK_SET;
 -    flock.l_start = 0;
 -    flock.l_len = 0;
-+    pa_flock.l_type = b ? F_WRLCK : F_UNLCK;
++    pa_flock.l_type = (short) (b ? F_WRLCK : F_UNLCK);
 +    pa_flock.l_whence = SEEK_SET;
 +    pa_flock.l_start = 0;
 +    pa_flock.l_len = 0;
@@ -47,7 +47,7 @@
              return 0;
      }
  
-@@ -1990,7 +1992,11 @@ int pa_reset_sigs(int except, ...) {
+@@ -2334,7 +2336,11 @@ int pa_reset_sigs(int except, ...) {
  int pa_reset_sigsv(const int except[]) {
      int sig;
  
