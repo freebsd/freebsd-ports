4e967735b2f411174fbcaf250dc5f8718d55fb92
Cheng-Lung Sung <clsung@FreeBSD.org>
2008-09-30 03:09:33 +0000

    This module is the Sybase extensions to Ruby.
    WWW: http://raa.ruby-lang.org/project/sybase-ctlib/
    
    PR:		ports/127734
    Submitted by:	Alexander Logvinov <ports at logvinov.com>

---
+++ databases/ruby-sybct/files/patch-extconf.rb
@@ -0,0 +1,17 @@
+--- extconf.rb.orig	2008-09-10 14:37:10.000000000 +1000
++++ extconf.rb	2008-09-10 14:38:20.000000000 +1000
+@@ -59,10 +59,10 @@
+ ###########################################################
+ 
+ ### Linux + Free TDS (freetds-0.64 or above) ################
+-# sybase = "/usr/local/freetds"
+-# $CFLAGS = "-g -Wall -DFREETDS -I#{sybase}/include"
+-# $LDFLAGS = " -L#{sybase}/lib " 
+-# $LOCAL_LIBS = "-lct  -lsybdb -ltds -rdynamic -ldl -lnsl -lm"
++ sybase = "/usr/local"
++ $CFLAGS = "-g -Wall -DFREETDS -I#{sybase}/include"
++ $LDFLAGS = " -L#{sybase}/lib " 
++ $LOCAL_LIBS = "-lct  -lsybdb -ltds -rdynamic -lm -liconv"
+ 
+ ### mac-osx-tiger + Free TDS (freetds-0.64 ) ################
+ # sybase = "/usr/local/freetds"
