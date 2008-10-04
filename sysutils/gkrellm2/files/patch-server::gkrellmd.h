b58bb7c83b79de7cec7e05718f03a4172b51f531
Hajimu UMEMOTO <ume@FreeBSD.org>
2008-10-04 15:00:55 +0000

    - Update to 2.3.2 - OPTIONS'fy

--- sysutils/gkrellm2/files/patch-server::gkrellmd.h
+++ sysutils/gkrellm2/files/patch-server::gkrellmd.h
@@ -1,17 +1,17 @@
 Index: server/gkrellmd.h
 diff -u server/gkrellmd.h.orig server/gkrellmd.h
---- server/gkrellmd.h.orig	Mon Apr 12 23:32:29 2004
-+++ server/gkrellmd.h	Tue Apr 13 17:16:02 2004
-@@ -105,8 +105,12 @@
- #endif
- 
- #if !defined(WIN32)
-+#if !defined(GKRELLMD_SYS_ETC)
- #define	GKRELLMD_SYS_ETC	"/etc"
-+#endif
-+#if 0
- #define GKRELLMD_LOCAL_ETC	"/usr/local/etc"
-+#endif
- #endif
+--- server/gkrellmd.h.orig	2008-10-04 06:52:48.000000000 +0900
++++ server/gkrellmd.h	2008-10-04 19:55:03.000000000 +0900
+@@ -118,8 +118,12 @@
+ 	#if !defined(GKRELLMD_SYSTEM_PLUGINS_DIR)
+ 		#define GKRELLMD_SYSTEM_PLUGINS_DIR	"/usr/lib/gkrellm2/plugins-gkrellmd"
+ 	#endif
++	#if !defined(GKRELLMD_SYS_ETC)
+ 	#define GKRELLMD_SYS_ETC	"/etc"
++	#endif
++	#if 0
+ 	#define GKRELLMD_LOCAL_ETC	"/usr/local/etc"
++	#endif
+ #endif // !defined(WIN32)
  
  
