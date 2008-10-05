74403e12bb092d734d907b58fa3e04e9c8c196cd
miwi <miwi@FreeBSD.org>
2008-10-05 14:41:50 +0000

    - Update to 1.3.1
    PR:		127852
    Submitted by:	Tobias Roth <ports@fsck.ch> (maintainer)

--- x11/slim/files/patch-app.cpp
+++ x11/slim/files/patch-app.cpp
@@ -1,19 +1,11 @@
---- app.cpp.orig	Sun Jul 15 16:09:28 2007
-+++ app.cpp	Wed Jul 18 20:28:27 2007
-@@ -135,6 +135,7 @@
-     ServerPID = -1;
-     testing = false;
-     mcookie = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-+    daemonmode = false;
-     Dpy = NULL;
- 
-     // Parse command line
-@@ -858,7 +859,7 @@
+--- app.cpp.orig	2008-10-04 13:45:58.000000000 +0200
++++ app.cpp	2008-10-04 13:46:07.000000000 +0200
+@@ -887,7 +887,7 @@
      }
  
      if (!hasVtSet && daemonmode) {
--        server[argc++] = "vt07";
-+        server[argc++] = "vt09";
+-        server[argc++] = (char*)"vt07";
++        server[argc++] = (char*)"vt09";
      }
      server[argc] = NULL;
  
