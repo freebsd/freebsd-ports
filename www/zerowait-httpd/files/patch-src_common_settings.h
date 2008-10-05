474f5cd212706f48c090cfa9f84c88ea933f32eb
Martin Wilke <miwi@FreeBSD.org>
2008-10-05 13:33:12 +0000

    - Update to 0.8d - Pass maintainership to submitter
    PR:		127781
    Submitted by:	Dennis Herrmann <adox@mcx2.org>

--- www/zerowait-httpd/files/patch-src_common_settings.h
+++ www/zerowait-httpd/files/patch-src_common_settings.h
@@ -1,6 +1,6 @@
---- src/common/settings.h.orig	Sat Apr 16 06:48:58 2005
-+++ src/common/settings.h	Wed Jun 22 13:35:16 2005
-@@ -37,18 +37,18 @@
+--- src/common/settings.h.orig	2008-02-04 12:42:36.000000000 +0100
++++ src/common/settings.h	2008-09-26 18:59:48.000000000 +0200
+@@ -40,15 +40,15 @@
  #ifndef ROOT
  #define ROOT "/var/0W"
  #endif
@@ -9,21 +9,17 @@
 -#define CMD  ROOT "/cmd"
 -#define LOGS ROOT "/logs"
 -#define TMP  ROOT "/tmp"
-+
 +#define CONF ROOT "/etc/0W-httpd/"
 +#define DATA ROOT "/0W-httpd"
-+#define CMD     ROOT "/0W-httpd/cmd"
++#define CMD  ROOT "/0W-httpd/cmd"
 +#define LOGS ROOT "/0W-httpd/logs"
-+#define TMP     ROOT "/0W-httpd/tmp"
++#define TMP  ROOT "/0W-httpd/tmp"
  #define PIDS DATA "/pids"
  #define TMPL DATA "/templates"
 -#define WWW  ROOT "/www"
 -#define HTML WWW  "/data"
-+#define WWW     ROOT "/0W-httpd"
-+#define HTML ROOT  "/0W-httpd/data"
++#define WWW  ROOT "/0W-httpd"
++#define HTML WWW  "/0W-httpd/data"
  
  #define RN "\r\n"
--
  
- #if __GLIBC__ == 2 && __GLIBC_MINOR__ >= 1
- #define HAVE_BACKTRACE
