08f403812c10d9461ddbcc04d5d099ae546f9151
Greg Lewis <glewis@FreeBSD.org>
2008-10-18 17:54:04 +0000

    . Properly scope the calls to clock_get* so that we use the system versions   if available rather than the local versions.  Fixes System.nanoTime().
    Reported by:	Kurt Miller

--- java/jdk16/files/patch-hotspot-vm-os_bsd.cpp
+++ java/jdk16/files/patch-hotspot-vm-os_bsd.cpp
@@ -11,3 +11,17 @@ $FreeBSD$
  #endif
  
  #define EXTENSIONS_DIR	"/lib/ext"
+@@ -1431,10 +1431,10 @@
+ void os::Bsd::clock_init() {
+   struct timespec res;
+   struct timespec tp;
+-  if (clock_getres(CLOCK_MONOTONIC, &res) == 0 &&
+-      clock_gettime(CLOCK_MONOTONIC, &tp)  == 0) {
++  if (::clock_getres(CLOCK_MONOTONIC, &res) == 0 &&
++      ::clock_gettime(CLOCK_MONOTONIC, &tp)  == 0) {
+     // yes, monotonic clock is supported
+-    _clock_gettime = clock_gettime;
++    _clock_gettime = ::clock_gettime;
+   }
+ }
+ #else
