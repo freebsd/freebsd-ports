7bf412c30db4602d1bdd09223072eafb53839a77
miwi <miwi@FreeBSD.org>
2008-10-19 14:59:04 +0000

    - Update to 0.10.2
    PR:		128077
    Submitted by:	Ashish Shukla <wahjava@gmail.com> (maintainer)

--- deskutils/google-gadgets/files/patch-extensions_gst__video__element_gadget__videosink.cc
+++ deskutils/google-gadgets/files/patch-extensions_gst__video__element_gadget__videosink.cc
@@ -1,22 +1,9 @@
 
 $FreeBSD$
 
---- extensions/gst_mediaplayer_element/gadget_videosink.cc.orig
-+++ extensions/gst_mediaplayer_element/gadget_videosink.cc
-@@ -17,6 +17,12 @@
- #include "gadget_videosink.h"
- #include <pthread.h>
- 
-+#if __GNUC__ < 4
-+#define CAST_HACK(x) (x)
-+#else
-+#define CAST_HACK(x) reinterpret_cast<x>
-+#endif
-+
- namespace ggadget {
- namespace gst {
- 
-@@ -454,10 +460,10 @@
+--- extensions/gst_video_element/gadget_videosink.cc.orig
++++ extensions/gst_video_element/gadget_videosink.cc
+@@ -459,10 +459,10 @@
        nom = gst_value_get_fraction_numerator(videosink->par_);
        den = gst_value_get_fraction_denominator(videosink->par_);
        gst_structure_set(structure, "pixel-aspect-ratio",
@@ -29,7 +16,7 @@ $FreeBSD$
      }
    }
  
-@@ -669,18 +675,18 @@
+@@ -672,18 +672,18 @@
        desired_caps = gst_caps_copy(caps);
        desired_struct = gst_caps_get_structure(desired_caps, 0);
  
@@ -44,7 +31,7 @@ $FreeBSD$
          den = gst_value_get_fraction_denominator(videosink->par_);
          gst_structure_set(desired_struct, "pixel-aspect-ratio",
 -                           GST_TYPE_FRACTION, nom, den, NULL);
-+                           GST_TYPE_FRACTION, nom, den, (gchar*)0);
++			  GST_TYPE_FRACTION, nom, den, (gchar*)0);
        } else {
          gst_structure_set(desired_struct, "pixel-aspect-ratio",
 -                          GST_TYPE_FRACTION, 1, 1, NULL);
@@ -52,16 +39,7 @@ $FreeBSD$
        }
  
        // see if peer accepts our new suggestion, if there is no peer, this
-@@ -859,7 +865,7 @@
-         g_value_transform(videosink->par_, value);
-       break;
-     case PROP_RECEIVE_IMAGE_HANDLER:
--      g_value_set_pointer(value, reinterpret_cast<void*>(&ReceiveImageHandler));
-+      g_value_set_pointer(value, CAST_HACK(void*)(&ReceiveImageHandler));
-       break;
-     default:
-       G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
-@@ -892,7 +898,7 @@
+@@ -895,7 +895,7 @@
    nom = gst_value_get_fraction_numerator(par_);
    den = gst_value_get_fraction_denominator(par_);
    gst_caps_set_simple(caps_, const_cast<gchar*>("pixel-aspect-ratio"),
--- deskutils/google-gadgets/files/patch-extensions_linux__system__framework_file__system.cc
+++ deskutils/google-gadgets/files/patch-extensions_linux__system__framework_file__system.cc
@@ -3,47 +3,25 @@ $FreeBSD$
 
 --- extensions/linux_system_framework/file_system.cc.orig
 +++ extensions/linux_system_framework/file_system.cc
-@@ -356,8 +356,8 @@
-     if (stat(path_.c_str(), &statbuf))
-       return Date(0);
+@@ -463,8 +463,8 @@
+   if (stat(path, &stat_value))
+     return Date(0);
  
--    return Date(statbuf.st_mtim.tv_sec * 1000
--                + statbuf.st_mtim.tv_nsec / 1000000);
-+    return Date(statbuf.st_mtimespec.tv_sec * 1000
-+                + statbuf.st_mtimespec.tv_nsec / 1000000);
-   }
+-  return Date(stat_value.st_mtim.tv_sec * 1000
+-              + stat_value.st_mtim.tv_nsec / 1000000);
++  return Date(statbuf.st_mtimespec.tv_sec * 1000
++              + statbuf.st_mtimespec.tv_nsec / 1000000);
+ }
  
-   virtual Date GetDateLastAccessed() {
-@@ -368,8 +368,8 @@
-     if (stat(path_.c_str(), &statbuf))
-       return Date(0);
+ static Date GetDateLastAccessed(const char *path) {
+@@ -476,8 +476,8 @@
+   if (stat(path, &stat_value))
+     return Date(0);
  
--    return Date(statbuf.st_atim.tv_sec * 1000
--                + statbuf.st_atim.tv_nsec / 1000000);
-+    return Date(statbuf.st_atimespec.tv_sec * 1000
-+                + statbuf.st_atimespec.tv_nsec / 1000000);
-   }
+-  return Date(stat_value.st_atim.tv_sec * 1000
+-              + stat_value.st_atim.tv_nsec / 1000000);
++  return Date(statbuf.st_atimespec.tv_sec * 1000
++              + statbuf.st_atimespec.tv_nsec / 1000000);
+ }
  
-   virtual int64_t GetSize() {
-@@ -646,8 +646,8 @@
-     if (stat(path_.c_str(), &statbuf))
-       return Date(0);
- 
--    return Date(statbuf.st_mtim.tv_sec * 1000
--                + statbuf.st_mtim.tv_nsec / 1000000);
-+    return Date(statbuf.st_mtimespec.tv_sec * 1000
-+                + statbuf.st_mtimespec.tv_nsec / 1000000);
-   }
- 
-   virtual Date GetDateLastAccessed() {
-@@ -658,8 +658,8 @@
-     if (stat(path_.c_str(), &statbuf))
-       return Date(0);
- 
--    return Date(statbuf.st_atim.tv_sec * 1000
--                + statbuf.st_atim.tv_nsec / 1000000);
-+    return Date(statbuf.st_atimespec.tv_sec * 1000
-+                + statbuf.st_atimespec.tv_nsec / 1000000);
-   }
- 
-   virtual std::string GetType() {
+ class Drive : public DriveInterface {
