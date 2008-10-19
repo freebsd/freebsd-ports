bedf41ba6f3d3f15b03b508bac7b58f3e63d2e6f
stefan <stefan@FreeBSD.org>
2008-10-19 08:09:02 +0000

    Fix a potential buffer overflow.
    PR:		128216
    Submitted by:	maintainer

---
+++ comms/qpage/files/patch-srvrsnpp.c
@@ -0,0 +1,28 @@
+--- srvrsnpp.c.orig	1998-10-25 14:55:05.000000000 -0500
++++ srvrsnpp.c	2008-10-18 18:09:44.175331511 -0400
+@@ -523,6 +523,7 @@
+ 	char		*errmsg;
+ 	char		*a;
+ 	char		*b;
++	char		*m;
+ 	int		i;
+ 	int		badarg;
+ 	int		gotpager;
+@@ -701,7 +702,16 @@
+ 
+ 				p->created = time(NULL);
+ 				(void)sprintf(buff, "%d", pagecount++);
+-				(void)strcat(p->messageid, buff);
++				m = (void *)malloc(sizeof(*m) * strlen(p->messageid) + sizeof(*m) * strlen(buff));
++				if ( m == NULL ) {
++					message("554 Message failed (out of memory)");
++					qpage_log(LOG_ERR, "snpp(): cannot allocate memory for p->messageid");
++					clear_page(p, TRUE);
++					break;
++				}
++				(void)sprintf(m, "%s%s", p->messageid, buff);
++				my_free(p->messageid);
++				p->messageid = m;
+ 
+ 				qpage_log(LOG_ALERT, "page submitted, id=%s, from=%s",
+ 					p->messageid,
