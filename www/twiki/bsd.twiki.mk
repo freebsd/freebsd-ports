377b33645e03173f38e16716d245bf40e46c9f23
Greg Larkin <glarkin@FreeBSD.org>
2008-10-20 02:10:51 +0000

    - Unforbid TWiki - Upgrade to 4.2.3 - Replace MASTER_SITES - Upgrade TWiki plugins to latest svn.twiki.org versions - Take maintainership of TWiki and existing plugins
    Reviewed by:	miwi
    Approved by:	erwin
    Approved by:	beech (mentor)
    Security:	9227dcaf-827f-11dd-9cd7-0050568452ac

--- www/twiki/bsd.twiki.mk
+++ www/twiki/bsd.twiki.mk
@@ -20,7 +20,8 @@ PKGNAMEPREFIX?=	twiki-
 PNAME=	${PORTNAME}
 FILESDIR?=	${.CURDIR}/../twiki/files
 .endif
-MASTER_SITES?=	CENKES/myports/twiki
+MASTER_SITES?=	http://www.sourcehosting.net/freebsd/distfiles/ \
+		LOCAL/glarkin
 CATEGORIES?=	www
 SVNURL?=	http://svn.twiki.org/svn/twiki/trunk/${PNAME}
 NO_BUILD=	yes
@@ -34,7 +35,7 @@ DIST_SUBDIR?=	twiki
 RUN_DEPENDS+=	${TWDEP:C/([^=<>]*)([=<>]*)(.*)/twiki-\1\20.0.\3:${PORTSDIR}\/www\/twiki-\1/}
 
 make-dist:
-	@${MKDIR} ${WRKDIR}/
+	@${INSTALL} -d ${WRKDIR}/
 	@cd ${WRKDIR}/ && svn export -r ${SVNREV} ${SVNURL} && \
 		${MV} ${PNAME} ${DISTNAME} && \
 		${FIND} . -type d -empty | ${SED} -e 's|$$|/.keep_me|' | \
