ea7159c637894a46d01fdb20e91418b3f53f6a6d
hrs <hrs@FreeBSD.org>
2008-10-26 19:35:08 +0000

    - Separate the server part and the library part from japanese/sj3, and   remove japanese/sj3. - Use localhost as the default listen address instead of INADDR_ANY. - Add "servername" directive to serverrc. - Fix incorrect descriptions in serverrc. - Take maintainership.

---
+++ japanese/sj3-server/files/post-patch-include-Const.h
@@ -0,0 +1,10 @@
+--- include/Const.h.orig	2008-10-26 23:35:29.000000000 +0900
++++ include/Const.h	2008-10-26 23:35:44.000000000 +0900
+@@ -68,6 +68,7 @@
+ #define	MaxClientNum		512
+ #define	LogOutFile		NULL	
+ #define	PortName		"sj3"
++#define ServerName              "localhost"
+ #ifdef TLI
+ #define LocalHost               "localhost"
+ #define ProtoName               "tcp"
