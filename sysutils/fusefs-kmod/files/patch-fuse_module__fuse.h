07e7ccf97afa440edf699e84c469104c68a40c74
Jung-uk Kim <jkim@FreeBSD.org>
2008-10-30 15:36:35 +0000

    Fix build on -CURRENT after the introduction of accmode_t and renaming of VOP_ACCESS 'a_mode' argument to 'a_accmode'.
    Reviewed by:	Anish Mistry <amistry at am-productions dot biz> (maintainer)

--- sysutils/fusefs-kmod/files/patch-fuse_module__fuse.h
+++ sysutils/fusefs-kmod/files/patch-fuse_module__fuse.h
@@ -1,6 +1,21 @@
---- fuse_module/fuse.h.orig	2008-09-26 12:40:20.000000000 -0400
-+++ fuse_module/fuse.h	2008-09-26 12:41:54.000000000 -0400
-@@ -49,6 +49,14 @@
+--- fuse_module/fuse.h.orig	2008-02-05 00:25:57.000000000 -0500
++++ fuse_module/fuse.h	2008-10-29 19:18:43.000000000 -0400
+@@ -25,6 +25,14 @@
+ #endif
+ #endif
+ 
++#ifndef VOP_ACCESS_TAKES_ACCMODE_T
++#if __FreeBSD_version >= 800052
++#define VOP_ACCESS_TAKES_ACCMODE_T 1
++#else
++#define VOP_ACCESS_TAKES_ACCMODE_T 0
++#endif
++#endif
++
+ #ifndef VOP_OPEN_TAKES_FP
+ #if __FreeBSD_version >= 700044
+ #define VOP_OPEN_TAKES_FP 1
+@@ -49,6 +57,14 @@
  #endif
  #endif
  
