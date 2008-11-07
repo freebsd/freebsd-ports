81d8836db0d1eca10cdd99548b0b3a69f9a0d7c7
hrs <hrs@FreeBSD.org>
2008-11-07 18:08:59 +0000

    Remove japanese/magicpoint and merge Japanese character support into misc/magicpoint.  The Japanese support is now based on FreeType library, not VFlib, as the non-Japanese vector font support is.
    Approved by:	dinoex (maintainer)

---
+++ misc/magicpoint/files/patch-image-rlelib.c
@@ -0,0 +1,11 @@
+--- image/rlelib.c.bak	2007-05-18 01:47:33.000000000 +0900
++++ image/rlelib.c	2008-11-06 23:38:04.000000000 +0900
+@@ -13,7 +13,7 @@
+ #include <stdio.h>
+ #include <math.h>
+ #if 0
+-#include <varargs.h>
++#include <stdarg.h>
+ #endif
+ #include <ctype.h>
+ 
