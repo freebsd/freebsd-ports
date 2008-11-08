f57f8d4a576b4254af716b6b7390db5bccb3e8a6
Edward Tomasz Napierala <trasz@FreeBSD.org>
2008-11-08 12:03:15 +0000

    Fix build after recent audio/jack update.
    PR:		ports/125900

--- audio/zynaddsubfx/files/patch-Output-JACKaudiooutput.C
+++ audio/zynaddsubfx/files/patch-Output-JACKaudiooutput.C
@@ -1,5 +1,5 @@
---- Output/JACKaudiooutput.C.orig	Sun Jan 28 01:26:00 2007
-+++ Output/JACKaudiooutput.C	Sun Jan 28 01:28:35 2007
+--- Output/JACKaudiooutput.C.orig	2008-07-23 14:05:40.000000000 +0200
++++ Output/JACKaudiooutput.C	2008-07-23 14:06:33.000000000 +0200
 @@ -38,6 +38,7 @@
      jackmaster=master_;
      jackclient=0;
@@ -24,12 +24,19 @@
       return(true);
  };
  
-@@ -114,7 +124,7 @@
+@@ -114,13 +124,13 @@
    jack_midi_event_t jack_midi_event;
    jack_nframes_t event_index = 0;
    jack_nframes_t event_count = 
 -    jack_midi_port_get_info(midi_buf, frames)->event_count;
-+    jack_midi_get_event_count(midi_buf, frames);
++    jack_midi_get_event_count(midi_buf);
    unsigned char* midi_data;
    unsigned char type, chan;
    
+   while (event_index < event_count) {
+     
+-    jack_midi_event_get(&jack_midi_event, midi_buf, event_index, frames);
++    jack_midi_event_get(&jack_midi_event, midi_buf, event_index);
+     midi_data = jack_midi_event.buffer;
+     type = midi_data[0] & 0xF0;
+     chan = midi_data[0] & 0x0F;
