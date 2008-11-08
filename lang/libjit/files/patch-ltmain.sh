b259a11861f22455661b5941e6b3c94660a380db
Martin Wilke <miwi@FreeBSD.org>
2008-11-08 20:09:19 +0000

    - Update to 0.1.0
    PR:		128699
    Submitted by:	Wen heping <wenheping@gmail.com>

--- lang/libjit/files/patch-ltmain.sh
+++ lang/libjit/files/patch-ltmain.sh
@@ -1,43 +1,38 @@
---- ltmain.sh.orig	Wed May 12 23:24:02 2004
-+++ ltmain.sh	Fri May 14 14:47:58 2004
-@@ -1865,7 +1865,7 @@
- 	  if test $? -eq 0 ; then
+--- ltmain.sh.orig	2008-11-09 00:28:51.000000000 +0800
++++ ltmain.sh	2008-11-09 00:32:32.000000000 +0800
+@@ -3420,7 +3420,7 @@
+ 	  if test "$?" -eq 0 ; then
  	    ldd_output=`ldd conftest`
  	    for i in $deplibs; do
 -	      name="`expr $i : '-l\(.*\)'`"
 +	      name="`expr X$i : 'X-l\(.*\)'`"
  	      # If $name is empty we are operating on a -L argument.
- 	      if test "$name" != "" ; then
- 		libname=`eval \\$echo \"$libname_spec\"`
-@@ -1890,7 +1890,7 @@
- 	    # Error occured in the first compile.  Let's try to salvage the situation:
- 	    # Compile a seperate program for each library.
+               if test "$name" != "" && test "$name" -ne "0"; then
+ 		if test "X$allow_libtool_libs_with_static_runtimes" = "Xyes" ; then
+@@ -3457,7 +3457,7 @@
+ 	    # Error occurred in the first compile.  Let's try to salvage
+ 	    # the situation: Compile a separate program for each library.
  	    for i in $deplibs; do
 -	      name="`expr $i : '-l\(.*\)'`"
 +	      name="`expr X$i : 'X-l\(.*\)'`"
- 	     # If $name is empty we are operating on a -L argument.
- 	      if test "$name" != "" ; then
+ 	      # If $name is empty we are operating on a -L argument.
+               if test "$name" != "" && test "$name" != "0"; then
  		$rm conftest
-@@ -1930,7 +1930,7 @@
+@@ -3509,7 +3509,7 @@
  	  set dummy $deplibs_check_method
- 	  file_magic_regex="`expr \"$deplibs_check_method\" : \"$2 \(.*\)\"`"
+ 	  file_magic_regex=`expr "$deplibs_check_method" : "$2 \(.*\)"`
  	  for a_deplib in $deplibs; do
 -	    name="`expr $a_deplib : '-l\(.*\)'`"
 +	    name="`expr X$a_deplib : 'X-l\(.*\)'`"
  	    # If $name is empty we are operating on a -L argument.
- 	    if test "$name" != "" ; then
- 	      libname=`eval \\$echo \"$libname_spec\"`
-@@ -3360,11 +3360,12 @@
- 	fi
- 
- 	# Install the pseudo-library for information purposes.
-+	if /usr/bin/false ; then
- 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
- 	instname="$dir/$name"i
- 	$show "$install_prog $instname $destdir/$name"
- 	$run eval "$install_prog $instname $destdir/$name" || exit $?
--
-+	fi
- 	# Maybe install the static library, too.
- 	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
- 	;;
+             if test "$name" != "" && test  "$name" != "0"; then
+ 	      if test "X$allow_libtool_libs_with_static_runtimes" = "Xyes" ; then
+@@ -3578,7 +3578,7 @@
+ 	  set dummy $deplibs_check_method
+ 	  match_pattern_regex=`expr "$deplibs_check_method" : "$2 \(.*\)"`
+ 	  for a_deplib in $deplibs; do
+-	    name="`expr $a_deplib : '-l\(.*\)'`"
++	    name="`expr X$a_deplib : 'X-l\(.*\)'`"
+ 	    # If $name is empty we are operating on a -L argument.
+ 	    if test -n "$name" && test "$name" != "0"; then
+ 	      if test "X$allow_libtool_libs_with_static_runtimes" = "Xyes" ; then
