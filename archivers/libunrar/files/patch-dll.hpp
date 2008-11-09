6d7a1267838d634a163b87ba59783934231fd750
Martin Wilke <miwi@FreeBSD.org>
2008-11-09 16:36:08 +0000

    - Update to 3.8.5
    PR:		128624
    Submitted by:	Alex Samorukov <samm@os2.kiev.ua> (maintainer)

--- archivers/libunrar/files/patch-dll.hpp
+++ archivers/libunrar/files/patch-dll.hpp
@@ -1,18 +1,19 @@
---- dll.hpp.orig  2004-09-08 17:57:20.000000000 +0400
-+++ dll.hpp       2004-10-22 10:43:08.000000000 +0400
-@@ -26,13 +26,13 @@
+--- dll.hpp	2008-11-06 01:37:37.000000000 +0100
++++ dll.hpp	2008-11-06 01:38:47.000000000 +0100
+@@ -28,14 +28,14 @@
  
  #define RAR_DLL_VERSION       4
  
 -#ifdef _UNIX
-+//#ifdef _UNIX
++// #ifdef _UNIX
  #define CALLBACK
  #define PASCAL
  #define LONG long
  #define HANDLE void *
+ #define LPARAM long
  #define UINT unsigned int
 -#endif
-+//#endif
++// #endif
  
  struct RARHeaderData
  {
