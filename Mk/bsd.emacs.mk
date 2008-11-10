80012e1ea94e4d2eb3f2a5c5abca884be6a7e626
Boris Samorodov <bsam@FreeBSD.org>
2008-11-10 22:30:51 +0000

    . update to version 22.3 (includes a security fix in the way Emacs   interacts with Python); . change EMACS_VER at bsd.emacs.mk.
    While I'm here: remove old PORTREVISION.
    
    PR:		127168
    Submitted by:	keramida (maintainer)

--- Mk/bsd.emacs.mk
+++ Mk/bsd.emacs.mk
@@ -1,5 +1,5 @@
 #
-#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.73 2008-06-03 14:36:49 bsam Exp $
+#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.74 2008-11-10 22:30:51 bsam Exp $
 #
 #	bsd.emacs.mk - 19990829 Shigeyuki Fukushima.
 #
@@ -83,7 +83,7 @@ PLIST?=                 ${PKGDIR}/pkg-plist.${EMACS_PORT_NAME}
 # Emacs-22.x
 .elif (${EMACS_PORT_NAME} == "emacs22")
 EMACS_NAME=		emacs
-EMACS_VER=		22.2
+EMACS_VER=		22.3
 EMACS_MAJOR_VER=	22
 EMACS_LIBDIR?=		share/${EMACS_NAME}
 EMACS_LIBDIR_WITH_VER?=	share/${EMACS_NAME}/${EMACS_VER}
