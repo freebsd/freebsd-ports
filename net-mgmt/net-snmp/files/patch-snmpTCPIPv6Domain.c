d3eeea591c869eb130ae1cbb43138849b725a85f
kuriyama <kuriyama@FreeBSD.org>
2008-11-15 00:56:29 +0000

    Upgrade to 5.4.2.1 (includes fix for CVE-2008-4309).

--- net-mgmt/net-snmp/files/patch-snmpTCPIPv6Domain.c
+++ net-mgmt/net-snmp/files/patch-snmpTCPIPv6Domain.c
@@ -1,19 +1,16 @@
-Index: snmplib/snmpTCPIPv6Domain.c
-diff -u -p snmplib/snmpTCPIPv6Domain.c.orig snmplib/snmpTCPIPv6Domain.c
---- snmplib/snmpTCPIPv6Domain.c.orig	Fri Sep 16 19:30:49 2005
-+++ snmplib/snmpTCPIPv6Domain.c	Fri Jan 11 13:40:09 2008
-@@ -96,13 +96,24 @@ netsnmp_tcp6_fmtaddr(netsnmp_transport *
+--- snmplib/snmpTCPIPv6Domain.c.orig	2008-06-17 17:57:14.000000000 +0900
++++ snmplib/snmpTCPIPv6Domain.c	2008-11-14 21:34:12.000000000 +0900
+@@ -96,12 +96,23 @@
      if (to == NULL) {
          return strdup("TCP/IPv6: unknown");
      } else {
 -        char addr[INET6_ADDRSTRLEN];
--        char tmp[INET6_ADDRSTRLEN + 8];
-+	char addr[NI_MAXHOST], tmp[NI_MAXHOST + NI_MAXSERV + 12];
+-        char tmp[INET6_ADDRSTRLEN + 18];
++        char addr[NI_MAXHOST], tmp[NI_MAXHOST + NI_MAXSERV + 12];
  
 -        sprintf(tmp, "TCP/IPv6: [%s]:%hd",
 -                inet_ntop(AF_INET6, (void *) &(to->sin6_addr), addr,
 -                          INET6_ADDRSTRLEN), ntohs(to->sin6_port));
--        return strdup(tmp);
 +/*
 + * NI_WITHSCOPEID will be obsoleted.  But some implementations require
 + * this flag to retrieve scoped name.
@@ -29,7 +26,6 @@ diff -u -p snmplib/snmpTCPIPv6Domain.c.orig snmplib/snmpTCPIPv6Domain.c
 +	}
 +	snprintf(tmp, sizeof(tmp), "TCP/IPv6: [%s]:%hd", addr,
 +		 ntohs(to->sin6_port));
-+	return strdup(tmp);
+         return strdup(tmp);
      }
  }
- 
