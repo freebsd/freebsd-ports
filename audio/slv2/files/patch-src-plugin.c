7019ba39c1c9d5057e131f707d75198e55a095bf
amdmi3 <amdmi3@FreeBSD.org>
2008-11-26 22:16:08 +0000

    SLV2 is a library to make the use of LV2 plugins as simple as possible for applications.
    WWW: http://drobilla.net/software/slv2/
    
    PR:		129158
    Submitted by:	xaimus <xaimus at gmail dot com>

---
+++ audio/slv2/files/patch-src-plugin.c
@@ -0,0 +1,11 @@
+--- src/plugin.c.orig
++++ src/plugin.c
+@@ -16,8 +16,6 @@
+  * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+  */
+ 
+-#define _XOPEN_SOURCE 500
+-
+ #include <math.h>
+ #include <string.h>
+ #include <stdlib.h>
