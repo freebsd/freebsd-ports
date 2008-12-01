565b098b7767ef71467a22591135b4da2d6a0100
miwi <miwi@FreeBSD.org>
2008-12-01 21:15:44 +0000

    - Fix Country database url
    PR:		129124
    Submitted by:	Bogdan Culibrk <bc@default.rs>
    Approved by:	maintainer

--- net/GeoIP/files/geoipupdate.sh
+++ net/GeoIP/files/geoipupdate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /tmp
-fetch http://www.maxmind.com/download/geoip/database/GeoIP.dat.gz
+fetch http://64.246.48.99/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
 gunzip GeoIP.dat.gz
 mv -f GeoIP.dat /usr/local/share/GeoIP
