1479c411fb8d2b6c341a0b43dc5d0255d4591420
Jung-uk Kim <jkim@FreeBSD.org>
2008-12-06 00:40:05 +0000

    It's often useful to be able to make real-mode x86 BIOS calls from userland. Linux lrmi provides a simple interface to this for i386 machines, but this doesn't help on other platforms.  libx86 provides the Linux lrmi interface, but will also run on platforms such as amd64 and alpha.
    WWW: http://www.codon.org.uk/~mjg59/libx86/

---
+++ devel/libx86/files/patch-thunk.c
@@ -0,0 +1,12 @@
+--- thunk.c	2008-04-02 20:48:00.000000000 -0400
++++ thunk.c	2008-12-03 19:19:25.000000000 -0500
+@@ -11,7 +11,9 @@
+ #include <unistd.h>
+ #include <stdlib.h>
+ #include <sys/mman.h>
++#if 0
+ #include <sys/io.h>
++#endif
+ #include <string.h>
+ #include <sys/ipc.h>
+ #include <sys/shm.h>
