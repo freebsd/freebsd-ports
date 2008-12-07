ee9041954dbb5eee6cf7ad23033f279fb9baf1fb
miwi <miwi@FreeBSD.org>
2008-12-07 20:36:48 +0000

    2008-09-19 japanese/escpf: Has been broken for more than 6 months 2008-09-19 java/java-gcj-compat: Has been broken for more than 6 months 2008-09-19 lang/screamer: Has been broken for more than 6 months 2008-10-01 misc/documancer: Unmaintained upstream 2008-09-19 misc/ipbt: Has been broken for more than 6 months 2008-10-13 multimedia/manslide: Use multimedia/smile instead 2008-09-19 net/globus4: Has been broken for more than 6 months 2008-09-19 net/p5-Parallel-MPI: Has been broken for more than 6 months 2008-01-28 net/p54u: website disappeared 2008-09-19 net-im/ginsu: Has been broken for more than 6 months 2008-09-19 net-p2p/py-kenosis-bittorrent: Has been broken for more than 6 months 2008-09-19 sysutils/sjog: Has been broken for more than 6 months 2008-09-19 textproc/Ebnf2ps: Has been broken for more than 6 months 2008-09-19 www/roxen: Has been broken for more than 6 months 2008-09-19 x11-fm/evidence: Has been broken for more than 6 months

--- lang/screamer/files/build.lisp
+++
@@ -1,7 +0,0 @@
-;; -*- Mode: Lisp -*-
-(declaim (optimize (speed 3)
-                   (compilation-speed 0)
-                   (safety 0)
-                   (debug 0)))
-(compile-file "screamer.lisp")
-(quit)
