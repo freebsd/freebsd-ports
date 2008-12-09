78b216cdb49f41592f2a8a3970e281d68de9be0a
bsam <bsam@FreeBSD.org>
2008-12-09 22:18:53 +0000

    . fix the build WITHOUT_GTK;   [1] . update to a newer snapshot;  [2] . while I'm here: reset PORTREVISION.
    PR:		28513  [1], 129536  [2]
    Submitted by:	Ashish Shukla <wahjava at gmail.com>  [1],
    		keramida (maintainer)  [2]
    Approved by:	keramida (maintainer)  [1]

--- editors/emacs-devel/files/patch-src-alloc.c
+++ editors/emacs-devel/files/patch-src-alloc.c
@@ -1,9 +1,9 @@
---- src/alloc.c.orig	2008-01-02 14:45:48.000000000 +0200
-+++ src/alloc.c	2008-01-02 14:41:21.000000000 +0200
-@@ -4532,8 +4532,12 @@
+--- src/alloc.c.orig	2008-12-09 19:27:59.000000000 +0200
++++ src/alloc.c	2008-12-09 19:25:42.000000000 +0200
+@@ -4488,8 +4488,12 @@
       needed on ia64 too.  See mach_dep.c, where it also says inline
       assembler doesn't work with relevant proprietary compilers.  */
- #ifdef sparc
+ #ifdef __sparc__
 +#ifdef __sparc64__
 +  asm ("flushw");
 +#else
