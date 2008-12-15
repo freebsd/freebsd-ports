cd9ce931c14ad2e16c9bc8ac4b3050229ccf0779
brooks <brooks@FreeBSD.org>
2008-12-15 20:45:44 +0000

    Upgrade to the official 6.2 release.
    Remove the option to install DRMAA .jar files as they are not
    automatically downloadable due to the move the the sun donwload
    center.  Adding a new port to install them or adding an option to use
    the manually downloaded would probably be a good idea, but I've been
    stalling the upgrade too long as it is.

--- sysutils/sge62/files/patch-clients_common_sge__client__ijs.c
+++ sysutils/sge62/files/patch-clients_common_sge__client__ijs.c
@@ -12,29 +12,3 @@ $FreeBSD$
  #  include <termios.h>
  #  include <sys/ttycom.h>
  #  include <sys/ioctl.h>
-@@ -192,19 +192,19 @@
-     *  indication until the terminal is closed."
-     */
-    if (signal(SIGHUP, SIG_IGN) != SIG_IGN) {
--      sigset(SIGHUP, signal_handler);
-+      signal(SIGHUP, signal_handler);
-    }
-    if (signal(SIGINT, SIG_IGN) != SIG_IGN) {
--      sigset(SIGINT, signal_handler);
-+      signal(SIGINT, signal_handler);
-    }
-    if (signal(SIGQUIT, SIG_IGN) != SIG_IGN) {
--      sigset(SIGQUIT, signal_handler);
-+      signal(SIGQUIT, signal_handler);
-    }
-    if (signal(SIGTERM, SIG_IGN) != SIG_IGN) {
--      sigset(SIGTERM, signal_handler);
-+      signal(SIGTERM, signal_handler);
-    }
--   sigset(SIGWINCH, window_change_handler);
--   sigset(SIGPIPE,  broken_pipe_handler);
-+   signal(SIGWINCH, window_change_handler);
-+   signal(SIGPIPE,  broken_pipe_handler);
- }
- 
- /****** client_check_window_change() *******************************************
