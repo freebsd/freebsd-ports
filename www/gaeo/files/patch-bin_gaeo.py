5559b4fe56498103fedb42f3f2202802537a5c98
lwhsu <lwhsu@FreeBSD.org>
2008-12-21 06:00:14 +0000

    - Update to 0.2
    PR:		ports/129718
    Submitted by:	Yi-Jheng Lin <yzlin AT cs.nctu.edu.tw> (maintainer)

--- www/gaeo/files/patch-bin_gaeo.py
+++ www/gaeo/files/patch-bin_gaeo.py
@@ -1,11 +1,24 @@
---- bin/gaeo.py.orig	2008-07-24 15:24:37.000000000 +0800
-+++ bin/gaeo.py	2008-07-24 15:25:07.000000000 +0800
-@@ -196,7 +196,7 @@
-     create_file(os.path.join(project_home, 'favicon.ico'), [])
+--- bin/gaeo.py.orig	2008-12-03 01:05:19.000000000 +0800
++++ bin/gaeo.py	2008-12-18 00:52:37.000000000 +0800
+@@ -212,7 +212,7 @@
  
      # copy GAEO directory
--    copytree(os.path.join(os.path.dirname(__file__), '..', 'gaeo'), os.path.join(project_home, 'gaeo'))
-+    copytree(os.path.join('%%GAEOHOME%%', 'gaeo'), os.path.join(project_home, 'gaeo'))
+     gaeo_folder = os.path.join(project_home, 'gaeo')
+-    copytree(os.path.join(os.path.dirname(os.path.dirname(__file__)), template_base, 'gaeo'), gaeo_folder)
++    copytree(os.path.join('%%GAEOHOME%%', template_base, 'gaeo'), gaeo_folder)
+     
+     # if zipped option is enabled
+     if zipped_core:
+@@ -231,10 +231,10 @@
+         os.chdir(os.path.join(project_home, '..'))
+     
+     # copy plugins directory
+-    copytree(os.path.join(os.path.dirname(os.path.dirname(__file__)), template_base, 'plugins'), os.path.join(project_home, 'plugins'))
++    copytree(os.path.join('%%GAEOHOME%%', template_base, 'plugins'), os.path.join(project_home, 'plugins'))
+ 
+     # copy sample main.py
+-    copyfile(os.path.join(os.path.dirname(os.path.dirname(__file__)), template_base, 'main-sample.py'), os.path.join(project_home, 'main.py'))
++    copyfile(os.path.join('%%GAEOHOME%%', template_base, 'main-sample.py'), os.path.join(project_home, 'main.py'))
  
      # create the eclipse project file
      if create_eclipse_proj:
