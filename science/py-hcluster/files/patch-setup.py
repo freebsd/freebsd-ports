6b208eade02c93e38aa5427ea6326512fe4c27b8
Martin Wilke <miwi@FreeBSD.org>
2008-12-22 21:10:33 +0000

    - Update to 0.2.0
    PR:		129652
    Submitted by:	Wen Heping <wenheping@gmail.com> (maintainer)

--- science/py-hcluster/files/patch-setup.py
+++ science/py-hcluster/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2008-09-21 22:20:16.000000000 +0800
-+++ setup.py	2008-09-21 22:20:34.000000000 +0800
+--- setup.py.orig	2008-12-14 21:42:50.000000000 +0800
++++ setup.py	2008-12-14 21:43:35.000000000 +0800
 @@ -33,7 +33,8 @@
      print string.join(l, '\n')
      # Prompt the user with a list of selections.
      while not (s >= 1 and s <= len(valid_paths)):
--        s = input('Selection [default=1]:' % s)
-+#        s = input('Selection [default=1]:' % s)
+-        s = input('Selection [default=1]:')
++#        s = input('Selection [default=1]:')
 +	s=1
          if s == '':
              s = 1
