00fdefdf41f7dea2abec601f634dd2188eef4c77
dinoex <dinoex@FreeBSD.org>
2008-12-24 07:49:52 +0000

    - fix build for gnustep-gui 0.16

---
+++ sysutils/burn/files/patch-WorkInProgress.m
@@ -0,0 +1,10 @@
+--- WorkInProgress.m.orig	2005-05-12 17:58:49.000000000 +0200
++++ WorkInProgress.m	2008-12-24 01:53:34.000000000 +0100
+@@ -23,6 +23,7 @@
+ 
+ #include <Foundation/Foundation.h>
+ #include <AppKit/AppKit.h>
++#include <GNUstepGUI/GSTheme.h>
+ 
+ #include "Functions.h"
+ #include "Constants.h"
