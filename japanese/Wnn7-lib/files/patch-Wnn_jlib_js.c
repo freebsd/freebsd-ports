5e90017d5ec2d868bbb3553b5242324e847a7a2a
hrs <hrs@FreeBSD.org>
2008-12-29 17:22:53 +0000

    - Fix off-by-one error in sockaddr_un length calculation[*]. - Style clean-ups.
    Spotted by:	Yoshihiko Sarumaru[*]

--- japanese/Wnn7-lib/files/patch-Wnn_jlib_js.c
+++ japanese/Wnn7-lib/files/patch-Wnn_jlib_js.c
@@ -1,5 +1,5 @@
---- Wnn/jlib/js.c.orig	2007-11-11 17:46:58.000000000 +0900
-+++ Wnn/jlib/js.c	2007-11-11 17:47:59.000000000 +0900
+--- Wnn/jlib/js.c.orig	2001-10-17 10:37:17.000000000 +0900
++++ Wnn/jlib/js.c	2008-12-30 01:41:03.000000000 +0900
 @@ -159,9 +159,9 @@
  */
  
@@ -21,3 +21,23 @@
  typedef struct _host_address {
  	int address_len;
  	char *address;
+@@ -358,14 +356,17 @@
+ 	/* find socket name from table by lang */
+ 	sock_name = UNIX_SOCKET_NAME;		/* Jserver */
+ 	strcpy(saddr.sun_path, sock_name);
+-#if ((defined AIXV3) || (defined FREEBSD))
++#if ((defined AIXV3))
+ 	strcat(saddr.sun_path, "=");
+ #endif
+ 
+ 	if ((sd = socket(AF_UNIX,SOCK_STREAM, 0)) == ERROR) {
+ 		return -1;
+ 	}
+-	if (connect(sd,(struct sockaddr *)&saddr,strlen(saddr.sun_path)+sizeof(saddr.sun_family)) == ERROR) {
++#if !defined(SUN_LEN)
++# define SUN_LEN(su) (sizeof(*(su)) - sizeof((su)->sun_path) + strlen((su)->sun_path))
++#endif
++	if (connect(sd,(struct sockaddr *)&saddr, SUN_LEN(&saddr)) == ERROR) {
+ 		close(sd);
+ 		return -1;
+ 	}
