e32d20489c105d96cdd53df34f1d54cb0081babb
chinsan <chinsan@FreeBSD.org>
2009-01-02 08:06:15 +0000

    - Fix compiling warning.
    PR:			ports/129819
    Submitted by:		Hung-Yi Chen <gaod@hychen.org>

--- chinese/ve/files/extrapatch-screen.c
+++ chinese/ve/files/extrapatch-screen.c
@@ -1,15 +1,25 @@
---- screen.c.orig	Thu Dec 18 01:08:35 2003
-+++ screen.c	Thu Dec 18 01:09:44 2003
-@@ -6,7 +6,7 @@
+--- screen.c_orig	2008-12-21 22:41:24.000000000 +0800
++++ screen.c	2008-12-21 22:41:40.000000000 +0800
+@@ -6,7 +6,8 @@
  /* update : 95/12/15                                     */
  /*-------------------------------------------------------*/
  
 -#include <varargs.h>
 +#include <stdarg.h>
++#include <stdlib.h>
  #include "bbs.h"
  
  extern char clearbuf[];
-@@ -601,13 +601,12 @@
+@@ -46,8 +47,6 @@
+ {
+   if (!dumb_term && !big_picture)
+   {
+-    extern char *calloc();
+-
+     scr_lns = t_lines;
+     scr_cols = t_columns = ANSILINELEN;
+     /* scr_cols = MIN(t_columns, ANSILINELEN); */
+@@ -601,13 +600,12 @@
  
  
  void
