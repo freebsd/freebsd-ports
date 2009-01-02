e85df6113a64656fbd5f94bdd388cc06597bb682
Hiroki Sato <hrs@FreeBSD.org>
2009-01-02 11:49:48 +0000

    - Enable IPv6 support.
    - Add multiple "listenaddr" support.  The jserverrc now
      understands the following statements:
    
       listenaddr 127.0.0.1
       listenaddr ::1
    
      The option "-a <listenaddr>" can also be specified more than
      once.  Currently the maximum number of listen addresses is 256.
    
    - SIGINT is no longer hooked when the -D (not daemonize,
      primarily for debugging) flag is specified.
    
    - Add --unix, long opt counterpart of -u.

--- japanese/FreeWnn-server/files/patch-Wnn-jserver-de_header.h
+++ japanese/FreeWnn-server/files/patch-Wnn-jserver-de_header.h
@@ -1,5 +1,10 @@
---- Wnn/jserver/de_header.h.orig	2004-12-30 02:45:32.000000000 +0900
-+++ Wnn/jserver/de_header.h	2008-10-30 15:45:17.000000000 +0900
+Index: Wnn/jserver/de_header.h
+===================================================================
+RCS file: /home/cvs/private/hrs/freewnn/Wnn/jserver/de_header.h,v
+retrieving revision 1.1
+diff -d -u -I\$FreeBSD: /tmp/pcvs/ports/japanese/FreeWnn-server/files/patch-Wnn-jserver-de_header.h,v 1.2 2009-01-02 11:49:47 hrs Exp $ -I\$NetBSD:.*\$ -I\$OpenBSD:.*\$ -I\$DragonFly:.*\$ -I\$Id:.*\$ -I\$Translation:.*\$ -I\$hrs:.*\$ -w -r1.1 de_header.h
+--- Wnn/jserver/de_header.h	20 Dec 2008 07:13:30 -0000	1.1
++++ Wnn/jserver/de_header.h	2 Jan 2009 10:20:44 -0000
 @@ -52,6 +52,8 @@
  #  endif        /* KOREAN */
  #endif        /* CHINESE */
@@ -9,11 +14,14 @@
  #include "jslib.h"
  #include "commonhd.h"
  #include "jdata.h"
-@@ -205,6 +207,7 @@
+@@ -205,6 +207,10 @@
  
  GLOBAL char jserver_dir[MAXPATHLEN];
  GLOBAL char jserverrcfile[MAXPATHLEN];
-+GLOBAL char listenaddr[NI_MAXHOST];
++
++#define MAXLISTENADDR	256
++GLOBAL char listenaddr[MAXLISTENADDR][NI_MAXHOST];
++
  GLOBAL char *hinsi_file_name GLOBAL_VAL(NULL);
  GLOBAL char lang_dir[MAXPATHLEN];
  
--- japanese/FreeWnn-server/files/patch-Wnn-jserver-initjserv.c
+++ japanese/FreeWnn-server/files/patch-Wnn-jserver-initjserv.c
@@ -1,22 +1,35 @@
---- Wnn/jserver/initjserv.c.orig	2003-05-12 03:43:15.000000000 +0900
-+++ Wnn/jserver/initjserv.c	2008-10-30 16:01:50.000000000 +0900
-@@ -215,6 +215,15 @@
+Index: Wnn/jserver/initjserv.c
+===================================================================
+RCS file: /home/cvs/private/hrs/freewnn/Wnn/jserver/initjserv.c,v
+retrieving revision 1.1
+diff -d -u -I\$FreeBSD: /tmp/pcvs/ports/japanese/FreeWnn-server/files/patch-Wnn-jserver-initjserv.c,v 1.3 2009-01-02 11:49:48 hrs Exp $ -I\$NetBSD:.*\$ -I\$OpenBSD:.*\$ -I\$DragonFly:.*\$ -I\$Id:.*\$ -I\$Translation:.*\$ -I\$hrs:.*\$ -w -r1.1 initjserv.c
+--- Wnn/jserver/initjserv.c	20 Dec 2008 07:13:30 -0000	1.1
++++ Wnn/jserver/initjserv.c	2 Jan 2009 11:14:37 -0000
+@@ -154,6 +154,7 @@
+   char param[EXPAND_PATH_LENGTH +1];
+   char *word;
+   int i, num, *v[17];
++  int lindex = 0;
+ 
+   strcpy (jserver_dir, JSERVER_DIR);
+ 
+@@ -215,6 +216,15 @@
  	    }
            log_debug ("max_client=%d", max_client);
          }
-+      else if (listenaddr[0] == '\0' && strcmp (code, "listenaddr") == 0)
-+        {
-+          num = sscanf (data, "%s %s ", code, &listenaddr);
-+	  if (num != 2)
-+	    {
-+	      log_err ("command %s invalid.", code);
-+	      continue;
-+	    }
-+	}
++      else if (listenaddr[lindex][0] == '\0'
++	       && strcmp(code, "listenaddr") == 0) {
++	      num = sscanf (data, "%s %s ", code, listenaddr[lindex]);
++	      if (num != 2) {
++		      log_err ("command %s invalid.", code);
++		      continue;
++	      }
++	      lindex++;
++      }
        else if (strcmp (code, "max_sticky_env") == 0)
          {
            num = sscanf (data, "%s %d ", code, &max_sticky_env);
-@@ -305,7 +314,9 @@
+@@ -305,7 +315,9 @@
      }
    fclose (fp);
  
@@ -26,7 +39,7 @@
  
    return (0);
  }
-@@ -340,7 +351,9 @@
+@@ -340,7 +352,9 @@
  
    files[fid].localf = LOCAL;
    strcpy (files[fid].name, buffer);
