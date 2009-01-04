8c64cb93dd30649e0d12e0a7b9aec2969fe4951a
Chin-San Huang <chinsan@FreeBSD.org>
2009-01-04 06:18:02 +0000

    - Update to 6.9   Changes 6.9:     New features/improvements:     - With postfix that support DSN (Delivery Status Notifications) we exclude       some lines to avoid counting mails twice in maillogconvert.pl script.     - Logresolvemerge.pl support FreeRADIUS logs or anything else using (the       fixed length!) ctime format timestamp.     - Add option stoponfirsteof in logresolvemerge tool.     - Add patch to support host_proxy tag in LogFormat (for Apache LogFormat       containing %{X-Forwarded-For}i)     - Renamed Add to favourites on "Hit on favicon".     - Increase robots, search engines database (Added Google Chrome browser,       better Vista, WII, detection, ...)     - Update languages files.     - Added a lot of patch from sourceforge.
        Fixes:
        - Fixed broken maxmind citi, org and isp plugins.
        - Remove &nbsp; in name html tag to have HtmlHeadSection first.
        - Fix: [ 2001151 ] Security fix.
        - Fix: [ 2038681 ] missing <br _/_> in plugins/geoip_org_maxmind.pm
        - Fix: [ 1921942 ] html footer is missing from the allextraN report.
        - Fix: [ 1943466 ] error geoip_city_maxmind Can't locate object method "record_
        - Fix: [ 1808277 ] Incorrect function call in geoip_isp_maxmind.pm
        - Fix: Full list of extrasections was not ordered correctly
        - A lot of other fixes.
        - Added missing icons
    
        Other/Documentation:
        - None
    
    - Fix CVE-2008-3714 from the upstream
      (Sec: Enhance security of sanitizing parameters)
      http://awstats.cvs.sourceforge.net/viewvc/awstats/awstats/wwwroot/cgi-bin/awstats.pl?r1=1.925&r2=1.926
    
    PR:		ports/129957,
    		ports/130143
    Submitted by:	Eygene Ryabinkin <rea-fbsd _at\ codelabs.ru>,
    		Naram Qashat <cyberbotx -at\ cyberbotx.com>
    Approved by:	Alex Samorukov (maintainer)
    Security:	http://secunia.com/advisories/31519

--- www/awstats/files/patch-wwwroot-cgi-bin-awstats.pl
+++ www/awstats/files/patch-wwwroot-cgi-bin-awstats.pl
@@ -1,11 +1,11 @@
---- wwwroot/cgi-bin/awstats.pl.orig	Sun Jan 15 22:28:43 2006
-+++ wwwroot/cgi-bin/awstats.pl	Sun Jan 15 22:30:17 2006
-@@ -1911,7 +1911,7 @@
- 	# Windows and standard package:        		"$DIR/plugins" (plugins in same dir than awstats.pl)
- 	# Redhat :                                  "/usr/local/awstats/wwwroot/cgi-bin/plugins"
- 	# Debian package :                    		"/usr/share/awstats/plugins"
--	my @PossiblePluginsDir=("$DIR/plugins","/usr/local/awstats/wwwroot/cgi-bin/plugins","/usr/share/awstats/plugins");
-+	my @PossiblePluginsDir=("$DIR/plugins","%%PREFIX%%/www/awstats/cgi-bin/plugins","/usr/share/awstats/plugins");
-  	my %DirAddedInINC=();
- 
- 	#Removed for security reason
+--- wwwroot/cgi-bin/awstats.pl.orig	2008-11-30 10:42:46.000000000 -0500
++++ wwwroot/cgi-bin/awstats.pl	2009-01-03 16:32:27.000000000 -0500
+@@ -2925,7 +2925,7 @@
+ # Debian package :                    		"/usr/share/awstats/plugins"
+ 	my @PossiblePluginsDir = (
+ 							   "$DIR/plugins",
+-							   "/usr/local/awstats/wwwroot/cgi-bin/plugins",
++							   "%%PREFIX%%/www/awstats/cgi-bin/plugins",
+ 							   "/usr/share/awstats/plugins"
+ 	);
+ 	my %DirAddedInINC = ();
