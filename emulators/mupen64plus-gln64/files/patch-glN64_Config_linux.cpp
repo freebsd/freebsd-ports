ff955dfa09075e5d1ec18cc59b181293b33cd2fd
acm <acm@FreeBSD.org>
2009-01-06 21:54:58 +0000

    - Update to 1.4.1

--- emulators/mupen64plus-gln64/files/patch-glN64_Config_linux.cpp
+++ emulators/mupen64plus-gln64/files/patch-glN64_Config_linux.cpp
@@ -1,39 +1,39 @@
---- glN64/Config_linux.cpp	2008-05-16 17:58:34.000000000 -0500
-+++ glN64/Config_linux.cpp	2008-05-16 18:00:13.000000000 -0500
+--- glN64/Config_gtk2.cpp	2008-12-15 01:42:14.000000000 -0500
++++ glN64/Config_gtk2.cpp	2008-12-15 01:50:17.000000000 -0500
 @@ -1,4 +1,3 @@
 -#include <features.h>
  #include <dlfcn.h>
  #include <unistd.h>
  #include "../main/winlnxdefs.h"
 @@ -44,29 +43,8 @@
- 	}
- 	else
- 	{
+     }
+     else
+     {
 -#ifdef __USE_GNU
 -                Dl_info info;
 -                void *addr = (void*)GetPluginDir;
 -                if(dladdr(addr, &info) != 0)
 -                {
-- 	               strncpy(path, info.dli_fname, PATH_MAX);
-- 	               *(strrchr(path, '/')) = '\0';
+-                   strncpy(path, info.dli_fname, PATH_MAX);
+-                   *(strrchr(path, '/')) = '\0';
 -                }
 -                else
 -                {
-- 	               fprintf(stderr, "(WW) Couldn't get path of .so, trying to get emulator's path\n");
+-                   fprintf(stderr, "(WW) Couldn't get path of .so, trying to get emulator's path\n");
 -#endif // __USE_GNU
--	               if(readlink("/proc/self/exe", path, PATH_MAX) == -1)
--	               {
--	                       fprintf(stderr, "(WW) readlink() /proc/self/exe failed: %s\n", strerror(errno));
--	 	              path[0] = '.';
--	 	              path[1] = '\0';
--	               }
--	               *(strrchr(path, '/')) = '\0';
--	               strncat(path, "/plugins", PATH_MAX);
+-                   if(readlink("/proc/self/exe", path, PATH_MAX) == -1)
+-                   {
+-                           fprintf(stderr, "(WW) readlink() /proc/self/exe failed: %s\n", strerror(errno));
+-                      path[0] = '.';
+-                      path[1] = '\0';
+-                   }
+-                   *(strrchr(path, '/')) = '\0';
+-                   strncat(path, "/plugins", PATH_MAX);
 -#ifdef __USE_GNU
--		}
+-        }
 -#endif
-+		strcpy(path, getenv("HOME"));
-+		strncat(path, "/.mupen64plus/plugins", PATH_MAX);
- 	}
++	strcpy(path, getenv("HOME"));
++	strncat(path, "/.mupen64plus/plugins", PATH_MAX);
+     }
          return path;
  }
