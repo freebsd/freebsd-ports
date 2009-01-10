a69936c42186c951823e485d3e70b2254583f97d
Vasil Dimov <vd@FreeBSD.org>
2009-01-10 15:04:38 +0000

    * Upgrade sysutils/duplicity from 0.5.03 to 0.5.06 * Install all the files that the port would install into doc/ if we didn't   modify its installation script * Leave the installator to install the man pages by itself, only tune the   installation directory
    ChangeLog:
    http://duplicity.nongnu.org/CHANGELOG

--- sysutils/duplicity/files/patch-setup.py
+++ sysutils/duplicity/files/patch-setup.py
@@ -1,8 +1,8 @@
---- setup.py.orig	2008-09-21 17:21:44.000000000 +0300
-+++ setup.py	2008-09-28 22:24:48.000000000 +0300
-@@ -21,6 +21,10 @@
+--- setup.py.orig	2009-01-09 16:05:11.000000000 +0200
++++ setup.py	2009-01-10 16:06:40.000000000 +0200
+@@ -12,6 +12,10 @@
  
- incdir_list = libdir_list = None 
+ incdir_list = libdir_list = None
  
 +LOCALBASE = os.environ.get("LOCALBASE", "/usr/local")
 +incdir_list = ['%s/include' % LOCALBASE]
@@ -11,14 +11,21 @@
  if os.name == 'posix':
      LIBRSYNC_DIR = os.environ.get('LIBRSYNC_DIR', '')
      args = sys.argv[:]
-@@ -48,9 +52,6 @@
-                                include_dirs=incdir_list,
-                                library_dirs=libdir_list,
-                                libraries=["rsync"])],
--      scripts = ['rdiffdir', 'duplicity'],
--      data_files = [('share/man/man1', ['duplicity.1', 'rdiffdir.1']),
--                    ('share/doc/duplicity-%s' % version_string,
--                     ['COPYING', 'README', 'CHANGELOG'])])
-+      scripts = ['rdiffdir', 'duplicity'])
+@@ -23,16 +27,9 @@
+         incdir_list = [os.path.join(LIBRSYNC_DIR, 'include')]
+         libdir_list = [os.path.join(LIBRSYNC_DIR, 'lib')]
  
+-data_files = [('share/man/man1',
++data_files = [('man/man1',
+                ['duplicity.1',
+                 'rdiffdir.1']),
+-              ('share/doc/duplicity-%s' % version_string,
+-               ['COPYING',
+-                'CVS-README',
+-                'LOG-README',
+-                'README',
+-                'tarfile-LICENSE',
+-                'CHANGELOG']),
+               ]
  
+ assert os.path.exists("po"), "Missing 'po' directory."
