aa6b8344aec1d8f0f3c3f4604c52eb842f5faf4c
sobomax <sobomax@FreeBSD.org>
2009-01-12 20:48:54 +0000

    o Update to 1.2.30.2;
    o Make speex codec working with recent version of libspeex. PR: 129970.

---
+++ net/asterisk12/files/patch-include_asterisk_astobj2.h
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- include/asterisk/astobj2.h
++++ include/asterisk/astobj2.h
+@@ -17,6 +17,8 @@
+ #ifndef _ASTERISK_ASTOBJ2_H
+ #define _ASTERISK_ASTOBJ2_H
+ 
++#define uint unsigned int
++
+ #include "asterisk/compat.h"
+ 
+ /*! \file 
