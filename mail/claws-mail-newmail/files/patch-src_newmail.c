52cd1d214c146b38ed0ef383d084ab007e88dacc
miwi <miwi@FreeBSD.org>
2009-01-12 12:59:37 +0000

    - Update to 0.0.15
    Approved by:	maintainer implicit

---
+++ mail/claws-mail-newmail/files/patch-src_newmail.c
@@ -0,0 +1,10 @@
+--- src/newmail.c.orig	2008-10-04 19:08:42.000000000 +0200
++++ src/newmail.c	2008-10-04 19:08:52.000000000 +0200
+@@ -25,6 +25,7 @@
+ 
+ #include <glib.h>
+ #include <glib/gi18n.h>
++#include <inttypes.h>
+ 
+ #include "version.h"
+ #include "claws.h"
