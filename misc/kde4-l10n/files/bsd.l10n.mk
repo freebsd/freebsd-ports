999db758f636d0697b452b372b70deb476f862fd
makc <makc@FreeBSD.org>
2009-01-13 23:16:38 +0000

    kde@freebsd team is pleased to announce KDE 4.1.4, the last bugfix release in 4.1.x series. You have only two weeks to play with it -- KDE 4.2.0 is coming ;) Great thanks for those who helped us with testing 4.1.2 and 4.1.3 during long-long ports ice-age.
    For lists of bugfixes and improvements please see:
    http://www.kde.org/announcements/changelogs/changelog4_1_1to4_1_2.php
    http://www.kde.org/announcements/changelogs/changelog4_1_2to4_1_3.php
    http://www.kde.org/announcements/changelogs/changelog4_1_3to4_1_4.php
    
    Approved by:	miwi (mentor)

--- misc/kde4-l10n/files/bsd.l10n.mk
+++ misc/kde4-l10n/files/bsd.l10n.mk
@@ -1,11 +1,13 @@
 .if defined(KDE4_L10N)
 USE_BZIP2=	yes
 USE_QT_VER=	4
-QT_COMPONENTS=	uic moc qmake rcc xml
+QT_COMPONENTS=	uic_build moc_build qmake_build rcc_build xml
 USE_GETTEXT=	yes
 USE_KDE4=	kdelibs kdeprefix automoc4
 USE_CMAKE=	yes
 MASTER_SITES?=	${MASTER_SITE_KDE}
 MASTER_SITE_SUBDIR?=	stable/${PORTVERSION}/src/kde-l10n/
+PKGNAMEPREFIX=	${KDE4_L10N}-
+DISTNAME=	${PORTNAME}-${KDE4_L10N}-${PORTVERSION}
 DIST_SUBDIR?=	KDE/kde-l10n
 .endif
