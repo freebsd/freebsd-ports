8cead46f5fdac9423d41aa3052f1b5189c5f9534
Joe Marcus Clarke <marcus@FreeBSD.org>
2009-01-13 03:51:40 +0000

    Update to 2.24.3.

--- databases/evolution-data-server/files/patch-calendar_libical_src_libical_icaltz-util.c
+++ databases/evolution-data-server/files/patch-calendar_libical_src_libical_icaltz-util.c
@@ -1,5 +1,5 @@
---- calendar/libical/src/libical/icaltz-util.c.orig	2007-08-24 02:48:28.000000000 -0400
-+++ calendar/libical/src/libical/icaltz-util.c	2007-08-27 16:23:59.000000000 -0400
+--- calendar/libical/src/libical/icaltz-util.c.orig	2008-12-22 04:49:23.000000000 -0500
++++ calendar/libical/src/libical/icaltz-util.c	2009-01-12 22:33:53.000000000 -0500
 @@ -23,6 +23,15 @@
  #include <string.h>
  #if defined(sun) && defined(__SVR4)
@@ -13,6 +13,6 @@
 +#define bswap_16                bswap16
 +#define bswap_32                bswap32
 +#define bswap_64                bswap64
- #else
- #include <byteswap.h>
- #include <endian.h>
+ #elif defined(_WIN32)
+ #define BIG_ENDIAN 'B'
+ #define LITTLE_ENDIAN 'l'
