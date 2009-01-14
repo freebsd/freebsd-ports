002ffc4e275fe09dde64bd0b26ff8fcde2b66f46
acm <acm@FreeBSD.org>
2009-01-14 02:03:05 +0000

    - Update to 1.5

---
+++ emulators/mupen64plus-base/files/patch-main_romcache.h
@@ -0,0 +1,10 @@
+--- main/romcache.h	2009-01-09 01:16:19.000000000 -0500
++++ main/romcache.h	2009-01-09 01:16:33.000000000 -0500
+@@ -23,6 +23,7 @@
+ #define __ROMCACHE_H__
+ 
+ #include <limits.h> 
++#include <time.h>
+ #include "md5.h"
+ 
+ #define COMMENT_MAXLENGTH 256
