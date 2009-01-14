9ad23ff715e5edf7b3d8cd527b4009f17b96b76a
Dirk Meyer <dinoex@FreeBSD.org>
2009-01-14 18:54:25 +0000

    - fix build after gnome update

---
+++ graphics/pornview/files/patch-gtkcellrendererpixmap.c
@@ -0,0 +1,12 @@
+--- src/support/widgets/gtkcellrendererpixmap.c.orig	2002-12-13 12:02:45.000000000 +0100
++++ src/support/widgets/gtkcellrendererpixmap.c	2009-01-14 17:09:25.000000000 +0100
+@@ -12,9 +12,6 @@
+  *  Copyright (C) 2000  Red Hat, Inc.,  Jonathan Blandford <jrb@redhat.com>
+  */
+ 
+-#ifndef GTK_DISABLE_DEPRECATED
+-#define GTK_DISABLE_DEPRECATED
+-#endif /* GTK_DISABLE_DEPRECATED */
+ 
+ #include "gtkcellrendererpixmap.h"
+ 
