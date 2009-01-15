f4833a6056bcbe06d785edb36aa94d0f59b3840f
Martin Wilke <miwi@FreeBSD.org>
2009-01-15 12:11:25 +0000

    - Update to 0.10.4
    PR:		130265
    Submitted by:	Dima Panov <fluffy@fluffy.khv.ru>
    Approved by:	maintainer

--- deskutils/google-gadgets/files/patch-extensions_gtk__system__framework_gtk__system__framework.cc
+++ deskutils/google-gadgets/files/patch-extensions_gtk__system__framework_gtk__system__framework.cc
@@ -3,12 +3,12 @@ $FreeBSD$
 
 --- extensions/gtk_system_framework/gtk_system_framework.cc.orig
 +++ extensions/gtk_system_framework/gtk_system_framework.cc
-@@ -99,7 +99,7 @@
-         GTK_FILE_CHOOSER_ACTION_OPEN,
+@@ -137,7 +137,7 @@
+         whole_title.c_str(), NULL, action,
          GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
          GTK_STOCK_OK, GTK_RESPONSE_OK,
 -        NULL);
 +        (gint*)0);
+     gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_CENTER);
  
-     gtk_file_chooser_set_select_multiple(GTK_FILE_CHOOSER(dialog), multiple);
-     if (filter && *filter) {
+     if (action == GTK_FILE_CHOOSER_ACTION_SAVE) {
