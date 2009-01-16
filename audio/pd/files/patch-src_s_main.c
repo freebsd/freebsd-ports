6a4709a1701a9ea267893006b24d16d01ccde471
Martin Wilke <miwi@FreeBSD.org>
2009-01-16 00:10:07 +0000

    - Update to 0.42
    PR:		130470
    Submitted by:	Ports Fury

--- audio/pd/files/patch-src_s_main.c
+++ audio/pd/files/patch-src_s_main.c
@@ -1,47 +1,54 @@
---- src/s_main.c.orig	Wed Sep 27 19:22:35 2006
-+++ src/s_main.c	Sun Mar 25 19:47:12 2007
-@@ -310,7 +310,7 @@
+--- src/s_main.c.orig	2008-03-15 09:03:00.000000000 +0900
++++ src/s_main.c	2008-08-23 21:44:26.000000000 +0900
+@@ -301,7 +301,9 @@
      else
      {
              /* open audio and MIDI */
--        sys_reopen_midi();
-+//      sys_reopen_midi();
++#if !defined(__FreeBSD__)
+         sys_reopen_midi();
++#endif
          sys_reopen_audio();
              /* run scheduler until it quits */
-         return (m_scheduler());
-@@ -635,7 +635,7 @@
+         return (m_mainloop());
+@@ -633,7 +635,9 @@
          }
          else if (!strcmp(*argv, "-alsamidi"))
          {
--          sys_set_midi_api(API_ALSA);
-+//          sys_set_midi_api(API_ALSA);
++#if !defined(__FreeBSD__)
+           sys_set_midi_api(API_ALSA);
++#endif
              argc--; argv++;
          }
  #endif
-@@ -904,7 +904,7 @@
+@@ -916,7 +920,11 @@
              /* add "doc/5.reference" library to helppath */
      strncpy(sbuf, sys_libdir->s_name, MAXPDSTRING-30);
      sbuf[MAXPDSTRING-30] = 0;
--    strcat(sbuf, "/doc/5.reference");
++#if defined(__FreeBSD__)
 +    strcat(sbuf, "/share/doc/pd/5.reference");
++#else
+     strcat(sbuf, "/doc/5.reference");
++#endif
      sys_helppath = namelist_append_files(sys_helppath, sbuf);
          /* correct to make audio and MIDI device lists zero based.  On
          MMIO, however, "1" really means the second device (the first one
-@@ -957,7 +957,7 @@
+@@ -969,7 +977,9 @@
          for (i = 0; i < naudiooutdev; i++)
              audiooutdev[i] = sys_soundoutdevlist[i];
      }
--    sys_get_midi_params(&nmidiindev, midiindev, &nmidioutdev, midioutdev);
-+//    sys_get_midi_params(&nmidiindev, midiindev, &nmidioutdev, midioutdev);
++#if !defined(__FreeBSD__)
+     sys_get_midi_params(&nmidiindev, midiindev, &nmidioutdev, midioutdev);
++#endif
      if (sys_nmidiin >= 0)
      {
          post("sys_nmidiin %d, nmidiindev %d", sys_nmidiin, nmidiindev);
-@@ -977,7 +977,7 @@
-         rate = sys_main_srate;
-     sys_open_audio(naudioindev, audioindev, nchindev, chindev,
-         naudiooutdev, audiooutdev, nchoutdev, choutdev, rate, advance, 0);
--    sys_open_midi(nmidiindev, midiindev, nmidioutdev, midioutdev, 0);
-+//    sys_open_midi(nmidiindev, midiindev, nmidioutdev, midioutdev, 0);
+@@ -992,7 +1002,9 @@
+     sys_set_audio_settings(naudioindev, audioindev, nchindev, chindev,
+         naudiooutdev, audiooutdev, nchoutdev, choutdev, rate, advance, 
+         callback);
++#if !defined(__FreeBSD__)
+     sys_open_midi(nmidiindev, midiindev, nmidioutdev, midioutdev, 0);
++#endif
  }
  
  static void sys_addreferencepath(void)
