4e42390a7316b966caf13c9beb2e8cbb7f565592
acm <acm@FreeBSD.org>
2009-01-16 00:31:03 +0000

    - Update to 2.73

--- devel/bullet/files/patch-autogen.sh
+++ devel/bullet/files/patch-autogen.sh
@@ -1,16 +1,73 @@
---- autogen.sh.orig	2008-03-27 23:45:16.000000000 -0500
-+++ autogen.sh	2008-03-27 23:45:45.000000000 -0500
-@@ -13,11 +13,11 @@
-   cat $i >> $MACROFILE
- done
+--- autogen.sh	2009-01-15 18:47:45.000000000 -0500
++++ autogen.sh	2009-01-15 18:48:58.000000000 -0500
+@@ -1,61 +1,24 @@
+ #! /bin/sh
  
--autoheader
-+%%AUTOHEADER%%
- rc=$?
- 
- if test $rc -eq 0; then
--  autoconf
-+  %%AUTOCONF%%
-   rc=$?
+-if [ "$USER" = "root" ]; then
+-	echo "*** You cannot do this as "$USER" please use a normal user account."
+-	exit 1
+-fi
+ if test ! -f configure.ac ; then
+ 	echo "*** Please invoke this script from directory containing configure.ac."
+ 	exit 1
  fi
  
+ echo "running aclocal"
+-aclocal
+-rc=$?
+-
+-if test $rc -eq 0; then
+-	echo "running libtool"
+-	libtoolize --force --automake --copy
+-	rc=$?
+-else
+-	echo "An error occured, autogen.sh stopping."
+-	exit $rc
+-fi
++%%ACLOCAL%%
+ 
+-if test $rc -eq 0; then
+-	echo "libtool worked."
+-else
+-	echo "libtool not found. trying glibtool."
+-	glibtoolize --force --automake --copy
+-	rc=$?
+-fi
++echo "running libtool"
++%%LIBTOOLIZE%% --force --automake --copy
+ 
+-if test $rc -eq 0; then
+-	echo "running automake"
+-	automake --add-missing --copy
+-	rc=$?
+-else
+-	echo "An error occured, autogen.sh stopping."
+-	exit $rc
+-fi
++echo "running automake"
++%%AUTOMAKE%% --add-missing --copy
+ 
+-if test $rc -eq 0; then
+-	echo "running autoheader"
+-	autoheader
+-	rc=$?
+-else
+-	echo "An error occured, autogen.sh stopping."
+-	exit $rc
+-fi
++echo "running autoheader"
++%%AUTOHEADER%%
+ 
+-if test $rc -eq 0; then
+-	echo "running autoconf"
+-	autoconf
+-	rc=$?
+-else
+-	echo "An error occured, autogen.sh stopping."
+-	exit $rc
+-fi
++echo "running autoconf"
++%%AUTOCONF%%
+ 
+ echo "autogen.sh complete"
+ exit $rc
