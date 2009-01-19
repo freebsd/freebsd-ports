33cc7636ee168e63c8931bd9185b6cad30d1304d
novel <novel@FreeBSD.org>
2009-01-19 20:05:42 +0000

    Update to 3.2.4.

--- audio/mp3blaster/files/patch-mpegsound-nasplayer.cc
+++ audio/mp3blaster/files/patch-mpegsound-nasplayer.cc
@@ -1,5 +1,5 @@
---- mpegsound/nasplayer.cc.orig	Thu Aug  2 16:08:09 2007
-+++ mpegsound/nasplayer.cc	Thu Aug  2 16:08:35 2007
+--- mpegsound/nasplayer.cc.orig	2009-01-18 16:52:25.000000000 +0300
++++ mpegsound/nasplayer.cc	2009-01-19 22:36:01.000000000 +0300
 @@ -12,6 +12,7 @@
  #include "mpegsound.h"
  #include "mpegsound_locals.h"
