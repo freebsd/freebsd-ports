c83e454185188dddc57f26d4359e009e2c43abf1
Pav Lucistnik <pav@FreeBSD.org>
2009-01-20 22:55:08 +0000

    - Update to 1.8
    PR:		ports/130466
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- multimedia/libmpeg3/files/patch-mpeg3ifo.c
+++ multimedia/libmpeg3/files/patch-mpeg3ifo.c
@@ -1,23 +1,21 @@
---- ./mpeg3ifo.c.orig	Sun Feb 19 01:19:23 2006
-+++ ./mpeg3ifo.c	Sat Jul 22 18:02:04 2006
+--- mpeg3ifo.c.orig	2007-12-17 08:25:26.000000000 +0900
++++ mpeg3ifo.c	2008-09-11 05:39:21.000000000 +0900
 @@ -1,4 +1,4 @@
 -#include <byteswap.h>
 +//#include <byteswap.h>
  #include <dirent.h>
  #include <fcntl.h>
  #include <stdlib.h>
-@@ -9,6 +9,14 @@
- #include "ifo.h"
+@@ -10,6 +10,12 @@
  #include "mpeg3private.h"
  #include "mpeg3protos.h"
-+
-+#define bswap_16(x) ((((x) << 8) & 0xff00) | \
-+		     (((x) >> 8) & 0x00ff))
-+
-+#define bswap_32(x) ((((x) << 24) & 0xff000000) | \
-+                     (((x) <<  8) & 0x00ff0000) | \
-+                     (((x) >>  8) & 0x0000ff00) | \
-+                     (((x) >> 24) & 0x000000ff))
  
++#ifdef __FreeBSD__
++#include <sys/endian.h>
++#define bswap_16(x) bswap16(x)
++#define bswap_32(x) bswap32(x)
++#endif
++
  typedef struct
  {
+ // Bytes relative to start of stream.
