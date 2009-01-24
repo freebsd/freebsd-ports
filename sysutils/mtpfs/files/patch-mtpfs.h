da286aa686ccbf14f1b3130617bda73d3a5247ed
Chin-San Huang <chinsan@FreeBSD.org>
2009-01-24 14:27:01 +0000

    - Fix build (fogot to updte patch)

--- sysutils/mtpfs/files/patch-mtpfs.h
+++ sysutils/mtpfs/files/patch-mtpfs.h
@@ -1,22 +1,23 @@
---- mtpfs.h.orig	Sun Jul  1 11:45:49 2007
-+++ mtpfs.h	Sun Jul  1 11:46:22 2007
-@@ -18,7 +18,9 @@
+--- mtpfs.h.orig	2009-01-04 03:03:56.000000000 +0800
++++ mtpfs.h	2009-01-04 03:05:16.000000000 +0800
+@@ -18,8 +18,9 @@
  #include <fcntl.h>
  #include <dirent.h>
  #include <errno.h>
 -#include <sys/statfs.h>
+-
 +#include <sys/param.h>
 +#include <sys/mount.h>
 +#include <sys/statvfs.h>
- 
  #include <libmtp.h>
  #include <glib.h>
-@@ -48,7 +50,7 @@
+ #include <id3tag.h>
+@@ -49,7 +50,7 @@
  static int mtpfs_unlink (const gchar * path);
  static int mtpfs_mkdir (const char *path, mode_t mode);
  static int mtpfs_rmdir (const char *path);
 -static int mtpfs_statfs (const char *path, struct statfs *stbuf);
 +static int mtpfs_statfs (const char *path, struct statvfs *stbuf);
+ int calc_length(int f);
  
  
- static LIBMTP_mtpdevice_t *device;
