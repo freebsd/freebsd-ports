ee079202bf10761fb20c9ec6ae2f6562b8faeaa1
Soeren Straarup <xride@FreeBSD.org>
2009-01-26 22:13:03 +0000

    Unbreak.
    Obtained from:	portsmon

---
+++ misc/wmcalendar/files/patch-calendar.h
@@ -0,0 +1,8 @@
+--- calendar.h.orig	2009-01-26 18:35:48.000000000 +0100
++++ calendar.h	2009-01-26 18:36:03.000000000 +0100
+@@ -1,4 +1,4 @@
+-#include <ical.h>
++#include <libical/ical.h>
+ #include <stdlib.h>
+ #include <gtk/gtk.h>
+ #include <string.h>
