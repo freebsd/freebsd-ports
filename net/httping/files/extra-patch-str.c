f82b424d11cadb5d02378ddccdbc7cad8b33e568
ehaupt <ehaupt@FreeBSD.org>
2009-01-27 21:09:46 +0000

    Fix build for OSVERSION after the strndup merge

---
+++ net/httping/files/extra-patch-str.c
@@ -0,0 +1,15 @@
+--- ./str.c.orig	2009-01-27 21:44:56.621283000 +0100
++++ ./str.c	2009-01-27 21:45:24.936283000 +0100
+@@ -18,6 +18,7 @@
+ #include "mem.h"
+ #include "utils.h"
+ 
++/*
+ #ifndef strndup
+ char *strndup(char *in, int size)
+ {
+@@ -29,3 +30,4 @@
+ 	return out;
+ }
+ #endif
++*/
---
+++ net/httping/files/extra-patch-str.h
@@ -0,0 +1,11 @@
+--- ./str.h.orig	2009-01-27 21:42:34.920281000 +0100
++++ ./str.h	2009-01-27 21:44:40.137282000 +0100
+@@ -15,6 +15,8 @@
+ 
+ #include <string.h>
+ 
++/*
+ #ifndef strndup
+ char *strndup(char *in, int size);
+ #endif
++*/
