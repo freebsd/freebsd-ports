6dd998ee983ee922b053ce164d082ad28cbd48a1
Dmitry Marakasov <amdmi3@FreeBSD.org>
2009-01-29 18:37:47 +0000

    - Update to 5.0
    PR:		131058
    Submitted by:	"Beat GÃ¤tzi" <beat at chruetertee dot ch> (maintainer)

--- www/linkchecker/files/patch-setup.py
+++ www/linkchecker/files/patch-setup.py
@@ -1,29 +1,30 @@
---- setup.py.orig	2008-02-07 21:27:02.000000000 +0100
-+++ setup.py	2008-02-07 21:27:42.000000000 +0100
-@@ -471,7 +471,6 @@
+--- setup.py.orig	2009-01-26 22:51:22.000000000 +0100
++++ setup.py	2009-01-26 22:52:37.000000000 +0100
+@@ -344,8 +344,6 @@
+ if os.name == 'nt':
      # windows does not have unistd.h
      define_macros.append(('YY_NO_UNISTD_H', None))
- else:
+-else:
 -    extra_compile_args.append("-pedantic")
-     if win_compiling:
-         # we are cross compiling with mingw
-         # add directory for pyconfig.h
-@@ -488,7 +487,7 @@
+ 
+ myname = "Bastian Kleineidam"
+ myemail = "calvin@users.sourceforge.net"
+@@ -353,7 +351,7 @@
  data_files = [
-          ('share/linkchecker',
-              ['config/linkcheckerrc', 'config/logging.conf', ]),
--         ('share/linkchecker/examples',
-+         ('share/examples/linkchecker',
-              ['cgi-bin/lconline/leer.html.en',
-               'cgi-bin/lconline/leer.html.de',
-               'cgi-bin/lconline/index.html',
-@@ -500,11 +499,9 @@
-       ]
+     ('share/linkchecker',
+         ['config/linkcheckerrc', 'config/logging.conf', ]),
+-    ('share/linkchecker/examples',
++    ('share/examples/linkchecker',
+         ['cgi-bin/lconline/leer.html.en',
+          'cgi-bin/lconline/leer.html.de',
+          'cgi-bin/lconline/index.html',
+@@ -367,11 +365,9 @@
+ ]
  
  if os.name == 'posix':
--    data_files.append(('share/man/man1', ['doc/en/linkchecker.1']))
+-    data_files.append(('share/man/man1', ['doc/en/linkchecker.1', 'doc/en/linkchecker-gui.1']))
 -    data_files.append(('share/man/man5', ['doc/en/linkcheckerrc.5']))
--    data_files.append(('share/man/de/man1', ['doc/de/linkchecker.1']))
+-    data_files.append(('share/man/de/man1', ['doc/de/linkchecker.1', 'doc/de/linkchecker-gui.1']))
 -    data_files.append(('share/man/de/man5', ['doc/de/linkcheckerrc.5']))
 -    data_files.append(('share/linkchecker/examples',
 +    data_files.append(('man/man1', ['doc/en/linkchecker.1']))
