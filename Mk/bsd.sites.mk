f6d83c0792844b9706f8b8898983443bf0c456c6
pav <pav@FreeBSD.org>
2009-01-29 13:48:52 +0000

    - Put a fast european and american mirror in front of the taiwanese for   SOURCEFORGE

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -1165,7 +1165,7 @@ MASTER_SITE_SAVANNAH+= \
 .endif
 
 .if !defined(IGNORE_MASTER_SITE_SOURCEFORGE)
-.for mirror in nchc kent easynews ufpr umn mesh heanet
+.for mirror in garr superb-east nchc kent easynews ufpr mesh heanet
 MASTER_SITE_SOURCEFORGE+= \
 	http://${mirror}.dl.sourceforge.net/sourceforge/%SUBDIR%/
 .endfor
