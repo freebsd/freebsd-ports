5dc7a52886e62e8b7ad0d77a8b3991c58142fb2f
beech <beech@FreeBSD.org>
2009-01-30 07:10:17 +0000

    - Update to 2.48 - Added blender wrapper script for setting up an initial $HOME/.blender   link to point to the blender data directory (for easier script usage). - Install additional scripts - Install additional locales on demand - Install additional docs on demand - Pass maintainership to submitter
    PR:		ports/130753
    Submitted by:	Marcus von Appen <mva@sysfault.org>

--- graphics/blender/files/patch-user-def.mk
+++ graphics/blender/files/patch-user-def.mk
@@ -1,77 +1,33 @@
---- ./user-def.mk.orig	Thu Oct  4 22:55:33 2007
-+++ ./user-def.mk	Thu Oct  4 22:56:26 2007
-@@ -0,0 +1,74 @@
-+export NAN_DEBUG=-g
+--- user-def.mk.orig	2009-01-19 09:28:02.000000000 +0100
++++ user-def.mk	2009-01-19 09:31:01.000000000 +0100
+@@ -0,0 +1,30 @@
++export NAN_DEBUG=
 +
 +export TYPE := $(shell echo `uname -s`)
 +
 +# Allow 64bit compiles.
 +export NAN_YESIAMSTUPID=true
 +
-+ifeq ($(TYPE), SunOS)
-+	export NAN_PYTHON=/usr/local
-+	export NAN_PYTHON_VERSION=2.5
-+	export NAN_OPENAL=/export/scratch/blender/src/openal/SunOS5.8
-+	export NAN_JPEG=/usr/local
-+	export NAN_PNG=/usr/local
-+	export NAN_SDL=/usr/local
-+	export NAN_SDLLIBS ?= $(shell $(NAN_SDL)/bin/sdl-config --libs) -lSDL_mixer
-+	export NAN_OPENEXR=/usr/local
-+        export NAN_OPENEXR_LIBS ?= $(NAN_OPENEXR)/lib/libIlmImf.a \
-+		$(NAN_OPENEXR)/lib/libIlmThread.a \
-+		$(NAN_OPENEXR)/lib/libHalf.a \
-+		$(NAN_OPENEXR)/lib/libIex.a
-+
-+	export NAN_ZLIB=/usr/local
-+	export NAN_FREETYPE=/usr/local
-+	export NAN_MOZILLA_INC=/usr/local/include/mozilla-1.0.1/
-+	export NAN_MOZILLA_LIB=/usr/local/lib/mozilla-1.0.1/
-+	export NAN_NSPR=/export/scratch/blender/SunOS5.8
-+	export NAN_GETTEXT=/opt/gnu
-+	export WITH_FREETYPE2=true
-+	export INTERNATIONAL=true
-+	export EXPYTHON=true
-+	export WITH_BF_GAMEPLAYER=true
-+	export NAN_NO_PLUGIN=true
-+#	export NAN_NO_KETSJI=true
-+	export WITH_FFMPEG=true
-+#	export NAN_FFMPEG=/export/scratch/blender/src/lib/solaris-2.8-sparc/ffmpeg
-+	export NAN_ICONV_LIBS=-L/opt/gnu/lib -R/opt/gnu/lib -liconv
-+	export WITH_ICONV=false
-+else
-+	export WITH_ICONV=false
-+#	export WITH_FFMPEG=true
-+	export WITH_FFMPEG=false
-+	export NAN_PYTHON=/usr/local
-+	export NAN_PYTHON_VERSION=2.5
-+	#export NAN_OPENAL=/usr
-+	export NAN_FMOD=/usr/local
-+	export NAN_JPEG=/usr/local
-+	export NAN_PNG=/usr/local
-+	export WITH_OPENEXR=true
-+#	export NAN_OPENEXR=/usr/local
-+	export NAN_OPENEXR=/usr/local
-+        export NAN_OPENEXR_LIBS ?= $(NAN_OPENEXR)/lib/libIlmImf.a \
-+		$(NAN_OPENEXR)/lib/libHalf.a \
-+		$(NAN_OPENEXR)/lib/libIex.a
-+
-+#		$(NAN_OPENEXR)/lib/libIlmThread.a \
-+
-+	export NAN_SDL=/export/scratch/blender/src/lib/linux-glibc2.2.5-i386/sdl
-+	export NAN_ODE=/export/scratch/blender/src/blender/extern/ode/dist
-+	export NAN_ZLIB=/usr/local
-+#	export NAN_FREETYPE=/usr
-+	export NAN_MESA=/export/scratch/crap/Mesa-6.2.1
-+	export NAN_MOZILLA_INC=/usr/include/mozilla/
-+	export NAN_MOZILLA_LIB=/usr/lib/mozilla/
-+	# I think it only needs includes so should be alright...
-+	export NAN_NSPR=/export/scratch/blender/src/nspr-4.2.2/mozilla/nsprpub/dist
-+
-+	export WITH_FREETYPE2=true
-+	export INTERNATIONAL=true
-+	export NAN_NO_STATIC = true
-+	export NAN_NO_PLUGIN=true
-+endif
-+
-+export WITH_VERSE=true
-+export WITH_BULLET=true
++export WITH_ICONV = false
++export WITH_FFMPEG = false
++export NAN_PYTHON ?= /usr/local
++export NAN_PYTHON_VERSION ?= 2.5
++export NAN_OPENAL ?= /usr/local
++export NAN_FMOD ?= /usr/local
++export NAN_JPEG ?= /usr/local
++export NAN_PNG ?= /usr/local
++export NAN_GLEW ?= /usr/local
++export WITH_OPENEXR = true
++export NAN_OPENEXR ?= /usr/local
++export NAN_OPENEXR_LIBS ?= $(NAN_OPENEXR)/lib/libIlmImf.a \
++	$(NAN_OPENEXR)/lib/libHalf.a \
++	$(NAN_OPENEXR)/lib/libIex.a
++export NAN_ZLIB ?= /usr/local
++export NAN_ODE ?= /usr/local
++export NAN_MESA = /export/scratch/crap/Mesa-6.2.1
++export NAN_NSPR = /export/scratch/blender/src/nspr-4.2.2/mozilla/nsprpub/dist
++export WITH_FREETYPE2 = true
++export NAN_NO_STATIC = true
++export NAN_NO_PLUGIN = true
++export NAN_FREETYPE ?= /usr/local
++export WITH_VERSE = true
