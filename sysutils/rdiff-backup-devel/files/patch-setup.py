29a93fe744cd3ae3d0880bdd40571d61fc0af0e3
vd <vd@FreeBSD.org>
2009-02-01 06:58:18 +0000

    Upgrade sysutils/rdiff-backup-devel from 1.1.15 to 1.3.1 (start tracking the new development branch)
    ChangeLog:
    http://www.nongnu.org/rdiff-backup/CHANGELOG-devel

--- sysutils/rdiff-backup-devel/files/patch-setup.py
+++ sysutils/rdiff-backup-devel/files/patch-setup.py
@@ -1,8 +1,8 @@
---- setup.py.orig	Wed Jan  4 09:00:26 2006
-+++ setup.py	Wed Jan  4 09:08:19 2006
-@@ -41,6 +41,10 @@
- 		if '-lrsync' in LIBS:
- 			libname = []
+--- setup.py.orig	2008-08-06 21:24:28.756047734 +0200
++++ setup.py	2008-08-06 21:24:18.122056264 +0200
+@@ -58,6 +58,10 @@
+ 				'zipfile': None
+ 			})
  
 +LOCALBASE = os.getenv("LOCALBASE", "/usr/local")
 +include_dirs = ["%s/include" % LOCALBASE]
@@ -11,21 +11,21 @@
  setup(name="rdiff-backup",
  	  version=version_string,
  	  description="Local/remote mirroring+incremental backup",
-@@ -51,13 +55,9 @@
+@@ -68,14 +72,8 @@
  	  ext_modules = [Extension("rdiff_backup.C", ["cmodule.c"]),
  					 Extension("rdiff_backup._librsync",
  							   ["_librsyncmodule.c"],
 -							   include_dirs=incdir_list,
 -							   library_dirs=libdir_list,
--							   libraries=libname,
--							   extra_link_args=lflags_arg)],
-+							   libraries=["rsync"],
 +							   include_dirs=include_dirs,
-+							   library_dirs=library_dirs)],
- 	  scripts = ['rdiff-backup', 'rdiff-backup-statistics'],
++							   library_dirs=library_dirs,
+ 							   libraries=libname,
+ 							   extra_link_args=lflags_arg)],
+-	  scripts = ['rdiff-backup', 'rdiff-backup-statistics'],
 -	  data_files = [('share/man/man1', ['rdiff-backup.1',
 -										'rdiff-backup-statistics.1']),
 -					('share/doc/rdiff-backup-%s' % (version_string,),
--					 ['CHANGELOG', 'COPYING', 'README', 'FAQ.html'])])
-+)
- 
+-					 ['CHANGELOG', 'COPYING', 'README', 'FAQ.html'])],
+-					**extra_options)
+-
++	  scripts = ['rdiff-backup', 'rdiff-backup-statistics'])
