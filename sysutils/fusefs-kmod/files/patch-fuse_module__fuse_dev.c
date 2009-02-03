4b3e6e952ba4816f9b661dfe30a262177aa57a65
ed <ed@FreeBSD.org>
2009-02-03 20:45:37 +0000

    Make the fusefs kernel module compile on -CURRENT again.
    The kernel module uses unit2minor(), which is not available on -CURRENT
    anymore. I'm not increasing PORTREVISION, because this commit should not
    have any effect on <800062 users.
    
    Approved by:	port maintainer

--- sysutils/fusefs-kmod/files/patch-fuse_module__fuse_dev.c
+++ sysutils/fusefs-kmod/files/patch-fuse_module__fuse_dev.c
@@ -15,3 +15,17 @@
  #endif
  };
  
+@@ -548,7 +553,12 @@
+ 	/* find any existing device, or allocate new unit number */
+ 	i = clone_create(&fuseclones, &fuse_cdevsw, &unit, dev, 0);
+ 	if (i) {
+-		*dev = make_dev(&fuse_cdevsw, unit2minor(unit),
++		*dev = make_dev(&fuse_cdevsw,
++#if __FreeBSD_version < 800062
++				unit2minor(unit),
++#else /* __FreeBSD_version >= 800062 */
++				unit,
++#endif /* __FreeBSD_version < 800062 */
+ 			        UID_ROOT, GID_OPERATOR,
+ 		                S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP,
+                                 "fuse%d", unit);
