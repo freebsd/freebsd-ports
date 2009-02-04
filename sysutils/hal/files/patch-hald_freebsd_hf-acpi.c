03536a5aa93fea380bba4991b952ffb46156b288
jkim <jkim@FreeBSD.org>
2009-02-04 17:45:32 +0000

    - Do not allow openning /dev/sysmouse multiple times.  When there are multiple instances of moused(8) and Xorg mouse driver opens /dev/sysmouse multiple times, Xorg server gets confused badly, e.g., losing input events, waiting for already processed events, etc. - Really fix laptop brightness control.  The previous commit didn't fix IBM and Sony laptops.  Pointyhat to me, again.
    Reviewed by:	marcus

--- sysutils/hal/files/patch-hald_freebsd_hf-acpi.c
+++ sysutils/hal/files/patch-hald_freebsd_hf-acpi.c
@@ -1,6 +1,50 @@
 --- hald/freebsd/hf-acpi.c.orig	2008-05-07 19:23:59.000000000 -0400
-+++ hald/freebsd/hf-acpi.c	2009-02-02 20:32:05.000000000 -0500
-@@ -105,6 +105,9 @@
++++ hald/freebsd/hf-acpi.c	2009-02-04 11:32:27.000000000 -0500
+@@ -54,6 +54,7 @@
+ static const struct laptop_panel_type {
+   char *access;
+   char *name;
++  char *get_sysctl;
+   char *max_sysctl;
+   int max_levels;
+ #define HF_ACPI_IBM_MAX_LEVELS		8
+@@ -64,17 +65,29 @@
+ #define HF_ACPI_FUJITSU_MAX_LEVELS	8
+   /* NOTE: Each new type must also be added to hf-devtree.c */
+ } laptop_panel_types[] = {
+-  { "ibm",		"IBM",		NULL,
++  { "ibm",		"IBM",
++    "dev.acpi_ibm.0.lcd_brightness",
++    NULL,
+     HF_ACPI_IBM_MAX_LEVELS },
+-  { "toshiba",		"Toshiba",	NULL,
++  { "toshiba",		"Toshiba",
++    "hw.acpi.toshiba.lcd_brightness",
++    NULL,
+     HF_ACPI_TOSHIBA_MAX_LEVELS },
+-  { "sony",		"Sony",		NULL,
++  { "sony",		"Sony",
++    "dev.acpi_sony.0.brightness",
++    NULL,
+     HF_ACPI_SONY_MAX_LEVELS },
+-  { "panasonic",	"Panasonic",	"hw.acpi.panasonic.lcd_brightness_max",
++  { "panasonic",	"Panasonic",
++    "hw.acpi.panasonic.lcd_brightness",
++    "hw.acpi.panasonic.lcd_brightness_max",
+     HF_ACPI_PANASONIC_MAX_LEVELS },
+-  { "asus",		"Asus",		NULL,
++  { "asus",		"Asus",
++    "hw.acpi.asus.lcd_brightness",
++    NULL,
+     HF_ACPI_ASUS_MAX_LEVELS },
+-  { "fujitsu",		"Fujitsu",	NULL,
++  { "fujitsu",		"Fujitsu",
++    "hw.acpi.fujitsu.lcd_brightness",
++    NULL,
+     HF_ACPI_FUJITSU_MAX_LEVELS }
+ };
+ 
+@@ -105,6 +118,9 @@
    gboolean ispresent;
    union acpi_battery_ioctl_arg battif, battst, battinfo;
  
@@ -10,25 +54,31 @@
    battif.unit = battst.unit = battinfo.unit =
      hal_device_property_get_int(device, "freebsd.unit");
  
-@@ -500,10 +503,17 @@
-                           const char *max_sysctl, const char *access,
-                           const char *name)
+@@ -497,13 +513,15 @@
+ 
+ static HalDevice *
+ hf_acpi_laptop_panel_new (HalDevice *parent, int max_levels,
+-                          const char *max_sysctl, const char *access,
+-                          const char *name)
++                          const char *get_sysctl, const char *max_sysctl,
++                          const char *access, const char *name)
  {
-+  char *brightness;
    HalDevice *device;
  
    g_return_val_if_fail(HAL_IS_DEVICE(parent), NULL);
  
-+  if (! strcmp(access, "sony"))
-+    brightness = "brightness";
-+  else
-+    brightness = "lcd_brightness";
-+  if (! hf_has_sysctl("hw.acpi.%s.%s", access, brightness))
++  if (get_sysctl == NULL || ! hf_has_sysctl(get_sysctl))
 +    return NULL;
    device = hf_device_new(parent);
  
    hf_device_property_set_string_printf(device, "info.product", "Laptop Panel (%s)", name);
-@@ -597,7 +607,8 @@
+@@ -593,11 +611,13 @@
+               HalDevice *panel_device;
+ 
+ 	      panel_device = hf_acpi_laptop_panel_new(parent,
+-                                                      laptop_panel_types[i].max_levels,
++						      laptop_panel_types[i].max_levels,
++						      laptop_panel_types[i].get_sysctl,
  						      laptop_panel_types[i].max_sysctl,
  						      laptop_panel_types[i].access,
  						      laptop_panel_types[i].name);
