ed5e8719bc793ed0dd14171e254d343b52e68052
kuriyama <kuriyama@FreeBSD.org>
2009-02-04 22:55:46 +0000

    - Upgrade to 2.6.1. - Turn on SSH auth by default. - Sorry, previous default path of $AMANDA_DATES was typo.  Fixed here   it should be.  Please take care when upgrading.
    PR:	ports/131193 (partially merged to my update modifications) [1]
    Submitted by:	Goran Lowkrantz <glz@hidden-powers.com> [1]

--- misc/amanda32-server/files/extra-patch-sendbackup-dump.c
+++ misc/amanda32-server/files/extra-patch-sendbackup-dump.c
@@ -1,5 +1,5 @@
---- client-src/sendbackup-dump.c.orig	Fri Sep  8 13:09:14 2006
-+++ client-src/sendbackup-dump.c	Fri Sep  8 13:15:44 2006
+--- client-src/sendbackup-dump.c.orig	2008-12-13 04:18:11.000000000 +0900
++++ client-src/sendbackup-dump.c	2009-01-27 07:30:40.000000000 +0900
 @@ -35,6 +35,10 @@
  #include "clock.h"
  #include "version.h"
@@ -11,26 +11,13 @@
  #define LEAF_AND_DIRS "sed -e \'\ns/^leaf[ \t]*[0-9]*[ \t]*\\.//\nt\n/^dir[ \t]/ {\ns/^dir[ \t]*[0-9]*[ \t]*\\.//\ns%$%/%\nt\n}\nd\n\'"
  
  static amregex_t re_table[] = {
-@@ -370,6 +374,12 @@
- 
- 	dumpkeys = vstralloc(level_str,
- 			     options->no_record ? "" : "u",
-+#ifdef __FreeBSD__
-+			     "b",
-+#if defined(__FreeBSD_version) && (__FreeBSD_version >= 500043)
-+			     "L",
-+#endif
-+#endif
- 			     "s",
- #ifdef HAVE_HONOR_NODUMP
- 			     "h",
-@@ -392,6 +402,9 @@
- 			    &dumpin, &dumpout, &mesgf, 
- 			    cmd, config,
+@@ -310,6 +314,9 @@
+ 			    cmdX, config,
+ 			    "vxdump",
  			    dumpkeys,
 +#ifdef __FreeBSD__
 +			    "64",
 +#endif
  			    "1048576",
- #ifdef HAVE_HONOR_NODUMP
- 			    "0",
+ 			    "-",
+ 			    device,
