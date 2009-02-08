2acab2f82457d510c53ec88cc90c3351693aaa6b
Gabor Kovesdan <gabor@FreeBSD.org>
2009-02-08 15:46:37 +0000

    - Update to 1.2.8 - Fix installed Hungarian dictionary locating error
    PR:		ports/131366
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu> (maintainer)

--- textproc/hunspell/files/patch-src_tools_hunspell.cxx
+++ textproc/hunspell/files/patch-src_tools_hunspell.cxx
@@ -1,6 +1,19 @@
---- src/tools/hunspell.cxx	2008-08-15 06:06:33.000000000 -0400
-+++ src/tools/hunspell.cxx	2008-08-26 06:24:36.000000000 -0400
-@@ -70,18 +70,12 @@
+
+$FreeBSD$
+
+--- src/tools/hunspell.cxx.orig
++++ src/tools/hunspell.cxx
+@@ -63,25 +63,21 @@
+ #include "firstparser.hxx"
+ 
+ #define LIBDIR \
+-    "/usr/share/hunspell:" \
+-    "/usr/share/myspell:" \
+-    "/usr/share/myspell/dicts"
++    "%%PREFIX%%/share/hunspell:" \
++    "%%PREFIX%%/share/myspell:" \
++    "%%PREFIX%%/share/myspell/dicts"
+ #define USEROOODIR \
      ".openoffice.org2/user/wordbook:" \
      ".openoffice.org2.0/user/wordbook"
  #define OOODIR \
@@ -16,11 +29,13 @@
 -    "/usr/lib/openoffice.org2.1/share/dict/ooo:" \
 -    "/opt/openoffice.org2.0/share/dict/ooo:" \
 -    "/usr/lib/openoffice.org2.0/share/dict/ooo"
-+	"%%PREFIX%%/openoffice.org-DEV300_m29/share/dict/ooo:"	\
-+	"%%PREFIX%%/openoffice.org-3.0.0.b2/share/dict/ooo:"	\
-+	"%%PREFIX%%/openoffice.org-2.4/share/dict/ooo:"	\
-+	"%%PREFIX%%/openoffice.org-2.2/share/dict/ooo:"	\
-+	"%%PREFIX%%/openoffice.org-2.1/share/dict/ooo:"	\
++	"%%PREFIX%%/openoffice.org-DEV300_m41/share/dict/ooo:"  \
++	"%%PREFIX%%/openoffice.org-3.0.0/share/dict/ooo:"    \
++	"%%PREFIX%%/openoffice.org-2.4.2/share/dict/ooo:" \
++	"%%PREFIX%%/openoffice.org-2.4.1/share/dict/ooo:" \
++	"%%PREFIX%%/openoffice.org-2.4/share/dict/ooo:" \
++	"%%PREFIX%%/openoffice.org-2.2/share/dict/ooo:" \
++	"%%PREFIX%%/openoffice.org-2.1/share/dict/ooo:" \
 +	"%%PREFIX%%/openoffice.org2.0/share/dict/ooo"
  #define HOME getenv("HOME")
  #define DICBASENAME ".hunspell_"
