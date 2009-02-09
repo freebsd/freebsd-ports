bd1dea88ed21980d02cbf259d7e7df87ff032e5d
stas <stas@FreeBSD.org>
2009-02-09 17:49:51 +0000

    - Update ruby19 to 1.9.1.
    PR:		ports/129818 (based on)
    Submitted by:	Hung-Yi Chen <gaod@hychen.org>

--- lang/ruby19/files/patch-mkconfig.rb
+++ lang/ruby19/files/patch-mkconfig.rb
@@ -1,11 +1,11 @@
---- mkconfig.rb.orig	2007-12-28 02:40:14.000000000 +0300
-+++ mkconfig.rb	2007-12-28 02:40:25.000000000 +0300
-@@ -138,7 +138,7 @@
+--- mkconfig.rb.orig	2009-02-09 19:56:48.000000000 +0300
++++ mkconfig.rb	2009-02-09 19:59:30.000000000 +0300
+@@ -140,7 +140,7 @@
  print(*v_fast)
  print(*v_others)
  print <<EOS
--  CONFIG["ruby_version"] = "$(MAJOR).$(MINOR).$(TEENY)"
-+  CONFIG["ruby_version"] = "$(MAJOR).$(MINOR)"
-   CONFIG["rubylibdir"] = "$(libdir)/ruby/$(ruby_version)"
+-  CONFIG["rubylibdir"] = "$(libdir)/$(ruby_install_name)/$(ruby_version)"
++  CONFIG["rubylibdir"] = "$(libdir)/ruby/$(ruby_version)"
    CONFIG["archdir"] = "$(rubylibdir)/$(arch)"
    CONFIG["sitelibdir"] = "$(sitedir)/$(ruby_version)"
+   CONFIG["sitearchdir"] = "$(sitelibdir)/$(sitearch)"
