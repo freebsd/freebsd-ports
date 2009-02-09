5ed349d3d4112d87080033723b780a8d9e224536
miwi <miwi@FreeBSD.org>
2009-02-09 20:10:15 +0000

    pyliblo is a Python wrapper for the liblo OSC library. It supports almost the complete functionality of liblo, allowing you to send and receive OSC messages using a nice and simple Python API.
    Also included are the command line utilities send_osc and
    dump_osc.
    
    WWW:	http://das.nasophon.de/pyliblo/
    
    PR:		ports/130975
    Submitted by:	Wen Heping <wenheping at gmail.com>

---
+++ audio/py-pyliblo/files/patch-setup.py
@@ -0,0 +1,30 @@
+--- setup.py.orig	2009-01-25 16:03:31.000000000 +0800
++++ setup.py	2009-01-25 16:04:35.000000000 +0800
+@@ -34,7 +34,7 @@
+ ext_modules = [
+     Extension('liblo',
+               [with_pyrex and 'src/liblo.pyx' or 'src/liblo.c'],
+-              extra_compile_args = ['-fno-strict-aliasing'],
++              extra_compile_args = ['-fno-strict-aliasing','-I/usr/local/include'],
+               libraries = ['lo'])
+ ]
+ 
+@@ -54,12 +54,12 @@
+         'scripts/send_osc.py',
+         'scripts/dump_osc.py',
+     ],
+-    data_files = [
+-        ('share/man/man1', [
+-            'scripts/send_osc.1',
+-            'scripts/dump_osc.1',
+-        ]),
+-    ],
++#    data_files = [
++#        ('share/man/man1', [
++#            'scripts/send_osc.1',
++#            'scripts/dump_osc.1',
++#        ]),
++#    ],
+     cmdclass = cmdclass,
+     ext_modules = ext_modules
+ )
