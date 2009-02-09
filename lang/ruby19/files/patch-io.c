ffe7a6123957624b12dfcc9429855fe9271e2a72
Stanislav Sedov <stas@FreeBSD.org>
2009-02-09 17:49:51 +0000

    - Update ruby19 to 1.9.1.
    PR:		ports/129818 (based on)
    Submitted by:	Hung-Yi Chen <gaod@hychen.org>

--- lang/ruby19/files/patch-io.c
+++ lang/ruby19/files/patch-io.c
@@ -1,18 +1,18 @@
---- io.c.orig	2007-12-28 02:17:32.000000000 +0300
-+++ io.c	2007-12-28 02:17:58.000000000 +0300
-@@ -5324,7 +5324,7 @@
+--- io.c.orig	2009-01-28 12:20:46.000000000 +0300
++++ io.c	2009-02-09 19:18:58.000000000 +0300
+@@ -6685,7 +6685,7 @@
+ }
  
- #if !defined(MSDOS) && !defined(__human68k__)
  static int
 -io_cntl(int fd, int cmd, long narg, int io_p)
 +io_cntl(int fd, unsigned long cmd, long narg, int io_p)
  {
      int retval;
  
-@@ -5352,7 +5352,7 @@
+@@ -6707,7 +6707,7 @@
+ static VALUE
  rb_io_ctl(VALUE io, VALUE req, VALUE arg, int io_p)
  {
- #if !defined(MSDOS) && !defined(__human68k__)
 -    int cmd = NUM2ULONG(req);
 +    unsigned long cmd = NUM2ULONG(req);
      rb_io_t *fptr;
