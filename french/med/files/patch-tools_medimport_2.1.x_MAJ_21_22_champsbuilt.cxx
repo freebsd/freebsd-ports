602a05df809049afbf9c0c0a38a162620f118f38
Thierry Thomas <thierry@FreeBSD.org>
2009-02-10 15:04:40 +0000

    Fix compilation with gcc-43.
    Reported by:	pointyhat via pav

---
+++ french/med/files/patch-tools_medimport_2.1.x_MAJ_21_22_champsbuilt.cxx
@@ -0,0 +1,11 @@
+--- ./tools/medimport/2.1.x/MAJ_21_22_champsbuilt.cxx.orig	2008-10-22 15:31:25.000000000 +0200
++++ ./tools/medimport/2.1.x/MAJ_21_22_champsbuilt.cxx	2009-02-10 11:27:28.000000000 +0100
+@@ -16,6 +16,8 @@
+ *
+ *************************************************************************/
+ 
++#include <string.h>
++
+ #define MESGERR
+ 
+ #ifdef __cplusplus
---
+++ french/med/files/patch-tools_medimport_2.3.1_MED231champNormaliserbuilt.cxx
@@ -0,0 +1,11 @@
+--- ./tools/medimport/2.3.1/MED231champNormaliserbuilt.cxx.orig	2008-10-22 15:31:32.000000000 +0200
++++ ./tools/medimport/2.3.1/MED231champNormaliserbuilt.cxx	2009-02-10 15:31:10.000000000 +0100
+@@ -16,6 +16,8 @@
+ *
+ *************************************************************************/
+ 
++#include <string.h>
++
+ #define MESGERR
+ 
+ #ifdef __cplusplus
