a758e776b45fa6ddacb77d79faa0b7cc9d9eab0c
erwin <erwin@FreeBSD.org>
2009-02-11 21:17:53 +0000

    Fiddle a bit with __FreeBSD_version after strndup(3) was MFCed.
    Submitted by:	pointyhat
    Approved by:	miwi

--- devel/xdg-user-dirs/files/patch-xdg-user-dirs-update.c
+++ devel/xdg-user-dirs/files/patch-xdg-user-dirs-update.c
@@ -6,7 +6,7 @@
  
 +#if defined(__FreeBSD__)
 +#include <osreldate.h>
-+#if __FreeBSD_version <= 800057
++#if (__FreeBSD_version >= 800000 && __FreeBSD_version <= 800057) || __FreeBSD_version <= 701101
 +static char *strndup(const char *str, size_t len) {
 +	char *ret;
 +	
