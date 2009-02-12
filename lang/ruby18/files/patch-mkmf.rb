8047098399c69eb7af22e7b472656df9a06e76ba
Stanislav Sedov <stas@FreeBSD.org>
2009-02-12 14:24:56 +0000

    - Update ruby18 to 1.8.7p72.
    PR:		ports/124837 (based on)
    Submitted by:	KIMURA Yasuhiro <yasu@utahime.org>

--- lang/ruby18/files/patch-mkmf.rb
+++ lang/ruby18/files/patch-mkmf.rb
@@ -1,20 +1,22 @@
---- lib/mkmf.rb.orig	Thu Aug 17 09:47:50 2006
-+++ lib/mkmf.rb	Wed Aug 30 13:14:44 2006
-@@ -113,8 +113,12 @@
+--- lib/mkmf.rb.orig	2009-02-12 01:18:14.000000000 +0300
++++ lib/mkmf.rb	2009-02-12 01:19:47.000000000 +0300
+@@ -132,9 +132,13 @@
  
  topdir = File.dirname(libdir = File.dirname(__FILE__))
  extdir = File.expand_path("ext", topdir)
 +setuparchdir = topdir + \
 +  "/#{Config::CONFIG["ruby_version"]}/#{Config::CONFIG["arch"]}"
  $extmk = File.expand_path($0)[0, extdir.size+1] == extdir+"/"
--if not $extmk and File.exist?(Config::CONFIG["archdir"] + "/ruby.h")
+-if not $extmk and File.exist?(($hdrdir = Config::CONFIG["archdir"]) + "/ruby.h")
+-  $topdir = $hdrdir
 +if not $extmk and File.exist?(setuparchdir + "/ruby.h")
 +  $hdrdir = $topdir = setuparchdir
 +elsif not $extmk and File.exist?(Config::CONFIG["archdir"] + "/ruby.h")
-   $hdrdir = $topdir = Config::CONFIG["archdir"]
- elsif File.exist?(($top_srcdir ||= topdir)  + "/ruby.h") and
++  $topdir = $hdrdir = Config::CONFIG["archdir"]
+ elsif File.exist?(($hdrdir = ($top_srcdir ||= topdir))  + "/ruby.h") and
      File.exist?(($topdir ||= Config::CONFIG["topdir"]) + "/config.h")
-@@ -1281,7 +1285,7 @@
+ else
+@@ -1554,7 +1558,7 @@
  
    mfile.print <<-SITEINSTALL
  
