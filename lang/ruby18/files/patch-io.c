27cc4b05c71a823fab4caf233004c312231cfce3
stas <stas@FreeBSD.org>
2009-02-12 14:24:56 +0000

    - Update ruby18 to 1.8.7p72.
    PR:		ports/124837 (based on)
    Submitted by:	KIMURA Yasuhiro <yasu@utahime.org>

--- lang/ruby18/files/patch-io.c
+++ lang/ruby18/files/patch-io.c
@@ -1,6 +1,6 @@
---- io.c.orig	Thu Mar  2 22:15:47 2006
-+++ io.c	Thu Mar  2 22:16:58 2006
-@@ -4682,8 +4682,9 @@
+--- io.c.orig	2008-06-08 22:20:37.000000000 +0400
++++ io.c	2009-02-12 01:10:27.000000000 +0300
+@@ -4970,8 +4970,9 @@
  #if !defined(MSDOS) && !defined(__human68k__)
  static int
  io_cntl(fd, cmd, narg, io_p)
@@ -11,12 +11,12 @@
  {
      int retval;
  
-@@ -4713,7 +4714,7 @@
+@@ -5001,7 +5002,7 @@
      int io_p;
  {
  #if !defined(MSDOS) && !defined(__human68k__)
 -    int cmd = NUM2ULONG(req);
 +    unsigned long cmd = NUM2ULONG(req);
-     OpenFile *fptr;
+     rb_io_t *fptr;
      long len = 0;
      long narg = 0;
