9a0f6d078614ab76bf3fa8435968e58a7945d271
miwi <miwi@FreeBSD.org>
2009-02-14 19:25:53 +0000

    Dynagui is a GUI frontend to the dynamips Cisco 7200/36XX router simulator. It uses the dyna-gen library in order to communicate with the dynamips hypervisor.
    WWW: http://dynagui.sourceforge.net
    
    PR:		ports/131544
    Submitted by:	Andrew Greenwood

---
+++ emulators/dynagui/files/patch-dynagui.py
@@ -0,0 +1,40 @@
+--- dynagui.py.orig	2007-12-13 16:04:43.000000000 -0500
++++ dynagui.py	2009-02-05 18:42:36.000000000 -0500
+@@ -27,17 +27,17 @@
+ import pygtk; pygtk.require('2.0')
+ import gtk
+ import gobject
+-from libs.canvas import MyCanvas
+-from libs import lab_io
+-from libs.lab import lab
+-from libs.gui_router import GuiRouter
+-from libs.dynamips_properties_dialog import DynamipsPropertiesDialog
+-from libs.C3600_properties_dialog import C3600PropertiesDialog
+-from libs.C7200_properties_dialog import C7200PropertiesDialog
+-from libs.frsw_properties_dialog import FRSwitchPropertiesDialog
+-from libs.atmsw_properties_dialog import ATMSwitchPropertiesDialog
+-from libs.ethsw_properties_dialog import EthSwitchPropertiesDialog
+-from libs.util import RouterSelectionDialog
++from dynagui_libs.canvas import MyCanvas
++from dynagui_libs import lab_io
++from dynagui_libs.lab import lab
++from dynagui_libs.gui_router import GuiRouter
++from dynagui_libs.dynamips_properties_dialog import DynamipsPropertiesDialog
++from dynagui_libs.C3600_properties_dialog import C3600PropertiesDialog
++from dynagui_libs.C7200_properties_dialog import C7200PropertiesDialog
++from dynagui_libs.frsw_properties_dialog import FRSwitchPropertiesDialog
++from dynagui_libs.atmsw_properties_dialog import ATMSwitchPropertiesDialog
++from dynagui_libs.ethsw_properties_dialog import EthSwitchPropertiesDialog
++from dynagui_libs.util import RouterSelectionDialog
+ 
+ 
+ ui_popup = \
+@@ -509,7 +509,7 @@
+         if len(lab.devices)>0:
+             text = 'Select routers from which to extract configuration'
+             rcd = RouterSelectionDialog(lab.devices, text, show_path=True)
+-            from libs.nvram_io import read_conf_b64gz
++            from dynagui_libs.nvram_io import read_conf_b64gz
+             for d in rcd.selected_devices:
+                 cgz64 = d.config_gz64
+                 conf = read_conf_b64gz(cgz64)
---
+++ emulators/dynagui/files/patch-libs_canvas.py
@@ -0,0 +1,35 @@
+--- libs/canvas.py.orig	2007-09-24 17:33:03.000000000 -0400
++++ libs/canvas.py	2009-02-05 18:43:07.000000000 -0500
+@@ -25,19 +25,19 @@
+ import time
+ from gtk import gdk
+ import dynamips_lib
+-from libs.lab import lab, Connection
+-from libs.util import setup_ghost
+-from libs.gui_c3600 import GuiC3600
+-from libs.gui_c7200 import GuiC7200
+-from libs.gui_router import GuiRouter
+-from libs.gui_frsw import GuiFRSwitch
+-from libs.gui_atmsw import GuiATMSwitch
+-from libs.gui_ethsw import GuiEthSwitch
+-from libs.C3600_properties_dialog import C3600PropertiesDialog
+-from libs.C7200_properties_dialog import C7200PropertiesDialog
+-from libs.frsw_properties_dialog import FRSwitchPropertiesDialog
+-from libs.atmsw_properties_dialog import ATMSwitchPropertiesDialog
+-from libs.ethsw_properties_dialog import EthSwitchPropertiesDialog
++from dynagui_libs.lab import lab, Connection
++from dynagui_libs.util import setup_ghost
++from dynagui_libs.gui_c3600 import GuiC3600
++from dynagui_libs.gui_c7200 import GuiC7200
++from dynagui_libs.gui_router import GuiRouter
++from dynagui_libs.gui_frsw import GuiFRSwitch
++from dynagui_libs.gui_atmsw import GuiATMSwitch
++from dynagui_libs.gui_ethsw import GuiEthSwitch
++from dynagui_libs.C3600_properties_dialog import C3600PropertiesDialog
++from dynagui_libs.C7200_properties_dialog import C7200PropertiesDialog
++from dynagui_libs.frsw_properties_dialog import FRSwitchPropertiesDialog
++from dynagui_libs.atmsw_properties_dialog import ATMSwitchPropertiesDialog
++from dynagui_libs.ethsw_properties_dialog import EthSwitchPropertiesDialog
+ 
+ term = 'xterm'          # linux terminal
+ win_telnet = "telnet "  # windows terminal
