9ab5d0dc52f11c516e37116521b947038d98ec65
Edward Tomasz Napierala <trasz@FreeBSD.org>
2009-02-15 10:01:07 +0000

    Update to V3.8-1.

--- emulators/simh/files/patch-VAX-vax_sysdev.c
+++ emulators/simh/files/patch-VAX-vax_sysdev.c
@@ -1,17 +1,18 @@
---- VAX/vax_sysdev.c.orig	Sat Mar 24 09:13:42 2007
-+++ VAX/vax_sysdev.c	Sat Mar 24 09:17:45 2007
-@@ -1524,8 +1524,15 @@
-     if (sim_log) fprintf (sim_log, 
-         "Loading boot code from ka655x.bin\n");
+--- VAX/vax_sysdev.c.orig	2008-11-23 11:57:54.000000000 +0100
++++ VAX/vax_sysdev.c	2009-02-12 06:24:49.000000000 +0100
+@@ -1553,9 +1553,15 @@
+     if (sim_log)
+         fprintf (sim_log, "Loading boot code from ka655x.bin\n");
      r = load_cmd (0, "-R ka655x.bin");
--    if (r != SCPE_OK) return r;
+-    if (r != SCPE_OK)
+-        return r;
 +    if (r != SCPE_OK) {
-+        printf ("Loading boot code from %%PREFIX%%/share/simh/ka655x.bin\n");
-+        if (sim_log) fprintf (sim_log, 
-+            "Loading boot code from %%PREFIX%%/share/simh/ka655x.bin\n");
-+        r = load_cmd (0, "-R %%PREFIX%%/share/simh/ka655x.bin");
++        printf ("Loading boot code from /usr/local/share/simh/ka655x.bin\n");
++        if (sim_log)
++	    fprintf (sim_log, "Loading boot code from /usr/local/share/simh/ka655x.bin\n");
++        r = load_cmd (0, "-R /usr/local/share/simh/ka655x.bin");
 +        if (r != SCPE_OK)
-+    	    return r;
++            return r;
      }
 +}
  return SCPE_OK;
