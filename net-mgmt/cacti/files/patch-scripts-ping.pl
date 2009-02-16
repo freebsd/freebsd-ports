55ec2e1fd97207f04229c19d7dc1ecc014d5dd96
sem <sem@FreeBSD.org>
2009-02-16 16:43:39 +0000

    - Update to 0.8.7d

--- net-mgmt/cacti/files/patch-scripts-ping.pl
+++ net-mgmt/cacti/files/patch-scripts-ping.pl
@@ -1,10 +1,11 @@
---- cacti-0.8.6j/scripts/ping.pl.orig	Sat Apr 15 22:26:44 2006
-+++ cacti-0.8.6j/scripts/ping.pl	Sat Apr 15 22:27:16 2006
-@@ -1,6 +1,6 @@
- #!/usr/bin/perl 
+--- cacti-0.8.7d/scripts/ping.pl.orig	2009-02-12 05:55:50.000000000 +0300
++++ cacti-0.8.7d/scripts/ping.pl	2009-02-16 17:01:59.000000000 +0300
+@@ -4,7 +4,7 @@
+ $host = $ARGV[0];
+ $host =~ s/tcp:/$1/gis;
  
--open(PROCESS, "ping -c 1 $ARGV[0] | grep icmp_seq |");
-+open(PROCESS, "/sbin/ping -c 1 $ARGV[0] | grep icmp_seq |");
+-open(PROCESS, "ping -c 1 $host | grep icmp_seq | grep time |");
++open(PROCESS, "/sbin/ping -c 1 $host | grep icmp_seq | grep time |");
  $ping = <PROCESS>;
  close(PROCESS);
- $ping =~ m/(.*time=)(.*) (ms|usec)/; 
+ $ping =~ m/(.*time=)(.*) (ms|usec)/;
