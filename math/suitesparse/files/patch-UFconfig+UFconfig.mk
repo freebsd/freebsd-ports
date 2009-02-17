9ea99774b59ff689094c5d8205690b3f5e62ae0d
maho <maho@FreeBSD.org>
2009-02-17 04:48:35 +0000

    Update to 3.2.0 and build fix with gcc43.
    Spotted by:	pav, brooks

--- math/suitesparse/files/patch-UFconfig+UFconfig.mk
+++ math/suitesparse/files/patch-UFconfig+UFconfig.mk
@@ -1,40 +1,43 @@
---- UFconfig/UFconfig.mk.orig	2007-10-24 22:55:44.000000000 +0900
-+++ UFconfig/UFconfig.mk	2008-02-03 12:04:00.000000000 +0900
-@@ -32,8 +32,8 @@
+--- UFconfig/UFconfig.mk~	2009-02-16 14:40:18.000000000 +0900
++++ UFconfig/UFconfig.mk	2009-02-16 14:44:33.000000000 +0900
+@@ -33,11 +33,11 @@
  # C compiler and compiler flags:  These will normally not give you optimal
  # performance.  You should select the optimization parameters that are best
  # for your system.  On Linux, use "CFLAGS = -O3 -fexceptions" for example.
 -CC = cc
--CFLAGS = -O
+-# CFLAGS = -O   (for example; see below for details)
 +CC = %%CC%%
 +CFLAGS = %%CFLAGS%%
  
+ # C++ compiler (also uses CFLAGS)
+-CPLUSPLUS = g++
++CPLUSPLUS = %%CXX%%
+ 
  # ranlib, and ar, for generating libraries
  RANLIB = ranlib
-@@ -44,8 +44,9 @@
+@@ -48,8 +48,8 @@
  MV = mv -f
  
  # Fortran compiler (not normally required)
 -F77 = f77
 -F77FLAGS = -O
 +F77 = %%F77%%
-+FC  = %%FC%%
 +F77FLAGS = %%FFLAGS%%
  F77LIB =
  
  # C and Fortran libraries
-@@ -85,8 +86,8 @@
- # BLAS = -lgoto -lgfortran -lgfortranbegin
+@@ -89,8 +89,8 @@
+ # BLAS = -lgoto -lgfortran -lgfortranbegin -lg2c
  
  # This is probably slow ... it might connect to the Standard Reference BLAS:
--BLAS = -lblas -lgfortran -lgfortranbegin
+-BLAS = -lblas -lgfortran -lgfortranbegin -lg2c
 -LAPACK = -llapack
-+BLAS = %%BLAS%%
++BLAS = %%BLAS%% %%FORTRANRUNTIME%%
 +LAPACK = %%LAPACK%%
  
- # The BLAS might not contain xerbla, an error-handling routine for LAPACK and
- # the BLAS.  Also, the standard xerbla requires the Fortran I/O library, and
-@@ -114,8 +115,8 @@
+ # Using non-optimized versions:
+ # BLAS = -lblas_plain -lgfortran -lgfortranbegin -lg2c
+@@ -122,8 +122,8 @@
  # The path is relative to where it is used, in CHOLMOD/Lib, CHOLMOD/MATLAB, etc.
  # You may wish to use an absolute path.  METIS is optional.  Compile
  # CHOLMOD with -DNPARTITION if you do not wish to use METIS.
@@ -45,16 +48,7 @@
  
  # If you use CHOLMOD_CONFIG = -DNPARTITION then you must use the following
  # options:
-@@ -138,7 +139,7 @@
- # -DNRECIPROCAL	do not multiply by the reciprocal
- # -DNO_DIVIDE_BY_ZERO	do not divide by zero
- 
--UMFPACK_CONFIG = 
-+UMFPACK_CONFIG = -DGETRUSAGE
- 
- #------------------------------------------------------------------------------
- # CHOLMOD configuration
-@@ -177,7 +177,7 @@
+@@ -184,7 +184,7 @@
  # -DNSUNPERF	    for Solaris only.  If defined, do not use the Sun
  #			Performance Library
  
@@ -62,13 +56,22 @@
 +CHOLMOD_CONFIG = %%CHOLMOD_CONFIG%%
  
  #------------------------------------------------------------------------------
- # Linux
-@@ -184,15 +185,15 @@
+ # SuiteSparseQR configuration:
+@@ -198,7 +198,7 @@
+ # -DHAVE_TBB        enable the use of Intel's Threading Building Blocks (TBB)
+ 
+ # default, without timing, without TBB:
+-SPQR_CONFIG =
++SPQR_CONFIG = %%CHOLMOD_CONFIG%%
+ # with timing and TBB:
+ # SPQR_CONFIG = -DTIMING -DHAVE_TBB
+ # with timing
+@@ -220,15 +220,15 @@
  
  # Using default compilers:
  # CC = gcc
--CFLAGS = -O3
-+# CFLAGS = -O3
+-CFLAGS = -O3 -fexceptions
++# CFLAGS = -O3 -fexceptions
  
  # alternatives:
  # CFLAGS = -g -fexceptions \
@@ -83,3 +86,5 @@
 +#    	-Wredundant-decls -Wnested-externs -Wdisabled-optimization -ansi
  # CFLAGS = -O3 -fexceptions -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE
  # CFLAGS = -O3
+ # CFLAGS = -O3 -g -fexceptions
+
