4a022b4c9c0860e617b799d59c1eb9c50a6053f1
beech <beech@FreeBSD.org>
2009-02-17 18:09:08 +0000

    - Update to 1.6
    PR:		ports/131694
    Submitted by:	Milan Obuch <bsd@dino.sk> (maintainer)

--- mail/courier-pythonfilter/files/patch-filters__clamav.py
+++ mail/courier-pythonfilter/files/patch-filters__clamav.py
@@ -1,18 +1,11 @@
---- filters/clamav.py.orig	Sat Dec 16 03:11:51 2006
-+++ filters/clamav.py	Sun Jan  7 10:56:30 2007
-@@ -31,13 +31,13 @@
-     except Exception, e:
-         return "554 " + str(e)
-     if avresult[0]:
--        return "554 %s was detected. Abort!" % avresult[1]
-+        return "554 Virus found - Signature is %s" % avresult[1]
-     return ''
+--- filters/clamav.py.orig	2008-03-23 09:06:04.000000000 +0100
++++ filters/clamav.py	2008-12-30 14:45:03.000000000 +0100
+@@ -22,7 +22,7 @@
+ import courier.config
+ import courier.quarantine
  
+-localSocket = ''
++localSocket = '/var/run/clamav/clamd.sock'
+ action = 'reject'
  
- if __name__ == '__main__':
-     # we only work with 1 parameter
-     if len(sys.argv) != 2:
--        print "Usage: attachment.py <message_body_file>"
-+        print "Usage: clamav.py <message_body_file>"
-         sys.exit(0)
-     print doFilter(sys.argv[1], "")
+ try:
