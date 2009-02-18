9f190b75819e83e32c16a56fa10135b4d81b151f
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2009-02-18 13:44:10 +0000

    Apache 2.2 now sets the APR version as 1.3.0. mod_lisp2 looks for an APR version of 1.2.2-1.2.inf. This patch allows it to correctly see the bump in the minor, allowing mod_lisp2 to compile against apache22.
    PR:		ports/131649
    Submitted by:	Erik Greenwald
    Approved by:	maintainer

--- www/mod_lisp2/files/patch-mod_lisp2.c
+++ www/mod_lisp2/files/patch-mod_lisp2.c
@@ -1,6 +1,6 @@
---- mod_lisp2.c.orig	2008-06-17 17:51:40.000000000 +0200
-+++ mod_lisp2.c	2008-06-17 17:53:20.000000000 +0200
-@@ -153,7 +153,7 @@
+--- mod_lisp2.c.orig	2007-07-17 09:11:05.000000000 -0400
++++ mod_lisp2.c	2009-02-13 10:30:48.000000000 -0500
+@@ -153,11 +153,11 @@
  #include <stdlib.h>
  #include <string.h>
  
@@ -9,6 +9,11 @@
  /* Work out the version of the apache portable runtime (APR) we're
   * compiling against... with version 1.2.2 some of the interfaces
   * changed a bit. */
+-#if (APR_MAJOR_VERSION==1 && APR_MINOR_VERSION==2 && APR_PATCH_VERSION>=2)
++#if (APR_MAJOR_VERSION==1 && ((APR_MINOR_VERSION==2 && APR_PATCH_VERSION>=2) || APR_MINOR_VERSION>=3))
+ #define HAVE_APR_1_2_2    1
+ #endif
+ 
 @@ -576,7 +576,7 @@
  lisp_handler (request_rec * r)
  {
