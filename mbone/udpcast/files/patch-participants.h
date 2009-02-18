c75a02662a1fc5fb69774114ca6d96a5a072b810
pav <pav@FreeBSD.org>
2009-02-18 17:03:44 +0000

    - Update to 20081213
    PR:		ports/131561
    Submitted by:	Leon Kos <leon.kos@lecad.fs.uni-lj.si>

--- mbone/udpcast/files/patch-participants.h
+++ mbone/udpcast/files/patch-participants.h
@@ -1,10 +1,13 @@
---- participants.h.orig	Tue Jul 20 13:49:28 2004
-+++ participants.h	Tue Jul 20 13:49:40 2004
-@@ -3,6 +3,7 @@
+--- participants.h.orig	2005-12-22 23:59:18.000000000 +0100
++++ participants.h	2009-02-05 12:18:19.000000000 +0100
+@@ -3,6 +3,10 @@
  
  #define MAX_CLIENTS 1024
  
 +#include <sys/types.h>
- #include <sys/socket.h>
- 
++#include <sys/socket.h>
++
++
  typedef struct participantsDb *participantsDb_t;
+ 
+ #define isParticipantValid udpc_isParticipantValid
