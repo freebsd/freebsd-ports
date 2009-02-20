30e41c617c5929550723adc351d42fa1c02a9eec
Martin Wilke <miwi@FreeBSD.org>
2009-02-20 16:02:22 +0000

    - Update to 0.7.2
    PR:		131846
    Submitted by:	Ports Fury

--- multimedia/mplayerxp/files/patch-mplayerxp__libao2__ao_null.c
+++ multimedia/mplayerxp/files/patch-mplayerxp__libao2__ao_null.c
@@ -1,9 +1,8 @@
---- mplayerxp/libao2/ao_null.c.orig	Thu Nov 30 15:32:02 2006
-+++ mplayerxp/libao2/ao_null.c	Sun Dec 24 21:48:25 2006
-@@ -1,7 +1,7 @@
--#include <stdint.h>
- #include <stdio.h>
+--- mplayerxp/libao2/ao_null.c.orig	2007-12-02 18:19:44.000000000 +0900
++++ mplayerxp/libao2/ao_null.c	2009-01-07 18:28:00.000000000 +0900
+@@ -3,6 +3,7 @@
  #include <stdlib.h>
+ #include <string.h>
  #include <limits.h>
 +#include <sys/types.h>
  #include <sys/time.h>
