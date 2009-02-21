bb37a6937834b70e286f2492869327ad4ed088c7
mezz <mezz@FreeBSD.org>
2009-02-21 23:18:22 +0000

    Update to 1.1.3. Thanks to my friend for let me use his FreeBSD box.

--- net-p2p/deluge/files/patch-deluge_ui_gtkui_gtkui.py
+++ net-p2p/deluge/files/patch-deluge_ui_gtkui_gtkui.py
@@ -1,15 +1,13 @@
---- deluge/ui/gtkui/gtkui.py.orig	2009-01-17 11:34:35.000000000 -0600
-+++ deluge/ui/gtkui/gtkui.py	2009-01-17 11:34:52.000000000 -0600
-@@ -119,11 +119,11 @@
+--- deluge/ui/gtkui/gtkui.py.orig	2009-02-21 16:50:22.000000000 -0600
++++ deluge/ui/gtkui/gtkui.py	2009-02-21 16:50:24.000000000 -0600
+@@ -119,10 +119,10 @@
  
          # Initialize gettext
          try:
--            locale.setlocale(locale.LC_ALL, '')
 -            if hasattr(locale, "bindtextdomain"):
 -                locale.bindtextdomain("deluge", pkg_resources.resource_filename("deluge", "i18n"))
 -            if hasattr(locale, "textdomain"):
 -                locale.textdomain("deluge")
-+#            locale.setlocale(locale.LC_ALL, '')
 +#            if hasattr(locale, "bindtextdomain"):
 +#                locale.bindtextdomain("deluge", pkg_resources.resource_filename("deluge", "i18n"))
 +#            if hasattr(locale, "textdomain"):
