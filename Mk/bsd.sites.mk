0c91710c56d2a91178df36d726f4467404eee92d
knu <knu@FreeBSD.org>
2009-02-21 06:13:22 +0000

    Remove sakura from MASTER_SITE_RINGSERVER and add k-opti.com instead.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -1109,7 +1109,7 @@ MASTER_SITE_REDHAT_LINUX+= \
 .if !defined(IGNORE_MASTER_SITE_RINGSERVER)
 MASTER_SITE_RINGSERVER+=	\
 	http://ring.nict.go.jp/archives/%SUBDIR%/ \
-	http://ring.sakura.ad.jp/archives/%SUBDIR%/ \
+	http://ring.k-opti.com/archives/%SUBDIR%/ \
 	http://ring.riken.jp/archives/%SUBDIR%/
 .endif
 
