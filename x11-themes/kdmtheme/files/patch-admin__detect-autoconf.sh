973908eb0760b028b7ad122d713103382639f02b
Max Brazhnikov <makc@FreeBSD.org>
2009-02-21 17:55:46 +0000

    Update to 1.2.2 (backported from KDE4)

---
+++ x11-themes/kdmtheme/files/patch-admin__detect-autoconf.sh
@@ -0,0 +1,11 @@
+--- ./admin/detect-autoconf.sh.orig	2006-11-10 01:42:52.000000000 +0300
++++ ./admin/detect-autoconf.sh	2007-12-17 21:15:07.000000000 +0300
+@@ -10,7 +10,7 @@
+ # Please add higher versions first. The last version number is the minimum
+ # needed to compile KDE. Do not forget to include the name/version #
+ # separator if one is present, e.g. -1.2 where - is the separator.
+-KDE_AUTOCONF_VERS="-2.58 -2.57 257 -2.54 -2.53a -2.53 -2.52 -2.5x"
++KDE_AUTOCONF_VERS="-2.62 -2.58 -2.57 257 -2.54 -2.53a -2.53 -2.52 -2.5x"
+ KDE_AUTOMAKE_VERS="-1.7 17 -1.6"
+ 
+ # We don't use variable here for remembering the type ... strings. Local 
