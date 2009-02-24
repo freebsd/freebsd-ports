f1b345479624b6c10824f6a924523afad6c4c7c8
mnag <mnag@FreeBSD.org>
2009-02-24 19:06:37 +0000

    - Backport EPIPE case in sendfile backend. [1] - portlint(1) - Bump PORTREVISION
    Reported by:	Jeroen Ruigrok van der Werven <asmodai__in-nomine.org> [1]

--- www/lighttpd/files/patch-src__network_freebsd_sendfile.c
+++ www/lighttpd/files/patch-src__network_freebsd_sendfile.c
@@ -1,14 +1,18 @@
 #
-# Backport http://redmine.lighttpd.net/projects/lighttpd/repository/revisions/2403
+# Backport http://redmine.lighttpd.net/issues/show/1913
 #
---- src/network_freebsd_sendfile.c.orig	2009-02-24 11:05:06.000000000 -0300
-+++ src/network_freebsd_sendfile.c	2009-02-24 11:12:15.000000000 -0300
-@@ -167,7 +167,7 @@
+--- src/network_freebsd_sendfile.c.orig	2009-02-05 09:41:44.000000000 -0200
++++ src/network_freebsd_sendfile.c	2009-02-24 15:11:29.000000000 -0300
+@@ -167,8 +167,9 @@
  				switch(errno) {
  				case EAGAIN:
  				case EINTR:
 -					r = 0; /* try again later */
-+					/* for EAGAIN and EINTR r still contains the sent bytes */
- 					break;
+-					break;
++					/* for EAGAIN/EINTR r still contains the sent bytes */
++					break; /* try again later */
++				case EPIPE:
  				case ENOTCONN:
  					return -2;
+ 				default:
+
