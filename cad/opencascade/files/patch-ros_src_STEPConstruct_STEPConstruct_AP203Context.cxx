bcf7ceab4d810aed04a0a64bb20176af0f44d0a4
Thierry Thomas <thierry@FreeBSD.org>
2009-02-24 19:32:17 +0000

    Fix the previous patch.
    PR:		ports/131600
    Submitted by:	Cristiano Rolim Pereira <cristianorolim (at) hotmail.com>

--- cad/opencascade/files/patch-ros_src_STEPConstruct_STEPConstruct_AP203Context.cxx
+++ cad/opencascade/files/patch-ros_src_STEPConstruct_STEPConstruct_AP203Context.cxx
@@ -1,11 +1,16 @@
 --- ./ros/src/STEPConstruct/STEPConstruct_AP203Context.cxx.orig	2001-09-21 10:00:28.000000000 +0200
-+++ ./ros/src/STEPConstruct/STEPConstruct_AP203Context.cxx	2009-01-24 11:45:15.000000000 +0100
-@@ -103,7 +103,7 @@
++++ ./ros/src/STEPConstruct/STEPConstruct_AP203Context.cxx	2009-02-24 18:59:57.000000000 +0100
+@@ -103,6 +103,13 @@
  
      Handle(StepBasic_CoordinatedUniversalTimeOffset) zone = 
        new StepBasic_CoordinatedUniversalTimeOffset;
--    Standard_Integer shift = Standard_Integer(timezone);
-+    Standard_Integer shift = Standard_Integer((long long) timezone);
++
++    struct tm newtime;
++    time_t ltime;
++    ltime=time(&ltime);
++    localtime_r(&ltime, &newtime);
++    int timezone=newtime.tm_gmtoff;
++
+     Standard_Integer shift = Standard_Integer(timezone);
      Standard_Integer shifth = abs ( shift ) / 3600;
      Standard_Integer shiftm = ( abs ( shift ) - shifth * 3600 ) / 60;
-     StepBasic_AheadOrBehind sense = ( shift >0 ? StepBasic_aobBehind : 
