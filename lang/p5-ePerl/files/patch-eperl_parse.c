078fbe5b7a296d3fdd8fc04651ce706ce1062f48
miwi <miwi@FreeBSD.org>
2009-02-28 20:00:23 +0000

    - Unbreak after strndup MFC - Pass maintainership to submitter
    PR:		132160
    Submitted by:	Florian Smeets <flo@kasimir.com>

--- lang/p5-ePerl/files/patch-eperl_parse.c
+++ lang/p5-ePerl/files/patch-eperl_parse.c
@@ -1,5 +1,5 @@
---- eperl_parse.c.orig	Thu Nov 21 21:27:34 2002
-+++ eperl_parse.c	Thu Nov 21 21:27:42 2002
+--- eperl_parse.c.orig	2009-02-27 12:23:36.000000000 +0000
++++ eperl_parse.c	2009-02-27 12:25:38.000000000 +0000
 @@ -298,6 +298,7 @@
      return NULL;
  }
@@ -16,3 +16,23 @@
  
  char *strncasestr(char *buf, char *str, int n)
  {
+@@ -326,6 +328,9 @@
+     return NULL;
+ }
+ 
++#if defined(__FreeBSD__)  
++#include <osreldate.h>  
++#if __FreeBSD_version <= 800057 && __FreeBSD_version > 800000 || __FreeBSD_version <= 701100  
+ char *strndup(char *buf, int n)
+ {
+     char *cp;
+@@ -334,7 +339,8 @@
+     strncpy(cp, buf, n);
+     return cp;
+ }
+-
++#endif
++#endif
+ 
+ /*
+ **  convert buffer from bristled format to plain format
