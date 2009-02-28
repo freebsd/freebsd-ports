25ab70bed323f8c3391d5856dd3462b868a2c0cf
Rong-En Fan <rafan@FreeBSD.org>
2009-02-28 06:40:04 +0000

    - Update to 1.4.2
    PR:		ports/132090
    Submitted by:	Eric L. Chen <d9364104 at mail.nchu.edu.tw>

---
+++ audio/soundconverter/files/patch-src_soundconverter.py
@@ -0,0 +1,8 @@
+--- src/soundconverter.py.orig	2009-02-25 16:08:50.000000000 +0800
++++ src/soundconverter.py	2009-02-25 16:10:30.000000000 +0800
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python -tt
++#!/usr/bin/python
+ # -*- coding: utf-8 -*-
+ #
+ # SoundConverter - GNOME application for converting between audio formats. 
