ba127194e9f9130d7c690fa15cbf40406d12b573
Martin Wilke <miwi@FreeBSD.org>
2009-02-28 20:00:23 +0000

    - Unbreak after strndup MFC - Pass maintainership to submitter
    PR:		132160
    Submitted by:	Florian Smeets <flo@kasimir.com>

--- lang/p5-ePerl/files/patch-eperl_proto.h
+++ lang/p5-ePerl/files/patch-eperl_proto.h
@@ -1,5 +1,5 @@
---- eperl_proto.h.orig	Fri Jul 10 09:52:24 1998
-+++ eperl_proto.h	Wed May 30 19:13:42 2007
+--- eperl_proto.h.orig	2009-02-27 12:23:02.000000000 +0000
++++ eperl_proto.h	2009-02-27 12:24:39.000000000 +0000
 @@ -36,6 +36,7 @@
  #define EPERL_PROTO_H 1
  
@@ -8,12 +8,19 @@
  
  /* eperl_main.c */
  extern int mode;
-@@ -79,7 +80,7 @@
+@@ -79,9 +80,14 @@
  extern char *ePerl_Efwrite(char *cpBuf, int nBuf, int cNum, char *cpOut);
  extern char *ePerl_Cfwrite(char *cpBuf, int nBuf, int cNum, char *cpOut);
  extern char *strnchr(char *buf, char chr, int n);
 -extern char *strnstr(char *buf, char *str, int n);
 +/*extern char *strnstr(char *buf, char *str, int n);*/
  extern char *strncasestr(char *buf, char *str, int n);
++#if defined(__FreeBSD__)
++#include <osreldate.h>
++#if __FreeBSD_version <= 800057 && __FreeBSD_version > 800000 || __FreeBSD_version <= 701100
  extern char *strndup(char *buf, int n);
++#endif
++#endif
  extern char *ePerl_Bristled2Plain(char *cpBuf);
+ 
+ /* eperl_pp.c */
