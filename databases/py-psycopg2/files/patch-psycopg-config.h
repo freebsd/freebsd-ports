82f095602a3ba997b13ac3af79a1af8e14928530
Martin Wilke <miwi@FreeBSD.org>
2009-03-01 13:32:52 +0000

    - Update to 2.0.9

--- databases/py-psycopg2/files/patch-psycopg-config.h
+++ databases/py-psycopg2/files/patch-psycopg-config.h
@@ -1,10 +1,10 @@
---- psycopg/config.h.orig	2008-05-05 17:41:37.000000000 +0200
-+++ psycopg/config.h	2008-09-18 13:33:04.000000000 +0200
+--- psycopg/config.h.orig	2009-02-16 09:57:03.000000000 +0100
++++ psycopg/config.h	2009-03-01 10:59:26.000000000 +0100
 @@ -129,7 +129,7 @@
  #define inline
  #endif
  
--#if defined(__FreeBSD__) || (defined(_WIN32) && !defined(__GNUC__)) || defined(__sun__) || defined(sun)
+-#if (defined(__FreeBSD__) && __FreeBSD_version < 503000) || (defined(_WIN32) && !defined(__GNUC__)) || defined(__sun__) || defined(sun)
 +#if (defined(_WIN32) && !defined(__GNUC__)) || defined(__sun__) || defined(sun)
  /* what's this, we have no round function either? */
  static double round(double num)
