e05fddbd1de0968d421b94a93ed69d6462f8927c
Brooks Davis <brooks@FreeBSD.org>
2009-03-01 22:45:34 +0000

    Upgrade to a snapshot of svn rev r65789. Improve the patch for format string checking. [1] Correctly refect that this is a 2.6 snapshot since 2.5 has been branched.
    Submitted by:	ed [1]

--- devel/llvm-devel/files/patch-tools_clang_lib_Sema_SemaChecking.cpp
+++ devel/llvm-devel/files/patch-tools_clang_lib_Sema_SemaChecking.cpp
@@ -3,11 +3,28 @@ $FreeBSD$
 
 --- tools/clang/lib/Sema/SemaChecking.cpp.orig
 +++ tools/clang/lib/Sema/SemaChecking.cpp
-@@ -643,6 +643,7 @@
-     case 'G':
-     case 'a':
-     case 'A':
-+    case 'b':
-     case 'c':
-     case 'C':
+@@ -699,7 +699,6 @@
+     case 'u': 
+     case 'x':
+     case 'X':
+-    case 'D':
+     case 'O':
+     case 'U':
+     case 'e':
+@@ -715,9 +714,16 @@
      case 'S':
+     case 's':
+     case 'p': 
++    case 'r':
++    case 'y':
+       ++numConversions;
+       CurrentState = state_OrdChr;
+       break;
++    case 'b':
++    case 'D':
++      numConversions += 2;
++      CurrentState = state_OrdChr;
++      break;
+ 
+     // CHECK: Are we using "%n"?  Issue a warning.
+     case 'n': {
