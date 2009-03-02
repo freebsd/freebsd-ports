fda5dab9f3a175e91bbcbda67bce4c7533de8f1b
bsam <bsam@FreeBSD.org>
2009-03-02 10:08:21 +0000

    Update to version 1.23.0.

--- comms/gammu/files/patch-common.device.devfunc.c
+++ comms/gammu/files/patch-common.device.devfunc.c
@@ -1,9 +1,9 @@
---- common/device/devfunc.c.orig	Fri Feb 23 23:50:01 2007
-+++ common/device/devfunc.c	Tue Apr 10 23:46:39 2007
-@@ -18,6 +18,8 @@
- 
+--- libgammu/device/devfunc.c.orig	Fri Feb 23 23:50:01 2007
++++ libgammu/device/devfunc.c	Tue Apr 10 23:46:39 2007
+@@ -19,6 +19,8 @@
  #include <string.h>
  #include <fcntl.h>
+ #include <stdlib.h>
 +#include <sys/stat.h>
 +#include <sys/socket.h>
  #ifdef WIN32
--- comms/gammu/files/patch-common.service.gsmmisc.c
+++ comms/gammu/files/patch-common.service.gsmmisc.c
@@ -1,10 +1,10 @@
---- common/service/gsmmisc.c.orig	Fri Dec 30 18:18:47 2005
-+++ common/service/gsmmisc.c	Fri Dec 30 18:19:07 2005
+--- libgammu/service/gsmmisc.c.orig	Fri Dec 30 18:18:47 2005
++++ libgammu/service/gsmmisc.c	Fri Dec 30 18:19:07 2005
 @@ -3,6 +3,7 @@
  #include <string.h>
  #include <stdlib.h>
  #include <sys/stat.h>
 +#include <sys/socket.h>
- #ifdef WIN32
- #  include <io.h>
- #  include <fcntl.h>
+ 
+ #include <gammu-keys.h>
+ #include <gammu-debug.h>
