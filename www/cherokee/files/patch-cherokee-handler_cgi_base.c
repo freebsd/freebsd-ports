deca4284414ec80f0d7fdb46b79400aa69707235
Beech Rintoul <beech@FreeBSD.org>
2009-03-03 09:17:32 +0000

    - Update to 0.99.2 - Add mirrors

--- www/cherokee/files/patch-cherokee-handler_cgi_base.c
+++ www/cherokee/files/patch-cherokee-handler_cgi_base.c
@@ -1,10 +1,9 @@
---- cherokee/handler_cgi_base.c.orig	Mon Dec 11 11:44:26 2006
-+++ cherokee/handler_cgi_base.c	Thu Dec 14 10:09:38 2006
-@@ -21,7 +21,8 @@
-  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-  * USA
-  */
--
+--- cherokee/handler_cgi_base.c.orig	2009-02-26 00:07:37.000000000 -0900
++++ cherokee/handler_cgi_base.c	2009-03-02 15:03:03.000000000 -0900
+@@ -22,6 +22,8 @@
+  * 02110-1301, USA.
+  */ 
+ 
 +#include <stdlib.h>
 +#include <limits.h>
  #include "common-internal.h"
