c8f7cc4d4d47c0c289661fe159f401acc8a2c938
Alex Dupre <ale@FreeBSD.org>
2009-03-04 15:45:54 +0000

    Add mysql embedded server library.

---
+++ databases/mysql60-server/files/patch-include_my_handler.h
@@ -0,0 +1,11 @@
+--- include/my_handler.h.orig	2009-02-16 23:09:40.000000000 +0100
++++ include/my_handler.h	2009-02-16 23:09:55.000000000 +0100
+@@ -39,7 +39,7 @@
+   But beware the dependency of MI_MAX_POSSIBLE_KEY_BUFF and HA_MAX_KEY_LENGTH.
+ */
+ 
+-#define HA_MAX_KEY_LENGTH           1332        /* Max length in bytes */
++#define HA_MAX_KEY_LENGTH           4000        /* Max length in bytes */
+ #define HA_MAX_KEY_SEG              16          /* Max segments for key */
+ 
+ #define HA_MAX_POSSIBLE_KEY_BUFF    (HA_MAX_KEY_LENGTH + 24+ 6+6)
