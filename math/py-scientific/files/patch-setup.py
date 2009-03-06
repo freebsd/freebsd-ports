8343515ec6e5849abdd45af0a42f0171afdc0dfb
lwhsu <lwhsu@FreeBSD.org>
2009-03-06 15:29:32 +0000

    - Update to 2.8
    PR:		ports/130676
    Submitted by:	TAOKA Fumiyoshi <fmysh AT iijmio-mail.jp> (maintainer)

--- math/py-scientific/files/patch-setup.py
+++ math/py-scientific/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Mon Oct 18 08:34:00 2004
-+++ setup.py	Fri Feb 24 15:33:58 2006
-@@ -14,7 +14,7 @@
-     try:
-         netcdf_prefix=os.environ['NETCDF_PREFIX']
+--- setup.py.orig	2008-10-28 22:31:44.000000000 +0900
++++ setup.py	2009-01-18 12:49:38.000000000 +0900
+@@ -76,7 +76,7 @@
      except KeyError:
--        for netcdf_prefix in ['/usr/local', '/usr', '/sw']:
-+        for netcdf_prefix in [sys.prefix, '/usr', '/sw']:
-             netcdf_include = os.path.join(netcdf_prefix, 'include')
-             netcdf_lib = os.path.join(netcdf_prefix, 'lib')
-             if os.path.exists(os.path.join(netcdf_include, 'netcdf.h')):
+         pass
+ if netcdf_prefix is None:
+-    for netcdf_prefix in ['/usr/local', '/usr', '/sw']:
++    for netcdf_prefix in [sys.prefix, '/usr', '/sw']:
+         netcdf_include = os.path.join(netcdf_prefix, 'include')
+         netcdf_lib = os.path.join(netcdf_prefix, 'lib')
+         if os.path.exists(os.path.join(netcdf_include, 'netcdf.h')):
