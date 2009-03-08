a3721399910adfdc6f17cdc907fea48d41f975e9
beat <beat@FreeBSD.org>
2009-03-08 12:53:52 +0000

    - Update to 0.10.5
    PR:		ports/132291
    Submitted by:	Ashish Shukla <wahjava AT gmail.com> (maintainer)
    Approved by:	miwi (mentor)

--- deskutils/google-gadgets/files/patch-extensions_gtkmoz__browser__element_browser__element.cc
+++ deskutils/google-gadgets/files/patch-extensions_gtkmoz__browser__element_browser__element.cc
@@ -3,7 +3,7 @@ $FreeBSD$
 
 --- extensions/gtkmoz_browser_element/browser_element.cc.orig
 +++ extensions/gtkmoz_browser_element/browser_element.cc
-@@ -157,7 +157,7 @@
+@@ -164,7 +164,7 @@
        std::string up_fd_str = StringPrintf("%d", up_fds[1]);
        for (size_t i = 0; kBrowserChildNames[i]; ++i) {
          execl(kBrowserChildNames[i], kBrowserChildNames[i],
@@ -12,41 +12,3 @@ $FreeBSD$
        }
        LOG("Failed to execute browser child");
        _exit(-1);
-@@ -416,6 +416,8 @@
-           object_id_str_(StringPrintf("%zu", object_id)),
-           call_self_(this),
-           to_string_(NewSlot(this, &BrowserObjectWrapper::ToString)) {
-+      if (parent_)
-+        parent_->Ref();
-     }
- 
-     virtual ~BrowserObjectWrapper() {
-@@ -426,6 +428,8 @@
-         owner_->controller_->SendCommand(kUnrefCommand, owner_->browser_id_,
-                                          object_id_str_.c_str(), NULL);
-       }
-+      if (parent_)
-+        parent_->Unref();
-     }
- 
-     void OnOwnerDestroy() {
-@@ -523,8 +527,8 @@
-         buffer += '\n';
-         buffer += wrapper_->object_id_str_;
-         buffer += '\n';
--        if (wrapper_->parent_.Get())
--          buffer += wrapper_->parent_.Get()->object_id_str_;
-+        if (wrapper_->parent_)
-+          buffer += wrapper_->parent_->object_id_str_;
-         for (int i = 0; i < argc; i++) {
-           buffer += '\n';
-           buffer += wrapper_->owner_->EncodeValue(argv[i]);
-@@ -546,7 +550,7 @@
-     };
- 
-     BrowserElementImpl *owner_;
--    ScriptableHolder<BrowserObjectWrapper> parent_;
-+    BrowserObjectWrapper *parent_;
-     size_t object_id_;
-     std::string object_id_str_;
-     CallSelfSlot call_self_;
