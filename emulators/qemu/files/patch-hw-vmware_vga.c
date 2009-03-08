e332617a7d04779931f8a5672946248611972643
Juergen Lock <nox@FreeBSD.org>
2009-03-08 17:09:43 +0000

    Update to 0.10.0 - from the official changelog:
      - TCG support (No longer requires GCC 3.x)
      - Kernel Virtual Machine acceleration support [kernel bits not ported
        to FreeBSD yet]
      - BSD userspace emulation [untested on FreeBSD, probably doesn't work
        at least for i386 hosts]
      - Bluetooth emulation and host passthrough support [not ported to
        FreeBSD yet]
      - GDB XML register description support
      - Intel e1000 emulation
      - HPET emulation
      - VirtIO paravirtual device support
      - Marvell 88w8618 / MusicPal emulation
      - Nokia N-series tablet emulation / OMAP2 processor emulation
      - PCI hotplug support
      - Live migration and new save/restore formats
      - Curses display support
      - qemu-nbd utility to mount supported block formats [not ported to
        FreeBSD yet]
      - Altivec support in PPC emulation and new firmware (OpenBIOS)
      - Multiple VNC clients are now supported
      - TLS encryption is now supported in VNC
      - MIPS Magnum R4000 machine (Herve Poussineau)
      - Braille support (Samuel Thibault)
      - Freecom MusicPal system emulation (Jan Kiszka)
      - OMAP242x and Nokia N800, N810 machines (Andrzej Zaborowski)
      - EsounD audio driver (Frederick Reeve)
      - Gravis Ultrasound GF1 sound card (Tibor "TS" Schuetz)
      - Many, many, bug fixes and new features

--- emulators/qemu/files/patch-hw-vmware_vga.c
+++ emulators/qemu/files/patch-hw-vmware_vga.c
@@ -1,12 +1,84 @@
 Index: qemu/hw/vmware_vga.c
-@@ -26,8 +26,8 @@
+@@ -28,9 +28,9 @@
  #define VERBOSE
  #define EMBED_STDVGA
  #undef DIRECT_VRAM
 -#define HW_RECT_ACCEL
 -#define HW_FILL_ACCEL
+-#define HW_MOUSE_ACCEL
 +/* #define HW_RECT_ACCEL */
 +/* #define HW_FILL_ACCEL */
- #define HW_MOUSE_ACCEL
++/* #define HW_MOUSE_ACCEL */
  
  #ifdef EMBED_STDVGA
+ # include "vga_int.h"
+@@ -76,6 +76,7 @@
+     uint32_t wblue;
+     int syncing;
+     int fb_size;
++    int empty;
+ 
+     union {
+         uint32_t *fifo;
+@@ -487,7 +488,7 @@
+ 
+ static inline int vmsvga_fifo_empty(struct vmsvga_state_s *s)
+ {
+-    if (!s->config || !s->enable)
++    if (!s->config || !s->enable || s->empty)
+         return 1;
+     return (s->cmd->next_cmd == s->cmd->stop);
+ }
+@@ -495,6 +496,10 @@
+ static inline uint32_t vmsvga_fifo_read_raw(struct vmsvga_state_s *s)
+ {
+     uint32_t cmd = s->fifo[CMD(stop) >> 2];
++    if (s->cmd->next_cmd == s->cmd->stop) {
++        s->empty = 1;
++        return 0;
++    }
+     s->cmd->stop = cpu_to_le32(CMD(stop) + 4);
+     if (CMD(stop) >= CMD(max))
+         s->cmd->stop = s->cmd->min;
+@@ -512,6 +517,7 @@
+     int args = 0;
+     int x, y, dx, dy, width, height;
+     struct vmsvga_cursor_definition_s cursor;
++    s->empty = 0;
+     while (!vmsvga_fifo_empty(s))
+         switch (cmd = vmsvga_fifo_read(s)) {
+         case SVGA_CMD_UPDATE:
+@@ -533,6 +539,7 @@
+             vmsvga_fill_rect(s, colour, x, y, width, height);
+             break;
+ #else
++            args = 0;
+             goto badcmd;
+ #endif
+ 
+@@ -547,6 +554,7 @@
+             vmsvga_copy_rect(s, x, y, dx, dy, width, height);
+             break;
+ #else
++            args = 0;
+             goto badcmd;
+ #endif
+ 
+@@ -609,6 +617,7 @@
+             break; /* Nop */
+ 
+         default:
++            args = 0;
+         badcmd:
+             while (args --)
+                 vmsvga_fifo_read(s);
+@@ -914,7 +914,7 @@
+     s->width = -1;
+     s->height = -1;
+     s->svgaid = SVGA_ID;
+-    s->depth = 24;
++    s->depth = 32;
+     s->bypp = (s->depth + 7) >> 3;
+     s->cursor.on = 0;
+     s->redraw_fifo_first = 0;
+
