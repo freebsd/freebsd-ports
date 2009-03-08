a16782ab9f1007df2410b82d7050ecb7263e1bca
lwhsu <lwhsu@FreeBSD.org>
2009-03-08 17:49:26 +0000

    - Update to 1.5.1
    PR:		ports/131352
    Submitted by:	Kuan-Chung Chiu <buganini AT gmail.com>

--- games/fgrun/files/patch-src-run_posix.cxx
+++ games/fgrun/files/patch-src-run_posix.cxx
@@ -1,6 +1,6 @@
---- src/run_posix.cxx.orig	2008-04-23 04:51:01.000000000 +0800
-+++ src/run_posix.cxx	2008-10-18 17:28:27.000000000 +0800
-@@ -58,7 +58,7 @@
+--- src/run_posix.cxx.orig	2009-01-25 17:01:58.000000000 +0000
++++ src/run_posix.cxx	2009-03-09 01:08:38.080801315 +0000
+@@ -59,7 +59,7 @@
  #if defined(HAVE_TERMIOS_H)
      struct termios term;
      tcgetattr( STDOUT_FILENO, &term );
@@ -8,4 +8,13 @@
 +    term.c_oflag &= ~ONLCR;
  
      pid = pty_fork( &master, 0, &term, 0 );
+ #else
+@@ -171,7 +171,7 @@
+ #if defined(HAVE_TERMIOS_H)
+     struct termios term;
+     tcgetattr( STDOUT_FILENO, &term );
+-    term.c_oflag &= ~( OLCUC | ONLCR );
++    term.c_oflag &= ~ONLCR;
+ 
+     tsPid = pty_fork( &master, 0, &term, 0 );
  #else
