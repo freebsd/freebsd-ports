40c0b38bfb75211cd9b99a1e0e107ee18aaa163f
makc <makc@FreeBSD.org>
2009-03-09 00:55:48 +0000

    Update KDE to 4.2.1.
    multimedia/phonon port has been split into phonon itself, phonon-xine
    and phono-gstreamer backends. After updating phonon port you have
    to install at least one backend. phonon-xine backend is recommended
    for KDE.

---
+++ deskutils/kdepimlibs4/files/patch-cmake-modules_FindLibical.cmake
@@ -0,0 +1,11 @@
+--- ../cmake/modules/FindLibical.cmake.orgi	2009-02-27 17:59:51.000000000 +0100
++++ ../cmake/modules/FindLibical.cmake	2009-02-27 18:00:20.000000000 +0100
+@@ -52,7 +52,7 @@
+ 
+ if(LIBICAL_INCLUDE_DIRS AND LIBICAL_LIBRARIES)
+   set(FIND_LIBICAL_VERSION_SOURCE
+-    "#include <libical/ical.h>\n int main()\n {\n printf(\"%s\",ICAL_VERSION);return 1;\n }\n")
++    "#include <ical.h>\n int main()\n {\n printf(\"%s\",ICAL_VERSION);return 1;\n }\n")
+   set(FIND_LIBICAL_VERSION_SOURCE_FILE ${CMAKE_BINARY_DIR}/CMakeTmp/FindLIBICAL.cxx)
+   file(WRITE "${FIND_LIBICAL_VERSION_SOURCE_FILE}" "${FIND_LIBICAL_VERSION_SOURCE}")
+ 
