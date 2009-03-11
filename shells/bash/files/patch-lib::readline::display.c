24b0c8880fff32794ccb325f1002ceeb9ed54f41
obrien <obrien@FreeBSD.org>
2009-03-11 16:49:28 +0000

    Update to 4.0 PL0.

--- shells/bash/files/patch-lib::readline::display.c
+++ shells/bash/files/patch-lib::readline::display.c
@@ -1,24 +1,11 @@
-#
-# Fix handling of `read -e' while PS1 is set longer than the screen width
-#
-# http://lists.gnu.org/archive/html/bug-bash/2004-11/msg00220.html
-# http://lists.gnu.org/archive/html/bug-bash/2004-11/msg00266.html
-#
---- lib/readline/display.c.orig	Wed Nov 30 17:05:02 2005
-+++ lib/readline/display.c	Wed Dec 28 20:13:15 2005
-@@ -594,11 +594,10 @@
- 
+--- lib/readline/display.c.orig	2009-01-04 11:32:32.000000000 -0800
++++ lib/readline/display.c	2009-03-08 00:51:31.000000000 -0800
+@@ -654,5 +654,8 @@ rl_redisplay ()
    /* inv_lbreaks[i] is where line i starts in the buffer. */
    inv_lbreaks[newlines = 0] = 0;
--#if 0
--  lpos = out - wrap_offset;
--#else
--  lpos = prompt_physical_chars + modmark;
--#endif
 +  if (local_prompt)
-+    lpos = prompt_physical_chars + modmark;
+   lpos = prompt_physical_chars + modmark;
 +  else
 +    lpos = 0;
  
  #if defined (HANDLE_MULTIBYTE)
-   memset (_rl_wrapped_line, 0, vis_lbsize);
