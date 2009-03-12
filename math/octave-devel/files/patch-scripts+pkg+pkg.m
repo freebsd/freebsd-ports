c4fd38f5afe6a9213ef233c787477247a035fcfd
Maho Nakata <maho@FreeBSD.org>
2009-03-12 12:24:13 +0000

    Update to 3.1.54 and unbreak. The pkg-plist is now generated semi-automatically.

--- math/octave-devel/files/patch-scripts+pkg+pkg.m
+++ math/octave-devel/files/patch-scripts+pkg+pkg.m
@@ -1,11 +1,11 @@
---- scripts/pkg/pkg.m~	2008-07-22 00:59:05.000000000 +0900
-+++ scripts/pkg/pkg.m	2008-08-23 05:47:31.000000000 +0900
-@@ -1242,7 +1242,7 @@
-     ## make
+--- scripts/pkg/pkg.m~	2009-03-09 11:04:57.000000000 +0900
++++ scripts/pkg/pkg.m	2009-03-09 11:05:12.000000000 +0900
+@@ -1239,7 +1239,7 @@
+     ## Make.
      if (exist (fullfile (src, "Makefile"), "file"))
        [status, output] = shell (cstrcat ("export INSTALLDIR=\"", desc.dir,
--					 "\"; make -C ", src));
-+					 "\"; gmake -C ", src));
+-					 "\"; make -C '", src, "'"));
++					 "\"; gmake -C '", src, "'"));
        if (status != 0)
  	rm_rf (desc.dir);
  	error ("'make' returned the following error: %s", output);
