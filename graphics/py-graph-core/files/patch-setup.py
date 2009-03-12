cad64609b277e9ccbcfcf5b89c0a8a4be82089b9
Martin Wilke <miwi@FreeBSD.org>
2009-03-12 19:24:03 +0000

    - Update to 1.4.2
    PR:		132030
    Submitted by:	Wen Heping <wenheping@gmail.com> (maintainer)

--- graphics/py-graph-core/files/patch-setup.py
+++ graphics/py-graph-core/files/patch-setup.py
@@ -1,15 +1,13 @@
---- setup.py.orig	2008-10-28 21:22:57.000000000 +0800
-+++ setup.py	2008-10-28 21:23:17.000000000 +0800
-@@ -18,9 +18,9 @@
+--- setup.py.orig	2009-02-23 22:14:56.000000000 +0800
++++ setup.py	2009-02-23 22:14:38.000000000 +0800
+@@ -37,8 +37,8 @@
          name = appname,
          version = appversion,
-         packages = ['graph'],
--        data_files = [(docfolder, ['README','Changelog','COPYING']),
--                                (docfolder + docs, docfiles),
--                                ],
-+#        data_files = [(docfolder, ['README','Changelog','COPYING']),
-+#                                (docfolder + docs, docfiles),
-+#                                ],
- 
-         # metadata
+         packages = ['graph', 'graph.algorithms', 'graph.algorithms.filters', 'graph.algorithms.heuristics', 'graph.classes'],
+-        data_files = [(docsdir,docsfiles),
+-                       (datadir,datafiles)],
++#        data_files = [(docsdir,docsfiles),
++#                       (datadir,datafiles)],
          author = "Pedro Matiello",
+         author_email = "pmatiello@gmail.com",
+         description = "A library for working with graphs in Python",
