5ae212dded54a847e04a1c0e71eecf30e1261619
Alex Dupre <ale@FreeBSD.org>
2009-03-13 14:55:09 +0000

    Update to 1.12 release.
    Submitted by:	maintainer

---
+++ sysutils/cpdup/files/patch-cpdup.h
@@ -0,0 +1,10 @@
+--- cpdup.h.orig	2009-03-13 15:53:06.000000000 +0100
++++ cpdup.h	2009-03-13 15:53:45.000000000 +0100
+@@ -14,6 +14,7 @@
+ #include <stdlib.h>
+ #include <stdarg.h>
+ #include <stddef.h>
++#include <stdint.h>
+ #include <string.h>
+ #include <errno.h>
+ #include <unistd.h>
