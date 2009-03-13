1a908783368153ef8dd41163028a8741ee138f09
Tilman Keskinoz <arved@FreeBSD.org>
2009-03-13 18:15:57 +0000

    Unbreak on 64bit platforms

---
+++ archivers/xmill/files/patch-XMill-PPMDI.cpp
@@ -0,0 +1,11 @@
+--- XMill/PPMDI.cpp.orig	2009-03-13 19:12:05.000000000 +0100
++++ XMill/PPMDI.cpp	2009-03-13 19:12:29.000000000 +0100
+@@ -245,7 +245,7 @@
+ 
+ cleanup:
+    *outused = j;
+-   *inused = inlen - (int)endptr + (int)src;
++   *inused = inlen - (intptr_t)endptr + (intptr_t)src;
+    return ret;
+ }
+  
