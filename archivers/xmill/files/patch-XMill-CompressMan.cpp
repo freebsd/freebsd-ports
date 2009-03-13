8e361ff77db65796be34a1356c6864b9f2734bda
arved <arved@FreeBSD.org>
2009-03-13 18:15:57 +0000

    Unbreak on 64bit platforms

---
+++ archivers/xmill/files/patch-XMill-CompressMan.cpp
@@ -0,0 +1,11 @@
+--- XMill/CompressMan.cpp.orig	2009-03-13 19:11:05.000000000 +0100
++++ XMill/CompressMan.cpp	2009-03-13 19:11:08.000000000 +0100
+@@ -423,7 +423,7 @@
+    UserCompressorFactory *compressor=compressorlist;
+    while(compressor!=NULL)
+    {
+-      printf("%lu =>",(unsigned int)compressor);
++      printf("%lu =>",(unsigned intptr_t)compressor);
+       printf("%s\n",compressor->GetName());
+       compressor=compressor->next;
+    }
