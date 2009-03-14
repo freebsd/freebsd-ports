f165475b8147ce866122025efeb3876459c87462
miwi <miwi@FreeBSD.org>
2009-03-14 13:36:57 +0000

    - Update to 3.2 - Update maintainer mail adress
    PR:		132601
    Submitted by;	Nikos Ntarmos <ntarmos@cs.uoi.gr> (maintainer)

--- x11-wm/awesome2/files/patch-awesomeConfig.cmake
+++ x11-wm/awesome2/files/patch-awesomeConfig.cmake
@@ -11,23 +11,6 @@
  
  link_directories(/usr/local/lib)
  
-@@ -134,14 +135,14 @@
-     cairo
-     pango
-     pangocairo
--    x11-xcb
-+    xcb
-     xcb-randr
-     xcb-xinerama
-     xcb-event>=0.3.0
-     xcb-aux>=0.3.0
-     xcb-atom>=0.3.0
-     xcb-keysyms>=0.3.0
--    xcb-icccm>=0.3.0
-+    xcb-icccm>=0.3.3
-     cairo-xcb
-     xproto>=7.0.11
-     imlib2)
 @@ -248,7 +249,7 @@
  if(DEFINED AWESOME_MAN_PATH)
     set(AWESOME_MAN_PATH ${AWESOME_MAN_PATH} CACHE PATH "awesome manpage directory")
