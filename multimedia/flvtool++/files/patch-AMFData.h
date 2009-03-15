3bd5f36a86c07bda8417bf43f4635ef26a65c833
lwhsu <lwhsu@FreeBSD.org>
2009-03-15 02:14:14 +0000

    - Update to 1.1.7
    PR:		ports/132628
    Submitted by:	Gea-Suan Lin <gslin AT gslin.org> (maintainer)

--- multimedia/flvtool++/files/patch-AMFData.h
+++ multimedia/flvtool++/files/patch-AMFData.h
@@ -1,10 +1,11 @@
---- AMFData.h.orig	2008-08-07 21:59:28.000000000 +0800
-+++ AMFData.h	2008-08-07 21:59:34.000000000 +0800
-@@ -8,7 +8,6 @@
- #pragma once
- 
- #include "common.h"
--#include <byteswap.h>
- #include <float.h>
- #include <math.h>
- #include <netinet/in.h>
+--- AMFData.h.orig	2009-01-22 09:09:49.000000000 +0800
++++ AMFData.h	2009-03-15 01:52:25.000000000 +0800
+@@ -18,7 +18,7 @@
+   #include <architecture/byte_order.h>
+   #define hton64(x) OSSwapHostToBigInt64(x)
+ #else
+-  #include <byteswap.h>
++  #include <sys/endian.h>
+   #if __BYTE_ORDER == __LITTLE_ENDIAN
+     #define hton64(x) bswap_64(x)
+   #else
