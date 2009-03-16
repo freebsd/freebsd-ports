7fa652b43ab75d9ac6786c8658262af1959434bd
Nicola Vitale <nivit@FreeBSD.org>
2009-03-16 08:07:47 +0000

    Fabric is a simple pythonic remote deployment tool.
    It is designed to upload files to, and run shell commands on, a number
    of servers in parallel or serially. These commands are grouped in tasks
    (regular python functions) and specified in a 'fabfile.'
    
    This is called remote automation, and the primary use case is deploying
    applications to multiple similar hosts.
    
    WWW:	http://www.nongnu.org/fab/
    
    PR:		ports/132653
    Submitted by:	Yi-Jheng Lin <yzlin at cs.nctu.edu.tw>

---
+++ devel/py-fabric/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig
++++ setup.py
+@@ -23,7 +23,7 @@
+     author = 'Christian Vest Hansen',
+     author_email = 'karmazilla@gmail.com',
+     url = 'http://www.nongnu.org/fab/',
+-    install_requires = ['paramiko >=1.6, <2.0'],
++    #install_requires = ['paramiko >=1.6, <2.0'],
+     py_modules = ['fabric'],
+     entry_points = {
+         'console_scripts': [
