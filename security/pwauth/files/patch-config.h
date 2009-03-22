e63d042ec9a4af5348c0698aed211bedf09044ed
pav <pav@FreeBSD.org>
2009-03-22 20:56:49 +0000

    - Update to 2.3.7
    PR:		ports/131791
    Submitted by:	Edmondas Girkantas <eg@fbsd.lt>
    Approved by:	maintainer timeout (clement; 1 month)

--- security/pwauth/files/patch-config.h
+++ security/pwauth/files/patch-config.h
@@ -1,9 +1,9 @@
---- config.h.orig	Wed Sep 29 01:07:18 2004
-+++ config.h	Sun Oct 30 13:49:52 2005
-@@ -106,13 +106,13 @@
+--- config.h.orig	2008-05-19 21:59:42.000000000 +0300
++++ config.h	2009-02-17 23:01:51.000000000 +0200
+@@ -112,13 +112,13 @@
  
  /* #define SHADOW_NONE		/**/
- /* #define SHADOW_BSD		/* FreeBSD, NetBSD, OpenBSD, BSDI */
+ /* #define SHADOW_BSD		/* FreeBSD, NetBSD, OpenBSD, BSDI, OS X */
 -#define SHADOW_SUN		/* Linux, Solaris, IRIX */
 +/* #define SHADOW_SUN		/* Linux, Solaris, IRIX */
  /* #define SHADOW_JFH		/**/
@@ -13,10 +13,10 @@
  
 -/* #define PAM			/* Linux PAM or OpenPAM*/
 +   #define PAM			/* Linux PAM or OpenPAM*/
+ /* #define PAM_OS_X		/* PAM on OS X */
  /* #define PAM_SOLARIS		/* PAM on Solaris other than 2.6 */
  /* #define PAM_SOLARIS_26	/* PAM on Solaris 2.6 */
- /* #define LOGIN_CONF_OPENBSD	/**/
-@@ -193,7 +193,7 @@
+@@ -200,7 +200,7 @@
   */
  
  #define UNIX_LASTLOG		/**/
@@ -25,21 +25,21 @@
  
  
  /* If NOLOGIN_FILE is defined to the full path name of a file, then the
-@@ -238,7 +238,7 @@
+@@ -245,7 +245,7 @@
   * last).
   */
  
 -#define SERVER_UIDS 72		/* user "nobody" */
-+#define SERVER_UIDS %%UIDS%%	/* user "www" */
++#define SERVER_UIDS %%UIDS%%	/* user "nobody" */
  
  
  /* If MIN_UNIX_UID is defined to an integer, logins with uid numbers less than
-@@ -250,7 +250,7 @@
+@@ -257,7 +257,7 @@
   * given value will be accepted).
   */
  
 -#define MIN_UNIX_UID 500	/**/
-+#define MIN_UNIX_UID %%MINUID%%	/**/
++#define MIN_UNIX_UID %%MINUID%%        /**/
  
  
  /* If IGNORE_CASE is defined, the login given is checked in two different
