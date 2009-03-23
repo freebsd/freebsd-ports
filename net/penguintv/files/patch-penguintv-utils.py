c3bdb2681b201607f6d15098de9e11fbe6e688eb
edwin <edwin@FreeBSD.org>
2009-03-23 03:09:53 +0000

    Update to version 4.0:
    # Lots of bugfixes, perfomance improvements, etc
    # Supports Nokia N800/N810 Maemo devices
    # Better setup of mozilla environment, should no longer crash on
      startup on any platform
    # Commandline control of media player
    # Local image caching so images still appear when offline

--- net/penguintv/files/patch-penguintv-utils.py
+++ net/penguintv/files/patch-penguintv-utils.py
@@ -1,10 +1,12 @@
---- penguintv/utils.py.orig	2008-03-13 21:59:27.000000000 +0000
-+++ penguintv/utils.py	2008-03-13 22:00:06.000000000 +0000
-@@ -186,6 +186,7 @@
+--- penguintv/utils.py.orig	2009-03-23 08:35:01.000000000 +1100
++++ penguintv/utils.py	2009-03-23 08:35:31.000000000 +1100
+@@ -228,7 +228,8 @@
  		  os.path.join(os.path.split(os.path.abspath(sys.argv[0]))[0],"share"),
  		  os.path.join(GetPrefix(),"share","sugar","activities","ptv","share"),
- 		  os.path.join(os.path.split(os.path.split(utils.__file__)[0])[0],'share'),
-+		  "/usr/local/share/penguintv",
- 		  "/usr/share/penguintv"):
+ 		  os.path.join(os.path.split(os.path.split(utils.__file__)[0])[0],'share','glade'),
+-		  "/usr/share/penguintv/glade"):
++		  "/usr/share/penguintv/glade",
++		  "%%LOCALBASE%%/penguintv/glade"):
  		try:
- 			os.stat(os.path.join(p,"penguintv.glade"))
+ 			os.stat(os.path.join(p,"dialogs.glade"))
+ 			logging.debug("glade prefix found: %s" % (p,))
