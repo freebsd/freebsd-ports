4867e5bfda5f9249f20a9288ad7d9bb322e3b0b3
pav <pav@FreeBSD.org>
2009-03-24 15:32:31 +0000

    - Update to 18
    PR:		ports/130047
    Submitted by:	Pedro F. Giffuni <giffunip@tutopia.com>
    Approved by:	maintainer timeout (2 months)

--- cad/ngspice_rework/files/patch-src__frontend__resource.c
+++ cad/ngspice_rework/files/patch-src__frontend__resource.c
@@ -1,40 +1,10 @@
---- src/frontend/resource.c.orig	Thu Jun 30 23:38:21 2005
-+++ src/frontend/resource.c	Sat Dec 10 20:41:22 2005
-@@ -424,6 +424,7 @@
+--- src/frontend/resource.c.orig	2008-11-19 14:45:28.000000000 -0500
++++ src/frontend/resource.c	2008-12-29 15:44:52.000000000 -0500
+@@ -605,6 +605,7 @@
  fault(void)
  {
- 	signal(SIGSEGV, (SIGNAL_FUNCTION) fault);	/* SysV style */
-+	signal(SIGBUS, (SIGNAL_FUNCTION) fault);	/* SysV style */
- 	LONGJMP(env, 1);
+     signal(SIGSEGV, (SIGNAL_FUNCTION) fault);	/* SysV style */
++    signal(SIGBUS, (SIGNAL_FUNCTION) fault);	/* SysV style */
+     LONGJMP(env, 1);
  }
  
-@@ -436,7 +437,8 @@
- 	char *low, *high, *at;
- 	/* char *sbrk(int);  */
- 	long x;
--	RETSIGTYPE	(*orig_signal)( );
-+	RETSIGTYPE	(*orig_sigsegv)( );
-+	RETSIGTYPE	(*orig_sigbus)( );
- 
- 	if (getenv("SPICE_NO_DATASEG_CHECK"))
- 		return 0;
-@@ -444,7 +446,8 @@
- 	low = 0;
- 	high = (char *) ((unsigned long) sbrk(0) & ~((1 << LOG2_PAGESIZE) - 1));
- 
--	orig_signal = signal(SIGSEGV, (SIGNAL_FUNCTION) fault);
-+	orig_sigsegv = signal(SIGSEGV, (SIGNAL_FUNCTION) fault);
-+	orig_sigbus = signal(SIGBUS, (SIGNAL_FUNCTION) fault);
- 
- 	do {
- 
-@@ -472,7 +475,8 @@
- 
- 	} while (1);
- 
--	(void) signal(SIGSEGV, (SIGNAL_FUNCTION) orig_signal);
-+	(void) signal(SIGSEGV, (SIGNAL_FUNCTION) orig_sigsegv);
-+	(void) signal(SIGBUS, (SIGNAL_FUNCTION) orig_sigbus);
- 	return (void *) high;
- 
- #endif
