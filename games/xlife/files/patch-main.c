969258ec17df7733a29894a126bb4ff1c91925be
pav <pav@FreeBSD.org>
2009-03-24 12:04:16 +0000

    - Fix runtime on amd64
    PR:		ports/132949
    Submitted by:	billy <markslashbill@yahoo.co.uk>

---
+++ games/xlife/files/patch-main.c
@@ -0,0 +1,10 @@
+--- main.c.orig	1991-04-03 04:04:28.000000000 +0200
++++ main.c	2009-03-24 13:02:59.000000000 +0100
+@@ -31,6 +31,7 @@
+ #include <X11/Xatom.h>
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ 
+ #include "defs.h"
+ #include "icon.h"
