7b4639d20c13a32165e09360f232cbf52ac84504
makc <makc@FreeBSD.org>
2009-03-24 19:11:23 +0000

    Update devel/cmake to 2.6.3.
    - Set INSTALL_TARGET for cmake based ports to install/strip. This solves
      problem of installing non-stripped binaries (noticed by delphij for KDE4 ports)
    - Fix linking to -lpthread for cmake based ports (KDE4 ports are affected mostly)

--- x11/kdelibs4/files/patch-cmake_modules_FindKDE4Internal.cmake
+++ x11/kdelibs4/files/patch-cmake_modules_FindKDE4Internal.cmake
@@ -1,6 +1,15 @@
---- ../cmake/modules/FindKDE4Internal.cmake.orig	2008-12-04 11:35:54.000000000 +0300
-+++ ../cmake/modules/FindKDE4Internal.cmake	2008-12-15 02:56:07.000000000 +0300
-@@ -743,8 +743,8 @@
+--- ../cmake/modules/FindKDE4Internal.cmake.orig	2009-02-26 17:18:26.000000000 +0300
++++ ../cmake/modules/FindKDE4Internal.cmake	2009-03-22 14:42:53.000000000 +0300
+@@ -550,7 +550,7 @@
+ # whether to install a CMake FooConfig.cmake into lib/foo/cmake/ or /lib/cmake/foo/
+ # (with 2.6.3 and above also lib/cmake/foo/ is supported):
+ if(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION} VERSION_GREATER 2.6.2)
+-   option(KDE4_USE_COMMON_CMAKE_PACKAGE_CONFIG_DIR "Prefer to install the <package>Config.cmake files to lib/cmake/<package> instead to lib/<package>/cmake" TRUE)
++   option(KDE4_USE_COMMON_CMAKE_PACKAGE_CONFIG_DIR "Prefer to install the <package>Config.cmake files to lib/cmake/<package> instead to lib/<package>/cmake" FALSE)
+ else(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION} VERSION_GREATER 2.6.2)
+    set(KDE4_USE_COMMON_CMAKE_PACKAGE_CONFIG_DIR  FALSE)
+ endif(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION} VERSION_GREATER 2.6.2)
+@@ -694,8 +694,8 @@
     _set_fancy(XDG_MIME_INSTALL_DIR     "${SHARE_INSTALL_PREFIX}/mime/packages"  "The install dir for the xdg mimetypes")
  
     _set_fancy(SYSCONF_INSTALL_DIR      "${CMAKE_INSTALL_PREFIX}/etc"            "The kde sysconfig install dir (default ${CMAKE_INSTALL_PREFIX}/etc)")
