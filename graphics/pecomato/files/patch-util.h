17a948f6ff65f926a9162d2611cf8ce09affd6a9
miwi <miwi@FreeBSD.org>
2009-03-25 15:46:07 +0000

    - Fix build after addition of getdelim() and strndup()
    PR:		133053
    Submitted by:	David Schultz <das@freebsd.org>

--- graphics/pecomato/files/patch-util.h
+++ graphics/pecomato/files/patch-util.h
@@ -1,10 +1,22 @@
---- util.h.orig	Mon Dec 18 20:22:56 2006
-+++ util.h	Mon Dec 18 20:23:03 2006
-@@ -8,6 +8,7 @@
- 
- #ifndef _util_h_
+--- util.h.orig	2007-09-28 12:04:57.000000000 -0400
++++ util.h	2009-03-25 03:18:26.000000000 -0400
+@@ -10,6 +10,7 @@
  #define _util_h_
-+#define _SSIZE_T_DECLARED
  
  
++#include <sys/param.h>
  #include <stdio.h>
+ #include <stdarg.h>
+ 
+@@ -67,9 +68,11 @@
+ #define _SSIZE_T_DECLARED
+ #endif
+ #endif
++#if __FreeBSD_version < 800067
+ #include "gnu-getline.h"
+ #include "gnu-strndup.h"
+ #endif
++#endif
+ 
+ /* Microsoft Windows specific defines */
+ #ifdef OS_WIN32
