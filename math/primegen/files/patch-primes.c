42425447f7887b1ba3ad0b1f8433b6b60bdcdae0
lioux <lioux@FreeBSD.org>
2009-03-27 12:47:38 +0000

    New port primegen version 0.97: A small, fast library to generate prime numbers in order

---
+++ math/primegen/files/patch-primes.c
@@ -0,0 +1,8 @@
+--- primes.c.orig	2009-03-27 09:19:08.000000000 -0300
++++ primes.c	2009-03-27 09:19:14.000000000 -0300
+@@ -1,4 +1,5 @@
+ #include <stdio.h>
++#include <stdlib.h>
+ #include "primegen.h"
+ #include "fs64.h"
+ 
---
+++ math/primegen/files/patch-primespeed.c
@@ -0,0 +1,8 @@
+--- primespeed.c.orig	2009-03-27 09:17:41.000000000 -0300
++++ primespeed.c	2009-03-27 09:18:54.000000000 -0300
+@@ -1,3 +1,5 @@
++#include <stdio.h>
++#include <stdlib.h>
+ #include "timing.h"
+ #include "primegen.h"
+ #include "primegen_impl.h"
