b7c0de4d2277992ee87958d8f4cbf8779ceb197e
roam <roam@FreeBSD.org>
2009-03-27 22:21:06 +0000

    Update to prips-0.9.5, released by me after adopting the upstream package.

--- sysutils/prips/files/patch-except.c
+++
@@ -1,9 +0,0 @@
---- except.c.orig	Wed Jun  2 10:50:51 1999
-+++ except.c	Fri May 26 03:59:25 2006
-@@ -1,5 +1,6 @@
- #include <stdio.h>
- #include <stdlib.h>
-+#include <string.h>
- #include <ctype.h>
- #include <math.h>
- #include <assert.h>
