f911c8e90e03a71a0e4b7af45e25f147c27ce4a6
naddy <naddy@FreeBSD.org>
2009-03-27 16:52:58 +0000

    Mark MAKE_JOBS_SAFE and squash some warnings.

---
+++ astro/xworld/files/patch-compile_world.c
@@ -0,0 +1,10 @@
+--- compile_world.c.orig	2009-03-27 17:51:01.000000000 +0100
++++ compile_world.c	2009-03-27 17:51:15.000000000 +0100
+@@ -33,6 +33,7 @@
+ #include <sys/types.h>
+ #include <sys/mman.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ #include "Sphere.h"
+ #include "SphereDim.h"
+ 
