981f0b55f422e83c9707f7d3e6573a4fafb43284
Mario Sergio Fujikawa Ferreira <lioux@FreeBSD.org>
2009-03-27 12:52:23 +0000

    o Silence some compiler warnings by adding some missing header files o No PORTREVISION bump since there was no functional change

---
+++ math/djbfft/files/patch-accuracy2.c
@@ -0,0 +1,8 @@
+--- accuracy2.c.orig	2009-03-27 09:49:20.000000000 -0300
++++ accuracy2.c	2009-03-27 09:49:29.000000000 -0300
+@@ -1,4 +1,5 @@
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <math.h>
+ #include "fftc4.h"
+ #include "fftc8.h"
---
+++ math/djbfft/files/patch-speed.c
@@ -0,0 +1,8 @@
+--- speed.c.orig	2009-03-27 09:49:42.000000000 -0300
++++ speed.c	2009-03-27 09:49:57.000000000 -0300
+@@ -1,4 +1,5 @@
+ #include <stdio.h>
++#include <stdlib.h>
+ #include "fftr4.h"
+ #include "fftr8.h"
+ #include "fftc4.h"
