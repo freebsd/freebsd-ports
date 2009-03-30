a339efba1ead766d721aa85a2598d90fe29af24c
Munechika SUMIKAWA <sumikawa@FreeBSD.org>
2009-03-30 04:47:37 +0000

    Upgrade to 20090327c.

--- security/racoon2/files/patch-spmd-spmd_internal.h
+++ security/racoon2/files/patch-spmd-spmd_internal.h
@@ -1,10 +1,10 @@
---- spmd/spmd_internal.h~	2007-12-13 19:40:24.000000000 +0900
-+++ spmd/spmd_internal.h	2008-08-26 15:10:46.000000000 +0900
+--- spmd/spmd_internal.h.orig	2008-07-06 11:41:36.000000000 +0900
++++ spmd/spmd_internal.h	2009-03-30 13:35:24.000000000 +0900
 @@ -35,7 +35,7 @@
  
  extern int spmd_foreground;
  
--#define RACOON2_CONFIG_FILE SYSCONFDIR"/racoon2/racoon2.conf"
+-#define RACOON2_CONFIG_FILE SYSCONFDIR"/racoon2.conf"
 +#define RACOON2_CONFIG_FILE RACOON_CONF
  
  #if defined(HAVE_NSSWITCH_CONF)
