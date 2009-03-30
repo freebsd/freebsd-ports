d3c45e5f78108702351d2c7476f0701b2ac841f6
Pav Lucistnik <pav@FreeBSD.org>
2009-03-30 20:19:19 +0000

    - Convert to USE_TK and allow on any tk (changes default package to use tk85)
    PR:		ports/132181
    Submitted by:	Hirohisa Yamaguchi <umq@ueo.co.jp>
    Approved by:	maintainer timeout (perky; 1 month)

--- x11-toolkits/py-tkinter/files/setup.py
+++ x11-toolkits/py-tkinter/files/setup.py
@@ -15,7 +15,7 @@ try:
 except:
     raise SystemExit, "Distutils problem"
 
-tkversion = "8.4"
+tkversion = "%%TK_VER%%"
 prefix = sysconfig.PREFIX
 # Python 1.5 doesn't have os.getenv()?
 x11base = os.environ['LOCALBASE'] or '/usr/X11R6'
