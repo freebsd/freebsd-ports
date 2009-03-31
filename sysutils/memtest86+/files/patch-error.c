c7177fcff9d72954f13045f59526545ce4ce8e8f
danfe <danfe@FreeBSD.org>
2009-03-31 22:23:19 +0000

    Add memtest86+ 2.11, stand-alone memory test for x86 architecture computers.
    PR:		ports/132700 (based on)
    Submitted by:	Andriy Gapon

---
+++ sysutils/memtest86+/files/patch-error.c
@@ -0,0 +1,10 @@
+--- error.c	2008-11-16 02:18:14.000000000 +0200
++++ error.c	2009-02-18 00:27:40.000000000 +0200
+@@ -11,7 +11,6 @@
+  
+ #include "test.h"
+ #include "config.h"
+-#include <sys/io.h>
+ #include "dmi.h"
+ 
+ extern int test_ticks, nticks, beepmode;
