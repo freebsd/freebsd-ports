2e36f107a0f28e1a733b09293f4e300e7df15398
Pietro Cerutti <gahr@FreeBSD.org>
2009-04-02 21:57:50 +0000

    - Update to 1.4.2 - Prepare for upcoming boost 1.37.0 [1]
    PR:		132833 [1]
    Submitted by:	Dmitry Marakasov <amdmi3@amdmi3.ru>

---
+++ graphics/aqsis/files/patch-displays-piqsl_piqsl.cpp
@@ -0,0 +1,10 @@
+--- displays/piqsl/piqsl.cpp.orig	2009-04-01 00:14:24.000000000 +0200
++++ displays/piqsl/piqsl.cpp	2009-04-01 00:13:48.000000000 +0200
+@@ -29,6 +29,7 @@
+ #define WIN32_LEAN_AND_MEAN
+ #endif
+ 
++#include	<iostream.h>
+ #include	<tiffio.h>
+ #include	<string>
+ #include	<list>
