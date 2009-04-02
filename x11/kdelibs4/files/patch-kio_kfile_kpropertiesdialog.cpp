b10dc19b83a34255e660e5c62310c18b0ed5dbab
makc <makc@FreeBSD.org>
2009-04-02 20:20:43 +0000

    Fix patch to allow build on HEAD

--- x11/kdelibs4/files/patch-kio_kfile_kpropertiesdialog.cpp
+++ x11/kdelibs4/files/patch-kio_kfile_kpropertiesdialog.cpp
@@ -1,15 +1,15 @@
 --- ../kio/kfile/kpropertiesdialog.cpp.orig	2009-03-26 17:44:17.000000000 +0300
-+++ ../kio/kfile/kpropertiesdialog.cpp	2009-04-02 10:12:09.000000000 +0400
++++ ../kio/kfile/kpropertiesdialog.cpp	2009-04-03 00:02:27.000000000 +0400
 @@ -1770,7 +1770,15 @@
      // pick the groups to which the user belongs
      int groupCount = 0;
      gid_t *groups = NULL;
 +#ifdef Q_OS_FREEBSD
 +#include <osreldate.h>
-+#if (__FreeBSD_version <= 800000) // Fixed in kern/114655
++#endif
++#if defined(Q_OS_FREEBSD) && (__FreeBSD_version <= 800000) // Fixed in kern/114655
 +    gid_t tmp_group;
 +    if (getgrouplist(strUser, user->pw_gid, &tmp_group, &groupCount) < 0) {
-+#endif
 +#else
      if (getgrouplist(strUser, user->pw_gid, NULL, &groupCount) < 0) {
 +#endif
