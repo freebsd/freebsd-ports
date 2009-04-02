803c7a4818508c95b4617cabd5caeae911b08c5c
miwi <miwi@FreeBSD.org>
2009-04-02 09:23:03 +0000

    The KDE FreeBSD team is proud to announce the release of KDE 4.2.2 for FreeBSD. The official KDE 4.2.2 release changelog can be found at: http://www.kde.org/announcements/changelogs/changelog4_2_1to4_2_2.php.
    Also mark some ports MAKE_JOBS_SAFE.
    
    New Ports:
    	misc/kde4-l10n-sk - Slovak

---
+++ x11/kdelibs4/files/patch-kio_kfile_kpropertiesdialog.cpp
@@ -0,0 +1,18 @@
+--- ../kio/kfile/kpropertiesdialog.cpp.orig	2009-03-26 17:44:17.000000000 +0300
++++ ../kio/kfile/kpropertiesdialog.cpp	2009-04-02 10:12:09.000000000 +0400
+@@ -1770,7 +1770,15 @@
+     // pick the groups to which the user belongs
+     int groupCount = 0;
+     gid_t *groups = NULL;
++#ifdef Q_OS_FREEBSD
++#include <osreldate.h>
++#if (__FreeBSD_version <= 800000) // Fixed in kern/114655
++    gid_t tmp_group;
++    if (getgrouplist(strUser, user->pw_gid, &tmp_group, &groupCount) < 0) {
++#endif
++#else
+     if (getgrouplist(strUser, user->pw_gid, NULL, &groupCount) < 0) {
++#endif
+         groups = new gid_t[groupCount];
+         if (groups) {
+             getgrouplist(strUser, user->pw_gid, groups, &groupCount);
