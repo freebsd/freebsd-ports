9647c301e78e62780f2dd58285bdccfbd845951e
miwi <miwi@FreeBSD.org>
2009-04-03 09:28:02 +0000

    - Update to 3.1.0
    PR:		133322
    Submitted by:	Ports Fury

--- net/linphone-base/files/patch-coreapi::linphonecore.c
+++ net/linphone-base/files/patch-coreapi::linphonecore.c
@@ -1,5 +1,5 @@
---- coreapi/linphonecore.c.orig	2008-02-12 20:18:04.000000000 +0900
-+++ coreapi/linphonecore.c	2008-02-14 02:54:42.000000000 +0900
+--- coreapi/linphonecore.c.orig	2009-03-10 00:22:47.000000000 +0900
++++ coreapi/linphonecore.c	2009-03-26 01:38:40.000000000 +0900
 @@ -29,11 +29,9 @@
  #include "exevents.h"
  
@@ -12,7 +12,7 @@
  
  #ifdef WIN32
  #define HAVE_EXOSIP_GET_VERSION 1
-@@ -318,7 +316,7 @@
+@@ -341,7 +339,7 @@
  	const char **devices;
  	int ndev;
  	int i;
@@ -21,3 +21,12 @@
  	/*alsadev let the user use custom alsa device within linphone*/
  	devid=lp_config_get_string(lc->config,"sound","alsadev",NULL);
  	if (devid){
+@@ -2108,7 +2106,7 @@
+ 	if (olddev!=NULL && olddev!=lc->video_conf.device){
+ 		toggle_video_preview(lc,FALSE);/*restart the video local preview*/
+ 	}
+-	if (lc->ready){
++	if (lc->ready && lc->video_conf.device){
+ 		vd=ms_web_cam_get_string_id(lc->video_conf.device);
+ 		if (vd && strstr(vd,"Static picture")!=NULL){
+ 			vd=NULL;
