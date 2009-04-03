6f5c34aeb1860da3c8db7c438025b70e33508ad5
Martin Wilke <miwi@FreeBSD.org>
2009-04-03 08:52:31 +0000

    - Update to 0.6.0
    PR:		133320
    Submitted by:	Ports Fury

--- multimedia/dvbcut/files/patch-defines.h
+++ multimedia/dvbcut/files/patch-defines.h
@@ -1,6 +1,6 @@
---- src/defines.h~	Sun Dec 11 21:13:26 2005
-+++ src/defines.h	Mon Dec 19 23:05:03 2005
-@@ -40,6 +40,12 @@
+--- src/defines.h.orig	2007-09-30 23:31:46.000000000 +0900
++++ src/defines.h	2009-03-27 01:33:10.000000000 +0900
+@@ -42,6 +42,12 @@
  #endif
    }
  
@@ -11,13 +11,12 @@
 +#define htom16(x) htobe16(x)
 +#else
  #if __BYTE_ORDER == __LITTLE_ENDIAN
- #define mbo32(x) (__bswap_constant_32(x))
- #define htom32(x) (__bswap_32(x))
-@@ -50,6 +56,7 @@
- #define htom32(x) (x)
+ #define mbo32(x) \
+       ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) | \
+@@ -55,5 +61,6 @@
  #define mbo16(x) (x)
  #define htom16(x) (x)
-+#endif
  #endif
++#endif
  
- #define DVBCUT_QSETTINGS_DOMAIN "dvbcut.sf.net"
+ #endif
