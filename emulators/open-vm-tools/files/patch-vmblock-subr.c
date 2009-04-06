0f0942af4f26e0e635f784bef831bbcfb96749e1
mbr <mbr@FreeBSD.org>
2009-04-06 14:57:03 +0000

    Upgrade to 2009.03.18
    Various fixes for X11
    
    Submitted by:	Alexander Kabaev <kabaev@gmail.com>

--- emulators/open-vm-tools/files/patch-vmblock-subr.c
+++ emulators/open-vm-tools/files/patch-vmblock-subr.c
@@ -1,6 +1,19 @@
 --- modules/freebsd/vmblock/subr.c.orig	2009-02-28 23:04:14.000000000 +0100
 +++ modules/freebsd/vmblock/subr.c	2009-02-28 23:05:38.000000000 +0100
-@@ -372,6 +372,13 @@
+@@ -334,7 +334,12 @@
+    vp->v_vnlock = &vp->v_lock;
+    FREE(xp, M_VMBLOCKFSNODE);
+    vp->v_op = &dead_vnodeops;
++
++#if __FreeBSD_version >= 800011
++   (void) vn_lock(vp, LK_EXCLUSIVE | LK_RETRY);
++#else
+    (void) vn_lock(vp, LK_EXCLUSIVE | LK_RETRY, curthread);
++#endif
+    vgone(vp);
+    vput(vp);
+ }
+@@ -405,6 +410,13 @@
        return error;
     }
  
