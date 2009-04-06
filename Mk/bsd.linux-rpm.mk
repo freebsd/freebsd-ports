d6cf761d43bdca78f36bb493b2fa9d0a511c4424
Boris Samorodov <bsam@FreeBSD.org>
2009-04-06 12:57:35 +0000

    Add comments forgotten by me at the previous commit.

--- Mk/bsd.linux-rpm.mk
+++ Mk/bsd.linux-rpm.mk
@@ -1,7 +1,7 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: /tmp/pcvs/ports/Mk/bsd.linux-rpm.mk,v 1.16 2009-04-06 12:48:13 bsam Exp $
+# $FreeBSD: /tmp/pcvs/ports/Mk/bsd.linux-rpm.mk,v 1.17 2009-04-06 12:57:35 bsam Exp $
 #
 
 # Variables:
@@ -28,9 +28,9 @@
 # BRANDELF_FILES	- A list of files to brand as a linux executable in
 #					  case BRANDELF_DIRS can't be used.
 # _F8_COMPATIBLE_LINUX_BASE_PORTS
-			- A list of linux base ports compatible (may be used) with -f8-
-					  linux infrastructure ports. This variable is ment
-					  to be used at *.mk files
+#			- A list of linux base ports compatible (may be used) with -f8-
+#					  linux infrastructure ports. This variable is ment
+#					  to be used at *.mk files
 
 .if !defined(_POSTMKINCLUDED) && !defined(Linux_RPM_Pre_Include)
 
