96dfb13c9ba82217448e632324be7f4d7dc686bb
Mikhail Teterin <mi@FreeBSD.org>
2009-04-07 21:53:13 +0000

    Make it compile cleanly (with -Wall) on 64-bit platforms. It still does not work right... Allow parallel build.
    The project seems dead (no releases in 8 years) and the port should,
    probably, be dropped...

--- mail/kiltdown/files/patch-mailfolder.cc
+++ mail/kiltdown/files/patch-mailfolder.cc
@@ -1,7 +1,67 @@
---- kiltdown/mailfolder.cc.orig	Mon May 14 05:25:23 2001
-+++ kiltdown/mailfolder.cc	Mon Sep  9 12:04:36 2002
-@@ -1353,10 +1353,10 @@
-         messagePreview->setFont(QFont(pstr, i));
+--- kiltdown/mailfolder.cc	2001-05-13 23:25:23.000000000 -0400
++++ kiltdown/mailfolder.cc	2008-05-21 23:27:13.000000000 -0400
+@@ -263,5 +263,5 @@
+ 
+         item.boldEntry = unread;
+-        item.itemData = (unsigned int)(message);
++        item.itemData = (void *)(message);
+         er = populateColumnsFromInfo(&item.columns, ccolumns, columns, message);
+         RETURN_ON_FAILURE(er);
+@@ -354,5 +354,5 @@
+         K_ASSERT((int)hdr[x].itemData >= 0 && (int)hdr[x].itemData <= MC_LAST, "Bogus MC_ Code!!!");
+ 
+-        info[x].which = (enum MailColumn)hdr[x].itemData;
++        info[x].which = (enum MailColumn)(uintptr_t)hdr[x].itemData;
+         info[x].fixed = hdr[x].type == CW_FIXED;
+         info[x].width = hdr[x].width;
+@@ -497,5 +497,5 @@
+ {
+     int fewerUnread = 0;
+-    unsigned int *rg;
++    void **rg;
+     ReadStatus rs;
+     bool ignoreAsk = false;
+@@ -530,8 +530,8 @@
+      * First, try and remove them from the UI
+      */
+-    rg = (unsigned int *)localAlloc(count * sizeof(unsigned int));
++    rg = (void **)localAlloc(count * sizeof(void *));
+     if (!rg) return E_OUTOFMEMORY;
+     for (int x = 0; x < count; x++)
+-        rg[x] = (unsigned int)deleteme[x];
++        rg[x] = (void *)deleteme[x];
+     messageView->removeRows(count, rg);
+     localFree(rg);
+@@ -655,8 +655,8 @@
+ ERRCODE MailFolder::enableMailListPopupItems()
+ {
+-    LinkList<unsigned int> selectedItemDataList;
++    LinkList<void *> selectedItemDataList;
+     bool enableUnread = false;
+     bool enableRead = false;
+-    unsigned int ui;
++    void *ui;
+     ReadStatus rs;
+     ERRCODE er;
+@@ -1127,6 +1127,6 @@
+ )
+ {
+-    LinkList<unsigned int> list;
+-    unsigned int a;
++    LinkList<void *> list;
++    void *a;
+     ERRCODE er;
+     int x;
+@@ -1221,6 +1221,6 @@
+ )
+ {
+-    LinkList<unsigned int> selectedItemDataList;
+-    unsigned int itemData;
++    LinkList<void *> selectedItemDataList;
++    void * itemData;
+     MailMessage *message;
+     ReadStatus rs;
+@@ -1354,8 +1354,8 @@
          localFree(pstr);
      }
 -    messagePreview->setUndoEnabled(false);
@@ -12,4 +72,54 @@
 +    //messagePreview->setUndoEnabled(true);
  
      er = S_OK;
+@@ -1709,6 +1709,6 @@
+ )
+ {
+-    LinkList<unsigned int> selectedItemDataList;
+-    unsigned int itemData;
++    LinkList<void *> selectedItemDataList;
++    void * itemData;
+     ERRCODE er;
  
+@@ -1777,5 +1777,5 @@
+     KMailListItem *item;
+ 
+-    item = messageView->nextItem((unsigned int)index);
++    item = messageView->nextItem((void *)index);
+     
+     return item ? (MailMessage *)item->itemData : NULL;
+@@ -1802,5 +1802,5 @@
+     KMailListItem *item;
+ 
+-    item = messageView->previousItem((unsigned int)index);
++    item = messageView->previousItem((void *)index);
+ 
+     return item ? (MailMessage *)item->itemData : NULL;
+@@ -2414,5 +2414,5 @@
+ {
+ //    updateMessageStatus(m);
+-    messageView->selectRowFromItemData((unsigned int)m);
++    messageView->selectRowFromItemData((void *)m);
+ }
+  
+@@ -2470,5 +2470,5 @@
+     for (int x = 0; x < ccolumns; x++) {
+         
+-        headers[x].itemData = columns[x].which;
++        headers[x].itemData = (void *)(uintptr_t)columns[x].which;
+         headers[x].width = columns[x].width;
+         headers[x].type = columns[x].fixed ? CW_FIXED : CW_PERCENTAGE;
+@@ -2610,5 +2610,5 @@
+                 break;
+         }
+-        item.itemData = (unsigned int)(messages[x]);
++        item.itemData = (void *)(messages[x]);
+ 
+         er = populateColumnsFromInfo(&item.columns, ccolumns, columns, messages[x]);
+@@ -2750,5 +2750,5 @@
+             memset(&item, 0, sizeof(KMailListItem));
+             item.boldEntry = (rsNew == MSG_UNOPENED);
+-            item.itemData = (unsigned int)(message);
++            item.itemData = (void *)(message);
+             er = populateColumnsFromInfo(&item.columns, ccolumns, columns, message);
+             GOTO_ON_FAILURE(er, Error);
--- mail/kiltdown/files/patch-mailviewer.cc
+++ mail/kiltdown/files/patch-mailviewer.cc
@@ -1,7 +1,6 @@
---- kiltdown/mailviewer.cc.orig	Sun May 15 22:29:50 2005
-+++ kiltdown/mailviewer.cc	Sun May 15 22:28:00 2005
-@@ -950,13 +950,13 @@ void MailViewer::layoutComponents()
-                                   ? ID_LOW_PRIORITY_TEXT 
+--- kiltdown/mailviewer.cc	2001-05-13 23:25:23.000000000 -0400
++++ kiltdown/mailviewer.cc	2008-05-21 23:27:52.000000000 -0400
+@@ -951,11 +951,11 @@
                                    : ID_HIGH_PRIORITY_TEXT);
              if (pstr) {
 -                QPixmap p(p == LowPriority ? lowpriIcon : priorityhdrIcon);
@@ -18,4 +17,21 @@
 +                                           this->width() - qp.width() - 13,
                                             ctlHeight);
                  top += ctlHeight + 5;
-                 priorityPixLabel->show();
+@@ -1563,8 +1563,8 @@
+ )
+ {
+-    LinkList<unsigned int> list;
++    LinkList<void *> list;
+     const char *pstr, *pstr2;
+     SelectMeDialog *smd;
+-    unsigned int ui;
++    void *ui;
+     Attachment *a;
+     ERRCODE er;
+@@ -1592,5 +1592,5 @@
+         a->getFileName(&pstr);
+         if (pstr) {
+-            smd->addItem(pstr, (unsigned int)a);
++            smd->addItem(pstr, (void *)a);
+             localFree(pstr);
+         }
