cd2b31c61c4629ce5a133cb4623ee10dec3e9e7d
lwhsu <lwhsu@FreeBSD.org>
2009-04-11 06:24:21 +0000

    - Update to 0.1.1
    PR:		ports/133573
    Submitted by:	Yi-Jheng Lin <yzlin AT cs.nctu.edu.tw> (maintainer)

--- devel/py-fabric/files/patch-setup.py
+++ devel/py-fabric/files/patch-setup.py
@@ -4,8 +4,8 @@
      author = 'Christian Vest Hansen',
      author_email = 'karmazilla@gmail.com',
      url = 'http://www.nongnu.org/fab/',
--    install_requires = ['paramiko >=1.6, <2.0'],
-+    #install_requires = ['paramiko >=1.6, <2.0'],
+-    install_requires = ['paramiko >=1.7, <2.0'],
++    #install_requires = ['paramiko >=1.7, <2.0'],
      py_modules = ['fabric'],
      entry_points = {
          'console_scripts': [
