7451f7c9037b2a74510be34dbb6f4efadf650143
pgollucci <pgollucci@FreeBSD.org>
2009-04-12 05:10:27 +0000

    - USE_APACHE=yes is deprecated - Convert to AP_FAST_BUILD/AP_GEPLIST - OPTIONify - Bump PORTREVISION

--- www/mod_blowchunks/files/patch-mod_blowchunks.c
+++ www/mod_blowchunks/files/patch-mod_blowchunks.c
@@ -1,5 +1,5 @@
---- mod_blowchunks-1.3.c.orig	Sat Jun 22 22:44:01 2002
-+++ mod_blowchunks-1.3.c	Sat Jun 22 22:45:18 2002
+--- mod_blowchunks.c.orig	Sat Jun 22 22:44:01 2002
++++ mod_blowchunks.c	Sat Jun 22 22:45:18 2002
 @@ -56,8 +56,10 @@
      ap_table_do(blowchunks_check_one_header,&found,r->headers_in,
  	    "Transfer-Encoding",NULL);
