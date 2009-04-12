2ae036e78c72f62a0043c415a22041b5e0802c93
Martin Wilke <miwi@FreeBSD.org>
2009-04-12 19:27:13 +0000

    Take maintainership. The first of two steps is to take all Gecko Stuff from the Gnome team. I'd like to say thanks a lot for all the work in the past years.

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -32,7 +32,7 @@
 .if !defined(Gecko_Pre_Include)
 # Please make sure all changes to this file are passed through the maintainer.
 # Do not commit them yourself (unless of course you're the Port's Wraith ;).
-Gecko_Include_MAINTAINER=		gnome@FreeBSD.org
+Gecko_Include_MAINTAINER=		miwi@FreeBSD.org
 Gecko_Pre_Include=			bsd.gecko.mk
 
 # Users should use the following syntax:
