6b97b2f5048fedb18ba6058be7137d6755379e19
Joe Marcus Clarke <marcus@FreeBSD.org>
2009-04-16 17:32:57 +0000

    Fix a number of long-standing bugs with gamin.
    * Do not use kernel notifications for paths like /mnt and /media.  This
      will allow such volumes to be unmounted.  This matches the behvaior
      of Linux.
    * Allow the "poll" directive in gaminrc files to have an effect.
    * Add some verbiage to pkg-message to instruct users how to configure
      gamin [1]
    
    PR:		131121 [1]
    Submitted by:	Graham Todd <gtodd@bellanet.org> (based on) [1]
    Reported by:	countless users
    Approved by:	portmgr (implicit)

--- devel/gamin/files/patch-server_gam_server.c
+++ devel/gamin/files/patch-server_gam_server.c
@@ -1,5 +1,5 @@
 --- server/gam_server.c.orig	2007-07-04 09:36:49.000000000 -0400
-+++ server/gam_server.c	2007-08-10 15:09:14.000000000 -0400
++++ server/gam_server.c	2009-04-15 22:37:01.000000000 -0400
 @@ -32,7 +32,7 @@
  #include "gam_server.h"
  #include "gam_channel.h"
@@ -9,7 +9,17 @@
  #ifdef ENABLE_INOTIFY
  #include "gam_inotify.h"
  #endif
-@@ -438,7 +438,7 @@ gam_server_get_kernel_handler (void)
+@@ -200,7 +200,8 @@ gam_add_subscription(GamSubscription * s
+ 			return gam_poll_add_subscription (sub);
+ 		else
+ #endif
+-			return gam_kernel_add_subscription (sub);
++			/*return gam_kernel_add_subscription (sub);*/
++			return gam_poll_add_subscription (sub);
+ 	} else {
+ 		gam_fs_mon_type type;
+ 		type = gam_fs_get_mon_type (path);
+@@ -438,7 +439,7 @@ gam_server_get_kernel_handler (void)
  GamPollHandler
  gam_server_get_poll_handler (void)
  {
