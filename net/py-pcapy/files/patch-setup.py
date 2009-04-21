718016d4e5f35bf386bf81b124a52a3caa3d40b6
lwhsu <lwhsu@FreeBSD.org>
2009-04-21 08:25:09 +0000

    - Respect to NOPORTDOCS
    PR:		ports/133821
    Submitted by:	Yi-Jheng Lin <yzlin AT cs.nctu.edu.tw>

---
+++ net/py-pcapy/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2009-04-19 02:22:16.549958526 +0800
++++ setup.py	2009-04-19 02:24:10.837075743 +0800
+@@ -60,7 +60,5 @@
+           include_dirs = include_dirs,
+           library_dirs = library_dirs,
+           libraries = libraries)],
+-      data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
+-                     ['README', 'LICENSE', 'pcapy.html'])],
+       )
+ 
