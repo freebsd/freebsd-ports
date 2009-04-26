aa3d4c052bb47d0ac97090f67936e8d7362df93d
Martin Wilke <miwi@FreeBSD.org>
2009-04-26 23:21:50 +0000

    It will transparently hijack HTTP traffic on a network, watch for HTTPS links and redirects, then map those links into either look-alike HTTP links or homograph-similar HTTPS links. It also supports modes for supplying a favicon which looks like a lock icon, selective logging, and session denial.
    WWW:	http://www.thoughtcrime.org/software/sslstrip/
    
    PR:		ports/134021
    Submitted by:	Matt Donovan <kitchetech@gmail.com>

---
+++ security/py-sslstrip/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2009-04-24 10:19:32.000000000 -0400
++++ setup.py	2009-04-24 10:19:45.000000000 -0400
+@@ -5,7 +5,7 @@
+ shutil.copyfile("sslstrip.py", "sslstrip/sslstrip")
+ 
+ setup  (name        = 'sslstrip',
+-        version     = '0.1',
++        version     = '0.2',
+         description = 'A MITM tool that implements Moxie Marlinspike\'s HTTPS stripping attacks.',
+         author = 'Moxie Marlinspike',
+         author_email = 'moxie@thoughtcrime.org',
