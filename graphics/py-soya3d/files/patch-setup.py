ff254cd7cd4a1a87121fd7037973f94a346d6513
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2009-04-27 01:16:35 +0000

    - Update to 0.14 - Remove obsolete dependency

--- graphics/py-soya3d/files/patch-setup.py
+++ graphics/py-soya3d/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2007-11-05 20:44:27.000000000 -0500
-+++ setup.py	2007-11-05 20:47:14.000000000 -0500
-@@ -22,6 +22,7 @@
+--- setup.py	2009-04-22 10:00:23.000000000 -0500
++++ setup.py	2009-04-22 10:03:23.000000000 -0500
+@@ -21,6 +21,7 @@
  
  # Modify the following if needed :
  USE_OPENAL = 1     # use OpenAL
@@ -8,20 +8,37 @@
  #USE_OPENAL = 0
  
  # Modify the following if needed :
-@@ -76,12 +77,6 @@
+@@ -32,7 +33,7 @@
+ INCDIR = [
+ 	#"ode-0.5/include",
+ 	"/usr/include",
+-	"/usr/local/include",
++	"%%LOCALBASE%%/include",
+ 	"/usr/X11R6/include",
+ 	"/usr/X11/include",
+ 	"/usr/include/freetype2",
+@@ -48,7 +49,7 @@
+ LIBDIR = [
+ 	#"ode-0.5/lib",
+ 	"/usr/lib",
+-	"/usr/local/lib",
++	"%%LOCALBASE%%/lib",
+ 	"/opt/local/lib", # For Mac OS X "darwin port"
+ 	"/usr/X11R6/lib",
+ 	"/usr/X11/lib",
+@@ -79,11 +80,6 @@
  SDISTING = ("sdist" in sys.argv[1:]) and not ("--help" in sys.argv[1:])
  
- 
+ MACOSX_DEPLOYMENT_TARGET  = os.getenv('MACOSX_DEPLOYMENT_TARGET')
 -try:
 -	from Pyrex.Distutils import build_ext
 -	HAVE_PYREX = 1
 -except:
 -	HAVE_PYREX = 0
--
- HERE = os.path.dirname(sys.argv[0])
- #ODE_DIR = os.path.join(HERE, "ode-0.5")
- DEFINES = []
-@@ -129,6 +124,7 @@
+ 
+ # Only enable Pyrex compilation for SVN sources
+ if not os.path.exists(os.path.join(os.path.dirname(__file__), ".svn")):
+@@ -159,6 +155,7 @@
  		DEFINES.append(('SOYA_MACOSX',1))
  	else:
  		LIBS.append("openal")
