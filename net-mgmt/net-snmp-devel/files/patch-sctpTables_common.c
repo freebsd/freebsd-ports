b6bb8c3d3a17625aa826094130b3d84530b6dae4
cvs2svn <cvs2svn@FreeBSD.org>
2009-04-28 21:27:54 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_7_2_0'.

--- net-mgmt/net-snmp-devel/files/patch-sctpTables_common.c
+++
@@ -1,9 +0,0 @@
---- agent/mibgroup/sctp-mib/sctpTables_common.c.orig	2009-03-07 23:32:28.000000000 +0900
-+++ agent/mibgroup/sctp-mib/sctpTables_common.c	2009-03-07 23:33:09.000000000 +0900
-@@ -1,3 +1,6 @@
-+#include <net-snmp/net-snmp-config.h>
-+#include <net-snmp/net-snmp-includes.h>
-+#include <net-snmp/agent/net-snmp-agent-includes.h>
- #include "sctpTables_common.h"
- #include "sctpAssocTable.h"
- #include "sctpAssocRemAddrTable.h"
