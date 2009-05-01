b34f7e417c08280f5d543181071e27f1726bd03c
wxs <wxs@FreeBSD.org>
2009-05-01 14:41:13 +0000

    - Update to 1.6.0
    PR:		ports/133361
    Submitted by:	Andrew Clark <andrewclarkii@gmail.com>
    Approved by:	maintainer timeout

---
+++ www/free-sa/files/extra-patch-global-i386.mk
@@ -0,0 +1,13 @@
+--- global.mk.orig	2009-04-04 14:04:07.000000000 +0400
++++ global.mk	2009-04-04 14:04:45.000000000 +0400
+@@ -1,8 +1,8 @@
+ # Operating system profile
+-OSTYPE = generic-any-cc
++#OSTYPE = generic-any-cc
+ #OSTYPE = altlinux-i586-gcc4
+ #OSTYPE = cygwin-native-gcc3
+-#OSTYPE = freebsd-i386-gcc3
++OSTYPE = freebsd-i386-gcc3
+ #OSTYPE = freebsd-native-gcc4
+ #OSTYPE = hpux-64_2.0W-cc
+ #OSTYPE = openbsd-i386-gcc3
