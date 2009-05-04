8d390b0a3f7958b993525c0e50d5e07e763c2933
beat <beat@FreeBSD.org>
2009-05-04 20:06:22 +0000

    - Update to 2.0 - Enable multiple make jobs - Fix build on amd64 - Fix build on sparc64 - Pass maintainership to gecko@
    New Features in 2.0:
      * Flock 2.0 is based on the Firefox 3 technology.
      * MySpace has been integrated as a people service.
      * Revver has been added as Media Bar search service.
      * TinyPic has been added as a Photo Uploader service.
      * Flock can now detect Media RSS streams, load them in the
        Media Bar, and subscribe to them.
    
    On behalf of the Gecko Gang.
    
    Approved by:	miwi (mentor)

--- www/flock/files/patch-build_unix_run-mozilla.sh
+++ www/flock/files/patch-build_unix_run-mozilla.sh
@@ -1,21 +1,21 @@
---- build/unix/run-mozilla.sh.orig	Mon Dec 15 12:27:28 2003
-+++ build/unix/run-mozilla.sh	Wed Feb 11 14:24:17 2004
-@@ -355,7 +355,7 @@
- fi
- ##
- ## Set LD_LIBRARY_PATH
--LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
-+LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko18:%%LOCALBASE%%/lib/npapi/symlinks/flock:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
- if [ -n "$LD_LIBRARYN32_PATH" ]
+--- build/unix/run-mozilla.sh.orig	2007-09-05 02:18:08.000000000 +0200
++++ build/unix/run-mozilla.sh	2009-04-20 23:17:23.000000000 +0200
+@@ -353,7 +353,7 @@
+ ##  a symbolic link, all other shared libraries are symbolic links also.
+ if [ `uname -s` != "SunOS" -o -h "$MOZ_DIST_BIN/libmozjs.so" ]
  then
- 	LD_LIBRARYN32_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARYN32_PATH+":$LD_LIBRARYN32_PATH"}
-@@ -442,6 +442,9 @@
- #
+-	LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
++	LD_LIBRARY_PATH=${MOZ_DIST_BIN}:${MOZ_DIST_BIN}/plugins:%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko18:%%LOCALBASE%%/lib/npapi/symlinks/flock:${MRE_HOME}${LD_LIBRARY_PATH+":$LD_LIBRARY_PATH"}
+ fi 
+ 
+ if [ -n "$LD_LIBRARYN32_PATH" ]
+@@ -434,6 +434,9 @@
  export MOZILLA_FIVE_HOME LD_LIBRARY_PATH
  export SHLIB_PATH LIBPATH LIBRARY_PATH ADDON_PATH DYLD_LIBRARY_PATH
-+
+ 
 +MOZ_PLUGIN_PATH=%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko18:%%LOCALBASE%%/lib/npapi/symlinks/flock
 +export MOZ_PLUGIN_PATH
- 
++
  if [ $moz_debug -eq 1 ]
  then
+ 	moz_debug_program ${1+"$@"}
