efc28cb43f595994237072b1fcc6a73cfb17acf0
Martin Wilke <miwi@FreeBSD.org>
2009-05-09 09:19:26 +0000

    - Update to version 1.0.63
    PR:		134291
    Submitted by:	Ports Fury

--- net/gsk/files/patch-src_gskipv4.c
+++ net/gsk/files/patch-src_gskipv4.c
@@ -1,10 +1,12 @@
---- src/gskipv4.c.orig	Tue Dec  7 06:53:22 2004
-+++ src/gskipv4.c	Sun Jan 30 23:55:09 2005
-@@ -1,5 +1,7 @@
+--- src/gskipv4.c.orig	2006-05-18 03:39:35.000000000 +0900
++++ src/gskipv4.c	2009-04-08 00:18:45.000000000 +0900
+@@ -1,7 +1,9 @@
  #include <string.h>
  #include <stdlib.h>
-+#include <sys/types.h>
+ #include <sys/types.h>
 +#include <sys/socket.h>
  #include <netinet/in.h>
++#include <arpa/inet.h>
  #include "gskipv4.h"
  
+ /* Standard ipv4 addresses in guint8[4] format. */
