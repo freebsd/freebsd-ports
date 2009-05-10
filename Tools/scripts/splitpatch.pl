ec5050b6b2680e6d1ef4998e60358f19e35abcd0
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2009-05-10 16:28:52 +0000

    When naming the split patch files, replace the original path '/' with '__' instead of a single '_' to follow the current patch naming guidelines.

--- Tools/scripts/splitpatch.pl
+++ Tools/scripts/splitpatch.pl
@@ -57,7 +57,7 @@ sub cstart
 	if (!/^---\s+\d+,\d+\s+/ && /^---\s+(\S+)\s+/) {
 		$state = \&body;
 		$out = $1;
-		$out =~ s|/|_|g;
+		$out =~ s|/|__|g;
 		$out = "patch-$out";
 		if (open OUT, "> $out") {
 			print OUT $fl;
