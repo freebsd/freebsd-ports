08d631c9b27206cd6bf90dcfff816ae9dacd2977
dhn <dhn@FreeBSD.org>
2009-05-11 19:24:41 +0000

    - Update to 0.32.5 - Fix build on 64-bit archs
    PR:		ports/134306
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu> (maintainer)
    Tested by:	gahr
    Approved by:	miwi (mentor implicit)

--- emulators/yape/files/patch-archdep.cpp
+++ emulators/yape/files/patch-archdep.cpp
@@ -3,7 +3,7 @@ $FreeBSD$
 
 --- archdep.cpp.orig
 +++ archdep.cpp
-@@ -49,7 +49,7 @@
+@@ -75,7 +75,7 @@
  int ad_makedirs(char *path)
  {
    strcpy(temp,path);
@@ -12,30 +12,21 @@ $FreeBSD$
    CreateDirectory(temp, NULL);
  
    return 1;
-@@ -108,7 +108,7 @@
+@@ -296,7 +296,7 @@
+ 	// Possible buffer overflow fixed.
+ 	strncpy(temp, path, 512);
+ 	if (strlen(temp) > 506) return 0;
+-	strcat(temp, "/yape");
++	strcat(temp, "/.yape");
+ 	mkdir(temp, 0777);
  
-   strcpy( homedir , getenv( "HOME" ));
-   //strcpy( filter, filefilter);
--  strcat( homedir, "/yape" );
-+  strcat( homedir, "/.yape" );
-   chdir ( homedir );
-   dirp = opendir( homedir );
-   //fprintf( stderr, "reading from %s\n", homedir);
-@@ -136,7 +136,7 @@
- int ad_makedirs(char *path)
- {
-   strcpy(temp,path);
--  strcat(temp, "/yape");
-+  strcat(temp, "/.yape");
-   mkdir(temp, 0777);
- 
-   return 1;
-@@ -146,7 +146,7 @@
- {
- 
- 	strcpy( temp, tmpchr);
+ 	return 1;
+@@ -307,7 +307,7 @@
+ 	// Possible buffer overflow fixed.
+ 	strncpy(temp, tmpchr, 512);
+ 	if (strlen(temp) > 496) return NULL;
 -	strcat( temp, "/yape/yape.conf");
 +	strcat( temp, "/.yape/yape.conf");
- 
  	return temp;
  }
+ 
---
+++ emulators/yape/files/patch-serial.h
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- serial.h.orig
++++ serial.h
+@@ -1,7 +1,7 @@
+ #ifndef _SERIAL_H
+ #define _SERIAL_H
+ 
+-#include "SDL/SDL.h"
++#include "SDL.h"
+ 
+ class CSerial {
+ 
