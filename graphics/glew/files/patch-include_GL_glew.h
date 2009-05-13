b5687bbd7bb4975271f50af7377cf38380beaf6b
mva <mva@FreeBSD.org>
2009-05-13 17:36:51 +0000

    Update to 1.5.1
    Approved by:	miwi (mentor)

--- graphics/glew/files/patch-include_GL_glew.h
+++ graphics/glew/files/patch-include_GL_glew.h
@@ -1,11 +1,11 @@
---- include/GL/glew.h.orig
-+++ include/GL/glew.h
-@@ -9647,7 +9647,9 @@
+--- include/GL/glew.h.orig	2008-11-03 07:25:21.000000000 +0100
++++ include/GL/glew.h	2009-05-04 21:55:35.000000000 +0200
+@@ -12243,7 +12243,9 @@
  #ifdef GLEW_APIENTRY_DEFINED
  #undef GLEW_APIENTRY_DEFINED
  #undef APIENTRY
 -#undef GLAPIENTRY
-+/* Do not undef GLAPIENTRY here. This is a quick hack to avoid 
++/* Do not undef GLAPIENTRY here. This is a quick hack to avoid
 +   undefined symbols when including glut.h just after glew.h */
 +/* #undef GLAPIENTRY */
  #endif
