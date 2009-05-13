b9bf66abe81343e64dbf8563225daee5b872f7b4
ale <ale@FreeBSD.org>
2009-05-13 05:22:58 +0000

    Update to 5.0.81 release.

--- databases/mysql50-server/files/patch-mysys_default.c
+++ databases/mysql50-server/files/patch-mysys_default.c
@@ -1,5 +1,5 @@
---- mysys/default.c.orig	2008-08-04 14:19:45.000000000 +0200
-+++ mysys/default.c	2008-08-27 12:23:54.000000000 +0200
+--- mysys/default.c.orig	2009-04-16 13:49:52.000000000 +0200
++++ mysys/default.c	2009-05-12 07:54:11.000000000 +0200
 @@ -634,7 +634,7 @@
    {
      MY_STAT stat_info;
@@ -12,7 +12,7 @@
 @@ -1084,7 +1084,10 @@
      errors += add_directory(alloc, env, dirs);
  #elif defined(DEFAULT_SYSCONFDIR)
-   if (DEFAULT_SYSCONFDIR != "")
+   if (DEFAULT_SYSCONFDIR[0])
 +  {
      errors += add_directory(alloc, DEFAULT_SYSCONFDIR, dirs);
 +    errors += add_directory(alloc, DEFAULT_SYSCONFDIR "/mysql", dirs);
