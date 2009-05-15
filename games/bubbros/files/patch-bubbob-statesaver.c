52b5b14fa33520590316898115e16b3e0f43cfa4
Steven Kreuzer <skreuzer@FreeBSD.org>
2009-05-15 21:15:02 +0000

    Maintainer update to version 1.6
    PR:		ports/134313
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu>
    Approved by:	wxs (mentor)

--- games/bubbros/files/patch-bubbob-statesaver.c
+++ games/bubbros/files/patch-bubbob-statesaver.c
@@ -1,26 +1,26 @@
---- bubbob/statesaver.c.orig	Sun Jul 15 08:59:52 2007
-+++ bubbob/statesaver.c	Sun Jul 15 09:01:41 2007
-@@ -44,10 +44,12 @@
+--- bubbob/statesaver.c.orig
++++ bubbob/statesaver.c
+@@ -75,10 +75,12 @@
      Py_INCREF(g);  /* exhausted -- can return 'g' itself */
      return g;
    }
 +#if PY_VERSION_HEX < 0x02050000
    if (f->f_nfreevars || f->f_ncells) {
      PyErr_SetString(PyExc_ValueError, "generator has cell or free vars");
-     return NULL;
+     goto error;
    }
 +#endif
  
-   dummy = (PyObject**) malloc(co->co_argcount * sizeof(PyObject*));
-   if (dummy == NULL)
-@@ -95,10 +97,12 @@
-         return -1;
+   if (co->co_argcount == 0)
+     dummy = NULL;
+@@ -143,10 +145,12 @@
        }
        f2 = (PyFrameObject*) x;
+ 
 +#if PY_VERSION_HEX < 0x02050000
        if (f2->f_stacksize != f->f_stacksize) {
          PyErr_SetString(PyExc_TypeError, "stack size mismatch");
-         return -1;
+         goto error;
        }
 +#endif
  
