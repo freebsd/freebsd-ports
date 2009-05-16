c424711199ee3e5bdaf98e7f9a4e1cb16f90070b
Martin Wilke <miwi@FreeBSD.org>
2009-05-16 22:35:20 +0000

    Multiple repositories Trac version ----------------------------------
    This branch adds support for accessing multiple repositories inside one Trac
    project and based on 0.12 development version of Trac.
    
    Trac uses a minimalistic approach to web-based software project management.
    Our mission; to help developers write great software while staying out of
    the way.  Trac should impose as little as possible on a team's established
    development process and policies.
    
    What is Trac?
    
        * An integrated system for managing software projects
        * An enhanced wiki
        * A flexible web-based issue tracker
        * An interface to the Subversion revision control system
    
    At the core of Trac lies an integrated wiki and issue/bug database.  Using
    wiki markup, all objects managed by Trac can directly link to other
    issues/bug reports, code changesets, documentation and files.
    
    WWW: http://trac.edgewall.org/
    
    PR:		ports/134314
    Submitted by:	Alexey V. Degtyarev <alexey at renatasystems.org>

---
+++ www/trac-multirepos/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2009-05-06 00:26:12.000000000 +0400
++++ setup.py	2009-05-06 00:26:19.000000000 +0400
+@@ -70,7 +70,7 @@
+ 
+     install_requires = [
+         'setuptools>=0.6b1',
+-        'Genshi>=0.6dev-r960'
++        'Genshi>=0.6dev'
+     ],
+     extras_require = {
+         'Babel': ['Babel>=0.9.4'],
