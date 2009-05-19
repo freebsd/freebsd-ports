a3b133da8edb7cdbe69feed2617e04f4b3a2b803
kwm <kwm@FreeBSD.org>
2009-05-19 08:31:39 +0000

    Update to 2.26.2.

--- net/vino/files/patch-server_vino-dbus-listener.c
+++ net/vino/files/patch-server_vino-dbus-listener.c
@@ -1,12 +1,11 @@
---- server/vino-dbus-listener.c.orig	2009-02-27 20:36:08.000000000 -0500
-+++ server/vino-dbus-listener.c	2009-02-27 20:36:30.000000000 -0500
-@@ -35,6 +35,9 @@
+--- server/vino-dbus-listener.c.orig	2009-05-18 17:59:03.000000000 +0200
++++ server/vino-dbus-listener.c	2009-05-19 10:13:22.000000000 +0200
+@@ -35,6 +35,8 @@
  #include <string.h>
  #include <unistd.h>
  #include <netdb.h>
 +#include <sys/types.h>
 +#include <sys/socket.h>
-+#include <netinet/in.h>
  #include <net/if.h>
  #include <arpa/inet.h>
- 
+ #include <netinet/in.h>
