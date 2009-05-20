ff2e9b908b9a9c3be8ba877bd086a4e14bf6eb72
ale <ale@FreeBSD.org>
2009-05-20 19:32:13 +0000

    Update to 6.0.11 release and add new manpages.

--- databases/mysql60-server/files/patch-mysys_default.c
+++ databases/mysql60-server/files/patch-mysys_default.c
@@ -1,6 +1,6 @@
---- mysys/default.c.orig	2008-08-12 17:31:37.000000000 +0200
-+++ mysys/default.c	2008-08-27 12:38:14.000000000 +0200
-@@ -638,7 +638,7 @@
+--- mysys/default.c.orig	2009-05-11 14:50:54.000000000 +0200
++++ mysys/default.c	2009-05-19 07:45:53.000000000 +0200
+@@ -679,7 +679,7 @@
    {
      MY_STAT stat_info;
      if (!my_stat(name,&stat_info,MYF(0)))
@@ -9,10 +9,10 @@
      /*
        Ignore world-writable regular files.
        This is mainly done to protect us to not read a file created by
-@@ -1091,7 +1091,10 @@
+@@ -1128,7 +1128,10 @@
  
  #if defined(DEFAULT_SYSCONFDIR)
-   if (DEFAULT_SYSCONFDIR != "")
+   if (DEFAULT_SYSCONFDIR[0])
 +  {
      errors += add_directory(alloc, DEFAULT_SYSCONFDIR, dirs);
 +    errors += add_directory(alloc, DEFAULT_SYSCONFDIR "/mysql", dirs);
