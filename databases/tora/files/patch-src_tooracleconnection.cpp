5828cf08c915c4f644083e5c8054477c7bc6b889
dhn <dhn@FreeBSD.org>
2009-05-22 10:13:20 +0000

    - Update to 2.0.0 - Pass maintainership to submitter
    PR:		ports/134429
    Submitted by:	Cezary Morga <cm@therek>

---
+++ databases/tora/files/patch-src_tooracleconnection.cpp
@@ -0,0 +1,10 @@
+--- src/tooracleconnection.cpp.orig	2008-10-29 16:59:11.000000000 +0100
++++ src/tooracleconnection.cpp	2009-05-07 19:16:09.390371657 +0200
+@@ -57,7 +57,6 @@
+ 
+ #define OTL_STL
+ #define OTL_EXCEPTION_ENABLE_ERROR_OFFSET
+-#define OTL_ORA_UTF8
+ #define OTL_ORA_UNICODE
+ #define OTL_ORA_TIMESTAMP
+ #define OTL_ANSI_CPP
