d7515fb3b564dcdb13526b119954aa81d9e8969c
Martin Wilke <miwi@FreeBSD.org>
2009-05-28 23:49:42 +0000

    - Update to 1.1.8
    PR:		135036
    Submitted by:	Ports Fury

--- net-p2p/deluge/files/patch-deluge_ui_gtkui_gtkui.py
+++ net-p2p/deluge/files/patch-deluge_ui_gtkui_gtkui.py
@@ -1,17 +1,18 @@
---- deluge/ui/gtkui/gtkui.py.orig	2009-02-21 16:50:22.000000000 -0600
-+++ deluge/ui/gtkui/gtkui.py	2009-02-21 16:50:24.000000000 -0600
-@@ -119,10 +119,10 @@
- 
-         # Initialize gettext
-         try:
--            if hasattr(locale, "bindtextdomain"):
--                locale.bindtextdomain("deluge", pkg_resources.resource_filename("deluge", "i18n"))
--            if hasattr(locale, "textdomain"):
--                locale.textdomain("deluge")
-+#            if hasattr(locale, "bindtextdomain"):
-+#                locale.bindtextdomain("deluge", pkg_resources.resource_filename("deluge", "i18n"))
-+#            if hasattr(locale, "textdomain"):
-+#                locale.textdomain("deluge")
-             gettext.bindtextdomain("deluge", pkg_resources.resource_filename("deluge", "i18n"))
-             gettext.textdomain("deluge")
-             gettext.install("deluge", pkg_resources.resource_filename("deluge", "i18n"))
+--- deluge/ui/gtkui/gtkui.py.orig	2009-04-10 01:40:52.000000000 +0900
++++ deluge/ui/gtkui/gtkui.py	2009-05-27 04:37:04.000000000 +0900
+@@ -81,7 +81,6 @@
+     "start_in_tray": False,
+     "lock_tray": False,
+     "tray_password": "",
+-    "check_new_releases": True,
+     "default_load_path": None,
+     "window_maximized": False,
+     "window_x_pos": 0,
+@@ -101,7 +100,6 @@
+     "autoadd_enable": False,
+     "autoadd_location": "",
+     "choose_directory_dialog_path": deluge.common.get_default_download_dir(),
+-    "show_new_releases": True,
+     "signal_port": 40000,
+     "ntf_tray_blink": True,
+     "ntf_sound": False,
