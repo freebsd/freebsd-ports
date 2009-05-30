09e65acfe87dd5cd640df3886fe0978429e07b53
Martin Wilke <miwi@FreeBSD.org>
2009-05-30 21:04:14 +0000

    - Update to 0.9a2
    PR:		134850
    Submitted by:	Yi-Jheng Lin <yzlin@cs.nctu.edu.tw> (maintainer)

--- devel/py-fabric/files/patch-setup.py
+++ devel/py-fabric/files/patch-setup.py
@@ -1,11 +1,11 @@
 --- setup.py.orig
 +++ setup.py
-@@ -23,7 +23,7 @@
-     author = 'Christian Vest Hansen',
-     author_email = 'karmazilla@gmail.com',
-     url = 'http://www.nongnu.org/fab/',
--    install_requires = ['paramiko >=1.7, <2.0'],
-+    #install_requires = ['paramiko >=1.7, <2.0'],
-     py_modules = ['fabric'],
-     entry_points = {
+@@ -13,7 +13,7 @@
+     author='Jeff Forcier',
+     author_email='jeff@bitprophet.org',
+     url='http://fabfile.org',
+-    install_requires=['paramiko >=1.7, <2.0'],
++    #install_requires=['paramiko >=1.7, <2.0'],
+     packages=find_packages(),
+     entry_points={
          'console_scripts': [
