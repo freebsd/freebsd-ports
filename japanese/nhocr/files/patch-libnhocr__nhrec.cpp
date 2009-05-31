2ff34db50ff88b971ac22ab254b1145df4f6035f
amdmi3 <amdmi3@FreeBSD.org>
2009-05-31 22:34:54 +0000

    OCR engine for Japanese language
    WWW: http://code.google.com/p/nhocr
    
    PR:		134720
    Submitted by:	Hiroto Kagotani <hiroto.kagotani@gmail.com>

---
+++ japanese/nhocr/files/patch-libnhocr__nhrec.cpp
@@ -0,0 +1,11 @@
+--- ./libnhocr/nhrec.cpp.orig	2009-05-10 23:56:11.000000000 +0900
++++ ./libnhocr/nhrec.cpp	2009-05-19 19:24:48.000000000 +0900
+@@ -45,7 +45,7 @@
+ 
+ 
+ // default file/path names
+-static char	*nhocrlibdir0 = "/opt/nhocr/share";
++static char	*nhocrlibdir0 = "/usr/local/share/nhocr";
+ static char	*cctablefile0 = "cctable.utf-8";
+ static char	*dicfile0 = "PLM.dic";
+ 
---
+++ japanese/nhocr/files/patch-libnhocr__ocrbase.cpp
@@ -0,0 +1,11 @@
+--- ./libnhocr/ocrbase.cpp.orig	2009-05-15 15:55:20.000000000 +0900
++++ ./libnhocr/ocrbase.cpp	2009-05-19 19:24:48.000000000 +0900
+@@ -9,7 +9,7 @@
+ #include	<stdlib.h>
+ #include	<string.h>
+ #include	<math.h>
+-#include	<values.h>
++#include	<float.h>
+ 
+ #if defined (__SVR4) && defined (__sun)
+ #include	<float.h>
