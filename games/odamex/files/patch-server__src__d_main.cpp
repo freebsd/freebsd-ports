6c9268bd82def01f6471ed67c6722a9d4faa9d03
Dmitry Marakasov <amdmi3@FreeBSD.org>
2009-05-31 18:34:04 +0000

    - Update to 0.4.3 - Pass maintainership to submitter
    PR:		134595
    Submitted by:	Josh Rickmar <joshua_rickmar@eumx.net>

--- games/odamex/files/patch-server__src__d_main.cpp
+++ games/odamex/files/patch-server__src__d_main.cpp
@@ -1,6 +1,6 @@
---- ./client/src/d_main.cpp.orig	2009-03-08 00:28:16.000000000 -0500
-+++ ./client/src/d_main.cpp	2009-05-16 18:23:51.000000000 -0400
-@@ -807,6 +807,8 @@
+--- ./server/src/d_main.cpp.orig	2009-03-08 00:28:11.000000000 -0500
++++ ./server/src/d_main.cpp	2009-05-18 16:40:24.000000000 -0400
+@@ -597,6 +597,8 @@
  	AddSearchDir(dirs, Args.CheckValue("-waddir"), separator);
  	AddSearchDir(dirs, getenv("DOOMWADDIR"), separator);
  	AddSearchDir(dirs, getenv("DOOMWADPATH"), separator);
@@ -8,4 +8,4 @@
 +    AddSearchDir(dirs, "%%DMDIR%%", separator);
      AddSearchDir(dirs, getenv("HOME"), separator);
  
-     dirs.erase(std::unique(dirs.begin(), dirs.end()), dirs.end());
+ 	dirs.erase(std::unique(dirs.begin(), dirs.end()), dirs.end());
