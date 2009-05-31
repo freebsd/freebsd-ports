382c7c051293a37c088f5c73406a68962042254f
anray <anray@FreeBSD.org>
2009-05-31 09:13:59 +0000

    - Fix libdir detection - Delete `-Wno-pointer-sign' for build with gcc<4 (1)
    Reported by:	pav (1)

--- graphics/ida/files/patch-mk::Autoconf.mk
+++ graphics/ida/files/patch-mk::Autoconf.mk
@@ -1,5 +1,14 @@
---- mk/Autoconf.mk.old	2009-05-24 18:47:11.000000000 +0300
-+++ mk/Autoconf.mk	2009-05-24 18:49:32.000000000 +0300
+--- mk/Autoconf.mk.orig	2008-06-09 17:56:36.000000000 +0300
++++ mk/Autoconf.mk	2009-05-28 08:51:09.000000000 +0300
+@@ -109,7 +109,7 @@
+ #	$(call ac_fini))
+ ac_lib64 = $(shell \
+ 	$(call ac_init,for libdir name);\
+-	$(call ac_s_cmd,/sbin/ldconfig -p | grep -q lib64 &&\
++	$(call ac_s_cmd,$(CC) -print-search-dirs | grep -q lib64 &&\
+ 		echo "lib64" || echo "lib");\
+ 	$(call ac_fini))
+ 
 @@ -118,9 +118,9 @@
  	$(call ac_init,for X11 app-defaults prefix);\
  	$(call ac_s_cmd, for dir in \
