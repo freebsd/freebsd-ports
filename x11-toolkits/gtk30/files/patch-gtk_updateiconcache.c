eae5d68c3f96d954d2396df560b03796374949ef
marcus <marcus@FreeBSD.org>
2009-05-31 06:13:04 +0000

    Update to 2.16.2.

--- x11-toolkits/gtk30/files/patch-gtk_updateiconcache.c
+++ x11-toolkits/gtk30/files/patch-gtk_updateiconcache.c
@@ -4,7 +4,7 @@
    if (!validate_file (tmp_cache_path))
      {
        g_printerr (_("The generated cache was invalid.\n"));
--      //g_unlink (tmp_cache_path);
+-      /*g_unlink (tmp_cache_path);*/
 +      g_unlink (tmp_cache_path);
        exit (1);
      }
