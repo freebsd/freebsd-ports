a8bbcc5af60151a6559ff4aec0fcdc3d30f8d3b5
dinoex <dinoex@FreeBSD.org>
2009-06-01 10:34:39 +0000

    - do not modify owner/group of NLS dirs Reported By:	QAT

--- comms/hylafax/files/patch-nls.mk
+++ comms/hylafax/files/patch-nls.mk
@@ -1,14 +1,16 @@
 --- po/nls.mk.orig	2009-01-21 15:32:14.000000000 +0100
-+++ po/nls.mk	2009-05-31 12:40:46.000000000 +0200
-@@ -8,6 +8,7 @@
++++ po/nls.mk	2009-06-01 12:30:35.000000000 +0200
+@@ -8,6 +8,9 @@
  	${MSGCAT} libhylafax/messages.pot hylafax-client/messages.pot hylafax-server/messages.pot >> $@.tmp
  	mv $@.tmp $@
  
 +FIXMATCH=	${DEPTH}/*/messages.po
++SYSUSER=	root
++SYSGROUP=	wheel
  
  # A bit more trikery here
  # We want $(wildcard...) because we don't want to try to "rebuild" thes messages.pot
-@@ -15,18 +16,18 @@
+@@ -15,18 +18,18 @@
  # But legacy makes (SCO) don't support $(wildcard...).  Luckly, they don't support $(...)
  # At all, so they just drop it all.  Real makes support $(wildcard)
  # That's why we use the $(...${...})
