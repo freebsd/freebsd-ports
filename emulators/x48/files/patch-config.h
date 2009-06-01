d287692c6f4969f18204fe00674ef86a16aa7bfa
Pav Lucistnik <pav@FreeBSD.org>
2009-06-01 13:21:28 +0000

    - Fix corrupted display on xorg-7.4
    PR:		ports/133398
    Submitted by:	Esa Karkkainen <ejk@iki.fi>
    Approved by:	maintainer timeout (danfe; 7 weeks)

---
+++ emulators/x48/files/patch-config.h
@@ -0,0 +1,11 @@
+--- config.h.orig	2005-04-14 21:33:56.000000000 +0300
++++ config.h	2009-04-05 19:46:14.000000000 +0300
+@@ -49,7 +49,7 @@
+ /*
+  * If you don't have the XShm extension, comment the following line
+  */
+-#define HAVE_XSHM
++/* #define HAVE_XSHM */
+ 
+ /*
+  * If you don't want to use the readline library,
