b67e89375e2935769a740e6daa93cab4e01ce4e4
bsam <bsam@FreeBSD.org>
2009-06-01 17:26:31 +0000

    Here are new Linux Fedora 10 infrastructure ports.
    Those ports are intended to be used with 8-CURRENT at least
    with SVN r192206.
    
    If you want to switch to linux-f10 ports, please define at /etc/make.conf:
      OVERRIDE_LINUX_BASE_PORT=f10
      OVERRIDE_LINUX_NONBASE_PORTS=f10
    
    An upgrading procedure is shown at /usr/ports/UPDATING, entries 20090401
    and 20070327.
    
    For the first time all tested linux ports work as expected(!):
    . acroread8;
    . google-earth;
    . skype;
    . seamonkey.
    
    Many thanks for kernel folks who really did the main work
    (and I wrote only some lines of ports).
    
    There is a good chance that those ports may become a default
    for 8.0-RELEASE. Please, test and report back to emulation@ ML.

--- Mk/bsd.linux-apps.mk
+++ Mk/bsd.linux-apps.mk
@@ -24,7 +24,7 @@
 #					  (components), covered here.
 # OVERRIDE_LINUX_NONBASE_PORTS
 #			- This specifies a none-default linux infrastructure ports to use.
-#					  Valid value is "f8" to use Linux Fedora 8 ports.
+#					  Valid values are "f8" and "f10" to use Linux Fedora [8,10] ports.
 #					  This is an user-only variable. Don't use it in any port,
 #					  it's meant to be used in make.conf.
 
@@ -43,8 +43,10 @@ Linux_APPS_Post_Include=	bsd.linux-apps.mk
 .  if (${LINUX_OSRELEASE} == "2.6.16") && defined(OVERRIDE_LINUX_NONBASE_PORTS)
 .    if ${OVERRIDE_LINUX_NONBASE_PORTS} == "f8"
 LINUX_DIST_SUFFIX=	-f8
+.    elif ${OVERRIDE_LINUX_NONBASE_PORTS} == "f10"
+LINUX_DIST_SUFFIX=	-f10
 .    else
-IGNORE=		valid value for OVERRIDE_LINUX_NONBASE_PORTS is \"f8\"
+IGNORE=		valid values for OVERRIDE_LINUX_NONBASE_PORTS are \"f8\", \"f10\"
 .    endif
 .  else
 LINUX_DIST_SUFFIX=
@@ -89,122 +91,147 @@ IGNORE=	bsd.linux-apps.mk test failed: The component ${component} can be used wi
 
 allegro_FILE=		${LINUXBASE}/usr/lib/liballeg-4.0.3.so
 allegro_f8_FILE=	${LINUXBASE}/usr/lib/liballeg-4.2.2.so
+allegro_f10_FILE=	${LINUXBASE}/usr/lib/liballeg-4.2.2.so
 allegro_DETECT=		${allegro${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 allegro_PORT=		${PORTSDIR}/devel/linux${LINUX_DIST_SUFFIX}-allegro
 allegro_DEPENDS=	xorglibs
 
 alsalib_FILE=		${LINUXBASE}/lib/libasound.so.2.0.0
 alsalib_f8_FILE=	${alsalib_FILE}
+alsalib_f10_FILE=	${LINUXBASE}/lib/libasound.so.2.0.0
 alsalib_DETECT=		${alsalib${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 alsalib_PORT=		${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-alsa-lib
 
 arts_FILE=			${LINUXBASE}/usr/lib/libartsc.so.0
 arts_f8_FILE=		${arts_FILE}
+arts_f10_FILE=		${LINUXBASE}/usr/lib/libartsc.so.0
 arts_DETECT=		${arts${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 arts_PORT=			${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-arts
 
 aspell_FILE=		${LINUXBASE}/usr/lib/libaspell.so.15.0.2
 aspell_f8_FILE=		${LINUXBASE}/usr/lib/libaspell.so.15.1.4
+aspell_f10_FILE=	${LINUXBASE}/usr/lib/libaspell.so.15.1.4
 aspell_DETECT=		${aspell${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 aspell_PORT=		${PORTSDIR}/textproc/linux${LINUX_DIST_SUFFIX}-aspell
 
 atk_FILE=			${LINUXBASE}/usr/lib/libatk-1.0.so.0.901.0
 atk_f8_FILE=		${LINUXBASE}/usr/lib/libatk-1.0.so.0.2009.1
+atk_f10_FILE=		${LINUXBASE}/usr/lib/libatk-1.0.so.0.2409.1
 atk_DETECT=			${atk${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 atk_PORT=			${PORTSDIR}/accessibility/linux${LINUX_DIST_SUFFIX}-atk
 
 cairo_FILE=			${LINUXBASE}/usr/lib/libcairo.so.2.2.3
 cairo_f8_FILE=		${LINUXBASE}/usr/lib/libcairo.so.2.11.7
+cairo_f10_FILE=		${LINUXBASE}/usr/lib/libcairo.so.2.10800.0
 cairo_DETECT=		${cairo${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 cairo_PORT=			${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-cairo
 cairo_DEPENDS=		fontconfig png xorglibs
 
 curl_FILE=		${LINUXBASE}/usr/lib/libcurl.so.3.0.0
 curl_f8_FILE=		${LINUXBASE}/usr/lib/libcurl.so.4.1.0
+curl_f10_FILE=		${LINUXBASE}/usr/lib/libcurl.so.4.1.1
 curl_DETECT=		${curl${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 curl_PORT=		${PORTSDIR}/ftp/linux${LINUX_DIST_SUFFIX}-curl
 
 dri_FILE=		${LINUXBASE}/usr/X11R6/lib/libGL.so.1
 dri_f8_FILE=		${LINUXBASE}/usr/lib/libGL.so.1
+dri_f10_FILE=		${LINUXBASE}/usr/lib/libGL.so.1.2
 dri_DETECT=		${dri${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
-. if ${LINUX_DIST_SUFFIX} == "-f8"
-dri_PORT=		${PORTSDIR}/graphics/linux-f8-dri
-.else
+.  if ${LINUX_DIST_SUFFIX} == ""
 dri_PORT=		${PORTSDIR}/graphics/linux_dri
-.endif
+.  elif ${LINUX_DIST_SUFFIX} == "f8"
+dri_PORT=		${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-dri
+.  else
+dri_PORT=		${PORTSDIR}/graphics/linux-dri74
+.  endif
+dri_DEPENDS=		xorglibs
 
 esound_FILE=		${LINUXBASE}/usr/lib/libesd.so.0.2.36
 esound_f8_FILE=		${LINUXBASE}/usr/lib/libesd.so.0.2.38
+esound_f10_FILE=	${LINUXBASE}/usr/lib/libesd.so.0.2.39
 esound_DETECT=		${esound${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 esound_PORT=		${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-esound
 esound_DEPENDS=		libaudiofile
 
 expat_FILE=			${LINUXBASE}/usr/lib/libexpat.so.0
 expat_f8_FILE=		${LINUXBASE}/lib/libexpat.so.1
+expat_f10_FILE=		${LINUXBASE}/lib/libexpat.so.1
 expat_DETECT=		${expat${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 expat_PORT=			${PORTSDIR}/textproc/linux${LINUX_DIST_SUFFIX}-expat
 
 fontconfig_FILE=	${LINUXBASE}/usr/lib/libfontconfig.so.1
 fontconfig_f8_FILE=	${LINUXBASE}/usr/lib/libfontconfig.so.1.2.0
+fontconfig_f10_FILE=	${LINUXBASE}/usr/lib/libfontconfig.so.1.3.0
 fontconfig_DETECT=	${fontconfig${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 fontconfig_PORT=	${PORTSDIR}/x11-fonts/linux${LINUX_DIST_SUFFIX}-fontconfig
 fontconfig_DEPENDS=	expat
 
 freealut_FILE=		${LINUXBASE}/usr/lib/libalut.so.0.1.0
 freealut_f8_FILE=	${freealut_FILE}
+freealut_f10_FILE=	${LINUXBASE}/usr/lib/libalut.so.0.1.0
 freealut_DETECT=	${freealut${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 freealut_PORT=		${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-freealut
 freealut_DEPENDS=	openal
 
 gdkpixbuf_FILE=		${LINUXBASE}/usr/lib/libgdk_pixbuf.so.2
 gdkpixbuf_f8_FILE=	${gdkpixbuf_FILE}
+gdkpixbuf_f10_FILE=	${LINUXBASE}/usr/lib/libgdk_pixbuf.so.2
 gdkpixbuf_DETECT=	${gdkpixbuf${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 gdkpixbuf_PORT=		${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-gdk-pixbuf
 
 gtk_FILE=			${LINUXBASE}/usr/lib/libgtk-1.2.so.0.9.1
 gtk_f8_FILE=		${gtk_FILE}
+gtk_f10_FILE=		${LINUXBASE}/usr/lib/libgtk-1.2.so.0.9.1
 gtk_DETECT=			${gtk${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 gtk_PORT=			${PORTSDIR}/x11-toolkits/linux${LINUX_DIST_SUFFIX}-gtk
 gtk_DEPENDS=		xorglibs
 
 gtk2_FILE=			${LINUXBASE}/usr/lib/libgtk-x11-2.0.so.0.600.10
 gtk2_f8_FILE=		${LINUXBASE}/usr/lib/libgtk-x11-2.0.so.0.1200.8
+gtk2_f10_FILE=		${LINUXBASE}/usr/lib/libgtk-x11-2.0.so.0.1400.7
 gtk2_DETECT=		${gtk2${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 gtk2_PORT=			${PORTSDIR}/x11-toolkits/linux${LINUX_DIST_SUFFIX}-gtk2
 gtk2_DEPENDS=		atk jpeg png pango tiff xorglibs
 
 hicontheme_FILE=	${LINUXBASE}/usr/share/icons/hicolor
 hicontheme_f8_FILE=	${hicontheme_FILE}
+hicontheme_f10_FILE=	${LINUXBASE}/usr/share/icons/hicolor
 hicontheme_DETECT=	${hicontheme${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 hicontheme_PORT=	${PORTSDIR}/x11-themes/linux${LINUX_DIST_SUFFIX}-hicolor-icon-theme
 
 imlib_FILE=			${LINUXBASE}/usr/lib/libgdk_imlib.so.1.9.13
 imlib_f8_FILE=		${LINUXBASE}/usr/lib/libgdk_imlib.so.1.9.15
+imlib_f10_FILE=		${LINUXBASE}/usr/lib/libgdk_imlib.so.1.9.15
 imlib_DETECT=		${imlib${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 imlib_PORT=			${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-imlib
 
 jpeg_FILE=			${LINUXBASE}/usr/lib/libjpeg.so.62.0.0
 jpeg_f8_FILE=		${jpeg_FILE}
+jpeg_f10_FILE=		${LINUXBASE}/usr/lib/libjpeg.so.62.0.0
 jpeg_DETECT=		${jpeg${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 jpeg_PORT=			${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-jpeg
 
 libaudiofile_FILE=	${LINUXBASE}/usr/lib/libaudiofile.so.0.0.2
 libaudiofile_f8_FILE=	${libaudiofile_FILE}
+libaudiofile_f10_FILE=	${LINUXBASE}/usr/lib/libaudiofile.so.0.0.2
 libaudiofile_DETECT=	${libaudiofile${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libaudiofile_PORT=	${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-libaudiofile
 
 libg2c_FILE=		${LINUXBASE}/usr/lib/libg2c.so.0.0.0
 libg2c_f8_FILE=		${libg2c_FILE}
+libg2c_f10_FILE=	${LINUXBASE}/usr/lib/libg2c.so.0.0.0
 libg2c_DETECT=		${libg2c${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libg2c_PORT=		${PORTSDIR}/lang/linux${LINUX_DIST_SUFFIX}-libg2c
 
 libglade_FILE=		${LINUXBASE}/usr/lib/libglade.so.0.4.2
 libglade_f8_FILE=	${libglade_FILE}
+libglade_f10_FILE=	${LINUXBASE}/usr/lib/libglade.so.0.4.2
 libglade_DETECT=	${libglade${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libglade_PORT=		${PORTSDIR}/devel/linux${LINUX_DIST_SUFFIX}-libglade
 
 libglade2_FILE=		${LINUXBASE}/usr/lib/libglade-2.0.so.0.0.7
 libglade2_f8_FILE=	${libglade2_FILE}
+libglade2_f10_FILE=	${LINUXBASE}/usr/lib/libglade-2.0.so.0.0.7
 libglade2_DETECT=	${libglade2${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libglade2_PORT=		${PORTSDIR}/devel/linux${LINUX_DIST_SUFFIX}-libglade2
 
@@ -213,51 +240,60 @@ libglu_f8_FILE=		${LINUXBASE}/usr/lib/libGLU.so.1.3.070002
 libglu_DETECT=		${libglu${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libglu_PORT=		${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-libGLU
 
-# no libidn_FILE
+# no libidn_FILE (there is no libidn port for Fedora 4 distribution)
 libidn_f8_FILE=		${LINUXBASE}/usr/lib/libidn.so.11
+# no libidn_f10_FILE (libidn is integrated into linux_base-f10 port)
 libidn_DETECT=		${libidn${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libidn_PORT=		${PORTSDIR}/dns/linux${LINUX_DIST_SUFFIX}-libidn
 
 libmng_FILE=		${LINUXBASE}/usr/lib/libmng.so.1.0.0
 libmng_f8_FILE=		${libmng_FILE}
+libmng_f10_FILE=	${LINUXBASE}/usr/lib/libmng.so.1.0.0
 libmng_DETECT=		${libmng${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libmng_PORT=		${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-libmng
 libmng_DEPENDS=		jpeg
 
 libogg_FILE=		${LINUXBASE}/usr/lib/libogg.so.0.5.2
 libogg_f8_FILE=		${LINUXBASE}/usr/lib/libogg.so.0.5.3
+libogg_f10_FILE=	${LINUXBASE}/usr/lib/libogg.so.0.5.3
 libogg_DETECT=		${libogg${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libogg_PORT=		${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-libogg
 
 libsigcpp20_FILE=	${LINUXBASE}/usr/lib/libsigc-2.0.so.0
 libsigcpp20_f8_FILE=	${libsigcpp20_FILE}
+libsigcpp20_f10_FILE=	${LINUXBASE}/usr/lib/libsigc-2.0.so.0
 libsigcpp20_DETECT=	${libsigcpp20${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libsigcpp20_PORT=	${PORTSDIR}/devel/linux${LINUX_DIST_SUFFIX}-libsigc++20
 
 # no libssh2_FILE
 libssh2_f8_FILE=	${LINUXBASE}/usr/lib/libssh2.so.1
+libssh2_f10_FILE=	${LINUXBASE}/usr/lib/libssh2.so.1
 libssh2_DETECT=		${libssh2${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libssh2_PORT=		${PORTSDIR}/security/linux${LINUX_DIST_SUFFIX}-libssh2
 libssh2_DEPENDS=	openssl
 
 libtheora_FILE=		${LINUXBASE}/usr/lib/libtheora.so.0.1.0
 libtheora_f8_FILE=	${LINUXBASE}/usr/lib/libtheora.so.0.3.2
+libtheora_f10_FILE=	${LINUXBASE}/usr/lib/libtheora.so.0.3.3
 libtheora_DETECT=	${libtheora${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libtheora_PORT=		${PORTSDIR}/multimedia/linux${LINUX_DIST_SUFFIX}-libtheora
 
 libvorbis_FILE=		${LINUXBASE}/usr/lib/libvorbis.so.0.3.0
 libvorbis_f8_FILE=	${LINUXBASE}/usr/lib/libvorbis.so.0.4.0
+libvorbis_f10_FILE=	${LINUXBASE}/usr/lib/libvorbis.so.0.4.0
 libvorbis_DETECT=	${libvorbis${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libvorbis_PORT=		${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-libvorbis
 libvorbis_DEPENDS=	libogg
 
 libxml_FILE=		${LINUXBASE}/usr/lib/libxml.so.1.8.17
 libxml_f8_FILE=		${libxml_FILE}
+libxml_f10_FILE=	${LINUXBASE}/usr/lib/libxml.so.1.8.17
 libxml_DETECT=		${libxml${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libxml_PORT=		${PORTSDIR}/textproc/linux${LINUX_DIST_SUFFIX}-libxml
 
 libxml2_FILE=		${LINUXBASE}/usr/lib/libxml2.so.2.6.19
 libxml2_f8_FILE=	${LINUXBASE}/usr/lib/libxml2.so.2.7.2
+libxml2_f10_FILE=	${LINUXBASE}/usr/lib/libxml2.so.2.7.3
 libxml2_DETECT=		${libxml2${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 libxml2_PORT=		${PORTSDIR}/textproc/linux${LINUX_DIST_SUFFIX}-libxml2
 
@@ -268,124 +304,148 @@ mikmod_PORT=		${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-mikmod
 
 openmotif_FILE=		${LINUXBASE}/usr/X11R6/lib/libXm.so.3.0.3
 openmotif_f8_FILE=	${LINUXBASE}/usr/lib/libXm.so.4.0.2
+openmotif_f10_FILE=	${LINUXBASE}/usr/X11R6/lib/libXm.so.3.0.3
 openmotif_DETECT=	${openmotif${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 openmotif_PORT=		${PORTSDIR}/x11-toolkits/linux${LINUX_DIST_SUFFIX}-openmotif
 openmotif_DEPENDS=	xorglibs
 
 # no nspr_FILE
 nspr_f8_FILE=		${LINUXBASE}/usr/lib/libnspr4.so
+nspr_f10_FILE=		${LINUXBASE}/lib/libnspr4.so
 nspr_DETECT=		${nspr${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 nspr_PORT=		${PORTSDIR}/devel/linux${LINUX_DIST_SUFFIX}-nspr
 
 # no nss_FILE
 nss_f8_FILE=		${LINUXBASE}/usr/lib/libnss3.so
+nss_f10_FILE=		${LINUXBASE}/lib/libnss3.so
 nss_DETECT=		${nss${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 nss_PORT=		${PORTSDIR}/security/linux${LINUX_DIST_SUFFIX}-nss
 nss_DEPENDS=		nspr sqlite3
 
 openal_FILE=		${LINUXBASE}/usr/lib/libopenal.so.0.0.0
 openal_f8_FILE=		${openal_FILE}
+openal_f10_FILE=	${LINUXBASE}/usr/lib/libopenal.so.0.0.0
 openal_DETECT=		${openal${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 openal_PORT=		${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-openal
 openal_DEPENDS=		alsalib arts esound libaudiofile libvorbis sdl12
 
 openssl_FILE=		${LINUXBASE}/lib/libssl.so.0.9.7f
 openssl_f8_FILE=	${LINUXBASE}/lib/libssl.so.0.9.8b
+openssl_f10_FILE=	${LINUXBASE}/lib/libssl.so.0.9.8g
 openssl_DETECT=		${openssl${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 openssl_PORT=		${PORTSDIR}/security/linux${LINUX_DIST_SUFFIX}-openssl
 
 pango_FILE=			${LINUXBASE}/usr/lib/libpango-1.0.so.0.1001.1
 pango_f8_FILE=		${LINUXBASE}/usr/lib/libpango-1.0.so.0.1800.4
+pango_f10_FILE=		${LINUXBASE}/usr/lib/libpango-1.0.so.0.2203.0
 pango_DETECT=		${pango${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 pango_PORT=			${PORTSDIR}/x11-toolkits/linux${LINUX_DIST_SUFFIX}-pango
 pango_DEPENDS=		cairo expat fontconfig xorglibs
 
 png_FILE=			${LINUXBASE}/usr/lib/libpng.so.3.1.2.8
 png_f8_FILE=		${LINUXBASE}/usr/lib/libpng.so.3.22.0
+png_f10_FILE=		${LINUXBASE}/usr/lib/libpng.so.3.35.0
 png_DETECT=			${png${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 png_PORT=			${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-png
 
 png10_FILE=			${LINUXBASE}/usr/lib/libpng.so.2.1.0.18
 png10_f8_FILE=		${LINUXBASE}/usr/lib/libpng.so.2.1.0.30
+png10_f10_FILE=		${LINUXBASE}/usr/lib/libpng.so.2.1.0.43
 png10_DETECT=		${png10${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 png10_PORT=			${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-png10
 
 qt33_FILE=			${LINUXBASE}/usr/lib/qt-3.3/lib/libqt-mt.so.3.3.4
 qt33_f8_FILE=		${LINUXBASE}/usr/lib/qt-3.3/lib/libqt-mt.so.3.3.8
+qt33_f10_FILE=		${LINUXBASE}/usr/lib/qt-3.3/lib/libqt-mt.so.3.3.8
 qt33_DETECT=		${qt33${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 qt33_PORT=			${PORTSDIR}/x11-toolkits/linux${LINUX_DIST_SUFFIX}-qt33
 qt33_DEPENDS=		fontconfig libmng png
 
 sdl12_FILE=		${LINUXBASE}/usr/lib/libSDL-1.2.so.0.7.3
 sdl12_f8_FILE=		${LINUXBASE}/usr/lib/libSDL-1.2.so.0.11.2
+sdl12_f10_FILE=		${LINUXBASE}/usr/lib/libSDL-1.2.so.0.11.2
 sdl12_DETECT=		${sdl12${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 sdl12_PORT=			${PORTSDIR}/devel/linux${LINUX_DIST_SUFFIX}-sdl12
 sdl12_DEPENDS=		xorglibs
 
 sdlimage_FILE=		${LINUXBASE}/usr/lib/libSDL_image-1.2.so.0.1.4
 sdlimage_f8_FILE=	${LINUXBASE}/usr/lib/libSDL_image-1.2.so.0.1.5
+sdlimage_f10_FILE=	${LINUXBASE}/usr/lib/libSDL_image-1.2.so.0.1.5
 sdlimage_DETECT=	${sdlimage${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 sdlimage_PORT=		${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-sdl_image
 sdlimage_DEPENDS=	sdl12
 
 sdlmixer_FILE=		${LINUXBASE}/usr/lib/libSDL_mixer-1.2.so.0.2.4
 sdlmixer_f8_FILE=	${LINUXBASE}/usr/lib/libSDL_mixer-1.2.so.0.2.6
+sdlmixer_f10_FILE=	${LINUXBASE}/usr/lib/libSDL_mixer-1.2.so.0.2.6
 sdlmixer_DETECT=	${sdlmixer${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 sdlmixer_PORT=		${PORTSDIR}/audio/linux${LINUX_DIST_SUFFIX}-sdl_mixer
 sdlmixer_DEPENDS=	sdl12
 
 scimgtk_FILE=		${LINUXBASE}/usr/lib/gtk-2.0/immodules/im-scim.so
 scimgtk_f8_FILE=	${scimgtk_FILE}
+scimgtk_f10_FILE=	${LINUXBASE}/usr/lib/gtk-2.0/immodules/im-scim.so
 scimgtk_DETECT=		${scimgtk${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 scimgtk_PORT=		${PORTSDIR}/textproc/linux${LINUX_DIST_SUFFIX}-scim-gtk
 scimgtk_DEPENDS=	gtk2 scimlibs
 
 scimlibs_FILE=		${LINUXBASE}/usr/lib/libscim-1.0.so.8
 scimlibs_f8_FILE=	${LINUXBASE}/usr/lib/libscim-1.0.so.8
+scimlibs_f10_FILE=	${LINUXBASE}/usr/lib/libscim-1.0.so.8
 scimlibs_DETECT=	${scimlibs${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 scimlibs_PORT=		${PORTSDIR}/textproc/linux${LINUX_DIST_SUFFIX}-scim-libs
 scimlibs_DEPENDS=	gtk2
 
 # no sqlite3_FILE
 sqlite3_f8_FILE=	${LINUXBASE}/usr/lib/libsqlite3.so.0
+sqlite3_f10_FILE=	${LINUXBASE}/usr/lib/libsqlite3.so.0
 sqlite3_DETECT=		${sqlite3${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 sqlite3_PORT=		${PORTSDIR}/databases/linux${LINUX_DIST_SUFFIX}-sqlite3
 
 # no tcl84_FILE
 tcl84_f8_FILE=		${LINUXBASE}/usr/lib/tcl8.4
+# XXX: tcl85!!!
+tcl84_f10_FILE=		${LINUXBASE}/usr/lib/libtcl8.5.so
 tcl84_DETECT=		${tcl84${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 tcl84_PORT=		${PORTSDIR}/lang/linux${LINUX_DIST_SUFFIX}-tcl84
 
 tiff_FILE=			${LINUXBASE}/usr/lib/libtiff.so.3.7.1
 tiff_f8_FILE=		${LINUXBASE}/usr/lib/libtiff.so.3.8.2
+tiff_f10_FILE=		${LINUXBASE}/usr/lib/libtiff.so.3.8.2
 tiff_DETECT=		${tiff${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 tiff_PORT=			${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-tiff
 tiff_DEPENDS=		jpeg
 
 # no tk84_FILE
 tk84_f8_FILE=		${LINUXBASE}/usr/lib/libtk8.4.so
+# XXX: tk85!!!
+tk84_f10_FILE=		${LINUXBASE}/usr/lib/libtk8.5.so
 tk84_DETECT=		${tk84${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 tk84_PORT=		${PORTSDIR}/x11-toolkits/linux${LINUX_DIST_SUFFIX}-tk84
 tk84_DEPENDS=		xorglibs
 
 xorglibs_FILE=		${LINUXBASE}/usr/X11R6/lib/libXrandr.so.2.0
 xorglibs_f8_FILE=	${LINUXBASE}/usr/lib/libXrandr.so.2.1.0
+xorglibs_f10_FILE=	${LINUXBASE}/usr/lib/libXrandr.so.2.1.0
 xorglibs_DETECT=	${xorglibs${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 xorglibs_PORT=		${PORTSDIR}/x11/linux${LINUX_DIST_SUFFIX}-xorg-libs
 xorglibs_DEPENDS=	fontconfig
 
 ucl_FILE=		${LINUXBASE}/usr/lib/libucl.so.1
 ucl_f8_FILE=		${ucl_FILE}
+ucl_f10_FILE=		${LINUXBASE}/usr/lib/libucl.so.1
 ucl_DETECT=		${ucl${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 ucl_PORT=		${PORTSDIR}/archivers/linux${LINUX_DIST_SUFFIX}-ucl
 
 ungif_FILE=			${LINUXBASE}/usr/lib/libgif.so.4.1.3
 ungif_f8_FILE=		${ungif_FILE}
+ungif_f10_FILE=		${LINUXBASE}/usr/lib/libgif.so.4.1.3
 ungif_DETECT=		${ungif${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 ungif_PORT=			${PORTSDIR}/graphics/linux${LINUX_DIST_SUFFIX}-ungif
 
 upx_FILE=			${LINUXBASE}/usr/bin/upx
 upx_f8_FILE=		${upx_FILE}
+upx_f10_FILE=			${LINUXBASE}/usr/bin/upx
 upx_DETECT=			${upx${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 upx_PORT=			${PORTSDIR}/archivers/linux${LINUX_DIST_SUFFIX}-upx
 upx_DEPENDS=		ucl
@@ -393,6 +453,7 @@ upx_DEPENDS=		ucl
 # use the same wabauth for now
 webauth_FILE=		${LOCALBASE}/bin/linux-nvu
 webauth_f8_FILE=	${webauth_FILE}
+webauth_f10_FILE=	${LOCALBASE}/bin/linux-nvu
 webauth_DETECT=		${webauth${LINUX_DIST_SUFFIX:S/-/_/}_FILE}
 webauth_PORT=		${PORTSDIR}/www/linux-${WEB_AUTH}
 webauth_DEPENDS=	gtk2 atk pango fontconfig
