455e076380b8f8f55b30f93c9b16cdc16992893a
miwi <miwi@FreeBSD.org>
2009-06-02 21:32:53 +0000

    - Update to 2.9
    PR:		ports/125139
    Submitted by:	Ashish SHUKLA <wahjava@gmail.com> (maintainer)

--- www/xpi-foxyproxy/files/patch-components_foxyproxy.js
+++ www/xpi-foxyproxy/files/patch-components_foxyproxy.js
@@ -3,7 +3,7 @@ $FreeBSD$
 
 --- components/foxyproxy.js.orig
 +++ components/foxyproxy.js
-@@ -430,15 +430,12 @@
+@@ -424,15 +424,13 @@
    },
  
    getDefaultPath : function() {
@@ -13,21 +13,14 @@ $FreeBSD$
 -    f.append("foxyproxy.xml");
 +    var file = CC["@mozilla.org/file/local;1"].createInstance(CI.nsILocalFile);
 +    var dir = CC["@mozilla.org/file/directory_service;1"].getService(CI.nsIProperties).get("ProfD", CI.nsILocalFile);
-     //dump("settings file: " + f.path + "\n");
--    return f;
--    //file.initWithPath(dir.path);
--    //file.appendRelativePath("foxyproxy.xml");
--    //return file;
+     // dump("settings file: " + f.path + "\n");
+     return f;
+-    // file.initWithPath(dir.path);
+-    // file.appendRelativePath("foxyproxy.xml");
+-    // return file;
 +    file.initWithPath(dir.path);
 +    file.appendRelativePath("foxyproxy.xml");
 +    return file;
    },
  
    // Convert |o| from:
-@@ -1471,4 +1468,4 @@
-   _xpComObjects: {},
-   _catObserverName: null,
-   _catContractId: null
--};
-\ No newline at end of file
-+};
