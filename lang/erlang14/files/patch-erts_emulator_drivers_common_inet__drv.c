d7145e63aacb7c8ef5164c156a2b8d3fab3a2dce
olgeni <olgeni@FreeBSD.org>
2009-06-03 13:47:15 +0000

    Upgrade to version R13B.
    Submitted by:	Kenji Rikitake <kenji.rikitake at acm.org> (with a few changes)

--- lang/erlang14/files/patch-erts_emulator_drivers_common_inet__drv.c
+++ lang/erlang14/files/patch-erts_emulator_drivers_common_inet__drv.c
@@ -3,38 +3,36 @@ $FreeBSD$
 
 --- erts/emulator/drivers/common/inet_drv.c.orig
 +++ erts/emulator/drivers/common/inet_drv.c
-@@ -3460,9 +3460,14 @@
-     /* Check the size of SCTP AssocID -- currently both this driver and the
-        Erlang part require 32 bit: */
-     ASSERT(sizeof(sctp_assoc_t)==ASSOC_ID_LEN);
--#   ifndef LIBSCTP
--#     error LIBSCTP not defined
--#   endif
-+#   if defined (__FreeBSD__)
-+    /* In FreeBSD, sctp_bindx belongs to libc - see sctp_bindx(3) */
-+    inet_init_sctp();
-+    add_driver_entry(&sctp_inet_driver_entry);
-+#   else
-+#     ifndef LIBSCTP
-+#       error LIBSCTP not defined
-+#     endif
-     if (erts_sys_ddll_open_noext(STRINGIFY(LIBSCTP), &h_libsctp) == 0) {
- 	void *ptr;
- 	if (erts_sys_ddll_sym(h_libsctp, "sctp_bindx", &ptr) == 0) {
-@@ -3471,6 +3476,7 @@
- 	    add_driver_entry(&sctp_inet_driver_entry);
- 	}
-     }
-+#   endif
- #  endif
- #endif /* _OSE_ */
-     /* remove the dummy inet driver */
-@@ -8997,7 +9003,7 @@
- 	    rflag = add_flag ? SCTP_BINDX_ADD_ADDR : SCTP_BINDX_REM_ADDR;
+@@ -5305,12 +5305,15 @@
+ 	    if (pmtud_enable)			cflags |= SPP_PMTUD_ENABLE;
+ 	    if (pmtud_disable)			cflags |= SPP_PMTUD_DISABLE;
  
- 	    /* Invoke the call: */
--	    if (p_sctp_bindx(desc->s, addrs, n, rflag) < 0)
-+	    if (sctp_bindx(desc->s, addrs, n, rflag) < 0)
- 		return ctl_error(sock_errno(), rbuf, rsize);
++#	    ifdef HAVE_STRUCT_SCTP_PADDRPARAMS_SPP_SACKDELAY
++	    /* The followings are missing in FreeBSD 7.1 */
+ 	    sackdelay_enable =eflags& SCTP_FLAG_SACDELAY_ENABLE;
+ 	    sackdelay_disable=eflags& SCTP_FLAG_SACDELAY_DISABLE;
+ 	    if (sackdelay_enable && sackdelay_disable)
+ 		return -1;
+ 	    if (sackdelay_enable)		cflags |= SPP_SACKDELAY_ENABLE;
+ 	    if (sackdelay_disable)		cflags |= SPP_SACKDELAY_DISABLE;
++#           endif
  
- 	    desc->state = INET_STATE_BOUND;
+ 	    arg.pap.spp_flags  = cflags;
+ #	    endif
+@@ -6211,13 +6214,15 @@
+ 	    
+ 	    if (ap.spp_flags & SPP_PMTUD_DISABLE)
+ 		{ i = LOAD_ATOM (spec, i, am_pmtud_disable);         n++; }
+-	    
++#	    ifdef HAVE_STRUCT_SCTP_PADDRPARAMS_SPP_SACKDELAY
++	    /* SPP_SACKDELAY_* not in FreeBSD 7.1 */
+ 	    if (ap.spp_flags & SPP_SACKDELAY_ENABLE)
+ 		{ i = LOAD_ATOM (spec, i, am_sackdelay_enable);      n++; }
+ 	    
+ 	    if (ap.spp_flags & SPP_SACKDELAY_DISABLE)
+ 		{ i = LOAD_ATOM (spec, i, am_sackdelay_disable);     n++; }
+ #	    endif
++#	    endif
+ 	    
+ 	    PLACE_FOR(spec, i,
+ 		      LOAD_NIL_CNT + LOAD_LIST_CNT + 2*LOAD_TUPLE_CNT);
