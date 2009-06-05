a6ac8133f8d86609f9376b454de25efcb8b40d0d
jpaetzel <jpaetzel@FreeBSD.org>
2009-06-05 09:48:26 +0000

    Update devel/popt to latest version
    Approved by:	linimon (mentor)

--- devel/popt/files/patch-popt.c
+++ devel/popt/files/patch-popt.c
@@ -1,6 +1,6 @@
---- ./popt.c.orig	Thu Aug 22 18:17:44 2002
-+++ ./popt.c	Wed Apr  4 12:25:14 2007
-@@ -14,6 +14,9 @@
+--- popt.c.old	2009-05-07 15:44:42.000000000 -0500
++++ popt.c	2009-05-07 15:47:12.000000000 -0500
+@@ -22,6 +22,9 @@
  #include <float.h>
  #endif
  #include <math.h>
@@ -8,14 +8,5 @@
 +#include <machine/float.h>
 +#endif
  
- #include "findme.h"
  #include "poptint.h"
-@@ -1153,7 +1156,7 @@
-     /*@=nullderef@*/
- }
  
--const char *const poptStrerror(const int error)
-+const char * poptStrerror(const int error)
- {
-     switch (error) {
-       case POPT_ERROR_NOARG:
