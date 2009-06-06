a8f951a208e00591352ff647c5f6a481f1be5459
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2009-06-06 16:01:46 +0000

    Fix build with custom LOCALBASE.
    Submitted by:	maintainer
    Reported by:	QATty

--- audio/py-fastaudio/files/patch-mysndfile.h
+++ audio/py-fastaudio/files/patch-mysndfile.h
@@ -1,9 +1,9 @@
---- mysndfile.h.orig	Tue Jun 15 13:37:39 2004
-+++ mysndfile.h	Sat Mar  3 11:53:57 2007
+--- mysndfile.h.orig	2009-06-06 18:51:14.000000000 +0900
++++ mysndfile.h	2009-06-06 18:51:38.000000000 +0900
 @@ -1,5 +1,5 @@
  #ifdef WIN32
  #include "sndfile-win32.h"
  #else
 -#include "sndfile.h"
-+#include "/usr/local/include/sndfile.h"
++#include "%%LOCALBASE%%/include/sndfile.h"
  #endif
