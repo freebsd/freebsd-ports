2e4c90528ae7dfbbd15f1be531bc6d10404502c5
maho <maho@FreeBSD.org>
2009-06-08 07:07:19 +0000

    Fix build on amd64 and now it's usable.

---
+++ science/gamess/files/patch-zunix.c
@@ -0,0 +1,10 @@
+--- source/zunix.c~	2008-10-31 23:05:41.000000000 +0900
++++ source/zunix.c	2009-06-08 15:37:42.000000000 +0900
+@@ -440,7 +440,6 @@
+ #ifdef LINUX64
+ 
+ #include <stdlib.h>
+-#include <malloc.h>
+ 
+ #define FORTINT long
+ 
