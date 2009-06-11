40023f8d761177432e13fc23338e62f14f0977c0
Li-Wen Hsu <lwhsu@FreeBSD.org>
2009-06-11 22:53:00 +0000

    - Fix build for customized PREFIX and LOCALBASE
    Submitted by:	QAT

--- databases/py-pytc/files/patch-setyp.py
+++ databases/py-pytc/files/patch-setyp.py
@@ -1,11 +1,13 @@
---- setup.py.orig	2009-02-18 08:58:48.199458174 +0800
-+++ setup.py	2009-02-18 08:58:56.569760265 +0800
-@@ -5,7 +5,7 @@
+--- setup.py.orig	2009-06-12 02:49:50.405623403 +0800
++++ setup.py	2009-06-12 02:50:50.873879838 +0800
+@@ -8,8 +8,8 @@
  if sys.version_info < (2, 3):
    raise Error, "Python 2.3 or later is required"
  
 -include_dirs = []
-+include_dirs = ['/usr/local/include']
- library_dirs = ['/usr/local/lib']
+-library_dirs = ['/usr/local/lib']
++include_dirs = ['%%LOCALBASE%%/include']
++library_dirs = ['%%LOCALBASE%%/lib']
  
- if sys.platform == 'darwin':
+ if sys.platform == 'linux2':
+   os.environ['PATH'] += ":/usr/local/bin:$home/bin:.:..:../.."
