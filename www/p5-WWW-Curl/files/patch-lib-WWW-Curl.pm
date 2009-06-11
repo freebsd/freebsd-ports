c8a148d3bfc2d0cb465d6b0de4b48f9146f4da8e
Peter Pentchev <roam@FreeBSD.org>
2009-06-11 09:28:15 +0000

    Update to WWW-Curl-4.07, refresh the patches and add descriptive headers on the patch files.

--- www/p5-WWW-Curl/files/patch-lib-WWW-Curl.pm
+++ www/p5-WWW-Curl/files/patch-lib-WWW-Curl.pm
@@ -1,11 +1,13 @@
---- lib/WWW/Curl.pm.orig	Thu Jul 13 15:04:35 2006
-+++ lib/WWW/Curl.pm	Thu Jul 13 15:04:18 2006
-@@ -35,7 +35,7 @@
- Version 3.02 adds some backwards compatibility for scripts still using
- 'WWW::Curl::easy' names.
+Fix a grammatical error.
+
+--- lib/WWW/Curl.pm.orig
++++ lib/WWW/Curl.pm
+@@ -306,7 +306,7 @@
+ 
+ Version 3.01 added some support for pre-multi versions of libcurl.
  
 -Version 3.00 adds WWW::Curl::Multi interface, and a new module names
 +Version 3.00 adds WWW::Curl::Multi interface, and new module names
  following perl conventions (WWW::Curl::Easy rather than WWW::Curl::easy),
- by Sebastian Riedel <sri@cpan.org>
+ by Sebastian Riedel <sri at cpan.org>.
  
