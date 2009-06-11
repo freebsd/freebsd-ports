ab818439fc13c7c0c959a39aeb9d244636b51416
Dmitry Marakasov <amdmi3@FreeBSD.org>
2009-06-11 14:46:44 +0000

    - Update to 0.4
    PR:		135472
    Submitted by:	Hiroto Kagotani <hiroto.kagotani@gmail.com> (maintainer)

--- graphics/iulib/files/patch-vidio__vidio.cc
+++ graphics/iulib/files/patch-vidio__vidio.cc
@@ -1,5 +1,5 @@
---- ./vidio/vidio.cc.orig	2008-10-16 03:50:34.000000000 +0900
-+++ ./vidio/vidio.cc	2009-05-22 09:19:51.000000000 +0900
+--- ./vidio/vidio.cc.orig	2009-06-10 07:32:20.000000000 +0900
++++ ./vidio/vidio.cc	2009-06-11 18:24:08.000000000 +0900
 @@ -27,7 +27,6 @@
  extern "C" {
  #include <assert.h>
@@ -8,12 +8,3 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <ctype.h>
-@@ -481,7 +480,7 @@
-             av_freep(&oc->streams[i]);
-         }
- 
--        url_fclose(&oc->pb);
-+        url_fclose(oc->pb);
- 
-         // free the stream
-         av_free(oc);
