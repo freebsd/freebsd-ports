2a00fc12fadcddaa21f1ba4a36ca39ac402c84f3
Josh Paetzel <jpaetzel@FreeBSD.org>
2009-06-12 16:46:43 +0000

    Update to 1.2.0 Add LOCALBASE support
    Approved by:	itetcu@ (mentor)

--- sysutils/vobcopy/files/patch-vobcopy.h
+++ sysutils/vobcopy/files/patch-vobcopy.h
@@ -1,29 +1,11 @@
---- vobcopy.h.orig	2008-02-19 06:27:04.000000000 +0100
-+++ vobcopy.h	2008-07-03 17:04:11.890928512 +0200
-@@ -58,7 +58,7 @@
+--- vobcopy.h.old	2009-06-12 10:46:16.000000000 +0000
++++ vobcopy.h	2009-06-12 10:47:20.000000000 +0000
+@@ -69,7 +69,7 @@
  /* //////////  *BSD //////////  */
  #if ( defined( BSD ) && ( BSD >= 199306 ) )
-
--#  if  !defined( __NetBSD__ ) ) || \
-+#  if  ! ( defined( __NetBSD__ ) ) || \
+ 
+-#if !defined( __NetBSD__ ) || \
++#if  ! ( defined( __NetBSD__ ) ) || \
         ( defined( __NetBSD__) && ( __NetBSD_Version__ < 200040000 ) )
  #include <sys/mount.h>
  #define USE_STATFS 1
-@@ -87,12 +87,14 @@
- #define GETMNTINFO_USES_STATVFS
-
- #    endif
-+#  endif
-
--#  else
--
-+#if defined(__FreeBSD__)
-+#define USE_STATFS_FOR_DEV
-+#include <sys/statvfs.h>
-+#else
- #include <sys/vfs.h>
--
--#  endif
-+#endif
-
- #define HAS_LARGEFILE 1
