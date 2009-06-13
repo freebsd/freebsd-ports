ad47e0bb889531d543664ae51a1c241272575606
Erwin Lansing <erwin@FreeBSD.org>
2009-06-13 20:00:48 +0000

    Remove science/elmerfront as it has been marked BROKEN over 3 months ago.

--- science/elmerfront/files/patch-include+ecif_mesh.h
+++
@@ -1,9 +0,0 @@
---- include/ecif_mesh.h.orig	Tue Feb  7 20:21:53 2006
-+++ include/ecif_mesh.h	Tue Feb  7 20:22:24 2006
-@@ -218,4 +218,4 @@
-   static int elementNodesBuffer[27];
- };
- 
--#endif
-\ No newline at end of file
-+#endif
