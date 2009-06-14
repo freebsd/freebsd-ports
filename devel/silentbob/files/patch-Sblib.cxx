841bbaa9eae6c8cd59de886ce28f9dd6fad23876
TAKATSU Tomonari <tota@FreeBSD.org>
2009-06-14 09:26:24 +0000

    - Update to 1.7.6 - Change MAINTAINER address from tota@rtfm.jp to tota@FreeBSD.org
    Approved by:	maho (mentor)

--- devel/silentbob/files/patch-Sblib.cxx
+++ devel/silentbob/files/patch-Sblib.cxx
@@ -1,10 +1,10 @@
---- sblib/Sblib.cxx.orig	2008-09-18 12:45:29.000000000 +0900
-+++ sblib/Sblib.cxx	2008-11-13 15:53:16.000000000 +0900
-@@ -346,7 +346,7 @@
+--- sblib/Sblib.cxx.orig	2009-04-04 17:46:58.000000000 +0900
++++ sblib/Sblib.cxx	2009-06-13 02:15:32.000000000 +0900
+@@ -363,7 +363,7 @@
  			lseek (fd, 0, SEEK_END);
  		dup2 (devnull, 2);
  		dup2 (fd, 1);
--		execlp ("find", path, "-name", name, NULL);
+-		execlp ("find", "find", "-name", name, NULL);
 +		execlp ("find", "find", path, "-name", name, NULL);
  	} else if (pid > 0) {
  		waitpid (pid, &status, 0);
