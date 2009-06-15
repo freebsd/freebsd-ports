afbf09cc33941f6e8015ea2a99665add0df3b03a
Martin Wilke <miwi@FreeBSD.org>
2009-06-15 18:01:41 +0000

    VirtualBox is a family of powerful x86 virtualization products for enterprise as well as home use. Not only is VirtualBox an extremely feature rich, high performance product for enterprise customers, it is also the only professional solution that is freely available as Open Source Software under the terms of the GNU General Public License.
    WWW:	http://www.virtualbox.org/
    
    Note:
    
    Please READ pkg-messages carefully, also please
    take a look in the Wiki page (http://wiki.freebsd.org/VirtualBox)
    
    Thanks to: thank you to everyone who helped make this possible.

---
+++ emulators/virtualbox/files/patch-src-VBox-Additions-common-VBoxGuest-VBoxGuest-freebsd.c
@@ -0,0 +1,14 @@
+--- src/VBox/Additions/common/VBoxGuest/VBoxGuest-freebsd.c.old	2009-05-04 20:29:23.000000000 +0200
++++ src/VBox/Additions/common/VBoxGuest/VBoxGuest-freebsd.c	2009-05-04 20:30:28.000000000 +0200
+@@ -228,7 +228,11 @@
+     if (rc)
+     {
+         *ppDev = make_dev(&g_VBoxGuestFreeBSDChrDevSW,
++#if __FreeBSD_version < 800062
+                           unit2minor(iUnit),
++#else
++			  iUnit,
++#endif
+                           UID_ROOT,
+                           GID_WHEEL,
+                           0644,
