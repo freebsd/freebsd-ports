4d14516c6d4fc9752a038eb29876c5ea6902cb37
garga <garga@FreeBSD.org>
2009-06-16 16:38:27 +0000

    - Update qmail-spamcontrol to 2525a

--- mail/qmail/files/extra-patch-spamcontrol-qmail-smtpd.c
+++ mail/qmail/files/extra-patch-spamcontrol-qmail-smtpd.c
@@ -1,6 +1,6 @@
---- qmail-smtpd.c.orig	2008-12-01 12:55:54.000000000 -0200
-+++ qmail-smtpd.c	2008-12-01 12:57:31.000000000 -0200
-@@ -136,6 +136,27 @@
+--- qmail-smtpd.c.orig	2009-06-16 13:17:52.000000000 -0300
++++ qmail-smtpd.c	2009-06-16 13:20:56.000000000 -0300
+@@ -137,6 +137,27 @@
    logs(s1,s2,s3,s4,s5,s6,s7);
    return;
    }
@@ -28,7 +28,7 @@
  void err_brcptto(s1,s2,s3,s4,s5,s6,s7) char *s1, *s2, *s3, *s4, *s5, *s6, *s7; {
    out("550 sorry, your envelope recipient is in my badrcptto list (#5.7.1)\r\n");
    logs(s1,s2,s3,s4,s5,s6,s7);
-@@ -280,6 +301,8 @@
+@@ -281,6 +302,8 @@
  
  char *auth;
  char *reqauth;
@@ -37,7 +37,7 @@
  int smtpauth = 0;
  int seenauth = 0;
  
-@@ -403,6 +426,9 @@
+@@ -404,6 +427,9 @@
    if (env_get("UCSPITLS")) starttls = 1;
    if (starttls) reqttls = env_get("REQUIRETLS");
  
@@ -47,14 +47,14 @@
  #ifdef DELIVERTO
    delivermailto = env_get("DELIVERTO");
  #endif
-@@ -891,6 +917,10 @@
-       flagerrcpts++;
+@@ -888,6 +914,10 @@
        return; 
-     } 
+     }
+ 
 +    if (!seenauth && rbl2smtpd && rblmatch && str_len(rblmatch) != 0) {
 +      err_rbl("Reject::RBL::RBL_Listed:",protocol.s,remoteip,remotehost,helohost.s,mailfrom.s,addr.s);
 +      return;
 +    }
      if (tarpitcount && flagerrcpts >= tarpitcount) { 	/* Tarpitting et al. */
+       if (tarpitdelay == 999) flagnotorious++;
        err_rcpts("Reject::RCPT::Toomany_Rcptto:",protocol.s,remoteip,remotehost,helohost.s,mailfrom.s,addr.s); 
-       return; 
