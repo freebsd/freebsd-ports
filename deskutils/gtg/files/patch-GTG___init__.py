fdef6b0ea81976c25b4de8374f3ffdc326947dc2
Dmitry Marakasov <amdmi3@FreeBSD.org>
2009-06-17 19:20:33 +0000

    - Fix localization - Bump portrevision - While here, make it respect PREFIX
    PR:		135661
    Submitted by:	"R.Mahmatkhanov" <cvs-src@yandex.ru> (maintainer)

--- deskutils/gtg/files/patch-GTG___init__.py
+++ deskutils/gtg/files/patch-GTG___init__.py
@@ -1,11 +1,20 @@
---- GTG/__init__.py.orig	2009-03-07 02:09:57.000000000 +0300
-+++ GTG/__init__.py	2009-03-16 01:19:21.000000000 +0300
-@@ -22,7 +22,7 @@
+--- GTG/__init__.py.orig	2009-04-04 01:00:16.000000000 +0400
++++ GTG/__init__.py	2009-06-17 12:59:18.000000000 +0400
+@@ -32,7 +32,7 @@
  EMAIL           = "gtg@lists.launchpad.net"
- VERSION         = '0.1'
+ VERSION         = '0.1.1'
  LOCAL_ROOTDIR   = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) 
 -DIST_ROOTDIR    = "/usr/share/gtg"
 +DIST_ROOTDIR    = "/usr/local/share/gtg"
  
- if not os.path.isdir( os.path.join(LOCAL_ROOTDIR,'data') ) :
-     DATA_DIR = DIST_ROOTDIR
+ # CREDITS
+ AUTHORS     = ["Main developers:", \
+@@ -60,7 +60,7 @@
+ GETTEXT_DOMAIN = 'gtg'
+ LOCALE_PATH = abspath(join(dirname(__file__), pardir, 'locales'))
+ if not os.path.isdir(LOCALE_PATH):
+-    LOCALE_PATH = '/usr/share/locale'
++    LOCALE_PATH = '/usr/local/share/locale'
+ languages_used = []
+ lc, encoding = locale.getdefaultlocale()
+ if lc:
