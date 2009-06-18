073f625500eef593c87765b92841fe6221d868cb
itetcu <itetcu@FreeBSD.org>
2009-06-18 15:51:47 +0000

    - update 2.1.1 - fix build with custom LOCALBASE [1]
    Submitted by:	maintainer
    Reported by:	QATty [1]

--- devel/py-tables/files/patch-setup.py
+++ devel/py-tables/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	Wed Jul 26 23:43:50 2006
-+++ setup.py	Wed Jul 26 23:44:46 2006
-@@ -72,8 +72,8 @@
+--- setup.py.orig	2009-06-18 17:57:21.000000000 +0800
++++ setup.py	2009-06-18 18:02:43.000000000 +0800
+@@ -81,8 +81,8 @@
  debug = '--debug' in sys.argv
  
  # Global variables
@@ -9,5 +9,18 @@
 +lib_dirs = ['%%LOCALBASE%%/lib']
 +inc_dirs = ['%%LOCALBASE%%/include']
  optional_libs = []
+ data_files = []    # list of data files to add to packages (mainly for DLL's)
  
- default_header_dirs = None
+@@ -91,10 +91,10 @@
+ default_runtime_dirs = None
+ 
+ if os.name == 'posix':
+-    default_header_dirs = ['/usr/include', '/usr/local/include']
++    default_header_dirs = ['/usr/include', '/usr/local/include','%%LOCALBASE%%/include']
+     default_library_dirs = [
+         os.path.join(_tree, _arch)
+-        for _tree in ('/', '/usr', '/usr/local')
++        for _tree in ('/', '/usr', '/usr/local','%%LOCALBASE%%')
+         for _arch in ('lib64', 'lib') ]
+     default_runtime_dirs = default_library_dirs
+ 
