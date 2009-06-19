449281ca030c19479c490c0e082be61007bd827b
wxs <wxs@FreeBSD.org>
2009-06-19 18:34:56 +0000

    - Update to 1.16.0
    PR:		ports/135698
    Submitted by:	Matthew D. Fuller <fullermd@over-yonder.net> (maintainer)

--- devel/bzrtools/files/patch-version.py
+++
@@ -1,6 +0,0 @@
---- version.py.orig	2009-04-10 16:11:51.000000000 -0500
-+++ version.py	2009-05-25 08:23:15.000000000 -0500
-@@ -1,2 +1,2 @@
- __version__ = '1.14.0'
--version_info = tuple(int(n) for n in __version__.split('.'))
-+version_info = tuple(int(n) for n in '1.15.0'.split('.'))
