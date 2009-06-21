4e69e474eab31f3851b15fe68b24e404c4c85f31
thierry <thierry@FreeBSD.org>
2009-06-21 17:46:55 +0000

    Upgrade to 10.0.3-2.

--- french/aster/files/patch-check_compilers.py
+++ french/aster/files/patch-check_compilers.py
@@ -1,15 +1,29 @@
---- check_compilers.py.orig	2008-12-23 18:01:02.000000000 +0100
-+++ check_compilers.py	2009-01-04 16:59:06.000000000 +0100
-@@ -394,7 +394,7 @@
-             args='-ffree-form -fdefault-integer-8', src=trivial_src['F90'])
-       self.fcheck(self.is_F77_is_gfortran, 'F77 (%s) is gfortran' % self.cfg.get('F77', '?'))
-       if True or not self.is_F77_is_gfortran: # on ne sait pas si les blas/lapack en ont besoin, le tester
--         self.libs.append(('math', 'g2c'))
-+         self.libs.append(('math', 'f2c'))
-          self.find_libs(lib=self.libs[-1])
+--- check_compilers.py.orig	2009-06-16 14:26:10.000000000 +0200
++++ check_compilers.py	2009-06-19 23:08:19.000000000 +0200
+@@ -268,9 +268,9 @@
+          self._print('---------- ERROR MESSAGE ----------', os.linesep, errmsg)
+          self._print(blas_lapack['__error__'])
+          rep = should_continue()
+-      # add to global (g2c/gfortran must be used by aster compiled with Intel)
++      # add to global (f2c/gfortran must be used by aster compiled with Intel)
+       for k, v in self.cfg.items():
+-         if    re.search('__LIBS_math_[0-9]+_g2c',      k) is not None \
++         if    re.search('__LIBS_math_[0-9]+_f2c',      k) is not None \
+             or re.search('__LIBS_math_[0-9]+_gfortran', k) is not None:
+             self.cfg_add_global[k] = v
+ 
+@@ -416,8 +416,8 @@
+       self.fcheck(self.fortran_supports_openmp, "F77 (%s) and F90 (%s) support '-fopenmp' option" \
+          % (self.cfg.get('F77', '?'), self.cfg.get('F90', '?')))
+       
+-      # les blas/lapack en ont peut-etre besoin, on rend libg2c facultative si F77=gfortran
+-      self.libs.append(('math', 'g2c', not self.is_F77_is_gfortran))
++      # les blas/lapack en ont peut-etre besoin, on rend libf2c facultative si F77=gfortran
++      self.libs.append(('math', 'f2c', not self.is_F77_is_gfortran))
+       self.find_libs(lib=self.libs[-1])
        if self.cfg.get('F90') or self.is_F77_is_gfortran:
           self.libs.append(('math', 'gfortran'))
-@@ -487,6 +487,16 @@
+@@ -518,6 +518,16 @@
           else:                      # x86_64
              intel_arch   = 'intel64'
              mkl_src_name = 'mklvarsem64t.sh'
