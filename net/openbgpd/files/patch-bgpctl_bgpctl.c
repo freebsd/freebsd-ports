b8ade4ca2a61b49600b827773bbf36efafae56ec
Hiroki Sato <hrs@FreeBSD.org>
2009-06-23 16:44:19 +0000

    Update to 4.4.1.  Take maintainership.

--- net/openbgpd/files/patch-bgpctl_bgpctl.c
+++ net/openbgpd/files/patch-bgpctl_bgpctl.c
@@ -1,5 +1,5 @@
---- bgpctl/bgpctl.c	2007-08-06 21:16:06.000000000 +0200
-+++ bgpctl/bgpctl.c	2008-03-18 14:27:24.000000000 +0100
+--- bgpctl/bgpctl.c.orig	2009-01-16 23:03:29.000000000 +0900
++++ bgpctl/bgpctl.c	2009-06-24 01:26:24.000000000 +0900
 @@ -20,7 +20,6 @@
  #include <sys/socket.h>
  #include <sys/un.h>
@@ -8,7 +8,7 @@
  #include <net/if_types.h>
  
  #include <err.h>
-@@ -29,14 +28,19 @@
+@@ -29,14 +28,20 @@
  #include <stdlib.h>
  #include <string.h>
  #include <unistd.h>
@@ -22,6 +22,7 @@
 +#include "util.h"
  #include "irrfilter.h"
 +#include "if_media.h"
++#include "compat-openbsd.h"
 +
 +#if defined(__FreeBSD__) /* FreeBSD has no LINK_STATE_IS_UP macro. */
 +#define LINK_STATE_IS_UP(_s)  ((_s) >= LINK_STATE_UP)
@@ -29,3 +30,21 @@
  
  enum neighbor_views {
  	NV_DEFAULT,
+@@ -84,7 +89,7 @@
+ 
+ struct imsgbuf	*ibuf;
+ 
+-__dead void
++ void
+ usage(void)
+ {
+ 	extern char	*__progname;
+@@ -677,7 +682,7 @@
+ }
+ 
+ void
+-print_timer(const char *name, timer_t d)
++print_timer(const char *name, time_t d)
+ {
+ 	printf("  %-20s ", name);
+ 
--- net/openbgpd/files/patch-bgpd_parse.y
+++ net/openbgpd/files/patch-bgpd_parse.y
@@ -1,19 +1,19 @@
---- bgpd/parse.y	2007-05-31 20:38:58.000000000 +0200
-+++ bgpd/parse.y	2008-03-18 13:27:29.000000000 +0100
+--- bgpd/parse.y.orig	2009-01-16 23:03:20.000000000 +0900
++++ bgpd/parse.y	2009-06-22 14:49:56.000000000 +0900
 @@ -523,11 +523,16 @@
  			free($4);
  		}
- 		| RTABLE number {
+ 		| RTABLE NUMBER {
 +#ifdef __FreeBSD__
-+ 			yyerror("rtable id not supported in FreeBSD, yet");
-+ 			YYERROR;
++			yyerror("rtable id not supported in FreeBSD, yet");
++			YYERROR;
 +#else
  			if ($2 > RT_TABLEID_MAX || $2 < 0) {
  				yyerror("invalid rtable id");
  				YYERROR;
  			}
  			conf->rtableid = $2;
-+#endif
++#endif /* __FreeBSD__ */
  		}
  		;
  
