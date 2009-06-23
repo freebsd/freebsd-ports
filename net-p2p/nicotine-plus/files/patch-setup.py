d33be93aef821aec5a00d307345530231da905c4
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2009-06-23 21:43:49 +0000

    - Update to 1.2.12
    PR:		135957
    Submitted by:	Alexander Best <alexbestms at math.uni-muenster.de>

--- net-p2p/nicotine-plus/files/patch-setup.py
+++ net-p2p/nicotine-plus/files/patch-setup.py
@@ -1,17 +1,20 @@
---- setup.py	Sat Mar 24 07:31:17 2007
-+++ setup.py	Sat Mar 24 07:33:11 2007
-@@ -27,12 +27,12 @@
- 		files.append((os.path.join(sys.prefix, "share", "nicotine", "sounds", theme), [os.path.join(sounds, file)]))
- doc_files = glob.glob(os.path.join("doc", "*"))
- for file in doc_files:
--	files.append((os.path.join(sys.prefix, "share", "nicotine", "documentation"), [file]))
-+	files.append((os.path.join(sys.prefix, "share", "doc", "nicotine"), [file]))
- files.append((os.path.join(sys.prefix, "share", "applications"), ["files/nicotine.desktop"]))
- # Manuals
- manpages = glob.glob(os.path.join("manpages", "*.1"))
- for man in manpages:
--	files.append((os.path.join(sys.prefix, "share", "man", "man1"), [man]))
-+	files.append((os.path.join(sys.prefix, "man", "man1"), [man]))
- files.append((os.path.join(sys.prefix, "share", "pixmaps"), ["files/nicotine-plus-32px.png"]))
- if sys.platform.startswith("win"):
-   try:
+--- setup.py	2009-05-22 14:59:23.000000000 -0500
++++ setup.py	2009-06-23 13:14:20.000000000 -0500
+@@ -84,7 +84,7 @@
+ 	# Manuals
+ 	manpages = glob.glob(os.path.join("manpages", "*.1"))
+ 	for man in manpages:
+-		files.append((os.path.join(sys.prefix, "share", "man", "man1"), [man]))
++		files.append((os.path.join(sys.prefix, "man", "man1"), [man]))
+ 	files.append((os.path.join(sys.prefix, "share", "pixmaps"), ["files/nicotine-plus-32px.png"]))
+ 	files.append((os.path.join(sys.prefix, "share", "applications"), ["files/nicotine.desktop"]))
+ 
+@@ -115,7 +115,7 @@
+ 	if is_windows:
+ 		files.append((os.path.join("share", "nicotine", "documentation"), [file]))
+ 	else:
+-		files.append((os.path.join(sys.prefix, "share", "nicotine", "documentation"), [file]))
++		files.append((os.path.join(sys.prefix, "share", "doc", "nicotine", "documentation"), [file]))
+ 
+ # Glade files (GUI)
+ #glade_files = glob.glob(os.path.join("pynicotine", "gtkgui", "*.glade"))
