9b89ef7a6ec6dfed14377e39c66096f927885724
acm <acm@FreeBSD.org>
2009-06-23 21:43:49 +0000

    - Update to 1.2.12
    PR:		135957
    Submitted by:	Alexander Best <alexbestms at math.uni-muenster.de>

--- net-p2p/nicotine-plus/files/patch-pynicotine-gtkgui_frame.py
+++ net-p2p/nicotine-plus/files/patch-pynicotine-gtkgui_frame.py
@@ -1,11 +1,25 @@
---- pynicotine/gtkgui/frame.py	Tue Jan 16 11:22:06 2007
-+++ pynicotine/gtkgui/frame.py	Tue Jan 16 11:23:53 2007
-@@ -1235,7 +1235,7 @@
- 			url = "file://%s/%s" % (os.environ["PWD"], file)
- 			self.OpenUrl(url)
+--- pynicotine/gtkgui/frame.py	2009-05-25 06:09:14.000000000 -0500
++++ pynicotine/gtkgui/frame.py	2009-06-23 13:46:11.000000000 -0500
+@@ -2605,18 +2605,18 @@
+ 			paths.append(path1split[0])
  		else:
--			file = "%s/share/nicotine/documentation/NicotinePlusGuide.html" % sys.prefix
-+			file = "%s/share/doc/nicotine/NicotinePlusGuide.html" % sys.prefix
- 			if os.path.exists(file):
- 				url = "file://%s" % file
+ 			paths.append(path1)
+-		path2 = "%s/share/nicotine/documentation" % sys.prefix
++		path2 = "%s/share/doc/nicotine" % sys.prefix
+ 		paths.append(path2)
+ 		winpath = "C:\Program Files\Nicotine+" 
+ 		paths.append(winpath)
+ 		for path in paths:
+-			if os.path.exists(os.sep.join([path, "doc", file])):
+-				url = "file:%s/%s/%s" % (urllib.pathname2url(path).replace("|", ":") ,"doc", file)
++			if os.path.exists(os.sep.join([path, "documentation", file])):
++				url = "file:%s/%s/%s" % (urllib.pathname2url(path).replace("|", ":") ,"documentation", file)
+ 
  				self.OpenUrl(url)
+ 				return
+ 		else:
+-			popupWarning(None, _("Cannot Find Guide"), _("The Nicotine Offline Guide ( NicotinePlusGuide.html ) was not found in either the following directories:\n\n<u>%(pwd)s\n</u><b>and</b>\n<u>%(prefix)s/share/nicotine/documentation/</u>\n\nEither install Nicotine-Plus, or start from inside the Nicotine-Plus source directory." % {'pwd':path1, 'prefix':sys.prefix } ) )
++			popupWarning(None, _("Cannot Find Guide"), _("The Nicotine Offline Guide ( NicotinePlusGuide.html ) was not found in either the following directories:\n\n<u>%(pwd)s\n</u><b>and</b>\n<u>%(prefix)s/share/doc/nicotine/documentation/</u>\n\nEither install Nicotine-Plus, or start from inside the Nicotine-Plus source directory." % {'pwd':path1, 'prefix':sys.prefix } ) )
+ 	
+ 	def OnOnlineNicotineGuide(self, widget):
+ 		url = "http://nicotine-plus.sourceforge.net/NicotinePlusGuide/"
