cb4c3f54bfdba190358085e23f55413a09d7bb22
wxs <wxs@FreeBSD.org>
2009-06-25 02:15:46 +0000

    - Update to 2.0.1
    PR:		ports/135996
    Submitted by:	Vladimir Korkodinov <viper@perm.raid.ru>
    Approved by:	maintainer

--- net/scapy/files/patch-scapy_config.py
+++ net/scapy/files/patch-scapy_config.py
@@ -1,10 +1,10 @@
---- scapy/config.py.orig	2008-11-12 05:01:38.000000000 +0800
-+++ scapy/config.py	2008-11-12 05:01:59.000000000 +0800
-@@ -308,7 +308,7 @@
+--- scapy/config.py.orig	2009-02-09 17:41:39.000000000 +0500
++++ scapy/config.py	2009-06-24 16:11:35.000000000 +0600
+@@ -309,7 +309,7 @@
      services_tcp = TCP_SERVICES
      services_udp = UDP_SERVICES
      extensions_paths = "."
--    manufdb = load_manuf("/usr/share/wireshark/wireshark/manuf")
+-    manufdb = load_manuf("/usr/share/wireshark/manuf")
 +    manufdb = load_manuf("%%LOCALBASE%%/share/wireshark/manuf")
      stats_classic_protocols = []
      stats_dot11_protocols = []
