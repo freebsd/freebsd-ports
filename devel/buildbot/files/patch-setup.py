c9983a5055a6f3a0093b7e1914fbf40c1a5903d0
Cheng-Lung Sung <clsung@FreeBSD.org>
2009-06-26 16:01:02 +0000

    - Update to 0.7.10p1

--- devel/buildbot/files/patch-setup.py
+++ devel/buildbot/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2009-02-19 00:08:42.135500043 +0800
-+++ setup.py	2009-02-19 00:08:50.857400452 +0800
-@@ -89,20 +89,6 @@
+--- setup.py.orig	2009-03-03 01:13:03.000000000 +0800
++++ setup.py	2009-06-26 23:28:27.000000000 +0800
+@@ -107,19 +107,6 @@
      'cmdclass': {'install_data': install_data_twisted},
      }
  
@@ -14,8 +14,7 @@
 -    setup_args['install_requires'] = ['twisted >= 2.0.0']
 -    entry_points={
 -        'console_scripts': [
--            'buildbot = buildbot.scripts.runner:run'
--            ]
+-            'buildbot = buildbot.scripts.runner:run'],
 -        },
 -
  setup(**setup_args)
