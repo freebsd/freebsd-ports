b916dcb48c66bb25672fbd72a19a392320ce2bb9
nork <nork@FreeBSD.org>
2009-06-28 15:31:52 +0000

    Update to 0.0.19.
    Approved by:	Henrik Friedrichsen  hrkfdn gmail com (maintainer)

---
+++ net-im/pidgin-msn-pecan/files/patch-io-pecan_ssl_conn.h
@@ -0,0 +1,10 @@
+--- io/pecan_ssl_conn.h.orig	2009-06-16 08:23:23.000000000 +0900
++++ io/pecan_ssl_conn.h	2009-06-28 01:25:04.495846164 +0900
+@@ -20,6 +20,7 @@
+ #define PECAN_SSL_CONN_H
+ 
+ #include <glib-object.h>
++#include <libpurple/sslconn.h>
+ 
+ #include "io/pecan_node.h"
+ 
