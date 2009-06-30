ecab794ccc338faa9d280fc2c065f7611a9e0221
Pav Lucistnik <pav@FreeBSD.org>
2009-06-30 13:49:05 +0000

    - Update to 1.7 - Switch maintainer to the submitter
    PR:		ports/135950
    Submitted by:	bf <bf1783@gmail.com>
    Approved by:	old maintainer

---
+++ devel/udis86/files/patch-udcli__udcli.c
@@ -0,0 +1,11 @@
+--- udcli/udcli.c.orig	2009-06-23 02:05:47.000000000 -0400
++++ udcli/udcli.c	2009-06-23 02:06:25.000000000 -0400
+@@ -46,7 +46,7 @@
+   "    -o <pc>  : Set the value of program counter to <pc>. (default = 0)\n"
+   "    -s <n>   : Set the number of bytes to skip before disassembly to <n>.\n"
+   "    -c <n>   : Set the number of bytes to disassemble to <n>.\n"
+-  "    -x       : Set the input mode to whitespace seperated 8-bit numbers in\n"
++  "    -x       : Set the input mode to whitespace-separated 8-bit numbers in\n"
+   "               hexadecimal representation. Example: 0f 01 ae 00\n"
+   "    -noff    : Do not display the offset of instructions.\n"
+   "    -nohex   : Do not display the hexadecimal code of instructions.\n"
