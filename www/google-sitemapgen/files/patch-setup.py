2d6eb1d051b81764807fc8cc4a7947165b141406
skv <skv@FreeBSD.org>
2009-07-05 08:41:33 +0000

    Update to 1.5
    PR:		ports/135076
    Submitted by:	John Marshall <john xx rwsrv05.mby.riverwillow.net.au>

--- www/google-sitemapgen/files/patch-setup.py
+++ www/google-sitemapgen/files/patch-setup.py
@@ -1,8 +1,8 @@
---- setup.py.orig	Thu May 26 23:53:25 2005
-+++ setup.py	Thu Jun  9 12:39:45 2005
+--- setup.py.orig	2009-05-27 17:43:25.000000000 +1000
++++ setup.py	2009-05-27 17:45:52.000000000 +1000
 @@ -9,4 +9,5 @@
        author='Google Inc.',
        author_email='opensource@google.com',
-       url='http://sourceforge.net/projects/goog-sitemap_gen/',
+       url='http://code.google.com/p/sitemap-generators/',
 +      py_modules = [ 'sitemap_gen' ]
       )
