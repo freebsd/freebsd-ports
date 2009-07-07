0ea5c2a8cab290bcd41af87b2b2cd115232725aa
miwi <miwi@FreeBSD.org>
2009-07-07 20:25:19 +0000

    - Update to 0.4.14
    PR:		136362
    Submitted by:	Eygene Ryabinkin <rea-fbsd@codelabs.ru> (maintainer)

--- net-mgmt/pnp/files/extra-patch-share::pnp::include::function.inc.php
+++ net-mgmt/pnp/files/extra-patch-share::pnp::include::function.inc.php
@@ -1,150 +1,18 @@
---- share/pnp/include/function.inc.php.orig	2008-10-11 18:13:34.000000000 +0400
-+++ share/pnp/include/function.inc.php	2008-12-24 01:03:58.000000000 +0300
-@@ -982,123 +982,6 @@
+--- share/pnp/include/function.inc.php.orig	2009-05-02 13:25:24.000000000 +0400
++++ share/pnp/include/function.inc.php	2009-07-02 19:05:07.000000000 +0400
+@@ -999,6 +999,7 @@
  }
  
  function doPDF($display,$data) {
--	require ('fpdi.php');
--	global $NAGIOS;
--	global $conf;
--        if(file_exists($conf['background_pdf'])){
--                $use_bg=1;
--        }
--	class PDF extends FPDI {
--		//Page header
--		function Header() {
--			//Arial bold 10 
--			$this->SetFont('Arial', 'B', 10);
--			//Move to the right
--			$this->Cell(80);
--			//Title
--			$this->Cell(30, 10, _PDFTITLE, 0, 1, 'C');
--			//Line break
--			#$this->Ln(20);
--		}
--
--		//Page footer
--		function Footer() {
--			//Position at 1.5 cm from bottom
--			$this->SetY(-20);
--			//Arial italic 8
--			$this->SetFont('Arial', 'I', 8);
--			//Page number
--			$this->Cell(0, 10, _PDFPAGE . $this->PageNo() . '/{nb}', 0, 0, 'C');
--		}
--	}
--
--	$pdf =& new PDF('P', 'mm', 'A4');
--	$pdf->AliasNbPages();
--        $pdf->SetAutoPageBreak('off');
--	$pdf->SetMargins(12.5,25,10);
--	$pdf->AddPage();
--	if($use_bg){
--		$pdf->setSourceFile($conf['background_pdf']);
--		$tplIdx = $pdf->importPage(1,'/MediaBox');
--		$pdf->useTemplate($tplIdx);
--	}
--	$pdf->SetCreator('Created with PNP');
--	$pdf->SetAuthor($_PHP['REMOTE_USER']);
--	$pdf->SetFont('Arial', '', 10);
--	$pdf->Cell(120, 4, '', 0, 1, 'L');
--	if($display == "service"){
--		foreach($data as $d) {
--			if ($pdf->GetY() > 220) {
--				$pdf->AddPage();
--				if($use_bg){$pdf->useTemplate($tplIdx);}	
--			}
--                        if($d['source'] == 1){
--                            $pdf->SetFont('Arial', '', 10);
--                            $pdf->CELL(120, 5, $d["n_hostname"]." / ".$d["n_servicedesc"] , 0, 1);
--                            $pdf->SetFont('Arial', '', 8);
--                            $pdf->CELL(120, 5, $d["view_title"]. " (" . $d["f_start"]." - ".$d["f_end"].")", 0, 1);
--                            $pdf->CELL(120, 5, _DATASOURCE ." ".$d["ds_name"], 0, 1);
--                        }else{
--                            $pdf->SetFont('Arial', '', 8);
--                            $pdf->CELL(120, 5, _DATASOURCE ." ".$d["ds_name"], 0, 1);
--                        }
--			$img = saveImage($d['rrd_opts']);
--                        $Y = $pdf->GetY();
--			$cell_height = ($img['height'] * 0.23);
--			$cell_width = ($img['width'] * 0.23);
--                        $pdf->Image($img['name'], 12.5, $Y, $cell_width, $cell_height, 'PNG');
--                        $pdf->CELL(120, $cell_height, '', 0, 1);
--			unlink($img['name']);
--		}
--	}
--	if($display == "page"){
--                foreach($data as $d) {
--			if ($pdf->GetY() > 220) {
--				$pdf->AddPage();
--				if($use_bg){$pdf->useTemplate($tplIdx);}
--			}
--
--			$pdf->SetFont('Arial', '', 10);
--			$pdf->CELL(120, 5, $d["hostname"]." / ".$d["servicedesc"] , 0, 1);
--			$pdf->SetFont('Arial', '', 8);
--			$pdf->CELL(120, 5, $d["f_start"]." - ".$d["f_end"], 0, 1);
--
--			$img = saveImage($d['rrd_opts']);
--                        $Y = $pdf->GetY();
--			$cell_height = ($img['height'] * 0.23);
--			$cell_width = ($img['width'] * 0.23);
--                        $pdf->Image($img['name'], 12.5, $Y, $cell_width, $cell_height, 'PNG');
--                        $pdf->CELL(120, $cell_height, '', 0, 1);
--			unlink($img['name']);
--               }
--        }
--        if($display == "host_list"){
--                foreach($data as $d) {
--                        if ($pdf->GetY() > 220) {
--                                $pdf->AddPage();
--				if($use_bg){$pdf->useTemplate($tplIdx);}
--                        }
--                        if($d['source'] == 1){
--                            $pdf->SetFont('Arial', '', 10);
--                            $pdf->CELL(120, 5, $d["n_hostname"]." / ".$d["n_servicedesc"] , 0, 1);
--                            $pdf->SetFont('Arial', '', 8);
--                            $pdf->CELL(120, 5, $d["view_title"]. " (" . $d["f_start"]." - ".$d["f_end"].")", 0, 1);
--                            $pdf->CELL(120, 5, _DATASOURCE ." ".$d["ds_name"], 0, 1);
--                        }else{
--                            $pdf->SetFont('Arial', '', 8);
--                            $pdf->CELL(120, 5, _DATASOURCE ." ".$d["ds_name"], 0, 1);
--                        }
--
--			$img = saveImage($d['rrd_opts']);
--                        $Y = $pdf->GetY();
--			$cell_height = ($img['height'] * 0.23);
--			$cell_width = ($img['width'] * 0.23);
--                        $pdf->Image($img['name'], 12.5, $Y, $cell_width, $cell_height, 'PNG');
--                        $pdf->CELL(120, $cell_height, '', 0, 1);
--			unlink($img['name']);
--                }
--        }
--	$pdf->Output("pnp4nagios.pdf","I");
- 	exit;
- }
- 
-@@ -1350,23 +1233,6 @@
++	exit;
+ 	require ('fpdi.php');
+ 	global $NAGIOS;
+ 	global $conf;
+@@ -1443,6 +1444,7 @@
  }
  
  function doPDFIcon($type){
--	global $hostname;
--	global $servicedesc;
--	global $start;
--	global $end;
--	global $source;
--	global $view;
--	global $display;
--	global $page;
--
--	switch($type){
--		case "PAGE":
--			print "<a href=\"index.php?page=$page&display=$display&view=$view&end=$end&start=$start&do=pdf\"><img src=\"images/pdf.png\" HEIGHT=\"32px\" WIDTH=\"32px\" title=\"Display PDF\" ></a>\n";
--			break;
--		default:
--			print "<a href=\"index.php?host=$hostname&srv=$servicedesc&display=$display&view=$view&source=$source&end=$end&start=$start&do=pdf\"><img src=\"images/pdf.png\" HEIGHT=\"32px\" WIDTH=\"32px\" title=\"Display PDF\" ></a>\n";
--			break;
--	}
- }
- 
- function doXMLIcon(){
++	return;
+ 	global $hostname;
+ 	global $servicedesc;
+ 	global $special;
