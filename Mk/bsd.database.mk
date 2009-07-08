25201318447df8b858d12085f591eca64b7a8888
girgen <girgen@FreeBSD.org>
2009-07-08 06:54:19 +0000

    Add postgresql 8.4 to the list of versions.

--- Mk/bsd.database.mk
+++ Mk/bsd.database.mk
@@ -173,6 +173,7 @@ PGSQL80_LIBVER=		4
 PGSQL81_LIBVER=		4
 PGSQL82_LIBVER=		5
 PGSQL83_LIBVER=		5
+PGSQL84_LIBVER=		5
 
 # Setting/finding PostgreSQL version we want.
 .if exists(${LOCALBASE}/bin/pg_config)
