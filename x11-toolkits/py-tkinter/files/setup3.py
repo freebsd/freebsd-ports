e5635ff7fd8859d27ca6be6004f9e7a1ddfff2df
lwhsu <lwhsu@FreeBSD.org>
2009-07-08 09:10:02 +0000

    - Fix building with python3* - Use checksum file under lang/python${PYTHON_SUFFIX}/,   for removal lang/python/distinfo
    PR:		ports/136198
    Submitted by:	lwhsu
    Approved by:	perky

---
+++ x11-toolkits/py-tkinter/files/setup3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# To use:
+#       python setup.py install
+#
+
+__version__ = "$FreeBSD$"
+
+import os, string
+
+try:
+    import distutils
+    from distutils import sysconfig
+    from distutils.command.install import install
+    from distutils.core import setup, Extension
+except:
+    raise SystemExit("Distutils problem")
+
+tkversion = "%%TK_VER%%"
+prefix = sysconfig.PREFIX
+# Python 1.5 doesn't have os.getenv()?
+x11base = os.environ['LOCALBASE'] or '/usr/X11R6'
+inc_dirs = [prefix + "/include",
+            prefix + "/include/tcl" + tkversion,
+            prefix + "/include/tk" + tkversion,
+            x11base + "/include"]
+lib_dirs = [prefix + "/lib", x11base + "/lib"]
+# use string.replace() for the benefit of Python 1.5 users
+libs = ["tcl" + tkversion.replace(".", ""),
+        "tk" + tkversion.replace(".", ""),
+        "X11"]
+
+setup(name = "Tkinter",
+      description = "Tk Extension to Python",
+      
+      ext_modules = [Extension('_tkinter', ['_tkinter.c', 'tkappinit.c'],
+                               define_macros=[('WITH_APPINIT', 1)],
+                               include_dirs = inc_dirs,
+                               libraries = libs,
+                               library_dirs = lib_dirs)]
+      )
