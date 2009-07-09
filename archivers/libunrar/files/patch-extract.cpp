63affdb4fe836489f252bb10925790283fe9e2bd
amdmi3 <amdmi3@FreeBSD.org>
2009-07-09 13:56:37 +0000

    - Update to 3.9.5
    PR:		136605
    Submitted by:	Alex Samorukov <samm@os2.kiev.ua> (maintainer)

--- archivers/libunrar/files/patch-extract.cpp
+++ archivers/libunrar/files/patch-extract.cpp
@@ -1,10 +1,10 @@
---- extract.cpp.orig	Wed Jun  7 08:52:35 2006
-+++ extract.cpp	Wed Jun  7 08:55:35 2006
-@@ -291,6 +291,7 @@
+--- extract.cpp.orig	2009-06-30 09:18:53.000000000 +0300
++++ extract.cpp	2009-07-09 10:02:21.000000000 +0300
+@@ -354,6 +354,7 @@
      char CurVolName[NM];
      strcpy(CurVolName,ArcName);
  
 +    if(strcmp(ArcName,"")==0) return(false);
-     VolNameToFirstName(ArcName,ArcName,(Arc.NewMhd.Flags & MHD_NEWNUMBERING));
+     VolNameToFirstName(ArcName,ArcName,(Arc.NewMhd.Flags & MHD_NEWNUMBERING)!=0);
      if (stricomp(ArcName,CurVolName)!=0 && FileExist(ArcName))
      {
