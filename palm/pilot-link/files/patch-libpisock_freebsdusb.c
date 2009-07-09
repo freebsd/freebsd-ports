a7050f4671ca6ed5b374a82dda4f4b4edc9f97b7
wxs <wxs@FreeBSD.org>
2009-07-09 03:12:35 +0000

    - Unbreak on -current.
    PR:		ports/135694
    Submitted by:	Ulrich Spoerlein <uqs@spoerlein.net>
    Approved by:	maintainer timeout

--- palm/pilot-link/files/patch-libpisock_freebsdusb.c
+++ palm/pilot-link/files/patch-libpisock_freebsdusb.c
@@ -1,15 +1,36 @@
---- libpisock/freebsdusb.c.orig	2006-10-12 06:21:22.000000000 -0800
-+++ libpisock/freebsdusb.c	2009-03-02 10:06:01.000000000 -0900
-@@ -48,7 +48,12 @@
+--- libpisock/freebsdusb.c.orig	2006-10-12 16:21:22.000000000 +0200
++++ libpisock/freebsdusb.c	2009-06-18 13:05:35.383212316 +0200
+@@ -48,7 +48,15 @@
  
  #if defined(__FreeBSD__)
  /* freebsd usb header */
 +#include <sys/param.h>
-+#if __FreeBSD_version+0 >= 800064
-+#include <legacy/dev/usb/usb.h>
-+#else
  #include <dev/usb/usb.h>
++#if __FreeBSD_version+0 >= 800092
++#include <dev/usb/usb_ioctl.h>
++#else
++/* redefine to legacy names, so this continues to build on FreeBSD 7.x */
++#define USB_SET_TX_TIMEOUT USB_SET_TIMEOUT
++#define USB_SET_RX_SHORT_XFER USB_SET_SHORT_XFER
 +#endif
  #define MAX_BUF 256
  #endif
  
+@@ -173,7 +181,7 @@
+ 	   will don't know exactly
+ 	what is coming so we can't specify exact byte amounts */
+ 	i = 1;
+-	if (ioctl(endpoint_fd, USB_SET_SHORT_XFER, &i) < 0) {
++	if (ioctl(endpoint_fd, USB_SET_RX_SHORT_XFER, &i) < 0) {
+ 		LOG((PI_DBG_DEV, PI_DBG_LVL_WARN,
+ 		 "DEV USB_SET_SHORT_XFER USB FreeBSD fd: %d failed\n",
+ 			endpoint_fd));
+@@ -182,7 +190,7 @@
+ 	/* 0 timeout value will cause us the wait until the device has data
+            available or is disconnected */
+ 	i = 0;
+-	if (ioctl(endpoint_fd, USB_SET_TIMEOUT, &i) < 0) {
++	if (ioctl(endpoint_fd, USB_SET_TX_TIMEOUT, &i) < 0) {
+ 		LOG((PI_DBG_DEV, PI_DBG_LVL_WARN,
+ 		 "DEV USB_SET_TIMEOUT USB FreeBSD fd: %d failed\n",
+ 			endpoint_fd));
