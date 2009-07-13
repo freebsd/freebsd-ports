bee0aa8a2bc7b17c4010f859802392ca431c4587
lwhsu <lwhsu@FreeBSD.org>
2009-07-13 05:59:54 +0000

    - Update to to 0.99D - Respect PORTEXAMPLES, NOPORTEXAMPLES - portlint(1)
    PR:		ports/136462
    Submitted by:	Kouki Hashimoto <hsmtkk AT gmail.com> (based on)

--- math/py-gato/files/patch-Gato.py
+++ math/py-gato/files/patch-Gato.py
@@ -1,46 +1,12 @@
---- Gato.py	Fri Jan 20 15:15:55 2006
-+++ Gato.py	Wed Mar  8 15:23:21 2006
+--- Gato.py	Wed Jul 08 22:14:29 2009 +0900
++++ Gato.py	Wed Jul 08 22:16:07 2009 +0900
 @@ -1,4 +1,4 @@
 -#!/usr/bin/env python2.3
 +#!/usr/bin/env python
  ################################################################################
  #
  #       This file is part of Gato (Graph Animation Toolbox) 
-@@ -48,22 +48,22 @@
- import tkFont
- import copy
- 
--import Gred
-+from Gato import Gred
- 
- from Tkinter import *
- from tkFileDialog import askopenfilename, asksaveasfilename
- from tkMessageBox import askokcancel, showerror, askyesno
- from ScrolledText import ScrolledText
--from GatoConfiguration import GatoConfiguration
--from Graph import Graph
--from GraphUtil import *
--from GraphDisplay import GraphDisplayToplevel
--from GatoUtil import *
--from GatoGlobals import *
--from GatoDialogs import AboutBox, SplashScreen, HTMLViewer
--import GatoIcons
--import GatoSystemConfiguration
--from AnimationHistory import AnimationHistory
-+from Gato.GatoConfiguration import GatoConfiguration
-+from Gato.Graph import Graph
-+from Gato.GraphUtil import *
-+from Gato.GraphDisplay import GraphDisplayToplevel
-+from Gato.GatoUtil import *
-+from Gato.GatoGlobals import *
-+from Gato.GatoDialogs import AboutBox, SplashScreen, HTMLViewer
-+from Gato import GatoIcons
-+from Gato import GatoSystemConfiguration
-+from Gato.AnimationHistory import AnimationHistory
- 
- # put someplace else
- def WMExtrasGeometry(window):
-@@ -1419,11 +1419,11 @@
+@@ -1590,11 +1590,11 @@
              self.algoGlobals['A'] = self.GUI.graphDisplay
              # XXX
              # explictely loading packages we want to make available to the algorithm
