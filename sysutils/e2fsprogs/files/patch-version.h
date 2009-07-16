194fc80cd2300ec6947eeb80a9d27935a791108f
Martin Wilke <miwi@FreeBSD.org>
2009-07-16 21:41:38 +0000

    - Update to 1.41.8
    PR:		136795
    Submitted by:	Matthias Andree <matthias.andree@gmx.de> (maintainer)

---
+++ sysutils/e2fsprogs/files/patch-version.h
@@ -0,0 +1,8 @@
+--- ./version.h~	2009-07-11 23:24:37.000000000 +0200
++++ ./version.h	2009-07-15 16:47:11.000000000 +0200
+@@ -8,4 +8,4 @@
+  */
+ 
+ #define E2FSPROGS_VERSION "1.41.8"
+-#define E2FSPROGS_DATE "11-July-2009"
++#define E2FSPROGS_DATE "11-Jul-2009"
