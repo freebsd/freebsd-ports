b9c7133d1d3ac567f93607cedcd9e0528d4025a3
Dmitry Marakasov <amdmi3@FreeBSD.org>
2009-07-16 14:36:33 +0000

    - Update to 2.3.6 - While here, clean up pkg-plist a bit
    PR:		136724
    Submitted by:	Mark Starovoytov <mark_sf@kikg.ifmo.ru> (maintainer)

--- mail/dbmail23/files/patch-imap4.c
+++ mail/dbmail23/files/patch-imap4.c
@@ -1,11 +1,11 @@
---- src/imap4.c.orig	Tue Jan  6 14:51:55 2009
-+++ src/imap4.c	Sun Jan 25 18:35:09 2009
-@@ -172,7 +172,7 @@
+--- src/imap4.c.orig	Sun Jun 28 17:48:24 2009
++++ src/imap4.c	Mon Jul 13 21:25:51 2009
+@@ -204,7 +204,7 @@
  	if (strlen(banner) > 0)
  		imap_session_printf(session, "* OK %s\r\n", banner);
  	else
 -		imap_session_printf(session, "* OK imap 4r1 server (dbmail %s)\r\n", VERSION);
 +		imap_session_printf(session, "* OK imap 4r1 server (dbmail %s)\r\n", DBMAIL_VERSION);
- 	dbmail_imap_session_set_state(session,IMAPCS_NON_AUTHENTICATED);
+ 	dbmail_imap_session_set_state(session,CLIENTSTATE_NON_AUTHENTICATED);
  }
  
