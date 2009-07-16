6dc4e42629ac796bfe7bd353d05c2055329b8a14
Pietro Cerutti <gahr@FreeBSD.org>
2009-07-16 15:10:58 +0000

    - Fix hang while picking schema in query browser [1] - Add patch for automatic column width in query browser [2]
    PR:		[1] 132352
    Submitted by:	[1] Henry Hu <henry.hu.sh@gmail.com>
    		[2] Joerg Wunsch <j@uriah.heep.sax.de> (via private e-mail)
    Patch from:	[1] http://bugs.mysql.com/bug.php?id=30055
    		[2] http://bugs.mysql.com/bug.php?id=11461

---
+++ databases/mysql-gui-tools/files/patch-query-browser-source-linux_MQQueryDispatcher.cc
@@ -0,0 +1,12 @@
+--- query-browser/source/linux/MQQueryDispatcher.cc.orig	2009-07-16 16:30:10.000000000 +0200
++++ query-browser/source/linux/MQQueryDispatcher.cc	2009-07-16 16:33:04.000000000 +0200
+@@ -558,9 +558,6 @@
+ 
+   Gtk::Main::instance()->run();
+ 
+-  while(!req->is_complete())
+-    ;
+-
+   return sps;
+ }
+ 
