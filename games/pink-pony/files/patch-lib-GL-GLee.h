e4e77645b5f0a41e3c862ac590e152a4583c5471
Dmitry Marakasov <amdmi3@FreeBSD.org>
2009-07-17 23:07:39 +0000

    Pink Pony is a Tron-like multiplayer racing game. You control little ponies that leave a trail of flowers everywhere they step. You have to evade these trails and force other ponies into them. The last pony standing wins the game.
    WWW: http://code.google.com/p/pink-pony/

---
+++ games/pink-pony/files/patch-lib-GL-GLee.h
@@ -0,0 +1,11 @@
+--- lib/GL/GLee.h.orig	2009-04-12 22:06:20.000000000 +0400
++++ lib/GL/GLee.h	2009-07-09 23:58:05.000000000 +0400
+@@ -747,8 +747,6 @@
+ 
+ 	/* GLX */
+ 
+-	typedef void (*__GLXextFuncPtr)(void);
+-
+ 	#ifndef GLX_ARB_get_proc_address 
+ 	#define GLX_ARB_get_proc_address 1
+ 	extern __GLXextFuncPtr glXGetProcAddressARB (const GLubyte *);
