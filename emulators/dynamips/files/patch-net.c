26174263bb431bd643ec89b0875ecfea19dd4fe4
Norikatsu Shigemura <nork@FreeBSD.org>
2009-07-20 18:25:36 +0000

    o Fix -m 0001.0002.0003 (cisco format) option issue. o Fix not real world communications via gen_eth (FreeBSD's libpcap) issue. o Take MAINTAINERship to myself. o Bump PORTREVISION accordingly.
    Approved by:		fjoe (maintainer)

--- emulators/dynamips/files/patch-net.c
+++ emulators/dynamips/files/patch-net.c
@@ -1,6 +1,14 @@
---- net.c.orig	Wed Mar 21 11:34:17 2007
-+++ net.c	Wed Mar 21 11:55:29 2007
-@@ -353,7 +353,7 @@
+--- net.c.orig	2007-05-26 18:52:33.000000000 +0900
++++ net.c	2009-07-21 03:16:45.891581213 +0900
+@@ -208,6 +208,7 @@
+       addr->eth_addr_byte[3] = v[1] & 0xFF;
+       addr->eth_addr_byte[4] = (v[2] >> 8) & 0xFF;
+       addr->eth_addr_byte[5] = v[2] & 0xFF;
++      return(0);
+    }
+ 
+    return(-1);
+@@ -353,7 +354,7 @@
  
  #if HAS_RFC2553
  /* Listen on the specified port */
@@ -9,7 +17,7 @@
  {
     struct addrinfo hints,*res,*res0;
     char port_str[20];
-@@ -370,7 +370,7 @@
+@@ -370,7 +371,7 @@
  
     snprintf(port_str,sizeof(port_str),"%d",port);
  
@@ -18,7 +26,7 @@
        fprintf(stderr,"ip_listen: %s", gai_strerror(error));
        return(-1);
     }
-@@ -405,7 +405,7 @@
+@@ -405,7 +406,7 @@
  }
  #else
  /* Listen on the specified port */
@@ -27,7 +35,7 @@
  {
     struct sockaddr_in sin;
     int i,sck,reuse=1;
-@@ -422,6 +422,8 @@
+@@ -422,6 +423,8 @@
     memset(&sin,0,sizeof(sin));
     sin.sin_family = PF_INET;
     sin.sin_port   = htons(port);
