3f9a1f3e98ab7b5d7b9b9b4259f789145212e598
Xin LI <delphij@FreeBSD.org>
2009-07-20 08:35:01 +0000

    Use "UTF-8" when "UTF8" is being used.
    Submitted by:	"O. Hartmann" <ohartman zedat fu-berlin de>
    PR:		ports/136856

---
+++ databases/libdbi-drivers/files/patch-drivers__pgsql__dbd_pgsql.c
@@ -0,0 +1,10 @@
+--- ./drivers/pgsql/dbd_pgsql.c.orig	2008-01-26 09:39:50.000000000 -0800
++++ ./drivers/pgsql/dbd_pgsql.c	2009-07-20 01:30:00.376376795 -0700
+@@ -75,6 +75,7 @@
+   "EUC_JP", "EUC-JP",
+   "EUC_KR", "EUC-KR",
+   "UNICODE", "UTF-8",
++  "UTF8", "UTF-8",
+   "LATIN1", "ISO-8859-1",
+   "LATIN2", "ISO-8859-2",
+   "LATIN3", "ISO-8859-3",
