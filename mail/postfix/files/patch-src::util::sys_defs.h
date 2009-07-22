a60cdb4c1342e5d2bdf55bfda04136f37acb60f9
keramida <keramida@FreeBSD.org>
2009-07-22 19:46:39 +0000

    Now that closefrom() has been merged to 7.X too, we have to patch sys_defs.h of Postfix to use closefrom() for __FreeBSD_version >= 702104 too.
    Submitted by:	Sahil Tandon, sahil at tandon.net (maintainer)
    Tested by:	wxs (7.X), keramida (head)

--- mail/postfix/files/patch-src::util::sys_defs.h
+++ mail/postfix/files/patch-src::util::sys_defs.h
@@ -1,15 +1,18 @@
---- src/util/sys_defs.h.orig	2009-07-08 13:38:52.000000000 +0400
-+++ src/util/sys_defs.h	2009-07-08 13:46:41.000000000 +0400
-@@ -110,6 +110,15 @@
+--- src/util/sys_defs.h.orig	2009-05-11 03:45:27.000000000 +0300
++++ src/util/sys_defs.h	2009-07-22 21:26:44.208909929 +0300
+@@ -25,6 +25,7 @@
+   */
+ #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
+     || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
++    || defined(FREEBSD8) \
+     || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
+     || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
+     || defined(NETBSD1) || defined(NETBSD2) || defined(NETBSD3) \
+@@ -110,6 +111,10 @@
  #define HAS_DUPLEX_PIPE			/* 4.1 breaks with kqueue(2) */
  #endif
  
-+#if __FreeBSD_version >= 800100		/*
-+					 * Actually, slightly before,
-+					 * but we can't say precisely
-+					 * when -- FreeBSD wasn't bumped
-+					 * for closefrom() addition
-+					 */
++#if __FreeBSD_version >= 702104 || __FreeBSD_version >= 800100
 +#define HAS_CLOSEFROM
 +#endif
 +
