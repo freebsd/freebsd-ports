7cfabea7f9a6510c55c2e2d9d6aee8c43ba11ffc
blackend <blackend@FreeBSD.org>
2009-07-26 20:13:38 +0000

    Unbreak for real the build with libusb support under 8.X.  Now one can use USB scanners under 8.X
    Submitted by:	Hans Petter Selasky <hselasky@c2i.net>
    Reviewed by:	rink
    Approved by:	miwi
    Tested by:	blackend (with some CanoScan LIDE scanners)

--- graphics/sane-backends/files/patch-sanei_sanei_usb.c
+++ graphics/sane-backends/files/patch-sanei_sanei_usb.c
@@ -1,14 +1,16 @@
---- sanei/sanei_usb.c.orig	2009-03-18 17:05:03.000000000 -0700
-+++ sanei/sanei_usb.c	2009-03-18 17:05:06.000000000 -0700
-@@ -88,6 +88,7 @@
+--- sanei/sanei_usb.c.orig	2009-07-26 21:29:33.000000000 +0200
++++ sanei/sanei_usb.c	2009-07-26 21:31:42.000000000 +0200
+@@ -96,7 +96,8 @@
+ }
  #endif /* HAVE_USBCALLS */
  
- #if defined (__FreeBSD__) 
+-#if defined (__FreeBSD__) 
++#if (defined (__FreeBSD__) && (__FreeBSD_version < 800064))
 +#include <sys/param.h>
  #include <dev/usb/usb.h>
  #endif /* __FreeBSD__ */
  #if defined (__DragonFly__)
-@@ -252,7 +253,7 @@
+@@ -273,7 +274,7 @@
      *vendorID = vendor;
      *productID = product;
    }
