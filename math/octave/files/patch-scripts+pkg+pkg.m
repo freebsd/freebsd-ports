a20304db7274af3a4b70ce4fd0691181f4450492
Maho Nakata <maho@FreeBSD.org>
2009-08-03 11:20:23 +0000

    Update to 3.2.2.

--- math/octave/files/patch-scripts+pkg+pkg.m
+++ math/octave/files/patch-scripts+pkg+pkg.m
@@ -1,11 +1,11 @@
---- scripts/pkg/pkg.m~	2009-04-09 17:26:54.000000000 +0900
-+++ scripts/pkg/pkg.m	2009-04-09 17:27:14.000000000 +0900
-@@ -1242,7 +1242,7 @@
-     ## make
+--- scripts/pkg/pkg.m~	2009-07-27 18:31:53.000000000 +0900
++++ scripts/pkg/pkg.m	2009-07-27 18:32:52.000000000 +0900
+@@ -1247,7 +1247,7 @@
+     ## Make.
      if (exist (fullfile (src, "Makefile"), "file"))
-       [status, output] = shell (strcat ("export INSTALLDIR=\"", desc.dir,
-- 					 "\"; make -C '", src, "'"));
-+ 					 "\"; gmake -C '", src, "'"));
+       [status, output] = shell (cstrcat ("export INSTALLDIR=\"", desc.dir,
+-					 "\"; make -C '", src, "'"));
++					 "\"; gmake -C '", src, "'"));
        if (status != 0)
  	rm_rf (desc.dir);
  	error ("'make' returned the following error: %s", output);
