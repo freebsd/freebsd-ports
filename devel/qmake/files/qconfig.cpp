20171fc57c4e8bd9325486cf010eb993156d21ca
miwi <miwi@FreeBSD.org>
2009-08-04 22:27:55 +0000

    - Fix INDEX (really sorry for this mistake :()

--- devel/qmake/files/qconfig.cpp
+++ devel/qmake/files/qconfig.cpp
@@ -4,16 +4,12 @@
  * The default prefix (/ usr / local) is dynamicly replaced
  * at configure time.
  */
-#define QT_CONFIGURE_LICENSEE "Open Source";
-#define QT_CONFIGURE_LICENSED_PRODUCTS "OpenSource";
-#define QT_CONFIGURE_PREFIX_PATH "/usr/local";
-#define QT_CONFIGURE_DOCUMENTATION_PATH "/usr/local/share/doc/qt4";
-#define QT_CONFIGURE_HEADERS_PATH "/usr/local/include/qt4";
-#define QT_CONFIGURE_LIBRARIES_PATH "/usr/local/lib/qt4";
-#define QT_CONFIGURE_BINARIES_PATH "/usr/local/bin";
-#define QT_CONFIGURE_PLUGINS_PATH "/usr/local/lib/qt4/plugins";
-#define QT_CONFIGURE_DATA_PATH "/usr/local/share/qt4";
-#define QT_CONFIGURE_TRANSLATIONS_PATH "/usr/local/share/qt4/translations";
-#define QT_CONFIGURE_SETTINGS_PATH "/usr/local/etc/xdg";
-#define QT_CONFIGURE_EXAMPLES_PATH "/usr/local/share/examples/qt4/examples";
-#define QT_CONFIGURE_DEMOS_PATH "/usr/local/share/examples/qt4/demos/";
+const char *qInstallPath()             { return "/usr/local"; }
+const char *qInstallPathDocs()         { return "/usr/local/share/doc/qt"; }
+const char *qInstallPathHeaders()      { return "/usr/local/include"; }
+const char *qInstallPathLibs()         { return "/usr/local/lib"; }
+const char *qInstallPathBins()         { return "/usr/local/bin"; }
+const char *qInstallPathPlugins()      { return "/usr/local/plugins"; }
+const char *qInstallPathData()         { return "/usr/local/share/qt"; }
+const char *qInstallPathTranslations() { return "/usr/local/translations"; }
+const char *qInstallPathSysconf()      { return "/usr/local/etc"; }
