3988212a47d8817d69d6a71fd069355fc562fd4b
db <db@FreeBSD.org>
2009-08-04 16:18:53 +0000

    - Patch submitted via e-mail by Emanuele Madeo (author of xwota) - Submitted to Emanuele by Lucian Langa cooly@gnome.eu.org - ... likely strcpy (eor, "<EOR>"); will overflow eor because of terminating   null byte.
    Submitted by:	e.madeo@fabaris.it

--- comms/xwota/files/patch-src_stuff.c
+++ comms/xwota/files/patch-src_stuff.c
@@ -1,5 +1,5 @@
---- src/stuff.c.orig	Wed Sep 13 15:23:19 2006
-+++ src/stuff.c	Wed Sep 13 15:24:18 2006
+--- src/stuff.c.orig	2006-07-28 12:00:10.000000000 -0400
++++ src/stuff.c	2009-08-04 12:03:56.000000000 -0400
 @@ -22,6 +22,7 @@
  #include "global.h"
  #include "stuff.h"
@@ -8,3 +8,12 @@
  
  
  int load_preferences()
+@@ -330,7 +331,7 @@
+ 	  GtkTreePath *path;
+ 	  GtkWidget *spot_list;
+ 	
+-char eor[5];
++char eor[6];
+ char *p_eor;	  
+ gchar *call,*freq,*country,*state,*grid,*county,*lat,*longitude,*status,*comments,*program,*f1,*f2,*d_ate,*t_ime; 
+ char datetime[50],latlong[50],status_comment[30],display_country[50];
