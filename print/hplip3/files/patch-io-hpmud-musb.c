870f23db75637a3604b3010e44ce7cb415876ffc
amdmi3 <amdmi3@FreeBSD.org>
2009-08-05 00:44:53 +0000

    - Update to 3.9.6b
    PR:		135702
    Submitteed by:	Chris Petrik <c.petrik.sosa@gmail.com>
    Thanks to:	"Juergen Lock" <nox@jelal.kn-bremen.de>

--- print/hplip3/files/patch-io-hpmud-musb.c
+++ print/hplip3/files/patch-io-hpmud-musb.c
@@ -1,12 +1,18 @@
---- io/hpmud/musb.c.orig	2008-01-23 01:27:15.000000000 +0100
-+++ io/hpmud/musb.c	2009-03-25 17:01:30.000000000 +0100
-@@ -123,8 +123,10 @@
+Index: io/hpmud/musb.c
+@@ -122,10 +122,16 @@
+    {
+       ret = usb_control_msg(dev, USB_ENDPOINT_IN, USB_REQ_GET_DESCRIPTOR, (USB_DT_STRING << 8) + index, 
                 0x409, tbuf, sizeof(tbuf), LIBUSB_CONTROL_REQ_TIMEOUT);
-       if (ret==0)
+-      if (ret==0)
++      if (ret==0
++#ifdef __FreeBSD__
++       || ret == -EIO
++#endif
++       )
        {
 -         /* This retry is necessary for lj1000 and lj1005. des 12/12/07 */
 -         BUG("get_string_descriptor zero result, retrying...");
-+	 /* This retry is necessary for lj1000 and lj1005. des 12/12/07
++         /* This retry is necessary for lj1000 and lj1005. des 12/12/07
 +         Also HP Photosmart 42xx seems to suffer transient errors with serial id */
 +         BUG("get_string_descriptor error result %d, retrying in 2 secs...", ret);
 +         sleep(2);
