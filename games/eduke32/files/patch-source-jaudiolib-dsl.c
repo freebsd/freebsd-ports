6b9f906236ef6af47a05e31e547d2c7491c949e2
Dmitry Marakasov <amdmi3@FreeBSD.org>
2009-08-07 17:50:46 +0000

    - Update to 20090313 - Unbreak fetch
    PR:		137466
    Submitted by:	Olivier Smedts <olivier@gid0.org>

---
+++ games/eduke32/files/patch-source-jaudiolib-dsl.c
@@ -0,0 +1,10 @@
+--- source/jaudiolib/dsl.c.orig	2009-02-02 23:13:40.000000000 +0100
++++ source/jaudiolib/dsl.c	2009-02-02 23:13:53.000000000 +0100
+@@ -22,6 +22,7 @@
+ */
+ #include <stdlib.h>
+ #include <string.h>
++#include <sys/types.h>
+ 
+ #include "dsl.h"
+ #include "compat.h"
