efdbcf3e6f273269383b76bd9d606cfa2fc7e74d
Joe Marcus Clarke <marcus@FreeBSD.org>
2009-08-08 18:53:12 +0000

    Update to 0.0.18.

--- net-im/farsight2/files/patch-transmitters_multicast_fs-multicast-transmitter.c
+++ net-im/farsight2/files/patch-transmitters_multicast_fs-multicast-transmitter.c
@@ -8,14 +8,3 @@
  # include <netinet/ip.h>
  # include <arpa/inet.h>
  #endif /*G_OS_WIN32*/
-@@ -644,8 +645,10 @@ _bind_port (
-     GST_WARNING ("could not set TOS: %s", g_strerror (errno));
- 
-   prio = 6;
-+#ifdef SO_PRIORITY
-   if (setsockopt (sock, SOL_SOCKET, SO_PRIORITY, &prio, sizeof (tos)) < 0)
-     GST_WARNING ( "could not set socket priority: %s", g_strerror (errno));
-+#endif
- 
-   address.sin_port = htons (port);
-   retval = bind (sock, (struct sockaddr *) &address, sizeof (address));
