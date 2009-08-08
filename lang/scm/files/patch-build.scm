0cdb385e5ce141fcf19eb24718e93fd4544a6232
miwi <miwi@FreeBSD.org>
2009-08-08 06:24:17 +0000

    - Update to 5e6
    PR:		137530
    Submitted by:	Ports Fury

--- lang/scm/files/patch-build.scm
+++ lang/scm/files/patch-build.scm
@@ -1,15 +1,23 @@
---- build.scm.orig	Mon Jan 10 00:23:55 2005
-+++ build.scm	Wed Jun 29 02:32:25 2005
-@@ -1576,7 +1576,7 @@
+--- build.scm.orig	2009-08-04 03:35:57.000000000 +0900
++++ build.scm	2009-08-07 01:59:30.000000000 +0900
+@@ -659,6 +659,7 @@
+      (curses freebsd "" "-lncurses" "/usr/lib/libncurses.a" () ())
+      (regex freebsd "-I/usr/include/gnu" "-lgnuregex" "" () ())
+      (editline freebsd "" "-lreadline" "" () ())
++     (graphics freebsd "-I%%LOCALBASE%%/include -DX11" "-L%%LOCALBASE%%/lib -lX 11" "%%LOCALBASE%%/lib/libX11.so" () ())
+      (dlll freebsd "-DSUN_DL" "-export-dynamic" "" () ())
+      (nostart freebsd "" "-e start -dc -dp -Bstatic -lgnumalloc" #f ("pre-crt0.c") ())
+      (dump freebsd "" "/usr/lib/crt0.o" "" ("unexsunos4.c") ())
+@@ -1619,7 +1620,7 @@
  	  parms
  ;;; gcc 3.4.2 for FreeBSD does not allow options other than default i.e. -O0 if NO -DGCC_SPARC_BUG - dai 2004-10-30
  	  ;;"cc" "-O3 -pipe -DGCC_SPARC_BUG " "-c"
 -	  "cc" "-O3 -pipe " "-c"
 +	  "%%CC%%" "%%CFLAGS%%" "-c"
+ 	  (include-spec "-I" parms)
  	  (c-includes parms)
  	  (c-flags parms)
- 	  files)
-@@ -1586,7 +1586,7 @@
+@@ -1630,7 +1631,7 @@
      (batch:rename-file parms
  		       oname (string-append oname "~"))
      (and (batch:try-command parms
@@ -18,24 +26,29 @@
  			    (must-be-first
  			     '("-nostartfiles"
  			       "pre-crt0.o" "crt0.o"
-@@ -1596,14 +1596,14 @@
+@@ -1640,17 +1641,18 @@
  (defcommand compile-dll-c-files freebsd
    (lambda (files parms)
      (and (batch:try-chopped-command
 -	  parms "cc" "-O3 -pipe " "-fPIC" "-c"
 +	  parms "%%CC%%" "%%CFLAGS%%" "-fPIC" "-c"
- 	  (c-includes parms) (c-flags parms) files)
+ 	  (include-spec "-I" parms)
+ 	  (c-includes parms)
+ 	  (c-flags parms)
+ 	  files)
  	 (let ((fnames (truncate-up-to (map c-> files) #\/)))
  	   (and (batch:try-command
- 		 parms "cc" "-shared"
+-		 parms "cc" "-shared"
++		 parms "%%CC%%" "-shared"
  		 (cond
  		  ((equal? (car fnames) "edline") "-lreadline")
 -		  ((equal? (car fnames) "x") "-L/usr/X11R6/lib -lSM -lICE -lXext -lX11 -lxpg4")
-+		  ((equal? (car fnames) "x") "-L%%LOCALBASE%%/lib -lSM -lICE -lXext -lX11 -lxpg4")
++		  ((equal? (car fnames) "rgx") "-lgnuregex")
++		  ((equal? (car fnames) "x") "-L%%LOCALBASE%%/lib -lSM -lICE -lXext -lX11")
  		  (else ""))
  		 "-o" (string-append (car fnames) ".so")
  		 (map (lambda (fname) (string-append fname ".o")) fnames))
-@@ -1616,7 +1616,7 @@
+@@ -1663,7 +1665,7 @@
    (lambda (oname objects libs parms)
      (and (batch:try-command
  	  parms
