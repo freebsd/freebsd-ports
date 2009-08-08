4f2336ba07d22e327f691336f6fc0c6cf7fd7f07
marcus <marcus@FreeBSD.org>
2009-08-08 18:53:12 +0000

    Update to 0.0.18.

--- net-im/farsight2/files/patch-transmitters_rawudp_fs-rawudp-transmitter.c
+++ net-im/farsight2/files/patch-transmitters_rawudp_fs-rawudp-transmitter.c
@@ -1,13 +1,10 @@
---- transmitters/rawudp/fs-rawudp-transmitter.c.orig	2009-08-01 16:45:45.000000000 -0400
-+++ transmitters/rawudp/fs-rawudp-transmitter.c	2009-08-01 16:46:50.000000000 -0400
-@@ -603,8 +603,10 @@ _bind_port (
-     GST_WARNING ( "could not set TOS: %s", g_strerror (errno));
- 
-   prio = 6;
-+#ifdef SO_PRIORITY
-   if (setsockopt (sock, SOL_SOCKET, SO_PRIORITY, &prio, sizeof (tos)) < 0)
-     GST_WARNING ( "could not set socket priority: %s", g_strerror (errno));
-+#endif
- 
-   return sock;
- }
+--- transmitters/rawudp/fs-rawudp-transmitter.c.orig	2009-08-03 16:20:07.000000000 -0400
++++ transmitters/rawudp/fs-rawudp-transmitter.c	2009-08-08 14:47:44.000000000 -0400
+@@ -53,6 +53,7 @@
+ #else /*G_OS_WIN32*/
+ # include <netdb.h>
+ # include <sys/socket.h>
++# include <netinet/in.h>
+ # include <netinet/ip.h>
+ # include <arpa/inet.h>
+ #endif /*G_OS_WIN32*/
