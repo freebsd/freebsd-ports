35507a56f279786c471790f763e439b255ddd6c6
Joe Marcus Clarke <marcus@FreeBSD.org>
2009-08-09 23:22:22 +0000

    Fix the build on 6.X.
    Reported by:	pointyhat via erwin

--- net-im/farsight2/files/patch-transmitters_multicast_fs-multicast-transmitter.c
+++ net-im/farsight2/files/patch-transmitters_multicast_fs-multicast-transmitter.c
@@ -1,10 +1,11 @@
---- transmitters/multicast/fs-multicast-transmitter.c.orig	2009-08-01 16:47:43.000000000 -0400
-+++ transmitters/multicast/fs-multicast-transmitter.c	2009-08-01 16:48:29.000000000 -0400
-@@ -53,6 +53,7 @@
+--- transmitters/multicast/fs-multicast-transmitter.c.orig	2009-08-03 16:20:07.000000000 -0400
++++ transmitters/multicast/fs-multicast-transmitter.c	2009-08-09 19:20:18.000000000 -0400
+@@ -53,6 +53,8 @@
  #else /*G_OS_WIN32*/
  # include <netdb.h>
  # include <sys/socket.h>
 +# include <netinet/in.h>
++# include <netinet/in_systm.h>
  # include <netinet/ip.h>
  # include <arpa/inet.h>
  #endif /*G_OS_WIN32*/
--- net-im/farsight2/files/patch-transmitters_rawudp_fs-rawudp-transmitter.c
+++ net-im/farsight2/files/patch-transmitters_rawudp_fs-rawudp-transmitter.c
@@ -1,10 +1,11 @@
 --- transmitters/rawudp/fs-rawudp-transmitter.c.orig	2009-08-03 16:20:07.000000000 -0400
-+++ transmitters/rawudp/fs-rawudp-transmitter.c	2009-08-08 14:47:44.000000000 -0400
-@@ -53,6 +53,7 @@
++++ transmitters/rawudp/fs-rawudp-transmitter.c	2009-08-09 19:19:33.000000000 -0400
+@@ -53,6 +53,8 @@
  #else /*G_OS_WIN32*/
  # include <netdb.h>
  # include <sys/socket.h>
 +# include <netinet/in.h>
++# include <netinet/in_systm.h>
  # include <netinet/ip.h>
  # include <arpa/inet.h>
  #endif /*G_OS_WIN32*/
