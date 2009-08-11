ff4e9ad292f545c519f445ab2ca228fe13c18d61
Philippe Audeoud <jadawin@FreeBSD.org>
2009-08-11 06:58:24 +0000

    - Fix previous commit.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -931,7 +931,7 @@ MASTER_SITE_PACKETSTORM+= \
 	http://packetstorm.offensive-security.com/%SUBDIR%/ \
 	http://packetstorm.igor.onlinedirect.bg/%SUBDIR%/ \
 	http://packetstorm.unixteacher.org/%SUBDIR%/ \
-	http://packetstorm.orion-hosting.co.uk/%SUBDIR%/ \
+	http://packetstorm.orion-hosting.co.uk/%SUBDIR%/
 .endif
 
 .if !defined(IGNORE_MASTER_SITE_PERL_CPAN)
