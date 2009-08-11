ace7191291501c89d3b18f32b7c4a35bc6145a4a
skreuzer <skreuzer@FreeBSD.org>
2009-08-11 18:26:43 +0000

    Update to version 0.5.1
    PR:		ports/137481
    Submitted by:	Tobias Rehbein <tobias.rehbein@web.de>

--- games/stonesoup/files/patch-AppHdr.h
+++ games/stonesoup/files/patch-AppHdr.h
@@ -1,6 +1,6 @@
---- ./AppHdr.h.orig	2009-06-12 12:32:16.000000000 +0200
-+++ ./AppHdr.h	2009-06-19 13:54:51.000000000 +0200
-@@ -124,7 +124,7 @@
+--- ./AppHdr.h.orig	2009-07-29 01:31:27.000000000 +0200
++++ ./AppHdr.h	2009-08-03 10:10:09.000000000 +0200
+@@ -126,7 +126,7 @@
      //          setuid or setgid. Filenames passed to this command *are not
      //          validated in any way*.
      //
@@ -9,14 +9,12 @@
  
      // For cases when the game will be played on terms that don't support the
      // curses "bold == lighter" 16 colour mode. -- bwr
-@@ -401,8 +401,8 @@
-     // PACKAGE_SUFFIX is used when the package file name is needed
-     //
-     // Comment these lines out if you want to leave the save files uncompressed.
--    #define SAVE_PACKAGE_CMD    "/usr/bin/zip -m -q -j -1 %s.zip %s.*"
--    #define LOAD_UNPACKAGE_CMD  "/usr/bin/unzip -q -o %s.zip -d %s"
-+    // #define SAVE_PACKAGE_CMD    "/usr/bin/zip -m -q -j -1 %s.zip %s.*"
-+    // #define LOAD_UNPACKAGE_CMD  "/usr/bin/unzip -q -o %s.zip -d %s"
- 
- #ifdef SAVE_PACKAGE_CMD
-     // This is used to unpack specific files from the archive.
+@@ -413,6 +413,8 @@
+     // The default behaviour is to compress with zip.
+     // To use GNU tar instead, define SAVE_PACKAGE_TAR.
+     // To avoid compression entirely, define SAVE_PACKAGE_NONE.
++    #define SAVE_PACKAGE_NONE
++
+     #ifndef SAVE_PACKAGE_NONE
+     #ifdef SAVE_PACKAGE_TAR
+       // The --absolute-names switch is only there to suppress noise on stdout.
