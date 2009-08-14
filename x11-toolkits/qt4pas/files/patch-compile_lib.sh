22c3e0e247594fba58b00d71be07357dc81619a3
makc <makc@FreeBSD.org>
2009-08-14 10:46:23 +0000

    Fix build by update to 1.70 for Qt-4.5.0.
    Approved by:	portmgr (miwi)

--- x11-toolkits/qt4pas/files/patch-compile_lib.sh
+++ x11-toolkits/qt4pas/files/patch-compile_lib.sh
@@ -1,10 +1,16 @@
---- compile_lib.sh	2009-01-21 20:18:56.000000000 -0500
-+++ compile_lib.sh	2009-01-21 20:20:21.000000000 -0500
+--- ./compile_lib.sh.orig	2009-04-20 17:59:59.000000000 +0400
++++ ./compile_lib.sh	2009-07-12 15:18:58.000000000 +0400
+@@ -1,4 +1,4 @@
+-#!/bin/env bash
++#!/bin/env sh
+ # get latest from http://users.telenet.be/Jan.Van.hijfte/qtforfpc/fpcqt4.html
+ # for more info, you can visit the links on above page to FreePascal and Lazarus wiki
+ 
 @@ -7,18 +7,16 @@
  # do configure and gmake in that Qt4 dir
  
  NAME=libqt4intf.so
--QTDIR=../qt-x11-opensource-src-4.4.3
+-QTDIR=../qt-all-opensource-src-4.5.0
 -INCLUDE_PATH="-I. -I$QTDIR/include -I$QTDIR/include/Qt -I$QTDIR/include/QtGui -I$QTDIR/include/QtCore -Iqlcl "
 -LIB_PATH=$QTDIR/lib
 -export LD_LIBRARY_PATH=$LIB_PATH
@@ -15,10 +21,10 @@
  if [ -e "$LIB_PATH/libQtCore.so.4" ]
  then
    echo please wait for compile to finish ...
--  g++ -D BINUX $INCLUDE_PATH qtpas.cpp -o libqt4intf.so -shared -fPIC -lQtCore -lQtGui  -Xlinker -soname=$NAME -Xlinker --library-path -Xlinker $LIB_PATH
+-  g++ -D BINUX $INCLUDE_PATH qtpas.cpp -o libqt4intf.so -shared -fPIC -lQtCore -lQtGui -lQtNetwork -lQtWebKit  -Xlinker -soname=$NAME -Xlinker --library-path -Xlinker $LIB_PATH
 -  echo Showing used Qt libraries when LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 -  ldd $NAME | grep libQt
-+  %%CXX%% -D BINUX $INCLUDE_PATH qtpas.cpp -o libqt4intf.so -shared -fPIC -lQtCore -lQtGui  -Xlinker -soname=$NAME -Xlinker --library-path -Xlinker $LIB_PATH
++  %%CXX%% -D BINUX $INCLUDE_PATH qtpas.cpp -o libqt4intf.so -shared -fPIC -lQtCore -lQtGui -lQtNetwork -lQtWebKit  -Xlinker -soname=$NAME -Xlinker --library-path -Xlinker $LIB_PATH
    echo stripping library
 -  strip --strip-all $NAME
 +  %%STRIP_CMD%% --strip-all $NAME
