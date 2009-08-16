7114bd2326b956dc6503e8f26c1a3a2244160408
Stanislav Sedov <stas@FreeBSD.org>
2009-08-16 20:37:41 +0000

    - Fix on 7.x.
    Reported by:	pav

--- misc/ftdi-eeprom/files/patch-ftdi-eeprom.c
+++ misc/ftdi-eeprom/files/patch-ftdi-eeprom.c
@@ -1,10 +1,20 @@
---- ftdi-eeprom.c.orig	2009-08-09 11:21:23.000000000 +0400
-+++ ftdi-eeprom.c	2009-08-09 11:21:37.000000000 +0400
-@@ -44,6 +44,7 @@
+--- ftdi-eeprom.c.orig	2003-02-16 07:02:56.000000000 +0300
++++ ftdi-eeprom.c	2009-08-17 00:38:28.000000000 +0400
+@@ -34,6 +34,7 @@
+  */
+ 
+ #include <sys/ioctl.h>
++#include <sys/param.h>
+ #include <sys/types.h>
+ #include <errno.h>
+ #include <fcntl.h>
+@@ -44,6 +45,9 @@
  #include <string.h>
  #include <limits.h>
  #include <dev/usb/usb.h>
++#if __FreeBSD_version >= 800069
 +#include <dev/usb/usb_ioctl.h>
++#endif
  
  #include "ftdi-eeprom.h"
  
