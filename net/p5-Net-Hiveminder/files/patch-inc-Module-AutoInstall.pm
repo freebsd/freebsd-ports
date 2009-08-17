bf6138e6cca403f7bd24596ff4a90c94005a0131
clsung <clsung@FreeBSD.org>
2009-08-17 07:54:42 +0000

    - Update to 0.08
    Changes:	http://search.cpan.org/dist/Net-Hiveminder/Changes

--- net/p5-Net-Hiveminder/files/patch-inc-Module-AutoInstall.pm
+++ net/p5-Net-Hiveminder/files/patch-inc-Module-AutoInstall.pm
@@ -1,11 +1,11 @@
---- inc/Module/AutoInstall.pm.orig	2009-04-20 15:31:17.000000000 +0800
-+++ inc/Module/AutoInstall.pm	2009-04-22 16:08:41.000000000 +0800
-@@ -115,7 +115,7 @@
-         )[0]
-     );
+--- inc/Module/AutoInstall.pm.orig	2009-07-09 16:51:30.000000000 +0800
++++ inc/Module/AutoInstall.pm	2009-08-15 13:40:38.000000000 +0800
+@@ -125,7 +125,7 @@
+     # check entirely since we don't want to have to load (and configure)
+     # an old CPAN just for a cosmetic message
  
--    $UnderCPAN = _check_lock(1);    # check for $UnderCPAN
-+    #$UnderCPAN = _check_lock(1);    # check for $UnderCPAN
+-    $UnderCPAN = _check_lock(1) unless $SkipInstall;
++    #$UnderCPAN = _check_lock(1) unless $SkipInstall;
  
      while ( my ( $feature, $modules ) = splice( @args, 0, 2 ) ) {
          my ( @required, @tests, @skiptests );
