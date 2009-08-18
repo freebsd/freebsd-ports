d7d19c8326cb1f176a5184fa8da9b351fcd59c6a
Martin Wilke <miwi@FreeBSD.org>
2009-08-18 20:48:24 +0000

    - Update to 3.2.8.1
    PR:		137923
    Submitted by:	Gerrit Beine <gerrit.beine@gmx.de> (maintainer)

--- irc/unreal/files/patch-modules.c
+++ irc/unreal/files/patch-modules.c
@@ -1,6 +1,6 @@
---- src/modules.c	Wed Oct 27 20:45:27 2004
-+++ src/modules.c	Mon Feb  7 04:01:47 2005
-@@ -90,12 +90,12 @@
+--- src/modules.c	2009-04-13 13:03:58.000000000 +0200
++++ src/modules.c	2009-08-18 16:29:34.000000000 +0200
+@@ -177,12 +177,12 @@
  {
  	char tempbuf[PATH_MAX+1];
  #ifndef _WIN32
@@ -15,7 +15,7 @@
  			strerror(errno));
  		if (!loop.ircd_booted)
  			exit(7);
-@@ -106,7 +106,7 @@
+@@ -193,19 +193,19 @@
  	{
  		if (!strcmp(dir->d_name, ".") || !strcmp(dir->d_name, ".."))
  			continue;
@@ -24,7 +24,12 @@
  		strcat(tempbuf, dir->d_name);
  		remove(tempbuf);
  	}
-@@ -118,7 +118,7 @@
+ 	closedir(fd);
+ #else
+ 	WIN32_FIND_DATA hData;
+-	HANDLE hFile = FindFirstFile("tmp/*", &hData);
++	HANDLE hFile = FindFirstFile("%%RUNDIR%%/tmp/*", &hData);
+ 	if (hFile != INVALID_HANDLE_VALUE)
  	{
  		if (strcmp(hData.cFileName, ".") || strcmp(hData.cFileName, ".."))
  		{
@@ -33,7 +38,7 @@
  			strcat(tempbuf, hData.cFileName);
  			remove(tempbuf);
  		}
-@@ -127,7 +127,7 @@
+@@ -214,7 +214,7 @@
  	{
  		if (!strcmp(hData.cFileName, ".") || !strcmp(hData.cFileName, ".."))
  			continue;
@@ -42,12 +47,3 @@
  		strcat(tempbuf, hData.cFileName);
  		remove(tempbuf);
  	}
-@@ -200,7 +200,7 @@
- 	path = path_;
- 
- 	
--	tmppath = unreal_mktemp("tmp", unreal_getfilename(path));
-+	tmppath = unreal_mktemp("%%RUNDIR%%/tmp", unreal_getfilename(path));
- 	if (!tmppath)
- 		return "Unable to create temporary file!";
- 	if(!strchr(path, '/'))
