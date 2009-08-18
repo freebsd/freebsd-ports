5110cc841e5f5dfdf4748aaa3ba394b3be43cbe1
miwi <miwi@FreeBSD.org>
2009-08-18 20:48:24 +0000

    - Update to 3.2.8.1
    PR:		137923
    Submitted by:	Gerrit Beine <gerrit.beine@gmx.de> (maintainer)

--- irc/unreal/files/patch-config.h
+++ irc/unreal/files/patch-config.h
@@ -1,6 +1,6 @@
---- include/config.h	Sun Mar 13 22:02:43 2005
-+++ include/config.h	Mon Mar 21 09:10:41 2005
-@@ -201,16 +201,16 @@
+--- include/config.h	2009-04-13 13:03:57.000000000 +0200
++++ include/config.h	2009-08-18 16:16:39.000000000 +0200
+@@ -210,16 +210,16 @@
   * these are only the recommened names and paths. Change as needed.
   * You must define these to something, even if you don't really want them.
   */
@@ -16,9 +16,9 @@
 -#define IRCDTUNE 	"ircd.tune"	/* tuning .. */
 +#define	CPATH		"%%PREFIX%%/etc/Unreal/unrealircd.conf"	/* server configuration file */
 +#define	MPATH		"%%PREFIX%%/etc/Unreal/ircd.motd"	/* server MOTD file */
-+#define SMPATH          "%%PREFIX%%/etc/Unreal/ircd.smotd"    /* short MOTD file */
-+#define RPATH   	"%%PREFIX%%/etc/Unreal/ircd.rules"	/* server rules file */
-+#define OPATH   	"%%PREFIX%%/etc/Unreal/oper.motd"	/* Operators MOTD file */
++#define SMPATH		"%%PREFIX%%/etc/Unreal/ircd.smotd"	/* short MOTD file */
++#define RPATH		"%%PREFIX%%/etc/Unreal/ircd.rules"	/* server rules file */
++#define OPATH		"%%PREFIX%%/etc/Unreal/oper.motd"	/* Operators MOTD file */
 +#define	LPATH		"%%LOGDIR%%/debug.log"	/* Where the debug file lives, if DEBUGMODE */
 +#define	PPATH		"%%RUNDIR%%/ircd.pid"	/* file for server pid */
 +#define VPATH		"%%PREFIX%%/etc/Unreal/ircd.svsmotd"	/* Services MOTD append. */
