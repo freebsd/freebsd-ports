e9db7734cfb1eb44398ef0011d4cdbb6d20b356c
marcus <marcus@FreeBSD.org>
2009-08-25 19:19:07 +0000

    Fix the build with the new fribidi.
    Submitted by:	cperciva

---
+++ graphics/kdegraphics3/files/patch-ksvg_impl_libs_libtext2path_src_Converter.cpp
@@ -0,0 +1,11 @@
+--- ksvg/impl/libs/libtext2path/src/Converter.cpp.orig	2009-08-24 22:00:18.000000000 -0700
++++ ksvg/impl/libs/libtext2path/src/Converter.cpp	2009-08-24 21:40:14.000000000 -0700
+@@ -23,7 +23,7 @@
+ #include "myboost/shared_ptr.hpp"
+ #include <fontconfig/fontconfig.h>
+ #include <fribidi/fribidi.h>
+-#include <fribidi/fribidi_types.h>
++#include <fribidi/fribidi-types.h>
+ 
+ #include "Font.h"
+ #include "Glyph.h"
