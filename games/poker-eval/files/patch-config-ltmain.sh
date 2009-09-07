50f183f66dc45faf407ded359559a8f02cce7cb2
ijliao <ijliao@FreeBSD.org>
2009-09-07 17:04:40 +0000

    upgrade to 0.136

--- games/poker-eval/files/patch-config-ltmain.sh
+++ games/poker-eval/files/patch-config-ltmain.sh
@@ -1,14 +1,14 @@
---- config/ltmain.sh.orig	Thu Dec 14 17:26:54 2006
-+++ config/ltmain.sh	Thu Dec 14 17:27:08 2006
-@@ -6003,10 +6003,12 @@
+--- config/ltmain.sh.orig	2009-09-08 00:56:30.722444585 +0800
++++ config/ltmain.sh	2009-09-08 00:57:14.609940717 +0800
+@@ -2077,10 +2077,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
 +	if /usr/bin/false ; then
- 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	func_basename "$file"
+ 	name="$func_basename_result"
  	instname="$dir/$name"i
- 	$show "$install_prog $instname $destdir/$name"
- 	$run eval "$install_prog $instname $destdir/$name" || exit $?
+ 	func_show_eval "$install_prog $instname $destdir/$name" 'exit $?'
 +	fi
  
  	# Maybe install the static library, too.
