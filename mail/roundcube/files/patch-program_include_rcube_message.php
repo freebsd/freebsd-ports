d19d532496c5079bd1dcc6bee12f48126b1c4102
ale <ale@FreeBSD.org>
2009-09-07 20:57:06 +0000

    Update to 0.3 release: - add plugins support - add a fix for encrypted sessions

--- mail/roundcube/files/patch-program_include_rcube_message.php
+++ mail/roundcube/files/patch-program_include_rcube_message.php
@@ -1,11 +1,11 @@
---- ./program/include/rcube_message.php.orig	2009-07-30 14:35:17.000000000 +0200
-+++ ./program/include/rcube_message.php	2009-07-30 14:35:35.000000000 +0200
-@@ -373,7 +373,7 @@
+--- program/include/rcube_message.php.orig	2009-08-27 10:08:27.000000000 +0200
++++ program/include/rcube_message.php	2009-09-07 17:47:43.000000000 +0200
+@@ -384,7 +384,7 @@
  
-         // part is file/attachment
-         else if ($mail_part->disposition == 'attachment' || $mail_part->disposition == 'inline' ||
+         // part is a file/attachment
+         else if (preg_match('/^(inline|attach)/', $mail_part->disposition) ||
 -                 $mail_part->headers['content-id'] || (empty($mail_part->disposition) && $mail_part->filename)) {
 +                 $mail_part->headers['content-id'] || $mail_part->filename) {
+ 
            // skip apple resource forks
            if ($message_ctype_secondary == 'appledouble' && $secondary_type == 'applefile')
-             continue;
