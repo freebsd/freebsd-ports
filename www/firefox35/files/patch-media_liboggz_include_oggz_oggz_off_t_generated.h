63d5af0e602740a0cf21d108437a3dabfa0e3c92
miwi <miwi@FreeBSD.org>
2009-09-10 18:04:21 +0000

    - Update to 3.5.3
    Security:	http://www.vuxml.org/freebsd/922d2398-9e2d-11de-a998-0030843d3802.html

--- www/firefox35/files/patch-media_liboggz_include_oggz_oggz_off_t_generated.h
+++ www/firefox35/files/patch-media_liboggz_include_oggz_oggz_off_t_generated.h
@@ -1,11 +1,11 @@
---- media/liboggz/include/oggz/oggz_off_t_generated.h.orig	2009-04-24 01:46:48.000000000 +0000
-+++ media/liboggz/include/oggz/oggz_off_t_generated.h	2009-04-28 08:44:02.369576627 +0000
+--- media/liboggz/include/oggz/oggz_off_t_generated.h.orig	2009-08-24 17:52:50.000000000 +0200
++++ media/liboggz/include/oggz/oggz_off_t_generated.h	2009-09-10 15:26:59.000000000 +0200
 @@ -59,7 +59,7 @@
  
  #include <sys/types.h>
  
--#if defined(__APPLE__) || defined(SOLARIS)
-+#if defined(__APPLE__) || defined(SOLARIS) || defined (__FreeBSD__)
+-#if defined(__APPLE__) || defined(SOLARIS) || defined(OS2)
++#if defined(__APPLE__) || defined(SOLARIS) || defined(OS2) || defined(__FreeBSD__)
  typedef off_t oggz_off_t;
  #else
  typedef loff_t oggz_off_t;
