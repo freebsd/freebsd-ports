6c7829a221f84cad3e023a49b3b2ea2058d10669
makc <makc@FreeBSD.org>
2009-09-10 09:09:03 +0000

    Add new port net-p2p/eiskaltdc:
    EiskaltDC++ is a fork of Valknut client for the Direct Connect
    network. It is compatible with other DC clients, such as the original
    DC from Neomodus, DC++ and derivatives. EiskaltDC++ also interoperates
    with all common DC hub software.
    
    WWW:	http://eiskaltdc.sourceforge.net
    
    PR:		based on ports/138436
    Submitted by:	Stas Timokhin <stast at bsdportal.ru>

---
+++ net-p2p/eiskaltdc/files/patch-dcfilebrowser.cpp
@@ -0,0 +1,11 @@
+--- eiskaltdc/dcfilebrowser.cpp.orig	2009-08-28 10:26:38.000000000 +0700
++++ eiskaltdc/dcfilebrowser.cpp	2009-08-28 10:26:57.000000000 +0700
+@@ -37,7 +37,7 @@
+ 
+ #include <set>
+ 
+-#include <malloc.h>
++#include <stdlib.h>
+ 
+ #include "dcmenuhandler.h"
+ #include "dcconfig.h"
