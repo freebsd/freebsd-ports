996b995a4fb1b32993d5944d2149635205b487c0
Daniel Eischen <deischen@FreeBSD.org>
2009-09-11 14:07:52 +0000

    Unbreak the build for 9-current.

---
+++ net-mgmt/net-snmp/files/patch-freebsd9.h
@@ -0,0 +1,5 @@
+--- include/net-snmp/system/freebsd9.h.orig	2009-09-11 10:02:17.000000000 -0400
++++ include/net-snmp/system/freebsd9.h	2009-09-11 10:02:17.000000000 -0400
+@@ -0,0 +1,2 @@
++#include "freebsd7.h"
++#define freebsd7 freebsd7
