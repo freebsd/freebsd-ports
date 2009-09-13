020cc4fe024e90ab718ed12232182175b3a9b2e9
Gerald Pfeifer <gerald@FreeBSD.org>
2009-09-13 18:40:40 +0000

    Remove editors/bed.  It has been marked IGNORE for more than two years and also still depends on lang/gcc34, failing to work with any reasonably recent version of GCC.  Plus, it lacks a maintainer.

--- editors/bed/files/patch-plugins__examples__index2table.h
+++
@@ -1,8 +0,0 @@
---- plugins/examples/index2table.h.orig	Thu May 23 16:28:24 2002
-+++ plugins/examples/index2table.h	Tue Jan  2 00:15:22 2007
-@@ -52,3 +52,5 @@
- 		return sym;\
- 		}\
- 	char *nam##name= initfilter(#nam,&get##nam);
-+
-+typedef Filter * startindex2table_func_t(int fb,int sb,char *opts,ScreenPart *part=NULL);
