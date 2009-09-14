d33794819c987cee227f8d58595175c1230f21d2
Dirk Meyer <dinoex@FreeBSD.org>
2009-09-14 10:28:33 +0000

    - update to 3.9.1

--- graphics/tiff/files/patch-tiff2pdf.c
+++ graphics/tiff/files/patch-tiff2pdf.c
@@ -1,13 +1,11 @@
-CVE-2006-2193
-===================================================================
---- tools/tiff2pdf.c.orig	2006-06-04 18:26:40.000000000 -0700
-+++ tools/tiff2pdf.c	2006-06-04 18:27:22.000000000 -0700
-@@ -3668,7 +3668,7 @@
- 	written += TIFFWriteFile(output, (tdata_t) "(", 1);
- 	for (i=0;i<len;i++){
+--- tools/tiff2pdf.c.orig	2009-01-01 01:10:43.000000000 +0100
++++ tools/tiff2pdf.c	2009-09-14 12:13:39.000000000 +0200
+@@ -3726,7 +3726,7 @@
+ 	written += t2pWriteFile(output, (tdata_t) "(", 1);
+ 	for (i=0; i<len; i++) {
  		if((pdfstr[i]&0x80) || (pdfstr[i]==127) || (pdfstr[i]<32)){
--			sprintf(buffer, "\\%.3o", pdfstr[i]);
-+			snprintf(buffer, sizeof(buffer), "\\%.3o", pdfstr[i]);
- 			written += TIFFWriteFile(output, (tdata_t) buffer, 4);
+-			sprintf(buffer, "\\%.3hho", pdfstr[i]);
++			snprintf(buffer, sizeof(buffer), "\\%.3hho", pdfstr[i]);
+ 			buffer[sizeof(buffer) - 1] = '\0';
+ 			written += t2pWriteFile(output, (tdata_t) buffer, 4);
  		} else {
- 			switch (pdfstr[i]){
