da0907606807548d521541615fb798c624dd8012
Dag-Erling Smørgrav <des@FreeBSD.org>
2009-09-14 16:37:25 +0000

    Fix build on 9.
    Approved by:	maintainer
    Reviewed by:	wxs@, keramida@

--- mail/postfix28/files/patch-src::util::sys_defs.h
+++ mail/postfix28/files/patch-src::util::sys_defs.h
@@ -4,15 +4,16 @@
    */
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
-+    || defined(FREEBSD8) \
++    || defined(FREEBSD8) || defined(FREEBSD9) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(NETBSD1) || defined(NETBSD2) || defined(NETBSD3) \
-@@ -110,6 +111,10 @@
+@@ -110,6 +111,11 @@
  #define HAS_DUPLEX_PIPE			/* 4.1 breaks with kqueue(2) */
  #endif
  
-+#if __FreeBSD_version >= 702104 || __FreeBSD_version >= 800100
++#if (__FreeBSD_version >= 702104 && __FreeBSD_version <= 800000) \
++    || __FreeBSD_version >= 800100
 +#define HAS_CLOSEFROM
 +#endif
 +
