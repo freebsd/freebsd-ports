87ee28693e2fb6ecbb98d21239fa820e011b77db
Martin Wilke <miwi@FreeBSD.org>
2009-09-15 21:41:21 +0000

    Fix build if POSIX semaphore enabled is
    Reported by:	many on ports@/python@
    Patched by:	beat@
    
    Feature safe: yes

--- lang/python27/files/extra-patch-setup.py
+++ lang/python27/files/extra-patch-setup.py
@@ -4,7 +4,7 @@
                  )
              libraries = []
  
--        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
+-        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8', 'freebsd9'):
 +        elif platform in ('freebsd4', 'freebsd5', 'freebsd6'):
              # FreeBSD's P1003.1b semaphore support is very experimental
              # and has many known problems. (as of June 2008)
