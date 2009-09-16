a6ffc9b07563c90b8d04053945558ca6079707f7
kuriyama <kuriyama@FreeBSD.org>
2009-09-16 06:23:56 +0000

    - Upgrade to 0.11.5.ja1. - Remove my hack about notification.local_timezone flag, and use   recently added trac.default_timezone setting. - Rewrite my patch about "prefer iso-2022-jp" encoding in notification   mail.
    Feature safe:	yes

--- japanese/trac/files/patch-notification.py
+++ japanese/trac/files/patch-notification.py
@@ -1,6 +1,6 @@
---- trac/notification.py.orig	2008-08-17 14:28:38.000000000 +0900
-+++ trac/notification.py	2008-08-24 18:30:45.000000000 +0900
-@@ -248,6 +248,7 @@
+--- trac/notification.py.orig	2009-09-16 14:26:41.787493029 +0900
++++ trac/notification.py	2009-09-16 14:25:56.397348835 +0900
+@@ -243,6 +243,7 @@
      def format_header(self, key, name, email=None):
          from email.Header import Header
          maxlength = MAXHEADERLEN-(len(key)+2)
@@ -8,36 +8,55 @@
          # Do not sent ridiculous short headers
          if maxlength < 10:
              raise TracError(_("Header length is too short"))
-@@ -255,6 +256,13 @@
+@@ -250,7 +251,14 @@
              tmp = name.encode('ascii')
              header = Header(tmp, 'ascii', maxlinelen=maxlength)
          except UnicodeEncodeError:
-+          if self.env.config.get('notification', 'mime_encoding').lower() == 'none':
-+            try:
-+              tmp = name.encode('iso-2022-jp')
-+              header = Header(tmp, 'iso-2022-jp', maxlinelen=maxlength)
-+            except UnicodeEncodeError:
-+              pass
-+          if not header:
-             header = Header(name, self._charset, maxlinelen=maxlength)
+-            header = Header(name, self._charset, maxlinelen=maxlength)
++            if self.env.config.get('notification', 'mime_encoding').lower() == 'none':
++                try:
++                    tmp = name.encode('iso-2022-jp')
++                    header = Header(tmp, 'iso-2022-jp', maxlinelen=maxlength)
++                except UnicodeEncodeError:
++                    pass
++            if not header:
++                header = Header(name, self._charset, maxlinelen=maxlength)
          if not email:
              return header
-@@ -386,12 +394,17 @@
-                 headers['To'] = ', '.join(toaddrs)
+         else:
+@@ -383,6 +391,35 @@
          if pcc:
              headers['Cc'] = ', '.join(pcc)
--        headers['Date'] = formatdate()
-+        headers['Date'] = formatdate(time.time(), self.config.getbool('notification', 'local_timezone'))
-         # sanity check
-         if not self._charset.body_encoding:
-             try:
-                 dummy = body.encode('ascii')
-             except UnicodeDecodeError:
-+             try:
-+              dummy = unicode(body, 'utf8')
-+              body = dummy.encode('iso-2022-jp')
-+              self._charset.output_charset = 'iso-2022-jp'
-+             except UnicodeDecodeError:
-                 raise TracError(_("Ticket contains non-ASCII chars. " \
-                                   "Please change encoding setting"))
+         headers['Date'] = formatdate()
++        # Use specified timezone in Date: header.
++        tz = self.config.get('trac', 'default_timezone')
++        if tz:
++            import os
++            preserve_tz = 0
++            old_tz = ''
++            if os.environ.has_key('TZ'):
++                preserve_tz = 1
++                old_tz = os.environ['TZ']
++            os.environ['TZ'] = tz
++            time.tzset()
++            headers['Date'] = formatdate(time.time(), True)
++            if preserve_tz:
++                os.environ['TZ'] = old_tz
++            else:
++                del os.environ['TZ']
++            time.tzset()
++        if not self._charset.body_encoding:
++            try:
++                dummy = body.encode('ascii')
++            except UnicodeDecodeError:
++                try:
++                    dummy = unicode(body, 'utf8')
++                    body = dummy.encode('iso-2022-jp')
++                    self._charset.output_charset = 'iso-2022-jp'
++                except UnicodeEncodeError:
++                    pass
++                except UnicodeDecodeError:
++                    pass
          msg = MIMEText(body, 'plain')
+         # Message class computes the wrong type from MIMEText constructor,
+         # which does not take a Charset object as initializer. Reset the
