b208b22ec08bb6fdb1ba80ca34bbacdbda91d656
Joe Marcus Clarke <marcus@FreeBSD.org>
2009-09-17 02:59:49 +0000

    Fix the build on sparc64.
    PR:		138877
    Submitted by:	beat
    Feature safe:	yes

--- security/nss/files/patch-..::coreconf::FreeBSD.mk
+++ security/nss/files/patch-..::coreconf::FreeBSD.mk
@@ -1,6 +1,6 @@
---- ../coreconf/FreeBSD.mk.orig	2008-07-12 16:28:59.000000000 +0200
-+++ ../coreconf/FreeBSD.mk	2009-08-31 10:20:16.000000000 +0200
-@@ -37,16 +37,23 @@
+--- ../../security/coreconf/FreeBSD.mk.orig	2008-07-12 10:28:59.000000000 -0400
++++ ../../security/coreconf/FreeBSD.mk	2009-09-16 22:57:28.000000000 -0400
+@@ -37,16 +37,28 @@
  
  include $(CORE_DEPTH)/coreconf/UNIX.mk
  
@@ -18,16 +18,21 @@
 +ifeq ($(OS_TEST),powerpc)
 +CPU_ARCH		= powerpc
 +else
++ifeq ($(OS_TEST),sparc64)
++CPU_ARCH		= sparc64
++USE_64			= 1
++else
 +ifeq ($(OS_TEST),amd64)
 +USE_64			= 1
 +endif
  CPU_ARCH		= x86
  endif
 +endif
++endif
  
  OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -Wno-switch -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
  
-@@ -60,20 +67,18 @@
+@@ -60,20 +72,18 @@ ifndef CLASSIC_NSPR
  USE_PTHREADS		= 1
  DEFINES			+= -D_THREAD_SAFE -D_REENTRANT
  OS_LIBS			+= -pthread
@@ -53,7 +58,7 @@
  ifdef MAPFILE
  	MKSHLIB += -Wl,--version-script,$(MAPFILE)
  endif
-@@ -82,4 +87,5 @@
+@@ -82,4 +92,5 @@ PROCESS_MAP_FILE = grep -v ';-' $< | \
  
  G++INCLUDES		= -I/usr/include/g++
  
