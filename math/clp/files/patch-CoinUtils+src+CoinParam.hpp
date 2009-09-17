bfcf1f790a07ec206ff2e013c5870cac219c356c
gerald <gerald@FreeBSD.org>
2009-09-17 13:02:43 +0000

    Update to version 1.10.0, fixing an existing build error with GCC 4.4 by means of an additional patch.
    Reported by:	pav and miwi (from pointyhat)
    Submitted by:	Pedro F. Giffuni <giffunip@yahoo.com>
    Approved by:	maintainer timeout
    Feature safe:	yes

---
+++ math/clp/files/patch-CoinUtils+src+CoinParam.hpp
@@ -0,0 +1,10 @@
+--- CoinUtils/src/CoinParam.hpp.orig	2009-08-16 22:33:13.000000000 -0500
++++ CoinUtils/src/CoinParam.hpp	2009-08-16 22:34:23.000000000 -0500
+@@ -12,6 +12,7 @@
+ 
+ #include <vector>
+ #include <string>
++#include <cstdio>
+ 
+ /*! \class CoinParam
+     \brief A base class for `keyword value' command line parameters.
