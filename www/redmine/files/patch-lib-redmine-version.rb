2a8bea7b768c734b7fd0982bb42c3d43f2a557c7
miwi <miwi@FreeBSD.org>
2009-09-18 22:52:35 +0000

    - Update to 0.8.5 - Removed rails dependency because redmine includes rails 2.1.2 in vendor/ - Added patch for missing redmine version bump to 0.8.5 - Added pkg-message with links to Redmine Install/Upgrade Guide - Makefile improvements
    PR:		138857
    Submitted by:	Bernhard Froehlich <decke@bluelife.at> (maintainer)
    Feature safe: yes

---
+++ www/redmine/files/patch-lib-redmine-version.rb
@@ -0,0 +1,11 @@
+--- lib/redmine/version.rb.orig	2009-09-13 14:10:18.000000000 +0200
++++ lib/redmine/version.rb	2009-09-15 09:30:33.000000000 +0200
+@@ -4,7 +4,7 @@
+   module VERSION #:nodoc:
+     MAJOR = 0
+     MINOR = 8
+-    TINY  = 4
++    TINY  = 5
+     
+     # Branch values:
+     # * official release: nil
