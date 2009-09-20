fbbc2f288f6c7a7f017108233a2302592162ba63
Felippe de Meirelles Motta <lippe@FreeBSD.org>
2009-09-20 01:36:55 +0000

    New port: free-sa development version
    Free-SA is a statistic analyzer for daemons log files similar to SARG.
    Its main advantages over SARG are much better speed (7x-20x times),
    more reports support, crossplatform work and W3C compliance of
    generated HTML/CSS reports code.
    
    WWW: http://free-sa.sourceforge.net
    
    PR:		ports/137172
    Submitted by:	Alexander Moisseev <moiseev@mezonplus.ru>
    Reworked by:	myself
    Feature safe:	yes

---
+++ www/free-sa-devel/files/extra-patch-global-i386.mk
@@ -0,0 +1,13 @@
+--- global.mk.old	2009-09-19 21:58:10.000000000 -0300
++++ global.mk	2009-09-19 21:58:20.000000000 -0300
+@@ -1,8 +1,8 @@
+ # Operating system profile
+-OSTYPE = generic-any-cc
++#OSTYPE = generic-any-cc
+ #OSTYPE = altlinux-i586-gcc4
+ #OSTYPE = cygwin-native-gcc3
+-#OSTYPE = freebsd-i386-gcc3
++OSTYPE = freebsd-i386-gcc3
+ #OSTYPE = freebsd-native-gcc4
+ #OSTYPE = hpux-64_2.0W-cc
+ #OSTYPE = openbsd-i386-gcc3
---
+++ www/free-sa-devel/files/extra-patch-global-native.mk
@@ -0,0 +1,14 @@
+--- global.mk.orig	2009-03-23 01:39:59.000000000 +0300
++++ global.mk	2009-04-04 14:24:39.000000000 +0400
+@@ -1,9 +1,9 @@
+ # Operating system profile
+-OSTYPE = generic-any-cc
++#OSTYPE = generic-any-cc
+ #OSTYPE = altlinux-i586-gcc4
+ #OSTYPE = cygwin-native-gcc3
+ #OSTYPE = freebsd-i386-gcc3
+-#OSTYPE = freebsd-native-gcc4
++OSTYPE = freebsd-native-gcc4
+ #OSTYPE = hpux-64_2.0W-cc
+ #OSTYPE = openbsd-i386-gcc3
+ #OSTYPE = redhat-native-gcc4
