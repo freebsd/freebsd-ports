65808a88b096d6833ad473acc0e9e0e259570271
miwi <miwi@FreeBSD.org>
2009-09-23 11:41:48 +0000

    - Update to 1.3.07.09
    PR:		139074
    Submitted by:	Martin Tournoij <carpetsmoker@rwxrwxrwx.net>
    Feature safe:	yes

--- dns/maradns/files/patch-tools_duende.c
+++ dns/maradns/files/patch-tools_duende.c
@@ -1,5 +1,5 @@
---- tools/duende.c.orig	Sat Jun  2 13:40:35 2007
-+++ tools/duende.c	Sat Jun  2 13:41:01 2007
+--- tools/duende.c.orig	2008-08-03 20:10:49.000000000 +0200
++++ tools/duende.c	2009-09-23 01:57:16.000000000 +0200
 @@ -35,10 +35,13 @@
  #include <signal.h>
  #include <stdio.h>
@@ -14,23 +14,21 @@
  #include <unistd.h>
  #include "../MaraDns.h"
  
-@@ -139,6 +142,9 @@
+@@ -139,6 +142,10 @@
      int exit_status;
      pid_t pid, log_pid;
      int stream1[2]; /* Used for piping */
 +    struct pidfh *pfh;
 +    char *name;
 +    char *pidfile;
++
      if(argv[0] == NULL || argv[1] == NULL) {
          printf("Usage: duende [program] [arguments]\n");
- 	exit(1);
-@@ -202,7 +208,24 @@
- 	    log_helper(argv[1],stream1[0]);
+         exit(1);
+@@ -203,6 +210,23 @@
              syslog(LOG_ALERT,"log_helper finished, terminating\n");
- 	    exit(1);
--	    } 
-+	    }
-+        
+             exit(1);
+             }
 +        name = strrchr(argv[1], '/');
 +        if (name && *name != '\0') {
 +          pidfile = malloc(strlen("/var/run/") + strlen(name) + strlen(".pid") + 1);
@@ -47,6 +45,7 @@
 +            free(pidfile);
 +          }
 +        }
++
          for(;;) {
- 	    /* If we got a HUP signal, send it to the child */
- 	    if(got_hup_signal == 1) {
+             /* If we got a HUP signal, send it to the child */
+             if(got_hup_signal == 1) {
