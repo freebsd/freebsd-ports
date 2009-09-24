d7c81e0aa23cb8dc9b6dac415c6f7451c5b5d737
Dmitry Marakasov <amdmi3@FreeBSD.org>
2009-09-24 11:39:42 +0000

    - Update to 1.3.5 - While here, also make it respect LOCALBASE
    PR:		139020
    Submitted by:	"Pedro F. Giffuni" <giffunip@tutopia.com>
    Feature safe:	yes

--- net-p2p/qbittorrent-22/files/patch-src__createtorrent_imp.cpp
+++ net-p2p/qbittorrent-22/files/patch-src__createtorrent_imp.cpp
@@ -1,13 +1,13 @@
---- src/createtorrent_imp.cpp.orig	2009-03-07 07:22:19.000000000 +0900
-+++ src/createtorrent_imp.cpp	2009-03-23 02:00:49.000000000 +0900
-@@ -245,7 +245,11 @@
+--- src/createtorrent_imp.cpp.orig	2009-09-21 10:02:50.000000000 +0000
++++ src/createtorrent_imp.cpp	2009-09-21 10:05:18.000000000 +0000
+@@ -257,7 +257,11 @@
      }
      if(abort) return;
      // calculate the hash for all pieces
 +#if defined(__GNUC__) && __GNUC__ <= 3
-+    set_piece_hashes(t, full_path.branch_path(), boost::bind(&sendProgressUpdateSignal, _1, t.num_pieces(), parent));
++    set_piece_hashes(t, full_path.branch_path(), boost::bind(&sendProgressUpdateSignal, _1, t.num_pieces(), this));
 +#else
-     set_piece_hashes(t, full_path.branch_path(), boost::bind<void>(&sendProgressUpdateSignal, _1, t.num_pieces(), parent));
+     set_piece_hashes(t, full_path.branch_path(), boost::bind<void>(&sendProgressUpdateSignal, _1, t.num_pieces(), this));
 +#endif
      // Set qBittorrent as creator and add user comment to
      // torrent_info structure
