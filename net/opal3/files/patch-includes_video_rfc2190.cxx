f30fe30f99f110b0fd6b0e146ecc82905a1b8620
Alexander Nedotsukov <bland@FreeBSD.org>
2009-09-27 15:52:55 +0000

    Add net/opal3, VoIP abstraction library v3.
    PR:		138718
    Feature safe:	yes

---
+++ net/opal3/files/patch-includes_video_rfc2190.cxx
@@ -0,0 +1,11 @@
+--- plugins/video/H.263-1998/rfc2190.cxx.orig	2009-08-30 02:08:09.000000000 +0400
++++ plugins/video/H.263-1998/rfc2190.cxx	2009-08-30 02:08:11.000000000 +0400
+@@ -21,7 +21,7 @@
+ 
+ #include <iostream>
+ #include <string.h>
+-#include <malloc.h>
++#include <stdlib.h>
+ using namespace std;
+ 
+ #include "rfc2190.h"
