ab47cf32098c5d258e4015c5262c3c4fc37de144
Brooks Davis <brooks@FreeBSD.org>
2009-09-27 21:43:25 +0000

    Upgrade to r82217.
    Feature safe:	yes

--- devel/llvm-devel/files/patch-tools_clang_lib_Sema_SemaChecking.cpp
+++ devel/llvm-devel/files/patch-tools_clang_lib_Sema_SemaChecking.cpp
@@ -3,37 +3,28 @@ $FreeBSD$
 
 --- tools/clang/lib/Sema/SemaChecking.cpp.orig
 +++ tools/clang/lib/Sema/SemaChecking.cpp
-@@ -142,7 +142,7 @@
- 
-   // Printf checking.
-   if (const FormatAttr *Format = FDecl->getAttr<FormatAttr>()) {
--    if (Format->getType() == "printf") {
-+    if (Format->getType() == "printf" || Format->getType() == "printf0") {
-       bool HasVAListArg = Format->getFirstArg() == 0;
-       if (!HasVAListArg) {
-         if (const FunctionProtoType *Proto 
-@@ -701,7 +701,6 @@
-     case 'u': 
+@@ -1120,7 +1120,6 @@
+     case 'u':
      case 'x':
      case 'X':
 -    case 'D':
      case 'O':
      case 'U':
      case 'e':
-@@ -717,9 +716,16 @@
+@@ -1136,9 +1135,16 @@
      case 'S':
      case 's':
-     case 'p': 
-+    case 'r':
-+    case 'y':
+     case 'p':
++    case 'r':                                                 
++    case 'y': 
        ++numConversions;
        CurrentState = state_OrdChr;
        break;
-+    case 'b':
-+    case 'D':
-+      numConversions += 2;
-+      CurrentState = state_OrdChr;
-+      break;
++   case 'b':                                                 
++   case 'D':                                                 
++     numConversions += 2;                                    
++     CurrentState = state_OrdChr;                            
++     break;
  
-     // CHECK: Are we using "%n"?  Issue a warning.
-     case 'n': {
+     case 'm':
+       // FIXME: Warn in situations where this isn't supported!
