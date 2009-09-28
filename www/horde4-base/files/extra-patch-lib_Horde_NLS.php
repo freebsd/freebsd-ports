5f0ce9c9c5eba7f8172367cef0a60e1ec39019aa
Thierry Thomas <thierry@FreeBSD.org>
2009-09-28 16:18:08 +0000

    Fix extra-patch for FreeBSD < 7.
    Reported by:	pointyhat via pav and miwi
    Feature safe:	yes
    Committed from:	Abidjan, Ivory Coast

--- www/horde4-base/files/extra-patch-lib_Horde_NLS.php
+++ www/horde4-base/files/extra-patch-lib_Horde_NLS.php
@@ -1,14 +1,14 @@
---- lib/Horde/NLS.php.orig	2008-01-22 10:08:06.000000000 -0900
-+++ lib/Horde/NLS.php	2008-02-29 09:50:02.000000000 -0900
-@@ -132,7 +132,10 @@
-                 setlocale(LC_ALL, $lang_charset);
+--- lib/Horde/NLS.php.orig	2009-09-14 10:08:48.000000000 +0000
++++ lib/Horde/NLS.php	2009-09-28 18:06:58.000000000 +0000
+@@ -133,6 +133,11 @@
              }
          }
--
+ 
 +	/* avoid FreeBSD issapce(3) bug */
 +	if(NLS::getCharset() == "UTF-8"){
 +		setlocale(LC_CTYPE,"C");
 +	}
++
+         @putenv('LC_ALL=' . $lang_charset);
          @putenv('LANG=' . $lang_charset);
          @putenv('LANGUAGE=' . $lang_charset);
-     }
