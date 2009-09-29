972faa0f831ccb4abea40c815f85055002905d94
Beat Gaetzi <beat@FreeBSD.org>
2009-09-29 15:15:02 +0000

    - Improve error message if /usr/lib32 is not installed on amd64. [1] - Fix pkg-plist. Additions directory is only created with   WITH_GUESTADDITIONS option. [2] - Add patch to fix panic with vboxnet driver. [3] - Bump PORTREVISION.
    PR:		ports/138935 [1] (based on)
    Reported by:	scf@ [3]
    Submitted by:	Julian H. Stacey <jhs AT berklix.com> [1],
    		Bernhard Froehlich <decke AT bluelife.at> [2],
    		Fredrik Lindberg <fli AT shapeshifter.se> [3]
    On behalf of:	vbox@ (decke, dhn, itetcu, miwi, nox)
    Feature safe:	yes

---
+++ emulators/virtualbox-ose-legacy/files/patch-src-VBox-HostDrivers-VBoxNetFlt-VBoxNetFltInternal.h
@@ -0,0 +1,28 @@
+Index: src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h
+===================================================================
+--- src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h	(revision 23391)
++++ src/VBox/HostDrivers/VBoxNetFlt/VBoxNetFltInternal.h	(working copy)
+@@ -206,6 +206,14 @@
+             hook_p output;
+             /** Original interface flags */
+             unsigned int flags;
++            /** Input queue */
++            struct ifqueue inq;
++            /** Output queue */
++            struct ifqueue outq;
++            /** Input task */
++            struct task tskin;
++            /** Output task */
++            struct task tskout;
+             /** The MAC address of the interface. */
+             RTMAC Mac;
+             /** @} */
+@@ -241,6 +249,8 @@
+ # endif
+ #elif defined(RT_OS_LINUX)
+         uint8_t abPadding[320];
++#elif defined(RT_OS_FREEBSD)
++        uint8_t abPadding[256];
+ #else
+         uint8_t abPadding[128];
+ #endif
