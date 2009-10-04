2d0f8ca35dae174eab3dbb724c3733c9fb2bf6ed
Wen Heping <wen@FreeBSD.org>
2009-10-04 23:40:14 +0000

    - Update to 0.8.0 - Update my mail address to FreeBSD
    Approved by:	jadawin(co-mentor)

--- audio/py-pyliblo/files/patch-setup.py
+++ audio/py-pyliblo/files/patch-setup.py
@@ -1,15 +1,14 @@
---- setup.py.orig	2009-06-01 03:12:01.000000000 +0800
-+++ setup.py	2009-06-01 03:14:23.000000000 +0800
-@@ -34,7 +34,7 @@
- ext_modules = [
-     Extension('liblo',
-               [with_pyrex and 'src/liblo.pyx' or 'src/liblo.c'],
--              extra_compile_args = ['-fno-strict-aliasing'],
-+              extra_compile_args = ['-fno-strict-aliasing','-I%%LOCALBASE%%/include/'],
-               libraries = ['lo'])
- ]
- 
-@@ -54,12 +54,12 @@
+--- setup.py.orig	2009-10-04 19:12:34.000000000 +0800
++++ setup.py	2009-10-04 19:14:46.000000000 +0800
+@@ -42,6 +42,7 @@
+         extra_compile_args = [
+             '-fno-strict-aliasing',
+             '-Werror-implicit-function-declaration',
++	    '-I%%LOCALBASE%%/include/',
+         ],
+         libraries = ['lo']
+     )
+@@ -63,12 +64,7 @@
          'scripts/send_osc.py',
          'scripts/dump_osc.py',
      ],
@@ -19,12 +18,7 @@
 -            'scripts/dump_osc.1',
 -        ]),
 -    ],
-+#    data_files = [
-+#        ('share/man/man1', [
-+#            'scripts/send_osc.1',
-+#            'scripts/dump_osc.1',
-+#        ]),
-+#    ],
++
      cmdclass = cmdclass,
      ext_modules = ext_modules
  )
