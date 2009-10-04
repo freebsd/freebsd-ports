14001d0127c62b94f7690c386296db795735f7d9
miwi <miwi@FreeBSD.org>
2009-10-04 10:08:14 +0000

    - Update to 1.0.3
    PR:		139291
    Submitted by:	Ports Fury

--- multimedia/lives/files/patch-lives-plugins__plugins__playback__video__lives2lives_stream.c
+++ multimedia/lives/files/patch-lives-plugins__plugins__playback__video__lives2lives_stream.c
@@ -1,14 +1,13 @@
---- lives-plugins/plugins/playback/video/lives2lives_stream.c.orig	2008-07-29 16:13:33.000000000 +0900
-+++ lives-plugins/plugins/playback/video/lives2lives_stream.c	2009-06-03 01:03:03.000000000 +0900
-@@ -8,6 +8,11 @@
- 
- #include "videoplugin.h"
- 
+--- lives-plugins/plugins/playback/video/lives2lives_stream.c.orig	2009-08-12 05:42:46.000000000 +0900
++++ lives-plugins/plugins/playback/video/lives2lives_stream.c	2009-09-30 00:31:51.000000000 +0900
+@@ -41,7 +41,10 @@
+ #include <unistd.h>
+ #include <stdlib.h>
+ #include <netdb.h>
 +#include <sys/types.h>
-+#include <sys/socket.h>
+ #include <sys/socket.h>
 +#include <netinet/in.h>
 +#include <arpa/inet.h>
-+
- 
- //////////////////////////////////////
+ #include <string.h>
+ #include <errno.h>
  
