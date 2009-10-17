2c2dacfb0f75ab8ab31280975dfeffed08ebd988
miwi <miwi@FreeBSD.org>
2009-10-17 11:53:59 +0000

    - Update to 0.5.2 (20091009)
    PR:		139648
    Submitted by:	Tobias Rehbein <tobias.rehbein@web.de> (maintainer)

--- games/stonesoup/files/patch-AppHdr.h
+++ games/stonesoup/files/patch-AppHdr.h
@@ -1,6 +1,6 @@
---- ./AppHdr.h.orig	2009-07-29 01:31:27.000000000 +0200
-+++ ./AppHdr.h	2009-08-03 10:10:09.000000000 +0200
-@@ -126,7 +126,7 @@
+--- ./AppHdr.h.orig	2009-10-06 20:44:04.000000000 +0200
++++ ./AppHdr.h	2009-10-15 18:40:29.000000000 +0200
+@@ -151,7 +151,7 @@
      //          setuid or setgid. Filenames passed to this command *are not
      //          validated in any way*.
      //
@@ -9,12 +9,3 @@
  
      // For cases when the game will be played on terms that don't support the
      // curses "bold == lighter" 16 colour mode. -- bwr
-@@ -413,6 +413,8 @@
-     // The default behaviour is to compress with zip.
-     // To use GNU tar instead, define SAVE_PACKAGE_TAR.
-     // To avoid compression entirely, define SAVE_PACKAGE_NONE.
-+    #define SAVE_PACKAGE_NONE
-+
-     #ifndef SAVE_PACKAGE_NONE
-     #ifdef SAVE_PACKAGE_TAR
-       // The --absolute-names switch is only there to suppress noise on stdout.
