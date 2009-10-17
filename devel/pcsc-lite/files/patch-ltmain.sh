8014a4bf4976f42acb3dbd75e0c0b3db05c10597
arved <arved@FreeBSD.org>
2009-10-17 11:36:28 +0000

    Update to 1.5.5

--- devel/pcsc-lite/files/patch-ltmain.sh
+++ devel/pcsc-lite/files/patch-ltmain.sh
@@ -1,14 +1,14 @@
---- ltmain.sh.orig	Mon Jan  2 14:34:57 2006
-+++ ltmain.sh	Mon Jan  2 14:37:12 2006
-@@ -5667,10 +5667,12 @@
+--- ltmain.sh.orig	2009-07-28 23:45:57.000000000 +0200
++++ ltmain.sh	2009-10-16 19:51:48.000000000 +0200
+@@ -2077,10 +2077,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
 +	if /usr/bin/false; then
- 	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
+ 	func_basename "$file"
+ 	name="$func_basename_result"
  	instname="$dir/$name"i
- 	$show "$install_prog $instname $destdir/$name"
- 	$run eval "$install_prog $instname $destdir/$name" || exit $?
+ 	func_show_eval "$install_prog $instname $destdir/$name" 'exit $?'
 +	fi
  
  	# Maybe install the static library, too.
