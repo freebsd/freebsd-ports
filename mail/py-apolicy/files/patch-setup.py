97d40fa8ff188112ab41e3a3aeea06c25a28eed7
Ion-Mihai Tetcu <itetcu@FreeBSD.org>
2009-10-18 06:17:50 +0000

    - remove files/patch-setup.py - replace the above with a ${REINPLACE_CMD} in the Makefile that uses the environment PREFIX Thus fix install the port when custom PREFIX is used.
    PR:		139539
    Submitted by:	Sahil Tandon
    Approved by:	maintainer

--- mail/py-apolicy/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
---- setup.py.orig	2008-11-24 06:35:54.000000000 +0000
-+++ setup.py	2008-11-24 06:36:11.000000000 +0000
-@@ -28,4 +28,4 @@
-     author_email="miguel.filho@gmail.com", packages=["apolicy"],
-     package_dir={"apolicy": "src/apolicy"},
-     url="http://www.apolicy.org",
--    data_files=[("/etc/apolicy", data_files)])
-+    data_files=[("/usr/local/etc/apolicy", data_files)])
