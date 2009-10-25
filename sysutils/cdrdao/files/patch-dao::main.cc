eb3847a7ba46dd29b41ec157780677e12987ccbe
marius <marius@FreeBSD.org>
2009-10-25 14:52:40 +0000

    Update to 1.2.3.
    Approved by:	netchild

--- sysutils/cdrdao/files/patch-dao::main.cc
+++ sysutils/cdrdao/files/patch-dao::main.cc
@@ -1,15 +1,15 @@
 --- dao/main.cc.orig	Sat Dec 28 03:01:20 2002
 +++ dao/main.cc	Sat Dec 28 03:04:38 2002
-@@ -1890,11 +1890,7 @@
+@@ -2314,11 +2314,7 @@
  
-   SETTINGS = new Settings;
+     Settings* settings = new Settings;
  
--  settingsPath = "/etc/cdrdao.conf";
--  if (SETTINGS->read(settingsPath) == 0)
--    message(3, "Read settings from \"%s\".", settingsPath);
+-    settingsPath = "/etc/cdrdao.conf";
+-    if (settings->read(settingsPath) == 0)
+-	log_message(3, "Read settings from \"%s\".", settingsPath);
 -
--  settingsPath = "/etc/defaults/cdrdao";
-+  settingsPath = "%%PREFIX%%/etc/cdrdao.conf";
-   if (SETTINGS->read(settingsPath) == 0)
-     message(3, "Read settings from \"%s\".", settingsPath);
+-    settingsPath = "/etc/defaults/cdrdao";
++    settingsPath = "%%PREFIX%%/etc/cdrdao.conf";
+     if (settings->read(settingsPath) == 0)
+ 	log_message(3, "Read settings from \"%s\".", settingsPath);
  
