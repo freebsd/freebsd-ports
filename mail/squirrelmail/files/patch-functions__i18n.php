1a5d375f7e916b10304a1e8c8a8c4966c5982c44
Thomas Abthorpe <tabthorpe@FreeBSD.org>
2009-10-29 18:44:23 +0000

    - Fix Japanese (and possibly other) text corruption on forward/reply - Bump PORTREVISION
    PR:		ports/139360
    Submitted by:	Kazuo Dohzono <dohzono axion-software.com>

--- mail/squirrelmail/files/patch-functions__i18n.php
+++ mail/squirrelmail/files/patch-functions__i18n.php
@@ -1,6 +1,15 @@
---- ./functions/i18n.php.orig	2009-05-07 17:55:44.000000000 -0400
-+++ ./functions/i18n.php	2009-05-19 23:27:43.000000000 -0400
-@@ -640,7 +640,8 @@
+--- ./functions/i18n.php.orig	2009-07-28 22:21:06.000000000 -0400
++++ ./functions/i18n.php	2009-10-08 10:30:25.000000000 -0400
+@@ -146,7 +146,7 @@
+     }
+ 
+     /* safety checks */
+-    if (preg_match("/^.*\/.*\/.*\/.*\/.*\/.*$/",$ret)) {
++    if (preg_match("/^.*\/.*\/.*\/.*\/.*\/.*$/i",$ret)) {
+         /**
+          * Welcome to We-Don't-Follow-Own-Fine-Manual department
+          * OpenBSD 3.8, 3.9-current and maybe later versions
+@@ -683,7 +683,8 @@
              $useragent = func_get_arg(2);
              if (strstr($useragent, 'Windows') !== false ||
                  strstr($useragent, 'Mac_') !== false) {
