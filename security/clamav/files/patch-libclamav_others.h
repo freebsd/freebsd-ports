143dfac64e7a22fc93b8958146d49e4464d4a707
Renato Botelho <garga@FreeBSD.org>
2009-10-29 10:46:26 +0000

    Update to 0.95.3

---
+++ security/clamav/files/patch-libclamav_others.h
@@ -0,0 +1,10 @@
+--- libclamav/others.h.orig	2009-10-29 08:11:34.000000000 -0200
++++ libclamav/others.h	2009-10-29 08:11:45.000000000 -0200
+@@ -29,6 +29,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <unistd.h>
+ #include "cltypes.h"
+ 
+ #include "clamav.h"
