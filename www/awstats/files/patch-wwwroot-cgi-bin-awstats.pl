bfe7de9e16e2236103f75c790db22acb856baf36
Martin Wilke <miwi@FreeBSD.org>
2009-10-30 09:51:20 +0000

    - Update to 6.95
    PR:		140065
    Submitted by:	Naram Qashat <cyberbotx@cyberbotx.com>
    Approved by:	maintainer

--- www/awstats/files/patch-wwwroot-cgi-bin-awstats.pl
+++ www/awstats/files/patch-wwwroot-cgi-bin-awstats.pl
@@ -1,11 +1,11 @@
---- wwwroot/cgi-bin/awstats.pl.orig	2008-11-30 10:42:46.000000000 -0500
-+++ wwwroot/cgi-bin/awstats.pl	2009-01-03 16:32:27.000000000 -0500
-@@ -2925,7 +2925,7 @@
+--- wwwroot/cgi-bin/awstats.pl.orig	2009-10-10 08:36:38.000000000 -0400
++++ wwwroot/cgi-bin/awstats.pl	2009-10-28 21:33:22.000000000 -0400
+@@ -3040,7 +3040,7 @@
  # Debian package :                    		"/usr/share/awstats/plugins"
  	my @PossiblePluginsDir = (
- 							   "$DIR/plugins",
--							   "/usr/local/awstats/wwwroot/cgi-bin/plugins",
-+							   "%%PREFIX%%/www/awstats/cgi-bin/plugins",
- 							   "/usr/share/awstats/plugins"
+ 		"$DIR/plugins",
+-		"/usr/local/awstats/wwwroot/cgi-bin/plugins",
++		"%%PREFIX%%/www/awstats/cgi-bin/plugins",
+ 		"/usr/share/awstats/plugins"
  	);
  	my %DirAddedInINC = ();
