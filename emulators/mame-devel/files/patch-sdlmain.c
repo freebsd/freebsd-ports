6f7a0e4da81b6d1db64939d09281ae83c053a1c6
miwi <miwi@FreeBSD.org>
2009-11-04 10:07:50 +0000

    - Update to 0.134u4
    PR:		140154
    Submitted by:	Emmanuel Vadot <elbarto@arcadebsd.org> (maintainer)

--- emulators/mame-devel/files/patch-sdlmain.c
+++ emulators/mame-devel/files/patch-sdlmain.c
@@ -1,11 +1,11 @@
---- src/osd/sdl/sdlmain.c.orig	2009-03-25 05:55:00.000000000 +0100
-+++ src/osd/sdl/sdlmain.c	2009-03-25 05:56:04.000000000 +0100
-@@ -82,7 +82,7 @@
+--- src/osd/sdl/sdlmain.c.orig	2009-11-01 01:22:48.000000000 +0100
++++ src/osd/sdl/sdlmain.c	2009-11-01 01:23:26.000000000 +0100
+@@ -78,7 +78,7 @@
  #ifdef MESS
- 	{ "inipath",                              "$HOME/.mess;.;ini",     0,     "path to ini files" },
+ 	#define INI_PATH "$HOME/.mess;.;ini"
  #else
--	{ SDLOPTION_INIPATH,                     "$HOME/.mame;.;ini",     0,     "path to ini files" },
-+	{ SDLOPTION_INIPATH,                     "$HOME/.mame-devel;.;ini",     0,     "path to ini files" },
+-	#define INI_PATH "$HOME/.mame;.;ini"
++	#define INI_PATH "$HOME/.mame-devel;.;ini"
  #endif // MESS
- #endif // INI_PATH
  #endif // MACOSX
+ #endif // INI_PATH
