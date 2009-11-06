796740a46650fdd5c170cb29f14ce8c9f5e0bce1
Dmitry Marakasov <amdmi3@FreeBSD.org>
2009-11-06 01:40:47 +0000

    - Update to 112

--- games/powder/files/patch-gfx-rebuild.sh
+++ games/powder/files/patch-gfx-rebuild.sh
@@ -1,9 +1,9 @@
---- gfx/rebuild.sh.orig	2009-02-06 04:42:17.000000000 +0300
-+++ gfx/rebuild.sh	2009-06-08 00:18:29.000000000 +0400
+--- gfx/rebuild.sh.orig	2009-09-24 06:33:55.000000000 +0400
++++ gfx/rebuild.sh	2009-11-05 18:35:53.000000000 +0300
 @@ -3,10 +3,10 @@
  echo Rebuilding Graphics Files
  
- for tileset in classic adambolt nethack ascii ibsongrey akoimeexx; do
+ for tileset in classic distorted adambolt nethack ascii ibsongrey akoimeexx lomaka ; do
 -	pushd $tileset
 +	cd $tileset
  	echo Building tiles...
