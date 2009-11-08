f885d49e7e87c60a4f9865e3564e20a45127d56c
Tilman Keskinoz <arved@FreeBSD.org>
2009-11-08 00:06:57 +0000

    Fix an initialization bug Bump PORTREVISION
    PR:		138408
    Submitted by:	takawata

--- finance/libofx/files/patch-lib_ofx_utilities.cpp
+++ finance/libofx/files/patch-lib_ofx_utilities.cpp
@@ -1,13 +1,20 @@
-*** lib/ofx_utilities.old	Sun Apr 13 12:39:29 2003
---- lib/ofx_utilities.cpp	Sun Apr 13 12:11:04 2003
-***************
-*** 16,21 ****
---- 16,22 ----
-   *                                                                         *
-   ***************************************************************************/
-  #include <config.h>
-+ #include <locale.h>
-  #include <iostream>
-  #include "ParserEventGeneratorKit.h"
-  #include "SGMLApplication.h"
-
+--- lib/ofx_utilities.cpp.orig	2007-10-27 14:15:58.000000000 +0200
++++ lib/ofx_utilities.cpp	2009-11-08 01:02:47.000000000 +0100
+@@ -16,6 +16,7 @@
+  *                                                                         *
+  ***************************************************************************/
+ #include <config.h>
++#include <locale.h>
+ #include <iostream>
+ #include <assert.h>
+ 
+@@ -113,6 +114,9 @@
+   char time_zone_specified = false;
+ 
+   time_t temptime;
++
++  bzero(&time, sizeof(time));
++
+   std::time(&temptime);
+   local_offset = difftime(mktime(localtime(&temptime)), mktime(gmtime(&temptime)));
+   
