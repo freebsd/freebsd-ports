a8c9efd8e5304cc99a4f142125293b0a6450aa05
tabthorpe <tabthorpe@FreeBSD.org>
2009-11-10 04:10:37 +0000

    - Fix imap search [1] - Really fix Japanese text corruption [2] - Bump PORTREVISION
    Submitted by:	Carlos Fernando Assis Paniago via Simon Dick [1]
    Submitted by:	Kazuo Dohzono [2]
    Obtained from:	http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=550763 [1]

--- mail/squirrelmail/files/patch-functions__i18n.php
+++ mail/squirrelmail/files/patch-functions__i18n.php
@@ -1,14 +1,14 @@
 --- ./functions/i18n.php.orig	2009-07-28 22:21:06.000000000 -0400
-+++ ./functions/i18n.php	2009-10-08 10:30:25.000000000 -0400
-@@ -146,7 +146,7 @@
-     }
- 
-     /* safety checks */
--    if (preg_match("/^.*\/.*\/.*\/.*\/.*\/.*$/",$ret)) {
-+    if (preg_match("/^.*\/.*\/.*\/.*\/.*\/.*$/i",$ret)) {
-         /**
-          * Welcome to We-Don't-Follow-Own-Fine-Manual department
-          * OpenBSD 3.8, 3.9-current and maybe later versions
++++ ./functions/i18n.php	2009-11-09 11:22:46.000000000 -0500
+@@ -675,7 +675,7 @@
+             break;
+         case 'decodeheader':
+             $ret = str_replace("\t", "", $ret);
+-            if (preg_match('/=\?([^?]+)\?(q|b)\?([^?]+)\?=/', $ret))
++            if (preg_match('/=\?([^?]+)\?(q|b)\?([^?]+)\?=/i', $ret))
+                 $ret = @mb_decode_mimeheader($ret);
+             $ret = @mb_convert_encoding($ret, 'EUC-JP', 'AUTO');
+             break;
 @@ -683,7 +683,8 @@
              $useragent = func_get_arg(2);
              if (strstr($useragent, 'Windows') !== false ||
