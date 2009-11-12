5e7a4c68b98ad9ecb08c31c805d05b51f4636ba4
Martin Wilke <miwi@FreeBSD.org>
2009-11-12 10:52:28 +0000

    - Update to 3.6.163
    PR:		140481
    Submitted by:	Ports Fury

---
+++ cad/xcircuit/files/patch-svg.c
@@ -0,0 +1,11 @@
+--- svg.c.orig	2009-09-23 21:50:37.000000000 +0900
++++ svg.c	2009-11-09 22:27:21.000000000 +0900
+@@ -8,6 +8,7 @@
+ #include <string.h>
+ #include <math.h>
+ #include <limits.h>
++#include <unistd.h>
+ #include <sys/stat.h>
+ #include <sys/wait.h>
+ 
+
