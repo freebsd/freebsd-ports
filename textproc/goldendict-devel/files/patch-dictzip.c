8410624681fe2366ba2f723815ada593e471a24d
Boris Samorodov <bsam@FreeBSD.org>
2009-11-13 14:07:49 +0000

    The project aims to create a feature-rich dictionary lookup program. It supports:     * Babylon .BGL files, complete with images and resources;     * StarDict .ifo/.dict./.idx/.syn dictionaries;     * Dictd .index/.dict(.dz) dictionary files;     * ABBYY Lingvo .dsl source files, together with abbreviations.       The files can be optionally compressed with dictzip. Dictionary       resources can be packed together into a .zip file;     * ABBYY Lingvo .lsa/.dat audio archives. Those can be indexed       separately, or be referred to from .dsl files.
    LICENSE: GPL3 or later
    
    WWW: http://goldendict.berlios.de/

---
+++ textproc/goldendict-devel/files/patch-dictzip.c
@@ -0,0 +1,10 @@
+--- dictzip.c.orig	2009-11-09 01:12:06.000000000 +0300
++++ dictzip.c	2009-11-09 01:12:21.000000000 +0300
+@@ -23,6 +23,7 @@
+  */
+ 
+ #include <stdlib.h>
++#include <time.h>
+ #include "dictzip.h"
+ #include <limits.h>
+ #include <stdarg.h>
