d659cdfb0be31f7e7b99f009e0c58c8619b4a18c
dinoex <dinoex@FreeBSD.org>
2009-11-14 22:52:22 +0000

    - fix build for gnustep-devel

--- sysutils/burn/files/patch-WorkInProgress.m
+++ sysutils/burn/files/patch-WorkInProgress.m
@@ -1,5 +1,5 @@
 --- WorkInProgress.m.orig	2005-05-12 17:58:49.000000000 +0200
-+++ WorkInProgress.m	2008-12-24 01:53:34.000000000 +0100
++++ WorkInProgress.m	2009-11-14 23:48:17.000000000 +0100
 @@ -23,6 +23,7 @@
  
  #include <Foundation/Foundation.h>
@@ -8,3 +8,25 @@
  
  #include "Functions.h"
  #include "Constants.h"
+@@ -32,6 +33,12 @@
+ static NSString *nibName = @"WorkInProgress";
+ #define PI_LENGTH 2
+ 
++static inline NSSize
++my_sizeForBorderType (NSBorderType aType)
++{
++  return [[GSTheme theme] sizeForBorderType: aType];
++}
++
+ @implementation WorkInProgressIndicator
+ 
+ /** Display the next step
+@@ -77,7 +84,7 @@
+     [super drawRect: rect];
+ 
+     // Calculate the inside rect to be drawn
+-	NSSize borderSize = _sizeForBorderType (NSBezelBorder);
++	NSSize borderSize = my_sizeForBorderType (NSBezelBorder);
+ 	r = NSInsetRect(_bounds, borderSize.width, borderSize.height);
+ 
+     value = [self doubleValue];
