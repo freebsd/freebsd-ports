f8f3cc86827fbb68275756f6c7d2e6775529be51
Dirk Meyer <dinoex@FreeBSD.org>
2009-11-14 22:52:22 +0000

    - fix build for gnustep-devel

--- sysutils/burn/files/patch-ExtProgressIndicator.m
+++ sysutils/burn/files/patch-ExtProgressIndicator.m
@@ -1,5 +1,5 @@
 --- ExtProgressIndicator.m.orig	2005-05-12 17:58:49.000000000 +0200
-+++ ExtProgressIndicator.m	2008-05-03 09:35:27.000000000 +0200
++++ ExtProgressIndicator.m	2009-11-14 23:42:55.000000000 +0100
 @@ -21,6 +21,8 @@
   *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
   */
@@ -9,3 +9,24 @@
  #include <AppKit/NSColor.h>
  #include <AppKit/NSParagraphStyle.h>
  #include <Foundation/Foundation.h>
+@@ -29,6 +31,11 @@
+ 
+ #include "Constants.h"
+ 
++static inline NSSize
++my_sizeForBorderType (NSBorderType aType)
++{
++  return [[GSTheme theme] sizeForBorderType: aType];
++}
+ 
+ @implementation ExtProgressIndicator
+ 
+@@ -79,7 +86,7 @@
+ 
+     // Calculate the inside rect to be drawn
+ 	if (_isBezeled) {
+-		NSSize borderSize = _sizeForBorderType (NSBezelBorder);
++		NSSize borderSize = my_sizeForBorderType (NSBezelBorder);
+ 		origRect = NSInsetRect(_bounds, borderSize.width, borderSize.height);
+ 	} else
+ 		origRect = _bounds;
