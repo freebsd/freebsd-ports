ab3b3a4c2c4d9db0b8ffb48e8a01b155ec6039bd
Alex Dupre <ale@FreeBSD.org>
2009-11-16 09:28:06 +0000

    Add JRadioButton fix.

--- devel/charva/files/patch-java_src_charvax_swing_ButtonGroup.java
+++ devel/charva/files/patch-java_src_charvax_swing_ButtonGroup.java
@@ -1,6 +1,6 @@
---- java/src/charvax/swing/ButtonGroup.java.orig	Sat Dec 17 15:27:30 2005
-+++ java/src/charvax/swing/ButtonGroup.java	Mon Jan 29 09:39:30 2007
-@@ -63,9 +63,10 @@ public class ButtonGroup
+--- java/src/charvax/swing/ButtonGroup.java.orig	2005-12-17 15:27:30.000000000 +0100
++++ java/src/charvax/swing/ButtonGroup.java	2009-11-16 10:26:42.000000000 +0100
+@@ -63,9 +63,10 @@
       * Adds the specified button to the group.
       */
      public void add(AbstractButton button_) {
@@ -12,3 +12,15 @@
      }
  
      /**
+@@ -133,10 +134,9 @@
+         for (Enumeration e = _buttons.elements(); e.hasMoreElements();) {
+ 
+             AbstractButton b = (AbstractButton) e.nextElement();
+-            if (source != b || statechange != ItemEvent.SELECTED) {
++            if (source != b && statechange == ItemEvent.SELECTED) {
+                 if (b.isSelected()) {
+                     b._selected = false;
+-                    b.setEnabled(true);
+                 }
+             }
+         }
