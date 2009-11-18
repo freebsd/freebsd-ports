70e3ae72134a6cb45c750f7cbb01b246ccba17a2
wen <wen@FreeBSD.org>
2009-11-18 07:06:54 +0000

    - Fix the build on FreeBSD9
    PR:		ports/140164
    Submitted by:	Wen Heping (myself)
    Approved by:	Maintainer(timeout, >15 days)

--- math/py-matplotlib/files/patch-setupext.py
+++ math/py-matplotlib/files/patch-setupext.py
@@ -1,15 +1,16 @@
---- setupext.py.orig	2008-08-03 13:15:22.000000000 -0500
-+++ setupext.py	2008-09-29 13:20:02.000000000 -0500
-@@ -55,6 +55,8 @@
+--- setupext.py.orig	2009-08-02 03:15:24.000000000 +0800
++++ setupext.py	2009-11-18 15:03:10.000000000 +0800
+@@ -55,6 +55,9 @@
      'freebsd4' : ['/usr/local', '/usr'],
      'freebsd5' : ['/usr/local', '/usr'],
      'freebsd6' : ['/usr/local', '/usr'],
 +    'freebsd7' : ['/usr/local', '/usr'],
 +    'freebsd8' : ['/usr/local', '/usr'],
++    'freebsd9' : ['/usr/local', '/usr'],
      'sunos5' : [os.getenv('MPLIB_BASE') or '/usr/local',],
      'gnukfreebsd5' : ['/usr/local', '/usr'],
      'gnukfreebsd6' : ['/usr/local', '/usr'],
-@@ -814,7 +816,7 @@
+@@ -756,7 +759,7 @@
          module.libraries.extend(wxlibs)
          return
  
@@ -18,7 +19,7 @@
  
  # Make sure you use the Tk version given by Tkinter.TkVersion
  # or else you'll build for a wrong version of the Tcl
-@@ -1096,9 +1098,9 @@
+@@ -1054,9 +1057,9 @@
  
          # Add final versions of directories and libraries to module lists
          tcl_lib, tcl_inc, tk_lib, tk_inc = result
