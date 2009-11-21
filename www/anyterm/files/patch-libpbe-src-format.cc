f9c065abb9059f32dd0bdd6b64160eff7e589c80
Martin Wilke <miwi@FreeBSD.org>
2009-11-21 10:18:54 +0000

    Anyterm provides a terminal emulator on a Web page using Javascript and a server daemon. The daemon typically runs behind an HTTP proxy; it forks a shell and communicates with the script using XMLHTTP on port 80 or securely using SSL. This provides you with shell access to your machine from almost any Web browser, even when firewalls are in the way.
    WWW: http://anyterm.org/
    
    PR:		ports/140740
    Submitted by:	Douglas Thrift

---
+++ www/anyterm/files/patch-libpbe-src-format.cc
@@ -0,0 +1,11 @@
+--- libpbe/src/format.cc.orig	2009-11-20 00:12:52.878597868 -0800
++++ libpbe/src/format.cc	2009-11-20 00:13:07.402710981 -0800
+@@ -21,7 +21,7 @@
+ #include <string>
+ #include <cstdarg>
+ #include <cstdio>
+-#include <malloc.h>
++#include <stdlib.h>
+ 
+ 
+ namespace pbe {
