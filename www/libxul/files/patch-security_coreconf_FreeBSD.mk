463a211c906e273044de1e10f77d7e360ccb3038
Marcel Moolenaar <marcel@FreeBSD.org>
2009-11-22 21:32:27 +0000

    Fix build for ia64, powerpc & sparc64.
    Ok'd by: miwi (thanks!)

--- www/libxul/files/patch-security_coreconf_FreeBSD.mk
+++ www/libxul/files/patch-security_coreconf_FreeBSD.mk
@@ -1,6 +1,6 @@
---- security/coreconf/FreeBSD.mk.orig	Wed Mar 26 20:17:25 2003
-+++ security/coreconf/FreeBSD.mk	Thu Jul  8 12:56:24 2004
-@@ -35,16 +35,18 @@
+--- security/coreconf/FreeBSD.mk.orig	2009-11-22 11:51:31.000000000 -0800
++++ security/coreconf/FreeBSD.mk	2009-11-22 11:51:44.000000000 -0800
+@@ -37,16 +37,32 @@
  
  include $(CORE_DEPTH)/coreconf/UNIX.mk
  
@@ -13,16 +13,30 @@
  ifeq ($(OS_TEST),alpha)
  CPU_ARCH		= alpha
  else
++ifeq ($(OS_TEST),powerpc)
++CPU_ARCH		= powerpc
++else
++ifeq ($(OS_TEST),sparc64)
++CPU_ARCH		= sparc64
++USE_64			= 1
++else
++ifeq ($(OS_TEST),ia64)
++CPU_ARCH		= ia64
++USE_64			= 1
++else
 +ifeq ($(OS_TEST),amd64)
 +CPU_ARCH		= amd64
 +else
  CPU_ARCH		= x86
  endif
 +endif
++endif
++endif
++endif
  
  OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
  
-@@ -71,7 +73,11 @@
+@@ -73,7 +89,11 @@
  DLL_SUFFIX		= so.1.0
  endif
  
@@ -33,5 +47,5 @@
 +MKSHLIB			= $(CC) -Wl,-Bsymbolic $(DSO_LDOPTS) -o $@
 +endif
  ifdef MAPFILE
- # Add LD options to restrict exported symbols to those in the map file
+ 	MKSHLIB += -Wl,--version-script,$(MAPFILE)
  endif
