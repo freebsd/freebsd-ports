f951c5aaa7888e5f6dbedbf6f17a291940d42278
dinoex <dinoex@FreeBSD.org>
2009-11-28 21:49:45 +0000

    - update to 0.17.1

--- Mk/bsd.gnustep.mk
+++ Mk/bsd.gnustep.mk
@@ -274,7 +274,7 @@ RUN_DEPENDS+=	${GNUSTEP_SYSTEM_LIBRARIES}/libgnustep-gui.so:${PORTSDIR}/${GNUSTE
 # using any backend
 #
 .if defined(USE_GNUSTEP_BACK)
-BACKSUFFIX?=	-016
+BACKSUFFIX?=	-017
 .if defined(WITH_GNUSTEP_XDPS)
 GNUSTEP_WITH_XDPS=yes
 .elif defined(WITH_GNUSTEP_LIBART)
