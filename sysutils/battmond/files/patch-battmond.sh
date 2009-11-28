3c9ffc0ed454cf0b6ba9b153f2fa6d4ec0544da7
amdmi3 <amdmi3@FreeBSD.org>
2009-11-28 06:55:26 +0000

    - Fix rc script
    PR:		140834
    Submitted by:	Nikos Ntarmos <ntarmos@cs.uoi.gr> (maintainer)

---
+++ sysutils/battmond/files/patch-battmond.sh
@@ -0,0 +1,11 @@
+--- battmond.sh.orig
++++ battmond.sh
+@@ -12,7 +12,7 @@
+ #
+ 
+ battmond_enable=${battmond_enable-"NO"}
+-battmond_flags=${battmond_falgs-""}
++battmond_flags=${battmond_flags-""}
+ 
+ . /etc/rc.subr
+ 
