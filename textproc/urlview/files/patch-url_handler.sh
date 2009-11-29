17213429b40e3947db5c480de5adbc335c467a94
obrien <obrien@FreeBSD.org>
2009-11-29 08:24:50 +0000

    + Use bsd.gecko.mk to set GECKO, if WITH_GECKO is set, or default to firefox. + Respect X11BASE.
    PR:		102954
    Submitted by:	Thierry Thomas <thierry@@pompo.net>

--- textproc/urlview/files/patch-url_handler.sh
+++ textproc/urlview/files/patch-url_handler.sh
@@ -1,3 +1,4 @@
+$FreeBSD$
 --- url_handler.sh.orig	Tue Jul  4 03:14:30 2000
 +++ url_handler.sh	Wed Aug 22 10:42:49 2001
 @@ -1,4 +1,4 @@
@@ -15,8 +16,8 @@
 -mailto_prgs="/usr/bin/mutt:VT /usr/bin/elm:VT /usr/bin/pine:VT /usr/bin/mail:VT"
 -gopher_prgs="/usr/bin/lynx:XT /usr/bin/gopher:XT"
 -ftp_prgs="/usr/bin/lynx:XT /usr/bin/ncftp:XT"
-+https_prgs="!!PREFIX!!/bin/netscape:XW !!PREFIX!!/bin/lynx:XT !!PREFIX!!/bin/w3m:XT"
-+http_prgs="!!PREFIX!!/bin/netscape:XW !!PREFIX!!/bin/lynx:XT !!PREFIX!!/bin/w3m:XT !!PREFIX!!/bin/links:XT"
++https_prgs="!!PREFIX!!/bin/gecko.sh:XW !!PREFIX!!/bin/lynx:XT !!PREFIX!!/bin/w3m:XT"
++http_prgs="!!PREFIX!!/bin/gecko.sh:XW !!PREFIX!!/bin/lynx:XT !!PREFIX!!/bin/w3m:XT !!PREFIX!!/bin/links:XT"
 +mailto_prgs="!!PREFIX!!/bin/mutt:VT !!PREFIX!!/bin/elm:VT !!PREFIX!!/bin/pine:VT /usr/bin/mail:VT"
 +gopher_prgs="!!PREFIX!!/bin/lynx:XT !!PREFIX!!/bin/gopher:XT"
 +ftp_prgs="!!PREFIX!!/bin/lynx:XT !!PREFIX!!/bin/ncftp2:XT !!PREFIX!!/bin/ncftp3:XT !!PREFIX!!/bin/ncftp:XT"
