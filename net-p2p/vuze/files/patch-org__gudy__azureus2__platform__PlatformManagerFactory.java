72ec507ad453856af29aa2ae2996ce17bd8b1c0d
rnoland <rnoland@FreeBSD.org>
2009-12-01 21:05:21 +0000

    Update to 3.4.0.4
     - Only support the libxul gecko backend now.
    	You will need to set the path to libxul in your
    	Options->Interface->Display section for browsing to work.
    
     - Special hacks are no longer required for it to perform well.

--- net-p2p/vuze/files/patch-org__gudy__azureus2__platform__PlatformManagerFactory.java
+++ net-p2p/vuze/files/patch-org__gudy__azureus2__platform__PlatformManagerFactory.java
@@ -1,6 +1,6 @@
---- org/gudy/azureus2/platform/PlatformManagerFactory.java.orig	2008-10-22 19:53:40.000000000 -0400
-+++ org/gudy/azureus2/platform/PlatformManagerFactory.java	2008-10-22 19:53:58.000000000 -0400
-@@ -48,21 +48,17 @@
+--- org/gudy/azureus2/platform/PlatformManagerFactory.java.orig	2009-07-10 06:30:32.000000000 -0500
++++ org/gudy/azureus2/platform/PlatformManagerFactory.java	2009-07-10 06:36:34.000000000 -0500
+@@ -50,21 +50,17 @@
  				try{
  					if ( getPlatformType() == PlatformManager.PT_WINDOWS ){
  						
@@ -14,7 +14,7 @@
  	                    
  					}else if( getPlatformType() == PlatformManager.PT_UNIX ){
  						
-             platform_manager = org.gudy.azureus2.platform.unix.PlatformManagerImpl.getSingleton();
+ 						platform_manager = org.gudy.azureus2.platform.unix.PlatformManagerImpl.getSingleton();
  
  					}
 -				}catch( PlatformManagerException e ){
