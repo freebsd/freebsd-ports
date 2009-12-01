57dd744e404d29cd6a5bc93cabb1be8dd12e78e3
Robert Noland <rnoland@FreeBSD.org>
2009-12-01 21:05:21 +0000

    Update to 3.4.0.4
     - Only support the libxul gecko backend now.
    	You will need to set the path to libxul in your
    	Options->Interface->Display section for browsing to work.
    
     - Special hacks are no longer required for it to perform well.

--- net-p2p/vuze/files/patch-org__gudy__azureus2__platform__unix__ScriptBeforeStartup.java
+++ net-p2p/vuze/files/patch-org__gudy__azureus2__platform__unix__ScriptBeforeStartup.java
@@ -1,6 +1,6 @@
---- org/gudy/azureus2/platform/unix/ScriptBeforeStartup.java.orig	2008-06-03 11:25:03.000000000 -0400
-+++ org/gudy/azureus2/platform/unix/ScriptBeforeStartup.java	2008-06-03 11:27:27.000000000 -0400
-@@ -139,6 +139,7 @@
+--- org/gudy/azureus2/platform/unix/ScriptBeforeStartup.java.orig	2009-09-20 15:37:15.000000000 -0500
++++ org/gudy/azureus2/platform/unix/ScriptBeforeStartup.java	2009-09-20 15:53:35.000000000 -0500
+@@ -141,10 +141,12 @@
  						return false;
  					}
  				});
@@ -8,3 +8,8 @@
  				possibleDirs.add(new File("/usr/local"));
  				possibleDirs.add(new File("/opt"));
  
+ 				final String[] possibleDirNames = {
++					"libxul",
+ 					"mozilla",
+ 					"firefox",
+ 					"seamonkey",
