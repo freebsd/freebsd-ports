5a3c3868fc395f25e37202c34206945349c38bab
rnoland <rnoland@FreeBSD.org>
2009-12-01 21:00:28 +0000

    Update to 3.5
     - Remove traditional gecko support and replace with libxul.

--- x11-toolkits/swt/files/patch-build.sh
+++ x11-toolkits/swt/files/patch-build.sh
@@ -1,12 +1,22 @@
---- build.sh.orig	Thu May 31 18:04:22 2007
-+++ build.sh	Mon Jun 11 18:25:04 2007
-@@ -65,15 +65,16 @@
+--- build.sh.orig	2009-06-11 16:10:54.000000000 -0500
++++ build.sh	2009-12-01 14:42:09.000000000 -0600
+@@ -44,6 +44,9 @@
+ 	"FreeBSD")
+ 		SWT_OS=freebsd
+ 		MAKEFILE=make_freebsd.mak
++		if uname -p > /dev/null 2>&1; then
++			MODEL=`uname -p`
++		fi
+ 		;;
+ 	*)
+ 		SWT_OS=`uname -s | tr -s '[:upper:]' '[:lower:]'`
+@@ -75,15 +78,16 @@
  esac
  
  # For 64-bit CPUs, we have a switch
 -if [ ${MODEL} = 'x86_64' -o ${MODEL} = 'ppc64' -o ${MODEL} = 'ia64' -o ${MODEL} = 's390x' ]; then
 +if [ ${MODEL} = 'x86_64' -o ${MODEL} = 'ppc64' -o ${MODEL} = 'ia64' -o ${MODEL} = 's390x' -o ${MODEL} = 'amd64' ]; then
- 	SWT_PTR_CFLAGS=-DSWT_PTR_SIZE_64
+ 	SWT_PTR_CFLAGS=-DJNI64
  	export SWT_PTR_CFLAGS
  	if [ -d /lib64 ]; then
 -		XLIB64=-L/usr/X11R6/lib64
@@ -19,7 +29,7 @@
  if [ x`pkg-config --exists gnome-vfs-module-2.0 libgnome-2.0 libgnomeui-2.0 && echo YES` = "xYES" ]; then
  	echo "libgnomeui-2.0 found, compiling SWT program support using GNOME"
  	MAKE_GNOME=make_gnome
-@@ -81,7 +82,9 @@
+@@ -91,7 +95,9 @@
  	echo "libgnome-2.0 and libgnomeui-2.0 not found:"
  	echo "    *** SWT Program support for GNOME will not be compiled."
  fi
@@ -29,21 +39,44 @@
  if [ x`pkg-config --exists cairo && echo YES` = "xYES" ]; then
  	echo "Cairo found, compiling SWT support for the cairo graphics library."
  	MAKE_CAIRO=make_cairo
-@@ -89,7 +92,9 @@
+@@ -99,30 +105,17 @@
  	echo "Cairo not found:"
  	echo "    *** Advanced graphics support using cairo will not be compiled."
  fi
 +fi
  
-+if [ x${MAKE_MOZILLA} = "xmake_mozilla" ]; then
- if [ -z "${MOZILLA_INCLUDES}" -a -z "${MOZILLA_LIBS}" ]; then
- 	if [ x`pkg-config --exists mozilla-xpcom && echo YES` = "xYES" ]; then
- 		MOZILLA_INCLUDES=`pkg-config --cflags mozilla-xpcom`
-@@ -107,6 +112,7 @@
- 		echo "Mozilla/XPCOM or Firefox/XPCOM libraries not found:"
- 		echo "    *** Mozilla embedding support will not be compiled."
- 	fi
-+fi
+-if [ -z "${MOZILLA_INCLUDES}" -a -z "${MOZILLA_LIBS}" ]; then
+-	if [ x`pkg-config --exists mozilla-xpcom && echo YES` = "xYES" ]; then
+-		MOZILLA_INCLUDES=`pkg-config --cflags mozilla-xpcom`
+-		MOZILLA_LIBS=`pkg-config --libs mozilla-xpcom`
+-		export MOZILLA_INCLUDES
+-		export MOZILLA_LIBS
+-		MAKE_MOZILLA=make_mozilla
+-	elif [ x`pkg-config --exists firefox-xpcom && echo YES` = "xYES" ]; then
+-		MOZILLA_INCLUDES=`pkg-config --cflags firefox-xpcom`
+-		MOZILLA_LIBS=`pkg-config --libs firefox-xpcom`
+-		export MOZILLA_INCLUDES
+-		export MOZILLA_LIBS
+-		MAKE_MOZILLA=make_mozilla
+-	elif [ x`pkg-config --exists libxul && echo YES` = "xYES" ]; then
+-		XULRUNNER_INCLUDES=`pkg-config --cflags libxul`
+-		XULRUNNER_LIBS=`pkg-config --libs libxul`
+-		export XULRUNNER_INCLUDES
+-		export XULRUNNER_LIBS
+-		MAKE_MOZILLA=make_xulrunner
+-	else
+-		echo "None of the following libraries were found:  Mozilla/XPCOM, Firefox/XPCOM, or XULRunner/XPCOM"
+-		echo "    *** Mozilla embedding support will not be compiled."
+-	fi
++if [ x${MAKE_MOZILLA} = "xmake_xulrunner" ]; then
++	echo "Using libxul for gecko support"
++	XULRUNNER_INCLUDES=`pkg-config --cflags libxul libxul-embedding`
++	XULRUNNER_LIBS=`pkg-config --libs libxul libxul-embedding`
++	export XULRUNNER_INCLUDES
++	export XULRUNNER_LIBS
++else
++	echo "None of the following libraries were found:  XULRunner/XPCOM"
++	echo "    *** Mozilla embedding support will not be compiled."
  fi
  
  # Find AWT if available
