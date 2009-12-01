3aa37c5febd09b10d14b5691fe57ecd5e3a934a1
amdmi3 <amdmi3@FreeBSD.org>
2009-12-01 22:12:57 +0000

    - Update to 20091108

--- games/linwarrior/files/extra-patch-source-cMech.cpp
+++ games/linwarrior/files/extra-patch-source-cMech.cpp
@@ -1,6 +1,6 @@
---- source/cMech.cpp.orig	2009-05-23 21:01:21.000000000 +0400
-+++ source/cMech.cpp	2009-06-08 18:39:11.000000000 +0400
-@@ -1756,9 +1756,14 @@
+--- source/cMech.cpp.orig	2009-11-08 16:18:05.000000000 +0300
++++ source/cMech.cpp	2009-11-12 07:03:04.000000000 +0300
+@@ -1494,9 +1494,15 @@
          // Set XYZ to Not-A-Number (NaN) for no location.
          // Note that NaN-ity can only be tested either by
          // isnanf(x), !finite(x) or by x!=x as NaN always != NaN.
@@ -12,9 +12,10 @@
 +			uint32_t bits[1];
 +		} u;
 +		u.bits[0] = 0x7fc00000;
-+        mDestination[0] = u.f;
-+        mDestination[1] = u.f;
-+        mDestination[2] = u.f;
-         //printf("Destination is %s\n", (finitef(mDestination[0])) ? "finite" : "infinite" );
++		mDestination[0] = u.f;
++		mDestination[1] = u.f;
++		mDestination[2] = u.f;
++
+         //cout << "Destination is " << ((finitef(mDestination[0])) ? "finite" : "infinite" ) << "\n";
      }
  }
