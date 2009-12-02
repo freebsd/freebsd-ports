855479b39693781981254235f6b6e5541f770860
Martin Matuska <mm@FreeBSD.org>
2009-12-02 18:54:03 +0000

    - Update to 6.5.7-10

--- graphics/ImageMagick/files/patch-tests-validate.h
+++ graphics/ImageMagick/files/patch-tests-validate.h
@@ -1,10 +1,10 @@
---- tests/validate.h.orig	2009-03-04 17:05:46.000000000 +0100
-+++ tests/validate.h	2009-03-04 17:06:04.000000000 +0100
-@@ -281,7 +281,6 @@
-     { "MIFF", 0.0 },
-     { "MNG", 0.0 },
-     { "MONO", 0.0 },
--    { "MOV", 0.0 },
-     { "MRW", 0.0 },
-     { "MTV", 0.0 },
-     { "NEF", 0.0 },
+--- tests/validate.h.orig	2009-10-09 19:57:50.000000000 +0200
++++ tests/validate.h	2009-12-02 17:35:09.784675288 +0100
+@@ -282,7 +282,6 @@
+     { "MIFF", UndefinedCompression, 0.0 },
+     { "MNG", UndefinedCompression, 0.0 },
+     { "MONO", UndefinedCompression, 0.0 },
+-    { "MOV", UndefinedCompression, 0.0 },
+     { "MRW", UndefinedCompression, 0.0 },
+     { "MTV", UndefinedCompression, 0.0 },
+     { "NEF", UndefinedCompression, 0.0 },
