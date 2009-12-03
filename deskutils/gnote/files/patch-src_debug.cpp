2e19dace3f96e309f0675ec63ba2d99d5030a323
Koop Mast <kwm@FreeBSD.org>
2009-12-03 18:47:46 +0000

    Fix build on amd64 and fix the plist while here.
    Submitted by:	pointyhat (via pav) & QAT

---
+++ deskutils/gnote/files/patch-src_debug.cpp
@@ -0,0 +1,11 @@
+--- src/debug.cpp.orig	2009-11-25 14:12:33.000000000 +0100
++++ src/debug.cpp	2009-12-03 17:28:54.000000000 +0100
+@@ -126,7 +126,7 @@
+ //    static boost::recursive_mutex mutex;
+ //    boost::recursive_mutex::scoped_lock lock(mutex);
+     char buf[128];
+-    snprintf(buf, 128, "(%d) ", (int)pthread_self());
++    snprintf(buf, 128, "(%d) ", (pthread_t)pthread_self());
+     fwrite(buf, 1, strlen(buf), stderr);
+     fwrite(prefix, 1, strlen(prefix), stderr);
+ 
