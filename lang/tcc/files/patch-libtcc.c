ec7129c79b30fd3674c1ed827b746c36a56f6998
luigi <luigi@FreeBSD.org>
2009-12-06 15:08:37 +0000

    Update files/README.tccelf files/patch-libtcc.c to reflect some fixes i made recently. Add files/patch-z1-preproc which implements -include, -M and some related preprocessing macros to improve portability Bump portrevision accordingly
    Should close PR/141185

--- lang/tcc/files/patch-libtcc.c
+++ lang/tcc/files/patch-libtcc.c
@@ -24,28 +24,34 @@ diff -ubwr ./libtcc.c ../../work.2/tcc-0.9.25/libtcc.c
      tcc_define_symbol(s, "__STDC__", NULL);
      tcc_define_symbol(s, "__STDC_VERSION__", "199901L");
  #if defined(TCC_TARGET_I386)
--    tcc_define_symbol(s, "__i386__", NULL);
-+    tcc_define_symbol(s, "__i386__", "1");
-+    tcc_define_symbol(s, "__i386", "1");
-+    tcc_define_symbol(s, "i386", "1");
+     tcc_define_symbol(s, "__i386__", NULL);
++    tcc_define_symbol(s, "__i386", NULL); /* NULL defaults to "1" */
++    tcc_define_symbol(s, "i386", NULL);
  #endif
  #if defined(TCC_TARGET_X86_64)
      tcc_define_symbol(s, "__x86_64__", NULL);
-@@ -1802,8 +1812,15 @@
- #ifdef TCC_TARGET_PE
-     tcc_define_symbol(s, "_WIN32", NULL);
+@@ -1802,6 +1812,14 @@
  #else
--    tcc_define_symbol(s, "__unix__", NULL);
--    tcc_define_symbol(s, "__unix", NULL);
-+    tcc_define_symbol(s, "__unix__", "1");
-+    tcc_define_symbol(s, "__unix", "1");
-+    tcc_define_symbol(s, "unix", "1");
+     tcc_define_symbol(s, "__unix__", NULL);
+     tcc_define_symbol(s, "__unix", NULL);
++    tcc_define_symbol(s, "unix", NULL);
 +#if defined(__FreeBSD__)
 +#define str(s) #s
 +    tcc_define_symbol(s, "__FreeBSD__", str( __FreeBSD__));
-+    tcc_define_symbol(s, "__INTEL_COMPILER", "1");
 +#undef str
++    tcc_define_symbol(s, "__aligned(x)", "__attribute__((__aligned__(x)))");
++    tcc_define_symbol(s, "__packed", "__attribute__((packed))");
 +#endif
  #if defined(__linux)
      tcc_define_symbol(s, "__linux__", NULL);
      tcc_define_symbol(s, "__linux", NULL);
+@@ -1834,6 +1841,9 @@
+     tcc_define_symbol(s, "__TINYC__", NULL);
+
+     /* tiny C & gcc defines */
++    tcc_define_symbol(s, "__CC_SUPPORTS___INLINE", NULL);
++    tcc_define_symbol(s, "__builtin_inline(x,y)", "(x)");
++    tcc_define_symbol(s, "__PRETTY_FUNCTION__", "__FUNCTION__");
+     tcc_define_symbol(s, "__SIZE_TYPE__", "unsigned int");
+     tcc_define_symbol(s, "__PTRDIFF_TYPE__", "int");
+ #ifdef TCC_TARGET_PE
