458d27eb576d599777d0254b6ca7aa3168b31ba3
marcus <marcus@FreeBSD.org>
2009-12-06 23:00:44 +0000

    Fix the language list in GDM.
    PR:		141136
    Submitted by:	MOROHOSHI Akihiko <moro@remus.dti.ne.jp>

--- x11/gdm/files/patch-gui_simple-greeter_gdm-languages.c
+++ x11/gdm/files/patch-gui_simple-greeter_gdm-languages.c
@@ -16,15 +16,6 @@
          }
  
          if (modifierp != NULL) {
-@@ -314,7 +314,7 @@ language_name_has_translations (const ch
-         const char  *name;
-         gboolean     has_translations;
- 
--        path = g_build_filename (GNOMELOCALEDIR, language_name, "LC_MESSAGES", NULL);
-+        path = g_build_filename ("/usr/share/locale", language_name, "LC_MESSAGES", NULL);
- 
-         has_translations = FALSE;
-         dir = g_dir_open (path, 0, NULL);
 @@ -352,7 +352,7 @@ add_locale (const char *language_name)
          if (language_name_is_utf8 (language_name)) {
                  name = g_strdup (language_name);
@@ -34,12 +25,3 @@
  
                  if (!language_name_is_utf8 (name)) {
                          g_free (name);
-@@ -501,7 +501,7 @@ collect_locales_from_directory (void)
-         int             ndirents;
-         int             cnt;
- 
--        ndirents = scandir (LIBLOCALEDIR, &dirents, select_dirs, alphasort);
-+        ndirents = scandir ("/usr/share/locale", &dirents, select_dirs, alphasort);
- 
-         for (cnt = 0; cnt < ndirents; ++cnt) {
-                 add_locale (dirents[cnt]->d_name);
