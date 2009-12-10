9a29a3542961b6a79f97b6cae7a3ae7e1e88ec4e
Pietro Cerutti <gahr@FreeBSD.org>
2009-12-10 08:28:54 +0000

    - Update to 2.0.3365

--- editors/codelite/files/patch-sdk-codelite_indexer_utils.cpp
+++ editors/codelite/files/patch-sdk-codelite_indexer_utils.cpp
@@ -1,10 +1,9 @@
---- sdk/codelite_indexer/utils.cpp.orig	2009-09-15 08:00:45.000000000 +0200
-+++ sdk/codelite_indexer/utils.cpp	2009-11-19 16:00:54.000000000 +0100
-@@ -8,6 +8,16 @@
- # include <process.h>
- # include <Tlhelp32.h>
+--- sdk/codelite_indexer/utils.cpp.orig	2009-12-07 16:27:20.000000000 +0100
++++ sdk/codelite_indexer/utils.cpp	2009-12-07 16:26:55.000000000 +0100
+@@ -11,6 +11,15 @@
+ #    include <signal.h>
  #endif
-+
+ 
 +#ifdef __FreeBSD__
 +#include <fcntl.h>
 +#include <paths.h>
