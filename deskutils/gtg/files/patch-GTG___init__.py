9965937b4c93fa19f766d6498e8a076b02970f5c
amdmi3 <amdmi3@FreeBSD.org>
2009-12-11 02:22:31 +0000

    - Update to 0.1.99
    PR:		141349
    Submitted by:	Ruslan Mahmatkhanov <cvs-src@yandex.ru> (maintainer)

--- deskutils/gtg/files/patch-GTG___init__.py
+++ deskutils/gtg/files/patch-GTG___init__.py
@@ -1,14 +1,5 @@
 --- GTG/__init__.py.orig	2009-04-04 01:00:16.000000000 +0400
 +++ GTG/__init__.py	2009-06-17 12:59:18.000000000 +0400
-@@ -32,7 +32,7 @@
- EMAIL           = "gtg@lists.launchpad.net"
- VERSION         = '0.1.1'
- LOCAL_ROOTDIR   = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) 
--DIST_ROOTDIR    = "/usr/share/gtg"
-+DIST_ROOTDIR    = "/usr/local/share/gtg"
- 
- # CREDITS
- AUTHORS     = ["Main developers:", \
 @@ -60,7 +60,7 @@
  GETTEXT_DOMAIN = 'gtg'
  LOCALE_PATH = abspath(join(dirname(__file__), pardir, 'locales'))
