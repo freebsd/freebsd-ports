0cfa50271171c9e0d5f4022d1148913afd698035
Beat Gaetzi <beat@FreeBSD.org>
2009-12-14 15:35:18 +0000

    - Update to 3.0
    Some of the new features:
    
      * Thunderbird 3 is available in 49 languages - get your local version.
      * Tabbed Email.
      * The new search experience is much faster, and new search tools will
        help you narrow down your search.
      * A new Message Archive folder system will help you organize your email.
      * Lots of user experience improvements: main toolbar has been simplified,
        new search widget, new Mail Account Wizard, Smart Folders, Activity
        Manager, one-click Address Book, Attachment Reminder.
    
    Thanks to:	 Andreas Tobler, Florian Smeets, miwi@

---
+++ mail/thunderbird3/files/patch-mozilla-security-coreconf-FreeBSD.mk
@@ -0,0 +1,38 @@
+--- mozilla/security/coreconf/FreeBSD.mk.orig	2009-09-16 02:57:02.000000000 +0200
++++ mozilla/security/coreconf/FreeBSD.mk	2009-09-30 16:44:43.000000000 +0200
+@@ -45,8 +45,20 @@
+ ifeq ($(OS_TEST),alpha)
+ CPU_ARCH		= alpha
+ else
++ifeq ($(OS_TEST),amd64)
++CPU_ARCH		= amd64
++else
++ifeq ($(OS_TEST),powerpc)
++CPU_ARCH		= powerpc
++else
++ifeq ($(OS_TEST),sparc64)
++CPU_ARCH		= sparc64
++else
+ CPU_ARCH		= x86
+ endif
++endif
++endif
++endif
+ 
+ OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -Wno-switch -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
+ 
+@@ -73,7 +85,7 @@
+ DLL_SUFFIX		= so.1.0
+ endif
+ 
+-MKSHLIB			= $(CC) $(DSO_LDOPTS)
++MKSHLIB			= $(CC) -Wl,-Bsymbolic -lc $(DSO_LDOPTS) -o $@
+ ifdef MAPFILE
+ 	MKSHLIB += -Wl,--version-script,$(MAPFILE)
+ endif
+@@ -82,4 +94,4 @@
+ 
+ G++INCLUDES		= -I/usr/include/g++
+ 
+-INCLUDES		+= -I/usr/X11R6/include
++#INCLUDES		+= -I/usr/local/include
---
+++ mail/thunderbird3/files/patch-mozilla-toolkit-mozapps-installer-packager.mk
@@ -0,0 +1,11 @@
+--- mozilla/toolkit/mozapps/installer/packager.mk.orig	2007-09-26 13:45:01.000000000 -0400
++++ mozilla/toolkit/mozapps/installer/packager.mk	2007-09-26 13:47:29.000000000 -0400
+@@ -409,7 +409,7 @@
+ 	(cd $(DIST)/sdk/include && tar $(TAR_CREATE_FLAGS) - .) | \
+ 	  (cd $(DESTDIR)$(includedir)/stable && tar -xf -)
+ # The dist/include has module subdirectories that we need to flatten
+-	find $(DIST)/include -xtype f -exec $(SYSINSTALL) $(IFLAGS1) {} $(DESTDIR)$(includedir)/unstable \;
++	find $(DIST)/include -type f -o -type l -exec $(SYSINSTALL) $(IFLAGS1) {} $(DESTDIR)$(includedir)/unstable \;
+ # IDL directory is stable (dist/sdk/idl) and unstable (dist/idl)
+ 	$(NSINSTALL) -D $(DESTDIR)$(idldir)/stable 
+ 	$(NSINSTALL) -D $(DESTDIR)$(idldir)/unstable
