a2eaf53e00751c5f38a89c90d94024927cad8da2
garga <garga@FreeBSD.org>
2009-12-15 10:07:48 +0000

    Update to 0.99.19

--- editors/jed/files/patch-src_misc.c
+++ editors/jed/files/patch-src_misc.c
@@ -1,11 +1,11 @@
---- src/misc.c.orig	Mon Apr  3 09:05:53 2006
-+++ src/misc.c	Mon Apr  3 09:05:05 2006
-@@ -741,12 +741,14 @@
+--- src/misc.c.orig	2009-12-14 00:12:55.000000000 -0200
++++ src/misc.c	2009-12-14 15:12:53.000000000 -0200
+@@ -769,12 +769,14 @@
  
  void jed_ungetkey_wchar (SLwchar_Type wc)
  {
 +#if JED_HAS_UTF8_SUPPORT
-    SLuchar_Type *b, buf[SLUTF8_MAX_MBLEN];
+    SLuchar_Type *b, buf[JED_MAX_MULTIBYTE_SIZE];
  
     if (NULL == (b = jed_wchar_to_multibyte (wc, buf)))
       return;
