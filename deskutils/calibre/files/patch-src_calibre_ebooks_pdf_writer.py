8b9a36bc2190b1e1c1063756c28b8db4c0272401
Stanislav Sedov <stas@FreeBSD.org>
2009-12-15 02:40:17 +0000

    - Update to 0.6.27.

--- deskutils/calibre/files/patch-src_calibre_ebooks_pdf_writer.py
+++ deskutils/calibre/files/patch-src_calibre_ebooks_pdf_writer.py
@@ -1,9 +1,9 @@
---- src/calibre/ebooks/pdf/writer.py.orig	2009-10-11 23:06:22.000000000 +0400
-+++ src/calibre/ebooks/pdf/writer.py	2009-10-11 23:18:59.000000000 +0400
+--- src/calibre/ebooks/pdf/writer.py.orig	2009-12-11 15:04:41.000000000 -0800
++++ src/calibre/ebooks/pdf/writer.py	2009-12-13 16:45:34.000000000 -0800
 @@ -19,7 +19,7 @@
  from PyQt4 import QtCore
  from PyQt4.Qt import QUrl, QEventLoop, SIGNAL, QObject, \
-     QApplication, QPrinter, QMetaObject, QSizeF, Qt
+     QPrinter, QMetaObject, QSizeF, Qt
 -from PyQt4.QtWebKit import QWebView
 +from PyQt4.QtWebKit import *
  
--- deskutils/calibre/files/patch-src_calibre_linux.py
+++ deskutils/calibre/files/patch-src_calibre_linux.py
@@ -1,6 +1,6 @@
---- src/calibre/linux.py.orig	2009-10-10 06:15:01.000000000 +0400
-+++ src/calibre/linux.py	2009-10-12 14:10:34.000000000 +0400
-@@ -128,10 +128,7 @@
+--- src/calibre/linux.py.orig	2009-12-11 15:04:41.000000000 -0800
++++ src/calibre/linux.py	2009-12-14 17:29:52.000000000 -0800
+@@ -128,20 +128,12 @@
          self.icon_resources = []
          self.menu_resources = []
          self.mime_resources = []
@@ -11,16 +11,17 @@
          self.create_uninstaller()
  
          from calibre.utils.config import config_dir
-@@ -140,8 +137,6 @@
-             for f in os.listdir('.'):
-                 if os.stat(f).st_uid == 0:
-                     os.rmdir(f) if os.path.isdir(f) else os.unlink(f)
--        if os.stat(config_dir).st_uid == 0:
--            os.rmdir(config_dir)
+         if os.path.exists(config_dir):
+             os.chdir(config_dir)
+-            for f in os.listdir('.'):
+-                if os.stat(f).st_uid == 0:
+-                    os.rmdir(f) if os.path.isdir(f) else os.unlink(f)
+-            if os.stat(config_dir).st_uid == 0:
+-                os.rmdir(config_dir)
  
          if warn is None and self.warnings:
              self.info('There were %d warnings'%len(self.warnings))
-@@ -319,7 +314,7 @@
+@@ -318,7 +310,7 @@
      def install_man_pages(self):
          try:
              from calibre.utils.help2man import create_man_page
@@ -29,7 +30,7 @@
              if not os.path.exists(manpath):
                  os.makedirs(manpath)
              self.info('Installing MAN pages...')
-@@ -332,7 +327,7 @@
+@@ -331,7 +323,7 @@
                      continue
                  parser = parser()
                  raw = create_man_page(prog, parser)
