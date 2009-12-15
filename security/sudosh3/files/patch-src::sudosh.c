ae94719fd664f1a8f6598164ebf3fe9fd2fe2675
cy <cy@FreeBSD.org>
2009-12-15 07:17:46 +0000

    Welcome sudosh3 to the ports collection.

--- security/sudosh3/files/patch-src::sudosh.c
+++ security/sudosh3/files/patch-src::sudosh.c
@@ -1,6 +1,6 @@
---- src/sudosh.c.orig	Sun Jun 12 19:35:07 2005
-+++ src/sudosh.c	Fri Jan  5 14:14:05 2007
-@@ -28,6 +28,13 @@
+--- src/sudosh.c.orig	2009-11-27 02:19:58.000000000 -0800
++++ src/sudosh.c	2009-12-14 17:30:23.000000000 -0800
+@@ -27,6 +27,13 @@
  
  #define WRITE(a, b, c) do_write(a, b, c, __FILE__, __LINE__)
  
@@ -11,71 +11,60 @@
 +#include <sys/param.h>
 +#endif
 +
- static struct termios termorig;
- static struct winsize winorig;
+ typedef enum {false=0, true=1} bool;
  
-@@ -545,19 +552,43 @@
+ static struct termios termorig;
+@@ -443,12 +450,32 @@
  {
-   char *sname;
+ 	char *sname;
  
 +#ifdef __FreeBSD__
 +#define PTYLEN        16
-+  char sname_area[PTYLEN];
-+  struct termios tt;
-+  struct winsize win;
++	char sname_area[PTYLEN];
++	struct termios tt;
++	struct winsize win;
 +
-+  sname = sname_area;
-+  if (tcgetattr(STDIN_FILENO, &tt) == -1)
-+    {
-+      perror ("tcgetattr");
-+      return -1;
-+    }
-+  if (ioctl(STDIN_FILENO, TIOCGWINSZ, &win) == -1)
-+    {
-+      perror ("ioctl");
-+      return -1;
-+    }
-+    if (openpty(&p->mfd, &p->sfd, sname, &tt, &win) == -1) {
++	sname = sname_area;
++	if (tcgetattr(STDIN_FILENO, &tt) == -1) {
++		perror ("tcgetattr");
++		return -1;
++	}
++	if (ioctl(STDIN_FILENO, TIOCGWINSZ, &win) == -1) {
++		perror ("ioctl");
++		return -1;
++	}
++	if (openpty(&p->mfd, &p->sfd, sname, &tt, &win) == -1) {
 +#else
-   if ((p->mfd = open ("/dev/ptmx", O_RDWR)) == -1)
-     {
-       if ((p->mfd = open ("/dev/ptc", O_RDWR)) == -1)
- 	{
+ 	if ((p->mfd = open ("/dev/ptmx", O_RDWR)) == -1) {
+ 
+ 		if ((p->mfd = open ("/dev/ptc", O_RDWR)) == -1) {
 +#endif
- 	  perror ("Cannot open cloning master pty");
- 	  return -1;
+ 			perror ("Cannot open cloning master pty");
+ 			return -1;
 +#ifndef __FreeBSD__
+ 		}
++#endif
  	}
-+#endif
-     }
  
-+#if !defined(__FreeBSD_version) || (defined(__FreeBSD_version) && __FreeBSD_version >= 500000)
-   (void) unlockpt (p->mfd);
-   (void) grantpt (p->mfd);
- 
-   sname = (char *) ptsname (p->mfd);
-+#endif
- 
-   if ((p->sfd = open (sname, O_RDWR)) == -1)
-     {
-@@ -619,9 +650,14 @@
-   for (i = 3; i < 100; ++i)
-     close (i);
+ 	(void) unlockpt (p->mfd);
+@@ -515,9 +542,14 @@
+ 	for (i = 3; i < 100; ++i)
+ 		close (i);
  
 +#ifdef __FreeBSD__
-+  (void) tcsetattr(0, TCSADRAIN, &termorig);
-+  (void) login_tty(pst->sfd);
++	(void) tcsetattr(0, TCSADRAIN, &termorig);
++	(void) login_tty(pst->sfd);
 +#else
  #ifdef TCSETS
-   (void) ioctl (0, TCSETS, &termorig);
+ 	(void) ioctl (0, TCSETS, &termorig);
  #endif
 +#endif
-   (void) ioctl (0, TIOCSWINSZ, &winorig);
+ 	(void) ioctl (0, TIOCSWINSZ, &winorig);
  
-   setuid (getuid ());
-@@ -663,6 +699,13 @@
+ 	setuid (getuid ());
+@@ -671,12 +703,20 @@
  {
-   static struct termios termnew;
+ 	static struct termios termnew;
  
 +#ifdef __FreeBSD__
 +  if (tcgetattr(ttyfd, &termorig) == -1)
@@ -85,45 +74,43 @@
 +    }
 +#else
  #ifdef TCGETS
-   if (ioctl (ttyfd, TCGETS, &termorig) == -1)
-     {
-@@ -670,6 +713,7 @@
-       exit (EXIT_FAILURE);
-     }
+ 	if (ioctl (ttyfd, TCGETS, &termorig) == -1) {
+ 		perror ("ioctl TCGETS failed");
+ 		exit (EXIT_FAILURE);
+ 	}
  #endif
 +#endif
  
-   if (ioctl (ttyfd, TIOCGWINSZ, &winorig) == -1)
-     {
-@@ -677,6 +721,11 @@
-       exit (EXIT_FAILURE);
-     }
+ 	if (ioctl (ttyfd, TIOCGWINSZ, &winorig) == -1) {
+ 	//	perror ("ioctl TIOCGWINSZ failed");
+@@ -685,6 +725,11 @@
+ 		winorig.ws_col = 80;
+ 	}
  
 +#ifdef __FreeBSD__
-+  (void) cfmakeraw(&termnew);
-+  termnew.c_lflag &= ~ECHO;
-+  (void) tcsetattr(ttyfd, TCSAFLUSH, &termnew);
++	(void) cfmakeraw(&termnew);
++	termnew.c_lflag &= ~ECHO;
++	(void) tcsetattr(ttyfd, TCSAFLUSH, &termnew);
 +#else
-   termnew.c_cc[VEOF] = 1;
-   termnew.c_iflag = BRKINT | ISTRIP | IXON | IXANY;
-   termnew.c_oflag = 0;
-@@ -686,13 +735,19 @@
+ 	termnew.c_cc[VEOF] = 1;
+ 	termnew.c_iflag = BRKINT | ISTRIP | IXON | IXANY;
+ 	termnew.c_oflag = 0;
+@@ -694,13 +739,18 @@
  #ifdef TCSETS
-   (void) ioctl (ttyfd, TCSETS, &termnew);
+ 	(void) ioctl (ttyfd, TCSETS, &termnew);
  #endif
 +#endif
  }
  
- static void
- bye (int signum)
+ static void bye (int signum)
  {
-+
 +#ifdef __FreeBSD__
-+  (void) tcsetattr(0, TCSADRAIN, &termorig);
++	(void) tcsetattr(0, TCSADRAIN, &termorig);
 +#else
  #ifdef TCSETS
-   (void) ioctl (0, TCSETS, &termorig);
-+#endif
+ 	(void) ioctl (0, TCSETS, &termorig);
  #endif
++#endif
  
-   close (timing.fd);
+ 	close (timing.fd);
+ 	close (script.fd);
