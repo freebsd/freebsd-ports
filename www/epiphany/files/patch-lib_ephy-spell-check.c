cff040913b4052d5d7d779a7e90df171d8a16d3b
kwm <kwm@FreeBSD.org>
2009-12-18 11:34:35 +0000

    Fix patching.
    Submitted by:	QAT

--- www/epiphany/files/patch-lib_ephy-spell-check.c
+++ www/epiphany/files/patch-lib_ephy-spell-check.c
@@ -18,23 +18,3 @@
  }
  
  static void
---- lib/ephy-spell-check.c.orig	2008-03-17 00:41:47.000000000 -0400
-+++ lib/ephy-spell-check.c	2008-03-17 00:47:22.000000000 -0400
-@@ -69,8 +69,15 @@ ephy_spell_check_init (EphySpellCheck *s
- 		priv->dict = enchant_broker_request_dict (priv->broker, *locale);
- 		if (priv->dict != NULL) break;
- 	}
--	if (priv->dict == NULL)
--		g_warning (enchant_broker_get_error (priv->broker));
-+	if (priv->dict == NULL) {
-+		g_warning ("No dictionary found for current locale; trying 'en'");
-+		priv->dict = enchant_broker_request_dict (priv->broker, "en");
-+
-+		if (priv->dict == NULL) {
-+			g_warning (enchant_broker_get_error (priv->broker));
-+			g_warning ("No dictionary found; expect a crash until LANG is properly set");
-+		}
-+	}
- }
- 
- static void
