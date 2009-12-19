5e905f101b004140659d083c505a9aa45ed9a990
marcus <marcus@FreeBSD.org>
2009-12-19 19:55:26 +0000

    Fix build on 6.X.

--- devel/seed/files/patch-modules_os_seed-os.c
+++ devel/seed/files/patch-modules_os_seed-os.c
@@ -1,7 +1,12 @@
---- modules/os/seed-os.c.orig	2009-08-10 16:23:35.000000000 -0400
-+++ modules/os/seed-os.c	2009-08-22 14:43:42.000000000 -0400
-@@ -31,6 +31,10 @@
+--- modules/os/seed-os.c.orig	2009-10-07 01:43:54.000000000 -0400
++++ modules/os/seed-os.c	2009-12-19 14:53:48.000000000 -0500
+@@ -29,8 +29,15 @@
+ 
+ #include <sys/stat.h>
  #include <sys/utsname.h>
++#if defined(__FreeBSD__)
++#include <sys/param.h>
++#endif
  
  #include <sys/types.h>
 +#include <sys/ioctl.h>
@@ -11,7 +16,7 @@
  
  #include <fcntl.h>
  
-@@ -54,18 +58,32 @@ seed_os_realpath (SeedContext ctx,
+@@ -54,18 +61,32 @@ seed_os_realpath (SeedContext ctx,
  	          const SeedValue arguments[],
  	          SeedException * exception)
  {
@@ -46,7 +51,20 @@
  }
  
  SeedValue
-@@ -702,6 +720,7 @@ seed_os_fdatasync (SeedContext ctx,
+@@ -559,7 +580,12 @@ seed_os_unsetenv (SeedContext ctx,
+     }
+ 
+   arg = seed_value_to_string (ctx, arguments[0], exception);
++#if __FreeBSD_version < 700000
++  ret = 0;
++  unsetenv (arg);
++#else
+   ret = unsetenv (arg);
++#endif
+   g_free (arg);
+ 
+   return seed_value_from_int (ctx, ret, exception);
+@@ -702,6 +728,7 @@ seed_os_fdatasync (SeedContext ctx,
  		   const SeedValue arguments[],
  		   SeedException * exception)
  {
@@ -54,7 +72,7 @@
    gint fd;
  
    if (argument_count != 1)
-@@ -711,6 +730,10 @@ seed_os_fdatasync (SeedContext ctx,
+@@ -711,6 +738,10 @@ seed_os_fdatasync (SeedContext ctx,
    fd = seed_value_to_int (ctx, arguments[0], exception);
  
    return seed_value_from_int (ctx, fdatasync (fd), exception);
@@ -65,7 +83,7 @@
  }
  
  SeedValue
-@@ -1112,7 +1135,9 @@ seed_module_init(SeedEngine * eng)
+@@ -1112,7 +1143,9 @@ seed_module_init(SeedEngine * eng)
  #if defined (O_DIRECT)
    OS_DEFINE_QUICK_ENUM (O_DIRECT);
  #endif
