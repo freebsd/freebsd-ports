9ed853c43d3664aafac37b2a97b9d34365025beb
marcus <marcus@FreeBSD.org>
2009-12-19 20:23:35 +0000

    Use the gdb6 from ports to satisfy the gdb requirements of Anjuta.
    PR:		141780

---
+++ devel/anjuta/files/patch-plugins_gdb_debugger.c
@@ -0,0 +1,11 @@
+--- plugins/gdb/debugger.c.orig	2009-12-19 15:21:02.000000000 -0500
++++ plugins/gdb/debugger.c	2009-12-19 15:21:13.000000000 -0500
+@@ -54,7 +54,7 @@
+ 
+ #define GDB_PROMPT  "(gdb)"
+ #define FILE_BUFFER_SIZE 1024
+-#define GDB_PATH "gdb"
++#define GDB_PATH "gdb66"
+ #define SUMMARY_MAX_LENGTH   90	 /* Should be smaller than 4K to be displayed
+ 				  * in GtkCellRendererCell */
+ 
