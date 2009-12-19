5c6a1954ff61106d3f7bbf1938ae782cd459d368
Joe Marcus Clarke <marcus@FreeBSD.org>
2009-12-19 20:04:15 +0000

    Adapt a patch from metacity to fix the build on 6.X.

--- x11-wm/mutter/files/patch-src_core_main.c
+++ x11-wm/mutter/files/patch-src_core_main.c
@@ -1,5 +1,5 @@
---- src/core/main.c.orig	2009-08-14 00:40:56.000000000 +0000
-+++ src/core/main.c	2009-08-14 00:41:22.000000000 +0000
+--- src/core/main.c.orig	2009-08-25 13:49:41.000000000 -0400
++++ src/core/main.c	2009-12-19 15:03:26.000000000 -0500
 @@ -59,7 +59,11 @@
  
  #include <stdlib.h>
@@ -12,3 +12,13 @@
  #include <stdio.h>
  #include <string.h>
  #include <signal.h>
+@@ -445,6 +449,9 @@ sigterm_handler (int signum)
+ }
+ 
+ static guint sigchld_signal_id = 0;
++#ifndef CLD_EXITED
++#define CLD_EXITED 1
++#endif
+ 
+ static void
+ sigchld_handler (int signum, siginfo_t *info, void *context)
