ba8a0cfdfebc0227f55f5628318cf9fa71535c04
Cy Schubert <cy@FreeBSD.org>
2009-12-21 23:16:04 +0000

    Fix build under FreeBSD 9.0-CURRENT.
    PR:		141743

--- sysutils/screen/files/patch-utmp.c
+++ sysutils/screen/files/patch-utmp.c
@@ -1,6 +1,30 @@
---- utmp.c.orig	Mon Sep  8 16:27:17 2003
-+++ utmp.c	Wed Oct 19 14:58:40 2005
-@@ -726,9 +728,12 @@
+--- utmp.c.orig	2003-09-08 07:27:17.000000000 -0700
++++ utmp.c	2009-12-21 15:10:06.000000000 -0800
+@@ -21,6 +21,9 @@
+  ****************************************************************
+  */
+ 
++#ifdef __FreeBSD__
++#include <sys/param.h>
++#endif
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
+@@ -70,8 +73,13 @@
+  *  (won't work for login-slots)
+  */
+ #if (defined(sun) && defined(SVR4) && defined(GETUTENT)) || defined(HAVE_UTEMPTER)
++/* XXX FreeBSD-9 includes libutempter however if UTMP_HELPER is defined,
++       the build is broken due to utmpfd not being defined.
++*/
++#if defined(__FreeBSD_version) && __FreeBSD_version < 900004 || !defined(__FreeBSD_version)
+ # define UTMP_HELPER
+ #endif
++#endif
+ 
+ 
+ 
+@@ -726,9 +734,12 @@
  char *line, *user;
  int pid;
  {
@@ -14,4 +38,3 @@
  }
  
  static slot_t
-
