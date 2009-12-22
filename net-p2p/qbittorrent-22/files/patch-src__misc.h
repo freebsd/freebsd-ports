afc3d1ed905ee9f1d03bd3db238b60c41db5dea0
miwi <miwi@FreeBSD.org>
2009-12-22 11:25:40 +0000

    - Update to 2.0.2
    PR:		141819
    Submitted by:	Ports Fury

--- net-p2p/qbittorrent-22/files/patch-src__misc.h
+++ net-p2p/qbittorrent-22/files/patch-src__misc.h
@@ -1,11 +1,11 @@
---- src/misc.h.orig	2009-10-01 03:44:58.000000000 +0900
-+++ src/misc.h	2009-11-27 02:33:54.000000000 +0900
-@@ -43,7 +43,7 @@
- #include <QThread>
+--- src/misc.h.orig	2009-12-13 19:15:50.000000000 +0900
++++ src/misc.h	2009-12-17 00:34:12.000000000 +0900
+@@ -47,7 +47,7 @@
+ #include <boost/date_time/posix_time/conversion.hpp>
  
  #ifndef Q_WS_WIN
 -#ifdef Q_WS_MAC
 +#if defined(Q_WS_MAC) || defined(__FreeBSD__)
-   #include <sys/param.h>
-   #include <sys/mount.h>
+ #include <sys/param.h>
+ #include <sys/mount.h>
  #else
