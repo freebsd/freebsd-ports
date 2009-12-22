cbafaf051ac1a120aa5a1dd25843eb859aeab149
miwi <miwi@FreeBSD.org>
2009-12-22 11:25:49 +0000

    - Update to 2.4.2
    PR:		141821
    Submitted by:	Ports Fury

--- x11-toolkits/vdk/files/patch-include__vdk__FileDialog.h
+++ x11-toolkits/vdk/files/patch-include__vdk__FileDialog.h
@@ -1,5 +1,5 @@
---- vdk/FileDialog.h.orig	Sun Jun  5 20:03:15 2005
-+++ vdk/FileDialog.h	Sun Jun  5 20:03:31 2005
+--- include/vdk/FileDialog.h.orig	Sun Jun  5 20:03:15 2005
++++ include/vdk/FileDialog.h	Sun Jun  5 20:03:31 2005
 @@ -35,7 +35,9 @@
  #ifndef _USE_FNMATCH
  # include <regex.h>
