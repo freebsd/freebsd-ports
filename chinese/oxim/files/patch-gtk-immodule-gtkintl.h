4fba1d0651a191b9167da2f7efc1d70c5b46c209
Rong-En Fan <rafan@FreeBSD.org>
2009-12-27 07:19:03 +0000

    - Update to 1.2.2 which is compatible with libchewing >= 0.3.2 - oxim-setup is now removed

--- chinese/oxim/files/patch-gtk-immodule-gtkintl.h
+++ chinese/oxim/files/patch-gtk-immodule-gtkintl.h
@@ -1,11 +1,10 @@
---- src/gtk-immodule/gtkintl.h.orig	Thu Mar 23 08:43:55 2006
-+++ src/gtk-immodule/gtkintl.h	Thu Mar 23 08:46:33 2006
-@@ -1,7 +1,7 @@
+--- src/gtk-immodule/gtkintl.h.orig	2009-01-07 11:32:19.000000000 +0800
++++ src/gtk-immodule/gtkintl.h	2009-12-27 14:43:18.000000000 +0800
+@@ -1,6 +1,6 @@
  #ifndef __GTKINTL_H__
  #define __GTKINTL_H__
- #define ENABLE_NLS 1
 -#define GTK_LOCALEDIR "/usr/share/locale"
 +#define GTK_LOCALEDIR "%%PREFIX%%/share/locale"
  #define GETTEXT_PACKAGE "gtk20"
  
- #ifdef ENABLE_NLS
+ #include <langinfo.h>
