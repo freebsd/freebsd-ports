907a0f5ce63437783a96bffebdc2ecd8c534e780
ehaupt <ehaupt@FreeBSD.org>
2009-12-28 13:50:13 +0000

    Update to 1.3.2

--- net/httping/files/extra-patch-str.c
+++ net/httping/files/extra-patch-str.c
@@ -1,11 +1,11 @@
---- ./str.c.orig	2009-01-27 21:44:56.621283000 +0100
-+++ ./str.c	2009-01-27 21:45:24.936283000 +0100
+--- ./str.c.orig	2009-12-28 14:41:29.000000000 +0100
++++ ./str.c	2009-12-28 14:41:55.000000000 +0100
 @@ -18,6 +18,7 @@
  #include "mem.h"
  #include "utils.h"
  
 +/*
- #ifndef strndup
+ #ifndef _GNU_SOURCE
  char *strndup(char *in, int size)
  {
 @@ -29,3 +30,4 @@
