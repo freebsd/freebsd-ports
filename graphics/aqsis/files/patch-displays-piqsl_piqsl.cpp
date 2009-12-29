e57418f6ae5c83f870a00bb9e313fbaceb9a54b5
pav <pav@FreeBSD.org>
2009-12-29 13:50:46 +0000

    - Fix build with 1.41

--- graphics/aqsis/files/patch-displays-piqsl_piqsl.cpp
+++ graphics/aqsis/files/patch-displays-piqsl_piqsl.cpp
@@ -1,5 +1,5 @@
---- displays/piqsl/piqsl.cpp.orig	2009-04-01 00:14:24.000000000 +0200
-+++ displays/piqsl/piqsl.cpp	2009-04-01 00:13:48.000000000 +0200
+--- displays/piqsl/piqsl.cpp.orig	2009-01-22 00:12:32.000000000 +0300
++++ displays/piqsl/piqsl.cpp	2009-11-23 05:15:05.000000000 +0300
 @@ -29,6 +29,7 @@
  #define WIN32_LEAN_AND_MEAN
  #endif
@@ -8,3 +8,12 @@
  #include	<tiffio.h>
  #include	<string>
  #include	<list>
+@@ -39,7 +40,7 @@
+ #include	<boost/archive/iterators/transform_width.hpp>
+ #include	<boost/archive/iterators/remove_whitespace.hpp>
+ 
+-#include	<boost/pfto.hpp>
++#include	<boost/serialization/pfto.hpp>
+ #ifndef	AQSIS_SYSTEM_WIN32
+ #include	<sys/types.h>
+ #include	<sys/socket.h>
