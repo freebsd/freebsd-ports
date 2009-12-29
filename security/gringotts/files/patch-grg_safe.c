c2dfe5d488778fef89d562aaaf7d1e6b1350b973
miwi <miwi@FreeBSD.org>
2009-12-29 11:15:11 +0000

    - Update to 1.2.10
    PR:		142043
    Submitted by:	Ports Fury

--- security/gringotts/files/patch-grg_safe.c
+++ security/gringotts/files/patch-grg_safe.c
@@ -1,34 +1,35 @@
---- src/grg_safe.c.orig	Wed May 14 02:02:13 2003
-+++ src/grg_safe.c	Wed May 14 02:03:43 2003
+--- src/grg_safe.c.orig	2008-01-04 22:08:43.000000000 +0900
++++ src/grg_safe.c	2009-12-22 04:53:36.000000000 +0900
 @@ -33,6 +33,7 @@
  
  #include <stdlib.h>
  #include <string.h>
 +#include <sys/types.h>
+ #include <errno.h>
  #include <regex.h>
  #include <unistd.h>
- #include <fcntl.h>
-@@ -43,7 +44,6 @@
+@@ -44,7 +45,6 @@
  #include <sys/fsuid.h>
  #endif
  #include <sys/time.h>
 -#include <sys/types.h>
  #include <sys/resource.h>
- #include <sys/stat.h>
- 
-@@ -59,8 +59,6 @@
+ #ifdef linux
+ #include <sys/utsname.h>
+@@ -96,9 +96,6 @@
  grg_mlockall_and_drop_root_privileges(void)
  {
-     // drop eventual group root privileges
+     /* drop eventual group root privileges */
 -    setgid(getgid());
--    setgid(getgid());		// twice for counter "saved IDs", cfr.
- 				// Secure Programming HowTo
+-    setgid(getgid());		/* twice for counter "saved IDs", cfr. */
+-				/* Secure Programming HowTo */
  #ifdef HAVE_SYS_FSUID_H
      setfsgid(getgid());
-@@ -84,8 +82,6 @@
+     setfsgid(getgid());
+@@ -148,8 +145,6 @@
  #endif
  
- 	// drop root privileges
+ 	/* drop root privileges */
 -	setuid(getuid());
 -	setuid(getuid());
  #ifdef HAVE_SYS_FSUID_H
