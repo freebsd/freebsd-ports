01e0fec27cfb2cb9f964b45c4905a3fa2139bf61
Martin Wilke <miwi@FreeBSD.org>
2009-12-29 11:18:39 +0000

    - Update to 0.36.1
    PR:		142036
    Submitted by:	Ports Fury

--- deskutils/gdesklets/files/patch-libdesklets-system-gtop.c
+++ deskutils/gdesklets/files/patch-libdesklets-system-gtop.c
@@ -1,21 +1,10 @@
---- libdesklets/system/gtop.c.orig	Sat Jan  1 20:33:47 2005
-+++ libdesklets/system/gtop.c	Thu Feb 17 21:53:53 2005
-@@ -48,6 +48,8 @@
+--- libdesklets/system/gtop.c.orig	2008-03-20 08:00:58.000000000 +0900
++++ libdesklets/system/gtop.c	2009-12-23 01:21:26.000000000 +0900
+@@ -30,6 +30,7 @@
+ 
+ #include <sys/types.h>
  #include <sys/socket.h>
++#include <netinet/in.h>
  #include <arpa/inet.h>
  
-+/* for INET_ADDRSTRLEN and INET6_ADDRSTRLEN on FreeBSD 4 */
-+#include <netinet/in.h>
  
- typedef struct
- {
-@@ -1275,8 +1277,8 @@
-         tuple = PyTuple_New(len);
-
-         for(i = 0; i < len; ++i) {
--            p = &glibtop_sys_siglist[i];
-             PyObject* d;
-+            p = &glibtop_sys_siglist[i];
-
-             d = PyDict_New();
-             my_dict_add_and_decref(d, "number", PyI_L(p->number));
