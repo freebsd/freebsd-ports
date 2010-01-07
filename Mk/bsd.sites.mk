ed1f86b1af1de00b53c31ead7f309513ef2bbc9f
pgollucci <pgollucci@FreeBSD.org>
2010-01-07 04:49:40 +0000

    The Java front-end for lang/gcc42 downloads a DISTFILE from MASTER_SITE_SOURCEWARE. There are only 2 sites defined as sourceware.org mirrors, and some
    PR:             ports/142393
    Submitted by:   glarkin
    Tested by:      several p6 tb runs

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -1184,7 +1184,11 @@ MASTER_SITE_SOURCEFORGE_JP+= \
 .if !defined(IGNORE_MASTER_SITE_SOURCEWARE)
 MASTER_SITE_SOURCEWARE+= \
 	ftp://ftp.funet.fi/pub/mirrors/sources.redhat.com/pub/%SUBDIR%/ \
-	ftp://ftp-stud.fht-esslingen.de/pub/Mirrors/sources.redhat.com/%SUBDIR%/
+	ftp://ftp-stud.fht-esslingen.de/pub/Mirrors/sources.redhat.com/%SUBDIR%/ \
+	http://mirrors.kernel.org/sources.redhat.com/%SUBDIR%/ \
+	http://gd.tuwien.ac.at/gnu/sourceware/%SUBDIR%/ \
+	http://sources-redhat.mirrors.airband.net/%SUBDIR%/ \
+	http://ftp.cs.pu.edu.tw/Linux/sourceware/%SUBDIR%/
 .endif
 
 .if !defined(IGNORE_MASTER_SITE_SUNSITE)
