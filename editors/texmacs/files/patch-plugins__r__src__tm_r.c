8321bffe7ebe8d96f633ed74a338157818c94960
pgollucci <pgollucci@FreeBSD.org>
2010-01-07 04:52:28 +0000

    - Update to 1.0.7.3 - Submitter is now maintainer
    PR:             ports/142396
    Submitted by:   Rob Farmer <rfarmer@predatorlabs.net>

--- editors/texmacs/files/patch-plugins__r__src__tm_r.c
+++ editors/texmacs/files/patch-plugins__r__src__tm_r.c
@@ -1,20 +1,22 @@
---- plugins/r/src/tm_r.c.orig	Sat Dec  3 03:08:07 2005
-+++ plugins/r/src/tm_r.c	Tue Dec  6 01:00:12 2005
-@@ -10,8 +10,13 @@
- ******************************************************************************/
+--- plugins/r/src/tm_r.c.orig	2010-01-05 12:02:13.000000000 -0800
++++ plugins/r/src/tm_r.c	2010-01-05 12:06:43.000000000 -0800
+@@ -11,10 +11,15 @@
  
+ #include "config.h"
  #include <stdio.h>
 +#include <sys/types.h>
  #include <sys/select.h>
 +#ifdef __FreeBSD__
 +#include <libutil.h>
 +#else
+ #if HAVE_PTY_H
  #include <pty.h>
+ #endif
 +#endif
  #include <utmp.h>
  #include <unistd.h>
  #include <termios.h>
-@@ -20,7 +25,6 @@
+@@ -23,7 +28,6 @@
  #include <string.h>
  
  
@@ -22,7 +24,7 @@
  #include <sys/stat.h>
  #include <fcntl.h>
  #include <sys/time.h>
-@@ -458,7 +462,11 @@
+@@ -469,7 +473,11 @@
  #endif
  
        /* Main pselect switch --------------- */
