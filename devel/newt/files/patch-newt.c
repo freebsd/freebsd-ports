787b48bfc5e6c4ea8c8f79455685ef16fa88c0d9
pgollucci <pgollucci@FreeBSD.org>
2010-01-10 23:40:08 +0000

    - Update to 0.5.11 [1] - sort pkg-plist - bump PORTREVISION for SHLIB bump
    Submitted by:   mi (via e-mail) [1] with minor changes

--- devel/newt/files/patch-newt.c
+++ devel/newt/files/patch-newt.c
@@ -1,15 +1,12 @@
 
 $FreeBSD$
 
---- newt.c
-+++ newt.c
-@@ -94,6 +94,9 @@
- 	{ "\033[6~",		NEWT_KEY_PGDN,		NULL },
- 	{ "\033V",		NEWT_KEY_PGUP, 		"kH" },
- 	{ "\033v",		NEWT_KEY_PGUP, 		"kH" },
-+
-+	{ "\033[G",		NEWT_KEY_PGDN,		NULL },
-+	{ "\033[I",		NEWT_KEY_PGUP,		NULL },
+--- newt.c	2009-09-24 11:03:09.000000000 -0400
++++ newt.c	2010-01-06 01:04:01.000000000 -0500
+@@ -99,4 +99,6 @@
+ 	{ "\033V",		NEWT_KEY_PGUP, 		NULL },
+ 	{ "\033v",		NEWT_KEY_PGUP, 		NULL },
++	{ "\033[G",             NEWT_KEY_PGDN,          NULL },
++	{ "\033[I",             NEWT_KEY_PGUP,          NULL },
+         { "\026",		NEWT_KEY_PGDN,		NULL },
  
- 	{ "\033[[A",		NEWT_KEY_F1,		NULL },
- 	{ "\033[[B",		NEWT_KEY_F2,		NULL },
