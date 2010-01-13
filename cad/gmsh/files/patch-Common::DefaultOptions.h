23c300ef8b4de7cd1c2bde0bd843fac918704e96
pav <pav@FreeBSD.org>
2010-01-13 12:47:20 +0000

    - Update to 2.4.2
    PR:		ports/142360
    Submitted by:	Stephen Montgomery-Smith <stephen@missouri.edu> (maintainer)

--- cad/gmsh/files/patch-Common::DefaultOptions.h
+++ cad/gmsh/files/patch-Common::DefaultOptions.h
@@ -1,11 +1,11 @@
---- ./Common/DefaultOptions.h.orig	Sun Oct 17 04:49:26 2004
-+++ ./Common/DefaultOptions.h	Sun Oct 17 22:50:05 2004
-@@ -65,7 +65,7 @@
+--- Common/DefaultOptions.h.orig	2010-01-01 14:21:13.000000000 -0600
++++ Common/DefaultOptions.h	2010-01-01 14:22:28.000000000 -0600
+@@ -70,7 +70,7 @@
  #elif defined(__APPLE__)
-     "open -e %s" ,
+     "open -t %s" ,
  #else
--    "emacs %s &" ,
-+    "$EDITOR %s &" ,
+-    "emacs '%s' &" ,
++    "$EDITOR '%s' &" ,
  #endif
      "System command to launch a text editor" },
    { F|S, "TmpFileName" , opt_general_tmp_filename , ".gmsh-tmp" ,
