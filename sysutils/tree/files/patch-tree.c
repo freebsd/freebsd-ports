817604a84bc4c730cd92dfb6eb2d11ea6ca3bb2f
Philippe Audeoud <jadawin@FreeBSD.org>
2010-01-14 19:38:55 +0000

    - Update to 1.5.3
    PR:		ports/142830
    Submitted by:	Sofian Brabez <sbrabez gmail.com>
    Approved by:	maintainer

--- sysutils/tree/files/patch-tree.c
+++ sysutils/tree/files/patch-tree.c
@@ -1,6 +1,6 @@
---- tree.c.orig	2008-08-28 03:05:43.000000000 +0800
-+++ tree.c	2008-09-22 21:37:05.000000000 +0800
-@@ -663,7 +663,7 @@
+--- ./tree.c.orig	2010-01-14 14:08:55.000000000 +0100
++++ ./tree.c	2010-01-14 14:15:23.000000000 +0100
+@@ -682,7 +682,7 @@
  #ifdef __USE_FILE_OFFSET64
      if (inodeflag) sprintf(path," %7lld",(*dir)->inode);
  #else
@@ -9,22 +9,23 @@
  #endif
      if (devflag) sprintf(path+strlen(path), " %3d", (int)(*dir)->dev);
  #ifdef __EMX__
-@@ -1087,8 +1087,10 @@
+@@ -1106,8 +1106,10 @@
    } else {
      if (Hflag) fprintf(outfile,"<br>\t\t\t\t   ");
-     for(i=0;dirs[i];i++) {
-+      if( dirs[i] <= 0 )
-+        break;
+     for(i=0; dirs[i] && i <= maxlevel; i++) {
++      if (dirs[i] <= 0)
++         break;
        fprintf(outfile,"%s ",
 -	      dirs[i+1] ? (dirs[i]==1 ? linedraw->vert     : (Hflag? "&nbsp;&nbsp;&nbsp;" : "   ") )
-+          dirs[i+1] > 0 ? (dirs[i]==1 ? linedraw->vert     : (Hflag? "&nbsp;&nbsp;&nbsp;" : "   ") )
++             dirs[i+1] > 0 ? (dirs[i]==1 ? linedraw->vert     : (Hflag? "&nbsp;&nbsp;&nbsp;" : "   ") )
  			: (dirs[i]==1 ? linedraw->vert_left:linedraw->corner));
      }
    }
-@@ -1276,7 +1276,12 @@
+@@ -1294,8 +1296,12 @@
+ {
    char *unit="BKMGTPEZY";
    int idx;
- 
+-
 -  if (!hflag) sprintf(buf, sizeof(off_t) == sizeof(long long)? " %11lld" : " %9ld", size);
 +  if (!hflag) {
 +	if (sizeof(off_t) == sizeof(__int64_t))
