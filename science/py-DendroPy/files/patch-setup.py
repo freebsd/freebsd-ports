67d327a8f9e1af133603e5bca98117e40ecec942
wen <wen@FreeBSD.org>
2010-01-15 06:21:10 +0000

    - Update to 3.0.5

--- science/py-DendroPy/files/patch-setup.py
+++ science/py-DendroPy/files/patch-setup.py
@@ -1,15 +1,16 @@
---- setup.py.orig	2010-01-09 13:22:53.000000000 +0800
-+++ setup.py	2010-01-09 13:23:15.000000000 +0800
-@@ -28,9 +28,9 @@
- import os
+--- setup.py.orig	2010-01-15 12:55:37.000000000 +0800
++++ setup.py	2010-01-15 12:55:53.000000000 +0800
+@@ -31,13 +31,6 @@
+ # setuptools/distutils/etc. import and configuration
  
  try:
 -    import ez_setup
--    sys.stderr.write("using ez_setup ('%s')\n" % os.path.abspath(ez_setup.__file__))
+-    try:
+-        ez_setup_path = " ('" + os.path.abspath(ez_setup.__file__) + "')"
+-    except OSError:
+-        ez_setup_path = ""
+-    sys.stderr.write("using ez_setup%s\n" %  ez_setup_path)
 -    ez_setup.use_setuptools()
-+#    import ez_setup
-+#    sys.stderr.write("using ez_setup ('%s')\n" % os.path.abspath(ez_setup.__file__))
-+#    ez_setup.use_setuptools()
      import setuptools
-     sys.stderr.write("using setuptools ('%s')\n" % os.path.abspath(setuptools.__file__))
-     from setuptools import setup, find_packages
+     try:
+         setuptools_path = " ('" +  os.path.abspath(setuptools.__file__) + "')"
