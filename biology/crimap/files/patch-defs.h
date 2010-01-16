c49b8e0836c0dce6b9ea7b78e8dab7c99867b2f4
Philip M. Gollucci <pgollucci@FreeBSD.org>
2010-01-16 17:54:38 +0000

    - Fix on amd64
    PR:             ports/142472
    Submitted by:   Edmund Sumbar <esumbar@ualberta.ca>

---
+++ biology/crimap/files/patch-defs.h
@@ -0,0 +1,11 @@
+--- ./defs.h.orig	1990-10-10 17:07:22.000000000 -0400
++++ ./defs.h	2010-01-16 12:51:21.484797488 -0500
+@@ -17,7 +17,7 @@
+ typedef short SHORT;
+ typedef long ID;
+ typedef long LINDEX;
+-typedef int INT;
++typedef long INT;
+ 
+ struct loci_orders{
+      SHORT num_loci;
