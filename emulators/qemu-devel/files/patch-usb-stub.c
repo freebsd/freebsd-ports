d4e517700042730d5420402bec25ccfa88fed6c8
Juergen Lock <nox@FreeBSD.org>
2010-01-17 16:20:56 +0000

    Finally bring qemu 0.12.1 into ports as the new emulators/qemu-devel, release announcement is here: 	http://lists.gnu.org/archive/html/qemu-devel/2009-12/msg02151.html
    Quoting from pkg-message:
    
    - kqemu is no longer supported in qemu upstream after the 0.11 branch
    was created, which means also not in this version.  (Linux has moved
    on to kvm now for qemu(-like) virtualization needs, so if you want qemu
    to go faster and don't want to switch to virtualbox or stick to the older
    emulators/qemu port which is at 0.11.1 atm and as such still supports
    kqemu you should help getting the FreeBSD kvm port updated and
    completed:
    	http://wiki.freebsd.org/FabioChecconi/PortingLinuxKVMToFreeBSD
    )

--- emulators/qemu-devel/files/patch-usb-stub.c
+++
@@ -1,9 +0,0 @@
-Index: qemu/usb-stub.c
-@@ -1,4 +1,6 @@
--#include "vl.h"
-+#include "qemu-common.h"
-+#include "console.h"
-+#include "hw/usb.h"
- 
- void usb_host_info(void)
- {
--- emulators/qemu-devel/files/patch-vl.c
+++ emulators/qemu-devel/files/patch-vl.c
@@ -1,39 +1,9 @@
 Index: qemu/vl.c
-@@ -1727,6 +1728,7 @@
-     chr->chr_ioctl = pp_ioctl;
-     return chr;
- }
-+#endif /* defined(__linux__) */
- 
+@@ -57,6 +57,7 @@
+ #include <sys/stat.h>
+ #if defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <libutil.h>
++#include <sys/param.h>
  #else
- CharDriverState *qemu_chr_open_pty(void)
-@@ -2334,7 +2334,7 @@
- }
+ #include <util.h>
  #endif
- 
--#if defined(__linux__) || defined(__sun__)
-+#if defined(__linux__) || defined(__sun__) || defined(__FreeBSD__)
- static CharDriverState *qemu_chr_open_pty(void)
- {
-     struct termios tty;
-@@ -3036,7 +3036,7 @@
-         return qemu_chr_open_pp(filename);
-     } else 
- #endif
--#if defined(__linux__) || defined(__sun__)
-+#if defined(__linux__) || defined(__sun__) || defined(__FreeBSD__)
-     if (strstart(filename, "/dev/", NULL)) {
-         return qemu_chr_open_tty(filename);
-     } else
-@@ -8423,6 +8423,11 @@
-     nb_nics = 0;
-     /* default mac address of the first network interface */
- 
-+#ifdef __FreeBSD__
-+    if (modfind("aio") == -1)
-+        fprintf(stderr, "warning: aio not (kld)loaded, may cause `Invalid system call' traps on disk IO\n");
-+#endif
-+
-     optind = 1;
-     for(;;) {
-         if (optind >= argc)
