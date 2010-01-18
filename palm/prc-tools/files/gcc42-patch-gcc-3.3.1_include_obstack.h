5eccbb21e0e9b3da9ea547696bbffd90afa95243
Philip M. Gollucci <pgollucci@FreeBSD.org>
2010-01-18 06:46:44 +0000

    - remove broken/expired ports@ ports
    Approved by: portmgr (miwi)
    
    audio/ccaudio||2010-01-18|Has expired: has been broken for 4 months
    audio/py-libmpdclient||2010-01-18|Has expired: has been broken for 3 months
    chinese/gbk2uni||2010-01-18|Has expired: has been broken for 6 months
    chinese/iiimf-le-xcin||2010-01-18|Has expired: has been broken for 3 months
    devel/adabindx||2010-01-18|Has expired: has been broken for 3 months
    devel/agide||2010-01-18|Has expired: has been broken for 6 months
    devel/asis||2010-01-18|Has expired: has been broken for 3 months
    devel/callgrind||2010-01-18|Has expired: Included in devel/valgrind
    devel/florist||2010-01-18|Has expired: has been broken for 3 months
    editors/xml2rfc-xxe||2010-01-18|Has expired: has been broken for 4 months
    graphics/gephex||2010-01-18|Has expired: has been broken for 3 months
    graphics/irit||2010-01-18|Has expired: has been broken for 3 months
    graphics/pixieplus||2010-01-18|Has expired: has been broken for 6 months
    japanese/expect||2010-01-18|Has expired: has been broken for 3 months
    lang/pnetc||2010-01-18|Has expired: has been broken for 5 months
    mail/libnewmail||2010-01-18|Has expired: has been broken for 3 months
    net-mgmt/flowscan||2010-01-18|Has expired: has been broken for 5 months
    net/astmanproxy||2010-01-18|Has expired: has been broken for 3 months
    palm/prc-tools||2010-01-18|Has expired: has been broken for 4 months
    print/latex-msc||2010-01-18|Has expired: has been broken for 3 months
    science/xloops-ginac||2010-01-18|Has expired: has been broken for 3 months
    shells/bush||2010-01-18|Has expired: has been broken for 3 months
    textproc/iiimf-gnome-im-switcher||2010-01-18|Has expired: has been broken for 4 months
    textproc/iiimf-gtk||2010-01-18|Has expired: has been broken for 3 months
    x11-toolkits/gtkada-devel||2010-01-18|Has expired: has been broken for 3 months
    x11-toolkits/gtkada||2010-01-18|Has expired: has been broken for 3 months
    x11-wm/ion-2||2010-01-18|Has expired: has been broken for 4 months

--- palm/prc-tools/files/gcc42-patch-gcc-3.3.1_include_obstack.h
+++
@@ -1,11 +0,0 @@
---- gcc-3.3.1/include/obstack.h.ORIG	2007-10-17 11:39:14.000000000 -0400
-+++ gcc-3.3.1/include/obstack.h	2007-10-17 09:44:47.000000000 -0400
-@@ -349,7 +349,7 @@
- 
- #define obstack_memory_used(h) _obstack_memory_used (h)
- 
--#if defined __GNUC__ && defined __STDC__ && __STDC__
-+#if defined USE_OBSTACK_MACROS && defined __GNUC__ && defined __STDC__ && __STDC__
- /* NextStep 2.0 cc is really gcc 1.93 but it defines __GNUC__ = 2 and
-    does not implement __extension__.  But that compiler doesn't define
-    __GNUC_MINOR__.  */
