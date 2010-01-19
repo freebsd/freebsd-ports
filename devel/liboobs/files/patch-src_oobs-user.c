b8209f772503582130e2387c919a17548fabcd4b
ed <ed@FreeBSD.org>
2010-01-19 11:19:41 +0000

    Fix build of liboobs on HEAD.
    Now we have utmpx, we can easily enable the code that was previously
    disabled. This patch (without the __FreeBSD_version bits) has been sent
    to the upstream author and will likely be part of the next release.
    
    Approved by:	kwm

--- devel/liboobs/files/patch-src_oobs-user.c
+++ devel/liboobs/files/patch-src_oobs-user.c
@@ -1,11 +1,18 @@
---- oobs/oobs-user.c.orig	2007-10-30 09:16:53.406333206 -0400
-+++ oobs/oobs-user.c	2007-10-30 09:40:02.560797530 -0400
-@@ -18,12 +18,18 @@
+--- oobs/oobs-user.c
++++ oobs/oobs-user.c
+@@ -18,13 +18,28 @@
   * Authors: Carlos Garnacho Parro  <carlosg@gnome.org>
   */
  
 +#if defined(HAVE_CONFIG_H)
 +#include <config.h>
++#endif
++
++#ifdef __FreeBSD__
++# include <sys/param.h>
++# if __FreeBSD_version >= 900007
++#  define HAVE_UTMPX_H
++# endif
 +#endif
 +
  #include <glib-object.h>
@@ -15,21 +22,42 @@
  #include <string.h>
 +#ifdef HAVE_CRYPT_H
  #include <crypt.h>
+-#include <utmp.h>
 +#endif
- #include <utmp.h>
++#ifdef HAVE_UTMPX_H
++#include <utmpx.h>
++#endif
  
  #include "oobs-usersconfig.h"
-@@ -852,6 +858,7 @@
+ #include "oobs-user.h"
+@@ -811,7 +826,8 @@
+ gboolean
+ oobs_user_get_active (OobsUser *user)
+ {
+-  struct utmp *entry;
++#ifdef HAVE_UTMPX_H
++  struct utmpx *entry;
+   const gchar *login;
+   gboolean match = FALSE;
+ 
+@@ -819,14 +835,18 @@
  
    login = oobs_user_get_login_name (user);
  
-+#if 0 /* FreeBSD does not have ut_type in utmp */
-   while (!match && (entry = getutent ()) != NULL)
+-  while (!match && (entry = getutent ()) != NULL)
++  setutxent ();
++  while (!match && (entry = getutxent ()) != NULL)
      {
        match = (entry->ut_type == USER_PROCESS &&
-@@ -862,4 +869,5 @@
-   endutent ();
+ 	       strcmp (entry->ut_user, login) == 0);
+     }
+ 
+   /* close utmp */
+-  endutent ();
++  endutxent ();
  
    return match;
++#else
++  return FALSE;
 +#endif
  }
