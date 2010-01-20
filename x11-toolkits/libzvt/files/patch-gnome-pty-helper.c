894daa2177e7c92f298524221f04b621aa834dd6
ed <ed@FreeBSD.org>
2010-01-20 23:02:08 +0000

    Unbreak libzvt on HEAD.
    Use similar patches that have been used for gnome-libs and vte as well.
    
    Approved by:	miwi (portmgr, implicit)

---
+++ x11-toolkits/libzvt/files/patch-gnome-pty-helper.c
@@ -0,0 +1,10 @@
+--- libzvt/gnome-pty-helper.c
++++ libzvt/gnome-pty-helper.c
+@@ -50,7 +50,6 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <stdio.h>
+-#include <utmp.h>
+ #include <grp.h>
+ #include <glib/galloca.h>
+ #include "gnome-pty.h"
