7b8eedc6c14c066fbac8bb373117a5b25bb376b9
Ed Schouten <ed@FreeBSD.org>
2010-01-20 09:24:24 +0000

    Remove an unneeded inclusion of <utmp.h>.
    Submitted by:	Alex Kozlov <spam rm-rf kiev ua>

---
+++ security/dropbear/files/patch-svr-chansession.c
@@ -0,0 +1,10 @@
+--- svr-chansession.c
++++ svr-chansession.c
+@@ -33,7 +33,6 @@
+ #include "termcodes.h"
+ #include "ssh.h"
+ #include "random.h"
+-#include "utmp.h"
+ #include "x11fwd.h"
+ #include "agentfwd.h"
+ #include "runopts.h"
