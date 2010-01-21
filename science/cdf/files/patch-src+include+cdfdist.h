4f8bbc1fa50a67722592b18c186d4f9954e3a51f
miwi <miwi@FreeBSD.org>
2010-01-21 21:14:54 +0000

    - Update to 3.3.0
    PR:		142950
    Submitted by:	Stephen Montgomery-Smith <stephen@missouri.edu> (maintainer)

--- science/cdf/files/patch-src+include+cdfdist.h
+++ science/cdf/files/patch-src+include+cdfdist.h
@@ -1,11 +1,11 @@
---- src/include/cdflib.h.orig	2008-05-02 20:16:55.000000000 +0200
-+++ src/include/cdflib.h	2008-08-19 21:59:08.000000000 +0200
-@@ -370,7 +370,7 @@
+--- src/include/cdflib.h.orig	2010-01-18 15:22:03.000000000 -0600
++++ src/include/cdflib.h	2010-01-18 15:22:32.000000000 -0600
+@@ -375,7 +375,7 @@
+ #elif defined(unix) && !defined(AIX) && !defined(Mach) && !defined(hpux) || \
      defined(__MINGW32__)
  #  if (defined(linux) && !defined(__ICC)) || defined(__CYGWIN__) || \
-       defined(macosXppc) || defined(macosXintel) || \
--      defined(sun) || defined(__MINGW32__) || defined(__FreeBSD__)
-+      defined(sun) || defined(__MINGW32__)
+-      defined(sun) || defined(__MINGW32__) || defined(__FreeBSD__) || \
++      defined(sun) || defined(__MINGW32__) || \
+       defined(__PPC__)
  #    define Fif_ENTRYPOINT(ep__,ep_,ep,EP) ep__
  #  else
- #    define Fif_ENTRYPOINT(ep__,ep_,ep,EP) ep_
