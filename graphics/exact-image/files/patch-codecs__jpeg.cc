fc3b6a0406636f83d9d9da8f1416440965bd28f6
Martin Wilke <miwi@FreeBSD.org>
2010-01-21 21:14:51 +0000

    - Update to 0.7.5
    PR:		142954
    Submitted by:	bf <bf1783@gmail.com> (maintainer)

--- graphics/exact-image/files/patch-codecs__jpeg.cc
+++ graphics/exact-image/files/patch-codecs__jpeg.cc
@@ -1,11 +1,11 @@
---- codecs/jpeg.cc.orig	2009-07-02 18:34:41.000000000 -0400
-+++ codecs/jpeg.cc	2009-07-02 18:43:42.000000000 -0400
+--- codecs/jpeg.cc.orig	2009-06-14 08:19:28.000000000 -0400
++++ codecs/jpeg.cc	2010-01-16 21:51:34.000000000 -0500
 @@ -683,7 +683,7 @@
       * We need to clean up the JPEG object, close the input file, and return.
       */
      jpeg_destroy_decompress (cinfo);
 -    free (cinfo);
-+    delete[] cinfo;
++    delete cinfo;
      return false;
    }
    
