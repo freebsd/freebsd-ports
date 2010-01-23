eb871800dbebbe11a201575254d19b59e0db2711
marcus <marcus@FreeBSD.org>
2010-01-23 20:11:42 +0000

    Fix the build now that utmp.h is a stub.
    PR:		143064
    Submitted by:	Rob Farmer <rfarmer@predatorlabs.net>

--- sysutils/gnome-system-tools/files/patch-src_users_user-settings.c
+++ sysutils/gnome-system-tools/files/patch-src_users_user-settings.c
@@ -1,6 +1,24 @@
---- src/users/user-settings.c.orig	Sun Apr 22 23:32:58 2007
-+++ src/users/user-settings.c	Sun Apr 22 23:34:18 2007
-@@ -632,10 +632,10 @@
+--- src/users/user-settings.c.orig	2009-12-15 05:17:23.000000000 -0800
++++ src/users/user-settings.c	2010-01-20 22:25:39.000000000 -0800
+@@ -29,7 +29,7 @@
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <stdlib.h>
+-#include <utmp.h>
++#include <sys/param.h>
+ #include <ctype.h>
+ 
+ #include "users-table.h"
+@@ -367,7 +367,7 @@
+ {
+ 	gint max_len;
+ #ifdef __FreeBSD__
+-	max_len = UT_NAMESIZE;
++	max_len = MAXLOGNAME - 1;
+ #else
+ 	struct utmp ut;
+ 
+@@ -819,10 +819,10 @@
  		password_changed = GPOINTER_TO_INT (g_object_get_data (G_OBJECT (widget), "changed"));
  
  		if (password_changed)
