52e83a75c0176e8ea5822ab57328e09d978240ee
wen <wen@FreeBSD.org>
2010-01-24 00:45:04 +0000

    The Web client of OpenObject offers a high quality and ergonomy client for OpenObject and OpenERP. It is known to work with all major web browsers available today, including Firefox, IE6, IE7, Safari3 and Opera9.
    WWW: https://launchpad.net/openobject-client-web
    
    PR:		ports/143036
    Submitted by:	Francisco de Borja Lapez Rao <borja@pexego.es>

---
+++ finance/openerp-web/files/openerp-web.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# EASY-INSTALL-ENTRY-SCRIPT: 'openerp-web==5.0.6','console_scripts','openerp-web'
+__requires__ = 'openerp-web==5.0.6'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('openerp-web==5.0.6', 'console_scripts', 'openerp-web')()
+)
