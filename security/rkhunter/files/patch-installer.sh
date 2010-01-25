c1746b6bbbca91feb308d6aa197a339445b47c8c
jadawin <jadawin@FreeBSD.org>
2010-01-25 08:34:48 +0000

    - Update to 1.3.6 [1]
    PR:		ports/142962
    Submitted by:	Sergiy Borodych <bor univ.kiev.ua>
    [1] Done by:	jadawin@ (myself)

--- security/rkhunter/files/patch-installer.sh
+++ security/rkhunter/files/patch-installer.sh
@@ -1,28 +1,26 @@
---- installer.sh.orig	Wed Oct 17 13:38:52 2007
-+++ installer.sh	Wed Oct 17 13:40:59 2007
-@@ -399,7 +399,7 @@
+--- installer.sh.orig	2010-01-23 12:14:34.000000000 +0000
++++ installer.sh	2010-01-23 12:14:51.000000000 +0000
+@@ -657,22 +657,7 @@
+ 		esac
+ 	done
  
- # Perl will be found in Rkhunter itself.
+-
+-	# Application documents
+-	for FILE in ${RKHINST_DOC_FILES}; do
+-		cp -f ./files/"${FILE}" "${RKHINST_DOC_DIR}"
+-		ERRCODE=$?
+-
+-		if [ $ERRCODE -eq 0 ]; then
+-			echo " Installing ${FILE}: OK"
+-			chmod "${RKHINST_MODE_RWR}" "${RKHINST_DOC_DIR}/${FILE}"
+-		else
+-			echo " Installing ${FILE}: FAILED: Code $ERRCODE: exiting."
+-			exit 1
+-		fi
+-	done
+-
+-
++	
+ 	# Language support files
+ 	ERRCODE=0
  
--RKHINST_DIRS="$RKHINST_DOC_DIR $RKHINST_MAN_DIR $RKHINST_ETC_DIR $RKHINST_BIN_DIR"
-+RKHINST_DIRS="$RKHINST_ETC_DIR $RKHINST_BIN_DIR"
- RKHINST_DIRS_EXCEP="$RKHINST_SCRIPT_DIR $RKHINST_DB_DIR $RKHINST_TMP_DIR $RKHINST_LANG_DIR"
- 
- echo "Starting installation/update"
-@@ -526,11 +526,11 @@
- done
- 
- # Application documents
--for FILE in ${RKHINST_DOC_FILES}; do
--	echo $N " Installing ${FILE}: "
--	cp -f ./files/"${FILE}" "${RKHINST_DOC_DIR}"; retValChk
--	chmod "${RKHINST_MODE_RWR}" "${RKHINST_DOC_DIR}/${FILE}"
--done
-+#for FILE in ${RKHINST_DOC_FILES}; do
-+#	echo $N " Installing ${FILE}: "
-+#	cp -f ./files/"${FILE}" "${RKHINST_DOC_DIR}"; retValChk
-+#	chmod "${RKHINST_MODE_RWR}" "${RKHINST_DOC_DIR}/${FILE}"
-+#done
- 
- # Language support files
- echo $N " Installing language support files: "
