d26751199690d3748bb497b2102bad83784558a9
Cy Schubert <cy@FreeBSD.org>
2010-01-27 06:39:00 +0000

    Make screen build on the latest HEAD (900008).

--- sysutils/screen/files/patch-os.h
+++ sysutils/screen/files/patch-os.h
@@ -1,5 +1,5 @@
---- os.h.orig	Tue Jan  8 07:42:33 2002
-+++ os.h	Tue Sep  9 17:39:58 2003
+--- os.h.orig	2002-01-08 07:42:33.000000000 -0800
++++ os.h	2010-01-26 21:20:44.000000000 -0800
 @@ -161,7 +161,7 @@
  # define setregid(rgid, egid) setresgid(rgid, egid, -1)
  #endif
@@ -9,3 +9,13 @@
  # define USE_SETEUID
  #endif
  
+@@ -262,7 +262,9 @@
+ #if defined(UTMPOK) || defined(BUGGYGETLOGIN)
+ # if defined(SVR4) && !defined(DGUX) && !defined(__hpux) && !defined(linux)
+ #  include <utmpx.h>
++#ifndef __FreeBSD_version
+ #  define UTMPFILE	UTMPX_FILE
++#endif
+ #  define utmp		utmpx
+ #  define getutent	getutxent
+ #  define getutid	getutxid
