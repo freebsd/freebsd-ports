7a409cbc345561a8cb15aec6cef59015299d74cc
Martin Wilke <miwi@FreeBSD.org>
2010-01-27 01:04:01 +0000

    - Update to 23.1
    PR:		143159
    Submitted by:	Florian Smeets <flo@kasimir.com> (maintainer)

---
+++ games/crafty/files/patch-chess.h
@@ -0,0 +1,10 @@
+--- chess.h.orig	2010-01-24 12:22:38.000000000 +0100
++++ chess.h	2010-01-24 12:22:45.000000000 +0100
+@@ -23,7 +23,6 @@
+ #include <stdio.h>
+ #include <assert.h>
+ #include <stdlib.h>
+-#include <malloc.h>
+ #include <string.h>
+ #if !defined(TYPES_INCLUDED)
+ #  include "lock.h"
