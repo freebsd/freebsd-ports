f2680dffc78a05139e7a79dbf9c247a820e968c6
knu <knu@FreeBSD.org>
2010-01-27 05:59:59 +0000

    This is a port of UnNetHack, a new variant of NetHack, based on and repocopied from the slashem-tty port.

--- games/unnethack/files/patch-config.h
+++ games/unnethack/files/patch-config.h
@@ -1,39 +1,38 @@
 $FreeBSD$
 
---- include/config.h.orig	Sat Dec 30 05:08:31 2006
-+++ include/config.h	Fri Aug 17 23:09:31 2007
-@@ -230,12 +230,12 @@
- 
+--- include/config.h.orig	2010-01-10 23:51:18.000000000 +0900
++++ include/config.h	2010-01-19 13:36:18.734207870 +0900
+@@ -178,11 +178,11 @@
+ #ifndef AUTOCONF
  #ifdef UNIX
  /* path and file name extension for compression program */
--# define COMPRESS "/usr/bin/compress" /* Lempel-Ziv compression */
--# define COMPRESS_EXTENSION ".Z"	     /* compress's extension */
-+/* # define COMPRESS "/usr/bin/compress" */ /* Lempel-Ziv compression */
-+/* # define COMPRESS_EXTENSION ".Z"	 */    /* compress's extension */
- 
+-#define COMPRESS "/usr/bin/compress"	/* Lempel-Ziv compression */
+-#define COMPRESS_EXTENSION ".Z"		/* compress's extension */
++/* #define COMPRESS "/usr/bin/compress" */	/* Lempel-Ziv compression */
++/* #define COMPRESS_EXTENSION ".Z" */		/* compress's extension */
  /* An example of one alternative you might want to use: */
--/* # define COMPRESS "/usr/local/bin/gzip" */   /* FSF gzip compression */
--/* # define COMPRESS_EXTENSION ".gz" */	     /* normal gzip extension */
-+# define COMPRESS "/usr/bin/gzip"   /* FSF gzip compression */
-+# define COMPRESS_EXTENSION ".gz"	     /* normal gzip extension */
+-/* #define COMPRESS "/usr/local/bin/gzip" */	/* FSF gzip compression */
+-/* #define COMPRESS_EXTENSION ".gz" */		/* normal gzip extension */
++#define COMPRESS "/usr/bin/gzip"	/* FSF gzip compression */
++#define COMPRESS_EXTENSION ".gz"		/* normal gzip extension */
+ #endif
  
- /* # define COMPRESS "/usr/bin/bzip2"	*//* bzip2 compression */
- /* # define COMPRESS_EXTENSION ".bz2"	*//* bzip2 extension */
-@@ -249,7 +249,7 @@
+ #ifndef COMPRESS
+@@ -195,7 +195,7 @@
   *	a tar-like file, thus making a neater installation.  See *conf.h
   *	for detailed configuration.
   */
--/* #define DLB */             /* not supported on all platforms */
-+#define DLB             /* not supported on all platforms */
+-/* #define DLB */	/* not supported on all platforms */
++#define DLB	/* not supported on all platforms */
  
  /*
   *	Defining INSURANCE slows down level changes, but allows games that
-@@ -271,7 +271,7 @@
- #  ifdef __APPLE__
- #    define HACKDIR "nethackdir"      /* nethack directory */
- #  else
--#    define HACKDIR "."
-+#    define HACKDIR "%%DATADIR%%"	/* nethack directory */
- #  endif
+@@ -214,7 +214,7 @@
+  * otherwise it will be the current directory.
+  */
+ # ifndef HACKDIR
+-#  define HACKDIR "/usr/games/lib/unnethackdir"
++#  define HACKDIR "%%DATADIR%%"
  # endif
  
+ /*
