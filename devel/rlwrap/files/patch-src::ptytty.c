d733861f82a7226cdfeb65e8cc0c17ef10f5806d
miwi <miwi@FreeBSD.org>
2010-01-27 13:01:11 +0000

    - Update to 0.36
    PR:		143202
    Submitted by:	Rob Farmer <rfarmer@predatorlabs.net>
    Approved by:	maintainer

--- devel/rlwrap/files/patch-src::ptytty.c
+++ devel/rlwrap/files/patch-src::ptytty.c
@@ -1,12 +1,12 @@
---- src/ptytty.c.orig	2005-10-17 09:36:46.000000000 +0200
-+++ src/ptytty.c	2007-09-27 14:12:26.000000000 +0200
+--- src/ptytty.c.orig	2010-01-24 01:05:53.000000000 -0800
++++ src/ptytty.c	2010-01-24 01:09:19.000000000 -0800
 @@ -41,7 +41,7 @@
  #ifdef HAVE_SYS_IOCTL_H
  # include <sys/ioctl.h>
  #endif
 -#if defined(PTYS_ARE_PTMX) && !defined(__CYGWIN32__)
 +#if defined(PTYS_ARE_PTMX) && !defined(__CYGWIN32__) && !defined(__FreeBSD__)
- # include <sys/stropts.h>	/* for I_PUSH */
+ # include <sys/stropts.h>       /* for I_PUSH */
  #endif
  
 @@ -159,11 +159,19 @@
@@ -14,10 +14,10 @@
      char tty_name[] = "/dev/tty??";
  
 -# ifndef PTYCHAR1
--#  define PTYCHAR1	"pqrstuvwxyz"
+-#  define PTYCHAR1      "pqrstuvwxyz"
 -# endif
 -# ifndef PTYCHAR2
--#  define PTYCHAR2	"0123456789abcdef"
+-#  define PTYCHAR2      "0123456789abcdef"
 +# ifdef __FreeBSD__
 +/* Auto-detected pty names do not work well with devfs! */
 +#  undef  PTYCHAR1
