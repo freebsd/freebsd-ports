645a2537dea4a75f955327b6e6f1937d21171782
Dmitry Marakasov <amdmi3@FreeBSD.org>
2010-01-29 11:35:33 +0000

    - Fix warning in FindSDL.cmake - Use @dirrmtry for Modules dir, as other ports may install their own cmake modules
    PR:		143313
    Submitted by:	myself
    Approved by:	Max Brazhnikov <makc@issp.ac.ru>

--- devel/cmake/files/patch-Modules_FindSDL.cmake
+++ devel/cmake/files/patch-Modules_FindSDL.cmake
@@ -1,5 +1,5 @@
---- ./Modules/FindSDL.cmake.orig	2009-11-13 21:32:52.000000000 +0300
-+++ ./Modules/FindSDL.cmake	2010-01-25 22:23:16.749128606 +0300
+--- Modules/FindSDL.cmake.orig	2009-11-13 21:32:52.000000000 +0300
++++ Modules/FindSDL.cmake	2010-01-28 17:32:40.000000000 +0300
 @@ -81,6 +81,11 @@
    /opt/csw # Blastwave
    /opt
@@ -12,3 +12,21 @@
  #MESSAGE("SDL_INCLUDE_DIR is ${SDL_INCLUDE_DIR}")
  
  # SDL-1.1 is the name used by FreeBSD ports...
+@@ -100,7 +105,7 @@
+ #MESSAGE("SDL_LIBRARY_TEMP is ${SDL_LIBRARY_TEMP}")
+ 
+ IF(NOT SDL_BUILDING_LIBRARY)
+-  IF(NOT ${SDL_INCLUDE_DIR} MATCHES ".framework")
++  IF(NOT "${SDL_INCLUDE_DIR}" MATCHES ".framework")
+     # Non-OS X framework versions expect you to also dynamically link to 
+     # SDLmain. This is mainly for Windows and OS X. Other (Unix) platforms 
+     # seem to provide SDLmain for compatibility even though they don't
+@@ -116,7 +121,7 @@
+       /opt/csw
+       /opt
+     )
+-  ENDIF(NOT ${SDL_INCLUDE_DIR} MATCHES ".framework")
++  ENDIF(NOT "${SDL_INCLUDE_DIR}" MATCHES ".framework")
+ ENDIF(NOT SDL_BUILDING_LIBRARY)
+ 
+ # SDL may require threads on your system.
