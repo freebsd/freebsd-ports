5f45b50af4a0c2675db9d1ab74531572907a3487
Li-Wen Hsu <lwhsu@FreeBSD.org>
2010-01-29 20:35:38 +0000

    - Update to 1.11.0

--- www/py-funkload/files/patch-setup.py
+++ www/py-funkload/files/patch-setup.py
@@ -1,11 +1,12 @@
---- setup.py.orig	Sat Mar  3 12:15:17 2007
-+++ setup.py	Sat Mar  3 12:15:24 2007
-@@ -135,8 +135,6 @@
+--- setup.py.orig	2010-01-30 02:16:39.051847143 +0800
++++ setup.py	2010-01-30 02:16:50.065356904 +0800
+@@ -149,9 +149,6 @@
          'Topic :: System :: Monitoring',
      ],
      # setuptools specific keywords
--    install_requires = ['webunit  == 1.3.8',
--                        'docutils >= 0.3.7'],
+-    install_requires = ['webunit  >= 1.3.8',
+-                        'docutils >= 0.3.7',
+-                        'setuptools'],
      zip_safe=True,
      package_data={'funkload': ['data/*',
                                 'demo/simple/*', 'demo/zope/*',
