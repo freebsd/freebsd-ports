92b104a326691ffd893f61eebd88a42608d288ba
miwi <miwi@FreeBSD.org>
2010-01-31 00:50:27 +0000

    - Update to 5.21
    PR:		143331
    Submitted by:	Daniel Roethlisberger <daniel@roe.ch> (maintainer)

--- security/nmap/files/patch-output.cc
+++ security/nmap/files/patch-output.cc
@@ -1,21 +1,19 @@
---- output.cc.orig	2009-04-01 00:29:03.000000000 +0200
-+++ output.cc	2009-04-01 22:52:01.000000000 +0200
-@@ -108,6 +108,7 @@
- #include <math.h>
- 
- #include <set>
-+#include <sys/param.h>
+--- output.cc.orig	2009-12-20 04:22:19.000000000 +0100
++++ output.cc	2010-01-21 17:42:20.000000000 +0100
+@@ -111,6 +111,7 @@
  #include <string>
  #include <vector>
  #include <list>
-@@ -1030,8 +1031,8 @@
++#include <sys/param.h>
+ 
+ /* Workaround for lack of namespace std on HP-UX 11.00 */
+ namespace std {};
+@@ -1037,7 +1038,7 @@
    case LOG_MACHINE:
    case LOG_SKID:
    case LOG_XML:
 -#ifdef WIN32
--	  apcopy = ap;
 +#if defined(WIN32) || (defined(FREEBSD) && (__FreeBSD_version < 500000))
-+    apcopy = ap;
+     apcopy = ap;
  #else
-     va_copy(apcopy, ap); /* Needed in case we need to so a second vnsprintf */
- #endif
+     va_copy(apcopy, ap); /* Needed in case we need to do a second vsnprintf */
