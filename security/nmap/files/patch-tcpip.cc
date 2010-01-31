6d09d8dd4f10eeefcd3f67f145777dd94374d6e9
Martin Wilke <miwi@FreeBSD.org>
2010-01-31 00:50:27 +0000

    - Update to 5.21
    PR:		143331
    Submitted by:	Daniel Roethlisberger <daniel@roe.ch> (maintainer)

--- security/nmap/files/patch-tcpip.cc
+++ security/nmap/files/patch-tcpip.cc
@@ -9,21 +9,3 @@
  extern NmapOps o;
  
  #ifdef WIN32
-@@ -1959,7 +1961,7 @@
- 
- // Returns whether the system supports pcap_get_selectable_fd() properly
- bool pcap_selectable_fd_valid() {
--#if defined(WIN32) || defined(MACOSX)
-+#if defined(WIN32) || defined(MACOSX) || (defined(FREEBSD) && (__FreeBSD_version < 500000))
-   return false;
- #endif
-   return true;
-@@ -1972,7 +1974,7 @@
-    results.  If you just want to test whether the function is supported,
-    use pcap_selectable_fd_valid() instead. */
- int my_pcap_get_selectable_fd(pcap_t *p) {
--#if defined(WIN32) || defined(MACOSX)
-+#if defined(WIN32) || defined(MACOSX) || (defined(FREEBSD) && (__FreeBSD_version < 500000))
-   return -1;
- #else
-   assert(pcap_selectable_fd_valid());
