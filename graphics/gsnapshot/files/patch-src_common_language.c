2bbacf5efcf6dc9601e5ffc6c6879493f5774972
pgollucci <pgollucci@FreeBSD.org>
2010-02-01 03:32:22 +0000

    - Update to 1.1
    PR:             ports/142839
    Submitted by:   Bernhard Froehlich <decke@bluelife.at>

--- graphics/gsnapshot/files/patch-src_common_language.c
+++ graphics/gsnapshot/files/patch-src_common_language.c
@@ -1,11 +1,11 @@
---- ./src/language.c.orig	2007-07-16 01:05:23.000000000 +0300
-+++ ./src/language.c	2008-05-06 10:20:22.000000000 +0300
+--- src/common/language.c.orig	2007-07-28 15:49:17.000000000 +0200
++++ src/common/language.c	2010-01-13 21:07:22.000000000 +0100
 @@ -335,6 +335,9 @@
  {
    if (spoken == NULL) {
      char *lang = getenv("LANG");
 +    if(!lang)
-+           return TranslationEnglish[label].locale;
++      return TranslationEnglish[label].locale;
 +
      if (strcmp(lang, "de") == 0 || strcmp(lang, "de_DE") == 0)
        spoken = TranslationGerman;
