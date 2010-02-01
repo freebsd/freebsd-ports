6241ceda04b587bd4aecd8d15fa9fdf0652aacd4
miwi <miwi@FreeBSD.org>
2010-02-01 13:04:03 +0000

    - Revert back to previous version (to many problems) - Bump PORTEPOCH
    Requested by:	many

--- net-p2p/deluge/files/patch-setup.py
+++ net-p2p/deluge/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2010-01-10 06:16:08.000000000 +0900
-+++ setup.py	2010-01-13 16:48:06.000000000 +0900
-@@ -67,7 +67,6 @@
+--- setup.py.orig	2009-01-10 20:24:16.000000000 -0600
++++ setup.py	2009-01-17 15:02:02.000000000 -0600
+@@ -56,7 +56,6 @@
      "-D_FILE_OFFSET_BITS=64",
      "-DNDEBUG",
      "-DTORRENT_USE_OPENSSL=1",
@@ -8,7 +8,7 @@
      ]
  
  if windows_check():
-@@ -113,12 +112,14 @@
+@@ -95,12 +94,14 @@
          sysconfig.get_config_vars()["OPT"] = " ".join(cv_opt.split())
  
  _library_dirs = [
@@ -24,7 +24,7 @@
  ]
  
  if windows_check():
-@@ -140,14 +141,6 @@
+@@ -122,11 +123,6 @@
          'zlib'
      ]
  else:
@@ -32,14 +32,11 @@
 -        '/usr/include/python' + python_version,
 -        sysconfig.get_config_var("INCLUDEDIR")
 -        ]
--    for include in os.environ.get("INCLUDEDIR", "").split(":"):
--        _include_dirs.append(include)
--
 -    _library_dirs += [sysconfig.get_config_var("LIBDIR"), '/opt/local/lib']
      if osx_check():
          _include_dirs += [
              '/opt/local/include/boost-1_35',
-@@ -161,7 +154,6 @@
+@@ -138,7 +134,6 @@
          'boost_iostreams',
          'boost_python',
          'boost_thread',
@@ -47,20 +44,21 @@
          'ssl',
          'z'
          ]
-@@ -204,12 +196,6 @@
+@@ -181,13 +176,6 @@
  
  # Check for a system libtorrent and if found, then do not build the libtorrent extension
  build_libtorrent = True
 -try:
--    from deluge._libtorrent import lt
+-    import libtorrent
 -except ImportError:
 -    build_libtorrent = True
 -else:
--    build_libtorrent = False
+-    if libtorrent.version_major == 0 and libtorrent.version_minor == 14:
+-        build_libtorrent = False
  
- if build_libtorrent and os.path.exists("libtorrent"):
+ if build_libtorrent:
      # There isn't a system libtorrent library, so let's build the one included with deluge
-@@ -219,6 +205,7 @@
+@@ -197,6 +185,7 @@
          include_dirs = _include_dirs,
          libraries = _libraries,
          library_dirs = _library_dirs,
@@ -68,7 +66,7 @@
          sources = _sources
      )
  
-@@ -356,8 +343,8 @@
+@@ -310,8 +299,8 @@
          for cmd_name in self.get_sub_commands():
              self.run_command(cmd_name)
          _install.run(self)
@@ -79,7 +77,7 @@
  
  cmdclass = {
      'build': build,
-@@ -371,7 +358,7 @@
+@@ -324,7 +313,7 @@
  
  # Data files to be installed to the system
  _data_files = [
@@ -88,12 +86,12 @@
      ('share/icons/hicolor/128x128/apps', ['deluge/data/icons/hicolor/128x128/apps/deluge.png']),
      ('share/icons/hicolor/16x16/apps', ['deluge/data/icons/hicolor/16x16/apps/deluge.png']),
      ('share/icons/hicolor/192x192/apps', ['deluge/data/icons/hicolor/192x192/apps/deluge.png']),
-@@ -386,7 +373,7 @@
+@@ -339,7 +328,7 @@
      ('share/icons/hicolor/96x96/apps', ['deluge/data/icons/hicolor/96x96/apps/deluge.png']),
      ('share/applications', ['deluge/data/share/applications/deluge.desktop']),
      ('share/pixmaps', ['deluge/data/pixmaps/deluge.png', 'deluge/data/pixmaps/deluge.xpm']),
--    ('share/man/man1', [
-+    ('man/man1', [
-         'docs/man/deluge.1',
-         'docs/man/deluged.1',
-         'docs/man/deluge-gtk.1',
+-    ('share/man/man1', ['deluge/docs/man/deluge.1', 'deluge/docs/man/deluged.1'])
++    ('man/man1', ['deluge/docs/man/deluge.1', 'deluge/docs/man/deluged.1'])
+ ]
+ 
+ # Main setup
