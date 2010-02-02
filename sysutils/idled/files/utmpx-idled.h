d2253bb205f27fb8ad3e14d2c967046c20cbcf20
Wen Heping <wen@FreeBSD.org>
2010-02-02 06:03:37 +0000

    - Convert from utmp.h to utmpx.h
    PR:		ports/143238
    Submitted by:	Rob Farmer <rfarmer@predatorlabs.net>

---
+++ sysutils/idled/files/utmpx-idled.h
@@ -0,0 +1,10 @@
+--- idled.h.orig	2010-01-25 16:07:42.000000000 -0800
++++ idled.h	2010-01-25 16:07:50.000000000 -0800
+@@ -1,7 +1,6 @@
+ #include <sys/types.h>
+ #include <stdio.h>
+ #include <sys/param.h>
+-#include <utmp.h>
+ 
+ #define qelem qelem_sys	/* Work around to use our own qelem below */
+ #include <stdlib.h>
