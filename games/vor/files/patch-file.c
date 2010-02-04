9fc70070715dcdf8b9b4c3f25af08cc33774ecbc
amdmi3 <amdmi3@FreeBSD.org>
2010-02-04 10:42:11 +0000

    - Update to 0.5.5

--- games/vor/files/patch-file.c
+++ games/vor/files/patch-file.c
@@ -1,13 +1,10 @@
---- file.c.orig	Sun Mar 26 06:52:35 2006
-+++ file.c	Mon Mar 27 02:30:51 2006
-@@ -101,9 +101,9 @@
- {
+--- file.c.orig	2009-12-10 00:28:59.000000000 +0300
++++ file.c	2010-02-04 05:50:54.000000000 +0300
+@@ -101,7 +101,6 @@
  	int i;
  	char *data_options[3] = {
-+		DATA_PREFIX,
  		getenv("VOR_DATA"),
- 		"data",
--		DATA_PREFIX
+-		"data",
+ 		DATA_PREFIX
  	};
  
- 	for(i=0; i<3; i++) {
