ca88e6915741a3ca3556572cf0c01051e40167c8
miwi <miwi@FreeBSD.org>
2010-02-06 21:52:30 +0000

    Presenting KDE 4.3.5 for FreeBSD.  The official release notes for this release can be found at http://kde.org/announcements/announce-4.3.5.php.
    KDE 4.3.5, the last bugfix release in 4.3.x series. We'd like to say
    thanks to all helpers and submitters.

--- Mk/bsd.kde4.mk
+++ Mk/bsd.kde4.mk
@@ -52,11 +52,10 @@ Kde_Include_MAINTAINER=	kde@FreeBSD.org
 #		but this could be changed in a future.
 #
 
-KDE4_VERSION=		4.3.4
+KDE4_VERSION=		4.3.5
 KDE4_BRANCH?=		stable
 KOFFICE2_VERSION=	2.1.0
 KOFFICE2_BRANCH?=	stable
-
 
 #
 # KDE4 is installed into its own prefix to not conflict with KDE3
