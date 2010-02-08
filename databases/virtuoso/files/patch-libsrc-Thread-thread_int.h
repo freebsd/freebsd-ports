6397374e4644f5679a79c12cf8fd51af161cbcce
fjoe <fjoe@FreeBSD.org>
2010-02-08 18:24:16 +0000

    - Update to 6.1.0 - Add amd64 to the list of supported arches - Add missing depend on htmldoc - Add conflict with unixODBC [1]
    PR:		122801 [1]

--- databases/virtuoso/files/patch-libsrc-Thread-thread_int.h
+++
@@ -1,6 +0,0 @@
---- libsrc/Thread/thread_int.h.orig	Mon Jan 22 06:28:12 2007
-+++ libsrc/Thread/thread_int.h	Sun Nov 25 10:31:20 2007
-@@ -36,2 +36,3 @@
- #include <assert.h>
-+#include <pthread.h>		/* For FreeBSD 5.x */
- #define _THREAD_INT_HS
