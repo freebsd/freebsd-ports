0debb538115b56b08ea7ed5e1db7a1467ff23e02
Cy Schubert <cy@FreeBSD.org>
2010-02-09 07:05:42 +0000

    Screen failed to work properly under HEAD. This patch resolve the issue.
    Submitted by:	ed

--- sysutils/screen/files/patch-utmp.c
+++ sysutils/screen/files/patch-utmp.c
@@ -1,5 +1,5 @@
 --- utmp.c.orig	2003-09-08 07:27:17.000000000 -0700
-+++ utmp.c	2010-01-26 22:27:46.000000000 -0800
++++ utmp.c	2009-12-21 15:10:06.000000000 -0800
 @@ -21,6 +21,9 @@
   ****************************************************************
   */
@@ -17,44 +17,23 @@
 +/* XXX FreeBSD-9 includes libutempter however if UTMP_HELPER is defined,
 +       the build is broken due to utmpfd not being defined.
 +*/
-+#if defined(__FreeBSD_version) && __FreeBSD_version < 900004 || !defined(__FreeBSD_version)
++#if defined(__FreeBSD_version) && (__FreeBSD_version < 900004 || __FreeBSD_version >= 900007) || !defined(__FreeBSD_version)
  # define UTMP_HELPER
  #endif
 +#endif
  
  
  
-@@ -590,9 +598,11 @@
+@@ -589,7 +597,7 @@
+ struct utmp *u;
  {
    u->ut_type = DEAD_PROCESS;
- #if !defined(linux) || defined(EMPTY)
-+#ifndef __FreeBSD__
+-#if !defined(linux) || defined(EMPTY)
++#if (!defined(linux) || defined(EMPTY)) && !defined(__FreeBSD__)
    u->ut_exit.e_termination = 0;
    u->ut_exit.e_exit = 0;
  #endif
-+#endif
- #if !defined(sun) || !defined(SVR4)
-   u->ut_user[0] = 0;	/* for Digital UNIX, kilbi@rad.rwth-aachen.de */
- #endif
-@@ -618,7 +628,9 @@
- #endif /* sgi */
-   strncpy(u->ut_line, line, sizeof(u->ut_line));
-   u->ut_pid = pid;
-+#ifndef __FreeBSD__
-   (void)time((time_t *)&u->ut_time);
-+#endif
- }
- 
- static slot_t
-@@ -630,6 +642,7 @@
- 
- 
- #else /* GETUTENT */
-+#if defined(__FreeBSD_version) && __FreeBSD_version < 900008
- 
- /*********************************************************************
-  *
-@@ -726,9 +739,12 @@
+@@ -726,9 +734,12 @@
  char *line, *user;
  int pid;
  {
@@ -68,11 +47,3 @@
  }
  
  static slot_t
-@@ -759,6 +775,7 @@
-   return slot;
- }
- 
-+#endif	/* __FreeBSD_version */
- #endif	/* GETUTENT */
- 
- 
