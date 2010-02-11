7a02b15fa29feb4759cc44a4a08c85808e0f8de1
kwm <kwm@FreeBSD.org>
2010-02-11 11:29:57 +0000

    Update to 0.10.26.
    Changelog:	http://gstreamer.freedesktop.org/releases/gstreamer/0.10.26.html

--- multimedia/gstreamer/files/patch-gst_gstregistry.c
+++ multimedia/gstreamer/files/patch-gst_gstregistry.c
@@ -1,11 +1,11 @@
---- gst/gstregistry.c.orig	2009-07-14 11:54:05.000000000 +0200
-+++ gst/gstregistry.c	2009-08-05 14:05:20.000000000 +0200
-@@ -896,7 +896,7 @@ gst_registry_scan_path_level (GstRegistr
-         GST_DEBUG_OBJECT (registry, "mtime %ld != %ld or size %"
+--- gst/gstregistry.c.orig	2010-01-20 02:48:56.000000000 +0100
++++ gst/gstregistry.c	2010-02-11 10:46:09.000000000 +0100
+@@ -1163,7 +1163,7 @@ gst_registry_scan_path_level (GstRegistr
              G_GINT64_FORMAT " != %" G_GINT64_FORMAT " or external dependency "
-             "env_vars changed: %d or external dependencies changed: %d",
+             "env_vars changed: %d or external dependencies changed: %d"
+             " or old path %s != new path %s",
 -            plugin->file_mtime, file_status.st_mtime,
 +            (glong) plugin->file_mtime, (glong) file_status.st_mtime,
              (gint64) plugin->file_size, (gint64) file_status.st_size,
-             env_vars_changed, deps_changed);
-         gst_registry_remove_plugin (gst_registry_get_default (), plugin);
+             env_vars_changed, deps_changed, plugin->filename, filename);
+         gst_registry_remove_plugin (context->registry, plugin);
