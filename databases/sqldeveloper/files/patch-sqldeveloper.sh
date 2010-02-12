430915877c8a761520fabda4a1a1172ba0b852fc
Pav Lucistnik <pav@FreeBSD.org>
2010-02-12 10:16:58 +0000

    - Update to 2.1.0.63.73
    PR:		ports/142343
    Submitted by:	Jose Garcia Juanino <jjuanino@gmail.com> (maintainer)

--- databases/sqldeveloper/files/patch-sqldeveloper.sh
+++ databases/sqldeveloper/files/patch-sqldeveloper.sh
@@ -1,5 +1,5 @@
---- sqldeveloper.sh.orig	2008-07-16 21:07:00.000000000 +0200
-+++ sqldeveloper.sh	2008-07-16 21:10:20.000000000 +0200
+--- sqldeveloper.sh.orig
++++ sqldeveloper.sh
 @@ -1,2 +1,2 @@
  #!/bin/bash
 -cd "`dirname $0`"/sqldeveloper/bin && bash sqldeveloper $*
