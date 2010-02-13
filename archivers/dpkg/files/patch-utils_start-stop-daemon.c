39472f960a8505a1770c1a114332a606fff88ac4
miwi <miwi@FreeBSD.org>
2010-02-13 18:41:06 +0000

    - Update to 1.14.25
    PR:		140699
    Submitted by:	Gea-Suan Lin <gslin@gslin.org>
    Approved by:	maintainer timeout

--- archivers/dpkg/files/patch-utils_start-stop-daemon.c
+++ archivers/dpkg/files/patch-utils_start-stop-daemon.c
@@ -1,38 +1,30 @@
---- utils/start-stop-daemon.c.orig	Wed Jun 21 07:59:36 2006
-+++ utils/start-stop-daemon.c	Thu Oct 26 18:59:40 2006
-@@ -48,6 +48,7 @@
+--- utils/start-stop-daemon.c.orig	2009-02-01 14:19:02.000000000 +0800
++++ utils/start-stop-daemon.c	2009-11-12 07:23:18.000000000 +0800
+@@ -50,6 +50,7 @@
  #endif
  
- #if  defined(OSOpenBSD) || defined(OSFreeBSD) || defined(OSNetBSD)
+ #if defined(OSOpenBSD) || defined(OSFreeBSD) || defined(OSNetBSD)
 +#define _WANT_UCRED
  #include <sys/param.h>
  #include <sys/proc.h>
  #include <sys/stat.h>
-@@ -928,9 +929,15 @@
- 		errx(1, "%s", errbuf);
- 	if ((kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &nentries)) == 0)
+@@ -941,8 +942,8 @@
+ 	kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &nentries);
+ 	if (kp == NULL)
  		errx(1, "%s", kvm_geterr(kd));
-+#if defined(OSOpenBSD) || (defined(OSFreeBSD) && __FreeBSD_version < 500014)
- 	if (kp->kp_proc.p_cred )
- 		kvm_read(kd, (u_long)&(kp->kp_proc.p_cred->p_ruid),
- 			&proc_uid, sizeof(uid_t));
-+#elif defined(OSFreeBSD)
-+	if ( kp->ki_paddr->p_ucred )
+-	if (kp->kp_proc.p_cred)
+-		kvm_read(kd, (u_long)&(kp->kp_proc.p_cred->p_ruid),
++	if (kp->ki_paddr->p_ucred)
 +		kvm_read(kd, (u_long)&(kp->ki_paddr->p_ucred->cr_ruid),
-+			&proc_uid, sizeof(uid_t));
-+#endif
+ 		         &proc_uid, sizeof(uid_t));
  	else
  		return 0;
- 	return (proc_uid == (uid_t)uid);
-@@ -949,7 +956,11 @@
- 		errx(1, "%s", errbuf);
- 	if ((kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &nentries)) == 0)
+@@ -963,7 +964,7 @@
+ 	kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &nentries);
+ 	if (kp == NULL)
  		errx(1, "%s", kvm_geterr(kd));
-+#if defined(OSOpenBSD) || (defined(OSFreeBSD) && __FreeBSD_version < 500014)
- 	pidexec = (&kp->kp_proc)->p_comm;
-+#elif defined(OSFreeBSD)
+-	pidexec = (&kp->kp_proc)->p_comm;
 +	pidexec = kp->ki_paddr->p_comm;
-+#endif
  	if (strlen(name) != strlen(pidexec))
  		return 0;
  	return (strcmp(name, pidexec) == 0) ? 1 : 0;
