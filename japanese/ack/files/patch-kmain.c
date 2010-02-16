4147e7d8cf3d00bf1f7eadccba647483d89b353f
Pav Lucistnik <pav@FreeBSD.org>
2010-02-16 19:37:40 +0000

    - Fix runtime segfault on amd64
    PR:		ports/143999
    Submitted by:	Katsuyoshi OHARA <ohara@air.s.kanazawa-u.ac.jp>
    Feature safe:	yes

---
+++ japanese/ack/files/patch-kmain.c
@@ -0,0 +1,11 @@
+--- kmain.c.orig	1994-07-26 18:30:35.000000000 +0900
++++ kmain.c	2010-02-16 19:33:47.000000000 +0900
+@@ -6,6 +6,8 @@
+ /* v1.30  1994  4/16	*/
+ 
+ #include	<stdio.h>
++#include	<stdlib.h>
++#include	<string.h>
+ 
+ #include	"kanjicode.h"
+ #include	"ackstring.h"
