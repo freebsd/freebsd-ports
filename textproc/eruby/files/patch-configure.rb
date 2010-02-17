876787e3b3da982c58eb21596fbc94d01084bc46
Dirk Meyer <dinoex@FreeBSD.org>
2010-02-17 17:01:03 +0000

    - Fix for ruby 1.9.1 Obtained from:	github Feature safe:	yes

--- textproc/eruby/files/patch-configure.rb
+++ textproc/eruby/files/patch-configure.rb
@@ -1,24 +1,6 @@
---- configure.rb.orig	2003-02-10 04:18:10.000000000 +0100
-+++ configure.rb	2009-07-12 16:05:45.000000000 +0200
-@@ -59,7 +59,7 @@
-   end
- end
- 
--require 'ftools'
-+require 'fileutils'
- 
- def AC_OUTPUT(*files)
-   if $AC_LIST_HEADER
-@@ -71,7 +71,7 @@
-   for file in files
-     print "creating ", file, "\n"
-     open(File.join($srcdir, file + ".in")) do |fin|
--      File.makedirs(File.dirname(file))
-+      FileUtils.mkdir_p(File.dirname(file))
-       open(file, "w") do |fout|
- 	while line = fin.gets
- 	  line.gsub!(/@([A-Za-z_]+)@/) do |s|
-@@ -301,12 +301,22 @@
+--- configure.rb.orig	2010-02-17 16:52:48.000000000 +0100
++++ configure.rb	2010-02-17 16:52:48.000000000 +0100
+@@ -317,7 +317,16 @@
  else
    $CFLAGS = CFLAGS + " " + CONFIG["CCDLFLAGS"]
  end
@@ -35,22 +17,7 @@
  if $LDFLAGS.to_s.empty? && /mswin32/ =~ RUBY_PLATFORM
    $LDFLAGS = "-link -incremental:no -pdb:none"
  end
- $LIBS = CONFIG["LIBS"]
- $XLDFLAGS = CONFIG["XLDFLAGS"]
-+$XLDFLAGS="" if $XLDFLAGS.nil?
- $XLDFLAGS.gsub!(/-L\./, "")
- if /mswin32/ !~ RUBY_PLATFORM
-   $XLDFLAGS += " -L$(libdir)"
-@@ -333,7 +343,7 @@
- $LIBRUBY_A = CONFIG["LIBRUBY_A"]
- $RUBY_SO_NAME = CONFIG["RUBY_SO_NAME"]
- 
--case PLATFORM
-+case RUBY_PLATFORM
- when /-aix/
-   if $RUBY_SHARED
-     $LIBRUBYARG = "-Wl,$(libdir)/" + CONFIG["LIBRUBY_SO"]
-@@ -407,7 +417,7 @@
+@@ -435,7 +444,7 @@
  AC_MSG_CHECKING("whether we are using gcc")
  if $CC == "gcc" || `#{$CC} -v 2>&1` =~ /gcc/
    $using_gcc = true
@@ -59,39 +26,3 @@
  else
    $using_gcc = false
  end
-@@ -431,7 +441,7 @@
- $ENABLE_SHARED = false
- AC_ENABLE("shared") { |enableval|
-   if enableval == "yes"
--    if PLATFORM =~ /-mswin32/
-+    if RUBY_PLATFORM =~ /-mswin32/
-       AC_MSG_ERROR("can't enable shared on mswin32")
-     end
-     $ENABLE_SHARED = true
-@@ -449,7 +459,7 @@
- if $ENABLE_SHARED
-   $LIBERUBY = "${LIBERUBY_SO}"
-   $LIBERUBYARG = "-L. -leruby"
--  case PLATFORM
-+  case RUBY_PLATFORM
-   when /-sunos4/
-     $LIBERUBY_ALIASES = "liberuby.so.$(MAJOR).$(MINOR) liberuby.so"
-   when /-linux/
-@@ -457,7 +467,7 @@
-     $LIBERUBY_ALIASES = "liberuby.so.$(MAJOR).$(MINOR) liberuby.so"
-   when /-(freebsd|netbsd)/
-     $LIBERUBY_SO = "liberuby.so.$(MAJOR).$(MINOR)"
--    if PLATFORM =~ /elf/ || PLATFORM =~ /-freebsd[3-9]/
-+    if RUBY_PLATFORM =~ /elf/ || RUBY_PLATFORM =~ /-freebsd[3-9]/
-       $LIBERUBY_SO = "liberuby.so.$(MAJOR_MINOR)"
-       $LIBERUBY_ALIASES = "liberuby.so"
-     else
-@@ -493,7 +503,7 @@
-   end
- end
- 
--if PLATFORM =~ /-mswin32/
-+if RUBY_PLATFORM =~ /-mswin32/
-   $AR = "lib"
-   $AROPT = "/out:$@"
-   $LIBERUBY_A = "liberuby.lib"
--- textproc/eruby/files/patch-eruby_lib.c
+++ textproc/eruby/files/patch-eruby_lib.c
@@ -1,5 +1,5 @@
---- eruby_lib.c.orig	2003-07-29 05:42:56.000000000 +0200
-+++ eruby_lib.c	2009-06-30 12:42:12.000000000 +0200
+--- eruby_lib.c.orig	2010-02-17 16:52:48.000000000 +0100
++++ eruby_lib.c	2010-02-17 16:52:48.000000000 +0100
 @@ -34,10 +34,18 @@
  #include <signal.h>
  
@@ -8,7 +8,7 @@
  #include "eruby.h"
  #include "config.h"
  
-+#if defined(DWITH_RUBY_19)
++#if defined(WITH_RUBY_19)
 +#include "ruby/regex.h"
 +#undef ismbchar
 +#define ismbchar(c,e,enc)	((mbclen(c,e,enc)) != 1)
@@ -20,15 +20,15 @@
  EXTERN VALUE rb_stdin;
  EXTERN VALUE ruby_top_self;
  
-@@ -133,6 +141,7 @@
+@@ -144,6 +152,7 @@
  	    }
  	    s++;
  	    goto again;
-+#if !defined(DWITH_RUBY_19)
++#if !defined(WITH_RUBY_19)
  	case 'K':
  	    s++;
  	    if (*s == '\0') {
-@@ -142,6 +151,7 @@
+@@ -153,6 +162,7 @@
  	    rb_set_kcode(s);
  	    s++;
  	    goto again;
@@ -36,54 +36,11 @@
  	case 'C':
  	    s++;
  	    if (isspace(*s)) s++;
-@@ -296,18 +306,18 @@
-     VALUE s = compiler->lex_input;
-     char *beg, *end, *pend;
- 
--    if (RSTRING(s)->len == compiler->lex_gets_ptr)
-+    if (RSTRING_LEN(s) == compiler->lex_gets_ptr)
- 	return Qnil;
--    beg = RSTRING(s)->ptr;
-+    beg = RSTRING_PTR(s);
-     if (compiler->lex_gets_ptr > 0) {
- 	beg += compiler->lex_gets_ptr;
-     }
--    pend = RSTRING(s)->ptr + RSTRING(s)->len;
-+    pend = RSTRING_PTR(s) + RSTRING_LEN(s);
-     end = beg;
-     while (end < pend) {
- 	if (*end++ == '\n') break;
-     }
--    compiler->lex_gets_ptr = end - RSTRING(s)->ptr;
-+    compiler->lex_gets_ptr = end - RSTRING_PTR(s);
-     return rb_str_new(beg, end - beg);
- }
- 
-@@ -326,8 +336,8 @@
- 
- 	    if (NIL_P(v)) return EOF;
- 	    compiler->sourceline++;
--	    compiler->lex_pbeg = compiler->lex_p = RSTRING(v)->ptr;
--	    compiler->lex_pend = compiler->lex_p + RSTRING(v)->len;
-+	    compiler->lex_pbeg = compiler->lex_p = RSTRING_PTR(v);
-+	    compiler->lex_pend = compiler->lex_p + RSTRING_LEN(v);
- 	    compiler->lex_lastline = v;
- 	}
- 	else {
-@@ -471,7 +481,7 @@
- 	if (c == '!') {
- 	    unsigned char *p;
- 	    char *argv[2];
--	    char *line = RSTRING(compiler->lex_lastline)->ptr;
-+	    char *line = RSTRING_PTR(compiler->lex_lastline);
- 
- 	    if (line[strlen(line) - 1] == '\n') {
- 		line[strlen(line) - 1] = '\0';
-@@ -577,8 +587,13 @@
+@@ -592,8 +602,13 @@
  		if (prevc < 0) output_literal(compiler, "print \"");
  		output_char(compiler, c);
  		prevc = c;
-+#if defined(DWITH_RUBY_19)
++#if defined(WITH_RUBY_19)
 +                if (ismbchar(c,c+4,OnigEncDefaultCharEncoding)) {
 +                    int i, len = mbclen(c,c,OnigEncDefaultCharEncoding) - 1;
 +#else
