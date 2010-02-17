17fbbdae8bd5a8e62ab1a47830073d62616de8f6
lwhsu <lwhsu@FreeBSD.org>
2010-02-17 20:40:31 +0000

    - Update to 1.13
    PR:		ports/144048
    Submitted by:	bf <bf1783 AT gmail.com> (maintainer)
    
    Feature safe:	yes

--- devel/abi-compliance-checker/files/patch-abi-compliance-checker.pl
+++ devel/abi-compliance-checker/files/patch-abi-compliance-checker.pl
@@ -1,14 +1,23 @@
---- abi-compliance-checker.pl.orig	2009-11-08 14:27:20.000000000 -0500
-+++ abi-compliance-checker.pl	2009-11-08 14:33:06.000000000 -0500
-@@ -6673,9 +6673,9 @@
-     }
-     else
+--- abi-compliance-checker.pl.orig	2010-02-16 07:13:09.000000000 -0500
++++ abi-compliance-checker.pl	2010-02-17 13:20:46.000000000 -0500
+@@ -481,7 +481,7 @@
+     return if(not $InfoPath or not -f $InfoPath);
+     my $InfoPath_New = $InfoPath.".1";
+     #my $Keywords = join("\\|", keys(%check_node));#|grep "$Keywords"
+-    system("sed ':a;N;\$!ba;s/\\n[^\@]//g' ".esc($InfoPath)."|sed 's/ [ ]\\+/  /g' > ".esc($InfoPath_New));
++    system("gsed ':a;N;\$!ba;s/\\n[^\@]//g' ".esc($InfoPath)."|gsed 's/ [ ]\\+/  /g' > ".esc($InfoPath_New));
+     system("rm", "-fr", $InfoPath);
+     #getting info
+     open(INFO, $InfoPath_New) || die ("can't open file '\$InfoPath_New\': $!\n");
+@@ -7123,9 +7123,9 @@
+ 
+ sub get_solib_default_paths()
+ {
+-    foreach my $Line (split("\n", `ldconfig -p`))
++    foreach my $Line (split("\n", `ldconfig -r`))
      {
--        foreach my $Line (split("\n", `ldconfig -p`))
-+        foreach my $Line (split("\n", `ldconfig -r`))
+-        if($Line=~/\A[ \t]*([^ \t]+) .* \=\> (.+)\Z/)
++        if($Line=~/\A\t[0-9]*:(.+) \=\> (.+)\Z/)
          {
--            if($Line=~/\A[ \t]*([^ \t]+) .* \=\> (.+)\Z/)
-+            if($Line=~/\A\t[0-9]*:(.+) \=\> (.+)\Z/)
-             {
-                 my ($SoCandidate, $Path) = ($1, $2);
-                 
+             $SoLib_DefaultPath{$1} = $2;
+         }
