b27e4b2d3361a3119063909a5dcc4974af94117d
amdmi3 <amdmi3@FreeBSD.org>
2010-02-18 20:52:28 +0000

    - Update to 1.0.2
    Feature safe:	yes

--- games/bitefusion/files/patch-main.c
+++ games/bitefusion/files/patch-main.c
@@ -1,18 +1,17 @@
---- main.c.orig	Thu Oct  5 18:49:20 2006
-+++ main.c	Wed Nov  1 14:09:15 2006
-@@ -5,7 +5,7 @@
+--- main.c.orig	2010-01-31 20:07:57.000000000 +0300
++++ main.c	2010-02-18 22:34:59.000000000 +0300
+@@ -5,7 +5,6 @@
  #define __BIT_ENDIAN 1234
  #define __BYTE_ORDER __LITTLE_ENDIAN
  #else
 -#include <endian.h>
-+//#include <endian.h>
  #endif
  
  #include <SDL/SDL.h>
-@@ -17,6 +17,7 @@
- #include <math.h>
+@@ -18,6 +17,7 @@
  #include <stdlib.h>
  #include <string.h>
+ #include <getopt.h>
 +#include <unistd.h>
  
  #include "error.h"
