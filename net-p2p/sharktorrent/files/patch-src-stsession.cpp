2af396cd0d0265d32f8ac86621e1a44609fa9f26
amdmi3 <amdmi3@FreeBSD.org>
2010-02-19 05:38:19 +0000

    - Update to 0.1.8.4 - Take maintainership
    Approved by:	dougb (via private email)
    Feature safe:	yes

---
+++ net-p2p/sharktorrent/files/patch-src-stsession.cpp
@@ -0,0 +1,11 @@
+--- src/stsession.cpp.orig	2009-07-06 01:40:21.000000000 +0400
++++ src/stsession.cpp	2010-02-17 19:01:03.000000000 +0300
+@@ -501,7 +501,7 @@
+ 	for (std::vector<peer_info>::const_iterator i = peers.begin();
+ 						i != peers.end(); ++i, _icount++)
+ 	{
+-		asio::ip::address _address =  i->ip.address();
++		libtorrent::asio::ip::address _address =  i->ip.address();
+ 
+ 		m_map_peers[_icount].append( QString("%1").arg(_icount)  );
+ 		m_map_peers[_icount].append( QString(_address.to_string().c_str()) );
