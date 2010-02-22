064dd3de5a1ad06fd7de9800e549f6453ecf8909
wxs <wxs@FreeBSD.org>
2010-02-22 14:22:45 +0000

    - Update to 1.8.7 - Pass maintainer to submitter
    PR:		ports/142513
    Submitted by:	Ben Kaduk <kaduk@mit.edu>
    Feature safe:	yes

--- science/vmd/files/patch-utilities.C
+++ science/vmd/files/patch-utilities.C
@@ -1,11 +1,11 @@
 --- src/utilities.C.orig	Sat Jan 22 16:15:24 2005
 +++ src/utilities.C	Sat Jan 22 16:15:45 2005
-@@ -275,7 +275,7 @@
+@@ -396,7 +396,7 @@
      return stringdup("Windows User");
    }
  #else
--#if defined(ARCH_FREEBSD) || defined(ARCH_MACOSX) || defined(ARCH_LINUX) || defined(ARCH_LINUXALPHA) || defined(ARCH_LINUXAMD64) || defined(ARCH_LINUXPPC)
-+#if defined(ARCH_FREEBSD) || defined(ARCH_FREEBSDAMD64) || defined(ARCH_MACOSX) || defined(ARCH_LINUX) || defined(ARCH_LINUXALPHA) || defined(ARCH_LINUXAMD64) || defined(ARCH_LINUXPPC)
+-#if defined(ARCH_FREEBSD) || defined(__APPLE__) || defined(__linux)
++#if defined(ARCH_FREEBSD) || defined(ARCH_FREEBSDAMD64) || defined(__APPLE__) || defined(__linux)
    return stringdup(getlogin());
  #else
    return stringdup(cuserid(NULL));
