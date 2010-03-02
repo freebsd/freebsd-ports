c04f872b71a1d46e5d2fc469d57445ed3cfcaefa
sylvio <sylvio@FreeBSD.org>
2010-03-02 00:01:09 +0000

    - Update to 1.1.0 - Add my site in MASTER_SITES - Removed OPTIONS because this port only run with all this OPTIONS.
    Approved by:	itetcu (mentor)
    Feature safe:	yes

--- games/pydance/files/patch-fontfx.py
+++ games/pydance/files/patch-fontfx.py
@@ -1,6 +1,6 @@
---- fontfx.py.orig      Wed Feb 14 02:45:59 2007
-+++ fontfx.py   Wed Feb 14 02:50:23 2007
-@@ -144,7 +144,7 @@
+--- fontfx.py.orig	2008-12-30 13:04:11.000000000 -0200
++++ fontfx.py	2010-02-05 23:37:31.000000000 -0200
+@@ -141,7 +141,7 @@
      self.bgcolor = bgcolor
      self.offcolor = [c^40 for c in color]
      self.notcolor = [c^0xFF for c in color]
@@ -9,13 +9,12 @@
      self.text.set_colorkey(1, RLEACCEL)
      self.outline = self.textHollow(font, message, color)
      self.bar = pygame.Surface(self.text.get_size())
-@@ -155,7 +155,7 @@
+@@ -152,7 +152,7 @@
      self.ratio = width / 100.0
-
+ 
    def textHollow(self, font, message, fontcolor):
--    base = font.render(message, 0, fontcolor, self.notcolor)
+-    base = font.render(message, False, fontcolor, self.notcolor)
 +    base = font.render(message, True, fontcolor, self.notcolor)
      size = base.get_width() + 2, base.get_height() + 2
      img = pygame.Surface(size, 16)
      img.fill(self.notcolor)
-
