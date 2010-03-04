072c396444bf8d2a4db5c01820e90b39b7ee0c99
itetcu <itetcu@FreeBSD.org>
2010-03-04 17:00:47 +0000

    - Update to 2.7.0 - users wanting to continue on 2.6 series should use mail/postfix26
    PR:		144189
    Submitted by:	Sahil Tandon (maintainer)
    Feature safe:	yes

--- mail/postfix27/files/patch-src::util::sys_defs.h
+++ mail/postfix27/files/patch-src::util::sys_defs.h
@@ -1,22 +1,21 @@
---- src/util/sys_defs.h.orig	2009-05-11 03:45:27.000000000 +0300
-+++ src/util/sys_defs.h	2009-07-22 21:26:44.208909929 +0300
-@@ -25,6 +25,7 @@
+--- src/util/sys_defs.h.orig	2010-02-16 20:15:11.000000000 -0500
++++ src/util/sys_defs.h	2010-02-16 20:19:41.000000000 -0500
+@@ -25,7 +25,7 @@
    */
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
+-    || defined(FREEBSD8) \
 +    || defined(FREEBSD8) || defined(FREEBSD9) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(NETBSD1) || defined(NETBSD2) || defined(NETBSD3) \
-@@ -110,6 +111,11 @@
+@@ -111,7 +111,8 @@
  #define HAS_DUPLEX_PIPE			/* 4.1 breaks with kqueue(2) */
  #endif
  
+-#if __FreeBSD_version >= 800107		/* safe; don't believe the experts */
 +#if (__FreeBSD_version >= 702104 && __FreeBSD_version <= 800000) \
 +    || __FreeBSD_version >= 800100
-+#define HAS_CLOSEFROM
-+#endif
-+
- /* OpenBSD version is year+month */
+ #define HAS_CLOSEFROM
+ #endif
  
- #if OpenBSD >= 199805			/* XXX */
