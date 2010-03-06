a96d556c0f2dee123394fcb06bbd8d58caada58d
wxs <wxs@FreeBSD.org>
2010-03-06 01:41:24 +0000

    - Update to 0.5.0
    PR:		ports/144473
    Submitted by:	Marco BrÃ¶der <marco.broeder@gmx.eu> (maintainer)
    Feature safe:	yes

--- devel/dulwich/files/patch-setup.py
+++ devel/dulwich/files/patch-setup.py
@@ -1,15 +1,14 @@
---- ./setup.py.orig     2010-01-03 19:09:07.000000000 +0100
-+++ ./setup.py  2010-01-03 19:09:19.000000000 +0100
+--- ./setup.py.orig     2010-03-03 16:33:52.000000000 +0100
++++ ./setup.py  2010-03-03 21:13:31.000000000 +0100
 @@ -2,10 +2,7 @@
  # Setup file for bzr-git
- # Copyright (C) 2008-2009 Jelmer Vernooij <jelmer@samba.org>
+ # Copyright (C) 2008-2010 Jelmer Vernooij <jelmer@samba.org>
 
 -try:
--    from setuptools import setup
+-    from setuptools import setup, Extension
 -except ImportError:
--    from distutils.core import setup
-+from distutils.core import setup
- from distutils.extension import Extension
+-    from distutils.core import setup, Extension
++from distutils.core import setup, Extension
+ from distutils.core import Distribution
 
- dulwich_version_string = '0.4.1'
-
+ dulwich_version_string = '0.5.0'
