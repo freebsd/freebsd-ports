16ad276e69ef9a78ba1b6abcac2f39a0eaa5609f
yzlin <yzlin@FreeBSD.org>
2010-03-09 13:40:33 +0000

    Add bpython-0.9.6.2, bpython is a fancy interface to the Python interpreter for Unix-like operating systems.
    Approved by:	lwhsu/rafan (mentors, implicit)
    Feature safe:	yes

---
+++ devel/bpython/files/patch-setup.py
@@ -0,0 +1,19 @@
+
+$FreeBSD$
+
+--- setup.py.orig
++++ setup.py
+@@ -37,13 +37,8 @@
+     url = "http://www.bpython-interpreter.org/",
+     long_description = """bpython is a fancy interface to the Python
+     interpreter for Unix-like operating systems.""",
+-    install_requires = [
+-        'pygments'
+-    ],
+     packages = ["bpython"],
+     data_files = [
+-        (os.path.join(man_dir, 'man1'), ['doc/bpython.1']),
+-        (os.path.join(man_dir, 'man5'), ['doc/bpython-config.5']),
+         ('share/applications', ['data/bpython.desktop'])
+     ],
+     package_data = {'bpython': ['logo.png']},
