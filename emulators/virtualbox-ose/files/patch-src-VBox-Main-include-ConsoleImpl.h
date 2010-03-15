ac21f2df2024403d2112ffa190793084e82fa7b7
miwi <miwi@FreeBSD.org>
2010-03-15 20:54:35 +0000

    Presenting VirtualBox 3.1.4 for FreeBSD
    Changelog from VirtualBox is available here:
    http://www.virtualbox.org/wiki/Changelog
    
    Changes in the port:
      - VirtualBox and Guest Additions iso updated to 3.1.4
      - Add UNIQUENAME to fix options file clashes between the virtualbox ports [0]
      - Fixed vtophys problems on CURRENT and STABLE kernes newer than
        January 23th [1]
      - Fixed page double wiring issue in rtR0MemObjNativeAllocPage [2]
    
    PR: 		ports/143361 [0]
    Submitted by:	Martin Birgmeier <martin.birgmeier AT aon.at> [0]
    		Alexander Eichner <Alexander.Eichner AT Sun.COM> [1]
    Thanks To:	All testers on emulation@ and current@ [1]
               	Giovanni Trematerra <giovanni.trematerra AT gmail.com> [1]
    		Ian Freislich <ianf AT clue.co.za> [1]
    Reported by: 	Alan Cox <alc AT cs dot rice dot edu> [2]

--- emulators/virtualbox-ose/files/patch-src-VBox-Main-include-ConsoleImpl.h
+++ emulators/virtualbox-ose/files/patch-src-VBox-Main-include-ConsoleImpl.h
@@ -1,6 +1,6 @@
---- src/VBox/Main/include/ConsoleImpl.h.orig
-+++ src/VBox/Main/include/ConsoleImpl.h
-@@ -410,7 +410,7 @@ private:
+--- src/VBox/Main/include/ConsoleImpl.h.orig	2010-02-12 19:49:27.000000000 +0000
++++ src/VBox/Main/include/ConsoleImpl.h	2010-02-13 14:26:54.000000000 +0000
+@@ -410,7 +410,7 @@
  
      HRESULT callTapSetupApplication(bool isStatic, RTFILE tapFD, Bstr &tapDevice,
                                      Bstr &tapSetupApplication);
@@ -9,12 +9,12 @@
      HRESULT attachToTapInterface(INetworkAdapter *networkAdapter);
      HRESULT detachFromTapInterface(INetworkAdapter *networkAdapter);
  #endif
-@@ -582,7 +582,7 @@ private:
-     PPDMLED     mapNetworkLeds[SchemaDefs::NetworkAdapterCount];
-     PPDMLED     mapSharedFolderLed;
-     PPDMLED     mapUSBLed[2];
+@@ -592,7 +592,7 @@
+     PPDMLED      mapNetworkLeds[SchemaDefs::NetworkAdapterCount];
+     PPDMLED      mapSharedFolderLed;
+     PPDMLED      mapUSBLed[2];
 -#if !defined(VBOX_WITH_NETFLT) && (defined(RT_OS_LINUX) || defined(RT_OS_FREEBSD))
 +#if ((defined(RT_OS_LINUX) && !defined(VBOX_WITH_NETFLT)) || defined(RT_OS_FREEBSD))
-     Utf8Str     maTAPDeviceName[8];
-     RTFILE      maTapFD[8];
+     Utf8Str      maTAPDeviceName[8];
+     RTFILE       maTapFD[8];
  #endif
