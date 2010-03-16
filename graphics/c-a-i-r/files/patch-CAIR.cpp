295b2ceef7583911316c60b763d2876763c9404f
wen <wen@FreeBSD.org>
2010-03-16 01:07:34 +0000

    - Update to 2.19
    PR:		ports/144371
    Submitted by:	Kuan-Chung Chiu <buganini@gmail.com> (maintainer)

--- graphics/c-a-i-r/files/patch-CAIR.cpp
+++ graphics/c-a-i-r/files/patch-CAIR.cpp
@@ -1,56 +1,38 @@
---- CAIR.cpp.orig	2008-10-19 19:15:39.000000000 +0800
-+++ CAIR.cpp	2008-10-19 19:17:24.000000000 +0800
-@@ -252,7 +252,7 @@
+--- CAIR.cpp.orig	2010-03-16 08:51:06.000000000 +0800
++++ CAIR.cpp	2010-03-16 08:55:11.000000000 +0800
+@@ -276,7 +276,7 @@
  //Our thread function for the Grayscale
  void * Gray_Quadrant( void * id )
  {
--	int num = (int)id;
-+	long int num = (long int)id;
+-	int num = *((int *)id);
++	long int num = *((long int *)id);
  
  	while( true )
  	{
-@@ -417,7 +417,7 @@
- //This is multi-threaded to 4 threads, spliting the image into 4 strips
+@@ -437,7 +437,7 @@
+ //The thread function, splitting the image into strips
  void * Edge_Quadrant( void * id )
  {
--	int num = (int)id;
-+	long int num = (long int)id;
+-	int num = *((int *)id);
++	long int num = *((long int *)id);
  
  	while( true )
  	{
-@@ -612,7 +612,7 @@
- //=========================================================================================================//
- void * Energy_Left( void * id )
- {
--	int num = (int)id;
-+	long int num = (long int)id;
- 	int energy = 0;// current calculated enery
- 	int min_x = 0, max_x = 0;
- 
-@@ -727,7 +727,7 @@
- //=========================================================================================================//
- void * Energy_Right( void * id )
- {
--	int num = (int)id;
-+	long int num = (long int)id;
- 	int energy = 0;// current calculated enery
- 	int min_x = 0, max_x = 0;
- 
-@@ -938,7 +938,7 @@
- //This works like Remove_Quadrant, stripes across the image.
+@@ -744,7 +744,7 @@
+ //This works like Remove_Quadrant, strips across the image.
  void * Add_Quadrant( void * id )
  {
--	int num = (int)id;
-+	long int num = (long int)id;
+-	int num = *((int *)id);
++	long int num = *((long int *)id);
  	Thread_Params add_area;
  
  	while( true )
-@@ -1245,7 +1245,7 @@
+@@ -907,7 +907,7 @@
  //the areas are not quadrants, rather, more like strips, but I keep the name convention
  void * Remove_Quadrant( void * id )
  {
--	int num = (int)id;
-+	long int num = (long int)id;
+-	int num = *((int *)id);
++	long int num = *((long int *)id);
  	Thread_Params remove_area;
  
  	while( true )
