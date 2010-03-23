db2c49723a693c67b0403e054740835d757b9bcd
gahr <gahr@FreeBSD.org>
2010-03-23 21:06:53 +0000

    - Update x11-toolkits/fltk to 1.1.10 - Unconditionally enable threads on x11-toolkits/fltk - Mark x11-toolkits/fltk-threads as IGNORE (master port has threads now)

--- x11-toolkits/fltk/files/patch-src_filename_list.cxx
+++ x11-toolkits/fltk/files/patch-src_filename_list.cxx
@@ -1,5 +1,5 @@
---- src/filename_list.cxx.orig	2006-06-09 18:16:34.000000000 +0200
-+++ src/filename_list.cxx	2010-02-15 14:57:40.000000000 +0100
+--- src/filename_list.cxx.orig	2010-03-09 08:52:26.000000000 +0100
++++ src/filename_list.cxx	2010-03-09 08:54:53.000000000 +0100
 @@ -30,7 +30,7 @@
  #include <FL/filename.H>
  #include "flstring.h"
@@ -13,8 +13,8 @@
                       Fl_File_Sort_F *sort) {
  #ifndef HAVE_SCANDIR
    int n = scandir(d, list, 0, sort);
--#elif defined(__hpux) || defined(__CYGWIN__)
-+#elif defined(__hpux) || defined(__CYGWIN__) || (defined(__FreeBSD__)  && (__FreeBSD_version >= 900006 || (__FreeBSD_version >= 800501 && __FreeBSD_version < 900000) || (__FreeBSD_version >= 730000 && __FreeBSD_version < 800000)))
-   // HP-UX, Cygwin define the comparison function like this:
+-#elif defined(HAVE_SCANDIR_POSIX)
++#elif defined(HAVE_SCANDIR_POSIX) || (defined(__FreeBSD__)  && (__FreeBSD_version >= 900006 || (__FreeBSD_version >= 800501 && __FreeBSD_version < 900000) || (__FreeBSD_version >= 730000 && __FreeBSD_version < 800000)))
+   // POSIX (2008) defines the comparison function like this:
    int n = scandir(d, list, 0, (int(*)(const dirent **, const dirent **))sort);
  #elif defined(__osf__)
