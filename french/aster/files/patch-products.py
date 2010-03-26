40f9eb7e76a03f92fa54ab9a3b867bdf35c336f3
Thierry Thomas <thierry@FreeBSD.org>
2010-03-26 18:30:58 +0000

    - use $SUB_FILES to dynamically adjust pkg-message (1);
    - upgrade Code_Aster to 10.1.0-4 (2).
    
    PR:		ports/142516 (1)
    Submitted by:	Sevan Janiyan <venture37 (at) geeklan.co.uk> (1)

--- french/aster/files/patch-products.py
+++ french/aster/files/patch-products.py
@@ -1,16 +1,16 @@
---- products.py.orig	2009-01-16 15:50:07.000000000 +0100
-+++ products.py	2009-01-19 22:34:19.000000000 +0100
-@@ -934,16 +934,19 @@
-            'HOME_PYTHON', 'PYTHON_EXE', 'PYTHONLIB', 'PYMODULES_PREFIX',
-            'HOME_MUMPS', 'HOME_ZMAT', 'HOME_MPI', 'INCLUDE_MUMPS',
-            'HOME_MED', 'HOME_HDF', 'HOME_CRPCRS',
+--- products.py.orig	2010-02-09 10:04:58.000000000 +0100
++++ products.py	2010-02-17 21:35:50.000000000 +0100
+@@ -900,16 +900,19 @@
+            'HOME_PYTHON', 'PYTHON_EXE', 'PYTHONLIB',
+            'HOME_MUMPS', 'HOME_ZMAT', 'HOME_MPI', 'INCLUDE_MUMPS', 'HOME_METIS',
+            'HOME_MED', 'HOME_HDF', 'HOME_CRPCRS', 'HOME_NUMERIC',
 -           'LD', 'CC', 'F77', 'F90', 'CXXLIB', 'OTHERLIB',],
 +           'LD', 'CC', 'F77', 'F90', 'LDFLAGS', 'SYSLIB',
 +           'CFLAGS', 'CFLAGS_DBG',
 +           'FFLAGS', 'FFLAGS_DBG',
 +           'F90FLAGS', 'F90FLAGS_DBG', 'CXXLIB', 'OTHERLIB',],
-       reqobj=['file:?HOME_ASTK?/ASTK_SERV/bin/as_run',
-               'file:?HOME_ASTK?/ASTK_SERV/conf/aster_profile.sh'],
+       reqobj=['file:?ASTER_ROOT?/bin/as_run',
+               'file:?ASTER_ROOT?/etc/codeaster/profile.sh'],
        set=['SYSLIB',
             'MEDLIB', 'HDFLIB', 'MATHLIB',
             'MUMPSLIB', 'ZMATLIB', 'SCOTCHLIB',
@@ -24,7 +24,7 @@
             'NOBUILD', ],
     )
     cfg['ENV_SH']   = cfg.get('ENV_SH', '')
-@@ -978,6 +981,19 @@
+@@ -945,6 +948,19 @@
        opt['FFLAGS']     = '-O2'
        zmat_platform='Linux'
        mpilibs.extend(['mpich'])
@@ -44,11 +44,29 @@
     elif cfg['IFDEF'] == 'LINUX64':
        opt['SYSLIB']     = '-Wl,--allow-multiple-definition -Wl,--export-dynamic -lieee -ldl -lutil -lm'
        opt['LDFLAGS']    = '-v'
-@@ -1059,12 +1075,15 @@
+@@ -1017,11 +1033,12 @@
+       cfg['MEDLIB']=''
+ 
+    # ----- libs c++ (for MED and ZMAT)
+-   if cfg['HOME_MED'] != '' or cfg['HOME_ZMAT'] != '':
+-      for lib in cxxlibs:
+-         ftools.findlib_and_set(cfg, 'CXXLIB', lib,
+-            [cfg['HOME_MED'], cfg['HOME_ZMAT']],
+-            err=False, append=True, maxdepth=max(ftools.maxdepth,10))
++   # CXXLIB already set for FreeBSD
++   #if cfg['HOME_MED'] != '' or cfg['HOME_ZMAT'] != '':
++   #   for lib in cxxlibs:
++   #      ftools.findlib_and_set(cfg, 'CXXLIB', lib,
++   #         [cfg['HOME_MED'], cfg['HOME_ZMAT']],
++   #         err=False, append=True, maxdepth=max(ftools.maxdepth,10))
+ 
     # ----- MUMPS
     if cfg['HOME_MUMPS'] != '':
-       cfg['DEFINED'] += ' _HAVE_MUMPS'
--      for lib in ('dmumps', 'zmumps', 'pord', 'mpiseq'):
+@@ -1030,12 +1047,15 @@
+       if not less_than_version(dict_prod['mumps'], '4.8.0'):
+          mumps_lib.extend(['smumps', 'cmumps', 'mumps_common'])
+       mumps_lib.extend(['pord', 'mpiseq'])
+-      for lib in mumps_lib:
 -         ftools.findlib_and_set(cfg, 'MUMPSLIB', lib,
 -            cfg['HOME_MUMPS'],
 -            err=False, append=True)
@@ -57,21 +75,12 @@
 +      if cfg['MUMPSLIB'] != '':
 +         opt['MUMPSLIB'] =  cfg['MUMPSLIB']
 +      else:
-+         for lib in ('dmumps', 'zmumps', 'pord', 'mpiseq'):
++         for lib in mumps_lib:
 +            ftools.findlib_and_set(cfg, 'MUMPSLIB', lib,
 +               cfg['HOME_MUMPS'],
 +               err=False, append=True)
 +            if lib == 'dmumps':
 +               ftools.CheckFromLastFound(cfg, 'HOME_MUMPS', 'lib')
+       if cfg['HOME_METIS'] != '':
+          cfg['MUMPSLIB'] += " -L%s/lib -lmetis" % cfg['HOME_METIS']
        opt['F90INCLUDE'] += ' -I%s' % os.path.join(cfg['ASTER_ROOT'],cfg['ASTER_VERSION'],'bibf90',cfg['INCLUDE_MUMPS'])
-       if cfg['HOME_MPI'] != '':
-          cfg['DEFINED'] += ' _USE_MPI_MUMPS'
-@@ -1120,7 +1139,7 @@
-             cfg['HOME_SCOTCH'],
-             err=True, append=True)
-          if lib == 'common':
--            ftools.CheckFromLastFound(cfg, 'HOME_SCOTCH', 'bin')
-+            ftools.CheckFromLastFound(cfg, 'HOME_SCOTCH', 'lib')
-       ftools.find_and_set(cfg, 'CINCLUDE', 'scotchf.h',
-          cfg['HOME_SCOTCH'],
-          typ='inc', err=True, append=True)
--- french/aster/files/patch-setup.py
+++ french/aster/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2008-12-23 18:01:02.000000000 +0100
-+++ setup.py	2009-01-04 17:54:20.000000000 +0100
-@@ -296,6 +296,14 @@
+--- ./setup.py.orig	2010-02-09 10:04:59.000000000 +0100
++++ ./setup.py	2010-02-11 23:32:34.000000000 +0100
+@@ -318,6 +318,14 @@
              cfg['ARCH'] = 'x86_64'
        else:
           cfg['IFDEF'] = 'LINUX'
@@ -15,7 +15,21 @@
     elif sys.platform[:4] == 'osf1':
        cfg['IFDEF']='TRU64'
     elif sys.platform == 'sunos5':
-@@ -509,6 +517,7 @@
+@@ -442,13 +450,6 @@
+    if not type(math_lib) in (list, tuple):
+       math_lib = [math_lib,]
+    sys_lib = []
+-   for glob_lib in ('pthread', 'z',):
+-      cfg['__tmp__'] = ''
+-      del cfg['__tmp__']
+-      ftools.findlib_and_set(cfg, '__tmp__', glob_lib, prefshared=True, err=False, silent=False)
+-      if cfg.get('__tmp__'):
+-         ftools.AddToCache('lib', glob_lib, cfg['__tmp__'])
+-         sys_lib.append(glob_lib)
+ 
+    # 1.4.2. ----- check for compilers
+    cfg_ini = cfg.copy()
+@@ -541,6 +542,7 @@
     # and modify command line of those which don't have a --display= option
     ListEdit=[
        ['nedit' , 'nedit',],
