ac00cca6e50cdd00ccce56b0121c6a16a2f7d066
Dirk Meyer <dinoex@FreeBSD.org>
2010-03-28 09:22:32 +0000

    - fix breakage with png-1.4.1

--- graphics/gd/files/patch-gd_png.c
+++ graphics/gd/files/patch-gd_png.c
@@ -1,6 +1,15 @@
---- gd_png.c.orig	Sat Feb  3 02:41:00 2007
-+++ gd_png.c	Tue Feb 13 15:31:57 2007
-@@ -189,6 +189,16 @@
+--- gd_png.c.orig	2007-06-14 21:51:41.000000000 +0200
++++ gd_png.c	2010-03-28 11:23:08.000000000 +0200
+@@ -149,7 +149,7 @@
+ 	return NULL;
+   }
+ 
+-	if (!png_check_sig (sig, 8)) { /* bad signature */
++	if (!png_sig_cmp (sig, 0, 8)) { /* bad signature */
+     	return NULL;		/* bad signature */
+ 	}
+ 
+@@ -193,6 +193,16 @@
    png_read_info (png_ptr, info_ptr);	/* read all PNG info up to image data */
  
    png_get_IHDR (png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, &interlace_type, NULL, NULL);
