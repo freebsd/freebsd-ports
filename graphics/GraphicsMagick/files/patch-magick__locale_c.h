0ab000c3947acd7f76640a011e5902f9d4f18675
Dirk Meyer <dinoex@FreeBSD.org>
2010-03-28 18:22:49 +0000

    - fix build for png-1.4.1 PR:		145111

---
+++ graphics/GraphicsMagick/files/patch-magick__locale_c.h
@@ -0,0 +1,11 @@
+--- magick/locale_c.h.orig	2004-07-06 02:06:31.000000000 +0200
++++ magick/locale_c.h	2010-03-28 20:13:08.000000000 +0200
+@@ -251,7 +251,7 @@
+ #define MGK_OptionErrorStereoImageRequired 244
+ #define MGK_OptionErrorSubimageSpecificationReturnsNoImages 245
+ #define MGK_OptionErrorUnableToAddOrRemoveProfile 246
+-#define MGK_OptionErrorUnableToAllocateICCProfile 247
++#define MGK_ResourceLimitErrorUnableToAllocateICCProfile 247
+ #define MGK_OptionErrorUnableToAverageImageSequence 248
+ #define MGK_OptionErrorUnableToBlurImage 249
+ #define MGK_OptionErrorUnableToChopImage 250
