800eb659354b9e393ecbfb8a75a26e9212bce24b
glewis <glewis@FreeBSD.org>
2010-04-04 04:43:04 +0000

    . Split the kernel mudlib out of the main dgd port and into its own   separate port (dgd-kernel) to put it on an equal footing with the   other mudlib port (dgd-lpmud).  This also causes less update churn   and reflects the current state of the distributions themselves.

---
+++ net/dgd-kernel/files/dgd-kernel.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# $FreeBSD: /tmp/pcvs/ports/net/dgd-kernel/files/dgd-kernel.sh,v 1.1 2010-04-04 04:43:04 glewis Exp $
+
+DGD_CONF=%%PREFIX%%/etc/dgd-kernel/kernel.dgd
+DGD_USER=mud
+DGD=%%PREFIX%%/sbin/dgd
+DGD_LOG=%%DATADIR%%/kernel.log
+DGD_PID=/var/run/dgd-kernel.pid
+
+case "$1" in
+	start)
+		if [ -x "${DGD}" ]; then
+			su ${DGD_USER} -c "${DGD} ${DGD_CONF}" >> "${DGD_LOG}" 2>&1 &
+			echo $(($!+2)) > "${DGD_PID}"
+			echo -n ' dgd'
+		fi
+		;;
+	stop)
+		kill -TERM `cat "${DGD_PID}"` && rm -f "${DGD_PID}"
+		;;
+	*)
+		echo ""
+		echo "Usage: `basename $0` { start | stop }"
+		echo ""
+		exit 64
+		;;
+esac
