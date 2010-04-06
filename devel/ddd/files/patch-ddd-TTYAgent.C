9aa11b58e93dc5b2b2f287334000c180c02027d1
kan <kan@FreeBSD.org>
2010-04-06 18:33:47 +0000

    Update to 3.3.12 and silence a long standing bug in how DDD handles controlling terminals for child processes.
    No response from maintainer since: Sat, 22 Aug 2009

--- devel/ddd/files/patch-ddd-TTYAgent.C
+++ devel/ddd/files/patch-ddd-TTYAgent.C
@@ -1,6 +1,16 @@
---- ddd/TTYAgent.C.orig	Sat Nov  6 19:05:52 2004
-+++ ddd/TTYAgent.C	Sat Nov  6 19:08:23 2004
-@@ -715,8 +715,19 @@
+--- ddd/TTYAgent.C.orig	2009-02-11 12:25:07.000000000 -0500
++++ ddd/TTYAgent.C	2009-08-22 23:43:03.712807083 -0400
+@@ -280,7 +280,9 @@
+ #if !defined(__osf__) && \
+     HAVE_PTSNAME && HAVE_GRANTPT && HAVE_UNLOCKPT && HAVE_IOCTL
+ #define HAVE_UNIX98PTYS 1
++#if !defined(__FreeBSD__)
+ #define HAVE_STREAMS_IMPLEMENTATION 1
++#endif
+ // FIXME: We should have a proper test for this.
+ // For instance, GNU libc 2.1 on Linux (and probably HURD too) has UNIX98-style
+ // pty allocation functions, but does not have a STREAMS-based implementation.
+@@ -715,8 +717,19 @@
      }
      else
      {
