45433f042765a5e650d56b9e63cfa6683c860bed
wen <wen@FreeBSD.org>
2010-04-07 01:50:33 +0000

    - Update to 0.5.0 - Pass maintainership to submitter
    PR:		ports/145431
    Submitted by:	Gvozdikov Veniamin <g.veniamin@googlemail.com>
    Approved by:	maintainer

--- net-im/py-xmpppy/files/patch-xmpp-auth.py
+++ net-im/py-xmpppy/files/patch-xmpp-auth.py
@@ -1,14 +1,20 @@
---- xmpp/auth.py.orig	2009-12-10 20:43:19.000000000 +0800
-+++ xmpp/auth.py	2009-12-10 20:44:17.000000000 +0800
-@@ -21,9 +21,9 @@
+--- xmpp/auth.py.orig	2010-04-03 21:45:50.000000000 +0800
++++ xmpp/auth.py	2010-04-03 21:45:56.000000000 +0800
+@@ -19,13 +19,13 @@ Provides library with all Non-SASL and S
+ Can be used both for client and transport authentication.
+ """
  
++from hashlib import md5
  from protocol import *
  from client import PlugIn
 -import sha,base64,random,dispatcher,re
 +import base64,random,dispatcher,re
  
 -import md5
-+import hashlib 
- def HH(some): return md5.new(some).hexdigest()
- def H(some): return md5.new(some).digest()
+-def HH(some): return md5.new(some).hexdigest()
+-def H(some): return md5.new(some).digest()
++def HH(some): return md5(some).hexdigest()
++def H(some): return md5(some).digest()
  def C(some): return ':'.join(some)
+ 
+ class NonSASL(PlugIn):
