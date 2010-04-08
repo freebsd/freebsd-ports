6723e1a10ae255a32ca7054e229f764eb7b58e31
demon <demon@FreeBSD.org>
2010-04-08 08:54:52 +0000

    Unbreak on recent versions of FreeBSd-8.
    Submitted by:	Brian Powell <bp2626@yahoo.com>

---
+++ sysutils/xfce4-netload-plugin/files/patch-panel-plugin-os.h
@@ -0,0 +1,10 @@
+--- panel-plugin/os.h.orig	2010-04-08 12:52:28.000000000 +0400
++++ panel-plugin/os.h	2010-04-08 12:52:47.000000000 +0400
+@@ -95,7 +95,6 @@
+ #include <net/if_mib.h>
+ #include <arpa/inet.h>
+ #include <net/ppp_defs.h>
+-#include <net/if_ppp.h>
+ #elif __NetBSD__            /* N E T B S D */
+ #include <stdio.h>
+ #include <sys/param.h>
