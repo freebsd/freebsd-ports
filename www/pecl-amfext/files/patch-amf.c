89efa68f704186b3d00b7637485f9c31bcc4c818
Greg Larkin <glarkin@FreeBSD.org>
2010-04-12 20:53:37 +0000

    - Unbreak after recent PHP 5.3.2 update
    Reported by:	pointyhat
    Approved by:	portmgr (itetcu, erwin - implicit)

---
+++ www/pecl-amfext/files/patch-amf.c
@@ -0,0 +1,20 @@
+--- ./amf.c.orig	2010-04-12 16:48:28.000000000 -0400
++++ ./amf.c	2010-04-12 16:51:32.000000000 -0400
+@@ -1185,7 +1185,7 @@
+ 		int resultType = AMFC_TYPEDOBJECT;
+ 		int resultValueLength = 0;
+ 		zval** resultValue = struc;
+-		int deallocResult = (*struc)->refcount;
++		int deallocResult = Z_REFCOUNT_P((*struc));
+ 
+ 		resultType = amf_perform_serialize_callback(struc, &className,&classNameLen,&resultValue,var_hash TSRMLS_CC);
+ 		
+@@ -2670,7 +2670,7 @@
+ 		}
+ 		else
+ 		{
+-			newval->refcount--;
++			Z_DELREF_P(newval);
+ 		}
+ 		*rval = newval;
+ 	}
