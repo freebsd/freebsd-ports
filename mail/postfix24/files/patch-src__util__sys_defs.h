800c5471d1d9787c94f7ef9533fe5caf20272744
Marcus Alves Grando <mnag@FreeBSD.org>
2010-04-13 13:19:32 +0000

    - Update to 2.4.14 - Fix build when WITH_SASLKRB5 is set on 8.0 & 9.0 which have the new Heimdal
    PR:		145394
    Submitted by:	Sahil Tandon <sahil___tandon.net>

--- mail/postfix24/files/patch-src__util__sys_defs.h
+++ mail/postfix24/files/patch-src__util__sys_defs.h
@@ -1,13 +1,20 @@
---- src/util/sys_defs.h.orig	2009-07-29 16:58:28.000000000 -0300
-+++ src/util/sys_defs.h	2009-07-29 16:59:15.000000000 -0300
-@@ -110,6 +110,10 @@
- #define HAS_DUPLEX_PIPE			/* 4.1 breaks with kqueue(2) */
+--- src/util/sys_defs.h.orig	2010-04-04 17:19:32.000000000 -0400
++++ src/util/sys_defs.h	2010-04-04 17:23:49.000000000 -0400
+@@ -25,7 +25,7 @@
+   */
+ #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
+     || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
+-    || defined(FREEBSD8) \
++    || defined(FREEBSD8) || defined(FREEBSD9) \
+     || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
+     || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
+     || defined(NETBSD1) || defined(NETBSD2) || defined(NETBSD3) \
+@@ -98,7 +98,7 @@
+ #define SOCKOPT_SIZE	socklen_t
  #endif
  
+-#if __FreeBSD_version >= 800107		/* safe; don't believe the experts */
 +#if (__FreeBSD_version >= 702104 && __FreeBSD_version < 800000) || (__FreeBSD_version >= 800099)
-+#define HAS_CLOSEFROM
-+#endif
-+
- /* OpenBSD version is year+month */
+ #define HAS_CLOSEFROM
+ #endif
  
- #if OpenBSD >= 199805			/* XXX */
