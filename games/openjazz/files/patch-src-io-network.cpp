afe669155be25e619dabae032c16768ff8fe4a82
wen <wen@FreeBSD.org>
2010-04-14 01:40:25 +0000

    - Update to v.20100328
    PR:		ports/145258
    Submitted by:	Ganael Laplanche <ganael.laplanche@martymac.org> (maintainer)

---
+++ games/openjazz/files/patch-src-io-network.cpp
@@ -0,0 +1,10 @@
+--- src/io/network.cpp.orig	2010-03-30 17:07:37.715288674 +0000
++++ src/io/network.cpp	2010-03-30 17:13:25.119068484 +0000
+@@ -41,6 +41,7 @@
+ 		#define MSG_NOSIGNAL 0
+ 	#else
+ 		#include <sys/types.h>
++        #include <netinet/in.h>
+ 		#include <sys/socket.h>
+ 		#include <arpa/inet.h>
+ 		#include <sys/ioctl.h>
