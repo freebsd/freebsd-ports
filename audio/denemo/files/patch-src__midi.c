1c58d05675a32ed7391d9e3e9cb669ea73d3e894
wxs <wxs@FreeBSD.org>
2010-04-16 13:16:59 +0000

    - Update to 0.8.14
    PR:		ports/145453
    Submitted by:	Joseph S. Atkinson <jsa@wickedmachine.net>
    Approved by:	multimedia@ (maintainer, via sylvio@)

--- audio/denemo/files/patch-src__midi.c
+++ audio/denemo/files/patch-src__midi.c
@@ -1,11 +1,11 @@
---- src/midi.c.orig	2009-08-24 03:03:26.000000000 +0900
-+++ src/midi.c	2009-09-28 17:25:00.000000000 +0900
-@@ -384,7 +384,7 @@
-   if(channel)
-     g_io_channel_shutdown(channel, FALSE, &error);
-   if(error)
--    g_warning(error->message);
-+    g_warning("%s", error->message);
-   else
-     channel = NULL;
- #endif
+--- ./src/midi.c.orig	2010-04-06 23:54:58.000000000 -0400
++++ ./src/midi.c	2010-04-06 23:56:14.000000000 -0400
+@@ -370,7 +370,7 @@
+     if(channel)
+       g_io_channel_shutdown(channel, FALSE, &error);
+     if(error)
+-      g_warning(error->message);
++      g_warning("%s", error->message);
+     else
+       channel = NULL;
+     return 0;
