e096acc03cf50fabcd6de409f8c8d3f4a548b119
Kevin Lo <kevlo@FreeBSD.org>
2010-04-19 07:06:28 +0000

    Update to 3.1.3
    PR:	ports/143007
    Submitted by:	bf1783 at gmail dot com

---
+++ textproc/source-highlight/files/patch-lib_srchilite_fileutil.h
@@ -0,0 +1,10 @@
+--- lib/srchilite/fileutil.h.orig	2010-01-19 00:12:06.000000000 -0500
++++ lib/srchilite/fileutil.h	2010-01-19 00:12:30.000000000 -0500
+@@ -9,6 +9,7 @@
+ 
+ #include <iostream>
+ #include <string>
++#include <cstdio>
+ #include "ioexception.h"
+ 
+ using std::istream;
