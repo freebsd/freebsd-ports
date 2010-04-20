d24ffaed20f043ef1b481b5c3571913d1b3c0f07
kevlo <kevlo@FreeBSD.org>
2010-04-20 03:43:47 +0000

    Update to 0.7

--- astro/gmapcatcher/files/patch-src_mapArgs.py
+++ astro/gmapcatcher/files/patch-src_mapArgs.py
@@ -1,5 +1,5 @@
---- src/mapArgs.py.orig	2010-03-25 15:48:48.000000000 +0800
-+++ src/mapArgs.py	2010-03-25 15:49:26.000000000 +0800
+--- src/mapArgs.py.orig	2010-04-20 10:41:04.000000000 +0800
++++ src/mapArgs.py	2010-04-20 10:42:13.000000000 +0800
 @@ -38,9 +38,9 @@
          print '                very useful to download maps of entire world'
          print ' '
