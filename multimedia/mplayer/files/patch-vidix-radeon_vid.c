c2902ebdd2f0afbf27998afd98dc20876a812226
Martin Matuska <mm@FreeBSD.org>
2010-04-21 13:07:23 +0000

    - Update to 1.0.r20100117
    Submitted by:	Thomas E. Zander <riggs@rrr.de> (maintainer, private e-mail)

--- multimedia/mplayer/files/patch-vidix-radeon_vid.c
+++ multimedia/mplayer/files/patch-vidix-radeon_vid.c
@@ -1,6 +1,6 @@
---- vidix/radeon_vid.c.orig	2007-10-08 03:49:27.000000000 +0800
-+++ vidix/radeon_vid.c	2007-10-20 16:26:57.000000000 +0800
-@@ -353,6 +353,8 @@
+--- vidix/radeon_vid.c.orig	2009-05-12 21:58:57.000000000 -0500
++++ vidix/radeon_vid.c	2009-07-23 20:43:25.733650341 -0500
+@@ -345,6 +345,8 @@
   { DEVICE_ATI_RAGE_128_PRO2, 0 },
   { DEVICE_ATI_RAGE_128_PRO3, 0 },
  /* these seem to be based on rage 128 instead of mach64 */
