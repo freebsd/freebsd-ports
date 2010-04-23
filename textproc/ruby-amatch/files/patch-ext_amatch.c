5bb91840659c433d4d2dcc542908678d54478014
dinoex <dinoex@FreeBSD.org>
2010-04-23 11:01:43 +0000

    - update to 0.2.5

--- textproc/ruby-amatch/files/patch-ext_amatch.c
+++ textproc/ruby-amatch/files/patch-ext_amatch.c
@@ -1,28 +1,25 @@
-
-$FreeBSD$
-
---- ext/amatch.c.orig
-+++ ext/amatch.c
-@@ -86,9 +86,10 @@
+--- ext/amatch.c.orig	2009-08-26 02:35:48.000000000 +0200
++++ ext/amatch.c	2010-04-23 12:48:35.000000000 +0200
+@@ -89,9 +89,10 @@
      if (TYPE(strings) == T_STRING) {                                \
          return match_function(amatch, strings);                     \
      } else {                                                        \
 -        Check_Type(strings, T_ARRAY);                               \
          int i;                                                      \
--        VALUE result = rb_ary_new2(RARRAY(strings)->len);           \
-+	VALUE result;						    \
+-        VALUE result = rb_ary_new2(RARRAY_LEN(strings));            \
++        VALUE result;                                               \
 +        Check_Type(strings, T_ARRAY);                               \
-+        result = rb_ary_new2(RARRAY(strings)->len);           \
-         for (i = 0; i < RARRAY(strings)->len; i++) {                \
++        result = rb_ary_new2(RARRAY_LEN(strings));                  \
+         for (i = 0; i < RARRAY_LEN(strings); i++) {                 \
              VALUE string = rb_ary_entry(strings, i);                \
              if (TYPE(string) != T_STRING) {                         \
-@@ -949,8 +950,8 @@
+@@ -1098,8 +1099,8 @@
      if (TYPE(strings) == T_STRING) {
          result = PairDistance_match(amatch, strings, regexp, use_regexp);
      } else {
 -        Check_Type(strings, T_ARRAY);
          int i;
 +        Check_Type(strings, T_ARRAY);
-         result = rb_ary_new2(RARRAY(strings)->len);
-         for (i = 0; i < RARRAY(strings)->len; i++) {
+         result = rb_ary_new2(RARRAY_LEN(strings));
+         for (i = 0; i < RARRAY_LEN(strings); i++) {
              VALUE string = rb_ary_entry(strings, i);
