8457bf58f0415c0920a8e5a1a86b83abda58f658
stas <stas@FreeBSD.org>
2010-04-28 19:10:27 +0000

    - Fix build without pthreads.
    PR:		ports/146112
    Reported by:	Paul <Paul.Shepel@gmail.com>

--- lang/ruby18/files/patch-process.c
+++ lang/ruby18/files/patch-process.c
@@ -1,11 +1,14 @@
---- process.c.orig	2010-04-27 01:11:32.000000000 -0700
-+++ process.c	2010-04-27 01:11:39.000000000 -0700
-@@ -920,7 +920,7 @@
+--- process.c.orig	2008-06-29 02:34:43.000000000 -0700
++++ process.c	2010-04-28 12:05:10.000000000 -0700
+@@ -920,7 +920,11 @@
  #endif
  
  #ifdef HAVE_SETITIMER
--#define before_exec() rb_thread_stop_timer()
++#if defined(_THREAD_SAFE)
 +#define before_exec() rb_thread_stop_timer_fake()
++#else
+ #define before_exec() rb_thread_stop_timer()
++#endif
  #define after_exec() rb_thread_start_timer()
  #else
  #define before_exec()
