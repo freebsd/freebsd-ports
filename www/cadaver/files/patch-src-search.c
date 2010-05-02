dbf72b2fa37051127e95bdab1081efb5e3399984
jpaetzel <jpaetzel@FreeBSD.org>
2010-05-02 18:16:52 +0000

    Update www/cadaver to latest version Add FreeBSD specific patches
    Submitted by:	"Mikhail T." <mi+thun@aldan.algebra.com>
    Approved by:	itetcu@ (mentor)

---
+++ www/cadaver/files/patch-src-search.c
@@ -0,0 +1,8 @@
+--- src/search.c	2007-08-22 08:24:56.000000000 -0400
++++ src/search.c	2010-04-16 14:31:42.000000000 -0400
+@@ -42,5 +42,4 @@
+ #include "commands.h"
+ #include "cadaver.h"
+-#include "basename.h"
+ #include "options.h"
+ #include "utils.h"
