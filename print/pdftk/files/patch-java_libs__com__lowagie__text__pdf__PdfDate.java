61f4f8ba6b14a2441f049c46c96876c169a0e385
glarkin <glarkin@FreeBSD.org>
2010-05-03 19:56:44 +0000

    - Build pdftk with gcj45 instead of gcj42 - Add a bunch of patch files to work around the Java duplicate symbol   problem reported here (http://gcc.gnu.org/bugzilla/show_bug.cgi?id=42143)   until a real fix is committed to the upstream toolchain - Bump PORTREVISION

---
+++ print/pdftk/files/patch-java_libs__com__lowagie__text__pdf__PdfDate.java
@@ -0,0 +1,11 @@
+--- ./java_libs/com/lowagie/text/pdf/PdfDate.java.orig	2006-07-26 19:56:42.000000000 -0400
++++ ./java_libs/com/lowagie/text/pdf/PdfDate.java	2010-04-08 18:13:54.000000000 -0400
+@@ -76,7 +76,7 @@
+     // ssteward; static builds of pdftk (Windows, gcc 3.3.1) would
+     // omit this class because of its reference by reflection;
+     // this treatment ensures that ld will include it
+-    private static Class c1= gnu.java.locale.Calendar.class;
++    private static Class c1= java.util.Calendar.class;
+ 
+     private static final int dateSpace[] = {Calendar.YEAR, 4, 0, Calendar.MONTH, 2, -1, Calendar.DAY_OF_MONTH, 2, 0,
+         Calendar.HOUR_OF_DAY, 2, 0, Calendar.MINUTE, 2, 0, Calendar.SECOND, 2, 0};
