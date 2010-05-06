f39d2fbf1541cd0b382703bde4680f2149d9c04c
Nicola Vitale <nivit@FreeBSD.org>
2010-05-06 19:38:08 +0000

    - Update to 1.6.1 - New download site

--- textproc/py-enchant/files/patch-setup.py
+++ textproc/py-enchant/files/patch-setup.py
@@ -1,13 +1,13 @@
---- setup.py.orig	2008-11-29 00:25:54.000000000 +0800
-+++ setup.py	2008-11-29 00:26:05.000000000 +0800
-@@ -5,8 +5,8 @@
- #  This script is placed in the public domain.
+--- setup.py	2010-02-23 01:51:41.000000000 +0100
++++ setup.py.port	2010-05-06 17:28:46.000000000 +0200
+@@ -6,8 +6,8 @@
  #
  
--import ez_setup
--ez_setup.use_setuptools()
-+#import ez_setup
-+#ez_setup.use_setuptools()
  
+-import distribute_setup
+-distribute_setup.use_setuptools()
++#import distribute_setup
++#distribute_setup.use_setuptools()
  from setuptools import setup, find_packages, Extension
  
+ import sys
