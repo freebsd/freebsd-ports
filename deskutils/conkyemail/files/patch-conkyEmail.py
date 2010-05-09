03c8a47d139b66a108d86f255a57e88216852b94
miwi <miwi@FreeBSD.org>
2010-05-09 07:50:34 +0000

    conkyemail Email inbox count script with ssl capabilities, for use in Conky
    WWW:	https://code.launchpad.net/~m-buck/+junk/conkyemail
    
    PR:		ports/146396
    Submitted by:	Gvozdikov Veniamin <g.veniamin at googlemail.com>

---
+++ deskutils/conkyemail/files/patch-conkyEmail.py
@@ -0,0 +1,8 @@
+--- conkyEmail.py.orig	2009-06-27 22:45:20.000000000 +0800
++++ conkyEmail.py	2010-05-08 02:31:30.000000000 +0800
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python
++#!/usr/bin/env python
+ # -*- coding: utf-8 -*-
+ ###############################################################################
+ # conkyEmail.py is a simple python script to gather
