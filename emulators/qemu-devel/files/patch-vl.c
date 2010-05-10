b418f601b35b14a12414db7ab685fe3a9eda6556
Juergen Lock <nox@FreeBSD.org>
2010-05-10 20:03:56 +0000

    - Update to 0.12.4, announce message is here:
    	http://lists.gnu.org/archive/html/qemu-devel/2010-05/msg00465.html
    
    - Add patch that allows running the resulting qemu{,-system-*} from the
      build dir without having to pass the pc-bios dir with -L if no qemu port
      or another version is installed.

--- emulators/qemu-devel/files/patch-vl.c
+++ emulators/qemu-devel/files/patch-vl.c
@@ -1,9 +1,28 @@
 Index: qemu/vl.c
-@@ -57,6 +57,7 @@
+@@ -57,6 +57,8 @@
  #include <sys/stat.h>
  #if defined(__FreeBSD__) || defined(__DragonFly__)
  #include <libutil.h>
 +#include <sys/param.h>
++#include <sys/sysctl.h>
  #else
  #include <util.h>
  #endif
+@@ -3368,10 +3369,13 @@ static char *find_datadir(const char *ar
+     }
+ #elif defined(__FreeBSD__)
+     {
+-        int len;
+-        len = readlink("/proc/curproc/file", buf, sizeof(buf) - 1);
+-        if (len > 0) {
+-            buf[len] = 0;
++        static int mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1};
++        size_t len = sizeof(buf) - 1;
++
++        *buf = '\0';
++        if (!sysctl(mib, sizeof(mib)/sizeof(*mib), buf, &len, NULL, 0) &&
++            *buf) {
++            buf[sizeof(buf) - 1] = '\0';
+             p = buf;
+         }
+     }
