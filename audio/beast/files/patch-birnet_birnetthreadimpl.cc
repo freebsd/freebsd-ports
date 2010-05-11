8fce5f3c0b4032dce4b61159194198996b5d804a
Koop Mast <kwm@FreeBSD.org>
2010-05-11 15:25:59 +0000

    Get patch from beast git. Now it shouldn't hang on QAT/Pointy.
    Submitted by:	QAT

--- audio/beast/files/patch-birnet_birnetthreadimpl.cc
+++ audio/beast/files/patch-birnet_birnetthreadimpl.cc
@@ -1,17 +1,27 @@
-Description: Correct signature for the g_atomic_pointer_get() function.
-Author: Jakub Wilk <ubanus@users.sf.net>
-Bug-Debian: http://bugs.debian.org/540681
-
-Index: beast-0.7.1/birnet/birnetthreadimpl.cc
-===================================================================
---- birnet/birnetthreadimpl.cc.orig	2009-12-16 20:21:24.000000000 +0100
-+++ birnet/birnetthreadimpl.cc	2009-12-16 20:26:16.000000000 +0100
-@@ -1491,7 +1491,7 @@
+--- birnet/birnetthreadimpl.cc.orig	2010-05-11 15:06:34.000000000 +0200
++++ birnet/birnetthreadimpl.cc	2010-05-11 15:07:34.000000000 +0200
+@@ -1483,18 +1483,22 @@ fallback_thread_exit (gpointer retval)
+ 
+ #ifdef g_atomic_int_get
+ static int
+-(g_atomic_int_get) (volatile int *atomic)
++birnet_g_atomic_int_get (volatile int *atomic)
+ {
+   return g_atomic_int_get (atomic);
+ }
++#undef  g_atomic_int_get
++#define g_atomic_int_get birnet_g_atomic_int_get
+ #endif
  
  #ifdef g_atomic_pointer_get
  static void*
 -(g_atomic_pointer_get) (volatile void **atomic)
-+(g_atomic_pointer_get) (void * volatile *atomic)
++birnet_g_atomic_pointer_get (volatile void **atomic)
  {
    return (void*) g_atomic_pointer_get (atomic);
  }
++#undef  g_atomic_pointer_get
++#define g_atomic_pointer_get birnet_g_atomic_pointer_get
+ #endif
+ 
+ 
