13a9ada88b952065c56953dc87511d7d44280a78
fluffy <fluffy@FreeBSD.org>
2010-05-11 11:59:56 +0000

    - Update CMake to 2.8.1 release
    With hat on:	kde@

---
+++ devel/cmake/files/patch-Modules_FindJava.cmake
@@ -0,0 +1,11 @@
+--- ./Modules/FindJava.cmake.orig	2010-04-16 16:49:52.000000000 +1100
++++ ./Modules/FindJava.cmake	2010-04-16 16:51:27.398957616 +1100
+@@ -90,7 +90,7 @@
+       # 2. OpenJDK 1.6
+       # 3. GCJ 1.5
+       # 4. Kaffe 1.4.2
+-      IF(var MATCHES "java version \"[0-9]+\\.[0-9]+\\.[0-9_]+[oem-]*\".*")
++      IF(var MATCHES "[java|openjdk] version \"[0-9]+\\.[0-9]+\\.[0-9_]+[oem-]*\".*")
+         # This is most likely Sun / OpenJDK, or maybe GCJ-java compat layer
+         STRING( REGEX REPLACE ".* version \"([0-9]+\\.[0-9]+\\.[0-9_]+)[oem-]*\".*"
+                 "\\1" Java_VERSION_STRING "${var}" )
--- devel/cmake/files/patch-Modules_FindQt4.cmake
+++ devel/cmake/files/patch-Modules_FindQt4.cmake
@@ -1,11 +1,11 @@
---- Modules/FindQt4.cmake.orig	2009-05-10 21:46:40.000000000 +0200
-+++ Modules/FindQt4.cmake	2009-05-10 21:47:58.000000000 +0200
-@@ -336,7 +336,7 @@
- 
- GET_FILENAME_COMPONENT(qt_install_version "[HKEY_CURRENT_USER\\Software\\trolltech\\Versions;DefaultQtVersion]" NAME)
+--- ./Modules/FindQt4.cmake.orig	2010-03-16 23:29:30.000000000 +0300
++++ ./Modules/FindQt4.cmake	2010-03-26 22:48:58.291402760 +0300
+@@ -392,7 +392,7 @@
  # check for qmake
--FIND_PROGRAM(QT_QMAKE_EXECUTABLE NAMES qmake qmake4 qmake-qt4 PATHS
-+FIND_PROGRAM(QT_QMAKE_EXECUTABLE NAMES qmake-qt4 qmake4 qmake PATHS
+ # Debian uses qmake-qt4
+ # macports' Qt uses qmake-mac
+-FIND_PROGRAM(QT_QMAKE_EXECUTABLE NAMES qmake qmake4 qmake-qt4 qmake-mac PATHS
++FIND_PROGRAM(QT_QMAKE_EXECUTABLE NAMES qmake-qt4 qmake qmake4 qmake-mac PATHS
    "[HKEY_CURRENT_USER\\Software\\Trolltech\\Qt3Versions\\4.0.0;InstallDir]/bin"
    "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\4.0.0;InstallDir]/bin"
    "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\${qt_install_version};InstallDir]/bin"
