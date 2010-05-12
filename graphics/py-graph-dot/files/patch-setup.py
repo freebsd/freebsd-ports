884c504b36a1579e8b6647051c1016e1f48ca2cb
yzlin <yzlin@FreeBSD.org>
2010-05-12 13:39:30 +0000

    - Update to 1.7.0
    Approved by:	lwhsu/rafan (mentors)

--- graphics/py-graph-dot/files/patch-setup.py
+++ graphics/py-graph-dot/files/patch-setup.py
@@ -1,26 +1,27 @@
+
+$FreeBSD$
+
 --- setup.py.orig
 +++ setup.py
 @@ -4,12 +4,7 @@
  import os
- import logging
+ 
  
 -try:
 -    from setuptools import setup, find_packages
--except ImportError, ie:
+-except ImportError as ie:
 -    import ez_setup
 -    ez_setup.use_setuptools()
 -    from setuptools import setup, find_packages
 +from setuptools import setup, find_packages
  
  # Startup
- appname = "python-graph"
-@@ -40,9 +35,6 @@
-         name = appname,
+ appname = "python-graph-dot"
+@@ -20,7 +15,6 @@
          version = appversion,
-         packages = ['pygraph', 'pygraph.algorithms', 'pygraph.algorithms.filters', 'pygraph.algorithms.heuristics', 'pygraph.classes', 'pygraph.readwrite'],
--        data_files = [(docsdir,docsfiles),
--                       (datadir,datafiles)],
--        install_requires = ['pydot'],
+         namespace_packages = ["pygraph"],
+         packages = ["pygraph"] + [ os.path.join("pygraph", a) for a in find_packages("pygraph") ],
+-        install_requires = [ 'python-graph-core==%s' % appversion, 'pydot' ],
          author = "Pedro Matiello",
          author_email = "pmatiello@gmail.com",
-         description = "A library for working with graphs in Python",
+         description = "DOT support for python-graph",
