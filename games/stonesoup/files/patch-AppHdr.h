9c50b1e5e8240f28a4837fa069ee7e6c72ba2da5
Dmitry Marakasov <amdmi3@FreeBSD.org>
2010-05-14 14:12:48 +0000

    - Update to 0.6.0
    PR:		145525
    Submitted by:	Tobias Rehbein <tobias.rehbein@web.de> (maintainer)

--- games/stonesoup/files/patch-AppHdr.h
+++ games/stonesoup/files/patch-AppHdr.h
@@ -1,6 +1,6 @@
---- ./AppHdr.h.orig	2009-10-06 20:44:04.000000000 +0200
-+++ ./AppHdr.h	2009-10-15 18:40:29.000000000 +0200
-@@ -151,7 +151,7 @@
+--- ./AppHdr.h.orig	2010-03-27 00:43:25.000000000 +0100
++++ ./AppHdr.h	2010-04-18 13:17:52.000000000 +0200
+@@ -157,7 +157,7 @@
      //          setuid or setgid. Filenames passed to this command *are not
      //          validated in any way*.
      //
@@ -9,3 +9,14 @@
  
      // For cases when the game will be played on terms that don't support the
      // curses "bold == lighter" 16 colour mode. -- bwr
+@@ -457,8 +457,8 @@
+     #ifndef SAVE_PACKAGE_NONE
+     #ifdef USE_TAR
+       #define PACKAGE_SUFFIX ".tar.gz"
+-      #define SAVE_PACKAGE_CMD "tar"
+-      #define LOAD_UNPACKAGE_CMD "tar -zxf %s"PACKAGE_SUFFIX" -C %s"
++      #define SAVE_PACKAGE_CMD "gtar"
++      #define LOAD_UNPACKAGE_CMD "gtar -zxf %s"PACKAGE_SUFFIX" -C %s"
+       #define UNPACK_SPECIFIC_FILE_CMD LOAD_UNPACKAGE_CMD " %s"
+     #else
+     #ifdef USE_ZIP
