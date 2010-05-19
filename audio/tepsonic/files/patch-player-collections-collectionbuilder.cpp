1c621ed881c6db36ce3c078cf64a5fa6606f5d77
Max Brazhnikov <makc@FreeBSD.org>
2010-05-19 20:43:02 +0000

    Add new port audio/tepsonic:
    TepSonic is a simple and fast audio player but it has all the basic functions
    that every good audio player should have. TepSonic is not trying to be
    like Amarok or Banshee, it stays small and simple.
    
    WWW:	http://www.tepsonic.org/
    
    PR:		ports/146700
    Submitted by:	Gvozdikov Veniamin <g.veniamin at googlemail.com>

---
+++ audio/tepsonic/files/patch-player-collections-collectionbuilder.cpp
@@ -0,0 +1,15 @@
+--- player/collections/collectionbuilder.cpp.orig	2010-05-18 02:04:47.000000000 +0800
++++ player/collections/collectionbuilder.cpp	2010-05-18 18:09:23.000000000 +0800
+@@ -22,9 +22,9 @@
+ #include "collectionmodel.h"
+ #include "databasemanager.h"
+ 
+-#include <taglib/fileref.h>
+-#include <taglib/tag.h>
+-#include <taglib/tstring.h>
++#include <fileref.h>
++#include <tag.h>
++#include <tstring.h>
+ 
+ #include <QDebug>
+ 
