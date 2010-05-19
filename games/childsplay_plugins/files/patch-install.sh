6fb6e5384ca1057a2b89a7345bb484e3acbd265c
Max Brazhnikov <makc@FreeBSD.org>
2010-05-19 20:21:29 +0000

    Update to 0.90 Sort plist while here
    PR:		ports/144523
    Submitted by:	Stas Timokhin <devel at stasyan.com>
    Apporved by:	maintainer timeout (two months)

--- games/childsplay_plugins/files/patch-install.sh
+++ games/childsplay_plugins/files/patch-install.sh
@@ -1,20 +1,17 @@
-diff -U 4 -r -N ./install.sh.orig ./install.sh
---- ./install.sh.orig	Fri Mar 18 09:51:09 2005
-+++ ./install.sh	Tue Mar 22 18:33:38 2005
-@@ -1,5 +1,5 @@
+--- install.sh.orig	2007-08-25 14:53:40.000000000 +0700
++++ install.sh	2010-03-06 12:25:59.000000000 +0600
+@@ -1,4 +1,4 @@
 -#!/bin/bash 
 +#!/bin/sh 
  # install childsplay.  
  # Be aware that childsplay probably won't work when you edit this file :-(
  # As of 11/2004 childsplay will be converted to follow the File System
- # Hierarchy (FSH).
-@@ -18,16 +18,16 @@
- then PREFIX=$1
- fi
+@@ -20,14 +20,14 @@
  
  LOCALEDIR=$PREFIX/share/locale
+ MTDIR=$PREFIX/bin
 -ASSETMLDIR=/usr/share/assetml
-+ASSETMLDIR=$PREFIX/share/assetml
++ASSETMLDIR=${PREFIX}/share/assetml
  PYTHON=`which python`
  SCOREDIR=/var/games
  DOCDIR=$PREFIX/share/doc/childsplay
@@ -27,9 +24,7 @@ diff -U 4 -r -N ./install.sh.orig ./install.sh
  SHARELIBDATADIR=$SHAREDIR/lib
  SHAREDATADIR=$SHAREDIR/Data
  
- ##################################################
-@@ -66,23 +66,8 @@
- fi
+@@ -76,21 +76,6 @@
  
  set -e
  
@@ -50,17 +45,4 @@ diff -U 4 -r -N ./install.sh.orig ./install.sh
 -fi
  echo " Installing in $CPDIR"
  
- if [ `expr $VERSION \< $DEPEN` -eq 1 ]
- then
-@@ -91,11 +76,8 @@
-     echo "exit";
-     exit 1
- fi
- 
--
--echo "Compiling modules to bytecode..."
--$PYTHON $CPDIR/install.py --compile $CWD/lib
- 
- echo " Copy libraries"
- for DIR in `ls $CWD/lib`
- do 
+ echo "Compiling modules to bytecode..."
