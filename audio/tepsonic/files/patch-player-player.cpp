747aecf50e1880ed48ba0cf6839db15332fbbd0f
makc <makc@FreeBSD.org>
2010-05-19 20:43:02 +0000

    Add new port audio/tepsonic:
    TepSonic is a simple and fast audio player but it has all the basic functions
    that every good audio player should have. TepSonic is not trying to be
    like Amarok or Banshee, it stays small and simple.
    
    WWW:	http://www.tepsonic.org/
    
    PR:		ports/146700
    Submitted by:	Gvozdikov Veniamin <g.veniamin at googlemail.com>

---
+++ audio/tepsonic/files/patch-player-player.cpp
@@ -0,0 +1,15 @@
+--- player/player.cpp.orig	2010-05-18 02:04:47.000000000 +0800
++++ player/player.cpp	2010-05-18 18:07:34.000000000 +0800
+@@ -27,9 +27,9 @@
+ #include <phonon/AudioOutput>
+ #include <phonon/MediaSource>
+ 
+-#include <taglib/fileref.h>
+-#include <taglib/tag.h>
+-#include <taglib/tstring.h>
++#include <fileref.h>
++#include <tag.h>
++#include <tstring.h>
+ 
+ Player::Player()
+ {
---
+++ audio/tepsonic/files/patch-player-playlist-playlistmodel.cpp
@@ -0,0 +1,15 @@
+--- player/playlist/playlistmodel.cpp.orig	2010-05-18 02:04:47.000000000 +0800
++++ player/playlist/playlistmodel.cpp	2010-05-18 18:11:45.000000000 +0800
+@@ -21,9 +21,9 @@
+ #include <QtGui>
+ #include <QTime>
+ 
+-#include <taglib/fileref.h>
+-#include <taglib/tag.h>
+-#include <taglib/tstring.h>
++#include <fileref.h>
++#include <tag.h>
++#include <tstring.h>
+ 
+ #include "playlistmodel.h"
+ #include "playlistitem.h"
