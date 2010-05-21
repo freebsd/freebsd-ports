16563512f1aa5b852cd9195203fd20b4d49b92ba
Martin Wilke <miwi@FreeBSD.org>
2010-05-21 11:08:49 +0000

    - Update tp 1.0.21
    PR:		146674
    Submitted by:	Ports Fury

--- graphics/sane-backends/files/patch-backend__epson2.c
+++ graphics/sane-backends/files/patch-backend__epson2.c
@@ -1,17 +1,11 @@
---- backend/epson2.c.orig	2009-04-30 22:15:19.000000000 +0900
-+++ backend/epson2.c	2009-05-05 02:16:46.000000000 +0900
-@@ -3446,12 +3446,14 @@
- 	 */
- 	DBG(1, "max req size: %d\n", sanei_scsi_max_request_size);
+--- backend/epson2.c.orig	2010-04-05 22:18:04.000000000 +0900
++++ backend/epson2.c	2010-05-10 22:18:47.000000000 +0900
+@@ -57,6 +57,8 @@
+ #include <unistd.h>
+ #include <errno.h>
+ #include <sys/time.h>
++#include <sys/types.h>
++#include <sys/socket.h>
  
-+#if 0
- 	if (s->lcount < 3 && e2_model(s, "GT-X800")) {
- 		s->lcount = 21;
- 		DBG(17,
- 		    "%s: set lcount = %i bigger than sanei_scsi_max_request_size\n",
- 		    __func__, s->lcount);
- 	}
-+#endif
- 
- 	if (s->lcount >= 255) {
- 		s->lcount = 255;
+ #include "sane/saneopts.h"
+ #include "sane/sanei_scsi.h"
