4d7a13e61fc81f15918ac78078313b908eb23ecb
dinoex <dinoex@FreeBSD.org>
2010-05-21 15:08:40 +0000

    - fix build with gnustep-make 2.4

---
+++ databases/gdl2/files/patch-EOAttribute.m
@@ -0,0 +1,10 @@
+--- EOAccess/EOAttribute.m.orig	2008-06-06 11:26:14.000000000 +0200
++++ EOAccess/EOAttribute.m	2010-05-21 17:04:41.000000000 +0200
+@@ -45,6 +45,7 @@
+ #include <Foundation/NSArchiver.h>
+ #include <Foundation/NSCalendarDate.h>
+ #include <Foundation/NSData.h>
++#include <Foundation/NSDebug.h>
+ #include <Foundation/NSDecimalNumber.h>
+ #include <Foundation/NSDictionary.h>
+ #include <Foundation/NSEnumerator.h>
