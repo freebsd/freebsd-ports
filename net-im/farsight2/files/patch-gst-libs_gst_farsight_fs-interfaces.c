d23942dcd8aa12a0b4388ac519958c4a1c8892a7
marcus <marcus@FreeBSD.org>
2010-05-22 07:11:39 +0000

    Update to 0.0.19.

--- net-im/farsight2/files/patch-gst-libs_gst_farsight_fs-interfaces.c
+++ net-im/farsight2/files/patch-gst-libs_gst_farsight_fs-interfaces.c
@@ -1,10 +1,24 @@
---- gst-libs/gst/farsight/fs-interfaces.c.orig	2009-02-27 23:28:15.000000000 -0500
-+++ gst-libs/gst/farsight/fs-interfaces.c	2009-02-27 23:28:33.000000000 -0500
-@@ -46,6 +46,7 @@ GST_DEBUG_CATEGORY_EXTERN (fs_base_confe
+--- gst-libs/gst/farsight/fs-interfaces.c.orig	2010-05-22 03:04:56.000000000 -0400
++++ gst-libs/gst/farsight/fs-interfaces.c	2010-05-22 03:08:16.000000000 -0400
+@@ -30,10 +30,7 @@
+ 
+ #ifdef G_OS_UNIX
+ 
+-#include <arpa/inet.h>
+ #include <errno.h>
+-#include <net/if.h>
+-#include <net/if_arp.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+@@ -44,6 +41,10 @@
   #include <sys/socket.h>
   #include <ifaddrs.h>
  #endif
 +#include <netinet/in.h>
- #include <net/if.h>
- #include <net/if_arp.h>
- #include <arpa/inet.h>
++#include <arpa/inet.h>
++#include <net/if.h>
++#include <net/if_arp.h>
+ 
+ #include <gst/gst.h>
+ 
