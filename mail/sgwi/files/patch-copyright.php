7918a27af31137e6810028794860244e2bdf4f24
Martin Wilke <miwi@FreeBSD.org>
2010-05-22 04:28:54 +0000

    - Update to 0.8
    PR:		146703
    Submitted by:	Lukasz Wasikowski <lukasz@wasikowski.net> (maintainer)

--- mail/sgwi/files/patch-copyright.php
+++ mail/sgwi/files/patch-copyright.php
@@ -4,5 +4,5 @@
  <TR HEIGHT=64><TD>
  <HR>
 -<FONT SIZE=-1><A HREF="http://www.vanheusden.com/sgwi/" TARGET="_new">SQLGrey webinterface v0.6</A> by <A HREF="http://www.vanheusden.com/feedbackform.php?subject=SQLGrey%20webinterface" TARGET="_new">folkert@vanheusden.com</A>
-+<FONT SIZE=-1><A HREF="http://www.vanheusden.com/sgwi/" TARGET="_new">SQLGrey webinterface v0.7</A> by <A HREF="http://www.vanheusden.com/feedbackform.php?subject=SQLGrey%20webinterface" TARGET="_new">folkert@vanheusden.com</A>
++<FONT SIZE=-1><A HREF="http://www.vanheusden.com/sgwi/" TARGET="_new">SQLGrey webinterface v0.8</A> by <A HREF="http://www.vanheusden.com/feedbackform.php?subject=SQLGrey%20webinterface" TARGET="_new">folkert@vanheusden.com</A>
  </TD></TR>
