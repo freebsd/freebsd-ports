b9d547bfcf833770938bdb72b684e5fef7970a5a
Alexey Dokuchaev <danfe@FreeBSD.org>
2010-05-23 14:50:33 +0000

    - Update to version 2.08.01 - Remove SF mirror now that it does not carry new distfiles any more - Reformat (kill line wrapping) and sort MAN1 values - Simplify PORTDOCS installation, canonicalize Makefile header - Replace `x86-64' with FreeBSDish `amd64' in COMMENT
    Approved by:	krion (maintainer)

--- devel/nasm/files/patch-output+outelf32.c
+++ devel/nasm/files/patch-output+outelf32.c
@@ -1,9 +1,9 @@
---- output/outelf32.c.orig	2008-03-03 16:15:43.564355152 -0500
-+++ output/outelf32.c	2008-03-03 16:21:02.242598895 -0500
-@@ -1605,7 +1605,7 @@
-         the source-file, the n_desc field should be set to the number
-         of remaining stabs
-         */
+--- output/outelf32.c.orig
++++ output/outelf32.c
+@@ -1702,7 +1702,7 @@
+          * the source-file, the n_desc field should be set to the number
+          * of remaining stabs
+          */
 -        WRITE_STAB(sptr, fileidx[0], 0, 0, 0, strlen(allfiles[0] + 12));
 +        WRITE_STAB(sptr, fileidx[0], 0, 0, 0, stabstrlen);
  
--- devel/nasm/files/patch-output+outelf64.c
+++ devel/nasm/files/patch-output+outelf64.c
@@ -1,9 +1,9 @@
---- output/outelf64.c.orig	2008-03-03 16:15:57.373145096 -0500
-+++ output/outelf64.c	2008-03-03 16:23:30.075070061 -0500
-@@ -1841,7 +1841,7 @@
-         the source-file, the n_desc field should be set to the number
-         of remaining stabs
-         */
+--- output/outelf64.c.orig
++++ output/outelf64.c
+@@ -1772,7 +1772,7 @@
+          * the source-file, the n_desc field should be set to the number
+          * of remaining stabs
+          */
 -        WRITE_STAB(sptr, fileidx[0], 0, 0, 0, strlen(allfiles[0] + 12));
 +        WRITE_STAB(sptr, fileidx[0], 0, 0, 0, stabstrlen);
  
