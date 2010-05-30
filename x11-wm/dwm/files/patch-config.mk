b854f7e66ba5ddc522cae9946bebb2bc8a5d61b7
sylvio <sylvio@FreeBSD.org>
2010-05-30 23:37:39 +0000

    - Update to 5.8
    PR:		ports/147161
    Submitted by:	Jeroen Schot <schot@A-Eskwadraat.nl> (maintainer)

--- x11-wm/dwm/files/patch-config.mk
+++ x11-wm/dwm/files/patch-config.mk
@@ -1,5 +1,5 @@
---- config.mk.orig	2009-07-14 20:07:55.000000000 +0200
-+++ config.mk	2009-07-20 13:29:40.000000000 +0200
+--- config.mk.orig	2010-05-28 12:43:17.000000000 +0200
++++ config.mk	2010-05-28 14:05:14.000000000 +0200
 @@ -4,11 +4,11 @@
  # Customize below to fit your system
  
@@ -14,23 +14,21 @@
 +X11INC = $(LOCALBASE)/include
 +X11LIB = $(LOCALBASE)/lib
  
- # Xinerama, un-comment if you want it
+ # Xinerama
  XINERAMALIBS = -L${X11LIB} -lXinerama
-@@ -19,15 +19,15 @@
- LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${XINERAMALIBS}
+@@ -20,9 +20,9 @@
  
  # flags
--CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-+CPPFLAGS+= -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
- #CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
--CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+ CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+-CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 +CFLAGS+= -std=c99 ${INCS} ${CPPFLAGS}
- #LDFLAGS = -g ${LIBS}
--LDFLAGS = -s ${LIBS}
-+LDFLAGS+=  ${LIBS}
+ #CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+-LDFLAGS = -g ${LIBS}
++LDFLAGS+= ${LIBS}
+ #LDFLAGS = -s ${LIBS}
  
  # Solaris
- #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+@@ -30,4 +30,4 @@
  #LDFLAGS = ${LIBS}
  
  # compiler and linker
