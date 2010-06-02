d8fd1a8696e0ed29195c9c40727c92134e01ad8a
Sylvio Cesar Teixeira <sylvio@FreeBSD.org>
2010-06-02 19:13:17 +0000

    - Update to 5.5

--- net-mgmt/net-snmp/files/patch-sctpTables.h
+++ net-mgmt/net-snmp/files/patch-sctpTables.h
@@ -1,11 +1,11 @@
---- agent/mibgroup/sctp-mib/sctpTables.h.orig	2009-01-23 23:46:39.000000000 +0900
-+++ agent/mibgroup/sctp-mib/sctpTables.h	2009-01-23 23:47:35.000000000 +0900
-@@ -14,6 +14,8 @@
- config_require(sctp-mib/sctpAssocTable)
+--- agent/mibgroup/sctp-mib/sctpTables.h.orig	2010-06-01 10:57:14.000000000 -0300
++++ agent/mibgroup/sctp-mib/sctpTables.h	2010-06-01 10:17:28.000000000 -0300
+@@ -15,7 +15,7 @@
  #if defined( linux )
  config_require(sctp-mib/sctpTables_linux)
-+#elif defined( freebsd7 )
-+config_require(sctp-mib/sctpTables_freebsd)
+ config_require(util_funcs/get_pid_from_inode)
+-#elif defined( freebsd7 ) || defined( freebsd8 )
++#elif defined( freebsd7 ) || defined( freebsd8 ) || defined( freebsd9 )
+ config_require(sctp-mib/sctpTables_freebsd)
  #else
- /*
- * couldn't determine the correct file!
+ config_error(SCTP-MIB is not available in tihs environment)
--- net-mgmt/net-snmp/files/patch-sctpTables_common.h
+++ net-mgmt/net-snmp/files/patch-sctpTables_common.h
@@ -1,15 +1,11 @@
---- agent/mibgroup/sctp-mib/sctpTables_common.h.orig	2008-08-18 06:29:11.000000000 +0900
-+++ agent/mibgroup/sctp-mib/sctpTables_common.h	2009-02-28 14:06:27.000000000 +0900
-@@ -1,9 +1,9 @@
+--- agent/mibgroup/sctp-mib/sctpTables_common.h.orig	2010-06-01 10:58:20.000000000 -0300
++++ agent/mibgroup/sctp-mib/sctpTables_common.h	2010-06-01 10:18:02.000000000 -0300
+@@ -1,7 +1,7 @@
  #ifndef SCTP_TABLES_COMMON_H
  #define SCTP_TABLES_COMMON_H
  
--#include <net-snmp/net-snmp-config.h>
--#include <net-snmp/net-snmp-includes.h>
--#include <net-snmp/agent/net-snmp-agent-includes.h>
-+#if defined(freebsd7)
-+#define netsnmp_table_registration_info_free SNMP_FREE
-+#endif
+-#if defined(freebsd8) || defined(freebsd7)
++#if defined(freebsd9) || defined(freebsd8) || defined(freebsd7)
+ #define netsnmp_table_registration_info_free SNMP_FREE
+ #endif
  
- #define SCTP_IPADDRESS_SIZE 16
- #define SCTP_HOSTNAME_SIZE 255
