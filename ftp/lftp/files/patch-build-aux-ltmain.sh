0313c443b0da5d16f6264a6ae6a7b43c941ae72b
Martin Wilke <miwi@FreeBSD.org>
2010-06-05 21:30:39 +0000

    - Update to 4.0.8
    PR:		146791
    Submitted by:	Ganael Laplanche <ganael.laplanche@martymac.com> (maintainer)

--- ftp/lftp/files/patch-build-aux-ltmain.sh
+++ ftp/lftp/files/patch-build-aux-ltmain.sh
@@ -1,15 +1,15 @@
---- build-aux/ltmain.sh.orig	2008-12-15 18:04:37.000000000 +0100
-+++ build-aux/ltmain.sh	2008-12-15 18:07:52.000000000 +0100
-@@ -2077,10 +2077,12 @@
+--- build-aux/ltmain.sh.orig	2010-05-20 17:09:31.539282017 +0000
++++ build-aux/ltmain.sh	2010-05-20 17:10:11.971297050 +0000
+@@ -2076,12 +2076,6 @@
+ 	  func_execute_cmds "$postinstall_cmds" 'exit $?'
  	fi
  
- 	# Install the pseudo-library for information purposes.
-+	if false; then
- 	func_basename "$file"
- 	name="$func_basename_result"
- 	instname="$dir/$name"i
- 	func_show_eval "$install_prog $instname $destdir/$name" 'exit $?'
-+	fi
- 
+-	# Install the pseudo-library for information purposes.
+-	func_basename "$file"
+-	name="$func_basename_result"
+-	instname="$dir/$name"i
+-	func_show_eval "$install_prog $instname $destdir/$name" 'exit $?'
+-
  	# Maybe install the static library, too.
  	test -n "$old_library" && staticlibs="$staticlibs $dir/$old_library"
+ 	;;
