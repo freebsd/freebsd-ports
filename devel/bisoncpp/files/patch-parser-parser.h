3b91d086b854fc8138cbbb037eb04f8dfba4dd15
pgj <pgj@FreeBSD.org>
2010-06-05 11:04:49 +0000

    - Update to 2.7.0

--- devel/bisoncpp/files/patch-parser-parser.h
+++ devel/bisoncpp/files/patch-parser-parser.h
@@ -1,6 +1,6 @@
---- parser/parser.h.orig	2007-10-15 11:58:20.000000000 +0200
-+++ parser/parser.h	2009-03-05 15:50:45.000000000 +0100
-@@ -151,7 +151,7 @@
+--- ./parser/parser.h.orig	2010-03-31 14:28:49.000000000 +0200
++++ ./parser/parser.h	2010-06-05 01:02:00.913097223 +0200
+@@ -155,7 +155,7 @@
          void multiplyDefined(Symbol const *sp);
  
          void nestedBlock(Block &block); // define inner block as pseudo N
@@ -9,10 +9,10 @@
  
          std::string nextHiddenName();
          void noDefaultTypeWarning();
-@@ -382,7 +382,7 @@
+@@ -406,7 +406,7 @@
+ {
      return d_stackDecl;
  }
- 
 -inline std::string *Parser::newYYText() const
 +inline std::string *Parser::newYYText()
  {
