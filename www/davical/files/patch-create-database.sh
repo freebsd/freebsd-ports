2d83c85c7fa26b5defa1e42abcbcb8c897055ca2
miwi <miwi@FreeBSD.org>
2010-06-07 06:27:29 +0000

    - Update to 0.9.9
    PR:		146489
    Submitted by:	ohn Marshall <john.marshall@riverwillow.com.au> (maintainer)

--- www/davical/files/patch-create-database.sh
+++
@@ -1,8 +0,0 @@
---- dba/create-database.sh.orig	2010-02-25 09:18:18.000000000 +1100
-+++ dba/create-database.sh	2010-02-25 09:17:48.000000000 +1100
-@@ -1,4 +1,4 @@
--#!/bin/bash
-+#!/usr/local/bin/bash
- #
- # Build the DAViCal database
- #
