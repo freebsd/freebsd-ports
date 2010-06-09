d15c60ff161c7d240b18fbfa80d760b8db1f8697
Joe Marcus Clarke <marcus@FreeBSD.org>
2010-06-09 22:49:30 +0000

    Fix the build on 6.X.
    Reported by:	pointyhat via pav

---
+++ net/netatalk/files/patch-include_atalk_dsi.h
@@ -0,0 +1,10 @@
+--- include/atalk/dsi.h.orig	2010-06-09 18:46:15.000000000 -0400
++++ include/atalk/dsi.h	2010-06-09 18:44:22.000000000 -0400
+@@ -11,6 +11,7 @@
+ #include <sys/time.h>
+ #include <signal.h>
+ 
++#include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <atalk/afp.h>
+ #include <atalk/server_child.h>
