ad1556bccfd2f9551d94f2da824efc431145f15b
johans <johans@FreeBSD.org>
2010-06-09 06:37:51 +0000

    - Add patchfile that was forgotten in the previous commit
    Reported by:	itetcu

---
+++ devel/opencvs/files/patch-rcs.h
@@ -0,0 +1,10 @@
+--- rcs.h.orig	2010-06-08 20:26:06.000000000 +0200
++++ rcs.h	2010-06-08 20:26:12.000000000 +0200
+@@ -26,6 +26,7 @@
+ 
+ #ifndef RCS_H
+ #define RCS_H
++#include <time.h>
+ #include "buf.h"
+ 
+ #define RCS_DIFF_DIV \
