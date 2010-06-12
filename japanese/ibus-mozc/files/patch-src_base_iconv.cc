e8a1f3b855155cabe9c74089d386c7197dd6b015
Daichi GOTO <daichi@FreeBSD.org>
2010-06-12 07:31:06 +0000

    - update to mozc-0.11.365.102 - you need 8.0 or over to build mozc - added UT dictionary options to add UT extra dictionary.   default is disable:     WITH_DIC_UT_MEISHI, WITH_DIC_UT_JINMEI, WITH_DIC_UT_KAOMOJI,     WITH_DIC_UT_EDICTKATAKANAGO, WITH_DIC_UT_ZIPCODE,     WITH_DIC_UT_JIGYOSHO, WITH_DIC_UT_GCANNAKIHONMEISHI
      WITH_DIC_UT is shortcut of:
        WITH_DIC_UT_MEISHI
        WITH_DIC_UT_JINMEI
    
      WITH_DIC_UT_FULL is shortcut of:
        WITH_DIC_UT_MEISHI
        WITH_DIC_UT_JINMEI
        WITH_DIC_UT_KAOMOJI
        WITH_DIC_UT_EDICTKATAKANAGO
        WITH_DIC_UT_ZIPCODE
        WITH_DIC_UT_JIGYOSHO
        WITH_DIC_UT_GCANNAKIHONMEISHI
    - added two original icons because mozc has no icon files.

--- japanese/ibus-mozc/files/patch-src_base_iconv.cc
+++ japanese/ibus-mozc/files/patch-src_base_iconv.cc
@@ -1,5 +1,5 @@
---- src/base/iconv.cc.org	2010-05-29 11:06:10.667672161 +0900
-+++ src/base/iconv.cc	2010-05-29 11:22:12.794853968 +0900
+--- base/iconv.cc.org	2010-06-11 18:01:03.000000000 +0900
++++ base/iconv.cc	2010-06-12 10:02:20.315839057 +0900
 @@ -52,7 +52,7 @@
    size_t olen_org = olen;
    iconv(ic, 0, &ilen, 0, &olen);  // reset iconv state
--- japanese/ibus-mozc/files/patch-src_ipc_unix_ipc.cc
+++ japanese/ibus-mozc/files/patch-src_ipc_unix_ipc.cc
@@ -1,11 +1,11 @@
---- src/ipc/unix_ipc.cc.org	2010-05-29 11:06:11.125610530 +0900
-+++ src/ipc/unix_ipc.cc	2010-05-29 11:33:18.938743764 +0900
+--- ipc/unix_ipc.cc.org	2010-06-11 18:01:03.000000000 +0900
++++ ipc/unix_ipc.cc	2010-06-12 10:21:51.905675963 +0900
 @@ -41,7 +41,7 @@
  #include <sys/time.h>
  #include <sys/types.h>
  #include <sys/un.h>
 -#ifdef OS_MACOSX
-+#ifdef __FreeBSD__
++#if defined(__FreeBSD__) || defined(OS_MACOSX)
  #include <sys/ucred.h>
  #endif
  #include <sys/wait.h>
@@ -14,24 +14,16 @@
    *pid = 0;
  
 -#ifdef OS_MACOSX
-+#ifdef __FreeBSD__
++#if defined(__FreeBSD__) || defined(OS_MACOSX)
    // If the OS is MAC, we should validate the peer by using LOCAL_PEERCRED.
    struct xucred peer_cred;
    socklen_t peer_cred_len = sizeof(struct xucred);
-@@ -146,6 +146,7 @@
+@@ -146,7 +146,7 @@
    *pid = 0;
  #endif
  
-+/*
- #ifdef OS_LINUX
+-#ifdef OS_LINUX
++#if !defined(__FreeBSD__) && defined(OS_LINUX)
    // On ARM Linux, we do nothing and just return true since the platform (at
    // least the qemu emulator) doesn't support the getsockopt(sock, SOL_SOCKET,
-@@ -171,7 +172,7 @@
-   *pid = peer_cred.pid;
- #endif  // __arm__
- #endif
--
-+*/
-   return true;
- }
- 
+   // SO_PEERCRED) system call.
