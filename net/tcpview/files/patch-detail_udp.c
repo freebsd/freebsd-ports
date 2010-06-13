ea4619af297fda50e95a5cdbf85f936074c5a342
cy <cy@FreeBSD.org>
2010-06-13 15:28:43 +0000

    Fix 8.X build errors.
    Take on maintainership of this port.

--- net/tcpview/files/patch-detail_udp.c
+++ net/tcpview/files/patch-detail_udp.c
@@ -1,5 +1,5 @@
---- detail-udp.c.orig	Thu Apr 22 22:39:55 1993
-+++ detail-udp.c	Thu Feb 13 18:57:17 2003
+--- detail-udp.c.orig	1993-04-22 13:39:55.000000000 -0700
++++ detail-udp.c	2010-06-13 08:09:33.000000000 -0700
 @@ -37,6 +37,7 @@
  #endif
  
@@ -8,7 +8,7 @@
  #include <sys/socket.h>
  #include <netinet/in.h>
  #include <netinet/in_systm.h>
-@@ -50,12 +51,15 @@
+@@ -50,12 +51,21 @@
  #include <errno.h>
  #include <sys/time.h>
  #include <rpc/types.h>
@@ -21,8 +21,14 @@
 -#include <nfs/nfs.h>
 +#include <sys/ucred.h> 
 +#include <sys/mount.h>
++#if defined(__FreeBSD_version) && __FreeBSD_version >= 800100
++#include <fs/nfs/nfsport.h>
++#include <fs/nfs/rpcv2.h>
++#include <fs/nfs/nfsproto.h>
++#else
 +#include <nfs/rpcv2.h>
 +#include <nfs/nfsproto.h>
++#endif
  
  #include "interface.h"
  #include "addrtoname.h"
--- net/tcpview/files/patch-print_udp.c
+++ net/tcpview/files/patch-print_udp.c
@@ -1,5 +1,5 @@
---- print-udp.c.orig	Thu Apr 22 22:40:29 1993
-+++ print-udp.c	Thu Feb 13 18:54:06 2003
+--- print-udp.c.orig	1993-04-22 13:40:29.000000000 -0700
++++ print-udp.c	2010-06-13 08:09:39.000000000 -0700
 @@ -39,10 +39,10 @@
  #include <errno.h>
  #include <sys/time.h>
@@ -12,14 +12,20 @@
  #include <rpc/rpc_msg.h>
  
  #ifdef TCPVIEW
-@@ -53,9 +53,10 @@
+@@ -53,9 +53,16 @@
  /* These must come after interface.h for BSD. */
  #if BSD >= 199006
  #include <sys/ucred.h>
 -#include <nfs/nfsv2.h>
 +#include <sys/mount.h>
++#if defined(__FreeBSD_version) && __FreeBSD_version >= 800100
++#include <fs/nfs/nfsport.h>
++#include <fs/nfs/rpcv2.h>
++#include <fs/nfs/nfsproto.h>
++#else
 +#include <nfs/rpcv2.h>
 +#include <nfs/nfsproto.h>
++#endif
  #endif
 -#include <nfs/nfs.h>
  
