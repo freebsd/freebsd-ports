52aa25daf63178022f402c085ed5b4d25e0cb1fc
lioux <lioux@FreeBSD.org>
2010-06-14 01:01:04 +0000

    Update to 0.5.0

---
+++ graphics/qcomicbook/files/patch-src__P7zipArchiverStrategy.cpp
@@ -0,0 +1,10 @@
+--- src/P7zipArchiverStrategy.cpp.orig	2010-06-13 09:48:03.000000000 -0300
++++ src/P7zipArchiverStrategy.cpp	2010-06-13 09:48:10.000000000 -0300
+@@ -28,6 +28,7 @@
+ void P7zipArchiverStrategy::configure()
+ {
+     addExtension(".7z");
++    addExtension(".cbz");
+     setExecutables("7z", "7zr");
+ 
+     if (which("7z") != QString::null)
