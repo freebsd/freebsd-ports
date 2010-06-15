d466b174e6f1da515555f9e05c53633c83e5d48d
makc <makc@FreeBSD.org>
2010-06-15 18:52:03 +0000

    Fix build with KDE 4.4 Update WWW and MASTER_SITES
    PR:		ports/147777
    Submitted by:	Jason E. Hale (maintainer)

---
+++ audio/audex/files/patch-utils__upload.h
@@ -0,0 +1,10 @@
+--- ./utils/upload.h.orig	2010-06-10 17:32:46.000000000 -0400
++++ ./utils/upload.h	2010-06-10 17:33:01.000000000 -0400
+@@ -28,7 +28,6 @@
+ #include <KLocale>
+ #include <KUrl>
+ #include <KIO/Job>
+-#include <KIO/MkdirJob>
+ #include <KIO/SimpleJob>
+ #include <KIO/CopyJob>
+ 
