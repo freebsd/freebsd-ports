022e2ec0baf5d6850ae8ae5004347d4889370904
makc <makc@FreeBSD.org>
2010-06-19 11:06:36 +0000

    Update to 1.1.18.1 Build with external ffmpeg by default Mark MAKE_JOBS_SAFE
    Feature safe:	yes

--- multimedia/libxine/files/patch-src_audio_out_audio_oss_out.c
+++ multimedia/libxine/files/patch-src_audio_out_audio_oss_out.c
@@ -1,6 +1,6 @@
---- src/audio_out/audio_oss_out.c.orig	Mon Apr 16 02:42:15 2007
-+++ src/audio_out/audio_oss_out.c	Sun Jul  1 14:42:59 2007
-@@ -163,7 +163,7 @@
+--- ./src/audio_out/audio_oss_out.c.orig	2010-01-15 01:17:54.000000000 +0300
++++ ./src/audio_out/audio_oss_out.c	2010-06-19 01:49:48.446683938 +0400
+@@ -160,7 +160,7 @@
  		       uint32_t bits, uint32_t rate, int mode) {
  
    oss_driver_t *this = (oss_driver_t *) this_gen;
@@ -9,7 +9,7 @@
  
    xprintf(this->xine, XINE_VERBOSITY_DEBUG,
  	  "audio_oss_out: ao_open rate=%d, mode=%d, dev=%s\n", rate, mode, this->audio_dev);
-@@ -295,14 +295,14 @@
+@@ -292,14 +292,14 @@
        if (bits==8) {
  	xprintf(this->xine, XINE_VERBOSITY_DEBUG, "audio_oss_out: SNDCTL_DSP_SETFMT failed for AFMT_U8.\n");
          if (tmp != AFMT_U8)
@@ -26,16 +26,16 @@
          else
            xprintf(this->xine, XINE_VERBOSITY_DEBUG, "audio_oss_out: The AFMT_S16_NE ioctl failed.\n");
          return 0;
-@@ -321,7 +321,7 @@
+@@ -318,7 +318,7 @@
      tmp = AFMT_AC3;
      if (ioctl(this->audio_fd, SNDCTL_DSP_SETFMT, &tmp) < 0 || tmp != AFMT_AC3) {
-       xprintf(this->xine, XINE_VERBOSITY_DEBUG, 
+       xprintf(this->xine, XINE_VERBOSITY_DEBUG,
 -	      "audio_oss_out: AC3 SNDCTL_DSP_SETFMT failed. %d. Using alternative.\n",tmp);
 +	      "audio_oss_out: AC3 SNDCTL_DSP_SETFMT failed. %ld. Using alternative.\n",tmp);
        tmp = AFMT_S16_LE;
        ioctl(this->audio_fd, SNDCTL_DSP_SETFMT, &tmp);
      }
-@@ -377,7 +377,7 @@
+@@ -374,7 +374,7 @@
  
    count_info    info;
    oss_driver_t *this = (oss_driver_t *) this_gen;
@@ -44,7 +44,7 @@
    int           frames;
    struct        timeval tv;
  
-@@ -515,7 +515,7 @@
+@@ -514,7 +514,7 @@
  static int ao_oss_get_property (ao_driver_t *this_gen, int property) {
  
    oss_driver_t *this = (oss_driver_t *) this_gen;
@@ -53,16 +53,16 @@
  
    switch(property) {
    case AO_PROP_PCM_VOL:
-@@ -524,7 +524,7 @@
-       
+@@ -523,7 +523,7 @@
+ 
        if(this->mixer.fd != -1) {
  	IOCTL_REQUEST_TYPE cmd = 0;
 -	int v;
 +	long v;
- 	
+ 
  	ioctl(this->mixer.fd, SOUND_MIXER_READ_DEVMASK, &audio_devs);
- 	
-@@ -554,7 +554,7 @@
+ 
+@@ -553,7 +553,7 @@
  static int ao_oss_set_property (ao_driver_t *this_gen, int property, int value) {
  
    oss_driver_t *this = (oss_driver_t *) this_gen;
@@ -71,25 +71,25 @@
  
    switch(property) {
    case AO_PROP_PCM_VOL:
-@@ -563,7 +563,7 @@
-       
+@@ -562,7 +562,7 @@
+ 
        if(this->mixer.fd != -1) {
  	IOCTL_REQUEST_TYPE cmd = 0;
 -	int v;
 +	long v;
- 	
+ 
  	ioctl(this->mixer.fd, SOUND_MIXER_READ_DEVMASK, &audio_devs);
- 	
-@@ -592,7 +592,7 @@
-       
+ 
+@@ -591,7 +591,7 @@
+ 
        if(this->mixer.fd != -1) {
  	IOCTL_REQUEST_TYPE cmd = 0;
 -	int v = 0;
 +	long v = 0;
- 	
+ 
  	ioctl(this->mixer.fd, SOUND_MIXER_READ_DEVMASK, &audio_devs);
- 	
-@@ -665,7 +665,7 @@
+ 
+@@ -664,7 +664,7 @@
  static int probe_audio_devices(oss_driver_t *this) {
    const char *base_names[2] = {"/dev/dsp", "/dev/sound/dsp"};
    int base_num, i;
@@ -98,7 +98,7 @@
    int best_rate;
    char devname[30];
  
-@@ -704,9 +704,9 @@
+@@ -703,9 +703,9 @@
    oss_class_t     *class = (oss_class_t *) class_gen;
    config_values_t *config = class->config;
    oss_driver_t    *this;
