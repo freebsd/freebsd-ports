85541027e3d9b52e84b9f7cc4448be938be3de16
Dima Panov <fluffy@FreeBSD.org>
2010-06-19 02:12:56 +0000

    - Update KOffice (and its l10n packs) to 2.2.0 release
    Major changes:
        Kexi now returns back
        New import filters for MS OOXML (Office 2007) Formats
        Improved OO.org and MS Office traditional formats compatibility
        Improved stability at all
    
    Discussed with:	miwi, itetcu
    Approved by:	portmgr (erwin)
    Feature safe:	yes

--- editors/calligra/files/patch-cmake__modules__FindWPD.cmake
+++ editors/calligra/files/patch-cmake__modules__FindWPD.cmake
@@ -1,16 +1,11 @@
 --- ./cmake/modules/FindWPD.cmake.orig	2010-03-30 13:10:16.507260418 +1100
 +++ ./cmake/modules/FindWPD.cmake	2010-03-30 13:09:54.253774780 +1100
-@@ -25,9 +25,10 @@
- endif(NOT WIN32)
+@@ -24,7 +24,7 @@
  
    FIND_PATH(WPD_INCLUDE_DIR libwpd/libwpd.h
--    ${_WPDIncDir}
+     ${WPD_INCLUDE_DIR}
 -    /usr/include/libwpd-0.8
--  )
-+     PATHS
-+   ${_WPDIncDir}
-+     PATH_SUFFIXES libwpd-0.8
-+ )
++    PATH_SUFFIXES libwpd-0.8
+   )
  
-   FIND_LIBRARY(WPD_LIBRARIES NAMES wpd-0.8
-     PATHS
+ 
