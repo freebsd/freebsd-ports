89543603fec7b7c5c5a4b766e975a89a7cee43ce
danfe <danfe@FreeBSD.org>
2010-06-22 09:03:13 +0000

    - Update to version 3.3.1, now with Qt4 GUI instead of Gtk+2! - Introduce OPTIONS (enable GUI, but disable graph rendering since  `x11-toolkits/qwt5' is built with Qt Designer plugin by default, which   pulls rather heavy `devel/qt4-designer' dependency) - Remove now-inaccurate pkg-message; please consult README instead - Get rid of pkg-plist and simplify port overall
    Feature safe:	yes

---
+++ security/ophcrack/files/patch-src_misc.c
@@ -0,0 +1,10 @@
+--- src/misc.c.orig	2009-07-29 13:50:26.000000000 +0800
++++ src/misc.c	2010-06-22 11:18:50.000000000 +0800
+@@ -31,6 +31,7 @@
+  *   
+  *   
+ */
++#include <sys/types.h>
+ #include <config.h>
+ #include <string.h>
+ #include <stdio.h>
