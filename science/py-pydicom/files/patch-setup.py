be40c8024e3b0e856f558da7d95a8141ec00d668
wen <wen@FreeBSD.org>
2010-06-25 02:39:08 +0000

    - Update to 0.9.4.1 - Update my email to FreeBSD
    Feature safe:	yes

---
+++ science/py-pydicom/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2010-06-25 10:22:29.000000000 +0800
++++ setup.py	2010-06-25 10:22:39.000000000 +0800
+@@ -1,6 +1,6 @@
+ #!/usr/bin/env python
+-from distribute_setup import use_setuptools
+-use_setuptools()
++#from distribute_setup import use_setuptools
++#use_setuptools()
+ 
+ from setuptools import setup, find_packages
+ import os
