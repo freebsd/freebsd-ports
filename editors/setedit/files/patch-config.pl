fcb06459f33f5a7a3a15399a8efde69cf2c6b830
rene <rene@FreeBSD.org>
2010-06-27 20:50:35 +0000

    Unbreak and update to 0.5.5
    PR:		ports/147780
    Submitted by:	Rob Farmer [rfarmer predatorlabs.net]
    Approved by:	maintainer timeout, tabthorpe (mentor)
    Feature safe:	yes

--- editors/setedit/files/patch-config.pl
+++ editors/setedit/files/patch-config.pl
@@ -1,93 +1,52 @@
-*** config.pl.orig	Tue Oct 12 22:00:54 2004
---- config.pl	Thu Oct 27 12:06:23 2005
-***************
-*** 169,175 ****
+--- config.pl.orig	2004-10-12 12:00:54.000000000 -0700
++++ config.pl	2010-06-10 01:14:22.000000000 -0700
+@@ -169,7 +169,7 @@
+   }
+ elsif ($OS eq 'UNIX')
+   {
+-   $MakeDefsRHIDE[0]='RHIDE_STDINC=/usr/include /usr/local/include /usr/include/g++ /usr/local/include/g++ /usr/lib/gcc-lib /usr/local/lib/gcc-lib';
++   $MakeDefsRHIDE[0]='RHIDE_STDINC=/usr/include ' . $ENV{'LOCALBASE'} . '/include /usr/include/g++ ' . $ENV{'LOCALBASE'} . '/include/g++ /usr/lib/gcc-lib ' . $ENV{'LOCALBASE'} . '/lib/gcc-lib';
+    if (@conf{'static'} eq 'yes')
+       { $libs=TVConfigOption('slibs'); }
+    else
+@@ -456,9 +456,9 @@
+     elsif ($i eq '--debug')
+       {
+        $conf{'XCFLAGS'}=
+-       $conf{'XCXXFLAGS'}='-O3 -fomit-frame-pointer -ffast-math -gstabs+3';
++       $conf{'XCXXFLAGS'}='-O3 -fomit-frame-pointer -ffast-math';
+        $conf{'CFLAGS'}=
+-       $conf{'CXXFLAGS'}='-O2 -Wall -Werror -gstabs+3';
++       $conf{'CXXFLAGS'}='-O2 -Wall -Werror';
+       }
+     elsif ($i eq '--with-mixer')
+       {
+@@ -783,8 +783,8 @@
     }
   elsif ($OS eq 'UNIX')
     {
-!    $MakeDefsRHIDE[0]='RHIDE_STDINC=/usr/include /usr/local/include /usr/include/g++ /usr/local/include/g++ /usr/lib/gcc-lib /usr/local/lib/gcc-lib';
-     if (@conf{'static'} eq 'yes')
-        { $libs=TVConfigOption('slibs'); }
-     else
---- 169,175 ----
+-    @dirsI=("$parent/tvision/include",'/usr/include/rhtvision','/usr/local/include/rhtvision');
+-    @dirsL=("$parent/tvision/makes",'/usr/lib','/usr/local/lib');
++    @dirsI=("$parent/tvision/include",'/usr/include/rhtvision',$ENV{'LOCALBASE'} . '/include/rhtvision');
++    @dirsL=("$parent/tvision/makes",'/usr/lib',$ENV{'LOCALBASE'} . '/lib');
     }
-  elsif ($OS eq 'UNIX')
+  elsif ($OS eq 'Win32')
     {
-!    $MakeDefsRHIDE[0]='RHIDE_STDINC=/usr/include ' . $ENV{'LOCALBASE'} . '/include /usr/include/g++ ' . $ENV{'LOCALBASE'} . '/include/g++ /usr/lib/gcc-lib ' . $ENV{'LOCALBASE'} . '/lib/gcc-lib';
-     if (@conf{'static'} eq 'yes')
-        { $libs=TVConfigOption('slibs'); }
-     else
-***************
-*** 456,464 ****
-      elsif ($i eq '--debug')
-        {
-         $conf{'XCFLAGS'}=
-!        $conf{'XCXXFLAGS'}='-O3 -fomit-frame-pointer -ffast-math -gstabs+3';
-         $conf{'CFLAGS'}=
-!        $conf{'CXXFLAGS'}='-O2 -Wall -Werror -gstabs+3';
-        }
-      elsif ($i eq '--with-mixer')
-        {
---- 456,464 ----
-      elsif ($i eq '--debug')
-        {
-         $conf{'XCFLAGS'}=
-!        $conf{'XCXXFLAGS'}='-O3 -fomit-frame-pointer -ffast-math';
-         $conf{'CFLAGS'}=
-!        $conf{'CXXFLAGS'}='-O2 -Wall -Werror';
-        }
-      elsif ($i eq '--with-mixer')
-        {
-***************
-*** 783,790 ****
-     }
-   elsif ($OS eq 'UNIX')
-     {
-!     @dirsI=("$parent/tvision/include",'/usr/include/rhtvision','/usr/local/include/rhtvision');
-!     @dirsL=("$parent/tvision/makes",'/usr/lib','/usr/local/lib');
-     }
-   elsif ($OS eq 'Win32')
-     {
---- 783,790 ----
-     }
-   elsif ($OS eq 'UNIX')
-     {
-!     @dirsI=("$parent/tvision/include",'/usr/include/rhtvision',$ENV{'LOCALBASE'} . '/include/rhtvision');
-!     @dirsL=("$parent/tvision/makes",'/usr/lib',$ENV{'LOCALBASE'} . '/lib');
-     }
-   elsif ($OS eq 'Win32')
-     {
-***************
-*** 1856,1862 ****
-   return 0;
-  }
-  ';
-!  $conf{'X11LibPath'}='/usr/X11R6/lib' unless $conf{'X11LibPath'};
-   $conf{'X11Lib'}='X11 Xmu' unless $conf{'X11Lib'};
-   $libs=$conf{'X11Lib'};
-   $libs=~s/(\S+)/-l$1/g;
---- 1856,1862 ----
-   return 0;
-  }
-  ';
-!  $conf{'X11LibPath'}=$ENV{'LOCALBASE'} . '/lib' unless $conf{'X11LibPath'};
-   $conf{'X11Lib'}='X11 Xmu' unless $conf{'X11Lib'};
-   $libs=$conf{'X11Lib'};
-   $libs=~s/(\S+)/-l$1/g;
-***************
-*** 1873,1879 ****
-     {
-      if (!$conf{'X11IncludePath'})
-        {
-!        $conf{'X11IncludePath'}='/usr/X11R6/include';
-         $o.="-I$conf{'X11IncludePath'} -L$conf{'X11LibPath'} $libs";
-         if ($test=~/OK, (\d+)\.(\d+)/)
-           {
---- 1873,1879 ----
-     {
-      if (!$conf{'X11IncludePath'})
-        {
-!        $conf{'X11IncludePath'}=$ENV{'LOCALBASE'} . '/include';
-         $o.="-I$conf{'X11IncludePath'} -L$conf{'X11LibPath'} $libs";
-         if ($test=~/OK, (\d+)\.(\d+)/)
-           {
+@@ -1856,7 +1856,7 @@
+  return 0;
+ }
+ ';
+- $conf{'X11LibPath'}='/usr/X11R6/lib' unless $conf{'X11LibPath'};
++ $conf{'X11LibPath'}=$ENV{'LOCALBASE'} . '/lib' unless $conf{'X11LibPath'};
+  $conf{'X11Lib'}='X11 Xmu' unless $conf{'X11Lib'};
+  $libs=$conf{'X11Lib'};
+  $libs=~s/(\S+)/-l$1/g;
+@@ -1873,7 +1873,7 @@
+    {
+     if (!$conf{'X11IncludePath'})
+       {
+-       $conf{'X11IncludePath'}='/usr/X11R6/include';
++       $conf{'X11IncludePath'}=$ENV{'LOCALBASE'} . '/include';
+        $o.="-I$conf{'X11IncludePath'} -L$conf{'X11LibPath'} $libs";
+        if ($test=~/OK, (\d+)\.(\d+)/)
+          {
--- editors/setedit/files/patch-editmain.cc
+++ editors/setedit/files/patch-editmain.cc
@@ -1,27 +1,25 @@
-*** setedit/editmain.cc.orig	Thu Sep 29 15:51:43 2005
---- setedit/editmain.cc	Thu Sep 29 16:03:36 2005
-***************
-*** 2299,2320 ****
-   else
-      if (Clock)
-         KillClock();
-- 
--  if (UseScreenSaver && TScreen::useScreenSaver())
--    {
--     int seconds=inIdleTime/clockResolution;
--     if (seconds==screenSaverTimeMouse)
--       {
--        MouseEventType me;
--        TMouse::getEvent(me);
--        TRect r=deskTop->getExtent();
--        if (me.where.x==r.b.x-1 && me.where.y==0)
--           screenSaver();
--       }
--     else
--     if (seconds>=screenSaverTime)
--        screenSaver();
--    }
-  }
-  
-  
---- 2299,2304 ----
+--- setedit/editmain.cc.orig	2004-10-18 09:38:28.000000000 -0700
++++ setedit/editmain.cc	2010-06-10 01:14:22.000000000 -0700
+@@ -2299,22 +2299,6 @@
+  else
+     if (Clock)
+        KillClock();
+-
+- if (UseScreenSaver && TScreen::useScreenSaver())
+-   {
+-    int seconds=inIdleTime/clockResolution;
+-    if (seconds==screenSaverTimeMouse)
+-      {
+-       MouseEventType me;
+-       TMouse::getEvent(me);
+-       TRect r=deskTop->getExtent();
+-       if (me.where.x==r.b.x-1 && me.where.y==0)
+-          screenSaver();
+-      }
+-    else
+-    if (seconds>=screenSaverTime)
+-       screenSaver();
+-   }
+ }
+ 
+ 
