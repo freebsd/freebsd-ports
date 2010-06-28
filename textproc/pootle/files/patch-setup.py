468af85ebb83da67f13c4d1fbb12e23ad9b5b2fc
mm <mm@FreeBSD.org>
2010-06-28 17:43:08 +0000

    - Update to 2.0.4 (fixes django 1.2 compatibility) - Honor PREFIX - Significantly improve startup script - Allow user to customize POOTLE(USER|GROUP|DBDIR|RUNDIR|LOGDIR)
    Feature safe:	yes
    Approved by:    Denis Pokataev <catone@cpan.org> (maintainer)

--- textproc/pootle/files/patch-setup.py
+++ textproc/pootle/files/patch-setup.py
@@ -1,18 +1,5 @@
 --- setup.py.orig	2009-12-02 07:33:29.000000000 -0200
 +++ setup.py	2010-04-01 17:34:44.000000000 -0300
-@@ -54,10 +54,9 @@
- It uses the Translate Toolkit to get access to translation files and therefore
- can edit a variety of files (including PO and XLIFF files)."""
- 
--INSTALL_CONFIG_DIR = '/etc/pootle'
-+INSTALL_CONFIG_DIR = 'etc/pootle'
- INSTALL_DATA_DIR = 'share/pootle'
--INSTALL_DOC_DIR = 'share/doc/pootle'
--INSTALL_WORKING_DIR = '/var/lib/pootle'
-+INSTALL_WORKING_DIR = '/var/db/pootle'
- 
- ###############################################################################
- # HELPER FUNCTIONS
 @@ -65,8 +64,7 @@
  
  def collect_options():
