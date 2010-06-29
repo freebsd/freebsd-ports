545baf45d1ce8ca86d99df4fc470ab2f293a6405
olgeni <olgeni@FreeBSD.org>
2010-06-29 21:33:03 +0000

    Upgrade to version 9.0.1.
    Feature safe:	yes

--- lang/mit-scheme/files/patch-obstack.h
+++
@@ -1,14 +0,0 @@
-
-$FreeBSD$
-
---- obstack.h.orig
-+++ obstack.h
-@@ -231,7 +231,7 @@
- 
- #define obstack_blank_fast(h,n) ((h)->next_free += (n))
- 
--#if defined (__GNUC__) && defined (__STDC__)
-+#if defined (USE_OBSTACK_MACROS) && defined (__GNUC__) && defined (__STDC__)
- 
- /* For GNU C, if not -traditional,
-    we can define these macros to compute all args only once
