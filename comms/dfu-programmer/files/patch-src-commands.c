a805e15dcdd690bcfca8a95e799fcc69a5e8bec7
stefan <stefan@FreeBSD.org>
2010-06-30 19:22:46 +0000

    Update to 0.5.2.
    PR:		147160
    Submitted by:	Lucius Windschuh <lwindschuh@gmail.com>
    Approved by:	maintainer timeout (1 month)
    Feature safe:	yes

---
+++ comms/dfu-programmer/files/patch-src-commands.c
@@ -0,0 +1,10 @@
+--- src/commands.c.old	2010-05-28 12:54:19.000000000 +0200
++++ src/commands.c	2010-05-28 12:54:37.000000000 +0200
+@@ -18,6 +18,7 @@
+  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+  */
+ 
++#include <stdlib.h>
+ #include <stdio.h>
+ #include <stdint.h>
+ #include <string.h>
