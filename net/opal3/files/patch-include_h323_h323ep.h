d9dd1cdb02cc59a2ca097359e95e5a91b306d23e
Stefan Walter <stefan@FreeBSD.org>
2010-06-30 04:45:01 +0000

    - Fix build with H.323 (fixed by Richard Kolkovich <richard@sigil.org>). - Add port license.
    PR:		146420
    Submitted by:	Richard Kolkovich <richard@sigil.org>
    Patch by:	Richard Kolkovich <richard@sigil.org>
    		"Alexander V. Chernikov" <melifaro@ipfw.ru> (maintainer)
    Feature safe:	yes

---
+++ net/opal3/files/patch-include_h323_h323ep.h
@@ -0,0 +1,10 @@
+--- include/h323/h323ep.h.orig	2010-06-29 22:41:03.000000000 +0400
++++ include/h323/h323ep.h	2010-06-29 22:41:17.000000000 +0400
+@@ -49,6 +49,7 @@
+ #include <h323/h323con.h>
+ #include <h323/h323caps.h>
+ #include <h323/h235auth.h>
++#include <asn/h225.h>
+ 
+ #if OPAL_H460
+ #include <h460/h4601.h>
