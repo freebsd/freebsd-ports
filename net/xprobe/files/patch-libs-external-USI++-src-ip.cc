28ee23d9ac4fdf91974c5ad0119235575de49419
Wesley Shields <wxs@FreeBSD.org>
2010-07-10 03:45:11 +0000

    Fix build with gcc45 Don't overwrite configuration file on install Mark MAKE_JOBS_SAFE
    PR:		ports/148451
    Submitted by:	swell.k@gmail.com

---
+++ net/xprobe/files/patch-libs-external-USI++-src-ip.cc
@@ -0,0 +1,10 @@
+--- libs-external/USI++/src/ip.cc~
++++ libs-external/USI++/src/ip.cc
+@@ -15,6 +15,7 @@
+ #include "config.h"
+ #include <iostream>
+ #include <string.h>
++#include <cstdlib>
+ #include <errno.h>
+ #include <new>
+ #include <vector>
---
+++ net/xprobe/files/patch-libs-external-USI++-src-misc.cc
@@ -0,0 +1,10 @@
+--- libs-external/USI++/src/misc.cc~
++++ libs-external/USI++/src/misc.cc
+@@ -1,6 +1,7 @@
+ #include "usi++/usi++"
+ #include "usi++/usi-structs.h"
+ #include <string.h>
++#include <cstdlib>
+ #include <unistd.h>
+ #include <sys/ioctl.h>
+ #include <errno.h>
