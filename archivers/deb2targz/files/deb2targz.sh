762043450fba5e3ebdba0dbf1d2b21c3ce625497
ehaupt <ehaupt@FreeBSD.org>
2010-07-10 17:14:57 +0000

    Update to 20100710. This update is actually a shell replacement of the original perl script.
    Benefits:
     - perl is no longer required as a dependency
     - other contents than data.tar.gz (data.tar, data.tar.bz2, data.tar.lzma,
       data.tar.xz) is now properly dealt with
     - deb package isn't sucked into memory anymore
    
    Submitted by:	Alex Kozlov <spam@rm-rf.kiev.ua>

---
+++ archivers/deb2targz/files/deb2targz.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+PATH=/bin:/usr/bin
+
+[ $# -eq 0 ] && {
+	echo 1>&2 "Usage: ${0##*/} <deb-file> [<deb-file> ...]";
+	exit 1;
+}
+
+for i in "$@"; do
+	[ "${i##*.}" != "deb" ] && {
+		echo "${0##*/}: ignoring '$i' (not a .deb)";
+		continue;
+	}
+
+	for file in `tar tf "$i"`; do
+		[ "${file%%.*}" = "data" ] && {
+			tar xOf "${i}" "${file}" > "${i%%.deb}.${file#data.}"
+		}
+	done
+done
+
