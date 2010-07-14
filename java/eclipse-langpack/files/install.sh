3be95731acdd0fc008e240e35b7df570bd239de2
glewis <glewis@FreeBSD.org>
2010-07-14 04:54:40 +0000

    . Update paths for new eclipse version.

--- java/eclipse-langpack/files/install.sh
+++ java/eclipse-langpack/files/install.sh
@@ -5,9 +5,9 @@ OWNER=$3; GROUP=$4; DPERM=$5; FPERM=$6
 
 cd ${WRKDIR}
 for dir  in `find eclipse -type d | sort`; do
-	install -d -o ${OWNER} -g ${GROUP} -m ${DPERM}         ${PREFIX}/${dir}
+	install -d -o ${OWNER} -g ${GROUP} -m ${DPERM} ${PREFIX}/lib/${dir}
 done
 
 for file in `find eclipse -type f | sort`; do
-	install -c -o ${OWNER} -g ${GROUP} -m ${FPERM} ${file} ${PREFIX}/${file}
+	install -c -o ${OWNER} -g ${GROUP} -m ${FPERM} ${file} ${PREFIX}/lib/${file}
 done
