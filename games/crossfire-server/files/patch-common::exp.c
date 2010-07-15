8dee8a7f0b87aa23139faa9cae407b80e29d6bc5
miwi <miwi@FreeBSD.org>
2010-07-15 05:13:38 +0000

    - Update to 1.15.0
    PR:		148554
    Submitted by:	Ports Fury

--- games/crossfire-server/files/patch-common::exp.c
+++ games/crossfire-server/files/patch-common::exp.c
@@ -1,11 +1,11 @@
---- common/exp.c.orig	Sat Mar 27 06:59:25 2004
-+++ common/exp.c	Thu Aug 26 05:56:20 2004
-@@ -190,7 +190,7 @@
- 		exit(1);
- 	    }
+--- common/exp.c.orig	2010-04-26 15:23:21.000000000 +0900
++++ common/exp.c	2010-05-25 04:22:52.000000000 +0900
+@@ -224,7 +224,7 @@
+                 exit(1);
+             }
  
--	    tmpexp = atoll(cp);
-+	    tmpexp = strtoll(cp, (char **)NULL, 10);
- 	    /* Do some sanity checking - if value is bogus, just exit because
- 	     * the table otherwise is probably in an inconsistent state
- 	     */
+-            tmpexp = atoll(cp);
++            tmpexp = strtoll(cp, (char **)NULL, 10);
+             /* Do some sanity checking - if value is bogus, just exit because
+              * the table otherwise is probably in an inconsistent state
+              */
