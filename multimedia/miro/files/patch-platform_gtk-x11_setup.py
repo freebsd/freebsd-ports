bd85f7fc4c6b14eff2a5a9b8b8c3dfe5a9bc55b1
miwi <miwi@FreeBSD.org>
2010-07-15 07:12:44 +0000

    - Update to 3.0.2
    PR:		148566
    Submitted by:	Ports Fury

--- multimedia/miro/files/patch-platform_gtk-x11_setup.py
+++ multimedia/miro/files/patch-platform_gtk-x11_setup.py
@@ -1,35 +1,6 @@
---- platform/gtk-x11/setup.py.orig	2009-12-03 01:40:42.000000000 +0900
-+++ platform/gtk-x11/setup.py	2009-12-21 06:10:08.000000000 +0900
-@@ -70,7 +70,7 @@
- # Examples:
- # XPCOM_RUNTIME_PATH = "/usr/lib/firefox"
- # XPCOM_RUNTIME_PATH = "/usr/lib/xulrunner-1.9.0.1"
--XPCOM_RUNTIME_PATH = None
-+XPCOM_RUNTIME_PATH = "%%XPCOM_RUNTIME_PATH%%"
- 
- # Location of xulrunner/firefox components for gtkmozembed.set_comp_path.
- # See documentation for set_comp_path here:
-@@ -112,14 +112,15 @@
- # XPCOM_LIB = "firefox-xpcom"
- # GTKMOZEMBED_LIB = "firefox-gtkmozembed"
- # XULRUNNER_19 = False
--XPCOM_LIB = None
--GTKMOZEMBED_LIB = None
--XULRUNNER_19 = None
-+XPCOM_LIB = "%%XPCOM_LIB%%"
-+GTKMOZEMBED_LIB = "%%GTKMOZEMBED_LIB%%"
-+XULRUNNER_19 = %%XULRUNNER_19%%
- 
- # The name of the boost python library.  Used for building extensions.
- # If this is set to None, setup.py will divine the right lib to use.
- # It should probably be either "boost_python" or "boost_python-mt".
--BOOST_LIB = None
-+BOOST_LIB = 'boost_python'
-+BOOST_LIB_PATH = '%%LOCALBASE%%/lib'
- 
- 
- ###############################################################################
-@@ -285,6 +286,9 @@
+--- platform/gtk-x11/setup.py.orig	2010-05-23 05:39:24.000000000 +0900
++++ platform/gtk-x11/setup.py	2010-06-14 18:47:40.000000000 +0900
+@@ -198,6 +198,9 @@
              options_dict['library_dirs'].append(rest)
          elif prefix == '-l':
              options_dict['libraries'].append(rest)
@@ -39,56 +10,16 @@
          else:
              options_dict['extra_compile_args'].append(comp)
  
-@@ -315,7 +319,7 @@
-     print "Using %s" % BOOST_LIB
- 
- def compile_xine_extractor():
--    rv = os.system("gcc %s -o %s `pkg-config --libs --cflags gdk-pixbuf-2.0 glib-2.0 libxine`" %
-+    rv = os.system("%%CC%% %s -o %s `pkg-config --libs --cflags gdk-pixbuf-2.0 glib-2.0 libxine` %%PTHREAD_LIBS%%" %
-                    (os.path.join(platform_dir, "xine/xine_extractor.c"), os.path.join(platform_dir, "xine/xine_extractor")))
-     if rv != 0:
-         raise RuntimeError("xine_extractor compilation failed.  Possibly missing libxine, gdk-pixbuf-2.0, or glib-2.0.")
-@@ -353,7 +357,7 @@
+@@ -241,7 +244,7 @@
          exit;
      fi
  
--    %(runtimelib)s$GDB -ex 'set breakpoint pending on' -ex 'run' --args $PYTHON ./miro.real --sync "$@"
-+    %(runtimelib)s$GDB -ex 'set breakpoint pending on' -ex 'run' --args $PYTHON %%PREFIX%%/bin/miro.real --sync "$@"
+-    $GDB -ex 'set breakpoint pending on' -ex 'run' --args $PYTHON ./miro.real --sync "$@"
++    $GDB -ex 'set breakpoint pending on' -ex 'run' --args $PYTHON %%PREFIX%%/bin/miro.real --sync "$@"
  else
-     %(runtimelib)smiro.real "$@"
+     miro.real "$@"
  fi
-@@ -366,6 +370,7 @@
-     Extension("miro.fasttypes",
-         sources = [os.path.join(portable_dir, 'fasttypes.cpp')],
-         libraries = [BOOST_LIB],
-+        library_dirs = [BOOST_LIB_PATH],
-     )
- 
- 
-@@ -402,12 +407,12 @@
-                           "-DHAVE___INCLUDE_LIBTORRENT_ASIO_SSL_STREAM_HPP=1",
-                           "-DHAVE___INCLUDE_LIBTORRENT_ASIO_IP_TCP_HPP=1",
-                           "-DHAVE_PTHREAD=1", "-DTORRENT_USE_OPENSSL=1", "-DHAVE_SSL=1",
--                          "-DNDEBUG=1", "-O2"]
-+                          "-DNDEBUG=1"]
- 
-     if is_x64():
-         extra_compile_args.append("-DAMD64")
- 
--    libraries = ['z', 'pthread', 'ssl']
-+    libraries = ['z', 'ssl']
- 
-     # get mt or non-mt versions of the boost libraries
-     libraries += [mt_or_not("boost_python"),
-@@ -432,6 +437,7 @@
-     return Extension("miro.libtorrent",
-                      include_dirs=include_dirs,
-                      libraries=libraries,
-+                     library_dirs=[BOOST_LIB_PATH],
-                      extra_compile_args=extra_compile_args,
-                      sources=sources)
- 
-@@ -558,7 +564,7 @@
+@@ -253,7 +256,7 @@
  xlib_ext = \
      Extension("miro.plat.xlibhelper",
          [ os.path.join(platform_package_dir,'xlibhelper.pyx') ],
@@ -97,7 +28,7 @@
          libraries = ['X11'],
      )
  
-@@ -644,36 +650,36 @@
+@@ -274,34 +277,34 @@
  # filter out app.config.template (which is handled specially)
  files = [f for f in listfiles(resource_dir) \
          if os.path.basename(f) != 'app.config.template']
@@ -139,12 +70,9 @@
 -    ('/usr/share/man/man1',
 +    ('%%MANPREFIX%%/man/man1',
       [os.path.join(platform_dir, 'miro.real.1.gz')]),
--    ('/usr/lib/miro/',
-+    ('%%PREFIX%%/libexec/miro',
-      [os.path.join(platform_dir, 'xine/xine_extractor')]),
  ]
  
-@@ -697,7 +703,7 @@
+@@ -324,7 +327,7 @@
  
      def install_app_config(self):
          source = os.path.join(resource_dir, 'app.config.template')
@@ -153,7 +81,7 @@
  
          config_file = util.read_simple_config_file(source)
          print "Trying to figure out the git revision...."
-@@ -739,7 +745,7 @@
+@@ -366,7 +369,7 @@
              lang = os.path.basename(source)[:-3]
              if 'LINGUAS' in os.environ and lang not in os.environ['LINGUAS']:
                  continue
@@ -162,7 +90,7 @@
              if self.root:
                  dest = change_root(self.root, dest)
              self.mkpath(os.path.dirname(dest))
-@@ -768,7 +774,7 @@
+@@ -395,7 +398,7 @@
  
  #### install_theme installs a specified theme .zip
  class install_theme(Command):
@@ -171,7 +99,7 @@
      user_options = [("theme=", None, 'ZIP file containing the theme')]
  
      def initialize_options(self):
-@@ -796,7 +802,7 @@
+@@ -423,7 +426,7 @@
              raise DistutilsOptionError, "invalid theme file"
          self.zipfile = zf
          self.theme_name = themeName
