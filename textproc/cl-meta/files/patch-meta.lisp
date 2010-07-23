0e5dcb10fc9998d1a60a5096b6e2dbf0be384031
Jimmy Olgeni <olgeni@FreeBSD.org>
2010-07-23 13:10:12 +0000

    Building textproc/cl-meta-clisp fails with the following message:
    ;; Compiling file /usr/local/lib/common-lisp/meta/meta.lisp ...
    *** - SYSTEM::LINE-COMMENT-READER: Invalid byte #xFC in CHARSET:ASCII
          conversion
    
    It turns out that clisp does not like non-ASCII characters, even
    in comments.
    
    textproc/cl-meta contains source code for textproc/cl-meta-clisp:
    patch a comment line to use "Nuremberg" rather than the actual
    German spelling, which contains the 0xFC character (U-umlaut).
    
    This unbreaks textproc/cl-meta-clisp.
    
    Noticed by:	QAT via itetcu, pointyhat via pav

---
+++ textproc/cl-meta/files/patch-meta.lisp
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- meta.lisp.orig
++++ meta.lisp
+@@ -47,7 +47,7 @@
+ ;;;;       Proc. 19'th Nat'l. Conf. of the ACM (Aug. 1964),D1.3-1-D1.3-11.
+ ;;;;
+ ;;;;
+-;;;; Nürnberg, 01.Jul.2001 Jochen Schmidt
++;;;; Nuremberg, 01.Jul.2001 Jochen Schmidt
+ 
+ 
+ (in-package :meta)
