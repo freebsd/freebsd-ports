9fc420d1466a96d1a7c5e666aa345944264cf627
makc <makc@FreeBSD.org>
2010-07-24 21:34:59 +0000

    Add new port games/oldrunner:
    Oldrunner is a remake of Broderbund's Loderunner which contains the 150
    original game levels. It is written in C and has a textmode interface.
    
    WWW:	http://culot.org/public/Code/oldrunner.html
    
    PR:		ports/148789
    Submitted by:	Frederic Culot <frederic at culot.org>

---
+++ games/oldrunner/files/patch-cfg.h
@@ -0,0 +1,7 @@
+--- cfg.h.orig	2010-07-20 12:04:52.252243199 +0200
++++ cfg.h	2010-07-20 13:51:34.642563477 +0200
+@@ -59,3 +59,4 @@
+  *
+  *   #define LEVELS_PATH     "/usr/local/share/games/oldrunner/levels"
+  */
++#define LEVELS_PATH "/usr/local/share/oldrunner"
