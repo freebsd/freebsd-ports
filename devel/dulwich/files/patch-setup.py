1e58a10ad986a5e178474522ddb750d67b015288
Sylvio Cesar Teixeira <sylvio@FreeBSD.org>
2010-07-25 01:12:13 +0000

    - Add other mirror - Ajust patch file
    PR:		ports/148766
    Submitted by:	Marco Broder <marco.broeder@gmx.eu> (maintainer)

--- devel/dulwich/files/patch-setup.py
+++ devel/dulwich/files/patch-setup.py
@@ -1,7 +1,6 @@
 --- ./setup.py.orig     2010-03-03 16:33:52.000000000 +0100
 +++ ./setup.py  2010-03-03 21:13:31.000000000 +0100
-@@ -2,10 +2,7 @@
- # Setup file for bzr-git
+@@ -3,8 +3,5 @@
  # Copyright (C) 2008-2010 Jelmer Vernooij <jelmer@samba.org>
 
 -try:
@@ -11,4 +10,3 @@
 +from distutils.core import setup, Extension
  from distutils.core import Distribution
 
- dulwich_version_string = '0.6.0'
