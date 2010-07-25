e9ad81a56b34d162fa488d13eefd1bb549426f25
mm <mm@FreeBSD.org>
2010-07-25 16:37:32 +0000

    Update to 20100724 (build 104)

--- multimedia/x264-devel/files/patch-common_cpu.c
+++ multimedia/x264-devel/files/patch-common_cpu.c
@@ -1,30 +1,29 @@
---- common/cpu.c.orig	2010-02-28 10:49:54.000000000 -0800
-+++ common/cpu.c	2010-02-28 10:56:06.000000000 -0800
-@@ -211,6 +211,27 @@
-     return cpu;
- }
+--- common/cpu.c.orig	2010-06-24 22:45:07.000000000 +0200
++++ common/cpu.c	2010-07-18 22:55:49.324572565 +0200
+@@ -234,7 +234,7 @@
  
-+#elif defined(SYS_FREEBSD)
-+#include <sys/sysctl.h>
-+uint32_t x264_cpu_detect( void )
-+{
-+    uint32_t cpu;
-+    int has_altivec, error;
-+    size_t length;
-+
-+    cpu = 0;
-+    has_altivec = 0;
-+    length = sizeof( has_altivec );
-+    error = sysctlbyname("hw.altivec", &has_altivec, &length, NULL, 0 );
-+
-+    if( error == 0 && has_altivec != 0 )
-+    {
-+        cpu |= X264_CPU_ALTIVEC;
-+    }
-+
-+    return cpu;
-+}
-+
- #elif defined( SYS_LINUX )
- #include <signal.h>
- #include <setjmp.h>
+ #elif ARCH_PPC
+ 
+-#if SYS_MACOSX || SYS_OPENBSD
++#if SYS_MACOSX || SYS_OPENBSD || SYS_FREEBSD
+ #include <sys/sysctl.h>
+ uint32_t x264_cpu_detect( void )
+ {
+@@ -242,12 +242,16 @@
+     uint32_t cpu = 0;
+ #if SYS_OPENBSD
+     int      selectors[2] = { CTL_MACHDEP, CPU_ALTIVEC };
+-#else
++#elif SYS_MACOSX
+     int      selectors[2] = { CTL_HW, HW_VECTORUNIT };
+ #endif
+     int      has_altivec = 0;
+     size_t   length = sizeof( has_altivec );
++#if SYS_MACOSX || SYS_OPENBSD
+     int      error = sysctl( selectors, 2, &has_altivec, &length, NULL, 0 );
++#else
++    int      error = sysctlbyname("hw.altivec", &has_altivec, &length, NULL, 0 );
++#endif
+ 
+     if( error == 0 && has_altivec != 0 )
+         cpu |= X264_CPU_ALTIVEC;
