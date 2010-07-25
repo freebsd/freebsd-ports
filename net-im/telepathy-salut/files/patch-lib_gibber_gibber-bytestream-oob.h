97df3c97c1f9a023b14943141a5c1b7e89255eff
marcus <marcus@FreeBSD.org>
2010-07-25 00:50:41 +0000

    Update to 0.3.12.

--- net-im/telepathy-salut/files/patch-lib_gibber_gibber-bytestream-oob.h
+++ net-im/telepathy-salut/files/patch-lib_gibber_gibber-bytestream-oob.h
@@ -1,10 +1,10 @@
---- lib/gibber/gibber-bytestream-oob.h.orig	2008-01-17 00:55:20.000000000 -0500
-+++ lib/gibber/gibber-bytestream-oob.h	2008-01-17 00:55:54.000000000 -0500
-@@ -22,6 +22,7 @@
- 
- #include <glib-object.h>
+--- lib/gibber/gibber-bytestream-oob.h.orig	2010-05-20 06:10:39.000000000 -0400
++++ lib/gibber/gibber-bytestream-oob.h	2010-07-24 20:41:19.000000000 -0400
+@@ -20,6 +20,7 @@
+ #ifndef __GIBBER_BYTESTREAM_OOB_H__
+ #define __GIBBER_BYTESTREAM_OOB_H__
  
 +#include <netinet/in.h>
- #include <netdb.h>
+ #include <glib-object.h>
  
  #include "gibber-bytestream-iface.h"
--- net-im/telepathy-salut/files/patch-lib_gibber_gibber-tcp-transport.c
+++ net-im/telepathy-salut/files/patch-lib_gibber_gibber-tcp-transport.c
@@ -1,10 +1,10 @@
---- lib/gibber/gibber-tcp-transport.c.orig	Thu Apr 19 16:54:23 2007
-+++ lib/gibber/gibber-tcp-transport.c	Thu Apr 19 16:54:46 2007
-@@ -18,6 +18,7 @@
-  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-  */
+--- lib/gibber/gibber-tcp-transport.c.orig	2010-05-20 06:10:39.000000000 -0400
++++ lib/gibber/gibber-tcp-transport.c	2010-07-24 20:42:16.000000000 -0400
+@@ -20,6 +20,7 @@
+ 
+ #include <config.h>
  
 +#include <netinet/in.h>
- 
  #include <stdio.h>
  #include <stdlib.h>
+ 
