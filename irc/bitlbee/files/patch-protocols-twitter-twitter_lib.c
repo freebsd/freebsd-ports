dd29be8bf20e9b138bbb0321e7a8cb48aa3f1846
brix <brix@FreeBSD.org>
2010-07-26 12:26:58 +0000

    - Add upstream fix for twitter protocol segfault (http://bugs.bitlbee.org/bitlbee/ticket/650)
    Submitted by:  ashish@

---
+++ irc/bitlbee/files/patch-protocols-twitter-twitter_lib.c
@@ -0,0 +1,37 @@
+--- protocols/twitter/twitter_lib.c.orig	2010-07-04 17:26:16.000000000 +0000
++++ protocols/twitter/twitter_lib.c	2010-07-26 11:39:17.000000000 +0000
+@@ -65,6 +65,8 @@ static void twitter_groupchat_init(struc
+  */
+ static void txu_free(struct twitter_xml_user *txu)
+ {
++	if (txu == NULL)
++		return;
+ 	g_free(txu->name);
+ 	g_free(txu->screen_name);
+ 	g_free(txu);
+@@ -88,6 +90,8 @@ static void txs_free(struct twitter_xml_
+ static void txl_free(struct twitter_xml_list *txl)
+ {
+ 	GSList *l;
++	if (txl == NULL)
++		return;
+ 	for ( l = txl->list; l ; l = g_slist_next(l) )
+ 		if (txl->type == TXL_STATUS)
+ 			txs_free((struct twitter_xml_status *)l->data);
+@@ -472,6 +476,9 @@ static void twitter_groupchat(struct im_
+ 	for ( l = list; l ; l = g_slist_next(l) )
+ 	{
+ 		status = l->data;
++		if (status->user == NULL || status->text == NULL)
++			continue;
++
+ 		twitter_add_buddy(ic, status->user->screen_name, status->user->name);
+ 		
+ 		strip_html(status->text);
+@@ -735,4 +742,4 @@ void twitter_friendships_create_destroy(
+ 	args[0] = "screen_name";
+ 	args[1] = who;
+ 	twitter_http(ic, create ? TWITTER_FRIENDSHIPS_CREATE_URL : TWITTER_FRIENDSHIPS_DESTROY_URL, twitter_http_post, ic, 1, args, 2);
+-}
+\ No newline at end of file
++}
