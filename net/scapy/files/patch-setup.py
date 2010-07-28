f5c791780862f5e476549e36cd1f90b5f16d960b
Tilman Keskinoz <arved@FreeBSD.org>
2010-07-28 15:44:07 +0000

    Update to 2.1.1
    PR:		148850
    Submitted by:	Ruslan Mahmatkhanov <cvs-src@yandex.ru>
    Approved by:	maintainer

--- net/scapy/files/patch-setup.py
+++ net/scapy/files/patch-setup.py
@@ -1,11 +1,11 @@
---- ./setup.py.orig	2010-01-05 11:40:22.000000000 +0100
-+++ ./setup.py	2010-01-05 11:40:57.000000000 +0100
+--- setup.py	2010-04-10 02:42:04.000000000 +0400
++++ setup.py	2010-07-22 21:17:18.000000000 +0400
 @@ -42,7 +42,7 @@
  setup(
      name = 'scapy',
-     version = '2.1.0', 
+     version = '2.1.1',
 -    packages=['scapy','scapy/arch', 'scapy/arch/windows', 'scapy/layers','scapy/asn1','scapy/tools','scapy/modules', 'scapy/crypto'],
-+    packages=['scapy','scapy/arch','scapy/layers','scapy/asn1','scapy/tools','scapy/modules', 'scapy/crypto'],
++    packages=['scapy','scapy/arch', 'scapy/layers','scapy/asn1','scapy/tools','scapy/modules', 'scapy/crypto'],
      scripts = SCRIPTS,
      data_files = [('share/man/man1', ["doc/scapy.1.gz"])],
  
