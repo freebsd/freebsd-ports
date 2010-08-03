b2ac0645e63ff81abe6036a216adc5a01c7b1ee6
rafan <rafan@FreeBSD.org>
2010-08-03 14:15:51 +0000

    - Update to 1.0

--- graphics/py-graphviz/files/patch-setup.py
+++ graphics/py-graphviz/files/patch-setup.py
@@ -1,24 +1,22 @@
---- setup.py.orig
-+++ setup.py
-@@ -20,8 +20,8 @@
-     print "To install, run 'python setup.py install'"
-     print
+--- setup.py.orig	2010-08-01 21:18:02.000000000 +0800
++++ setup.py	2010-08-02 15:59:57.000000000 +0800
+@@ -28,8 +28,8 @@
+           sys.version_info[:2]
+     sys.exit(-1)
  
 -library_path=None
 -include_path=None
 +#library_path=None
 +#include_path=None
  
- 
  # If the setup script couldn't find your graphviz installation you can
-@@ -33,8 +33,8 @@
- #include_path='/usr/include/graphviz'
- 
- # UNIX, Linux alternate
--#library_path='/usr/local/lib/graphviz'
--#include_path='/usr/local/include/graphviz'
+ # specify it here by uncommenting these lines or providing your own:
+@@ -42,6 +42,8 @@
+ # OSX, Linux, alternate location
+ #library_path='/usr/local/lib/graphviz'
+ #include_path='/usr/local/include/graphviz'
 +library_path='%%LOCALBASE%%/lib/graphviz'
 +include_path='%%LOCALBASE%%/include/graphviz'
  
- # Mac OS X (Fink)
+ # OSX (Fink)
  #library_path='/sw/lib/graphviz'
