235006770ee3cb59c022de038dbca00f8166f369
Thomas Abthorpe <tabthorpe@FreeBSD.org>
2010-08-04 20:37:13 +0000

    - Update to 1.4.21
    PR:		ports/149063 (based on)
    Submitted by:	Daniel Bond <db nsn.no>
    Security:	CVE-2010-1637 and CVE-2010-2813

--- mail/squirrelmail/files/patch-class__deliver__Deliver.class.php
+++ mail/squirrelmail/files/patch-class__deliver__Deliver.class.php
@@ -1,10 +1,10 @@
---- ./class/deliver/Deliver.class.php.orig	2009-04-15 18:00:49.000000000 -0400
-+++ ./class/deliver/Deliver.class.php	2009-05-21 01:18:26.000000000 -0400
+--- ./class/deliver/Deliver.class.php.orig	2010-08-03 16:33:49.000000000 -0400
++++ ./class/deliver/Deliver.class.php	2010-08-03 16:35:25.000000000 -0400
 @@ -542,7 +542,19 @@
          $cnt = count($header);
          $hdr_s = '';
          for ($i = 0 ; $i < $cnt ; $i++)    {
--            $hdr_s .= $this->foldLine($header[$i], 78);
+-            $hdr_s .= $this->foldLine($header[$i]);
 +          /*
 +           * FIXME: Leave some name fields (filename, username) as they
 +           * are, for now. If you want to fold these fields,
@@ -21,11 +21,11 @@
          }
          $header = $hdr_s;
          $header .= $rn; /* One blank line to separate mimeheader and body-entity */
-@@ -773,6 +785,7 @@
+@@ -778,6 +790,7 @@
              case 'Cc':
              case 'Bcc':
              case 'From':
-+            case 'Subject': // Prevent from base64-folding corruption.
++	    case 'Subject': // Prevent from base64-folding corruption.
                  $hdr_s .= $header[$i];
                  break;
-             default: $hdr_s .= $this->foldLine($header[$i], 78); break;
+             default: $hdr_s .= $this->foldLine($header[$i]); break;
