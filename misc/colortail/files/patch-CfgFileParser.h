762214a0af2c8ad7a8d6b5ec6f3187eac5351ba4
Thomas Abthorpe <tabthorpe@FreeBSD.org>
2010-08-04 20:59:44 +0000

    - Update to 0.3.3
    PR:		ports/149149
    Submitted by:	Frederic Culot <frederic culot.org>

---
+++ misc/colortail/files/patch-CfgFileParser.h
@@ -0,0 +1,10 @@
+--- CfgFileParser.h.bak	2010-07-31 14:18:50.529980803 +0200
++++ CfgFileParser.h	2010-07-31 14:18:59.977448567 +0200
+@@ -24,7 +24,6 @@
+ #include "config.h"
+ 
+ #include <sys/types.h>
+-#include <regex.h>
+ #include <iostream>
+ #include <fstream>
+ #include <string>
--- misc/colortail/files/patch-TailFile.cc
+++ misc/colortail/files/patch-TailFile.cc
@@ -1,21 +1,14 @@
---- TailFile.cc.orig	Thu Aug  5 08:09:38 1999
-+++ TailFile.cc	Thu Feb  5 19:49:47 2004
-@@ -375,7 +375,7 @@
-    // check if there isn't a follow buffer
-    if (m_follow_buffer == NULL)
-    {
--      m_follow_buffer = new ostrstream();
-+      m_follow_buffer = new std::ostrstream();
-    }
- 
-    // make buffer
-@@ -417,6 +417,9 @@
+--- TailFile.cc.bak	2010-07-31 14:12:15.145254811 +0200
++++ TailFile.cc	2010-07-31 14:13:31.996950208 +0200
+@@ -352,7 +352,10 @@
        int ch = fgetc(m_file);
  
        // add the character to the string
+-      m_follow_buffer->put(ch);
 +      if (ch == EOF)
 +          clearerr(m_file);     // required by POSIX and ANSI
 +      else
-       m_follow_buffer->put(ch);
++          m_follow_buffer->put(ch);
        
        // check if return
+       if (ch == '\n')
