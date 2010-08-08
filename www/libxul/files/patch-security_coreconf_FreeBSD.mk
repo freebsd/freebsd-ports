19859332183cdce53cd3f84d43b2b8a0af9817b2
beat <beat@FreeBSD.org>
2010-08-08 08:56:06 +0000

    - Update to 1.9.2.8
    PR:		ports/149044
    Expr-run by:	pav@
    Thanks to:	Andreas Tobler <andreast-list AT fgznet.ch>, decke@

--- www/libxul/files/patch-security_coreconf_FreeBSD.mk
+++ www/libxul/files/patch-security_coreconf_FreeBSD.mk
@@ -1,57 +1,18 @@
---- security/coreconf/FreeBSD.mk.orig	2009-11-22 11:51:31.000000000 -0800
-+++ security/coreconf/FreeBSD.mk	2009-11-22 11:51:44.000000000 -0800
-@@ -37,16 +37,32 @@
- 
- include $(CORE_DEPTH)/coreconf/UNIX.mk
- 
--DEFAULT_COMPILER	= gcc
--CC			= gcc
--CCC			= g++
-+CCC			= ${CXX}
- RANLIB			= ranlib
- 
- ifeq ($(OS_TEST),alpha)
- CPU_ARCH		= alpha
- else
-+ifeq ($(OS_TEST),powerpc)
-+CPU_ARCH		= powerpc
-+else
-+ifeq ($(OS_TEST),sparc64)
-+CPU_ARCH		= sparc64
-+USE_64			= 1
-+else
-+ifeq ($(OS_TEST),ia64)
-+CPU_ARCH		= ia64
-+USE_64			= 1
-+else
-+ifeq ($(OS_TEST),amd64)
-+CPU_ARCH		= amd64
-+else
+--- security/coreconf/FreeBSD.mk.orig	2010-06-26 07:29:06.000000000 +0200
++++ security/coreconf/FreeBSD.mk	2010-07-18 13:25:06.000000000 +0200
+@@ -49,9 +49,6 @@
+ ifeq ($(CPU_ARCH),pc98)
  CPU_ARCH		= x86
  endif
-+endif
-+endif
-+endif
-+endif
+-ifeq ($(CPU_ARCH),amd64)
+-CPU_ARCH		= x86_64
+-endif
  
- OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
+ OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -Wno-switch -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
  
-@@ -73,7 +89,11 @@
- DLL_SUFFIX		= so.1.0
- endif
- 
--MKSHLIB			= $(CC) $(DSO_LDOPTS)
-+ifneq (,$(filter alpha ia64,$(OS_TEST)))
-+MKSHLIB			= $(CC) -Wl,-Bsymbolic -lc $(DSO_LDOPTS) -o $@
-+else
-+MKSHLIB			= $(CC) -Wl,-Bsymbolic $(DSO_LDOPTS) -o $@
-+endif
- ifdef MAPFILE
- 	MKSHLIB += -Wl,--version-script,$(MAPFILE)
- endif
-@@ -81,5 +101,3 @@
-         sed -e 's,;+,,' -e 's; DATA ;;' -e 's,;;,,' -e 's,;.*,;,' > $@
+@@ -87,4 +84,4 @@
  
  G++INCLUDES		= -I/usr/include/g++
--
+ 
 -INCLUDES		+= -I/usr/X11R6/include
++#INCLUDES		+= -I/usr/X11R6/include
