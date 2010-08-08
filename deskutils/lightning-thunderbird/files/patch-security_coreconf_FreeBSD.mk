12d804c91e28f0945e125f1264bfccc0ffa4ff74
Beat Gaetzi <beat@FreeBSD.org>
2010-08-08 10:34:32 +0000

    - Add Lightning 1.0 Beta 2. This version works with Thunderbird 3.1.
    Thanks to:	Andreas Tobler <andreast-list AT fgznet.ch>, jsa@

--- deskutils/lightning-thunderbird/files/patch-security_coreconf_FreeBSD.mk
+++ deskutils/lightning-thunderbird/files/patch-security_coreconf_FreeBSD.mk
@@ -1,37 +1,33 @@
---- mozilla/security/coreconf/FreeBSD.mk.orig	Wed Mar 26 20:17:25 2003
-+++ mozilla/security/coreconf/FreeBSD.mk	Thu Jul  8 12:56:24 2004
-@@ -35,16 +35,18 @@
- 
- include $(CORE_DEPTH)/coreconf/UNIX.mk
- 
--DEFAULT_COMPILER	= gcc
--CC			= gcc
--CCC			= g++
-+CCC			= ${CXX}
- RANLIB			= ranlib
- 
- ifeq ($(OS_TEST),alpha)
- CPU_ARCH		= alpha
- else
+--- mozilla/security/coreconf/FreeBSD.mk.orig	2010-04-30 19:44:32.000000000 +0000
++++ mozilla/security/coreconf/FreeBSD.mk	2010-05-10 13:04:45.000000000 +0000
+@@ -52,6 +52,15 @@
+ ifeq ($(CPU_ARCH),amd64)
+ CPU_ARCH		= x86_64
+ endif
 +ifeq ($(OS_TEST),amd64)
 +CPU_ARCH		= amd64
-+else
- CPU_ARCH		= x86
- endif
++endif
++ifeq ($(OS_TEST),powerpc)
++CPU_ARCH		= powerpc
++endif
++ifeq ($(OS_TEST),sparc64)
++CPU_ARCH		= sparc64
 +endif
  
- OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
+ OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -Wno-switch -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
  
-@@ -71,7 +73,11 @@
+@@ -78,7 +87,7 @@
  DLL_SUFFIX		= so.1.0
  endif
  
 -MKSHLIB			= $(CC) $(DSO_LDOPTS)
-+ifneq (,$(filter alpha ia64,$(OS_TEST)))
 +MKSHLIB			= $(CC) -Wl,-Bsymbolic -lc $(DSO_LDOPTS) -o $@
-+else
-+MKSHLIB			= $(CC) -Wl,-Bsymbolic $(DSO_LDOPTS) -o $@
-+endif
  ifdef MAPFILE
- # Add LD options to restrict exported symbols to those in the map file
+ 	MKSHLIB += -Wl,--version-script,$(MAPFILE)
  endif
+@@ -87,4 +96,4 @@
+ 
+ G++INCLUDES		= -I/usr/include/g++
+ 
+-INCLUDES		+= -I/usr/X11R6/include
++#INCLUDES		+= -I/usr/local/include
