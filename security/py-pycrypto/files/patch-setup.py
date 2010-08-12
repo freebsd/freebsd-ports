986aefcfc4f15aca8b0341e0e5298432d057e444
Li-Wen Hsu <lwhsu@FreeBSD.org>
2010-08-12 19:15:37 +0000

    - Update to 2.2
    PR:		ports/149423
    Submitted by:	Frederic Culot <frederic AT culot.org>
    Approved by:	Keith Gaughan <k AT stereochro.me> (maintainer)

--- security/py-pycrypto/files/patch-setup.py
+++ security/py-pycrypto/files/patch-setup.py
@@ -1,14 +1,14 @@
---- ./setup.py.orig	2009-12-13 21:39:29.000000000 +0100
-+++ ./setup.py	2009-12-14 16:48:47.000000000 +0100
-@@ -136,6 +136,7 @@
+--- setup.py.orig	2010-08-08 14:02:23.895825034 +0200
++++ setup.py	2010-08-08 14:04:31.663792751 +0200
+@@ -139,6 +139,7 @@
                  # especially helps the DES modules.
                  self.__add_compiler_option("-O3")
                  self.__add_compiler_option("-fomit-frame-pointer")
 +                self.__add_compiler_option("-D_WCHAR_T_DEFINED")
                  # Don't include debug symbols unless debugging
                  self.__remove_compiler_option("-g")
-             if USE_GCOV:
-@@ -152,7 +153,7 @@
+                 # Don't include profiling information (incompatible with -fomit-frame-pointer)
+@@ -157,7 +158,7 @@
              self.compiler.include_dirs.insert(0, "src/inc-msvc/")
  
          # Detect libgmp and don't build _fastmath if it is missing.
@@ -17,7 +17,7 @@
          if not (self.compiler.find_library_file(lib_dirs, 'gmp')):
              print >>sys.stderr, "warning: GMP library not found; Not building Crypto.PublicKey._fastmath."
              self.__remove_extensions(["Crypto.PublicKey._fastmath"])
-@@ -259,6 +260,7 @@
+@@ -264,6 +265,7 @@
              Extension("Crypto.PublicKey._fastmath",
                        include_dirs=['src/'],
                        libraries=['gmp'],
