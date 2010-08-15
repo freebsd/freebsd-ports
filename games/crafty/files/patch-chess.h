ffd7962fd93c601336ec774a9af718cb92069e2f
miwi <miwi@FreeBSD.org>
2010-08-15 08:41:35 +0000

    - Update to 23.3
    PR:		149518
    Submitted by:	Florian Smeets <flo@kasimir.com> (maintainer)

--- games/crafty/files/patch-chess.h
+++ games/crafty/files/patch-chess.h
@@ -1,10 +1,11 @@
---- chess.h.orig	2010-01-24 12:22:38.000000000 +0100
-+++ chess.h	2010-01-24 12:22:45.000000000 +0100
-@@ -23,7 +23,6 @@
+--- ./chess.h.orig	2010-08-11 11:20:40.000000000 +0200
++++ ./chess.h	2010-08-11 11:20:49.000000000 +0200
+@@ -24,7 +24,7 @@
  #include <stdio.h>
  #include <assert.h>
  #include <stdlib.h>
--#include <malloc.h>
+-#if !defined(IPHONE)
++#if !defined(IPHONE) && !defined(__FreeBSD__)   
+ #  include <malloc.h>
+ #endif
  #include <string.h>
- #if !defined(TYPES_INCLUDED)
- #  include "lock.h"
