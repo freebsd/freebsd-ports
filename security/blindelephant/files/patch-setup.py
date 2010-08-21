08ae07d6045bbac02c8091182f924b541f24dae0
Niels Heinen <niels@FreeBSD.org>
2010-08-21 09:27:45 +0000

    New port: The BlindElephant Web Application Fingerprinter attempts to discover the version of a (known) web application by comparing static files at known locations against precomputed hashes for versions of those files in all available releases. The technique is fast, low-bandwidth, non-invasive, generic, and highly automatable.
    WWW: http://blindelephant.sourceforge.net/
    
    Approved by:	itetcu (mentor)

---
+++ security/blindelephant/files/patch-setup.py
@@ -0,0 +1,18 @@
+--- setup.py.orig	2010-08-20 15:32:51.000000000 +0200
++++ setup.py	2010-08-20 15:33:03.000000000 +0200
+@@ -1,7 +1,7 @@
+ from distutils.core import setup
+ 
+ setup(
+-    name='BlindElephant',
++    name='blindelephant',
+     version='1.0',
+     description='Uses static file fingerprinting to determine the version of web applications and plugins installed at a url', 
+     author="Patrick Thomas",
+@@ -17,4 +17,4 @@
+         "Topic :: Internet :: WWW/HTTP"
+         "Programming Language :: Python :: 2.6"
+     ]
+-)
+\ No newline at end of file
++)
