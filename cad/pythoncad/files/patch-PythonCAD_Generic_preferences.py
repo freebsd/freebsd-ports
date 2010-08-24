e01494e9cbca1fb0d76e6435f55828f0d8f85b02
amdmi3 <amdmi3@FreeBSD.org>
2010-08-24 17:47:47 +0000

    - Update to 37
    PR:		149027
    Submitted by:	Sven Esbjerg <esbjerg@xbsd.net> (maintainer)

---
+++ cad/pythoncad/files/patch-PythonCAD_Generic_preferences.py
@@ -0,0 +1,11 @@
+--- PythonCAD/Generic/preferences.py.orig	2010-07-25 18:10:27.707308605 +0200
++++ PythonCAD/Generic/preferences.py	2010-07-25 18:10:46.194466051 +0200
+@@ -41,7 +41,7 @@
+ # global variables
+ #
+ 
+-pref_file = '/etc/pythoncad/prefs.py'
++pref_file = '/usr/local/etc/pythoncad/prefs.py'
+ 
+ #
+ 
