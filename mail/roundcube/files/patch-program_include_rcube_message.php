482cbac6d7fd4820ba2253ae4cfaedf9a772d9b0
Alex Dupre <ale@FreeBSD.org>
2010-08-24 15:27:41 +0000

    Update to 0.4 final release.

--- mail/roundcube/files/patch-program_include_rcube_message.php
+++ mail/roundcube/files/patch-program_include_rcube_message.php
@@ -1,11 +1,11 @@
---- program/include/rcube_message.php.orig	2009-08-27 10:08:27.000000000 +0200
-+++ program/include/rcube_message.php	2009-09-07 17:47:43.000000000 +0200
-@@ -384,7 +384,7 @@
- 
-         // part is a file/attachment
-         else if (preg_match('/^(inline|attach)/', $mail_part->disposition) ||
--                 $mail_part->headers['content-id'] || (empty($mail_part->disposition) && $mail_part->filename)) {
-+                 $mail_part->headers['content-id'] || $mail_part->filename) {
- 
-           // skip apple resource forks
-           if ($message_ctype_secondary == 'appledouble' && $secondary_type == 'applefile')
+--- program/include/rcube_message.php.orig	2010-08-06 15:22:20.000000000 +0200
++++ program/include/rcube_message.php	2010-08-24 12:42:33.000000000 +0200
+@@ -427,7 +427,7 @@
+                 }
+                 // part is a file/attachment
+                 else if (preg_match('/^(inline|attach)/', $mail_part->disposition) ||
+-                    $mail_part->headers['content-id'] || (empty($mail_part->disposition) && $mail_part->filename)
++                    $mail_part->headers['content-id'] || $mail_part->filename
+                 ) {
+                     // skip apple resource forks
+                     if ($message_ctype_secondary == 'appledouble' && $secondary_type == 'applefile')
