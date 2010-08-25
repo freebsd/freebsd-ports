d93d49f0a23e0468a5cfbe4b8e8e250e8e38e36f
lwhsu <lwhsu@FreeBSD.org>
2010-08-25 16:08:02 +0000

    - Update to 5.0.11
    PR:		ports/148465
    Submitted by:	Francisco de Borja Lopez Rio <borja AT pexego.es> (maintainer)

--- finance/openerp-web/files/openerp-web.py
+++ finance/openerp-web/files/openerp-web.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # EASY-INSTALL-ENTRY-SCRIPT: 'openerp-web==5.0.6','console_scripts','openerp-web'
-__requires__ = 'openerp-web==5.0.6'
+__requires__ = 'openerp-web==5.0.11'
 import sys
 from pkg_resources import load_entry_point
 
 sys.exit(
-   load_entry_point('openerp-web==5.0.6', 'console_scripts', 'openerp-web')()
+   load_entry_point('openerp-web==5.0.11', 'console_scripts', 'openerp-web')()
 )
