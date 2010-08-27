fb69128ad71524f65eb8c60e976b9fdde8801c12
avilla <avilla@FreeBSD.org>
2010-08-27 18:16:16 +0000

    - Upgrade to 2.1.0. - Fix build on 6.X. - Fix build with GCC 4.5. [1] - Fix dependency on Video4Linux. [1] - Add LICENSE. - Use bsd.port.options.mk.
    PR:		ports/148955 [1]
    Submitted by:	swell.k@gmail.com [1]
    Approved by:	tabthorpe (mentor)

---
+++ graphics/opencv/files/patch-apps-traincascade-imagestorage.h
@@ -0,0 +1,10 @@
+--- apps/traincascade/imagestorage.h~
++++ apps/traincascade/imagestorage.h
+@@ -1,6 +1,7 @@
+ #ifndef _OPENCV_IMAGESTORAGE_H_
+ #define _OPENCV_IMAGESTORAGE_H_
+ 
++#include <cstdio>
+ #include <highgui.h>
+ 
+ using namespace cv;
---
+++ graphics/opencv/files/patch-samples-c-image.cpp
@@ -0,0 +1,7 @@
+--- samples/c/image.cpp~
++++ samples/c/image.cpp
+@@ -1,3 +1,4 @@
++#include <cstdio>
+ #include "cv.h" // include standard OpenCV headers, same as before
+ #include "highgui.h"
+ 
