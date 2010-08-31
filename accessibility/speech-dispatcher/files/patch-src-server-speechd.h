2d361c4363a2e22386c3e179e38745a424764fd4
avilla <avilla@FreeBSD.org>
2010-08-31 14:23:10 +0000

    - Upgrade to 0.7. - Remove rc.d script and pkg-message. - Add LICENSE. - Use bsd.port.options.mk.
    Approved by:	tabthorpe (mentor)

--- accessibility/speech-dispatcher/files/patch-src-server-speechd.h
+++ accessibility/speech-dispatcher/files/patch-src-server-speechd.h
@@ -1,11 +1,11 @@
---- src/server/speechd.h.orig	2009-12-08 16:41:26.578943406 +0000
-+++ src/server/speechd.h	2009-12-08 16:42:16.923002174 +0000
-@@ -53,7 +53,7 @@
- #include <sys/sem.h>
- 
- /* Definition of semun needed for semaphore manipulation */
--#if defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)
+--- src/server/speechd.h.orig	2010-08-25 03:20:44.157056094 +0200
++++ src/server/speechd.h	2010-08-25 03:21:00.004912354 +0200
+@@ -56,7 +56,7 @@
+ /* TODO: This fixes compilation for Mac OS X but might not be a correct
+    solution for other platforms. A better check is needed, possibly including
+    _POSIX_C_SOURCE and friends*/
+-#if (defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)) || defined(__APPLE__)
 +#if 1
- /* union semun is defined by including <sys/sem.h> */
+  /* union semun is defined by including <sys/sem.h> */
  #else
- /* according to X/OPEN we have to define it ourselves */
+  /* according to X/OPEN we have to define it ourselves */
