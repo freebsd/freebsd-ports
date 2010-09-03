7485e5adfc94aaf15801c95e1a4242590331d000
pav <pav@FreeBSD.org>
2010-09-03 09:29:06 +0000

    - Fix build with gcc44
    PR:		ports/150055
    Submitted by:	Ganael Laplanche <ganael.laplanche@martymac.com> (maintainer)

---
+++ archivers/par2cmdline-tbb/files/patch-par2cmdline.h
@@ -0,0 +1,10 @@
+--- par2cmdline.h.orig	2010-08-28 00:21:07.730518551 +0200
++++ par2cmdline.h	2010-08-28 00:21:42.779186595 +0200
+@@ -428,6 +428,7 @@
+ #include <vector>
+ #include <map>
+ #include <algorithm>
++#include <memory>
+ 
+ #include <ctype.h>
+ #include <iostream>
