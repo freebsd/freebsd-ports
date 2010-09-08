8e136452161039990b6692b09e4a8818f6e4cdee
bapt <bapt@FreeBSD.org>
2010-09-08 07:16:34 +0000

    - Fix build on CURRENT [1] - Fix build on 6.4 [2]
    Submitted by:	Benjamin Kaduk <kaduk-fbsd _at_ mit.edu> [1] (maintainer)
    Reported by:	pointy hat (erwin) [1], pointy hat (pav) [2]

---
+++ net-im/zephyr/files/patch-lib-Zinternal.c
@@ -0,0 +1,10 @@
+--- lib/Zinternal.c.orig	2010-09-07 22:44:54.000000000 -0400
++++ lib/Zinternal.c	2010-09-07 22:45:00.000000000 -0400
+@@ -14,7 +14,6 @@
+ #include <internal.h>
+ #include <arpa/inet.h>
+ #include <sys/socket.h>
+-#include <utmp.h>
+ 
+ #ifndef lint
+ static const char rcsid_Zinternal_c[] =
