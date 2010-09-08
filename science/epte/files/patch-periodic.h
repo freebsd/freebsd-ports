9cce9c1991bc2494c8e05addde57e94762852298
sahil <sahil@FreeBSD.org>
2010-09-08 03:09:31 +0000

    epte is a curses-based periodic table of the elements. It provides a set of examples of the basic sets of constants and procedures needed to understand the behavior of matter.
    WWW: http://www.toddmiller.com/epte/
    
    PR:		ports/150183
    Submitted by:	Frederic Culot <frederic@culot.org>

---
+++ science/epte/files/patch-periodic.h
@@ -0,0 +1,11 @@
+--- periodic.h.orig	2010-09-01 10:31:23.665697651 +0200
++++ periodic.h	2010-09-01 10:48:57.742657991 +0200
+@@ -296,7 +296,7 @@ modification follow.
+ #ifdef VMS
+ #include <curses.h>
+ #endif
+-#if defined(linux) || defined(__FreeBSD)
++#if defined(linux) || defined(__FreeBSD__)
+ #include <ncurses.h>
+ #endif
+ 
---
+++ science/epte/files/patch-setaudio.c
@@ -0,0 +1,11 @@
+--- setaudio.c.orig	2010-09-01 10:55:30.665968356 +0200
++++ setaudio.c	2010-09-01 10:56:04.763401409 +0200
+@@ -312,7 +312,7 @@ void setaudio ( void )
+ char File_Name[1024];
+ char *file_name;
+ 
+-#ifdef VMS
++#if defined(VMS) || defined(__FreeBSD__)
+ #else
+ 
+   /* The audio device sometimes does not work if it is left open for a long
