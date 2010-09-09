3d530650a018d5ed89a363a68a0968e897380dda
obrien <obrien@FreeBSD.org>
2010-09-09 20:00:00 +0000

    Fix issue where remote sounds over RDP does not work by applying the "Rdp Sound in Vista [PATCH]" patch from http://sourceforge.net/mailarchive/forum.php?thread_name=200902131243.10881.sameer.subscriptions%40damagehead.com&forum_name=rdesktop-devel
    PR:		135500
    Submitted by:	Lapo Luchini <lapo@lapo.it>

---
+++ net/rdesktop/files/patch-rdpdr.c
@@ -0,0 +1,9 @@
+--- rdpdr.c.orig	2008-02-14 03:37:17.000000000 -0800
++++ rdpdr.c	2010-09-09 12:56:03.000000000 -0700
+@@ -842,5 +842,5 @@ RD_BOOL
+ rdpdr_init()
+ {
+-	if (g_num_devices > 0)
++//	if (g_num_devices > 0)
+ 	{
+ 		rdpdr_channel =
