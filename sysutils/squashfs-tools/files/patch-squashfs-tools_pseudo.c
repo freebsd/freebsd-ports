75b4459430d753b3eb6e8adc9c2bd8f7b7d52827
Ashish SHUKLA <ashish@FreeBSD.org>
2010-09-14 16:19:16 +0000

    - Fix compile time warnings - Mark as MAKE_JOBS_SAFE - Refactor Makefile to avoid custom do-build/do-install targets - Add LICENSE
    Submitted by:	mi@ via private email
    Approved by:	tabthorpe(mentor)

--- sysutils/squashfs-tools/files/patch-squashfs-tools_pseudo.c
+++ sysutils/squashfs-tools/files/patch-squashfs-tools_pseudo.c
@@ -1,7 +1,7 @@
 $FreeBSD$
 
---- squashfs-tools/pseudo.c.orig	2009-04-05 04:01:58.000000000 +0200
-+++ squashfs-tools/pseudo.c	2010-04-29 17:45:49.000000000 +0200
+--- pseudo.c	2009-04-05 04:01:58.000000000 +0200
++++ pseudo.c	2010-04-29 17:45:49.000000000 +0200
 @@ -31,6 +31,10 @@
  #include <stdlib.h>
  #include <sys/types.h>
