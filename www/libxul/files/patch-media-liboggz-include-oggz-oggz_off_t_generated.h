a7e98fe15f30b8216ce0ffc8b704e06bd567cfef
Bernhard Froehlich <decke@FreeBSD.org>
2010-09-15 15:38:29 +0000

    - Update to 1.9.2.9
    Approved by:	beat (co-mentor)
    Security:	http://www.vuxml.org/freebsd/4a21ce2c-bb13-11df-8e32-000f20797ede.html

--- www/libxul/files/patch-media-liboggz-include-oggz-oggz_off_t_generated.h
+++ www/libxul/files/patch-media-liboggz-include-oggz-oggz_off_t_generated.h
@@ -1,11 +1,11 @@
---- media/liboggz/include/oggz/oggz_off_t_generated.h.orig	2009-08-24 17:52:50.000000000 +0200
-+++ media/liboggz/include/oggz/oggz_off_t_generated.h	2009-09-10 19:13:22.000000000 +0200
+--- media/liboggz/include/oggz/oggz_off_t_generated.h.orig	2010-09-06 11:33:54.000000000 +0200
++++ media/liboggz/include/oggz/oggz_off_t_generated.h	2010-09-06 11:34:16.000000000 +0200
 @@ -59,7 +59,7 @@
  
  #include <sys/types.h>
  
--#if defined(__APPLE__) || defined(SOLARIS) || defined(OS2)
-+#if defined(__APPLE__) || defined(SOLARIS) || defined(OS2) || defined(__FreeBSD__)
+-#if defined(__APPLE__) || defined(SOLARIS) || defined(OS2) || defined (_AIX)
++#if defined(__APPLE__) || defined(SOLARIS) || defined(OS2) || defined (_AIX) || defined (__FreeBSD__)
  typedef off_t oggz_off_t;
  #else
  typedef loff_t oggz_off_t;
