81549a3c2688bb35f5c3a9db381afe3319b5d3a1
jpaetzel <jpaetzel@FreeBSD.org>
2010-09-15 19:28:20 +0000

    Update to 2.1.1 Add OPTIONs to install optional software
    PR:	ports/150577
    Submitted by:	Josh Paetzel <jpaetzel@FreeBSD.org>
    Approved by:	Denis Pokataev <catone@cpan.org>

--- textproc/pootle/files/patch-setup.py
+++ textproc/pootle/files/patch-setup.py
@@ -1,12 +1,12 @@
---- setup.py.orig	2009-12-02 07:33:29.000000000 -0200
-+++ setup.py	2010-04-01 17:34:44.000000000 -0300
-@@ -65,8 +64,7 @@
+--- setup.py.orig	2010-08-26 13:51:15.000000000 -0500
++++ setup.py	2010-09-15 14:05:20.000000000 -0500
+@@ -65,8 +65,7 @@
  
  def collect_options():
      data_files = [
 -        (INSTALL_CONFIG_DIR, ['localsettings.py']),
 -        (INSTALL_DOC_DIR, ['wsgi.py', 'ChangeLog', 'COPYING', 'README', 'INSTALL']),
 +        (INSTALL_CONFIG_DIR, ['localsettings.py.sample']),
-         (INSTALL_WORKING_DIR + '/dbs', []) # Create the empty "dbs" dir
+         (INSTALL_WORKING_DIR + '/dbs', []), # Create the empty "dbs" dir
      ] + list_tree(INSTALL_DATA_DIR, 'templates') + list_tree(INSTALL_DATA_DIR, 'html') + \
          list_tree(INSTALL_WORKING_DIR, 'po') + list_tree(INSTALL_DATA_DIR, 'mo')
