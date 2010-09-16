893f4b14c03b827197b2381c1aecff3df081f8f6
Maho Nakata <maho@FreeBSD.org>
2010-09-16 21:48:04 +0000

    Update to  1.3r1.3913.
    Actually the version number is from SVN, and maho@ manually
    checked out from SVN.
    The source archive is not distributed as tar ball.

--- biology/pymol/files/patch-setup.py
+++ biology/pymol/files/patch-setup.py
@@ -1,19 +1,21 @@
---- setup.py.orig	2007-07-03 07:11:15.000000000 +0900
-+++ setup.py	2008-03-06 17:59:33.000000000 +0900
-@@ -88,6 +88,8 @@
-              "layer0","layer1","layer2",
-              "layer3","layer4","layer5",
-              "/usr/include/freetype2",
-+             "%%LOCALBASE%%/include",
-+             "%%LOCALBASE%%/include/freetype2",
- #             "/users/warren/ext/include",
+--- setup.py.bak	2010-09-16 10:45:18.000000000 +0900
++++ setup.py	2010-09-16 10:51:57.000000000 +0900
+@@ -127,7 +127,8 @@
+     inc_dirs=["ov/src",
+               "layer0","layer1","layer2",
+               "layer3","layer4","layer5",
+-              "/usr/include/freetype2",
++              "%%LOCALBASE%%/include",
++              "%%LOCALBASE%%/include/freetype2",
+ #                 "/users/warren/ext/include",
  # VMD plugin support
- #             "contrib/uiuc/plugins/include",
-@@ -98,6 +100,7 @@
-    pyogl_libs = ["GL","GLU","glut"]
-    lib_dirs=[
-       "/usr/X11R6/lib",
-+      "%%LOCALBASE%%/lib",
- #      "/users/warren/pymol/ext/lib"
-       ]
-    def_macros=[("_PYMOL_MODULE",None),
+ #              "contrib/uiuc/plugins/include",
+@@ -139,7 +140,7 @@
+           ]	
+     pyogl_libs = ["GL","GLU","glut"]
+     lib_dirs=[
+-        "/usr/X11R6/lib",
++         "%%LOCALBASE%%/lib",
+ #        "/users/warren/pymol/ext/lib"
+         ]
+     def_macros=[("_PYMOL_MODULE",None),
