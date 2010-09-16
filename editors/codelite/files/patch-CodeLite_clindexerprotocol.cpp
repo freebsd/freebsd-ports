97777871c9d03da2b3d97ed62ca2d73c8f49fbf8
gahr <gahr@FreeBSD.org>
2010-09-16 10:18:41 +0000

    - Update to 2.7.0.4375   Announcement and changelog available here:   http://www.codelite.org/forum/viewtopic.php?f=9&t=1075

--- editors/codelite/files/patch-CodeLite_clindexerprotocol.cpp
+++ editors/codelite/files/patch-CodeLite_clindexerprotocol.cpp
@@ -1,37 +1,37 @@
---- sdk/codelite_indexer/network/clindexerprotocol.cpp.orig	2009-05-26 14:27:26.000000000 +0200
-+++ sdk/codelite_indexer/network/clindexerprotocol.cpp	2009-05-26 14:30:37.000000000 +0200
-@@ -25,7 +25,7 @@
+--- sdk/codelite_indexer/network/clindexerprotocol.cpp.orig	2010-09-15 11:53:03.000000000 +0000
++++ sdk/codelite_indexer/network/clindexerprotocol.cpp	2010-09-15 11:54:50.000000000 +0000
+@@ -36,7 +36,7 @@
  	}
  
  	if (actual_read != sizeof(buff_len)) {
--		fprintf(stderr, "ERROR: ReadReply: Protocol error: expected %d bytes, got %d. reason: %d\n",
+-		fprintf(stderr, "ERROR: ReadReply: Protocol error: expected %u bytes, got %u. reason: %d\n",
 +		fprintf(stderr, "ERROR: ReadReply: Protocol error: expected %zu bytes, got %zu. reason: %d\n",
  				sizeof(buff_len),
  				actual_read,
  				conn->getLastError());
-@@ -39,7 +39,7 @@
+@@ -55,7 +55,7 @@
  	size_t bytes_read(0);
  	while (bytes_left > 0) {
  		if ( !conn->read(data+bytes_read, bytes_left, &actual_read, 10000) ) {
--			fprintf(stderr, "ERROR: Protocol error: expected %d bytes, got %d\n", buff_len, actual_read);
+-			fprintf(stderr, "ERROR: Protocol error: expected %u bytes, got %u\n", buff_len, actual_read);
 +			fprintf(stderr, "ERROR: Protocol error: expected %zu bytes, got %zu\n", buff_len, actual_read);
  			return false;
  		}
  		bytes_left -= actual_read;
-@@ -71,7 +71,7 @@
+@@ -78,7 +78,7 @@
  	}
  
  	if (actual_read != sizeof(buff_len)) {
--		fprintf(stderr, "ERROR: Protocol error: expected %d bytes, got %d\n", sizeof(buff_len), actual_read);
+-		fprintf(stderr, "ERROR: Protocol error: expected %u bytes, got %u\n", sizeof(buff_len), actual_read);
 +		fprintf(stderr, "ERROR: Protocol error: expected %zu bytes, got %zu\n", sizeof(buff_len), actual_read);
  		return false;
  	}
  
-@@ -82,7 +82,7 @@
+@@ -92,7 +92,7 @@
  	size_t bytes_read(0);
  	while (bytes_left > 0) {
  		if ( !conn->read(data+bytes_read, bytes_left, &actual_read, -1) ) {
--			fprintf(stderr, "ERROR: [%s] Protocol error: expected %d bytes, got %d\n", __PRETTY_FUNCTION__, buff_len, actual_read);
+-			fprintf(stderr, "ERROR: [%s] Protocol error: expected %u bytes, got %u\n", __PRETTY_FUNCTION__, buff_len, actual_read);
 +			fprintf(stderr, "ERROR: [%s] Protocol error: expected %zu bytes, got %zu\n", __PRETTY_FUNCTION__, buff_len, actual_read);
  			return false;
  		}
