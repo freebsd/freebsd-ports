01820d3d91d1b1f1b696175f2e466b9899f49642
Thierry Thomas <thierry@FreeBSD.org>
2010-09-16 21:23:10 +0000

    Upgrade to 10.2.0-2, add a common file to be included by the other related ports, and depends on Tcl/Tk 8.5.

---
+++ french/aster/bsd.aster.mk
@@ -0,0 +1,28 @@
+# Date created:			  Tue Sep 14, 2010
+# Whom:				  Thierry Thomas (<thierry@pompo.net>)
+#
+# $FreeBSD$
+#
+# Common variables used by ports related to Code_Aster
+
+DISTVERSIONPREFIX=	full-src-
+DISTVERSIONSUFFIX=	.noarch
+MASTER_SITES=	http://www.code-aster.org/V2/UPLOAD/DOC/Telechargement/
+DISTNAME=	aster-${DISTVERSIONPREFIX}${ASTER_DISTVERSION}${DISTVERSIONSUFFIX}
+MD5_FILE=	${.CURDIR}/../../french/aster/distinfo
+
+ASTER_VER=	10.2.0
+ASTER_DISTVERSION=	${ASTER_VER}-2
+EXTRACT_WRKSRC=	${WRKDIR}/aster-${DISTVERSIONPREFIX}${ASTER_VER}/SRC
+
+.if ${PORTNAME} == metis-edf
+DVSFX=		${DISTVERSIONSUFFIX}
+.elif ${PORTNAME} == homard
+DVSFX=		.all
+.endif
+
+.if ${PORTNAME} != aster
+post-extract:
+	(cd ${WRKDIR} &&	\
+	${EXTRACT_CMD} ${EXTRACT_BEFORE_ARGS} ${EXTRACT_WRKSRC}/${PORTNAME}-${DISTVERSION}${DVSFX}${EXTRACT_SUFX} ${EXTRACT_AFTER_ARGS})
+.endif
