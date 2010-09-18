67f714dbd8f3586a9d4c80bd3368b46628088f6e
makc <makc@FreeBSD.org>
2010-09-18 15:57:24 +0000

    Update to 0.4.3
    PR:		ports/150661
    Submitted by:	KATO Tsuguru <tkato432 at yahoo.com>

--- games/gtkatlantic/files/patch-readpng.c
+++ games/gtkatlantic/files/patch-readpng.c
@@ -1,11 +1,11 @@
---- src/readpng.c.orig	2004-06-25 09:02:45.000000000 +0200
-+++ src/readpng.c	2010-03-30 14:01:35.000000000 +0200
+--- src/readpng.c.orig	2010-04-18 11:36:49.000000000 +0900
++++ src/readpng.c	2010-09-14 03:27:35.000000000 +0900
 @@ -33,7 +33,7 @@
  	png_uint_32 width, height;
  
  	fread(sig, 1, 8, infile);
--	if (!png_check_sig(sig, 8))
-+	if (png_sig_cmp(sig, 0, 8))
+-	if (!png_check_sig((png_bytep)sig, 8))
++	if (png_sig_cmp((png_bytep)sig, 0, 8))
  		return FALSE;
  
  	*png_struct = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
