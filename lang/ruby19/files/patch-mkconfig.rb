5ae76ebf7a0d88047b9e823cf4f3c9c18595364c
stas <stas@FreeBSD.org>
2010-09-20 01:28:36 +0000

    - Provide the correct value for TOPDIR in ruby 1.9. - Bump portrevision.
    Reported by:	Jase Thew (irc)

--- lang/ruby19/files/patch-mkconfig.rb
+++ lang/ruby19/files/patch-mkconfig.rb
@@ -1,6 +1,16 @@
---- mkconfig.rb.orig	2009-02-09 19:56:48.000000000 +0300
-+++ mkconfig.rb	2009-02-09 19:59:30.000000000 +0300
-@@ -140,7 +140,7 @@
+--- mkconfig.rb.orig	2008-12-12 06:54:37.000000000 -0800
++++ mkconfig.rb	2010-09-19 17:51:43.000000000 -0700
+@@ -102,7 +102,8 @@
+ 
+ drive = File::PATH_SEPARATOR == ';'
+ 
+-prefix = '/lib/ruby/' + RUBY_VERSION + '/' + RUBY_PLATFORM
++major, minor, *rest = RUBY_VERSION.split('.')
++prefix = '/lib/ruby/' + "#{major}.#{minor}/" + RUBY_PLATFORM
+ print "  TOPDIR = File.dirname(__FILE__).chomp!(#{prefix.dump})\n"
+ print "  DESTDIR = ", (drive ? "TOPDIR && TOPDIR[/\\A[a-z]:/i] || " : ""), "'' unless defined? DESTDIR\n"
+ print "  CONFIG = {}\n"
+@@ -140,7 +141,7 @@
  print(*v_fast)
  print(*v_others)
  print <<EOS
