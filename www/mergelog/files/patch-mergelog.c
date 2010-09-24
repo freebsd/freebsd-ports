d49ae77ebc193d6ed859f83a78fbfa516fa1267c
tobez <tobez@FreeBSD.org>
2010-09-24 13:13:01 +0000

    Actually add a patch which should have gone into the previous commit.

---
+++ www/mergelog/files/patch-mergelog.c
@@ -0,0 +1,10 @@
+--- src/mergelog.c 2001-04-11 10:54:53.000000000 +0100
++++ src/mergelog.c 2010-09-22 11:11:12.000000000 +0100
+@@ -35,7 +35,7 @@
+ 
+ #define BUFFER_SIZE 32768
+ #define DATE_SIZE 14
+-#define SCAN_OFFSET 9
++#define SCAN_OFFSET 8
+ #define SCAN_SIZE 512
+ 
