478a268d25a687a8fe4ecb3d979cabfd72d24bf4
Gerald Pfeifer <gerald@FreeBSD.org>
2010-09-24 23:14:46 +0000

    USE_FORTRAN=yes now implies lang/gcc45 up from lang/gcc44.
    cad/salome and math/freemat needed some adjustments, apart from these
    this looks like a far more easy upgrade than previous ones and according
    to the upstream developers we do not even need to bump dependent ports
    since GNU Fortran 4.4 and 4.5 are sufficiently compatible.
    
    Tested by:	erwin (and pointyhat)

--- Mk/bsd.gcc.mk
+++ Mk/bsd.gcc.mk
@@ -19,7 +19,7 @@
 # If your port needs a Fortran compiler, please specify that with the
 # USE_FORTRAN= knob.  Here is the list of options for that knob:
 #
-#   USE_FORTRAN=	yes		# use gfortran44 (lang/gcc44)
+#   USE_FORTRAN=	yes		# use gfortran45 (lang/gcc45)
 #   USE_FORTRAN=	g77		# use g77-34 (lang/gcc34; FreeBSD>=7)
 #							# or system f77 (/usr/bin/f77; FreeBSD<=6)
 #   USE_FORTRAN=	ifort	# use the Intel compiler (lang/ifc)
@@ -74,9 +74,9 @@ _GCCVERSION_${v}_V=	${j}
 
 # The default case, with a current lang/gcc port.
 . if ${USE_FORTRAN} == yes
-_USE_GCC:=	4.4
-FC:=	gfortran44
-F77:=	gfortran44
+_USE_GCC:=	4.5
+FC:=	gfortran45
+F77:=	gfortran45
 
 # Intel Fortran compiler from lang/ifc.
 . elif ${USE_FORTRAN} == ifort
