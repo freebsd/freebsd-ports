bc59a3eafef40fb726faaa6bb39d660b88f63414
Wesley Shields <wxs@FreeBSD.org>
2010-09-26 19:22:23 +0000

    Update to 20100324
    PR:		ports/150499
    Submitted by:	Rudolf Cejka <cejkar@fit.vutbr.cz> (maintainer)

--- ftp/tnftpd/files/patch-src-ftpd.c
+++ ftp/tnftpd/files/patch-src-ftpd.c
@@ -1,109 +1,73 @@
 
-Add -C user@host support instead of just -C user and add support for
-IPv6 in ftpusers.
+Rewrite utmpx support.
 
-FreeBSD does not have _PASSWORD_CHGNOW defined, so use direct constant
-instead.
-
-We are sure, that we have good LOGIN_NAME_MAX from tnftpd.h, so use it
-instead of unnecessarily conservative _POSIX_LOGIN_NAME_MAX.
-
---- src/ftpd.c.orig	2008-06-08 20:52:33.000000000 -0400
-+++ src/ftpd.c	2008-06-08 20:52:33.000000000 -0400
-@@ -368,6 +368,24 @@
- 			break;
+--- src/ftpd.c.orig	2009-11-07 04:26:48.000000000 +0100
++++ src/ftpd.c	2009-11-07 04:26:48.000000000 +0100
+@@ -504,9 +504,6 @@
+ 		confdir = _DEFAULT_CONFDIR;
  
- 		case 'C':
-+			if ((p = strchr(optarg, '@')) != NULL) {
-+				*p++ = '\0';
-+				strlcpy(remotehost, p, MAXHOSTNAMELEN + 1);
-+				if (inet_pton(AF_INET, p,
-+				    &his_addr.su_addr) == 1) {
-+					his_addr.su_family = AF_INET;
-+					his_addr.su_len =
-+					    sizeof(his_addr.si_su.su_sin);
-+#ifdef INET6
-+				} else if (inet_pton(AF_INET6, p,
-+				    &his_addr.su_6addr) == 1) {
-+					his_addr.su_family = AF_INET6;
-+					his_addr.su_len =
-+					    sizeof(his_addr.si_su.su_sin6);
-+#endif
-+				} else
-+					his_addr.su_family = AF_UNSPEC;
-+			}
- 			pw = sgetpwnam(optarg);
- 			exit(checkaccess(optarg) ? 0 : 1);
- 			/* NOTREACHED */
-@@ -497,12 +515,12 @@
- 		exit(1);
- 	} else if (l <= 0) {
- 		syslog(LOG_WARNING, "using conservative LOGIN_NAME_MAX value");
--		curname_len = _POSIX_LOGIN_NAME_MAX;
-+		curname_len = LOGIN_NAME_MAX;
- 	} else 
- 		curname_len = (size_t)l;
- #else
- 	/* using conservative LOGIN_NAME_MAX value */
--	curname_len = _POSIX_LOGIN_NAME_MAX;
-+	curname_len = LOGIN_NAME_MAX;
+ 	if (dowtmp) {
+-#ifdef SUPPORT_UTMPX
+-		ftpd_initwtmpx();
+-#endif
+ #ifdef SUPPORT_UTMP
+ 		ftpd_initwtmp();
  #endif
- 	curname = malloc(curname_len);
- 	if (curname == NULL) {
-@@ -1179,18 +1197,38 @@
- 
- 					/* have a host specifier */
- 		if ((p = strchr(word, '@')) != NULL) {
--			unsigned long	net, mask, addr;
--			int		bits;
-+			char		net[16], mask[16], *addr;
-+			int		addrlen, bits, bytes, a;
- 
- 			*p++ = '\0';
- 					/* check against network or CIDR */
--			if (isdigit((unsigned char)*p) &&
--			    (bits = inet_net_pton(AF_INET, p,
--			    &net, sizeof(net))) != -1) {
--				net = ntohl(net);
--				mask = 0xffffffffU << (32 - bits);
--				addr = ntohl(his_addr.su_addr.s_addr);
--				if ((addr & mask) != net)
-+			memset(net, 0x00, sizeof(net));
-+			if ((bits = inet_net_pton(his_addr.su_family, p, net,
-+			    sizeof(net))) != -1) {
-+#ifdef INET6
-+				if (his_addr.su_family == AF_INET) {
-+#endif
-+					addrlen = 4;
-+					addr = (char *)&his_addr.su_addr;
-+#ifdef INET6
-+				} else {
-+					addrlen = 16;
-+					addr = (char *)&his_addr.su_6addr;
-+				}
-+#endif
-+				bytes = bits / 8;
-+				bits = bits % 8;
-+				if (bytes > 0)
-+					memset(mask, 0xFF, bytes);
-+				if (bytes < addrlen)
-+					mask[bytes] = 0xFF << (8 - bits);
-+				if (bytes + 1 < addrlen)
-+					memset(mask + bytes + 1, 0x00,
-+					    addrlen - bytes - 1);
-+				for (a = 0; a < addrlen; a++)
-+					if ((addr[a] & mask[a]) != net[a])
-+						break;
-+				if (a < addrlen)
- 					continue;
- 
- 					/* check against hostname glob */
-@@ -3776,7 +3814,7 @@
- 	expire = pwent->pw_expire;
+@@ -1319,23 +1316,18 @@
+ 	(void)gettimeofday(&tv, NULL);
  #endif
- #if defined(HAVE_STRUCT_PASSWD_PW_CHANGE)
--	change = (pwent->pw_change == _PASSWORD_CHGNOW)? now : pwent->pw_change;
-+	change = (pwent->pw_change == -1)? now : pwent->pw_change;
+ #ifdef SUPPORT_UTMPX
+-	if (doutmp) {
++	if (doutmp || dowtmp) {
+ 		(void)memset(&utmpx, 0, sizeof(utmpx));
+ 		utmpx.ut_tv = tv;
+ 		utmpx.ut_pid = getpid();
+-		utmpx.ut_id[0] = 'f';
+-		utmpx.ut_id[1] = 't';
+-		utmpx.ut_id[2] = 'p';
+-		utmpx.ut_id[3] = '*';
++		snprintf(utmpx.ut_id, sizeof(utmpx.ut_id), "%xftp",
++		    utmpx.ut_pid);
+ 		utmpx.ut_type = USER_PROCESS;
+-		(void)strncpy(utmpx.ut_name, name, sizeof(utmpx.ut_name));
++		(void)strncpy(utmpx.ut_user, name, sizeof(utmpx.ut_user));
+ 		(void)strncpy(utmpx.ut_line, line, sizeof(utmpx.ut_line));
+ 		(void)strncpy(utmpx.ut_host, host, sizeof(utmpx.ut_host));
+-		(void)memcpy(&utmpx.ut_ss, &haddr->si_su, haddr->su_len);
+-		ftpd_loginx(&utmpx);
++		(void)pututxline(&utmpx);
+ 	}
+-	if (dowtmp)
+-		ftpd_logwtmpx(line, name, host, haddr, 0, USER_PROCESS);
  #endif
+ #ifdef SUPPORT_UTMP
+ 	if (doutmp) {
+@@ -1355,17 +1347,23 @@
+ logout_utmp(void)
+ {
+ #ifdef SUPPORT_UTMPX
+-	int okwtmpx = dowtmp;
++	struct timeval tv;
++	(void)gettimeofday(&tv, NULL);
  #endif
- 
+ #ifdef SUPPORT_UTMP
+ 	int okwtmp = dowtmp;
+ #endif
+ 	if (logged_in) {
+ #ifdef SUPPORT_UTMPX
+-		if (doutmp)
+-			okwtmpx &= ftpd_logoutx(ttyline, 0, DEAD_PROCESS);
+-		if (okwtmpx)
+-			ftpd_logwtmpx(ttyline, "", "", NULL, 0, DEAD_PROCESS);
++		if (doutmp || dowtmp) {
++			(void)memset(&utmpx, 0, sizeof(utmpx));
++			utmpx.ut_tv = tv;
++			utmpx.ut_pid = getpid();
++			snprintf(utmpx.ut_id, sizeof(utmpx.ut_id), "%xftp",
++			    utmpx.ut_pid);
++			utmpx.ut_type = DEAD_PROCESS;
++			(void)pututxline(&utmpx);
++		}
+ #endif
+ #ifdef SUPPORT_UTMP
+ 		if (doutmp)
--- ftp/tnftpd/files/patch-src-logutmp.c
+++ ftp/tnftpd/files/patch-src-logutmp.c
@@ -1,21 +1,32 @@
 
 FreeBSD does not have header file util.h, fortunately it is not needed.
-Add header file time.h for time().
+Add header file time.h for time(). Rewrite utmpx support.
 
---- src/logutmp.c.orig	2006-12-04 16:13:22.000000000 -0500
-+++ src/logutmp.c	2006-12-04 16:13:22.000000000 -0500
-@@ -59,13 +59,13 @@
+--- src/logutmp.c.orig	2008-09-21 16:44:01.000000000 +0200
++++ src/logutmp.c	2008-09-21 16:44:01.000000000 +0200
+@@ -71,13 +71,15 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <string.h>
 +#include <time.h>
  #include <ttyent.h>
  #include <unistd.h>
++#ifdef SUPPORT_UTMP
  #include <utmp.h>
++#endif
  #ifdef SUPPORT_UTMPX
  #include <utmpx.h>
  #endif
 -#include <util.h>
  
- #include "extern.h"
+ #endif /* !defined(HAVE_TNFTPD_H) */
  
+@@ -161,7 +163,7 @@
+ }
+ #endif /* SUPPORT_UTMP */
+ 
+-#ifdef SUPPORT_UTMPX
++#if 0
+ /*
+  * special version of loginx which updates utmpx only.
+  */
