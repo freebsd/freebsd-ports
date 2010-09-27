3c854f9c6213de7beb2da8b7f1f48716009e8188
Pietro Cerutti <gahr@FreeBSD.org>
2010-09-27 19:20:13 +0000

    - Try to actually fix build on amd64

--- x11-toolkits/fox17/files/patch-lib_FXAtomic.cpp
+++ x11-toolkits/fox17/files/patch-lib_FXAtomic.cpp
@@ -1,5 +1,14 @@
---- lib/FXAtomic.cpp.orig	2010-09-27 15:06:25.000000000 +0000
-+++ lib/FXAtomic.cpp	2010-09-27 15:29:53.000000000 +0000
+--- lib/FXAtomic.cpp.orig	2010-09-18 00:09:37.000000000 +0000
++++ lib/FXAtomic.cpp	2010-09-27 18:35:26.000000000 +0000
+@@ -101,7 +101,7 @@
+ // Have API to find out which.
+ 
+ // If neither windows, nor inline assembly, then fallback to global mutex
+-#if !(defined(WIN32) || (defined(HAVE_INLINE_ASSEMBLY) && (defined(__i386__) || defined(__x86_64__))))
++#if !(defined(WIN32) || (defined(HAVE_INLINE_ASSEMBLY) && (defined(__i386__) || defined(__x86_64__)))) || defined(__FreeBSD__)
+ static pthread_mutex_t global_mutex=PTHREAD_MUTEX_INITIALIZER;
+ #endif
+ 
 @@ -323,7 +323,7 @@
                          "movl %%esi,%%ebx\n\t"                  // Swap ESI back to restore EBX
                          "setz %0\n\t" : "=a"(ret), "=D"(ptr) : "D"(ptr), "a"(cmpa), "d"(cmpb), "S"(a), "c"(b) : "memory", "cc");
