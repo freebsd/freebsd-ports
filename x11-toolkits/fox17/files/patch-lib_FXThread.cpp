472776ddd961f57272d38afec52616a9f7d955db
gahr <gahr@FreeBSD.org>
2010-09-27 10:40:06 +0000

    - Update to 1.7.22   Announcement:   http://sf.net/mailarchive/message.php?msg_name=20100925132949.77bd7d9e%40fox-toolkit.com

--- x11-toolkits/fox17/files/patch-lib_FXThread.cpp
+++ x11-toolkits/fox17/files/patch-lib_FXThread.cpp
@@ -1,11 +1,11 @@
---- lib/FXThread.cpp.orig	2010-08-23 13:21:01.000000000 +0000
-+++ lib/FXThread.cpp	2010-08-23 13:21:20.000000000 +0000
-@@ -1182,7 +1182,7 @@
+--- lib/FXThread.cpp.orig	2010-09-27 08:48:52.000000000 +0000
++++ lib/FXThread.cpp	2010-09-27 08:49:43.000000000 +0000
+@@ -582,7 +582,7 @@
+   InitializeCriticalSection(((RWLOCK*)data)->mutex);
+   InitializeCriticalSection(((RWLOCK*)data)->access);
+   ((RWLOCK*)data)->readers=0;
+-#elif defined(__APPLE__)
++#elif defined(__APPLE__) || defined(__FreeBSD__)
+   // If this fails on your machine, determine what value
+   // of sizeof(pthread_rwlock_t) is supposed to be on your
    // machine and mail it to: jeroen@fox-toolkit.com!!
-   //FXTRACE((150,"sizeof(pthread_rwlock_t)=%d\n",sizeof(pthread_rwlock_t)));
-   FXASSERT(sizeof(data)>=sizeof(pthread_rwlock_t));
--#ifdef __APPLE__
-+#if defined(__APPLE__) || defined(__FreeBSD__)
-   pthread_rwlock_init((pthread_rwlock_t*)data,NULL);
- #else
-   pthread_rwlockattr_t rwlockatt;
