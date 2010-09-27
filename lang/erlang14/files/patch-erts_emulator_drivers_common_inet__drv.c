7b250719489132632302d05f2a13136c5e65d0a6
olgeni <olgeni@FreeBSD.org>
2010-09-27 20:17:16 +0000

    Upgrade to version R14B, with a couple of fixes from the Erlang mailing lists.

--- lang/erlang14/files/patch-erts_emulator_drivers_common_inet__drv.c
+++ lang/erlang14/files/patch-erts_emulator_drivers_common_inet__drv.c
@@ -3,7 +3,7 @@ $FreeBSD$
 
 --- erts/emulator/drivers/common/inet_drv.c.orig
 +++ erts/emulator/drivers/common/inet_drv.c
-@@ -3490,17 +3490,9 @@
+@@ -3452,17 +3452,9 @@
      /* Check the size of SCTP AssocID -- currently both this driver and the
         Erlang part require 32 bit: */
      ASSERT(sizeof(sctp_assoc_t)==ASSOC_ID_LEN);
@@ -21,15 +21,14 @@ $FreeBSD$
 +    p_sctp_bindx = sctp_bindx;
 +    inet_init_sctp();
 +    add_driver_entry(&sctp_inet_driver_entry);
- #  endif
- #endif /* _OSE_ */
+ #endif
+ 
      /* remove the dummy inet driver */
-@@ -5305,12 +5297,15 @@
+@@ -5293,12 +5285,14 @@
  	    if (pmtud_enable)			cflags |= SPP_PMTUD_ENABLE;
  	    if (pmtud_disable)			cflags |= SPP_PMTUD_DISABLE;
  
 +#	    ifdef HAVE_STRUCT_SCTP_PADDRPARAMS_SPP_SACKDELAY
-+	    /* The followings are missing in FreeBSD 7.1 */
  	    sackdelay_enable =eflags& SCTP_FLAG_SACDELAY_ENABLE;
  	    sackdelay_disable=eflags& SCTP_FLAG_SACDELAY_DISABLE;
  	    if (sackdelay_enable && sackdelay_disable)
@@ -40,13 +39,11 @@ $FreeBSD$
  
  	    arg.pap.spp_flags  = cflags;
  #	    endif
-@@ -6211,13 +6206,15 @@
- 	    
+@@ -6200,12 +6194,14 @@
  	    if (ap.spp_flags & SPP_PMTUD_DISABLE)
  		{ i = LOAD_ATOM (spec, i, am_pmtud_disable);         n++; }
--	    
+ 	    
 +#	    ifdef HAVE_STRUCT_SCTP_PADDRPARAMS_SPP_SACKDELAY
-+	    /* SPP_SACKDELAY_* not in FreeBSD 7.1 */
  	    if (ap.spp_flags & SPP_SACKDELAY_ENABLE)
  		{ i = LOAD_ATOM (spec, i, am_sackdelay_enable);      n++; }
  	    
