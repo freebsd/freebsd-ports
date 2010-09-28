4ef9fbfee4da56883ff1444bab2014a42b639ec5
Erwin Lansing <erwin@FreeBSD.org>
2010-09-28 07:35:44 +0000

    We shouldn't trust vendor that their license filename is unique and will not cause collisions for different licenses as well as for _LICENSE_REPORT. The patch also makes it clear what license file belongs to what license in an installed package.
    PR:		148808
    Submitted by:	Anonymous <swell.k@gmail.com>
    Tested on:	pointyhat

--- Mk/bsd.licenses.mk
+++ Mk/bsd.licenses.mk
@@ -723,11 +723,11 @@ PLIST_FILES+=	${_LICENSE_DIR_REL}/${_LICENSE_CATALOG:T} \
 				${_LICENSE_DIR_REL}/${_LICENSE_REPORT:T}
 
 .if ${_LICENSE_COMB} == "single"
-PLIST_FILES+=	${_LICENSE_DIR_REL}/${_LICENSE_FILE:T}
+PLIST_FILES+=	${_LICENSE_DIR_REL}/${_LICENSE}
 .else
 .	for lic in ${_LICENSE}
 .		if defined(_LICENSE_FILE_${lic})
-PLIST_FILES+=	${_LICENSE_DIR_REL}/${_LICENSE_FILE_${lic}:T}
+PLIST_FILES+=	${_LICENSE_DIR_REL}/${lic}
 .		endif
 .	endfor
 .endif
@@ -737,10 +737,10 @@ install-license:
 	@${INSTALL_DATA} ${_LICENSE_CATALOG_TMP} ${_LICENSE_CATALOG}
 	@${INSTALL_DATA} ${_LICENSE_REPORT_TMP} ${_LICENSE_REPORT}
 .if ${_LICENSE_COMB} == "single"
-	@${INSTALL_DATA} ${_LICENSE_FILE} ${_LICENSE_DIR}
+	@${INSTALL_DATA} ${_LICENSE_FILE} ${_LICENSE_DIR}/${_LICENSE}
 .else
 .	for lic in ${_LICENSE}
-	@${INSTALL_DATA} ${_LICENSE_FILE_${lic}} ${_LICENSE_DIR}
+	@${INSTALL_DATA} ${_LICENSE_FILE_${lic}} ${_LICENSE_DIR}/${lic}
 .	endfor
 .endif
 # XXX @dirrmtry entry must be here (no way to do with PLIST_* vars)
