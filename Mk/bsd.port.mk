314d47ab383abda57c7b7661c156497e3de15da6
pav <pav@FreeBSD.org>
2010-09-29 20:51:55 +0000

    - Introduce a new virtual category enlightenment, for the popular desktop   environment
    PR:		ports/150492
    Requested by:	Grzegorz Blach <magik@roorback.net>

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -2944,7 +2944,7 @@ check-categories:
 
 VALID_CATEGORIES+= accessibility afterstep arabic archivers astro audio \
 	benchmarks biology cad chinese comms converters databases \
-	deskutils devel docs dns editors elisp emulators finance french ftp \
+	deskutils devel docs dns editors elisp emulators enlightenment finance french ftp \
 	games geography german gnome gnustep graphics hamradio haskell hebrew hungarian \
 	ipv6 irc japanese java kde kld korean lang linux lisp \
 	mail math mbone misc multimedia net net-im net-mgmt net-p2p news \
