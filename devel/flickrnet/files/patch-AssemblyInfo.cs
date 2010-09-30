d22c647ef2fcb63957ab40edac4268b18e0246e0
romain <romain@FreeBSD.org>
2010-09-30 18:20:48 +0000

    The Flickr.Net API is a .Net Library for accessing the Flickr API, written entirely in C#.
    WWW:	http://www.codeplex.com/FlickrNet

---
+++ devel/flickrnet/files/patch-AssemblyInfo.cs
@@ -0,0 +1,19 @@
+
+$FreeBSD$
+
+--- AssemblyInfo.cs.orig
++++ AssemblyInfo.cs
+@@ -61,10 +61,10 @@
+ [assembly: AssemblyKeyName("")]
+ 
+ #if !WindowsCE
+-[assembly: AssemblyKeyFile("..\\..\\FlickrNet.snk")]
++[assembly: AssemblyKeyFile("FlickrNet.snk")]
+ [assembly: AllowPartiallyTrustedCallers()]
+ [assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true)]
+ #endif
+ 
+ [assembly: CLSCompliantAttribute(true)]
+-[assembly: ComVisible(false)]
+\ No newline at end of file
++[assembly: ComVisible(false)]
