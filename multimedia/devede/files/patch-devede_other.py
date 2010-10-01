67e633725a8fc6ee38944418c6005183189db5c4
Lars Engels <lme@FreeBSD.org>
2010-10-01 16:12:24 +0000

    - Update to 3.16.9 - Remove no longer needed MASTER_SITE

--- multimedia/devede/files/patch-devede_other.py
+++ multimedia/devede/files/patch-devede_other.py
@@ -1,11 +1,11 @@
---- devede_other.py.orig	2010-04-21 06:12:32.000000000 -0300
-+++ devede_other.py	2010-04-21 06:14:28.000000000 -0300
+--- ./devede_other.py.orig	2010-10-01 17:35:11.558220218 +0200
++++ ./devede_other.py	2010-10-01 17:35:51.631522099 +0200
 @@ -233,7 +233,7 @@
  	
  	def convert_to_UTF8(self,infile_n,outfile_n,origin_format):
  		
--		command_line='iconv -f '+str(origin_format)+' -t UTF8 "'+str(infile_n)+'" -o "'+str(outfile_n)+'"'
-+		command_line='iconv -f '+str(origin_format)+' -t UTF-8 "'+str(infile_n)+'" > "'+str(outfile_n)+'"'
+-		command_line='iconv -f '+str(origin_format)+' -t UTF-8 "'+str(infile_n)+'" > "'+str(outfile_n)+'"'
++		command_line='iconv -f '+str(origin_format)+' -t UTF8 "'+str(infile_n)+'" > "'+str(outfile_n)+'"'
  		return self.launch_shell(command_line).wait()
  	
  	
