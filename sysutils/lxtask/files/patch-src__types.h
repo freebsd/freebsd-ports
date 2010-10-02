f1552c8c3cb05885866a9c45fbaa96cba62244b6
makc <makc@FreeBSD.org>
2010-10-02 11:19:30 +0000

    Update to 0.1.3 and unbreak Pass maintainership to submitter
    PR:		ports/150733
    Submitted by:	Davide Italiano <davide.italiano at gmail.com>

---
+++ sysutils/lxtask/files/patch-src__types.h
@@ -0,0 +1,9 @@
+--- ./src/types.h.orig	2010-04-12 13:54:32.000000000 +0400
++++ ./src/types.h	2010-09-25 23:50:53.953634766 +0400
+@@ -78,6 +78,4 @@
+ const gchar *custom_signal_0;
+ const gchar *custom_signal_1;
+ 
+-extern int PAGE_SIZE;
+-
+ #endif
