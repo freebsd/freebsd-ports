7e80cde3289d108a73c54656f4fde354dd2fb09e
Ade Lovett <ade@FreeBSD.org>
2010-10-05 19:57:56 +0000

    Round one migration of ports from automake{19,110} to automake111

---
+++ graphics/cal3d/files/patch-aclocal.m4
@@ -0,0 +1,12 @@
+--- aclocal.m4.orig	2006-06-29 11:04:20.000000000 -0500
++++ aclocal.m4	2010-09-28 19:09:08.000000000 -0500
+@@ -6392,3 +6392,3 @@
+ # generated from the m4 files accompanying Automake X.Y.
+-AC_DEFUN([AM_AUTOMAKE_VERSION], [am__api_version="1.9"])
++AC_DEFUN([AM_AUTOMAKE_VERSION], [am__api_version="1.11"])
+ 
+@@ -6399,3 +6399,3 @@
+ AC_DEFUN([AM_SET_CURRENT_AUTOMAKE_VERSION],
+-	 [AM_AUTOMAKE_VERSION([1.9.6])])
++	 [AM_AUTOMAKE_VERSION([1.11.1])])
+ 
--- x11-themes/kdmtheme/files/patch-admin__detect-autoconf.sh
+++ x11-themes/kdmtheme/files/patch-admin__detect-autoconf.sh
@@ -1,11 +1,9 @@
---- ./admin/detect-autoconf.sh.orig	2006-11-10 01:42:52.000000000 +0300
-+++ ./admin/detect-autoconf.sh	2007-12-17 21:15:07.000000000 +0300
-@@ -10,7 +10,7 @@
- # Please add higher versions first. The last version number is the minimum
- # needed to compile KDE. Do not forget to include the name/version #
+--- admin/detect-autoconf.sh.orig	2006-11-09 16:42:52.000000000 -0600
++++ admin/detect-autoconf.sh	2010-09-28 11:41:11.000000000 -0500
+@@ -12,4 +12,4 @@
  # separator if one is present, e.g. -1.2 where - is the separator.
 -KDE_AUTOCONF_VERS="-2.58 -2.57 257 -2.54 -2.53a -2.53 -2.52 -2.5x"
-+KDE_AUTOCONF_VERS="-2.67 -2.58 -2.57 257 -2.54 -2.53a -2.53 -2.52 -2.5x"
- KDE_AUTOMAKE_VERS="-1.7 17 -1.6"
+-KDE_AUTOMAKE_VERS="-1.7 17 -1.6"
++KDE_AUTOCONF_VERS="-2.67"
++KDE_AUTOMAKE_VERS="-1.11"
  
- # We don't use variable here for remembering the type ... strings. Local 
