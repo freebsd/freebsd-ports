cd1ccf8197217dfffff6f3705674a02b10df2814
glarkin <glarkin@FreeBSD.org>
2010-10-08 16:55:08 +0000

    - Fixed unknown variable InternalError when used with Trac 0.12 - Fixed formatting of wiki notification email when used with Trac 0.12
    PR:		ports/150084
    Submitted by:	Eygene Ryabinkin <rea-fbsd at codelabs dot ru>
    Approved by:	glarkin (maintainer)

---
+++ www/trac-wikinotification/files/patch-WikiNotification__notification.py
@@ -0,0 +1,72 @@
+--- ./WikiNotification/notification.py.orig	2008-03-14 05:02:03.000000000 -0400
++++ ./WikiNotification/notification.py	2010-10-08 11:41:19.000000000 -0400
+@@ -21,7 +21,7 @@
+ from trac.util.text import CRLF
+ from trac.wiki.model import WikiPage
+ from trac.versioncontrol.diff import unified_diff
+-from trac.notification import NotifyEmail
++from trac.notification import NotifyEmail, NotificationSystem
+ from trac.config import Option, BoolOption, ListOption, IntOption
+ 
+ from genshi.template.text import TextTemplate
+@@ -33,6 +33,7 @@
+ +++ %(name)s (version: %(version)s)
+ """
+ 
++
+ class WikiNotificationSystem(Component):
+     smtp_from = Option(
+         'wiki-notification', 'smtp_from', 'trac+wiki@localhost',
+@@ -131,7 +132,7 @@
+         if page.version > 0 and action == 'modified':
+             diff = diff_header % {'name': self.page.name,
+                                   'version': self.page.version,
+-                                  'oldversion': self.page.version -1
++                                  'oldversion': self.page.version -1,
+                                  }
+             oldpage = WikiPage(self.env, page.name, page.version - 1)
+             self.data["oldversion"]= oldpage.version
+@@ -183,8 +184,8 @@
+         public_cc = self.config.getbool('wiki-notification', 'use_public_cc')
+         headers = {}
+         headers['X-Mailer'] = 'Trac %s, by Edgewall Software' % __version__
+-        headers['X-Trac-Version'] =  __version__
+-        headers['X-Trac-Project'] =  projname
++        headers['X-Trac-Version'] = __version__
++        headers['X-Trac-Project'] = projname
+         headers['X-URL'] = self.config.get('project', 'url')
+         headers['Precedence'] = 'bulk'
+         headers['Auto-Submitted'] = 'auto-generated'
+@@ -284,18 +285,16 @@
+             del msg['Content-Transfer-Encoding']
+             msg.set_charset(self._charset)
+ 
+-        self.add_headers(msg, headers);
+-        self.add_headers(msg, mime_headers);
+-        self.env.log.info("Sending SMTP notification to %s:%d to %s"
+-                           % (self.smtp_server, self.smtp_port, recipients))
++        self.add_headers(msg, headers)
++        self.add_headers(msg, mime_headers)
++        self.env.log.info("Sending SMTP notification to %s"
++                           % str(recipients))
+         msgtext = msg.as_string()
+         # Ensure the message complies with RFC2822: use CRLF line endings
+         recrlf = re.compile("\r?\n")
+         msgtext = CRLF.join(recrlf.split(msgtext))
+-        try:
+-            self.server.sendmail(msg['From'], recipients, msgtext)
+-        except Exception, err:
+-            self.env.log.debug('Notification could not be sent: %r', err)
++        NotificationSystem(self.env).send_email(self.from_email, recipients,
++                                                msgtext)
+ 
+     def format_subject(self, action):
+         template = self.config.get('wiki-notification', 'subject_template')
+@@ -308,6 +307,6 @@
+         data = {
+             'page': self.page,
+             'prefix': prefix,
+-            'action': action
++            'action': action,
+         }
+         return template.generate(**data).render('text', encoding=None).strip()
