ee18ec8073e4b9338d34e229c53a792ec510134a
Alejandro Pulver <alepulver@FreeBSD.org>
2010-10-10 17:05:40 +0000

    - Fix compilation, and remove BROKEN.
    PR:		ports/148455
    Submitted by:	Dee-Nice <hostmaster@dee-nice.org>

--- games/egl/files/patch-renderer__rf_image.c
+++ games/egl/files/patch-renderer__rf_image.c
@@ -1,5 +1,6 @@
---- renderer/rf_image.c.orig	Wed Jan 10 18:55:48 2007
-+++ renderer/rf_image.c	Wed Jan 10 18:57:53 2007
+
+--- renderer/rf_image.c.orig	2010-07-08 22:47:22.720284451 +0200
++++ renderer/rf_image.c	2010-07-08 22:53:00.496664288 +0200
 @@ -44,7 +44,7 @@
  image_t			*r_lmTextures[R_MAX_LIGHTMAPS];
  static image_t	r_imageList[MAX_IMAGES];
@@ -9,3 +10,36 @@
  
  static byte		r_intensityTable[256];
  static byte		r_gammaTable[256];
+@@ -261,21 +261,6 @@
+     cinfo->src->bytes_in_buffer -= (size_t) num_bytes;
+ }
+ 
+-static void jpeg_mem_src (j_decompress_ptr cinfo, byte *mem, int len)
+-{
+-    cinfo->src = (struct jpeg_source_mgr *)
+-	(*cinfo->mem->alloc_small)((j_common_ptr) cinfo,
+-				   JPOOL_PERMANENT,
+-				   sizeof(struct jpeg_source_mgr));
+-    cinfo->src->init_source = jpg_noop;
+-    cinfo->src->fill_input_buffer = jpg_fill_input_buffer;
+-    cinfo->src->skip_input_data = jpg_skip_input_data;
+-    cinfo->src->resync_to_restart = jpeg_resync_to_restart;
+-    cinfo->src->term_source = jpg_noop;
+-    cinfo->src->bytes_in_buffer = len;
+-    cinfo->src->next_input_byte = mem;
+-}
+-
+ /*
+ =============
+ R_LoadJPG
+@@ -610,7 +595,7 @@
+ 		png_set_filler (png_ptr, 0xFF, PNG_FILLER_AFTER);
+ 
+ 	if (info_ptr->color_type == PNG_COLOR_TYPE_GRAY && info_ptr->bit_depth < 8)
+-		png_set_gray_1_2_4_to_8 (png_ptr);
++		png_set_expand_gray_1_2_4_to_8 (png_ptr);
+ 
+ 	if (png_get_valid (png_ptr, info_ptr, PNG_INFO_tRNS))
+ 		png_set_tRNS_to_alpha (png_ptr);
+
+
