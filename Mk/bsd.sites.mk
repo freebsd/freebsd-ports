00fe2b3894a6e8aef226b9dc4bde05107bc5b7a6
ehaupt <ehaupt@FreeBSD.org>
2010-10-21 15:24:24 +0000

    Remove cpan.pop-mg.com.br from MASTER_SITE_PERL_CPAN host does not resolve.

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -912,7 +912,6 @@ MASTER_SITE_PERL_CPAN+=	\
 	ftp://csociety-ftp.ecn.purdue.edu/pub/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.isu.net.sa/pub/CPAN/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.cs.colorado.edu/pub/perl/CPAN/modules/by-module/%SUBDIR%/ \
-	ftp://cpan.pop-mg.com.br/pub/CPAN/modules/by-module/%SUBDIR%/ \
 	http://at.cpan.org/modules/by-module/%SUBDIR%/ \
 	ftp://ftp.auckland.ac.nz/pub/perl/CPAN/modules/by-module/%SUBDIR%/ \
 	http://backpan.cpan.org/modules/by-module/%SUBDIR%/ \
