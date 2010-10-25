fc438b947f6f5955e0cd56f894d2b3b15d29e9e0
Stanislav Sedov <stas@FreeBSD.org>
2010-10-25 01:55:19 +0000

    - Properly escape filenames in cuetag.sh
    PR:		ports/151677
    Submitted by:	Nicole Reid <root@cooltrainer.org>

--- audio/cuetools/files/patch-extras_cuetag.sh
+++ audio/cuetools/files/patch-extras_cuetag.sh
@@ -1,5 +1,5 @@
---- extras/cuetag.sh.orig	2008-11-28 00:41:26.000000000 +0300
-+++ extras/cuetag.sh	2008-11-28 00:41:32.000000000 +0300
+--- extras/cuetag.sh.orig	2006-02-14 19:10:02.000000000 -0500
++++ extras/cuetag.sh	2010-10-24 03:20:01.349578391 -0400
 @@ -18,7 +18,7 @@
  {
  	# FLAC tagging
@@ -9,3 +9,47 @@
  
  	# Ogg Vorbis tagging
  	# -w overwrites existing comments
+@@ -63,7 +63,7 @@
+ 	(for field in $fields; do
+ 		value=""
+ 		for conv in `eval echo \\$$field`; do
+-			value=`$CUEPRINT -n $1 -t "$conv\n" $cue_file`
++			value=`$CUEPRINT -n $1 -t "$conv\n" "$cue_file"`
+ 
+ 			if [ -n "$value" ]; then
+ 				echo "$field=$value"
+@@ -96,7 +96,7 @@
+ 	for field in $fields; do
+ 		value=""
+ 		for conv in `eval echo \\$$field`; do
+-			value=`$CUEPRINT -n $1 -t "$conv\n" $cue_file`
++			value=`$CUEPRINT -n $1 -t "$conv\n" "$cue_file"`
+ 
+ 			if [ -n "$value" ]; then
+ 				break
+@@ -141,14 +141,14 @@
+ 	cue_file=$1
+ 	shift
+ 
+-	ntrack=`cueprint -d '%N' $cue_file`
++	ntrack=`cueprint -d '%N' "$cue_file"`
+ 	trackno=1
+ 
+ 	if [ $# -ne $ntrack ]; then
+ 		echo "warning: number of files does not match number of tracks"
+ 	fi
+ 
+-	for file in $@; do
++	for file in "$@"; do
+ 		case $file in
+ 		*.[Ff][Ll][Aa][Cc])
+ 			vorbis $trackno "$file"
+@@ -160,7 +160,7 @@
+ 			id3 $trackno "$file"
+ 			;;
+ 		*)
+-			echo "$file: uknown file type"
++			echo "$file: unknown file type"
+ 			;;
+ 		esac
+ 		trackno=$(($trackno + 1))
