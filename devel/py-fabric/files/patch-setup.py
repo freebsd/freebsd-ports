442431572dcd36986bff9c9461579bbf834c1aaa
yzlin <yzlin@FreeBSD.org>
2010-10-27 15:50:32 +0000

    - Update to 0.9.2
    PR:		ports/151445
    Submitted by:	lwhsu

--- devel/py-fabric/files/patch-setup.py
+++ devel/py-fabric/files/patch-setup.py
@@ -1,13 +1,10 @@
-
-$FreeBSD$
-
---- setup.py.orig
-+++ setup.py
-@@ -16,7 +16,6 @@
-     author_email='jeff@bitprophet.org',
-     url='http://fabfile.org',
+--- setup.py.orig	2010-10-14 13:37:04.963787042 +0800
++++ setup.py	2010-10-14 13:37:11.377290180 +0800
+@@ -31,7 +31,6 @@
      packages=find_packages(),
--    install_requires=['pycrypto >=1.9'],
+     test_suite='nose.collector',
+     tests_require=['nose', 'fudge'],
+-    install_requires=['pycrypto <2.1', 'paramiko >=1.7.6'],
      entry_points={
          'console_scripts': [
              'fab = fabric.main:main',
