1c5b6af38ca24f79ea33530c407b9f4258ab92c5
Alex Dupre <ale@FreeBSD.org>
2010-11-10 16:55:08 +0000

    Update to 5.5.7-rc release. Fix crash caused by MAX_DEFAULT_DIRS too small [1]
    PR:		ports/151927
    Submitted by:	Bartosz Fabianowski <freebsd@chillt.de>

--- databases/mysql55-server/files/patch-mysys_default.c
+++ databases/mysql55-server/files/patch-mysys_default.c
@@ -1,6 +1,15 @@
---- mysys/default.c.orig	2009-03-31 16:38:43.000000000 +0200
-+++ mysys/default.c	2009-04-21 08:22:14.000000000 +0200
-@@ -679,7 +679,7 @@
+--- mysys/default.c.orig	2010-11-02 23:01:13.000000000 +0100
++++ mysys/default.c	2010-11-10 16:39:51.000000000 +0100
+@@ -70,7 +70,7 @@
+ 
+ /* Which directories are searched for options (and in which order) */
+ 
+-#define MAX_DEFAULT_DIRS 6
++#define MAX_DEFAULT_DIRS 7
+ #define DEFAULT_DIRS_SIZE (MAX_DEFAULT_DIRS + 1)  /* Terminate with NULL */
+ static const char **default_directories = NULL;
+ 
+@@ -711,7 +711,7 @@
    {
      MY_STAT stat_info;
      if (!my_stat(name,&stat_info,MYF(0)))
@@ -9,7 +18,7 @@
      /*
        Ignore world-writable regular files.
        This is mainly done to protect us to not read a file created by
-@@ -1128,7 +1128,10 @@
+@@ -1153,7 +1153,10 @@
  
  #if defined(DEFAULT_SYSCONFDIR)
    if (DEFAULT_SYSCONFDIR[0])
