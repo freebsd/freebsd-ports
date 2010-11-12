a08b5e3eea33fce750616a5102af909a29dc7a5e
kevlo <kevlo@FreeBSD.org>
2010-11-12 06:45:51 +0000

    Fix mtree error

--- astro/gmapcatcher/files/patch-setup.py
+++ astro/gmapcatcher/files/patch-setup.py
@@ -1,15 +1,15 @@
 --- setup.py.orig	2010-08-23 02:36:19.000000000 +0800
-+++ setup.py	2010-09-08 11:00:27.000000000 +0800
-@@ -13,15 +13,14 @@
++++ setup.py	2010-11-12 14:34:24.000000000 +0800
+@@ -13,15 +13,13 @@
          description = 'Offline Map Viewer',
          version = VERSION,
          url = WEB_ADDRESS,
 -        data_files = [('share/doc/mapcatcher', ['README', 'changelog']),
-+        data_files = [('share/doc/gmapcatcher', ['README', 'changelog']),
-                     ('share/applications', ['gmapcatcher.desktop']),
+-                    ('share/applications', ['gmapcatcher.desktop']),
 -                    ('share/man/man1', 
 -                        ['man/mapcatcher.1.gz', 'man/mapdownloader.1.gz']),
 -                    ('share/pixmaps', ['images/mapcatcher.png']),
++        data_files = [('share/applications', ['gmapcatcher.desktop']),
 +                    ('man/man1', 
 +                        ['man/mapcatcher.1', 'man/mapdownloader.1']),
 +                    ('share/pixmaps', ['images/map.png']),
