af4473d7da8dc2f879e1aae7ffaf340ba855aeb8
Max Brazhnikov <makc@FreeBSD.org>
2010-11-12 12:25:34 +0000

    Project has been renamed to luminance Update to 2.0.1.1 Pass maintainership to submitter
    PR:		based on ports/151856
    Submitted by:	Joao Rocha Braga Filho <goffredo at gmail.com>

--- graphics/luminance/files/patch-src__HdrCreation__HdrCreationManager.cpp
+++ graphics/luminance/files/patch-src__HdrCreation__HdrCreationManager.cpp
@@ -1,10 +1,10 @@
---- src/HdrCreation/HdrCreationManager.cpp.prig	2008-04-29 01:18:04.000000000 +0900
-+++ src/HdrCreation/HdrCreationManager.cpp	2008-07-02 03:26:00.000000000 +0900
-@@ -28,6 +28,7 @@
- #include "../Exif/exif_operations.h"
- #include "mtb_alignment.h"
- #include "../Threads/hdrInputLoader.h"
+--- ./src/HdrCreation/HdrCreationManager.cpp.orig	2010-05-20 21:46:40.000000000 +0400
++++ ./src/HdrCreation/HdrCreationManager.cpp	2010-11-12 14:20:53.448299319 +0300
+@@ -23,6 +23,7 @@
+ 
+ #include <QApplication>
+ #include <QFileInfo>
 +#include "../arch/freebsd/math.h"
  
- HdrCreationManager::HdrCreationManager() {
- 	qtpfsgui_options=QtpfsguiOptions::getInstance();
+ #include "Fileformat/pfstiff.h"
+ #include "Exif/ExifOperations.h"
