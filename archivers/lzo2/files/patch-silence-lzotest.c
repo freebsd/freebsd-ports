5a1576904b5b3bc9e0040b611c0fbbfd6c2e94a8
Matthias Andree <mandree@FreeBSD.org>
2010-11-13 20:59:16 +0000

    Update to 2.04.
    Submitted by: Martin Matuska <mm@freebsd.org>
    PR:           ports/152183

--- archivers/lzo2/files/patch-silence-lzotest.c
+++ archivers/lzo2/files/patch-silence-lzotest.c
@@ -1,27 +1,27 @@
-This test reduces the lzotest verbosity so that it doesn't print anything
-on success.
-
---- lzotest/lzotest.c.orig	2008-06-01 10:01:36.000000000 +0200
-+++ lzotest/lzotest.c	2008-06-01 10:03:10.000000000 +0200
-@@ -226,7 +226,7 @@
- 
- struct corpus_entry_t;
+--- lzotest/lzotest.c.orig	2010-10-31 18:21:28.000000000 +0100
++++ lzotest/lzotest.c	2010-11-13 00:37:17.279807378 +0100
+@@ -233,7 +233,7 @@
+ // command line options
+ **************************************************************************/
  
 -int opt_verbose = 2;
 +int opt_verbose = 1;
  
  long opt_c_loops = 0;
  long opt_d_loops = 0;
-@@ -1910,10 +1910,12 @@
- #elif defined(LZOTEST_USE_DYNLOAD)
- #  include "dynload/init.ch"
- #else
+@@ -1928,6 +1928,7 @@
+         if ((*s == '/' || *s == '\\') && s[1])
+             progname = s + 1;
+ 
 +#if 0
-     printf("\nLZO real-time data compression library (v%s, %s).\n",
+ #if defined(__LZO_PROFESSIONAL__)
+     printf("\nLZO Professional real-time data compression library (v%s, %s).\n",
             lzo_version_string(), lzo_version_date());
-     printf("Copyright (C) 1996-2008 Markus Franz Xaver Johannes Oberhumer\nAll Rights Reserved.\n\n");
-+#endif
+@@ -1937,6 +1938,7 @@
+            lzo_version_string(), lzo_version_date());
+     printf("Copyright (C) 1996-2010 Markus Franz Xaver Johannes Oberhumer\nAll Rights Reserved.\n\n");
  #endif
++#endif
  
  
  /*
