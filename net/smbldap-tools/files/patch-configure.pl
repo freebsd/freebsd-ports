ca124aa0ddb2b58a448aef456b61e9f8e3635596
mm <mm@FreeBSD.org>
2010-11-15 15:16:28 +0000

    - Update to 0.9.6

--- net/smbldap-tools/files/patch-configure.pl
+++ net/smbldap-tools/files/patch-configure.pl
@@ -1,6 +1,6 @@
---- configure.pl.orgi	Wed Jul 12 18:36:29 2006
-+++ configure.pl	Wed Jul 12 18:40:30 2006
-@@ -49,7 +49,7 @@
+--- configure.pl.orig	2010-11-15 15:45:49.000000000 +0100
++++ configure.pl	2010-11-15 16:02:51.745592626 +0100
+@@ -48,7 +48,7 @@
  print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
  
  # we first check if Samba is up and running
@@ -9,7 +9,7 @@
  chomp($test_smb);
  die "\nSamba need to be started first !\n" if ($test_smb eq "" || not defined $test_smb);
  
-@@ -304,7 +304,7 @@
+@@ -303,7 +303,7 @@
  
  my $default_computer_gidnumber=read_entry(". default computer gidNumber","","515",0);
  
@@ -18,14 +18,14 @@
  
  my $skeletonDir=read_entry(". default skeleton directory","","/etc/skel",0);
  
-@@ -528,12 +528,12 @@
- # Allows not to use smbpasswd (if with_smbpasswd == 0 in smbldap_conf.pm) but
+@@ -527,12 +527,12 @@
+ # Allows not to use smbpasswd (if with_smbpasswd="0" in smbldap.conf) but
  # prefer Crypt::SmbHash library
  with_smbpasswd=\"0\"
 -smbpasswd=\"/usr/bin/smbpasswd\"
 +smbpasswd=\"%%LOCALBASE%%/bin/smbpasswd\"
  
- # Allows not to use slappasswd (if with_slappasswd == 0 in smbldap_conf.pm)
+ # Allows not to use slappasswd (if with_slappasswd="0" in smbldap.conf)
  # but prefer Crypt:: libraries
  with_slappasswd=\"0\"
 -slappasswd=\"/usr/sbin/slappasswd\"
