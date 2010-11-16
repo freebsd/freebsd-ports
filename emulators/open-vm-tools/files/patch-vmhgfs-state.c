e189332abbd65b46a0491a02885dafb73c11c738
Martin Blapp <mbr@FreeBSD.org>
2010-11-16 07:42:44 +0000

    Fix build errors (redundant declarations)
    PR:			152283
    Submitted by:		fumifumi@abacustech.jp

--- emulators/open-vm-tools/files/patch-vmhgfs-state.c
+++ emulators/open-vm-tools/files/patch-vmhgfs-state.c
@@ -1,5 +1,5 @@
---- modules/freebsd/vmhgfs/state.c.orig	2009-02-28 23:06:10.000000000 +0100
-+++ modules/freebsd/vmhgfs/state.c	2009-02-28 23:08:59.000000000 +0100
+--- modules/freebsd/vmhgfs/state.c.orig	2010-10-20 05:19:54.000000000 +0900
++++ modules/freebsd/vmhgfs/state.c	2010-11-16 13:27:57.000000000 +0900
 @@ -770,6 +770,12 @@
        goto destroyVnode;
     }
@@ -13,3 +13,19 @@
     /*
      * Now we'll initialize the vnode.  We need to set the file type, vnode
      * operations, flags, filesystem pointer, reference count, and device.
+@@ -1277,6 +1283,7 @@
+  *----------------------------------------------------------------------------
+  */
+ 
++#if 0 /* never used */
+ void
+ HgfsMarkFileMmapped(struct vnode *vp,    // vnode which state is being changed
+                     Bool mmapped)        // New mapping state
+@@ -1286,6 +1293,7 @@
+    fp = HGFS_VP_TO_FP(vp);
+    fp->mmapped = mmapped;
+ }
++#endif
+ 
+ /* Adding/finding/removing file state from hash table */
+ 
