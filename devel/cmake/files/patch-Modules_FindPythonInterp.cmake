5861032c9e60a113628088b0ebcdebe0afabcaec
Alberto Villa <avilla@FreeBSD.org>
2010-11-17 15:44:41 +0000

    - Update to 2.8.3.
    Approved by:	tabthorpe (mentor)

--- devel/cmake/files/patch-Modules_FindPythonInterp.cmake
+++ devel/cmake/files/patch-Modules_FindPythonInterp.cmake
@@ -1,11 +1,11 @@
---- ./Modules/FindPythonInterp.cmake.orig	2009-02-21 23:36:50.000000000 +0300
-+++ ./Modules/FindPythonInterp.cmake	2009-03-20 13:20:18.000000000 +0300
-@@ -7,7 +7,7 @@
- #
+--- Modules/FindPythonInterp.cmake.orig	2010-11-03 20:58:27.000000000 +0100
++++ Modules/FindPythonInterp.cmake	2010-11-17 10:51:18.700039887 +0100
+@@ -20,7 +20,7 @@
+ #  License text for the above reference.)
  
  FIND_PROGRAM(PYTHON_EXECUTABLE
--  NAMES python2.6 python2.5 python2.4 python2.3 python2.2 python2.1 python2.0 python1.6 python1.5 python
-+  NAMES python python2.6 python2.5 python2.4 python2.3 python2.2 python2.1 python2.0 python1.6 python1.5
+-  NAMES python2.7 python2.6 python2.5 python2.4 python2.3 python2.2 python2.1 python2.0 python1.6 python1.5 python
++  NAMES python python2.7 python2.6 python2.5 python2.4 python2.3 python2.2 python2.1 python2.0 python1.6 python1.5
    PATHS
+   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.7\\InstallPath]
    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.6\\InstallPath]
-   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.5\\InstallPath]
