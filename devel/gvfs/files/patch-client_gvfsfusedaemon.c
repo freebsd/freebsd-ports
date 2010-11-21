7b2947d1f809b9a68c1584f89419a67434316460
Koop Mast <kwm@FreeBSD.org>
2010-11-21 17:25:52 +0000

    Fix fuse support build failure.
    PR:		ports/152442
    Submitted by:	Oleg Ginzburg <olevole@olevole.ru>

--- devel/gvfs/files/patch-client_gvfsfusedaemon.c
+++ devel/gvfs/files/patch-client_gvfsfusedaemon.c
@@ -1,6 +1,6 @@
---- client/gvfsfusedaemon.c.orig	2007-11-06 18:06:41.000000000 -0500
-+++ client/gvfsfusedaemon.c	2007-11-06 18:07:30.000000000 -0500
-@@ -28,7 +28,9 @@
+--- client/gvfsfusedaemon.c.orig	2010-11-12 16:42:49.000000000 +0100
++++ client/gvfsfusedaemon.c	2010-11-21 18:23:36.000000000 +0100
+@@ -26,7 +26,9 @@
  #include <sys/types.h>
  #include <sys/socket.h>
  #include <sys/un.h>
@@ -10,3 +10,13 @@
  #include <sys/time.h>
  #include <unistd.h>
  #include <stdlib.h>
+@@ -2340,7 +2342,9 @@
+   subthread = g_thread_create ((GThreadFunc) subthread_main, NULL, FALSE, NULL);
+ 
+   /* Indicate O_TRUNC support for open() */
++#ifdef FUSE_CAP_ATOMIC_O_TRUNC
+   conn->want |= FUSE_CAP_ATOMIC_O_TRUNC;
++#endif
+ 
+   return NULL;
+ }
