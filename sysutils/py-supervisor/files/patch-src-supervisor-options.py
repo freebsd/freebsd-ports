048d322f5c01d03d14a21a12987867172a14dbd1
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2010-11-22 18:54:49 +0000

    - Update to 3.0a9
    PR:		ports/152196
    Submitted by:	Ildar Hizbulin <hizel@vyborg.ru> (maintainer)

--- sysutils/py-supervisor/files/patch-src-supervisor-options.py
+++ sysutils/py-supervisor/files/patch-src-supervisor-options.py
@@ -1,11 +1,14 @@
---- src/supervisor/options.py.orig	2010-02-11 13:25:24.000000000 +0300
-+++ src/supervisor/options.py	2010-02-11 13:25:55.000000000 +0300
-@@ -105,7 +105,7 @@
+--- src/supervisor/options.py.orig	2010-08-14 02:34:50.000000000 +0800
++++ src/supervisor/options.py	2010-11-23 02:33:20.000000000 +0800
+@@ -105,10 +105,7 @@
      def default_configfile(self):
          """Return the name of the found config file or raise. """
-         paths = ['supervisord.conf', 'etc/supervisord.conf',
+         here = os.path.dirname(os.path.dirname(sys.argv[0]))
+-        paths = [os.path.join(here, 'etc', 'supervisord.conf'),
+-                 os.path.join(here, 'supervisord.conf'),
+-                 'supervisord.conf', 'etc/supervisord.conf',
 -                 '/etc/supervisord.conf']
-+                 '/etc/supervisord.conf', '%%PREFIX%%/etc/supervisord.conf']
++        paths = [ '%%PREFIX%%/etc/supervisord.conf' ]
          config = None
          for path in paths:
              if os.path.exists(path):
