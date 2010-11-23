2ddc5f4cd8a1c73b3cb0116aac906d404a9bdeb7
Max Brazhnikov <makc@FreeBSD.org>
2010-11-23 21:08:39 +0000

    Fix search for QT_PLUGIN_PATH when it is empty. This should fix some ports after latest cmake update.

--- devel/cmake/files/patch-Modules_FindQt4.cmake
+++ devel/cmake/files/patch-Modules_FindQt4.cmake
@@ -1,6 +1,6 @@
---- ./Modules/FindQt4.cmake.orig	2010-03-16 23:29:30.000000000 +0300
-+++ ./Modules/FindQt4.cmake	2010-03-26 22:48:58.291402760 +0300
-@@ -392,7 +392,7 @@
+--- ./Modules/FindQt4.cmake.orig	2010-11-03 22:58:27.000000000 +0300
++++ ./Modules/FindQt4.cmake	2010-11-23 18:09:33.486039056 +0300
+@@ -461,7 +461,7 @@
  # check for qmake
  # Debian uses qmake-qt4
  # macports' Qt uses qmake-mac
@@ -9,3 +9,18 @@
    "[HKEY_CURRENT_USER\\Software\\Trolltech\\Qt3Versions\\4.0.0;InstallDir]/bin"
    "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\4.0.0;InstallDir]/bin"
    "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\${qt_install_version};InstallDir]/bin"
+@@ -705,13 +705,7 @@
+   # ask qmake for the plugins directory
+   IF (QT_LIBRARY_DIR AND NOT QT_PLUGINS_DIR  OR  QT_QMAKE_CHANGED)
+     _qt4_query_qmake(QT_INSTALL_PLUGINS qt_plugins_dir)
+-    SET(QT_PLUGINS_DIR NOTFOUND)
+-    foreach(qt_cross_path ${CMAKE_FIND_ROOT_PATH})
+-      set(qt_cross_paths ${qt_cross_paths} "${qt_cross_path}/plugins")
+-    endforeach(qt_cross_path)
+-    FIND_PATH(QT_PLUGINS_DIR NAMES accessible imageformats sqldrivers codecs designer
+-      HINTS ${qt_cross_paths} ${qt_plugins_dir}
+-      DOC "The location of the Qt plugins")
++    SET(QT_PLUGINS_DIR ${qt_plugins_dir} CACHE PATH "The location of the Qt plugins" FORCE)
+   ENDIF (QT_LIBRARY_DIR AND NOT QT_PLUGINS_DIR  OR  QT_QMAKE_CHANGED)
+ 
+   # ask qmake for the translations directory
