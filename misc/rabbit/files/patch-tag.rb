eb53541ff37c551e19c3900cc0300899f366b844
TAKATSU Tomonari <tota@FreeBSD.org>
2010-11-24 15:27:45 +0000

    - Update to 0.9.1   * Rename rabbitter to rabbiter - Add missing dependencies for rabbirc and rabbiter - Respect WITHOUT_NLS knob - Fix pkg-plist not to mix %%PORTDOCS%% with %%EXAMPLESDIR%%

--- misc/rabbit/files/patch-tag.rb
+++
@@ -1,8 +0,0 @@
---- lib/rabbit/element/tag.rb.orig	2010-07-31 20:33:07.000000000 +0900
-+++ lib/rabbit/element/tag.rb	2010-08-04 23:15:10.000000000 +0900
-@@ -1,4 +1,5 @@
- require 'rabbit/element/base'
-+require 'rabbit/element/text-container-element'
- 
- module Rabbit
-   module Element
