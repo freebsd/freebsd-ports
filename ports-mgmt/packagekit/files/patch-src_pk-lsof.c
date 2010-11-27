4c0bd8fa414093663125a360f90cf62566e3863e
Joe Marcus Clarke <marcus@FreeBSD.org>
2010-11-27 05:17:31 +0000

    Update to 0.6.10.

--- ports-mgmt/packagekit/files/patch-src_pk-lsof.c
+++ ports-mgmt/packagekit/files/patch-src_pk-lsof.c
@@ -1,3 +1,14 @@
+--- src/pk-lsof.c.orig	2010-10-21 12:50:10.000000000 -0400
++++ src/pk-lsof.c	2010-11-27 00:13:21.000000000 -0500
+@@ -154,7 +154,7 @@ pk_lsof_refresh (PkLsof *lsof)
+ 	g_return_val_if_fail (PK_IS_LSOF (lsof), FALSE);
+ 
+ 	/* try to find lsof */
+-	lsof_name = "/usr/sbin/lsof";
++	lsof_name = "%%LOCALBASE%%/sbin/lsof";
+ 	ret = g_file_test (lsof_name, G_FILE_TEST_EXISTS);
+ 	if (!ret) {
+ 		lsof_name = "/usr/bin/lsof";
 --- src/pk-lsof.c.orig	2010-01-09 15:57:54.000000000 -0500
 +++ src/pk-lsof.c	2010-01-09 15:58:15.000000000 -0500
 @@ -153,7 +153,7 @@ pk_lsof_refresh (PkLsof *lsof)
