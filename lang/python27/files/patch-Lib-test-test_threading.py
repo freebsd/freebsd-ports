ba679be64348d659cee83bbcd40f8bd5a0997245
wen <wen@FreeBSD.org>
2010-11-29 06:29:13 +0000

    - Update to 2.7.1

---
+++ lang/python27/files/patch-Lib-test-test_threading.py
@@ -0,0 +1,11 @@
+--- Lib/test/test_threading.py.orig	2010-11-29 08:22:00.000000000 +0800
++++ Lib/test/test_threading.py	2010-11-29 08:25:21.000000000 +0800
+@@ -476,7 +476,7 @@
+             return
+         # Skip platforms with known problems forking from a worker thread.
+         # See http://bugs.python.org/issue3863.
+-        if sys.platform in ('freebsd4', 'freebsd5', 'freebsd6', 'netbsd5',
++        if sys.platform in ('freebsd6', 'freebsd7', 'freebsd8', 'freebsd9','netbsd5',
+                            'os2emx'):
+             print >>sys.stderr, ('Skipping test_3_join_in_forked_from_thread'
+                                  ' due to known OS bugs on'), sys.platform
