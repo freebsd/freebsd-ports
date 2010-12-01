c3a1ed3529b996557b37438f433509614d158cb4
kwm <kwm@FreeBSD.org>
2010-12-01 23:07:20 +0000

    Fix seg fault on amd64.
    PR:		ports/150705
    Submitted by:	Fabian Keil <fk@fabiankeil.de>
    Reported by:	Richard Frewin <Richard.Frewin@astro.cf.ac.uk>
    Feature safe:	yes

---
+++ audio/grip/files/patch-src_tray.c
@@ -0,0 +1,10 @@
+--- src/tray.c.orig	2005-01-29 19:34:17.000000000 +0100
++++ src/tray.c	2010-12-01 22:20:00.000000000 +0100
+@@ -20,6 +20,7 @@
+  * USA
+  */
+ 
++#include "common.h"
+ #include "tray.h"
+ #include "../pixmaps/rip1.xpm"
+ #include "../pixmaps/menuplay.xpm"
