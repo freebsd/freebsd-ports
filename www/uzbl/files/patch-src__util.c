bd82c698df320f33be427687e2b7dacf6c25a302
Pav Lucistnik <pav@FreeBSD.org>
2010-12-01 12:48:45 +0000

    - Update to 2010.11.25
    PR:		ports/152728
    Submitted by:	Klaus Aehlig <aehlig@linta.de> (maintainer)

---
+++ www/uzbl/files/patch-src__util.c
@@ -0,0 +1,8 @@
+--- src/util.c.orig	2010-11-30 23:22:23.000000000 +0100
++++ src/util.c	2010-11-30 23:23:09.000000000 +0100
+@@ -1,5 +1,3 @@
+-#define _POSIX_SOURCE
+-
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <string.h>
