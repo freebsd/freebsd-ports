e2c8af6ff6c98ac9b496e207b0b28c411dec374b
Beech Rintoul <beech@FreeBSD.org>
2010-12-02 09:18:01 +0000

    Update mplayer and mencoder to the latest snapshots.
    PR:		ports/152714
    Submitted by:	Thomas Zander (thomas.e.zander@googlemail.com) (maintainer)
    Approved by:	itetcu (mentor) (implicit)

--- multimedia/mplayer/files/patch-libavutil-x86_cpu.h
+++ multimedia/mplayer/files/patch-libavutil-x86_cpu.h
@@ -1,6 +1,6 @@
---- libavutil/x86_cpu.h~	2009-03-19 09:49:47.000000000 -0500
-+++ libavutil/x86_cpu.h	2009-07-24 14:20:58.308327513 -0500
-@@ -69,7 +69,7 @@
+--- ffmpeg/libavutil/x86_cpu.h.orig	2010-10-06 03:26:13.000000000 +0200
++++ ffmpeg/libavutil/x86_cpu.h	2010-11-04 21:01:40.581647958 +0100
+@@ -71,7 +71,7 @@
  #define HAVE_7REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE && HAVE_EBP_AVAILABLE))
  #define HAVE_6REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE || HAVE_EBP_AVAILABLE))
  
