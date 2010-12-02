9f5d9c1e9e1a40dc499a7e7aa6fe90aef7a5da32
Max Brazhnikov <makc@FreeBSD.org>
2010-12-02 19:56:14 +0000

    Update to 2.0.1 for Qt-4.7 Pass maintainership to kde@ This update also fixes ports/144226
    PR:		ports/152752
    Approved by:	kmoore (former maintainer)

--- devel/qtcreator/files/patch-src-plugins-qt4projectmanager-makestep.cpp
+++ devel/qtcreator/files/patch-src-plugins-qt4projectmanager-makestep.cpp
@@ -1,13 +1,13 @@
---- src/plugins/qt4projectmanager/makestep.cpp.orig	2010-01-15 12:36:53.000000000 -0500
-+++ src/plugins/qt4projectmanager/makestep.cpp	2010-03-22 11:12:02.280703600 -0400
-@@ -106,8 +106,10 @@
-     if (toolchain)
-         type = toolchain->type();
-     if (type != ProjectExplorer::ToolChain::MSVC && type != ProjectExplorer::ToolChain::WINCE) {
+--- ./src/plugins/qt4projectmanager/makestep.cpp.orig	2010-08-24 11:46:09.000000000 +0400
++++ ./src/plugins/qt4projectmanager/makestep.cpp	2010-11-29 00:21:08.727128457 +0300
+@@ -163,8 +163,10 @@
+     if (toolchain) {
+         if (toolchain->type() != ProjectExplorer::ToolChain::MSVC &&
+             toolchain->type() != ProjectExplorer::ToolChain::WINCE) {
 +#ifndef Q_OS_FREEBSD
-         if (value(name, "makeCmd").toString().isEmpty())
-             args << "-w";
+             if (m_makeCmd.isEmpty())
+                 args << "-w";
 +#endif
+         }
      }
  
-     setEnabled(name, true);
