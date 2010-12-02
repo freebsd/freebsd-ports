3be8f467589fbc1061716d18e35c08abcca706af
Max Brazhnikov <makc@FreeBSD.org>
2010-12-02 08:02:18 +0000

    Fix build with upcoming Qt-4.7
    Approved by:	maintainer

---
+++ net-im/vacuum-im/files/patch-src__plugins__dataforms__dataforms.cpp
@@ -0,0 +1,14 @@
+--- ./src/plugins/dataforms/dataforms.cpp.orig	2010-02-04 23:15:54.000000000 +0300
++++ ./src/plugins/dataforms/dataforms.cpp	2010-12-02 00:40:53.396505897 +0300
+@@ -1087,9 +1087,11 @@
+     reply->ignoreSslErrors();
+ }
+ 
++#if (QT_VERSION <= QT_VERSION_CHECK(4, 7, 0))
+ uint qHash(const QUrl &key)
+ {
+   return qHash(key.toString());
+ }
++#endif
+ 
+ Q_EXPORT_PLUGIN2(plg_dataforms, DataForms);
