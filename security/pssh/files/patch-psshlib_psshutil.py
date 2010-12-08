3328e736fae20a0419c19803b767cec2b284363a
Brooks Davis <brooks@FreeBSD.org>
2010-12-08 23:46:28 +0000

    Move to the new distribution site on googlecode and upgrade to 2.1.1.

--- security/pssh/files/patch-psshlib_psshutil.py
+++ security/pssh/files/patch-psshlib_psshutil.py
@@ -3,15 +3,15 @@ $FreeBSD$
 
 --- psshlib/psshutil.py.orig
 +++ psshlib/psshutil.py
-@@ -7,7 +7,10 @@
-     three arrays: hosts, ports, and users.  These can be used directly
-     for all ssh-based commands (e.g., ssh, scp, rsync -e ssh, etc.)
-     """
--    f = open(pathname)
-+    if pathname == "-":
-+        f = open("/dev/stdin")
-+    else:
-+        f = open(pathname)
-     lines = f.readlines()
-     lines = map(lambda x: x.strip(), lines)
-     addrs = []
+@@ -15,7 +15,10 @@
+     if not pathnames:
+         return lines
+     for pathname in pathnames:
+-        f = open(pathname)
++        if pathname == "-":
++            f = open("/dev/stdin")
++        else:
++            f = open(pathname)
+         for line in f:
+             lines.append(line.strip())
+         f.close()
