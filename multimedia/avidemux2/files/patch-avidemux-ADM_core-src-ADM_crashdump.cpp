5517ee9a26d0fbc52876591ba213f7a11dfc389d
Pav Lucistnik <pav@FreeBSD.org>
2010-12-10 14:04:46 +0000

    - Update to 2.5.4 - Split plugins to separate port - Transfer maintainership to multimedia@
    PR:		ports/152419
    Submitted by:	nox

---
+++ multimedia/avidemux2/files/patch-avidemux-ADM_core-src-ADM_crashdump.cpp
@@ -0,0 +1,11 @@
+--- avidemux/ADM_core/src/ADM_crashdump.cpp.orig
++++ avidemux/ADM_core/src/ADM_crashdump.cpp
+@@ -47,7 +47,7 @@ void ADM_setCrashHook(ADM_saveFunction *
+ extern char *ADM_getBaseDir(void);
+ extern void A_parseECMAScript(const char *name);
+ 
+-#ifdef __APPLE__
++#if defined(__APPLE__) || defined(__FreeBSD__)
+ void installSigHandler() {}
+ 
+ void ADM_backTrack(const char *info,int lineno,const char *file)
