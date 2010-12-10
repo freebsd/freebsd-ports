34f8c5e54828e556a84da5f6d84b3abd21c69cd0
pgollucci <pgollucci@FreeBSD.org>
2010-12-10 23:03:35 +0000

    - Fix build with GCC 45 - Bump PORTREVISION
    PR:             ports/148990
    Submitted by:   Anonymous <swell.k@gmail.com>
    Approved by:    maintainer timeout (nork ; 136 days)

---
+++ graphics/OpenEXR/files/patch-exrenvmap-main.cpp
@@ -0,0 +1,10 @@
+--- exrenvmap/main.cpp~
++++ exrenvmap/main.cpp
+@@ -43,6 +43,7 @@
+ #include <makeLatLongMap.h>
+ #include <ImfEnvmap.h>
+ #include <iostream>
++#include <cstring>
+ #include <exception>
+ #include <stdlib.h>
+ 
---
+++ graphics/OpenEXR/files/patch-exrmaketiled-main.cpp
@@ -0,0 +1,10 @@
+--- exrmaketiled/main.cpp~
++++ exrmaketiled/main.cpp
+@@ -44,6 +44,7 @@
+ 
+ #include <iostream>
+ #include <exception>
++#include <cstring>
+ #include <string>
+ #include <stdlib.h>
+ 
