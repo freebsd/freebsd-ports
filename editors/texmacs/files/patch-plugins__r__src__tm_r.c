6cddd349b090c246fbb130e7d6f5ce385bd845c7
Wen Heping <wen@FreeBSD.org>
2010-12-15 07:06:23 +0000

    -Update to 1.0.7.8 -Remove checks for old FreeBSD versions -Prevent original versions of patched files from being installed -Install .desktop file in the right place
    Changelog: http://www.texmacs.org/tmweb/about/changes.en.html
    
    PR:		ports/153116
    Submitted by:	Rob Farmer <rfarmer@predatorlabs.net> (maintainer)

--- editors/texmacs/files/patch-plugins__r__src__tm_r.c
+++ editors/texmacs/files/patch-plugins__r__src__tm_r.c
@@ -1,8 +1,8 @@
---- plugins/r/src/tm_r.c.orig	2009-11-26 10:55:09.000000000 -0800
-+++ plugins/r/src/tm_r.c	2010-01-20 22:48:57.000000000 -0800
-@@ -11,11 +11,15 @@
+--- plugins/r/src/tm_r.c.orig	2010-11-24 12:52:55.000000000 -0800
++++ plugins/r/src/tm_r.c	2010-12-12 11:31:52.000000000 -0800
+@@ -10,20 +10,22 @@
+ ******************************************************************************/
  
- #include "config.h"
  #include <stdio.h>
 +#include <sys/types.h>
  #include <sys/select.h>
@@ -12,28 +12,17 @@
  #if HAVE_PTY_H
  #include <pty.h>
  #endif
--#include <utmp.h>
 +#endif
+ 
+-#include <utmp.h>
  #include <unistd.h>
  #include <termios.h>
  #include <stdlib.h>
-@@ -23,7 +27,6 @@
+ #include <signal.h>
  #include <string.h>
- 
+-#include <util.h>
  
 -#include <sys/types.h>
  #include <sys/stat.h>
  #include <fcntl.h>
  #include <sys/time.h>
-@@ -469,7 +472,11 @@
- #endif
- 
-       /* Main pselect switch --------------- */
-+#if defined(__FreeBSD__) && __FreeBSD__ < 5
-+      if( select( master+1, &rd, &wr, &er, NULL ) > 0 ) {
-+#else
-       if( pselect( master+1, &rd, &wr, &er, NULL, &orig_sigmask ) > 0 ) {
-+#endif
- 	if( FD_ISSET( STDIN_FILENO, &rd ) ) {
- 	  /* =============== read input from TeXmacs */
- 	  nread = read_B( STDIN_FILENO, RB, 1000 ) ;
