60dfc9c453e121e71a94373e5329544d788422b5
Pav Lucistnik <pav@FreeBSD.org>
2010-12-27 20:55:08 +0000

    - Fix build with new boost
    Submitted by:	Alexander Churanov <alexanderchuranov@gmail.com>

---
+++ games/alephone/files/patch-Source_Files_Network_Metaserver_network_metaserver.h
@@ -0,0 +1,10 @@
+--- Source_Files/Network/Metaserver/network_metaserver.h.orig	2010-12-10 15:54:11.000000000 +0300
++++ Source_Files/Network/Metaserver/network_metaserver.h	2010-12-10 15:54:23.000000000 +0300
+@@ -32,6 +32,7 @@
+ #include <map>
+ #include <memory> // auto_ptr
+ #include <set>
++#include <stdexcept>
+ 
+ #include "Logging.h"
+ 
