dd7417c07ed7c2d66005e2bb9dca99c800d89e57
TAKATSU Tomonari <tota@FreeBSD.org>
2011-01-04 02:17:01 +0000

    - Update to 1.10 [1] - Remove MD5 checksum from distinfo
    PR:	ports/153523 [1]
    Submitted by:	Hung-Yi Chen <gaod_AT_hychen_DOT_org> (maintainer) [1]

--- math/liblbfgs/files/patch-lib_arithmetic_sse_double.h
+++ math/liblbfgs/files/patch-lib_arithmetic_sse_double.h
@@ -1,19 +1,12 @@
---- lib/arithmetic_sse_double.h.orig	2010-06-08 08:17:38.000000000 +0800
-+++ lib/arithmetic_sse_double.h	2010-06-08 08:19:41.000000000 +0800
-@@ -26,7 +26,6 @@
- /* $Id: arithmetic_sse_double.h 65 2010-01-29 12:19:16Z naoaki $ */
+--- lib/arithmetic_sse_double.h.orig	2010-12-30 02:13:34.000000000 +0800
++++ lib/arithmetic_sse_double.h	2010-12-30 02:14:04.000000000 +0800
+@@ -26,9 +26,6 @@
+ /* $Id$ */
  
  #include <stdlib.h>
+-#ifndef __APPLE__
 -#include <malloc.h>
+-#endif
  #include <memory.h>
  
  #if     1400 <= _MSC_VER
-@@ -37,6 +36,8 @@
- #include <emmintrin.h>
- #endif/*HAVE_EMMINTRIN_H*/
- 
-+#define memalign(A,B)  malloc(B)
-+
- inline static void* vecalloc(size_t size)
- {
- #ifdef	_MSC_VER
