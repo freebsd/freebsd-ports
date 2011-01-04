a049198b40864f2d9c9ea76ba1f18ccc0e8c305b
Pietro Cerutti <gahr@FreeBSD.org>
2011-01-04 09:21:54 +0000

    - Update to 2.9.0.4684   Release notes:   https://sourceforge.net/projects/codelite/files/Releases/codelite-2.9/README/view

---
+++ editors/codelite/files/patch-Plugin_wxterminal.cpp
@@ -0,0 +1,16 @@
+--- Plugin/wxterminal.cpp.orig	2011-01-03 17:42:09.000000000 +0100
++++ Plugin/wxterminal.cpp	2011-01-03 17:43:05.000000000 +0100
+@@ -21,7 +21,13 @@
+ #endif
+ 
+ #if defined(__WXGTK__)
++#ifdef __FreeBSD__
++#    include <sys/ioctl.h>
++#    include <termios.h>
++#    include <libutil.h>
++#else
+ #    include <pty.h>
++#endif
+ #    include "unixprocess_impl.h"
+ #elif defined(__WXMAC__)
+ #    include <util.h>
