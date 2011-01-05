dd63f3eb4184938713eb37833c791e3e880fc06b
wen <wen@FreeBSD.org>
2011-01-05 00:31:02 +0000

    - Add Python Software Foundation License[1] as PSFL
    [1] http://en.wikipedia.org/wiki/Python_Software_Foundation_License
    
    PR:		ports/153341
    Submitted by:	Sofian Brabez <sbrabez@gmail.com>

--- Mk/bsd.licenses.db.mk
+++ Mk/bsd.licenses.db.mk
@@ -29,19 +29,19 @@
 
 # License definitions
 #
-# _LICENSE_LIST			- List of defined licenses
+# _LICENSE_LIST		- List of defined licenses
 # _LICENSE_LIST_GROUPS	- List of defined license groups
 #
 
-_LICENSE_LIST=			ART10 ART20 ASL BSD BSL CDDL EPL GFDL GPLv1 GPLv2 GPLv3 ISCL LGPL20 LGPL21 LGPL3 MIT MPL OWL PHP202 PHP30 PHP301
+_LICENSE_LIST=		ART10 ART20 ASL BSD BSL CDDL EPL GFDL GPLv1 GPLv2 GPLv3 ISCL LGPL20 LGPL21 LGPL3 MIT MPL OWL PHP202 PHP30 PHP301 PSFL
 
 # List of groups (only names must be present)
 
-_LICENSE_NAME_FSF=		Free Software Foundation Approved
+_LICENSE_NAME_FSF=	Free Software Foundation Approved
 
-_LICENSE_NAME_GPL=		GPL Compatible
+_LICENSE_NAME_GPL=	GPL Compatible
 
-_LICENSE_NAME_OSI=		OSI Approved
+_LICENSE_NAME_OSI=	OSI Approved
 
 # Default permissions for licenses here, if not defined
 
@@ -49,7 +49,7 @@ _LICENSE_PERMS_DEFAULT=	dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
 
 # Properties of license "xxx" (similar to port variables, but single)
 #
-# _LICENSE_NAME_xxx		- Full name/description of license/group
+# _LICENSE_NAME_xxx	- Full name/description of license/group
 # _LICENSE_PERMS_xxx	- Permissions (if not defined defaults to
 # 						  ${_LICENSE_PERMS_DEFAULT}.
 # _LICENSE_GROUPS_xxx	- Groups (optional)
@@ -64,22 +64,22 @@ _LICENSE_GROUPS_ART10=	OSI
 _LICENSE_NAME_ART20=	Artistic License version 2.0
 _LICENSE_GROUPS_ART20=	FSF GPL OSI
 
-_LICENSE_NAME_ASL=		Apache License
+_LICENSE_NAME_ASL=	Apache License
 _LICENSE_GROUPS_ASL=	FSF OSI
 
-_LICENSE_NAME_BSD=		BSD license
+_LICENSE_NAME_BSD=	BSD license
 _LICENSE_GROUPS_BSD=	FSF OSI
 
-_LICENSE_NAME_BSL=		Boost Software License
+_LICENSE_NAME_BSL=	Boost Software License
 _LICENSE_GROUPS_BSL=	FSF OSI
 
-_LICENSE_NAME_CDDL=		Common Development and Distribution License
+_LICENSE_NAME_CDDL=	Common Development and Distribution License
 _LICENSE_GROUPS_CDDL=	FSF OSI
 
-_LICENSE_NAME_EPL=		Eclipse Public License
+_LICENSE_NAME_EPL=	Eclipse Public License
 _LICENSE_GROUPS_EPL=	FSF OSI
 
-_LICENSE_NAME_GFDL=		GNU Free Documentation License
+_LICENSE_NAME_GFDL=	GNU Free Documentation License
 _LICENSE_GROUPS_GFDL=	FSF
 
 _LICENSE_NAME_GPLv1=	GNU General Public License version 1
@@ -91,7 +91,7 @@ _LICENSE_GROUPS_GPLv2=	FSF GPL OSI
 _LICENSE_NAME_GPLv3=	GNU General Public License version 3
 _LICENSE_GROUPS_GPLv3=	FSF GPL OSI
 
-_LICENSE_NAME_ISCL=		Internet Systems Consortium License
+_LICENSE_NAME_ISCL=	Internet Systems Consortium License
 _LICENSE_GROUPS_ISCL=	FSF GPL OSI
 
 _LICENSE_NAME_LGPL20=	GNU Library General Public License version 2.0
@@ -103,23 +103,26 @@ _LICENSE_GROUPS_LGPL21=	FSF GPL OSI
 _LICENSE_NAME_LGPL3=	GNU Lesser General Public License version 3
 _LICENSE_GROUPS_LGPL3=	FSF GPL OSI
 
-_LICENSE_NAME_MIT=		MIT license / X11 license
+_LICENSE_NAME_MIT=	MIT license / X11 license
 _LICENSE_GROUPS_MIT=	FSF GPL OSI
 
-_LICENSE_NAME_MPL=		Mozilla Public License
+_LICENSE_NAME_MPL=	Mozilla Public License
 _LICENSE_GROUPS_MPL=	FSF OSI
 
-_LICENSE_NAME_OWL=		Open Works License (owl.apotheon.org)
+_LICENSE_NAME_OWL=	Open Works License (owl.apotheon.org)
 _LICENSE_GROUPS_OWL=
 
-_LICENSE_NAME_PHP202=		PHP License version 2.02
+_LICENSE_NAME_PHP202=	PHP License version 2.02
 _LICENSE_GROUPS_PHP202=	FSF OSI
 
-_LICENSE_NAME_PHP30=		PHP License version 3.0
+_LICENSE_NAME_PHP30=	PHP License version 3.0
 _LICENSE_GROUPS_PHP30=	FSF OSI
 
-_LICENSE_NAME_PHP301=		PHP License version 3.01
+_LICENSE_NAME_PHP301=	PHP License version 3.01
 _LICENSE_GROUPS_PHP301=	FSF OSI
+
+_LICENSE_NAME_PSFL=	Python Software Foundation License
+_LICENSE_GROUPS_PSFL=	FSF GPL OSI
 
 # Set default permissions if not defined
 
