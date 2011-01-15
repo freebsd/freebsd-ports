015f8efcae7e5abe251c2a7927d9cf420fa5cb28
makc <makc@FreeBSD.org>
2011-01-15 21:09:43 +0000

    Fix build on 7.x and drop support for 6.x
    Reported by:	pointyhat
    Feature safe:	yes

--- x11-themes/qtcurve-gtk2/files/extra-patch-style-qt_settings.c
+++ x11-themes/qtcurve-gtk2/files/extra-patch-style-qt_settings.c
@@ -1,11 +1,11 @@
---- style/qt_settings.c.o	2010-05-06 14:56:53.000000000 +0000
-+++ style/qt_settings.c	2010-05-06 15:00:46.000000000 +0000
-@@ -22,6 +22,8 @@
- #include "common.h"
- #define CONFIG_READ
- #include "config_file.c"
+--- ./style/qt_settings.c.orig	2011-01-03 00:41:06.000000000 +0300
++++ ./style/qt_settings.c	2011-01-15 21:01:39.529481980 +0300
+@@ -35,6 +35,8 @@
+ #include <dirent.h>
+ #include <errno.h>
+ #include <locale.h>
 +#include "getline.h"
 +#include "getline.c"
- #include <gtk/gtk.h>
- #include <time.h>
- #include <gdk/gdkcolor.h>
+ 
+ QtCPalette qtcPalette;
+ Options    opts;
