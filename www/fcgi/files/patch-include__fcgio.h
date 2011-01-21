2100f08c257c8aa7decdd2d5fcd2b58ebc32a527
bapt <bapt@FreeBSD.org>
2011-01-21 13:17:26 +0000

    Fix compilation with CXX=g++45 While here, remove MD5 from distinfo.
    PR:		ports/154190
    Submitted by:	Julien Laffaye <kimelto _at_ gmail.com>
    Feature safe:	yes

---
+++ www/fcgi/files/patch-include__fcgio.h
@@ -0,0 +1,10 @@
+--- ./include/fcgio.h.orig	2011-01-21 12:26:36.000000000 +0000
++++ ./include/fcgio.h	2011-01-21 12:27:29.000000000 +0000
+@@ -31,6 +31,7 @@
+ #define FCGIO_H
+ 
+ #include <iostream>
++#include <cstdio>
+ 
+ #include "fcgiapp.h"
+ 
