4790837cab5589be2841b2e2ee40fd84f6938c3f
knu <knu@FreeBSD.org>
2011-01-22 04:05:05 +0000

    Update to 3.5.3.20101010.
    Feature safe:	yes

--- games/unnethack/files/patch-config.h
+++ games/unnethack/files/patch-config.h
@@ -1,24 +1,15 @@
-$FreeBSD$
-
---- include/config.h.orig	2010-01-10 23:51:18.000000000 +0900
-+++ include/config.h	2010-01-19 13:36:18.734207870 +0900
-@@ -178,11 +178,11 @@
- #ifndef AUTOCONF
- #ifdef UNIX
- /* path and file name extension for compression program */
--#define COMPRESS "/usr/bin/compress"	/* Lempel-Ziv compression */
--#define COMPRESS_EXTENSION ".Z"		/* compress's extension */
-+/* #define COMPRESS "/usr/bin/compress" */	/* Lempel-Ziv compression */
-+/* #define COMPRESS_EXTENSION ".Z" */		/* compress's extension */
+--- include/config.h.orig	2010-10-10 21:46:00.000000000 +0900
++++ include/config.h	2011-01-22 01:34:41.205152954 +0900
+@@ -188,7 +188,7 @@
+ /* #define COMPRESS "/usr/bin/compress" */	/* Lempel-Ziv compression */
+ /* #define COMPRESS_EXTENSION ".Z"	*/	/* compress's extension */
  /* An example of one alternative you might want to use: */
--/* #define COMPRESS "/usr/local/bin/gzip" */	/* FSF gzip compression */
--/* #define COMPRESS_EXTENSION ".gz" */		/* normal gzip extension */
+-#define COMPRESS "/bin/gzip"	/* FSF gzip compression */
 +#define COMPRESS "/usr/bin/gzip"	/* FSF gzip compression */
-+#define COMPRESS_EXTENSION ".gz"		/* normal gzip extension */
+ #define COMPRESS_EXTENSION ".gz"		/* normal gzip extension */
  #endif
  
- #ifndef COMPRESS
-@@ -195,7 +195,7 @@
+@@ -202,7 +202,7 @@
   *	a tar-like file, thus making a neater installation.  See *conf.h
   *	for detailed configuration.
   */
@@ -27,7 +18,7 @@ $FreeBSD$
  
  /*
   *	Defining INSURANCE slows down level changes, but allows games that
-@@ -214,7 +214,7 @@
+@@ -221,7 +221,7 @@
   * otherwise it will be the current directory.
   */
  # ifndef HACKDIR
