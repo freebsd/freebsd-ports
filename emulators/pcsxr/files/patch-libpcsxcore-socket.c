b2e8b17df8898890d6db7e06c5512a6107a379cc
Ganael LAPLANCHE <martymac@FreeBSD.org>
2011-01-31 09:03:02 +0000

    - Update to 1.9.92.r62569 (SVN rev. 62569) - Add a LOCAL mirror - Use REINPLACE_CMD instead of SED - Cleanup Makefile (wrap long lines, remove useless comments)
    Approved by:	ehaupt (mentor)
    Feature safe:	yes

--- emulators/pcsxr/files/patch-libpcsxcore-socket.c
+++ emulators/pcsxr/files/patch-libpcsxcore-socket.c
@@ -1,14 +1,10 @@
---- libpcsxcore/socket.c.orig	2010-05-19 17:43:40.987010639 +0000
-+++ libpcsxcore/socket.c	2010-05-19 17:46:42.625194542 +0000
-@@ -19,9 +19,11 @@
- #ifdef _WIN32
- #include <winsock2.h>
- #else
+--- libpcsxcore/socket.c.orig	2011-01-21 18:06:44.210865888 +0100
++++ libpcsxcore/socket.c	2011-01-21 18:07:58.238538120 +0100
+@@ -23,6 +23,7 @@
+ #include "socket.h"
+ 
+ #ifndef _WIN32
 +#include <sys/types.h>
  #include <sys/socket.h>
  #include <sys/ioctl.h>
  #include <arpa/inet.h>
-+#include <netinet/in.h>
- #include <unistd.h>
- #include <fcntl.h>
- #endif
