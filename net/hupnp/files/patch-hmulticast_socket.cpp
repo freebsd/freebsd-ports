2ab6f0f5368af84c12029c1805b7c73eb657339d
fluffy <fluffy@FreeBSD.org>
2011-02-05 16:16:13 +0000

    - Add HUpnp, a library for building UPnP devices and control points
    Feature safe:	yes

---
+++ net/hupnp/files/patch-hmulticast_socket.cpp
@@ -0,0 +1,11 @@
+--- ./hupnp/src/hupnp_core/socket/hmulticast_socket.cpp.orig	2010-12-29 14:08:26.000000000 +1000
++++ ./hupnp/src/hupnp_core/socket/hmulticast_socket.cpp	2010-12-29 14:10:00.000000000 +1000
+@@ -27,6 +27,8 @@
+ #include <ws2tcpip.h>
+ #else
+ #include <arpa/inet.h>
++#include <netinet/in.h>
++#include <sys/socket.h>
+ #endif
+ 
+ #include <QtNetwork/QNetworkProxy>
