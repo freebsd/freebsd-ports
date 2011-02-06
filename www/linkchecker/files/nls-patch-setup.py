61cc487b4e61d3d665064c9f7d945469e63d6a8d
Beat Gaetzi <beat@FreeBSD.org>
2011-02-06 14:28:26 +0000

    - Update to 6.3 - Add LICENSE
    PR:		ports/154541 (based on)
    Submitted by:	Oleg Ginzburg <olevole AT yandex.ru>
    Feature safe:	yes

--- www/linkchecker/files/nls-patch-setup.py
+++ www/linkchecker/files/nls-patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2009-08-26 15:56:00.000000000 +0200
-+++ setup.py	2009-08-26 15:57:17.000000000 +0200
-@@ -384,6 +386,8 @@
+--- setup.py.orig	2011-02-06 04:07:27.486400241 +0300
++++ setup.py	2011-02-06 04:08:17.222411661 +0300
+@@ -380,6 +380,8 @@
  if os.name == 'posix':
-     data_files.append(('man/man1', ['doc/en/linkchecker.1']))
-     data_files.append(('man/man5', ['doc/en/linkcheckerrc.5']))
-+    data_files.append(('man/de/man1', ['doc/de/linkchecker.1']))
-+    data_files.append(('man/de/man5', ['doc/de/linkcheckerrc.5']))
-     data_files.append(('share/examples/linkchecker',
+               data_files.append(('man/man1', ['doc/en/linkchecker.1']))
+               data_files.append(('man/man5', ['doc/en/linkcheckerrc.5']))
++              data_files.append(('man/de/man1', ['doc/de/linkchecker.1']))
++              data_files.append(('man/de/man5', ['doc/de/linkcheckerrc.5']))
+               data_files.append(('share/examples/linkchecker',
                ['config/linkchecker-completion',
                 'doc/examples/check_blacklist.sh',
--- www/linkchecker/files/patch-setup.py
+++ www/linkchecker/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2009-08-03 23:53:01.000000000 +0200
-+++ setup.py	2009-08-26 15:56:00.000000000 +0200
-@@ -364,17 +361,14 @@
+--- setup.py.orig	2011-02-06 03:58:59.454396452 +0300
++++ setup.py	2011-02-06 04:05:09.118442022 +0300
+@@ -357,17 +357,14 @@
  if os.name == 'nt':
      # windows does not have unistd.h
      define_macros.append(('YY_NO_UNISTD_H', None))
@@ -15,12 +15,12 @@
 -        ['config/linkcheckerrc', 'config/logging.conf',
 -        'doc/html/lccollection.qhc', 'doc/html/lcdoc.qch']),
 -    ('share/linkchecker/examples',
-+        ['config/linkcheckerrc', 'config/logging.conf']),
-+    ('share/examples/linkchecker',
++    ['config/linkcheckerrc', 'config/logging.conf']),
++        ('share/examples/linkchecker',
          ['cgi-bin/lconline/leer.html.en',
           'cgi-bin/lconline/leer.html.de',
           'cgi-bin/lconline/index.html',
-@@ -388,11 +382,9 @@
+@@ -381,11 +378,9 @@
  ]
  
  if os.name == 'posix':
@@ -29,9 +29,9 @@
 -    data_files.append(('share/man/de/man1', ['doc/de/linkchecker.1', 'doc/de/linkchecker-gui.1']))
 -    data_files.append(('share/man/de/man5', ['doc/de/linkcheckerrc.5']))
 -    data_files.append(('share/linkchecker/examples',
-+    data_files.append(('man/man1', ['doc/en/linkchecker.1']))
-+    data_files.append(('man/man5', ['doc/en/linkcheckerrc.5']))
-+    data_files.append(('share/examples/linkchecker',
++              data_files.append(('man/man1', ['doc/en/linkchecker.1']))
++              data_files.append(('man/man5', ['doc/en/linkcheckerrc.5']))
++              data_files.append(('share/examples/linkchecker',
                ['config/linkchecker-completion',
                 'doc/examples/check_blacklist.sh',
                 'doc/examples/check_for_x_errors.sh',
