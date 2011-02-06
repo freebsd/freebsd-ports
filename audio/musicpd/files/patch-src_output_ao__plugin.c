25b19e48b42b31d5cc544ac0711049b9bb3b699f
Martin Wilke <miwi@FreeBSD.org>
2011-02-06 18:06:30 +0000

    - Update to 1.16.1
    PR:		154549
    Submitted by:	maintainer
    Feature safe:	yes

--- audio/musicpd/files/patch-src_output_ao__plugin.c
+++ audio/musicpd/files/patch-src_output_ao__plugin.c
@@ -1,13 +1,12 @@
-
 $FreeBSD$
 
---- src/output/ao_plugin.c.orig
-+++ src/output/ao_plugin.c
-@@ -175,6 +175,7 @@
- 	if (audio_format->bits > 16)
- 		audio_format->bits = 16;
+--- src/output/ao_plugin.c.orig	2011-02-05 21:03:39.032765668 +0000
++++ src/output/ao_plugin.c	2011-02-05 21:04:11.006777588 +0000
+@@ -188,6 +188,7 @@
+ 		break;
+ 	}
  
 +	memset(&format, 0, sizeof(format));
- 	format.bits = audio_format->bits;
  	format.rate = audio_format->sample_rate;
  	format.byte_format = AO_FMT_NATIVE;
+ 	format.channels = audio_format->channels;
