5bafc9702d22cb28e352caeaa78998afcfec1cf2
Peter Pentchev <roam@FreeBSD.org>
2011-02-10 09:34:59 +0000

    Update to stunnel-4.35: - drop the transparent proxying patch, integrated upstream - while I'm here, fix the very first master site URL -   the download page on stunnel.org just links to the sites now,   and does not contain downloadable source
    PR:		154631
    Submitted by:	Tsurutani Naoki <turutani@scphys.kyoto-u.ac.jp>

--- security/stunnel/files/patch-src::common.h
+++ security/stunnel/files/patch-src::common.h
@@ -1,28 +1,11 @@
-Description: Build with older OpenSSL and enable transparent binding.
-Forwarded: yes (the transparent proxying part)
-Author: Peter Pentchev <roam@FreeBSD.org>,
-	Jason Helfman <jhelfman@experts-exchange.com>
-Last-Update: 2011-01-04
+Description: Build on FreeBSD versions of OpenSSL < 0.9.8b.
+Forwarded: not-needed
+Author: Peter Pentchev <roam@FreeBSD.org>
+Last-Update: 2011-02-10
 
 --- src/common.h.orig
 +++ src/common.h
-@@ -337,6 +337,15 @@
- /* old kernel headers without IP_TRANSPARENT definition */
- #define IP_TRANSPARENT 19
- #endif /* IP_TRANSPARENT */
-+#define STUNNEL_TRANSPARENT IP_TRANSPARENT
-+#define STUNNEL_TRANSPARENT_NAME "IP_TRANSPARENT"
-+#define STUNNEL_TRANSPARENT_LEVEL SOL_IP
-+#else /* __linux__ */
-+#ifdef IP_BINDANY
-+#define STUNNEL_TRANSPARENT IP_BINDANY
-+#define STUNNEL_TRANSPARENT_NAME "IP_BINDANY"
-+#define STUNNEL_TRANSPARENT_LEVEL IPPROTO_IP
-+#endif
- #endif /* __linux__ */
- 
- #endif /* USE_WIN32 */
-@@ -347,9 +356,6 @@
+@@ -353,9 +353,6 @@
  
  #define OPENSSL_THREAD_DEFINES
  #include <openssl/opensslconf.h>
