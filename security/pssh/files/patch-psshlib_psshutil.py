71f6498fb519ad62aa6ddb6ba1e31738a503cfcd
brooks <brooks@FreeBSD.org>
2011-02-11 18:12:28 +0000

    Update to 2.2.2
    PR:		ports/154568
    Submitted by:	Ruslan Mahmatkhanov

--- security/pssh/files/patch-psshlib_psshutil.py
+++ security/pssh/files/patch-psshlib_psshutil.py
@@ -3,15 +3,15 @@ $FreeBSD$
 
 --- psshlib/psshutil.py.orig
 +++ psshlib/psshutil.py
-@@ -15,7 +15,10 @@
-     if not pathnames:
-         return lines
-     for pathname in pathnames:
--        f = open(pathname)
-+        if pathname == "-":
-+            f = open("/dev/stdin")
-+        else:
-+            f = open(pathname)
-         for line in f:
-             lines.append(line.strip())
-         f.close()
+@@ -27,7 +27,10 @@
+     Returns a list of (host, port, user) triples.
+     """
+     lines = []
+-    f = open(path)
++    if path == "-":
++        f = open("/dev/stdin")
++    else:
++        f = open(path)
+     for line in f:
+         lines.append(line.strip())
+     f.close()
