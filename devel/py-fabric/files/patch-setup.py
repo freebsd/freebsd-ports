b97973287da41a432468d1e1ba9800839e6bedc4
Li-Wen Hsu <lwhsu@FreeBSD.org>
2011-02-12 17:20:12 +0000

    - Update to 0.9.3
    PR:		ports/154156
    Submitted by:	lwhsu
    Approved by:	maintainer timeout

--- devel/py-fabric/files/patch-setup.py
+++ devel/py-fabric/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2010-10-14 13:37:04.963787042 +0800
-+++ setup.py	2010-10-14 13:37:11.377290180 +0800
-@@ -31,7 +31,6 @@
+--- setup.py.orig	2010-11-13 04:57:34.000000000 +0800
++++ setup.py	2011-01-20 12:46:01.754034224 +0800
+@@ -38,7 +38,6 @@
      packages=find_packages(),
      test_suite='nose.collector',
      tests_require=['nose', 'fudge'],
--    install_requires=['pycrypto <2.1', 'paramiko >=1.7.6'],
+-    install_requires=['pycrypto %s' % PYCRYPTO, 'paramiko >=1.7.6'],
      entry_points={
          'console_scripts': [
              'fab = fabric.main:main',
