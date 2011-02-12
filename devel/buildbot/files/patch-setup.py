e07c245a9ed5f8971d30b4b86e66598a03bd772f
Cheng-Lung Sung <clsung@FreeBSD.org>
2011-02-12 15:00:55 +0000

    - Update to 0.8.2

--- devel/buildbot/files/patch-setup.py
+++ devel/buildbot/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2010-07-07 13:45:16.000000000 +0800
-+++ setup.py	2010-08-06 11:28:28.000000000 +0800
-@@ -233,31 +233,6 @@
+--- setup.py.orig	2011-02-12 22:21:14.000000000 +0800
++++ setup.py	2011-02-12 22:23:41.000000000 +0800
+@@ -251,34 +251,6 @@
  py_25 = sys.version_info[0] > 2 or (sys.version_info[0] == 2 and sys.version_info[1] >= 5)
  py_26 = sys.version_info[0] > 2 or (sys.version_info[0] == 2 and sys.version_info[1] >= 6)
  
@@ -14,7 +14,7 @@
 -    ## dependencies
 -    setup_args['install_requires'] = [
 -        'twisted >= 2.0.0',
--        'Jinja2',
+-        'Jinja2 >= 2.1',
 -    ]
 -    # Python-2.6 and up includes json
 -    if not py_26:
@@ -23,6 +23,9 @@
 -    # Python-2.6 and up includes a working A sqlite (py25's is broken)
 -    if not py_26:
 -        setup_args['install_requires'].append('pysqlite')
+-
+-    if os.getenv('NO_INSTALL_REQS'):
+-        setup_args['install_requires'] = None
 -
 -    entry_points={
 -        'console_scripts': [
