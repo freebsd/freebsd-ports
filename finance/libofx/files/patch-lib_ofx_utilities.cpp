84b6629f10549059b4e80d9661dd59768a8f0f9d
dhn <dhn@FreeBSD.org>
2011-02-17 21:31:17 +0000

    - Update to 0.9.2 - Pass maintainership to submitter - Mark MAKE_JOBS_SAFE - Add LICENSE (GPLv2) - Remove MD5 checksum
    PR:		ports/154783
    Submitted by:	Jason E. Hale <bsdkaffee@gmail.com>

--- finance/libofx/files/patch-lib_ofx_utilities.cpp
+++ finance/libofx/files/patch-lib_ofx_utilities.cpp
@@ -1,20 +1,15 @@
---- lib/ofx_utilities.cpp.orig	2007-10-27 14:15:58.000000000 +0200
-+++ lib/ofx_utilities.cpp	2009-11-08 01:02:47.000000000 +0100
-@@ -16,6 +16,7 @@
-  *                                                                         *
-  ***************************************************************************/
- #include <config.h>
-+#include <locale.h>
- #include <iostream>
- #include <assert.h>
- 
-@@ -113,6 +114,9 @@
-   char time_zone_specified = false;
+--- ./lib/ofx_utilities.cpp.orig	2011-02-12 11:51:02.000000000 -0500
++++ ./lib/ofx_utilities.cpp	2011-02-14 22:24:26.000000000 -0500
+@@ -115,9 +115,10 @@
  
    time_t temptime;
-+
+ 
+-  time.tm_isdst = daylight; // iniitialize dst setting
 +  bzero(&time, sizeof(time));
 +
    std::time(&temptime);
-   local_offset = difftime(mktime(localtime(&temptime)), mktime(gmtime(&temptime)));
+-  local_offset = difftime(mktime(localtime(&temptime)), mktime(gmtime(&temptime))) + (3600*daylight);
++  local_offset = difftime(mktime(localtime(&temptime)), mktime(gmtime(&temptime)));
    
+   if(ofxdate.size()!=0){
+     if (ofxdate.substr(0,8).find_first_not_of("0123456789") != string::npos ){
