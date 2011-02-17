0548dcac4197e5e3f12537b8a65219f9f8533d70
Beech Rintoul <beech@FreeBSD.org>
2011-02-17 01:56:59 +0000

    Update to 1.6.20
    PR:		ports/153752
    Submitted by:	Kristian Kraemmer Nielsen (jkkn@blackbird.jkkn.net)
    Approved by:	itetcu (mentor)(implicit)

--- irc/eggdrop/files/patch-src__eggdrop.h
+++
@@ -1,9 +0,0 @@
---- src/eggdrop.h.orig	Mon Jun  2 11:39:45 2003
-+++ src/eggdrop.h	Mon Jun  2 11:40:17 2003
-@@ -77,7 +77,7 @@
- /* Language stuff */
--#define LANGDIR  "./language" /* language file directory                   */
-+#define LANGDIR  __PREFIX__ "/share/eggdrop/language" /* language file directory */
- #define BASELANG "english"    /* language which always gets loaded before
-                                  all other languages. You do not want to
-                                  change this.                              */
