fd33828961e4fe1ea0da53baaba9836fff73ed1f
Dennis Herrmann <dhn@FreeBSD.org>
2011-02-17 21:31:17 +0000

    - Update to 0.9.2 - Pass maintainership to submitter - Mark MAKE_JOBS_SAFE - Add LICENSE (GPLv2) - Remove MD5 checksum
    PR:		ports/154783
    Submitted by:	Jason E. Hale <bsdkaffee@gmail.com>

--- finance/libofx/files/patch-lib_ofx_preproc.cpp
+++ finance/libofx/files/patch-lib_ofx_preproc.cpp
@@ -1,17 +1,9 @@
 
 $FreeBSD$
 
---- lib/ofx_preproc.cpp.orig	2008-12-07 07:22:57.000000000 +0900
-+++ lib/ofx_preproc.cpp	2009-03-16 03:31:53.000000000 +0900
-@@ -23,6 +23,7 @@
- #include <stdlib.h>
- #include <stdio.h>
- #include <string>
-+#include <unistd.h>
- #include "ParserEventGeneratorKit.h"
- #include "libofx.h"
- #include "messages.hh"
-@@ -185,7 +186,7 @@
+--- ./lib/ofx_preproc.cpp.orig	2011-02-12 11:51:02.000000000 -0500
++++ ./lib/ofx_preproc.cpp	2011-02-14 13:04:17.000000000 -0500
+@@ -194,7 +194,7 @@
  	    memset(iconv_buffer,0,READ_BUFFER_SIZE * 2);
  	    size_t inbytesleft = strlen(s_buffer.c_str());
  	    size_t outbytesleft = READ_BUFFER_SIZE * 2 - 1;
