2fdec5f011ef48f080506366c7f1eaaefbda277f
rene <rene@FreeBSD.org>
2011-02-21 22:17:16 +0000

    Remove expired ports: 2011-02-20 devel/root: Port does not build on supported versions of FreeBSD 2011-02-20 net/ztelnet: unfetchable / Does not compile with GCC 4.2 2011-02-20 print/kaspaliste: Does not compile with GCC 4.2 2011-02-20 sysutils/perf: Does not build on supported versions of FreeBSD

--- devel/root/files/patch-xml::Module.mk
+++
@@ -1,8 +0,0 @@
---- xmlparser/Module.mk.orig	Tue Feb 22 17:32:18 2005
-+++ xmlparser/Module.mk	Tue Feb 22 17:32:42 2005
-@@ -67,4 +67,4 @@
- 
- ##### extra rules ######
- $(XMLO): %.o: %.cxx
--	$(CXX) $(OPT) $(CXXFLAGS) -I$(XMLINCDIR) -o $@ -c $<
-+	$(CXX) $(OPT) $(CXXFLAGS) -I$(XMLINCDIR) -I/usr/local/include -o $@ -c $<
