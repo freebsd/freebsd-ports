04bbe7297033c62ede7c32757a0d897834213663
clsung <clsung@FreeBSD.org>
2011-02-21 04:02:51 +0000

    - Update to 1.5.1
    PR:		ports/154665
    Submitted by:	wen

--- devel/ruby-gems/files/patch-lib__rubygems__installer.rb
+++ devel/ruby-gems/files/patch-lib__rubygems__installer.rb
@@ -1,11 +1,11 @@
---- ./lib/rubygems/installer.rb.orig	2009-05-07 20:49:03.000000000 -0400
-+++ ./lib/rubygems/installer.rb	2009-06-12 00:49:09.432934400 -0400
-@@ -219,8 +219,6 @@
-     build_extensions
+--- lib/rubygems/installer.rb.orig	2011-02-10 14:03:04.000000000 +0000
++++ lib/rubygems/installer.rb	2011-02-10 14:05:02.000000000 +0000
+@@ -171,7 +171,7 @@
+     generate_bin
      write_spec
  
--    write_require_paths_file_if_needed
--
+-    write_require_paths_file_if_needed if Gem::QUICKLOADER_SUCKAGE
++    # write_require_paths_file_if_needed if Gem::QUICKLOADER_SUCKAGE
+ 
      # HACK remove?  Isn't this done in multiple places?
      cached_gem = File.join @gem_home, "cache", @gem.split(/\//).pop
-     unless File.exist? cached_gem then
