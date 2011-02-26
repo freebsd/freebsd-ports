e67d1e48c349a43d2380516a1110712ca713a79d
Dennis Herrmann <dhn@FreeBSD.org>
2011-02-26 10:02:37 +0000

    - Bump PORTREVISION - Update pkg-message - Remove MD5 checksum
    PR:		ports/154898
    Submitted by:	maintainer

--- games/iourbanterror/files/patch-code-qcommon-q_shared.h
+++ games/iourbanterror/files/patch-code-qcommon-q_shared.h
@@ -1,5 +1,5 @@
---- code/qcommon/q_shared.h.orig	2010-08-29 19:32:54.537567000 +0200
-+++ code/qcommon/q_shared.h	2010-11-27 16:55:16.000000000 +0100
+--- code/qcommon/q_shared.h.orig	2011-02-09 01:42:54.000000000 +0100
++++ code/qcommon/q_shared.h	2011-02-19 01:11:38.000000000 +0100
 @@ -27,11 +27,11 @@
  // A user mod should never modify this file
  
@@ -8,12 +8,12 @@
 -  #define BASEGAME			"foobar"
 -  #define CLIENT_WINDOW_TITLE     	"changeme"
 -  #define CLIENT_WINDOW_MIN_TITLE 	"changeme2"
--  #define GAMENAME_FOR_MASTER		"iofoo3"	// must NOT contain whitespaces
+-  #define GAMENAME_FOR_MASTER		"iofoo3"		// must NOT contain whitespaces
 +  #define PRODUCT_NAME			"ioq3+UT"
 +  #define BASEGAME			"q3ut4"
 +  #define CLIENT_WINDOW_TITLE     	"ioUrbanTerror"
 +  #define CLIENT_WINDOW_MIN_TITLE 	"ioUT"
-+  #define GAMENAME_FOR_MASTER		"Quake3Arena"	// must NOT contain whitespaces
++  #define GAMENAME_FOR_MASTER		"Quake3Arena"		// must NOT contain whitespaces
+   #define HEARTBEAT_FOR_MASTER		GAMENAME_FOR_MASTER
+   #define FLATLINE_FOR_MASTER		GAMENAME_FOR_MASTER "dead"
  #else
-   #define PRODUCT_NAME			"ioq3"
-   #define BASEGAME			"baseq3"
