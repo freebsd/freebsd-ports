2c6287833944d08ba7e7a8a46b443136dec8ea4a
nivit <nivit@FreeBSD.org>
2011-02-26 15:30:04 +0000

    - Fix build when Python version > 2.6 - Remove MD5 checksum from distinfo
    PR:		ports/154965
    Submitted by:	wen

--- x11-toolkits/py-kiwi/files/patch-kiwi__dist.py
+++ x11-toolkits/py-kiwi/files/patch-kiwi__dist.py
@@ -1,11 +1,11 @@
---- kiwi/dist.py	2009-07-14 19:01:15.000000000 +0200
-+++ kiwi/dist.py.port	2009-08-18 10:28:06.000000000 +0200
+--- ./kiwi/dist.py.orig	2009-07-14 19:01:15.000000000 +0200
++++ ./kiwi/dist.py	2011-02-26 14:52:14.000000000 +0100
 @@ -159,7 +159,7 @@
      python_version = sys.version_info[:2]
      libdir = get_python_lib(plat_specific=False,
                              standard_lib=True, prefix='')
 -    if python_version < (2, 6):
-+    if python_version <= (2, 6):
++    if python_version < (2, 6) or os.uname()[0] == 'FreeBSD':
          site = 'site-packages'
      else:
          site = 'dist-packages'
