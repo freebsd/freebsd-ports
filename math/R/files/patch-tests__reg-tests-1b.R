3d5cca35645599c079663d23d8c86260054bf1b9
Brendan Fabeny <bf@FreeBSD.org>
2011-02-28 16:03:13 +0000

    - Update R to 2.12.2, a bugfix release - Bump PORTREVISION of dependent ports,   where necessary
    Approved by:	makc, miwi (mentors, implicit)

--- math/R/files/patch-tests__reg-tests-1b.R
+++ math/R/files/patch-tests__reg-tests-1b.R
@@ -1,38 +1,5 @@
 --- tests/reg-tests-1b.R.orig	2010-12-22 03:50:30.000000000 -0500
 +++ tests/reg-tests-1b.R	2010-12-22 04:05:33.000000000 -0500
-@@ -300,8 +300,8 @@
- 
- 
- ## showDefault() problem with "unregistered" S3 classes:
--show(structure(1:3, class = "myClass"))
--## failed in R <= 2.7.0
-+## show(structure(1:3, class = "myClass"))
-+## failed in R <= 2.7.0, 2.12.1
- 
- 
- ## formatC(.., format="fg", flag="#"):
-@@ -862,14 +862,14 @@
- 
- 
- ## methods() gave two wrong warnings in some cases:
--op <- options(warn = 2)# no warning, please!
--m1 <- methods(na.omit) ## should give (no warning):
-+## op <- options(warn = 2)# no warning, please!
-+## m1 <- methods(na.omit) ## should give (no warning):
- ##
--setClass("bla")
--setMethod("na.omit", "bla", function(object, ...) "na.omit(<bla>)")
--(m2 <- methods(na.omit)) ## should give (no warning):
--stopifnot(identical(m1, m2))
--options(op)
-+## setClass("bla")
-+## setMethod("na.omit", "bla", function(object, ...) "na.omit(<bla>)")
-+## (m2 <- methods(na.omit)) ## should give (no warning):
-+## stopifnot(identical(m1, m2))
-+## options(op)
- ## gave two warnings, when an S3 generic had turned into an S4 one
- 
- 
 @@ -1344,11 +1344,11 @@
  
  ## found from fallback test in slam 0.1-15
