3d376cdc9ba8f407c146845ab533cb05026d9282
Beat Gaetzi <beat@FreeBSD.org>
2011-03-01 21:57:17 +0000

    - Update to 3.6.14 - Add support for powerpc64 [1]
    Submitted by:	andreast@ [1]

---
+++ www/firefox36/files/patch-security_nss_lib_freebl_mpi_mpcpucache.c
@@ -0,0 +1,44 @@
+--- security/nss/lib/freebl/mpi/mpcpucache.c.orig	2011-02-26 18:44:42.000000000 +0100
++++ security/nss/lib/freebl/mpi/mpcpucache.c	2011-02-26 18:46:41.000000000 +0100
+@@ -733,6 +733,33 @@
+ #endif
+ 
+ #if defined(__ppc64__) 
++#if defined(__FreeBSD__)
++#include <sys/stddef.h>
++#include <sys/sysctl.h>
++
++#include <machine/cpu.h>
++#include <machine/md_var.h>
++
++unsigned long
++s_mpi_getProcessorLineSize()
++{
++       static int cacheline_size = 0;
++       static int cachemib[] = { CTL_MACHDEP, CPU_CACHELINE };
++       int clen;
++
++       if (cacheline_size > 0)
++               return cacheline_size;
++  
++       clen = sizeof(cacheline_size);
++       if (sysctl(cachemib, sizeof(cachemib) / sizeof(cachemib[0]),
++           &cacheline_size, &clen, NULL, 0) < 0 || !cacheline_size)
++               return 128; /* guess */
++
++       return cacheline_size;
++}
++#else /* __FreeBSD__ */
++
++
+ /*
+  *  Sigh, The PPC has some really nice features to help us determine cache
+  *  size, since it had lots of direct control functions to do so. The POWER
+@@ -786,6 +813,7 @@
+     return 0;
+ }
+ 
++#endif /* __FreeBSD__ */
+ #define MPI_GET_PROCESSOR_LINE_SIZE_DEFINED 1
+ #endif
+ 
