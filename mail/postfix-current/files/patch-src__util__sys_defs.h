4dda0701508f9186b7cae513bfabedd99792c73e
Sahil Tandon <sahil@FreeBSD.org>
2011-03-02 03:10:49 +0000

    - Update to 2.9-20110228 and modify patching for   src/util/sys_defs.h, which is updated upstream   to be aware of the FreeBSD closefrom() support   time window [1]
    - Take maintainership [2]
    
    PR:		ports/155176 [1]
    Submitted by:	sahil (myself)
    Approved by:	portmgr (linimon) [2]

--- mail/postfix-current/files/patch-src__util__sys_defs.h
+++ mail/postfix-current/files/patch-src__util__sys_defs.h
@@ -1,5 +1,5 @@
---- src/util/sys_defs.h.orig	2009-12-03 22:30:03.000000000 -0200
-+++ src/util/sys_defs.h	2009-12-03 22:31:51.000000000 -0200
+--- src/util/sys_defs.h.orig	2011-03-01 19:35:19.000000000 -0500
++++ src/util/sys_defs.h	2011-03-01 19:36:54.000000000 -0500
 @@ -25,7 +25,7 @@
    */
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
@@ -9,13 +9,3 @@
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(NETBSD1) || defined(NETBSD2) || defined(NETBSD3) \
-@@ -111,7 +111,8 @@
- #define HAS_DUPLEX_PIPE			/* 4.1 breaks with kqueue(2) */
- #endif
- 
--#if __FreeBSD_version >= 800107		/* safe; don't believe the experts */
-+#if (__FreeBSD_version >= 702104 && __FreeBSD_version <= 800000) \
-+	|| __FreeBSD_version >= 800100
- #define HAS_CLOSEFROM
- #endif
- 
