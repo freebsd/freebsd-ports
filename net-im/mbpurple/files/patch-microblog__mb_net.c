4f5edea7ff80b370708d63f497b5ff07937d3ed9
Doug Barton <dougb@FreeBSD.org>
2011-03-02 17:42:25 +0000

    Add a patch from the svn repo to handle a crash while using https
    https://code.google.com/p/microblog-purple/issues/detail?id=200
    https://code.google.com/p/microblog-purple/issues/detail?id=212
    
    Obtained from:	e.a.agaf...@gmail.com

---
+++ net-im/mbpurple/files/patch-microblog__mb_net.c
@@ -0,0 +1,19 @@
+diff --git a/microblog/mb_net.c b/microblog/mb_net.c
+index 2752aba..962ac1f 100644
+--- microblog/mb_net.c
++++ microblog/mb_net.c
+@@ -160,13 +160,13 @@ void mb_conn_fetch_url_cb(PurpleUtilFetchUrlData * url_data, gpointer user_data,
+ 	conn_data->fetch_url_data = NULL;
+ 
+ 	if(error_message != NULL) {
+-		mb_conn_data_free(conn_data);
+ 		if(conn_data->handler) {
+ 			retval = conn_data->handler(conn_data, conn_data->handler_data, error_message);
+ 		}
+ 		if(ma->gc != NULL) {
+ 			purple_connection_error_reason(ma->gc, PURPLE_CONNECTION_ERROR_NETWORK_ERROR, error_message);
+ 		}
++        mb_conn_data_free(conn_data);
+ 	} else {
+ 		mb_http_data_post_read(conn_data->response, url_text, len);
+ 		if(conn_data->handler) {
