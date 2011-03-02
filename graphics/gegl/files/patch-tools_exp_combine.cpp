060c82e41608a58d31acd1b201056a4bb5521959
gahr <gahr@FreeBSD.org>
2011-03-02 12:42:51 +0000

    - Add a patch to workaround the lack of log2f in older releases.   NOTE: this should not hit users of CURRENT, 7.4 or 8.2.
    Reported by:	ajtiM <lumiwa@gmail.com>
    		Warren Block <wblock@wonkity.com>
    		Christoph Moench-Tegeder <cmt@burggraben.net>
    PR:		ports/155183

---
+++ graphics/gegl/files/patch-tools_exp_combine.cpp
@@ -0,0 +1,14 @@
+--- tools/exp_combine.cpp.orig	2011-03-02 09:30:13.000000000 +0100
++++ tools/exp_combine.cpp	2011-03-02 09:27:16.000000000 +0100
+@@ -6,6 +6,11 @@
+ #include <cerrno>
+ #include <cmath>
+ 
++#include <sys/param.h>
++#if __FreeBSD_version < 704000 || (__FreeBSD_version > 800000 && __FreeBSD_version < 802000) || (__FreeBSD_version > 900000 && __FreeBSD_version < 900027)
++#define log2f(x) (logf(x) / logf(2))
++#endif
++
+ #include <iostream>
+ 
+ #include <exiv2/image.hpp>
