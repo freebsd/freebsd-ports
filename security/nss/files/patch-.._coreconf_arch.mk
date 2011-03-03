5619f102fb9c3011b493dbf734f7a302d8056c72
kwm <kwm@FreeBSD.org>
2011-03-03 17:55:10 +0000

    Patch a assumtion that uname -m returns the architecture value instead of MACHINE. This makes it unbuildable on platforms where these differ (pc98, powerpc64, sun4v). Use uname -p to fix this.
    PR:		ports/155228
    Submitted by:	Nathan Whitehorn <nwhitehorn@FreeBSD.org>

---
+++ security/nss/files/patch-.._coreconf_arch.mk
@@ -0,0 +1,11 @@
+--- ../coreconf/arch.mk.orig	2011-03-03 18:13:52.000000000 +0100
++++ ../coreconf/arch.mk	2011-03-03 18:14:09.000000000 +0100
+@@ -66,7 +66,7 @@
+ # Attempt to differentiate between sparc and x86 Solaris
+ #
+ 
+-OS_TEST := $(shell uname -m)
++OS_TEST := $(shell uname -p)
+ ifeq ($(OS_TEST),i86pc)
+     OS_RELEASE := $(shell uname -r)_$(OS_TEST)
+ else
