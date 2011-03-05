d94301dcd059e13252a45f26deb5f8d346901551
lwhsu <lwhsu@FreeBSD.org>
2011-03-05 13:10:27 +0000

    - update to 2.5
    PR:		ports/155289
    Submitted by:	Ruslan Mahmatkhanov <cvs-src AT yandex.ru>

--- ftp/py-ftputil/files/patch-setup.py
+++ ftp/py-ftputil/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	Mon Dec 25 09:20:34 2006
-+++ setup.py	Mon Dec 25 09:20:44 2006
-@@ -54,8 +54,6 @@
+--- setup.py.orig	2010-08-14 13:22:25.000000000 +0400
++++ setup.py	2011-03-05 14:52:45.000000000 +0300
+@@ -29,8 +29,6 @@
    version=_version,
    packages=[_package],
    package_dir={_package: ""},
--  data_files=[(_data_target, ["ftputil.txt", "ftputil.html",
--                              "README.txt", "README.html"])],
-   # metadata
+-  data_files=[("doc/ftputil", ["ftputil.txt", "ftputil.html",
+-                               "README.txt", "README.html"])],
+   # Metadata
    author="Stefan Schwarzer",
    author_email="sschwarzer@sschwarzer.net",
