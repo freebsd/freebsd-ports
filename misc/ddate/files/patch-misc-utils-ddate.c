610ceb5c1c9be33cec5ae3ebf73f40d02ff0a33e
Emanuel Haupt <ehaupt@FreeBSD.org>
2011-03-07 11:02:36 +0000

    Update to 2.19

--- misc/ddate/files/patch-misc-utils-ddate.c
+++ misc/ddate/files/patch-misc-utils-ddate.c
@@ -1,11 +1,12 @@
---- misc-utils/ddate.c.orig	Wed Jan 26 16:09:58 2005
-+++ misc-utils/ddate.c	Wed Jan 26 16:10:30 2005
-@@ -61,7 +61,7 @@
+--- ./misc-utils/ddate.c.orig	2010-11-30 11:26:31.000000000 +0100
++++ ./misc-utils/ddate.c	2011-03-07 11:54:40.000000000 +0100
+@@ -61,7 +61,8 @@
  #include <string.h>
  #include <time.h>
  #include <stdio.h>
 -#include "nls.h"
-+#include "../lib/nls.h"
- #include "../defines.h"		/* for util-linux-version */
++#include "../include/nls.h"
++#include "../config.h"
  
  #ifndef __GNUC__
+ #define inline /* foo */
