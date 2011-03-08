1fcf359d66e64f8b985790c7827b35403c9c6289
Wen Heping <wen@FreeBSD.org>
2011-03-08 00:27:28 +0000

    - Fix build with python-2.7
    Reported by:	pointyhat@

--- databases/py-bsddb3/files/patch-setup2.py
+++ databases/py-bsddb3/files/patch-setup2.py
@@ -1,6 +1,14 @@
 --- setup2.py.orig	2010-11-05 06:19:08.000000000 +0800
-+++ setup2.py	2011-01-10 16:51:14.000000000 +0800
-@@ -50,10 +50,10 @@
++++ setup2.py	2011-03-08 08:12:34.000000000 +0800
+@@ -36,7 +36,6 @@
+                         'Please use capsule objects instead.',
+                 category=DeprecationWarning)
+ 
+-            import bsddb  # Import the 2.7 version, that uses CObject
+         finally :
+             context.__exit__()
+ 
+@@ -50,10 +49,10 @@
              category=DeprecationWarning)
  
  
@@ -15,7 +23,7 @@
  
  from distutils.dep_util import newer
  import distutils.ccompiler
-@@ -248,8 +248,8 @@
+@@ -248,8 +247,8 @@
          incdir = os.path.join(BERKELEYDB_DIR, 'include')
      if not libdir:
          libdir = os.path.join(BERKELEYDB_DIR, 'lib')
