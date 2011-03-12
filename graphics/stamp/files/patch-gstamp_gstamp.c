5ca3204a8a1480d69c94ef3ef3fe8b2df7ea2748
Christian Weisgerber <naddy@FreeBSD.org>
2011-03-12 20:37:56 +0000

    * Remove need for gmake (and it breaks with 3.82). * Add missing includes and fix LP64 warnings.

---
+++ graphics/stamp/files/patch-gstamp_gstamp.c
@@ -0,0 +1,10 @@
+--- gstamp/gstamp.c.orig	2011-03-12 21:29:21.000000000 +0100
++++ gstamp/gstamp.c	2011-03-12 21:29:42.000000000 +0100
+@@ -7,6 +7,7 @@
+ 
+ #include <gtk/gtk.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <getopt.h>
+ #include "gstamp.h"
---
+++ graphics/stamp/files/patch-gstamp_gstamp_ui.c
@@ -0,0 +1,7 @@
+--- gstamp/gstamp_ui.c.orig	2011-03-12 21:31:56.000000000 +0100
++++ gstamp/gstamp_ui.c	2011-03-12 21:32:20.000000000 +0100
+@@ -1,3 +1,4 @@
++#include <string.h>
+ #include <gtk/gtk.h>
+ #include "gstamp.h"
+ 
