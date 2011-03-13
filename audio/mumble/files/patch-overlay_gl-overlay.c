f3103c6f601ae9b3343b9f83daa51bc6714dfce0
Max Brazhnikov <makc@FreeBSD.org>
2011-03-13 14:55:51 +0000

    - Update to 1.2.3 - Add option to build against a system-wide CELT instead of using   the built-in one.
    PR:		ports/155324
    Submitted by:	Mark Felder <feld at feld.me>
    Approved by:	maintainer

---
+++ audio/mumble/files/patch-overlay_gl-overlay.c
@@ -0,0 +1,10 @@
+--- overlay_gl/overlay.c~	2011-02-19 16:35:15.000000000 -0500
++++ overlay_gl/overlay.c	2011-02-27 16:04:32.000000000 -0500
+@@ -45,6 +45,7 @@
+ #include <sys/ipc.h>
+ #include <sys/time.h>
+ #include <sys/socket.h>
++#include <sys/stat.h>
+ #include <sys/un.h>
+ #include <time.h>
+ #include <semaphore.h>
--- audio/mumble/files/patch-src-mumble-Audio.cpp
+++ audio/mumble/files/patch-src-mumble-Audio.cpp
@@ -1,27 +1,29 @@
---- src/mumble/Audio.cpp.orig	2010-01-08 00:37:46.000000000 +0200
-+++ src/mumble/Audio.cpp	2010-01-11 01:24:46.000000000 +0200
-@@ -49,6 +49,7 @@ CodecInit ciInit;
- void CodecInit::initialize() {
- 	CELTCodec *codec = NULL;
+--- ../mumble-1.2.3/src/mumble/Audio.cpp	2011-02-19 16:35:16.000000000 -0500
++++ src/mumble/Audio.cpp	2011-03-01 16:46:50.000000000 -0500
+@@ -65,6 +65,7 @@
+ 		}
+ 	}
  
-+#ifndef __FreeBSD__
- 	codec = new CELTCodec061(QLatin1String("0.6.1"));
++#if defined(__FreeBSD__) && !defined(FREEBSD_SYSTEM_CELT)
+ 	codec = new CELTCodec080(QLatin1String("0.9.0"));
  	if (codec->isValid()) {
  		codec->report();
-@@ -64,6 +65,7 @@ void CodecInit::initialize() {
- 	} else {
- 		delete codec;
+@@ -102,6 +103,7 @@
+ 			delete codec;
+ 		}
  	}
 +#endif
+ }
  
- 	codec = new CELTCodec070(QLatin1String("0.7.0"));
- 	if (codec->isValid()) {
-@@ -105,6 +107,8 @@ CELTCodec::CELTCodec(const QString &vers
- #if defined(Q_OS_MAC)
- 	alternatives << QString::fromLatin1("celt.%1.dylib").arg(version);
+ void CodecInit::destroy() {
+@@ -132,6 +134,10 @@
+ 	alternatives << QString::fromLatin1("celt0.%1.dylib").arg(version);
  	alternatives << QString::fromLatin1("libcelt.%1.dylib").arg(version);
-+#elif defined(__FreeBSD__)
+ 	alternatives << QString::fromLatin1("celt.%1.dylib").arg(version);
++#elif defined(__FreeBSD__) && defined(FREEBSD_SYSTEM_CELT)
 +	alternatives << QString::fromLatin1("libcelt.so.0");
++#elif defined(__FreeBSD__)
++	alternatives << QString::fromLatin1("libcelt-mumble.so.%1").arg(version);
  #elif defined(Q_OS_UNIX)
- 	alternatives << QString::fromLatin1("celt.so.%1").arg(version);
+ 	alternatives << QString::fromLatin1("libcelt0.so.%1").arg(version);
  	alternatives << QString::fromLatin1("libcelt.so.%1").arg(version);
