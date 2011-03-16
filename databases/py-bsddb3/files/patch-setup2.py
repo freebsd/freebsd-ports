d963c7dca27074c6a097997a3dd79cdd9d1b2135
Wen Heping <wen@FreeBSD.org>
2011-03-16 00:10:57 +0000

    - Update to 5.1.2 - Restore the patch to fix build with bsddb other than 4.2

--- databases/py-bsddb3/files/patch-setup2.py
+++ databases/py-bsddb3/files/patch-setup2.py
@@ -1,14 +1,6 @@
---- setup2.py.orig	2010-11-05 06:19:08.000000000 +0800
-+++ setup2.py	2011-03-08 08:12:34.000000000 +0800
-@@ -36,7 +36,6 @@
-                         'Please use capsule objects instead.',
-                 category=DeprecationWarning)
- 
--            import bsddb  # Import the 2.7 version, that uses CObject
-         finally :
-             context.__exit__()
- 
-@@ -50,10 +49,10 @@
+--- setup2.py.orig	2011-03-08 22:16:14.000000000 +0800
++++ setup2.py	2011-03-15 16:00:42.000000000 +0800
+@@ -53,10 +53,7 @@
              category=DeprecationWarning)
  
  
@@ -16,21 +8,18 @@
 -  from setuptools import setup, Extension
 -except :
 -  from distutils.core import setup, Extension
-+#try :
-+#  from setuptools import setup, Extension
-+#except :
 +from distutils.core import setup, Extension
  
  from distutils.dep_util import newer
  import distutils.ccompiler
-@@ -248,8 +247,8 @@
+@@ -247,8 +244,8 @@
          incdir = os.path.join(BERKELEYDB_DIR, 'include')
      if not libdir:
          libdir = os.path.join(BERKELEYDB_DIR, 'lib')
 -    if not '-ldb' in LIBS:
 -        libname = [dblib]
-+    if not '-ldb-4.2' in LIBS:
-+        libname = ['db-4.2']
++    if not '-l%%DBVERSION%%' in LIBS:
++        libname = ['%%DBVERSION%%']
      else:
          if debug: print "LIBS already contains '-ldb' not adding our own", "'-l"+dblib+"'"
          libname = []
