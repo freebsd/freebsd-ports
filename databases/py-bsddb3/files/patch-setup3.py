1da67ebef2015d70460343fcd09fd3ef33a2734f
wen <wen@FreeBSD.org>
2011-03-16 00:10:57 +0000

    - Update to 5.1.2 - Restore the patch to fix build with bsddb other than 4.2

--- databases/py-bsddb3/files/patch-setup3.py
+++ databases/py-bsddb3/files/patch-setup3.py
@@ -1,13 +1,25 @@
---- setup3.py.orig	2009-06-30 14:58:24.819924337 +0800
-+++ setup3.py	2009-06-30 14:59:39.207070800 +0800
-@@ -204,8 +204,8 @@
+--- setup3.py.orig	2011-03-15 16:10:41.000000000 +0800
++++ setup3.py	2011-03-15 16:12:40.000000000 +0800
+@@ -53,10 +53,7 @@
+             category=DeprecationWarning)
+ 
+ 
+-try :
+-  from setuptools import setup, Extension
+-except :
+-  from distutils.core import setup, Extension
++from distutils.core import setup, Extension
+ 
+ from distutils.dep_util import newer
+ import distutils.ccompiler
+@@ -247,8 +244,8 @@
          incdir = os.path.join(BERKELEYDB_DIR, 'include')
      if not libdir:
          libdir = os.path.join(BERKELEYDB_DIR, 'lib')
 -    if not '-ldb' in LIBS:
 -        libname = [dblib]
 +    if not '-l%%DBVERSION%%' in LIBS:
-+        libname = ['%%DBVERSION%%']
++        libname = [%%DBVERSION%%]
      else:
          if debug: print("LIBS already contains '-ldb' not adding our own", "'-l"+dblib+"'")
          libname = []
