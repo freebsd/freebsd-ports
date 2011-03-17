b64ae76c932b14e06355e28b5161c09e95e2b325
Martin Wilke <miwi@FreeBSD.org>
2011-03-17 15:47:11 +0000

    - Update to 0.5.1 - Pet Portlint
    PR:		155580
    Submitted by:	Ethan Fouts <freebsd.ports@efplace.com>

--- www/mod_jail/files/patch-mod_jail.c
+++ www/mod_jail/files/patch-mod_jail.c
@@ -1,54 +1,11 @@
---- ./mod_jail.c.orig	2007-02-15 12:37:06.000000000 +0000
-+++ ./mod_jail.c	2008-12-28 15:39:43.000000000 +0000
-@@ -130,7 +130,11 @@
-     p_jail_cfg_t cfg = (p_jail_cfg_t) ap_pcalloc(p, sizeof(jail_cfg_t));
- 
-     cfg->jail_scrlevel = 3; /* good default value */
-+#if ((__FreeBSD_version >= 800000 && __FreeBSD_version < 800056) || __FreeBSD_version < 701103)
-     cfg->jail.version = 0;
-+#else
-+    cfg->jail.version = JAIL_API_VERSION;
-+#endif
-     return (void *)cfg;
- }
- 	
-@@ -190,7 +194,13 @@
-     if (!inet_aton(arg, &in)) {
- 	return "could not make sense of jail ip address";
+--- mod_jail.c.orig	2009-09-24 02:05:27.000000000 -0700
++++ mod_jail.c	2010-10-20 15:16:31.000000000 -0700
+@@ -391,7 +391,7 @@
+     if (p_addr == NULL) {
+         return NULL;
      }
-+#if ((__FreeBSD_version >= 800000 && __FreeBSD_version < 800056) || __FreeBSD_version < 701103)
-     cfg->jail.ip_number = ntohl(in.s_addr);
-+#else
-+    cfg->jail.ip4s = 1;
-+    cfg->jail.ip4 = ap_pcalloc(cmd->pool, sizeof(struct in_addr));
-+    cfg->jail.ip4[0].s_addr = in.s_addr;
-+#endif
- 
-     return NULL;
- }
-@@ -321,7 +331,11 @@
- 	return NULL;
-     }
-     cfg->jail_scrlevel = 3; /* good default value */
-+#if ((__FreeBSD_version >= 800000 && __FreeBSD_version < 800056) || __FreeBSD_version < 701103)
-     cfg->jail.version = 0;
-+#else
-+    cfg->jail.version = JAIL_API_VERSION;
-+#endif
-     return (void*)cfg;
- }
- 	
-@@ -378,7 +392,13 @@
-     if (!inet_aton(arg, &in)) {
- 	return "could not make sense of jail ip address";
-     }
-+#if ((__FreeBSD_version >= 800000 && __FreeBSD_version < 800056) || __FreeBSD_version < 701103)
-     cfg->jail.ip_number = ntohl(in.s_addr);
-+#else
-+    cfg->jail.ip4s = 1;
-+    cfg->jail.ip4 = ap_pcalloc(cmd->pool, sizeof(struct in_addr));
-+    cfg->jail.ip4[0].s_addr = in.s_addr;
-+#endif
- 
-     return NULL;
- }
+-    addr->s_addr = htonl(INADDR_LOOPBACK);
++    p_addr->s_addr = htonl(INADDR_LOOPBACK);
+     cfg->jail = (struct jail) {
+ 	.version = JAIL_API_VERSION,
+ 	.path = NULL,
