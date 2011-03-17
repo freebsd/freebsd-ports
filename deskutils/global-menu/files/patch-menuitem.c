1171d1b7877bf498415b8a375c3ed7face30dedb
pav <pav@FreeBSD.org>
2011-03-17 17:24:07 +0000

    - Update to 0.7.10
    PR:		ports/152678
    Submitted by:	Ivan Klymenko <fidaj@ukr.net>
    Approved by:	maintainer

--- deskutils/global-menu/files/patch-menuitem.c
+++ deskutils/global-menu/files/patch-menuitem.c
@@ -1,9 +1,9 @@
---- libgnomenu/menuitem.c.old	2009-06-03 12:00:59.000000000 +0200
-+++ libgnomenu/menuitem.c	2009-06-03 12:01:05.000000000 +0200
-@@ -128,7 +128,7 @@
+--- libserver/widgets/menuitem.c.orig	2010-11-29 16:07:02.000000000 +0200
++++ libserver/widgets/menuitem.c	2010-11-29 16:07:09.000000000 +0200
+@@ -358,7 +358,7 @@
+ 		_tmp0_ = FALSE;
  	}
- 	if (_tmp0) {
- 		g_debug ("menuitem.vala:81: maybe?");
+ 	if (_tmp0_) {
 -		if (gtk_menu_item_get_submenu ((GtkMenuItem*) self) != GTK_MENU (self->_submenu_cache)) {
 +		if (gtk_menu_item_get_submenu ((GtkMenuItem*) self) != (GtkWidget*) GTK_MENU (self->_submenu_cache)) {
  			gtk_menu_item_set_submenu ((GtkMenuItem*) self, (GtkMenu*) self->_submenu_cache);
