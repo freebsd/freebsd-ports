c83691272a462706c4ee7a2d6c4a6380871860b1
decke <decke@FreeBSD.org>
2011-03-18 14:07:06 +0000

    - Improve fix to work with RubyGems 1.6.x
    RubyGems 1.6.x is incompatible with the bundled rails 2.3.5. It's
    documented in the RubyGems release notes that "RubyGems no longer requires
    'thread'. Rails < 3 will need to add require 'thread' to their applications."
    
    PR:		ports/155492
    Reported by:	Thomas Sandford <freebsduser@paradisegreen.co.uk>
    Thanks to:	Thomas Sandford <freebsduser@paradisegreen.co.uk>

--- www/redmine/files/patch-config-boot.rb
+++ www/redmine/files/patch-config-boot.rb
@@ -1,6 +1,15 @@
---- config/boot.rb.orig	2010-12-23 11:22:21.000000000 +0100
-+++ config/boot.rb	2010-12-23 14:14:20.000000000 +0100
-@@ -110,13 +110,13 @@
+--- config/boot.rb.orig	2011-03-07 20:44:40.000000000 +0100
++++ config/boot.rb	2011-03-17 21:15:51.000000000 +0100
+@@ -1,6 +1,8 @@
+ # Don't change this file!
+ # Configure your app in config/environment.rb and config/environments/*.rb
+ 
++require 'thread'
++
+ RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
+ 
+ module Rails
+@@ -110,13 +112,13 @@
  # Loads i18n 0.4.2 before Rails loads any more recent gem
  # 0.5.0 is not compatible with the old interpolation syntax
  # Plugins will have to migrate to the new syntax for 1.2.0
