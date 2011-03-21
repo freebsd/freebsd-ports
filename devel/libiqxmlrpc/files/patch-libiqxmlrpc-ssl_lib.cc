5ff25c0bf121f448cb55f9a6aeaa8e9e28ac700f
Max Khon <fjoe@FreeBSD.org>
2011-03-21 18:17:07 +0000

    Fix build with openssl < 1.0.

--- devel/libiqxmlrpc/files/patch-libiqxmlrpc-ssl_lib.cc
+++ devel/libiqxmlrpc/files/patch-libiqxmlrpc-ssl_lib.cc
@@ -1,20 +1,22 @@
---- libiqxmlrpc/ssl_lib.cc.orig	2011-03-21 23:16:31.000000000 +0600
-+++ libiqxmlrpc/ssl_lib.cc	2011-03-21 23:16:46.000000000 +0600
-@@ -55,7 +55,7 @@
+--- libiqxmlrpc/ssl_lib.cc.orig	2010-08-14 02:12:36.000000000 +0700
++++ libiqxmlrpc/ssl_lib.cc	2011-03-22 00:13:33.000000000 +0600
+@@ -55,8 +55,7 @@
  {
    init_library();
  
 -  SSL_METHOD* meth = client ? SSLv23_method() : SSLv23_server_method();
-+  const SSL_METHOD* meth = client ? SSLv23_method() : SSLv23_server_method();
-   ctx = SSL_CTX_new( meth );
+-  ctx = SSL_CTX_new( meth );
++  ctx = SSL_CTX_new( client ? SSLv23_method() : SSLv23_server_method() );
  
    if(
-@@ -71,7 +71,7 @@
+     !SSL_CTX_use_certificate_file( ctx, cert_path.c_str(), SSL_FILETYPE_PEM ) ||
+@@ -71,8 +70,7 @@
  {
    init_library();
  
 -  SSL_METHOD *meth = SSLv23_client_method();
-+  const SSL_METHOD *meth = SSLv23_client_method();
-   ctx = SSL_CTX_new( meth );
+-  ctx = SSL_CTX_new( meth );
++  ctx = SSL_CTX_new( SSLv23_client_method() );
  }
+ 
  
