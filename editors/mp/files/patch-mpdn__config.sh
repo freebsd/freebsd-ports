05286d6fde61bf59cad9b9e63784882255b03300
dhn <dhn@FreeBSD.org>
2011-03-23 17:54:43 +0000

    - Update to 5.1.4
    PR:		ports/155809
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ editors/mp/files/patch-mpdn__config.sh
@@ -0,0 +1,38 @@
+--- mpdm/config.sh.orig	2009-11-05 17:18:27.000000000 +0900
++++ mpdm/config.sh	2011-03-19 08:40:21.000000000 +0900
+@@ -309,13 +309,15 @@
+ 		echo "#define CONFOPT_GETTEXT 1" >> config.h
+ 	else
+ 		# try now with -lintl
+-		TMP_LDFLAGS="-lintl"
++		TMP_CFLAGS="-I%%LOCALBASE%%/include"
++		TMP_LDFLAGS="-L%%LOCALBASE%%/lib -lintl"
+ 
+-		$CC .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
++		$CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
+ 
+ 		if [ $? = 0 ] ; then
+ 			echo "OK (libintl needed)"
+ 			echo "#define CONFOPT_GETTEXT 1" >> config.h
++			echo "$TMP_CFLAGS" >> config.cflags
+ 			echo "$TMP_LDFLAGS" >> config.ldflags
+ 		else
+ 			echo "No"
+@@ -342,13 +344,15 @@
+ 		echo "#define CONFOPT_ICONV 1" >> config.h
+ 	else
+ 		# try now with -liconv
+-		TMP_LDFLAGS="-liconv"
++		TMP_CFLAGS="-I%%LOCALBASE%%/include -liconv"
++		TMP_LDFLAGS="-L%%LOCALBASE%%/lib -liconv"
+ 
+-		$CC .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
++		$CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
+ 
+ 		if [ $? = 0 ] ; then
+ 			echo "OK (libiconv needed)"
+ 			echo "#define CONFOPT_ICONV 1" >> config.h
++			echo "$TMP_CFLAGS" >> config.cflags
+ 			echo "$TMP_LDFLAGS" >> config.ldflags
+ 		else
+ 			echo "No"
