b7dcbee1665f3cbf4dbf366b340586d3a8f52db0
Martin Wilke <miwi@FreeBSD.org>
2011-03-25 11:19:21 +0000

    - Remove DEPRECATED - Update to 0.2.9 - Update WWW
    PR:		155868
    Submitted by:	Ports Fury

--- net/vyqchat/files/patch-src-settings.cpp
+++ net/vyqchat/files/patch-src-settings.cpp
@@ -1,13 +1,21 @@
---- src/settings.cpp	Mon Jul 18 21:54:08 2005
-+++ src/settings.cpp	Sun Sep 18 21:20:55 2005
-@@ -14,8 +14,9 @@
+--- src/settings.cpp.orig	2006-05-16 05:34:35.000000000 +0900
++++ src/settings.cpp	2011-03-16 13:57:41.000000000 +0900
+@@ -12,14 +12,14 @@
+ 
+ #include "global.h"
  #include "settings.h"
++#include <stdlib.h>
  #include <unistd.h>
  #include <sys/ioctl.h>
 -#include <net/if.h>
- #include <sys/socket.h>
-+#include <net/if.h>
-+#include <netinet/in.h>
+-#include <netinet/in.h>
+-#include <sys/socket.h>
  #include <sys/types.h>
++#include <sys/socket.h>
++#include <netinet/in.h>
  #include <arpa/inet.h>
- #include <stdlib.h>
+-#include <stdlib.h>
++#include <net/if.h>
+ 
+ Settings::Settings() /*{{{*/
+ {
