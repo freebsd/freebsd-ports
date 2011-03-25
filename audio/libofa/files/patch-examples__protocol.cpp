93a66d989164076c52f91c16d1512dd113242bbc
Martin Wilke <miwi@FreeBSD.org>
2011-03-25 10:35:37 +0000

    - Allow build with non-base gcc [1] - Respect CFLAGS/CXXFLAGS - Bump PORTREVISION since port was building with -g previously
    PR:		155662
    Submitted by:	Jason E. Hale <bsdkaffee@gmail.com> (maintainer)
    		[1] avg@

---
+++ audio/libofa/files/patch-examples__protocol.cpp
@@ -0,0 +1,10 @@
+--- ./examples/protocol.cpp.orig	2011-03-18 11:01:22.000000000 -0400
++++ ./examples/protocol.cpp	2011-03-18 11:01:22.000000000 -0400
+@@ -8,6 +8,7 @@
+ -------------------------------------------------------------------*/
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <string>
+ #include <map>
+ #include <expat.h>
