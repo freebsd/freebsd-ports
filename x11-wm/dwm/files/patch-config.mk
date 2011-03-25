f3b1a865e604b41367d1209acdb01f233bcbabd2
dhn <dhn@FreeBSD.org>
2011-03-25 13:43:01 +0000

    - Bump PORTREVISION - Add Xinerama support - Add pkg-message
    PR:		ports/155930
    Submitted by:	Jeroen Schot <schot@A-Eskwadraat.nl> (maintainer)

--- x11-wm/dwm/files/patch-config.mk
+++ x11-wm/dwm/files/patch-config.mk
@@ -1,26 +1,28 @@
---- config.mk.orig	2010-05-28 12:43:17.000000000 +0200
-+++ config.mk	2010-05-28 14:05:14.000000000 +0200
+--- config.mk.orig	2011-03-25 12:53:06.000000000 +0100
++++ config.mk	2011-03-25 12:54:25.000000000 +0100
 @@ -4,11 +4,11 @@
  # Customize below to fit your system
  
  # paths
 -PREFIX = /usr/local
 -MANPREFIX = ${PREFIX}/share/man
-+PREFIX?= /usr/local
-+MANPREFIX = ${PREFIX}/man
++PREFIX = %%PREFIX%% 
++MANPREFIX = %%MANPREFIX%%
  
 -X11INC = /usr/X11R6/include
 -X11LIB = /usr/X11R6/lib
-+X11INC = $(LOCALBASE)/include
-+X11LIB = $(LOCALBASE)/lib
++X11INC = %%LOCALBASE%%/include
++X11LIB = %%LOCALBASE%%/lib
  
  # Xinerama
  XINERAMALIBS = -L${X11LIB} -lXinerama
-@@ -20,9 +20,9 @@
+@@ -19,10 +19,10 @@
+ LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${XINERAMALIBS}
  
  # flags
- CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+-CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 -CFLAGS = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
++CPPFLAGS+= -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 +CFLAGS+= -std=c99 ${INCS} ${CPPFLAGS}
  #CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 -LDFLAGS = -g ${LIBS}
