6433b5b2e26eab09f4e8ca0e81fc7469b2963133
makc <makc@FreeBSD.org>
2011-04-05 19:08:30 +0000

    Update to 0.7.0 [1] Remove needless dependency on textproc/docbook-xml
    PR:		ports/156184 [1]
    Submitted by:	Alexander Pyhalov <alp at sfedu.ru>

--- www/rekonq/files/patch-src__main.cpp
+++ www/rekonq/files/patch-src__main.cpp
@@ -1,11 +1,11 @@
---- ./src/main.cpp.orig	2010-10-05 18:03:58.000000000 +0400
-+++ ./src/main.cpp	2010-11-07 16:58:54.850117314 +0300
-@@ -191,7 +191,7 @@
-     }
+--- src/main.cpp	2011-04-03 12:26:13.000000000 +0400
++++ src/main.cpp	2011-04-05 00:05:26.578186110 +0400
+@@ -207,7 +207,7 @@
+ 
  #if defined(Q_WS_X11)
      // On X11, the raster engine gives better performance than native.
--    QApplication::setGraphicsSystem(QString::fromLatin1("raster"));
-+    // QApplication::setGraphicsSystem(QString::fromLatin1("raster"));
+-    QApplication::setGraphicsSystem(QL1S("raster"));
++//    QApplication::setGraphicsSystem(QL1S("raster"));
  #endif
-     Application app;
  
+     Application app;
