ed1c28e9aabc3edc12de46b14aa07329977d32a0
avilla <avilla@FreeBSD.org>
2011-04-07 12:03:34 +0000

    The FreeBSD KDE Team is pleased to announce April updates for KDE Software Compilation: 4.6.2, codename "Congrats". Read the full announcement here: http://kde.org/announcements/announce-4.6.2.php.
    Special thanks to Raphael Kubo da Costa who ported the release.

--- Mk/bsd.kde4.mk
+++ Mk/bsd.kde4.mk
@@ -54,7 +54,7 @@ Kde_Include_MAINTAINER=	kde@FreeBSD.org
 #		but this could be changed in a future.
 #
 
-KDE4_VERSION=		4.6.1
+KDE4_VERSION=		4.6.2
 KDE4_BRANCH?=		stable
 KDEPIM4_VERSION=	4.4.10
 KDEPIM4_BRANCH?=	stable
