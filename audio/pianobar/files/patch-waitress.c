0285b63406875e8250ed6b55cb9fda0186b5405b
jpaetzel <jpaetzel@FreeBSD.org>
2011-04-08 12:36:57 +0000

    Update to 2011.01.24
    PR:	ports/156262
    Submitted by:	John Hixson <john@pcbsd.org>

--- audio/pianobar/files/patch-waitress.c
+++ audio/pianobar/files/patch-waitress.c
@@ -1,5 +1,5 @@
---- libwaitress/src/waitress.c.orig	2010-10-07 01:54:32.000000000 -0700
-+++ libwaitress/src/waitress.c	2010-10-08 14:51:41.000000000 -0700
+--- src/libwaitress/waitress.c.orig	2011-01-24 04:00:25.000000000 -0800
++++ src/libwaitress/waitress.c	2011-04-07 14:29:37.000000000 -0700
 @@ -21,9 +21,6 @@
  THE SOFTWARE.
  */
