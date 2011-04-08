077aba1989be6635b07e5b9e692442df044984d5
Josh Paetzel <jpaetzel@FreeBSD.org>
2011-04-08 12:36:57 +0000

    Update to 2011.01.24
    PR:	ports/156262
    Submitted by:	John Hixson <john@pcbsd.org>

--- audio/pianobar/files/patch-terminal.c
+++ audio/pianobar/files/patch-terminal.c
@@ -1,5 +1,5 @@
---- src/terminal.c.orig	2010-10-07 01:54:32.000000000 -0700
-+++ src/terminal.c	2010-10-08 14:51:21.000000000 -0700
+--- src/terminal.c.orig	2011-01-24 04:00:25.000000000 -0800
++++ src/terminal.c	2011-04-07 14:29:03.000000000 -0700
 @@ -21,9 +21,6 @@
  THE SOFTWARE.
  */
@@ -9,4 +9,3 @@
 -
  #include <termios.h>
  #include <stdio.h>
- 
