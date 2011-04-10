82c9048d6e634f72446fc470a89b6ebc44d452f5
makc <makc@FreeBSD.org>
2011-04-10 20:10:19 +0000

    Update to 2.4.1
    PR:		based on ports/156055
    Submitted by:	Gvozdikov Veniamin (maintainer)

--- graphics/zint/files/patch-backend_code1.c
+++ graphics/zint/files/patch-backend_code1.c
@@ -1,8 +1,5 @@
-
-$FreeBSD$
-
---- backend/code1.c.orig
-+++ backend/code1.c
+--- backend/code1.c.orig	2011-03-30 15:18:25.000000000 +0800
++++ backend/code1.c	2011-03-30 15:18:35.000000000 +0800
 @@ -28,7 +28,6 @@
  #ifdef __APPLE__
  #include <sys/malloc.h>
--- graphics/zint/files/patch-backend_maxicode.c
+++ graphics/zint/files/patch-backend_maxicode.c
@@ -1,8 +1,5 @@
-
-$FreeBSD$
-
---- backend/maxicode.c.orig
-+++ backend/maxicode.c
+--- backend/maxicode.c.orig	2011-03-30 15:17:49.000000000 +0800
++++ backend/maxicode.c	2011-03-30 15:18:15.000000000 +0800
 @@ -29,7 +29,6 @@
  #ifdef __APPLE__
  #include <sys/malloc.h> 
