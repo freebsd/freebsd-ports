f6fe2d99d737ec0f28bbb94a9882455fed922472
Max Brazhnikov <makc@FreeBSD.org>
2011-04-10 20:00:33 +0000

    Update to 0.9.9.2
    PR:		ports/156052
    Submitted by:	Gvozdikov Veniamin (maintainer)

--- graphics/scantailor/files/patch-ProjectFilesDialog.cpp
+++ graphics/scantailor/files/patch-ProjectFilesDialog.cpp
@@ -1,30 +1,31 @@
---- ProjectFilesDialog.cpp.orig	2010-04-03 23:47:33.000000000 +0400
-+++ ProjectFilesDialog.cpp	2010-12-10 20:05:23.000000000 +0300
-@@ -677,6 +677,17 @@
+--- ProjectFilesDialog.cpp.orig	2011-03-29 23:54:33.000000000 +0800
++++ ProjectFilesDialog.cpp	2011-03-30 00:04:45.000000000 +0800
+@@ -677,6 +677,17 @@ ProjectFilesDialog::FileList::clear()
  	endRemoveRows();
  }
  
 +namespace {
-+  struct less_for_first_members_of_pairs
-+  {
-+    template <typename Pair>
-+    bool operator()(Pair const& left, Pair const& right) const
-+    {
-+      return left.first < right.first;
-+    }
-+  };
++	struct less_for_first_members_of_pairs
++	{
++		template <typename Pair>
++		bool operator()(Pair const& left, Pair const& right) const
++	{
++	return left.first < right.first;
++	}
++	};
 +}
 +
  void
  ProjectFilesDialog::FileList::remove(QItemSelection const& selection)
  {
-@@ -699,7 +710,8 @@
- 	);
+@@ -703,7 +714,9 @@ ProjectFilesDialog::FileList::remove(QIt
+ 	
  	std::sort(
  		sorted_ranges.begin(), sorted_ranges.end(),
--		bind(&Range::first, _1) < bind(&Range::first, _2)
-+		// bind(&Range::first, _1) < bind(&Range::first, _2)
+-		bind((IntMemPtr)&Range::first, _1) < bind((IntMemPtr)&Range::first, _2)
++		//bind((IntMemPtr)&Range::first, _1) < bind((IntMemPtr)&Range::first, _2)
 +		less_for_first_members_of_pairs()
++
  	);
  	
  	QVectorIterator<Range> it(sorted_ranges);
