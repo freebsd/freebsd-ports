b8c1451e7e28b790f42206c86d7e23fa4c634857
vanilla <vanilla@FreeBSD.org>
2011-04-13 06:42:27 +0000

    1: upgrade to 2.0.1 2: fix compier warnings
    PR:		ports/155154 [2]
    Submitted by:	darth at vader.dk [2]

---
+++ devel/jansson/files/patch-src_hashtable.c
@@ -0,0 +1,56 @@
+--- src/hashtable.c.orig	2011-04-02 22:36:44.000000000 +0800
++++ src/hashtable.c	2011-04-02 22:37:52.000000000 +0800
+@@ -101,10 +101,10 @@ static int hashtable_do_del(hashtable_t 
+ {
+     pair_t *pair;
+     bucket_t *bucket;
+-    size_t index;
++    size_t my_index;
+ 
+-    index = hash % num_buckets(hashtable);
+-    bucket = &hashtable->buckets[index];
++    my_index = hash % num_buckets(hashtable);
++    bucket = &hashtable->buckets[my_index];
+ 
+     pair = hashtable_find_pair(hashtable, bucket, key, hash);
+     if(!pair)
+@@ -153,7 +153,7 @@ static int hashtable_do_rehash(hashtable
+ {
+     list_t *list, *next;
+     pair_t *pair;
+-    size_t i, index, new_size;
++    size_t i, my_index, new_size;
+ 
+     jsonp_free(hashtable->buckets);
+ 
+@@ -176,8 +176,8 @@ static int hashtable_do_rehash(hashtable
+     for(; list != &hashtable->list; list = next) {
+         next = list->next;
+         pair = list_to_pair(list);
+-        index = pair->hash % new_size;
+-        insert_to_bucket(hashtable, &hashtable->buckets[index], &pair->list);
++        my_index = pair->hash % new_size;
++        insert_to_bucket(hashtable, &hashtable->buckets[my_index], &pair->list);
+     }
+ 
+     return 0;
+@@ -244,7 +244,7 @@ int hashtable_set(hashtable_t *hashtable
+ {
+     pair_t *pair;
+     bucket_t *bucket;
+-    size_t hash, index;
++    size_t hash, my_index;
+ 
+     /* rehash if the load ratio exceeds 1 */
+     if(hashtable->size >= num_buckets(hashtable))
+@@ -252,8 +252,8 @@ int hashtable_set(hashtable_t *hashtable
+             return -1;
+ 
+     hash = hashtable->hash_key(key);
+-    index = hash % num_buckets(hashtable);
+-    bucket = &hashtable->buckets[index];
++    my_index = hash % num_buckets(hashtable);
++    bucket = &hashtable->buckets[my_index];
+     pair = hashtable_find_pair(hashtable, bucket, key, hash);
+ 
+     if(pair)
