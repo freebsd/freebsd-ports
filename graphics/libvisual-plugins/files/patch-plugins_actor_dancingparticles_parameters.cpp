490b206354af8f9c667e2ec65ca95e9cb4743f34
rene <rene@FreeBSD.org>
2011-04-18 22:25:41 +0000

    Remove most expired ports: 2011-04-17 cad/tclspice: has been broken for more than a year 2011-04-17 comms/hcfmdm: does not compile on 7.X or higher 2011-04-17 databases/mysqlcc: has been broken for almost a year 2011-04-17 devel/ruby-rjudy: does not compile on FreeBSD 7.x or newer 2011-04-17 devel/xfc: does not compile on FreeBSD 7.x or newer 2011-04-17 devel/lamson: has been broken for a half year 2011-04-17 devel/cocktail: does not build on FreeBSD 7.x or newer 2011-04-17 devel/djgpp-gcc: has been broken for half a year 2011-04-17 devel/gauche-sdl: has been broken for a year 2011-04-17 devel/gdb53-act: does not compile on FreeBSD 7.x and up 2011-04-17 editors/zed: does not compile on FreeBSD 7.x or newer 2011-04-17 games/aqbubble: does not compile on FreeBSD 7.x or newer 2011-04-17 graphics/libvisual-plugins: has been broken for 3 years 2011-04-17 japanese/roundcube: has been broken for almost a year 2011-04-17 japanese/tkstep80: does not compile on FreeBSD 7.x or newer 2011-04-17 lang/u++: has been broken for over a half year 2011-04-17 lang/pugs: has been broken for over a year 2011-04-17 lang/mozart: does not compile on FreeBSD 7.x or newer 2011-04-17 math/linalg: does not build on FreeBSD 7.x or newer 2011-04-17 math/R-cran-igraph: has been broken for over a half year 2011-04-17 misc/ftree: has been broken for over a half year 2011-04-17 multimedia/katchtv: has been broken for a half year 2011-04-17 multimedia/libomxil-bellagio: has been broken for almost a year 2011-04-17 multimedia/banshee-mirage: has been broken for over a half year 2011-04-17 net-p2p/trackerbt: has been broken for a half year 2011-04-17 net/cap: does not compile on FreeBSD 7.x or newer 2011-04-17 net/ggsd: does not compile on FreeBSD 7.x or newer 2011-04-17 net/b2bua: does not compile on FreeBSD 7.x or newer 2011-04-17 net/penguintv: has been broken for a half year 2011-04-17 news/openftd: has been broken for almost a year 2011-04-17 palm/romeo: does not compile on FreeBSD 7.x or newer 2011-04-17 science/pcp: does not compile on FreeBSD 7.x or newer 2011-04-17 science/elmer-fem: has been broken for over a year 2011-04-17 security/newpki-lib: does not compile on FreeBSD 7.x or newer 2011-04-17 security/newpki-server: does not compile on FreeBSD 7.x or newer 2011-04-17 security/xmlsec: does not compile on FreeBSD 7.x or newer 2011-04-17 security/f-protd: has been broken for over a year 2011-04-17 sysutils/xwlans: does not compile on FreeBSD 7.x or newer 2011-04-17 www/bk_edit: does not compile on FreeBSD 7.x and newer 2011-04-17 www/bricolage: has been broken for a half year 2011-04-17 x11-toolkits/gauche-gtk: has been broken for a year 2011-04-17 x11-toolkits/gambas2-gb-qt: has been broken for over a year 2011-04-17 x11-toolkits/php-gtk2: has been broken for over a half year 2011-04-17 x11-toolkits/p5-Tcl-Tk: has been broken for 2 year 2011-04-17 x11/metisse: has been broken for over a half year

--- graphics/libvisual-plugins/files/patch-plugins_actor_dancingparticles_parameters.cpp
+++
@@ -1,9 +0,0 @@
---- plugins/actor/dancingparticles/parameters.cpp.orig
-+++ plugins/actor/dancingparticles/parameters.cpp
-@@ -1,5 +1,6 @@
- #include <iostream>
- #include "etoile.h"
-+#include <locale.h>
- #include <stdio.h>
- #include <stdlib.h>
- #include "t1font.h"
