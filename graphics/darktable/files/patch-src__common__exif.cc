61a6428b8b1ecc4c2ccfed9a015a76cb006dc770
bapt <bapt@FreeBSD.org>
2011-04-24 10:49:49 +0000

    real fix with new exiv version
    Report by:	pointyhat (erwin)

--- graphics/darktable/files/patch-src__common__exif.cc
+++ graphics/darktable/files/patch-src__common__exif.cc
@@ -1,6 +1,6 @@
 --- ./src/common/exif.cc.orig	2010-12-07 12:49:56.000000000 +0100
-+++ ./src/common/exif.cc	2011-04-11 14:54:09.176904295 +0200
-@@ -29,11 +29,8 @@
++++ ./src/common/exif.cc	2011-04-24 12:43:20.476146336 +0200
+@@ -29,11 +29,7 @@
  #include "common/imageio.h"
  }
  // #include <libexif/exif-data.h>
@@ -8,7 +8,7 @@
 -#include <exiv2/error.hpp>
 -#include <exiv2/image.hpp>
 -#include <exiv2/exif.hpp>
- #include <exiv2/canonmn.hpp>
+-#include <exiv2/canonmn.hpp>
 +#include <exiv2/exiv2.hpp>
  #include <sqlite3.h>
  #include <iostream>
