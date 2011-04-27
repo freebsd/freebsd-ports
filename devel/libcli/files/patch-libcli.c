45d5ba5e7c73ce76c065ec9e94e3189bc831ca49
bapt <bapt@FreeBSD.org>
2011-04-27 12:20:22 +0000

    Libcli provides a shared library for including a Cisco-like command-line interface into other software. It's a telnet interface which supports command-line editing, history, authentication and callbacks for a user-definable function tree.
    WWW:	http://github.com/dparrish/libcli/

---
+++ devel/libcli/files/patch-libcli.c
@@ -0,0 +1,10 @@
+--- ./libcli.c.orig	2008-10-09 04:38:01.000000000 +0200
++++ ./libcli.c	2011-04-27 14:02:40.896273703 +0200
+@@ -9,7 +9,6 @@
+ #include <stdarg.h>
+ #include <stdlib.h>
+ #include <memory.h>
+-#include <malloc.h>
+ #include <string.h>
+ #include <ctype.h>
+ #include <unistd.h>
