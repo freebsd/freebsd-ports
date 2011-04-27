b537e59d304ddc389b7d7a20cba4f3f757a95b56
bapt <bapt@FreeBSD.org>
2011-04-27 09:29:03 +0000

    - update to 0.6.0.20110117 cvs snapshot - support for for Japanese JP106 keyboard - add license - Add runtime dependency on Xvnc
    PR:		ports/155509
    Submitted by:	"Iwao, Koichiro" <meta _at_ club.kyutech.ac.jp> (maintainer)

--- net/xrdp/files/patch-common__os_calls.c
+++ net/xrdp/files/patch-common__os_calls.c
@@ -1,6 +1,6 @@
---- ./common/os_calls.c.orig	Sun Jul 30 01:41:31 2006
-+++ ./common/os_calls.c	Mon Jun  4 17:37:42 2007
-@@ -1002,6 +1002,8 @@
+--- common/os_calls.c.orig	2011-03-12 16:10:35.000000000 +0900
++++ common/os_calls.c	2011-03-12 16:34:05.000000000 +0900
+@@ -1987,6 +1987,8 @@
  g_clearenv(void)
  {
  #if defined(_WIN32)
