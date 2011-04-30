c4bd68a60c226b55295a3f350517ba42d4497eed
dhn <dhn@FreeBSD.org>
2011-04-30 19:38:29 +0000

    - Update to 0.3.2
    PR:		ports/156738
    Submitted by:	Mikhail T. <m.tsatsenko@gmail.com> (maintainer)

--- mail/spamass-milter/files/patch-spamass-milter.h
+++
@@ -1,9 +0,0 @@
---- spamass-milter.h.orig	2010-05-12 11:58:14.000000000 +0200
-+++ spamass-milter.h	2010-05-12 12:05:27.000000000 +0200
-@@ -186,5 +186,6 @@
- void parse_debuglevel(char* string);
- char *strlwr(char *str);
- void warnmacro(char *macro, char *scope);
-+FILE *popenv(char *const argv[], const char *type, pid_t *pid);
- 
- #endif
