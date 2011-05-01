f10fbaeaf7cbffe6362c4dc52981f615bd4d980e
bapt <bapt@FreeBSD.org>
2011-05-01 19:52:00 +0000

    Remove unmaintained expired ports from sysutils
    2011-05-01 sysutils/acidlaunch: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/cpbk: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/dc42wrap: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/deleted: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/dolly+: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/durep: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/dvdtape: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/flock: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/fontedit: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/ftrace: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/gfslicer: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/ghasher: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/gkrellmouse: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/gkrellmwho: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/glload: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/gnomefind: Abandoned upstream
    2011-05-01 sysutils/graft: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/growspd: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/gtoaster: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/idled: Upstream disapear and distfile is no more available, consider using sysutils/doinkd
    2011-05-01 sysutils/maint: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/ndir: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/p5-LJ-Cache: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/p5-LJ-TextMessage: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/reclinker: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/rotate: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/ticker.app: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/upsmon: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/usbutil: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/uwatch: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmavgload: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmfsm: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmhm: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wminet: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmlmmon: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmlongrun: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmmemmon: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmmount: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmpccard: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmshutdown: Upstream disapear and distfile is no more available
    2011-05-01 sysutils/wmzazof: Upstream disapear and distfile is no more available

--- sysutils/dolly+/files/patch-Packet.cpp
+++
@@ -1,8 +0,0 @@
---- Packet.cpp.orig	Thu Jul 15 23:37:43 2004
-+++ Packet.cpp	Thu Jul 15 23:37:51 2004
-@@ -1,4 +1,5 @@
- //D is a mark for debuging
-+#include <sys/types.h>
- #include <stdlib.h>
- #include <stdio.h>
- #include <errno.h>
