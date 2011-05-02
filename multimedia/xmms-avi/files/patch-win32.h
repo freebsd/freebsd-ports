42c37c868eeecde4f3cb1447c2bce6dfa819d269
Baptiste Daroussin <bapt@FreeBSD.org>
2011-05-02 06:52:13 +0000

    remove unmaintained expired ports from multimedia
    2011-05-01 multimedia/gtksubtitler: Upstream disapear and distfile is no more available
    2011-05-01 multimedia/hayes: Upstream disapear and distfile is no more available
    2011-05-01 multimedia/linux-libtheora: deprecated please use linux-f[89]-libtheora
    2011-05-01 multimedia/netshow: Distfile is no more available
    2011-05-01 multimedia/smpeg-xmms: Distfile is no more available
    2011-05-01 multimedia/streamdvd: upstream and distfile is no more available
    2011-05-01 multimedia/sublib: Upstream has disapear and distfile is no more available
    2011-05-01 multimedia/tkxanim: Upstream has disapear and dstfile is no more available
    2011-05-01 multimedia/tosvcd: Upstream has disapear and distfile is no more available
    2011-05-01 multimedia/tv_grab_au: Upstream has disapear and distfile is no more available
    2011-05-01 multimedia/xdvshow: upstream has disapear and distfile is no more available
    2011-05-01 multimedia/xmms-avi: Distfile is no more available
    2011-05-01 multimedia/xmms-skins: Distfile is no more available
    2011-05-01 multimedia/xmps: Upstream has disapear and distfile is no more available
    2011-05-01 multimedia/xvid4conf: Upstream has disapear and distfile is no more available

--- multimedia/xmms-avi/files/patch-win32.h
+++
@@ -1,5 +0,0 @@
---- lib/loader/win32.h.orig	Tue Sep 12 18:12:21 2000
-+++ lib/loader/win32.h	Mon Jul  2 02:44:09 2007
-@@ -1 +1 @@
--int ext_unknown();
-+void* LookupExternalByName(const char* library, const char* name);
