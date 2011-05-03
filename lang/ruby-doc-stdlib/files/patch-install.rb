897a749666ba5e839f164ace510168b2b0b69a0e
swills <swills@FreeBSD.org>
2011-05-03 03:05:04 +0000

    - Fix build with Ruby 1.9
    PR:		ports/156506
    Submitted by:	Eric Freeman <freebsdports@chillibear.com>
    Approved by:	maintainer (stas@ via irc)

--- lang/ruby-doc-stdlib/files/patch-install.rb
+++ lang/ruby-doc-stdlib/files/patch-install.rb
@@ -1,11 +1,11 @@
---- install.rb.orig	Fri Aug  4 17:53:04 2006
-+++ install.rb	Fri Aug  4 17:53:10 2006
-@@ -125,7 +125,7 @@
-           Shall I bother?
-         }.trim
-       end
--    elsif Dir["stdlib"] > 0
-+    elsif Dir["stdlib"].size > 0
-       # It's got *something* in it, but not stdlib doc.
-       confirm %{
-         #@doc_base/stdlib contains something, but not what I expected.
+--- install.rb.orig	2011-04-18 19:15:14.000000000 +0100
++++ install.rb	2011-04-18 19:15:34.000000000 +0100
+@@ -83,7 +83,7 @@
+ opts = OptionParser.new do |o|
+   o.program_name = "ruby install.rb"
+   o.separator "Options:"
+-  o.on('--doc-base DIR', '-d', "Install documentation to DIR/stdlib") { |@doc_base| }
++  o.on('--doc-base DIR', '-d', "Install documentation to DIR/stdlib") { |_doc_base| @doc_base = _doc_base}
+   o.on('--force', '-f', "Don't ask any questions; just install") { @force = true }
+   o.on('--interactive', '-i', "Confirm any risky actions") { @force = false }
+   o.on_tail('--help', '-h', "Show this message") do
