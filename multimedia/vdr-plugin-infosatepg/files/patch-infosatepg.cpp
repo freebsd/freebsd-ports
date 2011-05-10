03d1d00ad2cf9c5731c2d3ab434b8420ea87ed19
Juergen Lock <nox@FreeBSD.org>
2011-05-10 17:35:36 +0000

    - Add bugfix for infosatepg grabbing a non-first tuner device that   still was in use. (like when the sat tuner was on /dev/dvb/adapter8.) - Bump PORTREVISION.

--- multimedia/vdr-plugin-infosatepg/files/patch-infosatepg.cpp
+++ multimedia/vdr-plugin-infosatepg/files/patch-infosatepg.cpp
@@ -1,5 +1,14 @@
 --- a/infosatepg.cpp
 +++ b/infosatepg.cpp
+@@ -193,7 +193,7 @@ void cPluginInfosatepg::MainThreadHook(v
+             if (EITScanner.UsesDevice(dev)) continue; // EITScanner is updating EPG -> skip
+             if (dev->Receiving()) continue; // device is recording -> skip
+             if (dev->IsPrimaryDevice()) continue; // device is primary -> skip
+-            if (cDevice::ActualDevice()->CardIndex()==i) continue; // device is live viewing -> skip
++            if (cDevice::ActualDevice()->CardIndex()==dev->CardIndex()) continue; // device is live viewing -> skip
+             if (dev->IsTunedToTransponder(chan))
+             {
+                 // we already have a device which is tuned (maybe switched manually?)
 @@ -403,7 +403,7 @@ cString cPluginInfosatepg::SVDRPCommand(
          cString head2;
          head2="\n" \
