8967e00b5aaa6617b9efd0c314d70c25836935ae
Sylvio Cesar Teixeira <sylvio@FreeBSD.org>
2011-05-11 19:34:28 +0000

    - Update to 3.8.5
    PR:		ports/155020
    Submitted by:	Aleksey V. Gnezdilov <aleksey@vtrek.ru>
    Approved by: 	Raffaele De Lorenzo <raffaele.delorenzo@libero.it> (maintainer)

--- net/ss5/files/patch-src-SS5Utils.c
+++ net/ss5/files/patch-src-SS5Utils.c
@@ -1,26 +1,13 @@
-*** src/SS5Utils.c.orig	Tue Jun 12 17:16:55 2007
---- src/SS5Utils.c	Tue Feb 19 11:49:50 2008
-***************
-*** 130,135 ****
---- 130,140 ----
-      case ALONE:
-        snprintf(logString,256 - 1,"[VERB] Role is ALONE.");
-      break;
-+ 
-+     default:
-+ 	fprintf (stderr, "[WARN] - Don't know Role %s\n", logString);
-+ 	return ERR;
-+ 	break;
-    }
-    LOGUPDATE()
-  
-***************
-*** 152,157 ****
---- 157,163 ----
-     * Open ss5.ha file
-     */
-    if( (S5PeerFile = fopen(S5PeersFile,"r")) == NULL ) {
-+     fprintf (stderr, "[ERR] Cannot Load %s file!\n", S5PeersFile);
-      return ERR;
-    }
-  
+--- src/SS5Utils.c.orig	2009-08-02 17:57:19.000000000 +1100
++++ src/SS5Utils.c	2011-02-25 13:34:42.311808628 +1000
+@@ -117,6 +117,10 @@
+     case ALONE:
+       snprintf(logString,256 - 1,"[VERB] Role is ALONE.");
+     break;
++    default:
++      fprintf (stderr, "[WARN] - Don't know Role %s\n", logString);
++      return ERR;
++    break;
+   }
+   LOGUPDATE()
+ 
