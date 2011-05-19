6c3d088da4b3b5508f728e341725f698e05182c9
gahr <gahr@FreeBSD.org>
2011-05-19 08:32:12 +0000

    - Update to 1.7.26   Changes: http://www.fox-toolkit.org/news.html

--- x11-toolkits/fox17/files/patch-lib_FXThread.cpp
+++ x11-toolkits/fox17/files/patch-lib_FXThread.cpp
@@ -1,6 +1,6 @@
---- lib/FXThread.cpp.orig	2010-09-27 08:48:52.000000000 +0000
-+++ lib/FXThread.cpp	2010-09-27 08:49:43.000000000 +0000
-@@ -582,7 +582,7 @@
+--- lib/FXThread.cpp.orig	2011-05-19 09:10:16.000000000 +0200
++++ lib/FXThread.cpp	2011-05-19 09:10:38.000000000 +0200
+@@ -599,7 +599,7 @@
    InitializeCriticalSection(((RWLOCK*)data)->mutex);
    InitializeCriticalSection(((RWLOCK*)data)->access);
    ((RWLOCK*)data)->readers=0;
