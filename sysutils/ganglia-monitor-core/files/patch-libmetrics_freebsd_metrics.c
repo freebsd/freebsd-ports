75d2eee606eab248808bcfda15889cea1d6028fa
brooks <brooks@FreeBSD.org>
2011-05-20 00:49:12 +0000

    Upgrade to ganglia 6.1.7.
    Drop support for early version of 6.x in the rc script support.

--- sysutils/ganglia-monitor-core/files/patch-libmetrics_freebsd_metrics.c
+++ sysutils/ganglia-monitor-core/files/patch-libmetrics_freebsd_metrics.c
@@ -3,22 +3,14 @@ $FreeBSD$
 
 --- libmetrics/freebsd/metrics.c.orig
 +++ libmetrics/freebsd/metrics.c
-@@ -106,7 +106,7 @@
- static size_t mibswap_size;
- static kvm_t *kd = NULL;
- static int pagesize;
--static int	  skipvfs;
-+static int	  skipvfs = 1;
- 
- /* Function prototypes */
- static long percentages(int cnt, int *out, register long *new,
-@@ -170,6 +170,18 @@
+@@ -170,9 +170,21 @@
     size_t len;
     uint32_t freq = 0, tmpfreq;
     uint64_t tscfreq;
 +   unsigned int cpu_freq;
-+
-+   /*
+ 
+    /*
+-    * If the system supports it, the cpufreq driver provides the best
 +    * Try the portable sysctl (introduced on ia64).
 +    */
 +   cpu_freq = 0;
@@ -28,9 +20,12 @@ $FreeBSD$
 +      freq = cpu_freq;
 +      goto done;
 +   }
- 
-    /*
-     * If the system supports it, the cpufreq driver provides the best
++
++   /*
++    * If the system supports it, the cpufreq driver provides
+     * access to CPU frequency.  Since we want a constant value, we're
+     * looking for the maximum frequency, not the current one.  We
+     * don't know what order the driver will report values in so we
 @@ -529,6 +541,17 @@
        goto output;
  
@@ -49,72 +44,3 @@ $FreeBSD$
  #ifdef KINFO_PROC_SIZE
        state = kp->ki_stat;
  #else
-@@ -881,7 +904,6 @@
- 
- 	netvfslist = makenetvfslist();
- 	vfslist = makevfslist(netvfslist);
--	free(netvfslist);
- 
- 	mntsize = getmntinfo(&mntbuf, MNT_NOWAIT);
- 	mntsize = regetmntinfo(&mntbuf, mntsize, vfslist);
-@@ -899,6 +921,8 @@
- 			*tot_avail += mntbuf[i].f_bavail / toru;
- 		}
- 	}
-+	free(vfslist);
-+	free(netvfslist);
- 
- 	return most_full;
- }
-@@ -955,7 +979,7 @@
- 		return (NULL);
- 	if (fslist[0] == 'n' && fslist[1] == 'o') {
- 		fslist += 2;
--		skipvfs = 1;
-+		skipvfs = 0;
- 	}
- 	for (i = 0, nextcp = fslist; *nextcp; nextcp++)
- 		if (*nextcp == ',')
-@@ -1007,7 +1031,10 @@
- 		goto done;
- 	}
- 
--	for (cnt = 0; cnt < maxvfsconf; xvfsp++) {
-+	cnt = 0;
-+	for (i = 0; i < maxvfsconf; i++, xvfsp++) {
-+		if (xvfsp->vfc_typenum == 0)
-+			continue;
- 		if (xvfsp->vfc_flags & VFCF_NONLOCAL)
- 			continue;
- 
-@@ -1057,10 +1084,13 @@
- 	 * Count up the string lengths, we need a extra byte to hold
- 	 * the between entries ',' or the NUL at the end.
- 	 */
-+	slen = 0;
- 	for (i = 0; i < cnt; i++)
--		slen = strlen(listptr[i]) + 1;
--	/* Add 2 for initial "no". */
--	slen += 2;
-+		slen += strlen(listptr[i]);
-+	/* for ',' */
-+	slen += cnt - 1;
-+	/* Add 3 for initial "no" and the NUL. */
-+	slen += 3;
- 
- 	if ((str = malloc(slen)) == NULL) {
- 		warnx("malloc failed");
-@@ -1069,10 +1099,11 @@
- 
- 	str[0] = 'n';
- 	str[1] = 'o';
--	for (i = 0, strptr = str + 2; i < cnt; i++, strptr++) {
-+	for (i = 0, strptr = str + 2; i < cnt; i++) {
-+		if (i > 0)
-+		    *strptr++ = ',';
- 		strcpy(strptr, listptr[i]);
- 		strptr += strlen(listptr[i]);
--		*strptr = ',';
- 	}
- 	*strptr = '\0';
- 
