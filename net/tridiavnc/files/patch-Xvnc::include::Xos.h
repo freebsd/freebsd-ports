7267a5054c27c081b13c6640ffb6a8f42d0516a6
cy <cy@FreeBSD.org>
2011-05-20 19:51:37 +0000

    Resurrect net/tridiavnc.

---
+++ net/tridiavnc/files/patch-Xvnc::include::Xos.h
@@ -0,0 +1,10 @@
+--- Xvnc/include/Xos.h.orig	Thu Apr 13 02:18:22 2000
++++ Xvnc/include/Xos.h	Wed Nov 20 20:42:00 2002
+@@ -151,7 +151,6 @@
+ #endif /* X_NOT_POSIX else */
+ 
+ #ifdef CSRG_BASED
+-#include <stdlib.h>
+ #include <unistd.h>
+ #endif /* CSRG_BASED */
+ 
