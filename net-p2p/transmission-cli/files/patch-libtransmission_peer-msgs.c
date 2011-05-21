646d4b70a993ec8679c8a6b03cc3ca7e703e4a50
mezz <mezz@FreeBSD.org>
2011-05-21 22:28:33 +0000

    Update to 2.31.

---
+++ net-p2p/transmission-cli/files/patch-libtransmission_peer-msgs.c
@@ -0,0 +1,11 @@
+--- libtransmission/peer-msgs.c.orig	2011-04-29 21:52:48.000000000 -0500
++++ libtransmission/peer-msgs.c	2011-04-29 21:52:54.000000000 -0500
+@@ -16,8 +16,6 @@
+ #include <stdlib.h>
+ #include <string.h>
+ 
+-#include <alloca.h>
+-
+ #include <event2/buffer.h>
+ #include <event2/bufferevent.h>
+ #include <event2/event.h>
