ebd4ef18b0407323b57f15c09ed2c48ac4674500
Wen Heping <wen@FreeBSD.org>
2011-05-23 08:36:50 +0000

    - Update to 1.0.1
    PR:		ports/156909
    Submitted by:	Alexandre Anriot <alexandre@atlantilde.com>
    Approved by:	maintainer(timeout, >14 days)

--- devel/py-fabric/files/patch-setup.py
+++ devel/py-fabric/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2010-11-13 04:57:34.000000000 +0800
-+++ setup.py	2011-01-20 12:46:01.754034224 +0800
-@@ -38,7 +38,6 @@
+--- setup.py.orig	2011-05-23 16:20:40.000000000 +0800
++++ setup.py	2011-05-23 16:21:33.000000000 +0800
+@@ -37,7 +37,6 @@
      packages=find_packages(),
      test_suite='nose.collector',
      tests_require=['nose', 'fudge'],
--    install_requires=['pycrypto %s' % PYCRYPTO, 'paramiko >=1.7.6'],
+-    install_requires=['pycrypto >= 1.9', 'paramiko >=1.7.6'],
      entry_points={
          'console_scripts': [
              'fab = fabric.main:main',
