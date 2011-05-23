e9013eabd8f4839de359ab40f79802d1064f774a
Wesley Shields <wxs@FreeBSD.org>
2011-05-23 16:05:41 +0000

    - Update to 1.5 - Don't hardcode py2.7 - Respect custom LOCALBASE

--- security/py-yara/files/patch-setup.py
+++ security/py-yara/files/patch-setup.py
@@ -1,12 +1,14 @@
---- ./setup.py.orig	2011-03-07 12:21:10.504810488 -0500
-+++ ./setup.py	2011-03-07 12:21:30.973804718 -0500
-@@ -11,8 +11,9 @@
+--- ./setup.py.orig	2011-03-08 15:18:51.000000000 -0500
++++ ./setup.py	2011-05-22 21:27:25.075485034 -0400
+@@ -10,9 +10,10 @@
+         ext_modules = [ Extension(
                                      name='yara', 
                                      sources=['yara-python.c'],
-                                     libraries=['yara','pcre'],
+-                                    libraries=['yara','pcre'],
 -                                    include_dirs=['/usr/local/include']
-+                                    include_dirs=['/usr/local/include'],
-+                                    library_dirs=['/usr/local/lib']
++                                    libraries=['yara','re2'],
++                                    include_dirs=['%%LOCALBASE%%/include'],
++                                    library_dirs=['%%LOCALBASE%%/lib']
                                      )])
       
   
