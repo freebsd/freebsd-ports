daab7480f1230510ca400999c2e6e1e4d659e02c
sbz <sbz@FreeBSD.org>
2011-05-27 08:35:09 +0000

    - Update to 10 - Add LICENSE
    Approved by:	jadawin@ (mentor)

--- devel/dissy/files/patch-setup.py
+++ devel/dissy/files/patch-setup.py
@@ -1,9 +1,9 @@
---- ./setup.py.orig	2010-10-19 05:11:45.000000000 +0200
-+++ ./setup.py	2010-10-19 05:13:08.000000000 +0200
-@@ -31,9 +31,6 @@
- 		      'gfx/red_arrow_right.png', 'gfx/red_plus.png', 'gfx/red_start_up.png',
+--- ./setup.py.orig	2011-05-25 13:03:56.000000000 +0200
++++ ./setup.py	2011-05-25 13:04:34.000000000 +0200
+@@ -53,9 +53,6 @@
+               'gfx/red_arrow_right.png', 'gfx/red_plus.png', 'gfx/red_start_up.png',
                        'gfx/icon.svg']),
- 		    ('share/%s/' % (Config.PROGRAM_NAME.lower()), ['menubar.xml']),
+ 		    ('share/%s/' % (Config.PROGRAM_NAME.lower()), ['dissy.ui']),
 -		    ('share/doc/%s/' % (Config.PROGRAM_NAME.lower()), ['README']),
 -		    ('share/doc/%s/' % (Config.PROGRAM_NAME.lower()), ['COPYING']),
 -		    ('share/man/man1/', ['dissy.1']),
