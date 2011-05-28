ad20c85bbb289c84f6b138736499b6019e5f3ac6
nobutaka <nobutaka@FreeBSD.org>
2011-05-28 20:13:20 +0000

    Update to 3.0.4.

--- x11/mlterm/files/patch-inputmethod_ibus_im_ibus.c
+++ x11/mlterm/files/patch-inputmethod_ibus_im_ibus.c
@@ -1,12 +1,10 @@
---- inputmethod/ibus/im_ibus.c.orig	2011-04-02 11:18:35.000000000 +0900
-+++ inputmethod/ibus/im_ibus.c	2011-04-02 11:19:03.000000000 +0900
-@@ -627,8 +627,8 @@
- 		return  NULL ;
+--- inputmethod/ibus/im_ibus.c.orig	2011-05-29 01:07:42.000000000 +0900
++++ inputmethod/ibus/im_ibus.c	2011-05-29 04:57:48.000000000 +0900
+@@ -631,7 +631,7 @@
  	}
  
--	result->id = strdup( "iBus") ;
+ 	result->id = strdup( "ibus") ;
 -	result->name = strdup( "iBus") ;
-+	result->id = strdup( "ibus") ;
 +	result->name = strdup( "ibus") ;
  	result->num_of_args = 0;
  	result->args = NULL ;
