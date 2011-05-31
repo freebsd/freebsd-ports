c93b803b400c72de08c337631f1c907f12a9af9c
Brooks Davis <brooks@FreeBSD.org>
2011-05-31 21:24:40 +0000

    Upgrade to the latest SVN revsion which fixes a number of bugs.
    Also patch a bug in the command line interface which prevented
    "trac-admin <env> download add <file>" from working.  I'm pretty sure it
    hadn't worked in several years.

---
+++ www/trac-downloads/files/patch-tracdownloads_consoleadmin.py
@@ -0,0 +1,15 @@
+
+$FreeBSD$
+
+--- tracdownloads/consoleadmin.py.orig
++++ tracdownloads/consoleadmin.py
+@@ -122,7 +122,8 @@
+         self.log.debug(download)
+ 
+         # Upload file to DB and file storage.
+-        api.store_download(context, download, file)
++        api._add_download(context, download, file)
++
+ 
+         # Close input file and commit changes in DB.
+         file.close()
