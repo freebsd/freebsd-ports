027cd8ab20a5d1b2ebce934ccc70f9d997cb30d4
decke <decke@FreeBSD.org>
2011-05-31 14:57:44 +0000

    - Update to 1.2.0 - Remove old hacks for i18n >= 0.5.0 and RubyGems >= 1.5.0

--- www/redmine/files/patch-config-boot.rb
+++ www/redmine/files/patch-config-boot.rb
@@ -1,15 +1,6 @@
---- config/boot.rb.orig	2011-03-07 20:44:40.000000000 +0100
-+++ config/boot.rb	2011-03-17 21:15:51.000000000 +0100
-@@ -1,6 +1,8 @@
- # Don't change this file!
- # Configure your app in config/environment.rb and config/environments/*.rb
- 
-+require 'thread'
-+
- RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
- 
- module Rails
-@@ -110,13 +112,13 @@
+--- config/boot.rb.orig	2011-05-30 18:51:21.000000000 +0200
++++ config/boot.rb	2011-05-31 14:07:01.000000000 +0200
+@@ -110,13 +110,13 @@
  # Loads i18n 0.4.2 before Rails loads any more recent gem
  # 0.5.0 is not compatible with the old interpolation syntax
  # Plugins will have to migrate to the new syntax for 1.2.0
