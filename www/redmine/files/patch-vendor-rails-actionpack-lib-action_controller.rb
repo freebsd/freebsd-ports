2f24febe8a027e8729f526e91aa9755347eebd12
Bernhard Froehlich <decke@FreeBSD.org>
2011-05-31 14:57:44 +0000

    - Update to 1.2.0 - Remove old hacks for i18n >= 0.5.0 and RubyGems >= 1.5.0

--- www/redmine/files/patch-vendor-rails-actionpack-lib-action_controller.rb
+++ www/redmine/files/patch-vendor-rails-actionpack-lib-action_controller.rb
@@ -1,11 +1,11 @@
---- vendor/rails/actionpack/lib/action_controller.rb.orig	2010-01-09 12:56:23.000000000 +0100
-+++ vendor/rails/actionpack/lib/action_controller.rb	2010-05-26 11:17:16.000000000 +0200
+--- vendor/rails/actionpack/lib/action_controller.rb.orig	2011-05-30 18:53:34.000000000 +0200
++++ vendor/rails/actionpack/lib/action_controller.rb	2011-05-31 11:50:23.000000000 +0200
 @@ -31,7 +31,7 @@
    end
  end
  
--gem 'rack', '~> 1.0.1'
-+gem 'rack', '>= 1.0.1'
+-gem 'rack', '~> 1.1.0'
++gem 'rack', '>= 1.1.0'
  require 'rack'
  require 'action_controller/cgi_ext'
  
