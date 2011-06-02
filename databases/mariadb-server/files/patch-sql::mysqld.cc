44708e91728187c3119e3d7a7812e4ad791beac8
Doug Barton <dougb@FreeBSD.org>
2011-06-02 06:16:25 +0000

    These aren't ready for use yet, just checking in my WIP

--- databases/mariadb-server/files/patch-sql::mysqld.cc
+++ databases/mariadb-server/files/patch-sql::mysqld.cc
@@ -1,8 +1,8 @@
---- sql/mysqld.cc       2011-02-28 17:39:30 +0000
-+++ sql/mysqld.cc       2011-03-04 10:39:27 +0000
+--- sql/mysqld.cc.orig	2011-03-01 22:24:07.000000000 -0800
++++ sql/mysqld.cc	2011-05-31 16:50:56.000000000 -0700
 @@ -744,9 +744,7 @@
  /* Static variables */
-
+ 
  static bool kill_in_progress, segfaulted;
 -#ifdef HAVE_STACKTRACE
  static my_bool opt_stack_trace;
@@ -10,5 +10,3 @@
  static my_bool opt_bootstrap, opt_myisam_log;
  static int cleanup_done;
  static ulong opt_specialflag, opt_myisam_block_size;
-
-
