b883368c434616e22b4cee560e0248b225cd970c
dougb <dougb@FreeBSD.org>
2011-06-02 06:16:25 +0000

    These aren't ready for use yet, just checking in my WIP

--- databases/mariadb-server/files/patch-extra::yassl::src::yassl_error.cpp
+++ databases/mariadb-server/files/patch-extra::yassl::src::yassl_error.cpp
@@ -1,13 +1,11 @@
---- extra/yassl/src/yassl_error.cpp     2010-04-28 12:52:24 +0000
-+++ extra/yassl/src/yassl_error.cpp     2011-03-01 17:03:38 +0000
+--- extra/yassl/src/yassl_error.cpp.orig	2011-03-01 22:22:55.000000000 -0800
++++ extra/yassl/src/yassl_error.cpp	2011-05-31 16:50:56.000000000 -0700
 @@ -128,7 +128,7 @@
          break;
-
+ 
      case badVersion_error :
 -        strncpy(buffer, "protocl version mismatch", max);
 +        strncpy(buffer, "protocol version mismatch", max);
          break;
-
+         
      case compress_error :
-
-
