a0b87ee68f04578a6d59f02d1b2c4e418f189808
Wen Heping <wen@FreeBSD.org>
2011-06-02 08:20:38 +0000

    - Update to 0.94
    PR:		ports/157515
    Submitted by:	Jason E. Hale <bsdkaffee@gmail.com> (maintainer)

--- finance/libofx/files/patch-lib_ofx_preproc.cpp
+++ finance/libofx/files/patch-lib_ofx_preproc.cpp
@@ -1,14 +1,11 @@
-
-$FreeBSD$
-
---- ./lib/ofx_preproc.cpp.orig	2011-02-12 11:51:02.000000000 -0500
-+++ ./lib/ofx_preproc.cpp	2011-02-14 13:04:17.000000000 -0500
-@@ -194,7 +194,7 @@
- 	    memset(iconv_buffer,0,READ_BUFFER_SIZE * 2);
- 	    size_t inbytesleft = strlen(s_buffer.c_str());
- 	    size_t outbytesleft = READ_BUFFER_SIZE * 2 - 1;
+--- ./lib/ofx_preproc.cpp.orig	2011-04-16 13:10:20.000000000 -0400
++++ ./lib/ofx_preproc.cpp	2011-04-21 11:03:43.000000000 -0400
+@@ -228,7 +228,7 @@
+           memset(iconv_buffer, 0, READ_BUFFER_SIZE * 2);
+           size_t inbytesleft = strlen(s_buffer.c_str());
+           size_t outbytesleft = READ_BUFFER_SIZE * 2 - 1;
 -#ifdef OS_WIN32
 +#if defined(OS_WIN32) || defined(OS_FREEBSD)
- 	    const char * inchar = (const char *)s_buffer.c_str();
+           const char * inchar = (const char *)s_buffer.c_str();
  #else
- 	    char * inchar = (char *)s_buffer.c_str();
+           char * inchar = (char *)s_buffer.c_str();
