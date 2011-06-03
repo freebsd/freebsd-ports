d9c6e6ba64bcc45244f6fa01d344a19e01d443bf
pav <pav@FreeBSD.org>
2011-06-03 13:41:39 +0000

    - Silence compile time warning
    PR:		ports/156588
    Submitted by:	Kalten <kalten@gmx.at>

--- games/KnightCap/files/patch-knightcap.h
+++ games/KnightCap/files/patch-knightcap.h
@@ -1,6 +1,16 @@
---- knightcap.h	Mon Jan 24 22:20:22 2005
-+++ knightcap.h	Mon Jan 24 22:17:19 2005
-@@ -632,16 +632,6 @@
+--- knightcap.h.orig	1998-05-18 01:09:21.000000000 +0200
++++ knightcap.h	2011-06-03 15:40:22.000000000 +0200
+@@ -216,6 +216,9 @@
+ #define BMAJOR_MASK (BPIECE_MASK & ~BMINOR_MASK)
+ #define MAJOR_MASK  (WMAJOR_MASK | BMAJOR_MASK)
+ 
++#ifdef INFINITY
++#undef INFINITY
++#endif
+ #define INFINITY (40.50*STATIC_PAWN_VALUE)
+ #define ILLEGAL (-40*STATIC_PAWN_VALUE)
+ #define WIN (-(ILLEGAL+1))
+@@ -632,16 +635,6 @@
  	return i1 > i2? i1 : i2;
  }
  
