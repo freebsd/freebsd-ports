31dad903a283fe20fedb14a074b88c5a5cbfbe35
culot <culot@FreeBSD.org>
2011-06-07 14:11:18 +0000

    - Update to 0.15
    PR:		ports/157570
    Submitted by:	Peter Jeremy <peterjeremy@acm.org> (maintainer)

--- devel/mspdebug/files/patch-olimex.c
+++ devel/mspdebug/files/patch-olimex.c
@@ -1,5 +1,5 @@
---- olimex.c.orig	2010-11-09 08:40:57.000000000 +1100
-+++ olimex.c	2011-01-23 20:52:02.005008013 +1100
+--- olimex.c.orig	2011-04-05 06:22:54.000000000 +1000
++++ olimex.c	2011-06-04 06:06:10.536246571 +1000
 @@ -20,6 +20,7 @@
  #include <stdio.h>
  #include <string.h>
@@ -8,21 +8,3 @@
  
  #include "olimex.h"
  #include "util.h"
-@@ -66,7 +67,7 @@
- static int open_interface(struct olimex_transport *tr,
- 			  struct usb_device *dev, int ino)
- {
--#if !(defined (__APPLE__) || defined(WIN32))
-+#if defined(__linux__)
- 	int drv;
- 	char drName[256];
- #endif
-@@ -82,7 +83,7 @@
- 		return -1;
- 	}
- 
--#if !(defined(__APPLE__) || defined(WIN32))
-+#if defined(__linux__)
- 	drv = usb_get_driver_np(tr->handle, tr->int_number, drName,
- 				sizeof(drName));
- 	printc(__FILE__" : driver %d\n", drv);
