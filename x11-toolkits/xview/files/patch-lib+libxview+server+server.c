4c1bb0c4657ce6b5b7b16f9b08065fc67452f41d
cy <cy@FreeBSD.org>
2011-06-08 17:55:19 +0000

    Fix XAllocID assertion in Xview applications.
    Assume maintainership of this port.
    
    Obtained from:	Ubuntu bug 89166

--- x11-toolkits/xview/files/patch-lib+libxview+server+server.c
+++ x11-toolkits/xview/files/patch-lib+libxview+server+server.c
@@ -1,6 +1,14 @@
---- ./lib/libxview/server/server.c.orig	Sun Jun 26 20:53:40 1994
-+++ ./lib/libxview/server/server.c	Sat Apr  1 18:25:28 2000
-@@ -65,6 +65,7 @@ static Notify_value 	 scheduler();
+--- lib/libxview/server/server.c.orig	1994-06-26 11:53:40.000000000 -0700
++++ lib/libxview/server/server.c	2011-06-08 10:21:36.653037943 -0700
+@@ -25,6 +25,7 @@
+ #include <xview/win_notify.h>
+ #include <xview/defaults.h>
+ #include <X11/Xlib.h>
++#include <X11/Xlibint.h>
+ #include <xview_private/portable.h>
+ #include <xview_private/svr_atom.h>
+ #include <xview_private/svr_impl.h>
+@@ -65,6 +66,7 @@
  static unsigned int 	 string_to_modmask();
  static Server_atom_type  save_atom();
  static void 		 server_yield_modifiers();
@@ -8,7 +16,7 @@
  
  Xv_private char		*xv_strtok();
  
-@@ -440,7 +441,7 @@ server_init(parent, server_public, avlis
+@@ -440,7 +442,7 @@
    	/* lumpi@dobag.in-berlin.de */
      if (XrmGetDatabase((Display *)server->xdisplay)) {
  	server->db = XrmGetStringDatabase(
@@ -17,3 +25,16 @@
  #else
      if (((Display *)server->xdisplay)->xdefaults) {
  	server->db = XrmGetStringDatabase(
+@@ -741,10 +743,8 @@
+      */
+ 
+     /* Used by atom mgr */
+-    server->atom_mgr[ATOM] = (XID) XAllocID((Display *)server->xdisplay);
+-    server->atom_mgr[NAME] = (XID) XAllocID((Display *)server->xdisplay);
+-    server->atom_mgr[TYPE] = (XID) XAllocID((Display *)server->xdisplay);
+-    server->atom_mgr[DATA] = (XID) XAllocID((Display *)server->xdisplay);
++    /* XXX Fixed by Ubuntu bug 89166 */
++    XAllocIDs((Display *)server->xdisplay, server->atom_mgr, 4);
+ 
+     /* Key for XV_KEY_DATA.  Used in local dnd ops. */
+     server->dnd_ack_key = xv_unique_key();
