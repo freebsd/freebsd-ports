ffecfabdf491de87ee9f6d477f787530d50b562f
Wen Heping <wen@FreeBSD.org>
2011-06-08 08:26:45 +0000

    - Fix build with python-2.7
    PR:		ports/157300
    Submitted by:	David Naylor <naylor.b.david@gmail.com> (maintainer)

--- archivers/py-liblzma/files/patch-setup.py
+++ archivers/py-liblzma/files/patch-setup.py
@@ -9,7 +9,16 @@
  
  descr = "Python bindings for liblzma"
  long_descr = """PylibLZMA provides a python interface for the liblzma library
-@@ -74,7 +74,6 @@
+@@ -54,6 +54,8 @@
+ pc_libs = subprocess.Popen("pkg-config --libs liblzma", shell=True, stdout=subprocess.PIPE, close_fds=True).stdout.readline().strip()
+ if(pc_libs):
+ 	link_args.extend(pc_libs.split(' '))
++link_args.append('-L%s/lib' % os.environ["LOCALBASE"])
++link_args.append('-lpython%i.%i' % sys.version_info[:2])
+ 
+ extens=[Extension('lzma', c_files, extra_compile_args=compile_args, extra_link_args=link_args, define_macros=version_define)]
+ 
+@@ -74,7 +76,6 @@
      ],
      py_modules = modules,
      ext_modules = extens,
