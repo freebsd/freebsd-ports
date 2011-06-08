6d73d3b15661c001b80751e0e4c22f71a1cc085b
Pietro Cerutti <gahr@FreeBSD.org>
2011-06-08 15:26:57 +0000

    - fix build on amd64, enable that platform
    PR:		157704
    Submitted by:	gahr@FreeBSD.org
    Approved by:	Christian Gusenbauer <c47g@gmx.at> (maintainer)

--- graphics/inventor/files/patch-machine.h
+++ graphics/inventor/files/patch-machine.h
@@ -1,23 +1,39 @@
-*** lib/database/include/machine.h.orig	Mon Aug 18 18:36:50 2003
---- lib/database/include/machine.h	Mon Aug 18 18:48:17 2003
-***************
-*** 152,157 ****
---- 152,169 ----
-  
-  
-  /*
-+  * Alpha under FreeBSD
-+  */
-+ 
-+ #if __alpha__ && __FreeBSD__
-+ 
-+ #define MACHINE_WORD_FORMAT	DGL_LITTLE_ENDIAN
-+ #define MACHINE_FLOAT_FORMAT	DGL_NON_IEEE
-+ 
-+ #endif /* __alpha__ && __FreeBSD__ */
-+ 
-+ 
-+ /*
-   * Apple Darwin (Mac OS X) machine dependent setup
-   */
-  
+--- lib/database/include/machine.h.orig	2003-07-12 00:40:53.000000000 +0200
++++ lib/database/include/machine.h	2011-06-08 14:30:26.000000000 +0200
+@@ -143,7 +143,7 @@
+  * Linux i386/ia64 machine dependent setup
+  */
+ 
+-#if __i386__ || __ia64__
++#if __i386__ || __ia64__ || __amd64__
+ 
+ #define MACHINE_WORD_FORMAT	DGL_LITTLE_ENDIAN
+ #define MACHINE_FLOAT_FORMAT	DGL_NON_IEEE
+@@ -152,6 +152,18 @@
+ 
+ 
+ /*
++ * Alpha under FreeBSD
++ */
++
++#if __alpha__ && __FreeBSD__
++
++#define MACHINE_WORD_FORMAT	DGL_LITTLE_ENDIAN
++#define MACHINE_FLOAT_FORMAT	DGL_NON_IEEE
++
++#endif /* __alpha__ && __FreeBSD__ */
++
++
++/*
+  * Apple Darwin (Mac OS X) machine dependent setup
+  */
+ 
+@@ -321,7 +333,7 @@
+  */
+ 
+ #if MACHINE_FLOAT_FORMAT == DGL_NON_IEEE
+-#if __i386__ || __ia64__
++#if __i386__ || __ia64__ || __amd64__
+ void mem_hton_float(float *t, float *f);
+ void mem_ntoh_float(float *t, float *f);
+ void mem_hton_double(double *t, double *f);
