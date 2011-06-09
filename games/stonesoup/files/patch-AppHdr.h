6af40cef44294dbfac7e6639f24f08fc2189f249
jlaffaye <jlaffaye@FreeBSD.org>
2011-06-09 22:28:40 +0000

    Update to 0.8.0
    PR:		ports/157539
    Submitted by:	Tobias Rehbein <tobias.rehbein@web.de> (maintainer)
    Approved by:	bapt (mentor)

--- games/stonesoup/files/patch-AppHdr.h
+++ games/stonesoup/files/patch-AppHdr.h
@@ -1,11 +1,11 @@
---- ./AppHdr.h.orig	2010-07-24 05:31:45.000000000 +0200
-+++ ./AppHdr.h	2010-08-05 09:25:27.000000000 +0200
-@@ -161,7 +161,7 @@
-     //          setuid or setgid. Filenames passed to this command *are not
-     //          validated in any way*.
+--- ./AppHdr.h.orig	2011-04-26 15:08:47.000000000 +0200
++++ ./AppHdr.h	2011-06-02 19:51:41.000000000 +0200
+@@ -135,7 +135,7 @@
      //
--    // #define SOUND_PLAY_COMMAND "/usr/bin/play -v .5 %s 2>/dev/null &"
-+    %%SOUND%%#define SOUND_PLAY_COMMAND "%%LOCALBASE%%/bin/play -v .5 %s 2>/dev/null &"
+     // WARNING: Filenames passed to this command *are not validated in any way*.
+     //
+-    // #define SOUND_PLAY_COMMAND "/usr/bin/play -v .5 \"%s\" 2>/dev/null &"
++    %%SOUND%%#define SOUND_PLAY_COMMAND "%%LOCALBASE%%/bin/play -v .5 \"%s\" 2>/dev/null &"
  
      // For cases when the game will be played on terms that don't support the
      // curses "bold == lighter" 16 colour mode. -- bwr
