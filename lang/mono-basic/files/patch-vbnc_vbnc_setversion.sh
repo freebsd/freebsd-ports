89f23b925a97abdb699e3fd78263af2805e399fb
romain <romain@FreeBSD.org>
2011-06-12 19:06:25 +0000

    Hello Mono 2.10!
    Main updates
    ------------
    devel/mono-tools                2.6.2 -> 2.10
    lang/mono                       2.6.7 -> 2.10.2
    lang/mono-basic                 2.6.2 -> 2.10.2
    www/mod_mono                    2.6.3 -> 2.10
    www/xsp                         2.6.5 -> 2.10.2
    x11-toolkits/libgdiplus         2.6.7 -> 2.10
    
    Other updates
    -------------
    deskutils/tomboy                1.4.2 -> 1.6.1
    devel/mono-addins               0.5 -> 0.6.1
    devel/monodevelop               2.4 -> 2.4.2
    graphics/f-spot                 0.8.0 -> 0.8.2
    print/pdfmod                    0.9.0 -> 0.9.1
    x11-toolkits/gnome-sharp20      2.24.1 -> 2.24.2
    
    Ports marked BROKEN
    -------------------
    multimedia/banshee
    multimedia/moonlight
    
    PR:		ports/155948
    Submitted by:	me

---
+++ lang/mono-basic/files/patch-vbnc_vbnc_setversion.sh
@@ -0,0 +1,11 @@
+
+$FreeBSD$
+
+--- vbnc/vbnc/setversion.sh.orig
++++ vbnc/vbnc/setversion.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash -ex
++#!/usr/bin/env bash -ex
+ 
+ VERSION_VB=$1
+ VERSION_TMP=version.tmp
