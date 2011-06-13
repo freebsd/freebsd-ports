c8f6aa91e53e4b6ed60a127db78786505073f400
amdmi3 <amdmi3@FreeBSD.org>
2011-06-13 18:39:00 +0000

    TortoiseHg is a Windows shell extension and a series of applications for the Mercurial  distributed revision control system. It also includes a Gnome/Nautilus extension and a CLI wrapper application so the TortoiseHg tools can be used on non-Windows platforms.
    WWW:	http://tortoisehg.bitbucket.org/
    
    PR:		157067
    Submitted by:	arrowdodger <6yearold@gmail.com>

---
+++ devel/tortoisehg2/files/patch-setup.py
@@ -0,0 +1,19 @@
+--- setup.py_	2011-05-02 07:06:56.000000000 +0400
++++ setup.py	2011-05-25 15:54:34.833312978 +0400
+@@ -407,11 +407,11 @@
+     _data_files = [(os.path.join('share/pixmaps/tortoisehg', root),
+         [os.path.join(root, file_) for file_ in files])
+         for root, dirs, files in os.walk('icons')]
+-    _data_files += [(os.path.join('share', root),
+-        [os.path.join(root, file_) for file_ in files])
+-        for root, dirs, files in os.walk('locale')]
+-    _data_files += [('lib/nautilus/extensions-2.0/python',
+-                     ['contrib/nautilus-thg.py'])]
++%%NLS%%    _data_files += [(os.path.join('share', root),
++%%NLS%%        [os.path.join(root, file_) for file_ in files])
++%%NLS%%        for root, dirs, files in os.walk('locale')]
++%%NAUTILUS%%    _data_files += [('lib/nautilus/extensions-2.0/python',
++%%NAUTILUS%%                     ['contrib/nautilus-thg.py'])]
+ 
+     # Create a config.py.  Distributions will need to supply their own
+     cfgfile = os.path.join('tortoisehg', 'util', 'config.py')
