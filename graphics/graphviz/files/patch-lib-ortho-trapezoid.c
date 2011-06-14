b7f3db2deadaf71c87edb20ffa031ece12fdb0ab
dinoex <dinoex@FreeBSD.org>
2011-06-14 17:21:23 +0000

    - patch needed for 704101 PR:		157217 Reported by:	Fedor Dikarev

--- graphics/graphviz/files/patch-lib-ortho-trapezoid.c
+++ graphics/graphviz/files/patch-lib-ortho-trapezoid.c
@@ -5,7 +5,7 @@
  #endif
  
 +#include <sys/param.h>
-+#if __FreeBSD_version <= 704100 || (__FreeBSD_version >= 800000 && __FreeBSD_version < 802502) || (__FreeBSD_version >= 900000 && __FreeBSD_version < 900027)
++#if __FreeBSD_version <= 704101 || (__FreeBSD_version >= 800000 && __FreeBSD_version < 802502) || (__FreeBSD_version >= 900000 && __FreeBSD_version < 900027)
 +#define log2(x)  (log(x)/log(2))
 +#endif
 +
