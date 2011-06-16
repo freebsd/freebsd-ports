f170abafc8b14ffe1326d0e05d124bcc0bd3fd45
swills <swills@FreeBSD.org>
2011-06-16 02:52:55 +0000

    - Update to 6.24 [1] - Fix pod [1] - Add missing depends
    Changes: http://search.cpan.org/dist/POE-Quickie/Changes
    
    PR:		ports/157864 [1]
    Submitted by:	Takefu <takefu@airport.fm> (maintainer) [1]

---
+++ devel/p5-POE-Quickie/files/patch-lib_POE-Quickie.pm
@@ -0,0 +1,20 @@
+--- lib/POE/Quickie.pm.orig	2011-05-25 14:44:40.000000000 +0900
++++ lib/POE/Quickie.pm	2011-06-14 12:00:27.000000000 +0900
+@@ -604,6 +604,8 @@
+
+ =item C<ARG2>: the context variable, if any
+
++=back
++
+ =head2 ResultEvent
+
+ =over 4
+@@ -620,6 +622,8 @@
+
+ =item C<ARG5>: the context variable, if any
+
++=over
++
+ =back
+
+ =back
