cc1ec24ac147ddcdbd00f3737470668e0b67cd0f
Juergen Lock <nox@FreeBSD.org>
2011-06-17 19:53:44 +0000

    Update to 20110616.

--- multimedia/w_scan/files/patch-scan.c
+++ multimedia/w_scan/files/patch-scan.c
@@ -1,13 +1,13 @@
 --- a/scan.c
 +++ b/scan.c
-@@ -2866,8 +2866,8 @@ int main (int argc, char **argv)
- 		bad_usage(argv[0]);
- 		return -1;
- 		}
--	if  (((adapter > 7) && (adapter != 999)) || (adapter < 0)) {
--		info("Invalid adapter: out of range (0..7)\n");
-+	if  (((adapter > 255) && (adapter != 999)) || (adapter < 0)) {
-+		info("Invalid adapter: out of range (0..255)\n");
- 		bad_usage(argv[0]);
- 		return -1;
- 		}
+@@ -2936,8 +2936,8 @@ int main (int argc, char **argv)
+                 cleanup();
+                 return -1;
+                 }
+-        if  (((adapter > 7) && (adapter != 999)) || (adapter < 0)) {
+-                info("Invalid adapter: out of range (0..7)\n");
++        if  (((adapter > 255) && (adapter != 999)) || (adapter < 0)) {
++                info("Invalid adapter: out of range (0..255)\n");
+                 bad_usage(argv[0]);
+                 cleanup();
+                 return -1;
