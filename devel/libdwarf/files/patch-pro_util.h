9a3ef3c006c24a77504e16c6b6b75f95a5eec79c
joerg <joerg@FreeBSD.org>
2011-06-18 12:48:43 +0000

    Slightly improve the previous patch so it should now still compile (the old way) on FreeBSD versions prior to 7.x which didn't have the extended symbols in <sys/elf_common.h>.

--- devel/libdwarf/files/patch-pro_util.h
+++ devel/libdwarf/files/patch-pro_util.h
@@ -1,11 +1,23 @@
---- pro_util.h.orig	2011-06-18 08:45:01.000000000 +0200
-+++ pro_util.h	2011-06-18 08:45:50.000000000 +0200
-@@ -120,7 +120,7 @@
+--- pro_util.h.orig	2011-06-18 08:46:00.000000000 +0200
++++ pro_util.h	2011-06-18 14:45:59.000000000 +0200
+@@ -34,6 +34,9 @@
+ 
+ */
+ 
++#if defined(__FreeBSD__)
++# include <osreldate.h>
++#endif
+ 
+ 
+ 
+@@ -120,7 +123,9 @@
  #define Get_REL_SEGREL_isa(dbg)    (R_386_NONE) /* I don't know! */
  #endif /* sparc || i386 */
  #else  /* !sun */
 -#ifdef HAVE_SYS_IA64_ELF_H
-+#if defined(HAVE_SYS_IA64_ELF_H) || defined(__FreeBSD__)
++#if defined(HAVE_SYS_IA64_ELF_H) ||\
++   /* FreeBSD 7.x and above always have these */ \
++   (defined(__FreeBSD__) && __FreeBSD_version > 700000)
  #define Get_REL64_isa(dbg)         (ISA_IA64(dbg) ? \
      DWARF_PRO_R_IA64_DIR64LSB : R_MIPS_64)
  #define Get_REL32_isa(dbg)         (ISA_IA64(dbg) ? \
