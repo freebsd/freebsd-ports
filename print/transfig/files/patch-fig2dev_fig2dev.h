bb9fd6aa6633d8f23cea7640dff90f7656406c81
glewis <glewis@FreeBSD.org>
2011-06-18 01:02:22 +0000

    . Update to 3.2.5d. . Fix MASTER_SITES to make it fetchable.

--- print/transfig/files/patch-fig2dev_fig2dev.h
+++ print/transfig/files/patch-fig2dev_fig2dev.h
@@ -1,11 +1,11 @@
---- fig2dev/fig2dev.h.orig	Tue Jul 15 09:21:12 2003
-+++ fig2dev/fig2dev.h
-@@ -213,7 +213,7 @@
+--- fig2dev/fig2dev.h.orig	2010-03-16 11:53:20.000000000 -0700
++++ fig2dev/fig2dev.h	2011-06-16 22:23:27.000000000 -0700
+@@ -214,7 +214,7 @@
  #endif /* MAXPATHLEN */
  #endif /* PATH_MAX */
  
--#if ( !defined(__NetBSD__) && !defined(__DARWIN__))
-+#if ( !defined(__NetBSD__) && !defined(__FreeBSD__) && !defined(__DARWIN__))
+-#if ( !defined(__NetBSD__) && !defined(__DARWIN__) && !defined(__FreeBSD) )
++#if ( !defined(__NetBSD__) && !defined(__DARWIN__) && !defined(__FreeBSD__) )
  extern int		sys_nerr, errno;
  #endif
  
