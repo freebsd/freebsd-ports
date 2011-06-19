2b88a7204af45f4ef92ef84785f45db12b59aad6
Greg Lewis <glewis@FreeBSD.org>
2011-06-19 17:40:38 +0000

    . Update to 3.2.5b.

---
+++ graphics/xfig/files/patch-w_intersect.c
@@ -0,0 +1,14 @@
+$FreeBSD$
+
+--- w_intersect.c.orig	2011-06-18 21:05:55.000000000 -0700
++++ w_intersect.c	2011-06-18 21:06:18.000000000 -0700
+@@ -26,7 +26,9 @@
+ #include "w_msgpanel.h"
+ #include "f_util.h"
+ #include "u_quartic.h"
++#ifndef __FreeBSD__
+ #include <alloca.h>
++#endif
+ #include <math.h>
+ #undef I
+   
