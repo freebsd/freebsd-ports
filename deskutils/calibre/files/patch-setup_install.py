fdfad8a205cc725fd0623287bb5c13c771bbddaf
Julien Laffaye <jlaffaye@FreeBSD.org>
2011-06-19 18:08:09 +0000

    Update to 0.8.5
    PR:		ports/157884
    Submitted by:	Rusty Nejdl <rnejdl@ringofsaturn.com> (maintainer)
    Approved by:	bapt (mentor)

--- deskutils/calibre/files/patch-setup_install.py
+++ deskutils/calibre/files/patch-setup_install.py
@@ -1,7 +1,7 @@
---- setup/install.py.orig	2011-05-02 21:06:12.000000000 -0500
-+++ setup/install.py	2011-05-02 21:06:17.000000000 -0500
+--- setup/install.py.orig	2011-06-14 14:16:28.000000000 -0500
++++ setup/install.py	2011-06-14 14:16:39.000000000 -0500
 @@ -12,7 +12,7 @@
-         __appname__, __version__
+         __appname__, __version__)
  
  HEADER = '''\
 -#!/usr/bin/env python2
