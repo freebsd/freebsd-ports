93f99253fdfa6276e438b8460d1dd2a5fb2c9faf
Emanuel Haupt <ehaupt@FreeBSD.org>
2011-06-21 19:13:50 +0000

    Fix build with clang.

--- audio/mp3guessenc/files/patch-mp3guessenc.c
+++ audio/mp3guessenc/files/patch-mp3guessenc.c
@@ -1,5 +1,5 @@
---- mp3guessenc.c.orig	Sat Oct  1 00:34:43 2005
-+++ mp3guessenc.c	Sat Oct  1 00:38:22 2005
+--- ./mp3guessenc.c.orig	2011-06-21 21:09:47.000000000 +0200
++++ ./mp3guessenc.c	2011-06-21 21:11:00.000000000 +0200
 @@ -515,6 +515,7 @@
    for(q--;q >= lame_string && *q == 'U';q--) *q = '\0';
  
@@ -20,3 +20,30 @@
  
    fseek(input_file,0,SEEK_END);
    filesize = ftell(input_file);
+@@ -636,7 +640,7 @@
+     {
+       printf("Cannot find valid mp3 header, scanning failed\n");
+       fclose(input_file);
+-      return;
++      return(0);
+     }
+ 
+   // read VBR tag
+@@ -669,7 +673,7 @@
+       if (pos == -1) {
+ 	printf("Cannot find valid mp3 header, scanning failed\n");
+ 	fclose(input_file);
+-	return;
++	return(0);
+       }
+     }
+   }
+@@ -705,7 +709,7 @@
+ 	    "Free format bitstream is not supported.\n"
+ 	    "Sorry.\n");
+     fclose(input_file);
+-    return;
++    return(0);
+   }
+ 
+   crc = (head >> 16) & 1;
