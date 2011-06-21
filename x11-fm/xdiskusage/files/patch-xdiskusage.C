82860081442133bb5cbbf9dd3b19515d9e181900
ehaupt <ehaupt@FreeBSD.org>
2011-06-21 23:13:11 +0000

    Make port build with clang. While we're here add an additional distfile mirror and get rid of custom do-build target.
    PR:		158136
    Submitted by:	Niclas Zeising <niclas.zeising@gmail.com>

--- x11-fm/xdiskusage/files/patch-xdiskusage.C
+++ x11-fm/xdiskusage/files/patch-xdiskusage.C
@@ -1,5 +1,27 @@
---- xdiskusage.C.orig	2008-09-15 19:21:03.000000000 +0000
-+++ xdiskusage.C	2008-09-15 19:23:54.000000000 +0000
+--- xdiskusage.C.orig	2004-09-21 07:23:14.000000000 +0200
++++ xdiskusage.C	2011-06-21 21:34:20.000000000 +0200
+@@ -392,8 +392,8 @@
+       strncpy(pathbuf, path, 1024);
+       for (int i=0; i<10; i++) {
+ 	char *p = (char*)fl_filename_name(pathbuf);
+-	int i = readlink(pathbuf, p, 1024-(p-pathbuf));
+-	if (i < 0) {
++	int j = readlink(pathbuf, p, 1024-(p-pathbuf));
++	if (j < 0) {
+ 	  if (errno != EINVAL) {
+ 	    strcat(pathbuf, ": no such file");
+ 	    fl_alert(pathbuf);
+@@ -401,8 +401,8 @@
+ 	  }
+ 	  break;
+ 	}
+-	if (*p == '/') {memmove(pathbuf, p, i); p = pathbuf;}
+-	p[i] = 0;
++	if (*p == '/') {memmove(pathbuf, p, j); p = pathbuf;}
++	p[j] = 0;
+ 	path = pathbuf;
+       }
+     }
 @@ -988,7 +988,7 @@
  void OutputWindow::sort_cb(Fl_Widget* o, void*v) {
    OutputWindow* d = (OutputWindow*)(o->window());
