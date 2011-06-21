925b91e9712889060ae6c6c275e6b08dce8570c8
Vanilla I. Shu <vanilla@FreeBSD.org>
2011-06-21 01:58:12 +0000

    Upgrade to 0.3, and fix build with clang.

--- net/spnetkit/files/patch-spnklog.cpp
+++ net/spnetkit/files/patch-spnklog.cpp
@@ -1,11 +1,11 @@
---- spnklog.cpp.orig	2008-02-18 12:19:32.000000000 +0800
-+++ spnklog.cpp	2008-02-18 12:19:41.000000000 +0800
-@@ -107,7 +107,7 @@
+--- spnklog.cpp.orig	2011-06-21 09:28:36.000000000 +0800
++++ spnklog.cpp	2011-06-21 09:29:24.000000000 +0800
+@@ -168,7 +168,7 @@ void SP_NKLog :: log( int pri, const cha
  		snprintf( logText, sizeof( logText ),
  			"%04i-%02i-%02i %02i:%02i:%02i #%i ",
  			1900 + tmTime.tm_year, tmTime.tm_mon+1, tmTime.tm_mday,
--			tmTime.tm_hour, tmTime.tm_min, tmTime.tm_sec, (int)pthread_self() );
-+			tmTime.tm_hour, tmTime.tm_min, tmTime.tm_sec, pthread_self() );
+-			tmTime.tm_hour, tmTime.tm_min, tmTime.tm_sec, (int)spnk_threadid() );
++			tmTime.tm_hour, tmTime.tm_min, tmTime.tm_sec, (long)spnk_threadid() );
  	}
  
  	if( mIsLogPriName ) {
