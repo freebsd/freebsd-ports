9672ec1d0930891a2a22d7f35e2dedfd4cac308e
dhn <dhn@FreeBSD.org>
2011-06-24 13:17:43 +0000

    - Fix build with clang

---
+++ x11-wm/antiwm/files/patch-manage.c
@@ -0,0 +1,10 @@
+--- ./manage.c.orig	2011-06-24 15:13:24.000000000 +0200
++++ ./manage.c	2011-06-24 15:13:59.000000000 +0200
+@@ -10,6 +10,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ 
+ #include "antiwm.h"
+ 
