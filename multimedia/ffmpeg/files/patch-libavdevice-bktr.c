26924fe0b776003dc0d244f9405a6b6969f71eb1
mm <mm@FreeBSD.org>
2011-06-24 22:37:15 +0000

    Update ffmpeg to 0.7.1 Add support for celt, freetype2, frei0r, rtmp, vo-aacenc, vo-amrwbenc

--- multimedia/ffmpeg/files/patch-libavdevice-bktr.c
+++ multimedia/ffmpeg/files/patch-libavdevice-bktr.c
@@ -1,5 +1,5 @@
---- libavdevice/bktr.c.orig	2009-05-15 17:34:55.000000000 +0200
-+++ libavdevice/bktr.c	2009-05-15 17:38:01.000000000 +0200
+--- libavdevice/bktr.c.orig	2011-06-21 21:29:25.000000000 +0200
++++ libavdevice/bktr.c	2011-06-23 13:39:44.160881669 +0200
 @@ -24,6 +24,11 @@
   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
   */
@@ -9,6 +9,6 @@
 +typedef unsigned int	u_int;
 +typedef unsigned long	u_long;
 +
- #define _BSD_SOURCE 1
- #include "libavformat/avformat.h"
- #if HAVE_DEV_BKTR_IOCTL_METEOR_H && HAVE_DEV_BKTR_IOCTL_BT848_H
+ #include "libavutil/log.h"
+ #include "libavutil/opt.h"
+ #include "libavutil/parseutils.h"
