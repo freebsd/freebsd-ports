0dfe708812262366ca18fe49f528c077cef1605b
Dennis Herrmann <dhn@FreeBSD.org>
2011-06-24 13:17:43 +0000

    - Fix build with clang

---
+++ x11-wm/antiwm/files/patch-list.c
@@ -0,0 +1,10 @@
+--- ./list.c.orig	2011-06-24 15:14:48.000000000 +0200
++++ ./list.c	2011-06-24 15:15:27.000000000 +0200
+@@ -4,6 +4,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ 
+ #include "antiwm.h"
+ 
