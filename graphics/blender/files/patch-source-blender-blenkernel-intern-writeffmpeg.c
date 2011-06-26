6b01558473f60632d5df21962d4e58af3f27ae7d
mva <mva@FreeBSD.org>
2011-06-26 14:35:52 +0000

    - Update to 2.58

--- graphics/blender/files/patch-source-blender-blenkernel-intern-writeffmpeg.c
+++ graphics/blender/files/patch-source-blender-blenkernel-intern-writeffmpeg.c
@@ -1,16 +1,16 @@
---- source/blender/blenkernel/intern/writeffmpeg.c.orig	2011-06-24 09:07:48.320613989 +0200
-+++ source/blender/blenkernel/intern/writeffmpeg.c	2011-06-24 09:08:17.697825516 +0200
-@@ -1101,12 +1101,12 @@
+--- source/blender/blenkernel/intern/writeffmpeg.c.orig	2011-06-26 11:28:46.000000000 +0200
++++ source/blender/blenkernel/intern/writeffmpeg.c	2011-06-26 11:29:27.000000000 +0200
+@@ -1053,12 +1053,12 @@
  	switch (o->type) {
  	case FF_OPT_TYPE_INT:
  	case FF_OPT_TYPE_INT64:
--		val.i = o->default_val;
+-		val.i = FFMPEG_DEF_OPT_VAL_INT(o);
 +		val.i = o->default_val.i64;
  		idp_type = IDP_INT;
  		break;
  	case FF_OPT_TYPE_DOUBLE:
  	case FF_OPT_TYPE_FLOAT:
--		val.f = o->default_val;
+-		val.f = FFMPEG_DEF_OPT_VAL_DOUBLE(o);
 +		val.f = o->default_val.dbl;
  		idp_type = IDP_FLOAT;
  		break;
