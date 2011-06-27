a14f667c9c65fb3705e574a39ed23ef68625d39f
Christian Weisgerber <naddy@FreeBSD.org>
2011-06-27 11:24:25 +0000

    Fix build with clang

--- audio/xmms-wavpack/files/patch-src_libwavpack.cpp
+++ audio/xmms-wavpack/files/patch-src_libwavpack.cpp
@@ -1,5 +1,5 @@
 --- src/libwavpack.cpp.orig	2009-09-28 04:52:56.000000000 +0200
-+++ src/libwavpack.cpp	2009-10-04 22:03:00.000000000 +0200
++++ src/libwavpack.cpp	2011-06-27 13:19:20.000000000 +0200
 @@ -40,7 +40,7 @@ extern "C" {
  #define M_LN10   2.3025850929940456840179914546843642
  #endif
@@ -9,6 +9,15 @@
  #define BUFFER_SIZE 256 // read buffer size, in samples
  
  extern "C" InputPlugin * get_iplugin_info(void);
+@@ -167,7 +167,7 @@ public:
+         int tsamples = num_samples * num_channels;
+ 
+         if (!(WavpackGetMode (ctx) & MODE_FLOAT)) {
+-            float scaler = (float) (1.0 / ((unsigned int32_t) 1 << (bytes_per_sample * 8 - 1)));
++            float scaler = (float) (1.0 / ((uint32_t) 1 << (bytes_per_sample * 8 - 1)));
+             float *fptr = (float *) input;
+             int32_t *lptr = input;
+             int cnt = tsamples;
 @@ -362,7 +362,7 @@ convertUTF8toLocale(char *utf8)
      size_t in_left = strlen(utf8);
      size_t out_left = 2 * in_left + 1;
