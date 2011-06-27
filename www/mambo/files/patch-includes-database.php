a755aaa01754092f5c7079d5b0ba0b05958d15c6
wxs <wxs@FreeBSD.org>
2011-06-27 14:38:50 +0000

    - Update to 4.6.5. - Assign maintainer to submitter.
    PR:		ports/158330
    Submitted by:	Terry Sposato <tabmow@freenode.net>
    Security:	8a5770b4-54b5-11db-a5ae-00508d6a62df

---
+++ www/mambo/files/patch-includes-database.php
@@ -0,0 +1,13 @@
+--- ./includes/database.php.orig	2011-06-27 12:26:39.000000000 +1000
++++ ./includes/database.php	2011-06-27 12:27:48.000000000 +1000
+@@ -784,6 +784,10 @@
+ 	/** @var mosDatabase Database connector */
+ 	var $_db = null;
+ 
++	function mosDBTable ($table='', $keyname='id', $db='') { 
++        	$this->mosDBAbstractRow($table, $keyname, $db); 
++	} 
++
+ 	/**
+ 	 *	@return bool True if DB query failed.  Sets the error message
+ 	 */
