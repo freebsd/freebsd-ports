dfadc9fbca9983efff202af4f1c19c2430314787
Florian Smeets <flo@FreeBSD.org>
2011-06-28 16:34:23 +0000

    - disable debug symbols for the gecko ports and enable striping of libraries and binaries otherwise firefox and thunderbird >= 5 will install a libxul.so which can be as big as 500MB
    PR:		ports/158380
    Submitted by:	Pan Tsu <inyaoo@gmail.com>

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -638,6 +638,7 @@ MOZ_OPTIONS+=	--enable-necko-protocols=${MOZ_PROTOCOLS}
 MOZ_OPTIONS+=	--with-system-zlib=/usr		\
 		--with-gssapi=${KRB5_HOME}	\
 		--disable-auto-deps		\
+		--disable-debug-symbols		\
 		--enable-chrome-format=jar	\
 		--disable-cpp-exceptions	\
 		--disable-cpp-rtti		\
@@ -673,6 +674,8 @@ MOZ_OPTIONS+=	--disable-gnomevfs
 
 .if !defined(STRIP) || ${STRIP} == ""
 MOZ_OPTIONS+=	--disable-strip --disable-install-strip
+.else
+MOZ_OPTIONS+=	--enable-strip --enable-install-strip
 .endif
 
 .if defined(WITH_DEBUG)
