2dd3231b85dd73bdc8219c1eaec83bcd4a293299
Florian Smeets <flo@FreeBSD.org>
2011-06-30 18:07:32 +0000

    update to 3.1.11 as mail/thunderbird will be updated to 5.0

--- mail/thunderbird3/files/patch-mozilla-security-coreconf-FreeBSD.mk
+++ mail/thunderbird3/files/patch-mozilla-security-coreconf-FreeBSD.mk
@@ -1,29 +1,22 @@
---- mozilla/security/coreconf/FreeBSD.mk.orig	2010-03-16 09:57:06.000000000 +0000
-+++ mozilla/security/coreconf/FreeBSD.mk	2010-03-20 17:40:12.000000000 +0000
-@@ -49,8 +49,20 @@
- ifeq ($(CPU_ARCH),pc98)
- CPU_ARCH		= x86
+--- mozilla/security/coreconf/FreeBSD.mk.orig	2010-04-30 19:44:32.000000000 +0000
++++ mozilla/security/coreconf/FreeBSD.mk	2010-05-10 13:04:45.000000000 +0000
+@@ -52,6 +52,15 @@
+ ifeq ($(CPU_ARCH),amd64)
+ CPU_ARCH		= x86_64
  endif
--ifeq ($(CPU_ARCH),amd64)
--CPU_ARCH		= x86_64
-+ifeq ($(OS_TEST),alpha)
-+CPU_ARCH		= alpha
-+endif
 +ifeq ($(OS_TEST),amd64)
 +CPU_ARCH		= amd64
-+endif
-+ifeq ($(OS_TEST),ia64) 
-+CPU_ARCH		= ia64
 +endif
 +ifeq ($(OS_TEST),powerpc)
 +CPU_ARCH		= powerpc
 +endif
 +ifeq ($(OS_TEST),sparc64)
 +CPU_ARCH		= sparc64
- endif
++endif
  
  OS_CFLAGS		= $(DSO_CFLAGS) -ansi -Wall -Wno-switch -DFREEBSD -DHAVE_STRERROR -DHAVE_BSD_FLOCK
-@@ -78,7 +90,7 @@
+ 
+@@ -78,7 +87,7 @@
  DLL_SUFFIX		= so.1.0
  endif
  
@@ -32,7 +25,7 @@
  ifdef MAPFILE
  	MKSHLIB += -Wl,--version-script,$(MAPFILE)
  endif
-@@ -87,4 +99,4 @@
+@@ -87,4 +96,4 @@
  
  G++INCLUDES		= -I/usr/include/g++
  
