6e6530fb1d25f3f0414594d3dc3ad61677f022c9
Diane Bruce <db@FreeBSD.org>
2011-07-01 12:30:51 +0000

    - Unbreak clang build - Replace hard coded g++ with ${CXX} in Makefile - main() was missing int declaration

--- comms/locator/files/patch-locator.cc
+++ comms/locator/files/patch-locator.cc
@@ -1,8 +1,8 @@
---- locator.cc.orig	Thu Jun 30 15:21:49 2005
-+++ locator.cc	Thu Jun 30 17:04:24 2005
-@@ -3,9 +3,13 @@
+--- locator.cc.orig	2001-07-29 07:33:02.000000000 -0400
++++ locator.cc	2011-07-01 08:26:29.000000000 -0400
+@@ -3,15 +3,19 @@
  /* Last change 29.07.2001  */
-
+ 
  /* A few includes */
 -#include <iostream.h>
 -#include <string.h>
@@ -14,6 +14,13 @@
 +using std::iostream;
 +using std::ostream;
 +using std::cout;
-
+ 
  /* A few definitions */
  #define VERSION "0.1"
+ 
+ /* Main program */
+-main(int argc, char *argv[]) {
++int main(int argc, char *argv[]) {
+   float c_long, c_lat;
+   char  *locator;
+ 
