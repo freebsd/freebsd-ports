700e6433ed243bca354b1e3f95347ca029af041a
swills <swills@FreeBSD.org>
2011-07-02 12:04:33 +0000

    - Add files missed in last commit
    pointy hat to:		swills

---
+++ mail/milter-manager/files/patch-binding_ruby_test_run-test.sh
@@ -0,0 +1,8 @@
+--- ./binding/ruby/test/run-test.sh.orig	2011-05-11 22:19:37.356427000 +0900
++++ ./binding/ruby/test/run-test.sh	2011-06-16 20:04:31.000000000 +0900
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/bin/sh
+ #
+ # Copyright (C) 2011  Kouhei Sutou <kou@clear-code.com>
+ #
