8e832b08e0002d216c8fa039149f542784cca98b
stephen <stephen@FreeBSD.org>
2011-07-03 18:40:21 +0000

    - Fix build errors when MAKE_NOUVEAU set in /etc/make.conf
    PR:		ports/150883
    Submitted by:	Rusty Nejdl <rnejdl@ringofsaturn.com> and swell.k@gmail.com
    Approved by:	gabor (mentor) and proler@gmail.com (maintainer)

---
+++ games/openastromenace/files/patch-AstroMenaceSource-Core-VirtualFileSystem-VFS.h
@@ -0,0 +1,10 @@
+--- AstroMenaceSource/Core/VirtualFileSystem/VFS.h~
++++ AstroMenaceSource/Core/VirtualFileSystem/VFS.h
+@@ -34,6 +34,7 @@
+ #define VFS_H
+ 
+ 
++#include <cstdio>
+ #include "../Base.h"
+ 
+ 
