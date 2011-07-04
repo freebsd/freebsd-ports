6e9bc86d4628a69582adfb7f37ee61f8464c85f7
Dmitry Sivachenko <demon@FreeBSD.org>
2011-07-04 11:57:38 +0000

    Fix crash when reporting IPv6 error.
    PR:	157571
    Submitted by:	Mike Andrews <mandrews@bit0.com>

--- net-mgmt/p5-SNMP_Session/files/patch-SNMP_Session.pm
+++ net-mgmt/p5-SNMP_Session/files/patch-SNMP_Session.pm
@@ -5,7 +5,7 @@
      if (eval {local $SIG{__DIE__};require Socket6;} &&
         eval {local $SIG{__DIE__};require IO::Socket::INET6; IO::Socket::INET6->VERSION("1.26");}) {
 -	import Socket6;
-+       	Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo));
++       	Socket6->import(qw(pack_sockaddr_in6 inet_pton inet_ntop unpack_sockaddr_in6 getaddrinfo));
  	$ipv6_addr_len = length(pack_sockaddr_in6(161, inet_pton(AF_INET6(), "::1")));
  	$SNMP_Session::ipv6available = 1;
      }
@@ -14,7 +14,7 @@
      if($SNMP_Session::ipv6available) {
  	import IO::Socket::INET6;
 -	import Socket6;
-+	Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo)); 
++	Socket6->import(qw(pack_sockaddr_in6 inet_pton inet_ntop unpack_sockaddr_in6 getaddrinfo)); 
      }
  }
  
