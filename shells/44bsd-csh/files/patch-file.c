335a7ae799944ed82837edad89dd13ef510ccaf3
Cy Schubert <cy@FreeBSD.org>
2011-07-05 13:34:43 +0000

    Previous commit introduced a file completion bug under FreeBSD versions prior to 8.X (7.X and earlier). This commit fixes that.

--- shells/44bsd-csh/files/patch-file.c
+++ shells/44bsd-csh/files/patch-file.c
@@ -1,5 +1,5 @@
---- file.c.orig	2011-07-04 06:54:10.348798410 -0700
-+++ file.c	2011-07-04 16:23:57.199132280 -0700
+--- file.c.orig	2011-07-05 06:22:52.572441868 -0700
++++ file.c	2011-07-05 06:25:04.233646750 -0700
 @@ -82,7 +82,7 @@
  
  static void	 setup_tty __P((int));
@@ -20,16 +20,18 @@
  {
      Char *p;
      struct termios tty, tty_normal;
-@@ -168,6 +169,8 @@
+@@ -168,6 +169,10 @@
      (void) tcgetattr(SHOUT, &tty);
      tty_normal = tty;
      tty.c_lflag &= ~(ECHOKE | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOCTL);
++#if __FreeBSD_version >= 800000
 +    if (list == LIST)
 +    	tty.c_lflag |= ECHO;
++#endif
      (void) tcsetattr(SHOUT, TCSANOW, &tty);
  
      for (p = string; (c = *p) != '\0'; p++)
-@@ -660,7 +663,7 @@
+@@ -660,7 +665,7 @@
  	    should_retype = TRUE;
  	if (should_retype)
  	    printprompt();
