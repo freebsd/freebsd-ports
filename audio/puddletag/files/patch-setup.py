d09f2e047157963dd5b3741c372fb3d48a14fa70
pawel <pawel@FreeBSD.org>
2011-07-05 19:24:52 +0000

    - update to version 0.10.6 - fix portlint error
    Approved by:	miwi (mentor)

---
+++ audio/puddletag/files/patch-setup.py
@@ -0,0 +1,9 @@
+--- setup.py.orig	2011-04-30 09:10:41.000000000 +0200
++++ setup.py	2011-07-04 19:15:43.000000000 +0200
+@@ -28,5 +28,5 @@
+     scripts = ['puddletag'],
+     data_files=[('share/pixmaps/', ('puddletag.xpm', 'puddletag.png',)),
+         ('share/applications/', ('puddletag.desktop',)),
+-        ('share/man/man1/', ('puddletag.1',))]
++        ('man/man1/', ('puddletag.1',))]
+      )
