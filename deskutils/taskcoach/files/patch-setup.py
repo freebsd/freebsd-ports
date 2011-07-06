05e855f5d88d2b128c46bb76a961637eacd8bb2a
kevlo <kevlo@FreeBSD.org>
2011-07-06 08:47:10 +0000

    Update to 1.2.22

--- deskutils/taskcoach/files/patch-setup.py
+++ deskutils/taskcoach/files/patch-setup.py
@@ -1,29 +1,26 @@
---- setup.py.orig	2011-02-06 01:18:05.000000000 +0800
-+++ setup.py	2011-03-04 16:47:57.000000000 +0800
+--- setup.py.orig	2011-06-11 03:03:09.000000000 +0800
++++ setup.py	2011-07-06 16:26:53.000000000 +0800
 @@ -54,7 +54,6 @@
      'license': meta.license,
      'download_url': meta.download,
      'packages': findPackages('taskcoachlib') + findPackages('buildlib'),
 -    'scripts': ['taskcoach.py'],
      'classifiers': [\
-         'Development Status :: 3 - Alpha',
+         'Development Status :: 5 - Production/Stable',
          'Intended Audience :: End Users/Desktop',
-@@ -73,14 +72,12 @@
+@@ -76,14 +75,8 @@
      setupOptions['classifiers'].append('Natural Language :: %s'%language)
- 
- # Add data files for Debian-based systems:
--current_dist = [dist.lower() for dist in platform.dist()]
--if 'debian' in current_dist or 'ubuntu' in current_dist:
--    setupOptions['data_files'] = [\
--        ('share/applications', ['build.in/fedora/taskcoach.desktop']), 
--        ('share/pixmaps', ['icons.in/taskcoach.png'])]
-+setupOptions['data_files'] = [\
-+    ('share/applications', ['build.in/fedora/taskcoach.desktop']), 
-+    ('share/pixmaps', ['icons.in/taskcoach.png'])]
  
  system = platform.system()
 -if system == 'Linux':
 +if (system == 'Linux') or (system == 'FreeBSD'):
      setupOptions['package_data'] = {'taskcoachlib': ['bin.in/linux/_pysyncml.so']}
+-    # Add data files for Debian-based systems:
+-    current_dist = [dist.lower() for dist in platform.dist()]
+-    if 'debian' in current_dist or 'ubuntu' in current_dist:
+-        setupOptions['data_files'] = [\
+-	    ('share/applications', ['build.in/fedora/taskcoach.desktop']), 
+-	    ('share/pixmaps', ['icons.in/taskcoach.png'])]
  elif system == 'Windows':
      setupOptions['scripts'].append('taskcoach.pyw')
+ elif system == 'Darwin':
