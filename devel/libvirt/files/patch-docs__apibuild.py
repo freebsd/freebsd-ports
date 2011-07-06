96733b7b6878a0c91db5d7c3f01f80ee0e5fb872
beech <beech@FreeBSD.org>
2011-07-06 22:34:54 +0000

    - Update to 0.9.3
    PR:		ports/158688
    Submitted by:	Jason Helfman <jhelfman@experts-exchange.com> (maintainer)

--- devel/libvirt/files/patch-docs__apibuild.py
+++
@@ -1,8 +0,0 @@
---- ./docs/apibuild.py	2011-06-07 21:31:43.000000000 -0700
-+++ ./docs/apibuild.py	2011-06-07 21:31:55.000000000 -0700
-@@ -1,4 +1,4 @@
--#!/usr/bin/python -u
-+#!/usr/bin/env python
- #
- # This is the API builder, it parses the C sources and build the
- # API formal description in XML.
