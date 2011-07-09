0a888c56fdba61e8365dfafb31953a316bd520af
Mark Linimon <linimon@FreeBSD.org>
2011-07-09 23:51:44 +0000

    Update to 2.3.5.  Changes:
     - Add SPF support
     - Added rotation of name servers - SF Patch ID: 2795929.
     - Address sourceforge patch requests 2981978, 2795932 to add revlookupall
       and raise DNSError instead of IndexError on server fail.
     - Use blocking IO with timeout for TCP replies.
     - Don't try to close socket when never opened.
     - Clarify CNRI (academic-style) license.
    
    PR:		ports/156029
    Submited by:	Ruslan Mahmatkhanov <cvs-src@yandex.ru>

--- dns/py-dns/files/patch-DNS-Type.py
+++ dns/py-dns/files/patch-DNS-Type.py
@@ -1,7 +1,11 @@
---- DNS/Type.py.orig	Wed Oct 11 05:44:59 2006
-+++ DNS/Type.py	Wed Oct 11 05:46:26 2006
-@@ -1,3 +1,4 @@
-+# This Python file uses the following encoding: iso8859-1
- """
-  $Id: Type.py,v 1.6 2002/03/19 12:41:33 anthonybaxter Exp $
- 
+--- DNS/Type.py.orig	2011-03-16 23:06:39.000000000 +0300
++++ DNS/Type.py	2011-03-29 12:44:27.000000000 +0400
+@@ -74,7 +74,7 @@
+ # added identifying header to top of each file
+ #
+ # Revision 1.3  2001/07/19 07:38:28  anthony
+-# added type code for SRV. From Michael Ströder.
++# added type code for SRV. From Michael StrÃ¶der.
+ #
+ # Revision 1.2  2001/07/19 06:57:07  anthony
+ # cvs keywords added
