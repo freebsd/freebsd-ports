bc657153de8fa2bf3a9b7ba5facc5c65b8b97323
linimon <linimon@FreeBSD.org>
2011-07-09 23:51:44 +0000

    Update to 2.3.5.  Changes:
     - Add SPF support
     - Added rotation of name servers - SF Patch ID: 2795929.
     - Address sourceforge patch requests 2981978, 2795932 to add revlookupall
       and raise DNSError instead of IndexError on server fail.
     - Use blocking IO with timeout for TCP replies.
     - Don't try to close socket when never opened.
     - Clarify CNRI (academic-style) license.
    
    PR:		ports/156029
    Submited by:	Ruslan Mahmatkhanov <cvs-src@yandex.ru>

--- dns/py-dns/files/patch-DNS-Lib.py
+++ dns/py-dns/files/patch-DNS-Lib.py
@@ -1,7 +1,15 @@
---- DNS/Lib.py.orig	Wed Oct 11 05:45:08 2006
-+++ DNS/Lib.py	Wed Oct 11 05:46:19 2006
-@@ -1,3 +1,4 @@
-+# This Python file uses the following encoding: iso8859-1
- """
-  $Id: Lib.py,v 1.11 2002/03/19 13:05:02 anthonybaxter Exp $
- 
+--- DNS/Lib.py.orig	2011-03-16 23:06:39.000000000 +0300
++++ DNS/Lib.py	2011-03-29 12:42:45.000000000 +0400
+@@ -706,10 +706,10 @@
+ # added identifying header to top of each file
+ #
+ # Revision 1.7  2001/07/19 07:50:44  anthony
+-# Added SRV (RFC 2782) support. Code from Michael Strˆder.
++# Added SRV (RFC 2782) support. Code from Michael Str√∂der.
+ #
+ # Revision 1.6  2001/07/19 07:39:18  anthony
+-# 'type' -> 'rrtype' in getRRheader(). Fix from Michael Strˆder.
++# 'type' -> 'rrtype' in getRRheader(). Fix from Michael Str√∂der.
+ #
+ # Revision 1.5  2001/07/19 07:34:19  anthony
+ # oops. glitch in storeRR (fixed now).
