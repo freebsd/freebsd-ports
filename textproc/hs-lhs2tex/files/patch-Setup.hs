c0e538d0196fdf5c5501adec2bf121f72289807e
Ashish SHUKLA <ashish@FreeBSD.org>
2011-07-10 02:17:14 +0000

    - Update to 1.17
    Obtained from:	FreeBSD Haskell

--- textproc/hs-lhs2tex/files/patch-Setup.hs
+++ textproc/hs-lhs2tex/files/patch-Setup.hs
@@ -1,5 +1,5 @@
---- ./Setup.hs.orig	2010-07-22 12:13:20.000000000 +0200
-+++ ./Setup.hs	2011-03-27 01:53:35.109600797 +0100
+--- ./Setup.hs.orig	2011-03-17 11:23:52.000000000 +0100
++++ ./Setup.hs	2011-05-22 14:33:40.000000000 +0200
 @@ -204,7 +204,7 @@
                         else docdir (absoluteInstallDirs pd lbi cd) `joinFileName` "doc"
          let manDir = if isWindows
@@ -7,17 +7,5 @@
 -                       else datadir (absoluteInstallDirs pd lbi cd) `joinFileName` ".." `joinFileName` "man" `joinFileName` "man1"
 +                       else prefix (absoluteInstallDirs pd lbi cd) `joinFileName` "man" `joinFileName` "man1"
          createDirectoryIfMissing True docDir
-         copyFileVerbose v (lhs2texDocDir `joinFileName` "Guide2.pdf") (docDir `joinFileName` "Guide2.pdf")
+         installOrdinaryFile v (lhs2texDocDir `joinFileName` "Guide2.pdf") (docDir `joinFileName` "Guide2.pdf")
          when (not isWindows) $
-@@ -291,9 +291,9 @@
-              do  let mProg = lookupProgram (simpleProgram progName) programConf
-                  case mProg of
-                    Just (ConfiguredProgram { programLocation = UserSpecified p,
--                                             programArgs = args })  -> return (p,args)
-+                                             programDefaultArgs = args })  -> return (p,args)
-                    Just (ConfiguredProgram { programLocation = FoundOnSystem p,
--                                             programArgs = args })  -> return (p,args)
-+                                             programDefaultArgs = args })  -> return (p,args)
-                    _ -> (die (progName ++ " command not found"))
- 
- -- | Run a command in a specific environment and return the output and errors.
