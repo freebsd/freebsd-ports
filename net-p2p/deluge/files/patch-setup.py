ada6e4972553d353363c2d12e260f483e63a25f9
jsa <jsa@FreeBSD.org>
2011-07-11 14:36:19 +0000

    Unbreak port and update to 1.3.2. Take maintainership.
    Approved by:	kwm (mentor), pav (portmgr)

--- net-p2p/deluge/files/patch-setup.py
+++ net-p2p/deluge/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2010-09-18 15:31:07.000000000 -0300
-+++ setup.py	2010-09-19 23:33:09.000000000 -0300
-@@ -211,6 +203,7 @@
+--- ./setup.py.orig	2011-07-08 18:15:27.000000000 -0400
++++ ./setup.py	2011-07-08 18:16:21.000000000 -0400
+@@ -213,6 +213,7 @@
      build_libtorrent = True
  else:
      build_libtorrent = False
@@ -8,19 +8,24 @@
  
  if build_libtorrent:
      got_libtorrent = False
-@@ -399,7 +392,7 @@
+@@ -401,27 +402,6 @@
  
  # Data files to be installed to the system
  _data_files = [
 -    ('share/icons/scalable/apps', ['deluge/data/icons/scalable/apps/deluge.svg']),
-+    ('share/icons/hicolor/scalable/apps', ['deluge/data/icons/scalable/apps/deluge.svg']),
-     ('share/icons/hicolor/128x128/apps', ['deluge/data/icons/hicolor/128x128/apps/deluge.png']),
-     ('share/icons/hicolor/16x16/apps', ['deluge/data/icons/hicolor/16x16/apps/deluge.png']),
-     ('share/icons/hicolor/192x192/apps', ['deluge/data/icons/hicolor/192x192/apps/deluge.png']),
-@@ -413,13 +406,7 @@
-     ('share/icons/hicolor/72x72/apps', ['deluge/data/icons/hicolor/72x72/apps/deluge.png']),
-     ('share/icons/hicolor/96x96/apps', ['deluge/data/icons/hicolor/96x96/apps/deluge.png']),
-     ('share/applications', ['deluge/data/share/applications/deluge.desktop']),
+-    ('share/icons/hicolor/128x128/apps', ['deluge/data/icons/hicolor/128x128/apps/deluge.png']),
+-    ('share/icons/hicolor/16x16/apps', ['deluge/data/icons/hicolor/16x16/apps/deluge.png']),
+-    ('share/icons/hicolor/192x192/apps', ['deluge/data/icons/hicolor/192x192/apps/deluge.png']),
+-    ('share/icons/hicolor/22x22/apps', ['deluge/data/icons/hicolor/22x22/apps/deluge.png']),
+-    ('share/icons/hicolor/24x24/apps', ['deluge/data/icons/hicolor/24x24/apps/deluge.png']),
+-    ('share/icons/hicolor/256x256/apps', ['deluge/data/icons/hicolor/256x256/apps/deluge.png']),
+-    ('share/icons/hicolor/32x32/apps', ['deluge/data/icons/hicolor/32x32/apps/deluge.png']),
+-    ('share/icons/hicolor/36x36/apps', ['deluge/data/icons/hicolor/36x36/apps/deluge.png']),
+-    ('share/icons/hicolor/48x48/apps', ['deluge/data/icons/hicolor/48x48/apps/deluge.png']),
+-    ('share/icons/hicolor/64x64/apps', ['deluge/data/icons/hicolor/64x64/apps/deluge.png']),
+-    ('share/icons/hicolor/72x72/apps', ['deluge/data/icons/hicolor/72x72/apps/deluge.png']),
+-    ('share/icons/hicolor/96x96/apps', ['deluge/data/icons/hicolor/96x96/apps/deluge.png']),
+-    ('share/applications', ['deluge/data/share/applications/deluge.desktop']),
 -    ('share/pixmaps', ['deluge/data/pixmaps/deluge.png', 'deluge/data/pixmaps/deluge.xpm']),
 -    ('share/man/man1', [
 -        'docs/man/deluge.1',
@@ -28,7 +33,6 @@
 -        'docs/man/deluge-gtk.1',
 -        'docs/man/deluge-web.1',
 -        'docs/man/deluge-console.1'])
-+    ('share/pixmaps', ['deluge/data/pixmaps/deluge.png', 'deluge/data/pixmaps/deluge.xpm'])
  ]
  
- # Main setup
+ entry_points = {
