4ddf65715fd6556c23a832a38cf1d9a26a293660
Alex Dupre <ale@FreeBSD.org>
2011-07-12 14:44:51 +0000

    Update to 5.5.14 release.

--- databases/percona55-server/files/patch-cmake_plugin.cmake
+++ databases/percona55-server/files/patch-cmake_plugin.cmake
@@ -1,10 +1,10 @@
---- cmake/plugin.cmake.orig	2011-04-12 09:41:40.000000000 +0200
-+++ cmake/plugin.cmake	2011-04-12 09:42:25.000000000 +0200
-@@ -179,7 +179,6 @@
+--- cmake/plugin.cmake.orig	2011-06-21 18:42:39.000000000 +0200
++++ cmake/plugin.cmake	2011-07-12 09:24:41.000000000 +0200
+@@ -196,7 +196,6 @@
        OUTPUT_NAME "${ARG_MODULE_OUTPUT_NAME}")  
      # Install dynamic library
      MYSQL_INSTALL_TARGETS(${target} DESTINATION ${INSTALL_PLUGINDIR} COMPONENT Server)
 -    INSTALL_DEBUG_TARGET(${target} DESTINATION ${INSTALL_PLUGINDIR}/debug)
-   ELSE()
-     IF(WITHOUT_${plugin})
-       # Update cache variable
+     # For internal testing in PB2, append collections files
+     IF(DEFINED ENV{PB2WORKDIR})
+       PLUGIN_APPEND_COLLECTIONS(${plugin})
