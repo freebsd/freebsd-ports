946b4754b800aa343e90511f85cb9346fabeab28
arved <arved@FreeBSD.org>
2011-07-14 15:44:34 +0000

    Fix build with clang
    PR:		158554
    Submitted by:	Niclas Zeising

---
+++ x11/libsx/files/patch-src-main.c
@@ -0,0 +1,11 @@
+--- src/main.c.orig	2011-07-01 09:34:23.000000000 +0200
++++ src/main.c	2011-07-01 09:34:37.000000000 +0200
+@@ -43,7 +43,7 @@
+ 
+ 
+ 
+-void main(int argc, char **argv)
++int main(int argc, char **argv)
+ {
+   MyProgram mydata;
+ 
