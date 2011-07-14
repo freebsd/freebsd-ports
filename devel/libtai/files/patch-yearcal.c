cf6025e37e32032baef6954b19647c3d51225661
Tilman Keskinoz <arved@FreeBSD.org>
2011-07-14 15:37:22 +0000

    Fix build with clang
    PR:		158555
    Submitted by:	Niclas Zeising

---
+++ devel/libtai/files/patch-yearcal.c
@@ -0,0 +1,11 @@
+--- yearcal.c.orig	1998-10-13 18:52:20.000000000 +0200
++++ yearcal.c	2011-07-01 09:45:48.000000000 +0200
+@@ -16,7 +16,7 @@
+ , "December"
+ } ;
+ 
+-void main(argc,argv)
++int main(argc,argv)
+ int argc;
+ char **argv;
+ {
