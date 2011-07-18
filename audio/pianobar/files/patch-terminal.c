09c84a461fabca7e7f20bd4d40db781fb7e34d22
Josh Paetzel <jpaetzel@FreeBSD.org>
2011-07-18 14:20:11 +0000

    Update to 2011.07.09
    PR:	ports/158951
    Submitted by:	John Hixson
    Sponsored by:	iXsystems

--- audio/pianobar/files/patch-terminal.c
+++ audio/pianobar/files/patch-terminal.c
@@ -1,11 +1,10 @@
---- src/terminal.c.orig	2011-01-24 04:00:25.000000000 -0800
-+++ src/terminal.c	2011-04-07 14:29:03.000000000 -0700
-@@ -21,9 +21,6 @@
+--- src/terminal.c.orig	2011-07-09 01:40:54.000000000 -0700
++++ src/terminal.c	2011-07-14 12:07:01.000000000 -0700
+@@ -21,7 +21,6 @@
  THE SOFTWARE.
  */
  
 -#define _POSIX_C_SOURCE 1 /* fileno() */
--#define _BSD_SOURCE /* setlinebuf() */
--
+ #define _BSD_SOURCE /* setlinebuf() */
+ 
  #include <termios.h>
- #include <stdio.h>
--- audio/pianobar/files/patch-ui.c
+++ audio/pianobar/files/patch-ui.c
@@ -1,12 +1,10 @@
---- src/ui.c.orig	2011-01-24 04:00:25.000000000 -0800
-+++ src/ui.c	2011-04-07 14:29:19.000000000 -0700
-@@ -23,9 +23,6 @@
+--- src/ui.c.orig	2011-07-09 01:40:54.000000000 -0700
++++ src/ui.c	2011-07-14 12:02:04.000000000 -0700
+@@ -23,7 +23,6 @@
  
  /* everything that interacts with the user */
  
 -#define _POSIX_C_SOURCE 1 /* fileno() */
--#define _BSD_SOURCE /* strdup() */
--
+ #define _BSD_SOURCE /* strdup() */
+ 
  #include <stdio.h>
- #include <stdarg.h>
- #include <unistd.h>
