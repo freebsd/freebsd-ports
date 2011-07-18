17f544534eb424175cd6d3d269aeee2e38afa860
Pietro Cerutti <gahr@FreeBSD.org>
2011-07-18 08:44:53 +0000

    - Update to latest snapshot 0.9.95-30-06-11

---
+++ x11-toolkits/gnocl/files/patch-src_gnocl.c
@@ -0,0 +1,11 @@
+--- src/gnocl.c.orig	2011-07-18 10:33:09.000000000 +0200
++++ src/gnocl.c	2011-07-18 10:33:16.000000000 +0200
+@@ -647,7 +647,7 @@
+ 	/* non-gtk widgets */
+ 	{ "spinner", gnoclSpinnerCmd },
+ 	{ "dial", gnoclDialCmd },
+-	{ "imageViewer", gnoclImageViewerCmd },
++//	{ "imageViewer", gnoclImageViewerCmd },
+ 
+ 	/* gnocl only widget */
+ 	{ "splashScreen", gnoclSplashScreenCmd },
