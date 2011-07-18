f53eee7adf87036c54e591e6fe0155e4b4485ce6
jpaetzel <jpaetzel@FreeBSD.org>
2011-07-18 14:20:11 +0000

    Update to 2011.07.09
    PR:	ports/158951
    Submitted by:	John Hixson
    Sponsored by:	iXsystems

--- audio/pianobar/files/patch-main.c
+++ audio/pianobar/files/patch-main.c
@@ -1,12 +1,10 @@
---- src/main.c.orig	2011-01-24 04:00:25.000000000 -0800
-+++ src/main.c	2011-04-07 14:28:49.000000000 -0700
-@@ -21,9 +21,6 @@
+--- src/main.c.orig	2011-07-09 01:40:54.000000000 -0700
++++ src/main.c	2011-07-14 12:01:40.000000000 -0700
+@@ -21,7 +21,6 @@
  THE SOFTWARE.
  */
  
 -#define _POSIX_C_SOURCE 1 /* fileno() */
--#define _BSD_SOURCE /* strdup() */
--
+ #define _BSD_SOURCE /* strdup() */
+ 
  /* system includes */
- #include <stdlib.h>
- #include <string.h>
--- audio/pianobar/files/patch-waitress.c
+++ audio/pianobar/files/patch-waitress.c
@@ -1,12 +1,10 @@
---- src/libwaitress/waitress.c.orig	2011-01-24 04:00:25.000000000 -0800
-+++ src/libwaitress/waitress.c	2011-04-07 14:29:37.000000000 -0700
-@@ -21,9 +21,6 @@
+--- src/libwaitress/waitress.c.orig	2011-07-09 01:40:54.000000000 -0700
++++ src/libwaitress/waitress.c	2011-07-14 12:02:14.000000000 -0700
+@@ -21,7 +21,6 @@
  THE SOFTWARE.
  */
  
 -#define _POSIX_C_SOURCE 1 /* required by getaddrinfo() */
--#define _BSD_SOURCE /* snprintf() */
--
+ #define _BSD_SOURCE /* snprintf() */
+ 
  #include <sys/types.h>
- #include <sys/socket.h>
- #include <netdb.h>
