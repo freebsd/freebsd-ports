79a61128a49c54045700d284bfc27c4afae47c82
stephen <stephen@FreeBSD.org>
2011-07-21 20:19:33 +0000

    - Fix PATCH_SITES, and update to version 3 of Kloeckner's arscnd patch. - Fix patch.tar.gz being unpacked into the wrong directory. - Respect AR, RANLIB, LD, LDFLAGS, etc. in ARmake.inc. - Fix install of arpack.h. - Fix broken sparc64 build. - Add tests. - Various other cleanups.
    PR:		ports/159075
    Submitted by:	bf
    Approved by:	maho (mentor, implicit)

--- math/arpack/files/patch-UTIL+second.f
+++ math/arpack/files/patch-UTIL+second.f
@@ -1,5 +1,5 @@
---- UTIL/second.f~	Tue Oct  3 03:56:44 1995
-+++ UTIL/second.f	Wed Jan 10 23:06:17 2007
+--- UTIL/arscnd.f.bak	2011-07-20 19:47:43.000000000 -0400
++++ UTIL/arscnd.f	2011-07-20 19:48:05.000000000 -0400
 @@ -21,7 +21,6 @@
  *     ..
  *     .. External Functions ..
