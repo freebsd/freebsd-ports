7276ec8bda4a94c28127316d5a9f33f010353514
stas <stas@FreeBSD.org>
2011-08-01 07:55:34 +0000

    - Update to 3.0.0 release.  As the port was version 3.0 before (3.0 development   snapshot), I'm keeping version the same, and just bumping the port   revision.

--- graphics/rawtherapee/files/patch-rtengine_myfile.h
+++ graphics/rawtherapee/files/patch-rtengine_myfile.h
@@ -1,12 +1,11 @@
---- rtengine/myfile.h.orig	2010-01-10 18:33:11.000000000 -0800
-+++ rtengine/myfile.h	2010-01-10 18:33:35.000000000 -0800
-@@ -22,6 +22,9 @@
+--- rtengine/myfile.h.orig	2011-07-30 14:05:38.000000000 -0700
++++ rtengine/myfile.h	2011-07-31 19:27:22.000000000 -0700
+@@ -22,6 +22,8 @@
  #include <glib/gstdio.h>
  #include <stdio.h>
  #include <string.h>
-+
 +#undef feof
 +#undef getc
  struct IMFILE {
- 
+ 	int fd;
  	int pos;
