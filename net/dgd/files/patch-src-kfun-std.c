0c48c80355842c2a891f808ec762b1906ad61cbc
glewis <glewis@FreeBSD.org>
2011-08-03 06:31:02 +0000

    . Update to 1.4.11. . Add an OPTIONS setting to turn on function pointer support.

--- net/dgd/files/patch-src-kfun-std.c
+++ net/dgd/files/patch-src-kfun-std.c
@@ -1,11 +1,13 @@
---- src/kfun/std.c.orig	2010-04-25 14:29:57.000000000 -0700
-+++ src/kfun/std.c	2010-04-25 14:30:09.000000000 -0700
-@@ -1278,10 +1278,6 @@
+$FreeBSD$
+
+--- src/kfun/std.c.orig	2011-07-30 00:30:37.000000000 -0700
++++ src/kfun/std.c	2011-07-30 00:30:49.000000000 -0700
+@@ -1291,10 +1291,6 @@
  	error("open_port() in special purpose object");
      }
  
 -    if (obj->flags & O_DRIVER) {
--        error("open_port() in driver object");
+-	error("open_port() in driver object");
 -    }
 -
      if (f->level != 0) {
