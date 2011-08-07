f62451dfef4e28ac1dec7b49c02f8e1b9c5cbd4a
Matthias Andree <mandree@FreeBSD.org>
2011-08-07 18:05:35 +0000

    Skip self-test more readily without addresses.

--- security/openvpn/files/patch-t_cltsrv.sh
+++ security/openvpn/files/patch-t_cltsrv.sh
@@ -1,8 +1,17 @@
---- ./t_cltsrv.sh.orig	2011-07-20 22:10:38.000000000 +0200
-+++ ./t_cltsrv.sh	2011-07-20 22:10:52.000000000 +0200
-@@ -33,7 +33,7 @@
+--- ./t_cltsrv.sh.orig	2011-05-16 14:21:55.000000000 +0200
++++ ./t_cltsrv.sh	2011-08-07 20:03:27.000000000 +0200
+@@ -26,14 +26,13 @@
+     FreeBSD)
+     # FreeBSD jails map the outgoing IP to the jail IP - we need to
+     # allow the real IP unless we want the test to run forever.
+-    if test "`sysctl 2>/dev/null -n security.jail.jailed`" = 1 \
+-    || ps -ostate= -p $$ | grep -q J; then
++    if true ; then
+ 	addopts="--float"
+ 	if test "x`ifconfig | grep inet`" = x ; then
  	    echo "###"
- 	    echo "### To run the test in a FreeBSD jail, you MUST add an IP alias for the jail's IP."
+-	    echo "### To run the test in a FreeBSD jail, you MUST add an IP alias for the jail's IP."
++	    echo "### To run this test, it needs an IPv4 address.  Skipping."
  	    echo "###"
 -	    exit 1
 +	    exit 77
