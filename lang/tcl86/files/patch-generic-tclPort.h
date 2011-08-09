a33b83db3fa0a19ef369c3915e73f62a3b394fc1
mm <mm@FreeBSD.org>
2011-08-09 15:00:44 +0000

    Update to 8.6b2

--- lang/tcl86/files/patch-generic-tclPort.h
+++ lang/tcl86/files/patch-generic-tclPort.h
@@ -1,11 +1,11 @@
---- ../generic/tclPort.h.orig	Tue Jan 30 11:39:50 2007
-+++ ../generic/tclPort.h	Tue Jan 30 11:41:05 2007
-@@ -24,7 +24,7 @@
- #if defined(__WIN32__)
- #   include "../win/tclWinPort.h"
- #else
+--- ../generic/tclPort.h.orig	2011-04-26 17:14:03.000000000 +0200
++++ ../generic/tclPort.h	2011-08-09 14:25:40.620587462 +0200
+@@ -22,7 +22,7 @@
+ #endif
+ #include "tcl.h"
+ #if !defined(_WIN32)
 -#   include "tclUnixPort.h"
 +#   include "../unix/tclUnixPort.h"
  #endif
  
- #if !defined(LLONG_MIN)
+ #if defined(__CYGWIN__)
