97c051f5965e5eabefa40f8dd41114b265f9de75
osa <osa@FreeBSD.org>
2011-08-10 16:22:58 +0000

    Update from 3.0.7 to 3.0.8:
    1) www/rubygem-passenger;
    
    2) passenger support for www/nginx and www/nginx-devel;
    
    Also add patch for fix NULL pointer crash that occurs when
    HTTP 1.0 Host header isn't given, obtained from passenger's
    upstream.
    
    Approved by:	maintainer [1]

--- www/nginx-devel/files/extra-patch-passenger::build::nginx.rb
+++ www/nginx-devel/files/extra-patch-passenger::build::nginx.rb
@@ -1,5 +1,5 @@
---- ../passenger-3.0.7/build/nginx.rb.orig	2010-12-07 13:57:32.000000000 +0300
-+++ ../passenger-3.0.7/build/nginx.rb	2010-12-07 13:58:13.000000000 +0300
+--- ../passenger-3.0.8/build/nginx.rb.orig	2010-12-07 13:57:32.000000000 +0300
++++ ../passenger-3.0.8/build/nginx.rb	2010-12-07 13:58:13.000000000 +0300
 @@ -23,10 +23,7 @@
  
  desc "Build Nginx helper agent"
