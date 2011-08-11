d2da483e52f989abac8c6a85fd0c637ffe80025c
Brendan Fabeny <bf@FreeBSD.org>
2011-08-11 21:12:52 +0000

    fix build with lang/gcc46
    Reported by:	pav, gerald
    Approved by:	stephen

---
+++ cad/gmsh/files/patch-Geo__CustomContainer.h
@@ -0,0 +1,10 @@
+--- Geo/CustomContainer.h.orig	2010-10-15 09:35:00.000000000 -0400
++++ Geo/CustomContainer.h	2011-08-09 05:12:35.000000000 -0400
+@@ -8,6 +8,7 @@
+ #ifndef _CUSTOMCONTAINER_H_
+ #define _CUSTOMCONTAINER_H_
+ 
++#include <cstddef>
+ #include <cstdlib>
+ #include <cstring>
+ 
