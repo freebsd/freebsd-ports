b214a7de2ab8b6be1f63e79ee294fd8f12256630
wxs <wxs@FreeBSD.org>
2011-08-11 16:31:19 +0000

    - Update to 2.1.2.
    PR:		ports/159649
    Submitted by:	arrowdodger <6yearold@gmail.com> (maintainer)

--- devel/tortoisehg2/files/patch-setup.py
+++ devel/tortoisehg2/files/patch-setup.py
@@ -1,5 +1,27 @@
 --- setup.py_	2011-05-02 07:06:56.000000000 +0400
 +++ setup.py	2011-05-25 15:54:34.833312978 +0400
+@@ -283,7 +283,7 @@
+ class build(_build_orig):
+     sub_commands = [
+         ('build_qt', None),
+-        ('build_mo', None),
++%%NLS%%        ('build_mo', None),
+         ] + _build_orig.sub_commands
+ 
+ class clean(_clean_orig):
+@@ -299,10 +299,10 @@
+ cmdclass = {
+         'build': build,
+         'build_qt': build_qt ,
+-        'build_mo': build_mo ,
++%%NLS%%        'build_mo': build_mo ,
+         'clean': clean,
+         'clean_local': clean_local,
+-        'update_pot': update_pot ,
++%%NLS%%        'update_pot': update_pot ,
+     }
+ 
+ def setup_windows(version):
 @@ -407,11 +407,11 @@
      _data_files = [(os.path.join('share/pixmaps/tortoisehg', root),
          [os.path.join(root, file_) for file_ in files])
