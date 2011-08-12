61232ace19fa3379e2f61feeeddabb1ef6ee3b95
ehaupt <ehaupt@FreeBSD.org>
2011-08-12 06:44:50 +0000

    Update to 0.5.0
    PR:		159426
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- audio/patchage/files/patch-src-Patchage.cpp
+++ audio/patchage/files/patch-src-Patchage.cpp
@@ -1,15 +1,14 @@
---- src/Patchage.cpp.orig	2008-07-22 22:19:44.000000000 +0200
-+++ src/Patchage.cpp	2008-07-22 22:22:20.000000000 +0200
-@@ -342,6 +342,12 @@
- }
+--- src/Patchage.cpp.orig	2011-01-10 07:53:06.000000000 +0900
++++ src/Patchage.cpp	2011-07-24 02:43:38.000000000 +0900
+@@ -21,6 +21,11 @@
+ #include <fstream>
+ #include <sstream>
  
- 
-+static float
-+log2f(float x)
-+{
-+	return (logf(x) / logf(2.0));
-+}
++#include <sys/param.h>
++#if __FreeBSD_version <= 704100 || (__FreeBSD_version >= 800000 && __FreeBSD_version < 802502) || (__FreeBSD_version >= 900000 && __FreeBSD_version < 900027)
++#define log2f(x) (logf(x) / logf(2))
++#endif
 +
- void
- Patchage::update_toolbar()
- {
+ #include <gtk/gtkwindow.h>
+ #include <libglademm/xml.h>
+ #include <libgnomecanvasmm.h>
