a7635e40e7b5dc1adea35a79159534d97d0320a1
kwm <kwm@FreeBSD.org>
2011-08-13 20:58:26 +0000

    Update to 9.6.1.
    PR:		ports/159718
    Submitted by:	Ruslan Mahmatkhanov <cvs-src@yandex.ru>

--- games/gcompris/files/patch-src_gcompris_gcompris.c
+++ games/gcompris/files/patch-src_gcompris_gcompris.c
@@ -1,11 +1,11 @@
---- src/gcompris/gcompris.c.orig	2010-02-13 08:38:20.000000000 -0500
-+++ src/gcompris/gcompris.c	2010-03-21 14:22:42.000000000 -0400
-@@ -1202,7 +1202,7 @@ static void load_properties ()
- #ifdef NSBUNDLE
-   exec_prefix = gcompris_nsbundle_resource ();
- #else
+--- src/gcompris/gcompris.c.orig	2011-02-22 23:55:08.000000000 +0300
++++ src/gcompris/gcompris.c	2011-08-12 10:54:59.000000000 +0400
+@@ -1295,7 +1295,7 @@
+       g_error_free (error);
+     }
+ 
 -  exec_prefix = gbr_find_exe_dir(NULL);
 +  exec_prefix = gbr_find_exe_dir("%%PREFIX%%");
- #endif
    g_warning("exec_prefix %s\n", (exec_prefix==NULL ? "NONE" : exec_prefix));
  
+   {
