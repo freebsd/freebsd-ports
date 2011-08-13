8fd93da743a9c291386aa935110215ee8f796662
bf <bf@FreeBSD.org>
2011-08-13 12:23:12 +0000

    Adjust headers for use with lang/gcc46
    Reported by:	gerald, pav

--- x11-toolkits/qt33/files/patch-src_tools-qsettings.cpp
+++ x11-toolkits/qt33/files/patch-src_tools-qsettings.cpp
@@ -1,6 +1,16 @@
---- src/tools/qsettings.cpp.orig	Fri Sep  2 14:42:53 2005
-+++ src/tools/qsettings.cpp	Mon Mar 20 18:36:27 2006
-@@ -500,7 +500,12 @@
+--- src/tools/qsettings.cpp.orig	2007-02-02 09:01:03.000000000 -0500
++++ src/tools/qsettings.cpp	2011-08-12 03:41:18.000000000 -0400
+@@ -34,6 +34,9 @@
+ ** not clear to you.
+ **
+ **********************************************************************/
++#ifndef Q_WS_WIN
++#include <stdlib.h>
++#endif
+ 
+ #include "qplatformdefs.h"
+ 
+@@ -500,7 +503,12 @@
      } );
  #endif // Q_OS_TEMP
  #else
