5e2cffad9430d2b4d0feffdd459669d463aade12
Gabor Pali <pgj@FreeBSD.org>
2011-08-14 17:46:17 +0000

    A library that performs fast, accurate conversion between double precision floating point and text.  This library is implemented as bindings to the C++ double-conversion library written by Florian Loitsch at Google: http://code.google.com/p/double-conversion/.
    The Text versions of these functions are about 30 times faster than the
    default show implementation for the Double type. The ByteString versions
    are slower than the Text versions; roughly half the speed. (This seems
    to be due to the cost of allocating ByteString values via malloc.)
    
    As a final note, be aware that the bytestring-show package is about 50%
    slower than simply using show.
    
    WWW:	http://github.com/mailrank/double-conversion
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -61,6 +61,7 @@ digest_port=			security/hs-digest
 directory-tree_port=		devel/hs-directory-tree
 distributive_port=		math/hs-distributive
 dlist_port=			devel/hs-dlist
+double-conversion_port=		textproc/hs-double-conversion
 enumerator_port=		devel/hs-enumerator
 erf_port=			math/hs-erf
 fastcgi_port=			www/hs-fastcgi		# lib_depends
