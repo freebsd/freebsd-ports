ea043f6bc0546786aec81af966c4d2547a045257
anders <anders@FreeBSD.org>
2011-08-15 22:12:16 +0000

    Update to 1.3.2.
    PR:		158871
    Submitted by:	Ralf Wenk <IZ-FreeBSD0701@hs-karlsruhe.de> (with changes)

--- net/dante/files/patch-ltmain.sh
+++ net/dante/files/patch-ltmain.sh
@@ -1,14 +1,14 @@
---- ltmain.sh.orig	Wed Mar 16 21:57:42 2005
-+++ ltmain.sh	Wed Mar 16 21:59:55 2005
-@@ -5540,10 +5540,12 @@
+--- ltmain.sh.orig	2008-08-12 19:06:26.000000000 +0200
++++ ltmain.sh	2011-07-12 16:01:37.595732703 +0200
+@@ -6115,10 +6115,12 @@
  	fi
  
  	# Install the pseudo-library for information purposes.
 +	if /usr/bin/false; then
  	name=`$echo "X$file" | $Xsed -e 's%^.*/%%'`
  	instname="$dir/$name"i
- 	$show "$install_prog $instname $destdir/$name"
- 	$run eval "$install_prog $instname $destdir/$name" || exit $?
+ 	$show "$install_prog $extra_mode $instname $destdir/$name"
+ 	$run eval "$install_prog $extra_mode $instname $destdir/$name" || exit $?
 +	fi
  
  	# Maybe install the static library, too.
