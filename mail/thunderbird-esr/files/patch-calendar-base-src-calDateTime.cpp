4cc74dd9dee243258927fdd90604448717e4bf7e
flo <flo@FreeBSD.org>
2011-08-16 18:23:05 +0000

    update to 6.0
    Security:	http://www.vuxml.org/freebsd/834591a9-c82f-11e0-897d-6c626dd55a41.html

---
+++ mail/thunderbird-esr/files/patch-calendar-base-src-calDateTime.cpp
@@ -0,0 +1,8 @@
+--- calendar/base/src/calDateTime.cpp.orig	2011-08-03 20:08:23.361859978 +0200
++++ calendar/base/src/calDateTime.cpp	2011-08-03 20:08:32.351860948 +0200
+@@ -1,4 +1,4 @@
+-﻿/* ***** BEGIN LICENSE BLOCK *****
++/* ***** BEGIN LICENSE BLOCK *****
+  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+  *
+  * The contents of this file are subject to the Mozilla Public License Version
