796de4628fd45bd5f2cc5eae82e10ec53d6518a6
mandree <mandree@FreeBSD.org>
2011-08-16 22:33:30 +0000

    Fix skipping t_cltsrv when IP missing. Really this time.
    Cause was a   trap "... ; exit 1" 0   shell construct that needs to be
    cancelled for the exit 77 to take effect. trap 0 inserted to that end.

--- security/openvpn/files/patch-t_cltsrv.sh
+++ security/openvpn/files/patch-t_cltsrv.sh
@@ -1,6 +1,6 @@
 --- ./t_cltsrv.sh.orig	2011-05-16 14:21:55.000000000 +0200
-+++ ./t_cltsrv.sh	2011-08-07 20:03:27.000000000 +0200
-@@ -26,14 +26,13 @@
++++ ./t_cltsrv.sh	2011-08-17 00:25:49.000000000 +0200
+@@ -26,14 +26,14 @@
      FreeBSD)
      # FreeBSD jails map the outgoing IP to the jail IP - we need to
      # allow the real IP unless we want the test to run forever.
@@ -11,9 +11,10 @@
  	if test "x`ifconfig | grep inet`" = x ; then
  	    echo "###"
 -	    echo "### To run the test in a FreeBSD jail, you MUST add an IP alias for the jail's IP."
-+	    echo "### To run this test, it needs an IPv4 address.  Skipping."
++	    echo "### To run this test, it needs an inet/IP address.  Skipping."
  	    echo "###"
 -	    exit 1
++	    trap 0
 +	    exit 77
  	fi
      fi
