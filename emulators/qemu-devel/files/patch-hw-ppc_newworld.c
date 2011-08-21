4a44907122378cb6b020b3813589b8b4cd2ffab5
Juergen Lock <nox@FreeBSD.org>
2011-08-21 15:49:39 +0000

    - Update to 0.15.0 - Changelog:
    	http://wiki.qemu.org/ChangeLog/0.15

---
+++ emulators/qemu-devel/files/patch-hw-ppc_newworld.c
@@ -0,0 +1,14 @@
+--- a/hw/ppc_newworld.c
++++ b/hw/ppc_newworld.c
+@@ -68,6 +68,11 @@
+ #include "hw/usb.h"
+ #include "blockdev.h"
+ 
++/* FreeBSD headers define this */
++#ifdef round_page
++#undef round_page
++#endif
++
+ #define MAX_IDE_BUS 2
+ #define CFG_ADDR 0xf0000510
+ 
