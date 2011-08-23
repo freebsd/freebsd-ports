737796d6f978fa9f801958a6d03cda6a3ac45c88
zi <zi@FreeBSD.org>
2011-08-23 01:50:48 +0000

    - Update to 3.3.1 [1] - Use USERS/GROUPS [1] - Fix whitespace in Makefile [1] - Add LICENSE [1] - Correct rc script issues [2]
    PR:		ports/159304
    Submitted by:	Ryan Steinmetz (zi@) [1], Doug Barton (dougb@) [2]
    Approved by: 	wxs (mentor), maintainer

---
+++ net-mgmt/nagios/files/patch-html-main.php
@@ -0,0 +1,39 @@
+--- html/main.php.orig	2011-07-26 12:46:26.000000000 +0930
++++ html/main.php	2011-08-20 19:57:13.000000000 +0930
+@@ -36,36 +36,10 @@
+ <div class="product">Nagios<sup><span style="font-size: small;">&reg;</span></sup> Core<sup><span style="font-size: small;">&trade;</span></sup></div>
+ <div class="version">Version 3.3.1</div>
+ <div class="releasedate">July 25, 2011</div>
+-<div class="checkforupdates"><a href="http://www.nagios.org/checkforupdates/?version=3.3.1&product=nagioscore" target="_blank">Check for updates</a></div>
+ <!--<div class="whatsnew"><a href="http://go.nagios.com/nagioscore/whatsnew">Read what's new in Nagios Core 3</a></div>-->
+ </div>
+ 
+ 
+-<div id="updateversioninfo">
+-<?php
+-	$updateinfo=get_update_information();
+-	//print_r($updateinfo);
+-	//$updateinfo['update_checks_enabled']=false;
+-	//$updateinfo['update_available']=true;
+-	if($updateinfo['update_checks_enabled']==false && $this_version!=$updateinfo['update_version']){
+-?>
+-		<div class="updatechecksdisabled">
+-		<div class="warningmessage">Warning: Automatic Update Checks are Disabled!</div>
+-		<div class="submessage">Disabling update checks presents a possible security risk.  Visit <a href="http://www.nagios.org/" target="_blank">nagios.org</a> to check for updates manually or enable update checks in your Nagios config file.</a></div>
+-		</div>
+-<?php
+-		}
+-	else if($updateinfo['update_available']==true){
+-?>
+-		<div class="updateavailable">
+-		<div class="updatemessage">A new version of Nagios Core is available!</div>
+-		<div class="submessage">Visit <a href="http://www.nagios.org/download/" target="_blank">nagios.org</a> to download Nagios <?php echo $updateinfo['update_version'];?>.</div>
+-		</div>
+-<?php
+-		}
+-?>
+-</div>
+-
+ <div id="splashboxes">
+ <div id="splashbox1" class="splashbox">
+ <h2>Get Started</h2>
