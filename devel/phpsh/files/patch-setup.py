1bcd43862c2c06e834118ec765f1d8d5bf447378
ashish <ashish@FreeBSD.org>
2011-08-23 17:44:59 +0000

    phpsh is an interactive shell for php that features readline history, tab completion, quick access to documentation. It was developed at Facebook and ironically, is written mostly in python. It is open source and released under a modified BSD license.
    WWW:	http://phpsh.org/

---
+++ devel/phpsh/files/patch-setup.py
@@ -0,0 +1,36 @@
+
+$FreeBSD$
+
+--- setup.py.orig
++++ setup.py
+@@ -7,15 +7,15 @@
+ sys.path.insert(0, 'src')
+ from phpsh import __version__
+ 
+-if len(sys.argv) > 1 and sys.argv[1] == "build":
+-    build_root = os.path.dirname(os.path.realpath(__file__))
+-    make_dir = os.path.join(build_root, "src/xdebug-clients/geben")
+-    p = Popen(["make", "-C", make_dir])
+-    os.waitpid(p.pid, 0)
++### EMACS:if len(sys.argv) > 1 and sys.argv[1] == "build":
++### EMACS:    build_root = os.path.dirname(os.path.realpath(__file__))
++### EMACS:    make_dir = os.path.join(build_root, "src/xdebug-clients/geben")
++### EMACS:    p = Popen(["gmake", "-C", make_dir])
++### EMACS:    os.waitpid(p.pid, 0)
+ 
+ # something better than this?
+ if os.getenv("USER") == "root":
+-    config_dir = "/etc/phpsh"
++    config_dir = "%%PREFIX%%/etc/phpsh"
+ else:
+     config_dir = os.getenv("HOME") + "/.phpsh"
+ 
+@@ -33,7 +33,7 @@
+                             "xdebug-clients/geben/LICENSE",
+                             "xdebug-clients/geben/Makefile",
+                             "xdebug-clients/geben/geben.el",
+-                            "xdebug-clients/geben/geben.elc",
++### EMACS:                            "xdebug-clients/geben/geben.elc",
+                             "xdebug-clients/geben/help",
+                             "xdebug-clients/geben/tree-widget/geben/*.png"]},
+     scripts=["src/phpsh", "src/dbgp-phpsh.py"],
---
+++ devel/phpsh/files/patch-src_phpsh.py
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/phpsh.py.orig
++++ src/phpsh.py
+@@ -274,7 +274,7 @@
+         self.config.add_section("Emacs")
+ 
+     def read(self):
+-        config_files = ["/etc/phpsh/config"]
++        config_files = ["%%PREFIX%%/etc/phpsh/config"]
+         home = os.getenv("HOME")
+         if home:
+             homestr = home.strip()
