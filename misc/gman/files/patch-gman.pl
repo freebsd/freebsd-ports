fa1ed449bd1d427b070917b28517c8a492668a2d
cy <cy@FreeBSD.org>
2011-08-27 00:10:00 +0000

    Fix broken gman.pl script to display man pages in a browser. HTML rendering was broken.

--- misc/gman/files/patch-gman.pl
+++ misc/gman/files/patch-gman.pl
@@ -1,27 +1,15 @@
---- gman.pl.orig	2011-08-25 13:06:54.517309488 -0700
-+++ gman.pl	2011-08-25 13:12:49.444936858 -0700
-@@ -1,10 +1,10 @@
- #!/usr/bin/perl -w
--use strict;
- 
- if (@ARGV != 2) {
-   print <<_EOF_;
- Content-type: text/html
- 
-+<!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
- <html><head><title>gman.pl: invalid use</title></head>
- <body>
- <h1>gman.pl cannot be used without arguments.</h1>
-@@ -14,14 +14,16 @@
- }
+--- gman.pl.orig	2011-08-26 13:11:36.331148726 -0700
++++ gman.pl	2011-08-26 13:52:56.017429820 -0700
+@@ -15,13 +15,16 @@
  
  my $section = $ARGV[0]; # should also have some sanity check
-+my $cut = "/usr/bin/cut -f 1 -d \" \" ";
  my $name = $ARGV[1];
 -my $man2html = "/usr/lib/cgi-bin/man/man2html";
 -my $path = `man -w $section $name`; chomp $path; $path =~ s/\s*$//;
++my $cut = "/usr/bin/cut -f 1 -d \" \" ";
 +my $man = "/usr/bin/man";
-+my $man2html = "/usr/local/bin/man2html -nodepage ";
++my $man2html = "/usr/local/bin/man2html";
++my $man2html_args =  "-nodepage ";
 +my $path = `$man -w $section $name |$cut`; chomp $path; $path =~ s/\s*$//;
  
  unless (-s "$path") {
@@ -32,7 +20,7 @@
  }
  unless (-s "$path") {
    warn "not -s $path\n";
-@@ -29,12 +31,12 @@
+@@ -29,12 +32,12 @@
  Content-type: text/html
  
  <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
@@ -50,21 +38,29 @@
  end_of_line
  	die;
  }
-@@ -44,15 +46,9 @@
- 	print <<end_of_line;
+@@ -45,19 +48,19 @@
  Content-type: text/html
  
--<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+ <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
 -<HTML><HEAD>
 -<TITLE>man2html missing</TITLE>
 -</HEAD><BODY>
 -<H1>man2html missing</H1>
--
--<p>You have to install <tt>man2html</tt> in order to use this mode in gman.
-+<!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
++<html><head>
++<title>man2html missing</title>
++</head><body>
++<h1>man2html missing</h1>
+ 
+ <p>You have to install <tt>man2html</tt> in order to use this mode in gman.
  
 -</BODY></HTML>
 +</body></html>
  end_of_line
  	die;
  }
+ 
+-my $page = `$man2html $path`;
++my $page = `$man $section $name | man2html $man2html_args`;
+ 
+ # $page =~ s/^Content-type: text\/html\n\n// if (not run as cgi...);
+ 
