7ada7f3113af57f950d86b31863a91d2385a5e67
Lars Engels <lme@FreeBSD.org>
2011-08-28 16:29:39 +0000

    Potential fix build on sparc64
    PR:		ports/160183
    Submitted by:	naddy
    Approved by:	Maintainer (me)

---
+++ net-mgmt/aircrack-ng/files/patch-src_osdep_byteorder.h
@@ -0,0 +1,11 @@
+--- src/osdep/byteorder.h.orig	2011-08-28 18:26:15.000000000 +0200
++++ src/osdep/byteorder.h	2011-08-28 18:26:25.000000000 +0200
+@@ -167,7 +167,7 @@
+ 	 * Solaris
+ 	 * -------
+ 	 */
+-	#if defined(__sparc__)
++	#if defined(__sun) && defined(__SVR4) && defined(__sparc__)
+ 	#include <sys/byteorder.h>
+ 	#include <sys/types.h>
+ 	#include <unistd.h>
