cbe058e06d7bc1e3494c6a8638c1d3084a023b27
dhn <dhn@FreeBSD.org>
2011-08-31 22:14:27 +0000

    - Update to 1.12
    PR:		ports/160112
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- games/xspringies/files/patch-widget.c
+++ games/xspringies/files/patch-widget.c
@@ -1,16 +1,11 @@
---- widget.c.orig	Wed May 30 16:24:53 2007
-+++ widget.c	Wed May 30 16:28:12 2007
-@@ -124,11 +124,11 @@
+--- widget.c.orig	1995-11-11 12:47:22.000000000 +0900
++++ widget.c	2011-08-19 07:11:46.000000000 +0900
+@@ -124,7 +124,7 @@
  extern Pixmap acts_pm;
  void (*notify_func)();
  
+-Pixmap get_pixmap();
 +Pixmap get_pixmap(char *bits, int width, int height, boolean inv);
-+
+ 
  void init_widgets(notify)
  void (*notify)();
- {
--    extern Pixmap get_pixmap();
--
-     numb = nums = numc = numm = cur_type = cur_num = 0;
-     key_active = cur_but = -1;
-     scan_flag = FALSE;
