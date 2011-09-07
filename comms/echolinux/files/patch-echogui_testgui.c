3f504eebb7348961a28e7715f524fb276a0a1771
gahr <gahr@FreeBSD.org>
2011-09-07 08:28:13 +0000

    - Chase x11-toolkits/xforms shlib version bump

---
+++ comms/echolinux/files/patch-echogui_testgui.c
@@ -0,0 +1,10 @@
+--- echogui/testgui.c.orig	2011-09-06 14:21:37.000000000 +0200
++++ echogui/testgui.c	2011-09-06 14:21:42.000000000 +0200
+@@ -1,6 +1,6 @@
+ /* Form definition file generated with fdesign. */
+ 
+-#include <X11/forms.h>
++#include <forms.h>
+ #include <stdlib.h>
+ #include "testgui.h"
+ 
