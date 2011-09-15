b2f06fb4c7cfcb80560d6dde30598c4959872816
Johan van Selst <johans@FreeBSD.org>
2011-09-15 16:51:00 +0000

    - Fix segmentation faults caused by missing libc function prototypes   Returned pointers could be truncated to (potentially smaller) integers
    Reported by:	will (with detailed analysis)

--- archivers/rpm4/files/patch-system.h
+++ archivers/rpm4/files/patch-system.h
@@ -1,10 +1,15 @@
 --- system.h.orig	2011-06-10 15:27:05.000000000 +0200
 +++ system.h	2011-06-10 15:27:09.000000000 +0200
-@@ -39,6 +39,8 @@ char * stpncpy(char * dest, const char *
+@@ -39,6 +39,13 @@ char * stpncpy(char * dest, const char *
  #define	getenv(_s)	__secure_getenv(_s)
  #endif
  
++/* Add includes for missing libc prototypes */
++#include <ctype.h>
++#include <string.h>
++#include <libgen.h>
 +#include <sys/wait.h>
++#include <netinet/in.h>
 +
  #ifdef HAVE_FCNTL_H
  #include <fcntl.h>
