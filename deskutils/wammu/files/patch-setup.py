f7427a1608ea024aab191ad7e4c97f356c3c0d66
bsam <bsam@FreeBSD.org>
2011-09-16 10:23:45 +0000

    Update to version 0.36. Changes: http://wammu.eu/download/wammu/0.36/

--- deskutils/wammu/files/patch-setup.py
+++ deskutils/wammu/files/patch-setup.py
@@ -1,23 +1,29 @@
---- setup.py.orig	2010-09-07 13:28:17.000000000 +0400
-+++ setup.py	2010-09-08 15:00:34.203678636 +0400
-@@ -346,13 +346,13 @@ data_files.append((os.path.join('share',
+--- setup.py.orig	2011-09-15 13:09:48.000000000 +0400
++++ setup.py	2011-09-16 00:51:39.020550442 +0400
+@@ -346,16 +346,16 @@ data_files.append((os.path.join('share',
      'icon/wammu.ico',
      'icon/wammu.svg',
      ]))
 -data_files.append((os.path.join('share','man','man1'), ['wammu.1', 'wammu-configure.1']))
 -data_files.append((os.path.join('share','man','cs','man1'), ['man/cs/wammu.1', 'man/cs/wammu-configure.1']))
 -data_files.append((os.path.join('share','man','de','man1'), ['man/de/wammu.1', 'man/de/wammu-configure.1']))
+-data_files.append((os.path.join('share','man','fr','man1'), ['man/fr/wammu.1', 'man/fr/wammu-configure.1']))
 -data_files.append((os.path.join('share','man','nl','man1'), ['man/nl/wammu.1', 'man/nl/wammu-configure.1']))
 -data_files.append((os.path.join('share','man','it','man1'), ['man/it/wammu.1', 'man/it/wammu-configure.1']))
 -data_files.append((os.path.join('share','man','sk','man1'), ['man/sk/wammu.1', 'man/sk/wammu-configure.1']))
 -data_files.append((os.path.join('share','man','ru','man1'), ['man/ru/wammu.1', 'man/ru/wammu-configure.1']))
+-data_files.append((os.path.join('share','man','es','man1'), ['man/es/wammu.1', 'man/es/wammu-configure.1']))
+-data_files.append((os.path.join('share','man','pt_BR','man1'), ['man/pt_BR/wammu.1', 'man/pt_BR/wammu-configure.1']))
 +data_files.append((os.path.join('man','man1'), ['wammu.1', 'wammu-configure.1']))
 +data_files.append((os.path.join('man','cs','man1'), ['man/cs/wammu.1', 'man/cs/wammu-configure.1']))
 +data_files.append((os.path.join('man','de','man1'), ['man/de/wammu.1', 'man/de/wammu-configure.1']))
++data_files.append((os.path.join('man','fr','man1'), ['man/fr/wammu.1', 'man/fr/wammu-configure.1']))
 +data_files.append((os.path.join('man','nl','man1'), ['man/nl/wammu.1', 'man/nl/wammu-configure.1']))
 +data_files.append((os.path.join('man','it','man1'), ['man/it/wammu.1', 'man/it/wammu-configure.1']))
 +data_files.append((os.path.join('man','sk','man1'), ['man/sk/wammu.1', 'man/sk/wammu-configure.1']))
 +data_files.append((os.path.join('man','ru','man1'), ['man/ru/wammu.1', 'man/ru/wammu-configure.1']))
++data_files.append((os.path.join('man','es','man1'), ['man/es/wammu.1', 'man/es/wammu-configure.1']))
++data_files.append((os.path.join('man','pt_BR','man1'), ['man/pt_BR/wammu.1', 'man/pt_BR/wammu-configure.1']))
  
  distutils.core.setup(name="wammu",
      version = Wammu.__version__,
