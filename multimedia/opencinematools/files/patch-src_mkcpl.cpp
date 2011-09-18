516400410e2cc0dbd950e55b56280a74ceeb53f6
Jimmy Olgeni <olgeni@FreeBSD.org>
2011-09-18 14:08:28 +0000

    Add opencinematools: tools for creating multi-reel DCPs from MXF source material.

---
+++ multimedia/opencinematools/files/patch-src_mkcpl.cpp
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/mkcpl.cpp.orig
++++ src/mkcpl.cpp
+@@ -589,7 +589,7 @@
+ 	              PCMReader.FillWriterInfo(WInfo);
+ 
+                 AAssets.push_back(Asset());
+-                sprintf(buff, "%u %u", ADesc.SampleRate.Numerator, ADesc.SampleRate.Denominator);
++                sprintf(buff, "%u %u", ADesc.AudioSamplingRate.Numerator, ADesc.AudioSamplingRate.Denominator);
+                 AAssets.back().EditRate = buff;
+ 
+                 AAssets.back().IntrinsicDuration = ADesc.ContainerDuration;
