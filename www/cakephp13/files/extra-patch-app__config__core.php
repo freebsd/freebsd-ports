08822c69aab3b0dcd6d94d071cedbf34f40a1555
Greg Larkin <glarkin@FreeBSD.org>
2011-09-19 19:00:25 +0000

    - Updated to 1.3.11 after repocopy - Connected www/cakephp13 to build
    PR:		ports/160772
    Submitted by:	Christoph Theis <theis@gmx.at>

--- www/cakephp13/files/extra-patch-app__config__core.php
+++ www/cakephp13/files/extra-patch-app__config__core.php
@@ -1,11 +1,11 @@
---- ./app/config/core.php.orig	2008-10-02 00:56:53.000000000 -0400
-+++ ./app/config/core.php	2008-10-08 08:42:15.000000000 -0400
-@@ -56,7 +56,7 @@
+--- ./app/config/core.php.orig	2011-07-26 13:46:14.000000000 -0400
++++ ./app/config/core.php	2011-09-16 14:24:26.000000000 -0400
+@@ -66,7 +66,7 @@
   *
   * And uncomment the App.baseUrl below:
   */
 -	//Configure::write('App.baseUrl', env('SCRIPT_NAME'));
 +	Configure::write('App.baseUrl', env('SCRIPT_NAME'));
+ 
  /**
-  * Uncomment the define below to use CakePHP admin routes.
-  *
+  * Uncomment the define below to use CakePHP prefix routes.
