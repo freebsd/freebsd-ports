93a92faacbb581ae11a9438e93f81a8b85d2c48a
sunpoet <sunpoet@FreeBSD.org>
2011-09-20 08:49:57 +0000

    - Update to 0.26 - Sort MAN3 and PLIST
    Changes:	http://search.cpan.org/dist/Math-GSL/Changes

--- math/p5-Math-GSL/files/patch-inc-GSLBuilder.pm
+++ math/p5-Math-GSL/files/patch-inc-GSLBuilder.pm
@@ -1,17 +1,11 @@
-Copy the attached patch to: files/patch-inc-GSLBuilder.pm
-
-Patch attached with submission follows:
-
---- inc/GSLBuilder.pm	2009-05-20 02:50:44.000000000 -0400
-+++ inc/GSLBuilder.pm	2009-08-04 20:15:57.000000000 -0400
-@@ -154,7 +154,7 @@
+--- inc/GSLBuilder.pm.orig	2011-09-18 00:40:52.000000000 +0800
++++ inc/GSLBuilder.pm	2011-09-20 10:26:22.405819397 +0800
+@@ -258,7 +258,7 @@
  
    $cf->{installarchlib} = $Config{archlib};
  
--  my @include_dirs = @{$p->{include_dirs}} 
-+  my @include_dirs = ( @{$p->{include_dirs}} && ( ${@{$p->{include_dirs}}}[0] ne "" ) ) 
+-  my @include_dirs = @{$p->{include_dirs}}
++  my @include_dirs = ( @{$p->{include_dirs}} && ( ${@{$p->{include_dirs}}}[0] ne "" ) )
  			? map {"-I$_"} (@{$p->{include_dirs}}, catdir($cf->{installarchlib}, 'CORE'))
  			: map {"-I$_"} ( catdir($cf->{installarchlib}, 'CORE') ) ;
  
-
-
