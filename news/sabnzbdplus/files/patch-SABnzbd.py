3ecf2048c1c0271b2932378b2a1f8eafd5ed2da0
jlaffaye <jlaffaye@FreeBSD.org>
2011-09-22 17:00:30 +0000

    - Update to 0.6.9 - Use internal cherrypy module - Update rc script
    PR:		ports/160710
    Submitted by:	David Brooks <dab@rj45.org> (maintainer)

--- news/sabnzbdplus/files/patch-SABnzbd.py
+++ news/sabnzbdplus/files/patch-SABnzbd.py
@@ -1,6 +1,16 @@
---- SABnzbd.py	2011-07-26 14:57:12.000000000 -0400
-+++ SABnzbd.py.new	2011-08-01 22:47:35.614282615 -0400
-@@ -928,7 +928,7 @@
+--- SABnzbd.py.orig	2011-09-10 05:04:42.000000000 -0400
++++ SABnzbd.py	2011-09-14 23:17:08.387342710 -0400
+@@ -40,6 +40,9 @@
+     print "The Python module Cheetah is required"
+     sys.exit(1)
+ 
++# Force python to load the patched version of cherrypy included with the port,
++# instead of any version that may be installed otherwise.
++sys.path.insert(0,%%PREFIX%%)
+ import cherrypy
+ if not cherrypy.__version__.startswith("3.2"):
+     print "Sorry, requires Python module Cherrypy 3.2 (use the included version)"
+@@ -932,7 +935,7 @@
  
      sabnzbd.MY_FULLNAME = os.path.normpath(os.path.abspath(sabnzbd.MY_FULLNAME))
      sabnzbd.MY_NAME = os.path.basename(sabnzbd.MY_FULLNAME)
