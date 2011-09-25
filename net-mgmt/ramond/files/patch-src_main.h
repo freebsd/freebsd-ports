fc6b80994d1c43915129cc9d592ead0348131d12
zi <zi@FreeBSD.org>
2011-09-25 16:10:51 +0000

    - Merge in Debian patch to allow daemonization - Fix default config file location to point to ${PREFIX}/etc - Fix build under CLANG - Bump PORTREVISION
    PR:		ports/159914
    Submitted by:	Janos Mohacsi <janos.mohacsi@bsd.hu> (maintainer)

---
+++ net-mgmt/ramond/files/patch-src_main.h
@@ -0,0 +1,12 @@
+
+$FreeBSD$
+
+--- src/main.h.orig
++++ src/main.h
+@@ -1,5 +1,6 @@
+ #include <stdlib.h>
+ #include <stdio.h>
++#include <fcntl.h>
+ #include <errno.h>
+ #include <unistd.h>
+ #include <time.h>
