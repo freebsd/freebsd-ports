331bef22b71f71eda65893ccdc2e71fe591322fb
Rene Ladan <rene@FreeBSD.org>
2011-09-27 17:29:06 +0000

    Enable running Chromium with kern.ipc.shm_allow_removed=1 by using SysV IPC
    PR:		ports/157730
    Submitted by:	Vladimir I. Pushkar [vladimir.pushkar gmail.com]
    Obtained from:	Chromium Chruetertee repository r227,r228,r229

--- www/chromium/files/patch-content__renderer__render_process_impl.cc
+++ www/chromium/files/patch-content__renderer__render_process_impl.cc
@@ -1,24 +1,28 @@
---- content/renderer/render_process_impl.cc.orig	2010-12-16 02:11:59.000000000 +0100
-+++ content/renderer/render_process_impl.cc	2010-12-20 20:15:08.000000000 +0100
-@@ -243,7 +243,7 @@
- #if defined(OS_WIN) || defined(OS_LINUX)
+--- content/renderer/render_process_impl.cc.orig	2011-09-14 01:53:06.000000000 +0300
++++ content/renderer/render_process_impl.cc	2011-09-14 01:55:08.000000000 +0300
+@@ -87,7 +87,7 @@
+ 
+ bool RenderProcessImpl::InProcessPlugins() {
+   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+-#if defined(OS_LINUX)
++#if defined(OS_LINUX) || defined(OS_FREEBSD)
+   // Plugin processes require a UI message loop, and the Linux message loop
+   // implementation only allows one UI loop per process.
+   if (command_line.HasSwitch(switches::kInProcessPlugins))
+@@ -103,10 +103,10 @@
+ // Platform specific code for dealing with bitmap transport...
+ 
+ TransportDIB* RenderProcessImpl::CreateTransportDIB(size_t size) {
+-#if defined(OS_WIN) || defined(OS_LINUX)
++#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_FREEBSD)
    // Windows and Linux create transport DIBs inside the renderer
    return TransportDIB::Create(size, transport_dib_next_sequence_number_++);
 -#elif defined(OS_MACOSX)  // defined(OS_WIN) || defined(OS_LINUX)
-+#elif defined(OS_MACOSX) || defined(OS_FREEBSD) // defined(OS_WIN) || defined(OS_LINUX)
++#elif defined(OS_MACOSX)  // defined(OS_WIN) || defined(OS_LINUX) || defined(OS_FREEBSD)
    // Mac creates transport DIBs in the browser, so we need to do a sync IPC to
    // get one.  The TransportDIB is cached in the browser.
    TransportDIB::Handle handle;
-@@ -260,7 +260,7 @@
-   if (!dib)
-     return;
- 
--#if defined(OS_MACOSX)
-+#if defined(OS_MACOSX) || defined(OS_FREEBSD)
-   // On Mac we need to tell the browser that it can drop a reference to the
-   // shared memory.
-   IPC::Message* msg = new ViewHostMsg_FreeTransportDIB(dib->id());
-@@ -278,7 +278,7 @@
+@@ -141,7 +141,7 @@
    int width = rect.width();
    int height = rect.height();
    const size_t stride = skia::PlatformCanvas::StrideForWidth(rect.width());
