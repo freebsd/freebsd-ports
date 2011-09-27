8c0f4d50348dcd3300d146ae43d8deaa460f4756
Rene Ladan <rene@FreeBSD.org>
2011-09-27 17:47:03 +0000

    - Fix build on 7.4 [1] - Bump PORTREVISION for previous commits.
    PR:		ports/160945 [1]
    Submitted by:	Henk van Oers [henk.van.oers xs4all.nl]
    Obtained from:	Chromium Chruetertee repository r231

--- www/chromium/files/patch-testing__gtest__src__gtest.cc
+++ www/chromium/files/patch-testing__gtest__src__gtest.cc
@@ -1,5 +1,5 @@
---- testing/gtest/src/gtest.cc.orig	2011-09-14 23:58:20.000000000 +0300
-+++ testing/gtest/src/gtest.cc	2011-09-15 00:01:10.000000000 +0300
+--- testing/gtest/src/gtest.cc.orig	2011-09-14 11:19:34.000000000 +0300
++++ testing/gtest/src/gtest.cc	2011-09-27 00:34:58.000000000 +0300
 @@ -114,6 +114,10 @@
  
  #endif  // GTEST_OS_LINUX
@@ -11,3 +11,12 @@
  #if GTEST_HAS_EXCEPTIONS
  # include <stdexcept>
  #endif
+@@ -1621,7 +1625,7 @@
+ 
+ #if GTEST_OS_WINDOWS
+   return _wcsicmp(lhs, rhs) == 0;
+-#elif GTEST_OS_LINUX && !GTEST_OS_LINUX_ANDROID
++#elif GTEST_OS_LINUX && !GTEST_OS_LINUX_ANDROID && !GTEST_OS_FREEBSD
+   return wcscasecmp(lhs, rhs) == 0;
+ #else
+   // Android, Mac OS X and Cygwin don't define wcscasecmp.
