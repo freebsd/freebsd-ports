cebaf61d7d6d8e6d5a63b04ca8af2a8f0c2ed080
kevlo <kevlo@FreeBSD.org>
2011-09-30 04:51:09 +0000

    Update to 1.2.28

--- deskutils/taskcoach/files/patch-setup.py
+++ deskutils/taskcoach/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2011-06-11 03:03:09.000000000 +0800
-+++ setup.py	2011-07-06 16:26:53.000000000 +0800
-@@ -54,7 +54,6 @@
+--- setup.py.orig	2011-09-19 03:48:34.000000000 +0800
++++ setup.py	2011-09-30 12:47:45.000000000 +0800
+@@ -46,7 +46,6 @@
      'license': meta.license,
      'download_url': meta.download,
      'packages': findPackages('taskcoachlib') + findPackages('buildlib'),
@@ -8,19 +8,3 @@
      'classifiers': [\
          'Development Status :: 5 - Production/Stable',
          'Intended Audience :: End Users/Desktop',
-@@ -76,14 +75,8 @@
-     setupOptions['classifiers'].append('Natural Language :: %s'%language)
- 
- system = platform.system()
--if system == 'Linux':
-+if (system == 'Linux') or (system == 'FreeBSD'):
-     setupOptions['package_data'] = {'taskcoachlib': ['bin.in/linux/_pysyncml.so']}
--    # Add data files for Debian-based systems:
--    current_dist = [dist.lower() for dist in platform.dist()]
--    if 'debian' in current_dist or 'ubuntu' in current_dist:
--        setupOptions['data_files'] = [\
--	    ('share/applications', ['build.in/fedora/taskcoach.desktop']), 
--	    ('share/pixmaps', ['icons.in/taskcoach.png'])]
- elif system == 'Windows':
-     setupOptions['scripts'].append('taskcoach.pyw')
- elif system == 'Darwin':
