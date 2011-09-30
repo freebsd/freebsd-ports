9766a65fd0afeee48b78ea6bda3a90fa11014f9e
Doug Barton <dougb@FreeBSD.org>
2011-09-30 09:12:36 +0000

    As previously advertised, delete ports that have vulnerabilities listed in portaudit, and those that depend on them - part 1
    math/mupad
    	Relies on xpm, vulnerable since 2004-09-15
    net-p2p/torrentflux
    	Vulnerable since 2006-10-07
    net/tptest
    	Vulnerable since 2009-12-17
    security/pgp6
    	Vulnerable since 2005-07-31
    www/p5-RTx-RightsMatrix
    	Only works with www/rt36, which is FORBIDDEN
    www/p5-RTx-Shredder
    	Only works with www/rt36, which is FORBIDDEN
    www/p5-RTx-Statistics
    	Only works with www/rt36, which is FORBIDDEN
    www/plone
    	Vulnerable since 2011-02-10
    www/pyblosxom
    	Vulnerable since 2009-02-11
    www/rt36
    	Vulnerable since 2009-12-09
    www/zope-archetypes
    	Depends on www/plone, which is FORBIDDEN
    www/zope-calendaring
    	Depends on www/plone, which is FORBIDDEN
    www/zope-coreblog2
    	Depends on www/plone, which is FORBIDDEN
    www/zope-i18nlayer
    	Depends on www/plone, which is FORBIDDEN
    www/zope-plonelanguagetool
    	Depends on www/plone, which is FORBIDDEN
    www/zope-simpleblog
    	Depends on www/plone, which is FORBIDDEN

--- security/pgp6/files/patch-cmdline%stubs.c
+++
@@ -1,6 +0,0 @@
---- clients/pgp/cmdline/stubs.c.orig	2008-08-31 01:34:02.000000000 -0700
-+++ clients/pgp/cmdline/stubs.c	2008-08-31 01:34:26.000000000 -0700
-@@ -0,0 +1 @@
-+#if 0
-@@ -49,0 +51 @@ int maintUpdate(PGPFileSpecRef ringfiles
-+#endif
