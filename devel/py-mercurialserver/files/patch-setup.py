ea6617c73b1094e3c9c658c140b75dd99c88e1f6
pawel <pawel@FreeBSD.org>
2011-09-30 21:31:50 +0000

    Update to version 1.2.0
    PR:		ports/160844
    Submitted by:	Aldis Berjoza <graudeejs@gmail.com> (maintainer)
    Approved by:	miwi, wen (mentors implicit)

--- devel/py-mercurialserver/files/patch-setup.py
+++ devel/py-mercurialserver/files/patch-setup.py
@@ -1,11 +1,18 @@
---- ./setup.py.orig	2010-11-06 17:40:20.926497770 +0200
-+++ ./setup.py	2010-11-06 17:40:39.706412807 +0200
-@@ -13,7 +13,7 @@
+--- ./setup.py.orig	2011-09-06 13:40:10.000000000 +0300
++++ ./setup.py	2011-09-20 11:01:50.960960465 +0300
+@@ -7,13 +7,13 @@
+     name="mercurial-server",
+     description="Centralized Mercurial repository manager",
+     url="http://www.lshift.net/mercurial-server.html",
+-    version="1.1", # FIXME: infer this
++    version="1.2", # FIXME: infer this
+     package_dir = {'': 'src'},
+     packages = ["mercurialserver"],
      requires = ["mercurial"], # FIXME: what version?
      scripts = ['src/hg-ssh', 'src/refresh-auth'],
      data_files = [
 -        ('init', [
-+        ('hg', [
-             'src/init/hginit', 
-             'src/init/dot-mercurial-server', 
++        ('etc/mercurialserver', [
+             'src/init/hginit',
+             'src/init/dot-mercurial-server',
              'src/init/hgadmin-hgrc'
