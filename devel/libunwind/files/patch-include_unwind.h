9dee1862b97871e540a7af509b4dee0858d85961
stas <stas@FreeBSD.org>
2011-10-05 17:38:28 +0000

    - Fix the unwind.h header, so it can be used in C++ applications.
    Reported by:	David Chisnall <theraven@theravensnest.org>
    Untested by:	me, because I don't have a working ports tree anymore

---
+++ devel/libunwind/files/patch-include_unwind.h
@@ -0,0 +1,11 @@
+--- include/unwind.h.orig	2011-10-05 10:31:59.000000000 -0700
++++ include/unwind.h	2011-10-05 10:32:18.000000000 -0700
+@@ -148,7 +148,7 @@
+ #endif /* _GNU_SOURCE */
+ 
+ #ifdef __cplusplus
+-};
++}
+ #endif
+ 
+ #endif /* _UNWIND_H */
