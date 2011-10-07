7c285d805015ed2e7414087dd934ed23e1f91fca
Alex Dupre <ale@FreeBSD.org>
2011-10-07 09:46:50 +0000

    Update to 5.5.16 release.

--- databases/mysql55-server/files/patch-cmake_plugin.cmake
+++ databases/mysql55-server/files/patch-cmake_plugin.cmake
@@ -1,10 +1,10 @@
---- cmake/plugin.cmake.orig	2011-06-21 18:42:39.000000000 +0200
-+++ cmake/plugin.cmake	2011-07-12 09:24:41.000000000 +0200
-@@ -196,7 +196,6 @@
+--- cmake/plugin.cmake.orig	2011-09-09 17:56:39.000000000 +0200
++++ cmake/plugin.cmake	2011-10-07 10:56:37.000000000 +0200
+@@ -195,7 +195,6 @@
        OUTPUT_NAME "${ARG_MODULE_OUTPUT_NAME}")  
      # Install dynamic library
      MYSQL_INSTALL_TARGETS(${target} DESTINATION ${INSTALL_PLUGINDIR} COMPONENT Server)
 -    INSTALL_DEBUG_TARGET(${target} DESTINATION ${INSTALL_PLUGINDIR}/debug)
-     # For internal testing in PB2, append collections files
-     IF(DEFINED ENV{PB2WORKDIR})
-       PLUGIN_APPEND_COLLECTIONS(${plugin})
+     # Add installed files to list for RPMs
+     FILE(APPEND ${CMAKE_BINARY_DIR}/support-files/plugins.files
+             "%attr(755, root, root) %{_prefix}/${INSTALL_PLUGINDIR}/${ARG_MODULE_OUTPUT_NAME}.so\n"
