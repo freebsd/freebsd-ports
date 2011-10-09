4e2f32cd373b2c0e00f5c45c549b14536b8aa848
rafan <rafan@FreeBSD.org>
2011-10-09 05:54:38 +0000

    - Update math/py-numpy to reflect changes in math/atlas - Bump PORTREVISION - Fix build for science/py-scipy
    PR:		ports/161400
    Submitted by:	Li-Lun Wang <llwang at infor.org> (maintainer)

--- math/py-numpy/files/patch-numpy-distutils-system_info.py
+++ math/py-numpy/files/patch-numpy-distutils-system_info.py
@@ -1,6 +1,17 @@
---- numpy/distutils/system_info.py.orig	2007-11-07 16:05:15.000000000 -0600
-+++ numpy/distutils/system_info.py	2008-04-07 11:39:05.000000000 -0500
-@@ -883,6 +883,7 @@
+--- numpy/distutils/system_info.py.orig	2010-11-18 05:11:51.000000000 -0600
++++ numpy/distutils/system_info.py	2011-10-08 11:34:44.399179056 -0500
+@@ -909,8 +909,8 @@
+     dir_env_var = 'ATLAS'
+     _lib_names = ['f77blas','cblas']
+     if sys.platform[:7]=='freebsd':
+-        _lib_atlas = ['atlas_r']
+-        _lib_lapack = ['alapack_r']
++        _lib_atlas = ['atlas']
++        _lib_lapack = ['alapack']
+     else:
+         _lib_atlas = ['atlas']
+         _lib_lapack = ['lapack']
+@@ -938,6 +938,7 @@
              atlas = self.check_libs2(d,atlas_libs,[])
              lapack_atlas = self.check_libs2(d,['lapack_atlas'],[])
              if atlas is not None:
@@ -8,7 +19,7 @@
                  lib_dirs2 = [d] + self.combine_paths(d,['atlas*','ATLAS*'])
                  for d2 in lib_dirs2:
                      lapack = self.check_libs2(d2,lapack_libs,[])
-@@ -1324,8 +1325,9 @@
+@@ -1383,8 +1384,9 @@
              atlas_info = get_info('atlas_blas')
          atlas_version = None
          need_blas = 0
@@ -18,4 +29,4 @@
 +        if lapack_type == 'atlas' and atlas_info:
              version_info = atlas_info.copy()
              atlas_version = get_atlas_version(**version_info)
-             if not atlas_info.has_key('define_macros'):
+             if 'define_macros' not in atlas_info:
