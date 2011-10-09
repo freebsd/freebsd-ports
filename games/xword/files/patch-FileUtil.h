c3ef22e5074da76f3c1ee829ef48d204ce040fc8
Doug Barton <dougb@FreeBSD.org>
2011-10-09 02:47:51 +0000

    Remove ports maintainted by ports@ which have passed their EXPIRATION_DATE
    astro/gkrellsun		Abandonware
    astro/xrmap		No more distfiles
    audio/midimountain	Abandonware
    audio/gkrellmss		Abandonware
    audio/gnapster		Abandonware
    databases/xbase		No more public distfiles
    databases/xbsql		No more public distfiles
    games/gtkabale		No more public distfiles
    games/battleball	No more public distfiles, does not compile on ia64 or sparc64
    games/race		No more public distfiles
    games/stvef-paks	No more public distfiles
    games/stvef-server	No more public distfiles
    games/tome		No more public distfiles
    games/qkmj		No more public distfiles
    games/oilwar		No more public distfiles
    games/xbaby		No more public distfiles
    games/xword		Abandonware
    games/kslide		No more public distfiles
    graphics/xmrm		No more public distfiles, does not compile on amd64 or sparc64
    graphics/giftrans	Upstream disappeared and distfile is not available
    graphics/wildmagic	BROKEN for more than 6 months, does not fetch
    irc/ircd-rusnet		No more public distfiles
    irc/tkirc		No more public distfiles
    korean/mizifont		No more public distfiles
    lang/freetxl		No more public distfiles
    math/nsc2ke		No more public distfiles
    misc/mgp-mode.el	No more public distfiles
    multimedia/p5-RIFF-Info	No more public distfiles
    net/p5-Net-OpenDHT	No more public distfiles
    net/mudix		No more public distfiles
    net-im/newsbot		No more public distfiles
    net-im/libicq2000	Abandonware
    net-im/gossip		No longer maintain by upstream, use net-im/empathy instead
    net-im/ickle		Abandonware
    net-mgmt/nipper		BROKEN for more than 6 month, does not fetch
    net-p2p/mldonkey-serverspy	No more public distfiles
    news/p5-Gateway		No more public distfiles
    print/epsmerge		No more public distfiles
    science/at		No more public distfiles
    science/pyvox		No more public distfiles
    security/p5-Email-Obfuscate	No more public distfiles
    security/libparanoia	No more public distfiles
    security/execwrap	No more public distfiles

--- games/xword/files/patch-FileUtil.h
+++
@@ -1,9 +0,0 @@
---- src/FileUtil.h.orig Mon Nov 18 16:12:05 2002
-+++ src/FileUtil.h      Mon Nov 18 16:12:41 2002
-@@ -29,6 +29,7 @@
- #include <stdio.h>
- #include <string>
- #include <vector>
-+#include <sys/types.h>
-
- #include "Filename.h"
