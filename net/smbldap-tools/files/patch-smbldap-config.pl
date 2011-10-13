9156494bbf392a5ed32bd78d6dd855f68ab7fe9d
mm <mm@FreeBSD.org>
2011-10-13 10:17:28 +0000

    Update to 0.9.7

--- net/smbldap-tools/files/patch-smbldap-config.pl
+++ net/smbldap-tools/files/patch-smbldap-config.pl
@@ -1,5 +1,5 @@
---- configure.pl.orig	2010-11-15 15:45:49.000000000 +0100
-+++ configure.pl	2010-11-15 16:02:51.745592626 +0100
+--- smbldap-config.pl.orig	2011-10-13 11:26:12.939953317 +0200
++++ smbldap-config.pl	2011-10-13 11:27:09.173960781 +0200
 @@ -48,7 +48,7 @@
  print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
  
@@ -7,9 +7,9 @@
 -my $test_smb=`pidof smbd`;
 +my $test_smb=`pgrep -x smbd`;
  chomp($test_smb);
- die "\nSamba need to be started first !\n" if ($test_smb eq "" || not defined $test_smb);
+ die "\nSamba needs to be started first !\n" if ($test_smb eq "" || not defined $test_smb);
  
-@@ -303,7 +303,7 @@
+@@ -290,7 +290,7 @@
  
  my $default_computer_gidnumber=read_entry(". default computer gidNumber","","515",0);
  
@@ -18,14 +18,23 @@
  
  my $skeletonDir=read_entry(". default skeleton directory","","/etc/skel",0);
  
-@@ -527,12 +527,12 @@
- # Allows not to use smbpasswd (if with_smbpasswd="0" in smbldap.conf) but
+@@ -441,7 +441,7 @@
+ 
+ # Login defs
+ # Default Login Shell
+-# Ex: userLoginShell=\"/bin/bash\"
++# Ex: userLoginShell=\"/bin/sh\"
+ userLoginShell=\"$userLoginShell\"
+ 
+ # Home directory
+@@ -511,12 +511,12 @@
+ # Allows not to use smbpasswd (if with_smbpasswd=\"0\" in smbldap.conf) but
  # prefer Crypt::SmbHash library
  with_smbpasswd=\"0\"
 -smbpasswd=\"/usr/bin/smbpasswd\"
 +smbpasswd=\"%%LOCALBASE%%/bin/smbpasswd\"
  
- # Allows not to use slappasswd (if with_slappasswd="0" in smbldap.conf)
+ # Allows not to use slappasswd (if with_slappasswd=\"0\" in smbldap.conf)
  # but prefer Crypt:: libraries
  with_slappasswd=\"0\"
 -slappasswd=\"/usr/sbin/slappasswd\"
