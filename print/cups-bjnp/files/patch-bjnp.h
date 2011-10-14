fb525bf129f647aaeeed30b835c07c5cca67805c
Pav Lucistnik <pav@FreeBSD.org>
2011-10-14 16:38:48 +0000

    - Fix build
    Submitted by:	Stephen Hurd <shurd@sasktel.net> (maintainer)

---
+++ print/cups-bjnp/files/patch-bjnp.h
@@ -0,0 +1,10 @@
+--- bjnp.h.orig	2009-03-14 22:47:19.000000000 +0100
++++ bjnp.h	2011-10-14 18:36:08.000000000 +0200
+@@ -34,6 +34,7 @@
+  */
+ 
+ #  include <stdlib.h>
++#  include <stdio.h>
+ #  include <errno.h>
+ #  include <signal.h>
+ #  include <stdint.h>
