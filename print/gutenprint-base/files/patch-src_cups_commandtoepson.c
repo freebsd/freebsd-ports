1104d83d68a3cf0f6e165bcaedaf13b9b1f3bd5c
Pawel Pekala <pawel@FreeBSD.org>
2011-10-17 00:11:10 +0000

    - Unbreak and update to version 5.2.7 - Assign maintainership to submitter
    PR:		ports/161680
    Submitted by:	Christoph Moench-Tegeder <cmt@burggraben.net>

---
+++ print/gutenprint-base/files/patch-src_cups_commandtoepson.c
@@ -0,0 +1,10 @@
+--- src/cups/commandtoepson.c.orig	2011-10-15 13:20:46.000000000 +0200
++++ src/cups/commandtoepson.c	2011-10-15 13:20:57.000000000 +0200
+@@ -30,6 +30,7 @@
+ 
+ #include <cups/cups.h>
+ #include <ctype.h>
++#include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ 
