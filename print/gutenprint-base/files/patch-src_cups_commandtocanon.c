d2c0e9bf20db2c57e6b4039f29203487206d9571
pawel <pawel@FreeBSD.org>
2011-10-17 00:11:10 +0000

    - Unbreak and update to version 5.2.7 - Assign maintainership to submitter
    PR:		ports/161680
    Submitted by:	Christoph Moench-Tegeder <cmt@burggraben.net>

---
+++ print/gutenprint-base/files/patch-src_cups_commandtocanon.c
@@ -0,0 +1,10 @@
+--- src/cups/commandtocanon.c.orig	2011-10-15 13:27:57.000000000 +0200
++++ src/cups/commandtocanon.c	2011-10-15 13:28:22.000000000 +0200
+@@ -13,6 +13,7 @@
+ 
+ #include <cups/cups.h>
+ #include <ctype.h>
++#include <stdio.h>
+ #include <stdlib.h>
+ 
+ 
