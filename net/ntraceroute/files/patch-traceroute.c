0bd17a7aaf12b7a0a13ecca63e7cf90cc0cf6317
zi <zi@FreeBSD.org>
2011-10-19 16:48:23 +0000

    - Bring back a patch to fix ntraceroute on 64-bit systems (this was missed whenever this port was revived) - Bump PORTREVISION
    Submitted by:	Gleb Kozyrev <gleb@gkoz.com>
    Obtained from:	Bill Fenner <fenner@research.att.com>, ports/103988

--- net/ntraceroute/files/patch-traceroute.c
+++ net/ntraceroute/files/patch-traceroute.c
@@ -1,5 +1,5 @@
---- traceroute.c	2011-06-12 18:10:52.000000000 -0400
-+++ traceroute.c	2011-06-12 18:11:00.000000000 -0400
+--- ./traceroute.c.orig	2011-10-19 10:04:10.000000000 -0400
++++ ./traceroute.c	2011-10-19 10:04:08.000000000 -0400
 @@ -2,6 +2,7 @@
  /*
   *  STILL BROKEN: traceroute -g on linux (LSRR)
@@ -8,3 +8,12 @@
  #define TRACE_TOS 1
  #define SPRAY
  #define FIXT
+@@ -798,7 +799,7 @@
+               struct icmp icmp_probe;
+         } ip_payload;
+ #endif /* V6.3.0 */
+-};
++} __attribute__((packed));
+ 
+ #ifdef SPRAY
+ /*
