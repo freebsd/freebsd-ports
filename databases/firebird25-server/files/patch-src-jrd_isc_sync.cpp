d43226da918df32ae0035f4fb005fc9a6a25b1d0
acm <acm@FreeBSD.org>
2011-10-19 22:26:38 +0000

    - Update firebird25 to 2.5.1

--- databases/firebird25-server/files/patch-src-jrd_isc_sync.cpp
+++ databases/firebird25-server/files/patch-src-jrd_isc_sync.cpp
@@ -1,10 +1,10 @@
---- src/jrd/isc_sync.cpp	2010-12-18 21:08:26.000000000 -0500
-+++ src/jrd/isc_sync.cpp	2010-12-18 21:09:05.000000000 -0500
+--- src/jrd/isc_sync.cpp	2011-08-30 08:48:04.000000000 -0500
++++ src/jrd/isc_sync.cpp	2011-10-19 15:00:50.000000000 -0500
 @@ -320,7 +320,7 @@
  	public:
  		const static int N_FILES = 8;
  		const static int N_SETS = 256;
--#ifdef DEV_BUILD
+-#if defined(DEV_BUILD)
 +#if defined(DEV_BUILD) || defined(FREEBSD)
  		const static int SEM_PER_SET = 4;	// force multiple sets allocation
  #else
