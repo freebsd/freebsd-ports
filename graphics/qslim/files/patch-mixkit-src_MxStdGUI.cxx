f12a7be8d0c22634bdc57d4e5d2ecb676aa632db
gahr <gahr@FreeBSD.org>
2011-10-24 13:12:45 +0000

    - Add patches to build against fltk-1.3.0

---
+++ graphics/qslim/files/patch-mixkit-src_MxStdGUI.cxx
@@ -0,0 +1,11 @@
+--- mixkit/src/MxStdGUI.cxx.orig	2011-09-06 14:12:15.000000000 +0200
++++ mixkit/src/MxStdGUI.cxx	2011-09-06 14:12:47.000000000 +0200
+@@ -15,7 +15,7 @@
+ #include "MxGLUtils.h"
+ #include "MxSMF.h"
+ #include <FL/Fl_Color_Chooser.H>
+-#include <FL/fl_file_chooser.H>
++#include <FL/Fl_File_Chooser.H>
+ #include <FL/filename.H>
+ 
+ 
---
+++ graphics/qslim/files/patch-tools-qslim_qvis.cxx
@@ -0,0 +1,11 @@
+--- tools/qslim/qvis.cxx.orig	2011-09-06 14:13:35.000000000 +0200
++++ tools/qslim/qvis.cxx	2011-09-06 14:14:02.000000000 +0200
+@@ -14,7 +14,7 @@
+ #include <MxStdGUI.h>
+ #include <stdio.h>
+ 
+-#include <FL/fl_file_chooser.H>
++#include <FL/Fl_File_Chooser.H>
+ #include <FL/filename.H>
+ #include <FL/filename.H>
+ #include <FL/Fl_Slider.H>
