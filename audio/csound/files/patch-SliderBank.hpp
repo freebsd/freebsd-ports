2efdc45e1542603dc8da4a4b238eff04e9f7530a
gahr <gahr@FreeBSD.org>
2011-10-27 11:37:46 +0000

    - fix build with newer FLTK
    Reported by:	PH (via pav)

---
+++ audio/csound/files/patch-SliderBank.hpp
@@ -0,0 +1,13 @@
+--- InOut/virtual_keyboard/SliderBank.hpp.orig	2011-10-27 13:20:10.000000000 +0200
++++ InOut/virtual_keyboard/SliderBank.hpp	2011-10-27 13:20:28.000000000 +0200
+@@ -26,8 +26,8 @@
+ 
+ #include <FL/Fl.H>
+ #include <FL/Fl_Group.H>
+-#include <Fl/Fl_Value_Slider.H>
+-#include <Fl/Fl_Spinner.H>
++#include <FL/Fl_Value_Slider.H>
++#include <FL/Fl_Spinner.H>
+ #include "csdl.h"
+ #include "SliderData.hpp"
+ 
