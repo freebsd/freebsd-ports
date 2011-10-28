42e54ccae2bfd0007da07f46ff583f26b852dafe
zi <zi@FreeBSD.org>
2011-10-28 15:47:08 +0000

    - Update to 0.8.7h.
    Software changes include:
    - Fix SQL injection issue with user login
    - Fix Cross-site scripting issues
    - Make one minute polling work properly
    
    PR:		ports/162044
    Submitted by:	moggie <moggie@elasticmind.net>
    Approved by:	sem
    Obtained from:	zi

--- net-mgmt/cacti/files/patch-scripts-ping.pl
+++ net-mgmt/cacti/files/patch-scripts-ping.pl
@@ -1,11 +1,11 @@
---- cacti-0.8.7d/scripts/ping.pl.orig	2009-02-12 05:55:50.000000000 +0300
-+++ cacti-0.8.7d/scripts/ping.pl	2009-02-16 17:01:59.000000000 +0300
-@@ -4,7 +4,7 @@
- $host = $ARGV[0];
- $host =~ s/tcp:/$1/gis;
+--- ./scripts/ping.pl.orig	2011-10-26 09:31:56.000000000 -0400
++++ ./scripts/ping.pl	2011-10-26 09:32:02.000000000 -0400
+@@ -6,7 +6,7 @@
  
--open(PROCESS, "ping -c 1 $host | grep icmp_seq | grep time |");
-+open(PROCESS, "/sbin/ping -c 1 $host | grep icmp_seq | grep time |");
+ # old linux version use "icmp_seq"
+ # newer use "icmp_req" instead
+-open(PROCESS, "ping -c 1 $host | grep 'icmp_[s|r]eq' | grep time |");
++open(PROCESS, "/sbin/ping -c 1 $host | grep 'icmp_[s|r]eq' | grep time |");
  $ping = <PROCESS>;
  close(PROCESS);
  $ping =~ m/(.*time=)(.*) (ms|usec)/;
