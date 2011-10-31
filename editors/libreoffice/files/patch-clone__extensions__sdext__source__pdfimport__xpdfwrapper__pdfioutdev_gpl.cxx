c0abc8197846b80d4b9c48cda40602d2988b39c7
Baptiste Daroussin <bapt@FreeBSD.org>
2011-10-31 16:49:14 +0000

    - fix build after new poppler import - while here fix plist (should have been fixed long time ago :() [2]
    Reported by:	pointyhat (pav)

--- editors/libreoffice/files/patch-clone__extensions__sdext__source__pdfimport__xpdfwrapper__pdfioutdev_gpl.cxx
+++ editors/libreoffice/files/patch-clone__extensions__sdext__source__pdfimport__xpdfwrapper__pdfioutdev_gpl.cxx
@@ -1,5 +1,14 @@
 --- clone/extensions/sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.cxx.orig	2011-10-26 13:23:40.000000000 +0000
 +++ clone/extensions/sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.cxx	2011-10-26 13:23:57.000000000 +0000
+@@ -150,7 +150,7 @@
+ void writeJpeg_( OutputBuffer& o_rOutputBuf, Stream* str, bool bWithLinefeed )
+ {
+     // dump JPEG file as-is
+-    str = ((DCTStream *)str)->getRawStream();
++    str = str->getBaseStream();
+     str->reset();
+ 
+     int c;
 @@ -489,7 +489,7 @@ void PDFOutDev::endPage()
      printf("endPage\n");
  }
