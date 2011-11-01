83fbcf01f9775822b7f8d00ef4ec7a27dc8202ae
Ryan Steinmetz <zi@FreeBSD.org>
2011-11-01 22:31:03 +0000

    - Fix memleak (obtained from upstream commit: 6ef6907642247c663b9b8964b9fd44a211dbcaa7) - Fix logic error in local patch (to avoid divide by zero condition) - Fix buffer overflow possibility - Bump PORTREVISION
    PR:		ports/161064
    Submitted by:	Helmut Schneider <jumper99@gmx.de>
    Approved by:	maintainer timeout (1 month)

--- net-mgmt/net-snmp/files/patch-agent-mibgroup-hardware-fsys-fsys_getfsstats.c
+++ net-mgmt/net-snmp/files/patch-agent-mibgroup-hardware-fsys-fsys_getfsstats.c
@@ -1,6 +1,14 @@
---- ./agent/mibgroup/hardware/fsys/fsys_getfsstats.c.orig       2011-08-05 10:20:55.000000000 +0200
-+++ ./agent/mibgroup/hardware/fsys/fsys_getfsstats.c    2011-08-05 10:21:38.000000000 +0200
-@@ -156,7 +156,7 @@
+--- agent/mibgroup/hardware/fsys/fsys_getfsstats.c.orig	2011-07-02 00:35:46.000000000 +0200
++++ agent/mibgroup/hardware/fsys/fsys_getfsstats.c	2011-10-27 17:32:32.000000000 +0200
+@@ -150,13 +150,13 @@
+         if (!entry)
+             continue;
+ 
+-        memcpy( entry->path,   stats[i].f_mntonname,   sizeof(entry->path)  );
++        strncpy( entry->path,   stats[i].f_mntonname,   sizeof(entry->path)  );
+         entry->path[sizeof(entry->path)-1] = '\0';
+-        memcpy( entry->device, stats[i].f_mntfromname, sizeof(entry->device));
++        strncpy( entry->device, stats[i].f_mntfromname, sizeof(entry->device));
          entry->device[sizeof(entry->device)-1] = '\0';
          entry->units = stats[i].f_bsize;    /* or f_frsize */
          entry->size  = stats[i].f_blocks;
@@ -9,3 +17,9 @@
          entry->avail = stats[i].f_bavail;
          entry->inums_total = stats[i].f_files;
          entry->inums_avail = stats[i].f_ffree;
+@@ -175,4 +175,5 @@
+         }
+         netsnmp_fsys_calculate32(entry);
+     }
++    free(stats);
+ }
