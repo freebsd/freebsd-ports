e07ecaa935b1dd119e1cba0957a1535a45ef3d4e
miwi <miwi@FreeBSD.org>
2011-11-05 16:53:01 +0000

    - Fix build on FreeBSD 10

--- mail/postfix27/files/patch-src::util::sys_defs.h
+++ mail/postfix27/files/patch-src::util::sys_defs.h
@@ -5,7 +5,7 @@
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
 -    || defined(FREEBSD8) \
-+    || defined(FREEBSD8) || defined(FREEBSD9) \
++    || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(NETBSD1) || defined(NETBSD2) || defined(NETBSD3) \
