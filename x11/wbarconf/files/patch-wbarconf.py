d1288b3526dfb3f25dce5ae207579c7466fb7071
Philippe Audeoud <jadawin@FreeBSD.org>
2011-11-07 07:58:42 +0000

    - Fix compilation issue.
    PR:		ports/162336
    Submitted by:	Olivier Cochard-Labbe <olivier _AT_ cochard.me> (maintainer)

---
+++ x11/wbarconf/files/patch-wbarconf.py
@@ -0,0 +1,8 @@
+--- wbarconf.orig	2010-10-20 17:27:09.000000000 +0200
++++ wbarconf	2011-11-04 15:31:00.000000000 +0100
+@@ -1,4 +1,4 @@
+-#!/usr/bin/env python2
++#!/usr/bin/env python
+ # coding= UTF-8
+ #
+ #	wbar configuration gui  v0.7.2.1
