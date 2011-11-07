de07c68a5784f49d0d19d81f6b684d242943379a
Baptiste Daroussin <bapt@FreeBSD.org>
2011-11-07 06:50:47 +0000

    Add the right patch
    Reported by:	Jan Beich <jbeich@tormail.net> (by private mail)

--- shells/zsh/files/patch-Src_jobs.c
+++ shells/zsh/files/patch-Src_jobs.c
@@ -1,5 +1,5 @@
 --- Src/jobs.c.orig	2011-11-06 19:20:13.978321106 +0100
-+++ Src/jobs.c	2011-11-06 19:22:30.185938633 +0100
++++ Src/jobs.c	2011-11-07 07:45:47.012600236 +0100
 @@ -583,7 +583,6 @@
  }
  
@@ -16,7 +16,12 @@
  
  /**/
  static void
-@@ -660,10 +658,9 @@
+@@ -656,14 +654,14 @@
+     /* go ahead and compute these, since almost every TIMEFMT will have them */
+     elapsed_time = real->tv_sec + real->tv_usec / 1000000.0;
+ 
++    set_clktck();
+ #ifdef HAVE_GETRUSAGE
      user_time = ti->ru_utime.tv_sec + ti->ru_utime.tv_usec / 1000000.0;
      system_time = ti->ru_stime.tv_sec + ti->ru_stime.tv_usec / 1000000.0;
      total_time = user_time + system_time;
@@ -29,7 +34,7 @@
      user_time    = ti->ut / (double) clktck;
      system_time  = ti->st / (double) clktck;
      percent      =  100.0 * (ti->ut + ti->st)
-@@ -749,7 +746,7 @@
+@@ -749,7 +747,7 @@
  #endif
  #ifdef HAVE_STRUCT_RUSAGE_RU_MAXRSS
  	    case 'M':
