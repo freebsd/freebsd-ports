bee68770e85bbd011b2f5dd459aeb20676810d8b
Cy Schubert <cy@FreeBSD.org>
2011-11-08 05:15:49 +0000

    Allow $TERM to be longer than 20 bytes, allow for 63 bytes.
    PR:		162339

--- sysutils/screen/files/patch-screen.h
+++ sysutils/screen/files/patch-screen.h
@@ -1,5 +1,5 @@
---- screen.h.orig	2003-08-22 14:28:43.000000000 +0200
-+++ screen.h	2007-07-30 10:25:45.000000000 +0200
+--- screen.h.orig	2003-08-22 05:28:43.000000000 -0700
++++ screen.h	2011-11-07 11:53:41.739890820 -0800
 @@ -22,6 +22,8 @@
   * $Id: screen.h,v 1.12 1994/05/31 12:32:54 mlschroe Exp $ FAU
   */
@@ -9,3 +9,12 @@
  #include "os.h"
  
  #if defined(__STDC__)
+@@ -202,7 +204,7 @@
+ 	  char preselect[20];
+ 	  int esc;		/* his new escape character unless -1 */
+ 	  int meta_esc;		/* his new meta esc character unless -1 */
+-	  char envterm[20 + 1];	/* terminal type */
++	  char envterm[63 + 1];	/* terminal type */
+ 	  int encoding;		/* encoding of display */
+ 	}
+       attach;
