4719412b442849f30918a17f730862aa820dc042
Jimmy Olgeni <olgeni@FreeBSD.org>
2011-11-10 22:17:26 +0000

    Upgrade to version 0.11 (with qtconsole support) and take over maintainership.
    PR:		ports/162234 (second patch)
    Submitted by:	olgeni
    Approved by:	wen (by email)
    Feature safe:	yes

--- devel/ipython/files/patch-setupbase.py
+++ devel/ipython/files/patch-setupbase.py
@@ -1,21 +1,24 @@
---- setupbase.py.orig	2008-09-15 05:26:16.000000000 +0800
-+++ setupbase.py	2008-10-03 13:45:28.000000000 +0800
-@@ -188,7 +188,8 @@
+
+$FreeBSD$
+
+--- setupbase.py.orig
++++ setupbase.py
+@@ -202,7 +202,8 @@
      """
      
-     docdirbase  = 'share/doc/ipython'
--    manpagebase = 'share/man/man1'
-+    manpagebase = 'man/man1'
-+    examplebase = 'share/examples/ipython'
- 
+     docdirbase  = pjoin('share', 'doc', 'ipython')
+-    manpagebase = pjoin('share', 'man', 'man1')
++    manpagebase = pjoin('man', 'man1')
++    examplebase = pjoin('share', 'examples', 'ipython')
+     
      # Simple file lists can be made by hand
-     manpages  = filter(isfile, glob('docs/man/*.1.gz'))
-@@ -196,7 +197,7 @@
- 
-     # For nested structures, use the utility above
-     example_files = make_dir_struct('data','docs/examples',
--                                    pjoin(docdirbase,'examples'))
-+                                    examplebase)
-     manual_files = make_dir_struct('data','docs/dist',pjoin(docdirbase,'manual'))
- 
-     # And assemble the entire output list
+     manpages  = filter(isfile, glob(pjoin('docs','man','*.1.gz')))
+@@ -216,7 +217,7 @@
+     example_files = make_dir_struct(
+         'data',
+         pjoin('docs','examples'),
+-        pjoin(docdirbase,'examples')
++        pjoin(examplebase)
+     )
+     manual_files = make_dir_struct(
+         'data',
