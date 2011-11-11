27d9b1ede5f6e45aa486d1c8c522e2bc02c7a00a
tabthorpe <tabthorpe@FreeBSD.org>
2011-11-11 23:10:40 +0000

    - Fix build
    PR:		ports/162302
    Submitted by:	Bengt Ahlgren <bengta sics.se>
    Feature safe:	yes

---
+++ print/gtklp/files/patch-gtklp__gtklp.h
@@ -0,0 +1,10 @@
+--- gtklp/gtklp.h.orig	2010-02-11 20:38:17.000000000 +0100
++++ gtklp/gtklp.h	2011-11-04 23:04:22.000000000 +0100
+@@ -26,6 +26,7 @@
+ 
+ #include <gtk/gtk.h>
+ #include <cups/cups.h>
++#include <cups/ppd.h>
+ #include <cups/http.h>
+ 
+ #include <defaults.h>
