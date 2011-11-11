bde59fa5f1b85923625154e7440b545a3fdf42e7
ashish <ashish@FreeBSD.org>
2011-11-11 05:07:42 +0000

    - Update version of editors/emacs-devel in bsd.emacs.mk
    PR:		ports/161706
    Reported by:	rakuco (private email)
    Feature safe:	yes

--- Mk/bsd.emacs.mk
+++ Mk/bsd.emacs.mk
@@ -1,5 +1,5 @@
 #
-#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.87 2011-09-03 15:36:18 nobutaka Exp $
+#	$FreeBSD: /tmp/pcvs/ports/Mk/bsd.emacs.mk,v 1.88 2011-11-11 05:07:42 ashish Exp $
 #
 #	bsd.emacs.mk - 19990829 Shigeyuki Fukushima.
 #
@@ -117,7 +117,7 @@ PLIST?=			${PKGDIR}/pkg-plist.${EMACS_PORT_NAME}
 # Emacs-24.x (development version)
 .elif (${EMACS_PORT_NAME} == "emacs-devel")
 EMACS_NAME=		emacs
-EMACS_VER=		24.0.50
+EMACS_VER=		24.0.90
 EMACS_MAJOR_VER=	24
 EMACS_LIBDIR?=		share/${EMACS_NAME}
 EMACS_LIBDIR_WITH_VER?=	share/${EMACS_NAME}/${EMACS_VER}
