b0edfc5a4c55107e5a51dc5e26fd5cc85b87b190
Pietro Cerutti <gahr@FreeBSD.org>
2011-11-17 10:41:36 +0000

    - Unbreak - fix build with latest x11-toolkits/fltk
    Feature safe:	yes

--- science/flounder/files/patch-Glarea.h
+++ science/flounder/files/patch-Glarea.h
@@ -1,6 +1,15 @@
---- src/Glarea.h.orig	Tue Oct 15 01:13:37 2002
-+++ src/Glarea.h	Tue Oct 15 01:15:44 2002
-@@ -43,6 +43,8 @@
+--- src/Glarea.h.orig	2002-12-19 19:37:48.000000000 +0100
++++ src/Glarea.h	2011-11-17 11:21:09.000000000 +0100
+@@ -29,7 +29,7 @@
+ extern "C" {
+ #include <gif_lib.h>
+ }
+-#include <FL/fl_file_chooser.H>
++#include <FL/Fl_File_Chooser.H>
+ #include <FL/fl_ask.H>
+ #include <FL/fl_draw.H>
+ #include <FL/fl_show_colormap.H>
+@@ -44,6 +44,8 @@
  #include "RenderEPS.h"	
  #include "IGBheader.h"
  
