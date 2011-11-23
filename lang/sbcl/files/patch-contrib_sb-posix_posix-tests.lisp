32b733780a8cd8e1f352f318c7c4674678eaae77
bf <bf@FreeBSD.org>
2011-11-23 02:43:12 +0000

    update lang/sbcl to 1.0.53, and adjust dependent ports
    PR:		161444
    Submitted by:	olgeni (earlier version)
    Approved by:	miwi (portmgr)
    Feature safe:	yes

--- lang/sbcl/files/patch-contrib_sb-posix_posix-tests.lisp
+++ lang/sbcl/files/patch-contrib_sb-posix_posix-tests.lisp
@@ -1,18 +1,20 @@
---- contrib/sb-posix/posix-tests.lisp.orig	2011-07-14 16:51:21.000000000 -0700
-+++ contrib/sb-posix/posix-tests.lisp	2011-07-14 16:51:36.000000000 -0700
-@@ -172,13 +172,13 @@
-       (sb-posix:rmdir #-win32 "/" #+win32 "C:/")
-     (sb-posix:syscall-error (c)
-       (sb-posix:syscall-errno c)))
--  #+darwin
-+  #+(or darwin freebsd)
-   #.sb-posix:eisdir
-   #+win32
-   #.sb-posix::eacces
-   #+sunos
-   #.sb-posix::einval
--  #-(or darwin win32 sunos)
-+  #-(or darwin freebsd win32 sunos)
-   #.sb-posix::ebusy)
+--- contrib/sb-posix/posix-tests.lisp.orig	2011-11-06 15:31:02.000000000 -0500
++++ contrib/sb-posix/posix-tests.lisp	2011-11-22 10:26:13.000000000 -0500
+@@ -174,7 +174,7 @@
+       (typep
+        (sb-posix:syscall-errno c)
+        '(member
+-         #+darwin
++         #+(or darwin freebsd)
+          #.sb-posix:eisdir
+          #+win32
+          #.sb-posix::eacces
+@@ -182,7 +182,7 @@
+          #.sb-posix::enotempty
+          #+sunos
+          #.sb-posix::einval
+-         #-(or darwin win32 sunos)
++         #-(or darwin freebsd win32 sunos)
+          #.sb-posix::ebusy)))) t)
  
  (deftest rmdir.error.4
