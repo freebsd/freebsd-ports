c7cfb041232f045a4a1877d110021fa343584b17
miwi <miwi@FreeBSD.org>
2011-11-25 19:01:04 +0000

    - Fix a typo
    PR:		161569
    Submitted by:	maintainer
    Feature safe:	yes

---
+++ audio/libaacplus/files/patch-include-aacplus.h
@@ -0,0 +1,11 @@
+--- include/aacplus.h.orig	2010-11-11 06:27:38.000000000 +0900
++++ include/aacplus.h	2011-10-13 18:19:31.000000000 +0900
+@@ -12,7 +12,7 @@
+
+ typedef enum {
+ 	AACPLUS_INPUT_16BIT = 0,
+-	AACPLUS_INPUT_FLOAT,
++	AACPLUS_INPUT_FLOAT
+ } aacplusInFormat;
+
+
