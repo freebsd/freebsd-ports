d665a4af67521dbcdcfdd8c3fff079f9a7c41255
miwi <miwi@FreeBSD.org>
2011-11-26 15:06:32 +0000

    - Update to 0.6.17
    PR:		162886
    Submitted by:	Jase Thew <freebsd@beardz.net> (maintainer)
    Feature safe:	yes

--- sysutils/duplicity/files/patch-setup.py
+++ sysutils/duplicity/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2009-01-09 16:05:11.000000000 +0200
-+++ setup.py	2009-01-10 16:06:40.000000000 +0200
-@@ -12,6 +12,10 @@
+--- setup.py.orig	2011-11-25 23:58:57.147597966 +0000
++++ setup.py	2011-11-26 00:00:43.169721395 +0000
+@@ -31,6 +31,10 @@
  
  incdir_list = libdir_list = None
  
@@ -11,20 +11,21 @@
  if os.name == 'posix':
      LIBRSYNC_DIR = os.environ.get('LIBRSYNC_DIR', '')
      args = sys.argv[:]
-@@ -23,16 +27,9 @@
+@@ -42,17 +46,9 @@
          incdir_list = [os.path.join(LIBRSYNC_DIR, 'include')]
          libdir_list = [os.path.join(LIBRSYNC_DIR, 'lib')]
  
 -data_files = [('share/man/man1',
 +data_files = [('man/man1',
-                ['duplicity.1',
-                 'rdiffdir.1']),
+                ['bin/duplicity.1',
+                 'bin/rdiffdir.1']),
 -              ('share/doc/duplicity-%s' % version_string,
 -               ['COPYING',
--                'REPO-README',
--                'LOG-README',
 -                'README',
+-                'README-REPO',
+-                'README-LOG',
 -                'tarfile-LICENSE',
+-                'tarfile-CHANGES',
 -                'CHANGELOG']),
                ]
  
