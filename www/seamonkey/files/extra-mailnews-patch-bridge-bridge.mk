6a934d9554e972145e90c5777a9da7550f9453c9
beat <beat@FreeBSD.org>
2011-11-29 21:27:59 +0000

    - Unbreak WITHOUT_MAILNEWS option
    PR:		ports/160444
    Submitted by:	Riccardo Torrini <riccardo AT torrini.org>
    Feature safe:	yes

--- www/seamonkey/files/extra-mailnews-patch-bridge-bridge.mk
+++ www/seamonkey/files/extra-mailnews-patch-bridge-bridge.mk
@@ -1,6 +1,6 @@
 --- bridge/bridge.mk.orig	2011-09-29 01:10:49.000000000 +0200
 +++ bridge/bridge.mk	2011-10-26 22:21:47.000000000 +0200
-@@ -38,12 +38,12 @@
+@@ -38,16 +38,16 @@
  
  APP_LIBXUL_DIRS += $(DEPTH)$(SUBDIR)/mozilla/xpfe/components/autocomplete
  
@@ -10,7 +10,11 @@
  APP_LIBXUL_DIRS += $(DEPTH)$(SUBDIR)/ldap/xpcom
 -#endif
 +endif
- 
+
+ #ifdef MOZ_MORK
+ APP_LIBXUL_DIRS += $(DEPTH)$(SUBDIR)/db/mork
+ #endif
+
 -ifdef MOZ_MAIL_NEWS
 +ifdef disabled
  APP_LIBXUL_DIRS += \
