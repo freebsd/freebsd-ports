3d8529836a84b64ec639e073cdc7e2bea53c3403
zi <zi@FreeBSD.org>
2011-12-01 00:34:25 +0000

    - Update to 5.7.1
    PR:		ports/162847
    Submitted by:	me
    Approved by:	maintainer (implicit)
    Feature safe:	yes

--- net-mgmt/net-snmp/files/patch-agent-mibgroup-hardware-fsys-fsys_getfsstats.c
+++ net-mgmt/net-snmp/files/patch-agent-mibgroup-hardware-fsys-fsys_getfsstats.c
@@ -1,6 +1,6 @@
---- agent/mibgroup/hardware/fsys/fsys_getfsstats.c.orig	2011-07-02 00:35:46.000000000 +0200
-+++ agent/mibgroup/hardware/fsys/fsys_getfsstats.c	2011-10-27 17:32:32.000000000 +0200
-@@ -150,13 +150,13 @@
+--- ./agent/mibgroup/hardware/fsys/fsys_getfsstats.c.orig	2011-11-24 12:01:07.000000000 -0500
++++ ./agent/mibgroup/hardware/fsys/fsys_getfsstats.c	2011-11-24 12:01:20.000000000 -0500
+@@ -150,9 +150,9 @@
          if (!entry)
              continue;
  
@@ -12,14 +12,3 @@
          entry->device[sizeof(entry->device)-1] = '\0';
          entry->units = stats[i].f_bsize;    /* or f_frsize */
          entry->size  = stats[i].f_blocks;
--        entry->used  = stats[i].f_bfree;
-+        entry->used  = stats[i].f_blocks - stats[i].f_bfree;
-         entry->avail = stats[i].f_bavail;
-         entry->inums_total = stats[i].f_files;
-         entry->inums_avail = stats[i].f_ffree;
-@@ -175,4 +175,5 @@
-         }
-         netsnmp_fsys_calculate32(entry);
-     }
-+    free(stats);
- }
