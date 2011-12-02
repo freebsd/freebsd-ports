ce929640d1faf5d1a472bf6c5e6598ddddcf5df7
osa <osa@FreeBSD.org>
2011-12-02 03:42:56 +0000

    Fix build on i386. Feature safe:	yes
    PR:	163004

--- databases/redis/files/patch-src::redis.c
+++ databases/redis/files/patch-src::redis.c
@@ -1,11 +1,15 @@
---- src/redis.c.orig	2011-11-30 16:29:38.000000000 +0400
-+++ src/redis.c	2011-11-30 16:29:59.000000000 +0400
-@@ -1685,7 +1685,7 @@
+--- src/redis.c.orig	2011-11-28 14:22:08.000000000 +0400
++++ src/redis.c	2011-12-02 06:23:15.000000000 +0400
+@@ -1685,7 +1685,11 @@
  #ifdef HAVE_BACKTRACE
  static void *getMcontextEip(ucontext_t *uc) {
  #if defined(__FreeBSD__)
 -    return (void*) uc->uc_mcontext.mc_eip;
++  #if __x86_64__
 +    return (void*) uc->uc_mcontext.mc_rip;
++  #elif __i386__
++    return (void*) uc->uc_mcontext.mc_eip;
++  #endif
  #elif defined(__dietlibc__)
      return (void*) uc->uc_mcontext.eip;
  #elif defined(__APPLE__) && !defined(MAC_OS_X_VERSION_10_6)
