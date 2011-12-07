e602751048dbad5755b2a830d003c2bfbbc3839e
Michael Scheidell <scheidell@FreeBSD.org>
2011-12-07 16:04:33 +0000

    -Update to 0.4.12
    PR:		ports/161415
    Submitted by:	Rodrigo OSORIO <rodrigo@bebik.net> (maintainer)
    Reviewed by:	scheidell
    Approved by:	gabor (mentor)
    Feature safe:	yes

--- audio/lmms/files/patch-plugins_organic_organic.cpp
+++ audio/lmms/files/patch-plugins_organic_organic.cpp
@@ -1,11 +1,11 @@
---- plugins/organic/organic.cpp.orig	2009-02-24 07:34:26.000000000 +0900
-+++ plugins/organic/organic.cpp	2009-04-09 03:38:36.000000000 +0900
-@@ -42,6 +42,9 @@
- #undef SINGLE_SOURCE_COMPILE
+--- plugins/organic/organic.cpp	2011-07-02 13:14:01.000000000 +0000
++++ plugins/organic/organic.cpp.new	2011-09-28 09:55:59.000000000 +0000
+@@ -41,6 +41,9 @@
+ 
  #include "embed.cpp"
  
-+#if defined(__FreeBSD__)
-+#define log2f(x) (logf((x)) / logf(2))
++#if __FreeBSD_version <= 704100 || (__FreeBSD_version >= 800000 && __FreeBSD_version < 802502) || (__FreeBSD_version >= 900000 && __FreeBSD_version < 900027)
++#define log2f(x) (logf(x) / logf(2))
 +#endif
  
  extern "C"
