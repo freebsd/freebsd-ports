d8be07e9b6dd0505f94ddd4105602bbc719790d8
Michael Scheidell <scheidell@FreeBSD.org>
2011-12-08 11:18:08 +0000

    -Update to 2.4
    PR:		ports/162242
    Submitted by:	Denis Generalov <gd@rambler-co.ru>
    Approved by:	k@stereochro.me (maintainer timeout, 30 days), gabor (mentor)
    Feature safe:	yes

--- security/py-pycrypto/files/patch-setup.py
+++ security/py-pycrypto/files/patch-setup.py
@@ -1,27 +1,10 @@
---- setup.py.orig	2010-08-08 14:02:23.895825034 +0200
-+++ setup.py	2010-08-08 14:04:31.663792751 +0200
-@@ -139,6 +139,7 @@
+--- setup.py.orig	2011-11-01 17:47:23.000000000 +0400
++++ setup.py	2011-11-01 17:54:51.000000000 +0400
+@@ -177,6 +177,7 @@
                  # especially helps the DES modules.
                  self.__add_compiler_option("-O3")
                  self.__add_compiler_option("-fomit-frame-pointer")
 +                self.__add_compiler_option("-D_WCHAR_T_DEFINED")
                  # Don't include debug symbols unless debugging
                  self.__remove_compiler_option("-g")
-                 # Don't include profiling information (incompatible with -fomit-frame-pointer)
-@@ -157,7 +158,7 @@
-             self.compiler.include_dirs.insert(0, "src/inc-msvc/")
- 
-         # Detect libgmp and don't build _fastmath if it is missing.
--        lib_dirs = self.compiler.library_dirs + ['/lib', '/usr/lib']
-+        lib_dirs = self.compiler.library_dirs + ['/lib', '/usr/lib', '%%LOCALBASE%%/lib']
-         if not (self.compiler.find_library_file(lib_dirs, 'gmp')):
-             print >>sys.stderr, "warning: GMP library not found; Not building Crypto.PublicKey._fastmath."
-             self.__remove_extensions(["Crypto.PublicKey._fastmath"])
-@@ -264,6 +265,7 @@
-             Extension("Crypto.PublicKey._fastmath",
-                       include_dirs=['src/'],
-                       libraries=['gmp'],
-+                      library_dirs=['%%LOCALBASE%%/lib'],
-                       sources=["src/_fastmath.c"]),
- 
-             # Hash functions
+                 # Don't include profiling information (incompatible with
