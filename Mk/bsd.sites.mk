60dae3cd265ef7e772fd0f29b5bef7561fcb60f3
sunpoet <sunpoet@FreeBSD.org>
2011-12-09 19:01:50 +0000

    - Add ftp.twaren.net to MASTER_SITE_PERL_CPAN_BY
    Feature safe:	yes

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -928,11 +928,11 @@ MASTER_SITE_PERL_CPAN_BY+= \
 	ftp://ftp.auckland.ac.nz/pub/perl/CPAN/%CPANSORT%/%SUBDIR%/ \
 	http://backpan.perl.org/%CPANSORT%/%SUBDIR%/ \
 	ftp://ftp.funet.fi/pub/languages/perl/CPAN/%CPANSORT%/%SUBDIR%/ \
-	http://cpan.nctu.edu.tw/%CPANSORT%/%SUBDIR%/
+	http://cpan.nctu.edu.tw/%CPANSORT%/%SUBDIR%/ \
+	http://ftp.twaren.net/Unix/Lang/CPAN/%CPANSORT%/%SUBDIR%/
 
-_PERL_CPAN_FLAG = ${MASTER_SITE_SUBDIR:C/(CPAN):.*$/\1/}
-
-_PERL_CPAN_ID = ${MASTER_SITE_SUBDIR:C/^CPAN:(.)(.)(.*)$/\1\/\1\2\/\1\2\3/}
+_PERL_CPAN_FLAG=${MASTER_SITE_SUBDIR:C/(CPAN):.*$/\1/}
+_PERL_CPAN_ID=	${MASTER_SITE_SUBDIR:C/^CPAN:(.)(.)(.*)$/\1\/\1\2\/\1\2\3/}
 
 .if !empty(_PERL_CPAN_ID) && ${_PERL_CPAN_FLAG:L} == "cpan"
     _PERL_CPAN_SORT= authors/id/${_PERL_CPAN_ID}
