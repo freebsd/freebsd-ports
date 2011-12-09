04798fb61089f2746e7f44c41d32e1ad1189b6e8
wen <wen@FreeBSD.org>
2011-12-09 08:01:13 +0000

    - Update to 1.6.1
    PR:		162894
    Submitted by:	Gereon Kaiping <kta1c10!gereon@linta.de>
    Approved by:	"Li-Lun Leland Wang" <llwang@infor.org> (maintainer)
    Feature safe:	yes

--- math/py-numpy/files/patch-numpy-distutils-system_info.py
+++ math/py-numpy/files/patch-numpy-distutils-system_info.py
@@ -1,6 +1,6 @@
---- numpy/distutils/system_info.py.orig	2010-11-18 05:11:51.000000000 -0600
-+++ numpy/distutils/system_info.py	2011-10-08 11:34:44.399179056 -0500
-@@ -909,8 +909,8 @@
+--- numpy/distutils/system_info.py.orig	2011-07-01 22:31:13.000000000 +0100
++++ numpy/distutils/system_info.py	2011-11-26 09:54:54.000000000 +0000
+@@ -910,8 +910,8 @@
      dir_env_var = 'ATLAS'
      _lib_names = ['f77blas','cblas']
      if sys.platform[:7]=='freebsd':
@@ -11,7 +11,7 @@
      else:
          _lib_atlas = ['atlas']
          _lib_lapack = ['lapack']
-@@ -938,6 +938,7 @@
+@@ -939,6 +939,7 @@
              atlas = self.check_libs2(d,atlas_libs,[])
              lapack_atlas = self.check_libs2(d,['lapack_atlas'],[])
              if atlas is not None:
@@ -19,14 +19,3 @@
                  lib_dirs2 = [d] + self.combine_paths(d,['atlas*','ATLAS*'])
                  for d2 in lib_dirs2:
                      lapack = self.check_libs2(d2,lapack_libs,[])
-@@ -1383,8 +1384,9 @@
-             atlas_info = get_info('atlas_blas')
-         atlas_version = None
-         need_blas = 0
-+        lapack_type = self.cp.get(self.section, 'lapack_type').strip()
-         info = {}
--        if atlas_info:
-+        if lapack_type == 'atlas' and atlas_info:
-             version_info = atlas_info.copy()
-             atlas_version = get_atlas_version(**version_info)
-             if 'define_macros' not in atlas_info:
