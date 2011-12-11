be538897b815719ee0835ec13ca0195ffb58d269
Brendan Fabeny <bf@FreeBSD.org>
2011-12-11 05:44:53 +0000

    update to 1.52; don't build or install the manpage if NOPORTDOCS is defined
    Feature safe:	yes

--- devel/cppcheck/files/patch-cli__cmdlineparser.cpp
+++ devel/cppcheck/files/patch-cli__cmdlineparser.cpp
@@ -1,11 +1,11 @@
---- ./cli/cmdlineparser.cpp.orig	2011-08-14 03:45:53.000000000 -0400
-+++ ./cli/cmdlineparser.cpp	2011-09-17 03:13:23.000000000 -0400
-@@ -535,7 +535,7 @@
-             if (doc.LoadFile(12+argv[i]))
-             {
+--- cli/cmdlineparser.cpp.orig	2011-12-10 07:00:45.000000000 -0500
++++ cli/cmdlineparser.cpp	2011-12-10 22:32:04.000000000 -0500
+@@ -557,7 +557,7 @@
+             TiXmlDocument doc;
+             if (doc.LoadFile(12+argv[i])) {
                  TiXmlElement *node = doc.FirstChildElement();
--                for (; node && node->ValueStr() == "rule"; node = node->NextSiblingElement())
-+                for (; node && node->Value() == "rule"; node = node->NextSiblingElement())
-                 {
+-                for (; node && node->ValueStr() == "rule"; node = node->NextSiblingElement()) {
++                for (; node && node->Value() == "rule"; node = node->NextSiblingElement()) {
                      Settings::Rule rule;
  
+                     TiXmlElement *pattern = node->FirstChildElement("pattern");
