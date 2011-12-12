50c0c232c7a9f40961fac8ff9ff9af227b349da1
Daichi GOTO <daichi@FreeBSD.org>
2011-12-12 17:43:50 +0000

    - fixed a gcc 4.2.1 's namespace bug. Now mozc can build   himself using the system's default gcc. - removed the dependency for external gyp - removed the gsed dependency - removed the gnugrep dependency - refined the mozc relative ports
    Feature safe: yes
    Submitted by:	hrs

--- devel/py-gyp-devel/files/patch-pylib_gyp_generator_make.py
+++ devel/py-gyp-devel/files/patch-pylib_gyp_generator_make.py
@@ -1,21 +1,11 @@
---- pylib/gyp/generator/make.py.org	2010-09-03 18:36:39.151835296 +0900
-+++ pylib/gyp/generator/make.py	2010-09-03 18:55:43.973773838 +0900
-@@ -177,14 +177,14 @@
- r"""
- define fixup_dep
- # Fixup path as in (1).
--sed -e "s|^$(notdir $@)|$@|" $(depfile).raw >> $(depfile)
-+gsed -e "s|^$(notdir $@)|$@|" $(depfile).raw >> $(depfile)
- # Add extra rules as in (2).
+--- pylib/gyp/generator/make.py.orig	2010-06-26 08:55:39.000000000 +0900
++++ pylib/gyp/generator/make.py	2011-12-11 22:54:34.000000000 +0900
+@@ -182,7 +182,7 @@
  # We remove slashes and replace spaces with new lines;
  # remove blank lines;
  # delete the first line and append a colon to the remaining lines.
 -sed -e 's|\\||' -e 's| |\n|g' $(depfile).raw |\
--  grep -v '^$$'                              |\
--  sed -e 1d -e 's|$$|:|'                      \
-+gsed -e 's|\\||' -e 's| |\n|g' $(depfile).raw |\
-+  @@LOCALBASE@@/bin/grep -E -v '^$$'          |\
-+  gsed -e 1d -e 's|$$|:|'                      \
++env NL=`printf "\n"` sed -e 's|\\||' -e 's| |${NL}|g' $(depfile).raw |\
+   grep -v '^$$'                              |\
+   sed -e 1d -e 's|$$|:|'                      \
      >> $(depfile)
- rm $(depfile).raw
- endef
