c1d2f3c41d81c40f0d1df8400275516fdd896889
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2011-12-18 14:17:06 +0000

    - Bump PORTREVISION for databases/kyotocabinet shlib change - Fix 'make patch': original files/patch-extconf.rb cannot be applied cleanly

--- databases/ruby-kyotocabinet/files/patch-extconf.rb
+++ databases/ruby-kyotocabinet/files/patch-extconf.rb
@@ -1,5 +1,5 @@
---- extconf.rb.orig	2010-04-28 13:27:42.120543437 +0900
-+++ extconf.rb	2010-04-28 13:04:57.387073893 +0900
+--- extconf.rb.orig	2010-11-30 19:41:45.000000000 +0800
++++ extconf.rb	2011-12-18 16:36:33.312316430 +0800
 @@ -3,8 +3,9 @@
  File::unlink("Makefile") if (File::exist?("Makefile"))
  dir_config('kyotocabinet')
@@ -9,6 +9,6 @@
 +#home = ENV["HOME"]
 +#ENV["PATH"] = ENV["PATH"] + ":/usr/local/bin:$home/bin:."
 +ENV["PATH"] = '%s:%s' % [with_config('bindir'), ENV["PATH"]]
- kccflags = `kcutilcodec conf -i 2>/dev/null`.chomp
- kcldflags = `kcutilcodec conf -l 2>/dev/null`.chomp
+ kccflags = `kcutilmgr conf -i 2>/dev/null`.chomp
+ kcldflags = `kcutilmgr conf -l 2>/dev/null`.chomp
  kcldflags = kcldflags.gsub(/-l[\S]+/, "").strip
