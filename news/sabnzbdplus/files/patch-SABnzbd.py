435a9aa494bc37b2a483ab5f94770d05822d52e2
culot <culot@FreeBSD.org>
2011-12-19 09:21:22 +0000

    - Update to 0.6.14 [1] - Pet portlint(1) by removing extra blank line
    PR:		ports/163302 [1]
    Submitted by:	David Brooks <dab@rj45.org> (maintainer)

--- news/sabnzbdplus/files/patch-SABnzbd.py
+++ news/sabnzbdplus/files/patch-SABnzbd.py
@@ -1,5 +1,5 @@
---- SABnzbd.py.orig	2011-09-10 05:04:42.000000000 -0400
-+++ SABnzbd.py	2011-09-14 23:17:08.387342710 -0400
+--- SABnzbd.py.orig	2011-12-14 11:47:28.584883016 -0500
++++ SABnzbd.py	2011-12-14 11:49:08.861713731 -0500
 @@ -40,6 +40,9 @@
      print "The Python module Cheetah is required"
      sys.exit(1)
@@ -10,7 +10,7 @@
  import cherrypy
  if not cherrypy.__version__.startswith("3.2"):
      print "Sorry, requires Python module Cherrypy 3.2 (use the included version)"
-@@ -932,7 +935,7 @@
+@@ -962,7 +965,7 @@
  
      sabnzbd.MY_FULLNAME = os.path.normpath(os.path.abspath(sabnzbd.MY_FULLNAME))
      sabnzbd.MY_NAME = os.path.basename(sabnzbd.MY_FULLNAME)
