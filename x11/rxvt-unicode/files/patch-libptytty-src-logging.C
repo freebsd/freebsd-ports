b6446ea9c57798f3fd10e2954df1842dd3a0e252
Thierry Thomas <thierry@FreeBSD.org>
2011-12-22 21:35:55 +0000

    Upgrade to 9.14.
    Changelog at: <http://dist.schmorp.de/rxvt-unicode/Changes>.
    
    Note: support for libafterimage is deprecated and will be removed in the
    next release.

--- x11/rxvt-unicode/files/patch-libptytty-src-logging.C
+++ x11/rxvt-unicode/files/patch-libptytty-src-logging.C
@@ -1,52 +1,12 @@
---- libptytty/src/logging.C.orig	2011-05-30 22:59:42.000000000 +0200
-+++ libptytty/src/logging.C	2011-10-05 11:12:04.370185467 +0200
-@@ -223,7 +223,8 @@
+--- libptytty/src/logging.C.orig	2011-12-03 09:58:20.000000000 +0100
++++ libptytty/src/logging.C	2011-12-22 21:20:55.000000000 +0100
+@@ -264,7 +264,8 @@
+   // posix says that ut_line is not meaningful for DEAD_PROCESS
    // records, but most implementations of last use ut_line to
    // associate records in wtmp file
-   strncpy (utx->ut_line, line, sizeof (utx->ut_line));
--  strncpy (utx->ut_id, id, sizeof (utx->ut_id));
+-  strncpy (utx->ut_line, line, sizeof (utx->ut_line));
 +  if(id)
-+  	strncpy (utx->ut_id, id, sizeof (utx->ut_id));
++  	strncpy (utx->ut_line, line, sizeof (utx->ut_line));
+   fill_id (utx->ut_id, line, sizeof (utx->ut_id));
    utx->ut_pid = pid;
    utx->ut_type = login ? USER_PROCESS : DEAD_PROCESS;
-   utx->ut_tv.tv_sec = time (NULL);
-@@ -284,11 +285,19 @@
- #endif
- 
- #ifdef HAVE_STRUCT_UTMP
-+# ifdef HAVE_UTMP_PID
-   fill_utmp (ut, true, cmd_pid, ut_id, pty, user, hostname);
-+# else
-+  fill_utmp (ut, true, cmd_pid, 0, pty, user, hostname);
-+# endif
- #endif
- 
- #ifdef HAVE_STRUCT_UTMPX
-+# ifdef HAVE_UTMP_PID
-   fill_utmpx (utx, true, cmd_pid, ut_id, pty, user, hostname);
-+# else
-+  fill_utmpx (utx, true, cmd_pid, 0, pty, user, hostname);
-+# endif
- #endif
- 
- #ifdef HAVE_STRUCT_UTMP
-@@ -363,11 +372,19 @@
- #endif
- 
- #ifdef HAVE_STRUCT_UTMP
-+# ifdef HAVE_UTMP_PID
-   fill_utmp (ut, false, cmd_pid, ut_id, pty, 0, 0);
-+# else
-+  fill_utmp (ut, false, cmd_pid, 0, pty, 0, 0);
-+# endif
- #endif
- 
- #ifdef HAVE_STRUCT_UTMPX
-+# ifdef HAVE_UTMP_PID
-   fill_utmpx (utx, false, cmd_pid, ut_id, pty, 0, 0);
-+# else
-+  fill_utmpx (utx, false, cmd_pid, 0, pty, 0, 0);
-+# endif
- #endif
- 
-   /*
