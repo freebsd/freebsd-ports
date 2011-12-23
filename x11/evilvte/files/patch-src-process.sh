0d1966599559c875a277c157de6ba31e7249c01c
Michael Scheidell <scheidell@FreeBSD.org>
2011-12-23 12:20:52 +0000

    - Update to 0.5.0 [1] - Pet portlint
    PR:		ports/163071
    Submitted by:	Aragon Gouveia <aragon@phat.za.net> [1]
    Approved by:	gabor (mentor)

--- x11/evilvte/files/patch-src-process.sh
+++ x11/evilvte/files/patch-src-process.sh
@@ -1,28 +1,11 @@
---- src/process.sh.orig	2010-10-06 08:19:31.000000000 +0200
-+++ src/process.sh	2010-11-02 21:09:49.000000000 +0200
-@@ -19,11 +19,11 @@
- grep ^#define src/config.o > $CONFFILE
- rm -f $1 src/evilvte.o
+--- src/process.sh.orig	2011-12-04 22:40:02.000000000 +0200
++++ src/process.sh	2011-12-04 22:42:40.000000000 +0200
+@@ -147,7 +147,7 @@
  
--MENU_DEFAULT_ENCODING=`grep   MENU_ENCODING_LIST src/custom.h | sed -e 's/\t/ /g' -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | grep 'Default Encoding'`
--MENU_ENCODING_LIST_SIZE=`grep MENU_ENCODING_LIST src/custom.h | sed -e 's/\t/ /g' -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | tr ',' '\n' | grep '"' | wc -l`
--TOGGLE_BG_ORDER_SIZE=`grep    TOGGLE_BG_ORDER    src/custom.h | sed -e 's/\t/ /g' -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | tr ',' '\n' | grep '"' | wc -l`
--MENU_CUSTOM_SIZE=`grep        MENU_CUSTOM        src/custom.h | sed -e 's/\t/ /g' -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | tr ',' '\n' | grep '"' | wc -l`
--UPPER_PROGRAM_NAME=`grep      PROGRAM_NAME       src/custom.h | sed -e 's/\t/ /g' -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | cut -d '"' -f 2 | sed 's/\b\(.\)/\u\1/'`
-+MENU_DEFAULT_ENCODING=`grep   MENU_ENCODING_LIST src/custom.h | tr '\t' ' ' | sed -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | grep 'Default Encoding'`
-+MENU_ENCODING_LIST_SIZE=`grep MENU_ENCODING_LIST src/custom.h | sed -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | tr '\t,' ' \n' | grep '"' | wc -l`
-+TOGGLE_BG_ORDER_SIZE=`grep    TOGGLE_BG_ORDER    src/custom.h | sed -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | tr '\t,' ' \n' | grep '"' | wc -l`
-+MENU_CUSTOM_SIZE=`grep        MENU_CUSTOM        src/custom.h | sed -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | tr '\t,' ' \n' | grep '"' | wc -l`
-+UPPER_PROGRAM_NAME=`grep      PROGRAM_NAME       src/custom.h | sed -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | cut -d '"' -f 2 | tr '[:lower:]' '[:upper:]'`
- [ "$MENU_DEFAULT_ENCODING" != "" ]    && echo \#define MENU_DEFAULT_ENCODING   1                        >> $CONFFILE
- [ "$MENU_ENCODING_LIST_SIZE" != "0" ] && echo \#define MENU_ENCODING_LIST_SIZE $MENU_ENCODING_LIST_SIZE >> $CONFFILE
- [ "$TOGGLE_BG_ORDER_SIZE" != "0" ]    && echo \#define TOGGLE_BG_ORDER_SIZE    $TOGGLE_BG_ORDER_SIZE    >> $CONFFILE
-@@ -52,7 +52,7 @@
- [ "$TOGGLE_BG_NO_BACKGROUND" != "" ] && echo \#define TOGGLE_BG_NO_BACKGROUND 1 >> $CONFFILE
- [ "$TOGGLE_BG_OPACITY" != "" ]       && echo \#define TOGGLE_BG_OPACITY       1 >> $CONFFILE
+ head -n 1 $0 > src/showvte
+ tail -n 6 $0 | grep -v ^grep >> src/showvte
+-cat src/custom.h | tr '\t' ' ' | tr -s ' ' ' ' | sed -e 's/^ //' -e 's~/\*~\n~g' | grep ^\#define >> src/showvte
++grep ^\#define src/custom.h | tr '\t' ' ' | tr -s ' ' ' ' | sed -e 's/^ //' -e 's;/\*.*$;;g' >>src/showvte
  
--MENU_CUSTOM_DEFINE=`grep MENU_CUSTOM src/custom.h | sed -e 's/\t/ /g' -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1`
-+MENU_CUSTOM_DEFINE=`grep MENU_CUSTOM src/custom.h | sed -e 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1`
- 
- MENU_COPY=`echo $MENU_CUSTOM_DEFINE | grep Copy`
- [ "$MENU_COPY" != "" ] && echo \#define MENU_COPY 1 >> $CONFFILE
+ SHOWVTE_PROG_NAME=`grep PROGRAM_NAME    src/custom.h | tr '\t' ' ' | sed 's/^\( \)*//g' | grep -v ^\/\/ | tail -n 1 | cut -d '"' -f 2`
+ [ "$SHOWVTE_PROG_NAME" = "" ] && SHOWVTE_PROG_NAME="evilvte"
