09d38183b2583fe63101d012f70d8eaa032bf623
ale <ale@FreeBSD.org>
2011-12-23 09:21:37 +0000

    Another fix for clang.

--- mail/qmailmrtg7/files/patch-checkq.c
+++ mail/qmailmrtg7/files/patch-checkq.c
@@ -1,10 +1,13 @@
---- checkq.c.orig	2011-12-15 12:22:43.000000000 +0100
-+++ checkq.c	2011-12-15 12:23:47.000000000 +0100
-@@ -21,6 +21,7 @@
+--- checkq.c.orig	2004-02-29 10:54:02.000000000 +0100
++++ checkq.c	2011-12-23 10:20:40.000000000 +0100
+@@ -21,8 +21,9 @@
  #include <sys/stat.h>
  #include <unistd.h>
  #include <dirent.h>
 +#include <string.h>
  
- int main(int argc, char *argv)
+-int main(int argc, char *argv)
++int main(int argc, char **argv)
  {
+  FILE *fs;
+  DIR *mydir;
