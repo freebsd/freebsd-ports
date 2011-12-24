74a31cbd7f8d2448daa28f93e65045bd0c9f6908
Jimmy Olgeni <olgeni@FreeBSD.org>
2011-12-24 21:34:10 +0000

    - Upgrade to version 0.12
    - Add OPTIONS support with optional X11 and HTML notebook support
      (only RUN_DEPENDS is affected)

--- devel/ipython/files/patch-setupbase.py
+++ devel/ipython/files/patch-setupbase.py
@@ -3,17 +3,17 @@ $FreeBSD$
 
 --- setupbase.py.orig
 +++ setupbase.py
-@@ -202,7 +202,8 @@
+@@ -197,7 +197,8 @@
      """
-     
+ 
      docdirbase  = pjoin('share', 'doc', 'ipython')
 -    manpagebase = pjoin('share', 'man', 'man1')
 +    manpagebase = pjoin('man', 'man1')
 +    examplebase = pjoin('share', 'examples', 'ipython')
-     
+ 
      # Simple file lists can be made by hand
      manpages  = filter(isfile, glob(pjoin('docs','man','*.1.gz')))
-@@ -216,7 +217,7 @@
+@@ -211,7 +212,7 @@
      example_files = make_dir_struct(
          'data',
          pjoin('docs','examples'),
