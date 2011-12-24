639f2649c9b1cbcc200e155cdcbe8b4742428bae
zi <zi@FreeBSD.org>
2011-12-24 13:35:30 +0000

    - Update to 1.4 - Honor custom LOCALBASE/PREFIX
    PR:		ports/163243
    Submitted by:	Frank Wall <fw@moov.de> (maintainer)

--- databases/autobackupmysql/files/patch-autobackupmysql.sh
+++ databases/autobackupmysql/files/patch-autobackupmysql.sh
@@ -1,20 +1,26 @@
---- autobackupmysql.sh.orig	2009-12-21 15:25:29.000000000 +0100
-+++ autobackupmysql.sh	2009-12-21 16:07:01.000000000 +0100
+--- autobackupmysql.sh.orig	2009-12-18 13:23:34.000000000 +0100
++++ autobackupmysql.sh	2011-12-13 13:18:11.000000000 +0100
 @@ -1,4 +1,4 @@
 -#!/bin/bash
 +#!/usr/local/bin/bash
  #
  # Automatic MySQL Backup Script
  # VER. 1.1 - http://sourceforge.net/projects/autobackupmysql/
-@@ -30,8 +30,8 @@
- # Check configuration files
- if [ -f "autobackupmysql.conf" ] ; then
- 	source "autobackupmysql.conf"
--elif [ - f "/etc/autobackupmysql.conf" ] ; then
--	source "/etc/autobackupmysql.conf"
-+elif [ -f "/usr/local/etc/autobackupmysql.conf" ] ; then
-+	source "/usr/local/etc/autobackupmysql.conf"
- else
- # Username to access the MySQL server e.g. dbuser
- USERNAME='dbuser'
-
+@@ -292,6 +292,7 @@
+ LOGERR=$BACKUPDIR/ERRORS_$DBHOST-`date +%N`.log		# Logfile Name
+ BACKUPFILES=""
+ OPT="--quote-names --opt"			# OPT string for use with mysqldump ( see man mysqldump )
++UNAME_S="`uname -s | tr "[:upper:]" "[:lower:]"`"	# identify operating system
+ 
+ # Add --compress mysqldump option to $OPT
+ if [ "$COMMCOMP" = "yes" ];
+@@ -440,7 +441,8 @@
+ 	# If DBs are excluded
+ 	for exclude in $DBEXCLUDE
+ 	do
+-		DBNAMES=`echo $DBNAMES | sed "s/\b$exclude\b//g"`
++		[ "$UNAME_S" = "linux" ] && DBNAMES=`echo $DBNAMES | sed "s/\b$exclude\b//g"`
++		[ "$UNAME_S" = "freebsd" ] && DBNAMES=`echo $DBNAMES | sed "s/[[:<:]]$exclude[[:>:]]//g"`
+ 	done
+ 
+         MDBNAMES=$DBNAMES
