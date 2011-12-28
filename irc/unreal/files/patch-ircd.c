8f2a577908e79b79333e663b4bcc8143bba22e45
crees <crees@FreeBSD.org>
2011-12-28 12:13:37 +0000

    Update to 3.2.9
    PR:		ports/162928
    Submitted by:	Jr Aquino <tanawts@gmail.com> (maintainer)

--- irc/unreal/files/patch-ircd.c
+++ irc/unreal/files/patch-ircd.c
@@ -1,14 +1,20 @@
---- src/ircd.c	2009-03-01 19:37:58.000000000 +0100
-+++ src/ircd.c	2009-08-18 16:20:39.000000000 +0200
-@@ -1369,9 +1369,9 @@
+$FreeBSD$
+
+--- src/ircd.c	2011-11-05 02:25:24.000000000 -0700
++++ src/ircd.c	2011-11-28 10:21:38.000000000 -0800
+@@ -1407,12 +1407,12 @@
  	}
  #endif
  #ifndef _WIN32
 -	mkdir("tmp", S_IRUSR|S_IWUSR|S_IXUSR); /* Create the tmp dir, if it doesn't exist */
 +	mkdir("%%RUNDIR%%/tmp", S_IRUSR|S_IWUSR|S_IXUSR); /* Create the tmp dir, if it doesn't exist */
+  #if defined(USE_LIBCURL) && defined(REMOTEINC_SPECIALCACHE)
+  	mkdir("cache", S_IRUSR|S_IWUSR|S_IXUSR); /* Create the cache dir, if using curl and it doesn't exist */
+  #endif
  #else
 -	mkdir("tmp");
 +	mkdir("%%RUNDIR%%/tmp");
- #endif
- #ifndef _WIN32
- 	/*
+  #if defined(USE_LIBCURL) && defined(REMOTEINC_SPECIALCACHE)
+  	mkdir("cache");
+  #endif
+
--- irc/unreal/files/patch-s_conf.c
+++ irc/unreal/files/patch-s_conf.c
@@ -1,11 +1,13 @@
---- src/s_conf.c	Mon Mar 21 09:10:42 2005
-+++ src/s_conf.c	Mon Mar 21 09:15:09 2005
-@@ -8609,7 +8609,7 @@
- 		{
- 			char *urlfile = url_getfilename(url);
- 			char *file = unreal_getfilename(urlfile);
--			char *tmp = unreal_mktemp("tmp", file);
-+			char *tmp = unreal_mktemp("%%RUNDIR%%/tmp", file);
- 			unreal_copyfileex(inc->file, tmp, 1);
- 			add_remote_include(tmp, url, 0, NULL);
- 			free(urlfile);
+$FreeBSD$
+
+--- src/s_conf.c	2011-11-05 02:25:24.000000000 -0700
++++ src/s_conf.c	2011-11-28 10:21:38.000000000 -0800
+@@ -9652,7 +9652,7 @@
+ 	{
+ 		char *urlfile = url_getfilename(url);
+ 		char *file_basename = unreal_getfilename(urlfile);
+-		char *tmp = unreal_mktemp("tmp", file_basename);
++		char *tmp = unreal_mktemp("%%RUNDIR%%/tmp", file);
+ 		free(urlfile);
+ 
+ 		if (cached)
