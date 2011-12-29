2b0f2325b60cc843ae100ec38a1a5360b5e0be8f
Pawel Pekala <pawel@FreeBSD.org>
2011-12-29 14:07:57 +0000

    - Update to version 0.6.5 [1] - Fix build when WITH_BUNDLED_LIBPNG=true
    PR:		ports/156376 [1]
    Submitted by:	Anton Yuzhaninov <ayuzhaninov@team.vega.ru> [1]
    Approved by:	maintainer timeout

--- graphics/optipng/files/patch-src-opngreduc.c
+++ graphics/optipng/files/patch-src-opngreduc.c
@@ -1,11 +1,11 @@
---- src/opngreduc.c.orig	2010-01-03 06:59:00.000000000 +0100
-+++ src/opngreduc.c	2010-04-05 22:02:26.000000000 +0200
-@@ -23,7 +23,7 @@
- #define PNG_NO_PEDANTIC_WARNINGS
- #include "png.h"
+--- src/opngreduc.c.orig	2011-12-29 14:47:34.000000000 +0100
++++ src/opngreduc.c	2011-12-29 14:48:52.000000000 +0100
+@@ -27,6 +27,8 @@
  #if PNG_LIBPNG_VER >= 10400
--#include "pngpriv.h"
+ #ifdef PNG_USER_PRIVATEBUILD
+ #include "pngpriv.h"
++#else
 +#include "libpng/pngpriv.h"
- #else
- #define trans_alpha trans
- #define trans_color trans_values
+ #endif
+ #endif
+ 
