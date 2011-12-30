9960c16a26f56270c73eaf99961ab911b5906462
ale <ale@FreeBSD.org>
2011-12-30 15:22:18 +0000

    Update to 20110201 release.

--- converters/dumpasn1/files/patch-dumpasn1.c
+++ converters/dumpasn1/files/patch-dumpasn1.c
@@ -1,14 +1,6 @@
 --- dumpasn1.c.orig	2010-09-30 11:17:16.000000000 +0200
 +++ dumpasn1.c	2010-09-30 11:18:35.000000000 +0200
-@@ -46,6 +46,7 @@
- 
- #include <ctype.h>
- #include <limits.h>
-+#include <stdarg.h>
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
-@@ -309,11 +310,13 @@
+@@ -309,11 +309,13 @@
     beginning with a '$' uses the appropriate environment variable.  In
     addition under Unix we also walk down $PATH looking for it */
  
