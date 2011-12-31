9584c9eb623b370be18ce3e2fe870d2f08509f96
oliver <oliver@FreeBSD.org>
2011-12-31 15:13:01 +0000

    update multimedia/audacious to 3.1.1 update multimedia/audacious-plugins to 3.1.1 update audio/audacious-duumb to 0.73 (to make it compatible with audacious 3.1.1) [1] mark audio/wmauda as broken (not compatible with audacious 3.1.1) bump PORTREVISION of depeding ports
    Approved by:	maintainer [1]

--- multimedia/audacious-plugins/files/patch-src-OSS4-audio.c
+++ multimedia/audacious-plugins/files/patch-src-OSS4-audio.c
@@ -1,11 +1,11 @@
---- src/oss4/oss.c.orig	2010-09-09 13:56:41.000000000 +0200
-+++ src/oss4/oss.c	2010-09-09 13:56:50.000000000 +0200
-@@ -212,7 +212,7 @@
-         {
-             ERROR_MSG;
+--- src/oss4/oss.c.orig	2011-11-08 17:38:49.000000000 +0100
++++ src/oss4/oss.c	2011-11-24 11:26:36.000000000 +0100
+@@ -191,7 +191,7 @@
+ {
+     AUDDBG("Drain.\n");
  
--            if (ioctl(oss_data->fd, SNDCTL_DSP_SYNC, NULL) == -1)
-+            if (ioctl(oss_data->fd, SNDCTL_DSP_RESET, NULL) == -1)
-                 DEBUG_MSG;
+-    if (ioctl(oss_data->fd, SNDCTL_DSP_SYNC, NULL) == -1)
++    if (ioctl(oss_data->fd, SNDCTL_DSP_RESET, NULL) == -1)
+         DESCRIBE_ERROR;
+ }
  
-             break;
