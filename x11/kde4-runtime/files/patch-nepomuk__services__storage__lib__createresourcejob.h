9136223a53c48fcfe39335727e9d5b3d607cf1a7
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2011-12-31 02:53:48 +0000

    Fix a few build issues with clang.
    Import my commit f1794e4 upstream (based on the PR's patch) as well as the
    patch to describeresourcejob.h in the PR (which does not apply upstream because
    the line it adds is already there).
    
    No need to bump PORTREVISION, this commit only fixes the build for people who
    had never been able to build the port anyway.
    
    PR:		ports/163699
    Submitted by:	Heath Nielson <heathn@gmail.com>
    Approved by:	avilla (mentor, implicit)

---
+++ x11/kde4-runtime/files/patch-nepomuk__services__storage__lib__createresourcejob.h
@@ -0,0 +1,8 @@
+--- ./nepomuk/services/storage/lib/createresourcejob.h
++++ ./nepomuk/services/storage/lib/createresourcejob.h
+@@ -27,6 +27,7 @@
+ #include <QtCore/QHash>
+ #include <QtCore/QUrl>
+ 
++#include "datamanagement.h"
+ #include "nepomukdatamanagement_export.h"
---
+++ x11/kde4-runtime/files/patch-nepomuk__services__storage__lib__describeresourcejob.h
@@ -0,0 +1,10 @@
+--- ./nepomuk/services/storage/lib/describeresourcesjob.h
++++ ./nepomuk/services/storage/lib/describeresourcesjob.h
+@@ -27,6 +27,7 @@
+ #include <QtCore/QList>
+ #include <QtCore/QUrl>
+ 
++#include "datamanagement.h"
+ #include "nepomukdatamanagement_export.h"
+ 
+ class QDBusPendingCallWatcher;
