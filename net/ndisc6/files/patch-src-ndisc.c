7434e99a3fda0eb684009c87cf2df59555157968
beech <beech@FreeBSD.org>
2011-12-31 22:14:33 +0000

    - Add startup script for rdnssd - Make merge-hook usable - Change ND_RA_FLAG_HOME_AGENT to ND_RA_FLAG_HA instead of deletion - Bump PORTREVISION
    PR:		ports/161172
    Submitted by:	Mikhail T. <m.tsatsenko@gmail.com>

--- net/ndisc6/files/patch-src-ndisc.c
+++ net/ndisc6/files/patch-src-ndisc.c
@@ -1,11 +1,11 @@
---- src/ndisc.c.orig	2011-09-10 14:17:49.714181834 +0400
-+++ src/ndisc.c	2011-09-10 14:17:51.893973268 +0400
-@@ -440,8 +440,6 @@
- 		        gettext ((v & ND_RA_FLAG_MANAGED) ? N_ ("Yes") : N_("No")));
+--- src/ndisc.c.orig    2011-09-10 14:17:49.714181834 +0400
++++ src/ndisc.c 2011-09-10 14:17:51.893973268 +0400
+@@ -441,7 +441,7 @@
  		printf (_("Stateful other conf.      :          %3s\n"),
  		        gettext ((v & ND_RA_FLAG_OTHER) ? N_ ("Yes") : N_("No")));
--		printf (_("Mobile home agent         :          %3s\n"),
+ 		printf (_("Mobile home agent         :          %3s\n"),
 -		        gettext ((v & ND_RA_FLAG_HOME_AGENT) ? N_ ("Yes") : N_("No")));
++		        gettext ((v & ND_RA_FLAG_HA) ? N_ ("Yes") : N_("No")));
  		printf (_("Router preference         :       %6s\n"), pref_i2n (v));
  		printf (_("Neighbor discovery proxy  :          %3s\n"),
  		        gettext ((v & 0x04) ? N_ ("Yes") : N_("No")));
