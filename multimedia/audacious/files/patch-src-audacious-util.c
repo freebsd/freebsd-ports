ac1b7fe1833fae667aaa4bad4e8c864274b6b3f6
Oliver Lehmann <oliver@FreeBSD.org>
2011-12-31 15:13:01 +0000

    update multimedia/audacious to 3.1.1 update multimedia/audacious-plugins to 3.1.1 update audio/audacious-duumb to 0.73 (to make it compatible with audacious 3.1.1) [1] mark audio/wmauda as broken (not compatible with audacious 3.1.1) bump PORTREVISION of depeding ports
    Approved by:	maintainer [1]

--- multimedia/audacious/files/patch-src-audacious-util.c
+++ multimedia/audacious/files/patch-src-audacious-util.c
@@ -1,14 +1,14 @@
---- src/audacious/util.c.orig	2011-04-23 14:49:07.000000000 +0200
-+++ src/audacious/util.c	2011-04-23 14:50:08.000000000 +0200
-@@ -174,9 +174,9 @@
-         return NULL;
-     }
+--- src/audacious/util.c.orig	2011-11-08 17:37:12.000000000 +0100
++++ src/audacious/util.c	2011-11-23 20:49:18.000000000 +0100
+@@ -142,9 +142,9 @@
+             return NULL;
+         }
  #else
--    if ((len = readlink ("/proc/self/exe", buf, sizeof buf)) < 0)
-+    if ((len = readlink ("/proc/curproc/file", buf, sizeof buf)) < 0)
-     {
--        fprintf (stderr, "Cannot access /proc/self/exe: %s.\n", strerror (errno));
-+        fprintf (stderr, "Cannot access /proc/curproc/file: %s.\n", strerror (errno));
-         return NULL;
-     }
- #endif
+-        if ((len = readlink ("/proc/self/exe", buf, size)) < 0)
++        if ((len = readlink ("/proc/curproc/file", buf, size)) < 0)
+         {
+-            fprintf (stderr, "Cannot access /proc/self/exe: %s.\n", strerror (errno));
++            fprintf (stderr, "Cannot access /proc/curproc/file: %s.\n", strerror (errno));
+             g_free (buf);
+             return NULL;
+         }
