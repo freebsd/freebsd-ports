8f56a53e0fd513265b1d317c25883978c3e6809b
Alexander Logvinov <avl@FreeBSD.org>
2012-01-01 23:56:48 +0000

    - Update to 0.7.0 - This version supports VirtualBox 4.X

--- deskutils/vboxgtk/files/patch-vboxgtk___init__.py
+++ deskutils/vboxgtk/files/patch-vboxgtk___init__.py
@@ -2,9 +2,9 @@
 +++ vboxgtk/__init__.py	2009-09-04 21:47:01.000000000 +1000
 @@ -37,8 +37,6 @@
  gettext_domain = 'vboxgtk'
- gettext.install(gettext_domain, locale_path, unicode = True)
+ gettext.install(gettext_domain, paths.locale_path, unicode = True)
  locale.setlocale(locale.LC_ALL, '')
--locale.bindtextdomain(gettext_domain, locale_path)
+-locale.bindtextdomain(gettext_domain, paths.locale_path)
 -locale.textdomain(gettext_domain)
  
  
