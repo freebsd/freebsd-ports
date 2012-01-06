d6b221a9cd263e93a293ac5300f31d3dcb1d47fe
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2012-01-06 08:25:43 +0000

    - Update to 1.1.1
    Changes:	http://savannah.gnu.org/forum/forum.php?forum_id=7073

--- sysutils/freeipmi/files/patch-libfreeipmi-driver-ipmi-kcs-driver.c
+++ sysutils/freeipmi/files/patch-libfreeipmi-driver-ipmi-kcs-driver.c
@@ -1,6 +1,6 @@
---- libfreeipmi/src/driver/ipmi-kcs-driver.c.orig	2011-01-21 05:38:22.000000000 +0800
-+++ libfreeipmi/src/driver/ipmi-kcs-driver.c	2011-01-24 09:49:23.000000000 +0800
-@@ -484,7 +484,7 @@
+--- libfreeipmi/driver/ipmi-kcs-driver.c.orig	2012-01-03 03:25:58.000000000 +0800
++++ libfreeipmi/driver/ipmi-kcs-driver.c	2012-01-06 04:38:31.485000601 +0800
+@@ -491,7 +491,7 @@
  #ifdef __FreeBSD__
  #ifdef USE_IOPERM
    /* i386_set_ioperm has known problems on FBSD 5.x (bus errors). */
