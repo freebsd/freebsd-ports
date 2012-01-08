a8a13f8d11f921ccce3f473cda866860dd856db2
maho <maho@FreeBSD.org>
2012-01-08 07:26:16 +0000

    Update to 2.4
    Spotted by:	pgf@

--- cad/calculix/files/patch-cgx_VER+src+cgx.h
+++ cad/calculix/files/patch-cgx_VER+src+cgx.h
@@ -1,11 +1,11 @@
---- cgx_2.3/src/cgx.h.orig	2009-08-13 15:58:59.000000000 +0900
-+++ cgx_2.3/src/cgx.h	2009-09-03 15:07:10.000000000 +0900
+--- cgx_2.4/src/cgx.h.orig	2009-08-13 15:58:59.000000000 +0900
++++ cgx_2.4/src/cgx.h	2009-09-03 15:07:10.000000000 +0900
 @@ -86,8 +86,8 @@
                               "/yaprod/cae/application/CalculiX/doc/ccx_2.0/ccx.html"}
    #endif
  #else
--  #define     HELPFILE        {"/usr/local/CalculiX/cgx_2.3/doc/cgx/cgx.html",\
--    "/usr/local/CalculiX/ccx_2.3/doc/ccx/ccx.html"}
+-  #define     HELPFILE        {"/usr/local/CalculiX/cgx_2.4/doc/cgx/cgx.html",\
+-    "/usr/local/CalculiX/ccx_2.4/doc/ccx/ccx.html"}
 +   #define     HELPFILE        {"%%DOCSDIR%%/cgx/cgx.html",\
 +     "%%DOCSDIR%%/ccx/ccx.html"}
  #endif
