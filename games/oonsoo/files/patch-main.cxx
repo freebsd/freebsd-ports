73664c4e4372748efe422c82395a5444234ff39b
Eitan Adler <eadler@FreeBSD.org>
2012-01-09 02:43:50 +0000

    Fix build with clang as compiler

---
+++ games/oonsoo/files/patch-main.cxx
@@ -0,0 +1,10 @@
+--- main.cxx.old	2012-01-08 21:41:40.000000000 -0500
++++ main.cxx	2012-01-08 21:41:46.000000000 -0500
+@@ -25,6 +25,7 @@
+ #include "Engine.hxx"
+ 
+ 
++int
+ main(int argc, char **argv)
+ {
+   // Build the sample collection
