b962d46bbca3e7eea6dc52472497cefaf41b7438
Pawel Pekala <pawel@FreeBSD.org>
2012-01-10 21:51:08 +0000

    Update to version 0.9
    PR:		ports/163952
    Submitted by:	maintainer, based on work by David Roffiaen

---
+++ games/pokerth/files/patch-src_engine_log.h
@@ -0,0 +1,10 @@
+--- src/engine/log.h.orig	2012-01-04 13:48:11.224976420 +0100
++++ src/engine/log.h	2012-01-04 14:01:30.113974000 +0100
+@@ -21,6 +21,7 @@
+ 
+ #include "engine_defs.h"
+ #include "game_defs.h"
++#include <string>
+ 
+ struct sqlite3;
+ 
