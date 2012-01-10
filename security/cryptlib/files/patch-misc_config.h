d4b7f9833199b4718b43dc313953c6f1a61e19dc
Alex Dupre <ale@FreeBSD.org>
2012-01-10 11:52:40 +0000

    Update to 3.4.1 release.

--- security/cryptlib/files/patch-misc_config.h
+++ security/cryptlib/files/patch-misc_config.h
@@ -1,10 +1,10 @@
---- misc/config.h.orig	2010-12-21 02:29:54.000000000 +0100
-+++ misc/config.h	2011-03-04 15:31:57.000000000 +0100
-@@ -287,6 +287,7 @@
+--- misc/config.h.orig	2011-08-14 01:40:52.000000000 +0200
++++ misc/config.h	2012-01-10 12:45:17.000000000 +0100
+@@ -280,6 +280,7 @@
  
  /* General device usage */
  
 +#define USE_PKCS11
- #if defined( USE_PKCS11 ) || defined( USE_FORTEZZA ) || defined( USE_CRYPTOAPI )
+ #if defined( USE_PKCS11 ) || defined( USE_CRYPTOAPI )
    #define USE_DEVICES
  #endif /* Device types */
