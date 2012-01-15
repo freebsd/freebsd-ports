36c6b8260133c93a4197536965dfb94c4ece41f0
Hiroki Sato <hrs@FreeBSD.org>
2012-01-15 08:02:26 +0000

    Update to 9.04.

--- print/ghostscript9/files/patch-base-gsicc_create.c
+++ print/ghostscript9/files/patch-base-gsicc_create.c
@@ -1,17 +1,10 @@
---- base/gsicc_create.c.orig	2011-05-23 13:33:34.000000000 +0900
-+++ base/gsicc_create.c	2011-05-23 13:33:52.000000000 +0900
-@@ -116,6 +116,7 @@
-    
+--- base/gsicc_create.c.orig	2011-08-05 20:12:20.000000000 +0900
++++ base/gsicc_create.c	2011-12-06 15:23:14.000000000 +0900
+@@ -113,6 +113,7 @@
+ 
     */
  
 +#include <sys/types.h>
- #include "icc34.h"   /* Note this header is needed even if lcms is not 
+ #include "icc34.h"   /* Note this header is needed even if lcms is not
                              compiled as default CMS */
  #include "string_.h"
-@@ -2350,4 +2351,4 @@
-     icHeader  *header = &(iccprofile.header);
- 
-     setheader_common(header);
--}
-\ No newline at end of file
-+}
