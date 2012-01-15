dbadfe7b3ab875ebd4f73c85ef3ff79423a38369
Michael Scheidell <scheidell@FreeBSD.org>
2012-01-15 22:31:53 +0000

    - Update to 1.0.r20111218
    PR:		ports/163969
    Submitted by:	thomas.e.zander@googlemail.com (maintainer)
    Approved by:	gabor (mentor)
    ----------------------------------------------------------------------

--- multimedia/mplayer/files/patch-stream-tvi_v4l2.c
+++ multimedia/mplayer/files/patch-stream-tvi_v4l2.c
@@ -1,14 +1,14 @@
---- stream/tvi_v4l2.c	2007-10-07 23:49:26.000000000 +0400
-+++ stream/tvi_v4l2.c	2010-01-27 13:25:47.000000000 +0300
-@@ -38,7 +38,6 @@ known issues:
- #ifdef HAVE_SYS_SYSINFO_H
- #include <sys/sysinfo.h>
- #endif
+--- stream/tvi_v4l2.c.orig	2011-06-26 16:42:35.000000000 +0200
++++ stream/tvi_v4l2.c	2011-11-26 20:09:03.369654854 +0100
+@@ -52,7 +52,6 @@
+ #ifdef HAVE_SYS_VIDEOIO_H
+ #include <sys/videoio.h>
+ #else
 -#include <linux/types.h>
  #include <linux/videodev2.h>
+ #endif
  #include "mp_msg.h"
- #include "libmpcodecs/img_format.h"
-@@ -46,6 +46,14 @@ known issues:
+@@ -62,6 +61,14 @@
  #include "tv.h"
  #include "audio_in.h"
  
