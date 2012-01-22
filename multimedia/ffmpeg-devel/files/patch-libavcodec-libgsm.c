e54af57539db3bfda623b48e1bdad20102008803
Martin Matuska <mm@FreeBSD.org>
2012-01-22 15:48:18 +0000

    Update ffmpeg-devel to 2012-01-22 (b4ec645) Unbreak for i386 and sparc64 Set PKGNAMESUFFIX to -devel Fix CONFLICTS with ffmpeg

--- multimedia/ffmpeg-devel/files/patch-libavcodec-libgsm.c
+++ multimedia/ffmpeg-devel/files/patch-libavcodec-libgsm.c
@@ -1,11 +1,11 @@
---- libavcodec/libgsm.c.orig	2010-04-18 19:05:53.640509610 +0200
-+++ libavcodec/libgsm.c	2010-04-18 19:05:59.140526816 +0200
-@@ -28,7 +28,7 @@
+--- libavcodec/libgsm.c.orig	2012-01-22 14:41:15.000000000 +0100
++++ libavcodec/libgsm.c	2012-01-22 16:06:24.357156664 +0100
+@@ -27,7 +27,7 @@
+ 
  // The idiosyncrasies of GSM-in-WAV are explained at http://kbs.cs.tu-berlin.de/~jutta/toast.html
  
- #include "avcodec.h"
 -#include <gsm/gsm.h>
 +#include <gsm.h>
  
- // gsm.h misses some essential constants
- #define GSM_BLOCK_SIZE 33
+ #include "avcodec.h"
+ #include "gsm.h"
--- multimedia/ffmpeg-devel/files/patch-libavdevice-bktr.c
+++ multimedia/ffmpeg-devel/files/patch-libavdevice-bktr.c
@@ -1,14 +1,14 @@
---- libavdevice/bktr.c.orig	2011-06-25 04:32:49.000000000 +0200
-+++ libavdevice/bktr.c	2011-06-25 10:11:19.281738648 +0200
+--- libavdevice/bktr.c.orig	2012-01-22 14:41:15.000000000 +0100
++++ libavdevice/bktr.c	2012-01-22 16:07:07.873280412 +0100
 @@ -24,6 +24,11 @@
   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
   */
  
-+typedef unsigned char	u_char;
-+typedef unsigned short	u_short;
-+typedef unsigned int	u_int;
-+typedef unsigned long	u_long;
++typedef unsigned char u_char;
++typedef unsigned short        u_short;
++typedef unsigned int  u_int;
++typedef unsigned long u_long;
 +
+ #include "libavformat/internal.h"
  #include "libavutil/log.h"
  #include "libavutil/opt.h"
- #include "libavutil/parseutils.h"
