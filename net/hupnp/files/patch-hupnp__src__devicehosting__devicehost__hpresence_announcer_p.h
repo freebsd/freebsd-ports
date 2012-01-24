2f1f5733668bf1222efed6db21b73d2032391544
Alberto Villa <avilla@FreeBSD.org>
2012-01-24 09:56:09 +0000

    - Fix build with Clang.
    Clang needs to be explicitly told that Herqq::Upnp::HSsdp is a parent
    of Herqq::Upnp::HDeviceHostSsdpHandler.
    
    Approved by:	fluffy (maintainer, implicit)
    With hat on:	kde

---
+++ net/hupnp/files/patch-hupnp__src__devicehosting__devicehost__hpresence_announcer_p.h
@@ -0,0 +1,10 @@
+--- ./hupnp/src/devicehosting/devicehost/hpresence_announcer_p.h.orig	2012-01-24 10:41:39.103825710 +0100
++++ ./hupnp/src/devicehosting/devicehost/hpresence_announcer_p.h	2012-01-24 10:42:13.739148738 +0100
+@@ -31,6 +31,7 @@
+ //
+ 
+ #include "hserverdevicecontroller_p.h"
++#include "hdevicehost_ssdp_handler_p.h"
+ 
+ #include "../../general/hupnp_global_p.h"
+ #include "../../devicemodel/hdevicestatus.h"
