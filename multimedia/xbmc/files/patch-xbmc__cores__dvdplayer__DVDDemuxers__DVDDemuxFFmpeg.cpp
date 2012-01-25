ce4f068467147da4da12ba80f43a27274f602c75
scheidell <scheidell@FreeBSD.org>
2012-01-25 13:48:29 +0000

    - to beta 2
    PR:		ports/164401
    Submitted by:	maintainer
    Approved by:	gabor(mentor)

--- multimedia/xbmc/files/patch-xbmc__cores__dvdplayer__DVDDemuxers__DVDDemuxFFmpeg.cpp
+++ multimedia/xbmc/files/patch-xbmc__cores__dvdplayer__DVDDemuxers__DVDDemuxFFmpeg.cpp
@@ -1,9 +1,9 @@
 --- ./xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp.orig	2011-12-23 04:12:21.000000000 +0100
 +++ ./xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp	2012-01-02 10:14:38.000000000 +0100
-@@ -90,12 +90,20 @@
- // these need to be put somewhere that are compiled, we should have some better place for it
+@@ -91,12 +91,20 @@
  
  CCriticalSection DllAvCodec::m_critSection;
+ static CCriticalSection m_logSection;
 +#ifdef __FreeBSD__
 +std::map<uint64_t, CStdString> g_logbuffer;
 +#else
@@ -12,7 +12,7 @@
  
  void ff_avutil_log(void* ptr, int level, const char* format, va_list va)
  {
-   CSingleLock lock(DllAvCodec::m_critSection);
+   CSingleLock lock(m_logSection);
 +#ifdef __FreeBSD__
 +  uint64_t threadId = (uint64_t)CThread::GetCurrentThreadId();
 +#else
@@ -21,7 +21,7 @@
    CStdString &buffer = g_logbuffer[threadId];
  
    AVClass* avc= ptr ? *(AVClass**)ptr : NULL;
-@@ -144,7 +152,11 @@
+@@ -145,7 +153,11 @@
    /* Loop through the logbuffer list and remove any blank buffers
       If the thread using the buffer is still active, it will just
       add a new buffer next time it writes to the log */
