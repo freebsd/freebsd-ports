ea47d1fb1e253f060c315d301eaf8f659004da3f
Doug Barton <dougb@FreeBSD.org>
2012-01-25 07:16:18 +0000

    Update to version 0.9.6, which is primarily a documentation update to new version 0.9.5.
    The upgrade to 0.9.5 includes a change from pickle to msgpack for
    request serialization. If you update the master and all the minions at
    the same time there is no problem. Otherwise follow the instructions
    in the release notes at:
    
    http://saltstack.org/topics/releases/0.9.5/
    
    Submitted by:	christer.edwards@gmail.com (maintainer) via private mail

--- sysutils/salt/files/patch-setup.py
+++ sysutils/salt/files/patch-setup.py
@@ -1,20 +1,20 @@
---- setup.py.orig	2011-12-25 21:31:34.267830738 +0000
-+++ setup.py	2011-12-25 21:32:29.325688840 +0000
-@@ -25,7 +25,7 @@
+--- salt-0.9.6/setup.py 2012-01-21 02:18:08.000000000 +0000
++++ setup.py    2012-01-22 19:21:35.158004570 +0000
+@@ -44,7 +44,7 @@
  if 'SYSCONFDIR' in os.environ:
      etc_path = os.environ['SYSCONFDIR']
  else:
 -    etc_path = os.path.join(os.path.dirname(PREFIX), 'etc')
 +    etc_path = os.path.join('%%PREFIX%%', 'etc')
  
- setup(
-       name=NAME,
-@@ -69,10 +69,10 @@
+ libraries = ['ws2_32'] if sys.platform == 'win32' else []
+ 
+@@ -91,10 +91,10 @@
                 'scripts/salt-run',
                 'scripts/salt'],
        data_files=[(os.path.join(etc_path, 'salt'),
--                    ['conf/master',
--                     'conf/minion',
+-                    ['conf/master.template',
+-                     'conf/minion.template',
 +                    ['conf/master.sample',
 +                     'conf/minion.sample',
                      ]),
@@ -23,7 +23,7 @@
                      ['doc/man/salt-master.1',
                       'doc/man/salt-key.1',
                       'doc/man/salt.1',
-@@ -82,14 +82,11 @@
+@@ -104,7 +104,7 @@
                       'doc/man/salt-run.1',
                       'doc/man/salt-minion.1',
                      ]),
@@ -31,11 +31,4 @@
 +                ('man/man7',
                      ['doc/man/salt.7',
                      ]),
-                 (mod_path,
-                     ['salt/modules/cytest.pyx',
-                     ]),
--                (doc_path,
--                    ['LICENSE'
--                    ]),
                   ],
-      )
