2230e5da25852787c75f471c876e01d6c0a5f063
Cy Schubert <cy@FreeBSD.org>
2012-01-26 02:34:46 +0000

    Binary package improvements.
    User option to specify the location of database files. (Can still be overriden
    in make.conf).

--- security/tripwire/files/patch-install-install.sh
+++ security/tripwire/files/patch-install-install.sh
@@ -1,5 +1,18 @@
 --- install/install.sh.orig	2011-11-21 08:06:56.000000000 -0800
-+++ install/install.sh	2012-01-17 22:57:34.124821110 -0800
++++ install/install.sh	2012-01-25 17:05:50.514792833 -0800
+@@ -181,10 +181,10 @@
+ TW_LOCAL_PASS=""
+ 
+ # If clobber==true, overwrite files; if false, do not overwrite files.
+-CLOBBER="false"
++CLOBBER=${TRIPWIRE_CLOBBER:-"false"}
+ 
+ # If prompt==true, ask for confirmation before continuing with install.
+-PROMPT="true"
++PROMPT=${TRIPWIRE_PROMPT:-"true"}
+ 
+ # Guess where the toplevel for the distribution is.
+ # A bad guess is the current directory
 @@ -200,6 +200,7 @@
  ## Parse the command line.
  ##-------------------------------------------------------
@@ -16,6 +29,33 @@
  
  ##-------------------------------------------------------
  ## Print the sign-on banner here before the first
+@@ -333,7 +335,7 @@
+ ## passphrases must be specified on the command line.
+ ##-------------------------------------------------------
+ 
+-if [ "$PROMPT" = "false" ] ; then
++if [ "$DO_NOT_CONFIG" != "yes" -a "$PROMPT" = "false" ] ; then
+ 	if [ -z "$TW_SITE_PASS" ] || [ -z "$TW_LOCAL_PASS" ] ; then
+ 		echo "Error: You must specify site and local passphrase" 1>&2
+ 		echo "if no prompting is chosen." 1>&2
+@@ -525,7 +527,7 @@
+ 			chmod 0750 "$d" > /dev/null
+ 		fi
+ 	else
+-		echo "$d: already exists"
++		test "$PACKAGE_INSTALLER" != "yes" && echo "$d: already exists"
+ 	fi
+ done
+ 
+@@ -541,7 +543,7 @@
+ 			chmod 0755 "$d" > /dev/null
+ 		fi
+ 	else
+-		echo "$d: already exists"
++		test "$PACKAGE_INSTALLER" != "yes" && echo "$d: already exists"
+ 	fi
+ done
+ 
 @@ -550,6 +552,7 @@
  ## Copy all files to the location specified.
  ##=======================================================
@@ -24,6 +64,15 @@
  echo
  echo "----------------------------------------------"
  echo "Copying files..."
+@@ -593,7 +596,7 @@
+ 	f=${TAR_DIR}$d/$ff
+ 	ff=${dd}/$ff
+ 	if [ -s $ff ] && [ "$CLOBBER" = "false" ] ; then
+-		echo "$ff: file already exists"
++		test "$PACKAGE_INSTALLER" != "yes" && echo "$ff: file already exists"
+ 	else
+ 		cp "$f" "$dd"
+                 if [ $? -eq 0 ]; then
 @@ -604,6 +607,12 @@
  		fi
         	fi
