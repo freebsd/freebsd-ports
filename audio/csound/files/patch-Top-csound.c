dfc0ddf778bf90a8d7aa93b3ac582c5479668451
Martin Wilke <miwi@FreeBSD.org>
2012-01-28 17:49:37 +0000

    - Update to 5.15.0
    PR:		164147
    Submitted by:	Ports Fury

--- audio/csound/files/patch-Top-csound.c
+++ audio/csound/files/patch-Top-csound.c
@@ -1,68 +1,65 @@
---- Top/csound.c.old	2011-01-30 01:43:37.000000000 -0600
-+++ Top/csound.c	2011-01-30 01:46:42.000000000 -0600
-@@ -58,7 +58,9 @@
- #include "namedins.h"
- #include "pvfileio.h"
- #include "fftlib.h"
--
+--- Top/csound.c.orig	2011-10-16 22:59:03.000000000 +0900
++++ Top/csound.c	2011-12-07 01:15:33.000000000 +0900
+@@ -67,11 +67,14 @@
+ 
+   /* **** MAJOR PROBLEM: PTHREAD_SPINLOCK_INITIALIZER is not defined in Linux */
+ 
+-#ifdef linux
++#if defined(linux) || defined(__FreeBSD__)
+   #define PTHREAD_SPINLOCK_INITIALIZER 0
+ #endif
+ #endif /* PARCS */
+ 
 +#if defined(__FreeBSD__)
 +#include <sys/sysctl.h>
 +#endif
  #if defined(USE_OPENMP)
  #include <omp.h>
- #endif
-@@ -2641,49 +2643,14 @@
-     static int getTimeResolution(void)
-     {
- #if defined(HAVE_RDTSC)
--        FILE    *f;
--        char    buf[256];
-+     size_t size;
-+     int timeResolutionSeconds;
-+     size = sizeof timeResolutionSeconds;
-+     sysctlbyname("hw.clockrate", &timeResolutionSeconds, &size, NULL, 0);
+ #endif /* USE_OPENMP */
+@@ -383,7 +386,7 @@
+     0.0,            /*  curBeat             */
+     0.0,            /*  curBeat_inc         */
+     0.0,            /*  beatTime            */
+-#if defined(HAVE_PTHREAD_SPIN_LOCK) && defined(PARCS)
++#if defined(__GNUC__) && defined(HAVE_PTHREAD_SPIN_LOCK) && defined(PARCS)
+     PTHREAD_SPINLOCK_INITIALIZER,              /*  spoutlock           */
+     PTHREAD_SPINLOCK_INITIALIZER,              /*  spinlock            */
+ #else
+@@ -2854,7 +2857,7 @@
+   /* ------------------------------------ */
  
--        /* if frequency is not known yet */
--        f = fopen("/proc/cpuinfo", "r");
--        if (UNLIKELY(f == NULL)) {
--            fprintf(stderr, Str("Cannot open /proc/cpuinfo. "
--                    "Support for RDTSC is not available.\n"));
--            return -1;
--        }
--        /* find CPU frequency */
--        while (fgets(buf, 256, f) != NULL) {
--            int     i;
--            char    *s = (char*) buf - 1;
--
--            buf[255] = '\0';          /* safety */
--            if (strlen(buf) < 9) {
--                continue;    /* too short, skip */
--            }
--            while (*++s != '\0')
--                if (isupper(*s)) {
--                    *s = tolower(*s);    /* convert to lower case */
--                }
--            if (strncmp(buf, "cpu mhz", 7) != 0) {
--                continue;    /* check key name */
--            }
--            s = strchr(buf, ':');             /* find frequency value */
--            if (s == NULL) {
--                continue;    /* invalid entry */
--            }
--            do {
--                s++;
--            } while (*s == ' ' || *s == '\t');    /* skip white space */
--            i = sscanf(s, "%lf", &timeResolutionSeconds);
--            if (i < 1 || timeResolutionSeconds < 1.0) {
--                timeResolutionSeconds = -1.0;       /* invalid entry */
--                continue;
--            }
--        }
--        fclose(f);
-         if (UNLIKELY(timeResolutionSeconds <= 0.0)) {
-             fprintf(stderr, Str("No valid CPU frequency entry "
--                    "was found in /proc/cpuinfo.\n"));
-+                    "was found.\n"));
-             return -1;
+ #if defined(HAVE_RDTSC)
+-#if !(defined(LINUX) && defined(__GNUC__) && defined(__i386__))
++#if !(defined(LINUX) && defined(__GNUC__) && defined(__i386__)) || !(defined(__FreeBSD__) && defined(__i386__))
+ #undef HAVE_RDTSC
+ #endif
+ #endif
+@@ -2867,6 +2870,13 @@
+   static int getTimeResolution(void)
+   {
+ #if defined(HAVE_RDTSC)
++#if defined(__FreeBSD__)
++      size_t size;
++      int timeResolutionSeconds;
++      size = sizeof timeResolutionSeconds;
++      sysctlbyname("hw.clockrate", &timeResolutionSeconds, &size, NULL, 0);
++
++#else
+       FILE    *f;
+       char    buf[256];
+ 
+@@ -2923,9 +2933,14 @@
          }
-         /* MHz -> seconds */
+       }
+       fclose(f);
++#endif /* __FreeBSD__ */
+       if (UNLIKELY(timeResolutionSeconds <= 0.0)) {
+         fprintf(stderr, Str("No valid CPU frequency entry "
++#if defined(__FreeBSD__)
++                            "was found.\n"));
++#else
+                             "was found in /proc/cpuinfo.\n"));
++#endif /* __FreeBSD__ */
+         return -1;
+       }
+       /* MHz -> seconds */
