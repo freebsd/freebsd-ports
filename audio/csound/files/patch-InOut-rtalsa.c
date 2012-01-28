2bff09e59b56679d8afe5d02b88f074f801b9a75
miwi <miwi@FreeBSD.org>
2012-01-28 17:49:37 +0000

    - Update to 5.15.0
    PR:		164147
    Submitted by:	Ports Fury

---
+++ audio/csound/files/patch-InOut-rtalsa.c
@@ -0,0 +1,18 @@
+--- InOut/rtalsa.c.orig	2011-12-22 19:41:53.000000000 +0900
++++ InOut/rtalsa.c	2011-12-27 23:53:00.000000000 +0900
+@@ -22,6 +22,7 @@
+     02111-1307 USA
+ */
+ 
++/*
+ #ifndef _ISOC99_SOURCE
+ #define _ISOC99_SOURCE 1
+ #endif
+@@ -35,6 +36,7 @@
+ #ifndef _POSIX_C_SOURCE
+ #define _POSIX_C_SOURCE 1
+ #endif
++*/
+ 
+ #include "csdl.h"
+ 
