0d4dfba0a745f17b77a04da72b82703afd4bc645
jgh <jgh@FreeBSD.org>
2012-01-31 20:19:07 +0000

    Mirror has been shut down. According to mirror => Last update: Sun Jul 31 19:20:47 2011 UTC
    Plenty of cheese in the cheeseshop!
    
    Approved by:	rene (mentor)

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -118,7 +118,6 @@ MASTER_SITE_CHEESESHOP+=	\
 	http://d.pypi.python.org/packages/%SUBDIR%/ \
 	http://e.pypi.python.org/packages/%SUBDIR%/ \
 	http://f.pypi.python.org/packages/%SUBDIR%/ \
-	http://pypi.d9t.de/${DISTNAME:S/${DISTVERSION}//:S/-//}/ \
 	http://pypi.inqbus.de/${DISTNAME:S/${DISTVERSION}//:S/-//}/ \
 	http://pypi.it.uwosh.edu/${DISTNAME:S/${DISTVERSION}//:S/-//}/ \
 	http://pypi.python.jp/${DISTNAME:S/${DISTVERSION}//:S/-//}/
