d26aa5c99432bcb59ff6486bdd7910227f69f41d
rm <rm@FreeBSD.org>
2012-02-01 03:25:24 +0000

    - update to 1.12.0 [1] - fix pkg-plist problem
    PR:		164391
    Submitted by:	Golyashov Sergey <svvord at ulona dot org>
    Approved by:	maintainer

--- net-im/pidgin-sipe/files/patch-src__purple__purple-dnsquery.c
+++ net-im/pidgin-sipe/files/patch-src__purple__purple-dnsquery.c
@@ -1,13 +1,15 @@
---- ./src/purple/purple-dnsquery.c.orig	2010-11-03 05:13:51.000000000 +0100
-+++ ./src/purple/purple-dnsquery.c	2011-06-21 11:23:35.000000000 +0200
-@@ -22,6 +22,10 @@
+--- src/purple/purple-dnsquery.c.orig	2011-08-29 17:05:26.000000000 +1100
++++ src/purple/purple-dnsquery.c	2012-01-23 12:24:33.000000000 +1100
+@@ -29,6 +29,12 @@
+ #endif
  
- #include "glib.h"
- 
+ #include <glib.h>
++#include <sys/socket.h>
++
 +#include "version.h"
 +#if PURPLE_VERSION_CHECK(2,8,0)
 +#include "account.h"
 +#endif
- #include "dnssrv.h"
  
- #include "sipe-backend.h"
+ #include "dnsquery.h"
+ #include "dnssrv.h"
