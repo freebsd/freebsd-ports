d9732fa45317ea9b97878f72614caca4bba152cb
Marcelo Araujo <araujo@FreeBSD.org>
2012-02-06 04:37:39 +0000

    - Update to 4.6.2.
    Submitted by:   Tassilo Philipp <tphilipp@potion-studios.com> (maintainer via email)

--- devel/psptoolchain-gcc-stage1/files/patch-gcc-config-mips-mips.c
+++ devel/psptoolchain-gcc-stage1/files/patch-gcc-config-mips-mips.c
@@ -1,53 +1,94 @@
---- gcc/config/mips/mips.c.orig	2005-12-09 08:15:58.000000000 +0000
-+++ gcc/config/mips/mips.c	2006-05-07 18:37:50.000000000 +0100
-@@ -179,6 +179,12 @@
-   MIPS_VOID_FTYPE_V2HI_V2HI,
-   MIPS_VOID_FTYPE_V4QI_V4QI,
- 
-+  /* For the Sony ALLEGREX.  */
-+  MIPS_SI_FTYPE_QI,
-+  MIPS_SI_FTYPE_HI,
-+  MIPS_VOID_FTYPE_VOID,
-+  MIPS_SI_FTYPE_SF,
-+
-   /* The last type.  */
-   MIPS_MAX_FTYPE_MAX
- };
-@@ -220,6 +226,11 @@
-   /* As above, but the instruction only sets a single $fcc register.  */
+--- ./gcc/config/mips/mips.c.orig	2011-05-29 17:48:14.000000000 +0000
++++ ./gcc/config/mips/mips.c	2012-01-21 14:11:18.000000000 +0000
+@@ -239,7 +239,12 @@
    MIPS_BUILTIN_CMP_SINGLE,
  
-+   /* The builtin corresponds to the ALLEGREX cache instruction.  Operand 0
-+      is the function code (must be less than 32) and operand 1 is the base
-+      address.  */
-+   MIPS_BUILTIN_CACHE,
-+
    /* For generating bposge32 branch instructions in MIPS32 DSP ASE.  */
-   MIPS_BUILTIN_BPOSGE32
+-  MIPS_BUILTIN_BPOSGE32
++  MIPS_BUILTIN_BPOSGE32,
++
++  /* The builtin corresponds to the ALLEGREX cache instruction.  Operand 0
++     is the function code (must be less than 32) and operand 1 is the base
++     address.  */
++  MIPS_BUILTIN_CACHE
  };
-@@ -405,6 +416,7 @@
- static rtx mips_expand_builtin_compare (enum mips_builtin_type,
- 					enum insn_code, enum mips_fp_condition,
- 					rtx, tree);
-+static rtx mips_expand_builtin_cache (enum insn_code icode, rtx, tree);
- static rtx mips_expand_builtin_bposge (enum mips_builtin_type, rtx);
- static void mips_encode_section_info (tree, rtx, int);
  
-@@ -721,6 +733,7 @@
+ /* Invoke MACRO (COND) for each C.cond.fmt condition.  */
+@@ -516,6 +521,10 @@
+    normal branch.  */
+ static bool mips_branch_likely;
  
-   /* MIPS II */
-   { "r6000", PROCESSOR_R6000, 2 },
-+  { "allegrex", PROCESSOR_ALLEGREX, 2 },
++/* Preferred stack boundary for proper stack vars alignment */
++unsigned int mips_preferred_stack_boundary;
++unsigned int mips_preferred_stack_align;
++
+ /* The current instruction-set architecture.  */
+ enum processor mips_arch;
+ const struct mips_cpu_info *mips_arch_info;
+@@ -691,6 +700,7 @@
  
-   /* MIPS III */
-   { "r4000", PROCESSOR_R4000, 3 },
-@@ -10169,6 +10182,67 @@
-   BPOSGE_BUILTIN (32, MASK_DSP)
- };
+   /* MIPS II processors.  */
+   { "r6000", PROCESSOR_R6000, 2, 0 },
++  { "allegrex", PROCESSOR_ALLEGREX, 2, 0 },
+ 
+   /* MIPS III processors.  */
+   { "r4000", PROCESSOR_R4000, 3, 0 },
+@@ -969,6 +979,9 @@
+ 		     1,           /* branch_cost */
+ 		     4            /* memory_latency */
+   },
++  { /* Allegrex */
++    DEFAULT_COSTS
++  },
+   { /* Loongson-2E */
+     DEFAULT_COSTS
+   },
+@@ -12605,6 +12618,7 @@
+ AVAIL_NON_MIPS16 (dspr2_32, !TARGET_64BIT && TARGET_DSPR2)
+ AVAIL_NON_MIPS16 (loongson, TARGET_LOONGSON_VECTORS)
+ AVAIL_NON_MIPS16 (cache, TARGET_CACHE_BUILTIN)
++AVAIL_NON_MIPS16 (allegrex, TARGET_ALLEGREX)
+ 
+ /* Construct a mips_builtin_description from the given arguments.
+ 
+@@ -12701,6 +12715,30 @@
+   MIPS_BUILTIN (bposge, f, "bposge" #VALUE,				\
+ 		MIPS_BUILTIN_BPOSGE ## VALUE, MIPS_SI_FTYPE_VOID, AVAIL)
+ 
++/* Define a MIPS_BUILTIN_DIRECT function for instruction CODE_FOR_allegrex_<INSN>.
++   FUNCTION_TYPE and TARGET_FLAGS are builtin_description fields.  */
++#define DIRECT_ALLEGREX_BUILTIN(INSN, FUNCTION_TYPE, TARGET_FLAGS) \
++  { CODE_FOR_allegrex_ ## INSN, MIPS_FP_COND_f, "__builtin_allegrex_" #INSN,        \
++    MIPS_BUILTIN_DIRECT, FUNCTION_TYPE, mips_builtin_avail_allegrex }
++
++/* Same as the above, but mapped to an instruction that doesn't share the
++   NAME.  NAME is the name of the builtin without the builtin prefix.  */
++#define DIRECT_ALLEGREX_NAMED_BUILTIN(NAME, INSN, FUNCTION_TYPE, TARGET_FLAGS) \
++  { CODE_FOR_ ## INSN, MIPS_FP_COND_f, "__builtin_allegrex_" #NAME,             \
++    MIPS_BUILTIN_DIRECT, FUNCTION_TYPE, mips_builtin_avail_allegrex }
++
++/* Define a MIPS_BUILTIN_DIRECT_NO_TARGET function for instruction
++   CODE_FOR_allegrex_<INSN>.  FUNCTION_TYPE and TARGET_FLAGS are
++   builtin_description fields.  */
++#define DIRECT_ALLEGREX_NO_TARGET_BUILTIN(INSN, FUNCTION_TYPE, TARGET_FLAGS)   \
++  { CODE_FOR_allegrex_ ## INSN, MIPS_FP_COND_f, "__builtin_allegrex_" #INSN,            \
++    MIPS_BUILTIN_DIRECT_NO_TARGET, FUNCTION_TYPE, mips_builtin_avail_allegrex }
++
++/* Define a builtin with a specific function TYPE.  */
++#define SPECIAL_ALLEGREX_BUILTIN(TYPE, INSN, FUNCTION_TYPE, TARGET_FLAGS)  \
++  { CODE_FOR_allegrex_ ## INSN, MIPS_FP_COND_f, "__builtin_allegrex_" #INSN,            \
++    MIPS_BUILTIN_ ## TYPE, FUNCTION_TYPE, mips_builtin_avail_allegrex }
++
+ /* Define a Loongson MIPS_BUILTIN_DIRECT function __builtin_loongson_<FN_NAME>
+    for instruction CODE_FOR_loongson_<INSN>.  FUNCTION_TYPE is a
+    builtin_description field.  */
+@@ -12945,6 +12983,40 @@
+   DIRECT_BUILTIN (dpsqx_s_w_ph, MIPS_DI_FTYPE_DI_V2HI_V2HI, dspr2_32),
+   DIRECT_BUILTIN (dpsqx_sa_w_ph, MIPS_DI_FTYPE_DI_V2HI_V2HI, dspr2_32),
  
 +/* Builtin functions for the Sony ALLEGREX processor.
 +
-+   These have the `__builtin_allgrex_' prefix instead of `__builtin_mips_'
++   These have the `__builtin_allegrex_' prefix instead of `__builtin_mips_'
 +   to maintain compatibility with Sony's ALLEGREX GCC port.
 +
 +   Some of the builtins may seem redundant, but they are the same as the
@@ -55,35 +96,9 @@
 +   trivial builtins to the original instruction instead of creating
 +   duplicate patterns specifically for the ALLEGREX (as Sony does).  */
 +
-+/* Define a MIPS_BUILTIN_DIRECT function for instruction CODE_FOR_allegrex_<INSN>.
-+   FUNCTION_TYPE and TARGET_FLAGS are builtin_description fields.  */
-+#define DIRECT_ALLEGREX_BUILTIN(INSN, FUNCTION_TYPE, TARGET_FLAGS)	\
-+  { CODE_FOR_allegrex_ ## INSN, 0, "__builtin_allegrex_" #INSN,		\
-+    MIPS_BUILTIN_DIRECT, FUNCTION_TYPE, TARGET_FLAGS }
-+
-+/* Same as the above, but mapped to an instruction that doesn't share the
-+   NAME.  NAME is the name of the builtin without the builtin prefix.  */
-+#define DIRECT_ALLEGREX_NAMED_BUILTIN(NAME, INSN, FUNCTION_TYPE, TARGET_FLAGS)	\
-+  { CODE_FOR_ ## INSN, 0, "__builtin_allegrex_" #NAME,				\
-+    MIPS_BUILTIN_DIRECT, FUNCTION_TYPE, TARGET_FLAGS }
-+
-+/* Define a MIPS_BUILTIN_DIRECT_NO_TARGET function for instruction
-+   CODE_FOR_allegrex_<INSN>.  FUNCTION_TYPE and TARGET_FLAGS are
-+   builtin_description fields.  */
-+#define DIRECT_ALLEGREX_NO_TARGET_BUILTIN(INSN, FUNCTION_TYPE, TARGET_FLAGS)	\
-+  { CODE_FOR_allegrex_ ## INSN, 0, "__builtin_allegrex_" #INSN,			\
-+    MIPS_BUILTIN_DIRECT_NO_TARGET, FUNCTION_TYPE, TARGET_FLAGS }
-+
-+/* Define a builtin with a specific function TYPE.  */
-+#define SPECIAL_ALLEGREX_BUILTIN(TYPE, INSN, FUNCTION_TYPE, TARGET_FLAGS)	\
-+  { CODE_FOR_allegrex_ ## INSN, 0, "__builtin_allegrex_" #INSN,			\
-+    MIPS_BUILTIN_ ## TYPE, FUNCTION_TYPE, TARGET_FLAGS }
-+
-+static const struct builtin_description allegrex_bdesc[] =
-+{
 +  DIRECT_ALLEGREX_BUILTIN(bitrev, MIPS_SI_FTYPE_SI, 0),
 +  DIRECT_ALLEGREX_BUILTIN(wsbh, MIPS_SI_FTYPE_SI, 0),
-+  DIRECT_ALLEGREX_BUILTIN(wsbw, MIPS_SI_FTYPE_SI, 0),
++  DIRECT_ALLEGREX_NAMED_BUILTIN(wsbw, bswapsi2, MIPS_SI_FTYPE_SI, 0),
 +  DIRECT_ALLEGREX_NAMED_BUILTIN(clz, clzsi2, MIPS_SI_FTYPE_SI, 0),
 +  DIRECT_ALLEGREX_BUILTIN(clo, MIPS_SI_FTYPE_SI, 0),
 +  DIRECT_ALLEGREX_NAMED_BUILTIN(ctz, ctzsi2, MIPS_SI_FTYPE_SI, 0),
@@ -103,121 +118,89 @@
 +  DIRECT_ALLEGREX_BUILTIN(ceil_w_s, MIPS_SI_FTYPE_SF, 0),
 +  DIRECT_ALLEGREX_BUILTIN(floor_w_s, MIPS_SI_FTYPE_SF, 0),
 +  DIRECT_ALLEGREX_BUILTIN(round_w_s, MIPS_SI_FTYPE_SF, 0),
-+  DIRECT_ALLEGREX_NAMED_BUILTIN(trunc_w_s, fix_truncsfsi2_insn, MIPS_SI_FTYPE_SF, 0)
-+};
++  DIRECT_ALLEGREX_NAMED_BUILTIN(trunc_w_s, fix_truncsfsi2_insn, MIPS_SI_FTYPE_SF, 0),
 +
- /* This helps provide a mapping from builtin function codes to bdesc
-    arrays.  */
+   /* Builtin functions for ST Microelectronics Loongson-2E/2F cores.  */
+   LOONGSON_BUILTIN (packsswh, MIPS_V4HI_FTYPE_V2SI_V2SI),
+   LOONGSON_BUILTIN (packsshb, MIPS_V8QI_FTYPE_V4HI_V4HI),
+@@ -13096,6 +13168,8 @@
+ /* Standard mode-based argument types.  */
+ #define MIPS_ATYPE_UQI unsigned_intQI_type_node
+ #define MIPS_ATYPE_SI intSI_type_node
++#define MIPS_ATYPE_HI intHI_type_node
++#define MIPS_ATYPE_QI intQI_type_node
+ #define MIPS_ATYPE_USI unsigned_intSI_type_node
+ #define MIPS_ATYPE_DI intDI_type_node
+ #define MIPS_ATYPE_UDI unsigned_intDI_type_node
+@@ -13270,6 +13344,9 @@
  
-@@ -10189,6 +10263,7 @@
- {
-   { mips_bdesc, ARRAY_SIZE (mips_bdesc), PROCESSOR_MAX },
-   { sb1_bdesc, ARRAY_SIZE (sb1_bdesc), PROCESSOR_SB1 },
-+  { allegrex_bdesc, ARRAY_SIZE (allegrex_bdesc), PROCESSOR_ALLEGREX },
-   { dsp_bdesc, ARRAY_SIZE (dsp_bdesc), PROCESSOR_MAX }
- };
- 
-@@ -10292,6 +10367,9 @@
-     case MIPS_BUILTIN_BPOSGE32:
-       return mips_expand_builtin_bposge (type, target);
- 
-+    case MIPS_BUILTIN_CACHE:
-+      return mips_expand_builtin_cache (icode, target, arglist);
-+
-     default:
-       return 0;
-     }
-@@ -10310,8 +10388,8 @@
-   tree V4QI_type_node;
-   unsigned int offset;
- 
--  /* We have only builtins for -mpaired-single, -mips3d and -mdsp.  */
--  if (!TARGET_PAIRED_SINGLE_FLOAT && !TARGET_DSP)
-+  /* We have only builtins for -mpaired-single, -mips3d and -mdsp and the Sony ALLEGREX.  */
-+  if (!TARGET_PAIRED_SINGLE_FLOAT && !TARGET_DSP && !TARGET_ALLEGREX)
-     return;
- 
-   if (TARGET_PAIRED_SINGLE_FLOAT)
-@@ -10376,6 +10454,44 @@
- 				    double_type_node, double_type_node, NULL_TREE);
-     }
- 
-+   if (TARGET_ALLEGREX)
-+     {
-+       types[MIPS_SI_FTYPE_QI]
-+ 	= build_function_type_list (intSI_type_node,
-+ 				    intQI_type_node,
-+ 				    NULL_TREE);
-+ 
-+       types[MIPS_SI_FTYPE_HI]
-+ 	= build_function_type_list (intSI_type_node,
-+ 				    intHI_type_node,
-+ 				    NULL_TREE);
-+ 
-+       types[MIPS_SI_FTYPE_SI]
-+ 	= build_function_type_list (intSI_type_node,
-+ 				    intSI_type_node,
-+ 				    NULL_TREE);
-+ 
-+       types[MIPS_SI_FTYPE_SI_SI]
-+ 	= build_function_type_list (intSI_type_node,
-+ 				    intSI_type_node, intSI_type_node,
-+ 				    NULL_TREE);
-+ 
-+       types[MIPS_VOID_FTYPE_VOID]
-+ 	= build_function_type_list (void_type_node, void_type_node, NULL_TREE);
-+ 
-+       types[MIPS_VOID_FTYPE_SI_SI]
-+ 	= build_function_type_list (void_type_node,
-+ 				    intSI_type_node, intSI_type_node, NULL_TREE);
-+ 
-+       types[MIPS_SF_FTYPE_SF]
-+ 	= build_function_type_list (float_type_node,
-+ 				    float_type_node, NULL_TREE);
-+ 
-+       types[MIPS_SI_FTYPE_SF]
-+ 	= build_function_type_list (intSI_type_node,
-+ 				    float_type_node, NULL_TREE);
-+     }
-+
-   if (TARGET_DSP)
+   switch (opno)
      {
-       V2HI_type_node = build_vector_type_for_mode (intHI_type_node, V2HImode);
-@@ -10557,6 +10673,10 @@
- 
-   switch (i)
-     {
-+     case 0:
-+       emit_insn (GEN_FCN (icode) (0));
-+       break;
-+
++    case 0:
++      emit_insn (GEN_FCN (icode) (0));
++      break;
      case 2:
        emit_insn (GEN_FCN (icode) (ops[0], ops[1]));
        break;
-@@ -10767,4 +10887,26 @@
-     }
+@@ -13439,6 +13516,28 @@
+ 				       const1_rtx, const0_rtx);
  }
- 
+ 
 +/* Expand a __builtin_allegrex_cache() function.  Make sure the passed
 +   cache function code is less than 32.  */
 +
 +static rtx
-+mips_expand_builtin_cache (enum insn_code icode, rtx target, tree arglist)
++mips_expand_builtin_cache (enum insn_code icode, rtx target, tree exp)
 +{
 +  rtx op0, op1;
 +
-+  op0 = mips_prepare_builtin_arg (icode, 0, &arglist);
-+  op1 = mips_prepare_builtin_arg (icode, 1, &arglist);
++  op0 = mips_prepare_builtin_arg (icode, 0, exp, 0);
++  op1 = mips_prepare_builtin_arg (icode, 1, exp, 1);
 +
 +  if (GET_CODE (op0) == CONST_INT)
 +    if (INTVAL (op0) < 0 || INTVAL (op0) > 0x1f)
 +      {
-+	error ("invalid function code '%d'", INTVAL (op0));
-+	return const0_rtx;
++   error ("invalid function code '%d'", INTVAL (op0));
++   return const0_rtx;
 +      }
 +
 +  emit_insn (GEN_FCN (icode) (op0, op1));
 +  return target;
 +}
-+
- #include "gt-mips.h"
++
+ /* Implement TARGET_EXPAND_BUILTIN.  */
+ 
+ static rtx
+@@ -13484,6 +13583,9 @@
+ 
+     case MIPS_BUILTIN_BPOSGE32:
+       return mips_expand_builtin_bposge (d->builtin_type, target);
++
++    case MIPS_BUILTIN_CACHE:
++      return mips_expand_builtin_cache (d->icode, target, exp);
+     }
+   gcc_unreachable ();
+ }
+@@ -15918,6 +16020,22 @@
+      Do all CPP-sensitive stuff in non-MIPS16 mode; we'll switch to
+      MIPS16 mode afterwards if need be.  */
+   mips_set_mips16_mode (false);
++
++  /* Validate -mpreferred-stack-boundary= value, or provide default.
++     The default of 128-bit is for newABI else 64-bit.  */
++  mips_preferred_stack_boundary = (TARGET_NEWABI ? 128 : 64);
++  mips_preferred_stack_align = (TARGET_NEWABI ? 16 : 8);
++  if (mips_preferred_stack_boundary_string)
++    {
++      i = atoi (mips_preferred_stack_boundary_string);
++      if (i < 2 || i > 12)
++       error ("-mpreferred-stack-boundary=%d is not between 2 and 12", i);
++      else
++        {
++          mips_preferred_stack_align = (1 << i);
++          mips_preferred_stack_boundary = mips_preferred_stack_align * 8;
++        }
++    }
+ }
+ 
+ /* Implement TARGET_OPTION_OPTIMIZATION_TABLE.  */
