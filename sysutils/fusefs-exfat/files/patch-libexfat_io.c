bbc77398c9fcbf98cd99c22b4db637e297a6d0d7
scheidell <scheidell@FreeBSD.org>
2012-02-09 17:21:59 +0000

    - Fixed time/date handling, fix taken from upstream - Device block size is now requested from the device - Added optional ublio support, enabled by default - Bump PORTREVISION
    PR:		ports/164790
    Submitted by:	Alex Samorukov<samm@os2.kiev.ua>  (maintainer)
    Approved by:	gabor (mentor)

--- sysutils/fusefs-exfat/files/patch-libexfat_io.c
+++ sysutils/fusefs-exfat/files/patch-libexfat_io.c
@@ -1,6 +1,6 @@
---- libexfat/io.c	2012-01-25 13:07:21.467957180 +0000
-+++ libexfat/io.c	2012-01-25 13:26:10.599952890 +0000
-@@ -23,6 +23,7 @@
+--- libexfat/io.c.orig	2012-01-14 10:24:11.000000000 +0100
++++ libexfat/io.c	2012-02-05 11:37:13.872786157 +0100
+@@ -23,9 +23,29 @@
  #include <sys/types.h>
  #include <sys/uio.h>
  #include <sys/stat.h>
@@ -8,7 +8,29 @@
  #include <fcntl.h>
  #define __USE_UNIX98 /* for pread() in Linux */
  #include <unistd.h>
-@@ -49,26 +50,177 @@
++#ifdef __FreeBSD__
++#ifdef USE_UBLIO
++#include <sys/uio.h>
++#include "ublio.h"
++#define UBLIO_DEFAULT_BLOCKSIZE	262144
++#define UBLIO_DEFAULT_ITEMS	64
++#define UBLIO_DEFAULT_GRACE	32
++#define UBLIO_DEFAULT_SYNC_IO	0
++#define UBLIO_USE_API		1;
++struct unix_filehandle {
++	int fd;
++	ublio_filehandle_t ublio_fh;
++};
++
++struct unix_filehandle *ufh;
++#endif
++#include <sys/disk.h>
++static long devbsize; // block size for the gived device
++#endif
+ 
+ #if _FILE_OFFSET_BITS != 64
+ 	#error You should define _FILE_OFFSET_BITS=64
+@@ -49,26 +69,219 @@
  		exfat_error("failed to fstat `%s'", spec);
  		return -1;
  	}
@@ -21,20 +43,54 @@
  				spec);
  		return -1;
  	}
++#ifdef __FreeBSD__
++	devbsize = 512;
++	if (S_ISCHR(stbuf.st_mode) && ioctl(fd, DIOCGSECTORSIZE, &devbsize) < 0) {
++		exfat_error("Failed to ioctl(DIOCGSECTORSIZE) '%s'", spec);
++		return -1;
++	}
++#if USE_UBLIO
++	ufh = malloc(sizeof(*ufh));
++	if (!ufh)
++		return -1;
++
++	struct ublio_param up;
++	ufh->ublio_fh = NULL;
++	ufh->fd = fd;
++
++	up.up_blocksize = UBLIO_DEFAULT_BLOCKSIZE;
++	up.up_items = UBLIO_DEFAULT_ITEMS;
++	up.up_grace = UBLIO_DEFAULT_GRACE;
++	up.up_sync_io = UBLIO_DEFAULT_SYNC_IO;
++	up.up_priv	= &ufh->fd;
++	up.up_pread	= NULL;
++	up.up_preadv	= NULL;
++	up.up_pwrite	= NULL;
++	up.up_pwritev	= NULL; 
++	ufh->ublio_fh = ublio_open(&up);
++	if (!ufh->ublio_fh) {
++		close(fd);
++		exfat_error("ublio_open failed for %s", spec);
++		return -1;
++	}
++#endif
++
++#endif
  	return fd;
  }
  
 +#define RAW_IO_MAX_SIZE (128 * 1024 * 1024)
-+#define DEVBSIZE 512 // better get from the device, but 512 should work as well
++// #define DEVBSIZE 4096 // better get from the device, but 512 should work as well
++
 +#define RAW_IO_ALIGNED(offset, count)			\
-+	(DEVBSIZE == 0 ||			\
-+	 ((offset) % DEVBSIZE == 0 &&	\
-+	  (count) % DEVBSIZE == 0))
++	(devbsize == 0 ||			\
++	 ((offset) % devbsize == 0 &&	\
++	  (count) % devbsize == 0))
 +#define RAW_IO_ALIGN(offset)				\
-+	((offset) / DEVBSIZE * DEVBSIZE)
++	((offset) / devbsize * devbsize)
 +#define RAW_IO_MAX_SIZE (128 * 1024 * 1024)
 +
-+
++#ifndef USE_UBLIO
 +static int64_t aligned_pread(int fd, void *buf, int64_t count, int64_t offset)
 +{
 +	int64_t start, start_aligned;
@@ -64,7 +120,7 @@
 +	start_aligned = RAW_IO_ALIGN(start);
 +	end = start + count;
 +	end_aligned = RAW_IO_ALIGN(end) +
-+	    (RAW_IO_ALIGNED(end, 0) ? 0 : DEVBSIZE);
++	    (RAW_IO_ALIGNED(end, 0) ? 0 : devbsize);
 +	count_aligned = end_aligned - start_aligned;
 +/*	exfat_debug(
 +	    "%s: count = 0x%llx/0x%x, start = 0x%llx/0x%llx, end = 0x%llx/0x%llx\n",
@@ -129,12 +185,12 @@
 +	start_aligned = RAW_IO_ALIGN(start);
 +	end = start + count;
 +	end_aligned = RAW_IO_ALIGN(end) +
-+	    (RAW_IO_ALIGNED(end, 0) ? 0 : DEVBSIZE);
++	    (RAW_IO_ALIGNED(end, 0) ? 0 : devbsize);
 +	count_aligned = end_aligned - start_aligned;
-+	exfat_debug(
++	/* exfat_debug(
 +	    "device:%d: count = 0x%llx/0x%x, start = 0x%llx/0x%llx, end = 0x%llx/0x%llx\n",
 +	    fd, count, count_aligned,
-+	    start, start_aligned, end, end_aligned);
++	    start, start_aligned, end, end_aligned); */
 +
 +	/* allocate buffer */
 +	buf_aligned = malloc(count_aligned);
@@ -144,15 +200,15 @@
 +	}
 +
 +	/* read aligned lead-in */
-+	if (pread(fd, buf_aligned, DEVBSIZE, start_aligned) != DEVBSIZE) {
++	if (pread(fd, buf_aligned, devbsize, start_aligned) != devbsize) {
 +		exfat_debug("read lead-in failed\n");
 +		free(buf_aligned);
 +		return -1;
 +	}
 +
 +	/* read aligned lead-out */
-+	if (end != end_aligned && count_aligned > DEVBSIZE) {
-+		if (pread(fd, buf_aligned + count_aligned - DEVBSIZE, DEVBSIZE, end_aligned - DEVBSIZE) != DEVBSIZE) {
++	if (end != end_aligned && count_aligned > devbsize) {
++		if (pread(fd, buf_aligned + count_aligned - devbsize, devbsize, end_aligned - devbsize) != devbsize) {
 +			exfat_error("read lead-out failed\n");
 +			free(buf_aligned);
 +			return -1;
@@ -173,12 +229,16 @@
 +		nw = count;
 +	return nw;
 +}
-+
++#endif
 +
  void exfat_read_raw(void* buffer, size_t size, off_t offset, int fd)
  {
 -	if (pread(fd, buffer, size, offset) != size)
++#ifdef USE_UBLIO
++	if (ublio_pread(ufh->ublio_fh, buffer, size, offset) != size)
++#else
 +	if (aligned_pread(fd, buffer, size, offset) != size)
++#endif
  		exfat_bug("failed to read %zu bytes from file at %"PRIu64, size,
  				(uint64_t) offset);
  }
@@ -186,7 +246,26 @@
  void exfat_write_raw(const void* buffer, size_t size, off_t offset, int fd)
  {
 -	if (pwrite(fd, buffer, size, offset) != size)
++#ifdef USE_UBLIO
++	if (ublio_pwrite(ufh->ublio_fh, buffer, size, offset) != size)
++#else
 +	if (aligned_pwrite(fd, buffer, size, offset) != size)
++#endif
  		exfat_bug("failed to write %zu bytes to file at %"PRIu64, size,
  				(uint64_t) offset);
  }
+@@ -155,3 +368,14 @@
+ 	exfat_update_mtime(node);
+ 	return size - remainder;
+ }
++
++int exfat_close(int fd) {
++#ifdef USE_UBLIO
++	ublio_fsync(ufh->ublio_fh);
++	ublio_close(ufh->ublio_fh);
++	
++#endif
++	if (close(fd) < 0)	/* close descriptor immediately after fsync */
++		return -1;
++	return 0;
++}
