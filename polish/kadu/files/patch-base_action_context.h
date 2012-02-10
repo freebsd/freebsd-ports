771808ca3acc331b529b86e29497edd65dd670e0
pawel <pawel@FreeBSD.org>
2012-02-10 20:57:43 +0000

    - Update to version 0.11.0 - Add more master sites - Use bsd.port.options.mk - Sort plist

---
+++ polish/kadu/files/patch-base_action_context.h
@@ -0,0 +1,8 @@
+--- kadu-core/gui/actions/base-action-context.h.orig	2012-02-09 21:46:02.000000000 +0100
++++ kadu-core/gui/actions/base-action-context.h	2012-02-09 21:46:15.000000000 +0100
+@@ -1,4 +1,4 @@
+-﻿/*
++/*
+  * %kadu copyright begin%
+  * Copyright 2011 Rafał Malinowski (rafal.przemyslaw.malinowski@gmail.com)
+  * %kadu copyright end%
