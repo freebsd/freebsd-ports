4f77ff64301f5e7285313fe2a076be19a3fa7a40
Philip M. Gollucci <pgollucci@FreeBSD.org>
2012-02-11 22:54:41 +0000

    - Fix pound.h:341: error: expected specifier-qualifier-list before 'LHASH'   by applying http://www.apsis.ch/pound/pound_list/archive/2010/2010-02/1266065082000/1266065107756.octet-stream/download
    PR:             ports/163838
    Submitted by:   Aaron D. Gifford <agifford@infowest.com>
    Approved by:    maintainer timeout (anders ; 34 days)

--- www/pound/files/patch-pound.h
+++ www/pound/files/patch-pound.h
@@ -1,12 +1,25 @@
---- pound.h.orig	2008-02-11 12:53:52.000000000 +0100
-+++ pound.h	2008-03-18 21:58:27.000000000 +0100
-@@ -132,7 +132,8 @@
- #include    <openssl/ssl.h>
- #include    <openssl/lhash.h>
- #include    <openssl/err.h>
--#if OPENSSL_VERSION_NUMBER >= 0x00907000L
-+#include    <openssl/opensslconf.h>
-+#if OPENSSL_VERSION_NUMBER > 0x0090705FL
- #ifndef OPENSSL_THREADS
- #error  "Pound requires OpenSSL with thread support"
- #endif
+--- ./pound.h.orig	2012-02-11 22:51:17.579834493 +0000
++++ ./pound.h	2012-02-11 22:51:28.220833901 +0000
+@@ -323,6 +323,10 @@
+ /* maximal session key size */
+ #define KEY_SIZE    127
+ 
++#if OPENSSL_VERSION_NUMBER >= 0x10000000L
++DECLARE_LHASH_OF(TABNODE);
++#endif
++
+ /* service definition */
+ typedef struct _service {
+     char                name[KEY_SIZE + 1]; /* symbolic name */
+@@ -338,7 +342,11 @@
+     int                 sess_ttl;   /* session time-to-live */
+     regex_t             sess_start; /* pattern to identify the session data */
+     regex_t             sess_pat;   /* pattern to match the session data */
++#if OPENSSL_VERSION_NUMBER >= 0x10000000L
++    LHASH_OF(TABNODE)   *sessions;  /* currently active sessions */
++#else
+     LHASH               *sessions;  /* currently active sessions */
++#endif
+     int                 dynscale;   /* true if the back-ends should be dynamically rescaled */
+     int                 disabled;   /* true if the service is disabled */
+     struct _service     *next;
