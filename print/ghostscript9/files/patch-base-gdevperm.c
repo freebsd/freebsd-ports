7ef6ca440998a75973c31ff5cc0c709a89f2dd2a
Hiroki Sato <hrs@FreeBSD.org>
2012-02-12 18:10:49 +0000

    Update to 9.05.  Changes include:
    - Ghostscript's PDF interpreter is now able to make use of the
      DroidSansFallback TrueType font to automatically substitute for
      missing CIDFonts. Whilst it is always best to ensure the
      original CIDFont is available for the best and most accurate
      output, the ability to make an automatic substitution will be
      valuable for those merely viewing or proofing such files.
    
    - This release includes support for a proofing ICC profile. The
      command option is specified using -sProofProfile=filename. With
      this option, the color output will emulate what would be
      obtained had the source file been rendered on a device defined
      by the proofing profile. (See GS9_Color_Management.pdf for
      details.)
    
    - This release includes support for a device link ICC
      profile. The command option is specified using
      -sDeviceLinkProfile=filename. With this option, the device link
      profile is added to the end of the link transform from source
      to destination. In this case, one can include a command line
      option like "-sDevice=tiff32nc -sOutputICCProfile=srgb.icc
      -sDeviceLinkProfile=linkRGBtoCMYK.icc" and source colors will
      be mapped through sRGB and through the device link profile to
      CMYK values for the device.
    
    - Ghostscript now supports "unmanaged color transformations" for
      source DeviceXXX colors (in other words, they use a simple
      conversion, rather than the ICC profile based color
      workflow). This is beneficial in uses where performance takes
      precedence over ultimate color fidelity (the command line
      parameter -dUseFastColor enables this).
    
    - The font set distributed with Ghostscript has been changed to
      the standard 35 Postscript-compatible fonts distributed by URW.
    
    - Ghostscript now includes a simple ink-coverage device,
      contributed by Sebastian Kapfer (inkconv).
    
    - The TIFF, JPEG and PNG output devices now support embedding of
      the device ICC profile in the output file.
    
    - Ghostscript now ships modified OpenJPEG sources for JPEG2000
      decoding (replacing JasPer). Performance, reliability and
      memory use whilst decoding JPX streams are all improved
    
    - jbig2dec now has simple halftone region support
    
    - The ps2write device has had a large number of output quality
      and stability improvements.

--- print/ghostscript9/files/patch-base-gdevperm.c
+++ print/ghostscript9/files/patch-base-gdevperm.c
@@ -1,5 +1,5 @@
---- base/gdevperm.c.orig	2011-12-06 15:37:48.000000000 +0900
-+++ base/gdevperm.c	2011-12-06 15:45:20.000000000 +0900
+--- base/gdevperm.c.orig	2012-02-08 17:48:48.000000000 +0900
++++ base/gdevperm.c	2012-02-13 00:44:23.000000000 +0900
 @@ -285,7 +285,7 @@
          (strncmp((const char *)name, (const char *)str, name_size) == 0))
  
@@ -9,3 +9,11 @@
                                          int name_size, int component_type)
  {
      const gx_device_perm_t * const dev = (const gx_device_perm_t *)pdev;
+@@ -332,7 +332,6 @@
+ perm_decode_color(gx_device *dev, gx_color_index color, gx_color_value *out)
+ {
+     int bpc = 8;
+-    int drop = sizeof(gx_color_value) * 8 - bpc;
+     int mask = (1 << bpc) - 1;
+     int i = 0;
+     int ncomp = dev->color_info.num_components;
