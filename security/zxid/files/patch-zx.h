a9a81534a4be1b90059ce88fbac3f6f46c6c0e3d
sunpoet <sunpoet@FreeBSD.org>
2012-02-12 14:06:02 +0000

    - Update to 1.05
    PR:		ports/163738
    Submitted by:	sunpoet (myself)
    Approved by:	clsung (maintainer, via IRC)

---
+++ security/zxid/files/patch-zx.h
@@ -0,0 +1,10 @@
+--- zx.h.orig	2011-07-25 03:56:45.000000000 +0800
++++ zx.h	2011-12-29 17:00:27.394555276 +0800
+@@ -25,6 +25,7 @@
+ #include <memory.h>
+ #include <string.h>
+ #include <stdarg.h>
++#include <limits.h>
+ 
+ #ifdef USE_OPENSSL
+ #include <openssl/x509.h>
