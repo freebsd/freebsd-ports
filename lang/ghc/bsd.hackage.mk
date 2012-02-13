0bdba6b9cb05de064a1de90f5f84d60a256f30e2
pgj <pgj@FreeBSD.org>
2012-02-13 02:42:56 +0000

    Generic cryptography public keys algorithm types.
    WWW:	http://github.com/vincenthz/hs-crypto-pubkey-types
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -56,6 +56,7 @@ cprng-aes_port=			security/hs-cprng-aes
 criterion_port=			benchmarks/hs-criterion
 Crypto_port=			security/hs-Crypto
 crypto-api_port=		security/hs-crypto-api
+crypto-pubkey-types_port=	security/hs-crypto-pubkey-types
 csv_port=			textproc/hs-csv
 curl_port=			ftp/hs-curl
 darcs_port=			devel/hs-darcs		# executable
