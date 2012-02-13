aff31ba4a5daf317f3b30556d0096237dff6c37c
pgj <pgj@FreeBSD.org>
2012-02-13 05:20:41 +0000

    - Update to 0.5.1
    Obtained from:	FreeBSD Haskell

---
+++ science/hs-bio/files/patch-examples__RSelectPE.hs
@@ -0,0 +1,8 @@
+--- ./examples/RSelectPE.hs.orig	2011-12-15 12:56:28.000000000 +0100
++++ ./examples/RSelectPE.hs	2011-12-17 11:46:58.000000000 +0100
+@@ -1,5 +1,3 @@
+-{- | Randomly select paired end (illumina) reads -}
+-
+ import System.Environment (getArgs)
+ import Data.List (isSuffixOf)
+ import System.Random
