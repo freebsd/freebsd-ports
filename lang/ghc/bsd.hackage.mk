10756bd8b83f10cbad21542f9fc96d09c92ecf20
pgj <pgj@FreeBSD.org>
2012-02-13 02:13:47 +0000

    A configuration management library for programs and daemons.
    Features include:
    
     * Automatic, dynamic reloading in response to modifications to configuration
       files.
    
     * A simple, but flexible, configuration language, supporting several of
       the most commonly needed types of data, along with interpolation of
       strings from the configuration or the system environment (e.g.
       $(HOME)).
    
     * Subscription-based notification of changes to configuration properties.
    
     * An import directive allows the configuration of a complex application
       to be split across several smaller files, or common configuration data
       to be shared across several applications.
    
    WWW:	http://github.com/mailrank/configurator
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -47,6 +47,7 @@ citeproc-hs_port=		textproc/hs-citeproc-hs
 cmdargs_port=			devel/hs-cmdargs
 comonad_port=			math/hs-comonad
 comonad-transformers_port=	math/hs-comonad-transformers
+configurator_port=		devel/hs-configurator
 contravariant_port=		math/hs-contravariant
 convertible_port=		devel/hs-convertible
 cpphs_port=			devel/hs-cpphs		# executable
