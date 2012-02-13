665fa4c3fae5c9c2a115ada3e638d7185a596a76
wen <wen@FreeBSD.org>
2012-02-13 06:40:31 +0000

    - Unbreak on FreeBSD-9 and above
    PR:		ports/164977
    Submitted by:	Daniel Austin <freebsd-ports@dan.me.uk>

---
+++ net/poptop/files/patch-plugins_pptpd-logwtmp.c
@@ -0,0 +1,10 @@
+--- plugins/pptpd-logwtmp.c.orig	2012-02-13 10:53:17.000000000 +0800
++++ plugins/pptpd-logwtmp.c	2012-02-13 10:53:26.000000000 +0800
+@@ -10,7 +10,6 @@
+  *  2 of the License, or (at your option) any later version.
+  */
+ #include <unistd.h>
+-#include <utmp.h>
+ #include <string.h>
+ #include "pppd.h"
+ 
