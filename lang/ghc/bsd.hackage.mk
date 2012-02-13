05eb37c0da7a36f198f709250a998ba4bb658b14
Gabor Pali <pgj@FreeBSD.org>
2012-02-13 02:24:19 +0000

    Provides newtype wrappers for phantom types to avoid unsafely passing dummy arguments.
    WWW:	http://github.com/ekmett/tagged
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -184,6 +184,7 @@ Stream_port=			devel/hs-Stream
 stringsearch_port=		textproc/hs-stringsearch
 svgcairo_port=			graphics/hs-svgcairo
 syb_port=			devel/hs-syb
+tagged_port=			devel/hs-tagged
 tagsoup_port=			textproc/hs-tagsoup	# executable
 tar_port=			archivers/hs-tar
 Tensor_port=			devel/hs-Tensor
