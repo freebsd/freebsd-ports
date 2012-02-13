1e265d2ad4798836c3e94f61e8b06f8799658195
Ryan Steinmetz <zi@FreeBSD.org>
2012-02-13 16:16:43 +0000

    - Merge in fix for upstream Bug ID 3480541 - Do not bump PORTREVISION as this is a minor edit and I plan to include additional patches in the near future
    Requested by:	Alexey Kouznetsov <alexey@kouznetsov.com>

--- net-mgmt/net-snmp/files/patch-agent__mibgroup__ucd-snmp__disk_hw.c
+++ net-mgmt/net-snmp/files/patch-agent__mibgroup__ucd-snmp__disk_hw.c
@@ -1,5 +1,5 @@
---- ./agent/mibgroup/ucd-snmp/disk_hw.c.orig	2012-01-27 09:51:02.000000000 -0500
-+++ ./agent/mibgroup/ucd-snmp/disk_hw.c	2012-01-27 09:51:54.000000000 -0500
+--- ./agent/mibgroup/ucd-snmp/disk_hw.c.orig	2011-09-28 00:53:47.000000000 -0400
++++ ./agent/mibgroup/ucd-snmp/disk_hw.c	2012-02-13 11:07:08.000000000 -0500
 @@ -137,6 +137,7 @@
    if (disks) {
       free( disks );
@@ -8,3 +8,12 @@
    }
    allDisksIncluded = 0;
  }
+@@ -321,7 +322,7 @@
+ 
+     switch (vp->magic) {
+     case MIBINDEX:
+-        long_ret = disknum;
++        long_ret = disknum + 1;
+         return ((u_char *) (&long_ret));
+     case ERRORNAME:            /* DISKPATH */
+         *var_len = strlen(entry->path);
