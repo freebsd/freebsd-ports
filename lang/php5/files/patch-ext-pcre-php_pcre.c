c0984d7212423fec61910cec885222df29522d13
mm <mm@FreeBSD.org>
2012-02-14 12:44:23 +0000

    Bump pcre library dependency due to 8.30 update Add (vendor) patch for deprecated pcre_info()

--- lang/php5/files/patch-ext-pcre-php_pcre.c
+++ lang/php5/files/patch-ext-pcre-php_pcre.c
@@ -1,16 +1,5 @@
---- php_pcre.c.orig	2009-03-06 10:58:43.000000000 +0100
-+++ php_pcre.c	2009-03-06 10:58:56.000000000 +0100
-@@ -18,6 +18,10 @@
- 
- /* $Id: php_pcre.c,v 1.168.2.9.2.30 2009/01/13 19:23:31 andrei Exp $ */
- 
-+#ifdef HAVE_CONFIG_H
-+#include "config.h"
-+#endif
-+
- #include "php.h"
- #include "php_ini.h"
- #include "php_globals.h"
+--- ext/pcre/php_pcre.c	2012/01/01 13:15:04	321634
++++ ext/pcre/php_pcre.c	2012/02/06 18:18:53	323097
 @@ -241,6 +241,7 @@
  	char				*pattern;
  	int					 do_study = 0;
