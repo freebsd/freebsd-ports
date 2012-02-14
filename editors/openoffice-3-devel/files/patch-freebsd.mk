c3a791762258eb325dfb6129fcbe5d52ec00107c
Maho Nakata <maho@FreeBSD.org>
2012-02-14 11:15:29 +0000

    1. update to svn 1243485. 2. enable --enable-category-b . 3. USE_GCC=4.6+ to avoid compilation errors.

--- editors/openoffice-3-devel/files/patch-freebsd.mk
+++ editors/openoffice-3-devel/files/patch-freebsd.mk
@@ -1,5 +1,13 @@
 --- solenv/gbuild/platform/freebsd.mk~	2011-12-24 18:45:27.000000000 +0900
 +++ solenv/gbuild/platform/freebsd.mk	2011-12-25 11:01:37.000000000 +0900
+@@ -120,6 +120,7 @@
+ 	-Wl,-z,combreloc \
+ 	-Wl,-z,defs \
+ 	$(subst -L../lib , ,$(SOLARLIB)) \
++	%%RPATH%% \
+ 
+ ifeq ($(HAVE_LD_HASH_STYLE),TRUE)
+ gb_LinkTarget_LDFLAGS += \
 @@ -302,7 +302,7 @@
  	$(foreach lib,$(gb_Library_UNOVERLIBS),$(lib):URELIB) \
  
