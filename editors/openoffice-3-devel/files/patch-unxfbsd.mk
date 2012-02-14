798755fe7b6e9cebd90986640ee0b26d4be54c24
maho <maho@FreeBSD.org>
2012-02-14 11:15:29 +0000

    1. update to svn 1243485. 2. enable --enable-category-b . 3. USE_GCC=4.6+ to avoid compilation errors.

--- editors/openoffice-3-devel/files/patch-unxfbsd.mk
+++ editors/openoffice-3-devel/files/patch-unxfbsd.mk
@@ -1,5 +1,22 @@
 --- solenv/inc/unxfbsd.mk.orig	2011-12-21 09:47:20.000000000 +0900
 +++ solenv/inc/unxfbsd.mk	2011-12-25 20:48:29.000000000 +0900
+@@ -84,14 +84,14 @@
+ .ENDIF
+ 
+ # flags for the C++ Compiler
+-CFLAGSCC= -pipe $(ARCH_FLAGS)
++CFLAGSCC= -pipe $(ARCH_FLAGS) %%RPATH%%
+ # Flags for enabling exception handling
+ CFLAGSEXCEPTIONS=-fexceptions -fno-enforce-eh-specs
+ # Flags for disabling exception handling
+ CFLAGS_NO_EXCEPTIONS=-fno-exceptions
+ 
+ # -fpermissive should be removed as soon as possible
+-CFLAGSCXX= -pipe $(ARCH_FLAGS)
++CFLAGSCXX= -pipe $(ARCH_FLAGS) %%RPATH%%
+ .IF "$(HAVE_GCC_VISIBILITY_FEATURE)" == "TRUE"
+ CFLAGSCXX += -fvisibility-inlines-hidden
+ .ENDIF # "$(HAVE_GCC_VISIBILITY_FEATURE)" == "TRUE"
 @@ -140,16 +140,16 @@
  LINKFLAGS_SYSBASE:=-Wl,--sysroot=$(SYSBASE)
  .ENDIF          # "$(SYSBASE)"!=""
@@ -20,7 +37,7 @@
 +LINKFLAGSRUNPATH_BOXT=-Wl,-z,origin -Wl,-rpath,\''$$ORIGIN/../../../basis-link/program'\'
  LINKFLAGSRUNPATH_NONE=
 -LINKFLAGS=-Wl,-z,combreloc $(LINKFLAGSDEFS) $(LINKFLAGS_SYSBASE)
-+LINKFLAGS=-Wl,-z,combreloc $(LINKFLAGSDEFS) $(LINKFLAGS_SYSBASE)
++LINKFLAGS=-Wl,-z,combreloc %%RPATH%% $(LINKFLAGSDEFS) $(LINKFLAGS_SYSBASE)
  
  # linker flags for linking applications
  LINKFLAGSAPPGUI= -Wl,-export-dynamic -Wl,--noinhibit-exec \
