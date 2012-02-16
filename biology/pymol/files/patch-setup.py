fb14d73dc76cb9de0b3f7e2c768a617e1c468344
maho <maho@FreeBSD.org>
2012-02-16 02:30:10 +0000

    update to 1.5.0.1.

--- biology/pymol/files/patch-setup.py
+++ biology/pymol/files/patch-setup.py
@@ -1,21 +1,31 @@
---- setup.py.bak	2010-09-16 10:45:18.000000000 +0900
-+++ setup.py	2010-09-16 10:51:57.000000000 +0900
-@@ -127,7 +127,8 @@
-     inc_dirs=["ov/src",
-               "layer0","layer1","layer2",
-               "layer3","layer4","layer5",
--              "/usr/include/freetype2",
-+              "%%LOCALBASE%%/include",
-+              "%%LOCALBASE%%/include/freetype2",
- #                 "/users/warren/ext/include",
- # VMD plugin support
- #              "contrib/uiuc/plugins/include",
-@@ -139,7 +140,7 @@
-           ]	
-     pyogl_libs = ["GL","GLU","glut"]
-     lib_dirs=[
--        "/usr/X11R6/lib",
-+         "%%LOCALBASE%%/lib",
- #        "/users/warren/pymol/ext/lib"
-         ]
-     def_macros=[("_PYMOL_MODULE",None),
+--- setup.py	2012-02-14 04:02:02.000000000 +0900
++++ setup.py	2012-02-15 13:09:38.000000000 +0900
+@@ -159,7 +159,8 @@
+                  "layer3",
+                  "layer4",
+                  "layer5",
+-                 "/usr/include/freetype2",
++                 "%%LOCALBASE%%/include/freetype2",
++                 "%%LOCALBASE%%/include",
+               # VMD plugin support
+               #              "contrib/uiuc/plugins/include",
+               #              "contrib/uiuc/plugins/molfile_plugin/src",
+@@ -181,7 +181,7 @@
+                    "GLU",
+                    "glut",
+                    "GLEW"]
+-    lib_dirs = [ "/usr/X11R6/lib64", ]
++    lib_dirs = [ "%%LOCALBASE%%/lib", ]
+     def_macros = [ ("_PYMOL_MODULE",None),
+                    ("_PYMOL_INLINE",None),
+                    ("_PYMOL_FREETYPE",None),
+@@ -201,7 +202,7 @@
+                       "-funroll-loops",
+                       "-O3",
+                       "-g" ]
+-    ext_link_args = []
++    ext_link_args = [ "-L%%LOCALBASE%%/lib" ]
+ 
+ 
+ distribution = setup ( # Distribution meta-data
+
