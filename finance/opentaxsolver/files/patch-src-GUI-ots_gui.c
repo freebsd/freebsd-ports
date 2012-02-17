3e87ddb9a3c8c1913d74c6236ff553033fef72b5
pgollucci <pgollucci@FreeBSD.org>
2012-02-17 22:43:30 +0000

    - Update to 9.00   Files in redports work as expected
    PR:             ports/164875
    Submitted by:   John Hein <jhein@symmetricom.com> (maintainer)

--- finance/opentaxsolver/files/patch-src-GUI-ots_gui.c
+++ finance/opentaxsolver/files/patch-src-GUI-ots_gui.c
@@ -1,15 +1,16 @@
---- src/GUI/ots_gui.c.orig	2011-01-16 10:31:20.000000000 -0700
-+++ src/GUI/ots_gui.c	2011-01-24 14:49:02.000000000 -0700
-@@ -664,7 +664,7 @@
+$FreeBSD$
+--- src/GUI/ots_gui.c.orig	2012-01-13 07:12:50.000000000 -0700
++++ src/GUI/ots_gui.c	2012-02-07 08:14:06.000000000 -0700
+@@ -731,7 +731,7 @@
    npanels = nlines / lines_per_page + 1;
    if (npanels < 2) npanels = 2;
    panelnames = (char **)malloc( (npanels+1) * sizeof(char *));
 -  for (j=0; j<npanels; j++) {panelnames[j] = (char *)malloc(30); sprintf( panelnames[j], "Page %d ", j+1); }
-+  for (j=0; j<npanels; j++) {panelnames[j] = (char *)malloc(30); snprintf(panelnames[j], sizeof(panelnames[j]), "Page %d ", j+1); }
++  for (j=0; j<npanels; j++) {int len = 30;panelnames[j] = (char *)malloc(len); snprintf(panelnames[j], len, "Page %d ", j+1); }
    Panels = Otk_Tabbed_Panel_New( main_panel, npanels, panelnames, Otk_LightGray, 1, 1, 98, 98, 5 );
    TabbedPanel = Panels;
  
-@@ -724,7 +724,7 @@
+@@ -791,7 +791,7 @@
       switch (entry->kind)
        {
         case VKIND_FLOAT:  // printf("Formbox: '%s'\n", messg);
@@ -18,7 +19,7 @@
  		pos_x = leftmargin;
  		leftmargin = leftmargin + box_width + 1.5;
  		pos_y = (float)linenum * 9.0 + 6.5;
-@@ -1048,9 +1048,9 @@
+@@ -1142,9 +1142,9 @@
     return;   
    }
   if (PLATFORM_KIND==Posix_Platform)
@@ -30,7 +31,16 @@
  
   printf("Invoking '%s'\n", cmd );
   system(cmd);	/* Invoke the TaxSolver. */
-@@ -1086,7 +1086,7 @@
+@@ -1166,7 +1166,7 @@
+    viewfile = fopen( outfname, "rb" );
+    if (viewfile == 0)
+     {
+-     sprintf(vline,"Cannot open: %s", outfname);
++     snprintf(vline, sizeof(vline), "Cannot open: %s", outfname);
+      Otk_Add_Selection_Item( viewwin, vline, 0, 0 ); 
+     }
+    else
+@@ -1206,7 +1206,7 @@
  
  void togprntcmd_in(void *x)
  { 
@@ -39,7 +49,7 @@
   Otk_Modify_Text( printerformbox, printer_command );
  }
  
-@@ -1094,7 +1094,7 @@
+@@ -1214,7 +1214,7 @@
  { char tmpstr[MaxFname];
    int k;
   predict_output_filename(wrkingfname,tmpstr);
@@ -48,7 +58,7 @@
   Otk_Modify_Text( printerformbox, printer_command );
  }
  
-@@ -1122,7 +1122,7 @@
+@@ -1242,7 +1242,7 @@
   Otk_Add_BoundingBox( printpopup, Otk_Blue, 1.0, 18.0, 16.0, 63.0, 47.0 );
  
   if (working_file==0) strcpy(wrkingfname,filename_dat); else strcpy(wrkingfname,working_file);
@@ -57,14 +67,14 @@
  
   OtkMakeTextLabel( printpopup, "Print Command:", Otk_Black, /*scale=*/ 1.5, /*weight=*/ 1, /*x=*/ 4, /*y=*/ 57 );
   printerformbox = OtkMakeTextFormBox( printpopup, printer_command, 60,  28.5, 55, 68, 18, acceptprinter_command2, 0 );
-@@ -1181,15 +1181,15 @@
+@@ -1302,15 +1302,15 @@
     if (k>0) k--;
     while ((k>0) && (tmpstr[k]!=slashchr)) k--;
     if (tmpstr[k]==slashchr)  tmpstr[k+1] = '\0';
 -   else  {sprintf(tmpstr,".%c", slashchr);}
 -   sprintf(directory_dat,"%sexamples_and_templates%c", tmpstr, slashchr);
 +   else  {snprintf(tmpstr, sizeof(tmpstr), ".%c", slashchr);}
-+   snprintf(directory_dat, sizeof(directory_dat), "%sexamples_and_templates%c", tmpstr, slashchr);
++   snprintf(directory_dat, sizeof(directory_dat), "%%PREFIX%%/share/ots/examples_and_templates%c", slashchr);
     Otk_Browse_Files( "Select TaxForm:", MaxFname, directory_dat, wildcards_dat, taxsolvestrng, open_taxfile );
     return;
    }
@@ -76,18 +86,18 @@
     printf("Setting Tax Program to be: '%s'\n", tmpstr);
     taxsolvecmd = strdup(tmpstr);
     strcpy(taxsolvestrng,tmpstr);
-@@ -1200,8 +1200,8 @@
+@@ -1321,8 +1321,8 @@
     if (k>0) k--;
     while ((k>0) && (tmpstr[k]!=slashchr)) k--;
     if (tmpstr[k]==slashchr)  tmpstr[k+1] = '\0';
 -   else  {sprintf(tmpstr,".%c", slashchr);}
 -   sprintf(directory_dat,"%sexamples_and_templates%c", tmpstr, slashchr);
 +   else  {snprintf(tmpstr, sizeof(tmpstr), ".%c", slashchr);}
-+   snprintf(directory_dat, sizeof(directory_dat), "%sexamples_and_templates%c", tmpstr, slashchr);
++   snprintf(directory_dat, sizeof(directory_dat), "%%PREFIX%%/share/ots/examples_and_templates%c", slashchr);
  
-    sel = strstr( strg, "_2010" );
-    sel[0] = '\0';
-@@ -1272,7 +1272,7 @@
+    sel = strstr( strg, "_2011" );
+    if (sel != 0)
+@@ -1399,7 +1399,7 @@
     {printf("Unknown command-line parameter '%s'\n", argv[argn]); /* exit(1); */ }
    argn = argn + 1;
   }
@@ -96,12 +106,12 @@
   invocation_path = strdup(argv[0]);
   k = strlen(invocation_path)-1;
   while ((k>0) && (invocation_path[k]!=slashchr)) k--;
-@@ -1298,7 +1298,7 @@
-  main_panel = 
-  OtkMakePanel( bckgrnd, Otk_Raised, Otk_LightGray, 1, 7.5, 98, 87 );	/* Main Panel. */
-  Otk_SetBorderThickness( main_panel, 0.25 );
+@@ -1436,7 +1436,7 @@
+   OtkMakeImagePanel_ImgPtr( subpanel, logo_image, 1.5, 5.0, 96.8, 89.85 );
+  }
+ #else
 - sprintf(tmpstr,"%s..%csrc%cGUI%cotslogo.ppm", invocation_path, slashchr, slashchr, slashchr );
 + snprintf(tmpstr, sizeof(tmpstr), "%%PREFIX%%/share/ots/otslogo.ppm");
-  subpanel = OtkMakePanel( main_panel, Otk_Raised, Otk_LightGray, 18.5, 2, 63, 17 );
-  Otk_SetBorderThickness( subpanel, 0.75 );
-  testfile = fopen(tmpstr,"r");
+  testfile = fopen(tmpstr,"r");	/* External image-file. */
+  if (testfile!=0)
+   { /* Only attempt to show image if image-file is where we expect to find it. */
