466708da026b65acbf477713e81b8bb04bd005b8
pgj <pgj@FreeBSD.org>
2012-02-18 03:05:26 +0000

    Parser and writer for handling sectioned config files in Haskell.  The ConfigFile module works with configuration files in a standard format that is easy for the user to edit, easy for the programmer to work with, yet remains powerful and flexible.  It is inspired by, and compatible with, Python's ConfigParser module.  It uses files that resemble Windows .INI-style files, but with numerous improvements.
    ConfigFile provides simple calls to both read and write config files.
    It is possible to make a config file parsable by this module, the Unix
    shell, and make.
    
    WWW:	http://software.complete.org/configfile
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -48,6 +48,7 @@ clientsession_port=		security/hs-clientsession
 cmdargs_port=			devel/hs-cmdargs
 comonad_port=			math/hs-comonad
 comonad-transformers_port=	math/hs-comonad-transformers
+ConfigFile_port=		devel/hs-ConfigFile
 configurator_port=		devel/hs-configurator
 contravariant_port=		math/hs-contravariant
 convertible_port=		devel/hs-convertible
