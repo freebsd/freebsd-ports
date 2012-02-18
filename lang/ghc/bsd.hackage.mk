45819fa5598607fa15d83b405cc786769b65cd82
Gabor Pali <pgj@FreeBSD.org>
2012-02-18 16:09:15 +0000

    This library implements the RSA encryption and signature algorithms for arbitrarily-sized ByteStrings.  While the implementations work, they are not necessarily the fastest ones on the planet.  Particularly key generation.  The algorithms included are based of RFC 3447, or the Public-Key Cryptography Standard for RSA, version 2.1 (a.k.a, PKCS#1 v2.1).
    WWW:	http://hackage.haskell.org/package/RSA
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -190,6 +190,7 @@ regex-base_port=		textproc/hs-regex-base
 regex-compat_port=		textproc/hs-regex-compat
 regex-pcre-builtin_port=	textproc/hs-regex-pcre-builtin
 regex-posix_port=		textproc/hs-regex-posix
+RSA_port=			security/hs-RSA
 safe_port=			devel/hs-safe
 scgi_port=			www/hs-scgi
 sendfile_port=			net/hs-sendfile
