50d689991ea3e0d6868a4b02292423545f914abd
pgj <pgj@FreeBSD.org>
2012-02-18 18:46:37 +0000

    HsOpenSSL is an (incomplete) OpenSSL binding for Haskell.  It can generate RSA and DSA keys, read and write PEM files, generate message digests, sign and verify messages, encrypt and decrypt messages.  It has also some capabilities of creating SSL clients and servers.
    WWW:	https://github.com/phonohawk/HsOpenSSL
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -141,6 +141,7 @@ hs-bibutils_port=		textproc/hs-hs-bibutils
 hS3_port=			www/hs-hS3		# executable
 hscolour_port=			print/hs-hscolour	# executable
 hslogger_port=			devel/hs-hslogger
+HsOpenSSL_port=			security/hs-HsOpenSSL
 HStringTemplate_port=		textproc/hs-HStringTemplate
 html_port=			textproc/hs-html
 HTTP_port=			www/hs-HTTP
