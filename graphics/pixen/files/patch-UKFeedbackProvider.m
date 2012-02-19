93d44f5389fb8757dc752bb2aae0ac82eb5cd4ec
Dirk Meyer <dinoex@FreeBSD.org>
2012-02-19 08:17:39 +0000

    - fix build with gcc46

---
+++ graphics/pixen/files/patch-UKFeedbackProvider.m
@@ -0,0 +1,12 @@
+--- UKFeedbackProvider.m.orig	2004-12-15 16:11:50.000000000 +0100
++++ UKFeedbackProvider.m	2012-02-19 07:59:17.000000000 +0100
+@@ -7,7 +7,9 @@
+ //
+ 
+ #import "UKFeedbackProvider.h"
++#if 0
+ #import <Message/NSMailDelivery.h>
++#endif
+ 
+ 
+ @implementation UKFeedbackProvider
