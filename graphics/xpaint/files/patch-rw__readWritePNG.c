12d02a75c5c994eafa9a89d14e93c79c61d82c61
johans <johans@FreeBSD.org>
2012-02-19 09:40:21 +0000

    - Update xpaint to 2.9.9 - Now using GNU configure rather than Imakefiles - Dependens on new port x11-toolkits/libxaw3dxft

--- graphics/xpaint/files/patch-rw__readWritePNG.c
+++ graphics/xpaint/files/patch-rw__readWritePNG.c
@@ -1,6 +1,6 @@
 --- rw/readWritePNG.c.orig	2010-05-14 06:24:49.000000000 +0900
 +++ rw/readWritePNG.c	2010-06-24 04:56:04.000000000 +0900
-@@ -10,15 +10,14 @@
+@@ -10,16 +10,15 @@
  #include <stdlib.h>
  #include <png.h>
  
@@ -12,6 +12,7 @@
  #include <X11/Xlib.h>
  #include <X11/Intrinsic.h>
  #include "xpaint.h"
+ #include "../version.h"
  
 +#include <messages.h>
 +#include <image.h>
