0132205daa257a59451507fe89a70edc3dd88aeb
Ryan Steinmetz <zi@FreeBSD.org>
2012-02-20 14:22:17 +0000

    - Resolve issue where local filesystems were displayed as remote file systems.
    PR:		ports/164817
    Submitted by:	Rainer Duffner <rainer@ultra-secure.de>
    Obtained from:	Mel Flynn <rflynn@acsalaska.net>

--- net-mgmt/net-snmp/files/patch-agent__mibgroup__ucd-snmp__disk_hw.c
+++ net-mgmt/net-snmp/files/patch-agent__mibgroup__ucd-snmp__disk_hw.c
@@ -1,5 +1,5 @@
---- ./agent/mibgroup/ucd-snmp/disk_hw.c.orig	2011-09-28 00:53:47.000000000 -0400
-+++ ./agent/mibgroup/ucd-snmp/disk_hw.c	2012-02-13 11:07:08.000000000 -0500
+--- agent/mibgroup/ucd-snmp/disk_hw.c.orig	2011-09-27 20:53:47.000000000 -0800
++++ agent/mibgroup/ucd-snmp/disk_hw.c	2012-02-19 16:21:29.000000000 -0900
 @@ -137,6 +137,7 @@
    if (disks) {
       free( disks );
@@ -7,6 +7,15 @@
 +     maxdisks = numdisks = 0;
    }
    allDisksIncluded = 0;
+ }
+@@ -189,7 +190,7 @@
+   if ( entry ) {
+       entry->minspace   = minspace;
+       entry->minpercent = minpercent;
+-      entry->flags     |= ~NETSNMP_FS_FLAG_UCD;
++      entry->flags     |= NETSNMP_FS_FLAG_UCD;
+       disks[numdisks++] = entry;
+   }
  }
 @@ -321,7 +322,7 @@
  
