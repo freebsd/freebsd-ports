781cb3ff251b57581947ceb7fee29a81ae0dcaaa
decke <decke@FreeBSD.org>
2012-02-21 14:31:55 +0000

    - Update to 4.1.8 - Mark broken on FreeBSD < 8.0 - Fix lib32 checks to use /usr/lib32/libc.so - R0 memobj rewrite [1] - Fix INVARIANTS warning: acquiring duplicate lock of same type: "IPRT Fast Mutex Semaphore" [1]
    Submitted by:	Andriy Gapon <avg@FreeBSD.org> [1]
    Obtained from:	redports.org/~virtualbox/

--- emulators/virtualbox-ose-kmod/files/patch-src-VBox-HostDrivers-Support-freebsd-SUPDrv-freebsd.c
+++ emulators/virtualbox-ose-kmod/files/patch-src-VBox-HostDrivers-Support-freebsd-SUPDrv-freebsd.c
@@ -1,5 +1,14 @@
---- src/VBox/HostDrivers/Support/freebsd/SUPDrv-freebsd.c.orig	2011-11-04 07:19:39.000000000 -0400
-+++ src/VBox/HostDrivers/Support/freebsd/SUPDrv-freebsd.c	2011-11-29 17:34:56.000000000 -0500
+- Fix compile error: 'D_PSEUDO' undeclared here on 10-CURRENT
+
+Submitted by:	Jung-uk Kim <jkim@FreeBSD.org>
+
+- Fix check that kernel module can not be unloaded when there
+are running VMs
+
+Submitted by:	aeichner
+Reported by:	avg
+--- src/VBox/HostDrivers/Support/freebsd/SUPDrv-freebsd.c.orig	2011-11-04 12:19:39.000000000 +0100
++++ src/VBox/HostDrivers/Support/freebsd/SUPDrv-freebsd.c	2011-12-13 12:22:44.325251163 +0100
 @@ -100,7 +100,9 @@
  static struct cdevsw        g_VBoxDrvFreeBSDChrDevSW =
  {
@@ -11,3 +20,12 @@
      .d_flags =          D_PSEUDO | D_TRACKCLOSE | D_NEEDMINOR,
  #else
      .d_flags =          D_PSEUDO | D_TRACKCLOSE,
+@@ -205,7 +207,7 @@
+     Log(("VBoxDrvFreeBSDUnload:\n"));
+ 
+     if (g_cUsers > 0)
+-        return EBUSY;
++        return VERR_RESOURCE_BUSY;
+ 
+     /*
+      * Reserve what we did in VBoxDrvFreeBSDInit.
