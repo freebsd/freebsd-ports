7765b6e0782244f6b8bd6d792854f6d9e7811754
Dennis Herrmann <dhn@FreeBSD.org>
2012-02-22 20:23:27 +0000

    - Update URL in files/geoipupdate.sh - Bump PORTREVISION
    PR:		ports/165379
    Submitted by:	Seiji Ariga <freebsd@khaotic.net>

--- net/GeoIP/files/geoipupdate.sh
+++ net/GeoIP/files/geoipupdate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /tmp
-fetch http://64.246.48.99/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+fetch http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
 gunzip GeoIP.dat.gz
 mv -f GeoIP.dat /usr/local/share/GeoIP
