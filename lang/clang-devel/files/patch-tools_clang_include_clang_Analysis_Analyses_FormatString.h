ff45cfe8338f0dcc0e76c37f0347aa6546d92a72
Brooks Davis <brooks@FreeBSD.org>
2012-02-22 23:19:27 +0000

    Upgrade to llvm/clang r150242.
    Enable installation of the shared libaray in LLVM.
    
    Add preliminary support for building and installing the gold linker
    plugin.  No option is avaiable to turn it on (use -DWITH_LTOPLUGIN)
    because devel/binutils does not yet build the gold linker.

--- lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
+++ lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
@@ -3,14 +3,14 @@ $FreeBSD$
 
 --- tools/clang/include/clang/Analysis/Analyses/FormatString.h.orig
 +++ tools/clang/include/clang/Analysis/Analyses/FormatString.h
-@@ -139,6 +139,10 @@
+@@ -141,6 +141,10 @@
      SArg,
-     
+ 
      // ** Printf-specific **
-+
++    
 +    // FreeBSD specific specifiers
 +    bArg,
 +    DArg,
-   
+ 
      // Objective-C specific specifiers.
      ObjCObjArg,  // '@'
