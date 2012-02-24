925ba7e0afc4387863167fa6f0d8dffded94712b
osa <osa@FreeBSD.org>
2012-02-24 13:04:14 +0000

    Update from 2.4.7 to 2.4.8.
    <ChangeLog>
    
    UPGRADE URGENCY: moderate if you don't experience any of the fixed problems.
    
    * [BUGFIX] Make install now uses cp -f to avoid 'text file busy' errors.
    * [BUGFIX] redis-check-aof is now large files safe also on 32 bit systems.
    * [BUGFIX] Issue #327 fixed: maxmemory and replication now work much better.
    * [BUGFIX] Now HINCRBY can detect overflows too. Fix for issue #330.
    * [BUGFIX] Fixed compilation with latest clang.
    * [BUGFIX] Fixed handling of empty sorted sets produced in RDB by
               very old Redis versions (1.2.x).
    
    </ChangeLog>

--- databases/redis/files/patch-src::config.h
+++ databases/redis/files/patch-src::config.h
@@ -1,11 +1,11 @@
---- src/config.h.orig	2011-11-30 16:28:05.000000000 +0400
-+++ src/config.h	2011-11-30 16:28:19.000000000 +0400
+--- src/config.h.orig	2012-02-24 16:56:36.000000000 +0400
++++ src/config.h	2012-02-24 16:57:01.000000000 +0400
 @@ -25,7 +25,7 @@
  #endif
  
  /* Test for backtrace() */
--#if defined(__APPLE__) || defined(__linux__)
-+#if defined(__APPLE__) || defined(__linux__) || defined(__FreeBSD__)
+-#if defined(__APPLE__) || defined(__linux__) || defined(__sun)
++#if defined(__APPLE__) || defined(__linux__) || defined(__sun) || defined(__FreeBSD__)
  #define HAVE_BACKTRACE 1
  #endif
  
