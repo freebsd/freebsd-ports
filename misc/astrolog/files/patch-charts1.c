68070a0fa892674425896eb4e6e40a0ca301faa5
ache <ache@FreeBSD.org>
2012-02-25 16:33:10 +0000

    Fix printed universal time calculation when time zone offset contains minutes.

--- misc/astrolog/files/patch-charts1.c
+++ misc/astrolog/files/patch-charts1.c
@@ -1,5 +1,5 @@
---- charts1.c.bak	1998-12-23 23:29:26.000000000 +0300
-+++ charts1.c	2007-06-23 16:28:33.000000000 +0400
+--- charts1.c.orig	1998-12-23 23:29:14.000000000 +0300
++++ charts1.c	2012-02-25 19:40:41.000000000 +0400
 @@ -53,7 +53,7 @@
    char sz[cchSzDef];
    int day, fNam, fLoc;
@@ -18,3 +18,12 @@
  
    PrintHeader();    /* Show time and date of the chart being displayed. */
  
+@@ -493,7 +493,7 @@
+   case 5:
+     nT = us.fEuroTime; us.fEuroTime = fTrue;
+     sprintf(szT, "%s", SzTim(DegToDec(DFromR(is.RA)*(24.0/rDegMax))));
+-    sprintf(sz, "UT: %s, Sid.T: %s", SzTim(Tim+Zon-Dst), szT);
++    sprintf(sz, "UT: %s, Sid.T: %s", SzTim(DegToDec(DecToDeg(Tim)+DecToDeg(Zon-Dst))), szT);
+     us.fEuroTime = nT;
+     break;
+   case 6:
