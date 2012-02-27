617463b82315fe8ebb210511fd0328555406fcb1
makc <makc@FreeBSD.org>
2012-02-27 16:30:45 +0000

    Update to 2.4.1 Perform outsource build as recommended in supplementary docs Remove bogus install target from port Makefile Build and install documentation Install desktop file Mark MAKE_JOBS_UNSAFE temporary

--- devel/qtcreator/files/patch-src-plugins-qt4projectmanager-makestep.cpp
+++ devel/qtcreator/files/patch-src-plugins-qt4projectmanager-makestep.cpp
@@ -1,13 +1,15 @@
---- ./src/plugins/qt4projectmanager/makestep.cpp.orig	2010-08-24 11:46:09.000000000 +0400
-+++ ./src/plugins/qt4projectmanager/makestep.cpp	2010-11-29 00:21:08.727128457 +0300
-@@ -163,8 +163,10 @@
-     if (toolchain) {
-         if (toolchain->type() != ProjectExplorer::ToolChain::MSVC &&
-             toolchain->type() != ProjectExplorer::ToolChain::WINCE) {
+--- ./src/plugins/qt4projectmanager/makestep.cpp.orig	2012-01-24 14:37:13.000000000 +0000
++++ ./src/plugins/qt4projectmanager/makestep.cpp	2012-02-26 15:30:04.084747079 +0000
+@@ -201,10 +201,12 @@
+     // FIXME doing this without the user having a way to override this is rather bad
+     // so we only do it for unix and if the user didn't override the make command
+     // but for now this is the least invasive change
 +#ifndef Q_OS_FREEBSD
-             if (m_makeCmd.isEmpty())
-                 args << "-w";
+     if (toolchain
+             && toolchain->targetAbi().binaryFormat() != ProjectExplorer::Abi::PEFormat
+             && m_makeCmd.isEmpty())
+         Utils::QtcProcess::addArg(&args, QLatin1String("-w"));
 +#endif
-         }
-     }
  
+     setEnabled(true);
+     pp->setArguments(args);
