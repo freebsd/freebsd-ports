ad1eaca34508729364854bc70216261f5c3a2f0a
demon <demon@FreeBSD.org>
2012-02-28 10:01:38 +0000

    Update to version 1.4.19. maintainer timeout.

---
+++ net/haproxy/files/patch-src-proto_http.c
@@ -0,0 +1,10 @@
+--- src/proto_http.c.orig	2012-02-28 13:58:43.000000000 +0400
++++ src/proto_http.c	2012-02-28 13:58:49.000000000 +0400
+@@ -19,7 +19,6 @@
+ #include <syslog.h>
+ #include <time.h>
+ 
+-#include <netinet/tcp.h>
+ 
+ #include <sys/socket.h>
+ #include <sys/stat.h>
