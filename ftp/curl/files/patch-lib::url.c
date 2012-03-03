91e625f942bf2fb0913b0a4c57ef581a936e3923
sunpoet <sunpoet@FreeBSD.org>
2012-03-03 08:05:19 +0000

    - Update to 7.24.0
    Changes:	http://curl.haxx.se/changes.html

--- ftp/curl/files/patch-lib::url.c
+++ ftp/curl/files/patch-lib::url.c
@@ -3,9 +3,9 @@ Forwarded: not-needed
 Author: Peter Pentchev <roam@FreeBSD.org>
 Last-Update: 2010-12-18
 
---- a/lib/url.c
-+++ b/lib/url.c
-@@ -857,6 +857,10 @@
+--- lib/url.c.orig	2012-01-23 23:31:30.000000000 +0800
++++ lib/url.c	2012-02-26 17:52:21.937920303 +0800
+@@ -797,6 +797,10 @@
      data->progress.flags |= PGRS_HIDE;
      data->state.current_speed = -1; /* init to negative == impossible */
  
