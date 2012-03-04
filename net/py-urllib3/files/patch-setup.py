8c385dac0794cfad72aaa5c5c2de6be1de422dcd
Martin Wilke <miwi@FreeBSD.org>
2012-03-04 12:39:34 +0000

    - Update to 1.2.2
    PR:		164856
    Submitted by:	Olivier Duchateau <duchateau.olivier@gmail.com> (maintainer)

--- net/py-urllib3/files/patch-setup.py
+++ net/py-urllib3/files/patch-setup.py
@@ -1,8 +1,13 @@
---- setup.py.orig	2012-01-07 22:23:02.000000000 +0100
-+++ setup.py	2012-01-09 19:08:23.000000000 +0100
-@@ -49,6 +49,6 @@
+--- setup.py.orig	2012-01-30 00:14:40.000000000 +0000
++++ setup.py	2012-02-07 13:49:58.000000000 +0000
+@@ -44,10 +44,10 @@
+       author_email='andrey.petrov@shazow.net',
+       url='http://urllib3.readthedocs.org/',
        license='MIT',
-       packages=['urllib3', 'dummyserver', 'urllib3.packages', 'urllib3.packages.ssl_match_hostname'],
+-      packages=['urllib3', 'dummyserver', 'urllib3.packages',
++      packages=['urllib3', 'urllib3.packages',
+                 'urllib3.packages.ssl_match_hostname', 'urllib3.packages.mimetools_choose_boundary',
+                 ],
        requires=requirements,
 -      tests_require=tests_requirements,
 -      test_suite='test',
