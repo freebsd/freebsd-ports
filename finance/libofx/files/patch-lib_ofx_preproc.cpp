ab74a2a896d97531b8ffca9d7df67bdf64b616b2
Steve Wills <swills@FreeBSD.org>
2012-03-05 21:50:55 +0000

    - Update to 0.9.5
    PR:		ports/165716
    Submitted by:	Jason E. Hale <bsdkaffee@gmail.com> (maintainer)

--- finance/libofx/files/patch-lib_ofx_preproc.cpp
+++ finance/libofx/files/patch-lib_ofx_preproc.cpp
@@ -1,11 +1,11 @@
---- ./lib/ofx_preproc.cpp.orig	2011-04-16 13:10:20.000000000 -0400
-+++ ./lib/ofx_preproc.cpp	2011-04-21 11:03:43.000000000 -0400
-@@ -228,7 +228,7 @@
+--- ./lib/ofx_preproc.cpp.orig	2011-06-08 08:07:52.000000000 -0400
++++ ./lib/ofx_preproc.cpp	2012-02-22 08:46:57.000000000 -0500
+@@ -231,7 +231,7 @@
            memset(iconv_buffer, 0, READ_BUFFER_SIZE * 2);
            size_t inbytesleft = strlen(s_buffer.c_str());
            size_t outbytesleft = READ_BUFFER_SIZE * 2 - 1;
 -#ifdef OS_WIN32
-+#if defined(OS_WIN32) || defined(OS_FREEBSD)
++#if defined(OS_WIN32) || defined(__FreeBSD__)
            const char * inchar = (const char *)s_buffer.c_str();
  #else
            char * inchar = (char *)s_buffer.c_str();
