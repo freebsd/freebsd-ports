573bb9ea545f317859f1680979a56debce05fe76
sunpoet <sunpoet@FreeBSD.org>
2012-03-05 02:00:45 +0000

    - Fix build with curl 7.24.0: curl/types.h was removed (unused since Apr 2004)
    PR:		ports/165701
    Notified by:	makc
    Submitted by:	sunpoet (myself)
    Approved by:	Jason E. Hale <bsdkaffee@gmail.com> (maintainer)

--- audio/libtunepimp/files/patch-lib_protocol.cpp
+++ audio/libtunepimp/files/patch-lib_protocol.cpp
@@ -1,6 +1,6 @@
 --- lib/protocol.cpp.orig	2010-06-07 10:47:20.653619576 +0300
 +++ lib/protocol.cpp	2010-06-07 10:47:55.249722982 +0300
-@@ -8,6 +8,7 @@
+@@ -8,11 +8,11 @@
  -------------------------------------------------------------------*/
  #include <stdio.h>
  #include <stdlib.h>
@@ -8,3 +8,8 @@
  #include <string>
  #include <map>
  #include <expat.h>
+ #include <curl/curl.h>
+-#include <curl/types.h>
+ #include <curl/easy.h>
+ using namespace std;
+ 
