a693ce357757457df90cefbe51cab488c387cb57
ehaupt <ehaupt@FreeBSD.org>
2012-03-07 10:29:11 +0000

    Use OPTIONS

--- misc/ddate/files/patch-misc-utils__ddate.c
+++ misc/ddate/files/patch-misc-utils__ddate.c
@@ -1,5 +1,14 @@
---- ./misc-utils/ddate.c.orig	2012-03-06 16:45:33.000000000 +0100
-+++ ./misc-utils/ddate.c	2012-03-06 16:46:18.000000000 +0100
+--- ./misc-utils/ddate.c.orig	2011-12-18 21:29:45.802837000 +0100
++++ ./misc-utils/ddate.c	2012-03-07 11:19:23.000000000 +0100
+@@ -51,7 +51,7 @@
+  * Church of the SubGenius and do not wish your copy of ddate(1) to contain
+  * code for counting down to X-Day, undefine KILL_BOB */
+ 
+-#define KILL_BOB 13013
++/* #define KILL_BOB 13013 */
+ 
+ /* If you wish ddate(1) to contain SubGenius slogans, define PRAISE_BOB */
+ 
 @@ -62,8 +62,9 @@
  #include <time.h>
  #include <stdio.h>
