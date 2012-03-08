a58bafa1fc37323ff92d2181624f8d5dff96c0a6
pgollucci <pgollucci@FreeBSD.org>
2012-03-08 05:07:14 +0000

    - Fix build with llvm/clang and other miscellanous compile issues - Bump PORTREVISION
    PR:             ports/164893
    Submitted by:   Brandon Gooch <jamesbrandongooch@gmail.com> &
                    Nikos Ntarmos <ntarmos@ceid.upatras.gr> (maintainer)

---
+++ sysutils/conky/files/patch-src-ccurl_thread.c
@@ -0,0 +1,10 @@
+--- src/ccurl_thread.c.orig	2010-10-06 00:29:36.000000000 +0300
++++ src/ccurl_thread.c	2012-03-05 01:08:23.000000000 +0200
+@@ -33,7 +33,6 @@
+ #endif /* DEBUG */
+ 
+ #include <curl/curl.h>
+-#include <curl/types.h>
+ #include <curl/easy.h>
+ 
+ /*
--- sysutils/conky/files/patch-src-freebsd.c
+++ sysutils/conky/files/patch-src-freebsd.c
@@ -1,6 +1,18 @@
---- src/freebsd.c.orig	2010-10-24 03:14:05.000000000 +0300
-+++ src/freebsd.c	2010-10-24 03:13:05.000000000 +0300
-@@ -266,7 +266,9 @@
+--- src/freebsd.c.orig	2010-10-06 00:29:36.000000000 +0300
++++ src/freebsd.c	2012-03-05 01:02:25.000000000 +0200
+@@ -38,6 +38,11 @@
+ #include <sys/types.h>
+ #include <sys/user.h>
+ 
++#if defined(__FreeBSD__) && (!defined(__FreeBSD_kernel__) || !(__FreeBSD_kernel__ + 0))
++# undef __FreeBSD_kernel__
++# define __FreeBSD_kernel__ __FreeBSD__
++#endif
++
+ #include <net/if.h>
+ #include <net/if_mib.h>
+ #include <net/if_media.h>
+@@ -266,7 +271,9 @@
  {
  	int n_processes;
  
@@ -10,15 +22,19 @@
  
  	info.procs = n_processes;
  	return 0;
-@@ -278,6 +280,7 @@
+@@ -278,9 +285,10 @@
  	int n_processes;
  	int i, cnt = 0;
  
 +	pthread_mutex_lock(&kvm_proc_mutex);
  	p = kvm_getprocs(kd, KERN_PROC_ALL, 0, &n_processes);
  	for (i = 0; i < n_processes; i++) {
- #if (__FreeBSD__ < 5) && (__FreeBSD_kernel__ < 5)
-@@ -288,6 +291,7 @@
+-#if (__FreeBSD__ < 5) && (__FreeBSD_kernel__ < 5)
++#if (defined(__FreeBSD__) && (__FreeBSD__ < 5)) || (defined(__FreeBSD_kernel__) && (__FreeBSD_kernel__ < 5))
+ 		if (p[i].kp_proc.p_stat == SRUN) {
+ #else
+ 		if (p[i].ki_stat == SRUN) {
+@@ -288,6 +296,7 @@
  			cnt++;
  		}
  	}
@@ -26,7 +42,15 @@
  
  	info.run_procs = cnt;
  	return 0;
-@@ -748,6 +752,7 @@
+@@ -296,7 +305,6 @@
+ void get_cpu_count(void)
+ {
+ 	int cpu_count = 0;
+-	size_t cpu_count_len = sizeof(cpu_count);
+ 
+ 	if (GETSYSCTL("hw.ncpu", cpu_count) == 0) {
+ 		info.cpu_count = cpu_count;
+@@ -748,6 +756,7 @@
  		CRIT_ERR(NULL, NULL, "Cannot read sysctl \"vm.stats.vm.v_page_count\"");
  	}
  
