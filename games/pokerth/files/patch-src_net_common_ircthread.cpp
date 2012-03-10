3dff154543c4e08a2c3335532efb574450f5b7de
jgh <jgh@FreeBSD.org>
2012-03-10 01:28:07 +0000

    - Update to 0.9.3 and unbreak
    PR:	ports/165824
    Submitted by:	maintainer, mad@madpilot.net
    Feature safe:	yes

--- games/pokerth/files/patch-src_net_common_ircthread.cpp
+++ games/pokerth/files/patch-src_net_common_ircthread.cpp
@@ -1,11 +1,12 @@
---- src/net/common/ircthread.cpp.orig	2012-01-04 14:33:45.259685354 +0100
-+++ src/net/common/ircthread.cpp	2012-01-04 14:32:53.601974192 +0100
-@@ -19,7 +19,7 @@
+--- src/net/common/ircthread.cpp.orig	2012-02-11 17:57:34.000000000 +0100
++++ src/net/common/ircthread.cpp	2012-03-07 11:03:40.501681051 +0100
+@@ -19,7 +19,8 @@
  #include <net/socket_helper.h>
  #include <net/ircthread.h>
  #include <net/socket_msg.h>
 -#include <libircclient/libircclient.h>
 +#include <libircclient.h>
++#include <libirc_rfcnumeric.h>
  #include <boost/algorithm/string/predicate.hpp>
  #include <queue>
  #include <sstream>
