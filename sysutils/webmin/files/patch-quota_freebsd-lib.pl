04c308af39068330808d6ccf60123e16efc1ce4e
olgeni <olgeni@FreeBSD.org>
2012-03-12 21:28:17 +0000

    More fixes for quota format on FreeBSD 9.
    Obtained from:	remko
    Feature safe:	yes

--- sysutils/webmin/files/patch-quota_freebsd-lib.pl
+++ sysutils/webmin/files/patch-quota_freebsd-lib.pl
@@ -3,7 +3,28 @@ $FreeBSD$
 
 --- quota/freebsd-lib.pl.orig
 +++ quota/freebsd-lib.pl
-@@ -240,11 +240,17 @@
+@@ -152,7 +152,9 @@
+ 	if (/^(\S+)$/) {
+ 		# Bogus wrapped line
+ 		$filesys{$n,'filesys'} = $1;
+-		<QUOTA>=~/^.{15}(.{8}).(.{7})(.{8}).{8}(.{8}).(.{7})(.{8})/;
++		local $nl = <QUOTA>;
++		$nl =~ /^\s+(\S+)\s+(\S+)\s+(\S+)(.{8}\s+)(\S+)\s+(\S+)\s+(\S+)(.*)/ ||
++		  $nl =~ /^.{15}(.{8}).(.{7})(.{8}).{8}(.{8}).(.{7})(.{8})/;
+ 		$filesys{$n,'ublocks'} = int($1);
+ 		$filesys{$n,'sblocks'} = int($2);
+ 		$filesys{$n,'hblocks'} = int($3);
+@@ -161,7 +163,8 @@
+ 		$filesys{$n,'hfiles'} = int($6);
+ 		$n++;
+ 		}
+-	elsif (/^(.{15})(.{8}).(.{7})(.{8}).{8}(.{8}).(.{7})(.{8})/) {
++	elsif (/^\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)(.{8}\s+)(\S+)\s+(\S+)\s+(\S+)(.*)/ ||
++	       /^(.{15})(.{8}).(.{7})(.{8}).{8}(.{8}).(.{7})(.{8})/) {
+ 		$filesys{$n,'ublocks'} = int($2);
+ 		$filesys{$n,'sblocks'} = int($3);
+ 		$filesys{$n,'hblocks'} = int($4);
+@@ -240,11 +243,17 @@
  @line = split(/\n/, $_[0]);
  for($i=0; $i<@line; $i++) {
  	if ($line[$i] =~ /^(\S+): (blocks|kbytes) in use: (\d+), limits \(soft = (\d+), hard = (\d+)\)$/ && $1 eq $_[1]) {
