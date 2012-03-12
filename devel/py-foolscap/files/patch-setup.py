36452bb9d106bf997f975e4e521d725e5e38c497
kevlo <kevlo@FreeBSD.org>
2012-03-12 07:19:07 +0000

    - Build with easy_install - Update plist - Bump PORTREVISION
    Feature safe:	yes

--- devel/py-foolscap/files/patch-setup.py
+++ devel/py-foolscap/files/patch-setup.py
@@ -1,18 +1,11 @@
 --- setup.py.orig	2012-01-06 11:21:39.000000000 +0800
-+++ setup.py	2012-03-02 15:14:09.000000000 +0800
-@@ -52,15 +52,6 @@
- }
- 
- have_setuptools = False
--try:
--    # If setuptools is installed, then we'll add setuptools-specific
--    # arguments to the setup args. If we're on windows, this includes
--    # entry_points= scripts to create the appropriate .bat files.
--    import setuptools
--    _hush_pyflakes = [setuptools]
++++ setup.py	2012-03-12 15:07:15.000000000 +0800
+@@ -58,7 +58,7 @@
+     # entry_points= scripts to create the appropriate .bat files.
+     import setuptools
+     _hush_pyflakes = [setuptools]
 -    have_setuptools = True
--except ImportError:
--    pass
++    have_setuptools = False
+ except ImportError:
+     pass
  
- if have_setuptools:
-     import platform
