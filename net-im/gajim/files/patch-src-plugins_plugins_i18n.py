9097d5215192fe3e23974a0d95a3f6ee210a2489
rm <rm@FreeBSD.org>
2012-03-21 04:28:45 +0000

    - update to 0.15 - actualize dependencies list according to docs - exclude -A from FETCH_ARGS - add WITH_CRYPTO option (default off) - actualize pkg-descr
    Feature safe:   yes

---
+++ net-im/gajim/files/patch-src-plugins_plugins_i18n.py
@@ -0,0 +1,10 @@
+--- src/plugins/plugins_i18n.py.orig	2012-03-18 15:26:00.000000000 +0400
++++ src/plugins/plugins_i18n.py	2012-03-21 08:21:52.000000000 +0400
+@@ -29,7 +29,6 @@
+ 
+ if os.name != 'nt':
+     locale.setlocale(locale.LC_ALL, '')
+-    locale.bindtextdomain(APP, plugins_locale_dir)
+     gettext.bindtextdomain(APP, plugins_locale_dir)
+     gettext.textdomain(APP)
+ 
