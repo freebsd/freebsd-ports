9085ce378c64a5995408339e4dc452ccd925030c
scheidell <scheidell@FreeBSD.org>
2012-03-24 22:02:35 +0000

    - Fixes broken read of '/proc/modules' [1] - Silence warning from setup.py during build [1] - Reorder _DEPENDS lines - Bump PORTREVISION
    PR:		ports/166322 [1]
    Submitted by:	Christer Edwards <christer.edwards@gmail.com> (maintainer)
    Approved by:	Christer Edwards <christer.edwards@gmail.com> (maintainer)
    Feature safe:	yes

--- sysutils/salt/files/patch-setup.py
+++ sysutils/salt/files/patch-setup.py
@@ -1,15 +1,15 @@
---- salt-0.9.6/setup.py 2012-01-21 02:18:08.000000000 +0000
-+++ setup.py    2012-01-22 19:21:35.158004570 +0000
-@@ -44,7 +44,7 @@
+--- setup.py    2012-03-21 11:46:26.000000000 -0600
++++ setup.py    2012-03-24 14:01:33.965200298 -0600
+@@ -47,7 +47,7 @@
  if 'SYSCONFDIR' in os.environ:
      etc_path = os.environ['SYSCONFDIR']
  else:
 -    etc_path = os.path.join(os.path.dirname(PREFIX), 'etc')
 +    etc_path = os.path.join('%%PREFIX%%', 'etc')
- 
+
  libraries = ['ws2_32'] if sys.platform == 'win32' else []
- 
-@@ -91,10 +91,10 @@
+
+@@ -98,10 +98,10 @@
                 'scripts/salt-run',
                 'scripts/salt'],
        data_files=[(os.path.join(etc_path, 'salt'),
@@ -23,7 +23,7 @@
                      ['doc/man/salt-master.1',
                       'doc/man/salt-key.1',
                       'doc/man/salt.1',
-@@ -104,7 +104,7 @@
+@@ -111,9 +111,8 @@
                       'doc/man/salt-run.1',
                       'doc/man/salt-minion.1',
                      ]),
@@ -32,3 +32,5 @@
                      ['doc/man/salt.7',
                      ]),
                   ],
+-      install_requires=requirements,
+      )
