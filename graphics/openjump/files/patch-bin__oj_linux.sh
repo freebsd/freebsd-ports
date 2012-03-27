f1339de2b461afb7d165a0fe2a6d728322fd896b
Jason Helfman <jgh@FreeBSD.org>
2012-03-27 17:22:09 +0000

    - Update to 1.5.1, follow upstream rename to OpenJUMP - reactivate java version checking - while here move licenses into license framework
    PR:	ports/166290
    Submitted by:	maintainer, rhurlin@gwdg.de
    Feature safe:	yes

--- graphics/openjump/files/patch-bin__oj_linux.sh
+++ graphics/openjump/files/patch-bin__oj_linux.sh
@@ -1,32 +1,20 @@
---- bin/oj_linux.sh.orig	2011-09-11 12:55:32.000000000 +0200
-+++ bin/oj_linux.sh	2011-10-23 09:46:55.000000000 +0200
-@@ -1,7 +1,7 @@
- #!/bin/sh
+--- bin/oj_linux.sh.orig	2012-01-25 20:15:16.000000000 +0100
++++ bin/oj_linux.sh	2012-03-10 18:01:15.000000000 +0100
+@@ -6,7 +6,7 @@
+ #JUMP_SETTINGS="/tmp/foobar"
  
  ## uncomment and put the path to your jre here
 -#JAVA_HOME="/home/ed/jre1.6.0_21"
-+JAVA_HOME="/usr/local/"
++JAVA_HOME="/usr/local"
  
  ## uncomment and change your memory configuration here 
  ## Xms is initial size, Xmx is maximum size
-@@ -75,13 +75,13 @@
+@@ -121,7 +121,7 @@
   echo "The found java binary '$JAVA' is no executable file." && ERROR=1 && end
  
  # java version check
 -JAVA_VERSION=$("$JAVA" -version 2>&1 | awk -F'"' '/^java version/{print $2}' | awk -F'.' '{print $1"."$2}')
--JAVA_NEEDED="1.5"
--if ! awk "BEGIN{if($JAVA_VERSION < $JAVA_NEEDED)exit 1}"; then
--  echo "Your java version '$JAVA_VERSION' is insufficient to run openjump.
--Please provide an at least version '$JAVA_NEEDED' java runtime."
--  ERROR=1
--fi
-+#JAVA_VERSION=$("$JAVA" -version 2>&1 | awk -F'"' '/^java version/{print $2}' | awk -F'.' '{print $1"."$2}')
-+#JAVA_NEEDED="1.5"
-+#if ! awk "BEGIN{if($JAVA_VERSION < $JAVA_NEEDED)exit 1}"; then
-+#  echo "Your java version '$JAVA_VERSION' is insufficient to run openjump.
-+#Please provide an at least version '$JAVA_NEEDED' java runtime."
-+#  ERROR=1
-+#fi
- 
- # always print java infos
- echo "Running -> '${JAVA}'; " $("$JAVA" -version 2>&1|awk 'BEGIN{ORS=""}{print $0"; "}')
++JAVA_VERSION=$("$JAVA" -version 2>&1 | awk -F'"' '/^java version/ || /^openjdk version/{print $2}' | awk -F'.' '{print $1"."$2}')
+ JAVA_NEEDED="1.5"
+ if ! awk "BEGIN{if($JAVA_VERSION < $JAVA_NEEDED)exit 1}"; then
+   echo "Your java version '$JAVA_VERSION' is insufficient to run openjump.
