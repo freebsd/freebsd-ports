d535a0395a125c14fd2855d3dbd10c6626bfd682
Daichi GOTO <daichi@FreeBSD.org>
2012-04-02 00:47:16 +0000

    Update to ibus-anthy-1.4.99.20120327
    Feature safe: yes

--- japanese/ibus-anthy/files/patch-setup_main.py
+++ japanese/ibus-anthy/files/patch-setup_main.py
@@ -1,11 +1,11 @@
---- setup/main.py.orig	2012-01-14 12:14:14.986154797 +0900
-+++ setup/main.py	2012-01-14 12:24:25.953154304 +0900
-@@ -58,7 +58,7 @@
-         # glade "icon_name" property has a custom scaling and it seems
+--- setup/main.py.orig	2012-04-02 08:48:25.111727456 +0900
++++ setup/main.py	2012-04-02 08:51:09.382726912 +0900
+@@ -71,7 +71,7 @@
+         # glade 'icon_name' property has a custom scaling and it seems
          # to be difficult to show the complicated small icon in metacity.
          # This can add the pixbuf without scaling.
 -        anthydir = path.dirname(path.dirname(__file__))
 +        anthydir = "@@LOCALBASE@@/share/ibus-anthy"
          if not anthydir:
-             anthydir = "/usr/share/ibus-anthy"
-         icon_path = path.join(anthydir, "icons", "ibus-anthy.png")
+             anthydir = '/usr/share/ibus-anthy'
+         icon_path = path.join(anthydir, 'icons', 'ibus-anthy.png')
