e5bdf3f1d144b5b710898b7d3cab7ed07bbd9cc3
Ryan Steinmetz <zi@FreeBSD.org>
2012-04-04 18:49:59 +0000

    - Merge in upstream patch to resolve bug 3460364: Fix use of block factor when detecting error conditions - Bump PORTREVISION
    Feature safe:	yes
    Obtained from:	upstream git repo

--- net-mgmt/net-snmp/files/patch-agent__mibgroup__ucd-snmp__disk_hw.c
+++ net-mgmt/net-snmp/files/patch-agent__mibgroup__ucd-snmp__disk_hw.c
@@ -1,5 +1,5 @@
---- agent/mibgroup/ucd-snmp/disk_hw.c.orig	2011-09-27 20:53:47.000000000 -0800
-+++ agent/mibgroup/ucd-snmp/disk_hw.c	2012-02-19 16:21:29.000000000 -0900
+--- ./agent/mibgroup/ucd-snmp/disk_hw.c.orig	2011-09-28 00:53:47.000000000 -0400
++++ ./agent/mibgroup/ucd-snmp/disk_hw.c	2012-04-04 06:33:38.000000000 -0400
 @@ -137,6 +137,7 @@
    if (disks) {
       free( disks );
@@ -26,3 +26,30 @@
          return ((u_char *) (&long_ret));
      case ERRORNAME:            /* DISKPATH */
          *var_len = strlen(entry->path);
+@@ -388,8 +389,9 @@
+ 
+     case ERRORFLAG:
+         long_ret = 0;
++	val = netsnmp_fsys_avail_ull(entry);
+         if (( entry->minspace >= 0 ) &&
+-            ( entry->avail < entry->minspace ))
++	    ( val < entry->minspace ))
+             long_ret = 1;
+         else if (( entry->minpercent >= 0 ) &&
+                  (_percent( entry->avail, entry->size ) < entry->minpercent ))
+@@ -398,12 +400,13 @@
+ 
+     case ERRORMSG:
+         errmsg[0] = 0;
++	val = netsnmp_fsys_avail_ull(entry);
+         if (( entry->minspace >= 0 ) &&
+-            ( entry->avail < entry->minspace ))
++	    ( val < entry->minspace ))
+                 snprintf(errmsg, sizeof(errmsg),
+                         "%s: less than %d free (= %d)",
+                         entry->path, entry->minspace,
+-                        (int) entry->avail);
++			(int) val);
+         else if (( entry->minpercent >= 0 ) &&
+                  (_percent( entry->avail, entry->size ) < entry->minpercent ))
+                 snprintf(errmsg, sizeof(errmsg),
