cd0e42c405f4922cf1d27fd3404cc9cdd2f37857
decke <decke@FreeBSD.org>
2012-04-05 11:16:31 +0000

    - Update to 4.1.12 - Fix PAM authentication for vboxwebsrv  [1]
    PR:		ports/166614 [1]
    Submitted by:	Landon Fuller <landonf@plausible.coop> [1]
    Feature safe:	yes

---
+++ emulators/virtualbox-ose/files/patch-src-VBox-HostServices-auth-pam-VBoxAuthPAM.c
@@ -0,0 +1,11 @@
+--- src/VBox/HostServices/auth/pam/VBoxAuthPAM.c.orig	2012-04-03 14:11:56.000000000 +0200
++++ src/VBox/HostServices/auth/pam/VBoxAuthPAM.c	2012-04-05 12:44:48.000000000 +0200
+@@ -62,6 +62,8 @@
+ /* The name of the PAM library */
+ # ifdef RT_OS_SOLARIS
+ #  define PAM_LIB_NAME "libpam.so.1"
++# elif defined(RT_OS_FREEBSD)
++#  define PAM_LIB_NAME "libpam.so"
+ # else
+ #  define PAM_LIB_NAME "libpam.so.0"
+ # endif
