3c033f3dd7ef221dc23d80e9479832ec10162a92
pawel <pawel@FreeBSD.org>
2012-04-06 17:09:06 +0000

    - Update to version 1.15.8.12 (squeeze stable branch) - Disable compile warnings, it needs more recent gcc to work - Move manpages declarations to Makefile.man - Remove some useless dirrmtry's of locale dirs,   they are now in /etc/mtree/BSD.local.dist - Use dirrm instead of dirrmtry on dirs specific to this package
    Feature safe:	yes

--- archivers/dpkg/files/patch-utils_start-stop-daemon.c
+++ archivers/dpkg/files/patch-utils_start-stop-daemon.c
@@ -1,14 +1,23 @@
---- utils/start-stop-daemon.c.orig	2009-02-01 14:19:02.000000000 +0800
-+++ utils/start-stop-daemon.c	2009-11-12 07:23:18.000000000 +0800
-@@ -50,6 +50,7 @@
+--- utils/start-stop-daemon.c.orig	2011-11-10 07:17:51.000000000 +0100
++++ utils/start-stop-daemon.c	2012-03-16 20:31:14.000000000 +0100
+@@ -59,6 +59,7 @@
  #endif
  
  #if defined(OSOpenBSD) || defined(OSFreeBSD) || defined(OSNetBSD)
 +#define _WANT_UCRED
  #include <sys/param.h>
  #include <sys/proc.h>
- #include <sys/stat.h>
-@@ -941,8 +942,8 @@
+ 
+@@ -989,7 +990,7 @@
+ 	kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &nentries);
+ 	if (kp == NULL)
+ 		errx(1, "%s", kvm_geterr(kd));
+-	pidexec = (&kp->kp_proc)->p_comm;
++	pidexec = kp->ki_paddr->p_comm;
+ 	if (strlen(name) != strlen(pidexec))
+ 		return false;
+ 	return (strcmp(name, pidexec) == 0) ? 1 : 0;
+@@ -1043,8 +1044,8 @@
  	kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &nentries);
  	if (kp == NULL)
  		errx(1, "%s", kvm_geterr(kd));
@@ -18,13 +27,4 @@
 +		kvm_read(kd, (u_long)&(kp->ki_paddr->p_ucred->cr_ruid),
  		         &proc_uid, sizeof(uid_t));
  	else
- 		return 0;
-@@ -963,7 +964,7 @@
- 	kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &nentries);
- 	if (kp == NULL)
- 		errx(1, "%s", kvm_geterr(kd));
--	pidexec = (&kp->kp_proc)->p_comm;
-+	pidexec = kp->ki_paddr->p_comm;
- 	if (strlen(name) != strlen(pidexec))
- 		return 0;
- 	return (strcmp(name, pidexec) == 0) ? 1 : 0;
+ 		return false;
