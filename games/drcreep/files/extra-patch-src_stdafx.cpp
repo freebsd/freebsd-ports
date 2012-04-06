c27f8ed8edf680cded462d2a596f54b179643c6c
Alex Kozlov <ak@FreeBSD.org>
2012-04-06 16:06:32 +0000

    Fix build on FreeBSD 7 (work with old scandir prototype)
    Submitted by:	pointyhat via pavmail
    Approved by:	eadler (mentor), maintainer (implicit)
    Feature safe:	yes

---
+++ games/drcreep/files/extra-patch-src_stdafx.cpp
@@ -0,0 +1,10 @@
+Index: src/stdafx.cpp
+@@ -364,7 +364,7 @@
+ 
+ string findType;
+ 
+-int file_select(const struct dirent *entry) {
++int file_select(struct dirent *entry) {
+ 	string name = entry->d_name;
+ 
+ 	transform( name.begin(), name.end(), name.begin(), ::toupper );
