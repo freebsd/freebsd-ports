c144b0fb1c9755e5e65c7c44c6f9ebfb576ef375
Michael Scheidell <scheidell@FreeBSD.org>
2012-04-07 16:22:23 +0000

    - Update to 0.2 - The bootstrap compiler needs "cc" to link objects.
    PR:		ports/166551
    Submitted by:	Jyun-Yan You <jyyou@cs.nctu.edu.tw> (maintainer)
    Feature safe:	yes

--- lang/rust/files/patch-mk_platform.mk
+++ lang/rust/files/patch-mk_platform.mk
@@ -1,11 +1,20 @@
---- mk/platform.mk.orig	2012-01-22 08:59:58.097803422 +0800
-+++ mk/platform.mk	2012-01-22 04:45:22.124872578 +0800
-@@ -188,7 +188,7 @@
-   CC=clang
-   CXX=clang++
-   CPP=cpp
+--- mk/platform.mk.orig	2012-04-05 22:34:59.678608449 +0800
++++ mk/platform.mk	2012-04-05 22:35:13.749110633 +0800
+@@ -213,7 +213,7 @@
+   ifeq ($(origin CPP),default)
+     CPP=cpp
+   endif
 -  CFG_GCCISH_CFLAGS += -Wall -Werror -fno-rtti -g
-+  CFG_GCCISH_CFLAGS += -Wall -Werror -Wno-c++11-compat -fno-rtti -g
++  CFG_GCCISH_CFLAGS += -Wall -fno-rtti -g
+   CFG_GCCISH_LINK_FLAGS += -g
+   CFG_DEPEND_C = $(CFG_GCCISH_CROSS)$(CXX) $(CFG_GCCISH_CFLAGS) -MT "$(1)" \
+     -MM $(2)
+@@ -244,7 +244,7 @@
+   ifeq ($(origin CPP),default)
+     CPP=cpp
+   endif
+-  CFG_GCCISH_CFLAGS += -Wall -Werror -fno-rtti -g
++  CFG_GCCISH_CFLAGS += -Wall -fno-rtti -g
    CFG_GCCISH_LINK_FLAGS += -g
    CFG_DEPEND_C = $(CFG_GCCISH_CROSS)$(CXX) $(CFG_GCCISH_CFLAGS) -MT "$(1)" \
      -MM $(2)
