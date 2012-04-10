5c7a749832146c21f386330995201ca75f28006a
miwi <miwi@FreeBSD.org>
2012-04-10 07:27:27 +0000

    - Update to 1.3
    PR:		166419
    Submitted by:	Olivier Duchateau <duchateau.olivier@gmail.com> (maintainer)

--- net/py-urllib3/files/patch-setup.py
+++ net/py-urllib3/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2012-01-30 00:14:40.000000000 +0000
-+++ setup.py	2012-02-07 13:49:58.000000000 +0000
-@@ -44,10 +44,10 @@
+--- setup.py.orig	2012-01-30 01:14:40.000000000 +0100
++++ setup.py	2012-03-26 17:32:14.000000000 +0200
+@@ -44,7 +44,7 @@
        author_email='andrey.petrov@shazow.net',
        url='http://urllib3.readthedocs.org/',
        license='MIT',
@@ -9,8 +9,3 @@
                  'urllib3.packages.ssl_match_hostname', 'urllib3.packages.mimetools_choose_boundary',
                  ],
        requires=requirements,
--      tests_require=tests_requirements,
--      test_suite='test',
-+      #tests_require=tests_requirements,
-+      #test_suite='test',
-       )
