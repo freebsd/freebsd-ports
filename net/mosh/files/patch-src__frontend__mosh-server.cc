04c613685c24f5effa2543bb611b9d2310bdf8bf
Ryan Steinmetz <zi@FreeBSD.org>
2012-04-11 17:05:25 +0000

    - Fix _DEPENDS - Merge in two additional patches from upstream to fix BSD-specific issues - Bump PORTREVISION

--- net/mosh/files/patch-src__frontend__mosh-server.cc
+++ net/mosh/files/patch-src__frontend__mosh-server.cc
@@ -1,5 +1,5 @@
 --- ./src/frontend/mosh-server.cc.orig	2012-04-02 18:08:49.000000000 -0400
-+++ ./src/frontend/mosh-server.cc	2012-04-10 15:29:12.000000000 -0400
++++ ./src/frontend/mosh-server.cc	2012-04-11 06:59:21.000000000 -0400
 @@ -39,6 +39,7 @@
  #include <arpa/inet.h>
  #include <getopt.h>
@@ -8,3 +8,12 @@
  
  extern "C" {
  #include "selfpipe.h"
+@@ -127,7 +128,7 @@
+        && (strcmp( argv[ 1 ], "new" ) == 0) ) {
+     /* new option syntax */
+     int opt;
+-    while ( (opt = getopt( argc, argv, "i:p:c:s" )) != -1 ) {
++    while ( (opt = getopt( argc - 1, argv + 1, "i:p:c:s" )) != -1 ) {
+       switch ( opt ) {
+       case 'i':
+ 	desired_ip = optarg;
