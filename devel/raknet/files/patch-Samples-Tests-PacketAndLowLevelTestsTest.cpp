1b95baf9c06869efcc59cf661dd448cd617fb3c3
Ganael LAPLANCHE <martymac@FreeBSD.org>
2012-04-12 07:31:03 +0000

    - Update devel/raknet to 3.9.2 - Add a note to LEGAL to point out that we have received a special   authorization to redistribute RakNet under GPL v3
    Discussed with: developers@

---
+++ devel/raknet/files/patch-Samples-Tests-PacketAndLowLevelTestsTest.cpp
@@ -0,0 +1,10 @@
+--- Samples/Tests/PacketAndLowLevelTestsTest.cpp.orig	2012-03-21 18:05:53.218690543 +0100
++++ Samples/Tests/PacketAndLowLevelTestsTest.cpp	2012-03-21 18:06:27.805674560 +0100
+@@ -1,6 +1,6 @@
+ #include "PacketAndLowLevelTestsTest.h"
+ 
+-#include "malloc.h"
++#include <stdlib.h>
+ 
+ /*
+ Description:
