4d24c791c4c96f65fb7f389cfe6772fb1c2457c9
Greg Lewis <glewis@FreeBSD.org>
2012-04-13 04:03:42 +0000

    . Instead of bailing when we hit a possibly conflicting variable being   defined just clear it and issue a warning that it was set.
    PR:		165115
    Approved by:	hq@ (co-maintainer)

--- Mk/bsd.java.mk
+++ Mk/bsd.java.mk
@@ -224,8 +224,8 @@ _JDK_FILE=bin/javac
 .		for variable in JAVA_HOME JAVA_PORT JAVA_PORT_VERSION JAVA_PORT_VENDOR JAVA_PORT_OS
 .			if defined(${variable})
 check-makevars::
-	@${ECHO_CMD} "${PKGNAME}: Environment error: \"${variable}\" should not be defined."
-	@${FALSE}
+	@${ECHO_CMD} "${PKGNAME}: Environment error: \"${variable}\" should not be defined -- clearing."
+.undef				${variable}
 .			endif
 .		endfor
 
