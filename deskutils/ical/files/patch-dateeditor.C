8077e1df4354de421a3fe5369a9ed3fe706ccb6b
obrien <obrien@FreeBSD.org>
2012-04-13 22:56:14 +0000

    Update for Tcl/Tk 8.5.
    Obtained from:	http://www.isi.edu/~johnh/SOFTWARE/ICAL/

--- deskutils/ical/files/patch-dateeditor.C
+++ deskutils/ical/files/patch-dateeditor.C
@@ -9,3 +9,32 @@
      if (Tcl_SplitList(tcl, argv[2], &count, &strlist) != TCL_OK) {
  	return TCL_ERROR;
      }
+@@ -148,12 +148,12 @@ int Cmd_HiliteLoop(ClientData, Tcl_Inter
+ 	char buffer[20];
+ 	sprintf(buffer, "%d", list[i].date.EpochDays());
+ 	if (Tcl_SetVar(tcl, dvar, buffer, 0) == NULL) {
+-	    free((char*) strlist);
++	    Tcl_Free((char*) strlist);
+ 	    TCL_Error(tcl, "could not set loop variable");
+ 	}
+ 
+ 	if (Tcl_SetVar(tcl, hvar, (char*)hilite, 0) == NULL) {
+-	    free((char*) strlist);
++	    Tcl_Free((char*) strlist);
+ 	    TCL_Error(tcl, "could not set loop variable");
+ 	}
+ 
+@@ -165,11 +165,11 @@ int Cmd_HiliteLoop(ClientData, Tcl_Inter
+ 	if (result == TCL_BREAK) break;
+ 
+ 	// Error of some sort
+-	free((char*) strlist);
++	Tcl_Free((char*) strlist);
+ 	return result;
+     }
+ 
+-    free((char*) strlist);
++    Tcl_Free((char*) strlist);
+     TCL_Return(tcl, "");
+ }
+ 
