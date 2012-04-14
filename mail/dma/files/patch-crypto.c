f588b4e15681c8bcd846989698d61150216dad94
crees <crees@FreeBSD.org>
2012-04-14 15:49:42 +0000

    Fix a couple of issues:  - Make compile with old and new openssl  - Do not try to call git to get the version number  - Fix compile on FreeBSD 8.1  - Change maintainer email address
    PR:		ports/165775
    Submitted by:	bz

--- mail/dma/files/patch-crypto.c
+++ mail/dma/files/patch-crypto.c
@@ -1,11 +1,14 @@
---- ./crypto.c.orig	2012-01-03 13:54:05.000000000 +0000
-+++ ./crypto.c	2012-01-24 22:57:41.000000000 +0000
-@@ -80,7 +80,7 @@
+--- crypto.c.orig	2012-01-03 13:54:05.000000000 +0000
++++ crypto.c	2012-03-06 23:50:42.000000000 +0000
+@@ -80,7 +80,11 @@ int
  smtp_init_crypto(int fd, int feature)
  {
  	SSL_CTX *ctx = NULL;
--	const SSL_METHOD *meth = NULL;
++#if (OPENSSL_VERSION_NUMBER >= 0x00909000L)
+ 	const SSL_METHOD *meth = NULL;
++#else
 +	SSL_METHOD *meth = NULL;
++#endif
  	X509 *cert;
  	int error;
  
