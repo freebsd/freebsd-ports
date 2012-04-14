9e180c6a27029cba6a7b7ee7bb62d85ab31f428c
Michael Scheidell <scheidell@FreeBSD.org>
2012-04-14 13:29:25 +0000

    - Update to official release 3.0.7 - Update MASTER_SITES (remove unofficial ircd-ratbox.org) - Unbreak cross compiles - Fix error handling with GNUTLS - Update sqllite3 code - Remove buggy sigio code.
    PR:		ports/166914
    Submitted by:	moggie <moggie@elasticmind.net> (maintainer)

--- irc/ircd-ratbox-devel/files/patch-include_config.h
+++ irc/ircd-ratbox-devel/files/patch-include_config.h
@@ -1,13 +1,9 @@
---- ./include/config.h.orig	2008-09-19 16:33:46.000000000 +0100
-+++ ./include/config.h	2008-11-15 02:55:35.000000000 +0000
-@@ -52,16 +52,18 @@
- #define AUTOMODPATH MODULE_DIR "/autoload/"
- #define ETCPATH ETC_DIR
+--- ./include/config.h.orig	2012-04-13 15:02:51.000000000 +0100
++++ ./include/config.h	2012-04-13 15:06:23.000000000 +0100
+@@ -54,15 +54,17 @@
  #define LOGPATH LOG_DIR
--#define UHPATH   HELP_DIR "/users"
--#define HPATH  HELP_DIR "/opers"
-+#define UHPATH	HELP_DIR "/users"
-+#define HPATH	HELP_DIR "/opers"
+ #define UHPATH   HELP_DIR "/users"
+ #define HPATH  HELP_DIR "/opers"
 +#define RUNPATH	"%%RUNDIR%%"
 +#define DBPTH	"%%DBDIR%%"
  
@@ -18,8 +14,9 @@
 +#define DBPATH   DBPTH "/ban.db"
  #define MPATH    ETCPATH "/ircd.motd"	/* MOTD file */
  #define LPATH    LOGPATH "/ircd.log"	/* ircd logfile */
--#define PPATH    ETCPATH "/ircd.pid"	/* pid file */
-+#define PPATH    RUNPATH "/ircd.pid"	/* pid file */
- #define OPATH    ETCPATH "/opers.motd"	/* oper MOTD file */
+ #define PPATH    ETCPATH "/ircd.pid"	/* pid file */
+-#define OPATH    ETCPATH "/opers.motd"	/* oper MOTD file */
++#define OPATH    RUNPATH "/opers.motd"	/* oper MOTD file */
  
  /* HANGONGOODLINK and HANGONGOODLINK
+  * Often net breaks for a short time and it's useful to try to
