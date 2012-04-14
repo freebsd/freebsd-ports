693f9b0cef0db98def75969234509a2972413d17
pawel <pawel@FreeBSD.org>
2012-04-14 18:04:19 +0000

    - Update to version 1.6.1 - Add missing build/lib deps - Add option to build doxygen documentation (default off)

--- multimedia/lives/files/patch-src__htmsocket.c
+++ multimedia/lives/files/patch-src__htmsocket.c
@@ -1,13 +1,11 @@
---- src/htmsocket.c.orig	2008-07-11 15:02:34.000000000 +0900
-+++ src/htmsocket.c	2009-06-03 00:58:22.000000000 +0900
-@@ -9,7 +9,10 @@
- #include <unistd.h>
- #include <netdb.h>
- #include <errno.h>
-+#include <sys/types.h>
- #include <sys/socket.h>
-+#include <netinet/in.h>
-+#include <arpa/inet.h>
- #include <sys/ioctl.h>
+--- src/htmsocket.c.orig	2012-01-17 04:02:52.000000000 +0100
++++ src/htmsocket.c	2012-04-13 23:32:15.000000000 +0200
+@@ -12,6 +12,8 @@
  
- #include "main.h"
+ #include <sys/socket.h>
+ #include <sys/ioctl.h>
++#include <sys/types.h>
++#include <netinet/in.h>
+ 
+ #if defined(__sun)
+ #include <sys/filio.h>
