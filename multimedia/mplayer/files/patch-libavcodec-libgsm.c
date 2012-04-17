649d3ef0ebfae198ec67cfc1dd3636ae97ac0e1c
scheidell <scheidell@FreeBSD.org>
2012-04-17 18:50:38 +0000

    - Update mplayer and memcoder to 2012-03-22 shapshot - Supersedes ports/164943
    PR:		ports/166946
    Submitted by:	Thomas Zander <thomas.e.zander@googlemail.com> (maintainer)

--- multimedia/mplayer/files/patch-libavcodec-libgsm.c
+++ multimedia/mplayer/files/patch-libavcodec-libgsm.c
@@ -1,11 +1,11 @@
---- ffmpeg/libavcodec/libgsm.c.orig	2011-03-12 15:13:38.000000000 +0100
-+++ ffmpeg/libavcodec/libgsm.c	2011-03-18 17:17:26.901267039 +0100
-@@ -28,7 +28,7 @@
+--- ffmpeg/libavcodec/libgsm.c.orig	2012-01-29 19:20:05.000000000 +0100
++++ ffmpeg/libavcodec/libgsm.c	2012-03-22 18:07:58.682722034 +0100
+@@ -27,7 +27,7 @@
+ 
  // The idiosyncrasies of GSM-in-WAV are explained at http://kbs.cs.tu-berlin.de/~jutta/toast.html
  
- #include "avcodec.h"
 -#include <gsm/gsm.h>
 +#include <gsm.h>
  
- // gsm.h misses some essential constants
- #define GSM_BLOCK_SIZE 33
+ #include "avcodec.h"
+ #include "gsm.h"
