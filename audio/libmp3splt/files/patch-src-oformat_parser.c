4fe1a78c7369539d62ed7a364fc227470fca06e2
Chris Rees <crees@FreeBSD.org>
2012-04-18 08:24:07 +0000

    - Update to 0.7.1
    - Fix WITHOUT_ID3 build
    
    PR:		ports/160821
    Submitted by:	Spil <spil.oss@googlemail.com>
    Submitted by:	Anton Yuzhaninov <citrin@citrin.ru> (maintainer)

--- audio/libmp3splt/files/patch-src-oformat_parser.c
+++ audio/libmp3splt/files/patch-src-oformat_parser.c
@@ -1,11 +1,11 @@
---- src/oformat_parser.c.orig	2010-09-07 05:12:05.000000000 +0800
-+++ src/oformat_parser.c	2010-09-29 16:12:11.000000000 +0800
-@@ -760,7 +760,7 @@
-   if (total_time > 0)
+--- src/oformat_parser.c.orig	2011-11-11 22:14:03.000000000 +0100
++++ src/oformat_parser.c	2012-04-11 09:45:52.000000000 +0200
+@@ -301,7 +301,7 @@
+   }
+ 
+   long minutes = total_time / 100 / 60;
+-  int i = (int) (log10l((long double) minutes));
++  int i = (int) (log10(minutes));
+   char number_of_digits = (char) (i + '1');
+   if (number_of_digits == '1')
    {
-     long minutes = total_time / 100 / 60;
--    int i = (int) (log10l((long double) minutes));
-+    int i = (int) (log10(minutes));
-     char number_of_digits = (char) (i + '1');
-     if (number_of_digits == '1')
-     {
