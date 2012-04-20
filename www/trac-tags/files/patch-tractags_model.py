70878dc55567eaa31b7fb9dca554ffee6fffa2d5
brooks <brooks@FreeBSD.org>
2012-04-20 00:10:54 +0000

    Upgrade to the latest version in svn.  Since the port has never installed a wiki_namespace table, remove the braindead code that checks for one and spews two lines of postgres log messages each time the plugin starts (over two lines per second in my slightly crazy enviornment).

---
+++ www/trac-tags/files/patch-tractags_model.py
@@ -0,0 +1,24 @@
+
+$FreeBSD$
+
+--- tractags/model.py.orig
++++ tractags/model.py
+@@ -62,17 +62,7 @@
+         self._upgrade_db(db)
+ 
+     def _need_migration(self, db):
+-        try:
+-            cursor = db.cursor()
+-            cursor.execute("SELECT COUNT(*) FROM wiki_namespace")
+-            cursor.fetchone()
+-            self.env.log.debug("tractags needs to migrate old data")
+-            return True
+-        except Exception, e:
+-            # The expected outcome for any up-to-date installation.
+-            if self.rollback_is_safe:
+-                db.rollback()
+-            return False
++	return False
+ 
+     def _upgrade_db(self, db):
+         try:
