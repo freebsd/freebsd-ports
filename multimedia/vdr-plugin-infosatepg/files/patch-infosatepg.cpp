924c145f00dbffd2cf75c51b012da537e66eeb59
nox <nox@FreeBSD.org>
2012-04-21 14:10:58 +0000

    - Update multimedia/vdr and multimedia/vdr-plugins to 1.7.27 . - Update multimedia/vdr-plugin-epgsearch to 1.0.1s20120322 git snapshot. - Update multimedia/vdr-plugin-femon to 1.7.15 . - Update multimedia/vdr-plugin-iptv to 0.5.0 . - Update multimedia/vdr-plugin-osdteletext to 0.9.2 . - Update multimedia/vdr-plugin-streamdev to 0.5.1p20120311 git snapthot. - Update multimedia/vdr-plugin-xineliboutput to 1.0.90s20111129.1002   cvs snapshot. - Bump PORTREVISION for all other plugins and add vdr 1.7.27   compatibility/bugfix patches where necessary.

--- multimedia/vdr-plugin-infosatepg/files/patch-infosatepg.cpp
+++ multimedia/vdr-plugin-infosatepg/files/patch-infosatepg.cpp
@@ -1,7 +1,14 @@
---- a/infosatepg.cpp
-+++ b/infosatepg.cpp
-@@ -193,7 +193,7 @@ void cPluginInfosatepg::MainThreadHook(v
+--- infosatepg.cpp.orig
++++ infosatepg.cpp
+@@ -190,10 +190,14 @@ void cPluginInfosatepg::MainThreadHook(v
+         if (dev)
+         {
+             if (!dev->ProvidesTransponder(chan)) continue; // device cannot provide transponder -> skip
++#if VDRVERSNUM >= 10726
++            if (statusMonitor->m_LiveView) continue; // EITScanner is updating EPG -> skip
++#else
              if (EITScanner.UsesDevice(dev)) continue; // EITScanner is updating EPG -> skip
++#endif
              if (dev->Receiving()) continue; // device is recording -> skip
              if (dev->IsPrimaryDevice()) continue; // device is primary -> skip
 -            if (cDevice::ActualDevice()->CardIndex()==i) continue; // device is live viewing -> skip
@@ -9,7 +16,7 @@
              if (dev->IsTunedToTransponder(chan))
              {
                  // we already have a device which is tuned (maybe switched manually?)
-@@ -403,7 +403,7 @@ cString cPluginInfosatepg::SVDRPCommand(
+@@ -403,7 +407,7 @@ cString cPluginInfosatepg::SVDRPCommand(
          cString head2;
          head2="\n" \
                "      |        | missed  |            |            | unlocated\n" \
--- multimedia/vdr/files/patch-dvbdevice.h
+++ multimedia/vdr/files/patch-dvbdevice.h
@@ -1,11 +1,11 @@
 --- dvbdevice.h.orig
 +++ dvbdevice.h
-@@ -19,7 +19,7 @@
- #error VDR requires Linux DVB driver API version 5.0 or higher!
+@@ -18,7 +18,7 @@
+ #error VDR requires Linux DVB driver API version 5.3 or higher!
  #endif
  
 -#define MAXDVBDEVICES  8
 +#define MAXDVBDEVICES  63
+ #define MAXDELIVERYSYSTEMS 8
  
  #define DEV_VIDEO         "/dev/video"
- #define DEV_DVB_ADAPTER   "/dev/dvb/adapter"
