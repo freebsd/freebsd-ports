75ce82c3abdc7a18ee6297680167dbbc6ad140b0
amdmi3 <amdmi3@FreeBSD.org>
2012-04-23 14:02:10 +0000

    - Update to 1.7.0 - Use canonical names for patch files - Take maintainership (1)
    PR:		167181
    Submitted by:	myself
    Approved by:	maintainer via private email (1)

--- games/instead/files/patch-configure.sh
+++ games/instead/files/patch-configure.sh
@@ -1,24 +1,6 @@
---- configure.sh.orig	2012-02-09 18:55:00.127534293 +0400
-+++ configure.sh	2012-02-09 18:58:52.803532634 +0400
-@@ -131,14 +131,14 @@
- echo -n "Checking iconv...("
- echo -n "$cc /tmp/iconv-test.c -o iconv-test)..."
- 
--if $cc /tmp/iconv-test.c -o /tmp/iconv-test; then
-+if $cc /tmp/iconv-test.c -o /tmp/iconv-test >/dev/null 2>&1; then
- 	CFLAGS="$CFLAGS -D_HAVE_ICONV"
- 	echo "ok"
--elif $cc /tmp/iconv-test.c -liconv -o /tmp/iconv-test; then
-+elif $cc /tmp/iconv-test.c -liconv -o /tmp/iconv-test >/dev/null 2>&1; then
- 	CFLAGS="$CFLAGS -D_HAVE_ICONV"
- 	LDFLAGS="$LDFLAGS -liconv"
- 	echo "ok, with -liconv"
--elif $cc /tmp/iconv-test.c -I/usr/local/include -L/usr/local/lib -liconv -o /tmp/iconv-test; then
-+elif $cc /tmp/iconv-test.c -I/usr/local/include -L/usr/local/lib -liconv -o /tmp/iconv-test >/dev/null 2>&1; then
- 	CFLAGS="$CFLAGS -I/usr/local/include -D_HAVE_ICONV"
- 	LDFLAGS="$LDFLAGS -L/usr/local/lib -liconv"
- 	echo "ok, with -liconv and -L/usr/local/lib"
-@@ -183,26 +183,7 @@
+--- configure.sh.orig	2012-04-21 18:53:11.000000000 +0400
++++ configure.sh	2012-04-22 03:17:17.136235398 +0400
+@@ -192,26 +192,7 @@
  echo "SDL_CFLAGS=\$(shell sdl-config --cflags)" >> config.make
  echo "SDL_LFLAGS=\$(shell sdl-config --libs) -lSDL_ttf -lSDL_mixer -lSDL_image" >> config.make
  echo "ok"
@@ -46,7 +28,7 @@
  	
  	rm -f Rules.make
  	ln -s Rules.make.system Rules.make
-@@ -218,16 +199,12 @@
+@@ -227,16 +208,12 @@
  	echo "ICONPATH=\$(DESTDIR)\$(PREFIX)/share/pixmaps" >> config.make
  	echo "DOCPATH=\$(DESTDIR)\$(PREFIX)/share/doc/instead" >> config.make
  	echo "LANGPATH=\$(DATAPATH)/lang" >> config.make
