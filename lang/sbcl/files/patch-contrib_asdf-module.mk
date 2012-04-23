0eb8a47869c51f4daeb8b0d253e3e070df076a6f
Brendan Fabeny <bf@FreeBSD.org>
2012-04-23 09:27:42 +0000

    update to 1.0.56 and adjust dependent ports; switch to the packaged html documents, to avoid problems with our texinfo tools; pass prefix, dynamic space size, and lisp bootstrap via make.sh options; update flags and use them in tests; remove unused patches; simplify and sort some variables and options-handling; shorten some long lines

--- lang/sbcl/files/patch-contrib_asdf-module.mk
+++
@@ -1,8 +0,0 @@
---- contrib/asdf-module.mk.orig	2011-09-21 17:44:49.000000000 -0700
-+++ contrib/asdf-module.mk	2011-09-21 17:45:24.000000000 -0700
-@@ -1,4 +1,4 @@
--CC=cc
-+CC?=cc
- 
- # We need to extend flags to the C compiler and the linker
- # here. sb-posix, sb-grovel, and sb-bsd-sockets depends upon these
--- lang/sbcl/files/patch-src_runtime_gencgc-internal.h
+++
@@ -1,13 +0,0 @@
---- src/runtime/gencgc-internal.h.orig	2011-09-21 17:45:32.000000000 -0700
-+++ src/runtime/gencgc-internal.h	2011-09-21 17:46:00.000000000 -0700
-@@ -25,8 +25,8 @@
- #include "genesis/code.h"
- 
- void gc_free_heap(void);
--inline page_index_t find_page_index(void *);
--inline void *page_address(page_index_t);
-+extern page_index_t find_page_index(void *);
-+extern void *page_address(page_index_t);
- int gencgc_handle_wp_violation(void *);
- 
- 
