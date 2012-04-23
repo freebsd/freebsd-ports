d6983f55ae523948819e16f7f6f934f5485647b3
Guido Falsi <madpilot@FreeBSD.org>
2012-04-23 17:49:24 +0000

    - Add CakePHP 2.1.1 - Connect to the build
    PR:		ports/167099
    Submitted by:	Christoph Theis <theis@gmx.at> (maintainer)
    Approved by:	crees (mentor)

--- www/cakephp2/files/extra-patch-app__Config__core.php
+++ www/cakephp2/files/extra-patch-app__Config__core.php
@@ -1,6 +1,6 @@
---- ./app/config/core.php.orig	2011-07-26 13:46:14.000000000 -0400
-+++ ./app/config/core.php	2011-09-16 14:24:26.000000000 -0400
-@@ -66,7 +66,7 @@
+--- app/Config/core.php.orig	2012-02-20 18:12:19.000000000 +0100
++++ app/Config/core.php	2012-02-20 18:12:37.000000000 +0100
+@@ -92,7 +92,7 @@
   *
   * And uncomment the App.baseUrl below:
   */
