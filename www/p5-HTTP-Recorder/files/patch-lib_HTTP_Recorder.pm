f2a5392b233c819757a47a122c9d3fc760133df6
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2012-04-24 19:17:53 +0000

    - Update to 0.06 - Use USE_DOS2UNIX to remove trailing ^M
    Changes:	http://search.cpan.org/dist/HTTP-Recorder/CHANGES

--- www/p5-HTTP-Recorder/files/patch-lib_HTTP_Recorder.pm
+++ www/p5-HTTP-Recorder/files/patch-lib_HTTP_Recorder.pm
@@ -1,25 +1,22 @@
-
 $FreeBSD$
 
---- lib/HTTP/Recorder.pm.orig
-+++ lib/HTTP/Recorder.pm
-@@ -344,12 +344,13 @@
-     # get rid of the arguments we added
-     my $prefix = $self->{prefix};
+--- lib/HTTP/Recorder.pm.orig	2012-04-23 22:20:16.000000000 +0800
++++ lib/HTTP/Recorder.pm	2012-04-25 01:11:56.207852080 +0800
+@@ -352,11 +352,12 @@
+ 		return $content;
+ 	}
  
 -    for my $key ($content->query_param) {
 -	if ($key =~ /^$prefix-/) {
 -	    $content->query_param_delete($key);
 -	}
 -    }
--    return $content;
-+	$content =~ s/$prefix-(.*?)\?(.*?)&//g;
-+	$content =~ s/$prefix-(.*?)&//g;
-+	$content =~ s/$prefix-(.*?)$//g;
-+	$content =~ s/&$//g;
-+	$content =~ s/\?$//g;
++    $content =~ s/$prefix-(.*?)\?(.*?)&//g;
++    $content =~ s/$prefix-(.*?)&//g;
++    $content =~ s/$prefix-(.*?)$//g;
++    $content =~ s/&$//g;
++    $content =~ s/\?$//g;
 +
-+	return $content;
+     return $content;
  }
  
- sub extract_values {
