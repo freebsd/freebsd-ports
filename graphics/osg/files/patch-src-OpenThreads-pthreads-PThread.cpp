eb22aa0d9c59142a4264f5cb822f05e830649ce7
Dmitry Marakasov <amdmi3@FreeBSD.org>
2012-04-26 00:47:48 +0000

    - Update graphics/osg to 3.0.1 - Bump revisions on dependent ports due to shlib version bump - Remove option to use graphics/osg-devel instead of graphics/osg from simgear and flightgear (1) - Remove option to use graphics/osg-devel instead of graphics/osg from fgrun for consistency
    Submitted by:	martymac (1)

--- graphics/osg/files/patch-src-OpenThreads-pthreads-PThread.cpp
+++ graphics/osg/files/patch-src-OpenThreads-pthreads-PThread.cpp
@@ -1,14 +1,14 @@
---- src/OpenThreads/pthreads/PThread.c++.orig	2008-11-05 13:27:49.000000000 +0300
-+++ src/OpenThreads/pthreads/PThread.c++	2009-02-04 18:06:28.000000000 +0300
+--- src/OpenThreads/pthreads/PThread.cpp.orig	2011-06-24 00:09:26.000000000 +0400
++++ src/OpenThreads/pthreads/PThread.cpp	2012-03-05 05:57:02.886704932 +0400
 @@ -21,6 +21,7 @@
  #include <sys/types.h>
  #include <unistd.h>
  #include <pthread.h>
 +#include <pthread_np.h>
+ #include <limits.h>
  
- #if defined __linux || defined __sun || defined __APPLE__
- #include <string.h>
-@@ -125,7 +126,7 @@
+ #if defined __linux || defined __sun || defined __APPLE__ || ANDROID
+@@ -136,7 +137,7 @@
  #if defined(__sgi)
              pthread_setrunon_np( pd->cpunum );
  #elif defined(HAVE_PTHREAD_SETAFFINITY_NP) || defined(HAVE_THREE_PARAM_SCHED_SETAFFINITY) || defined(HAVE_TWO_PARAM_SCHED_SETAFFINITY)
@@ -17,7 +17,7 @@
              CPU_ZERO( &cpumask );
              CPU_SET( pd->cpunum, &cpumask );
  
-@@ -541,7 +542,7 @@
+@@ -569,7 +570,7 @@
  
      if (pd->isRunning && Thread::CurrentThread()==this)
      {
@@ -26,7 +26,7 @@
          CPU_ZERO( &cpumask );
          CPU_SET( pd->cpunum, &cpumask );
  #if defined(HAVE_PTHREAD_SETAFFINITY_NP)
-@@ -975,7 +976,7 @@
+@@ -1031,7 +1032,7 @@
      else
      {
  #if defined(HAVE_PTHREAD_SETAFFINITY_NP) || defined(HAVE_THREE_PARAM_SCHED_SETAFFINITY) || defined(HAVE_TWO_PARAM_SCHED_SETAFFINITY)
@@ -34,4 +34,4 @@
 +        cpuset_t cpumask;
          CPU_ZERO( &cpumask );
          CPU_SET( cpunum, &cpumask );
- 
+ #if defined(HAVE_PTHREAD_SETAFFINITY_NP)
