d2d112631bb7fea2eb01ed2b043d0b952b9dfbaf
Nicola Vitale <nivit@FreeBSD.org>
2012-04-28 13:32:04 +0000

    - Update to 12.02 - Remove http://nivi.interfree.it from MASTER_SITES - Set USE_PYTHON to -2.7 - The man pages installation is independent from the value of NOPORTDOCS

--- audio/mma/files/patch-mma.py
+++ audio/mma/files/patch-mma.py
@@ -1,9 +1,10 @@
---- ./mma.py.orig	2011-02-27 11:14:24.000000000 +0100
-+++ ./mma.py	2011-02-27 11:17:00.000000000 +0100
+$FreeBSD$
+--- ./mma.py.orig	2012-04-28 14:45:21.000000000 +0200
++++ ./mma.py	2012-04-28 14:46:09.000000000 +0200
 @@ -67,6 +67,9 @@
      dirlist = ( sys.path[0], "/Users/Shared/mma", 
               "/usr/local/share/mma", "/usr/share/mma", '.' )
-     midiPlayer = ['']   # must be a list!
+     midiPlayer = ['open']   # must be a list!
 +elif platform == 'FreeBSD':
 +    dirlist = ( sys.path[0], '%%DATADIR%%', '.' )
 +    midiPlayer = ['timidity', 'playmidi']
