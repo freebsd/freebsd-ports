59c61f7945e0e85b4a0e7bad0d8c861ec9245dd3
scheidell <scheidell@FreeBSD.org>
2012-04-30 22:57:38 +0000

    - Update chinese/fcitx-googlepinyin to 0.1.4 - Update chinese/fcitx-configtool to 0.4.0 - Update chinese/fcitx-cloudpinyin to 0.2.0 - Update chinese/fcitx-sunpinyin to 0.3.3 - Update chinese/fcitx to 4.2.2 - Update textproc/fcitx-keyboard to 0.1.0
    PR:		ports/166689
    Submitted by:	Zhihao Yuan <lichray@gmail.com> (maintainer)

--- chinese/fcitx-googlepinyin/files/patch-src_eim.cpp
+++ chinese/fcitx-googlepinyin/files/patch-src_eim.cpp
@@ -1,11 +1,11 @@
---- src/eim.cpp~	2012-01-31 09:03:36.000000000 -0600
-+++ src/eim.cpp	2012-02-06 02:13:29.145600582 -0600
-@@ -466,7 +466,7 @@ void* FcitxGooglePinyinCreate (FcitxInst
-     if (fp)
-         fclose(fp);
- 
--    bool result = ime_pinyin::im_open_decoder(LIBDIR "/googlepinyin/data/dict_pinyin.dat", userDict);
-+    bool result = ime_pinyin::im_open_decoder(DATADIR "/googlepinyin/dict_pinyin.dat", userDict);
- 
-     if (userDict)
-         free(userDict);
+--- src/eim.cpp.orig	2012-03-08 22:07:27.000000000 -0600
++++ src/eim.cpp	2012-04-06 02:04:15.502210235 -0500
+@@ -476,7 +476,7 @@ void* FcitxGooglePinyinCreate (FcitxInst
+     else 
+ #endif
+     {
+-        syspath = strdup(GOOGLEPINYIN_LIBDIR "/googlepinyin/data/dict_pinyin.dat");
++        syspath = strdup(GOOGLEPINYIN_LIBDIR "/../share/googlepinyin/dict_pinyin.dat");
+     }
+     bool result = ime_pinyin::im_open_decoder(syspath, userDict);
+     free(syspath);
