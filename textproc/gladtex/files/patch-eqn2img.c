5800aafe793141a7cc7d3244c6c8b2702acce58f
nivit <nivit@FreeBSD.org>
2012-05-01 20:29:46 +0000

    - Update to 1.2 - Set MASTER_SITES to Sourceforge - Migrate from libungif to gif  [1] - Replace USE_PERL5_RUN with USE_PERL5 (Perl required to create the man page) - Remove post-extract and post-install targets - Add do-install target - Install man page of gladtex - Update the URL of the WWW site
    PR:		ports/165376
    Submitted by:	swills  [1]

--- textproc/gladtex/files/patch-eqn2img.c
+++ textproc/gladtex/files/patch-eqn2img.c
@@ -1,10 +1,9 @@
-$FreeBSD$
---- eqn2img.c	2007-10-05 17:51:48.000000000 +0200
-+++ eqn2img.c.port	2007-12-19 14:08:52.000000000 +0100
-@@ -691,7 +691,7 @@
+--- ./eqn2img.c.orig	2012-05-01 18:17:08.000000000 +0200
++++ ./eqn2img.c	2012-05-01 18:18:00.000000000 +0200
+@@ -715,7 +715,7 @@
      fprintf(stderr, " -> ps");
  
-   cmd = NEW(char, 2*strlen(basename) + 35);
+   cmd = NEW(char, 2*strlen(basename) + 38);
 -  sprintf(cmd, "dvips -E -o %s.ps %s.dvi > /dev/null 2>&1", basename, basename);
 +  sprintf(cmd, "dvips -q -E -o %s.ps %s.dvi > /dev/null 2>&1", basename, basename);
    if(system(cmd)) {
