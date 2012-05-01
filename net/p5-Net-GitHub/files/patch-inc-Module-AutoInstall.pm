9dc7d0ea0fcf0b07cc1bb2b9af613159a0c1c85d
Steve Wills <swills@FreeBSD.org>
2012-05-01 02:41:11 +0000

    - Update to 0.44
    PR:		ports/167337

--- net/p5-Net-GitHub/files/patch-inc-Module-AutoInstall.pm
+++ net/p5-Net-GitHub/files/patch-inc-Module-AutoInstall.pm
@@ -1,11 +1,11 @@
---- inc/Module/AutoInstall.pm.orig	2009-06-14 08:58:39.000000000 +0800
-+++ inc/Module/AutoInstall.pm	2009-07-03 08:02:48.000000000 +0800
-@@ -125,7 +125,7 @@
+--- inc/Module/AutoInstall.pm.orig	2012-04-26 12:38:21.028164683 -0400
++++ inc/Module/AutoInstall.pm	2012-04-26 12:38:39.733163954 -0400
+@@ -138,7 +138,7 @@
      # check entirely since we don't want to have to load (and configure)
      # an old CPAN just for a cosmetic message
  
--    $UnderCPAN = _check_lock(1) unless $SkipInstall;
-+#    $UnderCPAN = _check_lock(1) unless $SkipInstall;
+-    $UnderCPAN = _check_lock(1) unless $SkipInstall || $InstallDepsTarget;
++#    $UnderCPAN = _check_lock(1) unless $SkipInstall || $InstallDepsTarget;
  
      while ( my ( $feature, $modules ) = splice( @args, 0, 2 ) ) {
          my ( @required, @tests, @skiptests );
