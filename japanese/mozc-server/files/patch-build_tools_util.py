16a4e75bb915567955e06d0545227a07c017a7ca
daichi <daichi@FreeBSD.org>
2012-05-01 06:41:06 +0000

    added a needed patch file

---
+++ japanese/mozc-server/files/patch-build_tools_util.py
@@ -0,0 +1,11 @@
+--- build_tools/util.py.orig	2012-05-01 12:21:08.135692337 +0900
++++ build_tools/util.py	2012-05-01 13:03:43.227693771 +0900
+@@ -54,7 +54,7 @@
+ 
+ def IsLinux():
+   """Returns true if the platform is Linux."""
+-  return os.name == 'posix' and os.uname()[0] == 'Linux'
++  return os.name == 'posix' and ( os.uname()[0] == 'Linux' or os.uname()[0] == 'FreeBSD' )
+ 
+ 
+ def GetNumberOfProcessors():
