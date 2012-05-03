47e9ca454be17e23cd80cd7913005d6f349bd911
rakuco <rakuco@FreeBSD.org>
2012-05-03 02:14:57 +0000

    Update to 2.8.8.
    It includes an upstream patch which fixes the FindPkgConfig.cmake
    module. It will be part of 2.8.9.
    
    PR:		ports/167140
    Approved by:	portmgr (pav via exp-run)

--- devel/cmake/files/patch-Modules_FindQt4.cmake
+++ devel/cmake/files/patch-Modules_FindQt4.cmake
@@ -1,6 +1,6 @@
---- ./Modules/FindQt4.cmake.orig	2010-11-03 22:58:27.000000000 +0300
-+++ ./Modules/FindQt4.cmake	2010-11-23 18:09:33.486039056 +0300
-@@ -461,7 +461,7 @@
+--- ./Modules/FindQt4.cmake.orig	2011-12-30 17:49:56.000000000 +0100
++++ ./Modules/FindQt4.cmake	2012-04-07 18:45:53.442654169 +0200
+@@ -473,7 +473,7 @@
  # check for qmake
  # Debian uses qmake-qt4
  # macports' Qt uses qmake-mac
@@ -9,7 +9,7 @@
    "[HKEY_CURRENT_USER\\Software\\Trolltech\\Qt3Versions\\4.0.0;InstallDir]/bin"
    "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\4.0.0;InstallDir]/bin"
    "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\${qt_install_version};InstallDir]/bin"
-@@ -705,13 +705,7 @@
+@@ -665,13 +665,7 @@
    # ask qmake for the plugins directory
    IF (QT_LIBRARY_DIR AND NOT QT_PLUGINS_DIR  OR  QT_QMAKE_CHANGED)
      _qt4_query_qmake(QT_INSTALL_PLUGINS qt_plugins_dir)
@@ -24,3 +24,20 @@
    ENDIF (QT_LIBRARY_DIR AND NOT QT_PLUGINS_DIR  OR  QT_QMAKE_CHANGED)
  
    # ask qmake for the translations directory
+@@ -684,15 +678,7 @@
+   IF (QT_LIBRARY_DIR AND NOT QT_IMPORTS_DIR OR QT_QMAKE_CHANGED)
+     _qt4_query_qmake(QT_INSTALL_IMPORTS qt_imports_dir)
+     if(qt_imports_dir)
+-      SET(QT_IMPORTS_DIR NOTFOUND)
+-      foreach(qt_cross_path ${CMAKE_FIND_ROOT_PATH})
+-        set(qt_cross_paths ${qt_cross_paths} "${qt_cross_path}/imports")
+-      endforeach(qt_cross_path)
+-      FIND_PATH(QT_IMPORTS_DIR NAMES Qt
+-        HINTS ${qt_cross_paths} ${qt_imports_dir}
+-        DOC "The location of the Qt imports"
+-        NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH
+-        NO_CMAKE_SYSTEM_PATH)
++      SET(QT_IMPORTS_DIR ${qt_imports_dir} CACHE PATH "The location of the Qt imports" FORCE)
+       mark_as_advanced(QT_IMPORTS_DIR)
+     endif(qt_imports_dir)
+   ENDIF (QT_LIBRARY_DIR AND NOT QT_IMPORTS_DIR  OR  QT_QMAKE_CHANGED)
