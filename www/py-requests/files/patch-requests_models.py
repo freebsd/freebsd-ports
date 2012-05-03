d0844f0028961e871edef163b037416ada9b34f5
miwi <miwi@FreeBSD.org>
2012-05-03 16:16:46 +0000

    - Update to 0.11.2
    PR:		167243
    Submitted by:	Olivier Duchateau <duchateau.olivier@gmail.com> (maintainer)

--- www/py-requests/files/patch-requests_models.py
+++ www/py-requests/files/patch-requests_models.py
@@ -1,17 +1,17 @@
---- requests/models.py.orig	2012-03-09 17:51:20.000000000 +0100
-+++ requests/models.py	2012-03-19 17:49:25.000000000 +0100
+--- requests/models.py.orig	2012-04-23 04:13:53.000000000 +0200
++++ requests/models.py	2012-04-23 18:22:55.000000000 +0200
 @@ -15,12 +15,12 @@
  from .status_codes import codes
  
  from .auth import HTTPBasicAuth, HTTPProxyAuth
 -from .packages.urllib3.response import HTTPResponse
--from .packages.urllib3.exceptions import MaxRetryError
+-from .packages.urllib3.exceptions import MaxRetryError, LocationParseError
 -from .packages.urllib3.exceptions import SSLError as _SSLError
 -from .packages.urllib3.exceptions import HTTPError as _HTTPError
 -from .packages.urllib3 import connectionpool, poolmanager
 -from .packages.urllib3.filepost import encode_multipart_formdata
 +from urllib3.response import HTTPResponse
-+from urllib3.exceptions import MaxRetryError
++from urllib3.exceptions import MaxRetryError, LocationParseError
 +from urllib3.exceptions import SSLError as _SSLError
 +from urllib3.exceptions import HTTPError as _HTTPError
 +from urllib3 import connectionpool, poolmanager
