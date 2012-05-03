3ceed877433caa8e59b7a0799d6c1bc659372080
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2012-05-03 02:14:57 +0000

    Update to 2.8.8.
    It includes an upstream patch which fixes the FindPkgConfig.cmake
    module. It will be part of 2.8.9.
    
    PR:		ports/167140
    Approved by:	portmgr (pav via exp-run)

--- devel/cmake/files/patch-Modules_FindSDL.cmake
+++ devel/cmake/files/patch-Modules_FindSDL.cmake
@@ -1,19 +1,19 @@
---- Modules/FindSDL.cmake.orig	2009-11-13 21:32:52.000000000 +0300
-+++ Modules/FindSDL.cmake	2010-01-28 17:32:40.000000000 +0300
-@@ -81,6 +81,11 @@
-   /opt/csw # Blastwave
+--- Modules/FindSDL.cmake.orig	2012-04-18 22:24:55.000000000 -0300
++++ Modules/FindSDL.cmake	2012-04-18 22:26:06.000000000 -0300
+@@ -82,6 +82,11 @@
    /opt
  )
-+#On FreeBSD SDL depends on libiconv and SDL_stdinc.h includes iconv.h, which is
-+#located in ${LOCALBASE}/include. Append {LOCALBASE}/include to
-+#the SDL_INCLUDE_DIR, thus allow to build SDL apps out of box.
+ 
++# On FreeBSD SDL depends on libiconv and SDL_stdinc.h includes iconv.h, which is
++# located in ${LOCALBASE}/include. Append {LOCALBASE}/include to
++# the SDL_INCLUDE_DIR, thus allow to build SDL apps out of box.
 +LIST(APPEND SDL_INCLUDE_DIR /usr/local/include)
 +
- #MESSAGE("SDL_INCLUDE_DIR is ${SDL_INCLUDE_DIR}")
- 
  # SDL-1.1 is the name used by FreeBSD ports...
-@@ -100,7 +105,7 @@
- #MESSAGE("SDL_LIBRARY_TEMP is ${SDL_LIBRARY_TEMP}")
+ # don't confuse it for the version number.
+ FIND_LIBRARY(SDL_LIBRARY_TEMP 
+@@ -97,7 +102,7 @@
+ )
  
  IF(NOT SDL_BUILDING_LIBRARY)
 -  IF(NOT ${SDL_INCLUDE_DIR} MATCHES ".framework")
@@ -21,7 +21,7 @@
      # Non-OS X framework versions expect you to also dynamically link to 
      # SDLmain. This is mainly for Windows and OS X. Other (Unix) platforms 
      # seem to provide SDLmain for compatibility even though they don't
-@@ -116,7 +121,7 @@
+@@ -113,7 +118,7 @@
        /opt/csw
        /opt
      )
