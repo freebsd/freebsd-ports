aa8976055056c41a1eb0da887a934c6b9fde663b
Brendan Fabeny <bf@FreeBSD.org>
2012-05-04 20:56:15 +0000

    - update math/R-cran-psych to 1.2.1 [1] - update math/R to 2.15.0, and adjust dependent ports - minor changes to bsd.cran.mk [2]: rename MASTER_CRAN_SITES to   MASTER_SITE_CRAN, as in bsd.sites.mk; make the install target more   flexible and allow CRAN ports to override it; add a regression-test   target; set USE_FORTRAN to match math/R; remove some of the   redundant checks of USE_R_MOD; honor NOPORTDATA and NOPORTDOCS
    Reviewed by:	thierry, tota, wen
    Approved by:	D. Rue (maintainer) [1], wen [2]

--- Mk/bsd.cran.mk
+++ Mk/bsd.cran.mk
@@ -6,8 +6,7 @@
 
 CRAN_Include_MAINTAINER=	wen@FreeBSD.org
 
-.if defined(USE_R_MOD)
-MASTER_CRAN_SITES+=	http://ftp.ctex.org/mirrors/CRAN/src/contrib/ \
+MASTER_SITE_CRAN+=	http://ftp.ctex.org/mirrors/CRAN/src/contrib/ \
 			http://cran.rakanu.com/src/contrib/ \
 			http://cran.ms.unimelb.edu.au/src/contrib/ \
 			http://mirror.its.dal.ca/cran/src/contrib/ \
@@ -18,16 +17,14 @@ MASTER_CRAN_SITES+=	http://ftp.ctex.org/mirrors/CRAN/src/contrib/ \
 			http://cran.md.tsukuba.ac.jp/src/contrib/ \
 			http://mirrors.ibiblio.org/pub/mirrors/CRAN/src/contrib/ \
 			http://cran.cnr.berkeley.edu/src/contrib/
-.endif
 
-MASTER_SITES?=	${MASTER_CRAN_SITES}
+MASTER_SITES?=	${MASTER_SITE_CRAN}
 
-.if defined(USE_R_MOD)
+USE_FORTRAN=	yes
 BUILD_DEPENDS+=	${LOCALBASE}/bin/R:${PORTSDIR}/math/R
 RUN_DEPENDS+=	${LOCALBASE}/bin/R:${PORTSDIR}/math/R
 
 PKGNAMEPREFIX?=	R-cran-
-.endif
 
 R_MOD_DIR?=	lib/R/library/${PORTNAME}
 PLIST_SUB+=	R_MOD_DIR=${R_MOD_DIR}
@@ -35,14 +32,38 @@ WRKSRC?=	${WRKDIR}/${PORTNAME}
 
 NO_BUILD=	yes
 R_COMMAND=	${LOCALBASE}/bin/R
-R_INS=		${R_COMMAND} CMD INSTALL
 
-do-install:
-.if defined(USE_R_MOD)
-	@cd ${WRKDIR} && ${SETENV} ${MAKE_ENV} ${R_INS} ${PORTNAME}
+.if !target(regression-test)
+R_POSTCMD_CHECK_OPTIONS?=	--timings
+
+.if !exists(${LOCALBASE}/bin/pdflatex)
+R_POSTCMD_CHECK_OPTIONS+=	--no-manual --no-rebuild-vignettes
 .endif
 
-.if defined(USE_R_MOD) && defined(R_MOD_AUTOPLIST)
+regression-test: build
+	@cd ${WRKDIR} ; ${SETENV} ${MAKE_ENV} _R_CHECK_FORCE_SUGGESTS_=FALSE \
+	${R_COMMAND} ${R_PRECMD_CHECK_OPTIONS} CMD check \
+	${R_POSTCMD_CHECK_OPTIONS} ${PORTNAME}
+.endif
+
+.if !target(do-install)
+.if defined(NOPORTDATA)
+R_POSTCMD_INSTALL_OPTIONS+=	--no-data --no-demo
+.else
+R_POSTCMD_INSTALL_OPTIONS+=	--install-tests
+.endif
+
+.if defined(NOPORTDOCS)
+R_POSTCMD_INSTALL_OPTIONS+=	--no-docs --no-html
+.endif
+
+do-install:
+	@cd ${WRKDIR} ; ${SETENV} ${MAKE_ENV} ${R_COMMAND} \
+	${R_PRECMD_INSTALL_OPTIONS} CMD INSTALL \
+	${R_POSTCMD_INSTALL_OPTIONS} ${PORTNAME}
+.endif
+
+.if defined(R_MOD_AUTOPLIST)
 .if !target(post-install-script)
 post-install-script:
 	@${FIND} -ds ${PREFIX}/${R_MOD_DIR} \( -type f -or -type l \) -print | \
