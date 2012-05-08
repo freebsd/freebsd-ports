0ffb533bc20f1581a7c3c2375b55fe1655e362fd
makc <makc@FreeBSD.org>
2012-05-08 23:02:44 +0000

    Update to 0.4 for KDE 4 Take maintainership

---
+++ databases/kbibtex/files/patch-src__gui__bibtex__bibtexfileview.h
@@ -0,0 +1,10 @@
+--- ./src/gui/bibtex/bibtexfileview.h.orig	2012-05-05 10:50:42.000000000 +0000
++++ ./src/gui/bibtex/bibtexfileview.h	2012-05-06 00:53:34.029244696 +0000
+@@ -21,6 +21,7 @@
+ #define KBIBTEX_GUI_BIBTEXFILEVIEW_H
+ 
+ #include <QTreeView>
++#include <KMimeType>
+ 
+ #include <kbibtexgui_export.h>
+ 
