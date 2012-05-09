c4595ec690548aa4d18043c1d7883ad9040d9c58
ak <ak@FreeBSD.org>
2012-05-09 02:20:13 +0000

    - Update to version 111.2.2 - Use PORTDATA instead of a long plist - Remove plist files
    PR:	ports/167529
    Submitted by:	Tijl Coosemans <tijl@coosemans.org>
    Approved by:	eadler (mentor)

---
+++ games/simutrans/files/patch-utils-csv.cc
@@ -0,0 +1,10 @@
+--- utils/csv.cc.orig	2012-03-27 18:26:57.000000000 +0200
++++ utils/csv.cc	2012-03-27 18:29:14.000000000 +0200
+@@ -7,6 +7,7 @@
+ 
+ #include <cstring>
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <assert.h>
+ 
+ #include "csv.h"
