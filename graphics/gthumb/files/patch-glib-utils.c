81e6ab5e41dc949144fc8af583597c5680fb6a93
pav <pav@FreeBSD.org>
2012-05-10 14:36:17 +0000

    - Add a dependency on yelp to enable help system - Cosmetic change to glib patch
    PR:		ports/167725, ports/167726
    Submitted by:	Ronald F.Guilmette <rfg@tristatelogic.com>

--- graphics/gthumb/files/patch-glib-utils.c
+++ graphics/gthumb/files/patch-glib-utils.c
@@ -1,11 +1,19 @@
 --- gthumb/glib-utils.c.orig	2011-11-03 21:36:36.000000000 +0100
-+++ gthumb/glib-utils.c	2011-12-29 15:30:12.000000000 +0100
-@@ -552,7 +552,7 @@
++++ gthumb/glib-utils.c	2012-05-10 15:03:34.000000000 +0200
+@@ -552,7 +552,6 @@
  {
  	int offset;
  
 -	offset = -timezone;
-+	offset = 0;
  #if defined (HAVE_TM_GMTOFF)
  	offset = tm->tm_gmtoff;
  #elif defined (HAVE_TIMEZONE)
+@@ -564,6 +563,8 @@
+   #endif
+ 	} else
+ 		offset = -timezone;
++#else
++	offset = 0;
+ #endif
+ 
+ 	return offset;
