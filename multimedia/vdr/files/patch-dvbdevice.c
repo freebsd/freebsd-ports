2a6dfb7ae63afca2409854842b0f62c383a26937
Juergen Lock <nox@FreeBSD.org>
2012-05-11 17:17:24 +0000

    - Further fixes to the signal quality display, especially SNR. - Bump PORTREVISION.

--- multimedia/vdr/files/patch-dvbdevice.c
+++ multimedia/vdr/files/patch-dvbdevice.c
@@ -8,3 +8,30 @@
  #include "dvbdevice.h"
  #include <ctype.h>
  #include <errno.h>
+@@ -592,7 +598,7 @@ int cDvbTuner::GetSignalQuality(void) co
+      while (1) {
+            if (ioctl(fd_frontend, FE_READ_SNR, &Snr) != -1)
+               break;
+-           if (errno == EOPNOTSUPP) {
++           if (errno == EOPNOTSUPP || errno == ENXIO) {
+               Snr = 0xFFFF;
+               HasSnr = false;
+               break;
+@@ -605,7 +611,7 @@ int cDvbTuner::GetSignalQuality(void) co
+      while (1) {
+            if (ioctl(fd_frontend, FE_READ_BER, &Ber) != -1)
+               break;
+-           if (errno == EOPNOTSUPP) {
++           if (errno == EOPNOTSUPP || errno == ENXIO) {
+               Ber = 0;
+               HasBer = false;
+               break;
+@@ -618,7 +624,7 @@ int cDvbTuner::GetSignalQuality(void) co
+      while (1) {
+            if (ioctl(fd_frontend, FE_READ_UNCORRECTED_BLOCKS, &Unc) != -1)
+               break;
+-           if (errno == EOPNOTSUPP) {
++           if (errno == EOPNOTSUPP || errno == ENXIO) {
+               Unc = 0;
+               HasUnc = false;
+               break;
