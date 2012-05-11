0688b96c1b2fd0598958988f086c2de23322bb81
makc <makc@FreeBSD.org>
2012-05-11 08:58:15 +0000

    Update to 2.5.0

--- devel/qtcreator/files/patch-src-plugins-qt4projectmanager-makestep.cpp
+++ devel/qtcreator/files/patch-src-plugins-qt4projectmanager-makestep.cpp
@@ -1,15 +1,13 @@
---- ./src/plugins/qt4projectmanager/makestep.cpp.orig	2012-01-24 14:37:13.000000000 +0000
-+++ ./src/plugins/qt4projectmanager/makestep.cpp	2012-02-26 15:30:04.084747079 +0000
-@@ -201,10 +201,12 @@
-     // FIXME doing this without the user having a way to override this is rather bad
-     // so we only do it for unix and if the user didn't override the make command
-     // but for now this is the least invasive change
+--- ./src/plugins/qt4projectmanager/makestep.cpp.orig	2012-05-09 12:13:19.000000000 +0000
++++ ./src/plugins/qt4projectmanager/makestep.cpp	2012-05-10 09:13:05.254056057 +0000
+@@ -211,8 +211,10 @@
+     // We also prepend "L" to the MAKEFLAGS, so that nmake / jom are less verbose
+     ProjectExplorer::ToolChain *toolChain = bc->toolChain();
+     if (toolChain && m_makeCmd.isEmpty()) {
 +#ifndef Q_OS_FREEBSD
-     if (toolchain
-             && toolchain->targetAbi().binaryFormat() != ProjectExplorer::Abi::PEFormat
-             && m_makeCmd.isEmpty())
-         Utils::QtcProcess::addArg(&args, QLatin1String("-w"));
+         if (toolChain->targetAbi().binaryFormat() != ProjectExplorer::Abi::PEFormat )
+             Utils::QtcProcess::addArg(&args, QLatin1String("-w"));
 +#endif
- 
-     setEnabled(true);
-     pp->setArguments(args);
+         if (toolChain->targetAbi().os() == ProjectExplorer::Abi::WindowsOS
+                 && toolChain->targetAbi().osFlavor() != ProjectExplorer::Abi::WindowsMSysFlavor) {
+             const QString makeFlags = QLatin1String("MAKEFLAGS");
