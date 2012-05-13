50b237ff708442b3f42e84063bc6789941564668
Max Brazhnikov <makc@FreeBSD.org>
2012-05-13 12:19:04 +0000

    Fix build with Qt 4.7
    Reported by:	pav/pointyhat

---
+++ databases/kbibtex/files/patch-src__gui__preferences__settingsabstractwidget.h
@@ -0,0 +1,10 @@
+--- ./src/gui/preferences/settingsabstractwidget.h.orig	2011-11-20 20:36:54.000000000 +0000
++++ ./src/gui/preferences/settingsabstractwidget.h	2012-05-13 12:08:37.393207573 +0000
+@@ -24,6 +24,7 @@
+ #include <kbibtexgui_export.h>
+ 
+ #include <QWidget>
++#include <QModelIndex>
+ 
+ class KComboBox;
+ 
