0f0253156966267b2b3549a9a4ebd92f4163ed76
Stephen Montgomery-Smith <stephen@FreeBSD.org>
2012-05-15 11:56:52 +0000

    - Update to 5.0. - Add the following patches: http://trac.sagemath.org/sage_trac/ticket/12400 http://trac.sagemath.org/sage_trac/ticket/12818 http://trac.sagemath.org/sage_trac/ticket/12828 http://trac.sagemath.org/sage_trac/ticket/12862 - Remove obsolete patches. - Add options to build optional subpackages. - Add option to use 387 FPU instead of SSE (amd64 only). - Reduce size of what is installed in PREFIX. - It now passes all tests (except minor floating point differences) under amd64.  There are still unsolved failed tests with i386.

---
+++ math/sage/files/spkg-patch-polybori-0.8.1.p1_-_custom.py
@@ -0,0 +1,11 @@
+--- polybori-0.8.1.p1/custom.py-orig	2012-04-10 03:58:28.000000000 +0000
++++ polybori-0.8.1.p1/custom.py	2012-04-10 04:01:48.000000000 +0000
+@@ -60,6 +60,8 @@
+ HAVE_TEX4HT=False
+ HAVE_PYTHON_EXTENSION=False
+ EXTERNAL_PYTHON_EXTENSION=True
++BOOST_TEST=False
++BOOST_PYTHON=False
+ 
+ # (CC and CXX should have been set by sage-env, but never mind...:)
+ try:
