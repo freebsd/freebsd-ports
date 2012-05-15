e16d6b310f40fa575b557c1a913fd31e3e26955e
stephen <stephen@FreeBSD.org>
2012-05-15 11:56:52 +0000

    - Update to 5.0. - Add the following patches: http://trac.sagemath.org/sage_trac/ticket/12400 http://trac.sagemath.org/sage_trac/ticket/12818 http://trac.sagemath.org/sage_trac/ticket/12828 http://trac.sagemath.org/sage_trac/ticket/12862 - Remove obsolete patches. - Add options to build optional subpackages. - Add option to use 387 FPU instead of SSE (amd64 only). - Reduce size of what is installed in PREFIX. - It now passes all tests (except minor floating point differences) under amd64.  There are still unsolved failed tests with i386.

---
+++ math/sage/files/spkg-patch-polybori-0.8.1.p1_-_patches_Cudd.cudd.cudd.h
@@ -0,0 +1,18 @@
+--- polybori-0.8.1.p1/patches/Cudd.cudd.cudd.h.patch.orig	2012-04-13 01:19:43.000000000 +0000
++++ polybori-0.8.1.p1/patches/Cudd.cudd.cudd.h.patch	2012-04-13 01:22:34.000000000 +0000
+@@ -0,0 +1,15 @@
++--- polybori-0.8.1/Cudd/cudd/cudd.h.orig	2012-03-15 22:47:56.000000000 +0000
+++++ polybori-0.8.1/Cudd/cudd/cudd.h	2012-04-13 01:21:09.000000000 +0000
++@@ -87,6 +87,12 @@
++ #endif
++ 
++ #ifdef __cplusplus
+++#include <cstdio>
+++#else
+++#include <stdio.h>
+++#endif
+++
+++#ifdef __cplusplus
++ extern "C" {
++ #endif
++ 
