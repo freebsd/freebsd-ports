f1403c139e88ffe0f19334e8dbe9b28c35b70d32
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2012-05-16 13:10:35 +0000

    Remove expired ports: BROKEN for more than 6 month, there is no upstream distfiles anymore

--- net-p2p/py-bittorrent/files/extra-noportdocs-setup.py
+++
@@ -1,6 +0,0 @@
---- setup.py.orig	Mon May  7 05:49:59 2007
-+++ setup.py	Mon May  7 05:50:30 2007
-@@ -82,3 +82,0 @@
--    (doc_root, ['credits.txt', 'LICENSE.txt', 'README.txt',
--                'TRACKERLESS.txt', 'redirdonate.html', 
--                'INSTALL.unix.txt', ] + extra_docs),
