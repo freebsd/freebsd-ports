db69711f06d46748a9c3750a07a7bfbc281a406a
Jason Helfman <jgh@FreeBSD.org>
2012-05-16 17:24:23 +0000

    - fix startup of zenoss service - bump PORTREVISION
    PR:	ports/167897
    Submitted by:	xenophon+freebsd@irtnog.org
    Approved by:	maintainer, pphillips@experts-exchange.com
    Sponsored by: Experts Exchange, LLC.

--- net-mgmt/zenoss/files/patch-init.sh
+++ net-mgmt/zenoss/files/patch-init.sh
@@ -2,7 +2,7 @@ $FreeBSD$
 
 --- /dev/null	2011-04-19 17:01:32.000000000 -0700
 +++ init.sh	2011-04-19 17:02:19.000000000 -0700
-@@ -0,0 +1,15 @@
+@@ -0,0 +1,19 @@
 +#!/usr/bin/env bash
 +# load up the functions we'll use to build and install
 +. shared-functions.sh
@@ -14,6 +14,10 @@ $FreeBSD$
 +if [ "`whoami`" != "zenoss" ]; then
 +   echo "You must run this script as the zenoss user"
 +   exit 1
++fi
++
++if [ ! -f "$ZENHOME/etc/zope.conf" ]; then
++   cp -p $ZENHOME/etc/zope.conf.in $ZENHOME/etc/zope.conf
 +fi
 +
 +init
