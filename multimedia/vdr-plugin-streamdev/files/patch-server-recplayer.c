56b63e1681f21ba338ead038e65b1ef1981305f7
nox <nox@FreeBSD.org>
2012-05-17 18:57:17 +0000

    Update to 0.5.2 .

--- multimedia/vdr-plugin-streamdev/files/patch-server-recplayer.c
+++ multimedia/vdr-plugin-streamdev/files/patch-server-recplayer.c
@@ -1,12 +1,12 @@
 --- server/recplayer.c.orig
 +++ server/recplayer.c
-@@ -196,8 +196,10 @@ unsigned long RecPlayer::getBlock(unsign
+@@ -199,8 +199,10 @@ unsigned long RecPlayer::getBlock(unsign
      fseek(file, filePosition, SEEK_SET);
      if (fread(&buffer[got], getFromThisSegment, 1, file) != 1) return 0; // umm, big problem.
  
 +#ifdef __linux__
      // Tell linux not to bother keeping the data in the FS cache
-     posix_fadvise(file->_fileno, filePosition, getFromThisSegment, POSIX_FADV_DONTNEED);
+     posix_fadvise(fileno(file), filePosition, getFromThisSegment, POSIX_FADV_DONTNEED);
 +#endif
  
      got += getFromThisSegment;
