268fdd34dda9de8f9d1b798d9fc62f7462e66d4e
pawel <pawel@FreeBSD.org>
2012-05-18 16:27:18 +0000

    - Update to version 2.1.5 [1] - Add option for PGSQL support [1] - Switch to bsd.port.options
    PR:		ports/167841 [1]
    Submitted by:	Edmondas Girkantas <eg@fbsd.lt> (maintainer) [1]

--- dns/poweradmin/files/patch-templates.inc.php
+++ dns/poweradmin/files/patch-templates.inc.php
@@ -1,11 +1,25 @@
---- inc/templates.inc.php.orig	2010-03-14 16:47:47.000000000 +0200
-+++ inc/templates.inc.php	2010-03-14 16:48:00.000000000 +0200
-@@ -193,7 +193,7 @@
- 			while($r = $result->fetchRow()) {
- 				// Call get_record_from_id for each row.
- 				$ret[$retcount] = get_zone_templ_record_from_id($r["id"]);
--				$retcount;
-+				$retcount++;
- 			}
- 			return $ret;
- 		}
+--- inc/templates.inc.php.orig	2010-12-10 23:26:28.839400000 +0200
++++ inc/templates.inc.php	2011-05-30 12:39:13.319768001 +0300
+@@ -312,7 +312,7 @@
+ 
+ // Add a zone template from zone / another template.
+ 
+-function add_zone_templ_save_as($template_name, $description, $userid, $records) {
++function add_zone_templ_save_as($template_name, $description, $userid, $records, $domain = null) {
+ 	global $db;
+ 
+ 	if (!(verify_permission('zone_master_add'))) {
+@@ -339,9 +339,12 @@
+                             $content = $db->quote($record['content'], 'text');
+                     }
+ 
++                    $name = $domain ? preg_replace('/'.$domain.'/', '[ZONE]',$record['name']) : $record['name'];
++                    $content = $domain ? preg_replace('/'.$domain.'/', '[ZONE]',$content) : $content;
++
+                     $query2 = "INSERT INTO zone_templ_records (zone_templ_id, name, type, content, ttl, prio) VALUES ("
+                                             . $db->quote($zone_templ_id, 'integer') . ","
+-                                            . $db->quote($record['name'], 'text') . ","
++                                            . $db->quote($name, 'text') . ","
+                                             . $db->quote($record['type'], 'text') . ","
+                                             . $content . ","
+                                             . $db->quote($record['ttl'], 'integer') . ","
