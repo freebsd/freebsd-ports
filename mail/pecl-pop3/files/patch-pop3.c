3a1f7f4a0eddb06a7cf027cabb8f40ff479df252
ale <ale@FreeBSD.org>
2012-05-20 06:44:51 +0000

    Fix build with php 5.4.
    PR:		ports/168151
    Submitted by:	Kuan-Chung Chiu <buganini@gmail.com>

--- mail/pecl-pop3/files/patch-pop3.c
+++ mail/pecl-pop3/files/patch-pop3.c
@@ -1,5 +1,5 @@
---- pop3.c	Fri Aug 10 04:11:56 2007
-+++ pop3.c	Fri Aug 10 04:12:25 2007
+--- pop3.c.orig	2005-07-26 05:47:03.000000000 +0800
++++ pop3.c	2012-05-20 05:24:07.000000000 +0800
 @@ -172,8 +172,12 @@
  
  	intern = emalloc(sizeof(pop3_object));
@@ -13,3 +13,21 @@
  	intern->zo.properties = NULL;
  	intern->ptr = NULL;
  
+@@ -200,7 +204,7 @@
+  *
+  * Every user visible function must have an entry in pop3_functions[].
+  */
+-function_entry pop3_functions[] = {
++zend_function_entry pop3_functions[] = {
+ 	PHP_FE(pop3_open,	NULL)
+ 	PHP_FE(pop3_close, NULL)
+ 	PHP_FE(pop3_get_message_count, NULL)
+@@ -221,7 +225,7 @@
+ 
+ /* {{{ pop3_class_functions
+  */
+-function_entry pop3_class_functions[] = {
++zend_function_entry pop3_class_functions[] = {
+ 	POP3_ME_MAPPING(pop3, pop3_open, NULL)
+ 	POP3_ME_MAPPING(get_message_count, pop3_get_message_count, NULL)
+ 	POP3_ME_MAPPING(get_account_size, pop3_get_account_size, NULL)
