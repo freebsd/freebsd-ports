0a8da86765e1f0e9148642f1f177e6d2bd8bc23b
Martin Wilke <miwi@FreeBSD.org>
2012-05-22 09:24:47 +0000

    - Update 0.12.1
    PR:		168136
    Submitted by:	Olivier Duchateau <duchateau.olivier@gmail.com> (maintainer)

--- www/py-requests/files/patch-requests_models.py
+++ www/py-requests/files/patch-requests_models.py
@@ -1,21 +1,15 @@
---- requests/models.py.orig	2012-04-23 04:13:53.000000000 +0200
-+++ requests/models.py	2012-04-23 18:22:55.000000000 +0200
-@@ -15,12 +15,12 @@
- from .status_codes import codes
+--- requests/models.py.orig	2012-05-08 07:13:56.000000000 +0200
++++ requests/models.py	2012-05-19 13:33:03.000000000 +0200
+@@ -17,12 +17,6 @@
  
  from .auth import HTTPBasicAuth, HTTPProxyAuth
+ from .cookies import cookiejar_from_dict, extract_cookies_to_jar, get_cookie_header
 -from .packages.urllib3.response import HTTPResponse
 -from .packages.urllib3.exceptions import MaxRetryError, LocationParseError
 -from .packages.urllib3.exceptions import SSLError as _SSLError
 -from .packages.urllib3.exceptions import HTTPError as _HTTPError
 -from .packages.urllib3 import connectionpool, poolmanager
 -from .packages.urllib3.filepost import encode_multipart_formdata
-+from urllib3.response import HTTPResponse
-+from urllib3.exceptions import MaxRetryError, LocationParseError
-+from urllib3.exceptions import SSLError as _SSLError
-+from urllib3.exceptions import HTTPError as _HTTPError
-+from urllib3 import connectionpool, poolmanager
-+from urllib3.filepost import encode_multipart_formdata
  from .defaults import SCHEMAS
  from .exceptions import (
      ConnectionError, HTTPError, RequestException, Timeout, TooManyRedirects,
--- www/py-requests/files/patch-setup.py
+++ www/py-requests/files/patch-setup.py
@@ -1,22 +1,18 @@
---- setup.py.orig	2012-02-15 09:48:23.000000000 +0100
-+++ setup.py	2012-04-01 12:00:30.000000000 +0200
-@@ -4,10 +4,9 @@
- import os
- import sys
- import requests
--from requests.compat import is_py3, is_py2
+--- setup.py.orig	2012-05-08 06:56:28.000000000 +0200
++++ setup.py	2012-05-19 13:42:13.000000000 +0200
+@@ -11,7 +11,7 @@
+ from requests.compat import is_py3
  
  try:
 -    from setuptools import setup
 +    from setuptools import setup, find_packages
+     # hush pyflakes
+     setup
  except ImportError:
-     from distutils.core import setup
- 
-@@ -21,21 +20,8 @@
-     os.system('python test_requests.py')
+@@ -21,31 +21,8 @@
+     os.system('python setup.py sdist upload')
      sys.exit()
  
--required = ['certifi>=0.0.7',]
 -packages = [
 -    'requests',
 -    'requests.packages',
@@ -26,17 +22,28 @@
 -    'requests.packages.urllib3.packages.mimetools_choose_boundary',
 -]
 -
+-# certifi is a Python package containing a CA certificate bundle for SSL verification.
+-# On certain supported platforms (e.g., Red Hat / Debian / FreeBSD), Requests can
+-# use the system CA bundle instead; see `requests.utils` for details.
+-# If your platform is supported, set `requires` to [] instead:
+-requires = ['certifi>=0.0.7']
+-
+-# chardet is used to optimally guess the encodings of pages that don't declare one.
+-# At this time, chardet is not a required dependency. However, it's sufficiently
+-# important that pip/setuptools should install it when it's unavailable.
 -if is_py3:
--    required.append('chardet2')
+-    chardet_package = 'chardet2'
 -else:
--    required.append('chardet>=1.0.0')
--    packages.append('requests.packages.oreos')
+-    chardet_package = 'chardet>=1.0.0'
+-    requires.append('oauthlib>=0.1.0,<0.2.0')
+-
+-requires.append(chardet_package)
 +# FreeBSD ports tree, provides only chardet2
-+required = ['certifi>=0.0.7', 'chardet>=2.0.0']
++requires = ['certifi>=0.0.7', 'chardet>=2.0.0']
  
- 
- setup(
-@@ -47,7 +33,7 @@
+ # The async API in requests.async requires the gevent package.
+ # This is also not a required dependency.
+@@ -62,7 +39,7 @@
      author='Kenneth Reitz',
      author_email='me@kennethreitz.com',
      url='http://python-requests.org',
@@ -44,4 +51,4 @@
 +    packages=find_packages(exclude=['requests.packages.url*']),
      package_data={'': ['LICENSE', 'NOTICE']},
      include_package_data=True,
-     install_requires=required,
+     install_requires=requires,
