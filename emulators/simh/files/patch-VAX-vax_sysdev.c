58d7b4fa755cc1a607c36bbe1518391dcdd74ec3
miwi <miwi@FreeBSD.org>
2012-05-23 02:01:33 +0000

    - Update to 3.9.0
    PR:		168031
    Submitted by:	Ports Fury

--- emulators/simh/files/patch-VAX-vax_sysdev.c
+++ emulators/simh/files/patch-VAX-vax_sysdev.c
@@ -1,20 +1,19 @@
---- VAX/vax_sysdev.c.orig	2008-11-23 11:57:54.000000000 +0100
-+++ VAX/vax_sysdev.c	2009-02-12 06:24:49.000000000 +0100
-@@ -1553,9 +1553,15 @@
+--- VAX/vax_sysdev.c.orig	2012-05-05 22:40:08.000000000 +0900
++++ VAX/vax_sysdev.c	2012-05-05 22:43:28.000000000 +0900
+@@ -1556,8 +1556,14 @@
      if (sim_log)
          fprintf (sim_log, "Loading boot code from ka655x.bin\n");
      r = load_cmd (0, "-R ka655x.bin");
 -    if (r != SCPE_OK)
 -        return r;
 +    if (r != SCPE_OK) {
-+        printf ("Loading boot code from /usr/local/share/simh/ka655x.bin\n");
++        printf ("Loading boot code from %%DATADIR%%/ka655x.bin\n");
 +        if (sim_log)
-+	    fprintf (sim_log, "Loading boot code from /usr/local/share/simh/ka655x.bin\n");
-+        r = load_cmd (0, "-R /usr/local/share/simh/ka655x.bin");
++            fprintf (sim_log, "Loading boot code from %%DATADIR%%/ka655x.bin\n");
++        r = load_cmd (0, "-R %%DATADIR%%/ka655x.bin");
 +        if (r != SCPE_OK)
 +            return r;
++        }
      }
-+}
+ sysd_powerup ();
  return SCPE_OK;
- }
- 
