643bd7846d87bf2f88f1c2b9b15f2a2b6793d144
dinoex <dinoex@FreeBSD.org>
2012-05-24 18:35:20 +0000

    - update to 3.0.0

--- ftp/vsftpd/files/patch-sysdeputil.c
+++ ftp/vsftpd/files/patch-sysdeputil.c
@@ -1,5 +1,5 @@
---- sysdeputil.c.orig	2010-03-26 06:25:33.000000000 +0300
-+++ sysdeputil.c	2011-05-17 20:51:35.350022421 +0400
+--- sysdeputil.c.orig	2010-03-26 04:25:33.000000000 +0100
++++ sysdeputil.c	2012-04-14 10:31:00.000000000 +0200
 @@ -57,7 +57,7 @@
  #endif
  #define VSF_SYSDEP_HAVE_SHADOW
@@ -9,7 +9,20 @@
  #define VSF_SYSDEP_HAVE_UTMPX
  
  #define __USE_GNU
-@@ -1213,7 +1213,9 @@ vsf_insert_uwtmp(const struct mystr* p_u
+@@ -323,8 +323,12 @@
+                       const struct mystr* p_remote_host)
+ {
+   int retval;
++#ifdef PAM_USER
+   pam_item_t item;
++#endif
++#ifdef PAM_RUSER
+   const char* pam_user_name = 0;
++#endif
+   struct pam_conv the_conv =
+   {
+     &pam_conv_func,
+@@ -1213,7 +1217,9 @@
    setutxent();
    (void) pututxline(&s_utent);
    endutxent();
@@ -19,7 +32,7 @@
  }
  
  void
-@@ -1232,7 +1234,9 @@ vsf_remove_uwtmp(void)
+@@ -1232,7 +1238,9 @@
    (void) pututxline(&s_utent);
    endutxent();
    s_utent.ut_tv.tv_sec = vsf_sysutil_get_time_sec();
