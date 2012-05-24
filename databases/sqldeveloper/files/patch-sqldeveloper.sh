5751fa138b025e4bdf4055a96f570243765d6ed6
jgh <jgh@FreeBSD.org>
2012-05-24 18:52:57 +0000

    - Update to 3.1.07.42 - while here standardize IGNORE, and drop pkg-message from SUB_FILES and move to pkg-message as nothing is being substituted
    PR:     ports/168163
    Submitted by:   maintainer, jjuanino@gmail.com

--- databases/sqldeveloper/files/patch-sqldeveloper.sh
+++
@@ -1,6 +0,0 @@
---- sqldeveloper.sh.orig
-+++ sqldeveloper.sh
-@@ -1,2 +1,2 @@
- #!/bin/bash
--cd "`dirname $0`"/sqldeveloper/bin && bash sqldeveloper $*
-+cd %DATADIR%/sqldeveloper/bin && JAVA_HOME=%JAVA_HOME% exec bash sqldeveloper $*
