13e3c6bdcdb5250547b84e04f52b949afebc52ef
Martin Matuska <mm@FreeBSD.org>
2012-05-25 18:34:50 +0000

    Properly call freehostent()
    PR:		ports/168336
    Approved by:	maintainer <bapt@FreeBSD.org>

--- benchmarks/siege/files/patch-src-sock.c
+++ benchmarks/siege/files/patch-src-sock.c
@@ -1,12 +1,20 @@
---- src/sock.c.ORIG	Sun Aug 29 13:39:37 2004
-+++ src/sock.c	Sun Aug 29 13:38:01 2004
-@@ -132,7 +132,7 @@
-   if((gethostbyname_r( hn, &hent, hbf, sizeof(hbf), &hp, &herrno ) < 0)){
-     hp = NULL;
+--- src/sock.c.orig	2010-05-17 15:57:59.000000000 +0200
++++ src/sock.c	2012-05-25 15:55:21.743780806 +0200
+@@ -126,7 +126,7 @@
+       hp = NULL;
+     }
    }
 -#elif defined(sun)
 +#elif defined(sun) || defined(__FreeBSD__)
  # ifdef HAVE_GETIPNODEBYNAME
-   hp = getipnodebyname( hn, AF_INET, 0, &herrno );
+   hp = getipnodebyname(hn, AF_INET, 0, &herrno);
  # else /* default use gethostbyname_r*/
-
+@@ -154,7 +154,7 @@
+   if(hp == NULL){ return -1; } 
+   memset((void*) &cli, 0, sizeof(cli));
+   memcpy(&cli.sin_addr, hp->h_addr, hp->h_length);
+-#if defined(sun)
++#if defined(sun) || defined(__FreeBSD__)
+ # ifdef  HAVE_FREEHOSTENT
+   freehostent(hp);
+ # endif/*HAVE_FREEHOSTENT*/ 
