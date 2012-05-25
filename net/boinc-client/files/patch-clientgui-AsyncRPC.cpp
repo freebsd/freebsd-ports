317ef81a0bdc8888f6d614852c2c557c0797fe0b
pav <pav@FreeBSD.org>
2012-05-25 12:23:56 +0000

    - Enable per thread locale on recent FreeBSD 9
    PR:		ports/168257
    Submitted by:	Andrey Simonenko <simon@comsys.ntu-kpi.kiev.ua>

--- net/boinc-client/files/patch-clientgui-AsyncRPC.cpp
+++ net/boinc-client/files/patch-clientgui-AsyncRPC.cpp
@@ -1,15 +1,11 @@
 --- clientgui/AsyncRPC.cpp.orig		2012-04-07 11:55:37.000000000 +0300
 +++ clientgui/AsyncRPC.cpp		2012-04-07 11:56:14.000000000 +0300
-@@ -20,7 +20,12 @@
+@@ -20,7 +20,9 @@
  #endif
  
  #if !(defined(_WIN32) || (defined(__WXMAC__) && (MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_4)))
--#include <xlocale.h>
-+#ifdef __FreeBSD__
-+# include <osreldate.h>
-+# if __FreeBSD_version >= 1000002
-+#  include <xlocale.h>
-+# endif
++#if !defined(NO_PER_THREAD_LOCALE)
+ #include <xlocale.h>
 +#endif
  //#include "gui_rpc_client.h"
  #endif
