1df39a5fb17c0bb2418a5560b3a28cf1a7f8d2a1
Martin Wilke <miwi@FreeBSD.org>
2012-05-25 16:51:40 +0000

    - Welcome XFCE 4.10 to the portstree!
    Special Thanks to:      rene, Olivier Duchateau <duchateau.olivier@gmail.com>
    Submitted by:	 	Olivier Duchateau <duchateau.olivier@gmail.com>
    
    Tested by:	phw

--- x11-wm/xfce4-session/files/patch-xfce4-session_xfsm-shutdown-helper.c
+++ x11-wm/xfce4-session/files/patch-xfce4-session_xfsm-shutdown-helper.c
@@ -1,12 +1,12 @@
---- xfce4-session/xfsm-shutdown-helper.c.orig	2010-11-03 21:11:37.000000000 +0100
-+++ xfce4-session/xfsm-shutdown-helper.c	2010-11-16 17:15:07.000000000 +0100
-@@ -58,6 +58,9 @@
- #include <errno.h>
+--- xfce4-session/xfsm-shutdown.c.orig	2012-04-28 22:43:27.000000000 +0200
++++ xfce4-session/xfsm-shutdown.c	2012-05-17 20:32:41.000000000 +0200
+@@ -52,6 +52,9 @@
+ #include <signal.h>
  #endif
  
 +#include <sys/param.h>
 +#include <sys/user.h>
 +#include <sys/sysctl.h>
- #include <dbus/dbus.h>
- #include <dbus/dbus-glib-lowlevel.h>
  
+ #include <dbus/dbus-glib.h>
+ #include <dbus/dbus-glib-lowlevel.h>
