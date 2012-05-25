d76f1d8bf4b8da1c3ec5552071c5b716f238a677
Max Brazhnikov <makc@FreeBSD.org>
2012-05-25 01:44:04 +0000

    KDE/FreeBSD team presents Qt 4.8.1 in ports!

--- devel/qt4-script/files/patch-src__3rdparty__javascriptcore__JavaScriptCore__interpreter__Interpreter.cpp
+++ devel/qt4-script/files/patch-src__3rdparty__javascriptcore__JavaScriptCore__interpreter__Interpreter.cpp
@@ -1,5 +1,5 @@
---- ./src/3rdparty/javascriptcore/JavaScriptCore/interpreter/Interpreter.cpp.orig	2011-06-30 00:17:41.495446920 +0200
-+++ ./src/3rdparty/javascriptcore/JavaScriptCore/interpreter/Interpreter.cpp	2011-06-30 00:19:46.885439368 +0200
+--- ./src/3rdparty/javascriptcore/JavaScriptCore/interpreter/Interpreter.cpp.orig	2011-06-30 00:43:03.155579428 +0200
++++ ./src/3rdparty/javascriptcore/JavaScriptCore/interpreter/Interpreter.cpp	2011-06-30 00:43:34.275444696 +0200
 @@ -2493,8 +2493,10 @@
              vPC += OPCODE_LENGTH(op_get_by_pname);
              NEXT_INSTRUCTION();
