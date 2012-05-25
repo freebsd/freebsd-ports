8beed3b01c9463a29b0fa2e5b44c596cb7531656
Guido Falsi <madpilot@FreeBSD.org>
2012-05-25 07:05:33 +0000

    Add 0ad, real-time strategy (RTS) game of ancient warfare.
    Approved by:	crees (mentor)

---
+++ games/0ad/files/patch-build-resources-0ad.sh
@@ -0,0 +1,7 @@
+--- build/resources/0ad.sh.orig	2010-08-15 14:23:11.000000000 +0200
++++ build/resources/0ad.sh	2012-05-10 15:17:57.136470711 +0200
+@@ -1,3 +1,3 @@
+ #!/bin/sh
+ 
+-/usr/bin/pyrogenesis "$@"
++/usr/local/bin/pyrogenesis "$@"
---
+++ games/0ad/files/patch-source-tools-atlas-AtlasObject-AtlasObjectJS.cpp
@@ -0,0 +1,11 @@
+--- source/tools/atlas/AtlasObject/AtlasObjectJS.cpp.orig	2012-05-21 22:49:25.624250443 +0200
++++ source/tools/atlas/AtlasObject/AtlasObjectJS.cpp	2012-05-21 22:49:28.429250717 +0200
+@@ -24,7 +24,7 @@
+ # pragma warning(disable:4996)	// deprecated CRT
+ #endif
+ 
+-#include "wx/log.h"
++#include "wx-2.8/wx/log.h"
+ 
+ #include <sstream>
+ 
