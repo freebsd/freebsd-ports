b20dbcbdad727e3a89a9333a8a5bd8e54683e378
mm <mm@FreeBSD.org>
2012-05-26 22:45:52 +0000

    Move tests/validate-colorspace.sh patch to REINPLACE_CMD Fix shell comparsion in validate-colorspace.sh

--- graphics/ImageMagick/files/patch-tests-validate-colorspace.sh
+++
@@ -1,6 +0,0 @@
---- tests/validate-colorspace.sh.orig	2012-05-16 21:33:59.156775935 +0200
-+++ tests/validate-colorspace.sh	2012-05-16 21:34:05.407784427 +0200
-@@ -1,2 +1,2 @@
--#!/bin/bash
-+#!/bin/sh
- #
