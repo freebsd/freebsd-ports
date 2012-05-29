aae22bea014bf1b7048a77a9944937b4d0e96dc3
Baptiste Daroussin <bapt@FreeBSD.org>
2012-05-29 15:12:39 +0000

    Convert to new options framework Add shared AAC, DAGRAB, GOGO, MPP and SPEEX descriptions

--- Mk/bsd.options.desc.mk
+++ Mk/bsd.options.desc.mk
@@ -5,21 +5,27 @@
 
 Options_Desc_MAINTAINER=	ports@FreeBSD.org
 
+AAC_DESC?=		Enable support for aac
+CDDA2WAV_DESC?=		Enable cdda2was CD ripper
 CURL_DESC?=		Enable CURL support
+DAGRAB_DESC?=		Enable dagrab CD ripper
 DBUS_DESC?=		Enable D-Bus support
 DOCS_DESC?=		Build and install the documentation
 FLAC_DESC?=		Enable support for flac
+GOGO_DESC?=		Enable gogo mp3 encoder
 ID3_DESC?=		Enable support for id3 tags
 LDAP_DESC?=		Enable LDAP support
 MAGICK_DESC?=		Enable ImageMagick Library
 MMKEYS_DESC?=		Enable multimedia keys support
 MP4_DESC?=		Enable support for mp4
+MPP_DESC?=		Enable support for musepack
 MULTIBYTE_DESC?=	Enable multibyte character support
 MYSQL_DESC?=		Enable MySQL backend
 NLS_DESC?=		Build and install the localisation data
 PCRE_DESC?=		Enable regular expressions using libpcre
 PGSQL_DESC?=		Enable PostgreSQL backend
 PNG_DESC?=		Enable PNG support
+SPEEX_DESC?=		Enable support for speex audio format
 SSL_DESC?=		Enable SSL support
 STATIC_DESC?=		Build static executable/libraries
 SQLITE_DESC?=		Enable SQLite backend
