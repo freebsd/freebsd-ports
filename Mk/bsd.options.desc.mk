2a7dd8db0005fe0a91e88985230b3b4569b93dd7
bapt <bapt@FreeBSD.org>
2012-05-29 13:38:49 +0000

    Convert to new option framework
    Add PNG to shared options descriptions

--- Mk/bsd.options.desc.mk
+++ Mk/bsd.options.desc.mk
@@ -8,5 +8,6 @@ Options_Desc_MAINTAINER=	ports@FreeBSD.org
 DOCS_DESC?=	Build and install the documentation
 LDAP_DESC?=	Enable LDAP support
 NLS_DESC?=	Build and install the localisation data
+PNG_DESC?=	Enable PNG support
 SSL_DESC?=	Enable SSL support
 TIDY_DESC?=	Enable tidy html cleaner support
