cd1768db41f7b1311e3e7421da8a1623fb4f2081
bapt <bapt@FreeBSD.org>
2012-05-29 12:18:32 +0000

    Switch prayer to new options format. While here SSL, LDAP and TIDY description in shared bsd.options.desc.mk set maintainer of bsd.options.desc.mk to ports@FreeBSD.org to point that it is open to any committer

--- Mk/bsd.options.desc.mk
+++ Mk/bsd.options.desc.mk
@@ -1,5 +1,12 @@
 # $FreeBSD$
 #
+# Please keep description ordered alphabetically ordered by variable name
+#
+
+Options_Desc_MAINTAINER=	ports@FreeBSD.org
 
 DOCS_DESC?=	Build and install the documentation
+LDAP_DESC?=	Enable LDAP support
 NLS_DESC?=	Build and install the localisation data
+SSL_DESC?=	Enable SSL support
+TIDY_DESC?=	Enable tidy html cleaner support
