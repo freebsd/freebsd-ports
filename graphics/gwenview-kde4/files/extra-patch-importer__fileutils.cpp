6f971d8396464b5d95609a512cd8feb6a00e231e
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2012-05-30 22:40:39 +0000

    Make it build on FreeBSD 7.
    There's a call to mkdtemp(), which is supposed to be in stdlib.h. However,
    until FreeBSD 8 the definition was only in unistd.h, so we need to include that
    header for 7.

---
+++ graphics/gwenview-kde4/files/extra-patch-importer__fileutils.cpp
@@ -0,0 +1,10 @@
+--- importer/fileutils.cpp~	2012-05-30 19:27:44.000000000 -0300
++++ importer/fileutils.cpp	2012-05-30 19:27:57.000000000 -0300
+@@ -39,6 +39,7 @@
+ #include <errno.h>
+ #include <stdlib.h>
+ #include <string.h>
++#include <unistd.h>
+ 
+ namespace Gwenview
+ {
