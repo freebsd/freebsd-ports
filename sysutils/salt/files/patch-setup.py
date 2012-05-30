5fc4e235bf3c7d4bc576ddc508bb393e2e0f6ea5
Philippe Audeoud <jadawin@FreeBSD.org>
2012-05-30 13:04:12 +0000

    - Update to 0.9.9.1
    PR:		ports/167503
    Submitted by:	Denis Generalov <gd _AT_ rambler-co.ru> (maintainer)

--- sysutils/salt/files/patch-setup.py
+++ sysutils/salt/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py    2012-03-21 11:46:26.000000000 -0600
-+++ setup.py    2012-03-24 14:01:33.965200298 -0600
+--- setup.py	2012-04-28 08:33:41.000000000 +0400
++++ setup.py	2012-05-02 01:08:12.000000000 +0400
 @@ -47,7 +47,7 @@
  if 'SYSCONFDIR' in os.environ:
      etc_path = os.environ['SYSCONFDIR']
@@ -8,22 +8,17 @@
 +    etc_path = os.path.join('%%PREFIX%%', 'etc')
 
  libraries = ['ws2_32'] if sys.platform == 'win32' else []
-
-@@ -98,10 +98,10 @@
+ 
+@@ -100,7 +100,7 @@
+                'scripts/salt-call',
                 'scripts/salt-run',
                 'scripts/salt'],
-       data_files=[(os.path.join(etc_path, 'salt'),
--                    ['conf/master.template',
--                     'conf/minion.template',
-+                    ['conf/master.sample',
-+                     'conf/minion.sample',
-                     ]),
--                ('share/man/man1',
-+                ('man/man1',
+-      data_files=[('share/man/man1',
++      data_files=[('man/man1',
                      ['doc/man/salt-master.1',
                       'doc/man/salt-key.1',
                       'doc/man/salt.1',
-@@ -111,9 +111,8 @@
+@@ -110,7 +110,7 @@
                       'doc/man/salt-run.1',
                       'doc/man/salt-minion.1',
                      ]),
@@ -32,5 +27,3 @@
                      ['doc/man/salt.7',
                      ]),
                   ],
--      install_requires=requirements,
-      )
