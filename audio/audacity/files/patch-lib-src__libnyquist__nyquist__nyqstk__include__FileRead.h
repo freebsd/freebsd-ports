d0981a015d74adcbb726d2219d26819d378ad685
Michael Scheidell <scheidell@FreeBSD.org>
2012-05-30 20:43:12 +0000

    - update to 2.0 - submitter is now maintainer (email to previous maintainer bouces for weeks) - replaces audio/audacity-devel
    PR:		ports/167906
    Submitted by:	Jack <xxjack12xx@gmail.com> (maintainer)

---
+++ audio/audacity/files/patch-lib-src__libnyquist__nyquist__nyqstk__include__FileRead.h
@@ -0,0 +1,10 @@
+--- ./lib-src/libnyquist/nyquist/nyqstk/include/FileRead.h.orig	2011-05-04 17:18:48.000000000 +0200
++++ ./lib-src/libnyquist/nyquist/nyqstk/include/FileRead.h	2011-05-04 17:19:13.000000000 +0200
+@@ -33,6 +33,7 @@
+ #define STK_FILEREAD_H
+ 
+ #include "Stk.h"
++#include <stdio.h>
+ 
+ namespace Nyq
+ {
