3da322f9533aa0af2e4519a0cc80863889796f98
avilla <avilla@FreeBSD.org>
2012-05-31 00:09:24 +0000

    The KDE/FreeBSD team is pleased to announce Calligra Suite 2.4.2, KDE graphic art and office suite.
    Information on this release can be found here:
    http://www.calligra.org/news/calligra-2-4-2-released
    
    Information on Calligra Project can be found in its first ever release
    announcement:
    http://www.calligra.org/news/calligra-2-4-released
    
    Consequently, KOffice 2 has been removed from the ports collection.
    
    As usual, we would like to thank all testers and contributors.

--- editors/calligra/files/patch-cmake__modules__FindWPD.cmake
+++ editors/calligra/files/patch-cmake__modules__FindWPD.cmake
@@ -1,7 +1,7 @@
---- ./cmake/modules/FindWPD.cmake.orig	2011-03-25 01:34:51.930775929 +0100
-+++ ./cmake/modules/FindWPD.cmake	2011-03-25 01:37:46.910772674 +0100
-@@ -19,13 +19,11 @@
- else (WPD_INCLUDE_DIR AND WPD_LIBRARIES)
+--- ./cmake/modules/FindWPD.cmake.orig	2011-10-16 07:29:58.807077221 -0700
++++ ./cmake/modules/FindWPD.cmake	2011-10-16 07:30:23.717073061 -0700
+@@ -19,17 +19,13 @@
+ #else (WPD_INCLUDE_DIR AND WPD_LIBRARIES)
  if(NOT WIN32)
    INCLUDE(FindPkgConfig)
 -  pkg_check_modules(WPD libwpd-0.8)
@@ -9,9 +9,12 @@
  endif(NOT WIN32)
  
    FIND_PATH(WPD_INCLUDE_DIR libwpd/libwpd.h
-     ${WPD_INCLUDE_DIR}
--    /usr/include/libwpd-0.8
--    /usr/include/libwpd-0.9
-+    PATH_SUFFIXES libwpd-0.9
+     PATHS
+ 	  ${WPD_INCLUDE_DIR}
+-      /usr/include/libwpd-0.8
+-      /usr/include/libwpd-0.9
+ 	PATH_SUFFIXES
+-	  libwpd-0.8
+ 	  libwpd-0.9
    )
  
--- editors/calligra/files/patch-cmake__modules__FindWPG.cmake
+++ editors/calligra/files/patch-cmake__modules__FindWPG.cmake
@@ -1,24 +1,32 @@
---- ./cmake/modules/FindWPG.cmake.orig	2011-03-25 01:38:36.660731693 +0100
-+++ ./cmake/modules/FindWPG.cmake	2011-03-25 01:40:49.690727037 +0100
-@@ -18,11 +18,10 @@
+--- ./cmake/modules/FindWPG.cmake.orig	2011-10-16 07:30:32.257075896 -0700
++++ ./cmake/modules/FindWPG.cmake	2011-10-16 07:32:34.927100360 -0700
+@@ -18,7 +18,6 @@
  
    IF (NOT WIN32)
      INCLUDE(FindPkgConfig)
 -    pkg_check_modules(LIBWPG libwpg-0.1)
      pkg_check_modules(LIBWPG libwpg-0.2)
-   ENDIF (NOT WIN32)
+     IF(LIBWPG_FOUND)
+         SET(LIBWPG_LIBRARY ${LIBWPG_LIBRARIES})
+@@ -32,12 +31,8 @@
  
--  FIND_LIBRARY(LIBWPG_STREAM_LIBRARIES NAMES libwpg-stream-0.1 wpg-stream-0.1 libwpd-stream-0.9 wpd-stream-0.9
-+  FIND_LIBRARY(LIBWPG_STREAM_LIBRARIES NAMES libwpd-stream-0.9 wpd-stream-0.9
-     PATHS
-     ${LIBWPG_LIBRARIES}
-     )
-@@ -30,7 +29,7 @@
+   FIND_LIBRARY(LIBWPG_STREAM_LIBRARY
+       NAMES 
+-	  libwpg-stream-0.1 
+-	  wpg-stream-0.1 
+ 	  libwpd-stream-0.9 
+ 	  wpd-stream-0.9
+-	  libwpd-stream
+-	  wpd-stream
+       PATHS
+           ${LIBWPG_LIBRARIES}
+   )
+@@ -45,7 +40,7 @@
    FIND_PATH(LIBWPG_INCLUDE_DIR libwpg/libwpg.h
-     PATHS
-     ${LIBWPG_INCLUDE_DIRS}
--    PATH_SUFFIXES libwpg
-+    PATH_SUFFIXES libwpg-0.2
-     )
+       PATHS
+       ${LIBWPG_INCLUDE_DIRS}
+-      PATH_SUFFIXES libwpg-0.1 libwpg-0.2 libwpg 
++      PATH_SUFFIXES libwpg-0.2
+   )
  
-   IF (LIBWPG_INCLUDE_DIR AND LIBWPG_LIBRARIES)
+   SET(LIBWPG_LIBRARIES ${LIBWPG_LIBRARY} ${LIBWPG_STREAM_LIBRARY})
