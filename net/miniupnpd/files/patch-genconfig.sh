c7af6bb4f583ce0479c9e2d46b437e75573e0419
sperber <sperber@FreeBSD.org>
2012-06-02 11:03:26 +0000

    - update to 1.7
    PR:           ports/168462
    Submitted by: Tor Halvard Furulund <squat _at_ squat.no> (maintainer)
    Approved by:  decke (mentor)

--- net/miniupnpd/files/patch-genconfig.sh
+++ net/miniupnpd/files/patch-genconfig.sh
@@ -1,40 +1,35 @@
---- genconfig.sh.orig	2009-09-04 18:21:24.000000000 +0200
-+++ genconfig.sh	2009-11-25 12:31:04.344410750 +0100
-@@ -66,18 +66,25 @@
- 		fi
+--- genconfig.sh.orig	2012-05-24 20:05:47.000000000 +0200
++++ genconfig.sh	2012-05-30 20:12:57.685877655 +0200
+@@ -112,18 +112,20 @@
  		# new way to see which one to use PF or IPF.
  		# see http://miniupnp.tuxfamily.org/forum/viewtopic.php?p=957
--		# source file with handy subroutines like checkyesno
+ 		# source file with handy subroutines like checkyesno
 -		. /etc/rc.subr
 -		# source config file so we can probe vars
 -		. /etc/rc.conf
 -		if checkyesno ipfilter_enable; then
 -			echo "Using ipf"
 -			FW=ipf
--			echo "#define USE_IPF 1" >> ${CONFIGFILE}
 -		elif checkyesno pf_enable; then
 -			echo "Using pf"
 -			FW=pf
--			echo "#define USE_PF 1" >> ${CONFIGFILE}
-+		# source file wource config file so we can probe vars
+-		elif checkyesno firewall_enable; then
+-			echo "Using ifpw"
+-			FW=ipfw
 +		if [ -f /etc/rc.subr ] && [ -f /etc/rc.conf ]; then
-+			# source file with handy subroutines like checkyesno
 +			. /etc/rc.subr
 +			# source config file so we can probe vars
 +			. /etc/rc.conf
 +			if checkyesno ipfilter_enable; then
 +				echo "Using ipf"
 +				FW=ipf
-+				echo "#define USE_IPF 1" >> ${CONFIGFILE}
 +			elif checkyesno pf_enable; then
 +				echo "Using pf"
 +				FW=pf
-+				echo "#define USE_PF 1" >> ${CONFIGFILE}
-+			else
-+				echo "Could not detect usage of ipf or pf. Compiling for pf by default"
-+				FW=pf
-+				echo "#define USE_PF 1" >> ${CONFIGFILE}
++			elif checkyesno firewall_enable; then
++				echo "Using ifpw"
++				FW=ipfw
 +			fi
- 		# TODO : Add support for IPFW
- 		# echo "#define USE_IPFW 1" >> ${CONFIGFILE}
- 		# FW=ipfw
+ 		else
+ 			echo "Could not detect usage of ipf, pf, ipfw. Compiling for pf by default"
+ 			FW=pf
