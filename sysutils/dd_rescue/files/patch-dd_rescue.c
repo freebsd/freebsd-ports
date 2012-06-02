9b8a7f2433438f94be3936d4091ff49ec3dd05a3
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2012-06-02 13:33:49 +0000

    Update to 1.28.

--- sysutils/dd_rescue/files/patch-dd_rescue.c
+++ sysutils/dd_rescue/files/patch-dd_rescue.c
@@ -1,12 +1,16 @@
---- dd_rescue.c.orig	Sun Mar 18 07:29:10 2007
-+++ dd_rescue.c
-@@ -742,7 +742,8 @@
+--- dd_rescue.c.orig	2012-05-20 05:46:14.000000000 +0900
++++ dd_rescue.c	2012-06-02 22:24:44.000000000 +0900
+@@ -1194,11 +1194,11 @@
  		ipos = 0;
  
  #ifdef O_DIRECT
--	if (posix_memalign(mp, sysconf(_SC_PAGESIZE), softbs)) {
+-	if (posix_memalign(&mp, pagesize, softbs)) {
 +	buf = malloc(softbs);
 +	if (!buf) {
  		fplog(stderr, "dd_rescue: (fatal): allocation of aligned buffer failed!\n");
  		cleanup(); exit(18);
  	}
+-	buf = mp;
+ #else
+ 	buf = malloc(softbs);
+ 	if (!buf) {
