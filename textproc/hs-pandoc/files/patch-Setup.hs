fe2d15a7fd6a308dfb2f2e0e723476e609714eca
pgj <pgj@FreeBSD.org>
2012-06-03 22:32:23 +0000

    - Update to 1.9.3
    Obtained from:	FreeBSD Haskell

--- textproc/hs-pandoc/files/patch-Setup.hs
+++ textproc/hs-pandoc/files/patch-Setup.hs
@@ -1,20 +1,20 @@
---- ./Setup.hs.orig	2011-02-13 18:03:00.000000000 +0100
-+++ ./Setup.hs	2011-03-13 01:14:26.580023488 +0100
-@@ -6,7 +6,7 @@
- import Distribution.Simple.LocalBuildInfo
+--- ./Setup.hs.orig	2012-05-13 05:04:40.000000000 +0200
++++ ./Setup.hs	2012-05-15 16:22:31.000000000 +0200
+@@ -7,7 +7,7 @@
           (LocalBuildInfo(..), absoluteInstallDirs)
  import Distribution.Verbosity ( Verbosity, silent )
+ import Distribution.Simple.GHC (ghcPackageDbOptions)
 -import Distribution.Simple.InstallDirs (mandir, bindir, CopyDest (NoCopyDest))
 +import Distribution.Simple.InstallDirs (prefix, bindir, CopyDest (NoCopyDest))
- import Distribution.Simple.Utils (copyFiles)
+ import Distribution.Simple.Utils (installOrdinaryFiles)
  import Control.Exception ( bracket_ )
  import Control.Monad ( unless )
-@@ -85,7 +85,7 @@
+@@ -86,7 +86,7 @@
  installManpages :: PackageDescription -> LocalBuildInfo
                  -> Verbosity -> CopyDest -> IO ()
  installManpages pkg lbi verbosity copy =
--  copyFiles verbosity (mandir (absoluteInstallDirs pkg lbi copy))
-+  copyFiles verbosity (prefix (absoluteInstallDirs pkg lbi copy) </> "man")
+-  installOrdinaryFiles verbosity (mandir (absoluteInstallDirs pkg lbi copy))
++  installOrdinaryFiles verbosity (prefix (absoluteInstallDirs pkg lbi copy) </> manDir)
               (zip (repeat manDir) manpages)
  
  -- | Returns a list of 'dependencies' that have been modified after 'file'.
