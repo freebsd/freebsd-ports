8166d2e31bf64b0f351d9e9e102e86a86f4bff92
Gabor Pali <pgj@FreeBSD.org>
2012-06-03 22:58:54 +0000

    - Update to 0.90.0.1
    Obtained from:	FreeBSD Haskell

--- x11-toolkits/hs-wxcore/files/patch-Setup.hs
+++ x11-toolkits/hs-wxcore/files/patch-Setup.hs
@@ -1,11 +1,10 @@
---- ./Setup.hs.orig	2012-02-07 08:27:09.000000000 +0100
-+++ ./Setup.hs	2012-02-15 09:35:23.000000000 +0100
-@@ -56,7 +56,7 @@
-         wx_cfg_parms = if os == "mingw32"
-                        then [ "--unicode", "--libs", "gl,stc", "--cppflags" ]
-                        else [ "--libs", "std,gl,stc,xrc,richtext,aui,media", "--cppflags" ]
--    wx  <- fmap parseWxConfig (readProcess "wx-config" wx_cfg_parms "")
-+    wx  <- fmap parseWxConfig (readProcess "wxgtk2u-2.8-config" wx_cfg_parms "")
-     lbi <- confHook simpleUserHooks (pkg0, pbi) flags
+--- ./Setup.hs.orig	2012-04-13 14:40:36.000000000 +0200
++++ ./Setup.hs	2012-05-13 20:41:08.000000000 +0200
+@@ -66,7 +66,6 @@
  
-     let lpd   = localPkgDescr lbi
+     let libbi' = libbi
+           { extraLibDirs = extraLibDirs libbi ++ [wxcDirectory]
+-          , extraLibs    = extraLibs    libbi ++ ["wxc"]
+           , ldOptions    = ldOptions    libbi ++ ["-Wl,-rpath," ++ wxcDirectory]  }
+ 
+     let lib' = lib { libBuildInfo = libbi' }
