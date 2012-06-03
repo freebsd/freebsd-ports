70e280095150fb7a59a920901fcc0a0ddc8fd4b7
dinoex <dinoex@FreeBSD.org>
2012-06-03 12:43:04 +0000

    - update to 1.5.0 - new MASTER_SITE PR:		166744 Suggested by:	Zhihao Yuan

--- graphics/openjpeg/files/patch-opj_malloc.h
+++ graphics/openjpeg/files/patch-opj_malloc.h
@@ -1,18 +1,12 @@
---- libopenjpeg/opj_malloc.h.orig	2007-12-21 11:19:01.000000000 +0100
-+++ libopenjpeg/opj_malloc.h	2009-03-21 12:02:37.000000000 +0100
-@@ -76,8 +76,15 @@
- 	#if defined(__sun)
- 			#define HAVE_MEMALIGN
- 		#elif defined(__GNUC__)
-+			#if defined(__FreeBSD__)
-+			#include <stdlib.h>
-+			#include <osreldate.h>
-+			#if __FreeBSD_versioni >= 700000 
-+			#endif
-+			#else
- 			#define HAVE_MEMALIGN
- 			#include <malloc.h>		
-+			#endif
- 		/* Linux x86_64 and OSX always align allocations to 16 bytes */
- 		#elif !defined(__amd64__) && !defined(__APPLE__)	
- 			/* FIXME: Yes, this is a big assumption */
+--- libopenjpeg/opj_malloc.h.orig	2012-02-07 11:49:55.000000000 +0100
++++ libopenjpeg/opj_malloc.h	2012-06-02 11:25:56.000000000 +0200
+@@ -85,8 +85,7 @@
+ 		#define HAVE_MEMALIGN
+ 	/* Linux x86_64 and OSX always align allocations to 16 bytes */
+ 	#elif !defined(__amd64__) && !defined(__APPLE__)	
+-		#define HAVE_MEMALIGN
+-		#include <malloc.h>			
++		#include <stdlib.h>			
+ 	#endif
+ #endif
+ 
