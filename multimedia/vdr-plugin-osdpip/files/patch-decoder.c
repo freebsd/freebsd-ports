10e47bff36ed810ea37ae2791ab4c41c2a0585cd
nox <nox@FreeBSD.org>
2012-06-03 18:21:42 +0000

    - Fix osdteletext plugin calling free() when it should be calling av_free(),   and update to proper vdr 1.7.27 api while I'm here. - Bump PORTREVISION.

---
+++ multimedia/vdr-plugin-osdpip/files/patch-decoder.c
@@ -0,0 +1,20 @@
+--- decoder.c.orig
++++ decoder.c
+@@ -55,13 +55,13 @@ int cDecoder::Close()
+ {
+ #ifndef USE_SWSCALE
+     delete[] m_BufferConvert;
+-    free(m_PicConvert);
++    av_free(m_PicConvert);
+ #endif
+     delete[] m_BufferResample;
+-    free(m_PicResample);
++    av_free(m_PicResample);
+     avcodec_close(m_Context);
+-    free(m_Context);
+-    free(m_PicDecoded);
++    av_free(m_Context);
++    av_free(m_PicDecoded);
+ 
+     return 0;
+ }
