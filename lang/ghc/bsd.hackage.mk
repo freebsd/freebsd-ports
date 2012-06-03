d670032fc1db8f86cfb4b7c135e81027caf984a7
Gabor Pali <pgj@FreeBSD.org>
2012-06-03 19:30:26 +0000

    This package uses attoparsec for parsing the actual contents of the HTTP connection.  It also provides higher-level functions which allow you to avoid direct usage of enumerators.
    WWW:	http://www.yesodweb.com/book/http-conduit
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -156,6 +156,7 @@ HsOpenSSL_port=			security/hs-HsOpenSSL
 HStringTemplate_port=		textproc/hs-HStringTemplate
 html_port=			textproc/hs-html
 HTTP_port=			www/hs-HTTP
+http-conduit_port=		www/hs-http-conduit
 http-enumerator_port=		www/hs-http-enumerator
 http-server_port=		www/hs-http-server
 http-types_port=		www/hs-http-types
