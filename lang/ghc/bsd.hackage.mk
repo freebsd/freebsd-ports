4d514dec90380e5e6cdaceee29b96345dbf27a44
pgj <pgj@FreeBSD.org>
2012-06-03 19:04:00 +0000

    Optimized edit distances for fuzzy matching, including Levenshtein and restricted Damerau-Levenshtein algorithms.
    WWW:	http://github.com/batterseapower/edit-distance
    
    Obtained from:	FreeBSD Haskell

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -93,6 +93,7 @@ directory-tree_port=		devel/hs-directory-tree
 distributive_port=		math/hs-distributive
 dlist_port=			devel/hs-dlist
 double-conversion_port=		textproc/hs-double-conversion
+edit-distance_port=		devel/hs-edit-distance
 email-validate_port=		mail/hs-email-validate
 entropy_port=			security/hs-entropy
 enumerator_port=		devel/hs-enumerator
