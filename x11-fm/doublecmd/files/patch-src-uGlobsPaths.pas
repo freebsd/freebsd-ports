1a85ab13cd6f58f1f68d10a5ab547d66236bcc6e
dhn <dhn@FreeBSD.org>
2012-06-05 15:04:27 +0000

    - Update to 0.5.4
    PR:		ports/167278
    Submitted by:	Beñat Gonzalez Etxepare <bbtruk@users.sourceforge.net> (maintainer)

--- x11-fm/doublecmd/files/patch-src-uGlobsPaths.pas
+++ x11-fm/doublecmd/files/patch-src-uGlobsPaths.pas
@@ -1,40 +1,41 @@
---- src/uGlobsPaths.pas	2012-03-31 18:38:11.000000000 -0500
-+++ src/uGlobsPaths.pas	2012-03-31 18:51:02.000000000 -0500
-@@ -3,7 +3,7 @@
+--- src/uGlobsPaths.pas.old	2012-02-14 04:46:22.000000000 +0100
++++ src/uGlobsPaths.pas	2012-04-24 20:34:12.000000000 +0200
+@@ -3,9 +3,9 @@
  interface
  
  var
 -  gpExePath : String = '';  // executable directory
 +  gpExePath : String = '%%PREFIX%%/bin';  // executable directory
    gpCfgDir : String = '';  // directory from which configuration files are used
-   gpGlobalCfgDir : String = '';  // config dir global for all user
+-  gpGlobalCfgDir : String = '';  // config dir global for all user
++  gpGlobalCfgDir : String = '%%DATADIR%%';  // config dir global for all user
    gpCmdLineCfgDir : String = ''; // config dir passed on the command line
-@@ -26,10 +26,10 @@
- procedure LoadPaths;
- begin
-   OnGetApplicationName := @GetAppName;
--  gpExePath := ExtractFilePath(TryReadAllLinks(ParamStrUTF8(0)));
-+//  gpExePath := ExtractFilePath(TryReadAllLinks(ParamStrUTF8(0)));
+   gpLngDir : String = '';  // path to language *.po files
+   gpPixmapPath : String = '';  // path to pixmaps
+@@ -29,7 +29,7 @@
+   gpExePath := ExtractFilePath(TryReadAllLinks(ParamStrUTF8(0)));
    DCDebug('Executable directory: ', gpExePath);
    
 -  gpGlobalCfgDir := gpExePath;
-+  gpGlobalCfgDir := '%%DATADIR%%';
++  //gpGlobalCfgDir := gpExePath;
    if gpCmdLineCfgDir <> EmptyStr then
    begin
      if GetPathType(gpCmdLineCfgDir) <> ptAbsolute then
-@@ -48,12 +48,12 @@
+@@ -48,13 +48,13 @@
    end;
  
    gpCfgDir := IncludeTrailingPathDelimiter(gpCfgDir);
 -  gpLngDir := gpExePath + 'language' + DirectorySeparator;
 -  gpPixmapPath := gpExePath + 'pixmaps' + DirectorySeparator;
-+  gpLngDir :=  '%%DATADIR%%/' + 'language' + DirectorySeparator;
++  gpLngDir := '%%DATADIR%%/' + 'language' + DirectorySeparator;
 +  gpPixmapPath := '%%DATADIR%%/' + 'pixmaps' + DirectorySeparator;
    gpCacheDir := GetAppCacheDir;
  
    // set up environment variables
--  mbSetEnvironmentVariable('commander_path', ExcludeTrailingBackslash(gpExePath));
-+  mbSetEnvironmentVariable('commander_path', ExcludeTrailingBackslash('%%DATADIR%%/'));
+-  mbSetEnvironmentVariable('COMMANDER_DRIVE', ExtractRootDir(gpExePath));
+-  mbSetEnvironmentVariable('COMMANDER_PATH', ExcludeTrailingBackslash(gpExePath));
++  mbSetEnvironmentVariable('COMMANDER_DRIVE', ExtractRootDir('%%DATADIR%%'));
++  mbSetEnvironmentVariable('COMMANDER_PATH', ExcludeTrailingBackslash('%%DATADIR%%'));
  end;
  
  end.
