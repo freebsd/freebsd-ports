1b54ce26b6491943b76cc294ae9b9254d488d7b4
obrien <obrien@FreeBSD.org>
2012-06-05 02:00:19 +0000

    Update contact information.
    Approved by:	skreuzer

---
+++ shells/pdksh/files/patch-sigact.c
@@ -0,0 +1,8 @@
+--- sigact.c.orig	1996-03-07 09:41:43.000000000 -0800
++++ sigact.c	2012-06-01 09:54:31.000000000 -0700
+@@ -104 +104 @@
+- *      Simon J. Gerraty <sjg@zen.void.oz.au>
++ *      Simon J. Gerraty <sjg@crufty.net>
+@@ -120 +120 @@
+- *      sjg@zen.void.oz.au
++ *      sjg@crufty.net
