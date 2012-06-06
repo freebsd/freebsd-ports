797e0a2521596d9e0e75002205cb5aa74d9f2a6e
beat <beat@FreeBSD.org>
2012-06-06 07:55:46 +0000

    - Update Firefox to 13.0 - Remove whitespace from pkg-descr - Use GCC 4.6 to fix build on newer FreeBSD versions - Use asm optimized YUV conversion and RGB scaling [1] - Update required nspr version [2]
    PR:		ports/168423 [1], ports/167431 [2]
    Submitted by:	Jan Beich <jbeich AT tormail.org [1],
    		Toomas Aas <toomas.aas AT raad.tartu.ee> [2]
    Security:	http://www.vuxml.org/freebsd/bfecf7c1-af47-11e1-9580-4061862b8c22.html

--- www/firefox/files/patch-xpcom_base_nsStackWalk.cpp
+++ www/firefox/files/patch-xpcom_base_nsStackWalk.cpp
@@ -1,8 +1,8 @@
---- xpcom/base/nsStackWalk.cpp.orig	2010-12-15 02:03:41.000000000 +0100
-+++ xpcom/base/nsStackWalk.cpp	2010-12-15 11:28:20.000000000 +0100
-@@ -1137,7 +1137,7 @@
- #define __USE_GNU
- #endif
+--- xpcom/base/nsStackWalk.cpp.orig	2012-05-29 00:57:31.000000000 +0200
++++ xpcom/base/nsStackWalk.cpp	2012-06-03 12:33:40.000000000 +0200
+@@ -57,7 +57,7 @@
+ };
+ static CriticalAddress gCriticalAddress;
  
 -#if defined(HAVE_DLOPEN) || defined(XP_MACOSX)
 +#if defined(HAVE_DLOPEN) || defined(XP_MACOSX) || defined(__FreeBSD__)
