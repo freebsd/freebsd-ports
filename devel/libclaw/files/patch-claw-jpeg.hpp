eaaeed6dd3bfc944bb577fd319055caae5a0a6c2
amdmi3 <amdmi3@FreeBSD.org>
2012-06-07 12:29:49 +0000

    - Update to 1.7.0

---
+++ devel/libclaw/files/patch-claw-jpeg.hpp
@@ -0,0 +1,10 @@
+--- claw/jpeg.hpp.orig	2011-06-12 14:01:53.000000000 +0400
++++ claw/jpeg.hpp	2012-06-06 19:38:07.828165860 +0400
+@@ -34,6 +34,7 @@
+ #include <setjmp.h>
+ #include <iostream>
+ #include <string>
++#include <cstdio>
+ 
+ extern "C"
+ {
