343a1c69b19f3acd42bdd479ef3da46025f242c9
Brendan Fabeny <bf@FreeBSD.org>
2012-06-08 22:28:55 +0000

    fix the build after the giflib update
    Reported by:	pav

--- graphics/exact-image/files/patch-codecs__gif.cc
+++ graphics/exact-image/files/patch-codecs__gif.cc
@@ -1,6 +1,24 @@
---- codecs/gif.cc.orig	2009-07-02 18:34:16.000000000 -0400
-+++ codecs/gif.cc	2009-07-02 18:38:40.000000000 -0400
-@@ -232,7 +232,7 @@
+--- codecs/gif.cc.orig	2010-03-03 16:04:44.000000000 -0500
++++ codecs/gif.cc	2012-06-08 14:47:50.000000000 -0400
+@@ -22,6 +22,17 @@
+ 
+ #include <iostream>
+ 
++void ExactImagePrintGifError(void)
++{
++  char *Err = GifErrorString();
++  if (Err != NULL)
++    std::cerr << "\nGIF-LIB error: " << Err << std::endl;
++  else
++    std::cerr << "\nGIF-LIB undefined error " << GifError() << "." << std::endl;
++}
++
++#define PrintGifError ExactImagePrintGifError
++
+ /* The way Interlaced image should. */
+ static const int InterlacedOffset[] = { 0, 4, 2, 1 };
+ 
+@@ -232,7 +243,7 @@
    }
    free (OutputBuffer);
  
