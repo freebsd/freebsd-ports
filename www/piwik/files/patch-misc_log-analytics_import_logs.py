104dbceaee8d2ab8b053d15aa615a246b97c965b
beech <beech@FreeBSD.org>
2012-06-08 19:26:13 +0000

    - Update to 1.8.1
    PR:		ports/168630
    Submitted by:	Geoffroy Desvernay <dgeo@centrale-marseille.fr>
    Approved by:	Hans Fr. Nordhaug <hans@nordhaug.priv.no> (maintainer)

---
+++ www/piwik/files/patch-misc_log-analytics_import_logs.py
@@ -0,0 +1,8 @@
+--- misc/log-analytics/import_logs.py.orig	2012-06-03 11:52:22.000000000 +0200
++++ misc/log-analytics/import_logs.py	2012-06-03 11:52:29.000000000 +0200
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python
++#!/usr/local/bin/python
+ # -*- coding: utf-8 -*-
+ #
+ # Piwik - Open source web analytics
