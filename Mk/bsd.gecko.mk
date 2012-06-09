a0d969aa9b8c359b053aa981e4c8c06a58771c45
beat <beat@FreeBSD.org>
2012-06-09 08:28:38 +0000

    - Update Thunderbird to 13.0 - Use GCC 4.6 to fix build on newer FreeBSD versions - Update required nspr version [1]
    PR:		ports/167431 [1]
    Submitted by:	Toomas Aas <toomas.aas AT raad.tartu.ee> [1]
    Security:	http://www.vuxml.org/freebsd/bfecf7c1-af47-11e1-9580-4061862b8c22.html

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -203,7 +203,7 @@ Gecko_Pre_Include=			bsd.gecko.mk
 #                         version is given by the maintainer via the port 
 #                         or by the user via defined variable try to find 
 #                         the highest stable installed version.
-#                         Available values: yes 10+ 12+ 10 12
+#                         Available values: yes 10+ 13+ 10 13
 #                         NOTE:
 #                         default value 10 is used in case of USE_THUNDERBIRD=yes
 #
@@ -373,11 +373,11 @@ _THUNDERBIRD_BUILD_DEPENDS=		yes
 .endif
 
 _THUNDERBIRD_DEFAULT_VERSION=	10
-_THUNDERBIRD_VERSIONS=			10 12
-_THUNDERBIRD_RANGE_VERSIONS=	10+ 12+
+_THUNDERBIRD_VERSIONS=			10 13
+_THUNDERBIRD_RANGE_VERSIONS=	10+ 13+
 
 # For specifying [10, ..]+
-_THUNDERBIRD_12P=	12 ${_THUNDERBIRD_10P}
+_THUNDERBIRD_13P=	13 ${_THUNDERBIRD_10P}
 _THUNDERBIRD_10P=	10
 
 # Set the default Thunderbird version and check if USE_THUNDERBIRD=yes was given
@@ -424,7 +424,7 @@ IGNORE=			cannot install: unknown Thunderbird version: thunderbird-${USE_THUNDER
 
 # Dependence lines for different Thunderbird versions
 10_DEPENDS=		${LOCALBASE}/lib/thunderbird/thunderbird:${PORTSDIR}/mail/thunderbird-esr
-12_DEPENDS=		${LOCALBASE}/lib/thunderbird/thunderbird:${PORTSDIR}/mail/thunderbird
+13_DEPENDS=		${LOCALBASE}/lib/thunderbird/thunderbird:${PORTSDIR}/mail/thunderbird
 
 # Add dependencies
 .if defined(USE_THUNDERBIRD)
