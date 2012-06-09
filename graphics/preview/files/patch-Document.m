aa87086bd673b5b2f42f0670f5b9bc9eb186efc2
Dirk Meyer <dinoex@FreeBSD.org>
2012-06-09 12:54:52 +0000

    - fix build with gnustep 1.24.0

---
+++ graphics/preview/files/patch-Document.m
@@ -0,0 +1,10 @@
+--- Document.m.orig	2005-06-28 10:25:25.000000000 +0200
++++ Document.m	2012-06-09 11:09:51.000000000 +0200
+@@ -35,6 +35,7 @@
+ 
+ #include <Foundation/NSNotification.h>
+ 
++#include <AppKit/AppKit.h>
+ #include <AppKit/NSApplication.h>
+ #include <AppKit/NSAffineTransform.h>
+ #include <AppKit/NSClipView.h>
