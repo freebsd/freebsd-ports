dabf08f6cfac1fc15801708f5e6735dd30349d8d
pav <pav@FreeBSD.org>
2012-06-10 14:39:02 +0000

    - Fix header files integrity when X11 option is disabled - Fix header files to allow pure C client app to be built - OPTIONSng
    PR:		ports/168714, ports/168756
    Submitted by:	Alexey Shuvaev <bugsbeastie@gmail.com>, Andrey Simonenko <simon@comsys.ntu-kpi.kiev.ua>

--- net/boinc-client/files/patch-api__boinc_api.h
+++ net/boinc-client/files/patch-api__boinc_api.h
@@ -1,11 +1,18 @@
---- api/boinc_api.h.orig	2012-04-20 16:15:13.000000000 +0200
-+++ api/boinc_api.h	2012-04-20 16:17:11.000000000 +0200
-@@ -29,6 +29,8 @@
- extern "C" {
- #endif
+--- api/boinc_api.h.orig	2012-04-13 09:59:23.000000000 +0200
++++ api/boinc_api.h	2012-06-06 14:40:16.000000000 +0200
+@@ -83,7 +83,6 @@
  
-+#include <cstddef>
-+
- // parameters passed to the BOINC runtime system
- //
- typedef struct BOINC_OPTIONS {
+ extern int boinc_init(void);
+ extern int boinc_finish(int status);
+-extern int boinc_temporary_exit(int delay, const char* reason=NULL);
+ extern int boinc_get_init_data_p(struct APP_INIT_DATA*);
+ extern int boinc_parse_init_data_file(void);
+ extern int boinc_send_trickle_up(char* variety, char* text);
+@@ -139,6 +138,7 @@
+     double cpu_time, double checkpoint_cpu_time, double _fraction_done,
+     int other_pid, double bytes_sent, double bytes_received
+ );
++extern int boinc_temporary_exit(int delay, const char* reason=NULL);
+ 
+ /////////// API ENDS HERE
+ 
