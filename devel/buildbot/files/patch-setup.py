cd918661bcf04ae79dbc3becf979a69ed6254580
tj <tj@FreeBSD.org>
2012-06-14 00:51:53 +0000

    - Take over maintainer. - Upgrade to 0.8.6p1.
    PR:		ports/163675
    Submitted by:	Michael Gmelin <freebsd@grem.de>
    Approved by:	<eadler,db> (mentors)

--- devel/buildbot/files/patch-setup.py
+++ devel/buildbot/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2011-02-10 18:06:05.000000000 +0100
-+++ setup.py	2011-02-10 18:06:15.000000000 +0100
-@@ -260,29 +260,6 @@
+--- setup.py.orig	2012-06-02 08:58:34.000000000 -0400
++++ setup.py	2012-06-02 08:58:58.000000000 -0400
+@@ -279,34 +279,6 @@
  py_25 = sys.version_info[0] > 2 or (sys.version_info[0] == 2 and sys.version_info[1] >= 5)
  py_26 = sys.version_info[0] > 2 or (sys.version_info[0] == 2 and sys.version_info[1] >= 6)
  
@@ -13,8 +13,12 @@
 -else:
 -    ## dependencies
 -    setup_args['install_requires'] = [
--        'twisted >= 2.0.0',
+-        'twisted >= 8.0.0',
 -        'Jinja2 >= 2.1',
+-        'sqlalchemy >= 0.6',
+-        # buildbot depends on sqlalchemy internals, and these are the tested
+-        # versions.
+-        'sqlalchemy-migrate ==0.6.0, ==0.6.1, ==0.7.0, ==0.7.1, ==0.7.2',
 -    ]
 -    # Python-2.6 and up includes json
 -    if not py_26:
@@ -26,6 +30,7 @@
 -
 -    if os.getenv('NO_INSTALL_REQS'):
 -        setup_args['install_requires'] = None
+-        setup_args['tests_require'] = None
 -
  setup(**setup_args)
  
