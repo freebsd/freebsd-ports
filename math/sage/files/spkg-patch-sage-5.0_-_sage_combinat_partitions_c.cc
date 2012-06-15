62b9986bbecf157efed2c074d98db90535a79404
stephen <stephen@FreeBSD.org>
2012-06-15 03:56:31 +0000

    - Update to 5.0.1.

--- math/sage/files/spkg-patch-sage-5.0_-_sage_combinat_partitions_c.cc
+++ math/sage/files/spkg-patch-sage-5.0_-_sage_combinat_partitions_c.cc
@@ -1,5 +1,5 @@
 --- sage-5.0.beta13/sage/combinat/partitions_c.cc-orig	2012-04-20 01:01:41.000000000 +0000
-+++ sage-5.0/sage/combinat/partitions_c.cc	2012-04-20 01:02:47.000000000 +0000
++++ sage-5.0.1/sage/combinat/partitions_c.cc	2012-04-20 01:02:47.000000000 +0000
 @@ -152,7 +152,7 @@
  const unsigned int double_precision = DBL_MANT_DIG;                         // The assumed precision of a double.
  
