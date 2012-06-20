6d3de5eebff9409ef8cf449da5cb5e13133eaf50
gahr <gahr@FreeBSD.org>
2012-06-20 14:42:16 +0000

    - Fix build on FreeBSD-10

--- lang/g95/files/patch-gcc__config__freebsd-spec.h
+++ lang/g95/files/patch-gcc__config__freebsd-spec.h
@@ -1,6 +1,13 @@
---- ../gcc-4.0.3/gcc/config/freebsd-spec.h.orig	2007-12-26 10:31:59.000000000 -0500
-+++ ../gcc-4.0.3/gcc/config/freebsd-spec.h	2007-12-26 10:32:21.000000000 -0500
-@@ -55,7 +55,7 @@
+--- ../gcc-4.0.3/gcc/config/freebsd-spec.h.orig	2005-07-20 13:12:58.000000000 +0200
++++ ../gcc-4.0.3/gcc/config/freebsd-spec.h	2012-06-20 15:57:35.000000000 +0200
+@@ -51,11 +51,13 @@
+ #define FBSD_TARGET_OS_CPP_BUILTINS()					\
+   do									\
+     {									\
+-	if (FBSD_MAJOR == 9)						\
++	if (FBSD_MAJOR == 10)						\
++	  builtin_define ("__FreeBSD__=10");			       	\
++	else if (FBSD_MAJOR == 9)						\
  	  builtin_define ("__FreeBSD__=9");			       	\
  	else if (FBSD_MAJOR == 8)					\
  	  builtin_define ("__FreeBSD__=8");			       	\
