d0216e6ee330e5412ba27472754dce5d2bb5b5f6
sunpoet <sunpoet@FreeBSD.org>
2012-06-24 15:14:03 +0000

    - Update to 1.1.0
    Changes:	http://matplotlib.sourceforge.net/users/whats_new.html
    PR:		ports/166329
    Submitted by:	Geoffrey Mainland <mainland@apeiron.net> (maintainer)

--- math/py-matplotlib/files/patch-setupext.py
+++ math/py-matplotlib/files/patch-setupext.py
@@ -1,6 +1,6 @@
---- setupext.py.orig    2010-07-07 05:41:55.000000000 +0400
-+++ setupext.py    2011-10-16 18:49:43.000000000 +0400
-@@ -68,6 +68,10 @@
+--- setupext.py.orig	2011-10-06 15:50:23.000000000 +0100
++++ setupext.py	2012-03-22 23:29:06.881660702 +0000
+@@ -70,6 +70,10 @@
      'freebsd4' : ['/usr/local', '/usr'],
      'freebsd5' : ['/usr/local', '/usr'],
      'freebsd6' : ['/usr/local', '/usr'],
@@ -11,36 +11,16 @@
      'sunos5' : [os.getenv('MPLIB_BASE') or '/usr/local',],
      'gnukfreebsd5' : ['/usr/local', '/usr'],
      'gnukfreebsd6' : ['/usr/local', '/usr'],
-@@ -789,7 +793,7 @@
-         module.libraries.extend(wxlibs)
-         return
- 
--    get_pkgconfig(module, '', flags='--cppflags --libs', pkg_config_exec='wx-config')
-+    get_pkgconfig(module, '', flags='--cppflags --libs', pkg_config_exec=wxconfig)
- 
- # Make sure you use the Tk version given by Tkinter.TkVersion
- # or else you'll build for a wrong version of the Tcl
-@@ -828,8 +832,12 @@
-                 gotit = False
- 
-     if gotit:
-+        try:
-+          tk_v = Tkinter.__version__.split()[-2]
-+        except (AttributeError, IndexError):
-+          tk_v = 'version not identified'
-         print_status("Tkinter", "Tkinter: %s, Tk: %s, Tcl: %s" %
--                     (Tkinter.__version__.split()[-2], Tkinter.TkVersion, Tkinter.TclVersion))
-+                     (tk_v, Tkinter.TkVersion, Tkinter.TclVersion))
-     else:
-         print_status("Tkinter", "no")
-     if explanation is not None:
-@@ -1092,9 +1100,9 @@
+@@ -1018,9 +1022,12 @@
  
          # Add final versions of directories and libraries to module lists
-         tcl_lib, tcl_inc, tk_lib, tk_inc = result
--        module.include_dirs.extend([tcl_inc, tk_inc])
--        module.library_dirs.extend([tcl_lib, tk_lib])
--        module.libraries.extend(['tk' + tk_ver, 'tcl' + tk_ver])
+         tcl_lib_dir, tcl_inc_dir, tcl_lib, tk_lib_dir, tk_inc_dir, tk_lib = result
+-        module.include_dirs.extend([tcl_inc_dir, tk_inc_dir])
+-        module.library_dirs.extend([tcl_lib_dir, tk_lib_dir])
+-        module.libraries.extend([tcl_lib, tk_lib])
++        #module.include_dirs.extend([tcl_inc_dir, tk_inc_dir])
++        #module.library_dirs.extend([tcl_lib_dir, tk_lib_dir])
++        #module.libraries.extend([tcl_lib, tk_lib])
 +        module.include_dirs.extend(["/usr/local/include/tcl8.5", "/usr/local/include/tk8.5"])
 +        module.library_dirs.extend(["/usr/local/lib/tcl8.5", "/usr/local/lib/tk8.5"])
 +        module.libraries.extend(['tk' + "85", 'tcl' + "85"])
