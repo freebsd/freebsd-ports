2d69c846f59e6ef0e27c0302d15b592209aa5f81
scheidell <scheidell@FreeBSD.org>
2012-06-24 17:36:25 +0000

    - Update to 20111108 - Add MSN 2011 protocol support
    PR:		ports/162511
    Submitted by:	"Mikhail T." <m.tsatsenko@gmail.com> (maintainer)

--- net-im/imspector/files/patch-icqprotocolplugin.cpp
+++ net-im/imspector/files/patch-icqprotocolplugin.cpp
@@ -1,6 +1,14 @@
---- icqprotocolplugin.cpp.orig	2011-06-10 23:51:17.870320723 +0400
-+++ icqprotocolplugin.cpp	2011-06-10 23:54:37.586036961 +0400
-@@ -673,7 +673,7 @@
+--- icqprotocolplugin.cpp.orig	2010-11-23 17:53:29.000000000 +0300
++++ icqprotocolplugin.cpp	2011-11-08 19:13:36.000000000 +0400
+@@ -10,6 +10,7 @@
+  * in the README file. */
+ 
+ #include "imspector.h"
++#include <sys/endian.h>
+ 
+ #define PLUGIN_NAME "ICQ-AIM IMSpector protocol plugin"
+ #define PROTOCOL_NAME "ICQ-AIM"
+@@ -675,7 +676,7 @@
  					size_t inbytesleft = mylength - 4;
  					size_t outbytesleft = BUFFER_SIZE - 1; /* Trailing \0 */
  					size_t result = iconv(iconv_utf16be_utf8,
@@ -9,3 +17,12 @@
  
  					if (result == (size_t) -1)
  					{
+@@ -810,7 +811,7 @@
+ 	GET_TYPE(uint16_t)
+ 
+ # if __BYTE_ORDER == __BIG_ENDIAN
+-	*rc = __bswap_16(*rc);
++	*rc = bswap16(*rc);
+ #endif
+ 
+ 	return true;
--- net-im/imspector/files/patch-main.cpp
+++ net-im/imspector/files/patch-main.cpp
@@ -1,14 +1,16 @@
---- main.cpp	Thu Mar 13 20:55:41 2008
-+++ main.cpp	Mon Mar 17 15:07:27 2008
-@@ -9,9 +9,9 @@
- 
+--- main.cpp.orig	2010-02-23 20:21:10.000000000 +0300
++++ main.cpp	2011-11-08 19:05:36.000000000 +0400
+@@ -17,11 +17,11 @@
  #include "imspector.h"
  
+ /*! The default location for where to find the config file. */
 -#define DEFAULT_CONFIG "/usr/etc/imspector/imspector.conf"
 +#define DEFAULT_CONFIG "%%PREFIX%%/etc/imspector/imspector.conf"
+ /*! The location of the logging process's UNIX socket. */
  #define LOGGING_SOCKET "/tmp/.imspectorlog"
+ /*! The default place for where to find plugins. */
 -#define DEFAULT_PLUGIN_DIR "/usr/lib/imspector"
 +#define DEFAULT_PLUGIN_DIR "%%PREFIX%%/lib/imspector"
  
- std::vector<class ProtocolPlugin> protocolplugins;
- std::vector<class FilterPlugin> filterplugins;
+ #define DEFAULT_RESPONSE_PREFIX "Message from IMSpector: -="
+ #define DEFAULT_RESPONSE_POSTFIX "=-"
