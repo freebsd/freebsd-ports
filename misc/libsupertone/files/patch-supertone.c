5c6ac40423e69bf45819353e5e16a54505c7452e
Max Khon <fjoe@FreeBSD.org>
2012-06-26 11:11:02 +0000

    New port: libsupertone
    This library is designed to support supervisory tone generation and detection
    for all parts of the world.
    
    WWW:    http://www.soft-switch.org/

---
+++ misc/libsupertone/files/patch-supertone.c
@@ -0,0 +1,10 @@
+--- supertone.c.orig	2012-06-26 18:01:41.000000000 +0700
++++ supertone.c	2012-06-26 18:01:54.000000000 +0700
+@@ -53,6 +53,7 @@
+ #include <libxml/xinclude.h>
+ 
+ #include <spandsp.h>
++#include <spandsp/expose.h>
+ 
+ #include "supertone.h"
+ #include "libsupertone.h"
