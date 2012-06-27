56faf06df7f873f6a62d77f53f10f58e61a3b322
Brendan Fabeny <bf@FreeBSD.org>
2012-06-27 07:27:07 +0000

    update to 2.7, change options to the new format, and sort options-handling

--- www/links/files/patch-types.c
+++ www/links/files/patch-types.c
@@ -1,12 +1,12 @@
---- types.c.orig	2008-03-19 18:18:18.614955994 -0400
-+++ types.c	2008-03-19 18:21:18.345245179 -0400
-@@ -820,7 +820,8 @@
+--- types.c.orig	2012-06-06 15:49:37.000000000 -0400
++++ types.c	2012-06-27 02:28:56.000000000 -0400
+@@ -841,7 +841,8 @@
  			ext = NULL;
  		}
  	if (ext) while (ext[extl] && ext[extl] != '.' && !dir_sep(ext[extl]) && !end_of_dir(url, ext[extl])) extl++;
--	if ((extl == 3 && !casecmp(ext, "htm", 3)) ||
+-	if ((extl == 3 && !casecmp(ext, cast_uchar "htm", 3)) ||
 +	if (force_html || 
-+	    (extl == 3 && !casecmp(ext, "htm", 3)) ||
- 	    (extl == 4 && !casecmp(ext, "html", 4))) return stracpy("text/html");
++	    (extl == 3 && !casecmp(ext, cast_uchar "htm", 3)) ||
+ 	    (extl == 4 && !casecmp(ext, cast_uchar "html", 4))) return stracpy(cast_uchar "text/html");
  	foreach(e, extensions) {
  		unsigned char *fname = NULL;
