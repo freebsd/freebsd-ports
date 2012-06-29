e92c4a1964c6eb10dde44278a735d75d575b4e44
scheidell <scheidell@FreeBSD.org>
2012-06-29 23:06:06 +0000

    - Update to 1.9 [1] - Detection of insufficient memory [1] - Change %%  SUB vaes from fixed at python 27 to use any installed version of python [1] - Fix syntax of non system include "" vs <> [2]
    PR:		ports/168974 [1]
    Submitted by:	David Naylor <naylor.b.david@gmail.com> (maintainer) [1]
    Reviewed by:	scheidel@ (me) [2]

---
+++ lang/pypy/files/patch-lib__pypy1.9__lib_pypy__ctypes_config_cache__rebuild.py
@@ -0,0 +1,11 @@
+--- lib/pypy1.9/lib_pypy/ctypes_config_cache/rebuild.py~	2012-06-22 11:42:55.000000000 +0200
++++ lib/pypy1.9/lib_pypy/ctypes_config_cache/rebuild.py	2012-06-22 11:43:12.000000000 +0200
+@@ -6,7 +6,7 @@
+ # get the correct path
+ import os.path
+ this_dir = os.path.dirname(__file__)
+-autopath_py = os.path.join(this_dir, '../../pypy/tool/autopath.py')
++autopath_py = os.path.join(this_dir, '../../../../pypy/tool/autopath.py')
+ autopath_py = os.path.abspath(autopath_py)
+ execfile(autopath_py, dict(__name__='autopath', __file__=autopath_py))
+ 
--- lang/pypy/files/patch-pypy__rpython__tool__rffi_platform.py
+++ lang/pypy/files/patch-pypy__rpython__tool__rffi_platform.py
@@ -1,6 +1,6 @@
 --- pypy/rpython/tool/rffi_platform.py~	2011-11-19 09:44:54.000000000 +0200
 +++ pypy/rpython/tool/rffi_platform.py	2011-11-23 20:17:30.000000000 +0200
-@@ -789,7 +789,9 @@
+@@ -805,7 +805,9 @@
          includes=['gc.h']
      else:
          library_dir = ''
