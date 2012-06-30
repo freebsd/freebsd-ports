7e575ae7425cf0ad8ab7d848bb0f1c2366814191
Cy Schubert <cy@FreeBSD.org>
2012-06-30 02:14:20 +0000

    Fix segmentation fault.

--- sysutils/hdrecover/files/patch-hdrecover.cpp
+++ sysutils/hdrecover/files/patch-hdrecover.cpp
@@ -1,5 +1,5 @@
 --- hdrecover.cpp.orig	2011-09-25 04:48:10.000000000 -0700
-+++ hdrecover.cpp	2012-06-29 12:07:43.004361705 -0700
++++ hdrecover.cpp	2012-06-29 16:02:39.000000000 -0700
 @@ -26,7 +26,11 @@
  #include <sys/stat.h>
  #include <fcntl.h>
@@ -12,3 +12,12 @@
  #include <errno.h>
  #include <string.h>
  #include <time.h>
+@@ -260,7 +264,7 @@
+ 		sprintf(p, "%d seconds", remaining);
+ 	    }
+ 	    printf("Sector %Ld (%02d%%) ETR: %s\n",
+-		   sectornum, (int) ((sectornum * 100) / length), rs);
++		   (long) sectornum, (long int) ((sectornum * 100) / length), rs);
+ 	}
+     }
+ 
