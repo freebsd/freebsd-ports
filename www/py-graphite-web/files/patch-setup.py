e3cede6da6be2a2c2d84a42cae00679094937c5d
sunpoet <sunpoet@FreeBSD.org>
2012-07-04 08:39:01 +0000

    - Update to 0.9.10 - While I'm here:   - Update MASTER_SITES and WWW   - Do not use 'cp -r' which is strongly discouraged   - Pet portlint
    Changes:	http://graphite.wikidot.com/
    PR:		ports/169334
    Submitted by:	Brad Davis <brd@FreeBSD.org>
    Approved by:	Dan Rowe <drowe@wayfair.com> (maintainer)

--- www/py-graphite-web/files/patch-setup.py
+++ www/py-graphite-web/files/patch-setup.py
@@ -1,9 +1,9 @@
---- setup.py.orig	2011-04-03 22:20:03.000000000 -0400
-+++ setup.py	2011-05-24 11:48:33.000000000 -0400
+--- setup.py.ori	2012-06-21 15:38:54.044808453 -0700
++++ setup.py	2012-06-21 15:41:16.067809618 -0700
 @@ -15,11 +15,11 @@
  storage_dirs = []
  
- for subdir in ('whisper', 'lists', 'rrd', 'log', 'log/webapp'):
+ for subdir in ('whisper', 'rrd', 'log', 'log/webapp'):
 -  storage_dirs.append( ('storage/%s' % subdir, []) )
 +  storage_dirs.append( ('graphite/storage/%s' % subdir, []) )
  
@@ -20,6 +20,6 @@
  
 -conf_files = [ ('conf', glob('conf/*.example')) ]
 +conf_files = [ ('/usr/local/etc/graphite', glob('conf/*.example')) ]
+ examples = [ ('examples', glob('examples/example-*')) ]
  
  setup(
-   name='graphite-web',
