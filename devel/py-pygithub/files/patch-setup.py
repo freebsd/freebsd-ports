4c65b525194f99c6cb66f615a769d712a1ad3f2a
Sofian Brabez <sbz@FreeBSD.org>
2012-07-05 08:51:42 +0000

    - Update to 1.1
    PR:		ports/169537
    Submitted by:	koobs <koobs.freebsd at gmail.com> (maintainer)

--- devel/py-pygithub/files/patch-setup.py
+++ devel/py-pygithub/files/patch-setup.py
@@ -1,12 +1,12 @@
---- ./setup.py.orig	2012-06-04 19:29:37.000000000 +1000
-+++ ./setup.py	2012-06-04 19:29:46.000000000 +1000
+--- ./setup.py.orig	2012-06-29 17:52:26.000000000 +1000
++++ ./setup.py	2012-06-29 17:52:35.000000000 +1000
 @@ -54,9 +54,6 @@
      packages = [
          "github",
      ],
--    data_files = [
--        ( "", [ "ReadMe.md", "COPYING", "COPYING.LESSER", "doc/ReferenceOfClasses.md", "doc/ReferenceOfApis.md", "doc/Design.md" ] ),
--    ],
-     classifiers=[
+-    package_data = {
+-        "github": [ "../ReadMe.md", "../COPYING*", "../doc/*.md" ]
+-    },
+     classifiers = [
          "Development Status :: 5 - Production/Stable",
          "Environment :: Web Environment",
