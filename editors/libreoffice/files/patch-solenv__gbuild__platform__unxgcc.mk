296845293655b1d6924f3ecadfdfebfeec8c3698
jkim <jkim@FreeBSD.org>
2012-07-10 23:53:13 +0000

    - Add comments to note a clang bug and and the patched revision. - Add a knob WITH_SYSTEM_CLANG to force build with clang from base system. This is very useful when the user has patched the system clang, i.e., contrib/llvm/tools/clang/lib/Sema/SemaDeclCXX.cpp. - Temporarily disable -Wshadow and -Wunused-parameter.  These options caused too many warnings for system boost and made it really hard to debug problems. - Remove temporary clang++ options, which are no longer necessary. - Remove a duplicate patch for solenv/gbuild/platform/com_GCC_defs.mk.

--- editors/libreoffice/files/patch-solenv__gbuild__platform__unxgcc.mk
+++ editors/libreoffice/files/patch-solenv__gbuild__platform__unxgcc.mk
@@ -1,5 +1,17 @@
---- ./solenv/gbuild/platform/unxgcc.mk.orig	2012-03-27 18:22:01.000000000 +0200
-+++ ./solenv/gbuild/platform/unxgcc.mk	2012-04-16 14:24:48.000000000 +0200
+--- ./solenv/gbuild/platform/unxgcc.mk.orig	2012-05-22 08:48:18.000000000 -0400
++++ ./solenv/gbuild/platform/unxgcc.mk	2012-07-10 18:27:15.000000000 -0400
+@@ -58,10 +58,10 @@
+ gb_CXXFLAGS := \
+ 	$(gb_CXXFLAGS_COMMON) \
+ 	-fPIC \
+-	-Wshadow \
+ 	-Wsign-promo \
+ 	-Woverloaded-virtual \
+ 	-Wno-non-virtual-dtor \
++	-Wno-unused-parameter \
+ 
+ ifeq ($(HAVE_GCC_VISIBILITY_FEATURE),TRUE)
+ gb_COMPILERDEFS += \
 @@ -103,7 +103,7 @@
  ifeq ($(HAVE_CXX0X),TRUE)
  #Currently, as well as for its own merits, c++11/c++0x mode allows use to use
