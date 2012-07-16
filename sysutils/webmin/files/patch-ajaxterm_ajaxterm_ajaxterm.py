558da09404490efe410fc0f3e1fdad9bf1c6ec42
olgeni <olgeni@FreeBSD.org>
2012-07-16 19:01:18 +0000

    Fixes for Python 3.x compatibility (ajaxterm).

--- sysutils/webmin/files/patch-ajaxterm_ajaxterm_ajaxterm.py
+++ sysutils/webmin/files/patch-ajaxterm_ajaxterm_ajaxterm.py
@@ -12,3 +12,23 @@ $FreeBSD$
  			else:
  				sys.stdout.write("Login: ")
  				login=sys.stdin.readline().strip()
+@@ -549,16 +549,16 @@
+ 				file(o.pidfile,'w+').write(str(pid)+'\n')
+ 			except:
+ 				pass
+-			print 'AjaxTerm at http://localhost:%s/ pid: %d' % (o.port,pid)
++			print ('AjaxTerm at http://localhost:%s/ pid: %d' % (o.port,pid))
+ 			sys.exit(0)
+ 	else:
+-		print 'AjaxTerm at http://localhost:%s/' % o.port
++		print ('AjaxTerm at http://localhost:%s/' % o.port)
+ 	at=AjaxTerm(o.cmd,o.index_file)
+ #	f=lambda:os.system('firefox http://localhost:%s/&'%o.port)
+ #	qweb.qweb_wsgi_autorun(at,ip='localhost',port=int(o.port),threaded=0,log=o.log,callback_ready=None)
+ 	try:
+ 		qweb.QWebWSGIServer(at,ip='localhost',port=int(o.port),threaded=0,log=o.log).serve_forever()
+-	except KeyboardInterrupt,e:
++	except KeyboardInterrupt as e:
+ 		sys.excepthook(*sys.exc_info())
+ 	at.multi.die()
+ 
