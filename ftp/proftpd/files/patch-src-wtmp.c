e4f2553f64fc06c9aa463c726a1054dae03ba042
mm <mm@FreeBSD.org>
2012-07-17 14:38:28 +0000

    Unbreak ProFTPD on sparc [1]. Significantly simplify proftpd port options. Slave ports will follow in separate commits.
    PR:		169644 [1]
    Submitted by:	Dmitry Afanasiev [1]

--- ftp/proftpd/files/patch-src-wtmp.c
+++ ftp/proftpd/files/patch-src-wtmp.c
@@ -1,5 +1,23 @@
---- src/wtmp.c.orig	2010-07-05 09:52:50.147213835 +0200
-+++ src/wtmp.c	2010-07-05 09:53:25.075513542 +0200
+--- src/wtmp.c.orig	2011-05-23 23:22:24.000000000 +0200
++++ src/wtmp.c	2012-07-17 16:19:34.188505949 +0200
+@@ -42,7 +42,7 @@
+     !(defined(LINUX) || defined(__hpux) || defined (_AIX))
+   /* This "auxilliary" utmp doesn't exist under linux. */
+ 
+-#if defined(__sparcv9) && !defined(__NetBSD__)
++#if defined(__sparcv9) && !defined(__NetBSD__) && !defined(__FreeBSD__)
+   struct futmpx utx;
+   time_t t;
+ 
+@@ -97,7 +97,7 @@
+ #else /* SVR4 */
+     utx.ut_syslen = strlen(utx.ut_host)+1;
+ 
+-#  ifdef __sparcv9
++#  ifdef __sparcv9 && !defined(__FreeBSD__)
+     time(&t);
+     utx.ut_tv.tv_sec = (time32_t)t;
+ #  else
 @@ -126,7 +126,9 @@
    }
  
