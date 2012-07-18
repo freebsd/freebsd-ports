26ad5734253fb584cb1676b17f47ae8a787c6376
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2012-07-18 12:38:45 +0000

    - Update to 1.3 - Remove LICENSE_FILE
    Changes:
    
    1.3: (2012-07-13)
    - Implement markdown rendering
    - GitAuthor.date is now a datetime, thank you bilderbuchi
    - Fix documentation of Github.get_gist: id is a string, not an integer
    
    1.2: (2012-06-29)
    - Implement legacy search APIs, thank you kukuts for telling me Github had
      released them
    - Fix a bug with issue labels containing spaces, thank you philipkimmey for
      detecting the bug and fixing it
    - Clarify how collections of objects are returned by get_* methods, thank you
      bilderbuchi for asking
    
    PR:		ports/169848
    Submitted by:	Kubilay Kocak <koobs.freebsd@gmail.com> (maintainer)

--- devel/py-pygithub/files/patch-setup.py
+++ devel/py-pygithub/files/patch-setup.py
@@ -1,11 +1,11 @@
---- ./setup.py.orig	2012-06-29 17:52:26.000000000 +1000
-+++ ./setup.py	2012-06-29 17:52:35.000000000 +1000
+--- ./setup.py.orig	2012-07-13 21:42:12.000000000 +1000
++++ ./setup.py	2012-07-14 19:15:08.000000000 +1000
 @@ -54,9 +54,6 @@
      packages = [
          "github",
      ],
 -    package_data = {
--        "github": [ "../ReadMe.md", "../COPYING*", "../doc/*.md" ]
+-        "github": [ "ReadMe.md", "COPYING*", "doc/*.md" ]
 -    },
      classifiers = [
          "Development Status :: 5 - Production/Stable",
