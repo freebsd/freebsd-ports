688542680b84e0ebcdd18553a63fd8121dda06f4
sperber <sperber@FreeBSD.org>
2012-07-19 17:25:31 +0000

    - Update to version 1.5.0 - Add WWW: line - Switch to optionsng - Add LICENSE
    PR:           ports/169407
    Submitted by: KATO Tsuguru <tkato432 _at_ yahoo.com>
    Approved by:  beat (mentor)

--- devel/cil/files/patch-bin_teetwo.sh
+++
@@ -1,8 +0,0 @@
---- bin/teetwo.orig	2011-11-13 08:39:24.000000000 +0100
-+++ bin/teetwo	2011-11-13 08:39:27.000000000 +0100
-@@ -1,4 +1,4 @@
--#!/bin/bash
-+#!/bin/sh
- # run a command, sending stdout to one file and stderr to another,
- # but also sending both to this process' stdout/stderr, respectively
- 
