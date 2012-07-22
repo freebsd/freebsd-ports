11a42e9792c631360b731be5d89fcb90092d38f9
Steve Wills <swills@FreeBSD.org>
2012-07-22 14:04:11 +0000

    - Make patches for ruby19 environment - Fix Makefile - SUB_LIST: to work rc.d scripts proper. - PLIST_SUB: to change pkg-plist based on Make variables.
    PR:		ports/169345
    Submitted by:	Masaki TAGAWA <masaki@club.kyutech.ac.jp>
    Approved by:	maintainer timeout (yaizawa@2004.jukuin.keio.ac.jp, >3 weeks)

---
+++ mail/quickml/files/extra-patch-ruby19-mail.rb
@@ -0,0 +1,7 @@
+--- lib/quickml/mail.rb.orig	2012-06-21 16:10:20.000000000 +0900
++++ lib/quickml/mail.rb	2012-06-21 16:11:01.000000000 +0900
+@@ -1,3 +1,4 @@
++#coding: euc-jp
+ #
+ # quickml/mail - a part of quickml server
+ #
