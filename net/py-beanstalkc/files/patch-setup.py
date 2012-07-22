752176fd8cb2c0ddaebdb52553e8891fd3fb89e8
nivit <nivit@FreeBSD.org>
2012-07-22 11:57:35 +0000

    - Convert to the new options framework - Add zip_safe=False to setup() (with files/patch-setup.py) - Add pkg-plist
    Buildlog@redports:	http://goo.gl/X3Ah4
    
    PR:		ports/169856
    Submitted by:	Kubilay Kocak <koobs.freebsd at gmail.com> (maintainer)

---
+++ net/py-beanstalkc/files/patch-setup.py
@@ -0,0 +1,8 @@
+--- ./setup.py.orig	2012-07-15 01:01:36.000000000 +1000
++++ ./setup.py	2012-07-15 01:01:49.000000000 +1000
+@@ -30,4 +30,5 @@
+         'Programming Language :: Python',
+         'Topic :: Software Development :: Libraries :: Python Modules',
+     ],
++    zip_safe=False,
+ )
