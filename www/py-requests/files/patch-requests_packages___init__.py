61538bf058dd133cddb1801158d75700e6451c48
rm <rm@FreeBSD.org>
2012-07-28 17:25:24 +0000

    - update to 0.13.5 - pass maintainership to submitter (approved in ports/168569) - remove LICENSE_FILE - use bundled urllib3 and chardet because upstream bundle them with   local modifications, needed for py-requests to work. Remove related patches - add dependency on security/py-oauthlib - set zip_safe=False to setup.py to install egg extracted - move package list to pkg_plist
    While here:
    - tab -> space in pkg-descr:WWW
    - change USE_PYTHON to -3.1, because 2.6 is already bottom corner in the tree
    
    This port doesn't actually packages with python3 because it's dependency
    (security/py-certify) doesn't.
    
    PR:		170226
    Submitted by:	Kubilay Kocak <koobs.freebsd at gmail dot com>
    Approved by:	olivierd (maintainer)

--- www/py-requests/files/patch-requests_packages___init__.py
+++
@@ -1,7 +0,0 @@
---- requests/packages/__init__.py.orig	2011-12-06 23:55:15.000000000 +0100
-+++ requests/packages/__init__.py	2012-01-12 18:46:03.000000000 +0100
-@@ -1,3 +1,3 @@
- from __future__ import absolute_import
- 
--from . import urllib3
-+#from . import urllib3
