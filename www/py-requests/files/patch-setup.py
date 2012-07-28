2018cda1106ee3ff6031c2c930ba6cfa7ca57c4d
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2012-07-28 17:25:24 +0000

    - update to 0.13.5 - pass maintainership to submitter (approved in ports/168569) - remove LICENSE_FILE - use bundled urllib3 and chardet because upstream bundle them with   local modifications, needed for py-requests to work. Remove related patches - add dependency on security/py-oauthlib - set zip_safe=False to setup.py to install egg extracted - move package list to pkg_plist
    While here:
    - tab -> space in pkg-descr:WWW
    - change USE_PYTHON to -3.1, because 2.6 is already bottom corner in the tree
    
    This port doesn't actually packages with python3 because it's dependency
    (security/py-certify) doesn't.
    
    PR:		170226
    Submitted by:	Kubilay Kocak <koobs.freebsd at gmail dot com>
    Approved by:	olivierd (maintainer)

--- www/py-requests/files/patch-setup.py
+++ www/py-requests/files/patch-setup.py
@@ -1,54 +1,37 @@
---- setup.py.orig	2012-05-08 06:56:28.000000000 +0200
-+++ setup.py	2012-05-19 13:42:13.000000000 +0200
-@@ -11,7 +11,7 @@
- from requests.compat import is_py3
- 
- try:
--    from setuptools import setup
-+    from setuptools import setup, find_packages
-     # hush pyflakes
-     setup
- except ImportError:
-@@ -21,31 +21,8 @@
+--- ./setup.py.orig	2012-07-27 17:22:56.000000000 +1000
++++ ./setup.py	2012-07-28 10:46:43.000000000 +1000
+@@ -15,8 +15,6 @@
      os.system('python setup.py sdist upload')
      sys.exit()
  
--packages = [
--    'requests',
--    'requests.packages',
--    'requests.packages.urllib3',
--    'requests.packages.urllib3.packages',
--    'requests.packages.urllib3.packages.ssl_match_hostname',
--    'requests.packages.urllib3.packages.mimetools_choose_boundary',
--]
+-os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
 -
--# certifi is a Python package containing a CA certificate bundle for SSL verification.
--# On certain supported platforms (e.g., Red Hat / Debian / FreeBSD), Requests can
--# use the system CA bundle instead; see `requests.utils` for details.
--# If your platform is supported, set `requires` to [] instead:
--requires = ['certifi>=0.0.7']
--
--# chardet is used to optimally guess the encodings of pages that don't declare one.
--# At this time, chardet is not a required dependency. However, it's sufficiently
--# important that pip/setuptools should install it when it's unavailable.
--if is_py3:
--    chardet_package = 'chardet2'
--else:
--    chardet_package = 'chardet>=1.0.0'
--    requires.append('oauthlib>=0.1.0,<0.2.0')
--
--requires.append(chardet_package)
-+# FreeBSD ports tree, provides only chardet2
-+requires = ['certifi>=0.0.7', 'chardet>=2.0.0']
+ packages = [
+     'requests',
+     'requests.packages',
+@@ -27,11 +25,6 @@
  
- # The async API in requests.async requires the gevent package.
- # This is also not a required dependency.
-@@ -62,7 +39,7 @@
-     author='Kenneth Reitz',
-     author_email='me@kennethreitz.com',
-     url='http://python-requests.org',
--    packages=packages,
-+    packages=find_packages(exclude=['requests.packages.url*']),
-     package_data={'': ['LICENSE', 'NOTICE']},
+ if is_py2:
+     packages.extend([
+-        'requests.packages.oauthlib',
+-        'requests.packages.oauthlib.oauth1',
+-        'requests.packages.oauthlib.oauth1.rfc5849',
+-        'requests.packages.oauthlib.oauth2',
+-        'requests.packages.oauthlib.oauth2.draft25',
+         'requests.packages.chardet',
+     ])
+ else:
+@@ -52,6 +45,7 @@
+     package_data={'': ['LICENSE', 'NOTICE'], 'requests': ['*.pem']},
+     package_dir={'requests': 'requests'},
      include_package_data=True,
++    exclude_package_data={'': ['oauthlib/*']},
      install_requires=requires,
+     license=open('LICENSE').read(),
+     classifiers=(
+@@ -66,4 +60,5 @@
+         'Programming Language :: Python :: 3.0',
+         'Programming Language :: Python :: 3.1',
+     ),
++    zip_safe=False,
+ )
