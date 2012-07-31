3ae6fd417b4313c9efd2a275f16dfbcd5911cccc
Sergey A. Osokin <osa@FreeBSD.org>
2012-07-31 17:50:50 +0000

    Update:
    o) third-party rtmp module from 0.1.10 to 0.2.3;		(*)
    o) prefix path for third-party sflow module.
    
    Requested by:	Rudolph Sand aka r dot sand at new-net dot net	(*)

--- www/nginx/files/extra-patch-ngx_rtmp_exec_module.c
+++ www/nginx/files/extra-patch-ngx_rtmp_exec_module.c
@@ -1,5 +1,5 @@
---- ../arut-nginx-rtmp-module-80db4c4/ngx_rtmp_exec_module.c.orig	2012-06-14 15:14:50.000000000 +0400
-+++ ../arut-nginx-rtmp-module-80db4c4/ngx_rtmp_exec_module.c	2012-06-14 15:15:06.000000000 +0400
+--- ../arut-nginx-rtmp-module-f7f24ba/ngx_rtmp_exec_module.c.orig	2012-07-31 20:59:43.000000000 +0400
++++ ../arut-nginx-rtmp-module-f7f24ba/ngx_rtmp_exec_module.c	2012-07-31 21:00:28.000000000 +0400
 @@ -4,7 +4,11 @@
  
  
