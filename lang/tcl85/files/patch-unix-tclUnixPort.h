b06433da21b36da7d3f920cd3fe4ac70c5a23a78
mm <mm@FreeBSD.org>
2012-08-03 09:17:41 +0000

    Update tcl85/tk85 to 8.5.12

--- lang/tcl85/files/patch-unix-tclUnixPort.h
+++ lang/tcl85/files/patch-unix-tclUnixPort.h
@@ -1,12 +1,13 @@
---- tclUnixPort.h.orig	Wed Jan 17 13:14:18 2007
-+++ tclUnixPort.h	Wed Jan 17 13:08:39 2007
-@@ -628,6 +628,9 @@
- EXTERN struct tm *     	TclpLocaltime(CONST time_t *);
- EXTERN struct tm *     	TclpGmtime(CONST time_t *);
- EXTERN char *          	TclpInetNtoa(struct in_addr);
-+#ifdef inet_ntoa
-+#	undef inet_ntoa
-+#endif
+--- tclUnixPort.h.orig	2012-08-02 15:43:23.269506562 +0200
++++ tclUnixPort.h	2012-08-02 15:43:25.167507612 +0200
+@@ -631,7 +631,9 @@
+ EXTERN struct tm *TclpGmtime(CONST time_t *);
  /* #define localtime(x)	TclpLocaltime(x)
   * #define gmtime(x)	TclpGmtime(x)    */
- #   undef inet_ntoa
+-#   undef inet_ntoa
++#   ifdef inet_ntoa
++#       undef inet_ntoa
++#   endif
+ #   define inet_ntoa(x)	TclpInetNtoa(x)
+ #   ifdef HAVE_PTHREAD_ATTR_GET_NP
+ #	define TclpPthreadGetAttrs	pthread_attr_get_np
