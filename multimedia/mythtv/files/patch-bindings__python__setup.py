988408a7e62c51d245e466f5b4340f52a3131f90
Bernhard Froehlich <decke@FreeBSD.org>
2012-08-04 08:10:48 +0000

    - Update to 0.25.2 - Fix Perl and Python bindings and enable per default - Fix PHP bindings for PHP 5.4

--- multimedia/mythtv/files/patch-bindings__python__setup.py
+++ multimedia/mythtv/files/patch-bindings__python__setup.py
@@ -4,8 +4,8 @@
  
  setup(
          name='MythTV',
--        version='0.24.0',
-+        version='0.25.0',
+-        version='0.25.0',
++        version='0.25.2',
          description='MythTV Python bindings.',
          long_description='Provides canned database and protocol access to the MythTV database, mythproto, mythxml, and frontend remote control.',
          packages=['MythTV', 'MythTV/tmdb', 'MythTV/tmdb3', 'MythTV/ttvdb', 'MythTV/wikiscripts'],
