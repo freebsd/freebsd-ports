47ebbc6d924942da9e1c09bf5cc493f57fcf788c
Hiroki Sato <hrs@FreeBSD.org>
2012-08-04 00:56:02 +0000

    Fix compilation issue on emacs24.
    PR:		ports/170363

---
+++ japanese/lookup/files/patch-lisp-ndic.el
@@ -0,0 +1,8 @@
+--- lisp/ndic.el.orig	2012-08-04 09:49:32.000000000 +0900
++++ lisp/ndic.el	2012-08-04 09:49:36.000000000 +0900
+@@ -1,4 +1,4 @@
+-e;;; ndic.el --- Lookup by free dictionaries
++;;; ndic.el --- Lookup by free dictionaries
+ ;; Copyright (C) 1999 Lookup Development Team <lookup@ring.gr.jp>
+ 
+ ;; Author: Keisuke Nishida <kei@psn.net>
