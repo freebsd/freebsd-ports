0337d310c560ce70b8871cf9a02bcb615b22569d
rm <rm@FreeBSD.org>
2012-08-08 18:26:40 +0000

    - update to 2.3 - adjust local patch for new version - tab -> space in pkg-descr:WWW
    changelog:
    http://mercurial.selenic.com/wiki/WhatsNew#Mercurial_2.3_.282012-08-01.29

--- devel/mercurial/files/patch-setup.py
+++ devel/mercurial/files/patch-setup.py
@@ -1,19 +1,10 @@
---- setup 1.py	2012-06-02 08:48:21.000000000 +0400
-+++ setup.py	2012-06-12 17:05:27.091982103 +0400
-@@ -223,6 +223,7 @@
+--- setup.py.orig	2012-08-02 10:49:31.000000000 +0400
++++ setup.py	2012-08-08 18:34:01.000000000 +0400
+@@ -227,6 +227,7 @@
      description = "build translations (.mo files)"
  
      def run(self):
-+%%NLS%%        return
++        %%NLS%%return
          if not find_executable('msgfmt'):
              self.warn("could not find msgfmt executable, no translations "
                       "will be built")
-@@ -385,7 +386,7 @@
-             'install_scripts': hginstallscripts}
- 
- packages = ['mercurial', 'mercurial.hgweb',
--            'mercurial.httpclient', 'mercurial.httpclient.tests',
-+            'mercurial.httpclient',
-             'hgext', 'hgext.convert', 'hgext.highlight', 'hgext.zeroconf',
-             'hgext.largefiles']
- 
