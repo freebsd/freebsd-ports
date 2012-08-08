0e96986ab985575fcdde5046cb73b58fb2cb9512
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2012-08-08 07:18:24 +0000

    - update to 4.0.9 - adjust local patch to new version - pet portlint (drop shlib versions in LIB_DEPENDS) - tab -> space change in pkg-plist:WWW

--- graphics/rawtherapee/files/patch-AboutThisBuild.cmake
+++ graphics/rawtherapee/files/patch-AboutThisBuild.cmake
@@ -1,12 +1,13 @@
---- ./About-Linux.cmake.orig	2012-01-31 05:00:05.000000000 +0400
-+++ ./About-Linux.cmake	2012-03-29 19:28:04.000000000 +0400
-@@ -1,27 +1,8 @@
- # cmakefile executed within a makefile target
+--- ./AboutThisBuild.cmake.orig	2012-06-03 19:56:20.000000000 +0400
++++ ./AboutThisBuild.cmake	2012-08-07 15:18:18.000000000 +0400
+@@ -8,28 +8,9 @@
+   find_file(HG_CMD hg)
+   set (SHELL "/bin/bash")
+ else (WIN32) # Linux
+-  find_file(HG_CMD hg)
+   set (SHELL "/bin/bash")
+ endif (WIN32)
  
- # we look for the hg command in this paths by order of preference
--find_file(HG_CMD hg)
--
--
 -# the hg command is looked for again, at build time
 -if (HG_CMD STREQUAL HG_CMD-NOTFOUND)
 -  message(FATAL_ERROR "hg command not found!")
@@ -14,8 +15,7 @@
 -  message(STATUS "hg command found: ${HG_CMD}")
 -endif (HG_CMD STREQUAL HG_CMD-NOTFOUND)
 -
- set (OUT_FILE "${SRC_DIR}/AboutThisBuild.txt")
--set (SHELL "/bin/bash")
+-set (OUT_FILE "${SRC_DIR}/AboutThisBuild.txt")
 -# there should be a better way of getting the compiler + compiler version, not restrained to Gcc
 -execute_process(COMMAND gcc -dumpversion OUTPUT_VARIABLE GCC_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
 -execute_process(COMMAND ${HG_CMD} -R ${SRC_DIR} branch OUTPUT_VARIABLE HG_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -25,7 +25,7 @@
 -if (CACHE_NAME_SUFFIX STREQUAL "")
 -  execute_process(COMMAND ${HG_CMD} parents --template={latesttag} WORKING_DIRECTORY ${SRC_DIR} OUTPUT_VARIABLE CACHE_NAME_SUFFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
 -endif ()
-+set (SHELL "/bin/sh")
- 
+-
  # build version.h from template
  configure_file (${SRC_DIR}/rtgui/version.h.in ${SRC_DIR}/rtgui/version.h)
+ # build AboutThisBuild.txt from template
