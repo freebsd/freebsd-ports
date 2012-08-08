5bed6ae8972ac328d8805ebf39d2447d72c46f56
martymac <martymac@FreeBSD.org>
2012-08-08 13:23:41 +0000

    Update to 20120730

--- games/openjazz/files/patch-src-io-network.cpp
+++ games/openjazz/files/patch-src-io-network.cpp
@@ -4,7 +4,7 @@
  		#define MSG_NOSIGNAL 0
  	#else
  		#include <sys/types.h>
-+        #include <netinet/in.h>
++		#include <netinet/in.h>
  		#include <sys/socket.h>
  		#include <arpa/inet.h>
  		#include <sys/ioctl.h>
