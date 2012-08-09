2a328d258590ffe34445c8311dbfe143f9a25c49
db <db@FreeBSD.org>
2012-08-09 01:56:00 +0000

    - Update to latest 3.21.50 / Flarq 4.3.3 - Fix asciidocs use, man pages need asciidocs - Many changes to UI, Modem, Macros and Internationalisation

--- comms/fldigi/files/patch-src_dialogs_fl_digi.cxx
+++ comms/fldigi/files/patch-src_dialogs_fl_digi.cxx
@@ -1,5 +1,5 @@
---- src/dialogs/fl_digi.cxx.orig	2011-12-01 12:03:25.000000000 -0500
-+++ src/dialogs/fl_digi.cxx	2011-12-05 16:02:10.000000000 -0500
+--- src/dialogs/fl_digi.cxx.orig	2012-07-07 09:01:19.000000000 -0500
++++ src/dialogs/fl_digi.cxx	2012-07-21 15:24:09.000000000 -0500
 @@ -26,6 +26,7 @@
  #include <config.h>
  
@@ -8,12 +8,3 @@
  
  #ifdef __WOE32__
  #  ifdef __CYGWIN__
-@@ -1884,7 +1885,7 @@
- 	Fl::repeat_timeout(st, ztimer);
- 
- 	struct tm tm;
--	gmtime_r(&tv.tv_sec, &tm);
-+	gmtime_r((const time_t *)&tv.tv_sec, &tm);
- 	if (!strftime(ztbuf, sizeof(ztbuf), "%Y%m%d %H%M", &tm))
- 		memset(ztbuf, 0, sizeof(ztbuf));
- 	else
