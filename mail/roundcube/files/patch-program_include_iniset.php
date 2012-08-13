478ae877a95f7bacda450f665b07e67fddb7db59
ale <ale@FreeBSD.org>
2012-08-13 13:23:07 +0000

    Update to 0.8.0 release: - add optional dependency to php-gd for image resize/conversion - switch to OptionsNG - LICENSE is now GPLv3 - drop support for MAIL_REPLACE_URI

--- mail/roundcube/files/patch-program_include_iniset.php
+++ mail/roundcube/files/patch-program_include_iniset.php
@@ -1,10 +1,10 @@
---- program/include/iniset.php.orig	2010-04-26 15:54:45.000000000 +0200
-+++ program/include/iniset.php	2010-04-26 15:54:53.000000000 +0200
-@@ -25,7 +25,6 @@
- // an error/warning in the logs later. See (#1486307).
- $crit_opts = array(
-     'mbstring.func_overload' => 0,
+--- program/include/iniset.php.orig	2012-08-06 18:18:13.000000000 +0200
++++ program/include/iniset.php	2012-08-13 12:37:50.000000000 +0200
+@@ -29,7 +29,6 @@
+     // critical PHP settings here. Only these, which doesn't provide
+     // an error/warning in the logs later. See (#1486307).
+     'mbstring.func_overload'  => 0,
 -    'suhosin.session.encrypt' => 0,
-     'session.auto_start' => 0,
-     'file_uploads' => 1,
- );
+     'session.auto_start'      => 0,
+     'file_uploads'            => 1,
+     'magic_quotes_runtime'    => 0,
--- mail/roundcube/files/patch-program_include_rcube_session.php
+++ mail/roundcube/files/patch-program_include_rcube_session.php
@@ -1,14 +1,14 @@
---- program/include/rcube_session.php.orig	2011-10-07 08:21:40.000000000 +0200
-+++ program/include/rcube_session.php	2011-10-07 08:28:55.000000000 +0200
-@@ -33,7 +33,6 @@
+--- program/include/rcube_session.php.orig	2012-08-06 18:18:13.000000000 +0200
++++ program/include/rcube_session.php	2012-08-13 12:44:54.000000000 +0200
+@@ -37,7 +37,6 @@
    private $ip;
    private $start;
    private $changed;
 -  private $unsets = array();
    private $gc_handlers = array();
    private $cookiename = 'roundcube_sessauth';
-   private $vars = false;
-@@ -132,7 +131,7 @@
+   private $vars;
+@@ -134,7 +133,7 @@
      if ($sql_result && ($sql_arr = $this->db->fetch_assoc($sql_result))) {
        $this->changed = strtotime($sql_arr['changed']);
        $this->ip      = $sql_arr['ip'];
@@ -16,11 +16,11 @@
 +      $this->vars    = $sql_arr['vars'];
        $this->key     = $key;
  
-       if (!empty($this->vars))
-@@ -169,13 +168,13 @@
+       return !empty($this->vars) ? (string) $this->vars : '';
+@@ -170,13 +169,13 @@
      }
  
-     if ($oldvars !== false) {
+     if ($oldvars !== null) {
 -      $newvars = $this->_fixvars($vars, $oldvars);
 +      $newvars = $vars;
  
@@ -33,7 +33,7 @@
        }
        else if ($ts - $this->changed > $this->lifetime / 2) {
          $this->db->query("UPDATE ".get_table_name('session')." SET changed=$now WHERE sess_id=?", $key);
-@@ -186,7 +185,7 @@
+@@ -187,7 +186,7 @@
          sprintf("INSERT INTO %s (sess_id, vars, ip, created, changed) ".
            "VALUES (?, ?, ?, %s, %s)",
            get_table_name('session'), $now, $now),
@@ -42,7 +42,7 @@
      }
  
      return true;
-@@ -194,29 +193,6 @@
+@@ -195,29 +194,6 @@
  
  
    /**
@@ -50,7 +50,7 @@
 -   */
 -  private function _fixvars($vars, $oldvars)
 -  {
--    if ($oldvars !== false) {
+-    if ($oldvars !== null) {
 -      $a_oldvars = $this->unserialize($oldvars);
 -      if (is_array($a_oldvars)) {
 -        foreach ((array)$this->unsets as $k)
@@ -72,16 +72,16 @@
     * Handler for session_destroy()
     *
     * @param string Session ID
-@@ -295,7 +271,7 @@
+@@ -296,7 +272,7 @@
      else // else read data again
        $oldvars = $this->mc_read($key);
  
--    $newvars = $oldvars !== false ? $this->_fixvars($vars, $oldvars) : $vars;
+-    $newvars = $oldvars !== null ? $this->_fixvars($vars, $oldvars) : $vars;
 +    $newvars = $vars;
-     
-     if ($newvars !== $oldvars || $ts - $this->changed > $this->lifetime / 2) {
-       $value = serialize(array('changed' => time(), 'ip' => $this->ip, 'vars' => $newvars));
-@@ -386,7 +362,6 @@
+ 
+     if ($newvars !== $oldvars || $ts - $this->changed > $this->lifetime / 2)
+       return $this->memcache->set($key, serialize(array('changed' => time(), 'ip' => $this->ip, 'vars' => $newvars)), MEMCACHE_COMPRESSED, $this->lifetime);
+@@ -376,7 +352,6 @@
      if (empty($var))
        return $this->destroy(session_id());
  
