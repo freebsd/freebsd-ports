a2aa7e6c7beef771f85ba2120bfb9b149bc67aa0
pawel <pawel@FreeBSD.org>
2012-08-17 18:00:41 +0000

    - Update to version 1.6.3 - Add missing deps - Sort and remove ABI versions from LIB_DEPENDS - Port needs USE_PKGCONFIG=build

--- multimedia/lives/files/patch-src__plugins.h
+++ multimedia/lives/files/patch-src__plugins.h
@@ -1,5 +1,5 @@
---- ./src/plugins.h.orig	2012-06-25 19:44:08.000000000 +0200
-+++ ./src/plugins.h	2012-06-25 19:46:11.000000000 +0200
+--- src/plugins.h.orig	2012-08-17 16:05:34.000000000 +0200
++++ src/plugins.h	2012-08-17 16:10:09.000000000 +0200
 @@ -12,7 +12,7 @@
  #include <string.h>
  #include <fcntl.h>
@@ -9,11 +9,3 @@
  
  // generic plugins
  
-@@ -36,7 +36,6 @@
- GList *plugin_request_by_space (const gchar *plugin_type, const gchar *plugin_name, const gchar *request);
- GList *plugin_request_common (const gchar *plugin_type, const gchar *plugin_name, const gchar *request, const gchar *delim, gboolean allow_blanks);
- 
--typedef weed_plant_t *(*weed_bootstrap_f) (weed_default_getter_f *value, int num_versions, int *plugin_versions);
- 
- /// video playback plugins
- typedef gboolean (*plugin_keyfunc) (gboolean down, guint16 unicode, guint16 keymod);
