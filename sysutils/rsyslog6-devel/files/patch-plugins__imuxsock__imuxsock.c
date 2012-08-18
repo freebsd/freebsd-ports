09cd4e5d232f6d018810e6b082dd0392b5d3232b
brd <brd@FreeBSD.org>
2012-08-18 06:24:20 +0000

    - Update the patches to fix the build.
    Pointy hat to:	me
    Noticed by:	Andrzej Tobola < ato AT iem.pw.edu.pl >

--- sysutils/rsyslog6-devel/files/patch-plugins__imuxsock__imuxsock.c
+++ sysutils/rsyslog6-devel/files/patch-plugins__imuxsock__imuxsock.c
@@ -1,6 +1,15 @@
---- ./plugins/imuxsock/imuxsock.c.orig	2011-09-15 06:01:15.000000000 +0000
-+++ ./plugins/imuxsock/imuxsock.c	2012-01-17 01:17:22.085759310 +0000
-@@ -727,9 +727,7 @@
+--- plugins/imuxsock/imuxsock.c.orig	2012-06-18 02:13:18.000000000 -0600
++++ plugins/imuxsock/imuxsock.c	2012-08-16 16:37:27.000000000 -0600
+@@ -81,7 +81,7 @@
+ 
+ /* emulate struct ucred for platforms that do not have it */
+ #ifndef HAVE_SCM_CREDENTIALS
+-struct ucred { int pid; };
++struct ucred { int pid; uid_t uid; gid_t gid; };
+ #endif
+ 
+ /* handle some defines missing on more than one platform */
+@@ -891,9 +891,7 @@
  	int iMaxLine;
  	struct msghdr msgh;
  	struct iovec msgiov;
