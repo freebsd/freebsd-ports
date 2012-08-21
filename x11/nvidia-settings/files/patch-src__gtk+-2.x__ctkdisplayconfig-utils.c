18a8dd8aeca5f63f06cbc4310f1650a17b8256b6
Alexander Nedotsukov <bland@FreeBSD.org>
2012-08-21 14:49:33 +0000

    Allow display configuration with legacy nvidia-driver versions.
    PR:	155011

--- x11/nvidia-settings/files/patch-src__gtk+-2.x__ctkdisplayconfig-utils.c
+++ x11/nvidia-settings/files/patch-src__gtk+-2.x__ctkdisplayconfig-utils.c
@@ -9,3 +9,20 @@
          }
  
  
+--- src/gtk+-2.x/ctkdisplayconfig-utils.c.orig	2012-08-21 23:36:11.000000000 +0900
++++ src/gtk+-2.x/ctkdisplayconfig-utils.c	2012-08-21 23:36:46.000000000 +0900
+@@ -2534,11 +2534,14 @@
+     /* See if the screen is set to not scanout */
+     ret = NvCtrlGetAttribute(screen->handle, NV_CTRL_NO_SCANOUT, &val);
+     if (ret != NvCtrlSuccess) {
++#if 0
+         *err_str = g_strdup_printf("Failed to query NoScanout for "
+                                    "screen %d.",
+                                    screen_id);
+         nv_warning_msg(*err_str);
+         goto fail;
++#endif
++        val = NV_CTRL_NO_SCANOUT_DISABLED;
+     }
+     screen->no_scanout = (val == NV_CTRL_NO_SCANOUT_ENABLED);
+ 
