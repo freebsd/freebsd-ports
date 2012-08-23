7185129f3181623ec180cdc7735383744f2379ae
makc <makc@FreeBSD.org>
2012-08-23 11:16:21 +0000

    Update patch to fix build on 7.x
    Reported by:	erwin/pointyhat

--- x11-themes/qtcurve-gtk2/files/extra-patch-style-qt_settings.c
+++ x11-themes/qtcurve-gtk2/files/extra-patch-style-qt_settings.c
@@ -1,11 +1,12 @@
---- ./style/qt_settings.c.orig	2011-01-03 00:41:06.000000000 +0300
-+++ ./style/qt_settings.c	2011-01-15 21:01:39.529481980 +0300
-@@ -35,6 +35,8 @@
- #include <dirent.h>
- #include <errno.h>
- #include <locale.h>
+--- ./style/qt_settings.c.orig	2012-04-24 21:19:54.000000000 +0000
++++ ./style/qt_settings.c	2012-08-23 11:13:08.651930973 +0000
+@@ -46,6 +46,9 @@
+ 
+ #define strcmp_i(A, B) strncmp_i(A, B, -1)
+ 
 +#include "getline.h"
 +#include "getline.c"
- 
++
  QtCPalette qtcPalette;
  Options    opts;
+ 
