9cb359069ef9f06d235a064e7d30fabaef0c6692
Baptiste Daroussin <bapt@FreeBSD.org>
2012-08-24 09:48:40 +0000

    - Fix build with clang

--- sysutils/agef/files/patch-agef.c
+++ sysutils/agef/files/patch-agef.c
@@ -1,29 +1,32 @@
-*** agef.c.orig	Sat Jan 21 22:23:18 2006
---- agef.c	Sat Jan 21 22:23:36 2006
-***************
-*** 75,86 ****
-  unsigned        ages[MAXAGES];	/* age categories */
-  int             inodes[MAXAGES];/* inode count */
-  long            sizes[MAXAGES];	/* block count */
-  
-  char            topdir[NAMELEN];/* our starting directory */
-! long            today,
-!                 time();		/* today's date */
-  
-  
-  
-  main(argc, argv)
-      int             argc;
---- 75,86 ----
-  unsigned        ages[MAXAGES];	/* age categories */
-  int             inodes[MAXAGES];/* inode count */
-  long            sizes[MAXAGES];	/* block count */
-  
-  char            topdir[NAMELEN];/* our starting directory */
-! long            today;
-! time_t          time();		/* today's date */
-  
-  
-  
-  main(argc, argv)
-      int             argc;
+--- ./agef.c.orig	1989-02-26 23:37:25.000000000 +0100
++++ ./agef.c	2012-08-24 11:46:51.045913934 +0200
+@@ -77,9 +77,11 @@
+ long            sizes[MAXAGES];	/* block count */
+ 
+ char            topdir[NAMELEN];/* our starting directory */
+-long            today,
+-                time();		/* today's date */
++long            today;
++time_t          time();		/* today's date */
+ 
++static void get_data(char *path);
++static void down(char *subdir);
+ 
+ 
+ main(argc, argv)
+@@ -198,6 +200,7 @@
+   * Get the aged data on a file whose name is given.  If the file is a
+   * directory, go down into it, and get the data from all files inside. 
+   */
++static void
+ get_data(path)
+     char           *path;
+ {
+@@ -252,6 +255,7 @@
+   * in there. 
+   */
+ 
++static void
+ down(subdir)
+     char           *subdir;
+ {
