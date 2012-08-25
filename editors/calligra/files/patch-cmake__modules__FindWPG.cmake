8f9518e03fa54681be29d8103f5f3f283ab22cc3
Alberto Villa <avilla@FreeBSD.org>
2012-08-25 16:46:35 +0000

    The KDE/FreeBSD team is pleased to announce version 2.5 of Calligra, coming to the ports tree after a short testing period which showed it to be quite stable.
    For the productivity part of the suite (word processor, spreadsheet,
    and presentation program) the target user of version 2.5 is still the
    student or academic user. This version has a number of new features
    that will make it more suitable for these users.
    The artistic applications of the Calligra Suite are the most mature
    ones and are already used by professional users everywhere.
    
    As usual, detailed release notes can be found in the official
    announcement:
    http://www.calligra.org/news/calligra-2-5-released
    
    Meanwhile, new translations were added:
    - editors/calligra-l10n-gl (Gallegan)

--- editors/calligra/files/patch-cmake__modules__FindWPG.cmake
+++ editors/calligra/files/patch-cmake__modules__FindWPG.cmake
@@ -1,32 +1,20 @@
---- ./cmake/modules/FindWPG.cmake.orig	2011-10-16 07:30:32.257075896 -0700
-+++ ./cmake/modules/FindWPG.cmake	2011-10-16 07:32:34.927100360 -0700
-@@ -18,7 +18,6 @@
+--- ./cmake/modules/FindWPG.cmake.orig	2012-07-17 01:34:40.838916621 +0200
++++ ./cmake/modules/FindWPG.cmake	2012-07-17 01:36:33.058951606 +0200
+@@ -28,7 +28,7 @@
  
-   IF (NOT WIN32)
-     INCLUDE(FindPkgConfig)
--    pkg_check_modules(LIBWPG libwpg-0.1)
-     pkg_check_modules(LIBWPG libwpg-0.2)
-     IF(LIBWPG_FOUND)
-         SET(LIBWPG_LIBRARY ${LIBWPG_LIBRARIES})
-@@ -32,12 +31,8 @@
+     FIND_LIBRARY(LIBWPG_STREAM_LIBRARY
+         NAMES
+-            libwpd-stream-0.9 wpd-stream-0.9 libwpd-stream wpd-stream
++            libwpd-stream-0.9 wpd-stream-0.9
+         PATHS
+             ${LIBWPG_LIBRARIES}
+     )
+@@ -36,7 +36,7 @@
+     FIND_PATH(LIBWPG_INCLUDE_DIR libwpg/libwpg.h
+         PATHS
+             ${LIBWPG_INCLUDE_DIRS}
+-        PATH_SUFFIXES libwpg-0.2 libwpg
++        PATH_SUFFIXES libwpg-0.2
+     )
  
-   FIND_LIBRARY(LIBWPG_STREAM_LIBRARY
-       NAMES 
--	  libwpg-stream-0.1 
--	  wpg-stream-0.1 
- 	  libwpd-stream-0.9 
- 	  wpd-stream-0.9
--	  libwpd-stream
--	  wpd-stream
-       PATHS
-           ${LIBWPG_LIBRARIES}
-   )
-@@ -45,7 +40,7 @@
-   FIND_PATH(LIBWPG_INCLUDE_DIR libwpg/libwpg.h
-       PATHS
-       ${LIBWPG_INCLUDE_DIRS}
--      PATH_SUFFIXES libwpg-0.1 libwpg-0.2 libwpg 
-+      PATH_SUFFIXES libwpg-0.2
-   )
- 
-   SET(LIBWPG_LIBRARIES ${LIBWPG_LIBRARY} ${LIBWPG_STREAM_LIBRARY})
+     SET(LIBWPG_LIBRARIES ${LIBWPG_LIBRARY} ${LIBWPG_STREAM_LIBRARY})
