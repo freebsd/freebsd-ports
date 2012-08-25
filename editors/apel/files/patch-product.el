85a8a5bfccc3a37eaa58f1a66f4658bdac9b5029
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2012-08-25 12:01:11 +0000

    Fix old-style backquotes issue.
    PR:		ports/170961
    Submitted by:	Yasuhiro KIMURA <yasu@utahime.org>

---
+++ editors/apel/files/patch-product.el
@@ -0,0 +1,83 @@
+Index: product.el
+===================================================================
+--- product.el	(revision 2)
++++ product.el	(working copy)
+@@ -232,21 +232,21 @@
+ 	 (product-version (product-version product))
+ 	 (product-code-name (product-code-name product))
+ 	 (product-version-string (product-version-string product)))
+-    (` (progn
+-	 (, product-def)
+-	 (put (, feature) 'product
+-	      (let ((product (product-find-by-name (, product-name))))
+-		(product-run-checkers product '(, product-version))
+-		(and (, product-family)
+-		     (product-add-to-family (, product-family)
+-					    (, product-name)))
+-		(product-add-feature product (, feature))
+-		(if (equal '(, product-version) (product-version product))
+-		    product
+-		  (vector (, product-name) (, product-family)
+-			  '(, product-version) (, product-code-name)
+-			  nil nil nil (, product-version-string)))))
+-	 (, feature-def)))))
++    `(progn
++       ,product-def
++       (put ,feature 'product
++	    (let ((product (product-find-by-name ,product-name)))
++	      (product-run-checkers product ',product-version)
++	      (and ,product-family
++		   (product-add-to-family ,product-family
++					  ,product-name))
++	      (product-add-feature product ,feature)
++	      (if (equal ',product-version (product-version product))
++		  product
++		(vector ,product-name ,product-family
++			',product-version ,product-code-name
++			nil nil nil ,product-version-string))))
++       ,feature-def)))
+ 
+ (defun product-version-as-string (product)
+   "Return version number of product as a string.
+@@ -293,13 +293,13 @@
+ PRODUCT is a product structure which returned by `product-define'."
+   (let (dest)
+     (product-for-each product nil
+-     (function
+-      (lambda (product)
+-	(let ((str (product-string-1 product nil)))
+-	  (if str
+-	      (setq dest (if dest
+-			     (concat dest " " str)
+-			   str)))))))
++		      (function
++		       (lambda (product)
++			 (let ((str (product-string-1 product nil)))
++			   (if str
++			       (setq dest (if dest
++					      (concat dest " " str)
++					    str)))))))
+     dest))
+ 
+ (defun product-string-verbose (product)
+@@ -307,13 +307,13 @@
+ PRODUCT is a product structure which returned by `product-define'."
+   (let (dest)
+     (product-for-each product nil
+-     (function
+-      (lambda (product)
+-	(let ((str (product-string-1 product t)))
+-	  (if str
+-	      (setq dest (if dest
+-			     (concat dest " " str)
+-			   str)))))))
++		      (function
++		       (lambda (product)
++			 (let ((str (product-string-1 product t)))
++			   (if str
++			       (setq dest (if dest
++					      (concat dest " " str)
++					    str)))))))
+     dest))
+ 
+ (defun product-version-compare (v1 v2)
