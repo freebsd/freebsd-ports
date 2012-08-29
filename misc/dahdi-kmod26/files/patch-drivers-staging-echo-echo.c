2bb716e0835977e37c32d9768280dd8911437a0c
Max Khon <fjoe@FreeBSD.org>
2012-08-29 11:54:28 +0000

    DAHDI 2.6.1 development version: - Digium TE820: PCI-Express eight-port T1/E1/J1 is now supported - new FreeBSD compat shims (similar to OFED in FreeBSD 9 and later) - wcb1xxp support (temporarily) removed - it was not working in 2.4 port anyway - dahdi_dynamic is likely to be broken

---
+++ misc/dahdi-kmod26/files/patch-drivers-staging-echo-echo.c
@@ -0,0 +1,7 @@
+--- drivers/staging/echo/echo.c.orig	2012-08-29 15:54:31.000000000 +0700
++++ drivers/staging/echo/echo.c	2012-08-29 15:54:42.000000000 +0700
+@@ -659,4 +659,3 @@
+ MODULE_LICENSE("GPL");
+ MODULE_AUTHOR("David Rowe");
+ MODULE_DESCRIPTION("Open Source Line Echo Canceller");
+-MODULE_VERSION("0.3.0");
