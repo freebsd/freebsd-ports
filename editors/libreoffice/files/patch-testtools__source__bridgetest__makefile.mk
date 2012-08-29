8f7be8f639212ba49cd2311d6a3a1dbc38e4d5f4
jkim <jkim@FreeBSD.org>
2012-08-29 18:42:58 +0000

    Work around rpath/origin and visibility issues.

--- editors/libreoffice/files/patch-testtools__source__bridgetest__makefile.mk
+++ editors/libreoffice/files/patch-testtools__source__bridgetest__makefile.mk
@@ -1,6 +1,14 @@
 --- ./testtools/source/bridgetest/makefile.mk.orig	2012-08-08 09:00:06.000000000 -0400
-+++ ./testtools/source/bridgetest/makefile.mk	2012-08-22 16:18:28.000000000 -0400
-@@ -145,16 +145,6 @@
++++ ./testtools/source/bridgetest/makefile.mk	2012-08-27 15:06:28.000000000 -0400
+@@ -31,7 +31,6 @@
+ TARGET=bridgetest
+ USE_DEFFILE=TRUE
+ ENABLE_EXCEPTIONS=TRUE
+-VISIBILITY_HIDDEN=TRUE
+ LIBTARGET=NO
+ 
+ .INCLUDE: settings.mk
+@@ -145,16 +144,6 @@
  
  runtest : $(DLLDEST)$/uno_types.rdb $(DLLDEST)$/uno_services.rdb makefile.mk \
          $(SHL1TARGETN) $(SHL2TARGETN) $(SHL3TARGETN)
