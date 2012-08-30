e992ec3bfb75e338fd854cadf334c23b14af6286
flo <flo@FreeBSD.org>
2012-08-30 14:54:17 +0000

    - update firefox and thunderbird to 15.0 - update firefox-esr, thunderbird-esr, linux-thunderbird and linux-firefox to 10.0.7 - update seamonkey and linux-seamonkey to 2.12 - update nss to 3.13.6 - update bsdipc code (posix_spawn, SysV shared memory) - rename patches to easily track those not (yet) submitted upstream - reduce package size, except for www/libxul[1] - restore default objdir to what it was in 13.0 - fix mail/enigmail after thunderbird build changes - don't accidentally pick up headers from installed ports[3] - add support for PREFIX != LOCALBASE to Makefile.webplugins [4] - document vulnerabilities in vuln.xml - *miscellaneous cleanups and fixups*
    Obtained from:	OpenBSD ports[1]
    PR:		ports/159831, ports/160933, ports/170467[3], ports/170236 [4]
    Submitted by:	avilla [4]
    In collaboration with:	Jan Beich <jbeich@tormail.net> Who did most of the hard
    			work.

--- mail/thunderbird-esr/files/patch-ipc-chromium-src-base-file_util_linux.cc
+++
@@ -1,8 +0,0 @@
---- mozilla/ipc/chromium/src/base/file_util_linux.cc~
-+++ mozilla/ipc/chromium/src/base/file_util_linux.cc
-@@ -5,6 +5,7 @@
- #include "base/file_util.h"
- 
- #include <fcntl.h>
-+#include <unistd.h>
- 
