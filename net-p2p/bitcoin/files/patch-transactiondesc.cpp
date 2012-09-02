59f3e50a7a71aa217159fecc7065c0d7058088fa
swills <swills@FreeBSD.org>
2012-09-02 02:51:56 +0000

    - Update to 0.6.3
    PR:		ports/171080
    Security:	b50913ce-f4a7-11e1-b135-003067b2972c

---
+++ net-p2p/bitcoin/files/patch-transactiondesc.cpp
@@ -0,0 +1,10 @@
+--- src/qt/transaction.cpp.orig	2012-09-01 03:08:26.000000000 +0000
++++ src/qt/transactiondesc.cpp	2012-09-01 03:08:38.000000000 +0000
+@@ -7,6 +7,7 @@
+ #include "wallet.h"
+ #include "db.h"
+ #include "ui_interface.h"
++#include "walletdb.h"
+ 
+ #include <QString>
+ 
