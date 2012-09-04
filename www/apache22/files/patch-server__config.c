b1d80f7131b5f58099691e4c066f76571e74de2b
Olli Hauer <ohauer@FreeBSD.org>
2012-09-04 21:17:06 +0000

    - Simplify options with the removal of the last APR only related parameter [1]
    - disallow IPv6 sockets to handle IPv4 requests per default. [2]
    
    - move extra-patch-server__config.c
        -> patch-server__config.c
        https://issues.apache.org/bugzilla/show_bug.cgi?id=53823
    
    - bump PORTREVISION
    
    [1] Credits to Hajimu UMEMOTO (ume@) for finding the last APR related parameter
    [2] http://httpd.apache.org/docs/2.2/bind.html
    
    with hat apache@

--- www/apache22/files/patch-server__config.c
+++ www/apache22/files/patch-server__config.c
@@ -1,11 +1,14 @@
---- ./server/config.c.orig	2012-08-13 21:15:02.000000000 +0200
-+++ ./server/config.c	2012-08-13 21:22:15.000000000 +0200
-@@ -1979,6 +1979,11 @@
+Fix for IPv6 only hosts, reported by Curtis Villamizar
+http://lists.freebsd.org/pipermail/freebsd-apache/2012-August/002836.html
+https://issues.apache.org/bugzilla/show_bug.cgi?id=53823
+
+==================================================================
+--- ./server/config.c.orig	2010-10-07 18:56:54.000000000 +0200
++++ ./server/config.c	2012-09-03 21:23:31.000000000 +0200
+@@ -1979,6 +1979,9 @@
      /* NOT virtual host; don't match any real network interface */
      rv = apr_sockaddr_info_get(&s->addrs->host_addr,
                                 NULL, APR_INET, 0, 0, p);
-+    /* Support for IPv6 only, reported by Curtis Villamizar <curtis@occnc.com> */
-+    /* http://lists.freebsd.org/pipermail/freebsd-apache/2012-August/002836.html */
 +    if (rv != APR_SUCCESS)
 +	    rv = apr_sockaddr_info_get(&s->addrs->host_addr,
 +			    NULL, APR_INET6, 0, 0, p);
