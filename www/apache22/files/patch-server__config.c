2b025867efc1fb4d17ea5fe4e04bd81ff33d1df0
Olli Hauer <ohauer@FreeBSD.org>
2012-09-05 07:40:26 +0000

    - fix build on IPv4 only systems
    Thanks to John Marshall to identify the issue!

--- www/apache22/files/patch-server__config.c
+++ www/apache22/files/patch-server__config.c
@@ -4,14 +4,16 @@ https://issues.apache.org/bugzilla/show_bug.cgi?id=53823
 
 ==================================================================
 --- ./server/config.c.orig	2010-10-07 18:56:54.000000000 +0200
-+++ ./server/config.c	2012-09-03 21:23:31.000000000 +0200
-@@ -1979,6 +1979,9 @@
++++ ./server/config.c	2012-09-05 08:39:18.000000000 +0200
+@@ -1979,6 +1979,11 @@
      /* NOT virtual host; don't match any real network interface */
      rv = apr_sockaddr_info_get(&s->addrs->host_addr,
                                 NULL, APR_INET, 0, 0, p);
++#if APR_HAVE_IPV6
 +    if (rv != APR_SUCCESS)
 +	    rv = apr_sockaddr_info_get(&s->addrs->host_addr,
 +			    NULL, APR_INET6, 0, 0, p);
++#endif    
      ap_assert(rv == APR_SUCCESS); /* otherwise: bug or no storage */
  
      s->addrs->host_port = 0; /* matches any port */
