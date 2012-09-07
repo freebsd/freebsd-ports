159a1fccb81c2b5c4bca3233a9060574b722013e
garga <garga@FreeBSD.org>
2012-09-07 18:11:22 +0000

    Update to 1.31.04

---
+++ net/xmlrpc-c-devel/files/patch-test__cpp__test.cpp
@@ -0,0 +1,12 @@
+--- test/cpp/test.cpp	(revision 2386)
++++ test/cpp/test.cpp	(revision 2387)
+@@ -9,7 +9,6 @@
+ 
+ #include "xmlrpc-c/girerr.hpp"
+ using girerr::error;
+-#include "transport_config.h"
+ #include "xmlrpc-c/base.hpp"
+ #include "xmlrpc-c/oldcppwrapper.hpp"
+ #include "xmlrpc-c/registry.hpp"
+Index: test/cpp/Makefile
+===================================================================
