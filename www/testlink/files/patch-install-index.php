ceb74866068a18d9e59f68cea5caef6db975a4b5
tota <tota@FreeBSD.org>
2012-09-09 21:42:58 +0000

    - Update to 1.9.4 - Remove deprecated header information from Makefile

---
+++ www/testlink/files/patch-install-index.php
@@ -0,0 +1,18 @@
+--- install/index.php.orig	2012-09-01 22:55:30.000000000 +0900
++++ install/index.php	2012-09-10 01:06:21.000000000 +0900
+@@ -45,7 +45,7 @@
+ 		<p>Please read Section on README file or go to www.teamst.org (Forum: TestLink 1.9.4 News,changes, etc) </p>
+ 		<p>Open <a target="_blank" href="../docs/testlink_installation_manual.pdf">Installation manual</a>
+ 		for more information or troubleshooting. You could also look at
+-		<a href="../README">README</a> or <a href="../CHANGELOG">Changes Log</a>.
++		<a href="../docs/README">README</a> or <a href="../docs/CHANGELOG">Changes Log</a>.
+ 		You are welcome to visit our <a target="_blank" href="http://www.teamst.org">
+ 		forum</a> to browse or discuss.
+ 		</p>
+@@ -57,4 +57,4 @@
+ 
+ </div>
+ </body>
+-</html>
+\ No newline at end of file
++</html>
