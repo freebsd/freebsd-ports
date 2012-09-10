4f318554ab8f39edca277d19ca0723f658644d97
brd <brd@FreeBSD.org>
2012-09-10 04:23:04 +0000

    - Update to 6.4.1 - Unmark as BROKEN on i386 with SNMP
    Changes: http://www.rsyslog.com/changelog-for-6-4-1-v6-stable/

--- sysutils/rsyslog6/files/patch-plugins__imuxsock__imuxsock.c
+++ sysutils/rsyslog6/files/patch-plugins__imuxsock__imuxsock.c
@@ -1,6 +1,15 @@
---- ./plugins/imuxsock/imuxsock.c.orig	2011-09-15 06:01:15.000000000 +0000
-+++ ./plugins/imuxsock/imuxsock.c	2012-01-17 01:17:22.085759310 +0000
-@@ -727,9 +727,7 @@
+--- plugins/imuxsock/imuxsock.c.orig	2012-09-06 04:05:36.000000000 -0600
++++ plugins/imuxsock/imuxsock.c	2012-09-09 20:03:06.000000000 -0600
+@@ -81,7 +81,7 @@
+ 
+ /* emulate struct ucred for platforms that do not have it */
+ #ifndef HAVE_SCM_CREDENTIALS
+-struct ucred { int pid; };
++struct ucred { int pid; uid_t uid; gid_t gid; };
+ #endif
+ 
+ /* handle some defines missing on more than one platform */
+@@ -895,9 +895,7 @@
  	int iMaxLine;
  	struct msghdr msgh;
  	struct iovec msgiov;
