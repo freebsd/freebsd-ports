4eb68f089e43fc0c2df1cb992ee31886d096f73e
Michael Scheidell <scheidell@FreeBSD.org>
2012-09-10 12:56:16 +0000

    - Update to 2.5 - Update to OptionsNG - Remove extra Makefile #Header information
    PR:		ports/171418
    Submitted by:	arrowdodger <6yearold@gmail.com> (maintainer)

--- devel/tortoisehg2/files/patch-setup.py
+++ devel/tortoisehg2/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py_	2011-05-02 07:06:56.000000000 +0400
-+++ setup.py	2011-05-25 15:54:34.833312978 +0400
-@@ -283,7 +283,7 @@
+--- setup_.py	2012-09-05 19:36:36.000000000 +0400
++++ setup.py	2012-09-07 10:16:47.929179472 +0400
+@@ -280,7 +280,7 @@
  class build(_build_orig):
      sub_commands = [
          ('build_qt', None),
@@ -9,12 +9,8 @@
          ] + _build_orig.sub_commands
  
  class clean(_clean_orig):
-@@ -299,10 +299,10 @@
- cmdclass = {
-         'build': build,
-         'build_qt': build_qt ,
--        'build_mo': build_mo ,
-+%%NLS%%        'build_mo': build_mo ,
+@@ -299,7 +299,7 @@
+         'build_mo': build_mo ,
          'clean': clean,
          'clean_local': clean_local,
 -        'update_pot': update_pot ,
@@ -22,19 +18,19 @@
      }
  
  def setup_windows(version):
-@@ -407,11 +407,11 @@
+@@ -418,11 +418,11 @@
      _data_files = [(os.path.join('share/pixmaps/tortoisehg', root),
          [os.path.join(root, file_) for file_ in files])
          for root, dirs, files in os.walk('icons')]
 -    _data_files += [(os.path.join('share', root),
 -        [os.path.join(root, file_) for file_ in files])
 -        for root, dirs, files in os.walk('locale')]
--    _data_files += [('lib/nautilus/extensions-2.0/python',
+-    _data_files += [('/usr/share/nautilus-python/extensions/',
 -                     ['contrib/nautilus-thg.py'])]
 +%%NLS%%    _data_files += [(os.path.join('share', root),
 +%%NLS%%        [os.path.join(root, file_) for file_ in files])
 +%%NLS%%        for root, dirs, files in os.walk('locale')]
-+%%NAUTILUS%%    _data_files += [('lib/nautilus/extensions-2.0/python',
++%%NAUTILUS%%    _data_files += [('%%PREFIX%%/lib/nautilus/extensions-2.0/python',
 +%%NAUTILUS%%                     ['contrib/nautilus-thg.py'])]
  
      # Create a config.py.  Distributions will need to supply their own
