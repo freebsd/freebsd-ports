21c9a794e9fefb953b22c4345999ce2b94b9dc58
nivit <nivit@FreeBSD.org>
2012-09-11 16:04:47 +0000

    - Update to 2.1.1 - Remove files/ninja-ide.in - Set BUILD_DEPENDS equal to RUN_DEPENDS - Rearrange Makefile to use GutHub as master site,   and easy_install - Fix format of WWW field in pkg-descr
    BuildLog:	http://goo.gl/WNM1Z

--- devel/ninja-ide/files/patch-ninja_ide__resources.py
+++ devel/ninja-ide/files/patch-ninja_ide__resources.py
@@ -1,7 +1,6 @@
-$FreeBSD$
---- ./ninja_ide/resources.py.orig	2011-10-25 02:41:30.000000000 +0200
-+++ ./ninja_ide/resources.py	2011-12-12 00:42:45.000000000 +0100
-@@ -39,6 +39,8 @@
+--- ./ninja_ide/resources.py.orig	2012-08-23 01:57:46.000000000 +0200
++++ ./ninja_ide/resources.py	2012-09-11 10:44:09.000000000 +0200
+@@ -40,6 +40,8 @@
      PRJ_PATH = os.path.abspath(os.path.dirname(sys.executable))
  
  HOME_NINJA_PATH = os.path.join(HOME_PATH, ".ninja_ide")
@@ -9,13 +8,4 @@ $FreeBSD$
 +    os.mkdir(HOME_NINJA_PATH)
  
  ADDINS = os.path.join(HOME_NINJA_PATH, "addins")
- 
-@@ -53,7 +55,7 @@
- 
- EDITOR_SKINS = os.path.join(HOME_NINJA_PATH, "addins", "schemes")
- 
--START_PAGE_URL = os.path.join(PRJ_PATH, "doc", "startPage.html")
-+START_PAGE_URL = os.path.join("%%DOCSDIR%%", "startPage.html")
- 
- LOG_FILE_PATH = os.path.join(HOME_NINJA_PATH, 'ninja_ide.log')
  
