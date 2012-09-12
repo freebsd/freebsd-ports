7517a71d706edcc1260b3ed3decc4516f0601991
nivit <nivit@FreeBSD.org>
2012-09-12 09:04:32 +0000

    Pattern matching and various Python utilities for file systems paths.
    WWW: http://github.com/gorakhargosh/pathtools

---
+++ devel/py-pathtools/files/patch-docs__source__conf.py
@@ -0,0 +1,11 @@
+--- ./docs/source/conf.py.orig	2012-09-11 15:54:40.000000000 +0200
++++ ./docs/source/conf.py	2012-09-11 15:54:44.000000000 +0200
+@@ -104,7 +104,7 @@
+ 
+ # The theme to use for HTML and HTML Help pages.  See the documentation for
+ # a list of builtin themes.
+-html_theme = 'flask'
++html_theme = 'default'
+ 
+ # Theme options are theme-specific and customize the look and feel of a theme
+ # further.  For a list of options available for each theme, see the
