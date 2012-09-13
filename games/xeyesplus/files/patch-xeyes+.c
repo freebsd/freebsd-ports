b0292cd9e0f30b0e15cd8cf6939a25e9400ec3d0
Niclas Zeising <zeising@FreeBSD.org>
2012-09-13 10:34:25 +0000

    Fix build with clang.
    Approved by:	kwm (mentor)

---
+++ games/xeyesplus/files/patch-xeyes+.c
@@ -0,0 +1,11 @@
+--- xeyes+.c.orig	2012-09-11 09:39:46.000000000 +0200
++++ xeyes+.c	2012-09-11 09:40:06.000000000 +0200
+@@ -111,7 +111,7 @@
+ }
+ 
+ 
+-void main(argc, argv)
++int main(argc, argv)
+ int argc;
+ char **argv;
+ {
