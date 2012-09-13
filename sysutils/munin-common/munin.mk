b505760dd53752bbd29084d1b4e0febeb7d247cc
flo <flo@FreeBSD.org>
2012-09-13 22:00:33 +0000

    - Update to 2.0.6 based on [1] - Add support for ada disk devices to hddtemp_smartctl [2] - Add a patch for upstream bug 1251 to prevent huge munin-graph.log files
    PR:		ports/171116 [1]
    PR:		ports/169287 [2]
    Submitted by:	mm [1], TAKEMON Takashi <takemon@bamboogate.co.jp> [2]

--- sysutils/munin-common/munin.mk
+++ sysutils/munin-common/munin.mk
@@ -1,20 +1,30 @@
+MUNIN_VERSION=	2.0.6
+MUNIN_SITES=	SF/${PORTNAME}/stable/${PORTVERSION}
+MUNIN_DISTINFO=	${PORTSDIR}/sysutils/munin-common/distinfo
+MUNIN_PATCHES=	${PORTSDIR}/sysutils/munin-common/files/patch-Makefile \
+		${PORTSDIR}/sysutils/munin-common/files/patch-Makefile.config
+
 DBDIR?=		/var/${PORTNAME}
+DBDIRNODE?=	/var/${PORTNAME}
 LOGDIR?=	/var/log/${PORTNAME}
 STATEDIR?=	/var/run/${PORTNAME}
+SPOOLDIR?=	/var/spool/${PORTNAME}
 MUNIN_DIRS=	BINDIR=${PREFIX}/bin \
 		CGIDIR=${PREFIX}/www/cgi-bin \
 		CONFDIR=${ETCDIR} \
 		DBDIR=${DBDIR} \
+		DBDIRNODE=${DBDIRNODE} \
 		DOCDIR=${DOCSDIR} \
 		HTMLDIR=${WWWDIR} \
 		LIBDIR=${DATADIR} \
 		LOGDIR=${LOGDIR} \
 		MANDIR=${MANPREFIX}/man \
 		SBINDIR=${PREFIX}/sbin \
-		STATEDIR=${STATEDIR}
+		STATEDIR=${STATEDIR} \
+		SPOOLDIR=${SPOOLDIR}
 MAKE_ARGS=	${MUNIN_DIRS} \
 		BASH=${LOCALBASE}/bin/bash \
 		PERL=${PERL} PERLLIB=${PREFIX}/${SITE_PERL_REL}
-PLIST_SUB=	${MUNIN_DIRS}
-USERS=		munin
-GROUPS=		munin
+MUNIN_USER=	munin
+MUNIN_GROUP=	munin
+PLIST_SUB=	${MUNIN_DIRS} USER=${MUNIN_USER} GROUP=${MUNIN_GROUP}
