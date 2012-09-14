0ecaeefd5fc97fd49984c7dea8994a60160ce01c
Guido Falsi <madpilot@FreeBSD.org>
2012-09-14 18:34:21 +0000

    - Update to version 0.0.11 (alpha 11) - Trim Makefile header
    Approved by:	crees, tabthorpe (mentors, implicit)

--- games/0ad/files/patch-build-premake-premake4.lua
+++ games/0ad/files/patch-build-premake-premake4.lua
@@ -1,14 +1,14 @@
---- build/premake/premake4.lua.orig	2012-02-21 21:27:25.000000000 +0100
-+++ build/premake/premake4.lua	2012-05-17 23:23:30.211530880 +0200
-@@ -215,7 +215,6 @@
+--- build/premake/premake4.lua.orig	2012-08-15 02:10:44.000000000 +0200
++++ build/premake/premake4.lua	2012-09-13 04:13:10.184743291 +0200
+@@ -220,7 +220,6 @@
  
- 				-- enable security features (stack checking etc) that shouldn't have
- 				-- a significant effect on performance and can catch bugs
--				"-fstack-protector-all",
- 				"-D_FORTIFY_SOURCE=2",
+ 					-- enable security features (stack checking etc) that shouldn't have
+ 					-- a significant effect on performance and can catch bugs
+-					"-fstack-protector-all",
+ 					"-D_FORTIFY_SOURCE=2",
  
- 				-- always enable strict aliasing (useful in debug builds because of the warnings)
-@@ -276,11 +275,11 @@
+ 					-- always enable strict aliasing (useful in debug builds because of the warnings)
+@@ -292,11 +291,11 @@
  		-- X11 includes may be installed in one of a gadzillion of three places
  		-- Famous last words: "You can't include too much! ;-)"
  		includedirs {
