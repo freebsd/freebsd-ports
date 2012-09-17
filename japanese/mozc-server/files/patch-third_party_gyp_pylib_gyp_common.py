087f392b5cce343bc0b1b38e78a392f373b23ee6
daichi <daichi@FreeBSD.org>
2012-09-17 08:29:02 +0000

    fixed a FreeBSD 9.x build issue

--- japanese/mozc-server/files/patch-third_party_gyp_pylib_gyp_common.py
+++ japanese/mozc-server/files/patch-third_party_gyp_pylib_gyp_common.py
@@ -1,15 +1,17 @@
 --- third_party/gyp/pylib/gyp/common.py.orig	2012-09-07 10:21:29.147021135 +0900
-+++ third_party/gyp/pylib/gyp/common.py	2012-09-07 10:30:44.711021503 +0900
-@@ -362,8 +362,10 @@
++++ third_party/gyp/pylib/gyp/common.py	2012-09-17 17:14:43.781601968 +0900
+@@ -362,9 +362,11 @@
      'win32': 'win',
      'darwin': 'mac',
      'sunos5': 'solaris',
 -    'freebsd7': 'freebsd',
 -    'freebsd8': 'freebsd',
+-    'freebsd9': 'freebsd',
 +    'freebsd7': 'linux',
 +    'freebsd8': 'linux',
 +    'freebsd9': 'linux',
 +    'freebsd10': 'linux',
-     'freebsd9': 'freebsd',
++    'freebsd11': 'linux',
    }
    flavor = flavors.get(sys.platform, 'linux')
+   return params.get('flavor', flavor)
--- japanese/mozc-server/files/patch-third_party_gyp_pylib_gyp_generator_make.py
+++ japanese/mozc-server/files/patch-third_party_gyp_pylib_gyp_generator_make.py
@@ -1,5 +1,5 @@
 --- third_party/gyp/pylib/gyp/generator/make.py.orig	2012-09-07 10:21:29.146021648 +0900
-+++ third_party/gyp/pylib/gyp/generator/make.py	2012-09-07 10:30:44.778021410 +0900
++++ third_party/gyp/pylib/gyp/generator/make.py	2012-09-17 17:15:20.613807429 +0900
 @@ -1963,6 +1963,9 @@
      srcdir_prefix = '$(srcdir)/'
  
