b4d10320f4d3c3f05b42ed7fd5faa4bc6e5058b3
Wen Heping <wen@FreeBSD.org>
2012-09-17 06:43:10 +0000

    - Update to 0.7.0

--- science/py-obspy.taup/files/patch-setup.py
+++ science/py-obspy.taup/files/patch-setup.py
@@ -1,9 +1,9 @@
---- setup.py.orig	2012-01-16 08:34:50.000000000 +0800
-+++ setup.py	2012-01-16 08:35:13.000000000 +0800
-@@ -53,7 +53,7 @@
-     # Monkey patch UnixCCompiler for Unix, Linux and MacOS
-     UnixCCompiler.src_extensions.append(".f")
-     def _compile(self, obj, src, *args, **kwargs): #@UnusedVariable
+--- setup.py.orig	2012-09-16 18:44:32.000000000 +0800
++++ setup.py	2012-09-16 18:45:02.000000000 +0800
+@@ -55,7 +55,7 @@
+     UnixCCompiler.linker_so = ["gfortran"]
+ 
+     def _compile(self, obj, src, *args, **kwargs):  # @UnusedVariable
 -            self.compiler_so = ["gfortran"]
 +            self.compiler_so = [os.environ.get("F77")]
              cc_args = ['-c', '-fno-underscoring']
