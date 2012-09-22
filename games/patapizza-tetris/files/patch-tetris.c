89d3b15ecf6a500c1efe82efe0e2134c4ba4e5f2
bsam <bsam@FreeBSD.org>
2012-09-22 16:22:48 +0000

    Add patapizza-tetris 1.0, unofficial clone of the original Tetris game.
    PR:		ports/170704
    Submitted by:	nemysis@gmx.ch

---
+++ games/patapizza-tetris/files/patch-tetris.c
@@ -0,0 +1,10 @@
+--- tetris.c.orig	2012-04-09 13:32:21.000000000 +0200
++++ tetris.c	2012-08-17 00:10:28.000000000 +0200
+@@ -20,7 +20,6 @@
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+  
+-#include <malloc.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
