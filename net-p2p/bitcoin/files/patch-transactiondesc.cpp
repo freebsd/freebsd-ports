4507954b4c93a4cfebde3e7fdb3b926c8f4dd739
Steve Wills <swills@FreeBSD.org>
2012-09-27 13:29:34 +0000

    - Update to 0.7.0 - Set submitter as maintainer
    Submitted by:	"Robert Backhaus" <robbak@robbak.com> (private email)

--- net-p2p/bitcoin/files/patch-transactiondesc.cpp
+++ net-p2p/bitcoin/files/patch-transactiondesc.cpp
@@ -1,10 +1,10 @@
---- src/qt/transaction.cpp.orig	2012-09-01 03:08:26.000000000 +0000
-+++ src/qt/transactiondesc.cpp	2012-09-01 03:08:38.000000000 +0000
-@@ -7,6 +7,7 @@
- #include "wallet.h"
+--- src/qt/transactiondesc.cpp.orig	2012-09-13 04:07:22.000000000 +1000
++++ src/qt/transactiondesc.cpp	2012-09-19 18:44:39.000000000 +1000
+@@ -8,6 +8,7 @@
  #include "db.h"
  #include "ui_interface.h"
+ #include "base58.h"
 +#include "walletdb.h"
  
- #include <QString>
- 
+ QString TransactionDesc::FormatTxStatus(const CWalletTx& wtx)
+ {
