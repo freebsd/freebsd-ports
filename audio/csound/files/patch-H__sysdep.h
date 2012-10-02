23c938fff29f21963192e6cd68d9cd220d962cd0
madpilot <madpilot@FreeBSD.org>
2012-10-02 06:53:38 +0000

    - Update to 5.18.02 - Remove ABI version numbers from LIB_DEPENDS - Add patch to avoid a rare segfault - While here, trim Makefile headers
    PR:		ports/172109
    Submitted by:	Chris Petrik <c.petrik.sosa@gmail.com> (maintainer)

--- audio/csound/files/patch-H__sysdep.h
+++ audio/csound/files/patch-H__sysdep.h
@@ -1,26 +1,26 @@
---- H/sysdep.h.orig	2012-05-29 02:41:30.000000000 +0900
-+++ H/sysdep.h	2012-05-31 11:19:28.000000000 +0900
-@@ -96,7 +96,11 @@
+--- H/sysdep.h.bak	2012-09-27 02:58:13.000000000 +0000
++++ H/sysdep.h	2012-09-27 03:02:06.000000000 +0000
+@@ -100,6 +100,11 @@
    #define EXP exp
    #define LOG log
    #define LOG10 log10
 +#if __FreeBSD_version <= 704100 || (__FreeBSD_version >= 800000 && __FreeBSD_version < 802502) || (__FreeBSD_version >= 900000 && __FreeBSD_version < 900027)
 +  #define LOG2(x) (log(x) / log(2))
 +#else
-   #define LOG2 log2
++   #define LOG2 log2
 +#endif
+   /* #define LOG2 log2 */
    #define POWER pow
    #define SQRT sqrt
-   #define HYPOT hypot
-@@ -122,7 +126,11 @@
+@@ -126,6 +131,11 @@
    #define EXP expf
    #define LOG logf
    #define LOG10 log10f
 +#if __FreeBSD_version <= 704100 || (__FreeBSD_version >= 800000 && __FreeBSD_version < 802502) || (__FreeBSD_version >= 900000 && __FreeBSD_version < 900027)
 +  #define LOG2(x) (logf(x) / logf(2))
 +#else
-   #define LOG2 log2f
++   #define LOG2 log2f
 +#endif
+   /* #define LOG2 log2f */
    #define POWER powf
    #define SQRT sqrtf
-   #define HYPOT hypotf
