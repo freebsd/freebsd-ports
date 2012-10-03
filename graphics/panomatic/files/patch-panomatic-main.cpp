2dc83000f642298a849fe966a180a0c8ee240835
Steve Wills <swills@FreeBSD.org>
2012-10-03 15:01:55 +0000

    - Fix build with clang - Switch to SF mirror - Add USE_DOS2UNIX
    PR:		ports/171859
    Approved by:	Iouri Ivliev <ii@any.com.ru> (maintainer)

--- graphics/panomatic/files/patch-panomatic-main.cpp
+++ graphics/panomatic/files/patch-panomatic-main.cpp
@@ -1,5 +1,5 @@
---- panomatic/main.cpp.orig	2008-03-10 21:19:09.000000000 +0100
-+++ panomatic/main.cpp	2008-04-26 20:26:30.000000000 +0200
+--- panomatic/main.cpp.orig	2008-03-10 20:19:09.000000000 +0000
++++ panomatic/main.cpp	2012-09-24 12:02:46.529117189 +0000
 @@ -92,6 +92,7 @@
  		MyOutput my;
  		cmd.setOutput(&my);
@@ -8,15 +8,21 @@
  		SwitchArg aArgFullScale("","fullscale", "Uses full scale image to detect keypoints    (default:false)\n", false);
  		SwitchArg aArgSurfExtended("","surf128", "Uses extended SURF (128 descriptors)    (default:true)", true);
  		ValueArg<int> aArgSurfScoreThreshold("","surfscore", "SURF Detection score threshold    (default : 1000)\n", false, 1000, "int");
-@@ -175,8 +176,9 @@
+@@ -130,6 +131,7 @@
+ 		cmd.add(aArgSurfScoreThreshold);		
+ 		cmd.add(aArgSurfExtended);
+ 		cmd.add(aArgFullScale);
++		cmd.add(aArgScale);
+ 		
+ 		//cmd.add( aArgSurfExtended );
+ 
+@@ -175,7 +177,8 @@
  		if (aArgSieve2Size.isSet())			ioPanoDetector.setSieve2Size(aArgSieve2Size.getValue());
  		if (aArgLinearMatch.isSet())		ioPanoDetector.setLinearMatch(aArgLinearMatch.getValue());
  		if (aArgLinearMatchLen.isSet())		ioPanoDetector.setLinearMatchLen(aArgLinearMatchLen.getValue());
 -        if (aArgFullScale.isSet())          ioPanoDetector.setDownscale(false);
--            
 +        if (aArgFullScale.isSet())          ioPanoDetector.setScale(1.);
-+            if (aArgScale.isSet())          ioPanoDetector.setScale(aArgScale.getValue());
-+
++        if (aArgScale.isSet())          ioPanoDetector.setScale(aArgScale.getValue());
+             
  		if (aArgTest.isSet())				ioPanoDetector.setTest(aArgTest.getValue());
  		if (aArgCores.isSet())				ioPanoDetector.setCores(aArgCores.getValue());
- 
