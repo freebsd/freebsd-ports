2a04a031488df91382b620e5745848106092b501
sbz <sbz@FreeBSD.org>
2012-10-03 11:07:31 +0000

    - Update to 1.8.0

--- security/py-paramiko/files/patch-setup.py
+++ security/py-paramiko/files/patch-setup.py
@@ -1,20 +1,19 @@
---- ./setup.py.orig	2011-05-31 12:57:26.000000000 +0200
-+++ ./setup.py	2011-05-31 13:01:30.000000000 +0200
-@@ -33,14 +33,9 @@
+--- ./setup.py.orig	2012-10-03 11:45:35.000000000 +0200
++++ ./setup.py	2012-10-03 11:46:32.000000000 +0200
+@@ -37,14 +37,8 @@
  #ez_setup.use_setuptools()
  
  import sys
 -try:
 -    from setuptools import setup
 -    kw = {
--        'install_requires': 'pycrypto >= 2.1',
+-        'install_requires': 'pycrypto >= 2.1, != 2.4',
 -    }
 -except ImportError:
 -    from distutils.core import setup
 -    kw = {}
-+
 +from distutils.core import setup
 +kw = {}
  
  if sys.platform == 'darwin':
- 	import setup_helper
+     import setup_helper
