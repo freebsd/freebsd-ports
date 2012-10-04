2ed69d4811f1a978f05840a56e8e3fa253a6e7c4
Jung-uk Kim <jkim@FreeBSD.org>
2012-10-04 16:30:16 +0000

    Revert the previous accidental commit.  It is not ready yet.

---
+++ java/cacao/files/patch-boehm-gc-os_dep.c
@@ -0,0 +1,29 @@
+--- src/boehm-gc/os_dep.c.orig	Thu May 19 20:48:49 2005
++++ src/mm/boehm-gc/os_dep.c	Fri Jun 17 21:28:07 2005
+@@ -699,7 +699,7 @@
+     || defined(HURD) || defined(NETBSD)
+ 	static struct sigaction old_segv_act;
+ #	if defined(IRIX5) || defined(HPUX) \
+-	|| defined(HURD) || defined(NETBSD)
++	|| defined(HURD) || defined(NETBSD) || defined(FREEBSD)
+ 	    static struct sigaction old_bus_act;
+ #	endif
+ #   else
+@@ -714,7 +714,7 @@
+ #   endif
+     {
+ #	if defined(SUNOS5SIGS) || defined(IRIX5)  \
+-        || defined(OSF1) || defined(HURD) || defined(NETBSD)
++        || defined(OSF1) || defined(HURD) || defined(NETBSD) || defined(FREEBSD)
+ 	  struct sigaction	act;
+ 
+ 	  act.sa_handler	= h;
+@@ -736,7 +736,7 @@
+ #	  else
+ 	        (void) sigaction(SIGSEGV, &act, &old_segv_act);
+ #		if defined(IRIX5) \
+-		   || defined(HPUX) || defined(HURD) || defined(NETBSD)
++		   || defined(HPUX) || defined(HURD) || defined(NETBSD) || defined(FREEBSD)
+ 		    /* Under Irix 5.x or HP/UX, we may get SIGBUS.	*/
+ 		    /* Pthreads doesn't exist under Irix 5.x, so we	*/
+ 		    /* don't have to worry in the threads case.		*/
