046ba79beed9d5b481ee1905175f2503fd339b72
glarkin <glarkin@FreeBSD.org>
2012-10-04 19:51:33 +0000

    [devel/gearmand]:   - Updated to 0.39   - Added gearmand entries to UIDs and GIDs   - Registered conflict with devel/p5-Gearman-Server (conflicting rc.d scripts)   - Add patch to fix build with GCC < 4.4   - Cleaned up rc.d script   - Converted to OptionsNG
    [devel/p5-Gearman-Server]:
      - Registered conflict with devel/gearmand (conflicting rc.d scripts)
    
    [devel/p5-Gearman-XS]:
      - Removed explicit LIB_DEPENDS library version number
      - Bumped PORTREVISION
    
    PR:		ports/171593 (based on)
    Submitted by:	Gasol Wu <gasol.wu@gmail.com>

---
+++ devel/gearmand/files/patch-libgearman-server__server.cc
@@ -0,0 +1,10 @@
+--- ./libgearman-server/server.cc.orig	2012-09-19 22:42:05.000000000 -0400
++++ ./libgearman-server/server.cc	2012-10-03 15:37:17.000000000 -0400
+@@ -46,7 +46,6 @@
+ #include <libgearman-server/queue.h>
+ 
+ #include <errno.h>
+-#include <iso646.h>
+ #include <limits.h>
+ #include <stdlib.h>
+ #include <string.h>
