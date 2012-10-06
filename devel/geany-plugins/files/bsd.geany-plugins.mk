ac0af91a9eb75d64978029e4242d50fa29105b92
makc <makc@FreeBSD.org>
2012-10-06 18:20:59 +0000

    - Update to 0.21 [1] - Update to 1.22 and split into geany-plugin-* ports - Convert to new options framework - Take maintainership
    PR:		ports/165494 [1],
    		ports/171856
    Submitted by:	Marco Trentini <marco at datasked.com> [1]
    Approved by:	maintainer timeouts (2 weeks for ports/171856)

---
+++ devel/geany-plugins/files/bsd.geany-plugins.mk
@@ -0,0 +1,41 @@
+# $FreeBSD$
+
+GEANY_VER=	1.22
+
+GEANY_PLUGINS_ALL=	addons codenav debugger devhelp doc extrasel gdb \
+			gendoc geniuspaste gproject insertnum latex lipsum \
+			lua macro miniscript multiterm numberedbookmarks \
+			pg pretty-printer prj sendmail shiftcolumn \
+			spellcheck tableconvert treebrowser updatechecker \
+			vc webhelper xmlsnippets
+
+addons_DESC=		various small addons
+codenav_DESC=		some facilities for navigating in the code
+debugger_DESC=		debugging support (via GDB currently)
+devhelp_DESC=		API documentation browser
+doc_DESC=		execute command on the word at cursor position
+extrasel_DESC=		additional selection tools
+gdb_DESC=		debugging support via the GNU Debugger
+gendoc_DESC=		generate documentation from the sources comments
+geniuspaste_DESC=	pastebins support
+gproject_DESC=		extension of Geany's project management
+insertnum_DESC=		replaces selection with integer numbers
+latex_DESC=		LaTeX support
+lipsum_DESC=		Lorem Ipsum generator
+lua_DESC=		Lua scripting
+macro_DESC=		user defined macros
+miniscript_DESC=	pipe text/documents via script (shell, perl, awk, etc.)
+multiterm_DESC=		multi-tabbed virtual terminals emulator
+numberedbookmarks_DESC=	additional numbered bookmarks
+pg_DESC=		encrypt, decrypt and verify signatures with GnuPG
+pretty-printer_DESC=	formats XML and make it human-readable
+prj_DESC=		alternative project manager
+sendmail_DESC=		send a document as attachment
+shiftcolumn_DESC=	move blocks of text horizontally
+spellcheck_DESC=	spell check via Enchant
+tableconvert_DESC=	convert tabulator separated selection into a table
+treebrowser_DESC=	alternate file browser
+updatechecker_DESC=	check for new version of Geany
+vc_DESC=		access to different version-control systems
+webhelper_DESC=		web development facilities
+xmlsnippets_DESC=	XML/HTML tag autocompletion
