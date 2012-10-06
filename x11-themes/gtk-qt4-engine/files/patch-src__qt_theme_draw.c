0b724faf3231fbc0668298ca48ceb12a3620472f
makc <makc@FreeBSD.org>
2012-10-06 17:24:57 +0000

    - Change PORTNAME to original gtk-qt-engine  - Add patch to fix build  - Mark MAKE_JOBS_SAFE  - Improve COMMENT and description, update WWW  - Don't remove common directories installed by gtk and kdehier ports  - make bapt@ more happy (by converting to new options framework :)  - do respect option

---
+++ x11-themes/gtk-qt4-engine/files/patch-src__qt_theme_draw.c
@@ -0,0 +1,10 @@
+--- ./src/qt_theme_draw.c.orig	2008-03-11 01:30:19.000000000 +0000
++++ ./src/qt_theme_draw.c	2012-10-06 10:09:43.994767020 +0000
+@@ -19,6 +19,7 @@
+  ***************************************************************************/
+ 
+ #include <math.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <gtk/gtkprogressbar.h>
+ #include <gdk/gdk.h>
