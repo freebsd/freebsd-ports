266179525f1a084fa74c16305d6d4da159776222
flo <flo@FreeBSD.org>
2012-10-10 21:13:06 +0000

    - Update firefox-esr, thunderbird-esr, linux-firefox and linux-thunderbird to 10.0.8 - Update firefox and thunderbird to 16.0 - Update seamonkey to 2.13 - Update all -i18n ports respectively - switch firefox 16.0 and seamonkey 2.13 to ALSA by default for better   latency during pause and seeking with HTML5 video - remove fedisableexcept() hacks, obsolete since FreeBSD 4.0 - support system hunspell dictionaries [1] - unbreak -esr ports with clang3.2 [2] - unbreak nss build when CC contains full path [3] - remove GNOME option grouping [4] - integrate enigmail into thunderbird/seamonkey as an option [5] - remove mail/enigmail* [6] - enable ENIGMAIL, LIGHTNING and GIO options by default - add more reporters in about:memory: page-faults-hard, page-faults-soft,   resident, vsize - use bundled jemalloc 3.0.0 on FreeBSD < 10.0 for gecko 16.0,   only heap-allocated reporter works in about:memory (see bug 762445) - use lrintf() instead of slow C cast in bundled libopus - use libjpeg-turbo's faster color conversion if available during build - record startup time for telemetry - use -z origin instead of hardcoding path to gecko runtime - fail early if incompatible libxul version is installed (in USE_GECKO) - *miscellaneous cleanups and fixups*
    PR:		ports/171534 [1]
    PR:		ports/171566 [2]
    PR:		ports/172164 [3]
    PR:		ports/172201 [4]
    Discussed with:	ale, beat, Jan Beich [5]
    Approved by:	ale [6]
    In collaboration with:	Jan Beich <jbeich@tormail.org>
    Security:	6e5a9afd-12d3-11e2-b47d-c8600054b392
    Feature safe:	yes
    Approved by:	portmgr (beat)

---
+++ security/nss/files/patch-..::coreconf::ruleset.mk
@@ -0,0 +1,11 @@
+--- ../coreconf/ruleset.mk~
++++ ../coreconf/ruleset.mk
+@@ -85,7 +85,7 @@
+ #
+ 
+ ifndef COMPILER_TAG
+-    ifneq ($(DEFAULT_COMPILER), $(notdir $(firstword $(CC))))
++    ifneq ($(DEFAULT_COMPILER), $(CC))
+ #
+ # Temporary define for the Client; to be removed when binary release is used
+ #
---
+++ www/seamonkey/files/patch-mozilla-extensions-spellcheck-hunspell-src-mozHunspell.cpp
@@ -0,0 +1,15 @@
+--- mozilla/extensions/spellcheck/hunspell/src/mozHunspell.cpp~
++++ mozilla/extensions/spellcheck/hunspell/src/mozHunspell.cpp
+@@ -400,6 +400,12 @@ mozHunspell::LoadDictionaryList()
+     }
+   }
+ 
++  // load system hunspell dictionaries
++  nsIFile* hunDir;
++  NS_NewNativeLocalFile(NS_LITERAL_CSTRING("%%LOCALBASE%%/share/hunspell"),
++			true, &hunDir);
++  LoadDictionariesFromDir(hunDir);
++
+   // find dictionaries from extensions requiring restart
+   nsCOMPtr<nsISimpleEnumerator> dictDirs;
+   rv = dirSvc->Get(DICTIONARY_SEARCH_DIRECTORY_LIST,
