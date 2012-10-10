7d73f0d5a1b48155d19d5f9ae6ce50e72a638b07
Ryan Steinmetz <zi@FreeBSD.org>
2012-10-10 11:42:04 +0000

    - Update to 1.3.2

--- security/suricata/files/patch-src__suricata-common.h
+++ security/suricata/files/patch-src__suricata-common.h
@@ -1,10 +1,12 @@
---- ./src/suricata-common.h.orig	2012-09-05 21:43:08.000000000 -0400
-+++ ./src/suricata-common.h	2012-09-05 21:46:28.000000000 -0400
-@@ -131,6 +131,8 @@
+--- ./src/suricata-common.h.orig	2012-08-21 02:35:15.000000000 -0400
++++ ./src/suricata-common.h	2012-09-29 08:41:39.000000000 -0400
+@@ -131,6 +131,10 @@
  #include <assert.h>
  #define BUG_ON(x) assert(!(x))
  
 +#define table_getc(x, y) table_get_c(x, y)
++#define bstr_cmpc(x, y) bstr_cmp_c(x, y)
++#define bstr_tocstr(x) bstr_util_strdup_to_c(x)
 +
  /* we need this to stringify the defines which are supplied at compiletime see:
     http://gcc.gnu.org/onlinedocs/gcc-3.4.1/cpp/Stringification.html#Stringification */
