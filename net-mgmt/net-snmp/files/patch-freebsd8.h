5437390ccc10c1594062ef7985cb3d2df34b9603
Ryan Steinmetz <zi@FreeBSD.org>
2012-10-13 01:07:32 +0000

    - Update to 5.7.2 - Convert to OptionsNG
    Feature safe:	yes

--- net-mgmt/net-snmp/files/patch-freebsd8.h
+++
@@ -1,5 +0,0 @@
---- include/net-snmp/system/freebsd8.h.orig	2009-01-24 13:29:00.000000000 +0900
-+++ include/net-snmp/system/freebsd8.h	2009-01-24 23:23:25.000000000 +0900
-@@ -0,0 +1,2 @@
-+#include "freebsd7.h"
-+#define freebsd7 freebsd7
--- net-mgmt/net-snmp/files/patch-freebsd9.h
+++
@@ -1,5 +0,0 @@
---- include/net-snmp/system/freebsd9.h.orig	2009-09-11 10:02:17.000000000 -0400
-+++ include/net-snmp/system/freebsd9.h	2009-09-11 10:02:17.000000000 -0400
-@@ -0,0 +1,2 @@
-+#include "freebsd8.h"
-+#define freebsd8 freebsd8
