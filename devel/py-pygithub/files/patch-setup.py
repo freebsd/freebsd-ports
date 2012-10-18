ab95c2adf212875da8227f6c5cd2f82cafd86516
sbz <sbz@FreeBSD.org>
2012-10-18 11:07:45 +0000

    - Update to 1.8.0 - Add regression-test target
    PR:		ports/172383
    Submitted by:	Kubilay Kocak <koobs.freebsd at gmail.com>
    Feature safe:	yes

--- devel/py-pygithub/files/patch-setup.py
+++ devel/py-pygithub/files/patch-setup.py
@@ -1,11 +1,11 @@
---- ./setup.py.orig	2012-07-13 21:42:12.000000000 +1000
-+++ ./setup.py	2012-07-14 19:15:08.000000000 +1000
-@@ -54,9 +54,6 @@
-     packages = [
+--- ./setup.py.orig	2012-10-06 17:07:20.000000000 +1000
++++ ./setup.py	2012-10-06 17:07:31.000000000 +1000
+@@ -68,9 +68,6 @@
          "github",
+         "github.tests",
      ],
 -    package_data = {
--        "github": [ "ReadMe.md", "COPYING*", "doc/*.md" ]
+-        "github": [ "ReadMe.md", "COPYING*", "doc/*.md", "tests/ReplayData/*.txt" ]
 -    },
      classifiers = [
          "Development Status :: 5 - Production/Stable",
