8945bb430758099c116e2186d96cc2daf78cf91a
rakuco <rakuco@FreeBSD.org>
2012-10-21 11:14:59 +0000

    - Update to 0.10. - Convert to OptionsNG.
    PR:		ports/172684
    Submitted by:	Craig Leres <leres@ee.lbl.gov> (maintainer)
    Feature safe:	yes

--- devel/arduino-mk/files/patch-arduino-mk-Arduino.mk
+++ devel/arduino-mk/files/patch-arduino-mk-Arduino.mk
@@ -1,6 +1,6 @@
---- Arduino.mk.orig	2012-02-12 09:19:53.000000000 -0800
-+++ Arduino.mk	2012-08-04 23:47:14.000000000 -0700
-@@ -92,7 +92,7 @@
+--- arduino-mk/Arduino.mk.orig	2012-09-17 10:41:19.000000000 -0700
++++ arduino-mk/Arduino.mk	2012-09-17 19:21:52.000000000 -0700
+@@ -160,7 +160,7 @@
  #
  #    ARDUINO_LIBS - A list of any libraries used by the sketch (we
  #                   assume these are in
@@ -9,7 +9,7 @@
  #
  #    ARDUINO_PORT - The port where the Arduino can be found (only needed
  #                   when uploading
-@@ -176,7 +176,7 @@
+@@ -287,7 +287,7 @@
  #
  # Arduino version number
  ifndef ARDUINO_VERSION
@@ -17,17 +17,8 @@
 +ARDUINO_VERSION = %%ARDUINO%%
  endif
  
- #
-@@ -215,7 +215,7 @@
- endif
- 
- ifndef PARSE_BOARD
--PARSE_BOARD = ard-parse-boards --boards_txt=$(BOARDS_TXT)
-+PARSE_BOARD = $(ARDUINO_DIR)/tools/ard-parse-boards --boards_txt=$(BOARDS_TXT)
- endif
- 
- # Which variant ? This affects the include path
-@@ -268,12 +268,12 @@
+ ########################################################################
+@@ -448,12 +448,12 @@
  ########################################################################
  # Local sources
  #
@@ -46,7 +37,7 @@
  LOCAL_OBJ_FILES = $(LOCAL_C_SRCS:.c=.o)   $(LOCAL_CPP_SRCS:.cpp=.o) \
  		$(LOCAL_CC_SRCS:.cc=.o)   $(LOCAL_PDE_SRCS:.pde=.o) \
  		$(LOCAL_INO_SRCS:.ino=.o) $(LOCAL_AS_SRCS:.S=.o)
-@@ -392,7 +392,11 @@
+@@ -593,7 +593,11 @@
  
  # the pde -> cpp -> o file
  $(OBJDIR)/%.cpp: %.pde
