f155dc3ddca47e3baa9a31630aac238232becd83
Baptiste Daroussin <bapt@FreeBSD.org>
2012-10-25 18:02:31 +0000

    2012-10-20 security/sscep: No more public distfiles 2012-10-20 security/py-rijndael: No more public distfiles 2012-10-20 security/pam_af: No more public distfiles 2012-10-20 security/its4: No more public distfiles 2012-10-20 russian/xcyrBGR: No more public distfiles 2012-10-20 russian/wmcyrx: No more public distfiles 2012-10-20 print/advi: No more public distfiles 2012-10-20 palm/plucker: No more public distfiles 2012-10-20 net-p2p/py-bittornado-core: Depends on the deprecated wx 2.4 2012-10-20 net-p2p/py-bittornado: Depends on the deprecated wx 2.4 2012-10-20 net-mgmt/nagios-radauth-plugin: No more public distfiles 2012-10-20 net-mgmt/nagios-check_nick: No more public distfiles
    Feature safe:	yes

--- palm/plucker/files/patch-PyPlucker::Parser.py
+++
@@ -1,8 +0,0 @@
---- PyPlucker/Parser.py.orig	Wed Dec 24 16:05:12 2003
-+++ PyPlucker/Parser.py	Wed Dec 24 16:05:21 2003
-@@ -1,4 +1,5 @@
- #!/usr/bin/env python
-+# -*- coding: iso-8859-1 -*-
- 
- """
- Parser.py   $Id: Parser.py,v 1.21 2003/02/18 01:29:38 chrish Exp $
