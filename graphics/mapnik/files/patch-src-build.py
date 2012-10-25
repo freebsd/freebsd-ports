86c3152156e732b14f36fb6bbf85589ee6cb534f
Guido Falsi <madpilot@FreeBSD.org>
2012-10-25 20:47:23 +0000

    - Update to 2.1.0 - Add CONFLICT with graphics/svg2png due to common files installled. - Convert to new options framework - Trim Makefile headers
    PR:		ports/171722
    Submitted by:	Alexey Illarionov <littlesavage@rambler.ru> (maintainer)
    Feature safe:	yes

--- graphics/mapnik/files/patch-src-build.py
+++ graphics/mapnik/files/patch-src-build.py
@@ -1,48 +1,31 @@
---- src/build.py.orig	2012-04-11 14:29:19.000000000 +0400
-+++ src/build.py	2012-04-11 14:34:13.000000000 +0400
-@@ -80,6 +80,8 @@
- 
- if env['PLATFORM'] == 'Darwin':
-     mapnik_libname = 'libmapnik.dylib'
-+elif env['PLATFORM'] == 'FreeBSD':
-+    mapnik_libname = 'libmapnik.so.' + ("%d" % int(ABI_VERSION[0]))
- else:
-     mapnik_libname = 'libmapnik.so.' + ("%d.%d" % (int(ABI_VERSION[0]),int(ABI_VERSION[1])))
- 
-@@ -326,8 +328,12 @@
-         os.symlink(os.path.basename(src), trgt)
+--- src/build.py.orig	2012-08-24 01:57:06.000000000 +0400
++++ src/build.py	2012-09-16 19:38:43.000000000 +0400
+@@ -357,7 +357,11 @@
  
      major, minor, micro = ABI_VERSION
--    
--    soFile = "%s.%d.%d.%d" % (os.path.basename(str(mapnik[0])), int(major), int(minor), int(micro))
-+
-+    if env['PLATFORM'] == 'FreeBSD':
-+        soFile = "%s.%d" % (os.path.basename(str(mapnik[0])), int(major))
-+    else:
-+        soFile = "%s.%d.%d.%d" % (os.path.basename(str(mapnik[0])), int(major), int(minor), int(micro))
-+   
-     target = os.path.join(env['MAPNIK_LIB_BASE_DEST'], soFile)
-     
-     if 'uninstall' not in COMMAND_LINE_TARGETS:
-@@ -342,13 +348,17 @@
-     target2 = os.path.join(env['MAPNIK_LIB_BASE_DEST'], os.path.basename(str(mapnik[0])))
-     if 'uninstall' not in COMMAND_LINE_TARGETS:
-         if 'install' in COMMAND_LINE_TARGETS:
--            link1 = env.Command(target1, target, symlink)
--            env.Alias(target='install', source=link1)
--            link2 = env.Command(target2, target1, symlink)
-+            if env['PLATFORM'] == 'FreeBSD':
-+                link2 = env.Command(target2, target, symlink)
-+            else:
-+                link1 = env.Command(target1, target, symlink)
-+                env.Alias(target='install', source=link1)
-+                link2 = env.Command(target2, target1, symlink)
-             env.Alias(target='install', source=link2)
-     # delete in reverse order..
-     env['create_uninstall_target'](env, target2)
--    env['create_uninstall_target'](env, target1)
-+    if env['PLATFORM'] != 'FreeBSD':
-+        env['create_uninstall_target'](env, target1)
-     env['create_uninstall_target'](env, target)
  
- else:
+-    soFile = "%s.%d.%d.%d" % (os.path.basename(env.subst(env['MAPNIK_LIB_NAME'])), int(major), int(minor), int(micro))
++    if env['PLATFORM'] == 'FreeBSD':
++        soFile = "%s.%d.%d" % (os.path.basename(env.subst(env['MAPNIK_LIB_NAME'])), int(major), int(minor))
++    else:
++        soFile = "%s.%d.%d.%d" % (os.path.basename(env.subst(env['MAPNIK_LIB_NAME'])), int(major), int(minor), int(micro))
++
+     target = os.path.join(env['MAPNIK_LIB_BASE_DEST'], soFile)
+ 
+     if 'uninstall' not in COMMAND_LINE_TARGETS:
+@@ -372,8 +376,13 @@
+ 
+ 
+     # Install symlinks
+-    target1 = os.path.join(env['MAPNIK_LIB_BASE_DEST'], "%s.%d.%d" % \
+-        (os.path.basename(env.subst(env['MAPNIK_LIB_NAME'])),int(major), int(minor)))
++    if env['PLATFORM'] == 'FreeBSD':
++        target1 = os.path.join(env['MAPNIK_LIB_BASE_DEST'], "%s.%d" % \
++              (os.path.basename(env.subst(env['MAPNIK_LIB_NAME'])),int(major)))
++    else:
++        target1 = os.path.join(env['MAPNIK_LIB_BASE_DEST'], "%s.%d.%d" % \
++              (os.path.basename(env.subst(env['MAPNIK_LIB_NAME'])),int(major), int(minor)))
++
+     target2 = os.path.join(env['MAPNIK_LIB_BASE_DEST'], os.path.basename(env.subst(env['MAPNIK_LIB_NAME'])))
+     if 'uninstall' not in COMMAND_LINE_TARGETS:
+         link1 = env.Command(target1, target, symlink)
