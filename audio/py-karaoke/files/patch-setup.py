c247597771e30694a61ab743d1ae3380f30c0213
pawel <pawel@FreeBSD.org>
2012-11-01 14:47:37 +0000

    - Update to version 0.7.5 - Add optional run dependency on audio/py-mutagen - Install additional fonts - Add LICENSE
    PR:		ports/172217
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>
    Feature safe:	yes

---
+++ audio/py-karaoke/files/patch-setup.py
@@ -0,0 +1,16 @@
+--- setup.py.orig	2011-06-15 03:59:31.000000000 +0900
++++ setup.py	2012-09-28 07:03:50.000000000 +0900
+@@ -61,12 +61,7 @@
+       'icons/microphone.ico',
+       'icons/microphone.png',
+       'icons/pykaraoke.xpm',
+-      'icons/splash.png']),
+-    ('share/pykaraoke/fonts', [
+-    'fonts/DejaVuSans.ttf',
+-    'fonts/DejaVuSansCondensed.ttf',
+-    'fonts/DejaVuSansCondensed-Bold.ttf',
+-    ])]
++      'icons/splash.png'])]
+ 
+ # These data files only make sense on Unix-like systems.
+ if env != ENV_WINDOWS:
