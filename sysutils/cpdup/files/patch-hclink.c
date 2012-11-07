f37722a04211264a63e216a0197a6e8d82bb3efc
Guido Falsi <madpilot@FreeBSD.org>
2012-11-07 08:25:42 +0000

    - Don't pass -T option to ssh process, this allows use of the "null"   cipher if requested [1] - Fix build with clang - Trim Makefile headers
    Submitted by:	Oliver Fromme <olli@grabthar.secnetix.de> [1]
    Approved by:	Gianmarco Giovannelli <gmarco@gufi.org> (maintainer)
    Feature safe:	yes

--- sysutils/cpdup/files/patch-hclink.c
+++ sysutils/cpdup/files/patch-hclink.c
@@ -1,5 +1,14 @@
---- ./hclink.c.orig	2010-09-25 18:01:27.989863475 -0400
-+++ ./hclink.c	2010-09-25 18:01:46.138176079 -0400
+--- hclink.c.orig	2010-07-19 21:01:56.000000000 +0200
++++ hclink.c	2012-11-05 13:16:09.731500043 +0100
+@@ -49,7 +49,7 @@
+ 	    av[n++] = "-C";
+ 	for (m = 0; m < ssh_argc; m++)
+ 	    av[n++] = ssh_argv[m];
+-	av[n++] = "-T";
++	av[n++] = "--";
+ 	av[n++] = hc->host;
+ 	av[n++] = "cpdup";
+ 	av[n++] = (readonly ? "-RS" : "-S");
 @@ -388,7 +388,7 @@
  hcc_check_space(hctransaction_t trans, struct HCHead *head, int n, int size)
  {
