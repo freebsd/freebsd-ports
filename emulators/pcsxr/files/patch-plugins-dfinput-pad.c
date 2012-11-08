5e908740569ba6f475a96f38ee468b323b6df32b
Ganael LAPLANCHE <martymac@FreeBSD.org>
2012-11-08 10:06:53 +0000

    Update to 1.9.92.r81088
    Feature safe:	yes

--- emulators/pcsxr/files/patch-plugins-dfinput-pad.c
+++ emulators/pcsxr/files/patch-plugins-dfinput-pad.c
@@ -1,6 +1,15 @@
---- plugins/dfinput/pad.c.orig	2009-12-25 18:26:35.000000000 +0000
-+++ plugins/dfinput/pad.c	2010-06-03 08:06:29.658820184 +0000
-@@ -386,3 +386,6 @@
+--- plugins/dfinput/pad.c.orig	2012-11-08 06:07:00.000000000 +0100
++++ plugins/dfinput/pad.c	2012-11-08 06:09:26.000000000 +0100
+@@ -676,8 +676,6 @@
+ 	} else if (pid > 0) {
+ 		waitpid(pid, NULL, 0);
+ 	}
+-
+-	return PSE_PAD_ERR_SUCCESS;
+ }
+ 
+ #endif
+@@ -685,3 +683,6 @@
  long PADtest(void) {
  	return PSE_PAD_ERR_SUCCESS;
  }
