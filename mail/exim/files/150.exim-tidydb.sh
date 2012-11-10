0ea241bd439478217cafbe35ccfbe5cca835a493
rea <rea@FreeBSD.org>
2012-11-10 10:01:20 +0000

    mail/exim: add exim_tidydb_filter for periodic tidydb job
    It allows to filter/transform the output from the tidydb that is
    sometimes large and not very much suitable for informational mails
    from cron.
    Feature safe:	yes

--- mail/exim/files/150.exim-tidydb.sh
+++ mail/exim/files/150.exim-tidydb.sh
@@ -16,12 +16,13 @@ fi
 
 : ${exim_tidydb_enable="YES"}
 : ${exim_tidydb="%%PREFIX%%/sbin/exim_tidydb"}
+# Set this to arbitrary shell command to filter
+# the output from this periodic script, for example,
+# exim_tidydb_filter="| tail -100".
+: ${exim_tidydb_filter=""}
 : ${exim_dbdir="/var/spool/exim"}
 
-case "$exim_tidydb_enable" in
-    [Yy][Ee][Ss])
-	echo ""
-	echo "Tidying Exim hints databases:"
+tidy () {
 	for db in "$exim_dbdir"/db/*.lockfile; do
 	    [ "$db" = "$exim_dbdir/db/*.lockfile" ] && continue
 	    echo
@@ -30,6 +31,13 @@ case "$exim_tidydb_enable" in
 	        "$exim_tidydb" "$exim_dbdir" "$db_name"
 	    fi
 	done
+}
+
+case "$exim_tidydb_enable" in
+    [Yy][Ee][Ss])
+	echo ""
+	echo "Tidying Exim hints databases:"
+	eval tidy "$exim_tidydb_filter"
 	rc=1;;
 
     *)  rc=0;;
