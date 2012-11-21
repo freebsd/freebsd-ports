f01a69722f1a8f010e39bd98f3b07c132a3662f0
Kevin Lo <kevlo@FreeBSD.org>
2012-11-21 05:38:37 +0000

    Fix build with clang.
    Feature safe:	yes

--- ftp/atftp/files/patch-tftp_def.h
+++ ftp/atftp/files/patch-tftp_def.h
@@ -1,5 +1,5 @@
 --- tftp_def.h.orig	2004-02-13 11:16:09.000000000 +0800
-+++ tftp_def.h	2010-10-05 12:36:29.000000000 +0800
++++ tftp_def.h	2012-11-21 13:21:56.000000000 +0800
 @@ -19,6 +19,7 @@
  
  #include <sys/time.h>
@@ -8,3 +8,12 @@
  #include <netdb.h>
  
  /* standard return value */
+@@ -49,7 +50,7 @@
+ 
+ int timeval_diff(struct timeval *res, struct timeval *t1, struct timeval *t0);
+ int print_eng(double value, char *string, int size, char *format);
+-inline char *Strncpy(char *to, const char *from, size_t size);
++extern inline char *Strncpy(char *to, const char *from, size_t size);
+ int Gethostbyname(char *addr, struct hostent *host);
+ 
+ #endif
