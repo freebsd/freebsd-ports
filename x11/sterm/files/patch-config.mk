ff9e6152846341c22016d873a54141073d0ae4c5
madpilot <madpilot@FreeBSD.org>
2012-11-27 13:10:41 +0000

    - Update to 0.3 - Use new options framework for DOCS - Trim Makefile headers
    PR:		ports/173941
    Submitted by:	Matthew Donovan <kitche@kitchetech.com> (maintainer)
    Feature safe:	yes

--- x11/sterm/files/patch-config.mk
+++ x11/sterm/files/patch-config.mk
@@ -1,21 +1,11 @@
---- config.mk.orig	2011-01-23 12:30:01.000000000 +0100
-+++ config.mk	2011-04-06 14:25:05.291720015 +0200
-@@ -5,6 +5,7 @@
+--- config.mk.orig	2012-11-02 20:09:49.000000000 +0100
++++ config.mk	2012-11-26 22:28:12.270095047 +0100
+@@ -11,7 +11,7 @@
+ X11LIB = /usr/X11R6/lib
  
- # paths
- PREFIX = /usr/local
-+LOCALBASE= /usr/local
- MANPREFIX = ${PREFIX}/share/man
- 
- X11INC = /usr/X11R6/include
-@@ -16,8 +17,8 @@
+ # includes and libs
+-INCS = -I. -I/usr/include -I${X11INC} -I/usr/include/freetype2
++INCS = -I. -I/usr/include -I${X11INC} -I${X11INC}/freetype2
+ LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lutil -lXext -lXft -lfontconfig
  
  # flags
- CPPFLAGS = -DVERSION=\"${VERSION}\"
--CFLAGS += -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
--LDFLAGS += -s ${LIBS}
-+CFLAGS += -std=c99 -pedantic -Wall ${INCS} ${CPPFLAGS}
-+LDFLAGS += ${LIBS}
- 
- # compiler and linker
- CC ?= cc
