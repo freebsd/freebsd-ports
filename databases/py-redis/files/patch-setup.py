9d233257d2622daa53bf794122b1d555b6d51f79
Steve Wills <swills@FreeBSD.org>
2012-11-28 03:35:05 +0000

    - Update to 2.7.0 - Add CHEESESHOP as primary MASTERSITE - Remove LICENSE_FILE for those in bsd.licenses.db.mk - Whitespace alignment - Add HIREDIS OPTIONng - Use easy_install (make tests easier to run) - Add zip_safe=False to setup.py - Update and sort pkg-plist - Replace dirrmtry with dirrm - New Makefile header convention - Give maintainership to submitter
    PR:		ports/172481
    Submitted by:	Kubilay Kocak <koobs.freebsd@gmail.com>
    Approved by:	clsung via IRC
    Feature safe:	yes

--- databases/py-redis/files/patch-setup.py
+++ databases/py-redis/files/patch-setup.py
@@ -1,14 +1,22 @@
---- ./setup.py.orig	2012-05-27 06:05:41.000000000 +1000
-+++ ./setup.py	2012-05-27 06:05:49.000000000 +1000
-@@ -30,10 +30,7 @@
-         'Programming Language :: Python'],
- }
+--- ./setup.py.orig	2012-08-10 08:48:07.000000000 +1000
++++ ./setup.py	2012-08-15 22:27:30.000000000 +1000
+@@ -3,10 +3,7 @@
+ 
+ from redis import __version__
  
 -try:
 -    from setuptools import setup
 -except ImportError:
 -    from distutils.core import setup
-+from distutils.core import setup
++from setuptools import setup
  
- setup(**sdict)
- 
+ f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+ long_description = f.read()
+@@ -41,5 +38,6 @@
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.2',
+         'Programming Language :: Python :: 3.3',
+-        ]
++        ],
++    zip_safe=False,
+ )
