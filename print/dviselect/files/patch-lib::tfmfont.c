008eb357f9bb4aa064830822d8bf6eb05c714a10
gahr <gahr@FreeBSD.org>
2012-11-30 14:02:19 +0000

    - Fix build with clang
    Feature safe:	yes

--- print/dviselect/files/patch-lib::tfmfont.c
+++ print/dviselect/files/patch-lib::tfmfont.c
@@ -1,5 +1,5 @@
---- lib/tfmfont.c~	Wed Nov 15 13:20:03 1989
-+++ lib/tfmfont.c	Wed Dec 13 23:04:06 2006
+--- lib/tfmfont.c.orig	1989-11-15 13:20:03.000000000 +0100
++++ lib/tfmfont.c	2012-11-30 15:00:41.000000000 +0100
 @@ -139,7 +139,7 @@
  fail:
  	(void) fclose(fd);
@@ -9,3 +9,12 @@
  	}
  	return (-1);
  }
+@@ -199,7 +199,7 @@
+ #define EDGE 2
+ 
+ 	if (tfm->tfm_edge == 0)
+-		return;
++		return (-1);
+ if (tfm->tfm_edge != 2) panic("tfm_rasterise");
+ 	for (i = l; i < h; i++) {
+ 		g = f->f_gly[i];
