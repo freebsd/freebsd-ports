ce09d1b4b31551ef0c9cb8e39129ada9d44dba96
Pietro Cerutti <gahr@FreeBSD.org>
2012-11-30 14:02:19 +0000

    - Fix build with clang
    Feature safe:	yes

--- print/dviselect/files/patch-lib::pkfont.c
+++ print/dviselect/files/patch-lib::pkfont.c
@@ -1,5 +1,14 @@
---- lib/pkfont.c.orig	Wed Nov 15 13:19:52 1989
-+++ lib/pkfont.c	Wed Dec 13 23:01:34 2006
+--- lib/pkfont.c.orig	1989-11-15 13:19:52.000000000 +0100
++++ lib/pkfont.c	2012-11-30 14:59:23.000000000 +0100
+@@ -183,7 +183,7 @@
+ /*
+  * Skip over special commands (PK_XXX?, PK_YYY).
+  */
+-static
++static void
+ skip_specials(f)
+ 	struct font *f;
+ {
 @@ -781,8 +781,8 @@
  	if (pk != NULL) {
  		free(pk->pk_base); pk -> pk_base = 0;
