4c010744f97afc39c126ae2cd40ed176031faa2f
sperber <sperber@FreeBSD.org>
2012-12-01 00:00:26 +0000

    - Update to version 3.310 - Update MASTER_SITES - Add LICENSE - Add MAKE_JOBS_SAFE - Trim Makefile header
    PR:           ports/171954
    Submitted by: KATO Tsuguru <tkato432 _at_ yahoo.com>
    Approved by:  beat (mentor)
    Feature safe: yes

---
+++ astro/cfitsio/files/patch-fpackutil.c
@@ -0,0 +1,10 @@
+--- fpackutil.c.orig	2012-07-18 05:36:42.000000000 +0900
++++ fpackutil.c	2012-08-10 01:28:12.000000000 +0900
+@@ -5,6 +5,7 @@
+ #include <time.h>
+ #include <float.h>
+ #include <signal.h>
++#include <ctype.h>
+ 
+ /* #include  "bzlib.h"  only for experimental purposes */
+ 
