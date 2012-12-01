74c3d13afc9dbf9e4d7d9b4e7f3a262dbec600bd
sperber <sperber@FreeBSD.org>
2012-12-01 00:00:43 +0000

    - Update to version 6.1.01 - Add MAKE_JOBS_SAFE - Convert to optionsng - Trim Makefile header
    PR:           ports/171971
    Submitted by: KATO Tsuguru <tkato432 _at_ yahoo.com>
    Approved by:  beat (mentor)
    Feature safe: yes

--- biology/tinker/files/patch-source_openend.f
+++ biology/tinker/files/patch-source_openend.f
@@ -1,8 +1,8 @@
 
 $FreeBSD$
 
---- openend.f.orig	Fri Apr 18 13:31:51 2003
-+++ openend.f	Mon Jul 21 13:36:11 2003
+--- source/openend.f.orig	Fri Apr 18 13:31:51 2003
++++ source/openend.f	Mon Jul 21 13:36:11 2003
 @@ -29,11 +29,11 @@
  c
  c     standard Fortran 90, unavailable in some Fortran 77 compilers
