83cc3b9f8d6ae981c9d0790040692495bf7adc12
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2012-12-02 10:39:45 +0000

    - update to 1.0.1 - add OPTIONS for unit test support - patch setup.py to install the egg uncompressed - use a pkg-plist instead of PLIST_FILES - restore creator to Makefile header and use new convention - fix plist for python >= 3.2
    PR:		172592
    Submitted by:	Kubilay Kocak <koobs.freebsd@gmail.com>
    Approved by:	maintainer timeout (1 month+)
    Feature safe:	yes

---
+++ devel/py-mock/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- ./setup.py.orig	2012-10-09 20:11:41.000000000 +1100
++++ ./setup.py	2012-10-09 20:11:26.000000000 +1100
+@@ -59,6 +59,7 @@
+     keywords=KEYWORDS,
+     url=URL,
+     classifiers=CLASSIFIERS,
++    zip_safe=False,
+ )
+ 
+ try:
