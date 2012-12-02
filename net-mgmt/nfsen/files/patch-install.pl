3a4a03ce62a8ac1d9566d418444bc6cb7d475906
wxs <wxs@FreeBSD.org>
2012-12-02 02:41:19 +0000

    Update to 1.3.6p1.
    PR:		ports/173594
    Submitted by:	Mike Stupalov <landy2005@gmail.com>
    Approved by:	janos.mohacsi@bsd.hu (maintainer timeout)
    Feature safe:	yes

--- net-mgmt/nfsen/files/patch-install.pl
+++ net-mgmt/nfsen/files/patch-install.pl
@@ -1,12 +1,12 @@
---- install.pl.orig	2010-09-09 09:56:05.000000000 +0400
-+++ install.pl	2011-08-13 16:39:01.000000000 +0400
+--- install.pl.orig	2012-01-23 20:36:02.000000000 +0400
++++ install.pl	2012-11-12 15:52:05.000000000 +0400
 @@ -1,4 +1,4 @@
 -#!/usr/bin/perl
 +#!%%PERL%% -I %%PREFIX%%/libexec/nfsen
  #
  #
  #  Copyright (c) 2004, SWITCH - Teleinformatikdienste fuer Lehre und Forschung
-@@ -85,33 +85,7 @@
+@@ -88,33 +88,7 @@
  # Get Perl
  sub GetPerl {
  
@@ -41,27 +41,3 @@
  
  } # End of GetPerl
  
-@@ -200,11 +174,8 @@
- 	}
- 
- 	my @out = `$NfConf::PREFIX/nfdump -V`;
--	if ( scalar @out != 2 ) {
--		die "Error getting nfdump version";
--	}
- 	chomp $out[0];
--	my ($major, $minor) = $out[0] =~ /Version:\s(\d)\.(\d)\s/;
-+	my ($major, $minor) = $out[0] =~ /Version:\s(\d)\.(\d)[\.\s]/;
- 	if ( defined $major && defined $minor) {
- 		if ( $major >= 1 && $minor >= 6 ) {
- 			print "Found $out[0]\n";
-@@ -212,7 +183,9 @@
- 			print "out[0]\n";
- 			die "Nfdump version not compatible with current NfSen version.\n";
- 		}
--	} 
-+	} else {
-+	       die "Error getting nfdump version";
-+        }	       
- 
- 	my $www_gid = getgrnam($NfConf::WWWGROUP) || 
- 		die "WWW group '$NfConf::WWWGROUP' not found on this system\n";
