349c6e1957dcc0de46ad8477ece9db4c0c0f2605
gahr <gahr@FreeBSD.org>
2012-12-03 13:33:29 +0000

    - Fix build with clang - Trim Makefile header
    Feature safe:	yes

--- net/libsocketcpp/files/patch-src::tcptest.cpp
+++ net/libsocketcpp/files/patch-src::tcptest.cpp
@@ -1,12 +1,22 @@
-$FreeBSD$
-
---- src/tcptest.cpp.orig	Sun Jun  9 16:03:19 2002
-+++ src/tcptest.cpp	Sun Jun  9 16:03:31 2002
-@@ -22,6 +22,7 @@
+--- src/tcptest.cpp.orig	2001-02-16 19:45:31.000000000 +0100
++++ src/tcptest.cpp	2012-12-03 14:27:37.000000000 +0100
+@@ -22,8 +22,10 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <string.h>
 +#include <sys/types.h>
  #include "tcpsocket.h"
  
++int
  main() {
+ 	char *Host = new char[100];
+ 	char *Buffer = new char[2048];
+@@ -62,6 +64,8 @@
+ 	delete [] Buffer;
+ 	delete [] Host;
+ 	delete [] TempMsg;
++
++	return 0;
+ }
+ 
+ 
