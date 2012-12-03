440e9d17c5f7fa213d509798cb2e42943355b7ea
Pietro Cerutti <gahr@FreeBSD.org>
2012-12-03 09:51:46 +0000

    - Fix build with clang - Trim Makefile header
    Feature safe:	yes

--- devel/fortran-utils/files/patch-fpr.c
+++ devel/fortran-utils/files/patch-fpr.c
@@ -1,5 +1,5 @@
---- fpr/fpr.c.orig	Fri May 27 14:31:21 1994
-+++ fpr/fpr.c	Sat Jun 12 23:49:50 2004
+--- fpr/fpr.c.orig	1994-05-27 14:31:21.000000000 +0200
++++ fpr/fpr.c	2012-12-03 10:50:48.000000000 +0100
 @@ -45,6 +45,7 @@
  #endif /* not lint */
  
@@ -8,17 +8,26 @@
  
  #define BLANK ' '
  #define TAB '\t'
-@@ -80,9 +81,6 @@
+@@ -80,12 +81,13 @@
  int maxpos;
  int maxcol;
  
 -extern char *malloc();
 -extern char *calloc();
 -extern char *realloc();
+-
+-
++void init();
++void mygettext();
++void flush();
++void savech(int);
++void nospace();
  
- 
- 
-@@ -124,7 +122,7 @@
++int
+ main()
+ {
+   register int ch;
+@@ -124,7 +126,7 @@
  
    while ( ! ateof)
      {
@@ -27,12 +36,48 @@
        ch = getchar();
        if (ch == EOF)
  	{
-@@ -210,7 +208,7 @@
+@@ -176,6 +178,7 @@
+ 
+ 
+ 
++void
+ init()
+ {
+   register COLUMN *cp;
+@@ -210,7 +213,8 @@
  
  
  
 -gettext()
++void
 +mygettext()
  {
    register int i;
    register char ateol;
+@@ -283,8 +287,8 @@
+ 
+ 
+ 
+-savech(col)
+-int col;
++void
++savech(int col)
+ {
+   register char ch;
+   register int oldmax;
+@@ -340,6 +344,7 @@
+ 
+ 
+ 
++void
+ flush()
+ {
+   register int i;
+@@ -403,6 +408,7 @@
+ 
+ 
+ 
++void
+ nospace()
+ {
+   fputs("Storage limit exceeded.\n", stderr);
