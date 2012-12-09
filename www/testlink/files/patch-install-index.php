6b0bc9275e404b086622f0f1b922a8cd5678a3c1
TAKATSU Tomonari <tota@FreeBSD.org>
2012-12-09 08:27:45 +0000

    - Update to 1.9.5
    Feature safe:	yes

--- www/testlink/files/patch-install-index.php
+++ www/testlink/files/patch-install-index.php
@@ -1,7 +1,7 @@
---- install/index.php.orig	2012-09-01 22:55:30.000000000 +0900
-+++ install/index.php	2012-09-10 01:06:21.000000000 +0900
-@@ -45,7 +45,7 @@
- 		<p>Please read Section on README file or go to www.teamst.org (Forum: TestLink 1.9.4 News,changes, etc) </p>
+--- install/index.php.orig	2012-12-08 23:09:18.000000000 +0900
++++ install/index.php	2012-12-09 16:54:13.000000000 +0900
+@@ -39,7 +39,7 @@
+ 		<p>Please read Section on README file or go to www.teamst.org (Forum: TestLink 1.9.5 News,changes, etc) </p>
  		<p>Open <a target="_blank" href="../docs/testlink_installation_manual.pdf">Installation manual</a>
  		for more information or troubleshooting. You could also look at
 -		<a href="../README">README</a> or <a href="../CHANGELOG">Changes Log</a>.
@@ -9,7 +9,7 @@
  		You are welcome to visit our <a target="_blank" href="http://www.teamst.org">
  		forum</a> to browse or discuss.
  		</p>
-@@ -57,4 +57,4 @@
+@@ -51,4 +51,4 @@
  
  </div>
  </body>
