7266679e97e9f6501d290eed687ca2ec8e16c5b7
Maxim Sobolev <sobomax@FreeBSD.org>
2012-12-12 01:13:40 +0000

    Add support for the --exit-label option.

--- devel/py-dialog/files/patch-dialog.py
+++ devel/py-dialog/files/patch-dialog.py
@@ -3,17 +3,18 @@ $FreeBSD$
 
 --- dialog.py.orig
 +++ dialog.py
-@@ -214,6 +214,9 @@
+@@ -214,6 +214,10 @@
      "create_rc": lambda file: ("--create-rc", file),
      "defaultno": lambda enable: _simple_option("--defaultno", enable),
      "default_item": lambda string: ("--default-item", string),
 +    "yes_label": lambda string: ("--yes-label", string),
 +    "no_label": lambda string: ("--no-label", string),
++    "exit_label": lambda string: ("--exit-label", string),
 +    "extra_button": lambda enable: _simple_option("--extra-button", enable),
      "help": lambda enable: _simple_option("--help", enable),
      "help_button": lambda enable: _simple_option("--help-button", enable),
      "help_label": lambda string: ("--help-label", string),
-@@ -524,7 +527,7 @@
+@@ -524,7 +528,7 @@
  
      """
  
