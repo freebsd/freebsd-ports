0ea746cc5aabb6be06b78166bdde008d738be805
martymac <martymac@FreeBSD.org>
2012-12-12 07:42:34 +0000

    - Update devel/tbb to 4.1.1 and bump dependent port's PORTREVISIONs - Remove hardcoded dependency on gcc [1]
    PR:		ports/171297 [1]
    Submitted by:	Christian Mangin <christian.mangin@gmail.com> [1]

--- devel/tbb/files/version_info_FreeBSD.sh
+++ devel/tbb/files/version_info_FreeBSD.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# Script used to generate version info string
-echo "#define __TBB_VERSION_STRINGS \\"
-echo '"TBB:' "BUILD_HOST\t\t"`hostname -s`" ("`uname -p`")"'" ENDL \'
-echo '"TBB:' "BUILD_OS\t\t"`uname -sr`'" ENDL \'
-echo '"TBB:' "BUILD_KERNEL\t"`uname -rv`'" ENDL \'
-echo '"TBB:' "BUILD_GCC\t\t"`g++ -v </dev/null 2>&1 | grep 'gcc.*version'`'" ENDL \'
-[ -z "$COMPILER_VERSION" ] || echo '"TBB:' "BUILD_COMPILER\t"$COMPILER_VERSION'" ENDL \'
-echo '"TBB:' "BUILD_GLIBC\t"`ldconfig -r | grep -oE '/lib/libc.so.[0-9]+' | sort -rn | head -n 1 | sed 's|/lib/libc\.so\.||'`'" ENDL \'
-echo '"TBB:' "BUILD_LD\t\t"`ld -v | grep 'version'`'" ENDL \'
-echo '"TBB:' "BUILD_TARGET\t$arch on $runtime"'" ENDL \'
-echo '"TBB:' "BUILD_COMMAND\t"$*'" ENDL \'
+# Script used to generate FreeBSD version info string
+echo "#define __TBB_VERSION_STRINGS(N) \\"
+echo '#N": BUILD_HOST'"\t\t"`hostname -s`" ("`uname -m`")"'" ENDL \'
+echo '#N": BUILD_OS'"\t\t"`uname -sr`'" ENDL \'
+echo '#N": BUILD_KERNEL'"\t"`uname -i`'" ENDL \'
+echo '#N": BUILD_GCC'"\t\t"`${CXX} --version </dev/null 2>&1 | head -n 1`'" ENDL \'
+[ -z "$COMPILER_VERSION" ] || echo '#N": BUILD_COMPILER'"\t"$COMPILER_VERSION'" ENDL \'
+echo '#N": BUILD_GLIBC'"\t"`ldconfig -r | grep -oE '/lib/libc.so.[0-9]+' | sort -rn | head -n 1 | sed 's|/lib/libc\.so\.||'`'" ENDL \'
+echo '#N": BUILD_LD'"\t\t"`ld -v 2>&1 | head -n 1`'" ENDL \'
+echo '#N": BUILD_TARGET'"\t$arch on $runtime"'" ENDL \'
+echo '#N": BUILD_COMMAND'"\t"$*'" ENDL \'
 echo ""
 echo "#define __TBB_DATETIME \""`date -u`"\""
