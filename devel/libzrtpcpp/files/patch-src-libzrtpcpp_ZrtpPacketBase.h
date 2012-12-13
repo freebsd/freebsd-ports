0c44335da93cd99b67391b7a73b390d61d98e4fb
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2012-12-13 16:17:19 +0000

    - Update to 2.3.2
    PR:		170635
    Submitted by:	Waitman Gobble <uzimac  at  da3m0n8t3r.com>

--- devel/libzrtpcpp/files/patch-src-libzrtpcpp_ZrtpPacketBase.h
+++ devel/libzrtpcpp/files/patch-src-libzrtpcpp_ZrtpPacketBase.h
@@ -1,10 +1,10 @@
---- src/libzrtpcpp/ZrtpPacketBase.h	2008-12-10 14:30:18.000000000 -0500
-+++ src/libzrtpcpp/ZrtpPacketBase.h	2008-12-10 14:30:38.000000000 -0500
-@@ -26,6 +26,7 @@
- #include <stdint.h>
- #include <string.h>
- #include <stdlib.h>
+--- src/libzrtpcpp/ZrtpPacketBase.h	2012-10-17 08:16:16.000000000 -0500
++++ src/libzrtpcpp/ZrtpPacketBase.h	2012-12-13 01:23:58.000000000 -0500
+@@ -41,6 +41,7 @@
+ #if defined(_MSC_VER) || defined(WIN32) || defined(_WIN32)
+ #include <winsock2.h>
+ #else
 +#include <sys/types.h>
  #include <netinet/in.h>
+ #endif
  
- #include <libzrtpcpp/zrtpPacket.h>
