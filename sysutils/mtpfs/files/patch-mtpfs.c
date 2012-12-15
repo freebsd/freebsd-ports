8a9f8591dcef3e113e56178a649ca32c3a283f66
Beech Rintoul <beech@FreeBSD.org>
2012-12-15 23:55:36 +0000

    - Update to 1.1
    PR:		ports/174408
    Submitted by:	John Hixson <john@pcbsd.org> (maintainer)

--- sysutils/mtpfs/files/patch-mtpfs.c
+++ sysutils/mtpfs/files/patch-mtpfs.c
@@ -1,15 +1,6 @@
---- mtpfs.c.orig	2008-08-05 07:29:29.000000000 +0200
-+++ mtpfs.c	2012-10-30 00:47:13.487478272 +0100
-@@ -534,7 +534,7 @@
-         }
-     }
-     close (fi->fh);
--    return_unlock();
-+    return_unlock(0);
- }
- 
- void
-@@ -1143,7 +1143,7 @@
+--- mtpfs.c	2012-02-26 16:59:39.000000000 -0800
++++ mtpfs.c	2012-12-12 19:11:46.955525173 -0800
+@@ -1270,7 +1270,7 @@
  }
  
  static int
@@ -18,3 +9,37 @@
  {
      DBG("mtpfs_statfs");
      stbuf->f_bsize=1024;
+@@ -1282,7 +1282,7 @@
+ }
+ 
+ void *
+-mtpfs_init ()
++mtpfs_init (struct fuse_conn_info *conn)
+ {
+     LIBMTP_devicestorage_t *storage;
+     DBG("mtpfs_init");
+@@ -1298,8 +1298,14 @@
+     // Do nothing
+ }
+ 
++int
++mtpfs_blank_chmod(const char *path, mode_t mode)
++{
++	return (0);
++}
++
+ static struct fuse_operations mtpfs_oper = {
+-    .chmod   = mtpfs_blank,
++    .chmod   = mtpfs_blank_chmod,
+     .release = mtpfs_release,
+     .readdir = mtpfs_readdir,
+     .getattr = mtpfs_getattr,
+@@ -1428,7 +1434,7 @@
+ 
+     DBG("Start fuse");
+ 
+-    fuse_stat=fuse_main (argc, argv, &mtpfs_oper);
++    fuse_stat=fuse_main (argc, argv, &mtpfs_oper, NULL);
+     DBG("fuse_main returned %d\n", fuse_stat);
+     return fuse_stat;
+ }
