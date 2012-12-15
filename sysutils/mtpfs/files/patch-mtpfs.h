1cdffa4424c6fbd0a621cf483e0dd9913190a560
beech <beech@FreeBSD.org>
2012-12-15 23:55:36 +0000

    - Update to 1.1
    PR:		ports/174408
    Submitted by:	John Hixson <john@pcbsd.org> (maintainer)

--- sysutils/mtpfs/files/patch-mtpfs.h
+++ sysutils/mtpfs/files/patch-mtpfs.h
@@ -1,18 +1,26 @@
---- mtpfs.h.orig	2009-01-04 03:03:56.000000000 +0800
-+++ mtpfs.h	2009-01-04 03:05:16.000000000 +0800
-@@ -18,8 +18,9 @@
+--- mtpfs.h	2012-02-23 20:37:14.000000000 -0800
++++ mtpfs.h	2012-12-12 19:10:01.055523958 -0800
+@@ -18,7 +18,9 @@
  #include <fcntl.h>
  #include <dirent.h>
  #include <errno.h>
 -#include <sys/statfs.h>
--
 +#include <sys/param.h>
 +#include <sys/mount.h>
 +#include <sys/statvfs.h>
+ 
  #include <libmtp.h>
  #include <glib.h>
- #include <id3tag.h>
-@@ -49,7 +50,7 @@
+@@ -49,7 +51,7 @@
+ 
+ 
+     /* fuse functions */
+-static void * mtpfs_init (void);
++static void * mtpfs_init (struct fuse_conn_info *);
+ static int mtpfs_blank ();
+ static int mtpfs_release (const char *path, struct fuse_file_info *fi);
+ void mtpfs_destroy ();
+@@ -62,7 +64,7 @@
  static int mtpfs_unlink (const gchar * path);
  static int mtpfs_mkdir (const char *path, mode_t mode);
  static int mtpfs_rmdir (const char *path);
@@ -20,4 +28,4 @@
 +static int mtpfs_statfs (const char *path, struct statvfs *stbuf);
  int calc_length(int f);
  
- 
+ static LIBMTP_mtpdevice_t *device;
