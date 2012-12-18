dd221d63be7f2c642d62c872f0227edbba1df1ff
jhale <jhale@FreeBSD.org>
2012-12-18 13:28:14 +0000

    - USE_DOS2UNIX and remove ^M from patches - Use PORTDOCS macro; don't install useless Install.txt   or redundant license.txt - Bump PORTREVISION since installed files have changed - Improve COMMENT - Simplify do-install target - Convert to new options framework - Whitespace cleanup - Add additional WWW - Pass maintainership to submitter
    PR:		ports/173537
    Submitted by:	nemysis <nemysis@gmx.ch>

--- graphics/autoq3d/files/patch-src__fgui__glapparea.cpp
+++ graphics/autoq3d/files/patch-src__fgui__glapparea.cpp
@@ -1,11 +1,10 @@
---- src/fgui/glapparea.cpp~	2012-05-19 22:54:35.000000000 -0300
-+++ src/fgui/glapparea.cpp	2012-05-19 22:54:50.000000000 -0300
-@@ -24,6 +24,8 @@
+--- ./src/fgui/glapparea.cpp.orig	2012-12-02 18:48:47.000000000 -0500
++++ ./src/fgui/glapparea.cpp	2012-12-02 18:58:20.000000000 -0500
+@@ -23,6 +23,7 @@
+ #include <qimage.h>
  #include <qtimer.h>
  #include <math.h>
- 
 +#include <GL/glu.h>
-+
+ 
  
  GLAppArea::GLAppArea( QWidget* parent,QStringList* strlista)
-     : QGLWidget( parent)
