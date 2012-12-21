99a563929c4d91ea4717c85822275a19fdb418b1
Bryan Drewery <bdrewery@FreeBSD.org>
2012-12-21 16:11:05 +0000

    - Fix runtime version still showing 1.0.3
    Approved by:	bapt

---
+++ ports-mgmt/pkg/files/patch-newvers.sh
@@ -0,0 +1,19 @@
+commit 323c0b7c6e55a8e3736f0621e4504aef61828814
+Author: Bryan Drewery <bryan@shatow.net>
+Date:   Fri Dec 21 09:34:31 2012 -0600
+
+    Bump to 1.0.4_1
+
+diff --git newvers.sh newvers.sh
+index 8926a8c..2222329 100755
+--- newvers.sh
++++ newvers.sh
+@@ -41,7 +41,7 @@
+ : ${PKG_MINOR_VERSION="0"}
+ : ${PKG_PATCH_LEVEL="4"}
+  
+-: ${PORTREVISION:=""}
++: ${PORTREVISION:="1"}
+ : ${PORTEPOCH:=}
+ 
+ # Define this to a true value if creating a snapshot
