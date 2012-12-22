2d250bb95eac4419c2b2a81d30d1629c50a4ba4f
Grzegorz Blach <gblach@FreeBSD.org>
2012-12-22 20:54:59 +0000

    - Update Enlightenment to 0.17.0 - Update EFL to 1.7.4 - Convert to OptionsNG
    Approved by:	crees (mentor)

--- Mk/bsd.efl.mk
+++ Mk/bsd.efl.mk
@@ -23,7 +23,7 @@
 # Currently recognized variables are:
 # USE_EFL	- lists all EFL libraries which port depends on
 # WANT_EFL	- the port wants to test which of EFL libraries are installed
-#		  on the target system
+#			on the target system
 #
 # The following variables could be tested after inclusion of bsd.port.pre.mk:
 # HAVE_EFL	- lists all EFL libraries which are available on target system
@@ -36,66 +36,70 @@ EFL_Include_MAINTAINER=	gblach@FreeBSD.org
 #
 # Define all supported libraries
 #
-_USE_EFL_ALL=	ecore edbus edje eet efreet eio eina elementary embryo emotion \
-		epeg evas imlib2
+_USE_EFL_ALL=	ecore edbus edje eet efreet eio eina elementary embryo \
+		emotion ender enesim epeg esvg etch ethumb evas imlib2
 
 # For each library supported we define the following variables:
 #	_%%LIB%%_CATEGORY	- category the port belongs to
-#	_%%LIB%%_DEPENDS	- other EFL libraries the library
-#		itself depends on. We'll define them explicitly
-#		to handle unwanted deinstalls.
 #	_%%LIB%%_PREFIX		- where the library is installed
 #	_%%LIB%%_VERSION	- version of the shared library
 #	_%%LIB%%_SLIB		- name of the shared library
 #
 
 _ecore_CATEGORY=	devel
-_ecore_DEPENDS=		eina
 _ecore_PORTNAME=	ecore-main
-_ecore_VERSION=		2
+_ecore_VERSION=		8
 
 _edbus_CATEGORY=	devel
 _edbus_PORTNAME=	e_dbus
-_edbus_VERSION=		2
+_edbus_VERSION=		8
 
 _eet_CATEGORY=		devel
-_eet_DEPENDS=		eina
-_eet_VERSION=		6
+_eet_VERSION=		8
 
 _efreet_CATEGORY=	x11
-_efreet_DEPENDS=	ecore eina
-_efreet_VERSION=	2
+_efreet_VERSION=	8
 
 _edje_CATEGORY=		graphics
-_edje_DEPENDS=		ecore eet embryo evas
-_edje_VERSION=		2
+_edje_VERSION=		8
 
 _eio_CATEGORY=		devel
-_eio_DEPENDS=		ecore eina
-_eio_VERSION=		1
+_eio_VERSION=		8
 
 _eina_CATEGORY=		devel
-_eina_VERSION=		2
+_eina_VERSION=		8
 
 _elementary_CATEGORY=	x11-toolkits
-_elementary_DEPENDS=	ecore embryo edje
 _elementary_VERSION=	8
 
 _embryo_CATEGORY=	lang
-_embryo_DEPENDS=	eina
-_embryo_VERSION=	2
+_embryo_VERSION=	8
 
 _emotion_CATEGORY=	multimedia
-_emotion_DEPENDS=	ecore edje eet eio embryo evas
-_emotion_VERSION=	2
+_emotion_VERSION=	8
+
+_ender_CATEGORY=	graphics
+_ender_VERSION=		0
+
+_enesim_CATEGORY=	graphics
+_enesim_VERSION=	0
 
 _epeg_CATEGORY=		graphics
 _epeg_VERSION=		9
 
+_esvg_CATEGORY=		www
+_esvg_PORTNAME=		egueb
+_esvg_VERSION=		0
+
+_etch_CATEGORY=		graphics
+_etch_VERSION=		0
+
+_ethumb_CATEGORY=	graphics
+_ethumb_VERSION=	8
+
 _evas_CATEGORY=		graphics
-_evas_DEPENDS=		eina eet
 _evas_PORTNAME=		evas-core
-_evas_VERSION=		2
+_evas_VERSION=		8
 
 _imlib2_CATEGORY=	graphics
 _imlib2_VERSION=	5
@@ -105,9 +109,6 @@ _imlib2_SLIB=		Imlib2
 # Assign values for variables which were not defined explicitly
 #
 .for LIB in ${_USE_EFL_ALL}
-. if !defined(_${LIB}_DEPENDS)
-_${LIB}_DEPENDS=	#empty
-. endif
 . if !defined(_${LIB}_SLIB)
 _${LIB}_SLIB=${LIB}
 . endif
@@ -128,14 +129,15 @@ _${LIB}_PREFIX=${LOCALBASE}
 #
 
 # All components that are currently supported
-_EFL_EVAS_ENGINES_ALL= buffer opengl sdl x11
-_EFL_EVAS_LOADERS_ALL= bmp edb eet gif ico jpeg png pmaps psd svg tga tiff wbmp xpm
+_EFL_EVAS_ENGINES_ALL=	buffer opengl sdl x11
+_EFL_EVAS_LOADERS_ALL=	bmp edb eet generic gif ico jpeg \
+			png pmaps psd svg tga tiff wbmp xpm
 
 #
 # Generic evas engines definitions
 #
-_EFL_EVAS_CATEGORY=	graphics
-_EFL_EVAS_MODDIR=	${LOCALBASE}/lib/evas/modules/
+_EFL_EVAS_CATEGORY=		graphics
+_EFL_EVAS_MODDIR=		${LOCALBASE}/lib/evas/modules/
 _EFL_EVAS_ENGINES_MODDIR=	${_EFL_EVAS_MODDIR}/engines/
 _EFL_EVAS_LOADERS_MODDIR=	${_EFL_EVAS_MODDIR}/loaders/
 
@@ -145,7 +147,7 @@ _EFL_EVAS_LOADERS_MODDIR=	${_EFL_EVAS_MODDIR}/loaders/
 
 _evas_engine_buffer_DIR=	buffer
 _evas_engine_opengl_DIR=	gl_x11
-_evas_engine_sdl_DIR=		software_sdl
+_evas_engine_sdl_DIR=		software_16_sdl
 _evas_engine_x11_DIR=		software_x11
 
 #
@@ -184,8 +186,8 @@ _evas_loader_${COMP}_DIR=	${COMP}
 #
 
 # All components that are currently supported
-_EFL_ECORE_ALL=	con config desktop evas file imf imf_evas input \
-		input_evas ipc job sdl txt x11
+_EFL_ECORE_ALL=	con config desktop evas file imf imf_evas \
+		input input_evas ipc job sdl txt x11
 
 #
 # Generic ecore definitions
@@ -215,10 +217,10 @@ _ecore_x11_NAME=		ecore_x
 #
 .for COMP in ${_EFL_ECORE_ALL}
 . if !defined(_ecore_${COMP}_CATEGORY)
-_ecore_${COMP}_CATEGORY=	${_EFL_ECORE_CATEGORY}
+_ecore_${COMP}_CATEGORY=${_EFL_ECORE_CATEGORY}
 . endif
 . if !defined(_ecore_${COMP}_PORTNAME)
-_ecore_${COMP}_PORTNAME=	ecore-${COMP}
+_ecore_${COMP}_PORTNAME=ecore-${COMP}
 . endif
 . if !defined(_ecore_${COMP}_NAME)
 _ecore_${COMP}_NAME=	ecore_${COMP}
@@ -368,7 +370,7 @@ _USE_EFL=	#empty
 . if ${_USE_EFL_ALL:M${LIB}}==""
 IGNORE=	cannot install: unknown library ${LIB}
 . else
-_USE_EFL+=	${_${LIB}_DEPENDS} ${LIB}
+_USE_EFL+=	${LIB}
 . endif
 .endfor
 
@@ -405,8 +407,8 @@ PLIST_SUB+=	E17_ARCH=freebsd${OSREL}-${ARCH}
 post-patch: efl_drop_librt
 efl_drop_librt:
 	@${REINPLACE_CMD} -E \
-	    -e '/^[[:space:]]+freebsd\*/,/^[[:space:]];;/s/-lrt//g' \
-	    ${WRKSRC}/configure
+		-e '/^[[:space:]]+freebsd\*/,/^[[:space:]];;/s/-lrt//g' \
+		${WRKSRC}/configure
 .endif
 
 .if defined(_USE_EFL_MODARCH_HACK)
@@ -416,9 +418,9 @@ efl_drop_librt:
 post-patch: efl_patch_modarch
 efl_patch_modarch:
 	@${REINPLACE_CMD} -E \
-	    -e 's/(MODULE_ARCH="\$$host_os-\$$host_cpu)-[^"]+"/\1"/g' \
-	    -e 's/(MODULE_EDJE="\$$host_os-\$$host_cpu)-[^"]+"/\1"/g' \
-	    ${WRKSRC}/configure
+		-e 's/(MODULE_ARCH="\$$host_os-\$$host_cpu)-[^"]+"/\1"/g' \
+		-e 's/(MODULE_EDJE="\$$host_os-\$$host_cpu)-[^"]+"/\1"/g' \
+		${WRKSRC}/configure
 .endif
 
 .if defined(_USE_EFL_LIBTOOL_HACK)
@@ -428,7 +430,7 @@ efl_patch_modarch:
 post-configure: efl_drop_la
 efl_drop_la:
 	${REINPLACE_CMD} -E -e \
-	    '/Install the pseudo-library/,/staticlibs=/s,^,#,' ${WRKSRC}/libtool
+		'/Install the pseudo-library/,/staticlibs=/s,^,#,' ${WRKSRC}/libtool
 .endif
 
 .endif #USE_EFL
