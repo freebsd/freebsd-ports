801590780d8ffd1055403ca0c43ae1e8ba6d37ff
Johan van Selst <johans@FreeBSD.org>
2012-12-22 12:45:23 +0000

    Shortly after the initial release, a new tarball was uploaded which includes a small fix that was duplicated already in the FreeBSD port. - Update checksum and remove patch introduced with the original release
    No functional changes.

--- archivers/rpm4/files/patch-system.h
+++ archivers/rpm4/files/patch-system.h
@@ -1,6 +1,6 @@
 --- system.h.orig	2011-06-10 15:27:05.000000000 +0200
 +++ system.h	2011-06-10 15:27:09.000000000 +0200
-@@ -39,6 +39,14 @@ char * stpncpy(char * dest, const char *
+@@ -39,6 +39,13 @@ char * stpncpy(char * dest, const char *
  #define	getenv(_s)	__secure_getenv(_s)
  #endif
  
@@ -10,7 +10,6 @@
 +#include <libgen.h>
 +#include <sys/wait.h>
 +#include <netinet/in.h>
-+#include <errno.h>
 +
  #ifdef HAVE_FCNTL_H
  #include <fcntl.h>
