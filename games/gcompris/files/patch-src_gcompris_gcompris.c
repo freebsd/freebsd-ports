dbfff7a036012fafe62f7320827be364ad6d4b84
rm <rm@FreeBSD.org>
2012-12-26 09:10:11 +0000

    - update to 12.11 - trim Makefile header - remove dependency on libgnet, because it was dropped upstream - info file not generated anymore, so remove INFO knob accordingly - eliminate using of PYTHON_SITELIBDIR in dependencies - adjust local patch - tab -> space change in pkg-descr:WWW
    PR:		173948
    Submitted by:	rm (myself)
    Approved by:	maintainer timeout (1 month)

--- games/gcompris/files/patch-src_gcompris_gcompris.c
+++ games/gcompris/files/patch-src_gcompris_gcompris.c
@@ -1,11 +1,11 @@
---- src/gcompris/gcompris.c.orig	2011-02-22 23:55:08.000000000 +0300
-+++ src/gcompris/gcompris.c	2011-08-12 10:54:59.000000000 +0400
-@@ -1295,7 +1295,7 @@
-       g_error_free (error);
-     }
- 
+--- src/gcompris/gcompris.c.orig	2012-10-27 03:24:09.000000000 +0400
++++ src/gcompris/gcompris.c	2012-11-26 21:38:50.000000000 +0400
+@@ -1332,7 +1332,7 @@
+ /* Display the directory value we have */
+ static void display_property_dirs()
+ {
 -  exec_prefix = gbr_find_exe_dir(NULL);
 +  exec_prefix = gbr_find_exe_dir("%%PREFIX%%");
-   g_warning("exec_prefix %s\n", (exec_prefix==NULL ? "NONE" : exec_prefix));
+   printf("exec_prefix              = %s\n", (exec_prefix==NULL ? "NONE" : exec_prefix));
  
-   {
+   printf("package_data_dir         = %s\n", properties->package_data_dir);
