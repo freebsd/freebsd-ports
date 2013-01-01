a9a34489437b0170c6181cff760420cd869540ab
Guido Falsi <madpilot@FreeBSD.org>
2013-01-01 23:25:37 +0000

    - Modify clang 3.2 fix to be the same as committed to the project's   repository. - Bump PORTREVISION
    Obtained from:	http://trac.wildfiregames.com/changeset/13031#file10

--- games/0ad/files/patch-source-lib-debug.h
+++ games/0ad/files/patch-source-lib-debug.h
@@ -1,11 +1,11 @@
---- source/lib/debug.h.orig	2012-12-27 22:32:56.384331917 +0100
-+++ source/lib/debug.h	2012-12-27 22:33:41.699327215 +0100
+--- source/lib/debug.h.orig	2013-01-01 21:48:34.000000000 +0100
++++ source/lib/debug.h	2013-01-01 21:49:29.000000000 +0100
 @@ -122,7 +122,7 @@
   * debugging the symbol engine.
   * use 0 as the initial value to avoid allocating .rdata space.
   **/
 -const atomic_bool DEBUG_SUPPRESS = 0xAB;
-+extern const atomic_bool DEBUG_SUPPRESS;
++static const atomic_bool DEBUG_SUPPRESS = 0xAB;
  
  /**
   * choices offered by the error dialog that are returned
