e287f2ec7de6b34c93b94bb92468df99a2f090b3
Anton Berezin <tobez@FreeBSD.org>
2013-01-01 14:55:43 +0000

    Update to 0.11016.
    Changes:	http://search.cpan.org/dist/SQL-Translator/Changes

--- databases/p5-SQL-Translator/files/patch-lib::SQL::Translator::Parser::xSV.pm
+++ databases/p5-SQL-Translator/files/patch-lib::SQL::Translator::Parser::xSV.pm
@@ -1,12 +1,12 @@
 --- ./lib/SQL/Translator/Parser/xSV.pm.orig	2009-02-13 13:35:07.000000000 +0100
 +++ ./lib/SQL/Translator/Parser/xSV.pm	2009-03-10 15:19:30.000000000 +0100
 @@ -64,7 +64,7 @@ C<SQL::Translator::Utils::normalize_name
- # -------------------------------------------------------------------
  
  use strict;
--use vars qw($VERSION @EXPORT);
-+use vars qw($VERSION @EXPORT_OK);
- $VERSION = '1.59';
+ use warnings;
+-our @EXPORT;
++our @EXPORT_OK;
+ our $VERSION = '1.59';
  
  use Exporter;
 @@ -73,7 +73,7 @@ use Text::RecordParser;
