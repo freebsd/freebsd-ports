f29e67e8c397593e45edeee86ddfc917764f046e
Pawel Pekala <pawel@FreeBSD.org>
2013-01-04 16:29:34 +0000

    - Update to version 1.4.2 - Install config file by default - this allows to view compressed files,   add usual remove if not edited statements in pkg-plist - Remove leading article from COMMENT - Simplify REINPLACE_CMD - Convert to OptionsNG - Take maintainership

--- sysutils/lookat/files/patch-getline.c
+++ sysutils/lookat/files/patch-getline.c
@@ -1,33 +1,27 @@
---- ./getline.c.orig	2009-03-23 18:42:33.000000000 +0000
-+++ ./getline.c	2009-03-23 19:03:49.000000000 +0000
-@@ -45,7 +45,15 @@
- }
+--- getline.c.orig	2013-01-04 16:25:54.000000000 +0100
++++ getline.c	2013-01-04 16:37:47.000000000 +0100
+@@ -33,7 +33,7 @@
+ #include <stdio.h>
+ #include <sys/types.h>
+ 
+-#if defined __GNU_LIBRARY__ && HAVE_GETDELIM
++#if HAVE_GETDELIM
+ 
+ #ifndef HAVE_GETLINE
+ 
+@@ -50,15 +50,6 @@
  
  #else /* ! have getdelim */
-+
-+#if defined(__FreeBSD__)
-+#include <osreldate.h>
-+#if __FreeBSD_version <= 800066
- int
-+#else
-+ssize_t
-+#endif
-+#endif
- getdelim (lineptr, n, delimiter, stream);
  
+-#if defined(__FreeBSD__)
+-#include <osreldate.h>
+-#if __FreeBSD_version <= 800066
+-int getdelim (lineptr, n, delimiter, stream);
+-#else
+-ssize_t getdelim (lineptr, n, delimiter, stream);
+-#endif
+-#endif
+-
  # include <assert.h>
-@@ -150,7 +158,14 @@
-   return getstr (lineptr, n, stream, '\n', 0);
- }
  
-+#if defined(__FreeBSD__)
-+#include <osreldate.h>
-+#if __FreeBSD_version <= 800066
- int
-+#else
-+ssize_t
-+#endif
-+#endif
- getdelim (lineptr, n, delimiter, stream)
-      char **lineptr;
-      size_t *n;
+ # if HAVE_STDLIB_H
