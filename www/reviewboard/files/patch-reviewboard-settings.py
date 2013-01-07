f9bffd0973cea17f1a198fffd8d3e4b86c4bf398
rm <rm@FreeBSD.org>
2013-01-07 10:33:45 +0000

    - update to 1.7.1 - update dependencies list - limit to python 2.x only
    while here:
    - remove indefinite article from COMMENT
    - tab -> space change in pkg-descr:WWW
    
    PR:		174682
    Submitted by:	Po-Chien Lin <linpc@cs.nctu.edu.tw> (maintainer)

--- www/reviewboard/files/patch-reviewboard-settings.py
+++ www/reviewboard/files/patch-reviewboard-settings.py
@@ -1,12 +1,10 @@
---- reviewboard/settings.py.orig	2012-04-30 12:28:31.000000000 +0800
-+++ reviewboard/settings.py	2012-04-30 12:30:01.000000000 +0800
-@@ -16,8 +16,8 @@
- MANAGERS = ADMINS
- 
- # Local time zone for this installation. All choices can be found here:
--# http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
--TIME_ZONE = 'US/Pacific'
-+# http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
+--- reviewboard/settings.py.orig	2012-12-19 00:50:53.000000000 +0800
++++ reviewboard/settings.py	2012-12-19 00:52:55.000000000 +0800
+@@ -28,7 +28,7 @@
+ # http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
+ # When USE_TZ is enabled, this is used as the default time zone for datetime
+ # objects
+-TIME_ZONE = 'UTC'
 +TIME_ZONE = 'Etc/UTC'
  
  # Language code for this installation. All choices can be found here:
