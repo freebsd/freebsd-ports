db5d2a7cc1643a0088d7159cc0fdeb2bf3deeb1e
Jason E. Hale <jhale@FreeBSD.org>
2013-01-08 22:33:10 +0000

    - Update to 1.18 [1] - Convert KNOBS to new options framework [1] - Drop ABI version numbers in LIB_DEPENDS [1] - Add UPDATING entry to inform users of IPv6 changes to   devel/libvanessa_socket that affect perdition [2]
    PR:		ports/174550 [1], ports/174410 [2]
    Submitted by:	Thomas Vogt <thomas@bsdunix.ch> (maintainer) [1],
    		Joel Ray Holveck <joelh@piquan.org> [2]

---
+++ mail/perdition/files/patch-makegdbm-options.h
@@ -0,0 +1,10 @@
+--- ./makegdbm/options.h.orig	2013-01-08 16:04:27.000000000 -0500
++++ ./makegdbm/options.h	2013-01-08 16:04:51.000000000 -0500
+@@ -39,7 +39,6 @@
+ #endif
+ 
+ extern gdbm_error gdbm_errno;
+-extern char *gdbm_version;
+ 
+ #define COPYRIGHT \
+   "(c) 1999 Horms <horms@verge.net.au>\nReleased under the GNU GPL\n"
