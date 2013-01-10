d10a7026cc0000f1aa2fa9a91b5258a63d95469b
Brooks Davis <brooks@FreeBSD.org>
2013-01-10 18:01:28 +0000

    Update the develoment version of the llvm, clang and dragonegg ports to 3.3.r172076.

--- lang/clang-devel/files/patch-tools_clang_lib_Analysis_PrintfFormatString.cpp
+++ lang/clang-devel/files/patch-tools_clang_lib_Analysis_PrintfFormatString.cpp
@@ -1,26 +1,48 @@
 
-$FreeBSD: /tmp/pcvs/ports/lang/clang-devel/files/patch-tools_clang_lib_Analysis_PrintfFormatString.cpp,v 1.1 2010-10-08 14:00:31 brooks Exp $
+$FreeBSD$
 
 --- tools/clang/lib/Analysis/PrintfFormatString.cpp.orig
 +++ tools/clang/lib/Analysis/PrintfFormatString.cpp
-@@ -192,11 +192,20 @@
+@@ -198,9 +198,10 @@
      case '@': k = ConversionSpecifier::ObjCObjArg; break;
      // Glibc specific.
      case 'm': k = ConversionSpecifier::PrintErrno; break;
-+    // FreeBSD format extensions
-+    case 'b': k = ConversionSpecifier::bArg; break; /* check for int and then char * */
-+    case 'r': k = ConversionSpecifier::xArg; break;
-+    case 'y': k = ConversionSpecifier::iArg; break;
-+    case 'D': k = ConversionSpecifier::DArg; break; /* check for u_char * pointer and a char * string */
+-    // Apple-specific
 +
++    // Apple-specific (and one FreeBSD)
+     case 'D':
+-      if (Target.getTriple().isOSDarwin())
++      if (Target.getTriple().isOSDarwin() || Target.getTriple().isOSFreeBSD())
+         k = ConversionSpecifier::DArg;
+       break;
+     case 'O':
+@@ -211,11 +212,29 @@
+       if (Target.getTriple().isOSDarwin())
+         k = ConversionSpecifier::UArg;
+       break;
++
++    // FreeBSD-specific
++    case 'b':
++      if (Target.getTriple().isOSFreeBSD())
++        k = ConversionSpecifier::bArg;
++      break;
++    case 'r':
++      if (Target.getTriple().isOSFreeBSD())
++        k = ConversionSpecifier::xArg;
++      break;
++    case 'y':
++      if (Target.getTriple().isOSFreeBSD())
++        k = ConversionSpecifier::iArg;
++      break;
    }
    PrintfConversionSpecifier CS(conversionPosition, k);
    FS.setConversionSpecifier(CS);
    if (CS.consumesDataArgument() && !FS.usesPositionalArg())
      FS.setArgIndex(argIndex++);
 +  // FreeBSD extension
-+  if (k == ConversionSpecifier::bArg || k == ConversionSpecifier::DArg)
-+    argIndex++;
++  if (Target.getTriple().isOSFreeBSD() && (k == ConversionSpecifier::bArg ||
++    k == ConversionSpecifier::DArg))
++      argIndex++;
  
    if (k == ConversionSpecifier::InvalidSpecifier) {
      // Assume the conversion takes one argument.
