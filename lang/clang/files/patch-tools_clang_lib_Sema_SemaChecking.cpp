0b2290d74fc3dcb5e57b41908a8dc1ba312d6b6a
brooks <brooks@FreeBSD.org>
2013-01-10 16:34:44 +0000

    Upgrade llvm and clang to the 3.2 release.

--- lang/clang/files/patch-tools_clang_lib_Sema_SemaChecking.cpp
+++ lang/clang/files/patch-tools_clang_lib_Sema_SemaChecking.cpp
@@ -16,8 +16,8 @@ $FreeBSD$
 +    // format specifier.
 +    const Expr *Ex = getDataArg(argIndex);
 +    QualType type = (CS.getKind() == ConversionSpecifier::bArg) ? S.Context.IntTy : S.Context.getPointerType(S.Context.UnsignedCharTy);
-+    //const analyze_printf::ArgTypeResult &ATR = S.Context.IntTy;
-+    const analyze_printf::ArgTypeResult &ATR = type;
++    //const analyze_printf::ArgType &ATR = S.Context.IntTy;
++    const analyze_printf::ArgType &ATR = type;
 +    if (ATR.isValid() && !ATR.matchesType(S.Context, Ex->getType()))
 +      S.Diag(getLocationOfByte(CS.getStart()),
 +             diag::warn_printf_conversion_argument_type_mismatch)
@@ -28,7 +28,7 @@ $FreeBSD$
 +    // Now type check the data expression that matches the
 +    // format specifier.
 +    Ex = getDataArg(argIndex + 1);
-+    const analyze_printf::ArgTypeResult &ATR2 = ArgTypeResult::CStrTy;
++    const analyze_printf::ArgType &ATR2 = ArgType::CStrTy;
 +    if (ATR2.isValid() && !ATR2.matchesType(S.Context, Ex->getType()))
 +      S.Diag(getLocationOfByte(CS.getStart()),
 +             diag::warn_printf_conversion_argument_type_mismatch)
