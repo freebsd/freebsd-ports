94f36f3759ed17cda94ae85947250c33f9bce82c
mva <mva@FreeBSD.org>
2013-01-12 14:18:49 +0000

    - Update to 1.9.1
    PR:		ports/175165
    Submitted by:	Erik Greenwald <erik@bz.bzflag.bz> (maintainer)

---
+++ devel/picprog/files/patch-testport.cc
@@ -0,0 +1,10 @@
+--- testport.cc.orig	2013-01-09 14:43:31.000000000 -0500
++++ testport.cc	2013-01-09 14:43:44.000000000 -0500
+@@ -38,7 +38,6 @@
+ #include <ctime>
+ 
+ #include <sys/ioctl.h>
+-#include <sys/io.h>
+ #include <fcntl.h>
+ #include <sys/time.h>
+ #include <unistd.h>
