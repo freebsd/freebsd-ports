f656b630634e127fae10ad311aec9bb9c68e3101
mva <mva@FreeBSD.org>
2013-01-13 22:14:17 +0000

    - Update to 1.9.7 - Pass maintainership to submitter - Add LICENSE
    PR:		ports/172866
    Submitted by:	nemysis <nemysis@gmx.ch>

---
+++ games/icebreaker/files/patch-penguin.c
@@ -0,0 +1,13 @@
+--- penguin.c.orig	2002-05-31 05:15:46.000000000 +0200
++++ penguin.c	2013-01-13 19:57:45.000000000 +0100
+@@ -40,8 +40,9 @@
+ 
+ Penguin createpenguinxy(int x, int y)
+ {
+-	Penguin p;
++	static Penguin p;
+ 
++	memset(&p, 0, sizeof(p));
+ 	switch (random() % 4)
+ 	{
+ 		case 0:
