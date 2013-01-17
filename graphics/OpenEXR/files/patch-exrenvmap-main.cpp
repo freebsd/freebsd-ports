e9591ed4640bf2cb93eb00d67b40f5505c09b97a
Steve Wills <swills@FreeBSD.org>
2013-01-17 03:25:07 +0000

    - Update to 1.7.1 - Give maintainership to submitter
    PR:		ports/172339
    Submitted by:	nemysis <nemysis@gmx.ch>

--- graphics/OpenEXR/files/patch-exrenvmap-main.cpp
+++ graphics/OpenEXR/files/patch-exrenvmap-main.cpp
@@ -1,10 +1,10 @@
---- exrenvmap/main.cpp~
-+++ exrenvmap/main.cpp
-@@ -43,6 +43,7 @@
- #include <makeLatLongMap.h>
+--- exrenvmap/main.cpp.orig	2012-07-18 01:27:28.000000000 +0200
++++ exrenvmap/main.cpp	2012-10-04 10:44:12.000000000 +0200
+@@ -46,6 +46,7 @@
  #include <ImfEnvmap.h>
+ #include <ImfHeader.h>
  #include <iostream>
 +#include <cstring>
  #include <exception>
- #include <stdlib.h>
- 
+ #include <string>
+ #include <string.h>
