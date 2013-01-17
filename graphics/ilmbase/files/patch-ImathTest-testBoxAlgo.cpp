c68b4fb0ace4351f38c3b498b1b628955a3c3f19
swills <swills@FreeBSD.org>
2013-01-17 03:19:42 +0000

    - Update to 1.0.3 - Give maintainership to submitter
    PR:		ports/172337
    Submitted by:	nemysis <nemysis@gmx.ch>

--- graphics/ilmbase/files/patch-ImathTest-testBoxAlgo.cpp
+++ graphics/ilmbase/files/patch-ImathTest-testBoxAlgo.cpp
@@ -1,11 +1,11 @@
---- ImathTest/testBoxAlgo.cpp.orig	2007-07-13 13:48:45.000000000 +0900
-+++ ImathTest/testBoxAlgo.cpp	2007-10-07 23:26:22.000000000 +0900
-@@ -336,7 +336,7 @@
+--- ImathTest/testBoxAlgo.cpp.orig	2012-07-18 01:27:26.000000000 +0200
++++ ImathTest/testBoxAlgo.cpp	2012-10-04 15:45:00.000000000 +0200
+@@ -356,7 +356,7 @@
  	Box3f ()
      };
  
 -    for (int i = 0; i < sizeof (boxes) / sizeof (boxes[0]); ++i)
 +    for (unsigned int i = 0; i < sizeof (boxes) / sizeof (boxes[0]); ++i)
- 	testRayBoxIntersection (boxes[i]);
+ 	testEntryAndExitPoints (boxes[i]);
  }
  
