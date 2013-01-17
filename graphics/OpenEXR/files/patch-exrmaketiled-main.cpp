26ed9183ea624f690f79593644623044d3bf201e
swills <swills@FreeBSD.org>
2013-01-17 03:25:07 +0000

    - Update to 1.7.1 - Give maintainership to submitter
    PR:		ports/172339
    Submitted by:	nemysis <nemysis@gmx.ch>

--- graphics/OpenEXR/files/patch-exrmaketiled-main.cpp
+++ graphics/OpenEXR/files/patch-exrmaketiled-main.cpp
@@ -1,10 +1,10 @@
---- exrmaketiled/main.cpp~
-+++ exrmaketiled/main.cpp
+--- exrmaketiled/main.cpp.orig	2012-07-18 01:27:28.000000000 +0200
++++ exrmaketiled/main.cpp	2012-10-04 10:45:18.000000000 +0200
 @@ -44,6 +44,7 @@
  
  #include <iostream>
  #include <exception>
 +#include <cstring>
  #include <string>
+ #include <string.h>
  #include <stdlib.h>
- 
