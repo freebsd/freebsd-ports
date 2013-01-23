be9c1d4ee9d8d92f656499462af4370c2727b283
rene <rene@FreeBSD.org>
2013-01-23 11:34:09 +0000

    Remove shared memory keys only if no segments are attached.
    This fixes a bug when kern.ipc.shm_allow_removed was set to 0.
    Submitted by:	George Liaskos

--- www/chromium/files/patch-ui__surface__transport_dib_linux.cc
+++ www/chromium/files/patch-ui__surface__transport_dib_linux.cc
@@ -1,18 +1,21 @@
---- ui/surface/transport_dib_linux.cc.orig	2011-09-14 11:02:00.000000000 +0300
-+++ ui/surface/transport_dib_linux.cc	2011-09-21 13:59:55.000000000 +0300
-@@ -31,6 +31,11 @@
-     address_ = kInvalidAddress;
-   }
- 
-+#if defined(OS_FREEBSD)
-+  if(is_valid_id(key_))
-+    shmctl(key_.shmkey, IPC_RMID, 0);
-+#endif
-+
-   if (x_shm_) {
+--- ui/surface/transport_dib_linux.cc.orig	2013-01-08 04:46:06.000000000 +0200
++++ ui/surface/transport_dib_linux.cc	2013-01-17 23:27:10.221168481 +0200
+@@ -37,6 +37,14 @@
      DCHECK(display_);
      ui::DetachSharedMemory(display_, x_shm_);
-@@ -50,10 +55,13 @@
+   }
++
++#if defined(OS_FREEBSD)
++  if (is_valid_id(key_)) {
++    struct shmid_ds shmst;
++    if (shmctl(key_.shmkey, IPC_STAT, &shmst) == 0 && shmst.shm_nattch == 0)
++      shmctl(key_.shmkey, IPC_RMID, 0);
++  }
++#endif
+ }
+ 
+ // static
+@@ -51,10 +59,13 @@
    }
  
    void* address = shmat(shmkey, NULL /* desired address */, 0 /* flags */);
