a7caa28d3078f25ab189975eb8e31c7aa864ae19
Pietro Cerutti <gahr@FreeBSD.org>
2013-01-30 14:13:20 +0000

    - Update to 3.15.4   See: http://freeimage.sourceforge.net/news.html
    Notified by:	danfe

---
+++ graphics/freeimage/files/patch-Source-OpenEXR-IlmImf_ImfAutoArray.h
@@ -0,0 +1,11 @@
+--- Source/OpenEXR/IlmImf/ImfAutoArray.h.orig	2013-01-30 11:10:28.000000000 +0100
++++ Source/OpenEXR/IlmImf/ImfAutoArray.h	2013-01-30 12:05:10.000000000 +0100
+@@ -46,6 +46,8 @@
+ 
+ #include "OpenEXRConfig.h"
+ 
++#include <string.h>
++
+ namespace Imf {
+ 
+ 
