cc7e777f8138d07de0b29457ff46dbe847c3682e
Dmitry Marakasov <amdmi3@FreeBSD.org>
2013-01-30 19:10:42 +0000

    - Update to 3.1.0

--- games/warzone2100/files/patch-lib-netplay-netsocket.cpp
+++ games/warzone2100/files/patch-lib-netplay-netsocket.cpp
@@ -1,6 +1,6 @@
---- lib/netplay/netplay.c.orig	2010-08-12 01:53:49.000000000 +0400
-+++ lib/netplay/netplay.c	2010-11-09 03:37:53.000000000 +0300
-@@ -1231,9 +1231,6 @@
+--- lib/netplay/netsocket.cpp.orig	2013-01-15 03:54:07.000000000 +0400
++++ lib/netplay/netsocket.cpp	2013-01-18 19:32:35.110932805 +0400
+@@ -1399,9 +1399,6 @@
  	hint.ai_family    = AF_UNSPEC;
  	hint.ai_socktype  = SOCK_STREAM;
  	hint.ai_protocol  = 0;
