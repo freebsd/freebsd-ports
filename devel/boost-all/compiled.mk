3c4982eca430d79988a5ca61480675b2e2af5e05
bapt <bapt@FreeBSD.org>
2013-01-31 13:33:04 +0000

    - update to 1.52.0 - pass maintainership to office@ - detect if compiler is gcc or clang and correctly set the toolset for bjam - enable long-double support - some cleanup
    Approved by:	maintainer timeout (3 weeks with no reply)

--- devel/boost-all/compiled.mk
+++ devel/boost-all/compiled.mk
@@ -1,51 +1,60 @@
-
-USE_GCC=	4.2+
 USE_LDCONFIG=	yes
-BOOST_SHARED_LIB_VER=	4
+BOOST_SHARED_LIB_VER=	5
 PKGMESSAGE=	${WRKDIR}/pkg-message
 MAKE_JOBS_SAFE=	yes
 BJAM=		bjam
-CXXFLAGS+=	-Wno-long-long
 
 PLIST_SUB+=	BOOST_SHARED_LIB_VER=${BOOST_SHARED_LIB_VER}
 
 PKG_MESSAGE_FILE_THREADS=	${PORTSDIR}/devel/boost-all/pkg-message.threads
 PKG_MESSAGE_FILE_PYTHON=	${PORTSDIR}/devel/boost-all/pkg-message.python
 
-BOOST_TOOLS=	gcc
+BJAM_ARGS=	--layout=system \
+		--prefix=${PREFIX}
 
-BJAM_OPTIONS=	--layout=system --disable-long-double
-BJAM_OPTIONS+=	${_MAKE_JOBS}
-.if defined (WITH_VERBOSE_BUILD)
-BJAM_OPTIONS+=	-d2
+.if ${ARCH} == amd64
+BJAM_ARGS+=	cxxflags=-fPIC
 .endif
 
-.if defined (WITHOUT_DEBUG)
-BJAM_OPTIONS+=	variant=release
+NO_CCACHE=	yes
+.if ${CC:T:Mgcc*}
+BOOST_TOOLSET:=	gcc
+.elif ${CC:T:Mclang}
+BOOST_TOOLSET:=	clang
 .else
-BJAM_OPTIONS+=	variant=debug
+_COMPILER_VERSION!=	${CC} --version
+. if ${_COMPILER_VERSION:Mgcc}
+BOOST_TOOLSET:=	gcc
+. elif ${_COMPILER_VERSION:M\(GCC\)}
+BOOST_TOOLSET:=	gcc
+. elif ${_COMPILER_VERSION:Mclang}
+BOOST_TOOLSET:=	clang
+. else
+.error Unable to determine compiler type for ${CC}
+. endif
+. undef _COMPILER_VERSION
 .endif
 
-BJAM_OPTIONS+=	threading=multi
-
-# Unless WITH_OPTIMIZED_CFLAGS is defined, the port uses
-# CXXFLAGS as defined by the user (overridng Boost's 'speed'
-# configuration scheme
-BJAM_OPTIONS+=	optimization=speed
-.if defined (WITH_OPTIMIZED_CFLAGS)
-BJAM_OPTIONS+=	inlining=full
-CXXFLAGS+=	-O3
+BJAM_ARGS+=	--toolset=${BOOST_TOOLSET} \
+		${_MAKE_JOBS}
+.if ${PORT_OPTIONS:MVERBOSE_BUILD}
+BJAM_ARGS+=	-d2
 .endif
 
-customize-boost-build:
-# Do the right thing(tm) for pthread support and respect CXX
-	@${REINPLACE_CMD}\
-		-e 's|%%PTHREAD_CFLAGS%%|${PTHREAD_CFLAGS}|'\
-		-e 's|%%PTHREAD_LIBS%%|${PTHREAD_LIBS}|'\
-		-e 's|%%CXX%%|${CXX}|'\
-		-e 's|%%CXXFLAGS%%|${CXXFLAGS}|'\
-	${WRKSRC}/tools/build/v2/tools/gcc.jam
+.if ${PORT_OPTIONS:MDEBUG}
+BJAM_ARGS+=	debug
+.else
+BJAM_ARGS+=	release
+.endif
 
-	@${REINPLACE_CMD}\
-		-e 's|%%PTHREAD_LIBS%%|${PTHREAD_LIBS:S/-//}|'\
-	${WRKSRC}/tools/build/v2/tools/python.jam
+BJAM_ARGS+=	threading=multi \
+		link=shared,static
+
+BJAM_ARGS+=	optimization=speed
+.if ${PORT_OPTIONS:MOPTIMIZED_CFLAGS}
+BJAM_ARGS+=	inlining=full
+.endif
+
+post-patch:
+	@${REINPLACE_CMD} -e 's|${PORTVERSION}|${BOOST_SHARED_LIB_VER}|g' \
+		${WRKSRC}/Jamroot
