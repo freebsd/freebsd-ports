ad6838744ebb0bfc5c0895e5fcad13e8d74a270a
olivierd <olivierd@FreeBSD.org>
2013-02-08 09:36:08 +0000

    - Update to 0.4.8 - Add new option ZEITGEIST (still experimental) - Update description and WWW entry in pkg-desc file - Improve patches (work with Vala 0.12 and higher) - Remove unneeded patches - Fix user script stock icon
    ChangeLog: http://mail.xfce.org/pipermail/xfce-announce/2013-February/000246.html
    
    Approved by:	rene, miwi (mentors, implicit)

--- www/midori/files/patch-extensions__delayed-load.vala
+++ www/midori/files/patch-extensions__delayed-load.vala
@@ -1,44 +1,26 @@
---- ./extensions/delayed-load.vala.orig	2012-09-19 21:17:43.000000000 +0000
-+++ ./extensions/delayed-load.vala	2012-09-21 16:51:55.000000000 +0000
-@@ -86,7 +86,7 @@
-                     item.ref();
- 
-                     int64 delay = item.get_meta_integer ("delay");
--                    if (delay == -2) {
-+                    if (delay == Midori.Delay.PENDING_UNDELAY) {
-                         view.reload (true);
-                     }
-                 }
-@@ -124,7 +124,7 @@
+--- ./extensions/delayed-load.vala.orig	2013-02-05 23:28:05.000000000 +0000
++++ ./extensions/delayed-load.vala	2013-02-07 06:05:11.000000000 +0000
+@@ -123,7 +123,11 @@
              if (this.timeout == 0)
                  view.reload (true);
              else {
--                unowned TabShaker shaker = tasks.get (browser);
++#if !HAVE_GLIB_2_30
 +                unowned TabShaker shaker = tasks.lookup (browser);
++#else
+                 unowned TabShaker shaker = tasks.get (browser);
++#endif
                  if (shaker != null) {
                      shaker.tasks.add (view);
-                     Timeout.add (this.timeout, shaker.reload_tab);
-@@ -141,7 +141,7 @@
-                 item.ref();
- 
-                 int64 delay = item.get_meta_integer ("delay");
--                if (delay == -2 && new_view.progress < 1.0) {
-+                if (delay == Midori.Delay.PENDING_UNDELAY && new_view.progress < 1.0) {
-                     this.schedule_reload (browser, new_view);
-                 }
-             }
-@@ -157,12 +157,12 @@
-                 item.ref();
- 
-                 int64 delay = item.get_meta_integer ("delay");
--                if (delay != 1) {
-+                if (delay != Midori.Delay.DELAYED) {
-                     unowned WebKit.WebView web_view = view.get_web_view ();
-                     WebKit.LoadStatus load_status = web_view.load_status;
+                     Midori.Timeout.add (this.timeout, shaker.reload_tab);
+@@ -160,7 +164,11 @@
+                     WebKit.LoadStatus load_status = view.web_view.load_status;
                      if (load_status == WebKit.LoadStatus.FINISHED) {
                          if (this.timeout != 0)
--                            this.tasks.set (browser, new TabShaker (browser));
++#if !HAVE_GLIB_2_30
 +                            this.tasks.replace (browser, new TabShaker (browser));
++#else
+                             this.tasks.set (browser, new TabShaker (browser));
++#endif
  
                          if (view.progress < 1.0)
                              this.schedule_reload (browser, view);
--- www/midori/files/patch-midori__midori-view.c
+++ www/midori/files/patch-midori__midori-view.c
@@ -1,62 +1,16 @@
---- ./midori/midori-view.c.orig	2012-09-19 21:17:43.000000000 +0000
-+++ ./midori/midori-view.c	2012-09-21 14:22:48.000000000 +0000
-@@ -1025,6 +1025,9 @@
-     #endif
-     view->special = FALSE;
- 
-+    if (katze_item_get_meta_integer (view->item, "delay") == MIDORI_DELAY_PENDING_UNDELAY)
-+        katze_item_set_meta_integer (view->item, "delay", MIDORI_DELAY_UNDELAYED);
-+
-     /* Remove link labels */
-     js_context = webkit_web_frame_get_global_context (web_frame);
-     result = sokoke_js_script_eval (js_context,
-@@ -4205,7 +4208,7 @@
-             katze_assign (view->uri, NULL);
-             katze_assign (view->mime_type, NULL);
-             katze_item_set_meta_string (view->item, "mime-type", NULL);
--            katze_item_set_meta_integer (view->item, "delay", -1);
-+            katze_item_set_meta_integer (view->item, "delay", MIDORI_DELAY_UNDELAYED);
- 
-             html = midori_speed_dial_get_html (dial, NULL);
-             midori_view_load_alternate_string (view, html, "about:blank", NULL);
-@@ -4365,10 +4368,12 @@
+--- ./midori/midori-view.c.orig	2013-02-05 23:28:05.000000000 +0000
++++ ./midori/midori-view.c	2013-02-06 22:29:13.000000000 +0000
+@@ -3981,10 +3981,12 @@
                  gchar const* version_strings[] = {
                      LIBSOUP_VERSION,
                      CAIRO_VERSION_STRING, cairo_version_string (),
-+                    #ifdef HAVE_GCR
++                    #if HAVE_GCR
                      GCR_VERSION,
 +                    #elif HAVE_GRANITE
                      GRANITE_VERSION,
                      LIBNOTIFY_VERSION,
--                    #ifdef HAVE_HILDON_2_2
-+                    #elif HAVE_HILDON_2_2
-                     "Hildon 2.2",
-                     #elif HAVE_HILDON
-                     "Hildon",
-@@ -4437,15 +4442,15 @@
-             webkit_web_view_load_html_string (
-                 WEBKIT_WEB_VIEW (view->web_view), data, view->uri);
-             g_free (data);
--            katze_item_set_meta_integer (view->item, "delay", -1);
-+            katze_item_set_meta_integer (view->item, "delay", MIDORI_DELAY_UNDELAYED);
-             if (g_strcmp0 (view->item->uri, view->uri))
-                 katze_item_set_uri (view->item, view->uri);
-             g_object_notify (G_OBJECT (view), "uri");
-         }
--        else if (katze_item_get_meta_boolean (view->item, "delay"))
-+        else if (katze_item_get_meta_integer (view->item, "delay") == MIDORI_DELAY_DELAYED)
-         {
-             katze_assign (view->uri, g_strdup (uri));
--            katze_item_set_meta_integer (view->item, "delay", -2);
-+            katze_item_set_meta_integer (view->item, "delay", MIDORI_DELAY_PENDING_UNDELAY);
-             midori_view_display_error (
-                 view, view->uri, view->title ? view->title : view->uri,
-                 _("Page loading delayed"),
-@@ -5479,7 +5484,6 @@
-         webkit_web_view_reload (WEBKIT_WEB_VIEW (view->web_view));
-     else
-         webkit_web_view_reload_bypass_cache (WEBKIT_WEB_VIEW (view->web_view));
--    katze_item_set_meta_integer (view->item, "delay", -1);
- }
- 
- /**
+-                    #if HAVE_UNIQUE
++                    #elif HAVE_UNIQUE
+                     "libunique " UNIQUE_VERSION,
+                     #else
+                     "Sockets",
