265d1f23ca22b4076623b6635c8a7cede2913e3a
Max Brazhnikov <makc@FreeBSD.org>
2013-02-15 16:19:50 +0000

    Remove -std=iso9899:1990 from CFLAGS, base gcc's not happy with it
    Reported by:	ale
    Reviewed by:	rakuco

--- x11/kdelibs4/files/patch-cmake_modules_FindKDE4Internal.cmake
+++ x11/kdelibs4/files/patch-cmake_modules_FindKDE4Internal.cmake
@@ -1,6 +1,6 @@
---- cmake/modules/FindKDE4Internal.cmake.orig	2010-06-02 02:55:14.000000000 -0400
-+++ cmake/modules/FindKDE4Internal.cmake	2010-06-02 02:57:53.000000000 -0400
-@@ -855,8 +855,8 @@
+--- ./cmake/modules/FindKDE4Internal.cmake.orig	2012-11-30 07:17:39.000000000 +0000
++++ ./cmake/modules/FindKDE4Internal.cmake	2013-02-15 15:45:41.732493091 +0000
+@@ -893,8 +893,8 @@
     _set_fancy(XDG_MIME_INSTALL_DIR     "${SHARE_INSTALL_PREFIX}/mime/packages"  "The install dir for the xdg mimetypes")
  
     _set_fancy(SYSCONF_INSTALL_DIR      "${CMAKE_INSTALL_PREFIX}/etc"            "The sysconfig install dir (default ${CMAKE_INSTALL_PREFIX}/etc)")
@@ -11,3 +11,12 @@
     _set_fancy(DBUS_INTERFACES_INSTALL_DIR      "${SHARE_INSTALL_PREFIX}/dbus-1/interfaces" "The dbus interfaces install dir (default  ${SHARE_INSTALL_PREFIX}/dbus-1/interfaces)")
     _set_fancy(DBUS_SERVICES_INSTALL_DIR      "${SHARE_INSTALL_PREFIX}/dbus-1/services"     "The dbus services install dir (default  ${SHARE_INSTALL_PREFIX}/dbus-1/services)")
     _set_fancy(DBUS_SYSTEM_SERVICES_INSTALL_DIR      "${SHARE_INSTALL_PREFIX}/dbus-1/system-services"     "The dbus system services install dir (default  ${SHARE_INSTALL_PREFIX}/dbus-1/system-services)")
+@@ -1204,7 +1204,7 @@
+    set(CMAKE_C_FLAGS_DEBUGFULL        "-g3 -fno-inline")
+    set(CMAKE_C_FLAGS_PROFILE          "-g3 -fno-inline -ftest-coverage -fprofile-arcs")
+ 
+-   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -std=iso9899:1990 -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
++   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
+    # As of Qt 4.6.x we need to override the new exception macros if we want compile with -fno-exceptions
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wnon-virtual-dtor -Wno-long-long -Wundef -Wcast-align -Wchar-subscripts -Wall -W -Wpointer-arith -Wformat-security -fno-exceptions -DQT_NO_EXCEPTIONS -fno-check-new -fno-common")
+ 
