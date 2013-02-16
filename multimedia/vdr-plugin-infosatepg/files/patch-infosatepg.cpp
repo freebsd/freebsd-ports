03eb5721759c4803c7bbcd86e8b2826f44320737
Juergen Lock <nox@FreeBSD.org>
2013-02-16 15:04:16 +0000

    - Add bugfix for infosatepg grabbing a bonded device. - Convert Makefile header.

--- multimedia/vdr-plugin-infosatepg/files/patch-infosatepg.cpp
+++ multimedia/vdr-plugin-infosatepg/files/patch-infosatepg.cpp
@@ -16,7 +16,16 @@
              if (dev->IsTunedToTransponder(chan))
              {
                  // we already have a device which is tuned (maybe switched manually?)
-@@ -403,7 +407,7 @@ cString cPluginInfosatepg::SVDRPCommand(
+@@ -202,6 +206,8 @@ void cPluginInfosatepg::MainThreadHook(v
+                 return;
+             }
+ 
++            if (!dev->MaySwitchTransponder(chan)) continue; // bonded?
++
+             // ok -> use this device
+             dsyslog("infosatepg: found free device %i",dev->DeviceNumber()+1);
+             dev->SwitchChannel(chan,false);
+@@ -403,7 +409,7 @@ cString cPluginInfosatepg::SVDRPCommand(
          cString head2;
          head2="\n" \
                "      |        | missed  |            |            | unlocated\n" \
