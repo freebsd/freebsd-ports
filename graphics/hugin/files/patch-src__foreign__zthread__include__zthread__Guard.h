46c49bae4d08c0797688c035f005eb8338e6a346
Vasil Dimov <vd@FreeBSD.org>
2013-02-17 17:12:59 +0000

    Upgrade graphics/hugin from 2011.4.0 to 2012.0.0
    Submitted by:	grog@

--- graphics/hugin/files/patch-src__foreign__zthread__include__zthread__Guard.h
+++ graphics/hugin/files/patch-src__foreign__zthread__include__zthread__Guard.h
@@ -2,29 +2,10 @@
 +++ src/foreign/zthread/include/zthread/Guard.h    2013-01-18 19:08:14.000000000 +0100
 @@ -108,7 +108,7 @@
    }
- 
+
    template <class LockType>
 -  static void createScope(LockHolder<LockType>& l, unsigned long ms) {
 +  static bool createScope(LockHolder<LockType>& l, unsigned long ms) {
- 
+
      if(Scope1::createScope(l, ms))
        if(!Scope2::createScope(l, ms)) {
-@@ -428,8 +428,8 @@
-   template <class U, class V>
-   Guard(Guard<U, V>& g) : LockHolder<LockType>(g) {
- 
--    LockingPolicy::shareScope(*this, extract(g));
--    
-+    LockingPolicy::shareScope(*this, this->extract(g));
-+
-   }
- 
-   /**
-@@ -458,7 +458,7 @@
-   template <class U, class V>
-   Guard(Guard<U, V>& g, LockType& lock) : LockHolder<LockType>(lock) {
- 
--    LockingPolicy::transferScope(*this, extract(g));
-+    LockingPolicy::transferScope(*this, this->extract(g));
- 
-   }
