4b78c17734679b2ac4c10fbe873f642a1e6d7078
miwi <miwi@FreeBSD.org>
2013-02-18 01:01:11 +0000

    - Update to 0.8.0 - Optionng - Trim header
    PR:		174591
    Submitted by:	maintainer

--- security/sguil-server/files/patch-lib-SguildLoaderd.tcl
+++ security/sguil-server/files/patch-lib-SguildLoaderd.tcl
@@ -1,15 +1,15 @@
---- lib/SguildLoaderd.tcl.orig	2007-09-25 14:17:13.000000000 +0000
-+++ lib/SguildLoaderd.tcl	2011-08-11 20:22:18.000000000 +0000
-@@ -129,7 +129,7 @@
+--- lib/SguildLoaderd.tcl.orig	2012-10-12 21:07:19.000000000 +0000
++++ lib/SguildLoaderd.tcl	2012-10-12 21:15:06.000000000 +0000
+@@ -124,7 +124,7 @@
          INDEX dst_port (dst_port),                         \
          INDEX src_port (src_port),                         \
          INDEX start_time (start_time)                      \
 -        )                                                  \
-+        ) ENGINE=MyISAM                                    \
++        )ENGINE=MyISAM                                     \
          "
-
+ 
      # Create the table
-@@ -182,7 +182,7 @@
+@@ -177,7 +177,7 @@
          INDEX dst_port (dst_port),                         \
          INDEX src_port (src_port),                         \
          INDEX start_time (start_time)                      \
@@ -18,12 +18,12 @@
          "
      # Create our MERGE sancp table
      mysqlexec $dbSocketID $createQuery
-@@ -225,7 +225,7 @@
+@@ -220,7 +220,7 @@
      } else {
          # Make sure its a MERGE table and not the old monster
          set tableStatus [mysqlsel $LOADERD_DB_ID {SHOW TABLE STATUS LIKE 'sancp'} -flatlist]
--        if { [lindex $tableStatus 1] != "MRG_MyISAM" } {
-+        if { [lindex $tableStatus 1] != "MRG_MYISAM" } {
+-        if { $tableStatus != "" && ![ string equal -nocase [lindex $tableStatus 1] "MRG_MyISAM" ] } {
++        if { $tableStatus != "" && ![ string equal -nocase [lindex $tableStatus 1] "MRG_MYISAM" ] } {
+ 
              ErrorMessage "ERROR: loaderd: You appear to be using an old version of the\n\
                            sguil database schema that does not support the MERGE sancp\n\
-                           table. Please see the CHANGES document for more information\n."
