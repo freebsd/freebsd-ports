ae6b9a8f6da49f4351a36b761a4dc023f6014f9f
Timur I. Bakeyev <timur@FreeBSD.org>
2013-02-18 14:24:56 +0000

    New 4.0.3 revision of the net/samba4 port. Now we have our own AD!

---
+++ net/samba4/files/patch-source3__smbd__quotas.c
@@ -0,0 +1,19 @@
+--- ./source3/smbd/quotas.c.orig	2010-04-01 15:26:22.000000000 +0200
++++ ./source3/smbd/quotas.c	2010-04-23 01:08:35.000000000 +0200
+@@ -1235,6 +1235,7 @@
+ 	if (!cutstr)
+ 		return False;
+ 
++	memset(&D, '\0', sizeof(D));
+ 	memset(cutstr, '\0', len+1);
+ 	host = strncat(cutstr,mnttype, sizeof(char) * len );
+ 	DEBUG(5,("nfs_quotas: looking for mount on \"%s\"\n", cutstr));
+@@ -1243,7 +1244,7 @@
+ 	args.gqa_pathp = testpath+1;
+ 	args.gqa_uid = uid;
+ 
+-	DEBUG(5,("nfs_quotas: Asking for host \"%s\" rpcprog \"%i\" rpcvers \"%i\" network \"%s\"\n", host, RQUOTAPROG, RQUOTAVERS, "udp"));
++	DEBUG(5,("nfs_quotas: Asking for host \"%s\" rpcprog \"%lu\" rpcvers \"%lu\" network \"%s\"\n", host, RQUOTAPROG, RQUOTAVERS, "udp"));
+ 
+ 	if ((clnt = clnt_create(host, RQUOTAPROG, RQUOTAVERS, "udp")) == NULL) {
+ 		ret = False;
