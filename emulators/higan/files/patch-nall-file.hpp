3b5c805ef469145ed99afeae751890efa43c8e5c
Max Brazhnikov <makc@FreeBSD.org>
2013-02-22 13:57:56 +0000

    - Rename emulators/bsnes to emulators/higan due to upstream name change - Update to 0.92
    PR:		ports/176333
    Submitted by:	Naram Qashat (maintainer)

--- emulators/higan/files/patch-nall-file.hpp
+++ emulators/higan/files/patch-nall-file.hpp
@@ -1,6 +1,6 @@
---- bsnes/nall/file.hpp.orig	2012-03-06 14:03:53.000000000 -0500
-+++ bsnes/nall/file.hpp	2012-04-16 23:42:33.000000000 -0400
-@@ -160,8 +160,8 @@
+--- higan/nall/file.hpp.orig	2013-01-14 13:27:28.000000000 -0500
++++ higan/nall/file.hpp	2013-02-21 14:05:00.000000000 -0500
+@@ -218,8 +218,8 @@
  
      static bool exists(const string &filename) {
        #if !defined(_WIN32)
@@ -11,7 +11,7 @@
        #else
        struct __stat64 data;
        return _wstat64(utf16_t(filename), &data) == 0;
-@@ -170,8 +170,8 @@
+@@ -228,8 +228,8 @@
  
      static uintmax_t size(const string &filename) {
        #if !defined(_WIN32)
@@ -22,7 +22,7 @@
        #else
        struct __stat64 data;
        _wstat64(utf16_t(filename), &data);
-@@ -181,8 +181,8 @@
+@@ -239,8 +239,8 @@
  
      static time_t timestamp(const string &filename, file::time mode = file::time::create) {
        #if !defined(_WIN32)
@@ -33,9 +33,9 @@
        #else
        struct __stat64 data;
        _wstat64(utf16_t(filename), &data);
---- purify/nall/file.hpp.orig	2012-04-23 02:11:07.000000000 -0400
-+++ purify/nall/file.hpp	2012-05-06 22:54:42.000000000 -0400
-@@ -160,8 +160,8 @@
+--- ananke/nall/file.hpp.orig	2013-01-11 13:26:29.000000000 -0500
++++ ananke/nall/file.hpp	2013-02-21 14:06:08.000000000 -0500
+@@ -218,8 +218,8 @@
  
      static bool exists(const string &filename) {
        #if !defined(_WIN32)
@@ -46,7 +46,7 @@
        #else
        struct __stat64 data;
        return _wstat64(utf16_t(filename), &data) == 0;
-@@ -170,8 +170,8 @@
+@@ -228,8 +228,8 @@
  
      static uintmax_t size(const string &filename) {
        #if !defined(_WIN32)
@@ -57,7 +57,7 @@
        #else
        struct __stat64 data;
        _wstat64(utf16_t(filename), &data);
-@@ -181,8 +181,8 @@
+@@ -239,8 +239,8 @@
  
      static time_t timestamp(const string &filename, file::time mode = file::time::create) {
        #if !defined(_WIN32)
