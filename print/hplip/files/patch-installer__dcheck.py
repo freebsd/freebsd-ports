ba98eecf70932b9cf9833c6ff808a66e5da64650
Max Brazhnikov <makc@FreeBSD.org>
2013-02-22 13:36:10 +0000

    Update to 3.13.2
    PR:		ports/176341
    Submitted by:	delphij

--- print/hplip/files/patch-installer__dcheck.py
+++ print/hplip/files/patch-installer__dcheck.py
@@ -1,6 +1,6 @@
---- ./installer/dcheck.py.orig	2012-11-20 09:51:37.000000000 +0000
-+++ ./installer/dcheck.py	2013-01-22 14:25:58.685224662 +0000
-@@ -44,7 +44,11 @@
+--- installer/dcheck.py.orig	2013-02-21 18:40:18.368768559 -0800
++++ installer/dcheck.py	2013-02-21 18:43:03.116783408 -0800
+@@ -46,7 +46,11 @@
  def update_ld_output():
      # For library checks
      global ld_output
@@ -13,12 +13,12 @@
  
      if status != 0:
          log.debug("ldconfig failed.")
-@@ -373,6 +377,8 @@
+@@ -360,6 +364,8 @@
      except ImportError:
          return '-'
      else:
 +        if sys.platform.startswith('freebsd'):
 +            return '-'
-         LIBC = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
+ #        LIBC = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
+         LIBC = ctypes.CDLL(ctypes.util.find_library('c'),ctypes.DEFAULT_MODE,None, True)
          LIBC.gnu_get_libc_version.restype = ctypes.c_char_p
-         return LIBC.gnu_get_libc_version()
