c417b6f10d59733f3984b0ab4bb24f1850fb663a
Po-Chien Lin <pclin@FreeBSD.org>
2013-02-23 07:21:57 +0000

    - Update to 1.2 - Remove unused USE_LDCONFIG knob
    Approved by:	swills (mentor)

--- chinese/pcmanx/files/patch-src-view-telnetcon.cpp
+++ chinese/pcmanx/files/patch-src-view-telnetcon.cpp
@@ -1,6 +1,6 @@
 --- src/view/telnetcon.cpp.orig	2010-10-12 21:12:03.000000000 +0800
 +++ src/view/telnetcon.cpp	2010-10-12 21:12:09.000000000 +0800
-@@ -72,7 +72,6 @@
+@@ -77,7 +77,6 @@
  #ifdef USING_LINUX
  #include <pty.h>
  #endif
