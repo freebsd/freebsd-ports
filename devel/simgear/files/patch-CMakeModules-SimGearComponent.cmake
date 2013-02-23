64bdb0e98944859a66aa32a877157c83558777b3
martymac <martymac@FreeBSD.org>
2013-02-23 13:47:49 +0000

    Fix build when upgrading from a previous version
    Reported by:	Mikhail Tsatsenko <m.tsatsenko@gmail.com>

--- devel/simgear/files/patch-CMakeModules-SimGearComponent.cmake
+++
@@ -1,7 +0,0 @@
---- CMakeModules/SimGearComponent.cmake.orig	2013-02-21 05:58:12.000000000 +0100
-+++ CMakeModules/SimGearComponent.cmake	2013-02-21 05:58:58.000000000 +0100
-@@ -1,3 +1,4 @@
-+include_directories(BEFORE ${PROJECT_SOURCE_DIR})
- 
- macro(simgear_component_common name includePath sourcesList sources headers)
-     set(fc${sourcesList} ${name})
