3e152aaa1690c874d1590ed0ce642db5314e73a4
miwi <miwi@FreeBSD.org>
2013-03-02 06:38:39 +0000

    - Update to 0.73
    PR:		176541
    Submitted by:	ports fury

--- devel/kaptain/files/patch-kaptain-tools.cpp
+++
@@ -1,9 +0,0 @@
---- kaptain/tools.cpp.orig	Wed Mar 12 01:38:12 2003
-+++ kaptain/tools.cpp	Wed Sep  3 18:43:40 2003
-@@ -1,5 +1,6 @@
- #include <stdlib.h>
- #include <stdio.h>
-+#include <sys/types.h>
- #include <regex.h>
- #include <iostream>
- #include <iterator>
