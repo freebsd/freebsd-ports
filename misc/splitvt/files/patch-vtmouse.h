7a68967d8aa03077fac3a6b190c330163d05827b
rene <rene@FreeBSD.org>
2013-03-06 15:05:43 +0000

    Remove expired, unmaintained ports: 2013-03-01 sysutils/sge60: Ancient and unsupported release 2013-03-01 sysutils/sge61: Ancient and unsupported release 2013-03-05 x11-themes/sapphire-themes: Broken for more than 6 month 2013-03-05 misc/fep: Broken for more than 6 month 2013-03-05 devel/gauche-gaunit: Broken for more than 6 month 2013-03-05 games/tuxracer_golf: Broken for more than 6 month 2013-03-05 net/bfilter: Broken for more than 6 month 2013-03-05 graphics/fnlib: Broken for more than 6 month 2013-03-05 print/gfontview: Broken for more than 6 month 2013-03-05 print/hugelatex: Broken for more than 6 month 2013-03-05 misc/gtktalog: Broken for more than 6 month 2013-03-05 x11/wterm: Broken for more than 6 month 2013-03-05 databases/xapian-bindings10: Broken for more than 6 month 2013-03-05 databases/adstudio: Broken for more than 6 month 2013-03-05 misc/splitvt: Broken for more than 6 month 2013-03-05 sysutils/udesc_dump: Broken for more than 6 month 2013-03-05 textproc/gxditview: Broken for more than 6 month 2013-03-05 x11/powershell: Broken for more then 6 month

--- misc/splitvt/files/patch-vtmouse.h
+++
@@ -1,9 +0,0 @@
---- vtmouse.h.orig	Sat Oct  7 02:22:11 2006
-+++ vtmouse.h	Sat Oct  7 02:22:36 2006
-@@ -39,5 +39,5 @@
- extern int  event_getc();
- extern void event_quit();
- 
--extern FILE *xt_input, *xt_output;	/* Usually untouched */
-+static FILE *xt_input, *xt_output;	/* Usually untouched */
- 
