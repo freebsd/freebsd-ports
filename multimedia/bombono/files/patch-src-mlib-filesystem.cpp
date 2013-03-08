32b35224b94cc3e8dbefc480ce0db2b1dfbe129a
pawel <pawel@FreeBSD.org>
2013-03-08 19:57:07 +0000

    - Update to version 1.2.2 - Tidy up dependencies - Remove not needed boost patches - Sort plist

--- multimedia/bombono/files/patch-src-mlib-filesystem.cpp
+++ multimedia/bombono/files/patch-src-mlib-filesystem.cpp
@@ -1,6 +1,6 @@
---- src/mlib/filesystem.cpp.orig	2012-06-05 18:44:31.000000000 +0200
-+++ src/mlib/filesystem.cpp	2012-06-05 18:46:42.000000000 +0200
-@@ -47,7 +47,7 @@
+--- ./src/mlib/filesystem.cpp.orig	2013-03-07 22:20:45.000000000 +0100
++++ src/mlib/filesystem.cpp	2013-03-08 16:42:27.000000000 +0100
+@@ -105,7 +105,7 @@
  
  std::string get_basename(const fs::path& pth)
  {
@@ -9,7 +9,7 @@
      const char* name = name_s.c_str();
  
      //if( const char* dot = strstr(name, ".") )
-@@ -58,7 +58,7 @@
+@@ -116,7 +116,7 @@
  
  std::string get_extension(const fs::path& pth)
  {
@@ -18,11 +18,3 @@
  
      if( const char* dot = FindExtDot(name_s.c_str()) )
          return std::string(dot+1);
-@@ -72,7 +72,6 @@
-     tune_boost_filesystem()
-     {
-         // чтоб любые символы в именах файлов позволялись, для utf8
--        fs::path::default_name_check(fs::native);
-     }
- } tune_boost_filesystem_obj;
- 
