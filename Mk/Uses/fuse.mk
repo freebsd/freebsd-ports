051cbebcc8048ad3e56ad943504c537077e0d5e3
Baptiste Daroussin <bapt@FreeBSD.org>
2013-03-08 15:04:18 +0000

    - Add a short usage description inside each Uses/*.mk - Simplify logic for Uses/bison.mk and make error message more informative - Add error handler for Uses/fuse.mk and Uses/pathfix.mk
    Submitted by:	ak@, az@

--- Mk/Uses/fuse.mk
+++ Mk/Uses/fuse.mk
@@ -4,8 +4,17 @@
 #
 # MAINTAINER: portmgr@FreeBSD.org
 #
+# Feature:	fuse
+# Usage:	USES=fuse
+# Valid ARGS	does not require args
+#
+#
 .if !defined(_INCLUDE_FUSE_MK)
 _INCLUDE_FUSE_MK=	yes
+
+.if defined(fuse_ARGS)
+IGNORE=	USES=fuse does not require args
+.endif
 
 LIB_DEPENDS+=	fuse:${PORTSDIR}/sysutils/fusefs-libs
 .if !exists(/sbin/mount_fusefs)
