aaa4ce99f65687a6220b0f4ee7fb0744762eeaf8
bapt <bapt@FreeBSD.org>
2013-03-08 15:04:18 +0000

    - Add a short usage description inside each Uses/*.mk - Simplify logic for Uses/bison.mk and make error message more informative - Add error handler for Uses/fuse.mk and Uses/pathfix.mk
    Submitted by:	ak@, az@

--- Mk/Uses/bison.mk
+++ Mk/Uses/bison.mk
@@ -4,24 +4,29 @@
 #
 # MAINTAINER: portmgr@FreeBSD.org
 #
+# Feature:	bison
+# Usage:	USES=bison or USES=bison:ARGS
+# Valid ARGS	build (default, implicit), run, both
+#
+#
 .if !defined(_INCLUDE_BISON_MK)
 _INCLUDE_BISON_MK=	yes
 
 _BISON_DEPENDS=	bison:${PORTSDIR}/devel/bison
 
 .if !defined(bison_ARGS)
-BUILD_DEPENDS+=	${_BISON_DEPENDS}
-.else
+bison_ARGS=build
+.endif
+
 .if ${bison_ARGS} == "build"
-BUILD_DEPENDS+=	${_BUILD_DEPENDS}
+BUILD_DEPENDS+=	${_BISON_DEPENDS}
 .elif ${bison_ARGS} == "run"
-RUN_DEPENDS+=	${_BUILD_DEPENDS}
+RUN_DEPENDS+=	${_BISON_DEPENDS}
 .elif ${bison_ARGS} == "both"
 BUILD_DEPENDS+=	${_BISON_DEPENDS}
-RUN_DEPENDS+=	${_BUILD_DEPENDS}
+RUN_DEPENDS+=	${_BISON_DEPENDS}
 .else
-IGNORE=	Bad usage of USES= bison
-.endif
+IGNORE=	USES=bison - invalid args: [${bison_ARGS}] specifed
 .endif
 
 .endif
--- Mk/Uses/pathfix.mk
+++ Mk/Uses/pathfix.mk
@@ -5,8 +5,17 @@
 #
 # MAINTAINER: portmgr@FreeBSD.org
 #
+# Feature:	pathfix
+# Usage:	USES=pathfix
+# Valid ARGS	does not require args
+#
+#
 .if !defined(_INCLUDE_PATHFIX_MK)
 _INCLUDE_PATHFIX_MK=	yes
+
+.if defined(pathfix_ARGS)
+IGNORE=	USES=pathfix does not require args
+.endif
 
 PATHFIX_MAKEFILEIN?=	Makefile.in
 
