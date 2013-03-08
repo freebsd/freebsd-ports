f8285fcd88ab53623aba5de56740b05827cfac9f
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2013-03-08 19:34:05 +0000

    - update to 12.0 [1] - fix build with ffmpeg - add --headless option to run xbmc w/o gui [2]
    PR:		175933 [1]
    PR: 174996 [2]
    Submitted by:	Leo Schmitt <neomorphs@gmail.com> [1] (original patch)
                    Mickael Maillot <mickael.maillot@gmail.com> (maintainer, final patch)
                    Alexander Yerenkow <yerenkow@gmail.com> [2]

--- multimedia/xbmc/files/patch-xbmc__threads__platform__pthreads__ThreadImpl.cpp
+++ multimedia/xbmc/files/patch-xbmc__threads__platform__pthreads__ThreadImpl.cpp
@@ -1,18 +1,15 @@
---- xbmc/threads/platform/pthreads/ThreadImpl.cpp.orig	2012-09-21 15:52:13.000000000 +0200
-+++ xbmc/threads/platform/pthreads/ThreadImpl.cpp	2012-09-21 15:55:45.000000000 +0200
-@@ -59,12 +59,12 @@
+--- xbmc/threads/platform/pthreads/ThreadImpl.cpp.orig	2013-03-07 15:55:46.000000000 +0100
++++ xbmc/threads/platform/pthreads/ThreadImpl.cpp	2013-03-07 15:55:58.000000000 +0100
+@@ -58,11 +58,7 @@
  void CThread::SetThreadInfo()
  {
  #ifdef __FreeBSD__
 -#if __FreeBSD_version < 900031
-+#if __FreeBSD_version > 900030
-+  m_ThreadOpaque.LwpId = pthread_getthreadid_np();
-+#elif __FreeBSD_version > 800000
-   long lwpid;
-   thr_self(&lwpid);
-   m_ThreadOpaque.LwpId = lwpid;
+-  long lwpid;
+-  thr_self(&lwpid);
+-  m_ThreadOpaque.LwpId = lwpid;
 -#else
--  m_ThreadOpaque.LwpId = pthread_getthreadid_np();
++#if __FreeBSD_version > 900030
+   m_ThreadOpaque.LwpId = pthread_getthreadid_np();
  #endif
  #elif defined(TARGET_ANDROID)
-   m_ThreadOpaque.LwpId = gettid();
