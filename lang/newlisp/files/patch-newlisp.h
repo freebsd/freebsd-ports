e45341193944239158557611c72a9b6a609196ed
Jimmy Olgeni <olgeni@FreeBSD.org>
2013-03-09 21:28:49 +0000

    Add support for the extended FFI, thus enabling the optional use of the math/gsl interface.
    Also, cleanup Makefile patches.

---
+++ lang/newlisp/files/patch-newlisp.h
@@ -0,0 +1,16 @@
+
+$FreeBSD$
+
+--- newlisp.h.orig
++++ newlisp.h
+@@ -36,6 +36,10 @@
+ #  include <ffi.h>
+ #endif
+ 
++#ifdef __FreeBSD__
++#  include <ffi.h>
++#endif
++
+ #define LIBFFI " libffi"
+ #else /* not FFI */
+ #define LIBFFI ""
