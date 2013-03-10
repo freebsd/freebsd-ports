3fd17a9dd885c53076c101d3ebd26a8d79f1ba5e
Kubilay Kocak <koobs@FreeBSD.org>
2013-03-10 08:51:21 +0000

    - Update to 0.8.7p1 - Take maintainership - Update pkg-plist - Trim Makefile header - Add LICENSE - Install buildslave man page - Switch to setuptools and make zip_safe=False unconditional - Add patch to buildslave.test that fixes pydoc -k breakage (upstreamed) - Remove unecessary post-patch: target - Use NO_INSTALL_REQ env var to avoid dependencies - Add TESTS options, TEST_DEPENDS and regression-test target:
    Changes: http://buildbot.net/buildbot/docs/0.8.7p1/relnotes/index.html
    
    Approved by:	rm (mentor), tj (maintainer)
    PR:		ports/174412

--- devel/buildbot-slave/files/patch-setup.py
+++ devel/buildbot-slave/files/patch-setup.py
@@ -1,21 +1,15 @@
---- setup.py.orig	2011-06-20 11:58:27.000000000 +0800
-+++ setup.py	2011-06-20 11:59:29.000000000 +0800
-@@ -103,18 +103,4 @@
- if sys.platform == "win32":
-     setup_args['zip_safe'] = False
+--- ./setup.py.orig	2012-09-23 00:57:03.000000000 +1000
++++ ./setup.py	2012-12-07 22:19:57.724590221 +1100
+@@ -105,11 +105,7 @@
+         }
+     }
  
--try:
--    # If setuptools is installed, then we'll add setuptools-specific arguments
--    # to the setup args.
--    import setuptools #@UnusedImport
--except ImportError:
--    pass
--else:
--    setup_args['install_requires'] = [
--        'twisted >= 8.0.0',
--    ]
--
--    if os.getenv('NO_INSTALL_REQS'):
--        setup_args['install_requires'] = None
--
- setup(**setup_args)
+-# set zip_safe to false to force Windows installs to always unpack eggs
+-# into directories, which seems to work better --
+-# see http://buildbot.net/trac/ticket/907
+-if sys.platform == "win32":
+-    setup_args['zip_safe'] = False
++setup_args['zip_safe'] = False
+ 
+ try:
+     # If setuptools is installed, then we'll add setuptools-specific arguments
