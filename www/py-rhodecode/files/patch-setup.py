679670e2cfdd1c08b6a95f12162501467a43ae36
Frederic Culot <culot@FreeBSD.org>
2013-03-11 16:03:40 +0000

    - Update to 1.5.4
    PR:             ports/176854
    Submitted by:	William Grzybowski <william88@gmail.com> (maintainer)

--- www/py-rhodecode/files/patch-setup.py
+++ www/py-rhodecode/files/patch-setup.py
@@ -1,13 +1,14 @@
 diff --git setup.py setup.py
-index 70b6aa5..6f2c869 100644
+index 3e48554..2ff6cd7 100644
 --- setup.py
 +++ setup.py
-@@ -33,23 +33,23 @@ __platform__ = platform.system()
+@@ -33,24 +33,24 @@ __platform__ = platform.system()
  is_windows = __platform__ in _get_meta_var('PLATFORM_WIN', _metadata)
  
  requirements = [
 -    "waitress==0.8.1",
 -    "webob==1.0.8",
+-    "webtest==1.4.3",
 -    "Pylons==1.0.0",
 -    "Beaker==1.6.4",
 -    "WebHelpers==1.3",
@@ -16,6 +17,7 @@ index 70b6aa5..6f2c869 100644
 -    "Mako==0.7.3",
 +    "waitress>=0.8.1",
 +    "webob>=1.0.8",
++    "webtest>=1.4.3",
 +    "Pylons>=1.0.0",
 +    "Beaker>=1.6.4",
 +    "WebHelpers>=1.3",
@@ -40,7 +42,7 @@ index 70b6aa5..6f2c869 100644
      "mock",
  ]
  
-@@ -64,7 +64,7 @@ if is_windows:
+@@ -65,7 +65,7 @@ if is_windows:
      requirements.append("mercurial==2.4.2")
  else:
      requirements.append("py-bcrypt")
