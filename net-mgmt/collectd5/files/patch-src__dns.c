ec78422e8b228db43a222dacfce8bfd29612645e
Beech Rintoul <beech@FreeBSD.org>
2013-03-12 18:29:33 +0000

    - Update to 5.2.1 - Fix build with clang
    PR:		ports/176691
    Submitted by:	William Grzybowski <william88@gmail.com>
    Approved by:	Krzysztof Stryjek <ports@bsdserwis.com> (maintainer)

--- net-mgmt/collectd5/files/patch-src__dns.c
+++ net-mgmt/collectd5/files/patch-src__dns.c
@@ -1,6 +1,8 @@
---- src/dns.c.orig	2008-09-05 15:26:02.000000000 +0000
-+++ src/dns.c	2008-09-05 15:25:55.000000000 +0000
-@@ -210,6 +210,15 @@
+diff --git src/dns.c src/dns.c
+index fe3b672..0c43985 100644
+--- src/dns.c
++++ src/dns.c
+@@ -223,6 +223,15 @@ static void *dns_child_loop (__attribute__((unused)) void *dummy)
  		pthread_sigmask (SIG_SETMASK, &sigmask, NULL);
  	}
  
@@ -8,11 +10,11 @@
 +#ifdef __sparc64__
 +	if (pcap_device == NULL)
 +	{
-+	    ERROR ("dns plugin: Interface required");
-+	    return (NULL);
++		ERROR ("dns plugin: Interface required");
++		return (NULL);
 +	}
 +#endif
 +
  	/* Passing `pcap_device == NULL' is okay and the same as passign "any" */
- 	DEBUG ("Creating PCAP object..");
- 	pcap_obj = pcap_open_live (pcap_device,
+ 	DEBUG ("dns plugin: Creating PCAP object..");
+ 	pcap_obj = pcap_open_live ((pcap_device != NULL) ? pcap_device : "any",
