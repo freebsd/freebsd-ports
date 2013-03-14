2b4983b05d4b3e55be202bca641b679d38454bc2
miwi <miwi@FreeBSD.org>
2013-03-14 05:35:08 +0000

    - Update to 8.4
    PR:		176531
    Submitted by:	Ports Fury

--- www/linkchecker/files/patch-setup.py
+++ www/linkchecker/files/patch-setup.py
@@ -1,6 +1,38 @@
---- setup.py.orig	2011-02-06 03:58:59.454396452 +0300
-+++ setup.py	2011-02-06 04:05:09.118442022 +0300
-@@ -357,17 +357,14 @@
+--- setup.py.orig
++++ setup.py
+@@ -326,15 +326,6 @@
+ class MyInstallLib (install_lib, object):
+     """Custom library installation."""
+ 
+-    def install (self):
+-        """Install the generated config file."""
+-        outs = super(MyInstallLib, self).install()
+-        infile = self.create_conf_file()
+-        outfile = os.path.join(self.install_dir, os.path.basename(infile))
+-        self.copy_file(infile, outfile)
+-        outs.append(outfile)
+-        return outs
+-
+     def create_conf_file (self):
+         """Create configuration file."""
+         cmd_obj = self.distribution.get_command_obj("install")
+@@ -385,7 +376,6 @@
+ 
+     def run (self):
+         """Adjust permissions on POSIX systems."""
+-        self.add_message_files()
+         super(MyInstallData, self).run()
+         self.fix_permissions()
+ 
+@@ -583,7 +573,6 @@
+     def run (self):
+         """Check MANIFEST and build message files before building."""
+         check_manifest()
+-        self.build_message_files()
+         build.run(self)
+ 
+ 
+@@ -627,8 +616,6 @@
  if os.name == 'nt':
      # windows does not have unistd.h
      define_macros.append(('YY_NO_UNISTD_H', None))
@@ -8,30 +40,33 @@
 -    extra_compile_args.append("-pedantic")
  
  myname = "Bastian Kleineidam"
- myemail = "calvin@users.sourceforge.net"
- 
- data_files = [
-     ('share/linkchecker',
--        ['config/linkcheckerrc', 'config/logging.conf',
--        'doc/html/lccollection.qhc', 'doc/html/lcdoc.qch']),
--    ('share/linkchecker/examples',
-+    ['config/linkcheckerrc', 'config/logging.conf']),
-+        ('share/examples/linkchecker',
-         ['cgi-bin/lconline/leer.html.en',
-          'cgi-bin/lconline/leer.html.de',
-          'cgi-bin/lconline/index.html',
-@@ -381,11 +378,9 @@
- ]
- 
- if os.name == 'posix':
--    data_files.append(('share/man/man1', ['doc/en/linkchecker.1', 'doc/en/linkchecker-gui.1']))
--    data_files.append(('share/man/man5', ['doc/en/linkcheckerrc.5']))
--    data_files.append(('share/man/de/man1', ['doc/de/linkchecker.1', 'doc/de/linkchecker-gui.1']))
--    data_files.append(('share/man/de/man5', ['doc/de/linkcheckerrc.5']))
--    data_files.append(('share/linkchecker/examples',
-+              data_files.append(('man/man1', ['doc/en/linkchecker.1']))
-+              data_files.append(('man/man5', ['doc/en/linkcheckerrc.5']))
-+              data_files.append(('share/examples/linkchecker',
-               ['config/linkchecker-completion',
-                'doc/examples/check_blacklist.sh',
-                'doc/examples/check_for_x_errors.sh',
+ myemail = "bastian.kleineidam@web.de"
+@@ -898,9 +885,6 @@
+         'py2app': MyPy2app,
+         'register': MyRegister,
+     },
+-    package_dir = {
+-        'linkcheck_dns.dns': 'third_party/dnspython/dns',
+-    },
+     packages = [
+         'linkcheck',
+         'linkcheck.bookmarks',
+@@ -913,10 +897,6 @@
+         'linkcheck.HtmlParser',
+         'linkcheck.logger',
+         'linkcheck.network',
+-        'linkcheck_dns.dns',
+-        'linkcheck_dns.dns.rdtypes',
+-        'linkcheck_dns.dns.rdtypes.ANY',
+-        'linkcheck_dns.dns.rdtypes.IN',
+     ],
+     ext_modules = [
+         Extension('linkcheck.HtmlParser.htmlsax',
+@@ -941,7 +921,6 @@
+         ),
+     ],
+     scripts = scripts,
+-    data_files = data_files,
+     classifiers = [
+         'Topic :: Internet :: WWW/HTTP :: Site Management :: Link Checking',
+         'Development Status :: 5 - Production/Stable',
