2104542c7d60419a051b2de52d7fb92e19db7aa6
tota <tota@FreeBSD.org>
2013-03-16 02:54:51 +0000

    - Update to 6.7.0 - Remove indefinite article from COMMENT - Adapt to the new OPTIONS framework
    Release Notes:	http://astah.net/release-notes/community/com-6.7.0

--- devel/astah-community/files/patch-astah-command.sh
+++ devel/astah-community/files/patch-astah-command.sh
@@ -1,16 +1,17 @@
---- astah-command.sh.orig	2012-03-19 00:16:44.000000000 +0900
-+++ astah-command.sh	2012-03-25 21:36:30.000000000 +0900
-@@ -22,11 +22,11 @@
+--- astah-command.sh.orig	2013-03-08 06:02:20.000000000 +0900
++++ astah-command.sh	2013-03-15 10:50:23.000000000 +0900
+@@ -22,7 +22,7 @@
  #export PATH=$PATH:$JAVA_HOME/bin
  
  #ASTAH_HOME=/usr/lib/astah_community
--ASTAH_HOME=`pwd`
+-ASTAH_HOME=`dirname "$0"`
 +ASTAH_HOME=%%DATADIR%%
  
  INITIAL_HEAP_SIZE=64m
  MAXIMUM_HEAP_SIZE=1024m
- 
+@@ -30,4 +30,4 @@
  JAVA_OPTS="-Xms$INITIAL_HEAP_SIZE -Xmx$MAXIMUM_HEAP_SIZE"
+ #JAVA_OPTS="\$JAVA_OPTS -DrootLevel=DEBUG"
  
 -java $JAVA_OPTS -cp "$ASTAH_HOME/astah-community.jar" com.change_vision.jude.cmdline.JudeCommandRunner "$@"
 +%%JAVA_HOME%%/bin/java $JAVA_OPTS -cp "$ASTAH_HOME/astah-community.jar" com.change_vision.jude.cmdline.JudeCommandRunner "$@"
