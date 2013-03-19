f6079e07a35dd6628d7749d805fd3379c56886dd
thierry <thierry@FreeBSD.org>
2013-03-19 20:34:59 +0000

    Upgrade to 1.10.4.

--- cad/astk-serveur/files/patch-asrun_config.py
+++ cad/astk-serveur/files/patch-asrun_config.py
@@ -1,13 +1,13 @@
---- ./asrun/config.py.orig	2010-07-07 18:18:08.000000000 +0200
-+++ ./asrun/config.py	2010-08-11 19:03:21.000000000 +0200
-@@ -66,8 +66,8 @@
-             'SRCHIST'         : ['histor'],
-             # name of "binaries" (as results of a make)
-             'MAKE'            : ['debug nodebug'],
--            'BIN_NODBG'       : ['asteru.exe'],
--            'BIN_DBG'         : ['asterd.exe'],
-+            'BIN_NODBG'       : ['asteru'],
-+            'BIN_DBG'         : ['asterd'],
-             'BINCMDE'         : ['commande'],
-             'BINELE'          : ['elements'],
-             'BINPICKLED'      : ['cata_ele.pickled'],
+--- asrun/config.py.orig	2013-01-08 09:55:52.000000000 +0100
++++ asrun/config.py	2013-03-17 17:38:29.000000000 +0100
+@@ -44,8 +44,8 @@
+     'SRCHIST'         : ['histor'],
+     # name of "binaries" (as results of a make)
+     'MAKE'            : ['debug nodebug'],
+-    'BIN_NODBG'       : ['asteru.exe'],
+-    'BIN_DBG'         : ['asterd.exe'],
++    'BIN_NODBG'       : ['asteru'],
++    'BIN_DBG'         : ['asterd'],
+     'BINCMDE'         : ['commande'],
+     'BINELE'          : ['elements'],
+     'BINPICKLED'      : ['cata_ele.pickled'],
