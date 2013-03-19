e9b220a1164bdf71a225da1fdcbe68302c448efc
makc <makc@FreeBSD.org>
2013-03-19 18:00:07 +0000

    - Update cmake to 2.8.10.2 - Trim Makefile header - Drop indefinite article from COMMENT
    Obtained from:	area51
    Tested by:	exp-run (bapt)

--- devel/cmake/files/patch-Modules_FindQt4.cmake
+++ devel/cmake/files/patch-Modules_FindQt4.cmake
@@ -1,43 +1,43 @@
---- ./Modules/FindQt4.cmake.orig	2011-12-30 17:49:56.000000000 +0100
-+++ ./Modules/FindQt4.cmake	2012-04-07 18:45:53.442654169 +0200
-@@ -473,7 +473,7 @@
+--- Modules/FindQt4.cmake.orig	2012-11-06 21:41:36.000000000 +0200
++++ Modules/FindQt4.cmake	2012-11-27 22:31:37.000000000 +0200
+@@ -497,7 +497,7 @@
  # check for qmake
  # Debian uses qmake-qt4
  # macports' Qt uses qmake-mac
--FIND_PROGRAM(QT_QMAKE_EXECUTABLE NAMES qmake qmake4 qmake-qt4 qmake-mac PATHS
-+FIND_PROGRAM(QT_QMAKE_EXECUTABLE NAMES qmake-qt4 qmake qmake4 qmake-mac PATHS
-   "[HKEY_CURRENT_USER\\Software\\Trolltech\\Qt3Versions\\4.0.0;InstallDir]/bin"
-   "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\4.0.0;InstallDir]/bin"
-   "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\${qt_install_version};InstallDir]/bin"
-@@ -665,13 +665,7 @@
+-find_program(QT_QMAKE_EXECUTABLE NAMES qmake qmake4 qmake-qt4 qmake-mac
++find_program(QT_QMAKE_EXECUTABLE NAMES qmake-qt4 qmake qmake4 qmake-mac
+   PATHS
+     ENV QTDIR
+     "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\${qt_install_version};InstallDir]"
+@@ -690,13 +690,7 @@
    # ask qmake for the plugins directory
-   IF (QT_LIBRARY_DIR AND NOT QT_PLUGINS_DIR  OR  QT_QMAKE_CHANGED)
+   if (QT_LIBRARY_DIR AND NOT QT_PLUGINS_DIR  OR  QT_QMAKE_CHANGED)
      _qt4_query_qmake(QT_INSTALL_PLUGINS qt_plugins_dir)
--    SET(QT_PLUGINS_DIR NOTFOUND)
+-    set(QT_PLUGINS_DIR NOTFOUND)
 -    foreach(qt_cross_path ${CMAKE_FIND_ROOT_PATH})
 -      set(qt_cross_paths ${qt_cross_paths} "${qt_cross_path}/plugins")
--    endforeach(qt_cross_path)
--    FIND_PATH(QT_PLUGINS_DIR NAMES accessible imageformats sqldrivers codecs designer
+-    endforeach()
+-    find_path(QT_PLUGINS_DIR NAMES accessible imageformats sqldrivers codecs designer
 -      HINTS ${qt_cross_paths} ${qt_plugins_dir}
 -      DOC "The location of the Qt plugins")
-+    SET(QT_PLUGINS_DIR ${qt_plugins_dir} CACHE PATH "The location of the Qt plugins" FORCE)
-   ENDIF (QT_LIBRARY_DIR AND NOT QT_PLUGINS_DIR  OR  QT_QMAKE_CHANGED)
++    set(QT_PLUGINS_DIR ${qt_plugins_dir} CACHE PATH "The location of the Qt plugins" FORCE)
+   endif ()
  
    # ask qmake for the translations directory
-@@ -684,15 +678,7 @@
-   IF (QT_LIBRARY_DIR AND NOT QT_IMPORTS_DIR OR QT_QMAKE_CHANGED)
+@@ -709,15 +703,7 @@
+   if (QT_LIBRARY_DIR AND NOT QT_IMPORTS_DIR OR QT_QMAKE_CHANGED)
      _qt4_query_qmake(QT_INSTALL_IMPORTS qt_imports_dir)
      if(qt_imports_dir)
--      SET(QT_IMPORTS_DIR NOTFOUND)
+-      set(QT_IMPORTS_DIR NOTFOUND)
 -      foreach(qt_cross_path ${CMAKE_FIND_ROOT_PATH})
 -        set(qt_cross_paths ${qt_cross_paths} "${qt_cross_path}/imports")
--      endforeach(qt_cross_path)
--      FIND_PATH(QT_IMPORTS_DIR NAMES Qt
+-      endforeach()
+-      find_path(QT_IMPORTS_DIR NAMES Qt
 -        HINTS ${qt_cross_paths} ${qt_imports_dir}
 -        DOC "The location of the Qt imports"
 -        NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH
 -        NO_CMAKE_SYSTEM_PATH)
-+      SET(QT_IMPORTS_DIR ${qt_imports_dir} CACHE PATH "The location of the Qt imports" FORCE)
++      set(QT_IMPORTS_DIR ${qt_imports_dir} CACHE PATH "The location of the Qt imports" FORCE)
        mark_as_advanced(QT_IMPORTS_DIR)
-     endif(qt_imports_dir)
-   ENDIF (QT_LIBRARY_DIR AND NOT QT_IMPORTS_DIR  OR  QT_QMAKE_CHANGED)
+     endif()
+   endif ()
--- devel/cmake/files/patch-Modules_FindSDL.cmake
+++ devel/cmake/files/patch-Modules_FindSDL.cmake
@@ -1,32 +1,23 @@
---- Modules/FindSDL.cmake.orig	2012-04-18 22:24:55.000000000 -0300
-+++ Modules/FindSDL.cmake	2012-04-18 22:26:06.000000000 -0300
-@@ -82,6 +82,11 @@
-   /opt
+--- Modules/FindSDL.cmake.orig	2012-11-06 21:41:36.000000000 +0200
++++ Modules/FindSDL.cmake	2012-11-27 22:34:53.000000000 +0200
+@@ -73,6 +73,11 @@
+   PATH_SUFFIXES include/SDL include/SDL12 include/SDL11 include
  )
  
 +# On FreeBSD SDL depends on libiconv and SDL_stdinc.h includes iconv.h, which is
 +# located in ${LOCALBASE}/include. Append {LOCALBASE}/include to
 +# the SDL_INCLUDE_DIR, thus allow to build SDL apps out of box.
-+LIST(APPEND SDL_INCLUDE_DIR /usr/local/include)
++list(APPEND SDL_INCLUDE_DIR /usr/local/include)
 +
  # SDL-1.1 is the name used by FreeBSD ports...
  # don't confuse it for the version number.
- FIND_LIBRARY(SDL_LIBRARY_TEMP 
-@@ -97,7 +102,7 @@
+ find_library(SDL_LIBRARY_TEMP
+@@ -83,7 +88,7 @@
  )
  
- IF(NOT SDL_BUILDING_LIBRARY)
--  IF(NOT ${SDL_INCLUDE_DIR} MATCHES ".framework")
-+  IF(NOT "${SDL_INCLUDE_DIR}" MATCHES ".framework")
-     # Non-OS X framework versions expect you to also dynamically link to 
-     # SDLmain. This is mainly for Windows and OS X. Other (Unix) platforms 
+ if(NOT SDL_BUILDING_LIBRARY)
+-  if(NOT ${SDL_INCLUDE_DIR} MATCHES ".framework")
++  if(NOT "${SDL_INCLUDE_DIR}" MATCHES ".framework")
+     # Non-OS X framework versions expect you to also dynamically link to
+     # SDLmain. This is mainly for Windows and OS X. Other (Unix) platforms
      # seem to provide SDLmain for compatibility even though they don't
-@@ -113,7 +118,7 @@
-       /opt/csw
-       /opt
-     )
--  ENDIF(NOT ${SDL_INCLUDE_DIR} MATCHES ".framework")
-+  ENDIF(NOT "${SDL_INCLUDE_DIR}" MATCHES ".framework")
- ENDIF(NOT SDL_BUILDING_LIBRARY)
- 
- # SDL may require threads on your system.
