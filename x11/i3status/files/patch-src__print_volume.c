6fb4ea4326c1779d73b4f9b5767ecc104e30989c
bapt <bapt@FreeBSD.org>
2013-03-19 18:38:14 +0000

    - update to 2.7 Make patch for colours in volume output actually works. Add a patch for colours on low remaining battery Add a patch for colours on high CPU temperature

--- x11/i3status/files/patch-src__print_volume.c
+++ x11/i3status/files/patch-src__print_volume.c
@@ -1,25 +1,35 @@
---- ./src/print_volume.c.orig	2012-10-03 13:44:44.000000000 +0200
-+++ ./src/print_volume.c	2012-12-29 01:14:29.384748264 +0100
-@@ -175,8 +175,9 @@
+--- ./src/print_volume.c.orig	2013-03-19 21:06:34.895386349 +0100
++++ ./src/print_volume.c	2013-03-19 21:06:39.429385083 +0100
+@@ -175,16 +175,32 @@
  	}
  #endif
- #if defined(__FreeBSD__) || defined(__OpenBSD__)
+ #if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
 -        char mixerpath[] = "/dev/mixer";
-+	char mixerpath[] = "/dev/mixer";
++        char *mixerpath;
++        char defaultmixer[] = "/dev/mixer";
          int mixfd, vol, devmask = 0;
-+	int pbval = 1;
++        pbval = 1;
++
++        if (mixer_idx >= 0)
++                asprintf(&mixerpath, "/dev/mixer%d", mixer_idx);
++        else
++                mixerpath = defaultmixer;
  
          if ((mixfd = open(mixerpath, O_RDWR)) < 0)
                  return;
-@@ -185,6 +186,12 @@
++
++        if (mixer_idx >= 0)
++                free(mixerpath);
++
+         if (ioctl(mixfd, SOUND_MIXER_READ_DEVMASK, &devmask) == -1)
+                 return;
          if (ioctl(mixfd, MIXER_READ(0),&vol) == -1)
                  return;
  
-+	if ((vol & 0x7f) == 0 && (vol >> 8) & 0x7f == 0)
-+		pbval = 0;
-+
-+	if (!pbval)
-+		START_COLOR("color_bad");
++        if (((vol & 0x7f) == 0) && (((vol >> 8) & 0x7f) == 0)) {
++                START_COLOR("color_degraded"); 
++                pbval = 0;
++        }
 +
          const char *walk = fmt;
          for (; *walk != '\0'; walk++) {
