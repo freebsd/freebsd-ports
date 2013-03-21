1280305fc18f722b6000942ee6c7890b7a35dabb
sbruno <sbruno@FreeBSD.org>
2013-03-21 02:29:17 +0000

    Add wrapper FreeBSD_Version check at the break point for VM_OBJECT_RLOCK name change so we still build on stable branches.
    Suggested by: bdrewery

--- emulators/virtualbox-ose-kmod/files/patch-src-VBox-Runtime-r0drv-freebsd-the-freebsd-kernel.h
+++ emulators/virtualbox-ose-kmod/files/patch-src-VBox-Runtime-r0drv-freebsd-the-freebsd-kernel.h
@@ -1,12 +1,14 @@
 $FreeBSD$
 
---- src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h	2012-12-19 10:27:29.000000000 -0800
-+++ src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h	2013-03-20 14:50:35.330638254 -0700
-@@ -50,6 +50,7 @@
+--- /home/sbruno/ports/virtualbox-ose-kmod/work/VirtualBox-4.2.6/src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h	2012-12-19 10:27:29.000000000 -0800
++++ ../work/VirtualBox-4.2.6/src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h	2013-03-20 19:10:15.854804053 -0700
+@@ -50,6 +50,9 @@
  #include <sys/unistd.h>
  #include <sys/kthread.h>
  #include <sys/lock.h>
++#if __FreeBSD_version >= 1000030
 +#include <sys/rwlock.h>
++#endif
  #include <sys/mutex.h>
  #include <sys/sched.h>
  #include <sys/callout.h>
