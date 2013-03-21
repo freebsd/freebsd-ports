644aa2a709df1ebd5f141f8e4b28bb1efac1b304
Max Brazhnikov <makc@FreeBSD.org>
2013-03-21 14:43:24 +0000

    - Update Geany and plugins to 1.23 - Add ports for new plugins (commander, scope) and localization forgotten   after splitting (geany-plugins-l10n). geanyGDB plugin has been removed   upstream. markdown plugin excluded from ports as it requires currently   missing port for libmarkdown. - Patch configure script and remove build dependence on shells/bash

--- devel/geany-plugins/files/bsd.geany-plugins.mk
+++ devel/geany-plugins/files/bsd.geany-plugins.mk
@@ -1,13 +1,13 @@
 # $FreeBSD$
 
-GEANY_VER=	1.22
+GEANY_VER=	1.23
 GEANY_PLUGINS_ALL=	addons \
 			codenav \
+			commander \
 			debugger \
 			devhelp \
 			doc \
 			extrasel \
-			gdb \
 			gendoc \
 			geniuspaste \
 			gproject \
@@ -22,6 +22,7 @@ GEANY_PLUGINS_ALL=	addons \
 			pg \
 			pretty-printer \
 			prj \
+			scope \
 			sendmail \
 			shiftcolumn \
 			spellcheck \
@@ -32,13 +33,15 @@ GEANY_PLUGINS_ALL=	addons \
 			webhelper \
 			xmlsnippets
 
+# markdown plugin is excluded, requires peg-markdown
+
 addons_DESC=		various small addons
 codenav_DESC=		some facilities for navigating in the code
+commander_DESC=		command panel for rapid access to any action
 debugger_DESC=		debugging support (via GDB currently)
 devhelp_DESC=		API documentation browser
 doc_DESC=		execute command on the word at cursor position
 extrasel_DESC=		additional selection tools
-gdb_DESC=		debugging support via the GNU Debugger
 gendoc_DESC=		generate documentation from the sources comments
 geniuspaste_DESC=	pastebins support
 gproject_DESC=		extension of Geany's project management
@@ -47,12 +50,14 @@ latex_DESC=		LaTeX support
 lipsum_DESC=		Lorem Ipsum generator
 lua_DESC=		Lua scripting
 macro_DESC=		user defined macros
+# markdown_DESC=		real-time preview of rendered Markdown
 miniscript_DESC=	pipe text/documents via script (shell, perl, awk, etc.)
 multiterm_DESC=		multi-tabbed virtual terminals emulator
 numberedbookmarks_DESC=	additional numbered bookmarks
 pg_DESC=		encrypt, decrypt and verify signatures with GnuPG
 pretty-printer_DESC=	formats XML and make it human-readable
 prj_DESC=		alternative project manager
+scope_DESC=		graphical GDB front-end
 sendmail_DESC=		send a document as attachment
 shiftcolumn_DESC=	move blocks of text horizontally
 spellcheck_DESC=	spell check via Enchant
@@ -62,3 +67,20 @@ updatechecker_DESC=	check for new version of Geany
 vc_DESC=		access to different version-control systems
 webhelper_DESC=		web development facilities
 xmlsnippets_DESC=	XML/HTML tag autocompletion
+
+doc_DIR=		geanydoc
+extrasel_DIR=		geanyextrasel
+gendoc_DIR=		geanygendoc
+insertnum_DIR=		geanyinsertnum
+latex_DIR=		geanylatex
+lipsum_DIR=		geanylipsum
+lua_DIR=		geanylua
+macro_DIR=		geanymacro
+miniscript_DIR=		geanyminiscript
+numberedbookmarks_DIR=	geanynumberedbookmarks
+pg_DIR=			geanypg
+prj_DIR=		geanyprj
+sendmail_DIR=		geanysendmail
+vc_DIR=			geanyvc
+
+pretty-printer_SWITCH=	--enable-pretty_printer
