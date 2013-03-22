b9ef7d0740007ed408c5f03b7f8e9c8a2f162b85
Emanuel Haupt <ehaupt@FreeBSD.org>
2013-03-22 16:24:53 +0000

    Remove superfluous comment from patch file.

--- audio/milkytracker/files/patch-src__compression__DecompressorGZIP.cpp
+++ audio/milkytracker/files/patch-src__compression__DecompressorGZIP.cpp
@@ -1,17 +1,15 @@
---- ./src/compression/DecompressorGZIP.cpp.orig	2013-03-22 15:58:05.955397673 +0100
-+++ ./src/compression/DecompressorGZIP.cpp	2013-03-22 15:59:20.987397134 +0100
-@@ -57,11 +57,13 @@
+--- ./src/compression/DecompressorGZIP.cpp.orig	2013-03-22 17:21:02.000000000 +0100
++++ ./src/compression/DecompressorGZIP.cpp	2013-03-22 17:21:46.000000000 +0100
+@@ -57,11 +57,11 @@
  	
  bool DecompressorGZIP::decompress(const PPSystemString& outFileName, Hints hint)
  {
 -    gzFile *gz_input_file = NULL;
-+//    gzFile *gz_input_file = NULL;
 +	gzFile gz_input_file = NULL;
  	int len = 0;
  	pp_uint8 *buf;
  	
 -	if ((gz_input_file = (void **)gzopen (fileName.getStrBuffer(), "r")) == NULL)
-+//	if ((gz_input_file = (void **)gzopen (fileName.getStrBuffer(), "r")) == NULL)
 +	if ((gz_input_file = gzopen (fileName.getStrBuffer(), "r")) == NULL)
  		return false;
  	
