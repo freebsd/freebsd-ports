bba9c27d6057794e6685c93561c6ebdb5c061d7b
David Naylor <dbn@FreeBSD.org>
2013-03-23 16:01:56 +0000

    Update lang/pypy to 2.0.b1.
    The internals of the port have been substantially reworked:
     * All predefined instances can be selected via options [1]
     * Optionally use options, if user does not overwrite instance list
     * Make translation with pypy an option, if it is available.
     * Make memory checking more refined [2]
     * Add a Wiki page details lang/pypy
     * Fix the test target
     * Refactor build target (easier to review, edit)
     * Rename patches to prevent churn
    
    [1] Although two are broken upstream and one possibly discontinued
    [2] My memory limits appear to be too conservative.  Set PYPY_IGNORE_MEM for now
    
    Reviewed by:	Kuro <poyopoyo@puripuri.plala.or.jp>, rm@
    Approved by:	bdrewery (mentor)

--- lang/pypy/files/patch-lib__pypyX.Y__lib_pypy__ctypes_config_cache__rebuild.py
+++ lang/pypy/files/patch-lib__pypyX.Y__lib_pypy__ctypes_config_cache__rebuild.py
@@ -1,5 +1,5 @@
---- lib/pypy1.9/lib_pypy/ctypes_config_cache/rebuild.py~	2012-06-22 11:42:55.000000000 +0200
-+++ lib/pypy1.9/lib_pypy/ctypes_config_cache/rebuild.py	2012-06-22 11:43:12.000000000 +0200
+--- lib/pypy2.0/lib_pypy/ctypes_config_cache/rebuild.py~	2012-06-22 11:42:55.000000000 +0200
++++ lib/pypy2.0/lib_pypy/ctypes_config_cache/rebuild.py	2012-06-22 11:43:12.000000000 +0200
 @@ -6,7 +6,7 @@
  # get the correct path
  import os.path
