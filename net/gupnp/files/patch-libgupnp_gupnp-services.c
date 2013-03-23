0640834aa49b6581be0e01fba37ea2f6035d4de6
sunpoet <sunpoet@FreeBSD.org>
2013-03-23 08:09:34 +0000

    - Update to 0.20.1 - Use USES=pathfix - Cleanup Makefile header
    Changes:	http://git.gnome.org/browse/gupnp/tree/NEWS

--- net/gupnp/files/patch-libgupnp_gupnp-services.c
+++ net/gupnp/files/patch-libgupnp_gupnp-services.c
@@ -1,26 +1,30 @@
---- libgupnp/gupnp-service.c.orig	2009-12-02 15:27:43.000000000 +0100
-+++ libgupnp/gupnp-service.c	2009-12-26 14:30:23.000000000 +0100
-@@ -30,7 +30,11 @@
+--- libgupnp/gupnp-service.c.orig	2013-02-20 20:35:57.000000000 +0800
++++ libgupnp/gupnp-service.c	2013-03-15 16:04:28.277418472 +0800
+@@ -30,6 +30,7 @@
  #include <gobject/gvaluecollector.h>
  #include <gmodule.h>
  #include <libsoup/soup-date.h>
-+#if defined(__NetBSD__) || defined(__DragonFly__) || defined(__FreeBSD__)
-+#include <uuid.h>
-+#else
- #include <uuid/uuid.h>
-+#endif
++#include <libsoup/soup-date.h>
  #include <string.h>
  #include "gupnp-service.h"
  #include "gupnp-root-device.h"
-@@ -867,9 +871,19 @@
- {
+@@ -43,6 +44,8 @@
+ 
+ #ifdef G_OS_WIN32
+ #include <rpc.h>
++#elsif defined(__NetBSD__) || defined(__DragonFly__) || defined(__FreeBSD__)
++#include <uuid.h>
+ #else
+ #include <uuid/uuid.h>
+ #endif
+@@ -1100,9 +1103,19 @@
+ #else
          uuid_t id;
          char out[39];
--
 +#if defined(__NetBSD__) || defined(__DragonFly__) || defined(__FreeBSD__)
 +	char *myout;
 +#endif
-+
+ 
 +#if defined(__NetBSD__) || defined(__DragonFly__) || defined(__FreeBSD__)
 +	uuid_create(&id, 0);
 +	uuid_to_string(&id, &myout, 0);
@@ -32,4 +36,4 @@
 +#endif
  
          return g_strdup_printf ("uuid:%s", out);
- }
+ #endif
