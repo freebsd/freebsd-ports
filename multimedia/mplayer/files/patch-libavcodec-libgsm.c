b1cce6c73e3c35d42654b58ad507ec72ef273297
naddy <naddy@FreeBSD.org>
2013-03-23 15:45:25 +0000

    Update mplayer and mencoder to a recent snapshot. Major changes: - Reduce the number of dependencies (use internal ffmpeg whenever possible)   in mplayer - Do not build with clang on i386 - Use new options framework
    PR:		177083
    Submitted by:	Thomas Zander <thomas.e.zander@googlemail.com> (maintainer)

--- multimedia/mplayer/files/patch-libavcodec-libgsm.c
+++ multimedia/mplayer/files/patch-libavcodec-libgsm.c
@@ -1,11 +1,14 @@
---- ffmpeg/libavcodec/libgsm.c.orig	2012-01-29 19:20:05.000000000 +0100
-+++ ffmpeg/libavcodec/libgsm.c	2012-03-22 18:07:58.682722034 +0100
-@@ -27,7 +27,7 @@
- 
+--- ffmpeg/libavcodec/libgsm.c.orig	2013-03-07 19:20:04.000000000 +0100
++++ ffmpeg/libavcodec/libgsm.c	2013-03-22 16:31:14.983780842 +0100
+@@ -28,11 +28,7 @@
  // The idiosyncrasies of GSM-in-WAV are explained at http://kbs.cs.tu-berlin.de/~jutta/toast.html
  
+ #include "config.h"
+-#if HAVE_GSM_H
+ #include <gsm.h>
+-#else
 -#include <gsm/gsm.h>
-+#include <gsm.h>
+-#endif
  
- #include "avcodec.h"
- #include "gsm.h"
+ #include "libavutil/channel_layout.h"
+ #include "libavutil/common.h"
--- multimedia/mplayer/files/patch-libavcodec-mpegaudio_parser.c
+++ multimedia/mplayer/files/patch-libavcodec-mpegaudio_parser.c
@@ -1,11 +1,10 @@
---- ffmpeg/libavcodec/mpegaudio_parser.c.orig	2012-03-04 19:20:05.000000000 +0100
-+++ ffmpeg/libavcodec/mpegaudio_parser.c	2012-05-01 14:57:54.645626270 +0200
-@@ -78,7 +78,7 @@
-                     if (s->header_count > 1) {
+--- ffmpeg/libavcodec/mpegaudio_parser.c.orig	2013-03-05 19:20:04.000000000 +0100
++++ ffmpeg/libavcodec/mpegaudio_parser.c	2013-03-08 18:28:59.428478324 +0100
+@@ -81,6 +81,7 @@
                          avctx->sample_rate= sr;
                          avctx->channels   = channels;
--                        s1->duration      = frame_size;
+                         s1->duration      = frame_size;
 +                        avctx->frame_size = frame_size;
-                         avctx->bit_rate   = bit_rate;
-                     }
-                     break;
+                         if (s->no_bitrate || !avctx->bit_rate) {
+                             s->no_bitrate = 1;
+                             avctx->bit_rate += (bit_rate - avctx->bit_rate) / s->header_count;
