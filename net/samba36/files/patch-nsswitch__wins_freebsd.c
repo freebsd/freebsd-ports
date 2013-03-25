ac1bf3ed5510ff76334fdbeb652f953461179fee
timur <timur@FreeBSD.org>
2013-03-25 15:18:58 +0000

    Bump port to the 3.6.13 version. Incorporate patch for winbind from Samba4. Switch to OPTIONS_NG. Fix download paths.

--- net/samba36/files/patch-nsswitch__wins_freebsd.c
+++ net/samba36/files/patch-nsswitch__wins_freebsd.c
@@ -1,25 +1,25 @@
 --- ./nsswitch/wins_freebsd.c.orig	2010-04-23 01:08:35.000000000 +0200
 +++ ./nsswitch/wins_freebsd.c	2010-04-23 01:08:35.000000000 +0200
 @@ -0,0 +1,79 @@
-+/* 
++/*
 +   Unix SMB/CIFS implementation.
 +
 +   Copyright (C) Timur I. Bakeyev 2007
-+   
++
 +   This library is free software; you can redistribute it and/or
 +   modify it under the terms of the GNU Library General Public
 +   License as published by the Free Software Foundation; either
 +   version 2 of the License, or (at your option) any later version.
-+   
++
 +   This library is distributed in the hope that it will be useful,
 +   but WITHOUT ANY WARRANTY; without even the implied warranty of
 +   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 +   Library General Public License for more details.
-+   
++
 +   You should have received a copy of the GNU Library General Public
 +   License along with this library; if not, write to the
 +   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-+   Boston, MA  02111-1307, USA.   
++   Boston, MA  02111-1307, USA.
 +*/
 +
 +#include "winbind_client.h"
