9e8881703e37fd60633033bb758db6940cbbff3c
Martin Wilke <miwi@FreeBSD.org>
2013-03-26 14:11:35 +0000

    - Update to 2.1.3
    PR:		v176562
    Submitted by:	Ports Fury

---
+++ devel/kyra/files/patch-guitest__guitest.cpp
@@ -0,0 +1,26 @@
+--- guitest/guitest.cpp.orig
++++ guitest/guitest.cpp
+@@ -96,19 +96,19 @@
+ 		switch ( event.type )
+ 		{
+ 			case KrWidgetEvent::ACTIVATED:
+-				sprintf( buf, "ACTIVATED %s source=0x%x\n", source->WidgetType(), (unsigned)source );
++				sprintf( buf, "ACTIVATED %s source=0x%x\n", source->WidgetType(), (uintptr_t)source );
+ 				break;
+ 
+ 			case KrWidgetEvent::DEACTIVATED:
+-				sprintf( buf, "DEACTIVATED %s source=0x%x\n", source->WidgetType(), (unsigned)source );
++				sprintf( buf, "DEACTIVATED %s source=0x%x\n", source->WidgetType(), (uintptr_t)source );
+ 				break;
+ 
+ 			case KrWidgetEvent::COMMAND:
+-				sprintf( buf, "COMMAND %s source=0x%x command=%s arg=%s\n", source->WidgetType(), (unsigned)source, event.command.command, event.command.arg );
++				sprintf( buf, "COMMAND %s source=0x%x command=%s arg=%s\n", source->WidgetType(), (uintptr_t)source, event.command.command, event.command.arg );
+ 				break;
+ 
+ 			case KrWidgetEvent::SELECTION:
+-				sprintf( buf, "SELECTION %s source=0x%x id=%d text=%s\n", source->WidgetType(), (unsigned)source, event.selection.index, event.selection.text ? event.selection.text : "(null)" );
++				sprintf( buf, "SELECTION %s source=0x%x id=%d text=%s\n", source->WidgetType(), (uintptr_t)source, event.selection.index, event.selection.text ? event.selection.text : "(null)" );
+ 				break;
+ 				
+ 			default:
