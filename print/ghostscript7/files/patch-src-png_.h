f73c736077ae7155788121e76439141694581f69
bapt <bapt@FreeBSD.org>
2013-03-26 15:55:59 +0000

    ghostscript7 is very very outdated consider using newer version like print/ghostscript9
    Approved by:	doceng (gjb)

--- print/ghostscript7/files/patch-src-png_.h
+++
@@ -1,9 +0,0 @@
---- src/png_.h.orig	2003-01-17 01:49:05.000000000 +0100
-+++ src/png_.h	2012-04-27 19:22:59.000000000 +0200
-@@ -27,5 +27,6 @@
- #else
- #include "png.h"
- #endif
-+#include "pngpriv.h"
- 
- #endif /* png__INCLUDED */
