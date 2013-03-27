7d5bf1875eda8c850b0243c91db382956e9ded00
Olli Hauer <ohauer@FreeBSD.org>
2013-03-27 18:17:22 +0000

    - update to SNAP-0910052141 (unofficial ver. 2.4.7) - adopt some small useful changes (errno report) from   debian bugtracker [1] requested on the apache@ list
    Changelog:
    2.4.7
     *) *nix: On graceful restart, send SIGTERM, then wait 2sec before
        removing any unix domain sockets.  Based on a patch by Bernd Wurst.
    
     *) Fix uid_t/gid_t compiler warnings.  [Artur Zaprzala <arturz ownmail.net>]
    
     *) A few more EINTR fixes. [Artur Zaprzala <arturz ownmail.net>]
    
     *) Don't let the Content-Length header propagate on errors or across
        redirects.  [Artur Zaprzala <arturz ownmail.net>]
    
     *) Fix pass-header handling (prefix with HTTP_).  Based on a patch by
        [Christian Seiler <chris_se gmx.net>]
    
     *) Add an EOS bucket to the output filter chain.  Based on a patch by
        [Philipp Dunkel <p.dunkel i5invest.com>]
    
     *) Handle EINTR and EAGAIN in places AIX stress testing revealed
        issues.  Based on a patch by [Rainer Jung <rainer.jung kippdata.de>]
    
     *) Allow duplicate Status, Location and ContentType headers
        (consistent with mod_cgi).  Based on a patch by
        [Christian Seiler <chris_se gmx.net>]
    
     *) Fix a spurious idle timeout error that occurred under unique
        application timing and response size conditions.  Based on a
        report and patch by [Joe Strout <joe strout.net>]
    
    [1]  http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=504132
    [2]  http://lists.freebsd.org/pipermail/freebsd-apache/2013-March/003069.html
    
    Submitted by:	Lilian RIGARD <lilian@devclic.fr> (apache@ list)

--- www/mod_fastcgi/files/patch-mod_fastcgi.c
+++ www/mod_fastcgi/files/patch-mod_fastcgi.c
@@ -1,7 +1,7 @@
---- mod_fastcgi.c.orig	2011-12-12 15:37:24.211384000 +0800
-+++ mod_fastcgi.c	2011-12-12 15:37:50.565004000 +0800
-@@ -754,6 +754,11 @@
-                 continue;
+--- ./mod_fastcgi.c.orig	2008-11-09 15:31:03.000000000 +0100
++++ ./mod_fastcgi.c	2013-03-27 18:51:40.000000000 +0100
+@@ -753,6 +753,11 @@
+             	continue;
              }
  
 +            if (strcasecmp(name, "Content-Length") == 0) {
@@ -12,3 +12,21 @@
              /* If the script wants them merged, it can do it */
              ap_table_add(r->err_headers_out, name, value);
              continue;
+@@ -2215,7 +2220,7 @@
+         if (select_status < 0)
+         {
+             ap_log_rerror(FCGI_LOG_ERR_ERRNO, r, "FastCGI: comm with server "
+-                "\"%s\" aborted: select() failed", fr->fs_path);
++                "\"%s\" aborted: select() failed: \"%s\"", fr->fs_path, strerror(errno));
+             state = STATE_ERROR;
+             break;
+         }
+@@ -2298,7 +2303,7 @@
+             	else 
+             	{
+             		ap_log_rerror(FCGI_LOG_ERR, r, "FastCGI: comm with server "
+-            				"\"%s\" aborted: read failed", fr->fs_path);
++            				"\"%s\" aborted: read failed: \"%s\"", fr->fs_path, strerror(errno));
+             		state = STATE_ERROR;
+             		break;
+             	}
