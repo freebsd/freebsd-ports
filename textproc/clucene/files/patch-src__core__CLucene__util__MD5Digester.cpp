3dc90020955fe59f4ac68d13322559c52ce3c88c
Jung-uk Kim <jkim@FreeBSD.org>
2013-03-28 02:24:17 +0000

    A minor style tweak.

--- textproc/clucene/files/patch-src__core__CLucene__util__MD5Digester.cpp
+++ textproc/clucene/files/patch-src__core__CLucene__util__MD5Digester.cpp
@@ -1,5 +1,5 @@
 --- src/core/CLucene/util/MD5Digester.cpp.orig	2011-03-16 20:21:07.000000000 -0400
-+++ src/core/CLucene/util/MD5Digester.cpp	2013-03-27 21:18:10.000000000 -0400
++++ src/core/CLucene/util/MD5Digester.cpp	2013-03-27 22:19:22.000000000 -0400
 @@ -72,18 +72,16 @@
  // PrintMD5: Converts a completed md5 digest into a char* string.
  char* PrintMD5(uint8_t md5Digest[16])
@@ -7,13 +7,14 @@
 -	char chBuffer[256];
 -	char chEach[10];
 +	const char toHex[] = "0123456789abcdef";
-+	char chBuffer[16 * 2 + 1];
++	char chBuffer[sizeof(md5Digest) * 2 + 1];
  	int nCount;
  
 -	memset(chBuffer,0,256);
 -	memset(chEach, 0, 10);
 -
- 	for (nCount = 0; nCount < 16; nCount++)
+-	for (nCount = 0; nCount < 16; nCount++)
++	for (nCount = 0; nCount < sizeof(md5Digest); nCount++)
  	{
 -		cl_sprintf(chEach, 10, "%02x", md5Digest[nCount]);
 -		strncat(chBuffer, chEach, sizeof(chEach));
