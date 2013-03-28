1da7faff1666b96272ed74df3a5899633627537e
makc <makc@FreeBSD.org>
2013-03-28 12:58:40 +0000

    - Fix build after glib update - Explicitly depend on qt4 libraries (though no need for PORTREVISION bump,   kdelibs4 already depends on lots of qt4 stuff)

--- x11-themes/gtk-qt4-engine/files/patch-src__qt_theme_draw.c
+++ x11-themes/gtk-qt4-engine/files/patch-src__qt_theme_draw.c
@@ -1,6 +1,6 @@
 --- ./src/qt_theme_draw.c.orig	2008-03-11 01:30:19.000000000 +0000
-+++ ./src/qt_theme_draw.c	2012-10-06 10:09:43.994767020 +0000
-@@ -19,6 +19,7 @@
++++ ./src/qt_theme_draw.c	2013-03-28 12:45:32.271244758 +0000
+@@ -19,11 +19,12 @@
   ***************************************************************************/
  
  #include <math.h>
@@ -8,3 +8,9 @@
  #include <string.h>
  #include <gtk/gtkprogressbar.h>
  #include <gdk/gdk.h>
+ #include <gtk/gtk.h>
+-#include <glib/glist.h>
++#include <glib.h>
+ 
+ #ifdef HAVE_BONOBO
+ #include <libbonobo.h>
