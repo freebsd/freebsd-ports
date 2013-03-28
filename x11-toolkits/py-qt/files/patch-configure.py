0e0349426d78812742d3b71883b8732eb1bfdb99
rm <rm@FreeBSD.org>
2013-03-28 20:11:08 +0000

    - unbreak the build - trim Makefile header, while here
    PR:		177350
    Submitted by:	John Marino <draco@marino.st>
    Obtained from:	DragonFly BSD

--- x11-toolkits/py-qt/files/patch-configure.py
+++ x11-toolkits/py-qt/files/patch-configure.py
@@ -1,6 +1,41 @@
---- configure.py.orig	Sun May 13 20:08:01 2007
-+++ configure.py	Sun May 13 20:14:01 2007
-@@ -1081,15 +1081,12 @@
+--- configure.py.orig	2013-03-27 00:54:08.000000000 +0000
++++ configure.py
+@@ -258,27 +258,27 @@ class ConfigurePyQt3(ConfigureBase):
+         }
+ 
+     def code(self, extra_include_dirs, extra_lib_dir, extra_libs):
+-        generate_code("qt", extra_include_dirs=extra_include_dirs, extra_lib_dir=extra_lib_dir, extra_libs=extra_libs)
++        generate_code("qt", extra_include_dirs=extra_include_dirs, extra_lib_dir=extra_lib_dir, extra_libs=extra_libs, extra_cxxflags="-DANY=void")
+ 
+         if "qtext" in pyqt_modules:
+-            generate_code("qtext", extra_define=qsci_define, extra_include_dirs=[opt_qsciincdir], extra_lib_dir=opt_qscilibdir, extra_libs=["qscintilla"], sip_flags=qtext_sip_flags)
++            generate_code("qtext", extra_define=qsci_define, extra_include_dirs=[opt_qsciincdir], extra_lib_dir=opt_qscilibdir, extra_libs=["qscintilla"], sip_flags=qtext_sip_flags, extra_cxxflags="-DANY=void")
+ 
+         if "qtgl" in pyqt_modules:
+-            generate_code("qtgl", opengl=1)
++            generate_code("qtgl", opengl=1, extra_cxxflags="-DANY=void")
+ 
+         if "qtpe" in pyqt_modules:
+-            generate_code("qtpe", extra_libs=["qpe"], sip_flags=qtpe_sip_flags)
++            generate_code("qtpe", extra_libs=["qpe"], sip_flags=qtpe_sip_flags, extra_cxxflags="-DANY=void")
+ 
+         if "qtui" in pyqt_modules:
+-            generate_code("qtui", extra_libs=["qui"])
++            generate_code("qtui", extra_libs=["qui"], extra_cxxflags="-DANY=void")
+ 
+         if "qtaxcontainer" in pyqt_modules:
+-            generate_code("qtaxcontainer", extra_libs=["qaxcontainer"])
++            generate_code("qtaxcontainer", extra_libs=["qaxcontainer"], extra_cxxflags="-DANY=void")
+ 
+         # The rest don't need special handling.
+         for m in ("qtcanvas", "qtnetwork", "qtsql", "qttable", "qtxml"):
+             if m in pyqt_modules:
+-                generate_code(m)
++                generate_code(m, extra_cxxflags="-DANY=void")
+ 
+     def tools(self):
+         tool_dirs = []
+@@ -1092,15 +1092,12 @@ def check_qt_installation(macros):
          macros["LIBDIR_QT"] = qt_libdir
  
      # Check the Qt header files have been installed.  Quietly check for Qt v4.
