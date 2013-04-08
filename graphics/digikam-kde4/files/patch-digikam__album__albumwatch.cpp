e7ae0125df81fadd3e2ddfa76cf808fdfebbe843
Max Brazhnikov <makc@FreeBSD.org>
2013-04-08 17:10:56 +0000

    - Update to digiKam and Kipi Plugins ports to 3.1.0 - Split documentations and l10ns to separate ports - Split kipi-plugins-kde4 port
    Approved by:	portmgr (miwi)

--- graphics/digikam-kde4/files/patch-digikam__album__albumwatch.cpp
+++ graphics/digikam-kde4/files/patch-digikam__album__albumwatch.cpp
@@ -1,11 +1,11 @@
---- ./digikam/album/albumwatch.cpp.orig	2012-09-05 19:38:42.000000000 -0400
-+++ ./digikam/album/albumwatch.cpp	2012-09-05 19:40:59.000000000 -0400
-@@ -221,7 +221,7 @@
-             #ifdef Q_WS_MAC
+--- ./digikam/album/albumwatch.cpp.orig	2013-04-04 16:03:45.920385000 +0000
++++ ./digikam/album/albumwatch.cpp	2013-04-04 16:04:05.410385977 +0000
+@@ -278,7 +278,7 @@
+             // Disable file watch for OS X and Windows and hope for future
+             // improvement (possibly with the improvements planned for
+             // QFileSystemWatcher in Qt 5.1)
+-#if defined(Q_WS_MAC) || defined(Q_WS_WIN)
++#if defined(Q_WS_MAC) || defined(Q_WS_WIN) || defined(Q_OS_FREEBSD)
              d->dirWatch->addDir(dir, KDirWatch::WatchDirOnly);
-             #else
--            d->dirWatch->addDir(dir, KDirWatch::WatchFiles | KDirWatch::WatchDirOnly);
-+            d->dirWatch->addDir(dir, KDirWatch::WatchDirOnly);
-             #endif
-         }
-     }
+ #else
+             d->dirWatch->addDir(dir, KDirWatch::WatchFiles | KDirWatch::WatchDirOnly);
