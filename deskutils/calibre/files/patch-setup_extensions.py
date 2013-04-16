ef8587d3fcd5394f72ee25a2abfb5be838b52e23
Guido Falsi <madpilot@FreeBSD.org>
2013-04-16 21:40:44 +0000

    - Update to 0.9.27 - Remove support for unsupported FreeBSD versions
    Approved by:	Rusty Nejdl <rnejdl@ringofsaturn.com> (maintainer, via email)
    Feature safe:	yes

--- deskutils/calibre/files/patch-setup_extensions.py
+++ deskutils/calibre/files/patch-setup_extensions.py
@@ -1,6 +1,20 @@
---- setup/extensions.py.orig	2013-01-04 06:02:00.000000000 +0100
-+++ setup/extensions.py	2013-01-05 18:07:03.534921171 +0100
-@@ -492,8 +492,9 @@
+--- setup/extensions.py.orig	2013-03-29 06:15:02.000000000 +0100
++++ setup/extensions.py	2013-04-09 18:10:19.236217223 +0200
+@@ -183,13 +183,6 @@
+                 sip_files = ['calibre/gui2/progress_indicator/QProgressIndicator.sip']
+                 ),
+ 
+-    Extension('qt_hack',
+-                ['calibre/ebooks/pdf/render/qt_hack.cpp'],
+-                inc_dirs = qt_private_inc + ['calibre/ebooks/pdf/render', 'qt-harfbuzz/src'],
+-                headers = ['calibre/ebooks/pdf/render/qt_hack.h'],
+-                sip_files = ['calibre/ebooks/pdf/render/qt_hack.sip']
+-                ),
+-
+     Extension('unrar',
+               ['unrar/%s.cpp'%(x.partition('.')[0]) for x in '''
+                rar.o strlist.o strfn.o pathfn.o savepos.o smallfn.o global.o file.o
+@@ -492,8 +485,9 @@
  
          pro = textwrap.dedent('''
          TEMPLATE = lib
