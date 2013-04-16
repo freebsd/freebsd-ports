f20364972a149420446bd9c7e3f6a00234b59c29
madpilot <madpilot@FreeBSD.org>
2013-04-16 21:40:44 +0000

    - Update to 0.9.27 - Remove support for unsupported FreeBSD versions
    Approved by:	Rusty Nejdl <rnejdl@ringofsaturn.com> (maintainer, via email)
    Feature safe:	yes

---
+++ deskutils/calibre/files/patch-calibre_constants.py
@@ -0,0 +1,10 @@
+--- src/calibre/constants.py.orig	2013-03-29 06:15:02.000000000 +0100
++++ src/calibre/constants.py	2013-04-09 18:11:37.710213634 +0200
+@@ -136,7 +136,6 @@
+                 'freetype',
+                 'woff',
+                 'unrar',
+-                'qt_hack',
+             ]
+         if iswindows:
+             plugins.extend(['winutil', 'wpd', 'winfonts'])
