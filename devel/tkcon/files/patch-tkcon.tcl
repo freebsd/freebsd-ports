38bbf2dfe37392814eda30136d4983294310d94e
Pietro Cerutti <gahr@FreeBSD.org>
2013-04-18 08:31:37 +0000

    - Fix CATEGORIES - Add WWW line to pkg-descr
    Feature safe:	yes

--- devel/tkcon/files/patch-tkcon.tcl
+++ devel/tkcon/files/patch-tkcon.tcl
@@ -1,6 +1,6 @@
---- tkcon.tcl.orig	2009-02-27 09:17:21.000000000 +0900
-+++ tkcon.tcl	2009-05-16 18:38:23.000000000 +0900
-@@ -194,7 +194,7 @@
+--- tkcon.tcl.orig	2009-02-27 01:17:21.000000000 +0100
++++ tkcon.tcl	2013-04-16 09:25:50.000000000 +0200
+@@ -194,7 +192,7 @@
  	RCS		{RCS: @(#) $Id: tkcon.tcl,v 1.102 2008/02/07 21:02:53 hobbs Exp $}
  	HEADURL		{http://tkcon.cvs.sourceforge.net/tkcon/tkcon/tkcon.tcl?rev=HEAD}
  
@@ -9,7 +9,7 @@
  	email		{jeff(a)hobbs(.)org}
  	root		.
  	uid		0
-@@ -1586,8 +1586,8 @@
+@@ -1586,8 +1584,8 @@
      foreach m [list [menu $w.help] [menu $w.pop.help]] {
  	$m add command -label "About " -underline 0 -accel Ctrl-A \
  		-command ::tkcon::About
