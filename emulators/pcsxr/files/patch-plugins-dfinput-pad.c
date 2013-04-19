f78c7a9d6184ce543ccf48760b0fc313c50fe319
martymac <martymac@FreeBSD.org>
2013-04-19 07:40:04 +0000

    - Update to svn rev. 84102 - Use new Makefile header format
    Feature safe:	yes

--- emulators/pcsxr/files/patch-plugins-dfinput-pad.c
+++ emulators/pcsxr/files/patch-plugins-dfinput-pad.c
@@ -1,15 +1,6 @@
---- plugins/dfinput/pad.c.orig	2012-11-08 06:07:00.000000000 +0100
-+++ plugins/dfinput/pad.c	2012-11-08 06:09:26.000000000 +0100
-@@ -676,8 +676,6 @@
- 	} else if (pid > 0) {
- 		waitpid(pid, NULL, 0);
- 	}
--
--	return PSE_PAD_ERR_SUCCESS;
- }
- 
- #endif
-@@ -685,3 +683,6 @@
+--- plugins/dfinput/pad.c.orig	2013-04-17 16:44:04.000000000 +0200
++++ plugins/dfinput/pad.c	2013-04-17 16:46:50.000000000 +0200
+@@ -683,3 +683,6 @@
  long PADtest(void) {
  	return PSE_PAD_ERR_SUCCESS;
  }
