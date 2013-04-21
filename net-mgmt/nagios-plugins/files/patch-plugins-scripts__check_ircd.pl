d4f392dc8b668bf8817828d19437908e663c4262
bdrewery <bdrewery@FreeBSD.org>
2013-04-21 22:29:06 +0000

    - Fix check_ircd.pl so it works   More information: http://www.nntp.perl.org/group/perl.beginners/2011/02/msg115898.html - Trim header
    PR:		ports/173638
    Submitted by:	Ryan Frederick <ryanrfrederick@gmail.com>
    Approved by:	maintainer timeout
    Feature safe:	yes

--- net-mgmt/nagios-plugins/files/patch-plugins-scripts__check_ircd.pl
+++ net-mgmt/nagios-plugins/files/patch-plugins-scripts__check_ircd.pl
@@ -1,6 +1,6 @@
---- plugins-scripts/check_ircd.pl.orig	Tue May  7 15:05:49 2002
-+++ plugins-scripts/check_ircd.pl	Fri May 12 23:40:44 2006
-@@ -67,6 +67,7 @@
+--- plugins-scripts/check_ircd.pl.orig	2012-06-27 12:32:47.000000000 -0500
++++ plugins-scripts/check_ircd.pl	2012-11-14 20:58:01.479745211 -0600
+@@ -66,6 +66,7 @@
  $ENV{PATH} = "";
  $ENV{ENV} = "";
  $ENV{BASH_ENV} = "";
@@ -8,3 +8,12 @@
  
  # -----------------------------------------------------------------[ Global ]--
  
+@@ -153,7 +154,7 @@
+ 	    print "IRCD UNKNOWN: Could not start socket ($!)\n";
+ 	    exit $ERRORS{"UNKNOWN"};
+ 	}
+-	$sockaddr = 'S n a4 x8';
++	$sockaddr = 'x C n a4 x8';
+ 	$that = pack($sockaddr, AF_INET, $in_remoteport, $thataddr);
+ 	if (!connect(ClientSocket, $that)) { 
+ 	    print "IRCD UNKNOWN: Could not connect socket ($!)\n";
