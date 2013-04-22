8e99fc00fbd6c9d04dd7c50787c513a0feb0443c
Koop Mast <kwm@FreeBSD.org>
2013-04-22 15:11:27 +0000

    Fix the build on 10-i386
    Reported by:	miwi

--- audio/pulseaudio/files/patch-src_pulsecore_svolume_mmx.c
+++ audio/pulseaudio/files/patch-src_pulsecore_svolume_mmx.c
@@ -1,20 +1,26 @@
---- src/pulsecore/svolume_mmx.c.orig	2011-06-23 17:18:54.484125000 -0300
-+++ src/pulsecore/svolume_mmx.c	2012-01-04 23:36:03.000000000 -0200
-@@ -154,7 +154,7 @@
+--- src/pulsecore/svolume_mmx.c.orig	2011-06-23 20:18:54.484125000 +0000
++++ src/pulsecore/svolume_mmx.c	2013-04-18 09:22:42.000000000 +0000
+@@ -154,9 +154,9 @@
          "6:                             \n\t"
          " emms                          \n\t"
  
 -        : "+r" (samples), "+r" (volumes), "+r" (length), "=D" ((pa_reg_x86)channel), "=&r" (temp)
 +        : "+r" (samples), "+r" (volumes), "+r" (length), "=D" (channel), "=&r" (temp)
  #if defined (__i386__)
-         : "m" ((pa_reg_x86)channels)
+-        : "m" ((pa_reg_x86)channels)
++        : "m" (channels)
  #else
-@@ -235,7 +235,7 @@
+         : "r" ((pa_reg_x86)channels)
+ #endif
+@@ -235,9 +235,9 @@
          "6:                             \n\t"
          " emms                          \n\t"
  
 -        : "+r" (samples), "+r" (volumes), "+r" (length), "=D" ((pa_reg_x86)channel), "=&r" (temp)
 +        : "+r" (samples), "+r" (volumes), "+r" (length), "=D" (channel), "=&r" (temp)
  #if defined (__i386__)
-         : "m" ((pa_reg_x86)channels)
+-        : "m" ((pa_reg_x86)channels)
++        : "m" (channels)
  #else
+         : "r" ((pa_reg_x86)channels)
+ #endif
