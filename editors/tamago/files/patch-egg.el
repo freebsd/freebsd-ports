7f9f62aca3663dfaf3e58f808e6af4c6de04ca21
hrs <hrs@FreeBSD.org>
2013-04-23 16:21:21 +0000

    - Remove redundant lines and style clean-ups. - Fix (accept-process-output) to use (with-local-quit). - Replace (inactivate-current-input-method-function) with   (deactivate-current-input-method-function). - Replace (inactivate-input-method) with (deactivate-input-method).

--- editors/tamago/files/patch-egg.el
+++ editors/tamago/files/patch-egg.el
@@ -1,8 +1,11 @@
---- egg.el.orig	2013-03-30 17:08:47.000000000 +0900
-+++ egg.el	2013-03-30 17:11:22.000000000 +0900
-@@ -169,7 +169,8 @@
+--- egg.el.orig	2013-04-23 16:13:43.000000000 +0900
++++ egg.el	2013-04-23 16:15:26.000000000 +0900
+@@ -167,16 +167,17 @@
+ 	  (setq egg-modefull-mode t)
+ 	  (its-define-select-keys egg-modefull-map))
        (setq egg-modeless-mode t))
-     (setq inactivate-current-input-method-function 'egg-mode)
+-    (setq inactivate-current-input-method-function 'egg-mode)
++    (setq deactivate-current-input-method-function 'egg-mode)
      (setq describe-current-input-method-function 'egg-help)
 -    (make-local-hook 'input-method-activate-hook)
 +    (if (fboundp 'make-local-hook)
@@ -10,6 +13,14 @@
      (add-hook 'input-method-activate-hook 'its-set-mode-line-title nil t)
      (if (eq (selected-window) (minibuffer-window))
  	(add-hook 'minibuffer-exit-hook 'egg-exit-from-minibuffer))
+     (run-hooks 'egg-mode-hook)))
+ 
+ (defun egg-exit-from-minibuffer ()
+-  (inactivate-input-method)
++  (deactivate-input-method)
+   (if (<= (minibuffer-depth) 1)
+       (remove-hook 'minibuffer-exit-hook 'egg-exit-from-minibuffer)))
+ 
 @@ -184,7 +185,7 @@
  
  (defun egg-self-insert-char ()
