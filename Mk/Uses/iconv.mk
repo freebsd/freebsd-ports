0068a8a89d5baf4dd73c4d0ca465f11f620488a5
mva <mva@FreeBSD.org>
2013-04-26 20:44:59 +0000

    Introduces USES= iconv to replace USE_ICONV.
    It takes no arguments.
    
    This deprecates USE_ICONV, please convert your ports.
    USE_ICONV will be removed as soon as it is no more used in
    the ports tree.
    
    Approved by:	portmgr (bapt)

---
+++ Mk/Uses/iconv.mk
@@ -0,0 +1,21 @@
+# $FreeBSD$
+#
+# handle dependency on the iconv port
+#
+# MAINTAINER: portmgr@FreeBSD.org
+#
+# Feature:	iconv
+# Usage:	USES=iconv
+# Valid ARGS:	does not require args
+#
+#
+.if !defined(_INCLUDE_USES_ICONV_MK)
+_INCLUDE_USES_ICONV_MK=	yes
+
+.if defined(iconv_ARGS)
+IGNORE=	USES=iconv does not require args
+.endif
+
+LIB_DEPENDS+=	iconv.3:${PORTSDIR}/converters/libiconv
+
+.endif
