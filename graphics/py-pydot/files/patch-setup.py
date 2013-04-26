d1a4b334211b7146067c4d0130a3723f97e2ee14
stefan <stefan@FreeBSD.org>
2013-04-26 14:36:07 +0000

    Update to 1.0.28.

--- graphics/py-pydot/files/patch-setup.py
+++ graphics/py-pydot/files/patch-setup.py
@@ -1,24 +1,10 @@
---- setup.py.orig	2008-02-15 04:48:02.000000000 +0800
-+++ setup.py	2008-09-10 11:12:00.000000000 +0800
-@@ -1,9 +1,9 @@
- #!/usr/bin/env python
- 
--try:
--    from setuptools import setup
--except ImportError, excp:
--    from distutils.core import setup
-+#try:
-+#    from setuptools import setup
-+#except ImportError, excp:
-+from distutils.core import setup
-     
- import pydot
- 
-@@ -27,5 +27,5 @@
+--- setup.py.orig	2013-04-26 15:15:06.000000000 +0200
++++ setup.py	2013-04-26 15:16:48.000000000 +0200
+@@ -31,5 +31,5 @@
          'Topic :: Software Development :: Libraries :: Python Modules'],
      long_description = "\n".join(pydot.__doc__.split('\n')),
      py_modules = ['pydot', 'dot_parser'],
 -    install_requires = ['pyparsing', 'setuptools'],
--    data_files = [('.', ['ChangeLog', 'LICENSE', 'README'])] )
+-    data_files = [('.', ['LICENSE', 'README'])] )
 +    install_requires = ['pyparsing', 'setuptools'] )
-+    #data_files = [('.', ['ChangeLog', 'LICENSE', 'README'])]
++    #data_files = [('.', ['LICENSE', 'README'])] )
