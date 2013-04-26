bdef26427947ea718cb1245ddce26c994d775513
Martin Wilke <miwi@FreeBSD.org>
2013-04-26 07:01:32 +0000

    - Update to 0.44-3
    PR:		178027
    Submitted by:	Ports Fury

--- audio/pd/files/patch-src_s_main.c
+++ audio/pd/files/patch-src_s_main.c
@@ -1,25 +1,5 @@
 --- src/s_main.c.orig	2011-03-07 10:11:02.000000000 +0900
 +++ src/s_main.c	2011-04-06 19:30:04.000000000 +0900
-@@ -300,7 +300,9 @@
-     else
-     {
-             /* open audio and MIDI */
-+#if !defined(__FreeBSD__)
-         sys_reopen_midi();
-+#endif
-         sys_reopen_audio();
-             /* run scheduler until it quits */
-         return (m_mainloop());
-@@ -628,7 +630,9 @@
-         }
-         else if (!strcmp(*argv, "-alsamidi"))
-         {
-+#if !defined(__FreeBSD__)
-           sys_set_midi_api(API_ALSA);
-+#endif
-             argc--; argv++;
-         }
- #endif
 @@ -936,7 +940,11 @@
              /* add "doc/5.reference" library to helppath */
      strncpy(sbuf, sys_libdir->s_name, MAXPDSTRING-30);
@@ -32,23 +12,3 @@
      sys_helppath = namelist_append_files(sys_helppath, sbuf);
          /* correct to make audio and MIDI device lists zero based.  On
          MMIO, however, "1" really means the second device (the first one
-@@ -990,7 +998,9 @@
-         for (i = 0; i < naudiooutdev; i++)
-             audiooutdev[i] = sys_soundoutdevlist[i];
-     }
-+#if !defined(__FreeBSD__)
-     sys_get_midi_params(&nmidiindev, midiindev, &nmidioutdev, midioutdev);
-+#endif
-     if (sys_nmidiin >= 0)
-     {
-         post("sys_nmidiin %d, nmidiindev %d", sys_nmidiin, nmidiindev);
-@@ -1015,7 +1025,9 @@
-     sys_set_audio_settings(naudioindev, audioindev, nchindev, chindev,
-         naudiooutdev, audiooutdev, nchoutdev, choutdev, rate, advance, 
-         callback, blocksize);
-+#if !defined(__FreeBSD__)
-     sys_open_midi(nmidiindev, midiindev, nmidioutdev, midioutdev, 0);
-+#endif
- }
- 
- static void sys_addreferencepath(void)
