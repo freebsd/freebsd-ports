1b1d1eaf0422236c4b5635b07d826d902ec176e4
Martin Wilke <miwi@FreeBSD.org>
2013-04-26 06:34:46 +0000

    - Update to 35.0.0
    PR:		177911
    Submitted by:	Ports Fury

---
+++ editors/tea/files/patch-rvln.cpp
@@ -0,0 +1,10 @@
+--- rvln.cpp.orig
++++ rvln.cpp
+@@ -55,6 +55,7 @@
+ #include <QSettings>
+ #include <QLibraryInfo>
+ #include <QCryptographicHash>
++#include <QDebug>
+ 
+ 
+ #ifdef PRINTER_ENABLE
