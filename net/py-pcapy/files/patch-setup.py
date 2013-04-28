b73179cdecf47d8fd64559efa4385857e1f83120
antoine <antoine@FreeBSD.org>
2013-04-28 22:08:43 +0000

    Unbreak and update py-pcapy to 0.10.8
    Approved by:	eadler (mentor)

--- net/py-pcapy/files/patch-setup.py
+++ net/py-pcapy/files/patch-setup.py
@@ -1,9 +1,10 @@
---- setup.py.orig	2009-04-19 02:22:16.549958526 +0800
-+++ setup.py	2009-04-19 02:24:10.837075743 +0800
-@@ -60,7 +60,5 @@
+--- ./setup.py.orig	2013-04-28 16:58:49.000000000 +0000
++++ ./setup.py	2013-04-28 17:02:38.000000000 +0000
+@@ -62,8 +62,5 @@
            include_dirs = include_dirs,
            library_dirs = library_dirs,
            libraries = libraries)],
+-      scripts = ['tests/pcapytests.py', 'tests/96pings.pcap'],
 -      data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
 -                     ['README', 'LICENSE', 'pcapy.html'])],
        )
