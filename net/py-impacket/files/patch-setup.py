f81138ee85d363b5f1a647cd5bfc60ddd776667b
Antoine Brodin <antoine@FreeBSD.org>
2013-04-28 22:11:47 +0000

    Update py-impacket to 0.9.9.9
    Approved by:	eadler (mentor)

--- net/py-impacket/files/patch-setup.py
+++ net/py-impacket/files/patch-setup.py
@@ -1,12 +1,9 @@
---- Impacket-0.9.6.0/setup.py  2006-09-20 17:37:44.000000000 -0400
-+++ setup.py   2006-09-20 17:48:59.000000000 -0400
-@@ -17,7 +17,7 @@
-       maintainer = "Max Caceres",
-       maintainer_email = "max@coresecurity.com",
-       packages = ['impacket', 'impacket.dcerpc'],
--      scripts = glob.glob(os.path.join('examples', '*.py'))+glob.glob('doc/*'),
-+      scripts = glob.glob(os.path.join('examples', '*.py')),
-       data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
--                     ['README', 'LICENSE'])],
-+                     ['README', 'LICENSE']+glob.glob('doc/*'))]
+--- ./setup.py.orig	2013-04-28 18:06:57.000000000 +0000
++++ ./setup.py	2013-04-28 18:07:08.000000000 +0000
+@@ -18,6 +18,4 @@
+       maintainer_email = "bethus@gmail.com",
+       packages = ['impacket', 'impacket.dcerpc', 'impacket.examples'],
+       scripts = glob.glob(os.path.join('examples', '*.py')),
+-      data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
+-                     ['README', 'LICENSE']+glob.glob('doc/*'))],
        )
