0fb68fa55f5b6b885ac7dd4738fedfa3e61d3c92
Baptiste Daroussin <bapt@FreeBSD.org>
2013-04-30 11:41:44 +0000

    Add a new USES ada.mk to establish Ada-capable compiler as a build dependency and set the build environement properly
    Submitted by:	John Marino <draco@marino.st>

---
+++ Mk/Uses/ada.mk
@@ -0,0 +1,27 @@
+# $FreeBSD$
+#
+# Establish Ada-capable compiler as a build dependency
+#
+# MAINTAINER: draco@marino.st
+#
+# Feature:      ada
+# Usage:        USES=ada
+# Valid ARGS:   does not require args
+
+.if !defined(_INCLUDE_USES_ADA_MK)
+_INCLUDE_USES_ADA_MK=    yes
+
+.if defined(ada_ARGS)
+IGNORE= USES=ada does not require args
+.endif
+
+CC=		ada
+BUILD_DEPENDS+=	${LOCALBASE}/gcc-aux/bin/ada:${PORTSDIR}/lang/gcc-aux
+
+MAKE_ENV+=	PATH=${LOCALBASE}/gcc-aux/bin:${PATH}
+MAKE_ENV+=	ADA_PROJECT_PATH=${LOCALBASE}/lib/gnat
+
+CONFIGURE_ENV+=	PATH=${LOCALBASE}/gcc-aux/bin:${PATH}
+CONFIGURE_ENV+=	ADA_PROJECT_PATH=${LOCALBASE}/lib/gnat
+
+.endif
