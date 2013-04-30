bdbefe3abb6d2667a0a6a83523a67ffb205a5094
olivierd <olivierd@FreeBSD.org>
2013-04-30 17:16:04 +0000

    - Adopt USES for desktop-file-utils, pathfix - Add support for GStreamer 1.0 (GST10) and GStreamer 0.10 (GST01) - Add rtmp plugins (only available with GStreamer 1.0) - Apply 2 patches (obtained from upstream repository), to use gst-helper for setting the volume, and to reduce mousewheel volume-steps (10% -> 5%)

--- multimedia/xfce4-parole/files/patch-src__gst__parole-gst.c
+++ multimedia/xfce4-parole/files/patch-src__gst__parole-gst.c
@@ -1,6 +1,18 @@
 --- ./src/gst/parole-gst.c.orig	2013-03-07 00:14:28.000000000 +0000
-+++ ./src/gst/parole-gst.c	2013-03-09 22:44:40.000000000 +0000
-@@ -1573,22 +1573,36 @@
++++ ./src/gst/parole-gst.c	2013-04-28 16:06:10.000000000 +0000
+@@ -33,9 +33,11 @@
+ #if GST_CHECK_VERSION(1, 0, 0)
+ #include <gst/video/videooverlay.h>
+ #include <gst/video/navigation.h>
++#include <gst/audio/streamvolume.h>
+ #else
+ #include <gst/interfaces/xoverlay.h>
+ #include <gst/interfaces/navigation.h>
++#include <gst/interfaces/streamvolume.h>
+ #endif
+ 
+ #include <gst/pbutils/missing-plugins.h>
+@@ -1573,22 +1575,36 @@
      dialog = GTK_MESSAGE_DIALOG(gtk_message_dialog_new_with_markup(
                              NULL,
                              GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
@@ -37,7 +49,7 @@
                                               desc);
      
      return GTK_DIALOG(dialog);
-@@ -1712,7 +1726,7 @@
+@@ -1712,7 +1728,7 @@
  #endif
  	             gst_install_plugins_context_free(ctx);
  	        }
@@ -46,3 +58,16 @@
  	            gtk_widget_destroy(GTK_WIDGET(dialog));
          }
  	    break;
+@@ -2624,9 +2640,9 @@
+ 
+ void parole_gst_set_volume (ParoleGst *gst, gdouble value)
+ {
+-    g_object_set (G_OBJECT (gst->priv->playbin),
+-		  "volume", value,
+-		  NULL);
++    gst_stream_volume_set_volume (GST_STREAM_VOLUME (gst->priv->playbin),
++				    GST_STREAM_VOLUME_FORMAT_CUBIC,
++				    value);
+ }
+ 						    
+ gdouble	parole_gst_get_volume (ParoleGst *gst)
