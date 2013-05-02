f07e2f9e45c3d8e1c355ff67d324e4df469b6f60
bapt <bapt@FreeBSD.org>
2013-05-02 17:12:43 +0000

    bmake is quite strict with != make it really happy

--- Mk/bsd.ldap.mk
+++ Mk/bsd.ldap.mk
@@ -66,7 +66,7 @@ IGNORE=	cannot install: OpenLDAP versions mismatch: openldap${_OPENLDAP_VER}-cli
 
 CFLAGS+=	-DLDAP_DEPRECATED
 
-_OPENLDAP_CLIENT_PKG!=	${PKG_INFO} -Ex openldap.\*-client 2>/dev/null || ${ECHO_CMD}
+_OPENLDAP_CLIENT_PKG!=	${PKG_INFO} -Ex openldap.\*-client 2>/dev/null; ${ECHO_CMD}
 _OPENLDAP_FLAVOUR=	${_OPENLDAP_CLIENT_PKG:C/openldap//:C/-client-.*//}
 
 .if defined(WANT_OPENLDAP_SASL)
