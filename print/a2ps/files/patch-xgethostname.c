a35160b01ae59fb3352ad3d206f6fa59abd6ccec
dinoex <dinoex@FreeBSD.org>
2013-05-02 19:34:48 +0000

    - make bsdpatch happy PR:     178220
    - merge and cleanup patches

--- print/a2ps/files/patch-xgethostname.c
+++ print/a2ps/files/patch-xgethostname.c
@@ -1,6 +1,23 @@
---- lib/xgethostname.c	Tue Sep  4 00:51:26 2001
-+++ lib/xgethostname.c.new	Tue Sep  4 00:55:39 2001
-@@ -63,7 +63,7 @@
+--- lib/xgethostname.c.orig	Tue Sep  4 00:51:26 2001
++++ lib/xgethostname.c	Tue Sep  4 00:55:39 2001
+@@ -21,6 +21,7 @@
+ # include <config.h>
+ #endif
+ 
++#include <sys/param.h>
+ #include <sys/types.h>
+ 
+ #include <errno.h>
+@@ -38,7 +39,7 @@
+ int gethostname ();
+ 
+ #ifndef INITIAL_HOSTNAME_LENGTH
+-# define INITIAL_HOSTNAME_LENGTH 34
++# define INITIAL_HOSTNAME_LENGTH MAXHOSTNAMELEN
+ #endif
+ 
+ char *
+@@ -63,7 +64,7 @@
        if (err == 0 && hostname[k] == '\0')
  	break;
  #ifdef ENAMETOOLONG
@@ -9,4 +26,3 @@
  	error (EXIT_FAILURE, errno, "gethostname");
  #endif
        size *= 2;
-
