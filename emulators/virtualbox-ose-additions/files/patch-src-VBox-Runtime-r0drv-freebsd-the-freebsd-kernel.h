f86be9720fb7459c83b51bd93a5a41fd4ac73b29
decke <decke@FreeBSD.org>
2013-05-02 20:54:04 +0000

    - Update to 4.2.12 - Add missing patch to additions to fix building after r248084 which renamed VM_OBJECT_LOCK() to VM_OBJECT_WLOCK(). The patch was already included in the kmod port but additions also need it.

--- emulators/virtualbox-ose-additions/files/patch-src-VBox-Runtime-r0drv-freebsd-the-freebsd-kernel.h
+++ emulators/virtualbox-ose-additions/files/patch-src-VBox-Runtime-r0drv-freebsd-the-freebsd-kernel.h
@@ -1,6 +1,16 @@
---- ./src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h.orig	2012-06-12 02:54:01.440878507 +0400
-+++ ./src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h	2012-06-12 00:08:55.218232972 +0400
-@@ -64,6 +64,7 @@
+--- src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h.orig	2013-03-05 14:28:07.000000000 +0000
++++ src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h	2013-03-28 13:10:34.000000000 +0000
+@@ -50,6 +50,9 @@
+ #include <sys/unistd.h>
+ #include <sys/kthread.h>
+ #include <sys/lock.h>
++#if __FreeBSD_version >= 1000030
++#include <sys/rwlock.h>
++#endif
+ #include <sys/mutex.h>
+ #include <sys/sched.h>
+ #include <sys/callout.h>
+@@ -64,6 +67,7 @@
  #include <vm/vm_kern.h>
  #include <vm/vm_param.h>        /* KERN_SUCCESS ++ */
  #include <vm/vm_page.h>
