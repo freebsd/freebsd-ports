a6377295cb203c853dda7d278805699497a84257
stephen <stephen@FreeBSD.org>
2013-05-02 03:15:15 +0000

    - Update to 5.9.

--- math/sage/files/spkg-patch-python_-_src_setup.py
+++ math/sage/files/spkg-patch-python_-_src_setup.py
@@ -1,6 +1,65 @@
---- python-2.7.3.p0/src/setup.py-orig	2012-04-22 00:04:08.000000000 +0000
-+++ python-2.7.3.p5/src/setup.py	2012-04-22 00:10:25.000000000 +0000
-@@ -1432,10 +1432,24 @@
+--- python-2.7.3.p6/src/setup.py-orig	2013-03-17 03:13:46.000000000 +0000
++++ python-2.7.3.p6/src/setup.py	2013-03-17 03:15:11.000000000 +0000
+@@ -642,7 +642,7 @@
+         # use the same library for the readline and curses modules.
+         if 'curses' in readline_termcap_library:
+             curses_library = readline_termcap_library
+-        elif self.compiler.find_library_file(lib_dirs, 'ncursesw'):
++        elif self.compiler.find_library_file(lib_dirs, 'xxxncursesw'):
+             curses_library = 'ncursesw'
+         elif self.compiler.find_library_file(lib_dirs, 'ncurses'):
+             curses_library = 'ncurses'
+@@ -681,7 +681,7 @@
+                                                      'termcap'):
+                 readline_libs.append('termcap')
+             exts.append( Extension('readline', ['readline.c'],
+-                                   library_dirs=['/usr/lib/termcap'],
++                                   library_dirs=['/usr/lib', '/usr/lib/termcap'],
+                                    extra_link_args=readline_extra_link_args,
+                                    libraries=readline_libs) )
+         else:
+@@ -783,6 +783,8 @@
+             # OpenSSL doesn't do these until 0.9.8 so we'll bring our own hash
+             exts.append( Extension('_sha256', ['sha256module.c']) )
+             exts.append( Extension('_sha512', ['sha512module.c']) )
++        else:
++            open('.without_own_sha', 'w')
+ 
+         # Modules that provide persistent dictionary-like semantics.  You will
+         # probably want to arrange for at least one of them to be available on
+@@ -1120,7 +1122,7 @@
+                 sysroot = macosx_sdk_root()
+                 f = os.path.join(sysroot, f[1:])
+ 
+-        if os.path.exists(f) and not db_incs:
++        if os.path.exists(f):
+             data = open(f).read()
+             m = re.search(r"#s*define\s+HASHVERSION\s+2\s*", data)
+             if m is not None:
+@@ -1246,12 +1248,13 @@
+         # provided by the ncurses library.
+         panel_library = 'panel'
+         if curses_library.startswith('ncurses'):
+-            if curses_library == 'ncursesw':
++            if curses_library == 'xxxncursesw':
+                 # Bug 1464056: If _curses.so links with ncursesw,
+                 # _curses_panel.so must link with panelw.
+                 panel_library = 'panelw'
+             curses_libs = [curses_library]
+             exts.append( Extension('_curses', ['_cursesmodule.c'],
++                                   library_dirs = ['/usr/lib'],
+                                    libraries = curses_libs) )
+         elif curses_library == 'curses' and platform != 'darwin':
+                 # OSX has an old Berkeley curses, not good enough for
+@@ -1264,6 +1267,7 @@
+                 curses_libs = ['curses']
+ 
+             exts.append( Extension('_curses', ['_cursesmodule.c'],
++                                   library_dirs = ['/usr/lib'],
+                                    libraries = curses_libs) )
+         else:
+             missing.append('_curses')
+@@ -1432,10 +1436,22 @@
              macros = dict()
              libraries = []
  
@@ -17,8 +76,6 @@
 +            libraries = []
 +
 +        elif platform in ('freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
-+            # FreeBSD's P1003.1b semaphore support is very experimental
-+            # and has many known problems. (as of June 2008)
 +            macros = dict(                  # FreeBSD 7+
 +                HAVE_SEM_OPEN=1,
 +                HAVE_SEM_TIMEDWAIT=1,
@@ -27,3 +84,23 @@
              libraries = []
  
          elif platform.startswith('openbsd'):
+@@ -1484,7 +1500,7 @@
+             missing.append('linuxaudiodev')
+ 
+         if (platform in ('linux2', 'freebsd4', 'freebsd5', 'freebsd6',
+-                        'freebsd7', 'freebsd8')
++                        'freebsd7', 'freebsd8', 'freebsd9', 'freebsd10')
+             or platform.startswith("gnukfreebsd")):
+             exts.append( Extension('ossaudiodev', ['ossaudiodev.c']) )
+         else:
+@@ -2066,9 +2082,7 @@
+           ext_modules=[Extension('_struct', ['_struct.c'])],
+ 
+           # Scripts to install
+-          scripts = ['Tools/scripts/pydoc', 'Tools/scripts/idle',
+-                     'Tools/scripts/2to3',
+-                     'Lib/smtpd.py']
++          scripts = []
+         )
+ 
+ # --install-platlib
