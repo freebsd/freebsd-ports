39a5b214fccf0f0502bd1b6939f7eb5199da4eac
jgh <jgh@FreeBSD.org>
2013-05-02 06:36:43 +0000

    - adoption of USES for gettext
    Approved by:	portmgr (bapt@)

--- misc/kde4-l10n/files/bsd.l10n.mk
+++ misc/kde4-l10n/files/bsd.l10n.mk
@@ -6,9 +6,8 @@ DIST_SUBDIR?=	KDE/${PORTVERSION}/kde-l10n
 
 USE_XZ=		yes
 USE_QT4=	uic_build moc_build qmake_build rcc_build xml
-USE_GETTEXT=	yes
 USE_KDE4=	kdelibs kdehier kdeprefix automoc4
-USES=		cmake
+USES=		cmake gettext
 
 MAKE_JOBS_SAFE=	yes
 
