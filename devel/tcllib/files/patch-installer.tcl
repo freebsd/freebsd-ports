9a2a989f55031a78087145ca41e9d6ea95bce86b
gahr <gahr@FreeBSD.org>
2013-05-03 09:58:18 +0000

    - Add DOCS and MANPAGES options - Install man pages with a .tcllib suffix to avoid conflicts with other   ports such as devel/tcl-trf. - Bump PORTREVISION

---
+++ devel/tcllib/files/patch-installer.tcl
@@ -0,0 +1,20 @@
+--- installer.tcl.orig	2013-04-26 17:00:27.000000000 +0200
++++ installer.tcl	2013-04-26 17:00:45.000000000 +0200
+@@ -218,7 +218,7 @@
+ 
+ 	if {[file exists $aexe.man]} {
+ 	    if {$config(doc,nroff)} {
+-		_manfile $aexe.man nroff n $config(doc,nroff,path)
++		_manfile $aexe.man nroff tcllib $config(doc,nroff,path)
+ 	    }
+ 	    if {$config(doc,html)} {
+ 		_manfile $aexe.man html html $config(doc,html,path)
+@@ -247,7 +247,7 @@
+ 	xinstall   pkg $config(pkg,path)
+ 	gen_main_index $config(pkg,path) $package_name $package_version
+ 	if {$config(doc,nroff)} {
+-	    xinstall doc nroff n    $config(doc,nroff,path)
++	    xinstall doc nroff tcllib $config(doc,nroff,path)
+ 	}
+ 	if {$config(doc,html)}  {
+ 	    xinstall doc html  html $config(doc,html,path)
