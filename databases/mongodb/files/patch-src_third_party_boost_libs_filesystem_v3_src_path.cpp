3758819b39d281c6329912e13a6bcc19de040ff7
ale <ale@FreeBSD.org>
2013-05-03 14:18:14 +0000

    Really fix locale.

--- databases/mongodb/files/patch-src_third_party_boost_libs_filesystem_v3_src_path.cpp
+++ databases/mongodb/files/patch-src_third_party_boost_libs_filesystem_v3_src_path.cpp
@@ -1,5 +1,5 @@
---- src/third_party/boost/libs/filesystem/v3/src/path.cpp.orig	2013-05-01 19:03:02.000000000 +0200
-+++ src/third_party/boost/libs/filesystem/v3/src/path.cpp	2013-05-01 19:03:46.000000000 +0200
+--- src/third_party/boost/libs/filesystem/v3/src/path.cpp.orig	2013-04-22 16:48:39.000000000 +0200
++++ src/third_party/boost/libs/filesystem/v3/src/path.cpp	2013-05-03 14:46:44.000000000 +0200
 @@ -35,7 +35,7 @@
  #ifdef BOOST_WINDOWS_API
  # include "windows_file_codecvt.hpp"
@@ -18,3 +18,12 @@
  
    // "All BSD system functions expect their string parameters to be in UTF-8 encoding
    // and nothing else." See
+@@ -822,7 +822,7 @@
+   const path::codecvt_type*& path::wchar_t_codecvt_facet()
+   {
+ #   if defined(BOOST_POSIX_API) && \
+-      !(defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__))
++      !(defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__) || defined(__FreeBSD__))
+       // A local static initialized by calling path::imbue ensures that std::locale(""),
+       // which may throw, is called only if path_locale and condecvt_facet will actually
+       // be used. Thus misconfigured environmental variables will only cause an
