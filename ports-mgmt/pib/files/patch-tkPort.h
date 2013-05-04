5f0b1975daeeb89064b1dd344b58df1874c79228
eadler <eadler@FreeBSD.org>
2013-05-04 13:56:07 +0000

    Regenerate patchfiles to completely ignore OBJFORMAT which should be removed soon.

--- ports-mgmt/pib/files/patch-tkPort.h
+++ ports-mgmt/pib/files/patch-tkPort.h
@@ -1,6 +1,6 @@
---- tkPort.h.orig	Wed Apr 13 17:30:58 2005
-+++ tkPort.h	Wed Apr 13 17:30:13 2005
-@@ -133,8 +133,6 @@ extern int errno;
+--- ./tkPort.h.orig	1996-12-17 05:05:30.000000000 -0500
++++ ./tkPort.h	2013-05-04 09:54:01.114260299 -0400
+@@ -133,8 +133,6 @@
   * in any other header file.
   */
  
@@ -9,4 +9,3 @@
  /*
   * These functions do nothing under Unix, so we just eliminate calls them.
   */
-
