2dc386f60d791075fb5aae8c4b56a16f201c1b99
madpilot <madpilot@FreeBSD.org>
2013-05-06 21:56:18 +0000

    - Take advantage of accept4() in recent CURRENT [1] - Fix plist when DOXYGEN option is not enabled
    Suggested by:	jilles [1]

--- comms/openobex/files/patch-lib_cloexec.h
+++ comms/openobex/files/patch-lib_cloexec.h
@@ -1,5 +1,5 @@
 --- lib/cloexec.h.orig	2012-03-26 20:25:19.000000000 +0200
-+++ lib/cloexec.h	2013-04-11 16:12:55.161482749 +0200
++++ lib/cloexec.h	2013-05-06 23:36:45.087537539 +0200
 @@ -22,6 +22,7 @@
  #ifndef _WIN32
  #include <sys/types.h> 
@@ -13,7 +13,7 @@
  				      socklen_t *addrlen)
  {
 -#ifdef SOCK_CLOEXEC
-+#if defined(SOCK_CLOEXEC) && !defined(__FreeBSD__)
++#if defined(SOCK_CLOEXEC) && __FreeBSD_version < 1000032
  	return accept4(sockfd, addr, addrlen, SOCK_CLOEXEC);
  #else
  	socket_t fd = accept(sockfd, addr, addrlen);
