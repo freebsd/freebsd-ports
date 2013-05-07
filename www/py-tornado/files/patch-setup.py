784afb5487648dc5a9f221ed54df59ce248edf59
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2013-05-07 07:18:48 +0000

    - update to 3.0.1 - change USE_PYTHON to ``yes'' (both python branches are supported) - depend on py-curl only if python2 is used (it doesn't work with python3)
    PR:		178234
    Submitted by:	gblach
    Approved by:	Guixing <khsing.cn@gmail.com> (maintainer)

--- www/py-tornado/files/patch-setup.py
+++ www/py-tornado/files/patch-setup.py
@@ -1,21 +1,12 @@
---- ./setup.py.orig	2012-09-04 15:09:29.000000000 +1000
-+++ ./setup.py	2012-11-19 19:24:33.000000000 +1100
-@@ -25,6 +25,8 @@
- 
- kwargs = {}
- 
-+kwargs["zip_safe"] = False
-+
- # Build the epoll extension for Linux systems with Python < 2.6
- extensions = []
- major, minor = sys.version_info[:2]
-@@ -54,9 +56,6 @@
-             "test.key",
-             "static/robots.txt",
-             "templates/utf8.html",
+--- setup.py.orig	2013-04-29 08:46:17.852783104 +0200
++++ setup.py	2013-04-29 08:46:30.135662912 +0200
+@@ -41,9 +41,6 @@
+         # in the sdist tarball)
+         "tornado.test": [
+             "README",
 -            "csv_translations/fr_FR.csv",
 -            "gettext_translations/fr_FR/LC_MESSAGES/tornado_test.mo",
 -            "gettext_translations/fr_FR/LC_MESSAGES/tornado_test.po",
-             ],
-         },
-     ext_modules = extensions,
+             "options_test.cfg",
+             "static/robots.txt",
+             "templates/utf8.html",
