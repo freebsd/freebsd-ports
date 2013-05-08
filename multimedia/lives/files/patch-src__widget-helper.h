757ade45c405afbd8cf9239d2a91d2e4b91ebae6
pawel <pawel@FreeBSD.org>
2013-05-08 18:01:24 +0000

    Update to version 2.0.4

---
+++ multimedia/lives/files/patch-src__widget-helper.h
@@ -0,0 +1,10 @@
+--- src/widget-helper.h.orig	2013-05-08 16:51:04.000000000 +0200
++++ src/widget-helper.h	2013-05-08 16:54:33.000000000 +0200
+@@ -82,7 +82,6 @@
+ #define LIVES_ORIENTATION_VERTICAL   GTK_ORIENTATION_VERTICAL
+ 
+ typedef GdkEvent                          LiVESXEvent;
+-typedef GdkEventButton                    LiVESXEventButton;
+ typedef GdkDisplay                        LiVESXDisplay;
+ typedef GdkScreen                         LiVESXScreen;
+ typedef GdkDevice                         LiVESXDevice;
