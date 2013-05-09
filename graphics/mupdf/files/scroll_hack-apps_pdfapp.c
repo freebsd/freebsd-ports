c81459f6143f48dfa7c73904f8dfe6f7a65d863e
madpilot <madpilot@FreeBSD.org>
2013-05-09 11:44:36 +0000

    - Update to 1.2 - Add javascript support
    PR:		ports/178371
    Submitted by:	Zhihao Yuan <lichray@gmail.com> (maintainer)

--- graphics/mupdf/files/scroll_hack-apps_pdfapp.c
+++ graphics/mupdf/files/scroll_hack-apps_pdfapp.c
@@ -1,22 +1,22 @@
---- apps/pdfapp.c.orig	2012-03-29 05:46:53.000000000 -0500
-+++ apps/pdfapp.c	2012-04-07 16:43:55.030462608 -0500
-@@ -774,11 +774,15 @@ void pdfapp_onkey(pdfapp_t *app, int c)
+--- apps/pdfapp.c.orig	2013-02-26 12:07:15.000000000 -0500
++++ apps/pdfapp.c	2013-05-06 11:13:27.210765076 -0400
+@@ -1142,11 +1142,15 @@ void pdfapp_onkey(pdfapp_t *app, int c)
  		break;
  
  	case 'j':
 + 		if (app->pany + fz_pixmap_height(app->ctx, app->image) <= app->winh)
 + 			goto pagedown;
  		app->pany -= fz_pixmap_height(app->ctx, app->image) / 10;
- 		pdfapp_showpage(app, 0, 0, 1);
+ 		pdfapp_showpage(app, 0, 0, 1, 0);
  		break;
  
  	case 'k':
 + 		if (app->pany >= 0)
 + 			goto pageup;
  		app->pany += fz_pixmap_height(app->ctx, app->image) / 10;
- 		pdfapp_showpage(app, 0, 0, 1);
+ 		pdfapp_showpage(app, 0, 0, 1, 0);
  		break;
-@@ -842,6 +846,7 @@ void pdfapp_onkey(pdfapp_t *app, int c)
+@@ -1214,6 +1218,7 @@ void pdfapp_onkey(pdfapp_t *app, int c)
  	 */
  
  	case ',':
@@ -24,7 +24,7 @@
  		panto = PAN_TO_BOTTOM;
  		if (app->numberlen > 0)
  			app->pageno -= atoi(app->number);
-@@ -850,6 +855,7 @@ void pdfapp_onkey(pdfapp_t *app, int c)
+@@ -1222,6 +1227,7 @@ void pdfapp_onkey(pdfapp_t *app, int c)
  		break;
  
  	case '.':
@@ -32,7 +32,7 @@
  		panto = PAN_TO_TOP;
  		if (app->numberlen > 0)
  			app->pageno += atoi(app->number);
-@@ -1039,6 +1045,12 @@ void pdfapp_onmouse(pdfapp_t *app, int x
+@@ -1523,6 +1529,12 @@ void pdfapp_onmouse(pdfapp_t *app, int x
  				int isx = (modifiers & (1<<0));
  				int xstep = isx ? 20 * dir : 0;
  				int ystep = !isx ? 20 * dir : 0;
