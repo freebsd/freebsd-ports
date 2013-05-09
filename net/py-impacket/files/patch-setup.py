d383c1b389628ce16f3bbacdc963961c2e5eb7e1
antoine <antoine@FreeBSD.org>
2013-05-09 11:07:26 +0000

    Update py-impacket to 0.9.10
    Approved by:	miwi (mentor)

--- net/py-impacket/files/patch-setup.py
+++ net/py-impacket/files/patch-setup.py
@@ -1,9 +1,10 @@
---- ./setup.py.orig	2013-04-28 18:06:57.000000000 +0000
-+++ ./setup.py	2013-04-28 18:07:08.000000000 +0000
-@@ -18,6 +18,4 @@
-       maintainer_email = "bethus@gmail.com",
+--- ./setup.py.orig	2013-05-08 13:57:03.000000000 +0000
++++ ./setup.py	2013-05-08 13:57:03.000000000 +0000
+@@ -21,7 +21,5 @@
+       platforms = ["Unix","Windows"],
        packages = ['impacket', 'impacket.dcerpc', 'impacket.examples'],
        scripts = glob.glob(os.path.join('examples', '*.py')),
 -      data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
 -                     ['README', 'LICENSE']+glob.glob('doc/*'))],
+ 
        )
