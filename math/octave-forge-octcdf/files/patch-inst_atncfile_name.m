6f403859beed7200be9c927287529252fdc48da7
Stephen Montgomery-Smith <stephen@FreeBSD.org>
2013-05-10 02:13:17 +0000

    - Update to 1.1.6.

--- math/octave-forge-octcdf/files/patch-inst_atncfile_name.m
+++
@@ -1,7 +0,0 @@
---- ../inst/@ncfile/name.m	2012-07-06 00:05:01.000000000 +0000
-+++ ../inst/@ncfile/name.m-orig	2012-07-06 00:04:39.000000000 +0000
-@@ -1,3 +1,3 @@
- function n = name(self)
-- n = ncname(self)
-+ n = ncname(self);
- end
