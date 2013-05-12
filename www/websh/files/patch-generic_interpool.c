def410daddcecc65d5d620e42dded70881fa9aa7
Mikhail Teterin <mi@FreeBSD.org>
2013-05-12 19:08:49 +0000

    Add some more patches to satisfy increasingly picky compiler(s).
    Notified by:	itetcu

--- www/websh/files/patch-generic_interpool.c
+++ www/websh/files/patch-generic_interpool.c
@@ -1,11 +1,29 @@
---- ../generic/interpool.c.orig	2013-02-05 13:59:15.000000000 +0100
-+++ ../generic/interpool.c	2013-02-05 14:00:59.000000000 +0100
-@@ -573,7 +573,7 @@
- 			  Tcl_GetStringResult(conf->mainInterp));
+--- ../generic/interpool.c	2009-07-15 08:15:16.000000000 -0400
++++ ../generic/interpool.c	2013-05-12 15:00:06.000000000 -0400
+@@ -108,5 +108,4 @@
+ {
+ 
+-    int result = 0;
+     LogPlugIn *logtoap = NULL;
+     Tcl_Obj *code = NULL;
+@@ -135,5 +134,5 @@
+ 
+     /* now register here all websh modules */
+-    result = Tcl_Init(webInterp->interp);
++    Tcl_Init(webInterp->interp);
+     /* checkme: test result */
+ 
+@@ -143,5 +142,5 @@
+     Tcl_SetAssocData(webInterp->interp, WEB_APFUNCS_ASSOC_DATA, NULL, (ClientData *) apFuncs);
+ 
+-    result = Websh_Init(webInterp->interp);
++    Websh_Init(webInterp->interp);
+ 
+     /* also register the destrcutor, etc. functions, passing webInterp as
+@@ -574,5 +573,5 @@
  #else /* APACHE2 */
  	    ap_log_error(APLOG_MARK, APLOG_NOERRNO | APLOG_ERR, 0,
 -			 conf->server, Tcl_GetStringResult(conf->mainInterp));
 +			 conf->server, "%s", Tcl_GetStringResult(conf->mainInterp));
  #endif /* APACHE2 */
  	    return 0;
- 	}
