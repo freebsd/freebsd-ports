45b8e391140502b1df6ae9f66367636225bbc02a
mi <mi@FreeBSD.org>
2013-05-13 21:12:52 +0000

    Add a patch to allow building with gcc-4.7 and 4.8.
    Approved by:	maintainer

---
+++ converters/recode/files/patch-src::recodext.h
@@ -0,0 +1,9 @@
+--- src/recodext.h	2001-01-04 09:36:54.000000000 -0500
++++ src/recodext.h	2013-05-13 16:14:46.000000000 -0400
+@@ -219,5 +219,5 @@
+ 
+     /* Non zero if this one should be ignored.  */
+-    bool ignore : 2;
++    bool ignore : 1;
+   };
+ 
