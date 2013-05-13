a13d23f6fc89151710d7a04d83fe49126b4e85ec
Pietro Cerutti <gahr@FreeBSD.org>
2013-05-13 07:35:58 +0000

    - Fix build with Tcl/Tk 86 - While at it, trim Makefile header and s/PORTDOCS/PORT_OPTIONS:MDOCS/
    Reported by:	miwi (Tcl/Tk 86 exp-run)

--- devel/tclcl/files/patch-Tcl.cc
+++ devel/tclcl/files/patch-Tcl.cc
@@ -1,5 +1,14 @@
---- Tcl.cc.orig	Sun Apr 29 16:21:14 2007
-+++ Tcl.cc	Sun Apr 29 16:21:26 2007
+--- Tcl.cc.orig	2007-02-04 02:46:43.000000000 +0100
++++ Tcl.cc	2013-05-13 09:19:37.000000000 +0200
+@@ -213,7 +213,7 @@
+ 	if (strlen(s) > MAX_CODE_TO_DUMP) {
+ 		s = "\n[code omitted because of length]\n";
+ 	};
+-	fprintf(stderr, "%s: \"%s\": %s\n", application_, s, tcl_->result);
++	fprintf(stderr, "%s: \"%s\": %s\n", application_, s, Tcl_GetStringResult(tcl_));
+ 	exit(1);
+ }
+ 
 @@ -546,7 +546,7 @@
  			need_parse = 1;
  			continue;
