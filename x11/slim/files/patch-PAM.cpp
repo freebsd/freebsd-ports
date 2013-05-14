95fd99cf25645eac3097ddfc392c5f4575a21ee2
rakuco <rakuco@FreeBSD.org>
2013-05-14 21:34:12 +0000

    - Update to 1.3.5.
    PR:		ports/177857
    Submitted by:	Kevin Zheng <kevinz5000@gmail.com>
    Approved by:	Henry Hu <henry.hu.sh@gmail.com> (maintainer)

--- x11/slim/files/patch-PAM.cpp
+++ x11/slim/files/patch-PAM.cpp
@@ -1,10 +1,10 @@
---- PAM.cpp.orig	2012-06-26 12:20:14.000000000 +0400
-+++ PAM.cpp	2012-12-16 16:05:09.000000000 +0400
-@@ -128,6 +128,7 @@
-             case PAM_MAXTRIES:
-             case PAM_CRED_INSUFFICIENT:
-             case PAM_AUTH_ERR:
-+            case PAM_CONV_ERR:
-                 throw Auth_Exception(pam_handle, "pam_authentication()", last_result);
+--- ./PAM.cpp.orig	2012-12-31 07:03:42.000000000 -0600
++++ ./PAM.cpp	2013-03-23 14:10:35.000000000 -0500
+@@ -127,6 +127,7 @@
+ 			case PAM_MAXTRIES:
+ 			case PAM_CRED_INSUFFICIENT:
+ 			case PAM_AUTH_ERR:
++			case PAM_CONV_ERR:
+ 				throw Auth_Exception(pam_handle, "pam_authentication()", last_result);
  
-             case PAM_SUCCESS:
+ 			case PAM_SUCCESS:
--- x11/slim/files/patch-app.cpp
+++ x11/slim/files/patch-app.cpp
@@ -1,11 +1,11 @@
---- app.cpp.orig	2008-10-04 13:45:58.000000000 +0200
-+++ app.cpp	2008-10-04 13:46:07.000000000 +0200
-@@ -887,7 +887,7 @@
-     }
+--- ./app.cpp.orig	2012-12-31 07:03:42.000000000 -0600
++++ ./app.cpp	2013-03-23 14:10:35.000000000 -0500
+@@ -931,7 +931,7 @@
+ 	}
  
-     if (!hasVtSet && daemonmode) {
--        server[argc++] = (char*)"vt07";
-+        server[argc++] = (char*)"vt09";
-     }
-     server[argc] = NULL;
+ 	if (!hasVtSet && daemonmode) {
+-		server[argc++] = (char*)"vt07";
++		server[argc++] = (char*)"vt09";
+ 	}
+ 	server[argc] = NULL;
  
