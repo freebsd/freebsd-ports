07160c50c1e6e46392eb286ce4f393fe3acd3783
gahr <gahr@FreeBSD.org>
2013-05-14 09:28:58 +0000

    - Fix build
    Reported by:	miwi (Tcl/Tk 86 exp-run)

---
+++ audio/tclmidi/files/patch-TclmIntp.h
@@ -0,0 +1,9 @@
+--- TclmIntp.h.orig	2013-05-14 11:24:39.000000000 +0200
++++ TclmIntp.h	2013-05-14 11:25:36.000000000 +0200
+@@ -64,6 +64,5 @@
+ 	int current_song;
+ 	int current_dev;
+ 	int current_patch;
+-	Event *next_event;
+ };
+ #endif
