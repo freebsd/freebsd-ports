2c239f1248c9bd7072d56033ec9c7bda70e65c6f
gahr <gahr@FreeBSD.org>
2013-05-14 09:04:59 +0000

    - Fix build with Tcl 8.6 [1] - Trim Makefile header - Remove article from COMMENT
    Reported by:	miwi (Tcl/Tk 86 exp-run) [1]

--- irc/quirc/files/patch-tcltk.cc
+++ irc/quirc/files/patch-tcltk.cc
@@ -1,5 +1,5 @@
---- tcltk.cc.orig	Mon Jan 12 18:25:11 2004
-+++ tcltk.cc	Tue Feb  3 23:18:41 2004
+--- tcltk.cc.orig	2004-01-12 10:25:11.000000000 +0100
++++ tcltk.cc	2013-05-14 10:57:51.000000000 +0200
 @@ -1,6 +1,7 @@
  #include <stdlib.h>
  #include <stdarg.h>
@@ -8,3 +8,21 @@
  
  #ifdef DMALLOC
  #include <dmalloc.h>
+@@ -33,7 +34,7 @@
+ #endif
+ 
+   if (Tcl_Init(TT_Interp) == TCL_ERROR) { 
+-    fprintf(stderr,"Error initializing Tcl:\n%s\n",TT_Interp->result);
++    fprintf(stderr,"Error initializing Tcl:\n%s\n",Tcl_GetStringResult(TT_Interp));
+     return TCL_ERROR;
+   }
+ 
+@@ -43,7 +44,7 @@
+   // uses Tk_Init to process its command-line arguments).
+ 
+   if (Tk_Init(TT_Interp) == TCL_ERROR) {
+-    fprintf(stderr,"Error initializing Tk:\n%s\n",TT_Interp->result);
++    fprintf(stderr,"Error initializing Tk:\n%s\n",Tcl_GetStringResult(TT_Interp));
+     return TCL_ERROR;
+   }
+ 
