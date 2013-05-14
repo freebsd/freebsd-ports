09482620ecc66ae06f79ae63f4059f672bb13fdc
gahr <gahr@FreeBSD.org>
2013-05-14 14:34:33 +0000

    - Fix run-time with Tcl/Tk 8.6 - Remove INVALID_TK_VER= 86

--- cad/tkgate/files/patch-src-tkgate_tkgate.c
+++ cad/tkgate/files/patch-src-tkgate_tkgate.c
@@ -1,5 +1,5 @@
---- src/tkgate/tkgate.c.orig	2013-05-14 15:24:54.000000000 +0200
-+++ src/tkgate/tkgate.c	2013-05-14 15:25:15.000000000 +0200
+--- src/tkgate/tkgate.c.orig	2009-03-18 10:23:18.000000000 +0100
++++ src/tkgate/tkgate.c	2013-05-14 16:31:17.000000000 +0200
 @@ -1034,7 +1034,7 @@
     */
    r = Tk_Init(tcl);
@@ -9,3 +9,11 @@
      fprintf(stderr,"Perhaps you could try setting the environment variable TK_LIBRARY\n");
      fprintf(stderr,"to the directory in which tk init files can be found.  You can also\n");
      fprintf(stderr,"set TK_LIBRARY in options.h.\n");
+@@ -1097,6 +1097,7 @@
+  *****************************************************************************/
+ int Tkgate_Init(Tcl_Interp *tcl)
+ {
++  Tk_Init(tcl);
+   Tcl_CreateCommand(tcl,"tkgate_main",tkgate_main,0,0);
+   Tcl_CreateCommand(tcl,"tkgate_starttk",tkgate_starttk,0,0);
+   Tcl_CreateCommand(tcl,"tkgate_posttk",tkgate_posttk,0,0);
