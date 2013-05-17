11bf569497858a71d84e85105a6248a789725bc8
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2013-05-17 04:17:22 +0000

    - update to latest upstream versions:   . lang/python27: 2.7.3 -> 2.7.5   . lang/python32: 3.2.3 -> 3.2.4   . lang/python33: 3.3.0 -> 3.3.1 - update Mk/bsd.python.mk with new versions - mark lang/python26 and lang/python31 as deprecated (set them to   upstream EoL dates) - update docs (lang/python-doc-html) - align databases/py-bsddb patch for python27 - most of it was applied   upstream. Raise BDB version to 4.3 atleast, according to   upstream requirements.
    Many thanks to Martin (miwi) for his time on this update.
    
    PR:		178506
    Submitted by:	rm (myself)
    Exp-run by:	    portmgr (miwi)
    
    - revert erroneous threads patch in lang/python26 and lang/python27,
      that was added after ports/131080. It was rejected upstream, because it's
      not actually a bug, but misuse.
    
      Gabor Pali (pgj) in collaboration with Kubilay Kocak (koobs) did an
      independent investigation regard the issue. See here for details:
    
      http://lists.freebsd.org/pipermail/freebsd-python/2013-April/005376.html
    
    PR:	    153167
    Submitted by:	Duncan Findlay <duncan@duncf.ca>
    Reported by:	pgj/koobs (at python@ ML)
    Exp-run by:     portmgr (miwi)

--- lang/python27/files/patch-setup.py
+++ lang/python27/files/patch-setup.py
@@ -1,7 +1,7 @@
---- setup.py.orig	2011-06-11 17:46:28.000000000 +0200
-+++ setup.py	2011-10-18 12:05:34.000000000 +0200
-@@ -21,7 +21,7 @@
- COMPILED_WITH_PYDEBUG = hasattr(sys, 'gettotalrefcount')
+--- setup.py.orig	2013-04-06 18:02:41.000000000 +0400
++++ setup.py	2013-04-07 10:52:47.000000000 +0400
+@@ -33,7 +33,7 @@
+ COMPILED_WITH_PYDEBUG = ('--with-pydebug' in sysconfig.get_config_var("CONFIG_ARGS"))
  
  # This global variable is used to hold the list of modules to be disabled.
 -disabled_module_list = []
@@ -9,7 +9,7 @@
  
  def add_dir_to_list(dirlist, dir):
      """Add the directory 'dir' to the list 'dirlist' (at the front) if
-@@ -642,7 +642,7 @@
+@@ -720,7 +720,7 @@
          # use the same library for the readline and curses modules.
          if 'curses' in readline_termcap_library:
              curses_library = readline_termcap_library
@@ -18,16 +18,16 @@
              curses_library = 'ncursesw'
          elif self.compiler.find_library_file(lib_dirs, 'ncurses'):
              curses_library = 'ncurses'
-@@ -681,7 +681,7 @@
+@@ -755,7 +755,7 @@
+             elif curses_library:
+                 readline_libs.append(curses_library)
+             elif self.compiler.find_library_file(lib_dirs +
+-                                                     ['/usr/lib/termcap'],
++                                                     ['/usr/lib', '/usr/lib/termcap'],
                                                       'termcap'):
                  readline_libs.append('termcap')
              exts.append( Extension('readline', ['readline.c'],
--                                   library_dirs=['/usr/lib/termcap'],
-+                                   library_dirs=['/usr/lib', '/usr/lib/termcap'],
-                                    extra_link_args=readline_extra_link_args,
-                                    libraries=readline_libs) )
-         else:
-@@ -783,6 +783,8 @@
+@@ -862,6 +862,8 @@
              # OpenSSL doesn't do these until 0.9.8 so we'll bring our own hash
              exts.append( Extension('_sha256', ['sha256module.c']) )
              exts.append( Extension('_sha512', ['sha512module.c']) )
@@ -36,7 +36,7 @@
  
          # Modules that provide persistent dictionary-like semantics.  You will
          # probably want to arrange for at least one of them to be available on
-@@ -1120,7 +1122,7 @@
+@@ -1208,7 +1210,7 @@
                  sysroot = macosx_sdk_root()
                  f = os.path.join(sysroot, f[1:])
  
@@ -45,7 +45,7 @@
              data = open(f).read()
              m = re.search(r"#s*define\s+HASHVERSION\s+2\s*", data)
              if m is not None:
-@@ -1246,12 +1248,13 @@
+@@ -1338,12 +1340,13 @@
          # provided by the ncurses library.
          panel_library = 'panel'
          if curses_library.startswith('ncurses'):
@@ -58,9 +58,9 @@
              exts.append( Extension('_curses', ['_cursesmodule.c'],
 +                                   library_dirs = ['/usr/lib'],
                                     libraries = curses_libs) )
-         elif curses_library == 'curses' and platform != 'darwin':
+         elif curses_library == 'curses' and host_platform != 'darwin':
                  # OSX has an old Berkeley curses, not good enough for
-@@ -1264,6 +1267,7 @@
+@@ -1356,6 +1359,7 @@
                  curses_libs = ['curses']
  
              exts.append( Extension('_curses', ['_cursesmodule.c'],
@@ -68,25 +68,25 @@
                                     libraries = curses_libs) )
          else:
              missing.append('_curses')
-@@ -1432,7 +1436,7 @@
+@@ -1540,7 +1544,7 @@
              macros = dict()
              libraries = []
  
--        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
-+        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
+-        elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
++        elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
              # FreeBSD's P1003.1b semaphore support is very experimental
              # and has many known problems. (as of June 2008)
              macros = dict()
-@@ -1484,7 +1488,7 @@
+@@ -1592,7 +1596,7 @@
              missing.append('linuxaudiodev')
  
-         if (platform in ('linux2', 'freebsd4', 'freebsd5', 'freebsd6',
+         if (host_platform in ('linux2', 'freebsd4', 'freebsd5', 'freebsd6',
 -                        'freebsd7', 'freebsd8')
 +                        'freebsd7', 'freebsd8', 'freebsd9', 'freebsd10')
-             or platform.startswith("gnukfreebsd")):
+             or host_platform.startswith("gnukfreebsd")):
              exts.append( Extension('ossaudiodev', ['ossaudiodev.c']) )
          else:
-@@ -2066,9 +2070,7 @@
+@@ -2176,9 +2180,7 @@
            ext_modules=[Extension('_struct', ['_struct.c'])],
  
            # Scripts to install
