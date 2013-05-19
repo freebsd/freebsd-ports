22c59078157cad892ac3e12960fab7c70ace5dc7
mva <mva@FreeBSD.org>
2013-05-19 12:36:00 +0000

    - Update to 2.67 - Reorganise options to better match the package's option names

--- graphics/blender/files/patch-intern_ffmpeg_compat.h
+++ graphics/blender/files/patch-intern_ffmpeg_compat.h
@@ -1,44 +1,44 @@
---- intern/ffmpeg/ffmpeg_compat.h.orig	2013-03-06 17:47:13.000000000 +0100
-+++ intern/ffmpeg/ffmpeg_compat.h	2013-03-06 17:49:33.000000000 +0100
-@@ -113,14 +113,14 @@
+--- intern/ffmpeg/ffmpeg_compat.h.orig	2013-05-09 10:51:58.000000000 +0200
++++ intern/ffmpeg/ffmpeg_compat.h	2013-05-09 10:53:27.000000000 +0200
+@@ -179,14 +179,14 @@
  #define avformat_close_input(x) av_close_input_file(*(x))
  #endif
  
 -#if ((LIBAVCODEC_VERSION_MAJOR < 53) || (LIBAVCODEC_VERSION_MAJOR == 53 && LIBAVCODEC_VERSION_MINOR < 35))
--static inline
+-FFMPEG_INLINE
 -int avcodec_open2(AVCodecContext *avctx, AVCodec *codec, AVDictionary **options)
 -{
 -	/* TODO: no options are taking into account */
 -	return avcodec_open(avctx, codec);
 -}
 -#endif
-+// #if ((LIBAVCODEC_VERSION_MAJOR < 53) || (LIBAVCODEC_VERSION_MAJOR == 53 && LIBAVCODEC_VERSION_MINOR < 35))
-+// static inline
-+// int avcodec_open2(AVCodecContext *avctx, AVCodec *codec, AVDictionary **options)
-+// {
-+// 	/* TODO: no options are taking into account */
-+// 	return avcodec_open(avctx, codec);
-+// }
-+// #endif
++//#if ((LIBAVCODEC_VERSION_MAJOR < 53) || (LIBAVCODEC_VERSION_MAJOR == 53 && LIBAVCODEC_VERSION_MINOR < 35))
++//FFMPEG_INLINE
++//int avcodec_open2(AVCodecContext *avctx, AVCodec *codec, AVDictionary **options)
++//{
++//	/* TODO: no options are taking into account */
++//	return avcodec_open(avctx, codec);
++//}
++//#endif
  
  #if ((LIBAVFORMAT_VERSION_MAJOR < 53) || (LIBAVFORMAT_VERSION_MAJOR == 53 && LIBAVFORMAT_VERSION_MINOR < 21))
- static inline
-@@ -130,12 +130,12 @@
+ FFMPEG_INLINE
+@@ -196,12 +196,12 @@
  	return av_new_stream(s, 0);
  }
  
--static inline
+-FFMPEG_INLINE
 -int avformat_find_stream_info(AVFormatContext *ic, AVDictionary **options)
 -{
 -	/* TODO: no options are taking into account */
 -	return av_find_stream_info(ic);
 -}
-+// static inline
-+// int avformat_find_stream_info(AVFormatContext *ic, AVDictionary **options)
-+// {
-+// 	/* TODO: no options are taking into account */
-+// 	return av_find_stream_info(ic);
-+// }
++//FFMPEG_INLINE
++//int avformat_find_stream_info(AVFormatContext *ic, AVDictionary **options)
++//{
++//	/* TODO: no options are taking into account */
++//	return av_find_stream_info(ic);
++//}
  #endif
  
  #if ((LIBAVFORMAT_VERSION_MAJOR > 53) || ((LIBAVFORMAT_VERSION_MAJOR == 53) && (LIBAVFORMAT_VERSION_MINOR > 32)) || ((LIBAVFORMAT_VERSION_MAJOR == 53) && (LIBAVFORMAT_VERSION_MINOR == 24) && (LIBAVFORMAT_VERSION_MICRO >= 100)))
