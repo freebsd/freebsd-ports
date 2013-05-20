81817b7ee24d0908a14757bd568e0e304bbb8dfa
dbn <dbn@FreeBSD.org>
2013-05-20 21:13:01 +0000

    Update lang/pypy to 2.0.1.
    Port ChangeLog:
     * Sqlite3 added as a dependency
     * DIST_SUBDIR no longer used as upstream now releases with a proper tarball
     * Added ability to translate with pypy running in restricted memory mode
       ( faster than python2.7 and uses less memory!)
     * Added support for pypy modules that use cffi (_sqlite3 and _curses)
    
    Approved by:	eadler,bdrewery (mentors, implicit)

--- lang/pypy/files/patch-pypy__module__sys__initpath.py
+++ lang/pypy/files/patch-pypy__module__sys__initpath.py
@@ -1,6 +1,6 @@
 --- pypy/module/sys/initpath.py.orig	2012-11-27 18:15:02.000000000 +0200
 +++ pypy/module/sys/initpath.py	2012-11-27 18:19:45.000000000 +0200
-@@ -91,14 +91,13 @@
+@@ -90,14 +90,13 @@
      least contain a directory called ``lib-python/X.Y`` and another one called
      ``lib_pypy``. If they cannot be found, it raises OSError.
      """
@@ -12,10 +12,10 @@
 +    dirname = 'pypy%d.%d' % PYPY_VERSION[:2]
 +    lib_python = os.path.join(prefix, 'lib')
      python_std_lib = os.path.join(lib_python, dirname)
-     checkdir(python_std_lib)
-     
+     _checkdir(python_std_lib)
+ 
 -    lib_pypy = os.path.join(prefix, 'lib_pypy')
 +    lib_pypy = os.path.join(python_std_lib, 'lib_pypy')
-     checkdir(lib_pypy)
+     _checkdir(lib_pypy)
  
      importlist = []
