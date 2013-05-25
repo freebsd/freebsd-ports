5db4819e6beff78f3850e374e169930feb5001a4
tota <tota@FreeBSD.org>
2013-05-25 14:41:31 +0000

    - Update to 1.9.7

--- www/testlink/files/patch-install-index.php
+++ www/testlink/files/patch-install-index.php
@@ -1,15 +1,15 @@
---- install/index.php.orig	2012-12-08 23:09:18.000000000 +0900
-+++ install/index.php	2012-12-09 16:54:13.000000000 +0900
-@@ -39,7 +39,7 @@
- 		<p>Please read Section on README file or go to www.teamst.org (Forum: TestLink 1.9.5 News,changes, etc) </p>
- 		<p>Open <a target="_blank" href="../docs/testlink_installation_manual.pdf">Installation manual</a>
- 		for more information or troubleshooting. You could also look at
--		<a href="../README">README</a> or <a href="../CHANGELOG">Changes Log</a>.
-+		<a href="../docs/README">README</a> or <a href="../docs/CHANGELOG">Changes Log</a>.
- 		You are welcome to visit our <a target="_blank" href="http://www.teamst.org">
- 		forum</a> to browse or discuss.
- 		</p>
-@@ -51,4 +51,4 @@
+--- install/index.php.orig	2013-05-22 03:52:53.000000000 +0900
++++ install/index.php	2013-05-22 22:10:33.000000000 +0900
+@@ -44,7 +44,7 @@
+     <p>Please read Section on README file or go to www.teamst.org (Forum: TestLink 1.9.4 and greater News,changes, etc) </p>
+     <p>Open <a target="_blank" href="../docs/testlink_installation_manual.pdf">Installation manual</a>
+     for more information or troubleshooting. You could also look at
+-    <a href="../README">README</a> or <a href="../CHANGELOG">Changes Log</a>.
++    <a href="../docs/README">README</a> or <a href="../docs/CHANGELOG">Changes Log</a>.
+     You are welcome to visit our <a target="_blank" href="http://www.teamst.org">
+     forum</a> to browse or discuss.
+     </p>
+@@ -56,4 +56,4 @@
  
  </div>
  </body>
