e9277ce26f2cfc54202645e69247d77276f4fde5
Boris Samorodov <bsam@FreeBSD.org>
2013-05-26 15:32:09 +0000

    . update to version 3.2; . trim Makefile header; . use new MASTER_SITES (SF); . refine COMMENT a little; . incorporate USES=iconv (which is needed since 3.0); . optionify using DOCS; . mute mkdir; . use utmp.h -> utmpx.h for OSVESION>900007.

--- sysutils/mapchan/files/patch-mapchan.c
+++ sysutils/mapchan/files/patch-mapchan.c
@@ -1,6 +1,19 @@
---- mapchan.c.orig	Sat Oct 25 09:21:42 2003
-+++ mapchan.c	Sun Feb 27 15:27:01 2005
-@@ -19,7 +19,7 @@
+--- mapchan.c.orig	2008-09-12 01:47:59.000000000 +0500
++++ mapchan.c	2013-05-26 14:55:43.000000000 +0400
+@@ -15,7 +15,12 @@
+ #include <stdlib.h>
+ #include <signal.h>
+ #include <libgen.h>
++#ifdef __FreeBSD__
++#include <libutil.h>
++#include <sys/ioctl.h>
++#else
+ #include <pty.h>
++#endif
+ #include <utmp.h>
+ #ifdef SYSLOG
+ #include <syslog.h>
+@@ -50,7 +55,7 @@ char *DEFAULT_LOGFILE = "mapchan.log";
  #endif
  
  #ifndef CPATH
@@ -8,4 +21,4 @@
 +  #define CPATH "/usr/local/etc/mapchan"
  #endif
  
- #define BUFSIZE 4096
+ #define BUFSIZE 32*1024
