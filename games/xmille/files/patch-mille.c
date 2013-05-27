1d7185ca93f6aaa2331bc39d8b25c4d204a1187f
Antoine Brodin <antoine@FreeBSD.org>
2013-05-27 17:49:44 +0000

    - Randomize starting player [1] - Trim Makefile header - Unbreak with clang using -Wno-error=return-type  (this is early K&R code) - Respect PREFIX
    PR:		ports/171792 [1]
    Submitted by:	David Wolfskill [1]
    Approved by:	eadler (mentor)

--- games/xmille/files/patch-mille.c
+++ games/xmille/files/patch-mille.c
@@ -1,5 +1,5 @@
---- mille.c.orig	Thu Mar 28 14:29:31 1996
-+++ mille.c	Fri Jun 15 02:27:49 2007
+--- ./mille.c.orig	1996-03-28 06:29:31.000000000 +0100
++++ ./mille.c	2013-05-26 18:05:50.000000000 +0200
 @@ -1,16 +1,17 @@
  # include	"mille.h"
  # include	<signal.h>
@@ -21,6 +21,33 @@
  
  main(ac, av)
  reg int		ac;
+@@ -49,8 +50,6 @@
+ 		/* NOTREACHED */
+ 	}
+ 	setbuf(stdout, _sobuf);
+-	Play = PLAYER;
+-	init_ui ();
+ # ifndef PROF
+ #ifdef sun
+ 	srandom(getpid());
+@@ -60,6 +59,8 @@
+ # else
+ 	srand(0);
+ # endif
++	Play = random()&01 ? COMP : PLAYER;
++	init_ui();
+ 	signal(SIGINT, rub);
+ 	for (;;) {
+ 		if (!restore || (Player[PLAYER].total >= 5000
+@@ -73,7 +74,7 @@
+ 		}
+ 		do {
+ 			if (!restore)
+-				Handstart = Play = other(Handstart);
++				Handstart = other(Handstart);
+ 			if (!restore || On_exit) {
+ 				shuffle();
+ 				init();
 @@ -123,9 +124,9 @@
   *	Routine to trap rubouts, and make sure they really want to
   * quit.
