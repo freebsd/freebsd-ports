8f6d9298531706f8d772ea049468b80b6bbd62d5
bapt <bapt@FreeBSD.org>
2013-05-27 10:34:54 +0000

    Fix build with recent clang

--- archivers/zipmix/files/patch-zipmix.c
+++ archivers/zipmix/files/patch-zipmix.c
@@ -1,6 +1,6 @@
---- zipmix.c.orig	2009-01-08 22:05:10.000000000 -0200
-+++ zipmix.c	2009-01-08 22:10:30.000000000 -0200
-@@ -4,6 +4,17 @@
+--- zipmix.c.orig	2013-05-27 12:33:32.795062151 +0200
++++ zipmix.c	2013-05-27 12:33:52.048486869 +0200
+@@ -4,6 +4,17 @@ zipmix.exe: zipmix.c; cl zipmix.c /O1 /G
  !if 0
  #endif
  
@@ -18,3 +18,17 @@
  #if defined(_WIN32)
  
  #include <windows.h>
+@@ -167,10 +178,11 @@ void showhelp ()
+ }
+ 
+ enum { BOOL_NONE=0,BOOL_OR,BOOL_AND,BOOL_SUB };
+-int main (long argc, char **argv)
++int main (int argc, char **argv)
+ {
+ 	unsigned long leng[2];
+-	long i, j, k, l, fileng[3], score[4], fmax, ftell43;
++	int i;
++	long j, k, l, fileng[3], score[4], fmax, ftell43;
+ 	long filespecified = 0, boolop = BOOL_NONE, quiet = 0, overwriteyesmode = 0;
+ 	short s;
+ 	char ch, *cptr, *cptr2;
