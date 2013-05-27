8ba705aa0e43f45d26eaf1885bb4d6d8bccf571d
araujo <araujo@FreeBSD.org>
2013-05-27 09:03:02 +0000

    - Update MASTER_SITES. - Add option for GTK2 support. - Add LICENSE.
    PR:		ports/174185
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- audio/autozen/files/patch-autozen.c
+++ audio/autozen/files/patch-autozen.c
@@ -36,3 +36,44 @@
  }
  
  gint volTimeOut(gpointer data) {
+@@ -729,6 +729,11 @@
+ 	GtkWidget *help_text;
+ 	GtkWidget *HelpQuit;
+ 
++#if GTK_MAJOR_VERSION >= 2
++	GtkTextIter help_text_iter;
++	GtkTextBuffer *help_text_buf;
++#endif
++
+ 	HelpWindow = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+ 	gtk_object_set_data (GTK_OBJECT (HelpWindow), "HelpWindow", HelpWindow);
+ 	gtk_window_set_title (GTK_WINDOW (HelpWindow), "AutoZen Help");
+@@ -740,9 +745,16 @@
+ 	gtk_widget_show (vbox1);
+ 	gtk_container_add (GTK_CONTAINER (HelpWindow), vbox1);
+ 
++#if GTK_MAJOR_VERSION >= 2
++	help_text = gtk_text_view_new ();
++	help_text_buf = gtk_text_view_get_buffer (GTK_TEXT_VIEW (help_text));
++	gtk_text_buffer_get_iter_at_offset (help_text_buf, &help_text_iter, 0);
++	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW (help_text), GTK_WRAP_WORD);
++#else
+ 	help_text = gtk_text_new (NULL, NULL);
+ 	gtk_text_set_word_wrap( GTK_TEXT( help_text ), TRUE);
+ 	gtk_text_set_line_wrap( GTK_TEXT( help_text ), TRUE);
++#endif
+ 
+ 
+ 	gtk_object_set_data (GTK_OBJECT (HelpWindow), "help_text", help_text);
+@@ -751,7 +763,11 @@
+ //	gtk_widget_set_sensitive (help_text, FALSE);
+ 	GTK_WIDGET_UNSET_FLAGS (help_text, GTK_CAN_FOCUS);
+ 	gtk_widget_realize (help_text);
++#if GTK_MAJOR_VERSION >= 2
++	gtk_text_buffer_insert (help_text_buf, &help_text_iter, text, strlen(text));
++#else
+ 	gtk_text_insert (GTK_TEXT (help_text), NULL, NULL, NULL, text, strlen(text));
++#endif
+ 
+ 	HelpQuit = gtk_button_new_with_label ("Close");
+ 	gtk_object_set_data (GTK_OBJECT (HelpWindow), "HelpQuit", HelpQuit);
