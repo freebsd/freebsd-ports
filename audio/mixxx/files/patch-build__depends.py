b2ccf8589317765b31abfa5084f245545210e4f6
miwi <miwi@FreeBSD.org>
2013-05-28 13:47:40 +0000

    - Update to 1.11.0
    PR:		178956
    Submitted by:	Ports Fury

--- audio/mixxx/files/patch-build__depends.py
+++ audio/mixxx/files/patch-build__depends.py
@@ -1,6 +1,6 @@
 --- build/depends.py.orig	2012-01-16 06:02:51.000000000 +0900
 +++ build/depends.py	2012-01-16 06:04:08.000000000 +0900
-@@ -99,7 +99,7 @@
+@@ -150,7 +150,7 @@
  
  class Qt(Dependence):
      DEFAULT_QTDIRS = {'linux': '/usr/share/qt4',
@@ -9,57 +9,50 @@
                        'osx': '/Library/Frameworks',
                        'windows': 'C:\\qt\\4.6.0'}
  
-@@ -145,6 +145,8 @@
-         # Setup Qt library includes for non-OSX
-         if build.platform_is_linux or build.platform_is_bsd:
+@@ -219,6 +219,7 @@
+             build.env.Append(LIBS = 'QtOpenGL')
              build.env.Append(LIBS = 'QtXml')
-+            build.env.Append(LIBS = 'QtXmlPatterns')
-+            build.env.Append(LIBS = 'QtSql')
-             build.env.Append(LIBS = 'QtGui')
-             build.env.Append(LIBS = 'QtCore')
              build.env.Append(LIBS = 'QtNetwork')
-@@ -164,14 +166,14 @@
++            build.env.Append(LIBS = 'QtSql')
+             build.env.Append(LIBS = 'QtScript')
+             if use_qtwebkit:
+                 build.env.Append(LIBS = 'QtWebKit')
+@@ -264,16 +265,16 @@
  
          # Set Qt include paths for non-OSX
          if not build.platform_is_osx:
--            build.env.Append(CPPPATH=['$QTDIR/include/QtCore',
--                                      '$QTDIR/include/QtGui',
--                                      '$QTDIR/include/QtXml',
--                                      '$QTDIR/include/QtNetwork',
--                                      '$QTDIR/include/QtSql',
--                                      '$QTDIR/include/QtOpenGL',
--                                      '$QTDIR/include/QtWebKit',
--                                      '$QTDIR/include/Qt'])
-+            build.env.Append(CPPPATH=['$QTDIR/include/qt4/QtCore',
-+                                      '$QTDIR/include/qt4/QtGui',
-+                                      '$QTDIR/include/qt4/QtXml',
-+                                      '$QTDIR/include/qt4/QtNetwork',
-+                                      '$QTDIR/include/qt4/QtSql',
-+                                      '$QTDIR/include/qt4/QtOpenGL',
-+                                      '$QTDIR/include/qt4/QtWebKit',
-+                                      '$QTDIR/include/qt4/Qt'])
+-            include_paths = ['$QTDIR/include/QtCore',
+-                             '$QTDIR/include/QtGui',
+-                             '$QTDIR/include/QtOpenGL',
+-                             '$QTDIR/include/QtXml',
+-                             '$QTDIR/include/QtNetwork',
+-                             '$QTDIR/include/QtSql',
+-                             '$QTDIR/include/QtScript',
+-                             '$QTDIR/include/Qt']
++            include_paths = ['$QTDIR/include/qt4/QtCore',
++                             '$QTDIR/include/qt4/QtGui',
++                             '$QTDIR/include/qt4/QtOpenGL',
++                             '$QTDIR/include/qt4/QtXml',
++                             '$QTDIR/include/qt4/QtNetwork',
++                             '$QTDIR/include/qt4/QtSql',
++                             '$QTDIR/include/qt4/QtScript',
++                             '$QTDIR/include/qt4/Qt']
+             if use_qtwebkit:
+-                include_paths.append('$QTDIR/include/QtWebKit')
++                include_paths.append('$QTDIR/include/qt4/QtWebKit')
+             build.env.Append(CPPPATH=include_paths)
  
          # Set the rpath for linux/bsd/osx.
-         # This is not support on OS X before the 10.5 SDK.
-@@ -179,8 +181,6 @@
-         compiling_on_104 = False
-         if build.platform_is_osx:
-             compiling_on_104 = (os.popen('sw_vers').readlines()[1].find('10.4') >= 0)
--        if not build.platform_is_windows and not (using_104_sdk or compiling_on_104):
--            build.env.Append(LINKFLAGS = "-Wl,-rpath,$QTDIR/lib")
- 
-         #QtSQLite DLL
-         if build.platform_is_windows:
-@@ -298,7 +298,7 @@
+@@ -387,7 +388,7 @@
          # the files correctly. Adding this folder ot the include path should fix
          # it, though might cause issues. This is safe to remove once we
          # deprecate Karmic support. rryan 2/2011
 -        build.env.Append(CPPPATH='/usr/include/taglib/')
 +        build.env.Append(CPPPATH='%%LOCALBASE%%/include/taglib/')
- class MixxxCore(Feature):
  
-     def description(self):
-@@ -623,10 +623,8 @@
+         if build.platform_is_windows and build.static_dependencies:
+             build.env.Append(CPPDEFINES = 'TAGLIB_STATIC')
+@@ -782,10 +783,8 @@
          if build.toolchain_is_gnu:
              # Default GNU Options
              # TODO(XXX) always generate debugging info?
@@ -70,18 +63,7 @@
  
              # Check that g++ is present (yeah, SCONS is a bit dumb here)
              if os.system("which g++ > /dev/null"): #Checks for non-zero return code
-@@ -676,8 +674,8 @@
- 
-         elif build.platform_is_osx:
-             #Stuff you may have compiled by hand
--            build.env.Append(LIBPATH = ['/usr/local/lib'])
--            build.env.Append(CPPPATH = ['/usr/local/include'])
-+            build.env.Append(LIBPATH = ['%%LOCALBASE%%/lib'])
-+            build.env.Append(CPPPATH = ['%%LOCALBASE%%/include'])
- 
-             #Non-standard libpaths for fink and certain (most?) darwin ports
-             build.env.Append(LIBPATH = ['/sw/lib'])
-@@ -689,13 +687,15 @@
+@@ -851,16 +850,19 @@
  
          elif build.platform_is_bsd:
              build.env.Append(CPPDEFINES='__BSD__')
@@ -98,23 +80,31 @@
 +            build.env.Append(LIBPATH=['%%LOCALBASE%%/lib/portaudio2',
 +                                      '%%LOCALBASE%%/lib/qt4',
 +                                      '%%LOCALBASE%%/lib'])
-+            build.env.Append(LINKFLAGS=['-Wl,-rpath,%%LOCALBASE%%/lib/portaudio2',
++            build.env.Append(LINKFLAGS=['%%LOCALBASE%%/lib/portaudio2/libportaudio.so',
++                                        '-Wl,-rpath,%%LOCALBASE%%/lib/portaudio2',
 +                                        '-Wl,-rpath,%%LOCALBASE%%/lib/qt4',
 +                                        '-Wl,-rpath,%%LOCALBASE%%/lib'])
              # why do we need to do this on OpenBSD and not on Linux?  if we
              # don't then CheckLib("vorbisfile") fails
-             build.env.Append(LIBS=['ogg', 'vorbis'])
-@@ -735,11 +735,11 @@
+-            build.env.Append(LIBS=['ogg', 'vorbis'])
++            # build.env.Append(LIBS=['ogg', 'vorbis'])
+ 
+         # Define for things that would like to special case UNIX (Linux or BSD)
+         if build.platform_is_bsd or build.platform_is_linux:
+@@ -897,14 +899,14 @@
          # Say where to find resources on Unix. TODO(XXX) replace this with a
          # RESOURCE_PATH that covers Win and OSX too:
          if build.platform_is_linux or build.platform_is_bsd:
--            share_path = os.path.join(SCons.ARGUMENTS.get('prefix', '/usr/local'), 'share/mixxx')
-+            share_path = os.path.join(SCons.ARGUMENTS.get('prefix', '%%PREFIX%%'), 'share/mixxx')
+-            prefix = SCons.ARGUMENTS.get('prefix', '/usr/local')
++            prefix = SCons.ARGUMENTS.get('prefix', '%%PREFIX%%')
+             share_path = os.path.join(prefix, 'share/mixxx')
              build.env.Append(CPPDEFINES=('UNIX_SHARE_PATH', r'\"%s\"' % share_path))
+             lib_path = os.path.join(prefix, 'lib/mixxx')
+             build.env.Append(CPPDEFINES=('UNIX_LIB_PATH', r'\"%s\"' % lib_path))
  
      def depends(self, build):
--        return [SoundTouch, KissFFT, ReplayGain, PortAudio, PortMIDI, Qt,
-+        return [SoundTouch, KissFFT, ReplayGain, PortAudio, Qt,
-                 FidLib, SndFile, FLAC, OggVorbis, OpenGL, TagLib,]
+-        return [SoundTouch, ReplayGain, PortAudio, PortMIDI, Qt,
++        return [SoundTouch, ReplayGain, PortAudio, Qt,
+                 FidLib, SndFile, FLAC, OggVorbis, OpenGL, TagLib, ProtoBuf]
  
      def post_dependency_check_configure(self, build, conf):
--- audio/mixxx/files/patch-src__soundsourceproxy.cpp
+++ audio/mixxx/files/patch-src__soundsourceproxy.cpp
@@ -1,14 +1,11 @@
 --- src/soundsourceproxy.cpp.orig	2011-12-24 11:53:20.000000000 +0900
 +++ src/soundsourceproxy.cpp	2012-01-16 06:10:09.000000000 +0900
-@@ -80,7 +80,10 @@
-         qDebug() << "Adding plugin path from commandline arg:" << clArgs.at(pluginPath + 1);
-         pluginDirs.append(QDir(clArgs.at(pluginPath + 1)));
+@@ -84,7 +84,7 @@
+         qDebug() << "Adding plugin path from commandline arg:" << pluginPath;
+         pluginDirs.append(QDir(pluginPath));
      }
 -#ifdef __LINUX__
-+#ifdef __BSD__
-+    pluginDirs.append(QDir("%%PREFIX%%/lib/mixxx/plugins/soundsource/"));
-+    pluginDirs.append(QDir(QDesktopServices::storageLocation(QDesktopServices::HomeLocation) + "/.mixxx/plugins/soundsource/"));
-+#elif __LINUX__
-     pluginDirs.append(QDir("/usr/local/lib/mixxx/plugins/soundsource/"));
-     pluginDirs.append(QDir("/usr/lib/mixxx/plugins/soundsource/"));
-     pluginDirs.append(QDir(QDesktopServices::storageLocation(QDesktopServices::HomeLocation) + "/.mixxx/plugins/soundsource/"));
++#if defined(__BSD__) || defined(__LINUX__)
+     QDir libPath(UNIX_LIB_PATH);
+     if (libPath.cd("plugins") && libPath.cd("soundsource")) {
+ 	pluginDirs.append(libPath.absolutePath());
