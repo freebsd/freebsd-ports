34db843c6db66fdf518434c6ada8e8f7226fd0a4
Sergey A. Osokin <osa@FreeBSD.org>
2013-05-29 16:52:39 +0000

    Update from 4.0.4 to 4.0.5:
    o) www/rubygem-passenger;
    o) third-party passenger module for www/nginx and www/nginx-devel.

--- www/nginx-devel/files/extra-patch-passenger-build-nginx.rb
+++ www/nginx-devel/files/extra-patch-passenger-build-nginx.rb
@@ -1,10 +1,10 @@
---- ../passenger-4.0.4/build/nginx.rb.orig	2013-05-28 01:13:38.000000000 +0400
-+++ ../passenger-4.0.4/build/nginx.rb	2013-05-28 19:21:20.000000000 +0400
+--- ../passenger-4.0.5/build/nginx.rb.orig	2013-05-28 01:13:38.000000000 +0400
++++ ../passenger-4.0.5/build/nginx.rb	2013-05-28 19:21:20.000000000 +0400
 @@ -24,14 +24,11 @@
  desc "Build Nginx support files"
  task :nginx => [
  	:nginx_without_native_support,
-+        LIBBOOST_OXT,
++	LIBBOOST_OXT,
  	NATIVE_SUPPORT_TARGET
  ].compact
  
