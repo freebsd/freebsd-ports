a0749f32481fe3abac083afc69709a5efbd792f9
olivierd <olivierd@FreeBSD.org>
2013-06-03 20:55:44 +0000

    - Update to 0.5.1

--- multimedia/xfce4-parole/files/patch-src__gst__parole-gst.c
+++ multimedia/xfce4-parole/files/patch-src__gst__parole-gst.c
@@ -1,18 +1,6 @@
---- ./src/gst/parole-gst.c.orig	2013-03-07 00:14:28.000000000 +0000
-+++ ./src/gst/parole-gst.c	2013-04-28 16:06:10.000000000 +0000
-@@ -33,9 +33,11 @@
- #if GST_CHECK_VERSION(1, 0, 0)
- #include <gst/video/videooverlay.h>
- #include <gst/video/navigation.h>
-+#include <gst/audio/streamvolume.h>
- #else
- #include <gst/interfaces/xoverlay.h>
- #include <gst/interfaces/navigation.h>
-+#include <gst/interfaces/streamvolume.h>
- #endif
- 
- #include <gst/pbutils/missing-plugins.h>
-@@ -1573,22 +1575,36 @@
+--- ./src/gst/parole-gst.c.orig	2013-05-26 15:18:14.000000000 +0000
++++ ./src/gst/parole-gst.c	2013-06-03 20:41:34.000000000 +0000
+@@ -1579,22 +1579,36 @@
      dialog = GTK_MESSAGE_DIALOG(gtk_message_dialog_new_with_markup(
                              NULL,
                              GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
@@ -36,7 +24,7 @@
 +#elif defined(__FreeBSD__)
 +    gtk_dialog_add_button( GTK_DIALOG(dialog), 
 +                            GTK_STOCK_OK,
-+                            GTK_RESPONSE_OK);
++                            GTK_RESPONSE_ACCEPT);
 +#endif
      
      gtk_message_dialog_format_secondary_markup(dialog,
@@ -49,25 +37,12 @@
                                               desc);
      
      return GTK_DIALOG(dialog);
-@@ -1712,7 +1728,7 @@
+@@ -1718,7 +1732,7 @@
  #endif
  	             gst_install_plugins_context_free(ctx);
  	        }
 -	        else if ( response == GTK_RESPONSE_REJECT )
-+	        else if ( ( response == GTK_RESPONSE_REJECT ) || (response == GTK_RESPONSE_OK) )
++	        else if ( (response == GTK_RESPONSE_REJECT) || (response == GTK_RESPONSE_OK) )
  	            gtk_widget_destroy(GTK_WIDGET(dialog));
          }
  	    break;
-@@ -2624,9 +2640,9 @@
- 
- void parole_gst_set_volume (ParoleGst *gst, gdouble value)
- {
--    g_object_set (G_OBJECT (gst->priv->playbin),
--		  "volume", value,
--		  NULL);
-+    gst_stream_volume_set_volume (GST_STREAM_VOLUME (gst->priv->playbin),
-+				    GST_STREAM_VOLUME_FORMAT_CUBIC,
-+				    value);
- }
- 						    
- gdouble	parole_gst_get_volume (ParoleGst *gst)
