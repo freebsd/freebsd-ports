92f330d01930ed4a862e42044b6531f0395e98d1
Martin Wilke <miwi@FreeBSD.org>
2013-06-04 10:03:18 +0000

    - Fix build with clang
    PR:		179225
    Submitted by:	Ports Fury

--- graphics/qslim/files/patch-tools-qslim_qvis.cxx
+++ graphics/qslim/files/patch-tools-qslim_qvis.cxx
@@ -9,3 +9,12 @@
  #include <FL/filename.H>
  #include <FL/filename.H>
  #include <FL/Fl_Slider.H>
+@@ -228,7 +228,7 @@
+ // Main application routine
+ //
+ 
+-main(int argc, char **argv)
++int main(int argc, char **argv)
+ {
+     // Change default values for application variables in GUI version
+     will_record_history = true;
