6fd6d4dccb205bcb4ad7d22db5bf955025eee9c5
Florian Smeets <flo@FreeBSD.org>
2013-06-07 15:05:38 +0000

    Update to 5.6.11-rc60.3

--- databases/percona56-server/files/patch-sql_mysqld.cc
+++ databases/percona56-server/files/patch-sql_mysqld.cc
@@ -1,6 +1,6 @@
---- sql/mysqld.cc.orig	2013-03-05 00:04:54.000000000 +0100
-+++ sql/mysqld.cc	2013-03-18 09:55:35.499120018 +0100
-@@ -6291,7 +6291,7 @@
+--- sql/mysqld.cc.orig	2013-06-03 00:02:34.000000000 +0200
++++ sql/mysqld.cc	2013-06-06 00:57:21.510741832 +0200
+@@ -6422,7 +6422,7 @@
  
  #ifdef HAVE_LIBWRAP
      {
@@ -9,12 +9,3 @@
        {
          struct request_info req;
          signal(SIGCHLD, SIG_DFL);
-@@ -6323,7 +6323,7 @@
-             The connection was refused by TCP wrappers.
-             There are no details (by client IP) available to update the host_cache.
-           */
--          statistic_increment(connection_tcpwrap_errors, &LOCK_status);
-+          statistic_increment(connection_errors_tcpwrap, &LOCK_status);
-           continue;
-         }
-       }
