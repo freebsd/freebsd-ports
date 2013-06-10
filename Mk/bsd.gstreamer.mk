8a0e32c0aa737929ae49f0fabd2a8d95ca0860b8
Baptiste Daroussin <bapt@FreeBSD.org>
2013-06-10 07:27:05 +0000

    Removed useless quotes

--- Mk/bsd.gstreamer.mk
+++ Mk/bsd.gstreamer.mk
@@ -60,6 +60,8 @@ GST1_MINOR_VERSION=	.0
 GST1_SHLIB_VERSION=	0
 GST1_MINIMAL_VERSION=	.5
 
+MAKE_ENV+=	GI_SCANNER_DISABLE_CACHE=yes
+
 #
 # missing base: alsa ivorbisdec
 # missing good: pulseaudio(need newer pulse version)
