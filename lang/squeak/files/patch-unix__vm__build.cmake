3c83ba4035a54c24dbcef5f160ad5808e034d058
Carlo Strub <cs@FreeBSD.org>
2013-06-10 04:46:44 +0000

    - Update to 4.10.2-2614 - Fix patch for MAN page install - USE_GCC
    PR:		ports/178656
    Submitted by:	Takeshi MUTOH <mutoh@openedu.org>

--- lang/squeak/files/patch-unix__vm__build.cmake
+++ lang/squeak/files/patch-unix__vm__build.cmake
@@ -1,8 +1,8 @@
---- unix/vm/build.cmake.bak	2012-04-14 22:26:13.000000000 +0900
-+++ unix/vm/build.cmake	2012-04-14 22:26:30.000000000 +0900
-@@ -83,4 +83,4 @@
+--- unix/vm/build.cmake.orig	2013-06-09 22:54:13.000000000 +0200
++++ unix/vm/build.cmake	2013-06-09 22:55:18.000000000 +0200
+@@ -90,4 +90,4 @@
    COMMAND sh ${bld}/config ${unix}/doc/squeak.1 ${bld}/squeak.1
  )
- ADD_DEPENDENCIES (squeakvm squeak.1)
+ ADD_DEPENDENCIES (squeakvm${scriptsuffix} squeak.1)
 -INSTALL (FILES ${bld}/squeak.1 DESTINATION share/man/man1)
 +INSTALL (FILES ${bld}/squeak.1 DESTINATION man/man1)
