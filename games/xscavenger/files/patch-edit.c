3d022c61e61a9fe629fd9ce55a30a3caa1a0208c
Boris Samorodov <bsam@FreeBSD.org>
2013-06-11 15:43:23 +0000

    . fix build with clang (CFLAGS+=-Wno-return-type); . remove ~80 warnings (CFLAGS+=-Wno-pointer-sign); . trim Makefile headers; . remove the indefinite article from COMMENT; . adopt WRKSRC_SUBDIR instead WRKSRC.

---
+++ games/xscavenger/files/patch-edit.c
@@ -0,0 +1,10 @@
+--- edit.c.orig	2013-06-11 18:21:01.902812410 +0400
++++ edit.c	2013-06-11 18:21:15.931811652 +0400
+@@ -2,6 +2,7 @@
+ 
+ #include <stdio.h>
+ #include <unistd.h>
++#include <string.h>
+ #include "scav.h"
+ #include "edit.h"
+ #include "x.h"
