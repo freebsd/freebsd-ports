6f41b547e8e54932306ac7467aeae4ffef75d26b
Frederic Culot <culot@FreeBSD.org>
2013-06-11 14:21:48 +0000

    - Update to 0.8.9 [1] - Make additional documentation installation conditional   (note: run-rootless.txt not installed as not relevant for FreeBSD)
    Changes:        https://raw.github.com/fail2ban/fail2ban/master/ChangeLog
    PR:             ports/179426 [1]
    Submitted by:   Christoph Theis <theis@gmx.at> (maintainer)

---
+++ security/py-fail2ban/files/patch-setup.py
@@ -0,0 +1,12 @@
+--- setup.py.orig	2013-06-10 03:40:55.000000000 -0400
++++ setup.py	2013-06-10 03:41:17.000000000 -0400
+@@ -67,9 +67,6 @@
+ 						),
+ 						('/var/run/fail2ban',
+ 							''
+-						),
+-						('/usr/share/doc/fail2ban',
+-							['README.md', 'DEVELOP', 'doc/run-rootless.txt']
+ 						)
+ 					]
+ )
