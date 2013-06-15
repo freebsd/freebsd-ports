308d88a41c159bfecc368f61f0717c6dbbaf66ea
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2013-06-15 11:29:10 +0000

    Electrum is an easy to use Bitcoin client. It protects you from losing coins in a backup mistake or computer failure, because your wallet can be recovered from a secret phrase that you can write on paper or learn by heart. There is no waiting time when you start the client, because it does not download the Bitcoin blockchain.
    WWW: http://electrum.org/
    
    PR:		179446
    Submitted by:	Neil Booth <kyuupichan@gmail.com>

---
+++ finance/electrum/files/patch-lib__util.py
@@ -0,0 +1,14 @@
+--- ./lib/util.py.orig	2013-04-07 21:30:06.000000000 +0900
++++ ./lib/util.py	2013-06-09 19:36:01.945732000 +0900
+@@ -59,8 +59,10 @@
+         return os.path.join(os.environ["APPDATA"], "Electrum")
+     elif platform.system() == "Linux":
+         return os.path.join(sys.prefix, "share", "electrum")
++    elif (sys.platform.startswith("dragonfly") or
++          sys.platform.startswith("freebsd")):
++	return os.path.join(sys.prefix, "share", "electrum")
+     elif (platform.system() == "Darwin" or
+-          platform.system() == "DragonFly" or
+ 	  platform.system() == "NetBSD"):
+         return "/Library/Application Support/Electrum"
+     else:
