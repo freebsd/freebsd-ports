900aa641c5ac3cdae6d5be421c6b3b833f229997
mandree <mandree@FreeBSD.org>
2013-06-15 18:01:05 +0000

    Update to Rawtherapee 4.0.11. New features, faster, requires GCC 4.6+.
    This should fix the issues mentioned in the PRs given below,
    in particular, it should do away with the need for libmap.conf hacks
    for libgcc_s, libgomp, libobjc, libssp, libstdc++.
    (the Makefile as I took it already set the linker's -rpath, and
    patch-CMakeLists.txt ensures CMake keeps it during "make install").
    
    ChangeLog 4.0.10: http://rawtherapee.com/blog/rawtherapee-4.0.10-released
    
    ChangeLog 4.0.11: http://rawtherapee.com/blog/rawtherapee-4.0.11-released
    
    PR:		ports/171079
    PR:		ports/178206

--- graphics/rawtherapee/files/patch-AboutThisBuild.cmake
+++ graphics/rawtherapee/files/patch-AboutThisBuild.cmake
@@ -1,31 +1,27 @@
---- ./AboutThisBuild.cmake.orig	2012-06-03 19:56:20.000000000 +0400
-+++ ./AboutThisBuild.cmake	2012-08-07 15:18:18.000000000 +0400
-@@ -8,28 +8,9 @@
-   find_file(HG_CMD hg)
-   set (SHELL "/bin/bash")
- else (WIN32) # Linux
--  find_file(HG_CMD hg)
-   set (SHELL "/bin/bash")
- endif (WIN32)
+--- ./AboutThisBuild.cmake.orig	2013-05-29 16:29:36.000000000 +0200
++++ ./AboutThisBuild.cmake	2013-06-15 19:16:41.000000000 +0200
+@@ -11,24 +11,9 @@
+     find_file(HG_CMD hg)
+     set (SHELL "/bin/bash")
+   else (WIN32) # Linux
+-    find_file(HG_CMD hg)
+     set (SHELL "/bin/bash")
+   endif (WIN32)
  
--# the hg command is looked for again, at build time
--if (HG_CMD STREQUAL HG_CMD-NOTFOUND)
--  message(FATAL_ERROR "hg command not found!")
--else (HG_CMD STREQUAL HG_CMD-NOTFOUND)
--  message(STATUS "hg command found: ${HG_CMD}")
--endif (HG_CMD STREQUAL HG_CMD-NOTFOUND)
+-  # Fail if Mercurial is not installed
+-  if (HG_CMD STREQUAL HG_CMD-NOTFOUND)
+-    message(FATAL_ERROR "hg command not found!")
+-  else (HG_CMD STREQUAL HG_CMD-NOTFOUND)
+-    message(STATUS "hg command found: ${HG_CMD}")
+-  endif (HG_CMD STREQUAL HG_CMD-NOTFOUND)
 -
--set (OUT_FILE "${SRC_DIR}/AboutThisBuild.txt")
--# there should be a better way of getting the compiler + compiler version, not restrained to Gcc
--execute_process(COMMAND gcc -dumpversion OUTPUT_VARIABLE GCC_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
--execute_process(COMMAND ${HG_CMD} -R ${SRC_DIR} branch OUTPUT_VARIABLE HG_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE)
--execute_process(COMMAND ${HG_CMD} parents --template={latesttag}.{latesttagdistance} WORKING_DIRECTORY ${SRC_DIR} OUTPUT_VARIABLE HG_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
--execute_process(COMMAND ${HG_CMD} parents --template={node|short} WORKING_DIRECTORY ${SRC_DIR} OUTPUT_VARIABLE HG_CHANGESET OUTPUT_STRIP_TRAILING_WHITESPACE)
--execute_process(COMMAND ${HG_CMD} parents --template={latesttagdistance} WORKING_DIRECTORY ${SRC_DIR} OUTPUT_VARIABLE HG_TAGDISTANCE OUTPUT_STRIP_TRAILING_WHITESPACE)
--if (CACHE_NAME_SUFFIX STREQUAL "")
--  execute_process(COMMAND ${HG_CMD} parents --template={latesttag} WORKING_DIRECTORY ${SRC_DIR} OUTPUT_VARIABLE CACHE_NAME_SUFFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
--endif ()
--
- # build version.h from template
- configure_file (${SRC_DIR}/rtgui/version.h.in ${SRC_DIR}/rtgui/version.h)
- # build AboutThisBuild.txt from template
+-  execute_process(COMMAND ${HG_CMD} -R "${PROJECT_SOURCE_DIR}" branch OUTPUT_VARIABLE HG_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE)
+-  execute_process(COMMAND ${HG_CMD} -R "${PROJECT_SOURCE_DIR}" parents --template={latesttag}.{latesttagdistance} WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}" OUTPUT_VARIABLE HG_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+-  execute_process(COMMAND ${HG_CMD} -R "${PROJECT_SOURCE_DIR}" parents --template={node|short} WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}" OUTPUT_VARIABLE HG_CHANGESET OUTPUT_STRIP_TRAILING_WHITESPACE)
+-  execute_process(COMMAND ${HG_CMD} -R "${PROJECT_SOURCE_DIR}" parents --template={latesttagdistance} WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}" OUTPUT_VARIABLE HG_TAGDISTANCE OUTPUT_STRIP_TRAILING_WHITESPACE)
+-  if (CACHE_NAME_SUFFIX STREQUAL "")
+-    execute_process(COMMAND ${HG_CMD} -R "${PROJECT_SOURCE_DIR}" parents --template={latesttag} WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}" OUTPUT_VARIABLE CACHE_NAME_SUFFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
+-  endif (CACHE_NAME_SUFFIX STREQUAL "")
+ else (REL_INFO_FILE STREQUAL REL_INFO_FILE-NOTFOUND)
+   include("${PROJECT_SOURCE_DIR}/ReleaseInfo.cmake")
+ endif (REL_INFO_FILE STREQUAL REL_INFO_FILE-NOTFOUND)
