dc9747a0598d94e2684bc9a01b398664da952659
antoine <antoine@FreeBSD.org>
2013-06-15 09:10:37 +0000

    Add load-octave-pkg to _FORCE_POST_PATTERNS. This fixes leftovers of octave-forge-* with pkg
    PR:		ports/179559
    Approved by:	portmgr (bapt)

--- Mk/bsd.pkgng.mk
+++ Mk/bsd.pkgng.mk
@@ -21,6 +21,7 @@ _FORCE_POST_PATTERNS=	rmdir kldxref mkfontscale mkfontdir fc-cache \
 						fonts.dir fonts.scale gtk-update-icon-cache \
 						gio-querymodules \
 						ldconfig \
+						load-octave-pkg \
 						update-desktop-database update-mime-database \
 						gdk-pixbuf-query-loaders catalog.ports \
 						glib-compile-schemas
