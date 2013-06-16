01020d179e912c12598e176d1ba18d0d6a7eb528
Kubilay Kocak <koobs@FreeBSD.org>
2013-06-16 09:10:19 +0000

    databases/py-redis: Update to 2.7.6
    - Update to 2.7.6
    - Update setup.py patch
    
    Changes:
    
    * 2.7.6
        * Added CONFIG RESETSTAT command. Thanks Yossi Gottlieb.
        * Fixed a bug introduced in 2.7.3 that caused issues with script objects
          and pipelines. Thanks Carpentier Pierre-Francois.
        * Converted redis-py's test suite to use the awesome py.test library.
        * Fixed a bug introduced in 2.7.5 that prevented a ConnectionError from
          being raised when the Redis server is LOADING data.
        * Added a BusyLoadingError exception that's raised when the Redis server
          is starting up and not accepting commands yet. BusyLoadingError
          subclasses ConnectionError, which this state previously returned.
          Thanks Yossi Gottlieb.
    * 2.7.5
        * DEL, HDEL and ZREM commands now return the numbers of keys deleted
          instead of just True/False.
        * from_url now supports URIs with a port number. Thanks Aaron Westendorf.

--- databases/py-redis/files/patch-setup.py
+++ databases/py-redis/files/patch-setup.py
@@ -1,22 +1,10 @@
---- ./setup.py.orig	2012-08-10 08:48:07.000000000 +1000
-+++ ./setup.py	2012-08-15 22:27:30.000000000 +1000
-@@ -3,10 +3,7 @@
- 
- from redis import __version__
- 
--try:
--    from setuptools import setup
--except ImportError:
--    from distutils.core import setup
-+from setuptools import setup
- 
- f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
- long_description = f.read()
-@@ -41,5 +38,6 @@
+--- ./setup.py.orig	2013-06-16 18:54:43.668848521 +1000
++++ ./setup.py	2013-06-16 18:54:57.779885292 +1000
+@@ -57,5 +57,6 @@
          'Programming Language :: Python :: 3',
          'Programming Language :: Python :: 3.2',
          'Programming Language :: Python :: 3.3',
--        ]
-+        ],
+-    ]
++    ],
 +    zip_safe=False,
  )
