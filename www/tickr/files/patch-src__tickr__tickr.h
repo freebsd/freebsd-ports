6d2e2078919f875b33ad081c1769e023b443c418
nivit <nivit@FreeBSD.org>
2013-06-16 13:54:02 +0000

    - Update to 0.6.4
    Build log:	http://goo.gl/R8rHs

--- www/tickr/files/patch-src__tickr__tickr.h
+++ www/tickr/files/patch-src__tickr__tickr.h
@@ -1,13 +1,13 @@
---- ./src/tickr/tickr.h.orig	2013-03-15 14:10:30.000000000 +0100
-+++ ./src/tickr/tickr.h	2013-03-15 14:11:17.000000000 +0100
-@@ -178,8 +178,8 @@
+--- ./src/tickr/tickr.h.orig	2013-06-16 15:01:04.000000000 +0200
++++ ./src/tickr/tickr.h	2013-06-16 15:01:29.000000000 +0200
+@@ -173,8 +173,8 @@
  # define STDERR_FILENAME2	"stderr2.txt"
  #endif
  #ifndef G_OS_WIN32
--# define INSTALL_PATH		"/usr/share/"APP_CMD
--# define IMAGES_PATH		INSTALL_PATH"/pixmaps"
-+# define INSTALL_PATH		"%%PREFIX%%/share"
-+# define IMAGES_PATH		INSTALL_PATH"/pixmaps/"APP_CMD
+-# define INSTALL_PATH		"/usr/share/" APP_CMD
+-# define IMAGES_PATH		INSTALL_PATH "/pixmaps"
++# define INSTALL_PATH		"%%PREFIX%%/share/"
++# define IMAGES_PATH		INSTALL_PATH "/pixmaps/" APP_CMD
  #else
- # define IMAGES_PATH		APP_NAME	/* actually not a path but a dir name */
+ # define IMAGES_PATH		APP_NAME	/* Actually not a path but a dir name */
  #endif
