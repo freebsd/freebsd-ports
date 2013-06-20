1c770cdd6da466d6ad4cbbad21e6ee65f8f5ec6f
Stephen Montgomery-Smith <stephen@FreeBSD.org>
2013-06-20 13:30:42 +0000

    - Update to 5.10.

--- math/sage/files/spkg-patch-python_-_src_setup.py
+++ math/sage/files/spkg-patch-python_-_src_setup.py
@@ -1,5 +1,5 @@
---- python-2.7.3.p6/src/setup.py-orig	2013-03-17 03:13:46.000000000 +0000
-+++ python-2.7.3.p6/src/setup.py	2013-03-17 03:15:11.000000000 +0000
+--- python-2.7.4.p0/src/setup.py-orig	2013-03-17 03:13:46.000000000 +0000
++++ python-2.7.4.p0/src/setup.py	2013-03-17 03:15:11.000000000 +0000
 @@ -642,7 +642,7 @@
          # use the same library for the readline and curses modules.
          if 'curses' in readline_termcap_library:
@@ -63,8 +63,8 @@
              macros = dict()
              libraries = []
  
--        elif platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
-+        elif platform in ('freebsd4', 'freebsd5', 'freebsd6'):
+-        elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
++        elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6'):
              # FreeBSD's P1003.1b semaphore support is very experimental
              # and has many known problems. (as of June 2008)
 -            macros = dict()
@@ -75,7 +75,7 @@
 +                )
 +            libraries = []
 +
-+        elif platform in ('freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
++        elif host_platform in ('freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
 +            macros = dict(                  # FreeBSD 7+
 +                HAVE_SEM_OPEN=1,
 +                HAVE_SEM_TIMEDWAIT=1,
@@ -83,14 +83,14 @@
 +                )
              libraries = []
  
-         elif platform.startswith('openbsd'):
+         elif host_platform.startswith('openbsd'):
 @@ -1484,7 +1500,7 @@
              missing.append('linuxaudiodev')
  
-         if (platform in ('linux2', 'freebsd4', 'freebsd5', 'freebsd6',
+         if (host_platform in ('linux2', 'freebsd4', 'freebsd5', 'freebsd6',
 -                        'freebsd7', 'freebsd8')
 +                        'freebsd7', 'freebsd8', 'freebsd9', 'freebsd10')
-             or platform.startswith("gnukfreebsd")):
+             or host_platform.startswith("gnukfreebsd")):
              exts.append( Extension('ossaudiodev', ['ossaudiodev.c']) )
          else:
 @@ -2066,9 +2082,7 @@
