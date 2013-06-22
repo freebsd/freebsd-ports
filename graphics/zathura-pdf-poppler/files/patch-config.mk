196007cb6df2d6fd7791fdf3ad36e1aa134a74b1
madpilot <madpilot@FreeBSD.org>
2013-06-22 15:03:32 +0000

    Update to 0.2.3

--- graphics/zathura-pdf-poppler/files/patch-config.mk
+++ graphics/zathura-pdf-poppler/files/patch-config.mk
@@ -1,16 +1,16 @@
---- config.mk.orig	2012-06-09 23:37:36.000000000 +0200
-+++ config.mk	2012-08-20 18:26:30.000000000 +0200
-@@ -11,7 +11,7 @@
+--- config.mk.orig	2013-05-12 23:21:06.000000000 +0200
++++ config.mk	2013-06-21 13:16:15.646661467 +0200
+@@ -12,7 +12,7 @@
  
  # paths
  PREFIX ?= /usr
 -LIBDIR ?= ${PREFIX}/lib
 +LIBDIR = ${PREFIX}/lib
+ DESKTOPPREFIX ?= ${PREFIX}/share/applications
  
  # libs
- CAIRO_INC ?= $(shell pkg-config --cflags cairo)
-@@ -27,10 +27,7 @@
- GIRARA_LIB ?= $(shell pkg-config --libs girara-gtk2)
+@@ -26,10 +26,7 @@
+ GIRARA_LIB ?= $(shell pkg-config --libs girara-gtk${ZATHURA_GTK_VERSION})
  
  ZATHURA_INC ?= $(shell pkg-config --cflags zathura)
 -PLUGINDIR ?= $(shell pkg-config --variable=plugindir zathura)
@@ -18,5 +18,5 @@
  PLUGINDIR = ${LIBDIR}/zathura
 -endif
  
- INCS = ${GIRARA_INC} ${GLIB_INC} ${DJVU_INC} ${ZATHURA_INC}
- LIBS = ${GIRARA_LIB} ${GLIB_LIB} ${DJVU_LIB}
+ INCS = ${CAIRO_INC} ${PDF_INC} ${ZATHURA_INC} ${GIRARA_INC}
+ LIBS = ${GIRARA_LIB} ${CAIRO_LIB} ${PDF_LIB}
