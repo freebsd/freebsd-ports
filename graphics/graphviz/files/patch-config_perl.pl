47eedb79f21a258d79fa834bcac0ac649c725879
Dirk Meyer <dinoex@FreeBSD.org>
2013-06-24 18:57:56 +0000

    - fix option PERL PR:		179666 Submitted by:	Geraud CONTINSOUZAS

--- graphics/graphviz/files/patch-config_perl.pl
+++ graphics/graphviz/files/patch-config_perl.pl
@@ -1,9 +1,14 @@
---- config/config_perl.pl.orig	2008-07-14 14:54:04.000000000 +0200
-+++ config/config_perl.pl	2009-03-15 11:08:29.000000000 +0100
-@@ -11,5 +11,5 @@
+--- config/config_perl.pl.orig	2013-02-15 00:27:39.000000000 +1100
++++ config/config_perl.pl	2013-06-18 11:18:34.000000000 +1100
+@@ -11,10 +11,9 @@
  	print "-I$archlib/CORE";
  }
  if ($ARGV[0] eq "PERL_INSTALL_DIR") {
--	print "$Config{vendorarch}"
-+	print "$Config{sitearch}"
- }
+-
+ 	my $d;
+ 
+-	foreach $d qw(installvendorarch vendorarch installsitearch sitearch) {
++	foreach $d ( qw(installvendorarch vendorarch installsitearch sitearch) ) {
+ 		if (exists($Config{$d}) and defined($Config{$d}) and
+ 			($Config{$d} ne '') ) {
+ 			print "$Config{$d}";
