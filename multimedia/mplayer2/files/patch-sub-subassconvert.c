cdc818f15931c03714fd507e7ee59054c7a5f1e1
Martin Matuska <mm@FreeBSD.org>
2013-06-25 07:41:26 +0000

    Update multimedia/ffmpeg1 to 1.2.1
    PR:		ports/177145
    Submitted by:	Alexander Kojevnikov <alexander@kojevnikov.com>

---
+++ multimedia/mplayer2/files/patch-sub-subassconvert.c
@@ -0,0 +1,10 @@
+--- sub/subassconvert.c.orig
++++ sub/subassconvert.c
+@@ -18,6 +18,7 @@
+  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+  */
+ 
++#include <ctype.h>
+ #include <string.h>
+ #include <stdint.h>
+ #include <stdlib.h>
---
+++ multimedia/mplayer2/files/patch-sub-subreader.c
@@ -0,0 +1,10 @@
+--- sub/subreader.c.orig
++++ sub/subreader.c
+@@ -21,6 +21,7 @@
+  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+  */
+ 
++#include <ctype.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
