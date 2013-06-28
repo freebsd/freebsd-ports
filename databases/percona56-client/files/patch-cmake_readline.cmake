82a4550e14cbffb668e1374ead9ed048ce8edb1e
Florian Smeets <flo@FreeBSD.org>
2013-06-28 19:24:14 +0000

    Update to 5.6.12-rc60.4

--- databases/percona56-client/files/patch-cmake_readline.cmake
+++ databases/percona56-client/files/patch-cmake_readline.cmake
@@ -1,11 +1,11 @@
---- cmake/readline.cmake.orig	2013-02-13 11:49:33.000000000 +0100
-+++ cmake/readline.cmake	2013-02-13 11:49:56.000000000 +0100
-@@ -192,7 +192,7 @@
-     IF(WITH_LIBEDIT) 
-      MYSQL_USE_BUNDLED_LIBEDIT()
-     ELSE()
--      FIND_SYSTEM_LIBEDIT(edit)
-+      FIND_SYSTEM_LIBEDIT(readline)
-       IF(NOT_LIBEDIT_FOUND)
-         MESSAGE(FATAL_ERROR "Cannot find system libedit libraries.Use WITH_LIBEDIT") 
-       ENDIF()
+--- cmake/readline.cmake.orig	2013-06-28 21:04:29.807976388 +0200
++++ cmake/readline.cmake	2013-06-28 21:04:49.685326016 +0200
+@@ -147,7 +147,7 @@
+ 
+   FIND_LIBRARY(EDITLINE_LIBRARY
+     NAMES
+-    edit
++    readline
+   )
+   MARK_AS_ADVANCED(EDITLINE_INCLUDE_DIR EDITLINE_LIBRARY)
+ 
