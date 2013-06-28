ecc7994ed979391fb1b16fc4610b52f7a4eb2d29
William Grzybowski <wg@FreeBSD.org>
2013-06-28 22:50:37 +0000

    deskutils/cairo-dock: update to 3.2.1
    - Update to 3.2.1 [1]
    - Remove ABI version from lib depends [1]
    - USES pkgconfig
    - Fix build with clang
    - Use space instead of tab for WWW
    
    Changes: http://glx-dock.org/bg_topic.php?t=6836
    
    PR:		ports/180032
    Submitted by:	Ivan Klymenko <fidaj@ukr.net> (maintainer) [1]

--- deskutils/cairo-dock/files/patch-cairo-dock_theme-creator.sh
+++
@@ -1,8 +0,0 @@
---- cairo-dock_theme-creator.sh.orig	2012-03-10 13:56:15.674011880 +0200
-+++ cairo-dock_theme-creator.sh	2012-03-10 13:56:23.814030235 +0200
-@@ -1,4 +1,4 @@
--#!/bin/bash
-+#!/usr/local/bin/bash
- 
- # Theme creator for Cairo-Dock
- #
