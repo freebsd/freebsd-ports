8885aed96f895510135a857f02e93f301f1883dd
wg <wg@FreeBSD.org>
2013-06-29 01:04:39 +0000

    deskutils/cairo-dock-plugins: update to 3.2.1
    - Update to 3.2.1 [1]
    - Fix build with clang
    - Use space instead of tab for WWW
    - USES pkgconfig
    
    Changes: http://glx-dock.org/bg_topic.php?t=6836
    
    PR:		ports/180031
    Submitted by:	Ivan Klymenko <fidaj@ukr.net> (maintainer) [1]

--- deskutils/cairo-dock-plugins/files/patch-CDBashApplet.sh
+++
@@ -1,8 +0,0 @@
---- Dbus/interfaces/bash/CDBashApplet.sh.orig	2012-03-09 17:46:25.024862245 +0200
-+++ Dbus/interfaces/bash/CDBashApplet.sh	2012-03-09 17:46:37.734860652 +0200
-@@ -1,4 +1,4 @@
--#!/bin/bash
-+#!/usr/local/bin/bash
- 
- # This is a part of the external applets for Cairo-Dock
- # Copyright : (C) 2010-2011 by Nochka85
