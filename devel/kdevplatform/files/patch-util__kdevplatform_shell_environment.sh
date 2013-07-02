39f90f86a09860c35705e48b6407524808831444
rakuco <rakuco@FreeBSD.org>
2013-07-02 19:20:28 +0000

    Update KDevelop & friends to 4.5.1 and KDevelop to 1.5.1.
    Proudly presented by the KDE on FreeBSD team, with commits by makc@, rakuco@
    and Schaich Alonso. Everything should build fine with both gcc and clang.

---
+++ devel/kdevplatform/files/patch-util__kdevplatform_shell_environment.sh
@@ -0,0 +1,8 @@
+--- util/kdevplatform_shell_environment.sh.orig	2013-04-30 14:51:27.865270062 +0200
++++ util/kdevplatform_shell_environment.sh	2013-04-30 14:51:42.680269833 +0200
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/usr/bin/env bash
+ 
+ # This file is part of KDevelop
+ # Copyright 2011 David Nolden <david.nolden.kdevelop@art-master.de>
