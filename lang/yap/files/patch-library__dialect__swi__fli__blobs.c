125fbd4ee0dddddcc3b50078180c59b262bccafd
lippe <lippe@FreeBSD.org>
2013-07-03 23:16:34 +0000

    YAP is a high-performance Prolog compiler developed at LIACC/Universidade do Porto and at COPPE Sistemas/UFRJ. Its Prolog engine is based in the WAM (Warren Abstract Machine), with several optimizations for better performance. YAP follows the Edinburgh tradition, and is largely compatible with the ISO-Prolog standard and with Quintus and SICStus Prolog.
    WWW: http://www.dcc.fc.up.pt/~vsc/Yap/
    
    PR:		ports/180068
    Submitted by:	Danilo Egea Gondolfo <danilogondolfo@gmail.com>

---
+++ lang/yap/files/patch-library__dialect__swi__fli__blobs.c
@@ -0,0 +1,10 @@
+--- ./library/dialect/swi/fli/blobs.c.orig	2013-06-28 00:56:12.000000000 -0300
++++ ./library/dialect/swi/fli/blobs.c	2013-06-28 00:56:27.000000000 -0300
+@@ -18,6 +18,7 @@
+ #include	<Yap.h>
+ #include	<Yatom.h>
+ 
++#include <stdio.h>
+ #include <string.h>
+ 
+ #include	<SWI-Prolog.h>
