94d43048e5fdb9433a08517b0a6e73cdf149505b
Diane Bruce <db@FreeBSD.org>
2013-07-03 17:45:02 +0000

    Updated trustedqsl and removed tqsllib port which is no longer needed Some cleanups and fixes needed.
    Original comment from shurd@sasktel.net
    I noticed that the new version allows uploading directly from the tqsl
    app, so I've updated the port.  The tqsllib is no longer a separate
    package from upstream, so I added the CONFLICTS_INSTALL for it.  A note
    will be needed in ports/UPDATING regarding this.
    
    Submitted by:		 Stephen Hurd <shurd@sasktel.net>

---
+++ comms/trustedqsl/files/patch-src_tqslconvert.cpp
@@ -0,0 +1,11 @@
+--- src/tqslconvert.cpp.orig	2013-06-23 17:30:50.000000000 -0700
++++ src/tqslconvert.cpp	2013-06-23 17:31:00.000000000 -0700
+@@ -22,7 +22,7 @@
+ #include <vector>
+ #include <ctype.h>
+ #include <set>
+-#include <db.h>
++#include <db5/db.h>
+ 
+ #include <locale.h>
+ //#include <iostream>
