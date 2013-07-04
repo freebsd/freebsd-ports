0ea6cb65122fcf0ad337ba6ee359982c650c8188
flo <flo@FreeBSD.org>
2013-07-04 22:46:33 +0000

    - Don't force gcc, make this work with clang, too [1] - Don't call gmake directly [1] - Make the -p (Run as pseudo-realtime thread) work on FreeBSD [1] - Use @dirrmtry for directories that can contain files after deinstall
    Submitted by:	tijl [1]

--- net/asterisk/files/patch-main::utils.c
+++ net/asterisk/files/patch-main::utils.c
@@ -6,7 +6,7 @@
  
 -#ifdef __linux__
 -	/* On Linux, pthread_attr_init() defaults to PTHREAD_EXPLICIT_SCHED,
-+#ifdef __linux__ || defined(__FreeBSD__)
++#if defined(__linux__) || defined(__FreeBSD__)
 +	/* On Linux and FreeBSD, pthread_attr_init() defaults to PTHREAD_EXPLICIT_SCHED,
  	   which is kind of useless. Change this here to
  	   PTHREAD_INHERIT_SCHED; that way the -p option to set realtime
