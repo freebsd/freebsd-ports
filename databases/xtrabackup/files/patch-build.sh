28807e530d9e15b03b07578c318d4e30f7d9dcdf
William Grzybowski <wg@FreeBSD.org>
2013-07-04 17:57:17 +0000

    databases/xtrabackup: update to 2.1.3
    - Update to 2.1.3 [1]
    - USES cmake
    - Unmute install commands
    
    Changelog: https://launchpad.net/percona-xtrabackup/+milestone/2.1.3
    
    PR:		ports/180224 [1]
    Submitted by:	Alex Samorukov <samm@os2.kiev.ua>
    Approved by:	maintainer

--- databases/xtrabackup/files/patch-build.sh
+++ databases/xtrabackup/files/patch-build.sh
@@ -1,6 +1,14 @@
---- utils/build.sh	2012-02-10 15:54:51.000000000 +0100
-+++ utils/build.sh	2012-03-16 20:53:58.366366222 +0100
-@@ -225,9 +225,14 @@
+--- utils/build.sh	2013-05-22 19:00:32.000000000 +0200
++++ utils/build.sh	2013-07-02 21:36:25.534672963 +0200
+@@ -281,6 +281,7 @@
+                 -DWITH_EXTRA_CHARSETS=all \
+                 -DWITH_EMBEDDED_SERVER=1 \
+                 -DENABLE_DTRACE=OFF $extra_config_55plus"
++        export LIBS=-lcrypt
+         build_all $type
+         ;;
+ 
+@@ -316,9 +317,14 @@
  	    bzr revert
  	    bzr pull --overwrite
  	else
@@ -17,7 +25,7 @@
  	fi
  
  	$MAKE_CMD main
-@@ -283,9 +288,15 @@
+@@ -370,9 +376,15 @@
  	    bzr revert
  	    bzr pull --overwrite
  	else
