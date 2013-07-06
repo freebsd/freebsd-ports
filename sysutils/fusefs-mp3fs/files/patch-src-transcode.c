f69446f8cf58cc87931e49b33925e3299c8e1848
Lars Engels <lme@FreeBSD.org>
2013-07-06 09:25:02 +0000

    Fix build with a recent version of FLAC
    PR:		ports/179577
    Submitted by:	maintainer

---
+++ sysutils/fusefs-mp3fs/files/patch-src-transcode.c
@@ -0,0 +1,10 @@
+--- src/transcode.c	2012-05-01 22:12:48.000000000 +0200
++++ src/transcode.c	2013-06-15 19:19:13.000000000 +0200
+@@ -21,6 +21,7 @@
+ 
+ #define _GNU_SOURCE
+ 
++#include <inttypes.h>
+ #include <stdio.h>
+ #include <string.h>
+ #include <unistd.h>
