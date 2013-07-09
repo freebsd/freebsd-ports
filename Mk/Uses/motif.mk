9b17397fe093d41486f2ef131dbe24c1f3068bb4
Baptiste Daroussin <bapt@FreeBSD.org>
2013-07-09 21:05:14 +0000

    New USES=motif that will handle the open-motif dependency. If a user want to enforce lesstif in place of open-motif then the usual WANT_LESSTIF in make.conf will be respected

---
+++ Mk/Uses/motif.mk
@@ -0,0 +1,30 @@
+# $FreeBSD$
+#
+# handle dependency on motif
+#
+# MAINTAINER: portmgr@FreeBSD.org
+#
+# Feature:	motif
+# Usage:	USES=motif
+#
+# If WANT_LESSTIF is defined in user make.conf then lesstif will be used
+# instead of open-motif
+
+.if !defined(_INCLUDE_USES_MOTIF_MK)
+_INCLUDE_USES_MOTIF_MK=	yes
+
+.if defined(motif_ARGS)
+IGNORE=	USES=motif takes no arguments
+.endif
+
+.if defined(WANT_LESSTIF)
+LIB_DEPENDS+=		libXm.so:${PORTSDIR}/x11-toolkits/lesstif
+.else
+USE_XORG+=	xpm
+LIB_DEPENDS+=		libXm.so.4:${PORTSDIR}/x11-toolkits/open-motif
+.endif
+
+MOTIFLIB?=	-L${LOCALBASE}/lib -lXm -lXp
+MAKE_ENV+=	MOTIFLIB="${MOTIFLIB}"
+
+.endif
