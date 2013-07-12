5688abbf508472f8c0b2f03572dbe745c4fdef44
deischen <deischen@FreeBSD.org>
2013-07-12 17:44:49 +0000

    From the (new) maintainer:
      - Update to 0.27
      - Take maintainership
      - Shorten header
      - Add license (LGPL21)
      - Add dependency for devel/doxygen
      - Add manual pages
      - Add docs
      - Add examples
      - Add DOCS DOXYGEN EXAMPLES Options
      - Trim files/patch-src-lo_types_internal.h
      - Remove not needed patch files/patch-src-server.c
      - Trim pkg-descr
      - Trim pkg-plist
    
    Note that the previous maintainer was ports@.
    
    PR:		179916
    Submitted by:	nemysis at gmx dot ch (new maintainer)

--- audio/liblo/files/patch-src-lo_types_internal.h
+++ audio/liblo/files/patch-src-lo_types_internal.h
@@ -1,10 +1,18 @@
---- src/lo_types_internal.h.orig	Mon Dec  4 23:29:22 2006
-+++ src/lo_types_internal.h	Mon Dec  4 23:29:32 2006
-@@ -9,6 +9,7 @@
+--- src/lo_types_internal.h.orig	2013-05-22 18:34:31.000000000 +0200
++++ src/lo_types_internal.h	2013-06-24 01:54:53.000000000 +0200
+@@ -20,6 +20,7 @@
+ #else
+ #define closesocket close
+ #include <netdb.h>
++#include <netinet/in.h>
+ #include <arpa/inet.h>
  #endif
  
+@@ -33,6 +34,7 @@
+ 
+ #ifdef ENABLE_THREADS
  #include <pthread.h>
 +#include <sys/socket.h>
+ #endif
  
  #include "lo/lo_osc_types.h"
- 
