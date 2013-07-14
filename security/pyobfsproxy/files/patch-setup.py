72b32adac7358090f20684d6b689402f469aa8cb
cs <cs@FreeBSD.org>
2013-07-14 20:38:13 +0000

    Update to 0.2.2
    PR:		ports/179893 ports/179928

---
+++ security/pyobfsproxy/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2013-07-14 19:01:36.000000000 +0000
++++ setup.py	2013-07-14 19:02:02.000000000 +0000
+@@ -31,7 +31,7 @@
+     install_requires = [
+         'setuptools',
+         'PyCrypto',
+-        'Twisted',
++        'Twisted_Core',
+         'argparse',
+         'pyptlib'
+         ],
