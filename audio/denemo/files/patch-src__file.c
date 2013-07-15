259b883c42f80a02eb0cd0c0fd94589a6c64d97f
Martin Wilke <miwi@FreeBSD.org>
2013-07-15 14:54:29 +0000

    - Update to 1.0.4
    PR:		180501
    Submitted by:	Ports Fury

--- audio/denemo/files/patch-src__file.c
+++ audio/denemo/files/patch-src__file.c
@@ -1,11 +1,11 @@
 --- src/file.c.orig
 +++ src/file.c
-@@ -556,7 +556,7 @@
-     default_template_path = local_template_path;   
-   } else if(local==EXAMPLE){
-     if(system_example_path==NULL) {
--      system_example_path = g_build_filename (get_data_dir (), "examples", NULL);
-+      system_example_path = g_build_filename (get_data_dir (), "..", "examples", "denemo", NULL);
-       GDir *denemo_path = g_dir_open(system_example_path, 0, NULL);
-       if(denemo_path == NULL) {
- 	warningdialog ("No examples directory in installation");
+@@ -571,7 +571,7 @@
+     {
+       if (system_example_path == NULL)
+         {
+-          system_example_path = g_build_filename (get_data_dir (), "examples", NULL);
++          system_example_path = g_build_filename (get_data_dir (), "..", "examples", NULL);
+           GDir *denemo_path = g_dir_open (system_example_path, 0, NULL);
+           if (denemo_path == NULL)
+             {
