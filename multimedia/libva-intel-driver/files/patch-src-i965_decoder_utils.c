3702c5e03eecd913dab3cd47e445108c0966e576
madpilot <madpilot@FreeBSD.org>
2013-07-18 22:19:55 +0000

    - Update to 1.2.0
    PR:		ports/180040
    Submitted by:	Myself
    Approved by:	Maintainer timeout

--- multimedia/libva-intel-driver/files/patch-src-i965_decoder_utils.c
+++ multimedia/libva-intel-driver/files/patch-src-i965_decoder_utils.c
@@ -1,14 +1,14 @@
---- src/i965_decoder_utils.c.orig	2012-11-09 02:44:47.000000000 +0100
-+++ src/i965_decoder_utils.c	2012-11-14 10:45:13.909019239 +0100
-@@ -22,7 +22,11 @@
-  */
+--- src/i965_decoder_utils.c.orig	2013-06-26 05:14:51.000000000 +0200
++++ src/i965_decoder_utils.c	2013-06-27 16:06:27.234183751 +0200
+@@ -23,7 +23,11 @@
  
  #include "sysdeps.h"
+ 
 +#ifdef __FreeBSD__
 +#include <stdlib.h>
 +#else
  #include <alloca.h>
 +#endif
+ 
  #include "intel_batchbuffer.h"
- #include "i965_decoder_utils.h"
  #include "i965_drv_video.h"
