84569d17ee7d0ab1d8106d743e3b1c03570372f3
Andrej Zverev <az@FreeBSD.org>
2013-07-19 09:51:46 +0000

    - Update to 3.2.3 - Add patch for tests/test_connect_delay.cpp (no newline at the end of the file error) - Pet portlint(1) by replacing tab into space in WWW link - Drop maintainership

---
+++ devel/zmq-devel/files/patch-tests__test_connect_delay.cpp
@@ -0,0 +1,9 @@
+--- ./tests/test_connect_delay.cpp.orig	2013-07-19 13:30:55.436108722 +0400
++++ ./tests/test_connect_delay.cpp	2013-07-19 13:30:59.430156726 +0400
+@@ -257,4 +257,4 @@
+     
+     rc = zmq_ctx_destroy(context2);
+     assert (rc == 0);
+-}
+\ No newline at end of file
++}
