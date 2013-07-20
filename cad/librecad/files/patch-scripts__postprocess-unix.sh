d15fa81db03bbd3c7fb01adbc87d6199f52c1e24
Max Brazhnikov <makc@FreeBSD.org>
2013-07-20 11:08:57 +0000

    - Update to 1.0.3 - Switch to GITHUB - Prevent from writing to /root during build (XDG_CONFIG_HOME) - Add build-time dependence on math/muparser

--- cad/librecad/files/patch-scripts__postprocess-unix.sh
+++ cad/librecad/files/patch-scripts__postprocess-unix.sh
@@ -1,24 +1,33 @@
---- ./scripts/postprocess-unix.sh.orig	2011-12-14 18:07:34.000000000 +0000
-+++ ./scripts/postprocess-unix.sh	2012-01-03 10:28:08.510074137 +0000
-@@ -18,19 +18,9 @@
- cp support/patterns/*.dxf $RESOURCEDIR/patterns
- #cp support/fonts/*.cxf $RESOURCEDIR/fonts
- cp support/fonts/*.lff* $RESOURCEDIR/fonts
--#cp support/doc/*.qhc $RESOURCEDIR/doc
--#cp support/doc/*.qch $RESOURCEDIR/doc
-+cp support/doc/*.qhc $RESOURCEDIR/doc
-+cp support/doc/*.qch $RESOURCEDIR/doc
- find support/library -type d -not -path "*.svn*"  | sed s/support// | xargs -IFILES  mkdir $RESOURCEDIR/FILES
- find support/library -type f -iname *.dxf -not -path "*.svn*"  | sed s/support// | xargs -IFILES  cp support/FILES $RESOURCEDIR/FILES
+--- ./scripts/postprocess-unix.sh.orig	2012-12-30 05:13:26.000000000 +0000
++++ ./scripts/postprocess-unix.sh	2013-07-19 19:52:16.249098376 +0000
+@@ -6,7 +6,7 @@
+ DOCDIR="${THISDIR}/support/doc"
+ 
+ # Generate Help Files
+-cd "{$DOCDIR}"
++cd "${DOCDIR}"
+ qcollectiongenerator LibreCADdoc.qhcp
+ 
+ cd "${THISDIR}"
+@@ -18,19 +18,8 @@
+ cp support/patterns/*.dxf "${RESOURCEDIR}"/patterns
+ #cp support/fonts/*.cxf "${RESOURCEDIR}"/fonts
+ cp support/fonts/*.lff* "${RESOURCEDIR}"/fonts
+-#cp support/doc/*.qhc "${RESOURCEDIR}"/doc
+-#cp support/doc/*.qch "${RESOURCEDIR}"/doc
++cp support/doc/*.qhc "${RESOURCEDIR}"/doc
++cp support/doc/*.qch "${RESOURCEDIR}"/doc
+ find support/library -type d | sed s/support// | xargs -IFILES  mkdir "${RESOURCEDIR}"/FILES
+ find support/library -type f -iname *.dxf | sed s/support// | xargs -IFILES  cp support/FILES "${RESOURCEDIR}"/FILES
  
 -# Generate translations
 -lrelease librecad.pro
--mkdir -p $RESOURCEDIR/qm
+-mkdir -p "${RESOURCEDIR}"/qm
 - 
 -# Go into translations directory
--cd "$TSDIR"
+-cd "${TSDIR}"
 -for tf in *.qm
 -do
--		cp $tf $RESOURCEDIR/qm/$tf
+-		cp "${tf}" "${RESOURCEDIR}/qm/${tf}"
 -done
- 
+-
