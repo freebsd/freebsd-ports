8dd5280ac73f24463d94f41577843a0612608123
amdmi3 <amdmi3@FreeBSD.org>
2013-07-22 17:28:38 +0000

    - Fix timestamp display in file view - Fix colors on some terminals - Add LICENSE
    PR:		180623
    Submitted by:	"r4721@tormail.org" <r4721@tormail.org>

--- editors/chexedit/files/patch-src-file.c
+++ editors/chexedit/files/patch-src-file.c
@@ -8,14 +8,3 @@
  #include <grp.h>
  #include <pwd.h>
  
-@@ -618,8 +619,8 @@
- 
-          wprintw (Globals.wmain,
-    /* why does it seem like on OpenBSD this is broken? */
--#if defined(__OpenBSD__) || defined(BROKEN)
--            "%s %-8s %-8s %10ld %1d\b2d-%02d-%02d %02d:%02d ",
-+#if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(BROKEN)
-+            "%s %-8s %-8s %10ld %1d\b%04d-%02d-%02d %02d:%02d ",
- #else
-             "%s %-8s %-8s %10ld %d-%02d-%02d %02d:%02d ",
- #endif
