fd9883553c6af4faf86c2e3e3bf8f5847c96b4de
wg <wg@FreeBSD.org>
2013-07-25 19:58:17 +0000

    mail/dbmail: update to 3.1.1
    - Update to 3.1.1 [1]
    - Remove leading article from COMMENT
    
    Check share/doc/dbmail/UPGRADING for upgrading details.
    
    Changes: http://git.dbmail.eu/paul/dbmail/log/
    
    PR:		ports/180768
    Submitted by:	Alan Hicks <ahicks p-o.co.uk> (maintainer)

--- mail/dbmail/files/patch-src_clientbase.c
+++ mail/dbmail/files/patch-src_clientbase.c
@@ -1,20 +1,28 @@
---- src/clientbase.c.orig	2012-06-19 17:49:50.000000000 +0100
-+++ src/clientbase.c	2012-06-19 17:50:29.000000000 +0100
-@@ -160,7 +160,7 @@
+--- src/clientbase.c.orig	2013-07-22 20:03:36.000000000 +0100
++++ src/clientbase.c	2013-07-23 13:14:56.000000000 +0100
+@@ -159,14 +159,14 @@
+ 		client->tx		= STDOUT_FILENO;
  	} else {
  		/* server-side */
- 		TRACE(TRACE_DEBUG,"saddr [%p] sa_family [%d] len [%d]", c->saddr, c->saddr->sa_family, c->saddr_len);
 -		if ((serr = getnameinfo(c->saddr, c->saddr_len, client->dst_ip, NI_MAXHOST, client->dst_port, NI_MAXSERV, 
 +		if ((serr = getnameinfo(c->saddr, c->saddr->sa_len, client->dst_ip, NI_MAXHOST, client->dst_port, NI_MAXSERV, 
  						NI_NUMERICHOST | NI_NUMERICSERV))) {
  			TRACE(TRACE_INFO, "getnameinfo::error [%s]", gai_strerror(serr));
  		}
-@@ -168,7 +168,7 @@
  
  		/* client-side */
- 		TRACE(TRACE_DEBUG,"caddr [%p] sa_family [%d] len [%d]", c->caddr, c->caddr->sa_family, c->caddr_len);
--		if ((serr = getnameinfo(c->caddr, c->caddr_len, client->src_ip, NI_MAXHOST, client->src_port, NI_MAXSERV,
-+		if ((serr = getnameinfo(c->caddr, c->caddr->sa_len, client->src_ip, NI_MAXHOST, client->src_port, NI_MAXSERV,
- 						NI_NUMERICHOST | NI_NUMERICSERV))) {
- 			TRACE(TRACE_EMERG, "getnameinfo:error [%s]", gai_strerror(serr));
- 		} 
+ 		if (server_conf->resolveIP) {
+-			if ((serr = getnameinfo(c->caddr, c->caddr_len, client->clientname, NI_MAXHOST, NULL, 0,
++			if ((serr = getnameinfo(c->caddr, c->caddr->sa_len, client->clientname, NI_MAXHOST, NULL, 0,
+ 						       	NI_NAMEREQD))) {
+ 				TRACE(TRACE_INFO, "getnameinfo:error [%s]", gai_strerror(serr));
+ 			} 
+@@ -177,7 +177,7 @@
+ 					client->clientname[0] ? client->clientname : "Lookup failed");
+ 		} else {
+ 
+-			if ((serr = getnameinfo(c->caddr, c->caddr_len, client->src_ip, NI_MAXHOST, client->src_port,
++			if ((serr = getnameinfo(c->caddr, c->caddr->sa_len, client->src_ip, NI_MAXHOST, client->src_port,
+ 						       	NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV))) {
+ 				TRACE(TRACE_INFO, "getnameinfo:error [%s]", gai_strerror(serr));
+ 			} 
