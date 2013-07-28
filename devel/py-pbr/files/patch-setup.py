654d002861996b2a0d284bd28b2845c43de6888e
Kubilay Kocak <koobs@FreeBSD.org>
2013-07-28 10:58:11 +0000

    devel/py-pbr: Update to 0.5.19
    - Update to 0.5.19
    - Update pkg-plist
    - Remove d2to1 from BUILD_DEPENDS (now included in pbr)
    - Add setuptools-git to RUN_DEPENDS
    - Remove unecessary setup.py patch
    
    Reviewed by:	Mikolaj Golub <trociny@freebsd.org>

--- devel/py-pbr/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
---- ./setup.py.orig	2013-06-18 20:01:04.819952450 +1000
-+++ ./setup.py	2013-06-18 20:01:17.687327396 +1000
-@@ -18,4 +18,5 @@
- 
- setuptools.setup(
-     setup_requires=['d2to1>=0.2.10,<0.3'],
-+    zip_safe=False,
-     d2to1=True)
