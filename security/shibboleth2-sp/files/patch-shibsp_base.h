aa8c3f531ae18cfed8195bb45485f4d084901c65
girgen <girgen@FreeBSD.org>
2013-07-29 14:49:11 +0000

    Move /var/cache/shibboleth to /var/db/shibboleth, since /var/cache has mode 750 and cannot be read by the www user. According to hier(7):    db/   misc. automatically generated system-specific database files so /var/db seems like the best choice

---
+++ security/shibboleth2-sp/files/patch-shibsp_base.h
@@ -0,0 +1,11 @@
+--- shibsp/base.h.orig	2013-07-29 14:46:37.905428862 +0200
++++ shibsp/base.h	2013-07-29 14:46:44.286416221 +0200
+@@ -127,7 +127,7 @@
+ #define SHIBSP_RUNDIR   "var/run"
+ 
+ /** Cache directory for installation (used to resolve relative paths). */
+-#define SHIBSP_CACHEDIR "var/cache"
++#define SHIBSP_CACHEDIR "var/db"
+ 
+ /** XML directory for installation (used to resolve relative paths). */
+ #define SHIBSP_XMLDIR   "share/xml"
