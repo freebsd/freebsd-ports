677adc2584d3da0d708b3015d4c4195b16468a31
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2013-07-31 16:02:37 +0900

    bugfix: plist, path to shebang

---
+++ net/rabbitmq/files/patch-plugins-src__rabbitmq-amqp1.0__codegen.py
@@ -0,0 +1,8 @@
+--- ./plugins-src/rabbitmq-amqp1.0/codegen.py.orig	2013-07-31 15:53:07.000000000 +0900
++++ ./plugins-src/rabbitmq-amqp1.0/codegen.py	2013-07-31 15:53:20.000000000 +0900
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python
++#!/usr/local/bin/python
+ import sys
+ import os
+ import re
