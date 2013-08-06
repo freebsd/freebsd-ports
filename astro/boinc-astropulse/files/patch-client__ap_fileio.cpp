bdffb028ce2713085c9bd4baf00c8ffa094a1137
Rene Ladan <rene@FreeBSD.org>
2013-08-06 11:57:04 +0000

    Partially fix build with planned net/boinc-client update (ports/176724).
    There is still a dangling function call to be resolved (see
    http://lists.ssl.berkeley.edu/pipermail/boinc_opt/2013-August/001329.html )

---
+++ astro/boinc-astropulse/files/patch-client__ap_fileio.cpp
@@ -0,0 +1,10 @@
+--- ap_fileio.cpp.orig	2012-02-28 00:01:04.000000000 +0100
++++ ap_fileio.cpp	2013-08-05 22:45:10.000000000 +0200
+@@ -22,6 +22,7 @@
+ #include "windows.h"
+ #endif
+ 
++#include <cmath>
+ #include <cstdio>
+ #include <cstdlib>
+ #include <vector>
