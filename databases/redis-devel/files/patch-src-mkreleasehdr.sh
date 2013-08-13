6d8f784f2c5d14479fc17408eb7a86ba63edc02e
Sergey A. Osokin <osa@FreeBSD.org>
2013-08-13 17:39:57 +0000

    Update from 2.6.14 to 2.8.0-rc2.
    Changes:	https://raw.github.com/antirez/redis/2.8/00-RELEASENOTES

--- databases/redis-devel/files/patch-src-mkreleasehdr.sh
+++ databases/redis-devel/files/patch-src-mkreleasehdr.sh
@@ -1,11 +1,11 @@
---- src/mkreleasehdr.sh.orig	2013-03-12 22:27:27.000000000 +0400
-+++ src/mkreleasehdr.sh	2013-03-12 22:27:37.000000000 +0400
+--- src/mkreleasehdr.sh.orig	2013-08-13 15:39:40.000000000 +0400
++++ src/mkreleasehdr.sh	2013-08-13 15:39:55.000000000 +0400
 @@ -1,6 +1,6 @@
  #!/bin/sh
 -GIT_SHA1=`(git show-ref --head --hash=8 2> /dev/null || echo 00000000) | head -n1`
 -GIT_DIRTY=`git diff --no-ext-diff 2> /dev/null | wc -l`
 +GIT_SHA1="00000000"
 +GIT_DIRTY="0"
+ BUILD_ID=`uname -n`"-"`date +%s`
  test -f release.h || touch release.h
  (cat release.h | grep SHA1 | grep $GIT_SHA1) && \
- (cat release.h | grep DIRTY | grep $GIT_DIRTY) && exit 0 # Already up-to-date
