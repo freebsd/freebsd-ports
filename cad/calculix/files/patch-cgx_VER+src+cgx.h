4524259466f3fac58a001a02b2b8c366e7d110ef
Maho Nakata <maho@FreeBSD.org>
2013-08-16 01:45:13 +0000

    update to 2.6.1
    PR:		 181234
    Submitted by:	Pedro Giffuni <giffunip@tutopia.com>

--- cad/calculix/files/patch-cgx_VER+src+cgx.h
+++ cad/calculix/files/patch-cgx_VER+src+cgx.h
@@ -1,11 +1,11 @@
---- cgx_2.5/src/cgx.h.orig	2009-08-13 15:58:59.000000000 +0900
-+++ cgx_2.5/src/cgx.h	2009-09-03 15:07:10.000000000 +0900
+--- cgx_2.6/src/cgx.h.orig	2009-08-13 15:58:59.000000000 +0900
++++ cgx_2.6/src/cgx.h	2009-09-03 15:07:10.000000000 +0900
 @@ -86,8 +86,8 @@
                               "/yaprod/cae/application/CalculiX/doc/ccx_2.0/ccx.html"}
    #endif
  #else
--  #define     HELPFILE        {"/usr/local/CalculiX/cgx_2.5/doc/cgx/cgx.html",\
--    "/usr/local/CalculiX/ccx_2.5/doc/ccx/ccx.html"}
+-  #define     HELPFILE        {"/usr/local/CalculiX/cgx_2.6/doc/cgx/cgx.html",\
+-    "/usr/local/CalculiX/ccx_2.6/doc/ccx/ccx.html"}
 +   #define     HELPFILE        {"%%DOCSDIR%%/cgx/cgx.html",\
 +     "%%DOCSDIR%%/ccx/ccx.html"}
  #endif
