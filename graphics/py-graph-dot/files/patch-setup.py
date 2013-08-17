fc05dacb4597b6822dd6a443903f52cf11c8cbac
Dmitry Sivachenko <demon@FreeBSD.org>
2013-08-17 17:19:06 +0000

    Update to version 1.8.2, take maintainership.

--- graphics/py-graph-dot/files/patch-setup.py
+++ graphics/py-graph-dot/files/patch-setup.py
@@ -3,20 +3,6 @@ $FreeBSD$
 
 --- setup.py.orig
 +++ setup.py
-@@ -4,12 +4,7 @@
- import os
- 
- 
--try:
--    from setuptools import setup, find_packages
--except ImportError as ie:
--    import ez_setup
--    ez_setup.use_setuptools()
--    from setuptools import setup, find_packages
-+from setuptools import setup, find_packages
- 
- # Startup
- appname = "python-graph-dot"
 @@ -20,7 +15,6 @@
          version = appversion,
          namespace_packages = ["pygraph"],
