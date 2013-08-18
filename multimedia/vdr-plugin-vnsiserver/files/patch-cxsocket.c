b5658fce01d15bca9ce32597601236ed366ecae8
Juergen Lock <nox@FreeBSD.org>
2013-08-18 16:11:00 +0000

    - Update to 12.0 (Frodo) to work with the new multimedia/xbmc-addon-pvr   port. - Convert Makefile header.

--- multimedia/vdr-plugin-vnsiserver/files/patch-cxsocket.c
+++ multimedia/vdr-plugin-vnsiserver/files/patch-cxsocket.c
@@ -1,20 +1,13 @@
 --- a/cxsocket.c
 +++ b/cxsocket.c
-@@ -35,7 +35,7 @@
- #include <sys/types.h>
- #include <sys/socket.h>
- #include <sys/ioctl.h>
--#ifndef __APPLE__
-+#if !defined(__APPLE__) && !defined(__FreeBSD__)
- # include <sys/sendfile.h>
- #endif
- #include <netinet/tcp.h>
-@@ -191,7 +191,7 @@ bool cxSocket::set_multicast(int ttl)
+@@ -48,6 +48,10 @@
+ #include "config.h"
+ #include "cxsocket.h"
  
- bool cxSocket::set_cork(bool state)
++#ifndef MSG_MORE
++#define MSG_MORE 0
++#endif
++
+ cxSocket::~cxSocket()
  {
--#ifdef __APPLE__
-+#if defined(__APPLE__) || defined(__FreeBSD__)
-   return false;
- #else
-   int iCork = state ? 1 : 0;
+   close();
--- multimedia/vdr-plugin-vnsiserver/files/patch-responsepacket.c
+++ multimedia/vdr-plugin-vnsiserver/files/patch-responsepacket.c
@@ -1,10 +1,18 @@
 --- a/responsepacket.c
 +++ b/responsepacket.c
-@@ -27,6 +27,7 @@
+@@ -31,8 +31,15 @@
  #include <arpa/inet.h>
  #include <stdlib.h>
  #include <string.h>
 +#include <inttypes.h>
  
++#ifndef __FreeBSD__
+ #include <asm/byteorder.h>
++#else
++#include <sys/endian.h>
++#define __be64_to_cpu be64toh
++#define __cpu_to_be64 htobe64
++#endif
+ 
  #include "responsepacket.h"
- #include "vdrcommand.h"
+ #include "vnsicommand.h"
