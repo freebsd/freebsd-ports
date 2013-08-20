ec5e9104a3d15ce00ccb53a0531d10da5bb03e97
Wen Heping <wen@FreeBSD.org>
2013-08-20 06:49:44 +0000

    - Update to 3.0.0

--- devel/py-tables/files/patch-setup.py
+++ devel/py-tables/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2012-03-25 19:20:35.000000000 +0900
-+++ setup.py	2012-03-25 19:22:53.000000000 +0900
-@@ -96,8 +96,8 @@
+--- setup.py.orig	2013-08-20 10:38:37.000000000 +0800
++++ setup.py	2013-08-20 10:40:44.000000000 +0800
+@@ -133,8 +133,8 @@
  debug = '--debug' in sys.argv
  
  # Global variables
@@ -11,19 +11,19 @@
  optional_libs = []
  data_files = []    # list of data files to add to packages (mainly for DLL's)
  
-@@ -121,13 +121,12 @@
+@@ -160,13 +160,13 @@
      add_from_path("CPATH", default_header_dirs)
      add_from_path("C_INCLUDE_PATH", default_header_dirs)
      add_from_flags("CPPFLAGS", "-I", default_header_dirs)
 -    default_header_dirs.extend(['/usr/include', '/usr/local/include'])
--
-+    default_header_dirs = ['/usr/include', '/usr/local/include','%%LOCALBASE%%/include']
++    default_header_dirs.extend(['/usr/include', '/usr/local/include','%%LOCALBASE%%/include'])
+ 
      default_library_dirs = []
      add_from_flags("LDFLAGS", "-L", default_library_dirs)
      default_library_dirs.extend(
          os.path.join(_tree, _arch)
--        for _tree in ('/', '/usr', '/usr/local')
-+	for _tree in ('/', '/usr', '/usr/local','%%LOCALBASE%%')
-         for _arch in ('lib64', 'lib') )
+-        for _tree in ('/usr/local', '/sw', '/opt', '/opt/local', '/usr', '/')
++        for _tree in ('/usr/local', '/sw', '/opt', '/opt/local', '/usr', '/','%%LOCALBASE%%')
+             for _arch in ('lib64', 'lib'))
      default_runtime_dirs = default_library_dirs
  
