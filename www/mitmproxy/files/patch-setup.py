3f0f7d3b33dd03e1765b9419e4f441496dc9e927
William Grzybowski <wg@FreeBSD.org>
2013-08-21 16:14:48 +0000

    www/mitmproxy: update to 0.9.1
    - Update to 0.9.1 [1]
    - Fix flask dependency name
    - Add LICENSE (MIT)
    - Remove leading article from COMMENT
    
    Changes: https://github.com/mitmproxy/mitmproxy/blob/v0.9.1/CHANGELOG
    
    PR:		ports/180747
    Submitted by:	Hung-Yi Chen <gaod hychen.org> (maintainer)

--- www/mitmproxy/files/patch-setup.py
+++ www/mitmproxy/files/patch-setup.py
@@ -1,14 +1,22 @@
---- setup.py.orig	2012-04-09 11:55:00.000000000 +0800
-+++ setup.py	2012-11-08 01:43:57.000000000 +0800
+--- setup.py.orig	2013-07-23 03:08:39.785592808 +0800
++++ setup.py	2013-07-23 03:09:19.367592415 +0800
 @@ -1,4 +1,5 @@
  from distutils.core import setup
 +from setuptools import setup
  import fnmatch, os.path
  from libmproxy import version
  
-@@ -92,5 +93,4 @@
+@@ -92,13 +93,4 @@
              "Topic :: Internet :: Proxy Servers",
              "Topic :: Software Development :: Testing"
          ],
--        install_requires=['urwid>=1.0', 'pyasn1', 'pyopenssl>=0.12', "PIL", "lxml"],
+-        install_requires=[
+-            "netlib>=%s"%version.VERSION,
+-            "urwid>=1.1",
+-            "pyasn1>0.1.2",
+-            "pyopenssl>=0.13",
+-            "PIL",
+-            "lxml",
+-            "flask"
+-        ],
  )
