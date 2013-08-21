4d29a17759a374d8a72695cfb4cefa07197c5be0
wg <wg@FreeBSD.org>
2013-08-21 14:42:17 +0000

    Netlib is a collection of network utility classes, used by the pathod and mitmproxy projects.
    It differs from other projects in some fundamental
    respects, because both pathod and mitmproxy often
    need to violate standards.
    
    WWW: https://pypi.python.org/pypi/netlib
    
    PR:		ports/180746
    Submitted by:	Hung-Yi Chen <gaod hychen.org>

---
+++ net/py-netlib/files/patch-setup.py
@@ -0,0 +1,8 @@
+--- setup.py.orig	2013-07-23 03:39:16.157592949 +0800
++++ setup.py	2013-07-23 03:39:30.392145219 +0800
+@@ -88,5 +88,4 @@
+             "Topic :: Software Development :: Testing :: Traffic Generation",
+             "Topic :: Internet :: WWW/HTTP",
+         ],
+-        install_requires=["pyasn1>0.1.2", "pyopenssl>=0.12"],
+ )
