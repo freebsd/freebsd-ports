822a0ec9aaaf231fbdccba52d6bfcef01d76dc4e
olgeni <olgeni@FreeBSD.org>
2013-08-25 16:13:06 +0000

    - Upgrade to version 1.0.0. - Replace PKGNAMESUFFIX with PKGNAMEPREFIX.

--- devel/ipython/files/patch-setupbase.py
+++ devel/ipython/files/patch-setupbase.py
@@ -3,26 +3,16 @@ $FreeBSD$
 
 --- setupbase.py.orig
 +++ setupbase.py
-@@ -201,7 +201,8 @@
+@@ -208,7 +208,7 @@
      """
  
      docdirbase  = pjoin('share', 'doc', 'ipython')
 -    manpagebase = pjoin('share', 'man', 'man1')
 +    manpagebase = pjoin('man', 'man1')
-+    examplebase = pjoin('share', 'examples', 'ipython')
  
      # Simple file lists can be made by hand
      manpages = [f for f in glob(pjoin('docs','man','*.1.gz')) if isfile(f)]
-@@ -215,7 +216,7 @@
-     example_files = make_dir_struct(
-         'data',
-         pjoin('docs','examples'),
--        pjoin(docdirbase,'examples')
-+        pjoin(examplebase)
-     )
-     manual_files = make_dir_struct(
-         'data',
-@@ -225,7 +226,6 @@
+@@ -232,7 +232,6 @@
  
      # And assemble the entire output list
      data_files = [ (manpagebase, manpages),
