8d2e6ab41968936f3c4b720624d2426f738c1524
Jason Helfman <jgh@FreeBSD.org>
2013-08-27 07:08:44 +0000

    - update to 1.0.2 - properly support DOCS, and add option

--- databases/pg_activity/files/patch-setup.py
+++ databases/pg_activity/files/patch-setup.py
@@ -1,11 +1,8 @@
---- ./setup.py.orig	2013-04-13 22:35:31.000000000 -0700
-+++ ./setup.py	2013-04-13 22:37:21.000000000 -0700
-@@ -17,7 +17,5 @@
-         "psutil >= 0.4.1",
+--- ./setup.py.orig	2013-08-22 23:42:26.000000000 -0700
++++ ./setup.py	2013-08-22 23:42:36.000000000 -0700
+@@ -27,4 +27,5 @@
          "psycopg2 >= 2.2.1",
      ],
--    data_files = [
--        ('/usr/share/man/man1', ['docs/man/pg_activity.1'])
--    ],
+     data_files = data_files,
 +    zip_safe=False,
  )
