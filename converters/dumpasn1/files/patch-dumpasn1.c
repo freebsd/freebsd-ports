fdc61e235aedf31064c561ae08a383ca37321dc0
ale <ale@FreeBSD.org>
2013-08-27 09:08:47 +0000

    Update to 20130805 release.

--- converters/dumpasn1/files/patch-dumpasn1.c
+++ converters/dumpasn1/files/patch-dumpasn1.c
@@ -1,15 +1,6 @@
 --- dumpasn1.c.orig	2013-02-14 12:03:56.000000000 +0100
 +++ dumpasn1.c	2013-02-14 12:13:31.000000000 +0100
-@@ -60,6 +60,8 @@
- 
- /* Useful defines */
- 
-+typedef unsigned char	BYTE;
-+
- #ifndef TRUE
-   #define FALSE	0
-   #define TRUE	( !FALSE )
-@@ -356,11 +358,13 @@
+@@ -356,11 +356,13 @@
     beginning with a '$' uses the appropriate environment variable.  In
     addition under Unix we also walk down $PATH looking for it */
  
