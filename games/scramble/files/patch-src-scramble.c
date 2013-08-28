c637b4560ee8b683de5fbd2ae8b3af5bd254b0c5
Dmitry Marakasov <amdmi3@FreeBSD.org>
2013-08-28 04:09:52 +0000

    - Fix build with clang - Remove unneeded patches

--- games/scramble/files/patch-src-scramble.c
+++ games/scramble/files/patch-src-scramble.c
@@ -1,51 +1,20 @@
---- src/scramble.c.orig	Thu Nov 10 05:20:17 2005
-+++ src/scramble.c	Thu Nov 10 05:22:01 2005
-@@ -290,14 +290,13 @@
+--- src/scramble.c.orig	2013-08-28 03:08:02.758225796 +0400
++++ src/scramble.c	2013-08-28 03:08:42.456378896 +0400
+@@ -534,7 +534,7 @@
+ 
+ /*****************************************************
   ****************************************************/
- void putLetter(char * ch, int s)
+-int moveLetter(int i)
++void moveLetter(int i)
  {
--	if(!running)
--		return;
--
- 	char ch2[2];
-+	SDL_Rect dest;
- 	ch2[0] = toupper(ch[0]);
- 	ch2[1] = 0;
--
--	SDL_Rect dest;
-+
-+	if(!running)
-+		return;
+ 	int minx, maxx, miny, maxy, advance;
+ 	i--;
+@@ -1141,7 +1141,7 @@
  
- 	s--;
- 	
-@@ -694,6 +693,8 @@
- 
- 	for(i = 0; i < numberCurrentWords; i++) {
- 		if(strcmp(wordAttempt, currentWordSet[i]) == 0 && currentWordSetShow[i] == 0) {
-+			double per;
-+
- 			currentWordSetShow[i] = 1;
- 			playSound(hitSound);	
- 
-@@ -706,7 +707,7 @@
- 			stateChanged = 1;
- 			redrawAll = 1;
- 			wordsFound++;
--			double per = ((double)wordsFound / (double)numberCurrentWords);
-+			per = ((double)wordsFound / (double)numberCurrentWords);
- 			if(per > percentage) {
- 				nextLevel = 1;
- 				if(nextLevel % 5 && percentage < .8)
-@@ -1127,9 +1128,10 @@
+ /*****************************************************
   ****************************************************/
- void letter(char c)
+-inline void SE_CheckEvents()
++void SE_CheckEvents()
  {
-+	int i;
- 	if(!running)
- 		return;
--	int i;
-+
- 	for(i = 0; i < 6; i++) {
- 		if(c == letters[i].letter[0] && !letters[i].played) {
- 			playSound(clickSound);
+ 
+ 	SDL_Event event;
