4a254c77c9df67fc3af6a2c8cef4f822334a58d1
madpilot <madpilot@FreeBSD.org>
2013-08-28 09:01:38 +0000

    Fix build on recent current with clang.

--- graphics/amide/files/patch-src-alignment_mutual_information.c
+++ graphics/amide/files/patch-src-alignment_mutual_information.c
@@ -1,5 +1,5 @@
---- src/alignment_mutual_information.c.orig	2011-07-15 07:08:17.000000000 +0200
-+++ src/alignment_mutual_information.c	2011-09-19 14:55:39.733328817 +0200
+--- src/alignment_mutual_information.c.orig	2012-01-22 17:25:06.000000000 +0100
++++ src/alignment_mutual_information.c	2013-08-28 10:30:30.265534615 +0200
 @@ -28,6 +28,13 @@
  #include "amitk_data_set_DOUBLE_0D_SCALING.h"
  #include "alignment_mutual_information.h"
@@ -14,3 +14,12 @@
  /* this algorithm will calculate the amount of mutual information between two data sets in their current orientations    */
  /* it is a re-write of the original algorithm for purposes of improved speed. the hope is that it won't affect accuracy. */
  /* rather than computing mutual information for the whole volume of data, the algorithm computes it for three orthogonal */
+@@ -209,7 +216,7 @@
+       //g_print("\t\%i", mutual_information_array[i][j] );  // for point-wise counts
+       // g_print("\t\%4.3f", incremental_mi );               // for point-wise probability
+       
+-      if isinf(incremental_mi) {
++      if(isinf(incremental_mi)) {
+         //count it (because lots and lots of zeroes mean bad registration) and go to the next loop
+         mi_nan_count++;
+         // this is most often a problem when the incemental MI is infinity, due to divide by zero
