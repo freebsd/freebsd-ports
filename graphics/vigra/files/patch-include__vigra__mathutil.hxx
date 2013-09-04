fa95fbc45d12fabd06d6a5207bc0ab7f47f58667
bapt <bapt@FreeBSD.org>
2013-09-04 16:14:12 +0000

    Remove patch that is not needed anymore (tested on 8.3 and 9.1) and that makes clang+libc++ failing to build properly the port

--- graphics/vigra/files/patch-include__vigra__mathutil.hxx
+++ graphics/vigra/files/patch-include__vigra__mathutil.hxx
@@ -1,14 +1,5 @@
 --- include/vigra/mathutil.hxx.orig	2011-09-20 18:38:36.000000000 +0000
 +++ include/vigra/mathutil.hxx
-@@ -149,7 +149,7 @@ VIGRA_DEFINE_UNSIGNED_ABS(unsigned long 
- VIGRA_DEFINE_MISSING_ABS(signed char)
- VIGRA_DEFINE_MISSING_ABS(signed short)
- 
--#if defined(_MSC_VER) && _MSC_VER < 1600
-+#if defined(_MSC_VER) && _MSC_VER < 1600 || defined(__GNUC__) && (__GNUC__ < 4 || __GNUC__ == 4 && __GNUC_MINOR__ < 3) || defined(__clang__)
- VIGRA_DEFINE_MISSING_ABS(signed long long)
- #endif
- 
 @@ -553,7 +553,12 @@ VIGRA_DEFINE_NORM(int)
  VIGRA_DEFINE_NORM(unsigned int)
  VIGRA_DEFINE_NORM(long)
