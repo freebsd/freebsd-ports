527c438444bb3b04872cccbcd0aa48dce31d9f4f
hrs <hrs@FreeBSD.org>
2013-09-04 06:12:09 +0000

    Fix build on 10.0 and 8.X.
    Reported by:	Lars Eggert, pkg-fallout

--- net/packetdrill/files/patch-platforms.h
+++ net/packetdrill/files/patch-platforms.h
@@ -1,18 +1,18 @@
---- platforms.h.orig	2013-07-24 19:55:39.000000000 +0900
-+++ platforms.h	2013-07-24 19:56:36.000000000 +0900
-@@ -43,11 +43,15 @@
+--- platforms.h.orig	2013-07-21 04:53:41.000000000 +0900
++++ platforms.h	2013-09-04 10:34:56.000000000 +0900
+@@ -43,12 +43,14 @@
  
  #if defined(__FreeBSD__)
  
-+#include <sys/param.h>
++#include <paths.h>
  #define USE_LIBPCAP             1
- #define TUN_PATH                "/dev/tun0"
+-#define TUN_PATH                "/dev/tun0"
++#define TUN_PATH                _PATH_DEV "tun0"
  #define TUN_DEV                 "tun0"
  
  #define HAVE_TCP_INFO           1
-+#if __FreeBSD_version > 1000028
-+#define	HAVE_FMEMOPEN		1 
-+#endif
  
++#include "ports_config.h"
  #include "open_memstream.h"
  #include "fmemopen.h"
+ 
