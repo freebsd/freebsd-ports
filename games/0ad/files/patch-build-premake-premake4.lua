56a69dc50de38274346095dd6b872378b75aa7a6
Guido Falsi <madpilot@FreeBSD.org>
2013-09-05 13:08:30 +0000

    - Update to 0.0.14 (Alpha 14 "Naukratis") - Add explicit dependency on pkgconf - Convert to USES=gmake

--- games/0ad/files/patch-build-premake-premake4.lua
+++ games/0ad/files/patch-build-premake-premake4.lua
@@ -1,6 +1,6 @@
---- build/premake/premake4.lua.orig	2013-03-07 14:49:49.000000000 +0100
-+++ build/premake/premake4.lua	2013-04-03 00:14:47.033326555 +0200
-@@ -136,7 +136,7 @@
+--- build/premake/premake4.lua.orig	2013-08-27 04:42:56.000000000 +0200
++++ build/premake/premake4.lua	2013-09-03 15:32:07.321963983 +0200
+@@ -138,7 +138,7 @@
  
  function project_set_build_flags()
  
@@ -9,15 +9,7 @@
  	if not _OPTIONS["icc"] and (os.is("windows") or not _OPTIONS["minimal-flags"]) then
  		-- adds the -Wall compiler flag
  		flags { "ExtraWarnings" } -- this causes far too many warnings/remarks on ICC
-@@ -217,7 +217,6 @@
- 
- 					-- enable security features (stack checking etc) that shouldn't have
- 					-- a significant effect on performance and can catch bugs
--					"-fstack-protector-all",
- 					"-D_FORTIFY_SOURCE=2",
- 
- 					-- always enable strict aliasing (useful in debug builds because of the warnings)
-@@ -343,11 +342,11 @@
+@@ -356,11 +356,11 @@
  		-- X11 includes may be installed in one of a gadzillion of three places
  		-- Famous last words: "You can't include too much! ;-)"
  		includedirs {
