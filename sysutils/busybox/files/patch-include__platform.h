6322d7a7acb70996e1b10532d64a46d181d07268
mandree <mandree@FreeBSD.org>
2013-09-05 07:00:07 +0000

    Attempt to fix 10-CURRENT build failure involving strchrnul.

--- sysutils/busybox/files/patch-include__platform.h
+++ sysutils/busybox/files/patch-include__platform.h
@@ -1,6 +1,6 @@
---- ./include/platform.h.orig	2013-01-14 05:19:43.000000000 +0100
-+++ ./include/platform.h	2013-01-21 21:26:34.000000000 +0100
-@@ -433,6 +433,15 @@
+--- ./include/platform.h.orig	2013-06-29 17:00:01.000000000 +0200
++++ ./include/platform.h	2013-09-05 08:58:27.000000000 +0200
+@@ -433,6 +433,18 @@
  
  #if defined(__FreeBSD__)
  # undef HAVE_STRCHRNUL
@@ -11,6 +11,9 @@
 +#   define HAVE_GETLINE 1 /* FreeBSD added getdelim(), getline(),
 +			     stpncpy(), strnlen(), wcsnlen(),
 +			     wcscasecmp(), and wcsncasecmp() in 800067 */
++#  endif
++#  if __FreeBSD_version >= 1000029
++#   define HAVE_STRCHRNUL 1 /* added short before 1000029 */
 +#  endif
 +# endif
  #endif
