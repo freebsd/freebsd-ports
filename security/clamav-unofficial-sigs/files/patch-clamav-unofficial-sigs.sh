86b34211daba9859632b45574289b608dcc74dfb
wg <wg@FreeBSD.org>
2013-09-05 15:33:39 +0000

    security/clamav-unofficial-sigs: updte to 3.7.2
    - Update to 3.7.2 [1]
    - USES perl5
    - Add DOCS to options
    - NOPORTDOCS -> PORT_OPTIONS:MDOCS
    
    PR:		ports/181814
    Submitted by:	Marko Njezic <sf maxempire.com> (maintainer)

--- security/clamav-unofficial-sigs/files/patch-clamav-unofficial-sigs.sh
+++ security/clamav-unofficial-sigs/files/patch-clamav-unofficial-sigs.sh
@@ -1,15 +1,15 @@
---- clamav-unofficial-sigs.sh	2010-06-06 18:43:07.000000000 +0200
-+++ clamav-unofficial-sigs.sh.patched	2010-06-07 18:42:17.000000000 +0200
-@@ -19,7 +19,7 @@
+--- clamav-unofficial-sigs.sh	2013-08-27 18:08:25.000000000 +0200
++++ clamav-unofficial-sigs.sh.patched	2013-09-04 16:10:35.000000000 +0200
+@@ -16,7 +16,7 @@
  #                                                                              #
  ################################################################################
  
 -default_config="/etc/clamav-unofficial-sigs.conf"
 +default_config="%%PREFIX%%/etc/clamav-unofficial-sigs.conf"
  
- version="v3.7.1 (updated 2010-06-06)"
+ version="v3.7.2 (updated 2013-08-25)"
  output_ver="
-@@ -407,6 +407,8 @@
+@@ -418,6 +418,8 @@
            exit
            ;;
        r)  no_default_config
