f985cdfe4c076d6ed44dd0170078863209895d79
Alexey Dokuchaev <danfe@FreeBSD.org>
2013-09-05 16:33:40 +0000

    - Attempt to unbreak the build against Clang - Get rid of bogus dependency on devel/py-subversion - Modernize LIB_DEPENDS syntax, copy files with -a - Add an explicit dependency on pkgconfig
    Reported by:	pkg-fallout

--- games/gtkradiant/files/patch-makeversion.py
+++ games/gtkradiant/files/patch-makeversion.py
@@ -1,6 +1,15 @@
 --- ./makeversion.py.orig	Sun Feb 12 16:47:01 2006
 +++ ./makeversion.py	Thu Mar 16 16:09:46 2006
-@@ -82,7 +82,7 @@
+@@ -39,8 +39,6 @@
+ 
+ import sys, re, string, os
+ 
+-import svn
+-
+ def get_version():
+   # version
+   f = open('include/version.default', 'r')
+@@ -82,7 +80,7 @@ def radiant_makeversion(append_about):
      line = f.readline()
      f.close()
    else:
