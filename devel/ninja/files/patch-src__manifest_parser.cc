f984209e903ba81f5dd1559bfa5bb525febcd883
Baptiste Daroussin <bapt@FreeBSD.org>
2013-09-06 15:08:07 +0000

    Fix build with libc++

---
+++ devel/ninja/files/patch-src__manifest_parser.cc
@@ -0,0 +1,10 @@
+--- ./src/manifest_parser.cc.orig	2013-06-04 20:47:31.000000000 +0200
++++ ./src/manifest_parser.cc	2013-09-06 16:58:05.243065037 +0200
+@@ -15,6 +15,7 @@
+ #include "manifest_parser.h"
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <vector>
+ 
+ #include "graph.h"
