e3c4ac79c57140230ad7e570f56b0137793600e1
nox <nox@FreeBSD.org>
2013-09-06 21:45:37 +0000

    (Attempt to) Fix build with clang+libc++.
    Submitted by:	pb2 via bapt

--- multimedia/libxine/files/patch-include-xine-post.h
+++ multimedia/libxine/files/patch-include-xine-post.h
@@ -1,6 +1,16 @@
 --- include/xine/post.h.orig
 +++ include/xine/post.h
-@@ -397,7 +397,7 @@ static xine_post_api_parameter_t temp_p[
+@@ -23,6 +23,9 @@
+ #ifndef XINE_POST_H
+ #define XINE_POST_H
+ 
++/* for offsetof() */
++#include <stddef.h>
++
+ #include <xine.h>
+ #include <xine/video_out.h>
+ #include <xine/audio_out.h>
+@@ -397,7 +400,7 @@ static xine_post_api_parameter_t temp_p[
  
  #define PARAM_ITEM( param_type, var, enumv, min, max, readonly, descr ) \
  { param_type, #var, sizeof(temp_s.var), \
