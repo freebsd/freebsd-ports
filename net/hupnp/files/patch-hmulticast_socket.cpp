b61cf1a74ae38385d3b4616f6352636dd7f20d57
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2013-09-09 22:41:43 +0000

    - Fix the build with clang and libc++. - Point to the new domain in WWW. - Convert Makefile header. - Get rid of files/patch-herqq.pro by passing the proper arguments in   QMAKE_ARGS.
    fluffy is usually responsive to PRs, but I ended up asking bapt for an
    approval here because hupnp is a dependency of kdelibs4 and I want to reduce
    the likelihood of an avalanche of PRs and complaints about KDE4 not building
    on -CURRENT (there are other ports to fix, though).
    
    PR:		ports/181974
    Approved by:	portmgr (bapt)

--- net/hupnp/files/patch-hmulticast_socket.cpp
+++ net/hupnp/files/patch-hmulticast_socket.cpp
@@ -1,8 +1,10 @@
---- ./hupnp/src/socket/hmulticast_socket.cpp.orig	2011-03-28 00:42:32.000000000 +1100
-+++ ./hupnp/src/socket/hmulticast_socket.cpp	2011-08-08 11:59:59.868629425 +1100
-@@ -27,6 +27,8 @@
+--- ./hupnp/src/socket/hmulticast_socket.cpp.orig	2011-03-28 00:42:32.000000000 +0300
++++ ./hupnp/src/socket/hmulticast_socket.cpp	2013-09-10 00:35:26.000000000 +0300
+@@ -26,7 +26,10 @@
+ #include <winsock2.h>
  #include <ws2tcpip.h>
  #else
++#include <sys/types.h>
  #include <arpa/inet.h>
 +#include <netinet/in.h>
 +#include <sys/socket.h>
