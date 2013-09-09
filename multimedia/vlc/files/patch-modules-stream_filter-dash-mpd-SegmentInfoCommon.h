c7313bae5ef9f5efc9ddd4545748d764dfdc6f40
nox <nox@FreeBSD.org>
2013-09-09 18:05:40 +0000

    - Update to 2.0.8 . - Add subtitle, video, and audio track scrolling to VLC's ncurses.c   module. [1] - Add GME knob. (audio/libgme dependency) [2] - Fix build with clang+libc++. [3]
    PR:		ports/181596 [1], ports/181909 [3]
    Submitted by:	Cam Karnes <karnes.cameron@gmail.com> [1]
    Submitted by:	pawel (on -multimedia) [2]
    Submitted by:	"O. Hartmann" <ohartman@zedat.fu-berlin.de>, bapt via pb2 [3]
    Obtained from:	vlc upstream git [1]

---
+++ multimedia/vlc/files/patch-modules-stream_filter-dash-mpd-SegmentInfoCommon.h
@@ -0,0 +1,10 @@
+--- modules/stream_filter/dash/mpd/SegmentInfoCommon.h.orig
++++ modules/stream_filter/dash/mpd/SegmentInfoCommon.h
+@@ -27,6 +27,7 @@
+ 
+ #include <string>
+ #include <list>
++#include <ctime>
+ 
+ namespace dash
+ {
