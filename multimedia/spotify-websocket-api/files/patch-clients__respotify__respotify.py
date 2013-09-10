75911882389a09e4211f21ccdcff43a239bffef1
Rene Ladan <rene@FreeBSD.org>
2013-09-10 20:17:11 +0000

    Fix some crashes: - when entering the 'album' or 'artist' command without a parameter - after displaying the root playlist with 'list'
    Bump PORTREVISION

--- multimedia/spotify-websocket-api/files/patch-clients__respotify__respotify.py
+++ multimedia/spotify-websocket-api/files/patch-clients__respotify__respotify.py
@@ -1,6 +1,37 @@
---- clients/respotify/respotify.py.orig	2013-08-08 20:18:57.000000000 +0200
-+++ clients/respotify/respotify.py	2013-08-09 00:15:15.000000000 +0200
-@@ -278,7 +278,7 @@
+--- ./clients/respotify/respotify.py.orig	2013-09-10 22:10:39.000000000 +0200
++++ ./clients/respotify/respotify.py	2013-09-10 22:11:58.000000000 +0200
+@@ -88,8 +88,10 @@
+         print "Playlists\n"
+         index = 1
+         for playlist in rootlist:
+-            print " ["+str(index)+"] "+playlist.getName()
+-            index += 1
++            name = playlist.getName()
++            if name != None:
++                print " ["+str(index)+"] "+name
++                index += 1
+     else:
+         try:
+             if len(rootlist) >= int(args[0][0]):
+@@ -115,7 +117,7 @@
+ 
+ 
+ def command_album(*args):
+-    if args[0][0] == "" or current_playlist is None:
++    if len(*args) == 0 or args[0][0] == "" or current_playlist is None:
+         return
+ 
+     index = int(args[0][0])-1
+@@ -127,7 +129,7 @@
+ 
+ 
+ def command_artist(*args):
+-    if args[0][0] == "" or current_playlist is None:
++    if len(*args) == 0 or args[0][0] == "" or current_playlist is None:
+         return
+ 
+     index = int(args[0][0])-1
+@@ -278,7 +280,7 @@
      if spotify.logged_in():
          os.system("kill `pgrep -f respotify-helper` &> /dev/null")
          uri_resolver = subprocess.Popen([sys.executable, "respotify-helper.py",
