e236b0d5d0bf30e07d1c26e06ba427a7bb47a2b8
Sergey A. Osokin <osa@FreeBSD.org>
2013-09-11 15:17:12 +0000

    Update from 4.0.16 to 4.0.17:
    o) www/rubygem-passenger;
    o) third-party modules for www/nginx and www/nginx-devel.

--- www/nginx-devel/files/extra-patch-passenger-build-nginx.rb
+++ www/nginx-devel/files/extra-patch-passenger-build-nginx.rb
@@ -1,5 +1,5 @@
---- ../passenger-4.0.16/build/nginx.rb.orig	2013-09-07 14:09:13.000000000 +0400
-+++ ../passenger-4.0.16/build/nginx.rb	2013-09-07 14:10:31.000000000 +0400
+--- ../passenger-4.0.17/build/nginx.rb.orig	2013-09-07 14:09:13.000000000 +0400
++++ ../passenger-4.0.17/build/nginx.rb	2013-09-07 14:10:31.000000000 +0400
 @@ -32,15 +32,12 @@
  desc "Build Nginx support files"
  task :nginx => [
