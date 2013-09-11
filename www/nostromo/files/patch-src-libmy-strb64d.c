162ff0b14d301ba126e7b60bc37cecd898bf0b88
madpilot <madpilot@FreeBSD.org>
2013-09-11 19:57:07 +0000

    - Modify clang patch to match upstream
    PR:		ports/181493
    Submitted by:   Adrian Steinmann <ast@NetBSD.org> (maintainer from
    		different email, as followup)

--- www/nostromo/files/patch-src-libmy-strb64d.c
+++ www/nostromo/files/patch-src-libmy-strb64d.c
@@ -1,11 +1,19 @@
---- src/libmy/strb64d.c.orig	2005-06-04 10:30:04.000000000 +0200
-+++ src/libmy/strb64d.c	2013-08-23 19:17:25.700331962 +0200
-@@ -54,7 +54,7 @@
+--- src/libmy/strb64d.c.orig	2013-09-10 09:58:37.814139989 +0200
++++ src/libmy/strb64d.c	2013-09-10 10:00:47.489957357 +0200
+@@ -47,14 +47,14 @@
+ int
+ strb64d(char *dst, const char *src, const int dsize)
+ {
+-	int		ch, i, j = 0;
++	int		ch, i, j;
+ 	char		out[3];
+ 	unsigned char	in_a[4], in_b[4];
+ 
  	dst[0] = '\0';
  
  	while (1) {
 -		for (i = 0, j = j; i < 4; i++, j++) {
-+		for (i = 0; i < 4; i++, j++) {
++		for (i = 0, j = 0; i < 4; i++, j++) {
  			if (src[j] == '\0')
  				goto quit;
  			ch = src[j];
