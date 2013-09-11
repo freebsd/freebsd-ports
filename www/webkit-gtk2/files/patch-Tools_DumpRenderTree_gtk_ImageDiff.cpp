c0ff713fb9820888e2d8ea3ab0d5187523647b64
Koop Mast <kwm@FreeBSD.org>
2013-09-11 11:28:45 +0000

    Fix the build on recent head with libc++ [1]. Also when building with clang make the build quieter by not spamming about c11 keywords.
    Submitted by:	stefanf@ (combined patches with) [1]

---
+++ www/webkit-gtk2/files/patch-Tools_DumpRenderTree_gtk_ImageDiff.cpp
@@ -0,0 +1,10 @@
+--- Tools/DumpRenderTree/gtk/ImageDiff.cpp.orig	2013-09-11 12:29:19.000000000 +0200
++++ Tools/DumpRenderTree/gtk/ImageDiff.cpp	2013-09-11 12:29:38.000000000 +0200
+@@ -30,6 +30,7 @@
+ #include <algorithm>
+ #include <cmath>
+ #include <cstdio>
++#include <cstdlib>
+ #include <cstring>
+ #include <gdk/gdk.h>
+ 
