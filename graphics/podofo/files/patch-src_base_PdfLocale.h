0bc3d3fc40f4d42c1a2c00e8402628b821a8670d
madpilot <madpilot@FreeBSD.org>
2013-09-11 21:55:49 +0000

    Patch to remove forward declaration of a standard C++ class in installed include file.
    Approved by:	pawel (maintainer)

---
+++ graphics/podofo/files/patch-src_base_PdfLocale.h
@@ -0,0 +1,11 @@
+--- src/base/PdfLocale.h.orig	2010-10-25 19:19:15.000000000 +0200
++++ src/base/PdfLocale.h	2013-09-10 18:00:08.338272701 +0200
+@@ -1,8 +1,6 @@
+ #ifndef PODOFO_PDFLOCALE_H
+ #define PODOFO_PDFLOCALE_H
+ 
+-namespace std { class ios_base; };
+-
+ namespace PoDoFo {
+ 
+ /**
