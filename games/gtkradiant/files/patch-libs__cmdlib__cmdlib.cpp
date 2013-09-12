d8d4744d3105a55c31b6d8c04b2f84251f1333b7
Alexey Dokuchaev <danfe@FreeBSD.org>
2013-09-12 15:26:52 +0000

    Add a couple of missing #include <stdlib.h>'s to please contemporary Clang.
    Reported by:	pkg-fallout

---
+++ games/gtkradiant/files/patch-libs__cmdlib__cmdlib.cpp
@@ -0,0 +1,10 @@
+--- libs/cmdlib/cmdlib.cpp.orig
++++ libs/cmdlib/cmdlib.cpp
+@@ -27,6 +27,7 @@ Foundation, Inc., 51 Franklin St, Fifth 
+ 
+ #include <string.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ 
+ #include "string/string.h"
+ #include "os/path.h"
