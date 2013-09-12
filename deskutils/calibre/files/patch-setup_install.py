5101af06591caa5936fee0eda0e6ea4ce7defa86
madpilot <madpilot@FreeBSD.org>
2013-09-12 11:45:41 +0000

    - Update to 1.2.0
    PR:		ports/181939
    Submitted by:	Rusty Nejdl <rnejdl@ringofsaturn.com> (maintainer)

--- deskutils/calibre/files/patch-setup_install.py
+++ deskutils/calibre/files/patch-setup_install.py
@@ -1,11 +1,11 @@
---- setup/install.py.orig	2011-06-14 14:16:28.000000000 -0500
-+++ setup/install.py	2011-06-14 14:16:39.000000000 -0500
-@@ -12,7 +12,7 @@
-         __appname__, __version__)
- 
- HEADER = '''\
--#!/usr/bin/env python2
-+#!/usr/bin/env python2.7
- 
- """
- This is the standard runscript for all of calibre's tools.
+--- setup/install.py.orig	2013-09-06 06:28:39.000000000 +0200
++++ setup/install.py	2013-09-10 22:34:23.970135119 +0200
+@@ -160,7 +160,7 @@
+         self.info('Installing mount helper to '+ dest)
+         cflags = os.environ.get('OVERRIDE_CFLAGS', '-Wall -pedantic')
+         cflags = shlex.split(cflags)
+-        p = subprocess.Popen(['gcc']+cflags+[src, '-o', dest])
++        p = subprocess.Popen([os.getenv('CC', 'gcc')]+cflags+[src, '-o', dest])
+         ret = p.wait()
+         if ret != 0:
+             return warn()
