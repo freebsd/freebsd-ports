d1cfa261c4ccf5fc1e3f7edf43be1be0c5e966bb
Brad Davis <brd@FreeBSD.org>
2013-09-13 01:32:38 +0000

    - Update net-mgmt/collectd5 to 5.4.0 (by me) - Convert to the new options layout like net-mgmt/collectd (by maintainer: Krzysztof Stryjek <ports@bsdserwis.com>) - Clean up some options and simplify the CONFIGURE_ARGS (by swills@)
    PR:		181443
    Submitted by:	brd@
    Reviewed by:	swills@
    Approved by:	Krzysztof Stryjek <ports@bsdserwis.com> (maintainer)

--- net-mgmt/collectd5/files/patch-version-gen.sh
+++ net-mgmt/collectd5/files/patch-version-gen.sh
@@ -1,9 +1,10 @@
---- ./version-gen.sh.orig	2013-05-21 21:22:23.000000000 +0000
-+++ ./version-gen.sh	2013-05-21 21:23:19.000000000 +0000
+--- version-gen.sh.orig	2013-08-19 10:57:47.000000000 -0600
++++ version-gen.sh	2013-08-19 10:58:15.000000000 -0600
 @@ -1,13 +1,3 @@
 -#!/usr/bin/env bash
--
--DEFAULT_VERSION="5.3.0.git"
++#!/bin/sh
+ 
+-DEFAULT_VERSION="5.4.0.git"
 -
 -VERSION="`git describe 2> /dev/null | sed -e 's/^collectd-//'`"
 -
@@ -12,7 +13,6 @@
 -fi
 -
 -VERSION="`echo \"$VERSION\" | sed -e 's/-/./g'`"
-+#!/bin/sh
- 
+-
 -echo -n "$VERSION"
-+echo -n "5.3.0.git"
++echo -n '5.4.0.git'
