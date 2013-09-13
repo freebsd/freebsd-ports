6b36b9b01cab021e359c70aeba25e884a2d20d17
Dmitry Marakasov <amdmi3@FreeBSD.org>
2013-09-13 18:34:10 +0000

    Fix build with clang/libc++

---
+++ games/connectfive/files/patch-AdjacentPosition.cpp
@@ -0,0 +1,10 @@
+--- AdjacentPosition.cpp.orig	2004-01-04 09:19:04.000000000 +0300
++++ AdjacentPosition.cpp	2013-09-13 20:22:10.991226348 +0400
+@@ -32,6 +32,7 @@
+ 
+ // Header Files #############################################################
+ #include <stdio.h>
++#include <stdlib.h> // for exit()
+ 
+ #include "AdjacentPosition.h"
+ 
--- games/connectfive/files/patch-SimpleHeuristic.cpp
+++ games/connectfive/files/patch-SimpleHeuristic.cpp
@@ -1,10 +1,11 @@
---- SimpleHeuristic.cpp.orig	Thu Jan  1 03:21:53 2004
-+++ SimpleHeuristic.cpp	Wed Sep 21 09:30:36 2005
-@@ -31,6 +31,7 @@
+--- SimpleHeuristic.cpp.orig	2004-01-01 05:21:53.000000000 +0300
++++ SimpleHeuristic.cpp	2013-09-13 20:21:14.435230147 +0400
+@@ -31,6 +31,8 @@
  
  
  // Header Files #############################################################
 +#include <stdio.h>
++#include <stdlib.h> // for exit()
  #include "SimpleHeuristic.h"
  
  // Macros ###################################################################
