ae91937cb888409f0ce0dc6f7e7e051bca138c45
Bryan Drewery <bdrewery@FreeBSD.org>
2013-09-14 15:41:55 +0000

    - Fix build on head
    Reported by:	pkg-fallout

---
+++ devel/privman/files/patch-src__priv_client.cc
@@ -0,0 +1,10 @@
+--- ./src/priv_client.cc.orig	2013-09-14 10:36:08.617190025 -0500
++++ ./src/priv_client.cc	2013-09-14 10:37:29.712183808 -0500
+@@ -10,6 +10,7 @@
+  */
+ 
+ #include "../config.h"
++#include <limits.h>
+ #include <stdarg.h>
+ #include <stdlib.h>
+ #include <stdio.h>
