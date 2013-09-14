226a8c0e9ff6b92edc70109678ab6439f9409ed7
amdmi3 <amdmi3@FreeBSD.org>
2013-09-14 03:44:51 +0000

    Fix build with clang/libc++

---
+++ audio/audiere/files/patch-src-utility.h
@@ -0,0 +1,10 @@
+--- src/utility.h.orig	2006-02-14 07:57:01.000000000 +0300
++++ src/utility.h	2013-09-12 07:09:46.054424472 +0400
+@@ -10,6 +10,7 @@
+ #include <map>
+ #include <string>
+ #include <utility>
++#include <cstdlib>
+ #include "audiere.h"
+ #include "types.h"
+ 
