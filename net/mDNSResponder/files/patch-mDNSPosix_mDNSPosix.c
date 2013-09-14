9d278ad9c8f7554cb80ba11a625c75ba4b590aa5
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2013-09-14 08:51:13 +0000

    - Update to 379.38.1 - Update IPV6 support: port two operations to RFC3542 [1] - Use USES=gmake - Use NOT_FOR_ARCHS - Do not silence installation message - Cosmetic change
    Submitted by:	Matthieu Volat <mazhe@alkumuna.eu> [1]

--- net/mDNSResponder/files/patch-mDNSPosix_mDNSPosix.c
+++ net/mDNSResponder/files/patch-mDNSPosix_mDNSPosix.c
@@ -1,19 +1,40 @@
---- mDNSPosix/mDNSPosix.c.orig	2011-04-07 06:48:43.000000000 +0800
-+++ mDNSPosix/mDNSPosix.c	2011-08-28 22:14:54.089049545 +0800
-@@ -487,7 +487,7 @@
- 	{
- 	char line[256];
- 	char nameserver[16];
--	char keyword[10];
-+	char keyword[11];
- 	int  numOfServers = 0;
- 	FILE *fp = fopen(filePath, "r");
- 	if (fp == NULL) return -1;
-@@ -506,6 +506,7 @@
- 			numOfServers++;
- 			}
- 		}  
-+	fclose(fp);
- 	return (numOfServers > 0) ? 0 : -1;
- 	}
+--- mDNSPosix/mDNSPosix.c.orig	2012-04-18 07:01:01.000000000 +0800
++++ mDNSPosix/mDNSPosix.c	2013-08-16 23:04:31.089791517 +0800
+@@ -503,6 +503,7 @@
+             numOfServers++;
+         }
+     }
++    fclose(fp);
+     return (numOfServers > 0) ? 0 : -1;
+ }
  
+@@ -708,7 +709,13 @@
+     {
+         struct ipv6_mreq imr6;
+         struct sockaddr_in6 bindAddr6;
+-    #if defined(IPV6_PKTINFO)
++    #if defined(IPV6_RECVPKTINFO)
++        if (err == 0)
++        {
++            err = setsockopt(*sktPtr, IPPROTO_IPV6, IPV6_RECVPKTINFO, &kOn, sizeof(kOn));
++            if (err < 0) { err = errno; perror("setsockopt - IPV6_RECVPKTINFO"); }
++        }
++    #elif defined(IPV6_PKTINFO)
+         if (err == 0)
+         {
+             err = setsockopt(*sktPtr, IPPROTO_IPV6, IPV6_2292_PKTINFO, &kOn, sizeof(kOn));
+@@ -717,7 +724,13 @@
+     #else
+         #warning This platform has no way to get the destination interface information for IPv6 -- will only work for single-homed hosts
+     #endif
+-    #if defined(IPV6_HOPLIMIT)
++    #if defined(IPV6_RECVHOPLIMIT)
++        if (err == 0)
++        {
++            err = setsockopt(*sktPtr, IPPROTO_IPV6, IPV6_RECVHOPLIMIT, &kOn, sizeof(kOn));
++            if (err < 0) { err = errno; perror("setsockopt - IPV6_RECVHOPLIMIT"); }
++        }
++    #elif defined(IPV6_HOPLIMIT)
+         if (err == 0)
+         {
+             err = setsockopt(*sktPtr, IPPROTO_IPV6, IPV6_2292_HOPLIMIT, &kOn, sizeof(kOn));
