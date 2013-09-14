499b86f6af6b5abcd5312e15c5062f442b3bdc8b
Dmitry Marakasov <amdmi3@FreeBSD.org>
2013-09-14 01:14:22 +0000

    Fix build with clang/libc++

--- games/untahris/files/patch-untahris.cpp
+++ games/untahris/files/patch-untahris.cpp
@@ -1,7 +1,12 @@
 --- untahris.cpp.orig	2009-05-20 01:54:16.000000000 +0400
-+++ untahris.cpp	2009-07-07 17:39:06.000000000 +0400
-@@ -6,7 +6,7 @@
++++ untahris.cpp	2013-09-13 21:12:51.205227100 +0400
+@@ -4,9 +4,12 @@
+ // --------------------------------------------------------------
+ // Main file.
  
++#include <unistd.h>
++#include <time.h>
++
  #include "untahris.h"
  
 -char *configfile = "config.unt";
@@ -9,7 +14,7 @@
  
  #include "tools.cpp"
  #include "intface.cpp"
-@@ -291,6 +291,8 @@
+@@ -291,6 +294,8 @@
    }
  
  int main(int argc, char **argv) {
