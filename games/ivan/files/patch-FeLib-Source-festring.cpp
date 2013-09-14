31c2dea97956fd76aa10014b0e5b156b9c66844f
amdmi3 <amdmi3@FreeBSD.org>
2013-09-14 01:15:56 +0000

    Fix build with clang/libc++

---
+++ games/ivan/files/patch-FeLib-Source-festring.cpp
@@ -0,0 +1,10 @@
+--- FeLib/Source/festring.cpp.orig	2004-11-11 20:27:44.000000000 +0300
++++ FeLib/Source/festring.cpp	2013-09-14 02:16:26.736227688 +0400
+@@ -11,6 +11,7 @@
+  */
+ 
+ #include <cctype>
++#include <cstdlib>
+ 
+ #include "festring.h"
+ #include "allocate.h"
