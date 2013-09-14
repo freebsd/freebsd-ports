9b0e5207a7e2fc8729026a44a1cd450c7d125ef3
fluffy <fluffy@FreeBSD.org>
2013-09-14 07:02:38 +0000

    - Update to 0.99.4 release - Fix build with clang/libc++

---
+++ deskutils/fbreader/files/patch-fbreader__src__fbreader__FBView.cpp
@@ -0,0 +1,10 @@
+--- ./fbreader/src/fbreader/FBView.cpp.orig	2010-04-02 00:14:24.000000000 +1100
++++ ./fbreader/src/fbreader/FBView.cpp	2013-09-12 00:33:47.204357243 +1100
+@@ -18,6 +18,7 @@
+  */
+ 
+ #include <cmath>
++#include <cstdlib>
+ #include <algorithm>
+ 
+ #include <ZLUnicodeUtil.h>
--- deskutils/fbreader/files/patch-zlibrary__src__unix__time__ZLUnixTime.cpp
+++ deskutils/fbreader/files/patch-zlibrary__src__unix__time__ZLUnixTime.cpp
@@ -1,6 +1,6 @@
 --- ./zlibrary/core/src/unix/time/ZLUnixTime.cpp.orig	2010-01-30 04:35:22.000000000 +1000
 +++ ./zlibrary/core/src/unix/time/ZLUnixTime.cpp	2010-02-02 01:23:23.000000000 +1000
-@@ -17,35 +17,39 @@
+@@ -17,17 +17,16 @@
   * 02110-1301, USA.
   */
  
@@ -15,38 +15,10 @@
  ZLTime ZLUnixTimeManager::currentTime() const {
 -	struct timeb timeB;
 -	ftime(&timeB);
--	return ZLTime(timeB.time, timeB.millitm);
+-	return ZLTime((long)timeB.time, timeB.millitm);
 +	struct timeval timeB;
 +	gettimeofday(&timeB,NULL);
-+	return ZLTime(timeB.tv_sec, timeB.tv_usec/1000);
++	return ZLTime((long)timeB.tv_sec, timeB.tv_usec/1000);
  }
  
  short ZLUnixTimeManager::hoursBySeconds(long seconds) const {
--	return localtime(&seconds)->tm_hour;
-+	time_t st = seconds;
-+	return localtime(&st)->tm_hour;
- }
- 
- short ZLUnixTimeManager::minutesBySeconds(long seconds) const {
--	return localtime(&seconds)->tm_min;
-+	time_t st = seconds;
-+	return localtime(&st)->tm_min;
- }
- 
- short ZLUnixTimeManager::yearBySeconds(long seconds) const {
--	return localtime(&seconds)->tm_year + 1900;
-+	time_t st = seconds;
-+	return localtime(&st)->tm_year + 1900;
- }
- 
- short ZLUnixTimeManager::monthBySeconds(long seconds) const {
--	return localtime(&seconds)->tm_mon + 1;
-+	time_t st = seconds;
-+	return localtime(&st)->tm_mon + 1;
- }
- 
- short ZLUnixTimeManager::dayOfMonthBySeconds(long seconds) const {
--	return localtime(&seconds)->tm_mday;
-+	time_t st = seconds;
-+	return localtime(&st)->tm_mday;
- }
