627739127c5762d5ec28902187dfde06df5ae70f
Pietro Cerutti <gahr@FreeBSD.org>
2013-09-17 08:36:00 +0000

    - Update to 0.9.96.31.08.13

--- x11-toolkits/gnocl/files/patch-src_parseMarkup.c
+++ x11-toolkits/gnocl/files/patch-src_parseMarkup.c
@@ -1,11 +1,11 @@
---- src/parseMarkup.c.orig	2013-02-01 12:41:08.000000000 +0100
-+++ src/parseMarkup.c	2013-02-01 12:41:30.000000000 +0100
-@@ -27,6 +27,8 @@
- #include <stdio.h>
- #include <string.h>
- 
-+static void applyTags ( GtkTextBuffer *buffer, GtkTextMark *tagStart, GtkTextMark *tagEnd );
-+
- /* list to hold names of applied tags */
+--- src/parseMarkup.c.orig	2013-08-31 12:36:08.000000000 +0200
++++ src/parseMarkup.c	2013-09-17 10:09:40.000000000 +0200
+@@ -29,6 +29,8 @@
  static GSList *tagList = NULL;
  
+ 
++static void applyTags ( GtkTextBuffer *buffer, GtkTextIter *iter );
++
+ /* remove range of characters from string */
+ char * strcremove ( char *str, char *dest, int first, int last )
+ {
