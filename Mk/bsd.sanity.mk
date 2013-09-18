d01b11f6fc0a1cbcbef50bc975daad971580fe24
Baptiste Daroussin <bapt@FreeBSD.org>
2013-09-18 22:13:34 +0000

    Add comas, to improve grammar
    Suggested by:	sunpoet

--- Mk/bsd.sanity.mk
+++ Mk/bsd.sanity.mk
@@ -4,7 +4,7 @@
 #
 
 .if defined(USE_GMAKE)
-DEV_WARNING+=	"USE_GMAKE is deprecated consider using USES=gmake"
+DEV_WARNING+=	"USE_GMAKE is deprecated, consider using USES=gmake"
 .endif
 
 #.if defined(WITHOUT_NLS)
@@ -20,23 +20,23 @@ DEV_WARNING+=	"USE_GMAKE is deprecated consider using USES=gmake"
 #.endif
 
 .if defined(USE_PERL5) && ${USE_PERL5} == yes
-DEV_ERROR+=	"USE_PERL5=yes is unsupported please use USES=perl5 instead"
+DEV_ERROR+=	"USE_PERL5=yes is unsupported, please use USES=perl5 instead"
 .endif
 
 .if defined(PERL_CONFIGURE)
-DEV_ERROR+=	"PERL_CONFIGURE is unsupported please use USES=perl5 along with USE_PERL5=configure"
+DEV_ERROR+=	"PERL_CONFIGURE is unsupported, please use USES=perl5 along with USE_PERL5=configure"
 .endif
 
 .if defined(PERL_MODBUILD)
-DEV_ERROR+=	"PERL_MODBUILD is unsupported please use USES=perl5 along with USE_PERL5=modbuild"
+DEV_ERROR+=	"PERL_MODBUILD is unsupported, please use USES=perl5 along with USE_PERL5=modbuild"
 .endif
 
 .if defined(USE_PERL5_BUILD)
-DEV_ERROR+=	"USE_PERL5_BUILD is unsupported please use USES=perl5 along with USE_PERL5=build"
+DEV_ERROR+=	"USE_PERL5_BUILD is unsupported, please use USES=perl5 along with USE_PERL5=build"
 .endif
 
 .if defined(USE_PERL5_RUN)
-DEV_ERROR+=	"USE_PERL5_RUN is unsupported please use USES=perl5 along with USE_PERL5=run"
+DEV_ERROR+=	"USE_PERL5_RUN is unsupported, please use USES=perl5 along with USE_PERL5=run"
 .endif
 
 .if defined(USE_KDEBASE_VER)
@@ -52,35 +52,35 @@ DEV_ERROR+=	"USE_QT_VER is unsupported"
 .endif
 
 .if defined(USE_DISPLAY)
-DEV_WARNING+=	"USE_DISPLAY is deprecated please use USES=display"
+DEV_WARNING+=	"USE_DISPLAY is deprecated, please use USES=display"
 .endif
 
 .if defined(USE_ICONV)
-DEV_ERROR+=	"USE_ICONV is unsupported please use USES=iconv"
+DEV_ERROR+=	"USE_ICONV is unsupported, please use USES=iconv"
 .endif
 
 .if defined(USE_CMAKE)
-DEV_ERROR+=	"USE_CMAKE is unsupported please use USES=cmake"
+DEV_ERROR+=	"USE_CMAKE is unsupported, please use USES=cmake"
 .endif
 
 .if defined(USE_READLINE)
-DEV_ERROR+=	"USE_READLINE is unsupported please use USES=readline"
+DEV_ERROR+=	"USE_READLINE is unsupported, please use USES=readline"
 .endif
 
 .if defined(USE_FUSE)
-DEV_ERROR+=	"USE_FUSE is unupported please use USES=fuse"
+DEV_ERROR+=	"USE_FUSE is unupported, please use USES=fuse"
 .endif
 
 .if defined(USE_GETTEXT)
-DEV_ERROR+=	"USE_GETTEXT is unsupported replaced by USES=gettext"
+DEV_ERROR+=	"USE_GETTEXT is unsupported, replaced by USES=gettext"
 .endif
 
 .if defined(USE_GNOME) && ${USE_GNOME:Mpkgconfig}
-DEV_ERROR+=	"USE_GNOME=pkgconfig is unsupported please use USES=pkgconfig"
+DEV_ERROR+=	"USE_GNOME=pkgconfig is unsupported, please use USES=pkgconfig"
 .endif
 
 .if defined(USE_GNOME) && ${USE_GNOME:Mgnomehack}
-DEV_WARNING+=	"USE_GNOME=gnomehack is deprecated please use USES=pathfix"
+DEV_WARNING+=	"USE_GNOME=gnomehack is deprecated, please use USES=pathfix"
 .endif
 
 .if defined(USE_GNOME) && ${USE_GNOME:Mltverhack*}
