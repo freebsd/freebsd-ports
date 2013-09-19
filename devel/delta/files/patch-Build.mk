dd5ab278c4523390cbc4f778e3166d34bbc93b1c
Eitan Adler <eadler@FreeBSD.org>
2013-09-19 13:42:05 +0000

    Add patch to respect CC

---
+++ devel/delta/files/patch-Build.mk
@@ -0,0 +1,9 @@
+--- ./Build.mk.orig	2013-09-19 09:41:07.282741683 -0400
++++ ./Build.mk	2013-09-19 09:41:14.007747039 -0400
+@@ -1,6 +1,5 @@
+ # see License.txt for copyright and terms of use
+ 
+-CC := gcc
+ CFLAGS := -Wall
+ # CFLAGS += -g
+ FLEX := flex
