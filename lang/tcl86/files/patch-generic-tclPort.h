335927cf6f6f9726660614a916625a9acbfb04e1
Pietro Cerutti <gahr@FreeBSD.org>
2013-09-20 07:32:38 +0000

    - Update Tcl/Tk 8.6 to 8.6.1
      Release notes: http://code.activestate.com/lists/tcl-core/13590/

--- lang/tcl86/files/patch-generic-tclPort.h
+++ lang/tcl86/files/patch-generic-tclPort.h
@@ -1,11 +1,11 @@
---- ../generic/tclPort.h.orig	2012-06-26 15:59:02.000000000 +0200
-+++ ../generic/tclPort.h	2012-09-17 16:29:41.000000000 +0200
-@@ -22,7 +22,7 @@
- #endif
- #include "tcl.h"
- #if !defined(_WIN32)
+--- ../generic/tclPort.h.orig	2013-09-03 14:47:31.000000000 +0200
++++ ../generic/tclPort.h	2013-09-04 10:23:22.000000000 +0200
+@@ -20,7 +20,7 @@
+ #if defined(_WIN32)
+ #   include "tclWinPort.h"
+ #else
 -#   include "tclUnixPort.h"
 +#   include "../unix/tclUnixPort.h"
  #endif
+ #include "tcl.h"
  
- #if !defined(LLONG_MIN)
