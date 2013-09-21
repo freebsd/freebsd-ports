83914147070c7228ba1dc1000b2d52f7ff982f90
Boris Samorodov <bsam@FreeBSD.org>
2013-09-21 06:13:38 +0000

    - Update to 0.3.2_beta - Change maintainer email gmx.ch --> FreeBSD.org - Use the new format for LIB_DEPENDS  [1] - Add USE_DOS2UNIX - Change dependencies for QT4 - Use pkg-plist instead of PLIST_FILES and PLIST_DIRS - Change desktop entry and icons - Add patch, fix build at 10.x  [1]
    PR:		ports/182233  [1]
    Submitted by:	bsam (me)  [1]
    Final patch by:	nemysis (maintainer)

---
+++ graphics/eos-movrec/files/patch-main.cpp
@@ -0,0 +1,10 @@
+--- main.cpp.orig	2013-09-19 22:25:36.000000000 +0400
++++ main.cpp	2013-09-19 22:26:10.000000000 +0400
+@@ -19,6 +19,7 @@
+  ***************************************************************************/
+ 
+ #include <qapplication.h>
++#include <clocale>
+ #include "mainwnd.h"
+ 
+ int main(int argc, char **argv)
