420dd1455af7ef2151d26916efdd88c83ff7dbd8
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2013-09-23 09:18:55 +0000

    Update Plone to 4.3.2.
    Release notes:
     http://plone.org/products/plone/releases/4.3
     http://plone.org/products/plone/releases/4.3.1
     http://plone.org/products/plone/releases/4.3.2
    
    This update includes:
     -devel/py-DateTime: 3.0 -> 3.0.3
     -devel/py-Products.ATContentTypes: 2.1.12 -> 2.1.13
     -devel/py-Products.Archetypes: 1.8.6 -> 1.9.4
     -devel/py-Products.CMFDiffTool: 2.0.2 -> 2.1
     -devel/py-Products.CMFDynamicViewFTI: 4.0.3 -> 4.0.5
     -devel/py-Products.GenericSetup: 1.7.3 -> 1.7.4
     -devel/py-Products.MimetypesRegistry: 2.0.4 -> 2.0.5
     -devel/py-Products.PasswordResetTool: 2.0.12 -> 2.0.15
     -devel/py-Products.PlacelessTranslationService: 2.0.3 -> 2.0.4
     -devel/py-Products.PloneTestCase: 0.9.15 -> 0.9.17
     -devel/py-Products.ResourceRegistries: 2.2.7 -> 2.2.9
     -devel/py-Products.contentmigration: 2.1.3 -> 2.1.5
     -devel/py-archetypes.querywidget: 1.0.8 -> 1.0.9
     -devel/py-archetypes.referencebrowserwidget: 2.4.17 -> 2.4.19
     -devel/py-collective.z3cform.datetimewidget: 1.2.3 -> 1.2.5
     -devel/py-five.customerize: 1.0.3 -> 1.1
     -devel/py-z3c.form: 2.5.1 -> 3.0.2
     -devel/py-zope.schema: 4.2.1 -> 4.2.2
     -security/py-AccessControl: 2.13.13 -> 3.0.8
     -security/py-Products.PlonePAS: 4.0.16 -> 4.1.1
     -www/py-Products.CMFPlone: 4.2.5 -> 4.3.2
     -www/py-Products.TinyMCE: 1.2.16 -> 1.3.5
     -www/py-plone.app.blob: 1.5.7 -> 1.5.8
     -www/py-plone.app.caching: 1.1.3 -> 1.1.6
     -www/py-plone.app.collection: 1.0.8 -> 1.0.11
     -www/py-plone.app.content: 2.0.12 -> 2.1.3
     -www/py-plone.app.contentlisting: 1.0.4 -> 1.0.5
     -www/py-plone.app.contentrules: 2.1.9 -> 3.0.4
     -www/py-plone.app.controlpanel: 2.2.11 -> 2.3.7
     -www/py-plone.app.discussion: 2.1.9 -> 2.2.8
     -www/py-plone.app.form: 2.1.2 -> 2.2.3
     -www/py-plone.app.imaging: 1.0.7 -> 1.0.9
     -www/py-plone.app.iterate: 2.1.9 -> 2.1.10
     -www/py-plone.app.jquery: 1.4.4 -> 1.7.2
     -www/py-plone.app.jquerytools: 1.3.2 -> 1.5.6
     -www/py-plone.app.layout: 2.2.9 -> 2.3.7
     -www/py-plone.app.linkintegrity: 1.5.1 -> 1.5.3
     -www/py-plone.app.locales: 4.2.5 -> 4.3.2
     -www/py-plone.app.portlets: 2.3.8 -> 2.4.5
     -www/py-plone.app.querystring: 1.0.7 -> 1.0.8
     -www/py-plone.app.redirector: 1.1.3 -> 1.2
     -www/py-plone.app.registry: 1.1 -> 1.2.3
     -www/py-plone.app.search: 1.0.8 -> 1.1.5
     -www/py-plone.app.theming: 1.0.4 -> 1.1.1
     -www/py-plone.app.upgrade: 1.2.5 -> 1.3.4
     -www/py-plone.app.viewletmanager: 2.0.3 -> 2.0.4
     -www/py-plone.app.vocabularies: 2.1.10 -> 2.1.11
     -www/py-plone.app.workflow: 2.0.10 -> 2.1.6
     -www/py-plone.app.z3cform: 0.6.2 -> 0.7.4
     -www/py-plone.autoform: 1.3 -> 1.5
     -www/py-plone.contentrules: 2.0.2 -> 2.0.3
     -www/py-plone.i18n: 2.0.5 -> 2.0.9
     -www/py-plone.outputfilters: 1.8 -> 1.11.1
     -www/py-plone.portlet.collection: 2.1.3 -> 2.1.5
     -www/py-plone.portlets: 2.1 -> 2.2
     -www/py-plone.scale: 1.2.2 -> 1.3.2
     -www/py-plone.stringinterp: 1.0.7 -> 1.0.10
     -www/py-plone.supermodel: 1.1.4 -> 1.2.3
     -www/py-plone.z3cform: 0.7.8 -> 0.8.0
     -www/py-plonetheme.classic: 1.2.5 -> 1.3.2
     -www/py-plonetheme.sunburst: 1.2.8 -> 1.4.5

--- www/py-plone.supermodel/files/patch-plone-supermodel_exportimport.py
+++
@@ -1,8 +0,0 @@
---- ./plone/supermodel/exportimport.py.orig	2012-04-15 17:56:36.000000000 +0400
-+++ ./plone/supermodel/exportimport.py	2012-04-25 00:12:57.000000000 +0400
-@@ -1,4 +1,4 @@
--from elementtree import ElementTree
-+from xml.etree import ElementTree
- 
- from zope.interface import Interface, implements, implementedBy
- from zope.component import queryUtility
