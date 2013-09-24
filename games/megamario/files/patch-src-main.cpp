1044bdc49956a63ca8bf377b56718a7db965a7fb
nemysis <nemysis@FreeBSD.org>
2013-09-24 21:04:58 +0000

    - Change Makefile header - Change maintainer email gmx.ch --> FreeBSD.org - USES gmake - Use pkg-plist instead of PLIST_FILES and PLIST_DIRS - Add DOCS Option - Mute dir install
    Approved by:	wg/pawel (mentors)

--- games/megamario/files/patch-src-main.cpp
+++ games/megamario/files/patch-src-main.cpp
@@ -1,5 +1,5 @@
---- src/main.cpp.orig	2012-05-06 00:56:40.000000000 +0200
-+++ src/main.cpp	2012-05-06 01:03:55.000000000 +0200
+--- src/main.cpp.orig	2013-09-20 13:22:00.000000000 +0200
++++ src/main.cpp	2013-09-20 13:23:53.000000000 +0200
 @@ -61,7 +61,7 @@
  
  #endif /* defined __unix__ */
@@ -9,6 +9,15 @@
  
  	dictionary * d = iniparser_new(szIniFile);
  	pixelation = iniparser_getboolean (d, "Mario:Pixelation", 1);
+@@ -238,7 +238,7 @@
+ 
+ 			if(event.key.keysym.sym == SDLK_F12)
+ 			{
+-				char gammel[100];
++				char *gammel = new char[100];
+ 				sprintf(gammel,"screenshots/%d.bmp",rand()%10000);
+ 				SDL_SaveBMP(screen,gammel);				
+ 				delete[] gammel;
 @@ -413,7 +413,7 @@
  
  
