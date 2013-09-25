05a3aa4fe5fdb6b432304656e4742a3fc8574ba8
marino <marino@FreeBSD.org>
2013-09-25 21:47:11 +0000

    finance/libofx: add <unistd.h> for modern compilers
    Approved by:	portmgr (bapt, implicit)

---
+++ finance/libofx/files/patch-ofxconnect_ofxpartner.cpp
@@ -0,0 +1,10 @@
+--- ofxconnect/ofxpartner.cpp.orig	2011-03-30 22:30:50.000000000 +0000
++++ ofxconnect/ofxpartner.cpp
+@@ -37,6 +37,7 @@
+ #include <vector>
+ #include <algorithm>
+ #include <string.h>
++#include <unistd.h>
+ 
+ using std::string;
+ using std::vector;
