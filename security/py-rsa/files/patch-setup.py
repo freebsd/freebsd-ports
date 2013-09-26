b6a668220f7c213c42d479a2d2363f263d2e0555
koobs <koobs@FreeBSD.org>
2013-09-26 14:40:36 +0000

    security/py-rsa: Update to 3.1.2
    - Update to 3.1.2
    - Update pkg-plist
    - pkg-descr: Tab->space for WWW:
    - Update setup.py patch
    - Add TEST_DEPENDS and regression-test: target
    - BUILD_DEPENDS are actually RUN_DEPENDS
    - Whitespace alignment
    
    Changes:
    
    https://bitbucket.org/sybren/python-rsa/commits/tag/version-3.1.2
    
    PR:		ports/182298
    Submitted by:	Alexey Degtyarev <alexey@renatasystems.org>

--- security/py-rsa/files/patch-setup.py
+++ security/py-rsa/files/patch-setup.py
@@ -1,16 +1,6 @@
---- ./setup.py.orig	2012-06-19 00:14:17.000000000 +1000
-+++ ./setup.py	2012-06-29 17:34:36.000000000 +1000
-@@ -1,9 +1,5 @@
- #!/usr/bin/env python
- 
--# Ensure that a reasonably recent version of 'distribute' is installed.
--from distribute_setup import use_setuptools
--use_setuptools('0.6.10')
--
- from setuptools import setup
- 
- import rsa
-@@ -41,5 +37,5 @@
+--- ./setup.py.orig	2013-09-26 22:34:41.233363167 +1000
++++ ./setup.py	2013-09-26 22:34:50.192985862 +1000
+@@ -42,5 +42,5 @@
          'pyrsa-encrypt-bigfile = rsa.cli:encrypt_bigfile',
          'pyrsa-decrypt-bigfile = rsa.cli:decrypt_bigfile',
      ]},
