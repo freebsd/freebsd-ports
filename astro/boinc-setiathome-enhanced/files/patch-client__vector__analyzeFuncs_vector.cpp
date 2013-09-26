f795e6fb25af3eb0e7a9fa651583a2dbaa354724
Rene Ladan <rene@FreeBSD.org>
2013-09-26 08:51:49 +0000

    Partially fix build on CURRENT, linking still fails.

--- astro/boinc-setiathome-enhanced/files/patch-client__vector__analyzeFuncs_vector.cpp
+++ astro/boinc-setiathome-enhanced/files/patch-client__vector__analyzeFuncs_vector.cpp
@@ -1,8 +1,10 @@
---- client/vector/analyzeFuncs_vector.cpp.orig	2009-07-14 21:02:10.000000000 +0200
-+++ client/vector/analyzeFuncs_vector.cpp	2013-09-12 17:02:12.000000000 +0200
-@@ -87,12 +87,10 @@
+--- ./client/vector/analyzeFuncs_vector.cpp.orig	2009-07-14 21:02:10.000000000 +0200
++++ ./client/vector/analyzeFuncs_vector.cpp	2013-09-23 19:16:42.000000000 +0200
+@@ -86,13 +86,12 @@
+ #include <sys/sysctl.h>
  #endif
  
++using namespace std;
  
 -#ifdef HAVE___ISNAN
 -#define isnotnan(x) (!__isnan(x))
@@ -11,7 +13,7 @@
  #define isnotnan(x) (!_isnan(x))
  #elif defined(HAVE_ISNAN)
 -#define isnotnam(x) (!isnan(x))
-+#define isnotnan(x) (!std::isnan(x))
++#define isnotnan(x) (!isnan(x))
  #else
  #define isnotnan(x) ((x) == (x))
  #endif
