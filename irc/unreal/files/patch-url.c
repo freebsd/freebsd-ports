90b165a7572f4f4847aeeefd91b7e5328c97311c
Mark Felder <feld@FreeBSD.org>
2013-09-27 19:47:16 +0000

    - Update to 3.2.10.1 - rc script cleanup
    PR:		ports/177506
    Submitted by:	JR Aquino <tanawts@gmail.com>
    Approved by:	crees (mentor)

--- irc/unreal/files/patch-url.c
+++ irc/unreal/files/patch-url.c
@@ -1,6 +1,6 @@
---- src/url.c	Sat Jul  3 21:04:31 2004
-+++ src/url.c	Mon Feb  7 04:31:06 2005
-@@ -141,7 +141,7 @@
+--- src/url.c	2012-12-28 08:04:54.000000000 -0800
++++ src/url.c	2012-12-31 18:03:14.000000000 -0800
+@@ -171,7 +171,7 @@
  	CURLcode res;
  	char *file = url_getfilename(url);
  	char *filename = unreal_getfilename(file);
@@ -9,12 +9,12 @@
  	FILE *fd;
  
  
-@@ -236,7 +236,7 @@
+@@ -356,7 +356,7 @@
  	{
  		char *file = url_getfilename(url);
  		char *filename = unreal_getfilename(file);
 -        	char *tmp = unreal_mktemp("tmp", filename ? filename : "download.conf");
 +        	char *tmp = unreal_mktemp("%%RUNDIR%%/tmp", filename ? filename : "download.conf");
- 		FileHandle *handle = malloc(sizeof(FileHandle));
+ 		FileHandle *handle = MyMallocEx(sizeof(FileHandle));
  		handle->fd = fopen(tmp, "wb");
  		if (!handle->fd)
