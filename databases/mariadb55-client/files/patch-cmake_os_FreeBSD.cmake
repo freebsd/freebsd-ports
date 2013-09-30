3e42368c2900f8c82d787e2b3edf2bf4dcee5c64
Florian Smeets <flo@FreeBSD.org>
2013-09-30 22:32:28 +0000

    - Update to 5.5.33a [1] - Remove PBXT storage engine support, as it was removed upstream [1] - Allow building mariadb with libc++ and clang [ports/182408] [2] - Use bundled jemalloc on FreeBSD < 10 [1] - Add libexecinfo support [1] - Use "FreeBSD Ports" as build comment instead of default "Source distribution" [1]
    PR:		ports/182426 [1]
    PR:		ports/182408 [2]
    Submitted by:	Alexandr Kovalenko <never@nevermind.kiev.ua> (maintainer) [1]
    		Michael Gmelin <freebsd@grem.de> [2]

--- databases/mariadb55-client/files/patch-cmake_os_FreeBSD.cmake
+++ databases/mariadb55-client/files/patch-cmake_os_FreeBSD.cmake
@@ -1,15 +1,16 @@
---- cmake/os/FreeBSD.cmake.orig	2013-01-13 02:13:17.000000000 +0200
-+++ cmake/os/FreeBSD.cmake	2013-01-13 02:14:15.000000000 +0200
-@@ -24,6 +24,7 @@
+--- cmake/os/FreeBSD.cmake.orig	2013-09-27 00:09:44.000000000 +0300
++++ cmake/os/FreeBSD.cmake	2013-09-27 00:09:57.000000000 +0300
+@@ -24,9 +24,10 @@
  # ADD_DEFINITIONS(-DHAVE_BROKEN_REALPATH)
  
  # Find libexecinfo (library that contains backtrace_symbols etc)
-+IF(FALSE)
- INCLUDE_DIRECTORIES(/usr/local/include)
- SET(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} /usr/local/include )
- SET(ENV{LIB} "$ENV{LIB}:/usr/local/lib")
-@@ -31,6 +32,7 @@
+-INCLUDE_DIRECTORIES(/usr/local/include)
+-SET(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} /usr/local/include )
+-SET(ENV{LIB} "$ENV{LIB}:/usr/local/lib")
++SET(EXECINFO_ROOT /usr/local CACHE INTERNAL "Where to find execinfo library and header")
++INCLUDE_DIRECTORIES(${EXECINFO_ROOT}/include)
++SET(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${EXECINFO_ROOT}/include)
++SET(ENV{LIB} "$ENV{LIB}:${EXECINFO_ROOT}/lib")
+ FIND_LIBRARY(EXECINFO NAMES execinfo)
  IF(EXECINFO)
   SET(LIBEXECINFO ${EXECINFO})
- ENDIF()
-+ENDIF()
