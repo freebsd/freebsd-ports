8a6d7e3dffcc6bc8a77d8ac2b622e2342b08979e
Steve Wills <swills@FreeBSD.org>
2013-09-30 19:24:34 +0000

    - Update to 0.9.12
    PR:		ports/181442
    Approved by:	maintainer timeout (bsdports@wayfair.com, >3 weeks)
    Security:	CVE-2013-5942 (vuxml entry on the way)

--- databases/py-carbon/files/patch-setup.py
+++ databases/py-carbon/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.ori	2012-06-21 16:04:03.284808706 -0700
-+++ setup.py	2012-06-21 16:05:09.681811949 -0700
-@@ -12,9 +12,9 @@
+--- setup.py.orig	2013-08-20 17:37:08.000000000 +0000
++++ setup.py	2013-08-21 01:19:40.025390594 +0000
+@@ -13,9 +13,9 @@
    setup_kwargs = dict()
  
  
@@ -10,6 +10,6 @@
 +storage_dirs = [ ('/var/db/graphite/storage/whisper',[]), ('/var/db/graphite/storage/lists',[]),
 +                 ('/var/db/graphite/storage/log',[]), ('/var/db/graphite/storage/rrd',[]) ]
 +conf_files = [ ('/usr/local/etc/carbon', glob('conf/*.example')) ]
- #XXX Need a way to have these work for bdist_rpm but be left alone for everything else
- #init_scripts = [ ('/etc/init.d', ['distro/redhat/init.d/carbon-cache',
- #                                  'distro/redhat/init.d/carbon-relay',
+ 
+ install_files = storage_dirs + conf_files
+ 
