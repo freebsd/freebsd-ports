88899ee07e0df952399704518c177cff72d70991
John Marino <marino@FreeBSD.org>
2013-10-01 12:00:07 +0000

    www/ttf2eot: add <cstddef> for modern compilers
    Approved by:	portmgr (bapt, implicit)

---
+++ www/ttf2eot/files/patch-OpenTypeUtilities.cpp
@@ -0,0 +1,10 @@
+--- OpenTypeUtilities.cpp.orig	2009-04-30 09:18:46.000000000 +0000
++++ OpenTypeUtilities.cpp
+@@ -27,6 +27,7 @@
+ 
+ #include <string.h>
+ #include <vector>
++#include <cstddef>
+ 
+ #ifndef _MSC_VER
+ # include <stdint.h>
