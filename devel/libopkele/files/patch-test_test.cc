a303cb3ef4c444254cdeb01bc4d635e50d3ca82e
marino <marino@FreeBSD.org>
2013-10-01 21:27:49 +0000

    devel/libopkele: add <unistd.h> and other C++ fixes for modern compilers
    Approved by:	portmgr (bapt, implicit)

---
+++ devel/libopkele/files/patch-test_test.cc
@@ -0,0 +1,9 @@
+--- test/test.cc.orig	2009-12-12 20:22:34.000000000 +0000
++++ test/test.cc
+@@ -1,5 +1,6 @@
+ #include <iostream>
+ #include <stdexcept>
++#include <unistd.h>
+ using namespace std;
+ #include <opkele/exception.h>
+ #include <opkele/util.h>
