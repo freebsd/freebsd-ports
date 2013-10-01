4a0bfa3cb7a382d1f31aeb4ff519e02cb40d45b0
marino <marino@FreeBSD.org>
2013-10-01 21:37:46 +0000

    devel/gwenhywfar-fox16: add <unistd.h> for modern compilers
    Since this is a slave port, patch had to be added to devel/gwenhywfar.
    
    Approved by:	portmgr (bapt, implicit)

---
+++ devel/gwenhywfar/files/patch-gui_fox16_libtest.cpp
@@ -0,0 +1,8 @@
+--- gui/fox16/libtest.cpp.orig	2010-08-28 13:40:12.000000000 +0000
++++ gui/fox16/libtest.cpp
+@@ -1,4 +1,5 @@
+ 
++#include <unistd.h>
+ #ifdef HAVE_CONFIG_H
+ # include <config.h>
+ #endif
