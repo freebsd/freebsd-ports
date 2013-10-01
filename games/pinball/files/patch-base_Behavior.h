c3e08dc27c84704743774f14dea53653c9f8398a
marino <marino@FreeBSD.org>
2013-10-01 20:35:53 +0000

    games/pinball: add <unistd.h> for modern compilers
    Approved by:	portmgr (bapt, implicit)

---
+++ games/pinball/files/patch-base_Behavior.h
@@ -0,0 +1,10 @@
+--- base/Behavior.h.orig	2003-11-20 16:46:16.000000000 +0000
++++ base/Behavior.h
+@@ -10,6 +10,7 @@
+ #ifndef BEHAVIOR_H
+ #define BEHAVIOR_H
+ 
++#include <unistd.h>
+ #include "EMath.h"
+ 
+ class Group;
