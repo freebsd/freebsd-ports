ca023713a3c84888a67cf758c0ce2d4502ce1935
makc <makc@FreeBSD.org>
2013-10-02 20:50:35 +0000

    - Update patches to fix build on 8.x (with old zlib) and 10 (unistd.h) - gnomehack -> pathfix - Convert LIB_DEPENDS to new syntax - Support staging

--- textproc/stardict3/files/patch-lib__src__libcommon.cpp
+++ textproc/stardict3/files/patch-lib__src__libcommon.cpp
@@ -1,11 +1,10 @@
---- ./lib/src/libcommon.cpp.orig	2011-07-08 12:35:48.000000000 +0000
-+++ ./lib/src/libcommon.cpp	2012-12-11 07:48:00.601016758 +0000
-@@ -599,7 +599,7 @@
- 
- int unpack_zlib(const char* arch_file_name, const char* out_file_name)
- {
--	zip::gzFile in(gzopen(arch_file_name, "rb"));
-+	zip::gzFileWrapper in(gzopen(arch_file_name, "rb"));
- 	if(!in) {
- 		g_critical("Unable to open archive file: %s.", arch_file_name);
- 		return EXIT_FAILURE;
+--- ./lib/src/libcommon.cpp.orig	2012-01-13 06:21:34.000000000 +0000
++++ ./lib/src/libcommon.cpp	2013-10-02 15:54:43.701934693 +0000
+@@ -27,6 +27,7 @@
+ #include <sstream>
+ #include <glib.h>
+ #include <glib/gstdio.h>
++#include <unistd.h>
+ #include "libcommon.h"
+ #ifdef _WIN32
+ #  include <Shlwapi.h>
--- textproc/stardict3/files/patch-lib__src__libcommon.h
+++ textproc/stardict3/files/patch-lib__src__libcommon.h
@@ -1,11 +1,14 @@
---- ./lib/src/libcommon.h.orig	2011-07-08 12:32:09.000000000 +0000
-+++ ./lib/src/libcommon.h	2012-12-11 07:48:00.604014902 +0000
-@@ -188,7 +188,7 @@
+--- ./lib/src/libcommon.h.orig	2012-01-13 06:21:34.000000000 +0000
++++ ./lib/src/libcommon.h	2013-10-02 15:13:21.563936478 +0000
+@@ -188,7 +188,11 @@
  }
  
  namespace zip {
--typedef ResourceWrapper<void, void*, int, gzclose> gzFile;
-+typedef ResourceWrapper<struct gzFile_s, struct gzFile_s *, int, gzclose> gzFileWrapper;
++#if ZLIB_VERNUM > 0x1250
++typedef ResourceWrapper<gzFile_s, gzFile, int, gzclose> gzFile;
++#else
+ typedef ResourceWrapper<void, void*, int, gzclose> gzFile;
++#endif
  }
  
  /* Create a new temporary file. Return file name in file name encoding.
