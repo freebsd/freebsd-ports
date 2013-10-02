643f64f681c276bdc5b67922f06e8e0868b585a7
John Marino <marino@FreeBSD.org>
2013-10-02 06:31:10 +0000

    databases/tora: add <unistd.h> for modern compilers

---
+++ databases/tora/files/patch-src_toreport.cpp
@@ -0,0 +1,10 @@
+--- src/toreport.cpp.orig	2010-01-14 16:39:02.000000000 +0000
++++ src/toreport.cpp
+@@ -39,6 +39,7 @@
+  *
+  * END_COMMON_COPYRIGHT_HEADER */
+ 
++#include <unistd.h>
+ #include "utils.h"
+ 
+ #include "toconf.h"
