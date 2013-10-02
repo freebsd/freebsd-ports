015bed6ef707c63a18e7192fdb4cf641e28e9b6f
Thierry Thomas <thierry@FreeBSD.org>
2013-10-02 13:17:06 +0000

    - Upgrade to 5.9.10   Changelog at http://sourceforge.net/p/plplot/news/2013/10/plplot-release-5910/
    - Pet portlint
    
    - Use staging.

--- math/plplot/files/patch-cmake_modules_pkg-config.cmake
+++ math/plplot/files/patch-cmake_modules_pkg-config.cmake
@@ -1,9 +1,9 @@
---- ./cmake/modules/pkg-config.cmake.orig	2009-09-06 21:04:43.000000000 +0200
-+++ ./cmake/modules/pkg-config.cmake	2009-12-26 23:55:00.000000000 +0100
-@@ -48,7 +48,7 @@
+--- cmake/modules/pkg-config.cmake.orig	2012-01-05 14:59:36.000000000 +0100
++++ cmake/modules/pkg-config.cmake	2013-10-01 23:01:57.000000000 +0200
+@@ -39,7 +39,7 @@
+ 
  if(PKG_CONFIG_EXECUTABLE)
    message(STATUS "Looking for pkg-config - found")
-   set(pkg_config_true "")
 -  set(PKG_CONFIG_DIR ${LIB_DIR}/pkgconfig)
 +  set(PKG_CONFIG_DIR ${LIB_DIR}data/pkgconfig)
    set(PKG_CONFIG_ENV PKG_CONFIG_PATH=${PKG_CONFIG_DIR})
--- math/plplot/files/patch-examples__tk__xtk02.c
+++ math/plplot/files/patch-examples__tk__xtk02.c
@@ -1,15 +1,6 @@
---- examples/tk/xtk02.c.orig
-+++ examples/tk/xtk02.c
-@@ -49,7 +49,7 @@
-         pm->fdata[i] = y;
-     }
- 
--    interp->result = "Things are cool in gumbyville.";
-+    Tcl_SetResult(interp,(char*)"Things are cool in gumbyville.",TCL_VOLATILE);
-     return TCL_OK;
- }
- 
-@@ -82,7 +82,7 @@
+--- examples/tk/xtk02.c.orig	2011-11-30 23:04:42.000000000 +0100
++++ examples/tk/xtk02.c	2013-10-01 23:06:58.000000000 +0200
+@@ -85,7 +85,7 @@
  //
  // Results:
  //	Returns a standard Tcl completion code, and leaves an error
