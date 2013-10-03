d7f9cdb38eee4df2eca44202d109ba389d0797e8
Baptiste Daroussin <bapt@FreeBSD.org>
2013-10-03 06:25:13 +0000

    Introduce the new "scons" USES. The goal is to replace the old bsd.scons.mk Instead of redifining the building target, let's just reuse the existing ones. Bonus, MAKE_JOBS is now respected

---
+++ Mk/Uses/scons.mk
@@ -0,0 +1,30 @@
+# $FreeBSD$
+#
+# Provide support to use the scons
+#
+# MAINTAINER: python@FreeBSD.org
+#
+# Feature:		scons
+# Usage:		USES=scons
+#
+
+.if !defined(_INCLUDE_USES_SCONS_MK)
+_INCLUDE_USES_SCONS_MK=	yes
+
+.if defined(scons_ARGS)
+IGNORE=	Incorrect 'USES+= scons:${scons_ARGS}' scons takes no arguments
+.endif
+
+MAKEFILE=		#
+MAKE_FLAGS=		#
+ALL_TARGET=		#
+LIBPATH?=		${LOCALBASE}/lib
+CPPPATH?=		${LOCALBASE}/include
+SCONS=			${LOCALBASE}/bin/scons
+BUILD_DEPENDS+=		${SCONS}:${PORTSDIR}/devel/scons
+MAKE_CMD?=		${SCONS}
+MAKE_ENV+=		PKGCONFIGDIR="${PKGCONFIGDIR}" \
+			CCFLAGS="${CFLAGS}" LINKFLAGS="${LDFLAGS}" \
+			LIBPATH="${LIBPATH}" CPPPATH="${CPPPATH}"
+
+.endif
