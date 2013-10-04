e5e3d93fd787748a09d9a254ebbf5d8a74586dc1
Koop Mast <kwm@FreeBSD.org>
2013-10-04 13:38:44 +0000

    Use new LIB_DEPEND syntax, USE_GMAKE=>USES=gmake, stageify. Update comment to remove indefinite article Fix build on HEAD. This port uses pangox-compat.

---
+++ x11-toolkits/gtkmathview/files/patch-mathmlps_main.cc
@@ -0,0 +1,10 @@
+--- mathmlps/main.cc.orig	2013-10-04 15:05:03.000000000 +0200
++++ mathmlps/main.cc	2013-10-04 15:05:17.000000000 +0200
+@@ -18,6 +18,7 @@
+ 
+ #include <config.h>
+ 
++#include <unistd.h>
+ #include <cassert>
+ #include <fstream>
+ 
---
+++ x11-toolkits/gtkmathview/files/patch-mathmlsvg_main.cc
@@ -0,0 +1,10 @@
+--- mathmlsvg/main.cc.orig	2013-10-04 15:04:01.000000000 +0200
++++ mathmlsvg/main.cc	2013-10-04 15:04:19.000000000 +0200
+@@ -18,6 +18,7 @@
+ 
+ #include <config.h>
+ 
++#include <unistd.h>
+ #include <cassert>
+ #include <fstream>
+ 
