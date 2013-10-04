6b1509093b3b78a2d5ff8f1a55f291da519f992a
jhale <jhale@FreeBSD.org>
2013-10-04 13:26:28 +0000

    - Update finance/libofx to 0.9.9 - Support staging - Remove USE_GCC= any; port seems to build fine with clang - Move LICENSE to proper location - Modernize LIB_DEPENDS - USE_GMAKE -> USES= gmake - Update pkg-descr to reflect current capabilities - Bump PORTREVISION on dependent ports due to so version bump

--- finance/libofx/files/patch-lib_ofx_preproc.cpp
+++ finance/libofx/files/patch-lib_ofx_preproc.cpp
@@ -1,11 +1,11 @@
---- ./lib/ofx_preproc.cpp.orig	2011-06-08 08:07:52.000000000 -0400
-+++ ./lib/ofx_preproc.cpp	2012-02-22 08:46:57.000000000 -0500
-@@ -231,7 +231,7 @@
-           memset(iconv_buffer, 0, READ_BUFFER_SIZE * 2);
-           size_t inbytesleft = strlen(s_buffer.c_str());
-           size_t outbytesleft = READ_BUFFER_SIZE * 2 - 1;
--#ifdef OS_WIN32
-+#if defined(OS_WIN32) || defined(__FreeBSD__)
-           const char * inchar = (const char *)s_buffer.c_str();
+--- ./lib/ofx_preproc.cpp.orig	2013-04-25 14:06:29.000000000 -0400
++++ ./lib/ofx_preproc.cpp	2013-10-04 06:27:25.000000000 -0400
+@@ -310,7 +310,7 @@
+             size_t outbytesleft = inbytesleft * 2 - 1;
+             iconv_buffer = (char*) malloc (inbytesleft * 2);
+             memset(iconv_buffer, 0, inbytesleft * 2);
+-#if defined(OS_WIN32) || defined(__sun)
++#if defined(OS_WIN32) || defined(__sun) || defined(__FreeBSD__)
+             const char * inchar = (const char *)s_buffer.c_str();
  #else
-           char * inchar = (char *)s_buffer.c_str();
+             char * inchar = (char *)s_buffer.c_str();
