2bd232e0604d727574047f26b0b26557131463c7
Boris Samorodov <bsam@FreeBSD.org>
2013-10-05 18:30:05 +0000

    . support staging; . remove well-known license file (GPLv3).

---
+++ devel/eric4/files/extra-patch-install-i18n.py
@@ -0,0 +1,43 @@
+--- install-i18n.py.orig	2013-08-19 13:16:47.862300000 +0400
++++ install-i18n.py	2013-10-05 17:55:20.162267518 +0400
+@@ -45,6 +45,7 @@
+ # Define the globals.
+ progName = None
+ configDir = getConfigDir()
++distDir = None
+ privateInstall = False
+ 
+ def usage(rcode = 2):
+@@ -71,7 +72,9 @@
+     global privateInstall, configDir
+     
+     if privateInstall:
+-        targetDir = configDir
++        targetDir = distDir + getConfig('ericTranslationsDir')
++        if not os.path.exists(targetDir):
++            os.makedirs(targetDir, 0755)
+     else:
+         targetDir = getConfig('ericTranslationsDir')
+     
+@@ -97,11 +100,12 @@
+     progName = os.path.basename(argv[0])
+ 
+     try:
+-        optlist, args = getopt.getopt(argv[1:],"hp")
++        optlist, args = getopt.getopt(argv[1:],"hp:")
+     except getopt.GetoptError:
+         usage()
+ 
+     global platBinDir
++    global distDir
+     
+     depChecks = 1
+ 
+@@ -110,6 +114,7 @@
+             usage(0)
+         elif opt == "-p":
+             privateInstall = 1
++            distDir = os.path.normpath(arg)
+         
+     installTranslations()
+ 
