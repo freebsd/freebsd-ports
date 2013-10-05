954323d03288074f577a0c5f33cf5ad159bfc221
olgeni <olgeni@FreeBSD.org>
2013-10-05 13:45:02 +0000

    - Upgrade to version 1.650. - Add staging support. - Do not mute install commands. - Remove automatic PLIST generation. - Do not use /usr/sbin/pkg for pkgng detection; if you run it in batch it   may stop with an interactive prompt on 9.x.

---
+++ sysutils/webmin/files/patch-software_freebsd-lib.pl
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- software/freebsd-lib.pl.orig
++++ software/freebsd-lib.pl
+@@ -21,7 +21,7 @@
+ 
+ sub use_pkg_ng
+ {
+-return 0 if (!-x "/usr/sbin/pkg");
++return 0 if (!-e "/var/db/pkg/local.sqlite");
+ local @lines = &backquote_command("/usr/sbin/pkg info 2>/dev/null </dev/null");
+ return @lines > 1 ? 1 : 0;
+ }
