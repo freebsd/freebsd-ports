eb7bb6a16ade4eebb93ac19df5431161ecb865ca
Baptiste Daroussin <bapt@FreeBSD.org>
2013-10-05 14:42:53 +0000

    Removed unmaintained (ports@) or gnome@ ports depending on gnome1
    Approved by:	gnome (implicit)

--- news/grn/files/patch-libmutt_sort.h
+++
@@ -1,8 +0,0 @@
---- libmutt/sort.h.orig	2007-12-20 19:32:43.000000000 +0100
-+++ libmutt/sort.h	2007-12-20 19:32:55.000000000 +0100
-@@ -45,4 +45,4 @@
- WHERE short SortAux INITVAL (SORT_DATE); /* auxiallary sorting method */
- WHERE short SortAlias INITVAL (SORT_ALIAS);
- 
--extern const struct mapping_t SortMethods[];
-+extern const struct mapping_t *SortMethods;
