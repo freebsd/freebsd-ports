3f22518c3f41fd2e8e19b0423c2d36f944d9a132
John Marino <marino@FreeBSD.org>
2013-10-06 23:17:08 +0000

    audio/ecasound: #include <cstring>
    C++ fix taken from dports.

---
+++ audio/ecasound/files/patch-ecasound_eca-curses.cpp
@@ -0,0 +1,10 @@
+--- ecasound/eca-curses.cpp.orig	2009-04-11 09:44:06.000000000 +0000
++++ ecasound/eca-curses.cpp
+@@ -27,6 +27,7 @@
+ #include <cstdlib>
+ #include <iostream>
+ #include <map>
++#include <cstring>
+ #include <string>
+ 
+ #if defined(ECA_PLATFORM_CURSES) 
