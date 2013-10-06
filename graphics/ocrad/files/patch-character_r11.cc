479a61ed17243bf704f6d08630cf90bf91144595
lippe <lippe@FreeBSD.org>
2013-10-06 05:06:13 +0000

    - Update to 0.22. - Stage Support - Fix build on current

---
+++ graphics/ocrad/files/patch-character_r11.cc
@@ -0,0 +1,10 @@
+--- character_r11.cc.orig	2013-10-03 17:20:43.000000000 -0300
++++ character_r11.cc	2013-10-03 17:20:54.000000000 -0300
+@@ -18,6 +18,7 @@
+ 
+ #include <algorithm>
+ #include <cstdio>
++#include <cstdlib>
+ #include <vector>
+ #include <stdint.h>
+ 
---
+++ graphics/ocrad/files/patch-feats_test0.cc
@@ -0,0 +1,10 @@
+--- feats_test0.cc.orig	2013-10-03 17:19:50.000000000 -0300
++++ feats_test0.cc	2013-10-03 17:20:00.000000000 -0300
+@@ -18,6 +18,7 @@
+ 
+ #include <algorithm>
+ #include <cstdio>
++#include <cstdlib>
+ #include <vector>
+ #include <stdint.h>
+ 
