a913751f89f9d967c3db770bd54fb24cca153438
Felippe de Meirelles Motta <lippe@FreeBSD.org>
2013-10-06 05:06:13 +0000

    - Update to 0.22. - Stage Support - Fix build on current

---
+++ graphics/ocrad/files/patch-profile.cc
@@ -0,0 +1,10 @@
+--- profile.cc.orig	2013-10-03 17:16:53.000000000 -0300
++++ profile.cc	2013-10-03 17:17:01.000000000 -0300
+@@ -18,6 +18,7 @@
+ 
+ #include <algorithm>
+ #include <cstdio>
++#include <cstdlib>
+ #include <vector>
+ #include <stdint.h>
+ 
---
+++ graphics/ocrad/files/patch-track.cc
@@ -0,0 +1,10 @@
+--- track.cc.orig	2013-10-03 17:16:32.000000000 -0300
++++ track.cc	2013-10-03 17:16:46.000000000 -0300
+@@ -18,6 +18,7 @@
+ 
+ #include <algorithm>
+ #include <cstdio>
++#include <cstdlib>
+ #include <vector>
+ 
+ #include "common.h"
