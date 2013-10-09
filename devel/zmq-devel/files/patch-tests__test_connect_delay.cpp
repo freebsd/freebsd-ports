59c1f2ef1e733c2610e531fa8416d95828bab8e8
danilo <danilo@FreeBSD.org>
2013-10-09 16:29:29 +0000

    - Update to version 3.2.4 - Add stage support
    PR:		ports/182804
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>
    Approved by:	wg/culot (mentors, implicit)

--- devel/zmq-devel/files/patch-tests__test_connect_delay.cpp
+++ devel/zmq-devel/files/patch-tests__test_connect_delay.cpp
@@ -1,9 +1,10 @@
 --- ./tests/test_connect_delay.cpp.orig	2013-07-19 13:30:55.436108722 +0400
 +++ ./tests/test_connect_delay.cpp	2013-07-19 13:30:59.430156726 +0400
-@@ -257,4 +257,4 @@
-     
-     rc = zmq_ctx_destroy(context2);
-     assert (rc == 0);
--}
-\ No newline at end of file
-+}
+@@ -22,6 +22,7 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
++#include <time.h>
+ #include <string>
+ 
+ #undef NDEBUG
