06f24dcab4f59268220879553f08eb6ded3a24c2
wg <wg@FreeBSD.org>
2013-10-09 02:14:33 +0000

    multimedia/sabbu: link using ffmpeg0
    - Link using ffmpeg0
    
    Approved by:	portmgr (bapt, implicit)

--- multimedia/sabbu/files/patch-video.h
+++ multimedia/sabbu/files/patch-video.h
@@ -1,12 +1,13 @@
---- Source/video.h.orig	Thu Jul 14 17:52:07 2005
-+++ Source/video.h	Sun Jun 17 08:57:32 2007
+--- Source/video.h.orig	2005-07-14 12:52:07.000000000 -0300
++++ Source/video.h	2013-10-08 23:10:25.214159730 -0300
 @@ -23,11 +23,13 @@
  };
  
  #else
 +extern "C" {
  #ifndef _WINDOWS
- #include <ffmpeg/avformat.h>
+-#include <ffmpeg/avformat.h>
++#include <libavformat/avformat.h>
  #else
  #include "video_ffmpeg.h"
  #endif
