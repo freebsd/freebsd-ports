ef28472c253e52ab8504af1ecb39efc6b50a8fcb
makc <makc@FreeBSD.org>
2013-10-09 17:02:33 +0000

    - One more patch to fix build on 10.x
    Submitted by:	clutton at zoho.com

---
+++ textproc/stardict3/files/patch-dict__src__lib__stddict.cpp
@@ -0,0 +1,10 @@
+--- ./dict/src/lib/stddict.cpp.orig	2012-02-17 08:58:47.000000000 +0000
++++ ./dict/src/lib/stddict.cpp	2013-10-09 16:16:36.938640507 +0000
+@@ -32,6 +32,7 @@
+ #include <glib.h>
+ #include <glib/gi18n.h>
+ #include <glib/gstdio.h>
++#include <stdlib.h>
+ #include <algorithm>
+ #include <memory>
+ 
