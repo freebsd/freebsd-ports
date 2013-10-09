eab0b2caf4335b4261116e4cd30aafe816ba43be
Stefan Eßer <se@FreeBSD.org>
2013-10-09 15:54:30 +0000

    Fix build: Patch was not relative to build directory.

--- multimedia/dvbcut/files/patch-main.cpp
+++ multimedia/dvbcut/files/patch-main.cpp
@@ -1,5 +1,5 @@
---- /usr/work/usr/svn/ports/head/multimedia/dvbcut/work/dvbcut-0.6.1/src/main.cpp~	2011-04-22 23:51:29.000000000 +0200
-+++ /usr/work/usr/svn/ports/head/multimedia/dvbcut/work/dvbcut-0.6.1/src/main.cpp	2013-10-04 11:10:52.016225398 +0200
+--- src/main.cpp~	2011-04-22 23:51:29.000000000 +0200
++++ src/main.cpp	2013-10-04 11:10:52.016225398 +0200
 @@ -21,6 +21,7 @@
  #include <cstring>
  #include <cstdlib>
