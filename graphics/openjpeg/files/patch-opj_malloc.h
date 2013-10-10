82ea044d8f20f06dc4c6c248fc20c354142279db
madpilot <madpilot@FreeBSD.org>
2013-10-10 00:19:01 +0000

    - Convert to staging - Convert LIB_DEPENDS to new format - Use DOS2UNIX and regenerate patch without DOS style newlines

--- graphics/openjpeg/files/patch-opj_malloc.h
+++ graphics/openjpeg/files/patch-opj_malloc.h
@@ -1,12 +1,12 @@
---- libopenjpeg/opj_malloc.h.orig	2012-02-07 11:49:55.000000000 +0100
-+++ libopenjpeg/opj_malloc.h	2012-06-02 11:25:56.000000000 +0200
+--- libopenjpeg/opj_malloc.h.orig	2013-10-08 17:19:09.755013928 +0200
++++ libopenjpeg/opj_malloc.h	2013-10-08 17:20:11.403012744 +0200
 @@ -85,8 +85,7 @@
  		#define HAVE_MEMALIGN
  	/* Linux x86_64 and OSX always align allocations to 16 bytes */
  	#elif !defined(__amd64__) && !defined(__APPLE__)	
 -		#define HAVE_MEMALIGN
 -		#include <malloc.h>			
-+		#include <stdlib.h>			
++		#include <stdlib.h>
  	#endif
  #endif
  
