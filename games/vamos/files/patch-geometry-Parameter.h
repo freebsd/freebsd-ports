26c9b3435c59310818670b8afa2ac8ab4629b3ee
Rusmir Dusko <nemysis@FreeBSD.org>
2013-10-10 11:49:18 +0000

    - Update to 0.7.1 - Change maintainer email to @FreeBSD.org - Use the new format for LIB_DEPENDS - Add dependency for devel/boost-libs - Add patch, fix build --> --disable-unit-tests - Remove PORTSCOUT - Change DOCS - Support STAGEDIR
    Approved by:	wg/pawel (mentors)

---
+++ games/vamos/files/patch-geometry-Parameter.h
@@ -0,0 +1,10 @@
+--- geometry/Parameter.h.orig	2011-08-28 20:54:18.000000000 +0200
++++ geometry/Parameter.h	2013-09-19 23:10:49.000000000 +0200
+@@ -21,6 +21,7 @@
+ #define PARAMETER_H
+ 
+ #include <vector>
++#include <stdlib.h>
+ 
+ /// A container for numbers passed from the command line.
+ namespace Vamos_Geometry
