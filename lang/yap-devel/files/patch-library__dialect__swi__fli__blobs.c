1a6e668a5a3beb0b6e1ab1d6b883595d35730067
danilo <danilo@FreeBSD.org>
2013-10-10 14:08:01 +0000

    - Add new port lang/yap-devel
    Yap-6.3.3 is the development version of lang/yap (6.2.2).
    
    YAP is a high-performance Prolog compiler developed at
    LIACC/Universidade do Porto and at COPPE Sistemas/UFRJ. Its Prolog
    engine is based in the WAM (Warren Abstract Machine), with several
    optimizations for better performance. YAP follows the Edinburgh
    tradition, and is largely compatible with the ISO-Prolog standard and
    with Quintus and SICStus Prolog.
    
    PR:		ports/181334
    Submitted by:	Danilo Egea Gondolfo <danilogondolfo@gmail.com>
    Approved by:	wg/culot (mentors)

---
+++ lang/yap-devel/files/patch-library__dialect__swi__fli__blobs.c
@@ -0,0 +1,10 @@
+--- ./library/dialect/swi/fli/blobs.c.orig	2013-01-19 07:33:19.000000000 -0200
++++ ./library/dialect/swi/fli/blobs.c	2013-08-13 18:37:02.000000000 -0300
+@@ -18,6 +18,7 @@
+ #include	<Yap.h>
+ #include	<Yatom.h>
+ 
++#include <stdio.h>
+ #include <string.h>
+ 
+ /* for freeBSD9.1 */
