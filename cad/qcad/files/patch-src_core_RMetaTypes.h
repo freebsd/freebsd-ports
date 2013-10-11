e41ba52b72b857aea4765df1fd55943cfd523f2f
Michael Reifenberger <mr@FreeBSD.org>
2013-10-11 16:25:50 +0000

    Update to 3.3.3.0
    PR:             ports/182881
    Submitted by:   rakuco@

--- cad/qcad/files/patch-src_core_RMetaTypes.h
+++ cad/qcad/files/patch-src_core_RMetaTypes.h
@@ -1,11 +1,10 @@
---- src/core/RMetaTypes.h.orig	2013-08-06 17:32:09.000000000 +0200
-+++ src/core/RMetaTypes.h	2013-08-08 10:41:01.000000000 +0200
-@@ -121,7 +121,7 @@
+--- src/core/RMetaTypes.h.orig	2013-10-07 15:13:12.654243671 +0300
++++ src/core/RMetaTypes.h	2013-10-07 15:13:24.530910626 +0300
+@@ -125,7 +125,7 @@
  Q_DECLARE_METATYPE(unsigned int*)
  Q_DECLARE_METATYPE(qint64*)
- //Q_DECLARE_METATYPE(uint8_t*)
+ 
 -#if !defined Q_OS_WIN32 && !defined Q_OS_LINUX
 +#if !defined Q_OS_WIN32 && !defined Q_OS_LINUX && !defined Q_OS_FREEBSD
  Q_DECLARE_METATYPE(size_t*)
  #endif
- Q_DECLARE_METATYPE(char*)
