5782400308d703411eb7837dc1795e676bcec7f4
osa <osa@FreeBSD.org>
2013-10-12 07:20:39 +0000

    Update from 4.0.19 to 4.0.20:
    o) www/rubygem-passenger;
    o) third-party modules for www/nginx and www/nginx-devel.

--- www/nginx/files/extra-patch-passenger-build-nginx.rb
+++ www/nginx/files/extra-patch-passenger-build-nginx.rb
@@ -1,10 +1,10 @@
---- ../passenger-4.0.19/build/nginx.rb.orig	2013-09-07 14:09:13.000000000 +0400
-+++ ../passenger-4.0.19/build/nginx.rb	2013-09-07 14:10:31.000000000 +0400
-@@ -32,15 +32,12 @@
+--- ../passenger-4.0.20/build/nginx.rb.orig	2013-10-12 11:05:08.000000000 +0400
++++ ../passenger-4.0.20/build/nginx.rb	2013-10-12 11:06:18.000000000 +0400
+@@ -32,16 +32,12 @@
  desc "Build Nginx support files"
  task :nginx => [
  	:nginx_without_native_support,
-+	LIBBOOST_OXT,
++        LIBBOOST_OXT,
  	NATIVE_SUPPORT_TARGET
  ].compact
  
@@ -14,6 +14,7 @@
 -	AGENT_OUTPUT_DIR + 'PassengerWatchdog',
 -	AGENT_OUTPUT_DIR + 'PassengerLoggingAgent',
 -	AGENT_OUTPUT_DIR + 'SpawnPreparer',
+-	AGENT_OUTPUT_DIR + 'TempDirToucher',
  	COMMON_LIBRARY.only(*NGINX_LIBS_SELECTOR).link_objects
  ].flatten
  
