900e6f472475e045deaa35b3634313d287fe81b8
danilo <danilo@FreeBSD.org>
2013-10-12 17:55:50 +0000

    - Add forgotten patches
    Reported by:	Sender Ghost <lightside@gmx.com> (maintainer)
    Approved by:	wg/culot (mentors, implicit)

---
+++ games/assaultcube/files/patch-source_src_console.h
@@ -0,0 +1,17 @@
+--- source/src/console.h.orig	2013-10-09 12:27:31.000000000 +0400
++++ source/src/console.h	2013-10-11 02:23:35.000000000 +0400
+@@ -127,7 +127,7 @@
+     }
+ };
+ 
+-/** WIP ALERT */
++/** WIP ALERT *//*
+ struct textinputbuffer_wip
+ {
+     string buf;
+@@ -257,4 +257,4 @@
+         return false;
+     }
+ };
+-
++*/
