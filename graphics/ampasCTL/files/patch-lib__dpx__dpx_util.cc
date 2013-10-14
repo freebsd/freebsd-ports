87ede44a767337d5af2094ff78fd8d180ce21af9
Rusmir Dusko <nemysis@FreeBSD.org>
2013-10-14 13:40:48 +0000

    - Update to 1.5 - Merge ampasCTL-1.4.1 and openexr_ctl-1.0.1 into ampasCTL-1.5 - Add conflict with graphics/openexr_ctl - Add dependencies for OpenEXR, tiff and ampasACES-container - USES cmake pkgconfig - Support STAGEDIR - Change pkg-descr
    Approved by:	wg/pawel (mentors)

---
+++ graphics/ampasCTL/files/patch-lib__dpx__dpx_util.cc
@@ -0,0 +1,10 @@
+--- ./lib/dpx/dpx_util.cc.orig	2013-10-12 18:55:34.000000000 +0200
++++ ./lib/dpx/dpx_util.cc	2013-10-12 18:55:55.000000000 +0200
+@@ -57,6 +57,7 @@
+ #include <stdio.h>
+ 
+ #include <string.h>
++#include <stdlib.h>
+ 
+ namespace ctl {
+ 
