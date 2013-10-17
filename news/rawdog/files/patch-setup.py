744c119ee3baf9d9b7f85fb6aa537f789a1dcdb0
tdb <tdb@FreeBSD.org>
2013-10-17 15:54:33 +0000

    - Update to 2.18 [1] - Add stage support
    Submitted by:	Andre Heider <a.heider@gmail.com> [1]

--- news/rawdog/files/patch-setup.py
+++ news/rawdog/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.bak	Thu Dec  9 18:20:00 2004
-+++ setup.py	Thu Dec  9 18:20:12 2004
-@@ -10,7 +10,7 @@
- 	url = "http://offog.org/code/rawdog.html",
- 	license = "GNU GPL v2 or later",
+--- ./setup.py.orig	2013-10-17 15:43:03.702103811 +0100
++++ ./setup.py	2013-10-17 15:43:13.421556778 +0100
+@@ -14,7 +14,7 @@
+ 	author_email = "ats@offog.org",
+ 	url = "http://offog.org/code/rawdog/",
  	scripts = ['rawdog'],
 -	data_files = [('share/man/man1', ['rawdog.1'])],
 +	data_files = [('man/man1', ['rawdog.1'])],
- 	packages = ['rawdoglib'])
- 
- 
+ 	packages = ['rawdoglib'],
+ 	classifiers = [
+ 		"Development Status :: 5 - Production/Stable",
