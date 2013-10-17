6b8e996d9f99e9901d436e83344690c05f18d38e
Veniamin Gvozdikov <vg@FreeBSD.org>
2013-10-17 07:38:22 +0000

    - Fix build with clang
    PR:		ports/182885
    Submitted by:	Oleg Ginzburg <olevole@olevole.ru> (maintainer)
    Approved by:	eadler, osa, rm (mentors, implicit)

---
+++ sysutils/fusefs-zip/files/patch-lib_bigBuffer.h
@@ -0,0 +1,10 @@
+--- lib/bigBuffer.h.orig	2013-10-11 01:12:58.000000000 +0400
++++ lib/bigBuffer.h	2013-10-11 01:13:02.000000000 +0400
+@@ -26,6 +26,7 @@
+ #include <unistd.h>
+ 
+ #include <vector>
++#include <string>
+ 
+ #include "types.h"
+ 
