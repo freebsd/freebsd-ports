986800b6027b318d3ec76d63d9c6a5cd683e6c80
Koop Mast <kwm@FreeBSD.org>
2013-10-19 07:59:41 +0000

    Correct typo in one of the patches.
    Submitted by:	Tomasz Sowa <t.sowa@ttmath.org>

--- graphics/inkscape/files/patch-src_sp-item-rm-unsatisfied-cns.cpp
+++ graphics/inkscape/files/patch-src_sp-item-rm-unsatisfied-cns.cpp
@@ -1,5 +1,5 @@
---- src/sp-item-rm-unsatisfied-cns.cpp.orig	2013-10-17 12:49:51.000000000 +0200
-+++ src/sp-item-rm-unsatisfied-cns.cpp	2013-10-17 12:50:49.000000000 +0200
+--- src/sp-item-rm-unsatisfied-cns.cpp.orig	2011-07-08 20:25:09.468790000 +0200
++++ src/sp-item-rm-unsatisfied-cns.cpp	2013-10-19 09:55:40.000000000 +0200
 @@ -23,9 +23,9 @@
          g_assert( snappoint_ix < int(snappoints.size()) );
          if (!approx_equal( sp_guide_distance_from_pt(cn.g, snappoints[snappoint_ix].getPoint()), 0) ) {
@@ -8,7 +8,7 @@
              g_assert( i < item.constraints.size() );
 -            vector<SPGuideConstraint>::iterator const ei(&item.constraints[i]);
 -            item.constraints.erase(ei);
-+            item.constraints.erase(item.constraints.begin() + 1);
++            item.constraints.erase(item.constraints.begin() + i);
          }
      }
  }
