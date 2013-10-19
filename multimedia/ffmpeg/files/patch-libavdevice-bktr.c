3624ddf181da71b2f213d44844a13f92e7088879
William Grzybowski <wg@FreeBSD.org>
2013-10-19 16:43:32 +0000

    multimedia/ffmpeg: update to 2.0.1
    - Update main ffmpeg port to 2.0.1
    - Bump and update dependent ports when necessary (API change)
    
    Approved by:	portmgr (bapt)

--- multimedia/ffmpeg/files/patch-libavdevice-bktr.c
+++ multimedia/ffmpeg/files/patch-libavdevice-bktr.c
@@ -1,14 +1,14 @@
---- libavdevice/bktr.c.orig	2011-06-21 21:29:25.000000000 +0200
-+++ libavdevice/bktr.c	2011-06-23 13:39:44.160881669 +0200
+--- libavdevice/bktr.c.orig	2012-01-22 14:41:15.000000000 +0100
++++ libavdevice/bktr.c	2012-01-22 16:07:07.873280412 +0100
 @@ -24,6 +24,11 @@
   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
   */
  
-+typedef unsigned char	u_char;
-+typedef unsigned short	u_short;
-+typedef unsigned int	u_int;
-+typedef unsigned long	u_long;
++typedef unsigned char u_char;
++typedef unsigned short        u_short;
++typedef unsigned int  u_int;
++typedef unsigned long u_long;
 +
+ #include "libavformat/internal.h"
  #include "libavutil/log.h"
  #include "libavutil/opt.h"
- #include "libavutil/parseutils.h"
