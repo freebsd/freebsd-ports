df3238e851ed72103084992ab5fdfffdc2ee1caa
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2013-10-20 09:33:04 +0000

    - Fix build with clang - Use USES=gmake - Support STAGEDIR
    PR:		ports/183116
    Submitted by:	sunpoet (myself)
    Approved by:	Douglas Thrift <douglas@douglasthrift.net> (maintainer)

---
+++ sysutils/dtpstree/files/patch-dtpstree.cpp
@@ -0,0 +1,10 @@
+--- dtpstree.cpp.orig	2010-08-05 16:01:47.000000000 +0800
++++ dtpstree.cpp	2013-10-19 19:48:34.750149337 +0800
+@@ -21,6 +21,7 @@
+ 
+ #include <cerrno>
+ #include <climits>
++#include <clocale>
+ #include <cstdarg>
+ #include <cstdio>
+ #include <cstdlib>
