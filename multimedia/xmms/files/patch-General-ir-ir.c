0e1b15d418c6d863a2e9c5229a787e17244074df
Tijl Coosemans <tijl@FreeBSD.org>
2013-10-22 08:30:47 +0000

    multimedia/xmms: - Add zh-xmms to CONFLICTS. - Convert USE_GNOME=desktopfileutils and USE_GMAKE to USES. - Make USE_GL conditional on the SPEC option. - Make gettext conditional on new NLS option. - Only set USE_AUTOTOOLS for the RUSXMMS_PATCHES option. - Remove LIBS from CONFIGURE_ENV.  Set LDFLAGS instead. - Add missing xorg dependencies. - Add xmms_cv_newpcm_driver=yes to CONFIGURE_ARGS.  The configure test   fails on recent version of FreeBSD but the code it enables is still   relevant.  Without it you cannot select a different output device. - Remove SUB_FILES.  None of the files require substitutions. - Remove IPV6 from OPTIONS_DEFAULT.  It's implicit. - Don't exclude SIMD option from amd64. - Use option helpers like OPTIONS_SUB, opt_CONFIGURE_ENABLE, etc. - Install stripped libraries when !defined(WITH_DEBUG). - Use new style LIB_DEPENDS. - Add aclocal, autoheader and libtoolize to USE_AUTOTOOLS to fully   regenerate the build infrastructure.  This fixes a lot of warnings   and allows some patches to be removed. - Add USE_OPENSSL for RUSXMMS_PATCHES option. - Fix the SIMD option on all architectures by removing the post-extract   target. - Remove the message about the RUSXMMS_PATCHES option.  This is from   before there were option dialogs. - Use the same cp command as in the script included with rusxmms instead   of INSTALL_DATA. - Remove the ALSA test from configure with REINPLACE_CMD instead of a   patch (RUSXMMS_PATCHES case). - Remove old libtool macros from acinclude.m4.  Libtoolize adds new   macros. - Remove MKDIR_P patching.  No longer needed after running aclocal. - Patch configure to disable ALSA (not RUSXMMS_PATCHES case). - Fix po/Makefile.in.in with REINPLACE_CMD instead of a patch. - Fix assembly source code files to support clang:   * Remove comments that begin with a single slash.   * Replace 'fist' instruction with 'fists' and 'fistp' with 'fistps'.   * Append '.section ".note.GNU-stack","",@progbits' to every file to     say they don't require an executable stack.  These files are     compiled with -Wa,--noexecstack which clang supports but the ancient     binutils in FreeBSD base does not so without this change xmms runs     with an executable stack when compiled with base gcc. - Support staging. - Remove extra-patch-Visualization_Makefile.am.  It's not needed when   passing VISUALIZATION_PLUGINS to configure. - Rename patch-gcc41 to patch-General-ir-ir.c and modify it to keep the   variables static, but mark keepGoing volatile. - Remove patch-aclocal.m4.  Not needed when running aclocal. - Remove patch-configure.in.  The PTHREAD_LIBS related changes are no   longer needed and ALSA change is handled in the Makefile. - Remove patch-ltmain.sh.  Not needed with staging. - Remove patch-po-Makefile.in.in.  Handled in Makefile. - Remove patch-xmms-config.in.  Not 100% sure about this.  It doesn't   affect xmms itself.  Perhaps some plugins need it but I'd rather fix   those plugins then. - Add patch-xmms-i18n.h.in.  It's needed when NLS option is disabled. - Remove pkg-message.in.  All supported versions of FreeBSD have unzip   in base. - Rename xmms.desktop.in to xmms.desktop to reflect that it doesn't   require substitutions. - Adjust pkg-plist for OPTIONS_SUB, NLS option, staging and   USES=desktop-file-utils.
    chinese/xmms, russian/xmms:
    - Minor cleanups.
    
    PR:		ports/183165
    Approved by:	"Chris Petrik" <c.petrik.sosa@gmail.com> (maintainer)

--- multimedia/xmms/files/patch-General-ir-ir.c
+++ multimedia/xmms/files/patch-General-ir-ir.c
@@ -1,15 +1,11 @@
 --- General/ir/ir.c.orig	Mon Mar  5 14:17:44 2001
 +++ General/ir/ir.c	Sat Dec 16 15:41:23 2006
-@@ -16,10 +16,10 @@
+@@ -16,7 +16,7 @@
  #include "ir.h"
  
  /* Important stuff to know */
 -static gboolean keepGoing = FALSE;
-+gboolean keepGoing = FALSE;
++static volatile gboolean keepGoing = FALSE;
  
  /* The thread handle */
--static pthread_t irapp_thread;
-+pthread_t irapp_thread;
- 
- /* Declarations for calls that we need to mention in the plugin struct */
- static void init(void);
+ static pthread_t irapp_thread;
