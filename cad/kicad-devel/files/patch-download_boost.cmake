b9f534fd683e4ad77e48a232d83e95f68fbd5010
Michael Reifenberger <mr@FreeBSD.org>
2013-10-24 11:34:55 +0000

    Avoid to download the boost library during the build. Its bundled in the distfile instead. Tell bzr to use ${WRKSRC} as the HOME directory.
    PR:             ports/183121

--- cad/kicad-devel/files/patch-download_boost.cmake
+++
@@ -1,8 +0,0 @@
---- CMakeModules/download_boost.cmake.orig	2013-09-17 18:50:32.424234883 +0000
-+++ CMakeModules/download_boost.cmake	2013-09-17 18:33:58.024299100 +0000
-@@ -167,4 +167,5 @@
- 
- ExternalProject_Add_Step( boost bzr_commit_boost
-+    COMMAND bzr whoami "M R <mr@freebsd.org>"
-     COMMAND bzr ci -q -m pristine <SOURCE_DIR>
-     COMMENT "committing pristine boost files to 'boost scratch repo'"
