ad95a0ae90d99a603725c258048b3c898e668ba9
wg <wg@FreeBSD.org>
2013-10-25 13:23:32 +0000

    audio/denemo: update to 1.0.8
    - Update to 1.0.8 [1]
    - Allow staging [1]
    - Convert lib depends to new format
    
    PR:		ports/181881 [1]
    Submitted by:	KATO Tsuguru <tkato432 yahoo.com>

--- audio/denemo/files/patch-src__file.c
+++ audio/denemo/files/patch-src__file.c
@@ -1,11 +1,11 @@
 --- src/file.c.orig
 +++ src/file.c
-@@ -571,7 +571,7 @@
+@@ -569,7 +569,7 @@
      {
        if (system_example_path == NULL)
          {
--          system_example_path = g_build_filename (get_data_dir (), "examples", NULL);
-+          system_example_path = g_build_filename (get_data_dir (), "..", "examples", NULL);
+-          system_example_path = g_build_filename (get_system_data_dir (), "examples", NULL);
++          system_example_path = g_build_filename (get_system_data_dir (), "..", "examples", "denemo", NULL);
            GDir *denemo_path = g_dir_open (system_example_path, 0, NULL);
            if (denemo_path == NULL)
              {
