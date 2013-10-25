cb44276f21fbeda7033eec3b7318d01f1c032abb
ashish <ashish@FreeBSD.org>
2013-10-25 07:11:22 +0000

    - Add staging support - Fix ownership of installed files[0] - Fix building on systems with non UTF-8 locale[1] - Fix building on 10.x and later
    PR:		ports/182693[0], ports/169369[1]
    Submitted by:	Herbert J. Skuhra <h.skuhra@gmail.com> (via private mail)[1]

--- multimedia/libvpx/files/patch-libs.mk
+++ multimedia/libvpx/files/patch-libs.mk
@@ -3,9 +3,18 @@ $FreeBSD$
 
 --- libs.mk.orig
 +++ libs.mk
-@@ -242,8 +242,8 @@
- 	$(qexec)echo 'Conflicts:' >> $@
+@@ -209,7 +209,7 @@
+     $(addprefix $(BUILD_PFX),$(notdir $(LIBVPX_SO_SYMLINKS))),\
+     $(BUILD_PFX)$(LIBVPX_SO)))
+ $(eval $(call libvpx_symlink_template,\
+-    $(addprefix $(DIST_DIR)/,$(LIBVPX_SO_SYMLINKS)),\
++    $(addprefix $(DESTDIR)/$(DIST_DIR)/,$(LIBVPX_SO_SYMLINKS)),\
+     $(DIST_DIR)/$(LIBSUBDIR)/$(LIBVPX_SO)))
+ 
+ INSTALL-LIBS-$(CONFIG_SHARED) += $(LIBVPX_SO_SYMLINKS)
+@@ -232,8 +232,8 @@
  	$(qexec)echo 'Libs: -L$${libdir} -lvpx' >> $@
+ 	$(qexec)echo 'Libs.private: -lm -pthread' >> $@
  	$(qexec)echo 'Cflags: -I$${includedir}' >> $@
 -INSTALL-LIBS-yes += $(LIBSUBDIR)/pkgconfig/vpx.pc
 -INSTALL_MAPS += $(LIBSUBDIR)/pkgconfig/%.pc %.pc
