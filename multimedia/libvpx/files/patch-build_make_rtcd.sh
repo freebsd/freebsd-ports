a9f0a8ca33d086b402cb9b7929ecabf79b0c6f2d
Ashish SHUKLA <ashish@FreeBSD.org>
2013-10-25 07:11:22 +0000

    - Add staging support - Fix ownership of installed files[0] - Fix building on systems with non UTF-8 locale[1] - Fix building on 10.x and later
    PR:		ports/182693[0], ports/169369[1]
    Submitted by:	Herbert J. Skuhra <h.skuhra@gmail.com> (via private mail)[1]

--- multimedia/libvpx/files/patch-build_make_rtcd.sh
+++
@@ -1,9 +0,0 @@
---- build/make/rtcd.sh.orig	2012-06-24 13:12:00.000000000 +0200
-+++ build/make/rtcd.sh	2012-06-24 13:13:01.000000000 +0200
-@@ -1,5 +1,6 @@
- #!/bin/sh
- self=$0
-+LANG=C
- 
- usage() {
-   cat <<EOF >&2
