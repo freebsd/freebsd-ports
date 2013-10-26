aea910a6a487e44ba7bd4515dda713bf3fb9c4e7
William Grzybowski <wg@FreeBSD.org>
2013-10-26 15:29:07 +0000

    net/py-kombu: update to 3.0.1
    - Update to 3.0.1

--- net/py-kombu/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
-diff --git requirements/default.txt requirements/default.txt
-index e0c2232..5c48b92 100644
---- requirements/default.txt
-+++ requirements/default.txt
-@@ -1,2 +1,2 @@
- anyjson>=0.3.3
--amqp>=1.0.9,<1.1.0
-+amqp>=1.0.9
