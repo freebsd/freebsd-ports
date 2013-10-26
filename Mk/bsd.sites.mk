9c20e813c200aed061fc919711466f8b7f4464cc
sunpoet <sunpoet@FreeBSD.org>
2013-10-26 12:37:32 +0000

    - Remove cpan.nctu.edu.tw from MASTER_SITE_PERL_CPAN_BY

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -937,7 +937,6 @@ MASTER_SITE_PERL_CPAN_BY+= \
 	ftp://ftp.auckland.ac.nz/pub/perl/CPAN/%CPANSORT%/%SUBDIR%/ \
 	http://backpan.perl.org/%CPANSORT%/%SUBDIR%/ \
 	ftp://ftp.funet.fi/pub/languages/perl/CPAN/%CPANSORT%/%SUBDIR%/ \
-	http://cpan.nctu.edu.tw/%CPANSORT%/%SUBDIR%/ \
 	http://ftp.twaren.net/Unix/Lang/CPAN/%CPANSORT%/%SUBDIR%/ \
 	ftp://ftp.cpan.org/pub/CPAN/modules/by-module/%SUBDIR%/ \
 	http://www.cpan.dk/modules/by-module/%SUBDIR%/
