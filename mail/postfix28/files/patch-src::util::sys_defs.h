79fe2eb9961ceda2e9c2f1a796a454da8bd45771
sahil <sahil@FreeBSD.org>
2013-10-27 03:30:20 +0000

    - Update to 2.8.16

--- mail/postfix28/files/patch-src::util::sys_defs.h
+++ mail/postfix28/files/patch-src::util::sys_defs.h
@@ -1,11 +1,12 @@
---- src/util/sys_defs.h.orig	2013-01-27 19:14:53.000000000 +0000
-+++ src/util/sys_defs.h	2013-01-27 19:15:20.000000000 +0000
-@@ -25,7 +25,7 @@
+--- src/util/sys_defs.h.orig	2013-10-27 02:15:19.000000000 +0000
++++ src/util/sys_defs.h	2013-10-27 02:15:54.000000000 +0000
+@@ -25,7 +25,8 @@
    */
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
 -    || defined(FREEBSD8) || defined(FREEBSD9) \
 +    || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
++    || defined(FREEBSD11) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(OPENBSD5) \
