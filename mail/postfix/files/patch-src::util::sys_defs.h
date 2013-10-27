fed5a045aa6ad58244eb80fe44b29207cd9dd5a2
Sahil Tandon <sahil@FreeBSD.org>
2013-10-27 03:29:59 +0000

    - Update to 2.10.2

--- mail/postfix/files/patch-src::util::sys_defs.h
+++ mail/postfix/files/patch-src::util::sys_defs.h
@@ -1,11 +1,12 @@
---- src/util/sys_defs.h.orig	2013-01-19 18:30:14.000000000 +0000
-+++ src/util/sys_defs.h	2013-01-19 18:31:50.000000000 +0000
-@@ -25,7 +25,7 @@
+--- src/util/sys_defs.h.orig	2012-09-24 23:53:56.000000000 +0000
++++ src/util/sys_defs.h	2013-10-27 01:17:14.000000000 +0000
+@@ -25,7 +25,8 @@
    */
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
 -    || defined(FREEBSD8) || defined(FREEBSD9) \
 +    || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
++    || defined(FREEBSD11) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(OPENBSD5) \
