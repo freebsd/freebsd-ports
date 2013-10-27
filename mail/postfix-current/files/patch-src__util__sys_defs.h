30b3d0d121b44011d2227e08b0fbd434f86501f2
Sahil Tandon <sahil@FreeBSD.org>
2013-10-27 03:30:06 +0000

    - Update to Postfix 2.11 Snapshot 20131001

--- mail/postfix-current/files/patch-src__util__sys_defs.h
+++ mail/postfix-current/files/patch-src__util__sys_defs.h
@@ -1,11 +1,12 @@
---- src/util/sys_defs.h.orig	2012-03-06 22:23:23.000000000 -0500
-+++ src/util/sys_defs.h	2012-03-06 22:23:47.000000000 -0500
-@@ -25,7 +25,7 @@
+--- src/util/sys_defs.h.orig	2013-10-27 02:03:59.000000000 +0000
++++ src/util/sys_defs.h	2013-10-27 02:04:50.000000000 +0000
+@@ -25,7 +25,8 @@
    */
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
 -    || defined(FREEBSD8) || defined(FREEBSD9) \
 +    || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
++    || defined(FREEBSD11) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(OPENBSD5) \
