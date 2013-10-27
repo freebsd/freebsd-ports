93ea88ba08c29ff57f791027a57b689312df2106
pawel <pawel@FreeBSD.org>
2013-10-27 15:17:05 +0000

    - Update net/yaz to version 5.0.2 - Update net/zebra-server to 2.0.55 and fix build   with new net/yaz [1] - Bump other affected ports
    PR:		ports/183355 [1]
    Submitted by:	myself
    Approved by:	maintainer

--- net/zebra-server/files/patch-index_mod_grs_regx.c
+++ net/zebra-server/files/patch-index_mod_grs_regx.c
@@ -1,16 +1,16 @@
---- index/mod_grs_regx.c.orig	2010-06-29 10:57:13.000000000 +0200
-+++ index/mod_grs_regx.c	2013-07-24 09:27:20.000000000 +0200
-@@ -1298,8 +1298,12 @@
+--- index/mod_grs_regx.c.orig	2013-01-08 10:26:06.000000000 +0100
++++ index/mod_grs_regx.c	2013-10-27 11:54:01.655086748 +0100
+@@ -1301,8 +1301,12 @@
      {
      	const char *err = Tcl_GetVar(spec->tcl_interp, "errorInfo", 0);
- 	yaz_log(YLOG_FATAL, "Tcl error, line=%d, \"%s\"\n%s", 
+ 	yaz_log(YLOG_FATAL, "Tcl error, line=%d, \"%s\"\n%s",
 +#if TCL_MAJOR_VERSION == 8 && TCL_MINOR_VERSION < 5
  	    spec->tcl_interp->errorLine,
 -	    spec->tcl_interp->result,
 +#else
 +	    Tcl_GetErrorLine(spec->tcl_interp),
 +#endif
-+	    Tcl_GetStringResult(spec->tcl_interp),
++	    Tcl_GetStringResult(spec->tcl_interp),	    
  	    err ? err : "[NO ERRORINFO]");
      }
  }
