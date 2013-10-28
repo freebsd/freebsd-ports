62d10a7c993672601d46f0b567dcf2048047b38a
pawel <pawel@FreeBSD.org>
2013-10-28 16:06:16 +0000

    - Update to version 0.45.3 - Support STAGEDIR
    While here:
    - Use options helpers
    - Install dektop entry file
    
    PR:		ports/181877
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- audio/pd/files/patch-extra_pd_pd.c
+++ audio/pd/files/patch-extra_pd_pd.c
@@ -1,10 +1,10 @@
 --- extra/pd~/pd~.c.orig	2010-07-29 05:55:17.000000000 +0900
 +++ extra/pd~/pd~.c	2011-04-06 19:18:20.000000000 +0900
-@@ -11,6 +11,7 @@
+@@ -19,6 +19,7 @@
  #include <stdlib.h>
  #include <errno.h>
  #include <ctype.h>
 +#include <signal.h>
- #include <sys/types.h>
- #include <sys/stat.h>
  #include <sys/wait.h>
+ #include <fcntl.h>
+ #endif
