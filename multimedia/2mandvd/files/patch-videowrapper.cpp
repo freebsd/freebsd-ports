0d305a2054ad07ad90b3e2b59392953bff06d863
William Grzybowski <wg@FreeBSD.org>
2013-10-28 14:57:30 +0000

    multimedia/2mandvd: fix build with newer ffmpeg
    - Add patch to chase the ffmpeg 2.0 API change
    
    Reported by:	marino

--- multimedia/2mandvd/files/patch-videowrapper.cpp
+++ multimedia/2mandvd/files/patch-videowrapper.cpp
@@ -1,11 +1,21 @@
---- videowrapper.cpp.orig
-+++ videowrapper.cpp
-@@ -38,7 +38,7 @@ void videowrapper::delete_videowrapper()
+diff -urN videowrapper.cpp videowrapper.cpp
+--- videowrapper.cpp	2012-06-06 20:25:24.000000000 +1100
++++ videowrapper.cpp	2013-08-03 10:54:05.426228163 +1100
+@@ -38,7 +38,7 @@
  int videowrapper::load_video(QString filename)
  {
      // *** OUVERTURE DU FICHIER VIDEO ***
 -    #ifdef avformat_open_input
-+    #if LIBAVFORMAT_VERSION_INT > AV_VERSION_INT(53,5,0)
++#if LIBAVFORMAT_VERSION_MAJOR >= 53
          if (avformat_open_input(&pFormatCtx,filename.toAscii().data(),NULL,NULL)!=0)
          {
      #else
+@@ -90,7 +90,7 @@
+ 
+     // *** OUVERTURE DU CODEC ***
+ 
+-    if(avcodec_open(pCodecCtx, pCodec)<0)
++    if(avcodec_open2(pCodecCtx, pCodec, NULL)<0)
+     {
+         qDebug() << "CAN'T FIND VIDEO CODEC";
+         return 1;
