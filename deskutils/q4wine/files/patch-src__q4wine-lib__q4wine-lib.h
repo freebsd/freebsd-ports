f692f32ad8df8f0d659937ad25dedb11f6fc026a
Rusmir Dusko <nemysis@FreeBSD.org>
2013-10-29 15:27:17 +0000

    - Change REINPLACE_CMD - Add patches, fix build on FreeBSD 10
    Approved by:	pawel / wg (mentors, implicit)

---
+++ deskutils/q4wine/files/patch-src__q4wine-lib__q4wine-lib.h
@@ -0,0 +1,10 @@
+--- ./src/q4wine-lib/q4wine-lib.h.orig	2013-05-02 22:38:39.000000000 +0200
++++ ./src/q4wine-lib/q4wine-lib.h	2013-10-29 00:21:12.000000000 +0100
+@@ -47,6 +47,7 @@
+ #include <QDesktopServices>
+ #include <QUrl>
+ #include <QTranslator>
++#include <locale.h>
+ 
+ #include "process.h"
+ 
