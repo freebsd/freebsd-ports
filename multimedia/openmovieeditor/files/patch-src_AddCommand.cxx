1e72f1e676f9ddaf43fb0b8988bb6d726ba17606
marino <marino@FreeBSD.org>
2013-10-30 12:54:19 +0000

    multimedia/openmovieeditor: add <unistd.h>, <cstdio>, <stdint.h>
    Approved by:	portmgr (bapt, implicit)

---
+++ multimedia/openmovieeditor/files/patch-src_AddCommand.cxx
@@ -0,0 +1,10 @@
+--- src/AddCommand.cxx.orig	2013-10-30 13:00:57.000000000 +0100
++++ src/AddCommand.cxx
+@@ -18,6 +18,7 @@
+  */
+ 
+ 
++#include <cstdio>
+ #include "AddCommand.H"
+ #include "timeline/Clip.H"
+ #include "Timeline.H"
