54c16ef873d2758950e41aae50747e26568aafdd
kevlo <kevlo@FreeBSD.org>
2013-10-30 05:45:42 +0000

    - Support stage - Silent warnings

--- ftp/atftp/files/patch-tftp_def.h
+++ ftp/atftp/files/patch-tftp_def.h
@@ -1,5 +1,5 @@
 --- tftp_def.h.orig	2004-02-13 11:16:09.000000000 +0800
-+++ tftp_def.h	2012-11-21 13:21:56.000000000 +0800
++++ tftp_def.h	2013-10-30 13:23:22.000000000 +0800
 @@ -19,6 +19,7 @@
  
  #include <sys/time.h>
@@ -13,7 +13,7 @@
  int timeval_diff(struct timeval *res, struct timeval *t1, struct timeval *t0);
  int print_eng(double value, char *string, int size, char *format);
 -inline char *Strncpy(char *to, const char *from, size_t size);
-+extern inline char *Strncpy(char *to, const char *from, size_t size);
++char *Strncpy(char *to, const char *from, size_t size);
  int Gethostbyname(char *addr, struct hostent *host);
  
  #endif
--- ftp/atftp/files/patch-tftpd.h
+++ ftp/atftp/files/patch-tftpd.h
@@ -1,5 +1,5 @@
 --- tftpd.h.orig	2004-02-27 10:05:26.000000000 +0800
-+++ tftpd.h	2012-11-21 13:07:15.000000000 +0800
++++ tftpd.h	2013-10-30 13:25:35.000000000 +0800
 @@ -71,6 +71,11 @@
  struct client_info {
       struct sockaddr_in client;
@@ -17,7 +17,7 @@
   * Defined in tftpd_list.c, operation on client structure list.
   */
 -inline void tftpd_clientlist_ready(struct thread_data *thread);
-+extern inline void tftpd_clientlist_ready(struct thread_data *thread);
++void tftpd_clientlist_ready(struct thread_data *thread);
  void tftpd_clientlist_remove(struct thread_data *thread,
                               struct client_info *client);
  void tftpd_clientlist_free(struct thread_data *thread);
