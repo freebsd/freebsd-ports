f60555bf6b0de3726309ee31c87a4e0db2c6efc0
Ganael LAPLANCHE <martymac@FreeBSD.org>
2013-10-30 07:16:48 +0000

    - Enable staging - Switch to new LIB_DEPENDS format - Fix build with Clang

---
+++ games/blobby/files/patch-src-Clock.h
@@ -0,0 +1,10 @@
+--- src/Clock.h.orig	2013-10-28 17:23:32.000000000 +0100
++++ src/Clock.h	2013-10-28 17:28:11.000000000 +0100
+@@ -19,6 +19,7 @@
+ 
+ #pragma once
+ 
++#include <ctime>
+ #include <string>
+ 
+ /*! \class Clock
