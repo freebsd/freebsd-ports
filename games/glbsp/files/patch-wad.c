dee7d29adf6996db335704f13e7c175f57d0906f
danilo <danilo@FreeBSD.org>
2013-11-01 23:28:31 +0000

    - Add stage support - Convert USE_GMAKE to USES - Convert LIB_DEPENDS to new syntax - Use options helpers - Fix build with clang

--- games/glbsp/files/patch-wad.c
+++ games/glbsp/files/patch-wad.c
@@ -1,6 +1,6 @@
---- wad.c.orig
-+++ wad.c
-@@ -1632,13 +1632,12 @@
+--- ./wad.c.orig	2005-09-13 07:19:36.000000000 -0300
++++ ./wad.c	2013-11-01 20:55:13.000000000 -0200
+@@ -1632,14 +1632,13 @@
  void ReportV5Switches(void)
  {
    lump_t *cur;
@@ -10,8 +10,9 @@
      "V5 FORMAT UPGRADES.  The following levels require a Doom port\n"
      "which supports V5 GL-Nodes, otherwise they will fail (or crash).\n\n"
    );
--
--  int saw_zdbsp = FALSE;
  
+-  int saw_zdbsp = FALSE;
+-
    for (cur=wad.dir_head; cur; cur=cur->next)
    {
+     level_t *lev = cur->lev_info;
