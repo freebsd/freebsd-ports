1a8785035bcea4385a2ef2b954860630a0805972
kwm <kwm@FreeBSD.org>
2013-11-01 22:14:07 +0000

    Stagify Gstreamer ports. Fix build of gstreamer and gstreamer1 with glib 2.38.x [1]. Update gstreamer and gstreamer-plugins to new rules, USES=gmake pathfix, new LIB_DEPENDS syntax. Only pull in flex if base version is too old. Be more selective for which OSVERSIONS we need gcc due to the clang bug.
    Obtained from:	Gstreamer upstream [1]

---
+++ multimedia/gstreamer1/files/patch-gst_gstdatetime.c
@@ -0,0 +1,12 @@
+--- gst/gstdatetime.c.orig	2013-10-20 16:05:12.000000000 +0200
++++ gst/gstdatetime.c	2013-10-20 16:05:24.000000000 +0200
+@@ -21,8 +21,8 @@
+ #include "config.h"
+ #endif
+ 
+-#include "glib-compat-private.h"
+ #include "gst_private.h"
++#include "glib-compat-private.h"
+ #include "gstdatetime.h"
+ #include "gstvalue.h"
+ #include <glib.h>
