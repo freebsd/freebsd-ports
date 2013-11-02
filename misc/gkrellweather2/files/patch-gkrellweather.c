758e3ded502bde74382e1a32fdbecf4674011684
pawel <pawel@FreeBSD.org>
2013-11-02 12:58:52 +0000

    - Update to version 2.0.8 - Add LICENSE - Support staging - Update homepage
    PR:		ports/181066
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- misc/gkrellweather2/files/patch-gkrellweather.c
+++ misc/gkrellweather2/files/patch-gkrellweather.c
@@ -1,19 +1,19 @@
 --- gkrellweather.c.orig	Mon Mar 31 13:12:57 2003
 +++ gkrellweather.c	Sun Jul 13 14:30:14 2003
-@@ -983,7 +983,7 @@
+@@ -986,7 +986,7 @@
      options.windspeed_unit = 1;
      options.pressure_unit = 1;
      strcpy(options.station, DEFAULT_STATION_ID);
--    sprintf(options.command, "/usr/share/gkrellm/GrabWeather %s", options.station);
-+    sprintf(options.command, "GrabWeather %s", options.station);
-     sprintf(options.filename, "%s/.wmWeatherReports/%s.dat",
-             getenv("HOME"), options.station);
- }
-@@ -993,6 +993,7 @@
+-    snprintf(options.command, 512, "/usr/share/gkrellm/GrabWeather %s", options.station);
++    snprintf(options.command, 512, "GrabWeather %s", options.station);
+     options.command[511] = 0;
+     snprintf(options.filename, 512, "%s/.wmWeatherReports/%s.dat",
+              getenv("HOME"), options.station);
+@@ -998,6 +998,7 @@
  gkrellm_init_plugin(void)
  {
      #ifdef ENABLE_NLS
-+    	bindtextdomain(PACKAGE, LOCALEDIR);
++        bindtextdomain(PACKAGE, LOCALEDIR);
          bind_textdomain_codeset(PACKAGE, "UTF-8");
      #endif /* ENABLE_NLS */
      style_id = gkrellm_add_meter_style(&air_mon, STYLE_NAME);
