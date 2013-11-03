0fd684f2dd000fc89b789547c8409675f78c3c99
Danilo Egea Gondolfo <danilo@FreeBSD.org>
2013-11-03 18:11:29 +0000

    - Fix build with clang on i386

--- lang/yap/files/patch-library__dialect__swi__fli__blobs.c
+++ lang/yap/files/patch-library__dialect__swi__fli__blobs.c
@@ -1,5 +1,5 @@
---- ./library/dialect/swi/fli/blobs.c.orig	2013-06-28 00:56:12.000000000 -0300
-+++ ./library/dialect/swi/fli/blobs.c	2013-06-28 00:56:27.000000000 -0300
+--- ./library/dialect/swi/fli/blobs.c.orig	2010-12-02 09:47:28.000000000 -0200
++++ ./library/dialect/swi/fli/blobs.c	2013-11-02 21:10:56.000000000 -0200
 @@ -18,6 +18,7 @@
  #include	<Yap.h>
  #include	<Yatom.h>
