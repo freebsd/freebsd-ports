3dfb9161d3c901fd7a0caa1530ab6a7b0ec39e98
tabthorpe <tabthorpe@FreeBSD.org>
2013-11-03 05:07:32 +0000

    - Fix build problems for amd64 compatibility - Other minor updates
    PR:		ports/182587
    Submitted by:	Joe Greco <jgreco@ns.sol.net> (maintainer)

---
+++ security/tripwire12/files/conf-freebsd8.h
@@ -0,0 +1,57 @@
+/* $FreeBSD$ */
+
+/*
+ * conf-freebsd8.h
+ *
+ *	Tripwire configuration file
+ *
+ * Joe Greco
+ * sol.net Network Services
+ * Derived from the other BSD config.h's
+ */
+
+/***
+ *** Operating System specifics
+ ***	
+ ***	If the answer to a question in the comment is "Yes", then
+ ***	change the corresponding "#undef" to a "#define"
+ ***/
+
+/*
+ * is your OS a System V derivitive?  if so, what version?
+ *			(e.g., define SYSV 4)
+ */
+
+#undef SYSV
+
+/* 
+ * does your system have a <malloc.h> like System V? 
+ */
+
+#undef MALLOCH 	
+
+/* 
+ * does your system have a <stdlib.h> like POSIX says you should? 
+ */
+
+#define STDLIBH
+
+/*
+ * does your system use readdir(3) that returns (struct dirent *)?
+ */
+
+#define DIRENT
+
+/*
+ * is #include <string.h> ok?  (as opposed to <strings.h>)
+ */
+
+#define STRINGH
+ 
+/* 
+ * does your system have gethostname(2) (instead of uname(2))?
+ */
+
+#define GETHOSTNAME
+
+#define	TW_TYPE32	int
