67c852f4fe36beea3b49fabd912de96110dfe4f3
Hiroki Sato <hrs@FreeBSD.org>
2013-11-03 12:56:34 +0000

    - Fix build on 10.x. - Add STAGEDIR support.

--- japanese/mozc-server/files/patch-base_compiler_specific.h
+++ japanese/mozc-server/files/patch-base_compiler_specific.h
@@ -1,13 +1,11 @@
 --- base/compiler_specific.h.orig	2013-07-17 11:38:04.000000000 +0900
-+++ base/compiler_specific.h	2013-07-20 00:41:09.000000000 +0900
-@@ -236,4 +236,10 @@
++++ base/compiler_specific.h	2013-11-03 21:42:23.000000000 +0900
+@@ -236,4 +236,8 @@
  #endif  // _MSC_VER or !_MSC_VER
  // === End suppress warning C4355 macro definitions ===
  
-+#if 0
-+#define	static_assert(a,b) { char _dum[(a) ? 1 : 0]; }
-+#endif
++#ifndef static_assert
 +#define	static_assert(a,b)
-+#define	unique_ptr auto_ptr
++#endif
 +
  #endif  // MOZC_BASE_COMPILER_SPECIFIC_H
