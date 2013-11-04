d7e39fa6283317efb42dc5272a8bc9a1bc3e2d39
novel <novel@FreeBSD.org>
2013-11-04 18:51:45 +0000

    Fix build on i386 by fixing mismatching function declarations due to previous patch.
    Reported by:	Lena@lena.kiev.ua

--- security/gnome-keyring/files/patch-egg_egg-asn1x.c
+++ security/gnome-keyring/files/patch-egg_egg-asn1x.c
@@ -1,6 +1,6 @@
---- egg/egg-asn1x.c.orig	2010-09-04 12:08:41.000000000 +0200
-+++ egg/egg-asn1x.c	2010-09-04 12:10:05.000000000 +0200
-@@ -1988,8 +1988,8 @@ anode_write_integer_ulong (gulong value,
+--- egg/egg-asn1x.c.orig	2010-10-19 06:11:38.000000000 +0400
++++ egg/egg-asn1x.c	2013-11-04 22:37:43.000000000 +0400
+@@ -1988,8 +1988,8 @@
  	guchar buf[sizeof (gulong)];
  	gint bytes, i, off;
  
@@ -11,3 +11,21 @@
  		buf[i] = (value >> (off * 8)) & 0xFF;
  	}
  
+@@ -3746,7 +3746,7 @@
+  * TIME PARSING
+  */
+ 
+-glong
++time_t
+ egg_asn1x_parse_time_general (const gchar *time, gssize n_time)
+ {
+ 	gboolean ret;
+@@ -3777,7 +3777,7 @@
+ 	return value;
+ }
+ 
+-glong
++time_t
+ egg_asn1x_parse_time_utc (const gchar *time, gssize n_time)
+ {
+ 	gboolean ret;
