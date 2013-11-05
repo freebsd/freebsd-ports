33d49d4ae8e80cd68268e84353cb2413c7d240ed
wg <wg@FreeBSD.org>
2013-11-05 16:12:04 +0000

    net/minidlna: update to 1.1.1
    - Update to 1.1.1
    - Fix build for non-default LOCALBASE
    - Use options helpers

--- net/minidlna/files/patch-getifaddr.c
+++ net/minidlna/files/patch-getifaddr.c
@@ -1,13 +1,11 @@
-diff --git getifaddr.c getifaddr.c
-index 9c437b7..5eb6716 100644
---- getifaddr.c
-+++ getifaddr.c
-@@ -175,7 +175,7 @@ getsysaddrs(void)
- 		if (p->ifa_addr && p->ifa_addr->sa_family == AF_INET)
- 		{
- 			addr_in = (struct sockaddr_in *)p->ifa_addr;
--			if (p->ifa_flags & (IFF_LOOPBACK | IFF_SLAVE))
-+			if (p->ifa_flags & (IFF_LOOPBACK))
- 				continue;
- 			memcpy(&lan_addr[n_lan_addr].addr, &addr_in->sin_addr, sizeof(lan_addr[n_lan_addr].addr));
- 			if (!inet_ntop(AF_INET, &addr_in->sin_addr, lan_addr[n_lan_addr].str, sizeof(lan_addr[0].str)) )
+--- getifaddr.c.orig	2013-11-01 23:06:41.000000000 -0200
++++ getifaddr.c	2013-11-05 13:51:05.978629077 -0200
+@@ -82,7 +82,7 @@
+ 		if (ifname && strcmp(p->ifa_name, ifname) != 0)
+ 			continue;
+ 		addr_in = (struct sockaddr_in *)p->ifa_addr;
+-		if (!ifname && (p->ifa_flags & (IFF_LOOPBACK | IFF_SLAVE)))
++		if (!ifname && (p->ifa_flags & IFF_LOOPBACK))
+ 			continue;
+ 		memcpy(&lan_addr[n_lan_addr].addr, &addr_in->sin_addr, sizeof(lan_addr[n_lan_addr].addr));
+ 		if (!inet_ntop(AF_INET, &addr_in->sin_addr, lan_addr[n_lan_addr].str, sizeof(lan_addr[0].str)) )
