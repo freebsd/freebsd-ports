3437a3151148eb1c1f7ebec33e1f7c64bca59426
Christian Weisgerber <naddy@FreeBSD.org>
2013-11-05 15:52:36 +0000

    Convert to staging, add missing includes.

---
+++ math/grpn/files/patch-help.c
@@ -0,0 +1,10 @@
+--- help.c.orig	2002-04-04 05:46:11.000000000 +0200
++++ help.c	2013-11-05 16:44:41.000000000 +0100
+@@ -19,6 +19,7 @@ Foundation, Inc., 59 Temple Place - Suit
+ */
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <gtk/gtk.h>
+ 
+ #include "help.h"
---
+++ math/grpn/files/patch-run_cmd.c
@@ -0,0 +1,10 @@
+--- run_cmd.c.orig	2002-04-04 05:46:11.000000000 +0200
++++ run_cmd.c	2013-11-05 16:41:30.000000000 +0100
+@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suit
+ /* run_cmd.c  by Paul Wilkins */
+ 
+ #include <stdio.h>
++#include <string.h>
+ #include <gtk/gtk.h>
+ 
+ #include "buttons.h"
