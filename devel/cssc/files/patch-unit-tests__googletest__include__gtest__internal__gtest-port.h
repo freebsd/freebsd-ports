freebsd/freebsd-ports

commit 0ca3a6a9ada8f55c31e8c2b8a5d977319349ad78

- Fix build on 10.x and HEAD
PR:		ports/183442
Submitted by:	Oliver Fromme <oliver@fromme.com> (maintainer)


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@332099 35697150-7ecd-e111-bb59-0022644237b5

devel/cssc/files/patch-unit-tests__googletest__include__gtest__internal__gtest-port.h
@@ -1,0 +1,52 @@
+--- ./unit-tests/googletest/include/gtest/internal/gtest-port.h.orig	2010-05-15 18:30:14.000000000 -0400
++++ ./unit-tests/googletest/include/gtest/internal/gtest-port.h	2013-10-29 12:25:07.000000000 -0400
+@@ -80,6 +80,7 @@
+ // the given platform; otherwise undefined):
+ //   GTEST_OS_AIX      - IBM AIX
+ //   GTEST_OS_CYGWIN   - Cygwin
++//   GTEST_OS_FREEBSD  - FreeBSD
+ //   GTEST_OS_LINUX    - Linux
+ //   GTEST_OS_MAC      - Mac OS X
+ //   GTEST_OS_SOLARIS  - Sun Solaris
+@@ -219,10 +220,13 @@
+ #define GTEST_OS_SOLARIS 1
+ #elif defined(_AIX)
+ #define GTEST_OS_AIX 1
++#elif defined(__FreeBSD__)
++#define GTEST_OS_FREEBSD 1
++#define GTEST_USE_OWN_TR1_TUPLE 1
+ #endif  // __CYGWIN__
+ 
+ #if GTEST_OS_CYGWIN || GTEST_OS_LINUX || GTEST_OS_MAC || GTEST_OS_SYMBIAN || \
+-    GTEST_OS_SOLARIS || GTEST_OS_AIX
++    GTEST_OS_SOLARIS || GTEST_OS_AIX || GTEST_OS_FREEBSD
+ 
+ // On some platforms, <regex.h> needs someone to define size_t, and
+ // won't compile otherwise.  We can #include it here as we already
+@@ -254,7 +258,8 @@
+ #define GTEST_USES_SIMPLE_RE 1
+ 
+ #endif  // GTEST_OS_CYGWIN || GTEST_OS_LINUX || GTEST_OS_MAC ||
+-        // GTEST_OS_SYMBIAN || GTEST_OS_SOLARIS || GTEST_OS_AIX
++        // GTEST_OS_SYMBIAN || GTEST_OS_SOLARIS || GTEST_OS_AIX ||
++        // GTEST_OS_FREEBSD
+ 
+ #ifndef GTEST_HAS_EXCEPTIONS
+ // The user didn't tell us whether exceptions are enabled, so we need
+@@ -376,7 +381,7 @@
+ //
+ // To disable threading support in Google Test, add -DGTEST_HAS_PTHREAD=0
+ // to your compiler flags.
+-#define GTEST_HAS_PTHREAD (GTEST_OS_LINUX || GTEST_OS_MAC)
++#define GTEST_HAS_PTHREAD (GTEST_OS_LINUX || GTEST_OS_MAC || GTEST_OS_FREEBSD)
+ #endif  // GTEST_HAS_PTHREAD
+ 
+ // Determines whether Google Test can use tr1/tuple.  You can define
+@@ -483,6 +488,7 @@
+ // abort() in a VC 7.1 application compiled as GUI in debug config
+ // pops up a dialog window that cannot be suppressed programmatically.
+ #if (GTEST_OS_LINUX || GTEST_OS_MAC || GTEST_OS_CYGWIN || GTEST_OS_SOLARIS || \
++     GTEST_OS_FREEBSD || \
+      (GTEST_OS_WINDOWS_DESKTOP && _MSC_VER >= 1400) || \
+      GTEST_OS_WINDOWS_MINGW || GTEST_OS_AIX)
+ #define GTEST_HAS_DEATH_TEST 1

commit 4b768be37941ea742a9d67459b99b6d3779f91c3

devel/cssc: Unbreak on DragonFly caused by r332099 and r332192

devel/cssc/files/patch-unit-tests__googletest__include__gtest__internal__gtest-port.h
@@ -1,5 +1,5 @@
---- ./unit-tests/googletest/include/gtest/internal/gtest-port.h.orig	2010-05-15 18:30:14.000000000 -0400
-+++ ./unit-tests/googletest/include/gtest/internal/gtest-port.h	2013-10-29 12:25:07.000000000 -0400
+--- unit-tests/googletest/include/gtest/internal/gtest-port.h.orig	2010-05-15 22:30:14.000000000 +0000
++++ unit-tests/googletest/include/gtest/internal/gtest-port.h
 @@ -80,6 +80,7 @@
  // the given platform; otherwise undefined):
  //   GTEST_OS_AIX      - IBM AIX
@@ -12,7 +12,7 @@
  #define GTEST_OS_SOLARIS 1
  #elif defined(_AIX)
  #define GTEST_OS_AIX 1
-+#elif defined(__FreeBSD__)
++#elif defined(__FreeBSD__) || defined(__DragonFly__)
 +#define GTEST_OS_FREEBSD 1
 +#define GTEST_USE_OWN_TR1_TUPLE 1
  #endif  // __CYGWIN__