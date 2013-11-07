b2e4ff09e93636a8d00782f9a128cbbc07417226
Cheng-Lung Sung <clsung@FreeBSD.org>
2013-11-07 02:52:10 +0000

    convert to STAGE

--- devel/googletest/files/patch-include_gtest_internal_gtest-port.h
+++ devel/googletest/files/patch-include_gtest_internal_gtest-port.h
@@ -1,5 +1,5 @@
---- include/gtest/internal/gtest-port.h.orig	2010-04-15 22:02:02.000000000 +0000
-+++ include/gtest/internal/gtest-port.h
+--- include/gtest/internal/gtest-port.h.orig	2010-04-16 06:02:02.000000000 +0800
++++ include/gtest/internal/gtest-port.h	2013-10-16 23:39:02.000000000 +0800
 @@ -173,6 +173,7 @@
  #include <stdlib.h>
  #include <stdio.h>
@@ -8,3 +8,12 @@
  #ifndef _WIN32_WCE
  #include <sys/stat.h>
  #endif  // !_WIN32_WCE
+@@ -399,7 +400,7 @@
+ // defining __GNUC__ and friends, but cannot compile GCC's tuple
+ // implementation.  MSVC 2008 (9.0) provides TR1 tuple in a 323 MB
+ // Feature Pack download, which we cannot assume the user has.
+-#if (defined(__GNUC__) && !defined(__CUDACC__) && (GTEST_GCC_VER_ >= 40000)) \
++#if (defined(__GNUC__) && !defined(__CUDACC__) && !defined(_LIBCPP_VERSION) && (GTEST_GCC_VER_ >= 40000)) \
+     || _MSC_VER >= 1600
+ #define GTEST_USE_OWN_TR1_TUPLE 0
+ #else
