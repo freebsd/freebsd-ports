bca30675515ecce996b04b343c1111a4785abac9
amdmi3 <amdmi3@FreeBSD.org>
2013-11-07 11:47:06 +0000

    - Support staging - Fix build on CURRENT
    PR:		182452
    Submitted by:	amdmi3
    Approved by:	Ilya Bakulin <webmaster@kibab.com> (maintainer)

--- net/wpa_gui/files/patch-wpagui.cpp
+++ net/wpa_gui/files/patch-wpagui.cpp
@@ -1,5 +1,5 @@
---- wpagui.cpp.orig	2010-09-07 15:43:39.000000000 +0000
-+++ wpagui.cpp
+--- wpagui.cpp.orig	2010-09-07 19:43:39.000000000 +0400
++++ wpagui.cpp	2013-11-07 14:07:50.086773239 +0400
 @@ -12,10 +12,7 @@
   * See README and COPYING for more details.
   */
@@ -11,3 +11,12 @@
  
  #ifdef CONFIG_NATIVE_WINDOWS
  #include <windows.h>
+@@ -27,6 +24,8 @@
+ #include <QImageReader>
+ #include <QSettings>
+ 
++#include <getopt.h>
++
+ #include "wpagui.h"
+ #include "dirent.h"
+ #include "common/wpa_ctrl.h"
