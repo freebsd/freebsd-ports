a3fe739b2c35ea387c88b7b3707f5c0bd50cac00
Dirk Meyer <dinoex@FreeBSD.org>
2013-11-08 18:47:55 +0000

    - use prefixed DEPENDS and CONFIGURE - fix build warnings

---
+++ misc/magicpoint/files/patch-ctlwords.c
@@ -0,0 +1,10 @@
+--- ctlwords.c.orig	1999-02-15 00:02:25.000000000 +0100
++++ ctlwords.c	2013-11-08 09:05:06.000000000 +0100
+@@ -24,6 +24,7 @@
+ 
+ #include <stdio.h>
+ #include <string.h>
++#include <stdlib.h>
+ 
+ main(int argc, char *argv[])
+ {
