46b4cd6a1d2806ec29c184934074b000a4137a87
vanilla <vanilla@FreeBSD.org>
2013-11-08 12:14:43 +0000

    support STAGEDIR.

---
+++ sysutils/anteater/files/patch-anteater_parameter.h
@@ -0,0 +1,10 @@
+--- anteater/parameter.h.orig	2013-11-08 10:21:48.000000000 +0800
++++ anteater/parameter.h	2013-11-08 10:21:58.000000000 +0800
+@@ -16,6 +16,7 @@
+ #include <config.h>
+ #endif
+ 
++#include <stdlib.h>
+ #include <string>
+ #include <map>
+ #include <list>
