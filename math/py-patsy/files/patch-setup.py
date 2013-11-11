1d6d865274b61986b5bb027cbad2078aeabf87f2
William Grzybowski <wg@FreeBSD.org>
2013-11-11 20:32:09 +0000

    math/py-patsy: Python package for statistical models and design matrices
    Patsy is a Python library for describing statistical models (especially linear
    models, or models that have a linear component) and building design matrices.
    Patsy brings the convenience of R "formulas" to Python.
    
    WWW: https://www.github.com/pydata/patsy
    
    PR:		ports/183870
    Submitted by:	Johannes Jost Meixner <xmj chaot.net>

---
+++ math/py-patsy/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- ./setup.py.orig	2013-08-20 18:59:30.000000000 +0300
++++ ./setup.py	2013-11-11 16:18:52.782247350 +0200
+@@ -27,7 +27,6 @@
+     license="2-clause BSD",
+     packages=["patsy"],
+     url="https://github.com/pydata/patsy",
+-    install_requires=["numpy"],
+     classifiers =
+       [ "Development Status :: 4 - Beta",
+         "Intended Audience :: Developers",
