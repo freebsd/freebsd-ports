90080c59cd62c94df66bef0b061a6e2871383a37
wg <wg@FreeBSD.org>
2013-11-11 16:16:03 +0000

    net/py-gspreadsheet: use easy_install, removing patch
    - easy_install now supports staging
    
    PR:		ports/183862
    Submitted by:	maintainer

--- net/py-gspreadsheet/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
---- ./setup.py.orig	2013-10-29 16:50:19.871040385 +0200
-+++ ./setup.py	2013-10-29 16:51:00.781040301 +0200
-@@ -1,4 +1,4 @@
--from setuptools import setup
-+from distutils.core import setup
- 
- with open('README.rst') as f:
-     long_description = f.read()
