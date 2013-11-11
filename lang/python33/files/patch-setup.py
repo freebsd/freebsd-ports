91e30655499c7e9443e844a08d3460a7c28d2841
William Grzybowski <wg@FreeBSD.org>
2013-11-11 13:00:06 +0000

    lang/python33: add support for freebsd11
    PR:		ports/183597

--- lang/python33/files/patch-setup.py
+++ lang/python33/files/patch-setup.py
@@ -57,7 +57,7 @@
              libraries = []
  
 -        elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
-+        elif host_platform in ('freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
++        elif host_platform in ('freebsd7', 'freebsd8', 'freebsd9', 'freebsd10', 'freebsd11'):
              # FreeBSD's P1003.1b semaphore support is very experimental
              # and has many known problems. (as of June 2008)
              macros = dict()
