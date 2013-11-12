87a8afef20aade749c2b99ba23012a0dfd3ecaab
mva <mva@FreeBSD.org>
2013-11-12 17:30:33 +0000

    - Move games/tmw to games/manaplus   The original tmw client has been retired in favour of manaplus - While here, enable staging support and pet portlint
    PR:		ports/177680
    Submitted by:	Kevin Zheng <kevinz5000@gmail.com>

---
+++ games/manaplus/files/patch-src_gui_gui.h
@@ -0,0 +1,10 @@
+--- src/gui/gui.h.orig	2013-11-12 08:05:48.000000000 +0100
++++ src/gui/gui.h	2013-11-12 08:05:59.000000000 +0100
+@@ -33,6 +33,7 @@
+ class GuiConfigListener;
+ class ImageSet;
+ class MouseEvent;
++class MouseInput;
+ class SDLFont;
+ class SDLInput;
+ class Window;
