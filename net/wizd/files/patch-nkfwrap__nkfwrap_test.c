9edf275cfabe8741821f7e73d576037d5c14572e
Tijl Coosemans <tijl@FreeBSD.org>
2013-11-12 16:28:52 +0000

    - Ressurect net/wizd and assign to new maintainer. - Extract files in WRKSRC rather than WRKDIR because USE_RC_SUBR=wizd   overwrites WRKDIR/wizd. - Remove carriage return from existing patches. - Add patches to fix parallel build. - Staging.
    PR:		ports/182495
    Submitted by:	"Scot W. Hetzel" <swhetzel@gmail.com>

---
+++ net/wizd/files/patch-nkfwrap__nkfwrap_test.c
@@ -0,0 +1,8 @@
+--- nkfwrap/nkfwrap_test.c.orig	2008-07-02 12:30:10.000000000 +0900
++++ nkfwrap/nkfwrap_test.c	2008-07-02 13:13:52.000000000 +0900
+@@ -1,4 +1,5 @@
+ #include <stdio.h>
++#include <string.h>
+ #include "nkfwrap.h"
+ 
+ int main()
