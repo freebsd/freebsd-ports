c659c62d1da48629e41a926d673ce89ba00b27b9
Koop Mast <kwm@FreeBSD.org>
2013-11-12 21:32:22 +0000

    Fix eel2 lib_depend line.
    Submitted by:	antoine@

--- Mk/bsd.gnome.mk
+++ Mk/bsd.gnome.mk
@@ -395,7 +395,7 @@ librsvg2_LIB_DEPENDS=	librsvg-2.so:${PORTSDIR}/graphics/librsvg2
 librsvg2_DETECT=	${LOCALBASE}/libdata/pkgconfig/librsvg-2.0.pc
 librsvg2_USE_GNOME_IMPL=libgsf gtk20
 
-eel2_LIB_DEPENDS=	libeel-2.2.so:${PORTSDIR}/x11-toolkits/eel
+eel2_LIB_DEPENDS=	libeel-2.so:${PORTSDIR}/x11-toolkits/eel
 eel2_DETECT=		${LOCALBASE}/libdata/pkgconfig/eel-2.0.pc
 eel2_USE_GNOME_IMPL=	gnomedesktop
 
