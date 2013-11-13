f451bf37d7e103f58ef22ca50b3c1da05c7b19d3
William Grzybowski <wg@FreeBSD.org>
2013-11-13 09:42:04 +0000

    lang/python27: fix patch for SEM option
    PR:		ports/183884

--- lang/python27/files/extra-patch-setup.py
+++ lang/python27/files/extra-patch-setup.py
@@ -4,7 +4,7 @@
              macros = dict()
              libraries = []
  
--        elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
+-        elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8', 'freebsd9', 'freebsd10', 'freebsd11'):
 +        elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6'):
              # FreeBSD's P1003.1b semaphore support is very experimental
              # and has many known problems. (as of June 2008)
@@ -16,7 +16,7 @@
 +                )
 +            libraries = []
 +
-+        elif host_platform in ('freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
++        elif host_platform in ('freebsd7', 'freebsd8', 'freebsd9', 'freebsd10', 'freebsd11'):
 +            macros = dict(                  # FreeBSD 7+
 +                HAVE_SEM_OPEN=1,
 +                HAVE_SEM_TIMEDWAIT=1,
