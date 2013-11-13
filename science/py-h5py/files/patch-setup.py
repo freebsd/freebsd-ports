4f66e28f87db3af4bbff0cbee78ab997344edbd9
wg <wg@FreeBSD.org>
2013-11-13 21:28:29 +0000

    science/py-h5py: update to 2.2.0
    - Update to 2.2.0
    - Add LICENSE (BSD)
    - Add DOCS
    - Pass maintainership to submitter
    
    PR:		ports/183519
    Submitted by:	Johannes Jost Meixner <xmj chaot.net>
    Approved by:	maintainer (timeout, 2 weeks, 3 times)

--- science/py-h5py/files/patch-setup.py
+++ science/py-h5py/files/patch-setup.py
@@ -1,12 +1,13 @@
---- ./setup.py.orig	2013-10-29 15:50:15.741120947 +0200
-+++ ./setup.py	2013-10-29 15:51:01.621119992 +0200
-@@ -68,6 +68,9 @@
-     elif sys.platform == 'darwin':
-         COMPILER_SETTINGS['include_dirs'] += ['/opt/local/include']
-         COMPILER_SETTINGS['library_dirs'] += ['/opt/local/lib']
-+    elif sys.platform.startswith('freebsd'):
-+        COMPILER_SETTINGS['include_dirs'] += ['%%LOCALBASE%%/include']
-+        COMPILER_SETTINGS['library_dirs'] += ['%%LOCALBASE%%/lib']
+--- ./setup.py.orig	2013-09-04 02:32:03.000000000 +0300
++++ ./setup.py	2013-10-30 23:46:03.668134592 +0200
+@@ -136,8 +136,8 @@
+         COMPILER_SETTINGS['include_dirs'] += ['/usr/local/include'] # homebrew
+         COMPILER_SETTINGS['library_dirs'] += ['/usr/local/lib']     # homebrew
+     elif sys.platform.startswith('freebsd'):
+-        COMPILER_SETTINGS['include_dirs'] += ['/usr/local/include'] # homebrew
+-        COMPILER_SETTINGS['library_dirs'] += ['/usr/local/lib']     # homebrew
++        COMPILER_SETTINGS['include_dirs'] += ['%%LOCALBASE%%/include'] # homebrew
++        COMPILER_SETTINGS['library_dirs'] += ['%%LOCALBASE%%/lib']     # homebrew
+     if MPI:
+         COMPILER_SETTINGS['include_dirs'] += [mpi4py.get_include()]
      COMPILER_SETTINGS['runtime_library_dirs'] = [op.abspath(x) for x in COMPILER_SETTINGS['library_dirs']]
- 
- MODULES =  ['defs','_errors','_objects','_proxy', 'h5fd', 'h5z',
