964958999802f2de250535be466adf31151e0481
Jung-uk Kim <jkim@FreeBSD.org>
2013-11-14 21:20:35 +0000

    - Update plugin directories to automatically pick up. - Support staging and tidy up plist. - Remove article from comment and add LICENSE.

--- www/nspluginwrapper/files/patch-src__npw-config.c
+++ www/nspluginwrapper/files/patch-src__npw-config.c
@@ -1,6 +1,6 @@
---- src/npw-config.c.orig	2011-04-23 14:47:21.000000000 -0400
-+++ src/npw-config.c	2011-04-25 17:46:44.000000000 -0400
-@@ -186,8 +186,9 @@
+--- src/npw-config.c.orig	2011-06-30 23:18:57.000000000 -0400
++++ src/npw-config.c	2013-11-14 14:41:13.000000000 -0500
+@@ -133,8 +133,9 @@
  #if defined(__FreeBSD__)
  	{
  	  static const char *freebsd_dirs[] = {
@@ -12,7 +12,7 @@
  	  };
  	  dirs = freebsd_dirs;
  	}
-@@ -283,11 +284,13 @@
+@@ -232,11 +233,12 @@
  	"/usr/lib32/nsbrowser/plugins",				// XXX how unfortunate
  	"/usr/lib64/nsbrowser/plugins",
  #if defined(__FreeBSD__)
@@ -24,10 +24,9 @@
 +	"%%LOCALBASE%%/lib/browser_plugins",
 +	"%%LOCALBASE%%/lib/firefox/plugins",
 +	"%%LOCALBASE%%/lib/seamonkey/plugins",
-+	"%%LOCALBASE%%/lib/npapi/linux-flashplugin",
-+	"%%LOCALBASE%%/lib/npapi/linux-f10-flashplugin",
-+	"%%LOCALBASE%%/Adobe/Acrobat7.0/ENU/Browser/intellinux",
++	"%%LOCALBASE%%/lib/browser_plugins/linux-f10-flashplugin",
 +	"%%LOCALBASE%%/Adobe/Reader8/ENU/Adobe/Reader8/Browser/intellinux",
++	"%%LOCALBASE%%/Adobe/Reader9/ENU/Adobe/Reader9/Browser/intellinux",
  #endif
  #if defined(__DragonFly__)
  	"/usr/pkg/lib/netscape/plugins",
