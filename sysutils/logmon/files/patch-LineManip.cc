1ef59acb8dbc37717c22f918f100e8f1da179ce1
Vanilla I. Shu <vanilla@FreeBSD.org>
2013-11-15 14:58:59 +0000

    Support STAGEDIR.

---
+++ sysutils/logmon/files/patch-LineManip.cc
@@ -0,0 +1,10 @@
+--- LineManip.cc.orig	2013-11-15 21:52:28.000000000 +0800
++++ LineManip.cc	2013-11-15 21:52:51.000000000 +0800
+@@ -2,6 +2,7 @@
+ // $Id: LineManip.cc 72 2005-07-12 00:46:02Z amackenz $
+ // 
+ 
++#include <stdlib.h>
+ #include <unistd.h>
+ #include <ncurses.h>
+ #include <ctype.h>
