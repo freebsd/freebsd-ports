7258de6f3e27a5bc069b9779b43c995fd57dd48d
rakuco <rakuco@FreeBSD.org>
2013-11-17 21:35:00 +0000

    - Fix the build with libc++. - Fix the build with iconv in base. - Be verbose in the sed calls done in post-patch.

--- archivers/unalz/files/patch-UnAlz.cpp
+++ archivers/unalz/files/patch-UnAlz.cpp
@@ -1,5 +1,14 @@
 --- UnAlz.cpp.orig
 +++ UnAlz.cpp
+@@ -37,7 +37,7 @@
+ #	include <errno.h>
+ #endif
+ 
+-#if defined(__NetBSD__)
++#if defined(__NetBSD__) || defined(__FreeBSD__)
+ #	include <sys/param.h>	// __NetBSD_Version__
+ #	include <errno.h>		// iconv.h 때문에 필요 
+ #endif
 @@ -1907,7 +1907,11 @@
  ////////////////////////////////////////////////////////////////////////////////////////////////////
  UINT32 CUnAlz::CRC32(UINT32 l, BYTE c)
