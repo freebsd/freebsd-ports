675fe80f494b8171aa1aa8fd9f34ab8b1b6b1c7f
fjoe <fjoe@FreeBSD.org>
2013-11-18 14:16:15 +0000

    Unbreak clang build.
    Obtained from:	upstream

---
+++ devel/libiqxmlrpc/files/patch-libiqxmlrpc-net_except.h
@@ -0,0 +1,10 @@
+--- libiqxmlrpc/net_except.h.orig	2013-11-18 21:01:34.000000000 +0700
++++ libiqxmlrpc/net_except.h	2013-11-18 21:01:41.000000000 +0700
+@@ -7,6 +7,7 @@
+ #include "api_export.h"
+ 
+ #include <stdexcept>
++#include <string>
+ 
+ namespace iqnet
+ {
