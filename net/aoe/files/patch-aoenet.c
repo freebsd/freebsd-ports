0343b627fcced0b967c5a13c52dd38aebb15e885
fjoe <fjoe@FreeBSD.org>
2013-11-18 14:44:49 +0000

    Unbreak build on FreeBSD 10 (new mbuf API).

--- net/aoe/files/patch-aoenet.c
+++ net/aoe/files/patch-aoenet.c
@@ -1,5 +1,5 @@
---- aoenet.c.orig	2010-09-13 12:24:34.000000000 +0700
-+++ aoenet.c	2010-09-13 12:26:12.000000000 +0700
+--- aoenet.c.orig	2013-11-18 21:43:13.000000000 +0700
++++ aoenet.c	2013-11-18 21:43:21.000000000 +0700
 @@ -77,8 +77,11 @@
  #define NECODES (sizeof(aoe_errlist) /  sizeof(char *) - 1)
  #if (__FreeBSD_version < 600000)
@@ -13,7 +13,26 @@
  #endif
  #define IFLISTSZ 1024
  
-@@ -223,6 +226,9 @@
+@@ -190,10 +193,18 @@
+ /* 
+  * a dummy "free" function for mbuf ext buffer 
+  */
++#if __FreeBSD_version >= 1000050
++static int
++nilfn(struct mbuf *m, void *a, void *b)
++{
++	return EXT_FREE_OK;
++}
++#else
+ static void
+ nilfn(void *a, void *b)
+ {
+ }
++#endif
+ 
+ /* Create a mbuf chain and point to our data section(s). */
+ static struct mbuf *
+@@ -223,6 +234,9 @@
  
  		m1->m_ext.ref_cnt = NULL;
  		MEXTADD(m1, f->f_data, len, nilfn, 
