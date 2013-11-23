f38a81fc6271e5ab1e7bb514401f0dd74a70b8e0
Steve Wills <swills@FreeBSD.org>
2013-11-23 03:10:04 +0000

    - Fix and report heap overflow in floating point parsing issue in ruby
    Security:	cc9043cf-7f7a-426e-b2cc-8d1980618113

--- lang/ruby20/files/patch-lib_uri_generic.rb
+++ lang/ruby20/files/patch-lib_uri_generic.rb
@@ -1,19 +1,20 @@
---- lib/uri/generic.rb.orig	2009-06-18 16:47:45.000000000 +0400
-+++ lib/uri/generic.rb	2009-06-18 16:48:54.000000000 +0400
-@@ -1032,7 +1032,15 @@
-           end
+--- lib/uri/generic.rb.orig	2012-07-20 01:56:21.000000000 +0000
++++ lib/uri/generic.rb	2013-11-21 20:50:14.143925606 +0000
+@@ -1482,6 +1482,17 @@
          end
  
--        str << path_query
+         str << path_query
 +        path = path_query
 +
 +        #
 +        # Add URI delimiter if the path misses it (like as in FTP)
 +        #
 +        if not path.empty? and not str.empty? and path[0, 1] != '/'
-+          path = '/' + path 
++          path = '/' + path
 +        end
 +        str << path
++       end 
++       if @fragment
        end
  
        if @fragment
