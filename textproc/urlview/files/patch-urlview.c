82bb6334e8f86985680550caeba2be530fdd4b2e
danilo <danilo@FreeBSD.org>
2013-11-25 15:06:59 +0000

    - Fix warnings in urlview.c [1] - Add LICENSE - Add stage support
    PR:		ports/180681
    Submitted by:	Anton Shterenlikht <mexas@bris.ac.uk> (maintainer) [1]

--- textproc/urlview/files/patch-urlview.c
+++ textproc/urlview/files/patch-urlview.c
@@ -1,5 +1,5 @@
---- urlview.c.orig	Wed Nov  9 15:34:39 2005
-+++ urlview.c	Wed Nov  9 15:35:06 2005
+--- urlview.c.orig	2013-07-19 22:44:59.000000000 +0100
++++ urlview.c	2013-07-19 22:45:22.000000000 +0100
 @@ -46,6 +46,8 @@
  #include <rx/rxposix.h>
  #endif
@@ -7,5 +7,14 @@
 +#include "quote.h"
 +
  #define DEFAULT_REGEXP "(((https?|ftp|gopher)://|(mailto|file|news):)[^' \t<>\"]+|(www|web|w3)\\.[-a-z0-9.]+)[^' \t.,;<>\"\\):]"
- #define DEFAULT_COMMAND "url_handler.sh %s"
+ #define DEFAULT_COMMAND "firefox %s"
  #define SYSTEM_INITFILE "/usr/local/etc/urlview.conf"
+@@ -61,7 +63,7 @@
+   MOTION
+ };
+ 
+-extern int mutt_enter_string (unsigned char *buf, size_t buflen, int y, int x,
++extern int mutt_enter_string (char *buf, size_t buflen, int y, int x,
+ 		int flags);
+ 
+ void search_forward (char *search, int urlcount, char **url, int *redraw, int *current, int *top)
