17b15ee259c11071b260899c4a6c8e10a693289b
kevlo <kevlo@FreeBSD.org>
2013-11-26 02:16:54 +0000

    - Fix hang on reading config [1] - support STAGE
    PR:	ports/184015 [1]
    Submitted by:	Henry Hu <henry.hu.sh at gmail dot com>

--- sysutils/synergy/files/patch-CConfig.cpp
+++ sysutils/synergy/files/patch-CConfig.cpp
@@ -1,5 +1,5 @@
 --- src/lib/server/CConfig.cpp.orig	2011-01-21 11:51:35.000000000 +0800
-+++ src/lib/server/CConfig.cpp	2013-09-12 17:23:04.000000000 +0800
++++ src/lib/server/CConfig.cpp	2013-11-26 10:00:44.000000000 +0800
 @@ -1908,9 +1908,9 @@
  	return m_line;
  }
@@ -8,7 +8,7 @@
 +CConfigReadContext::operator bool() const
  {
 -	return m_stream;
-+	return !m_stream.bad();
++	return m_stream.good();
  }
  
  bool
