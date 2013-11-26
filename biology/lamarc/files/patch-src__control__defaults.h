38592c3bf229ae5d7d534d1fa1c2251e7f164e13
William Grzybowski <wg@FreeBSD.org>
2013-11-26 15:37:05 +0000

    biology/lamarc: fix build on current
    PR:		ports/184219
    Submitted by:	KATO Tsuguru <tkato432 yahoo.com>

---
+++ biology/lamarc/files/patch-src__control__defaults.h
@@ -0,0 +1,10 @@
+--- src/control/defaults.h.orig
++++ src/control/defaults.h
+@@ -3,6 +3,7 @@
+ #ifndef DEFAULTS_H
+ #define DEFAULTS_H
+ 
++#include <ctime>
+ #include <string>
+ #include "constants.h"
+ #include "local_build.h"
