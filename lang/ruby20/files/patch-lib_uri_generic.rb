574550b32658b4cf18b22ba5bc285f905376e279
swills <swills@FreeBSD.org>
2013-11-27 13:21:30 +0000

    - Fix patch to uri module that I broke in r334630 - Bump PORTREVISION
    PR:		ports/184315
    Submitted by:	Andris Raugulis <andris@govs.lv>

--- lang/ruby20/files/patch-lib_uri_generic.rb
+++ lang/ruby20/files/patch-lib_uri_generic.rb
@@ -1,9 +1,10 @@
---- lib/uri/generic.rb.orig	2012-07-20 01:56:21.000000000 +0000
-+++ lib/uri/generic.rb	2013-11-21 20:50:14.143925606 +0000
-@@ -1482,6 +1482,17 @@
+--- lib/uri/generic.rb.orig	2013-11-27 04:27:22.851503866 +0000
++++ lib/uri/generic.rb	2013-11-27 04:27:58.130502926 +0000
+@@ -1481,7 +1481,17 @@
+           end
          end
  
-         str << path_query
+-        str << path_query
 +        path = path_query
 +
 +        #
