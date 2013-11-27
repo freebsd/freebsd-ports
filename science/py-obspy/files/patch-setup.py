9d88e7853343b918a25ad44d0945ac3208bb595c
William Grzybowski <wg@FreeBSD.org>
2013-11-27 19:21:00 +0000

    - Fix build with distribute -> setuptools change
    Approved by:	portmgr (bapt, implicit)

--- science/py-obspy/files/patch-setup.py
+++ science/py-obspy/files/patch-setup.py
@@ -1,13 +1,30 @@
---- setup.py.orig	2012-12-17 14:04:38.000000000 +0000
-+++ setup.py	2013-05-16 15:35:53.000000000 +0000
-@@ -362,8 +362,8 @@
+--- ./setup.py.orig	2013-06-28 18:07:26.000000000 -0300
++++ ./setup.py	2013-11-27 17:04:07.589319517 -0200
+@@ -27,7 +27,6 @@
+ from distutils.unixccompiler import UnixCCompiler
+ from setuptools import find_packages, setup
+ from setuptools.extension import Extension
+-import distribute_setup
+ import glob
+ import os
+ import platform
+@@ -370,8 +369,8 @@
              # otherwise we just use the original compile method
              UnixCCompiler.linker_so = None
              return self._original_compile(obj, src, *args, **kwargs)
 -        UnixCCompiler.linker_so = ["gfortran"]
 -        self.compiler_so = ["gfortran"]
-+        UnixCCompiler.linker_so = ["FC"]
-+        self.compiler_so = ["FC"]
++        UnixCCompiler.linker_so = ["gfortran46"]
++        self.compiler_so = ["gfortran46"]
          cc_args = ['-c', '-fno-underscoring']
-         if sys.platform == 'darwin':
-             self.compiler_so = _darwin_compiler_fixup(self.compiler_so,
+         cc_args.append('-fPIC')
+         try:
+@@ -629,8 +628,6 @@
+ 
+ 
+ def setupPackage(gfortran=True, ccompiler=True):
+-    # automatically install distribute if the user does not have it installed
+-    distribute_setup.use_setuptools()
+     # use lib2to3 for Python 3.x
+     if sys.version_info[0] == 3:
+         convert2to3()
---
+++ www/py-cssmin/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- ./setup.py.orig	2013-11-27 16:51:22.144320041 -0200
++++ ./setup.py	2013-11-27 16:51:29.013319136 -0200
+@@ -4,7 +4,6 @@
+ import os
+ import re
+ 
+-from distribute_setup import use_setuptools; use_setuptools()
+ from setuptools import setup, find_packages
+ 
+ 
