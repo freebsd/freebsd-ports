d8f6eda33aa2e443badba0801688599aa84ed65e
wg <wg@FreeBSD.org>
2013-11-27 19:21:00 +0000

    - Fix build with distribute -> setuptools change
    Approved by:	portmgr (bapt, implicit)

---
+++ sysutils/py-filelike/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- ./setup.py.orig	2013-11-27 16:56:21.489321513 -0200
++++ ./setup.py	2013-11-27 16:57:06.823319522 -0200
+@@ -1,7 +1,5 @@
+ 
+ import sys
+-import distribute_setup
+-distribute_setup.use_setuptools()
+ from setuptools import setup, find_packages
+ 
+ 
