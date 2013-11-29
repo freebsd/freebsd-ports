5ba46f0790aa7e3e691fc97b12fc55a2dcc93c8a
William Grzybowski <wg@FreeBSD.org>
2013-11-29 11:40:24 +0000

    science/py-pydap: update to 3.1.1
    PR:		ports/183985
    Submitted by:	maintainer

--- science/py-pydap/files/patch-pavement.py
+++ science/py-pydap/files/patch-pavement.py
@@ -1,11 +1,11 @@
---- ./pavement.py.orig	2013-08-30 15:00:07.469952201 +0300
-+++ ./pavement.py	2013-08-30 15:00:24.309518177 +0300
+--- ./pavement.py.orig	2013-11-15 10:29:56.254992996 +0200
++++ ./pavement.py	2013-11-15 10:31:41.824987045 +0200
 @@ -9,7 +9,7 @@
  try:
      from pydap.lib import __version__
  except ImportError:
 -    __version__ = ('unknown',)
-+    __version__ = ('3.1',)
++    __version__ = (3,1,1)
  
  options = environment.options
  setup(**setup_meta)
