5e751d3303828881e3ae453520ed0b96f87d82ea
sunpoet <sunpoet@FreeBSD.org>
2013-11-29 22:09:32 +0000

    - Update to 8.6.9 - Use REINPLACE_CMD instead of patch files
    Changes:	http://www.methods.co.nz/asciidoc/CHANGELOG.html

--- textproc/asciidoc/files/patch-asciidoc.py
+++
@@ -1,8 +0,0 @@
---- ./asciidoc.py.orig	2013-05-20 13:25:45.362337047 +0200
-+++ ./asciidoc.py	2013-05-20 13:25:55.524336439 +0200
-@@ -1,4 +1,4 @@
--#!/usr/bin/env python
-+#!/usr/bin/env python2
- """
- asciidoc - converts an AsciiDoc text file to HTML or DocBook
- 
