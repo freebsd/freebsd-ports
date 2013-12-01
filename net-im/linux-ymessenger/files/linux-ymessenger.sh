39edf56f0933f7c5f614aad7fca76ecb6470fe23
Rene Ladan <rene@FreeBSD.org>
2013-12-01 00:37:20 +0000

    Remove expired ports: 2013-12-01 x11-fm/xnc: Latest release from 2005, upstream disappeared, does not build 2013-12-01 audio/liteamp: Dead upstream since 2004, does not build with clang 2013-12-01 net-im/tapioca-qt: Unmaintained upstream since 2007, not used by anything 2013-12-01 net-im/telepathy-qt: Unmaintained upstream since 2007, use net-im/telepathy-qt4 2013-12-01 x11-toolkits/linux-f10-gtk: GTK1 is deprecated 2013-12-01 net-im/linux-ymessenger: Depends on deprecated Linux GTK1 2013-12-01 emulators/linux-pete-mesagpu: Depends on deprecated Linux GTK1 2013-12-01 emulators/linux-pete-xgl2gpu: Depends on deprecated Linux GTK1 2013-12-01 emulators/linux-peops-softgpu: Depends on deprecated Linux GTK1 2013-12-01 finance/eqonomize-kde4: No upstream releases since 2008, does not build on FreeBSD 10 and 11 2013-12-01 comms/uhso-kmod: included in base system on all supported releases 2013-12-01 deskutils/ecru: No longer maintained upstream 2013-12-01 editors/gphpedit: No longer maintaned upstream 2013-12-01 irc/inspircd12: No upstream support; known vulnerable. Please use irc/inspircd 2013-12-01 graphics/py-clutter: Broken for more than 6 month 2013-12-01 graphics/py-clutter-gtk: depends on expired graphics/py-clutter

--- net-im/linux-ymessenger/files/linux-ymessenger.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -w $HOME/.ymessenger/preferences ]; then
-  /usr/bin/grep -q '^PrimaryIdentity $' $HOME/.ymessenger/preferences&&/usr/bin/sed -i '' -e '/^PrimaryIdentity $/s/$/ ""/' $HOME/.ymessenger/preferences
-fi
-export LD_LIBRARY_PATH=%%PREFIX%%/lib/linux-ymessenger
-cd %%PREFIX%%/libexec/linux-ymessenger
-./ymessenger.bin $*
