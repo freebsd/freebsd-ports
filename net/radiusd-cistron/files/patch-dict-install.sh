be00cf684507d29af3ccb739910a6b7c19244206
Dirk Meyer <dinoex@FreeBSD.org>
2013-12-01 05:42:35 +0000

    - fix default dictionary file

--- net/radiusd-cistron/files/patch-dict-install.sh
+++ net/radiusd-cistron/files/patch-dict-install.sh
@@ -1,5 +1,14 @@
 --- src/dict-install.sh.orig	2003-04-10 16:17:11.000000000 +0200
-+++ src/dict-install.sh	2013-11-17 09:11:22.000000000 +0100
++++ src/dict-install.sh	2013-12-01 06:23:00.000000000 +0100
+@@ -31,7 +31,7 @@
+ 
+ if [ $NEWTYPE = yes ]
+ then
+-	DESTDIR=$SHAREDIR
++	DESTDIR=$DESTROOT$SHAREDIR
+ else
+ 	DESTDIR=$RADDBDIR
+ fi
 @@ -41,9 +41,9 @@
  	mkdir -p $DESTDIR
  fi
