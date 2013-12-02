85625d4a3ad2c146877f28b0971c95affb5f247f
William Grzybowski <wg@FreeBSD.org>
2013-12-02 13:23:26 +0000

    games/megaglest: update to 3.9.0.4
    - Update to 3.9.0.4 [1]
    - Allow staging
    - Convert lib depends to new format
    
    PR:		ports/184342 [1]
    Submitted by:	maintainer

--- games/megaglest/files/patch-source-shared_lib-sources-platform-posix-ircclient.cpp
+++ games/megaglest/files/patch-source-shared_lib-sources-platform-posix-ircclient.cpp
@@ -1,14 +1,13 @@
---- source/shared_lib/sources/platform/posix/ircclient.cpp	2012-11-10 03:22:28.000000000 -0600
-+++ source/shared_lib/sources/platform/posix/ircclient.cpp.new	2012-11-20 11:51:10.000000000 -0600
-@@ -14,11 +14,7 @@
- #include "util.h"
- #include "platform_common.h"
+--- source/shared_lib/sources/platform/posix/ircclient.cpp.orig	2013-11-22 15:51:23.000000000 -0600
++++ source/shared_lib/sources/platform/posix/ircclient.cpp	2013-11-22 15:51:42.000000000 -0600
+@@ -19,10 +19,7 @@
+ #if !defined(DISABLE_IRCCLIENT)
+ 
  #include <libircclient.h>
--
 -// upstream moved some defines into new headers as of 1.6
 -#ifndef LIBIRCCLIENT_PRE1_6
  #include <libirc_rfcnumeric.h>
 -#endif
  
- #include <stdio.h>
- #include <stdarg.h>
+ #endif
+ 
