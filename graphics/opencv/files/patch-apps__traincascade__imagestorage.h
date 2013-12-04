481fb7ae25d2cadd7407b763c6fd10f97c884b84
jhale <jhale@FreeBSD.org>
2013-12-04 02:51:00 +0000

    - Update graphics/*opencv* to 2.4.7 - Revert options helpers to if statements since the OFF condition is not   applied when OPTIONS_EXCLUDE is used - Move most of the OpenCV modules from the graphics/opencv-core port to   graphics/opencv, leaving opencv-core as just the bare minimum required   for building ffmpeg with OpenCV support - Install examples for python and java bindings - Add new slave port graphics/opencv-java: Java bindings for OpenCV - Bump PORTREVISION and make dependency adjustments and fixes for   dependent ports - Add UPDATING entry

---
+++ graphics/opencv/files/patch-apps__traincascade__imagestorage.h
@@ -0,0 +1,10 @@
+--- ./apps/traincascade/imagestorage.h.orig	2013-07-10 07:49:00.000000000 -0400
++++ ./apps/traincascade/imagestorage.h	2013-10-23 23:52:32.000000000 -0400
+@@ -1,6 +1,7 @@
+ #ifndef _OPENCV_IMAGESTORAGE_H_
+ #define _OPENCV_IMAGESTORAGE_H_
+ 
++#include <cstdio>
+ #include "highgui.h"
+ 
+ using namespace cv;
---
+++ math/saga/files/patch-src__modules_images__modules_opencv__opencv__opencv.h
@@ -0,0 +1,12 @@
+--- ./src/modules_images/modules_opencv/opencv/opencv.h.orig	2013-12-03 19:17:30.000000000 -0500
++++ ./src/modules_images/modules_opencv/opencv/opencv.h	2013-12-03 19:19:37.000000000 -0500
+@@ -73,7 +73,8 @@
+ #include "MLB_Interface.h"
+ 
+ //---------------------------------------------------------
+-#include <opencv/cv.h>
++#include <opencv2/core/core_c.h>
++#include <opencv2/imgproc/imgproc_c.h>
+ 
+ 
+ ///////////////////////////////////////////////////////////
