67efdd653dbf94a2896dbceb09ad525ce43de235
makc <makc@FreeBSD.org>
2013-12-10 18:42:46 +0000

    - Update to 2.0.0rc3 - Remove NLS options and related patches for simplicity - Add patch to fix build on 10 and head

---
+++ cad/librecad/files/patch-librecad__src__lib__engine__rs_color.h
@@ -0,0 +1,10 @@
+--- ./librecad/src/lib/engine/rs_color.h.orig	2013-11-13 10:16:38.000000000 +0000
++++ ./librecad/src/lib/engine/rs_color.h	2013-12-10 17:20:29.235786970 +0000
+@@ -29,6 +29,7 @@
+ #define RS_COLOR_H
+ 
+ #include <QColor>
++#include <ostream>
+ 
+ #include "rs_flags.h"
+ 
