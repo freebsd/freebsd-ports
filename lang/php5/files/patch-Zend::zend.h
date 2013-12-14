0d6e949ffb7ca61ae987a7f297c232452fff2aae
ale <ale@FreeBSD.org>
2013-12-14 12:45:40 +0000

    Update to 5.4.23 release and refresh patches.

--- lang/php5/files/patch-Zend::zend.h
+++ lang/php5/files/patch-Zend::zend.h
@@ -1,8 +1,8 @@
---- Zend/zend.h.orig	2008-08-15 21:47:23.000000000 +0200
-+++ Zend/zend.h	2008-09-25 22:52:50.000000000 +0200
-@@ -192,7 +192,7 @@
- # define ZEND_FASTCALL
+--- Zend/zend.h.orig	2013-12-10 19:32:43.000000000 +0000
++++ Zend/zend.h	2013-12-13 21:55:53.836235768 +0000
+@@ -180,7 +180,7 @@ char *alloca ();
  #endif
+ #define restrict __restrict__
  
 -#if (HAVE_ALLOCA || (defined (__GNUC__) && __GNUC__ >= 2)) && !(defined(ZTS) && defined(ZEND_WIN32)) && !(defined(ZTS) && defined(NETWARE)) && !(defined(ZTS) && defined(HPUX)) && !defined(DARWIN)
 +#if (HAVE_ALLOCA || (defined (__GNUC__) && __GNUC__ >= 2)) && !(defined(ZTS) && defined(ZEND_WIN32)) && !(defined(ZTS) && defined(NETWARE)) && !(defined(ZTS) && defined(HPUX)) && !defined(DARWIN) && !(defined(ZTS) && defined(__FreeBSD__))
