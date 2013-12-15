413d190273b34a781f1f79dfc4d81bf422407100
Koop Mast <kwm@FreeBSD.org>
2013-12-15 23:46:53 +0000

    Update to 0.10.
    Adopt.

---
+++ devel/regexxer/files/patch-src_fileshared.h
@@ -0,0 +1,11 @@
+--- src/fileshared.h.orig	2011-10-06 14:50:48.000000000 +0200
++++ src/fileshared.h	2013-12-16 00:09:23.000000000 +0100
+@@ -23,7 +23,7 @@
+ 
+ #include "sharedptr.h"
+ 
+-#include <gtkmm/textbuffer.h>
++#include <gtkmm.h>
+ #include <functional>
+ #include <utility>
+ #include <vector>
