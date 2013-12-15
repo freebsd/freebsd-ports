1e34739b64d384b87ee2c57027df596b464c79ab
William Grzybowski <wg@FreeBSD.org>
2013-12-15 16:22:17 +0000

    Use setuptools for all Python ports.
    Setuptools is the preferred method to manage Python distributions after
    many changes to the packaging ecosystem over the past couple of years.
    
    Only ports using USE_PYDISTUTILS= yes are affected by this commit, ports using
    USE_PYDISTUTILS= easy_install remains the same however this usage is now
    deprecated and should be converted to USE_PYDISTUTILS= yes.
    
    Some Python distributions do not work with setuptools out of the box because
    they extend the install command from distutils and not setuptools, and
    so they need to be patched accordingly.
    
    pip (which leverages setuptools) works around the issue by using eggs, however
    we want to get rid of those as well, as support for "flat" installation is
    unavailable or has other issues associated with it.
    
    This work allows us to unify how python packages are built, ensure that Python
    distributions are installed consistently, reduces complexity for Python port
    maintainers and paves the way for simplifying the Python ports framework in
    the future.
    
    With hat on:	python
    Reviewed by:	koobs, antoine
    Exp-run:	bdrewery
    Approved by:	bdrewery (portmgr)

---
+++ net/py-ldaptor/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2013-12-13 13:49:46.929425897 -0200
++++ setup.py	2013-12-13 13:49:58.999423613 -0200
+@@ -154,7 +154,6 @@
+                     'build_locale': build_locale,
+                     'clean': clean,
+                     'clean_locale': clean_locale,
+-                    'install': install,
+                     'install_locale': install_locale,
+                     },
+ 
