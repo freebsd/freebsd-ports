860a64278596900a78e9ed658a7b7b12d5400a9d
avilla <avilla@FreeBSD.org>
2013-12-16 22:59:42 +0000

    - Update Calligra Suite to 2.7.5. - Require a new compiler to build Calligra. - Make GTL really an option in Calligra. - STAGEify. - Use OPTIONS helpers. - Set NO_ARCH for translation ports. - Add translation port for Intelingua.
    Calligra 2.7 release notes:
    http://www.calligra.org/news/calligra-2-7-released

--- editors/calligra-l10n/files/bsd.l10n.mk
+++ editors/calligra-l10n/files/bsd.l10n.mk
@@ -8,7 +8,8 @@ DIST_SUBDIR=	KDE/calligra-l10n
 CONFLICTS=	${PKGNAMEPREFIX}koffice-l10n-2.*
 
 USE_KDE4=	kdehier kdelibs kdeprefix automoc4
-USE_QT4=	xml moc_build qmake_build rcc_build uic_build
-USE_BZIP2=	yes
+USE_QT4=	corelib xml moc_build qmake_build rcc_build uic_build
 USES=		cmake gettext
+NO_ARCH=	yes
+USE_XZ=		yes
 .endif
