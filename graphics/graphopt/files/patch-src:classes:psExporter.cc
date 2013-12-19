d70ee3fa27ad2832ce604081fd5f867e7bcd6711
Pawel Pekala <pawel@FreeBSD.org>
2013-12-19 14:14:28 +0000

    - Fix build with clang - Add LICENSE - Add desktop entry file - Support staging
    PR:		pors/184815
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- graphics/graphopt/files/patch-src:classes:psExporter.cc
+++ graphics/graphopt/files/patch-src:classes:psExporter.cc
@@ -1,9 +1,12 @@
 --- src/classes/psExporter.cc.orig	Mon Aug  9 09:31:11 2004
 +++ src/classes/psExporter.cc	Mon Aug  9 09:31:38 2004
-@@ -23,13 +23,13 @@ void psExporter::do_export() {
-    fclose(outfile);
- }
+@@ -1,4 +1,5 @@
+ #include "psExporter.h"
++#include <cstdlib>
  
+ 
+ psExporter::psExporter(char *what_file, nodes *what_nodes) {
+@@ -26,7 +27,7 @@
  
  void psExporter::write_headers(FILE *outfile) {
     char *line = new char[200];
@@ -12,6 +15,3 @@
             translatexcoord((double) biggest_x) + 2 * margin,
             translateycoord((double) biggest_y) + 2 * margin);
     fwrite(line, strlen(line), 1, outfile);
-    delete line;
- }
- 
