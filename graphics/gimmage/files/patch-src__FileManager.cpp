aaafecb9c54da76fa6171cc40eef795c91a39ea0
Pawel Pekala <pawel@FreeBSD.org>
2013-12-19 20:04:37 +0000

    - Fix build with clang - Support desktop-file-utils - Port supports staging
    PR:		ports/184832
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- graphics/gimmage/files/patch-src__FileManager.cpp
+++ graphics/gimmage/files/patch-src__FileManager.cpp
@@ -1,6 +1,6 @@
 --- ./src/FileManager.cpp.orig	2007-07-20 23:42:55.000000000 +0400
 +++ ./src/FileManager.cpp	2007-07-20 23:43:03.000000000 +0400
-@@ -23,6 +23,7 @@
+@@ -23,12 +23,13 @@
  
  #include "FileManager.h"
  #include "defines.h"
@@ -8,3 +8,9 @@
  
  #include "../config.h"
  
+ extern "C" {
+ #include <libintl.h>
+-// #include <unistd.h>
++#include <unistd.h>
+ #include <sys/stat.h>
+ }
