e4d3fabab0126a23b2796482c3036a97dbb5e0f4
Pawel Pekala <pawel@FreeBSD.org>
2013-12-20 16:21:19 +0000

    - Fix build with clang - Update MASTER_SITES - Support staging
    PR:		ports/184863
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ audio/mp3stat/files/patch-base.cpp
@@ -0,0 +1,9 @@
+--- base.cpp.orig
++++ base.cpp
+@@ -1,4 +1,5 @@
+-#include <stdio.h>
++#include <cstdio>
++#include <cstdlib>
+ #include <dlfcn.h>
+ #include <dirent.h>
+ 
---
+++ audio/mp3stat/files/patch-ui__console__cli.cpp
@@ -0,0 +1,9 @@
+--- ui/console/cli.cpp.orig
++++ ui/console/cli.cpp
+@@ -1,4 +1,5 @@
+-#include <stdio.h>
++#include <cstdio>
++#include <cstdlib>
+ #include "cli.h"
+ 
+ using namespace std;
