1fc27b753e14e84f860265d36f4ca6396bf00714
Wen Heping <wen@FreeBSD.org>
2013-12-20 15:29:07 +0000

    - Fix build with clang/libc++. - Support staging.
    PR:		184974
    Submitted by:	tijl@

---
+++ biology/clustalw/files/patch-src-general-VectorOutOfRange.h
@@ -0,0 +1,10 @@
+--- src/general/VectorOutOfRange.h.orig	2013-12-20 23:16:13.000000000 +0800
++++ src/general/VectorOutOfRange.h	2013-12-20 23:16:37.000000000 +0800
+@@ -5,6 +5,7 @@
+  */
+ #include <stdexcept>
+ #include <exception>
++#include <string>
+ namespace clustalw
+ {
+ 
