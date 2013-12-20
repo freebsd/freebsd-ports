adda302813281706cdc478850c10a267f8e260fd
amdmi3 <amdmi3@FreeBSD.org>
2013-12-20 23:32:26 +0000

    - Support staging - Fix segfault when built with clang

--- games/osgg/files/patch-main.cpp
+++ games/osgg/files/patch-main.cpp
@@ -1,5 +1,5 @@
---- main.cpp.orig	2013-09-13 20:24:16.310226753 +0400
-+++ main.cpp	2013-09-13 20:24:50.127352703 +0400
+--- main.cpp.orig	2009-03-14 01:46:04.000000000 +0300
++++ main.cpp	2013-12-21 03:29:52.401817465 +0400
 @@ -26,6 +26,7 @@
  #include <fstream>
  #include <iostream>
@@ -8,3 +8,11 @@
  
  #include <sys/time.h>
  
+@@ -340,6 +341,7 @@
+       }
+     }
+   }
++  return false;
+ }
+ 
+ void classBullets::shoot(entity owner, gPs velocity)
