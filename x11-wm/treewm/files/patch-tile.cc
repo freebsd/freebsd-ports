31e36f9e33fbd8342fdadd7e489ad8fa45ed50cf
Mathieu Arnold <mat@FreeBSD.org>
2013-12-21 10:24:27 +0000

    MFH: r337090
    - Fix build with clang
    - Add LICENSE
    - Support staging
    - Reformat pkg-descr
    
    PR:		ports/184888
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>
    Approved by:	portmgr (implicit)

---
+++ x11-wm/treewm/files/patch-tile.cc
@@ -0,0 +1,10 @@
+--- src/tile.cc.orig
++++ src/tile.cc
+@@ -5,6 +5,7 @@
+ #include <algorithm>
+ #include <stack>
+ //#include <stdio.h>
++#include <stdlib.h>
+ 
+ /* 
+  * changed<05.02.2003> by Rudolf Polzer: including namespace std (C++)
