bc36b898c309d5bb99256b3c19ed31b90bf836c1
swills <swills@FreeBSD.org>
2013-12-21 12:52:58 +0000

    - Update to 0.10.10

--- x11/xpra/files/patch-setup.py
+++ x11/xpra/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2013-02-21 15:27:40.000000000 +0000
-+++ setup.py	2013-03-24 18:51:02.000000000 +0000
-@@ -568,7 +568,7 @@
+--- ./setup.py.orig	2013-12-20 21:07:29.448820756 +0000
++++ ./setup.py	2013-12-20 21:07:38.485819828 +0000
+@@ -862,7 +862,7 @@
      scripts = ["scripts/xpra", "scripts/xpra_launcher"]
      man_pages = ["man/xpra.1", "man/xpra_launcher.1"]
      data_files += [
 -                    ("share/man/man1", man_pages),
 +                    ("man/man1", man_pages),
-                     ("share/xpra", ["xpra.README", "COPYING"]),
-                     ("share/wimpiggy", ["wimpiggy.README"]),
+                     ("share/xpra", ["README", "COPYING"]),
                      ("share/xpra/icons", glob.glob("icons/*")),
+                     ("share/applications", ["xdg/xpra_launcher.desktop", "xdg/xpra.desktop"]),
