327885f1f90fed5f18251ec4f576b58af21de2e3
swills <swills@FreeBSD.org>
2013-12-22 03:31:32 +0000

    - Improve DEPENDS [1] - Correct USE_XORG [1] - Install config files [1] - Patch server to use glib [1] - Use space, not tab for xpra [1] - Add license - Support staging
    PR:		ports/181482 [1]
    Submitted by:	Andrew Childs <lorne@cons.org.nz> [1]

--- x11/xpra/files/patch-setup.py
+++ x11/xpra/files/patch-setup.py
@@ -1,5 +1,5 @@
---- ./setup.py.orig	2013-12-20 21:07:29.448820756 +0000
-+++ ./setup.py	2013-12-20 21:07:38.485819828 +0000
+--- ./setup.py.orig	2013-12-19 09:14:12.000000000 +0000
++++ ./setup.py	2013-12-21 21:13:18.689932655 +0000
 @@ -862,7 +862,7 @@
      scripts = ["scripts/xpra", "scripts/xpra_launcher"]
      man_pages = ["man/xpra.1", "man/xpra_launcher.1"]
@@ -9,3 +9,16 @@
                      ("share/xpra", ["README", "COPYING"]),
                      ("share/xpra/icons", glob.glob("icons/*")),
                      ("share/applications", ["xdg/xpra_launcher.desktop", "xdg/xpra.desktop"]),
+@@ -910,7 +910,11 @@
+             if not use_Xdummy_wrapper and "scripts/xpra_Xdummy" in scripts:
+                 #if we're not using the wrapper, don't install it
+                 scripts.remove("scripts/xpra_Xdummy")
+-            etc_files.append(xorg_conf)
++            ## FreeBSD ports: we want to install this as xpra.conf.sample
++            ## Prevent it here, handle in post-install target
++            #etc_files.append(xorg_conf)
++            ## FreeBSD ports: override etc_prefix
++            etc_prefix='etc/xpra'
+         data_files.append((etc_prefix, etc_files))
+     setup_options["scripts"] = scripts
+ 
