d8338d9bfa2f5ef8dca1a9ebe8c1dabed694678e
Rene Ladan <rene@FreeBSD.org>
2013-12-22 11:48:43 +0000

    Remove expired ports: 2013-12-22 devel/ruby-cvs: Broken for more than 6 month 2013-12-22 devel/hs-reactive: Broken for more than 6 month 2013-12-22 databases/rubygem-delayed_job_data_mapper: Broken for more than 6 month 2013-12-22 graphics/xmagv: Broken for more than 6 month 2013-12-22 lang/slib-gauche: Broken for more than 6 month 2013-12-22 graphics/f-spot: Broken for more than 6 month 2013-12-22 sysutils/linux-megamgr: Broken for more than 6 month 2013-12-22 games/linux-savage-samuraiwars: Broken for more than 6 month 2013-12-22 ftp/gftp: Broken for more than 6 month 2013-12-22 devel/hs-DeepArrow: Broken for more than 6 month 2013-12-22 converters/py-svglib: Broken for more than 6 month 2013-12-22 converters/p5-Unicode-Lite: Broken for more than 6 month 2013-12-22 mail/squirrelmail-calendar_sql_backend-plugin: Broken for more than 6 month 2013-12-22 finance/quantlib: Broken for more than 6 month 2013-12-22 games/xkobo: Broken for more than 6 month 2013-12-22 games/quake3-rq3: Broken for more than 6 month 2013-12-22 math/fbm: No new release since 2004, new supported upstream

--- devel/hs-reactive/files/patch-src__FRP__Reactive__Num.hs
+++
@@ -1,8 +0,0 @@
---- ./src/FRP/Reactive/Num.hs.orig	2010-07-28 18:48:55.000000000 +0200
-+++ ./src/FRP/Reactive/Num.hs	2012-05-13 12:54:39.226470104 +0200
-@@ -1,4 +1,4 @@
--{-# LANGUAGE TypeSynonymInstances #-}
-+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
- {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
- ----------------------------------------------------------------------
- -- |
