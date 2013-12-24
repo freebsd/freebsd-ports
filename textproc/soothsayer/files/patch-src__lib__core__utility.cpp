3ef8f2c31023fd832d4a7ea32cf9ab0c59a2b826
rakuco <rakuco@FreeBSD.org>
2013-12-24 17:41:24 +0000

    Fix the build with clang.

---
+++ textproc/soothsayer/files/patch-src__lib__core__utility.cpp
@@ -0,0 +1,10 @@
+--- ./src/lib/core/utility.cpp.orig	2013-12-24 19:36:58.000000000 +0200
++++ ./src/lib/core/utility.cpp	2013-12-24 19:37:26.000000000 +0200
+@@ -22,6 +22,7 @@
+                                                                 **********(*)*/
+ 
+ #include "utility.h"
++#include <cstdlib>
+ 
+ 
+ /** Convert string to lower case
