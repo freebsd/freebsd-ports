10443b190a4bae23473dc9a42a8dc2403abc1e60
pawel <pawel@FreeBSD.org>
2013-12-24 15:04:04 +0000

    - Fix build with clang - Add LICENSE - Switch to PLIST_FILES
    PR:		ports/184905
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

--- games/gma/files/patch-scores.h
+++ games/gma/files/patch-scores.h
@@ -1,15 +1,16 @@
 --- src/scores.h.orig	Mon Apr 17 02:52:22 2000
 +++ src/scores.h	Sun May 18 19:19:41 2003
-@@ -22,9 +22,11 @@
+@@ -22,10 +22,12 @@
  #ifndef SCORES_H
  #define SCORES_H
  
 -#include <stl.h>
-+#include <map.h>
++#include <map>
  #include <string>
  #include "pattern.h"
-+
-+using namespace std;
  
++using std::map;
++
  class Scores
  {
+ public:
