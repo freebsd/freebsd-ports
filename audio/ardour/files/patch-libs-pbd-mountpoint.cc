4d15cb7b6fa65319c46d9de569b3d85bf338292d
Alexey Dokuchaev <danfe@FreeBSD.org>
2013-12-24 01:15:44 +0000

    - Fix several C++ issues with modern compilers - While here, stagify and mute REINPLACE_CMD's

---
+++ audio/ardour/files/patch-libs-pbd-mountpoint.cc
@@ -0,0 +1,11 @@
+--- libs/pbd/mountpoint.cc.orig
++++ libs/pbd/mountpoint.cc
+@@ -19,7 +19,7 @@
+ */
+ 
+ #include <cstdio>
+-#include <cstring>
++#include <cstdlib>
+ #include <string>
+ #include <cstring>
+ #include <limits.h>
