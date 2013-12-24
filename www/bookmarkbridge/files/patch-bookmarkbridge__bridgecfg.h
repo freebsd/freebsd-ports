3120212e0dadce1a0f51e52fe57f79c820b92db3
pawel <pawel@FreeBSD.org>
2013-12-24 14:23:21 +0000

    - Fix build with clang - Add desktop entry file - Use shebangfix - Support staging
    PR:		ports/184901
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ www/bookmarkbridge/files/patch-bookmarkbridge__bridgecfg.h
@@ -0,0 +1,9 @@
+--- bookmarkbridge/bridgecfg.h.orig	2013-12-06 03:29:29.000000000 +0900
++++ bookmarkbridge/bridgecfg.h	2013-12-06 03:29:46.000000000 +0900
+@@ -31,6 +31,7 @@
+ #include "bknode.h"
+ #include "bkexcept.h"
+ 
++#include <cstdlib>
+ #include <vector>
+ #include <qstring.h>
