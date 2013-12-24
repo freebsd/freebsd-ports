8f8daf14a39e700fe184c7f1b4e6fe14671e9aa0
Pawel Pekala <pawel@FreeBSD.org>
2013-12-24 15:04:04 +0000

    - Fix build with clang - Add LICENSE - Switch to PLIST_FILES
    PR:		ports/184905
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ games/gma/files/patch-apprentice.cc
@@ -0,0 +1,11 @@
+--- src/apprentice.cc.orig
++++ src/apprentice.cc
+@@ -32,6 +32,8 @@
+ #include "moves.h"
+ #include "gma.h"
+ 
++using std::string;
++
+ #define BOARD_SIZE 50
+ #define MAX_DEPTH 2
+ 
