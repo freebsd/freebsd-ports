e55550db1e027dc6e96499057e15cbdf314161c7
mat <mat@FreeBSD.org>
2013-12-27 17:31:10 +0000

    Fix build on 10.
    maintainer timeout.
    
    PR:		ports/184724
    Submitted by:	mat

--- graphics/libecwj2/files/patch-Source-C-NCSnet-NCScnet3-NCSProxy.cpp
+++ graphics/libecwj2/files/patch-Source-C-NCSnet-NCScnet3-NCSProxy.cpp
@@ -1,6 +1,17 @@
 --- Source/C/NCSnet/NCScnet3/NCSProxy.cpp.orig	2009-04-05 21:38:54.000000000 +0400
-+++ Source/C/NCSnet/NCScnet3/NCSProxy.cpp	2009-04-05 21:39:11.000000000 +0400
-@@ -639,7 +639,7 @@
++++ Source/C/NCSnet/NCScnet3/NCSProxy.cpp	2013-12-12 15:30:11.000000000 +0100
+@@ -43,6 +43,10 @@
+ #include <stdio.h> // for cuserid
+ #endif
+ 
++#ifdef FREEBSD
++#include <unistd.h>
++#endif
++
+ /** Dialog to prompt for Username and Password.
+  */
+ bool NCSGetPasswordDlg( const char *pszSite, const char *pszRealm, string &sUsername, string &sPassword, bool &bRememberAuth );
+@@ -639,7 +643,7 @@
  		return(NCSStrDup(szNameBuf));
  	}
  #elif defined(POSIX)
