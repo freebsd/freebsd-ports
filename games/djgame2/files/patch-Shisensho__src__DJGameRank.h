5459d209ffe61419d6ca521ed0e4dc5a71f89e4e
Pawel Pekala <pawel@FreeBSD.org>
2013-12-27 19:16:55 +0000

    - Fix build with clang - Use multiple make jobs - Strip binaries - Support staging

---
+++ games/djgame2/files/patch-Shisensho__src__DJGameRank.h
@@ -0,0 +1,10 @@
+--- ./Shisensho/src/DJGameRank.h.orig	2008-03-11 13:17:22.000000000 +0100
++++ ./Shisensho/src/DJGameRank.h	2013-12-27 20:07:09.040179358 +0100
+@@ -22,6 +22,7 @@
+ #ifndef DJGAMERANK_H
+ #define DJGAMERANK_H
+ 
++#include <time.h>
+ #include "DJTreeWidget.h"
+ #include "protocol.h"
+ 
