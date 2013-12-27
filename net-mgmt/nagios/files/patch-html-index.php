78873ed8a42c1d9d949934c9576c4cce0d7db35c
Dmitry Sivachenko <demon@FreeBSD.org>
2013-12-27 12:00:02 +0000

    Fix tac.cgi location for people with redefined $cgi_base_url in config.inc.php.
    Submitted by:	mat

--- net-mgmt/nagios/files/patch-html-index.php
+++ net-mgmt/nagios/files/patch-html-index.php
@@ -1,11 +1,19 @@
 --- html/index.php.orig	2013-08-30 21:46:14.000000000 +0400
-+++ html/index.php	2013-12-27 11:53:31.000000000 +0400
-@@ -43,7 +43,7 @@
++++ html/index.php	2013-12-27 15:56:06.000000000 +0400
+@@ -8,6 +8,7 @@
+ </head>
+ 
+ <?php
++include_once(dirname(__FILE__).'/includes/utils.inc.php');
+  // allow specifying main window URL for permalinks, etc.
+ $corewindow="main.php";
+ if(isset($_GET['corewindow'])){
+@@ -43,7 +44,7 @@
  
  <frameset cols="180,*" style="border: 0px; framespacing: 0px">
  <frame src="side.php" name="side" frameborder="0" style="">
 -<frame src="<?php echo $corewindow;?>" name="main" frameborder="0" style="">
-+<frame src="cgi-bin/tac.cgi" name="main" frameborder="0">
++<frame src="<?php echo $cfg["cgi_base_url"];?>/tac.cgi" name="main" frameborder="0">
  
  <noframes>
  <!-- This page requires a web browser which supports frames. --> 
