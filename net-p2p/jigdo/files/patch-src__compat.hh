cc3ed94d9b22efafa88145eff6845daaa246e824
Pawel Pekala <pawel@FreeBSD.org>
2013-12-28 18:01:26 +0000

    MFH: r337742
    - Fix build with clang
    - Fix build when GUI=on, needs pkgconfig to find GTK libs
    - Use options helpers, subs, modernize descriptions
    - Support staging
    
    Approved by:    portmgr

---
+++ net-p2p/jigdo/files/patch-src__compat.hh
@@ -0,0 +1,10 @@
+--- ./src/compat.hh.orig	2013-12-27 16:00:53.907143596 +0100
++++ ./src/compat.hh	2013-12-27 16:01:15.205152700 +0100
+@@ -20,6 +20,7 @@
+ 
+ #include <string>
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <unistd-jigdo.h>
+ #include <sys/stat.h>
+ #include <sys/types.h>
