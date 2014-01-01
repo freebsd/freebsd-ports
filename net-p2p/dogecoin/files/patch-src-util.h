97d91945f2e4a4f048abac8da1dff83a756db611
Steve Wills <swills@FreeBSD.org>
2014-01-01 16:32:00 +0000

    - Update to 1.3 [1] - Fix build on 8.x - Switch to C++11 compiler
    PR:		ports/185247 [1]
    Submitted by:	David Bern <odiegit@gmail.com> [1]

---
+++ net-p2p/dogecoin/files/patch-src-util.h
@@ -0,0 +1,10 @@
+--- src/util.h.orig	2013-12-31 20:53:22.000000000 +0000
++++ src/util.h	2013-12-31 20:53:40.000000000 +0000
+@@ -18,6 +18,7 @@
+ #include <map>
+ #include <vector>
+ #include <string>
++#include <cstdarg>
+ 
+ #include <boost/thread.hpp>
+ #include <boost/filesystem.hpp>
