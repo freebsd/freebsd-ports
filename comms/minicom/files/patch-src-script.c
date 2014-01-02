250294032e6629fb63118cdc412c4633ebd312f3
Johan van Selst <johans@FreeBSD.org>
2014-01-02 19:32:27 +0000

    Add patch to fix build, forgotten in previous commit
    Reported by:	Jan Henrik Sylvester <me@janh.de>, Eric Camachat <eric.camachat@gmail.com>
    Pointyhat to:	johans

--- comms/minicom/files/patch-src-script.c
+++ comms/minicom/files/patch-src-script.c
@@ -1,6 +1,14 @@
---- src/script.c.orig	2011-07-23 19:58:32.000000000 +0200
-+++ src/script.c	2011-12-28 16:07:59.000000000 +0100
-@@ -69,7 +69,7 @@ struct env {
+--- src/script.c.orig	2013-10-29 12:07:42.000000000 +0100
++++ src/script.c	2014-01-02 20:30:55.000000000 +0100
+@@ -32,6 +32,7 @@
+ #include <config.h>
+ #endif
+ 
++#include <sys/wait.h>
+ #include <stdarg.h>
+ 
+ #include "port.h"
+@@ -76,7 +77,7 @@ struct env {
  struct env *curenv;		/* Execution environment */
  int gtimeout = 120;		/* Global Timeout */
  int etimeout = 0;		/* Timeout in expect routine */
