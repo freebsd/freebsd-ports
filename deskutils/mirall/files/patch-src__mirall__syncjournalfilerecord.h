ce90c812298c4c8f63c7bbfb4d8c3ebd6a542149
Mathieu Arnold <mat@FreeBSD.org>
2014-01-09 18:54:47 +0000

    Add new patch to fix build on 10+ Regen old patch.

---
+++ deskutils/mirall/files/patch-src__mirall__syncjournalfilerecord.h
@@ -0,0 +1,10 @@
+--- ./src/mirall/syncjournalfilerecord.h.orig	2014-01-09 19:53:32.000000000 +0100
++++ ./src/mirall/syncjournalfilerecord.h	2014-01-09 19:53:44.000000000 +0100
+@@ -14,6 +14,7 @@
+ #ifndef SYNCJOURNALFILERECORD_H
+ #define SYNCJOURNALFILERECORD_H
+ 
++#include <time.h>
+ #include <QString>
+ #include <QDateTime>
+ 
