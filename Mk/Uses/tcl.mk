a6c4cdd34fa392290e3a4584e53db7d945f7352a
bapt <bapt@FreeBSD.org>
2014-01-10 13:10:34 +0000

    backout 2 changes that should not have been committed with r339348

--- Mk/Uses/tcl.mk
+++ Mk/Uses/tcl.mk
@@ -57,7 +57,6 @@
 # Build-time / Run-time only dependencies can be specified with build or run.
 #
 
-.if ${USES:Mtk} || ${USES:Mtk\:*}
 .if !defined(_INCLUDE_USES_TCL_MK)
 _INCLUDE_USES_TCL_MK=	yes
 
