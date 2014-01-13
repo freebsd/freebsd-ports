ae19526691ddadb5ae949d67d5aadf4c039900ff
gahr <gahr@FreeBSD.org>
2014-01-13 10:44:50 +0000

    - Convert to USES=tk - STAGE-clean - Fix a run-time error when no LANG environment variable is defined

--- textproc/tkxmlive/files/patch-interface.tcl
+++ textproc/tkxmlive/files/patch-interface.tcl
@@ -1,6 +1,5 @@
-$FreeBSD$
---- interface.tcl	Thu May 30 23:56:36 2002
-+++ interface.tcl.new	Fri Feb 20 11:06:41 2004
+--- interface.tcl.orig	2002-05-30 23:56:36.000000000 +0200
++++ interface.tcl	2013-08-07 14:58:22.000000000 +0200
 @@ -1,4 +1,6 @@
 -#!/usr/bin/wish
 +#!/bin/sh
@@ -9,3 +8,18 @@ $FreeBSD$
  
  
  package require msgcat
+@@ -17,9 +19,11 @@
+ source library/scrolled_text.tcl
+ 
+ 
+-set mcDir [file join [file dirname [info script]] gui/msgs]
+-msgcat::mcload $mcDir
+-msgcat::mclocale $env(LANG)
++if {[info exists env(LANG)]} {
++  set mcDir [file join [file dirname [info script]] gui/msgs]
++  msgcat::mcload $mcDir
++  msgcat::mclocale $env(LANG)
++}
+ 
+ proc SetResource { win } {
+ 
