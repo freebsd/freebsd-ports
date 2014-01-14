f784419aa2702d5c5df571dba27d623a524f86eb
Antoine Brodin <antoine@FreeBSD.org>
2014-01-14 21:11:25 +0000

    MFH: r339718
    - Fix build with clang
    - Add LICENSE
    
    PR:		ports/185697
    Submitted by:	KATO Tsuguru
    Approved by:	portmgr (implicit)

--- net/libsocketcpp/files/patch-src::tcpservertest.cpp
+++ net/libsocketcpp/files/patch-src::tcpservertest.cpp
@@ -1,5 +1,14 @@
 --- src/tcpservertest.cpp.orig	2012-12-03 14:28:35.000000000 +0100
 +++ src/tcpservertest.cpp	2012-12-03 14:28:46.000000000 +0100
+@@ -22,7 +22,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <iostream.h>
++#include <iostream>
+ #include <unistd.h>
+ #include <sys/wait.h>
+ #include <sys/types.h>
 @@ -30,6 +30,7 @@
  
  #include "tcpsocket.h"
