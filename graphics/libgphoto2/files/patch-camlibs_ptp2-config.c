0a5b325d944558d5fc1f0dca49198ea231512171
miwi <miwi@FreeBSD.org>
2014-01-17 16:55:22 +0000

    - Update to 2.5.3
    PR:		ports/181073
    Submitted by:	Ports Fury

--- graphics/libgphoto2/files/patch-camlibs_ptp2-config.c
+++ graphics/libgphoto2/files/patch-camlibs_ptp2-config.c
@@ -1,11 +1,10 @@
 --- camlibs/ptp2/config.c.orig	Tue Dec 12 18:43:53 2006
 +++ camlibs/ptp2/config.c	Tue Dec 12 19:00:45 2006
-@@ -19,6 +19,8 @@
-  */
- #include "config.h"
- 
-+#include <sys/types.h>
-+#include <netinet/in.h>
- #include <stdlib.h>
+@@ -24,6 +24,7 @@
  #include <string.h>
  #include <stdio.h>
+ #include <time.h>
++#include <sys/types.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
