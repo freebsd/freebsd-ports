81d0f2b19b64e912890ac54ad10217feb0c8d317
Martin Wilke <miwi@FreeBSD.org>
2014-01-19 14:28:36 +0000

    - Fix build with clang
    PR:		185696
    Submitted by:	ports fury

---
+++ textproc/sdcv/files/patch-src__utils.cpp
@@ -0,0 +1,11 @@
+--- src/utils.cpp.orig
++++ src/utils.cpp
+@@ -22,6 +22,8 @@
+ #  include "config.h"
+ #endif
+ 
++#include <cstdio>
++#include <cstdlib>
+ #include <glib.h>
+ #include <glib/gi18n.h>
+ 
