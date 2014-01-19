1a3b876706daa9b5f480ebcf818fed8cc5fb2d0d
miwi <miwi@FreeBSD.org>
2014-01-19 14:25:38 +0000

    - Fix build with clang
    PR:		185708
    Submitted by:	ports fury

---
+++ x11-toolkits/viewklass/files/patch-VkProgram.C
@@ -0,0 +1,11 @@
+--- VkProgram.C.orig
++++ VkProgram.C
+@@ -34,6 +34,8 @@
+ #endif
+ = "$Id: VkProgram.C,v 1.8 2009/03/21 11:44:34 jostle Exp $";
+ 
++#include <cstdlib>
++
+ #include <Vk/VkProgram.h>
+ 
+ using namespace std;
