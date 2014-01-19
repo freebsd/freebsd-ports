d44adee0d55638f442d92a399abf15447728f89d
Martin Wilke <miwi@FreeBSD.org>
2014-01-19 13:46:19 +0000

    - Fix build with clang
    PR:		185318
    Submitted by:	ports fury

---
+++ graphics/ipe/files/patch-ipeextract__ipeextract.cpp
@@ -0,0 +1,10 @@
+--- ipeextract/ipeextract.cpp.orig
++++ ipeextract/ipeextract.cpp
+@@ -31,6 +31,7 @@
+ #include "ipexml.h"
+ #include "ipeutils.h"
+ #include "ipepdfparser.h"
++#include <cstdlib>
+ 
+ using namespace ipe;
+ 
