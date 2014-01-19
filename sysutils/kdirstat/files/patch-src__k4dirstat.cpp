9f88b77e844a82b948d34bc2d93dc128d648f03d
miwi <miwi@FreeBSD.org>
2014-01-19 13:51:13 +0000

    - Stage support - Fix build with clang
    PR:		185333
    Submitted by:	ports fury

---
+++ sysutils/kdirstat/files/patch-src__k4dirstat.cpp
@@ -0,0 +1,10 @@
+--- src/k4dirstat.cpp.orig
++++ src/k4dirstat.cpp
+@@ -14,6 +14,7 @@
+ //#include "k4dirstatview.h"
+ #include "settings.h"
+ 
++#include <unistd.h>
+ #include <QtGui/QDropEvent>
+ #include <QtGui/QPainter>
+ #include <QtGui/QPrinter>\
