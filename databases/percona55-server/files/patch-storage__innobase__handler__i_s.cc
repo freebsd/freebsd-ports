0842412af8b18c0a00c731df28068f320daebddc
flo <flo@FreeBSD.org>
2014-01-19 20:21:00 +0000

    - Update to 5.5.35-33.0 - Pet portlint
    PR:		ports/185889
    Submitted by:	Jim Ohlstein <jim@selenium.jlkhosting.com>

--- databases/percona55-server/files/patch-storage__innobase__handler__i_s.cc
+++ databases/percona55-server/files/patch-storage__innobase__handler__i_s.cc
@@ -1,20 +1,23 @@
---- storage/innobase/handler/i_s.cc.orig	2013-08-27 16:58:46.000000000 +0200
-+++ storage/innobase/handler/i_s.cc	2013-09-30 19:31:59.983048770 +0200
-@@ -48,14 +48,18 @@
- #include "btr0types.h"
+--- storage/innobase/handler/i_s.cc.orig	2013-12-02 01:43:24.000000000 -0500
++++ storage/innobase/handler/i_s.cc	2014-01-19 12:21:24.113243651 -0500
+@@ -49,17 +49,21 @@
  #include "buf0buddy.h"
  #include "buf0buf.h"
-+#include "buf0lru.h"
+ #include "buf0lru.h"
 +#include "dict0load.h"
++#include "buf0lru.h"
  #include "ibuf0ibuf.h"
  #include "dict0mem.h"
  #include "dict0types.h"
  #include "dict0boot.h"
+ #include "dict0load.h"
  #include "ha_prototypes.h"
 +#include "srv0srv.h"
  #include "srv0start.h"
+ #include "srv0srv.h"
  #include "trx0i_s.h"
  #include "trx0rseg.h"
+ #include "trx0trx.h"
 +#include "trx0trx.h"
  #include "trx0undo.h"
  #include "log0online.h"
