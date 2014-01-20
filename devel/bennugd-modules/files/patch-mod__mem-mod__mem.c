f98dbe9543a4c4ee7d9682771d1613329d6d3ff0
Dmitry Marakasov <amdmi3@FreeBSD.org>
2014-01-20 01:34:53 +0000

    - Update to svn20130912 - Support staging - Fix build on 10.x

--- devel/bennugd-modules/files/patch-mod__mem-mod__mem.c
+++ devel/bennugd-modules/files/patch-mod__mem-mod__mem.c
@@ -1,20 +1,20 @@
---- mod_mem/mod_mem.c.orig	2009-06-15 22:34:24.000000000 +0400
-+++ mod_mem/mod_mem.c	2009-11-06 06:28:55.000000000 +0300
-@@ -128,7 +128,7 @@
+--- mod_mem/mod_mem.c.orig	2013-09-12 06:03:12.588226648 +0400
++++ mod_mem/mod_mem.c	2013-09-12 06:03:50.452231577 +0400
+@@ -133,7 +133,7 @@
      get_system_info( &info );
      return B_PAGE_SIZE * ( info.max_pages - info.used_pages );
  
--#elif !defined(TARGET_MAC)
-+#elif !defined(TARGET_MAC) && !defined(TARGET_BSD)
+-#elif !defined(TARGET_MAC) && !defined(TARGET_WII)
++#elif !defined(TARGET_MAC) && !defined(TARGET_WII) && !defined(TARGET_BSD)
      /* Linux and other Unix (?) */
      struct sysinfo meminf;
      int fv;
-@@ -166,7 +166,7 @@
+@@ -171,7 +171,7 @@
      get_system_info( &info );
      return  B_PAGE_SIZE * ( info.max_pages );
  
--#elif !defined(TARGET_MAC)
-+#elif !defined(TARGET_MAC) && !defined(TARGET_BSD)
+-#elif !defined(TARGET_MAC) && !defined(TARGET_WII)
++#elif !defined(TARGET_MAC) && !defined(TARGET_WII) && !defined(TARGET_BSD)
      /* Linux and other Unix (?) */
      struct sysinfo meminf;
      int fv;
