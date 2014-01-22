8a93d5f3e6f70cc4fa302cace3aef043f918e286
marino <marino@FreeBSD.org>
2014-01-22 11:20:17 +0000

    databases/tablelog: Fix pgsql 9.2+ and stage support
    PR: ports/179955

---
+++ databases/tablelog/files/patch-table__log.c
@@ -0,0 +1,11 @@
+--- table_log.c.orig	2007-05-16 23:14:04.000000000 +0000
++++ table_log.c
+@@ -22,6 +22,8 @@
+ #include "utils/formatting.h"
+ #include "utils/builtins.h"
+ #include <utils/lsyscache.h>
++#include <utils/rel.h>
++#include <utils/timestamp.h>
+ #include <funcapi.h>
+ 
+ /* for PostgreSQL >= 8.2.x */
