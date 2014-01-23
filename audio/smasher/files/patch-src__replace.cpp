f1e1beb54bc25d46982f277d2a13c24d9f878136
danilo <danilo@FreeBSD.org>
2014-01-23 23:13:39 +0000

    - Add stage support - Fix build - Add USE_GCC. clang fails with a lot of c++ erros - Bump PORTREVISION

---
+++ audio/smasher/files/patch-src__replace.cpp
@@ -0,0 +1,8 @@
+--- ./src/replace.cpp.orig	2014-01-20 16:50:27.000000000 -0200
++++ ./src/replace.cpp	2014-01-20 16:50:38.000000000 -0200
+@@ -1,4 +1,5 @@
+ #include <cstring>
++#include <cstdio>
+ #include "replace.h"
+ 
+ int Replace::run (
