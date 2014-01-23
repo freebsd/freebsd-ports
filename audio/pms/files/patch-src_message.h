df01cf7e197b091106363f0f94c86d36d275b2e0
Thomas Zander <riggs@FreeBSD.org>
2014-01-23 20:13:09 +0000

    MFH: r340845
    (Note: Log in original commit r340845 was incomplete.
     Complete log is provided below.)
    
    - Fix build failure on FreeBSD 10
    - Add stage support
    - Add license
    - Change whitespaces in pkg-descr due to portlint warning
    
    Approved by:	portmgr (mat), maintainer (ashish), mentor (thierry)

---
+++ audio/pms/files/patch-src_message.h
@@ -0,0 +1,10 @@
+--- src/message.h.orig	2010-01-02 21:53:36.000000000 +0100
++++ src/message.h	2014-01-19 16:59:54.616726275 +0100
+@@ -25,6 +25,7 @@
+ #define _PMS_MESSAGE_H_
+ 
+ #include <string>
++#include <ctime>
+ 
+ using namespace std;
+ 
