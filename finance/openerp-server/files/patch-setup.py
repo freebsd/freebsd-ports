7bc1438648cab43446bc0cff10b4a5f88ea423d8
marino <marino@FreeBSD.org>
2014-01-24 22:12:02 +0000

    finance/openerp: Update version from 6.0.3 to 7.0 (LTS)
    Port maintenance assigned to Pakhom Golynga.
    
    PR:		ports/185844
    Submitted by:	Pakhom Golynga
    Modified by:	marino@

--- finance/openerp-server/files/patch-setup.py
+++ finance/openerp-server/files/patch-setup.py
@@ -1,29 +1,10 @@
---- setup.py	2011-08-21 16:34:59.526991138 +0200
-+++ setup.diffs	2011-08-21 16:35:49.515560426 +0200
-@@ -145,16 +145,16 @@
-         #    files.append((root, [join(root, name) for name in names]))
-         files.append(('.', [join('bin', 'import_xml.rng'),]))
-     else:
--        man_directory = join('share', 'man')
--        files.append((join(man_directory, 'man1'), ['man/openerp-server.1']))
--        files.append((join(man_directory, 'man5'), ['man/openerp_serverrc.5']))
--
--        doc_directory = join('share', 'doc', 'openerp-server-%s' % version)
--        files.append((doc_directory, filter(isfile, glob.glob('doc/*'))))
--        files.append((join(doc_directory, 'migrate', '3.3.0-3.4.0'),
--                      filter(isfile, glob.glob('doc/migrate/3.3.0-3.4.0/*'))))
--        files.append((join(doc_directory, 'migrate', '3.4.0-4.0.0'),
--                      filter(isfile, glob.glob('doc/migrate/3.4.0-4.0.0/*'))))
-+#        man_directory = join('share', 'man')
-+#        files.append((join(man_directory, 'man1'), ['man/openerp-server.1']))
-+#        files.append((join(man_directory, 'man5'), ['man/openerp_serverrc.5']))
-+#
-+#        doc_directory = join('share', 'doc', 'openerp-server-%s' % version)
-+#        files.append((doc_directory, filter(isfile, glob.glob('doc/*'))))
-+#        files.append((join(doc_directory, 'migrate', '3.3.0-3.4.0'),
-+#                      filter(isfile, glob.glob('doc/migrate/3.3.0-3.4.0/*'))))
-+#        files.append((join(doc_directory, 'migrate', '3.4.0-4.0.0'),
-+#                      filter(isfile, glob.glob('doc/migrate/3.4.0-4.0.0/*'))))
- 
-         openerp_site_packages = join(get_python_lib(prefix=''), 'openerp-server')
- 
+--- setup.py.orig	2013-05-15 03:10:46.000000000 +0400
++++ setup.py	2014-01-15 10:13:59.067058613 +0400
+@@ -128,7 +128,6 @@
+           'psycopg2 >= 2.2',
+           'pydot',
+           'python-dateutil < 2',
+-          'python-ldap', # optional
+           'python-openid',
+           'pytz',
+           'pywebdav',
