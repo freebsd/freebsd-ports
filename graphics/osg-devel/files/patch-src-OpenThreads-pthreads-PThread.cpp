26696920eb22251fd001ead2591a9f7d8de14d28
Dmitry Marakasov <amdmi3@FreeBSD.org>
2014-01-24 01:22:59 +0000

    - Update to 3.3.0
    Based on r340402 (graphics/osg version 3.2.0), additional changes include
    
    - Add new V8, LUA and PYTHON options
    
    PR:		185228
    Submitted by:	Hardy Schumacher <hardy.schumacher@gmx.de>

--- graphics/osg-devel/files/patch-src-OpenThreads-pthreads-PThread.cpp
+++ graphics/osg-devel/files/patch-src-OpenThreads-pthreads-PThread.cpp
@@ -1,5 +1,5 @@
---- src/OpenThreads/pthreads/PThread.cpp.orig	2009-11-20 13:27:43.000000000 +0300
-+++ src/OpenThreads/pthreads/PThread.cpp	2009-12-15 20:26:51.000000000 +0300
+--- src/OpenThreads/pthreads/PThread.cpp.orig	2011-06-24 00:09:26.000000000 +0400
++++ src/OpenThreads/pthreads/PThread.cpp	2012-03-05 05:57:02.886704932 +0400
 @@ -21,6 +21,7 @@
  #include <sys/types.h>
  #include <unistd.h>
@@ -7,8 +7,8 @@
 +#include <pthread_np.h>
  #include <limits.h>
  
- #if defined __linux || defined __sun || defined __APPLE__
-@@ -123,7 +124,7 @@
+ #if defined __linux || defined __sun || defined __APPLE__ || ANDROID
+@@ -136,7 +137,7 @@
  #if defined(__sgi)
              pthread_setrunon_np( pd->cpunum );
  #elif defined(HAVE_PTHREAD_SETAFFINITY_NP) || defined(HAVE_THREE_PARAM_SCHED_SETAFFINITY) || defined(HAVE_TWO_PARAM_SCHED_SETAFFINITY)
@@ -17,7 +17,7 @@
              CPU_ZERO( &cpumask );
              CPU_SET( pd->cpunum, &cpumask );
  
-@@ -539,7 +540,7 @@
+@@ -569,7 +570,7 @@
  
      if (pd->isRunning && Thread::CurrentThread()==this)
      {
@@ -26,7 +26,7 @@
          CPU_ZERO( &cpumask );
          CPU_SET( pd->cpunum, &cpumask );
  #if defined(HAVE_PTHREAD_SETAFFINITY_NP)
-@@ -969,7 +970,7 @@
+@@ -1031,7 +1032,7 @@
      else
      {
  #if defined(HAVE_PTHREAD_SETAFFINITY_NP) || defined(HAVE_THREE_PARAM_SCHED_SETAFFINITY) || defined(HAVE_TWO_PARAM_SCHED_SETAFFINITY)
@@ -34,4 +34,4 @@
 +        cpuset_t cpumask;
          CPU_ZERO( &cpumask );
          CPU_SET( cpunum, &cpumask );
- 
+ #if defined(HAVE_PTHREAD_SETAFFINITY_NP)
