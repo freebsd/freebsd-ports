8d6009c5c3a31e126d5b19fab7cdfd014f466e12
Martin Wilke <miwi@FreeBSD.org>
2014-01-25 12:59:35 +0000

    MFH: r340257
    - Fix build with clang
    
    PR:		185506
    Submitted by:	ports fury

--- net/netmap/files/patch-belgolib::files.c
+++ net/netmap/files/patch-belgolib::files.c
@@ -1,14 +1,16 @@
 --- belgolib/files.c.orig	2010-01-06 08:00:52.000000000 +0900
 +++ belgolib/files.c	2011-11-23 22:22:36.000000000 +0900
-@@ -17,7 +17,7 @@
+@@ -17,8 +17,8 @@
  
  Infile::Infile() : ifstream() { }
  
 -Infile::Infile(const string& file_name, int mode, bool fatal)
+-    : ifstream(file_name.c_str(), (std::_Ios_Openmode)mode)
 +Infile::Infile(const string& file_name, ios::openmode mode, bool fatal)
-     : ifstream(file_name.c_str(), (std::_Ios_Openmode)mode)
++    : ifstream(file_name.c_str(), (std::ios_base::openmode)mode)
  {
  
+     if(cdb>d_list)
 @@ -39,7 +39,7 @@
  }
  
@@ -18,3 +20,12 @@
  {
      
      if(cdb>d_list)
+@@ -49,7 +49,7 @@
+ 	//file is open, close it first
+ 	close();
+ 
+-    open(file_name.c_str(), (std::_Ios_Openmode)mode);
++    open(file_name.c_str(), (std::ios_base::openmode)mode);
+ 
+     if(int error_nr = check_open())  { 
+ 	if(cdb>d_list)
--- net/netmap/files/patch-netmap::lookup.c
+++ net/netmap/files/patch-netmap::lookup.c
@@ -1,7 +1,7 @@
 --- netmap/lookup.c.orig	2010-01-06 07:59:38.000000000 +0900
 +++ netmap/lookup.c	2011-11-23 22:32:15.000000000 +0900
-@@ -1,6 +1,8 @@
- #include <strstream>
+@@ -1,6 +1,7 @@
+-#include <strstream>
  #include <iostream>
  
 +#include <sys/types.h>
