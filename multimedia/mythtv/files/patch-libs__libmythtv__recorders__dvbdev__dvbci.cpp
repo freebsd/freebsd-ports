a0bbf092a74f38dcc6803ff132515545d70f04c5
Bernhard Froehlich <decke@FreeBSD.org>
2014-01-25 19:44:21 +0000

    - Update to latest 0.27 stable branch from github - Add patches to build with clang [1] - Use OPTIONS_SUB
    PR:		ports/186005 [1]
    Submitted by:	John Clark <clarkjc@runbox.com> [1]

--- multimedia/mythtv/files/patch-libs__libmythtv__recorders__dvbdev__dvbci.cpp
+++ multimedia/mythtv/files/patch-libs__libmythtv__recorders__dvbdev__dvbci.cpp
@@ -1,11 +1,13 @@
---- libs/libmythtv/recorders/dvbdev/dvbci.cpp.orig	2010-03-07 04:11:52.000000000 +0100
-+++ libs/libmythtv/recorders/dvbdev/dvbci.cpp	2010-04-26 23:30:52.000000000 +0200
-@@ -28,7 +28,9 @@
+--- libs/libmythtv/recorders/dvbdev/dvbci.cpp.orig	2010-04-26 23:30:52.000000000 +0200
++++ libs/libmythtv/recorders/dvbdev/dvbci.cpp	2014-01-22 09:13:22.000000000 -0500
+@@ -28,7 +28,11 @@
  #include <errno.h>
  #include <ctype.h>
  #include <linux/dvb/ca.h>
 +#ifndef __FreeBSD__
  #include <malloc.h>
++#else
++#include <stdlib.h>
 +#endif
  #include <netinet/in.h>
  #include <poll.h>
