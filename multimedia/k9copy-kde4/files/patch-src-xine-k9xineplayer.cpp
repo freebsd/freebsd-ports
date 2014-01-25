2d32eff9fa8f3574f712b14d73392390965ae901
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2014-01-25 22:41:42 +0000

    - Fix the build with clang and 10+'s ld(1).   On the clang side, there's a non-void function missing a return statement   and some wrong QString-to-char* casts that had to be fixed.   On the linker side, r253839 made our ld stricter, so we had to add some   shared libraries that we use since they are not being pulled in indirectly   anymore.   Based on a patch by Jan Henrik Sylvester <me@janh.de> [1]. - Set LICENSE - Support staging. - Remove LATEST_LINK, it is deprecated. - Use the new OPTIONS helpers for conditional RUN_DEPENDS.
    PR:		ports/185032 [1]
    Submitted by:	Jan Henrik Sylvester <me@janh.de> [1]
    Approved by:	maintainer timeout (37 days)
    MFH:		2014Q1

--- multimedia/k9copy-kde4/files/patch-src-xine-k9xineplayer.cpp
+++ multimedia/k9copy-kde4/files/patch-src-xine-k9xineplayer.cpp
@@ -1,6 +1,15 @@
---- src/xine/k9xineplayer.cpp.orig
-+++ src/xine/k9xineplayer.cpp
-@@ -260,8 +260,10 @@ void k9xinePlayer::init(WId _wid) {
+--- src/xine/k9xineplayer.cpp.orig	2009-12-06 10:13:37.000000000 +0000
++++ src/xine/k9xineplayer.cpp	2013-12-06 15:34:35.000000000 +0000
+@@ -251,7 +251,7 @@
+ 
+     if ((vo_port = xine_open_video_driver(xine,
+                                           m_vo.toAscii(), XINE_VISUAL_TYPE_X11, (void *) &vis)) == NULL) {
+-        printf("I'm unable to initialize '%s' video driver. Giving up.\n", m_vo.toAscii());
++        printf("I'm unable to initialize '%s' video driver. Giving up.\n", m_vo.toAscii().constData());
+         return ;
+     }
+ 
+@@ -260,8 +260,10 @@
      event_queue = xine_event_new_queue(stream);
      xine_event_create_listener_thread(event_queue, event_listener, this);
  
@@ -13,7 +22,16 @@
      m_execute=true;
  
  }
-@@ -284,7 +286,8 @@ void k9xinePlayer::quit() {
+@@ -270,7 +272,7 @@
+     running=false;
+     m_title="";
+     if ((!xine_open(stream, m_mrl.toUtf8())) || (!xine_play(stream, 0, 0))) {
+-        printf("Unable to open mrl '%s'\n", m_mrl.toUtf8());
++        printf("Unable to open mrl '%s'\n", m_mrl.toUtf8().constData());
+         return ;
+     }
+     QTimer::singleShot (500, this, SLOT (updatePosition ()));
+@@ -284,7 +286,8 @@
  void k9xinePlayer::update() {
      if (stream && !running ) {
          if (m_mutex.tryLock()) {
