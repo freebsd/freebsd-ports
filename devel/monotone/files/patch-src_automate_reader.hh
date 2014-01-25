3ea81b93310378aa28be58a650aa4bda5dc62b87
Guido Falsi <madpilot@FreeBSD.org>
2014-01-25 18:06:08 +0000

    - Fix build on 10 and head with clang - Convert to staging - Improve library detection patch with a cleaner one - Mark jobs safe
    PR:		ports/186060
    Submitted by:	Lapo Luchini <lapo@lapo.it> (maintainer)

---
+++ devel/monotone/files/patch-src_automate_reader.hh
@@ -0,0 +1,10 @@
+--- src/automate_reader.hh.orig	2014-01-22 01:04:27.675051506 +0100
++++ src/automate_reader.hh	2014-01-22 01:04:41.682040341 +0100
+@@ -13,6 +13,7 @@
+ #define __AUTOMATE_READER_HH__
+ 
+ #include <vector>
++#include <iostream>
+ 
+ class automate_reader
+ {
