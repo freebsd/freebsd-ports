5dc8c9c9010d9fedf93390ae164074c001f73fce
koobs <koobs@FreeBSD.org>
2014-01-28 09:33:51 +0000

    devel/buildbot-slave: Modernise and enable STAGE support
    - Enable STAGE support
    - Enable AUTOPLIST support (Delete pkg-plist accordingly)
    - Deprecate easy_install target
    - Remove OPTIONSFILE override
    - Switch MASTER_SITES to CHEESESHOP (Remove PROJECTHOST accordingly)
    - Patch out empty directory in setup.py:data_files
    - Re-order USE[S] sections
    - Tweak COMMENT
    
    Requested by:	bapt

--- devel/buildbot-slave/files/patch-setup.py
+++ devel/buildbot-slave/files/patch-setup.py
@@ -1,15 +1,12 @@
---- ./setup.py.orig	2012-09-23 00:57:03.000000000 +1000
-+++ ./setup.py	2012-12-07 22:19:57.724590221 +1100
-@@ -105,11 +105,7 @@
-         }
-     }
- 
--# set zip_safe to false to force Windows installs to always unpack eggs
--# into directories, which seems to work better --
--# see http://buildbot.net/trac/ticket/907
--if sys.platform == "win32":
--    setup_args['zip_safe'] = False
-+setup_args['zip_safe'] = False
- 
- try:
-     # If setuptools is installed, then we'll add setuptools-specific arguments
+--- ./setup.py.orig	2014-01-28 19:18:29.885753132 +1100
++++ ./setup.py	2014-01-28 19:18:53.182782954 +1100
+@@ -96,9 +96,6 @@
+         "buildslave.test.unit",
+     ],
+     'scripts': scripts,
+-    # mention data_files, even if empty, so install_data is called and
+-    # VERSION gets copied
+-    'data_files': [("buildslave", [])],
+     'cmdclass': {
+         'install_data': our_install_data,
+         'sdist': our_sdist
