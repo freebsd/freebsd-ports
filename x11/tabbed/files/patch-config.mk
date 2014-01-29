34d6a2e4409c7d4236ee76ac40b410fe9332c43e
pawel <pawel@FreeBSD.org>
2014-01-29 21:30:31 +0000

    - Update to version 0.6 [1] - Support staging [1] - Respect CFLAGS
    PR:		ports/185903 [1]
    Submitted by:	maintainer

--- x11/tabbed/files/patch-config.mk
+++ x11/tabbed/files/patch-config.mk
@@ -1,25 +1,39 @@
---- config.mk.orig	2010-09-03 23:40:32.000000000 -0500
-+++ config.mk	2010-09-03 23:42:18.000000000 -0500
-@@ -4,12 +4,12 @@
- # Customize below to fit your system
- 
- # paths
--PREFIX = /usr/local
--MANPREFIX = ${PREFIX}/share/man
-+PREFIX?= /usr/local
-+MANPREFIX = ${PREFIX}/man
- 
- # includes and libs
--INCS = -I. -I/usr/include
--LIBS = -L/usr/lib -lc -lX11
-+INCS = -I. -I/usr/include -I/usr/local/include
-+LIBS = -L/usr/lib -L/usr/local/lib -lc -lX11
- 
- # flags
- CPPFLAGS = -DVERSION=\"${VERSION}\"
-@@ -21,4 +21,4 @@
- #LDFLAGS = ${LIBS}
- 
- # compiler and linker
--CC = cc
-+CC?= cc
+*** config.mk.orig	2014-01-19 21:53:24.000000000 +0400
+--- config.mk	2014-01-19 22:40:13.000000000 +0400
+***************
+*** 5,15 ****
+  
+  # paths
+  PREFIX = /usr/local
+! MANPREFIX = ${PREFIX}/share/man
+  
+  # includes and libs
+! INCS = -I. -I/usr/include
+! LIBS = -L/usr/lib -lc -lX11
+  
+  # flags
+  CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE
+--- 5,15 ----
+  
+  # paths
+  PREFIX = /usr/local
+! MANPREFIX = ${PREFIX}/man
+  
+  # includes and libs
+! INCS = -I. -I/usr/local/include
+! LIBS = -L/usr/local/lib -lc -lX11
+  
+  # flags
+  CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE
+***************
+*** 21,25 ****
+  #LDFLAGS = ${LIBS}
+  
+  # compiler and linker
+! CC = cc
+  
+--- 21,25 ----
+  #LDFLAGS = ${LIBS}
+  
+  # compiler and linker
+! CC?= cc
