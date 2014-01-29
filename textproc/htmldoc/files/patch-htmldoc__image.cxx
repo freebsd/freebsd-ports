9365b9a3fb4a129d91b1653cc561846e414de8c5
Mathieu Arnold <mat@FreeBSD.org>
2014-01-29 20:35:48 +0000

    MFH: r341774
    Upgrade to HTMLDOC 1.8.28, fixing security-relevant bugs.
    Support staging, and modernize LIB_DEPENDS.
    Tweak build environment to get proper detection of png/jpeg libs.
    Switch to using BZIP2 tarballs.
    
    Sponsored by:	Absolight

---
+++ textproc/htmldoc/files/patch-htmldoc__image.cxx
@@ -0,0 +1,10 @@
+--- ./htmldoc/image.cxx.orig	2013-08-09 04:53:39.000000000 +0200
++++ ./htmldoc/image.cxx	2014-01-28 04:47:41.000000000 +0100
+@@ -44,6 +44,7 @@
+ }
+ 
+ #include <png.h>	/* Portable Network Graphics (PNG) definitions */
++#include <pngpriv.h>
+ 
+ 
+ /*
