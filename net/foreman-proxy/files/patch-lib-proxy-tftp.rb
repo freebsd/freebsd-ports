1b7d97b18415edf3a426d3c58af6f708faa94e91
Martin Matuska <mm@FreeBSD.org>
2014-01-29 11:46:59 +0000

    The Foreman Smart Proxy is a project which provides a restful API to various sub-systems.
    Its goal is to provide API for a higher level orchestration tools (such as
    Foreman). The Smart proxy provides an easy way to add or extended existing
    subsystems and API's.
    
    Currently supported are:
    DHCP - ISC DHCP and MS DHCP Servers
    DNS - Bind and MS DNS Servers
    TFTP - any UNIX based tftp server
    Puppet - Any Puppet server from 0.24.x
    Puppet CA - Manage certificate signing, cleaning and autosign on a Puppet CA
                server
    BMC - BMC management etc
    
    WWW: http://theforeman.org

---
+++ net/foreman-proxy/files/patch-lib-proxy-tftp.rb
@@ -0,0 +1,11 @@
+--- lib/proxy/tftp.rb.orig	2014-01-29 00:26:35.629061321 +0100
++++ lib/proxy/tftp.rb	2014-01-29 00:28:05.669054835 +0100
+@@ -104,7 +104,7 @@
+       # as the dst might contain another sub directory
+       FileUtils.mkdir_p destination.parent
+ 
+-      cmd = "wget --timeout=10 --tries=3 --no-check-certificate -nv -c #{src} -O \"#{destination}\""
++      cmd = "/usr/bin/fetch --timeout=10 -a --no-verify-hostname --no-verify-peer -a -m -o \"#{destination}\" #{src}"
+       Proxy::Util::CommandTask.new(cmd)
+     end
+   end
