1b9b990e8331ba961c53bb42bbbf877213f99152
Guido Falsi <madpilot@FreeBSD.org>
2014-01-30 22:03:16 +0000

    - Update to 1.21.0
    PR:		ports/186267
    Submitted by:	Me
    Approved by:	Rusty Nejdl <rnejdl@ringofsaturn.com> (maintainer)

--- deskutils/calibre/files/patch-calibre_constants.py
+++ deskutils/calibre/files/patch-calibre_constants.py
@@ -1,10 +1,10 @@
---- src/calibre/constants.py.orig	2013-11-22 09:46:59.000000000 -0600
-+++ src/calibre/constants.py	2013-11-22 09:47:07.000000000 -0600
+--- src/calibre/constants.py.orig	2014-01-24 05:56:21.000000000 +0100
++++ src/calibre/constants.py	2014-01-29 00:21:48.318819375 +0100
 @@ -138,7 +138,6 @@
                  'freetype',
                  'woff',
                  'unrar',
 -                'qt_hack',
-                 '_regex'
-             ]
-         if iswindows:
+                 '_regex',
+                 'hunspell',
+                 '_patiencediff_c',
