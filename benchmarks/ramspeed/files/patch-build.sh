5cee45ab75bd61e119c7b36c8edb6880f97bbce4
pawel <pawel@FreeBSD.org>
2014-01-31 13:40:21 +0000

    RAMspeed is a command line utility to measure cache and memory performance of computer systems. It offers 18 cache and memory benchmarks for i386 and amd64 machines, though 6 only for alpha ones. There are *mark benchmarks such as INTmark, FLOATmark, MMXmark and SSEmark. They operate with linear (sequential) data streams passed through ALU, FPU, MMX and SSE units respectively. There are also *mem benchmarks such as INTmem, FLOATmem, MMXmem and SSEmem. These are supposed to illustrate how fast is actual read/write memory performance. There are also non-temporal versions of MMX and SSE benchmarks. They have been coded with special instructions to minimise cache pollution on memory reads and to eliminate it completely on memory writes. In addition, they operate with a built in aggressive data prefetching algorithm. In some cases, non-temporal MMXmark and SSEmark can deliver almost 100% of theoretical bandwidth while reading.
    WWW: http://alasir.com/software/ramspeed/
    
    PR:		ports/186108
    Submitted by:	Martin Kammerhofer <mkamm@gmx.net>

---
+++ benchmarks/ramspeed/files/patch-build.sh
@@ -0,0 +1,56 @@
+--- build.sh.orig	2007-11-01 22:00:00.000000000 +0100
++++ build.sh	2014-01-25 19:53:28.000000000 +0100
+@@ -15,6 +15,16 @@
+ if [ "$1" ]; then OS=$1; fi
+ if [ "$2" ]; then ARCH=$2; fi
+ 
++# respect the user's options patch here
++echo " "
++echo "=== this is RAMspeed's build script ===================================="
++echo " "
++if [ -n "$CC" -a \( -n "$LD" -o -n "$AS" -o -n "$CFLAGS" -o -n "$LDFLAGS" \) ]
++then
++    _CC=$CC;_LD=$LD;_AS=$AS;_CFLAGS=$CFLAGS;_LDFLAGS=$LDFLAGS;_respect=yes
++fi
++# end of patch
++
+ case $OS in
+ 
+     FreeBSD)
+@@ -127,10 +137,20 @@
+ esac
+ 
+ echo "building for $OS $RELEASE $ARCH"
+-echo "compiler is $CC, linker is $LD, assembler is $AS"
+-echo "compiler's flags are $CFLAGS"
+-echo "linker's flags are $LFLAGS"
++echo "compiler is '$CC', linker is '$LD', assembler is '$AS'"
++echo "compiler's flags are '$CFLAGS'"
++echo "linker's flags are '$LFLAGS'"
+ echo " "
++# respect the user's options patch here
++if [ X"$_respect" = Xyes ] ; then
++    CC=$_CC;LD=$_CC;AS=${_AS:-$AS};CFLAGS=$_CFLAGS;LFLAGS=$_LDFLAGS
++    echo "OVERRIDING build script defaults with your system's defaults:"
++    echo "compiler is '$CC', linker is '$LD', assembler is '$AS'"
++    echo "compiler's flags are '$CFLAGS'"
++    echo "linker's flags are '$LFLAGS'"
++    echo " "
++fi
++# end of patch
+ 
+ if [ $TARGET = "GENERIC" ]
+ then echo "WARNING! BUILDING FOR AN UNSUPPORTED OPERATING SYSTEM AND\OR ARCHITECTURE!"
+@@ -139,8 +159,10 @@
+      echo " "
+ fi
+ 
+-echo "press Enter to continue or Control-C to abort"
+-read ANS
++if [ X"$_respect" != Xyes -a -t 0 -a -t 1 ] ; then
++    echo "press Enter to continue or Control-C to abort"
++    read ANS
++fi
+ 
+ case $TARGET in
+ 
