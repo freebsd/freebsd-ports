7c7a3ee4b8812b58a43f576449393ea0b28fe72c
Ryan Steinmetz <zi@FreeBSD.org>
2014-01-31 23:02:59 +0000

    - Resolve RDoc generation problems
    PR:		ports/186313
    Submitted by:	zi@
    Approved by:	bdrewery@ (portmgr)
    With hat:	ruby@

--- Mk/bsd.ruby.mk
+++ Mk/bsd.ruby.mk
@@ -186,7 +186,7 @@ RUBY_DISTVERSION?=	${RUBY_RELVERSION}-p${RUBY_PATCHLEVEL}
 
 RUBY_WRKSRC=		${WRKDIR}/ruby-${RUBY_DISTVERSION}
 
-GEM_ENV?=		LC_CTYPE=UTF-8
+GEM_ENV?=		LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 LC_CTYPE=UTF-8
 
 RUBY_CONFIGURE_ARGS+=	--with-rubyhdrdir="${PREFIX}/include/ruby-1.9/" \
 			--with-rubylibprefix="${PREFIX}/lib/ruby" \
