92500c08eb8d25a5ff56cef96f9a6f28df306408
Matthias Andree <mandree@FreeBSD.org>
2014-02-07 01:01:37 +0000

    Unbreak and upgrade to new upstream release 14.02.2. Based on a patch to upgrade to 14.01.1 by Rainer Hurling, based on a patch to upgrade to 13.03.1 by Rod Person.
    Rainer's changes:
    - new mastersite; 2nd mastersites contents has to be updated
    - unbreak the port
    - modernize LIB_DEPENDS
    - support STAGE_DIR
    - strip bin/fotoxx
    - correct usage of desktop-file-utils
    - update URL in pkg-descr
    - update pkg-plist
    
    Committer's changes:
    - work around fetch(1)/nginx incompatibility making fetch commands with
      unmerged fix r261263 (and ancestors) unable to fetch from the primary
      site.
    - add USES=pkgconfig
    - add mirror sites
    - fix installation of manfile
    - fix paths in desktop file
    - stop using xdg-desktop-menu for now, breaks the build and does not
      appear to be required
    
    PR:		ports/177643
    Submitted by:	Rod Person, Rainer Hurling

--- graphics/fotoxx/files/patch-zfuncs.h
+++ graphics/fotoxx/files/patch-zfuncs.h
@@ -1,35 +1,37 @@
---- zfuncs.h.orig	2011-02-28 16:04:24.000000000 +0000
-+++ zfuncs.h	2013-03-30 08:58:28.000000000 +0000
-@@ -36,7 +36,13 @@
- #include <signal.h>
- #include <execinfo.h>
- #include <locale.h>
--#include <sys/inotify.h>
-+
-+#ifdef __linux__
-+   #include <sys/inotify.h>
-+#elif defined(__FreeBSD__)
-+   #include <sys/event.h>
-+   #include <sys/types.h>
-+#endif
+--- ./zfuncs.h.orig	2014-02-05 20:42:34.000000000 +0100
++++ ./zfuncs.h	2014-02-06 10:32:37.000000000 +0100
+@@ -22,7 +22,7 @@
  
- #define  int8   char                                                       //  number types
- #define  int16  short
-@@ -52,7 +58,7 @@
+ //     zfuncs.h   version  v.5.8
+ 
+-#include <sys/sysinfo.h> 
++//#include <sys/sysinfo.h> 
+ #include <sys/time.h>
+ #include <sys/stat.h>
+ #include <sys/resource.h>
+@@ -33,6 +33,7 @@
+ #include <unistd.h>
+ #include <stdlib.h>
+ #include <stdio.h>
++#include <sys/wait.h>
+ #include <string.h>
+ #include <ctype.h>
+ #include <math.h>
+@@ -60,7 +61,7 @@
  
  #define  wstrerror(err) strerror(WEXITSTATUS(err))                         //  get text status for child process
  
 -#define  mutex          pthread_mutex_t                                    //  abbreviations
-+#define  mutex_tp          pthread_mutex_t                                    //  abbreviations
++#define  mutex_tp       pthread_mutex_t                                    //  abbreviations
  #define  mutex_init     pthread_mutex_init
  #define  mutex_lock     pthread_mutex_lock
  #define  mutex_trylock  pthread_mutex_trylock
-@@ -504,7 +510,7 @@
+@@ -625,7 +626,7 @@
  {
     char        wmi[8];
     Vxstring  * vd;                                                         //  vector of xstrings
 -   mutex       qmutex;                                                     //  for multi-thread access
-+   mutex_tp       qmutex;                                                     //  for multi-thread access
++   mutex_tp    qmutex;                                                     //  for multi-thread access
     int         qcap;                                                       //  queue capacity
     int         qcount;                                                     //  curr. queue count
     int         ent1;                                                       //  first entry pointer
