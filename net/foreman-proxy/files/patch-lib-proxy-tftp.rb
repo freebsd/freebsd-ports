7d7d631b924ced0db5c8b5699d46b37aa538d413
Martin Matuska <mm@FreeBSD.org>
2014-02-08 11:40:34 +0000

    Update foreman-proxy to 1.4.0

--- net/foreman-proxy/files/patch-lib-proxy-tftp.rb
+++ net/foreman-proxy/files/patch-lib-proxy-tftp.rb
@@ -1,11 +1,14 @@
---- lib/proxy/tftp.rb.orig	2014-01-29 00:26:35.629061321 +0100
-+++ lib/proxy/tftp.rb	2014-01-29 00:28:05.669054835 +0100
-@@ -104,7 +104,7 @@
+--- lib/proxy/tftp.rb.orig	2014-01-29 11:01:36.000000000 +0100
++++ lib/proxy/tftp.rb	2014-02-08 12:38:15.087475784 +0100
+@@ -105,8 +105,9 @@
        # as the dst might contain another sub directory
        FileUtils.mkdir_p destination.parent
  
--      cmd = "wget --timeout=10 --tries=3 --no-check-certificate -nv -c #{src} -O \"#{destination}\""
+-      wget = which("wget")
+-      cmd = "#{wget} --timeout=10 --tries=3 --no-check-certificate -nv -c #{src} -O \"#{destination}\""
++      # wget = which("wget")
++      # cmd = "#{wget} --timeout=10 --tries=3 --no-check-certificate -nv -c #{src} -O \"#{destination}\""
 +      cmd = "/usr/bin/fetch --timeout=10 --no-verify-hostname --no-verify-peer -a -m -q -o \"#{destination}\" #{src}"
-       Proxy::Util::CommandTask.new(cmd)
+       CommandTask.new(cmd)
      end
    end
