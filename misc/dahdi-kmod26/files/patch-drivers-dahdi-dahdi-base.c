16cc6764fcf0454af091387437701bef46cd8d7c
Max Khon <fjoe@FreeBSD.org>
2014-02-09 10:47:49 +0000

    Fix FreeBSD 10 (and later) compilation patch (FreeBSD version check).

--- misc/dahdi-kmod26/files/patch-drivers-dahdi-dahdi-base.c
+++ misc/dahdi-kmod26/files/patch-drivers-dahdi-dahdi-base.c
@@ -1,9 +1,11 @@
---- drivers/dahdi/dahdi-base.c.orig	2013-11-18 19:11:51.000000000 +0700
-+++ drivers/dahdi/dahdi-base.c	2013-11-18 19:12:07.000000000 +0700
-@@ -10224,6 +10224,8 @@
+--- drivers/dahdi/dahdi-base.c.orig	2012-09-28 02:45:03.000000000 +0700
++++ drivers/dahdi/dahdi-base.c	2014-02-09 17:45:02.000000000 +0700
+@@ -10223,7 +10223,9 @@
+ 	.d_ioctl	= dahdi_device_ioctl,
  	.d_poll		= dahdi_device_poll,
  	.d_name		= "dahdi",
- #if __FreeBSD_version >= 800039
+-#if __FreeBSD_version >= 800039
++#if __FreeBSD_version >= 1000000
 +	.d_flags	= D_TRACKCLOSE | D_NEEDMINOR
 +#elif __FreeBSD_version >= 800039
  	.d_flags	= D_PSEUDO | D_TRACKCLOSE | D_NEEDMINOR
