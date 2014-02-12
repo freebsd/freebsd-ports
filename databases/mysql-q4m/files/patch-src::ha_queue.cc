a07f361be641a556b4eb325e64657e6dfe11d4ea
Akinori MUSHA <knu@FreeBSD.org>
2014-02-12 04:52:54 +0000

    Update to 0.9.13.  Set USE_PERL5=run.

--- databases/mysql-q4m/files/patch-src::ha_queue.cc
+++ databases/mysql-q4m/files/patch-src::ha_queue.cc
@@ -1,6 +1,6 @@
---- src/ha_queue.cc.orig	2013-05-21 18:33:11.000000000 +0900
-+++ src/ha_queue.cc	2014-01-22 18:50:08.633290803 +0900
-@@ -64,9 +64,7 @@
+--- src/ha_queue.cc.orig	2014-02-06 14:51:01.000000000 +0900
++++ src/ha_queue.cc	2014-02-12 13:48:57.069544726 +0900
+@@ -68,9 +68,7 @@
  #define Q4M_DELETE_MT_PWRITE 2
  #define Q4M_DELETE_SERIAL_PWRITE 3
  
@@ -10,3 +10,12 @@
  
  #if SIZEOF_OFF_T != 8
  #  error "support for 64-bit file offsets is mandatory"
+@@ -763,7 +761,7 @@
+     if (share->mmap_table(max(min((info->_header.end() + EXPAND_BY - 1)
+ 				  / EXPAND_BY * EXPAND_BY,
+ 				  mmap_max),
+-			      EXPAND_BY))
++			      static_cast<ulonglong>EXPAND_BY))
+ 	!= 0) {
+       log("mmap failed\n");
+       goto ERR_AFTER_FILEOPEN;
