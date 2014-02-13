93bddf9c81747b20f8933ad4df53fa3f09cde708
Dirk Meyer <dinoex@FreeBSD.org>
2014-02-13 20:42:54 +0000

    - fix build with clang PR:		186649 Submitted by:	Vsevolod Volkov

---
+++ net-im/licq-icq/files/patch-filetransfer.h
@@ -0,0 +1,10 @@
+--- ../../include/licq/icq/filetransfer.h.orig	2013-08-25 11:45:17.000000000 +0200
++++ ../../include/licq/icq/filetransfer.h	2014-02-13 21:38:18.000000000 +0100
+@@ -76,6 +76,7 @@
+ #include <cstring>
+ #include <list>
+ #include <string>
++#include <time.h>
+ 
+ #include <licq/userid.h>
+ 
