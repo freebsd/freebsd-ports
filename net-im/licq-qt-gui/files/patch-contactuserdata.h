4ff757171e6997e7eb36a9c8067df8a133031214
Dirk Meyer <dinoex@FreeBSD.org>
2014-02-13 21:05:16 +0000

    - fix build with clang PR:		186650 Submitted by:	Vsevolod Volkov

---
+++ net-im/licq-qt-gui/files/patch-contactuserdata.h
@@ -0,0 +1,11 @@
+--- src/contactlist/contactuserdata.h.orig	2013-08-25 11:45:17.000000000 +0200
++++ src/contactlist/contactuserdata.h	2014-02-13 21:44:51.000000000 +0100
+@@ -20,6 +20,8 @@
+ #ifndef CONTACTUSERDATA_H
+ #define CONTACTUSERDATA_H
+ 
++#include <time.h>
++
+ #include <QList>
+ #include <QString>
+ #include <QTimer>
