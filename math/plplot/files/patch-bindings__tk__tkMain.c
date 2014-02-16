7879648e20789017a43773aef2347a8f59f55b9a
marino <marino@FreeBSD.org>
2014-02-16 19:24:06 +0000

    math/plplot: Upgrade from dev ver 5.9.10 to latest stable ver 5.10.0

--- math/plplot/files/patch-bindings__tk__tkMain.c
+++ math/plplot/files/patch-bindings__tk__tkMain.c
@@ -1,20 +1,13 @@
---- bindings/tk/tkMain.c.orig	2013-01-30 00:36:01.000000000 +0100
-+++ bindings/tk/tkMain.c	2013-10-01 22:51:31.000000000 +0200
-@@ -85,17 +85,6 @@
+--- bindings/tk/tkMain.c.orig	2013-12-18 20:25:14.000000000 -0500
++++ bindings/tk/tkMain.c
+@@ -92,10 +92,6 @@ EXTERN int Tcl_Import _ANSI_ARGS_( ( Tcl
+                                      int allowOverwrite ) );
+ #endif
  
- // From tclIntDecls.h
- 
--#ifndef Tcl_Import_TCL_DECLARED
--EXTERN int Tcl_Import _ANSI_ARGS_( ( Tcl_Interp * interp,
--                                     Tcl_Namespace * nsPtr, char * pattern,
--                                     int allowOverwrite ) );
--#endif
--
 -#ifndef Tcl_GetGlobalNamespace_TCL_DECLARE
 -EXTERN Tcl_Namespace * Tcl_GetGlobalNamespace _ANSI_ARGS_( (
 -                                                               Tcl_Interp * interp ) );
 -#endif
--
+ 
  //
  // Declarations for various library procedures and variables (don't want
- // to include tkInt.h or tkConfig.h here, because people might copy this
--- math/plplot/files/patch-cmake_modules_instdirs.cmake
+++ math/plplot/files/patch-cmake_modules_instdirs.cmake
@@ -1,6 +1,6 @@
---- ./cmake/modules/instdirs.cmake.orig	2009-09-06 21:04:43.000000000 +0200
-+++ ./cmake/modules/instdirs.cmake	2009-12-26 22:13:15.000000000 +0100
-@@ -61,13 +61,13 @@
+--- cmake/modules/instdirs.cmake.orig	2013-11-11 19:54:54.000000000 -0500
++++ cmake/modules/instdirs.cmake
+@@ -61,13 +61,13 @@ CACHE PATH "install location for C heade
  
  set(
  CMAKE_INSTALL_INFODIR
@@ -16,29 +16,29 @@
  CACHE PATH "install location for man documentation"
  )
  
-@@ -75,7 +75,7 @@
+@@ -75,7 +75,7 @@ CACHE PATH "install location for man doc
  # cached values above.
  
  # Data.
--set(DATA_DIR ${CMAKE_INSTALL_DATADIR}/${PACKAGE}${VERSION})
+-set(DATA_DIR ${CMAKE_INSTALL_DATADIR}/${PACKAGE}${PLPLOT_VERSION})
 +set(DATA_DIR ${CMAKE_INSTALL_DATADIR}/${PACKAGE})
  
  # Libraries.
  set(LIB_DIR ${CMAKE_INSTALL_LIBDIR})
-@@ -90,7 +90,7 @@
+@@ -90,7 +90,7 @@ set(BIN_DIR ${CMAKE_INSTALL_BINDIR})
  set(SHLIB_DIR ${CMAKE_INSTALL_LIBDIR})
  
  # Tcl files.
--set(TCL_DIR ${CMAKE_INSTALL_DATADIR}/${PACKAGE}${VERSION}/tcl)
+-set(TCL_DIR ${CMAKE_INSTALL_DATADIR}/${PACKAGE}${PLPLOT_VERSION}/tcl)
 +set(TCL_DIR ${CMAKE_INSTALL_DATADIR}/${PACKAGE}/tcl)
  
  # Ada source files (*.adb, *.ads) (following recommendations in
  # http://www.ada-france.org/debian/debian-ada-policy.html
-@@ -101,7 +101,7 @@
+@@ -101,7 +101,7 @@ set(ADA_INCLUDE_DIR ${CMAKE_INSTALL_DATA
  set(ADA_LIB_DIR ${CMAKE_INSTALL_LIBDIR}/ada/adalib/plplotada${LIB_TAG})
  
  # Drivers.
--set(DRV_DIR ${CMAKE_INSTALL_LIBDIR}/${PACKAGE}${VERSION}/drivers${LIB_TAG})
+-set(DRV_DIR ${CMAKE_INSTALL_LIBDIR}/${PACKAGE}${PLPLOT_VERSION}/drivers${LIB_TAG})
 +set(DRV_DIR ${CMAKE_INSTALL_LIBDIR}/${PACKAGE}/drivers${LIB_TAG})
  
  # Documentation.
