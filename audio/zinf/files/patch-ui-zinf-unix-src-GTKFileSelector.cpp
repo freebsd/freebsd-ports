bcccbdfaf86fb0333ce6caaf9c5989073bcfc324
Pawel Pekala <pawel@FreeBSD.org>
2014-02-18 17:58:16 +0000

    - Fix build with clang - Support staging - Convert to new options framework
    PR:		ports/186803 (based on)
    Submitted by:	Mathias Monnerville <mathias@monnerville.com>
    MFH:		2014Q1

--- audio/zinf/files/patch-ui-zinf-unix-src-GTKFileSelector.cpp
+++ audio/zinf/files/patch-ui-zinf-unix-src-GTKFileSelector.cpp
@@ -1,11 +1,12 @@
---- ui/zinf/unix/src/GTKFileSelector.cpp.orig	Wed Feb 19 16:39:23 2003
-+++ ui/zinf/unix/src/GTKFileSelector.cpp	Sat Sep 13 03:02:14 2003
-@@ -25,7 +25,7 @@
+--- ui/zinf/unix/src/GTKFileSelector.cpp.orig	2003-09-16 19:36:23.000000000 +0200
++++ ui/zinf/unix/src/GTKFileSelector.cpp	2014-02-18 17:46:56.364931015 +0100
+@@ -25,7 +25,8 @@
  #include <unistd.h>
  #include <dirent.h>
  
 -#if defined(solaris)                                                          
 +#if defined(solaris) || defined(__FreeBSD__)                                                         
++#include <stdlib.h>
  #include <sys/types.h>                                                        
  #include <sys/param.h>         /* for MAXPATHLEN */                           
  #include <sys/stat.h>                                                         
