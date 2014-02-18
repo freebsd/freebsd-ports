98546ce6aa08363951a890e778b319c0fd3c5318
tijl <tijl@FreeBSD.org>
2014-02-18 19:34:04 +0000

    - Convert USE_AUTOTOOLS=libtool to USES=libtool. - Replace PERL with REINPLACE_CMD in post-patch. - Replace "iconv.h" with "${ICONV_PREFIX}/include/iconv.h" so   /usr/include/iconv.h is included on FreeBSD 10 even when   converters/iconv is installed.  The port uses iconvctl(3) which   isn't declared in /usr/local/include/iconv.h with -DLIBICONV_PLUG. - files/patch-lib-error.c: Implement program_name using __progname. - files/patch-libiconv-Makefile.in: deleted because the libiconv   directory isn't built. - files/patch-src-Makefile.in:   + Remove chunks that aren't needed.   + Remove ../lib/libreco.a from LIBS so CFLAGS+=-fPIC can be removed     from the port Makefile.  Instead, just copy lib/error.c (the only     file needed from libreco.a) to src/error.c and compile it into     librecode.so.
    PR:		186637
    Approved by:	demon (maintainer)

--- converters/recode/files/patch-lib-error.c
+++ converters/recode/files/patch-lib-error.c
@@ -1,28 +1,12 @@
 --- lib/error.c.orig	Thu Aug  3 03:21:13 2000
 +++ lib/error.c	Thu Apr 17 00:29:29 2003
-@@ -25,6 +25,16 @@
- # include <config.h>
- #endif
- 
-+# ifdef __FreeBSD__
-+# include <sys/types.h>
-+# include <sys/exec.h>
-+# include <sys/param.h>
-+# include <vm/vm.h>
-+# include <vm/vm_param.h>
-+# include <vm/pmap.h>
-+# include <vm/swap_pager.h>
-+# endif
-+
- #include <stdio.h>
- 
- #if HAVE_VPRINTF || HAVE_DOPRNT || _LIBC
-@@ -80,7 +90,11 @@
+@@ -80,7 +80,12 @@
  
  /* The calling program should define program_name and set it to the
     name of the executing program.  */
 +# ifdef __FreeBSD__
-+# define program_name *((struct ps_strings *)PS_STRINGS)->ps_argvstr
++extern char *__progname;
++# define program_name __progname
 +# else
  extern char *program_name;
 +# endif
