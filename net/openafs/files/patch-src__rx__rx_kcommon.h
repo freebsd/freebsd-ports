4cfa01d6acd68f34cb9a4239e40291a28e147f2d
hrs <hrs@FreeBSD.org>
2014-02-18 03:50:18 +0000

    - Update to 1.6.6. - Fix build on 10 or later. - Add STAGEDIR support. - Use USES=kmod. - Use *_USES, CONFIGURE_OFF instead of PORT_OPTIONS:MFUSE. - Use ${CFLAGS} for XCFLAGS. - Style cleanups.
    Approved by:	bjk (maintainer)

--- net/openafs/files/patch-src__rx__rx_kcommon.h
+++ net/openafs/files/patch-src__rx__rx_kcommon.h
@@ -1,17 +1,16 @@
 diff --git a/src/rx/rx_kcommon.h b/src/rx/rx_kcommon.h
-index 64d03cb..58bf69c 100644
+index 9b04123..92752db 100644
 --- a/src/rx/rx_kcommon.h
 +++ b/src/rx/rx_kcommon.h
-@@ -141,7 +141,11 @@ typedef unsigned short etap_event_t;
- #include "h/errno.h"
- #if !(defined(AFS_SUN5_ENV) && defined(KERNEL))
- /* if sys/systm.h includes varargs.h some versions of solaris have conflicts */
--#include "stdarg.h"
-+# if defined(AFS_FBSD_ENV)
-+#  include "machine/stdarg.h"
-+# else
-+#  include "stdarg.h"
+@@ -124,6 +124,11 @@ typedef unsigned short etap_event_t;
+ #include "h/file.h"
+ #endif
+ #include "net/if.h"
++# ifdef AFS_FBSD_ENV
++#  include <sys/socket.h>
++#  include <net/if.h>
++#  include <net/if_var.h>
 +# endif
  #endif
- #ifdef KERNEL
- #include "afs/sysincludes.h"
+ #if !defined(AFS_HPUX110_ENV) && !defined(AFS_LINUX22_ENV) && !defined(AFS_DARWIN_ENV)
+ #include "netinet/in_var.h"
