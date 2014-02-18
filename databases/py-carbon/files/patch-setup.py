f25b2a73041d154b0ff8ca13e77c65a7d27277a6
Steve Wills <swills@FreeBSD.org>
2014-02-18 15:43:57 +0000

    - Enhance init script [1] - Fix with newer Django [2] - Lots of other cleanups and improvements, including moving the data to a more   standard location - Take maintainership, maintainer has not responded to PRs in many months
    PR:		ports/181043 [1]
    PR:		ports/185097 [2]
    Submitted by:	brd [1]
    Submitted by:	swills (myself) [2]
    Approved by:	maintainer timeout (bsdports@wayfair.com, >3 months)

--- databases/py-carbon/files/patch-setup.py
+++ databases/py-carbon/files/patch-setup.py
@@ -7,8 +7,8 @@
 -storage_dirs = [ ('storage/whisper',[]), ('storage/lists',[]),
 -                 ('storage/log',[]), ('storage/rrd',[]) ]
 -conf_files = [ ('conf', glob('conf/*.example')) ]
-+storage_dirs = [ ('/var/db/graphite/storage/whisper',[]), ('/var/db/graphite/storage/lists',[]),
-+                 ('/var/db/graphite/storage/log',[]), ('/var/db/graphite/storage/rrd',[]) ]
++storage_dirs = [ ('/var/db/carbon/whisper',[]), ('/var/db/carbon/lists',[]),
++                 ('/var/db/carbon/log',[]), ('/var/db/carbon/rrd',[]) ]
 +conf_files = [ ('/usr/local/etc/carbon', glob('conf/*.example')) ]
  
  install_files = storage_dirs + conf_files
