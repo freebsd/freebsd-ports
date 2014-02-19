6140644bbaeffbeae5d0a9c59b7fb52f83162359
Beat Gaetzi <beat@FreeBSD.org>
2014-02-19 09:25:23 +0000

    Update to 2.24
    Submitted by:	Jan Beich
    Security:	http://www.vuxml.org/freebsd/1753f0ff-8dd5-11e3-9b45-b4b52fce4ce8.html

---
+++ www/seamonkey/files/extra-patch-mailnews-extensions-enigmail-config-getOsTarget.pl
@@ -0,0 +1,43 @@
+commit 49a1daf
+Author: Patrick Brunschwig <patrick@enigmail.net>
+Date:   Tue Jan 28 18:10:54 2014 +0100
+
+    fixed detection of compiler type for better supporting cross-compilers
+---
+ config/getOsTarget.pl | 8 +++++---
+ 1 file changed, 5 insertions(+), 3 deletions(-)
+
+diff --git config/getOsTarget.pl config/getOsTarget.pl
+index 91285cd..cf4a233 100755
+--- mailnews/extensions/enigmail/config/getOsTarget.pl
++++ mailnews/extensions/enigmail/config/getOsTarget.pl
+@@ -9,13 +9,15 @@
+ # -c : CPU and compiler type
+ # -o : OS type
+ 
++use File::Basename;
++
+ if ($#ARGV != 3) {
+   exit (1);
+ }
+ 
+ my $srcOs = $ARGV[1];
+ my $srcCpu = $ARGV[2];
+-my $srcComp = $ARGV[3];
++my $srcComp = basename($ARGV[3]);
+ 
+ my $targetOs = "";
+ my $targetCpu = "";
+@@ -95,10 +97,10 @@ else {
+ if ($targetOs eq "WINNT") {
+   $targetComp = "msvc";
+ }
+-if ($srcComp =~ /^gcc/) {
++elsif ($srcComp =~ /gcc/ || $srcComp =~ /clang/) {
+   $targetComp = "gcc3";
+ }
+-elsif ($srcComp =~ /^cc/) {
++elsif ($srcComp =~ /cc/) {
+   if ($targetOs eq "SunOS") {
+     $targetComp = "sunc";
+   }
