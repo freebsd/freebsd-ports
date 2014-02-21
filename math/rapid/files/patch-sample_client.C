c4b5d04df4d5253449e499a96e986ebe80419fe4
ehaupt <ehaupt@FreeBSD.org>
2014-02-21 10:35:44 +0000

    - Support staging - Fix build with a modern clang

---
+++ math/rapid/files/patch-sample_client.C
@@ -0,0 +1,11 @@
+--- ./sample_client.C.orig	2014-02-21 11:34:06.215654443 +0100
++++ ./sample_client.C	2014-02-21 11:34:17.068652509 +0100
+@@ -42,7 +42,7 @@
+ #define LISTS 0
+ 
+ 
+-main()
++int main()
+ {
+   // first, get a couple of boxes in which to put our models
+ 
