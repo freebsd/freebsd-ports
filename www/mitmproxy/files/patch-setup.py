0bccc6b989ac21002dbb7fc97a9f3bf62c630183
sunpoet <sunpoet@FreeBSD.org>
2014-02-23 15:21:06 +0000

    - Update to 0.10 - While I'm here:   - Use PYDISTUTILS_AUTOPLIST and PLIST_FILES instead of PLIST   - Use USE_PYDISTUTILS=yes   - Support STAGEDIR
    Changes:	https://github.com/mitmproxy/mitmproxy/blob/master/CHANGELOG
    PR:		ports/186935
    Submitted by:	Hung-Yi Chen <gaod@hychen.org> (maintainer)

--- www/mitmproxy/files/patch-setup.py
+++ www/mitmproxy/files/patch-setup.py
@@ -1,12 +1,12 @@
---- setup.py.orig	2013-07-23 03:08:39.785592808 +0800
-+++ setup.py	2013-07-23 03:09:19.367592415 +0800
+--- setup.py.orig	2014-02-21 08:17:11.000000000 +0800
++++ setup.py	2014-02-21 08:19:43.000000000 +0800
 @@ -1,4 +1,5 @@
  from distutils.core import setup
 +from setuptools import setup
  import fnmatch, os.path
  from libmproxy import version
  
-@@ -92,13 +93,4 @@
+@@ -98,13 +99,4 @@
              "Topic :: Internet :: Proxy Servers",
              "Topic :: Software Development :: Testing"
          ],
@@ -15,7 +15,7 @@
 -            "urwid>=1.1",
 -            "pyasn1>0.1.2",
 -            "pyopenssl>=0.13",
--            "PIL",
+-            "Pillow>=2.3.0,<2.4",
 -            "lxml",
 -            "flask"
 -        ],
