efd4784754c90e3492b07cdca65fdc0a4c8f6ed4
Martin Wilke <miwi@FreeBSD.org>
2014-02-27 15:20:09 +0000

    -  Update to version 9.8l
    PR:		186420
    Submitted by:	ports fury

---
+++ editors/xvile/files/patch-x11menu.c
@@ -0,0 +1,24 @@
+--- x11menu.c.orig
++++ x11menu.c
+@@ -29,12 +29,21 @@
+ #endif
+ 
+ #if ATHENA_WIDGETS
++#ifdef HAVE_LIB_XAW3D
++#include <X11/Xaw3d/Form.h>
++#include <X11/Xaw3d/SimpleMenu.h>
++#include <X11/Xaw3d/MenuButton.h>
++#include <X11/Xaw3d/SmeLine.h>
++#include <X11/Xaw3d/SmeBSB.h>
++#endif
++#ifdef HAVE_LIB_XAW
+ #include <X11/Xaw/Form.h>
+ #include <X11/Xaw/SimpleMenu.h>
+ #include <X11/Xaw/MenuButton.h>
+ #include <X11/Xaw/SmeLine.h>
+ #include <X11/Xaw/SmeBSB.h>
+ #endif
++#endif
+ 
+ #define Nval(name,value) name, (XtArgVal)(value)
+ #define Sval(name,value) name, (value)
