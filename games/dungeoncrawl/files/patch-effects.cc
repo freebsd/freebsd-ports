b9594659331b70b27540349d802f23a172b6cc10
Martin Wilke <miwi@FreeBSD.org>
2014-02-27 09:16:22 +0000

    - Fix build on -current - Update MASTER_SITES
    PR:		186736
    Submitted by:	Ports FUry

---
+++ games/dungeoncrawl/files/patch-effects.cc
@@ -0,0 +1,10 @@
+--- effects.cc.orig
++++ effects.cc
+@@ -13,6 +13,7 @@
+ 
+ #include <string.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ 
+ #include "externs.h"
+ 
---
+++ games/dungeoncrawl/files/patch-tags.cc
@@ -0,0 +1,10 @@
+--- tags.cc.orig
++++ tags.cc
+@@ -54,6 +54,7 @@
+ */
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <string.h>            // for memcpy
+ 
+ #ifdef LINUX
