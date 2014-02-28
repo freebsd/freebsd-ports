e156816f5533045bd997e523e6c49e4fe5ec8fd7
sunpoet <sunpoet@FreeBSD.org>
2014-02-28 15:15:14 +0000

    - Update to 5.01 (5.0.14) - Use CONFLICTS_INSTALL instead off CONFLICTS - Respect LDFLAGS - Remove contiguous blank lines - Cosmetic change
    Thanks to:	naddy, vanilla

--- archivers/unrar/files/patch-rijndael.hpp
+++ archivers/unrar/files/patch-rijndael.hpp
@@ -1,24 +1,20 @@
---- rijndael.hpp.orig	2012-01-09 14:46:08.000000000 +0100
-+++ rijndael.hpp	2012-04-05 22:42:56.000000000 +0200
-@@ -18,15 +18,21 @@ class Rijndael
-   public:
-     enum Direction { Encrypt , Decrypt };
+--- rijndael.hpp.orig	2013-12-01 16:10:14.000000000 +0800
++++ rijndael.hpp	2014-02-04 08:48:42.137144316 +0800
+@@ -16,6 +16,9 @@
+ class Rijndael
+ { 
    private:
-+#ifndef OPENSSL_AES
++#ifdef OPENSSL_AES
++    EVP_CIPHER_CTX ctx;
++#else // OPENSSL_AES
      void keySched(byte key[_MAX_KEY_COLUMNS][4]);
      void keyEncToDec();
      void encrypt(const byte a[16], byte b[16]);
-     void decrypt(const byte a[16], byte b[16]);
-     void GenerateTables();
-+#endif
- 
-+#ifdef OPENSSL_AES
-+    EVP_CIPHER_CTX ctx;
-+#else
-     Direction m_direction;
+@@ -25,6 +28,7 @@
+     int      m_uRounds;
      byte     m_initVector[MAX_IV_SIZE];
      byte     m_expandedKey[_MAX_ROUNDS+1][4][4];
-+#endif
++#endif // OPENSSL_AES
    public:
      Rijndael();
-     void init(Direction dir,const byte *key,byte *initVector);
+     void Init(bool Encrypt,const byte *key,uint keyLen,const byte *initVector);
