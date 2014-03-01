90feebd9fc3cc790d471689169ce745b9d1abda6
Diane Bruce <db@FreeBSD.org>
2014-03-01 14:32:39 +0000

    - Updating to 3.7.2 - disabled USRP default option until usrp is fixed on -10 and head - disable docs on i386 for now as it fails to build - enable staging
    PR:		ports/186799
    Submitted by:	lev

---
+++ comms/gnuradio/files/patch-gnuradio-runtime_lib_thread_thread.cc
@@ -0,0 +1,34 @@
+--- gnuradio-runtime/lib/thread/thread.cc.orig	2013-08-28 14:27:03.000000000 -0400
++++ gnuradio-runtime/lib/thread/thread.cc	2014-01-05 20:39:46.220592423 -0500
+@@ -217,6 +217,7 @@
+     void
+     thread_bind_to_processor(gr_thread_t thread, const std::vector<int> &mask)
+     {
++#ifdef _GNU_SOURCE
+       cpu_set_t set;
+       size_t len = sizeof(cpu_set_t);
+       std::vector<int> _mask = mask;
+@@ -232,6 +233,7 @@
+         s << "thread_bind_to_processor failed with error: " << ret << std::endl;
+         throw std::runtime_error(s.str());
+       }
++#endif
+     }
+ 
+     void
+@@ -243,6 +245,7 @@
+     void
+     thread_unbind(gr_thread_t thread)
+     {
++#ifdef _GNU_SOURCE
+       cpu_set_t set;
+       size_t len = sizeof(cpu_set_t);
+ 
+@@ -258,6 +261,7 @@
+         s << "thread_unbind failed with error: " << ret << std::endl;
+         throw std::runtime_error(s.str());
+       }
++#endif
+     }
+ 
+     int 
