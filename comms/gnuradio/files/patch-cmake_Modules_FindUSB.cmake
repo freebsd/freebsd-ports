11a104141e92c2a05040ced62d288eef555daf6f
db <db@FreeBSD.org>
2014-03-01 14:32:39 +0000

    - Updating to 3.7.2 - disabled USRP default option until usrp is fixed on -10 and head - disable docs on i386 for now as it fails to build - enable staging
    PR:		ports/186799
    Submitted by:	lev

--- comms/gnuradio/files/patch-cmake_Modules_FindUSB.cmake
+++ comms/gnuradio/files/patch-cmake_Modules_FindUSB.cmake
@@ -1,6 +1,6 @@
---- cmake/Modules/FindUSB.cmake.orig	2012-09-04 19:43:06.000000000 -0500
-+++ cmake/Modules/FindUSB.cmake	2012-09-27 14:10:21.000000000 -0500
-@@ -1,20 +1,34 @@
+--- cmake/Modules/FindUSB.cmake.orig	2012-04-09 11:38:58.000000000 -0400
++++ cmake/Modules/FindUSB.cmake	2014-02-06 10:29:24.962247114 -0500
+@@ -1,20 +1,38 @@
  if(NOT LIBUSB_FOUND)
 -  pkg_check_modules (LIBUSB_PKG libusb-1.0)
 -  find_path(LIBUSB_INCLUDE_DIR NAMES libusb.h
@@ -13,30 +13,34 @@
      /usr/include
 -    /usr/local/include
    )
++  message(status "--- LIBUSB_INCLUDE_DIR ${LIBUSB_INCLUDE_DIR}")
++
++  if(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
++    message(status "--- Looking for FreeBSD usb")
++    find_library(LIBUSB_LIBRARIES NAMES usb
++      PATHS
++      ${LIBUSB_PKG_LIBRARY_DIRS}
++      /usr/lib
++    )
++
++  message(status "--- LIBUSB_LIBRARIES ${LIBUSB_LIBRARIES}")
  
 -  find_library(LIBUSB_LIBRARIES NAMES usb-1.0
-+  if(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
-+    message(status " Looking for FreeBSD usb")
-+    find_library(LIBUSB_LIBRARIES NAMES usb-1.0
-+    PATHS
-+    ${LIBUSB_PKG_LIBRARY_DIRS}
-+    /usr/lib
-+  )
-+  elseif(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
-+    find_library(LIBUSB_LIBRARIES NAMES usb-1.0
++    CHECK_LIBRARY_EXISTS(${LIBUSB_LIBRARIES} "libusb_get_string_descriptor"
++	"/usr/lib" HAVE_LIBUSB_GET_STRING_DESCRIPTOR)
++    if (HAVE_LIBUSB_GET_STRING_DESCRIPTOR)
++      add_definitions( -DHAVE_LIBUSB_GET_STRING_DESCRIPTOR=${HAVE_LIBUSB_GET_STRING_DESCRIPTOR})
++    endif()
++    message(status "--- HAVE_LIBUSB_GET_STRING_DESCRIPTOR ${HAVE_LIBUSB_STRING_DESCRIPTOR}")
++  endif(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
++
++  find_library(LIBUSB_LIBRARIES NAMES usb
      PATHS
      ${LIBUSB_PKG_LIBRARY_DIRS}
      /usr/lib
--    /usr/local/lib
+     /usr/local/lib
    )
-+  endif(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
- 
-+  find_library(LIBUSB_LIBRARIES NAMES usb
-+    PATHS
-+    ${LIBUSB_PKG_LIBRARY_DIRS}
-+    /usr/lib
-+    /usr/local/lib
-+  )
+-
  if(LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
    set(LIBUSB_FOUND TRUE CACHE INTERNAL "libusb-1.0 found")
    message(STATUS "Found libusb-1.0: ${LIBUSB_INCLUDE_DIR}, ${LIBUSB_LIBRARIES}")
