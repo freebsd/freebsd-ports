477efa96a0dd148d10e9761eff8240f3d76eae05
Tijl Coosemans <tijl@FreeBSD.org>
2014-03-02 13:14:07 +0000

    In libtool.mk, let the regexp for DragonFly BSD support match both "freebsd*)" and "freebsd* ", and add "dragonfly*" only if it isn't there already.
    Approved by:	bapt

--- Mk/Uses/libtool.mk
+++ Mk/Uses/libtool.mk
@@ -19,10 +19,10 @@ _INCLUDE_USES_LIBTOOL_POST_MK=	yes
 patch-libtool:
 	@${FIND} ${WRKDIR} \( -name configure -or -name ltconfig \)	\
 		-type f | ${XARGS} ${REINPLACE_CMD}			\
+		-e '/dragonfly\*/!s/freebsd\*[ )]/dragonfly* | &/'	\
 		-e '/gcc_dir=\\`/s/gcc /$$CC /'				\
 		-e '/gcc_ver=\\`/s/gcc /$$CC /'				\
 		-e '/link_all_deplibs[0-9A-Z_]*=/s/=unknown/=no/'	\
-		-e 's,freebsd\*),freebsd\*|dragonfly\*),g'		\
 		-e '/objformat=/s/echo aout/echo elf/'			\
 		-e "/freebsd-elf\\*)/,/;;/ {				\
 		    /deplibs_check_method=/s/=.*/=pass_all/; }"	
