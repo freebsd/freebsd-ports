e089af9fbabf1cd3225135810c39258db83346d3
rm <rm@FreeBSD.org>
2014-03-03 09:47:32 +0000

    devel/py-Jinja2: support staging and do general clean-up
    - remove indefinite article from COMMENT
    - add license (BSD3C)
    - do not use easy_install for installation and convert to auto-generated packing list
    - add staging support
    - simplify docs installation:
      - install docs manually
      - install docs unconditionally into staging area
      - no need to install doc sources, compiled html versions are suffice
    - limit python version to 2.x only:
      ===>  Configuring for py33-Jinja-1.2_3
      Traceback (most recent call last):
        File "<string>", line 1, in <module>
        File "setup.py", line 28
          except DistutilsError, e:
                             ^
      SyntaxError: invalid syntax
      *** Error code 1
    - remove deprecated author attribution from pkg-descr and do tab->space change in WWW
    - bump PORTREVISION
    
    Approved by:	nivit (maintainer, explicitly for such changes in his ports)

--- devel/py-Jinja/files/patch-setup.py
+++ devel/py-Jinja/files/patch-setup.py
@@ -1,20 +1,6 @@
---- setup.py	2007-11-17 18:13:24.000000000 +0100
-+++ setup.py.port	2008-02-16 14:36:17.000000000 +0100
-@@ -51,6 +51,13 @@
-             print '  problem: %s' % e
-             print '=' * 79
- 
-+if not os.environ.has_key('NOPORTDOCS'):
-+    port_data_files=[
-+        ('docs/html', list(list_files('docs/html'))),
-+        ('docs/txt', list(list_files('docs/src')))
-+    ]
-+else:
-+    port_data_files=[]
- 
- setup(
-     name='Jinja',
-@@ -78,10 +85,7 @@
+--- ./setup.py.orig	2007-11-17 20:13:24.000000000 +0300
++++ ./setup.py	2014-03-03 13:21:43.000000000 +0400
+@@ -78,10 +78,7 @@
      ],
      keywords=['python.templating.engines'],
      packages=['jinja', 'jinja.translators'],
@@ -22,7 +8,7 @@
 -        ('docs/html', list(list_files('docs/html'))),
 -        ('docs/txt', list(list_files('docs/src')))
 -    ],
-+    data_files=port_data_files,
++    data_files=[],
      entry_points='''
      [python.templating.engines]
      jinja = jinja.plugin:BuffetPlugin
