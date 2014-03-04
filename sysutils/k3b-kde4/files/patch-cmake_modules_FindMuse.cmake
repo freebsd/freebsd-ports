a97c79212b3092e32b1d271555fa8c3ef0e761ca
marino <marino@FreeBSD.org>
2014-03-04 08:37:44 +0000

    sysutils/k3b-kde4: Restore MUSEPACK option with DPorts fix
    - Revert r346996 entirely
    - Bypass MUSEPACK detection (hardcode it as detected) to fix that option
    - Bump - This was default options change
    
    Approved by:	makc

---
+++ sysutils/k3b-kde4/files/patch-cmake_modules_FindMuse.cmake
@@ -0,0 +1,18 @@
+--- cmake/modules/FindMuse.cmake.orig	2011-01-15 20:47:29.000000000 +0000
++++ cmake/modules/FindMuse.cmake
+@@ -15,13 +15,10 @@ if( MUSE_INCLUDE_DIR AND MUSE_LIBRARIES
+ endif( MUSE_INCLUDE_DIR AND MUSE_LIBRARIES )
+ 
+ include(CheckIncludeFiles)
+-check_include_files(mpc/mpcdec.h HAVE_MPC_MPCDEC_H)
+-check_include_files(mpcdec/mpcdec.h HAVE_MPCDEC_MPCDEC_H)
+-check_include_files(musepack/musepack.h HAVE_MUSEPACK_MUSEPACK_H)
++set ( HAVE_MPC_MPCDEC_H 1 )
+ 
+ if( HAVE_MPC_MPCDEC_H )
+-    find_path( MUSE_INCLUDE_DIR mpc/mpcdec.h )
+-    find_library( MUSE_LIBRARIES NAMES mpcdec )
++    set( MUSE_LIBRARIES mpcdec )
+     set( MPC_HEADER_FILE "<mpc/mpcdec.h>" )
+ elseif( HAVE_MPCDEC_MPCDEC_H )
+     find_path( MUSE_INCLUDE_DIR mpcdec/mpcdec.h )
