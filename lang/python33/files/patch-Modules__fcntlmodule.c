ea56f87d19047e35293dbec61811a10bd0226b72
Kubilay Kocak <koobs@FreeBSD.org>
2014-03-04 12:38:02 +0000

    lang/python33: Update to 3.3.4
    - Update to 3.3.4
    - Update pkg-plist
    - Remove patch-CVE-2014-1912 (upstream)
    - Remove patch-issue20374 (upstream)
    - Repatch and rename patch-Modules__fcntlmodule.c
    - Add patch-issue-20695-Lib__test__test_urllibnet.py (tests) [1]
    - Add regression-test target for QA
    
    Changes: 3.3.4 - 09/02/2014
    
        http://docs.python.org/3.3/whatsnew/changelog.html#python-3-3-4
    
    Extra:
    
    - Update python-doc-html distinfo for Python 3.3 docs
    - Update bsd.python.mk to support the new version
    
    [1] http://bugs.python.org/issue20695

--- lang/python33/files/patch-Modules__fcntlmodule.c
+++ lang/python33/files/patch-Modules__fcntlmodule.c
@@ -1,6 +1,6 @@
---- ./Modules/fcntlmodule.c.orig	2012-09-29 18:00:45.000000000 +1000
-+++ ./Modules/fcntlmodule.c	2012-10-04 20:21:15.000000000 +1000
-@@ -97,20 +97,15 @@
+--- ./Modules/fcntlmodule.c.orig	2014-03-04 20:15:17.641117835 +1100
++++ ./Modules/fcntlmodule.c	2014-03-04 20:19:36.141145958 +1100
+@@ -98,20 +98,15 @@
  {
  #define IOCTL_BUFSZ 1024
      int fd;
@@ -12,7 +12,7 @@
         whereas the system expects it to be a 32bit bit field value
         regardless of it being passed as an int or unsigned long on
 -       various platforms.  See the termios.TIOCSWINSZ constant across
--       platforms for an example of thise.
+-       platforms for an example of this.
 -
 -       If any of the 64bit platforms ever decide to use more than 32bits
 -       in their unsigned long ioctl codes this will break and need
@@ -24,7 +24,7 @@
      int arg;
      int ret;
      Py_buffer pstr;
-@@ -119,7 +114,7 @@
+@@ -120,7 +115,7 @@
      int mutate_arg = 1;
      char buf[IOCTL_BUFSZ+1];  /* argument plus NUL byte */
  
@@ -33,7 +33,7 @@
                           conv_descriptor, &fd, &code,
                           &pstr, &mutate_arg)) {
          char *arg;
-@@ -174,7 +169,7 @@
+@@ -175,7 +170,7 @@
      }
  
      PyErr_Clear();
@@ -42,7 +42,7 @@
                           conv_descriptor, &fd, &code, &pstr)) {
          str = pstr.buf;
          len = pstr.len;
-@@ -201,7 +196,7 @@
+@@ -202,7 +197,7 @@
      PyErr_Clear();
      arg = 0;
      if (!PyArg_ParseTuple(args,
