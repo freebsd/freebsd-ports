2cc1ca0998e260d6ba7677d3193fb8a9080bd960
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2014-03-06 16:44:34 +0000

    MFH: r338064
    - Allow build with clang [1]
    - Remove explicit -lstdc++
    - Bump PORTREVISION for package change
    
    Submitted by:	vanilla [1]
    Approved by:	portmgr (mat, via IRC)

---
+++ databases/kyototycoon/files/patch-ktdbext.h
@@ -0,0 +1,10 @@
+--- ktdbext.h.orig	2012-05-25 01:44:34.000000000 +0800
++++ ktdbext.h	2013-12-28 01:21:30.081508681 +0800
+@@ -16,6 +16,7 @@
+ #ifndef _KTDBEXT_H                       // duplication check
+ #define _KTDBEXT_H
+ 
++#include <myconf.h>
+ #include <ktcommon.h>
+ #include <ktutil.h>
+ #include <ktulog.h>
