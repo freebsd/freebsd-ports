a73973bf9d44d82e1e109cfd75001f9d5b97bab7
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2014-03-08 23:35:13 +0000

    MFH: r347541
    Enable use of OpenSSL in the bundled copy of libcurl.
    
    The default is off, which makes calls such as FILE(DOWNLOAD ...) from HTTPS
    locations fail.
    
    PR:		ports/187374
    Approved by:	portmgr (miwi)

--- devel/cmake/files/InitialCache.cmake
+++ devel/cmake/files/InitialCache.cmake
@@ -4,3 +4,7 @@
 # devel/qt4-corelib is not).
 # See https://mail.kde.org/pipermail/kde-freebsd/2013-July/015703.html
 set(BUILD_TESTING OFF CACHE BOOL "Build the testing tree.")
+
+# Explicitly enable use of OpenSSL in the bundled copy of libcurl, as it
+# defaults to off. See ports/187374.
+set(CMAKE_USE_OPENSSL ON CACHE BOOL "Use OpenSSL code with curl.")
