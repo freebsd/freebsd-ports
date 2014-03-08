884d427663bb0d9a23f2f0cb5087f857faad9972
koobs <koobs@FreeBSD.org>
2014-03-08 03:24:56 +0000

    net/py-beanstalkc: Update to 0.4.0, Add QA bits
    - Update to 0.4.0
    - Add TEST_DEPENDS and regression-test: target for QA
    - Patch setup.py to integrate the test command with py-nose
    - Remove setup.py patch adding zip_safe=False (no longer needed)
    - pkg-descr: Update WWW: homepage link
    
    Changes:
    
     * https://github.com/earl/beanstalkc/commits/v0.4.0

--- net/py-beanstalkc/files/patch-setup.py
+++ net/py-beanstalkc/files/patch-setup.py
@@ -1,8 +1,8 @@
---- ./setup.py.orig	2012-07-15 01:01:36.000000000 +1000
-+++ ./setup.py	2012-07-15 01:01:49.000000000 +1000
-@@ -30,4 +30,5 @@
-         'Programming Language :: Python',
+--- ./setup.py.orig	2014-03-08 14:16:00.573786621 +1100
++++ ./setup.py	2014-03-08 14:16:32.606016182 +1100
+@@ -29,4 +29,5 @@
+         'Operating System :: OS Independent',
          'Topic :: Software Development :: Libraries :: Python Modules',
      ],
-+    zip_safe=False,
++    test_suite = 'nose.collector',
  )
