9d7089ffb5bffe133b98851c8d4585a070b8070c
Gerald Pfeifer <gerald@FreeBSD.org>
2014-03-09 23:34:47 +0000

    Fix the build with newer compilers (such as GCC 4.7).
    PR:		187395, 182136
    Submitted by:	Christoph Moench-Tegeder <cmt@burggraben.net>
    Approved by:	maintainer (Jason Bacon <jwbacon@tds.net>)

---
+++ lang/x10/files/patch-x10.runtime-src-cpp-x10-utils-concurrent-AtomicReference.h
@@ -0,0 +1,11 @@
+--- ../x10.runtime/src-cpp/x10/util/concurrent/AtomicReference.h.orig	2014-03-09 15:50:47.000000000 +0100
++++ ../x10.runtime/src-cpp/x10/util/concurrent/AtomicReference.h	2014-03-09 15:51:15.000000000 +0100
+@@ -15,6 +15,7 @@
+ #include <x10aux/config.h>
+ #include <x10aux/ref.h>
+ #include <x10aux/RTT.h>
++#include <x10aux/basic_functions.h>
+ #include <x10aux/string_utils.h>
+ #include <x10aux/atomic_ops.h>
+ 
+
