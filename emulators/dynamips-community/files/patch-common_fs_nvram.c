06efbf78f03457b56c8e3740453672b9df4a5776
Pawel Pekala <pawel@FreeBSD.org>
2014-03-13 18:36:36 +0000

    - Update to version 0.2.11 [1] - Remove optional installation of DOCS files, staging   takes care of that, make it shorter
    PR:		ports/187447 [1]
    Submitted by:	maintainer

---
+++ emulators/dynamips-community/files/patch-common_fs_nvram.c
@@ -0,0 +1,9 @@
+--- common/fs_nvram.c.orig	2014-03-03 12:41:30.000000000 +0400
++++ common/fs_nvram.c	2014-03-03 12:41:46.000000000 +0400
+@@ -862,4 +862,4 @@
+    }
+ 
+    return(0);
+-}
+\ No newline at end of file
++}
