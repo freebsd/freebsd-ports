ca10231729420b15896fef8ebfc39a044d72bcbf
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2014-03-14 15:51:35 +0000

    - Update to 1.4.1 - Use PORTDOCS - Simplify Makefile
    Changes:	http://savannah.gnu.org/forum/forum.php?forum_id=7899

--- sysutils/freeipmi/files/patch-ipmi-oem-ipmi-oem.h
+++ sysutils/freeipmi/files/patch-ipmi-oem-ipmi-oem.h
@@ -1,7 +1,7 @@
---- ipmi-oem/ipmi-oem.h.orig	2012-01-03 03:26:10.000000000 +0800
-+++ ipmi-oem/ipmi-oem.h	2012-01-06 04:36:44.994004384 +0800
+--- ipmi-oem/ipmi-oem.h.orig	2014-03-04 03:36:03.000000000 +0800
++++ ipmi-oem/ipmi-oem.h	2014-03-06 04:16:04.700980190 +0800
 @@ -20,6 +20,7 @@
- #define _IPMI_OEM_H
+ #define IPMI_OEM_H
  
  #include <freeipmi/freeipmi.h>
 +#include <sys/limits.h>
