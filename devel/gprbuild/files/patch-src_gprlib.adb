8483905680cf939e06c456b17045d002a61d83f1
John Marino <marino@FreeBSD.org>
2014-03-14 08:36:59 +0000

    devel/gprbuild: New version as result of gcc-aux upgrade to gcc49
    GPRBuild was previously based on the 2012 release, but it never should
    have gone past the 2011 issue.  GPRBuild needs to use the same sources as
    the GNAT that builds it, but it was built with the embedded sources
    instead.  These "GPL" sources were ahead of the "FSF" sources, and
    GPRBuild only worked by chance.  Using the proper GNAT Sources (gnat_util)
    resulted in build failure!  Now gcc-aux has been updated and now
    GPRBuild 2013 can be properly built with FSF GNAT 4.9 sources.

--- devel/gprbuild/files/patch-src_gprlib.adb
+++ devel/gprbuild/files/patch-src_gprlib.adb
@@ -1,6 +1,4 @@
-$NetBSD: patch-src_gprlib.adb,v 1.1 2012/07/08 20:08:42 marino Exp $
-
---- src/gprlib.adb.orig	2012-05-10 13:59:05.000000000 +0000
+--- src/gprlib.adb.orig	2013-04-16 14:36:02.000000000 +0000
 +++ src/gprlib.adb
 @@ -390,6 +390,11 @@ procedure Gprlib is
  
