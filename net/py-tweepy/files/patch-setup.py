f3a8e58f521ce4787196e8adf4e6d0d665f4dd94
Kubilay Kocak <koobs@FreeBSD.org>
2014-03-15 15:29:20 +0000

    net/py-tweepy: Update to 2.2
    - Update to 2.2
    - Remove post-extract target: (tests no longer included)
    - Remove zip_safe=False patch in setup.py (no longer necessary)
    
    Changes:
    
        https://github.com/tweepy/tweepy/blob/2.2/CHANGELOG.md
    
    Approved by:	maintainer (via Twitter)

--- net/py-tweepy/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
---- ./setup.py.orig	2013-06-16 00:45:48.756665404 +1000
-+++ ./setup.py	2013-06-16 00:45:58.696879511 +1000
-@@ -12,4 +12,4 @@
-       url="http://github.com/tweepy/tweepy",
-       packages = find_packages(),
-       keywords= "twitter library",
--      zip_safe = True)
-+      zip_safe = False)
