a139f18a4c493c00c9f49215ae462bb18ecfc1ed
nemysis <nemysis@FreeBSD.org>
2014-03-15 18:34:55 +0000

    - Bump PORTREVISION for pkg-plist change - Cleanup port - Add master site, icons - Take maintainership - Use the new format for LIB_DEPENDS - Remove obsolete REINPLACE -I/usr/local/include and -L/usr/local/lib,   don't use -I${LOCALBASE}/include and -L${LOCALBASE}/lib - Remove PORT_VERBS, not needed - Add NLS Option, install conditional NLS - Support STAGEDIR and add OPTIONS_SUB, add REINPLACE for staging - Add Desktop entry file - Remove obsolete REINPLACE, add patch-Makefile.in, copy and install   /etc/ultimatestunts.conf as example,   user gets ~/.ultimatestunts/ultimatestunts.conf when the game starts - Recreate patches with make makepatch - Change pkg-descr - Change pkg-plist, remove DOCS, add icons

--- games/ultimatestunts/files/patch-simulation__metaserver.cpp
+++ games/ultimatestunts/files/patch-simulation__metaserver.cpp
@@ -1,5 +1,5 @@
---- simulation/metaserver.cpp.orig	2012-08-25 10:07:01.000000000 +0000
-+++ simulation/metaserver.cpp
+--- ./simulation/metaserver.cpp.orig	2012-08-25 12:07:01.000000000 +0200
++++ ./simulation/metaserver.cpp	2014-03-15 13:54:11.868249585 +0100
 @@ -16,6 +16,7 @@
   ***************************************************************************/
  #include <cstdio>
