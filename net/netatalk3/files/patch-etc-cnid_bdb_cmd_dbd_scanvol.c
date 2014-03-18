783d4491d60fef527fc164e719809538de463fae
marcus <marcus@FreeBSD.org>
2014-03-18 21:53:40 +0000

    Update to 3.1.1.  See http://netatalk.sourceforge.net/3.1/ReleaseNotes3.1.1.html for the list of changes in this release.
    Additionally, fix some new LIB_DEPENDS usage, and adjust the pkg-descr to
    match reality [1].
    
    PR:		187658 [1]

---
+++ net/netatalk3/files/patch-etc-cnid_bdb_cmd_dbd_scanvol.c
@@ -0,0 +1,11 @@
+--- etc/cnid_bdb/cmd_dbd_scanvol.c.orig	2014-03-18 17:42:49.000000000 -0400
++++ etc/cnid_bdb/cmd_dbd_scanvol.c	2014-03-18 17:43:31.000000000 -0400
+@@ -616,7 +616,7 @@ static int check_orphaned(const char *na
+     struct stat sb;
+ 
+     if (strlen(name) < 3)
+-        return;
++        return 0;
+ 
+     rc = lstat(&name[2], &sb);
+ 
