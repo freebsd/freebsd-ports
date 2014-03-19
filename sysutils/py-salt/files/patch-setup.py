062a1e423be61751d32f99e7590e0cab0a0a2ab9
Veniamin Gvozdikov <vg@FreeBSD.org>
2014-03-19 07:03:52 +0000

    - Bump version - Update to 2014.1.0 - Moved py-salt-cloud because py-salt included cloud part - Link to full changelog: http://docs.saltstack.com/topics/releases/2014.1.0.html
    PR:		ports/186950
    Submitted by:	Christer Edwards <christer.edwards@gmail.com> (maintainer)

--- sysutils/py-salt/files/patch-setup.py
+++ sysutils/py-salt/files/patch-setup.py
@@ -1,20 +1,17 @@
---- ./setup.py.orig	2013-10-17 21:27:29.000000000 +0400
-+++ ./setup.py	2013-10-25 10:43:51.000000000 +0400
-@@ -313,7 +313,7 @@
-                              'salt.log.handlers',
-                              ],
-                 'package_data': {'salt.modules': ['rh_ip/*.jinja']},
+--- ./setup.py.orig	2014-03-06 10:50:27.000000000 +0200
++++ ./setup.py	2014-03-06 10:52:20.000000000 +0200
+@@ -451,12 +451,12 @@
+                                     'virt/*.jinja'
+                                     ],
+                                 },
 -                'data_files': [('share/man/man1',
 +                'data_files': [('man/man1',
-                                 ['doc/man/salt-master.1',
-                                  'doc/man/salt-key.1',
-                                  'doc/man/salt.1',
-@@ -324,7 +324,7 @@
-                                  'doc/man/salt-ssh.1',
+                                 ['doc/man/salt-cp.1',
+                                  'doc/man/salt-call.1',
                                   'doc/man/salt-minion.1',
                                   ]),
--                               ('share/man/man7', ['doc/man/salt.7']),
-+                               ('man/man7', ['doc/man/salt.7']),
+-                               ('share/man/man7',
++                               ('man/man7',
+                                 ['doc/man/salt.7',
+                                  ]),
                                 ],
-                 # Required for esky builds
-                 'install_requires': REQUIREMENTS,
