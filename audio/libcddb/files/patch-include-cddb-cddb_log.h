5ec77b4e269e4c941f14348e60202078b20fef75
Tijl Coosemans <tijl@FreeBSD.org>
2014-03-20 15:35:34 +0000

    - Add patches to fix include guards (found by Clang). - Modernise port Makefile a bit.

---
+++ audio/libcddb/files/patch-include-cddb-cddb_log.h
@@ -0,0 +1,11 @@
+--- include/cddb/cddb_log.h.orig	2005-03-11 22:29:29.000000000 +0100
++++ include/cddb/cddb_log.h	2014-03-20 16:00:15.000000000 +0100
+@@ -19,7 +19,7 @@
+     Boston, MA  02111-1307, USA.
+ */
+ 
+-#ifndef CDDB_LOH_H
++#ifndef CDDB_LOG_H
+ #define CDDB_LOG_H
+ 
+ #ifdef __cplusplus
---
+++ audio/libcddb/files/patch-include-cddb-cddb_log_ni.h
@@ -0,0 +1,11 @@
+--- include/cddb/cddb_log_ni.h.orig	2005-03-11 22:29:29.000000000 +0100
++++ include/cddb/cddb_log_ni.h	2014-03-20 16:00:24.000000000 +0100
+@@ -19,7 +19,7 @@
+     Boston, MA  02111-1307, USA.
+ */
+ 
+-#ifndef CDDB_LOH_NI_H
++#ifndef CDDB_LOG_NI_H
+ #define CDDB_LOG_NI_H
+ 
+ #ifdef __cplusplus
