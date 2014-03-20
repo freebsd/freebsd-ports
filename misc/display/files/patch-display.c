fd1b3e6fd6f6262e15857f3ee8185930afa499e1
mat <mat@FreeBSD.org>
2014-03-20 00:09:25 +0000

    - Convert to staging - Cleanup unneeded bits in the Makefile - Patch a bit more to fix compiler warnings - While there, rename patches to follow makepatch naming.
    Sponsored by:	Absolight

--- misc/display/files/patch-display.c
+++ misc/display/files/patch-display.c
@@ -1,14 +1,17 @@
---- display.c.orig	Thu Dec 15 14:29:14 2005
-+++ display.c	Thu Dec 15 14:29:58 2005
-@@ -12,6 +12,7 @@
+--- ./display.c.orig	2005-12-14 04:40:15.000000000 +0100
++++ ./display.c	2014-03-20 01:01:07.000000000 +0100
+@@ -12,6 +12,10 @@
  #include <signal.h>
  #include <ctype.h>
  #include <time.h>
 +#include <stdio.h>
++#include <unistd.h>
++#include <stdlib.h>
++#include <string.h>
  
  #define		DEFAULT_DELAY	5
  #define         DISPLAY_VERSION "1.2"
-@@ -19,6 +20,8 @@
+@@ -19,17 +23,16 @@
  static	char	*Command;
  static	int	Delay;
  
@@ -17,7 +20,9 @@
  char lt[128];
  int ntimes;
  
-@@ -27,9 +30,6 @@
+-main(argc, argv)
++int main(argc, argv)
+ int	argc;
  char	*argv[];
  {
    int i, c;
@@ -27,7 +32,7 @@
    
    for(i = 0; i < 128; i++)
      {
-@@ -90,7 +90,7 @@
+@@ -90,7 +93,7 @@
  
  void display()
  {
@@ -36,3 +41,11 @@
  	int	ch;
  	char    *dt;
  	time_t  tnow;
+@@ -133,7 +136,6 @@
+ int	argc;
+ char	*argv[];
+ {
+-	extern	void	usage();
+ 	auto	int	argn,
+ 			delay_found;
+ 
