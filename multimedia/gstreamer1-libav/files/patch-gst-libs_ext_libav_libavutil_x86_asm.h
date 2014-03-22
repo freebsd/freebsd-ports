63ae5c83784bc2113d80657134dcc4752746ba95
Tijl Coosemans <tijl@FreeBSD.org>
2014-03-22 10:53:52 +0000

    Copy a patch from multimedia/libav to fix compilation with clang on i386 with MMX and SSE.

---
+++ multimedia/gstreamer1-libav/files/patch-gst-libs_ext_libav_libavutil_x86_asm.h
@@ -0,0 +1,11 @@
+--- gst-libs/ext/libav/libavutil/x86/asm.h.orig
++++ gst-libs/ext/libav/libavutil/x86/asm.h
+@@ -68,7 +68,7 @@
+ typedef int x86_reg;
+ #endif
+ 
+-#define HAVE_7REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE && HAVE_EBP_AVAILABLE))
++#define HAVE_7REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE && HAVE_EBP_AVAILABLE && !defined(__clang__)))
+ #define HAVE_6REGS (ARCH_X86_64 || (HAVE_EBX_AVAILABLE || HAVE_EBP_AVAILABLE))
+ 
+ #if ARCH_X86_64 && defined(PIC)
