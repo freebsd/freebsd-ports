999fc210ce677e2ebdac035a5376516440326845
Tijl Coosemans <tijl@FreeBSD.org>
2014-03-23 19:58:33 +0000

    - Make Qt4 and Qt5 respect CXXFLAGS during configure so they pick up   -DLIBICONV_PLUG from USES=iconv and always use libc iconv when it is   available. - Remove the iconv dependency from Qt5.  It uses icu instead.
    PR:		ports/186707
    Approved by:	kde (makc)

--- Mk/bsd.qt.mk
+++ Mk/bsd.qt.mk
@@ -166,7 +166,8 @@ CONFIGURE_ARGS+=-verbose
 . endif
 
 . if ${QT_DIST} == "base" || ${_QT_VERSION:M4*}
-EXTRA_PATCHES?=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-configure
+EXTRA_PATCHES?=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-configure \
+		${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-config.tests-unix-compile.test
 .  if ${_QT_VERSION:M5*}
 EXTRA_PATCHES+=	${.CURDIR:H:H}/devel/qt5-core/files/extrapatch-src__corelib__tools__qdatetime.cpp
 .  endif
