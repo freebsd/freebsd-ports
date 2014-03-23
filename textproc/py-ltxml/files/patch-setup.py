fda5c29790b8d949d06e006dedae3b29fde21ca5
nemysis <nemysis@FreeBSD.org>
2014-03-23 02:07:12 +0000

    - Bump PORTREVISION for rerolled tarball - Take maintainership - Change DOCS - Add DOCS and EXAMPLES Options - Remove obsolete REINPLACE - Strip library - Change patch, old was rejected

--- textproc/py-ltxml/files/patch-setup.py
+++ textproc/py-ltxml/files/patch-setup.py
@@ -1,14 +1,22 @@
---- setup.py.orig	Mon Nov  4 16:50:20 2002
-+++ setup.py	Fri Jan 31 12:08:31 2003
-@@ -16,10 +16,7 @@
-     self.install_dir = getattr(install_cmd, 'install_lib')
-     return install_data.run(self)
+--- ./setup.py.orig	2008-01-11 15:26:31.000000000 +0100
++++ ./setup.py	2014-03-23 01:17:23.320011401 +0100
+@@ -6,9 +6,7 @@
+ from distutils.core import setup, Extension
+ from distutils import sysconfig
  
--datafiles=[('PyLTXML/example',
--            ['example/small.xml',
+-datafiles= ['example/small.xml',
 -             'example/gscore.dtd',
--             'example/simple.py'])] # only works for bdist and friends
+-             'example/simple.py'] # only works for bdist and friends
 +datafiles=[]
  
  if sys.platform.find("win32",0,5)==0:
-   incldirs=['C:\Program Files\HCRC LTG\LT XML\include']
+   incldirs=['C:\\Program Files\\msys\\1.0\\include\\ltxml12','C:\\Program Files\\msys\\1.0\\include']
+@@ -32,7 +30,7 @@
+   libs=['ltapi12','ltstd12', 'z']
+   macros=[('FOR_LT', '1')] # ,('ALLOC_DEBUG', '1')
+ 
+-  data_files=[( sysconfig.get_python_lib() + "/PyLTXML/example",
++  data_files=[( sysconfig.get_python_lib() + "/PyLTXML/",
+                 datafiles)]
+   if sys.platform.find("sunos",0,5)==0:
+     extra_link_args=['-mimpure-text']
