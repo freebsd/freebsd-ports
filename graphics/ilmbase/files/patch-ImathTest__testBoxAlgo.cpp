737e8c96095bcee260134043bb09df0f022c0d90
Rusmir Dusko <nemysis@FreeBSD.org>
2014-03-24 17:49:30 +0000

    - Change license BSD to BSD3CLAUSE - Add MAJORVER, VER - Strip libraries - Recreate patches with make makepatch

--- graphics/ilmbase/files/patch-ImathTest__testBoxAlgo.cpp
+++ graphics/ilmbase/files/patch-ImathTest__testBoxAlgo.cpp
@@ -1,5 +1,5 @@
---- ImathTest/testBoxAlgo.cpp.orig	2013-06-18 21:51:38.000000000 +0200
-+++ ImathTest/testBoxAlgo.cpp	2013-10-04 20:10:10.000000000 +0200
+--- ./ImathTest/testBoxAlgo.cpp.orig	2013-06-18 21:51:38.000000000 +0200
++++ ./ImathTest/testBoxAlgo.cpp	2014-01-29 13:16:44.000000000 +0100
 @@ -356,7 +356,7 @@
  	Box3f ()
      };
--- graphics/ilmbase/files/patch-ImathTest__testShear.cpp
+++ graphics/ilmbase/files/patch-ImathTest__testShear.cpp
@@ -1,5 +1,5 @@
---- ImathTest/testShear.cpp.orig	2013-06-18 21:51:38.000000000 +0200
-+++ ImathTest/testShear.cpp	2013-10-04 20:16:08.000000000 +0200
+--- ./ImathTest/testShear.cpp.orig	2013-06-18 21:51:38.000000000 +0200
++++ ./ImathTest/testShear.cpp	2014-01-29 13:16:44.000000000 +0100
 @@ -54,7 +54,6 @@
  
      const float         epsilon = IMATH_INTERNAL_NAMESPACE::limits< float >::epsilon();
