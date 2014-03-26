1c990c8f51eb9079e70f72915985b483f32b1ac3
John Marino <marino@FreeBSD.org>
2014-03-26 17:54:08 +0000

    ocaml update 4/4: graphics/ocaml-images, update to version 4.0.1
    Ocaml 4.00 can't build ocaml-images 3.0.1, so the solution is to update
    the package to a newer version.  This arrived from dports, but it has
    been heavily modified since then.  It has also been staged.  The
    pkg-plist needs work because it doesn't support all the options if they
    are turned off.  GTK2 has been left OFF by default since it pulls in
    136 packages when enabled.
    
    The Ocaml framework did not support staging at the time, so several
    knobs were turned off, especially the "WASH" function which disrupted pkg
    greatly.  The options were cleaned up in a modern way too.  An additional
    patch was addded to make clang happy so this builds on FreeBSD 10 inside
    poudriere.
    
    Urged by: portmgr(bapt)

--- graphics/ocaml-images/files/patch-src_tiffread.c
+++ graphics/ocaml-images/files/patch-src_tiffread.c
@@ -1,11 +1,12 @@
---- src/tiffread.c.orig	2009-10-26 13:42:03.000000000 +0100
-+++ src/tiffread.c	2010-08-28 10:47:42.000000000 +0200
-@@ -20,15 +20,21 @@
- #include <caml/alloc.h>
- #include <caml/memory.h>
- #include <caml/fail.h>
-+#include "oversized.h"
+--- src/tiffread.c.orig	2011-06-22 20:04:32.000000000 +0200
++++ src/tiffread.c
+@@ -23,14 +23,22 @@
  
+ #include "oversized.h"
+ 
++/* Avoid redefinitions */
++#undef int16
++#undef uint16
  /* These are defined in caml/config.h */
  #define int16 int16tiff
  #define uint16 uint16tiff
@@ -22,9 +23,9 @@
  extern value *imglib_error;
  
  value open_tiff_file_for_read( name )
-@@ -64,6 +70,10 @@
-     TIFFGetField(tif, TIFFTAG_YRESOLUTION, &yres);
-     TIFFGetField(tif, TIFFTAG_PHOTOMETRIC, &photometric);
+@@ -70,6 +78,10 @@
+       failwith_oversized("tiff");
+     }
  
 +    if (oversized (imagewidth, imagelength)) {
 +	failwith_oversized("tiff");
