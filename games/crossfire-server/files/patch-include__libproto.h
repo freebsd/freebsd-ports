4fe4baea7f7b0582bed8ede4666f40c2395c1148
nemysis <nemysis@FreeBSD.org>
2014-03-27 23:21:07 +0000

    - Bump PORTREVISION for pkg-plist change - Use same sources for game and maps (tar.gz) - Remove EXTRACR_ONLY - Take maintainership - Add dependency for textproc/libxslt and textproc/sablotron - Use the new format for LIB_DEPENDS - Remove DEPRECATED, EXPIRATION_DATE - Use uidfix instead of BINOWN - USES gmake instead of USE_GMAKE - Support STAGEDIR, add CONFIGURE_ARGS=--localstatedir=/var/games   to work with stage - Remove not needed post-extract - Add DOCS and Option, remove Makefile* that are not in DOCS installation - Change REINPLACE, cosmetical changes and add for perl one - Strip libraries - Fix the usage of 'perl' to get rid of the implicit lang/perl dependency - Fix the usage of 'bash' to get rid of the implicit shells/bash dependency - Fix usage, adjust permission for /var/games/crossfire - Recreate patches with make makepatch - Change pkg-descr - Recreate pkg-plist with make makeplist and proper use @group and @mode

--- games/crossfire-server/files/patch-include__libproto.h
+++ games/crossfire-server/files/patch-include__libproto.h
@@ -1,6 +1,6 @@
---- include/libproto.h.orig	2012-03-18 06:31:46.000000000 +0000
-+++ include/libproto.h
-@@ -441,7 +441,7 @@ extern void yypop_buffer_state(void);
+--- ./include/libproto.h.orig	2012-03-18 07:31:46.000000000 +0100
++++ ./include/libproto.h	2014-03-27 18:59:52.056923524 +0100
+@@ -441,7 +441,7 @@
  extern int yyget_lineno(void);
  extern FILE *yyget_in(void);
  extern FILE *yyget_out(void);
