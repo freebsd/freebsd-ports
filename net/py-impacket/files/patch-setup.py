0edbf66803c1b6fa0f0cb11a0f692c6838ba7822
antoine <antoine@FreeBSD.org>
2014-03-28 21:58:17 +0000

    - Update to 0.9.11 - Apache license 1.1 is now in the framework
    PR:		ports/187973
    Submitted by:	KATO Tsuguru

--- net/py-impacket/files/patch-setup.py
+++ net/py-impacket/files/patch-setup.py
@@ -1,10 +1,11 @@
---- ./setup.py.orig	2013-05-08 13:57:03.000000000 +0000
-+++ ./setup.py	2013-05-08 13:57:03.000000000 +0000
-@@ -21,7 +21,5 @@
+--- ./setup.py.orig	2014-01-30 16:07:52.000000000 +0000
++++ ./setup.py	2014-03-26 22:12:00.000000000 +0000
+@@ -21,8 +21,4 @@
        platforms = ["Unix","Windows"],
-       packages = ['impacket', 'impacket.dcerpc', 'impacket.examples'],
+       packages = ['impacket', 'impacket.dcerpc', 'impacket.examples', 'impacket.dcerpc.v5'],
        scripts = glob.glob(os.path.join('examples', '*.py')),
--      data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
--                     ['README', 'LICENSE']+glob.glob('doc/*'))],
- 
+-      data_files = [(os.path.join('share', 'doc', PACKAGE_NAME), ['README', 'LICENSE']+glob.glob('doc/*')),
+-                    (os.path.join('share', 'doc', PACKAGE_NAME, 'testcases', 'dot11'),glob.glob('impacket/testcases/dot11/*')),
+-                    (os.path.join('share', 'doc', PACKAGE_NAME, 'testcases', 'ImpactPacket'),glob.glob('impacket/testcases/ImpactPacket/*')),
+-                    (os.path.join('share', 'doc', PACKAGE_NAME, 'testcases', 'SMB-RPC'),glob.glob('impacket/testcases/SMB-RPC/*'))],
        )
