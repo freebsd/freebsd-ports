1a90e3d6fb0c10aa8df4ee264a534a34cf76f9cb
Rusmir Dusko <nemysis@FreeBSD.org>
2014-03-30 19:07:09 +0000

    - Don't silence warnings - Remove TODO from DOCS - Add patches, fix bad C++ and GTK code - Change pkg-plist, proper use @group and @mode

--- games/gtkballs/files/patch-src__game.c
+++ games/gtkballs/files/patch-src__game.c
@@ -1,6 +1,46 @@
 --- ./src/game.c.orig	2004-10-06 13:06:46.000000000 +0200
-+++ ./src/game.c	2014-02-01 20:44:28.000000000 +0100
-@@ -495,8 +495,8 @@
++++ ./src/game.c	2014-03-30 20:05:17.338033553 +0200
+@@ -406,7 +406,8 @@
+ 					for(i = 1;
+  					    i < rules_get_width() - x &&
+                                 	    board_get_at_xy(x + i, y) == board_get_at_xy(x, y);
+-	 				    i++);
++	 				    i++)
++	 				    ;
+         				if(i >= rules_get_destroy()) {
+                                                 have_del = 1;
+                         	                for(j = 0; j < i; j ++) {
+@@ -419,7 +420,8 @@
+                                 	for(i = 1;
+ 					    i < rules_get_height() - y &&
+         	                            board_get_at_xy(x, y + i) == board_get_at_xy(x, y);
+-                	                    i++);
++                	                    i++)
++                	                    ;
+                         	        if(i >= rules_get_destroy()) {
+                                                 have_del = 1;
+                                         	for(j = 0; j < i; j ++) {
+@@ -434,7 +436,8 @@
+  					    i < rules_get_width() - x &&
+  					    i < rules_get_height() - y &&
+ 	                                    board_get_at_xy(x + i, y + i) == board_get_at_xy(x, y);
+- 					    i++);
++ 					    i++)
++ 					    ;
+         				if(i >= rules_get_destroy()) {
+                                                 have_del = 1;
+                                 	        for(j = 0; j < i; j ++) {
+@@ -449,7 +452,8 @@
+  					    i <= x &&
+  					    i < rules_get_height() - y &&
+ 	                                    board_get_at_xy(x - i, y + i) == board_get_at_xy(x, y);
+- 					    i++);
++ 					    i++)
++ 					    ;
+         				if(i >= rules_get_destroy()) {
+                                                 have_del = 1;
+                                 	        for(j = 0; j < i; j ++) {
+@@ -495,8 +499,8 @@
                          lock_actions(1);
                          draw_board();
  	                for(animtime = 0, i = 0; i < animcadres;) {
