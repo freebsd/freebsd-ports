521524a0ceefe62b066de5bd40170beb3b8e1315
Steve Wills <swills@FreeBSD.org>
2014-03-31 03:26:38 +0000

    - Fix STR2CSTR usage [1] - While here, stage.
    PR:		ports/188079 [1]
    Submitted by:	Christoph Moench-Tegeder <cmt@burggraben.net> [1]

--- databases/ruby-qdbm/files/patch-curia-mod_curia.c
+++ databases/ruby-qdbm/files/patch-curia-mod_curia.c
@@ -1,40 +1,58 @@
---- curia/mod_curia.c.orig	2011-07-24 16:00:48.000000000 +0000
-+++ curia/mod_curia.c	2011-07-24 16:02:05.000000000 +0000
-@@ -277,9 +277,9 @@
+--- curia/mod_curia.c.orig	2014-03-29 18:35:17.000000000 +0100
++++ curia/mod_curia.c	2014-03-29 18:37:22.000000000 +0100
+@@ -233,7 +233,7 @@
+   const char *name;
+   int index, omode, bnum, dnum;
+   if((index = getnewindex()) == -1) myerror(DP_EMISC);
+-  name = STR2CSTR(vname);
++  name = StringValuePtr(vname);
+   FIXNUM_P(vomode);
+   omode = FIX2INT(vomode);
+   FIXNUM_P(vbnum);
+@@ -276,10 +276,10 @@
+   int index, ksiz, vsiz, dmode;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
-   kbuf = STR2CSTR(vkey);
+-  kbuf = STR2CSTR(vkey);
 -  ksiz = RSTRING(vkey)->len;
-+  ksiz = RSTRING_LEN(vkey);
-   vbuf = STR2CSTR(vval);
+-  vbuf = STR2CSTR(vval);
 -  vsiz = RSTRING(vval)->len;
++  kbuf = StringValuePtr(vkey);
++  ksiz = RSTRING_LEN(vkey);
++  vbuf = StringValuePtr(vval);
 +  vsiz = RSTRING_LEN(vval);
    FIXNUM_P(vdmode);
    dmode = FIX2INT(vdmode);
    curia = crtable[index];
-@@ -298,7 +298,7 @@
+@@ -297,8 +297,8 @@
+   int index, ksiz;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
-   kbuf = STR2CSTR(vkey);
+-  kbuf = STR2CSTR(vkey);
 -  ksiz = RSTRING(vkey)->len;
++  kbuf = StringValuePtr(vkey);
 +  ksiz = RSTRING_LEN(vkey);
    curia = crtable[index];
    if(!crout(curia, kbuf, ksiz)){
      if(crsltable[index] && dpecode == DP_ENOITEM) return Qfalse;
-@@ -317,7 +317,7 @@
+@@ -316,8 +316,8 @@
+   VALUE vval;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
-   kbuf = STR2CSTR(vkey);
+-  kbuf = STR2CSTR(vkey);
 -  ksiz = RSTRING(vkey)->len;
++  kbuf = StringValuePtr(vkey);
 +  ksiz = RSTRING_LEN(vkey);
    FIXNUM_P(vstart);
    start = FIX2INT(vstart);
    FIXNUM_P(vmax);
-@@ -340,7 +340,7 @@
+@@ -339,8 +339,8 @@
+   int index, ksiz, vsiz;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
-   kbuf = STR2CSTR(vkey);
+-  kbuf = STR2CSTR(vkey);
 -  ksiz = RSTRING(vkey)->len;
++  kbuf = StringValuePtr(vkey);
 +  ksiz = RSTRING_LEN(vkey);
    curia = crtable[index];
    if((vsiz = crvsiz(curia, kbuf, ksiz)) == -1){
