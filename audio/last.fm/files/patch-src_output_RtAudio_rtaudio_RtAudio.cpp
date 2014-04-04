fbbdea1765d78d46010c58da12e06972546c9f80
John Marino <marino@FreeBSD.org>
2014-04-04 09:27:09 +0000

    audio/last.fm: consolidate patches
    Due to strange naming scheme of original patch, I didn't immediately
    realize the same file now has two patches.  Consolidate them and keep
    the new patch file name.

--- audio/last.fm/files/patch-src_output_RtAudio_rtaudio_RtAudio.cpp
+++ audio/last.fm/files/patch-src_output_RtAudio_rtaudio_RtAudio.cpp
@@ -1,5 +1,14 @@
---- src/output/RtAudio/rtaudio/RtAudio.cpp.intermediate	2014-04-04 08:59:58.181014000 +0000
+--- src/output/RtAudio/rtaudio/RtAudio.cpp.orig	2014-04-04 08:59:58.151014000 +0000
 +++ src/output/RtAudio/rtaudio/RtAudio.cpp
+@@ -42,7 +42,7 @@
+ #include <QtGlobal>
+ 
+ #ifdef Q_WS_X11
+-    #define __LINUX_ALSA__
++    #define __LINUX_OSS__
+ #endif
+ #ifdef Q_WS_FREEBSD
+     #define __LINUX_OSS__
 @@ -57,6 +57,7 @@
  #include "RtAudio.h"
  #include <iostream>
