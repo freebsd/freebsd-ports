bbe944514370d87e4579121626b8df2b1a993ce0
Boris Samorodov <bsam@FreeBSD.org>
2014-04-05 16:23:57 +0000

    print/cups-filters: update to version 1.0.50.
    This should fix a reported regression (I managed to reproduce
    it with HP laser printers) when a job is printed but remains
    in a job query.
    
    Reported by:	Dr. Peter Voigt <pvoigt@uos.de>
    Tested by:	Dr. Peter Voigt <pvoigt@uos.de>
    Discussed at:	freebsd-ports@
    MFH:		2014Q2

--- print/cups-filters/files/patch-utils_cups-browsed.c
+++ print/cups-filters/files/patch-utils_cups-browsed.c
@@ -1,13 +1,15 @@
---- utils/cups-browsed.c.orig  2013-12-27 20:45:55.188455147 +0400
-+++ utils/cups-browsed.c   2013-12-27 20:46:23.397446686 +0400
-@@ -23,7 +23,6 @@
+--- utils/cups-browsed.c.orig	2014-04-05 16:31:42.635763683 +0400
++++ utils/cups-browsed.c	2014-04-05 16:37:07.384740688 +0400
+@@ -23,8 +23,7 @@
  
  #include <ctype.h>
  #include <errno.h>
 -#include <ifaddrs.h>
- #if defined(__OpenBSD__)
+-#if defined(__OpenBSD__)
++#if defined(__OpenBSD__) || defined(__FreeBSD__)
  #include <sys/socket.h>
  #endif /* __OpenBSD__ */
+ #include <net/if.h>
 @@ -33,6 +32,7 @@
  #include <stdio.h>
  #include <sys/types.h>
