2a8826c2a1eb2e8f3c150f407ab60b9484f84259
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2014-04-06 05:51:57 +0000

    - Update to 1c3 - Add LICENSE - Support STAGEDIR - While I'm here, pet stage-qa: fix STAGEDIR-prefix
    Changes:	http://cvs.savannah.gnu.org/viewvc/*checkout*/jacal/jacal/ChangeLog
    PR:		188157
    Submitted by:	Muhammad Moinur Rahman <5u623l20@gmail.com> (maintainer)

--- math/jacal/files/patch-jacal.sh
+++ math/jacal/files/patch-jacal.sh
@@ -1,11 +1,11 @@
 --- jacal.sh.orig	Wed Jun 22 03:52:36 2005
 +++ jacal.sh	Mon Jul 11 08:02:25 2005
-@@ -98,6 +98,8 @@
- 	  SCHEME_LIBRARY_PATH=/usr/local/lib/slib/
-        elif [ -d /usr/share/slib/ ]; then
- 	  SCHEME_LIBRARY_PATH=/usr/share/slib/
-+       elif [ -d %%PREFIX%%/lib/slib/ ]; then
-+ 	  export SCHEME_LIBRARY_PATH=%%PREFIX%%/lib/slib/
-        fi
-      export SCHEME_LIBRARY_PATH
-      fi;;
+@@ -107,6 +107,8 @@
+ 		SCHEME_LIBRARY_PATH=/usr/local/lib/slib/
+ 	    elif [ -d /usr/share/slib/ ]; then
+ 		SCHEME_LIBRARY_PATH=/usr/share/slib/
++	    elif [ -d %%PREFIX%%/lib/slib/ ]; then
++		export SCHEME_LIBRARY_PATH=%%PREFIX%%/lib/slib/
+ 	    fi
+ 	    export SCHEME_LIBRARY_PATH
+ 	fi;;
