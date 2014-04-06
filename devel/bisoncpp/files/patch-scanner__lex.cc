53fe402f20f36aebd20529194f8d1f213a39d998
sunpoet <sunpoet@FreeBSD.org>
2014-04-06 05:49:34 +0000

    - Update to 4.08.00 - Pass maintainership to submitter - While I'm here, relax USE_GCC to yes
    Changes:	http://sourceforge.net/projects/bisoncpp/files/bisonc%2B%2B/4.08.00/README/view
    		http://sourceforge.net/projects/bisoncpp/files/bisonc%2B%2B/4.07.00/README/view
    		http://sourceforge.net/projects/bisoncpp/files/bisonc%2B%2B/4.06.00/README/view
    PR:		188267
    Submitted by:	Muhammad Moinur Rahman <5u623l20@gmail.com>

--- devel/bisoncpp/files/patch-scanner__lex.cc
+++ devel/bisoncpp/files/patch-scanner__lex.cc
@@ -1,11 +1,11 @@
---- ./scanner/lex.cc.orig	2013-08-10 10:17:17.000000000 +0200
-+++ ./scanner/lex.cc	2013-11-27 19:14:33.000000000 +0100
-@@ -2836,7 +2836,7 @@
+--- scanner/lex.cc.orig	2014-04-04 14:46:17.624123806 +0600
++++ scanner/lex.cc	2014-04-04 14:46:39.301107135 +0600
+@@ -2702,7 +2702,7 @@
          {
- #line 192 "lexer"
+ #line 193 "lexer"
              {
--    d_number = stoul(d_matched);
-+    d_number = A2x(d_matched);
-     return Parser::NUMBER;
- }
- 
+-                d_number = stoul(d_matched);
++                d_number = A2x(d_matched);
+                 return Parser::NUMBER;
+             }
+         }
