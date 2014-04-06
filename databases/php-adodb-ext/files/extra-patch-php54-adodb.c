eab3d37c7f81bc01fd8eb6f2363766243cdefc9b
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2014-04-06 05:49:11 +0000

    - Allow build with php55 - Remove DEFAULT_PHP_VER - Bump PORTREVISION for dependency change - While I'm here:   - Use USE_PHPEXT and PHP_MODNAME   - Remove unnecessary CONFIGURE_ARGS   - Simplify Makefile: move common part of two extra patches to patch-adodb.c   - Use USES=dos2unix   - Support STAGEDIR   - Use single space after WWW:
    PR:		ports/188236
    Submitted by:	Joe Horn <joehorn@gmail.com> (maintainer)

--- databases/php-adodb-ext/files/extra-patch-php54-adodb.c
+++ databases/php-adodb-ext/files/extra-patch-php54-adodb.c
@@ -9,29 +9,3 @@
     	PHP_FE(adodb_movenext,NULL) 
  	PHP_FE(adodb_getall,NULL)   
  	{NULL, NULL, NULL} 
-@@ -159,11 +159,11 @@
- 
- static void adodb_init_zval(zval *v, char *s)
- {
--	v->type = IS_STRING;
--	v->value.str.val = s;
--	v->value.str.len = strlen(s);
--	v->is_ref = 0;
--	v->refcount = 1;
-+	Z_TYPE_P(v) = IS_STRING;
-+	Z_STRVAL_P(v) = s;
-+	Z_STRLEN_P(v) = strlen(s);
-+	Z_UNSET_ISREF_P(v);
-+	Z_SET_REFCOUNT_P(v, 1);
- }
- 
- /* {{{ PHP_MINIT_FUNCTION
-@@ -277,7 +277,7 @@
- 	fci.function_table = EG(function_table);
- 	fci.function_name = &adodb_zvals[zval_ocifetch];
- 	fci.symbol_table = NULL;
--	fci.object_pp = NULL;
-+	fci.object_ptr = NULL;
- 	fci.retval_ptr_ptr = &retval;
- 	fci.param_count = 2;
- 	fci.params = &params[1];
