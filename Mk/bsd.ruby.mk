b5bcc68987a69415173f6916a4a519854473bcdf
bapt <bapt@FreeBSD.org>
2014-04-09 09:25:29 +0000

    Remove support for not staged ports
    Approved by:	ruby@ (swills)

--- Mk/bsd.ruby.mk
+++ Mk/bsd.ruby.mk
@@ -463,12 +463,8 @@ GEMFILES=	${DISTFILES:C/:[^:]+$//}
 GEMFILES=	${DISTNAME}${EXTRACT_SUFX}
 . endif
 
-.if defined(NO_STAGE)
-RUBYGEM_ARGS=-l --no-update-sources --no-ri --install-dir ${PREFIX}/lib/ruby/gems/${RUBY_VER}
-.else
 RUBYGEM_ARGS=-l --no-update-sources --no-ri --install-dir ${STAGEDIR}${PREFIX}/lib/ruby/gems/${RUBY_VER} --ignore-dependencies --bindir=${STAGEDIR}${PREFIX}/bin
 GEM_ENV+=	RB_USER_INSTALL=yes
-.endif
 .if defined(NOPORTDOCS)
 RUBYGEM_ARGS+=	--no-rdoc
 .endif
@@ -567,13 +563,8 @@ do-install:	ruby-setup-install
 
 ruby-setup-install:
 	@${ECHO_MSG} "===>  Running ${RUBY_SETUP} to install"
-.  if defined(NO_STAGE)
-	@cd ${INSTALL_WRKSRC}; \
-	${SETENV} ${MAKE_ENV} ${RUBY} ${RUBY_FLAGS} ${RUBY_SETUP} install
-.  else
 	@cd ${INSTALL_WRKSRC}; \
 	${SETENV} ${MAKE_ENV} ${RUBY} ${RUBY_FLAGS} ${RUBY_SETUP} install --prefix=${STAGEDIR}
-.  endif
 .endif
 
 .if defined(USE_LIBRUBY)
