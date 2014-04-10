03bbb043b2e63b2c9eae4e7094b0919c41a5bb10
Jimmy Olgeni <olgeni@FreeBSD.org>
2014-04-10 08:57:56 +0000

    Add lang/erlang-runtime17, an Erlang runtime for version 17.

---
+++ lang/erlang-runtime17/files/patch-erts_etc_unix_run__erl.c
@@ -0,0 +1,15 @@
+
+$FreeBSD$
+
+--- erts/etc/unix/run_erl.c.orig
++++ erts/etc/unix/run_erl.c
+@@ -69,9 +69,6 @@
+ #ifdef HAVE_UTMP_H
+ #  include <utmp.h>
+ #endif
+-#ifdef HAVE_UTIL_H
+-#  include <util.h>
+-#endif
+ #ifdef HAVE_SYS_IOCTL_H
+ #  include <sys/ioctl.h>
+ #endif
