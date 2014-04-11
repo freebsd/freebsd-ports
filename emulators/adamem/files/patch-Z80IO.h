ea07e760e62a0ecc2792386f9ed701ae673f4add
ak <ak@FreeBSD.org>
2014-04-11 12:51:21 +0000

    - Fix plist and build on i386

--- emulators/adamem/files/patch-Z80IO.h
+++ emulators/adamem/files/patch-Z80IO.h
@@ -7,3 +7,17 @@ Index: Z80IO.h
  
  #ifdef __GNUC__
  #define FASTCALL        __attribute__ ((regparm(3)))
+@@ -36,11 +35,11 @@
+  /***************************************************************************/
+  /* Read a byte from given memory location                                  */
+  /***************************************************************************/
+- unsigned Z80_RDMEM(dword a) FASTCALL;
++ unsigned Z80_RDMEM(dword a);
+  /***************************************************************************/
+  /* Write a byte to given memory location                                   */
+  /***************************************************************************/
+- void Z80_WRMEM(dword a,byte v) FASTCALL;
++ void Z80_WRMEM(dword a,byte v);
+ #else
+  extern byte *AddrTabl[256];
+  extern byte *WriteAddrTabl[256];
