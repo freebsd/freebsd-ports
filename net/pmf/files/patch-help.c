c5e857d4b8cdd5c0a3872b6b7cd99c8519e4fffe
Antoine Brodin <antoine@FreeBSD.org>
2014-04-12 21:54:53 +0000

    Fix make patch, some patches were applied twice
    Reported by:	pkg-fallout

--- net/pmf/files/patch-help.c
+++ net/pmf/files/patch-help.c
@@ -13,18 +13,3 @@
  char *orig_arg;
  {
      char *arg, *lc = NULL, *try2 = NULL, *try3 = NULL, *try4 = NULL;
---- help.c.orig
-+++ help.c
-@@ -16,10 +16,11 @@
- #include "globals.h"
- 
- extern char compile_time[], version[];
-+extern void cmd_help();
- 
- /*---------------------------------------------------------------------------*/
- 
--cmd_help(orig_arg)
-+void cmd_help(orig_arg)
- char *orig_arg;
- {
-     char *arg, *lc = NULL, *try2 = NULL, *try3 = NULL, *try4 = NULL;
