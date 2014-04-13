b018584ae229b97aa22e8693faa771e2ee92c3ac
Greg Lewis <glewis@FreeBSD.org>
2014-04-13 17:39:57 +0000

    . Don't try to represent a unicode literal in a character literal.  In this   case just use the decimal literal. . Add $FreeBSD$.

--- x11-wm/afterstep-stable/files/patch-src-ASDocGen-xmlproc.c
+++ x11-wm/afterstep-stable/files/patch-src-ASDocGen-xmlproc.c
@@ -1,6 +1,17 @@
---- src/ASDocGen/xmlproc.c.orig	2008-09-15 15:54:38.000000000 +0000
-+++ src/ASDocGen/xmlproc.c	2008-09-15 16:00:20.000000000 +0000
-@@ -504,9 +504,10 @@
+$FreeBSD$
+
+--- src/ASDocGen/xmlproc.c.orig	2013-05-01 06:34:11.000000000 -0700
++++ src/ASDocGen/xmlproc.c	2014-04-13 10:29:12.000000000 -0700
+@@ -179,7 +179,7 @@
+ 				fputs( "\\&", state->dest_fp );	
+ 			else if ( c == '\\' && state->doc_type == DocType_NROFF ) 
+ 				fputc( '\\', state->dest_fp );	
+-			else if ( c == '' && state->doc_type == DocType_NROFF ) 
++			else if ( c == 152 /*''*/ && state->doc_type == DocType_NROFF ) 
+ 				c = '~';
+ 			fputc( c, state->dest_fp );
+ 		}		   
+@@ -514,9 +514,10 @@
  	 	char *target = NULL, *target2 ;
  		char *term = NULL, *term2 ;
  		char *ptr = &(state->dest_file[strlen(state->dest_file)-4]);
