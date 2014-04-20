21c7eb29520ac1b037a61fad06cc3f6deb9d8fec
Kubilay Kocak <koobs@FreeBSD.org>
2014-04-20 01:35:39 +0000

    lang/python{31,32,33}: Add clang 3.4 fix from python27
    Copy change committed to python27 [1] to python31, python32 and
    python33 to fix builds of some extensions with Clang 3.4.
    
    Also add breadcrumbs to the patch header in lang/python27 referencing
    the upstream issue. [2]
    
    The Python 3.4 port (lang/python34) already carries the patch.
    
    [1] https://svnweb.freebsd.org/ports?view=revision&revision=346428
    [2] http://bugs.python.org/issue20767

--- lang/python31/files/patch-Lib__distutils__unixccompiler.py
+++ lang/python31/files/patch-Lib__distutils__unixccompiler.py
@@ -2,9 +2,9 @@
 # Issue ID: http://bugs.python.org/issue20767
 # Submitted by: antoine
 
---- ./Lib/distutils/unixccompiler.py.orig      2013-11-10 07:36:40.000000000 +0000
-+++ ./Lib/distutils/unixccompiler.py   2014-02-19 15:41:48.000000000 +0000
-@@ -228,6 +228,8 @@
+--- ./Lib/distutils/unixccompiler.py.orig	2014-03-09 14:58:11.840899942 +1100
++++ ./Lib/distutils/unixccompiler.py	2014-03-09 15:07:24.038743110 +1100
+@@ -285,6 +285,8 @@
          if sys.platform[:6] == "darwin":
              # MacOSX's linker doesn't understand the -R flag at all
              return "-L" + dir
--- lang/python33/files/patch-Lib__distutils__unixccompiler.py
+++ lang/python33/files/patch-Lib__distutils__unixccompiler.py
@@ -2,9 +2,9 @@
 # Issue ID: http://bugs.python.org/issue20767
 # Submitted by: antoine
 
---- ./Lib/distutils/unixccompiler.py.orig      2013-11-10 07:36:40.000000000 +0000
-+++ ./Lib/distutils/unixccompiler.py   2014-02-19 15:41:48.000000000 +0000
-@@ -228,6 +228,8 @@
+--- ./Lib/distutils/unixccompiler.py.orig	2014-03-09 15:25:15.921204441 +1100
++++ ./Lib/distutils/unixccompiler.py	2014-03-09 15:25:50.643809649 +1100
+@@ -225,6 +225,8 @@
          if sys.platform[:6] == "darwin":
              # MacOSX's linker doesn't understand the -R flag at all
              return "-L" + dir
