e87f135c704d3126bcb1d32f94963e83ea1caafd
Bernhard Froehlich <decke@FreeBSD.org>
2014-04-22 12:29:45 +0000

    - Fix building with clang 3.4 on head
    Reported by:	pkg-fallout

---
+++ multimedia/tvheadend/files/patch-src__tvhtime.h
@@ -0,0 +1,11 @@
+--- src/tvhtime.h.orig	2013-07-26 16:17:22.000000000 +0000
++++ src/tvhtime.h	2014-04-22 12:26:51.000000000 +0000
+@@ -18,7 +18,7 @@
+  */
+ 
+ #ifndef __TVH_TIME_H__
+-#define __TVH_TIME_H_
++#define __TVH_TIME_H__
+ 
+ extern uint32_t tvhtime_update_enabled;
+ extern uint32_t tvhtime_ntp_enabled;
