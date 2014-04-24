e7fbb4600d4cb4d895e7b4f92b70a08d8cce889a
David Naylor <dbn@FreeBSD.org>
2014-04-24 20:04:48 +0000

    Update x11-toolkits/py-kivy to 1.8.0.
    Changes:
     * Kivy now supports python 2 and 3
    
    ChangeLog:
    - Python 3.3 support: It's finally here, using the same codebase! And
      we deliver also 3 portables for Windows, one that package Python 2.7
      and another with Python 3.3!
    - New media player named GstPlayer, based on Gstreamer 1.0. This
      player is currently used as a implementation for Audio and Video
      core provider. This allow us to not only use the latest Gstreamer
      and all the improvements done by them, but also have a common player
      for both Python 2.7 and 3.3 on desktop platforms.
    - Scrollview has been slightly enhanced to support smoother scrolling
      utilizing Matrix transformation by default. It also now supports
      scrolling through bars or content or both.
    - Widgets now can be disabled simply by using `disabled` property.
    - Textinput now has support for selection handles.
    - New Widgets!
      - ActionBar: Mimics Android?s own ActionBar appearance and
        mechanisms
      - Behaviors: ButtonBehavior, ToggleButtonBehavior, DragBehavior,
        just subclass a behavior to an existing widgets, and you get the
        behavior! (eg. class ImageButton(ButtonBehavior, Image))
      - PageLayout: A simple multi-page layout allowing flipping through
        pages using borders
      - SandBox (experimental): Runs itself and its children in an
        exception-catching sandbox

--- x11-toolkits/py-kivy/files/patch-setup.py
+++ x11-toolkits/py-kivy/files/patch-setup.py
@@ -1,46 +1,50 @@
---- setup.py.orig	2013-05-28 15:08:56.000000000 +0200
-+++ setup.py	2013-11-18 20:41:50.000000000 +0200
-@@ -6,9 +6,10 @@
- import sys
- from copy import deepcopy
- from os.path import join, dirname, sep, exists
--from os import walk, environ
-+from os import walk, environ, path
+--- setup.py
++++ setup.py
+@@ -12,6 +12,7 @@ from os import walk, environ
  from distutils.core import setup
  from distutils.extension import Extension
+ from collections import OrderedDict
 +from subprocess import check_output
  
+ if sys.version > '3':
+     PY3 = True
+@@ -36,7 +37,6 @@ def pkgconfig(*packages, **kw):
+         kw.setdefault(flag, []).append(token[2:].decode('utf-8'))
+     return kw
+ 
+-
  # -----------------------------------------------------------------------------
  # Determine on which platform we are
-@@ -124,7 +125,7 @@
+ 
+@@ -178,7 +178,7 @@ elif platform == 'win32':
      c_options['use_glew'] = True
  else:
      # searching GLES headers
 -    default_header_dirs = ['/usr/include', '/usr/local/include']
-+    default_header_dirs = ['/usr/include', path.join(environ.get('LOCALBASE', '/usr/local'), 'include')]
++    default_header_dirs = ['/usr/include', join(environ.get('LOCALBASE', '/usr/local'), 'include')]
      found = False
      for hdir in default_header_dirs:
          filename = join(hdir, 'GLES2', 'gl2.h')
-@@ -201,6 +202,9 @@
-         sysroot = '/System/Library/Frameworks/ApplicationServices.framework/Frameworks'
+@@ -283,6 +283,9 @@ def determine_base_flags():
+                        'ApplicationServices.framework/Frameworks')
          flags['extra_compile_args'] += ['-F%s' % sysroot]
          flags['extra_link_args'] += ['-F%s' % sysroot]
 +    elif platform.startswith('freebsd'):
-+        flags['include_dirs'] += [path.join(environ.get('LOCALBASE', '/usr/local'), 'include')]
-+        flags['extra_link_args'] += ['-L', path.join(environ.get('LOCALBASE', '/usr/local'), 'lib')]
++        flags['include_dirs'] += [join(environ.get('LOCALBASE', '/usr/local'), 'include')]
++        flags['extra_link_args'] += ['-L', join(environ.get('LOCALBASE', '/usr/local'), 'lib')]
      return flags
  
- def determine_gl_flags():
-@@ -214,8 +218,6 @@
-         flags['extra_link_args'] = ['-framework', 'OpenGL', '-arch', 'x86_64']
-         flags['extra_compile_args'] = ['-arch', 'x86_64']
+ 
+@@ -297,8 +300,6 @@ def determine_gl_flags():
+         flags['extra_link_args'] = ['-framework', 'OpenGL', '-arch', osx_arch]
+         flags['extra_compile_args'] = ['-arch', osx_arch]
      elif platform.startswith('freebsd'):
 -        flags['include_dirs'] = ['/usr/local/include']
 -        flags['extra_link_args'] = ['-L', '/usr/local/lib']
          flags['libraries'] = ['GL']
      elif platform.startswith('openbsd'):
          flags['include_dirs'] = ['/usr/X11R6/include']
-@@ -283,6 +285,11 @@
+@@ -367,6 +368,11 @@ def determine_sdl():
      elif platform == 'darwin':
          flags['extra_link_args'] += [
              '-framework', 'ApplicationServices']
@@ -51,19 +55,4 @@
 +        flags['libraries'] = ["SDL2_ttf", "SDL2_mixer"]
      return flags
  
- def determine_graphics_pxd():
-@@ -491,13 +498,7 @@
-         'data/glsl/*.fs',
-         'tools/highlight/*.vim',
-         'tools/highlight/*.el',
--        'tools/packaging/README.txt',
--        'tools/packaging/win32/kivy.bat',
--        'tools/packaging/win32/kivyenv.sh',
--        'tools/packaging/win32/README.txt',
--        'tools/packaging/osx/Info.plist',
--        'tools/packaging/osx/InfoPlist.strings',
--        'tools/packaging/osx/kivy.sh']},
-+        'tools/packaging/README.txt']},
-     data_files=examples.items(),
-     classifiers=[
-         'Development Status :: 5 - Production/Stable',
+ 
