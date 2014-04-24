ac643c6a5b34c0697752fb36008d3bd8168f72bd
Kris Moore <kris@pcbsd.org>
2014-04-24 11:23:40 -0400

    Update Cinnamon from marcuscom repo

---
+++ x11/cinnamon/files/patch-src_cinnamon-global.c
@@ -0,0 +1,13 @@
+--- src/cinnamon-global.c.orig	2014-04-04 09:39:11.488900078 +0000
++++ src/cinnamon-global.c	2014-04-04 09:39:20.117899806 +0000
+@@ -477,8 +477,8 @@
+                                                         G_PARAM_READABLE));
+ }
+ 
+-/**•
+- * _cinnamon_global_init: (skip)•
++/**
++ * _cinnamon_global_init: (skip)
+  * @first_property_name: the name of the first property
+  * @...: the value of the first property, followed optionally by more
+  *  name/value pairs, followed by %NULL
