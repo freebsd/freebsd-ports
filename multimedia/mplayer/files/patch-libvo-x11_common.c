fd39b1ac01f5235aa5a3416c508455c5993fdc3e
Thomas Zander <riggs@FreeBSD.org>
2014-04-27 14:22:25 +0000

    - Sync with upstream snapshot of 2014-04-18 - General Makefile cleanup - Fix x11 dependency handling (xproto was missing) - Builds with LOCALBASE != PREFIX should work everywhere now - Remove support for esd unconditionally - mplayer: default build enables fribidi option, so smplayer and smtube   packages from official FreeBSD package repos will work out of the box - Add profiling option (disabled by default) - Remove WITH_LANG knob (was unused for a long time anyway) - Chase libbluray shared lib version bump from ports/188573
    Approved by:	thierry (mentor)

--- multimedia/mplayer/files/patch-libvo-x11_common.c
+++ multimedia/mplayer/files/patch-libvo-x11_common.c
@@ -1,10 +1,11 @@
---- libvo/x11_common.c	2013-10-16 17:07:12.000000000 -0700
-+++ libvo/x11_common.c	2014-04-16 09:15:27.888658589 -0700
+--- libvo/x11_common.c.orig	2014-02-08 21:39:02.000000000 +0100
++++ libvo/x11_common.c	2014-04-19 14:43:53.715749607 +0200
 @@ -430,7 +430,7 @@
      }
  
      // Required so that XLookupString returns UTF-8
 -    if (!setlocale(LC_CTYPE, "C.UTF-8") && !setlocale(LC_CTYPE, "en_US.utf8"))
 +    if (!setlocale(LC_CTYPE, "en_US.UTF-8"))
-         mp_msg(MSGT_VO, MSGL_WARN, "Could not find a UTF-8 locale, some keys will not be handled.\n");
+         mp_msg(MSGT_VO, MSGL_WARN, MSGTR_CouldntFindUTF8Locale);
      XSetErrorHandler(x11_errorhandler);
+ 
