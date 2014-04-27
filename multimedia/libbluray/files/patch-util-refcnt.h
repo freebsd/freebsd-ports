85d711de42bb2c23fd80d4eab7a8674caaa243ee
Thomas Zander <riggs@FreeBSD.org>
2014-04-27 14:14:26 +0000

    - Update libaacs to 0.7.0   - Add LICENSE   - USES tar:bzip2 - Update libbluray 0.5.0   - Unbreak JAVA option   - USES tar:bzip2 - Bump PORTREVISION in depdendent ports due to shared lib version bump
    PR:		ports/188573
    Submitted by:	Dominic Fandrey <kamikaze@bsdforen.de> (maintainer)
    Approved by:	thierry (mentor)

---
+++ multimedia/libbluray/files/patch-util-refcnt.h
@@ -0,0 +1,11 @@
+--- src/util/refcnt.h.orig	2014-04-13 17:52:33.000000000 +0200
++++ src/util/refcnt.h	2014-04-13 17:52:43.000000000 +0200
+@@ -18,7 +18,7 @@
+  */
+ 
+ #ifndef BD_REFCNT_H_
+-#define BD_FEFCNT_H_
++#define BD_REFCNT_H_
+ 
+ #include "attributes.h"
+ 
