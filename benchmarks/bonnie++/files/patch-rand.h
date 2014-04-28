b8847fc9851f363736017082cd58f3b0ee4eea6a
martymac <martymac@FreeBSD.org>
2014-04-28 11:39:52 +0000

    - Remove GCC dependency - Fix several compilation warnings
    PR:		ports/188678
    Submitted by:	Matthew Rezny <matthew@reztek.cz>

---
+++ benchmarks/bonnie++/files/patch-rand.h
@@ -0,0 +1,15 @@
+--- rand.h.orig 2009-08-21 18:45:50.068536643 +0000
++++ rand.h      2009-08-21 18:45:58.564755017 +0000
+@@ -1,11 +1,11 @@
+ #ifndef RAND_H
+ #define RAND_H
+ 
+-using namespace std;
+ #include "port.h"
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string>
++using namespace std;
+ 
+ class Rand
+ {
