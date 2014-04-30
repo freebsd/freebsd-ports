d66f9b27cad29ccc4238caeb3836586c7de20210
cy <cy@FreeBSD.org>
2014-04-30 04:22:34 +0000

    Update screen 4.0.3 --> 4.2.1.
    Put final touches onto screen-legacy.

--- sysutils/screen/files/patch-os.h
+++ sysutils/screen/files/patch-os.h
@@ -1,15 +1,6 @@
---- os.h.orig	Tue Jan  8 07:42:33 2002
-+++ os.h	Tue Sep  9 17:39:58 2003
-@@ -161,7 +161,7 @@
- # define setregid(rgid, egid) setresgid(rgid, egid, -1)
- #endif
- 
--#if defined(HAVE_SETEUID) || defined(HAVE_SETREUID)
-+#if (defined(HAVE_SETEUID) || defined(HAVE_SETREUID)) && __FreeBSD_version < 500000
- # define USE_SETEUID
- #endif
- 
-@@ -262,7 +262,11 @@
+--- os.h.orig	2014-04-26 03:58:35.000000000 -0700
++++ os.h	2014-04-29 19:16:31.730773575 -0700
+@@ -252,7 +252,11 @@
  #if defined(UTMPOK) || defined(BUGGYGETLOGIN)
  # if defined(SVR4) && !defined(DGUX) && !defined(__hpux) && !defined(linux)
  #  include <utmpx.h>
@@ -22,7 +13,7 @@
  #  define utmp		utmpx
  #  define getutent	getutxent
  #  define getutid	getutxid
-@@ -270,7 +274,7 @@
+@@ -260,7 +264,7 @@
  #  define pututline	pututxline
  #  define setutent	setutxent
  #  define endutent	endutxent
@@ -31,3 +22,11 @@
  # else /* SVR4 */
  #  include <utmp.h>
  # endif /* SVR4 */
+@@ -524,6 +528,6 @@
+ /* Changing those you won't be able to attach to your old sessions
+  * when changing those values in official tree don't forget to bump
+  * MSG_VERSION */
+-#define MAXTERMLEN	32
++#define MAXTERMLEN	63
+ #define MAXLOGINLEN	256
+ 
