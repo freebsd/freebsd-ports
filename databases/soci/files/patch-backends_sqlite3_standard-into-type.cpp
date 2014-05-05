f4f46a920e21b409b3f3b16873c6cef1eb4af378
Baptiste Daroussin <bapt@FreeBSD.org>
2014-05-05 16:24:00 +0000

    Fix unixODBC vs libiodbc conflicts
    Always rely on unixODBC each time a port is looking for libodbc.so
    Remove odbc compat from libiodbc
    This allows to install both kde and gnome at the same time
    
    While here:
    - Convert libiodbc to USES=libtool
    - Convert a bunch of libiodbc dependencies to USES=libtool
    - Chase libiodbc.so shlib change
    - Stagify some ports
    - Convert some ports to USES=pgsql
    
    Discussed with:	rakuco (kde)
    With hat:	portmgr

--- databases/soci/files/patch-backends_sqlite3_standard-into-type.cpp
+++ databases/soci/files/patch-backends_sqlite3_standard-into-type.cpp
@@ -1,26 +1,20 @@
---- backends/sqlite3/standard-into-type.cpp.orig	2012-06-19 19:12:10.168809305 +0300
-+++ backends/sqlite3/standard-into-type.cpp	2012-06-19 19:22:12.580359214 +0300
-@@ -113,7 +113,11 @@ void sqlite3_standard_into_type_backend:
+--- ./backends/sqlite3/standard-into-type.cpp.orig	2013-08-22 00:40:11.000000000 +0200
++++ ./backends/sqlite3/standard-into-type.cpp	2014-05-05 16:45:06.981013401 +0200
+@@ -107,7 +107,7 @@
          case x_long_long:
              {
                  long long* dest = static_cast<long long*>(data_);
-+#ifdef __FreeBSD__
+-                *dest = std::strtoll(buf, NULL, 10);
 +                *dest = strtoq(buf, NULL, 10);
-+#else
-                 *dest = strtoll(buf, NULL, 10);
-+#endif
              }
              break;
          case x_unsigned_long_long:
-@@ -142,7 +146,11 @@ void sqlite3_standard_into_type_backend:
+@@ -136,7 +136,7 @@
  
                  rowid *rid = static_cast<rowid *>(data_);
                  sqlite3_rowid_backend *rbe = static_cast<sqlite3_rowid_backend *>(rid->get_backend());
-+#ifdef __FreeBSD__
+-                long long val = std::strtoll(buf, NULL, 10);
 +                long long val = strtoq(buf, NULL, 10);
-+#else
-                 long long val = strtoll(buf, NULL, 10);
-+#endif
                  rbe->value_ = static_cast<unsigned long>(val);
              }
              break;
