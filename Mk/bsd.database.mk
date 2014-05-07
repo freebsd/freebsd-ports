9ceb54dcbb40feba4d16018d5eb90235b81806fa
Baptiste Daroussin <bapt@FreeBSD.org>
2014-05-07 23:38:02 +0000

    Use modern LIB_DEPENDS

--- Mk/bsd.database.mk
+++ Mk/bsd.database.mk
@@ -191,7 +191,7 @@ RUN_DEPENDS+=	${LOCALBASE}/libexec/mysqld:${PORTSDIR}/${_MYSQL_SERVER}
 BUILD_DEPENDS+=	${LOCALBASE}/lib/mysql/libmysqld.a:${PORTSDIR}/${_MYSQL_SERVER}
 .endif
 .else
-LIB_DEPENDS+=	mysqlclient.${MYSQL${MYSQL_VER}_LIBVER}:${PORTSDIR}/${_MYSQL_CLIENT}
+LIB_DEPENDS+=	libmysqlclient.so.${MYSQL${MYSQL_VER}_LIBVER}:${PORTSDIR}/${_MYSQL_CLIENT}
 .endif
 .else
 IGNORE=		cannot install: unknown MySQL version: ${MYSQL_VER}
@@ -266,7 +266,7 @@ IGNORE?=		cannot install: does not work with postgresql${PGSQL_VER}-client (Post
 .	endfor
 .endif # IGNORE_WITH_PGSQL
 
-LIB_DEPENDS+=	pq.${PGSQL${PGSQL_VER}_LIBVER}:${PORTSDIR}/databases/postgresql${PGSQL_VER}-client
+LIB_DEPENDS+=	libpq.so.${PGSQL${PGSQL_VER}_LIBVER}:${PORTSDIR}/databases/postgresql${PGSQL_VER}-client
 
 _USE_PGSQL_DEP=			contrib docs pgtcl pltcl plperl server
 _USE_PGSQL_DEP_contrib=	pgbench
@@ -538,13 +538,13 @@ FIREBIRD_VER=	${USE_FIREBIRD}
 .endif
 
 .if ${FIREBIRD_VER} == "2"
-LIB_DEPENDS+=	fbclient.2:${PORTSDIR}/databases/firebird20-client
+LIB_DEPENDS+=	libfbclient.so:${PORTSDIR}/databases/firebird20-client
 .elif ${FIREBIRD_VER} == "20"
-LIB_DEPENDS+=	fbclient.2:${PORTSDIR}/databases/firebird20-client
+LIB_DEPENDS+=	libfbclient.so:${PORTSDIR}/databases/firebird20-client
 .elif ${FIREBIRD_VER} == "21"
-LIB_DEPENDS+=	fbclient.2:${PORTSDIR}/databases/firebird21-client
+LIB_DEPENDS+=	libfbclient.so:${PORTSDIR}/databases/firebird21-client
 .elif ${FIREBIRD_VER} == "25"
-LIB_DEPENDS+=	fbclient.2:${PORTSDIR}/databases/firebird25-client
+LIB_DEPENDS+=	libfbclient.so:${PORTSDIR}/databases/firebird25-client
 .else
 IGNORE=		cannot install: unknown Firebird version: ${FIREBIRD_VER}
 .endif
