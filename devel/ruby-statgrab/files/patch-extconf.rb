eb030f5a5cf485990753e90d557470a92a82094e
Tim Bishop <tdb@FreeBSD.org>
2014-05-12 16:56:23 +0000

    - Add devel/libstatgrab0 (copied from devel/libstatgrab) - Switch bindings to use devel/libstatgrab0 instead of devel/libstatgrab - Fix devel/pecl-statgrab to work with current PHP versions
    I have an update ready for devel/libstatgrab (version 0.90) that
    brings a number of API changes that are incompatible with the current
    version. These bindings haven't yet been updated upstream to support
    the new API, so instead of holding off indefinitely I've created the
    devel/libstatgrab0 port to provide just the library and header file
    for the older API version so that these bindings can continue to be
    used when the main port is updated. Once these bindings are updated
    the libstatgrab0 port can be removed.
    
    Both devel/libstatgrab and devel/libstatgrab0 can be installed at the
    same time.
    
    The other ports that depend on devel/libstatgrab will be updated to
    use the new API.

--- devel/ruby-statgrab/files/patch-extconf.rb
+++ devel/ruby-statgrab/files/patch-extconf.rb
@@ -1,7 +1,8 @@
---- ./extconf.rb.orig	2008-06-16 20:37:28.000000000 +0100
-+++ ./extconf.rb	2008-06-16 20:37:35.000000000 +0100
+--- ./extconf.rb.orig	2008-04-28 01:39:02.000000000 +0100
++++ ./extconf.rb	2014-05-10 02:17:32.621965686 +0100
 @@ -1,3 +1,4 @@
  require 'mkmf'
-+pkg_config('libstatgrab')
- have_library("statgrab")
+-have_library("statgrab")
++pkg_config('libstatgrab0')
++have_library("statgrab0")
  create_makefile("statgrab")
