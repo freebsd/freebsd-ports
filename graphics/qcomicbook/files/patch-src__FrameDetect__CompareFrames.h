1f6d0abe578c353b75f9daa410db9fe727b81d9c
makc <makc@FreeBSD.org>
2014-05-13 08:33:54 +0000

    graphics/qcomicbook: - Add patch to fix build on 10 and head - Update MASTER_SITES and WWW
    PR:		ports/187766
    Submitted by:	KATO Tsuguru <tkato432@yahoo.com>

---
+++ graphics/qcomicbook/files/patch-src__FrameDetect__CompareFrames.h
@@ -0,0 +1,10 @@
+--- ./src/FrameDetect/CompareFrames.h.orig	2012-05-30 20:32:02.000000000 +0000
++++ ./src/FrameDetect/CompareFrames.h	2014-05-13 08:15:11.199946861 +0000
+@@ -14,6 +14,7 @@
+ #define __COMPARE_FRAMES_H
+ 
+ #include <ComicFrame.h>
++#include <cstdlib>
+ 
+ namespace QComicBook
+ {
