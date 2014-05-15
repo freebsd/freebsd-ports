11cebe40d728a793e65cc54168fd9f0dda602f99
db <db@FreeBSD.org>
2014-05-15 13:41:18 +0000

    - Fix compiler now that -Wl,B has been added to fortran link args
    Submitted by:	me

--- math/py-numpy/files/patch-numpy-f2py-f2py2e.py
+++ math/py-numpy/files/patch-numpy-f2py-f2py2e.py
@@ -1,10 +1,10 @@
---- numpy/f2py/f2py2e.py.orig	2012-01-25 06:59:14.651304791 -0600
-+++ numpy/f2py/f2py2e.py	2012-01-25 07:00:58.475075133 -0600
+--- numpy/f2py/f2py2e.py.orig	2013-10-30 13:31:41.000000000 -0500
++++ numpy/f2py/f2py2e.py	2014-04-09 10:23:49.000000000 -0500
 @@ -520,6 +520,7 @@
      include_dirs, sources = filter_files('-I', '', sources, remove_prefix=1)
      library_dirs, sources = filter_files('-L', '', sources, remove_prefix=1)
      libraries, sources = filter_files('-l', '', sources, remove_prefix=1)
-+    extra_link_args, sources = filter_files('-Wl,','',sources,remove_prefix=0)
++    extra_link_args, sources = filter_files('-Wl,|-B','',sources,remove_prefix=0)
      undef_macros, sources = filter_files('-U', '', sources, remove_prefix=1)
      define_macros, sources = filter_files('-D', '', sources, remove_prefix=1)
      using_numarray = 0
