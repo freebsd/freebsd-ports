f98a530b0673cb3baaa35696b640a12f068b6cd7
garga <garga@FreeBSD.org>
2014-05-15 15:25:38 +0000

    Fix build with modern compilers
    Approved by:	portmgr (blanket)

---
+++ mail/courier/files/patch-courier__cdfilters.c
@@ -0,0 +1,10 @@
+--- courier/cdfilters.C.orig	2014-05-15 12:11:32.000000000 -0300
++++ courier/cdfilters.C	2014-05-15 12:11:44.000000000 -0300
+@@ -13,6 +13,7 @@
+ #include	<unistd.h>
+ #endif
+ #include	<stdlib.h>
++#include	<string.h>
+ #include	<errno.h>
+ #include	<ctype.h>
+ 
