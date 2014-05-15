795fc4961d8a015346072b047cbd9594ab083fb7
John Marino <marino@FreeBSD.org>
2014-05-15 18:40:10 +0000

    lang/asis: Upgrade version 2013 => 2014
    The annual Adacore release happened!

--- lang/asis/files/patch-asis_a4g-gnat__int.adb
+++ lang/asis/files/patch-asis_a4g-gnat__int.adb
@@ -1,7 +1,7 @@
---- asis/a4g-gnat_int.adb.orig	2013-04-16 14:49:49.000000000 +0000
+--- asis/a4g-gnat_int.adb.orig	2014-04-10 13:38:35.000000000 +0000
 +++ asis/a4g-gnat_int.adb
-@@ -231,44 +231,11 @@ package body A4G.GNAT_Int is
-       end if;
+@@ -229,40 +229,10 @@ package body A4G.GNAT_Int is
+       Opt.Tree_Read;
  
        if Strong_Version_Check then
 -         --  We check only the dates here!
@@ -35,18 +35,13 @@
 -         if ASIS_GNAT_V (First_A_Idx .. Last_A_Idx) /=
 -            Tree_Version_String (First_T_Idx .. Last_T_Idx)
 -         then
--            Close (Desc, File_Closed);
--            Ada.Exceptions.Raise_Exception
--              (Program_Error'Identity,
--               A_Name_Buffer (1 .. A_Name_Len) &
--               ": Inconsistent versions of GNAT [" & Tree_Version_String.all &
--               "] and ASIS [" & ASIS_GNAT_V & ']');
+-            Note_Version_Mismatch;
 -         end if;
+-
 +         --  No need for any version check at all
 +         --  FreeBSD Ports system ensures gnat_util is from same sources
-+         --  as comiler building ASIS
-+         
++         --  as compiler building ASIS
 +         null;
- 
        end if;
  
+       --  GNAT/ASIS version check
