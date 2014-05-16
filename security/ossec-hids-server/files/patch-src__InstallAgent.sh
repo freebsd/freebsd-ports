8ae8a4f09004a91a7c10511af5bfc0a4298f1937
Brad Davis <brd@FreeBSD.org>
2014-05-16 13:27:59 +0000

    - Add a patch I missed that fixes the build of ossec-hids-client.
    Reviewed by:	zi@
    Noticed by:		swills@ / jenkins
    Pointy hat to:	me

--- security/ossec-hids-server/files/patch-src__InstallAgent.sh
+++ security/ossec-hids-server/files/patch-src__InstallAgent.sh
@@ -1,5 +1,22 @@
---- src/InstallAgent.sh.orig	2014-05-15 09:23:09.596592731 -0600
-+++ src/InstallAgent.sh	2014-05-15 09:23:33.770590120 -0600
+--- ./src/InstallAgent.sh.orig	2013-10-29 12:13:44.000000000 -0600
++++ ./src/InstallAgent.sh	2014-05-16 07:12:31.133178776 -0600
+@@ -37,11 +37,11 @@
+ 
+ # Creating groups/users
+ if [ "$UNAME" = "FreeBSD" -o "$UNAME" = "DragonFly" ]; then
+-    grep "^${USER}" /etc/passwd > /dev/null 2>&1
+-    if [ ! $? = 0 ]; then
+-    /usr/sbin/pw groupadd ${GROUP}
+-	/usr/sbin/pw useradd ${USER} -d ${DIR} -s /sbin/nologin -g ${GROUP}
+-    fi
++#    grep "^${USER}" /etc/passwd > /dev/null 2>&1
++#    if [ ! $? = 0 ]; then
++#    /usr/sbin/pw groupadd ${GROUP}
++#	/usr/sbin/pw useradd ${USER} -d ${DIR} -s /sbin/nologin -g ${GROUP}
++#    fi
+ 
+ elif [ "$UNAME" = "SunOS" ]; then
+     grep "^${USER}" /etc/passwd > /dev/null 2>&1
 @@ -223,10 +223,10 @@
  if [ $? = 0 ]; then
      cp -pr ../etc/ossec.mc ${DIR}/etc/ossec.conf
