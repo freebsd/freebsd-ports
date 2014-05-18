a84be60ae9cf6a7883bfabd9063e89f776be963d
Stephen Montgomery-Smith <stephen@FreeBSD.org>
2014-05-18 18:15:36 +0000

    - Simplify code in patch.

--- math/octave-forge-lssa/files/patch-fastlscomplex.cc
+++ math/octave-forge-lssa/files/patch-fastlscomplex.cc
@@ -1,74 +1,62 @@
---- fastlscomplex.cc-orig	2014-05-17 13:56:21.000000000 -0500
-+++ fastlscomplex.cc	2014-05-17 14:06:47.000000000 -0500
-@@ -372,8 +372,9 @@
+--- fastlscomplex.cc.orig	2012-08-16 21:00:23.000000000 +0000
++++ fastlscomplex.cc	2014-05-18 17:56:32.000000000 +0000
+@@ -372,8 +372,7 @@
  
                              --tpra;
                              h = *tpra * *exp_ptr;
 -                            record_current->power_series[p].real() -= h.imag();
 -                            record_current->power_series[p].imag() += h.real();
-+                            // record_current->power_series[p].real() -= h.imag();
-+                            // record_current->power_series[p].imag() += h.real();
-+                            record_current->power_series[p] = std::complex<double>(record_current->power_series[p].real() - h.imag(), record_current->power_series[p].imag() + h.real());
++                            record_current->power_series[p] += std::complex<double>(-h.imag(), h.real());
  
                              if (++exp_ptr >= exp_pse_ptr )
                                break;
-@@ -386,8 +387,9 @@
+@@ -386,8 +385,7 @@
  
                              --tpra;
                              h = -*tpra * *exp_ptr;
 -                            record_current->power_series[p].real() -= h.imag();
 -                            record_current->power_series[p].imag() += h.real();
-+                            // record_current->power_series[p].real() -= h.imag();
-+                            // record_current->power_series[p].imag() += h.real();
-+                            record_current->power_series[p] = std::complex<double>(record_current->power_series[p].real() - h.imag(), record_current->power_series[p].imag() + h.real());
++                            record_current->power_series[p] += std::complex<double>(-h.imag(), h.real());
  
                              if (++exp_ptr >= exp_pse_ptr)
                                break;
-@@ -422,8 +424,9 @@
+@@ -422,8 +420,7 @@
  
                                    tprb -= 2;
                                    h = *tprb * *exp_ptr;
 -                                  record_current->power_series[p].real() -= h.imag();
 -                                  record_current->power_series[p].imag() += h.real();
-+                                  // record_current->power_series[p].real() -= h.imag();
-+                                  // record_current->power_series[p].imag() += h.real();
-+                                  record_current->power_series[p] = std::complex<double>(record_current->power_series[p].real() - h.imag(), record_current->power_series[p].imag() + h.real());
++                                  record_current->power_series[p] += std::complex<double>(-h.imag(), h.real());
  
                                    if ( ++exp_ptr >= exp_pse_ptr )
                                      break;
-@@ -436,8 +439,9 @@
+@@ -436,8 +433,7 @@
  
                                    tprb -= 2;
                                    h = - *tprb * *exp_ptr;
 -                                  record_current->power_series[p].real() -= h.imag();
 -                                  record_current->power_series[p].imag() += h.real();
-+                                  // record_current->power_series[p].real() -= h.imag();
-+                                  // record_current->power_series[p].imag() += h.real();
-+                                  record_current->power_series[p] = std::complex<double>(record_current->power_series[p].real() - h.imag(), record_current->power_series[p].imag() + h.real());
++                                  record_current->power_series[p] += std::complex<double>(-h.imag(), h.real());
  
                                    if (++exp_ptr >= exp_pse_ptr)
                                      break;
-@@ -467,8 +471,9 @@
+@@ -467,8 +463,7 @@
  
                                    --tpra;
                                    h = *tpra * *exp_ptr;
 -                                  record_next->power_series[q].real() -= h.imag();
 -                                  record_next->power_series[q].imag() += h.real();
-+                                  // record_current->power_series[q].real() -= h.imag();
-+                                  // record_current->power_series[q].imag() += h.real();
-+                                  record_current->power_series[q] = std::complex<double>(record_current->power_series[q].real() - h.imag(), record_current->power_series[q].imag() + h.real());
++                                  record_current->power_series[q] += std::complex<double>(-h.imag(), h.real());
  
                                    if (++exp_ptr >= exp_pse_ptr)
                                      break;
-@@ -481,8 +486,9 @@
+@@ -481,8 +476,7 @@
  
                                    --tpra;
                                    h = -*tpra * *exp_ptr;
 -                                  record_next->power_series[q].real() -= h.imag();
 -                                  record_next->power_series[q].imag() += h.real();
-+                                  // record_current->power_series[q].real() -= h.imag();
-+                                  // record_current->power_series[q].imag() += h.real();
-+                                  record_current->power_series[q] = std::complex<double>(record_current->power_series[q].real() - h.imag(), record_current->power_series[q].imag() + h.real());
++                                  record_current->power_series[q] += std::complex<double>(-h.imag(), h.real());
  
                                    if (++exp_ptr >= exp_pse_ptr)
                                      break;
