8261413c934c2a8adabbef134629ee318d160254
Kris Moore <kris@pcbsd.org>
2014-05-20 14:23:49 -0400

    Fix build of new grub 2.02_2

--- sysutils/grub2/files/patch-grub-core_disk_geli.c
+++ sysutils/grub2/files/patch-grub-core_disk_geli.c
@@ -1,11 +1,11 @@
---- grub-core/disk/geli.c.orig	2014-05-15 14:00:10.237337451 -0400
-+++ grub-core/disk/geli.c	2014-05-15 14:00:22.467443163 -0400
-@@ -225,7 +225,7 @@
+--- grub-core/disk/geli.c.orig	2014-05-20 14:18:46.333972233 -0400
++++ grub-core/disk/geli.c	2014-05-20 14:19:12.973970142 -0400
+@@ -265,7 +265,7 @@
  
    /* Look for GELI magic sequence.  */
-   if (grub_memcmp (header->magic, GELI_MAGIC, sizeof (GELI_MAGIC))
--      || grub_le_to_cpu32 (header->version) > 5
-+      || grub_le_to_cpu32 (header->version) > 7
-       || grub_le_to_cpu32 (header->version) < 1)
-     grub_util_error ("%s", _("wrong ELI magic or version"));
- 
+   if (grub_memcmp (header.magic, GELI_MAGIC, sizeof (GELI_MAGIC))
+-      || grub_le_to_cpu32 (header.version) > 5
++      || grub_le_to_cpu32 (header.version) > 7
+       || grub_le_to_cpu32 (header.version) < 1)
+     {
+       grub_dprintf ("geli", "wrong magic %02x\n", header.magic[0]);
