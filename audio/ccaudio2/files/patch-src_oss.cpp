b1f5de69f7edd8a742ba40564f636a7ad6504841
Pietro Cerutti <gahr@FreeBSD.org>
2014-05-20 08:39:56 +0000

    - Update to 2.1.2
      Changes:
    
      from ccaudio2 2.1.1 to ccaudio2 2.1.2
      - fixed cmake shared library builds
      - windows audio fixups
    
      from ccaudio2 2.1.0 to ccaudio2 2.1.1
      - fixed library naming in CMakeLists.txt
      - updated spec files
      - updated lib .so name
    
      from ccaudio2 2.0.5 to ccaudio2 2.1.0
      - modernized automake
      - patches from Brandon Invergo for current ucommon
      - various code cleanups
    
    - Fix MASTER_SITE, general description and WWW line in pkg-descr

--- audio/ccaudio2/files/patch-src_oss.cpp
+++ audio/ccaudio2/files/patch-src_oss.cpp
@@ -1,7 +1,7 @@
---- src/oss.cpp.orig	2011-02-21 11:44:54.000000000 +0000
-+++ src/oss.cpp	2011-02-21 11:45:19.000000000 +0000
+--- src/oss.cpp.orig	2014-01-28 20:43:54.000000000 +0100
++++ src/oss.cpp	2014-05-20 10:23:04.000000000 +0200
 @@ -20,6 +20,13 @@
- #include <config.h>
+ #include <ccaudio2-config.h>
  #include <math.h>
  
 +#ifdef __FreeBSD__
