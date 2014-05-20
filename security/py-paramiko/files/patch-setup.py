aad6c779546946c76117d1affc7488501b5a0bb9
Sofian Brabez <sbz@FreeBSD.org>
2014-05-20 13:20:22 +0000

    - Update to 1.14.0

--- security/py-paramiko/files/patch-setup.py
+++ security/py-paramiko/files/patch-setup.py
@@ -1,15 +1,16 @@
---- ./setup.py.orig	2014-02-06 14:49:52.000000000 +0100
-+++ ./setup.py	2014-02-06 14:51:23.000000000 +0100
-@@ -37,16 +37,8 @@
+--- ./setup.py.orig	2014-05-20 15:06:31.000000000 +0200
++++ ./setup.py	2014-05-20 15:07:11.000000000 +0200
+@@ -37,17 +37,8 @@
  #ez_setup.use_setuptools()
  
  import sys
 -try:
 -    from setuptools import setup
 -    kw = {
--        'install_requires': ['pycrypto >= 2.1, != 2.4',
--                             'ecdsa',
--                             ],
+-        'install_requires': [
+-            'pycrypto >= 2.1, != 2.4',
+-            'ecdsa',
+-        ],
 -    }
 -except ImportError:
 -    from distutils.core import setup
