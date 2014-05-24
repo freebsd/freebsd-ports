faa4b78a31ab46e6d7464871b47784ff2f2a8eaf
wg <wg@FreeBSD.org>
2014-05-24 12:47:05 +0000

    misc/py-pexpect: update to 3.2
    PR:		ports/189708
    Submitted by:	Bartek Rutkowski <ports at robakdesign com>

--- misc/py-pexpect/files/patch-pexpect-__init__.py
+++ misc/py-pexpect/files/patch-pexpect-__init__.py
@@ -1,13 +1,10 @@
---- pexpect/__init__.py.orig	2013-11-11 17:58:03.000000000 +0000
-+++ pexpect/__init__.py	2013-12-26 08:28:18.423632870 +0000
-@@ -1558,10 +1558,7 @@
-         # TIOCSWINSZ and they don't have a truncate problem.
-         # Newer versions of Linux have totally different values for TIOCSWINSZ.
-         # Note that this fix is a hack.
+--- pexpect/__init__.py.orig	2014-04-15 03:31:01.000000000 +0200
++++ pexpect/__init__.py	2014-05-12 14:41:21.690335992 +0200
+@@ -1562,7 +1562,7 @@
+         # termios.TIOCSWINSZ to be truncated. There was a hack here to work
+         # around this, but it caused problems with newer platforms so has been
+         # removed. For details see https://github.com/pexpect/pexpect/issues/39
 -        TIOCSWINSZ = getattr(termios, 'TIOCSWINSZ', -2146929561)
--        if TIOCSWINSZ == 2148037735:
--            # Same bits, but with sign.
--            TIOCSWINSZ = -2146929561
 +        TIOCSWINSZ = getattr(termios, 'TIOCSWINSZ', 2148037735)
          # Note, assume ws_xpixel and ws_ypixel are zero.
          s = struct.pack('HHHH', rows, cols, 0, 0)
