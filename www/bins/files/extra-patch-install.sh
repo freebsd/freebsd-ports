619b9228e60e36009d94b2d0f4def0e6eec57904
Martin Wilke <miwi@FreeBSD.org>
2014-05-25 05:09:50 +0000

    - Stage support
    PR:		190015
    Submitted by:	Joseph Benden <joe@thrallingpenguin.com>

--- www/bins/files/extra-patch-install.sh
+++ www/bins/files/extra-patch-install.sh
@@ -1,6 +1,23 @@
---- install.sh.orig	Thu Aug 25 06:07:05 2005
-+++ install.sh	Mon Mar 19 02:47:24 2007
-@@ -67,11 +67,10 @@
+--- install.sh.orig	2014-05-20 08:56:00.000000000 -0700
++++ install.sh	2014-05-20 09:00:43.000000000 -0700
+@@ -24,6 +24,7 @@
+ GLOBAL_RC="$RC"
+ 
+ DESTDIR=`echo "$DESTDIR"|sed 's%/$%%'`
++ORIGPREFIX=$PREFIX
+ PREFIX=$DESTDIR/`echo "$PREFIX"|sed 's%^/%%'`
+ PREFIX=`echo "$PREFIX"|sed 's%/$%%'`
+ ETC=$DESTDIR/`echo "$ETC"|sed 's%^/%%'`
+@@ -35,7 +36,7 @@
+ SHARE="$PREFIX/share"
+ DATA="$SHARE/bins"
+ GLADE="$DATA"
+-LOCALE="$SHARE/locale"
++LOCALE="${SHARE}/locale"
+ MAN="$PREFIX/man/man1"
+ 
+ cd `dirname $0`
+@@ -67,11 +68,10 @@
    if [ -w $BIN ] ; then
      echo "Installing program files in $BIN"
      # bins_cleanupgallery anti_bins
@@ -13,7 +30,7 @@
      chmod a+rx $BIN/anti_bins
  #    perl -pi -e "s{/usr/local/share}{$SHARE}g" \
  #     $BIN/bins $BIN/bins_edit $BIN/bins-edit-gui
-@@ -169,13 +168,6 @@
+@@ -169,13 +169,6 @@
  fi
  set -o errexit
  
@@ -27,7 +44,7 @@
  # man pages
  mkdir -p $MAN 2>/dev/null || true
  if [ -w $MAN ] ; then
-@@ -203,9 +195,6 @@
+@@ -203,9 +196,6 @@
    do
      mkdir -p $LOCALE/$L/LC_MESSAGES 2>/dev/null || true
      cp intl/$L.mo $LOCALE/$L/LC_MESSAGES/bins.mo
@@ -37,3 +54,12 @@
    done
  else
    cat <<!EoF  
+@@ -227,7 +217,7 @@
+ do
+   echo -n " $FILE"
+   ed "$FILE" <<EoF 2>/dev/null
+-1,\$s%/usr/local/share%$SHARE%g
++1,\$s%/usr/local/share%$ORIGPREFIX/share%g
+ 1,\$s%/etc/bins%$GLOBAL_RC%g
+ wq
+ EoF
