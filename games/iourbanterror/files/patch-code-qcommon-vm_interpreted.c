4a5c97cd515131649397e390a219423754e3d667
John Marino <marino@FreeBSD.org>
2014-06-01 15:28:29 +0000

    games: ioquak3* iourbanterror* openarena*: Update and stage support
    The patch applied without incident.  Tested on DragonFly poudriere,
    everything built fine.  It include an i386 fix, which is why the
    previous commit was reverted.
    
    The only modification was to change EXTRACT_CMD to TAR in a couple
    of places.
    
    PR:		ports/187859
    submitted by;	maintainer (Dominic Fandrey)

--- games/iourbanterror/files/patch-code-qcommon-vm_interpreted.c
+++ games/iourbanterror/files/patch-code-qcommon-vm_interpreted.c
@@ -1,37 +1,16 @@
---- code/qcommon/vm_interpreted.c.orig	2013-07-15 18:54:41.000000000 +0000
-+++ code/qcommon/vm_interpreted.c
-@@ -436,21 +436,21 @@ nextInstruction2:
- 				return 0;
- 			}
- #endif
--			r0 = opStack[opStackOfs] = *(int *) &image[r0 & dataMask & ~3 ];
-+			r0 = opStack[opStackOfs] = *(int *) &image[r0 & dataMask ];
- 			goto nextInstruction2;
- 		case OP_LOAD2:
--			r0 = opStack[opStackOfs] = *(unsigned short *)&image[ r0&dataMask&~1 ];
-+			r0 = opStack[opStackOfs] = *(unsigned short *)&image[ r0&dataMask ];
- 			goto nextInstruction2;
- 		case OP_LOAD1:
- 			r0 = opStack[opStackOfs] = image[ r0&dataMask ];
+--- code/qcommon/vm_interpreted.c.orig	2014-03-23 17:14:22.000000000 +0100
++++ code/qcommon/vm_interpreted.c	2014-03-23 17:17:07.000000000 +0100
+@@ -458,11 +458,11 @@
  			goto nextInstruction2;
  
  		case OP_STORE4:
 -			*(int *)&image[ r1&(dataMask & ~3) ] = r0;
-+			*(int *)&image[ r1&dataMask ] = r0;
- 			opStackOfs -= 2;
++			*(int *)&image[ r1&(dataMask) ] = r0;
+ 			opStack -= 2;
  			goto nextInstruction;
  		case OP_STORE2:
 -			*(short *)&image[ r1&(dataMask & ~1) ] = r0;
-+			*(short *)&image[ r1&dataMask ] = r0;
- 			opStackOfs -= 2;
++			*(short *)&image[ r1&(dataMask) ] = r0;
+ 			opStack -= 2;
  			goto nextInstruction;
  		case OP_STORE1:
-@@ -460,7 +460,7 @@ nextInstruction2:
- 
- 		case OP_ARG:
- 			// single byte offset from programStack
--			*(int *)&image[ (codeImage[programCounter] + programStack)&dataMask&~3 ] = r0;
-+			*(int *)&image[ (codeImage[programCounter] + programStack)&dataMask ] = r0;
- 			opStackOfs--;
- 			programCounter += 1;
- 			goto nextInstruction;
