d1d110b799ef0bbd997fae3a7aba8060685c3aad
miwi <miwi@FreeBSD.org>
2014-06-02 12:28:58 +0000

    - Update to  5.5.37
    PR:		189887
    Submitted by:	maintainer

--- databases/mariadb55-server/files/patch-scripts_mysql_config.sh
+++ databases/mariadb55-server/files/patch-scripts_mysql_config.sh
@@ -1,13 +1,13 @@
---- scripts/mysql_config.sh.orig	2013-09-20 01:34:23.000000000 +0300
-+++ scripts/mysql_config.sh	2013-10-15 21:23:34.000000000 +0300
+--- scripts/mysql_config.sh.orig	2014-04-15 14:02:39.000000000 +0300
++++ scripts/mysql_config.sh	2014-05-16 19:08:41.000000000 +0300
 @@ -108,8 +108,11 @@
  # We intentionally add a space to the beginning and end of lib strings, simplifies replace later
- libs=" $ldflags -L$pkglibdir -lmysqlclient @ZLIB_DEPS@ @NON_THREADED_LIBS@"
+ libs=" $ldflags -L$pkglibdir @RPATH_OPTION@ -lmysqlclient @ZLIB_DEPS@ @NON_THREADED_LIBS@"
  libs="$libs @openssl_libs@ @STATIC_NSS_FLAGS@ "
 +libs="$libs -L%%LOCALBASE%%/lib "
- libs_r=" $ldflags -L$pkglibdir -lmysqlclient_r @ZLIB_DEPS@ @CLIENT_LIBS@ @openssl_libs@ "
+ libs_r=" $ldflags -L$pkglibdir  @RPATH_OPTION@ -lmysqlclient_r @ZLIB_DEPS@ @CLIENT_LIBS@ @openssl_libs@ "
 +libs_r="$libs_r -L%%LOCALBASE%%/lib "
- embedded_libs=" $ldflags -L$pkglibdir -lmysqld @LIBDL@ @ZLIB_DEPS@ @LIBS@ @WRAPLIBS@ @openssl_libs@ "
+ embedded_libs=" $ldflags -L$pkglibdir @RPATH_OPTION@ -lmysqld @LIBDL@ @ZLIB_DEPS@ @LIBS@ @WRAPLIBS@ @openssl_libs@ "
 +embedded_libs="$embedded_libs -L%%LOCALBASE%%/lib "
  
  if [ -r "$pkglibdir/libmygcc.a" ]; then
