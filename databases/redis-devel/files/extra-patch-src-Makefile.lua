c4bf2a232e706f0bebd7338097555bb8e44e908b
Sergey A. Osokin <osa@FreeBSD.org>
2014-06-02 01:42:11 +0000

    Upgrade from 3.0.0-beta2 to 3.0.0-beta5. Fix lua/luajit-specific extra patches. Stagefy.
    <ChangeLog>
    
    --[ Redis 3.0.0 Beta 5 (version 2.9.54) ] Release date: 26 may 2014
    
    This is the 5th beta of Redis 3.0.0. It does not include any real
    worthwhile change (just three days passed since the previous beta), but
    fixes two stupid bugs preventing cluster tests to pass.
    
    --[ Redis 3.0.0 Beta 4 (version 2.9.53) ] Release date: 23 may 2014
    
    This is the fourth beta of Redis 3.0.0.
    
    >> General changes
    
    * [NEW] Scripting engine performances improvements.
    * [NEW] Log format slightly changed to report current node role.
    
    * [FIX] Correct the HyperLogLog stale cache flag to prevent unnecessary
            computation.
    
    >> Cluster changes
    
    * [NEW] redis-trib: ability to import data from standalone Redis instances.
    * [NEW] redis-trib: "fix" subcommand much better at fixing errors.
    * [NEW] CLUSTER FAILOVER FORCE implemented.
    * [NEW] CLUSTER RESET implemented, it is now possible to completely reset
            nodes and create a new cluster without restarting anything.
    * [NEW] Slave validity factor (max estimated data age to still failover)
            is now under the control of the user, and can be configured via
            redis.conf or CONFIG SET. Option name cluster-slave-validity-factor.
    * [NEW] Cluster test: failure detection and failover initial tests.
    * [NEW] CLUSTER MEET: better error messages when address is invalid.
    * [NEW] Bulk-accept new Cluster nodes in the Cluster bus instead of
            performing just a single accept() per event fired.
    
    * [FIX] Bypass data_age check for manual failovers.
    * [FIX] Fixed data_age computation / check integer overflow.
    * [FIX] Various fixes to Tcl client.tcl Redis Cluster client used in tests.
    * [FIX] Better handling of stolen slots.
    * [FIX] Don't accept cluster bus connections during startup.
    
    >> Sentinel changes
    
    * [NEW] Generate +config-update-from event when a new config is received.
    * [NEW] Log when a failover will be re-attempted again.
    
    * [FIX] Sentinel: Add "dir /tmp" directive in example sentinel.conf.
    
    --[ Redis 3.0.0 Beta 3 (version 2.9.52) ] Release date: 5 may 2014
    
    This is the third beta of Redis 3.0.0.
    
    >> General changes
    
    * [NEW] New data structure: the HyperLogLog (see 2.8 release notes).
    * [NEW] Lexicograhical range queries in sorted sets (see 2.8 release notes).
    * [NEW] LRU algorithm precision greatly improved.
    
    * [FIX] Redis is now much more responsive to reply with LOADING / BUSY errors.
    
    >> Cluster changes
    
    * [NEW] Cluster testing framework and initial tests.
    * [NEW] Cluster epochs collision resolution (make Redis Cluster more resilient
            to admin and programming errors).
    * [NEW] Persist / fsync some global state to ensure correct crash-recovery
            semantics.
    * [NEW] New command introduced: CLUSTER SET-CONFIG-EPOCH, still not used
            by redis-trib. Will be used to speedup the assignment of unique
            epochs to different nodes at cluster creation time. For now this is
            handled as a side effect of the cluster epochs collision resolution.
    
    * [FIX] Different fixes to redis-trib cluster creation.
    * [FIX] Fix an error in the CLUSTER NODES output for nodes slots.
    
    >> Sentinel changes
    
    * [NEW] Sentinels are now able to send update messages in a peer-to-peer
            fashion even if no Redis instances are available. Now the Sentinel
            liveness property that the most updated configuration in a given
            partition is propagated to all the Sentinels is extended to partitions
            without reachable instances.
    * [NEW] Sentinel safety properties are now ensured in a crash-recovery system
            model since some state is persisted on disk before replying to other
            nodes, and reloaded at startup.
    * [NEW] Sentinel now uses CLIENT SETNAME so that it is easy to identify
            Sentinels using CLIENT LIST among other clients.
    * [NEW] Sentinel failure detection and reconnection code improved.
    
    </ChangeLog>

--- databases/redis-devel/files/extra-patch-src-Makefile.lua
+++ databases/redis-devel/files/extra-patch-src-Makefile.lua
@@ -1,5 +1,5 @@
---- src/Makefile.orig	2013-08-13 15:52:22.000000000 +0400
-+++ src/Makefile	2013-08-13 15:54:55.000000000 +0400
+--- src/Makefile.orig	2014-05-26 20:06:48.000000000 +0400
++++ src/Makefile	2014-06-02 00:19:36.000000000 +0400
 @@ -15,7 +15,7 @@
  release_hdr := $(shell sh -c './mkreleasehdr.sh')
  uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
@@ -18,7 +18,7 @@
  FINAL_LDFLAGS=$(LDFLAGS) $(REDIS_LDFLAGS) $(DEBUG)
  FINAL_LIBS=-lm
  DEBUG=-g -ggdb
-@@ -85,6 +85,9 @@
+@@ -88,6 +88,9 @@
  	FINAL_LIBS+= ../deps/jemalloc/lib/libjemalloc.a -ldl
  endif
  
@@ -28,15 +28,15 @@
  REDIS_CC=$(QUIET_CC)$(CC) $(FINAL_CFLAGS)
  REDIS_LD=$(QUIET_LINK)$(CC) $(FINAL_LDFLAGS)
  REDIS_INSTALL=$(QUIET_INSTALL)$(INSTALL)
-@@ -105,6 +108,7 @@
+@@ -108,6 +111,7 @@
  REDIS_SERVER_NAME=redis-server
  REDIS_SENTINEL_NAME=redis-sentinel
- REDIS_SERVER_OBJ=adlist.o ae.o anet.o dict.o redis.o sds.o zmalloc.o lzf_c.o lzf_d.o pqsort.o zipmap.o sha1.o ziplist.o release.o networking.o util.o object.o db.o replication.o rdb.o t_string.o t_list.o t_set.o t_zset.o t_hash.o config.o aof.o pubsub.o multi.o debug.o sort.o intset.o syncio.o migrate.o endianconv.o slowlog.o scripting.o bio.o rio.o rand.o memtest.o crc64.o bitops.o sentinel.o notify.o setproctitle.o
-+REDIS_SERVER_OBJ+= lua_cjson.o lua_cmsgpack.o lua_struct.o strbuf.o
+ REDIS_SERVER_OBJ=adlist.o ae.o anet.o dict.o redis.o sds.o zmalloc.o lzf_c.o lzf_d.o pqsort.o zipmap.o sha1.o ziplist.o release.o networking.o util.o object.o db.o replication.o rdb.o t_string.o t_list.o t_set.o t_zset.o t_hash.o config.o aof.o pubsub.o multi.o debug.o sort.o intset.o syncio.o cluster.o crc16.o endianconv.o slowlog.o scripting.o bio.o rio.o rand.o memtest.o crc64.o bitops.o sentinel.o notify.o setproctitle.o blocked.o hyperloglog.o
++REDIS_SERVER_OBJ+=lua_cjson.o lua_cmsgpack.o lua_struct.o strbuf.o
  REDIS_CLI_NAME=redis-cli
  REDIS_CLI_OBJ=anet.o sds.o adlist.o redis-cli.o zmalloc.o release.o anet.o ae.o crc64.o
  REDIS_BENCHMARK_NAME=redis-benchmark
-@@ -159,7 +163,7 @@
+@@ -162,7 +166,7 @@
  
  # redis-server
  $(REDIS_SERVER_NAME): $(REDIS_SERVER_OBJ)
