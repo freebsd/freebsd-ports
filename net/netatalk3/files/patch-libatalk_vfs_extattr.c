505839235c0414c104ccec44723f1477cfee853d
Joe Marcus Clarke <marcus@FreeBSD.org>
2014-06-07 18:35:01 +0000

    Update to 3.1.2.  See http://netatalk.sourceforge.net/3.1/ReleaseNotes3.1.2.html for the changes in this release.

--- net/netatalk3/files/patch-libatalk_vfs_extattr.c
+++ net/netatalk3/files/patch-libatalk_vfs_extattr.c
@@ -1,5 +1,5 @@
---- libatalk/vfs/extattr.c.orig	2013-10-18 07:57:33.000000000 -0400
-+++ libatalk/vfs/extattr.c	2014-05-27 12:31:57.000000000 -0400
+--- libatalk/vfs/extattr.c.orig	2014-06-03 01:40:44.000000000 -0400
++++ libatalk/vfs/extattr.c	2014-06-07 14:09:11.000000000 -0400
 @@ -193,6 +193,8 @@ ssize_t sys_fgetxattr (int filedes, cons
      const char *attrname = ((s=strchr(name, '.')) == NULL) ? name : s + 1;
  
@@ -9,14 +9,3 @@
          if(retval > size) {
              errno = ERANGE;
              return -1;
-@@ -241,6 +243,10 @@ ssize_t sys_lgetxattr (const char *path,
- #elif defined(HAVE_EXTATTR_GET_LINK)
- 	ssize_t retval;
- 	if((retval=extattr_get_link(path, EXTATTR_NAMESPACE_USER, uname, NULL, 0)) >= 0) {
-+		if (size == 0) {
-+			/* size == 0 means only return size */
-+			return retval;
-+		}
- 		if(retval > size) {
- 			errno = ERANGE;
- 			return -1;
