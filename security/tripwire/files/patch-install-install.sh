20a17da9a2caa628b6cbc32f6008d5c6156eb5b2
cy <cy@FreeBSD.org>
2014-06-09 19:58:33 +0000

    Stagify tripwire, tripwire12, and tripwire-131.
    Take maintainership of tripwire12.

--- security/tripwire/files/patch-install-install.sh
+++ security/tripwire/files/patch-install-install.sh
@@ -1,5 +1,5 @@
 --- install/install.sh.orig	2011-11-21 08:06:56.000000000 -0800
-+++ install/install.sh	2012-01-26 14:46:55.995316577 -0800
++++ install/install.sh	2014-06-01 20:55:20.632399290 -0700
 @@ -174,17 +174,11 @@
  # Starting directory.
  START_DIR=`pwd`
@@ -45,7 +45,16 @@
  	if [ -z "$TW_SITE_PASS" ] || [ -z "$TW_LOCAL_PASS" ] ; then
  		echo "Error: You must specify site and local passphrase" 1>&2
  		echo "if no prompting is chosen." 1>&2
-@@ -525,7 +521,7 @@
+@@ -514,7 +510,7 @@
+ ##-------------------------------------------------------
+ 
+ for i in $path2; do
+-	eval "d=\$${i}"
++	eval "d=${DESTDIR}/\$${i}"
+ 	if [ ! -d "$d" ] ; then
+ 		mkdir -p "$d"
+ 		if [ ! -d "$d" ] ; then
+@@ -525,12 +521,12 @@
  			chmod 0750 "$d" > /dev/null
  		fi
  	else
@@ -54,6 +63,12 @@
  	fi
  done
  
+ for i in $path3; do
+-	eval "d=\$${i}"
++	eval "d=${DESTDIR}/\$${i}"
+ 	if [ ! -d "$d" ] ; then
+ 		mkdir -p "$d"
+ 		if [ ! -d "$d" ] ; then
 @@ -541,7 +537,7 @@
  			chmod 0755 "$d" > /dev/null
  		fi
@@ -71,6 +86,25 @@
  echo
  echo "----------------------------------------------"
  echo "Copying files..."
+@@ -568,14 +565,14 @@
+ 
+ #f1=' ff=$README ; d="" ; dd=$TWDOCS ; rr=0444 '
+ #f2=' ff=$REL_NOTES ; d="" ; dd=$TWDOCS ; rr=0444 '
+-f3=' ff=$TWLICENSEFILE ; d="" ; dd=$TWDOCS ; rr=0444 '
++f3=' ff=$TWLICENSEFILE ; d="" ; dd=$DESTDIR/$TWDOCS ; rr=0444 '
+ #f4=' ff=tripwire ; d="/bin" ; dd=$TWBIN ; rr=0550 '
+ #f5=' ff=twadmin ; d="/bin" ; dd=$TWBIN ; rr=0550 '
+ #f6=' ff=twprint ; d="/bin" ; dd=$TWBIN ; rr=0550 '
+ #f7=' ff=siggen ; d="/bin" ; dd=$TWBIN ; rr=0550 '
+-f8=' ff=TRADEMARK ; d="" ; dd=$TWDOCS ; rr=0444 '
+-f9=' ff=policyguide.txt ; d="/policy" ; dd=$TWDOCS ; rr=0444 '
+-f10=' ff=${POLICYSRC} ; d="/policy" ; dd=$TWPOLICY ; rr=0640 '
++f8=' ff=TRADEMARK ; d="" ; dd=$DESTDIR/$TWDOCS ; rr=0444 '
++f9=' ff=policyguide.txt ; d="/policy" ; dd=$DESTDIR/$TWDOCS ; rr=0444 '
++f10=' ff=${POLICYSRC} ; d="/policy" ; dd=$DESTDIR/$TWPOLICY ; rr=0640 '
+ #f11=' ff=twpolicy.4 ; d="/man/man4" ; dd=$TWMAN/man4 ; rr=0444 '
+ #f12=' ff=twconfig.4 ; d="/man/man4" ; dd=$TWMAN/man4 ; rr=0444 '
+ #f13=' ff=twfiles.5 ; d="/man/man5" ; dd=$TWMAN/man5 ; rr=0444 '
 @@ -593,7 +590,7 @@
  	f=${TAR_DIR}$d/$ff
  	ff=${dd}/$ff
