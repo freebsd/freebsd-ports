42105c2d013a1716c26277e242c237f938dbe7ce
William Grzybowski <wg@FreeBSD.org>
2014-06-16 16:49:16 +0000

    games/py-mnemosyne: update to 2.3
    - Update to 2.3
    - Pass maintainership to submitter
    
    PR:		188930
    Submitted by:	hhh sdf org

--- games/py-mnemosyne/files/patch-setup.py
+++ games/py-mnemosyne/files/patch-setup.py
@@ -1,29 +1,18 @@
---- setup.py.orig	2012-07-28 07:11:07.000000000 +0200
-+++ setup.py	2012-12-23 20:52:55.564991943 +0100
-@@ -133,8 +133,7 @@
+--- ./setup.py.orig	2014-04-23 23:17:14.713606058 +0200
++++ ./setup.py	2014-04-23 23:46:52.244901795 +0200
+@@ -125,8 +125,9 @@
  else:
      base_path = os.path.join(sys.exec_prefix, "lib", "python" + sys.version[:3],
                               "site-packages","mnemosyne")
 -    data_files = [("/usr/share/applications", ["mnemosyne.desktop"]),
 -                  ("/usr/share/icons", ["pixmaps/mnemosyne.png"])]
-+    data_files = []
++    data_files = [("share/applications", ["mnemosyne.desktop"]),
++                  ("share/icons", ["pixmaps/mnemosyne.png"])]
++
  # Translations.
  if sys.platform == "win32":
      for mo in [x for x in glob.glob(os.path.join("mo", "*")) \
-@@ -145,12 +144,6 @@
-             [os.path.join(mo, "LC_MESSAGES", "mnemosyne.mo")]))
-         data_files.append((os.path.join("share", "qt4", "translations"),
-             glob.glob(os.path.join("mnemosyne", "pyqt_ui", "qm", '*'))))
--else:
--    for mo in [x for x in glob.glob(os.path.join("mo", "*"))
--               if os.path.isdir(x)]:
--        data_files.append((os.path.join(sys.exec_prefix, "share", "locale",
--            os.path.split(mo)[1], "LC_MESSAGES"),
--            [os.path.join(mo, "LC_MESSAGES", "mnemosyne.mo")]))
- 
- pixmap_path = os.path.join(base_path, "pixmaps")
- util_path = os.path.join(base_path, "util")
-@@ -250,4 +243,3 @@
+@@ -240,4 +241,3 @@
        options = {"py2app": py2app_options, "py2exe": py2exe_options},
        app = py2app_app
  )
