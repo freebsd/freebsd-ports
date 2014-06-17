f43acf1e5dc93d51c7d59ecb2576beb5148bbdc0
Dmitry Marakasov <amdmi3@FreeBSD.org>
2014-06-17 13:53:44 +0000

    - Drop subversion dependency (not used, breaks build by touching ~) - Cosmetic fixes
    PR:		190885
    Submitted by:	amdmi3

--- net/ntop/files/patch-autogen.sh
+++ net/ntop/files/patch-autogen.sh
@@ -1,6 +1,6 @@
---- ./autogen.sh.orig	2011-02-02 10:40:23.000000000 +0100
-+++ ./autogen.sh	2011-02-02 10:40:40.000000000 +0100
-@@ -202,16 +202,6 @@
+--- autogen.sh.orig	2012-02-20 14:35:43.000000000 +0400
++++ autogen.sh	2014-06-10 16:29:45.873177238 +0400
+@@ -202,30 +202,6 @@
    GNU_OR_DIE=0
  }
  
@@ -14,6 +14,30 @@
 -  GNU_OR_DIE=0
 -}
 -
+-if test "$GNU_OR_DIE" -eq 0; then
+-  exit 1
+-fi
+-
+-SVN=`find_command 'svn'`
+-($SVN --version) < /dev/null > /dev/null 2>&1 ||
+-{
+-  echo
+-  echo "You must have svn/subversion installed to compile $progname."
+-  echo "Download the appropriate package for your distribution, or get the"
+-  echo "source from http://subversion.tigris.org"
+-  GNU_OR_DIE=0
+-}
+-
  if test "$GNU_OR_DIE" -eq 0; then
    exit 1
  fi
+@@ -442,7 +418,8 @@
+ if test -d nDPI; then
+     echo "nDPI already available"
+ else
+-    svn co $NDPI_URL
++    echo "nDPI not bundled"
++    exit 1
+ fi
+ 
+ dnl> nDPI compilation
