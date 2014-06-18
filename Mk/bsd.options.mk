7c484e741d67a2db4cadd07bd88229ef7ce1df5f
Baptiste Daroussin <bapt@FreeBSD.org>
2014-06-18 07:13:26 +0000

    Switch back to default tabspace: 8

--- Mk/bsd.options.mk
+++ Mk/bsd.options.mk
@@ -1,47 +1,44 @@
-#-*- tab-width: 4; -*-
-# ex:ts=4
-#
 # $FreeBSD$
 #
 # These variables are used in port makefiles to define the options for a port.
 #
-# OPTIONS_DEFINE			- List of options this ports accept
+# OPTIONS_DEFINE		- List of options this ports accept
 # OPTIONS_DEFINE_${ARCH}	- List of options this ports accept and are
-#							  specific to ${ARCH}
-# OPTIONS_DEFAULT			- List of options activated by default
+#				  specific to ${ARCH}
+# OPTIONS_DEFAULT		- List of options activated by default
 # OPTIONS_DEFAULT_${ARCH}	- List of options activated by default for a
-#							  given arch
+#				  given arch
 #
-# ${OPTION}_DESC			- Description of the ${OPTION}
+# ${OPTION}_DESC		- Description of the ${OPTION}
 #
-# OPTIONS_SINGLE			- List of single-choice grouped options: 1 and
-# 							  only 1 among N
-# OPTIONS_RADIO				- List of radio-choice grouped options: 0 or 1
-#							  among N
-# OPTIONS_MULTI				- List of multiple-choice grouped options: at
-#							  least 1 among N
-# OPTIONS_GROUP				- List of group-choice grouped options: 0 or
-#							  more among N
+# OPTIONS_SINGLE		- List of single-choice grouped options: 1 and
+#				  only 1 among N
+# OPTIONS_RADIO			- List of radio-choice grouped options: 0 or 1
+#				  among N
+# OPTIONS_MULTI			- List of multiple-choice grouped options: at
+#				  least 1 among N
+# OPTIONS_GROUP			- List of group-choice grouped options: 0 or
+#				  more among N
 #
 # OPTIONS_SINGLE_${NAME}	- List of OPTIONS grouped as single choice (for
-#							  the single named as ${NAME} as defined in
-#							  OPTIONS_SINGLE)
+#				  the single named as ${NAME} as defined in
+#				  OPTIONS_SINGLE)
 # OPTIONS_RADIO_${NAME}		- List of OPTIONS grouped as radio choice (for
-#							  the radio named as ${NAME} as defined in
-#							  OPTIONS_RADIO)
+#				  the radio named as ${NAME} as defined in
+#				  OPTIONS_RADIO)
 # OPTIONS_MULTI_${NAME}		- List of OPTIONS grouped as multiple-choice
-#							  (for the multi named as ${NAME} as defined in
-#							  OPTIONS_MULTI)
+#				  (for the multi named as ${NAME} as defined in
+#				  OPTIONS_MULTI)
 # OPTIONS_GROUP_${NAME}		- List of OPTIONS grouped as group-choice (for
-#							  the group named as ${NAME} as defined in
-#							  OPTIONS_GROUP)
+#				  the group named as ${NAME} as defined in
+#				  OPTIONS_GROUP)
 #
-# OPTIONS_EXCLUDE			- List of options unsupported (useful for slave ports)
+# OPTIONS_EXCLUDE		- List of options unsupported (useful for slave ports)
 # OPTIONS_EXCLUDE_${ARCH}	- List of options unsupported on a given ${ARCH}
-# OPTIONS_SLAVE				- This is designed for slave ports, it removes an
-#							  option from the options list inherited from the
-#							  master port and it always adds it to PORT_OPTIONS
-#							  meaning activated
+# OPTIONS_SLAVE			- This is designed for slave ports, it removes an
+#				  option from the options list inherited from the
+#				  master port and it always adds it to PORT_OPTIONS
+#				  meaning activated
 #
 # These variables can be used in make.conf to configure options.  They are
 # processed in the order listed below, i.e. later variables override the effects
@@ -52,56 +49,56 @@
 # using the variables below.  You can force the dialog to appear by running
 # "make config".
 #
-# OPTIONS_SET				- List of options to enable for all ports.
-# OPTIONS_UNSET				- List of options to disable for all ports.
+# OPTIONS_SET			- List of options to enable for all ports.
+# OPTIONS_UNSET			- List of options to disable for all ports.
 # ${OPTIONS_NAME}_SET		- List of options to enable for a specific port.
 # ${OPTIONS_NAME}_UNSET		- List of options to disable for a specific port.
 #
-# OPTIONS_SET_FORCE			- List of options to enable for all ports.
+# OPTIONS_SET_FORCE		- List of options to enable for all ports.
 # OPTIONS_UNSET_FORCE		- List of options to disable for all ports.
 # ${OPTIONS_NAME}_SET_FORCE	- List of options to enable for a specific port.
 # ${OPTIONS_NAME}_UNSET_FORCE
-#							- List of options to disable for a specific port.
+#				- List of options to disable for a specific port.
 #
 # These variables can be used on the command line. They override the effects of
 # the make.conf variables above.
 #
-# WITH						- Set options from the command line
-# WITHOUT					- Unset options from the command line
+# WITH				- Set options from the command line
+# WITHOUT			- Unset options from the command line
 #
 #
 # The following knobs are there to simplify the handling of OPTIONS in simple
 # cases :
 #
-# OPTIONS_SUB				When defined it will add to PLIST_SUB:
-#							Option enabled  ${opt}=""
-#							Option disabled ${opt}="@comment "
+# OPTIONS_SUB			When defined it will add to PLIST_SUB:
+#				Option enabled  ${opt}=""
+#				Option disabled ${opt}="@comment "
 #
 # ${opt}_CONFIGURE_ON		When option is enabled, it will add its content to
-#							the CONFIGURE_ARGS.
+#				the CONFIGURE_ARGS.
 # ${opt}_CONFIGURE_OFF		When option is disabled, it will add its content to
-#							the CONFIGURE_ARGS.
+#				the CONFIGURE_ARGS.
 # ${opt}_CONFIGURE_ENABLE	Will add to CONFIGURE_ARGS:
-#							Option enabled  --enable-${content}
-#							Option disabled --disable-${content}
+#				Option enabled  --enable-${content}
+#				Option disabled --disable-${content}
 # ${opt}_CONFIGURE_WITH		Will add to CONFIGURE_ARGS:
-#							Option enabled  --with-${content}
-#							Option disabled --without-${content}
+#				Option enabled  --with-${content}
+#				Option disabled --without-${content}
 #
-# ${opt}_CMAKE_ON			When option is enabled, it will add its content to
-#							the CMAKE_ARGS.
-# ${opt}_CMAKE_OFF			When option is disabled, it will add its content to
-#							the CMAKE_ARGS.
+# ${opt}_CMAKE_ON		When option is enabled, it will add its content to
+#				the CMAKE_ARGS.
+# ${opt}_CMAKE_OFF		When option is disabled, it will add its content to
+#				the CMAKE_ARGS.
 #
-# ${opt}_QMAKE_ON			When option is enabled, it will add its content to
-#							the QMAKE_ARGS.
-# ${opt}_QMAKE_OFF			When option is disabled, it will add its content to
-#							the QMAKE_ARGS.
+# ${opt}_QMAKE_ON		When option is enabled, it will add its content to
+#				the QMAKE_ARGS.
+# ${opt}_QMAKE_OFF		When option is disabled, it will add its content to
+#				the QMAKE_ARGS.
 #
 # ${opt}_USE=	FOO=bar		When option is enabled, it will  enable
-#							USE_FOO+= bar
-#							If you need more than one option, you can do
-#							FOO=bar,baz and you'll get USE_FOO=bar baz
+#				USE_FOO+= bar
+#				If you need more than one option, you can do
+#				FOO=bar,baz and you'll get USE_FOO=bar baz
 #
 # For each of:
 # ALL_TARGET CATEGORIES CFLAGS CONFIGURE_ENV CONFLICTS CONFLICTS_BUILD
@@ -129,18 +126,18 @@ OPTIONS_NAME?=	${PKGORIGIN:S/\//_/}
 OPTIONSFILE?=	${PORT_DBDIR}/${UNIQUENAME}/options
 OPTIONS_FILE?=	${PORT_DBDIR}/${OPTIONS_NAME}/options
 
-_OPTIONS_FLAGS= ALL_TARGET CATEGORIES CFLAGS CONFIGURE_ENV CONFLICTS \
-				CONFLICTS_BUILD CONFLICTS_INSTALL CPPFLAGS CXXFLAGS DISTFILES \
-				EXTRA_PATCHES INSTALL_TARGET LDFLAGS LIBS MAKE_ARGS MAKE_ENV \
-				PATCHFILES PATCH_SITES PLIST_DIRS PLIST_DIRSTRY PLIST_FILES \
-				USES
+_OPTIONS_FLAGS=	ALL_TARGET CATEGORIES CFLAGS CONFIGURE_ENV CONFLICTS \
+		CONFLICTS_BUILD CONFLICTS_INSTALL CPPFLAGS CXXFLAGS DISTFILES \
+		EXTRA_PATCHES INSTALL_TARGET LDFLAGS LIBS MAKE_ARGS MAKE_ENV \
+		PATCHFILES PATCH_SITES PLIST_DIRS PLIST_DIRSTRY PLIST_FILES \
+		USES
 _OPTIONS_DEPENDS=	PKG FETCH EXTRACT PATCH BUILD LIB RUN
 
 # Set the default values for the global options, as defined by portmgr
 .if !defined(NOPORTDOCS)
 PORT_OPTIONS+=	DOCS
 .else
-OPTIONS_WARNINGS+= "NOPORTDOCS"
+OPTIONS_WARNINGS+=		"NOPORTDOCS"
 WITHOUT+=			DOCS
 OPTIONS_WARNINGS_UNSET+=	DOCS
 .endif
@@ -154,7 +151,7 @@ WITHOUT+=		NLS
 .if !defined(NOPORTEXAMPLES)
 PORT_OPTIONS+=	EXAMPLES
 .else
-OPTIONS_WARNINGS+= "NOPORTEXAMPLES"
+OPTIONS_WARNINGS+=		"NOPORTEXAMPLES"
 WITHOUT+=			EXAMPLES
 OPTIONS_WARNINGS_UNSET+=	EXAMPLES
 .endif
