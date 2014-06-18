e7ed3b7b784ef0a6bb0496e188d5918298b93637
Diane Bruce <db@FreeBSD.org>
2014-06-18 01:01:31 +0000

    - update to latest - the ARRL licence is a 6 part BSD
    PR:		ports/190172
    Submitted by:	shurd

--- comms/trustedqsl/files/patch-apps_tqsl.cpp
+++ comms/trustedqsl/files/patch-apps_tqsl.cpp
@@ -1,29 +1,11 @@
---- apps/tqsl.cpp.orig	2013-10-20 20:33:20.000000000 -0400
-+++ apps/tqsl.cpp	2014-01-26 10:20:28.036914222 -0500
-@@ -71,7 +71,7 @@
+--- apps/tqsl.cpp	2014-05-08 17:59:25.000000000 -0700
++++ ../../work.fixed/tqsl-2.0.2/apps/tqsl.cpp	2014-05-24 00:30:44.000000000 -0700
+@@ -54,7 +54,7 @@
  #endif
  #include <zlib.h>
  #include <openssl/opensslv.h> // only for version info!
 -#include <db.h> //only for version info!
 +#include <db5/db.h> //only for version info!
- #include "tqslwiz.h"
- #include "qsodatadialog.h"
- #include "tqslerrno.h"
-@@ -1862,7 +1862,7 @@
- 				return TQSL_EXIT_LIB_ERROR;
- 			}
- 		} else {
--			out<<output;
-+			out<<output.mb_str();
- 			if (out.fail()) {
- 				tqsl_converterRollBack(conv);
- 				tqsl_endConverter(&conv);
-@@ -2016,7 +2016,7 @@
- 	} else {
- 		//compress the upload
- 		string compressed;
--		size_t compressedSize=compressToBuf(compressed, (const char*)signedOutput.mb_str());
-+		ssize_t compressedSize=compressToBuf(compressed, (const char*)signedOutput.mb_str());
- 		//ofstream f; f.open("testzip.tq8", ios::binary); f<<compressed; f.close(); //test of compression routine
- 		if (compressedSize<0) { 
- 			wxLogMessage(wxT("Error compressing before upload")); 
+ 
+ #include <iostream>
+ #include <fstream>
