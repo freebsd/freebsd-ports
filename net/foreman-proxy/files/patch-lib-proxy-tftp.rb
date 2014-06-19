4e873dae5817817a40a032a3325064354da90925
Martin Matuska <mm@FreeBSD.org>
2014-06-19 13:56:09 +0000

    Update foreman-proxy to 1.5.1

--- net/foreman-proxy/files/patch-lib-proxy-tftp.rb
+++ net/foreman-proxy/files/patch-lib-proxy-tftp.rb
@@ -1,14 +1,14 @@
---- lib/proxy/tftp.rb.orig	2014-01-29 11:01:36.000000000 +0100
-+++ lib/proxy/tftp.rb	2014-02-08 12:38:15.087475784 +0100
-@@ -105,8 +105,9 @@
+--- lib/proxy/tftp.rb.orig	2014-06-18 10:54:21.000000000 +0200
++++ lib/proxy/tftp.rb	2014-06-19 15:34:09.887122187 +0200
+@@ -107,8 +107,9 @@
        # as the dst might contain another sub directory
        FileUtils.mkdir_p destination.parent
  
 -      wget = which("wget")
--      cmd = "#{wget} --timeout=10 --tries=3 --no-check-certificate -nv -c #{src} -O \"#{destination}\""
-+      # wget = which("wget")
-+      # cmd = "#{wget} --timeout=10 --tries=3 --no-check-certificate -nv -c #{src} -O \"#{destination}\""
-+      cmd = "/usr/bin/fetch --timeout=10 --no-verify-hostname --no-verify-peer -a -m -q -o \"#{destination}\" #{src}"
+-      cmd = "#{wget} --timeout=10 --tries=3 --no-check-certificate -nv -c \"#{escape_for_shell(src.to_s)}\" -O \"#{escape_for_shell(destination.to_s)}\""
++#      wget = which("wget")
++#      cmd = "#{wget} --timeout=10 --tries=3 --no-check-certificate -nv -c \"#{escape_for_shell(src.to_s)}\" -O \"#{escape_for_shell(destination.to_s)}\""
++      cmd = "/usr/bin/fetch --timeout=10 --no-verify-hostname --no-verify-peer -a -m -q -o \"#{escape_for_shell(destination.to_s)}\" \"#{escape_for_shell(src.to_s)}\""
        CommandTask.new(cmd)
      end
    end
