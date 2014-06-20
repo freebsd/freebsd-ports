61ef0492cb05b9e34f44deb632ba10acdc65e35c
marino <marino@FreeBSD.org>
2014-06-20 10:20:17 +0000

    emulators/visualboyadvance-m: Upgrade version r1001 => r1231
    Improvements:
      * Add Link support
      * Add Lirc support
      * Patch SDL interface (thanks dennylin93)
      * Several new translations
      * Many small fixes to various titles
    
    PR:		189647
    Submitted by:	maintainer (Nicole Reid)

---
+++ emulators/visualboyadvance-m/files/patch-src-gba-GBALink.cpp
@@ -0,0 +1,14 @@
+--- src/gba/GBALink.cpp.orig	2014-04-29 17:12:28.260667843 -0700
++++ src/gba/GBALink.cpp	2014-04-29 17:15:31.307655026 -0700
+@@ -2,9 +2,10 @@
+ // with major changes by tjm
+ #include <string.h>
+ #include <stdio.h>
++#include <sys/param.h>
+ 
+ // malloc.h does not seem to exist on Mac OS 10.7
+-#ifdef __APPLE__
++#if defined(__APPLE__) || defined(__FreeBSD__)
+ #include <stdlib.h>
+ #else
+ #include <malloc.h>
