af7410e2d474af6ea4cc106677d1ec19ef14b351
oliver <oliver@FreeBSD.org>
2014-06-21 15:09:24 +0000

    update audacious to 3.5 audclient got removed upstream so this breaks some ports

--- multimedia/audacious/files/patch-src-audacious-util.c
+++ multimedia/audacious/files/patch-src-audacious-util.c
@@ -1,14 +1,33 @@
---- src/audacious/util.c.orig	2011-11-08 17:37:12.000000000 +0100
-+++ src/audacious/util.c	2011-11-23 20:49:18.000000000 +0100
-@@ -142,9 +142,9 @@
+--- src/audacious/util.c.orig	2014-04-23 04:36:33.000000000 +0200
++++ src/audacious/util.c	2014-06-19 23:47:29.000000000 +0200
+@@ -35,6 +35,9 @@
+ 
+ #include <libaudcore/audstrings.h>
+ 
++#include <sys/types.h>
++#include <sys/sysctl.h>
++
+ #include "debug.h"
+ #include "i18n.h"
+ #include "misc.h"
+@@ -212,7 +215,19 @@
              return NULL;
-         }
+     }
  #else
--        if ((len = readlink ("/proc/self/exe", buf, size)) < 0)
-+        if ((len = readlink ("/proc/curproc/file", buf, size)) < 0)
-         {
--            fprintf (stderr, "Cannot access /proc/self/exe: %s.\n", strerror (errno));
-+            fprintf (stderr, "Cannot access /proc/curproc/file: %s.\n", strerror (errno));
-             g_free (buf);
-             return NULL;
-         }
+-    return NULL;
++    char path[PATH_MAX + 1];
++    size_t len = PATH_MAX + 1;
++    int mib[4];
++
++    mib[0] = CTL_KERN;
++    mib[1] = KERN_PROC;
++    mib[2] = KERN_PROC_PATHNAME;
++    mib[3] = -1;
++
++    if (sysctl(mib, 4, &path, &len, NULL, 0) != 0)
++        return NULL;
++
++    return str_get (path);
+ #endif
+ }
+ 
