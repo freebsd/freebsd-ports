2a03266e09a78fe0ac3f6eed4070b82aeb4a4727
rakuco <rakuco@FreeBSD.org>
2014-06-22 18:44:17 +0000

    Update to 1.1.1.
    PR:		191271
    Submitted by:	gaod@hychen.org (maintainer)

--- www/pecl-solr/files/patch-php_solr.c
+++ www/pecl-solr/files/patch-php_solr.c
@@ -1,11 +1,11 @@
---- php_solr.c.orig	2013-01-28 00:00:42.000000000 +0800
-+++ php_solr.c	2013-01-28 00:00:54.000000000 +0800
-@@ -879,8 +879,6 @@ static zend_function_entry solr_utils_me
+--- php_solr.c.orig	2014-06-22 19:01:39.785187204 +0800
++++ php_solr.c	2014-06-22 19:02:20.171185939 +0800
+@@ -901,8 +901,6 @@
  
  /* {{{ solr_module_deps  */
  static zend_module_dep solr_module_deps[] = {
--    ZEND_MOD_REQUIRED(PHP_CURL_EXTENSION_NAME)
 -    ZEND_MOD_REQUIRED(PHP_LIBXML_EXTENSION_NAME)
- 
+-    ZEND_MOD_REQUIRED(PHP_JSON_EXTENSION_NAME)
      { NULL, NULL, NULL }
  };
+ /* }}} */
