5622aecd2cda4d3562577e03264d55b897c5ecc9
olivierd <olivierd@FreeBSD.org>
2014-06-24 20:28:18 +0000

    - Switch to USES= libtool:keepla tar:bzip2 - Replace FreshPorts by Bugzilla url (patch-src__smartbookmark.c) - Bump PORTREVISION

--- www/xfce4-smartbookmark-plugin/files/patch-src__smartbookmark.c
+++ www/xfce4-smartbookmark-plugin/files/patch-src__smartbookmark.c
@@ -1,5 +1,5 @@
 --- ./src/smartbookmark.c.orig	2013-01-03 21:18:38.000000000 +0000
-+++ ./src/smartbookmark.c	2013-01-21 20:08:22.000000000 +0000
++++ ./src/smartbookmark.c	2014-06-02 19:08:44.000000000 +0000
 @@ -183,9 +183,9 @@
      box = gtk_vbox_new(FALSE, 0);
  
@@ -7,9 +7,9 @@
 -    search->url = "http://bugs.debian.org/";
 -    search->label_text = "BTS";
 -    search->size = 5;
-+    search->url = "http://www.freshports.org/search.php?query=";
-+    search->label_text = "FreshPorts";
-+    search->size = 12;
++    search->url = "https://bugs.freebsd.org/bugzilla/";
++    search->label_text = "Bugzilla";
++    search->size = 10;
      search->hide_label = FALSE;
      /* read config file options */
      filename = xfce_panel_plugin_save_location(plugin, TRUE);
@@ -20,9 +20,9 @@
 -        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","http://bugs.debian.org/"));
 -        search->label_text = g_strdup(xfce_rc_read_entry(rcfile,"value","DBS"));
 -        search->size = xfce_rc_read_int_entry(rcfile, "size", 5);
-+        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","http://www.freshports.org/search.php?query="));
-+        search->label_text = g_strdup(xfce_rc_read_entry(rcfile,"value","FreshPorts"));
-+        search->size = xfce_rc_read_int_entry(rcfile, "size", 12);
++        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","https://bugs.freebsd.org/bugzilla/"));
++        search->label_text = g_strdup(xfce_rc_read_entry(rcfile,"value","Bugzilla"));
++        search->size = xfce_rc_read_int_entry(rcfile, "size", 10);
          search->hide_label = xfce_rc_read_bool_entry(rcfile, "hidelabel", FALSE);
      }
  }
