7a699892f722e7c8dcf570400c0eea28ed2c61e7
Jung-uk Kim <jkim@FreeBSD.org>
2014-06-24 02:40:06 +0000

    Update to 4.2.5.

--- editors/libreoffice/files/patch-shell__Module_shell.mk
+++ editors/libreoffice/files/patch-shell__Module_shell.mk
@@ -1,11 +1,11 @@
---- ./shell/Module_shell.mk.orig	2013-01-31 05:54:53.000000000 -0500
-+++ ./shell/Module_shell.mk	2013-02-19 20:26:39.000000000 -0500
-@@ -32,7 +32,7 @@
+--- shell/Module_shell.mk.orig	2014-06-12 04:25:19.000000000 -0400
++++ shell/Module_shell.mk	2014-06-23 18:40:42.000000000 -0400
+@@ -14,7 +14,7 @@
  	Library_localebe \
  ))
  
 -ifeq ($(OS),LINUX)
-+ifeq ($(GUI),UNX)
++ifneq ($(filter DRAGONFLY FREEBSD LINUX NETBSD OPENBSD,$(OS)),)
  ifeq ($(ENABLE_GIO),TRUE)
  $(eval $(call gb_Module_add_targets,shell,\
  	Library_losessioninstall \
