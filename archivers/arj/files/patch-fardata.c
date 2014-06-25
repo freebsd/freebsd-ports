52919007a995d44a7926bcdfb06fe8d2157b120e
Alexey Dokuchaev <danfe@FreeBSD.org>
2014-06-25 03:23:59 +0000

    - Remove BROKEN_powerpc, builds and runs just fine (tested on Mac mini G4) - Avoid changing the same file both via the patch and with sed(1) - Now that with staging PLIST_FILES become long again, resurrect pkg-plist - Improve COMMENT, port description, sort the knobs, and cleanup the port

--- archivers/arj/files/patch-fardata.c
+++ archivers/arj/files/patch-fardata.c
@@ -1,6 +1,24 @@
 --- fardata.c.orig	2010-01-18 10:06:57.000000000 -0200
 +++ fardata.c	2010-01-18 10:08:24.000000000 -0200
-@@ -655,7 +655,7 @@
+@@ -190,7 +190,7 @@ int msg_sprintf(char *str, FMSG *fmt, ..
+ 
+ /* Length-limited strlen() */
+ 
+-static int strnlen(const char FAR *s, int count)
++static int _strnlen(const char FAR *s, int count)
+ {
+  const char FAR *sc;
+ 
+@@ -569,7 +569,7 @@ int vcprintf(int ccode, FMSG *fmt, va_li
+     if(!s)
+      s="(null)";
+ #endif
+-    len=strnlen(s, precision);
++    len=_strnlen(s, precision);
+     if(!(flags&LEFT))
+     {
+      while(len<field_width--)
+@@ -655,7 +655,7 @@ int vcprintf(int ccode, FMSG *fmt, va_li
      num=va_arg(args, unsigned long);
     else if(qualifier=='h')
     {
