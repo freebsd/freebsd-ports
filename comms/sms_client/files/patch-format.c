652967646e03c447133b67cfc676f0c6cf75fff2
John Marino <marino@FreeBSD.org>
2014-06-25 21:52:43 +0000

    Revive comms/sms_client with stage support and a new maintainer
    This port was removed August 2011 for lack of a public distfile.  It's
    been cleaned up and the distfile has a new home.  Pass maintainership
    to submitter.
    
    PR:		188971
    Submitted by:	Chris Hutchinson

---
+++ comms/sms_client/files/patch-format.c
@@ -0,0 +1,10 @@
+--- src/common/format.c.orig	2014-04-24 11:16:10.068089322 -0700
++++ src/common/format.c	2014-04-24 11:17:12.226546612 -0700
+@@ -249,6 +249,7 @@
+ 					break;
+
+ 				default:
++					break;
+ 			}
+
+ 			fmt++;
