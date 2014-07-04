a0f283b8e69400004c77661ebf63f8b0920a3d29
Matthew Seaman <matthew@FreeBSD.org>
2014-07-04 05:32:09 +0000

    Update to 0.21
    ChangeLog:	http://cpansearch.perl.org/src/FALCONE/RT-Authen-ExternalAuth-0.21/ChangeLog

--- www/p5-RT-Authen-ExternalAuth/files/patch-inc__Module__Install__RTx.pm
+++ www/p5-RT-Authen-ExternalAuth/files/patch-inc__Module__Install__RTx.pm
@@ -1,14 +1,11 @@
-
-$FreeBSD$
-
---- inc/Module/Install/RTx.pm.orig
-+++ inc/Module/Install/RTx.pm
-@@ -101,7 +101,7 @@
+--- ./inc/Module/Install/RTx.pm.orig	2014-07-02 03:19:37.000000000 +0100
++++ ./inc/Module/Install/RTx.pm	2014-07-03 22:52:03.274581922 +0100
+@@ -77,7 +77,7 @@
      my %index = map { $_ => 1 } @INDEX_DIRS;
      $self->no_index( directory => $_ ) foreach grep !$index{$_}, @DIRS;
  
 -    my $args = join ', ', map "q($_)", map { ($_, $path{$_}) }
 +    my $args = join ', ', map "q($_)", map { ($_, "\${DESTDIR}$path{$_}") }
-         grep $subdirs{$_}, keys %path;
+         sort keys %path;
  
-     print "./$_\t=> $path{$_}\n" for sort keys %subdirs;
+     printf "%-10s => %s\n", $_, $path{$_} for sort keys %path;
