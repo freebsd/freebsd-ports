32b2d43624b28bb73f252f15aad4c92e30d3f2cd
matthew <matthew@FreeBSD.org>
2014-07-04 05:33:06 +0000

    Update to 1.01
    ChangeLog:	http://cpansearch.perl.org/src/ALEXMV/RT-Extension-SLA-1.01/Changes

--- www/p5-RT-Extension-SLA/files/patch-inc__Module__Install__RTx.pm
+++ www/p5-RT-Extension-SLA/files/patch-inc__Module__Install__RTx.pm
@@ -1,14 +1,11 @@
-
-$FreeBSD$
-
---- inc/Module/Install/RTx.pm.orig
-+++ inc/Module/Install/RTx.pm
-@@ -100,7 +100,7 @@
+--- ./inc/Module/Install/RTx.pm.orig	2014-07-01 20:40:45.000000000 +0100
++++ ./inc/Module/Install/RTx.pm	2014-07-03 23:02:03.735533399 +0100
+@@ -77,7 +77,7 @@
      my %index = map { $_ => 1 } @INDEX_DIRS;
      $self->no_index( directory => $_ ) foreach grep !$index{$_}, @DIRS;
  
 -    my $args = join ', ', map "q($_)", map { ($_, $path{$_}) }
 +    my $args = join ', ', map "q($_)", map { ($_, "\${DESTDIR}$path{$_}") }
-         grep $subdirs{$_}, keys %path;
+         sort keys %path;
  
-     print "./$_\t=> $path{$_}\n" for sort keys %subdirs;
+     printf "%-10s => %s\n", $_, $path{$_} for sort keys %path;
