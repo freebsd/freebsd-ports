45292c1a2509fd6f19557740f03c1bdccaa36ce0
marino <marino@FreeBSD.org>
2014-07-06 09:41:02 +0000

    science/paje: Stage, bring in DragonFly support from dports

--- science/paje/files/patch-DrawView+Finding.m
+++ science/paje/files/patch-DrawView+Finding.m
@@ -1,10 +1,10 @@
 --- SpaceTimeViewer/DrawView+Finding.m.orig	2006-10-29 20:52:48.000000000 +0100
-+++ SpaceTimeViewer/DrawView+Finding.m	2012-01-14 08:09:36.000000000 +0100
++++ SpaceTimeViewer/DrawView+Finding.m
 @@ -24,7 +24,9 @@
  #include "DrawView.h"
  
  #include <math.h>
-+#if !defined(__FreeBSD__)
++#if !defined(__FreeBSD__) && !defined(__DragonFly__)
  #include <values.h>
 +#endif
  
