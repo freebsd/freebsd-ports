136fdffb5c192b746ca85efa42e403d648b2fd67
Guido Falsi <madpilot@FreeBSD.org>
2014-07-07 19:33:00 +0000

    - Update libtorrent to 0.13.4 - Update rtorrent to 0.9.4 - Convert to new LIB_DEPENDS format - Remove uneeded ?= - Remove libtorrent-devel and rtorrent-devel ports since the non   devel port has a newer version and no development release is available   anymore
    PR:		191621
    Submitted by:	Colin <anonymous.bug.report@gmail.com>

--- net-p2p/libtorrent/files/patch-src_torrent_connection__manager.h
+++ net-p2p/libtorrent/files/patch-src_torrent_connection__manager.h
@@ -1,5 +1,5 @@
---- ./src/torrent/connection_manager.h.orig	2013-12-15 17:55:18.427951513 +0400
-+++ ./src/torrent/connection_manager.h	2013-12-15 17:55:43.459221755 +0400
+--- src/torrent/connection_manager.h
++++ src/torrent/connection_manager.h
 @@ -42,6 +42,7 @@
  
  #include <list>
