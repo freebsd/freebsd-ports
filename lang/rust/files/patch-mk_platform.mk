df4a2efa1aac368ce183650c3b075d0a5866bd3d
Bartek Rutkowski <robak@FreeBSD.org>
2014-07-11 07:27:37 +0000

    lang/rust: update 0.10 -> 0.11
    Approved by:	marino (mentor)

--- lang/rust/files/patch-mk_platform.mk
+++ lang/rust/files/patch-mk_platform.mk
@@ -1,11 +1,11 @@
---- mk/platform.mk.orig	2014-01-20 12:47:42.902789736 +0800
-+++ mk/platform.mk	2014-01-20 12:47:53.647790608 +0800
-@@ -522,7 +522,7 @@
- CFG_STATIC_LIB_NAME_x86_64-unknown-freebsd=lib$(1).a
+--- mk/platform.mk.orig	2014-07-03 11:11:37.699489136 +0200
++++ mk/platform.mk	2014-07-03 11:13:34.454205727 +0200
+@@ -562,7 +562,7 @@
  CFG_LIB_GLOB_x86_64-unknown-freebsd=lib$(1)-*.so
  CFG_LIB_DSYM_GLOB_x86_64-unknown-freebsd=$(1)-*.dylib.dSYM
--CFG_GCCISH_CFLAGS_x86_64-unknown-freebsd := -Wall -Werror -g -fPIC -I/usr/local/include
-+CFG_GCCISH_CFLAGS_x86_64-unknown-freebsd := -Wall -g -fPIC -I/usr/local/include
+ CFG_CFLAGS_x86_64-unknown-freebsd := -I/usr/local/include $(CFLAGS)
+-CFG_GCCISH_CFLAGS_x86_64-unknown-freebsd := -Wall -Werror -g -fPIC -I/usr/local/include $(CFLAGS)
++CFG_GCCISH_CFLAGS_x86_64-unknown-freebsd := -Wall -g -fPIC -I/usr/local/include $(CFLAGS)
  CFG_GCCISH_LINK_FLAGS_x86_64-unknown-freebsd := -shared -fPIC -g -pthread  -lrt
  CFG_GCCISH_DEF_FLAG_x86_64-unknown-freebsd := -Wl,--export-dynamic,--dynamic-list=
  CFG_GCCISH_PRE_LIB_FLAGS_x86_64-unknown-freebsd := -Wl,-whole-archive
