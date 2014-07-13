231c8094957dc94631b2556282805a64de68f472
John Marino <marino@FreeBSD.org>
2014-07-13 10:12:12 +0000

    emulators/yabause: Update version 0.9.11.1 => 0.9.13.1 and stage
    PR:		191820
    Submitted by:	Ports Fury

--- emulators/yabause/files/patch-autogen.sh
+++
@@ -1,7 +0,0 @@
---- autogen.sh.orig	2012-03-25 20:41:13.000000000 -0500
-+++ autogen.sh	2012-03-25 20:42:38.000000000 -0500
-@@ -1,2 +1,2 @@
--aclocal && autoconf && automake --add-missing --copy
--cd src/c68k && aclocal && autoconf && automake
-+%%ACLOCAL%% && %%AUTOCONF%% && %%AUTOMAKE%% --add-missing --copy
-+cd src/c68k && %%ACLOCAL%% && %%AUTOCONF%% && %%AUTOMAKE%%
