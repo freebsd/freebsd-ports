8324a5caefc4874a23740a6a1929beb76a5cf60b
ehaupt <ehaupt@FreeBSD.org>
2014-07-15 12:24:32 +0000

    Add an Xaw 3d xft library support option.

---
+++ x11/xterm/files/patch-version.h
@@ -0,0 +1,19 @@
+--- ./version.h.orig	2014-07-13 23:54:02.000000000 +0200
++++ ./version.h	2014-07-15 09:09:15.488621885 +0200
+@@ -1,4 +1,4 @@
+-/* $XTermId: version.h,v 1.393 2014/07/13 21:54:02 tom Exp $ */
++/* $XTermId: version.h,v 1.394 2014/07/14 23:36:58 tom Exp $ */
+ 
+ /*
+  * Copyright 1998-2013,2014 by Thomas E. Dickey
+@@ -38,8 +38,8 @@
+  * version of X to which this version of xterm has been built.  The resulting
+  * number in parentheses is my patch number (Thomas E. Dickey).
+  */
+-#define XTERM_PATCH   309
+-#define XTERM_DATE    2014-07-13
++#define XTERM_PATCH   310
++#define XTERM_DATE    2014-07-14
+ 
+ #ifndef __vendorversion__
+ #define __vendorversion__ "XTerm"
