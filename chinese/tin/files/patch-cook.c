f05126f723a4c7f6e593f9d6a70fc104e248bddc
lwhsu <lwhsu@FreeBSD.org>
2014-07-15 17:28:52 +0000

    - Unbreak

--- chinese/tin/files/patch-cook.c
+++ chinese/tin/files/patch-cook.c
@@ -1,11 +1,11 @@
---- src/cook.c.orig	2011-08-18 03:58:24.288554846 +0800
-+++ src/cook.c	2011-08-18 13:36:23.791151539 +0800
-@@ -472,7 +472,12 @@
- 			break;	/* premature end of file, file error etc. */
+--- src/cook.c.orig	2014-07-16 00:43:47.656653653 +0800
++++ src/cook.c	2014-07-16 00:47:26.498655673 +0800
+@@ -471,7 +471,12 @@
  
  		/* convert network to local charset, tex2iso, iso2asc etc. */
+ 		ncharset = get_param(part->params, "charset");
 +#ifndef CHARSET_CONVERSION
- 		process_charsets(&line, &max_line_len, get_param(part->params, "charset"), tinrc.mm_local_charset, curr_group->attribute->tex2iso_conv && art->tex2iso);
+ 		process_charsets(&line, &max_line_len, ncharset ? ncharset : "US-ASCII" , tinrc.mm_local_charset, curr_group->attribute->tex2iso_conv && art->tex2iso);
 +#else
 +		/* use undeclared charset */
 +		process_charsets(&line, &max_line_len, get_param(part->params, "charset") ? get_param(part->params, "charset") : curr_group->attribute->undeclared_charset, tinrc.mm_local_charset, curr_group->attribute->tex2iso_conv && art->tex2iso);
