d2dfbc7f64612438b299b225b8b5f7377bd96656
Thierry Thomas <thierry@FreeBSD.org>
2014-07-21 21:19:04 +0000

    - Stagify - Fix pcre - Mark BROKEN with clang - Add license.

---
+++ databases/tora/files/patch-src_topiechart.h
@@ -0,0 +1,10 @@
+--- ./src/topiechart.h.orig	2009-10-23 18:19:33.000000000 +0200
++++ ./src/topiechart.h	2014-07-21 19:25:58.000000000 +0200
+@@ -44,6 +44,7 @@
+ 
+ #include "config.h"
+ 
++#include <time.h>
+ #include <list>
+ #include <qwidget.h>
+ 
