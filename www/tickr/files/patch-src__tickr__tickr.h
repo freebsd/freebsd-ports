f75d2aa2cdd1cbae6291bd0af1754f01618da360
Nicola Vitale <nivit@FreeBSD.org>
2014-07-22 23:24:03 +0000

    - Bump PORTREVISION (change pkg-plist - Take maintainership - Use desktop-file-utils and libtool helpers - Add support for stage dir - Install a predefined list of RSS feeds - Add files/patch-src__libetm-0.4.4__Makefile.in
    Build Log:	https://redports.org/buildarchive/20140722225327-99178/

--- www/tickr/files/patch-src__tickr__tickr.h
+++ www/tickr/files/patch-src__tickr__tickr.h
@@ -6,8 +6,8 @@
  #ifndef G_OS_WIN32
 -# define INSTALL_PATH		"/usr/share/" APP_CMD
 -# define IMAGES_PATH		INSTALL_PATH "/pixmaps"
-+# define INSTALL_PATH		"%%PREFIX%%/share/"
-+# define IMAGES_PATH		INSTALL_PATH "/pixmaps/" APP_CMD
++# define INSTALL_PATH		"%%DATADIR%%"
++# define IMAGES_PATH		"%%PREFIX%%/share/pixmaps/" APP_CMD
  #else
  # define IMAGES_PATH		APP_NAME	/* Actually not a path but a dir name */
  #endif
