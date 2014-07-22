4dec3bf11e4d2c01ad477e74c0856c58622daddc
kwm <kwm@FreeBSD.org>
2014-07-22 09:59:01 +0000

    Fix two problems.
    Use class instead of struct for RateCtl in encoderparams.hh. Class RateCtl is
      defined in ratectl.hh and used elsewhere in the code too.
    Fix headerguard typo in ontheflyratectlpass[12].hh.
    
    Found by:	Clang

---
+++ multimedia/mjpegtools/files/patch-mpeg2enc_ontheflyratectlpass1.hh
@@ -0,0 +1,9 @@
+--- mpeg2enc/ontheflyratectlpass1.hh.orig	2014-07-21 16:51:41.000000000 +0200
++++ mpeg2enc/ontheflyratectlpass1.hh	2014-07-21 16:52:29.000000000 +0200
+@@ -1,5 +1,5 @@
+ #ifndef _ONTHEFLYRATECTLPASS1_HH
+-#define _ONTHELFYRATECTLPASS1_HH
++#define _ONTHEFLYRATECTLPASS1_HH
+ 
+ /*  (C) 2003 Andrew Stevens */
+ 
