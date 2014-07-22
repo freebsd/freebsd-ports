0246ce141b5d6fc01b87202548d41d3b939db401
Adam Weinberger <adamw@FreeBSD.org>
2014-07-22 14:45:40 +0000

    Update to 3.8.1. Along with this, savegame data is now in /usr/local/var/games instead of /var/games. This was necessary because @sample cannot operate outside of %%PREFIX%%, and previously the port would overwrite or remove previously saved games. This is easier, better and more hier(7)ful.

--- games/bsdgames/files/patch-battlestar_externs.h
+++ games/bsdgames/files/patch-battlestar_externs.h
@@ -1,14 +1,3 @@
-diff -Naur battlestar.orig/fly.c battlestar/fly.c
---- battlestar.orig/fly.c	2012-03-22 13:59:00.000000000 +0100
-+++ battlestar/fly.c	2012-03-22 13:59:09.000000000 +0100
-@@ -294,6 +294,7 @@
- 	signal(SIGALRM, SIG_DFL);
- 	mvcur(0,COLS-1,LINES-1,0);
- 	endwin();
-+	setlinebuf(stdout);
- 	signal(SIGTSTP, SIG_DFL);
- 	signal(SIGINT, oldsig);
- }
 --- battlestar/externs.h.orig	2012-05-25 11:57:52.000000000 +0200
 +++ battlestar/externs.h	2012-05-25 11:57:55.000000000 +0200
 @@ -44,15 +44,18 @@
