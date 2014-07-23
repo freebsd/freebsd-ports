1e9f7f2bd97904cc9b2a564dc60edc85f9bf726a
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2014-07-23 20:00:28 +0000

    - Fix build on 11-CURRENT

---
+++ www/aria2/files/patch-src-DownloadEngine.cc
@@ -0,0 +1,10 @@
+--- src/DownloadEngine.cc.orig	2014-07-22 22:33:38.000000000 +0800
++++ src/DownloadEngine.cc	2014-07-24 02:58:37.228676022 +0800
+@@ -37,6 +37,7 @@
+ #include <signal.h>
+ 
+ #include <cstring>
++#include <cstdlib>
+ #include <cerrno>
+ #include <algorithm>
+ #include <numeric>
