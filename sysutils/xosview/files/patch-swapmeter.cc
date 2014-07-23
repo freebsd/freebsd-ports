bd7e951a06cd97d1f9b0dc0db12d8a9631844f59
vanilla <vanilla@FreeBSD.org>
2014-07-23 03:23:22 +0000

    1: Upgrade to 1.16. 2: Stagify.
    PR:		ports/177349
    Submitted by:	tomi.o.tapper at student.jyu.fi[1], me[2]
    Approved by:	maintainer

--- sysutils/xosview/files/patch-swapmeter.cc
+++
@@ -1,7 +0,0 @@
---- bsd/swapmeter.cc.orig	Tue Aug 21 10:52:35 2001
-+++ bsd/swapmeter.cc	Tue Aug 21 10:53:02 2001
-@@ -76,3 +76,3 @@
- void SwapMeter::getswapinfo( void ){
--  unsigned long long total_int, free_int;
-+  int64_t total_int, free_int;
- 
