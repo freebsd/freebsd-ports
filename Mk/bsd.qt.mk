61e264e947904ae4bbd6e1b521197730be6f7b70
rakuco <rakuco@FreeBSD.org>
2014-07-24 18:52:53 +0000

    qt4: Do not apply the patch from r362770 if EXTRA_PATCHES is unset.
    This unbreaks qmake4 and qt4-doc.

--- Mk/bsd.qt.mk
+++ Mk/bsd.qt.mk
@@ -172,7 +172,8 @@ EXTRA_PATCHES?=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-configure \
 .  if ${_QT_VERSION:M5*}
 EXTRA_PATCHES+=	${.CURDIR:H:H}/devel/qt5-core/files/extrapatch-src__corelib__tools__qdatetime.cpp
 .  elif ${_QT_VERSION:M4*}
-EXTRA_PATCHES+=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-src-corelib-global-qglobal.h
+EXTRA_PATCHES?=	${EXTRA_PATCHES} \
+				${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-src-corelib-global-qglobal.h
 .  endif
 . endif
 
