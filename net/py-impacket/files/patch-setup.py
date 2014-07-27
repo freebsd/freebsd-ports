68f61c9556a89e34c52c09ce6701c0a0f64a9b87
antoine <antoine@FreeBSD.org>
2014-07-27 10:06:49 +0000

    Update to 0.9.12

--- net/py-impacket/files/patch-setup.py
+++ net/py-impacket/files/patch-setup.py
@@ -1,11 +1,12 @@
---- ./setup.py.orig	2014-01-30 16:07:52.000000000 +0000
-+++ ./setup.py	2014-03-26 22:12:00.000000000 +0000
-@@ -21,8 +21,4 @@
+--- ./setup.py.orig	2014-07-27 09:34:58.000000000 +0000
++++ ./setup.py	2014-07-27 09:34:58.000000000 +0000
+@@ -21,9 +21,5 @@
        platforms = ["Unix","Windows"],
-       packages = ['impacket', 'impacket.dcerpc', 'impacket.examples', 'impacket.dcerpc.v5'],
+       packages = ['impacket', 'impacket.dcerpc', 'impacket.examples', 'impacket.dcerpc.v5', 'impacket.dcerpc.v5.dcom'],
        scripts = glob.glob(os.path.join('examples', '*.py')),
 -      data_files = [(os.path.join('share', 'doc', PACKAGE_NAME), ['README', 'LICENSE']+glob.glob('doc/*')),
 -                    (os.path.join('share', 'doc', PACKAGE_NAME, 'testcases', 'dot11'),glob.glob('impacket/testcases/dot11/*')),
 -                    (os.path.join('share', 'doc', PACKAGE_NAME, 'testcases', 'ImpactPacket'),glob.glob('impacket/testcases/ImpactPacket/*')),
--                    (os.path.join('share', 'doc', PACKAGE_NAME, 'testcases', 'SMB-RPC'),glob.glob('impacket/testcases/SMB-RPC/*'))],
+-                    (os.path.join('share', 'doc', PACKAGE_NAME, 'testcases', 'SMB_RPC'),glob.glob('impacket/testcases/SMB_RPC/*'))],
+       requires=['pycrypto (>=2.6)'],
        )
