b5e31c70a11bb03405b6d0bad6860be0129dcf33
Adam Weinberger <adamw@FreeBSD.org>
2014-07-28 14:22:16 +0000

    Rename biology/ patch-xy patches to reflect the files they modify.

--- biology/p5-AcePerl/files/patch-acelib_wh_mydirent.h
+++ biology/p5-AcePerl/files/patch-acelib_wh_mydirent.h
@@ -1,15 +1,3 @@
---- acelib/wh/mystdlib.h.orig	Mon Jul 16 23:04:45 2001
-+++ acelib/wh/mystdlib.h	Mon Aug 13 12:42:25 2001
-@@ -161,7 +161,8 @@
- #endif
- 
- #if defined(POSIX) || defined(LINUX) || defined(SOLARIS) || defined(SGI) || \
--	defined(HP) || defined(WIN32) || defined(INTEL_SOLARIS)
-+	defined(HP) || defined(WIN32) || defined(INTEL_SOLARIS) || \
-+	defined(FREEBSD)
- 
- #ifdef WIN32
- #include <mbctype.h>
 --- acelib/wh/mydirent.h.orig	Mon Jul 16 23:04:45 2001
 +++ acelib/wh/mydirent.h	Mon Aug 13 12:41:59 2001
 @@ -55,7 +55,7 @@
