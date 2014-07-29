a1f89b6003463bb5a09e384124b703da615489c5
Adam Weinberger <adamw@FreeBSD.org>
2014-07-29 22:58:34 +0000

    Fix build.

--- mail/gubby/files/patch-include_initcurs.h
+++ mail/gubby/files/patch-include_initcurs.h
@@ -1,9 +1,14 @@
-
-$FreeBSD$
-
---- include/initcurs.h.orig
-+++ include/initcurs.h
-@@ -41,7 +41,7 @@
+--- include/initcurs.h.orig	2004-12-17 23:29:19.000000000 -0500
++++ include/initcurs.h	2014-07-29 18:53:52.000000000 -0400
+@@ -33,6 +33,7 @@
+ #undef clear
+ 
+ #include <string>
++#include <stdlib.h>
+ 
+ //! Curses resource-aquisition-is-initialization
+ class initcurs {
+@@ -41,7 +42,7 @@
  	initcurs();
  	~initcurs();
  
