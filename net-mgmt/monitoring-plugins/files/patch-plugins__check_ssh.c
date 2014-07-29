fc70d30ac4af7b7255baf12e2a077937bb11cb73
Mathieu Arnold <mat@FreeBSD.org>
2014-07-29 14:43:17 +0000

    Update to 2.0.
    Changes:	https://www.monitoring-plugins.org/news/release-2-0.html
    Sponsored by:	Absolight

--- net-mgmt/monitoring-plugins/files/patch-plugins__check_ssh.c
+++ net-mgmt/monitoring-plugins/files/patch-plugins__check_ssh.c
@@ -1,6 +1,6 @@
---- ./plugins/check_ssh.c.orig	2014-04-27 19:59:06.000000000 +0200
-+++ ./plugins/check_ssh.c	2014-05-09 18:45:31.000000000 +0200
-@@ -250,6 +250,7 @@
+--- ./plugins/check_ssh.c.orig	2014-07-06 12:26:39.000000000 +0200
++++ ./plugins/check_ssh.c	2014-07-29 15:15:33.000000000 +0200
+@@ -255,6 +255,7 @@
  			printf
  				(_("SSH WARNING - %s (protocol %s) version mismatch, expected '%s'\n"),
  				 ssh_server, ssh_proto, remote_version);
@@ -8,7 +8,7 @@
  			close(sd);
  			exit (STATE_WARNING);
  		}
-@@ -260,6 +261,7 @@
+@@ -273,6 +274,7 @@
  			(_("SSH OK - %s (protocol %s) | %s\n"),
  			 ssh_server, ssh_proto, fperfdata("time", elapsed_time, "s",
  			 FALSE, 0, FALSE, 0, TRUE, 0, TRUE, (int)socket_timeout));
