1d16cf243a212499ae3ee66630392c003e09e69a
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2014-07-31 15:11:55 +0000

    - Update to 5.10 (5.1.6)

--- archivers/unrar/files/patch-rijndael.cpp
+++ archivers/unrar/files/patch-rijndael.cpp
@@ -1,15 +1,15 @@
---- rijndael.cpp.orig	2013-12-01 16:10:14.000000000 +0800
-+++ rijndael.cpp	2014-02-04 08:42:17.943981810 +0800
+--- rijndael.cpp.orig	2014-06-11 01:14:06.000000000 +0800
++++ rijndael.cpp	2014-07-31 22:46:27.956084561 +0800
 @@ -7,6 +7,8 @@
   ***************************************************************************/
  #include "rar.hpp"
  
 +#ifndef OPENSSL_AES
 +
- static byte S[256],S5[256],rcon[30];
- static byte T1[256][4],T2[256][4],T3[256][4],T4[256][4];
- static byte T5[256][4],T6[256][4],T7[256][4],T8[256][4];
-@@ -52,6 +54,7 @@
+ #ifdef USE_SSE
+ #include <wmmintrin.h>
+ #endif
+@@ -56,6 +58,7 @@
  #endif
  }
  
@@ -17,7 +17,7 @@
  
  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  // API
-@@ -59,13 +62,34 @@
+@@ -63,14 +66,35 @@
  
  Rijndael::Rijndael()
  {
@@ -25,6 +25,7 @@
    if (S[0]==0)
      GenerateTables();
 +#endif // OPENSSL_AES
+   CBCMode = true; // Always true for RAR.
  }
  
  
@@ -49,10 +50,10 @@
 +  EVP_CipherInit_ex(&ctx, cipher, NULL, key, initVector, Encrypt);
 +  EVP_CIPHER_CTX_set_padding(&ctx, 0);
 +#else // OPENSSL_AES
-   uint uKeyLenInBytes;
-   switch(keyLen)
-   {
-@@ -95,6 +119,7 @@
+ #ifdef USE_SSE
+   // Check SSE here instead of constructor, so if object is a part of some
+   // structure memset'ed before use, this variable is not lost.
+@@ -111,6 +135,7 @@
  
    if(!Encrypt)
      keyEncToDec();
@@ -60,35 +61,42 @@
  }
  
  
-@@ -104,6 +129,11 @@
-   if (input == 0 || inputLen <= 0)
-     return 0;
+@@ -120,6 +145,11 @@
+   if (inputLen <= 0)
+     return;
  
 +#ifdef OPENSSL_AES
 +  int outLen;
 +  EVP_CipherUpdate(&ctx, outBuffer, &outLen, input, inputLen);
-+  return outLen;
++  return;
 +#else // OPENSSL_AES
-   byte block[16], iv[4][4];
-   memcpy(iv,m_initVector,16); 
+   size_t numBlocks=inputLen/16;
+ #ifdef USE_SSE
+   if (AES_NI)
+@@ -182,6 +212,8 @@
+   }
  
-@@ -126,9 +156,11 @@
    memcpy(m_initVector,iv,16);
-   
-   return 16*numBlocks;
++
 +#endif // OPENSSL_AES
  }
  
  
+@@ -217,7 +249,7 @@
+ }
+ #endif
+ 
+-
 +#ifndef OPENSSL_AES
  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  // ALGORITHM
  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-@@ -309,6 +341,7 @@
+@@ -357,7 +389,7 @@
      U1[b][0]=U2[b][1]=U3[b][2]=U4[b][3]=T5[i][0]=T6[i][1]=T7[i][2]=T8[i][3]=FFmul0e(b);
    }
  }
+-
 +#endif // OPENSSL_AES
  
- 
  #if 0
+ static void TestRijndael();
