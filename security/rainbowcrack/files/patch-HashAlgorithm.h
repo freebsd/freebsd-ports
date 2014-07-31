aba771276cb66bfc0e4f7cd2679cee8d8b6a7f5c
Adam Weinberger <adamw@FreeBSD.org>
2014-07-31 16:26:58 +0000

    Stage and unbreak by adding missing headers and removing the MD2 code.

--- security/rainbowcrack/files/patch-HashAlgorithm.h
+++ security/rainbowcrack/files/patch-HashAlgorithm.h
@@ -1,12 +1,11 @@
---- ./HashAlgorithm.h.orig	2008-07-22 20:30:34.000000000 -0400
-+++ ./HashAlgorithm.h	2008-07-22 20:31:07.000000000 -0400
-@@ -8,7 +8,13 @@
+--- HashAlgorithm.h.orig	2014-07-31 12:12:45.000000000 -0400
++++ HashAlgorithm.h	2014-07-31 12:21:55.000000000 -0400
+@@ -8,7 +8,12 @@
  #define _HASHALGORITHM_H
  
  void HashLM(unsigned char* pPlain, int nPlainLen, unsigned char* pHash);
 +void HashNTLM(unsigned char* pPlain, int nPlainLen, unsigned char* pHash);
 +
-+void HashMD2(unsigned char* pPlain, int nPlainLen, unsigned char* pHash);
 +void HashMD4(unsigned char* pPlain, int nPlainLen, unsigned char* pHash);
  void HashMD5(unsigned char* pPlain, int nPlainLen, unsigned char* pHash);
 +
--- security/rainbowcrack/files/patch-HashRoutine.cpp
+++ security/rainbowcrack/files/patch-HashRoutine.cpp
@@ -1,11 +1,10 @@
---- ./HashRoutine.cpp.orig	2008-07-22 20:27:54.000000000 -0400
-+++ ./HashRoutine.cpp	2008-07-22 20:28:34.000000000 -0400
-@@ -18,8 +18,12 @@
+--- HashRoutine.cpp.orig	2014-07-31 12:12:45.000000000 -0400
++++ HashRoutine.cpp	2014-07-31 12:20:45.000000000 -0400
+@@ -18,8 +18,11 @@
  	// Notice: MIN_HASH_LEN <= nHashLen <= MAX_HASH_LEN
  
  	AddHashRoutine("lm",   HashLM,   8);
 +	AddHashRoutine("ntlm", HashNTLM, 16);
-+	AddHashRoutine("md2",  HashMD2,  16);
 +	AddHashRoutine("md4",  HashMD4,  16);
  	AddHashRoutine("md5",  HashMD5,  16);
  	AddHashRoutine("sha1", HashSHA1, 20);
