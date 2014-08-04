72dc6ca9be3ad647f1caeda62f9e5e604a0db82f
bapt <bapt@FreeBSD.org>
2014-08-04 23:01:26 +0000

    Add USES=execinfo to handle dependency on libexecinfo avoiding pull a useless libdependency on ports libexecinfo on freebsd 10+

---
+++ Mk/Uses/execinfo.mk
@@ -0,0 +1,17 @@
+# $FreeBSD$
+#
+# handle dependency on libexecinfo
+#
+# Feature:	execinfo
+# Usage:	USES=exexinfo
+#
+# Take no arguments
+
+.if !defined(_INCLUDE_USES_EXECINFO_MK)
+_INCLUDE_USES_EXECINFO_MK=	yes
+
+.if !exists(/usr/lib/libexecinfo.so)
+LIB_DEPENDS+=	libexecinfo.so:${PORTSDIR}/devel/libexecinfo
+.endif
+
+.endif
