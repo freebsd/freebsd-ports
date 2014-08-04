5e17b9936d359cb1f2a3ac0c261a290c5b8ba0c7
zi <zi@FreeBSD.org>
2014-08-04 12:05:40 +0000

    - Bring back heartbleed record, however, make it impossible to match on

--- net/freeradius3/files/patch-src__main__tls.c
+++ net/freeradius3/files/patch-src__main__tls.c
@@ -1,16 +1,13 @@
---- ./src/main/tls.c.orig	2014-07-29 15:55:29.000000000 -0400
-+++ ./src/main/tls.c	2014-07-29 15:57:18.000000000 -0400
-@@ -62,13 +62,6 @@
- /* Record critical defects in libssl here (newest first)*/
+--- ./src/main/tls.c.orig	2014-08-04 08:03:48.000000000 -0400
++++ ./src/main/tls.c	2014-08-04 08:04:14.000000000 -0400
+@@ -63,8 +63,8 @@
  static libssl_defect_t libssl_defects[] =
  {
--	{
+ 	{
 -		.low		= 0x010001000,		/* 1.0.1  */
 -		.high		= 0x01000106f,		/* 1.0.1f */
--		.id		= "CVE-2014-0160",
--		.name		= "Heartbleed",
--		.comment	= "For more information see http://heartbleed.com"
--	}
- };
- 
- /* record */
++		.low		= 0x090001000,		/* 9.0.1  */
++		.high		= 0x09000106f,		/* 9.0.1f */
+ 		.id		= "CVE-2014-0160",
+ 		.name		= "Heartbleed",
+ 		.comment	= "For more information see http://heartbleed.com"
