6ca9392f742e6500913fbbd73152e27d7544464b
pi <pi@FreeBSD.org>
2014-08-04 09:31:05 +0000

    net/whois: 5.0.23 -> 5.1.4, unbreak, passing maintainership to submitter
    PR:		190799
    Submitted by:	coco@executive-computing.de
    Approved by:	lapo@lapo.it (old maintainer)

---
+++ net/whois/files/patch-config.h
@@ -0,0 +1,11 @@
+--- config.h.orig	2014-06-08 18:25:19.000000000 +0200
++++ config.h	2014-06-08 18:25:39.000000000 +0200
+@@ -8,7 +8,7 @@
+ 
+ /* Configuration file */
+ /*
+-#define CONFIG_FILE "/etc/whois.conf"
++#define CONFIG_FILE "%%MWHOISCONF%%"
+ */
+ 
+ 
