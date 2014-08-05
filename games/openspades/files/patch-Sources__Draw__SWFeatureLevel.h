ce9da2be1abfc4e72cbff1fb273c2c2703fcb294
amdmi3 <amdmi3@FreeBSD.org>
2014-08-05 00:18:17 +0000

    OpenSpades is a compatible client of Ace of Spades 0.75.
    * Can connect to a vanilla/pyspades/pysnip server.
    * Uses OpenGL/AL for better visuals.
    * Open source, and cross platform.
    
    WWW: http://www.openspades.org/

---
+++ games/openspades/files/patch-Sources__Draw__SWFeatureLevel.h
@@ -0,0 +1,10 @@
+--- Sources/Draw/SWFeatureLevel.h.orig	2014-06-23 15:55:03.000000000 +0400
++++ Sources/Draw/SWFeatureLevel.h	2014-07-31 00:18:47.036897321 +0400
+@@ -55,6 +55,7 @@
+ #endif
+ 
+ #include <algorithm>
++#include <cmath>
+ #include <Core/ConcurrentDispatch.h>
+ #include <Core/Debug.h>
+ 
