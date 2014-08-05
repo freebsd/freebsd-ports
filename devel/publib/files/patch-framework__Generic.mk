bf828ec4a3de9d64bb38a4fbbdec3bfc0bda1611
jhale <jhale@FreeBSD.org>
2014-08-05 09:58:21 +0000

    - Stagify - Add LICENSE (GPLv2)

--- devel/publib/files/patch-framework__Generic.mk
+++ devel/publib/files/patch-framework__Generic.mk
@@ -1,8 +1,15 @@
---- framework/Generic.mk.orig	Tue Jan 11 00:49:05 2005
-+++ framework/Generic.mk	Tue Jan 11 00:50:08 2005
-@@ -48,7 +48,5 @@
+--- ./framework/Generic.mk.orig	2006-03-12 06:04:21.000000000 -0500
++++ ./framework/Generic.mk	2014-08-05 05:46:03.000000000 -0400
+@@ -43,12 +43,10 @@
+ pre-install post-install:
+ 	@true
+ install:
+-	$(INSTALL) -d "$(man3dir)"
++	$(INSTALL) -d "$(DESTDIR)$(man3dir)"
+ 	for i in $(srcdir)/*.3; do \
  		if test -f $$i; then \
- 		t="$(man3dir)/`basename $$i .3`.$(man3ext)"; \
+-		t="$(man3dir)/`basename $$i .3`.$(man3ext)"; \
++		t="$(DESTDIR)$(man3dir)/`basename $$i .3`.$(man3ext)"; \
  		$(INSTALL_MAN) $$i "$$t"; \
 -		gzip -9f "$$t"; \
 -		sh $(fwdir)/link-alternative-names $$t $(man3dir) $(man3ext); \
