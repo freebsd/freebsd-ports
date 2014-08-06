cd0c911794978c6b8519e0a2220032768c601e1e
nobutaka <nobutaka@FreeBSD.org>
2014-08-06 14:11:16 +0000

    Fix build on amd64.
    PR:		192364
    Submitted by:	Ivan Klymenko <fidaj@ukr.net>
    MFH:		2014Q3

--- sysutils/dd_rescue/files/patch-fmt_no.h
+++ sysutils/dd_rescue/files/patch-fmt_no.h
@@ -1,10 +1,10 @@
---- fmt_no.h.orig	2014-06-29 00:25:12.000000000 +0900
-+++ fmt_no.h	2014-06-29 00:28:32.000000000 +0900
+--- fmt_no.h.orig	2014-08-06 22:50:41.000000000 +0900
++++ fmt_no.h	2014-08-06 22:51:11.000000000 +0900
 @@ -4,6 +4,8 @@
  #define _FMT_NO_H
  
  #include <sys/types.h>
-+typedef long long loff_t;
++#include "ddr_ctrl.h"
 +
  char* fmt_int_b(unsigned char pre, unsigned char post, unsigned int scale,
  	       	loff_t no, const char* bold, const char* norm, 
