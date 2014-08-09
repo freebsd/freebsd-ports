72a345f2b6a212c4dcfbdf611198caac5886a3b6
Kris Moore <kmoore@FreeBSD.org>
2014-08-09 15:54:11 +0000

    - Update to 1.0.0
    Approved by: Maintainer

--- audio/pithos/files/patch-setup.py
+++ audio/pithos/files/patch-setup.py
@@ -1,22 +1,23 @@
---- setup.py	2013-12-05 06:59:17.000000000 -0800
-+++ setup.py	2014-01-01 23:25:57.944668958 -0800
-@@ -50,12 +50,13 @@
-         'License :: OSI Approved :: GPL License',
-         'Programming Language :: Python'
-     ],
-+
-     data_files=[
--        ('/usr/share/icons/hicolor/scalable/apps', [
-+        ('share/pithos/icons/hicolor/scalable/apps', [
-              'data/icons/scalable/apps/pithos-mono.svg',
-              'data/icons/scalable/apps/pithos.svg'
-          ]),
+--- setup.py.orig	2014-05-01 18:33:16.000000000 -0400
++++ setup.py	2014-07-24 20:19:53.091960936 -0400
+@@ -37,11 +37,11 @@
+ 
+ if sys.platform != 'win32':
+     data_files = [
+-        ('/usr/share/icons/hicolor/scalable/apps', ['data/icons/hicolor/pithos.svg']),
+-        ('/usr/share/icons/hicolor/48x48/apps', ['data/icons/hicolor/pithos-tray-icon.png']),
+-        ('/usr/share/icons/ubuntu-mono-dark/apps/16', ['data/icons/ubuntu-mono-dark/pithos-tray-icon.svg']),
+-        ('/usr/share/icons/ubuntu-mono-light/apps/16', ['data/icons/ubuntu-mono-light/pithos-tray-icon.svg']),
 -        ('/usr/share/applications', ['data/pithos.desktop'])
-+        ('share/pithos/applications', ['data/pithos.desktop'])
-     ],
-     package_data={
-         'pithos': [
-@@ -70,6 +71,7 @@
++        ('share/icons/hicolor/scalable/apps', ['data/icons/hicolor/pithos.svg']),
++        ('share/icons/hicolor/48x48/apps', ['data/icons/hicolor/pithos-tray-icon.png']),
++        ('share/icons/ubuntu-mono-dark/apps/16', ['data/icons/ubuntu-mono-dark/pithos-tray-icon.svg']),
++        ('share/icons/ubuntu-mono-light/apps/16', ['data/icons/ubuntu-mono-light/pithos-tray-icon.svg']),
++        ('share/applications', ['data/pithos.desktop'])
+     ]
+ else:
+     data_files = []
+@@ -78,6 +78,7 @@
      ],
      packages=find_packages(),
      include_package_data=True,
