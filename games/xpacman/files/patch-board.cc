0dbc01a290e871a86202d2537f15634884bf997a
Wen Heping <wen@FreeBSD.org>
2014-08-10 15:02:19 +0000

    - Update to 1.002 - Stage support - Fix build with clang
    PR:		192546
    Submitted by:	Vladimir Kondratiev <wulf@cicgroup.ru>

--- games/xpacman/files/patch-board.cc
+++ games/xpacman/files/patch-board.cc
@@ -1,15 +1,6 @@
 --- board.cc.orig	Tue Jul 18 11:03:22 1995
 +++ board.cc	Mon Mar 10 20:52:04 2008
-@@ -119,7 +119,7 @@
-  oldtemp=oldlist;
-  while (oldtemp) {	//delete elements in the now previous sprite list
-   oldnext=oldtemp->next;
--  delete oldnext;
-+  delete oldtemp;
-   oldtemp=oldnext;
-  }
- if (zero && oldlist) {	//personal thingie used for debug, not useful
-@@ -157,6 +157,7 @@
+@@ -527,6 +527,7 @@
  void Board::sprite(DynamicElement *g) {	//let dynamicelement be a sprite
   liststruct *temp=0,*last=0;		//resets to null
   liststruct *ny=new liststruct;		//get a new liststruct
