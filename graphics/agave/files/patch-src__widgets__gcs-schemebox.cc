5b9fd5277921ae09ba13536915cf44d9217c6ad8
John Marino <marino@FreeBSD.org>
2014-08-10 08:22:55 +0000

    graphics/agave: Upgrade version 0.4.2 => 0.4.7 and stage
    PR:		192477
    Submitted by:	Ports Fury

---
+++ graphics/agave/files/patch-src__widgets__gcs-schemebox.cc
@@ -0,0 +1,11 @@
+--- src/widgets/gcs-schemebox.cc.orig
++++ src/widgets/gcs-schemebox.cc
+@@ -45,7 +45,7 @@
+ 
+         SchemeBox::SchemeBox(Scheme schm)
+         {
+-            SchemeBox::SchemeBox();
++            SchemeBox();
+             set_scheme(schm);
+         }
+ 
