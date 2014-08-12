0dd1438ebc257694bc42998e04d940a54188daca
Adam Weinberger <adamw@FreeBSD.org>
2014-08-12 15:01:16 +0000

    Include sys/types.h to fix build.

---
+++ sysutils/pflogx/files/patch-include__Options.h
@@ -0,0 +1,10 @@
+--- ./include/Options.h.orig	2014-08-12 10:59:56.000000000 -0400
++++ ./include/Options.h	2014-08-12 11:00:09.000000000 -0400
+@@ -33,6 +33,7 @@
+ 
+ #include <string>
+ #include <vector>
++#include <sys/types.h>
+ 
+ using namespace std;
+ 
