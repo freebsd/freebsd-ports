2a570486ab6e90a1163f2710bf5b068f6e781439
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2014-08-13 14:14:35 +0000

    Update to 1.46.

--- sysutils/dd_rescue/files/patch-ffs.h
+++ sysutils/dd_rescue/files/patch-ffs.h
@@ -1,15 +1,22 @@
 --- ffs.h.orig	2014-02-22 05:39:33.000000000 +0900
-+++ ffs.h	2014-06-29 01:21:32.000000000 +0900
-@@ -27,7 +27,7 @@
++++ ffs.h	2014-08-13 22:46:51.000000000 +0900
+@@ -27,7 +27,15 @@
  #ifdef HAVE_ENDIAN_H
  #include <endian.h>
  #endif
--
 +#include <stdint.h>
+ 
++#ifndef __WORDSIZE
++#if defined(UINTPTR_MAX) && defined(UINT64_MAX) && (UINTPTR_MAX == UINT64_MAX)
++#define __WORDSIZE 64
++#else
++#define __WORDSIZE 32
++#endif
++#endif
  
  #ifdef HAVE_FFS
  # define myffs(x) ffs(x)
-@@ -50,7 +50,7 @@
+@@ -50,12 +58,9 @@
  # define myffsl(x) myffsl_c(x)
  #endif
  
@@ -17,8 +24,13 @@
 +#ifndef BYTE_ORDER
  # error Need to define __BYTE_ORDER
  #endif
- #ifndef __WORDSIZE
-@@ -88,7 +88,7 @@
+-#ifndef __WORDSIZE
+-# error Need to define __WORDSIZE
+-#endif
+ 
+ #ifndef HAVE_FFS
+ /** Find first (lowest) bit set in word val, returns a val b/w 1 and __WORDSIZE, 0 if no bit is set */
+@@ -88,7 +93,7 @@
  }
  #endif
  
@@ -27,7 +39,7 @@
  /** Find last (highest) bit set in word val, returns a val b/w __WORDSIZE and 1, 0 if no bit is set */
  static int myflsl(unsigned long val)
  {
-@@ -117,6 +117,8 @@
+@@ -117,6 +122,8 @@
  	}
  	return res;
  }
