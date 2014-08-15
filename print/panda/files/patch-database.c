fd85f3c4db18fcb54e2d635fab366aa188474ae8
mandree <mandree@FreeBSD.org>
2014-08-15 17:20:46 +0000

    Fix a few of its glaring bugs around type widths. Find newer BerkeleyDB versions. Link with PNG again.

--- print/panda/files/patch-database.c
+++ print/panda/files/patch-database.c
@@ -1,5 +1,5 @@
---- database.c.orig	Tue Apr 13 09:46:24 2004
-+++ database.c	Fri Apr 16 01:13:10 2004
+--- ./database.c.orig	2004-04-13 02:46:24.000000000 +0200
++++ ./database.c	2014-08-15 19:09:39.000000000 +0200
 @@ -160,6 +160,12 @@
      panda_windbwrite (document, key, value);
  #else
@@ -13,17 +13,17 @@
  #if defined DEBUG
    printf ("Storing (%s, %s) in 0x%08x\n", key, value, document->db);
  #endif
-@@ -168,13 +174,6 @@
-     panda_error (panda_true, "Cannot store null key\n");
+@@ -169,13 +175,6 @@
    if (value == NULL)
      panda_error (panda_true, "Cannot store null value\n");
--
+ 
 -#ifdef USE_EDB
 -
 -  e_db_data_set((E_DB_File *)document->db, key, value, strlen(value)+1);
 -#else
 -  DBT db_key, db_data;
 -  int ec;
- 
+-
    memset(&db_key, 0, sizeof(db_key));
    memset(&db_data, 0, sizeof(db_data));
+   db_key.data = key;
