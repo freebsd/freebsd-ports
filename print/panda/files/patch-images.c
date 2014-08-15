3d2f6f3e81e796db17fd3ba795539479d24e199a
Matthias Andree <mandree@FreeBSD.org>
2014-08-15 17:20:46 +0000

    Fix a few of its glaring bugs around type widths. Find newer BerkeleyDB versions. Link with PNG again.

--- print/panda/files/patch-images.c
+++ print/panda/files/patch-images.c
@@ -1,5 +1,14 @@
---- images.c.orig	2004-04-13 02:46:24.000000000 +0200
-+++ images.c	2010-03-29 12:55:47.000000000 +0200
+--- ./images.c.orig	2004-04-13 02:46:24.000000000 +0200
++++ ./images.c	2014-08-15 19:11:07.000000000 +0200
+@@ -835,7 +835,7 @@
+ 		 panda_object * imageObj, char *filename)
+ {
+   FILE *image;
+-  unsigned long width, height;
++  png_uint_32 width, height;
+   int bitdepth, colourtype, outColourType;
+   png_uint_32 i, rowbytes;
+   png_structp png;
 @@ -854,7 +854,7 @@
  
    // Check that it really is a PNG file
@@ -9,6 +18,15 @@
      panda_error (panda_true, "PNG file was invalid");
  
    // Start decompressing
+@@ -1399,7 +1399,7 @@
+ panda_imagesizePNG (int *width, int *height, char *filename) 
+ {
+   FILE * image;
+-  unsigned long pwidth, pheight;
++  png_uint_32 pwidth, pheight;
+   int bitdepth, colourtype;
+   png_structp png;
+   png_infop info;
 @@ -1411,7 +1411,7 @@
    
      // Check that it really is a PNG file
