7d88ed0cdb7a1799a0c92cde3121bf28e5d0b236
pi <pi@FreeBSD.org>
2014-08-15 18:11:34 +0000

    net-mgmt/cacti: replace most perl patches with shebangfix
    PR:		192618
    Submitted by:	Lars Engels <lars.engels@0x20.net>

--- net-mgmt/cacti/files/patch-scripts__3com_cable_modem.pl
+++
@@ -1,8 +0,0 @@
---- ./scripts/3com_cable_modem.pl.orig	2013-08-06 19:31:19.000000000 -0700
-+++ ./scripts/3com_cable_modem.pl	2014-08-11 18:37:08.000000000 -0700
-@@ -1,4 +1,4 @@
--#!/usr/bin/perl
-+#!/usr/local/bin/perl
- 
- open(PROCESS, "/bin/bash -c 'wget --quiet -O - http://192.168.100.1/cgibin/opcfg | grep \"10.18.18.11\" -c' |");
- $status = <PROCESS>;
--- net-mgmt/cacti/files/patch-scripts__unix_users.pl
+++
@@ -1,8 +0,0 @@
---- ./scripts/unix_users.pl.orig	2013-08-06 19:31:19.000000000 -0700
-+++ ./scripts/unix_users.pl	2014-08-11 18:37:08.000000000 -0700
-@@ -1,4 +1,4 @@
--#!/usr/bin/perl
-+#!/usr/local/bin/perl
- 
- my $grep_string = $ARGV[0];
- 
