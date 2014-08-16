784455eb9dedcba395598a419a9b491d95c0745d
John Marino <marino@FreeBSD.org>
2014-08-16 09:47:24 +0000

    textproc/teckit: Add header to fix build on lang/gcc47
    PR:		192698
    Submitted by:	Justin Hibbits
    Approved by:	maintainer (Nikola Lecic)

---
+++ textproc/teckit/files/patch-source_Compiler.cpp
@@ -0,0 +1,10 @@
+--- source/Compiler.cpp.orig	2008-04-07 14:21:12 UTC
++++ source/Compiler.cpp
+@@ -33,6 +33,7 @@ Description:
+ #include <iomanip>
+ #include <algorithm>
+ #include <cstring>
++#include <cstdio>
+ 
+ #include "zlib.h"
+ 
