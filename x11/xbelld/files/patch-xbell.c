73f94af7749da96f7e4350b5897cd4039fec1284
adamw <adamw@FreeBSD.org>
2014-08-16 18:10:14 +0000

    Fix build with clang.

---
+++ x11/xbelld/files/patch-xbell.c
@@ -0,0 +1,12 @@
+--- xbell.c.orig	2002-10-26 20:26:48.000000000 -0400
++++ xbell.c	2014-08-16 14:05:35.000000000 -0400
+@@ -38,8 +38,7 @@
+ #include    <sysexits.h>
+ 
+ #ifndef lint
+-static const char   rcsid[] =
+-	"$Id: xbell.c,v 1.1 2002/10/27 00:26:48 sethk Exp $";
++__RCSID("$Id: xbell.c,v 1.1 2002/10/27 00:26:48 sethk Exp $");
+ #endif /* !lint */
+ 
+ int
--- x11/xbelld/files/patch-xbelld.c
+++ x11/xbelld/files/patch-xbelld.c
@@ -1,9 +1,16 @@
-
-$FreeBSD$
-
---- xbelld.c.orig
-+++ xbelld.c
-@@ -268,7 +268,7 @@
+--- xbelld.c.orig	2002-10-26 20:28:36.000000000 -0400
++++ xbelld.c	2014-08-16 14:07:14.000000000 -0400
+@@ -60,8 +60,7 @@
+ #include    <dirent.h>
+ 
+ #ifndef lint
+-static const char rcsid[] =
+-	"$Id: xbelld.c,v 1.7 2002/10/27 00:28:36 sethk Exp $";
++__RCSID("$Id: xbelld.c,v 1.7 2002/10/27 00:28:36 sethk Exp $");
+ #endif /* !lint */
+ 
+ #define XskRFilename	"XskRFilename"
+@@ -268,7 +267,7 @@
      u_int   i;
  
      if (!hp->h_size)
@@ -12,7 +19,7 @@ $FreeBSD$
      i = hp->h_hash(data) % hp->h_size;
      if (!TAILQ_EMPTY(&(hp->h_buckets[i])))
      {
-@@ -914,7 +914,7 @@
+@@ -914,7 +913,7 @@
      close(dfd);
      for (nent = 0, dep = (struct dirent *)dbuf;
  	    (char *)dep - dbuf < nb;
@@ -21,7 +28,7 @@ $FreeBSD$
  	;
      _hash_resize(&(_bd.bd_sndhash), nent * 2);
      if (_bd.bd_defsnd)
-@@ -924,7 +924,7 @@
+@@ -924,7 +923,7 @@
      }
      for (dep = (struct dirent *)dbuf;
  	    (char *)dep - dbuf < nb;
