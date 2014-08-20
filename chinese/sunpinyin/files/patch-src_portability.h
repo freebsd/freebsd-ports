2be7f11e2f8581c29d78f60f0a13ce578af53c07
John Marino <marino@FreeBSD.org>
2014-08-20 23:36:35 +0000

    Stage chinese/sunpinyin and unbreak on FreeBSD 10+
    PR:		192844
    Submitted by:	Henry Hu

---
+++ chinese/sunpinyin/files/patch-src_portability.h
@@ -0,0 +1,10 @@
+--- src/portability.h.orig	2014-08-19 21:12:42.000000000 -0400
++++ src/portability.h	2014-08-19 21:12:51.000000000 -0400
+@@ -42,6 +42,7 @@
+ #include <stdio.h>
+ #include <math.h>
+ #include <stdint.h>
++#include <stdlib.h>
+ #include <string>
+ #include <cstring>
+ 
