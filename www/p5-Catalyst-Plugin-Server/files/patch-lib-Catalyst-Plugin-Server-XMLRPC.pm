20a74ca5209407090198f227be0b43459ac17af0
des <des@FreeBSD.org>
2014-08-22 13:40:47 +0000

    The previous commit was broken - don't know how I managed to install the result.  Fix the version requirement for Catalyst::DispatchType::Regex. While there, add a patch that ensures that errors are reported correctly, i.e. 500 for an internal error and 400 for an invalid request.

--- www/p5-Catalyst-Plugin-Server/files/patch-lib-Catalyst-Plugin-Server-XMLRPC.pm
+++ www/p5-Catalyst-Plugin-Server/files/patch-lib-Catalyst-Plugin-Server-XMLRPC.pm
@@ -1,6 +1,15 @@
---- lib/Catalyst/Plugin/Server/XMLRPC.pm.orig
-+++ lib/Catalyst/Plugin/Server/XMLRPC.pm
-@@ -512,7 +512,7 @@ Alias of $c->req->parameters
+--- lib/Catalyst/Plugin/Server/XMLRPC.pm.orig	2010-07-08 20:39:58.000000000 +0200
++++ lib/Catalyst/Plugin/Server/XMLRPC.pm	2014-08-22 15:37:10.637508261 +0200
+@@ -349,7 +349,7 @@
+                                             }
+                 ) {
+                     $c->req->xmlrpc->_error( 
+-                        $c, qq[Invalid XMLRPC request: No such method]
++                        $c, 400, qq[Invalid XMLRPC request: No such method]
+                     );
+                     last PREPARE;
+                 }
+@@ -512,7 +512,7 @@
  
      __PACKAGE__->mk_accessors(
          qw/ path prefix separator attribute convert_params
@@ -9,7 +18,7 @@
          /
      );
  
-@@ -532,6 +532,8 @@ Alias of $c->req->parameters
+@@ -532,6 +532,8 @@
                                  || $DefaultShowErrors );
          $self->xml_encoding( $c->config->{xmlrpc}->{xml_encoding} )
                  if $c->config->{xmlrpc}->{xml_encoding};
@@ -18,7 +27,16 @@
          $self->attribute($DefaultAttr);
          $self->convert_params( 1 );
  
-@@ -690,6 +692,9 @@ Alias of $c->req->parameters
+@@ -668,7 +670,7 @@
+ 
+         ### an error in parsing the request
+         } elsif ( $@ ) {
+-            $self->_error( $c, qq[Invalid XMLRPC request "$@"] );
++            $self->_error( $c, 400, qq[Invalid XMLRPC request "$@"] );
+             return;
+ 
+         ### something is wrong, but who knows what...
+@@ -690,6 +692,9 @@
          local $RPC::XML::ENCODING = $c->server->xmlrpc->config->xml_encoding
                  if $c->server->xmlrpc->config->xml_encoding;
          
@@ -28,7 +46,21 @@
          local $Clone::Fast::BREAK_REFS = 1;
  
          my $res = RPC::XML::response->new(clone($status));
-@@ -808,6 +813,13 @@ default encoding to C<UTF-8> for instance.
+@@ -700,10 +705,11 @@
+ 
+     ### record errors in the error and debug log -- just for convenience 
+     sub _error {
+-        my($self, $c, $msg) = @_;
++        my($self, $c, $code, $msg) = @_;
+ 
++	($code, $msg) = (500, $code) unless defined $msg;
+         $c->log->debug( $msg ) if $c->debug;
+-        $c->error( $msg );
++        $self->error( [ $code, $msg ] );
+     }
+ }
+ 
+@@ -808,6 +814,13 @@
  
  Defaults to C<us-ascii> which is the default of C<RPC::XML>.
  
