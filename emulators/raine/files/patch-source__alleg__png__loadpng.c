77d0a1428452368a1f115fa5d5aafbb7dfbc7942
Thomas Zander <riggs@FreeBSD.org>
2014-08-23 16:58:25 +0000

    - Update to version 0.51.9 - Update WWW in pkg-descr - Pet portlint
    PR:		192790
    Submitted by:	tkato432@yahoo.com
    Approved by:	mentors (implicit)

--- emulators/raine/files/patch-source__alleg__png__loadpng.c
+++ emulators/raine/files/patch-source__alleg__png__loadpng.c
@@ -1,6 +1,6 @@
---- source/png/loadpng.c.orig	2005-08-18 10:52:15.000000000 +0200
-+++ source/png/loadpng.c	2012-05-06 15:58:26.000000000 +0200
-@@ -275,7 +275,7 @@
+--- source/alleg/png/loadpng.c.orig
++++ source/alleg/png/loadpng.c
+@@ -322,7 +322,7 @@
       * the normal method of doing things with libpng).  REQUIRED unless you
       * set up your own error handlers in the png_create_read_struct() earlier.
       */
@@ -8,8 +8,8 @@
 +    if (setjmp(png_jmpbuf(png_ptr))) {
  	/* Free all of the memory associated with the png_ptr and info_ptr */
  	png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
- 	pack_fclose(fp);
-@@ -337,7 +337,7 @@
+ 	fclose(fp);
+@@ -384,7 +384,7 @@
       * the normal method of doing things with libpng).  REQUIRED unless you
       * set up your own error handlers in the png_create_read_struct() earlier.
       */
@@ -18,7 +18,7 @@
  	/* Free all of the memory associated with the png_ptr and info_ptr */
  	png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
  	/* If we get here, we had a problem reading the file */
-@@ -435,7 +435,7 @@
+@@ -482,7 +482,7 @@
       * the normal method of doing things with libpng).  REQUIRED unless you
       * set up your own error handlers in the png_create_read_struct() earlier.
       */
