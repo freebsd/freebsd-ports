d62e7a046f24630ef94cf08ce7d1f260365f2b05
adamw <adamw@FreeBSD.org>
2014-08-24 17:00:04 +0000

    Upgrade mail/dbmail to 3.2.0
    3.2.0 contains all the stability improvements done in 3.1 over the
    last year. On top of that, it has some nice additions of it's own:
    
    - auto-migrations; An admittedly little tested feature is automatic
    migrations of the database schema. Running any of the binaries against
    an up-to-date and standard 3.1 schema will try to do all the necessary
    upgrades if required.
    
    - static code audit; the Coverity service was used to flush out as
    many potential problems as possible.
    
    - new capabilities; some interesting new IMAP capabilities have been
    added. QRESYNC (rfc5162), LITERAL+ (rfc2088), UIDPLUS (rfc4315) will
    improve performance especially for mobile clients.
    
    Also:
    - Remove redundant option THREADS
    - Add additional mirror
    - Rework post-install for auto-migration scripts
    - Use @sample framework
    
    PR:		192967
    Submitted by:	maintainer

--- mail/dbmail/files/patch-src_clientbase.c
+++ mail/dbmail/files/patch-src_clientbase.c
@@ -1,28 +1,29 @@
---- src/clientbase.c.orig	2013-07-22 20:03:36.000000000 +0100
-+++ src/clientbase.c	2013-07-23 13:14:56.000000000 +0100
-@@ -159,14 +159,14 @@
+--- src/clientbase.c.orig	2014-08-23 14:01:38.000000000 +0100
++++ src/clientbase.c	2014-08-24 10:12:06.000000000 +0100
+@@ -151,7 +151,7 @@
  		client->tx		= STDOUT_FILENO;
  	} else {
  		/* server-side */
--		if ((serr = getnameinfo(c->saddr, c->saddr_len, client->dst_ip, NI_MAXHOST, client->dst_port, NI_MAXSERV, 
-+		if ((serr = getnameinfo(c->saddr, c->saddr->sa_len, client->dst_ip, NI_MAXHOST, client->dst_port, NI_MAXSERV, 
- 						NI_NUMERICHOST | NI_NUMERICSERV))) {
+-		if ((serr = getnameinfo(&c->saddr, c->saddr_len, client->dst_ip, 
++		if ((serr = getnameinfo(&c->saddr, c->saddr.sa_len, client->dst_ip, 
+ 						NI_MAXHOST, client->dst_port, 
+ 						NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV))) {
  			TRACE(TRACE_INFO, "getnameinfo::error [%s]", gai_strerror(serr));
- 		}
+@@ -159,7 +159,7 @@
  
  		/* client-side */
  		if (server_conf->resolveIP) {
--			if ((serr = getnameinfo(c->caddr, c->caddr_len, client->clientname, NI_MAXHOST, NULL, 0,
-+			if ((serr = getnameinfo(c->caddr, c->caddr->sa_len, client->clientname, NI_MAXHOST, NULL, 0,
- 						       	NI_NAMEREQD))) {
+-			if ((serr = getnameinfo(&c->caddr, c->caddr_len, client->clientname,
++			if ((serr = getnameinfo(&c->caddr, c->caddr.sa_len, client->clientname,
+ 						       	NI_MAXHOST-1, NULL, 0, NI_NAMEREQD))) {
  				TRACE(TRACE_INFO, "getnameinfo:error [%s]", gai_strerror(serr));
  			} 
-@@ -177,7 +177,7 @@
+@@ -170,7 +170,7 @@
  					client->clientname[0] ? client->clientname : "Lookup failed");
  		} else {
  
--			if ((serr = getnameinfo(c->caddr, c->caddr_len, client->src_ip, NI_MAXHOST, client->src_port,
-+			if ((serr = getnameinfo(c->caddr, c->caddr->sa_len, client->src_ip, NI_MAXHOST, client->src_port,
- 						       	NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV))) {
+-			if ((serr = getnameinfo(&c->caddr, c->caddr_len, client->src_ip,
++			if ((serr = getnameinfo(&c->caddr, c->caddr.sa_len, client->src_ip,
+ 						       	NI_MAXHOST-1, client->src_port,
+ 						       	NI_MAXSERV-1, NI_NUMERICHOST | NI_NUMERICSERV))) {
  				TRACE(TRACE_INFO, "getnameinfo:error [%s]", gai_strerror(serr));
- 			} 
