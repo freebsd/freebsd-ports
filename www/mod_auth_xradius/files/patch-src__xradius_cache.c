c05d77e2a5de77c718577a1b33e5e451afb8a39e
Mathieu Arnold <mat@FreeBSD.org>
2014-08-25 15:02:11 +0000

    Make the patch cleaner and always apply it.
    Sponsored by:	Absolight

--- www/mod_auth_xradius/files/patch-src__xradius_cache.c
+++ www/mod_auth_xradius/files/patch-src__xradius_cache.c
@@ -1,14 +1,20 @@
 --- ./src/xradius_cache.c.orig	2005-04-27 08:49:25.000000000 +0200
-+++ ./src/xradius_cache.c	2014-08-12 11:56:22.000000000 +0200
-@@ -143,9 +143,9 @@
++++ ./src/xradius_cache.c	2014-08-25 17:00:48.000000000 +0200
+@@ -143,10 +143,17 @@
      /* Running as Root */
      if (geteuid() == 0)  {
          /* Allow the configured Apache use to read/write to the DBM */
--        chown(path1, unixd_config.user_id, -1);
++#if MODULE_MAGIC_NUMBER_MAJOR >= 20111130
 +        chown(path1, ap_unixd_config.user_id, -1);
-         if (path2 != NULL) { 
--            chown(path2, unixd_config.user_id, -1);
++        if (path2 != NULL) { 
 +            chown(path2, ap_unixd_config.user_id, -1);
++        }
++#else
+         chown(path1, unixd_config.user_id, -1);
+         if (path2 != NULL) { 
+             chown(path2, unixd_config.user_id, -1);
          }
++#endif
      }
  #endif
+     
