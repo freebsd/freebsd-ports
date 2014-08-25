6e53519c3a06414a04c8308b49ab124be8c56f28
madpilot <madpilot@FreeBSD.org>
2014-08-25 10:16:42 +0000

    - Update games/ufoai and games/ufoai-data to 2.5 - Stagify games/ufoai - Modernize port - Fix UFORADIANT option (off by default) - Assign maintainership to submitter [1]
    PR:		192419
    Submitted by:	m.tsatsenko at gmail.com
    Approved by:	portmgr [1]

---
+++ games/ufoai/files/patch-src__libs__vorbis__config.h
@@ -0,0 +1,11 @@
+--- ./src/libs/vorbis/config.h.orig	2014-06-05 06:18:39.000000000 +0200
++++ ./src/libs/vorbis/config.h	2014-08-07 16:49:53.700302995 +0200
+@@ -14,7 +14,7 @@
+ 
+ /* Define to 1 if you have <alloca.h> and it should be used (not on Ultrix).
+    */
+-#define HAVE_ALLOCA_H 1
++/* #define HAVE_ALLOCA_H 1 */
+ 
+ /* Define to 1 if you have the <dlfcn.h> header file. */
+ #define HAVE_DLFCN_H 1
