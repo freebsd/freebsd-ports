9515607e8681885f505f56f72eb3e239ce415d15
rene <rene@FreeBSD.org>
2014-08-26 23:10:04 +0000

    devel/etl: - fix build with clang - bump PORTREVISION devel/synfig, graphics/synfigstudio:i - stage - unbreak - add license (GPLv3) - drop .la files - update USES - bump PORTREVISION
    PR:		193031
    Submitted by:	Ports Fury
    Approved by:	portmgr blanket (devel/etl)

---
+++ devel/etl/files/patch-ETL___surface.h
@@ -0,0 +1,10 @@
+--- ETL/_surface.h.orig
++++ ETL/_surface.h
+@@ -34,6 +34,7 @@
+ #include "_misc.h"
+ #include <algorithm>
+ #include <cstring>
++#include <cstdlib>
+ 
+ /* === M A C R O S ========================================================= */
+ 
