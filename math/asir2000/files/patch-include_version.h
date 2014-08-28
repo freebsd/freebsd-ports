d38c7c094a7f2ce7147451498a7f06600941b6d6
William Grzybowski <wg@FreeBSD.org>
2014-08-28 13:59:13 +0000

    math/asir2000: update to 20140819
    - Pass maintainership to submitter
    
    PR:		192873
    Submitted by:	Saito Tomokatsu
    Approved by:	maintainer

--- math/asir2000/files/patch-include_version.h
+++ math/asir2000/files/patch-include_version.h
@@ -1,8 +1,8 @@
---- include/version.h.orig	2011-08-08 14:41:05.000000000 +0900
-+++ include/version.h	2011-08-08 14:41:28.000000000 +0900
+--- include/version.h.orig	2014-08-19 15:35:02.000000000 +0900
++++ include/version.h	2014-08-25 02:37:52.000000000 +0900
 @@ -48,4 +48,4 @@
-  * $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.331 2011/07/21 04:43:26 noro Exp $ 
+  * $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.366 2014/08/19 06:35:02 noro Exp $ 
  */
- #define ASIR_VERSION 20110721
+ #define ASIR_VERSION 20140819
 -#define ASIR_DISTRIBUTION "Kobe"
 +#define ASIR_DISTRIBUTION "Plotting Group"
