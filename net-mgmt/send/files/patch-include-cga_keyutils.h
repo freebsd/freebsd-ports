4e9893c05f89009895ad60d72772aea459494552
hrs <hrs@FreeBSD.org>
2014-08-29 16:56:34 +0000

    Fix build on branches with OpenSSL 0.9.8.

--- net-mgmt/send/files/patch-include-cga_keyutils.h
+++ net-mgmt/send/files/patch-include-cga_keyutils.h
@@ -1,10 +1,10 @@
---- include/cga_keyutils.h.orig	2014-08-20 04:17:35.000000000 +0900
-+++ include/cga_keyutils.h	2014-08-20 04:17:58.000000000 +0900
+--- include/cga_keyutils.h.orig	2010-08-28 17:58:02.000000000 +0900
++++ include/cga_keyutils.h	2014-08-30 01:53:41.000000000 +0900
 @@ -36,6 +36,9 @@
  #include <openssl/x509.h>
  #include <openssl/evp.h>
  #include <openssl/stack.h>
-+#ifndef STACK
++#if OPENSSL_VERSION_NUMBER >= 0x0090900fL
 +#define	STACK _STACK
 +#endif
  #include <cga.h>
