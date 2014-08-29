c1472237a170e292f4bc32ec324bca571d46489e
hrs <hrs@FreeBSD.org>
2014-08-29 18:12:20 +0000

    - Fix build with newer versions of makeinfo. - Fix shebang line in sh and perl scripts. - Remove gcc dependency. - Move example files to EXAMPLESDIR.

---
+++ japanese/groff/files/patch-contrib-eqn2graph-eqn2graph.sh
@@ -0,0 +1,8 @@
+--- contrib/eqn2graph/eqn2graph.sh.orig	2014-08-30 03:07:55.000000000 +0900
++++ contrib/eqn2graph/eqn2graph.sh	2014-08-30 03:08:01.000000000 +0900
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/bin/sh
+ #
+ # eqn2graph -- compile EQN equation descriptions to bitmap images
+ #
