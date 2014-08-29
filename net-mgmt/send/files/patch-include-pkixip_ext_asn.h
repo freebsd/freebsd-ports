edc1598cfcaeb11371a508fd6806640219431195
Hiroki Sato <hrs@FreeBSD.org>
2014-08-29 16:56:34 +0000

    Fix build on branches with OpenSSL 0.9.8.

--- net-mgmt/send/files/patch-include-pkixip_ext_asn.h
+++ net-mgmt/send/files/patch-include-pkixip_ext_asn.h
@@ -1,11 +1,11 @@
---- include/pkixip_ext_asn.h.orig	2014-08-20 04:16:53.000000000 +0900
-+++ include/pkixip_ext_asn.h	2014-08-20 04:17:09.000000000 +0900
+--- include/pkixip_ext_asn.h.orig	2010-08-28 17:58:02.000000000 +0900
++++ include/pkixip_ext_asn.h	2014-08-30 01:54:23.000000000 +0900
 @@ -33,6 +33,10 @@
  #ifndef	_PKIXIP_EXT_ASN_H
  #define	_PKIXIP_EXT_ASN_H
  
 +#include <openssl/stack.h>
-+#ifndef STACK
++#if OPENSSL_VERSION_NUMBER >= 0x0090900fL
 +#define	STACK	_STACK
 +#endif
  #include <openssl/asn1t.h>
