537d732605166587be8d8cea3b2500fdc248ef1e
hrs <hrs@FreeBSD.org>
2014-08-30 22:19:55 +0000

    Update Mozc to 1.15.1868.102.

--- japanese/mozc-server/files/patch-build_mozc.py
+++ japanese/mozc-server/files/patch-build_mozc.py
@@ -1,20 +1,16 @@
---- build_mozc.py.orig	2013-07-17 11:37:50.000000000 +0900
-+++ build_mozc.py	2013-07-19 22:27:55.000000000 +0900
-@@ -332,6 +332,13 @@
-                     help='use rsync to copy files instead of builtin function')
-   AddTargetPlatformOption(parser)
- 
+--- build_mozc.py.orig	2014-08-31 03:36:22.000000000 +0900
++++ build_mozc.py	2014-08-31 06:04:34.000000000 +0900
+@@ -351,6 +351,9 @@
+   parser.add_option('--version_file', dest='version_file',
+                     help='use the specified version template file',
+                     default='mozc_version_template.txt')
 +  parser.add_option('--localbase', dest='localbase')
 +  parser.add_option('--ldflags', dest='ldflags')
 +  parser.add_option('--include_dirs', dest='include_dirs')
-+  parser.add_option('--openssl_cflags', dest='openssl_cflags')
-+  parser.add_option('--openssl_ldflags', dest='openssl_ldflags')
-+  parser.add_option('--openssl_lib', dest='openssl_lib')
-+  parser.add_option('--openssl_inc', dest='openssl_inc')
+   AddTargetPlatformOption(parser)
  
    # Mac and Linux
-   warn_as_error_default = False
-@@ -518,7 +525,7 @@
+@@ -538,7 +541,7 @@
    parser = optparse.OptionParser(usage='Usage: %prog build [options]')
    AddCommonOptions(parser)
    if IsLinux():
@@ -23,36 +19,17 @@
      parser.add_option('--jobs', '-j', dest='jobs',
                        default=('%d' % default_build_concurrency),
                        metavar='N', help='run build jobs in parallel')
-@@ -566,9 +573,6 @@
- 
-   return parser.parse_args(args, values)
- 
--
--
--
- def AddPythonPathToEnvironmentFilesForWindows(out_dir):
-   """Add PYTHONPATH to environment files for Ninja."""
-   mozc_root = os.path.abspath(GetTopLevelSourceDirectoryName())
-@@ -746,6 +750,22 @@
-   else:
-     command_line.extend(['-D', 'warn_as_error=0'])
+@@ -701,6 +704,13 @@
+   logging.info('Building GYP command line...')
+   gyp_options = ['--depth=.', '--include=%s/gyp/common.gypi' % SRC_DIR]
  
 +  localbase = options.localbase or '/usr'
-+  command_line.extend(['-D', 'localbase=%s' % localbase])
++  gyp_options.extend(['-D', 'localbase=%s' % localbase])
 +
 +  ldflags = options.ldflags or ''
-+  command_line.extend(['-D', 'ldflags=%s' % ldflags])
++  gyp_options.extend(['-D', 'ldflags=%s' % ldflags])
 +  include_dirs = options.include_dirs or ''
-+  command_line.extend(['-D', 'include_dirs=%s' % include_dirs])
-+
-+  openssl_cflags = options.openssl_cflags or ''
-+  command_line.extend(['-D', 'openssl_cflags=%s' % openssl_cflags])
-+  openssl_ldflags = options.openssl_cflags or ''
-+  command_line.extend(['-D', 'openssl_ldflags=%s' % openssl_ldflags])
-+  openssl_inc = options.openssl_inc or ''
-+  command_line.extend(['-D', 'openssl_inc=%s' % openssl_inc])
-+  openssl_lib = options.openssl_lib or ''
-+  command_line.extend(['-D', 'openssl_lib=%s' % openssl_lib])
++  gyp_options.extend(['-D', 'include_dirs=%s' % include_dirs])
  
-   # mac_dir should be started with '<(DEPTH)', otherwise some
-   # operations in XCode fails.  So if the mac_dir option is an
+   mozc_root = os.path.abspath(GetTopLevelSourceDirectoryName())
+   gyp_options.extend(['-D', 'abs_depth=%s' % mozc_root])
--- japanese/mozc-server/files/patch-unix_ibus_mozc_engine.cc
+++ japanese/mozc-server/files/patch-unix_ibus_mozc_engine.cc
@@ -1,11 +1,11 @@
---- unix/ibus/mozc_engine.cc.orig	2013-07-17 11:37:50.000000000 +0900
-+++ unix/ibus/mozc_engine.cc	2013-07-19 22:37:55.000000000 +0900
-@@ -265,7 +265,7 @@
+--- unix/ibus/mozc_engine.cc.orig	2014-08-31 03:35:19.000000000 +0900
++++ unix/ibus/mozc_engine.cc	2014-08-31 04:04:15.000000000 +0900
+@@ -224,7 +224,7 @@
    }
  
-   const uint32 selection_start = min(cursor_pos, anchor_pos);
--  const uint32 selection_length = abs(info->relative_selected_length);
-+  const uint32 selection_length = ::abs(info->relative_selected_length);
-   info->preceding_text = surrounding_text.substr(0, selection_start);
-   Util::SubString(surrounding_text,
-                   selection_start,
+   const size_t selection_start = min(cursor_pos, anchor_pos);
+-  const size_t selection_length = abs(info->relative_selected_length);
++  const size_t selection_length = ::abs(info->relative_selected_length);
+   Util::SubStringPiece(surrounding_text, 0, selection_start)
+       .CopyToString(&info->preceding_text);
+   Util::SubStringPiece(surrounding_text, selection_start, selection_length)
