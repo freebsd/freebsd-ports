4671731e5cccdf750284d0adc5505800ca5d32ec
John Marino <marino@FreeBSD.org>
2014-08-30 22:14:10 +0000

    Stage java/eclipse-langpack
    PR:		193088
    Submitted by:	turutani (Kyoto)

--- java/eclipse-langpack/files/install.sh
+++ java/eclipse-langpack/files/install.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 WRKDIR=$1; PREFIX=$2
-OWNER=$3; GROUP=$4; DPERM=$5; FPERM=$6
 
 cd ${WRKDIR}
 for dir  in `find eclipse -type d | sort`; do
-	install -d -o ${OWNER} -g ${GROUP} -m ${DPERM} ${PREFIX}/lib/${dir}
+	install -d ${PREFIX}/lib/${dir}
 done
 
 for file in `find eclipse -type f | sort`; do
-	install -c -o ${OWNER} -g ${GROUP} -m ${FPERM} ${file} ${PREFIX}/lib/${file}
+	install ${file} ${PREFIX}/lib/${file}
 done
