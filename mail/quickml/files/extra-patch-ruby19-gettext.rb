6e9472cb35f269c5f204f2ca91cfa6f948ae1c9b
bapt <bapt@FreeBSD.org>
2014-09-01 21:25:59 +0000

    Remove non staged ports without pending PR from ma*

--- mail/quickml/files/extra-patch-ruby19-gettext.rb
+++
@@ -1,7 +0,0 @@
---- lib/quickml/gettext.rb.orig	2012-06-21 16:12:56.000000000 +0900
-+++ lib/quickml/gettext.rb	2012-06-21 16:13:31.000000000 +0900
-@@ -1,3 +1,4 @@
-+#coding: euc-jp
- #
- # quickml/gettext - a part of quickml server
- #
