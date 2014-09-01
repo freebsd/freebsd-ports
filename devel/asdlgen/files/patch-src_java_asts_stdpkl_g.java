30abae36263a9c0303cee3e0b7e06774695fc2af
bapt <bapt@FreeBSD.org>
2014-09-01 14:46:34 +0000

    Remove non staged ports without pending PR from d*

--- devel/asdlgen/files/patch-src_java_asts_stdpkl_g.java
+++
@@ -1,8 +0,0 @@
---- src/java/asts/stdpkl/g.java.orig	1999-09-13 00:27:02.000000000 +0200
-+++ src/java/asts/stdpkl/g.java	2007-10-11 19:12:08.000000000 +0200
-@@ -1,4 +1,4 @@
--package asts.StdPkl;
-+package asts.stdpkl;
- final public class g extends PklJava {
-     public static void write_tag (int x, java.io.OutputStream s) {
- 	PklJava.write_java_int(x,s);
--- devel/asdlgen/files/patch-src_java_asts_stdprims_identifier.java
+++
@@ -1,8 +0,0 @@
---- src/java/asts/stdprims/identifier.java.orig	1999-05-31 21:21:29.000000000 +0200
-+++ src/java/asts/stdprims/identifier.java	2007-10-11 19:12:08.000000000 +0200
-@@ -1,4 +1,4 @@
--package asts.StdPrims;
-+package asts.stdprims;
- 
- public class identifier {
-   private identifier() {}
