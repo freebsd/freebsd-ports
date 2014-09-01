691651b9ab31f3462db4b3fddc2441f678b03803
adamw <adamw@FreeBSD.org>
2014-09-01 16:34:26 +0000

    Fix build on 8 and 9.

--- emulators/mupen64plus-video-glide64mk2/files/patch-source-mupen64plus-video-glide64mk2-src-Glide64_Util.h
+++ emulators/mupen64plus-video-glide64mk2/files/patch-source-mupen64plus-video-glide64mk2-src-Glide64_Util.h
@@ -1,6 +1,15 @@
---- source/mupen64plus-video-glide64mk2/src/Glide64/Util.h	2013-07-03 19:28:17.000000000 -0500
-+++ source/mupen64plus-video-glide64mk2/src/Glide64/Util.h	2014-03-23 16:59:30.000000000 -0500
-@@ -90,12 +90,11 @@
+--- source/mupen64plus-video-glide64mk2/src/Glide64/Util.h.orig	2013-07-03 20:28:17.000000000 -0400
++++ source/mupen64plus-video-glide64mk2/src/Glide64/Util.h	2014-09-01 12:32:50.282026994 -0400
+@@ -40,6 +40,8 @@
+ #ifndef Util_H
+ #define Util_H
+ 
++#include <sys/endian.h>
++
+ #define NOT_TMU0	0x00
+ #define NOT_TMU1	0x01
+ #define NOT_TMU2	0x02
+@@ -90,12 +92,11 @@
  			lx = lc; \
  		}
  
@@ -15,7 +24,7 @@
  static inline uint32_t bswap32(uint32_t val)
  {
  	return (((val & 0xff000000) >> 24) |
-@@ -104,6 +103,7 @@
+@@ -104,6 +105,7 @@
  		((val & 0x000000ff) << 24));
  }
  #endif
