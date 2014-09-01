39a5b78f8797ded2c50107a77794cd671dff707f
Baptiste Daroussin <bapt@FreeBSD.org>
2014-09-01 23:05:26 +0000

    Remove non staged ports without pending PR from net-p2p, net and news

--- net/ris/files/patch-decode.py
+++
@@ -1,8 +0,0 @@
---- decode.py.orif	Tue Aug  7 16:37:19 2007
-+++ decode.py	Tue Aug  7 16:37:41 2007
-@@ -1,4 +1,4 @@
--#!/usr/bin/env python
-+#! @PYTHON_CMD@
- # -*- Mode: Python; tab-width: 4 -*-
- #
- # Boot Information Negotiation Layer Packet decoder
--- net/tac_plus-libradius/files/patch-parse.h
+++
@@ -1,9 +0,0 @@
---- ./parse.h.orig	2001-03-16 09:18:44.000000000 +0100
-+++ ./parse.h	2011-08-29 00:31:55.000000000 +0200
-@@ -85,3 +85,6 @@
- #ifdef USE_LDAP
- #define S_ldap            46
- #endif /* LDAP */
-+#ifdef USE_RADIUS
-+#define S_radius	  47
-+#endif /* radius */
