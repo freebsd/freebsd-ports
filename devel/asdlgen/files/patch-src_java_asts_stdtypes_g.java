f70c30791c76c63238eb883cb9cc9d62013fa580
Baptiste Daroussin <bapt@FreeBSD.org>
2014-09-01 14:46:34 +0000

    Remove non staged ports without pending PR from d*

--- devel/asdlgen/files/patch-src_java_asts_stdtypes_g.java
+++
@@ -1,9 +0,0 @@
---- src/java/asts/stdtypes/g.java.orig	1999-09-13 00:27:04.000000000 +0200
-+++ src/java/asts/stdtypes/g.java	2007-10-11 19:13:53.000000000 +0200
-@@ -1,4 +1,4 @@
--package asts.StdTypes;
--final public class g extends asts.StdPkl.PklJava {
-+package asts.stdtypes;
-+final public class g extends asts.stdpkl.PklJava {
-     // inherit methods from PklJava
- }
