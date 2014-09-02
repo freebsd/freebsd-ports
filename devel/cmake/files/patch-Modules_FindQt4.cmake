f2e617b3169c55c1faded52dd5d1e127c50d62dd
rakuco <rakuco@FreeBSD.org>
2014-09-02 12:59:54 +0000

    Update CMake to 3.0.1.
    The 3.0 series is an incremental improvement over the previous 2.8 series
    despite the major version number change. A list of important changes is
    available at http://www.cmake.org/cmake/help/v3.0/release/3.0.0.html
    
    On the porting side
    * The minimum FreeBSD release we have to support in the ports tree is now
      recent enough that ports/168671 can finally be committed: instead of
      building and using CMake's own copies of bzip2, curl, expat, libarchive,
      liblzma and zlib, we use the versions in ports and/or the base system.
    
    * CMake's documentation system has been changed and vastly improved at the
      cost of now depending on Sphinx. We still generate only man pages, but can
      start generating the HTML documentation in the future if desired.
    
    * devel/cmake-gui now uses Qt5 instead of Qt4 and does not needlessly build
      the ncurses UI that is installed by devel/cmake itself.
    
    * CMake commit 3816cd2 fixes a longstanding issue in the detection of the
      Python interpreter and its libraries, but requires us to revert a
      workaround for that in Mk/Uses/python.mk itself, effectively reverting
      the patch introduced by ports/168159.
    
    * Similarly, a few ports had to be fixed manually due to CMake being
      stricter when parsing some files or the ports detecting Python the wrong
      way. Fortunately, they all had been fixed upstream so I just grabbed the
      appropriate commits and pointed to them in the patches.
      science/gnudatalanguage had to have its PORTREVISION bumped because
      switching to USES=cmake:outsource removed a few files from the plist that
      were not supposed to have been installed in the first place.
    
    PR:		168671
    PR:		192644

--- devel/cmake/files/patch-Modules_FindQt4.cmake
+++ devel/cmake/files/patch-Modules_FindQt4.cmake
@@ -1,15 +1,6 @@
---- Modules/FindQt4.cmake.orig	2013-06-22 14:07:09.000000000 +0300
-+++ Modules/FindQt4.cmake	2013-06-22 14:07:22.000000000 +0300
-@@ -569,7 +569,7 @@
- 
- set(QT4_INSTALLED_VERSION_TOO_OLD FALSE)
- 
--set(_QT4_QMAKE_NAMES qmake qmake4 qmake-qt4 qmake-mac)
-+set(_QT4_QMAKE_NAMES qmake-qt4 qmake qmake4 qmake-mac)
- _qt4_find_qmake("${_QT4_QMAKE_NAMES}" QT_QMAKE_EXECUTABLE QTVERSION)
- 
- if (QT_QMAKE_EXECUTABLE AND QTVERSION)
-@@ -739,13 +739,7 @@
+--- Modules/FindQt4.cmake.orig	2014-07-27 16:08:51.000000000 +0300
++++ Modules/FindQt4.cmake	2014-07-27 16:10:01.000000000 +0300
+@@ -701,14 +701,7 @@
    # ask qmake for the plugins directory
    if (QT_LIBRARY_DIR AND NOT QT_PLUGINS_DIR  OR  QT_QMAKE_CHANGED)
      _qt4_query_qmake(QT_INSTALL_PLUGINS qt_plugins_dir)
@@ -19,12 +10,13 @@
 -    endforeach()
 -    find_path(QT_PLUGINS_DIR NAMES accessible imageformats sqldrivers codecs designer
 -      HINTS ${qt_cross_paths} ${qt_plugins_dir}
--      DOC "The location of the Qt plugins")
+-      DOC "The location of the Qt plugins"
+-      NO_CMAKE_FIND_ROOT_PATH)
 +    set(QT_PLUGINS_DIR ${qt_plugins_dir} CACHE PATH "The location of the Qt plugins" FORCE)
    endif ()
  
    # ask qmake for the translations directory
-@@ -758,15 +752,7 @@
+@@ -721,16 +714,7 @@
    if (QT_LIBRARY_DIR AND NOT QT_IMPORTS_DIR OR QT_QMAKE_CHANGED)
      _qt4_query_qmake(QT_INSTALL_IMPORTS qt_imports_dir)
      if(qt_imports_dir)
@@ -35,6 +27,7 @@
 -      find_path(QT_IMPORTS_DIR NAMES Qt
 -        HINTS ${qt_cross_paths} ${qt_imports_dir}
 -        DOC "The location of the Qt imports"
+-        NO_CMAKE_FIND_ROOT_PATH
 -        NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH
 -        NO_CMAKE_SYSTEM_PATH)
 +      set(QT_IMPORTS_DIR ${qt_imports_dir} CACHE PATH "The location of the Qt imports" FORCE)
