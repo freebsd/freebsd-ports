24d38ba8a5499473e798884ce6e9e916959915b7
demon <demon@FreeBSD.org>
2014-09-02 11:40:07 +0000

    Do it in more C++ way.

--- textproc/irstlm/files/patch-src-interpolate-lm.cpp
+++ textproc/irstlm/files/patch-src-interpolate-lm.cpp
@@ -1,27 +1,11 @@
---- src/interpolate-lm.cpp.orig	2014-09-01 15:59:41.000000000 +0400
-+++ src/interpolate-lm.cpp	2014-09-01 16:00:21.000000000 +0400
+--- src/interpolate-lm.cpp.orig	2012-10-02 11:06:58.000000000 +0400
++++ src/interpolate-lm.cpp	2014-09-02 14:42:03.000000000 +0400
 @@ -230,7 +230,7 @@ int main(int argc, char **argv)
    //Learning mixture weights
    if (learn) {
  
 -    std::vector<float> p[N]; //LM probabilities
-+    std::vector<float> *p = new std::vector<float>[N]; //LM probabilities
++    std::vector< std::vector<float> > p(N); //LM probabilities
      float c[N]; //expected counts
      float den,norm; //inner denominator, normalization term
      float variation=1.0; // global variation between new old params
-@@ -256,6 +256,7 @@ int main(int argc, char **argv)
-         lstream >> token >> id >> newlm;
-         if(id <= 0 || id > N) {
-           std::cerr << "LM id out of range." << std::endl;
-+          delete[] p;
-           return 1;
-         }
-         id--; // count from 0 now
-@@ -318,6 +319,7 @@ int main(int argc, char **argv)
-     outtxt << "LMINTERPOLATION " << N << "\n";
-     for (int i=0; i<N; i++) outtxt << w[i] << " " << lmf[i] << "\n";
-     outtxt.close();
-+    delete[] p;
-   }
- 
-   for(int i = 0; i < N; i++)
