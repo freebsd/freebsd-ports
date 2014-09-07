5490fffe07911b62be856469859e5cb1f7cc75e5
nemysis <nemysis@FreeBSD.org>
2014-09-07 11:34:27 +0000

    - Readdition of games/djgame2 which was removed on r367488 - Mastersite disappeared, mirror on my SF site - Transfer maintainership to games@ team - Add license GPLv2

---
+++ games/djgame2/files/patch-Base__src__DJGraphicsChessTimerItem.cpp
@@ -0,0 +1,10 @@
+--- ./Base/src/DJGraphicsChessTimerItem.cpp.orig	2013-12-27 20:08:04.531116404 +0100
++++ ./Base/src/DJGraphicsChessTimerItem.cpp	2013-12-27 20:08:30.220122689 +0100
+@@ -19,6 +19,7 @@
+ //Contact us at ggwizard@gmail.com or darkdong@gmail.com.
+ //****************************************************************************/
+ 
++#include <time.h>
+ #include "DJGraphicsChessTimerItem.h"
+ #include "DJGraphicsTextItem.h"
+ #include "DJDesktop.h"
