b633db0914ed8c7cfe90be1a2252cdfb18441ad3
riggs <riggs@FreeBSD.org>
2014-09-08 19:41:56 +0000

    - Bump PORTREVISION - Simplify port makefile (mostly OPTIONS) - Address build failures in certain situations
    PR:		193065
    Submitted by:	FreeBSD@ShaneWare.Biz (maintainer)

---
+++ audio/hydrogen-devel/files/patch-src__core__include__hydrogen__LashClient.h
@@ -0,0 +1,11 @@
+--- ./src/core/include/hydrogen/LashClient.h.orig	2014-06-19 06:38:04.000000000 +0930
++++ ./src/core/include/hydrogen/LashClient.h	2014-08-13 02:18:54.984463002 +0930
+@@ -27,7 +27,7 @@
+ #ifndef LASH_CLIENT
+ #define LASH_CLIENT
+ 
+-#include <lash-1.0/lash/lash.h>
++#include <lash/lash.h>
+ 
+ #include <string>
+ #include <cassert>
