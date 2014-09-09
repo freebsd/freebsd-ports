2f6b1d53173bbd8bb80479ac235a56a91b16fcc4
adamw <adamw@FreeBSD.org>
2014-09-09 19:31:28 +0000

    Add USE_XORG=xaw. Not clear how this was building successfully before.
    While here, include stdlib.h to quiet warnings about exit.

---
+++ x11/xgrab/files/patch-xgrabsc.h
@@ -0,0 +1,10 @@
+--- xgrabsc.h.orig	2014-09-09 19:24:54 UTC
++++ xgrabsc.h
+@@ -43,6 +43,7 @@
+ #include "cmdopts.h"
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ 
+ #include <X11/Xos.h>
+ #include <X11/Xlib.h>
