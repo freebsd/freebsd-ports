e94ec1e344340b7910ca03e080b2307a974bbfad
Max Khon <fjoe@FreeBSD.org>
2014-09-13 12:21:33 +0000

    SYSCTL_ROOT_NODE exists only on FreeBSD >= 1100024

--- misc/dahdi-kmod/files/patch-freebsd-freebsd-dahdi-bsd-compat.c
+++ misc/dahdi-kmod/files/patch-freebsd-freebsd-dahdi-bsd-compat.c
@@ -1,11 +1,14 @@
---- freebsd/freebsd/dahdi/bsd-compat.c.orig	2014-09-01 00:42:34.000000000 +0700
-+++ freebsd/freebsd/dahdi/bsd-compat.c	2014-09-01 00:47:49.000000000 +0700
-@@ -45,7 +45,7 @@
+--- freebsd/freebsd/dahdi/bsd-compat.c.orig	2014-09-13 19:10:00.000000000 +0700
++++ freebsd/freebsd/dahdi/bsd-compat.c	2014-09-13 19:10:59.000000000 +0700
+@@ -45,7 +45,11 @@
  
  #include <dahdi/compat/bsd.h>
  
--SYSCTL_NODE(, OID_AUTO, dahdi, CTLFLAG_RW, 0, "DAHDI");
++#if __FreeBSD_version >= 1100024
 +SYSCTL_ROOT_NODE(OID_AUTO, dahdi, CTLFLAG_RW, 0, "DAHDI");
++#else
+ SYSCTL_NODE(, OID_AUTO, dahdi, CTLFLAG_RW, 0, "DAHDI");
++#endif
  SYSCTL_NODE(_dahdi, OID_AUTO, echocan, CTLFLAG_RW, 0, "DAHDI Echo Cancelers");
  
  /*
