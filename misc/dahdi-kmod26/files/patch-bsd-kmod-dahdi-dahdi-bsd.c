c012e27e3e5f97dcd0a8e6794f79fed1efc536ab
Max Khon <fjoe@FreeBSD.org>
2014-09-13 12:26:07 +0000

    SYSCTL_ROOT_NODE exists only on FreeBSD >= 1100024

--- misc/dahdi-kmod26/files/patch-bsd-kmod-dahdi-dahdi-bsd.c
+++ misc/dahdi-kmod26/files/patch-bsd-kmod-dahdi-dahdi-bsd.c
@@ -1,11 +1,14 @@
---- bsd-kmod/dahdi/dahdi-bsd.c.orig	2014-09-01 01:18:32.000000000 +0700
-+++ bsd-kmod/dahdi/dahdi-bsd.c	2014-09-01 01:18:41.000000000 +0700
-@@ -38,7 +38,7 @@
+--- bsd-kmod/dahdi/dahdi-bsd.c.orig	2012-09-28 02:45:50.000000000 +0700
++++ bsd-kmod/dahdi/dahdi-bsd.c	2014-09-13 19:23:54.000000000 +0700
+@@ -38,7 +38,11 @@
  
  MALLOC_DEFINE(M_DAHDI, "dahdi", "DAHDI interface data structures");
  
--SYSCTL_NODE(, OID_AUTO, dahdi, CTLFLAG_RW, 0, "DAHDI");
++#if __FreeBSD_version >= 1100024
 +SYSCTL_ROOT_NODE(OID_AUTO, dahdi, CTLFLAG_RW, 0, "DAHDI");
++#else
+ SYSCTL_NODE(, OID_AUTO, dahdi, CTLFLAG_RW, 0, "DAHDI");
++#endif
  SYSCTL_NODE(_dahdi, OID_AUTO, echocan, CTLFLAG_RW, 0, "DAHDI Echo Cancelers");
  
  void
