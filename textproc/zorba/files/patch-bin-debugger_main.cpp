616a1749aecdafff6a45d227543a3414ea22e653
gahr <gahr@FreeBSD.org>
2014-09-15 07:28:58 +0000

    - Fix build on 9.x and above. 8.x needs some more investigation.

--- textproc/zorba/files/patch-bin-debugger_main.cpp
+++ textproc/zorba/files/patch-bin-debugger_main.cpp
@@ -1,10 +1,11 @@
---- bin/debugger/main.cpp.orig	2012-06-21 11:19:43.000000000 +0200
-+++ bin/debugger/main.cpp	2012-06-21 11:19:27.000000000 +0200
-@@ -22,6 +22,8 @@
+--- bin/debugger/main.cpp.orig	2012-10-04 00:35:17.000000000 +0200
++++ bin/debugger/main.cpp	2014-09-11 15:46:51.000000000 +0200
+@@ -22,6 +22,9 @@
  #include <unistd.h>
  #endif
  
 +#include <signal.h>
++#include <stdlib.h>
 +
  #include <vector>
  
