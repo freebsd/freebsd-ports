4b5be4197f71438c0b318edc74d5579a600cdc15
erwin <erwin@FreeBSD.org>
2014-09-19 09:35:32 +0000

    Update to 2.3.0
    Sponsored by:	DK Hostmaster A/S

--- www/hypermail/files/patch-src__proto.h
+++ www/hypermail/files/patch-src__proto.h
@@ -1,11 +1,11 @@
---- src/proto.h.orig	Thu Jul  3 18:00:18 2003
-+++ src/proto.h	Thu Jan  8 00:27:25 2004
-@@ -94,7 +94,7 @@
+--- src/proto.h.orig	2014-09-19 05:03:40.694275686 +0200
++++ src/proto.h	2014-09-19 05:03:48.494270677 +0200
+@@ -104,7 +104,7 @@
  char *PushString(struct Push *, const char *);
  char *PushNString(struct Push *, const char *, int);
  
--char *strcasestr (char *, const char *);
-+// char *strcasestr (char *, const char *);
+-char *strcasestr (const char *, const char *);
++// char *strcasestr (const char *, const char *);
  char *strsav(const char *);
  char *strreplace(char *, char *);
  void strcpymax(char *, const char *, int);
