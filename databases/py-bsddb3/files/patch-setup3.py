ca54191a3e14e2ae78441206c76764ddeaf97d6c
marino <marino@FreeBSD.org>
2014-09-20 17:10:36 +0000

    databases/py-bsddb3: Fix build with python 3.x
    PR:		189131
    Reported by:	arcade (b1t.name)
    Fixed by:	maintainer (Muhammad Rahman)

--- databases/py-bsddb3/files/patch-setup3.py
+++ databases/py-bsddb3/files/patch-setup3.py
@@ -1,6 +1,6 @@
---- setup3.py.orig	2011-03-15 16:10:41.000000000 +0800
-+++ setup3.py	2011-03-15 16:12:40.000000000 +0800
-@@ -53,10 +53,7 @@
+--- setup3.py.orig
++++ setup3.py
+@@ -86,10 +86,7 @@
              category=DeprecationWarning)
  
  
@@ -12,14 +12,23 @@
  
  from distutils.dep_util import newer
  import distutils.ccompiler
-@@ -247,8 +244,8 @@
+@@ -295,8 +292,8 @@
          incdir = os.path.join(BERKELEYDB_DIR, 'include')
      if not libdir:
          libdir = os.path.join(BERKELEYDB_DIR, 'lib')
 -    if not '-ldb' in LIBS:
 -        libname = [dblib]
-+    if not '-l%%DBVERSION%%' in LIBS:
-+        libname = ['%%DBVERSION%%']
++    if not '-l%%BDB_LIB_NAME%%' in LIBS:
++        libname = ['%%BDB_LIB_NAME%%']
      else:
          if debug: print("LIBS already contains '-ldb' not adding our own", "'-l"+dblib+"'")
          libname = []
+@@ -336,7 +333,7 @@
+     # read db.h to figure out what version of Berkeley DB this is
+     ver = None
+     # This should move to "with" when we drop support for Python 2.4 and 2.5
+-    f = open(os.path.join(incdir, 'db.h'), 'r')
++    f = open('%%BDB_INCLUDE_DIR%%/db.h', 'r')
+     db_h_lines = f.readlines()
+     f.close()
+     db_ver_re = re.compile(
