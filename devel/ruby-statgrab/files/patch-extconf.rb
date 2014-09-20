392c9453953a028d3058aa099363c756e611ff2f
rene <rene@FreeBSD.org>
2014-09-20 09:45:04 +0000

    Remove expired ports Keep some which are optional dependencies of non-expired ports 2014-09-18 sysutils/moreutils-parallel: Use moreutils-parallel in sysutils/moreutils instead 2014-09-20 science/ruby-netcdf: Depends on deprecated math/ruby-narray 2014-09-20 x11-toolkits/ruby-fox16: RubyForge shutdown May 15 2014 2014-09-20 math/ruby-gnuplot: Depends on deprecated math/ruby-narray 2014-09-20 science/ruby-gphys: Depends on deprecated math/ruby-narray 2014-09-20 math/ruby-fftw3: Depends on deprecated math/ruby-narray 2014-09-20 science/ruby-dcl: Depends on deprecated math/ruby-narray 2014-09-20 math/ruby-numru_misc: Depends on deprecated math/ruby-narray 2014-09-20 math/ruby-narray: RubyForge shutdown May 15 2014 2014-09-20 textproc/ruby-liquid: RubyForge shutdown May 15 2014 2014-09-20 comms/callsign: RubyForge shutdown May 15 2014 2014-09-20 misc/ruby-vpim: RubyForge shutdown May 15 2014 2014-09-20 net/ruby-ldap: RubyForge shutdown May 15 2014 2014-09-20 devel/rubygem-echoe: Depends on deprecated rubyforge 2014-09-20 www/ruby-borges: RubyForge shutdown May 15 2014 2014-09-20 www/ruby-fcgiwrap: RubyForge shutdown May 15 2014 2014-09-20 www/rubygem-scrubyt: RubyForge shutdown May 15 2014 2014-09-20 devel/ruby-multi: RubyForge shutdown May 15 2014 2014-09-20 databases/ruby-dbd_sqlite3: RubyForge shutdown May 15 2014 2014-09-20 math/ruby-gsl: RubyForge shutdown May 15 2014 2014-09-20 security/ruby-gpgme: RubyForge shutdown May 15 2014 2014-09-20 multimedia/vodcatcher: RubyForge shutdown May 15 2014 2014-09-20 japanese/ruby-ming: RubyForge shutdown May 15 2014 2014-09-20 devel/ruby-statgrab: RubyForge shutdown May 15 2014 2014-09-20 security/ruby-crypt: RubyForge shutdown May 15 2014 2014-09-20 sysutils/philesight: Depends on deprecated devel/ruby-locale2 2014-09-20 multimedia/podcatcher: RubyForge shutdown May 15 2014 2014-09-20 audio/ruby-mp3info: RubyForge shutdown May 15 2014 2014-09-20 multimedia/ruby-flvtool2: RubyForge shutdown May 15 2014 2014-09-20 audio/ruby-freedb: RubyForge shutdown May 15 2014 2014-09-20 graphics/ruby-ming: RubyForge shutdown May 15 2014 2014-09-20 devel/ruby-locale2: RubyForge shutdown May 15 2014 2014-09-20 devel/rubygem-rubyforge: RubyForge shutdown May 15 2014 2014-09-20 databases/ruby-dbd_odbc: RubyForge shutdown May 15 2014 2014-09-20 devel/ruby-fastri: RubyForge shutdown May 15 2014 2014-09-20 devel/ri-emacs: RubyForge shutdown May 15 2014 2014-09-20 security/p5-openxpki-client-html-mason: security/p5-openxpki-0.15+ abandoned Mason UI technology in favor of FastCGI

--- devel/ruby-statgrab/files/patch-extconf.rb
+++
@@ -1,8 +0,0 @@
---- ./extconf.rb.orig	2008-04-28 01:39:02.000000000 +0100
-+++ ./extconf.rb	2014-05-10 02:17:32.621965686 +0100
-@@ -1,3 +1,4 @@
- require 'mkmf'
--have_library("statgrab")
-+pkg_config('libstatgrab0')
-+have_library("statgrab0")
- create_makefile("statgrab")
