20d68fc6c423f54a5c20442a23f6602dd38a5691
az <az@FreeBSD.org>
2014-09-20 06:30:59 +0000

    fix build with perl 5.18
    PR:		193064

---
+++ databases/mysqlsla/files/patch-lib__mysqlsla.pm
@@ -0,0 +1,11 @@
+--- ./lib/mysqlsla.pm.orig	2014-09-19 23:35:02.701375256 +0400
++++ ./lib/mysqlsla.pm	2014-09-19 23:35:46.694100925 +0400
+@@ -6,6 +6,8 @@
+ 
+ __END__
+ 
++=encoding utf8
++
+ =head1 NAME
+ 
+ mysqlsla - Parse, filter, analyze and sort MySQL slow, general and binary logs
