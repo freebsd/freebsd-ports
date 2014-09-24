3f77b2e3abc35b0e846cdaec1f2b22b03cae86f0
Alberto Villa <avilla@FreeBSD.org>
2014-09-24 01:01:34 +0000

    multimedia/mlt: - Update to 0.9.2. - Add optional dependency on Vid.Stab. - Remove deprecated plugins. - Remove stale MAN* macro. - Use OPTIONS handlers. - Remove @dirrm*. - Strip binary and libraries.
    This is a bugfix and enhancement release:
    http://mltframework.blogspot.com/2014/06/version-092-released.html
    
    multimedia/py-mlt:
    - Force dependency on Python 2.
    - Take maintainership.

--- multimedia/mlt/files/patch-src__modules__gtk2__producer_pango.c
+++ multimedia/mlt/files/patch-src__modules__gtk2__producer_pango.c
@@ -1,10 +1,10 @@
---- src/modules/gtk2/producer_pango.c.orig	2013-12-24 21:37:38.000000000 +0100
-+++ src/modules/gtk2/producer_pango.c	2013-12-24 21:37:11.000000000 +0100
+--- src/modules/gtk2/producer_pango.c.orig	2014-06-29 20:23:17 UTC
++++ src/modules/gtk2/producer_pango.c
 @@ -25,7 +25,8 @@
  #include <string.h>
  #include <gdk-pixbuf/gdk-pixbuf.h>
  #include <pango/pangoft2.h>
--#include <freetype/freetype.h>
+-#include <freetype.h>
 +#include <ft2build.h>
 +#include FT_FREETYPE_H
  #include <iconv.h>
