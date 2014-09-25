6063cfa2c4420d0ec24311f6e5bfafbd551d6c1a
brooks <brooks@FreeBSD.org>
2014-09-25 19:10:19 +0000

    Upgrade to r218451.
    USES=python
    
    Give llvm a chance to build on arm* and powerpc64 systems. [0]
    
    PR:		191996 [0]
    Submitted by:	direct727@gmail.com [0]
    Sponsored by:	DARPA, AFRL

--- lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
+++ lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
@@ -3,7 +3,7 @@ $FreeBSD$
 
 --- tools/clang/include/clang/Analysis/Analyses/FormatString.h.orig
 +++ tools/clang/include/clang/Analysis/Analyses/FormatString.h
-@@ -115,9 +115,10 @@
+@@ -122,9 +122,10 @@
    enum Kind {
      InvalidSpecifier = 0,
        // C99 conversion specifiers.
@@ -15,12 +15,3 @@ $FreeBSD$
      iArg,
      IntArgBeg = dArg, IntArgEnd = iArg,
  
-@@ -147,7 +148,7 @@
-     SArg,
- 
-     // ** Printf-specific **
--
-+    
-     // Objective-C specific specifiers.
-     ObjCObjArg,  // '@'
-     ObjCBeg = ObjCObjArg, ObjCEnd = ObjCObjArg,
