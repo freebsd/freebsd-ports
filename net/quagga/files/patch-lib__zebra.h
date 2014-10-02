1287d3fec2c54dc7d1550c5ace2000f9d82ca6ba
Mathieu Arnold <mat@FreeBSD.org>
2014-10-02 15:44:03 +0000

    Update to 0.99.23.1. [1] General cleanup.
    PR:		194090 [1]
    Submitted by:	Olivier Cochard [1]
    Sponsored by:	Absolight

--- net/quagga/files/patch-lib__zebra.h
+++ net/quagga/files/patch-lib__zebra.h
@@ -1,6 +1,6 @@
---- lib/zebra.h.orig	2014-03-16 09:54:05.510349508 +0100
-+++ lib/zebra.h	2014-03-16 10:57:36.640087484 +0100
-@@ -355,7 +355,8 @@
+--- lib/zebra.h.orig	2014-08-25 16:56:53 UTC
++++ lib/zebra.h
+@@ -358,7 +358,8 @@
   * OpenBSD: network byte order, apart from older versions which are as per 
   *          *BSD
   */
@@ -8,5 +8,5 @@
 +#if defined(__NetBSD__) \
 +   || (defined(__FreeBSD__) && (__FreeBSD_version < 1100030)) \
     || (defined(__OpenBSD__) && (OpenBSD < 200311)) \
+    || (defined(__APPLE__)) \
     || (defined(SUNOS_5) && defined(WORDS_BIGENDIAN))
- #define HAVE_IP_HDRINCL_BSD_ORDER
