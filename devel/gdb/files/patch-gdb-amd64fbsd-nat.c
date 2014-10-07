86fb2cb5b780b898e40d943fa25f733e25fb3265
Tijl Coosemans <tijl@FreeBSD.org>
2014-10-07 10:25:42 +0000

    - Fix a buffer overflow when copying a string - Use sysctl KERN_PROC_SIGTRAMP to retrieve the signal trampoline   location for the native amd64 ABI.  This fixes unwinding over the   signal frame after trampoline was moved to the shared page.   (https://svnweb.freebsd.org/changeset/base/258663) - Remove some gcc specific warning flags
    PR:		194190
    Submitted by:	luca.pizzamiglio@gmail.com (maintainer)

--- devel/gdb/files/patch-gdb-amd64fbsd-nat.c
+++ devel/gdb/files/patch-gdb-amd64fbsd-nat.c
@@ -1,6 +1,6 @@
---- gdb/amd64fbsd-nat.c.orig	2012-02-09 17:06:44.000000000 +0100
-+++ gdb/amd64fbsd-nat.c	2012-08-30 10:58:55.000000000 +0200
-@@ -21,11 +21,13 @@
+--- gdb/amd64fbsd-nat.c.orig	2014-06-11 18:34:41.000000000 +0200
++++ gdb/amd64fbsd-nat.c	2014-09-24 18:27:50.618458853 +0200
+@@ -21,13 +21,17 @@
  #include "inferior.h"
  #include "regcache.h"
  #include "target.h"
@@ -13,8 +13,12 @@
 +#include <sys/procfs.h>
  #include <sys/ptrace.h>
  #include <sys/sysctl.h>
++#include <sys/user.h>
++#include <sys/param.h>
  #include <machine/reg.h>
-@@ -93,6 +95,46 @@
+ 
+ #include "fbsd-nat.h"
+@@ -93,6 +97,46 @@
  };
  
  
@@ -60,4 +64,41 @@
 +
  /* Support for debugging kernel virtual memory images.  */
  
- #include <sys/types.h>
+ #include <machine/pcb.h>
+@@ -247,6 +291,10 @@
+ 
+   SC_RBP_OFFSET = offset;
+ 
++// Newer versions of FreeBSD provide a better way to locate the signtramp
++#if ( __FreeBSD_version/100000 < 9 ) \
++	|| ( ( __FreeBSD_version/100000 == 9 ) && ( __FreeBSD_version < 902505 ) ) \
++	|| ( ( __FreeBSD_version/100000 == 10 ) && ( __FreeBSD_version < 1000700 ) )
+   /* FreeBSD provides a kern.ps_strings sysctl that we can use to
+      locate the sigtramp.  That way we can still recognize a sigtramp
+      if its location is changed in a new kernel.  Of course this is
+@@ -267,4 +315,25 @@
+ 	amd64fbsd_sigtramp_end_addr = ps_strings;
+       }
+   }
++#else
++  /* FreeBSD provides a kern.proc.sigtramp sysctl that we can use to
++     locate the sigtramp.  That way we can still recognize a sigtramp
++     if its location is changed in a new kernel. */
++  {
++    int mib[4];
++    struct kinfo_sigtramp kst;
++    size_t len;
++
++    mib[0] = CTL_KERN;
++    mib[1] = KERN_PROC;
++    mib[2] = KERN_PROC_SIGTRAMP;
++    mib[3] = getpid();
++    len = sizeof (kst);
++    if (sysctl (mib, sizeof(mib)/sizeof(mib[0]), &kst, &len, NULL, 0) == 0)
++      {
++	amd64fbsd_sigtramp_start_addr = kst.ksigtramp_start;
++	amd64fbsd_sigtramp_end_addr = kst.ksigtramp_end;
++      }
++  }
++#endif
+ }
