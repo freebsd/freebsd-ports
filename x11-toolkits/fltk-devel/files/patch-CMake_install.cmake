0694ea86e18390d16d6c77e3c36145e238fd2b1a
gahr <gahr@FreeBSD.org>
2014-10-08 12:55:38 +0000

    - Update to snapshot 10350   * Release notes: http://www.fltk.org/articles.php?L1387

--- x11-toolkits/fltk-devel/files/patch-CMake_install.cmake
+++ x11-toolkits/fltk-devel/files/patch-CMake_install.cmake
@@ -1,9 +1,18 @@
---- CMake/install.cmake.orig	2014-02-14 15:10:59.000000000 +0100
-+++ CMake/install.cmake	2014-02-14 15:11:07.000000000 +0100
-@@ -63,7 +63,7 @@
+--- CMake/install.cmake.orig	2014-09-27 02:41:06.000000000 +0200
++++ CMake/install.cmake	2014-10-08 14:32:55.000000000 +0200
+@@ -32,7 +32,7 @@
+ )
+ 
+ install(DIRECTORY ${FLTK_SOURCE_DIR}/FL
+-   DESTINATION include USE_SOURCE_PERMISSIONS
++   DESTINATION include/fltk-devel USE_SOURCE_PERMISSIONS
+    PATTERN ".svn" EXCLUDE
+ )
+ 
+@@ -75,7 +75,7 @@
     install(FILES
        ${FLTK_SOURCE_DIR}/documentation/src/${FILE}.man
-       DESTINATION ${PREFIX_MAN}/man${LEVEL}
+       DESTINATION man/man${LEVEL}
 -      RENAME ${FILE}.${LEVEL}
 +      RENAME ${FILE}-devel.${LEVEL}
     )
--- x11-toolkits/fltk-devel/files/patch-CMake_setup.cmake
+++ x11-toolkits/fltk-devel/files/patch-CMake_setup.cmake
@@ -1,15 +1,13 @@
---- CMake/setup.cmake.orig	2014-02-14 15:09:21.000000000 +0100
-+++ CMake/setup.cmake	2014-02-14 15:10:00.000000000 +0100
-@@ -56,6 +56,12 @@
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -framework Cocoa")
- endif(APPLE)
+--- CMake/setup.cmake.orig	2014-10-08 14:16:19.000000000 +0200
++++ CMake/setup.cmake	2014-10-08 14:16:59.000000000 +0200
+@@ -58,8 +58,8 @@
+    set(FLTK_CONFIG_PATH FLTK/.framework/Resources/CMake)
+    set(FLTK_EXAMPLES_PATH share/fltk-examples)
+ else()
+-   set(FLTK_CONFIG_PATH lib/fltk)
+-   set(FLTK_EXAMPLES_PATH share/fltk-examples)
++   set(FLTK_CONFIG_PATH lib/fltk-devel)
++   set(FLTK_EXAMPLES_PATH share/examples/fltk-devel)
+ endif(WIN32 AND NOT CYGWIN)
  
-+if(${CMAKE_SYSTEM_NAME} STREQUAL FreeBSD)
-+    set(HAVE_STRTOL 1)
-+    set(HAVE_STRCASECMP 1)
-+    set(HAVE_DIRENT_H 1)
-+endif()
-+
- if(WIN32)
-    if(MSVC)
-       add_definitions(-DWIN32_LEAN_AND_MEAN)
+ include(TestBigEndian)
