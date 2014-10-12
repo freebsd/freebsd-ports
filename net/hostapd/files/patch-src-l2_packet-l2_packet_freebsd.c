0f77513f639fb25d62bb31b1f204b969680bc477
John Marino <marino@FreeBSD.org>
2014-10-12 10:46:34 +0000

    net/hostapd: Update WWW + MASTER_SITES and support DragonFly
    The domain for hostapd has changed from hostap.epitest.fi to w1.fi
    although the former still redirects.  Update WWW and MASTER_SITES to
    reflect the new name.
    
    Regenerate the l2 packet patch so that hostapd also builds on DragonFly
    (no-op for FreeBSD).
    
    While here, rearrange makefile to remove need for <pre> and <post> and
    use of $PORTNAME in $WRKSRC which would break if PORTNAME changes.

--- net/hostapd/files/patch-src-l2_packet-l2_packet_freebsd.c
+++ net/hostapd/files/patch-src-l2_packet-l2_packet_freebsd.c
@@ -1,11 +1,14 @@
---- src/l2_packet/l2_packet_freebsd.c.orig	2011-02-05 17:11:19.000000000 -0800
-+++ src/l2_packet/l2_packet_freebsd.c	2011-02-05 17:11:35.000000000 -0800
-@@ -14,7 +14,7 @@
+--- src/l2_packet/l2_packet_freebsd.c.orig	2014-06-04 13:26:14 UTC
++++ src/l2_packet/l2_packet_freebsd.c
+@@ -8,7 +8,10 @@
   */
  
  #include "includes.h"
 -#if defined(__APPLE__) || defined(__GLIBC__)
-+#if defined(__FreeBSD__) || defined(__APPLE__) || defined(__GLIBC__)
++#if defined(__FreeBSD__) \
++ || defined(__DragonFly__) \
++ || defined(__APPLE__) \
++ || defined(__GLIBC__)
  #include <net/bpf.h>
  #endif /* __APPLE__ */
  #include <pcap.h>
