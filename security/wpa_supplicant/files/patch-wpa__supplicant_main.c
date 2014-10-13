51b8291f38e20409683a897ce63939f9c1d333e0
John Marino <marino@FreeBSD.org>
2014-10-13 12:56:04 +0000

    wpa/supplicant: Fix driver list with NDIS, take 2
    It turns out the driver list was only showing "null" for NDIS when -h
    (help) or an unknown switch was passed to wpa_supplicant.  The cause is
    that the NDIS driver is the only that that has to be initialized, and
    the initialization wasn't occurring in the above case.
    
    The fix is the move the NDIS driver initialization before the command
    switches are examined in case wpa_supplicant aborts to usage early.

---
+++ security/wpa_supplicant/files/patch-wpa__supplicant_main.c
@@ -0,0 +1,14 @@
+--- wpa_supplicant/main.c.orig	2014-10-09 14:41:31 UTC
++++ wpa_supplicant/main.c
+@@ -173,6 +173,11 @@ int main(int argc, char *argv[])
+ 
+ 	wpa_supplicant_fd_workaround(1);
+ 
++#ifdef CONFIG_DRIVER_NDIS
++	void driver_ndis_init_ops(void);
++	driver_ndis_init_ops();
++#endif /* CONFIG_DRIVER_NDIS */
++
+ 	for (;;) {
+ 		c = getopt(argc, argv,
+ 			   "b:Bc:C:D:de:f:g:G:hi:I:KLm:No:O:p:P:qsTtuvW");
