88f9ef80624a6a3df03d8dc7cc4acb0dc86e2266
db <db@FreeBSD.org>
2014-10-14 21:28:09 +0000

    - modernise Makefile, fix pkg-descr - change Maintainer to hamradio group - update to chirp 0.4.1
    Version 0.4.1 contains a special backport for Baofeng users to work
    around a firmware incompatibility issue.

--- comms/chirp/files/patch-chirp_platform.py
+++ comms/chirp/files/patch-chirp_platform.py
@@ -1,12 +1,10 @@
---- chirp/platform.py.orig	2012-09-07 19:08:30.000000000 -0500
-+++ chirp/platform.py	2012-09-17 09:48:00.000000000 -0500
-@@ -246,6 +246,9 @@
+--- chirp/platform.py.orig	2013-04-09 03:01:35.000000000 -0400
++++ chirp/platform.py	2014-10-14 17:06:48.000000000 -0400
+@@ -284,6 +284,7 @@
  
      def list_serial_ports(self):
-         return sorted(glob.glob("/dev/ttyS*") +
-+		      glob.glob("/dev/tty*") +
-+		      glob.glob("/dev/cu*") +
-+		      glob.glob("/dev/ttyU*") +
-                       glob.glob("/dev/ttyUSB*") +
-                       glob.glob("/dev/cu.*") +
-                       glob.glob("/dev/term/*") +
+         ports = ["/dev/ttyS*",
++                 "/dev/tty*",
+                  "/dev/ttyUSB*",
+                  "/dev/ttyAMA*",
+                  "/dev/cu.*",
