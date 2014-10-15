c72f1de9bba38100961311b3995a6af2a5613cce
rene <rene@FreeBSD.org>
2014-10-15 21:55:33 +0000

    MFH: r370928
    www/chromium: desupport SSLv3.0, taken from upstream GIT repository.
    
    While here really fix the desktop icon.
    
    Bump PORTREVISION
    Obtained from:	https://chromium.googlesource.com/chromium/src/+/701bb044ac5ad4f1572e86b83a673cc49383efb4
    Obtained from:	https://chromium.googlesource.com/chromium/src/+/32352ad08ee673a4d43e8593ce988b224f6482d3
    Security:	CVE-2014-3566 ("Poodle")
    Approved by:	portmgr (bdrewery)

--- www/chromium/files/patch-chrome__common__pref_names.cc
+++ www/chromium/files/patch-chrome__common__pref_names.cc
@@ -1,5 +1,5 @@
---- chrome/common/pref_names.cc.orig	2014-10-02 17:39:46 UTC
-+++ chrome/common/pref_names.cc
+--- chrome/common/pref_names.cc.orig	2014-10-02 19:39:46.000000000 +0200
++++ chrome/common/pref_names.cc	2014-10-15 11:59:52.000000000 +0200
 @@ -898,7 +898,7 @@
  // Boolean controlling whether SafeSearch is mandatory for Google Web Searches.
  const char kForceSafeSearch[] = "settings.force_safesearch";
@@ -9,3 +9,11 @@
  // Linux specific preference on whether we should match the system theme.
  const char kUsesSystemTheme[] = "extensions.theme.use_system";
  #endif
+@@ -1288,6 +1288,7 @@
+     "ssl.rev_checking.required_for_local_anchors";
+ const char kSSLVersionMin[] = "ssl.version_min";
+ const char kSSLVersionMax[] = "ssl.version_max";
++const char kSSLVersionFallbackMin[] = "ssl.version_fallback_min";
+ const char kCipherSuiteBlacklist[] = "ssl.cipher_suites.blacklist";
+ const char kDisableSSLRecordSplitting[] = "ssl.ssl_record_splitting.disabled";
+ 
