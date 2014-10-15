2ce21d6d97f970ca17569035fd92acb40c67a911
Dmitry Marakasov <amdmi3@FreeBSD.org>
2014-10-15 22:26:24 +0000

    MFH: r370950
    - Fix rule reset time on arm
    
    PR:		194381
    Submitted by:	ronald-lists@klop.ws
    Approved by:	portmgr

---
+++ security/bruteblock/files/patch-bruteblock.c
@@ -0,0 +1,11 @@
+--- bruteblock.c.orig	2006-08-20 08:22:37.000000000 +0200
++++ bruteblock.c	2014-10-15 16:19:19.000000000 +0200
+@@ -99,7 +99,7 @@
+ 				snprintf(table, sizeof(table), "%d", ipfw2_table_no);
+ 				argv[1] = table;
+ 				argv[2] = command;
+-				snprintf(utime, sizeof(utime), "%d",
++				snprintf(utime, sizeof(utime), "%lld",
+ 				time(NULL) + reset_ip);
+ 				argv[4] = utime;
+ 				argv[3] = host;
