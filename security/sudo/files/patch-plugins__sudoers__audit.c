15ebe402bf2915623bfae5b7aa75d59006c246d6
wxs <wxs@FreeBSD.org>
2014-10-16 02:05:17 +0000

    Update to 1.8.11p1
    PR:		194259
    Submitted by:	cy@ and Yasuhiro KIMURA <yasu@utahime.org>

--- security/sudo/files/patch-plugins__sudoers__audit.c
+++ security/sudo/files/patch-plugins__sudoers__audit.c
@@ -1,10 +1,10 @@
---- ./plugins/sudoers/audit.c.orig	2013-06-04 11:48:28.000000000 +0200
-+++ ./plugins/sudoers/audit.c	2013-07-05 13:10:01.000000000 +0200
-@@ -39,6 +39,7 @@
+--- plugins/sudoers/audit.c.orig	2014-09-24 01:40:15.000000000 +0900
++++ plugins/sudoers/audit.c	2014-10-10 07:24:43.000000000 +0900
+@@ -42,6 +42,7 @@
  
  #ifdef HAVE_BSM_AUDIT
  # include "bsm_audit.h"
-+# include "gettext.h"
++# include "sudo_gettext.h"
  #endif
  #ifdef HAVE_LINUX_AUDIT
  # include "linux_audit.h"
