9cee92ad5835b0e1d246ad9576fddfe8de759dda
Jung-uk Kim <jkim@FreeBSD.org>
2014-10-16 21:14:18 +0000

    Update to 4.3.2.

--- editors/libreoffice/files/patch-editeng__Module_editeng.mk
+++ editors/libreoffice/files/patch-editeng__Module_editeng.mk
@@ -1,10 +1,11 @@
---- ./editeng/Module_editeng.mk.orig	2013-03-12 07:00:34.000000000 -0400
-+++ ./editeng/Module_editeng.mk	2013-03-21 13:26:15.000000000 -0400
-@@ -28,7 +28,6 @@
- # add any runtime tests (unit tests) here
- # remove if no tests
+--- editeng/Module_editeng.mk.orig	2014-09-16 16:10:41.000000000 -0400
++++ editeng/Module_editeng.mk	2014-10-08 17:20:09.000000000 -0400
+@@ -28,8 +28,6 @@
+ ))
+ 
  $(eval $(call gb_Module_add_check_targets,editeng,\
--    CppunitTest_editeng_core \
-     CppunitTest_editeng_borderline \
-     CppunitTest_editeng_lookuptree \
+-	$(if $(and $(filter $(COM),MSC),$(MERGELIBS)),, \
+-		CppunitTest_editeng_core) \
+ 	CppunitTest_editeng_borderline \
+ 	CppunitTest_editeng_lookuptree \
  ))
--- editors/libreoffice/files/patch-sw__Module_sw.mk
+++ editors/libreoffice/files/patch-sw__Module_sw.mk
@@ -1,17 +1,13 @@
---- sw/Module_sw.mk.orig	2013-12-11 01:11:21.000000000 +0900
-+++ sw/Module_sw.mk	2014-01-29 17:12:45.000000000 +0900
-@@ -41,14 +41,8 @@
- 
+--- sw/Module_sw.mk.orig	2014-09-16 16:10:41.000000000 -0400
++++ sw/Module_sw.mk	2014-10-08 17:26:44.000000000 -0400
+@@ -47,10 +47,7 @@
  endif
  
--$(eval $(call gb_Module_add_check_targets,sw,\
--    CppunitTest_sw_uwriter \
--))
--
  $(eval $(call gb_Module_add_slowcheck_targets,sw,\
+-    CppunitTest_sw_uwriter \
      CppunitTest_sw_htmlexport \
 -    CppunitTest_sw_macros_test \
 -    CppunitTest_sw_ooxmlexport \
+     CppunitTest_sw_ooxmlsdrexport \
+     CppunitTest_sw_ooxmlw14export \
      CppunitTest_sw_ooxmlimport \
-     CppunitTest_sw_ww8export \
-     CppunitTest_sw_ww8import \
