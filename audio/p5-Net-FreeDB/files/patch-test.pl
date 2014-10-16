2d86907f0f3949ec8dc954c6a23b61c12f88808c
John Marino <marino@FreeBSD.org>
2014-10-16 15:26:11 +0000

    audio/p5-Net-FreeDB: Upgrade version 0.08 => 0.09
    Also assign maintainership to submitter
    
    PR:		194354
    Submitted by:	Muhammad Rahman

--- audio/p5-Net-FreeDB/files/patch-test.pl
+++ audio/p5-Net-FreeDB/files/patch-test.pl
@@ -1,6 +1,6 @@
---- test.pl.orig	2005-09-28 14:45:55.000000000 +0000
-+++ test.pl
-@@ -40,6 +40,8 @@ if ($^O =~ /MSWin32/) {
+--- test.pl.orig	2014-10-14 17:57:41.459066370 +0600
++++ test.pl	2014-10-14 17:59:37.614059704 +0600
+@@ -38,6 +38,8 @@
  	$id = $cddb->getdiscid(0);
  } elsif ($^O =~ /freebsd/) {
  	$id = $cddb->getdiscid('/dev/acd0');
@@ -9,16 +9,16 @@
  } else {
  	$id = $cddb->getdiscid('/dev/cdrom');
  }
-@@ -51,6 +53,8 @@ if ($^O =~ /MSWin32/) {
+@@ -49,6 +51,8 @@
  	$id = Net::FreeDB::getdiscid(0);
  } elsif ($^O =~ /freebsd/) {
  	$id = Net::FreeDB::getdiscid('/dev/acd0');
 +} elsif ($^O =~ /dragonfly/) {
 +	$id = Net::FreeDB::getdiscid('/dev/cd0');
- } else {
- 	$id = Net::FreeDB::getdiscid('/dev/cdrom');
+ } else { $id = Net::FreeDB::getdiscid('/dev/cdrom');
  }
-@@ -62,6 +66,8 @@ if ($^O =~ /MSWin32/) {
+ ok($id);
+@@ -59,6 +63,8 @@
  	$id = Net::FreeDB::getdiscdata(0);
  } elsif ($^O =~ /freebsd/) {
  	$id = Net::FreeDB::getdiscdata('/dev/acd0');
