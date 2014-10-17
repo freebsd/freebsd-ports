1d803f71bba5156f57c6f2ec4c16e17431b98aba
Koop Mast <kwm@FreeBSD.org>
2014-10-17 09:53:13 +0000

    Update Mesa ports to new default of 9.1.7 and more recent version to 10.3.0.
    The port will switch to the newer version if hw context is available in the
    i915kms driver.
    
    - Get ride of WITH_NEW_XORG.
    - Use @comment in plist to ignore unwanted files in the stagedir, instead of
      trying to remove them in post-install.
    - Bump portrevision of 9.1.7 due to dependency changes.
    - Drop :keepla from USES=libtool.
    - Drop @dirrm[try] from plists
    - Give dri propper options, with pkg-help for additional information.
    - Make separate plist for dri for the different versions, the combined plist
      was headache inducing.
    - Add "workaround" patches to allow clang to build the dri port on i386 [1].
      USE_GCC is now only needed for 8.x.
    - Add gbm port and USE_GL switch for it.
    
    PR:		192286 [1]
    Submitted by:	Carlos Jacobo Puga Medina [1]
    Approved by:	portmgr (bapt@)
    In collaberation with:	dumbbell@
    Obtained from:	xorg-dev

--- graphics/libGL/files-old/patch-src__gallium__include__pipe__p_config.h
+++ graphics/libGL/files-old/patch-src__gallium__include__pipe__p_config.h
@@ -1,5 +1,5 @@
---- ./src/gallium/include/pipe/p_config.h.orig	2014-01-23 18:02:42.000000000 +0100
-+++ ./src/gallium/include/pipe/p_config.h	2014-02-23 22:01:50.000000000 +0100
+--- ./src/gallium/include/pipe/p_config.h.orig	2014-02-03 18:42:39.000000000 +0100
++++ ./src/gallium/include/pipe/p_config.h	2014-02-15 21:11:34.000000000 +0100
 @@ -81,6 +81,13 @@
  #define PIPE_CC_SUNPRO
  #endif
