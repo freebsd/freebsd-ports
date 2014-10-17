d6afb37e19f66eed665826275fba54cca09a8c89
kwm <kwm@FreeBSD.org>
2014-10-17 09:53:13 +0000

    Update Mesa ports to new default of 9.1.7 and more recent version to 10.3.0.
    The port will switch to the newer version if hw context is available in the
    i915kms driver.
    
    - Get ride of WITH_NEW_XORG.
    - Use @comment in plist to ignore unwanted files in the stagedir, instead of
      trying to remove them in post-install.
    - Bump portrevision of 9.1.7 due to dependency changes.
    - Drop :keepla from USES=libtool.
    - Drop @dirrm[try] from plists
    - Give dri propper options, with pkg-help for additional information.
    - Make separate plist for dri for the different versions, the combined plist
      was headache inducing.
    - Add "workaround" patches to allow clang to build the dri port on i386 [1].
      USE_GCC is now only needed for 8.x.
    - Add gbm port and USE_GL switch for it.
    
    PR:		192286 [1]
    Submitted by:	Carlos Jacobo Puga Medina [1]
    Approved by:	portmgr (bapt@)
    In collaberation with:	dumbbell@
    Obtained from:	xorg-dev

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1773,6 +1773,7 @@ RUN_DEPENDS+=	${LINUX_BASE_PORT}
 
 PKG_IGNORE_DEPENDS?=		'this_port_does_not_exist'
 
+_GL_gbm_LIB_DEPENDS=		libgbm.so:${PORTSDIR}/graphics/gbm
 _GL_glesv2_LIB_DEPENDS=		libGLESv2.so:${PORTSDIR}/graphics/libglesv2
 _GL_egl_LIB_DEPENDS=		libEGL.so:${PORTSDIR}/graphics/libEGL
 _GL_gl_LIB_DEPENDS=		libGL.so:${PORTSDIR}/graphics/libGL
