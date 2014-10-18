ccb7128a1c0ebc6f6f1b395eda68d4eca8a30bb4
Antoine Brodin <antoine@FreeBSD.org>
2014-10-18 18:13:58 +0000

    Switch from clang33 to clang34 for ports using compiler:*11*, objc or gnustep on freebsd 8.4 and 9.1
    PR:		ports/193555
    Differential Revision:	https://reviews.freebsd.org/D935
    Reviewed by:	bapt
    Exp-run:	self

--- Mk/Uses/objc.mk
+++ Mk/Uses/objc.mk
@@ -45,10 +45,10 @@ CC=	/usr/bin/clang
 CPP=	/usr/bin/clang-cpp
 CXX=	/usr/bin/clang++
 .else
-BUILD_DEPENDS+=	${LOCALBASE}/bin/clang33:${PORTSDIR}/lang/clang33
-CPP=	${LOCALBASE}/bin/clang-cpp33
-CC=	${LOCALBASE}/bin/clang33
-CXX=	${LOCALBASE}/bin/clang++33
+BUILD_DEPENDS+=	${LOCALBASE}/bin/clang34:${PORTSDIR}/lang/clang34
+CPP=	${LOCALBASE}/bin/clang-cpp34
+CC=	${LOCALBASE}/bin/clang34
+CXX=	${LOCALBASE}/bin/clang++34
 .if ${OSVERSION} < 900033
 USE_BINUTILS=	yes
 LDFLAGS+=	-B${LOCALBASE}/bin
