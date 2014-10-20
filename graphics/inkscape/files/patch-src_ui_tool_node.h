238087016a9e37b103c1f461ac2a690dc9f9ffa4
Koop Mast <kwm@FreeBSD.org>
2014-10-20 09:12:19 +0000

    Update to 0.48.5.
    Adobe Illustrator SVG files containing entities can be loaded again
    Support for Poppler 0.26
    Support for Boehm-GC 7.4.0 and later
    Several crash bugs were fixed
    
    Remove the gnomevfs option.
    Install all available manpages.

--- graphics/inkscape/files/patch-src_ui_tool_node.h
+++ graphics/inkscape/files/patch-src_ui_tool_node.h
@@ -1,30 +1,11 @@
---- src/ui/tool/node.h.orig	2012-02-14 04:22:17.670697000 +0100
-+++ src/ui/tool/node.h	2013-10-17 12:47:44.000000000 +0200
-@@ -16,7 +16,13 @@
- #include <iosfwd>
- #include <stdexcept>
- #include <cstddef>
-+
-+#if __cplusplus >= 201103L
-+#include <functional>
-+#else
- #include <tr1/functional>
-+#endif
-+
- #include <boost/utility.hpp>
- #include <boost/shared_ptr.hpp>
- #include <boost/optional.hpp>
-@@ -32,11 +38,13 @@
+--- src/ui/tool/node.h.orig	2014-10-20 01:38:50.972399465 +0200
++++ src/ui/tool/node.h	2014-10-20 01:37:10.561420788 +0200
+@@ -38,7 +38,7 @@
  }
  }
  
+-#if __cplusplus < 201103L
 +#if __cplusplus < 201103L && !defined(_LIBCPP_VERSION)
  namespace std {
  namespace tr1 {
  template <typename N> struct hash< Inkscape::UI::NodeIterator<N> >;
- }
- }
-+#endif
- 
- namespace Inkscape {
- namespace UI {
