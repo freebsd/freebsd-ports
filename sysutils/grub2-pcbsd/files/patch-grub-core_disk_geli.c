a4db70e71b2a33ebac365bf97c1a1f3aba2bd0fb
kmoore <kmoore@FreeBSD.org>
2014-10-21 15:24:01 +0000

    - Update to latest git pull - 2.02_3 - Add support for GELI passphrase kernel passthrough - Patch grub-install / EFI support to disable trying to use Linux 'efibootmgr'

--- sysutils/grub2-pcbsd/files/patch-grub-core_disk_geli.c
+++ sysutils/grub2-pcbsd/files/patch-grub-core_disk_geli.c
@@ -1,5 +1,5 @@
 --- grub-core/disk/geli.c.orig	2014-05-15 14:00:10.000000000 -0400
-+++ grub-core/disk/geli.c	2014-05-20 14:46:07.604565349 -0400
++++ grub-core/disk/geli.c	2014-09-26 10:18:53.325111693 -0400
 @@ -225,7 +225,7 @@
  
    /* Look for GELI magic sequence.  */
@@ -18,3 +18,13 @@
        || grub_le_to_cpu32 (header.version) < 1)
      {
        grub_dprintf ("geli", "wrong magic %02x\n", header.magic[0]);
+@@ -430,6 +430,9 @@
+   if (!grub_password_get (passphrase, MAX_PASSPHRASE))
+     return grub_error (GRUB_ERR_BAD_ARGUMENT, "Passphrase not supplied");
+ 
++  /* Set the GELI passphrase to GRUB env, for passing to FreeBSD kernel */
++  grub_env_set ("gelipassphrase", passphrase);
++
+   /* Calculate the PBKDF2 of the user supplied passphrase.  */
+   if (grub_le_to_cpu32 (header.niter) != 0)
+     {
