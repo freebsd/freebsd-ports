9c4ec43ef3493f9b8ae381bc920daa87de5666a6
kmoore <kmoore@FreeBSD.org>
2014-10-22 17:02:59 +0000

    - Add GELI passphrase passthrough support to default grub.cfg  - Add detection for EFI mode at boot  - Don't clobber ${PREFIX}/etc/grub.d/40_custom on re-install  - Bump PORTREV

--- sysutils/grub2-pcbsd/files/patch-grub-core_disk_geli.c
+++ sysutils/grub2-pcbsd/files/patch-grub-core_disk_geli.c
@@ -1,23 +1,5 @@
 --- grub-core/disk/geli.c.orig	2014-05-15 14:00:10.000000000 -0400
 +++ grub-core/disk/geli.c	2014-09-26 10:18:53.325111693 -0400
-@@ -225,7 +225,7 @@
- 
-   /* Look for GELI magic sequence.  */
-   if (grub_memcmp (header->magic, GELI_MAGIC, sizeof (GELI_MAGIC))
--      || grub_le_to_cpu32 (header->version) > 5
-+      || grub_le_to_cpu32 (header->version) > 7
-       || grub_le_to_cpu32 (header->version) < 1)
-     grub_util_error ("%s", _("wrong ELI magic or version"));
- 
-@@ -265,7 +265,7 @@
- 
-   /* Look for GELI magic sequence.  */
-   if (grub_memcmp (header.magic, GELI_MAGIC, sizeof (GELI_MAGIC))
--      || grub_le_to_cpu32 (header.version) > 5
-+      || grub_le_to_cpu32 (header.version) > 7
-       || grub_le_to_cpu32 (header.version) < 1)
-     {
-       grub_dprintf ("geli", "wrong magic %02x\n", header.magic[0]);
 @@ -430,6 +430,9 @@
    if (!grub_password_get (passphrase, MAX_PASSPHRASE))
      return grub_error (GRUB_ERR_BAD_ARGUMENT, "Passphrase not supplied");
