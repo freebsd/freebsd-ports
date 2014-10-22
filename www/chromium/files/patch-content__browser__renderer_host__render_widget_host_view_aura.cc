d5661766271661df8acb62c82e82624825bd894d
Rene Ladan <rene@FreeBSD.org>
2014-10-22 07:38:07 +0000

    MFH: r371240
    www/chromium: fix input of accented keys, bump PORTREVISION.
    
    PR:		193725
    Submitted by:	Miguel Clara <miguelmclara@gmail.com>
    Patch by:	Tomek <tomek@apostata.org>
    Approved by:	ports-secteam (rea)

--- www/chromium/files/patch-content__browser__renderer_host__render_widget_host_view_aura.cc
+++ www/chromium/files/patch-content__browser__renderer_host__render_widget_host_view_aura.cc
@@ -1,5 +1,14 @@
---- content/browser/renderer_host/render_widget_host_view_aura.cc.orig	2014-10-02 17:39:47 UTC
-+++ content/browser/renderer_host/render_widget_host_view_aura.cc
+--- content/browser/renderer_host/render_widget_host_view_aura.cc.orig	2014-10-02 19:39:47.000000000 +0200
++++ content/browser/renderer_host/render_widget_host_view_aura.cc	2014-10-15 19:03:21.000000000 +0200
+@@ -88,7 +88,7 @@
+ #include "ui/gfx/win/dpi.h"
+ #endif
+ 
+-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
++#if defined(OS_LINUX) && !defined(OS_CHROMEOS) || defined(OS_BSD)
+ #include "content/common/input_messages.h"
+ #include "ui/events/linux/text_edit_command_auralinux.h"
+ #include "ui/events/linux/text_edit_key_bindings_delegate_auralinux.h"
 @@ -653,7 +653,7 @@
    if (host)
      return reinterpret_cast<gfx::NativeViewId>(host->GetAcceleratedWidget());
@@ -9,3 +18,12 @@
  }
  
  gfx::NativeViewAccessible RenderWidgetHostViewAura::GetNativeViewAccessible() {
+@@ -2418,7 +2418,7 @@
+ 
+ void RenderWidgetHostViewAura::ForwardKeyboardEvent(
+     const NativeWebKeyboardEvent& event) {
+-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
++#if defined(OS_LINUX) && !defined(OS_CHROMEOS) || defined(OS_BSD)
+   ui::TextEditKeyBindingsDelegateAuraLinux* keybinding_delegate =
+       ui::GetTextEditKeyBindingsDelegate();
+   std::vector<ui::TextEditCommandAuraLinux> commands;
