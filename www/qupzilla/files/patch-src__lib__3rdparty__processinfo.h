13d49ff2008797814d9ddaa625ad44bcb2ba9b28
makc <makc@FreeBSD.org>
2014-11-01 18:56:19 +0000

    www/qupzilla: - update to 1.8.3 and add DEBUG option - pass maintainership to submitter while I'm here: - amend COMMENT - convert to USES=execinfo - convert to options helpers
    PR:		194310
    Submitted by:	matthew@reztek.cz

--- www/qupzilla/files/patch-src__lib__3rdparty__processinfo.h
+++ www/qupzilla/files/patch-src__lib__3rdparty__processinfo.h
@@ -1,11 +1,20 @@
 --- src/lib/3rdparty/processinfo.h.orig	2014-09-26 12:35:35.000000000 +0400
 +++ src/lib/3rdparty/processinfo.h	2014-09-26 23:04:10.000000000 +0400
-@@ -18,6 +18,8 @@
+@@ -18,16 +18,12 @@
  #ifndef PROCESSINFO_H
  #define PROCESSINFO_H
  
-+#include <QtGlobal>
-+
+-#include <QtGlobal>
++#include "qzcommon.h"
+
  #if defined(Q_OS_UNIX) && !defined(Q_OS_MAC)
  #include <sys/types.h>
  #endif
+ 
+-#include <QString>
+-
+-#include "qzcommon.h"
+-
+ /*
+  * Code used from http://ubuntuforums.org/showpost.php?p=6593782&postcount=5
+  * written by user WitchCraft 
