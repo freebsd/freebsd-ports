6993bfbaf687f7fc74fc81659b822f884f86bbad
swills <swills@FreeBSD.org>
2014-11-04 01:59:37 +0000

    net/syncthing: update to 0.10.4

--- net/syncthing/files/patch-build.go
+++ net/syncthing/files/patch-build.go
@@ -1,23 +1,37 @@
---- build.go.orig	2014-10-10 03:17:54.000000000 +0000
-+++ build.go	2014-10-10 03:21:21.000000000 +0000
-@@ -299,18 +299,11 @@
+--- build.go.orig	2014-10-24 08:27:14.000000000 +0000
++++ build.go	2014-10-25 12:42:18.706050568 +0000
+@@ -35,7 +35,6 @@
+ 	"runtime"
+ 	"strconv"
+ 	"strings"
+-	"time"
+ )
+ 
+ var (
+@@ -302,24 +301,11 @@
  }
  
- func version() string {
--	v := run("git", "describe", "--always", "--dirty")
+ func getVersion() string {
+-	v, err := runError("git", "describe", "--always", "--dirty")
+-	if err != nil {
+-		return "unknown-dev"
+-	}
 -	v = versionRe.ReplaceAllFunc(v, func(s []byte) []byte {
 -		s[0] = '+'
 -		return s
 -	})
 -	return string(v)
-+	return string("v0.10.1")
++	return string("v0.10.4")
  }
  
  func buildStamp() int64 {
--	bs := run("git", "show", "-s", "--format=%ct")
+-	bs, err := runError("git", "show", "-s", "--format=%ct")
+-	if err != nil {
+-		return time.Now().Unix()
+-	}
 -	s, _ := strconv.ParseInt(string(bs), 10, 64)
 -	return s
-+	return 1413115780
++	return 1414418716
  }
  
  func buildUser() string {
