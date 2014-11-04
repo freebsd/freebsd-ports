778924eeb5fd466fe775de6dfae4e0344f38cbcd
Kris Moore <kris@pcbsd.org>
2014-11-03 23:54:47 -0500

    Add missing patch for EFI -> GELI passphrase passthrough

--- sysutils/grub2-efi/files/patch-grub-core_disk_geli.c
+++ sysutils/grub2-efi/files/patch-grub-core_disk_geli.c
@@ -1,20 +1,12 @@
 --- grub-core/disk/geli.c.orig	2014-05-15 14:00:10.000000000 -0400
-+++ grub-core/disk/geli.c	2014-05-20 14:46:07.604565349 -0400
-@@ -225,7 +225,7 @@
++++ grub-core/disk/geli.c	2014-09-26 10:18:53.325111693 -0400
+@@ -430,6 +430,9 @@
+   if (!grub_password_get (passphrase, MAX_PASSPHRASE))
+     return grub_error (GRUB_ERR_BAD_ARGUMENT, "Passphrase not supplied");
  
-   /* Look for GELI magic sequence.  */
-   if (grub_memcmp (header->magic, GELI_MAGIC, sizeof (GELI_MAGIC))
--      || grub_le_to_cpu32 (header->version) > 5
-+      || grub_le_to_cpu32 (header->version) > 7
-       || grub_le_to_cpu32 (header->version) < 1)
-     grub_util_error ("%s", _("wrong ELI magic or version"));
- 
-@@ -265,7 +265,7 @@
- 
-   /* Look for GELI magic sequence.  */
-   if (grub_memcmp (header.magic, GELI_MAGIC, sizeof (GELI_MAGIC))
--      || grub_le_to_cpu32 (header.version) > 5
-+      || grub_le_to_cpu32 (header.version) > 7
-       || grub_le_to_cpu32 (header.version) < 1)
++  /* Set the GELI passphrase to GRUB env, for passing to FreeBSD kernel */
++  grub_env_set ("gelipassphrase", passphrase);
++
+   /* Calculate the PBKDF2 of the user supplied passphrase.  */
+   if (grub_le_to_cpu32 (header.niter) != 0)
      {
-       grub_dprintf ("geli", "wrong magic %02x\n", header.magic[0]);
