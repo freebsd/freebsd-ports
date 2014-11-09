70db521251f6a41d424a03de0d57ddb43d1d3e84
Guido Falsi <madpilot@FreeBSD.org>
2014-11-09 22:07:25 +0000

    - Update to 2014.7.0 - Remove dirrms
    PR:		194827
    Submitted by:	christer.edwards at gmail.com (maintainer)

--- sysutils/py-salt/files/patch-salt__grains__core.py
+++ sysutils/py-salt/files/patch-salt__grains__core.py
@@ -1,9 +1,9 @@
---- ./salt/grains/core.py.orig	2014-02-19 22:21:14.000000000 +0200
-+++ ./salt/grains/core.py	2014-03-06 10:50:07.000000000 +0200
-@@ -1133,7 +1133,7 @@
+--- ./salt/grains/core.py.orig  2014-11-03 12:38:21.000000000 -0700
++++ ./salt/grains/core.py       2014-11-04 13:34:47.769728417 -0700
+@@ -1320,7 +1320,7 @@
+     '''
      # Provides:
      #   path
- 
 -    return {'path': os.environ['PATH'].strip()}
 +    return {'path': os.environ['PATH'].strip() + ':/usr/local/bin:/usr/local/sbin'}
  
