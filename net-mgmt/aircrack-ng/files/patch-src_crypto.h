bc3941ef34e258006b3f2f099ebf2d858576c57f
Lars Engels <lme@FreeBSD.org>
2014-11-09 18:31:45 +0000

    Update to 1.2-rc1

--- net-mgmt/aircrack-ng/files/patch-src_crypto.h
+++ net-mgmt/aircrack-ng/files/patch-src_crypto.h
@@ -1,10 +1,10 @@
---- src/crypto.h.orig	2014-04-01 19:15:06.228646731 +0200
-+++ src/crypto.h	2014-04-01 19:16:22.960640528 +0200
-@@ -35,6 +35,7 @@
+--- src/crypto.h.orig	2014-07-12 04:24:03.000000000 +0200
++++ src/crypto.h	2014-11-02 21:23:28.529312627 +0100
+@@ -34,6 +34,7 @@
+ 
  #ifndef _CRYPTO_H
  #define _CRYPTO_H
- 
 +#include <sys/types.h>
- #ifndef uint8
- #define uint8  unsigned char
- #endif
+ 
+ #ifdef USE_GCRYPT
+ #include "gcrypt-openssl-wrapper.h"
