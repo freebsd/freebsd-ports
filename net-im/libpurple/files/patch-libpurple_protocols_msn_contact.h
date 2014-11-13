e11feb48462660fb8cf98c77226fe13ed1e959f0
Joe Marcus Clarke <marcus@FreeBSD.org>
2014-11-13 20:47:41 +0000

    Fix the MSN application ID to re-enable the buddy list.
    Reported by:    Matthias Apitz <guru@unixarea.de>
    Obtained from:  Pidgin git

---
+++ net-im/libpurple/files/patch-libpurple_protocols_msn_contact.h
@@ -0,0 +1,11 @@
+--- libpurple/protocols/msn/contact.h.orig	2014-11-13 15:42:16.000000000 -0500
++++ libpurple/protocols/msn/contact.h	2014-11-13 15:42:27.000000000 -0500
+@@ -60,7 +60,7 @@ typedef enum
+ #include "session.h"
+ #include "soap.h"
+ 
+-#define MSN_APPLICATION_ID "CFE80F9D-180F-4399-82AB-413F33A1FA11"
++#define MSN_APPLICATION_ID "484AAC02-7F59-41B7-9601-772045DCC569"
+ 
+ #define MSN_CONTACT_SERVER	"local-bay.contacts.msn.com"
+ 
