a158751212463ce2f94edf8dfb7cbae19b605887
Guido Falsi <madpilot@FreeBSD.org>
2014-11-14 18:15:35 +0000

    - Update to 2.4-RC7, latest version available - Add LICENSE - Add support for DOCS option - Update WWW line in pkg-descr
    PR:		194503
    Submitted by:	Kalten <kalten at gmx.at>
    Approved by:	maintainer timeout

---
+++ deskutils/phpicalendar/files/patch-config.inc.php
@@ -0,0 +1,11 @@
+--- config.inc.php.orig	2009-04-08 06:42:59.000000000 +0200
++++ config.inc.php	2012-05-04 19:43:02.000000000 +0200
+@@ -100,7 +100,7 @@
+ */
+ $list_webcals = array(
+ #	'webcal://dimer.tamu.edu/calendars/seminars/Biochem.ics'
+-'webcal://calendar.sxsw.com/iCal-EXDrB-sa-sxsw@rollerfeet.com.ics'
++#	'webcal://calendar.sxsw.com/iCal-EXDrB-sa-sxsw@rollerfeet.com.ics'
+ );
+ $more_webcals['recur_tests'] = array();
+ $locked_cals = array(
