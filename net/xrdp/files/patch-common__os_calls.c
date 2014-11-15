e7d732d5266f0b398b40a21935e09a4ad63331b9
Bartek Rutkowski <robak@FreeBSD.org>
2014-11-15 00:26:18 +0000

    net/xrdp: fix PAM authentication issue
    - Fix PAM authentication failure after recent base update
    - Replace local patch with new patch from upstream
    - Rename patch files, remove dirrms, use @sample, some plist fixes
    
    PR:		194474
    Submitted by:	Koichiro Iwao <meta+ports@vmeta.jp>
    Approved by:	mentors (implicit)

--- net/xrdp/files/patch-common__os_calls.c
+++ net/xrdp/files/patch-common__os_calls.c
@@ -1,11 +1,17 @@
---- common/os_calls.c.orig	2011-03-12 16:10:35.000000000 +0900
-+++ common/os_calls.c	2011-03-12 16:34:05.000000000 +0900
-@@ -1987,6 +1987,8 @@
- g_clearenv(void)
+diff --git common/os_calls.c common/os_calls.c
+index 692dc01..7075ee3 100644
+--- common/os_calls.c
++++ common/os_calls.c
+@@ -2115,8 +2115,12 @@ g_clearenv(void)
  {
  #if defined(_WIN32)
-+#elif defined(__FreeBSD__)
-+  environ[0] = NULL;
  #else
++#if defined(BSD)
++  environ[0] = 0;
++#else
    environ = 0;
  #endif
++#endif
+ }
+ 
+ /*****************************************************************************/
