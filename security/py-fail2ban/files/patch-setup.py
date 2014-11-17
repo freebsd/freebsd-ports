801ee20f9d7f10ad838738606a5483c793be60e6
wen <wen@FreeBSD.org>
2014-11-17 14:41:24 +0000

    - Update to 0.9.1
    PR:		194815
    Submitted by:	theis@gmx.at(maintainer)

--- security/py-fail2ban/files/patch-setup.py
+++ security/py-fail2ban/files/patch-setup.py
@@ -1,13 +1,40 @@
---- setup.py.orig	2014-04-02 18:37:35.000000000 +0200
-+++ setup.py	2014-04-02 18:38:12.000000000 +0200
-@@ -141,10 +141,6 @@
- 						),
- 						('/var/lib/fail2ban',
- 							''
--						),
--						('/usr/share/doc/fail2ban',
--							['README.md', 'README.Solaris', 'DEVELOP', 'FILTERS',
--							 'doc/run-rootless.txt']
- 						)
- 					],
+--- setup.py.orig	2014-11-08 10:14:31.000000000 +0100
++++ setup.py	2014-11-08 10:17:37.000000000 +0100
+@@ -96,26 +96,13 @@
+ 		'bin/fail2ban-client',
+ 		'bin/fail2ban-server',
+ 		'bin/fail2ban-regex',
+-		'bin/fail2ban-testcases',
+ 	],
+ 	packages = [
+ 		'fail2ban',
+ 		'fail2ban.client',
+ 		'fail2ban.server',
+-		'fail2ban.tests',
+-		'fail2ban.tests.action_d',
+ 	],
+ 	package_data = {
+-		'fail2ban.tests':
+-			[ join(w[0], f).replace("fail2ban/tests/", "", 1)
+-				for w in os.walk('fail2ban/tests/files')
+-				for f in w[2]] +
+-			[ join(w[0], f).replace("fail2ban/tests/", "", 1)
+-				for w in os.walk('fail2ban/tests/config')
+-				for f in w[2]] +
+-			[ join(w[0], f).replace("fail2ban/tests/", "", 1)
+-				for w in os.walk('fail2ban/tests/action_d')
+-				for f in w[2]]
+ 	},
+ 	data_files = [
+ 		('/etc/fail2ban',
+@@ -136,10 +123,6 @@
+ 		),
+ 		('/var/lib/fail2ban',
+ 			''
+-		),
+-		('/usr/share/doc/fail2ban',
+-			['README.md', 'README.Solaris', 'DEVELOP', 'FILTERS',
+-			 'doc/run-rootless.txt']
+ 		)
+ 	],
  	**setup_extra
