5676e4c0b6eee6942cffc01ddef3d080fc2f64f5
Thierry Thomas <thierry@FreeBSD.org>
2014-11-22 18:44:55 +0000

    Upgrade cad/opencascade to 6.8.0.
    Changelog at http://www.opencascade.com/pub/doc/Release_Notes_6.8.0.pdf
    
    PR:		ports/195172
    Submitted by:	/me

--- cad/opencascade/files/patch-inc_Standard_CLocaleSentry.hxx
+++ cad/opencascade/files/patch-inc_Standard_CLocaleSentry.hxx
@@ -1,11 +1,11 @@
---- inc/Standard_CLocaleSentry.hxx.orig	2014-04-29 11:40:41.000000000 +0200
-+++ inc/Standard_CLocaleSentry.hxx	2014-05-15 21:33:15.000000000 +0200
+--- inc/Standard_CLocaleSentry.hxx.orig	2014-11-07 16:16:43.000000000 +0100
++++ inc/Standard_CLocaleSentry.hxx	2014-11-16 14:42:51.000000000 +0100
 @@ -25,7 +25,7 @@
- //! Notice that this is impossible to test (_POSIX_C_SOURCE >= 200809L)
- //! since POSIX didn't declared such identifier.
- //! We check _GNU_SOURCE for glibc extensions here and it is always defined by g++ compiler.
--#if defined(__APPLE__) || defined(_GNU_SOURCE) || defined(HAVE_XLOCALE_H)
-+#if defined(__APPLE__) || defined(_GNU_SOURCE) || (defined(__FreeBSD__) && (__FreeBSD_version > 910000)) || defined(HAVE_XLOCALE_H)
-   #include <xlocale.h>
-   #ifndef HAVE_XLOCALE_H
+   //! and become part of POSIX since '2008.
+   //! Notice that this is impossible to test (_POSIX_C_SOURCE >= 200809L)
+   //! since POSIX didn't declared such identifier.
+-  #if defined(__APPLE__)
++  #if defined(__APPLE__) || (defined(__FreeBSD__) && (__FreeBSD_version > 910000))
      #define HAVE_XLOCALE_H
+   #endif
+ 
