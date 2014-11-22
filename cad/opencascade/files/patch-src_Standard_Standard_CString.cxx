3edf7e535c0bfdaaa651cbbe94f6926a784275aa
thierry <thierry@FreeBSD.org>
2014-11-22 18:44:55 +0000

    Upgrade cad/opencascade to 6.8.0.
    Changelog at http://www.opencascade.com/pub/doc/Release_Notes_6.8.0.pdf
    
    PR:		ports/195172
    Submitted by:	/me

--- cad/opencascade/files/patch-src_Standard_Standard_CString.cxx
+++ cad/opencascade/files/patch-src_Standard_Standard_CString.cxx
@@ -1,11 +1,11 @@
---- src/Standard/Standard_CString.cxx.orig	2014-04-29 11:40:41.000000000 +0200
-+++ src/Standard/Standard_CString.cxx	2014-10-18 16:05:36.000000000 +0200
-@@ -94,7 +94,7 @@
-   // So we switch to C locale temporarily
-   #define SAVE_TL() Standard_CLocaleSentry aLocaleSentry;
-   #ifndef HAVE_XLOCALE_H
--    #error System does not support xlocale. Import/export could be broken if C locale did not specified by application.
-+    // #error System does not support xlocale. Import/export could be broken if C locale did not specified by application.
+--- src/Standard/Standard_CString.cxx.orig	2014-11-07 16:16:43.000000000 +0100
++++ src/Standard/Standard_CString.cxx	2014-11-16 14:57:27.000000000 +0100
+@@ -81,7 +81,7 @@
+     // strtod, strtol, strtoll functions. For other system with locale-depended
+     // implementations problems may appear if "C" locale is not set explicitly.
+     #ifndef __ANDROID__
+-      #error System does not support xlocale. Import/export could be broken if C locale did not specified by application.
++      #warning System does not support xlocale. Import/export could be broken if C locale did not specified by application.
+     #endif
      #define strtod_l(thePtr, theNextPtr, theLocale)              strtod(thePtr, theNextPtr)
    #endif
-   #define vprintf_l(theLocale, theFormat, theArgPtr)             vprintf(theFormat, theArgPtr)
