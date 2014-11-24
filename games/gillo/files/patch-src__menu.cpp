ec236a19e3a7293e5204bb07995532480786848b
amdmi3 <amdmi3@FreeBSD.org>
2014-11-24 23:45:29 +0000

    - Clarify LICENSE - Add LICENSE_FILE - Fix UB caused by absent return statements which leads to crashes on 10.0+ - Drop @dirrm* from plist

---
+++ games/gillo/files/patch-src__menu.cpp
@@ -0,0 +1,10 @@
+--- src/menu.cpp.orig	2005-07-19 03:25:16.000000000 +0400
++++ src/menu.cpp	2014-11-24 16:08:56.000000000 +0300
+@@ -341,6 +341,7 @@
+ 	} else {
+ 		//@fixme throw something
+ 	}
++	return false;
+ }
+ 
+ };
