72499a985f3ca4d718f4f2328aaef5df4e2a1c4e
Danilo Egea Gondolfo <danilo@FreeBSD.org>
2014-11-25 00:21:43 +0000

    - Update to 0.6.3
    PR:		195337
    Submitted by:	C Hutchinson <portmaster@bsdforge.com> (maintainer)

--- devel/ecgi/files/patch-html2h_html2h.c
+++ devel/ecgi/files/patch-html2h_html2h.c
@@ -1,5 +1,5 @@
---- html2h/html2h.c.orig	2014-04-26 08:44:43.000000000 +0000
-+++ html2h/html2h.c
+--- html2h/html2h.c.orig	2014-11-24 13:09:14.000000000 -0800
++++ html2h/html2h.c	2014-11-24 13:25:12.000000000 -0800
 @@ -6,15 +6,10 @@
  
  void usage()
@@ -8,22 +8,24 @@
 -html2h v0.1
 -usage:
 -	html2h input.html [output.h]
--	
+-
 -	if output is not set, input.h will be generated and overwritten!
 -
 -	debug messages are written to stderr!
 -
 +	printf("html2h v0.1\nusage:\n \
-+	html2h input.html [output.h]\n\n \
-+	if output is not set, input.h will be generated and overwritten!\n\n \
-+	debug messages are written to stderr!\n\n \
+++	html2h input.html [output.h]\n\n \
+++	if output is not set, input.h will be generated and overwritten!\n\n \
+++	debug messages are written to stderr!\n\n \
  ");
  
  	exit(0);
-@@ -415,4 +410,4 @@ void pexit(char *msg, const char *commen
+@@ -415,4 +410,6 @@
  {
  	fprintf(stderr, "%s%s\n", msg, comment);
  	exit(1);
 -}
 \ No newline at end of file
++
 +}
++
