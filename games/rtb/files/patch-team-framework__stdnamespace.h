9f86d5d19e0380a1a04a4fa4790e9a40cc9a621f
Dmitry Marakasov <amdmi3@FreeBSD.org>
2014-11-30 20:09:12 +0000

    MFH: r373682
    - Fix build with clang 3.5
    
    Submitted by:	antoine
    Approved by:	ports-secteam (rea)

--- games/rtb/files/patch-team-framework__stdnamespace.h
+++ games/rtb/files/patch-team-framework__stdnamespace.h
@@ -1,16 +1,12 @@
---- team-framework/stdnamespace.h.orig
-+++ team-framework/stdnamespace.h
-@@ -25,9 +25,13 @@
- 
+--- team-framework/stdnamespace.h.orig	2005-01-06 20:59:18.000000000 +0300
++++ team-framework/stdnamespace.h	2014-11-30 22:10:41.000000000 +0300
+@@ -26,8 +26,4 @@
  **************************************************************************/
  
-+#if !defined(_LIBCPP_VERSION)
  #include <iosfwd>	// This file contains the forward declaration in particular of std::string
- 
- namespace std
- {
- 	template <class _Tp> class auto_ptr;	// Origin: The header file <memory>
- }
-+#else
+-
+-namespace std
+-{
+-	template <class _Tp> class auto_ptr;	// Origin: The header file <memory>
+-}
 +#include <memory>
-+#endif
