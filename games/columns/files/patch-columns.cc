c067c89f61c6038bbe4eac5b6355d6b3726d9e30
Dmitry Marakasov <amdmi3@FreeBSD.org>
2014-12-02 02:25:47 +0000

    - Clarify LICENSE - Add LICENSE_FILE - Simplify MASTER_SITES - Add distfile mirror - Move highscores to /var/games, use @sample to handle them - Fix font rendering - Drop @dirrm* from plist
    MFH:		2014Q4

--- games/columns/files/patch-columns.cc
+++ games/columns/files/patch-columns.cc
@@ -1,15 +1,10 @@
-
-$FreeBSD$
-
---- columns.cc.orig	2000-03-17 04:52:55.000000000 +0400
-+++ columns.cc	2013-06-13 15:21:46.472644056 +0400
-@@ -22,10 +22,12 @@
+--- columns.cc.orig	2000-03-17 03:52:55.000000000 +0300
++++ columns.cc	2014-12-02 05:13:18.850698698 +0300
+@@ -22,10 +22,9 @@
  #include <stdarg.h>
  #include <stdio.h>
  #include <string.h>
-+#ifndef __STDC__
- #include <malloc.h>
-+#endif
+-#include <malloc.h>
  #include <stdlib.h>
  #include <time.h>
 -#include <SDL/SDL.h>
@@ -17,7 +12,33 @@ $FreeBSD$
  
  #define HIGHSCORE_X 10
  #define HIGHSCORE_Y 30
-@@ -1365,6 +1367,7 @@ void eval_args(int argc, char **argv)
+@@ -269,6 +268,7 @@
+     for (int i=0; i<256; i++) {
+ 	data[i] = NULL;
+ 	fnt->w[i] = fnt->h[i]= fnt->x[i] = 0;
++	fnt->strut = 0;
+     }
+   
+     // read character chunks one by one
+@@ -357,7 +357,7 @@
+ 
+ void highscore_save()
+ {
+-    char *fname = find_datafile("columns.hsc");
++    char *fname = "/var/games/columns.hsc";
+     FILE *fp = fopen(fname,"wb");
+     if (fp != NULL) {
+ 	for (int i=0; i<HIGHSCORE_ENTRIES; i++)
+@@ -369,7 +369,7 @@
+ void highscore_load()
+ {
+     char line[30];
+-    char *fname = find_datafile("columns.hsc");
++    char *fname = "/var/games/columns.hsc";
+     FILE *fp = fopen(fname,"rb");
+     if (fp != NULL) {
+ 	for (int i=0; i<HIGHSCORE_ENTRIES; i++) {
+@@ -1365,6 +1365,7 @@
      }
  }
  
