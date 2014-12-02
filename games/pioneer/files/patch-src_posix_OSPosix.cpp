5b8c897dba5392dcd3d7959a6809e23205edea0c
Dmitry Marakasov <amdmi3@FreeBSD.org>
2014-12-02 03:15:33 +0000

    - Update to 20141128 (changes: https://github.com/pioneerspacesim/pioneer/compare/20141029...20141128) - Update licenses - Added OPSYS in conjunction to OSVERSION - Wrapped cd related command to parentheses - Corrected patch for src/posix/OSPosix.cpp file - Fix multiple WWW in pkg-descr
    PR:		195514
    Submitted by:	lightside@gmx.com (maintainer)

--- games/pioneer/files/patch-src_posix_OSPosix.cpp
+++ games/pioneer/files/patch-src_posix_OSPosix.cpp
@@ -5,7 +5,7 @@
  
  	char infoString[2048];
 -#if defined(__APPLE__)
-+#if defined(__APPLE__) || defined(__FREEBSD__)
++#if !defined(__linux__)
  	snprintf(infoString, 2048, "System Name: %s\nHost Name: %s\nRelease(Kernel) Version: %s\nKernel Build Timestamp: %s\nMachine Arch: %s\n",
  		uts.sysname, uts.nodename, uts.release, uts.version, uts.machine);
  #else
