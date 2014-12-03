c0f7cae3328950cd536c5b5e5d0c5643287d3801
Koop Mast <kwm@FreeBSD.org>
2014-12-03 23:27:39 +0000

    Update fontconfig to 2.11.1.
    While here:
     - Drop :keepla from USES=libtool
     - Use INSTALL_TARGET for stripping
     - Sprinkle some carrier returns in the post-install target so they stay within 77 char length.
     - Switch to @sample for config file
     - Remove @dirrm[try]'s from plist
    
    PR:		195595
    Submitted by:	delphij@

--- x11-fonts/fontconfig/files/patch-src__fccompat.c
+++ x11-fonts/fontconfig/files/patch-src__fccompat.c
@@ -2,12 +2,14 @@ There is an undiagnosed ARMv6 core dump in this code
 as discovered in FreeBSD Bug 183536 when using clang.
 Removing this code from ARM builds resolves it.
 
---- src/fccompat.c.orig	2013-10-11 03:10:18 UTC
-+++ src/fccompat.c
-@@ -178,7 +178,7 @@
-     }
- 
-     random_r(&fcrandbuf, &result);
+https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=183536
+
+--- src/fccompat.c.orig	2014-08-12 16:34:15.875384345 +0200
++++ src/fccompat.c	2014-08-12 16:35:04.752379905 +0200
+@@ -191,7 +191,7 @@
+ #else
+     random_r (&fcrandbuf, &result);
+ #endif
 -#elif HAVE_RANDOM
 +#elif HAVE_RANDOM && !defined(__arm__)
      static char statebuf[256];
