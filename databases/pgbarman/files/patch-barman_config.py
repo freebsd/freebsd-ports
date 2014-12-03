0bd8ff190bf2a302d59fbed7bf964bc11fce6f21
Matthew Seaman <matthew@FreeBSD.org>
2014-12-03 17:05:04 +0000

    - Install man pages to a location where man(1) can find them
    - While here update man pages and speciment barman.conf file to use
      FreeBSD-ish locations for various things, particularly that our idea
      of the PostgreSQL default user is 'pgsql' although the database to
      connect to is 'postgres'

--- databases/pgbarman/files/patch-barman_config.py
+++ databases/pgbarman/files/patch-barman_config.py
@@ -1,4 +1,4 @@
---- barman/config.py.orig	2014-12-02 14:47:11 UTC
+--- barman/config.py.orig	2014-08-01 13:50:46 UTC
 +++ barman/config.py
 @@ -341,8 +341,8 @@ class Config(object):
      """
@@ -6,8 +6,8 @@
          '~/.barman.conf',
 -        '/etc/barman.conf',
 -        '/etc/barman/barman.conf',
-+        '%%PREFIX%%/etc/barman.conf',
-+        '%%PREFIX%%/etc/barman/barman.conf',
++        '/usr/local/etc/barman.conf',
++        '/usr/local/etc/barman/barman.conf',
      ]
  
      _QUOTE_RE = re.compile(r"""^(["'])(.*)\1$""")
