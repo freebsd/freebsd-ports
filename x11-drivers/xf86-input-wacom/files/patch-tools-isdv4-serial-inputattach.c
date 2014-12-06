5570f85834cf808b757e3f54c9a93a60fbfaede7
Thomas Zander <riggs@FreeBSD.org>
2014-12-06 14:07:53 +0000

    Update to upstream version 0.27.0
    PR:		195370
    Submitted by:	jbeich@vfemail.net (maintainer)

--- x11-drivers/xf86-input-wacom/files/patch-tools-isdv4-serial-inputattach.c
+++ x11-drivers/xf86-input-wacom/files/patch-tools-isdv4-serial-inputattach.c
@@ -31,13 +31,13 @@
  static int bind_kernel_driver(int fd)
  {
  	unsigned long devt;
-@@ -77,9 +85,11 @@ static int bind_kernel_driver(int fd)
+@@ -77,9 +84,11 @@ static int bind_kernel_driver(int fd)
  
  	return 0;
  }
 +#endif
  
- int get_baud_rate(int fd)
+ static int get_baud_rate(int fd)
  {
 +#ifdef __linux__
  	struct stat st;
