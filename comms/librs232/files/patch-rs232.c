1748d4d1397e4479f86201f4db80b7e2b20a23b0
kevlo <kevlo@FreeBSD.org>
2014-12-08 06:32:39 +0000

    Update to 20141206.

--- comms/librs232/files/patch-rs232.c
+++ comms/librs232/files/patch-rs232.c
@@ -1,46 +1,6 @@
---- rs232.c.orig	2014-10-05 15:21:14.000000000 +0800
-+++ rs232.c	2014-12-04 14:24:26.000000000 +0800
-@@ -38,19 +38,23 @@
- 
- 
- 
--#ifdef __linux__   /* Linux */
-+#if defined(__linux__) || defined(__FreeBSD__)
- 
- 
--int Cport[30],
-+int Cport[38],
-     error;
- 
- struct termios new_port_settings,
--       old_port_settings[30];
-+       old_port_settings[38];
- 
--char comports[30][16]={"/dev/ttyS0","/dev/ttyS1","/dev/ttyS2","/dev/ttyS3","/dev/ttyS4","/dev/ttyS5",
--                       "/dev/ttyS6","/dev/ttyS7","/dev/ttyS8","/dev/ttyS9","/dev/ttyS10","/dev/ttyS11",
--                       "/dev/ttyS12","/dev/ttyS13","/dev/ttyS14","/dev/ttyS15","/dev/ttyUSB0",
--                       "/dev/ttyUSB1","/dev/ttyUSB2","/dev/ttyUSB3","/dev/ttyUSB4","/dev/ttyUSB5",
-+char comports[38][16]={"/dev/cuau0", "/dev/cuau1", "/dev/cuau2", "/dev/cuau3",
-+                       "/dev/cuaU0", "/dev/cuaU1", "/dev/cuaU2", "/dev/cuaU3",
-+                       "/dev/ttyS0","/dev/ttyS1","/dev/ttyS2","/dev/ttyS3",
-+		       "/dev/ttyS4","/dev/ttyS5", "/dev/ttyS6","/dev/ttyS7",
-+                       "/dev/ttyS8","/dev/ttyS9","/dev/ttyS10","/dev/ttyS11",
-+                       "/dev/ttyS12","/dev/ttyS13","/dev/ttyS14","/dev/ttyS15",
-+		       "/dev/ttyUSB0", "/dev/ttyUSB1","/dev/ttyUSB2",
-+		       "/dev/ttyUSB3", "/dev/ttyUSB4","/dev/ttyUSB5",
-                        "/dev/ttyAMA0","/dev/ttyAMA1","/dev/ttyACM0","/dev/ttyACM1",
-                        "/dev/rfcomm0","/dev/rfcomm1","/dev/ircomm0","/dev/ircomm1"};
- 
-@@ -61,7 +65,7 @@
-   int baudr,
-       status;
- 
--  if((comport_number>29)||(comport_number<0))
-+  if((comport_number>37)||(comport_number<0))
-   {
-     printf("illegal comport number\n");
-     return(1);
-@@ -107,28 +111,6 @@
+--- rs232.c.orig	2014-12-08 14:19:24.000000000 +0800
++++ rs232.c	2014-12-08 14:19:38.000000000 +0800
+@@ -107,28 +107,6 @@
                     break;
      case  460800 : baudr = B460800;
                     break;
