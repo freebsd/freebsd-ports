17d28a2bdda0661330f991745e15ed0ba1716df7
pi <pi@FreeBSD.org>
2014-12-09 15:58:50 +0000

    misc/mc: really re-add small patch that was deleted in r369133
    Thanks to tijl: patch syntax was bogus and was ignored.
    
    PR:		194627

--- misc/mc/files/patch-lib__tty__win.c
+++ misc/mc/files/patch-lib__tty__win.c
@@ -1,14 +1,14 @@
 --- lib/tty/win.c.orig	2014-09-02 09:23:58 UTC
 +++ lib/tty/win.c
-+@@ -95,7 +95,7 @@
-+ void
-+ do_enter_ca_mode (void)
-+ {
-+-    if (mc_global.tty.xterm_flag && smcup != NULL)
-++    if (mc_global.tty.xterm_flag)
-+     {
-+         fprintf (stdout, /* ESC_STR ")0" */ ESC_STR "7" ESC_STR "[?47h");
-+         fflush (stdout);
+@@ -95,7 +95,7 @@
+ void
+ do_enter_ca_mode (void)
+ {
+-    if (mc_global.tty.xterm_flag && smcup != NULL)
++    if (mc_global.tty.xterm_flag)
+     {
+         fprintf (stdout, /* ESC_STR ")0" */ ESC_STR "7" ESC_STR "[?47h");
+         fflush (stdout);
 @@ -107,7 +107,7 @@
  void
  do_exit_ca_mode (void)
