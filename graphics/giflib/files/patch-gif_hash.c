a96c525ff055f8bd3dfc4c42447ee84399237f04
antoine <antoine@FreeBSD.org>
2014-12-10 15:52:21 +0000

    Restore those ports to their previous state No PORTEPOCH needed as they didn't build

---
+++ graphics/giflib/files/patch-gif_hash.c
@@ -0,0 +1,10 @@
+--- lib/gif_hash.c.orig	2013-08-29 21:32:18.000000000 +0200
++++ lib/gif_hash.c	2013-09-03 05:53:45.000000000 +0200
+@@ -21,6 +21,7 @@
+ #include <fcntl.h>
+ #include <stdio.h>
+ #include <string.h>
++#include <stdlib.h>
+ 
+ #include "gif_lib.h"
+ #include "gif_hash.h"
