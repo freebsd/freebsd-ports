3317196d9068c70ce35a18561a657e6e0bfde8cb
Diane Bruce <db@FreeBSD.org>
2014-12-11 13:00:44 +0000

    - Tiny fix to f2py2e.py to allow it to deal with -fstack-protector flag   as without this fix, f2py will attempt to compile -fstack-protector
    Submitted by:	db
    Reviewed by:	wg
    Approved by:	wg

--- math/py-numpy/files/patch-numpy-f2py-f2py2e.py
+++ math/py-numpy/files/patch-numpy-f2py-f2py2e.py
@@ -1,14 +1,14 @@
---- numpy/f2py/f2py2e.py.orig	2013-10-30 13:31:41.000000000 -0500
-+++ numpy/f2py/f2py2e.py	2014-04-09 10:23:49.000000000 -0500
-@@ -520,6 +520,7 @@
+--- numpy/f2py/f2py2e.py.orig	2014-10-26 10:36:14.000000000 -0400
++++ numpy/f2py/f2py2e.py	2014-12-11 07:52:11.000000000 -0500
+@@ -516,6 +516,7 @@
      include_dirs, sources = filter_files('-I', '', sources, remove_prefix=1)
      library_dirs, sources = filter_files('-L', '', sources, remove_prefix=1)
      libraries, sources = filter_files('-l', '', sources, remove_prefix=1)
-+    extra_link_args, sources = filter_files('-Wl,|-B','',sources,remove_prefix=0)
++    extra_link_args, sources = filter_files('-Wl,|-B|-f','',sources,remove_prefix=0)
      undef_macros, sources = filter_files('-U', '', sources, remove_prefix=1)
      define_macros, sources = filter_files('-D', '', sources, remove_prefix=1)
      using_numarray = 0
-@@ -555,6 +556,7 @@
+@@ -551,6 +552,7 @@
                  'undef_macros': undef_macros,
                  'extra_objects': extra_objects,
                  'f2py_options': f2py_flags,
