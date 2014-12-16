4a50f64bed005f9a3474d315bff2094795c00365
Diane Bruce <db@FreeBSD.org>
2014-12-16 02:58:47 +0000

    Update py-numpy use in WsprMode/spectjt.py as well

--- comms/wspr/files/patch-WsprMode_specjt.py
+++ comms/wspr/files/patch-WsprMode_specjt.py
@@ -1,6 +1,6 @@
---- WsprMod/specjt.py.orig	2014-12-14 19:17:21.630624057 -0500
-+++ WsprMod/specjt.py	2014-12-14 19:19:41.021613990 -0500
-@@ -34,10 +34,7 @@
+--- WsprMod/specjt.py.orig	2014-12-15 21:51:30.000000000 -0500
++++ WsprMod/specjt.py	2014-12-15 21:54:38.000000000 -0500
+@@ -34,10 +34,8 @@
  import tkinter.messagebox
  from . import w
  
@@ -8,7 +8,17 @@
 -    from numpy.oldnumeric import zeros, multiarray
 -except:
 -    from Numeric import zeros, multiarray
-+from numpy import zeros, multiarray
++from numpy import zeros
++from numpy.core import multiarray
  import Image, ImageTk, ImageDraw
  from .palettes import colormapblue, colormapgray0, colormapHot, \
       colormapAFMHot, colormapgray1, colormapLinrad, Colormap2Palette
+@@ -94,7 +92,7 @@
+ ttot=0.0
+ 
+ c=Canvas()
+-a=zeros(225000,'s')
++##a=zeros(225000,'s')
+ im=Image.new('P',(750,300))
+ line0=Image.new('P',(750,1))      #Image fragment for top line of waterfall
+ draw=ImageDraw.Draw(im)
