7f834d2f52898fbfba4e7a286852b5b8a67b52bc
pawel <pawel@FreeBSD.org>
2014-12-16 19:35:06 +0000

    Update synfig ports to 0.64.2 release
    PR:		194495
    Submitted by:	tkato432@yahoo.com

--- graphics/synfigstudio/files/patch-src_synfigapp_cvs.cpp
+++ graphics/synfigstudio/files/patch-src_synfigapp_cvs.cpp
@@ -4,8 +4,8 @@
  #define HAVE_STRPTIME
  #endif
  
--#ifdef __APPLE__
-+#if defined(__APPLE__) || defined(__FreeBSD__)
+-#if defined(__APPLE__) || defined(__OpenBSD__)
++#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__)
  time_t _daylight_() { time_t t(time(0)); return localtime(&t)->tm_gmtoff; }
  #define daylight _daylight_()
  #endif
