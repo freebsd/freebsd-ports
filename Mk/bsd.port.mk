82f930622e350490aacfbee595d9a6e7309941f2
bapt <bapt@FreeBSD.org>
2014-12-17 14:32:59 +0000

    Add USES=fakeroot to allow packaging ports that are not stageable as a user It uses debian's fakeroot, like NEED_ROOT (which it is supposed to replace) it is preferred to fix the port to properly stage without those hacks

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -3557,7 +3557,7 @@ check-install-conflicts:
 
 .if !target(do-install) && !defined(NO_INSTALL)
 do-install:
-	@(cd ${INSTALL_WRKSRC} && ${SETENV} ${MAKE_ENV} ${MAKE_CMD} ${MAKE_FLAGS} ${MAKEFILE} ${MAKE_ARGS} ${INSTALL_TARGET})
+	@(cd ${INSTALL_WRKSRC} && ${SETENV} ${MAKE_ENV} ${FAKEROOT} ${MAKE_CMD} ${MAKE_FLAGS} ${MAKEFILE} ${MAKE_ARGS} ${INSTALL_TARGET})
 .endif
 
 # Package
