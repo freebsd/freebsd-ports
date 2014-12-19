e7860f124ba62509614b603e6104d8ca4e4fdec5
swills <swills@FreeBSD.org>
2014-12-19 14:26:39 +0000

    RubyForge has been gone for ages, remove it
    Differential Revision:	https://reviews.freebsd.org/D1339
    With hat:		portmgr

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -1137,12 +1137,6 @@ MASTER_SITE_RUBY+= \
 	ftp://ftp.iDaemons.org/pub/mirror/ftp.ruby-lang.org/ruby/%SUBDIR%/
 .endif
 
-# See http://rubyforge.org/credits/
-.if !defined(IGNORE_MASTER_SITE_RUBYFORGE)
-MASTER_SITE_RUBYFORGE+= \
-	http://files.rubyforge.vm.bytemark.co.uk/%SUBDIR%/
-.endif
-
 # See http://rubygems.org/pages/about
 .if !defined(IGNORE_MASTER_SITE_RUBYGEMS)
 MASTER_SITE_RUBYGEMS+= \
@@ -1519,8 +1513,7 @@ MASTER_SITES_ABBREVS=	CPAN:PERL_CPAN \
 			NL:NETLIB \
 			SF:SOURCEFORGE \
 			SFJP:SOURCEFORGE_JP \
-			RG:RUBYGEMS \
-			RF:RUBYFORGE
+			RG:RUBYGEMS
 MASTER_SITES_SUBDIRS=	APACHE_JAKARTA:${PORTNAME:S,-,/,}/source \
 			BERLIOS:${PORTNAME:tl}.berlios \
 			CENKES:myports \
@@ -1545,8 +1538,7 @@ MASTER_SITES_SUBDIRS=	APACHE_JAKARTA:${PORTNAME:S,-,/,}/source \
 			RUBY_DBI:${RUBY_DBI_MASTER_SITE_SUBDIR} \
 			RUBY_GNOME:${RUBY_GNOME_MASTER_SITE_SUBDIR} \
 			SAVANNAH:${PORTNAME:tl} \
-			SOURCEFORGE:${PORTNAME:tl}/${PORTNAME:tl}/${PORTVERSION} \
-			RUBYFORGE:${PORTNAME:tl}
+			SOURCEFORGE:${PORTNAME:tl}/${PORTNAME:tl}/${PORTVERSION}
 
 .if defined(MASTER_SITES) && ${MASTER_SITES:N*\:/*}
 
