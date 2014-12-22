b91214c7207893da973ac8071041560845388487
antoine <antoine@FreeBSD.org>
2014-12-22 21:32:30 +0000

    - Add missing run dependency on perl - Shebangfix

--- www/demoroniser/files/patch-demoroniser.pl
+++
@@ -1,8 +0,0 @@
---- demoroniser.pl.orig	Sat Jan 10 22:29:54 1998
-+++ demoroniser.pl	Thu Aug 12 18:36:53 1999
-@@ -1,4 +1,4 @@
--#! /bin/perl
-+#!/usr/bin/perl
- #
- #           De-moron-ise Text from Microsoft Applications
- # 
