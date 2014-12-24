d1c004c289ff21226ddf3c58979799c23a0c8dad
kevlo <kevlo@FreeBSD.org>
2014-12-24 05:59:32 +0000

    Check event_initialized before event_del if event may not have been set up; libevent2 complains about this.

--- net/ifstated/files/patch-log.c
+++ net/ifstated/files/patch-log.c
@@ -1,6 +1,13 @@
---- log.c.orig	2010-06-11 13:29:09.127347360 -0500
-+++ log.c	2010-06-11 13:29:20.636464838 -0500
-@@ -28,8 +28,8 @@
+--- log.c.orig	2007-10-25 14:03:31.000000000 +0800
++++ log.c	2014-12-05 15:11:31.000000000 +0800
+@@ -22,14 +22,15 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <syslog.h>
++#include <time.h>
+ 
+ void	log_init(int);
+ void	log_warn(const char *, ...);
  void	log_warnx(const char *, ...);
  void	log_info(const char *, ...);
  void	log_debug(const char *, ...);
