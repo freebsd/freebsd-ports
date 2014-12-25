8473fcddb83c087fc22e43d7d97f48e5ce240371
Koop Mast <kwm@FreeBSD.org>
2014-12-25 19:12:08 +0000

    Update to 2.4.7.
    Sync some changes from the webkit-gtk3 port.
    The 2.4.x series is the last webkitgtk version that will have WebKit1.

--- www/webkit-gtk2/files/patch-Source__JavaScriptCore__jit__ExecutableAllocator.h
+++ www/webkit-gtk2/files/patch-Source__JavaScriptCore__jit__ExecutableAllocator.h
@@ -1,8 +1,20 @@
---- Source/JavaScriptCore/jit/ExecutableAllocator.h.orig	2014-10-15 14:25:58 UTC
-+++ Source/JavaScriptCore/jit/ExecutableAllocator.h
-@@ -223,6 +223,11 @@
-         UNUSED_PARAM(size);
- #endif
+
+
+--- Source/JavaScriptCore/jit/ExecutableAllocator.h.orig	2014-12-25 12:52:32.786521149 +0100
++++ Source/JavaScriptCore/jit/ExecutableAllocator.h	2014-12-25 12:52:41.921520911 +0100
+@@ -104,7 +104,8 @@
+ #if ENABLE(EXECUTABLE_ALLOCATOR_FIXED)
+ #if CPU(ARM) || CPU(ARM64)
+ static const size_t fixedExecutableMemoryPoolSize = 16 * 1024 * 1024;
+-#elif CPU(X86_64)
++// We don't want an initial allocation of 1GB, it's above the default ulimit
++#elif CPU(X86_64) && !defined(__OpenBSD__) && !defined(__FreeBSD__)
+ static const size_t fixedExecutableMemoryPoolSize = 1024 * 1024 * 1024;
+ #else
+ static const size_t fixedExecutableMemoryPoolSize = 32 * 1024 * 1024;
+@@ -146,6 +147,11 @@
+     {
+         reprotectRegion(start, size, Executable);
      }
 +#elif CPU(ARM_TRADITIONAL) && OS(FREEBSD) && COMPILER(CLANG)
 +    static void cacheFlush(void* code, size_t size)
@@ -10,5 +22,5 @@
 +        __clear_cache(code, reinterpret_cast<char*>(code) + size);
 +    }
  #else
-     #error "The cacheFlush support is missing on this platform."
- #endif
+     static void makeWritable(void*, size_t) {}
+     static void makeExecutable(void*, size_t) {}
