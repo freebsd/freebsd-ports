ba96e1cdca501dcda7679d3c8a07849661ae77e9
bapt <bapt@FreeBSD.org>
2014-12-25 01:17:03 +0000

    Use PLIST_FILES instead of defining a custom post-install target to prepare the plist

--- Mk/bsd.xorg.mk
+++ Mk/bsd.xorg.mk
@@ -104,14 +104,13 @@ RUN_DEPENDS+=	${LOCALBASE}/bin/mkfontdir:${PORTSDIR}/x11-fonts/mkfontdir \
 				${LOCALBASE}/bin/mkfontscale:${PORTSDIR}/x11-fonts/mkfontscale
 .  endif
 
-post-install:
 .  for _fontdir in ${FONTDIR}
 .    if ${INSTALLS_TTF} == yes && ${NEED_MKFONTFOO} == yes
-		@${ECHO_CMD} "@fcfontsdir lib/X11/fonts/${_fontdir}" >> ${TMPPLIST}
+PLIST_FILES+=	"@fcfontsdir ${PREFIX}/lib/X11/fonts/${_fontdir}"
 .    elif ${INSTALLS_TTF} == yes && ${NEED_MKFONTFOO} == no
-		@${ECHO_CMD} "@fc lib/X11/fonts/${_fontdir}" >> ${TMPPLIST}
+PLIST_FILES+=	"@fc ${PREFIX}/lib/X11/fonts/${_fontdir}"
 .    elif ${NEED_MKFONTFOO} == yes
-		@${ECHO_CMD} "@fontsdir lib/X11/fonts/${_fontdir}" >> ${TMPPLIST}
+PLIST_FILES+=	"@fontsdir ${PREFIX}lib/X11/fonts/${_fontdir}"
 .    endif
 .  endfor
 .endif
