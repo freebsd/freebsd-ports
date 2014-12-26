25ca83f2f5da0e685e766eb253af47f57cadffbe
Antoine Brodin <antoine@FreeBSD.org>
2014-12-26 17:44:35 +0000

    Allow building with either giflib 4.2 or 5.0

--- japanese/ming/files/patch-util_gif2dbl.c
+++ japanese/ming/files/patch-util_gif2dbl.c
@@ -1,11 +1,22 @@
---- ../util/gif2dbl.c~	2002-06-24 21:21:54.000000000 +0900
-+++ ../util/gif2dbl.c	2013-03-14 04:57:51.000000000 +0900
-@@ -16,7 +16,7 @@
+--- ../util/gif2dbl.c.orig	2002-06-24 12:21:54.000000000 +0000
++++ ../util/gif2dbl.c	2014-12-26 17:29:52.000000000 +0000
+@@ -16,7 +16,6 @@
  void error(char *msg)
  {
    printf("%s:\n\n", msg);
 -  PrintGifError();
-+  GifError();
    exit(-1);
  }
  
+@@ -50,7 +49,11 @@ unsigned char *readGif(char *fileName, i
+   unsigned char *p;
+   int i, nColors, size, alpha, bgColor, alignedWidth;
+ 
++#if GIFLIB_MAJOR >= 5
++  if((file = DGifOpenFileName(fileName, NULL)) == NULL)
++#else
+   if((file = DGifOpenFileName(fileName)) == NULL)
++#endif
+     error("Error opening file");
+ 
+   if(DGifSlurp(file) != GIF_OK)
