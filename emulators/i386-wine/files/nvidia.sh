145b805ccb5dc316fa255469486b50643dabca6a
David Naylor <dbn@FreeBSD.org>
2014-12-26 18:52:16 +0000

    Backport changes from emulators/i386-wine-devel to emulators/i386-wine.
    Changes:
     - Fix install conflicts [1] (for the "newly" added compholio port)
     - nvidia.sh: Gracefully handle a corrupt nVidia tarball
     - nvidia.sh: Provide checksum and size information for nVidia tarball
     - Reduce diff between i386-wine and i386-wine-devel:
       - Add support for sub-ports (unused by this port)
       - Properly detect linked (and dlopen) libraries
       - binbounce: Properly set LD_(32_)?LIBRARY_PATH_RPATH variables
       - nvidia.sh: Add detection for i386-wine-compholio
     - Bump master port [1] due to changes to binbounce, nvidia.sh and shared
       library handling.
    
    Approved by:    gerald@ [1]

--- emulators/i386-wine/files/nvidia.sh
+++ emulators/i386-wine/files/nvidia.sh
@@ -63,6 +63,11 @@
 #  - add deinstall option
 # Version 1.12 - 2013/11/03
 #  - add detection for i386-wine-devel
+# Version 1.13 - 2014/08/05
+#  - add detection for i386-wine-compholio
+# Version 1.14 - 2014/12/26
+#  - gracefully handle a corrupt nVidia tarball
+#  - provide checksum and size information for nVidia tarball
 
 set -e
 
@@ -147,7 +152,7 @@ echo "===> Patching i386-wine to work with x11/nvidia-driver:"
 
 if [ -z "${WINE}" ]
 then
-  WINE=`version i386-wine; version i386-wine-devel`
+  WINE=`version i386-wine; version i386-wine-devel; version i386-wine-compholio`
 fi
 [ -n "$WINE" ] \
   || terminate 255 "Unable to detect i386-wine, please install first"
@@ -161,13 +166,18 @@ echo "=> Detected nvidia-driver: ${NV}"
 NVIDIA=${NV}
 NV=`echo ${NV} | cut -f 1 -d _ | cut -f 1 -d ,`
 
-if [ ! -f NVIDIA-FreeBSD-x86-${NV}.tar.gz ]
+if [ ! -f NVIDIA-FreeBSD-x86-${NV}.tar.gz ] || !(tar -tf NVIDIA-FreeBSD-x86-${NV}.tar.gz > /dev/null 2>&1)
 then
   [ -n "$NO_FETCH" ] \
     && terminate 8 "NVIDIA-FreeBSD-x86-${NV}.tar.gz unavailable"
   echo "=> Downloading NVIDIA-FreeBSD-x86-${NV}.tar.gz from ftp://download.nvidia.com..."
+  rm -f NVIDIA-FreeBSD-x86-${NV}.tar.gz
   fetch -apRr ftp://download.nvidia.com/XFree86/FreeBSD-x86/${NV}/NVIDIA-FreeBSD-x86-${NV}.tar.gz \
     || terminate 2 "Failed to download NVIDIA-FreeBSD-x86-${NV}.tar.gz"
+  echo "=> Downloaded NVIDIA-FreeBSD-x86-${NV}.tar.gz"
+  echo "Please check the following information against /usr/ports/x11/nvidia-driver/distinfo"
+  sha256 NVIDIA-FreeBSD-x86-${NV}.tar.gz
+  echo "SIZE (NVIDIA-FreeBSD-x86-${NV}.tar.gz) = `stat -f "%z" NVIDIA-FreeBSD-x86-${NV}.tar.gz`"
 fi
 
 echo "=> Extracting NVIDIA-FreeBSD-x86-${NV}.tar.gz to $PREFIX/lib32..."
