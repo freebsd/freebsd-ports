5a8fcb495b1e27da1748a2b86cf28a95c0e3b9e0
Jimmy Olgeni <olgeni@FreeBSD.org>
2014-12-27 18:54:35 +0000

    Fix infinite loop at startup when _SC_OPEN_MAX > 64k.

---
+++ x11/eterm/files/patch-src_command.c
@@ -0,0 +1,14 @@
+
+$FreeBSD$
+
+--- src/command.c.orig
++++ src/command.c
+@@ -1561,7 +1561,7 @@
+      * child processes remain alive upon deletion of the window.
+      */
+     {
+-        unsigned short i;
++        unsigned long i;
+         unsigned long max_fds;
+ 
+         /* get number of available file descriptors */
