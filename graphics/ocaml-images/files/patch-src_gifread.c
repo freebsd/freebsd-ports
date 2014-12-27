fdc3c430a34ff7da5719b003cc0d4cfa0304adff
antoine <antoine@FreeBSD.org>
2014-12-27 00:44:05 +0000

    Allow building with either giflib 4.2 or 5.0

--- graphics/ocaml-images/files/patch-src_gifread.c
+++ graphics/ocaml-images/files/patch-src_gifread.c
@@ -1,6 +1,18 @@
---- src/gifread.c.orig	2012-11-07 22:33:55.000000000 +0100
-+++ src/gifread.c	2012-11-07 22:34:04.000000000 +0100
-@@ -200,7 +200,6 @@
+--- src/gifread.c.orig	2011-06-22 18:04:32 UTC
++++ src/gifread.c
+@@ -140,7 +140,11 @@ value dGifOpenFileName( value name )
+   GifFileType *GifFile;
+   int i;
+ 
++#if GIFLIB_MAJOR >= 5
++  if((GifFile = DGifOpenFileName( String_val(name), NULL )) == NULL){
++#else
+   if((GifFile = DGifOpenFileName( String_val(name) )) == NULL){
++#endif
+     failwith("DGifOpenFileName");
+   }
+ 
+@@ -200,7 +204,6 @@ value dGifGetLine( value hdl )
  
    if( DGifGetLine(GifFile, String_val(buf), GifFile->Image.Width ) 
        == GIF_ERROR ){
