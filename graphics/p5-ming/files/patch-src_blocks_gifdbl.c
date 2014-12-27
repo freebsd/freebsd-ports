14388e135289bd2f2bea8e62319ec4da3c9e2c27
antoine <antoine@FreeBSD.org>
2014-12-27 00:49:30 +0000

    Allow building with either giflib 4.2 or 5.0

---
+++ graphics/p5-ming/files/patch-src_blocks_gifdbl.c
@@ -0,0 +1,26 @@
+--- ../src/blocks/gifdbl.c.orig	2010-10-10 07:19:23 UTC
++++ ../src/blocks/gifdbl.c
+@@ -227,7 +227,11 @@ SWFDBLBitmapData newSWFDBLBitmapData_fro
+ 	SWFDBLBitmapData ret;
+ 	struct dbl_data gifdata;
+ 
++#if GIFLIB_MAJOR >= 5
++	if((file = DGifOpenFileName(fileName, NULL)) == NULL)
++#else
+ 	if((file = DGifOpenFileName(fileName)) == NULL)
++#endif
+ 		return NULL;
+ 	if(!readGif(file, &gifdata))
+ 		return NULL;
+@@ -246,7 +250,11 @@ SWFDBLBitmapData newSWFDBLBitmapData_fro
+ 	SWFDBLBitmapData ret;
+ 	struct dbl_data gifdata;
+ 
++#if GIFLIB_MAJOR >= 5
++	if((file = DGifOpen(input, (InputFunc) gifReadFunc, NULL)) == NULL)
++#else
+ 	if((file = DGifOpen(input, (InputFunc) gifReadFunc)) == NULL)
++#endif
+ 		return NULL;
+ 	if(!readGif(file, &gifdata))
+ 		return NULL;
