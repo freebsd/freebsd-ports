da7c990dbb881827dbcb80a4666c090fa0eb86fd
koobs <koobs@FreeBSD.org>
2014-12-31 06:20:13 +0000

    multimedia/{lib}x264: Fix build on ARMv6
    - Fix build for missing symbols on ARMv6 [1]
    - Backport upstream 9463ec00 to fix default AS on ARM [2]
    - Remove unecessary WRKSRC override (x264)
    - Whitespace alignment
    
    [2] http://git.videolan.org/?p=x264.git;a=patch;h=9463ec
    
    PR:		196033 [1]
    Submitted by:	Mikael Urandar <mikael.urankar at gmail com> [1]
    Tested by:	sbruno

--- multimedia/x264/files/patch-common_cpu.c
+++ multimedia/x264/files/patch-common_cpu.c
@@ -1,6 +1,6 @@
---- common/cpu.c.orig	2010-06-24 22:45:07.000000000 +0200
-+++ common/cpu.c	2010-07-18 22:55:49.324572565 +0200
-@@ -234,7 +234,7 @@
+--- common/cpu.c.orig	2014-08-27 20:45:08 UTC
++++ common/cpu.c
+@@ -313,7 +313,7 @@ uint32_t x264_cpu_detect( void )
  
  #elif ARCH_PPC
  
@@ -9,7 +9,7 @@
  #include <sys/sysctl.h>
  uint32_t x264_cpu_detect( void )
  {
-@@ -242,12 +242,16 @@
+@@ -321,12 +321,16 @@ uint32_t x264_cpu_detect( void )
      uint32_t cpu = 0;
  #if SYS_OPENBSD
      int      selectors[2] = { CTL_MACHDEP, CPU_ALTIVEC };
