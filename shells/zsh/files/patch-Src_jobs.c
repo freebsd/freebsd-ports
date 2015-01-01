1d15d261eeed199fadd128f45c8669301de8ef6d
Baptiste Daroussin <bapt@FreeBSD.org>
2015-01-01 14:35:03 +0000

    Correct time report after r367805
    PR:		367805
    Submitted by:	peterj

--- shells/zsh/files/patch-Src_jobs.c
+++ shells/zsh/files/patch-Src_jobs.c
@@ -1,6 +1,6 @@
 --- Src/jobs.c.orig	2014-08-23 20:40:52.000000000 +0200
 +++ Src/jobs.c	2014-09-10 10:33:50.283018759 +0200
-@@ -694,15 +694,14 @@
+@@ -694,15 +694,15 @@
      /* go ahead and compute these, since almost every TIMEFMT will have them */
      elapsed_time = real->tv_sec + real->tv_usec / 1000000.0;
  
@@ -8,7 +8,7 @@
  #ifdef HAVE_GETRUSAGE
      user_time = ti->ru_utime.tv_sec + ti->ru_utime.tv_usec / 1000000.0;
      system_time = ti->ru_stime.tv_sec + ti->ru_stime.tv_usec / 1000000.0;
--    total_time = user_time + system_time;
+     total_time = user_time + system_time;
 -    percent = 100.0 * total_time
 -	/ (real->tv_sec + real->tv_usec / 1000000.0);
 +    percent = 100.0 * total_time / elapsed_time;
