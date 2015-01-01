3a80912c720c39bcb315890421b87e11c253bbb3
pawel <pawel@FreeBSD.org>
2015-01-01 19:56:44 +0000

    - Update to version 0.2.8, add LICENSE [1] - Fix build on DragonFly and when LOCALBASE != PREFIX [2] - Make sure to link with audio/portaudio2 [2]
    PR:		196054
    Submitted by:	maintainer [1], Jan Beich [2]

--- audio/py-pyaudio/files/patch-setup.py
+++ audio/py-pyaudio/files/patch-setup.py
@@ -1,29 +1,15 @@
 --- ./setup.py.orig	2012-11-07 00:17:06.624869638 +0100
 +++ ./setup.py	2012-11-07 00:37:17.129040078 +0100
-@@ -57,7 +57,7 @@
- 
- pyaudio_module_sources = ['src/_portaudiomodule.c']
- 
--include_dirs = []
-+include_dirs = ['%%PREFIX%%/include/portaudio2']
- external_libraries = []
- extra_compile_args = ['-fno-strict-aliasing']
- extra_link_args = []
-@@ -74,6 +74,9 @@
+@@ -74,6 +74,12 @@ else:
      external_libraries = ['portaudio']
      extra_link_args = []
  
-+if sys.platform.startswith('freebsd'):
-+    extra_link_args = ['-L%%PREFIX%%/lib/portaudio2']
++if sys.platform.startswith('dragonfly') or \
++   sys.platform.startswith('freebsd'):
++    include_dirs = ['%%LOCALBASE%%/include/portaudio2']
++    external_libraries = []
++    extra_link_args = ['%%LOCALBASE%%/lib/portaudio2/libportaudio.so']
 +
  if sys.platform == 'darwin':
      defines += [('MACOSX', '1')]
  
-@@ -83,7 +86,6 @@
- 
- if STATIC_LINKING:
- 
--    # platform specific configuration
-     if sys.platform == 'darwin':
-         extra_link_args += ['-framework', 'CoreAudio',
-                             '-framework', 'AudioToolbox',
