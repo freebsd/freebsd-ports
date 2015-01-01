44844afe6b7df30ebfcd547bd199b9d10a2ba910
sunpoet <sunpoet@FreeBSD.org>
2015-01-01 16:26:28 +0000

    - Update to 5.2.11 - While I'm here, add NO_ARCH and use @dir
    Changes:	http://www.sixapart.jp/movabletype/news/2014/12/24-1300.html
    PR:		ports/196406
    Submitted by:	Takefu <takefu@airport.fm> (maintainer)

--- russian/MT/files/patch-mt-static__js__editor.js
+++
@@ -1,8 +0,0 @@
---- mt-static/js/editor.js.orig	2013-12-02 02:40:32.000000000 +0900
-+++ mt-static/js/editor.js	2014-04-16 15:20:42.000000000 +0900
-@@ -1,4 +1,4 @@
--/* Movable Type (r) Open Source (C) 2001-2013 Six Apart, Ltd.
-+/* Movable Type (r) Open Source (C) 2001-2014 Six Apart, Ltd.
-  * This file is combined from multiple sources.  Consult the source files for their
-  * respective licenses and copyrights.
-  */;(function($){MT.EditorManager=function(){this.init.apply(this,arguments);};$.extend(MT.EditorManager,{editors:{},editorsForFormat:{},map:{},defaultWrapTag:'div',defaultWrapClass:'mt-editor-manager-wrap',register:function(id,editor){var thisConstructor=this;this.editors[id]=editor;$.each(editor.formatsForCurrentContext(),function(){if(!thisConstructor.editorsForFormat[this]){thisConstructor.editorsForFormat[this]=[];}
