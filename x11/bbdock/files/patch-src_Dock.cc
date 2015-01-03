01b60a3456714246b0e316611f09bbcfaf0d1430
marino <marino@FreeBSD.org>
2015-01-03 18:37:50 +0000

    x11/bbdock: #include <cstring>
    This is fallout from png upgrade.  Modern GCC needs <cstring> for several
    functions, although clang seems to be happy without it.

---
+++ x11/bbdock/files/patch-src_Dock.cc
@@ -0,0 +1,10 @@
+--- src/Dock.cc.orig	2011-03-29 19:48:36 UTC
++++ src/Dock.cc
+@@ -32,6 +32,7 @@
+ #include <stdio.h>
+ #include <unistd.h>
+ #include <iostream>
++#include <cstring>
+ #include <string>
+ 
+ #include "Dock.hh"
