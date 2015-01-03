ec209f1750c7226e6ca32e9aeb90155de011f2d9
marino <marino@FreeBSD.org>
2015-01-03 01:07:50 +0000

    graphics/libgfx: #include <cstring>
    This is fallout from png upgrade.  Modern GCC needs <cstring> for memcpy,
    although clang seems to be happy without it.

--- graphics/libgfx/files/patch-png.cxx
+++ graphics/libgfx/files/patch-png.cxx
@@ -1,6 +1,14 @@
---- src/raster-png.cxx.orig	2004-09-27 06:45:31.000000000 +0200
-+++ src/raster-png.cxx	2012-05-04 12:59:52.000000000 +0200
-@@ -42,7 +42,7 @@
+--- src/raster-png.cxx.orig	2004-09-27 04:45:31 UTC
++++ src/raster-png.cxx
+@@ -9,6 +9,7 @@
+ 
+  ************************************************************************/
+ 
++#include <cstring>
+ #include <vector>
+ #include <gfx/gfx.h>
+ #include <gfx/raster.h>
+@@ -42,7 +43,7 @@ ByteRaster *read_png_image(const char *f
     // Because we didn't set up any error handlers, we need to be
     // prepared to handle longjmps out of the library on error
     // conditions.
@@ -9,7 +17,7 @@
     {
        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
        fclose(fp);
-@@ -132,7 +132,7 @@
+@@ -132,7 +133,7 @@ bool write_png_image(const char *file_na
        return false;
     }
  
