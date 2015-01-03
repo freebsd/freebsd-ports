af30c6fc5aad1c0c96da6347df4da24d99f80e79
John Marino <marino@FreeBSD.org>
2015-01-03 02:05:41 +0000

    graphics/png2ico: #include <cstring>
    This is fallout from png upgrade.  Modern GCC needs <cstring> for memset,
    although clang seems to be happy without it.

--- graphics/png2ico/files/patch-png2ico.cpp
+++ graphics/png2ico/files/patch-png2ico.cpp
@@ -1,10 +1,11 @@
---- png2ico.cpp.orig
+--- png2ico.cpp.orig	2002-12-07 22:37:58 UTC
 +++ png2ico.cpp
-@@ -33,6 +33,7 @@
+@@ -33,6 +33,8 @@ Notes about transparent and inverted pix
  
  
  #include <cstdio>
++#include <cstring>
 +#include <cstdlib>
  #include <vector>
  #include <climits>
-
+ 
