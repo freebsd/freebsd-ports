cef8f1f082687667a93e11631512b3c06144b933
John Marino <marino@FreeBSD.org>
2015-01-04 15:05:06 +0000

    math/yacas: Unbreak DragonFly by modifying new patch to include it.

--- math/yacas/files/patch-src_yacasmain.cpp
+++ math/yacas/files/patch-src_yacasmain.cpp
@@ -1,11 +1,11 @@
---- src/yacasmain.cpp.orig	2014-12-11 21:11:48.000000000 +0000
-+++ src/yacasmain.cpp	2014-12-11 21:13:21.000000000 +0000
-@@ -1083,7 +1083,9 @@
+--- src/yacasmain.cpp.orig	2014-11-25 15:18:46 UTC
++++ src/yacasmain.cpp
+@@ -1083,7 +1083,9 @@ int main(int argc, char** argv)
      unsigned char first_stack_var = 0;
      the_first_stack_var = &first_stack_var;
  
 -#if defined (__APPLE__)
-+#if defined (__FreeBSD__)
++#if defined (__FreeBSD__) || defined (__DragonFly__)
 +	root_dir = "%%DATADIR%%/scripts";
 +#elif defined (__APPLE__)
      char buf[PATH_MAX];
