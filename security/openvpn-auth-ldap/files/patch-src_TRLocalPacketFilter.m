6f977b55ed0bed92525ded4d3801c26d8addbc2b
Matthias Andree <mandree@FreeBSD.org>
2015-01-05 23:03:40 +0000

    Update to upstream SVN revision 1379 and refresh patches.
    Needs real-world testing, so is still marked BROKEN.
    If there are positive test results from reporter or third parties,
    the BROKEN can be removed.
    
    PR:		190497

---
+++ security/openvpn-auth-ldap/files/patch-src_TRLocalPacketFilter.m
@@ -0,0 +1,10 @@
+--- src/TRLocalPacketFilter.m.orig	2012-03-03 19:17:45 UTC
++++ src/TRLocalPacketFilter.m
+@@ -43,6 +43,7 @@
+ #import <errno.h>
+ #import <assert.h>
+ 
++#import "TRLog.h"
+ 
+ /* Private Methods */
+ 
