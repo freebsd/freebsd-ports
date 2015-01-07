d5e50c78c6d7252afed3397f93a0c3cc549e7d00
shurd <shurd@FreeBSD.org>
2015-01-07 12:12:22 +0000

    Update to 14.08
    Switch to cmake
    Switch to github
    Add the STATIC, DOCS, ALSA, and OSS options
    Make a master port so comms/qtel can be a slave port

--- comms/svxlink/files/patch-async_audio_AsyncAudioDeviceOSS.cpp
+++ comms/svxlink/files/patch-async_audio_AsyncAudioDeviceOSS.cpp
@@ -1,5 +1,5 @@
---- async/audio/AsyncAudioDeviceOSS.cpp.orig	2014-04-09 22:38:17.000000000 -0700
-+++ async/audio/AsyncAudioDeviceOSS.cpp	2014-04-09 22:38:29.000000000 -0700
+--- src/async/audio/AsyncAudioDeviceOSS.cpp.orig	2014-04-09 22:38:17.000000000 -0700
++++ src/async/audio/AsyncAudioDeviceOSS.cpp	2014-04-09 22:38:29.000000000 -0700
 @@ -41,6 +41,7 @@
  #include <sys/soundcard.h>
  #include <sys/ioctl.h>
