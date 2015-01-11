803871c6ee294b41cfb7e50f0eae13cf496943ae
Olli Hauer <ohauer@FreeBSD.org>
2015-01-11 21:05:21 +0000

    - update to 2.12-20141228 - use new AUXLIBS_(CDB|LDAP|LMDB|MYSQL|PCRE|PGSQL) build instructions - use OPTIONS_SUB - install posttls-finger - build dynamic AUXLIBS (shared=yes and dynamicmaps=yes) - set META_DIRECTORY to DAEMONDIR (this may change in future to ETCDIR)
    Parts from HISTORY (heavily shortened)
    
    20141126
    	Cleanup: report nullmx DNS records as "domain does not
    	accept mail", instead of "invalid DNS response".  The Postfix
    	SMTP client already bounced mail for such domains, and the
    	Postfix SMTP server already rejected such domains with
    	reject_unknown_sender/recipient_domain. This introduces a
    	new SMTP server configuration parameter nullmx_reject_code
    	(default: 556).
    
    20141127
    	Feature: DNS reply filter, configured with smtp_dns_reply_filter,
    
    20141130
    	Cleanup: when searching multiple DNS record types for a
    	specific name, and not all queries return the same result
    	status, do not blindly return the last query's rcode and
    	diagnostic text.  Instead, return rcode and text that is
    	consistent with the aggregate result status.
    
    	Documentation: added note on Milter-signing bounces.
    
    20141202
    	Cleanup: to increase clarity. rename DNS result status from
    	DNS_UNAVAIL to DNS_NULLMX. If someone uses the same zero-length
    	name trick with some other resource type, then we will worry
    	about that later.
    
    20141203
    	Feature: support to match UTF8 domain names against ASCII
    	names in TLS certificates.
    
    20141212
    	Cleanup: nullmx SMTP reply codes 550 and 556, and enhanced
    	status codes X.1.10 and X.7.27. The nullmx SMTP reply codes
    	are no longer configurable.
    
    20141224
    	Cleanup: the compile-time argument typechecks for attribute-value
    	APIs are now by default implemented with inline functions.
    	Compile with -DNO_INLINE to implement the argument typechecks
    	with ternary operators and unreachable assignments.
    
    20141227
    	Feature: smtp_address_verify_target (default: rcpt) that
    	determines what protocol stage decides if a recipient is
    	valid. Specify "data" for servers that reject recipients
    	after the DATA command.
    
    20141228
    	Cleanup: the IDNA conversion routines now accept both
    	ASCII and UTF8 inputs. The functions als verify that
    	either their result is a valid ASCII domain name or that
    	it converts into a valid ASCII domain name.
    
    Approved by:	sahil (implicit)

--- mail/postfix-current/files/patch-src__util__sys_defs.h
+++ mail/postfix-current/files/patch-src__util__sys_defs.h
@@ -1,11 +1,9 @@
---- src/util/sys_defs.h.orig	2013-10-27 02:03:59.000000000 +0000
-+++ src/util/sys_defs.h	2013-10-27 02:04:50.000000000 +0000
-@@ -25,7 +25,8 @@
-   */
+--- src/util/sys_defs.h.orig	2014-12-25 22:33:02 UTC
++++ src/util/sys_defs.h
+@@ -26,6 +26,7 @@
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
--    || defined(FREEBSD8) || defined(FREEBSD9) \
-+    || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
+     || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
 +    || defined(FREEBSD11) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
