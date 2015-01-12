64bc476bb52bf1490f1d434d0ac403db0f8fa34e
Dmitry Marakasov <amdmi3@FreeBSD.org>
2015-01-12 17:25:03 +0000

    - Remove diskmode fix for FreeBSD - it didn't work anyway and HEAD doesn't even have the ioctl used

--- editors/chexedit/files/patch-src-init.c
+++ editors/chexedit/files/patch-src-init.c
@@ -1,18 +1,6 @@
---- src/init.c.orig	Sun Aug  8 09:56:11 1999
-+++ src/init.c	Sat Mar 17 22:23:57 2001
-@@ -33,6 +33,11 @@
- #include <sys/disklabel.h>
- #endif
- 
-+#if defined(__FreeBSD__)
-+#include <fcntl.h>
-+#include <sys/disklabel.h>
-+#endif
-+
- extern char **environ;
- 
- 
-@@ -250,6 +255,7 @@
+--- src/init.c.orig	1999-08-08 05:56:11.000000000 +0400
++++ src/init.c	2015-01-12 20:23:12.000000000 +0300
+@@ -250,6 +250,7 @@
     if (color_term != -1)
     {
        start_color ();
@@ -20,12 +8,3 @@
        if ((color_term = has_colors ()))
        {
           color_term = 1;
-@@ -463,7 +469,7 @@
-       filestat.st_size = DEFAULT_BUFFER_SIZE;
-    }
- 
--#elif defined (__OpenBSD__)
-+#elif defined (__OpenBSD__) || defined(__FreeBSD__)
-    else if (Globals.fixed_disk)
-    {
- 
