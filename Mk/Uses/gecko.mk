151389816dbb01d354af703feab30788d1e11fe9
Beat Gaetzi <beat@FreeBSD.org>
2015-01-15 08:51:16 +0000

    - Update to 2.32
    Submitted by:	Jan Beich
    MFH:		2015Q1
    Security:	http://www.vuxml.org/freebsd/bd62c640-9bb9-11e4-a5ad-000c297fb80f.html

--- Mk/Uses/gecko.mk
+++ Mk/Uses/gecko.mk
@@ -51,8 +51,8 @@ _GECKO_INSTALLED_VER:=	${_GECKO_INSTALLED_VER:M[0-9][0-9]*:C/([0-9][0-9]).*/\1/g
 
 .elif ${gecko_ARGS:Mseamonkey}
 
-_GECKO_DEFAULT_VERSION=	31
-_GECKO_VERSIONS=		31
+_GECKO_DEFAULT_VERSION=	32
+_GECKO_VERSIONS=		32
 _GECKO_TYPE=	seamonkey
 
 .if exists(${LOCALBASE}/bin/seamonkey)
@@ -61,7 +61,7 @@ _GECKO_INSTALLED_VER:=	${_GECKO_INSTALLED_VER:M[0-9]*:C/[0-9].([0-9][0-9]).*/\1/
 .endif
 
 # Dependence lines for different Seamonkey versions
-31_DEPENDS=		${LOCALBASE}/lib/seamonkey/seamonkey:${PORTSDIR}/www/seamonkey
+32_DEPENDS=		${LOCALBASE}/lib/seamonkey/seamonkey:${PORTSDIR}/www/seamonkey
 
 .elif ${gecko_ARGS:Mthunderbird}
 
