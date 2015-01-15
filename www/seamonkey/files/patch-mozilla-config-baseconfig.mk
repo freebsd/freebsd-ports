69d6a101b83df7d1102b74ea9c20529273b55da9
Beat Gaetzi <beat@FreeBSD.org>
2015-01-15 11:34:54 +0000

    MFH: r377062
    - Update to 2.32
    
    Submitted by:	Jan Beich
    Security:	http://www.vuxml.org/freebsd/bd62c640-9bb9-11e4-a5ad-000c297fb80f.html
    Approved by:	portmgr (erwin)

--- www/seamonkey/files/patch-mozilla-config-baseconfig.mk
+++ www/seamonkey/files/patch-mozilla-config-baseconfig.mk
@@ -1,5 +1,5 @@
---- config/baseconfig.mk~
-+++ config/baseconfig.mk
+--- mozilla/config/baseconfig.mk~
++++ mozilla/config/baseconfig.mk
 @@ -1,7 +1,7 @@
 -includedir := $(includedir)/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
 -idldir = $(datadir)/idl/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
@@ -9,6 +9,6 @@
 +idldir = $(datadir)/idl/%%MOZILLA%%
 +installdir = $(libdir)/%%MOZILLA%%
 +sdkdir = $(libdir)/%%MOZILLA%%
- MOZILLA_SRCDIR = $(topsrcdir)/mozilla
- MOZDEPTH = $(DEPTH)/mozilla
- DIST = $(MOZDEPTH)/dist
+ ifndef TOP_DIST
+ TOP_DIST = dist
+ endif
