94a546830ced7d57db22fd2e356698c990be8175
Michael Reifenberger <mr@FreeBSD.org>
2015-01-16 10:21:47 +0000

    Update to bzr rev 5363 Delete Patches which shouldn't be needed when using boost from ports.

--- cad/kicad-devel/files/patch-include-kiway.h
+++ cad/kicad-devel/files/patch-include-kiway.h
@@ -1,9 +1,9 @@
---- include/kiway.h.orig	2014-05-23 15:44:36.884457126 +0200
-+++ include/kiway.h	2014-05-23 15:45:05.138279780 +0200
+--- include/kiway.h.orig	2015-01-10 18:11:39.000000000 +0000
++++ include/kiway.h	2015-01-11 10:51:52.458174797 +0000
 @@ -115,5 +115,5 @@
  #define KIFACE_INSTANCE_NAME_AND_VERSION   "KIFACE_1"
  
--#if defined(__linux__)
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined(__linux__) || defined(__FreeBSD__)
++#if defined(__linux__) || defined(__FreeBSD__) || defined(__DragonFly__)
   #define LIB_ENV_VAR    wxT( "LD_LIBRARY_PATH" )
  #elif defined(__WXMAC__)
