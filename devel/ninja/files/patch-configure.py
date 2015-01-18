743a2d9f8d26f0bf7aecb4931e8f02cb732f47b6
Grzegorz Blach <gblach@FreeBSD.org>
2015-01-18 20:35:19 +0000

    Fix build with Python 3
    PR:		196402
    Submitted by:	Kevin Zheng (kevinz5000 <at> gmail <dot> com)

--- devel/ninja/files/patch-configure.py
+++ devel/ninja/files/patch-configure.py
@@ -1,6 +1,15 @@
---- configure.py.orig	2012-06-26 19:09:41.000000000 +0200
-+++ configure.py	2012-06-26 19:09:54.000000000 +0200
-@@ -117,7 +117,7 @@
+--- configure.py.orig	2014-11-24 18:37:47.000000000 +0100
++++ configure.py	2015-01-18 21:26:42.000000000 +0100
+@@ -164,7 +164,7 @@
+         """Run a subcommand, quietly.  Prints the full command on error."""
+         try:
+             subprocess.check_call(cmdline, shell=True)
+-        except subprocess.CalledProcessError, e:
++        except subprocess.CalledProcessError:
+             print('when running: ', cmdline)
+             raise
+ 
+@@ -297,7 +297,7 @@
          cflags += ['/Ox', '/DNDEBUG', '/GL']
          ldflags += ['/LTCG', '/OPT:REF', '/OPT:ICF']
  else:
