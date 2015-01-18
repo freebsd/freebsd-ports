6118b957b41370485c6b2d317d05de94e0184270
Rene Ladan <rene@FreeBSD.org>
2015-01-18 22:20:24 +0000

    MFH: r376780
    www/chromium:
    - install libpdf.so to enable the built-in PDF viewer [1]
    - show memory usage of browser, GPU, individual plugins, and individual tabs
      in chrome://memory-redirect [2]
    - bump PORTREVISION
    
    Submitted by:	Imre Vadász via GitHub [1,2]
    Approved by:	portmgr (unanswered, but needed for next MFH to align)

--- www/chromium/files/patch-base__process__process_iterator_freebsd.cc
+++ www/chromium/files/patch-base__process__process_iterator_freebsd.cc
@@ -11,3 +11,26 @@
  #include "base/logging.h"
  #include "base/strings/string_util.h"
  
+@@ -68,19 +72,13 @@
+   for (; index_of_kinfo_proc_ < kinfo_procs_.size(); ++index_of_kinfo_proc_) {
+     size_t length;
+     struct kinfo_proc kinfo = kinfo_procs_[index_of_kinfo_proc_];
+-    int mib[] = { CTL_KERN, KERN_PROC_ARGS, kinfo.ki_pid };
++    int mib[] = { CTL_KERN, KERN_PROC, KERN_PROC_ARGS, kinfo.ki_pid };
+ 
+     if ((kinfo.ki_pid > 0) && (kinfo.ki_stat == SZOMB))
+       continue;
+ 
+-    length = 0;
+-    if (sysctl(mib, arraysize(mib), NULL, &length, NULL, 0) < 0) {
+-      LOG(ERROR) << "failed to figure out the buffer size for a command line";
+-      continue;
+-    }
+-
+-    data.resize(length);
+-
++    data.resize(ARG_MAX);
++    length = ARG_MAX;
+     if (sysctl(mib, arraysize(mib), &data[0], &length, NULL, 0) < 0) {
+       LOG(ERROR) << "failed to fetch a commandline";
+       continue;
