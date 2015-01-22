469e0c88d847dbd358d37675689d04e26b36ee63
Vsevolod Stakhov <vsevolod@FreeBSD.org>
2015-01-22 16:48:37 +0000

    - Update to 2.1.3
    PR:		197005
    Submitted by:	Bernard Spil <spil.oss at gmail.com>

--- security/libressl/files/patch-include-openssl-opensslv.h
+++ security/libressl/files/patch-include-openssl-opensslv.h
@@ -1,11 +1,11 @@
---- include/openssl/opensslv.h.orig	2014-10-17 10:44:31.485191132 +0100
-+++ include/openssl/opensslv.h	2014-10-17 10:45:36.353625132 +0100
+--- include/openssl/opensslv.h.orig	2014-12-14 23:08:55 UTC
++++ include/openssl/opensslv.h
 @@ -3,7 +3,7 @@
  #define HEADER_OPENSSLV_H
  
  #define LIBRESSL_VERSION_NUMBER	0x20000000L
 -#define OPENSSL_VERSION_NUMBER	0x20000000L
 +#define OPENSSL_VERSION_NUMBER	0x1000107fL
- #define OPENSSL_VERSION_TEXT	"LibreSSL 2.1"
+ #define OPENSSL_VERSION_TEXT	"LibreSSL 2.1.3"
  #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
  
