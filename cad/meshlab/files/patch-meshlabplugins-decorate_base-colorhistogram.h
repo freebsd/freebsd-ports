c95f2d2f741b7149e2d72a7a629bb62372db2dc4
Thierry Thomas <thierry@FreeBSD.org>
2015-01-25 21:37:31 +0000

    Upgrade to 1.3.3 and define LICENSE.

--- cad/meshlab/files/patch-meshlabplugins-decorate_base-colorhistogram.h
+++ cad/meshlab/files/patch-meshlabplugins-decorate_base-colorhistogram.h
@@ -1,12 +1,12 @@
---- ../../meshlab/src/meshlabplugins/decorate_base/colorhistogram.h.orig	2013-11-16 22:04:41.000000000 +0100
-+++ ../../meshlab/src/meshlabplugins/decorate_base/colorhistogram.h	2013-11-16 22:04:56.000000000 +0100
+--- meshlabplugins/decorate_base/colorhistogram.h.orig	2013-07-17 17:20:11.000000000 +0200
++++ meshlabplugins/decorate_base/colorhistogram.h	2015-01-24 18:01:12.000000000 +0100
 @@ -39,10 +39,11 @@
    //ScalarType BinWidth(ScalarType v);
  
    //! Reset histogram data.
 +  /* dead code
    void Clear() {
-     this->::Clear();
+     Histogram<ScalarType>::Clear();
      CV.clear();
 -  }
 +  } */
