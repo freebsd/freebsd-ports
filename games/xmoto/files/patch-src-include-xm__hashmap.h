82d109bcf56d84a8b281f0149da28286f8b039d9
Dmitry Marakasov <amdmi3@FreeBSD.org>
2015-01-25 01:41:04 +0000

    - Fix build with new libc++
    PR:		196652
    Submitted by:	dim

--- games/xmoto/files/patch-src-include-xm__hashmap.h
+++ games/xmoto/files/patch-src-include-xm__hashmap.h
@@ -1,11 +1,28 @@
---- src/include/xm_hashmap.h.orig	2011-10-12 00:18:17.000000000 +0400
-+++ src/include/xm_hashmap.h	2013-09-13 22:30:46.334969348 +0400
-@@ -14,7 +14,7 @@
+--- src/include/xm_hashmap.h.orig	2015-01-12 23:04:54.000000000 +0100
++++ src/include/xm_hashmap.h	2015-01-12 23:07:53.000000000 +0100
+@@ -13,13 +13,18 @@
+ #include <hash_map>
  namespace HashNamespace=std;
  #endif
- struct hashcmp_str {
+-struct hashcmp_str {
 -  bool operator()(const char* s1, const char* s2) {
-+  bool operator()(const char* s1, const char* s2) const {
-     if(s1 == NULL || s2 == NULL) {
-       return false;
+-    if(s1 == NULL || s2 == NULL) {
+-      return false;
++
++#ifdef _LIBCPP_VERSION
++namespace __gnu_cxx {
++  template<> struct hash<std::string>
++    : public unary_function<std::string, size_t>
++  {
++    size_t operator()(const std::string& s) const
++    {
++      return hash<const char*>()(s.c_str());
      }
+-    return strcmp(s1, s2) == 0;
+-  }
+-};
++  };
++}
++#endif // _LIBCPP_VERSION
+ 
+ #endif
