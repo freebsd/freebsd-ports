e8f2d860c05741a46263e3bae6ee343610bf57b2
Mikhail Teterin <mi@FreeBSD.org>
2015-01-27 08:40:34 +0000

    Upgrade from 5f1 to 5f2.
    The bad news:
    
    	1. gcc is still required -- clang can build the binaries, but
    	   some self-tests will fail, unless all optimization was
               disabled. This is, probably, due to some suspect code in
               bytenumb.c -- would be good to investigate.
    	2. Building this port in parallel is still not possible -- the
    	   Makefile is too convoluted and same sources are recompiled
    	   multiple times with different #defines set.
    
    The good news:
    
    	1. Resolve a large number of warnings.
    	2. Fix build on ia64 and sparc64 (tested on pluto and flame
               respectively). Other platforms (alpha, powerpc?) have a
    	   better chance of working now...

--- lang/scm/files/patch-scmfig.h
+++ lang/scm/files/patch-scmfig.h
@@ -1,4 +1,4 @@
---- scmfig.h.orig
+--- scmfig.h
 +++ scmfig.h
 @@ -256,11 +256,7 @@
  #ifdef __alpha
@@ -9,24 +9,30 @@
 -# define CDR_DOUBLES
 -#endif
 -#ifdef __x86_64
-+#if defined(__amd64__) || defined(__ia64__) || defined(__powerpc64__) || defined(__x86_64__)
++#if defined(__amd64__) || defined(__ia64__) || defined(__powerpc64__) || defined(__x86_64__) || defined(__sparc64__)
  # define SHORT_INT
  # define CDR_DOUBLES
  #endif
-@@ -393,14 +389,10 @@
+@@ -393,14 +389,6 @@
  # define WHITE_SPACES  ' ':case '\t':case '\r':case '\f'
  #endif
  
 -#ifdef __ia64__
-+# if defined(__amd64__) || defined(__ia64__) || defined(__powerpc64__) || defined(__x86_64__)
- # define PTR2INT(x) ((long)(x))
- #else
+-# define PTR2INT(x) ((long)(x))
++#define PTR2INT(x) ((intptr_t)(x))
+-#else
 -# ifdef __x86_64
 -#  define PTR2INT(x) ((long)(x))
 -# else
 -#  define PTR2INT(x) ((int)(x))
 -# endif
-+# define PTR2INT(x) ((int)(x))
- #endif
+-#endif
  
  #ifndef __builtin_expect
+@@ -824,5 +816,5 @@
+ # ifdef ARM_ULIB
+     extern volatile int errno;
+-# else
++# elif defined(PLAN9)
+     extern int errno;
+ # endif
