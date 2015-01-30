10654365dc34b7dfb75bdcfbd1cb174c90d8d578
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2015-01-30 19:48:59 +0000

    multimedia/photofilmstrip: Update version 1.5.0=>2.1.0
    - Application behavior changed more like working as Python Module
    - Removed pkg-plist in lieu of autoplist
    - databases/py-sqlite3 is now a BUILD_DEPENDS as it fails to build without
      this
    - Removed PORTSCOUT as this version is passed the mentioned versions to
      skip
    
    Differential Revision:	https://reviews.freebsd.org/D1732
    Approved by:		marino(mentor)

--- multimedia/photofilmstrip/files/patch-photofilmstrip_gui_HelpViewer.py
+++ multimedia/photofilmstrip/files/patch-photofilmstrip_gui_HelpViewer.py
@@ -1,10 +1,10 @@
---- ./src/gui/HelpViewer.py.orig	2010-12-20 01:52:21.000000000 -0800
-+++ ./src/gui/HelpViewer.py	2011-02-05 12:17:23.000000000 -0800
+--- photofilmstrip/gui/HelpViewer.py.orig	2015-01-25 19:27:30.213707995 +0600
++++ photofilmstrip/gui/HelpViewer.py	2015-01-25 19:28:35.363702863 +0600
 @@ -36,7 +36,7 @@
      def __init__(self):
          self.__htmlCtrl = wx.html.HtmlHelpController()
          docFile = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), 
--                               "..", "doc", "photofilmstrip", "photofilmstrip.hhp")
+-                               "..", "share", "doc", "photofilmstrip", "photofilmstrip.hhp")
 +                               "..", "photofilmstrip", "doc", "photofilmstrip.hhp")
          fn = os.path.abspath(docFile)
          self.__htmlCtrl.AddBook(fn)
