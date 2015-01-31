942b4ad45cb9752d588617033ba29538e81d87b7
bsam <bsam@FreeBSD.org>
2015-01-31 15:09:42 +0000

    www/web2ldap: upgrade to version 1.2.19.

--- www/web2ldap/files/patch-fcgi__web2ldap.py
+++ www/web2ldap/files/patch-fcgi__web2ldap.py
@@ -2,12 +2,12 @@
 --- fcgi/web2ldap.py	Wed Mar 20 15:57:25 2002
 ***************
 *** 1,4 ****
-! #!/usr/bin/python -OO
+! #!/usr/bin/python
   
   """
   fcgi/web2ldap.py - stub script for running as FastCGI server
 --- 1,4 ----
-! #!/usr/local/bin/python -OO
+! #!/usr/local/bin/python
   
   """
   fcgi/web2ldap.py - stub script for running as FastCGI server
