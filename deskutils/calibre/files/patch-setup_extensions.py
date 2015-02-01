425d2781744de349dff968ec6ca374617c0e7a36
Guido Falsi <madpilot@FreeBSD.org>
2015-02-01 15:46:04 +0000

    - Enable the qt_hack component. It requires private qt headers, so   add a depend on extracting those from the qt4-gui port [1] - While here, strip binaries
    PR:		197218 [1]
    Submitted by:	yuri@rawbw.com

--- deskutils/calibre/files/patch-setup_extensions.py
+++ deskutils/calibre/files/patch-setup_extensions.py
@@ -1,19 +1,5 @@
 --- setup/extensions.py.orig	2013-11-22 10:29:37.000000000 -0600
 +++ setup/extensions.py	2013-11-22 10:30:39.000000000 -0600
-@@ -187,13 +187,6 @@
-                 sip_files=['calibre/gui2/progress_indicator/QProgressIndicator.sip']
-                 ),
- 
--    Extension('qt_hack',
--                ['calibre/ebooks/pdf/render/qt_hack.cpp'],
--                inc_dirs=qt_private_inc + ['calibre/ebooks/pdf/render', 'qt-harfbuzz/src'],
--                headers=['calibre/ebooks/pdf/render/qt_hack.h'],
--                sip_files=['calibre/ebooks/pdf/render/qt_hack.sip']
--                ),
--
-     Extension('unrar',
-               ['unrar/%s.cpp'%(x.partition('.')[0]) for x in '''
-                rar.o strlist.o strfn.o pathfn.o savepos.o smallfn.o global.o file.o
 @@ -496,8 +489,9 @@
  
          pro = textwrap.dedent('''
