bfb91f944244400b72c23eaca845e51def8f31c5
John Marino <marino@FreeBSD.org>
2015-02-04 12:25:36 +0000

    security/vpnc: Update sysdep.h patch to support dragonfly
    While here, remove the __FreeBSD_version check as this will always
    pass on supported releases of FreeBSD.
    
    Approved by:	blanket (non-invasive DragonFly support)

--- security/vpnc/files/patch-sysdep.h
+++ security/vpnc/files/patch-sysdep.h
@@ -1,13 +1,18 @@
---- sysdep.h.orig	2009-03-25 03:28:26.000000000 -0400
-+++ sysdep.h	2009-03-25 03:29:18.000000000 -0400
-@@ -86,6 +86,10 @@
+--- sysdep.h.orig	2008-11-19 20:36:12 UTC
++++ sysdep.h
+@@ -86,6 +86,7 @@ int tun_get_hwaddr(int fd, char *dev, ui
  #define HAVE_FGETLN    1
  #define HAVE_UNSETENV  1
  #define HAVE_SETENV    1
-+#include <sys/param.h>
-+#if __FreeBSD_version >= 800067
-+#define	HAVE_GETLINE   1
-+#endif
++#define HAVE_GETLINE   1
+ #endif
+ 
+ /***************************************************************************/
+@@ -97,6 +98,7 @@ int tun_get_hwaddr(int fd, char *dev, ui
+ #define HAVE_FGETLN    1
+ #define HAVE_UNSETENV  1
+ #define HAVE_SETENV    1
++#define HAVE_GETLINE   1
  #endif
  
  /***************************************************************************/
