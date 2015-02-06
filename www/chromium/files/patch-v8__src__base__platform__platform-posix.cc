6a3c920ee578f122f1ebc31de9b7d2cffd132701
Rene Ladan <rene@FreeBSD.org>
2015-02-06 22:35:54 +0000

    MFH: r378563
    www/chromium: update to 40.0.2214.111 [1]
    Apply a build fix for DragonFlyBSD [2] and FreeBSD 8 [3]
    
    Submitted by:	Imre Vadasz [1,3]
    		marino [2]
    Security:	http://www.vuxml.org/freebsd/a6eb239f-adbe-11e4-9fce-080027593b9a.html
    Approved by:	ports-secteam (delphij)

--- www/chromium/files/patch-v8__src__base__platform__platform-posix.cc
+++ www/chromium/files/patch-v8__src__base__platform__platform-posix.cc
@@ -1,11 +1,17 @@
---- v8/src/base/platform/platform-posix.cc.orig	2015-01-25 14:30:34 UTC
-+++ v8/src/base/platform/platform-posix.cc
-@@ -259,6 +259,8 @@
+--- v8/src/base/platform/platform-posix.cc.orig	2015-01-27 03:22:59.000000000 +0100
++++ v8/src/base/platform/platform-posix.cc	2015-02-06 18:41:53.881294389 +0100
+@@ -259,6 +259,14 @@
    return static_cast<int>(syscall(__NR_gettid));
  #elif V8_OS_ANDROID
    return static_cast<int>(gettid());
 +#elif V8_OS_FREEBSD
++#  ifdef __DragonFly__
++  return static_cast<int>(lwp_gettid());
++#  elif __FreeBSD_version__ < 900031
++  return 0; /* ! */
++#  else
 +  return static_cast<int>(pthread_getthreadid_np());
++#  endif
  #else
    return static_cast<int>(pthread_self());
  #endif
