b4ea8bc6867f104a19b5a9dace56d3388f012ccc
Guido Falsi <madpilot@FreeBSD.org>
2015-02-07 00:09:35 +0000

    - Update net/asterisk11 to 11.16.0 - Update net/asterisk13 to 13.2.0

--- net/asterisk11/files/patch-main__lock.c
+++ net/asterisk11/files/patch-main__lock.c
@@ -1,13 +1,12 @@
---- ./main/lock.c.orig	2010-11-06 15:00:15.000000000 +0100
-+++ ./main/lock.c	2010-11-06 15:00:23.000000000 +0100
-@@ -679,10 +679,6 @@
+--- main/lock.c.orig	2014-12-12 23:31:38 UTC
++++ main/lock.c
+@@ -701,9 +701,6 @@ int __ast_rwlock_init(int tracking, cons
+ #endif /* DEBUG_THREADS */
  
  	pthread_rwlockattr_init(&attr);
- 
 -#ifdef HAVE_PTHREAD_RWLOCK_PREFER_WRITER_NP
 -	pthread_rwlockattr_setkind_np(&attr, PTHREAD_RWLOCK_PREFER_WRITER_NP);
 -#endif
--
  	res = pthread_rwlock_init(&t->lock, &attr);
  	pthread_rwlockattr_destroy(&attr);
- 	return res;
+ 
