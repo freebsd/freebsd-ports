444283b2632a1c92f6ddccd34148f7893debf845
Ashish SHUKLA <ashish@FreeBSD.org>
2015-02-08 22:32:50 +0000

    - Update to 4.3

---
+++ sysutils/squashfs-tools/files/patch-action.c
@@ -0,0 +1,19 @@
+Hack for lack of strdupa() in BSD libc
+
+$FreeBSD$
+
+--- action.c.orig
++++ action.c
+@@ -44,6 +44,12 @@
+ #include "action.h"
+ #include "error.h"
+ 
++#ifndef strdupa
++#include <string.h>  // required for str*()
++#include <stdlib.h>  // required for alloca
++#define strdupa(foo) (strncpy( alloca( strlen(foo) + 1 ), foo, strlen( foo ) + 1 ))
++#endif
++
+ /*
+  * code to parse actions
+  */
