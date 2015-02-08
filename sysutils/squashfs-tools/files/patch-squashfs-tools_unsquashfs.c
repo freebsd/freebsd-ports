97b40102727a5729b68cf6a1d33a1a0998bd5805
ashish <ashish@FreeBSD.org>
2015-02-08 22:32:50 +0000

    - Update to 4.3

--- sysutils/squashfs-tools/files/patch-squashfs-tools_unsquashfs.c
+++ sysutils/squashfs-tools/files/patch-squashfs-tools_unsquashfs.c
@@ -1,21 +1,19 @@
 $FreeBSD$
 
---- unsquashfs.c.orig	2010-09-20 03:14:38.000000000 +0530
-+++ unsquashfs.c	2010-09-29 09:34:43.000000000 +0530
-@@ -29,9 +29,16 @@
- #include "compressor.h"
- #include "xattr.h"
+--- unsquashfs.c.orig
++++ unsquashfs.c
+@@ -31,7 +31,14 @@
+ #include "unsquashfs_info.h"
+ #include "stdarg.h"
  
 +#ifdef __linux__
  #include <sys/sysinfo.h>
 +#endif
 +
- #include <sys/types.h>
- 
 +#ifdef __FreeBSD__
 +#include <sys/sysctl.h>
 +#endif
 +
- struct cache *fragment_cache, *data_cache;
- struct queue *to_reader, *to_deflate, *to_writer, *from_writer;
- pthread_t *thread, *deflator_thread;
+ #include <sys/types.h>
+ #include <sys/time.h>
+ #include <sys/resource.h>
