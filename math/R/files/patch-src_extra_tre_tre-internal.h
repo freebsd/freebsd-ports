24d46ae7725c9af9dc36bd20c9b815ab945e5b72
Sean Bruno <sbruno@FreeBSD.org>
2015-02-10 22:24:01 +0000

    Repair overflow condition where WCHAR_MAX != INT_MAX https://github.com/andreas-schwab/tre/commit/182e1060dc3bfc175490f6e0e961d9330971299c
    Add MISSING option to the default build on armv6.
    
    PR:		194183
    Submitted by:	mikael.urankar@gmail.com, Adreas Schwab
    Approved by:	maintainer timeout (3 months without PR comment)

---
+++ math/R/files/patch-src_extra_tre_tre-internal.h
@@ -0,0 +1,22 @@
+--- src/extra/tre/tre-internal.h.orig	2015-01-20 15:25:01 UTC
++++ src/extra/tre/tre-internal.h
+@@ -17,6 +17,7 @@
+ #include <wctype.h>
+ #endif /* !HAVE_WCTYPE_H */
+ 
++#include <limits.h>
+ #include <ctype.h>
+ #include "tre.h"
+ 
+@@ -48,7 +49,11 @@
+ 
+ /* Wide characters. */
+ typedef wint_t tre_cint_t;
++#if WCHAR_MAX <= INT_MAX
+ #define TRE_CHAR_MAX WCHAR_MAX
++#else /* WCHAR_MAX > INT_MAX */
++#define TRE_CHAR_MAX INT_MAX
++#endif
+ 
+ #ifdef TRE_MULTIBYTE
+ #define TRE_MB_CUR_MAX MB_CUR_MAX
