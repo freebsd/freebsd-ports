38a2d4ae3461e79498d1dc2d8acc3d904a338947
Vanilla I. Shu <vanilla@FreeBSD.org>
2015-02-11 05:18:58 +0000

    Upgrade to 0.12.0
    PR:		197507
    Submitted by:	maintainer

--- www/node/files/patch-deps_v8_src_platform-freebsd.cc
+++ www/node/files/patch-deps_v8_src_platform-freebsd.cc
@@ -1,6 +1,46 @@
---- deps/v8/src/platform-freebsd.cc.orig	2014-09-16 18:47:52.000000000 -0400
-+++ deps/v8/src/platform-freebsd.cc	2014-10-08 13:23:25.000000000 -0400
-@@ -343,7 +343,7 @@
+--- ./deps/v8/src/base/platform/platform-freebsd.cc.orig	2015-02-07 04:04:23.000000000 +0800
++++ ./deps/v8/src/base/platform/platform-freebsd.cc	2015-02-10 10:53:45.000000000 +0800
+@@ -131,23 +131,23 @@
+     addr_buffer[0] = '0';
+     addr_buffer[1] = 'x';
+     addr_buffer[10] = 0;
+-    int result = read(fd, addr_buffer + 2, 8);
+-    if (result < 8) break;
++    ssize_t bytes_read = read(fd, addr_buffer + 2, 8);
++    if (bytes_read < 8) break;
+     unsigned start = StringToLong(addr_buffer);
+-    result = read(fd, addr_buffer + 2, 1);
+-    if (result < 1) break;
++    bytes_read = read(fd, addr_buffer + 2, 1);
++    if (bytes_read < 1) break;
+     if (addr_buffer[2] != '-') break;
+-    result = read(fd, addr_buffer + 2, 8);
+-    if (result < 8) break;
++    bytes_read = read(fd, addr_buffer + 2, 8);
++    if (bytes_read < 8) break;
+     unsigned end = StringToLong(addr_buffer);
+     char buffer[MAP_LENGTH];
+-    int bytes_read = -1;
++    bytes_read = -1;
+     do {
+       bytes_read++;
+       if (bytes_read >= MAP_LENGTH - 1)
+         break;
+-      result = read(fd, buffer + bytes_read, 1);
+-      if (result < 1) break;
++      bytes_read = read(fd, buffer + bytes_read, 1);
++      if (bytes_read < 1) break;
+     } while (buffer[bytes_read] != '\n');
+     buffer[bytes_read] = 0;
+     // Ignore mappings that are not executable.
+@@ -182,13 +182,13 @@
+ 
+ VirtualMemory::VirtualMemory(size_t size, size_t alignment)
+     : address_(NULL), size_(0) {
+-  DCHECK(IsAligned(alignment, static_cast<intptr_t>(OS::AllocateAlignment())));
++  DCHECK((alignment % OS::AllocateAlignment()) == 0);
+   size_t request_size = RoundUp(size + alignment,
+                                 static_cast<intptr_t>(OS::AllocateAlignment()));
    void* reservation = mmap(OS::GetRandomMmapAddr(),
                             request_size,
                             PROT_NONE,
@@ -9,7 +49,7 @@
                             kMmapFd,
                             kMmapFdOffset);
    if (reservation == MAP_FAILED) return;
-@@ -415,7 +415,7 @@
+@@ -260,7 +260,7 @@
    void* result = mmap(OS::GetRandomMmapAddr(),
                        size,
                        PROT_NONE,
@@ -18,7 +58,7 @@
                        kMmapFd,
                        kMmapFdOffset);
  
-@@ -445,7 +445,7 @@
+@@ -288,7 +288,7 @@
    return mmap(base,
                size,
                PROT_NONE,
