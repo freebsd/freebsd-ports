a79e3d0f2f8ae96d7df6ddb0af10f11532ed730e
koobs <koobs@FreeBSD.org>
2015-02-11 13:34:46 +0000

    net/py-oauth2: Clean up, modernize & add QA bits
    - Take maintainership under python@'s wings
    - httplib2 is a RUN not BUILD dependency
    - Add TEST_DEPENDS and regression-test target
    - Limit to Python 2.x
    - Sort USE_PYTHON values
    - Add breadcrumb/notes to setup.py patch
    - Make pkg-descr a bit prettier

--- net/py-oauth2/files/patch-setup.py
+++ net/py-oauth2/files/patch-setup.py
@@ -1,3 +1,6 @@
+# This package installs its 'test' module in site-packages/. Patch it out
+# so tests/ is excluded from installation
+
 --- ./setup.py.orig	2014-03-15 23:48:44.604925965 +1100
 +++ ./setup.py	2014-03-15 23:49:01.462781884 +1100
 @@ -31,7 +31,7 @@
