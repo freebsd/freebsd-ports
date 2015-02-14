ecc14c28252a0ea3a48c7d56ffeb471238d59265
Kurt Jaeger <pi@FreeBSD.org>
2015-02-14 18:52:05 +0000

    databases/pyspatialite: some updates
    - Remove amalgamation
    - Link with spatialite 4.x
    
    PR:		196387
    Submitted by:	coder@tuxfamily.org (maintainer)

--- databases/pyspatialite/files/patch-setup.py
+++ databases/pyspatialite/files/patch-setup.py
@@ -1,85 +1,37 @@
---- setup.py.orig	2012-01-11 23:54:57.000000000 +0100
-+++ setup.py	2013-03-24 09:52:46.000000000 +0100
-@@ -44,7 +44,7 @@
+--- setup.py.orig	2014-12-30 18:38:25.823292562 +0100
++++ setup.py	2014-12-30 18:39:43.418635165 +0100
+@@ -65,7 +65,7 @@
+         if rc != 0:
+             print "Is sphinx installed? If not, try 'sudo easy_install sphinx'."
  
- include_dirs = []
- library_dirs = []
--libraries = ['geos','geos_c','proj']
-+libraries = ['geos','geos_c','proj'%%ICONV%%]
- runtime_library_dirs = []
- extra_objects = []
- define_macros = []
-@@ -86,29 +86,30 @@
+-AMALGAMATION_ROOT = "amalgamation/libspatialite-amalgamation-3.0.1"
++#AMALGAMATION_ROOT = "amalgamation/libspatialite-amalgamation-3.0.1"
  
- def get_amalgamation():
-     """Download the Spatialite amalgamation if it isn't there, already."""
--    if os.path.exists(AMALGAMATION_ROOT):
--        return
--    os.mkdir(AMALGAMATION_ROOT)
--    print "Downloading amalgation."
--
--    # find out what's current amalgamation ZIP file
--    download_page = urllib.urlopen("https://www.gaia-gis.it/fossil/libspatialite/index").read()
--    pattern = re.compile("(libspatialite-amalgamation.*?\.zip)")
--    download_file = pattern.findall(download_page)[0]
--    amalgamation_url = "http://www.gaia-gis.it/gaia-sins/" + download_file
--    zip_dir = string.replace(download_file,'.zip','')
--    # and download it
--    urllib.urlretrieve(amalgamation_url, "tmp.zip")
--
--    zf = zipfile.ZipFile("tmp.zip")
--    files = ["sqlite3.c", "headers/spatialite/sqlite3.h", "spatialite.c", "headers/spatialite/sqlite3ext.h","headers/spatialite/spatialite.h","headers/spatialite/gaiaaux.h","headers/spatialite/gaiaexif.h","headers/spatialite/gaiageo.h"]
--    for fn in files:
--        print "Extracting", fn
--        outf = open(AMALGAMATION_ROOT + os.sep + string.split(fn,'/')[-1], "wb")
--        outf.write(zf.read(zip_dir + '/' + fn))
--        outf.close()
--    zf.close()
--    os.unlink("tmp.zip")
-+#   into ${FILESDIR} for FreeBSD
-+#    if os.path.exists(AMALGAMATION_ROOT):
-+#        return
-+#    os.mkdir(AMALGAMATION_ROOT)
-+#    print "Downloading amalgation."
-+#
-+#    # find out what's current amalgamation ZIP file
-+#    download_page = urllib.urlopen("http://www.gaia-gis.it/gaia-sins/libspatialite-sources/").read()
-+#    pattern = re.compile("(libspatialite-amalgamation.*?\.zip)")
-+#    download_file = pattern.findall(download_page)[-1]
-+#    amalgamation_url = "http://www.gaia-gis.it/gaia-sins/libspatialite-sources/" + download_file
-+#    zip_dir = string.replace(download_file,'.zip','')
-+#    # and download it
-+#    urllib.urlretrieve(amalgamation_url, "tmp.zip")
-+#
-+#    zf = zipfile.ZipFile("tmp.zip")
-+#    files = ["sqlite3.c", "headers/spatialite/sqlite3.h", "spatialite.c", "headers/spatialite/sqlite3ext.h","headers/spatialite/spatialite.h","headers/spatialite/gaiaaux.h","headers/spatialite/gaiaexif.h","headers/spatialite/gaiageo.h"]
-+#    for fn in files:
-+#        print "Extracting", fn
-+#        outf = open(AMALGAMATION_ROOT + os.sep + string.split(fn,'/')[-1], "wb")
-+#        outf.write(zf.read(zip_dir + '/' + fn))
-+#        outf.close()
-+#    zf.close()
-+#    os.unlink("tmp.zip")
+ TRUTHY = ("yes", "true", "t", "1")
  
- class MyBuildExt(build_ext):
- 
-@@ -119,8 +120,10 @@
-           ext.libraries.append("iconv")
- 
-         #Default locations for Mac
-+        ext.include_dirs.append("%%LOCALBASE%%/include/")
-         ext.include_dirs.append("/Library/Frameworks/GEOS.framework/unix/include/")
-         ext.include_dirs.append("/Library/Frameworks/PROJ.framework/unix/include/")
-+        ext.library_dirs.append("%%LOCALBASE%%/lib")
-         ext.library_dirs.append("/Library/Frameworks/GEOS.framework/unix/lib")
-         ext.library_dirs.append("/Library/Frameworks/PROJ.framework/unix/lib")
- 
-@@ -132,7 +135,7 @@
-         ext.sources.append(os.path.join(AMALGAMATION_ROOT, "spatialite.c"))
-         ext.include_dirs.append(AMALGAMATION_ROOT)
-         build_ext.build_extension(self, ext)
--        
-+
- 
- #    def __setattr__(self, k, v):
- #        # Make sure we don't link against the SQLite library, no matter what setup.cfg says
+@@ -258,12 +258,12 @@
+                         "src/prepare_protocol.c",
+                         "src/statement.c",
+                         "src/util.c",
+-                        "src/row.c",
+-                        os.path.join(AMALGAMATION_ROOT, "sqlite3.c"),
+-                        os.path.join(AMALGAMATION_ROOT, "spatialite.c")
++                        "src/row.c"
++#                        os.path.join(AMALGAMATION_ROOT, "sqlite3.c"),
++#                        os.path.join(AMALGAMATION_ROOT, "spatialite.c")
+                     ],
+                     include_dirs = [
+-                        os.path.join(AMALGAMATION_ROOT,"headers")
++#                        os.path.join(AMALGAMATION_ROOT,"headers")
+                     ],
+                     library_dirs = [],
+                     runtime_library_dirs = [],
+@@ -272,7 +272,7 @@
+                         ("VERSION",'"%s"' % PYSPATIALITE_VERSION),
+                         ("SQLITE_ENABLE_RTREE", "1"),   # build with fulltext search enabled
+                         ("NDEBUG","1"),
+-                        ("SPL_AMALGAMATION","1"),
++#                        ("SPL_AMALGAMATION","1"),
+                         ('MODULE_NAME', '\\"spatialite.dbapi2\\"') if sys.platform == "win32" else ('MODULE_NAME', '"spatialite.dbapi2"')
+                     ],
+                 )
