42623f002778dacd5c8a6de53cbc0c318a54c343
pi <pi@FreeBSD.org>
2015-02-14 18:52:05 +0000

    databases/pyspatialite: some updates
    - Remove amalgamation
    - Link with spatialite 4.x
    
    PR:		196387
    Submitted by:	coder@tuxfamily.org (maintainer)

--- databases/pyspatialite/files/patch-src-connection.h
+++ databases/pyspatialite/files/patch-src-connection.h
@@ -1,11 +1,11 @@
---- src/connection.h.bak	2012-10-25 19:40:06.000000000 +0200
-+++ src/connection.h	2012-10-25 19:40:25.000000000 +0200
-@@ -33,8 +33,6 @@
- #include "sqlite3.h"
+--- src/connection.h.orig	2014-12-30 18:40:27.628284595 +0100
++++ src/connection.h	2014-12-30 18:40:37.947283409 +0100
+@@ -30,7 +30,7 @@
+ #include "cache.h"
+ #include "module.h"
+ 
+-#include "spatialite/sqlite3.h"
++#include "spatialite/sqlite.h"
  #include "spatialite.h"
  
--int spatialite_init(int verbose);
--
- typedef struct
- {
-     PyObject_HEAD
+ SPATIALITE_DECLARE void spatialite_init(int verbose);
