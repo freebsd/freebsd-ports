8d8703f03011cd1ee24d6cca5f6ff1f757888c0d
culot <culot@FreeBSD.org>
2015-02-16 14:15:15 +0000

    - Update to 2014.7.2
    PR:		197597
    Submitted by:	Christer Edwards <christer.edwards@gmail.com> (maintainer)

--- sysutils/py-salt/files/patch-setup.py
+++ sysutils/py-salt/files/patch-setup.py
@@ -1,33 +1,13 @@
---- setup.py.orig	2014-11-03 12:38:21.000000000 -0700
-+++ setup.py        2014-11-04 14:42:58.775471457 -0700
-@@ -212,12 +212,12 @@
-             self.distribution.package_data.pop('salt.daemons.flo', None)
-             self.filelist.files.append(os.path.basename(PACKAGED_FOR_SALT_SSH_FILE))
-             self.distribution.metadata.name = 'salt-ssh'
--            self.distribution.data_files = [('share/man/man1',
-+            self.distribution.data_files = [('man/man1',
-                                              ['doc/man/salt-ssh.1',
-                                               'doc/man/salt-run.1',
-                                               'doc/man/salt-call.1',
-                                               'doc/man/salt-cloud.1']),
--                                            ('share/man/man7', ['doc/man/salt.7'])]
-+                                            ('man/man7', ['doc/man/salt.7'])]
- 
-         sdist.make_release_tree(self, base_dir, files)
- 
-@@ -666,13 +666,13 @@
-                                     '*.flo'
-                                     ]
-                                 },
--                'data_files': [('share/man/man1',
-+                'data_files': [('man/man1',
-                                 ['doc/man/salt-cp.1',
-                                  'doc/man/salt-call.1',
-                                  'doc/man/salt-minion.1',
-                                  'doc/man/salt-unity.1',
-                                  ]),
--                               ('share/man/man7',
-+                               ('man/man7',
-                                 ['doc/man/salt.7',
-                                  ]),
-                                ],
+--- setup.py.orig	2015-01-24 14:55:05.507732433 -0700
++++ setup.py	2015-01-24 14:59:07.595716022 -0700
+@@ -621,8 +621,8 @@
+     def _property_data_files(self):
+         # Data files common to all scenarios
+         data_files = [
+-            ('share/man/man1', ['doc/man/salt-call.1']),
+-            ('share/man/man7', ['doc/man/salt.7'])
++            ('man/man1', ['doc/man/salt-call.1']),
++            ('man/man7', ['doc/man/salt.7'])
+         ]
+         if self.ssh_packaging or PACKAGED_FOR_SALT_SSH:
+             data_files[0][1].append('doc/man/salt-ssh.1')
