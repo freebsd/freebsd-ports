b23728f30be7eb77538a2c07aca930a2a76feca3
Jan Beich <jbeich@FreeBSD.org>
2015-02-16 07:56:47 +0000

    Don't use the default backup suffix
    Rename backup file in order to avoid conflict with default suffix as
    used by REINPLACE_CMD in post-patch. This facilitates reviewing
    changes done by each step after the build.
    
    PR:		197502
    Exp-run by:	antoine
    Approved by:	autotools (tijl, maintainer)
    Approved by:	portmgr (antoine)
    Approved by:	mentors (implicit)

--- Mk/Uses/libtool.mk
+++ Mk/Uses/libtool.mk
@@ -27,7 +27,7 @@ _INCLUDE_USES_LIBTOOL_POST_MK=	yes
 
 patch-libtool:
 	@${FIND} ${WRKDIR} \( -name configure -or -name ltconfig \)	\
-		-type f | while read i; do ${SED} -i.bak		\
+		-type f | while read i; do ${SED} -i.libtool.bak	\
 		-e '/dragonfly\*/!s/^ *freebsd\*[ )]/dragonfly* | &/'	\
 		-e '/gcc_dir=\\`/s/gcc /$$CC /'				\
 		-e '/gcc_ver=\\`/s/gcc /$$CC /'				\
@@ -44,7 +44,7 @@ patch-libtool:
 			\$$libname\$$release\$$shared_ext\$$major	\
 			\$$libname\$$shared_ext'			\
 		    soname_spec='\$$libname\$$release\$$shared_ext\$$major'/; \
-		    }" $${i} && ${TOUCH} -mr $${i}.bak $${i}; done
+		    }" $${i} && ${TOUCH} -mr $${i}.libtool.bak $${i}; done
 
 	@${FIND} ${WRKDIR} -type f -name ltmain.sh |			\
 		${XARGS} ${REINPLACE_CMD}				\
