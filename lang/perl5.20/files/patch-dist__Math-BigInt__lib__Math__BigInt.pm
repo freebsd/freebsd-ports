dd8b3514893e4e20b134aa5334dda01acdbce3a6
mat <mat@FreeBSD.org>
2015-02-17 15:58:12 +0000

    Update Perl 5.20 to 5.20.2.
    Poked by:	az
    Sponsored by:	Absolight

--- lang/perl5.20/files/patch-dist__Math-BigInt__lib__Math__BigInt.pm
+++ lang/perl5.20/files/patch-dist__Math-BigInt__lib__Math__BigInt.pm
@@ -1,6 +1,6 @@
---- dist/Math-BigInt/lib/Math/BigInt.pm.orig	2014-09-14 11:31:01 UTC
+--- dist/Math-BigInt/lib/Math/BigInt.pm.orig	2014-12-27 11:49:23 UTC
 +++ dist/Math-BigInt/lib/Math/BigInt.pm
-@@ -62,7 +62,7 @@
+@@ -62,7 +62,7 @@ use overload
  '>>='	=>	sub { $_[0]->brsft($_[1]); },
  
  # not supported by Perl yet
