88e1d47dacebd4c1628648d182df57d654add6cc
tijl <tijl@FreeBSD.org>
2015-02-17 10:30:55 +0000

    Run intltoolize before running autoreconf when a port uses intltool and configure.ac (or configure.in) contains '^(AC|IT)_PROG_INTLTOOL'.
    Intltoolize updates intltool related build scripts and m4 files that are
    used by autoreconf to generate configure.  This change guarantees that
    the build scripts, configure and LOCALBASE/bin/intltool-* are all in sync.
    
    PR:		194393
    Differential Revision:	https://reviews.freebsd.org/D976
    Submitted by:	olivierd, kwm (modified)
    Exp-run by:	antoine
    Approved by:	portmgr (antoine)

--- Mk/Uses/autoreconf.mk
+++ Mk/Uses/autoreconf.mk
@@ -75,6 +75,13 @@ do-autoreconf:
 # Don't modify time stamps if the files already exist
 	@test -e ${CONFIGURE_WRKSRC}/${f} || ${TOUCH} ${CONFIGURE_WRKSRC}/${f}
 .endfor
+.if defined(_USE_GNOME) && ${_USE_GNOME:Mintltool}
+	@(cd ${CONFIGURE_WRKSRC} && \
+		if test -f configure.ac; then configure=configure.ac; \
+		else configure=configure.in; fi && \
+		if ${EGREP} -q '^(AC|IT)_PROG_INTLTOOL' $${configure}; \
+		then ${LOCALBASE}/bin/intltoolize -f -c; fi)
+.endif
 	@(cd ${CONFIGURE_WRKSRC} && ${LOCALBASE}/bin/autoreconf -f -i)
 .endif
 
