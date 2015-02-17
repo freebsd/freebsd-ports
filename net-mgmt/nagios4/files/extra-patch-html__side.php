4f2120f85ca2e7b99754904d2e3cbf143ddb90f3
Mathieu Arnold <mat@FreeBSD.org>
2015-02-17 13:11:02 +0000

    Add a couple of patches to fix the number of workers and CPU load. While there, regen patches, and update pkg-message to apache 2.4.
    PR:		197204
    Submitted by:	Rudolf Čejka
    Sponsored by:	Absolight

--- net-mgmt/nagios4/files/extra-patch-html__side.php
+++ net-mgmt/nagios4/files/extra-patch-html__side.php
@@ -1,6 +1,6 @@
 --- html/side.php.orig	2014-08-12 15:00:01 UTC
 +++ html/side.php
-@@ -56,8 +56,8 @@
+@@ -56,8 +56,8 @@ $link_target = 'main';
  			<li>
  				<a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?host=all&amp;servicestatustypes=28" target="<?php echo $link_target;?>">Problems</a>
  				<ul>
