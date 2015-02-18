549662a0f89137f104210f43ab9ee5d1ed700b06
John Baldwin <jhb@FreeBSD.org>
2015-02-18 19:24:15 +0000

    - Add support for XSAVE on amd64 and i386 using the recently added   PT_GETXSTATE_INFO and XSTATE core dump sections. - Use kern.proc.sigtramp on i386 to fix unwinding across signal   frames. - Change the amd64 FreeBSD ABI to return an i386 target description   when debugging a 32-bit process.  This allows XSAVE debugging to   work for an i386 binary on amd64. - Rework thread patches to pull all threads-related patches out of   the main port patches and into a single optional patch. - Adjust the thread patches so that fbsd-threads.c passes register   requests for LWPs down to the native target instead of calling   ptrace directly.  This allows XSAVE to work with threads without   any other changes.  The x86 nat.c files now pass LWP IDs to   ptrace register operations (other platforms already did this).   The supply/collect regset stubs are now only needed to support   userland threads for libkse.
    PR:		197501
    Approved by:	maintainer, bdrewery

--- devel/gdb/files/patch-gdb-amd64fbsd-nat.c
+++ devel/gdb/files/patch-gdb-amd64fbsd-nat.c
@@ -1,10 +1,9 @@
 --- gdb/amd64fbsd-nat.c.orig	2014-06-11 18:34:41.000000000 +0200
 +++ gdb/amd64fbsd-nat.c	2014-09-24 18:27:50.618458853 +0200
-@@ -21,13 +21,17 @@
+@@ -21,13 +21,16 @@
  #include "inferior.h"
  #include "regcache.h"
  #include "target.h"
-+#include "gregset.h"
  
  #include "gdb_assert.h"
  #include <signal.h>
@@ -18,53 +17,6 @@
  #include <machine/reg.h>
  
  #include "fbsd-nat.h"
-@@ -93,6 +97,46 @@
- };
- 
- 
-+/* Transfering the registers between GDB, inferiors and core files.  */
-+
-+/* Fill GDB's register array with the general-purpose register values
-+   in *GREGSETP.  */
-+
-+void
-+supply_gregset (struct regcache *regcache, const gregset_t *gregsetp)
-+{
-+  amd64_supply_native_gregset (regcache, gregsetp, -1);
-+}
-+
-+/* Fill register REGNUM (if it is a general-purpose register) in
-+   *GREGSETPS with the value in GDB's register array.  If REGNUM is -1,
-+   do this for all registers.  */
-+
-+void
-+fill_gregset (const struct regcache *regcache, gdb_gregset_t *gregsetp, int regnum)
-+{
-+  amd64_collect_native_gregset (regcache, gregsetp, regnum);
-+}
-+
-+/* Fill GDB's register array with the floating-point register values
-+   in *FPREGSETP.  */
-+
-+void
-+supply_fpregset (struct regcache *regcache, const fpregset_t *fpregsetp)
-+{
-+  amd64_supply_fxsave (regcache, -1, fpregsetp);
-+}
-+
-+/* Fill register REGNUM (if it is a floating-point register) in
-+   *FPREGSETP with the value in GDB's register array.  If REGNUM is -1,
-+   do this for all registers.  */
-+
-+void
-+fill_fpregset (const struct regcache *regcache, gdb_fpregset_t *fpregsetp, int regnum)
-+{
-+  amd64_collect_fxsave (regcache, regnum, fpregsetp);
-+}
-+
- /* Support for debugging kernel virtual memory images.  */
- 
- #include <machine/pcb.h>
 @@ -247,6 +291,10 @@
  
    SC_RBP_OFFSET = offset;
--- devel/gdb/files/patch-gdb-amd64fbsd-tdep.c
+++ devel/gdb/files/patch-gdb-amd64fbsd-tdep.c
@@ -1,13 +1,5 @@
 --- gdb/amd64fbsd-tdep.c.orig	2010-01-01 02:31:29.000000000 -0500
 +++ gdb/amd64fbsd-tdep.c	2011-01-05 17:27:29.264869000 -0500
-@@ -29,7 +29,6 @@
- #include "gdb_string.h"
- 
- #include "amd64-tdep.h"
--#include "bsd-uthread.h"
- #include "solib-svr4.h"
- 
- /* Support for signal handlers.  */
 @@ -40,12 +39,16 @@
  static CORE_ADDR
  amd64fbsd_sigcontext_addr (struct frame_info *this_frame)
@@ -26,61 +18,3 @@
    return sp + 16;
  }
  
-@@ -142,46 +145,6 @@
- };
- 
- static void
--amd64fbsd_supply_uthread (struct regcache *regcache,
--			  int regnum, CORE_ADDR addr)
--{
--  gdb_byte buf[8];
--  int i;
--
--  gdb_assert (regnum >= -1);
--
--  for (i = 0; i < ARRAY_SIZE (amd64fbsd_jmp_buf_reg_offset); i++)
--    {
--      if (amd64fbsd_jmp_buf_reg_offset[i] != -1
--	  && (regnum == -1 || regnum == i))
--	{
--	  read_memory (addr + amd64fbsd_jmp_buf_reg_offset[i], buf, 8);
--	  regcache_raw_supply (regcache, i, buf);
--	}
--    }
--}
--
--static void
--amd64fbsd_collect_uthread (const struct regcache *regcache,
--			   int regnum, CORE_ADDR addr)
--{
--  gdb_byte buf[8];
--  int i;
--
--  gdb_assert (regnum >= -1);
--
--  for (i = 0; i < ARRAY_SIZE (amd64fbsd_jmp_buf_reg_offset); i++)
--    {
--      if (amd64fbsd_jmp_buf_reg_offset[i] != -1
--	  && (regnum == -1 || regnum == i))
--	{
--	  regcache_raw_collect (regcache, i, buf);
--	  write_memory (addr + amd64fbsd_jmp_buf_reg_offset[i], buf, 8);
--	}
--    }
--}
--
--static void
- amd64fbsd_init_abi (struct gdbarch_info info, struct gdbarch *gdbarch)
- {
-   struct gdbarch_tdep *tdep = gdbarch_tdep (gdbarch);
-@@ -201,10 +164,6 @@
-   tdep->sc_reg_offset = amd64fbsd_sc_reg_offset;
-   tdep->sc_num_regs = ARRAY_SIZE (amd64fbsd_sc_reg_offset);
- 
--  /* FreeBSD provides a user-level threads implementation.  */
--  bsd_uthread_set_supply_uthread (gdbarch, amd64fbsd_supply_uthread);
--  bsd_uthread_set_collect_uthread (gdbarch, amd64fbsd_collect_uthread);
--
-   /* FreeBSD uses SVR4-style shared libraries.  */
-   set_solib_svr4_fetch_link_map_offsets
-     (gdbarch, svr4_lp64_fetch_link_map_offsets);
