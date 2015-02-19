617860334cbc42d25e73a9dbda91c7ddae399b5b
Alex Dupre <ale@FreeBSD.org>
2015-02-19 11:34:05 +0000

    Add armv6 as supported arch.
    PR:		197609
    Submitted by:	egypcio@googlemail.com

--- security/cryptlib/files/patch-misc_os_spec.h
+++ security/cryptlib/files/patch-misc_os_spec.h
@@ -1,6 +1,17 @@
---- misc/os_spec.h.orig	2010-10-13 23:58:52.000000000 +0200
-+++ misc/os_spec.h	2011-03-04 15:31:57.000000000 +0100
-@@ -601,7 +601,7 @@ typedef unsigned char		BYTE;
+--- misc/os_spec.h.orig	2011-08-02 23:52:50.000000000 +0000
++++ misc/os_spec.h	2015-02-19 11:32:33.126607217 +0000
+@@ -606,9 +606,7 @@
+    support for the change was added in gcc 4.4, so any newer version with 
+    ARM_EABI defined will have a scalar va_list */
+ 
+-#if defined( __GNUC__ ) && \
+-	( ( __GNUC__ == 4 && __GNUC_MINOR__ >= 4 ) || ( __GNUC__ > 4 ) ) && \
+-	defined( __ARM_EABI__ )
++#if defined( __ARM_EABI__ )
+ 	/* In theory we could check __ap but in practice it's too risky to rely 
+ 	   on the type and state of hidden internal fields, and in any case it's 
+ 	   only a sanity check, not a hard requirement, so we just no-op the 
+@@ -666,7 +664,7 @@
  
  #if defined( __WINDOWS__ ) || \
  	( defined( __UNIX__ ) && \
