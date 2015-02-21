1fd6a816e667909ed76110e6bce2039a3e85b8c9
rakuco <rakuco@FreeBSD.org>
2015-02-21 20:16:21 +0000

    Fix `make patch' after r379541.
    The new patch just didn't apply, and apparently nobody tested it before
    committing...
    
    PR:		197483

--- www/webkit-qt5/files/patch-Source_JavaScriptCore_assembler_ARMAssembler.h
+++ www/webkit-qt5/files/patch-Source_JavaScriptCore_assembler_ARMAssembler.h
@@ -1,11 +1,11 @@
---- Source/JavaScriptCore/assembler/ARMAssembler.h.orig	2015-01-19 21:25:28.000000000 +0000
-+++ Source/JavaScriptCore/assembler/ARMAssembler.h	2015-01-19 21:25:57.000000000 +0000
-@@ -1121,6 +1121,8 @@ namespace JSC {
+--- Source/JavaScriptCore/assembler/ARMAssembler.h
++++ Source/JavaScriptCore/assembler/ARMAssembler.h
+@@ -1064,6 +1064,8 @@
              linuxPageFlush(current, end);
  #elif OS(WINCE)
              CacheRangeFlush(code, size, CACHE_SYNC_ALL);
 +#elif CPU(ARM_TRADITIONAL) && OS(FREEBSD) && COMPILER(CLANG)
-+	__clear_cache(code, reinterpret_cast<char*>(code) + size);
- #else
- #error "The cacheFlush support is missing on this platform."
- #endif
++            __clear_cache(code, reinterpret_cast<char*>(code) + size);
+ #elif OS(QNX) && ENABLE(ASSEMBLER_WX_EXCLUSIVE)
+             UNUSED_PARAM(code);
+             UNUSED_PARAM(size);
