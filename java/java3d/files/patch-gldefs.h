879cdc904113215bf20a67ba2a81b3d498862cea
Jung-uk Kim <jkim@FreeBSD.org>
2015-02-21 01:33:00 +0000

    Fix build with old Mesa3D.  r378494 fixed new Mesa3D but it broke old ones.

--- java/java3d/files/patch-gldefs.h
+++ java/java3d/files/patch-gldefs.h
@@ -1,11 +1,13 @@
 --- j3d-core/src/native/ogl/gldefs.h.orig	2008-02-28 15:18:01.000000000 -0500
-+++ j3d-core/src/native/ogl/gldefs.h	2015-02-05 16:38:10.813942000 -0500
-@@ -65,6 +65,8 @@
++++ j3d-core/src/native/ogl/gldefs.h	2015-02-20 17:51:50.420236000 -0500
+@@ -65,6 +65,10 @@
  #include <X11/Xlib.h>
  #include <X11/Xutil.h>
  
-+typedef ptrdiff_t GLsizeiptr;
++#define GL_VERSION_1_5_TYPES_DEFINED
 +typedef ptrdiff_t GLintptr;
++typedef ptrdiff_t GLsizeiptr;
++
  #include <GL/gl.h>
  #include <GL/glx.h>
  #ifdef Java3D_undef__glext_h_
