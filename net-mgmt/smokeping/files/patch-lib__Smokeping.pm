d60aac6a0a7ef4fa3b24b5bcb1c32d15af1c7076
rodrigo <rodrigo@FreeBSD.org>
2015-02-22 19:37:33 +0000

    Upgrade smokeping to 2.6.11
    PR:		197579
    Submitted by:	pi

--- net-mgmt/smokeping/files/patch-lib__Smokeping.pm
+++ net-mgmt/smokeping/files/patch-lib__Smokeping.pm
@@ -1,11 +1,57 @@
---- ./lib/Smokeping.pm.orig	2014-09-20 00:55:47.498042436 +0400
-+++ ./lib/Smokeping.pm	2014-09-20 00:56:31.929038660 +0400
-@@ -2631,7 +2631,7 @@
- The base directory where SmokePing keeps the files related to the DYNAMIC function.
- This directory must be writeable by the WWW server. It is also used for temporary
- storage of slave polling results by the master in 
--L<the master/slave mode|smokeping_master_slave>.
-+L<the masterE<sol>slave mode|smokeping_master_slave>.
+--- lib/Smokeping.pm.orig	2015-02-12 19:31:21.000000000 +0100
++++ lib/Smokeping.pm	2015-02-12 19:34:23.000000000 +0100
+@@ -2632,12 +2632,14 @@
+         {
+          %$DIRCHECK_SUB,
+          _doc => <<DOC,
+-The base directory where SmokePing keeps the files related to the DYNAMIC function.
+-This directory must be writeable by the WWW server. It is also used for temporary
+-storage of slave polling results by the master in 
++The base directory where SmokePing keeps the files related to the DYNAMIC
++function.
++This directory must be writeable by the WWW server. It is also used for
++temporary storage of slave polling results by the master in 
+ L<the masterE<sol>slave mode|smokeping_master_slave>.
  
- If this variable is not specified, the value of C<datadir> will be used instead.
+-If this variable is not specified, the value of C<datadir> will be used instead.
++If this variable is not specified, the value of C<datadir> will be
++used instead.
  DOC
+         },
+         piddir  =>
+@@ -4214,7 +4216,7 @@
+         if(defined $opt{'check'}) { verify_cfg($cfgfile); exit 0; }
+         if($opt{reload})  { 
+             load_cfg $cfgfile, 'noinit'; # we need just the piddir
+-            kill_smoke $cfg->{General}{piddir}."/smokeping.pid", SIGHUP; 
++            kill_smoke $cfg->{General}{piddir}."/pid", SIGHUP; 
+             print "HUP signal sent to the running SmokePing process, exiting.\n";
+             exit 0;
+         };
+@@ -4223,7 +4225,7 @@
+         if(defined $opt{'static-pages'}) { makestaticpages $cfg, $opt{'static-pages'}; exit 0 };
+         if($opt{email})    { enable_dynamic $cfg, $cfg->{Targets},"",""; exit 0 };
+     }
+-    if($opt{restart})  { kill_smoke $cfg->{General}{piddir}."/smokeping.pid", SIGINT;};
++    if($opt{restart})  { kill_smoke $cfg->{General}{piddir}."/pid", SIGINT;};
+ 
+     if($opt{logfile})      { initialize_filelog($opt{logfile}) };
+ 
+@@ -4236,7 +4238,7 @@
+                 initialize_syslog($cfg->{General}{syslogfacility}, 
+                                   $cfg->{General}{syslogpriority});
+         }
+-        daemonize_me $cfg->{General}{piddir}."/smokeping.pid";
++        daemonize_me $cfg->{General}{piddir}."/pid";
+     }
+     do_log "Smokeping version $VERSION successfully launched.";
+ 
+@@ -4413,7 +4415,7 @@
+             my $new_conf = Smokeping::Slave::submit_results $slave_cfg,$cfg,$myprobe,$probes;
+             if ($new_conf && !$gothup){
+                 do_log('server has new config for me ... HUPing the parent');
+-                kill_smoke $cfg->{General}{piddir}."/smokeping.pid", SIGHUP; 
++                kill_smoke $cfg->{General}{piddir}."/pid", SIGHUP; 
+                 # wait until the parent signals back if it didn't already
+                 sleep if (!$gothup);
+                 if (!$gothup) {
--- net-mgmt/smokeping/files/patch-lib__Smokeping__probes__AnotherDNS.pm
+++ net-mgmt/smokeping/files/patch-lib__Smokeping__probes__AnotherDNS.pm
@@ -1,5 +1,5 @@
---- lib/Smokeping/probes/AnotherDNS.pm.orig	2012-07-09 09:45:46.000000000 +0000
-+++ lib/Smokeping/probes/AnotherDNS.pm	2014-07-22 23:07:45.626343897 +0000
+--- lib/Smokeping/probes/AnotherDNS.pm.orig	2014-10-29 09:03:10.000000000 +0100
++++ lib/Smokeping/probes/AnotherDNS.pm	2015-02-12 19:43:50.000000000 +0100
 @@ -25,6 +25,8 @@
  use IO::Select;
  use Net::DNS;
@@ -9,11 +9,11 @@
  sub pod_hash {
  	return {
  		name => <<DOC,
-@@ -95,7 +97,7 @@
+@@ -108,7 +110,7 @@
          $elapsed = tv_interval( $t0, $t1 );
          if ( defined $ready ) {
              my $buf = '';
--            $ready->recv( $buf, &Net::DNS::PACKETSZ );
+-            $ready->recv( $buf, 512 );
 +            $ready->recv( $buf, PACKETSZ );
  	    my ($recvPacket, $err) = Net::DNS::Packet->new(\$buf);
  	    if (defined $recvPacket) {
