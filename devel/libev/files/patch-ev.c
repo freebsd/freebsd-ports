903aa424c328fef57e26ec7cea31513c7dad5c4d
Vsevolod Stakhov <vsevolod@FreeBSD.org>
2015-02-23 09:39:07 +0000

    - Fix positioning of _Noreturn attribute for libev - Do not bump portrevision, since a package has not been changed
    PR:		197864
    Submitted by:	dim

--- devel/libev/files/patch-ev.c
+++ devel/libev/files/patch-ev.c
@@ -1,6 +1,17 @@
 --- ./ev.c.orig	2011-02-09 00:17:37.000000000 +0100
 +++ ./ev.c	2011-08-19 17:01:35.000000000 +0200
-@@ -1591,10 +1591,12 @@
+@@ -967,8 +967,8 @@
+   #define ecb_unreachable() __builtin_unreachable ()
+ #else
+   /* this seems to work fine, but gcc always emits a warning for it :/ */
+-  ecb_inline void ecb_unreachable (void) ecb_noreturn;
+-  ecb_inline void ecb_unreachable (void) { }
++  ecb_inline ecb_noreturn void ecb_unreachable (void);
++  ecb_inline ecb_noreturn void ecb_unreachable (void) { }
+ #endif
+ 
+ /* try to tell the compiler that some condition is definitely true */
+@@ -2443,10 +2443,12 @@
    unsigned int flags = ev_supported_backends ();
  
  #ifndef __NetBSD__
