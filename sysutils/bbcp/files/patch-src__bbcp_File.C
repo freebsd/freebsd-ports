5472acb50711eeee2192ccae48ae6b56ac682f80
robak <robak@FreeBSD.org>
2015-02-24 11:27:04 +0000

    sysutils/bbcp: update 20150113 -> 20150116
    - Add LICENSE_FILE
    - Change pkg-descr
    
    PR:		196814
    Submitted by:	Chris Hutchinson <portmaster@bsdforge.com>

--- sysutils/bbcp/files/patch-src__bbcp_File.C
+++ sysutils/bbcp/files/patch-src__bbcp_File.C
@@ -1,5 +1,5 @@
---- src/bbcp_File.C.orig	2015-01-14 12:12:57.000000000 -0800
-+++ src/bbcp_File.C	2015-01-14 12:24:35.000000000 -0800
+--- src/bbcp_File.C.orig	2015-01-15 18:41:10.000000000 -0800
++++ src/bbcp_File.C	2015-01-15 18:43:04.000000000 -0800
 @@ -44,7 +44,7 @@
  
  #ifdef FREEBSD
@@ -9,3 +9,12 @@
  #endif
  
  /******************************************************************************/
+@@ -153,7 +153,7 @@
+ 
+ // Find a buffer
+ //
+-   if (bp = nextbuff)
++   if ((bp = nextbuff))
+       while(bp && bp->boff != offset) {pp = bp; bp = bp->next;}
+ 
+ // If we found a buffer, unchain it
