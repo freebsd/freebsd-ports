a4beb47215e68317ed1cd5e74187aa233e474129
Bernard Spil <Sp1l@users.noreply.github.com>
2015-02-27 11:49:36 +0100

    Fix build with LibreSSL OPENSSL_VERSION

---
+++ devel/ice/files/patch-cpp_src_IceGrid_RegistryI.cpp
@@ -0,0 +1,10 @@
+--- cpp/src/IceGrid/RegistryI.cpp	2015-02-27 10:23:10.105521095 +0100
++++ cpp/src/IceGrid/RegistryI.cpp.orig	2013-10-04 17:48:14.000000000 +0200
+@@ -41,7 +41,6 @@
+ #include <fstream>
+ 
+ #include <openssl/des.h> // For crypt() passwords
+-#include <openssl/opensslv.h>
+ 
+ #include <sys/types.h>
+ 
