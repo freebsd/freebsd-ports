96e5fe935a03f945f65e1852de18cbbda1df8266
Bernard Spil <spil.oss@gmail.com>
2015-02-27 19:29:35 +0100

    Fix inverted LibreSSL patch

--- devel/ice/files/patch-cpp_src_IceGrid_RegistryI.cpp
+++ devel/ice/files/patch-cpp_src_IceGrid_RegistryI.cpp
@@ -1,10 +1,10 @@
---- cpp/src/IceGrid/RegistryI.cpp	2015-02-27 10:23:10.105521095 +0100
-+++ cpp/src/IceGrid/RegistryI.cpp.orig	2013-10-04 17:48:14.000000000 +0200
-@@ -41,7 +41,6 @@
+--- cpp/src/IceGrid/RegistryI.cpp.orig	2013-10-04 17:48:14.000000000 +0200
++++ cpp/src/IceGrid/RegistryI.cpp	2015-02-27 18:59:30.002347000 +0100
+@@ -41,6 +41,7 @@
  #include <fstream>
  
  #include <openssl/des.h> // For crypt() passwords
--#include <openssl/opensslv.h>
++#include <openssl/opensslv.h>
  
  #include <sys/types.h>
  
