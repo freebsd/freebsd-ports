83c1767ceb1dc019b0cd37b698392ff70df17ca7
Steve Wills <swills@FreeBSD.org>
2015-03-09 13:30:51 +0000

    lang/ruby22: update to 2.2.1
    PR:		198338
    Submitted by:	Yasuhiro KIMURA <freebsd.org@pob01.utahime.jp>

--- lang/ruby22/files/patch-tool_rbinstall.rb
+++ lang/ruby22/files/patch-tool_rbinstall.rb
@@ -1,5 +1,5 @@
---- tool/rbinstall.rb.orig	2014-10-14 20:39:17.000000000 +0800
-+++ tool/rbinstall.rb	2014-12-28 03:26:30.432471677 +0800
+--- tool/rbinstall.rb.orig	2014-12-26 15:44:52.000000000 +0900
++++ tool/rbinstall.rb	2015-03-06 07:28:12.508424655 +0900
 @@ -311,6 +311,7 @@
  libdir = CONFIG[CONFIG.fetch("libdirname", "libdir"), true]
  rubyhdrdir = CONFIG["rubyhdrdir", true]
@@ -17,7 +17,7 @@
      install pc, pkgconfigdir, :mode => $data_mode
    end
  end
-@@ -653,78 +654,6 @@
+@@ -653,86 +654,6 @@
  end
  # :startdoc:
  
@@ -77,20 +77,28 @@
 -end
 -
 -install?(:ext, :comm, :gem) do
--  require 'pathname'
--  gem_dir = Gem.default_dir
--  directories = Gem.ensure_gem_subdirectories(gem_dir, :mode => $dir_mode)
--  prepare "bundle gems", gem_dir, directories
--  Dir.glob(srcdir+'/gems/*.gem').each do |gem|
--    Gem.install gem, Gem::Requirement.default, :install_dir => with_destdir(Gem.dir), :domain => :local, :ignore_dependencies => true
--    gemname = Pathname(gem).basename
--    puts "#{" "*30}#{gemname}"
+-  begin
+-    require "zlib"
+-  rescue LoadErroe
 -  end
--  # fix directory permissions
--  # TODO: Gem.install should accept :dir_mode option or something
--  File.chmod($dir_mode, *Dir.glob(with_destdir(Gem.dir)+"/**/"))
--  # fix .gemspec permissions
--  File.chmod($data_mode, *Dir.glob(with_destdir(Gem.dir)+"/specifications/*.gemspec"))
+-  if defined?(Zlib)
+-    require 'pathname'
+-    gem_dir = Gem.default_dir
+-    directories = Gem.ensure_gem_subdirectories(gem_dir, :mode => $dir_mode)
+-    prepare "bundle gems", gem_dir, directories
+-    Dir.glob(srcdir+'/gems/*.gem').each do |gem|
+-      Gem.install gem, Gem::Requirement.default, :install_dir => with_destdir(Gem.dir), :domain => :local, :ignore_dependencies => true
+-      gemname = Pathname(gem).basename
+-      puts "#{" "*30}#{gemname}"
+-    end
+-    # fix directory permissions
+-    # TODO: Gem.install should accept :dir_mode option or something
+-    File.chmod($dir_mode, *Dir.glob(with_destdir(Gem.dir)+"/**/"))
+-    # fix .gemspec permissions
+-    File.chmod($data_mode, *Dir.glob(with_destdir(Gem.dir)+"/specifications/*.gemspec"))
+-  else
+-    puts "skip installing bundle gems because of lacking zlib"
+-  end
 -end
 -
  parse_args()
