f5f0cfd4a1638a09d02bfc9da1993cd7d255634e
olivierd <olivierd@FreeBSD.org>
2015-03-10 18:22:43 +0000

    - Update to 0.4.6 - Adjust URL of FreeBSD Bugzilla (quick search)

--- www/xfce4-smartbookmark-plugin/files/patch-src__smartbookmark.c
+++ www/xfce4-smartbookmark-plugin/files/patch-src__smartbookmark.c
@@ -1,28 +1,32 @@
---- ./src/smartbookmark.c.orig	2013-01-03 21:18:38.000000000 +0000
-+++ ./src/smartbookmark.c	2014-06-02 19:08:44.000000000 +0000
-@@ -183,9 +183,9 @@
-     box = gtk_vbox_new(FALSE, 0);
+--- src/smartbookmark.c.orig	2013-02-18 21:14:36 UTC
++++ src/smartbookmark.c
+@@ -182,10 +182,10 @@ static t_search *search_new(XfcePanelPlu
+     gtk_container_add(GTK_CONTAINER(align), search->box);
  
      /* default options */
 -    search->url = "http://bugs.debian.org/";
 -    search->label_text = "BTS";
 -    search->size = 5;
-+    search->url = "https://bugs.freebsd.org/bugzilla/";
-+    search->label_text = "Bugzilla";
+-    search->hide_label = FALSE;
++    search->url = "https://bugs.freebsd.org/bugzilla/buglist.cgi?quicksearch=";
++    search->label_text = "FreeBSD";
 +    search->size = 10;
-     search->hide_label = FALSE;
++    search->hide_label = TRUE;
      /* read config file options */
      filename = xfce_panel_plugin_save_location(plugin, TRUE);
-@@ -255,9 +255,9 @@
+     search_read_config(search, filename);
+@@ -218,10 +218,10 @@ static void search_read_config(t_search 
      if( (rcfile = xfce_rc_simple_open(filename, TRUE) ))
      {
          xfce_rc_set_group(rcfile, NULL);
 -        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","http://bugs.debian.org/"));
 -        search->label_text = g_strdup(xfce_rc_read_entry(rcfile,"value","DBS"));
 -        search->size = xfce_rc_read_int_entry(rcfile, "size", 5);
-+        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","https://bugs.freebsd.org/bugzilla/"));
-+        search->label_text = g_strdup(xfce_rc_read_entry(rcfile,"value","Bugzilla"));
+-        search->hide_label = xfce_rc_read_bool_entry(rcfile, "hidelabel", FALSE);
++        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","https://bugs.freebsd.org/bugzilla/buglist.cgi?quicksearch="));
++        search->label_text = g_strdup(xfce_rc_read_entry(rcfile,"value","FreeBSD"));
 +        search->size = xfce_rc_read_int_entry(rcfile, "size", 10);
-         search->hide_label = xfce_rc_read_bool_entry(rcfile, "hidelabel", FALSE);
++        search->hide_label = xfce_rc_read_bool_entry(rcfile, "hidelabel", TRUE);
      }
  }
+ 
