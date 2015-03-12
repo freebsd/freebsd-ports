f6ccafc39db593f58f92cdd884b9d242c1308fff
Kris Moore <kris@pcbsd.org>
2015-03-12 18:38:27 -0400

    Fix a typo in the lang/mono patch

--- lang/mono/files/patch-mono_io-layer_handles.c
+++ lang/mono/files/patch-mono_io-layer_handles.c
@@ -6,7 +6,7 @@
  	_wapi_fd_reserve = wapi_getdtablesize ();
 +	/* This being very large causes SIGSEGV on FreeBSD */
 +	if ( _wapi_fd_reserve > 50000 )
-+		_wapi_fd_reserver = 50000;
++		_wapi_fd_reserve = 50000;
  
  	/* This is needed by the code in _wapi_handle_new_internal */
  	_wapi_fd_reserve = (_wapi_fd_reserve + (_WAPI_HANDLE_INITIAL_COUNT - 1)) & ~(_WAPI_HANDLE_INITIAL_COUNT - 1);
