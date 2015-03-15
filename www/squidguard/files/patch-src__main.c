ba9c04ff0507b0b6022bc378441922d1af224b0a
rene <rene@FreeBSD.org>
2015-03-15 18:15:26 +0000

    www/squidguard: remove optional dependency on expired www/squid33
    Since there is only one squid version left, always depend on www/squid34 [1]
    
    Differential Revision:	https://reviews.freebsd.org/D2067
    Submitted by:	myself, madpilot [1]
    Approved by:	maintainer (madpilot)

--- www/squidguard/files/patch-src__main.c
+++ www/squidguard/files/patch-src__main.c
@@ -1,6 +1,6 @@
---- src/main.c.orig	Thu Jun 14 01:56:54 2001
-+++ src/main.c	Sun Jan  1 17:04:13 2006
-@@ -108,7 +108,11 @@
+--- src/main.c.orig	2008-08-30 10:27:41 UTC
++++ src/main.c
+@@ -114,7 +114,11 @@ int main(argc, argv, envp)
        globalUpdate = 1;
        break;
      case 'v':
@@ -12,3 +12,52 @@
        exit(0);
        break;
      case 't':
+@@ -175,7 +179,7 @@ int main(argc, argv, envp)
+ 	sgReloadConfig();
+       }
+       if(failsafe_mode) {
+-	puts("");
++	puts("ERR message=\"squidGuard failsafe mode\"");
+ 	fflush(stdout);
+ 	if(sig_hup){
+           sgReloadConfig();
+@@ -184,7 +188,7 @@ int main(argc, argv, envp)
+       }
+       if(parseLine(buf,&squidInfo) != 1){
+ 	sgLogError("Error parsing squid line: %s",buf);
+-	puts("");
++	puts("BH message=\"squidGuard error parsing squid line\"");
+       }
+         else {
+ 	src = Source;
+@@ -196,14 +200,14 @@ int main(argc, argv, envp)
+ 	  acl = sgAclCheckSource(src);
+ 	  if((redirect = sgAclAccess(src,acl,&squidInfo)) == NULL){
+ 	    if(src == NULL || src->cont_search == 0){
+-	      puts(""); 
++	      puts("ERR"); 
+ 	      break;
+ 	    } else
+ 	      if(src->next != NULL){
+ 		src = src->next;
+ 		continue;
+ 	      } else {
+-		puts("");
++		puts("ERR");
+ 		break;
+ 	      }
+ 	  } else {
+@@ -215,9 +219,10 @@ int main(argc, argv, envp)
+ 	      squidInfo.ident[0] = '-';
+ 	      squidInfo.ident[1] = '\0';
+ 	    }
+-	    fprintf(stdout,"%s %s/%s %s %s\n",redirect,squidInfo.src,
+-		    squidInfo.srcDomain,squidInfo.ident,
+-		    squidInfo.method);
++            if (isdigit(redirect[0]) && isdigit(redirect[1]) && isdigit(redirect[2]) && redirect[3]==':') {
++              fprintf(stdout,"OK status=%c%c%c url=\"%s\"\n", redirect[0], redirect[1], redirect[2], &redirect[4]);
++            } else
++              fprintf(stdout,"OK rewrite-url=\"%s\"\n",redirect);
+             /* sgLogError("%s %s/%s %s %s\n",redirect,squidInfo.src,squidInfo.srcDomain,squidInfo.ident,squidInfo.method);  */
+ 	    break;
+ 	  }
