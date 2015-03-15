4b48b66f6a1674586f925a3bd4c4ad6d10ba2a95
jbeich <jbeich@FreeBSD.org>
2015-03-15 11:51:06 +0000

    Improve style, consistency and fix minor issues in Cube-based ports
    - Convert to option helpers
    - Drop unused libGLU dependency (except for games/cube)
    - Track direct dependency on libX11 (for XFetchBytes)
    - Drop redundant MKDIR before COPYTREE_* macros [1]
    - Wrap cd related commands with parentheses [1]
    - Wrap lines exceeding 80 characters
    - Bump PORTREVISION to pick up changes in dependencies, plist (assaultcube)
      and catch regressions early [1]
    - games/assaultcube: CLIENT uses openal/vorbisfile *instead* of SDL_mixer
    - games/assaultcube: drop unused gettext-tools dependency [1]
    - games/assaultcube: convert to PLIST_DIRS [1]
    - games/assaultcube: add patch for source/src/bot/bot_waypoint.cpp
      file to fix "invalid source encoding" warning [1]
    - games/{cube,bloodfrontier}: MASTER or SERVER don't need libX11
    - games/redeclipse: tell how large the package is in IGNORE message
    - games/redeclipse: use PORTDATA to hold list of dirs for COPYTREE_SHARE
    
    Differential Revision:	https://reviews.freebsd.org/D1831
    PR:		197582 [1]
    PR:		197583 [2]
    Submitted by:	lightside@gmx.com [1]
    Requested by:	lightside@gmx.com [1]
    Reviewed by:	amdmi3, lightside@gmx.com (maintainers)
    Approved by:	maintainer timeout (1 month) [2]
    Approved by:	bapt (mentor)

---
+++ games/assaultcube/files/patch-source_src_bot_bot_waypoint.cpp
@@ -0,0 +1,11 @@
+--- source/src/bot/bot_waypoint.cpp.orig	2013-11-10 22:50:03.000000000 +0400
++++ source/src/bot/bot_waypoint.cpp	2015-02-13 07:33:14.000000000 +0400
+@@ -848,7 +848,7 @@
+ 
+      if (!pWP)
+      {
+-          conoutf("Error: Couldn´t find near waypoint");
++          conoutf("Error: Couldn't find near waypoint");
+           return;
+      }
+ 
