f25ea6508c3b8258bedc9351ff79f0d20502d3e7
jkim <jkim@FreeBSD.org>
2015-03-16 17:56:59 +0000

    Fix the fix.  My patch was actually wrong and upstream properly fixed it.
    Pointy hat to:	jkim

--- emulators/virtualbox-ose/files/patch-src-VBox-Devices-Input-PS2M.cpp
+++ emulators/virtualbox-ose/files/patch-src-VBox-Devices-Input-PS2M.cpp
@@ -1,13 +1,11 @@
 --- src/VBox/Devices/Input/PS2M.cpp.orig	2015-03-02 10:09:15.000000000 -0500
-+++ src/VBox/Devices/Input/PS2M.cpp	2015-03-12 18:44:38.787177000 -0400
-@@ -712,8 +712,8 @@
-     int8_t      dX, dY, dZ;
++++ src/VBox/Devices/Input/PS2M.cpp	2015-03-16 13:38:03.324517000 -0400
+@@ -709,7 +709,7 @@
+ static void ps2mReportAccumulatedEvents(PPS2M pThis)
+ {
+     uint8_t     val;
+-    int8_t      dX, dY, dZ;
++    int         dX, dY, dZ;
  
      /* Clamp the accumulated delta values to the allowed range. */
--    dX = RT_MIN(RT_MAX(pThis->iAccumX, -256), 255);
--    dY = RT_MIN(RT_MAX(pThis->iAccumY, -256), 255);
-+    dX = RT_MIN(RT_MAX(pThis->iAccumX, -128), 127);
-+    dY = RT_MIN(RT_MAX(pThis->iAccumY, -128), 127);
-     dZ = RT_MIN(RT_MAX(pThis->iAccumZ, -8), 7);
- 
-     /* Start with the sync bit and buttons 1-3. */
+     dX = RT_MIN(RT_MAX(pThis->iAccumX, -256), 255);
