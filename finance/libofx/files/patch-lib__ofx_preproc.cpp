eb06a64599e783d5f7e7c75b39789ff77c716b2c
John Marino <marino@FreeBSD.org>
2015-03-20 10:00:24 +0000

    finance/libobx: Fix numerous issues
    I came to fix only one problem with libofx, but when it failed to pass
    stage-QA checks, I found others.
    
      1) CXXFLAGS not propagated (PHB 12.9) due to bug in configure (blanket)
      2) DragonFly iconv support (blanket, taken from DPorts)
      3) DOXYGEN option was no implemented
         * --disable-doxygen was set rather than DOXYGEN_CONFIGURE_ENABLE
         * DOXYGEN_BUILD_DEPENDS was not set
      4) disable doxygen setting, html documentation got installed
         unconditionally, but this was not reflected in plist
    
    I decided that --disable-doxygen was the correct option because html files
    were already provided an installed, so the DOXYGEN option was removed.  I
    used PORTDOCS to fix the pkg-plist.  This port passes stage-QA now.
    
    Approved by:	blanket and just-fix-it

--- finance/libofx/files/patch-lib__ofx_preproc.cpp
+++ finance/libofx/files/patch-lib__ofx_preproc.cpp
@@ -1,11 +1,11 @@
---- ./lib/ofx_preproc.cpp.orig	2013-04-25 14:06:29.000000000 -0400
-+++ ./lib/ofx_preproc.cpp	2014-06-22 19:44:48.000000000 -0400
-@@ -310,7 +310,7 @@
+--- lib/ofx_preproc.cpp.orig	2014-09-12 19:26:30 UTC
++++ lib/ofx_preproc.cpp
+@@ -310,7 +310,7 @@ int ofx_proc_file(LibofxContextPtr ctx, 
              size_t outbytesleft = inbytesleft * 2 - 1;
              iconv_buffer = (char*) malloc (inbytesleft * 2);
              memset(iconv_buffer, 0, inbytesleft * 2);
 -#if defined(OS_WIN32) || defined(__sun)
-+#if defined(OS_WIN32) || defined(__sun) || defined(__FreeBSD__)
++#if defined(__FreeBSD__) || defined(__DragonFly__)
              const char * inchar = (const char *)s_buffer.c_str();
  #else
              char * inchar = (char *)s_buffer.c_str();
