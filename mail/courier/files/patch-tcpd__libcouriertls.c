e70483d19fa7c7065d324bba3edb5fd593dbd9ae
jkim <jkim@FreeBSD.org>
2015-03-23 23:07:18 +0000

    Make it also buildable without SSLv3 support.
    PR:		198399

--- mail/courier/files/patch-tcpd__libcouriertls.c
+++ mail/courier/files/patch-tcpd__libcouriertls.c
@@ -1,6 +1,6 @@
---- tcpd/libcouriertls.c.orig	2009-11-21 15:07:32.000000000 -0500
-+++ tcpd/libcouriertls.c	2015-03-23 18:04:57.776710000 -0400
-@@ -551,8 +551,10 @@
+--- tcpd/libcouriertls.c.orig	2009-11-21 21:07:32.000000000 +0100
++++ tcpd/libcouriertls.c	2015-03-07 22:46:47.521076321 +0100
+@@ -551,9 +551,13 @@
  	if (!protocol || !*protocol)
  		protocol="SSL23";
  
@@ -10,6 +10,9 @@
 +#ifndef OPENSSL_NO_SSL2
 +		protocol && strcmp(protocol, "SSL2") == 0 ? SSLv2_method():
 +#endif
++#ifndef OPENSSL_NO_SSL3
  		protocol && strcmp(protocol, "SSL3") == 0 ? SSLv3_method():
++#endif
  		protocol && strcmp(protocol, "SSL23") == 0 ? SSLv23_method():
  		TLSv1_method());
+ 
