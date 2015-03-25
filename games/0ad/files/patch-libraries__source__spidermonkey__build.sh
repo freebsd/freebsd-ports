c2f11811ee2f7b47d0f2b3a6b450f427c2125209
Guido Falsi <madpilot@FreeBSD.org>
2015-03-25 14:30:39 +0000

    Fix build with clang 3.6.0.

--- games/0ad/files/patch-libraries__source__spidermonkey__build.sh
+++ games/0ad/files/patch-libraries__source__spidermonkey__build.sh
@@ -1,8 +1,8 @@
---- libraries/source/spidermonkey/build.sh.orig	2014-09-24 08:29:20 UTC
+--- libraries/source/spidermonkey/build.sh.orig	2015-01-24 14:46:52 UTC
 +++ libraries/source/spidermonkey/build.sh
-@@ -66,6 +66,14 @@
- 
- cd mozjs24/js/src
+@@ -95,6 +95,14 @@ cd mozjs31/js/src
+ rm -rf build-debug
+ rm -rf build-release
  
 +cp jsnativestack.cpp jsnativestack.cpp.orig
 +sed 's/#  include <pthread_np.h>/#pragma GCC visibility push(default)\
@@ -10,7 +10,7 @@
 +#pragma GCC visibility pop/' jsnativestack.cpp.orig >jsnativestack.cpp
 +
 +cp configure configure.orig
-+sed 's/"$ac_cv_have_visibility_class_bug" = "no"; then/"$ac_cv_have_visibility_class_bug" = "no" -a "$OS_ARCH" != "FreeBSD" ; then/' configure.orig >configure
++sed -e 's/"$ac_cv_have_visibility_class_bug" = "no"; then/"$ac_cv_have_visibility_class_bug" = "no" -a "$OS_ARCH" != "FreeBSD" ; then/' -e 's/-Werror=conversion-null//' configure.orig >configure
 +
  # We want separate debug/release versions of the library, so we have to change
  # the LIBRARY_NAME for each build.
