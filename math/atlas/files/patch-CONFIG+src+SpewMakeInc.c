e59c5e2e130d16dc7ae04b960115c07010eaed3a
marino <marino@FreeBSD.org>
2015-03-25 07:37:15 +0000

    math category: Remove $PTHREAD_LIBS
    Note that math/PDL did not pass stage-QA before, so it was fixed.
    
    approved by:	PTHREAD blanket

--- math/atlas/files/patch-CONFIG+src+SpewMakeInc.c
+++ math/atlas/files/patch-CONFIG+src+SpewMakeInc.c
@@ -1,6 +1,6 @@
---- CONFIG/src/SpewMakeInc.c.orig	2011-05-14 13:33:24.000000000 -0400
-+++ CONFIG/src/SpewMakeInc.c	2011-09-06 06:06:00.000000000 -0400
-@@ -399,7 +399,8 @@
+--- CONFIG/src/SpewMakeInc.c.orig	2011-05-14 17:33:24 UTC
++++ CONFIG/src/SpewMakeInc.c
+@@ -399,7 +399,8 @@ main(int nargs, char **args)
              &mhz, &ptrbits, &ncpu, comps, &gccflags, &outfile, &srcdir,
              &blddir, &USEDEFL1, &USEARCHDEF, &nof77, &f2cdefs, &cdefs, &pmake,
              &flapack, &smaflags, &dmaflags, &f77lib, &l2size, &targ);
@@ -10,7 +10,7 @@
     if (!outfile)
        fpout = stdout;
     else
-@@ -482,7 +483,7 @@
+@@ -482,7 +483,7 @@ main(int nargs, char **args)
     fprintf(fpout, "#  -------------------------------------------------\n");
     fprintf(fpout, "#  Name indicating the platform to configure BLAS to\n");
     fprintf(fpout, "#  -------------------------------------------------\n");
@@ -19,7 +19,7 @@
     fprintf(fpout, "%d", ptrbits);
     if (ISAX)
        fprintf(fpout, "%s", ISAXNAM[ISAX]);
-@@ -649,7 +650,7 @@
+@@ -649,7 +650,7 @@ main(int nargs, char **args)
            comps[i][j-2] == 'c' && comps[i][j-1] == 'c')
           break;
     }
@@ -28,7 +28,7 @@
     fprintf(fpout, "   GOODGCC = %s", goodgcc);
     if (gccflags)
        fprintf(fpout, " %s", gccflags);
-@@ -661,7 +662,7 @@
+@@ -661,7 +662,7 @@ main(int nargs, char **args)
         fprintf(fpout, " %s", sp);
     fprintf(fpout, "\n");
  
@@ -37,7 +37,7 @@
     if (MachIsX86(mach))
     {
        if (ptrbits == 32)
-@@ -679,7 +680,7 @@
+@@ -679,7 +680,7 @@ main(int nargs, char **args)
     fprintf(fpout, "   FCLINKFLAGS = $(FLINKFLAGS)");
     if (strstr(comps[F77_], "ifort") && !OSIsWin(OS))
        fprintf(fpout, " -nofor_main");
@@ -46,7 +46,7 @@
     fprintf(fpout, "   ARFLAGS  = r\n");
  /*
   * JF Mertens says that even x86 OS X still need ranlib for safety
-@@ -702,14 +703,14 @@
+@@ -702,8 +703,8 @@ main(int nargs, char **args)
     fprintf(fpout, "#  ------------------------------------\n");
     fprintf(fpout, "#  Reference and system libraries\n");
     fprintf(fpout, "#  ------------------------------------\n");
@@ -57,10 +57,3 @@
     fprintf(fpout, "   FLAPACKlib = ");
     if (flapack) fprintf(fpout, "%s", flapack);
     fprintf(fpout, "\n");
-    fprintf(fpout, "   SLAPACKlib = \n");
-    if (THREADS)
--      fprintf(fpout, "   LIBS = -lpthread");
-+      fprintf(fpout, "   LIBS = %%PTHREAD_LIBS%%");
-    else
-       fprintf(fpout, "   LIBS =");
-    if (!OSIsWin(OS))
