9e002d2aeec0cb8de09ecb80d5f8f2b95a16c4ca
John Marino <marino@FreeBSD.org>
2015-03-27 07:05:29 +0000

    Mk/bsd.gecko.mk: Fix typo from yesterday
    Reported by:	danfe (good catch!)

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -524,7 +524,7 @@ gecko-post-patch:
 			${WRKSRC}/configure; do \
 		if [ -f $$f ] ; then \
 			${REINPLACE_CMD} -Ee 's|-lc_r|-pthread|g ; \
-				s|-l?pthread|-pthread}|g ; \
+				s|-l?pthread|-pthread|g ; \
 				s|echo aout|echo elf|g ; \
 				s|/usr/X11R6|${LOCALBASE}|g' \
 				$$f; \
