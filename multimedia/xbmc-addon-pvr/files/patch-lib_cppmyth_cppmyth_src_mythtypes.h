4d9f2e3c3ce1c805b465ea1ff6f8d34cb1fd3597
Jan Beich <jbeich@FreeBSD.org>
2015-03-27 07:06:54 +0000

    Unbreak build on 10.0 and 10.1 after r381731
    time_t tends to be bootlegged otherwise:
    
      lib/cppmyth/cppmyth/src/mythtypes.h:28,
      /usr/include/c++/4.2/string:47,
      /usr/include/c++/4.2/bits/char_traits.h:46,
      /usr/include/c++/4.2/bits/stl_algobase.h:70,
      /usr/include/c++/4.2/iosfwd:46,
      /usr/include/c++/4.2/bits/c++io.h:43,
      /usr/include/c++/4.2/bits/gthr.h:114,
      /usr/include/c++/4.2/bits/gthr-default.h:43,
      /usr/include/pthread.h:47,
      /usr/include/time.h:65,
    
      lib/cppmyth/cppmyth/src/mythtypes.h:28:
      /usr/include/c++/v1/string:434:
      /usr/include/c++/v1/__config:23:
      /usr/include/unistd.h:37:
      /usr/include/sys/types.h:209:
    
    Reported by:	decke (maintainer, via pkg-fallout)
    Pointy hat:	jbeich

---
+++ multimedia/xbmc-addon-pvr/files/patch-lib_cppmyth_cppmyth_src_mythtypes.h
@@ -0,0 +1,10 @@
+--- lib/cppmyth/cppmyth/src/mythtypes.h.orig	2015-03-01 20:42:14 UTC
++++ lib/cppmyth/cppmyth/src/mythtypes.h
+@@ -27,6 +27,7 @@
+ 
+ #include <string>
+ #include <stdint.h>
++#include <time.h>
+ #include <vector>
+ #include <map>
+ 
