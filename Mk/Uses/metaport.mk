8f30b87c72ee50a03d378353f81044ac2beaeb66
Baptiste Daroussin <bapt@FreeBSD.org>
2015-03-28 14:50:22 +0000

    Add USES=metaport to simplify writing metaports

---
+++ Mk/Uses/metaport.mk
@@ -0,0 +1,20 @@
+# $FreeBSD$
+#
+# Sets the default variables to handle metaports
+#
+# Feature:	metaport
+# Usage:	USES=metaport
+#
+# Take no arguments
+
+.if !defined(_INCLUDE_USES_METAPORT_MK)
+_INCLUDE_USES_METAPORT_MK=	yes
+
+MASTER_SITES=	#
+DISTFILES=	#
+EXTRACT_ONLY=	#
+NO_BUILD=	yes
+NO_INSTALL=	yes
+NO_MTREE=	yes
+
+.endif
