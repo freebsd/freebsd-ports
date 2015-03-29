640c4ef5e427314407d526a66f0534e97f4170e7
robak <robak@FreeBSD.org>
2015-03-29 12:43:58 +0000

    devel/boost-all: remove CPE information
    - Since CPE should not be applied to meta ports, I am reverting the commit with CPE info
    
    PR:		197711
    Approved by:	bapt

--- devel/boost-all/common.mk
+++ devel/boost-all/common.mk
@@ -9,6 +9,4 @@ MAINTAINER=	office@FreeBSD.org
 CONFLICTS+=	boost-python-1* boost-1*
 MASTER_SITES=	SF/boost/boost/${PORTVERSION}
 
-USES+=		cpe tar:bzip2
-CPE_VENDOR=	boost
-CPE_PRODUCT=	boost
+USES+=		tar:bzip2
