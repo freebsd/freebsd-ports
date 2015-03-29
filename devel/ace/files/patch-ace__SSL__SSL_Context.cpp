7371c79da43f677fb092222cc5e99a590eb4ac6b
pi <pi@FreeBSD.org>
2015-03-29 19:00:17 +0000

    devel/ace: Make EGD conditional/Fix build with LibreSSL
    PR:		198776
    Submitted by:	Bernard Spil <spil.oss@gmail.com>

--- devel/ace/files/patch-ace__SSL__SSL_Context.cpp
+++ devel/ace/files/patch-ace__SSL__SSL_Context.cpp
@@ -1,6 +1,6 @@
---- ace/SSL/SSL_Context.cpp.orig
+--- ace/SSL/SSL_Context.cpp.orig	2014-12-29 10:41:20 UTC
 +++ ace/SSL/SSL_Context.cpp
-@@ -274,7 +274,7 @@
+@@ -274,7 +274,7 @@ ACE_SSL_Context::set_mode (int mode)
      case ACE_SSL_Context::TLSv1:
        method = ::TLSv1_method ();
        break;
@@ -9,7 +9,7 @@
      case ACE_SSL_Context::TLSv1_1_client:
        method = ::TLSv1_1_client_method ();
        break;
-@@ -285,7 +285,7 @@
+@@ -285,7 +285,7 @@ ACE_SSL_Context::set_mode (int mode)
        method = ::TLSv1_1_method ();
        break;
  #endif
@@ -18,3 +18,12 @@
      case ACE_SSL_Context::TLSv1_2_client:
        method = ::TLSv1_2_client_method ();
        break;
+@@ -556,7 +556,7 @@ ACE_SSL_Context::random_seed (const char
+ int
+ ACE_SSL_Context::egd_file (const char * socket_file)
+ {
+-#if OPENSSL_VERSION_NUMBER < 0x00905100L
++#if OPENSSL_VERSION_NUMBER < 0x00905100L || defined(LIBRESSL_VERSION_NUMBER)
+   // OpenSSL < 0.9.5 doesn't have EGD support.
+   ACE_UNUSED_ARG (socket_file);
+   ACE_NOTSUP_RETURN (-1);
