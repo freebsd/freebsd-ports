09ee2e08c6f536d675b1398510adf8a748297424
pawel <pawel@FreeBSD.org>
2015-03-30 16:35:39 +0000

    - Update to version 0.18, now hosted at GitHub [1] - Reword pkgdescr, new homepage [1] - Convert to USES=autoreconf
    PR:		198891 [1]
    Submitted by:	lichray@gmail.com (maintainer) [1]

--- x11/stjerm/files/patch-src_config.c
+++ x11/stjerm/files/patch-src_config.c
@@ -1,18 +1,6 @@
---- src/config.c.orig	2011-07-19 16:52:33.000000000 -0500
-+++ src/config.c	2011-10-12 17:05:50.164711868 -0500
-@@ -146,7 +146,11 @@ pid_t get_stjerm_pid(void)
-     char buffer[100];
-     char **list;
-     int i = 0;
-+#ifdef __FreeBSD__
-+    FILE *p = popen("pgrep stjerm", "r");
-+#else
-     FILE *p = popen("pidof stjerm", "r");
-+#endif
-     
-     if(p == NULL)
-     {
-@@ -552,7 +556,7 @@ void conf_init(void)
+--- src/config.c~	2014-01-12 11:31:51.000000000 -0500
++++ src/config.c	2015-03-24 17:32:49.873800297 -0400
+@@ -614,7 +614,7 @@ void conf_init(void)
          exit(1);
      }
  
