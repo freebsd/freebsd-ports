b4b3c4d8ab8f99a5714554f47fb247bd9181e029
Steve Wills <swills@FreeBSD.org>
2015-03-30 13:16:20 +0000

    databases/ruby-bdb: fix build with ruby 2.1 and 2.2
    With hat:	ruby@

---
+++ databases/ruby-bdb/files/patch-src-extconf.rb
@@ -0,0 +1,23 @@
+--- src/extconf.rb.orig	2015-01-22 16:09:31.000000000 +0000
++++ src/extconf.rb	2015-01-22 16:09:34.000000000 +0000
+@@ -36,16 +36,16 @@
+ 
+ if unknown = enable_config("unknown")
+    libs = if CONFIG.key?("LIBRUBYARG_STATIC")
+-	     Config::expand(CONFIG["LIBRUBYARG_STATIC"].dup).sub(/^-l/, '')
++	     RbConfig::expand(CONFIG["LIBRUBYARG_STATIC"].dup).sub(/^-l/, '')
+ 	  else
+-	     Config::expand(CONFIG["LIBRUBYARG"].dup).sub(/lib([^.]*).*/, '\\1')
++	     RbConfig::expand(CONFIG["LIBRUBYARG"].dup).sub(/lib([^.]*).*/, '\\1')
+ 	  end
+    unknown = find_library(libs, "ruby_init", 
+-			  Config::expand(CONFIG["archdir"].dup))
++			  RbConfig::expand(CONFIG["archdir"].dup))
+ end
+ 
+ inc_dir, lib_dir = dir_config("db", "/usr/include", "/usr/lib")
+-case Config::CONFIG["arch"]
++case RbConfig::CONFIG["arch"]
+ when /solaris2/
+    $DLDFLAGS ||= ""
+    $DLDFLAGS += " -R#{lib_dir}"
