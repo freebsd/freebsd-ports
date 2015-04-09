5bd325869bdeccff8037a64e2089b799abe798e0
Alonso Schaich <alonso@FreeBSD.org>
2015-04-09 19:27:31 +0000

    MFH: r382722
    Obtain a credential on login with KDM4
    
    PAM_SETCRED(3)'s second parameter does not default to PAM_ESTABLISH_CRED on
    FreeBSD, so explify it, as was proposed on the forums[1] back in decembre.
    
    Bump port revision as this commit changes the runtime behavior of the
    installed port.
    
    [1] https://forums.freebsd.org/threads/problem-with-kdm4-kerberos-and-nfsv4.49253
    
    PR:		195899
    Approved by:	rakuco (mentor)
    Approved by:	ports-secteam (delphij)

--- x11/kde4-workspace/files/patch-kdm__backend__client.c
+++ x11/kde4-workspace/files/patch-kdm__backend__client.c
@@ -1,6 +1,6 @@
---- ./kdm/backend/client.c.orig	2010-07-06 10:54:30.000000000 +0400
-+++ ./kdm/backend/client.c	2010-07-30 22:23:48.264408318 +0400
-@@ -1309,6 +1309,9 @@
+--- kdm/backend/client.c.orig	2014-11-06 23:36:58.000000000 +0100
++++ kdm/backend/client.c	2015-03-09 13:20:24.870430799 +0100
+@@ -1322,6 +1322,9 @@
      char ckDeviceBuf[20] = "";
      const char *ckDevice = ckDeviceBuf;
      dbus_bool_t isLocal;
@@ -10,7 +10,7 @@
  #endif
      char *failsafeArgv[2];
      char *buf, *buf2;
-@@ -1340,6 +1343,13 @@
+@@ -1353,6 +1356,13 @@
  # ifdef HAVE_VTS
      if (td->serverVT > 0)
          sprintf(ckDeviceBuf, "/dev/tty%d", td->serverVT);
@@ -24,3 +24,12 @@
  # endif
      isLocal = ((td->displayType & d_location) == dLocal);
  # ifdef XDMCP
+@@ -1532,7 +1542,7 @@
+     environ = pam_env;
+ # endif
+     removeCreds = True; /* set it first - i don't trust PAM's rollback */
+-    pretc = pam_setcred(pamh, 0);
++    pretc = pam_setcred(pamh, PAM_ESTABLISH_CRED);
+     reInitErrorLog();
+ # ifndef HAVE_PAM_GETENVLIST
+     pam_env = environ;
