cef8dde6cd96b8712562912558204486e4f03210
Dmitry Marakasov <amdmi3@FreeBSD.org>
2015-04-09 09:59:39 +0000

    - Fix build with base gcc on 8.x and 9.x, remove USE_GCC
    PR:		196712
    Approved by:	mandree (maintainer)

--- graphics/OpenEXR/files/patch-IlmImf__ImfSystemSpecific.cpp
+++ graphics/OpenEXR/files/patch-IlmImf__ImfSystemSpecific.cpp
@@ -1,25 +1,29 @@
---- IlmImf/ImfSystemSpecific.cpp.orig	2014-08-10 04:23:57 UTC
-+++ IlmImf/ImfSystemSpecific.cpp
-@@ -35,6 +35,9 @@
- #include "ImfSystemSpecific.h"
- #include "ImfNamespace.h"
- #include "OpenEXRConfig.h"
-+#if defined(IMF_HAVE_SSE2) &&  defined(__GNUC__)
-+#include <cpuid.h>
-+#endif
- 
- OPENEXR_IMF_INTERNAL_NAMESPACE_SOURCE_ENTER
- 
-@@ -44,11 +47,7 @@ namespace {
+--- IlmImf/ImfSystemSpecific.cpp.orig	2014-08-10 08:23:57.000000000 +0400
++++ IlmImf/ImfSystemSpecific.cpp	2015-04-09 00:08:50.100958000 +0300
+@@ -44,11 +44,23 @@
      // Helper functions for gcc + SSE enabled
      void cpuid(int n, int &eax, int &ebx, int &ecx, int &edx)
      {
--        __asm__ __volatile__ (
++#if __i386__
+         __asm__ __volatile__ (
 -            "cpuid"
 -            : /* Output  */ "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) 
 -            : /* Input   */ "a"(n)
--            : /* Clobber */);
-+	__cpuid(n, eax, ebx, ecx, edx);
++            "xchgl %%ebx,%k1\n"
++            "cpuid\n"
++            "xchgl %%ebx,%k1" \
++            : /* Output  */ "=a"(eax), "=&r"(ebx), "=c"(ecx), "=d"(edx)
++            : /* Input   */ "0"(n)
+             : /* Clobber */);
++#else
++        __asm__ __volatile__ (
++            "xchgq %%rbx,%q1\n"
++            "cpuid\n"
++            "xchgq %%rbx,%q1" \
++            : /* Output  */ "=a"(eax), "=&r"(ebx), "=c"(ecx), "=d"(edx)
++            : /* Input   */ "0"(n)
++            : /* Clobber */);
++#endif
      }
  
  #else // IMF_HAVE_SSE2 && __GNUC__
