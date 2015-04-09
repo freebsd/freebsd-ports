656b1ea17a4d84221bcc02640f0fb33191bd346d
Veniamin Gvozdikov <vg@FreeBSD.org>
2015-04-09 08:53:42 +0000

    - Update to 0.15.2
    PR:		ports/198733
    Submitted by:	nomoo@nomoo.ru (maintainer)

---
+++ x11/rofi/files/patch-source_helper.c
@@ -0,0 +1,10 @@
+--- source/helper.c.orig	2015-02-27 14:12:52 UTC
++++ source/helper.c
+@@ -35,6 +35,7 @@
+ #include <fcntl.h>
+ #include <sys/types.h>
+ #include <sys/file.h>
++#include <sys/stat.h>
+ #include "helper.h"
+ #include "rofi.h"
+ 
