92f41f4f0e4edebe659200dea5fc3d0608b34790
antoine <antoine@FreeBSD.org>
2015-04-10 08:45:09 +0000

    - Remove dead function from find-lib.sh - Print a message if dependency registration on a package failed
    With hat:	portmgr

--- Mk/Scripts/find-lib.sh
+++ Mk/Scripts/find-lib.sh
@@ -14,25 +14,6 @@ fi
 lib=$1
 dirs="${LIB_DIRS} `cat ${LOCALBASE}/libdata/ldconfig/* 2>/dev/null || :`"
 
-absolute_path() {
-	local file myifs target
-	file=$1
-
-	myifs=${IFS}
-	IFS='/'
-	set -- ${file}
-	IFS=${myifs}
-	for el; do
-		case $el in
-		.) continue ;;
-		'') continue ;;
-		..) target=${target%/*} ;;
-		*) target="${target}/${el}" ;;
-		esac
-	done
-	echo ${target}
-}
-
 for libdir in ${dirs} ; do
 	test -f ${libdir}/${lib} || continue
 	libfile=${libdir}/${lib}
