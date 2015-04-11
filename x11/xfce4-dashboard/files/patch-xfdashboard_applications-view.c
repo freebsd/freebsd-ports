8a6376763d20e33cf4b614b4dc9f75439eafbc63
Olivier Duchateau <olivierd@FreeBSD.org>
2015-04-11 20:04:41 +0000

    - Update to 0.4.0 - Enhance description - Adjust dependencies
    Changelog: https://mail.xfce.org/pipermail/xfce/2015-April/034400.html

--- x11/xfce4-dashboard/files/patch-xfdashboard_applications-view.c
+++ x11/xfce4-dashboard/files/patch-xfdashboard_applications-view.c
@@ -1,10 +1,10 @@
---- src/applications-view.c.orig	2014-08-26 15:48:00 UTC
-+++ src/applications-view.c
-@@ -949,7 +949,7 @@ static void xfdashboard_applications_vie
+--- xfdashboard/applications-view.c.orig	2015-03-04 05:45:24 UTC
++++ xfdashboard/applications-view.c
+@@ -1374,7 +1374,7 @@ static void xfdashboard_applications_vie
  	/* Set up view */
  	xfdashboard_view_set_internal_name(XFDASHBOARD_VIEW(self), "applications");
  	xfdashboard_view_set_name(XFDASHBOARD_VIEW(self), _("Applications"));
--	xfdashboard_view_set_icon(XFDASHBOARD_VIEW(self), GTK_STOCK_HOME);
+-	xfdashboard_view_set_icon(XFDASHBOARD_VIEW(self), "gtk-home");
 +	xfdashboard_view_set_icon(XFDASHBOARD_VIEW(self), "go-home");
  
  	/* Set up actor */
