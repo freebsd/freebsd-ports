d27bb9fcc4166300755f9c9b32168004d487d297
Jan Beich <jbeich@FreeBSD.org>
2015-04-13 13:43:47 +0000

    Add new port games/openbor
    PR:		196657
    Reviewed by:	games (danfe, partial of earlier version)
    Approved by:	bapt (mentor, earlier version)
    
    OpenBOR is a continuation of the Beats Of Rage 2D game engine, which
    was originally created by Senile Team.
    
    WWW: http://www.chronocrash.com

---
+++ games/openbor/files/patch-sdl__sdlport.c
@@ -0,0 +1,30 @@
+--- sdl/sdlport.c.orig	2013-12-29 14:05:10 UTC
++++ sdl/sdlport.c
+@@ -6,6 +6,11 @@
+  * Copyright (c) 2004 - 2014 OpenBOR Team
+  */
+ 
++#include <errno.h>
++#include <unistd.h>
++#include <err.h>
++#include <sys/stat.h>
++
+ #include "sdlport.h"
+ #include "packfile.h"
+ #include "ram.h"
+@@ -92,6 +97,15 @@ int main(int argc, char *argv[])
+ #ifdef ANDROID
+ 	dirExists(rootDir, 1);
+     chdir(rootDir);
++#else
++	{
++		if (chdir(getenv("HOME")) != 0)
++			err(1, "cannot cd to $HOME");
++		if (mkdir(".openbor", 0755) != 0 && errno != EEXIST)
++			err(1, "cannot mkdir $HOME/.openbor");
++		if (chdir(".openbor") != 0)
++			err(1, "cannot cd to $HOME/.openbor");
++	}
+ #endif
+ 	dirExists(paksDir, 1);
+ 	dirExists(savesDir, 1);
