8d59fb12c4b0bb779ec33770ed67dc635f26c3ad
riggs <riggs@FreeBSD.org>
2015-04-14 19:25:03 +0000

    MFH: r383192
    - Update to new upstream snapshot as of 2015-04-03
      (included ffmpeg snapshot as of 2015-04-03)
    - Fix vulnerabilities CVE-2014-8544 and CVE-2014-9604
      with bundled ffmpeg snapshot in the process
    
    Approved by:	ports-secteam (delphij)

--- multimedia/mplayer/files/patch-libavformat-udp.c
+++ multimedia/mplayer/files/patch-libavformat-udp.c
@@ -1,11 +1,11 @@
---- ffmpeg/libavformat/udp.c.orig	2011-03-16 13:49:21.000000000 +0100
-+++ ffmpeg/libavformat/udp.c	2011-03-18 17:18:12.884305376 +0100
-@@ -42,6 +42,8 @@
- #define IPV6_DROP_MEMBERSHIP IPV6_LEAVE_GROUP
- #endif
+--- ffmpeg/libavformat/udp.c.orig	2015-03-30 19:20:06.000000000 +0200
++++ ffmpeg/libavformat/udp.c	2015-04-03 13:29:34.228251670 +0200
+@@ -71,6 +71,8 @@
+ #define UDP_MAX_PKT_SIZE 65536
+ #define UDP_HEADER_SIZE 8
  
 +#define IPPROTO_IPV6 41
 +
- typedef struct {
+ typedef struct UDPContext {
+     const AVClass *class;
      int udp_fd;
-     int ttl;
