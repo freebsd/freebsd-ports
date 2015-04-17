5539e948efcbcf2507198cc01f3db2cf9115331d
jbeich <jbeich@FreeBSD.org>
2015-04-17 23:06:02 +0000

    MFH: r384194
    - Update to 0.10.1a
    
    PR:		199162
    Submitted by:	Marco Bröder (maintainer)
    Security:	https://vuxml.freebsd.org/freebsd/e426eda9-dae1-11e4-8107-94de806b0af9.html
    Approved by:	portmgr (erwin)

--- devel/dulwich/files/patch-setup.py
+++ devel/dulwich/files/patch-setup.py
@@ -1,20 +1,11 @@
---- ./setup.py.orig	2014-06-07 04:12:55.000000000 +0200
-+++ ./setup.py	2014-06-14 11:16:37.182700482 +0200
-@@ -51,8 +51,6 @@
- 
- setup_kwargs = {}
- 
--if has_setuptools:
--    setup_kwargs['test_suite'] = 'dulwich.tests.test_suite'
- 
- setup(name='dulwich',
-       description='Python Git Library',
-@@ -72,7 +70,7 @@
+--- setup.py.orig	2015-03-25 01:54:01 UTC
++++ setup.py
+@@ -75,7 +75,7 @@ setup(name='dulwich',
        The project is named after the part of London that Mr. and Mrs. Git live in
        in the particular Monty Python sketch.
        """,
 -      packages=['dulwich', 'dulwich.tests', 'dulwich.tests.compat', 'dulwich.contrib'],
 +      packages=['dulwich', 'dulwich.contrib'],
-       scripts=['bin/dulwich', 'bin/dul-web', 'bin/dul-receive-pack', 'bin/dul-upload-pack'],
-       ext_modules=[
-           Extension('dulwich._objects', ['dulwich/_objects.c'],
+       package_data={'': ['../docs/tutorial/*.txt']},
+       scripts=['bin/dulwich', 'bin/dul-receive-pack', 'bin/dul-upload-pack'],
+       classifiers=[
