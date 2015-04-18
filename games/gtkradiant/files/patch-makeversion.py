c3b4f43e44fc33956e72fa7cda281ab333a70084
Alexey Dokuchaev <danfe@FreeBSD.org>
2015-04-18 16:29:01 +0000

    - Fix the build against modern libpng (do not try to access private data) - Clang insists that reference cannot be bound to dereferenced null pointer   in well-defined C++ code (that is correct) and evaluates comparisons like   &foo == 0 to false, which breaks GtkRadiant; "fix" this with a dirty hack   by casting those "bad" references to a local volatile int variable - Remove no longer required and no-op -lpthread vs. -pthread patch hunk - Report operating system and correct compiler version in the About dialog - When fixing annoying "GtkSpinButton: setting an adjustment with non-zero   page size is deprecated" warnings, replace find(1) with grep(1) to narrow   down initial search results and, subsequently, sed(1) furiousness - Omit port revision bump as the port was BROKEN for quite some time

--- games/gtkradiant/files/patch-makeversion.py
+++ games/gtkradiant/files/patch-makeversion.py
@@ -1,20 +1,22 @@
 --- ./makeversion.py.orig	Sun Feb 12 16:47:01 2006
 +++ ./makeversion.py	Thu Mar 16 16:09:46 2006
-@@ -39,8 +39,6 @@
+@@ -37,9 +37,7 @@
+ # ouput:
+ #   include/aboutmsg.h
  
- import sys, re, string, os
- 
--import svn
+-import sys, re, string, os
 -
+-import svn
++import sys, re, string, os, platform
+ 
  def get_version():
    # version
-   f = open('include/version.default', 'r')
-@@ -82,7 +80,7 @@ def radiant_makeversion(append_about):
+@@ -82,7 +80,7 @@
      line = f.readline()
      f.close()
    else:
 -    line = "Custom build based on revision " + str(svn.getRevision(os.getcwd()))
-+    line = "Custom build based on revision 1.0"
++    line = "Custom build for %s (%s)" % (platform.system(), platform.machine())
    # optional additional message
    if ( not append_about is None ):
      line += append_about
