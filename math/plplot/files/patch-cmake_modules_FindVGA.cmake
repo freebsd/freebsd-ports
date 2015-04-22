779c36f5c4677e26ad544b72a061151c7019f23a
Thierry Thomas <thierry@FreeBSD.org>
2015-04-22 20:16:04 +0000

    - Upgrade math/plplot to 5.11.0   Announcement at   <https://sourceforge.net/p/plplot/plplot/ci/plplot-5.11.0/tree/README.release>
    - Take maintainership
    
    - Chase dependencies.
    
    PR:		ports/199539
    Submitted by:	Ben Woods <woodsb02 (at) gmail.com>
    Approved by:	marino (for plplot-ada), /me (for gnudatalanguage)

--- math/plplot/files/patch-cmake_modules_FindVGA.cmake
+++ math/plplot/files/patch-cmake_modules_FindVGA.cmake
@@ -1,5 +1,5 @@
---- cmake/modules/FindVGA.cmake.orig	2009-09-06 21:04:43.000000000 +0200
-+++ cmake/modules/FindVGA.cmake	2009-12-25 20:27:51.000000000 +0100
+--- cmake/modules/FindVGA.cmake.orig	2015-04-12 10:08:04 UTC
++++ cmake/modules/FindVGA.cmake
 @@ -6,7 +6,6 @@
  #  VGA_LIBRARIES, the libraries to link against to use the vga library
  #  VGA_LIBRARY_DIRS, the directory where the vga library is found.
@@ -8,7 +8,7 @@
  find_path(VGA_INCLUDE_DIR vga.h /usr/local/include /usr/include)
  
  if(VGA_INCLUDE_DIR)
-@@ -33,4 +32,3 @@
+@@ -33,4 +32,3 @@ else(VGA_FOUND)
      message(FATAL_ERROR "FindVGA: Could not find vga.h and/or the vga library")
    endif(VGA_FIND_REQUIRED)
  endif(VGA_FOUND)
--- math/plplot/files/patch-cmake_modules_csiro.cmake
+++ math/plplot/files/patch-cmake_modules_csiro.cmake
@@ -1,6 +1,6 @@
---- cmake/modules/csiro.cmake.orig	2009-09-06 21:04:43.000000000 +0200
-+++ cmake/modules/csiro.cmake	2009-12-30 21:10:58.000000000 +0100
-@@ -36,6 +36,10 @@
+--- cmake/modules/csiro.cmake.orig	2015-04-12 10:08:04 UTC
++++ cmake/modules/csiro.cmake
+@@ -36,6 +36,10 @@ if(PL_HAVE_QHULL OR WITH_CSA)
        else(CMAKE_C_COMPILER MATCHES "gcc")
          set(NAN_CFLAGS "${NAN_CFLAGS} -ieee")
        endif(CMAKE_C_COMPILER MATCHES "gcc")
