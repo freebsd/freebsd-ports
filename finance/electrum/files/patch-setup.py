df7adcaabf68362f014c4e9c2a79b5942c432fae
Dmitry Marakasov <amdmi3@FreeBSD.org>
2015-04-22 00:25:27 +0000

    - Update to 2.0.4
    PR:		198575
    Submitted by:	kyuupichan@gmail.com (maintainer)

--- finance/electrum/files/patch-setup.py
+++ finance/electrum/files/patch-setup.py
@@ -1,13 +1,11 @@
---- setup.py.orig	2014-03-16 14:01:35.000000000 +0100
-+++ setup.py	2014-06-03 11:12:40.621312285 +0200
-@@ -15,9 +15,7 @@
- if sys.version_info[:3] < (2, 6, 0):
-     sys.exit("Error: Electrum requires Python version >= 2.6.0...")
- 
--usr_share = '/usr/share'
--if not os.access(usr_share, os.W_OK):
--    usr_share = os.getenv("XDG_DATA_HOME", os.path.join(os.getenv("HOME"), ".local", "share"))
-+usr_share = '%%PREFIX%%/share'
+--- setup.py.orig	2015-03-14 13:27:54 UTC
++++ setup.py
+@@ -18,7 +18,7 @@ if sys.version_info[:3] < (2, 7, 0):
  
  data_files = []
- if (len(sys.argv) > 1 and (sys.argv[1] == "sdist")) or (platform.system() != 'Windows' and platform.system() != 'Darwin'):
+ if platform.system() == 'Linux' or platform.system() == 'FreeBSD':
+-    usr_share = os.path.join(sys.prefix, "share")
++    usr_share = '%%PREFIX%%/share'
+     data_files += [
+         (os.path.join(usr_share, 'applications/'), ['electrum.desktop']),
+         (os.path.join(usr_share, 'app-install', 'icons/'), ['icons/electrum.png'])
