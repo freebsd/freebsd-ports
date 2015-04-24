dd5be421699b7832722d149ece571020182ef885
Brad Davis <brd@FreeBSD.org>
2015-04-24 03:07:55 +0000

    Update cacti to 0.8.8c.
    PR:		198586
    Submitted by:	Chris Petrik <chris@bsdjunk.com>
    Approved by:	bdrewery (mentor)

--- net-mgmt/cacti/files/patch-include__global_settings.php
+++ net-mgmt/cacti/files/patch-include__global_settings.php
@@ -1,14 +1,11 @@
---- ./include/global_settings.php.orig	2013-08-06 19:31:19.000000000 -0700
-+++ ./include/global_settings.php	2014-08-11 18:37:08.000000000 -0700
-@@ -96,9 +96,9 @@
+--- include/global_settings.php.bak	2015-03-12 03:08:59.333297709 -0700
++++ include/global_settings.php	2015-03-12 03:10:47.532302704 -0700
+@@ -96,7 +96,7 @@
  			),
  		"path_cactilog" => array(
  			"friendly_name" => "Cacti Log File Path",
 -			"description" => "The path to your Cacti log file (if blank, defaults to <path_cacti>/log/cacti.log)",
 +			"description" => "The path to your Cacti log file (if blank, defaults to /var/log/cacti/log)",
  			"method" => "filepath",
--			"default" => $config["base_path"] . "/log/cacti.log",
-+			"default" => "/var/log/cacti/log",
+ 			"default" => $config["base_path"] . "/log/cacti.log",
  			"max_length" => "255"
- 			),
- 		"pollerpaths_header" => array(
