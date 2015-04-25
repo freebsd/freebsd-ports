f73231bc24e622f49695f222f9a67d9314dcbed7
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2015-04-25 07:45:17 +0000

    - Fix build without NTLM
    Reference:	https://github.com/bagder/curl/commit/fd9d3a1ef1f7b1cb5812d04bad07818efc6f3b3a

--- ftp/curl/files/patch-lib-url.c
+++ ftp/curl/files/patch-lib-url.c
@@ -1,11 +1,15 @@
+[1]
 Description: Different handling of signals and threads.
 Forwarded: not-needed
 Author: Peter Pentchev <roam@FreeBSD.org>
 Last-Update: 2010-12-18
 
---- lib/url.c.orig	2012-01-23 23:31:30.000000000 +0800
-+++ lib/url.c	2012-02-26 17:52:21.937920303 +0800
-@@ -797,6 +797,10 @@
+[2]
+Reference:	https://github.com/bagder/curl/commit/fd9d3a1ef1f7b1cb5812d04bad07818efc6f3b3a.patch
+
+--- lib/url.c.orig	2015-04-22 05:55:54 UTC
++++ lib/url.c
+@@ -658,6 +658,10 @@ CURLcode Curl_open(struct SessionHandle 
      data->progress.flags |= PGRS_HIDE;
      data->state.current_speed = -1; /* init to negative == impossible */
  
@@ -16,3 +20,35 @@ Last-Update: 2010-12-18
      data->wildcard.state = CURLWC_INIT;
      data->wildcard.filelist = NULL;
      data->set.fnmatch = ZERO_NULL;
+@@ -3069,9 +3073,11 @@ ConnectionExists(struct SessionHandle *d
+   struct connectdata *check;
+   struct connectdata *chosen = 0;
+   bool canPipeline = IsPipeliningPossible(data, needle);
++#ifdef USE_NTLM
+   bool wantNTLMhttp = ((data->state.authhost.want & CURLAUTH_NTLM) ||
+                        (data->state.authhost.want & CURLAUTH_NTLM_WB)) &&
+     (needle->handler->protocol & PROTO_FAMILY_HTTP) ? TRUE : FALSE;
++#endif
+   struct connectbundle *bundle;
+ 
+   *force_reuse = FALSE;
+@@ -3208,6 +3214,7 @@ ConnectionExists(struct SessionHandle *d
+           continue;
+       }
+ 
++#if defined(USE_NTLM)
+       if((!(needle->handler->flags & PROTOPT_CREDSPERREQUEST)) ||
+          (wantNTLMhttp || check->ntlm.state != NTLMSTATE_NONE)) {
+         /* This protocol requires credentials per connection or is HTTP+NTLM,
+@@ -3217,10 +3224,9 @@ ConnectionExists(struct SessionHandle *d
+           /* one of them was different */
+           continue;
+         }
+-#if defined(USE_NTLM)
+         credentialsMatch = TRUE;
+-#endif
+       }
++#endif
+ 
+       if(!needle->bits.httpproxy || needle->handler->flags&PROTOPT_SSL ||
+          (needle->bits.httpproxy && check->bits.httpproxy &&
