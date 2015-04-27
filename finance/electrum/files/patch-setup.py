eb25564403d7385242b3d270dd5d76c4f66e9dcd
John Marino <marino@FreeBSD.org>
2015-04-27 21:04:12 +0000

    finance/electrum: Unbreak on DragonFly (staging)
    Since the setup.py patch would never get sent upstream anyway, I
    modified the patch to install the desktop icons on every platform
    that ports supports unconditionally rather than check the OSNAME
    against a list.  Issue was introduced on last commit
    
    Approved by:	blanket (restore DF functionality)

--- finance/electrum/files/patch-setup.py
+++ finance/electrum/files/patch-setup.py
@@ -1,10 +1,12 @@
 --- setup.py.orig	2015-03-14 13:27:54 UTC
 +++ setup.py
-@@ -18,7 +18,7 @@ if sys.version_info[:3] < (2, 7, 0):
+@@ -17,8 +17,8 @@ if sys.version_info[:3] < (2, 7, 0):
+ 
  
  data_files = []
- if platform.system() == 'Linux' or platform.system() == 'FreeBSD':
+-if platform.system() == 'Linux' or platform.system() == 'FreeBSD':
 -    usr_share = os.path.join(sys.prefix, "share")
++if True:
 +    usr_share = '%%PREFIX%%/share'
      data_files += [
          (os.path.join(usr_share, 'applications/'), ['electrum.desktop']),
