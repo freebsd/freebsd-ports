e477c4d0fd444096276c821188d0d4a77ea7603b
madpilot <madpilot@FreeBSD.org>
2015-04-29 06:47:37 +0000

    Fix build after const removal from iconv.
    Reported by:	ajtiM <lumiwa at gmail.com> (via email)

--- games/0ad/files/patch-build__premake__extern_libs4.lua
+++ games/0ad/files/patch-build__premake__extern_libs4.lua
@@ -1,11 +1,20 @@
---- build/premake/extern_libs4.lua.orig	2014-09-24 10:05:27 UTC
+--- build/premake/extern_libs4.lua.orig	2015-02-20 05:52:18 UTC
 +++ build/premake/extern_libs4.lua
-@@ -237,7 +237,7 @@
+@@ -237,7 +237,7 @@ extern_lib_defs = {
  			end
  			-- TODO: This actually applies to most libraries we use on BSDs, make this a global setting.
  			if os.is("bsd") then
 -				includedirs { "/usr/local/include" }
 +				includedirs { "%%LOCALBASE%%/include" }
+ 			end
+ 		end,
+ 		link_settings = function()
+@@ -346,8 +346,6 @@ extern_lib_defs = {
+ 			elseif os.is("macosx") then
+ 				add_default_include_paths("iconv")
+ 				defines { "LIBICONV_STATIC" }
+-			elseif os.getversion().description == "FreeBSD" then
+-				defines { "HAVE_ICONV_CONST" }
  			end
  		end,
  		link_settings = function()
