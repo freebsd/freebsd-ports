cee4fd5452052a7506ff7b8f483419e4aee8a642
Jimmy Olgeni <olgeni@FreeBSD.org>
2015-05-02 18:59:17 +0000

    Upgrade to version 6.1.1.

--- lang/racket-minimal/files/patch-racket_src_bool.c
+++ lang/racket-minimal/files/patch-racket_src_bool.c
@@ -3,7 +3,7 @@ $FreeBSD$
 
 --- racket/src/bool.c.orig
 +++ racket/src/bool.c
-@@ -198,6 +198,9 @@
+@@ -203,6 +203,9 @@
  }
  
  #ifdef MZ_LONG_DOUBLE
@@ -13,7 +13,7 @@ $FreeBSD$
  XFORM_NONGCING static MZ_INLINE int mz_long_double_eqv(long_double a, long_double b)
  {
  # ifndef NAN_EQUALS_ANYTHING
-@@ -233,6 +236,13 @@
+@@ -238,6 +241,13 @@
    return 1;
  # endif
  }
@@ -27,7 +27,7 @@ $FreeBSD$
  #endif
  XFORM_NONGCING static MZ_INLINE int double_eqv(double a, double b)
  {
-@@ -269,7 +279,13 @@
+@@ -274,7 +284,13 @@
    return 1;
  # endif
  }
@@ -41,7 +41,18 @@ $FreeBSD$
  XFORM_NONGCING static int is_eqv(Scheme_Object *obj1, Scheme_Object *obj2)
  {
    Scheme_Type t1, t2;
-@@ -317,6 +333,9 @@
+@@ -323,12 +339,24 @@
+     }
+   }
+ }
++#ifdef MZ_XFORM
++END_XFORM_SKIP;
++#endif
+ 
++#ifdef MZ_XFORM
++START_XFORM_SKIP;
++#endif
+ int scheme_eqv (Scheme_Object *obj1, Scheme_Object *obj2)
  {
    return (is_eqv(obj1, obj2) > 0);
  }
@@ -49,5 +60,19 @@ $FreeBSD$
 +END_XFORM_SKIP;
 +#endif
  
- int scheme_equal (Scheme_Object *obj1, Scheme_Object *obj2)
++#ifdef MZ_XFORM
++START_XFORM_SKIP;
++#endif
+ XFORM_NONGCING int is_fast_equal (Scheme_Object *obj1, Scheme_Object *obj2, int for_chaperone)
+ {
+   Scheme_Type t1, t2;
+@@ -414,6 +442,9 @@
+ 
+  return -1;
+ }
++#ifdef MZ_XFORM
++END_XFORM_SKIP;
++#endif
+ 
+ int is_slow_equal (Scheme_Object *obj1, Scheme_Object *obj2)
  {
