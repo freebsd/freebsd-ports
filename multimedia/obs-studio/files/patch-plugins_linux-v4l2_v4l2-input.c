ce66670b266f4fae91b0c1682d8af8d32bb40e55
Kris Moore <kris@pcbsd.org>
2015-05-02 09:35:17 -0400

    Update patches from upstream

--- multimedia/obs-studio/files/patch-plugins_linux-v4l2_v4l2-input.c
+++ multimedia/obs-studio/files/patch-plugins_linux-v4l2_v4l2-input.c
@@ -1,92 +1,112 @@
 --- plugins/linux-v4l2/v4l2-input.c.orig	2015-03-27 21:29:37 UTC
 +++ plugins/linux-v4l2/v4l2-input.c
-@@ -264,7 +264,11 @@ static void v4l2_device_list(obs_propert
- 	size_t cur_device_index;
- 	const char *cur_device_name;
+@@ -41,6 +41,12 @@ along with this program.  If not, see <h
+ #include "v4l2-udev.h"
+ #endif
  
-+#ifdef __FreeBSD__
-+	dirp = opendir("/dev");
-+#else
- 	dirp = opendir("/sys/class/video4linux");
-+#endif
- 	if (!dirp)
- 		return;
- 
-@@ -280,6 +284,11 @@ static void v4l2_device_list(obs_propert
- 		uint32_t caps;
- 		struct v4l2_capability video_cap;
- 
-+#ifdef __FreeBSD__
-+		if ( strstr(dp->d_name, "video") == NULL)
-+			continue;
++/* The new dv timing api was introduced in Linux 3.4
++ * Currently we simply disable dv timings when this is not defined */
++#if !defined(VIDIOC_ENUM_DV_TIMINGS) || !defined(V4L2_IN_CAP_DV_TIMINGS)
++#define V4L2_IN_CAP_DV_TIMINGS 0
 +#endif
 +
- 		if (dp->d_type == DT_DIR)
+ #define V4L2_DATA(voidptr) struct v4l2_data *data = voidptr;
+ 
+ #define timeval2ns(tv) \
+@@ -74,7 +80,6 @@ struct v4l2_data {
+ 	obs_source_t *source;
+ 	pthread_t thread;
+ 	os_event_t *event;
+-	void *udev;
+ 
+ 	int_fast32_t dev;
+ 	int width;
+@@ -298,9 +303,14 @@ static void v4l2_device_list(obs_propert
  			continue;
+ 		}
  
-@@ -596,7 +605,11 @@ static bool format_selected(obs_properti
- 	uint32_t caps = 0;
- 	if (v4l2_get_input_caps(dev, input, &caps) < 0)
- 		return false;
-+#ifdef __FreeBSD__
-+	caps &= V4L2_IN_CAP_STD | V4L2_IN_CAP_CUSTOM_TIMINGS;
++#ifndef V4L2_CAP_DEVICE_CAPS
++		caps = video_cap.capabilities;
 +#else
- 	caps &= V4L2_IN_CAP_STD | V4L2_IN_CAP_DV_TIMINGS;
++		/* ... since Linux 3.3 */
+ 		caps = (video_cap.capabilities & V4L2_CAP_DEVICE_CAPS)
+ 			? video_cap.device_caps
+ 			: video_cap.capabilities;
 +#endif
  
- 	obs_property_t *resolution = obs_properties_get(props, "resolution");
- 	obs_property_t *framerate  = obs_properties_get(props, "framerate");
-@@ -607,8 +620,13 @@ static bool format_selected(obs_properti
- 	obs_property_set_visible(framerate,  (!caps) ? true : false);
- 	obs_property_set_visible(standard,
- 			(caps & V4L2_IN_CAP_STD) ? true : false);
-+#ifdef __FreeBSD__
-+	obs_property_set_visible(dv_timing,
-+			(caps & V4L2_IN_CAP_CUSTOM_TIMINGS) ? true : false);
-+#else
- 	obs_property_set_visible(dv_timing,
- 			(caps & V4L2_IN_CAP_DV_TIMINGS) ? true : false);
-+#endif
+ 		if (!(caps & V4L2_CAP_VIDEO_CAPTURE)) {
+ 			blog(LOG_INFO, "%s seems to not support video capture",
+@@ -663,12 +673,15 @@ static bool resolution_selected(obs_prop
+  * If everything went fine we can start capturing again when the device is
+  * reconnected
+  */
+-static void device_added(const char *dev, void *vptr)
++static void device_added(void *vptr, calldata_t *calldata)
+ {
+ 	V4L2_DATA(vptr);
  
- 	if (!caps) {
- 		v4l2_resolution_list(dev, obs_data_get_int(
-@@ -616,8 +634,13 @@ static bool format_selected(obs_properti
- 	}
- 	if (caps & V4L2_IN_CAP_STD)
- 		v4l2_standard_list(dev, standard);
-+#ifdef __FreeBSD__
-+	if (caps & V4L2_IN_CAP_CUSTOM_TIMINGS)
-+		v4l2_dv_timing_list(dev, dv_timing);
-+#else
- 	if (caps & V4L2_IN_CAP_DV_TIMINGS)
- 		v4l2_dv_timing_list(dev, dv_timing);
-+#endif
+ 	obs_source_update_properties(data->source);
  
- 	v4l2_close(dev);
++	const char *dev;
++	calldata_get_string(calldata, "device", &dev);
++
+ 	if (strcmp(data->device_id, dev))
+ 		return;
  
-@@ -625,8 +648,13 @@ static bool format_selected(obs_properti
- 		obs_property_modified(resolution, settings);
- 	if (caps & V4L2_IN_CAP_STD)
- 		obs_property_modified(standard, settings);
-+#ifdef __FreeBSD__
-+	if (caps & V4L2_IN_CAP_CUSTOM_TIMINGS)
-+		obs_property_modified(dv_timing, settings);
-+#else
- 	if (caps & V4L2_IN_CAP_DV_TIMINGS)
- 		obs_property_modified(dv_timing, settings);
-+#endif
+@@ -681,12 +694,15 @@ static void device_added(const char *dev
+  *
+  * We stop recording here so we don't block the device node
+  */
+-static void device_removed(const char *dev, void *vptr)
++static void device_removed(void *vptr, calldata_t *calldata)
+ {
+ 	V4L2_DATA(vptr);
  
- 	return true;
- }
-@@ -828,7 +856,11 @@ static void v4l2_init(struct v4l2_data *
- 		data->framerate  = -1;
- 	}
- 	/* set dv timing if supported */
-+#ifdef __FreeBSD__
-+	if (input_caps & V4L2_IN_CAP_CUSTOM_TIMINGS) {
-+#else
- 	if (input_caps & V4L2_IN_CAP_DV_TIMINGS) {
+ 	obs_source_update_properties(data->source);
+ 
++	const char *dev;
++	calldata_get_string(calldata, "device", &dev);
++
+ 	if (strcmp(data->device_id, dev))
+ 		return;
+ 
+@@ -779,7 +795,12 @@ static void v4l2_destroy(void *vptr)
+ 		bfree(data->device_id);
+ 
+ #if HAVE_UDEV
+-	v4l2_unref_udev(data->udev);
++	signal_handler_t *sh = v4l2_get_udev_signalhandler();
++
++	signal_handler_disconnect(sh, "device_added", device_added, data);
++	signal_handler_disconnect(sh, "device_removed", device_removed, data);
++
++	v4l2_unref_udev();
+ #endif
+ 
+ 	bfree(data);
+@@ -924,12 +945,22 @@ static void *v4l2_create(obs_data_t *set
+ 	data->dev = -1;
+ 	data->source = source;
+ 
++	/* Bitch about build problems ... */
++#ifndef V4L2_CAP_DEVICE_CAPS
++	blog(LOG_WARNING, "Plugin built without device caps support!");
 +#endif
- 		if (v4l2_set_dv_timing(data->dev, &data->dv_timing) < 0) {
- 			blog(LOG_ERROR, "Unable to set dv timing");
- 			goto fail;
++#if !defined(VIDIOC_ENUM_DV_TIMINGS) || !defined(V4L2_IN_CAP_DV_TIMINGS)
++	blog(LOG_WARNING, "Plugin built without dv-timing support!");
++#endif
++
+ 	v4l2_update(data, settings);
+ 
+ #if HAVE_UDEV
+-	data->udev = v4l2_init_udev();
+-	v4l2_set_device_added_callback(data->udev, &device_added, data);
+-	v4l2_set_device_removed_callback(data->udev, &device_removed, data);
++	v4l2_init_udev();
++	signal_handler_t *sh = v4l2_get_udev_signalhandler();
++
++	signal_handler_connect(sh, "device_added", &device_added, data);
++	signal_handler_connect(sh, "device_removed", &device_removed, data);
+ #endif
+ 
+ 	return data;
