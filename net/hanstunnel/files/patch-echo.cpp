e5d2e2c92a7f26314b134ec4182b876d48f64d1c
jbeich <jbeich@FreeBSD.org>
2015-05-02 19:33:12 +0000

    Back out part of r385209 related to bug 195313
    Not bumping PORTEPOCH because I'll eventually land those changes, anyway.
    
    PR:		199723

--- net/hanstunnel/files/patch-echo.cpp
+++ net/hanstunnel/files/patch-echo.cpp
@@ -1,10 +1,9 @@
---- src/echo.cpp.orig	2014-02-07 03:36:31 UTC
-+++ src/echo.cpp
-@@ -19,7 +19,7 @@
- 
+--- echo.cpp.orig	2014-08-06 10:11:40.000000000 -0400
++++ echo.cpp	2014-08-06 10:11:56.000000000 -0400
+@@ -20,6 +20,7 @@
  #include "echo.h"
  #include "exception.h"
--
+ 
 +#include <sys/types.h>
  #include <sys/socket.h>
  #include <netinet/in_systm.h>
