94b4d68eaef9aa4835a22dac58454caa76b866c4
Bryan Drewery <bdrewery@FreeBSD.org>
2015-05-04 22:02:29 +0000

    USE_GITHUB: Treat 'V' special just as 'v' is for DISTVERSIONPREFIX.
    Fixes security/libzrtpcppcore after r385420.
    
    Reported by:	antoine
    With hat:	portmgr

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -557,7 +557,7 @@ DISTNAME:=	${DISTNAME}_GH${_GITHUB_REV}
 GH_TAGNAME_SANITIZED=	${GH_TAGNAME:S,/,-,}
 # Github silently converts tags starting with v to not have v in the filename
 # and extraction directory.
-GH_TAGNAME_EXTRACT=	${GH_TAGNAME_SANITIZED:C/^v([0-9])/\1/}
+GH_TAGNAME_EXTRACT=	${GH_TAGNAME_SANITIZED:C/^[vV]([0-9])/\1/}
 .  endif
 .endif
 .endif
