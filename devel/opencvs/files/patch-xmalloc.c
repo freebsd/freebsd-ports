92124815b9b458cebd49df1002faeb78027eaddc
Johan van Selst <johans@FreeBSD.org>
2015-05-04 17:15:19 +0000

    - Update to 1 May version of opencvs - Redo patches to please portlint

--- devel/opencvs/files/patch-xmalloc.c
+++ devel/opencvs/files/patch-xmalloc.c
@@ -1,11 +1,10 @@
---- xmalloc.c.orig	2009-06-07 10:39:13.000000000 +0200
-+++ xmalloc.c	2010-06-08 20:18:08.000000000 +0200
-@@ -13,6 +13,8 @@
-  * called by a name other than "ssh" or "Secure Shell".
-  */
- 
-+#include <sys/types.h>
-+
- #include <limits.h>
+--- xmalloc.c.orig	2015-05-04 09:15:45 UTC
++++ xmalloc.c
+@@ -17,6 +17,7 @@
  #include <stdio.h>
  #include <stdlib.h>
+ #include <string.h>
++#include <sys/types.h>
+ 
+ #include "log.h"
+ #include "xmalloc.h"
