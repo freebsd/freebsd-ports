a59b0332ae82c31256be4a137e20a35ec2761aed
wen <wen@FreeBSD.org>
2015-05-05 06:08:41 +0000

    - Update to 2.1.1
    PR:		199679
    Submitted by:	kyuupichan@gmail.com(maintainer)

--- finance/electrum/files/patch-setup.py
+++ finance/electrum/files/patch-setup.py
@@ -1,13 +1,11 @@
---- setup.py.orig	2015-03-14 13:27:54 UTC
+--- setup.py.orig	2015-04-25 02:55:24 UTC
 +++ setup.py
-@@ -17,8 +17,8 @@ if sys.version_info[:3] < (2, 7, 0):
- 
+@@ -18,7 +18,7 @@ if sys.version_info[:3] < (2, 7, 0):
  
  data_files = []
--if platform.system() == 'Linux' or platform.system() == 'FreeBSD':
+ if platform.system() in [ 'Linux', 'FreeBSD', 'DragonFly']:
 -    usr_share = os.path.join(sys.prefix, "share")
-+if True:
 +    usr_share = '%%PREFIX%%/share'
      data_files += [
          (os.path.join(usr_share, 'applications/'), ['electrum.desktop']),
-         (os.path.join(usr_share, 'app-install', 'icons/'), ['icons/electrum.png'])
+         (os.path.join(usr_share, 'pixmaps/'), ['icons/electrum.png'])
