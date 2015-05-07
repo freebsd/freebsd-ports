408e0509b80e454ee008293ba238f2d0faeea6dc
olgeni <olgeni@FreeBSD.org>
2015-05-07 08:18:11 +0000

    Upgrade to 0.2.0 and fix linking with libhiredis.
    PR:		199997
    Submitted by:	olgeni
    Approved by:	maintainer

--- databases/py-hiredis/files/patch-setup.py
+++ databases/py-hiredis/files/patch-setup.py
@@ -1,16 +1,17 @@
---- ./setup.py.orig	2014-01-06 19:08:27.000000000 +0400
-+++ ./setup.py	2014-01-07 15:36:44.000000000 +0400
-@@ -41,7 +41,8 @@
+--- setup.py.orig	2015-04-03 14:08:45 UTC
++++ setup.py
+@@ -45,7 +45,9 @@ lib = ("hiredis_for_hiredis_py", {
  
  ext = Extension("hiredis.hiredis",
    sources=glob.glob("src/*.c"),
 -  include_dirs=["vendor"])
 +  include_dirs=["%%LOCALBASE%%/include"],
-+  library_dirs=["%%LOCALBASE%%/lib"],)
++  library_dirs=["%%LOCALBASE%%/lib"],
++  libraries=["hiredis"],)
  
  setup(
    name="hiredis",
-@@ -53,7 +54,6 @@
+@@ -57,7 +59,6 @@ setup(
    keywords=["Redis"],
    license="BSD",
    packages=["hiredis"],
