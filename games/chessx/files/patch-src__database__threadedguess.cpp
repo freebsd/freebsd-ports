d4fa7bf2b6bf46c670468f80bb62f3421ec3e609
Bartek Rutkowski <robak@FreeBSD.org>
2015-05-09 22:34:38 +0000

    games/chessx: update 1.2.2 -> 1.3.2
    PR:		199988
    Submitted by:	Yamagi <yamagi@yamagi.org> (maintainer)

---
+++ games/chessx/files/patch-src__database__threadedguess.cpp
@@ -0,0 +1,10 @@
+--- src/database/threadedguess.cpp_	2015-05-06 10:23:39.570986205 +0200
++++ src/database/threadedguess.cpp	2015-05-06 10:24:33.855963312 +0200
+@@ -2,6 +2,7 @@
+ *   Copyright (C) 2014 by Jens Nissen jens-chessx@gmx.net                   *
+ ****************************************************************************/
+ 
++#include <QMetaType>
+ #include "threadedguess.h"
+ #include "guess.h"
+ 
