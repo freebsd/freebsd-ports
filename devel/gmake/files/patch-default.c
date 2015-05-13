0c38e7085f315b847326bf0dd82b4b714a030802
shurd <shurd@FreeBSD.org>
2015-05-13 16:19:37 +0000

    Use c++ as default CXX rather than g++. (Patch was lost in upgrade to 4.1)
    PR:		185436
    Approved by:	bapt

---
+++ devel/gmake/files/patch-default.c
@@ -0,0 +1,11 @@
+--- default.c.orig	2015-05-13 16:16:24 UTC
++++ default.c
+@@ -447,7 +447,7 @@ static const char *default_variables[] =
+     "OBJC", "gcc",
+ #else
+     "CC", "cc",
+-    "CXX", "g++",
++    "CXX", "c++",
+     "OBJC", "cc",
+ #endif
+ 
