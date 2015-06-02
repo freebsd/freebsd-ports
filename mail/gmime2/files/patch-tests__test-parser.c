13c746b80aaec9016c6c8d135fb0f695d6827a04
bofh <bofh@FreeBSD.org>
2015-06-02 10:32:04 +0000

    mail/gmime2: Add PORTSCOUT variable to limit within 2.2.X
    - Fix patch files to reflect 'make makepatch'

--- mail/gmime2/files/patch-tests__test-parser.c
+++ mail/gmime2/files/patch-tests__test-parser.c
@@ -1,6 +1,6 @@
---- tests/test-parser.c.orig	Mon Feb 12 15:48:38 2007
-+++ tests/test-parser.c	Mon Feb 12 15:49:14 2007
-@@ -26,6 +26,7 @@
+--- tests/test-parser.c.orig	2011-03-07 18:09:21 UTC
++++ tests/test-parser.c
+@@ -30,6 +30,7 @@
  #include <fcntl.h>
  #include <time.h>
  
