2d993801e1d455e69984eca36acc1a8656043fe8
madpilot <madpilot@FreeBSD.org>
2015-06-05 23:26:11 +0000

    - Update to 1.82 - Convert to USES gnustep and unbreak - Regenerate patches to silence portlint warnings
    PR:		200496
    Submitted by:	lightside at gmx.com

--- games/oolite/files/patch-src_Core_OOPointMaths.h
+++ games/oolite/files/patch-src_Core_OOPointMaths.h
@@ -1,5 +1,5 @@
---- src/Core/OOPointMaths.h.orig	2014-06-30 12:50:36.000000000 +0400
-+++ src/Core/OOPointMaths.h	2014-10-16 13:51:36.000000000 +0400
+--- src/Core/OOPointMaths.h.orig	2014-06-30 08:50:36 UTC
++++ src/Core/OOPointMaths.h
 @@ -1,5 +1,6 @@
  #import "OOFunctionAttributes.h"
 -#include <tgmath.h>
