083f736ad35a8f8cc68d3e30ec81016bac274225
garga <garga@FreeBSD.org>
2015-06-09 09:51:07 +0000

    Update to 5.3.2
    PR:		200721
    Approved by:	strongswan@Nanoteq.com (maintainer)
    MFH:		2015Q2
    Security:	CVE-2015-3991
    Sponsored by:	Netgate

---
+++ security/strongswan/files/patch-src_starter_starterstroke.c
@@ -0,0 +1,10 @@
+--- src/starter/starterstroke.c.orig	2015-06-09 09:46:26 UTC
++++ src/starter/starterstroke.c
+@@ -16,6 +16,7 @@
+ 
+ #include <unistd.h>
+ #include <stdlib.h>
++#include <stdint.h>
+ #include <string.h>
+ 
+ #include <credentials/auth_cfg.h>
