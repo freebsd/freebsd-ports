1105eb3f69b560a826c13f05686ecc784754d7fa
bofh <bofh@FreeBSD.org>
2015-06-10 14:04:06 +0000

    print/py-reportlab: Fix PORTDOCS
    - Strip Libraries
    - Fix patch files to reflect 'make makepatch'
    
    PR:             200602
    Submitted by:   amdmi3@FreeBSD.org

--- print/py-reportlab/files/patch-docs-genAll.py
+++ print/py-reportlab/files/patch-docs-genAll.py
@@ -1,5 +1,5 @@
---- docs/genAll.py.orig	2014-02-14 23:22:37.000000000 +0900
-+++ docs/genAll.py	2014-02-16 16:20:21.000000000 +0900
+--- docs/genAll.py.orig	2014-02-14 14:22:37 UTC
++++ docs/genAll.py
 @@ -1,6 +1,7 @@
  #!/bin/env python
 -import os, sys, traceback
