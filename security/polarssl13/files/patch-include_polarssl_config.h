315902aa22ce81ddb0cf56b44aa6d5373143cf02
tijl <tijl@FreeBSD.org>
2015-06-12 16:14:54 +0000

    - Update security/polarssl13 to 1.3.11 - Patch a Makefile so regular make install works - Replace a patch with MAKE_ENV - Bump dependent ports
    PR:		200816

--- security/polarssl13/files/patch-include_polarssl_config.h
+++
@@ -1,9 +0,0 @@
---- include/polarssl/config.h.bak	2014-04-11 22:33:59.000000000 +0900
-+++ include/polarssl/config.h	2014-04-30 17:36:41.000000000 +0900
-@@ -2383,4 +2383,6 @@
- #error "POLARSSL_X509_CSR_WRITE_C defined, but not all prerequisites"
- #endif
- 
-+#define SHOW_PROTOTYPE_CHANGE_WARNINGS
-+
- #endif /* config.h */
