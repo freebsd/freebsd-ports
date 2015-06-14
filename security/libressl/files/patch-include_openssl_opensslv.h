e1cbff132c9fdc82b5935d2d5ce6937e4dbca775
Bernard Spil <brnrd@FreeBSD.org>
2015-06-14 13:28:26 +0000

    security/libressl: Update to 2.2.0
      - Update to 2.2.0
      - Remove opensslfeatures.h patch (included upstream)
      - Add pkg-plist (mainly documentation)
      - Bump libcrypto SHLIB version in Mk/bsd.openssl.mk
    
    Changes:
    
      http://marc.info/?l=openbsd-announce&m=143404058913441
    
    Reviewed_by:	vsevolod, koobs
    Approved by:	vsevolod (maintainer, mentor), koobs (mentor)
    Security:	8305e215-1080-11e5-8ba2-000c2980a9f3
    Differential revision:	D2770
    MFH:		2015Q2

--- security/libressl/files/patch-include_openssl_opensslv.h
+++ security/libressl/files/patch-include_openssl_opensslv.h
@@ -1,11 +1,11 @@
---- include/openssl/opensslv.h.orig	2015-03-08 23:21:45 UTC
+--- include/openssl/opensslv.h.orig	2015-05-23 21:50:16 UTC
 +++ include/openssl/opensslv.h
 @@ -3,7 +3,7 @@
  #define HEADER_OPENSSLV_H
  
  #define LIBRESSL_VERSION_NUMBER	0x20000000L
 -#define OPENSSL_VERSION_NUMBER	0x20000000L
-+#define OPENSSL_VERSION_NUMBER 	0x1000107fL
- #define OPENSSL_VERSION_TEXT	"LibreSSL 2.1.7"
++#define OPENSSL_VERSION_NUMBER	0x1000107fL
+ #define OPENSSL_VERSION_TEXT	"LibreSSL 2.2.0"
  #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
  
