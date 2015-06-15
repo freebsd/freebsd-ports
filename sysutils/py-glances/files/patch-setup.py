1d398fd23115fce20a95e0f8b4e7d57c1a009446
Dmitry Marakasov <amdmi3@FreeBSD.org>
2015-06-15 23:31:06 +0000

    - Update to 2.4.2 - Remove NLS support (has been removed from upstream) - Add LICENSE_FILE
    PR:		200877
    Submitted by:	gasol.wu@gmail.com

--- sysutils/py-glances/files/patch-setup.py
+++ sysutils/py-glances/files/patch-setup.py
@@ -1,6 +1,6 @@
---- ./setup.py.orig	2014-06-16 10:56:37.000000000 +0800
-+++ ./setup.py	2014-06-16 10:57:02.000000000 +0800
-@@ -11,8 +11,7 @@
+--- setup.py.orig	2015-06-15 02:42:15 UTC
++++ setup.py
+@@ -15,8 +15,7 @@ def get_data_files():
      data_files = [
          ('share/doc/glances', ['AUTHORS', 'COPYING', 'NEWS', 'README.rst',
                                 'conf/glances.conf', 'docs/glances-doc.html']),
@@ -9,15 +9,4 @@
 +        ('share/doc/glances/images', glob.glob('docs/images/*.png'))
      ]
  
-     if hasattr(sys, 'real_prefix') or 'bsd' in sys.platform:
-@@ -23,10 +22,6 @@
-         conf_path = os.path.join('/usr/local', 'etc', 'glances')
-     elif 'win32' in sys.platform:
-         conf_path = os.path.join(os.environ.get('APPDATA'), 'glances')
--    data_files.append((conf_path, ['conf/glances.conf']))
--
--    for mo in glob.glob('i18n/*/LC_MESSAGES/*.mo'):
--        data_files.append((os.path.dirname(mo).replace('i18n/', 'share/locale/'), [mo]))
- 
      return data_files
- 
