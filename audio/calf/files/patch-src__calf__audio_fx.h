8110d1df1dcd615eb41be3d4ca019c83bc666a69
wen <wen@FreeBSD.org>
2015-06-20 09:24:26 +0000

    - Update to 0.0.60
    PR:		199207
    Submitted by:	tkato432@yahoo.com

---
+++ audio/calf/files/patch-src__calf__audio_fx.h
@@ -0,0 +1,10 @@
+--- src/calf/audio_fx.h.orig
++++ src/calf/audio_fx.h
+@@ -27,6 +27,7 @@
+ #include "inertia.h"
+ #include "giface.h"
+ #include "onepole.h"
++#include <sys/types.h>
+ #include <complex>
+ 
+ namespace calf_plugins {
