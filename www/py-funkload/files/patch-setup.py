f3864e77eebbac60e290768f6e6175af4694c1e0
rm <rm@FreeBSD.org>
2015-06-23 18:12:46 +0000

    www/py-funkload: update to 1.17.1
    - update to 1.17.1
    - remove bogus py-xml dependency
    - shorten COMMENT
    - limit python version to 2.x
    - update patch to setup.py to make it build (there is upstream issue [1])
    
    [1] https://github.com/nuxeo/FunkLoad/issues/145
    
    Approved by:	lwhsu (maintainer, by email)

--- www/py-funkload/files/patch-setup.py
+++ www/py-funkload/files/patch-setup.py
@@ -1,12 +1,20 @@
---- setup.py.orig	2010-01-30 02:16:39.051847143 +0800
-+++ setup.py	2010-01-30 02:16:50.065356904 +0800
-@@ -149,9 +149,6 @@
-         'Topic :: System :: Monitoring',
-     ],
-     # setuptools specific keywords
--    install_requires = ['webunit  >= 1.3.8',
--                        'docutils >= 0.3.7',
--                        'setuptools'],
-     zip_safe=True,
-     package_data={'funkload': ['data/*',
-                                'demo/simple/*', 'demo/zope/*',
+--- setup.py.orig	2015-05-06 10:37:24 UTC
++++ setup.py
+@@ -21,8 +21,6 @@
+ """FunkLoad package setup
+ 
+ """
+-import ez_setup
+-ez_setup.use_setuptools()
+ from setuptools import setup, find_packages
+ __version__ = '1.17.1'
+ 
+@@ -30,7 +28,7 @@ setup(
+     name="funkload",
+     version=__version__,
+     description="Functional and load web tester.",
+-    long_description=''.join(open('README.txt').readlines()),
++    long_description='',
+     author="Benoit Delbosc",
+     author_email="bdelbosc@nuxeo.com",
+     url="http://funkload.nuxeo.org/",
