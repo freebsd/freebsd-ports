797c6bc3a41bfaec4ae76a023995f1785e4748c6
marino <marino@FreeBSD.org>
2015-06-23 10:57:16 +0000

    Uses/ada.mk: "47" is no longer a valid argument
    The lang/gcc47-aux compiler was removed last week, so it is being
    removed as a build option for the Ada framework.

--- Mk/Uses/ada.mk
+++ Mk/Uses/ada.mk
@@ -6,7 +6,7 @@
 #
 # Feature:      ada
 # Usage:        USES=ada
-# Valid ARGS:   47, 49, 5
+# Valid ARGS:   49, 5
 #
 # MAINTAINER: marino@FreeBSD.org
 
@@ -15,9 +15,7 @@ _INCLUDE_USES_ADA_MK=    yes
 
 CC= ada
 
-. if ${ada_ARGS:M47}
-ADAXX=	gcc47
-. elif ${ada_ARGS:M49}
+. if ${ada_ARGS:M49}
 ADAXX=	gcc
 . elif ${ada_ARGS:M5}
 ADAXX=	gcc5
