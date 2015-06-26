3d4cdb8e2f1441d2769671d9bca69de441dd0196
Dmitry Marakasov <amdmi3@FreeBSD.org>
2015-06-26 17:48:08 +0000

    - This uses c++11 features (for example, std::to_string), so add corresponding compiler requirements and flags - Add patches to fix missing includes (couldn't find any upstream contants to submit these) - Unfortunately, it's still BROKEN on 9.x as gcc 4.8's stdlibc++ doesn't seem to provide to_string, so mark it as such
    Submitted by:	pkg-fallout
    Approved by:	portmgr blanket

---
+++ ports-mgmt/portrac/files/patch-PortChecker.cc
@@ -0,0 +1,11 @@
+--- PortChecker.cc.orig	2014-09-29 18:19:21 UTC
++++ PortChecker.cc
+@@ -31,6 +31,8 @@
+ #include <string>
+ #include <algorithm>
+ #include <iostream>
++#include <cstdio>
++#include <cerrno>
+ 
+ #include <QStringList>
+ 
