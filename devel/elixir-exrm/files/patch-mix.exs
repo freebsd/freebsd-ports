c7b08070be2e3668771df19f5912e2373455f70c
olgeni <olgeni@FreeBSD.org>
2015-07-01 13:50:16 +0000

    Upgrade to version 0.17.3.

--- devel/elixir-exrm/files/patch-mix.exs
+++ devel/elixir-exrm/files/patch-mix.exs
@@ -1,11 +1,11 @@
---- mix.exs.orig	2015-06-30 07:38:34 UTC
+--- mix.exs.orig	2015-06-30 19:58:41 UTC
 +++ mix.exs
 @@ -13,10 +13,7 @@ defmodule ReleaseManager.Mixfile do
    def application, do: []
  
    def deps do
 -    [{:conform, "~> 0.14.5"},
--     {:relx, "~> 2.1.1" },
+-     {:relx, "~> 3.1.0" },
 -     {:earmark, "~> 0.1", only: :dev},
 -     {:ex_doc, "~> 0.5", only: :dev}]
 +    []
