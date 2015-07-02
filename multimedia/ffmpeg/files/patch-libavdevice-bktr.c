a93a44040e7685a22cc30a55359822a5d2a1d610
Thomas Zander <riggs@FreeBSD.org>
2015-07-02 19:40:15 +0000

    - Update multimdedia/ffmpeg to latest upstrem version 2.7.1 - Mark ports BROKEN that were discovered not to work with   this update; discovered by exp-run in bug 201181 - Note the need for manual upgrade of dependent ports in   UPDATING

--- multimedia/ffmpeg/files/patch-libavdevice-bktr.c
+++ multimedia/ffmpeg/files/patch-libavdevice-bktr.c
@@ -1,5 +1,5 @@
---- libavdevice/bktr.c.orig	2012-01-22 14:41:15.000000000 +0100
-+++ libavdevice/bktr.c	2012-01-22 16:07:07.873280412 +0100
+--- libavdevice/bktr.c.orig	2015-06-19 20:44:50 UTC
++++ libavdevice/bktr.c
 @@ -24,6 +24,11 @@
   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
   */
@@ -10,5 +10,5 @@
 +typedef unsigned long u_long;
 +
  #include "libavformat/internal.h"
+ #include "libavutil/internal.h"
  #include "libavutil/log.h"
- #include "libavutil/opt.h"
