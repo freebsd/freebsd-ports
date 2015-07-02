ba9cb24566062ed2049db13e080c21b98c75e8dd
olgeni <olgeni@FreeBSD.org>
2015-07-02 23:14:16 +0000

    Add devel/elixir-tzdata, time zone database for Elixir.

---
+++ devel/elixir-tzdata/files/patch-mix.exs
@@ -0,0 +1,11 @@
+--- mix.exs.orig	2015-07-02 22:52:47 UTC
++++ mix.exs
+@@ -17,8 +17,6 @@ defmodule Tzdata.Mixfile do
+ 
+   defp deps do
+     [
+-      {:earmark, "~> 0.1", only: :dev},
+-      {:ex_doc, "~> 0.7", only: :dev},
+     ]
+   end
+ 
