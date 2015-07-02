df4ffa656bb382eeec820fc626b02ff1d76c0d98
olgeni <olgeni@FreeBSD.org>
2015-07-02 23:19:52 +0000

    Add devel/elixir-timex-interval, a date/time interval library for Elixir.

---
+++ devel/elixir-timex-interval/files/patch-mix.exs
@@ -0,0 +1,11 @@
+--- mix.exs.orig	2015-07-02 23:05:16 UTC
++++ mix.exs
+@@ -12,7 +12,7 @@ defmodule TimexInterval.Mixfile do
+   end
+ 
+   defp deps do
+-    [ {:timex, "~> 0.13.1"} ]
++    []
+   end
+ 
+   defp package do
