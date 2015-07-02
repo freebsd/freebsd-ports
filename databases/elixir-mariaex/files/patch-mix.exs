2a0a791082d8617923cc958ce1deea4088148233
Jimmy Olgeni <olgeni@FreeBSD.org>
2015-07-02 13:11:54 +0000

    Add databases/elixir-mariaex, a database driver for MariaDB and MySQL written in pure Elixir.

---
+++ databases/elixir-mariaex/files/patch-mix.exs
@@ -0,0 +1,11 @@
+--- mix.exs.orig	2015-07-02 11:00:37 UTC
++++ mix.exs
+@@ -18,7 +18,7 @@ defmodule Mariaex.Mixfile do
+   end
+ 
+   defp deps do
+-    [{:decimal, "~> 1.0"}]
++    []
+   end
+ 
+   defp description do
