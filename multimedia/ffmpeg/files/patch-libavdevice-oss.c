c2783499b2a7eb36c5b79ac4a9fb033a292dc737
riggs <riggs@FreeBSD.org>
2015-07-02 19:40:15 +0000

    - Update multimdedia/ffmpeg to latest upstrem version 2.7.1 - Mark ports BROKEN that were discovered not to work with   this update; discovered by exp-run in bug 201181 - Note the need for manual upgrade of dependent ports in   UPDATING

--- multimedia/ffmpeg/files/patch-libavdevice-oss.c
+++ multimedia/ffmpeg/files/patch-libavdevice-oss.c
@@ -1,5 +1,5 @@
---- libavdevice/oss_audio.c.orig	2009-05-15 17:38:35.000000000 +0200
-+++ libavdevice/oss_audio.c	2009-05-15 17:41:27.000000000 +0200
+--- libavdevice/oss.c.orig	2015-06-19 20:44:50 UTC
++++ libavdevice/oss.c
 @@ -19,6 +19,11 @@
   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
   */
@@ -10,5 +10,5 @@
 +typedef unsigned long	u_long;
 +
  #include "config.h"
- #include <stdlib.h>
- #include <stdio.h>
+ 
+ #include <string.h>
