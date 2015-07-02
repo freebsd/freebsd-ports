847da878169c72f8afe130eaba19daa13a98ebbf
Jimmy Olgeni <olgeni@FreeBSD.org>
2015-07-02 13:28:16 +0000

    Add devel/elixir-lager_logger, a Lager backend for Elixir's Logger.

---
+++ devel/elixir-lager_logger/files/patch-mix.exs
@@ -0,0 +1,10 @@
+--- mix.exs.orig	2015-07-02 13:24:56 UTC
++++ mix.exs
+@@ -30,7 +30,6 @@ defmodule LagerLogger.Mixfile do
+ 
+   defp deps do
+     [
+-      {:lager, ">= 2.1.0"},
+     ]
+   end
+ end
