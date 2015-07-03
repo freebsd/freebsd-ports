ab37b19f25816b645070a375f43f046be36b6dfc
Jimmy Olgeni <olgeni@FreeBSD.org>
2015-07-03 09:34:33 +0000

    Add devel/elixir-exjsx, a JSON library for Elixir.

---
+++ devel/elixir-exjsx/files/patch-mix.exs
@@ -0,0 +1,11 @@
+--- mix.exs.orig	2015-07-03 09:30:54 UTC
++++ mix.exs
+@@ -19,7 +19,7 @@ defmodule EXJSX.Mixfile do
+   end
+ 
+   defp deps do
+-    [{:jsx, "~> 2.4.0"}]
++    []
+   end
+   
+   defp description do
