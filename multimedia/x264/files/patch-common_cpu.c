9b08af5e1a546b78fe93f99c39a534eeca7acf11
Kubilay Kocak <koobs@FreeBSD.org>
2015-07-03 07:02:44 +0000

    multimedia/{lib}x264: Update to 0.144.2533 and major refactor
    - Update to 0.144.2533 (from upstream stable branch) [1]
    - Switch x264 and libx264 ports to master/slave ports respectively
    - Bump PORTREVISION for dependent ports as the shared library version
      has changed.
    
    multimedia/x264:
    
    - Clean up and update "updating instructions" in Makefile header
    - Tweak COMMENT
    - Only LIB_DEPENDS on libx264 and add PLIST_FILES if this isn't the
      libx264 port.
    - Allow COMMENT, USES, OPTIONS_*, CONFIGURE_ARGS to be overriden in
      libx264 port.
    - Place common configure arguments in a CONFIGURE_COMMON_ARGS variable
      to make overriding values without duplication easier.
    - Use --prefix configure argument over post-patch replacements
    - Enable stripping of binaries and libraries if DEBUG is off. Remove
      post-install target STRIP_CMD accordingly.
    
    - Delete patch-Makefile in favour of patching WRKSRC/configure to
      identify amd64, arm64 and mipsn32. powerpc and powerpc64 are now
      covered upstream without needing patches.
    
    - Patch out a bogus compiler argument check (cc_check) that results
      in -Wno-maybe-uninitialized being added to CFLAGS causing causes
      warnings when clang is cc. The cc_check function checks for basename
      $CC to identify compiler type (icl, clang, gcc, etc).
    
    multimedia/libx264:
    
    - Remove all Makefile entries that are duplicated or common and found
      in the master port (x264).
    - Set lib as the PKGNAMEPREFIX
    - Tweak COMMENT
    - Delete upstreamed ARM patches [1]
    - Delete all but one patch, as they duplicate those in x264.
    - Dont remove pkgconfig/libdata directory
    
    Changes:
    
      https://git.videolan.org/?p=x264.git;a=shortlog;h=refs/heads/stable
    
    Based on:
    
    PR:		201260 [1]
    Submitted by:	Andrey Cherkashin <andoriyu gmail com> [1]

--- multimedia/x264/files/patch-common_cpu.c
+++ multimedia/x264/files/patch-common_cpu.c
@@ -1,6 +1,6 @@
---- common/cpu.c.orig	2014-08-27 20:45:08 UTC
+--- common/cpu.c.orig	2015-06-29 20:45:09 UTC
 +++ common/cpu.c
-@@ -313,7 +313,7 @@ uint32_t x264_cpu_detect( void )
+@@ -316,7 +316,7 @@ uint32_t x264_cpu_detect( void )
  
  #elif ARCH_PPC
  
@@ -9,7 +9,7 @@
  #include <sys/sysctl.h>
  uint32_t x264_cpu_detect( void )
  {
-@@ -321,12 +321,16 @@ uint32_t x264_cpu_detect( void )
+@@ -324,12 +324,16 @@ uint32_t x264_cpu_detect( void )
      uint32_t cpu = 0;
  #if SYS_OPENBSD
      int      selectors[2] = { CTL_MACHDEP, CPU_ALTIVEC };
