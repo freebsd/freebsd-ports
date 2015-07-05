589716e421b6394db8feb4b0649fc7d85baf740d
Jimmy Olgeni <olgeni@FreeBSD.org>
2015-07-05 07:39:14 +0000

    Add devel/elixir-calendar, a date and time library for Elixir.

---
+++ devel/elixir-calendar/files/patch-mix.exs
@@ -0,0 +1,12 @@
+--- mix.exs.orig	2015-07-05 07:36:59 UTC
++++ mix.exs
+@@ -7,8 +7,7 @@ defmodule Calendar.Mixfile do
+      version: "0.6.9",
+      elixir: "~> 1.1.0 or ~> 1.0.0",
+      package: package,
+-     description: description,
+-     deps: deps]
++     description: description]
+   end
+ 
+   def application do
