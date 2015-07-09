afcb58572a1772c00557546339d45d8cc6342a71
hrs <hrs@FreeBSD.org>
2015-07-09 21:12:03 +0000

    - Add japanese/uim-mozc, uim plugin for mozc input method[*]. - Fix XML file for ibus engine. - Fix stop_server() in mozc startup script.
    PR:			199258 [*]
    Based on work by:	Koichiro IWAO [*]

--- japanese/mozc-server/files/patch-unix_ibus_gen_mozc_xml.py
+++ japanese/mozc-server/files/patch-unix_ibus_gen_mozc_xml.py
@@ -1,6 +1,32 @@
---- unix/ibus/gen_mozc_xml.py.orig	2015-02-17 18:05:39.000000000 +0900
-+++ unix/ibus/gen_mozc_xml.py	2015-02-17 18:06:30.000000000 +0900
-@@ -203,13 +203,19 @@
+--- unix/ibus/gen_mozc_xml.py.orig	2015-02-15 04:18:31.000000000 +0900
++++ unix/ibus/gen_mozc_xml.py	2015-02-22 02:00:23.000000000 +0900
+@@ -67,7 +67,7 @@
+ # Information to generate <engines> part of mozc.xml for IBus 1.5 or later.
+ IBUS_1_5_ENGINE_COMMON_PROPS = {
+     'description': '%(product_name)s (Japanese Input Method)',
+-    'language': 'ja',
++    'language': 'jpn',
+     'icon': '%(ibus_mozc_icon_path)s',
+     'rank': '80',
+     'symbol': '&#x3042;',
+@@ -91,7 +91,7 @@
+         # unix/ibus/mozc_engine.cc.
+         'name': ['mozc-jp'],
+         'longname': ['%(product_name)s'],
+-        'layout': ['default'],
++        'layout': ['jp'],
+     },
+ }
+ 
+@@ -129,6 +129,7 @@
+     engines: A dictionary from a property name to a list of property values of
+         engines. For example, {'name': ['mozc-jp', 'mozc', 'mozc-dv']}.
+   """
++  print '<?xml version="1.0" encoding="utf-8"?>'
+   print '<component>'
+   for key in component:
+     OutputXmlElement(param_dict, key, component[key])
+@@ -203,13 +204,19 @@
    parser.add_option('--server_dir', dest='server_dir', default='',
                      help='The absolute directory path to be installed the '
                      'server executable.')
