28cb05e16bca60410f95c4b408205b5439ad078d
olgeni <olgeni@FreeBSD.org>
2015-07-09 05:12:51 +0000

    Upgrade to version 0.4.0.

--- www/elixir-maru/files/patch-mix.exs
+++ www/elixir-maru/files/patch-mix.exs
@@ -1,14 +1,10 @@
---- mix.exs.orig	2015-07-02 12:23:34 UTC
+--- mix.exs.orig	2015-07-09 05:06:08 UTC
 +++ mix.exs
-@@ -19,10 +19,7 @@ defmodule Maru.Mixfile do
-   end
- 
-   defp deps do
--    [ { :cowboy, "~> 1.0.0" },
--      { :plug,   "~> 0.12.2" },
--      { :poison, "~> 1.4.0" },
--    ]
-+    []
-   end
- 
-   defp package do
+@@ -5,7 +5,6 @@ defmodule Maru.Mixfile do
+     [ app: :maru,
+       version: "0.4.0",
+       elixir: "~> 1.0.0",
+-      deps: deps,
+       description: "Elixir copy of grape for creating REST-like APIs.",
+       source_url: "https://github.com/falood/maru",
+       package: package,
