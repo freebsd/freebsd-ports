31aa8fcaa98514fe370c4505b7f46ab26d19ae06
Rene Ladan <rene@FreeBSD.org>
2015-07-12 10:03:55 +0000

    www/chromium: update to 43.0.2357.132
    Modify the UI code for button labels to be more consistent with the UI found
    in Linux versions of Chromium.
    
    Modify the process code to use kqueue to wait on non-child processes, as is
    done on OSX, rather than doing the default which is nothing.
    
    Submitted by:	LeFroid
    Obtained from:	https://github.com/gliaskos/freebsd-chromium/pull/22
    MFH:		2015Q3

--- www/chromium/files/patch-base__process__process_posix.cc
+++ www/chromium/files/patch-base__process__process_posix.cc
@@ -1,13 +1,34 @@
 --- base/process/process_posix.cc.orig	2015-05-20 21:26:57.068650000 -0400
 +++ base/process/process_posix.cc	2015-05-20 21:25:28.077153000 -0400
-@@ -17,6 +17,10 @@
+@@ -17,6 +17,13 @@
  #include <sys/event.h>
  #endif
  
 +#if defined(OS_BSD)
 +#include <signal.h>
++#include <sys/types.h>
++#include <sys/event.h>
++#include <sys/time.h>
 +#endif
 +
  namespace {
  
  #if !defined(OS_NACL_NONSFI)
+@@ -83,7 +90,7 @@
+   return ret_pid > 0;
+ }
+ 
+-#if defined(OS_MACOSX)
++#if defined(OS_MACOSX) || defined(OS_BSD)
+ // Using kqueue on Mac so that we can wait on non-child processes.
+ // We can't use kqueues on child processes because we need to reap
+ // our own children using wait.
+@@ -180,7 +187,7 @@
+   base::ProcessHandle parent_pid = base::GetParentProcessId(handle);
+   base::ProcessHandle our_pid = base::GetCurrentProcessHandle();
+   if (parent_pid != our_pid) {
+-#if defined(OS_MACOSX)
++#if defined(OS_MACOSX) || defined(OS_BSD)
+     // On Mac we can wait on non child processes.
+     return WaitForSingleNonChildProcess(handle, timeout);
+ #else
