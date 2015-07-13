1d7d46b23db32151973ffbd4ca7112df8e77f998
stephen <stephen@FreeBSD.org>
2015-07-13 20:00:10 +0000

    - Update to 3.0.0.

---
+++ math/octave-forge-parallel/files/patch-parallel-gnutls.h
@@ -0,0 +1,11 @@
+--- parallel-gnutls.h.orig	2015-07-12 11:10:04 UTC
++++ parallel-gnutls.h
+@@ -41,7 +41,7 @@ along with this program; If not, see <ht
+ 
+ #include <stdio.h>
+ 
+-#include <malloc.h>
++#include <stdlib.h>
+ 
+ /*
+ not necessary with current Octave
