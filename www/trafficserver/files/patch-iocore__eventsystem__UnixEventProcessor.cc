95da35a090c4248608594fced3b9c5fa4e04e4f4
vanilla <vanilla@FreeBSD.org>
2015-07-13 14:00:55 +0000

    Upgrade to 5.3.1.
    PR:		200114
    Submitted by:	maintainer

--- www/trafficserver/files/patch-iocore__eventsystem__UnixEventProcessor.cc
+++ www/trafficserver/files/patch-iocore__eventsystem__UnixEventProcessor.cc
@@ -1,7 +1,7 @@
---- iocore/eventsystem/UnixEventProcessor.cc.orig	2014-07-07 00:33:37.106586000 +0800
-+++ iocore/eventsystem/UnixEventProcessor.cc	2014-07-07 00:34:18.143512000 +0800
+--- iocore/eventsystem/UnixEventProcessor.cc.orig	2015-06-30 04:11:56 UTC
++++ iocore/eventsystem/UnixEventProcessor.cc
 @@ -24,7 +24,6 @@
- #include "P_EventSystem.h"      /* MAGIC_EDITING_TAG */
+ #include "P_EventSystem.h" /* MAGIC_EDITING_TAG */
  #include <sched.h>
  #if TS_USE_HWLOC
 -#include <alloca.h>
