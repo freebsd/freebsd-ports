d13c5b314302dd0e20784bd97a19f08eda7dbead
Jimmy Olgeni <olgeni@FreeBSD.org>
2015-07-13 09:41:42 +0000

    Add devel/elixir-estree, a JavaScript AST to Javascript code generator in Elixir.

---
+++ devel/elixir-estree/files/patch-mix.exs
@@ -0,0 +1,10 @@
+--- mix.exs.orig	2015-07-13 09:38:15 UTC
++++ mix.exs
+@@ -5,7 +5,6 @@ defmodule ESTree.Mixfile do
+     [app: :estree,
+      version: "2.0.0",
+      elixir: "~> 1.0",
+-     deps: deps,
+      description: description,
+      package: package,
+      source_url: "https://github.com/bryanjos/elixir-estree"]
