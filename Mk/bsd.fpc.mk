40d640ed67bfe417e73824ef344ee1273dae6ccd
bapt <bapt@FreeBSD.org>
2015-07-14 20:16:29 +0000

    The lazarus GTK1 interface has been discontinued upstream: http://wiki.lazarus.freepascal.org/GTK1_Interface

--- Mk/bsd.fpc.mk
+++ Mk/bsd.fpc.mk
@@ -64,8 +64,8 @@ UNITSDIR=		${LOCALBASE}/lib/fpc/${FPC_VER}/units/${BUILDNAME}
 
 _FPC_ALL_UNITS=	a52 aspell bfd bzip2 cairo chm dblib dbus dts fastcgi fcl-async fcl-base \
 		fcl-db fcl-extra fcl-fpcunit fcl-image fcl-js fcl-json fcl-net fcl-passrc \
-		fcl-process fcl-registry fcl-res fcl-web fcl-xml fftw fpgtk fpindexer \
-		fpmkunit fppkg fv gdbint gdbm gmp graph gtk1 gtk2 hash \
+		fcl-process fcl-registry fcl-res fcl-web fcl-xml fftw fpindexer \
+		fpmkunit fppkg fv gdbint gdbm gmp graph gtk2 hash \
 		hermes httpd22 httpd24 ibase iconvenc imagemagick ldap libcurl libgd \
 		libpng libvlc libxml2 lua mad matroska modplug mysql ncurses newt numlib \
 		odbc oggvorbis openal opengl openssl oracle pasjpeg paszlib pcap \
@@ -145,14 +145,12 @@ fcl_xml_UNIT=	devel/fpc-fcl-xml
 fftw_UNIT=	math/fpc-fftw
 fpindexer_UNIT=	databases/fpc-fpindexer
 fpmkunit_UNIT=	devel/fpc-fpmkunit
-fpgtk_UNIT=	graphics/fpc-fpgtk
 fppkg_UNIT=	devel/fpc-fppkg
 fpvectorial_UNIT=	graphics/fpc-fpvectorial
 fv_UNIT=	devel/fpc-fv
 hermes_UNIT=	graphics/fpc-hermes
 gdbm_UNIT=	databases/fpc-gdbm
 gmp_UNIT=	math/fpc-gmp
-gtk1_UNIT=	x11-toolkits/fpc-gtk1
 gtk2_UNIT=	x11-toolkits/fpc-gtk2
 iconvenc_UNIT=	converters/fpc-iconvenc
 imagemagick_UNIT=	graphics/fpc-imagemagick
