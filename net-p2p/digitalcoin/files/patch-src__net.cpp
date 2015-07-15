d5d968974aa0905ca57b5a07a51103721a58f602
pgollucci <pgollucci@FreeBSD.org>
2015-07-15 20:59:13 +0000

    net-p2p/digitalcoin: cleanup
    - Remove Qt build tools from runtime dependencies;
    - Use qt4-linguisttools instead of qt4-linguist;
    - Add LICENSE;
    - Add USES=desktop-file-utils;
    - Wrap lines at 80 characters;
    - Regen patches.
    
    PR:                  201025
    Submitted by:        s3erios@gmail.com

--- net-p2p/digitalcoin/files/patch-src__net.cpp
+++ net-p2p/digitalcoin/files/patch-src__net.cpp
@@ -1,6 +1,6 @@
---- src/net.cpp.orig	2014-01-18 04:31:19.000000000 +0000
-+++ src/net.cpp	2014-01-18 04:32:57.000000000 +0000
-@@ -58,7 +58,7 @@
+--- src/net.cpp.orig	2013-10-13 13:51:25 UTC
++++ src/net.cpp
+@@ -58,7 +58,7 @@ static bool vfReachable[NET_MAX] = {};
  static bool vfLimited[NET_MAX] = {};
  static CNode* pnodeLocalHost = NULL;
  uint64 nLocalHostNonce = 0;
