041ec329ae129696701906f6d40ef4040ff402b3
Philip M. Gollucci <pgollucci@FreeBSD.org>
2015-07-16 18:35:16 +0000

    net-p2p/bitcoin: 0.10.2 -> 0.11.0
    PR:                  201524
    Submitted by:        robbak@gmail.com (maintainer)

--- net-p2p/bitcoin/files/patch-src_leveldb_helpers_memenv_memenv.cc
+++ net-p2p/bitcoin/files/patch-src_leveldb_helpers_memenv_memenv.cc
@@ -1,5 +1,5 @@
---- src/leveldb/helpers/memenv/memenv.cc.orig	2014-12-28 23:05:26.000000000 +1000
-+++ src/leveldb/helpers/memenv/memenv.cc	2014-12-29 17:12:58.000000000 +1000
+--- src/leveldb/helpers/memenv/memenv.cc.orig	2015-07-10 17:23:55 UTC
++++ src/leveldb/helpers/memenv/memenv.cc
 @@ -2,6 +2,8 @@
  // Use of this source code is governed by a BSD-style license that can be
  // found in the LICENSE file. See the AUTHORS file for names of contributors.
