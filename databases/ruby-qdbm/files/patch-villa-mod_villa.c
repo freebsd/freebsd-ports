d3298d0704fa249473e09437ceb482caaa065393
hrs <hrs@FreeBSD.org>
2015-07-18 18:15:17 +0000

    - Take maintainership. - Fix CFLAGS and LDFLAGS. - Fix a possible library path confusion. - Style clean-ups.

--- databases/ruby-qdbm/files/patch-villa-mod_villa.c
+++ databases/ruby-qdbm/files/patch-villa-mod_villa.c
@@ -1,6 +1,6 @@
---- villa/mod_villa.c.orig	2014-03-29 18:35:17.000000000 +0100
-+++ villa/mod_villa.c	2014-03-29 18:37:37.000000000 +0100
-@@ -304,7 +304,7 @@
+--- villa/mod_villa.c.orig	2006-09-25 14:47:04 UTC
++++ villa/mod_villa.c
+@@ -304,7 +304,7 @@ static VALUE rbvlopen(VALUE vself, VALUE
    int index, omode, cmode;
    VLCFUNC cmp;
    if((index = getnewindex()) == -1) myerror(DP_EMISC);
@@ -9,7 +9,7 @@
    FIXNUM_P(vomode);
    omode = FIX2INT(vomode);
    FIXNUM_P(vcmode);
-@@ -352,10 +352,10 @@
+@@ -352,10 +352,10 @@ static VALUE rbvlput(VALUE vself, VALUE 
    int index, ksiz, vsiz, dmode;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
@@ -24,7 +24,7 @@
    FIXNUM_P(vdmode);
    dmode = FIX2INT(vdmode);
    villa = vltable[index];
-@@ -373,8 +373,8 @@
+@@ -373,8 +373,8 @@ static VALUE rbvlout(VALUE vself, VALUE 
    int index, ksiz;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
@@ -35,7 +35,7 @@
    villa = vltable[index];
    if(!vlout(villa, kbuf, ksiz)){
      if(vlsltable[index] && dpecode == DP_ENOITEM) return Qfalse;
-@@ -391,8 +391,8 @@
+@@ -391,8 +391,8 @@ static VALUE rbvlget(VALUE vself, VALUE 
    VALUE vval;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
@@ -46,7 +46,7 @@
    villa = vltable[index];
    if(!(vbuf = vlget(villa, kbuf, ksiz, &vsiz))){
      if(vlsltable[index] && dpecode == DP_ENOITEM) return Qnil;
-@@ -409,8 +409,8 @@
+@@ -409,8 +409,8 @@ static VALUE rbvlvsiz(VALUE vself, VALUE
    int index, ksiz, vsiz;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
@@ -57,7 +57,7 @@
    villa = vltable[index];
    if((vsiz = vlvsiz(villa, kbuf, ksiz)) == -1){
      if(vlsltable[index] && dpecode == DP_ENOITEM) return INT2FIX(-1);
-@@ -426,8 +426,8 @@
+@@ -426,8 +426,8 @@ static VALUE rbvlvnum(VALUE vself, VALUE
    int index, ksiz, vnum;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
@@ -68,7 +68,7 @@
    villa = vltable[index];
    vnum = vlvnum(villa, kbuf, ksiz);
    return INT2FIX(vnum);
-@@ -497,8 +497,8 @@
+@@ -497,8 +497,8 @@ static VALUE rbvlcurjump(VALUE vself, VA
    VALUE vval;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
@@ -79,7 +79,7 @@
    FIXNUM_P(vjmode);
    jmode = FIX2INT(vjmode);
    villa = vltable[index];
-@@ -551,8 +551,8 @@
+@@ -551,8 +551,8 @@ static VALUE rbvlcurput(VALUE vself, VAL
    int index, vsiz, cpmode;
    FIXNUM_P(vindex);
    if((index = FIX2INT(vindex)) == -1) myerror(DP_EMISC);
