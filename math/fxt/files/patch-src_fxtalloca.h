c72ae87b5792876e0f5991b09e14c93d8d564333
pi <pi@FreeBSD.org>
2015-07-18 16:10:39 +0000

    math/fxt: upgrade 2012.06.18 -> 2015.07.17
    A substantial number of algorithms have been added, much in the
    combinatorial section, but also quite a few others.
    Very many routines have been improved (optimizations and
    handling corner cases).
    
    Everything (well, all demos) pass valgrind, so a handful of bugs have
    been squished that are next to impossible to find otherwise.
    
    Apart from that, a real and ongoing effort has been made for readability.

--- math/fxt/files/patch-src_fxtalloca.h
+++ math/fxt/files/patch-src_fxtalloca.h
@@ -1,5 +1,5 @@
---- ./src/fxtalloca.h.orig	2010-10-26 21:49:13.000000000 +0200
-+++ ./src/fxtalloca.h	2010-10-26 21:49:58.000000000 +0200
+--- src/fxtalloca.h.orig	2012-11-21 18:05:08 UTC
++++ src/fxtalloca.h
 @@ -16,13 +16,13 @@
  #if defined  __GNUC__  // GNU compiler
  #define ALLOCA(Type, v, n)  Type v[n]
@@ -13,6 +13,6 @@
  #undef ALLOCA
 -#include <alloca.h>
 +#include <stdlib.h>
+ //#include <stdlib.h>  // for BSD
  #define ALLOCA(Type, v, n)  Type *v = (Type *)alloca((n)*sizeof(Type))
  #endif
- 
