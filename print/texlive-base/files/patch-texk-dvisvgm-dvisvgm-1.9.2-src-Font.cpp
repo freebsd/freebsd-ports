90410e570da38ffd6b5077fafe644b17378e70a9
Hiroki Sato <hrs@FreeBSD.org>
2015-07-19 20:33:34 +0000

    Update TeXLive to TL2015.
    - print/texlive-infra was renamed with print/texlive-tlmgr.
      Note that currently tlmgr(1) does not work because of
      lack of TLPDB.  This will be fixed.
    
    - Lua libraries used in LuaTeX are separated into
      devel/tex-libtexlua and devel/texlibtexluajit.
      USE_TEX supports them via keywords "texlua" and "texluajit".
    
    - libsynctex is currently installed by devel/tex-web2c.
      This will be separated into a single port.
    
    - Add graphics/libpotrace and use installed libpotrace
      and libharfbuzz.

--- print/texlive-base/files/patch-texk-dvisvgm-dvisvgm-1.9.2-src-Font.cpp
+++ print/texlive-base/files/patch-texk-dvisvgm-dvisvgm-1.9.2-src-Font.cpp
@@ -1,5 +1,5 @@
---- texk/dvisvgm/dvisvgm-1.6/src/Font.cpp.orig	2014-04-13 22:09:23.000000000 +0900
-+++ texk/dvisvgm/dvisvgm-1.6/src/Font.cpp	2014-08-20 23:41:16.000000000 +0900
+--- texk/dvisvgm/dvisvgm-1.9.2/src/Font.cpp.orig	2015-03-02 08:48:21 UTC
++++ texk/dvisvgm/dvisvgm-1.9.2/src/Font.cpp
 @@ -19,6 +19,7 @@
  *************************************************************************/
  
--- print/texlive-base/files/patch-texk-dvisvgm-dvisvgm-1.9.2-src-FontEncoding.cpp
+++ print/texlive-base/files/patch-texk-dvisvgm-dvisvgm-1.9.2-src-FontEncoding.cpp
@@ -1,5 +1,5 @@
---- texk/dvisvgm/dvisvgm-1.6/src/FontEncoding.cpp.orig	2014-01-13 17:24:56.000000000 +0900
-+++ texk/dvisvgm/dvisvgm-1.6/src/FontEncoding.cpp	2014-08-20 23:41:52.000000000 +0900
+--- texk/dvisvgm/dvisvgm-1.9.2/src/FontEncoding.cpp.orig	2015-03-02 08:48:21 UTC
++++ texk/dvisvgm/dvisvgm-1.9.2/src/FontEncoding.cpp
 @@ -19,6 +19,7 @@
  *************************************************************************/
  
