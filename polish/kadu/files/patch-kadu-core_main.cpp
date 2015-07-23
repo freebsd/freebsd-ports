25969201477ae6b49814e2e755c2ad97b4dee318
pawel <pawel@FreeBSD.org>
2015-07-23 22:04:49 +0000

    - Update to version 2.1, now Qt5 based - USES=compiler for better handling of needed compiler - USES=libarchive to prevent linking with libarchive from base on 10+

---
+++ polish/kadu/files/patch-kadu-core_main.cpp
@@ -0,0 +1,11 @@
+--- kadu-core/main.cpp.orig	2015-07-20 19:41:44 UTC
++++ kadu-core/main.cpp
+@@ -33,7 +33,7 @@
+ 
+ #include <QtCore/QLibraryInfo>
+ #include <QtCore/QTranslator>
+-#include <QtCrypto/QtCrypto>
++#include <QtCrypto>
+ #include <QtWidgets/QApplication>
+ #include <QtWidgets/QMessageBox>
+ 
