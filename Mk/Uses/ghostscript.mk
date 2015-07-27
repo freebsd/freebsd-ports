fd0c38bf4011f349d96d83fe3dc8a97080ac354e
hrs <hrs@FreeBSD.org>
2015-07-27 01:18:53 +0000

    - Fix -nox11 packages and ghostscript:nox11 in ghostscript.mk.   This also fixes an issue that user-defined OPTIONS_UNSET was overridden.
    - Loosen up depdendency a bit by using a binary instead of a package name.
    
    PR:	201864

--- Mk/Uses/ghostscript.mk
+++ Mk/Uses/ghostscript.mk
@@ -84,19 +84,18 @@ _GS_SELECTED?=		7
 IGNORE?=	Invalid ghostscript argument or GHOSTSCRIPT_DEFAULT
 .endif
 
-.undef _GS_NOX11_SUFFIX
-.if ${_GS_ARGS:Mnox11} || defined(WITHOUT_X11)
-_GS_NOX11_SUFFIX=	-nox11
-.endif
-
 # dependencies
-_GS_PORT=	ghostscript${_GS_SELECTED}${_GS_AGPL_SUFFIX}${_GS_NOX11_SUFFIX}
+_GS_PORT=	ghostscript${_GS_SELECTED}${_GS_AGPL_SUFFIX}
+
+.if ${_GS_ARGS:Mnox11}
+DEPENDS_ARGS+=	print_${_GS_PORT}_UNSET_FORCE+=X11
+.endif
 
 .if defined(_GS_BUILD_DEP)
-BUILD_DEPENDS+=	${_GS_PORT}>=0:${PORTSDIR}/print/${_GS_PORT}
+BUILD_DEPENDS+=	gs:${PORTSDIR}/print/${_GS_PORT}
 .endif
 .if defined(_GS_RUN_DEP)
-RUN_DEPENDS+=	${_GS_PORT}>=0:${PORTSDIR}/print/${_GS_PORT}
+RUN_DEPENDS+=	gs:${PORTSDIR}/print/${_GS_PORT}
 .endif
 
 .endif # _INCLUDE_USES_GHOSTSCRIPT_MK
