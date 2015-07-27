b18513c396f0da554f784695a222d8383acd7127
Koop Mast <kwm@FreeBSD.org>
2015-07-27 20:19:33 +0000

    Update MATE DE to 1.10.0.
    This MATE is still build agains GTK+2.
    Sort USES here and there.
    Replace mate-dialogs with zenity and mate-calc with galculator.
    
    This update fixes the following PR's:
    PR:		193942, 191885
    Submitted by:	Gustau Perez <gustau.perez@gmail.com> via Gnome devel repo
    Obtained from:	gnome devel repo.

--- archivers/engrampa/files/patch-src_fr-command-tar.c
+++ archivers/engrampa/files/patch-src_fr-command-tar.c
@@ -1,12 +1,13 @@
---- src/fr-command-tar.c.orig	Sun Oct 15 12:33:56 2006
-+++ src/fr-command-tar.c	Mon Oct 16 19:18:03 2006
-@@ -231,6 +231,9 @@
- #if defined (__SVR4) && defined (__sun)
- 	if (g_file_test ("/usr/sfw/bin/gtar", G_FILE_TEST_IS_EXECUTABLE))
+--- src/fr-command-tar.c.orig	2014-11-19 15:44:09.000000000 +0100
++++ src/fr-command-tar.c	2014-12-04 18:28:31.998497849 +0100
+@@ -245,6 +245,10 @@
+ 		g_free (command);
  		command = g_strdup ("/usr/sfw/bin/gtar");
+ 	}
 +#elif defined(__FreeBSD__)
 +	if (g_file_test ("%%LOCALBASE%%/bin/gtar", G_FILE_TEST_IS_EXECUTABLE))
 +		command = g_strdup ("%%LOCALBASE%%/bin/gtar");
++
  #endif
  	if (command != NULL)
  		fr_process_begin_command (comm->process, command);
