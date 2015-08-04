999d09cef678efe5fd9ca448382afa859033f493
Don Lewis <truckman@FreeBSD.org>
2015-08-04 22:13:33 +0000

    Various build updates from openoffice-devel (primarily r382334 and r391960):
      * patch-i118574 no longer seems necessary for a clean build
    
      * Regnerate patch-i114430 with make makepatch
    
      * Build using boost from ports instead of the bundled boost
    
      * Build using silgraphite from ports instead of the bundled version
    
      * Add missing LIB_DEPENDS that are brought in by other dependencies,
        but are directly linked as requested by pkgconfig/*.pc
    
      * Build using sane and xrender headers from ports instead of bundled
        headers
    
      * Update LICENSE* to account for bundled software
    
      * Reformat pkg-message and mention that the scanner interface can be
        enabled by installing sane-backends
    
      * Pet portlint
    
    Update patch-bridges*, patch-freebsd.mk, patch-jpeg, patch-nss, patch-sal,
    patch-set_soenv.in, patch-unxfbsd.mk, and patch-webdav to the versions
    accepted upstream, regenerated with make makepatch.
    
    Regenerate patch-CVE-2015-1774 with make makepatch.
    
    Approved by:	mat (mentor, implicit)

--- editors/openoffice-4/files/patch-bridges_source_cpp__uno_gcc3__freebsd__x86-64_uno2cpp.cxx
+++ editors/openoffice-4/files/patch-bridges_source_cpp__uno_gcc3__freebsd__x86-64_uno2cpp.cxx
@@ -1,14 +1,45 @@
---- bridges/source/cpp_uno/gcc3_freebsd_x86-64/uno2cpp.cxx.orig	2014-09-19 10:51:13.000000000 -0700
-+++ bridges/source/cpp_uno/gcc3_freebsd_x86-64/uno2cpp.cxx	2015-03-13 09:48:24.000000000 -0700
-@@ -446,7 +446,11 @@
+--- bridges/source/cpp_uno/gcc3_freebsd_x86-64/uno2cpp.cxx.orig	2014-02-25 08:29:00 UTC
++++ bridges/source/cpp_uno/gcc3_freebsd_x86-64/uno2cpp.cxx
+@@ -24,9 +24,10 @@
+ // MARKER(update_precomp.py): autogen include statement, do not remove
+ #include "precompiled_bridges.hxx"
+ 
+-#include <exception>
+ #include <typeinfo>
+-#include <stdio.h>
++#include <exception>
++#include <cstddef>
++#include <cxxabi.h>
+ #include <stdlib.h>
+ #include <string.h>
+ 
+@@ -47,6 +48,11 @@
+ 
+ using namespace ::rtl;
+ using namespace ::com::sun::star::uno;
++#ifdef __GLIBCXX__
++using CPPU_CURRENT_NAMESPACE::__cxa_get_globals;
++#else
++using __cxxabiv1::__cxa_get_globals;
++#endif
+ 
+ //==================================================================================================
+ static void callVirtualMethod(void * pThis, sal_uInt32 nVtableIndex,
+@@ -407,7 +413,7 @@ static void cpp_call(
+                 Reference< XInterface >());
+         }
+ 
+-		// NO exception occured...
++		// NO exception occurred...
+ 		*ppUnoExc = 0;
+ 		
+ 		// reconvert temporary params
+@@ -446,7 +452,7 @@ static void cpp_call(
   	catch (...)
   	{
    		// fill uno exception
-+#ifndef __GLIBCXX__
+-		fillUnoException( CPPU_CURRENT_NAMESPACE::__cxa_get_globals()->caughtExceptions, *ppUnoExc, pThis->getBridge()->getCpp2Uno() );
 +		CPPU_CURRENT_NAMESPACE::fillUnoException( __cxa_get_globals()->caughtExceptions, *ppUnoExc, pThis->getBridge()->getCpp2Uno() );
-+#else /* __GLIBCXX__ */
- 		fillUnoException( CPPU_CURRENT_NAMESPACE::__cxa_get_globals()->caughtExceptions, *ppUnoExc, pThis->getBridge()->getCpp2Uno() );
-+#endif /* __GLIBCXX__ */
          
  		// temporary params
  		for ( ; nTempIndizes--; )
