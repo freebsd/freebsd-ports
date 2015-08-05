af610daf74ccca5bc98e1762bfb456089cb1e2a2
rene <rene@FreeBSD.org>
2015-08-05 22:06:05 +0000

    Drop support for FreeBSD 8.

--- www/chromium/files/patch-third_party__webrtc__base__thread_checker_impl.cc
+++ www/chromium/files/patch-third_party__webrtc__base__thread_checker_impl.cc
@@ -1,6 +1,6 @@
 --- third_party/webrtc/base/thread_checker_impl.cc.orig	2015-04-19 18:30:59.000000000 +0200
 +++ third_party/webrtc/base/thread_checker_impl.cc	2015-04-19 18:51:43.000000000 +0200
-@@ -14,10 +14,16 @@
+@@ -14,10 +14,15 @@
  
  #include "webrtc/base/checks.h"
  
@@ -11,25 +11,18 @@
  
 +#if defined(WEBRTC_BSD)
 +#include <pthread_np.h>
-+#include <sys/param.h>
 +#include <sys/thr.h>
 +#endif
 +
  namespace rtc {
  
  PlatformThreadId CurrentThreadId() {
-@@ -27,6 +33,14 @@
+@@ -27,6 +32,8 @@
  #elif defined(WEBRTC_POSIX)
  #if defined(WEBRTC_MAC) || defined(WEBRTC_IOS)
    ret = pthread_mach_thread_np(pthread_self());
 +#elif defined(WEBRTC_BSD)
-+#  if __FreeBSD_version < 900031
-+  long lwpid;
-+  thr_self(&lwpid);
-+  ret = static_cast<int>(lwpid);
-+#  else
 +  ret = static_cast<int>(pthread_getthreadid_np());
-+#  endif
  #elif defined(WEBRTC_LINUX)
    ret =  syscall(__NR_gettid);
  #elif defined(WEBRTC_ANDROID)
