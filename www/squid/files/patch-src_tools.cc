9d747aadc2e01fe5e55a6e8e5cdc39374a261a1f
Bartek Rutkowski <robak@FreeBSD.org>
2015-08-05 18:32:18 +0000

    www/squid: update 3.5.6 -> 3.5.7
    - Fix build with ecap by clang
    - Get rid of useless and always empty /var/squid/logs
    - Rework patches to make portlint a bit happier
    
    PR:		202053
    Submitted by:	Pavel Timofeev <timp87@gmail.com> (maintainer)

--- www/squid/files/patch-src_tools.cc
+++ www/squid/files/patch-src_tools.cc
@@ -1,6 +1,6 @@
---- src/tools.cc.orig	2014-08-19 13:38:40.000000000 +0400
-+++ src/tools.cc	2014-08-19 13:39:00.000000000 +0400
-@@ -735,7 +735,7 @@
+--- src/tools.cc.orig	2015-08-01 06:08:17 UTC
++++ src/tools.cc
+@@ -635,7 +635,7 @@ no_suid(void)
      uid = geteuid();
      debugs(21, 3, "no_suid: PID " << getpid() << " giving up root priveleges forever");
  
