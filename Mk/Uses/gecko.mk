38d2f168ce694b50454c27e5fe7417235a269628
jbeich <jbeich@FreeBSD.org>
2015-08-07 12:02:41 +0000

    - Update Firefox to 40.0 - Update Firefox ESR and libxul to 38.2.0
    Changes:	https://www.mozilla.org/firefox/40.0/releasenotes/
    Changes:	https://www.mozilla.org/firefox/38.2.0/releasenotes/
    MFH:		2015Q3

--- Mk/Uses/gecko.mk
+++ Mk/Uses/gecko.mk
@@ -37,12 +37,12 @@ RUN_DEPENDS+=	libxul>=38:${PORTSDIR}/www/libxul
 .elif ${gecko_ARGS:Mfirefox}
 
 _GECKO_DEFAULT_VERSION=	38
-_GECKO_VERSIONS=		38 39
+_GECKO_VERSIONS=		38 40
 _GECKO_TYPE=	firefox
 
 # Dependence lines for different Firefox versions
 38_DEPENDS=		${LOCALBASE}/lib/firefox/firefox:${PORTSDIR}/www/firefox-esr
-39_DEPENDS=		${LOCALBASE}/lib/firefox/firefox:${PORTSDIR}/www/firefox
+40_DEPENDS=		${LOCALBASE}/lib/firefox/firefox:${PORTSDIR}/www/firefox
 
 .if exists(${LOCALBASE}/bin/firefox)
 _GECKO_INSTALLED_VER!=	${LOCALBASE}/bin/firefox --version 2>/dev/null
