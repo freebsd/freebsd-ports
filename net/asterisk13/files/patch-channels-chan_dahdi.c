468bd72e6bc62d8452283d18ce8869143e71f1c4
madpilot <madpilot@FreeBSD.org>
2015-08-08 13:04:49 +0000

    - Update net/asterisk11 to 11.19.0 - Update net/asterisk13 to 13.5.0 - Add SQLITE2 option default description - Rename SQLITE option to SQLITE2, which better describes what it really does - Clean up Makefile and sort option helpers [1] - Add missed dependencies on openssl, ncurses and iconv [1] - Add back shabangfix for sample agi scripts - Make build log verbose [1] - Regenerated some patches
    Suggested by:	koobs (thanks!) [1]

--- net/asterisk13/files/patch-channels-chan_dahdi.c
+++ net/asterisk13/files/patch-channels-chan_dahdi.c
@@ -1,6 +1,6 @@
---- channels/chan_dahdi.c.orig	2014-07-25 16:47:17 UTC
+--- channels/chan_dahdi.c.orig	2015-07-24 22:20:17 UTC
 +++ channels/chan_dahdi.c
-@@ -4596,6 +4596,8 @@ void dahdi_ec_enable(struct dahdi_pvt *p
+@@ -4598,6 +4598,8 @@ void dahdi_ec_enable(struct dahdi_pvt *p
  		return;
  	}
  	if (p->echocancel.head.tap_length) {
@@ -9,7 +9,7 @@
  #if defined(HAVE_PRI) || defined(HAVE_SS7)
  		switch (p->sig) {
  #if defined(HAVE_PRI)
-@@ -4626,7 +4628,9 @@ void dahdi_ec_enable(struct dahdi_pvt *p
+@@ -4628,7 +4630,9 @@ void dahdi_ec_enable(struct dahdi_pvt *p
  			break;
  		}
  #endif	/* defined(HAVE_PRI) || defined(HAVE_SS7) */
@@ -20,7 +20,7 @@
  		if (res) {
  			ast_log(LOG_WARNING, "Unable to enable echo cancellation on channel %d (%s)\n", p->channel, strerror(errno));
  		} else {
-@@ -4660,8 +4664,9 @@ void dahdi_ec_disable(struct dahdi_pvt *
+@@ -4662,8 +4666,9 @@ void dahdi_ec_disable(struct dahdi_pvt *
  
  	if (p->echocanon) {
  		struct dahdi_echocanparams ecp = { .tap_length = 0 };
