cfff850167eb382d78e99b87ddd9d783a9b8e95d
Antoine Brodin <antoine@FreeBSD.org>
2015-08-10 17:06:50 +0000

    Make baselibs QA check non recursive and activate it again
    Reviewed by:	bapt
    Differential Revision:	https://reviews.freebsd.org/D3352

--- Mk/Scripts/qa.sh
+++ Mk/Scripts/qa.sh
@@ -99,19 +99,19 @@ baselibs() {
 	[ "${PKGBASE}" = "pkg" -o "${PKGBASE}" = "pkg-devel" ] && return
 	while read f; do
 		case ${f} in
-		/usr/lib/libarchive*)
-			err "Bad linking on ${f} please add USES=libarchive"
+		*NEEDED*\[libarchive.so.[56]])
+			err "Bad linking on ${f##* } please add USES=libarchive"
 			rc=1
 			;;
-		/lib/libedit*)
-			err "Bad linking on ${f} please add USES=libedit"
+		*NEEDED*\[libedit.so.7])
+			err "Bad linking on ${f##* } please add USES=libedit"
 			rc=1
 			;;
 		esac
 	done <<-EOF
 	$(find ${STAGEDIR}${PREFIX}/bin ${STAGEDIR}${PREFIX}/sbin \
 		${STAGEDIR}${PREFIX}/lib ${STAGEDIR}${PREFIX}/libexec \
-		-type f -exec ldd -a {} + 2>/dev/null)
+		-type f -exec readelf -d {} + 2>/dev/null)
 	EOF
 	return ${rc}
 }
@@ -293,7 +293,7 @@ prefixvar() {
 	fi
 }
 
-checks="shebang symlinks paths stripped desktopfileutils sharedmimeinfo suidfiles libtool libperl prefixvar" # baselibs"
+checks="shebang symlinks paths stripped desktopfileutils sharedmimeinfo suidfiles libtool libperl prefixvar baselibs"
 
 ret=0
 cd ${STAGEDIR}
