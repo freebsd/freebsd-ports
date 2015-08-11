44c72f503b80ec0b3a867c9fa495508126a92021
jbeich <jbeich@FreeBSD.org>
2015-08-11 13:22:54 +0000

    Switch OpenH264 plugin to use environment variable
    files/patch-system-openh264 causes patch churn on updates and hits
    assertion with DEBUG=on[1]. Rework to use MOZ_GMP_PATH instead.
    
    While here populate Last Updated field to avoid falling back to Epoch start
    and disable Automatic Updates in an unlikely case of
    - Cisco & Mozilla providing prebuilt version for FreeBSD
    - Firefox running with root priveleges i.e., write permission under /usr/local
    
    PR:		202218 [1]
    Reported by:	kib, pi [1]
    Inspired by:	Gentoo
    MFH:		2015Q3

--- mail/thunderbird/files/patch-mail-app-nsMailApp.cpp
+++ mail/thunderbird/files/patch-mail-app-nsMailApp.cpp
@@ -1,10 +1,12 @@
 --- mail/app/nsMailApp.cpp~
 +++ mail/app/nsMailApp.cpp
-@@ -320,6 +320,7 @@ int main(int argc, char* argv[])
+@@ -320,6 +320,9 @@ int main(int argc, char* argv[])
    TriggerQuirks();
  #endif
  
++  setenv("MOZ_GMP_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
 +  setenv("MOZ_PLUGIN_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
++
    int gotCounters;
  #if defined(XP_UNIX)
    struct rusage initialRUsage;
