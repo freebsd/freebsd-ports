702ccba8433676ea5cc3d1c43442967dc7a5273d
Steve Wills <swills@FreeBSD.org>
2015-08-14 19:09:58 +0000

    sysutils/terraform: update to 0.6.3

--- sysutils/terraform/files/patch-src_github.com_fsouza_go-dockerclient_vendor_github.com_Sirupsen_logrus_terminal__bsd.go
+++ sysutils/terraform/files/patch-src_github.com_fsouza_go-dockerclient_vendor_github.com_Sirupsen_logrus_terminal__bsd.go
@@ -1,5 +1,5 @@
---- src/github.com/fsouza/go-dockerclient/vendor/github.com/Sirupsen/logrus/terminal_bsd.go.orig	2015-07-17 20:42:10 UTC
-+++ src/github.com/fsouza/go-dockerclient/vendor/github.com/Sirupsen/logrus/terminal_bsd.go
+--- src/github.com/fsouza/go-dockerclient/external/github.com/Sirupsen/logrus/terminal_bsd.go.orig	2015-07-17 20:42:10 UTC
++++ src/github.com/fsouza/go-dockerclient/external/github.com/Sirupsen/logrus/terminal_bsd.go
 @@ -1,4 +1,4 @@
 -// +build darwin freebsd openbsd netbsd dragonfly
 +// +build darwin openbsd netbsd dragonfly
