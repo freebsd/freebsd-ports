844ee36119255293171847b259ae11180c2406d3
Adam Weinberger <adamw@FreeBSD.org>
2015-08-15 16:39:03 +0000

    Update to 7.4.
    Also included:
      - Fix warnings in perl 5.22
      - Honour ${WWWDIR} in plist
      - Sort USES
      - Fix some trailing spaces in the Makefile
    
    PR:		200953
    Submitted by:	takefu@airport.fm
    Approved by:	maintainer timeout

--- www/awstats/files/patch-tools-awstats_buildstaticpages.pl
+++ www/awstats/files/patch-tools-awstats_buildstaticpages.pl
@@ -1,6 +1,6 @@
---- tools/awstats_buildstaticpages.pl.orig	Sun Feb 20 11:11:06 2005
-+++ tools/awstats_buildstaticpages.pl	Thu Sep 22 20:33:32 2005
-@@ -293,8 +293,8 @@
+--- tools/awstats_buildstaticpages.pl.orig	2015-07-21 07:55:55 UTC
++++ tools/awstats_buildstaticpages.pl
+@@ -299,8 +299,8 @@
  # Check if AWSTATS prog is found
  my $AwstatsFound=0;
  if (-s "$Awstats") { $AwstatsFound=1; }
@@ -10,4 +10,4 @@
 +	$Awstats="%%PREFIX%%/www/awstats/cgi-bin/awstats.pl";
  	$AwstatsFound=1;
  }
- if (! $AwstatsFound) {
+ elsif (-s "/usr/lib/cgi-bin/awstats.pl") {
