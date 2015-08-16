fa5e09093a890b2ca71870b1c14799297f3b06fe
Grzegorz Blach <gblach@FreeBSD.org>
2015-08-16 21:55:31 +0000

    - Upgrade to 1.6.0 - Readopt port

--- devel/ninja/files/patch-configure.py
+++ devel/ninja/files/patch-configure.py
@@ -1,14 +1,5 @@
 --- configure.py.orig	2014-11-24 18:37:47.000000000 +0100
-+++ configure.py	2015-01-18 21:26:42.000000000 +0100
-@@ -164,7 +164,7 @@
-         """Run a subcommand, quietly.  Prints the full command on error."""
-         try:
-             subprocess.check_call(cmdline, shell=True)
--        except subprocess.CalledProcessError, e:
-+        except subprocess.CalledProcessError:
-             print('when running: ', cmdline)
-             raise
- 
++++ configure.py	2015-01-18 21:26:42.000000000 +0100 
 @@ -297,7 +297,7 @@
          cflags += ['/Ox', '/DNDEBUG', '/GL']
          ldflags += ['/LTCG', '/OPT:REF', '/OPT:ICF']
