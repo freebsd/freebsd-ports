23ee2a7d628e74980199fb95b27ce242a5f718a6
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2015-08-18 10:51:01 +0000

    Uses/cmake.mk: Replace CMAKE_ENV with CONFIGURE_ENV.
    Instead of defining a variable that is almost always based on CONFIGURE_ENV,
    just use CONFIGURE_ENV directly.
    
    This also matches the behavior of other ports that do not use autotools (so
    most ports can just worry about CONFIGURE_ENV). Additionally, the fact that
    we do not use ?= means we do not have problems if another file in Uses/
    needs to set CONFIGURE_ENV (with CMAKE_ENV, the order of the arguments to
    USES would matter).
    
    Ports which set CMAKE_ENV have been adjusted accordingly. In most cases,
    CMAKE_ENV was just replaced with CONFIGURE_ENV, the exceptions being:
    * databases/sqliteman: CMAKE_ENV line removed; setting QMAKESPEC there has
                           no effect on the build system.
    * devel/freeocl: CMAKE_ENV line removed; FREEOCL_CXX_COMPILER is already
                     retrieved from the CMAKE_CXX_COMPILER variable in the build
                     system.
    * graphics/openimageio: CMAKE_ENV line removed; setting Qt variables there
                            has no effect on the build system.
    
    Reviewed by:		makc
    Differential Revision:	https://reviews.freebsd.org/D3403

--- Mk/Uses/cmake.mk
+++ Mk/Uses/cmake.mk
@@ -19,8 +19,6 @@
 # CMAKE_NINJA		- Use ninja instead of make(1)
 #
 # Variables for ports:
-# CMAKE_ENV		- Environment passed to cmake.
-#			Default: ${CONFIGURE_ENV}
 # CMAKE_ARGS		- Arguments passed to cmake
 #			Default: see below
 # CMAKE_BUILD_TYPE	- Type of build (cmake predefined build types).
@@ -68,7 +66,6 @@ PLIST_SUB+=		CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:tl}"
 INSTALL_TARGET?=	install/strip
 .endif
 
-CMAKE_ENV?=		${CONFIGURE_ENV}
 CMAKE_ARGS+=		-DCMAKE_C_COMPILER:STRING="${CC}" \
 			-DCMAKE_CXX_COMPILER:STRING="${CXX}" \
 			-DCMAKE_C_FLAGS:STRING="${CFLAGS}" \
@@ -117,7 +114,7 @@ INSTALL_WRKSRC=		${CONFIGURE_WRKSRC}
 do-configure:
 	@${ECHO_MSG} ${_CMAKE_MSG}
 	${MKDIR} ${CONFIGURE_WRKSRC}
-	@cd ${CONFIGURE_WRKSRC}; ${SETENV} ${CMAKE_ENV} ${CMAKE_BIN} ${CMAKE_ARGS} ${CMAKE_SOURCE_PATH}
+	@cd ${CONFIGURE_WRKSRC}; ${SETENV} ${CONFIGURE_ENV} ${CMAKE_BIN} ${CMAKE_ARGS} ${CMAKE_SOURCE_PATH}
 .endif
 
 .endif #!defined(_INCLUDE_USES_CMAKE_MK)
