cdc6982fb0370edc54b62392ceb51a55ba04b023
Wen Heping <wen@FreeBSD.org>
2015-08-21 11:47:51 +0000

    - Update to 1.0 - Pass maintainership to submitter
    PR:		199802
    Submitted by:	woodsb02@gmail.com

--- devel/synfig/files/patch-src__synfig__main.cpp
+++ devel/synfig/files/patch-src__synfig__main.cpp
@@ -1,37 +1,39 @@
---- src/synfig/main.cpp.orig	2014-12-22 11:08:27 UTC
-+++ src/synfig/main.cpp
-@@ -193,8 +193,10 @@ synfig::Main::Main(const synfig::String&
+--- src/synfig/main.cpp.orig	2015-08-21 15:38:05.415768235 +0800
++++ src/synfig/main.cpp	2015-08-21 15:42:18.215824597 +0800
+@@ -198,7 +198,7 @@
  
  	unsigned int i;
  #ifdef _DEBUG
+-#ifndef __APPLE__
 +#ifndef __clang__
  	std::set_terminate(__gnu_cxx::__verbose_terminate_handler);
  #endif
-+#endif
- 
- #if defined(HAVE_SIGNAL_H) && defined(SIGPIPE)
- 	signal(SIGPIPE, broken_pipe_signal);
-@@ -464,7 +466,11 @@ synfig::get_binary_path(const String &fa
+ #endif
+@@ -490,7 +490,11 @@
  
  	/* Read from /proc/self/exe (symlink) */
  	char* path2 = (char*)malloc(buf_size);
 +#if defined(__FreeBSD__) || defined (__DragonFly__)
-+	strncpy(path2, "/proc/curproc/file", buf_size - 1);
++        strncpy(path2, "/proc/curproc/file", buf_size - 1);
 +#else
  	strncpy(path2, "/proc/self/exe", buf_size - 1);
 +#endif
  
  	while (1) {
  		int i;
-@@ -501,6 +507,7 @@ synfig::get_binary_path(const String &fa
- 	
+@@ -524,9 +528,10 @@
+ 		/* path is a symlink. Continue loop and resolve this. */
+ 		strncpy(path, path2, buf_size - 1);
+ 	}
+-	
++
  	free(path2);
  
 +#if ! (defined(__FreeBSD__) || defined(__DragonFly__))
  	if (result == "")
  	{
  		/* readlink() or stat() failed; this can happen when the program is
-@@ -542,6 +549,7 @@ synfig::get_binary_path(const String &fa
+@@ -568,6 +573,7 @@
  		free(line);
  		fclose(f);
  	}
