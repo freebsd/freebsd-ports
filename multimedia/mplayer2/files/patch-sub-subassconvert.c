57065d034b9edc39c09257c1cd84028f5e3ba3f1
jbeich <jbeich@FreeBSD.org>
2015-08-27 16:59:09 +0000

    multimedia/mplayer2: minor cleanup via portlint
    - Unsilence INSTALL_* lines
    - Regenerate patches with makepatch
    - Remove extra newline
    
    PR:		202404
    Submitted by:	Carlos J Puga Medina <cpm@fbsd.es> (maintainer)

--- multimedia/mplayer2/files/patch-sub-subassconvert.c
+++ multimedia/mplayer2/files/patch-sub-subassconvert.c
@@ -1,4 +1,4 @@
---- sub/subassconvert.c.orig
+--- sub/subassconvert.c.orig	2013-07-09 16:33:36 UTC
 +++ sub/subassconvert.c
 @@ -18,6 +18,7 @@
   * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
