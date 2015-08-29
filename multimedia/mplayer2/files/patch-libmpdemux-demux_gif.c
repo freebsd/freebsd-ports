3b279696cefc46b002790d76d13e170d47ae14cf
Jan Beich <jbeich@FreeBSD.org>
2015-08-29 22:07:25 +0000

    MFH: r395460
    multimedia/mplayer2: make GIF=on actually work
    
    GIF option was both auto-disabled during configure and broken when
    forced to be enabled. So, bump PORTREVISION to restore GIF support
    for users with GIF=on.
    
    PR:		202404
    Submitted by:	Carlos J Puga Medina <cpm@fbsd.es> (maintainer)
    Approved by:	ports-secteam (delphij)

--- multimedia/mplayer2/files/patch-libmpdemux-demux_gif.c
+++ multimedia/mplayer2/files/patch-libmpdemux-demux_gif.c
@@ -1,19 +1,86 @@
---- libmpdemux/demux_gif.c.orig	2012-12-31 00:26:50.000000000 +0100
-+++ libmpdemux/demux_gif.c	2012-12-31 00:30:54.000000000 +0100
-@@ -44,6 +44,16 @@
+--- libmpdemux/demux_gif.c.orig	2013-07-09 16:33:16 UTC
++++ libmpdemux/demux_gif.c
+@@ -93,14 +93,14 @@ static int demux_gif_fill_buffer(demuxer
  
- #define GIF_SIGNATURE (('G' << 16) | ('I' << 8) | 'F')
+   while (type != IMAGE_DESC_RECORD_TYPE) {
+     if (DGifGetRecordType(gif, &type) == GIF_ERROR) {
+-      PrintGifError();
++      printf("%s\n", GifErrorString(GIF_ERROR));	
+       return 0; // oops
+     }
+     if (type == TERMINATE_RECORD_TYPE)
+       return 0; // eof
+     if (type == SCREEN_DESC_RECORD_TYPE) {
+       if (DGifGetScreenDesc(gif) == GIF_ERROR) {
+-        PrintGifError();
++        printf("%s\n", GifErrorString(GIF_ERROR));
+         return 0; // oops
+       }
+     }
+@@ -108,7 +108,7 @@ static int demux_gif_fill_buffer(demuxer
+       int code;
+       unsigned char *p = NULL;
+       if (DGifGetExtension(gif, &code, &p) == GIF_ERROR) {
+-        PrintGifError();
++        printf("%s\n", GifErrorString(GIF_ERROR));
+         return 0; // oops
+       }
+       if (code == 0xF9) {
+@@ -137,7 +137,7 @@ static int demux_gif_fill_buffer(demuxer
+ 	  comments[length] = 0;
+ 	  printf("%s", comments);
+           if (DGifGetExtensionNext(gif, &p) == GIF_ERROR) {
+-            PrintGifError();
++            printf("%s\n", GifErrorString(GIF_ERROR));
+             return 0; // oops
+           }
+ 	}
+@@ -145,7 +145,7 @@ static int demux_gif_fill_buffer(demuxer
+       }
+       while (p != NULL) {
+         if (DGifGetExtensionNext(gif, &p) == GIF_ERROR) {
+-          PrintGifError();
++          printf("%s\n", GifErrorString(GIF_ERROR));
+           return 0; // oops
+         }
+       }
+@@ -153,7 +153,7 @@ static int demux_gif_fill_buffer(demuxer
+   }
  
-+static void PrintGifError(void)
-+{
-+  char *Err = GifErrorString();
-+
-+  if (Err != NULL)
-+    fprintf(stderr, "\nGIF-LIB error: %s.\n", Err);
-+  else
-+    fprintf(stderr, "\nGIF-LIB undefined error %d.\n", GifError());
-+}
-+
- #ifndef CONFIG_GIF_TVT_HACK
- // not supported by certain versions of the library
- static int my_read_gif(GifFileType *gif, uint8_t *buf, int len)
+   if (DGifGetImageDesc(gif) == GIF_ERROR) {
+-    PrintGifError();
++    printf("%s\n", GifErrorString(GIF_ERROR));
+     return 0; // oops
+   }
+ 
+@@ -166,7 +166,7 @@ static int demux_gif_fill_buffer(demuxer
+     memset(dp->buffer, gif->SBackGroundColor, priv->w * priv->h);
+ 
+   if (DGifGetLine(gif, buf, len) == GIF_ERROR) {
+-    PrintGifError();
++    printf("%s\n", GifErrorString(GIF_ERROR));
+     free(buf);
+     return 0; // oops
+   }
+@@ -256,10 +256,10 @@ static demuxer_t* demux_open_gif(demuxer
+   lseek(demuxer->stream->fd, 0, SEEK_SET);
+   gif = DGifOpenFileHandle(demuxer->stream->fd);
+ #else
+-  gif = DGifOpen(demuxer->stream, my_read_gif);
++  gif = DGifOpen(demuxer->stream, my_read_gif, NULL);
+ #endif
+   if (!gif) {
+-    PrintGifError();
++    printf("%s\n", GifErrorString(GIF_ERROR));
+     free(priv);
+     return NULL;
+   }
+@@ -301,7 +301,7 @@ static void demux_close_gif(demuxer_t* d
+   gif_priv_t *priv = demuxer->priv;
+   if (!priv) return;
+   if (priv->gif && DGifCloseFile(priv->gif) == GIF_ERROR)
+-    PrintGifError();
++    printf("%s\n", GifErrorString(GIF_ERROR));
+   free(priv->refimg);
+   free(priv);
+ }
