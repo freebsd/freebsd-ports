528e3584f5b3c1677352a7454c1aa59c8b4c877f
Kubilay Kocak <koobs@FreeBSD.org>
2015-08-31 12:56:30 +0000

    devel/py-virtualenvwrapper: Unmark BROKEN
    - Unmark BROKEN port after pbr update [1]
    - Patch conf.py as sphinx renamed the 'default' theme, which caused the
      following error:
    
      sphinx.errors.SphinxWarning: WARNING: 'default' html theme has been
      renamed to 'classic'. Please change your html_theme setting either to
      the new 'alabaster' default theme, or to 'classic' to keep using the
      old default.
    
    - Update pkg-plist accordingly
    
    While I'm here:
    
    - Add LICENSE_FILE
    - Sort USE_PYTHON values
    
    [1] https://svnweb.freebsd.org/changeset/ports/395669
    
    Approved by:	portmgr (blanket)

---
+++ devel/py-virtualenvwrapper/files/patch-docs_source_conf.py
@@ -0,0 +1,11 @@
+--- docs/source/conf.py.orig	2015-08-31 12:34:27 UTC
++++ docs/source/conf.py
+@@ -101,7 +101,7 @@ pygments_style = 'sphinx'
+ 
+ # The theme to use for HTML and HTML Help pages.  Major themes that come with
+ # Sphinx are currently 'default' and 'sphinxdoc'.
+-html_theme = 'default'
++html_theme = 'classic'
+ 
+ # Theme options are theme-specific and customize the look and feel of a theme
+ # further.  For a list of options available for each theme, see the
