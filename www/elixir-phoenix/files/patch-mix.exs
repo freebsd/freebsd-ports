c205d10d373746a5158e618246ce92e75001f37e
olgeni <olgeni@FreeBSD.org>
2015-09-07 05:41:46 +0000

    Upgrade to version 1.0.2.

--- www/elixir-phoenix/files/patch-mix.exs
+++ www/elixir-phoenix/files/patch-mix.exs
@@ -1,9 +1,9 @@
---- mix.exs.orig	2015-07-27 12:54:35 UTC
+--- mix.exs.orig	2015-09-06 23:29:02 UTC
 +++ mix.exs
 @@ -7,7 +7,7 @@ defmodule Phoenix.Mixfile do
      [app: :phoenix,
       version: @version,
-      elixir: "~> 1.0.2 or ~> 1.1-dev",
+      elixir: "~> 1.0.2 or ~> 1.1-beta",
 -     deps: deps,
 +     deps: [],
       package: package,
