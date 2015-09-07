ade4ea44db1707cda9f58f6d81e1d98fa740a6de
hrs <hrs@FreeBSD.org>
2015-09-07 19:34:40 +0000

    - Add LICENSE. - Do not override CFLAGS. - Fix some compiler warnings. - Take maintainership.

--- net/pimdd/files/patch-defs.h
+++ net/pimdd/files/patch-defs.h
@@ -1,6 +1,6 @@
---- defs.h.orig	2012-07-23 13:50:47.000000000 +0100
-+++ defs.h	2012-07-23 13:35:04.000000000 +0100
-@@ -262,7 +262,7 @@
+--- defs.h.orig	1999-11-30 17:58:53 UTC
++++ defs.h
+@@ -262,7 +262,7 @@ extern char *		sys_errlist[];
  
  #ifndef IGMP_MEMBERSHIP_QUERY
  #define IGMP_MEMBERSHIP_QUERY		IGMP_HOST_MEMBERSHIP_QUERY
@@ -9,7 +9,7 @@
  #define IGMP_V1_MEMBERSHIP_REPORT	IGMP_HOST_MEMBERSHIP_REPORT
  #define IGMP_V2_MEMBERSHIP_REPORT	IGMP_HOST_NEW_MEMBERSHIP_REPORT
  #else
-@@ -272,7 +272,7 @@
+@@ -272,7 +272,7 @@ extern char *		sys_errlist[];
  #define IGMP_V2_LEAVE_GROUP		IGMP_HOST_LEAVE_MESSAGE
  #endif
  
