cb8713c6e46c4abccb7ac6c067e5f3c9eb460f14
mat <mat@FreeBSD.org>
2015-09-08 11:57:29 +0000

    Update to v5.23.2-208-g7ce1b4c.
    Regen a few patches, one module moved from dist to cpan, and some offset
    changed.
    
    Changes:	https://github.com/Perl/perl5/compare/v5.23.2-66-gbdc4e4b...v5.23.2-208-g7ce1b4c
    Sponsored by:	Absolight

--- lang/perl5-devel/files/patch-cpan_Pod-Perldoc_lib_Pod_Perldoc.pm
+++ lang/perl5-devel/files/patch-cpan_Pod-Perldoc_lib_Pod_Perldoc.pm
@@ -1,6 +1,6 @@
---- cpan/Pod-Perldoc/lib/Pod/Perldoc.pm.orig	2015-04-27 14:10:52 UTC
+--- cpan/Pod-Perldoc/lib/Pod/Perldoc.pm.orig	2015-09-08 05:21:36 UTC
 +++ cpan/Pod-Perldoc/lib/Pod/Perldoc.pm
-@@ -1920,6 +1920,7 @@ sub page {  # apply a pager to the outpu
+@@ -1927,6 +1927,7 @@ sub page {  # apply a pager to the outpu
                  # fix visible escape codes in ToTerm output
                  # https://bugs.debian.org/758689
                  local $ENV{LESS} = defined $ENV{LESS} ? "$ENV{LESS} -R" : "-R";
