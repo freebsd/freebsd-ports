afae652a2a20e0b0caa335e766dff03094773e1f
Mathieu Arnold <mat@FreeBSD.org>
2015-09-14 12:19:48 +0000

    Make it so that the default Perl is always called perl5.
    - Move Perl's man1 files along with its man3 files.
    - Move where Perl installs its modules man1 pages.
    - Convert the ports installing man1 pages.
    - Make different Perl versions installable at the same time.
      Though you should note that only the default version can be used to
      install Perl modules, and the non default Perl versions cannot use the
      modules installed via ports if they contain .so as they are installed
      in a version specific directory.
    
    Reviewed by:	bapt (the Mk bits)
    Exp-run by:	antoine
    Sponsored by:	Absolight
    Differential Revision:	https://reviews.freebsd.org/D3542

--- textproc/po4a/files/patch-Po4aBuilder.pm
+++ textproc/po4a/files/patch-Po4aBuilder.pm
@@ -1,6 +1,6 @@
---- Po4aBuilder.pm.orig	2013-08-21 15:11:04.000000000 -0500
-+++ Po4aBuilder.pm	2015-04-06 11:57:21.938813000 -0500
-@@ -132,13 +132,13 @@
+--- Po4aBuilder.pm.orig	2013-08-21 20:11:04 UTC
++++ Po4aBuilder.pm
+@@ -132,13 +132,13 @@ sub ACTION_install {
  
      require ExtUtils::Install;
  #    $self->depends_on('build');
@@ -10,9 +10,10 @@
      $self->install_path(man => $mandir);
      $self->install_path(manl10n => $mandir);
  
-     my $localedir = $mandir;
+-    my $localedir = $mandir;
 -    $localedir =~ s,/man$,/locale,;
-+    $localedir =~ s,/man$,/share/locale,;
++    my $localedir = $self->install_path->{'bin'};
++    $localedir =~ s,/bin$,/share/locale,;
      $self->install_path(po => $localedir);
  
      ExtUtils::Install::install($self->install_map, !$self->quiet, 0, $self->{args}{uninst}||0);
