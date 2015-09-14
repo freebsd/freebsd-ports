8660a3a653ed93c63519d71e5388241f0479dd3e
amdmi3 <amdmi3@FreeBSD.org>
2015-09-14 12:20:09 +0000

    - Add LICENSE - Pet portlint - Strip library - Regenerate patches with `make makepatch` - Fix WWW:

--- science/py-hcluster/files/patch-setup.py
+++ science/py-hcluster/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2008-12-14 21:42:50.000000000 +0800
-+++ setup.py	2008-12-14 21:43:35.000000000 +0800
-@@ -33,7 +33,8 @@
+--- setup.py.orig	2008-12-14 09:19:42 UTC
++++ setup.py
+@@ -33,7 +33,8 @@ if len(valid_paths) > 1:
      print string.join(l, '\n')
      # Prompt the user with a list of selections.
      while not (s >= 1 and s <= len(valid_paths)):
