f8571a8b79a7cce32dc58599b195a129ac889eef
Bernard Spil <spil.oss@gmail.com>
2015-09-18 11:39:23 +0200

    lang/ruby21: Proper fix build with No-SSLv3
    Backported from https://bugs.ruby-lang.org/projects/ruby-trunk/repository/revisions/51722

--- lang/ruby21/files/patch-ext_openssl_ossl__ssl.c
+++ lang/ruby21/files/patch-ext_openssl_ossl__ssl.c
@@ -1,6 +1,5 @@
-$OpenBSD: patch-ext_openssl_ossl_ssl_c,v 1.1 2015/08/27 15:55:04 kili Exp $
---- ext/openssl/ossl_ssl.c.orig	Mon Jan 27 08:47:11 2014
-+++ ext/openssl/ossl_ssl.c	Thu Aug 27 17:22:10 2015
+--- ext/openssl/ossl_ssl.c.orig	2014-01-27 08:47:11.000000000 +0100
++++ ext/openssl/ossl_ssl.c	2015-09-18 11:29:48.512712383 +0200
 @@ -134,9 +134,12 @@ struct {
      OSSL_SSL_METHOD_ENTRY(SSLv2_server),
      OSSL_SSL_METHOD_ENTRY(SSLv2_client),
