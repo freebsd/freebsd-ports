d508d47476c285ec28b5e8e29b9be945db8138a2
jbeich <jbeich@FreeBSD.org>
2015-09-18 03:24:57 +0000

    games/aquaria: add new port
    PR:		195815
    
    The game follows Naija, a mermaid-like woman, as she explores the
    underwater world of Aquaria. Along her journey, she learns about both
    the history of the world and her own past. The gameplay focuses on a
    combination of swimming, singing and combat, through which Naija can
    interact with the world. Naija's songs can move items, affect plants
    and animals, and change her physical appearance into other
    forms. These forms have different abilities, such as firing
    projectiles at hostile creatures or passing through barriers
    inaccessible to her in her natural form. -- Wikipedia
    
    This package provides only game engine. Proprietary game data files
    have to be obtained separately.
    
    https://github.com/AquariaOSE/Aquaria

---
+++ games/aquaria/files/patch-ExternalLibs_minihttp.cpp
@@ -0,0 +1,10 @@
+--- ExternalLibs/minihttp.cpp.orig	2015-08-16 13:19:19 UTC
++++ ExternalLibs/minihttp.cpp
+@@ -32,6 +32,7 @@
+ #  include <sys/types.h>
+ #  include <unistd.h>
+ #  include <fcntl.h>
++#  include <netinet/in.h>
+ #  include <sys/socket.h>
+ #  include <netdb.h>
+ #  define SOCKET_ERROR (-1)
