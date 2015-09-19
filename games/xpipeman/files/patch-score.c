6e2bc26b2d4de6702cc852671abda6dd045b69f9
naddy <naddy@FreeBSD.org>
2015-09-19 20:04:52 +0000

    Properly fix the LP64 issue in the application resource handling rather than praying that globals end up in the lower 32-bit address space.
    Add a number of missing includes, missing prototypes, etc.

--- games/xpipeman/files/patch-score.c
+++ games/xpipeman/files/patch-score.c
@@ -1,5 +1,5 @@
---- score.c.orig	1991-09-13 22:32:12.000000000 +0200
-+++ score.c	2011-02-02 19:34:46.908945354 +0100
+--- score.c.orig	1991-09-13 20:32:12 UTC
++++ score.c
 @@ -50,8 +50,9 @@
  #include <X11/Xaw/Label.h>
  #endif
@@ -11,7 +11,7 @@
  #include "xpipeman.h"
  
  /*----------------------------------------------------------------------*/
-@@ -64,7 +65,7 @@
+@@ -64,13 +65,12 @@ typedef struct {
  
  static SCORE scores[MAXSCORES];
  
@@ -20,7 +20,24 @@
  	new_high_score(), 
  	load_scores(), 
  	write_out_scores();
-@@ -198,7 +199,7 @@
+ 
+ static FILE *scorefile = 0;
+-char *score_filename;
+ 
+ /*----------------------------------------------------------------------*/
+ 
+@@ -100,8 +100,8 @@ load_scores()
+ {
+   int i = 0;
+ 
+-  if( !(scorefile = fopen(score_filename,"r+")) ) {
+-    scorefile = fopen(score_filename, "w");
++  if( !(scorefile = fopen(app_data.score_filename,"r+")) ) {
++    scorefile = fopen(app_data.score_filename, "w");
+     return;
+   }
+ #ifndef SYSV
+@@ -198,7 +198,7 @@ static Arg arglist_popdown[] = {
  
  
  /*ARGSUSED*/
