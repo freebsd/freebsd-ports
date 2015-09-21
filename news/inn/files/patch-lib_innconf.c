9b77b73e134cfbcc80193704a3ddb14e14541ebc
Johan van Selst <johans@FreeBSD.org>
2015-09-21 20:24:30 +0000

    Update to INN 2.6.0 http://www.eyrie.org/~eagle/software/inn/docs-2.6/news.html#S1

--- news/inn/files/patch-lib_innconf.c
+++ news/inn/files/patch-lib_innconf.c
@@ -1,6 +1,6 @@
---- lib/innconf.c.orig	Mon Mar 20 04:14:57 2006
-+++ lib/innconf.c	Sat Aug 26 00:22:04 2006
-@@ -395,7 +395,7 @@
+--- lib/innconf.c.orig	2015-09-12 15:25:22.000000000 +0200
++++ lib/innconf.c	2015-09-21 21:15:18.446435000 +0200
+@@ -473,7 +473,7 @@ innconf_validate(struct config_group *gr
  
      if (GetFQDN(innconf->domain) == NULL) {
          warn("hostname does not resolve or domain not set in inn.conf");
