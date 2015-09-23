b3eae3732ed38a97cf0c036015d72794105116cd
junovitch <junovitch@FreeBSD.org>
2015-09-23 02:35:08 +0000

    multimedia/kodi: update 14.2 -> 15.1
    - Update to Kodi 15.1 "Isengard"
    - Remove CEC option from defaults
    - Set CEC option as broken, needs libcec >= 3.0.0 which has yet to be ported
    - Set USE_GCC for compilation on FreeBSD 9
    - Sort USE_XORG, set USE_LDCONFIG, add under java category (portlint)
    
    PR:		202812
    Submitted by:	mickael.maillot@gmail.com (maintainer)
    Security:	80c66af0-d1c5-449e-bd31-63b12525ff88
    Security:	CVE-2015-3395
    Security:	3d950687-b4c9-4a86-8478-c56743547af8
    Security:	CVE-2015-6818
    Security:	CVE-2015-6819
    Security:	CVE-2015-6820
    Security:	CVE-2015-6821
    Security:	CVE-2015-6822
    Security:	CVE-2015-6823
    Security:	CVE-2015-6824
    Security:	CVE-2015-6825
    Security:	CVE-2015-6826
    MFH:		2015Q3
    X-MFH-With:	r397342
    X-MFH-Note:	opt_VARS added post 2015Q3, needs manual resolution

---
+++ multimedia/kodi/files/patch-xbmc__guilib__GUISpinControl.cpp
@@ -0,0 +1,10 @@
+--- xbmc/guilib/GUISpinControl.cpp.orig	2015-04-13 12:48:49 UTC
++++ xbmc/guilib/GUISpinControl.cpp
+@@ -22,6 +22,7 @@
+ #include "input/Key.h"
+ #include "utils/StringUtils.h"
+ #include <stdio.h>
++#include <stdlib.h>
+ 
+ using namespace std;
+ 
