3b268fc4ae8307fb7ecb330ec252c320b7a7e1af
amdmi3 <amdmi3@FreeBSD.org>
2015-09-25 15:17:37 +0000

    - Add LICENSE - Pet portlint - Regenerate patches with `make makepatch`

--- deskutils/fbreader/files/patch-fbreader__src__formats__html__HtmlReader.cpp
+++ deskutils/fbreader/files/patch-fbreader__src__formats__html__HtmlReader.cpp
@@ -1,5 +1,5 @@
---- ./fbreader/src/formats/html/HtmlReader.cpp.orig	2010-04-02 00:14:24.000000000 +1100
-+++ ./fbreader/src/formats/html/HtmlReader.cpp	2013-09-12 00:31:39.434367202 +1100
+--- fbreader/src/formats/html/HtmlReader.cpp.orig	2014-01-11 12:45:25 UTC
++++ fbreader/src/formats/html/HtmlReader.cpp
 @@ -19,6 +19,7 @@
  
  #include <algorithm>
