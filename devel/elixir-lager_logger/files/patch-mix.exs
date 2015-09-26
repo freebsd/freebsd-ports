4dba6bd0307d6a90741ea03b6040d2f8e3f28b57
Jimmy Olgeni <olgeni@FreeBSD.org>
2015-09-26 11:40:27 +0000

    Fix dependencies for Elixir 1.1.

--- devel/elixir-lager_logger/files/patch-mix.exs
+++ devel/elixir-lager_logger/files/patch-mix.exs
@@ -1,10 +1,14 @@
---- mix.exs.orig	2015-07-02 13:24:56 UTC
+--- mix.exs.orig	2015-09-26 08:36:49 UTC
 +++ mix.exs
-@@ -30,7 +30,6 @@ defmodule LagerLogger.Mixfile do
- 
-   defp deps do
-     [
--      {:lager, ">= 2.1.0"},
-     ]
+@@ -4,10 +4,8 @@ defmodule LagerLogger.Mixfile do
+   def project do
+     [app: :lager_logger,
+      version: "1.0.0",
+-     elixir: "~> 1.0.0",
+      package: package,
+-     description: description,
+-     deps: deps]
++     description: description]
    end
- end
+ 
+   defp package do
--- www/elixir-joken/files/patch-mix.exs
+++ www/elixir-joken/files/patch-mix.exs
@@ -1,8 +1,10 @@
---- mix.exs.orig	2015-07-08 12:15:58 UTC
+--- mix.exs.orig	2015-07-13 13:00:06 UTC
 +++ mix.exs
-@@ -6,8 +6,7 @@ defmodule Joken.Mixfile do
-      version: "0.14.1",
-      elixir: "~> 1.0.0",
+@@ -4,10 +4,8 @@ defmodule Joken.Mixfile do
+   def project do
+     [app: :joken,
+      version: "0.15.0",
+-     elixir: "~> 1.0.0",
       description: description,
 -     package: package,
 -     deps: deps]
