freebsd/freebsd-ports

commit b8f039291c745000da8cf780325eb62eb733112a

Respect the locally set CXXFLAGS (and CFLAGS) -- except for optimization level (-O*), which Boost sets to -O3 automatically.
Approved by:	maintainer (bapt)


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@317845 35697150-7ecd-e111-bb59-0022644237b5

devel/boost-all/compiled.mk
@@ -16,6 +16,11 @@ BJAM_ARGS=	--layout=system \
 BJAM_ARGS+=	cxxflags=-fPIC
 .endif
 
+# Our compiler-flags will be added AFTER those set by bjam. We remove
+# the optimization level, because Boost sets it itself (to -O3 in case
+# of gcc/g++):
+BJAM_ARGS+=    cxxflags="${CXXFLAGS:N-O*}" cflags="${CFLAGS:N-O*}"
+
 NO_CCACHE=	yes
 .if ${CC:T:Mgcc*}
 BOOST_TOOLSET:=	gcc

commit 2669016fb0b2857ae1c9fb2510b88a56acd7adb7

devel/boost-*: remove nop ARCH-specific *FLAGS regressed by r317845
Approved by:	office (truckman, bapt) (maintainer)
Differential Revision:	https://reviews.freebsd.org/D3739

devel/boost-all/compiled.mk
@@ -13,10 +13,6 @@ PKG_MESSAGE_FILE_PYTHON=	${PORTSDIR}/devel/boost-all/pkg-message.python
 BJAM_ARGS=	--layout=system \
 		--prefix=${PREFIX} \
 
-.if ${ARCH} == amd64
-BJAM_ARGS+=	cxxflags=-fPIC
-.endif
-
 # Our compiler-flags will be added AFTER those set by bjam. We remove
 # the optimization level, because Boost sets it itself (to -O3 in case
 # of gcc/g++):

devel/boost-python-libs/Makefile
@@ -13,10 +13,6 @@ VERBOSE_BUILD_DESC=	Show compiler messages
 
 .include "${.CURDIR}/../boost-all/compiled.mk"
 
-.if ${ARCH} == "sparc64"
-CXXFLAGS=	-O0
-.endif
-
 MAKE_ENV+=	PYTHON_ROOT="${LOCALBASE}"\
 		PYTHON_VERSION="${PYTHON_VERSION}"\
 		PYTHON_INCLUDES="${PYTHON_INCLUDEDIR}"\