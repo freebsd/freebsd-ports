f271f72f19e4ba95e0502fd1623f6e40d577fc19
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2015-09-28 19:15:47 +0000

    x11-servers/Xfstt: update to 1.9.2
    - update to 1.9.2
    - pass maintainership to submitter
    - add new maintainer's distfile mirror to MASTER_SITES
    - specify a LICENSE
    - sort USES alphabetically
    - regenerate patches with make makepatch
    - remove absolute path from pkg-message
    - add PROVIDE, REQUIRE and KEYWORD to the rc script
    
    PR:		202267
    Submitted by:	A.J. "Fonz" van Werven <freebsd@skysmurf.nl>

--- x11-servers/Xfstt/files/patch-libfstt__arch.h
+++ x11-servers/Xfstt/files/patch-libfstt__arch.h
@@ -1,8 +1,8 @@
---- libfstt/arch.h.orig	Thu Nov 14 21:08:10 2002
-+++ libfstt/arch.h	Wed Nov 20 22:44:27 2002
-@@ -49,6 +49,7 @@
- typedef signed short	S16;
- typedef signed int	S32;
+--- libfstt/arch.h.orig	2012-03-02 15:33:23 UTC
++++ libfstt/arch.h
+@@ -48,6 +48,7 @@
+ 
+ // architecture specific
  
 +#include <sys/types.h>
  #include <netinet/in.h>
