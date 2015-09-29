f6baae329dc9a29c4442e8c1d2c6b63cc01ad76f
vanilla <vanilla@FreeBSD.org>
2015-09-29 13:49:46 +0000

    Upgrade to 0.16.0.

--- www/elixir-joken/files/patch-mix.exs
+++ www/elixir-joken/files/patch-mix.exs
@@ -1,10 +1,8 @@
---- mix.exs.orig	2015-07-13 13:00:06 UTC
+--- mix.exs.orig	2015-09-14 14:31:47 UTC
 +++ mix.exs
-@@ -4,10 +4,8 @@ defmodule Joken.Mixfile do
-   def project do
-     [app: :joken,
-      version: "0.15.0",
--     elixir: "~> 1.0.0",
+@@ -6,8 +6,7 @@ defmodule Joken.Mixfile do
+      version: "0.16.0",
+      elixir: "~> 1.0",
       description: description,
 -     package: package,
 -     deps: deps]
