bfa91cf33bf69573193f2082f9e31362a2c78512
amdmi3 <amdmi3@FreeBSD.org>
2015-10-01 20:32:07 +0000

    - Fix staging as non-root - While here, regenerate other patches with make makepatch - Add LICENSE - Drop USES=desktop-file-utils as suggested by stage-qa

--- ports-mgmt/bpm/files/patch-src_install.c
+++ ports-mgmt/bpm/files/patch-src_install.c
@@ -1,6 +1,6 @@
---- src/install.c.orig	Sun Apr  2 14:09:25 2006
-+++ src/install.c	Sun Apr  2 14:12:03 2006
-@@ -958,6 +958,8 @@
+--- src/install.c.orig	2004-08-24 04:03:44 UTC
++++ src/install.c
+@@ -958,6 +958,8 @@ install_action_run(struct install_data *
  	av[ac++] = "SU_CMD=" SU_CMD;
  	av[ac++] = "-M";
  	av[ac++] = "FETCH_CMD=" FETCH_CMD;
@@ -9,7 +9,7 @@
  	av[ac++] = "-s";
  	av[ac++] = "-S";
  	av[ac++] = SU_CMD " '%s'";
-@@ -971,6 +973,7 @@
+@@ -971,6 +973,7 @@ install_action_run(struct install_data *
  	av[ac++] = "make";
  	av[ac++] = "SU_CMD=" SU_CMD;
  	av[ac++] = "FETCH_CMD=" FETCH_CMD;
