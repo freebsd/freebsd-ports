93f6ff59874898a5b7147f23d7378e21e234103a
Bernard Spil <spil.oss@gmail.com>
2015-10-02 16:01:35 +0200

    net/turnserver: Improve fix for No-SSLv3

--- net/turnserver/files/patch-src_apps_common_apputils.c
+++ net/turnserver/files/patch-src_apps_common_apputils.c
@@ -1,6 +1,6 @@
---- src/apps/common/apputils.c.orig	2015-09-27 05:21:18 UTC
-+++ src/apps/common/apputils.c
-@@ -1037,13 +1037,17 @@ static const char* turn_get_method(const
+--- src/apps/common/apputils.c.orig	2015-06-28 09:26:54.000000000 +0200
++++ src/apps/common/apputils.c	2015-10-02 15:56:56.044250196 +0200
+@@ -1037,13 +1037,15 @@ static const char* turn_get_method(const
  		if(!method)
  			return mdefault;
  		else {
@@ -10,12 +10,12 @@
  				return "SSLv3";
  			} else if(method == SSLv3_client_method()) {
  				return "SSLv3";
- 			} else if(method == SSLv23_server_method()) {
-+			        return "SSLv23";
-+#else			
-+			if(method == SSLv23_server_method()) {
- 					return "SSLv23";
+-			} else if(method == SSLv23_server_method()) {
+-					return "SSLv23";
++			} else 
 +#endif
++			if(method == SSLv23_server_method()) {
++			        return "SSLv23";
  			} else if(method == SSLv23_client_method()) {
  					return "SSLv23";
  			} else if(method == TLSv1_server_method()) {
