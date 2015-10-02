7dd6c96e4a69ed9f49130ec43c06f53f70302f73
Bernard Spil <spil.oss@gmail.com>
2015-10-02 15:46:13 +0200

    mail/cone: Improve No-SSLv3 fix

--- mail/cone/files/patch-tcpd_libcouriertls.c
+++ mail/cone/files/patch-tcpd_libcouriertls.c
@@ -1,6 +1,6 @@
---- tcpd/libcouriertls.c.orig	2015-09-26 18:02:40 UTC
+--- tcpd/libcouriertls.c.orig	2011-06-20 03:13:54 UTC
 +++ tcpd/libcouriertls.c
-@@ -552,10 +552,11 @@ SSL_CTX *tls_create(int isserver, const 
+@@ -552,11 +552,16 @@ SSL_CTX *tls_create(int isserver, const 
  		protocol="SSL23";
  
  	ctx=SSL_CTX_new(protocol && strcmp(protocol, "SSL3") == 0
@@ -9,7 +9,13 @@
  			protocol && strcmp(protocol, "SSL23") == 0
  			? SSLv23_method():
  			TLSv1_method());
-+	SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv3 | SSL_OP_NO_SSLv2);
- 
+-
++	if (strcmp(protocol, "SSL3") == 0)
++		SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_TLSv1
++#ifdef OPENSSL_NO_TLSv1_2
++		| SSL_OP_NO_TLSv1_1 | SSL_OP_NO_TLSv1_2
++#endif
++		);
  	if (!ctx)
  	{
+ 		free(info_copy);
