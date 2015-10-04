2b9a99472ccfc4d88174fc4962ce73d0a7196bc9
pi <pi@FreeBSD.org>
2015-10-04 10:43:28 +0000

    devel/etl: 0.4.18 -> 0.04.19 devel/synfig: 1.0 -> 1.0.1 graphics/synfigstudio: 1.0 -> 1.0.1
    PR:		201175
    Submitted by:	Ben Woods <woodsb02@gmail.com> (maintainer)

--- devel/synfig/files/patch-src__synfig__main.cpp
+++ devel/synfig/files/patch-src__synfig__main.cpp
@@ -1,6 +1,6 @@
---- src/synfig/main.cpp.orig	2015-08-21 15:38:05.415768235 +0800
-+++ src/synfig/main.cpp	2015-08-21 15:42:18.215824597 +0800
-@@ -198,7 +198,7 @@
+--- src/synfig/main.cpp.orig	2015-03-28 10:15:01 UTC
++++ src/synfig/main.cpp
+@@ -198,7 +198,7 @@ synfig::Main::Main(const synfig::String&
  
  	unsigned int i;
  #ifdef _DEBUG
@@ -9,7 +9,7 @@
  	std::set_terminate(__gnu_cxx::__verbose_terminate_handler);
  #endif
  #endif
-@@ -490,7 +490,11 @@
+@@ -490,7 +490,11 @@ synfig::get_binary_path(const String &fa
  
  	/* Read from /proc/self/exe (symlink) */
  	char* path2 = (char*)malloc(buf_size);
@@ -21,7 +21,7 @@
  
  	while (1) {
  		int i;
-@@ -524,9 +528,10 @@
+@@ -524,9 +528,10 @@ synfig::get_binary_path(const String &fa
  		/* path is a symlink. Continue loop and resolve this. */
  		strncpy(path, path2, buf_size - 1);
  	}
@@ -33,7 +33,7 @@
  	if (result == "")
  	{
  		/* readlink() or stat() failed; this can happen when the program is
-@@ -568,6 +573,7 @@
+@@ -568,6 +573,7 @@ synfig::get_binary_path(const String &fa
  		free(line);
  		fclose(f);
  	}
