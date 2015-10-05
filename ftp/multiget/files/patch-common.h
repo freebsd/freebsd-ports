1b5d9db091b4118937722973903a8cb5f28ef652
marino <marino@FreeBSD.org>
2015-10-05 12:02:13 +0000

    ftp/multiget: Add headers to fix on modern gcc

---
+++ ftp/multiget/files/patch-common.h
@@ -0,0 +1,11 @@
+--- common.h.orig	2015-10-04 16:03:02 UTC
++++ common.h
+@@ -24,6 +24,8 @@
+ #ifndef _COMMON_H
+ #define _COMMON_H
+ 
++#include <cstring>
++#include <cstdlib>
+ #include <string>
+ #include <vector>
+ #include <list>
