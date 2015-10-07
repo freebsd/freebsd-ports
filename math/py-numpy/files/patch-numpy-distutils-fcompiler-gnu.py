d53e6ac1aa74a59c57f25310a82afd0bdeebf51a
olivierd <olivierd@FreeBSD.org>
2015-10-07 03:37:42 +0000

    Update to 1.10.0

--- math/py-numpy/files/patch-numpy-distutils-fcompiler-gnu.py
+++ math/py-numpy/files/patch-numpy-distutils-fcompiler-gnu.py
@@ -1,38 +1,35 @@
---- numpy/distutils/fcompiler/gnu.py.orig	2013-10-30 22:31:40.000000000 +0400
-+++ numpy/distutils/fcompiler/gnu.py	2014-02-24 15:11:58.000000000 +0400
-@@ -66,7 +66,7 @@
-     # Redhat: GNU Fortran (GCC 3.2.2 20030222 (Red Hat Linux 3.2.2-5)) 3.2.2 20030222 (Red Hat Linux 3.2.2-5)
-     # GNU Fortran (GCC) 3.4.2 (mingw-special)
+--- numpy/distutils/fcompiler/gnu.py.orig	2015-09-13 16:21:27 UTC
++++ numpy/distutils/fcompiler/gnu.py
+@@ -83,7 +83,7 @@ class GnuFCompiler(FCompiler):
+             return None
+         return v[1]
  
 -    possible_executables = ['g77', 'f77']
 +    possible_executables = ['%%FC%%', 'g77', 'f77']
      executables = {
-         'version_cmd'  : [None, "--version"],
+         'version_cmd'  : [None, "-dumpversion"],
          'compiler_f77' : [None, "-g", "-Wall", "-fno-second-underscore"],
-@@ -90,7 +90,7 @@
+@@ -107,11 +107,13 @@ class GnuFCompiler(FCompiler):
          for key in ['version_cmd', 'compiler_f77', 'linker_so', 'linker_exe']:
              executables[key].append('-mno-cygwin')
  
 -    g2c = 'g2c'
 +    g2c = '%%FC%%'
- 
      suggested_f90_compiler = 'gnu95'
- 
-@@ -103,6 +103,8 @@
  
      def get_flags_linker_so(self):
          opt = self.linker_so[1:]
 +        if 'FFLAGS' in os.environ:
-+                opt.append(os.environ['FFLAGS'])
-         if sys.platform=='darwin':
++            opt.append(os.environ['FFLAGS'])
+         if sys.platform == 'darwin':
              target = os.environ.get('MACOSX_DEPLOYMENT_TARGET', None)
              # If MACOSX_DEPLOYMENT_TARGET is set, we simply trust the value
-@@ -249,7 +251,7 @@
-     #       GNU Fortran 95 (GCC) 4.2.0 20060218 (experimental)
-     #       GNU Fortran (GCC) 4.3.0 20070316 (experimental)
+@@ -265,7 +267,7 @@ class Gnu95FCompiler(GnuFCompiler):
+                     self.executables[key].append('-mno-cygwin')
+         return v
  
 -    possible_executables = ['gfortran', 'f95']
 +    possible_executables = ['%%FC%%', 'gfortran', 'f95']
      executables = {
-         'version_cmd'  : ["<F90>", "--version"],
-         'compiler_f77' : [None, "-Wall", "-ffixed-form",
+         'version_cmd'  : ["<F90>", "-dumpversion"],
+         'compiler_f77' : [None, "-Wall", "-g", "-ffixed-form",
--- math/py-numpy/files/patch-numpy_f2py_setup.py
+++ math/py-numpy/files/patch-numpy_f2py_setup.py
@@ -1,19 +1,11 @@
---- numpy/f2py/setup.py.orig	Mon Oct  2 17:08:55 2006
-+++ numpy/f2py/setup.py	Mon Jun 11 12:17:13 2007
-@@ -34,14 +34,13 @@
-     config.add_data_dir('docs')
- 
-     config.add_data_files('src/fortranobject.c',
--                          'src/fortranobject.h',
--                          'f2py.1'
-+                          'src/fortranobject.h'
-                           )
- 
+--- numpy/f2py/setup.py.orig	2015-08-11 17:03:43 UTC
++++ numpy/f2py/setup.py
+@@ -55,7 +55,7 @@ def configuration(parent_package='', top
      config.make_svn_version_py()
  
      def generate_f2py_py(build_dir):
--        f2py_exe = 'f2py'+os.path.basename(sys.executable)[6:]
+-        f2py_exe = 'f2py' + os.path.basename(sys.executable)[6:]
 +        f2py_exe = 'f2py'
-         if f2py_exe[-4:]=='.exe':
+         if f2py_exe[-4:] == '.exe':
              f2py_exe = f2py_exe[:-4] + '.py'
          if 'bdist_wininst' in sys.argv and f2py_exe[-3:] != '.py':
