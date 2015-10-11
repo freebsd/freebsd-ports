3df91a3d877474f33ab058cd82244836c1b641fe
John Marino <marino@FreeBSD.org>
2015-10-11 23:04:12 +0000

    lang/gnatdroid-binutils: Upgrade version 2.25 => 2.25.1

--- lang/gnatdroid-binutils/files/patch-gas__config__tc-arm.c
+++ lang/gnatdroid-binutils/files/patch-gas__config__tc-arm.c
@@ -1,12 +1,6 @@
-From d840c081f8082e8b9e63fead5306643975a97bb3 Mon Sep 17 00:00:00 2001
-From: Richard Earnshaw <Richard.Earnshaw@arm.com>
-Date: Thu, 20 Nov 2014 17:02:47 +0000
-Subject: [PATCH] * config/tc-arm.c (rotate_left): Avoid undefined behaviour
- when N = 0.
-
---- gas/config/tc-arm.c.orig	2014-10-14 09:32:03.000000000 +0200
-+++ gas/config/tc-arm.c	2015-02-18 21:13:40.720573000 +0100
-@@ -7251,7 +7251,7 @@
+--- gas/config/tc-arm.c.orig	2015-07-21 08:20:58 UTC
++++ gas/config/tc-arm.c
+@@ -7261,7 +7261,7 @@ parse_operands (char *str, const unsigne
  
  /* Functions for operand encoding.  ARM, then Thumb.  */
  
