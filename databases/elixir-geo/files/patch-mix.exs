ddbaa26ec2d9591024a3ceb341c4cdadb464d229
olgeni <olgeni@FreeBSD.org>
2015-10-13 18:30:51 +0000

    Upgrade databases/elixir-geo to version 0.17.0.

--- databases/elixir-geo/files/patch-mix.exs
+++ databases/elixir-geo/files/patch-mix.exs
@@ -1,10 +1,10 @@
---- mix.exs.orig	2015-09-26 08:11:34 UTC
+--- mix.exs.orig	2015-10-13 18:25:07 UTC
 +++ mix.exs
 @@ -4,7 +4,6 @@ defmodule Geo.Mixfile do
    def project do
      [ app: :geo,
-       version: "0.16.1",
--      elixir: "~> 1.0.0",
+       version: "0.17.0",
+-      elixir: "~> 1.0",
        deps: deps,
        description: description,
        package: package,
