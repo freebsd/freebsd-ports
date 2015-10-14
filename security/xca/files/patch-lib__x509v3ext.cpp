d7d01230ff628f13727c719996e63a01a990c247
amdmi3 <amdmi3@FreeBSD.org>
2015-10-14 23:51:30 +0000

    - Add LICENSE_FILE - Regenerate patches with `make makepatch`

--- security/xca/files/patch-lib__x509v3ext.cpp
+++ security/xca/files/patch-lib__x509v3ext.cpp
@@ -1,6 +1,6 @@
 --- lib/x509v3ext.cpp.orig	2012-05-12 09:37:14 UTC
 +++ lib/x509v3ext.cpp
-@@ -27,6 +27,8 @@
+@@ -27,6 +27,8 @@ x509v3ext::x509v3ext(const X509_EXTENSIO
  x509v3ext::x509v3ext(const x509v3ext &n)
  {
  	ext = NULL;
@@ -9,7 +9,7 @@
  	set(n.ext);
  }
  
-@@ -743,7 +745,7 @@
+@@ -743,7 +745,7 @@ X509_EXTENSION *x509v3ext::get() const
  
  bool x509v3ext::isValid() const
  {
