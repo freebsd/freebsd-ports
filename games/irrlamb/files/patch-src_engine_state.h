858f5d7b2e102c18030bd1da4beca3ca1a293209
amdmi3 <amdmi3@FreeBSD.org>
2015-10-14 21:37:22 +0000

    - Fix build with clang 3.7

---
+++ games/irrlamb/files/patch-src_engine_state.h
@@ -0,0 +1,10 @@
+--- src/engine/state.h.orig	2009-12-24 05:02:41 UTC
++++ src/engine/state.h
+@@ -23,6 +23,7 @@
+ // Namespaces
+ using namespace irr;
+ using namespace core;
++using namespace irr::core;
+ using namespace scene;
+ using namespace video;
+ using namespace io;
