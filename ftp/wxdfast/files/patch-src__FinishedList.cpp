14f37d891af94b0ea57f8d58a0c782a7673f44c6
amdmi3 <amdmi3@FreeBSD.org>
2015-10-14 23:50:15 +0000

    - Add LICENSE_FILE - Drop unneeded WX_UNICODE - Regenerate patches with `make makepatch`

--- ftp/wxdfast/files/patch-src__FinishedList.cpp
+++ ftp/wxdfast/files/patch-src__FinishedList.cpp
@@ -1,6 +1,6 @@
---- ./src/FinishedList.cpp.orig	Mon Mar 12 01:45:24 2007
-+++ ./src/FinishedList.cpp	Sun Jun 24 23:27:13 2007
-@@ -203,7 +203,7 @@
+--- src/FinishedList.cpp.orig	2007-03-11 22:45:24 UTC
++++ src/FinishedList.cpp
+@@ -203,7 +203,7 @@ void mFinishedList::SelectUnselect(bool 
              wxDateTime date;
              value = 0;
              config->Read(START_REG,&value);
@@ -9,7 +9,7 @@
              infolist->SetItem(5,1,date.Format());
          }
  
-@@ -211,7 +211,7 @@
+@@ -211,7 +211,7 @@ void mFinishedList::SelectUnselect(bool 
              wxDateTime date;
              value = 0;
              config->Read(END_REG,&value);
