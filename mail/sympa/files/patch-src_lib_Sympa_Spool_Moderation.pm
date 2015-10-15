6ba68464cc225980e5ff044d65a65e1482d1fe6f
Mathieu Arnold <mat@FreeBSD.org>
2015-10-15 14:19:52 +0000

    Update to 6.2.8.
    PR:		203209
    Submitted by:	maintainer
    Sponsored by:	Absolight

---
+++ mail/sympa/files/patch-src_lib_Sympa_Spool_Moderation.pm
@@ -0,0 +1,11 @@
+--- src/lib/Sympa/Spool/Moderation.pm.orig	2015-10-03 09:26:05 UTC
++++ src/lib/Sympa/Spool/Moderation.pm
+@@ -223,6 +223,8 @@ None.
+ 
+ =back
+ 
++=back
++
+ =head1 CONFIGURATION PARAMETERS
+ 
+ Following site configuration parameters in sympa.conf will be referred.
