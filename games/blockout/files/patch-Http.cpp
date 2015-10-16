9bbcf4d0ccbc9da248eee74457fc8b0cef6bed23
Mathieu Arnold <mat@FreeBSD.org>
2015-10-16 14:19:25 +0000

    Update to 2.5
    PR:		203057
    Submitted by:	maintainer
    Sponsored by:	Absolight

--- games/blockout/files/patch-Http.cpp
+++ games/blockout/files/patch-Http.cpp
@@ -1,12 +1,14 @@
---- BlockOut/Http.cpp.orig	2009-08-06 15:04:34.000000000 +0700
-+++ BlockOut/Http.cpp	2009-08-06 15:14:02.000000000 +0700
-@@ -24,11 +24,16 @@
+--- BlockOut/Http.cpp.orig	2014-05-06 16:47:54.000000000 +0700
++++ BlockOut/Http.cpp	2015-08-17 10:39:47.000000000 +0600
+@@ -25,6 +25,7 @@
  #include <sys/types.h>
  #include <sys/socket.h>
  #include <netdb.h>
 +#include <netinet/in.h>
- 
- 
+ #define closesocket close
+ #else
+ #include <Winsock2.h>
+@@ -46,6 +47,10 @@
  #define WAIT_FOR_READ  1
  #define WAIT_FOR_WRITE 2
  
