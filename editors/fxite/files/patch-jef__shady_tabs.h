76b977571a3084992ce013833d8ed23c46aeea7f
marino <marino@FreeBSD.org>
2015-10-16 14:51:05 +0000

    editors/fxite: Fix when Fox-1.7 option selected
    Reorganizes port, rewrapps pkg-descr too.
    This uses a new distribution file from GitHub but for some reason
    the version number didn't change.
    
    PR:		200300
    Submitted by:	ports fury

---
+++ editors/fxite/files/patch-jef__shady_tabs.h
@@ -0,0 +1,11 @@
+--- jef/shady_tabs.h.orig	2013-10-03 09:22:51 UTC
++++ jef/shady_tabs.h
+@@ -28,7 +28,7 @@ protected:
+   void UpdateTabs();
+ public:
+   long onCmdOpenItem  ( FXObject* sender, FXSelector sel, void* p );
+-  void setCurrent(FXint i, FXbool notify=FALSE);
++  void setCurrent(FXint i, FXbool notify=false);
+   ShadyTabs(FXComposite* p,FXObject* tgt=NULL, FXSelector sel=0, FXuint opts=TABBOOK_NORMAL,
+               FXint x=0, FXint y=0, FXint w=0, FXint h=0, FXint pl=DEFAULT_SPACING,
+               FXint pr=DEFAULT_SPACING, FXint pt=DEFAULT_SPACING, FXint pb=DEFAULT_SPACING);
