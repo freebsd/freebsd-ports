bc3fc9f856c62745f61d788e0f1e83cae8f57cb7
Kurt Jaeger <pi@FreeBSD.org>
2015-10-17 16:41:29 +0000

    New port: mail/openemm2013
    OpenEMM is a feature-rich web-based enterprise application for email
    marketing, newsletters and service mails (transaction mails and
    event or time triggered mails). OpenEMM offers a great user interface,
    sophisticated bounce management, link tracking, lots of graphical
    realtime statistics, a CMS module and a scripting feature to implement
    individual tasks.
    
    OpenEMM is the first open source application for e-mail marketing.
    Its code base has been developed since 1999 and is used - as part
    of the commercial software product E-Marketing Manager (EMM) - by
    companies like IBM, Daimler, Siemens and Deutsche Telekom.
    
    OpenEMM offers already more than 95% of the functionality of most
    commercial products and some features most commercial products do
    not offer right now (for example MySQL support and CMS functionality).
    
    WWW: http://www.openemm.org
    
    PR:		196516
    Submitted by:	Horst Leitenmueller <horst.leitenmueller@liwest.at>

---
+++ mail/openemm2013/files/patch-src__java__org__agnitas__backend__StringOps.java
@@ -0,0 +1,21 @@
+--- src/java/org/agnitas/backend/StringOps.java.orig	2015-10-11 16:41:55 UTC
++++ src/java/org/agnitas/backend/StringOps.java
+@@ -63,12 +63,12 @@ public class StringOps {
+ 		transtab.put("apos", "'");
+ 		transtab.put("nbsp", " ");
+ /*
+-		transtab.put("auml", "ä");
+-		transtab.put("ouml", "ö");
+-		transtab.put("uuml", "ü");
+-		transtab.put("Auml", "Ä");
+-		transtab.put("Ouml", "Ö");
+-		transtab.put("Uuml", "Ü");
++		transtab.put("auml", "");
++		transtab.put("ouml", "");
++		transtab.put("uuml", "");
++		transtab.put("Auml", "");
++		transtab.put("Ouml", "");
++		transtab.put("Uuml", "");
+ */
+ 		
+ 		rtranstab = new Hashtable<String, String>();
