9190f2bec03cc294f3e8c5a65122f09894025eb5
Dmitry Marakasov <amdmi3@FreeBSD.org>
2015-10-18 17:33:20 +0000

    - Simplify wrapper script creation - Fix shebang - Add build conflict with itself

--- devel/drpython/files/patch-drToolBarDialog.py
+++ devel/drpython/files/patch-drToolBarDialog.py
@@ -1,6 +1,6 @@
---- drToolBarDialog.py.orig
+--- drToolBarDialog.py.orig	2014-04-20 17:50:40 UTC
 +++ drToolBarDialog.py
-@@ -36,7 +36,7 @@
+@@ -36,7 +36,7 @@ def GetToolBarLabels(filename, frame):
          text = f.read()
          f.close()
      except:
