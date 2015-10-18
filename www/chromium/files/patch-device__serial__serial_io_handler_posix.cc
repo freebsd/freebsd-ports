a4d3f74247ffdc054ba6676a6c2a8fe47fd960cf
rene <rene@FreeBSD.org>
2015-10-18 10:36:06 +0000

    www/chromium: update to 46.0.2490.71
    Submitted by: Christoph Moench-Tegeder
    Obtained from:	https://github.com/gliaskos/freebsd-chromium/pull/38
    MFH:		2015Q4
    Security:	http://vuxml.freebsd.org/freebsd/8301c04d-71df-11e5-9fcb-00262d5ed8ee.html

--- www/chromium/files/patch-device__serial__serial_io_handler_posix.cc
+++ www/chromium/files/patch-device__serial__serial_io_handler_posix.cc
@@ -1,6 +1,6 @@
---- device/serial/serial_io_handler_posix.cc.orig	2015-07-15 16:30:04.000000000 -0400
-+++ device/serial/serial_io_handler_posix.cc	2015-07-22 07:27:41.841184000 -0400
-@@ -61,7 +61,7 @@
+--- device/serial/serial_io_handler_posix.cc.orig	2015-10-14 16:49:00.395597000 +0200
++++ device/serial/serial_io_handler_posix.cc	2015-10-14 17:18:20.515698000 +0200
+@@ -59,7 +59,7 @@
      BITRATE_TO_SPEED_CASE(9600)
      BITRATE_TO_SPEED_CASE(19200)
      BITRATE_TO_SPEED_CASE(38400)
@@ -9,21 +9,3 @@
      BITRATE_TO_SPEED_CASE(57600)
      BITRATE_TO_SPEED_CASE(115200)
      BITRATE_TO_SPEED_CASE(230400)
-@@ -99,7 +99,7 @@
-     SPEED_TO_BITRATE_CASE(9600)
-     SPEED_TO_BITRATE_CASE(19200)
-     SPEED_TO_BITRATE_CASE(38400)
--#if !defined(OS_MACOSX)
-+#if !defined(OS_MACOSX) && !defined(OS_BSD)
-     SPEED_TO_BITRATE_CASE(57600)
-     SPEED_TO_BITRATE_CASE(115200)
-     SPEED_TO_BITRATE_CASE(230400)
-@@ -131,7 +131,7 @@
-     return false;
-   }
-   return true;
--#elif defined(OS_MACOSX)
-+#elif defined(OS_MACOSX) || defined(OS_BSD)
-   speed_t speed = static_cast<speed_t>(bitrate);
-   cfsetispeed(config, speed);
-   cfsetospeed(config, speed);
