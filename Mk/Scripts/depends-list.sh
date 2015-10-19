278dd71cd64bfa519c87a05c66250688bb147f5e
Bryan Drewery <bdrewery@FreeBSD.org>
2015-10-19 19:23:53 +0000

    - Combine clean-depends-list.sh into depends-list.sh - Refactor how depends-list.sh is called from bsd.port.mk for each variant.
    With hat:	portmgr

--- Mk/Scripts/depends-list.sh
+++ Mk/Scripts/depends-list.sh
@@ -7,10 +7,16 @@ set -e
 . ${dp_SCRIPTSDIR}/functions.sh
 
 recursive=0
-while getopts "r" FLAG; do
+requires_wrkdir=0
+while getopts "rw" FLAG; do
 	case "${FLAG}" in
 		r)
 			recursive=1
+			;;
+		w)
+			# Only list dependencies that have a WRKDIR.  Used for
+			# 'make clean-depends'.
+			requires_wrkdir=1
 			;;
 		*)
 			echo "Unknown flag" >&2
@@ -20,8 +26,8 @@ while getopts "r" FLAG; do
 done
 shift $((OPTIND-1))
 
-validate_env dp_ALLDEPENDS dp_PORTSDIR dp_PKGNAME
-if [ ${recursive} -eq 1 ]; then
+validate_env dp_PORTSDIR dp_PKGNAME
+if [ ${recursive} -eq 1 -o ${requires_wrkdir} -eq 1 ]; then
 	validate_env dp_MAKE
 	# Cache command executions to avoid looking them up again in every
 	# sub-make.
@@ -31,6 +37,8 @@ fi
 set -u
 
 check_dep() {
+	local _dep wrkdir show_dep
+
 	for _dep ; do
 		myifs=${IFS}
 		IFS=:
@@ -46,16 +54,36 @@ check_dep() {
 			*\ ${d}\ *) continue ;; # Already checked
 		esac
 		checked="${checked} ${d}"
+		# Check if the dependency actually exists or skip otherwise.
 		if [ ! -d ${d} ]; then
 			echo "${dp_PKGNAME}: \"${d}\" non-existent -- dependency list incomplete" >&2
 			continue
 		fi
-		echo ${d}
+
+		# Grab any needed vars from the port.
+
+		if [ ${requires_wrkdir} -eq 1 -a ${recursive} -eq 1 ]; then
+			set -- $(${dp_MAKE} -C ${d} -VWRKDIR -V_UNIFIED_DEPENDS)
+			wrkdir="$1"
+			shift
+		elif [ ${requires_wrkdir} -eq 1 -a ${recursive} -eq 0 ]; then
+			set -- "$(${dp_MAKE} -C ${d} -VWRKDIR)"
+			wrkdir="$1"
+		elif [ ${recursive} -eq 1 ]; then
+			set -- $(${dp_MAKE} -C ${d} -V_UNIFIED_DEPENDS)
+		fi
+
+		# If a WRKDIR is required to show the dependency, check for it.
+		show_dep=1
+		if [ ${requires_wrkdir} -eq 1 ] && ! [ -d "${wrkdir}" ]; then
+			show_dep=0
+		fi
+		[ ${show_dep} -eq 1 ] && echo ${d}
 		if [ ${recursive} -eq 1 ]; then
-			check_dep $(${dp_MAKE} -C ${d} -V_UNIFIED_DEPENDS)
+			check_dep $@
 		fi
 	done
 }
 
 checked=
-check_dep ${dp_ALLDEPENDS}
+check_dep $@
