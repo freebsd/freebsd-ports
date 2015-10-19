0ab6dff8a115c59aed3475e48cbf054cfadea60b
Rene Ladan <rene@FreeBSD.org>
2015-10-19 18:07:09 +0000

    MFH: r399613 r399621
    www/chromium: update to 46.0.2490.71
    
    Submitted by: Christoph Moench-Tegeder
    Obtained from:	https://github.com/gliaskos/freebsd-chromium/pull/38
    Security:	http://vuxml.freebsd.org/freebsd/8301c04d-71df-11e5-9fcb-00262d5ed8ee.html
    
    www/chromium: actually update files in the base directory, this was missing
    from r399613
    
    Submitted by:	Jan Henrik Sylvester (via private mail)
    
    Approved by:	portmgr (erwin)

--- www/chromium/files/patch-chrome__browser__ui__webui__chrome_web_ui_controller_factory.cc
+++ www/chromium/files/patch-chrome__browser__ui__webui__chrome_web_ui_controller_factory.cc
@@ -1,11 +1,11 @@
---- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc.orig	2015-04-19 00:04:55.000000000 +0200
-+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc	2015-04-19 00:05:15.000000000 +0200
-@@ -249,7 +249,7 @@
+--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc.orig	2015-10-14 16:37:51.456856000 +0200
++++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc	2015-10-14 16:38:21.306588000 +0200
+@@ -273,7 +273,7 @@
  #if !defined(OS_ANDROID)
            || url.host() == chrome::kChromeUITermsHost
  #endif
 -#if defined(OS_LINUX) || defined(OS_OPENBSD)
 +#if defined(OS_LINUX) || defined(OS_BSD)
-           || url.host() == chrome::kChromeUILinuxProxyConfigHost
-           || url.host() == chrome::kChromeUISandboxHost
+           || url.host() == chrome::kChromeUILinuxProxyConfigHost ||
+           url.host() == chrome::kChromeUISandboxHost
  #endif
