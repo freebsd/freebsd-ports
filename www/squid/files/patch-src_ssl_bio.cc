998236359f199d98ff815be465ecda5625379f00
Bernard Spil <spil.oss@gmail.com>
2015-10-23 21:20:43 +0200

    www/squid: Fix No-SSLv3 patches

--- www/squid/files/patch-src_ssl_bio.cc
+++ www/squid/files/patch-src_ssl_bio.cc
@@ -1,11 +1,11 @@
---- src/ssl/bio.cc.orig	2015-09-17 15:06:28.000000000 +0200
-+++ src/ssl/bio.cc	2015-09-26 16:32:06.329826494 +0200
+--- src/ssl/bio.cc.orig	2015-10-01 14:52:11 UTC
++++ src/ssl/bio.cc
 @@ -1009,7 +1009,7 @@ Ssl::Bio::sslFeatures::parseV3Hello(cons
  
      ciphers += 2;
      if (ciphersLen) {
 -        const SSL_METHOD *method = SSLv3_method();
 +        const SSL_METHOD *method = SSLv23_method();
-         const int cs = method->put_cipher_by_char(NULL, NULL);
-         assert(cs > 0);
-         for (size_t i = 0; i < ciphersLen; i += cs) {
+         for (size_t i = 0; i < ciphersLen; i += 2) {
+             // each cipher in v3/tls  HELLO message is of size 2
+             const SSL_CIPHER *c = method->get_cipher_by_char((ciphers + i));
