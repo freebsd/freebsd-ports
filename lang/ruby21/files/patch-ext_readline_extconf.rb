04d112809b44ea9f4c1b66cbf48fb804f876ffd6
mmoll <mmoll@FreeBSD.org>
2015-10-25 00:21:23 +0000

    lang/ruby2*: fix readline & libedit configure options
    The fixed error(s) were only visible with LOCALBASE not set to /usr/local.
    
    PR:		203988
    Submitted by:	John Hein <z7dr6ut7gs@snkmail.com>

--- lang/ruby21/files/patch-ext_readline_extconf.rb
+++ lang/ruby21/files/patch-ext_readline_extconf.rb
@@ -1,6 +1,14 @@
---- ext/readline/extconf.rb.orig	2015-01-23 15:37:01 UTC
-+++ ext/readline/extconf.rb
-@@ -62,7 +62,7 @@ else
+--- ext/readline/extconf.rb.orig	2014-05-01 11:59:37.000000000 +0000
++++ ext/readline/extconf.rb	2015-10-23 04:05:44.000000000 +0000
+@@ -37,6 +37,7 @@
+ case enable_libedit
+ when true
+   # --enable-libedit
++  dir_config("libedit")
+   unless (readline.have_header("editline/readline.h") ||
+           readline.have_header("readline/readline.h")) &&
+           have_library("edit", "readline")
+@@ -62,7 +63,7 @@ else
  end
  
  readline.have_func("rl_getc")
