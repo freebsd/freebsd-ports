dce76b7ab2d62710bfbb660ecb43909f66d65bd6
jbeich <jbeich@FreeBSD.org>
2015-10-28 14:30:46 +0000

    devel/android-tools-simpleperf: update to m.p.752
    Changes:	https://android.googlesource.com/platform/system/extras/+log/af995fe..4ea2a41/simpleperf

--- devel/android-tools-simpleperf/files/patch-simpleperf_record.h
+++ devel/android-tools-simpleperf/files/patch-simpleperf_record.h
@@ -1,10 +1,10 @@
---- simpleperf/record.h.orig	2015-08-12 01:49:40 UTC
+--- simpleperf/record.h.orig	2015-10-26 20:57:19 UTC
 +++ simpleperf/record.h
-@@ -19,6 +19,7 @@
- 
+@@ -20,6 +20,7 @@
+ #include <stdio.h>
  #include <sys/types.h>
  
 +#include <memory> // unique_ptr
+ #include <queue>
  #include <string>
  #include <vector>
- 
