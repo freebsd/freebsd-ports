cfc7c255a049dec22932678bc76c49f81fcb8e90
pawel <pawel@FreeBSD.org>
2015-10-28 23:24:20 +0000

    Are you a writer? Plume Creator will help you with this hard task! This software gives you an outliner, a distraction-free mode, a note manager and much more!
    WWW: http://www.plume-creator.eu/
    
    PR:		200117
    Submitted by:	yamagi@yamagi.org

---
+++ editors/plume-creator-devel/files/patch-src__textzone.cpp
@@ -0,0 +1,8 @@
+--- src/textzone.cpp.orig	2015-05-10 16:46:22 UTC
++++ src/textzone.cpp
+@@ -1,4 +1,4 @@
+-﻿#if QT_VERSION >= 0x050000
++#if QT_VERSION >= 0x050000
+ #include <QtWidgets>
+ #endif
+ #include <QtGui>   
