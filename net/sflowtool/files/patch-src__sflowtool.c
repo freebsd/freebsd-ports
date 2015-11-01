4e4ec78d539c7c3c02de2d7a0ffa99a1a4412c5a
Olli Hauer <ohauer@FreeBSD.org>
2015-11-01 11:18:52 +0000

    - update to 3.37 - change MASTER_SITE to GitHub - use autoreconf - adjust pkg-desc
    Changelog:
    3.37: - print rtmetric and rtflow samples
          - include timestamp in grep-friendly (-g) output
    3.36: - incorporate buffer-overrun protection suggestions from Andre Gruenenberg
          - incorporate portability changes from Rowan Thorpe
          - incorporate IPv6 forwarding changes from Rowan Thorpe
          - print IPID field from IPv4 sampled headers
    3.35: - add Broadcom ASIC counters structure
          - add TCP/IP counters structure
    3.34: - decode new counters in host cpu struct (steal, guest, and guest_nice)
    3.33: - bugfix: app-sample status and duration_uS fields were reversed

--- net/sflowtool/files/patch-src__sflowtool.c
+++ net/sflowtool/files/patch-src__sflowtool.c
@@ -1,33 +1,6 @@
---- ./src/sflowtool.c.orig	2014-05-27 21:09:17.000000000 +0200
-+++ ./src/sflowtool.c	2014-05-31 09:15:00.000000000 +0200
-@@ -442,7 +442,7 @@
- void *my_calloc(size_t bytes) {
-   void *mem = calloc(1, bytes);
-   if(mem == NULL) {
--    fprintf(ERROUT, "calloc(%u) failed: %s\n", bytes, strerror(errno));
-+    fprintf(ERROUT, "calloc(%zu) failed: %s\n", bytes, strerror(errno));
-     exit(-1);
-   }
-   return mem;
-@@ -554,7 +554,7 @@
-   register char c, *r = in, *w = out;
-   int maxlen = (strlen(in) * 3) + 1;
-   if(outlen < maxlen) return "URLEncode: not enough space";
--  while (c = *r++) {
-+  while ((c = *r++)) {
-     if(isalnum(c)) *w++ = c;
-     else if(isspace(c)) *w++ = '+';
-     else {
-@@ -3075,7 +3075,7 @@
-   uint8_t *uuid;
-   char hostname[SFL_MAX_HOSTNAME_LEN+1];
-   char os_release[SFL_MAX_OSRELEASE_LEN+1];
--  char uuidStr[100];
-+  u_char uuidStr[100];
-   if(getString(sample, hostname, SFL_MAX_HOSTNAME_LEN) > 0) {
-     sf_log(sample,"hostname %s\n", hostname);
-   }
-@@ -3921,7 +3921,8 @@
+--- src/sflowtool.c.orig	2015-10-06 05:37:21 UTC
++++ src/sflowtool.c
+@@ -4308,7 +4308,8 @@ static int ipv4MappedAddress(SFLIPv6 *ip
  static void readPacket(int soc)
  {
    struct sockaddr_in6 peer;
