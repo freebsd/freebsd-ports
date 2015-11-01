59acc2b1d4a24f79c5cc210ed156e7435ec9db76
rene <rene@FreeBSD.org>
2015-11-01 16:02:28 +0000

    Remove expired ports: 2015-10-31 audio/cmus-post.fm: Broken for more than 6 months 2015-10-31 devel/libphish: Broken for more than 6 months 2015-10-31 devel/rubygem-debugger: This port is for Ruby 1.9 only. Upstream stopped maintenance. 2015-10-31 games/linux-ningpo-demo: Broken for more than 6 months 2015-10-30 graphics/gdal-grass: Superseded by gdal-2.0.0 2015-10-31 graphics/picviz: Broken for more than 6 months 2015-10-31 irc/weechat-devel: far behind stable irc/weechat (use it instead) 2015-10-31 mail/cucipop: Broken for more than 6 months 2015-10-31 math/rkward-kde4: Depends on deprecated math/R-cran-car 2015-10-31 multimedia/streamanalyze: Broken for more than 6 months 2015-10-31 net-mgmt/collectd: Please move to collectd5 - collect4 is not developed/patched 2015-10-31 sysutils/sdd: Broken for more than 6 months 2015-10-31 sysutils/xfce4-minicmd-plugin: Broken for more than 6 months 2015-10-31 www/linux-libgtkembedmoz: Broken for more than 6 months 2015-10-31 www/py-feedfinder: Broken for more than 6 months 2015-10-31 www/py-openssl-proxy: Broken for more than 6 months 2015-10-31 www/py-webware: Broken for more than 6 months 2015-10-31 www/py-webware-component: Depends on deprecated www/py-webware 2015-10-31 x11-fonts/code2001: Broken for more than 6 months 2015-10-31 x11-wm/e-module-diskio: Broken for more than 7 months

--- net-mgmt/collectd/files/patch-version-gen.sh
+++
@@ -1,7 +0,0 @@
---- version-gen.sh.orig	2012-11-11 11:43:05.000000000 +0100
-+++ version-gen.sh	2012-12-06 14:49:39.204586447 +0100
-@@ -1,4 +1,4 @@
--#!/usr/bin/env bash
-+#!/usr/bin/env sh
- 
- DEFAULT_VERSION="4.10.8.git"
