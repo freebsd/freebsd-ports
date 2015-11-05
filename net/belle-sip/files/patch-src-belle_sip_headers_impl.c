34bd132f1e38b559690ffc49eabb6f49aa56db95
bofh <bofh@FreeBSD.org>
2015-11-05 12:11:32 +0000

    net/belle-sip: Update version 1.4.1=>1.4.2
    - Fix patch files for 'make makepatch' compatibility

--- net/belle-sip/files/patch-src-belle_sip_headers_impl.c
+++ net/belle-sip/files/patch-src-belle_sip_headers_impl.c
@@ -1,26 +1,30 @@
---- src/belle_sip_headers_impl.c.orig	2015-01-20 10:39:04 UTC
+--- src/belle_sip_headers_impl.c.orig	2015-10-19 11:43:47 UTC
 +++ src/belle_sip_headers_impl.c
-@@ -1663,7 +1663,6 @@ BELLESIP_EXPORT time_t belle_sip_header_
- 	char tmp2[16] ={0};
- 	int i,j;
+@@ -1670,8 +1670,6 @@ BELLESIP_EXPORT time_t belle_sip_header_
  	time_t seconds;
+ #ifdef BELLE_SIP_WINDOWS_UNIVERSAL
+ 	long adjust_timezone;
+-#else
 -	time_t adjust_timezone;
+ #endif
  
  
- 	/* time headers are in GMT as spec says */
-@@ -1685,22 +1684,12 @@ BELLESIP_EXPORT time_t belle_sip_header_
- 	return (time_t)-1;
+@@ -1695,25 +1693,11 @@ BELLESIP_EXPORT time_t belle_sip_header_
  success:
  	ret.tm_isdst=0;
--
+ 
 -#if TARGET_IPHONE_SIMULATOR
 -	/* 'timezone' is buggy on iOS simulator, use the timegm() function to convert to UTC timestamp
 -	   and discard the adjust timezone value */
- 	seconds = timegm(&ret);
+-	seconds = timegm(&ret);
 -	adjust_timezone = 0;
 -#else
 -	seconds = mktime(&ret);
+-#ifdef BELLE_SIP_WINDOWS_UNIVERSAL
+-	_get_timezone(&adjust_timezone);
+-#else
 -	adjust_timezone = timezone;
+-#endif
 -#endif
 -
  	if (seconds==(time_t)-1){
@@ -28,7 +32,7 @@
 +		belle_sip_error("timegm() failed: %s",strerror(errno));
  		return (time_t)-1;
  	}
--	return seconds-adjust_timezone;
+-	return seconds-(time_t)adjust_timezone;
 +	return seconds;
  }
  
