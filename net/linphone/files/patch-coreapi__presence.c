7b0228786918f72ca345b492b9ba807474c34d2f
tijl <tijl@FreeBSD.org>
2015-11-06 16:24:13 +0000

    Update the Linphone stack.
    Linphone 3.9.0
    Ortp 0.25.0 + bump PORTREVISION on dependent ports (devel/libosmo-abis)
    Libbzrtp 1.0.3
    Belle-sip (fix files/patch-src-belle_sip_headers_impl.c)
    Mediastreamer 2.12.0
    MS plugins (msbcg729 1.0.1, msilbc 2.1.2, msopenh264 1.1.1, msx264 1.5.2)

--- net/linphone/files/patch-coreapi__presence.c
+++ net/linphone/files/patch-coreapi__presence.c
@@ -1,6 +1,18 @@
---- coreapi/presence.c.orig	2015-04-02 09:04:01 UTC
+--- coreapi/presence.c.orig	2015-10-23 09:10:07 UTC
 +++ coreapi/presence.c
-@@ -164,12 +164,12 @@ static time_t parse_timestamp(const char
+@@ -157,11 +157,6 @@ static void presence_activity_delete(Lin
+ static time_t parse_timestamp(const char *timestamp) {
+ 	struct tm ret;
+ 	time_t seconds;
+-#ifdef LINPHONE_WINDOWS_UNIVERSAL
+-	long adjust_timezone;
+-#else
+-	time_t adjust_timezone;
+-#endif
+ 
+ 	memset(&ret, 0, sizeof(ret));
+ 	sscanf(timestamp, "%d-%d-%dT%d:%d:%d",
+@@ -169,17 +164,12 @@ static time_t parse_timestamp(const char
  	ret.tm_mon--;
  	ret.tm_year -= 1900;
  	ret.tm_isdst = 0;
@@ -11,7 +23,12 @@
 +		ms_error("timegm() failed: %s", strerror(errno));
  		return (time_t)-1;
  	}
--	return seconds - timezone;
+-#ifdef LINPHONE_WINDOWS_UNIVERSAL
+-	_get_timezone(&adjust_timezone);
+-#else
+-	adjust_timezone = timezone;
+-#endif
+-	return seconds - (time_t)adjust_timezone;
 +	return seconds;
  }
  
