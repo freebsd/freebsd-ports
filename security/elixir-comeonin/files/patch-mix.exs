b22dac7503f1ae03f054893a119ec4a64b0361c0
olgeni <olgeni@FreeBSD.org>
2015-11-07 14:01:04 +0000

    Upgrade security/elixir-comeonin to version 1.4.

--- security/elixir-comeonin/files/patch-mix.exs
+++ security/elixir-comeonin/files/patch-mix.exs
@@ -1,9 +1,9 @@
---- mix.exs.orig	2015-07-14 14:26:45 UTC
+--- mix.exs.orig	2015-11-06 02:07:11 UTC
 +++ mix.exs
-@@ -68,7 +68,6 @@ defmodule Comeonin.Mixfile do
+@@ -122,7 +122,6 @@ defmodule Comeonin.Mixfile do
        package: package,
        source_url: "https://github.com/elixircnx/comeonin",
-       compilers: [:comeonin, :elixir, :app],
+       compilers: [:comeonin, :gettext, :elixir, :app],
 -      deps: deps
      ]
    end
