bc9f2fb36d1ccf541a931ba389e532b76e840f09
sbruno <sbruno@FreeBSD.org>
2015-11-09 14:59:36 +0000

    Update port Makefile to support mips build of cairo.
    Add License Information
    
    PR:		201683
    Submitted by:	Carlos J Puga Medina <cpm@fbsd.es>

--- graphics/cairo/files/patch-src_cairo-xlib-display.c
+++ graphics/cairo/files/patch-src_cairo-xlib-display.c
@@ -1,6 +1,6 @@
---- src/cairo-xlib-display.c.orig	2010-11-21 14:41:31.000000000 -0500
-+++ src/cairo-xlib-display.c	2010-11-21 14:42:12.000000000 -0500
-@@ -353,11 +353,7 @@ _cairo_xlib_device_create (Display *dpy)
+--- src/cairo-xlib-display.c.orig	2015-03-10 22:21:07 UTC
++++ src/cairo-xlib-display.c
+@@ -263,11 +263,7 @@ _cairo_xlib_device_create (Display *dpy)
      /* Prior to Render 0.10, there is no protocol support for gradients and
       * we call function stubs instead, which would silently consume the drawing.
       */
