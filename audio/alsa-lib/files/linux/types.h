c14615bc50ff46bcb0de6c666f14936ea414d486
jbeich <jbeich@FreeBSD.org>
2015-11-10 22:21:08 +0000

    audio/alsa-*: update to 1.1.0
    Changes:	http://www.alsa-project.org/main/index.php/Changes_v1.0.29_v1.1.0
    ACC report:	https://people.freebsd.org/~jbeich/compat_reports/alsa-lib/1.0.29_to_1.1.0/compat_report.html

--- audio/alsa-lib/files/linux/types.h
+++ audio/alsa-lib/files/linux/types.h
@@ -1,11 +1,17 @@
 #ifndef _LINUX_TYPES_H
 #define _LINUX_TYPES_H
 
-#define __bitwise
+#include <sys/types.h>
 
-typedef int __kernel_pid_t;
+#define __bitwise
+#define __le16 __u16
+#define __le32 __u32
+#define __le64 __u64
+
+typedef pid_t __kernel_pid_t;
 typedef off_t __kernel_off_t;
 
+typedef uint16_t __u16;
 typedef uint32_t __u32;
 typedef uint64_t __u64;
 #endif /* _LINUX_TYPES_H */
