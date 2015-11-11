8c5fa79bd92ef34bf7d5d8b98cdf7d08706177bb
pawel <pawel@FreeBSD.org>
2015-11-11 15:40:39 +0000

    - Fix build with gcc 4.9 [1] - Use option helper
    PR:		204395 [1]
    Reported by:	gerald [1]

---
+++ polish/kadu/files/patch-kadu-core_execution-arguments_execution-arguments-parser.h
@@ -0,0 +1,11 @@
+--- kadu-core/execution-arguments/execution-arguments-parser.h.orig	2015-11-09 20:19:32 UTC
++++ kadu-core/execution-arguments/execution-arguments-parser.h
+@@ -20,7 +20,7 @@
+ #pragma once
+ 
+ #include "exports.h"
+-
++#include <cstdio>
+ #include <QtCore/QStringList>
+ 
+ class ExecutionArguments;
