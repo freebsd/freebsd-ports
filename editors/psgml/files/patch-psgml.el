8402b7a61056fe978bca48521f75872d799f81d7
Hiroki Sato <hrs@FreeBSD.org>
2015-11-12 07:09:05 +0000

    - Fix broken patches.
    - Remove obsolete xemacs support.
    
    - Bump version number to 1.4.0 to avoid possible misunderstandings.
      Major Linux distributions have adopted an unofficial version
      labeled as 1.4.0 which includes fixes for Emacs 24.x while the
      official distfile is still 1.3.2.  There is no substantial
      difference between 1.3.2 and 1.4.0 except for Emacs 24.x compatibility,
      and this FreeBSD port has already had almost the same changes.
      Although similar unofficial versions can be found at [1] or [2],
      they look inconsistent and/or not actively maintained.  The official
      distfile is used for this reason.
    
    [1] http://www.fsavigny.de/gpled-software/psgml-1.4.0.tar.gz
    [2] https://marmalade-repo.org/packages/psgml

--- editors/psgml/files/patch-psgml.el
+++ editors/psgml/files/patch-psgml.el
@@ -1,5 +1,5 @@
---- psgml.el.orig	2015-11-12 14:12:58.516938000 +0900
-+++ psgml.el	2015-11-12 14:13:03.188835000 +0900
+--- psgml.el.orig	2015-11-12 06:36:57 UTC
++++ psgml.el
 @@ -52,7 +52,7 @@
  
  ;;; Code:
@@ -9,7 +9,34 @@
    "Version of psgml package.")
  
  (defconst psgml-maintainer-address "lenst@lysator.liu.se")
-@@ -724,9 +724,9 @@
+@@ -381,7 +381,7 @@ Setting this variable automatically make
+ (put 'sgml-system-path 'sgml-type 'file-list)
+ 
+ (defvar sgml-public-map (or (sgml-parse-colon-path (getenv "SGML_PATH"))
+-			    '("%S" "/usr/local/lib/sgml/%o/%c/%d" ))
++			    '("%S" "@prefix@/share/sgml/%o/%c/%d" ))
+   "*Mapping from public identifiers to file names.
+ This is a list of possible file names.  To find the file for a public
+ identifier the elements of the list are used one at the time from the
+@@ -407,13 +407,15 @@ This variable is automatically local to 
+ (defvar sgml-catalog-files (or (delete nil
+ 				       (sgml-parse-colon-path
+ 					(getenv "SGML_CATALOG_FILES")))
+-			       '("catalog" "/usr/local/lib/sgml/catalog"))
++			       '("catalog" "@prefix@/share/sgml/catalog"
++			         "@prefix@/share/xml/catalog"))
+   "*List of catalog entry files.
+ The files are in the format defined in the SGML Open Draft Technical
+ Resolution on Entity Management.")
+ (put 'sgml-catalog-files 'sgml-type 'file-list)
+ 
+-(defvar sgml-ecat-files '("ECAT" "~/sgml/ECAT" "/usr/local/lib/sgml/ECAT")
++(defvar sgml-ecat-files '("ECAT" "~/sgml/ECAT" "@prefix@/share/sgml/ECAT"
++			  "@prefix@/share/xml/ECAT")
+   "*List of catalog files for PSGML.")
+ (put 'sgml-ecat-files 'sgml-type 'file-list)
+ 
+@@ -722,9 +724,9 @@ as that may change."
  
  (defun sgml-markup (entry text)
    (cons entry
@@ -21,7 +48,7 @@
  
  (defun sgml-insert-markup (text)
    (let ((end (sgml-mouse-region))
-@@ -1042,13 +1042,13 @@
+@@ -1040,13 +1042,13 @@ as that may change."
  (defun sgml-compute-insert-dtd-items ()
    (loop for e in sgml-custom-dtd collect
          (vector (first e)
@@ -37,7 +64,7 @@
                  t)))
  
  (defun sgml-build-custom-menus ()
-@@ -1231,12 +1231,14 @@
+@@ -1229,12 +1231,14 @@ All bindings:
      (make-local-variable 'text-property-default-nonsticky)
      ;; see `sgml-set-face-for':
      (add-to-list 'text-property-default-nonsticky '(face . t)))
