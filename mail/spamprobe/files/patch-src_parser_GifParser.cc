c905c203db0c9756a47ecca2243013bfa462026d
Antoine Brodin <antoine@FreeBSD.org>
2015-11-12 18:35:53 +0000

    Allow building with giflib 5.1
    PR:		204492

--- mail/spamprobe/files/patch-src_parser_GifParser.cc
+++ mail/spamprobe/files/patch-src_parser_GifParser.cc
@@ -1,6 +1,18 @@
 --- src/parser/GifParser.cc.orig	2006-11-17 07:24:49 UTC
 +++ src/parser/GifParser.cc
-@@ -99,7 +99,11 @@ bool GifParser::parseImage()
+@@ -81,7 +81,11 @@ GifParser::GifParser(Message *message,
+ GifParser::~GifParser()
+ {
+   if (m_gif) {
++#if GIFLIB_MAJOR == 5 && GIFLIB_MINOR >= 1 || GIFLIB_MAJOR > 5
++    DGifCloseFile(m_gif, NULL);
++#else
+     DGifCloseFile(m_gif);
++#endif
+   }
+ }
+ 
+@@ -99,7 +103,11 @@ bool GifParser::parseImage()
  void GifParser::openImage()
  {
    m_nextByteIndex = 0;
