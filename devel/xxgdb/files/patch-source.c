0a8fcae0a3773070b8d29faea5be5628d56b3627
marino <marino@FreeBSD.org>
2015-11-13 09:02:16 +0000

    devel/xxgdb: Expand patches to support malloc.h-less DF
    While here, regenerate all the patches.

--- devel/xxgdb/files/patch-source.c
+++ devel/xxgdb/files/patch-source.c
@@ -1,11 +1,11 @@
---- source.c.orig	Sat Apr 22 01:16:02 1995
-+++ source.c	Fri Nov 17 21:38:58 2000
+--- source.c.orig	1995-04-21 16:16:02 UTC
++++ source.c
 @@ -74,7 +74,7 @@
   *    LoadFile():	Search for a file and open it for display.
   */
  
 -#ifndef NeXT
-+#if !defined(NeXT) && !defined(__FreeBSD__)
++#if !defined(NeXT) && !defined(__FreeBSD__) && !defined(__DragonFly__)
  #include <malloc.h>
  #endif
  #include <stdlib.h>
