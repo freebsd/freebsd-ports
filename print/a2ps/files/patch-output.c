7162ab293f5762236d16ebcfcecf8ed5dace698a
dinoex <dinoex@FreeBSD.org>
2015-11-18 06:21:59 +0000

    - fix whitespaces in patch

--- print/a2ps/files/patch-output.c
+++ print/a2ps/files/patch-output.c
@@ -1,13 +1,13 @@
 Fix for CVE-2015-8107
 http://www.openwall.com/lists/oss-security/2015/11/16/4
---- lib/output.c.orig  2015-11-16 15:29:38 UTC
-+++ lib/output.c
-@@ -525,7 +525,7 @@ output_file (struct output * out, a2ps_j
-                    expand_user_string (job, FIRST_FILE (job),
-                                        (const uchar *) "Expand: requirement",
-                                        (const uchar *) token));
--      output (dest, expansion);
-+      output (dest, "%s", expansion);
-       continue;
+--- lib/output.c.orig	1999-08-31 19:42:41.000000000 +0200
++++ lib/output.c	2015-11-18 07:08:31.672864000 +0100
+@@ -525,7 +525,7 @@
+ 		     expand_user_string (job, FIRST_FILE (job),
+ 					 (const uchar *) "Expand: requirement",
+ 					 (const uchar *) token));
+-	output (dest, expansion);
++	output (dest, "%s", expansion);
+ 	continue;
        }
-
+ 
