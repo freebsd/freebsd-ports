9a44a67a58a0c307eb447f4011f3f448c5f85dd4
amdmi3 <amdmi3@FreeBSD.org>
2015-11-18 10:14:12 +0000

    - Add LICENSE - Switch to options helpers - Regenerate patches with `make makepatch`

--- audio/ksmp3play/files/patch-ksmp3play.c
+++ audio/ksmp3play/files/patch-ksmp3play.c
@@ -1,6 +1,6 @@
---- ksmp3play.c.orig	Mon Aug 20 04:43:37 2001
-+++ ksmp3play.c	Sun Dec 29 11:01:56 2002
-@@ -22,6 +22,7 @@
+--- ksmp3play.c.orig	2008-06-17 05:57:56 UTC
++++ ksmp3play.c
+@@ -42,6 +42,7 @@
  #include "ksmp3play.h"
  #include <smpeg/smpeg.h>
  #include <curses.h>
