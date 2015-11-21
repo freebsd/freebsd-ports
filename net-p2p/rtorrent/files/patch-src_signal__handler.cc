2a4ace345a87a616c3153aa1771353519572fe82
sunpoet <sunpoet@FreeBSD.org>
2015-11-21 02:10:09 +0000

    - Update LICENSE - Use PLIST_FILES and PORTEXAMPLES instead of PLIST - Use USE_CXXSTD - Convert to new options target helper - Cosmetic change - Pet portlint: fix diff header of patch files

--- net-p2p/rtorrent/files/patch-src_signal__handler.cc
+++ net-p2p/rtorrent/files/patch-src_signal__handler.cc
@@ -1,5 +1,5 @@
---- src/signal_handler.cc.orig	2012-03-29 15:06:11.000000000 +0200
-+++ src/signal_handler.cc	2014-02-02 22:46:43.000000000 +0100
+--- src/signal_handler.cc.orig	2014-05-14 14:30:51 UTC
++++ src/signal_handler.cc
 @@ -38,6 +38,7 @@
  
  #include <signal.h>
