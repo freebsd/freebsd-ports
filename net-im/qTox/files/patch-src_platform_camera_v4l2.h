013380961fe4b69de628a78aee094f42c94a43d6
thierry <thierry@FreeBSD.org>
2015-11-21 10:57:18 +0000

    Update to 0.151116.
    PR:		ports/204591
    Submitted by:	yuri (at) rawbw.com (maintainer)

--- net-im/qTox/files/patch-src_platform_camera_v4l2.h
+++ net-im/qTox/files/patch-src_platform_camera_v4l2.h
@@ -1,4 +1,4 @@
---- src/platform/camera/v4l2.h.orig	2015-06-12 10:53:59 UTC
+--- src/platform/camera/v4l2.h.orig	2015-11-14 22:51:30 UTC
 +++ src/platform/camera/v4l2.h
 @@ -24,7 +24,7 @@
  #include <QPair>
