209812b2d10cca6f053bc79938259579c8898fdd
pi <pi@FreeBSD.org>
2015-11-21 14:00:50 +0000

    sysutils/rename: unbreak, new distsite
    PR:		204498
    Submitted by:	matthew@reztek.cz

--- sysutils/rename/files/patch-rename.c
+++ sysutils/rename/files/patch-rename.c
@@ -1,5 +1,5 @@
---- ./rename.c.orig	2002-05-20 11:50:50.000000000 -0400
-+++ ./rename.c	2011-03-13 05:05:52.000000000 -0400
+--- rename.c.orig	2002-05-20 15:50:50 UTC
++++ rename.c
 @@ -65,7 +65,7 @@
  
  #if HAVE_REGEX_H
@@ -9,7 +9,7 @@
    #include "regex.h"
  #endif
  
-@@ -608,7 +608,7 @@
+@@ -608,7 +608,7 @@ int do_rename(char *oldp, char *newp)
  
  skip:
      if (attr & MOD_VERBO) 
@@ -18,7 +18,7 @@
      return rs;
  }
  
-@@ -659,6 +659,6 @@
+@@ -659,6 +659,6 @@ Report bugs to <xuming@bigfoot.com>.\n";
      if (mode)  
  	printf(version, VERSION);  
      else  
