5059a978bc2900f38be0d5399a0036fd923534f0
araujo <araujo@FreeBSD.org>
2015-11-24 09:17:47 +0000

    - Update to 2015.11.24.
    PR:		ports/204776
    Submitted by:	Christoph Moench-Tegeder <cmt@burggraben.net>

--- www/youtube_dl/files/patch-youtube_dl____init__.py
+++ www/youtube_dl/files/patch-youtube_dl____init__.py
@@ -1,6 +1,6 @@
---- youtube_dl/__init__.py.orig	2015-05-15 08:01:24 UTC
-+++ youtube_dl/__init__.py
-@@ -36,7 +36,6 @@ from .utils import (
+--- youtube_dl/__init__.py.orig	2015-11-24 09:44:40.030482000 +0100
++++ youtube_dl/__init__.py	2015-11-24 09:45:54.856110000 +0100
+@@ -36,7 +36,6 @@
      std_headers,
      write_string,
  )
@@ -8,13 +8,13 @@
  from .downloader import (
      FileDownloader,
  )
-@@ -370,17 +369,13 @@ def _real_main(argv=None):
+@@ -375,17 +374,13 @@
      }
  
      with YoutubeDL(ydl_opts) as ydl:
 -        # Update version
 -        if opts.update_self:
--            update_self(ydl.to_screen, opts.verbose)
+-            update_self(ydl.to_screen, opts.verbose, ydl._opener)
 -
          # Remove cache dir
          if opts.rm_cachedir:
