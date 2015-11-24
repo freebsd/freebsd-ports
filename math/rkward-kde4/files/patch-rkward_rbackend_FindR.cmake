09709725e063e1be4ee9756da06607b6aba17148
thierry <thierry@FreeBSD.org>
2015-11-24 21:34:05 +0000

    Update to 0.6.3.
    PR:		ports/204738
    Submitted by:	Rainer Hurling

--- math/rkward-kde4/files/patch-rkward_rbackend_FindR.cmake
+++ math/rkward-kde4/files/patch-rkward_rbackend_FindR.cmake
@@ -1,6 +1,6 @@
---- rkward/rbackend/FindR.cmake.orig	2011-02-23 06:43:20.000000000 -0500
-+++ rkward/rbackend/FindR.cmake	2011-05-03 01:42:49.000000000 -0400
-@@ -99,7 +99,7 @@
+--- rkward/rbackend/FindR.cmake.orig	2015-01-15 13:37:21 UTC
++++ rkward/rbackend/FindR.cmake
+@@ -120,7 +120,7 @@ ELSE(NOT LIBR_LAPACK)
  		# needed when linking to Rlapack on linux for some unknown reason.
  		# apparently not needed on windows (let's see, when it comes back to bite us, though)
  		# and compiling on windows is hard enough even without requiring libgfortran, too.
