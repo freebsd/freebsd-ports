1eb69a9b28d7c41225d4ab4d8633faf71daca8a3
John Marino <marino@FreeBSD.org>
2015-11-29 11:56:04 +0000

    devel/lfcbase: Fix build on modern gcc (add <cstdlib>)
    Approved by:	modern gcc/just fix it blankets

---
+++ devel/lfcbase/files/patch-basecheck_MatchTest.cc
@@ -0,0 +1,10 @@
+--- basecheck/MatchTest.cc.orig	2015-11-18 15:42:02 UTC
++++ basecheck/MatchTest.cc
+@@ -11,6 +11,7 @@
+ //
+ ///////////////////////////////////////////////////////////////////////////////
+ 
++#include <cstdlib>
+ #include "Exception.h"
+ #include "Matcher.h"
+ 
