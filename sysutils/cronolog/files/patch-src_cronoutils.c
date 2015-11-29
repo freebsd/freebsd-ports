e28a239c8e1891f29e80e1b22109f87672e52028
Olli Hauer <ohauer@FreeBSD.org>
2015-11-29 15:41:07 +0000

    - original MASTER_SITE is gone.   Author pushed the source to github, use it as new site [1] - add extra-patch to PATCHDIR - use new post-patch target - unbreak the port - bump PORTREVISION
    [1] verified there is no difference between old 1.6.2 and github 8ea64c0

--- sysutils/cronolog/files/patch-src_cronoutils.c
+++ sysutils/cronolog/files/patch-src_cronoutils.c
@@ -1,6 +1,6 @@
---- cronolog-1.6.2/src/cronoutils.c.orig	Thu May  3 17:43:21 2001
-+++ cronolog-1.6.2/src/cronoutils.c	Mon Dec 11 00:48:23 2006
-@@ -199,7 +199,7 @@
+--- src/cronoutils.c.orig	2013-12-04 14:46:09 UTC
++++ src/cronoutils.c
+@@ -199,7 +199,7 @@ create_link(char *pfilename, 
      {
  	unlink(prevlinkname);
      }
