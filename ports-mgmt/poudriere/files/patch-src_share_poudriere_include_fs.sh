8f4111adfb76efb322cdcab883602cf2004a1197
bdrewery <bdrewery@FreeBSD.org>
2015-11-30 17:26:50 +0000

    Revert usage of -x for rm(1) for now due to 9.x not having it.

---
+++ ports-mgmt/poudriere/files/patch-src_share_poudriere_include_fs.sh
@@ -0,0 +1,10 @@
+--- src/share/poudriere/include/fs.sh.orig	2015-11-30 17:25:11 UTC
++++ src/share/poudriere/include/fs.sh
+@@ -181,6 +181,6 @@ destroyfs() {
+ 		rmdir ${mnt}
+ 	else
+ 		chflags -R noschg ${mnt}
+-		rm -rfx ${mnt}
++		rm -rf ${mnt}
+ 	fi
+ }
