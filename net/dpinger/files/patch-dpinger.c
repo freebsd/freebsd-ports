38b416a729db02e4b7ebd400a301bcea0981fc4d
Renato Botelho <garga@FreeBSD.org>
2015-11-30 11:45:36 +0000

    - Update net/dpinger to 0.0.1.20151129 (commit c0adc9e57f) - Remove patches already applied upstream - Fix build on FreeBSD 9

--- net/dpinger/files/patch-dpinger.c
+++ net/dpinger/files/patch-dpinger.c
@@ -1,11 +1,40 @@
---- dpinger.c.orig	2015-11-25 04:21:35 UTC
+--- dpinger.c.orig	2015-11-30 00:12:21 UTC
 +++ dpinger.c
-@@ -164,7 +164,7 @@ cksum(
+@@ -497,7 +497,7 @@ alert_thread(
+     unsigned int		latency_alarm_decay = 0;
+     unsigned int		loss_alarm_decay = 0;
+     unsigned int		alert = 0;
+-    unsigned int		alarm;
++    unsigned int		alarm_on;
+     int				r;
  
-     if (len == 1)
-     {
--	sum += (uint16_t) *((uint8_t *) p);
-+	sum += (uint16_t) *((const uint8_t *) p);
+     // Set up the timespec for nanosleep
+@@ -599,12 +599,12 @@ alert_thread(
+ 	{
+ 	    alert = 0;
+ 
+-	    alarm = latency_alarm_decay || loss_alarm_decay;
+-	    logger("%s: latency %luus loss %lus\n", alarm ? "Alarm" : "Clear", average_latency, average_loss);
++	    alarm_on = latency_alarm_decay || loss_alarm_decay;
++	    logger("%s: latency %luus loss %lus\n", alarm_on ? "Alarm" : "Clear", average_latency, average_loss);
+ 
+ 	    if (alert_cmd)
+ 	    {
+-		r = snprintf(alert_cmd + alert_cmd_offset, ALERT_CMD_OUTPUT_MAX, " %u %lu %lu", alarm, average_latency, average_loss);
++		r = snprintf(alert_cmd + alert_cmd_offset, ALERT_CMD_OUTPUT_MAX, " %u %lu %lu", alarm_on, average_latency, average_loss);
+ 		if (r < 0 || r >= (int) ALERT_CMD_OUTPUT_MAX)
+ 		{
+ 		    logger("error formatting alert command\n");
+@@ -891,9 +891,9 @@ parse_args(
+ 		fatal("Invalid bind IP address %s\n", bind_arg);
+ 	    }
+ 
+-	    struct sockaddr_in * bind = (struct sockaddr_in *) &bind_addr;
+-	    bind->sin_family = AF_INET;
+-	    bind->sin_addr = addr;
++	    struct sockaddr_in * bind4 = (struct sockaddr_in *) &bind_addr;
++	    bind4->sin_family = AF_INET;
++	    bind4->sin_addr = addr;
+ 	    bind_addr_len = sizeof(struct sockaddr_in);
+ 	}
      }
- 
-     sum = (sum >> 16) + (sum & 0xFFFF);
