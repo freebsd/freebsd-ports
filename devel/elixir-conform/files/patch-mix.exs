e4aeed8cf171bc1749df49c56ac32028513cb6bc
olgeni <olgeni@FreeBSD.org>
2015-11-30 21:04:48 +0000

    Upgrade devel/elixir-conform to version 1.0.0rc6.

--- devel/elixir-conform/files/patch-mix.exs
+++ devel/elixir-conform/files/patch-mix.exs
@@ -1,12 +1,17 @@
---- mix.exs.orig	2015-08-18 14:59:08 UTC
+--- mix.exs.orig	2015-11-30 21:02:08 UTC
 +++ mix.exs
-@@ -26,9 +26,6 @@ defmodule Conform.Mixfile do
+@@ -25,13 +25,10 @@ defmodule Conform.Mixfile do
+   defp compilers(true), do: [:peg, :erlang, :elixir, :app]
    defp compilers(_),    do: nil
  
-   defp deps(true), do: [
--    {:neotoma, "~> 1.7.3"},
+-  defp deps(true), do: [{:neotoma, "~> 1.7.3"}] ++ core_deps()
++  defp deps(true), do: [] ++ core_deps()
+   defp deps(_),    do: core_deps()
+ 
+   defp core_deps(), do: [
+-    {:exrm, "~> 1.0.0-rc5", optional: true},
 -    {:ex_doc, "~> 0.7", only: [:docs, :dev]},
 -    {:earmark, "~> 0.1", only: [:docs, :dev]}
    ]
-   defp deps(_),    do: []
  
+   defp description, do: "Easy release configuration for Elixir apps."
