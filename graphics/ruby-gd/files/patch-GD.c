eb79b2811c9c0be8c7d58a9f4fa02937059f6ccd
dinoex <dinoex@FreeBSD.org>
2015-12-03 14:20:35 +0000

    - fix build for ruby >= 2.1

--- graphics/ruby-gd/files/patch-GD.c
+++ graphics/ruby-gd/files/patch-GD.c
@@ -1,6 +1,6 @@
---- GD.c.orig	Tue Feb  6 15:47:42 2001
-+++ GD.c	Mon Feb 12 19:39:15 2001
-@@ -1200,7 +1200,7 @@
+--- GD.c.orig	2001-05-30 16:14:17.000000000 +0200
++++ GD.c	2015-12-03 15:03:58.428323000 +0100
+@@ -1191,7 +1191,7 @@
      Check_Type(fontname, T_STRING);
      Check_Type(string, T_STRING);
  
@@ -9,7 +9,7 @@
                             &brect[0],
                             NUM2INT(fgcolor),
                             RSTRING(fontname)->ptr,
-@@ -1232,7 +1232,7 @@
+@@ -1223,7 +1223,7 @@
      Check_Type(string, T_STRING);
  
      Data_Get_Struct(img, gdImage, im);
@@ -18,7 +18,16 @@
                             &brect[0],
                             NUM2INT(fgcolor),
                             RSTRING(fontname)->ptr,
-@@ -1971,7 +1971,9 @@
+@@ -1613,7 +1613,7 @@
+ {
+     VALUE self = rb_ary_new();
+ 
+-    RBASIC(self)->klass = klass;
++    rb_obj_reveal(self, klass);
+     return self;
+ }
+ 
+@@ -2508,7 +2508,9 @@
  #endif    
  
  #ifdef HAVE_GDIMAGESTRINGFT
