4c39033031ef2df30e59e825e2bffed9dc0f96a5
sunpoet <sunpoet@FreeBSD.org>
2015-12-04 12:32:58 +0000

    - Update to 0.12 - Add LICENSE_FILE - Convert to new options helper - Convert to new options target helper - Convert to new test framework - Remove EXTRACT_AFTER_ARGS - Sort USES - Use USES=shebangfix - Repack test tarball - Cosmetic change - Split and rename patch files - Regenerate patch files with makepatch: - Update WWW - Add git repository to WWW - Take maintainership
    Changes:	http://git.ghostscript.com/?p=jbig2dec.git;a=blob;f=CHANGES;hb=HEAD

--- graphics/jbig2dec/files/patch-jbig2_image_png.c
+++ graphics/jbig2dec/files/patch-jbig2_image_png.c
@@ -1,6 +1,14 @@
---- jbig2_image_png.c
+--- jbig2_image_png.c.orig	2014-10-31 13:30:04 UTC
 +++ jbig2_image_png.c
-@@ -33,7 +33,7 @@
+@@ -26,7 +26,6 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <png.h>
+-#include <pngstruct.h>
+ #define CVT_PTR(ptr) (ptr)
+ 
+ #include "jbig2.h"
+@@ -40,7 +39,7 @@ jbig2_png_write_data(png_structp png_ptr
  {
      png_size_t check;
  
@@ -9,7 +17,7 @@
      if (check != length) {
        png_error(png_ptr, "Write Error");
      }
-@@ -43,7 +43,7 @@
+@@ -50,7 +49,7 @@ static void
  jbig2_png_flush(png_structp png_ptr)
  {
      png_FILE_p io_ptr;
