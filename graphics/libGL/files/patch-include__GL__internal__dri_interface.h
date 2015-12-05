df00749705ca0f530d568bb33c5a524fa5e95c3e
kwm <kwm@FreeBSD.org>
2015-12-05 10:46:58 +0000

    Add patch to use drm.h instead of redefining the types.
    This unbreaks the build of xorg-server on 9.3.

---
+++ graphics/libGL/files/patch-include__GL__internal__dri_interface.h
@@ -0,0 +1,19 @@
+GCC on 9.x doesn't allow types to be overwritten, these types are defined
+in drm.h also, which causes build issues in xorg-server.
+
+--- include/GL/internal/dri_interface.h.orig	2015-12-05 10:55:50.545108000 +0100
++++ include/GL/internal/dri_interface.h	2015-12-05 10:56:03.408589000 +0100
+@@ -40,13 +40,7 @@
+ #ifndef DRI_INTERFACE_H
+ #define DRI_INTERFACE_H
+ 
+-#ifdef HAVE_LIBDRM
+ #include <drm.h>
+-#else
+-typedef unsigned int drm_context_t;
+-typedef unsigned int drm_drawable_t;
+-typedef struct drm_clip_rect drm_clip_rect_t;
+-#endif
+ 
+ /**
+  * \name DRI interface structures
