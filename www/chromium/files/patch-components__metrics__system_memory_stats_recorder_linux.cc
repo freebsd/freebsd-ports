99efb6e2bf2299ffdfe9dd2a0a89fc9019465fde
rene <rene@FreeBSD.org>
2015-12-07 07:05:58 +0000

    www/chromium: update to 47.0.2526.73
    Currently broken on 9.
    Submitted by:	LeFroid (tmvfroid gmail)
    Obtained from:	https://github.com/gliaskos/freebsd-chromium/pull/41
    MFH:		2015Q4
    Security:	http://vuxml.freebsd.org/freebsd/548f74bd-993c-11e5-956b-00262d5ed8ee.html

--- www/chromium/files/patch-components__metrics__system_memory_stats_recorder_linux.cc
+++ www/chromium/files/patch-components__metrics__system_memory_stats_recorder_linux.cc
@@ -1,5 +1,5 @@
---- chrome/browser/memory/system_memory_stats_recorder_linux.cc.orig	2015-09-03 20:30:21.188785000 -0400
-+++ chrome/browser/memory/system_memory_stats_recorder_linux.cc	2015-09-03 20:25:00.522580000 -0400
+--- components/metrics/system_memory_stats_recorder_linux.cc.orig	2015-09-03 20:30:21.188785000 -0400
++++ components/metrics/system_memory_stats_recorder_linux.cc	2015-09-03 20:25:00.522580000 -0400
 @@ -29,6 +29,7 @@
    UMA_HISTOGRAM_LINEAR(name, sample, 2500, 50)
  
--- www/chromium/files/patch-ui__events__keycodes__dom__keycode_converter.cc
+++ www/chromium/files/patch-ui__events__keycodes__dom__keycode_converter.cc
@@ -1,11 +1,11 @@
---- ui/events/keycodes/dom/keycode_converter.cc.orig	2015-07-22 18:31:47.703757000 -0400
-+++ ui/events/keycodes/dom/keycode_converter.cc	2015-07-22 18:32:17.605463000 -0400
-@@ -16,7 +16,7 @@
+--- ui/events/keycodes/dom/keycode_converter.cc.orig	2015-10-14 03:01:24.000000000 -0400
++++ ui/events/keycodes/dom/keycode_converter.cc	2015-10-23 14:17:55.716373000 -0400
+@@ -17,7 +17,7 @@
  // and DOM Level 3 |code| strings.
  #if defined(OS_WIN)
- #define USB_KEYMAP(usb, xkb, win, mac, code, id) {usb, win, code}
+ #define USB_KEYMAP(usb, evdev, xkb, win, mac, code, id) {usb, win, code}
 -#elif defined(OS_LINUX)
 +#elif defined(OS_LINUX) || defined(OS_BSD)
- #define USB_KEYMAP(usb, xkb, win, mac, code, id) {usb, xkb, code}
+ #define USB_KEYMAP(usb, evdev, xkb, win, mac, code, id) {usb, xkb, code}
  #elif defined(OS_MACOSX)
- #define USB_KEYMAP(usb, xkb, win, mac, code, id) {usb, mac, code}
+ #define USB_KEYMAP(usb, evdev, xkb, win, mac, code, id) {usb, mac, code}
