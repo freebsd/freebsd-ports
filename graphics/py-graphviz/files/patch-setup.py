a90cd1dbe6a74bf0385c31403527838c907488dd
Vanilla I. Shu <vanilla@FreeBSD.org>
2015-12-08 15:55:03 +0000

    Upgrade to 1.3.1.

--- graphics/py-graphviz/files/patch-setup.py
+++ graphics/py-graphviz/files/patch-setup.py
@@ -1,22 +1,13 @@
---- setup.py.orig	2010-08-01 21:18:02.000000000 +0800
-+++ setup.py	2010-08-02 15:59:57.000000000 +0800
-@@ -28,8 +28,8 @@
-           sys.version_info[:2]
-     sys.exit(-1)
- 
--library_path=None
--include_path=None
-+#library_path=None
-+#include_path=None
- 
- # If the setup script couldn't find your graphviz installation you can
- # specify it here by uncommenting these lines or providing your own:
-@@ -42,6 +42,8 @@
- # OSX, Linux, alternate location
- #library_path='/usr/local/lib/graphviz'
- #include_path='/usr/local/include/graphviz'
-+library_path='%%LOCALBASE%%/lib/graphviz'
-+include_path='%%LOCALBASE%%/include/graphviz'
- 
- # OSX (Fink)
- #library_path='/sw/lib/graphviz'
+--- setup.py.orig	2015-09-06 13:49:49 UTC
++++ setup.py
+@@ -54,8 +54,8 @@ if __name__ == "__main__":
+         Extension(
+             "pygraphviz._graphviz",
+             ["pygraphviz/graphviz_wrap.c"],
+-            include_dirs=[],
+-            library_dirs=[],
++            include_dirs=["%%LOCALBASE%%/include"],
++            library_dirs=["%%LOCALBASE%%/lib/graphviz"],
+             libraries=["cgraph", "cdt"],
+             define_macros=define_macros
+         )
