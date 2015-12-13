397148bc4ad345b8913f6d43630854fa3b1ddda0
Dmitry Marakasov <amdmi3@FreeBSD.org>
2015-12-13 03:59:14 +0000

    - Add LICENSE_FILE - Pet portlint - Regenerate patches with `make makepatch`

--- security/nss_compat_ossl/files/patch-src_ssl.c
+++ security/nss_compat_ossl/files/patch-src_ssl.c
@@ -1,12 +1,15 @@
 Index: src/ssl.c
 ===================================================================
---- src/ssl.c	(revision 76)
-+++ src/ssl.c	(working copy)
-@@ -1754,12 +1754,13 @@
+--- src/ssl.c.orig	2010-01-05 20:54:55 UTC
++++ src/ssl.c
+@@ -1754,11 +1754,12 @@ SSL_METHOD *create_context(PRBool ssl2, 
      if (SSL_OptionSet(s, SSL_HANDSHAKE_AS_SERVER, server) != SECSuccess)
          goto error;
  
 -    if (SSL_OptionSet(s, SSL_ENABLE_SSL2, ssl2) != SECSuccess)
+-        goto error;
+-
+-    if (SSL_OptionSet(s, SSL_V2_COMPATIBLE_HELLO, ssl2) != SECSuccess)
 -        goto error;
 +    /* Don't fail if enabling SSL2 options doesn't succeed as it may
 +     * be disabled in NSS. So just ignore the return value from
@@ -15,9 +18,5 @@ Index: src/ssl.c
 +    SSL_OptionSet(s, SSL_ENABLE_SSL2, ssl2);
 +    SSL_OptionSet(s, SSL_V2_COMPATIBLE_HELLO, ssl2);
  
--    if (SSL_OptionSet(s, SSL_V2_COMPATIBLE_HELLO, ssl2) != SECSuccess)
--        goto error;
--
      if (SSL_OptionSet(s, SSL_ENABLE_SSL3, ssl3)  != SECSuccess)
          goto error;
- 
