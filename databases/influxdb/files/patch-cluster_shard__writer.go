97fa245239d63f8434bc903af0d097495358c3b8
Thomas Zander <riggs@FreeBSD.org>
2015-12-13 18:01:07 +0000

    Update to upstream version 0.9.6
    PR:		205140
    Submitted by:	cheffo@freebsd-bg.org (maintainer)

--- databases/influxdb/files/patch-cluster_shard__writer.go
+++ databases/influxdb/files/patch-cluster_shard__writer.go
@@ -1,9 +1,9 @@
---- cluster/shard_writer.go.orig	2015-06-23 14:38:36 UTC
+--- cluster/shard_writer.go.orig	2015-12-08 15:03:30 UTC
 +++ cluster/shard_writer.go
 @@ -7,7 +7,7 @@ import (
  
  	"github.com/influxdb/influxdb/meta"
- 	"github.com/influxdb/influxdb/tsdb"
+ 	"github.com/influxdb/influxdb/models"
 -	"gopkg.in/fatih/pool.v2"
 +	"github.com/fatih/pool"
  )
