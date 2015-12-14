c1c123d63ad9910b589d0fb2938ef3f129ce2ca1
Guido Falsi <madpilot@FreeBSD.org>
2015-12-14 13:52:11 +0000

    - Update to 2.46.0 - Use new MASTER_SITE
    This new version depends on the new py-qt5 ports. Thanks to rakuco
    and Tobias Berner for helping me make it land in the ports tree!
    
    Also thanks to Kovid Goyal (calibre developer) for his suggestions
    on making new calibre releases work on FreeBSD.

--- deskutils/calibre/files/patch-src_calibre_library_server_content.py
+++ deskutils/calibre/files/patch-src_calibre_library_server_content.py
@@ -1,6 +1,6 @@
---- src/calibre/library/server/content.py.orig	2014-07-18 13:13:33.000000000 -0500
-+++ src/calibre/library/server/content.py	2014-07-18 13:14:11.000000000 -0500
-@@ -111,6 +111,7 @@
+--- src/calibre/library/server/content.py.orig	2015-02-20 03:34:13 UTC
++++ src/calibre/library/server/content.py
+@@ -111,6 +111,7 @@ class ContentServer(object):
                       'png'  : 'image/png',
                       'gif'  : 'image/gif',
                       'html' : 'text/html',
