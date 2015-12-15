7b82cf6b46e845cc1e059a277f6c415665fb745c
tj <tj@FreeBSD.org>
2015-12-15 17:06:11 +0000

    Update to 2.1.
    Changes: https://www.elastic.co/guide/en/elasticsearch/reference/2.1/release-notes-2.1.0.html
    
    - Fix path to allow service to start at boot
    - Misc cleanup from ohauer
    
    PR:		195861, 204821, 204902, 204910

--- textproc/elasticsearch2/files/patch-bin-elasticsearch.in.sh
+++ textproc/elasticsearch2/files/patch-bin-elasticsearch.in.sh
@@ -1,6 +1,6 @@
---- bin/elasticsearch.in.sh.orig	2015-11-19 16:02:28.386051810 -0500
-+++ bin/elasticsearch.in.sh	2015-11-19 16:02:56.618049877 -0500
-@@ -1,5 +1,19 @@
+--- bin/elasticsearch.in.sh.orig	2015-10-21 04:41:12.000000000 -0400
++++ bin/elasticsearch.in.sh	2015-12-15 12:00:37.323441838 -0500
+@@ -1,5 +1,20 @@
  #!/bin/sh
  
 +if [ `uname -o` == "FreeBSD" ]; then
@@ -13,8 +13,9 @@
 +	ES_USE_IPV4=${elasticsearch_use_ipv4}
 +	ES_GC_OPTS=${elasticsearch_gc_opts}
 +	ES_GC_LOG_FILE=${elasticsearch_gc_logfile}
-+	JAVA_OPTS="$JAVA_OPTS -Des.path.conf=${elasticsearch_config:="%%PREFIX%%/etc/elasticsearch"}"
-+	JAVA_OPTS="$JAVA_OPTS -Des.path.scripts=${elasticsearch_scripts:="%%PREFIX%%/libexec/elasticsearch"}"
++	JAVA_OPTS="$JAVA_OPTS -Des.path.conf=${elasticsearch_config:="/usr/local/etc/elasticsearch"}"
++	JAVA_OPTS="$JAVA_OPTS -Des.path.scripts=${elasticsearch_scripts:="/usr/local/libexec/elasticsearch"}"
++	PATH=${PATH}:/usr/local/bin
 +fi
 +
  # check in case a user was using this mechanism
