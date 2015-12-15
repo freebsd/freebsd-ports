a4822bd4c92eeccabf92f42007cdb86bfa8a40d3
mva <mva@FreeBSD.org>
2015-12-15 13:32:27 +0000

    - Update to version 2.76b
    Blender 2.76 comes with hundreds of bug fixes and many new features,
    such as
    
    * support for Pixar's OpenSubdiv technology
    * huge performance improvements for the viewport
    * auto-offset support for nodes
    * new point density texture support for cycles
    * new modeling tools
    
    Changelog: http://wiki.blender.org/index.php/Dev:Ref/Release_Notes/2.76

---
+++ graphics/blender/files/patch-intern_opensubdiv_opensubdiv__converter.cc
@@ -0,0 +1,10 @@
+--- intern/opensubdiv/opensubdiv_converter.cc.orig	2015-11-02 13:25:37 UTC
++++ intern/opensubdiv/opensubdiv_converter.cc
+@@ -24,6 +24,7 @@
+  */
+ 
+ #include <cstdio>
++#include <cstdlib>
+ #include <vector>
+ 
+ #ifdef _MSC_VER
