ec4491517636f1ce8af910c7e098fe0e3b635b30
jbeich <jbeich@FreeBSD.org>
2015-12-16 09:36:24 +0000

    devel/android-tools-*: drop FreeBSD 8.x support

--- devel/android-tools-adb-devel/files/patch-base_logging.cpp
+++ devel/android-tools-adb-devel/files/patch-base_logging.cpp
@@ -1,14 +1,12 @@
 --- base/logging.cpp.orig	2015-12-14 21:12:52 UTC
 +++ base/logging.cpp
-@@ -66,6 +68,16 @@
+@@ -66,6 +68,14 @@
  #include <unistd.h>
  #elif defined(_WIN32)
  #include <windows.h>
 +#elif defined(__DragonFly__)
 +#include <unistd.h>
 +#elif defined(__FreeBSD__)
-+#include <sys/param.h>
-+#include <sys/thr.h>
 +#include <pthread_np.h>
 +#elif defined(__NetBSD__)
 +#include <lwp.h>
@@ -17,7 +15,7 @@
  #endif
  
  static pid_t GetThreadId() {
-@@ -77,6 +89,20 @@ static pid_t GetThreadId() {
+@@ -77,6 +89,14 @@ static pid_t GetThreadId() {
    return syscall(__NR_gettid);
  #elif defined(_WIN32)
    return GetCurrentThreadId();
@@ -26,13 +24,7 @@
 +#elif defined(__NetBSD__)
 +  return _lwp_self();
 +#elif defined(__FreeBSD__)
-+#  if __FreeBSD_version > 900030
-+    return pthread_getthreadid_np();
-+#  else
-+    long lwpid;
-+    thr_self(&lwpid);
-+    return lwpid;
-+#  endif
++  return pthread_getthreadid_np();
 +#else
 +  return (intptr_t) pthread_self();
  #endif
