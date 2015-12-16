f2442363ed5ccdffa640c61ec211541516c605ed
Jan Beich <jbeich@FreeBSD.org>
2015-12-16 09:36:24 +0000

    devel/android-tools-*: drop FreeBSD 8.x support

--- devel/android-tools-adb/files/patch-libcutils_threads.c
+++ devel/android-tools-adb/files/patch-libcutils_threads.c
@@ -1,14 +1,12 @@
 --- libcutils/threads.c.orig	2015-06-17 22:20:38 UTC
 +++ libcutils/threads.c
-@@ -29,6 +29,16 @@
+@@ -29,6 +29,14 @@
  #include <unistd.h>
  #elif defined(_WIN32)
  #include <windows.h>
 +#elif defined(__DragonFly__)
 +#include <unistd.h>
 +#elif defined(__FreeBSD__)
-+#include <sys/param.h>
-+#include <sys/thr.h>
 +#include <pthread_np.h>
 +#elif defined(__NetBSD__)
 +#include <lwp.h>
@@ -17,7 +15,7 @@
  #endif
  
  // No definition needed for Android because we'll just pick up bionic's copy.
-@@ -40,6 +48,20 @@ pid_t gettid() {
+@@ -40,6 +48,14 @@ pid_t gettid() {
    return syscall(__NR_gettid);
  #elif defined(_WIN32)
    return GetCurrentThreadId();
@@ -26,13 +24,7 @@
 +#elif defined(__NetBSD__)
 +  return _lwp_self();
 +#elif defined(__FreeBSD__)
-+#  if __FreeBSD_version > 900030
-+    return pthread_getthreadid_np();
-+#  else
-+    long lwpid;
-+    thr_self(&lwpid);
-+    return lwpid;
-+#  endif
++  return pthread_getthreadid_np();
 +#else
 +  return (intptr_t) pthread_self();
  #endif
