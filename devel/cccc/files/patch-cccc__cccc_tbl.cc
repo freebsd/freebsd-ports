9abdb670f2660be4722f68239b6400ccf1898903
amdmi3 <amdmi3@FreeBSD.org>
2015-12-16 02:02:16 +0000

    - Switch to options helpers - Simplify installation - Regenerate patches with `make makepatch`

--- devel/cccc/files/patch-cccc__cccc_tbl.cc
+++ devel/cccc/files/patch-cccc__cccc_tbl.cc
@@ -1,6 +1,6 @@
---- ./cccc/cccc_tbl.cc.orig	2011-07-01 03:42:55.000000000 +0200
-+++ ./cccc/cccc_tbl.cc	2011-07-01 03:43:07.000000000 +0200
-@@ -96,7 +96,7 @@
+--- cccc/cccc_tbl.cc.orig	2006-01-05 16:05:30 UTC
++++ cccc/cccc_tbl.cc
+@@ -96,7 +96,7 @@ bool CCCC_Table<T>::remove(T* old_item_p
    typename map_t::iterator value_iterator=map_t::find(old_item_ptr->key());
    if(value_iterator!=map_t::end())
      {
