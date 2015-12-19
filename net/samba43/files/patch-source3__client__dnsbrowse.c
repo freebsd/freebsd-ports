2a30a7b3e97bb6e78f1aae70686bca1a16b95e56
Timur I. Bakeyev <timur@FreeBSD.org>
2015-12-19 22:51:10 +0000

    A security fix release of Samba 4.1, 4.2 and 4.3. Samba 4.1 is also marked as deprecated.
    Security:	CVE-2015-3223
    		CVE-2015-5252
    		CVE-2015-5299
    		CVE-2015-5296
    		CVE-2015-8467
    		CVE-2015-5330

--- net/samba43/files/patch-source3__client__dnsbrowse.c
+++ net/samba43/files/patch-source3__client__dnsbrowse.c
@@ -1,5 +1,5 @@
---- source3/client/dnsbrowse.c.orig	2013-11-12 10:20:03.145351798 +0100
-+++ source3/client/dnsbrowse.c	2013-11-12 10:20:11.762761273 +0100
+--- source3/client/dnsbrowse.c.orig	2015-07-21 09:47:49.000000000 +0000
++++ source3/client/dnsbrowse.c	2015-12-07 02:08:01.627022117 +0000
 @@ -91,7 +91,7 @@
  		}
  	}
@@ -9,3 +9,12 @@
  	DNSServiceRefDeallocate(mdns_conn_sdref);
  }
  
+@@ -168,7 +168,7 @@
+ 	for (;;)  {
+ 		int revents;
+ 
+-		ret = poll_one_fd(mdnsfd, POLLIN|POLLHUP, &revents, 1000);
++		ret = poll_one_fd(mdnsfd, POLLIN|POLLHUP, 1000, &revents);
+ 		if (ret <= 0 && errno != EINTR) {
+ 			break;
+ 		}
