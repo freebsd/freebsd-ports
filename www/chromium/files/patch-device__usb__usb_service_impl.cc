19ec6c856e12f47e6b8d8a96298e584ebe02177f
rene <rene@FreeBSD.org>
2015-12-20 20:15:23 +0000

    MFH: r404050
    www/chromium: update to 47.0.2526.106
    
    Submitted by:	Christoph Moench-Tegeder via GitHub
    Security:	http://vuxml.freebsd.org/freebsd/bb7d4791-a5bf-11e5-a0e5-00262d5ed8ee.html
    Approved by:	ports-secteam (feld)

--- www/chromium/files/patch-device__usb__usb_service_impl.cc
+++ www/chromium/files/patch-device__usb__usb_service_impl.cc
@@ -1,11 +1,11 @@
---- device/usb/usb_service_impl.cc.orig	2015-10-21 18:00:38.000000000 -0400
-+++ device/usb/usb_service_impl.cc	2015-10-23 12:39:46.908676000 -0400
+--- device/usb/usb_service_impl.cc.orig	2015-12-04 22:33:43.708484000 +0100
++++ device/usb/usb_service_impl.cc	2015-12-04 22:39:47.903086000 +0100
 @@ -20,7 +20,11 @@
  #include "device/usb/usb_device_handle.h"
  #include "device/usb/usb_error.h"
  #include "device/usb/webusb_descriptors.h"
 +#if defined(OS_FREEBSD)
-+#include "libusb.h"
++#include <libusb.h>
 +#else
  #include "third_party/libusb/src/libusb/libusb.h"
 +#endif
@@ -20,11 +20,16 @@
    rv = libusb_hotplug_register_callback(
        context_->context(),
        static_cast<libusb_hotplug_event>(LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED |
-@@ -559,12 +564,15 @@
-     device_observer_.Add(device_monitor);
+@@ -553,6 +558,8 @@
    }
- #endif  // OS_WIN
-+#endif  // OS_FREEBSD
+ 
+   RefreshDevices();
++#endif // !defined(OS_FREEBSD)
++
+ #if defined(OS_WIN)
+   DeviceMonitorWin* device_monitor = DeviceMonitorWin::GetForAllInterfaces();
+   if (device_monitor) {
+@@ -562,9 +569,11 @@
  }
  
  UsbServiceImpl::~UsbServiceImpl() {
@@ -32,11 +37,11 @@
    if (hotplug_enabled_) {
      libusb_hotplug_deregister_callback(context_->context(), hotplug_handle_);
    }
-+#endif  // OS_FREEBSD
++#endif // !defined(OS_FREEBSD)
    for (const auto& map_entry : devices_) {
      map_entry.second->OnDisconnect();
    }
-@@ -802,6 +810,7 @@
+@@ -802,6 +811,7 @@
    device->OnDisconnect();
  }
  
@@ -44,11 +49,11 @@
  // static
  int LIBUSB_CALL UsbServiceImpl::HotplugCallback(libusb_context* context,
                                                  PlatformUsbDevice device,
-@@ -839,6 +848,7 @@
+@@ -839,6 +849,7 @@
  
    return 0;
  }
-+#endif // !OS_FREEBSD
++#endif // !defined(OS_FREEBSD)
  
  void UsbServiceImpl::OnPlatformDeviceAdded(PlatformUsbDevice platform_device) {
    DCHECK(CalledOnValidThread());
--- www/chromium/files/patch-media__capture__video__file_video_capture_device_factory.cc
+++ www/chromium/files/patch-media__capture__video__file_video_capture_device_factory.cc
@@ -1,5 +1,5 @@
---- media/capture/video/file_video_capture_device_factory.cc.orig	2015-10-14 11:10:41.842256000 -0400
-+++ media/capture/video/file_video_capture_device_factory.cc	2015-10-14 11:11:13.400581000 -0400
+--- media/capture/video/file_video_capture_device_factory.cc.orig	2015-10-14 17:42:06.832425000 +0200
++++ media/capture/video/file_video_capture_device_factory.cc	2015-10-14 17:42:45.561757000 +0200
 @@ -49,7 +49,7 @@
    device_names->push_back(VideoCaptureDevice::Name(
        command_line_file_path.value(), kFileVideoCaptureDeviceName,
