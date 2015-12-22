cf4cd7b290ec2060401997b059b6913346cd10ee
miwi <miwi@FreeBSD.org>
2015-12-22 04:07:14 +0000

    - Update to 15.05
    PR:		201844
    Submitted by:	Ports Fury
    Approved by:	mat (mentor)
    Differential Revision:	D4620

--- graphics/fotoxx/files/patch-zfuncs.h
+++ graphics/fotoxx/files/patch-zfuncs.h
@@ -1,15 +1,19 @@
---- ./zfuncs.h.orig	2014-02-05 20:42:34.000000000 +0100
-+++ ./zfuncs.h	2014-02-06 10:32:37.000000000 +0100
-@@ -22,7 +22,7 @@
+--- zfuncs.h.orig	2015-05-01 09:40:38 UTC
++++ zfuncs.h
+@@ -22,17 +22,18 @@
  
- //     zfuncs.h   version  v.5.8
+ //     zfuncs.h   version  v.6.1
  
--#include <sys/sysinfo.h> 
-+//#include <sys/sysinfo.h> 
+-#include <sys/sysinfo.h>
++//#include <sys/sysinfo.h>
  #include <sys/time.h>
  #include <sys/stat.h>
  #include <sys/resource.h>
-@@ -33,6 +33,7 @@
+ #include <sys/file.h>
+ #include <sys/utsname.h>
+-#include <malloc.h>
++//#include <malloc.h>
+ #include <errno.h>
  #include <unistd.h>
  #include <stdlib.h>
  #include <stdio.h>
@@ -17,21 +21,3 @@
  #include <string.h>
  #include <ctype.h>
  #include <math.h>
-@@ -60,7 +61,7 @@
- 
- #define  wstrerror(err) strerror(WEXITSTATUS(err))                         //  get text status for child process
- 
--#define  mutex          pthread_mutex_t                                    //  abbreviations
-+#define  mutex_tp       pthread_mutex_t                                    //  abbreviations
- #define  mutex_init     pthread_mutex_init
- #define  mutex_lock     pthread_mutex_lock
- #define  mutex_trylock  pthread_mutex_trylock
-@@ -625,7 +626,7 @@
- {
-    char        wmi[8];
-    Vxstring  * vd;                                                         //  vector of xstrings
--   mutex       qmutex;                                                     //  for multi-thread access
-+   mutex_tp    qmutex;                                                     //  for multi-thread access
-    int         qcap;                                                       //  queue capacity
-    int         qcount;                                                     //  curr. queue count
-    int         ent1;                                                       //  first entry pointer
