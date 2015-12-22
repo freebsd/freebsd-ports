a49e3959b9ec2acce699e6a694b9802e95c1635b
brooks <brooks@FreeBSD.org>
2015-12-22 21:35:30 +0000

    change .a libraries to .so in llvm-config37
    PR:		205103
    Submitted by:	kwm
    Obtained from:	https://bugs.gentoo.org/show_bug.cgi?id=565358

--- devel/llvm37/files/patch-utils_llvm-build_llvmbuild_main.py
+++ devel/llvm37/files/patch-utils_llvm-build_llvmbuild_main.py
@@ -1,6 +1,24 @@
---- utils/llvm-build/llvmbuild/main.py.orig	2015-07-20 23:51:12 UTC
-+++ utils/llvm-build/llvmbuild/main.py
-@@ -718,7 +718,17 @@ def add_magic_target_components(parser, 
+--- utils/llvm-build/llvmbuild/main.py.orig	2014-11-19 04:34:20.000000000 +0100
++++ utils/llvm-build/llvmbuild/main.py	2015-12-21 11:15:34.356019000 +0100
+@@ -393,6 +393,8 @@ subdirectories = %s
+ //
+ //===----------------------------------------------------------------------===//
+ 
++#include "llvm/Config/config.h"
++
+ """)
+         f.write('struct AvailableComponent {\n')
+         f.write('  /// The name of the component.\n')
+@@ -413,7 +415,7 @@ subdirectories = %s
+             if library_name is None:
+                 library_name_as_cstr = '0'
+             else:
+-                library_name_as_cstr = '"lib%s.a"' % library_name
++                library_name_as_cstr = '"lib%s" LTDL_SHLIB_EXT' % library_name
+             f.write('  { "%s", %s, %d, { %s } },\n' % (
+                 name, library_name_as_cstr, is_installed,
+                 ', '.join('"%s"' % dep
+@@ -718,7 +720,17 @@ def add_magic_target_components(parser, 
  
      # We handle a few special cases of target names here for historical
      # reasons, as these are the names configure currently comes up with.
