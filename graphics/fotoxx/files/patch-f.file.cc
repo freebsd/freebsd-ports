5cb63c3734c3fab137d0e864f605ad0b1045d2c5
miwi <miwi@FreeBSD.org>
2015-12-22 04:07:14 +0000

    - Update to 15.05
    PR:		201844
    Submitted by:	Ports Fury
    Approved by:	mat (mentor)
    Differential Revision:	D4620

--- graphics/fotoxx/files/patch-f.file.cc
+++ graphics/fotoxx/files/patch-f.file.cc
@@ -1,10 +1,10 @@
---- ./f.file.cc.orig	2014-02-05 20:42:34.000000000 +0100
-+++ ./f.file.cc	2014-02-06 10:32:37.000000000 +0100
-@@ -23,6 +23,7 @@
+--- f.file.cc.orig	2015-05-01 09:40:38 UTC
++++ f.file.cc
+@@ -74,6 +74,7 @@
  
  #define EX extern                                                          //  disable extern declarations
- #include "fotoxx.h"
+ #include "fotoxx.h"                                                        //  (variables in fotoxx.h are refs)
 +#include <sys/wait.h>
  
- /**************************************************************************
-       Fotoxx image edit - file menu functions
+ /**************************************************************************/
+ 
--- graphics/fotoxx/files/patch-zfuncs.cc
+++ graphics/fotoxx/files/patch-zfuncs.cc
@@ -1,20 +1,18 @@
---- ./zfuncs.cc.orig	2014-02-05 20:42:34.000000000 +0100
-+++ ./zfuncs.cc	2014-02-06 10:32:37.000000000 +0100
-@@ -854,7 +854,7 @@
- //  A mutex lock is used to insure one thread at a time has access to the parameter.
- //  Many parameters can be used but there is only one mutex lock.
+--- zfuncs.cc.orig	2015-05-01 09:40:38 UTC
++++ zfuncs.cc
+@@ -262,6 +262,7 @@ char *zstrdup(cchar *string, int addcc)
  
--mutex zget_lock = PTHREAD_MUTEX_INITIALIZER;
-+mutex_tp zget_lock = PTHREAD_MUTEX_INITIALIZER;
- 
- int zget_locked(int &param)                                                //  lock and return parameter
+ void zmalloc_report()
  {
-@@ -1276,7 +1276,7 @@
- namespace shell_asynch_names {
-    char     *command[10];
-    int      status[10];
--   mutex    mlock = PTHREAD_MUTEX_INITIALIZER;
-+   mutex_tp mlock = PTHREAD_MUTEX_INITIALIZER;
- }
++#ifdef __linux__
+    using namespace zfuncs;
  
- int shell_asynch(cchar *Fcommand, ...)                                     //  v.5.5
+    static double     ptime = 0;
+@@ -282,6 +283,7 @@ void zmalloc_report()
+ 
+    ptime = jobtime();                                                      //  reset some counters
+    Nmalloc = Nstrdup = Nfree = 0;
++#endif
+ 
+    return;
+ }
