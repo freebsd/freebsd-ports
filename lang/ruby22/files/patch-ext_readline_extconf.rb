513b718ed32e38d5429163bbd3f0d6a6cc402d97
sunpoet <sunpoet@FreeBSD.org>
2015-12-23 19:09:55 +0000

    - Update to 2.2.4 - Add LICENSE_FILE - Use USES=autoreconf instead of USE_AUTOTOOLS=autoconf - Use USES=libedit - Silence patch and build messages
    Changes:	https://www.ruby-lang.org/en/news/2015/12/16/ruby-2-2-4-released/
    		http://svn.ruby-lang.org/repos/ruby/tags/v2_2_4/ChangeLog
    Security:	3b50881d-1860-4721-aab1-503290e23f6c
    Differential Revision:	https://reviews.freebsd.org/D4668
    PR:		205487
    Exp-run by:	antoine
    MFH:		2015Q4

--- lang/ruby22/files/patch-ext_readline_extconf.rb
+++ lang/ruby22/files/patch-ext_readline_extconf.rb
@@ -1,13 +1,5 @@
 --- ext/readline/extconf.rb.orig	2014-05-01 11:59:37.000000000 +0000
 +++ ext/readline/extconf.rb	2015-10-23 04:05:44.000000000 +0000
-@@ -37,6 +37,7 @@
- case enable_libedit
- when true
-   # --enable-libedit
-+  dir_config("libedit")
-   unless (readline.have_header("editline/readline.h") ||
-           readline.have_header("readline/readline.h")) &&
-           have_library("edit", "readline")
 @@ -62,7 +63,7 @@ else
  end
  
