89317330060843847c20d79c1334d6ba4ec82c08
amdmi3 <amdmi3@FreeBSD.org>
2015-12-25 14:41:40 +0000

    - Add LICENSE - Switch to options helpers - Add NO_ARCH - Regenerate patches with `make makepatch`
    Approved by:	portmgr blanket

--- databases/py-forgetsql/files/patch-lib-forgetSQL.py
+++ databases/py-forgetsql/files/patch-lib-forgetSQL.py
@@ -1,5 +1,5 @@
---- lib/forgetSQL.py.orig	Sat Nov 20 16:59:07 2004
-+++ lib/forgetSQL.py	Sat Nov 20 16:59:39 2004
+--- lib/forgetSQL.py.orig	2004-03-08 12:16:25 UTC
++++ lib/forgetSQL.py
 @@ -1,4 +1,5 @@
  #!/usr/bin/env python
 +# -*- coding: ISO-8859-1 -*-
