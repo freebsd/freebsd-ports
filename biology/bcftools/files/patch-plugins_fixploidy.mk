9c6f651a3c6d44595e04c6aeac2ff968fd707dbc
junovitch <junovitch@FreeBSD.org>
2015-12-25 01:31:09 +0000

    biology/{bcftools,htslib,samtools}: update 1.2.x -> 1.3
    - Update ports to 1.3 and set BUILD_DEPENDS of dependent ports to require
      version 1.3 of htslib.
    - Add CURL option to htslib
    - Add TEST_TARGET with perl and bash dependencies for testing
    - Tidy up spacing and pkg-message's
    
    PR:		205524
    PR:		205525
    PR:		205526
    Submitted by:	cartwright@asu.edu (maintainer)

--- biology/bcftools/files/patch-plugins_fixploidy.mk
+++
@@ -1,7 +0,0 @@
---- plugins/fixploidy.mk.orig	2015-02-02 15:40:17 UTC
-+++ plugins/fixploidy.mk
-@@ -1,2 +1,2 @@
--plugins/fixploidy.so: plugins/fixploidy.c version.h version.c ploidy.h ploidy.c $(HTSDIR)/libhts.so
--	$(CC) $(CFLAGS) $(INCLUDES) -fPIC -shared -o $@ ploidy.c version.c $< -L$(HTSDIR) -lhts
-+plugins/fixploidy.so: plugins/fixploidy.c version.h version.c ploidy.h ploidy.c
-+	$(CC) $(CFLAGS) $(LDFLAGS) $(INCLUDES) -fPIC -shared -o $@ ploidy.c version.c $< -lhts
--- biology/samtools/files/patch-errmod.c
+++ biology/samtools/files/patch-errmod.c
@@ -1,16 +1,14 @@
---- errmod.c.orig	2014-12-15 20:22:22 UTC
+--- errmod.c.orig	2015-12-15 21:26:20 UTC
 +++ errmod.c
-@@ -24,6 +24,15 @@ FROM, OUT OF OR IN CONNECTION WITH THE S
+@@ -24,6 +24,13 @@ FROM, OUT OF OR IN CONNECTION WITH THE S
  DEALINGS IN THE SOFTWARE.  */
  
  #include <math.h>
 +
 +#include <sys/param.h>
-+#ifdef __FreeBSD__
-+#	if __FreeBSD_version < 1000034
-+#		define logl(x) log(x)
-+#		define expl(x) exp(x)
-+#	endif
++#if defined(__FreeBSD__) && (__FreeBSD_version < 1000034)
++#	define logl(x) log(x)
++#	define expl(x) exp(x)
 +#endif
 +
  #include "errmod.h"
