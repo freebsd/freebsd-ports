f644c9e93c4258bde67b3d6f7bda828a60d318ee
Dmitry Marakasov <amdmi3@FreeBSD.org>
2015-12-27 20:15:42 +0000

    - Move highscores and saves to /var/games - Modernize plist - Regenerate patches

--- games/xsokoban/files/patch-config.h
+++ games/xsokoban/files/patch-config.h
@@ -1,53 +1,55 @@
-*** config.h.orig	Tue Aug 27 21:41:47 1996
---- config.h	Mon Nov 18 18:25:14 1996
-***************
-*** 30,36 ****
-  	    variable in the installed version, but you know best...
-  */
-  #ifndef ROOTDIR
-! #define ROOTDIR "."
-  #endif
-  
-  /*
---- 30,36 ----
-  	    variable in the installed version, but you know best...
-  */
-  #ifndef ROOTDIR
-! #define ROOTDIR "%%PREFIX%%/lib/X11/xsokoban"
-  #endif
-  
-  /*
-***************
-*** 95,101 ****
-     OWNER: defines the name of the local game owner.
-  */
-  #ifndef OWNER
-! #define OWNER "andru"
-  #endif
-  
-  /*
---- 95,101 ----
-     OWNER: defines the name of the local game owner.
-  */
-  #ifndef OWNER
-! #define OWNER "root"
-  #endif
-  
-  /*
-***************
-*** 147,153 ****
-  */
-  
-  #ifndef WWW
-! #define WWW 1
-  #endif
-  
-  #define DEBUG_SERVER(x)
---- 147,153 ----
-  */
-  
-  #ifndef WWW
-! #define WWW 0
-  #endif
-  
-  #define DEBUG_SERVER(x)
+--- config.h.orig	1997-07-17 16:27:58 UTC
++++ config.h
+@@ -30,7 +30,7 @@
+ 	    variable in the installed version, but you know best...
+ */
+ #ifndef ROOTDIR
+-#define ROOTDIR "."
++#define ROOTDIR "%%PREFIX%%/lib/X11/xsokoban"
+ #endif
+ 
+ /*
+@@ -46,7 +46,7 @@
+                         the name <username>.sav
+ */
+ #ifndef SAVEPATH
+-#define SAVEPATH ROOTDIR "/saves"
++#define SAVEPATH "/var/games/xsokoban/saves"
+ #endif
+ 
+ /*
+@@ -62,14 +62,14 @@
+              in the same directory as the score file.
+ */
+ #ifndef LOCKFILE
+-#define LOCKFILE ROOTDIR "/scores/lock"
++#define LOCKFILE "/var/games/xsokoban/scores/lock"
+ #endif
+ 
+ /*
+    SCOREFILE: the full pathname of the score file
+ */
+ #ifndef SCOREFILE
+-#define SCOREFILE ROOTDIR "/scores/scores"
++#define SCOREFILE "/var/games/xsokoban/scores/scores"
+ #endif
+ 
+ /*
+@@ -95,7 +95,7 @@
+    OWNER: defines the name of the local game owner.
+ */
+ #ifndef OWNER
+-#define OWNER "andru"
++#define OWNER "root"
+ #endif
+ 
+ /*
+@@ -147,7 +147,7 @@
+ */
+ 
+ #ifndef WWW
+-#define WWW 1
++#define WWW 0
+ #endif
+ 
+ #define DEBUG_SERVER(x)
