e8a2d0eb233b3053026697bdc1b680166c78c800
marino <marino@FreeBSD.org>
2015-12-29 15:05:44 +0000

    biology/htslib: Replace FreeBSD-specific fix with a generic one
    This port will not build if ENOTRECOVERABLE is not defined, period.
    It's not necessary to check OPSYS and version, just apply the fallback
    definition if it's not defined.  This unbreaks DragonFly after last
    commit.

--- biology/htslib/files/patch-hfile__libcurl.c
+++ biology/htslib/files/patch-hfile__libcurl.c
@@ -1,12 +1,11 @@
---- hfile_libcurl.c.orig	2015-12-22 22:35:42 UTC
+--- hfile_libcurl.c.orig	2015-12-15 16:34:33 UTC
 +++ hfile_libcurl.c
-@@ -39,6 +39,11 @@ DEALINGS IN THE SOFTWARE.  */
+@@ -39,6 +39,10 @@ DEALINGS IN THE SOFTWARE.  */
  
  #include <curl/curl.h>
  
-+#include <sys/param.h>
-+#if defined(__FreeBSD__) && (__FreeBSD_version < 1000033) && !defined(ENOTRECOVERABLE)
-+#	define ENOTRECOVERABLE EIO
++#ifndef ENOTRECOVERABLE
++#define ENOTRECOVERABLE EIO
 +#endif
 +
  typedef struct {
