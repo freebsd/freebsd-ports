f5b3294960bb09982cc7ae16be2903b3bb0fecc2
rene <rene@FreeBSD.org>
2015-12-30 23:20:40 +0000

    Remove expired ports: 2015-12-30 www/ocsigen: Broken for more than 6 months 2015-12-30 devel/monodevelop-database: Broken for more than 6 months 2015-12-30 lang/cduce: Broken for more than 6 months 2015-12-30 science/hdf-java: Broken for more than 6 months 2015-12-30 math/p5-Math-Geometry-Planar-GPC-Polygon: Broken for more than 6 months 2015-12-30 www/eliom: Depends on broken and expiring www/ocsigen 2015-12-30 audio/py-fastaudio: Broken for more than 6 months 2015-12-30 devel/jgoodies-common: Broken for more than 6 months 2015-12-30 graphics/pinta: Broken for more than 6 months 2015-12-30 games/kanatest: Broken for more than 6 months 2015-12-30 editors/bless: Broken for more than 6 months 2015-12-30 security/burpsuite: Broken for more than 6 months

--- audio/py-fastaudio/files/patch-mysndfile.h
+++
@@ -1,9 +0,0 @@
---- mysndfile.h.orig	2009-06-06 18:51:14.000000000 +0900
-+++ mysndfile.h	2009-06-06 18:51:38.000000000 +0900
-@@ -1,5 +1,5 @@
- #ifdef WIN32
- #include "sndfile-win32.h"
- #else
--#include "sndfile.h"
-+#include "%%LOCALBASE%%/include/sndfile.h"
- #endif
