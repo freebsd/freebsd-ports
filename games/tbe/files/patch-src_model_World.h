df4886f0f49b69de7b2d1ee1cecfbed088759b1b
johans <johans@FreeBSD.org>
2015-12-31 22:05:14 +0000

    - Fix build, needs cmake, qmake and USES=compiler:c++11-lib - Switch to USES=execinfo - USE_QT4=linguisttools_build for less build dependencies - Remove INSTALLS_ICONS, it's GTk+ apps only - Remove DESKTOP_ENTRIES, port installs it's own now - Fix project CMakeLists.txt to respect PREFIX and install   files to correct paths
    PR:		205742
    Submitted by:	pawel

---
+++ games/tbe/files/patch-src_model_World.h
@@ -0,0 +1,10 @@
+--- src/model/World.h.orig	2015-12-30 21:31:37 UTC
++++ src/model/World.h
+@@ -31,6 +31,7 @@
+ #include <QtCore/QSet>
+ 
+ #include <unordered_map>
++#include <vector>
+ 
+ // Forward Definitions:
+ class Goal;
