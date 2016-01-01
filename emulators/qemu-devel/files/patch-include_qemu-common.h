ee0adc18e9750d68b4666752e8746a9a98f5d8fe
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2016-01-01 17:54:08 +0000

    emulators/qemu-devel: Update version 2.4.0=>2.5.0
    - Remove nox@ from MASTER_SITES (R.I.P. nox)
    - Take MAINTAINERSHIP
    - Add LICENSE (GPLv2)
    - Convert to OPTIONSNG
    - Fix patch files to be 'make makepatch' compatible
    - Fix Multiple Security Vulnerabilities [1]
    
    PR:		203112 [1]
    Submitted by:	venture37@geeklan.co.uk [1]
    Security:	CVE-2015-5165 [1]
    		CVE-2015-5745 [1]
    		CVE-2015-5154 [1]
    		CVE-2015-5225 [1]
    Differential Revision:	https://reviews.freebsd.org/D3691

--- emulators/qemu-devel/files/patch-include_qemu-common.h
+++ emulators/qemu-devel/files/patch-include_qemu-common.h
@@ -1,7 +1,7 @@
---- a/include/qemu-common.h
-+++ b/include/qemu-common.h
-@@ -445,7 +445,9 @@ void qemu_hexdump(const char *buf, FILE 
- #define ALL_EQ(v1, v2) vec_all_eq(v1, v2)
+--- include/qemu-common.h.orig	2015-11-03 20:01:34 UTC
++++ include/qemu-common.h
+@@ -469,7 +469,9 @@ void qemu_hexdump(const char *buf, FILE 
+ #define VEC_OR(v1, v2) ((v1) | (v2))
  /* altivec.h may redefine the bool macro as vector type.
   * Reset it to POSIX semantics. */
 +#ifndef __cplusplus
