ce3cdf6107630c2422fc0cdffd58f19df00d2a26
Jimmy Olgeni <olgeni@FreeBSD.org>
2016-01-02 19:33:44 +0000

    Upgrade devel/elixir-csv to version 1.2.2.

--- devel/elixir-csv/files/patch-mix.exs
+++ devel/elixir-csv/files/patch-mix.exs
@@ -1,12 +1,10 @@
---- mix.exs.orig	2015-10-17 21:00:59 UTC
+--- mix.exs.orig	2016-01-02 13:43:14 UTC
 +++ mix.exs
-@@ -5,8 +5,7 @@ defmodule CSV.Mixfile do
-     [
+@@ -6,7 +6,6 @@ defmodule CSV.Mixfile do
          app: :csv,
-         version: "1.2.1",
--        elixir: "~> 1.1.0",
+         version: "1.2.2",
+         elixir: "~> 1.1",
 -        deps: deps,
-+        elixir: "~> 1.0",
          package: package,
          docs: &docs/0,
          name: "CSV",
