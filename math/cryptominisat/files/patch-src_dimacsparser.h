720528daeec15d6a50bbb904731e97556d74b366
koobs <koobs@FreeBSD.org>
2016-01-06 14:07:05 +0000

    math/{py-}cryptominisat: Update to 4.5.3
    Common:
    
    - Update PORTVERSION and distinfo checksum (4.5.3) [1]
    - Switch to GITHUB for DISTFILES, remove USES=tar accordingly [1]
    
    py-cryptominisat:
    
    - Convert into fully fledged port
    - Add distinfo and proper (unique) pkg-descr
    - Remove MASTER port overrides accordingly
    - Match COMMENT to upstream setup.py:description
    - De-scope USES=compiler to compiler only
    - Remove upstreamed patches
    - Patch to properly use distutils and autoplist instead
      of an uncessary cmake build (Also add post-patch for this)
    - Strip shared library
    
    cryptominisat:
    
    - Remove slave port overrides
    - Add BUILD_DEPENDS on vim-lite for xxd(8) tool [1]
    - Remove upstreamed patches [1]
    - Add patches for missing includes breaking builds on 9.3 [1]
    - Update pkg-plist [1]
    
    [1] https://github.com/msoos/cryptominisat/issues/304
    
    PR:		204850 [1]
    Submitted by:	maintainer [1]
    Approved by:	maintainer [*]

---
+++ math/cryptominisat/files/patch-src_dimacsparser.h
@@ -0,0 +1,10 @@
+--- src/dimacsparser.h.orig	2016-01-05 09:44:15 UTC
++++ src/dimacsparser.h
+@@ -25,6 +25,7 @@ THE SOFTWARE.
+ #define DIMACSPARSER_H
+ 
+ #include <string>
++#include <cstdlib>
+ #include "streambuffer.h"
+ #include "cryptominisat4/cryptominisat.h"
+ 
