116751515f4e60f852631a84102055384b126b77
olgeni <olgeni@FreeBSD.org>
2016-01-08 16:39:13 +0000

    Add devel/elixir-apex, a term pretty-printing library for Elixir.

---
+++ devel/elixir-apex/files/patch-mix.exs
@@ -0,0 +1,11 @@
+--- mix.exs.orig	2016-01-08 16:30:36 UTC
++++ mix.exs
+@@ -4,7 +4,7 @@ defmodule Apex.Mixfile do
+   def project do
+     [ app: :apex,
+       version: "0.3.4",
+-      elixir: "~> 1.1.0",
++      elixir: "~> 1.2",
+       build_per_environment: false,
+       description: description,
+       package: package,
