e88f2b4e70e7b781e8b2ae433deb4becfd5528e4
pi <pi@FreeBSD.org>
2016-01-10 20:02:30 +0000

    databases/mysql57-client, databases/mysql57-server: 	re-add for proper repo-copy
    PR:		204607

---
+++ databases/mysql57-client/files/patch-cmake_build__configurations_compiler__options.cmake
@@ -0,0 +1,20 @@
+--- cmake/build_configurations/compiler_options.cmake.orig	2015-11-29 19:16:24 UTC
++++ cmake/build_configurations/compiler_options.cmake
+@@ -28,7 +28,7 @@ IF(UNIX)  
+ 
+   # Default GCC flags
+   IF(CMAKE_COMPILER_IS_GNUCC)
+-    SET(COMMON_C_FLAGS               "-g -fabi-version=2 -fno-omit-frame-pointer -fno-strict-aliasing")
++    SET(COMMON_C_FLAGS               "-g -fno-omit-frame-pointer -fno-strict-aliasing")
+     # Disable inline optimizations for valgrind testing to avoid false positives
+     IF(WITH_VALGRIND)
+       SET(COMMON_C_FLAGS             "-fno-inline ${COMMON_C_FLAGS}")
+@@ -37,7 +37,7 @@ IF(UNIX)  
+     SET(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 ${COMMON_C_FLAGS}")
+   ENDIF()
+   IF(CMAKE_COMPILER_IS_GNUCXX)
+-    SET(COMMON_CXX_FLAGS               "-g -fabi-version=2 -fno-omit-frame-pointer -fno-strict-aliasing")
++    SET(COMMON_CXX_FLAGS               "-g -fno-omit-frame-pointer -fno-strict-aliasing")
+     # Disable inline optimizations for valgrind testing to avoid false positives
+     IF(WITH_VALGRIND)
+       SET(COMMON_CXX_FLAGS             "-fno-inline ${COMMON_CXX_FLAGS}")
---
+++ databases/mysql57-server/files/patch-mysys__ssl_my__default.cc
@@ -0,0 +1,31 @@
+--- mysys_ssl/my_default.cc.orig	2015-11-29 19:16:24 UTC
++++ mysys_ssl/my_default.cc
+@@ -122,7 +122,7 @@ static my_bool is_login_file= FALSE;
+ 
+ /* Which directories are searched for options (and in which order) */
+ 
+-#define MAX_DEFAULT_DIRS 6
++#define MAX_DEFAULT_DIRS 7
+ #define DEFAULT_DIRS_SIZE (MAX_DEFAULT_DIRS + 1)  /* Terminate with NULL */
+ static const char **default_directories = NULL;
+ 
+@@ -1409,7 +1409,10 @@ static const char **init_default_directo
+ 
+ #if defined(DEFAULT_SYSCONFDIR)
+   if (DEFAULT_SYSCONFDIR[0])
++  {
+     errors += add_directory(alloc, DEFAULT_SYSCONFDIR, dirs);
++    errors += add_directory(alloc, DEFAULT_SYSCONFDIR "/mysql", dirs);
++  }
+ #endif /* DEFAULT_SYSCONFDIR */
+ 
+ #endif
+@@ -1480,7 +1483,7 @@ int check_file_permissions(const char *f
+   MY_STAT stat_info;
+ 
+   if (!my_stat(file_name,&stat_info,MYF(0)))
+-    return 1;
++    return 0;
+   /*
+     Ignore .mylogin.cnf file if not exclusively readable/writable
+     by current user.
