13621f785c97321302b379303754dc8aadb179f0
mat <mat@FreeBSD.org>
2016-01-13 14:11:05 +0000

    Update to v5.23.6-106-g0224bf4.
    Changes:	https://github.com/Perl/perl5/compare/v5.23.6-46-g476d3d3...v5.23.6-106-g0224bf4
    Sponsored by:	Absolight

--- lang/perl5-devel/files/patch-cpan_Math-BigInt_lib_Math_BigInt.pm
+++ lang/perl5-devel/files/patch-cpan_Math-BigInt_lib_Math_BigInt.pm
@@ -1,11 +1,11 @@
---- cpan/Math-BigInt/lib/Math/BigInt.pm.orig	2015-11-02 11:11:17 UTC
+--- cpan/Math-BigInt/lib/Math/BigInt.pm.orig	2016-01-06 13:21:01 UTC
 +++ cpan/Math-BigInt/lib/Math/BigInt.pm
-@@ -64,7 +64,7 @@ use overload
- '>>='	=>	sub { $_[0]->brsft($_[1]); },
+@@ -65,7 +65,7 @@ use overload
+ '>>='   =>      sub { $_[0]->brsft($_[1]); },
  
  # not supported by Perl yet
--'..'	=>	\&_pointpoint,
-+# '..'	=>	\&_pointpoint,
+-'..'    =>      \&_pointpoint,
++# '..'    =>      \&_pointpoint,
  
- '<=>'	=>	sub { my $rc = $_[2] ?
-                       ref($_[0])->bcmp($_[1],$_[0]) : 
+ '<=>'   =>      sub { my $rc = $_[2] ?
+                       ref($_[0])->bcmp($_[1],$_[0]) :
