05a9fdefa717068d3c7230c688bbba73f850e893
mat <mat@FreeBSD.org>
2016-01-13 14:11:22 +0000

    Update to 2.1.2.
    Sponsored by:	Absolight

--- net-mgmt/monitoring-plugins/files/patch-plugins-root_check__dhcp.c
+++ net-mgmt/monitoring-plugins/files/patch-plugins-root_check__dhcp.c
@@ -1,4 +1,4 @@
---- plugins-root/check_dhcp.c.orig	2014-11-30 10:36:26 UTC
+--- plugins-root/check_dhcp.c.orig	2015-10-16 09:06:18 UTC
 +++ plugins-root/check_dhcp.c
 @@ -323,7 +323,8 @@ int get_hardware_address(int sock,char *
  #elif defined(__bsd__)
@@ -10,12 +10,3 @@
          char                    *buf;
          unsigned char           *ptr;
          struct if_msghdr        *ifm;
-@@ -1182,7 +1183,7 @@ int call_getopt(int argc, char **argv){
- 		        }
- 	        }
- 
--	return i;
-+	return i+1;
-         }
- 
- 
