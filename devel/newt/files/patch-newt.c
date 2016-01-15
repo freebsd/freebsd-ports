a7a0dcb33a4221eb81894b9fffdf2a764ed0e1d5
miwi <miwi@FreeBSD.org>
2016-01-15 09:28:53 +0000

    - Update to 0.52.18 - Add new python option
    PR:		206245
    Submitted by:	ports fury

--- devel/newt/files/patch-newt.c
+++ devel/newt/files/patch-newt.c
@@ -1,9 +1,11 @@
---- newt.c	2009-09-24 11:03:09.000000000 -0400
-+++ newt.c	2010-01-06 01:04:01.000000000 -0500
-@@ -99,4 +99,6 @@
+--- newt.c.orig	2014-10-23 08:34:37 UTC
++++ newt.c
+@@ -98,6 +98,8 @@ static const struct keymap keymap[] = {
+ 	{ "\033[6~",		NEWT_KEY_PGDN,		"kN" },
  	{ "\033V",		NEWT_KEY_PGUP, 		NULL },
  	{ "\033v",		NEWT_KEY_PGUP, 		NULL },
 +	{ "\033[G",             NEWT_KEY_PGDN,          NULL },
 +	{ "\033[I",             NEWT_KEY_PGUP,          NULL },
          { "\026",		NEWT_KEY_PGDN,		NULL },
  
+ 	{ "\033[[A",		NEWT_KEY_F1,		NULL },
