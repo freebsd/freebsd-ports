2c37f5fc3b28f4e082536d5eefc8ed18dd2696c7
Koop Mast <kwm@FreeBSD.org>
2016-01-15 12:45:28 +0000

    Update libmapi to 2.3.
    Switch from samba 4.0 and 4.1 choice to only 4.2. Samba 4.0 and 4.1 are
    EOL and marked for deletion.
    
    Keep the switch logic around for possible later use, since samba 4.3
    is not supported at this time.
    
    A few patches where obtained from archlinux.

---
+++ mail/libmapi/files/patch-utils_ocnotify.c
@@ -0,0 +1,10 @@
+--- utils/ocnotify.c.orig	2016-01-14 11:24:28.465644000 +0100
++++ utils/ocnotify.c	2016-01-14 11:24:39.248704000 +0100
+@@ -23,6 +23,7 @@
+ #include <stdint.h>
+ #include <stdbool.h>
+ #include <stdlib.h>
++#include <time.h>
+ 
+ #include <talloc.h>
+ #include <param.h>
