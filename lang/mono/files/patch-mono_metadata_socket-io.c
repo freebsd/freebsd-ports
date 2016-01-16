993639d440aa9425810e11d877f6588a3cefebb4
romain <romain@FreeBSD.org>
2016-01-16 12:13:44 +0000

    Update to 4.2.2.10 [1]
    While here, add patch to increase FD limit for kqueue-based FileSystemWatcher
    [2].
    
    PR:		205902 [1], 205919 [2]
    Submitted by:	feld [1], razzfazz@gmail.com [2]

--- lang/mono/files/patch-mono_metadata_socket-io.c
+++ lang/mono/files/patch-mono_metadata_socket-io.c
@@ -1,13 +1,13 @@
 Fix build on FreeBSD 8
 https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=198404#c6
 
---- mono/metadata/socket-io.c.orig
+--- mono/metadata/socket-io.c.orig	2015-11-12 09:00:29 UTC
 +++ mono/metadata/socket-io.c
-@@ -23,6 +23,7 @@
+@@ -26,6 +26,7 @@
  #ifdef HOST_WIN32
  #include <ws2tcpip.h>
  #else
 +#include <sys/types.h>
  #include <sys/socket.h>
+ #ifdef HAVE_SYS_IOCTL_H
  #include <sys/ioctl.h>
- #include <netinet/in.h>
