3e293c025009727da5db2bc55029ca058411a43d
riggs <riggs@FreeBSD.org>
2016-01-16 20:14:03 +0000

    Update to upstream version 2.8
    PR:		206090
    Submitted by:	dieterich@ogolem.org (maintainer)

--- math/clfft/files/patch-src_statTimer_statisticalTimer.h
+++ math/clfft/files/patch-src_statTimer_statisticalTimer.h
@@ -1,5 +1,5 @@
---- src/statTimer/statisticalTimer.h.orig	2015-10-11 19:43:16.998055000 +0200
-+++ src/statTimer/statisticalTimer.h	2015-10-11 19:44:10.217558000 +0200
+--- src/statTimer/statisticalTimer.h.orig	2015-10-20 00:29:59 UTC
++++ src/statTimer/statisticalTimer.h
 @@ -21,6 +21,7 @@
  #include <vector>
  #include <functional>
