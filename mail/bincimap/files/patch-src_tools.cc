298e2af6078980c1330f65449a8b79ed6ef85bcb
danfe <danfe@FreeBSD.org>
2016-01-17 17:28:08 +0000

    - Resurrect `mail/bincimap' port (project was moved to SF.net from the   abandoned http://www.bincimap.org/ website) - Distfile is .tar.gz now instead of original .tar.bz2, but the contents   is identical (no changes whatsoever) - Clean up and modernize the port: convert to OPTIONS, define LICENSE,   get rid of CONFDIR in favor of ETCDIR, allow to build with GCC 4.7+,   use @sample in pkg-plist, kill bogus EOL whitespace in pkg-descr, etc.

---
+++ mail/bincimap/files/patch-src_tools.cc
@@ -0,0 +1,11 @@
+--- src/tools.cc.orig	2005-01-08 10:20:48 UTC
++++ src/tools.cc
+@@ -35,6 +35,8 @@
+ #include <config.h>
+ #endif
+ #include <errno.h>
++#include <stdlib.h>
++#include <string.h>
+ 
+ #include "tools.h"
+ 
