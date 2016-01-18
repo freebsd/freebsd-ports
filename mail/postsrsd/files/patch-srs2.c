f08273dced7bdc277f2cf66b2a9fd2b4c0969857
pi <pi@FreeBSD.org>
2016-01-18 20:29:11 +0000

    mail/postsrsd: 1.1 -> 1.3
    Changes:
      https://github.com/roehling/postsrsd/commits/master
    
    PR:		206116
    Submitted by:	Krzysztof <ports@bsdserwis.com> (maintainer)

--- mail/postsrsd/files/patch-srs2.c
+++ mail/postsrsd/files/patch-srs2.c
@@ -1,11 +1,15 @@
---- srs2.c	2014-01-03 01:01:47.000000000 +0100
-+++ srs2.c.new	2014-06-17 16:13:45.773381667 +0200
-@@ -21,7 +21,7 @@
+--- srs2.c.orig	2015-12-13 08:56:05 UTC
++++ srs2.c
+@@ -21,7 +21,11 @@
  #include <sys/types.h>  /* tyepdefs */
  #include <sys/time.h>   /* timeval / timezone struct */
  #include <string.h>		/* memcpy, strcpy, memset */
 -#include <alloca.h>
-+#include <stdlib.h>
++#if defined(__FreeBSD__)
++#  include <stdlib.h>
++#else
++#  include <alloca.h>
++#endif
  
  #ifdef USE_OPENSSL
  #include <openssl/hmac.h>
