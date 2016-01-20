cd5c3cd5fda39c86c0d17d673152eba0af7dd106
Kris Moore <kris@pcbsd.org>
2016-01-20 10:05:42 -0500

    Sync several ports with upstream FreeBSD, they all have stale / bad patches

--- benchmarks/httperf/files/patch-httperf.c
+++ benchmarks/httperf/files/patch-httperf.c
@@ -11,13 +11,3 @@
  #ifdef HAVE_SSL
  #  include <openssl/rand.h>
  #endif
-@@ -807,8 +807,7 @@ main (int argc, char **argv)
-       SSL_load_error_strings ();
-       SSLeay_add_ssl_algorithms ();
- 
--      /* for some strange reason, SSLv23_client_method () doesn't work here */
--      ssl_ctx = SSL_CTX_new (SSLv3_client_method ());
-+      ssl_ctx = SSL_CTX_new (SSLv23_client_method ());
-       if (!ssl_ctx)
- 	{
- 	  ERR_print_errors_fp (stderr);
