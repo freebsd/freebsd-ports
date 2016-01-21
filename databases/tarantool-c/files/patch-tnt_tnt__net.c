f12dfa05a2049e5e2f148655c7e74e10ec4ecb3d
vg <vg@FreeBSD.org>
2016-01-21 13:08:47 +0000

    - Update to latest git version - Bump PORTREVISION because developers call is version 0.0.1

---
+++ databases/tarantool-c/files/patch-tnt_tnt__net.c
@@ -0,0 +1,10 @@
+--- tnt/tnt_net.c.orig	2016-01-21 12:48:00 UTC
++++ tnt/tnt_net.c
+@@ -31,6 +31,7 @@
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <stdarg.h>
++#include <stdint.h>
+ #include <string.h>
+ #include <stdbool.h>
+ 
