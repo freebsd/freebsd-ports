ab38774acf99b9f50480e2ce1f455ec5099faf35
rakuco <rakuco@FreeBSD.org>
2016-01-21 11:02:03 +0000

    Add a few patches to fix the build with the upcoming Qt 5.5.1.
    Several places were using QDataStream without including the proper headers.
    These fixes are not enough: the ports needs to explicitly depend on more Qt
    components, but we are still working on that and may end up creating a new,
    smaller port out of devel/qt5-designer.

---
+++ databases/sqlitestudio/files/patch-sqlitestudiocli_cli__config.h
@@ -0,0 +1,10 @@
+--- sqlitestudiocli/cli_config.h.orig	2016-01-21 10:44:03 UTC
++++ sqlitestudiocli/cli_config.h
+@@ -2,6 +2,7 @@
+ #define CLI_CONFIG_H
+ 
+ #include "config_builder.h"
++#include <QDataStream>
+ 
+ namespace CliResultsDisplay
+ {
