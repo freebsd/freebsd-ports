840a1bd5ae06d1b037b36d6427d275a33741756b
rakuco <rakuco@FreeBSD.org>
2016-01-21 11:02:03 +0000

    Add a few patches to fix the build with the upcoming Qt 5.5.1.
    Several places were using QDataStream without including the proper headers.
    These fixes are not enough: the ports needs to explicitly depend on more Qt
    components, but we are still working on that and may end up creating a new,
    smaller port out of devel/qt5-designer.

---
+++ databases/sqlitestudio/files/patch-coreSQLiteStudio_db_db.h
@@ -0,0 +1,10 @@
+--- coreSQLiteStudio/db/db.h.orig	2016-01-21 10:33:48 UTC
++++ coreSQLiteStudio/db/db.h
+@@ -17,6 +17,7 @@
+ #include <QRunnable>
+ #include <QStringList>
+ #include <QSet>
++#include <QDataStream>
+ 
+ /** @file */
+ 
