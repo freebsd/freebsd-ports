2c78e2f8a2fabddf9a08eeba0b12ff55fce506fc
Bartek Rutkowski <robak@FreeBSD.org>
2016-01-22 09:12:14 +0000

    deskutils/py-khal: update 0.6 -> 0.7
    PR:		205863
    Submitted by:	Zane C. Bowers-Hadley <vvelox@vvelox.net>
    Approved by:	Maxim Filimonov <che@bein.link> (maintainer)

--- deskutils/py-khal/files/patch-setup.py
+++ deskutils/py-khal/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2015-08-22 23:17:24 UTC
+--- setup.py.orig	2016-01-05 12:53:52 UTC
 +++ setup.py
-@@ -41,8 +41,6 @@ setup(
+@@ -40,8 +40,6 @@ setup(
      },
      install_requires=requirements,
      extras_require=extra_requirements,
--    setup_requires=['setuptools_scm'],
+-    setup_requires=['setuptools_scm'],  # not needed when using packages from PyPI
 -    use_scm_version={'write_to': 'khal/version.py'},
      classifiers=[
          "Development Status :: 4 - Beta",
