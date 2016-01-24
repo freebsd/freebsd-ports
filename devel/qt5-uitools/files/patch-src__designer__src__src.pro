10da2670f07426d841e6b998e1200354a607ba3a
rakuco <rakuco@FreeBSD.org>
2016-01-24 18:10:14 +0000

    Update Qt5 to 5.5.1.
    This is the latest stable release at time of writing.
    
    Release announcement: http://blog.qt.io/blog/2015/07/01/qt-5-5-released/
    New features in Qt 5.5: https://wiki.qt.io/New_Features_in_Qt_5.5
    
    As usual, huge thanks to Tobias Berner (tcberner@gmail.com) for all his work
    on these ports in kde@'s experimental area51 repository. He's the one who
    started the update and did a lot of the initial work on Qt 5.5. Ralf Nolden
    (nolden@kde.org) has contributed the initial version of most of our new Qt5
    ports.
    
    Also thanks to Yuri Victorovich (yuri@rawbw.com) for contributing PR 205805
    with his own patch for the 5.5.1 update. Some of his changes there prompted
    additional fixes and changes present in the final patch generated from our
    experimental repository.
    
    New ports:
    - comms/qt5-connectivity, comms/qt5-sensors, devel/qt5-location,
      graphics/qt5-3d, net/qt5-enginio, x11-toolkits/qt5-canvas3d,
      x11-toolkits/qt5-uiplugin.
    
    General changes in all Qt5 ports:
    - All Qt5 ports are now built with -Wl,--as-needed to avoid overlinking,
      which is a problem with qmake-based because the libraries passed to the
      linker come from the modules .pri files and many are not necessary.
    - With this change, several ports had their USE_QT5 lines adjusted to
      explicitly include some libraries that were pulled in implicitly, and to
      exclude libraries no longer required with -Wl,--as-needed.
    
    Changes in specific ports:
    - devel/qt5: Drop the SQL_PLUGINS and TOOLS options and depend on all Qt5
      ports by default. It makes the Makefile much simpler, and those options
      were already on by default.
    - devel/qt5-core: The clang+base libstdc++ workaround has been expanded and
      more C++11 features have been disabled when that combination is used by a
      port (basically, FreeBSD 9 with USES=compiler:c++11-lang). The disabled
      features have explanations for why they were disabled in the patched
      header itself.
    - devel/qt5-designer: uiplugins has been split out following a similar
      change upstream. By depending on qt5-uiplugin, qt5-uitools avoids having
      to depend on the big qt5-designer port.
    - multimedia/qt5-multimedia: The port now uses GStreamer 1.0 instead of
      0.10.
    - net/qt5-network: The port now depends on libproxy for proxy settings.
      Using libproxy allows proxy settings to be read from different sources,
      and also allows .pac files to work with Qt.
    - www/qt5-webkit: The port now uses GStreamer 1.0 instead of 0.10.
    
    PR:		205805
    PR:		206435

--- devel/qt5-uitools/files/patch-src__designer__src__src.pro
+++ devel/qt5-uitools/files/patch-src__designer__src__src.pro
@@ -3,10 +3,10 @@ missing dependencies.
 
 --- src/designer/src/src.pro
 +++ src/designer/src/src.pro
-@@ -1,20 +1,4 @@
- TEMPLATE = subdirs
+@@ -2,22 +2,4 @@ TEMPLATE = subdirs
  
  SUBDIRS = \
+     uiplugin \
 -    uitools \
 -    lib \
 -    components \
@@ -14,6 +14,8 @@ missing dependencies.
 -
 -contains(QT_CONFIG, shared): SUBDIRS += plugins
 -
+-uitools.depends = uiplugin
+-lib.depends = uiplugin
 -components.depends = lib
 -designer.depends = components
 -plugins.depends = lib
--- devel/qt5/files/extrapatch-src_corelib_global_qcompilerdetection.h
+++ devel/qt5/files/extrapatch-src_corelib_global_qcompilerdetection.h
@@ -1,15 +1,31 @@
---- src/corelib/global/qcompilerdetection.h.orig	2016-01-03 18:13:55 UTC
+--- src/corelib/global/qcompilerdetection.h.orig	2015-10-13 04:35:31 UTC
 +++ src/corelib/global/qcompilerdetection.h
-@@ -900,10 +900,10 @@
- #    undef Q_COMPILER_REF_QUALIFIERS
+@@ -941,13 +941,26 @@
+ #    undef Q_COMPILER_CONSTEXPR
  #  endif
  # endif // Q_OS_QNX
 -# if (defined(Q_CC_CLANG) || defined(Q_CC_INTEL)) && defined(Q_OS_MAC) && defined(__GNUC_LIBSTD__) \
 -    && ((__GNUC_LIBSTD__-0) * 100 + __GNUC_LIBSTD_MINOR__-0 <= 402)
 +# if (defined(Q_CC_CLANG) || defined(Q_CC_INTEL)) && !defined(_LIBCPP_VERSION)
- // Mac OS X: Apple has not updated libstdc++ since 2007, which means it does not have
+ // Apple has not updated libstdc++ since 2007, which means it does not have
  // <initializer_list> or std::move. Let's disable these features
 +// The same applies to FreeBSD's base libstdc++ version used in FreeBSD 9.x.
  #  undef Q_COMPILER_INITIALIZER_LISTS
  #  undef Q_COMPILER_RVALUE_REFS
  #  undef Q_COMPILER_REF_QUALIFIERS
++// FreeBSD's base libstdc++ does not support std::u{16,32}string.
++#  undef Q_COMPILER_UNICODE_STRINGS
++// FreeBSD's base libstdc++ does not support std::declval, which is used in
++// together with noexcept in some headers such as qpair.h (as of Qt 5.5).
++// Since there is no macro checking for std::declval, just disable noexcept
++// altogether.
++#  undef Q_COMPILER_NOEXCEPT
++// FreeBSD's base libstdc++ is missing constexpr keywords on important
++// functions like std::numeric_limits<>::min(). Disable constexpr even if clang
++// advertises support for it.
++#  undef Q_COMPILER_CONSTEXPR
++// FreeBSD's base libstdc++ does not support std::nullptr_t.
++#  undef Q_COMPILER_NULLPTR
+ // Also disable <atomic>, since it's clearly not there
+ #  undef Q_COMPILER_ATOMICS
+ # endif
