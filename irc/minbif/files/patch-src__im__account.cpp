c0d6c179ddc1b5657adb037ec1d8b6341105876e
Alexey Dokuchaev <danfe@FreeBSD.org>
2016-01-25 15:13:23 +0000

    - Unbreak the build against new Imlib2: previous versions (e.g. 1.4.6) had   two typedefs (apparently, the latter was deprecated and finally removed   in 1.4.7):
        typedef enum _imlib_load_error Imlib_Load_Error;
        typedef enum _imlib_load_error ImlibLoadError;
    
    - Instead of handling of VIDEO and CACA options interdependency manually,
      define VIDEO_IMPLIES=CACA (available since r394573) and thus consummate
      conversion to option helpers
    - Install sample configuration files as @sample in pkg-plist (and convert
      PLIST_FILES to separate file due to better readability thereof)
    - Restore sane installation routine broken in r336927 and install couple
      of extra documentation files while here
    - Augment port description text and update WWW line to avoid redirect

--- irc/minbif/files/patch-src__im__account.cpp
+++ irc/minbif/files/patch-src__im__account.cpp
@@ -1,4 +1,4 @@
---- src/im/account.cpp.orig
+--- src/im/account.cpp.orig	2011-12-04 14:24:51 UTC
 +++ src/im/account.cpp
 @@ -18,6 +18,7 @@
  
@@ -8,3 +8,12 @@
  #ifdef HAVE_IMLIB
  	#include <Imlib2.h>
  #endif /* HAVE_IMLIB */
+@@ -272,7 +273,7 @@ void Account::setBuddyIcon(string filena
+ 				else
+ 				{
+ 					char** prpl_formats = g_strsplit(prplinfo->icon_spec.format,",",0);
+-					ImlibLoadError err = IMLIB_LOAD_ERROR_UNKNOWN;
++					Imlib_Load_Error err = IMLIB_LOAD_ERROR_UNKNOWN;
+ 
+ 					close(temp_fd);
+ 					/* Try to encode in a supported format. */
