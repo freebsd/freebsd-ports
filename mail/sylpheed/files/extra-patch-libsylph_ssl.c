0646c096b4b9e37a651b53d47a998e27799b9f20
ehaupt <ehaupt@FreeBSD.org>
2016-01-25 18:22:40 +0000

    Update to 3.5.0

--- mail/sylpheed/files/extra-patch-libsylph_ssl.c
+++ mail/sylpheed/files/extra-patch-libsylph_ssl.c
@@ -1,4 +1,4 @@
---- libsylph/ssl.c.orig	2014-06-10 04:06:36 UTC
+--- libsylph/ssl.c.orig	2016-01-25 16:00:23 UTC
 +++ libsylph/ssl.c
 @@ -59,6 +59,7 @@ static gchar *find_certs_file(const gcha
  		LOOK_FOR("ca-bundle.crt");
@@ -8,12 +8,3 @@
  	}
  
  #undef LOOK_FOR
-@@ -117,7 +118,7 @@ void ssl_init(void)
- 			certs_dir = NULL;
- 		}
- #else
--		certs_file = find_certs_file("/etc/ssl");
-+		certs_file = find_certs_file("/usr/local/etc/ssl");
- 		if (!certs_file)
- 			certs_file = find_certs_file("/etc");
- #endif
