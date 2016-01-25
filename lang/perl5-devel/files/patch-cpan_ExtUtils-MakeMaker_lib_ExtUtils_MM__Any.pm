50c2919f435bb86e158460b799db82393aad015e
Mathieu Arnold <mat@FreeBSD.org>
2016-01-25 12:03:33 +0000

    Update to v5.23.7-27-g6fa2dc6.
    Changes:	https://github.com/Perl/perl5/compare/v5.23.6-113-gdff8a39...v5.23.7-27-g6fa2dc6
    Sponsored by:	Absolight

--- lang/perl5-devel/files/patch-cpan_ExtUtils-MakeMaker_lib_ExtUtils_MM__Any.pm
+++ lang/perl5-devel/files/patch-cpan_ExtUtils-MakeMaker_lib_ExtUtils_MM__Any.pm
@@ -1,6 +1,6 @@
---- cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Any.pm.orig	2015-04-15 07:47:18 UTC
+--- cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Any.pm.orig	2016-01-25 10:18:16 UTC
 +++ cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Any.pm
-@@ -1871,13 +1871,14 @@ sub init_INSTALL_from_PREFIX {
+@@ -1880,13 +1880,14 @@ sub init_INSTALL_from_PREFIX {
            ('$(PREFIX)') x 3;
      }
      else {
