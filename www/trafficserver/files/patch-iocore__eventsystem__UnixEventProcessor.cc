497b1e5ddd046a4c6cf78438a4aff5c35ab2cde5
Vanilla I. Shu <vanilla@FreeBSD.org>
2016-01-25 14:14:03 +0000

    Upgrade to 6.0.0.
    PR:		206443
    Submitted by:	maintainer

--- www/trafficserver/files/patch-iocore__eventsystem__UnixEventProcessor.cc
+++ www/trafficserver/files/patch-iocore__eventsystem__UnixEventProcessor.cc
@@ -1,4 +1,4 @@
---- iocore/eventsystem/UnixEventProcessor.cc.orig	2015-06-30 04:11:56 UTC
+--- iocore/eventsystem/UnixEventProcessor.cc.orig	2015-09-08 17:43:45 UTC
 +++ iocore/eventsystem/UnixEventProcessor.cc
 @@ -24,7 +24,6 @@
  #include "P_EventSystem.h" /* MAGIC_EDITING_TAG */
@@ -7,4 +7,4 @@
 -#include <alloca.h>
  #include <hwloc.h>
  #endif
- #include "ink_defs.h"
+ #include "ts/ink_defs.h"
