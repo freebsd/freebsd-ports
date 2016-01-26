539a77696c173488801d9177163d3c265bc3c8ff
pi <pi@FreeBSD.org>
2016-01-26 19:59:29 +0000

    www/mod_dnssd: fix build with apache22
    Requested by:	Wolfgang Riegler <wolfgang.riegler@gmx.de>
    Submitted by:	olli hauer <ohauer@gmx.de>

--- www/mod_dnssd/files/patch-src__mod_dnssd.c
+++ www/mod_dnssd/files/patch-src__mod_dnssd.c
@@ -1,9 +1,14 @@
---- src/mod_dnssd.c.orig
+--- src/mod_dnssd.c.orig	2009-01-28 02:24:00 UTC
 +++ src/mod_dnssd.c
-@@ -27,6 +27,11 @@
+@@ -24,9 +24,16 @@
+ #include <ap_config.h>
+ #include <apr_strings.h>
+ #include <unixd.h>
++#if MODULE_MAGIC_COOKIE >= 0x41503234UL /* "AP24" */
++#include <mod_unixd.h>
++#endif
  #include <apr_signal.h>
  #include <mpm_common.h>
-+#include <mod_unixd.h>
  
 +#if MODULE_MAGIC_NUMBER_MAJOR >= 20090130
 +#define unixd_setup_child ap_unixd_setup_child
