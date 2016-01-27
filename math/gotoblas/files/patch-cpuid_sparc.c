bb6678501876aea084b8601132a4f9337a48133d
Carlos J. Puga Medina <cpm@FreeBSD.org>
2016-01-27 09:42:36 +0000

    Log:   - Fix build with new GCC and remove BROKEN   - Regenerate patches to make portlint(1) happy   - Add fortran to USES   - Bump PORTREVISION
    PR:		205241
    Submitted by:	Carlos J. Puga Medina <cpm@fbsd.es>
    Approved by:	junovitch (mentor)

--- math/gotoblas/files/patch-cpuid_sparc.c
+++ math/gotoblas/files/patch-cpuid_sparc.c
@@ -1,6 +1,6 @@
---- cpuid_sparc.c.orig	2010-11-05 09:57:49.000000000 -0400
-+++ cpuid_sparc.c	2011-09-23 13:10:18.000000000 -0400
-@@ -49,6 +49,7 @@
+--- cpuid_sparc.c.orig	2010-11-05 13:57:49 UTC
++++ cpuid_sparc.c
+@@ -49,6 +49,7 @@ void get_subdirname(void){
  }
  
  void get_cpuconfig(void){
