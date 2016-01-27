152130a455c3140586f1c325d079e5bdc1191539
johans <johans@FreeBSD.org>
2016-01-27 10:54:07 +0000

    Add patch to fix build on ARM v6
    Submitted by:	mmel

--- security/nettle/files/patch-arm__v6__sha256-compress.asm
+++ security/nettle/files/patch-arm__v6__sha256-compress.asm
@@ -1,6 +1,6 @@
---- arm/v6/sha256-compress.asm.orig	2013-05-28 16:21:54.000000000 +0200
-+++ arm/v6/sha256-compress.asm	2015-01-04 00:39:14.000000000 +0100
-@@ -112,7 +112,7 @@ define(<NOEXPN>, <
+--- arm/v6/sha256-compress.asm.orig	2015-04-24 19:22:03.000000000 +0200
++++ arm/v6/sha256-compress.asm	2016-01-27 11:48:29.249375000 +0100
+@@ -126,7 +126,7 @@ define(<NOEXPN>, <
  PROLOGUE(_nettle_sha256_compress)
  	push	{r4,r5,r6,r7,r8,r10,r11,r14}
  	sub	sp, sp, #68
@@ -9,7 +9,7 @@
  
  	C Load data up front, since we don't have enough registers
  	C to load and shift on-the-fly
-@@ -185,7 +185,7 @@ PROLOGUE(_nettle_sha256_compress)
+@@ -199,7 +199,7 @@ PROLOGUE(_nettle_sha256_compress)
  	EXPN(15) ROUND(SB,SC,SD,SE,SF,SG,SH,SA)
  	bne	.Loop2
  
