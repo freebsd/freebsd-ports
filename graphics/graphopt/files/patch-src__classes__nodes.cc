ff5011faa248f65f1504a3188f818363a36e63c2
Alexey Dokuchaev <danfe@FreeBSD.org>
2016-01-29 03:21:32 +0000

    Merge patches that patch the same file.

--- graphics/graphopt/files/patch-src__classes__nodes.cc
+++
@@ -1,9 +0,0 @@
---- src/classes/nodes.cc.orig	2013-12-10 00:22:15.000000000 +0900
-+++ src/classes/nodes.cc	2013-12-10 00:22:39.000000000 +0900
-@@ -1,4 +1,5 @@
--#include <stdio.h>
-+#include <cstdio>
-+#include <cstdlib>
- #include "nodes.h"
- 
- 
--- mail/dovecot/files/patch-src_login-common_ssl-proxy-openssl.c
+++ mail/dovecot/files/patch-src_login-common_ssl-proxy-openssl.c
@@ -1,5 +1,5 @@
---- src/login-common/ssl-proxy-openssl.c.orig	2015-03-07 13:25:36.487131678 +0100
-+++ src/login-common/ssl-proxy-openssl.c	2015-03-07 13:24:28.303136938 +0100
+--- src/login-common/ssl-proxy-openssl.c.orig	2011-01-20 21:16:59 UTC
++++ src/login-common/ssl-proxy-openssl.c
 @@ -21,6 +21,7 @@
  #include <openssl/ssl.h>
  #include <openssl/err.h>
@@ -8,3 +8,12 @@
  
  #define DOVECOT_SSL_DEFAULT_CIPHER_LIST "ALL:!LOW:!SSLv2"
  /* Check every 30 minutes if parameters file has been updated */
+@@ -817,7 +818,7 @@ static void ssl_proxy_ctx_init(SSL_CTX *
+ {
+ 	const char *cafile;
+ 
+-	SSL_CTX_set_options(ssl_ctx, SSL_OP_ALL);
++	SSL_CTX_set_options(ssl_ctx, SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
+ 
+ 	cafile = getenv("SSL_CA_FILE");
+ 	if (cafile != NULL) {
