b5d6e3871c9c18c5bffddb0307daf69183fef476
Michael Landin <mich@FreeBSD.org>
2016-01-31 13:48:57 +0000

    - fix IP truncation and add license info
    PR:		206782
    Submitted by:	Franco Fichtner (franco@opnsense.org)

--- net-mgmt/rate/files/patch-rate.c
+++ net-mgmt/rate/files/patch-rate.c
@@ -1,6 +1,6 @@
---- rate.c.orig
+--- rate.c.orig	2003-08-20 15:22:28 UTC
 +++ rate.c
-@@ -74,6 +74,7 @@
+@@ -74,6 +74,7 @@ int opt_separator = 0;
  int opt_cls = 0;
  int opt_dns = 1;
  int opt_cols = 80;
@@ -8,7 +8,7 @@
  
  #ifdef HAVE_REGEX
  char * opt_regex = NULL;
-@@ -214,6 +215,7 @@
+@@ -214,6 +215,7 @@ void help(unsigned char * name)
  	printf("  -p <pref> Datalink layer header size (gets substracted from each packet size, default: 14)\n");
  	printf("  -s <b>    Capture l bytes (default: 40)\n");
  	printf("  -i <int>  Bind to interface <int> - default eth0\n");
@@ -16,7 +16,7 @@
  	printf("  -n        Numeric IPs. Don't do reverse DNS lookups.\n");
  	printf("  -c        Use colors (ANSI-compatible) whenever possible.\n");
  	printf("  -v        Print exact values, do not use SI prefixes.\n");
-@@ -264,7 +266,7 @@
+@@ -264,7 +266,7 @@ int main(int argc, char ** argv)
  		}
  	}
  	
@@ -25,7 +25,7 @@
  	{
  		if(c == 'R' || c == 'A' || c == 'T' || c == 'E')
  			break;
-@@ -284,6 +286,7 @@
+@@ -284,6 +286,7 @@ int main(int argc, char ** argv)
  			case 'k': opt_onkeyboard = 1; break;
  			case 'c': opt_color = 1; break;
  			case 'v': opt_human = 0; break;
@@ -33,7 +33,7 @@
  			case 'e': opt_separator = 1; break;
  			case 'w': opt_cls = 1; break;
  			case 'n': opt_dns = 0; break;
-@@ -311,7 +314,7 @@
+@@ -311,7 +314,7 @@ int main(int argc, char ** argv)
  		}
  	}
  
