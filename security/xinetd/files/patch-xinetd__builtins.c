9d2e434521cda063e31d2c1e475e3f623d79fc6c
Renato Botelho <garga@FreeBSD.org>
2016-02-01 09:48:34 +0000

    Add a new option to conditionalize xconv installation
    xconv is the only piece of xinetd written in perl, removing it from package
    also removes perl dependency.
    
    Add a new option, on by default to don't change current behavior, to select
    to install or not xconv.
    
    While here, re-generate patches using `make makepatch` to silence portlint
    
    PR:		205661
    Approved by:	maintainer timeout (> 30 days)
    Obtained from:	pfSense
    Sponsored by:	Rubicon Communications (Netgate)

--- security/xinetd/files/patch-xinetd__builtins.c
+++ security/xinetd/files/patch-xinetd__builtins.c
@@ -1,6 +1,6 @@
---- xinetd/builtins.c.orig	2013-10-03 16:04:32.562080623 +0400
-+++ xinetd/builtins.c	2013-10-03 16:04:57.297350342 +0400
-@@ -617,7 +617,7 @@
+--- xinetd/builtins.c.orig	2012-05-09 15:40:29 UTC
++++ xinetd/builtins.c
+@@ -617,7 +617,7 @@ static void tcpmux_handler( const struct
     if( SC_IS_INTERNAL( scp ) ) {
        SC_INTERNAL(scp, nserp);
     } else {
