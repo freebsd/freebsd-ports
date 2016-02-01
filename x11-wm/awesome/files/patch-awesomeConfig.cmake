38c320b032cf9d30d826551bdfc4cbd80a3e3f11
garga <garga@FreeBSD.org>
2016-02-01 11:25:55 +0000

    Update x11-wm/awesome to 3.5.8

--- x11-wm/awesome/files/patch-awesomeConfig.cmake
+++ x11-wm/awesome/files/patch-awesomeConfig.cmake
@@ -1,6 +1,6 @@
---- awesomeConfig.cmake.orig	2012-12-21 21:18:18.000000000 -0200
-+++ awesomeConfig.cmake	2013-03-08 11:46:07.000000000 -0300
-@@ -14,6 +14,7 @@
+--- awesomeConfig.cmake.orig	2016-01-15 15:51:19 UTC
++++ awesomeConfig.cmake
+@@ -14,6 +14,7 @@ option(WITH_DBUS "build with D-BUS" ON)
  option(GENERATE_MANPAGES "generate manpages" ON)
  option(COMPRESS_MANPAGES "compress manpages" ON)
  option(GENERATE_DOC "generate API documentation" ON)
@@ -8,7 +8,7 @@
  
  # {{{ CFLAGS
  add_definitions(-O1 -std=gnu99 -ggdb3 -rdynamic -fno-strict-aliasing -Wall -Wextra
-@@ -58,7 +59,7 @@
+@@ -61,7 +62,7 @@ endif()
  # theme graphics
  a_find_program(CONVERT_EXECUTABLE convert TRUE)
  # doxygen
@@ -17,7 +17,7 @@
  # pkg-config
  include(FindPkgConfig)
  # lua 5.1
-@@ -256,7 +257,7 @@
+@@ -264,7 +265,7 @@ endif()
  if(DEFINED AWESOME_MAN_PATH)
     set(AWESOME_MAN_PATH ${AWESOME_MAN_PATH} CACHE PATH "awesome manpage directory")
  else()
