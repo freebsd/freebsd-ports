b81856b7821e2f8cb82c78dc721d13998d53b3e5
Kubilay Kocak <koobs@FreeBSD.org>
2016-02-04 10:44:33 +0000

    MFH: r408021 security/py-rsa: Update to 3.3 (Fixes CVE-2016-1494)
    - Update PORTVERSION and distinfo checksum (3.3)
    - Modernize TEST entries (test target, TEST_DEPENDS, et al)
    - Update setup.py patch (zip_safe no longer needed)
    - Add LICENSE_FILE
    - Enable NO_ARCH
    
    This version fixed a security vulnerability:
    
    https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2016-1494
    
    PR:		206746
    Reported by:	Sevan Janiyan <venture37 geeklan co.uk>
    Security:	e78bfc9d-cb1e-11e5-b251-0050562a4d7b
    Security:	CVE-2016-1494
    
    Approved by:	ports-secteam (security)

--- security/py-rsa/files/patch-setup.py
+++ security/py-rsa/files/patch-setup.py
@@ -1,9 +1,12 @@
---- ./setup.py.orig	2013-09-26 22:34:41.233363167 +1000
-+++ ./setup.py	2013-09-26 22:34:50.192985862 +1000
-@@ -42,5 +42,5 @@
-         'pyrsa-encrypt-bigfile = rsa.cli:encrypt_bigfile',
-         'pyrsa-decrypt-bigfile = rsa.cli:decrypt_bigfile',
-     ]},
--
-+    zip_safe=False,
- )
+--- setup.py.orig	2016-01-13 08:43:51 UTC
++++ setup.py
+@@ -42,6 +42,9 @@ setup(name='rsa',
+       install_requires=[
+           'pyasn1 >= 0.1.3',
+       ],
++      tests_require=[
++          'unittest2',
++      ],
+       entry_points={'console_scripts': [
+           'pyrsa-priv2pub = rsa.util:private_to_public',
+           'pyrsa-keygen = rsa.cli:keygen',
