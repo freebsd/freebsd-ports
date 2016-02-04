880579f55a8eac2d55fe4b1de7a47dbb71786c7e
John Marino <marino@FreeBSD.org>
2016-02-04 14:48:06 +0000

    sysutils/less: document ncurses requirement (USES+=ncurses), plus ...
    In addition to setting USES=ncurses:
      * align MASTER_SITES
      * align USES
      * change USES operator from += to =
      * line to libncurses rather than libtermcap
      * regenerate patches with makepatch
    
    approved by:	infrastructure blanket

--- sysutils/less/files/patch-charset.c
+++ sysutils/less/files/patch-charset.c
@@ -1,6 +1,6 @@
---- charset.c.orig	2010/10/30 02:38:17
-+++ charset.c	2010/10/30 02:40:03
-@@ -408,6 +408,10 @@
+--- charset.c.orig	2013-04-04 16:55:05 UTC
++++ charset.c
+@@ -407,6 +407,10 @@ binary_char(c)
  control_char(c)
  	LWCHAR c;
  {
@@ -11,7 +11,7 @@
  	c &= 0377;
  	return (chardef[c] & IS_CONTROL_CHAR);
  }
-@@ -423,6 +427,20 @@
+@@ -422,6 +426,20 @@ prchar(c)
  	/* {{ This buffer can be overrun if LESSBINFMT is a long string. }} */
  	static char buf[32];
  
@@ -32,7 +32,7 @@
  	c &= 0377;
  	if ((c < 128 || !utf_mode) && !control_char(c))
  		SNPRINTF1(buf, sizeof(buf), "%c", (int) c);
-@@ -446,6 +464,7 @@
+@@ -445,6 +463,7 @@ prchar(c)
  #endif
  	else
  		SNPRINTF1(buf, sizeof(buf), binfmt, c);
