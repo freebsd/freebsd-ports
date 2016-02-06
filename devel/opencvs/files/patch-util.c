14840b033b3aa27c6c7883632547fb3d86a8dfcc
johans <johans@FreeBSD.org>
2016-02-06 22:44:44 +0000

    Update to a more recent version of OpenBSD's CVS This lacks a replacement for OpenBSD's use of pledge(2)

--- devel/opencvs/files/patch-util.c
+++ devel/opencvs/files/patch-util.c
@@ -1,5 +1,5 @@
---- util.c.orig	2015-05-04 09:15:45 UTC
-+++ util.c
+--- util.c.orig	2016-02-06 23:25:26.000000000 +0100
++++ util.c	2016-02-06 23:33:35.548485000 +0100
 @@ -30,7 +30,7 @@
  #include <sys/types.h>
  #include <sys/wait.h>
