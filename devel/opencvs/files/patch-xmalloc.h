44ee51b321ef05f07a7721ed92146075f1aa69b6
johans <johans@FreeBSD.org>
2016-02-06 22:44:44 +0000

    Update to a more recent version of OpenBSD's CVS This lacks a replacement for OpenBSD's use of pledge(2)

--- devel/opencvs/files/patch-xmalloc.h
+++ devel/opencvs/files/patch-xmalloc.h
@@ -1,6 +1,6 @@
---- xmalloc.h.orig	2015-05-04 09:15:45 UTC
-+++ xmalloc.h
-@@ -29,7 +29,6 @@ int	 xasprintf(char **, const char *, ..
+--- xmalloc.h.orig	2016-02-06 23:25:26.000000000 +0100
++++ xmalloc.h	2016-02-06 23:33:35.565364000 +0100
+@@ -28,7 +28,6 @@ int	 xasprintf(char **, const char *, ..
                  __attribute__((__nonnull__ (2)));
  int	 xsnprintf(char *, size_t, const char *, ...)
  		__attribute__((__format__ (printf, 3, 4)))
