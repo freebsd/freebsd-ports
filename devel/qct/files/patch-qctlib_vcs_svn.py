4565e6be61f155831db7696c1b948389618d693c
Dmitry Marakasov <amdmi3@FreeBSD.org>
2016-02-07 11:22:59 +0000

    - Add LICENSE_FILE - Switch to options helpers - Fix docfile installation - Regenerate patches with `make makepatch`

--- devel/qct/files/patch-qctlib_vcs_svn.py
+++ devel/qct/files/patch-qctlib_vcs_svn.py
@@ -1,7 +1,7 @@
---- qctlib/vcs/svn.py.orig	2009-02-15 23:49:32.000000000 +0100
-+++ qctlib/vcs/svn.py	2009-07-22 17:58:33.000000000 +0200
+--- qctlib/vcs/svn.py.orig	2009-02-15 22:49:32 UTC
++++ qctlib/vcs/svn.py
 @@ -84,6 +84,8 @@ class qctVcsSvn:
-
+ 
          if showIgnored:  extra = ['--no-ignore']
          else:            extra = []
 +        versionOutput = runProgram([self.svn_exe, '--version', '--quiet'])
@@ -11,7 +11,7 @@
          recs.pop() # remove last entry (which is '')
 @@ -91,11 +93,19 @@ class qctVcsSvn:
          if pb: pb.setValue(2)
-
+ 
          for line in recs:
 -            if len(line) < 7:
 -                continue
@@ -33,3 +33,4 @@
 +                self.fileStatus[ fname ] = line[0:6]
              if status == 'M':  # modified
                  itemList.append('M ' + fname)
+             elif status == 'A':  # added
