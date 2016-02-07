148517e4757f72f97f4b454c9d3d8189c633a6e4
swills <swills@FreeBSD.org>
2016-02-07 01:55:37 +0000

    www/py-graphite-web: update to 0.9.15
    PR:		206534
    Submitted by:	Michael Zhilin <mizhka@gmail.com>

--- www/py-graphite-web/files/patch-webapp__graphite__dashboard__views.py
+++
@@ -1,8 +0,0 @@
---- webapp/graphite/dashboard/views.py.orig	2014-09-14 03:00:52 UTC
-+++ webapp/graphite/dashboard/views.py
-@@ -249,4 +249,4 @@
- 
- 
- def json_response(obj):
--  return HttpResponse(mimetype='application/json', content=json.dumps(obj))
-+  return HttpResponse(content_type='application/json', content=json.dumps(obj))
