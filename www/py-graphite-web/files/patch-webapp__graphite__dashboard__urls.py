9354f63ab0c146bb74a11220c94a8e96e2245d99
Steve Wills <swills@FreeBSD.org>
2016-02-07 01:55:37 +0000

    www/py-graphite-web: update to 0.9.15
    PR:		206534
    Submitted by:	Michael Zhilin <mizhka@gmail.com>

--- www/py-graphite-web/files/patch-webapp__graphite__dashboard__urls.py
+++
@@ -1,8 +0,0 @@
---- ./webapp/graphite/dashboard/urls.py.orig	2013-08-21 17:11:04.000000000 +0000
-+++ ./webapp/graphite/dashboard/urls.py	2014-02-13 02:01:59.482110196 +0000
-@@ -1,4 +1,4 @@
--from django.conf.urls.defaults import *
-+from django.conf.urls import *
- 
- urlpatterns = patterns('graphite.dashboard.views',
-   ('^save/(?P<name>[^/]+)', 'save'),
