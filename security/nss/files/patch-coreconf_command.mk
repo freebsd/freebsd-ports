50d2caa5c82d61a73f0ee4c8ede4ad3daeff37e2
Jan Beich <jbeich@FreeBSD.org>
2016-02-09 01:34:41 +0000

    MFH: r405494
    security/nss: update to 3.21
    
    Temporarily disable gtests as they require C++0x support and install a
    library that may have conflicting API with devel/googletest.
    
    Changes:	https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSS/NSS_3.21_release_notes
    Approved by:	ports-secteam (feld)

--- security/nss/files/patch-coreconf_command.mk
+++ security/nss/files/patch-coreconf_command.mk
@@ -1,9 +1,11 @@
 --- coreconf/command.mk	Mon Oct 10 19:46:12 2005
 +++ coreconf/command.mk	Wed Jan 18 17:23:28 2006
-@@ -46,5 +46,5 @@
- LINK_DLL      = $(LINK) $(OS_DLLFLAGS) $(DLLFLAGS)
- LINK_EXE      = $(LINK) $(OS_LFLAGS) $(LFLAGS)
--CFLAGS        = $(OPTIMIZER) $(OS_CFLAGS) $(XP_DEFINE) $(DEFINES) $(INCLUDES) \
-+CFLAGS        += $(OPTIMIZER) $(OS_CFLAGS) $(XP_DEFINE) $(DEFINES) $(INCLUDES) \
- 		$(XCFLAGS)
+@@ -12,7 +12,7 @@ AS            = $(CC)
+ ASFLAGS      += $(CFLAGS)
+ CCF           = $(CC) $(CFLAGS)
+ LINK_DLL      = $(LINK) $(OS_DLLFLAGS) $(DLLFLAGS) $(XLDFLAGS)
+-CFLAGS        = $(OPTIMIZER) $(OS_CFLAGS) $(WARNING_CFLAGS) $(XP_DEFINE) \
++CFLAGS       += $(OPTIMIZER) $(OS_CFLAGS) $(WARNING_CFLAGS) $(XP_DEFINE) \
+                 $(DEFINES) $(INCLUDES) $(XCFLAGS)
+ PERL          = perl
  RANLIB        = echo
