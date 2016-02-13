61063501f0eac5b507d3c4f8d697b5d3fcb990d7
osa <osa@FreeBSD.org>
2016-02-13 11:35:31 +0000

    Fix the build by removing needless check for a git client. Bump PORTREVISION.

--- devel/libbrotli/files/patch-autogen.sh
+++ devel/libbrotli/files/patch-autogen.sh
@@ -1,9 +1,15 @@
 --- autogen.sh.orig	2016-01-20 21:31:42 UTC
 +++ autogen.sh
-@@ -12,8 +12,6 @@ if test $? -ne 0; then
+@@ -6,14 +6,6 @@ if test $? -ne 0; then
    exit 1
  fi
  
+-GIT=`which git 2>/dev/null`
+-if test $? -ne 0; then
+-  echo "No 'git' found. You must install the git package."
+-  exit 1
+-fi
+-
 -$GIT submodule init
 -$GIT submodule update
  
