bad0c845b28839c9b495c6a6a93391003c069ab0
danilo <danilo@FreeBSD.org>
2016-02-15 23:49:11 +0000

    - Update to 3.0.11 - Address CVE-2014-9765
    PR:		207174
    Submitted by:	junovitch
    MFH:		2016Q1

--- misc/xdelta3/files/patch-testing_regtest.cc
+++ misc/xdelta3/files/patch-testing_regtest.cc
@@ -1,9 +1,9 @@
---- ../../xdelta3-3.0.7/testing/regtest.cc	2013-05-13 04:54:35.000000000 +0000
-+++ testing/regtest.cc	2013-05-20 02:32:05.243969745 +0000
-@@ -761,7 +761,7 @@
+--- testing/regtest.cc.orig	2016-02-15 23:07:07 UTC
++++ testing/regtest.cc
+@@ -1006,7 +1006,7 @@ void FourWayMergeTest(const FileSpec &sp
    ExtFile d01, d12, d23;
    Options options;
-   options.encode_srcwin_maxsz = 
+   options.encode_srcwin_maxsz =
 -    std::max(spec0.Size(), options.encode_srcwin_maxsz);
 +    std::max((unsigned long long)spec0.Size(), (unsigned long long)options.encode_srcwin_maxsz);
  
