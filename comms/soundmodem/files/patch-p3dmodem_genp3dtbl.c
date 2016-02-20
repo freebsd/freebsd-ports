2c99c062fbf62961eabd898d62a52758f28ad0e4
Diane Bruce <db@FreeBSD.org>
2016-02-20 17:21:29 +0000

    Update to 0.20 Clean up submitted patch to work with system clang compiler Change to new upstream repository
    PR:		ports/206705
    Submitted by:	takefu@airport.fm

--- comms/soundmodem/files/patch-p3dmodem_genp3dtbl.c
+++ comms/soundmodem/files/patch-p3dmodem_genp3dtbl.c
@@ -1,5 +1,5 @@
---- p3dmodem/genp3dtbl.c.orig	2002-08-09 15:01:52.000000000 -0500
-+++ p3dmodem/genp3dtbl.c	2013-12-14 17:48:45.000000000 -0500
+--- p3dmodem/genp3dtbl.c.orig	2002-08-09 20:01:52 UTC
++++ p3dmodem/genp3dtbl.c
 @@ -39,6 +39,8 @@
  #include <float.h>
  #include <stdio.h>
