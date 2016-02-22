aa95ec2b5e9409c5aa0942ec46022ca2fdf6a18b
mat <mat@FreeBSD.org>
2016-02-22 20:13:01 +0000

    Fix build as a user. Again.
    While there, make the port prefix safe, again.
    
    Sponsored by:	Absolight

--- databases/opentsdb/files/patch-src_utils_Config.java
+++ databases/opentsdb/files/patch-src_utils_Config.java
@@ -4,7 +4,7 @@
      } else {
        file_locations.add("/etc/opentsdb.conf");
        file_locations.add("/etc/opentsdb/opentsdb.conf");
-+      file_locations.add("/usr/local/etc/opentsdb/opentsdb.conf");
++      file_locations.add("%%PREFIX%%/etc/opentsdb/opentsdb.conf");
        file_locations.add("/opt/opentsdb/opentsdb.conf");
      }
  
