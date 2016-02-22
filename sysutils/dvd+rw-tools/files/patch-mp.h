5cf2b7440cf4ecd885c4f277aa74f06bf9d19bcd
Dmitry Marakasov <amdmi3@FreeBSD.org>
2016-02-22 10:08:54 +0000

    - Add LICENSE_FILE - Switch to options helpers - Pet portlint - Regenerate patches with `make makepatch`

--- sysutils/dvd+rw-tools/files/patch-mp.h
+++ sysutils/dvd+rw-tools/files/patch-mp.h
@@ -1,6 +1,6 @@
---- mp.h.orig	2008-02-24 18:56:07.000000000 +0100
-+++ mp.h	2013-01-15 00:54:45.484476000 +0100
-@@ -46,7 +46,9 @@
+--- mp.h.orig	2008-02-24 17:56:07 UTC
++++ mp.h
+@@ -46,7 +46,9 @@ static void *__thread_create(int (*func)
  
      if (pthread_attr_init(&attr)==0 &&
  	pthread_attr_setstacksize(&attr,stack_sz)==0 &&
