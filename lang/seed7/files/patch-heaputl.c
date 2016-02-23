fe905235c764ff3959009a2580c683aa4e690a63
Pietro Cerutti <gahr@FreeBSD.org>
2016-02-23 12:49:42 +0000

    land/seed7: better patch for missing uint64_t

--- lang/seed7/files/patch-heaputl.c
+++ lang/seed7/files/patch-heaputl.c
@@ -1,10 +1,10 @@
---- heaputl.c.orig	2016-02-05 14:25:17 UTC
+--- heaputl.c.orig	2015-08-29 06:55:33 UTC
 +++ heaputl.c
-@@ -36,6 +36,7 @@
- 
+@@ -37,6 +37,7 @@
  #include "stdlib.h"
  #include "stdio.h"
-+#include "stdint.h"
  #if HAS_GETRLIMIT && defined STACK_SIZE
++#include "sys/types.h"
  #include "sys/resource.h"
  #endif
+ 
