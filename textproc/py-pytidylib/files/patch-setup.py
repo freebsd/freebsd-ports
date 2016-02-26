2fed88315102466627d67600b791301d0b000b63
koobs <koobs@FreeBSD.org>
2016-02-26 13:19:07 +0000

    textproc/py-pytidylib: Update to 0.2.4
    - Update PORTVERSION and distinfo checksum (0.2.4)
    - Update COMMENT to more closely match setup.py:description
    - Update test target
    - Remove TEST_DEPENDS (pytest is not required)
    - Enable NO_ARCH (architecture independent)
    - Patch setup.py to enable setuptools test command

---
+++ textproc/py-pytidylib/files/patch-setup.py
@@ -0,0 +1,9 @@
+--- setup.py.orig	2016-02-26 12:55:44 UTC
++++ setup.py
+@@ -84,5 +84,6 @@ setup(
+           'Topic :: Text Processing :: Markup :: HTML',
+           'Topic :: Text Processing :: Markup :: XML',
+           ],
++    test_suite='tests',
+     )
+ 
