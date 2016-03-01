26a50ad18950256713773cef5b8cc5bc6745e5fb
Alexey Dokuchaev <danfe@FreeBSD.org>
2016-03-01 15:56:22 +0000

    - Update to the official release version 3.1.1 - Make MIDI playback via FluidSynth optional - Rename patches to chase new source structure
    Reported by:	portscout

--- games/slade/files/patch-src_MainEditor_UI_EntryPanel_AudioEntryPanel.h
+++ games/slade/files/patch-src_MainEditor_UI_EntryPanel_AudioEntryPanel.h
@@ -1,5 +1,5 @@
---- src/AudioEntryPanel.h.orig	2015-12-12 08:41:35 UTC
-+++ src/AudioEntryPanel.h
+--- src/MainEditor/UI/EntryPanel/AudioEntryPanel.h.orig	2015-12-12 08:41:35 UTC
++++ src/MainEditor/UI/EntryPanel/AudioEntryPanel.h
 @@ -27,7 +27,9 @@ private:
  	wxSlider*		slider_seek;
  	wxSlider*		slider_volume;
