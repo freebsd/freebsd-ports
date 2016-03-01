9e26f6188d6b7897eecca4c04dc617a41ce3ba21
danfe <danfe@FreeBSD.org>
2016-03-01 15:56:22 +0000

    - Update to the official release version 3.1.1 - Make MIDI playback via FluidSynth optional - Rename patches to chase new source structure
    Reported by:	portscout

--- games/slade/files/patch-src_MainEditor_UI_EntryPanel_AudioEntryPanel.cpp
+++ games/slade/files/patch-src_MainEditor_UI_EntryPanel_AudioEntryPanel.cpp
@@ -1,6 +1,6 @@
---- src/AudioEntryPanel.cpp.orig	2015-12-12 08:41:35 UTC
-+++ src/AudioEntryPanel.cpp
-@@ -89,15 +89,17 @@ AudioEntryPanel::AudioEntryPanel(wxWindo
+--- src/MainEditor/UI/EntryPanel/AudioEntryPanel.cpp.orig	2016-02-28 02:57:06 UTC
++++ src/MainEditor/UI/EntryPanel/AudioEntryPanel.cpp
+@@ -100,15 +100,17 @@ AudioEntryPanel::AudioEntryPanel(wxWindo
  		LOG_MESSAGE(3, "Windows Media Player installed, using wxMediaCtrl");
  		media_ctrl = new wxMediaCtrl(this, -1);
  	}
@@ -20,14 +20,14 @@
  	if (media_ctrl) sizer_main->Add(media_ctrl, 0);
  #endif
  	sizer_main->Add(sizer_gb, 0, wxALIGN_CENTER);
-@@ -144,13 +146,17 @@ AudioEntryPanel::AudioEntryPanel(wxWindo
- 	sound.setVolume(snd_volume);
- 	music.setVolume(snd_volume);
+@@ -155,13 +157,17 @@ AudioEntryPanel::AudioEntryPanel(wxWindo
+ 	sound->setVolume(snd_volume);
+ 	music->setVolume(snd_volume);
  	theMIDIPlayer->setVolume(snd_volume);
 +#if wxUSE_MEDIACTRL
  	if (media_ctrl) media_ctrl->SetVolume(snd_volume*0.01);
 +#endif
- 	mod.setVolume(snd_volume);
+ 	mod->setVolume(snd_volume);
  	//theGMEPlayer->setVolume(snd_volume);
  	//theOPLPlayer->setVolume(snd_volume);
  
@@ -38,7 +38,7 @@
  	toolbar->Show(false);
  
  	// Bind events
-@@ -497,6 +503,7 @@ bool AudioEntryPanel::openMod(MemChunk& 
+@@ -513,6 +519,7 @@ bool AudioEntryPanel::openMod(MemChunk& 
  *******************************************************************/
  bool AudioEntryPanel::openMedia(string filename)
  {
@@ -46,7 +46,7 @@
  	// Attempt to open with wxMediaCtrl
  	if (media_ctrl && media_ctrl->Load(filename))
  	{
-@@ -511,6 +518,7 @@ bool AudioEntryPanel::openMedia(string f
+@@ -527,6 +534,7 @@ bool AudioEntryPanel::openMedia(string f
  
  		return true;
  	}
@@ -54,8 +54,8 @@
  	
  	return false;
  }
-@@ -533,8 +541,10 @@ void AudioEntryPanel::startStream()
- 		mod.play(); break;
+@@ -549,8 +557,10 @@ void AudioEntryPanel::startStream()
+ 		mod->play(); break;
  	case AUTYPE_MIDI:
  		theMIDIPlayer->play(); break;
 +#if wxUSE_MEDIACTRL
@@ -65,8 +65,8 @@
  	}
  }
  
-@@ -553,8 +563,10 @@ void AudioEntryPanel::stopStream()
- 		mod.pause(); break;
+@@ -569,8 +579,10 @@ void AudioEntryPanel::stopStream()
+ 		mod->pause(); break;
  	case AUTYPE_MIDI:
  		theMIDIPlayer->pause();	break;
 +#if wxUSE_MEDIACTRL
@@ -76,8 +76,8 @@
  	}
  }
  
-@@ -574,8 +586,10 @@ void AudioEntryPanel::resetStream()
- 		mod.stop(); break;
+@@ -590,8 +602,10 @@ void AudioEntryPanel::resetStream()
+ 		mod->stop(); break;
  	case AUTYPE_MIDI:
  		theMIDIPlayer->stop(); break;
 +#if wxUSE_MEDIACTRL
@@ -87,8 +87,8 @@
  	}
  }
  
-@@ -757,8 +771,10 @@ void AudioEntryPanel::onTimer(wxTimerEve
- 		pos = mod.getPlayingOffset().asMilliseconds(); break;
+@@ -773,8 +787,10 @@ void AudioEntryPanel::onTimer(wxTimerEve
+ 		pos = mod->getPlayingOffset().asMilliseconds(); break;
  	case AUTYPE_MIDI:
  		pos = theMIDIPlayer->getPosition(); break;
 +#if wxUSE_MEDIACTRL
@@ -98,18 +98,18 @@
  	}
  
  	// Set slider
-@@ -769,7 +785,9 @@ void AudioEntryPanel::onTimer(wxTimerEve
- 	        (audio_type == AUTYPE_SOUND && sound.getStatus() == sf::Sound::Stopped) ||
- 	        (audio_type == AUTYPE_MUSIC && music.getStatus() == sf::Sound::Stopped) ||
- 			(audio_type == AUTYPE_MOD && mod.getStatus() == sf::Sound::Stopped) ||
+@@ -785,7 +801,9 @@ void AudioEntryPanel::onTimer(wxTimerEve
+ 	        (audio_type == AUTYPE_SOUND && sound->getStatus() == sf::Sound::Stopped) ||
+ 	        (audio_type == AUTYPE_MUSIC && music->getStatus() == sf::Sound::Stopped) ||
+ 			(audio_type == AUTYPE_MOD && mod->getStatus() == sf::Sound::Stopped) ||
 +#if wxUSE_MEDIACTRL
  			(audio_type == AUTYPE_MEDIA && media_ctrl && media_ctrl->GetState() == wxMEDIASTATE_STOPPED) ||
 +#endif
  			(audio_type == AUTYPE_MIDI && theMIDIPlayer && !theMIDIPlayer->isPlaying()))
  	{
  		timer_seek->Stop();
-@@ -793,8 +811,10 @@ void AudioEntryPanel::onSliderSeekChange
- 		mod.setPlayingOffset(sf::milliseconds(slider_seek->GetValue())); break;
+@@ -809,8 +827,10 @@ void AudioEntryPanel::onSliderSeekChange
+ 		mod->setPlayingOffset(sf::milliseconds(slider_seek->GetValue())); break;
  	case AUTYPE_MIDI:
  		theMIDIPlayer->setPosition(slider_seek->GetValue()); break;
 +#if wxUSE_MEDIACTRL
@@ -119,8 +119,8 @@
  	}
  }
  
-@@ -813,8 +833,10 @@ void AudioEntryPanel::onSliderVolumeChan
- 		music.setVolume(snd_volume); break;
+@@ -829,8 +849,10 @@ void AudioEntryPanel::onSliderVolumeChan
+ 		music->setVolume(snd_volume); break;
  	case AUTYPE_MIDI:
  		theMIDIPlayer->setVolume(snd_volume); break;
 +#if wxUSE_MEDIACTRL
@@ -128,5 +128,5 @@
  		if (media_ctrl) media_ctrl->SetVolume(snd_volume*0.01); break;
 +#endif
  	case AUTYPE_MOD:
- 		mod.setVolume(snd_volume); break;
+ 		mod->setVolume(snd_volume); break;
  	}
