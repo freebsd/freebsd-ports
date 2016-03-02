313eb92ef6e9204c752d1df21000921bbc16667e
Steven Kreuzer <skreuzer@FreeBSD.org>
2016-03-02 14:14:51 +0000

    Update to 1.1.2

--- devel/py-pex/files/patch-pex_version.py
+++
@@ -1,9 +0,0 @@
---- pex/version.py.orig	2015-07-14 15:45:35 UTC
-+++ pex/version.py
-@@ -3,5 +3,5 @@
- 
- __version__ = '1.0.1'
- 
--SETUPTOOLS_REQUIREMENT = 'setuptools>=2.2,<16'
-+SETUPTOOLS_REQUIREMENT = 'setuptools>=2.2,<=17'
- WHEEL_REQUIREMENT = 'wheel>=0.24.0,<0.25.0'
