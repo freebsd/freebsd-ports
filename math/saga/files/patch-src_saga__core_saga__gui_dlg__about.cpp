89f04895084a0da7c83a1c134bb3219e2d47cd6c
wen <wen@FreeBSD.org>
2016-03-03 12:09:26 +0000

    - Update to 2.2.4
    PR:		207565
    Submitted by:	rhurlin@gwdg.de(maintainer)

--- math/saga/files/patch-src_saga__core_saga__gui_dlg__about.cpp
+++ math/saga/files/patch-src_saga__core_saga__gui_dlg__about.cpp
@@ -1,36 +1,23 @@
---- src/saga_core/saga_gui/dlg_about.cpp.orig	2015-12-21 09:38:29 UTC
+--- src/saga_core/saga_gui/dlg_about.cpp.orig	2016-02-28 15:31:41 UTC
 +++ src/saga_core/saga_gui/dlg_about.cpp
-@@ -65,6 +65,7 @@
- #include <saga_api/clipper.hpp>
- 
- #include <wx/notebook.h>
-+#include <wx/platform.h>
- 
- #include "helper.h"
- 
-@@ -188,7 +189,7 @@ wxString CDLG_About::_Get_Version(void)
- 	s	+= "System for Automated Geoscientific Analyses\n";
- 	s	+= "http://www.saga-gis.org\n";
- 	s	+= "\n";
--	s	+= "Version: " + wxString(SAGA_VERSION) + "\n";
-+	s	+= "Version: " + wxString(SAGA_VERSION) + " (SVN r2737)" + "\n";
- 
- #ifdef _SAGA_MSW
- 	#ifdef _WIN64
-@@ -264,7 +265,21 @@ wxString CDLG_About::_Get_Version(void)
+@@ -264,7 +264,25 @@ wxString CDLG_About::_Get_Version(void)
  		#elif	defined(__GNUWIN32__)
  			"Gnu-Win32 compiler"
  		#elif	defined(__GNUG__)
 -			"Gnu C++"
-+			#if   wxCHECK_GCC_VERSION(5,1)
++			#if   wxCHECK_GCC_VERSION(5,3)
++				"Gnu C++ 5.3"
++			#elif wxCHECK_GCC_VERSION(5,2)
++				"Gnu C++ 5.2"
++			#elif wxCHECK_GCC_VERSION(5,1)
 +				"Gnu C++ 5.1"
-+			#elif   wxCHECK_GCC_VERSION(5,0)
++			#elif wxCHECK_GCC_VERSION(5,0)
 +				"Gnu C++ 5.0"
 +			#elif wxCHECK_GCC_VERSION(4,9)
 +				"Gnu C++ 4.9"
 +			#elif wxCHECK_GCC_VERSION(4,8)
 +				"Gnu C++ 4.8"
-+		#elif wxCHECK_GCC_VERSION(4,7)
++			#elif wxCHECK_GCC_VERSION(4,7)
 +				"Gnu C++ 4.7"
 +			#elif wxCHECK_GCC_VERSION(4,6)
 +				"Gnu C++ 4.6"
