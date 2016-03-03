d720ac2b4782d568ad48c477c5b81518ce373b6c
koobs <koobs@FreeBSD.org>
2016-03-03 10:11:31 +0000

    finance/py-stripe: Update to 1.30.0
    - Update PORTVERSION and distinfo checksum (1.30.0)
    - Update mock in TEST_DEPENDS (now any version)
    - Update setup.py patch to add tests_require for future upstreaming
    
    Changes:
    
      https://github.com/stripe/stripe-python/blob/v1.30.0/CHANGELOG

--- finance/py-stripe/files/patch-setup.py
+++ finance/py-stripe/files/patch-setup.py
@@ -1,15 +1,24 @@
-# Use latest mock for tests
-# Fix two test failures with latest mock #205 
-# https://github.com/stripe/stripe-python/issues/205
-
---- setup.py.orig	2016-02-22 10:32:35 UTC
+--- setup.py.orig	2016-03-03 09:42:16 UTC
 +++ setup.py
-@@ -57,7 +57,7 @@ setup(
+@@ -28,6 +28,11 @@ if sys.version_info < (2, 6):
+ else:
+     install_requires.append('requests >= 0.8.8')
+ 
++tests_require = [
++    'unittest2',
++    'pycurl>=7.19',
++    'mock',
++]
+ 
+ with open('LONG_DESCRIPTION.rst') as f:
+     long_description = f.read()
+@@ -56,8 +61,8 @@ setup(
+     packages=['stripe', 'stripe.test', 'stripe.test.resources'],
      package_data={'stripe': ['data/ca-certificates.crt']},
      install_requires=install_requires,
++    tests_require=tests_require,
      test_suite='stripe.test.all',
--    tests_require=['unittest2', 'mock == 1.0.1'],
-+    tests_require=['unittest2', 'mock>=1.0.1'],
+-    tests_require=['unittest2', 'mock'],
      use_2to3=True,
      classifiers=[
          "Development Status :: 5 - Production/Stable",
