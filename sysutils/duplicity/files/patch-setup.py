984fba7fe0885dd07b580ffaa314e76efa45eee2
pi <pi@FreeBSD.org>
2016-03-06 09:51:53 +0000

    sysutils/duplicity: 0.6.25 -> 0.7.06
    Changes:
      http://www.nongnu.org/duplicity/CHANGELOG
    
    PR:		199892
    Submitted by:	Claudius Herder <claudius.herder@ambtec.de>
    Approved by:	jase (maintainer timeout)

--- sysutils/duplicity/files/patch-setup.py
+++ sysutils/duplicity/files/patch-setup.py
@@ -1,25 +1,24 @@
---- setup.py.orig	2014-05-18 14:37:42.190089675 +0100
-+++ setup.py	2014-05-18 14:41:15.084751008 +0100
-@@ -22,7 +22,6 @@
- 
- import sys, os
+--- setup.py.orig	2016-03-05 09:59:07.250354000 +0100
++++ setup.py	2016-03-05 10:02:48.940832000 +0100
+@@ -23,7 +23,6 @@
+ import sys
+ import os
  from setuptools import setup, Extension
 -from setuptools.command.test import test
  from setuptools.command.install import install
  from setuptools.command.sdist import sdist
- 
-@@ -34,6 +33,10 @@
+ from distutils.command.build_scripts import build_scripts
+@@ -35,6 +34,9 @@
+     sys.exit(1)
  
  incdir_list = libdir_list = None
- 
 +LOCALBASE = os.environ.get("LOCALBASE", "/usr/local")
 +incdir_list = ['%s/include' % LOCALBASE]
 +libdir_list = ['%s/lib/' % LOCALBASE]
-+
+ 
  if os.name == 'posix':
      LIBRSYNC_DIR = os.environ.get('LIBRSYNC_DIR', '')
-     args = sys.argv[:]
-@@ -45,17 +48,9 @@
+@@ -47,15 +49,9 @@
          incdir_list = [os.path.join(LIBRSYNC_DIR, 'include')]
          libdir_list = [os.path.join(LIBRSYNC_DIR, 'lib')]
  
@@ -32,17 +31,17 @@
 -                'README',
 -                'README-REPO',
 -                'README-LOG',
--                'tarfile-LICENSE',
--                'tarfile-CHANGES',
 -                'CHANGELOG']),
                ]
  
  top_dir = os.path.dirname(os.path.abspath(__file__))
-@@ -70,45 +65,8 @@
+@@ -69,48 +65,9 @@
+                 ('share/locale/%s/LC_MESSAGES' % lang,
                   ["po/%s/duplicity.mo" % lang]))
  
- 
+-
 -class TestCommand(test):
+-
 -    def run(self):
 -        # Make sure all modules are ready
 -        build_cmd = self.get_finalized_command("build_py")
@@ -53,7 +52,7 @@
 -
 -        # make symlinks for test data
 -        if build_cmd.build_lib != top_dir:
--            for path in ['testfiles.tar.gz', 'testtar.tar', 'gnupg']:
+-            for path in ['testfiles.tar.gz', 'gnupg']:
 -                src = os.path.join(top_dir, 'testing', path)
 -                target = os.path.join(build_cmd.build_lib, 'testing', path)
 -                try:
@@ -69,6 +68,7 @@
 -
 -
  class InstallCommand(install):
+ 
      def run(self):
 -        # Normally, install will call build().  But we want to delete the
 -        # testing dir between building and installing.  So we manually build
@@ -84,27 +84,28 @@
          install.run(self)
  
  
-@@ -133,11 +91,7 @@
-       maintainer_email="kenneth@loafman.com",
+@@ -178,11 +135,7 @@
        url="http://duplicity.nongnu.org/index.html",
-       packages = ['duplicity',
--                  'duplicity.backends',
--                  'testing',
--                  'testing.functional',
--                  'testing.overrides',
--                  'testing.unit'],
-+                  'duplicity.backends'],
-       package_dir = {"duplicity" : "duplicity",
-                      "duplicity.backends" : "duplicity/backends",},
-       ext_modules = [Extension("duplicity._librsync",
-@@ -147,9 +101,6 @@
-                                libraries=["rsync"])],
-       scripts = ['bin/rdiffdir', 'bin/duplicity'],
-       data_files = data_files,
--      tests_require = ['lockfile', 'mock'],
--      test_suite = 'testing',
+       packages=['duplicity',
+                 'duplicity.backends',
+-                'duplicity.backends.pyrax_identity',
+-                'testing',
+-                'testing.functional',
+-                'testing.overrides',
+-                'testing.unit'],
++                'duplicity.backends.pyrax_identity'],
+       package_dir={"duplicity": "duplicity",
+                    "duplicity.backends": "duplicity/backends", },
+       ext_modules=[Extension("duplicity._librsync",
+@@ -193,10 +146,7 @@
+       scripts=['bin/rdiffdir', 'bin/duplicity'],
+       data_files=data_files,
+       install_requires=['lockfile'],
+-      tests_require=['lockfile', 'mock', 'pexpect'],
+-      test_suite='testing',
 -      cmdclass={'test': TestCommand,
 -                'install': InstallCommand,
 +      cmdclass={'install': InstallCommand,
-                 'sdist': SDistCommand},
+                 'sdist': SDistCommand,
+                 'build_scripts': BSCommand},
        )
