a6848ff6a376e87f7e4ea0326bfabf82a5f480f3
Wen Heping <wen@FreeBSD.org>
2016-03-06 10:53:58 +0000

    - Update to 1.3.0
    PR:		205894
    Submitted by:	tkato432@yahoo.com

--- math/fityk/files/patch-fityk__eparser.cpp
+++ math/fityk/files/patch-fityk__eparser.cpp
@@ -1,29 +1,29 @@
---- ./src/eparser.cpp.orig	2011-06-03 19:15:42.000000000 +0200
-+++ ./src/eparser.cpp	2014-04-08 14:55:51.807605460 +0200
-@@ -139,7 +139,7 @@
+--- fityk/eparser.cpp.orig	2015-05-25 17:42:29 UTC
++++ fityk/eparser.cpp
+@@ -154,7 +154,7 @@ int get_function_narg(int op)
      }
  }
  
 -bool is_function(int op)
-+bool my_is_function(int op)
++bool my_is_funcion(int op)
  {
-     return (bool) get_function_narg(op);
+     return get_function_narg(op) != 0;
  }
-@@ -812,7 +812,7 @@
+@@ -878,7 +878,7 @@ void ExpressionParser::parse_expr(Lexer&
                  // check if this is closing bracket of func()
                  if (!opstack_.empty()) {
                      int top = opstack_.back();
 -                    if (is_function(top)) {
-+                    if (my_is_function(top)) {
++                    if (my_is_funcion(top)) {
                          pop_onto_que();
-                         int n = arg_cnt_.back() + 1;
-                         int expected_n = get_function_narg(top);
-@@ -846,7 +846,7 @@
+                         int n = opstack_.back() + 1;
+                         opstack_.pop_back();
+@@ -911,7 +911,7 @@ void ExpressionParser::parse_expr(Lexer&
                      lex.throw_syntax_error("unexpected ',' after '?'");
                  // if we are here, opstack_.back() == OP_OPEN_ROUND
-                 else if (opstack_.size() < 2 ||
+                 else if (opstack_.size() < 3 ||
 -                         !is_function(*(opstack_.end() - 2)))
-+                         !my_is_function(*(opstack_.end() - 2)))
++                         !my_is_funcion(*(opstack_.end() - 2)))
                      lex.throw_syntax_error("',' outside of function");
                  else
                      // don't pop OP_OPEN_ROUND from the stack
