01075c40c4a6b61bd88b51db3acdfc6947cce8a5
mat <mat@FreeBSD.org>
2016-03-07 17:48:08 +0000

    Fix build on 10.x+
    PR:		206896
    Submitted by:	tkato432 yahoo com
    Sponsored by:	Absolight

--- multimedia/bombono/files/patch-src_mlib_filesystem.cpp
+++ multimedia/bombono/files/patch-src_mlib_filesystem.cpp
@@ -1,6 +1,6 @@
---- ./src/mlib/filesystem.cpp.orig	2013-03-07 22:20:45.000000000 +0100
-+++ src/mlib/filesystem.cpp	2013-03-08 16:42:27.000000000 +0100
-@@ -105,7 +105,7 @@
+--- src/mlib/filesystem.cpp.orig	2013-03-07 21:20:45 UTC
++++ src/mlib/filesystem.cpp
+@@ -105,7 +105,7 @@ std::string get_extension(const fs::path
  
  std::string get_basename(const fs::path& pth)
  {
@@ -9,7 +9,7 @@
      const char* name = name_s.c_str();
  
      //if( const char* dot = strstr(name, ".") )
-@@ -116,7 +116,7 @@
+@@ -116,7 +116,7 @@ std::string get_basename(const fs::path&
  
  std::string get_extension(const fs::path& pth)
  {
--- multimedia/bombono/files/patch-src_mlib_stream.h
+++ multimedia/bombono/files/patch-src_mlib_stream.h
@@ -1,6 +1,6 @@
---- src/mlib/stream.h.orig	2009-08-24 15:11:47.000000000 +0700
-+++ src/mlib/stream.h	2009-08-24 15:12:03.000000000 +0700
-@@ -186,6 +186,7 @@
+--- src/mlib/stream.h.orig	2013-03-07 21:20:45 UTC
++++ src/mlib/stream.h
+@@ -186,6 +186,7 @@ class basic_fstream : public basic_iostr
                          //   write(fd, "some text", 9);
                          //   ..
                          // Причины использования класса io::fd_proxy см. в его описании
