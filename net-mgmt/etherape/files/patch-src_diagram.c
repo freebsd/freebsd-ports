209c19ed3e4ea73aa7e01dd236f40852697c0347
Mathieu Arnold <mat@FreeBSD.org>
2016-03-07 17:47:40 +0000

    Update to 0.9.14
    PR:		207164
    Submitted by:	tkato432 yahoo com
    Sponsored by:	Absolight

--- net-mgmt/etherape/files/patch-src_diagram.c
+++ net-mgmt/etherape/files/patch-src_diagram.c
@@ -1,5 +1,5 @@
---- src/names.c.orig
-+++ src/names.c
+--- src/diagram.c.orig	2016-01-25 20:19:16 UTC
++++ src/diagram.c
 @@ -21,7 +21,18 @@
  #include <config.h>
  #endif
@@ -11,11 +11,11 @@
 +#include <sys/socket.h>
 +#endif
 +#ifdef HAVE_NETINET_IN_H
- #include <netinet/in.h>
++#include <netinet/in.h>
 +#endif
 +#ifdef HAVE_ARPA_INET_H
-+#include <arpa/inet.h>
+ #include <arpa/inet.h>
 +#endif
- #ifdef HAVE_UNISTD_H
- #include <unistd.h>
- #endif
+ 
+ #include <gnome.h>
+ #include <regex.h>
