72704a08a799038d894737574fd66e2f41dbc10c
Kurt Jaeger <pi@FreeBSD.org>
2016-03-08 06:53:10 +0000

    sysutils/mcelog: 1.0pre3 -> 133
    - Added version prefix
    - Switched to github over master sites
    - Removed patch_strip
    - Changed hard LDFLAGS to include
    - Fixed version in source
    - Updated distfile
    - Regenerated patchfiles for portlint
    - Modified several patchfiles, adapted to new version
    - submitter becomes maintainer
    
    PR:		207145
    Submitted by:	Ultima1252@gmail.com
    Approved by:	Jeremy Chadwick <jdc@koitsu.org> (maintainer)

--- sysutils/mcelog/files/patch-mcelog.h
+++ sysutils/mcelog/files/patch-mcelog.h
@@ -1,19 +1,20 @@
---- ./mcelog.h.orig	2009-12-15 07:18:40.000000000 -0500
-+++ ./mcelog.h	2011-10-14 22:37:06.000000000 -0400
-@@ -64,9 +64,11 @@
- #define MCI_STATUS_ADDRV (1ULL<<58)  /* addr reg. valid */
- #define MCI_STATUS_PCC   (1ULL<<57)  /* processor context corrupt */
+--- mcelog.h.orig	2016-02-10 18:38:43 UTC
++++ mcelog.h
+@@ -67,10 +67,12 @@ struct mce {
+ #define MCI_STATUS_AR	 (1ULL<<55)  /* action-required */
+ #define MCI_STATUS_FWST  (1ULL<<37)  /* Firmware updated status indicator */
  
 +#ifndef MCG_STATUS_RIPV
  #define MCG_STATUS_RIPV  (1ULL<<0)   /* restart ip valid */
  #define MCG_STATUS_EIPV  (1ULL<<1)   /* eip points to correct instruction */
  #define MCG_STATUS_MCIP  (1ULL<<2)   /* machine check in progress */
+ #define MCG_STATUS_LMCES (1ULL<<3)   /* local machine check signaled */
 +#endif
  
  #define MCG_CMCI_P		(1ULL<<10)   /* CMCI supported */
  #define MCG_TES_P		(1ULL<<11)   /* Yellow bit cache threshold supported */
-@@ -89,6 +91,10 @@
- #define PRINTFLIKE 
+@@ -97,6 +99,10 @@ struct mce {
+ #define noreturn
  #endif
  
 +#if defined(__FreeBSD__) && defined(_STDIO_H_)
