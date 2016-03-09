50bf2236e4cca0185ed773c834cfc8890c1efbd3
pi <pi@FreeBSD.org>
2016-03-09 16:49:38 +0000

    math/ogdf: 2012.07 -> 2015.05
    Changes:
      http://www.ogdf.net/doku.php/tech:versions#v_201505_baobab
    
    - Remove dependencies on math/abacus and math/coinmp, as they are
      now bundled into the OGDF source tarballs and compiled from there
      (COIN remains optional)
    - Point MASTER_SITES to the official OGDF website
    - Utilise USES=compiler:features to detect C++11 support, and
      add compiler flags -std=c++11 if so (required for build to complete
      without errors)
    - Remove custom do-install recipe, and instead use default
      make install target
    - Take maintainership
    
    PR:		207531
    Submitted by:	Ben Woods <woodsb02@gmail.com>

--- math/ogdf/files/patch-src_ogdf_basic_System.cpp
+++ math/ogdf/files/patch-src_ogdf_basic_System.cpp
@@ -1,6 +1,6 @@
---- src/basic/System.cpp.orig	2013-06-10 17:33:24.000000000 +0200
-+++ src/basic/System.cpp	2013-06-10 17:47:24.000000000 +0200
-@@ -51,6 +51,13 @@
+--- src/ogdf/basic/System.cpp.orig	2015-05-29 15:36:49 UTC
++++ src/ogdf/basic/System.cpp
+@@ -52,6 +52,13 @@
  #include <mach/vm_statistics.h>
  #include <mach/mach.h>
  #include <mach/machine.h>
@@ -14,7 +14,7 @@
  #elif defined(OGDF_SYSTEM_UNIX)
  #include <malloc.h>
  #endif
-@@ -310,6 +317,40 @@
+@@ -308,6 +315,40 @@ size_t System::memoryUsedByProcess()
  	return 0;
  }
  
@@ -22,21 +22,21 @@
 +
 +long long System::physicalMemory()
 +{
-+	unsigned long value;
-+	size_t  size = sizeof( value );
-+	if (sysctlbyname("hw.physmem", (void *)&value, &size, NULL, 0) != -1)
-+		return value;
-+	else 
-+		return 0;
++       unsigned long value;
++       size_t  size = sizeof( value );
++       if (sysctlbyname("hw.physmem", (void *)&value, &size, NULL, 0) != -1)
++               return value;
++       else
++               return 0;
 +}
 +
 +long long System::availablePhysicalMemory()
 +{
-+	int pageSize = getpagesize ();
-+	unsigned long result;
++       int pageSize = getpagesize ();
++       unsigned long result;
 +    size_t size = sizeof (result);
 +
-+	if (sysctlbyname("vm.stats.vm.v_free_count", (void *)&result, &size, NULL, 0) != -1)
++       if (sysctlbyname("vm.stats.vm.v_free_count", (void *)&result, &size, NULL, 0) != -1)
 +        return result * pageSize;
 +    else
 +        return 0;
@@ -55,7 +55,7 @@
  #else
  // LINUX, NOT MAC OS
  long long System::physicalMemory()
-@@ -396,6 +437,19 @@
+@@ -389,6 +430,19 @@ size_t System::memoryInFreelistOfMalloc(
  {
  	return mstats().chunks_free;
  }
@@ -64,12 +64,12 @@
 +
 +size_t System::memoryAllocatedByMalloc()
 +{
-+	return 0;
++       return 0;
 +}
 +
 +size_t System::memoryInFreelistOfMalloc()
 +{
-+	return 0;
++       return 0;
 +}
 +
  #else
