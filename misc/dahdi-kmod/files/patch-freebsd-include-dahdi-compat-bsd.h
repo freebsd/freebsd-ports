cecc70246a7aab25ee64a7826d7525d243b00bd7
fjoe <fjoe@FreeBSD.org>
2016-03-11 07:19:18 +0000

    Unbreak on -CURRENT (taskqueue_enqueue_fast(9) removed).

--- misc/dahdi-kmod/files/patch-freebsd-include-dahdi-compat-bsd.h
+++ misc/dahdi-kmod/files/patch-freebsd-include-dahdi-compat-bsd.h
@@ -1,6 +1,17 @@
---- freebsd/include/dahdi/compat/bsd.h.orig	2015-03-22 00:41:01.923397000 +0600
-+++ freebsd/include/dahdi/compat/bsd.h	2015-03-22 00:51:34.101470000 +0600
-@@ -66,9 +66,11 @@
+--- freebsd/include/dahdi/compat/bsd.h.orig	2016-03-11 06:59:31.516533000 +0000
++++ freebsd/include/dahdi/compat/bsd.h	2016-03-11 07:05:34.327218000 +0000
+@@ -45,6 +45,10 @@
+ #include <machine/atomic.h>
+ #include <machine/bus.h>
+ 
++#if __FreeBSD_version >= 1100101
++#define taskqueue_enqueue_fast(queue, task)	taskqueue_enqueue(queue, task)
++#endif
++
+ #define D_VERSION_LINEAR	(((D_VERSION & 0xffff) << 16) | (((D_VERSION >> 16) & 0xff) << 8) | ((D_VERSION >> 24) & 0xff))
+ 
+ #define LINUX_VERSION_CODE	KERNEL_VERSION(2, 6, 32)
+@@ -66,9 +70,11 @@
  #define __be32  u_int32_t
  
  #if _BYTE_ORDER == _LITTLE_ENDIAN
@@ -12,7 +23,7 @@
  #define __BIG_ENDIAN_BITFIELD
  #define __constant_htons(x)	(x)
  #endif
-@@ -454,7 +456,7 @@
+@@ -454,7 +460,7 @@
  #define _module_ptr_args	__CONCAT(_module_ptr_args_, __LINE__)
  #define _module_ptr_init(field, val)					\
  	static struct module_ptr_args _module_ptr_args = {		\
