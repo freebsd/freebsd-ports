b81c401316716347af3769b00043ae5e78d9bcd8
amdmi3 <amdmi3@FreeBSD.org>
2016-03-12 14:16:27 +0000

    - Clarify LICENSE - Don't install unneeded docfile - Silence stripping - Switch to options helpers - Regenerate patches with `make makepatch`

--- sysutils/installwatch/files/patch-installwatch.c
+++ sysutils/installwatch/files/patch-installwatch.c
@@ -1,6 +1,6 @@
---- installwatch.c.orig
+--- installwatch.c.orig	2001-12-22 23:55:11 UTC
 +++ installwatch.c
-@@ -574,7 +574,7 @@
+@@ -574,7 +574,7 @@ int open(const char *pathname, int flags
  
  	REFCOUNT;
  	va_start(ap, flags);
