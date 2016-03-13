3fd14d5a2cd99b0bdfa38337818b8be4dacc2d80
amdmi3 <amdmi3@FreeBSD.org>
2016-03-13 02:19:18 +0000

    - Fix license. COPYING explicitly states that APACHE20 does not apply, the license is actually commercial:
    Commercial software. Please write info@synergetics.be for licensing terms.
    
    LICENSE-2.0.txt does not apply. It is only provided for information,
    e.g. on some related software packages.
    
    - Switch to options helpers
    - Regenerate patches with `make makepatch`

--- security/zxid/files/patch-platform.h
+++ security/zxid/files/patch-platform.h
@@ -1,6 +1,6 @@
---- platform.h.orig	2013-11-27 10:56:48.000000000 +0900
-+++ platform.h	2014-07-28 07:51:37.000000000 +0900
-@@ -130,6 +130,7 @@
+--- platform.h.orig	2016-02-29 00:16:50 UTC
++++ platform.h
+@@ -132,6 +132,7 @@ unsigned int alarm(unsigned int secs);
   * NOT MINGW nor WIN32CL (i.e. its Unix) */
  
  #include <dirent.h>
