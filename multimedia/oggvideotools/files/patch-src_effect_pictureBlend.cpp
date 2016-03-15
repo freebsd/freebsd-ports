25684b59201e639e0ce0741da0cdceac0d31a0d7
mat <mat@FreeBSD.org>
2016-03-15 15:29:07 +0000

    Update to 0.9.
    PR:		207990
    Submitted by:	tkato432 yahoo com
    Sponsored by:	Absolight

--- multimedia/oggvideotools/files/patch-src_effect_pictureBlend.cpp
+++ multimedia/oggvideotools/files/patch-src_effect_pictureBlend.cpp
@@ -1,5 +1,5 @@
---- src/pictureBlend.cpp.orig
-+++ src/pictureBlend.cpp
+--- src/effect/pictureBlend.cpp.orig	2015-10-05 06:33:52 UTC
++++ src/effect/pictureBlend.cpp
 @@ -9,6 +9,8 @@
  // Copyright: See COPYING file that comes with this distribution
  //
