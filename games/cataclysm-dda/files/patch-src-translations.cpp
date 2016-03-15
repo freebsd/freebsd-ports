3af78e7414210b883e49abfba50fc3f1a3862e21
Mathieu Arnold <mat@FreeBSD.org>
2016-03-15 15:28:56 +0000

    Update to 0.C.
    PR:		207988
    Submitted by:	tkato432 yahoo com
    Sponsored by:	Absolight

--- games/cataclysm-dda/files/patch-src-translations.cpp
+++ games/cataclysm-dda/files/patch-src-translations.cpp
@@ -1,13 +1,6 @@
---- src/translations.cpp.orig	2014-11-17 06:09:38.000000000 +0300
-+++ src/translations.cpp	2014-11-30 13:22:06.603147068 +0300
-@@ -1,5 +1,6 @@
- #include "translations.h"
- 
-+#include <sys/param.h>
- #include <string>
- #ifdef LOCALIZE
- #undef __STRICT_ANSI__ // _putenv in minGW need that
-@@ -72,7 +73,7 @@ void set_language(bool reload_options)
+--- src/translations.cpp.orig	2015-03-09 05:25:34 UTC
++++ src/translations.cpp
+@@ -72,7 +72,7 @@ void set_language(bool reload_options)
  
      // Step 2. Bind to gettext domain.
      const char *locale_dir;
