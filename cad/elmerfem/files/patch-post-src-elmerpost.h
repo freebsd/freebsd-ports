f1089e0ea7504fe0c864fd4ed7090d23925fa5d9
Thierry Thomas <thierry@FreeBSD.org>
2016-03-17 18:24:38 +0000

    Update to version 8.1-20160311. Fix PR ports/201246.
    PR:		ports/196577
    Submitted by:	pfg

--- cad/elmerfem/files/patch-post-src-elmerpost.h
+++ cad/elmerfem/files/patch-post-src-elmerpost.h
@@ -1,5 +1,5 @@
---- post/src/elmerpost.h.orig	2014-09-09 15:50:25.000000000 -0500
-+++ post/src/elmerpost.h	2014-09-09 16:25:51.000000000 -0500
+--- post/src/elmerpost.h.orig	2015-08-18 12:26:58 UTC
++++ post/src/elmerpost.h
 @@ -62,6 +62,7 @@
  
  #include <elmer/matc.h>
