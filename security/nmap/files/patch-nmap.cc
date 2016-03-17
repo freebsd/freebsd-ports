91b7b2f79dadfdb45fd061b295f65d7bd7921943
ohauer <ohauer@FreeBSD.org>
2016-03-17 21:29:58 +0000

    - upate to version 7.10 - remove support for custom IPv4 only kernel [1]
    Short summary:
    - 12 new NSE scripts
    - hundreds of new OS/version fingerprints
    - dozens if smaller improvements and bug fixes
    
    Full Changelog:
     https://nmap.org/changelog.html
    
    [1] nmap does no longer build agains custom kernel without IPv6!

--- security/nmap/files/patch-nmap.cc
+++ security/nmap/files/patch-nmap.cc
@@ -1,8 +1,8 @@
---- nmap.cc.orig	2015-09-21 13:49:51 UTC
+--- nmap.cc.orig	2016-03-14 23:54:51 UTC
 +++ nmap.cc
 @@ -123,6 +123,7 @@
  
- /* $Id: nmap.cc 35268 2015-09-21 13:49:51Z dmiller $ */
+ /* $Id: nmap.cc 35683 2016-03-14 23:54:51Z dmiller $ */
  
 +#include <getopt.h>
  #include "nmap.h"
