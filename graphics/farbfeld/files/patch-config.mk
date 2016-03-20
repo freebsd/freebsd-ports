f61ef365d1895e68e788eb2eabeb9e00e1f66587
riggs <riggs@FreeBSD.org>
2016-03-20 12:21:11 +0000

    Update to upstream version 2 (no minor or patchlevel, just version 2)
    PR:		208037
    Submitted by:	aaron@baugher.biz

--- graphics/farbfeld/files/patch-config.mk
+++ graphics/farbfeld/files/patch-config.mk
@@ -1,11 +1,11 @@
---- config.mk.orig	2016-02-19 12:59:50.769556000 -0600
-+++ config.mk	2016-02-19 12:56:15.846580000 -0600
-@@ -18,8 +18,8 @@
+--- config.mk.orig	2016-03-15 17:46:09 UTC
++++ config.mk
+@@ -18,8 +18,8 @@ LIBS = -L${PNGLIB} -L${JPGLIB} -lpng -lj
  
  # flags
  CPPFLAGS = -D_DEFAULT_SOURCE
 -CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-+CFLAGS  += -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
++CFLAGS   += -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
  LDFLAGS  = -s ${LIBS}
  
  # compiler and linker
