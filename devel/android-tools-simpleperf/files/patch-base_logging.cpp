83b4253962b7517feb295171e56e43a0cc68791e
jbeich <jbeich@FreeBSD.org>
2016-03-21 18:45:59 +0000

    devel/android-tools-simpleperf: update to n.p.1.116
    Changes:	https://android.googlesource.com/platform/system/extras/+log/4ea2a41..ea4a5a4/simpleperf

--- devel/android-tools-simpleperf/files/patch-base_logging.cpp
+++ devel/android-tools-simpleperf/files/patch-base_logging.cpp
@@ -16,3 +16,33 @@
  #include <iostream>
  #include <limits>
  #include <sstream>
+@@ -66,6 +68,14 @@
+ #include <unistd.h>
+ #elif defined(_WIN32)
+ #include <windows.h>
++#elif defined(__DragonFly__)
++#include <unistd.h>
++#elif defined(__FreeBSD__)
++#include <pthread_np.h>
++#elif defined(__NetBSD__)
++#include <lwp.h>
++#else
++#include <stdint.h>
+ #endif
+ 
+ static pid_t GetThreadId() {
+@@ -77,6 +89,14 @@ static pid_t GetThreadId() {
+   return syscall(__NR_gettid);
+ #elif defined(_WIN32)
+   return GetCurrentThreadId();
++#elif defined(__DragonFly__)
++  return lwp_gettid();
++#elif defined(__NetBSD__)
++  return _lwp_self();
++#elif defined(__FreeBSD__)
++  return pthread_getthreadid_np();
++#else
++  return (intptr_t) pthread_self();
+ #endif
+ }
+ 
