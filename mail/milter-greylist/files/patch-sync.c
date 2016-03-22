9425fcf818a73ed8661545515a02d29b2d49b804
feld <feld@FreeBSD.org>
2016-03-22 13:07:56 +0000

    mail/milter-greylist: Fix options
    - Remove DKIM option as it reportedly never worked
    - Regenerate patches to appease portlint
    
    PR:		207246
    MFH:		2016Q1

--- mail/milter-greylist/files/patch-sync.c
+++ mail/milter-greylist/files/patch-sync.c
@@ -1,8 +1,7 @@
 Index: sync.c
-diff -u -p sync.c.orig sync.c
---- sync.c.orig	2010-04-17 12:19:02.000000000 +0900
-+++ sync.c	2013-03-14 14:57:39.284674283 +0900
-@@ -478,7 +478,11 @@ peer_connect(peer)	/* peer list is read-
+--- sync.c.orig	2010-06-16 01:30:30 UTC
++++ sync.c
+@@ -487,7 +487,11 @@ peer_connect(peer)	/* peer list is read-
  #ifdef AF_INET6
  		case AF_INET6:
  			SA6(res->ai_addr)->sin6_port = service;
