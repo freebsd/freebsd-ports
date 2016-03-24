fbdf2ff41afd1ecddec21bd356d2388ff8019a6f
marino <marino@FreeBSD.org>
2016-03-24 08:29:44 +0000

    lang/pypy: Restore build on DF (requires same ucontext fix as FBSD)
    Approved by:	Restore DF functionality blanket

--- lang/pypy/files/patch-rpython_rlib_rvmprof_src_vmprof__config.h
+++ lang/pypy/files/patch-rpython_rlib_rvmprof_src_vmprof__config.h
@@ -3,7 +3,7 @@
 @@ -1,5 +1,11 @@
  #define HAVE_SYS_UCONTEXT_H
 -#if defined(__FreeBSD__) || defined(__APPLE__)
-+#if defined(__FreeBSD__)
++#if defined(__FreeBSD__) || defined(__DragonFly__)
 +#if defined(__i386__)
 +#define PC_FROM_UCONTEXT uc_mcontext.mc_eip
 +#else
