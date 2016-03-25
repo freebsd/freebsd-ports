e448e549c77e590c3545476fce8f2ab8707e3039
riggs <riggs@FreeBSD.org>
2016-03-25 07:42:23 +0000

    Update to upstream version 0.7.0.20160321
    While on it:
    - Pet portlint
    
    PR:		208230
    Submitted by:	fidaj@ukr.net (maintainer)

---
+++ net-im/uTox/files/patch-src_ui.c
@@ -0,0 +1,21 @@
+--- src/ui.c.orig	2016-03-21 02:14:57 UTC
++++ src/ui.c
+@@ -275,16 +275,9 @@ static void draw_settings_header(int UNU
+     setfont(FONT_SELF_NAME);
+     drawstr(MAIN_LEFT + SCALE(10), SCALE(10), UTOX_SETTINGS);
+     #ifdef GIT_VERSION
+-        int x = MAIN_LEFT + SCALE(10) + UTOX_STR_WIDTH(UTOX_SETTINGS) + SCALE(10);
++        int x = MAIN_LEFT + UTOX_SCALE(5 ) + UTOX_STR_WIDTH(UTOX_SETTINGS) + UTOX_SCALE(5 );
+         setfont(FONT_TEXT);
+-        drawtext(x, SCALE(10), (uint8_t*)GIT_VERSION, strlen(GIT_VERSION));
+-        char version_string[64];
+-        int count;
+-        count = snprintf(version_string, 64, "Core v%u.%u.%u ToxAV v%u.%u.%u ToxES v%u.%u.%u",
+-                                    tox_version_major(),   tox_version_minor(),   tox_version_patch(),
+-                                    toxav_version_major(), toxav_version_minor(), toxav_version_patch(),
+-                                    toxes_version_major(), toxes_version_minor(), toxes_version_patch());
+-        drawtextwidth_right(w, textwidth((char_t*)version_string, count), SCALE(10), (uint8_t*)version_string, strlen(version_string));
++        drawtext(x, UTOX_SCALE(5), (uint8_t*)GIT_VERSION, strlen(GIT_VERSION));
+     #endif
+ }
+ 
