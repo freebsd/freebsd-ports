0e5a8819414b3ee340b4c5f569191731039d0c95
Jason Helfman <jgh@FreeBSD.org>
2016-03-25 15:15:52 +0000

    - update to 9.3.8 - address pkg listing to simplify future updates
    PR:		208149
    Reviewed by:	xmj@
    Submitted by:	dharrigan@gmail.com (maintainer)

--- www/jetty9/files/patch-bin_jetty.sh
+++ www/jetty9/files/patch-bin_jetty.sh
@@ -1,11 +1,11 @@
---- bin/jetty.sh.orig	2015-12-06 15:19:52 UTC
+--- bin/jetty.sh.orig	2016-03-19 21:43:53 UTC
 +++ bin/jetty.sh
-@@ -166,7 +166,7 @@ then 
+@@ -166,7 +166,7 @@ then
    ETC=$HOME/etc
  fi
  
 -for CONFIG in {/etc,~/etc}/default/${NAME}{,9} $HOME/.${NAME}rc; do
 +for CONFIG in {/etc,~/etc}/default/${NAME}{,9} $HOME/.${NAME}rc /usr/local/etc/${NAME}/${NAME}{,9}; do
-   if [ -f "$CONFIG" ] ; then 
+   if [ -f "$CONFIG" ] ; then
      readConfig "$CONFIG"
    fi
