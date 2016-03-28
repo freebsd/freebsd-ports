7c211f8057a42c686263921ff99536c94d135474
amdmi3 <amdmi3@FreeBSD.org>
2016-03-28 23:04:06 +0000

    - Add LICENSE_FILE - Switch to options helpers - Regenerate patches with `make makepatch`

--- math/xspread/files/patch-interp.c
+++ math/xspread/files/patch-interp.c
@@ -1,6 +1,6 @@
---- interp.c.orig	Tue Nov  7 03:17:26 1995
-+++ interp.c	Fri Mar 10 13:34:37 2000
-@@ -516,7 +516,7 @@
+--- interp.c.orig	1995-11-07 02:17:26 UTC
++++ interp.c
+@@ -516,7 +516,7 @@ int mo, day, yr;
      mdays[1] = 28 + (yr%4 == 0);
  
      if (mo < 1 || mo > 12 || day < 1 || day > mdays[--mo] ||
