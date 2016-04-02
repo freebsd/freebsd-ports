da27812132b4021fe635b06594759fd7e27a8542
pi <pi@FreeBSD.org>
2016-04-02 05:39:14 +0000

    sysutils/showbeastie: 0.1 -> 0.2, build-fixes
    PR:		208283
    Submitted by:	xmj, Tobias Kortkamp <t@tobik.me> (maintainer)

--- sysutils/showbeastie/files/patch-testmain.c
+++ sysutils/showbeastie/files/patch-testmain.c
@@ -1,6 +1,14 @@
---- testmain.c.orig	2016-03-02 15:04:22 UTC
+--- testmain.c.orig	2015-10-11 17:48:47 UTC
 +++ testmain.c
-@@ -316,17 +316,38 @@ int main(int argc, char **argv)
+@@ -296,6 +296,7 @@ void buildTestInterface(FICL_SYSTEM *pSy
+     ficlBuild(pSys, "cd",       ficlChDir,    FW_DEFAULT);
+     ficlBuild(pSys, "execxt",   execxt,       FW_DEFAULT);
+     ficlBuild(pSys, "load",     ficlLoad,     FW_DEFAULT);
++    ficlBuild(pSys, "include",  ficlLoad,     FW_DEFAULT);
+     ficlBuild(pSys, "pwd",      ficlGetCWD,   FW_DEFAULT);
+     ficlBuild(pSys, "system",   ficlSystem,   FW_DEFAULT);
+     ficlBuild(pSys, "spewhash", spewHash,     FW_DEFAULT);
+@@ -316,17 +317,58 @@ int main(int argc, char **argv)
      buildTestInterface(pSys);
      pVM = ficlNewVM(pSys);
  
@@ -9,15 +17,27 @@
 +    ficlEvaluate(pVM, "vocabulary support-functions");
 +    ficlEvaluate(pVM, ": contains? ( -- ) FALSE ;");
 +    ficlEvaluate(pVM, ": any_conf_read? ( -- ) FALSE ;");
++    ficlEvaluate(pVM, ": boot_serial? ( -- ) TRUE ;");
 +    ficlEvaluate(pVM, ": load_kernel ( -- ) ;");
 +    ficlEvaluate(pVM, ": load_modules ( -- ) ;");
 +    ficlEvaluate(pVM, ": delay_execute ( -- ) ;");
++    ficlEvaluate(pVM, ": boot ( -- ) ;");
++    ficlEvaluate(pVM, ": reboot ( -- ) ;");
++    ficlEvaluate(pVM, ": load_xen_throw ( -- ) ;");
 +    ficlEvaluate(pVM, "variable logoX");
 +    ficlEvaluate(pVM, "variable logoY");
++    ficlEvaluate(pVM, "variable brandX");
++    ficlEvaluate(pVM, "variable brandY");
 +
 +    sprintf(in, "load %s", "/boot/screen.4th");
 +    ficlEvaluate(pVM, in);
 +    sprintf(in, "load %s", "/boot/beastie.4th");
++    ficlEvaluate(pVM, in);
++    sprintf(in, "load %s", "/boot/brand.4th");
++    ficlEvaluate(pVM, in);
++    sprintf(in, "load %s", "/boot/frames.4th");
++    ficlEvaluate(pVM, in);
++    sprintf(in, "load %s", "/boot/menu.4th");
 +    ficlEvaluate(pVM, in);
  
      /*
@@ -26,15 +46,23 @@
      if (argc  > 1)
      {
 -        sprintf(in, ".( loading %s ) cr load %s\n cr", argv[1], argv[1]);
-+        sprintf(in, "load %s", argv[1], argv[1]);
++        char *brand = "/boot/brand-fbsd.4th";
++        sprintf(in, "load %s", argv[1]);
++        ficlEvaluate(pVM, in);        
++        if (argc > 2) {
++            brand = argv[2];
++        }
++        sprintf(in, "load %s", brand);
          ficlEvaluate(pVM, in);
 +    } else {
-+        fprintf(stderr, "usage: %s <logo.4th>\n", argv[0]);
++        fprintf(stderr, "usage: %s <logo.4th> [<brand.4th>]\n", argv[0]);
 +        return 1;
      }
  
 +    system("clear");
 +    ficlEvaluate(pVM, "draw-beastie");
++    ficlEvaluate(pVM, "draw-brand");
++    ficlEvaluate(pVM, "menu-init");
 +    printf("\n\n");
 +    return 0;
 +
