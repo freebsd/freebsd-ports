94d47c8e2b4205b2c9c03908598f7311bf69fa07
amdmi3 <amdmi3@FreeBSD.org>
2016-04-03 00:27:19 +0000

    - Add LICENSE_FILE - Switch to USES=tar:tgz - Switch to options helpers - Regenerate patches with `make makepatch`

--- audio/sbagen/files/patch-sbagen.c
+++ audio/sbagen/files/patch-sbagen.c
@@ -1,5 +1,5 @@
---- sbagen.c.orig	2013-03-18 12:02:21.000000000 +0100
-+++ sbagen.c	2013-03-18 12:02:43.000000000 +0100
+--- sbagen.c.orig	2011-08-08 00:12:21 UTC
++++ sbagen.c
 @@ -141,7 +141,7 @@
  #endif
  
@@ -9,7 +9,7 @@
   //WAS: #include <sys/soundcard.h>
  #endif
  #ifdef WIN_AUDIO
-@@ -181,7 +181,7 @@
+@@ -181,7 +181,7 @@ void debug(char *fmt, ...) ;
  void warn(char *fmt, ...) ;
  void * Alloc(size_t len) ;
  char * StrDup(char *str) ;
@@ -18,7 +18,7 @@
  void loop() ;
  void outChunk() ;
  void corrVal(int ) ;
-@@ -1207,7 +1207,7 @@
+@@ -1207,7 +1207,7 @@ setupRefTime() {
    time_ref_ms= 1000*tt->tm_sec + 60000*tt->tm_min + 3600000*tt->tm_hour;
  }  
  
