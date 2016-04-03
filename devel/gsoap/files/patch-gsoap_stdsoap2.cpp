44a7f6beb77a0d419d9ed9fafd28777a4611ba67
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2016-04-03 17:22:49 +0000

    devel/gsoap: Update version 2.8.29=>2.8.30

--- devel/gsoap/files/patch-gsoap_stdsoap2.cpp
+++ devel/gsoap/files/patch-gsoap_stdsoap2.cpp
@@ -1,13 +1,12 @@
---- gsoap/stdsoap2.cpp.orig	2016-01-01 16:24:55 UTC
+--- gsoap/stdsoap2.cpp.orig	2016-04-03 01:33:31 UTC
 +++ gsoap/stdsoap2.cpp
-@@ -5194,6 +5194,10 @@ soap_bind(struct soap *soap, const char 
+@@ -5396,6 +5396,9 @@ soap_bind(struct soap *soap, const char 
    }
  #endif
  #ifdef TCP_FASTOPEN
-+  	#ifndef SOL_TCP
-+  	#define SOL_TCP IPPROTO_TCP
-+  	#endif
-+
-   if (!(soap->omode & SOAP_IO_UDP) && setsockopt(soap->master, SOL_TCP, TCP_FASTOPEN, (char*)&set, sizeof(int)))
-   { soap->errnum = soap_socket_errno(soap->master);
-     soap_set_receiver_error(soap, tcp_error(soap), "setsockopt TCP_FASTOPEN failed in soap_bind()", SOAP_TCP_ERROR);
++       #ifndef SOL_TCP
++       #define SOL_TCP IPPROTO_TCP
++       #endif
+   if (!(soap->omode & SOAP_IO_UDP) && setsockopt(soap->master, IPPROTO_TCP, TCP_FASTOPEN, (char*)&set, sizeof(int)))
+   { /* silently ignore */
+     DBGLOG(TEST, SOAP_MESSAGE(fdebug, "setsockopt TCP_FASTOPEN failed in soap_bind()\n"));
