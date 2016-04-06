e96b1e467075932d3391c19af2da3adafa5b1359
Vanilla I. Shu <vanilla@FreeBSD.org>
2016-04-06 01:58:01 +0000

    Update to 1.2.0.

--- www/elixir-joken/files/patch-mix.exs
+++ www/elixir-joken/files/patch-mix.exs
@@ -1,10 +1,10 @@
---- mix.exs.orig	2016-02-01 23:55:46 UTC
+--- mix.exs.orig	2016-03-28 15:07:12 UTC
 +++ mix.exs
-@@ -7,7 +7,6 @@ defmodule Joken.Mixfile do
+@@ -9,7 +9,6 @@ defmodule Joken.Mixfile do
       elixir: "~> 1.0",
       description: description,
       package: package,
 -     deps: deps,
-      consolidate_protocols: Mix.env != :test]
-   end
- 
+      consolidate_protocols: Mix.env != :test,
+      test_coverage: [tool: ExCoveralls],
+      name: "Joken",
