933cee43fc23e3ffaeacbe8d01e7d5dd9cb34f6e
mat <mat@FreeBSD.org>
2016-04-08 12:37:48 +0000

    Update to 2.6.8.
    PR:		208612
    Submitted by:	tkato432 yahoo com
    Sponsored by:	Absolight

--- emulators/bochs/files/patch-iodev_network_slirp_compat.cc
+++ emulators/bochs/files/patch-iodev_network_slirp_compat.cc
@@ -1,6 +1,6 @@
---- iodev/network/slirp/compat.cc.orig
+--- iodev/network/slirp/compat.cc.orig	2015-05-01 19:41:08 UTC
 +++ iodev/network/slirp/compat.cc
-@@ -127,6 +127,8 @@
+@@ -128,6 +128,8 @@ int qemu_socket(int domain, int type, in
  #define CONFIG_SMBD_COMMAND "/usr/sbin/smbd"
  
  #include <pwd.h>
