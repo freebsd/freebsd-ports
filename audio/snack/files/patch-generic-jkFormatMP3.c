604574b2b60f8313ed31f91cf841404697c4b448
amdmi3 <amdmi3@FreeBSD.org>
2016-04-08 20:36:46 +0000

    - Add LICENSE - Switch to options helpers - Regenerate patches with `make makepatch`

--- audio/snack/files/patch-generic-jkFormatMP3.c
+++ audio/snack/files/patch-generic-jkFormatMP3.c
@@ -1,6 +1,6 @@
---- ../generic/jkFormatMP3.c.orig	Mon Jul 10 22:17:06 2006
-+++ ../generic/jkFormatMP3.c	Mon Jul 10 22:17:17 2006
-@@ -31,7 +31,6 @@
+--- ../generic/jkFormatMP3.c.orig	2005-12-14 11:29:38 UTC
++++ ../generic/jkFormatMP3.c
+@@ -31,7 +31,6 @@ must retain this copyright notice.
  #include <string.h>
  #define FRAS2(is,a) ((is) > 0 ? t_43[(is)]*(a):-t_43[-(is)]*(a))
  #define MAXFRAMESIZE 2106  /* frame size starting at header */
@@ -8,7 +8,7 @@
  static char *gblOutputbuf;
  static char *gblReadbuf;
  static int gblBufind = 0;
-@@ -1311,6 +1310,7 @@
+@@ -1311,6 +1310,7 @@ huffman_decode(int tbl,int *x,int *y)
  
  #include <math.h>
  
