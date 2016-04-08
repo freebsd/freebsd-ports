d9a686dc844f40fc1f03d5c374de71aab88ef5f9
ashish <ashish@FreeBSD.org>
2016-04-08 08:00:15 +0000

    editors/emacs:  - Clarify LICENSE, as it's "or any later version" clause  - Switch to CONFLICTS_INSTALL, as conflicts only happen on install  - Finish OPTIONS helpers conversion  - Convert patch-sources.el to SUB_FILES  - Convert *FLAGS to USES=localbase  - Convert IGNORE and SOUND to _IMPLIES  - Drop DBUS_PTHREAD_LIBS after ports r382294  - Drop default WRKSRC  - Drop clang workaround for unsupported FreeBSD versions
    editors/emacs-nox11:
     - Kill EMACS_NO_X11_SLAVE in favour of ?= variables
     - Conflict with any EMACS_VER
    
    PR:		208217
    Submitted by:	jbeich

--- editors/emacs/files/patch-sources.el
+++
@@ -1,7 +0,0 @@
---- /dev/null
-+++ sources.el
-@@ -0,0 +1,4 @@
-+;;; Path to Emacs C Sources.
-+(when (string-match (regexp-quote "%%EMACS_VER%%") emacs-version)
-+  (setq find-function-C-source-directory
-+        "%%DATADIR%%/%%EMACS_VER%%/src"))
