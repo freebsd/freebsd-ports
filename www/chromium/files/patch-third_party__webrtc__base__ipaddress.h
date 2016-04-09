e1df0c6753fa94f0080aeb96c61a3ef19e8a7cb8
cmt <cmt@FreeBSD.org>
2016-04-09 19:53:29 +0000

    Update chromium to 49.0.2623.112
    while here, unbreak build on FreeBSD 9
    
    Approved by:	rene (mentor)
    MFH:		2016Q2

--- www/chromium/files/patch-third_party__webrtc__base__ipaddress.h
+++ www/chromium/files/patch-third_party__webrtc__base__ipaddress.h
@@ -1,10 +1,11 @@
 --- third_party/webrtc/base/ipaddress.h.orig	2014-10-10 09:16:13 UTC
 +++ third_party/webrtc/base/ipaddress.h
-@@ -17,6 +17,9 @@
+@@ -17,6 +17,10 @@
  #include <arpa/inet.h>
  #include <netdb.h>
  #endif
 +#if defined(WEBRTC_BSD)
++#include <sys/types.h>
 +#include <sys/socket.h>
 +#endif
  #if defined(WEBRTC_WIN)
