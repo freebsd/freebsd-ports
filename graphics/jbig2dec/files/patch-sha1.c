81723b2a164ec37615a97770e6a29fec88c6902b
sunpoet <sunpoet@FreeBSD.org>
2016-04-09 12:29:43 +0000

    - Update to 0.13
    Changes:	http://git.ghostscript.com/?p=jbig2dec.git;a=blob;f=CHANGES;hb=HEAD

--- graphics/jbig2dec/files/patch-sha1.c
+++ graphics/jbig2dec/files/patch-sha1.c
@@ -7,18 +7,18 @@
 +
 +#include <sha.h>
 +#ifndef SHA1_DIGEST_SIZE
-+#	define SHA1_DIGEST_SIZE	20
++#define SHA1_DIGEST_SIZE 20
 +#endif
 +
 +#if 0
  #include "sha1.h"
  
  void SHA1_Transform(uint32_t state[5], const uint8_t buffer[64]);
-@@ -260,7 +267,6 @@ void SHA1_Final(SHA1_CTX* context, uint8
+@@ -258,7 +265,6 @@ SHA1_Final(SHA1_CTX *context, uint8_t di
  
  /*************************************************************/
  
 -#if 0
- int main(int argc, char** argv)
+ int
+ main(int argc, char **argv)
  {
- int i, j;
