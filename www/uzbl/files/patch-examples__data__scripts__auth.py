2c224bea41fa1943b3e4e33bdc0fc18957e643f5
riggs <riggs@FreeBSD.org>
2016-04-10 12:17:04 +0000

    Update to upstream version 0.9
    PR:		208293
    Submitted by:	aehlig@linta.de (maintainer)

--- www/uzbl/files/patch-examples__data__scripts__auth.py
+++
@@ -1,9 +0,0 @@
---- examples/data/scripts/auth.py.orig	2010-04-09 13:34:30.000000000 +0200
-+++ examples/data/scripts/auth.py	2010-04-09 13:35:05.000000000 +0200
-@@ -1,4 +1,5 @@
--#!/usr/bin/python
-+#!/usr/bin/env python
-+
- 
- import gtk
- import sys
