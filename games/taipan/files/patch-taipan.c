e12125add7fcb4a681f96047edb42b579899d233
Jason Unovitch <junovitch@FreeBSD.org>
2016-04-10 01:21:51 +0000

    games/taipan: fix out of bounds array subscript
    taipan.c:2709:7: warning: array index 4 is past the end of the array (which contains 4 elements) [-Warray-bounds]
    
    PR:		206771
    Submitted by:	Dave Walton <dw-fbsd@digger.net>
    Submitted by:	c.brinkhaus@t-online.de

--- games/taipan/files/patch-taipan.c
+++ games/taipan/files/patch-taipan.c
@@ -1,6 +1,6 @@
---- taipan.c.orig	Fri Mar 15 15:05:41 2002
-+++ taipan.c	Fri Mar 15 15:08:30 2002
-@@ -2572,7 +2572,7 @@
+--- taipan.c.orig	2001-12-31 16:09:20 UTC
++++ taipan.c
+@@ -2572,7 +2572,7 @@ void mchenry(void)
           amount = get_num(9);
           if (amount == -1)
           {
@@ -9,3 +9,13 @@
           }
           if (amount <= cash)
           {
+@@ -2705,8 +2705,8 @@ void final_stats(void)
+       bank         = 0;
+       hkw_[0]      = 0;
+       hkw_[1]      = 0;
++      hkw_[2]      = 0;
+       hkw_[3]      = 0;
+-      hkw_[4]      = 0;
+       hold_[0]     = 0;
+       hold_[1]     = 0;
+       hold_[2]     = 0;
