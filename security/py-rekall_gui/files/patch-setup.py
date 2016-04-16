4a20178fa46fa6375e3383d9f8565636c0e2eff2
Antoine Brodin <antoine@FreeBSD.org>
2016-04-16 13:31:18 +0000

    Update rekall to 1.5.0

--- security/py-rekall_gui/files/patch-setup.py
+++ security/py-rekall_gui/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2015-09-10 14:25:33 UTC
+--- setup.py.orig	2016-03-28 22:03:48 UTC
 +++ setup.py
-@@ -38,7 +38,8 @@ def find_data_files_directory(source):
+@@ -33,7 +33,8 @@ def find_data_files_directory(source):
      result = []
      for directory, _, files in os.walk(source):
          files = [os.path.join(directory, x) for x in files]
