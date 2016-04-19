407b0353e79188c66ae87d1ff204d32103b5cf86
cpm <cpm@FreeBSD.org>
2016-04-19 08:45:49 +0000

    - Update to 4.22 - Fix WWW in pkg-descr - Regenerate patches using 'make makepatch'
    Changelog: http://cvs.schmorp.de/libev/Changes?pathrev=rel-4_22
    
    PR:		208775
    Approved by:	vsevolod (maintainer), junovitch (mentor)

--- devel/libev/files/patch-ev.c
+++ devel/libev/files/patch-ev.c
@@ -1,6 +1,6 @@
---- ev.c.orig	2015-07-13 11:32:52 UTC
+--- ev.c.orig	2015-12-20 21:12:53 UTC
 +++ ev.c
-@@ -2603,7 +2603,7 @@ ev_recommended_backends (void) EV_THROW
+@@ -2736,7 +2736,7 @@ ev_recommended_backends (void) EV_THROW
  {
    unsigned int flags = ev_supported_backends ();
  
