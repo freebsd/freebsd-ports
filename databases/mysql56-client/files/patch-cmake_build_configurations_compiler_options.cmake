a3298d994e52e10810e13010431f68bf71c7ee33
ale <ale@FreeBSD.org>
2016-04-19 15:45:07 +0000

    Update to 5.6.30 release.

--- databases/mysql56-client/files/patch-cmake_build_configurations_compiler_options.cmake
+++ databases/mysql56-client/files/patch-cmake_build_configurations_compiler_options.cmake
@@ -1,6 +1,6 @@
---- cmake/build_configurations/compiler_options.cmake.orig	2014-12-05 10:49:17.394216277 +0000
-+++ cmake/build_configurations/compiler_options.cmake	2014-12-05 10:50:58.144209859 +0000
-@@ -25,7 +25,7 @@
+--- cmake/build_configurations/compiler_options.cmake.orig	2016-03-03 16:35:33.000000000 +0100
++++ cmake/build_configurations/compiler_options.cmake	2016-04-19 17:30:13.345558666 +0200
+@@ -25,7 +25,7 @@ IF(UNIX)  
  
    # Default GCC flags
    IF(CMAKE_COMPILER_IS_GNUCC)
@@ -9,7 +9,7 @@
      # Disable inline optimizations for valgrind testing to avoid false positives
      IF(WITH_VALGRIND)
        SET(COMMON_C_FLAGS             "-fno-inline ${COMMON_C_FLAGS}")
-@@ -34,7 +34,7 @@
+@@ -34,7 +34,7 @@ IF(UNIX)  
      SET(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 ${COMMON_C_FLAGS}")
    ENDIF()
    IF(CMAKE_COMPILER_IS_GNUCXX)
