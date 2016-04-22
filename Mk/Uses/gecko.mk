c0c8e78b3dff6e420a643c7fa79902bc92d6d721
Jan Beich <jbeich@FreeBSD.org>
2016-04-22 12:16:01 +0000

    MFH: r413326 r413726 r413760
    www/firefox{,-esr}: update to 46.0 (rc5) / 45.1esr
    
    Changes:	https://www.mozilla.org/firefox/46.0/releasenotes/
    Changes:	https://www.mozilla.org/firefox/45.1.0/releasenotes/
    Security:	92d44f83-a7bf-41cf-91ee-3d1b8ecf579f
    Approved by:	ports-secteam (junovitch)

--- Mk/Uses/gecko.mk
+++ Mk/Uses/gecko.mk
@@ -31,18 +31,18 @@ GECKO_CONFING?=	${LOCALBASE}/bin/${GECKO}-config
 XPIDL?=		${LOCALBASE}/lib/${GECKO}/xpidl
 XPIDL_INCL?=	`${GECKO_CONFIG} --idlflags`
 
-BUILD_DEPENDS+=	libxul>=38:www/libxul
-RUN_DEPENDS+=	libxul>=38:www/libxul
+BUILD_DEPENDS+=	libxul>=45:www/libxul
+RUN_DEPENDS+=	libxul>=45:www/libxul
 
 .elif ${gecko_ARGS:Mfirefox}
 
-_GECKO_DEFAULT_VERSION=	38
-_GECKO_VERSIONS=		38 45
+_GECKO_DEFAULT_VERSION=	45
+_GECKO_VERSIONS=		45 46
 _GECKO_TYPE=	firefox
 
 # Dependence lines for different Firefox versions
-38_DEPENDS=		${LOCALBASE}/lib/firefox/firefox:www/firefox-esr
-45_DEPENDS=		${LOCALBASE}/lib/firefox/firefox:www/firefox
+45_DEPENDS=		${LOCALBASE}/lib/firefox/firefox:www/firefox-esr
+46_DEPENDS=		${LOCALBASE}/lib/firefox/firefox:www/firefox
 
 .if exists(${LOCALBASE}/bin/firefox)
 _GECKO_INSTALLED_VER!=	${LOCALBASE}/bin/firefox --version 2>/dev/null
