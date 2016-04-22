2140691265ebd373ec8b22d9fa1c4ac676f60e62
Kurt Jaeger <pi@FreeBSD.org>
2016-04-22 09:50:42 +0000

    cad/openvsp: 2.3.0 -> 3.5.2
    Changes:
    - Latest:
      http://openvsp.org/blogs/announcements/2016/02/14/openvsp-3-5-2-released
    - all older release notes are in
      http://openvsp.org/blogs/
    - no longer includes example aircraft files, now seperatly available at
      http://hangar.openvsp.org/
    - Needs some math functions, so no luck on 9.x or i386
    
    PR:		208904
    Submitted by:	Fernando Apesteguia <fernando.apesteguia@gmail.com> (maintainer)

---
+++ cad/openvsp/files/patch-src_gui__and__draw_MenuItem.h
@@ -0,0 +1,11 @@
+--- src/gui_and_draw/MenuItem.h.orig	2016-02-14 00:01:30 UTC
++++ src/gui_and_draw/MenuItem.h
+@@ -10,7 +10,7 @@
+ #define MENUITEM__INCLUDED_
+ 
+ #include <string>
+-#include <FL/Fl_Sys_Menu_Bar.h>
++#include <FL/Fl_Sys_Menu_Bar.H>
+ 
+ class ScreenMgr;
+ class ActionScreen;
