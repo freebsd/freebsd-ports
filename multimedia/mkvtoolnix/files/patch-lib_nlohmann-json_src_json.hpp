4a08b220a5f3b4b505b2bb275d1fdc09b167908c
riggs <riggs@FreeBSD.org>
2016-04-23 19:02:11 +0000

    Update to upstream releaser 9.1.0

--- multimedia/mkvtoolnix/files/patch-lib_nlohmann-json_src_json.hpp
+++ multimedia/mkvtoolnix/files/patch-lib_nlohmann-json_src_json.hpp
@@ -1,8 +1,8 @@
---- lib/nlohmann-json/src/json.hpp.orig	2015-12-31 12:23:52 UTC
+--- lib/nlohmann-json/src/json.hpp.orig	2016-04-23 13:52:02 UTC
 +++ lib/nlohmann-json/src/json.hpp
-@@ -5737,7 +5737,9 @@ class basic_json
-                     /// use integer array index as key
-                     case (value_t::array):
+@@ -6224,7 +6224,9 @@ class basic_json
+                     // use integer array index as key
+                     case value_t::array:
                      {
 -                        return std::to_string(array_index);
 +                        std::ostringstream TempStream;
@@ -10,13 +10,13 @@
 +                        return TempStream.str();
                      }
  
-                     /// use key from the object
-@@ -6902,7 +6904,7 @@ basic_json_parser_59:
+                     // use key from the object
+@@ -8204,7 +8206,7 @@ basic_json_parser_63:
+         */
+         long double str_to_float_t(long double* /* type */, char** endptr) const
          {
-             // conversion
-             typename string_t::value_type* endptr;
--            const auto float_val = std::strtold(reinterpret_cast<typename string_t::const_pointer>(m_start),
-+            const auto float_val = std::strtod(reinterpret_cast<typename string_t::const_pointer>(m_start),
-                                                 &endptr);
+-            return std::strtold(reinterpret_cast<typename string_t::const_pointer>(m_start), endptr);
++            return std::strtod(reinterpret_cast<typename string_t::const_pointer>(m_start), endptr);
+         }
  
-             // return float_val if the whole number was translated and NAN
+         /*!
