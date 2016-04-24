4480d2cd19c7dbf5c1935ca605cee0296aa280c0
flo <flo@FreeBSD.org>
2016-04-24 08:09:27 +0000

    Update to 0.2.18
    PR:		208790
    Submitted by:	phd_kimberlite@yahoo.co.jp (maintainer), dieterich@ogolem.org
    Sponsored by:	Essen Linuxhotel Hackathon 2016

--- math/openblas/files/patch-driver_others_blas__server.c
+++ math/openblas/files/patch-driver_others_blas__server.c
@@ -1,11 +1,11 @@
---- driver/others/blas_server.c.orig	2015-10-27 20:44:50 UTC
+--- driver/others/blas_server.c.orig	2016-04-12 19:29:19 UTC
 +++ driver/others/blas_server.c
 @@ -70,7 +70,7 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  /*********************************************************************/
  
  #include "common.h"
--#if defined(OS_LINUX) || defined(OS_NETBSD) || defined(OS_DARWIN) || defined(OS_ANDROID)
-+#if defined(OS_LINUX) || defined(OS_NETBSD) || defined(OS_DARWIN) || defined(OS_ANDROID) || defined(OS_FREEBSD)
+-#if defined(OS_LINUX) || defined(OS_NETBSD) || defined(OS_DARWIN) || defined(OS_ANDROID) || defined(OS_SUNOS)
++#if defined(OS_LINUX) || defined(OS_NETBSD) || defined(OS_DARWIN) || defined(OS_ANDROID) || defined(OS_SUNOS) || defined(OS_FREEBSD)
  #include <dlfcn.h>
  #include <signal.h>
  #include <sys/resource.h>
