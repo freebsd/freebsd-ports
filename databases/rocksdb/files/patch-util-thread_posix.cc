c3a0c46801622c3b457b4a02806eff7c40df92a2
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2016-04-28 19:03:35 +0000

    - Update to 4.5.1
    Changes:	https://github.com/facebook/rocksdb/releases

--- databases/rocksdb/files/patch-util-thread_posix.cc
+++ databases/rocksdb/files/patch-util-thread_posix.cc
@@ -1,11 +1,11 @@
---- util/thread_local.cc.orig	2015-03-25 21:40:41 UTC
-+++ util/thread_local.cc
+--- util/thread_posix.cc.orig	2016-03-25 19:09:20 UTC
++++ util/thread_posix.cc
 @@ -7,6 +7,8 @@
  // Use of this source code is governed by a BSD-style license that can be
  // found in the LICENSE file. See the AUTHORS file for names of contributors.
  
 +#include <cstdlib>
 +
- #include "util/thread_local.h"
- #include "util/mutexlock.h"
- #include "port/likely.h"
+ #include "util/thread_posix.h"
+ #include <atomic>
+ #include <unistd.h>
