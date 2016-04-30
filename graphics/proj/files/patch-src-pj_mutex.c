132c674047c68a252f0d5f1ffbe757f3b2b3ebf3
sunpoet <sunpoet@FreeBSD.org>
2016-04-30 15:39:03 +0000

    - Update to 4.9.2
    Changes:	https://github.com/OSGeo/proj.4/blob/master/NEWS
    PR:		208512
    Reported by:	Darcy Buskermolen <darcy@dbitech.ca>
    Submitted by:	Taoka Fumiyoshi <fmysh@iijmio-mail.jp> (maintainer)

--- graphics/proj/files/patch-src-pj_mutex.c
+++ graphics/proj/files/patch-src-pj_mutex.c
@@ -1,13 +1,13 @@
---- src/pj_mutex.c.orig	2015-05-25 16:01:14.724346000 +0800
-+++ src/pj_mutex.c	2015-05-25 16:03:52.777784000 +0800
-@@ -118,11 +118,7 @@
+--- src/pj_mutex.c.orig	2015-09-10 13:41:24 UTC
++++ src/pj_mutex.c
+@@ -122,11 +122,7 @@ void pj_acquire_lock()
          pthread_mutex_lock( &pj_precreated_lock);
  
          pthread_mutexattr_init(&mutex_attr);
--#ifndef PTHREAD_MUTEX_RECURSIVE
--        pthread_mutexattr_settype(&mutex_attr, PTHREAD_MUTEX_RECURSIVE_NP);
--#else
+-#ifdef HAVE_PTHREAD_MUTEX_RECURSIVE
          pthread_mutexattr_settype(&mutex_attr, PTHREAD_MUTEX_RECURSIVE);
+-#else
+-        pthread_mutexattr_settype(&mutex_attr, PTHREAD_MUTEX_RECURSIVE_NP);
 -#endif
          pthread_mutex_init(&pj_core_lock, &mutex_attr);
          pj_core_lock_created = 1;
