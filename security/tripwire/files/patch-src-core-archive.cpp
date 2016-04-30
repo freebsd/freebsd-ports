bce879ffe64b3f7cfefbb23ce7eb563b76a4bc0d
cy <cy@FreeBSD.org>
2016-04-30 07:16:46 +0000

    Update tripwire 2.4.2.2 --> 2.4.3.1
    Reported by:	Brian Cox <bcox@tripwire.com>

--- security/tripwire/files/patch-src-core-archive.cpp
+++ security/tripwire/files/patch-src-core-archive.cpp
@@ -1,13 +1,13 @@
---- src/core/archive.cpp.orig	2011-11-21 17:06:56.000000000 +0100
-+++ src/core/archive.cpp	2014-06-15 01:45:01.000000000 +0200
-@@ -886,8 +886,8 @@ void cLockedTemporaryFileArchive::OpenRe
-         catch( eFSServices& e)
-           {
-             TSTRING errStr = TSS_GetString( cCore, core::STR_BAD_TEMPDIRECTORY );
--            eArchiveOpen e(strTempFile, errStr);
--            throw e;
-+            eArchiveOpen e2(strTempFile, errStr);
-+            throw e2;
-           }
-       }
-     ///////////////////////////////////////////////////////////////////////////////
+--- src/core/archive.cpp.orig	2016-04-23 19:12:41.000000000 -0700
++++ src/core/archive.cpp	2016-04-29 23:21:27.059769000 -0700
+@@ -919,8 +919,8 @@
+   }//try
+   catch (eFile& fileError) {
+     TSTRING errStr = TSS_GetString( cCore, core::STR_BAD_TEMPDIRECTORY );
+-    eArchiveOpen e(strTempFile, errStr);
+-    throw e;
++    eArchiveOpen e2(strTempFile, errStr);
++    throw e2;
+   }
+ 
+   ///////////////////////////////////////////////////////////////////////////////
