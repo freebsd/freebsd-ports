4da3639ecf8336b2de8f2342f1abf8a38c6a2a33
Bernard Spil <brnrd@FreeBSD.org>
2016-05-02 21:09:31 +0000

    net/socat: Fix OpenSSL build issues
      - While here fix CPE Vendor (NVD has dest-unreach)
      - Fix build when OpenSSL from ports was built without SSLv3 support
      - Fix build when OpenSSL is built without compression support
      - Fix build for OpenSSL 1.1.0pre without EGD support
    
    PR:		197192
    Reviewed by:	koobs (mentor), feld (mentor), ehaupt (maintainer)
    Approved by:	feld (mentor), ehaupt (maintainer)
    Differential Revision:	D2691

--- net/socat/files/patch-sslcls.c
+++ net/socat/files/patch-sslcls.c
@@ -1,6 +1,6 @@
---- sslcls.c.orig	2015-01-24 11:15:22.000000000 +0100
+--- sslcls.c.orig	2015-01-24 10:15:22 UTC
 +++ sslcls.c
-@@ -55,6 +55,7 @@
+@@ -55,6 +55,7 @@ const SSL_METHOD *sycSSLv2_server_method
  }
  #endif
  
@@ -8,7 +8,7 @@
  const SSL_METHOD *sycSSLv3_client_method(void) {
     const SSL_METHOD *result;
     Debug("SSLv3_client_method()");
-@@ -62,7 +63,9 @@
+@@ -62,7 +63,9 @@ const SSL_METHOD *sycSSLv3_client_method
     Debug1("SSLv3_client_method() -> %p", result);
     return result;
  }
@@ -18,7 +18,7 @@
  const SSL_METHOD *sycSSLv3_server_method(void) {
     const SSL_METHOD *result;
     Debug("SSLv3_server_method()");
-@@ -70,6 +73,7 @@
+@@ -70,6 +73,7 @@ const SSL_METHOD *sycSSLv3_server_method
     Debug1("SSLv3_server_method() -> %p", result);
     return result;
  }
@@ -26,4 +26,28 @@
  
  const SSL_METHOD *sycSSLv23_client_method(void) {
     const SSL_METHOD *result;
-
+@@ -331,6 +335,7 @@ void sycSSL_free(SSL *ssl) {
+    return;
+ }
+ 
++#ifndef OPENSSL_NO_EGD
+ int sycRAND_egd(const char *path) {
+    int result;
+    Debug1("RAND_egd(\"%s\")", path);
+@@ -338,6 +343,7 @@ int sycRAND_egd(const char *path) {
+    Debug1("RAND_egd() -> %d", result);
+    return result;
+ }
++#endif
+ 
+ DH *sycPEM_read_bio_DHparams(BIO *bp, DH **x, pem_password_cb *cb, void *u) {
+    DH *result;
+@@ -375,7 +381,7 @@ int sycFIPS_mode_set(int onoff) {
+ }
+ #endif /* WITH_FIPS */
+ 
+-#if OPENSSL_VERSION_NUMBER >= 0x00908000L
++#if OPENSSL_VERSION_NUMBER >= 0x00908000L && !defined OPENSSL_NO_COMP
+ const COMP_METHOD *sycSSL_get_current_compression(SSL *ssl) {
+    const COMP_METHOD *result;
+    Debug1("SSL_get_current_compression(%p)", ssl);
