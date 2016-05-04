ee0a22e6df83c47996f211cb1aab82af28d55dbb
Tilman Keskinoz <arved@FreeBSD.org>
2016-05-04 09:23:19 +0000

    Fix DVB support
    PR:		208097
    Reported by:	Stefan Rumetshofer
    
    Grab back maintainership

--- multimedia/kaffeine/files/patch-frontend.h
+++ multimedia/kaffeine/files/patch-frontend.h
@@ -1,6 +1,6 @@
 --- include/frontend.h.orig	2015-08-07 15:43:59 UTC
 +++ include/frontend.h
-@@ -26,7 +26,14 @@
+@@ -26,7 +26,15 @@
  #ifndef _DVBFRONTEND_H_
  #define _DVBFRONTEND_H_
  
@@ -12,6 +12,7 @@
 +typedef uint32_t __u32;
 +typedef uint16_t __u16;
 +typedef uint8_t __u8;
++typedef int64_t __s64;
 +#endif
  
  typedef enum fe_type {
