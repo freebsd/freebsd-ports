e10a937882c9b34e2f85e952b91fdf7b33c89d65
pi <pi@FreeBSD.org>
2016-05-05 13:44:59 +0000

    www/node: 5.11.0 -> 6.0.0 New port: www/node5, to ease migration from node-5.x to node-6.x
    This new major version of Node.js delivers significant improvements
    in performance, reliability, usability and security. Module loading
    is nearly four times faster than in Node.js v4, tests and documentation
    have seen significant improvements, usability of the Buffer and
    File System APIs have been improved, and Node.js now supports 93%
    of ES6 language features, thanks to V8 version 5.0.
    
    The full release announcement can be read here:
    https://nodejs.org/en/blog/release/v6.0.0/
    
    The full changelog is available on Github:
    https://github.com/nodejs/node/commit/ce3e3c5fe15479475c068482c48eb9cbf1ac9df5
    
    For node5:
    node.js v5.x will only be supported for a few months to give people
    time to move to v6. This new port is marked as deprecated with an
    expiration date of July 1, allowing all of May and June to transition.
    See https://nodejs.org/en/blog/release/v6.0.0/#what-about-node-js-v5
    
    PR:		209215, 209216
    Submitted by:	Bradley T. Hughes <bradleythughes@fastmail.fm> (maintainer)

--- www/node5/files/patch-deps_v8_src_zone-containers.h
+++ www/node5/files/patch-deps_v8_src_zone-containers.h
@@ -1,5 +1,5 @@
---- deps/v8/src/zone-containers.h.orig	2016-03-16 21:22:58.000000000 +0100
-+++ deps/v8/src/zone-containers.h	2016-03-24 23:34:37.741885000 +0100
+--- deps/v8/src/zone-containers.h.orig	2016-04-21 17:45:25 UTC
++++ deps/v8/src/zone-containers.h
 @@ -114,12 +114,12 @@ class ZoneSet : public std::set<K, Compa
  // a zone allocator.
  template <typename K, typename V, typename Compare = std::less<K>>
