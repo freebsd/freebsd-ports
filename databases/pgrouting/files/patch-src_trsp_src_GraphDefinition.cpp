ac379baa9501db2caaa87365a00b4735925148a2
wen <wen@FreeBSD.org>
2016-05-06 08:11:26 +0000

    - Update to 2.2.2
    PR:		209137
    Submitted by:	coder@tuxfamily.org(maintainer)

---
+++ databases/pgrouting/files/patch-src_trsp_src_GraphDefinition.cpp
@@ -0,0 +1,11 @@
+--- src/trsp/src/GraphDefinition.cpp.orig	2016-04-28 19:58:27 UTC
++++ src/trsp/src/GraphDefinition.cpp
+@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fi
+ #include <windows.h>
+ #endif
+ 
+-
++#include <cstdlib>
+ #include <sstream>
+ #include "GraphDefinition.h"
+ 
