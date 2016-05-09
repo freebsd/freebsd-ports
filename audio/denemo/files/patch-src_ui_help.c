9694b0366a8c95fde3ce4ec56b823b6ba2fc6636
mat <mat@FreeBSD.org>
2016-05-09 13:43:40 +0000

    Update to 2.0.6.
    maintainer timeout.
    
    PR:		209042
    Submitted by:	tkato432 yahoo com
    Sponsored by:	Absolight

--- audio/denemo/files/patch-src_ui_help.c
+++ audio/denemo/files/patch-src_ui_help.c
@@ -1,4 +1,4 @@
---- src/ui/help.c.orig	2015-02-09 02:55:09 UTC
+--- src/ui/help.c.orig	2016-04-10 12:10:21 UTC
 +++ src/ui/help.c
 @@ -57,7 +57,7 @@ browse_manual (GtkAction * action, Denem
    GError *error = NULL;
