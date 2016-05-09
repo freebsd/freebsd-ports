9ccead870a17ba60aacb4459d821756ed89a4484
mat <mat@FreeBSD.org>
2016-05-09 13:42:54 +0000

    Fix build on 9 and fix dependency list.
    PR:		209046
    Submitted by:	tkato432 yahoo com
    Sponsored by:	Absolight

--- multimedia/obs-studio/files/patch-obs_window-basic-main.cpp
+++ multimedia/obs-studio/files/patch-obs_window-basic-main.cpp
@@ -1,6 +1,6 @@
---- obs/window-basic-main.cpp.orig	2015-03-27 21:29:37 UTC
+--- obs/window-basic-main.cpp.orig	2016-04-24 23:05:32 UTC
 +++ obs/window-basic-main.cpp
-@@ -1056,6 +1056,10 @@ void OBSBasic::TimedCheckForUpdates()
+@@ -1824,6 +1824,10 @@ void OBSBasic::TimedCheckForUpdates()
  
  void OBSBasic::CheckForUpdates()
  {
