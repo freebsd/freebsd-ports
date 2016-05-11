24af7e3c61d52ea9f73b79329b154b44cfd02e17
Sean Bruno <sbruno@FreeBSD.org>
2016-05-11 20:54:32 +0000

    Update clementine-player to Upstream Release 1.3.1: https://github.com/clementine-player/Clementine/releases
    Requires the sqlite port to have FTS3_TOKENIZER enabled. Add a note
    to UPDATING about this.
    
    Fixes so many bugs, that its hard to keep up.
    
    Add PulseAudio support.
    
    Add Microsoft SkyDrive Support.
    
    Add Amazon Cloud Drive Support.
    
    Require the use of security/cryptopp for lastFM.
    
    Update to gstreamer1.
    
    Sort options and Makefile a bit.
    
    Fix typo in MTP_DEVICE.
    
    Upstream has fixed CD playback on FreeBSD.
    
    Differential Revision:	https://reviews.freebsd.org/D6200

---
+++ audio/clementine-player/files/patch-src_internet_vk_vkconnection.h
@@ -0,0 +1,11 @@
+--- src/internet/vk/vkconnection.h.orig	2016-04-19 15:08:35 UTC
++++ src/internet/vk/vkconnection.h
+@@ -19,6 +19,8 @@
+ #ifndef INTERNET_VK_VKCONNECTION_H_
+ #define INTERNET_VK_VKCONNECTION_H_
+ 
++#include <time.h>
++
+ #include "vreen/client.h"
+ #include "vreen/connection.h"
+ 
