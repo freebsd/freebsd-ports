132ef275bfede146e9e6581f89bc31eba0f8253d
bapt <bapt@FreeBSD.org>
2016-05-13 19:30:14 +0000

    Prevent collision with getline(3) While here, regenerate patches

--- mail/metamail/files/patch-metamail_mmencode.c
+++ mail/metamail/files/patch-metamail_mmencode.c
@@ -1,6 +1,6 @@
---- metamail/mmencode.c.orig	Wed Jan 26 19:47:37 1994
-+++ metamail/mmencode.c	Mon Dec 18 11:46:22 2006
-@@ -13,6 +13,7 @@
+--- metamail/mmencode.c.orig	1994-01-26 18:47:37 UTC
++++ metamail/mmencode.c
+@@ -13,6 +13,7 @@ OF THIS MATERIAL FOR ANY PURPOSE.  IT IS
  WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
  */
  #include <stdio.h>
