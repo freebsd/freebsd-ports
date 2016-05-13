8fa41c7b017454154a3460a295a0265dc8582d8f
bapt <bapt@FreeBSD.org>
2016-05-13 19:01:27 +0000

    Prevent collision with getline(3)

--- editors/gate/files/patch-spel.c
+++ editors/gate/files/patch-spel.c
@@ -1,13 +1,11 @@
-diff --git spel.c spel.c
-index 5c53256..1e9ad04 100644
---- spel.c
+--- spel.c.orig	2003-06-19 16:55:51 UTC
 +++ spel.c
-@@ -151,7 +151,7 @@ empty_repl()
- /* ADD_REPL - Add the given replacement to the list of replacements.
-  * "replace" may be NULL to indicate that the word is to be ignored.
-  */
--
-+void
- add_repl(char *word, int word_len, char *replace)
- {
-     struct repl *r;
+@@ -683,7 +683,7 @@ int fix_word(char *line, int offset, int
+     for(;;)
+     {
+ 	subtask= 1;
+-	if (getline(bf,NULL,&wcol,st))
++	if (get_line(bf,NULL,&wcol,st))
+ 	{
+ 	    printf("\nSpell Check Interupted.\n");
+ 	    return 1;
