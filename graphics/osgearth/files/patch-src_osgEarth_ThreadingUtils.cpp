8c6c5083e9d96200bea9ec495373f4b06e4a39f5
woodsb02 <woodsb02@FreeBSD.org>
2016-05-14 21:44:49 +0000

    graphics/osgearth: - Update to 2.7 (this new version also allows osgearth to build against osg-3.4.0) - Rename and update patch of src/osgEarth/ThreadingUtils.cpp (pet portlint) - Fix COMMENT (pet portlint) - Fix BUILD_DEPENDS for minizip
    Changes this release:
      http://docs.osgearth.org/en/latest/releasenotes.html#version-2-7-july-2015
    
    PR:		209236
    Reviewed by:	mat (mentor)
    Approved by:	lbartoletti@tuxfamily.org (maintainer), adamw (mentor)
    Differential Revision:	https://reviews.freebsd.org/D6344

--- graphics/osgearth/files/patch-src_osgEarth_ThreadingUtils.cpp
+++ graphics/osgearth/files/patch-src_osgEarth_ThreadingUtils.cpp
@@ -1,5 +1,5 @@
---- src/osgEarth/ThreadingUtils.cpp.orig	2013-05-16 20:29:30.000000000 +0200
-+++ src/osgEarth/ThreadingUtils.cpp	2013-05-16 20:28:01.000000000 +0200
+--- src/osgEarth/ThreadingUtils.cpp.orig	2016-02-10 18:21:28 UTC
++++ src/osgEarth/ThreadingUtils.cpp
 @@ -23,6 +23,7 @@
  #else
  #   include <unistd.h>
@@ -8,10 +8,10 @@
  #endif
  
  using namespace osgEarth::Threading;
-@@ -38,6 +39,10 @@
-   return (unsigned)::GetCurrentThreadId();
- #elif __APPLE__
+@@ -40,6 +41,10 @@ unsigned osgEarth::Threading::getCurrent
    return ::syscall(SYS_thread_selfid);
+ #elif __ANDROID__
+   return gettid();
 +#elif __FreeBSD__
 +  long lwpid;
 +  thr_self(&lwpid);
