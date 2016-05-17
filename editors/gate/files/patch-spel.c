669b32ca73e52fe476639c949772e9df7c39b129
antoine <antoine@FreeBSD.org>
2016-05-17 11:18:19 +0000

    Fix build with clang

--- editors/gate/files/patch-spel.c
+++ editors/gate/files/patch-spel.c
@@ -1,5 +1,14 @@
 --- spel.c.orig	2003-06-19 16:55:51 UTC
 +++ spel.c
+@@ -151,7 +151,7 @@ empty_repl()
+ /* ADD_REPL - Add the given replacement to the list of replacements.
+  * "replace" may be NULL to indicate that the word is to be ignored.
+  */
+-
++void
+ add_repl(char *word, int word_len, char *replace)
+ {
+     struct repl *r;
 @@ -683,7 +683,7 @@ int fix_word(char *line, int offset, int
      for(;;)
      {
