6cd41f462ef8f43f363b8d3b4e0b2fe89b11a638
feld <feld@FreeBSD.org>
2016-05-17 15:25:15 +0000

    multimedia/plexpy: Update to 1.4.0
    - Patch Plex.py to have corrrect shebang
    
    Changelog:	https://github.com/drzoidberg33/plexpy/releases/tag/v1.4.0

---
+++ multimedia/plexpy/files/patch-PlexPy.py
@@ -0,0 +1,11 @@
+--- PlexPy.py.orig	2016-05-17 15:13:59 UTC
++++ PlexPy.py
+@@ -1,8 +1,4 @@
+ #!/bin/sh
+-''''which python    >/dev/null 2>&1 && exec python    "$0" "$@" # '''
+-''''which python2   >/dev/null 2>&1 && exec python2   "$0" "$@" # '''
+-''''which python2.7 >/dev/null 2>&1 && exec python2.7 "$0" "$@" # '''
+-''''exec echo "Error: Python not found!" # '''
+ 
+ # -*- coding: utf-8 -*-
+ 
