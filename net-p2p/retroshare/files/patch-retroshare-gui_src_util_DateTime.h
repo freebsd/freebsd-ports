72eaf7590f60a48b2e8db4266eab91482afd0c4c
pi <pi@FreeBSD.org>
2016-05-21 02:10:58 +0000

    net-p2p/retroshare: 0.5.5c -> 0.6.0
    Changes:
     https://retroshareteam.wordpress.com/2016/02/07/release-notes-for-final-0-6-0/
    - distsite moved to github
    
    PR:		207550
    Submitted by:	w.schwarzenfeld@aon.at, Yuri Victorovich <yuri@rawbw.com>, peter@netkey.at (maintainer)
    Reviewed by:	feld

---
+++ net-p2p/retroshare/files/patch-retroshare-gui_src_util_DateTime.h
@@ -0,0 +1,10 @@
+--- retroshare-gui/src/util/DateTime.h.orig	2016-03-15 21:37:06 UTC
++++ retroshare-gui/src/util/DateTime.h
+@@ -23,6 +23,7 @@
+ #define _DATETIME_H
+ 
+ #include <QString>
++#include <time.h>
+ 
+ class QDateTime;
+ class QDate;
