4085cad972a7759287bba446127e77c5c396586e
rene <rene@FreeBSD.org>
2016-05-25 20:56:06 +0000

    Remove expired misc/kdehier4 and update all of its consumers to not reference it any longer. This is a no-op because KDE4_PREFIX is equal to LOCALBASE
    Fix up properties for misc/kde4-l10n/files/bsd.l10n.mk to make svn happy.
    
    PR:		209014 (partial)
    Submitted by:	myself
    Approved by:	portmgr (bapt)
    Differential Revision:	https://reviews.freebsd.org/D6542

--- editors/calligra-l10n/files/bsd.l10n.mk
+++ editors/calligra-l10n/files/bsd.l10n.mk
@@ -6,7 +6,7 @@ DIST_SUBDIR=	KDE/calligra/${PORTVERSION}/l10n
 
 PLIST_SUB+=	LANG=${CALLIGRA_L10N}
 
-USE_KDE4=	kdelibs kdeprefix automoc4
+USE_KDE4=	kdelibs automoc4
 USE_QT4=	corelib xml moc_build qmake_build rcc_build uic_build
 USES+=		cmake gettext tar:xz
 NO_ARCH=	yes
