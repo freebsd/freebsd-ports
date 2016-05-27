3848c3ec7ffecbdd0d557d9175bed3a19387f49b
Alexey Dokuchaev <danfe@FreeBSD.org>
2016-05-27 05:41:27 +0000

    - Update to version 2.10.0 - Remove `compiler:c++11-lang' from USES, it is not needed (the port does   not build on 9.x with any of c++11-lang/c++11-lib/USE_GCC=yes anyways) - Make image format support in helper programs optional (and off by default)

--- graphics/embree/files/patch-common_sys_thread.cpp
+++ graphics/embree/files/patch-common_sys_thread.cpp
@@ -1,21 +1,59 @@
---- common/sys/thread.cpp.orig	2015-09-17 06:11:18 UTC
+--- common/sys/thread.cpp.orig	2016-05-20 05:45:13 UTC
 +++ common/sys/thread.cpp
-@@ -171,13 +171,16 @@ namespace embree
- /// Linux Platform
- ////////////////////////////////////////////////////////////////////////////////
+@@ -239,6 +239,29 @@ namespace embree
+ #endif
  
--#if defined(__LINUX__)
-+#if defined(__LINUX__) || defined(__FreeBSD__)
+ ////////////////////////////////////////////////////////////////////////////////
++/// FreeBSD Platform
++////////////////////////////////////////////////////////////////////////////////
++
++#if defined(__FreeBSD__)
 +
 +#include <pthread_np.h>
 +
- namespace embree
- {
-   /*! set affinity of the calling thread */
-   void setAffinity(ssize_t affinity)
-   {
--    cpu_set_t cset;
++namespace embree
++{
++  /*! set affinity of the calling thread */
++  void setAffinity(ssize_t affinity)
++  {
 +    cpuset_t cset;
-     CPU_ZERO(&cset);
-     CPU_SET(affinity, &cset);
++    CPU_ZERO(&cset);
++    CPU_SET(affinity, &cset);
++
++    if (pthread_setaffinity_np(pthread_self(), sizeof(cset), &cset) != 0)
++      WARNING("pthread_setaffinity_np failed"); // on purpose only a warning
++  }
++}
++#endif
++
++////////////////////////////////////////////////////////////////////////////////
+ /// MacOSX Platform
+ ////////////////////////////////////////////////////////////////////////////////
  
+@@ -291,7 +314,8 @@ namespace embree
+   {
+     _mm_setcsr(_mm_getcsr() | /*FTZ:*/ (1<<15) | /*DAZ:*/ (1<<6));
+ 
+-#if !defined(__LINUX__)
++#if defined(__MACOSX__)
++    /*! Mac OS X does not support setting affinity at thread creation time */
+     if (parg->affinity >= 0)
+ 	setAffinity(parg->affinity);
+ #endif
+@@ -320,7 +344,15 @@ namespace embree
+       cpu_set_t cset;
+       CPU_ZERO(&cset);
+       CPU_SET(mapThreadID(threadID), &cset);
+-      if (pthread_setaffinity_np(*tid,sizeof(cpu_set_t),&cset))
++      if (pthread_setaffinity_np(*tid, sizeof(cset), &cset))
++        WARNING("pthread_setaffinity_np failed"); // on purpose only a warning
++    }
++#elif defined(__FreeBSD__)
++    if (threadID >= 0) {
++      cpuset_t cset;
++      CPU_ZERO(&cset);
++      CPU_SET(threadID, &cset);
++      if (pthread_setaffinity_np(*tid, sizeof(cset), &cset))
+         WARNING("pthread_setaffinity_np failed"); // on purpose only a warning
+     }
+ #endif
