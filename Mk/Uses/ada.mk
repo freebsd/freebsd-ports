cc7211ab41c0e2954858109f4ace6f73dab8ec15
John Marino <marino@FreeBSD.org>
2016-05-27 09:54:47 +0000

    Mk/Uses/ada.mk: Remove ADA_DEFAULT=49 option
    The lang/gcc-aux port has recently been removed, so the previously legal
    "49" argument is no longer so.  Adjust ada.mk accordingly by removing
    reference to option 49 and lang/gcc-aux.

--- Mk/Uses/ada.mk
+++ Mk/Uses/ada.mk
@@ -2,11 +2,10 @@
 #
 # Establish Ada-capable compiler as a build dependency
 # To change default compiler, define ADA_DEFAULT in make.conf to 5
-# Note that gcc47-aux is being removed soon, so 47 is not a legal default
 #
 # Feature:      ada
 # Usage:        USES=ada
-# Valid ARGS:   49, 5, 6, run
+# Valid ARGS:   5, 6, run
 #
 # MAINTAINER: marino@FreeBSD.org
 
@@ -16,16 +15,12 @@ _INCLUDE_USES_ADA_MK=    yes
 CC=	ada
 ADAXX=	gcc6	# framework default
 
-. if ${ada_ARGS:M49}
-ADAXX=	gcc
-. elif ${ada_ARGS:M5}
+. if ${ada_ARGS:M5}
 ADAXX=	gcc5
 . elif ${ada_ARGS:M6}
 ADAXX=	gcc6
 . elif defined(ADA_DEFAULT)
-.  if ${ADA_DEFAULT} == 49
-ADAXX=	gcc
-.  elif ${ADA_DEFAULT} == 5
+.  if ${ADA_DEFAULT} == 5
 ADAXX=	gcc5
 .  endif
 . endif
