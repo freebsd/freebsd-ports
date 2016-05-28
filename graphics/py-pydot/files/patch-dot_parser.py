5d5db631f0ff1cf7bb0371a0897227d23234568d
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2016-05-28 17:08:29 +0000

    - Update to 1.1.0 - Change MASTER_SITES back to CHEESESHOP - Remove post-patch:
    Changes:	https://github.com/erocarrera/pydot/blob/master/ChangeLog

--- graphics/py-pydot/files/patch-dot_parser.py
+++ graphics/py-pydot/files/patch-dot_parser.py
@@ -1,32 +1,23 @@
---- dot_parser.py.orig	2012-01-03 00:15:07 UTC
-+++ dot_parser.py
-@@ -25,7 +25,7 @@ from pyparsing import __version__ as pyp
- from pyparsing import ( nestedExpr, Literal, CaselessLiteral, Word, Upcase, OneOrMore, ZeroOrMore,
-     Forward, NotAny, delimitedList, oneOf, Group, Optional, Combine, alphas, nums,
-     restOfLine, cStyleComment, nums, alphanums, printables, empty, quotedString,
--    ParseException, ParseResults, CharsNotIn, _noncomma, dblQuotedString, QuotedString, ParserElement )
-+    ParseException, ParseResults, CharsNotIn, dblQuotedString, QuotedString, ParserElement )
- 
- 
- class P_AttrList:
-@@ -111,7 +111,7 @@ def push_top_graph_stmt(str, loc, toks):
+--- dot_parser.py.orig	2016-05-24 09:57:52.000000000 +0800
++++ dot_parser.py	2016-05-28 01:23:50.843873969 +0800
+@@ -111,7 +111,7 @@
              add_elements(g, element)
-             
+ 
          else:
 -            raise ValueError, "Unknown element statement: %r " % element
 +            raise ValueError("Unknown element statement: %r " % element)
-     
-     
+ 
+ 
      for g in top_graphs:
-@@ -218,14 +218,14 @@ def add_elements(g, toks, defaults_graph
+@@ -218,14 +218,14 @@
                  defaults_edge.update(element.attrs)
  
              else:
 -                raise ValueError, "Unknown DefaultStatement: %s " % element.default_type
 +                raise ValueError("Unknown DefaultStatement: %s " % element.default_type)
-                 
+ 
          elif isinstance(element, P_AttrList):
-         
+ 
              g.obj_dict['attributes'].update(element.attrs)
  
          else:
@@ -34,8 +25,8 @@
 +            raise ValueError("Unknown element statement: %r" % element)
  
  
- def push_graph_stmt(str, loc, toks):            
-@@ -267,7 +267,7 @@ def push_default_stmt(str, loc, toks):
+ def push_graph_stmt(str, loc, toks):
+@@ -267,7 +267,7 @@
      if default_type in ['graph', 'node', 'edge']:
          return DefaultStatement(default_type, attrs)
      else:
@@ -44,21 +35,13 @@
  
  
  def push_attr_list(str, loc, toks):
-@@ -414,6 +414,7 @@ def graph_definition():
-         
-         double_quoted_string = QuotedString('"', multiline=True, unquoteResults=False) # dblQuotedString
- 
-+        _noncomma = "".join( [ c for c in printables if c != "," ] )
-         alphastring_ = OneOrMore(CharsNotIn(_noncomma + ' '))
- 
-         def parse_html(s, loc, toks):
-@@ -523,9 +524,9 @@ def parse_dot_data(data):
+@@ -524,9 +524,9 @@
          else:
              return [g for g in tokens]
-         
+ 
 -    except ParseException, err:
 +    except ParseException as err:
-     
+ 
 -        print err.line
 -        print " "*(err.column-1) + "^"
 -        print err
