a3cea3d941673c56c8c860d08bee729bc05024c1
pi <pi@FreeBSD.org>
2016-05-28 20:09:26 +0000

    net-mgmt/cacti: 0.8.8g -> 0.8.8h
    This is a security update for cacti to resolve SQL exploits.
    - upgrade to 0.8.8h codebase from vendor
    - fix SQL vulnerabilities including CVE-2016-3659
    - fix USE_MYSQL -> USES:mysql
    - fix deprecated mysql php module requirement (use mysqli instead)
    - fix overwriting of failure/recovery dates after outages
    
    PR:		209809
    Submitted by:	Daniel Austin <freebsd-ports@dan.me.uk> (maintainer)
    Security:	CVE-2016-3659
    MFH:		2016Q2

--- net-mgmt/cacti/files/patch-install__index.php
+++ net-mgmt/cacti/files/patch-install__index.php
@@ -1,5 +1,14 @@
---- install/index.php.bak	2015-03-12 03:12:05.409276443 -0700
-+++ install/index.php	2015-03-12 03:14:17.596295374 -0700
+--- install/index.php.orig	2016-05-28 10:54:08.124059773 +0100
++++ install/index.php	2016-05-28 10:54:34.252057818 +0100
+@@ -63,7 +63,7 @@
+ }
+ 
+ function verify_php_extensions() {
+-	$extensions = array("session", "sockets", "mysql", "xml");
++	$extensions = array("session", "sockets", "mysqli", "xml");
+ 	$ok = true;
+ 	$missing_extension = "	<p style='font-family: Verdana, Arial; font-size: 16px; font-weight: bold; color: red;'>Error</p>
+ 							<p style='font-family: Verdana, Arial; font-size: 12px;'>The following PHP extensions are missing:</p><ul>";
 @@ -267,7 +267,7 @@
  if (config_value_exists("path_cactilog")) {
  	$input["path_cactilog"]["default"] = read_config_option("path_cactilog");
