63d39277537b4bdf78ffa40975539f446618b4fe
Ganael LAPLANCHE <martymac@FreeBSD.org>
2016-05-28 18:56:58 +0000

    Update Simgear & Flightgear ports to 2016.2.1

---
+++ devel/simgear/files/patch-simgear-io-DNSClient.hxx
@@ -0,0 +1,10 @@
+--- simgear/io/DNSClient.hxx.orig	2016-05-26 10:14:14.778785357 +0000
++++ simgear/io/DNSClient.hxx	2016-05-26 10:15:10.233184943 +0000
+@@ -27,6 +27,7 @@
+ #include <memory> // for std::auto_ptr
+ #include <string>
+ #include <vector>
++#include <ctime>
+ #include <simgear/structure/SGReferenced.hxx>
+ #include <simgear/structure/SGSharedPtr.hxx>
+ 
