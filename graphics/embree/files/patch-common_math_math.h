0cf74f30a46685b05398abf745adb76df54b97d8
danfe <danfe@FreeBSD.org>
2016-05-29 15:02:03 +0000

    Disable some overloaded math functions for libc++ 3.8.0 and higher, and thus unbreak the build.
    This is because this port defines its own versions of a number of <cmath>
    functions, which cause the ambiguity.  These functions are only declared in
    <cmath> for libc++ 3.8.0 and later, so fix it by disabling the custom
    functions for those versions of libc++.
    
    PR:		209652
    Submitted by:	dim

--- graphics/embree/files/patch-common_math_math.h
+++ graphics/embree/files/patch-common_math_math.h
@@ -1,5 +1,14 @@
 --- common/math/math.h.orig	2016-05-20 05:45:13 UTC
 +++ common/math/math.h
+@@ -83,7 +83,7 @@ namespace embree
+     return _mm_cvtss_f32(c);
+   }
+ 
+-#if !defined(__WIN32__)
++#if !defined(__WIN32__) && (!defined(_LIBCPP_VERSION) || _LIBCPP_VERSION < 3800)
+   __forceinline float abs  ( const float x ) { return ::fabsf(x); }
+   __forceinline float acos ( const float x ) { return ::acosf (x); }
+   __forceinline float asin ( const float x ) { return ::asinf (x); }
 @@ -179,7 +179,7 @@ namespace embree
    }
  #endif
