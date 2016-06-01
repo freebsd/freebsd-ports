62a898637e00dd02ab8d86204c9ae64355833a58
Tilman Keskinoz <arved@FreeBSD.org>
2016-06-01 16:16:22 +0000

    Update to 1.1.3
    PR:		209825
    Submitted by:	maintainer
    Reviewed by:	Vladimir Krstulja

--- games/mangband/files/patch-server_netserver.c
+++ games/mangband/files/patch-server_netserver.c
@@ -1,6 +1,6 @@
---- ./server/netserver.c.orig	2014-08-05 15:15:31.916658572 +0200
-+++ ./server/netserver.c	2014-08-05 15:15:32.006682679 +0200
-@@ -97,7 +97,7 @@
+--- server/netserver.c.orig	2016-05-31 18:04:59 UTC
++++ server/netserver.c
+@@ -97,7 +97,7 @@ static int		(*playing_receive[256])(int 
  			(*drain_receive[256])(int ind);
  int			login_in_progress;
  static int		num_logins, num_logouts;
