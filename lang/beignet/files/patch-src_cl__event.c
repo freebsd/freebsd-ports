ac819b058973f61b26166e06ea1d471934acfa9f
kwm <kwm@FreeBSD.org>
2016-06-02 18:17:00 +0000

    Update beignet to 1.1.2.
    1. Add Broxton support.
    2. Add llvm3.7 support.
    3. Fix event bugs.
    4. Fix the annoying "Failed to release userptr..." error message.
    5. Fix printf bugs.
    
    Changelog: https://01.org/beignet/downloads/beignet-1.1.2-2016-04-19

--- lang/beignet/files/patch-src_cl__event.c
+++ lang/beignet/files/patch-src_cl__event.c
@@ -1,17 +1,23 @@
---- src/cl_event.c.orig	2015-09-24 02:49:06 UTC
-+++ src/cl_event.c
-@@ -27,8 +27,9 @@
+--- src/cl_event.c.orig	2015-12-18 07:57:09.000000000 +0100
++++ src/cl_event.c	2016-05-21 00:04:34.412251000 +0200
+@@ -27,6 +27,7 @@
  
  #include <assert.h>
  #include <stdio.h>
 +#include <time.h>
+ 
+ void cl_event_update_last_events(cl_command_queue queue, int wait)
+ {
+@@ -55,7 +56,7 @@ void cl_event_insert_last_events(cl_comm
+   else set_last_event(queue,event);
+ }
  
 -inline cl_bool
 +static inline cl_bool
  cl_event_is_gpu_command_type(cl_command_type type)
  {
    switch(type) {
-@@ -617,7 +618,7 @@ cl_ulong cl_event_get_cpu_timestamp(cl_u
+@@ -649,7 +650,7 @@ cl_ulong cl_event_get_cpu_timestamp(cl_u
  {
    struct timespec ts;
  
