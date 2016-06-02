2d6cbe226c5f0ed56d330b202efad7e7c86c16c9
cpm <cpm@FreeBSD.org>
2016-06-02 23:38:00 +0000

    - Update to 5.2.1 - Update LICENSE - Add LICENSE_FILE - Fix PORTDOCS - Use post-install-DOCS-on target - Remove .include <bsd.port.options.mk> line - Use do-test target instead regression-test - Remove unneeded patches - Update WWW in pkg-descr
    Reviewed by:	amdmi3 (mentor)
    Approved by:	sbz (maintainer)
    Differential Revision:	D6549

--- textproc/py-feedparser/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
---- ./setup.py.orig	2012-07-03 15:02:11.000000000 +0200
-+++ ./setup.py	2012-07-03 15:03:10.000000000 +0200
-@@ -1,4 +1,4 @@
--from setuptools import setup
-+from distutils.core import setup
- import sys
- 
- extra = {}
