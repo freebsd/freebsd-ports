a8e13e0172f86f884687ee9108f3eb45b3ac136a
mat <mat@FreeBSD.org>
2016-06-04 09:35:21 +0000

    Fix logic a bit, in case pkg is not installed (I guess.)
    PR:		209754
    Reported by:	Nick Evans
    Sponsored by:	Absolight

--- Mk/Scripts/check-vulnerable.sh
+++ Mk/Scripts/check-vulnerable.sh
@@ -24,14 +24,12 @@ if [ -x "${dp_PKG_BIN}" ]; then
 	vlist=$(${dp_PKG_BIN} audit "${dp_PKGNAME}" || :)
 	if [ "${vlist}" = "0 problem(s) in the installed packages found." ]; then
 		vlist=""
+	else
+		${dp_ECHO_MSG} "===>  ${dp_PKGNAME} has known vulnerabilities:"
+		${dp_ECHO_MSG} "$vlist"
+		${dp_ECHO_MSG} "=> Please update your ports tree and try again."
+		${dp_ECHO_MSG} "=> Note: Vulnerable ports are marked as such even if there is no update available."
+		${dp_ECHO_MSG} "=> If you wish to ignore this vulnerability rebuild with 'make DISABLE_VULNERABILITIES=yes'"
+		exit 1
 	fi
-fi
-
-if [ -n "$vlist" ]; then
-	${dp_ECHO_MSG} "===>  ${dp_PKGNAME} has known vulnerabilities:"
-	${dp_ECHO_MSG} "$vlist"
-	${dp_ECHO_MSG} "=> Please update your ports tree and try again."
-	${dp_ECHO_MSG} "=> Note: Vulnerable ports are marked as such even if there is no update available."
-	${dp_ECHO_MSG} "=> If you wish to ignore this vulnerability rebuild with 'make DISABLE_VULNERABILITIES=yes'"
-	exit 1
 fi
