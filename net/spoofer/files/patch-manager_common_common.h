45bb2843f61ea3e96f8a0b0bb588d7d44be4bb56
amdmi3 <amdmi3@FreeBSD.org>
2016-06-06 15:43:55 +0000

    - Fix build by including header needed for time_t - Properly respect compiler and flags when calling child qmake - Document c++11 requirement
    Approved by:	portmgr blanket

---
+++ net/spoofer/files/patch-manager_common_common.h
@@ -0,0 +1,10 @@
+--- manager/common/common.h.orig	2016-05-18 18:09:39 UTC
++++ manager/common/common.h
+@@ -23,6 +23,7 @@
+ 
+ #include "port.h"
+ #include <typeinfo>
++#include <ctime>
+ 
+ #define QT_NO_CAST_FROM_ASCII
+ #include <QString>
