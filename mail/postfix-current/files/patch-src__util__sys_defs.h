1c31181647d2654987abe5ae35306344b2781dbf
ohauer <ohauer@FreeBSD.org>
2016-06-12 19:04:57 +0000

    - update to 3.2-20160612 - fix build with LibreSSL [1]
    20160601
            Cleanup: moved parsing of '!' operators from cidr_match.c
            to dict_cidr.c. Files: util/cidr_match.[hc], util/dict_cidr.c,
            util/match_ops.c.
    
    20160604
            Cleanup: made parsing of '!' operators in regexp and pcre
            tables consistent with cidr tables. Files: util/dict_regexp.c,
            util/dict_pcre.c.
    
    20160605
            Cleanup: integer wrap-around detection in the MySQL and
            PostgreSQL clients. This is totally non-critical because
            Postfix strings are size-limited by design. Files:
            global/dict_mysqql.c, global/dict_pgsql.c.
    
    20160607
            Documentation: dnsblog.
    
    20160609
            Documentation: postsuper(1) manpage text for multiple -[dhH]
            options.  File: postsuper/postsuper.c.
    
    20160611
            Cleanup: Postfix SMTP server local IP address and port
            attributes in the policy delegation protocol (attribute
            names: server_address, server_port), in the Milter protocol
            (macro names: {daemon_addr}, {daemon_port}) and in the
            XCLIENT protocol (attribute names: DESTADDR, DESTPORT).
            Files: proto/MILTER_README.html, proto/SMTPD_POLICY_README.html,
            cleanup/cleanup.h, cleanup/cleanup_milter.c, global/mail_proto.h,
            milter/milter.h, smtpd/smtpd.c, smtpd/smtpd.h, smtpd/smtpd_check.c,
            smtpd/smtpd_haproxy.c, smtpd/smtpd_milter.c, smtpd/smtpd_peer.c.
    
    20140612
            Bugfix: missing server address/port conversion. File:
            smtpd/smtpd_peer.c.
    
    Submitted by:	brnrd@ [1]
    Obtained from:	http://cvsweb.openbsd.org/cgi-bin/cvsweb/ports/mail/postfix/stable/patches/ [1]

--- mail/postfix-current/files/patch-src__util__sys_defs.h
+++ mail/postfix-current/files/patch-src__util__sys_defs.h
@@ -1,10 +1,10 @@
---- src/util/sys_defs.h.orig	2014-12-25 22:33:02 UTC
+--- src/util/sys_defs.h.orig	2016-05-15 15:20:14 UTC
 +++ src/util/sys_defs.h
-@@ -26,6 +26,7 @@
+@@ -27,6 +27,7 @@
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
      || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
 +    || defined(FREEBSD11) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
-     || defined(OPENBSD5) \
+     || defined(OPENBSD5) || defined(OPENBSD6) \
