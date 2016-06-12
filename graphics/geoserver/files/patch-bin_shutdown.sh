ac194885cb36bbb9ae4e6ba6433035c9d10c67ed
bofh <bofh@FreeBSD.org>
2016-06-12 08:39:21 +0000

    graphics/geoserver: Update version 2.8.3=>2.9.0

--- graphics/geoserver/files/patch-bin_shutdown.sh
+++ graphics/geoserver/files/patch-bin_shutdown.sh
@@ -1,27 +1,6 @@
---- bin/shutdown.sh.orig	2015-09-30 16:13:46 UTC
+--- bin/shutdown.sh.orig	2016-05-28 00:30:28 UTC
 +++ bin/shutdown.sh
-@@ -5,20 +5,6 @@
- # $Id$
- # -----------------------------------------------------------------------------
- 
--# Make sure prerequisite environment variables are set
--if [ -z "$JAVA_HOME" ]; then
--  echo "The JAVA_HOME environment variable is not defined"
--  echo "This environment variable is needed to run this program"
--  exit 1
--fi
--if [ ! -r "$JAVA_HOME"/bin/java ]; then
--  echo "The JAVA_HOME environment variable is not defined correctly"
--  echo "This environment variable is needed to run this program"
--  exit 1
--fi
--# Set standard commands for invoking Java.
--_RUNJAVA="$JAVA_HOME"/bin/java
--
- if [ -z $GEOSERVER_HOME ]; then
-   #If GEOSERVER_HOME not set then guess a few locations before giving
-   # up and demanding user set it.
-@@ -52,4 +38,4 @@ if [ ! -r "$GEOSERVER_HOME"/start.jar ];
+@@ -60,4 +60,4 @@ if [ ! -r "$GEOSERVER_HOME"/start.jar ];
  fi
  
  cd "$GEOSERVER_HOME"
