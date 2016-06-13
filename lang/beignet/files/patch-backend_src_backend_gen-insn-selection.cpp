0db79da3d3605e046c81ed2d995ba881082b493b
grembo <grembo@FreeBSD.org>
2016-06-13 18:54:13 +0000

    Fix build on CURRENT.
    This is a bit defensive, static_cast<int> might've cut it as well.
    
    Approved by:	mentors (implicit)

--- lang/beignet/files/patch-backend_src_backend_gen-insn-selection.cpp
+++ lang/beignet/files/patch-backend_src_backend_gen-insn-selection.cpp
@@ -1,6 +1,16 @@
---- backend/src/backend/gen_insn_selection.cpp.orig	2015-08-26 11:57:48.961363000 +0200
-+++ backend/src/backend/gen_insn_selection.cpp	2015-08-26 11:58:15.326657000 +0200
-@@ -1752,11 +1752,11 @@ namespace gbe
+Error: No word lists can be found for the language "en_US".
+--- backend/src/backend/gen_insn_selection.cpp.orig	2015-12-18 07:34:19.000000000 +0000
++++ backend/src/backend/gen_insn_selection.cpp	2016-06-13 17:37:52.540024000 +0000
+@@ -1153,7 +1153,7 @@
+     SelectionInstruction *insn = this->appendInsn(SEL_OP_JMPI, 0, 1);
+     insn->src(0) = src;
+     insn->index = index.value();
+-    insn->extra.longjmp = abs(index - origin) > 800;
++    insn->extra.longjmp = index.value() > origin.value() ? (index - origin) > 800 : (origin - index) > 800;
+     return insn->extra.longjmp ? 2 : 1;
+   }
+ 
+@@ -1752,11 +1752,11 @@
  
    // Boiler plate to initialize the selection library at c++ pre-main
    static SelectionLibrary *selLib = NULL;
