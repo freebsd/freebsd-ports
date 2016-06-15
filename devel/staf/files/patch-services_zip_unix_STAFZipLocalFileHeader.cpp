5993ba73d301aa02fd0c4474ac7ba749d8717808
brd <brd@FreeBSD.org>
2016-06-15 21:21:06 +0000

    Build the zip service in devel/staf
    Submitted by:	asomers
    Approved by:	bdrewery (implicit), kozlov.sergey.404@gmail.com (maintainer)
    Differential Revision:	https://reviews.freebsd.org/D6680

---
+++ devel/staf/files/patch-services_zip_unix_STAFZipLocalFileHeader.cpp
@@ -0,0 +1,10 @@
+--- services/zip/unix/STAFZipLocalFileHeader.cpp.orig	2016-06-01 19:08:10 UTC
++++ services/zip/unix/STAFZipLocalFileHeader.cpp
+@@ -13,7 +13,6 @@
+ #include <map>
+ 
+ #include "zlib.h"
+-#include "zutil.h"
+ 
+ #include "STAFZip.h"
+ #include "STAFZipUtil.h"
