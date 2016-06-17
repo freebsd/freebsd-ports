f1c53e8001e19967dcdf662f60671f4cc86672bb
mat <mat@FreeBSD.org>
2016-06-17 14:58:36 +0000

    Refactor a bit.
    Sponsored by:	Absolight

--- Tools/scripts/indent_make_if.pl
+++ Tools/scripts/indent_make_if.pl
@@ -8,8 +8,15 @@ use warnings;
 
 my $extension = '.orig';
 my $oldargv   = q{};
+my $spaces    = 2;
 my $indent;
 my $argvout;
+
+sub dotindent {
+	my $amount = shift;
+	return '.' . (' ' x ($spaces * $amount));
+}
+
 LINE: while (<>) {
 
 	# For each file, save a .orig backup.
@@ -21,19 +28,19 @@ LINE: while (<>) {
 			($backup = $extension) =~ s/[*]/$ARGV/g;
 		}
 		rename $ARGV, $backup;
-		open $argvout, '>', $ARGV;
+		open $argvout, '>', $ARGV or die "Error for $ARGV: $!";
 		$oldargv = $ARGV;
 		$indent  = 0;
 	}
 
 	if (/^[.]\s*(?:if|for)/o) {    # if/for -> indent and increase indent
-		s/^[.]\s*/"." . "  " x $indent/oe;
+		s/^[.]\s*/dotindent($indent)/oe;
 		$indent++;
 	} elsif (/^[.]\s*end(?:if|for)/o) {    # endif/endfor -> decrease indent and indent
 		$indent--;
-		s/^[.]\s*/"." . "  " x $indent/oe;
+		s/^[.]\s*/dotindent($indent)/oe;
 	} elsif (/^[.]\s*(?:else|elif)/o) {    # else/elif -> indent one level down
-		s/^[.]\s*/"." . "  " x ($indent - 1)/oe;
+		s/^[.]\s*/dotindent($indent-1)/oe;
 	}
 } continue {
 
