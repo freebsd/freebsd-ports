f01553aa77d58e0015d95160874436f5df37504c
antoine <antoine@FreeBSD.org>
2016-06-17 19:32:35 +0000

    Update to 1.5.1

--- security/py-rekall-core/files/patch-setup.py
+++ security/py-rekall-core/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2016-03-28 15:25:48 UTC
+--- setup.py.orig	2016-05-23 16:08:02 UTC
 +++ setup.py
 @@ -46,23 +46,22 @@ def find_data_files(source):
  # approach ensures that any Rekall version will always work as tested - even
@@ -6,34 +6,34 @@
  install_requires = [
 -    "PyAFF4 >= 0.23, < 0.30",
 -    "PyYAML == 3.11",
--    "acora == 1.9",
--    "argparse == 1.2.1",
+-    "acora == 2.0",
+-    "argparse == 1.4.0",
 -    "arrow == 0.7.0",
--    "efilter == 1453815385",
+-    "efilter == 1!1.2",
 -    "intervaltree == 2.1.0",
 -    "pycrypto == 2.6.1",
 -    "pyelftools == 0.23",
--    "pytz == 2015.7",
+-    "pytz == 2016.4",
 -    "rekall-capstone == 3.0.4.post2",
 -    "rekall-yara == 3.4.0.1",
 -    "sortedcontainers == 1.4.4",
 +    "PyAFF4 >= 0.23",
 +    "PyYAML >= 3.11",
-+    "acora >= 1.9",
++    "acora >= 2.0",
 +    "arrow >= 0.7.0",
-+    "efilter >= 1453815385",
++    "efilter >= 1!1.2",
 +    "intervaltree >= 2.1.0",
 +    "pycrypto >= 2.6.1",
 +    "pyelftools >= 0.23",
-+    "pytz >= 2015.7",
++    "pytz >= 2016.4",
 +    "capstone >= 3.0.4",
 +    "yara-python >= 3.4.0",
 +    "sortedcontainers >= 1.4.4",
  
      # Version 2.5.0 is broken with pyinstaller.
      # https://github.com/pyinstaller/pyinstaller/issues/1848
--    "python-dateutil == 2.4.2",
-+    "python-dateutil >= 2.4.2",
+-    "python-dateutil == 2.5.3",
++    "python-dateutil >= 2.5.0",
  ]
  
  
