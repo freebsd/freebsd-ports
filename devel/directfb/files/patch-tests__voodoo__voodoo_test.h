19a137e719fe063a5f8e6c540e6325c911962f58
Dmitry Marakasov <amdmi3@FreeBSD.org>
2016-06-17 15:20:35 +0000

    - Update to 1.4.17 - Unbreak - Switch to options helpers
    PR:		185885
    Submitted by:	hardy.schumacher@gmx.de
    Approved by:	maintainer timeout (anatoly.borodin@gmail.com, 2 years)

--- devel/directfb/files/patch-tests__voodoo__voodoo_test.h
+++ devel/directfb/files/patch-tests__voodoo__voodoo_test.h
@@ -1,5 +1,5 @@
---- ./tests/voodoo/voodoo_test.h.orig	2010-12-03 16:44:12.000000000 +0100
-+++ ./tests/voodoo/voodoo_test.h	2010-12-03 16:44:12.000000000 +0100
+--- tests/voodoo/voodoo_test.h.orig	2016-06-08 05:59:40 UTC
++++ tests/voodoo/voodoo_test.h
 @@ -0,0 +1,10 @@
 +#ifndef __VOODOO_TEST_H__
 +#define __VOODOO_TEST_H__
