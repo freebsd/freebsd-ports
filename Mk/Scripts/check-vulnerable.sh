d8cacceb9dff9123c21078ff654f47f822c4b027
mat <mat@FreeBSD.org>
2016-06-19 11:25:35 +0000

    Reverse tests to decrease complexity.
    PR:		210198
    Submitted by:	mat
    Exp-run by:	antoine
    Sponsored by:	The FreeBSD Foundation, Absolight
    Differential Revision:	https://reviews.freebsd.org/D6779

--- Mk/Scripts/check-vulnerable.sh
+++ Mk/Scripts/check-vulnerable.sh
@@ -20,16 +20,17 @@ if [ "${dp_PORTNAME}" = "pkg" ]; then
 	exit 0
 fi
 
-if [ -x "${dp_PKG_BIN}" ]; then
-	vlist=$(${dp_PKG_BIN} audit "${dp_PKGNAME}" || :)
-	if [ "${vlist}" = "0 problem(s) in the installed packages found." ]; then
-		vlist=""
-	else
-		${dp_ECHO_MSG} "===>  ${dp_PKGNAME} has known vulnerabilities:"
-		${dp_ECHO_MSG} "$vlist"
-		${dp_ECHO_MSG} "=> Please update your ports tree and try again."
-		${dp_ECHO_MSG} "=> Note: Vulnerable ports are marked as such even if there is no update available."
-		${dp_ECHO_MSG} "=> If you wish to ignore this vulnerability rebuild with 'make DISABLE_VULNERABILITIES=yes'"
-		exit 1
-	fi
+if [ ! -x "${dp_PKG_BIN}" ]; then
+	exit 0
+fi
+
+vlist=$(${dp_PKG_BIN} audit "${dp_PKGNAME}" || :)
+
+if [ "${vlist}" != "0 problem(s) in the installed packages found." ]; then
+	${dp_ECHO_MSG} "===>  ${dp_PKGNAME} has known vulnerabilities:"
+	${dp_ECHO_MSG} "$vlist"
+	${dp_ECHO_MSG} "=> Please update your ports tree and try again."
+	${dp_ECHO_MSG} "=> Note: Vulnerable ports are marked as such even if there is no update available."
+	${dp_ECHO_MSG} "=> If you wish to ignore this vulnerability rebuild with 'make DISABLE_VULNERABILITIES=yes'"
+	exit 1
 fi
