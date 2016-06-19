392806adca96594002ff9ffe0a0dbeb1e3911842
Kurt Jaeger <pi@FreeBSD.org>
2016-06-19 03:26:35 +0000

    sysutils/fusefs-encfs: 1.7.4 -> 1.8.1
    - submitter becomes maintainer
    - reverse: re-enable kernel cache (bug #60)
    - reverse mode: disable unique IV by default (was enabled in 1.8)
    - add make benchmark-reverse
    - remove -o default_permissions unless needed to improve performance
    - add option --require-macs (bug #14)
    - add per-file IVs based on the inode number to reverse mode to
      improve security
    - add automatic benchmark (make benchmark)
    - compare MAC in constant time ( fixes bug #12 )
    - add --nocache option
    
    PR:		210196
    Submitted by:	Dmitri Goutnik <dg@syrec.org>

---
+++ sysutils/fusefs-encfs/files/patch-encfs_main.cpp
@@ -0,0 +1,10 @@
+--- encfs/main.cpp.orig	2016-06-18 20:53:13 UTC
++++ encfs/main.cpp
+@@ -27,6 +27,7 @@
+ #include <sys/time.h>
+ #include <cerrno>
+ #include <cstring>
++#include <cstdlib>
+ 
+ #include <getopt.h>
+ 
