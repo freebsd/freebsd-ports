83c3cb065488fa8651fecab1a3c9f4b5b2c34acf
amdmi3 <amdmi3@FreeBSD.org>
2016-06-21 12:09:59 +0000

    - Update to r5775
    PR:		209987
    Submitted by:	pkubaj@anongoth.pl (maintainer)

--- games/eduke32/files/patch-build_src_baselayer.c
+++ games/eduke32/files/patch-build_src_baselayer.c
@@ -1,11 +1,11 @@
---- build/src/baselayer.c.orig	2015-07-25 17:23:17 UTC
+--- build/src/baselayer.c.orig	2016-05-29 21:11:27 UTC
 +++ build/src/baselayer.c
-@@ -219,7 +219,7 @@ void calc_ylookup(int32_t bpl, int32_t l
+@@ -227,7 +227,7 @@ void calc_ylookup(int32_t bpl, int32_t l
      {
          Baligned_free(ylookup);
  
 -        ylookup = (intptr_t *)Xaligned_alloc(16, lastyidx * sizeof(intptr_t));
-+  	 posix_memalign(&ylookup, 16, lastyidx * sizeof(intptr_t));
- #if !defined(NOASM) && !defined(GEKKO) && !defined(__ANDROID__)
-         nx_unprotect((intptr_t)ylookup, (intptr_t)ylookup + (lastyidx * sizeof(intptr_t)));
- #endif
++	posix_memalign(&ylookup, 16, lastyidx * sizeof(intptr_t));
+         ylookupsiz = lastyidx;
+     }
+ 
