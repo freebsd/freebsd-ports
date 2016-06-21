fc253fe958239f23e88b9fc0e2284af4671b9f20
tz <tz@FreeBSD.org>
2016-06-21 08:36:56 +0000

    www/gitlab: Update from 8.7.5 to 8.8.5
    Changelog: https://github.com/gitlabhq/gitlabhq/blob/8-8-stable/CHANGELOG
    
    PR:          209903
    Approved by: swills (mentor)

--- www/gitlab/files/patch-config_initializers_1__settings.rb
+++ www/gitlab/files/patch-config_initializers_1__settings.rb
@@ -1,6 +1,6 @@
---- config/initializers/1_settings.rb.orig	2016-04-22 10:01:37 UTC
+--- config/initializers/1_settings.rb.orig	2016-06-15 01:41:18 UTC
 +++ config/initializers/1_settings.rb
-@@ -167,11 +167,7 @@ Settings.gitlab['email_reply_to'] ||= EN
+@@ -191,12 +191,8 @@ Settings.gitlab['email_reply_to'] ||= EN
  Settings.gitlab['base_url']   ||= Settings.send(:build_base_gitlab_url)
  Settings.gitlab['url']        ||= Settings.send(:build_gitlab_url)
  Settings.gitlab['user']       ||= 'git'
@@ -9,7 +9,9 @@
 -rescue ArgumentError # no user configured
 -  '/home/' + Settings.gitlab['user']
 -end
-+Settings.gitlab['user_home']  ||= '/home/' + Settings.gitlab['user']
- Settings.gitlab['time_zone']  ||= nil
+-Settings.gitlab['time_zone'] ||= nil
++Settings.gitlab['user_home']  ||= '/usr/home/' + Settings.gitlab['user']
++Settings.gitlab['time_zone']  ||= nil
  Settings.gitlab['signup_enabled'] ||= true if Settings.gitlab['signup_enabled'].nil?
  Settings.gitlab['signin_enabled'] ||= true if Settings.gitlab['signin_enabled'].nil?
+ Settings.gitlab['restricted_visibility_levels'] = Settings.send(:verify_constant_array, Gitlab::VisibilityLevel, Settings.gitlab['restricted_visibility_levels'], [])
