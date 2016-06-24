596e13c6f3d94b4420f8c4ca60271747a31cf2b9
Dmitry Sivachenko <demon@FreeBSD.org>
2016-06-24 13:48:31 +0000

    Update to version 3.3.0.
    PR:		210140
    Maintainer timeout: 16 days

--- sysutils/py-supervisor/files/patch-supervisor_options.py
+++ sysutils/py-supervisor/files/patch-supervisor_options.py
@@ -1,14 +1,17 @@
---- supervisor/options.py.orig	2013-06-20 21:00:12.000000000 +0400
-+++ supervisor/options.py	2013-08-08 14:41:08.000000000 +0400
-@@ -99,10 +99,7 @@
+--- supervisor/options.py.orig	2016-06-08 17:09:41.213297000 +0300
++++ supervisor/options.py	2016-06-08 17:10:18.970354000 +0300
+@@ -96,13 +96,7 @@ class Options:
          self.add("configfile", None, "c:", "configuration=")
  
          here = os.path.dirname(os.path.dirname(sys.argv[0]))
 -        searchpaths = [os.path.join(here, 'etc', 'supervisord.conf'),
 -                       os.path.join(here, 'supervisord.conf'),
--                       'supervisord.conf', 'etc/supervisord.conf',
--                       '/etc/supervisord.conf']
-+        searchpaths = [ '/usr/local/etc/supervisord.conf' ]
+-                       'supervisord.conf',
+-                       'etc/supervisord.conf',
+-                       '/etc/supervisord.conf',
+-                       '/etc/supervisor/supervisord.conf',
+-                       ]
++        searchpaths = [ '%%PREFIX%%/etc/supervisord.conf' ]
          self.searchpaths = searchpaths
  
-     def default_configfile(self):
+         self.environ_expansions = {}
