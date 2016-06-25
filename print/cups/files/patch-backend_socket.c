50ed08f39a3ac9f22af000e3b14e55c1a29dee3d
Tijl Coosemans <tijl@FreeBSD.org>
2016-06-25 15:41:38 +0000

    - Update to 2.1.4. - Let the socket backend print debug messages on the DEBUG channel.  The   other channels affect the printer status message in lpstat(1) which can   be confusing. [1]
    PR:		203058 [1]

---
+++ print/cups/files/patch-backend_socket.c
@@ -0,0 +1,36 @@
+--- backend/socket.c.orig	2014-06-09 18:35:32 UTC
++++ backend/socket.c
+@@ -316,7 +316,6 @@ main(int  argc,				/* I - Number of comm
+   */
+ 
+   fprintf(stderr, "DEBUG: Connecting to %s:%d\n", hostname, port);
+-  _cupsLangPrintFilter(stderr, "INFO", _("Connecting to printer."));
+ 
+   for (delay = 5;;)
+   {
+@@ -397,7 +396,6 @@ main(int  argc,				/* I - Number of comm
+   }
+ 
+   fputs("STATE: -connecting-to-device\n", stderr);
+-  _cupsLangPrintFilter(stderr, "INFO", _("Connected to printer."));
+ 
+   fprintf(stderr, "DEBUG: Connected to %s:%d...\n",
+ 	  httpAddrString(&(addr->addr), addrname, sizeof(addrname)),
+@@ -426,7 +424,7 @@ main(int  argc,				/* I - Number of comm
+                             0, backendNetworkSideCB);
+ 
+     if (print_fd != 0 && tbytes >= 0)
+-      _cupsLangPrintFilter(stderr, "INFO", _("Print file sent."));
++      fputs("DEBUG: Print file sent.\n", stderr);
+   }
+ 
+   fputs("STATE: +cups-waiting-for-job-completed\n", stderr);
+@@ -437,7 +435,7 @@ main(int  argc,				/* I - Number of comm
+     * Shutdown the socket and wait for the other end to finish...
+     */
+ 
+-    _cupsLangPrintFilter(stderr, "INFO", _("Waiting for printer to finish."));
++    fputs("DEBUG: Waiting for printer to finish.\n", stderr);
+ 
+     shutdown(device_fd, 1);
+ 
