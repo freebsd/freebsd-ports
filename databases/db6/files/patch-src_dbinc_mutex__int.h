4d72e69d667b576cc504ecbe6e106f2aed645e9b
sunpoet <sunpoet@FreeBSD.org>
2016-06-26 18:01:59 +0000

    - Revert WRKSRC_SUBDIR change in r417590 which breaks make makepatch
    Notified by:	mat

--- databases/db6/files/patch-src_dbinc_mutex__int.h
+++ databases/db6/files/patch-src_dbinc_mutex__int.h
@@ -1,5 +1,5 @@
---- ../src/dbinc/mutex_int.h.orig	2016-03-28 19:45:54 UTC
-+++ ../src/dbinc/mutex_int.h
+--- src/dbinc/mutex_int.h.orig	2016-03-28 19:45:54 UTC
++++ src/dbinc/mutex_int.h
 @@ -652,7 +652,7 @@ MUTEX_SET(int *tsl)  {
  	 : "=&r" (__r), "+r" (tsl)
  	 :
