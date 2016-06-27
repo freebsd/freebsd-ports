e247f368232d3779cc988b773d1508051d2dbed6
madpilot <madpilot@FreeBSD.org>
2016-06-27 20:01:42 +0000

    Update to 2.60.0.

--- deskutils/calibre/files/patch-setup_build__environment.py
+++ deskutils/calibre/files/patch-setup_build__environment.py
@@ -1,11 +1,11 @@
---- setup/build_environment.py.orig	2016-05-20 11:45:27 UTC
+--- setup/build_environment.py.orig	2016-06-24 01:54:27 UTC
 +++ setup/build_environment.py
-@@ -103,7 +103,7 @@ def get_sip_dir(q):
+@@ -98,7 +98,7 @@ pyqt['sip_bin'] = os.environ.get('SIP_BI
+ from PyQt5.QtCore import PYQT_CONFIGURATION
+ pyqt['sip_flags'] = PYQT_CONFIGURATION['sip_flags']
+ def get_sip_dir():
+-    q = os.environ.get('SIP_DIR', sys.prefix if iswindows else os.path.join(sys.prefix, 'share', 'sip'))
++    q = os.environ.get('SIP_DIR', sys.prefix if iswindows else os.path.join(sys.prefix, 'share', 'py-sip'))
+     for x in ('', 'Py2-PyQt5', 'PyQt5', 'sip/PyQt5'):
+         base = os.path.join(q, x)
          if os.path.exists(os.path.join(base, 'QtWidgets')):
-             return base
-     raise EnvironmentError('Failed to find the location of the PyQt5 .sip files')
--pyqt['pyqt_sip_dir'] = get_sip_dir(sys.prefix if iswindows else os.path.join(sys.prefix, 'share', 'sip'))
-+pyqt['pyqt_sip_dir'] = get_sip_dir(sys.prefix if iswindows else os.path.join(sys.prefix, 'share', 'py-sip'))
- pyqt['sip_inc_dir'] = sysconfig.get_path('include')
- 
- glib_flags = subprocess.check_output([PKGCONFIG, '--libs', 'glib-2.0']).strip() if islinux else ''
