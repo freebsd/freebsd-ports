5be1ac2b13621b8efd1cee4b264934e909285574
Hajimu UMEMOTO <ume@FreeBSD.org>
2016-06-27 10:07:14 +0000

    Add new port -- YubiKey PIV Manager
    Tool for configuring your PIV-enabled YubiKey.

---
+++ security/yubikey-piv-manager/files/patch-pivman_yubicommon_setup_qt.py
@@ -0,0 +1,10 @@
+--- pivman/yubicommon/setup/qt.py.orig	2015-11-10 13:48:09 UTC
++++ pivman/yubicommon/setup/qt.py
+@@ -32,7 +32,6 @@ from distutils.errors import DistutilsSe
+ import os
+ 
+ 
+-__dependencies__ = ['PySide']
+ __all__ = ['qt_resources']
+ 
+ 
