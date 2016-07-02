dbb48f206cf50960111eada776be627692094fcb
pi <pi@FreeBSD.org>
2016-07-02 03:10:14 +0000

    www/links: 2.9 -> 2.13
    - submitter becomes maintainer
    - patches now generated by make makepatch
    
    PR:		210744
    Changes:	http://links.twibright.com/download/ChangeLog
    Submitted by:	Chris Hutchinson <portmaster@bsdforge.com>

--- www/links/files/patch-default.c
+++ www/links/files/patch-default.c
@@ -1,7 +1,7 @@
---- default.c.orig	2008-03-06 22:43:25.117856165 -0500
-+++ default.c	2008-03-06 22:44:12.412560939 -0500
-@@ -400,7 +400,7 @@
- 	get_system_name();
+--- default.c.orig	2016-06-17 20:33:57 UTC
++++ default.c
+@@ -829,7 +829,7 @@ void init_home(void)
+ 	get_compiler_name();
  	links_home = get_home(&first_use);
  	if (!links_home) {
 -		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have $HOME variable set correctly and that you have write permission to your home directory.\n\007");
