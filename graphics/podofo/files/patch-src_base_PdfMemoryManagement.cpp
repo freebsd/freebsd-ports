fba93ad2e4c7abe2e6c735dda2d1ae202f123928
pawel <pawel@FreeBSD.org>
2016-07-03 19:34:37 +0000

    - Fix SIZE_MAX buildfix patch, use other method now upstreamed - Convert to USES=ssl - Group IMPOSE options helpers together

--- graphics/podofo/files/patch-src_base_PdfMemoryManagement.cpp
+++ graphics/podofo/files/patch-src_base_PdfMemoryManagement.cpp
@@ -1,17 +1,19 @@
---- src/base/PdfMemoryManagement.cpp.orig	2016-06-16 16:10:23 UTC
+--- src/base/PdfMemoryManagement.cpp.orig	2016-07-03 09:12:06 UTC
 +++ src/base/PdfMemoryManagement.cpp
-@@ -44,6 +44,14 @@
- #endif
- #endif // _WIN32
+@@ -34,15 +34,9 @@
+ #include "PdfMemoryManagement.h"
+ #include "PdfDefinesPrivate.h"
  
-+#ifdef __FreeBSD__
-+#if __WORDSIZE == 64
-+#define SIZE_MAX UINT64_MAX
-+#else
-+#define SIZE_MAX UINT_MAX
-+#endif
-+#endif
-+
+-#ifdef _WIN32
+ #ifndef SIZE_MAX
+-#ifdef _WIN64
+-#define SIZE_MAX _UI64_MAX
+-#else
+-#define SIZE_MAX UINT_MAX
+-#endif
++#define SIZE_MAX std::numeric_limits<size_t>::max()
+ #endif
+-#endif // _WIN32
+ 
  #if defined(_MSC_VER) &&  ( _MSC_VER <= 1200 )
  // errno.h isn't available in Visual C++ 6 (definitions are in stdlib.h which is already included)
- #else
