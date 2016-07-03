c2558fedf41aad08120d386c75235d99f22984c6
woodsb02 <woodsb02@FreeBSD.org>
2016-07-03 21:40:02 +0000

    audio/lastfm-desktop: Update to 2.1.36 - Move to official GitHub repo, since the official CDN is out of date - Patch of LAV_Source.cpp renamed to follow file move, and updated to   fix build with FFmpeg 3.0.x - obtained from ArchLinx [1].   Note that multimedia/ffmpeg is currently still on the 2.8.x branch,   however this patch is backwards compatible with ffmpeg 2.8.x. - Patch of RadioService.cpp updated - obtained from ArchLinux [2]. - Remove patch of podDevice_linux.cpp, as the change was upstreamed - Add additional dependencies to LIB_DEPENDS and USE_QT4 as found by   new stage-qa script. - Ensure program will appear in desktop manager menus by installing icon   and adding DESKTOP_ENTRIES. - Add the audio playback run dependencies, with the option of   either VLC or GSTREAMER. Without one of these, it crashes upon run. - Remove the LD_LIBRARY_PATH, as it is no longer needed now that the   lastfm-desktop binaries are statically linked to it's private libraries [3]. - Also do not install the *.a static libraries, as they are not used by any other   programs/ports and not needed by the lastfm-desktop binaries. - Install all of the files required by for the .css stylesheet to work.
    [1] https://aur.archlinux.org/cgit/aur.git/tree/LAV_Source_fix.patch?h=lastfm
    [2] https://aur.archlinux.org/cgit/aur.git/tree/cast-bug.patch?h=lastfm
    [3] https://github.com/lastfm/lastfm-desktop/commit/a6d2225e368aaeb66ec5345717120013b40f2518
    
    Reviewed by:	mat (mentor)
    Approved by:	vg (maintainer timeout), adamw (mentor)
    Differential Revision:	https://reviews.freebsd.org/D6874

--- audio/lastfm-desktop/files/patch-app_client_Services_RadioService_RadioService.cpp
+++ audio/lastfm-desktop/files/patch-app_client_Services_RadioService_RadioService.cpp
@@ -1,11 +1,11 @@
---- ./app/client/Services/RadioService/RadioService.cpp.orig	2014-08-29 11:45:14.000000000 +0200
-+++ ./app/client/Services/RadioService/RadioService.cpp	2014-08-29 11:45:43.000000000 +0200
-@@ -309,7 +309,7 @@
+--- app/client/Services/RadioService/RadioService.cpp.orig	2016-06-16 20:32:08 UTC
++++ app/client/Services/RadioService/RadioService.cpp
+@@ -307,7 +307,7 @@ RadioService::mute()
  void
  RadioService::onPhononStateChanged( Phonon::State newstate, Phonon::State oldstate )
  {
 -    qDebug() << oldstate << " -> " << newstate;
-+    //qDebug() << oldstate << " -> " << newstate;
++    qDebug() << (int)oldstate << " -> " << (int)newstate;
      if (m_mediaObject == 0) {
          qDebug() << "m_mediaObject is null!";
          return;
