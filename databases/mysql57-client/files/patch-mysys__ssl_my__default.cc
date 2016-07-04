461f0327abb01d1876598ccde96d36590fa938c7
riggs <riggs@FreeBSD.org>
2016-07-04 14:59:15 +0000

    Make my.cnf path correct according to hier(7), bump PORTREVISION
    PR:		209579
    Submitted by:	mokhi64@gmail.com
    Reviewed by:	rootservice@gmail.com

--- databases/mysql57-client/files/patch-mysys__ssl_my__default.cc
+++ databases/mysql57-client/files/patch-mysys__ssl_my__default.cc
@@ -9,18 +9,48 @@
  #define DEFAULT_DIRS_SIZE (MAX_DEFAULT_DIRS + 1)  /* Terminate with NULL */
  static const char **default_directories = NULL;
  
-@@ -1409,7 +1409,10 @@ static const char **init_default_directo
+@@ -909,6 +909,14 @@
+       return 1;                                 /* Ignore wrong files */
+   }
  
- #if defined(DEFAULT_SYSCONFDIR)
-   if (DEFAULT_SYSCONFDIR[0])
++  if (strstr(name, "/etc") == name)
 +  {
-     errors += add_directory(alloc, DEFAULT_SYSCONFDIR, dirs);
-+    errors += add_directory(alloc, DEFAULT_SYSCONFDIR "/mysql", dirs);
++    fprintf(stderr,
++             "error: Config file %s in invalid location, please move to or merge with /usr/local%s\n",
++             name,name);
++      goto err;
 +  }
- #endif /* DEFAULT_SYSCONFDIR */
++
+   while (mysql_file_getline(buff, sizeof(buff) - 1, fp))
+   {
+     line++;
+@@ -1245,7 +1253,8 @@
+             end[(strlen(end)-1)] = ' ';
+           else
+             strxmov(end, conf_file, *ext , " ",  NullS);
+-          fputs(name, stdout);
++          if (strstr(name, "/etc") != name)
++            fputs(name, stdout);
+         }
+       }
+     }
+@@ -1404,13 +1413,8 @@
+ 
+ #else
+ 
+-  errors += add_directory(alloc, "/etc/", dirs);
+-  errors += add_directory(alloc, "/etc/mysql/", dirs);
+-
+-#if defined(DEFAULT_SYSCONFDIR)
+-  if (DEFAULT_SYSCONFDIR[0])
+-    errors += add_directory(alloc, DEFAULT_SYSCONFDIR, dirs);
+-#endif /* DEFAULT_SYSCONFDIR */
++  errors += add_directory(alloc, "/usr/local/etc/", dirs);
++  errors += add_directory(alloc, "/usr/local/etc/mysql/", dirs);
  
  #endif
-@@ -1480,7 +1483,7 @@ int check_file_permissions(const char *f
+ 
+@@ -1480,7 +1484,7 @@
    MY_STAT stat_info;
  
    if (!my_stat(file_name,&stat_info,MYF(0)))
