566fe2dd8d2c5e474509b8fde66cb5138f423d0e
Dmitry Marakasov <amdmi3@FreeBSD.org>
2016-07-06 09:00:07 +0000

    - Switch to options helpers - Regenerate patches with `make makepatch`

--- net/ccxstream/files/patch-ccxclientconn.c
+++ net/ccxstream/files/patch-ccxclientconn.c
@@ -1,6 +1,6 @@
---- ccxclientconn.c.orig	Mon Mar 17 11:29:15 2003
-+++ ccxclientconn.c	Tue May 15 14:58:52 2007
-@@ -51,6 +51,11 @@
+--- ccxclientconn.c.orig	2003-03-17 14:29:15 UTC
++++ ccxclientconn.c
+@@ -51,6 +51,11 @@ static void cc_xstream_client_socket_set
    i = 1;
    setsockopt(sock, IPPROTO_TCP,TCP_NODELAY, &i, sizeof (i));
  #endif /* TCP_NODELAY */
