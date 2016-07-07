7021f1ec0e8acb5dcc0344c741d0ddfe41639d28
Joe Marcus Clarke <marcus@FreeBSD.org>
2016-07-07 17:42:25 +0000

    Update to 2.17.3.
    This release fixes a bug in newer versions of Perl where '{' and '}' caused
    regexp compilation to fail.
    
    PR:		210809
    Submitted by:	mat

--- ports-mgmt/portlint/src/portlint.pl
+++ ports-mgmt/portlint/src/portlint.pl
@@ -15,7 +15,7 @@
 # was removed.
 #
 # $FreeBSD$
-# $MCom: portlint/portlint.pl,v 1.388 2016/05/15 18:42:34 jclarke Exp $
+# $MCom: portlint/portlint.pl,v 1.390 2016/07/07 17:40:43 jclarke Exp $
 #
 
 use strict;
@@ -50,7 +50,7 @@ $portdir = '.';
 # version variables
 my $major = 2;
 my $minor = 17;
-my $micro = 2;
+my $micro = 3;
 
 # default setting - for FreeBSD
 my $portsdir = '/usr/ports';
@@ -2005,8 +2005,8 @@ xargs xmkmf
 	my %pathnames = ();
 	print "OK: checking for paths that have macro replacements.\n"
 		if ($verbose);
-	$pathnames{'${PREFIX}/share/java/classes'} = 'JAVADIR';
-	$pathnames{'${PREFIX}/share/java'} = 'JAVASHAREDIR';
+	$pathnames{'$\{PREFIX\}/share/java/classes'} = 'JAVADIR';
+	$pathnames{'$\{PREFIX\}/share/java'} = 'JAVASHAREDIR';
 	foreach my $i (keys %pathnames) {
 		my $lineno = &linenumber($`);
 		if ($j =~ m|$i|gm) {
