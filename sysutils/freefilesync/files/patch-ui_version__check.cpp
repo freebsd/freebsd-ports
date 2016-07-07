886fd696ae3c71ed32ee407d1f0fe45a78cce7ad
kevlo <kevlo@FreeBSD.org>
2016-07-07 06:51:25 +0000

    Update to 8.2

--- sysutils/freefilesync/files/patch-ui_version__check.cpp
+++ sysutils/freefilesync/files/patch-ui_version__check.cpp
@@ -1,23 +1,22 @@
---- ui/version_check.cpp.orig	2016-03-09 02:07:44 UTC
-+++ ui/version_check.cpp
-@@ -112,17 +112,10 @@ std::string geHttpPostParameters() //mus
+--- ui/version_check.cpp.orig	2016-06-30 13:33:44.614966000 +0800
++++ ui/version_check.cpp	2016-06-30 13:36:17.540454000 +0800
+@@ -104,16 +104,10 @@
      const auto osvMinor = getOsVersion().minor;
  
  #elif defined ZEN_LINUX
--    params += "&os_name=Linux";
--    assert(std::this_thread::get_id() == mainThreadId);
--
+-    params.emplace_back("os_name", "Linux");
++    params.emplace_back("os_name", "FreeBSD");
+ 
 -    const wxLinuxDistributionInfo distribInfo = wxGetLinuxDistributionInfo();
 -    assert(contains(distribInfo.Release, L'.'));
 -    std::vector<wxString> digits = split<wxString>(distribInfo.Release, L'.'); //e.g. "15.04"
 -    digits.resize(2);
 -    //distribInfo.Id //e.g. "Ubuntu"
-+    params += "&os_name=FreeBSD";
- 
+-
 -    const int osvMajor = stringTo<int>(digits[0]);
 -    const int osvMinor = stringTo<int>(digits[1]);
 +    const int osvMajor = 0;
 +    const int osvMinor = 0;
  
  #elif defined ZEN_MAC
-     params += "&os_name=Mac";
+     params.emplace_back("os_name", "Mac");
