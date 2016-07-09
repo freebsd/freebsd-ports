8a9bc098415dbd6fc99396c5bd161c8fbf0459bb
woodsb02 <woodsb02@FreeBSD.org>
2016-07-09 00:00:18 +0000

    graphics/tulip: Update to 4.9.0 - Add USE_GL+=gl as per new stage-qa dependency detection script - Refactor patches to apply cleanly against new release
    Approved by:	adamw (mentor)
    Relnotes:	http://tulip.labri.fr/TulipDrupal/?q=node/2141
    Differential Revision:	https://reviews.freebsd.org/D7164

--- graphics/tulip/files/patch-software_crash__handling_CrashHandling.cpp
+++ graphics/tulip/files/patch-software_crash__handling_CrashHandling.cpp
@@ -1,11 +1,11 @@
---- software/crash_handling/CrashHandling.cpp.orig	2015-04-18 20:15:52 UTC
+--- software/crash_handling/CrashHandling.cpp.orig	2016-07-07 20:15:39 UTC
 +++ software/crash_handling/CrashHandling.cpp
-@@ -48,7 +48,7 @@ void setExtraSymbolsSearchPaths(const st
+@@ -78,7 +78,7 @@ static void dumpStackTrace(StackWalker &
  /*
    Linux/MacOS-specific handling
   */
 -#if defined(__unix__) || defined(__APPLE__)
 +#if (defined(__unix__) || defined(__APPLE__)) && !defined(__FreeBSD__)
  
- # if defined(__i386__) || defined(__amd64__)
+ #if defined(__i386__) || defined(__amd64__)
  
