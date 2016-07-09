0d119bc2efad82c23020997ec371c82ba6d0d0b4
woodsb02 <woodsb02@FreeBSD.org>
2016-07-09 00:00:18 +0000

    graphics/tulip: Update to 4.9.0 - Add USE_GL+=gl as per new stage-qa dependency detection script - Refactor patches to apply cleanly against new release
    Approved by:	adamw (mentor)
    Relnotes:	http://tulip.labri.fr/TulipDrupal/?q=node/2141
    Differential Revision:	https://reviews.freebsd.org/D7164

--- graphics/tulip/files/patch-software_tulip_src_main.cpp
+++ graphics/tulip/files/patch-software_tulip_src_main.cpp
@@ -1,11 +1,11 @@
---- software/tulip/src/main.cpp.orig	2015-04-22 14:45:04 UTC
+--- software/tulip/src/main.cpp.orig	2016-07-07 20:15:40 UTC
 +++ software/tulip/src/main.cpp
 @@ -124,7 +124,9 @@ void checkTulipRunning(const QString& pe
  }
  
  int main(int argc, char **argv) {
 +#ifndef __FreeBSD__
-   start_crash_handler();
+   CrashHandling::installCrashHandler();
 +#endif
  
    QApplication tulip_agent(argc, argv);
