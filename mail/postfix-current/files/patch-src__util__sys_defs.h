91918d80576b12d3773d9eadd41e860f00d09482
ohauer <ohauer@FreeBSD.org>
2016-07-09 20:18:29 +0000

    - fix build on FreeBSD 12 - s/USE_OPENSSL=yes/USES=ssl/

--- mail/postfix-current/files/patch-src__util__sys_defs.h
+++ mail/postfix-current/files/patch-src__util__sys_defs.h
@@ -1,10 +1,10 @@
---- src/util/sys_defs.h.orig	2016-05-15 15:20:14 UTC
+--- src/util/sys_defs.h.orig	2016-07-09 20:09:34 UTC
 +++ src/util/sys_defs.h
 @@ -27,6 +27,7 @@
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
      || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
-+    || defined(FREEBSD11) \
++    || defined(FREEBSD11) || defined(FREEBSD12) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(OPENBSD5) || defined(OPENBSD6) \
