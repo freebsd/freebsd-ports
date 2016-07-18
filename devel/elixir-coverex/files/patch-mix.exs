0a6345c955344e442a487e36953042da54be68ab
vanilla <vanilla@FreeBSD.org>
2016-07-18 02:10:34 +0000

    Update to 1.4.10.

--- devel/elixir-coverex/files/patch-mix.exs
+++ devel/elixir-coverex/files/patch-mix.exs
@@ -1,11 +1,11 @@
---- mix.exs.orig	2015-11-18 09:50:28 UTC
+--- mix.exs.orig	2016-07-16 12:12:32 UTC
 +++ mix.exs
 @@ -13,8 +13,7 @@ defmodule Coverex.Mixfile do
       homepage_url: "https://github.com/alfert/coverex",
       docs: [readme: "README.md"],
-      description: description,
+      description: description(),
 -     test_coverage: [tool: Coverex.Task, coveralls: true, ignore_modules: []],
--     deps: deps]
+-     deps: deps()]
 +     test_coverage: [tool: Coverex.Task, coveralls: true, ignore_modules: []]]
    end
  
