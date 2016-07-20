60b7b4ef47ea3dc9d209fb840282547e038ff9c9
jpaetzel <jpaetzel@FreeBSD.org>
2016-07-20 15:36:05 +0000

    Update iozone to build on HEAD and take maintainership
    Sponsored by:	iXsystems

--- benchmarks/iozone/files/patch-pit__server.c
+++ benchmarks/iozone/files/patch-pit__server.c
@@ -1,12 +1,15 @@
---- pit_server.c	2015-10-20 14:12:13.000000000 +0000
+--- pit_server.c.orig	2015-10-20 14:12:13 UTC
 +++ pit_server.c
-@@ -171,5 +171,4 @@
+@@ -170,7 +170,6 @@ int main( int   argc,
+    if(need < 1)
     {
  	USAGE;
 -	exit;
     }
     /*
-@@ -311,6 +310,6 @@
+    ** Open both a TCP and UDP socket, for both IPv4 & IPv6, on which to receive
+@@ -310,8 +309,8 @@ static int openSckt( const char *service
+                   "   ai_family    = %d (PF_INET = %d, PF_INET6 = %d)\n"
                    "   ai_socktype  = %d (SOCK_STREAM = %d, SOCK_DGRAM = %d)\n"
                    "   ai_protocol  = %d (IPPROTO_TCP = %d, IPPROTO_UDP = %d)\n"
 -                  "   ai_addrlen   = %d (sockaddr_in = %lu, "
@@ -15,7 +18,9 @@
 +                  "sockaddr_in6 = %zu)\n",
                    ai->ai_flags,
                    ai->ai_family,
-@@ -621,6 +620,6 @@
+                   PF_INET,
+@@ -620,8 +619,8 @@ static void pit( int    tSckt[ ],
+                fprintf( stderr,
                          "Sockaddr info for new TCP client:\n"
                          "   sa_family = %d (AF_INET = %d, AF_INET6 = %d)\n"
 -                        "   addr len  = %d (sockaddr_in = %lu, "
@@ -24,14 +29,18 @@
 +                        "sockaddr_in6 = %zu)\n",
                          sadr->sa_family,
                          AF_INET,
-@@ -729,5 +728,5 @@
+                         AF_INET6,
+@@ -728,7 +727,7 @@ static void pit( int    tSckt[ ],
+             {
                 ssize_t rBytes = count;
                 fprintf( stderr,
 -                        "%s: UDP datagram received (%ld bytes).\n",
 +                        "%s: UDP datagram received (%zd bytes).\n",
                          pgmName,
                          count );
-@@ -746,6 +745,6 @@
+                while ( count > 0 )
+@@ -745,8 +744,8 @@ static void pit( int    tSckt[ ],
+                fprintf( stderr,
                          "Remote client's sockaddr info:\n"
                          "   sa_family = %d (AF_INET = %d, AF_INET6 = %d)\n"
 -                        "   addr len  = %d (sockaddr_in = %lu, "
@@ -40,3 +49,4 @@
 +                        "sockaddr_in6 = %zu)\n",
                          sadr->sa_family,
                          AF_INET,
+                         AF_INET6,
