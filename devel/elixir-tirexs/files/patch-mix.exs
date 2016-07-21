5d163fd3f72e923484da55f99233b5c70df8b119
olgeni <olgeni@FreeBSD.org>
2016-07-21 14:06:28 +0000

    Add devel/elixir-tirexs, an Elixir flavored HTTP client and DSL library for ElasticSearch.

---
+++ devel/elixir-tirexs/files/patch-mix.exs
@@ -0,0 +1,14 @@
+--- mix.exs.orig	2016-07-14 15:23:49 UTC
++++ mix.exs
+@@ -8,10 +8,9 @@ defmodule Tirexs.Mixfile do
+       version: "0.8.3",
+       source_url: github,
+       homepage_url: github,
+-      elixir: "~> 1.2.0",
++      elixir: "~> 1.3.0",
+       description: description,
+       package: package,
+-      deps: deps,
+       docs: [extras: ["README.md", "CONTRIBUTING.md"]]
+     ]
+   end
