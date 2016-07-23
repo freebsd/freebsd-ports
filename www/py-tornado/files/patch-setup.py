7b3f674b014bf09b11870f57000c401b1b10b955
gblach <gblach@FreeBSD.org>
2016-07-23 18:31:55 +0000

    Update to 4.4.1

--- www/py-tornado/files/patch-setup.py
+++ www/py-tornado/files/patch-setup.py
@@ -1,14 +1,5 @@
 --- setup.py.orig	2015-11-06 20:08:26 UTC
 +++ setup.py
-@@ -125,7 +125,7 @@ if setuptools is not None:
-     if sys.version_info < (2, 7):
-         # Only needed indirectly, for singledispatch.
-         install_requires.append('ordereddict')
--    if sys.version_info < (3, 2):
-+    if sys.version_info < (2, 7, 9):
-         install_requires.append('backports.ssl_match_hostname')
-     if sys.version_info < (3, 4):
-         install_requires.append('singledispatch')
 @@ -147,9 +147,6 @@ setup(
          # in the sdist tarball)
          "tornado.test": [
