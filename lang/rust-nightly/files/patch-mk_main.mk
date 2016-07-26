faac9a5dab2dac6ec9bff40c26f6ce06dbabe38a
dumbbell <dumbbell@FreeBSD.org>
2016-07-26 07:39:29 +0000

    lang/rust-nightly: Update to 1.12.0; snapshot from 2016-07-17
    Reviewed by:	asomers, jbeich, riggs
    Approved by:	jbeich, riggs
    Differential Revision:	https://reviews.freebsd.org/D7275

--- lang/rust-nightly/files/patch-mk_main.mk
+++ lang/rust-nightly/files/patch-mk_main.mk
@@ -1,6 +1,6 @@
---- mk/main.mk.orig	2015-09-03 11:50:44 UTC
+--- mk/main.mk.orig	2016-07-17 07:06:29 UTC
 +++ mk/main.mk
-@@ -107,7 +107,7 @@ ifneq ($(wildcard $(NON_BUILD_TARGET)),)
+@@ -135,7 +135,7 @@ ifneq ($(wildcard $(NON_BUILD_TARGET)),)
  CFG_INFO := $(info cfg: non-build target triples $(NON_BUILD_TARGET))
  endif
  
