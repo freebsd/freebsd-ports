3c4a170b07d0e40bc61f8df70fa8ddc462490f42
Baptiste Daroussin <bapt@FreeBSD.org>
2016-07-29 23:02:48 +0000

    Fix collision with dprintf(3)
    While here, regenerate patches and move some post-patch into an already existing
    patch

--- devel/elfsh/files/patch-vm_readln.c
+++ devel/elfsh/files/patch-vm_readln.c
@@ -1,6 +1,6 @@
---- vm/readln.c	Wed Aug 13 16:31:55 2003
-+++ vm/readln.c.patch	Sun Sep 14 00:58:08 2003
-@@ -43,7 +43,7 @@
+--- vm/readln.c.orig	2003-08-21 01:11:03 UTC
++++ vm/readln.c
+@@ -43,7 +43,7 @@ char** coustom_completion(const char* te
    char** matches=(char**) NULL;
    
    if (start == 0)
