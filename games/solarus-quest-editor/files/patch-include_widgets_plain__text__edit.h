d2c66c5cfc9edd0277160ddd422dca5cbf7ff41d
danilo <danilo@FreeBSD.org>
2016-07-30 22:33:20 +0000

    - Update to 1.5.0

--- games/solarus-quest-editor/files/patch-include_widgets_plain__text__edit.h
+++ games/solarus-quest-editor/files/patch-include_widgets_plain__text__edit.h
@@ -1,6 +1,6 @@
---- include/gui/plain_text_edit.h.orig	2015-11-29 14:07:34 UTC
-+++ include/gui/plain_text_edit.h
-@@ -73,7 +73,7 @@ protected:
+--- include/widgets/plain_text_edit.h.orig	2016-07-30 20:59:16 UTC
++++ include/widgets/plain_text_edit.h
+@@ -75,7 +75,7 @@ protected:
        if (show_margin) {
          const QRect rect = event->rect();
          const QFont font = currentCharFormat().font();
