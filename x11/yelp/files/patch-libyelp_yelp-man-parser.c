aa147e956396b891015635909019820d14937929
cpm <cpm@FreeBSD.org>
2016-07-30 22:31:21 +0000

    - Add USES=autoreconf - Enable compatibility with traditional man
    PR:	210927
    Submitted by:	cpm
    Reported by:	Rares Vernica <rvernica@gmail.com>
    Approved by:	gnome (maintainer timeout, 3 weeks)
    Obtained from:	Gentoo (https://github.com/gentoo/gentoo/blob/master/gnome-extra/yelp/files/yelp-3.16.0-man-compatibility.patch)
    MFH:	2016Q3

---
+++ x11/yelp/files/patch-libyelp_yelp-man-parser.c
@@ -0,0 +1,11 @@
+--- libyelp/yelp-man-parser.c.orig	2016-07-10 15:09:15 UTC
++++ libyelp/yelp-man-parser.c
+@@ -369,7 +369,7 @@ get_troff (gchar *path, GError **error)
+ {
+     gint ystdout;
+     GError *err = NULL;
+-    const gchar *argv[] = { "man", "-Z", "-Tutf8", "-EUTF-8", path, NULL };
++    const gchar *argv[] = { LIBEXECDIR "/yelp-groff", path, NULL };
+     gchar **my_argv;
+ 
+     /* g_strdupv() should accept a "const gchar **". */
