15da175933992b5c8d915b869a9907bf43eadcde
antoine <antoine@FreeBSD.org>
2016-07-31 14:48:49 +0000

    Fix build, using getline(3) from libc
    Reported by:	pkg-fallout

--- audio/raop_play/files/patch-rendezvous_Client.c
+++ audio/raop_play/files/patch-rendezvous_Client.c
@@ -1,13 +1,11 @@
 --- rendezvous/Client.c.orig	2005-12-16 14:17:02 UTC
 +++ rendezvous/Client.c
-@@ -83,8 +83,9 @@
+@@ -83,7 +83,7 @@
  #include <string.h>
  #include <unistd.h>
  #include <stdlib.h>
 -#include <asm/types.h>
 +#include <sys/types.h>
  
-+#include "getline.h"
  #include "mDNSClientAPI.h"// Defines the interface to the mDNS core code
  #include "mDNSPosix.h"    // Defines the specific types needed to run mDNS on this platform
- #include "ExampleClientApp.h"
