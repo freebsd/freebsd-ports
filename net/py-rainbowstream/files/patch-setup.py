857cb5439013552590a6c3450c783a7054a0202b
koobs <koobs@FreeBSD.org>
2016-08-04 09:52:51 +0000

    [NEW] net/py-rainbowstream: Smart and nice Twitter client on terminal
    Terminal-based Twitter Client. Realtime tweetstream, compose, search,
    favorite ... and much more fun directly from terminal
    
    WWW: http://www.rainbowstream.org/
    
    Requested by:	Markus Langer (@langerma, via Twitter)

---
+++ net/py-rainbowstream/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2016-08-04 08:41:32 UTC
++++ setup.py
+@@ -9,7 +9,7 @@ version = '1.3.3'
+ install_requires = [
+     "python-dateutil",
+     "arrow",
+-    "requests==2.5.3",
++    "requests>=2.5.3",
+     "pyfiglet",
+     "twitter",
+     "Pillow",
