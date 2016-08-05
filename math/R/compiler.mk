2bd7479e07720474a1258552e8ad7fe24b100fb2
marino <marino@FreeBSD.org>
2016-08-05 02:30:18 +0000

    math/R: maintainer rework
    See PR for signficant changes to math/R port including options,
    build defaults, and patch updates.
    
    PR:		211393
    Submitted by:	Joseph Mingrone (maintainer)

--- math/R/compiler.mk
+++ math/R/compiler.mk
@@ -1,2 +1,10 @@
 # Compiler settings required for R and the R-cran ports.
+.if ${OPSYS} == FreeBSD
+.  if ${OSVERSION} < 1000015
 USE_GCC=	yes
+.  endif
+.else
+.  if ${OPSYS} == DragonFly
+USE_GCC=	6
+.  endif
+.endif
