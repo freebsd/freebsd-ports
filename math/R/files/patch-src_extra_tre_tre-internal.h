b95421020da70c3d4ae85d4b9a337ec7fab7385b
John Marino <marino@FreeBSD.org>
2016-08-05 02:30:18 +0000

    math/R: maintainer rework
    See PR for signficant changes to math/R port including options,
    build defaults, and patch updates.
    
    PR:		211393
    Submitted by:	Joseph Mingrone (maintainer)

--- math/R/files/patch-src_extra_tre_tre-internal.h
+++ math/R/files/patch-src_extra_tre_tre-internal.h
@@ -1,22 +1,10 @@
---- src/extra/tre/tre-internal.h.orig	2016-05-05 10:54:54 UTC
+--- src/extra/tre/tre-internal.h.orig	2016-07-23 16:50:17 UTC
 +++ src/extra/tre/tre-internal.h
-@@ -17,6 +17,7 @@
- #include <wctype.h>
+@@ -18,6 +18,7 @@
  #endif /* !HAVE_WCTYPE_H */
  
-+#include <limits.h>
  #include <ctype.h>
++#include <stdint.h>
  #include "tre.h"
  
-@@ -47,7 +48,11 @@
- #ifdef TRE_WCHAR
- 
- /* Wide characters. */
-+#if WCHAR_MAX <= INT_MAX
- typedef wint_t tre_cint_t;
-+#else /* WCHAR_MAX > INT_MAX */
-+#define TRE_CHAR_MAX INT_MAX
-+#endif
- /* Workaround problem seen on AIX, (2010 & 2015), e.g.,
-     https://stat.ethz.ch/pipermail/r-devel/2015-October/071902.html
-   WCHAR_MAX = UINT32_MAX on AIX and that is "not possible to work"
+ #ifdef TRE_DEBUG
