8595892ea320d55b791e1009fcda22346c762ce0
amdmi3 <amdmi3@FreeBSD.org>
2016-08-07 09:01:03 +0000

    - Switch to USES=tar - Switch to options helpers - Regenerate patches with `make makepatch` - Cleanup pkg-descr

--- net/cryptcat/files/patch-farm9crypt.cc
+++ net/cryptcat/files/patch-farm9crypt.cc
@@ -1,5 +1,5 @@
---- farm9crypt.cc.orig	Sat Dec 18 05:34:27 2004
-+++ farm9crypt.cc	Sun Oct 30 07:45:05 2005
+--- farm9crypt.cc.orig	2004-12-18 04:34:27 UTC
++++ farm9crypt.cc
 @@ -23,6 +23,7 @@
  #include <sys/types.h>    // suggested by several people -- for OpenBSD, FreeBSD compiles
  #include <sys/socket.h>		/* basics, SO_ and AF_ defs, sockaddr, ... */
