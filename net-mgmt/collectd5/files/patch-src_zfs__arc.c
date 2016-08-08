b9302029ab43909aab5e328f300831223fcc14c8
Brad Davis <brd@FreeBSD.org>
2016-08-08 15:50:17 +0000

    Update net-mgmt/collectd5 to 5.5.2
    PR:		211613
    Submitted by:	brd
    Approved by:	ports-secteam (feld)
    Security:	6da45e38-5b55-11e6-8859-000c292ee6b8

--- net-mgmt/collectd5/files/patch-src_zfs__arc.c
+++ net-mgmt/collectd5/files/patch-src_zfs__arc.c
@@ -1,14 +1,13 @@
---- src/zfs_arc.c.orig	2016-01-22 09:51:17 UTC
+--- src/zfs_arc.c.orig	2016-07-26 05:57:19 UTC
 +++ src/zfs_arc.c
-@@ -270,8 +270,11 @@ static int za_read (void)
+@@ -270,10 +270,6 @@ static int za_read (void)
  	za_read_derive (ksp, "deleted",  "cache_operation", "deleted");
  #if __FreeBSD__
  	za_read_derive (ksp, "allocated","cache_operation", "allocated");
-+#if defined(__FreeBSD_version) && (__FreeBSD_version < 1002501)
-+	/* stolen removed from sysctl kstat.zfs.misc.arcstats on FreeBSD 10.2+ */
- 	za_read_derive (ksp, "stolen",   "cache_operation", "stolen");
+-#if defined(__FreeBSD_version) && (__FreeBSD_version < 1002501)
+-	/* stolen removed from sysctl kstat.zfs.misc.arcstats on FreeBSD 10.2+ */
+-	za_read_derive (ksp, "stolen",   "cache_operation", "stolen");
+-#endif
  #endif
-+#endif
  
  	/* Issue indicators */
- 	za_read_derive (ksp, "mutex_miss", "mutex_operations", "miss");
