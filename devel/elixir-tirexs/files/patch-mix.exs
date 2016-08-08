9b4322572d339444ec4331fbd69d4ae54001916c
Jimmy Olgeni <olgeni@FreeBSD.org>
2016-08-08 12:04:04 +0000

    Update devel/elixir-tirexs to version 0.8.5.

--- devel/elixir-tirexs/files/patch-mix.exs
+++ devel/elixir-tirexs/files/patch-mix.exs
@@ -1,11 +1,7 @@
---- mix.exs.orig	2016-07-14 15:23:49 UTC
+--- mix.exs.orig	2016-08-08 12:02:38 UTC
 +++ mix.exs
-@@ -8,10 +8,9 @@ defmodule Tirexs.Mixfile do
-       version: "0.8.3",
-       source_url: github,
-       homepage_url: github,
--      elixir: "~> 1.2.0",
-+      elixir: "~> 1.3.0",
+@@ -11,7 +11,6 @@ defmodule Tirexs.Mixfile do
+       elixir: "~> 1.3.0",
        description: description,
        package: package,
 -      deps: deps,
