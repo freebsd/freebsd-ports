b357ce4091c28ab644e101aadff18d03e70d0f69
cpm <cpm@FreeBSD.org>
2016-08-09 02:05:47 +0000

    - Update to 7.6.0 - Remove patch which has been incorporated upstream
    Changelog:
      https://github.com/ivmai/bdwgc/blob/master/ChangeLog
    
    Reviewed by:	junovitch (mentor)
    Approved by:	junovitch (mentor)
    Differential Revision:	D7443

--- devel/boehm-gc/files/patch-include_private_gcconfig.h
+++ devel/boehm-gc/files/patch-include_private_gcconfig.h
@@ -1,79 +1,14 @@
---- include/private/gcconfig.h.orig	2014-06-03 06:08:02 UTC
+--- include/private/gcconfig.h.orig	2016-08-08 20:30:40 UTC
 +++ include/private/gcconfig.h
-@@ -97,7 +97,7 @@
- # endif
- # if defined(__aarch64__)
- #    define AARCH64
--#    if !defined(LINUX)
-+#    if !defined(LINUX) && !defined(FREEBSD)
- #      define NOSYS
- #      define mach_type_known
- #    endif
-@@ -169,7 +169,7 @@
- #      define EWS4800
- #    endif
- #    if !defined(LINUX) && !defined(EWS4800) && !defined(NETBSD) \
--        && !defined(OPENBSD)
-+        && !defined(OPENBSD) && !defined(FREEBSD)
- #      if defined(ultrix) || defined(__ultrix)
- #        define ULTRIX
- #      else
-@@ -402,6 +402,10 @@
- #   define I386
- #   define mach_type_known
- # endif
-+# if defined(FREEBSD) && defined(__aarch64__)
-+#   define AARCH64
-+#   define mach_type_known
-+# endif
- # if defined(FREEBSD) && (defined(__amd64__) || defined(__x86_64__))
- #   define X86_64
- #   define mach_type_known
-@@ -1647,6 +1651,26 @@
- #    define DATAEND ((ptr_t)(&_end))
- #    define DYNAMIC_LOADING
- #  endif
-+#  ifdef FREEBSD
-+#    define OS_TYPE "FREEBSD"
-+#    define ALIGNMENT 4
-+#    define SIG_SUSPEND SIGUSR1
-+#    define SIG_THR_RESTART SIGUSR2
-+#    define FREEBSD_STACKBOTTOM
-+#    ifdef __ELF__
-+#        define DYNAMIC_LOADING
-+#    endif
-+     extern char etext[];
-+     extern char edata[];
-+     extern char end[];
-+#    define NEED_FIND_LIMIT
-+#    define DATASTART ((ptr_t)(&etext))
-+     ptr_t GC_find_limit(ptr_t, GC_bool);
-+#    define DATAEND (GC_find_limit (DATASTART, TRUE))
-+#    define DATAEND_IS_FUNC
-+#    define DATASTART2 ((ptr_t)(&edata))
-+#    define DATAEND2 ((ptr_t)(&end))
-+#  endif
- #  if defined(NONSTOP)
- #    define CPP_WORDSZ 32
- #    define OS_TYPE "NONSTOP"
-@@ -1998,6 +2022,20 @@
-       extern char _end[];
- #     define DATAEND ((ptr_t)(&_end))
- #   endif
-+#   ifdef FREEBSD
-+#     define OS_TYPE "FREEBSD"
-+#     ifndef GC_FREEBSD_THREADS
-+#       define MPROTECT_VDB
-+#     endif
-+#     define FREEBSD_STACKBOTTOM
-+#     ifdef __ELF__
-+#       define DYNAMIC_LOADING
-+#     endif
-+      extern char etext[];
-+      ptr_t GC_FreeBSDGetDataStart(size_t, ptr_t);
-+#     define DATASTART GC_FreeBSDGetDataStart(0x1000, (ptr_t)etext)
-+#     define DATASTART_IS_FUNC
-+#   endif
- #   ifdef NOSYS
-       /* __data_start is usually defined in the target linker script.   */
-       extern int __data_start[];
+@@ -2745,11 +2745,6 @@
+ # define SUNOS5SIGS
+ #endif
+ 
+-#ifdef DATASTART_USES_BSDGETDATASTART
+-  GC_INNER ptr_t GC_FreeBSDGetDataStart(size_t, ptr_t);
+-# define DATASTART_IS_FUNC
+-#endif
+-
+ #if !defined(GC_EXPLICIT_SIGNALS_UNBLOCK) && defined(SUNOS5SIGS) \
+     && !defined(GC_NO_PTHREAD_SIGMASK)
+ # define GC_EXPLICIT_SIGNALS_UNBLOCK
