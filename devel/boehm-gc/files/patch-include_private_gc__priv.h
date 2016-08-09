498449499597c11de4a5a3d8cc790ad945cad2d8
Carlos J. Puga Medina <cpm@FreeBSD.org>
2016-08-09 02:05:47 +0000

    - Update to 7.6.0 - Remove patch which has been incorporated upstream
    Changelog:
      https://github.com/ivmai/bdwgc/blob/master/ChangeLog
    
    Reviewed by:	junovitch (mentor)
    Approved by:	junovitch (mentor)
    Differential Revision:	D7443

--- devel/boehm-gc/files/patch-include_private_gc__priv.h
+++ devel/boehm-gc/files/patch-include_private_gc__priv.h
@@ -1,13 +1,11 @@
---- include/private/gc_priv.h.orig	2014-06-03 06:08:02 UTC
+--- include/private/gc_priv.h.orig	2016-08-08 20:30:31 UTC
 +++ include/private/gc_priv.h
-@@ -2461,7 +2461,9 @@ GC_INNER ptr_t GC_store_debug_info(ptr_t
- #endif
- 
- #if defined(FREEBSD) && (defined(I386) || defined(X86_64) \
--                        || defined(powerpc) || defined(__powerpc__))
-+                        || defined(powerpc) || defined(__powerpc__) \
-+			|| defined(__aarch64__))
-+
- # include <machine/trap.h>
+@@ -2500,6 +2500,8 @@ GC_INNER ptr_t GC_store_debug_info(ptr_t
  # if !defined(PCR)
  #   define NEED_FIND_LIMIT
+ # endif
++  GC_INNER ptr_t GC_FreeBSDGetDataStart(size_t, ptr_t);
++# define DATASTART_IS_FUNC
+ #endif /* DATASTART_USES_BSDGETDATASTART */
+ 
+ #if (defined(NETBSD) || defined(OPENBSD)) && defined(__ELF__) \
