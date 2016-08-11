b3e1bcf4f7d37679921acce77f77961eee9cc0b0
crees <crees@FreeBSD.org>
2016-08-11 21:06:42 +0000

    Add pkg-message with hints on replacing init
    Patch to allow running from PREFIX without copying (caveat-- must be on root
    filesystem!)
    
    Various minor runtime fixes

---
+++ sysutils/runit/files/patch-src_runit-init.c
@@ -0,0 +1,20 @@
+$FreeBSD$
+
+runit is designed to be placed in /sbin.  However, there is no reason that
+PREFIX cannot be respected here, so should a user define in loader.conf this
+in place, then it will still be found.
+
+init_path=/usr/local/sbin/runit-init:/sbin/init
+
+--- src/runit-init.c.orig	2016-08-10 20:53:15 UTC
++++ src/runit-init.c
+@@ -53,6 +53,9 @@ int main (int argc, const char * const *
+     /* kernel is starting init, runit does the job. */
+     execve(RUNIT, (char *const *)prog, envp);
+ 
++    /* OK, running from PREFIX-- should be default */
++    execve(PREFIX RUNIT, (char *const *)prog, envp);
++
+     /* serious error */
+     strerr_die4sys(111, FATAL, "unable to start ", prog[0], ": ");
+   }
