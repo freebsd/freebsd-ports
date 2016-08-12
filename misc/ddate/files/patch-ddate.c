07d0d588163154050f7895c7acecfb1d6da9014f
ehaupt <ehaupt@FreeBSD.org>
2016-08-12 21:32:53 +0000

    ddate has been officially removed from util-linux and move to github [1].
    The new version scheme requires EPOCH to be bumped. While here switch to new
    option helpers.
    
    [1] https://git.kernel.org/cgit/utils/util-linux/util-linux.git/commit/?id=b77ce61eb31e3b7ab8295e848ef741e43b16ca87
    
    Notified by:	Kathie Dart <kathiedart@gmail.com> (via mail)

---
+++ misc/ddate/files/patch-ddate.c
@@ -0,0 +1,12 @@
+--- ddate.c.orig	2016-08-12 21:07:26 UTC
++++ ddate.c
+@@ -64,7 +64,9 @@
+  * Church of the SubGenius and do not wish your copy of ddate(1) to contain
+  * code for counting down to X-Day, undefine KILL_BOB */
+ 
++/*
+ #define KILL_BOB 13013
++*/
+ 
+ /* If you wish ddate(1) to contain SubGenius slogans, define PRAISE_BOB */
+ 
