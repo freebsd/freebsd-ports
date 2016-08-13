8cf1a1179f673b696c726aa4937976d923d1877e
ume <ume@FreeBSD.org>
2016-08-13 17:23:13 +0000

    Fix build with recent libressl (2.4.2).
    Reported by:	"Dean E. Weimer" <dweimerxi [...] dweimer.net>`

--- mail/cyrus-imapd25/files/patch-imap__tls.c
+++ mail/cyrus-imapd25/files/patch-imap__tls.c
@@ -1,6 +1,15 @@
---- imap/tls.c.orig	2015-11-30 06:30:33 UTC
+--- imap/tls.c.orig	2016-07-22 01:55:57 UTC
 +++ imap/tls.c
-@@ -683,7 +683,9 @@ EXPORTED int     tls_init_serverengine(c
+@@ -222,7 +222,7 @@ static RSA *tmp_rsa_cb(SSL * s __attribu
+ }
+ #endif
+ 
+-#if OPENSSL_VERSION_NUMBER < 0x10100000L
++#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
+ /* replacements for new 1.1 API accessors */
+ /* XXX probably put these somewhere central */
+ static int DH_set0_pqg(DH *dh, BIGNUM *p, BIGNUM *q, BIGNUM *g)
+@@ -723,7 +723,9 @@ EXPORTED int     tls_init_serverengine(c
      off |= SSL_OP_ALL;            /* Work around all known bugs */
      off |= SSL_OP_NO_SSLv2;       /* Disable insecure SSLv2 */
      off |= SSL_OP_NO_SSLv3;       /* Disable insecure SSLv3 */
@@ -10,7 +19,7 @@
  
      const char *tls_versions = config_getstring(IMAPOPT_TLS_VERSIONS);
  
-@@ -1399,7 +1401,9 @@ HIDDEN int tls_init_clientengine(int ver
+@@ -1441,7 +1443,9 @@ HIDDEN int tls_init_clientengine(int ver
      off |= SSL_OP_ALL;            /* Work around all known bugs */
      off |= SSL_OP_NO_SSLv2;       /* Disable insecure SSLv2 */
      off |= SSL_OP_NO_SSLv3;       /* Disable insecure SSLv3 */
