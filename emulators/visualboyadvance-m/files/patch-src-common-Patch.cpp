78db5e3b9c609a60f567677295554d9d648bf8e1
pi <pi@FreeBSD.org>
2016-08-13 15:31:43 +0000

    emulators/visualboyadvance-m: 1.8.0 -> 2.0.0b2
    - patted portlint
    - patches are now makepatch compliant
    
    PR:		210058
    Changes:	http://vba-m.com/forum/showthread.php?tid=1037&pid=6091#pid6091
    		http://vba-m.com/forum/showthread.php?tid=1036&pid=6090#pid6090
    Submitted by:	Piotr Kubaj <pkubaj@anongoth.pl>
    Reviewed by:	pawel
    Approved by:	Allison N. Reid <root@cooltrainer.org> (maintainer)

--- emulators/visualboyadvance-m/files/patch-src-common-Patch.cpp
+++ emulators/visualboyadvance-m/files/patch-src-common-Patch.cpp
@@ -1,11 +1,10 @@
---- src/common/Patch.cpp.orig	2010-01-02 21:18:59.624899346 -0500
-+++ src/common/Patch.cpp	2010-01-02 21:18:45.448378670 -0500
-@@ -4,7 +4,7 @@
- #include <zlib.h>
+--- src/common/Patch.cpp.orig	2016-08-13 15:20:47 UTC
++++ src/common/Patch.cpp
+@@ -6,6 +6,7 @@
+ #endif
  
  #include "Patch.h"
--
 +#include <sys/param.h>
  
- #ifdef __GNUC__
- #if defined(__APPLE__) || defined (BSD)
+ #ifndef __LIBRETRO__
+ 
