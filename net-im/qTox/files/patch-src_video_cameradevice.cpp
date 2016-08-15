dfd7f1f1e929ad40bdbd398da08fdd1f64d4e42c
pawel <pawel@FreeBSD.org>
2016-08-15 14:02:23 +0000

    Update to version 1.5.1
    PR:		211733
    Submitted by:	maintainer

--- net-im/qTox/files/patch-src_video_cameradevice.cpp
+++ net-im/qTox/files/patch-src_video_cameradevice.cpp
@@ -1,4 +1,4 @@
---- src/video/cameradevice.cpp.orig	2016-04-24 12:44:18 UTC
+--- src/video/cameradevice.cpp.orig	2016-08-09 23:12:33 UTC
 +++ src/video/cameradevice.cpp
 @@ -31,7 +31,7 @@ extern "C" {
  #ifdef Q_OS_WIN
@@ -9,7 +9,7 @@
  #include "src/platform/camera/v4l2.h"
  #endif
  #ifdef Q_OS_OSX
-@@ -122,7 +122,7 @@ CameraDevice* CameraDevice::open(QString
+@@ -150,7 +150,7 @@ CameraDevice* CameraDevice::open(QString
  
      AVDictionary* options = nullptr;
      if (!iformat);
@@ -18,16 +18,7 @@
      else if (devName.startsWith("x11grab#"))
      {
          QSize screen;
-@@ -168,7 +168,7 @@ CameraDevice* CameraDevice::open(QString
-         av_dict_set(&options, "framerate", QString().setNum(mode.FPS).toStdString().c_str(), 0);
-     }
- #endif
--#ifdef Q_OS_LINUX
-+#ifdef Q_OS_UNIX
-     else if (iformat->name == QString("video4linux2,v4l2") && mode)
-     {
-         av_dict_set(&options, "video_size", QString("%1x%2").arg(mode.width).arg(mode.height).toStdString().c_str(), 0);
-@@ -311,7 +311,7 @@ QVector<QPair<QString, QString>> CameraD
+@@ -359,7 +359,7 @@ QVector<QPair<QString, QString>> CameraD
      else if (iformat->name == QString("dshow"))
          devices += DirectShow::getDeviceList();
  #endif
@@ -36,7 +27,7 @@
      else if (iformat->name == QString("video4linux2,v4l2"))
          devices += v4l2::getDeviceList();
  #endif
-@@ -358,7 +358,7 @@ QVector<VideoMode> CameraDevice::getVide
+@@ -461,7 +461,7 @@ QVector<VideoMode> CameraDevice::getVide
      else if (iformat->name == QString("dshow"))
          return DirectShow::getDeviceModes(devName);
  #endif
@@ -45,7 +36,25 @@
      else if (iformat->name == QString("video4linux2,v4l2"))
          return v4l2::getDeviceModes(devName);
  #endif
-@@ -400,7 +400,7 @@ bool CameraDevice::getDefaultInputFormat
+@@ -482,7 +482,7 @@ QVector<VideoMode> CameraDevice::getVide
+ */
+ QString CameraDevice::getPixelFormatString(uint32_t pixel_format)
+ {
+-#ifdef Q_OS_LINUX
++#ifdef Q_OS_UNIX
+     return v4l2::getPixelFormatString(pixel_format);
+ #else
+     return QString("unknown");
+@@ -498,7 +498,7 @@ false otherwise (such as if there's no p
+ */
+ bool CameraDevice::betterPixelFormat(uint32_t a, uint32_t b)
+ {
+-#ifdef Q_OS_LINUX
++#ifdef Q_OS_UNIX
+ 	return v4l2::betterPixelFormat(a, b);
+ #else
+ 	return false;
+@@ -518,7 +518,7 @@ bool CameraDevice::getDefaultInputFormat
      avdevice_register_all();
  
      // Desktop capture input formats
@@ -54,7 +63,7 @@
      idesktopFormat = av_find_input_format("x11grab");
  #endif
  #ifdef Q_OS_WIN
-@@ -408,7 +408,7 @@ bool CameraDevice::getDefaultInputFormat
+@@ -526,7 +526,7 @@ bool CameraDevice::getDefaultInputFormat
  #endif
  
      // Webcam input formats
---
+++ net-im/qTox/files/patch-src_video_videomode.cpp
@@ -0,0 +1,10 @@
+--- src/video/videomode.cpp.orig	2016-08-10 16:00:51 UTC
++++ src/video/videomode.cpp
+@@ -18,6 +18,7 @@
+ */
+ 
+ #include "videomode.h"
++#include <stdlib.h>
+ 
+ /**
+ @struct VideoMode
