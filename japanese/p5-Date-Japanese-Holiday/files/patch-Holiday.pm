2737338410e70ff8b8f07d10fe5bb5cea206fcf6
kuriyama <kuriyama@FreeBSD.org>
2016-08-15 13:52:24 +0000

    Add 山の日

---
+++ japanese/p5-Date-Japanese-Holiday/files/patch-Holiday.pm
@@ -0,0 +1,18 @@
+commit 0d47d6596136b5b6d57ca0aed2df67f6b6e4f29b
+Author: Jun Kuriyama <kuriyama@s2factory.co.jp>
+Date:   Tue Aug 9 11:32:18 2016 +0900
+
+    Add yama no hi (8/11).
+
+diff --git a/lib/Date/Japanese/Holiday.pm b/lib/Date/Japanese/Holiday.pm
+index b4f7c42..fc1bfb8 100644
+--- lib/Date/Japanese/Holiday.pm
++++ lib/Date/Japanese/Holiday.pm
+@@ -21,6 +21,7 @@ use constant FIRST_DAY => 2432753;
+     '05-03' => [FIRST_DAY, 0],
+     '05-05' => [FIRST_DAY, 0],
+     '07-20' => [2450084, 2452640],
++    '08-11' => [2457611, 0],
+     '09-15' => [2439302, 2452640],
+     '10-10' => [2439302, 2451544],
+     '11-03' => [FIRST_DAY, 0],
