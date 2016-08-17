8e3d13a494c6de29061caaf478025550cabb95d4
cpm <cpm@FreeBSD.org>
2016-08-17 00:45:17 +0000

    New port: games/iortcw Game engine for Return to Castle Wolfenstein
    Return to Castle Wolfenstein is a military- and occult-themed
    first-person shooter game in a Second World War setting, originally
    released in 2001.  It is a sequel to Wolfenstein 3D.
    
    This port contains the iortcw game engines for the single-player
    campaign and multiplayer, based on ioquake3 and the GPL source
    releases of RtCW.
    
    Return to Castle Wolfenstein requires non-distributable game data
    files, which you have to install manually (see pkg-message).
    
    WWW: https://github.com/iortcw/iortcw/
    
    PR:	211456
    Submitted by:	Tobias Kortkamp <t@tobik.me>
    Reviewed by:	amdmi3 (mentor)
    Approved by:	amdmi3 (mentor)

---
+++ games/iortcw/files/patch-MP_code_qcommon_q__platform.h
@@ -0,0 +1,23 @@
+--- MP/code/qcommon/q_platform.h.orig	2016-07-29 22:00:04 UTC
++++ MP/code/qcommon/q_platform.h
+@@ -35,11 +35,7 @@ Foundation, Inc., 51 Franklin St, Fifth 
+ 
+ #else
+ 
+-#if (defined _M_IX86 || defined __i386__) && !defined(C_ONLY)
+-#define id386 1
+-#else
+ #define id386 0
+-#endif
+ 
+ #if (defined(powerc) || defined(powerpc) || defined(ppc) || \
+ 	defined(__ppc) || defined(__ppc__)) && !defined(C_ONLY)
+@@ -229,8 +225,6 @@ Foundation, Inc., 51 Franklin St, Fifth 
+ #ifdef __i386__
+ #define ARCH_STRING "i386"
+ #elif defined __amd64__
+-#undef idx64
+-#define idx64 1
+ #define ARCH_STRING "amd64"
+ #elif defined __axp__
+ #define ARCH_STRING "alpha"
