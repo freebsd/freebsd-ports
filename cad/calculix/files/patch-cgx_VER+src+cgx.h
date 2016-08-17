a57862c18363cc8497007eb7f4e56f66385b98a0
maho <maho@FreeBSD.org>
2016-08-17 02:00:58 +0000

    Update to 2.11
    Main new features:
    
    * Rotational and translational cyclic symmetry for CFD was coded (see examples
    couette1per, couettecyl4 and couettecyl4comp).
    * Sensitivity analyses (cf. Section 6.9.22 in the User's Manual)
    * Speed-up of the treatment of SPC's and MPC's in the stiffness and mass
    matrix.
    
    See the complete notes
    http://www.dhondt.de/new_calc.htm
    
    PR:		211917
    Submitted by:	pfg@

--- cad/calculix/files/patch-cgx_VER+src+cgx.h
+++ cad/calculix/files/patch-cgx_VER+src+cgx.h
@@ -1,13 +1,13 @@
---- cgx_2.10/src/cgx.h.orig	2016-03-05 16:23:57 UTC
-+++ cgx_2.10/src/cgx.h
+--- cgx_2.11/src/cgx.h.orig	2016-07-29 10:15:15 UTC
++++ cgx_2.11/src/cgx.h
 @@ -83,8 +83,8 @@ rendering is done in the index-mode.
-     "/usr/local/CalculiX/ccx_2.10/doc/ccx/ccx.html",\
-     "/usr/local/CalculiX/cgx_2.10/doc/aflib/aflib.pdf"}
+     "/usr/local/CalculiX/ccx_2.11/doc/ccx/ccx.html",\
+     "/usr/local/CalculiX/cgx_2.11/doc/aflib/aflib.pdf"}
  #else
--  #define     HELPFILE        {"/usr/local/CalculiX/cgx_2.10/doc/cgx/cgx.html",\
--    "/usr/local/CalculiX/ccx_2.10/doc/ccx/ccx.html"}
-+   #define     HELPFILE        {"/usr/local/share/doc/CalculiX/cgx/cgx.html",\
-+     "/usr/local/share/doc/CalculiX/ccx/ccx.html"}
+-  #define     HELPFILE        {"/usr/local/CalculiX/cgx_2.11/doc/cgx/cgx.html",\
+-    "/usr/local/CalculiX/ccx_2.11/doc/ccx/ccx.html"}
++  #define     HELPFILE        {"/usr/local/share/doc/CalculiX/cgx/cgx.html",\
++    "/usr/local/share/doc/CalculiX/ccx/ccx.html"}
  #endif
  
  /* html browser, change if necessary */
