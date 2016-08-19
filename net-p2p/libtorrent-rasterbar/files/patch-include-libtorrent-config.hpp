e44475cdb1321cda0535a76aa0f06c273b2753d9
amdmi3 <amdmi3@FreeBSD.org>
2016-08-19 14:22:45 +0000

    Update net-p2p/libtorrent-rasterbar and its slave libtorrent-rasterbar-python to 1.1.0
    * USE_OPENSSL -> USES=ssl
    * remove GeoIP bits as those have been removed from the library
    * update LIB_DEPENDS and switch to += in the master as the slave needs the same boost libs
    * remove 2 obsolete patches, regen 1 patch
    * add a patch from upstream git to resolve CVE-2016-5301
    * add a TEST option and a patch from upstream git to let tests compile
    * simplify the install of DOCS and EXAMPLES
    * remove stale portscout restriction so updates can be discovered
    * update text and WWW in pkg-descr
    * update pkg-plist
    * cleanup the slave port's Makefile
    * assume maintainership
    
    PR:		211963
    Submitted by:	matthew@reztek.cz

--- net-p2p/libtorrent-rasterbar/files/patch-include-libtorrent-config.hpp
+++ net-p2p/libtorrent-rasterbar/files/patch-include-libtorrent-config.hpp
@@ -1,15 +1,15 @@
---- include/libtorrent/config.hpp.orig	2014-12-05 01:00:07 UTC
+--- include/libtorrent/config.hpp.orig  2016-04-10 21:39:14 UTC
 +++ include/libtorrent/config.hpp
-@@ -166,12 +166,6 @@ POSSIBILITY OF SUCH DAMAGE.
+@@ -180,12 +180,6 @@ POSSIBILITY OF SUCH DAMAGE.
+ #define TORRENT_USE_EXECINFO 1
+ #endif
  
- #endif // __APPLE__
- 
--#else
+-#else // __APPLE__
 -// FreeBSD has a reasonable iconv signature
 -// unless we're on glibc
 -#ifndef __GLIBC__
 -# define TORRENT_ICONV_ARG (const char**)
 -#endif
- #endif
- #define TORRENT_HAS_FALLOCATE 0
- #define TORRENT_USE_IFADDRS 1
+ #endif // __APPLE__
+ 
+ #define TORRENT_HAVE_MMAP 1
