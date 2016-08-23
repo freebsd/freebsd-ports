27566d96b73152373eaa361bbe1e1d51e50643ce
pawel <pawel@FreeBSD.org>
2016-08-23 20:31:07 +0000

    - Fix build after databases/sqlite update - Convert to USES=localbase - Register proxy dependencies found by stage-qa

---
+++ editors/texmacs/files/patch-src_Plugins_Sqlite3_sqlite3.hpp
@@ -0,0 +1,13 @@
+--- src/Plugins/Sqlite3/sqlite3.hpp.orig	2016-08-22 16:04:07 UTC
++++ src/Plugins/Sqlite3/sqlite3.hpp
+@@ -9,8 +9,8 @@
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
+ 
+-#ifndef SQLITE3_H
+-#define SQLITE3_H
++#ifndef TM_SQLITE3_H
++#define TM_SQLITE3_H
+ #include "url.hpp"
+ 
+ bool sqlite3_present ();
