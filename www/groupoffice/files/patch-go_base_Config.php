b50486a9cb08cb57038b6c6a318281976ff60d17
Wen Heping <wen@FreeBSD.org>
2016-08-23 11:35:52 +0000

    - Update to 6.2.14 - Update maintainer's email
    PR:		211692
    Submitted by:	joneum@bsdproject.de(maintainer)

--- www/groupoffice/files/patch-go_base_Config.php
+++ www/groupoffice/files/patch-go_base_Config.php
@@ -1,80 +1,74 @@
---- go/base/Config.php.orig	2015-11-02 07:39:37 UTC
+--- go/base/Config.php.orig	2016-07-25 13:35:05 UTC
 +++ go/base/Config.php
-@@ -733,21 +733,21 @@ class Config {
- 	 * @var     string
- 	 * @access  public
- 	 */
--	var $cmd_zip = '/usr/bin/zip';
-+	var $cmd_zip = '%%LOCALBASE%%/bin/zip';
- 
- 	/**
- 	 * Command to unpack ZIP archive
- 	 * @var     string
- 	 * @access  public
- 	 */
--	var $cmd_unzip = '/usr/bin/unzip';
-+	var $cmd_unzip = '%%LOCALBASE%%/bin/unzip';
- 
- 	/**
- 	 * Command to control TAR archives
- 	 * @var     string
- 	 * @access  public
- 	 */
--	var $cmd_tar = '/bin/tar';
-+	var $cmd_tar = '/usr/bin/tar';
- 
- 	/**
- 	 * Command to set system passwords. Used by passwd.users.class.inc.
-@@ -756,14 +756,14 @@ class Config {
- 	 * @var     string
- 	 * @access  public
- 	 */
--	var $cmd_chpasswd = '/usr/sbin/chpasswd';
-+	var $cmd_chpasswd = '%%LOCALBASE%%/sbin/chpasswd';
- 
- 	/**
- 	 * Command to SUDO
- 	 * @var     string
- 	 * @access  public
- 	 */
--	var $cmd_sudo = '/usr/bin/sudo';
-+	var $cmd_sudo = '%%LOCALBASE%%/bin/sudo';
- 
- 	/**
- 	 * Command to convert xml to wbxml
-@@ -771,7 +771,7 @@ class Config {
- 	 * @var     string
+@@ -22,7 +22,7 @@
+  * This class holds the main configuration options of Group-Office
+  * Don't modify this file. The values defined here are just default values.
+  * They are overwritten by the configuration options in /config.php or
+- * /etc/groupoffice/{HOSTNAME}/config.php
++ * /usr/local/etc/groupoffice/{HOSTNAME}/config.php
+  *
+  * To edit these options use install.php.
+  *
+@@ -786,7 +786,7 @@ class Config {
+ 	 * @var     StringHelper
  	 * @access  public
  	 */
 -	var $cmd_xml2wbxml = '/usr/bin/xml2wbxml';
-+	var $cmd_xml2wbxml = '%%LOCALBASE%%/bin/xml2wbxml';
++	var $cmd_xml2wbxml = '/usr/local/bin/xml2wbxml';
  
  	/**
  	 * Command to convert wbxml to xml
-@@ -779,7 +779,7 @@ class Config {
- 	 * @var     string
+@@ -794,7 +794,7 @@ class Config {
+ 	 * @var     StringHelper
  	 * @access  public
  	 */
 -	var $cmd_wbxml2xml = '/usr/bin/wbxml2xml';
-+	var $cmd_wbxml2xml = '%%LOCALBASE%%/bin/wbxml2xml';
++	var $cmd_wbxml2xml = '/usr/local/bin/wbxml2xml';
  
  	/**
  	 * Command to unpack winmail.dat files
-@@ -787,7 +787,7 @@ class Config {
- 	 * @var     string
+@@ -802,7 +802,7 @@ class Config {
+ 	 * @var     StringHelper
  	 * @access  public
  	 */
 -	var $cmd_tnef = '/usr/bin/tnef';
-+	var $cmd_tnef = '%%LOCALBASE%%/bin/tnef';
++	var $cmd_tnef = '/usr/local/bin/tnef';
  
  	/**
  	 * Command to execute the php command line interface
-@@ -795,7 +795,7 @@ class Config {
- 	 * @var     string
+@@ -810,7 +810,7 @@ class Config {
+ 	 * @var     StringHelper
  	 * @access  public
  	 */
 -	var $cmd_php = 'php';
-+	var $cmd_php = '%%LOCALBASE%%/bin/php';
++	var $cmd_php = '/usr/local/bin/php';
  
  
  	/**
+@@ -1290,8 +1290,8 @@ class Config {
+ 		$this->root_path = str_replace('\\','/',dirname(dirname(dirname(__FILE__)))).'/';
+ 
+ 		//suppress error for open_basedir warnings etc
+-		if(@file_exists('/etc/groupoffice/globalconfig.inc.php')) {
+-			require('/etc/groupoffice/globalconfig.inc.php');
++		if(@file_exists('/usr/local/etc/groupoffice/globalconfig.inc.php')) {
++			require('/usr/local/etc/groupoffice/globalconfig.inc.php');
+ 		}
+ 
+ 		$config_file = $this->get_config_file();
+@@ -1615,13 +1615,13 @@ class Config {
+ 				}
+ 			}*/
+ 			if(!empty($_SERVER['SERVER_NAME'])){
+-				$config_file = '/etc/groupoffice/'.$_SERVER['SERVER_NAME'].'/config.php';
++				$config_file = '/usr/local/etc/groupoffice/'.$_SERVER['SERVER_NAME'].'/config.php';
+ 				if(@file_exists($config_file)) {
+ 					$_SESSION['GO_SESSION']['config_file']=$config_file;
+ 					return $config_file;
+ 				}
+ 			}
+-			$config_file = '/etc/groupoffice/config.php';
++			$config_file = '/usr/local/etc/groupoffice/config.php';
+ 			if(@file_exists($config_file)) {
+ 				$_SESSION['GO_SESSION']['config_file']=$config_file;
+ 				return $config_file;
