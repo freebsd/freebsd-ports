21c6f9061b9e6d02fbdc31d7895edab54c523a66
John Marino <marino@FreeBSD.org>
2016-08-23 15:58:43 +0000

    textproc/soothsayer: fix ncurses, missing c++ headers, not jobs safe

---
+++ textproc/soothsayer/files/patch-src_tools_soothsayerDemo.cpp
@@ -0,0 +1,10 @@
+--- src/tools/soothsayerDemo.cpp.orig	2008-06-02 13:29:00 UTC
++++ src/tools/soothsayerDemo.cpp
+@@ -36,6 +36,7 @@
+ 
+ #include <iostream>
+ #include <sstream>
++#include <cstdlib>
+ #include <getopt.h>
+ #include <list>
+ 
---
+++ textproc/soothsayer/files/patch-src_tools_text2ngram.cpp
@@ -0,0 +1,10 @@
+--- src/tools/text2ngram.cpp.orig	2008-06-02 13:29:00 UTC
++++ src/tools/text2ngram.cpp
+@@ -29,6 +29,7 @@
+ #include <list>
+ #include <string>
+ #include <map>
++#include <cstdlib>
+ 
+ #ifdef HAVE_UNISTD_H
+ #include <unistd.h>
