e0f8c8317fba3d79aff8dc08d2a3b161de57fc45
marino <marino@FreeBSD.org>
2016-08-23 21:21:05 +0000

    audio/zinf: document ncurses requirement
    While here, import several dports patches to add missing headers
    for c++.

---
+++ audio/zinf/files/patch-base_include_metadata.h
@@ -0,0 +1,10 @@
+--- base/include/metadata.h.orig	2003-09-16 20:34:53.000000000 +0300
++++ base/include/metadata.h
+@@ -25,6 +25,7 @@ ________________________________________
+ #define INCLUDED_METADATA_H_
+ 
+ #include <string>
++#include <cstring>
+ #include <map>
+ 
+ 
---
+++ audio/zinf/files/patch-base_src_prefixprefs.cpp
@@ -0,0 +1,10 @@
+--- base/src/prefixprefs.cpp.orig	2003-09-16 20:34:54.000000000 +0300
++++ base/src/prefixprefs.cpp
+@@ -24,6 +24,7 @@ ________________________________________
+ 
+ #include <stdlib.h>
+ #include <string>
++#include <cstring>
+ using namespace std;
+ #include "prefixprefs.h"
+ 
