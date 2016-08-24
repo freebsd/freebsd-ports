1135e4fd7cd8cce9a6b9d847d32dfef4e1ad39b4
pi <pi@FreeBSD.org>
2016-08-24 19:21:11 +0000

    devel/godot: Update to 2.1 and add minimal devel/godot-tools slave port
    - Update to 2.1 release
    - Add LICENSE_FILE for MIT license
    - Add CCBYv3 license for logo
    - Add devel/xdg-user-dirs, devel/xdg-utils and x11/xmessage to RUN_DEPENDS
    - Add xrandr to USE_XORG
    - Replace USE_OPENSSL=yes with USES+=ssl
    - Add gl to USE_GL
    - Remove GLU dependency - glu.h was included from imported glew.h
      but GLU components are not used
    - Add DEBUG option and clarify build targets
    - Replace TOOLS option with devel/godot-tools port, while devel/godot
      port used as runtime
    - Allow to build ALSA and PulseAudio drivers independently
    - Allow to build RtAudio driver with OSS or ALSA or PulseAudio
    - Set OSS option by default
    - Add desktop entry for godot-tools
    - Add sed patch to fix executable paths inside of
      ${WRKSRC}/platform/x11/os_x11.cpp file
    - Remove mkdir, because of COPYTREE_SHARE usage, which recreates
      destination directories
    - Remove RM command for demos/2d/hexamap/.fscache file, because the
      file is not available for current version
    - Add patch to enable OSS usage for RtAudio driver on FreeBSD
    - Add patch to fix issue(s) with OS_Unix::execute and
      OS_Unix::get_executable_path functions on FreeBSD
    - Add patch to fix layout of --help text
    - Remove pkg-plist and use dynamic package list with PORTDATA and PLIST_FILES
    - Use current head commit of 2.1 branch from new github repo
      godotengine/godot-demo-projects to install examples (this was
      previously part of godot repo)
    
    PR:		209742
    Submitted by:	lightside@gmx.com
    Approved by:	FreeBSD@Shaneware.biz

--- devel/godot/files/patch-platform_x11_detect.py
+++ devel/godot/files/patch-platform_x11_detect.py
@@ -1,4 +1,4 @@
---- platform/x11/detect.py.orig	2016-02-29 16:49:54 UTC
+--- platform/x11/detect.py.orig	2016-08-09 07:52:15 UTC
 +++ platform/x11/detect.py
 @@ -30,11 +30,6 @@ def can_build():
  		print("X11 not found.. x11 disabled.")
@@ -12,8 +12,19 @@
  	x11_error=os.system("pkg-config xcursor --modversion > /dev/null ")
  	if (x11_error):
  		print("xcursor not found.. x11 disabled.")
-@@ -133,7 +128,7 @@ def configure(env):
- 	env.ParseConfig('pkg-config xcursor --cflags --libs')
+@@ -60,7 +55,9 @@ def get_opts():
+ 	('use_static_cpp','link stdc++ statically','no'),
+ 	('use_sanitizer','Use llvm compiler sanitize address','no'),
+ 	('use_leak_sanitizer','Use llvm compiler sanitize memory leaks','no'),
+-	('pulseaudio','Detect & Use pulseaudio','yes'),
++	('alsa','Detect & Use alsa audio','no'),
++	('oss','Detect & Use OSS audio','no'),
++	('pulseaudio','Detect & Use pulseaudio','no'),
+ 	('udev','Use udev for gamepad connection callbacks','no'),
+ 	('debug_release', 'Add debug symbols to release version','no'),
+ 	]
+@@ -141,7 +138,7 @@ def configure(env):
+ 	env.ParseConfig('pkg-config xrandr --cflags --libs')
  
  	if (env["openssl"]=="yes"):
 -		env.ParseConfig('pkg-config openssl --cflags --libs')
@@ -21,3 +32,34 @@
  
  
  	if (env["freetype"]=="yes"):
+@@ -152,12 +149,13 @@ def configure(env):
+ 
+ 	env.Append(CPPFLAGS=['-DOPENGL_ENABLED'])
+ 
+-	if os.system("pkg-config --exists alsa")==0:
+-		print("Enabling ALSA")
+-		env.Append(CPPFLAGS=["-DALSA_ENABLED"])
+-		env.Append(LIBS=['asound'])
+-	else:
+-		print("ALSA libraries not found, disabling driver")
++	if (env["alsa"]=="yes"):
++		if os.system("pkg-config --exists alsa")==0:
++			print("Enabling ALSA")
++			env.Append(CPPFLAGS=["-DALSA_ENABLED"])
++			env.Append(LIBS=['asound'])
++		else:
++			print("ALSA libraries not found, disabling driver")
+ 
+ 	if (platform.system() == "Linux"):
+ 		env.Append(CPPFLAGS=["-DJOYDEV_ENABLED"])
+@@ -172,6 +170,10 @@ def configure(env):
+ 		else:
+ 			print("libudev development libraries not found, disabling udev support")
+ 
++	if (env["oss"]=="yes"):
++		print("Enabling OSS Audio")
++		env.Append(CPPFLAGS=["-DOSS_ENABLED"])
++
+ 	if (env["pulseaudio"]=="yes"):
+ 		if not os.system("pkg-config --exists libpulse-simple"):
+ 			print("Enabling PulseAudio")
