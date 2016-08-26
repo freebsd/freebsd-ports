c2e7736bed7c5018f23fb00d9b626e996af46f77
Kevin Lo <kevlo@FreeBSD.org>
2016-08-26 01:02:36 +0000

    Fix the run dependency and install the bash completion file.
    PR:	212154
    Submitted by:	Tobias Kortkamp <t@tobik.me>

--- textproc/source-highlight/files/patch-gl_stdbool.in.h
+++ textproc/source-highlight/files/patch-gl_stdbool.in.h
@@ -1,6 +1,6 @@
---- gl/stdbool.in.h.orig	2010-04-19 14:33:17.000000000 +0800
-+++ gl/stdbool.in.h	2010-04-19 14:36:05.000000000 +0800
-@@ -102,11 +102,6 @@
+--- gl/stdbool.in.h.orig	2011-12-16 17:10:10 UTC
++++ gl/stdbool.in.h
+@@ -105,11 +105,6 @@ typedef bool _Bool;
          The only benefit of the enum, debuggability, is not important
          with these compilers.  So use 'signed char' and no enum.  */
  #  define _Bool signed char
