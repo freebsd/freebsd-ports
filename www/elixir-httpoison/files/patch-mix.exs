b08f3e9ddadaa225704407e6ba61a2fff2654631
olgeni <olgeni@FreeBSD.org>
2016-08-29 14:21:08 +0000

    Update www/elixir-httpoison to version 0.9.1.

--- www/elixir-httpoison/files/patch-mix.exs
+++ www/elixir-httpoison/files/patch-mix.exs
@@ -1,10 +1,10 @@
---- mix.exs.orig	2015-07-05 09:01:37 UTC
+--- mix.exs.orig	2016-08-29 14:16:38 UTC
 +++ mix.exs
 @@ -12,7 +12,6 @@ defmodule HTTPoison.Mixfile do
        name: "HTTPoison",
        description: @description,
-       package: package,
--      deps: deps,
+       package: package(),
+-      deps: deps(),
        source_url: "https://github.com/edgurgel/httpoison" ]
    end
  
