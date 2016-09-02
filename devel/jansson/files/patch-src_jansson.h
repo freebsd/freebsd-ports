ff4da9d6a400e559fa87c84c126f4d18f391403d
vanilla <vanilla@FreeBSD.org>
2016-09-02 05:17:16 +0000

    Update to 2.8.

--- devel/jansson/files/patch-src_jansson.h
+++ devel/jansson/files/patch-src_jansson.h
@@ -1,33 +1,33 @@
---- src/jansson.h.orig	2013-09-25 06:11:34.000000000 +0800
-+++ src/jansson.h	2013-09-25 06:13:20.000000000 +0800
-@@ -148,10 +148,10 @@ int json_object_iter_set_new(json_t *obj
-         key && (value = json_object_iter_value(json_object_key_to_iter(key))); \
-         key = json_object_iter_key(json_object_iter_next(object, json_object_key_to_iter(key))))
+--- src/jansson.h.orig	2016-08-26 17:37:02 UTC
++++ src/jansson.h
+@@ -159,10 +159,10 @@ int json_object_iter_set_new(json_t *obj
+         key = json_object_iter_key(n), \
+             n = json_object_iter_next(object, json_object_key_to_iter(key)))
  
 -#define json_array_foreach(array, index, value) \
 -	for(index = 0; \
 -		index < json_array_size(array) && (value = json_array_get(array, index)); \
 -		index++)
-+#define json_array_foreach(array, ind, value) \
-+	for(ind = 0; \
-+		ind < json_array_size(array) && (value = json_array_get(array, ind)); \
-+		ind++)
++#define json_array_foreach(array, idx, value) \
++	for(idx = 0; \
++		idx < json_array_size(array) && (value = json_array_get(array, idx)); \
++		idx++)
  
  static JSON_INLINE
  int json_object_set(json_t *object, const char *key, json_t *value)
-@@ -172,11 +172,11 @@ int json_object_iter_set(json_t *object,
+@@ -183,11 +183,11 @@ int json_object_iter_set(json_t *object,
  }
  
  size_t json_array_size(const json_t *array);
 -json_t *json_array_get(const json_t *array, size_t index);
 -int json_array_set_new(json_t *array, size_t index, json_t *value);
-+json_t *json_array_get(const json_t *array, size_t ind);
-+int json_array_set_new(json_t *array, size_t ind, json_t *value);
++json_t *json_array_get(const json_t *array, size_t idx);
++int json_array_set_new(json_t *array, size_t idx, json_t *value);
  int json_array_append_new(json_t *array, json_t *value);
 -int json_array_insert_new(json_t *array, size_t index, json_t *value);
 -int json_array_remove(json_t *array, size_t index);
-+int json_array_insert_new(json_t *array, size_t ind, json_t *value);
-+int json_array_remove(json_t *array, size_t ind);
++int json_array_insert_new(json_t *array, size_t idx, json_t *value);
++int json_array_remove(json_t *array, size_t idx);
  int json_array_clear(json_t *array);
  int json_array_extend(json_t *array, json_t *other);
  
--- devel/jansson/files/patch-src_value.c
+++ devel/jansson/files/patch-src_value.c
@@ -1,11 +1,11 @@
---- src/value.c.orig	2013-09-25 06:13:36.000000000 +0800
-+++ src/value.c	2013-09-25 06:15:37.000000000 +0800
-@@ -360,20 +360,20 @@ size_t json_array_size(const json_t *jso
+--- src/value.c.orig	2016-08-25 17:21:36 UTC
++++ src/value.c
+@@ -375,20 +375,20 @@ size_t json_array_size(const json_t *jso
      return json_to_array(json)->entries;
  }
  
 -json_t *json_array_get(const json_t *json, size_t index)
-+json_t *json_array_get(const json_t *json, size_t ind)
++json_t *json_array_get(const json_t *json, size_t idx)
  {
      json_array_t *array;
      if(!json_is_array(json))
@@ -13,24 +13,24 @@
      array = json_to_array(json);
  
 -    if(index >= array->entries)
-+    if(ind >= array->entries)
++    if(idx >= array->entries)
          return NULL;
  
 -    return array->table[index];
-+    return array->table[ind];
++    return array->table[idx];
  }
  
 -int json_array_set_new(json_t *json, size_t index, json_t *value)
-+int json_array_set_new(json_t *json, size_t ind, json_t *value)
++int json_array_set_new(json_t *json, size_t idx, json_t *value)
  {
      json_array_t *array;
  
-@@ -387,14 +387,14 @@ int json_array_set_new(json_t *json, siz
+@@ -402,14 +402,14 @@ int json_array_set_new(json_t *json, siz
      }
      array = json_to_array(json);
  
 -    if(index >= array->entries)
-+    if(ind >= array->entries)
++    if(idx >= array->entries)
      {
          json_decref(value);
          return -1;
@@ -38,73 +38,73 @@
  
 -    json_decref(array->table[index]);
 -    array->table[index] = value;
-+    json_decref(array->table[ind]);
-+    array->table[ind] = value;
++    json_decref(array->table[idx]);
++    array->table[idx] = value;
  
      return 0;
  }
-@@ -466,7 +466,7 @@ int json_array_append_new(json_t *json, 
+@@ -481,7 +481,7 @@ int json_array_append_new(json_t *json, 
      return 0;
  }
  
 -int json_array_insert_new(json_t *json, size_t index, json_t *value)
-+int json_array_insert_new(json_t *json, size_t ind, json_t *value)
++int json_array_insert_new(json_t *json, size_t idx, json_t *value)
  {
      json_array_t *array;
      json_t **old_table;
-@@ -480,7 +480,7 @@ int json_array_insert_new(json_t *json, 
+@@ -495,7 +495,7 @@ int json_array_insert_new(json_t *json, 
      }
      array = json_to_array(json);
  
 -    if(index > array->entries) {
-+    if(ind > array->entries) {
++    if(idx > array->entries) {
          json_decref(value);
          return -1;
      }
-@@ -492,21 +492,21 @@ int json_array_insert_new(json_t *json, 
+@@ -507,21 +507,21 @@ int json_array_insert_new(json_t *json, 
      }
  
      if(old_table != array->table) {
 -        array_copy(array->table, 0, old_table, 0, index);
 -        array_copy(array->table, index + 1, old_table, index,
 -                   array->entries - index);
-+        array_copy(array->table, 0, old_table, 0, ind);
-+        array_copy(array->table, ind + 1, old_table, ind,
-+                   array->entries - ind);
++        array_copy(array->table, 0, old_table, 0, idx);
++        array_copy(array->table, idx + 1, old_table, idx,
++                   array->entries - idx);
          jsonp_free(old_table);
      }
      else
 -        array_move(array, index + 1, index, array->entries - index);
-+        array_move(array, ind + 1, ind, array->entries - ind);
++        array_move(array, idx + 1, idx, array->entries - idx);
  
 -    array->table[index] = value;
-+    array->table[ind] = value;
++    array->table[idx] = value;
      array->entries++;
  
      return 0;
  }
  
 -int json_array_remove(json_t *json, size_t index)
-+int json_array_remove(json_t *json, size_t ind)
++int json_array_remove(json_t *json, size_t idx)
  {
      json_array_t *array;
  
-@@ -514,14 +514,14 @@ int json_array_remove(json_t *json, size
+@@ -529,14 +529,14 @@ int json_array_remove(json_t *json, size
          return -1;
      array = json_to_array(json);
  
 -    if(index >= array->entries)
-+    if(ind >= array->entries)
++    if(idx >= array->entries)
          return -1;
  
 -    json_decref(array->table[index]);
-+    json_decref(array->table[ind]);
++    json_decref(array->table[idx]);
  
      /* If we're removing the last element, nothing has to be moved */
 -    if(index < array->entries - 1)
 -        array_move(array, index, index + 1, array->entries - index - 1);
-+    if(ind < array->entries - 1)
-+        array_move(array, ind, ind + 1, array->entries - ind - 1);
++    if(idx < array->entries - 1)
++        array_move(array, idx, idx + 1, array->entries - idx - 1);
  
      array->entries--;
  
