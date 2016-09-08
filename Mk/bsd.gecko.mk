3239c833199ee8d0805095bbaa1d66a9803635a9
jbeich <jbeich@FreeBSD.org>
2016-09-08 06:49:24 +0000

    gecko: drop poorly maintained GIO=off case after r376999
    Glib IO is already used by Gtk internally, so there's little reason
    to avoid it unless using non-Gtk builds which are no longer supported.
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1286627
    https://bugzilla.mozilla.org/show_bug.cgi?id=1287288
    
    PR:		210220
    Reported by:	Marco Beishuizen, Arrigo Marchiori
    MFH:		2016Q3

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -341,12 +341,6 @@ MOZ_OPTIONS+=	--enable-gconf
 MOZ_OPTIONS+=	--disable-gconf
 .endif
 
-.if ${PORT_OPTIONS:MGIO}
-MOZ_OPTIONS+=	--enable-gio
-.else
-MOZ_OPTIONS+=	--disable-gio
-.endif
-
 .if ${PORT_OPTIONS:MGNOMEUI}
 BUILD_DEPENDS+=	${libgnomeui_DETECT}:${libgnomeui_LIB_DEPENDS:C/.*://}
 USE_GNOME+=		libgnomeui:build
