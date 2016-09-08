0554eadc5b29001492f3f005808e9e87140bbbdc
jbeich <jbeich@FreeBSD.org>
2016-09-08 06:48:53 +0000

    www/firefox: update to 49.0 (rc2)
    Bump PORTREVISION to account for slightly different behavior in other
    gecko@ ports and to get a discrete regression window.
    
    Changes:	https://www.mozilla.org/firefox/49.0/releasenotes/
    PR:		211792
    Submitted by:	Jochen Neumeister <joneum@bsdproject.de> (based on)
    Security:	2c57c47e-8bb3-4694-83c8-9fc3abad3964
    MFH:		2016Q3

--- www/firefox/files/patch-modules-libpref-init-all.js
+++ www/firefox/files/patch-modules-libpref-init-all.js
@@ -1,15 +1,14 @@
---- modules/libpref/init/all.js.orig	2015-08-06 22:39:34 UTC
+--- modules/libpref/init/all.js.orig	2016-07-25 20:22:05 UTC
 +++ modules/libpref/init/all.js
-@@ -4052,7 +4052,12 @@ pref("layers.max-active", -1);
+@@ -4453,6 +4453,11 @@ pref("layers.max-active", -1);
+ // if you change the tile size.
  pref("layers.tiles.adjust", true);
  
- // Set the default values, and then override per-platform as needed
-+#ifdef MOZ_TREE_CAIRO
- pref("layers.offmainthreadcomposition.enabled", true);
-+#else
++#ifndef MOZ_TREE_CAIRO
 +// Disable for system cairo on suspicion of causing random crashes
-+pref("layers.offmainthreadcomposition.enabled", false);
++pref("layers.offmainthreadcomposition.force-disabled", true);
 +#endif
++
  // Compositor target frame rate. NOTE: If vsync is enabled the compositor
  // frame rate will still be capped.
  // -1 -> default (match layout.frame_rate or 60 FPS)
