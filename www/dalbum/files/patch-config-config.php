2c0177e347de3ea5a3949d9d7ea0e87c19e69275
Dmitry Marakasov <amdmi3@FreeBSD.org>
2016-09-11 17:56:26 +0000

    - Add LICENSE - Add missing USES=php - Move permission handling to plist - Regenerate patches

--- www/dalbum/files/patch-config-config.php
+++ www/dalbum/files/patch-config-config.php
@@ -1,6 +1,6 @@
---- config/config.php.orig	Fri Oct 21 13:43:21 2005
-+++ config/config.php	Fri Oct 21 13:44:15 2005
-@@ -242,7 +242,7 @@
+--- config/config.php.orig	2005-09-20 21:40:56 UTC
++++ config/config.php
+@@ -242,7 +242,7 @@ $g_bGDVer2=(fixed_phpversion()>"4.3");
  $g_bGDSetMemoryLimit=true;
  
  // ImageMagick convert location and command lines (if $g_sResizeMethod="IM")
