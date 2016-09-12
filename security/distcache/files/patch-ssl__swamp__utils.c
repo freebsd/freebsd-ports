3e44fb49d94ddfb9aaf14b9240e1beaa7ca0a576
John Marino <marino@FreeBSD.org>
2016-09-12 13:23:47 +0000

    security/distcache: Configure SSL base, fix build on LibreSSL
    While here, simplify existing patch for modern FreeBSD and DragonFly

--- security/distcache/files/patch-ssl__swamp__utils.c
+++ security/distcache/files/patch-ssl__swamp__utils.c
@@ -1,6 +1,6 @@
---- ssl/swamp/utils.c.orig	2004-02-17 15:29:34.000000000 -0500
-+++ ssl/swamp/utils.c	2015-03-23 19:47:34.795067000 -0400
-@@ -269,8 +269,10 @@
+--- ssl/swamp/utils.c.orig	2004-02-17 20:29:34 UTC
++++ ssl/swamp/utils.c
+@@ -269,10 +269,14 @@ int util_parse_sslmeth(const char *str_t
  {
  	if(!strcmp(str_toconvert, "normal"))
  		*val = SWAMP_SSLMETH_NORMAL;
@@ -8,6 +8,10 @@
  	else if(!strcmp(str_toconvert, "sslv2"))
  		*val = SWAMP_SSLMETH_SSLv2;
 +#endif
++#ifndef OPENSSL_NO_SSL3
  	else if(!strcmp(str_toconvert, "sslv3"))
  		*val = SWAMP_SSLMETH_SSLv3;
++#endif
  	else if(!strcmp(str_toconvert, "tlsv1"))
+ 		*val = SWAMP_SSLMETH_TLSv1;
+ 	else
