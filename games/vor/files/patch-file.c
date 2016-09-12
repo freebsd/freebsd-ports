fcd0f7cf73c659c09de81478c0bf29ba922da547
amdmi3 <amdmi3@FreeBSD.org>
2016-09-12 09:01:23 +0000

    - Update to 0.5.7 - Fix LICENSE - Switch to upstream .desktop file

--- games/vor/files/patch-file.c
+++ games/vor/files/patch-file.c
@@ -1,6 +1,6 @@
---- file.c.orig	2009-12-10 00:28:59.000000000 +0300
-+++ file.c	2010-02-04 05:50:54.000000000 +0300
-@@ -101,7 +101,6 @@
+--- file.c.orig	2016-05-22 21:14:48 UTC
++++ file.c
+@@ -113,7 +113,6 @@ find_data_dir(void)
  	int i;
  	char *data_options[3] = {
  		getenv("VOR_DATA"),
