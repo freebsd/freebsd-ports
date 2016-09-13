7925ef82a778274a20db0b63a57a4619576d8a82
Brad Davis <brd@FreeBSD.org>
2016-09-13 15:34:28 +0000

    Fix build of www/webkit2-gtk3 on armv6 by matching the correct define and disabling the JIT compiler.
    PR:		207275
    Submitted by:	mikael.urankar@gmail.com
    Reported by:	sbruno
    Reviewed by:	bdrewery (implicit)
    Approved by:	maintainer timeout

---
+++ www/webkit2-gtk3/files/patch-Source_WTF_wtf_Platform.h
@@ -0,0 +1,18 @@
+--- Source/WTF/wtf/Platform.h.orig	2016-07-20 UTC
++++ Source/WTF/wtf/Platform.h
+@@ -218,6 +218,7 @@
+ #elif defined(__ARM_ARCH_6__) \
+     || defined(__ARM_ARCH_6J__) \
+     || defined(__ARM_ARCH_6K__) \
++    || defined(__ARM_ARCH_6KZ__) \
+     || defined(__ARM_ARCH_6Z__) \
+     || defined(__ARM_ARCH_6ZK__) \
+     || defined(__ARM_ARCH_6T2__) \
+@@ -264,6 +265,7 @@
+ 
+ #elif defined(__ARM_ARCH_6J__) \
+     || defined(__ARM_ARCH_6K__) \
++    || defined(__ARM_ARCH_6KZ__) \
+     || defined(__ARM_ARCH_6Z__) \
+     || defined(__ARM_ARCH_6ZK__) \
+     || defined(__ARM_ARCH_6M__)
---
+++ www/webkit2-gtk3/files/patch-Source_bmalloc_bmalloc_BPlatform.h
@@ -0,0 +1,10 @@
+--- Source/bmalloc/bmalloc/BPlatform.h.orig	2016-05-06 11:07:30 UTC
++++ Source/bmalloc/bmalloc/BPlatform.h
+@@ -100,6 +100,7 @@
+ #elif defined(__ARM_ARCH_6__) \
+ || defined(__ARM_ARCH_6J__) \
+ || defined(__ARM_ARCH_6K__) \
++|| defined(__ARM_ARCH_6KZ__) \
+ || defined(__ARM_ARCH_6Z__) \
+ || defined(__ARM_ARCH_6ZK__) \
+ || defined(__ARM_ARCH_6T2__) \
