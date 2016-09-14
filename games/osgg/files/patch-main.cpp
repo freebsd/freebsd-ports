b4c448290291bb58dab88074279f89af65277c83
Dmitry Marakasov <amdmi3@FreeBSD.org>
2016-09-14 01:45:32 +0000

    - Update to 1.0 - Add LICENSE - Add .desktop file - Simplify makefile

--- games/osgg/files/patch-main.cpp
+++ games/osgg/files/patch-main.cpp
@@ -1,6 +1,6 @@
---- main.cpp.orig	2009-03-14 01:46:04.000000000 +0300
-+++ main.cpp	2013-12-21 03:29:52.401817465 +0400
-@@ -26,6 +26,7 @@
+--- main.cpp.orig	2016-08-13 02:07:57 UTC
++++ main.cpp
+@@ -27,15 +27,18 @@
  #include <fstream>
  #include <iostream>
  #include <sstream>
@@ -8,7 +8,21 @@
  
  #include <sys/time.h>
  
-@@ -340,6 +341,7 @@
+-#ifndef WIN32
+-    #include <endian.h>
+-#else
++#if defined(WIN32)
+     typedef unsigned int uint;
+     #include <windows.h>
+     #include <GL/glext.h>
++#elif defined(__FreeBSD__)
++    #include <sys/endian.h>
++#else
++    #include <endian.h>
+ #endif
+ 
+ 
+@@ -348,6 +351,7 @@ bool classBullets::col(vector<vert> targ
        }
      }
    }
@@ -16,3 +30,25 @@
  }
  
  void classBullets::shoot(entity owner, gPs velocity)
+@@ -1826,9 +1830,9 @@ int main(int argc, char **argv)
+ 
+   GLfloat scale;
+ 
+-  readEnt("ship.txt", gameInfo.shipStaticVerts);
+-  readEnt("base.txt", gameInfo.baseStaticVerts);
+-  readEnt("enemy.txt", gameInfo.enemyStaticVerts);
++  readEnt(DATADIR "ship.txt", gameInfo.shipStaticVerts);
++  readEnt(DATADIR "base.txt", gameInfo.baseStaticVerts);
++  readEnt(DATADIR "enemy.txt", gameInfo.enemyStaticVerts);
+ 
+   //Enter Main loop
+   while(gameState != GameStateQuit)
+@@ -1958,7 +1962,7 @@ int main(int argc, char **argv)
+               break;
+ 
+             case SDLK_d:
+-              saveMap(polys, ents, "verts.txt");
++              saveMap(polys, ents, DATADIR "verts.txt");
+             break;
+ 
+             case SDLK_s:
