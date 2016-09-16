8338153336248ac176028bfdd46ddb57e047ea3e
Dmitry Marakasov <amdmi3@FreeBSD.org>
2016-09-16 22:30:31 +0000

    - Update to 2.6.4 - Update license - Add NO_ARCH
    PR:		212664
    Submitted by:	amdmi3
    Approved by:	kyuupichan@gmail.com (maintainer)

--- finance/electrum/files/patch-setup.py
+++ finance/electrum/files/patch-setup.py
@@ -1,11 +1,15 @@
---- setup.py.orig	2015-10-18 10:56:02 UTC
+--- setup.py.orig	2016-02-29 14:37:15 UTC
 +++ setup.py
-@@ -16,7 +16,7 @@ if sys.version_info[:3] < (2, 7, 0):
- data_files = []
- 
- if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
--    usr_share = os.path.join(sys.prefix, "share")
-+    usr_share = '%%PREFIX%%/share'
+@@ -21,12 +21,6 @@ if platform.system() in ['Linux', 'FreeB
+     parser.add_argument('--root=', dest='root_path', metavar='dir', default='/')
+     opts, _ = parser.parse_known_args(sys.argv[1:])
+     usr_share = os.path.join(sys.prefix, "share")
+-    if not os.access(opts.root_path + usr_share, os.W_OK) and \
+-       not os.access(opts.root_path, os.W_OK):
+-        if 'XDG_DATA_HOME' in os.environ.keys():
+-            usr_share = os.environ['$XDG_DATA_HOME']
+-        else:
+-            usr_share = os.path.expanduser('~/.local/share')
      data_files += [
          (os.path.join(usr_share, 'applications/'), ['electrum.desktop']),
          (os.path.join(usr_share, 'pixmaps/'), ['icons/electrum.png'])
