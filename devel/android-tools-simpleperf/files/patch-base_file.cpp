fa7c043c9ba6e3300517075dd78fd3b6345f65ce
jbeich <jbeich@FreeBSD.org>
2016-09-17 12:34:20 +0000

    devel/android-tools-simpleperf: update to 7.0.0.r1 (nougat)
    Note, reading .gnu_debugdata (lzma code) isn't supported in the release.
    
    Changes:	https://android.googlesource.com/platform/system/extras/+log/ea4a5a4..android-7.0.0_r1/simpleperf

--- devel/android-tools-simpleperf/files/patch-base_file.cpp
+++ devel/android-tools-simpleperf/files/patch-base_file.cpp
@@ -1,10 +1,11 @@
---- base/file.cpp.orig	2015-08-12 23:28:08 UTC
+--- base/file.cpp.orig	2016-08-08 21:10:17 UTC
 +++ base/file.cpp
-@@ -21,6 +21,7 @@
+@@ -21,6 +21,8 @@
  #include <sys/stat.h>
  #include <sys/types.h>
  
++#include <cstdio> // BUFSIZ
 +#include <cstring> // strerror
  #include <string>
  
- #include "base/macros.h"  // For TEMP_FAILURE_RETRY on Darwin.
+ #include "android-base/macros.h"  // For TEMP_FAILURE_RETRY on Darwin.
