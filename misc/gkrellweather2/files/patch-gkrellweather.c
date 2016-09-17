932e18ca26307093b37a095c088ace281800daf6
cpm <cpm@FreeBSD.org>
2016-09-17 10:34:56 +0000

    - Add LICENSE_FILE - Update patch due another URL change - Regenerate patches with 'make makepatch' (pet portlint) - Update pkg-descr - Bump PORTREVISION
    PR:		212693
    Submitted by:	etteldor@gmail.com
    Approved by:	amdmi3 (mentor)

--- misc/gkrellweather2/files/patch-gkrellweather.c
+++ misc/gkrellweather2/files/patch-gkrellweather.c
@@ -1,6 +1,15 @@
---- gkrellweather.c.orig	Mon Mar 31 13:12:57 2003
-+++ gkrellweather.c	Sun Jul 13 14:30:14 2003
-@@ -986,7 +986,7 @@
+--- gkrellweather.c.orig	2009-01-11 18:15:02 UTC
++++ gkrellweather.c
+@@ -777,7 +777,7 @@ create_air_tab (GtkWidget *tab)
+         " switch interval - number of seconds (0 = never)" \
+         " between switching display\n")
+     };
+-    static gchar url[] = "http://www.nws.noaa.gov/tg/siteloc.shtml";
++    static gchar url[] = "http://www.nws.noaa.gov/tg/siteloc.php";
+     laptop = gtk_notebook_new();
+     gtk_notebook_set_tab_pos(GTK_NOTEBOOK(laptop), GTK_POS_TOP);
+     gtk_box_pack_start(GTK_BOX(tab), laptop, TRUE, TRUE, 0);
+@@ -986,7 +986,7 @@ read_default(void)
      options.windspeed_unit = 1;
      options.pressure_unit = 1;
      strcpy(options.station, DEFAULT_STATION_ID);
@@ -9,7 +18,7 @@
      options.command[511] = 0;
      snprintf(options.filename, 512, "%s/.wmWeatherReports/%s.dat",
               getenv("HOME"), options.station);
-@@ -998,6 +998,7 @@
+@@ -998,6 +998,7 @@ GkrellmMonitor *
  gkrellm_init_plugin(void)
  {
      #ifdef ENABLE_NLS
