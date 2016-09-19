bc987eb5699db8e62ecb4967bb5006f7aadcce7b
Matthew Seaman <matthew@FreeBSD.org>
2016-09-19 13:52:34 +0000

    Fix autoconf detection of pthread_setschedparam() -- the test program needs to be linked against -lpthread.
    Regenerate patches by 'make makepatch'
    
    Take maintainership
    
    PR:		212823
    Approved by:	brd

--- sysutils/rsyslog8/files/patch-grammar_rainerscript.c
+++ sysutils/rsyslog8/files/patch-grammar_rainerscript.c
@@ -1,4 +1,4 @@
---- grammar/rainerscript.c.orig	2015-06-30 12:26:24 UTC
+--- grammar/rainerscript.c.orig	2016-05-30 10:40:35 UTC
 +++ grammar/rainerscript.c
 @@ -35,6 +35,7 @@
  #include <sys/stat.h>
