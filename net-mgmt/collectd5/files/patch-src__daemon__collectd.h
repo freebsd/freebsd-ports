abc9855fa2761a2549b04d5f77961cc09e6167d1
Joseph Mingrone <jrm@FreeBSD.org>
2016-09-26 14:51:59 +0000

    Update net-mgmt/collectd5 to version 5.6.0
    - new port options CHRONY and MQTT
    - disable disk plugin on FreeBSD 9.x due to missing OS files
    - various patch changes
    
    PR:		212832
    Submitted by:	ports@bsdserwis.com (maintainer)
    Reviewed by:	swills (mentor)
    Approved by:	swills (mentor)
    Differential Revision:	https://reviews.freebsd.org/D8028

--- net-mgmt/collectd5/files/patch-src__daemon__collectd.h
+++ net-mgmt/collectd5/files/patch-src__daemon__collectd.h
@@ -1,21 +1,21 @@
---- src/daemon/collectd.h.orig	2016-01-22 09:51:17 UTC
+--- src/daemon/collectd.h.orig	2016-09-11 08:10:25 UTC
 +++ src/daemon/collectd.h
-@@ -240,15 +240,15 @@ typedef int _Bool;
+@@ -243,15 +243,15 @@ typedef int _Bool;
  #endif
  
  #ifndef LOCALSTATEDIR
--#define LOCALSTATEDIR PREFIX "/var"
-+#define LOCALSTATEDIR "/var"
+-# define LOCALSTATEDIR PREFIX "/var"
++# define LOCALSTATEDIR "/var"
  #endif
  
  #ifndef PKGLOCALSTATEDIR
--#define PKGLOCALSTATEDIR PREFIX "/var/lib/" PACKAGE_NAME
-+#define PKGLOCALSTATEDIR PREFIX "/var/db/" PACKAGE_NAME
+-# define PKGLOCALSTATEDIR PREFIX "/var/lib/" PACKAGE_NAME
++# define PKGLOCALSTATEDIR PREFIX "/var/db/" PACKAGE_NAME
  #endif
  
  #ifndef PIDFILE
--#define PIDFILE PREFIX "/var/run/" PACKAGE_NAME ".pid"
-+#define PIDFILE "/var/run/" PACKAGE_NAME ".pid"
+-# define PIDFILE PREFIX "/var/run/" PACKAGE_NAME ".pid"
++# define PIDFILE "/var/run/" PACKAGE_NAME ".pid"
  #endif
  
  #ifndef PLUGINDIR
--- net-mgmt/collectd5/files/patch-src__modbus.c
+++ net-mgmt/collectd5/files/patch-src__modbus.c
@@ -1,8 +1,8 @@
---- src/modbus.c.orig	2016-01-22 09:51:17 UTC
+--- src/modbus.c.orig	2016-09-11 08:10:25 UTC
 +++ src/modbus.c
 @@ -25,6 +25,9 @@
+ #include "common.h"
  #include "plugin.h"
- #include "configfile.h"
  
 +/* FreeBSD needs this */
 +#include <sys/socket.h> 
