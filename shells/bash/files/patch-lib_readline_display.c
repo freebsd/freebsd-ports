f6c26299353d3a2e088fe6b6b2c894dfe695533f
ehaupt <ehaupt@FreeBSD.org>
2016-09-30 17:19:50 +0000

    Update to 4.4
    Differential Revision:  https://reviews.freebsd.org/D8085

--- shells/bash/files/patch-lib_readline_display.c
+++ shells/bash/files/patch-lib_readline_display.c
@@ -1,11 +1,13 @@
---- lib/readline/display.c.orig	2009-01-04 11:32:32.000000000 -0800
-+++ lib/readline/display.c	2009-03-08 00:51:31.000000000 -0800
-@@ -654,5 +654,8 @@ rl_redisplay ()
-   /* inv_lbreaks[i] is where line i starts in the buffer. */
+--- lib/readline/display.c.orig	2016-07-28 18:49:33 UTC
++++ lib/readline/display.c
+@@ -743,7 +743,10 @@ rl_redisplay ()
    inv_lbreaks[newlines = 0] = 0;
+   /* lpos is a physical cursor position, so it needs to be adjusted by the
+      number of invisible characters in the prompt, per line */
 +  if (local_prompt)
    lpos = prompt_physical_chars + modmark;
 +  else
 +    lpos = 0;
  
  #if defined (HANDLE_MULTIBYTE)
+   memset (line_state_invisible->wrapped_line, 0, line_state_invisible->wbsize * sizeof (int));
