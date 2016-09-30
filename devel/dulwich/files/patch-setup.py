a1e43ab1357185ce35d3a70da319c9f6220d88f5
Pawel Pekala <pawel@FreeBSD.org>
2016-09-30 17:56:03 +0000

    - Update to version 0.14.1 - Regenerate patches - Update WWW - Update license info: now dual licensed Apache v2 and GPLv2+   (it's unclear why it was previously labeled as GPLv3)
    PR:		212713
    Submitted by:	matthew@reztek.cz
    Approved by:	maintainer timeout

--- devel/dulwich/files/patch-setup.py
+++ devel/dulwich/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2015-03-25 01:54:01 UTC
+--- setup.py.orig	2016-07-05 22:58:08 UTC
 +++ setup.py
-@@ -75,7 +75,7 @@ setup(name='dulwich',
+@@ -83,7 +83,7 @@ setup(name='dulwich',
        The project is named after the part of London that Mr. and Mrs. Git live in
        in the particular Monty Python sketch.
        """,
