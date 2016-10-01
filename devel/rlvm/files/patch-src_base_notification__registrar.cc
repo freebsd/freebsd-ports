f2ef7e05caa57a2fa95c1f661598e05843413cb6
Jan Beich <jbeich@FreeBSD.org>
2016-10-01 06:32:14 +0000

    devel/rlvm: flatten gcc48 workaround

--- devel/rlvm/files/patch-src_base_notification__registrar.cc
+++ devel/rlvm/files/patch-src_base_notification__registrar.cc
@@ -1,16 +1,17 @@
 https://github.com/eglaysher/rlvm/issues/80
 
-diff --git src/base/notification_registrar.cc src/base/notification_registrar.cc
-index 2b9d99d..2a6f63f 100644
---- src/base/notification_registrar.cc
+--- src/base/notification_registrar.cc.orig	2016-07-30 22:27:57 UTC
 +++ src/base/notification_registrar.cc
-@@ -46,7 +46,8 @@
+@@ -46,7 +46,12 @@ void NotificationRegistrar::Remove(Notif
    }
  
    Record record = { observer, type, source };
--  auto found = std::find(registered_.cbegin(), registered_.cend(), record);
++#if !defined(__clang__) && defined(__GNUC__) && __GNUC__ < 5
 +  RecordVector::iterator found = std::find(
 +      registered_.begin(), registered_.end(), record);
++#else
+   auto found = std::find(registered_.cbegin(), registered_.cend(), record);
++#endif
    registered_.erase(found);
  
    // This can be NULL if our owner outlives the NotificationService, e.g. if our
