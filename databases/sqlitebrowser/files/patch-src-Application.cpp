78e39c6d50174b561754e17658e18b1ca0bebf9f
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2016-10-01 01:05:14 +0000

    databases/sqlitebrowser: update to 3.9.0
    - update to 3.9.0
    - update maintainer's email
    - pet portlint
    
    PR:		212395
    Submitted by:	 lbartoletti <lbartoletti@tuxfamily.org> (maintainer)

--- databases/sqlitebrowser/files/patch-src-Application.cpp
+++ databases/sqlitebrowser/files/patch-src-Application.cpp
@@ -1,6 +1,6 @@
---- src/Application.cpp.orig	2015-06-14 10:13:37 UTC
+--- src/Application.cpp.orig	2016-09-25 11:30:50 UTC
 +++ src/Application.cpp
-@@ -30,8 +30,15 @@ Application::Application(int& argc, char
+@@ -30,8 +30,16 @@ Application::Application(int& argc, char
  
      // First of all try to load the application translation file.
      m_translatorApp = new QTranslator(this);
@@ -13,16 +13,7 @@
      ok = m_translatorApp->load("sqlb_" + name,
                                 QCoreApplication::applicationDirPath() + "/translations");
 +#endif
++
      // If failed then try to load .qm file from resources
      if (ok == false) {
          ok = m_translatorApp->load("sqlb_" + name, ":/translations");
-@@ -51,7 +58,9 @@ Application::Application(int& argc, char
-         ok = m_translatorQt->load("qt_" + name,
-                                   QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-         if (ok == false)
-+	{
-             ok = m_translatorQt->load("qt_" + name, "translations");
-+	}
-         if (ok == true)
-             installTranslator(m_translatorQt);
-     }
--- databases/sqlitebrowser/files/patch-src-PreferencesDialog.cpp
+++ databases/sqlitebrowser/files/patch-src-PreferencesDialog.cpp
@@ -1,18 +1,15 @@
---- src/PreferencesDialog.cpp.orig	2015-04-27 09:31:47 UTC
+--- src/PreferencesDialog.cpp.orig	2016-08-24 11:00:01 UTC
 +++ src/PreferencesDialog.cpp
-@@ -356,8 +356,15 @@ void PreferencesDialog::removeExtension(
+@@ -465,8 +465,13 @@ void PreferencesDialog::removeExtension(
  
  void PreferencesDialog::fillLanguageBox()
  {
--    QDir translationsDir(QCoreApplication::applicationDirPath() + "/translations",
-+
 +#ifdef Q_OS_FREEBSD
 +    QDir translationsDir("%%DATADIR%%/translations",
-                          "sqlb_*.qm");
++                         "sqlb_*.qm");
 +#else
-+     QDir translationsDir(QCoreApplication::applicationDirPath() + "/translations",
-+                          "sqlb_*.qm");
-+
+     QDir translationsDir(QCoreApplication::applicationDirPath() + "/translations",
+                          "sqlb_*.qm");
 +#endif
  
      QLocale systemLocale = QLocale::system();
