8d63686d3008435aa6a3cde91bb406d6c4e9bd7a
rm <rm@FreeBSD.org>
2016-10-01 16:22:16 +0000

    net/py-pcapy: update to 0.10.10
    - update to 0.10.10
    - pass maintainership to submitter
    - expand python version to 2.7+ because since this release pcapy now
      supports both python branches
    
    PR:		213105
    Submitted by:	 Mageirias Anastasios <anastasios@mageirias.com>

--- net/py-pcapy/files/patch-setup.py
+++ net/py-pcapy/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2015-05-19 20:23:13 UTC
+--- setup.py.orig	2016-09-29 12:25:59 UTC
 +++ setup.py
-@@ -61,8 +61,5 @@ setup(name = PACKAGE_NAME,
-           include_dirs = include_dirs,
-           library_dirs = library_dirs,
-           libraries = libraries)],
--      scripts = ['tests/pcapytests.py', 'tests/96pings.pcap'],
--      data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
--                     ['README', 'LICENSE', 'pcapy.html'])],
+@@ -71,8 +71,4 @@ setup(name=PACKAGE_NAME,
+           library_dirs=library_dirs,
+           libraries=libraries)],
+       # scripts=['tests/pcapytests.py', 'tests/96pings.pcap'],
+-      data_files=[
+-          (os.path.join('share', 'doc', PACKAGE_NAME),
+-              ['README', 'LICENSE', 'pcapy.html']),
+-          ('tests', ['tests/pcapytests.py', 'tests/96pings.pcap'])]
        )
- 
