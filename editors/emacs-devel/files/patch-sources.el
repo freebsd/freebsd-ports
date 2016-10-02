5b974a37af175c2ea24904cef7a394b8be873807
Ashish SHUKLA <ashish@FreeBSD.org>
2016-10-02 05:07:22 +0000

    GNU Emacs updates
    editors/emacs:
    - Update to 25.1[1]
    - Update CANNA patchset[2]
    - Cleanup CONFLICTS_INSTALL list[1]
    - Add LLDB option for lldb support (provided by 3rd party) to GUD[1]
    - Add various other options, and re-order options[1]
    - Incorporate games stuff in pkg-plist[1]
    - Remove outdated diffs[1]
    
    editors/emacs-nox11:
    - Update to 25.1[3]
    - Cleanup CONFLICTS_INSTALL list[3]
    
    editors/emacs-devel:
    - Update to git revision 25dca60d
    
    Mk/bsd.emacs.mk:
    - Update version numbers[4]
    
    PR:		212807 [1], 212808 [3], 212809 [4]
    Submitted by:	Joseph Mingrone <jrm@ftfl.ca> [1][3][4]
    		TAKANO Yuji <takachan@running-dog.net> (private email) [2]

--- editors/emacs-devel/files/patch-sources.el
+++
@@ -1,7 +0,0 @@
---- sources.el.orig	2016-02-01 06:06:37 UTC
-+++ sources.el
-@@ -0,0 +1,4 @@
-+;;; Path to Emacs C Sources.
-+(when (string-match (regexp-quote "%%EMACS_VER%%") emacs-version)
-+  (setq find-function-C-source-directory
-+        "%%DATADIR%%/%%EMACS_VER%%/src"))
