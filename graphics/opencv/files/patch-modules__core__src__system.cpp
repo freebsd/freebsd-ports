5c157a4d01c27127a8a0b49224d06dc8c4aad527
jbeich <jbeich@FreeBSD.org>
2016-10-04 15:51:35 +0000

    MFH: r423215
    graphics/opencv: update to 2.4.13.1
    
    Changes:	https://github.com/opencv/opencv/compare/2.4.9...2.4.13.1
    ABI:		https://abi-laboratory.pro/tracker/timeline/opencv/
    Approved by:	ports-secteam (feld)

--- graphics/opencv/files/patch-modules__core__src__system.cpp
+++ graphics/opencv/files/patch-modules__core__src__system.cpp
@@ -1,15 +1,15 @@
---- ./modules/core/src/system.cpp.orig	2013-11-25 05:42:32.000000000 -0500
-+++ ./modules/core/src/system.cpp	2013-11-25 05:42:42.000000000 -0500
-@@ -126,7 +126,7 @@
+--- modules/core/src/system.cpp.orig	2016-09-16 15:55:58 UTC
++++ modules/core/src/system.cpp
+@@ -157,7 +157,7 @@ std::wstring GetTempFileNameWinRT(std::w
  
  #include <stdarg.h>
  
--#if defined __linux__ || defined __APPLE__ || defined __EMSCRIPTEN__
-+#if defined __linux__ || defined __APPLE__ || defined __EMSCRIPTEN__ || defined __FreeBSD__
+-#if defined __linux__ || defined __APPLE__ || defined __EMSCRIPTEN__ || defined __QNX__
++#if defined __linux__ || defined __APPLE__ || defined __EMSCRIPTEN__ || defined __QNX__ || defined __FreeBSD__
  #include <unistd.h>
  #include <stdio.h>
  #include <sys/types.h>
-@@ -897,14 +897,23 @@
+@@ -942,14 +942,23 @@ struct Mutex::Impl
  
  struct Mutex::Impl
  {
@@ -22,19 +22,19 @@
 +        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
 +        pthread_mutex_init(&mt, &attr);
 +        pthread_mutexattr_destroy(&attr);
- 
--    void lock() { pthread_mutex_lock(&sl); }
--    bool trylock() { return pthread_mutex_trylock(&sl) == 0; }
--    void unlock() { pthread_mutex_unlock(&sl); }
++
 +        refcount = 1;
 +    }
 +    ~Impl() { pthread_mutex_destroy(&mt); }
  
--    pthread_mutex_t sl;
+-    void lock() { pthread_mutex_lock(&sl); }
+-    bool trylock() { return pthread_mutex_trylock(&sl) == 0; }
+-    void unlock() { pthread_mutex_unlock(&sl); }
 +    void lock() { pthread_mutex_lock(&mt); }
 +    bool trylock() { return pthread_mutex_trylock(&mt) == 0; }
 +    void unlock() { pthread_mutex_unlock(&mt); }
-+
+ 
+-    pthread_mutex_t sl;
 +    pthread_mutex_t mt;
      int refcount;
  };
