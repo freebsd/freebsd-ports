2c49b21e676269183b57f188619281daf52d3687
brnrd <brnrd@FreeBSD.org>
2016-10-04 21:07:00 +0000

    irc/weechat: Update to 1.6
      - Update to 1.6
      - Take maintainership
      - OPT_CMAKE_ON= -DENABLE_OPT=yes to OPT_CMAKE_BOOL= OPT
      - Add LICENSE_FILE
      - Drop BSD_LUA_* extra CMAKE_ARGS
      - Add asciidoctor depend for DOCS and MANPAGES
      - Fix build with Python 3.5 / Add warning
      - Transform to post-patch-ICON-off target
    
    PR:		205981
    Reported by:	Vladimir Krstulja <vlad-fbsd@acheronmedia.com>

--- irc/weechat/files/patch-cmake_FindLua.cmake
+++
@@ -1,9 +0,0 @@
---- cmake/FindLua.cmake.orig	2016-01-10 08:17:50 UTC
-+++ cmake/FindLua.cmake
-@@ -35,5 +35,5 @@ endif()
- 
- find_package(PkgConfig)
- if(PKG_CONFIG_FOUND)
--  pkg_search_module(LUA lua5.3 lua-5.3 lua53 lua5.2 lua-5.2 lua52 lua5.1 lua-5.1 lua51 lua-5.0 lua5.0 lua50 lua)
-+  pkg_search_module(LUA lua-${BSD_LUA_VER})
- endif()
