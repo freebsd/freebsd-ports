2cff5da1db9112fa4faad6029b216a14bb94eaf7
madpilot <madpilot@FreeBSD.org>
2016-10-04 12:44:05 +0000

    - Update to 1.10.5 - Remove patches which have been accepted upstream

--- sysutils/conky/files/patch-src_luamm.hh
+++ sysutils/conky/files/patch-src_luamm.hh
@@ -1,16 +1,6 @@
---- src/luamm.hh.orig	2016-07-20 16:53:48 UTC
+--- src/luamm.hh.orig	2016-10-04 07:49:19 UTC
 +++ src/luamm.hh
-@@ -25,7 +25,9 @@
- #include <assert.h>
- #include <memory>
- #include <mutex>
-+#include <exception>
- #include <stdexcept>
-+#include <string>
- 
- #include <lua.hpp>
- 
-@@ -210,7 +212,7 @@ namespace lua {
+@@ -212,7 +212,7 @@ namespace lua {
  		bool islightuserdata(int index) throw() { return lua_islightuserdata(cobj.get(), index); }
  		bool isnil(int index) throw() { return lua_isnil(cobj.get(), index); }
  		bool isnone(int index) throw() { return lua_isnone(cobj.get(), index); }
