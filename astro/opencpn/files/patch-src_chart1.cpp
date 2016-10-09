070ac46e95c987de4fe52db831ea79d390992e6c
Jan Beich <jbeich@FreeBSD.org>
2016-10-09 01:22:59 +0000

    astro/opencpn: add new port
    PR:		211199
    Submitted by:	ml@netfence.it (based on)
    
    OpenCPN is a free software project to create a concise chart plotter and
    navigation software, for use underway or as a planning tool. OpenCPN is
    developed by a team of active sailors using real world conditions for
    program testing and refinement.
    
    http://opencpn.org/

---
+++ astro/opencpn/files/patch-src_chart1.cpp
@@ -0,0 +1,39 @@
+--- src/chart1.cpp.orig	2016-06-25 12:26:20 UTC
++++ src/chart1.cpp
+@@ -10308,7 +10308,11 @@ extern "C" int wait(int *);             
+ 
+ #include <termios.h>
+ #include <sys/ioctl.h>
++#ifdef __linux__
+ #include <linux/serial.h>
++#else
++#include <termios.h>
++#endif
+ 
+ #endif
+ 
+@@ -10374,16 +10378,24 @@ int paternFilter (const struct dirent * 
+ 
+ int isTTYreal(const char *dev)
+ {
++#ifdef __linux__
+     struct serial_struct serinfo;
++#else
++    struct termios termAttr;
++#endif
+     int ret = 0;
+ 
+     int fd = open(dev, O_RDWR | O_NONBLOCK | O_NOCTTY);
+ 
+     // device name is pointing to a real device
+     if(fd >= 0) {
++#ifdef __linux__
+         if (ioctl(fd, TIOCGSERIAL, &serinfo)==0) {
+             // If device type is no PORT_UNKNOWN we accept the port
+             if (serinfo.type != PORT_UNKNOWN)
++#else
++        if (tcgetattr(fd, &termAttr) == 0) {
++#endif
+                 ret = 1;
+         }
+         close (fd);
