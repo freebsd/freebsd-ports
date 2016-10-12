1c60cbe5b811cc9ba19f5d480479c6063ca98cda
osa <osa@FreeBSD.org>
2016-10-12 00:44:51 +0000

    Upgrade third-party upstream_sticky module to its latest version 08a395c66e42 to fix crashing with SIGSEGV.
    PR:	213113

--- www/nginx-devel/files/extra-patch-ngx_http_sticky_misc.c
+++ www/nginx-devel/files/extra-patch-ngx_http_sticky_misc.c
@@ -1,5 +1,5 @@
---- ../nginx-goodies-nginx-sticky-module-ng-c825ea7c5c91/ngx_http_sticky_misc.c.orig	2016-07-10 11:39:07.943834000 -0400
-+++ ../nginx-goodies-nginx-sticky-module-ng-c825ea7c5c91/ngx_http_sticky_misc.c	2016-07-10 11:43:05.601078000 -0400
+--- ../nginx-goodies-nginx-sticky-module-ng-08a395c66e42/ngx_http_sticky_misc.c.orig	2016-07-10 11:39:07.943834000 -0400
++++ ../nginx-goodies-nginx-sticky-module-ng-08a395c66e42/ngx_http_sticky_misc.c	2016-07-10 11:43:05.601078000 -0400
 @@ -12,6 +12,14 @@
  
  #include "ngx_http_sticky_misc.h"
