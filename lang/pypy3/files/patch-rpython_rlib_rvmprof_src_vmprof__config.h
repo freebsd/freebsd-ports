0048f68a61a2e13b4ca68825add4ce83d28ae673
dbn <dbn@FreeBSD.org>
2016-10-14 14:26:34 +0000

    lang/pypy3: update to 5.5.0
     - Remove pacthes that have been upstreamed
     - Fix detection of 'struct winsize'
     - Correctly handle case when ftime(3) is not used (depreciated)

--- lang/pypy3/files/patch-rpython_rlib_rvmprof_src_vmprof__config.h
+++
@@ -1,9 +0,0 @@
---- rpython/rlib/rvmprof/src/vmprof_config.h.orig	2016-05-29 20:38:19 UTC
-+++ rpython/rlib/rvmprof/src/vmprof_config.h
-@@ -1,5 +1,5 @@
- #define HAVE_SYS_UCONTEXT_H
--#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
-   #ifdef __i386__
-     #define PC_FROM_UCONTEXT uc_mcontext.mc_eip
-   #else
