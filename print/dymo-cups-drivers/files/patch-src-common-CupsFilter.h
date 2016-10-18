2bd5d32360db74775916f4ccd7c2ffda84ff54c3
Tijl Coosemans <tijl@FreeBSD.org>
2016-10-18 13:14:41 +0000

    Fix build with CUPS 2.2.x.
    Reported by:	hselasky

---
+++ print/dymo-cups-drivers/files/patch-src-common-CupsFilter.h
@@ -0,0 +1,10 @@
+--- src/common/CupsFilter.h.orig	2012-02-07 13:22:37 UTC
++++ src/common/CupsFilter.h
+@@ -22,6 +22,7 @@
+ #define hfc4bbdea_8a1b_427c_9ab5_50b84576b19e
+ 
+ #include <cups/cups.h>
++#include <cups/ppd.h>
+ #include <cups/raster.h>
+ #include <memory>
+ #include <string>
