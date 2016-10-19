c03f6ea0472fd25c49b60b3fe69477b67d8fe3d9
martymac <martymac@FreeBSD.org>
2016-10-19 10:20:01 +0000

    Update to 1.97.2

--- benchmarks/bonnie++/files/patch-rand.h
+++ benchmarks/bonnie++/files/patch-rand.h
@@ -1,14 +1,14 @@
---- rand.h.orig	2003-01-08 19:52:53 UTC
-+++ rand.h
+--- rand.h.orig	2016-06-30 09:38:55.000000000 +0000
++++ rand.h	2016-10-18 20:04:31.159630423 +0000
 @@ -1,11 +1,11 @@
  #ifndef RAND_H
  #define RAND_H
  
 -using namespace std;
  #include "port.h"
+ #include <string>
  #include <stdio.h>
  #include <stdlib.h>
- #include <string>
 +using namespace std;
  
  class Rand
