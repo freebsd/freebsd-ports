d9e66c8d11255b0b57c0523b9cf31108096762c8
amdmi3 <amdmi3@FreeBSD.org>
2016-10-19 16:22:48 +0000

    - Update to 0.07.00
    PR:		213591
    Submitted by:	luca.pizzamiglio@gmail.com (maintainer)

--- benchmarks/stress-ng/files/patch-stress-cpu.c
+++ benchmarks/stress-ng/files/patch-stress-cpu.c
@@ -1,4 +1,4 @@
---- stress-cpu.c.orig	2016-07-15 14:26:33 UTC
+--- stress-cpu.c.orig	2016-10-17 09:59:22 UTC
 +++ stress-cpu.c
 @@ -57,7 +57,6 @@
  #define	ccosl	ccos
@@ -7,4 +7,4 @@
 -#define powl	pow
  #endif
  
- /*
+ #if defined(__minix__)
