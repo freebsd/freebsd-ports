60e51bda11dcc0742419f2a459990d0d81cb07d8
mat <mat@FreeBSD.org>
2016-10-21 15:21:13 +0000

    Use USES=pathfix where applicable.
    PR:		213195
    Submitted by:	mat
    Exp-run by:	antoine
    Sponsored by:	Absolight
    Differential Revision:	https://reviews.freebsd.org/D8093

--- textproc/uim/files/patch-uim_uim.c
+++ textproc/uim/files/patch-uim_uim.c
@@ -1,6 +1,6 @@
---- uim/uim.c.orig	2008-04-29 10:39:34.000000000 +0900
-+++ uim/uim.c	2009-01-24 10:11:56.000000000 +0900
-@@ -90,10 +90,10 @@ fatal_error_hook(void)
+--- uim/uim.c.orig	2013-06-30 04:26:09 UTC
++++ uim/uim.c
+@@ -101,10 +101,10 @@ fatal_error_hook(void)
    uim_fatal_error("an unhandled error raised from Scheme interpreter");
  }
  
@@ -13,7 +13,7 @@
    char *sys_load_path;
  
    if (uim_initialized)
-@@ -108,7 +108,7 @@ uim_init(void)
+@@ -119,7 +119,7 @@ uim_init(void)
    uim_scm_init(sys_load_path);
    uim_scm_set_fatal_error_hook(fatal_error_hook);
  
