a0bcd2cf684ff6bc2faf242e6678282f20a3ee44
Mathieu Arnold <mat@FreeBSD.org>
2016-10-21 15:21:13 +0000

    Use USES=pathfix where applicable.
    PR:		213195
    Submitted by:	mat
    Exp-run by:	antoine
    Sponsored by:	Absolight
    Differential Revision:	https://reviews.freebsd.org/D8093

--- textproc/uim/files/patch-qt4_immodule_plugin.cpp
+++ textproc/uim/files/patch-qt4_immodule_plugin.cpp
@@ -8,9 +8,7 @@ Date:   Sun Mar 2 13:26:18 2014 +0200
       - Stop relying on implicit includes that may not always work and
         explicitly include <clocale> for the setlocale(3) call.
 
-diff --git a/qt4/immodule/plugin.cpp b/qt4/immodule/plugin.cpp
-index cb5863a..be2b9f2 100644
---- qt4/immodule/plugin.cpp
+--- qt4/immodule/plugin.cpp.orig	2013-06-30 04:26:20 UTC
 +++ qt4/immodule/plugin.cpp
 @@ -34,6 +34,8 @@
  
@@ -19,5 +17,5 @@ index cb5863a..be2b9f2 100644
 +#include <clocale>
 +
  #include <QtCore/QStringList>
+ #include <QtGui/QInputContext>
  #ifdef Q_WS_X11
- # include <QtGui/QX11Info>
--- textproc/uim/files/patch-xim_convdisp.cpp
+++ textproc/uim/files/patch-xim_convdisp.cpp
@@ -1,6 +1,6 @@
---- ./xim/convdisp.cpp.orig	2009-01-21 11:16:35.000000000 +0900
-+++ ./xim/convdisp.cpp	2009-01-23 02:41:58.000000000 +0900
-@@ -87,14 +87,14 @@
+--- xim/convdisp.cpp.orig	2013-06-30 04:26:09 UTC
++++ xim/convdisp.cpp
+@@ -87,14 +87,14 @@ init_default_xftfont() {
      gXftFont = XftFontOpen(XimServer::gDpy, DefaultScreen(XimServer::gDpy),
  		    XFT_FAMILY, XftTypeString, fontname,
  		    XFT_PIXEL_SIZE, XftTypeDouble, (double)DEFAULT_FONT_SIZE,
@@ -17,7 +17,7 @@
      }
  }
  
-@@ -112,7 +112,7 @@
+@@ -112,7 +112,7 @@ update_default_xftfont() {
  			DefaultScreen(XimServer::gDpy),
  			XFT_FAMILY, XftTypeString, fontname,
  			XFT_PIXEL_SIZE, XftTypeDouble, (double)DEFAULT_FONT_SIZE,
@@ -26,7 +26,7 @@
  	if (xftfont) {
  	    if (gXftFont)
  		XftFontClose(XimServer::gDpy, gXftFont);
-@@ -387,7 +387,7 @@
+@@ -387,7 +387,7 @@ PeWin::PeWin(Window pw, const char *im_l
  			    DefaultScreen(XimServer::gDpy),
  			    XFT_FAMILY, XftTypeString, gXftFontName,
  			    XFT_PIXEL_SIZE, XftTypeDouble, (double)mXftFontSize,
@@ -35,7 +35,7 @@
  	}
  	mXftDraw = XftDrawCreate(XimServer::gDpy, mPixmap,
  			DefaultVisual(XimServer::gDpy, scr_num),
-@@ -557,7 +557,7 @@
+@@ -557,7 +557,7 @@ void PeWin::set_xftfont(const char *xfld
  			    DefaultScreen(XimServer::gDpy),
  			    XFT_FAMILY, XftTypeString, gXftFontName,
  			    XFT_PIXEL_SIZE, XftTypeDouble, (double)size,
