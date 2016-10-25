1a3f24c8d8dfe3806fe89bd80612da04dac99d45
jrm <jrm@FreeBSD.org>
2016-10-25 17:04:38 +0000

    Include MASTER_SITE_CRAN in Mk/bsd.sites.mk
    With this change, math/R, math/libRmath, and all cran ports can share one
    list of sites.
    
    Reviewed by:	mat
    Approved by:	swills (mentor)
    Differential Revision:	https://reviews.freebsd.org/D8328

--- Mk/Uses/cran.mk
+++ Mk/Uses/cran.mk
@@ -14,20 +14,7 @@
 .if !defined(_INCLUDE_USES_CRAN_MK)
 _INCLUDE_USES_CRAN_MK=	yes
 
-MASTER_SITE_CRAN+=	http://cran.ms.unimelb.edu.au/src/contrib/ \
-			http://mirror.its.dal.ca/cran/src/contrib/ \
-			http://mirrors.dotsrc.org/cran/src/contrib/ \
-			http://cran.univ-lyon1.fr/src/contrib/ \
-			http://ftp5.gwdg.de/pub/misc/cran/src/contrib/ \
-			http://cran.stat.unipd.it/src/contrib/ \
-			http://cran.md.tsukuba.ac.jp/src/contrib/ \
-			http://mirrors.ibiblio.org/pub/mirrors/CRAN/src/contrib/ \
-			http://cran.cnr.berkeley.edu/src/contrib/ \
-			http://cran.rakanu.com/src/contrib/ \
-			http://ftp.ctex.org/mirrors/CRAN/src/contrib/
-MASTER_SITE_CRAN_ARCHIVE+=	${MASTER_SITE_CRAN:S,$,Archive/${PORTNAME}/,}
-
-MASTER_SITES?=	${MASTER_SITE_CRAN} ${MASTER_SITE_CRAN_ARCHIVE}
+MASTER_SITES?=	CRAN/src/contrib CRAN_ARCHIVE/src/contrib
 
 BUILD_DEPENDS+=	${LOCALBASE}/bin/R:math/R
 RUN_DEPENDS+=	${LOCALBASE}/bin/R:math/R
