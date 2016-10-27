2dda16d3b33251e5ff29e176edd0d620001822db
mat <mat@FreeBSD.org>
2016-10-27 14:12:23 +0000

    Update to 2.0.26.
    Sponsored by:	Absolight

--- sysutils/munin-common/munin.mk
+++ sysutils/munin-common/munin.mk
@@ -1,9 +1,9 @@
-MUNIN_VERSION=	2.0.25
-MUNIN_SITES=	SF/${PORTNAME}/stable/${MUNIN_VERSION}
+MUNIN_VERSION=	2.0.26
+MUNIN_SITES=	http://downloads.munin-monitoring.org/munin/stable/${MUNIN_VERSION}/
 DISTINFO_FILE=	${PORTSDIR}/sysutils/munin-common/distinfo
 PATCHDIR=	${.CURDIR}/../../sysutils/munin-common/files
 
-PORTSCOUT=	limit:^2\.0\.
+PORTSCOUT=	limitw:1,even
 
 DBDIR?=		/var/${PORTNAME}
 DBDIRNODE?=	/var/${PORTNAME}
