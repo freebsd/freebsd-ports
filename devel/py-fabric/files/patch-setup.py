264a45853cf4e7261d76dc4a62b652b7ab90bdae
lwhsu <lwhsu@FreeBSD.org>
2016-10-28 11:28:25 +0000

    - Update to 1.12.0
    PR:		211589
    Submitted by:	lwhsu
    Approved by:	koobs (maintainer)

--- devel/py-fabric/files/patch-setup.py
+++ devel/py-fabric/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2016-03-03 08:01:28 UTC
+--- setup.py.orig	2016-05-08 21:11:49 UTC
 +++ setup.py
 @@ -45,7 +45,7 @@ setup(
      url='http://fabfile.org',
      packages=find_packages(),
      test_suite='nose.collector',
--    tests_require=['nose', 'fudge<1.0', 'jinja2'],
+-    tests_require=['nose<2.0', 'fudge<1.0', 'jinja2<3.0'],
 +    tests_require=['nose', 'fudge', 'jinja2'],
      install_requires=install_requires,
      entry_points={
