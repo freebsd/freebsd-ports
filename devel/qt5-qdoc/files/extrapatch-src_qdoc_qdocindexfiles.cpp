42f5d8d0badf3420136e792a0563073e9777db22
tcberner <tcberner@FreeBSD.org>
2016-10-28 13:43:14 +0000

    Update Qt to 5.6.2 [1,2]
    Thanks to the upstream work of Marie Loise Nolden, we could get rid of a handful
    of patches, as they have been properly upstreamed. The rest of the work is just
    some minor plist changes.
    
    I would like to thank Loise <nolden@kde.org> for the upstream work, and Adriaan
    <groot@kde.org> for getting the update into shape.
    
    [1] http://blog.qt.io/blog/2016/10/12/qt-5-6-2-released/
    [2] http://wiki.qt.io/Qt_5.6.2_Change_Files
    
    PR: 213530
    Exp-run by: antoine
    Submitted by: Adriaan de Groot <groot@kde.org>
    Reviewed by: rakuco, mat, tcberner
    Approved by: rakuco (mentor)
    Differential Revision: https://reviews.freebsd.org/D8228

--- devel/qt5-qdoc/files/extrapatch-src_qdoc_qdocindexfiles.cpp
+++ devel/qt5-qdoc/files/extrapatch-src_qdoc_qdocindexfiles.cpp
@@ -8,14 +8,14 @@ qSort instead of std::sort on FreeBSD 9.x.
 [1] https://gcc.gnu.org/bugzilla/show_bug.cgi?id=59391
 [2] https://bugreports.qt.io/browse/QTBUG-43057
 
---- src/qdoc/qdocindexfiles.cpp.orig	2016-10-02 07:52:39 UTC
-+++ src/qdoc/qdocindexfiles.cpp
-@@ -1476,7 +1476,7 @@ void QDocIndexFiles::generateIndexSectio
+--- src/qdoc/qdocindexfiles.cpp.orig    2016-10-09 21:13:42.831072000 +0200
++++ src/qdoc/qdocindexfiles.cpp 2016-10-09 21:13:56.153968000 +0200
+@@ -1412,7 +1412,7 @@
              const Aggregate* inner = static_cast<const Aggregate*>(node);
 
              NodeList cnodes = inner->childNodes();
--            std::sort(cnodes.begin(), cnodes.end(), compareNodes);
-+            qSort(cnodes.begin(), cnodes.end(), compareNodes);
+-            std::sort(cnodes.begin(), cnodes.end(), Node::nodeNameLessThan);
++            qSort(cnodes.begin(), cnodes.end(), Node::nodeNameLessThan);
 
              foreach (Node* child, cnodes) {
                  generateIndexSections(writer, child, generateInternalNodes);
