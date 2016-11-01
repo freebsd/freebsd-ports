3692eb8011984f46494d1ae7381b9a1d35cb061a
jbeich <jbeich@FreeBSD.org>
2016-11-01 15:48:27 +0000

    graphics/aseprite: update to 1.1.9 [1]
    - Apply a workaround for old libc++ on 10.x systems [2]
    
    Changes:	https://www.aseprite.org/release-notes/#aseprite-v1-1
    PR:		213756 [1]
    PR:		213773 [2]
    Submitted by:	yuri@rawbw.com [1]
    Submitted by:	dim [2]

--- graphics/aseprite/files/patch-src_base_file__handle.cpp
+++ graphics/aseprite/files/patch-src_base_file__handle.cpp
@@ -1,6 +1,6 @@
---- src/base/file_handle.cpp.orig	2015-11-06 13:40:13 UTC
+--- src/base/file_handle.cpp.orig	2016-09-23 15:59:20 UTC
 +++ src/base/file_handle.cpp
-@@ -26,6 +26,8 @@
+@@ -27,6 +27,8 @@
  #define O_TEXT    0
  #endif
  
