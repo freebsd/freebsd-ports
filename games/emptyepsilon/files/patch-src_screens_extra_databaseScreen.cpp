9738c3de119219fd0b37181b3fdb7e9d4cebfe4d
jbeich <jbeich@FreeBSD.org>
2016-11-02 06:25:39 +0000

    games/emptyepsilon: add new port
    PR:		213939
    Submitted by:	yuri@rawbw.com
    
    EmptyEpsilon places you in the roles of a spaceship's bridge officers, like
    those seen in Star Trek. While you can play EmptyEpsilon alone or with friends,
    the best experience involves 6 players working together on each ship.
    
    Each officer fills a unique role: Captain, Helms, Weapons, Relay, Science, and
    Engineering. Except for the Captain, each officer operates part of the ship
    through a specialized screen. The Captain relies on their trusty crew to report
    information and follow orders.
    
    http://emptyepsilon.org/

---
+++ games/emptyepsilon/files/patch-src_screens_extra_databaseScreen.cpp
@@ -0,0 +1,8 @@
+--- src/screens/extra/databaseScreen.cpp.orig	2016-08-31 19:10:48 UTC
++++ src/screens/extra/databaseScreen.cpp
+@@ -1,4 +1,5 @@
+ #include "databaseScreen.h"
++#include "scienceDatabase.h"
+ 
+ #include "screenComponents/databaseView.h"
+ 
