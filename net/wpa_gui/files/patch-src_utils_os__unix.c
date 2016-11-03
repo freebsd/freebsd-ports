a4a67c8d186cd67dc9b971c27b26fbbb8294a3a5
Jason E. Hale <jhale@FreeBSD.org>
2016-11-03 09:18:03 +0000

    Add qt4-imageformats runtime dependency so tray icon appears
    While here:
    Remake patches with "make makepatch"
    
    PR:		213783
    Submitted by:	<rs.os@t-online.de>

--- net/wpa_gui/files/patch-src_utils_os__unix.c
+++ net/wpa_gui/files/patch-src_utils_os__unix.c
@@ -1,5 +1,5 @@
---- ../../src/utils/os_unix.c.orig	2015-09-27 19:02:05 UTC
-+++ ../../src/utils/os_unix.c
+--- src/utils/os_unix.c.orig	2015-09-27 19:02:05 UTC
++++ src/utils/os_unix.c
 @@ -23,6 +23,10 @@
  #include <mach/mach_time.h>
  #endif /* __MACH__ */
