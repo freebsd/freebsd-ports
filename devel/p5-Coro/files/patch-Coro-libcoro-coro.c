c041824301e546c917f67adf4e098d3d6c66843b
pi <pi@FreeBSD.org>
2016-11-03 22:58:27 +0000

    devel/p5-Coro: add patch to work with perl 5.24 and up
    - Patch from debian as described in
      https://rt.cpan.org/Public/Bug/Display.html?id=114708
    
    PR:		214168
    Reported by:	tom
    Approved by:	mat

--- devel/p5-Coro/files/patch-Coro-libcoro-coro.c
+++ devel/p5-Coro/files/patch-Coro-libcoro-coro.c
@@ -1,4 +1,4 @@
---- Coro/libcoro/coro.c.orig	2012-12-21 04:57:20 UTC
+--- Coro/libcoro/coro.c.orig	2015-12-10 10:24:21 UTC
 +++ Coro/libcoro/coro.c
 @@ -38,6 +38,7 @@
   * go to Ralf S. Engelschall <rse@engelschall.com>.
