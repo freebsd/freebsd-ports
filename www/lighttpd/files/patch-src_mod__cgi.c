082e190b7aa69ba6ae92d3ad20650948819ba575
madpilot <madpilot@FreeBSD.org>
2016-11-03 16:14:10 +0000

    - Modify patch to check is we are on FreeBSD before checking   __FreeBSD_version. [1] - Explicitly include param.h
    Suggested by:	marino@ [1]

--- www/lighttpd/files/patch-src_mod__cgi.c
+++ www/lighttpd/files/patch-src_mod__cgi.c
@@ -1,11 +1,13 @@
---- src/mod_cgi.c.orig 2016-11-02 13:53:21 UTC
+--- src/mod_cgi.c.orig	2016-10-31 13:16:40 UTC
 +++ src/mod_cgi.c
-@@ -36,7 +36,7 @@
+@@ -35,8 +35,9 @@
+ 
  #include <stdio.h>
  #include <fcntl.h>
-
++#include <sys/param.h>
+ 
 -#if defined(O_CLOEXEC) && (!defined(__FreeBSD__) || defined(F_DUPFD_CLOEXEC))
-+#if defined(O_CLOEXEC) && __FreeBSD_version >= 1000032
++#if defined(O_CLOEXEC) && (!defined(__FreeBSD__) || __FreeBSD_version >= 1000032) 
  #define pipe_cloexec(pipefd) pipe2((pipefd), O_CLOEXEC)
  #elif defined FD_CLOEXEC
  #define pipe_cloexec(pipefd) \
