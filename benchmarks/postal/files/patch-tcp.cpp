76a1600a4bfec4e50a433a6a67b0dd5ea08b325d
marino <marino@FreeBSD.org>
2016-11-05 14:16:28 +0000

    benchmarks/postal: Support DEFAULT_VERSIONS+=ssl
    The newest version of OpenSSL doesn't have SSLv3_method, similar to
    LibreSSL, so update the patches to support both.

--- benchmarks/postal/files/patch-tcp.cpp
+++ benchmarks/postal/files/patch-tcp.cpp
@@ -1,15 +1,23 @@
 --- tcp.cpp.orig	2012-01-01 11:24:45 UTC
 +++ tcp.cpp
-@@ -178,10 +178,10 @@ int tcp::ConnectTLS()
+@@ -178,10 +178,18 @@ int tcp::ConnectTLS()
  #ifdef USE_OPENSSL
    m_sslCtx = NULL;
    m_ssl = NULL;
 -  m_sslMeth = SSLv2_client_method();
++#ifndef OPENSSL_NO_SSL3
 +  m_sslMeth = SSLv3_client_method();
++#else
++  m_sslMeth = SSLv23_client_method();
++#endif
    if(m_sslMeth == NULL)
    {
 -    fprintf(stderr, "Can't get SSLv2_client_method.\n");
++#ifndef OPENSSL_NO_SSL3
 +    fprintf(stderr, "Can't get SSLv3_client_method.\n");
++#else
++    fprintf(stderr, "Can't get SSLv23_client_method.\n");
++#endif
      error();
      return 2;
    }
