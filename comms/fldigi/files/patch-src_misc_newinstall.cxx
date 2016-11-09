f53ad284316b8c26b94c57a4ab2a2de34ae635ca
Diane Bruce <db@FreeBSD.org>
2016-11-09 23:04:59 +0000

    Unbreak fldigi and update to latest version 3.23.15 Move distribution site to sourceforge
    Version 3.23.15 * Maintenance release
    Translation file
     * Updated po/pl.po Polish translation file
     - update provided by Roman Bagiñski, SP4JEU
    
    N3FJP command sequencing
     * changed command/response sequencing to correct
       difference in behavior Windows versus all other platforms.
    
    BARTG vice BART
     * Acronymn correction on Contest configuration panel
    
    speed test
     * improper use of memset
    
    XP fault on failure
     * debug thread timing caused delayed attempt to access
       a deleted Fl_Browser widget in debug panel
       - not evident on other OS or on later versions of Windows
    
    Default Logbook widget sizing
     * Added resizing to all logbook widgets
    
    Mono / Stereo wav file
     * Output monophonic wav file (left channel) unless configured
       for right channel recording
    
    ARQ thread
     * change server thread to make non blocking
    
    N3FJP logged frequency
      * User selectable for ssb Suppressed carrier, or
        ssb Suppressed carrier +/- wf audio injection
        frequency modified if RTTY
    
    update check
     * Use www.w1hkj.com file mirror for version check
    
    DX cluster
     * Add DX cluster access via telnet connection
    
    CQWW zone/state
     * Add parsing and auto log entry for CQzone when passed
       from CQWW's CALLTAB event
     * Fix error in not passing CQstate entry from both log views
    
    Contest fields
     * remove ambiguity for menu items View ... Contest fields
    
    OSX Clang build
     * Correct errors in clang build on OS X 10.11
    
    flrig default
     * Change flrig control default to FALSE

--- comms/fldigi/files/patch-src_misc_newinstall.cxx
+++ comms/fldigi/files/patch-src_misc_newinstall.cxx
@@ -1,5 +1,14 @@
---- src/misc/newinstall.cxx.orig	2015-08-12 20:42:32 UTC
+--- src/misc/newinstall.cxx.orig	2016-11-09 20:36:15 UTC
 +++ src/misc/newinstall.cxx
+@@ -67,7 +67,7 @@ text[3] = " btu <NAME> <CALL> de <MYCALL
+ 
+ label[4] = "SK @||";
+ text[4] = "\n\
+-tnx fer QSO <NAME>, 73, God bless.\n\
++tnx fer QSO <NAME>, 73.\n\
+ <ZDT> <CALL> de <MYCALL> sk\n\
+ <RX>";
+ 
 @@ -84,7 +84,7 @@ Age:   \n\
  Rig:   \n\
  Pwr:   \n\
