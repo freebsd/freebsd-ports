3fb7c23bdb275199e4f273781370b81039095deb
wen <wen@FreeBSD.org>
2016-11-10 08:06:03 +0000

    - Update to 0.1.6
    PR:		214311
    Submitted by:	gor@clogic.com.ua(maintainer)

--- editors/neovim/files/patch-src_nvim_os_pty__process__unix.c
+++ editors/neovim/files/patch-src_nvim_os_pty__process__unix.c
@@ -1,11 +1,11 @@
---- src/nvim/os/pty_process_unix.c.orig	2016-08-22 02:14:28 UTC
+--- src/nvim/os/pty_process_unix.c.orig	2016-11-08 05:50:09 UTC
 +++ src/nvim/os/pty_process_unix.c
 @@ -9,7 +9,7 @@
  #include <sys/ioctl.h>
  
  // forkpty is not in POSIX, so headers are platform-specific
 -#if defined(__FreeBSD__)
-+#if defined(__FreeBSD__) || defined __DragonFly__
++#if defined(__FreeBSD__) || defined (__DragonFly__)
  # include <libutil.h>
  #elif defined(__OpenBSD__) || defined(__NetBSD__) || defined(__APPLE__)
  # include <util.h>
