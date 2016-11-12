6721a47473835e528da7c2e3aa59ca9c19681c4a
Baptiste Daroussin <bapt@FreeBSD.org>
2016-11-12 09:45:22 +0000

    Update libreoffice to 5.2.3 and liborcus to 0.11.2

--- editors/libreoffice/files/patch-solenv_gbuild_platform_unxgcc.mk
+++ editors/libreoffice/files/patch-solenv_gbuild_platform_unxgcc.mk
@@ -1,6 +1,6 @@
---- solenv/gbuild/platform/unxgcc.mk.orig	2015-08-22 06:41:35 UTC
+--- solenv/gbuild/platform/unxgcc.mk.orig	2016-10-28 14:50:26 UTC
 +++ solenv/gbuild/platform/unxgcc.mk
-@@ -41,8 +41,7 @@ gb_CFLAGS := \
+@@ -44,8 +44,7 @@ gb_CFLAGS := \
  gb_CXXFLAGS := \
  	$(gb_CXXFLAGS_COMMON) \
  	-fPIC \
--- editors/libreoffice/files/patch-sw_Module__sw.mk
+++ editors/libreoffice/files/patch-sw_Module__sw.mk
@@ -1,12 +1,22 @@
---- sw/Module_sw.mk.orig	2015-08-22 06:41:35 UTC
+--- sw/Module_sw.mk.orig	2016-10-28 14:50:26 UTC
 +++ sw/Module_sw.mk
-@@ -62,7 +62,8 @@ $(eval $(call gb_Module_add_slowcheck_ta
+@@ -62,13 +62,16 @@ $(eval $(call gb_Module_add_slowcheck_ta
+     CppunitTest_sw_ooxmlfieldexport \
      CppunitTest_sw_ooxmlw14export \
      CppunitTest_sw_ooxmlimport \
-     CppunitTest_sw_ww8export \
+-    CppunitTest_sw_ww8export \
 -    CppunitTest_sw_ww8import \
++    $(if $(filter FREEBSD,$(OS)),, \
++        CppunitTest_sw_ww8export) \
 +    $(if $(filter FREEBSD,$(OS)),, \
 +        CppunitTest_sw_ww8import) \
      CppunitTest_sw_rtfexport \
      CppunitTest_sw_rtfimport \
      CppunitTest_sw_odfexport \
+     CppunitTest_sw_odfimport \
+-    CppunitTest_sw_uiwriter \
++    $(if $(filter FREEBSD,$(OS)),, \
++        CppunitTest_sw_uiwriter) \
+     CppunitTest_sw_mailmerge \
+     CppunitTest_sw_globalfilter \
+ ))
