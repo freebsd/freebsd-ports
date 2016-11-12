1856d0bcbcfe51e3a8ef82e5620d57129e3bed4b
Baptiste Daroussin <bapt@FreeBSD.org>
2016-11-12 14:09:41 +0000

    Fix typo I forgot to commit earlier
    Reported by:	Stari Karp <starikarp@yandex.com>

--- editors/libreoffice/files/patch-sc_Module__sc.mk
+++ editors/libreoffice/files/patch-sc_Module__sc.mk
@@ -5,7 +5,7 @@
  	CppunitTest_sc_condformats \
  	CppunitTest_sc_new_cond_format_api \
 -	CppunitTest_sc_subsequent_filters_test \
-+	$(if $(filter FEEBSD,$(OS)),,
++	$(if $(filter FREEBSD,$(OS)),, \
 +		CppunitTest_sc_subsequent_filters_test) \
  	CppunitTest_sc_subsequent_export_test \
  	CppunitTest_sc_html_export_test \
