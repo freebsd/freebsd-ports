b476df1f331f0c85be230fa4af4747d713d72425
thierry <thierry@FreeBSD.org>
2016-11-13 12:44:13 +0000

    Upgrade Code_Aster to 12.0.6-4.
    This brings french/eficas to 2016.0.1,
    	cad/astk-serveur and astk-client to 1.13.8.2,
    	french/med to 3.2.0 (built against hdf5-18),
    	bump PORTREVSION to math/metis-edf and fix soname,
    	and french/homard to 11.7-1.

--- french/aster/files/patch-products.py
+++ french/aster/files/patch-products.py
@@ -1,16 +1,29 @@
---- products.py.orig	2014-12-19 10:21:30.000000000 +0100
-+++ products.py	2015-02-15 19:39:55.000000000 +0100
-@@ -777,7 +777,8 @@
+--- products.py.orig	2016-09-07 08:44:22 UTC
++++ products.py
+@@ -117,8 +117,8 @@ def setup_hdf5(dep, summary, **kargs):
+ 
+       actions=(
+          ('IsInstalled', { 'filename' :
+-             [osp.join('__setup.installdir__', 'lib', 'libhdf5.a'),
+-              osp.join('__setup.installdir__', 'include', 'hdf5.h')]
++             [osp.join('__setup.installdir__', 'lib', 'libhdf5-18.a'),
++              osp.join('__setup.installdir__', 'include/hdf5-18', 'hdf5.h')]
+          } ),
+          ('Extract'  , {}),
+          # gcc>=4.9 not supported by configure, should not use -ansi
+@@ -842,8 +842,9 @@ def setup_aster(dep, summary, **kargs):
+       req=['ASTER_ROOT', 'ASTER_VERSION',
             'HOME_PYTHON', 'PYTHON_EXE', 'PYTHONLIB',
             'HOME_MUMPS', 'HOME_ZMAT', 'HOME_MPI', 'INCLUDE_MUMPS', 'HOME_METIS',
-            'HOME_MED', 'HOME_HDF', # 'HOME_MFRONT', because of v11 branch
+-           'HOME_MED', 'HOME_HDF', # 'HOME_MFRONT', because of v11 branch
 -           'LD', 'CC', 'F90', 'CXXLIB', 'OTHERLIB', 'SYSLIB', ],
++           'HOME_MED', 'HOME_HDF', 'HOME_MFRONT',
 +           'LD', 'CC', 'F90', 'CXXLIB', 'OTHERLIB', 'SYSLIB', 'CFLAGS', 'LDFLAGS',
 +           'CFLAGS_DBG', 'FFLAGS', 'FFLAGS_DBG','F90FLAGS', 'F90FLAGS_DBG', 'CXXLIB', ],
        reqobj=['file:?ASTER_ROOT?/bin/as_run',
                'file:?ASTER_ROOT?/etc/codeaster/profile.sh'],
     )
-@@ -817,7 +818,8 @@
+@@ -885,7 +886,8 @@ def setup_aster(dep, summary, **kargs):
              'capturestderr' : False,
           }),
           ('Make'  , {
