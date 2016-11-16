e0e4a9fc8207ddf5514820ed300c064d56965598
hrs <hrs@FreeBSD.org>
2016-11-16 00:33:36 +0000

    Fix build.

--- cad/klayout/files/patch-src-layApplication.cc
+++ cad/klayout/files/patch-src-layApplication.cc
@@ -1,5 +1,5 @@
 --- src/layApplication.cc.orig	2016-06-20 01:35:29.000000000 +0900
-+++ src/layApplication.cc	2016-11-05 04:21:44.257764000 +0900
++++ src/layApplication.cc	2016-11-16 08:59:47.386261000 +0900
 @@ -20,6 +20,7 @@
  
  */
@@ -8,7 +8,15 @@
  
  #include "layApplication.h"
  #include "layConfig.h"
-@@ -351,7 +352,7 @@
+@@ -74,6 +75,7 @@
+ #include <memory>
+ #include <algorithm>
+ #include <signal.h>
++#include <unistd.h>
+ 
+ namespace lay
+ {
+@@ -351,7 +353,7 @@
    act.sa_sigaction = signal_handler;
    sigemptyset (&act.sa_mask);
    act.sa_flags = SA_SIGINFO;
