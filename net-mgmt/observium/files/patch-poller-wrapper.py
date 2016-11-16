22eaf1b3ea3c10cbeaca8be78aaf18ecca8c1494
feld <feld@FreeBSD.org>
2016-11-16 15:23:47 +0000

    net-mgmt/observium: Fix shebang in poller-wrapper.py
    While here, modernize port

--- net-mgmt/observium/files/patch-poller-wrapper.py
+++ net-mgmt/observium/files/patch-poller-wrapper.py
@@ -1,11 +1,5 @@
 --- poller-wrapper.py.orig	2014-11-04 16:14:27 UTC
 +++ poller-wrapper.py
-@@ -1,4 +1,4 @@
--#! /usr/bin/env python
-+#! %%LOCALBASE%%/bin/python
- """
-  poller-wrapper A small tool which wraps around the Observium poller
-                 and tries to guide the polling process with a more modern
 @@ -38,6 +38,8 @@ except:
      print "threading, Queue, sys, subprocess, time, os, json"
      sys.exit(2)
