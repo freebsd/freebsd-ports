58d0d3d3d1159f6f517df19bf200d960847c9ec5
jrm <jrm@FreeBSD.org>
2016-11-17 17:43:32 +0000

    security/wpa_supplicant: Update to version 2.6 and patch for LibreSSL support
    Port changes:
    - Remove patches that have been incorporated upstream
    - Add patches for LibreSSL support
    
    Approved by:	AMDmi3 (mentor)
    Differential Revision:	https://reviews.freebsd.org/D8451

--- security/wpa_supplicant/files/patch-wpa__supplicant_main.c
+++ security/wpa_supplicant/files/patch-wpa__supplicant_main.c
@@ -1,6 +1,25 @@
---- wpa_supplicant/main.c.orig	2015-03-15 17:30:39 UTC
+--- wpa_supplicant/main.c.orig	2016-11-05 20:56:30 UTC
 +++ wpa_supplicant/main.c
-@@ -173,6 +173,11 @@ int main(int argc, char *argv[])
+@@ -66,7 +66,7 @@ static void usage(void)
+ 	       "  -c = Configuration file\n"
+ 	       "  -C = ctrl_interface parameter (only used if -c is not)\n"
+ 	       "  -d = increase debugging verbosity (-dd even more)\n"
+-	       "  -D = driver name (can be multiple drivers: nl80211,wext)\n"
++	       "  -D = driver name (can be multiple drivers: bsd,wired)\n"
+ 	       "  -e = entropy file\n"
+ #ifdef CONFIG_DEBUG_FILE
+ 	       "  -f = log output to debug file instead of stdout\n"
+@@ -105,8 +105,7 @@ static void usage(void)
+ 	       "  -W = wait for a control interface monitor before starting\n");
+ 
+ 	printf("example:\n"
+-	       "  wpa_supplicant -D%s -iwlan0 -c/etc/wpa_supplicant.conf\n",
+-	       wpa_drivers[0] ? wpa_drivers[0]->name : "nl80211");
++                "  wpa_supplicant -Dbsd -iwlan0 -c/etc/wpa_supplicant.conf\n");
+ #endif /* CONFIG_NO_STDOUT_DEBUG */
+ }
+ 
+@@ -199,6 +198,11 @@ int main(int argc, char *argv[])
  
  	wpa_supplicant_fd_workaround(1);
  
@@ -11,4 +30,4 @@
 +
  	for (;;) {
  		c = getopt(argc, argv,
- 			   "b:Bc:C:D:de:f:g:G:hi:I:KLm:No:O:p:P:qsTtuvW");
+ 			   "b:Bc:C:D:de:f:g:G:hi:I:KLMm:No:O:p:P:qsTtuvW");
