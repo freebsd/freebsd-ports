918c153569979d9cad039cce8c4e61b0cee917f2
jrm <jrm@FreeBSD.org>
2016-11-17 17:43:32 +0000

    security/wpa_supplicant: Update to version 2.6 and patch for LibreSSL support
    Port changes:
    - Remove patches that have been incorporated upstream
    - Add patches for LibreSSL support
    
    Approved by:	AMDmi3 (mentor)
    Differential Revision:	https://reviews.freebsd.org/D8451

---
+++ security/wpa_supplicant/files/patch-src_crypto_crypto__openssl.c
@@ -0,0 +1,20 @@
+--- src/crypto/crypto_openssl.c.orig	2016-11-02 18:04:18 UTC
++++ src/crypto/crypto_openssl.c
+@@ -611,7 +611,7 @@ void crypto_cipher_deinit(struct crypto_
+ 
+ void * dh5_init(struct wpabuf **priv, struct wpabuf **publ)
+ {
+-#if OPENSSL_VERSION_NUMBER < 0x10100000L
++#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
+ 	DH *dh;
+ 	struct wpabuf *pubkey = NULL, *privkey = NULL;
+ 	size_t publen, privlen;
+@@ -712,7 +712,7 @@ err:
+ 
+ void * dh5_init_fixed(const struct wpabuf *priv, const struct wpabuf *publ)
+ {
+-#if OPENSSL_VERSION_NUMBER < 0x10100000L
++#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
+ 	DH *dh;
+ 
+ 	dh = DH_new();
