248220d128d9477fc4cd40b37a40b3706e6a0744
sunpoet <sunpoet@FreeBSD.org>
2016-11-20 23:53:40 +0000

    - Update to 2.1.2 - Regenerate patch files with makepatch:
    Changes:	https://trac.osgeo.org/gdal/wiki/Release/2.1.2-News
    		https://trac.osgeo.org/gdal/wiki/Release/2.1.1-News

--- graphics/php-gdal/files/patch-gdal_wrap.cpp
+++ graphics/php-gdal/files/patch-gdal_wrap.cpp
@@ -1,6 +1,6 @@
---- gdal_wrap.cpp.orig	2012-10-09 08:58:28.000000000 +0800
-+++ gdal_wrap.cpp	2013-03-02 01:37:03.203201081 +0800
-@@ -932,7 +932,7 @@
+--- gdal_wrap.cpp.orig	2012-10-09 00:58:28 UTC
++++ gdal_wrap.cpp
+@@ -932,7 +932,7 @@ SWIG_ZTS_ConvertResourcePtr(zval *z, swi
    p = value->ptr;
    if (type==-1) return NULL;
  
@@ -9,7 +9,7 @@
  
    return SWIG_ZTS_ConvertResourceData(p, type_name, ty TSRMLS_CC);
  }
-@@ -1423,6 +1423,7 @@
+@@ -1423,6 +1423,7 @@ t_output_helper( zval **target, zval *o)
      return;
    }
    if ( (*target)->type == IS_NULL ) {
