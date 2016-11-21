0cbafb9b00e55cf07c2487037e534f421ea29ee3
amdmi3 <amdmi3@FreeBSD.org>
2016-11-21 14:52:46 +0000

    - Fix license - Switch to USES=localbase - makeparch - Take maintainership

--- games/qonk/files/patch-src-menumanager.h
+++ games/qonk/files/patch-src-menumanager.h
@@ -1,5 +1,5 @@
---- src/menumanager.h.orig	Thu Sep 20 22:01:32 2007
-+++ src/menumanager.h	Thu Sep 20 21:58:13 2007
+--- src/menumanager.h.orig	2007-09-14 05:43:03 UTC
++++ src/menumanager.h
 @@ -7,6 +7,8 @@
  #ifndef MENUMANAGER_H
  #define MENUMANAGER_H
