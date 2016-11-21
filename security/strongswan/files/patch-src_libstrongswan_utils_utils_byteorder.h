666557f5fb36523a409de127dcce57b7e41d03cb
garga <garga@FreeBSD.org>
2016-11-21 10:43:45 +0000

    Update security/strongswan to 5.5.1
    PR:		213844
    Approved by:	strongswan@Nanoteq.com (maintainer)
    Obtained from:	pfSense
    Sponsored by:	Rubicon Communications (Netgate)

--- security/strongswan/files/patch-src_libstrongswan_utils_utils_byteorder.h
+++ security/strongswan/files/patch-src_libstrongswan_utils_utils_byteorder.h
@@ -1,4 +1,4 @@
---- src/libstrongswan/utils/utils/byteorder.h.orig	2016-03-29 06:56:52 UTC
+--- src/libstrongswan/utils/utils/byteorder.h.orig	2016-08-16 15:24:34 UTC
 +++ src/libstrongswan/utils/utils/byteorder.h
 @@ -22,6 +22,10 @@
  #ifndef BYTEORDER_H_
