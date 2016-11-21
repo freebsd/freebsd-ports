57763638bad3af912c4a22c7a860bf46ac75cbae
amdmi3 <amdmi3@FreeBSD.org>
2016-11-21 14:52:46 +0000

    - Fix license - Switch to USES=localbase - makeparch - Take maintainership

--- games/qonk/files/patch-src_sdl__driver.cpp
+++ games/qonk/files/patch-src_sdl__driver.cpp
@@ -1,5 +1,5 @@
---- src/sdl_driver.cpp.orig	2007-09-14 09:43:03.000000000 +0400
-+++ src/sdl_driver.cpp	2015-03-27 02:55:29.588996000 +0300
+--- src/sdl_driver.cpp.orig	2007-09-14 05:43:03 UTC
++++ src/sdl_driver.cpp
 @@ -5,6 +5,7 @@
  #include <SDL/SDL.h>
  #include <SDL/SDL_ttf.h>
