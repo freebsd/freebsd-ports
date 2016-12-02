c67cb29943f48e2482e7f3b167d28a828bce7a20
Dirk Meyer <dinoex@FreeBSD.org>
2016-12-02 08:10:20 +0000

    - fix dependency on kde4 - fix build - add LICENSE - add patches from NetBSD

--- graphics/gnash/files/patch-gui-gtk-gtk_canvas.cpp
+++ graphics/gnash/files/patch-gui-gtk-gtk_canvas.cpp
@@ -1,11 +1,11 @@
---- gui/gtk/gtk_canvas.cpp.orig	2011-02-26 21:11:07.000000000 +0300
-+++ gui/gtk/gtk_canvas.cpp	2011-09-06 23:13:28.000000000 +0400
-@@ -207,7 +207,7 @@
+--- gui/gtk/gtk_canvas.cpp.orig	2012-01-19 19:17:48 UTC
++++ gui/gtk/gtk_canvas.cpp
+@@ -212,7 +212,7 @@ gnash_canvas_setup(GnashCanvas *canvas, 
          renderer = "agg";
  #elif defined (RENDERER_CAIRO)
          renderer = "cairo";
 -#elif defined (RENDERER_OGL)
 +#elif defined (RENDERER_OPENGL)
          renderer = "opengl";
- #endif
-     }
+ #elif defined (RENDERER_OPENVG)
+         renderer = "openvg";
