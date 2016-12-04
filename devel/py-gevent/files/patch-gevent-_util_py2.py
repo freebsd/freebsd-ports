d4eb70fc402714d5b127d8acff9b2a2ba8c33913
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2016-12-04 10:43:59 +0000

    - Fix _util_py2 build failure with Python 3 - Bump PORTREVISION for package change

--- devel/py-gevent/files/patch-gevent-_util_py2.py
+++
@@ -1,8 +0,0 @@
---- gevent/_util_py2.py.orig	2015-12-11 14:42:42 UTC
-+++ gevent/_util_py2.py
-@@ -5,4 +5,4 @@ __all__ = ['reraise']
- 
- 
- def reraise(type, value, tb):
--    raise type, value, tb
-+    raise type(value).with_traceback(tb)
