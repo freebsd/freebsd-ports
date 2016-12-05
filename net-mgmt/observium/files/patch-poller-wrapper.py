60ec7d981b27918cc3d597eedee5bb5eb7a954d0
Mark Felder <feld@FreeBSD.org>
2016-12-05 00:22:42 +0000

    net-mgmt/observium: Fix poller-wrapper.py
    It was attempting to set PATH before importing os module
    
    Reported by:	Adrian Murphy <amurphy [AT] gsoft com au>

--- net-mgmt/observium/files/patch-poller-wrapper.py
+++ net-mgmt/observium/files/patch-poller-wrapper.py
@@ -1,11 +1,18 @@
---- poller-wrapper.py.orig	2014-11-04 16:14:27 UTC
+--- poller-wrapper.py.orig	2016-12-05 00:19:29 UTC
 +++ poller-wrapper.py
-@@ -38,6 +38,8 @@ except:
-     print "threading, Queue, sys, subprocess, time, os, json"
+@@ -45,6 +45,8 @@ except:
+     print("threading, sys, subprocess, time, os, json")
      sys.exit(2)
  
 +os.environ['PATH'] += ':%%LOCALBASE%%/sbin:%%LOCALBASE%%/bin'
 +
- try:
-     import MySQLdb
- except:
+ # start time
+ s_time = time.time()
+ 
+@@ -608,4 +610,4 @@ db.close()
+ # Return exit code
+ sys.exit(exit_code)
+ 
+-# EOF
+\ No newline at end of file
++# EOF
