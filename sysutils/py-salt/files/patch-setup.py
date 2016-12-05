04a7c6fc7a59ab3bfbe238f8935c73ccc281e069
swills <swills@FreeBSD.org>
2016-12-05 15:00:49 +0000

    sysutils/py-salt : update to 2016.3.4
    While here, fix OpenSSL 1.1 compatibility
    
    PR:		214786
    PR:		214998
    PR:		215051
    Submitted by:	Christer Edwards <christer.edwards@gmail.com> (maintainer)
    Reported by:	Melvyn Sopacua <m.r.sopacua@gmail.com> (214998)
    Obtained from:	https://github.com/saltstack/salt/pull/37772 (215051)

--- sysutils/py-salt/files/patch-setup.py
+++ sysutils/py-salt/files/patch-setup.py
@@ -1,12 +1,12 @@
---- setup.py.orig	2016-05-25 22:30:31 UTC
+--- setup.py.orig	2016-11-22 16:50:54 UTC
 +++ setup.py
-@@ -986,8 +986,8 @@ class SaltDistribution(distutils.dist.Di
+@@ -988,8 +988,8 @@ class SaltDistribution(distutils.dist.Di
      def _property_data_files(self):
          # Data files common to all scenarios
          data_files = [
--            ('share/man/man1', ['doc/man/salt-call.1']),
+-            ('share/man/man1', ['doc/man/salt-call.1', 'doc/man/salt-run.1']),
 -            ('share/man/man7', ['doc/man/salt.7'])
-+            ('man/man1', ['doc/man/salt-call.1']),
++            ('man/man1', ['doc/man/salt-call.1', 'doc/man/salt-run.1']),
 +            ('man/man7', ['doc/man/salt.7'])
          ]
          if self.ssh_packaging or PACKAGED_FOR_SALT_SSH:
