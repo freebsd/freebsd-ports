1bf787c8c162c921d5d6d8e82e6c50b9801c64d4
osa <osa@FreeBSD.org>
2016-12-06 01:38:31 +0000

    Upgrade from 3.2.5 to 4.0-rc1.
    ChangeLog:	https://raw.githubusercontent.com/antirez/redis/4.0/00-RELEASENOTES

--- databases/redis-devel/files/extra-patch-src-Makefile.lua
+++ databases/redis-devel/files/extra-patch-src-Makefile.lua
@@ -1,47 +1,58 @@
---- src/Makefile.orig	2016-02-02 19:44:07.760203000 -0500
-+++ src/Makefile	2016-02-02 19:47:01.843358000 -0500
+--- src/Makefile.orig	2016-12-04 19:56:10.186400000 -0500
++++ src/Makefile	2016-12-04 20:01:02.427294000 -0500
 @@ -15,7 +15,7 @@
  release_hdr := $(shell sh -c './mkreleasehdr.sh')
  uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
  OPTIMIZATION?=-O2
--DEPENDENCY_TARGETS=hiredis linenoise lua geohash-int
-+DEPENDENCY_TARGETS=hiredis linenoise geohash-int
+-DEPENDENCY_TARGETS=hiredis linenoise lua
++DEPENDENCY_TARGETS=hiredis linenoise
+ NODEPS:=clean distclean
  
  # Default settings
- STD=-std=c99 -pedantic -DREDIS_STATIC=''
-@@ -53,7 +53,7 @@
+@@ -54,8 +54,8 @@
  # Override default settings if possible
  -include .make-settings
  
--FINAL_CFLAGS=$(STD) $(WARN) $(OPT) $(DEBUG) $(CFLAGS) $(REDIS_CFLAGS) -I../deps/geohash-int
-+FINAL_CFLAGS=$(STD) $(WARN) $(OPT) $(DEBUG) $(CFLAGS) -I../deps/geohash-int
- FINAL_LDFLAGS=$(LDFLAGS) $(REDIS_LDFLAGS) $(DEBUG)
+-FINAL_CFLAGS=$(STD) $(WARN) $(OPT) $(DEBUG) $(CFLAGS) $(REDIS_CFLAGS)
+-FINAL_LDFLAGS=$(LDFLAGS) $(REDIS_LDFLAGS) $(DEBUG)
++FINAL_CFLAGS=$(STD) $(WARN) $(OPT) $(DEBUG) $(CFLAGS)
++FINAL_LDFLAGS=$(LDFLAGS) $(DEBUG)
  FINAL_LIBS=-lm
- DEBUG=-g -ggdb
-@@ -98,6 +98,9 @@
- 	FINAL_LIBS+= ../deps/jemalloc/lib/libjemalloc.a -ldl
+ 
+ ifeq ($(uname_S),SunOS)
+@@ -96,7 +96,7 @@
+ endif
+ endif
+ # Include paths to dependencies
+-FINAL_CFLAGS+= -I../deps/hiredis -I../deps/linenoise -I../deps/lua/src
++FINAL_CFLAGS+= -I../deps/hiredis -I../deps/linenoise
+ 
+ ifeq ($(MALLOC),tcmalloc)
+ 	FINAL_CFLAGS+= -DUSE_TCMALLOC
+@@ -114,6 +114,9 @@
+ 	FINAL_LIBS+= ../deps/jemalloc/lib/libjemalloc.a
  endif
  
-+FINAL_CFLAGS+= -I${PREFIX}/include/lua51
++FINAL_CFLAGS+=-I${PREFIX}/include/lua51
 +FINAL_LIBS+= -L${PREFIX}/lib -llua-5.1
 +
  REDIS_CC=$(QUIET_CC)$(CC) $(FINAL_CFLAGS)
  REDIS_LD=$(QUIET_LINK)$(CC) $(FINAL_LDFLAGS)
  REDIS_INSTALL=$(QUIET_INSTALL)$(INSTALL)
-@@ -118,6 +121,7 @@
+@@ -134,6 +137,7 @@
  REDIS_SERVER_NAME=redis-server
  REDIS_SENTINEL_NAME=redis-sentinel
- REDIS_SERVER_OBJ=adlist.o quicklist.o ae.o anet.o dict.o server.o sds.o zmalloc.o lzf_c.o lzf_d.o pqsort.o zipmap.o sha1.o ziplist.o release.o networking.o util.o object.o db.o replication.o rdb.o t_string.o t_list.o t_set.o t_zset.o t_hash.o config.o aof.o pubsub.o multi.o debug.o sort.o intset.o syncio.o cluster.o crc16.o endianconv.o slowlog.o scripting.o bio.o rio.o rand.o memtest.o crc64.o bitops.o sentinel.o notify.o setproctitle.o blocked.o hyperloglog.o latency.o sparkline.o redis-check-rdb.o geo.o
+ REDIS_SERVER_OBJ=adlist.o quicklist.o ae.o anet.o dict.o server.o sds.o zmalloc.o lzf_c.o lzf_d.o pqsort.o zipmap.o sha1.o ziplist.o release.o networking.o util.o object.o db.o replication.o rdb.o t_string.o t_list.o t_set.o t_zset.o t_hash.o config.o aof.o pubsub.o multi.o debug.o sort.o intset.o syncio.o cluster.o crc16.o endianconv.o slowlog.o scripting.o bio.o rio.o rand.o memtest.o crc64.o bitops.o sentinel.o notify.o setproctitle.o blocked.o hyperloglog.o latency.o sparkline.o redis-check-rdb.o geo.o lazyfree.o module.o evict.o expire.o geohash.o geohash_helper.o childinfo.o
 +REDIS_SERVER_OBJ+=fpconv.o lua_bit.o lua_cjson.o lua_cmsgpack.o lua_struct.o strbuf.o
- REDIS_GEOHASH_OBJ=../deps/geohash-int/geohash.o ../deps/geohash-int/geohash_helper.o
  REDIS_CLI_NAME=redis-cli
  REDIS_CLI_OBJ=anet.o adlist.o redis-cli.o zmalloc.o release.o anet.o ae.o crc64.o
-@@ -172,7 +176,7 @@
+ REDIS_BENCHMARK_NAME=redis-benchmark
+@@ -186,7 +190,7 @@
  
  # redis-server
  $(REDIS_SERVER_NAME): $(REDIS_SERVER_OBJ)
--	$(REDIS_LD) -o $@ $^ ../deps/hiredis/libhiredis.a ../deps/lua/src/liblua.a $(REDIS_GEOHASH_OBJ) $(FINAL_LIBS)
-+	$(REDIS_LD) -o $@ $^ ../deps/hiredis/libhiredis.a $(REDIS_GEOHASH_OBJ) $(FINAL_LIBS)
+-	$(REDIS_LD) -o $@ $^ ../deps/hiredis/libhiredis.a ../deps/lua/src/liblua.a $(FINAL_LIBS)
++	$(REDIS_LD) -o $@ $^ ../deps/hiredis/libhiredis.a $(FINAL_LIBS)
  
  # redis-sentinel
  $(REDIS_SENTINEL_NAME): $(REDIS_SERVER_NAME)
