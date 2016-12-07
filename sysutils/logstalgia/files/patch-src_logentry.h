845fb5c8d0b8f578dfb1a02b391f9417b17d45df
ehaupt <ehaupt@FreeBSD.org>
2016-12-07 14:42:22 +0000

    - Unbreak by updating to 1.0.7 - Move master site and WWW from SF to GitHub
    MFH:		2016Q4

---
+++ sysutils/logstalgia/files/patch-src_logentry.h
@@ -0,0 +1,10 @@
+--- src/logentry.h.orig	2015-10-19 22:33:12 UTC
++++ src/logentry.h
+@@ -21,6 +21,7 @@
+ #include "core/vectors.h"
+ 
+ #include <string>
++#include <sys/types.h>
+ 
+ class LogEntry {
+ 
