55794e16585eac4b444462572af28139e54953a0
jhale <jhale@FreeBSD.org>
2016-12-07 02:15:19 +0000

    Update to 2.7.1
    PR:		215094
    Submitted by:	Gasol Wu <gasol.wu@gmail.com> (maintainer)

--- sysutils/py-glances/files/patch-setup.py
+++ sysutils/py-glances/files/patch-setup.py
@@ -1,12 +1,10 @@
---- setup.py.orig	2015-06-15 02:42:15 UTC
+--- setup.py.orig	2016-09-11 09:08:21 UTC
 +++ setup.py
-@@ -15,8 +15,7 @@ def get_data_files():
+@@ -26,7 +26,6 @@ def get_data_files():
      data_files = [
          ('share/doc/glances', ['AUTHORS', 'COPYING', 'NEWS', 'README.rst',
-                                'conf/glances.conf', 'docs/glances-doc.html']),
--        ('share/doc/glances/images', glob.glob('docs/images/*.png')),
--        ('share/man/man1', ['man/glances.1'])
-+        ('share/doc/glances/images', glob.glob('docs/images/*.png'))
+                                'conf/glances.conf']),
+-        ('share/man/man1', ['docs/man/glances.1'])
      ]
  
      return data_files
