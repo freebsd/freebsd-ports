0ec7f602865c1a840a128b107ee6b64a81588afb
Jason E. Hale <jhale@FreeBSD.org>
2016-12-07 01:00:11 +0000

    - Update security/cryptopp to 5.6.5 - Add LICENSE (BSL) - Add CPE data - Remove STATIC option and always install the shared library;   no reason not to. - Remove GCC option and use USES=compiler framework - Add FULL_DEBUG option to address CVE-2016-7420 and add a warning if trying   to use it. It will allow the asserts to be enabled which could be a   security risk. The standard DEBUG option is now safe to use. - Add SIMD option for i386 and amd64 - requires a recent clang or gcc - Don't install the cryptest binary; it is really only used for regression   testing, so only build it for the TEST_TARGET. It is useless without the   data files anyways. - Take maintainership - Bump PORTREVISON on dependent ports due to shared library bump
    PR:		215015
    Reported by:	<fcsk.aim@gmail.com>
    MFH:		2016Q4
    Security:	eab68cff-bc0c-11e6-b2ca-001b3856973b

--- security/cryptopp/files/patch-config.h
+++ security/cryptopp/files/patch-config.h
@@ -1,29 +1,33 @@
---- config.h.orig	2013-02-20 14:30:54.000000000 +0000
+The definition of CRYPTOPP_BOOL_ALIGN16_ENABLED was incorrect for i386.
+For NetBSD, this resulted in the inability to build cryptopp on i386.
+On FreeBSD, it built, but deskutils/cdcat choked on it while building on
+FreeBSD 10/i386 and FreeBSD 11/i386. (PR: 192199)
+
+--- config.h.orig	2016-10-10 23:49:54 UTC
 +++ config.h
-@@ -290,12 +290,6 @@ NAMESPACE_END
- 	#define CRYPTOPP_BOOL_AESNI_INTRINSICS_AVAILABLE 0
+@@ -534,12 +534,6 @@ NAMESPACE_END
+ # endif
  #endif
  
--#if CRYPTOPP_BOOL_SSE2_INTRINSICS_AVAILABLE || CRYPTOPP_BOOL_SSE2_ASM_AVAILABLE || defined(CRYPTOPP_X64_MASM_AVAILABLE)
--	#define CRYPTOPP_BOOL_ALIGN16_ENABLED 1
+-#if CRYPTOPP_BOOL_SSE2_INTRINSICS_AVAILABLE || CRYPTOPP_BOOL_SSE2_ASM_AVAILABLE || CRYPTOPP_BOOL_NEON_INTRINSICS_AVAILABLE || defined(CRYPTOPP_X64_MASM_AVAILABLE)
+-	#define CRYPTOPP_BOOL_ALIGN16 1
 -#else
--	#define CRYPTOPP_BOOL_ALIGN16_ENABLED 0
+-	#define CRYPTOPP_BOOL_ALIGN16 0
 -#endif
 -
  // how to allocate 16-byte aligned memory (for SSE2)
  #if defined(CRYPTOPP_MSVC6PP_OR_LATER)
  	#define CRYPTOPP_MM_MALLOC_AVAILABLE
-@@ -307,6 +301,13 @@ NAMESPACE_END
+@@ -553,6 +547,12 @@ NAMESPACE_END
  	#define CRYPTOPP_NO_ALIGNED_ALLOC
  #endif
  
-+#if CRYPTOPP_BOOL_SSE2_INTRINSICS_AVAILABLE || CRYPTOPP_BOOL_SSE2_ASM_AVAILABLE || defined(CRYPTOPP_X64_MASM_AVAILABLE) || !defined(CPRYPTO_NO_ALIGNED_ALLOC)
-+	#define CRYPTOPP_BOOL_ALIGN16_ENABLED 1
++#if CRYPTOPP_BOOL_SSE2_INTRINSICS_AVAILABLE || CRYPTOPP_BOOL_SSE2_ASM_AVAILABLE || CRYPTOPP_BOOL_NEON_INTRINSICS_AVAILABLE || defined(CRYPTOPP_X64_MASM_AVAILABLE) || !defined(CPRYPTO_NO_ALIGNED_ALLOC)
++	#define CRYPTOPP_BOOL_ALIGN16 1
 +#else
-+	#define CRYPTOPP_BOOL_ALIGN16_ENABLED 0
++	#define CRYPTOPP_BOOL_ALIGN16 0
 +#endif
 +
-+
- // how to disable inlining
- #if defined(_MSC_VER) && _MSC_VER >= 1300
- #	define CRYPTOPP_NOINLINE_DOTDOTDOT
+ // Apple always provides 16-byte aligned, and tells us to use calloc
+ // http://developer.apple.com/library/mac/documentation/Performance/Conceptual/ManagingMemory/Articles/MemoryAlloc.html
+ 
