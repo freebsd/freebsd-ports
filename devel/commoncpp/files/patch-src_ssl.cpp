17257140195f2cd3de75d11256be3fa7bad7395d
Bernard Spil <brnrd@FreeBSD.org>
2016-12-10 20:39:32 +0000

    devel/commoncpp: Fix build without SSLv3
      - Add patch to fix build without SSLv3
      - Register SSL dependency USES= ssl
    
    Approved by:	SSL blanket

---
+++ devel/commoncpp/files/patch-src_ssl.cpp
@@ -0,0 +1,11 @@
+--- src/ssl.cpp.orig	2010-11-01 00:42:51 UTC
++++ src/ssl.cpp
+@@ -386,7 +386,7 @@ bool SSLStream::getSession(void)
+     if(so == INVALID_SOCKET)
+         return false;
+ 
+-    ctx = SSL_CTX_new(SSLv3_client_method());
++    ctx = SSL_CTX_new(SSL23_client_method());
+     if(!ctx) {
+         SSL_CTX_free(ctx);
+         return false;
