f6fb1841164020a9b3856e5f31229c4ab324efef
stephen <stephen@FreeBSD.org>
2016-12-12 02:46:23 +0000

    - Add LICENCE, and small modification to description of error in   libqhullcpp_QhullSet.h.
    PR:		ports/215078
    Submitted by:	z7dr6ut7gs@snkmail.com

--- math/qhull/files/patch-src_libqhullcpp_QhullSet.h
+++ math/qhull/files/patch-src_libqhullcpp_QhullSet.h
@@ -1,7 +1,7 @@
-c++ 4.2 (in base 9.x) gives a bogus error:
+c++ 4.2 (in base 9.x) has a weakness that emits a bogus error:
 /usr/ports/math/qhull/work/qhull-2015.2/src/libqhullcpp/QhullSet.h:330: error: expected `;' before 'i'
 
- Work around that by removing overspecified part of type declaration.
+Work around that by removing overspecified part of type declaration (can remove after 9.x EOL).
 
 --- src/libqhullcpp/QhullSet.h.orig	2016-01-18 19:45:12 UTC
 +++ src/libqhullcpp/QhullSet.h
