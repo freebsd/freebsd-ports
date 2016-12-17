0fe4b337b460141146e6589f99fa6e79703da4e5
riggs <riggs@FreeBSD.org>
2016-12-17 18:02:09 +0000

    New port: textproc/zxing-cpp, a C++ Library for QR code recognition
    PR:		214519
    Submitted by:	yuri@rawbw.com (maintainer)

---
+++ textproc/zxing-cpp/files/patch-core_src_zxing_oned_OneDReader.cpp
@@ -0,0 +1,10 @@
+--- core/src/zxing/oned/OneDReader.cpp.orig	2016-11-10 14:45:28 UTC
++++ core/src/zxing/oned/OneDReader.cpp
+@@ -21,6 +21,7 @@
+ #include <zxing/oned/OneDResultPoint.h>
+ #include <zxing/NotFoundException.h>
+ #include <math.h>
++#include <float.h>
+ #include <limits.h>
+ #include <algorithm>
+ 
