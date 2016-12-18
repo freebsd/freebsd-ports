c93f6191bd8dd09831d6cb0dd41406269e512af8
amdmi3 <amdmi3@FreeBSD.org>
2016-12-18 09:05:44 +0000

    - Switch to USES=localbase - Switch to options helpers - Regenerate patches

--- net/trickle/files/patch-trickle-overload.c
+++ net/trickle/files/patch-trickle-overload.c
@@ -1,5 +1,5 @@
---- trickle-overload.c.orig	2004-12-13 02:21:53.000000000 +0100
-+++ trickle-overload.c	2008-04-21 19:23:49.000000000 +0200
+--- trickle-overload.c.orig	2004-12-13 01:21:53 UTC
++++ trickle-overload.c
 @@ -61,6 +61,7 @@
  
  #define SD_INSELECT 0x01
@@ -8,7 +8,7 @@
  struct sockdesc {
  	int                    sock;
  	int                    flags;
-@@ -141,8 +142,12 @@
+@@ -141,8 +142,12 @@ DECLARE(dup, int, (int));
  DECLARE(dup2, int, (int, int));
  
  #ifdef HAVE_SENDFILE
@@ -21,7 +21,7 @@
  
  static int             delay(int, ssize_t *, short);
  static struct timeval *getdelay(struct sockdesc *, ssize_t *, short);
-@@ -202,15 +207,11 @@
+@@ -202,15 +207,11 @@ trickle_init(void)
  
  	GETADDR(read);
  	GETADDR(readv);
@@ -37,7 +37,7 @@
  	GETADDR(sendto);
  
  	GETADDR(select);
-@@ -701,7 +702,6 @@
+@@ -701,7 +702,6 @@ readv(int fd, const struct iovec *iov, i
  	return (ret);
  }
  
@@ -45,7 +45,7 @@
  ssize_t
  recv(int sock, void *buf, size_t len, int flags)
  {
-@@ -730,7 +730,6 @@
+@@ -730,7 +730,6 @@ recv(int sock, void *buf, size_t len, in
  
  	return (ret);
  }
@@ -53,7 +53,7 @@
  
  #ifdef __sun__
  ssize_t
-@@ -832,7 +831,6 @@
+@@ -832,7 +831,6 @@ writev(int fd, const struct iovec *iov, 
  	return (ret);
  }
  
@@ -61,7 +61,7 @@
  ssize_t
  send(int sock, const void *buf, size_t len, int flags)
  {
-@@ -862,7 +860,6 @@
+@@ -862,7 +860,6 @@ send(int sock, const void *buf, size_t l
  
  	return (ret);
  }
@@ -69,7 +69,7 @@
  
  ssize_t
  sendto(int sock, const void *buf, size_t len, int flags, const struct sockaddr *to,
-@@ -1004,11 +1001,20 @@
+@@ -1004,11 +1001,20 @@ accept(int sock, struct sockaddr *addr, 
  }
  
  #ifdef HAVE_SENDFILE
@@ -90,7 +90,7 @@
  
  	INIT;
  
-@@ -1021,8 +1027,11 @@
+@@ -1021,8 +1027,11 @@ sendfile(int out_fd, int in_fd, off_t *o
  	/* This is a slightly ugly hack. */
  	bytes = MIN(inbytes, outbytes);
  	if (bytes > 0)
