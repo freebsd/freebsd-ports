3dea606a45e8c38d0d271b8124d33b346df8a136
Jan Beich <jbeich@FreeBSD.org>
2016-12-19 21:54:26 +0000

    games/openbor: update to 3.0.r4415
    - Pacify -fsanitize=address a bit since previous update
    - Drop pointless USES=dos2unix
    
    Changes:	svn log -r4415:r4391 https://svn.code.sf.net/p/openbor/engine/engine/

---
+++ games/openbor/files/patch-openbor.c
@@ -0,0 +1,29 @@
+--- openbor.c.orig	2016-12-19 15:29:57 UTC
++++ openbor.c
+@@ -5810,7 +5810,7 @@ s_collision_attack **collision_alloc_att
+     size_t             alloc_size;
+ 
+     // Get amount of memory we'll need.
+-    alloc_size = sizeof(*result);
++    alloc_size = max_collisons * sizeof(*result);
+ 
+     // Allocate memory and get pointer.
+     result = malloc(alloc_size);
+@@ -5859,7 +5859,7 @@ s_collision_body **collision_alloc_body_
+     size_t             alloc_size;
+ 
+     // Get amount of memory we'll need.
+-    alloc_size = sizeof(*result);
++    alloc_size = max_collisons * sizeof(*result);
+ 
+     // Allocate memory and get pointer.
+     result = malloc(alloc_size);
+@@ -34617,7 +34617,7 @@ void keyboard_setup(int player)
+     strncpy(buttonnames[SDID_SPECIAL], "Special", 16);
+     strncpy(buttonnames[SDID_START], "Start", 16);
+     strncpy(buttonnames[SDID_SCREENSHOT], "Screenshot", 16);
+-    strncpy(buttonnames[SDID_ESC], "Exit", 16);
++//    strncpy(buttonnames[SDID_ESC], "Exit", 16);
+ 
+     savesettings();
+     bothnewkeys = 0;
