24c4f9e8a8c0c4383cbbea984fc2d14384c48713
junovitch <junovitch@FreeBSD.org>
2016-12-23 04:41:07 +0000

    MFH: r425986 r429220
    www/squid-devel: update 4.0.14 -> 4.0.17
    
    - Spell CHOSEN_COMPILER_TYPE correctly
    - Remove upstreamed patch (IPv6 + PF crash)
    - Add --enable-zph-qos option to default set
      http://wiki.squid-cache.org/Features/QualityOfService
    - Mark broken if ssl set to openssl-devel, it doesn't build
    - Switch IPV6 option to options helpers
    
    PR:		215418
    Submitted by:	Pavel Timofeev <timp87@gmail.com> (maintainer)
    Security:	CVE-2016-10002
    Security:	CVE-2016-10003
    Security:	https://vuxml.FreeBSD.org/freebsd/41f8af15-c8b9-11e6-ae1b-002590263bf5.html
    Approved by:	ports-secteam (with hat)

--- www/squid-devel/files/patch-src__ip__Intercept.cc
+++ www/squid-devel/files/patch-src__ip__Intercept.cc
@@ -1,6 +1,6 @@
---- src/ip/Intercept.cc.orig	2015-11-01 10:44:25 UTC
+--- src/ip/Intercept.cc.orig	2016-12-16 10:06:20 UTC
 +++ src/ip/Intercept.cc
-@@ -202,10 +202,10 @@ Ip::Intercept::IpfInterception(const Com
+@@ -206,10 +206,10 @@ Ip::Intercept::IpfInterception(const Com
      // for NAT lookup set local and remote IP:port's
      if (newConn->remote.isIPv6()) {
  #if IPFILTER_VERSION < 5000003
@@ -11,43 +11,5 @@
 -        warningLevel = (warningLevel + 1) % 10;
 +        warningLevel = (warningLevel + 1) % 1048576;
          return false;
- #else
-         natLookup.nl_v = 6;
-@@ -323,13 +323,21 @@
      }
- 
-     memset(&nl, 0, sizeof(struct pfioc_natlook));
--    newConn->remote.getInAddr(nl.saddr.v4);
-+    if (newConn->remote.isIPv4()) {
-+        newConn->remote.getInAddr(nl.saddr.v4);
-+    } else {
-+        newConn->remote.getInAddr(nl.saddr.v6);
-+    }
-     nl.sport = htons(newConn->remote.port());
- 
--    newConn->local.getInAddr(nl.daddr.v4);
-+    if (newConn->local.isIPv4()) {
-+        newConn->local.getInAddr(nl.daddr.v4);
-+    } else {
-+        newConn->local.getInAddr(nl.daddr.v6);
-+    }
-     nl.dport = htons(newConn->local.port());
- 
--    nl.af = AF_INET;
-+    nl.af = newConn->remote.isIPv4() ? AF_INET : AF_INET6;
-     nl.proto = IPPROTO_TCP;
-     nl.direction = PF_OUT;
- 
-@@ -345,7 +353,11 @@
-         debugs(89, 9, HERE << "address: " << newConn);
-         return false;
-     } else {
--        newConn->local = nl.rdaddr.v4;
-+        if (nl.af == AF_INET) {
-+            newConn->local = nl.rdaddr.v4;
-+        } else {
-+            newConn->local = nl.rdaddr.v6;
-+        }
-         newConn->local.port(ntohs(nl.rdport));
-         debugs(89, 5, HERE << "address NAT: " << newConn);
-         return true;
+     newConn->local.getInAddr(natLookup.nl_inip);
