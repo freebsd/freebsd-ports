be4e994a2424d74a9c83c23c3ae79107f40dca89
Jan Beich <jbeich@FreeBSD.org>
2016-12-23 04:42:12 +0000

    games/openbor: update to 3.0.r4420
    Changes:	http://www.chronocrash.com/forum/index.php?topic=3178.0
    Changes:	svn log -r4420:4419 https://svn.code.sf.net/p/openbor/engine/engine/

--- games/openbor/files/patch-openbor.c
+++ games/openbor/files/patch-openbor.c
@@ -1,4 +1,6 @@
---- openbor.c.orig	2016-12-19 15:29:57 UTC
+Fix potential crashes found by ASan/Clang/GCC
+
+--- openbor.c.orig	2016-12-22 13:02:02 UTC
 +++ openbor.c
 @@ -5810,7 +5810,7 @@ s_collision_attack **collision_alloc_att
      size_t             alloc_size;
@@ -18,21 +20,42 @@
  
      // Allocate memory and get pointer.
      result = malloc(alloc_size);
-@@ -20480,7 +20480,7 @@ void common_dot()
+@@ -13933,7 +13933,7 @@ void generate_basemap(int map_index, flo
+ 
+ void load_level(char *filename)
+ {
+-    char *buf;
++    char *buf = NULL;
+     size_t size, len, sblen;
+     ptrdiff_t pos, oldpos;
+     char *command;
+@@ -15210,6 +15210,11 @@ void bar(int x, int y, int value, int ma
+         return;
+     }
+ 
++    if (value < 0)
++    {
++        value = 0;
++    }
++
+     if (value > maxvalue)
+     {
+         value = maxvalue;
+@@ -20471,7 +20476,7 @@ void common_dot()
      entity     *eOpp;       //Owner of dot effect.
      s_collision_attack    attack;     //Attack struct.
  
 -    for(iIndex = 0; iIndex <= MAX_DOTS; iIndex++)                                               //Loop through all DOT indexes.
-+    for(iIndex = 0; iIndex < MAX_DOTS; iIndex++)                                               //Loop through all DOT indexes.
++    for(iIndex = 0; iIndex < MAX_DOTS; iIndex++)                                                //Loop through all DOT indexes.
      {
          iDot_time   =   self->dot_time[iIndex];                                                 //Get expire time.
          iDot_cnt    =   self->dot_cnt[iIndex];                                                  //Get next tick time.
-@@ -34617,7 +34617,7 @@ void keyboard_setup(int player)
+@@ -34607,7 +34612,7 @@ void keyboard_setup(int player)
      strncpy(buttonnames[SDID_SPECIAL], "Special", 16);
      strncpy(buttonnames[SDID_START], "Start", 16);
      strncpy(buttonnames[SDID_SCREENSHOT], "Screenshot", 16);
 -    strncpy(buttonnames[SDID_ESC], "Exit", 16);
-+//    strncpy(buttonnames[SDID_ESC], "Exit", 16);
++    //strncpy(buttonnames[SDID_ESC], "Exit", 16);
  
      savesettings();
      bothnewkeys = 0;
