525a092e0e8ac68d3b17960bd6af4839f142de61
Jason Unovitch <junovitch@FreeBSD.org>
2016-12-23 01:35:39 +0000

    MFH: r423366 r424094 r425833 r428479
    editors/neovim: update 0.1.4 -> 0.1.7
    
    PR:		214897
    Submitted by:	Oleg Gushchenkov <gor@clogic.com.ua> (maintainer)
    Reported by:	ohauer (as a security issue)
    Security:	CVE-2016-1248
    Security:	https://vuxml.FreeBSD.org/freebsd/c11629d3-c8ad-11e6-ae1b-002590263bf5.html
    Approved by:	ports-secteam (with hat)

--- editors/neovim/files/patch-src_nvim_os_pty__process__unix.c
+++ editors/neovim/files/patch-src_nvim_os_pty__process__unix.c
@@ -1,11 +1,11 @@
---- src/nvim/event/pty_process.c.orig	2016-04-25 01:46:34 UTC
-+++ src/nvim/event/pty_process.c
-@@ -10,7 +10,7 @@
+--- src/nvim/os/pty_process_unix.c.orig	2016-11-08 05:50:09 UTC
++++ src/nvim/os/pty_process_unix.c
+@@ -9,7 +9,7 @@
  #include <sys/ioctl.h>
  
  // forkpty is not in POSIX, so headers are platform-specific
 -#if defined(__FreeBSD__)
-+#if defined(__FreeBSD__) || defined __DragonFly__
++#if defined(__FreeBSD__) || defined (__DragonFly__)
  # include <libutil.h>
  #elif defined(__OpenBSD__) || defined(__NetBSD__) || defined(__APPLE__)
  # include <util.h>
