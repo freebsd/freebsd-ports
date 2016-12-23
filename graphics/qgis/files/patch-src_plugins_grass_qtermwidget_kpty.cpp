90ff83398e65d0c70531580f858ff3f50b1fedcb
Wen Heping <wen@FreeBSD.org>
2016-12-23 03:15:52 +0000

    - Update to 2.18.2(based on) - New dependency devel/qjson - New makefile target 'check', which runs the test suite - Use of external python functionality, if possible (via CMAKE_ARGS) [2] - Switch from PostGIS 2.1 to 2.2
    PR:		214238
    Submitted by:	rhurlin@gwdg.de(maintainer)

--- graphics/qgis/files/patch-src_plugins_grass_qtermwidget_kpty.cpp
+++ graphics/qgis/files/patch-src_plugins_grass_qtermwidget_kpty.cpp
@@ -1,10 +1,11 @@
---- src/plugins/grass/qtermwidget/kpty.cpp.orig	2015-10-23 12:10:38 UTC
+--- src/plugins/grass/qtermwidget/kpty.cpp.orig	2016-10-21 12:14:02 UTC
 +++ src/plugins/grass/qtermwidget/kpty.cpp
-@@ -30,6 +30,7 @@
+@@ -28,6 +28,8 @@
+ 
+ 
  #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
++#define HAVE_UTEMPTER
++#define HAVE_UTMPX
  #define HAVE_LOGIN
  #define HAVE_LIBUTIL_H
-+#define HAVE_UTEMPTER
  #endif
- 
- #ifdef __sgi
