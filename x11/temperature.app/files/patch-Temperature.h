a59d9d36e5452de0fa1f6f717de0eb2de755be13
Dmitry Marakasov <amdmi3@FreeBSD.org>
2016-12-26 12:33:16 +0000

    - Fix wget argument, --cache=off -> --no-cache - Fix execlp call, terminate argument list with NULL instead of 0 (otherwise execve returns EFAULT) - Fix data url, as the previous one was discontinued
    PR:		212126
    Submitted by:	manny@computer.org

--- x11/temperature.app/files/patch-Temperature.h
+++ x11/temperature.app/files/patch-Temperature.h
@@ -1,6 +1,6 @@
---- Temperature.h.bak	2002-04-06 08:44:17.000000000 +0900
-+++ Temperature.h	2013-12-07 17:24:28.000000000 +0900
-@@ -22,9 +22,11 @@
+--- Temperature.h.bak	2016-08-24 21:28:32.665119000 -0400
++++ Temperature.h	2016-08-24 21:26:20.577933000 -0400
+@@ -22,14 +22,16 @@
  #ifndef _TEMPERATURE_H_
  #define _TEMPERATURE_H_
  
@@ -13,3 +13,9 @@
  #define APPNAME                 "Temperature.app"
  #define VERSION                 "1.4"
  #define INSTANCENAME            "temperature_app"
+ #define CLASSNAME               "Temperature_app"
+-#define METAR_URL               "http://weather.noaa.gov/pub/data/observations/metar/decoded/%s.TXT"
++#define METAR_URL               "ftp://tgftp.nws.noaa.gov/data/observations/metar/decoded/%s.TXT"
+ #define UPDATE_INTERVAL         900
+ #define TIME_POS                22
+ #define TEMP_POS                35
