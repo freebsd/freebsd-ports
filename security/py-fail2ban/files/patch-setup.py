f70c99bf009eaa26870c9b0859e6c50a66d0bf48
lifanov <lifanov@FreeBSD.org>
2016-12-27 21:25:07 +0000

    Update fail2ban to version 0.9.6
    PR:		215239
    Submitted by:	John W. O'Brien <john@saltant.com>
    Reviewed by:	matthew
    Approved by:	theis@gmx.at (maintainer), matthew (mentor)
    Differential Revision:	https://reviews.freebsd.org/D8917

--- security/py-fail2ban/files/patch-setup.py
+++ security/py-fail2ban/files/patch-setup.py
@@ -1,10 +1,11 @@
---- setup.py.orig	2015-08-01 01:32:13 UTC
+--- setup.py.orig	2016-12-09 14:36:08 UTC
 +++ setup.py
-@@ -104,26 +104,13 @@ setup(
+@@ -149,27 +149,14 @@ setup(
  		'bin/fail2ban-client',
  		'bin/fail2ban-server',
  		'bin/fail2ban-regex',
 -		'bin/fail2ban-testcases',
+ 		# 'bin/fail2ban-python', -- link (binary), will be installed via install_scripts_f2b wrapper
  	],
  	packages = [
  		'fail2ban',
@@ -27,14 +28,3 @@
  	},
  	data_files = [
  		('/etc/fail2ban',
-@@ -147,10 +134,6 @@ setup(
- 		),
- 		('/var/lib/fail2ban',
- 			''
--		),
--		('/usr/share/doc/fail2ban',
--			['README.md', 'README.Solaris', 'DEVELOP', 'FILTERS',
--			 'doc/run-rootless.txt']
- 		)
- 	] + data_files_extra,
- 	**setup_extra
