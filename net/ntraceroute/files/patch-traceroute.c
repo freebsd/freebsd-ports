fedfe012271f62c0ac13bdab5703e9f923f693ca
Mathieu Arnold <mat@FreeBSD.org>
2016-12-28 11:38:12 +0000

    Really fix on 11+.
    PR:		212283
    Submitted by:	ae
    Sponsored by:	Absolight

--- net/ntraceroute/files/patch-traceroute.c
+++ net/ntraceroute/files/patch-traceroute.c
@@ -1,17 +1,5 @@
 --- traceroute.c.orig	2016-09-02 21:07:07 UTC
 +++ traceroute.c
-@@ -2,6 +2,11 @@
- /*
-  *  STILL BROKEN: traceroute -g on linux (LSRR)
-  */
-+#include <string.h>
-+#if __FreeBSD_version >= 1100030
-+#define	BYTESWAP_IP_FLAGS
-+#define	BYTESWAP_IP_LEN
-+#endif
- #define TRACE_TOS 1
- #define SPRAY
- #define FIXT
 @@ -798,7 +803,7 @@ struct opacket {
                struct icmp icmp_probe;
          } ip_payload;
