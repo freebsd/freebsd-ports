751aa321cdaaaed627490dc9e5136be3d616b1b9
Timur I. Bakeyev <timur@FreeBSD.org>
2017-01-02 22:53:43 +0000

    Update Samba4* supplimentary libraries to the newer versions. Made databases/ldb to depend on openldap-sasl-client, so it won't conflict with the corresponding requirement of net/samba4* ports.

---
+++ databases/tdb/files/patch-third_party__waf__wafadmin__Tools__cc.py
@@ -0,0 +1,11 @@
+--- third_party/waf/wafadmin/Tools/cc.py.orig	2015-11-06 13:25:52 UTC
++++ third_party/waf/wafadmin/Tools/cc.py
+@@ -88,7 +88,7 @@ def c_hook(self, node):
+ 		raise Utils.WafError('Have you forgotten to set the feature "cc" on %s?' % str(self))
+ 	return task
+ 
+-cc_str = '${CC} ${CCFLAGS} ${CPPFLAGS} ${_CCINCFLAGS} ${_CCDEFFLAGS} ${CC_SRC_F}${SRC} ${CC_TGT_F}${TGT}'
++cc_str = '${CC} ${CCFLAGS} ${_CCINCFLAGS} ${CPPFLAGS} ${_CCDEFFLAGS} ${CC_SRC_F}${SRC} ${CC_TGT_F}${TGT}'
+ cls = Task.simple_task_type('cc', cc_str, 'GREEN', ext_out='.o', ext_in='.c', shell=False)
+ cls.scan = ccroot.scan
+ cls.vars.append('CCDEPS')
