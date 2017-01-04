bda391284d72d8a0af7be358a31a3ddddd9b2c5a
Mark Linimon <linimon@FreeBSD.org>
2017-01-04 20:51:46 +0000

    Attempt to fix build on tier-2 archs.
    Tested on:	armv6
    Submitted by:	madpilot (maintainer)

---
+++ sysutils/conky/files/patch-src_bsdapm.cc
@@ -0,0 +1,18 @@
+--- src/bsdapm.cc.orig	2016-10-04 07:49:19 UTC
++++ src/bsdapm.cc
+@@ -27,6 +27,10 @@
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  */
++
++#if (defined(__FreeBSD__) || defined(__FreeBSD_kernel__) \   
++	|| defined(__DragonFly__) \
++	|| defined(__OpenBSD__)) && (defined(i386) || defined(__i386__))
+ #include "config.h"
+ #include "conky.h"
+ #include "text_object.h"
+@@ -208,3 +212,4 @@ void print_apm_battery_time(struct text_
+ 	}
+ #endif
+ }
++#endif
