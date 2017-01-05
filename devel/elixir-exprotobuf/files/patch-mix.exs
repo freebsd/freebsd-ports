a51a20b11a06a76930a5d46926afb5aba50fd7f3
olgeni <olgeni@FreeBSD.org>
2017-01-05 06:24:27 +0000

    Update devel/elixir-exprotobuf to version 1.2.3.

--- devel/elixir-exprotobuf/files/patch-mix.exs
+++ devel/elixir-exprotobuf/files/patch-mix.exs
@@ -1,12 +1,12 @@
---- mix.exs.orig	2015-07-06 14:47:27 UTC
+--- mix.exs.orig	2017-01-05 03:20:53 UTC
 +++ mix.exs
 @@ -6,8 +6,7 @@ defmodule Protobuf.Mixfile do
-      version: "0.10.0",
-      elixir: "~> 1.0.0",
-      description: description,
--     package: package,
--     deps: deps]
-+     package: package]
+      version: "1.2.3",
+      elixir: "~> 1.2",
+      description: description(),
+-     package: package(),
+-     deps: deps()]
++     package: package()]
    end
  
    def application do
