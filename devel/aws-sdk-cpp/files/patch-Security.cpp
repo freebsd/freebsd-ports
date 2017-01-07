9b27d72ffa4db0c02f0dec99d9b7e1a219b09fae
Tilman Keskinoz <arved@FreeBSD.org>
2017-01-07 15:23:17 +0000

    Update to 1.0.51

--- devel/aws-sdk-cpp/files/patch-Security.cpp
+++ devel/aws-sdk-cpp/files/patch-Security.cpp
@@ -1,11 +1,11 @@
---- aws-cpp-sdk-core/source/platform/linux-shared/Security.cpp.orig	2017-01-03 09:10:58.573735000 +0000
-+++ aws-cpp-sdk-core/source/platform/linux-shared/Security.cpp	2017-01-03 09:11:08.709468000 +0000
-@@ -24,7 +24,7 @@
- 
+--- aws-cpp-sdk-core/source/platform/linux-shared/Security.cpp.orig	2017-01-05 20:03:15.000000000 +0000
++++ aws-cpp-sdk-core/source/platform/linux-shared/Security.cpp	2017-01-07 15:07:42.656838000 +0000
+@@ -25,7 +25,7 @@
  void SecureMemClear(unsigned char *data, size_t length)
  {
--#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__bsdi__) || defined(__DragonFly__)
-+#if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__bsdi__) || defined(__DragonFly__)
-     memset_s(data, length, 0, length);
+ #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__bsdi__) || defined(__DragonFly__)
+-    memset_s(data, length, 0, length);
++    memset(data, 0, length);
  #else
      memset(data, 0, length);
+     asm volatile("" : "+m" (data));
