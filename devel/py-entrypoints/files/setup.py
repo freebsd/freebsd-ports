435dfb4774bff94619ea0360e6b133533a16411e
antoine <antoine@FreeBSD.org>
2017-01-09 18:49:17 +0000

    New port: devel/py-entrypoints Discover and load entry points from installed packages. WWW: https://github.com/takluyver/entrypoints

---
+++ devel/py-entrypoints/files/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='entrypoints',
+    version='0.2.2',
+    description='Discover and load entry points from installed packages',
+    url='https://github.com/takluyver/entrypoints',
+    author='Thomas Kluyver',
+    license='MIT',
+    long_description='Discover and load entry points from installed packages',
+    py_modules=['entrypoints']
+)
