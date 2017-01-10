74e860eb44c29d792a8f7e0295aa406035a5ce0a
thierry <thierry@FreeBSD.org>
2017-01-10 20:53:15 +0000

    Chase toxcore and upgrade to 1.7.1.
    PR:		215556
    Submitted by:	maintainer (Yuri Victorovich)

--- net-im/qTox/files/patch-src_main.cpp
+++ net-im/qTox/files/patch-src_main.cpp
@@ -1,4 +1,4 @@
---- src/main.cpp.orig	2016-11-13 15:20:00 UTC
+--- src/main.cpp.orig	2016-12-25 17:46:01 UTC
 +++ src/main.cpp
 @@ -317,3 +317,10 @@ int main(int argc, char *argv[])
  #endif
--- net-im/qTox/files/patch-src_video_videomode.cpp
+++ net-im/qTox/files/patch-src_video_videomode.cpp
@@ -1,4 +1,4 @@
---- src/video/videomode.cpp.orig	2016-11-13 15:20:00 UTC
+--- src/video/videomode.cpp.orig	2016-12-25 17:46:01 UTC
 +++ src/video/videomode.cpp
 @@ -18,6 +18,7 @@
  */
