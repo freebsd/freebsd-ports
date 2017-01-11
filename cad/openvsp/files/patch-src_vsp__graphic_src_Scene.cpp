3c19f92edcba894e366ad2eb353bd18ec91b8b1e
robak <robak@FreeBSD.org>
2017-01-11 14:08:55 +0000

    cad/openvsp: update 3.9.1 -> 3.10.0
    PR:		215951
    Submitted by:	Fernando Apesteguia <fernando.apesteguia@gmail.com> (maintainer)
    MFH:		2017Q1

---
+++ cad/openvsp/files/patch-src_vsp__graphic_src_Scene.cpp
@@ -0,0 +1,11 @@
+$FreeBSD$
+--- src/vsp_graphic/src/Scene.cpp.orig	2017-01-09 21:50:15 UTC
++++ src/vsp_graphic/src/Scene.cpp
+@@ -15,6 +15,7 @@
+ #include "ByteOperationUtil.h"
+ 
+ #include <assert.h>
++#include <stdlib.h>
+ 
+ namespace VSPGraphic
+ {
