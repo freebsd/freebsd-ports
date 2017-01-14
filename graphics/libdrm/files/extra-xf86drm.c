d41f933bc5ee1ed7c6c32b312f5e0abe22bb0ebd
Baptiste Daroussin <bapt@FreeBSD.org>
2017-01-14 10:37:09 +0000

    Update to 2.4.74

--- graphics/libdrm/files/extra-xf86drm.c
+++ graphics/libdrm/files/extra-xf86drm.c
@@ -6,12 +6,12 @@ dragonfly fixed this issue in,
 http://gitweb.dragonflybsd.org/dragonfly.git/commit/b922632f623ee2cc2c1346bb3a6894a7756676aa
 which will be included in the 4.4 release when it is released.
 
---- xf86drm.c.orig	2015-11-02 12:26:19.910518000 +0100
-+++ xf86drm.c	2015-11-02 12:27:10.575894000 +0100
-@@ -174,7 +174,7 @@
+--- xf86drm.c.orig	2016-11-29 11:15:10 UTC
++++ xf86drm.c
+@@ -173,7 +173,7 @@ drmIoctl(int fd, unsigned long request, 
  
      do {
- 	ret = ioctl(fd, request, arg);
+         ret = ioctl(fd, request, arg);
 -    } while (ret == -1 && (errno == EINTR || errno == EAGAIN));
 +    } while (ret == -1 && (errno == EINTR || errno == EAGAIN || errno == 512));
      return ret;
