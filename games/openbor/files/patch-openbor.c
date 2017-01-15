0e377de2ce78c5d9926e5d5f049bac9a223ad1e7
jbeich <jbeich@FreeBSD.org>
2017-01-15 11:31:24 +0000

    games/openbor: warnings
    - With `make -s` only compiler warnings are allowed to break silence
    - -Wno-unsued-result is no longer fatal after 9.x EOL
    
    source/gamelib/packfile.c: In function 'packfile_music_read':
    source/gamelib/packfile.c:1402:13: warning: 'memset' used with length equal to number of elements without multiplication by element size [-Wmemset-elt-size]
                 memset(filelist[i].bgmTracks, 0, 256);
                 ^~~~~~
    source/scriptlib/Parser.c: In function 'Parser_Unary_expr':
    source/scriptlib/Parser.c:1879:30: warning: may write a terminating nul past the end of the destination [-Wformat-length=]
                 sprintf(buf, "-%s", pInstruction->theToken->theSource);
                               ~~~^
    source/scriptlib/Parser.c:1879:13: note: format output between 2 and 129 bytes into a destination ofsize 128
                 sprintf(buf, "-%s", pInstruction->theToken->theSource);
                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    source/scriptlib/Parser.c:1906:30: warning: may write a terminating nul past the end of the destination [-Wformat-length=]
                 sprintf(buf, "!%s", pInstruction->theToken->theSource);
                               ~~~^
    source/scriptlib/Parser.c:1906:13: note: format output between 2 and 129 bytes into a destination ofsize 128
                 sprintf(buf, "!%s", pInstruction->theToken->theSource);
                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    openbor.c:8224:26: warning: comparison of unsigned expression >= 0 is always true
          [-Wtautological-compare]
            for(i = len-1; i >= 0; i--)
                           ~ ^  ~
    openbor.c:21719:68: warning: using integer absolute value function 'abs' when argument is of floating
          point type [-Wabsolute-value]
            if(ent->direction == DIRECTION_RIGHT) ent->velocity.x = -1*abs(ent->velocity.x);
                                                                       ^
    openbor.c:21719:68: note: use function 'fabsf' instead
            if(ent->direction == DIRECTION_RIGHT) ent->velocity.x = -1*abs(ent->velocity.x);
                                                                       ^~~
                                                                       fabsf
    openbor.c:21720:32: warning: using integer absolute value function 'abs' when argument is of floating
          point type [-Wabsolute-value]
            else ent->velocity.x = abs(ent->velocity.x);
                                   ^
    openbor.c:21720:32: note: use function 'fabsf' instead
            else ent->velocity.x = abs(ent->velocity.x);
                                   ^~~
                                   fabsf
    openbor.c:25840:9: warning: taking the absolute value of unsigned type 'unsigned int' has no effect
          [-Wabsolute-value]
        if (abs(rand32()) % 2)
            ^
    openbor.c:25840:9: note: remove the call to 'abs' since unsigned values cannot be negative
        if (abs(rand32()) % 2)
            ^~~

--- games/openbor/files/patch-openbor.c
+++ games/openbor/files/patch-openbor.c
@@ -1,4 +1,6 @@
 Fix potential crashes found by ASan/Clang/GCC
+Fix an infinite loop in lcmScriptDeleteMain()
+Avoid accidental rounding from abs()
 
 --- openbor.c.orig	2016-12-22 13:02:02 UTC
 +++ openbor.c
@@ -20,6 +22,16 @@ Fix potential crashes found by ASan/Clang/GCC
  
      // Allocate memory and get pointer.
      result = malloc(alloc_size);
+@@ -8204,7 +8204,8 @@ size_t lcmScriptCopyBuffer(ArgList *argl
+ 
+ size_t lcmScriptDeleteMain(char **buf)
+ {
+-    size_t len = 0, i = 0;
++    size_t len = 0;
++    int i = 0;
+     ptrdiff_t pos = 0;
+     char *newbuf = NULL;
+ 
 @@ -13933,7 +13933,7 @@ void generate_basemap(int map_index, flo
  
  void load_level(char *filename)
@@ -50,6 +62,26 @@ Fix potential crashes found by ASan/Clang/GCC
      {
          iDot_time   =   self->dot_time[iIndex];                                                 //Get expire time.
          iDot_cnt    =   self->dot_cnt[iIndex];                                                  //Get next tick time.
+@@ -21710,8 +21716,8 @@ int reset_backpain(entity *ent)
+         if (ent->normaldamageflipdir == DIRECTION_RIGHT) ent->direction = DIRECTION_RIGHT;
+         else ent->direction = DIRECTION_LEFT;
+ 
+-        if(ent->direction == DIRECTION_RIGHT) ent->velocity.x = -1*abs(ent->velocity.x);
+-        else ent->velocity.x = abs(ent->velocity.x);
++        if(ent->direction == DIRECTION_RIGHT) ent->velocity.x = -1*ABS(ent->velocity.x);
++        else ent->velocity.x = ABS(ent->velocity.x);
+ 
+         return 1;
+     }
+@@ -25831,7 +25837,7 @@ int common_try_wander(entity *target, in
+         mod = -mod;
+     }
+     //if ((self->sortid / 100) % 2)
+-    if (abs(rand32()) % 2)
++    if (rand32() % 2)
+     {
+         mod = 3 - mod;
+     }
 @@ -34607,7 +34612,7 @@ void keyboard_setup(int player)
      strncpy(buttonnames[SDID_SPECIAL], "Special", 16);
      strncpy(buttonnames[SDID_START], "Start", 16);
