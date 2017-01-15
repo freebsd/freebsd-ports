54085c36e998e7fc7c3f9cbdbcd1a3d1752a7683
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2017-01-15 14:16:31 +0000

    Centralize all cmake/modules/FindHUNSPELL.cmake patches in one file.
    While here, note that the addition of hunspell-1.6 was done upstream too.

--- x11/kdelibs4/files/patch-cmake_modules_FindHUNSPELL.cmake
+++ x11/kdelibs4/files/patch-cmake_modules_FindHUNSPELL.cmake
@@ -1,3 +1,11 @@
+Includes the following two upstream commits:
+
+commit c828f8592fcfd6c2a66ebc18a826de38d6a2fef2
+Author: Pino Toscano <pino@kde.org>
+Date:   Sat Dec 31 12:08:59 2016 +0100
+
+    cmake: look for hunspell-1.6 as well
+
 commit 2ab2745eb01f73355c490ac8d5d1837dec84fd6c
 Author: Wolfgang Bauer <wbauer@tmo.at>
 Date:   Thu Oct 20 15:51:29 2016 +0200
@@ -13,7 +21,7 @@ Date:   Thu Oct 20 15:51:29 2016 +0200
  FIND_PATH(HUNSPELL_INCLUDE_DIR hunspell/hunspell.hxx )
  
 -FIND_LIBRARY(HUNSPELL_LIBRARIES NAMES hunspell-1.3 hunspell-1.2)
-+FIND_LIBRARY(HUNSPELL_LIBRARIES NAMES hunspell-2.0 hunspell-1.5 hunspell-1.4 hunspell-1.3 hunspell-1.2)
++FIND_LIBRARY(HUNSPELL_LIBRARIES NAMES hunspell-2.0 hunspell-1.6 hunspell-1.5 hunspell-1.4 hunspell-1.3 hunspell-1.2)
  
  # handle the QUIETLY and REQUIRED arguments and set HUNSPELL_FOUND to TRUE if 
  # all listed variables are TRUE
