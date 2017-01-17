360c81eaf17d83c1a42fc54a898a4430fade2938
olivierd <olivierd@FreeBSD.org>
2017-01-17 16:22:11 +0000

    Update to 1.14.1
    Changelog: https://github.com/michael-lazar/rtv/blob/v1.14.1/CHANGELOG.rst#1141-2017-01-12
    
    PR:		216023
    Submitted by:	Danilo G. Baio (maintainer)

--- www/rtv/files/patch-setup.py
+++ www/rtv/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2016-10-18 02:27:09 UTC
+--- setup.py.orig	2017-01-13 02:49:35 UTC
 +++ setup.py
-@@ -18,7 +18,7 @@ setuptools.setup(
+@@ -16,7 +16,7 @@ setuptools.setup(
      keywords='reddit terminal praw curses',
      packages=['rtv'],
      package_data={'rtv': ['templates/*']},
 -    data_files=[("share/man/man1", ["rtv.1"])],
 +    data_files=[("man/man1", ["rtv.1"])],
-     install_requires=requirements,
-     entry_points={'console_scripts': ['rtv=rtv.__main__:main']},
-     classifiers=[
+     install_requires=[
+         'praw >=3.5, <4',
+         'six',
