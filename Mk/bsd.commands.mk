d7b51d6e3b9874fae397ddfa8717c3be71d84edb
tijl <tijl@FreeBSD.org>
2017-01-18 13:20:31 +0000

    The output of tools like awk, date, sort, tr,... depends on the current locale set by the user.  Add LANG=C and LC_ALL=C at the beginning of bsd.port.mk and export them so all commands are executed with the C locale. LC_ALL=C overrides all other LC_* variables.  LANG is used by setlocale(3) as default value for LC_* variables, so normally it isn't used when LC_ALL is set, but there's code out there that looks at LANG directly so it's safer to set it as well.  The only commands not captured by this are != assignments before any inclusion of bsd.port.*mk.
    Introduce USE_LOCALE=<locale> that adds LANG=<locale> and LC_ALL=<locale> to
    CONFIGURE_ENV and MAKE_ENV so upstream build systems can be executed with a
    different locale (e.g. USE_LOCALE=en_US.UTF-8).
    
    PR:		215882
    Exp-run by:	antoine
    Approved by:	portmgr (antoine)

--- Mk/bsd.commands.mk
+++ Mk/bsd.commands.mk
@@ -88,7 +88,7 @@ SU_CMD?=		/usr/bin/su root -c
 SYSCTL?=		/sbin/sysctl
 TAIL?=			/usr/bin/tail
 TEST?=			test	# Shell builtin
-TR?=			LANG=C /usr/bin/tr
+TR?=			/usr/bin/tr
 TRUE?=			true	# Shell builtin
 UMOUNT?=		/sbin/umount
 UNAME?=			/usr/bin/uname
