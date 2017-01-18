1067a2e174d0008471c71d29032b33688ad479f3
junovitch <junovitch@FreeBSD.org>
2017-01-18 11:24:40 +0000

    dns/powerdns-recursor: update 4.0.3 -> 4.0.4
    Changes:	https://doc.powerdns.com/md/changelog/#powerdns-recursor-404
    
    PR:		216135
    Reported by:	Dani <i.dani@outlook.com>
    Submitted by:	ghostonthewire@gmail.com
    Approved by:	Ralf van der Enden <tremere@cainites.net> (maintainer)
    Security:	CVE-2016-7068
    Security:	CVE-2016-7073
    Security:	CVE-2016-7074
    Security:	https://vuxml.FreeBSD.org/freebsd/e3200958-dd6c-11e6-ae1b-002590263bf5.html
    MFH:		2017Q1

--- dns/powerdns-recursor/files/patch-dnsname.hh
+++ dns/powerdns-recursor/files/patch-dnsname.hh
@@ -1,13 +1,11 @@
-https://github.com/PowerDNS/pdns/issues/2955
-
---- dnsname.hh.orig	2016-09-06 12:50:29 UTC
+--- dnsname.hh.orig	2017-01-17 03:18:41 UTC
 +++ dnsname.hh
 @@ -30,7 +30,7 @@
  #include <boost/version.hpp>
  
  // it crashes on OSX and doesn't compile on OpenBSD
--#if BOOST_VERSION >= 104800 && ! defined( __APPLE__ ) && ! defined(__OpenBSD__)
-+#if BOOST_VERSION >= 104800 && ! defined(_LIBCPP_VERSION) && ! defined(__OpenBSD__)
+-#if BOOST_VERSION >= 105300 && ! defined( __APPLE__ ) && ! defined(__OpenBSD__)
++#if BOOST_VERSION >= 105300 && ! defined(_LIBCPP_VERSION) && ! defined(__OpenBSD__)
  #include <boost/container/string.hpp>
  #endif
  
@@ -15,8 +13,8 @@ https://github.com/PowerDNS/pdns/issues/2955
    inline bool canonCompare(const DNSName& rhs) const;
    bool slowCanonCompare(const DNSName& rhs) const;  
  
--#if BOOST_VERSION >= 104800 && ! defined( __APPLE__ ) && ! defined(__OpenBSD__)
-+#if BOOST_VERSION >= 104800 && ! defined(_LIBCPP_VERSION) && ! defined(__OpenBSD__)
+-#if BOOST_VERSION >= 105300 && ! defined( __APPLE__ ) && ! defined(__OpenBSD__)
++#if BOOST_VERSION >= 105300 && ! defined(_LIBCPP_VERSION) && ! defined(__OpenBSD__)
    typedef boost::container::string string_t;
  #else
    typedef std::string string_t;
