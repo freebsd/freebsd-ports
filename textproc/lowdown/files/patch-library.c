bc48b53f2c73e0f85f8d03138fd9b243c0ca023a
Baptiste Daroussin <bapt@FreeBSD.org>
2017-01-20 13:31:05 +0000

    New port: lowdown
    lowdown is just another Markdown translator. It can output traditional HTML or
    a document for your troff type-setter of choice, such as groff(1), Heirloom
    troff, or even mandoc(1). lowdown doesn't require XSLT, Python, or even Perl -
    it's just clean, secure, open source C code with no dependencies. Its
    canonical documentation is the lowdown(1) manpage.
    
    WWW: http://kristaps.bsd.lv/lowdown/

---
+++ textproc/lowdown/files/patch-library.c
@@ -0,0 +1,10 @@
+--- library.c.orig	2017-01-19 23:17:59 UTC
++++ library.c
+@@ -16,6 +16,7 @@
+  */
+ #include <err.h>
+ #include <stdio.h>
++#include <stdint.h>
+ #include <stdlib.h>
+ #include <string.h>
+ 
