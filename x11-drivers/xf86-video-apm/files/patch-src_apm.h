05e51dae771b9ce1be6b9b6d0950e41b283dd369
bapt <bapt@FreeBSD.org>
2017-01-23 22:43:23 +0000

    Import few patches from upstream to allow building with newer xorg
    Note that those drivers are barely maintained and might disappear in the futur
    
    PR:		216279
    Submitted by:	matthew@reztek.cz

--- x11-drivers/xf86-video-apm/files/patch-src_apm.h
+++ x11-drivers/xf86-video-apm/files/patch-src_apm.h
@@ -1,4 +1,13 @@
---- src/apm.h.orig	2012-07-17 04:52:01 UTC
+From 9f67408e4f078e907fbf4feb45cfdcfd0cdebcca Mon Sep 17 00:00:00 2001
+From: Adam Jackson <ajax@redhat.com>
+Date: Wed, 9 Jan 2013 22:03:29 -0500
+Subject: Remove #include "mibstore.h"
+
+Signed-off-by: Adam Jackson <ajax@redhat.com>
+
+diff --git a/src/apm.h b/src/apm.h
+index a3c06f8..365e5e2 100644
+--- src/apm.h	2012-07-17 04:52:01 UTC
 +++ src/apm.h
 @@ -33,9 +33,6 @@
  /* All drivers initialising the SW cursor need this */
@@ -10,3 +19,6 @@
  /* I2C support */
  #include "xf86i2c.h"
  
+-- 
+cgit v0.10.2
+
