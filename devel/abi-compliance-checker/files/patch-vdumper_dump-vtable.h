6577d2ecc6d44788be8fa05b687554cc82169310
kib <kib@FreeBSD.org>
2017-01-25 16:28:05 +0000

    Update devel/abi-compliance-checker to the current git snapshot.
    The script now depends on two tools abi-dumper and vtable-dumper.  The
    only purpose of the tools is to provide neccessary services to the
    abi-compliance-checker, so I included all of them into the updated
    port, instead of providing dependencies.
    
    Sponsored by:	The FreeBSD Foundation
    Reviewed, tested and approved by:	ehaupt
    Differential revision:	https://reviews.freebsd.org/D9315

---
+++ devel/abi-compliance-checker/files/patch-vdumper_dump-vtable.h
@@ -0,0 +1,10 @@
+--- vdumper/dump-vtable.h.orig	2016-10-04 16:28:00 UTC
++++ vdumper/dump-vtable.h
+@@ -22,6 +22,7 @@
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
++#include <stdint.h>
+ #include <dlfcn.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
