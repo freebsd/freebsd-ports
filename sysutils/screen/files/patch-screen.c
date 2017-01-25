1cbcdcecea47b92331e2f0381878d2d73477b969
Cy Schubert <cy@FreeBSD.org>
2017-01-25 01:36:42 +0000

    Circumvent a trivial root privilege escalation.
    See:
    https://lists.gnu.org/archive/html/screen-devel/2017-01/msg00025.html
    
    Reported by:	Tim Zingelman <tez@pkgsrc.org>
    Security:	CVE not assigned yet

--- sysutils/screen/files/patch-screen.c
+++ sysutils/screen/files/patch-screen.c
@@ -1,6 +1,21 @@
 --- screen.c.orig	2017-01-17 11:28:29.397404660 -0800
-+++ screen.c	2017-01-18 04:54:50.874421000 -0800
-@@ -2222,7 +2222,7 @@
++++ screen.c	2017-01-24 17:31:24.342944000 -0800
+@@ -674,11 +674,14 @@
+               if (strlen(screenlogfile) > PATH_MAX)
+                 Panic(0, "-L: logfile name too long. (max. %d char)", PATH_MAX);
+ 
++#if 0
++/* see https://lists.gnu.org/archive/html/screen-devel/2017-01/msg00025.html */
+               FILE *w_check;
+               if ((w_check = fopen(screenlogfile, "w")) == NULL)
+                 Panic(0, "-L: logfile name access problem");
+               else
+                 fclose(w_check);
++#endif
+             }
+             nwin_options.Lflag = 1;
+             break;
+@@ -2222,7 +2225,7 @@
    pn2 = pn = p + padlen;
    r = winmsg_numrend;
    while (p >= buf) {
