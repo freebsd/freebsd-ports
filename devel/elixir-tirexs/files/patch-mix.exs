6754da52d7f04d12833f94a1d3115a5b83253835
olgeni <olgeni@FreeBSD.org>
2017-01-25 18:38:12 +0000

    Fix devel/elixir-tirexs build for Elixir 1.4.

--- devel/elixir-tirexs/files/patch-mix.exs
+++ devel/elixir-tirexs/files/patch-mix.exs
@@ -1,7 +1,11 @@
---- mix.exs.orig	2016-08-08 12:02:38 UTC
+--- mix.exs.orig	2016-12-26 08:57:32 UTC
 +++ mix.exs
-@@ -11,7 +11,6 @@ defmodule Tirexs.Mixfile do
-       elixir: "~> 1.3.0",
+@@ -8,10 +8,9 @@ defmodule Tirexs.Mixfile do
+       version: "0.8.12",
+       source_url: github,
+       homepage_url: github,
+-      elixir: "~> 1.3.0",
++      elixir: "~> 1.4.0",
        description: description,
        package: package,
 -      deps: deps,
