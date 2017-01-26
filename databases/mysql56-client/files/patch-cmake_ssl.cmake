b6143e34ea781ed87d6812250da2f79695bc514b
Bernard Spil <brnrd@FreeBSD.org>
2017-01-26 19:58:04 +0000

    MFH: r431919 r431968 r431975 r432035 r432066 r432458
    databases/mysql56: Update to 5.6.35
    
    - Port improvements from MySQL 5.7 port
    - Use system libs instead of bundled
    - Fix many open PRs
    - Change MAINTAINER
    
    PR:		216244 192657 198812 199751 205093
    PR:		209618 211248 205983 209338
    
    databases/mysql56-server: Rollback rc script changes
    
    databases/mysql56-server: Do not install my.cnf sample
    
    An issue was discovered where users of mysql did not have a my.cnf and
    the recent update was causing mysqld to find a sample my.cnf and load
    its parameters. This was causing errors on startup for users of innodb
    as the parameters used to init the database did not match the ones in
    the sample config file it was now reading.
    
    databases/mysql56-server: Fix build with LibreSSL
    
      - Fix CMake SSL detection
      - Always set WITH_SSL=${OPENSSLBASE}
    
    PR:		216311
    Approved by:	Mahdi Moktari <mokhi64@gmail.com> (maintainer)
    Differential Revision:	D9272
    
    Revert r432035 part 2, it breaks build with openssl from base
    
    With hat:	portmgr
    
    databases/mysql56-server: Fix OpenSSL linking
    
      - Force dynamic linking with OpenSSL
    
    Approved by:	ports-secteam (feld)

---
+++ databases/mysql56-client/files/patch-cmake_ssl.cmake
@@ -0,0 +1,11 @@
+--- cmake/ssl.cmake.orig	2016-11-28 13:36:22 UTC
++++ cmake/ssl.cmake
+@@ -176,7 +176,7 @@ MACRO (MYSQL_CHECK_SSL)
+     IF(OPENSSL_INCLUDE_DIR AND
+        OPENSSL_LIBRARY   AND
+        CRYPTO_LIBRARY      AND
+-       OPENSSL_MAJOR_VERSION STREQUAL "1"
++       OPENSSL_MAJOR_VERSION VERSION_GREATER_EQUAL "1"
+       )
+       SET(OPENSSL_FOUND TRUE)
+     ELSE()
--- databases/mysql56-server/files/patch-mysys_ssl_my_default.cc
+++ databases/mysql56-server/files/patch-mysys_ssl_my_default.cc
@@ -1,6 +1,6 @@
---- mysys_ssl/my_default.cc.orig	2013-01-22 17:54:49.000000000 +0100
-+++ mysys_ssl/my_default.cc	2013-02-13 11:08:15.000000000 +0100
-@@ -119,7 +119,7 @@
+--- mysys_ssl/my_default.cc.orig	2016-11-28 13:36:22 UTC
++++ mysys_ssl/my_default.cc
+@@ -110,7 +110,7 @@ static my_bool defaults_already_read= FA
  
  /* Which directories are searched for options (and in which order) */
  
@@ -9,18 +9,48 @@
  #define DEFAULT_DIRS_SIZE (MAX_DEFAULT_DIRS + 1)  /* Terminate with NULL */
  static const char **default_directories = NULL;
  
-@@ -1395,7 +1395,10 @@
+@@ -903,6 +903,14 @@ static int search_default_file_with_ext(
+       return 1;                                 /* Ignore wrong files */
+   }
  
- #if defined(DEFAULT_SYSCONFDIR)
-   if (DEFAULT_SYSCONFDIR[0])
++  if (strstr(name, "/etc") == name)
 +  {
-     errors += add_directory(alloc, DEFAULT_SYSCONFDIR, dirs);
-+    errors += add_directory(alloc, DEFAULT_SYSCONFDIR "/mysql", dirs);
++    fprintf(stderr,
++             "error: Config file %s in invalid location, please move to or merge with /usr/local%s\n",
++             name,name);
++      goto err;
 +  }
- #endif /* DEFAULT_SYSCONFDIR */
++
+   while (mysql_file_getline(buff, sizeof(buff) - 1, fp, is_login_file))
+   {
+     line++;
+@@ -1241,7 +1249,8 @@ void my_print_default_files(const char *
+             end[(strlen(end)-1)] = ' ';
+           else
+             strxmov(end, conf_file, *ext , " ",  NullS);
+-          fputs(name, stdout);
++          if (strstr(name, "/etc") != name)
++            fputs(name, stdout);
+         }
+       }
+     }
+@@ -1400,13 +1409,8 @@ static const char **init_default_directo
+ 
+ #else
+ 
+-  errors += add_directory(alloc, "/etc/", dirs);
+-  errors += add_directory(alloc, "/etc/mysql/", dirs);
+-
+-#if defined(DEFAULT_SYSCONFDIR)
+-  if (DEFAULT_SYSCONFDIR[0])
+-    errors += add_directory(alloc, DEFAULT_SYSCONFDIR, dirs);
+-#endif /* DEFAULT_SYSCONFDIR */
++  errors += add_directory(alloc, "/usr/local/etc/", dirs);
++  errors += add_directory(alloc, "/usr/local/etc/mysql/", dirs);
  
  #endif
-@@ -1466,7 +1469,7 @@
+ 
+@@ -1477,7 +1481,7 @@ int check_file_permissions(const char *f
    MY_STAT stat_info;
  
    if (!my_stat(file_name,&stat_info,MYF(0)))
