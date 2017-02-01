1a3b32b8419e8a363d3bf16867b55dc434859b3e
jkim <jkim@FreeBSD.org>
2017-02-01 00:52:29 +0000

    Update to 5.2.5.

--- editors/libreoffice/files/patch-sw_Module__sw.mk
+++ editors/libreoffice/files/patch-sw_Module__sw.mk
@@ -1,14 +1,13 @@
---- sw/Module_sw.mk.orig	2016-10-28 14:50:26 UTC
+--- sw/Module_sw.mk.orig	2017-01-12 00:54:33 UTC
 +++ sw/Module_sw.mk
-@@ -62,13 +62,16 @@ $(eval $(call gb_Module_add_slowcheck_ta
+@@ -62,13 +62,15 @@ $(eval $(call gb_Module_add_slowcheck_ta
      CppunitTest_sw_ooxmlfieldexport \
      CppunitTest_sw_ooxmlw14export \
      CppunitTest_sw_ooxmlimport \
 -    CppunitTest_sw_ww8export \
 -    CppunitTest_sw_ww8import \
 +    $(if $(filter FREEBSD,$(OS)),, \
-+        CppunitTest_sw_ww8export) \
-+    $(if $(filter FREEBSD,$(OS)),, \
++        CppunitTest_sw_ww8export \
 +        CppunitTest_sw_ww8import) \
      CppunitTest_sw_rtfexport \
      CppunitTest_sw_rtfimport \
