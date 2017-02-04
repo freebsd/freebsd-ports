e3165aba9a25e9b979a6d8ae6a1d6afac214bb64
db <db@FreeBSD.org>
2017-02-04 20:09:25 +0000

    Use github for psci monitor  [brd] Chase upstream Redo save_boot_params in lowlevel__init.S to add reserve_memory remove save_boot_params from rpi.c Note network code diffs are now upstreamed already
    PR:		ports/216757
    Submitted by:	brd

--- sysutils/u-boot-rpi3/files/patch-arch_arm_include_asm_system.h
+++ sysutils/u-boot-rpi3/files/patch-arch_arm_include_asm_system.h
@@ -1,6 +1,6 @@
---- arch/arm/include/asm/system.h.orig	2016-11-14 16:27:11 UTC
+--- arch/arm/include/asm/system.h.orig	2017-01-09 16:57:05 UTC
 +++ arch/arm/include/asm/system.h
-@@ -201,22 +201,6 @@ void __noreturn psci_system_off(void);
+@@ -329,22 +329,6 @@ void psci_arch_init(void);
   * void save_boot_params(u32 r0, u32 r1, u32 r2, u32 r3);
   */
  
@@ -23,7 +23,7 @@
  #ifdef CONFIG_ARMV7_LPAE
  void switch_to_hypervisor_ret(void);
  #endif
-@@ -410,6 +394,22 @@ void mmu_page_table_flush(unsigned long 
+@@ -538,6 +522,22 @@ void mmu_page_table_flush(unsigned long 
  
  #ifndef __ASSEMBLY__
  /**
