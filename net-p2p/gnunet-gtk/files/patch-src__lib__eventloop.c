a835ea4d6850eff9b54120ce65e807cfeb7e5f7b
amdmi3 <amdmi3@FreeBSD.org>
2017-02-04 09:01:28 +0000

    - Fix LICENSE - Add LICENSE_FILE - Switch to options helpers - Regenerate patches

--- net-p2p/gnunet-gtk/files/patch-src__lib__eventloop.c
+++ net-p2p/gnunet-gtk/files/patch-src__lib__eventloop.c
@@ -1,6 +1,6 @@
---- src/lib/eventloop.c.orig
+--- src/lib/eventloop.c.orig	2014-02-13 15:26:55 UTC
 +++ src/lib/eventloop.c
-@@ -181,7 +181,7 @@
+@@ -181,7 +181,7 @@ GtkWidget *
  GNUNET_GTK_plug_me (const char *env_name,
                      GtkWidget *main_window)
  {
