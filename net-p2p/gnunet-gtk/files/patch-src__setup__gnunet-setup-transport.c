b2b24940f19fa597d0b60a5ac91f543808ffd08b
Dmitry Marakasov <amdmi3@FreeBSD.org>
2017-02-04 09:01:28 +0000

    - Fix LICENSE - Add LICENSE_FILE - Switch to options helpers - Regenerate patches

--- net-p2p/gnunet-gtk/files/patch-src__setup__gnunet-setup-transport.c
+++ net-p2p/gnunet-gtk/files/patch-src__setup__gnunet-setup-transport.c
@@ -1,6 +1,6 @@
---- src/setup/gnunet-setup-transport.c.orig
+--- src/setup/gnunet-setup-transport.c.orig	2014-01-07 10:40:01 UTC
 +++ src/setup/gnunet-setup-transport.c
-@@ -349,8 +349,7 @@
+@@ -349,8 +349,7 @@ nipo (void *cls, const char *name, int i
    if ( (sizeof (struct sockaddr_in6) == addrlen) &&
         (0 != memcmp (&in6addr_loopback,
                       &((struct sockaddr_in6 *)addr)->sin6_addr,
--- net-p2p/gnunet-gtk/files/patch-src_main_gnunet-gtk.c
+++ net-p2p/gnunet-gtk/files/patch-src_main_gnunet-gtk.c
@@ -1,6 +1,6 @@
---- src/main/gnunet-gtk.c.orig
+--- src/main/gnunet-gtk.c.orig	2014-04-08 07:44:56 UTC
 +++ src/main/gnunet-gtk.c
-@@ -177,7 +177,7 @@
+@@ -177,7 +177,7 @@ gnunet_gtk_quit_cb (GObject * object,
  }
  
  
@@ -9,7 +9,7 @@
  /**
   * Start the child process for the plug.
   *
-@@ -308,7 +308,7 @@
+@@ -308,7 +308,7 @@ run (void *cls,
    main_window = GTK_WIDGET (get_object ("gnunet_gtk_window"));
    GNUNET_SCHEDULER_add_delayed (GNUNET_TIME_UNIT_FOREVER_REL,
  				&cleanup_task, NULL);
