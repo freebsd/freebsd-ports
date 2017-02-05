0419f1099e0dca8a90f4f8f22347803e8ec786ff
ashish <ashish@FreeBSD.org>
2017-02-05 20:27:03 +0000

    - Update to 1.0.2 - Correct LICENSE to be GPLv2+ - Fix LIB_DEPENDS

--- irc/irssi-otr/files/patch-src_module.c
+++ irc/irssi-otr/files/patch-src_module.c
@@ -1,6 +1,6 @@
---- src/module.c	2014-02-12 18:50:45.000000000 -0600
-+++ src/module.c	2016-04-07 17:35:00.000000000 +0530
-@@ -32,6 +32,7 @@
+--- src/module.c.orig	2016-12-07 21:24:08 UTC
++++ src/module.c
+@@ -33,6 +33,7 @@
  #include "otr.h"
  #include "otr-formats.h"
  #include "utils.h"
@@ -8,7 +8,7 @@
  
  GCRY_THREAD_OPTION_PTHREAD_IMPL;
  
-@@ -90,8 +91,13 @@ static void sig_server_sendmsg(SERVER_REC *server, const char *target,
+@@ -91,8 +92,13 @@ end:
  /*
   * Pipes all incoming private messages through OTR
   */
@@ -23,7 +23,7 @@
  {
  	int ret;
  	char *new_msg = NULL;
-@@ -106,7 +112,11 @@ void sig_message_private(SERVER_REC *server, const char *msg,
+@@ -107,7 +113,11 @@ void sig_message_private(SERVER_REC *ser
  
  	if (!new_msg) {
  		/* This message was not OTR */
@@ -36,18 +36,3 @@
  	} else {
  		/*
  		 * Check for /me IRC marker and if so, handle it so the user does not
-@@ -371,3 +371,13 @@ void otr_deinit(void)
-  
- 	theme_unregister();
- }
-+
-+#ifdef IRSSI_ABI_VERSION
-+/*
-+ * irssi abicheck()
-+ */
-+void otr_abicheck(int *version)
-+{
-+	*version = IRSSI_ABI_VERSION;
-+}
-+#endif
- 
