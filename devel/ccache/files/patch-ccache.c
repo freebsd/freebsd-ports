8eb956b9717e100e89c44c63e0433accef10fae3
Bryan Drewery <bdrewery@FreeBSD.org>
2017-02-07 18:28:16 +0000

    - Update to 3.3.3
    Changes:	https://ccache.samba.org/releasenotes.html#_ccache_3_3_3

--- devel/ccache/files/patch-ccache.c
+++ devel/ccache/files/patch-ccache.c
@@ -4,49 +4,49 @@
   already available.
 -bdrewery
 
---- ccache.c.orig	2015-10-08 19:14:23 UTC
-+++ ccache.c
-@@ -1350,6 +1350,11 @@ compiler_is_clang(struct args *args)
+--- ccache.c.orig	2016-10-26 14:31:19.000000000 -0700
++++ ccache.c	2017-02-03 14:24:35.466505000 -0800
+@@ -1468,6 +1468,11 @@ compiler_is_clang(struct args *args)
  {
  	char *name = basename(args->argv[0]);
- 	bool is = strstr(name, "clang") != NULL;
+ 	bool result = strstr(name, "clang") != NULL;
 +#ifdef CC_IS_CLANG
 +	if (strcmp(name, "cc") == 0 || strcmp(name, "CC") == 0 ||
 +	    strcmp(name, "c++") == 0)
-+	        is = true;
++	        result = true;
 +#endif
  	free(name);
- 	return is;
+ 	return result;
  }
-@@ -1359,6 +1364,11 @@ compiler_is_gcc(struct args *args)
+@@ -1477,6 +1482,11 @@ compiler_is_gcc(struct args *args)
  {
  	char *name = basename(args->argv[0]);
- 	bool is = strstr(name, "gcc") || strstr(name, "g++");
+ 	bool result = strstr(name, "gcc") || strstr(name, "g++");
 +#ifdef CC_IS_GCC
 +	if (strcmp(name, "cc") == 0 || strcmp(name, "CC") == 0 ||
 +	    strcmp(name, "c++") == 0)
-+	        is = true;
++	        result = true;
 +#endif
  	free(name);
- 	return is;
+ 	return result;
  }
-@@ -1463,6 +1473,7 @@ calculate_common_hash(struct args *args,
+@@ -1592,6 +1602,7 @@ calculate_common_hash(struct args *args,
  		free(p);
  	}
  
 +#if 0
- 	/* Possibly hash GCC_COLORS (for color diagnostics). */
+ 	// Possibly hash GCC_COLORS (for color diagnostics).
  	if (compiler_is_gcc(args)) {
  		const char *gcc_colors = getenv("GCC_COLORS");
-@@ -1471,6 +1482,7 @@ calculate_common_hash(struct args *args,
+@@ -1600,6 +1611,7 @@ calculate_common_hash(struct args *args,
  			hash_string(hash, gcc_colors);
  		}
  	}
 +#endif
  }
  
- /*
-@@ -1509,6 +1521,13 @@ calculate_object_hash(struct args *args,
+ // Update a hash sum with information specific to the direct and preprocessor
+@@ -1629,6 +1641,13 @@ calculate_object_hash(struct args *args,
  			continue;
  		}
  
@@ -57,6 +57,6 @@
 +			continue;
 +		}
 +
- 		/* The -fdebug-prefix-map option may be used in combination with
- 		 * CCACHE_BASEDIR to reuse results across different directories. Skip it
- 		 * from hashing. */
+ 		// The -fdebug-prefix-map option may be used in combination with
+ 		// CCACHE_BASEDIR to reuse results across different directories. Skip it
+ 		// from hashing.
