a53c8a58654c2a6463b54264b116a33d2ab83a82
cpm <cpm@FreeBSD.org>
2017-02-09 11:52:49 +0000

    www/chromium: fix segfault which prevents to load some websites.
    PR:		207298
    MFH:		2017Q1 (blanket)

--- www/chromium/files/patch-content_browser_memory_memory__coordinator__impl.cc
+++ www/chromium/files/patch-content_browser_memory_memory__coordinator__impl.cc
@@ -1,11 +1,11 @@
---- content/browser/memory/memory_coordinator_impl.cc.orig	2017-01-26 00:49:13 UTC
+--- content/browser/memory/memory_coordinator_impl.cc.orig	2017-02-02 02:02:53 UTC
 +++ content/browser/memory/memory_coordinator_impl.cc
 @@ -116,8 +116,12 @@ void RecordMetricsOnStateChange(base::Me
  struct MemoryCoordinatorSingletonTraits
      : public base::LeakySingletonTraits<MemoryCoordinator> {
    static MemoryCoordinator* New() {
 +#if defined(OS_BSD)
-+    return NULL;
++    return nullptr;
 +#else
      return new MemoryCoordinatorImpl(base::ThreadTaskRunnerHandle::Get(),
                                       CreateMemoryMonitor());
--- www/chromium/files/patch-ui_gfx_font__fallback__linux.cc
+++ www/chromium/files/patch-ui_gfx_font__fallback__linux.cc
@@ -1,11 +1,11 @@
---- ui/gfx/font_fallback_linux.cc.orig	2017-01-26 00:49:31 UTC
+--- ui/gfx/font_fallback_linux.cc.orig	2017-02-02 02:03:13 UTC
 +++ ui/gfx/font_fallback_linux.cc
 @@ -15,6 +15,10 @@
  #include "base/memory/ptr_util.h"
  #include "ui/gfx/font.h"
  
 +#if defined(OS_BSD)
-+#  include <unistd.h>
++#include <unistd.h>
 +#endif
 +
  namespace gfx {
