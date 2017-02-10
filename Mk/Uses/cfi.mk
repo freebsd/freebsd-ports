1dddc889ea06347b3b2ca632c606cbc81dc54bd7
Shawn Webb <shawn.webb@hardenedbsd.org>
2017-02-10 18:50:39 -0500

    HBSD: Add cfi USES flag
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Sponsored-by:	SoldierX

---
+++ Mk/Uses/cfi.mk
@@ -0,0 +1,21 @@
+# $FreeBSD$
+#
+# Compile a port with CFI
+#
+# Feature:      cfi
+# Usage:        USES=cfi
+#
+# MAINTAINER:	shawn.webb@hardenedbsd.org
+
+.if !defined(_INCLUDE_USES_CFI_MK)
+_INCLUDE_USES_CFI_MK=    yes
+.if ${OSVERSION} >= 1200020
+.if ${ARCH} == "amd64"
+
+CFLAGS+=	-fsanitize=cfi -fvisibility=hidden -flto
+CXXFLAGS+=	-fsanitize=cfi -fvisibility=hidden -flto
+LDFLAGS+=	-fsanitize=cfi -fvisibility=hidden -flto
+
+.endif
+.endif
+.endif
