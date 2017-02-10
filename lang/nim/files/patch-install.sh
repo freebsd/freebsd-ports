985fde16e78158209db4c17c1e5c53eaf1bfd6d3
krion <krion@FreeBSD.org>
2017-02-10 10:19:25 +0000

    Update lang/nim to 0.16.0
    PR:		215941
    Submitted by:	maintainer
    Approved by:	mat (mentor)
    Differential Revision:	https://reviews.freebsd.org/D9509

--- lang/nim/files/patch-install.sh
+++ lang/nim/files/patch-install.sh
@@ -1,6 +1,6 @@
---- install.sh.orig	2016-12-13 16:08:57 UTC
-+++ install.sh
-@@ -32,48 +32,14 @@
+--- install.sh.orig	2017-01-10 13:55:39.983263000 +0000
++++ install.sh	2017-01-10 14:01:20.665343000 +0000
+@@ -15,48 +15,14 @@
      "--help"|"-h"|"help"|"h")
        echo "Nim installation script"
        echo "Usage: [sudo] sh install.sh DIR"
@@ -19,7 +19,7 @@
 -      libdir=/usr/lib/nim
 -      docdir=/usr/share/nim/doc
 -      datadir=/usr/share/nim/data
--      nimbleDir="/opt/nimble/pkgs/compiler-0.15.2"
+-      nimbleDir="/opt/nimble/pkgs/compiler-0.16.0"
 -      ;;
 -    "/usr/local/bin")
 -      bindir=/usr/local/bin
@@ -27,7 +27,7 @@
 -      libdir=/usr/local/lib/nim
 -      docdir=/usr/local/share/nim/doc
 -      datadir=/usr/local/share/nim/data
--      nimbleDir="/opt/nimble/pkgs/compiler-0.15.2"
+-      nimbleDir="/opt/nimble/pkgs/compiler-0.16.0"
 -      ;;
 -    "/opt")
 -      bindir="/opt/nim/bin"
@@ -35,7 +35,7 @@
 -      libdir="/opt/nim/lib"
 -      docdir="/opt/nim/doc"
 -      datadir="/opt/nim/data"
--      nimbleDir="/opt/nimble/pkgs/compiler-0.15.2"
+-      nimbleDir="/opt/nimble/pkgs/compiler-0.16.0"
 -      mkdir -p /opt/nim
 -      mkdir -p $bindir
 -      mkdir -p $configdir
@@ -54,3 +54,17 @@
        nimbleDir="$1/nim"
        mkdir -p $1/nim
        mkdir -p $bindir
+@@ -823,13 +789,6 @@
+ else
+   echo "Nim installation script"
+   echo "Usage: [sudo] sh install.sh DIR"
+-  echo "Where DIR may be:"
+-  echo "  /usr/bin"
+-  echo "  /usr/local/bin"
+-  echo "  /opt"
+-  echo "  <some other dir> (treated similar to '/opt')"
+-  echo "To deinstall, use the command:"
+-  echo "sh deinstall.sh DIR"
+   exit 1
+ fi
+ 
