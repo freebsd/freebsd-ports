72d107f4f7716900d6519167cf8ac0bac32f1ae7
jkim <jkim@FreeBSD.org>
2017-02-10 02:02:54 +0000

    Refine r433663.  If "use-header-bar" property of the dialog is set to TRUE, it uses a GtkHeaderBar for action buttons instead of the action area and gtk_dialog_get_header_bar() should not return NULL.  If it is set to FALSE, gtk_dialog_get_header_bar() always returns NULL.  In other words, we should not remove buttons from both header bar and action area in any case.  This patch also removes pointless assertions from the previous patch.
    PR:		203563

--- editors/libreoffice/files/patch-vcl_unx_gtk_fpicker_SalGtkFilePicker.cxx
+++ editors/libreoffice/files/patch-vcl_unx_gtk_fpicker_SalGtkFilePicker.cxx
@@ -1,26 +1,16 @@
 --- vcl/unx/gtk/fpicker/SalGtkFilePicker.cxx.orig	2017-01-12 00:54:33 UTC
 +++ vcl/unx/gtk/fpicker/SalGtkFilePicker.cxx
-@@ -418,8 +418,11 @@ shrinkFilterName( const OUString &rFilte
- static void
- dialog_remove_buttons(GtkWidget *pActionArea)
- {
--    GList *pChildren =
--        gtk_container_get_children( GTK_CONTAINER( pActionArea ) );
-+    GtkContainer *pContainer = GTK_CONTAINER( pActionArea );
-+
-+    g_return_if_fail( pContainer != nullptr );
-+
-+    GList *pChildren = gtk_container_get_children( pContainer );
- 
-     for( GList *p = pChildren; p; p = p->next )
-     {
-@@ -436,10 +439,10 @@ dialog_remove_buttons( GtkDialog *pDialo
+@@ -436,10 +436,13 @@ dialog_remove_buttons( GtkDialog *pDialo
  {
      g_return_if_fail( GTK_IS_DIALOG( pDialog ) );
  
 -#if GTK_CHECK_VERSION(3,0,0)
  #if GTK_CHECK_VERSION(3,12,0)
-     dialog_remove_buttons(gtk_dialog_get_header_bar(pDialog));
+-    dialog_remove_buttons(gtk_dialog_get_header_bar(pDialog));
++    GtkWidget *pHeaderBar = gtk_dialog_get_header_bar( pDialog );
++    if( pHeaderBar != nullptr )
++        dialog_remove_buttons( pHeaderBar );
++    else
  #endif
 +#if GTK_CHECK_VERSION(2,14,0)
      dialog_remove_buttons(gtk_dialog_get_action_area(pDialog));
