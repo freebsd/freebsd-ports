c1a733d7c2b0cc002ee2b816b91319bae5556097
Dmitry Marakasov <amdmi3@FreeBSD.org>
2017-02-11 09:01:29 +0000

    - Add LICENSE - Switch to USES=localbase - Switch to options helpers - Regenerate patches

--- security/gringotts/files/patch-src_grg__safe.c
+++ security/gringotts/files/patch-src_grg__safe.c
@@ -1,5 +1,5 @@
---- src/grg_safe.c.orig	2008-01-04 22:08:43.000000000 +0900
-+++ src/grg_safe.c	2009-12-22 04:53:36.000000000 +0900
+--- src/grg_safe.c.orig	2008-01-04 13:08:43 UTC
++++ src/grg_safe.c
 @@ -33,6 +33,7 @@
  
  #include <stdlib.h>
@@ -16,7 +16,7 @@
  #include <sys/resource.h>
  #ifdef linux
  #include <sys/utsname.h>
-@@ -96,9 +96,6 @@
+@@ -96,9 +96,6 @@ gboolean
  grg_mlockall_and_drop_root_privileges(void)
  {
      /* drop eventual group root privileges */
@@ -26,7 +26,7 @@
  #ifdef HAVE_SYS_FSUID_H
      setfsgid(getgid());
      setfsgid(getgid());
-@@ -148,8 +145,6 @@
+@@ -148,8 +145,6 @@ grg_mlockall_and_drop_root_privileges(vo
  #endif
  
  	/* drop root privileges */
