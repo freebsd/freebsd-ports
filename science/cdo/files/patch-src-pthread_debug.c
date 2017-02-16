0aa18c6dbdf02415538fed5255c7b83da380e15f
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2017-02-16 16:45:27 +0000

    Update to 1.8.0
    - Regenerate patch files with makepatch:
    
    Changes:	https://code.zmaw.de/news/363

--- science/cdo/files/patch-src-pthread_debug.c
+++ science/cdo/files/patch-src-pthread_debug.c
@@ -1,15 +1,15 @@
---- src/pthread_debug.c.orig	2010-11-12 20:22:44.000000000 +0800
-+++ src/pthread_debug.c	2011-02-26 03:30:00.000000000 +0800
-@@ -77,6 +77,7 @@
+--- src/pthread_debug.c.orig	2016-06-10 08:14:45 UTC
++++ src/pthread_debug.c
+@@ -76,6 +76,7 @@ void print_pthread_attr(const char *call
  }
  
  
 +#if defined (PTHREAD_MUTEXATTR_CONDATTR)
  void print_pthread_mutexattr(const char *caller,  pthread_mutexattr_t *m_attr)
  {
-   /*
-@@ -116,6 +117,7 @@
-   }
+   (void) caller;
+@@ -120,6 +121,7 @@ void print_pthread_condattr(const char *
+   (void) c_attr;
  #endif
  }
 +#endif
