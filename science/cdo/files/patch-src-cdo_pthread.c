6d76cbd89db91cd5db47d946cb716053c29cb369
sunpoet <sunpoet@FreeBSD.org>
2017-02-16 16:45:27 +0000

    Update to 1.8.0
    - Regenerate patch files with makepatch:
    
    Changes:	https://code.zmaw.de/news/363

--- science/cdo/files/patch-src-cdo_pthread.c
+++ science/cdo/files/patch-src-cdo_pthread.c
@@ -1,17 +1,17 @@
---- src/cdo_pthread.c.orig	2007-08-29 17:15:32.000000000 +0800
-+++ src/cdo_pthread.c	2011-02-26 03:30:00.000000000 +0800
-@@ -32,8 +32,10 @@
+--- src/cdo_pthread.c.orig	2017-01-19 08:25:24 UTC
++++ src/cdo_pthread.c
+@@ -32,8 +32,10 @@ void print_pthread_info()
  {
- #if  defined  (HAVE_LIBPTHREAD)
+ #if defined(HAVE_LIBPTHREAD)
    pthread_attr_t attr;
 +#if defined (PTHREAD_MUTEXATTR_CONDATTR)
    pthread_mutexattr_t m_attr;
    pthread_condattr_t c_attr;
 +#endif
  
- #if defined (PTHREAD_KEYS_MAX)
+ #if defined(PTHREAD_KEYS_MAX)
    fprintf(stderr, "PTHREAD_KEYS_MAX    = %d\n", PTHREAD_KEYS_MAX);
-@@ -54,6 +56,7 @@
+@@ -54,6 +56,7 @@ void print_pthread_info()
    print_pthread_attr("Default pthread attr", &attr);
    pthread_attr_destroy(&attr);
  
@@ -19,7 +19,7 @@
    pthread_mutexattr_init(&m_attr);
    print_pthread_mutexattr("Default pthread mutexattr", &m_attr);
    pthread_mutexattr_destroy(&m_attr);
-@@ -61,6 +64,7 @@
+@@ -61,6 +64,7 @@ void print_pthread_info()
    pthread_condattr_init(&c_attr);
    print_pthread_condattr("Default pthread condattr ", &c_attr);
    pthread_condattr_destroy(&c_attr);
