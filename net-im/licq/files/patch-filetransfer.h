6dcd0498ddd03cc8943ed8a97a2f7197f2b8d5f8
dinoex <dinoex@FreeBSD.org>
2017-02-18 08:00:14 +0000

    - make portlint happier - add dependecies

--- net-im/licq/files/patch-filetransfer.h
+++ net-im/licq/files/patch-filetransfer.h
@@ -1,5 +1,5 @@
---- include/licq/icq/filetransfer.h.orig	2014-06-01 21:16:42.000000000 +0200
-+++ include/licq/icq/filetransfer.h	2014-06-06 15:47:07.000000000 +0200
+--- include/licq/icq/filetransfer.h.orig	2014-06-01 19:16:42 UTC
++++ include/licq/icq/filetransfer.h
 @@ -73,6 +73,7 @@
   * which case the file will be saved as <filename>.<timestamp>
   *-------------------------------------------------------------------------*/
