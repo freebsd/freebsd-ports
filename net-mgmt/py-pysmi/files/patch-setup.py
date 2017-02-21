02607212dd8e339ffdf0263d8a3ecacd85358cdc
krion <krion@FreeBSD.org>
2017-02-21 14:32:23 +0000

    Update net-mgmt/py-pysmi to 0.0.7
    PR:		217206
    Submitted by:	maintainer
    Approved by:	mat (mentor)
    Differential Revision:	https://reviews.freebsd.org/D9667

--- net-mgmt/py-pysmi/files/patch-setup.py
+++ net-mgmt/py-pysmi/files/patch-setup.py
@@ -1,12 +1,13 @@
---- setup.py.orig	2015-11-07 19:23:39 UTC
+--- setup.py.orig	2016-02-13 18:11:15 UTC
 +++ setup.py
-@@ -80,7 +80,8 @@ params.update( {
-                   'pysmi.parser',
-                   'pysmi.codegen',
-                   'pysmi.borrower',
--                  'pysmi.writer' ],
-+                  'pysmi.writer',
-+                  'pysmi.tests' ],
-     'scripts': [ os.path.join('scripts','mibdump.py') ]
- } )
+@@ -83,7 +83,9 @@ params.update({
+                  'pysmi.parser',
+                  'pysmi.codegen',
+                  'pysmi.borrower',
+-                 'pysmi.writer'],
++                 'pysmi.writer',
++                 'pysmi.tests',
++    ],
+     'scripts': [os.path.join('scripts', 'mibdump.py')]
+ })
  
