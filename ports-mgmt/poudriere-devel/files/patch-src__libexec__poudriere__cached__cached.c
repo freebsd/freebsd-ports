c2e8980f0dcb4a78fd158409927f62432556e773
Bryan Drewery <bdrewery@FreeBSD.org>
2017-02-22 20:35:50 +0000

    Restore 'umount -f' usage for now as older releases do not support 'umount -n'.
    A better fix will be coming soon.
    
    Reported by:	ohauer

--- ports-mgmt/poudriere-devel/files/patch-src__libexec__poudriere__cached__cached.c
+++ ports-mgmt/poudriere-devel/files/patch-src__libexec__poudriere__cached__cached.c
@@ -1,6 +1,4 @@
-diff --git src/libexec/poudriere/cached/cached.c src/libexec/poudriere/cached/cached.c
-index 68ba6e6..ce266cd 100644
---- src/libexec/poudriere/cached/cached.c
+--- src/libexec/poudriere/cached/cached.c.orig	2017-02-20 23:37:27 UTC
 +++ src/libexec/poudriere/cached/cached.c
 @@ -26,6 +26,7 @@
  
