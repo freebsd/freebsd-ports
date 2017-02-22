15b5ec87a6f76fd4283a5d0214a88692f32a3b2f
bdrewery <bdrewery@FreeBSD.org>
2017-02-22 20:35:50 +0000

    Restore 'umount -f' usage for now as older releases do not support 'umount -n'.
    A better fix will be coming soon.
    
    Reported by:	ohauer

--- ports-mgmt/poudriere-devel/files/patch-src_share_poudriere_include_fs.sh
+++ ports-mgmt/poudriere-devel/files/patch-src_share_poudriere_include_fs.sh
@@ -1,15 +1,20 @@
---- src/share/poudriere/include/fs.sh.orig	2016-05-18 22:58:28 UTC
+--- src/share/poudriere/include/fs.sh.orig	2017-02-22 20:34:08 UTC
 +++ src/share/poudriere/include/fs.sh
-@@ -191,10 +191,10 @@ destroyfs() {
- 		zfs destroy -rf ${fs}
- 		rmdir ${mnt}
- 	else
--		rm -rfx ${mnt} 2>/dev/null || :
-+		rm -rf ${mnt} 2>/dev/null || :
- 		if [ -d "${mnt}" ]; then
- 			chflags -R 0 ${mnt}
--			rm -rfx ${mnt}
-+			rm -rf ${mnt}
- 		fi
+@@ -98,7 +98,7 @@ umountfs() {
+ 
+ 	[ -d "${mnt}" ] || return 0
+ 	mnt=$(realpath ${mnt})
+-	if ! findmounts "${mnt}" "${pattern}" | xargs umount -n; then
++	if ! findmounts "${mnt}" "${pattern}" | xargs umount -f; then
+ 		findmounts "${mnt}" "${pattern}" | xargs umount -fv || :
  	fi
- }
+ 
+@@ -198,7 +198,7 @@ destroyfs() {
+ 	umountfs ${mnt} 1
+ 	if [ ${TMPFS_ALL} -eq 1 ]; then
+ 		if [ -d "${mnt}" ]; then
+-			if ! umount -n "${mnt}" 2>/dev/null; then
++			if ! umount -f "${mnt}" 2>/dev/null; then
+ 				umount -f "${mnt}" 2>/dev/null || :
+ 			fi
+ 		fi
