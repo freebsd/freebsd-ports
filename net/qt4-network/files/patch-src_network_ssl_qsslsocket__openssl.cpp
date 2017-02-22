806a8a8d9f591e514bb34ebeb15a14232d7c63ac
rezny <rezny@FreeBSD.org>
2017-02-22 19:38:31 +0000

    Adjust SSL patches to match behavior of Qt5 in regards to SSL/TLS versions.
    PR:		216781
    Approved by:	swills (mentor)
    Differential Revision:	https://reviews.freebsd.org/D9727

--- net/qt4-network/files/patch-src_network_ssl_qsslsocket__openssl.cpp
+++ net/qt4-network/files/patch-src_network_ssl_qsslsocket__openssl.cpp
@@ -1,21 +1,31 @@
+* Make availability of SSLv3 in Qt4 same as in Qt5, i.e. not part of SecureProtocols
+*
 --- src/network/ssl/qsslsocket_openssl.cpp.orig	2015-05-07 14:14:44 UTC
 +++ src/network/ssl/qsslsocket_openssl.cpp
-@@ -267,15 +267,14 @@ init_context:
+@@ -267,9 +267,13 @@ init_context:
  #endif
          break;
      case QSsl::SslV3:
--        ctx = q_SSL_CTX_new(client ? q_SSLv3_client_method() : q_SSLv3_server_method());
++#ifndef OPENSSL_NO_SSL3_METHOD
+         ctx = q_SSL_CTX_new(client ? q_SSLv3_client_method() : q_SSLv3_server_method());
++#else
 +        ctx = 0; // SSL 3 not supported by the system, but chosen deliberately -> error
++#endif
          break;
 -    case QSsl::SecureProtocols: // SslV2 will be disabled below
--    case QSsl::TlsV1SslV3: // SslV2 will be disabled below
++    case QSsl::SecureProtocols: // SslV2/3 will be disabled below
+     case QSsl::TlsV1SslV3: // SslV2 will be disabled below
      case QSsl::AnyProtocol:
--    default:
-         ctx = q_SSL_CTX_new(client ? q_SSLv23_client_method() : q_SSLv23_server_method());
-         break;
-     case QSsl::TlsV1:
-+    case QSsl::SecureProtocols:
-+    default:
-         ctx = q_SSL_CTX_new(client ? q_TLSv1_client_method() : q_TLSv1_server_method());
-         break;
-     }
+     default:
+@@ -297,8 +301,10 @@ init_context:
+ 
+     // Enable bug workarounds.
+     long options;
+-    if (configuration.protocol == QSsl::TlsV1SslV3 || configuration.protocol == QSsl::SecureProtocols)
++    if (configuration.protocol == QSsl::TlsV1SslV3)
+         options = SSL_OP_ALL|SSL_OP_NO_SSLv2;
++    else if (configuration.protocol == QSsl::SecureProtocols)
++        options = SSL_OP_ALL|SSL_OP_NO_SSLv2|SSL_OP_NO_SSLv3;
+     else
+         options = SSL_OP_ALL;
+ 
