bea23107275636ffa22e3cfe8facc9877d558401
nobutaka <nobutaka@FreeBSD.org>
2017-02-24 15:00:21 +0000

    Update to 3.8.0.

--- x11/mlterm/files/patch-inputmethod_ibus_im__ibus.c
+++ x11/mlterm/files/patch-inputmethod_ibus_im__ibus.c
@@ -1,11 +1,11 @@
---- inputmethod/ibus/im_ibus.c.orig	2016-03-13 10:57:40 UTC
+--- inputmethod/ibus/im_ibus.c.orig	2017-01-07 22:24:38 UTC
 +++ inputmethod/ibus/im_ibus.c
-@@ -1289,7 +1289,7 @@ im_ibus_get_info(
- 	}
+@@ -1008,7 +1008,7 @@ im_info_t* im_ibus_get_info(char* locale
+   }
  
- 	result->id = strdup( "ibus") ;
--	result->name = strdup( "iBus") ;
-+	result->name = strdup( "ibus") ;
- 	result->num_of_args = 0;
- 	result->args = NULL ;
- 	result->readable_args = NULL ;
+   result->id = strdup("ibus");
+-  result->name = strdup("iBus");
++  result->name = strdup("ibus");
+   result->num_of_args = 0;
+   result->args = NULL;
+   result->readable_args = NULL;
