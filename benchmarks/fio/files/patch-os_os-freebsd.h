7b0649e44d677c6502c373167248fb718f27fe10
bapt <bapt@FreeBSD.org>
2017-02-25 15:30:08 +0000

    Update to 2.18

--- benchmarks/fio/files/patch-os_os-freebsd.h
+++ benchmarks/fio/files/patch-os_os-freebsd.h
@@ -1,9 +1,10 @@
---- os/os-freebsd.h.orig	2016-10-25 18:38:13 UTC
+--- os/os-freebsd.h.orig	2017-02-23 15:44:32 UTC
 +++ os/os-freebsd.h
-@@ -22,6 +22,9 @@
- #define FIO_HAVE_TRIM
- #define FIO_HAVE_GETTID
+@@ -24,7 +24,9 @@
  #define FIO_HAVE_CPU_AFFINITY
+ /* Only have attach-to-open-removed when kern.ipc.shm_allow_removed is 1 */
+ #undef  FIO_HAVE_SHM_ATTACH_REMOVED
+-
 +#if _POSIX_THREAD_PROCESS_SHARED > 0
 +#define FIO_HAVE_PSHARED_MUTEX
 +#endif
