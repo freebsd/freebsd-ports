dffd841d48514782b9915dd83f9498efa1a2edb2
jbeich <jbeich@FreeBSD.org>
2017-03-03 23:44:26 +0000

    gecko: drop unused OAuth keys
    https://hg.mozilla.org/mozilla-central/rev/6a49d0290fab
    https://hg.mozilla.org/mozilla-central/rev/aa14d58c7141

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -274,9 +274,7 @@ MOZ_OPTIONS+=	--with-system-zlib		\
 # http://www.chromium.org/developers/how-tos/api-keys
 # Note: these are for FreeBSD use ONLY. For your own distribution,
 # please get your own set of keys.
-MOZ_EXPORT+=	MOZ_GOOGLE_API_KEY=AIzaSyBsp9n41JLW8jCokwn7vhoaMejDFRd1mp8 \
-				MOZ_GOOGLE_OAUTH_API_CLIENTID=996322985003.apps.googleusercontent.com \
-				MOZ_GOOGLE_OAUTH_API_KEY=IR1za9-1VK0zZ0f_O8MVFicn
+MOZ_EXPORT+=	MOZ_GOOGLE_API_KEY=AIzaSyBsp9n41JLW8jCokwn7vhoaMejDFRd1mp8
 
 .if ${PORT_OPTIONS:MGTK3}
 MOZ_TOOLKIT=	cairo-gtk3
