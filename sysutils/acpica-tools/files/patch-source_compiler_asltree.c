d9a86bee90fea6c8d0834146f48f34818c11ac17
jkim <jkim@FreeBSD.org>
2017-03-03 18:28:00 +0000

    Update to 20170303.

--- sysutils/acpica-tools/files/patch-source_compiler_asltree.c
+++ sysutils/acpica-tools/files/patch-source_compiler_asltree.c
@@ -1,11 +1,27 @@
---- source/compiler/asltree.c.orig	2017-02-25 00:43:54 UTC
+--- source/compiler/asltree.c.orig	2017-03-03 17:25:50 UTC
 +++ source/compiler/asltree.c
-@@ -187,7 +187,7 @@ TrAllocateNode (
+@@ -295,17 +295,14 @@ TrAllocateNode (
          }
  
          Gbl_CommentState.Latest_Parse_Node = Op;
 -        if (Gbl_CommentState.Latest_Parse_Node->Asl.ParseOpName)
-+        if (Gbl_CommentState.Latest_Parse_Node->Asl.ParseOpName[0])
-         {
-             CvDbgPrint ("trallocatenode=Set latest parse node to this node.\n");
-             CvDbgPrint ("           Op->Asl.ParseOpName = %s\n",
+-        {
+-            CvDbgPrint ("trallocatenode=Set latest parse node to this node.\n");
+-            CvDbgPrint ("           Op->Asl.ParseOpName = %s\n",
+-                Gbl_CommentState.Latest_Parse_Node->Asl.ParseOpName);
+-            CvDbgPrint ("           Op->Asl.ParseOpcode = 0x%x\n", ParseOpcode);
++        CvDbgPrint ("trallocatenode=Set latest parse node to this node.\n");
++        CvDbgPrint ("           Op->Asl.ParseOpName = %s\n",
++            Gbl_CommentState.Latest_Parse_Node->Asl.ParseOpName);
++        CvDbgPrint ("           Op->Asl.ParseOpcode = 0x%x\n", ParseOpcode);
+ 
+-            if (Op->Asl.FileChanged)
+-            {
+-                CvDbgPrint("    file has been changed!\n");
+-            }
++        if (Op->Asl.FileChanged)
++        {
++            CvDbgPrint("    file has been changed!\n");
+         }
+ 
+         /*
