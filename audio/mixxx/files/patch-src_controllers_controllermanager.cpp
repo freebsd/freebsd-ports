e9f54497e1d980f7bf80dfd2f05c374bd81e0e36
riggs <riggs@FreeBSD.org>
2017-03-05 16:27:12 +0000

    Update to upstream release 2.0.0
    PR:		215814
    Submitted by:	joe@thrallingpenguin.com
    Reviewed by:	riggs

--- audio/mixxx/files/patch-src_controllers_controllermanager.cpp
+++ audio/mixxx/files/patch-src_controllers_controllermanager.cpp
@@ -1,18 +1,7 @@
---- src/controllers/controllermanager.cpp.orig	2013-05-08 23:20:26 UTC
+--- src/controllers/controllermanager.cpp.orig	2015-12-29 16:10:41 UTC
 +++ src/controllers/controllermanager.cpp
-@@ -12,7 +12,10 @@
- #include "controllers/defs_controllers.h"
- #include "controllers/controllerlearningeventfilter.h"
- 
-+#ifdef __PORTMIDI__
- #include "controllers/midi/portmidienumerator.h"
-+#endif
-+
- #ifdef __HSS1394__
-     #include "controllers/midi/hss1394enumerator.h"
- #endif
-@@ -78,7 +81,9 @@ ControllerManager::ControllerManager(Con
-     m_pPresetInfoManager = new PresetInfoEnumerator(m_pConfig);
+@@ -83,7 +83,9 @@ ControllerManager::ControllerManager(Con
+     m_pMainThreadPresetEnumerator = new PresetInfoEnumerator(presetSearchPaths);
  
      // Instantiate all enumerators
 +#ifdef __PORTMIDI__
