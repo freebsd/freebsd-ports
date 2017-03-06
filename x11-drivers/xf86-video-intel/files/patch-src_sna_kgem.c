2302d8986f0720b0d1896eb55c5496464d9d3cf4
rezny <rezny@FreeBSD.org>
2017-03-06 02:12:22 +0000

    Update to 20170228 snapshot, which ceases loading i915.ko after i915kms.ko Add an option for default AccelMethod, choice of UXA (default) or SNA (new)
    Approved by:	swills (mentor)
    Differential Revision:	https://reviews.freebsd.org/D9862

--- x11-drivers/xf86-video-intel/files/patch-src_sna_kgem.c
+++ x11-drivers/xf86-video-intel/files/patch-src_sna_kgem.c
@@ -1,12 +1,12 @@
---- src/sna/kgem.c.orig	2014-12-09 17:08:21.000000000 +0100
-+++ src/sna/kgem.c	2015-11-19 00:06:27.883010000 +0100
+--- src/sna/kgem.c.orig	2017-02-28 20:52:19 UTC
++++ src/sna/kgem.c
 @@ -29,6 +29,9 @@
  #include "config.h"
  #endif
-
+ 
 +#define _WITH_GETLINE	/* to expose getline() in stdio.h on FreeBSD */
 +#include <stdio.h>	/* for getline() */
 +
  #include "sna.h"
  #include "sna_reg.h"
-
+ 
