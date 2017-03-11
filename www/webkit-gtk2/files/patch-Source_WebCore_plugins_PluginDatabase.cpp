32bfee73fc07ef2e1194de4f9ec8c19170c2785d
rezny <rezny@FreeBSD.org>
2017-03-11 21:15:03 +0000

    - Fix building on PPC/PPC64 [1] - Fix building on ARMv6 [2] - Add missing indirect dependencies - Possibly fix build on sparc64 (unconfirmed)
    PR:	212903 [1]
    Submitted by:	jhibbits [1], strejda [2]
    Approved by:	swills (mentor)
    Differential Revision:	https://reviews.freebsd.org/D9924

--- www/webkit-gtk2/files/patch-Source_WebCore_plugins_PluginDatabase.cpp
+++ www/webkit-gtk2/files/patch-Source_WebCore_plugins_PluginDatabase.cpp
@@ -1,6 +1,6 @@
---- Source/WebCore/plugins/PluginDatabase.cpp.orig	2011-01-11 16:24:29.000000000 +0000
-+++ Source/WebCore/plugins/PluginDatabase.cpp	2011-01-11 16:24:54.000000000 +0000
-@@ -375,6 +375,7 @@ Vector<String> PluginDatabase::defaultPl
+--- Source/WebCore/plugins/PluginDatabase.cpp.orig	2016-04-10 06:48:37 UTC
++++ Source/WebCore/plugins/PluginDatabase.cpp
+@@ -394,6 +394,7 @@ Vector<String> PluginDatabase::defaultPl
      userPluginPath.append(String("/.netscape/plugins"));
      paths.append(userPluginPath);
  
