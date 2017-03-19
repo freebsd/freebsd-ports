fc880c7a16941546a0d6c36e05ef7625b9b44fb5
wen <wen@FreeBSD.org>
2017-03-19 00:41:08 +0000

    - Remove dependency on math/openblas if other blas lib is selected
    PR:		215764
    Submitted by:	riggs@

--- math/py-numpy/files/patch-numpy-distutils-system_info.py
+++ math/py-numpy/files/patch-numpy-distutils-system_info.py
@@ -1,6 +1,6 @@
---- numpy/distutils/system_info.py.orig	2013-02-09 22:04:56.000000000 +0100
-+++ numpy/distutils/system_info.py	2013-03-22 21:01:34.000000000 +0100
-@@ -995,8 +995,8 @@
+--- numpy/distutils/system_info.py.orig	2016-10-03 23:42:24 UTC
++++ numpy/distutils/system_info.py
+@@ -1038,8 +1038,8 @@ class atlas_info(system_info):
      dir_env_var = 'ATLAS'
      _lib_names = ['f77blas', 'cblas']
      if sys.platform[:7] == 'freebsd':
@@ -11,3 +11,15 @@
      else:
          _lib_atlas = ['atlas']
          _lib_lapack = ['lapack']
+@@ -1574,11 +1574,6 @@ class blas_opt_info(system_info):
+             self.set_info(**blas_mkl_info)
+             return
+ 
+-        openblas_info = get_info('openblas')
+-        if openblas_info:
+-            self.set_info(**openblas_info)
+-            return
+-
+         atlas_info = get_info('atlas_3_10_blas_threads')
+         if not atlas_info:
+             atlas_info = get_info('atlas_3_10_blas')
