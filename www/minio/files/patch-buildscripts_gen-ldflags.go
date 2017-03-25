0b7fcd6605c5a6e09c28fccd6280354b0d0090cf
Steve Wills <swills@FreeBSD.org>
2017-03-25 13:48:21 +0000

    www/minio: update to RELEASE.2017-03-16T21-50-32Z
    PR:		217857
    Submitted by:	jhixson@gmail.com

--- www/minio/files/patch-buildscripts_gen-ldflags.go
+++ www/minio/files/patch-buildscripts_gen-ldflags.go
@@ -1,5 +1,5 @@
---- buildscripts/gen-ldflags.go.orig	2017-02-15 20:51:19.000000000 -0500
-+++ buildscripts/gen-ldflags.go	2017-02-22 14:17:45.209088000 -0500
+--- buildscripts/gen-ldflags.go.orig	2017-02-22 14:17:45.000000000 -0500
++++ buildscripts/gen-ldflags.go	2017-03-16 16:57:45.209088000 -0500
 @@ -23,15 +23,14 @@ import (
  	"os"
  	"os/exec"
@@ -13,10 +13,10 @@
 -	ldflagsStr += " -X github.com/minio/minio/cmd.ReleaseTag=" + releaseTag(version)
 -	ldflagsStr += " -X github.com/minio/minio/cmd.CommitID=" + commitID()
 -	ldflagsStr += " -X github.com/minio/minio/cmd.ShortCommitID=" + commitID()[:12]
-+	ldflagsStr = "-X github.com/minio/minio/cmd.Version=2017-02-16T01:47:30Z"
-+	ldflagsStr += " -X github.com/minio/minio/cmd.ReleaseTag=RELEASE.2017-02-16T01-47-30Z"
-+        ldflagsStr += " -X github.com/minio/minio/cmd.CommitID=3d98311d9f4ceb78dba996dcdc0751253241e697"
-+	ldflagsStr += " -X github.com/minio/minio/cmd.ShortCommitID=83abb310b4ce"
++	ldflagsStr = "-X github.com/minio/minio/cmd.Version=2017-03-16T21:50:32Z"
++	ldflagsStr += " -X github.com/minio/minio/cmd.ReleaseTag=RELEASE.2017-03-16T21-50-32Z"
++        ldflagsStr += " -X github.com/minio/minio/cmd.CommitID=78dbb5bf141b4ff8003b7e5ef9b665a147c465bc"
++	ldflagsStr += " -X github.com/minio/minio/cmd.ShortCommitID=78dbb5bf141b4"
  	ldflagsStr += " -X github.com/minio/minio/cmd.GOPATH=" + os.Getenv("GOPATH")
  	return ldflagsStr
  }
@@ -25,5 +25,5 @@
  
  func main() {
 -	fmt.Println(genLDFlags(time.Now().UTC().Format(time.RFC3339)))
-+	fmt.Println(genLDFlags("2017-02-16T01-47-30Z"))
++	fmt.Println(genLDFlags("2017-03-16T21-50-32Z"))
  }
