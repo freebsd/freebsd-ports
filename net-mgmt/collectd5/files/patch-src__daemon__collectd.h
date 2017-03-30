aca8bc2c558e95557e663aa8e326a94807757c67
John Hixson <root@pcbsd.divinix.org>
2017-03-29 20:12:53 -0700

    Update collectd to 5.7.1 - correctly
    Ticket: #22788

--- net-mgmt/collectd5/files/patch-src__daemon__collectd.h
+++ net-mgmt/collectd5/files/patch-src__daemon__collectd.h
@@ -1,21 +1,21 @@
---- src/daemon/collectd.h.orig	2016-12-05 23:16:57.702128468 -0500
-+++ src/daemon/collectd.h	2016-12-05 23:18:17.932123583 -0500
-@@ -243,15 +243,15 @@
+--- src/daemon/collectd.h.orig	2017-01-23 07:53:57 UTC
++++ src/daemon/collectd.h
+@@ -243,15 +243,15 @@ typedef int _Bool;
  #endif
  
  #ifndef LOCALSTATEDIR
--# define LOCALSTATEDIR PREFIX "/var"
-+# define LOCALSTATEDIR "/var"
+-#define LOCALSTATEDIR PREFIX "/var"
++#define LOCALSTATEDIR "/var"
  #endif
  
  #ifndef PKGLOCALSTATEDIR
--# define PKGLOCALSTATEDIR PREFIX "/var/lib/" PACKAGE_NAME
-+# define PKGLOCALSTATEDIR PREFIX "/var/db/" PACKAGE_NAME
+-#define PKGLOCALSTATEDIR PREFIX "/var/lib/" PACKAGE_NAME
++#define PKGLOCALSTATEDIR "/var/db/" PACKAGE_NAME
  #endif
  
  #ifndef PIDFILE
--# define PIDFILE PREFIX "/var/run/" PACKAGE_NAME ".pid"
-+# define PIDFILE "/var/run/" PACKAGE_NAME ".pid"
+-#define PIDFILE PREFIX "/var/run/" PACKAGE_NAME ".pid"
++#define PIDFILE "/var/run/" PACKAGE_NAME ".pid"
  #endif
  
  #ifndef PLUGINDIR
--- net-mgmt/collectd5/files/patch-version-gen.sh
+++ net-mgmt/collectd5/files/patch-version-gen.sh
@@ -1,17 +1,13 @@
---- version-gen.sh.orig	2016-09-11 08:10:25 UTC
+--- version-gen.sh.orig	2017-02-03 20:46:49 UTC
 +++ version-gen.sh
-@@ -1,13 +1,2 @@
- #!/bin/sh
--
--DEFAULT_VERSION="5.6.0.git"
--
+@@ -2,10 +2,6 @@
+ 
+ DEFAULT_VERSION="5.7.1.git"
+ 
 -if [ -d .git ]; then
 -	VERSION="`git describe --dirty=+ --abbrev=7 2> /dev/null | grep collectd | sed -e 's/^collectd-//' -e 's/-/./g'`"
 -fi
 -
--if test -z "$VERSION"; then
--	VERSION="$DEFAULT_VERSION"
--fi
--
--printf "%s" "$VERSION"
-+echo -n '5.6.0.git'
+ if test -z "$VERSION"; then
+ 	VERSION="$DEFAULT_VERSION"
+ fi
