90640d364d397c71cd47bd6413144b3221868830
tz <tz@FreeBSD.org>
2017-03-30 14:20:59 +0000

    www/gitlab: Update from 8.15.8 to 8.16.8
    Changelog: https://github.com/gitlabhq/gitlabhq/blob/v8.16.8/CHANGELOG.md

--- www/gitlab/files/patch-config_initializers_1__settings.rb
+++ www/gitlab/files/patch-config_initializers_1__settings.rb
@@ -1,4 +1,4 @@
---- config/initializers/1_settings.rb.orig	2017-03-19 14:19:38 UTC
+--- config/initializers/1_settings.rb.orig	2017-03-19 20:06:47 UTC
 +++ config/initializers/1_settings.rb
 @@ -190,12 +190,8 @@ Settings.gitlab['email_subject_suffix'] 
  Settings.gitlab['base_url']   ||= Settings.send(:build_base_gitlab_url)
