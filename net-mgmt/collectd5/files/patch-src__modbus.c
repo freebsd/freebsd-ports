ddca0b735ab19229dc36c972020ac4b487afb4c5
Charlie Root <root@pcbsd.divinix.org>
2017-03-29 20:05:21 -0700

    Update collectd to 5.7.1
    Ticket: #22788

--- net-mgmt/collectd5/files/patch-src__modbus.c
+++ net-mgmt/collectd5/files/patch-src__modbus.c
@@ -1,12 +1,12 @@
---- src/modbus.c.orig	2016-01-22 09:51:17 UTC
+--- src/modbus.c.orig	2017-01-23 07:53:57 UTC
 +++ src/modbus.c
-@@ -25,6 +25,9 @@
- #include "plugin.h"
+@@ -26,6 +26,9 @@
  #include "configfile.h"
+ #include "plugin.h"
  
 +/* FreeBSD needs this */
 +#include <sys/socket.h> 
 +
- #include <netdb.h>
- 
  #include <modbus.h>
+ #include <netdb.h>
+ #include <sys/socket.h>
