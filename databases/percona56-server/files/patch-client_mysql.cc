920b723a87c30ba9bae3e0bd19ff336e2866b06c
bapt <bapt@FreeBSD.org>
2017-04-01 15:11:14 +0000

    Update libedit to 20170329

--- databases/percona56-server/files/patch-client_mysql.cc
+++ databases/percona56-server/files/patch-client_mysql.cc
@@ -1,23 +1,9 @@
---- client/mysql.cc.orig	2017-04-01 14:33:18 UTC
+--- client/mysql.cc.orig	2016-09-30 11:41:40 UTC
 +++ client/mysql.cc
-@@ -1903,11 +1903,11 @@ static void usage(int version)
- #endif
- 
- #ifdef HAVE_READLINE
--  printf("%s  Ver %s Distrib %s, for %s (%s) using %s %s\n",
-+  printf("%s  Ver %s Distrib %s Percona, for %s (%s) using %s %s\n",
- 	 my_progname, VER, MYSQL_SERVER_VERSION, SYSTEM_TYPE, MACHINE_TYPE,
-          readline, rl_library_version);
- #else
--  printf("%s  Ver %s Distrib %s, for %s (%s)\n", my_progname, VER,
-+  printf("%s  Ver %s Distrib %s Percona, for %s (%s)\n", my_progname, VER,
- 	MYSQL_SERVER_VERSION, SYSTEM_TYPE, MACHINE_TYPE);
- #endif
- 
-@@ -2865,9 +2865,11 @@ static void initialize_readline (char *name)
-   rl_add_defun("magic-space", (rl_command_func_t *)&fake_magic_space, -1);
- #elif defined(USE_LIBEDIT_INTERFACE)
+@@ -2738,9 +2738,11 @@ static void initialize_readline (char *n
+ #ifdef HAVE_LOCALE_H
    setlocale(LC_ALL,""); /* so as libedit use isprint */
+ #endif
 -  rl_attempted_completion_function= (CPPFunction*)&new_mysql_completion;
 -  rl_completion_entry_function= &no_completion;
 +  rl_attempted_completion_function= (rl_completion_func_t*)&new_mysql_completion;
@@ -28,7 +14,7 @@
  #else
    rl_attempted_completion_function= (CPPFunction*)&new_mysql_completion;
    rl_completion_entry_function= &no_completion;
-@@ -2886,7 +2888,7 @@ static char **new_mysql_completion(const char *text,
+@@ -2759,7 +2761,7 @@ static char **new_mysql_completion(const
                                     int end MY_ATTRIBUTE((unused)))
  {
    if (!status.batch && !quick)
