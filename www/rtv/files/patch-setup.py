167b9d26e7640f0be6d988f93855bdfa11470fcd
miwi <miwi@FreeBSD.org>
2017-04-02 14:37:41 +0000

    - Update to 1.15.0
    PR:		218279
    Submitted by:	maintainer

--- www/rtv/files/patch-setup.py
+++ www/rtv/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2017-01-13 02:49:35 UTC
+--- setup.py.orig	2017-03-31 04:04:10 UTC
 +++ setup.py
-@@ -16,7 +16,7 @@ setuptools.setup(
-     keywords='reddit terminal praw curses',
-     packages=['rtv'],
-     package_data={'rtv': ['templates/*']},
+@@ -22,7 +22,7 @@ setuptools.setup(
+         'rtv': ['templates/*'],
+         'rtv.packages.praw': ['praw.ini'],
+         },
 -    data_files=[("share/man/man1", ["rtv.1"])],
 +    data_files=[("man/man1", ["rtv.1"])],
      install_requires=[
-         'praw >=3.5, <4',
-         'six',
+         'beautifulsoup4',
+         'decorator',
