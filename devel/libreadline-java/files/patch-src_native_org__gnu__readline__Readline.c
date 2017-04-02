6a5e811ea607a9c85daae00a8ad95fff4a4b80af
Baptiste Daroussin <bapt@FreeBSD.org>
2017-04-02 09:54:38 +0000

    Fix build with newer libedit which has better compat with readline

--- devel/libreadline-java/files/patch-src_native_org__gnu__readline__Readline.c
+++ devel/libreadline-java/files/patch-src_native_org__gnu__readline__Readline.c
@@ -1,6 +1,6 @@
---- src/native/org_gnu_readline_Readline.c.orig	2003-01-07 10:14:35 UTC
+--- src/native/org_gnu_readline_Readline.c.orig	2017-04-02 09:52:09 UTC
 +++ src/native/org_gnu_readline_Readline.c
-@@ -430,7 +430,7 @@ const char *java_completer(char *text, i
+@@ -430,7 +430,7 @@ const char *java_completer(char *text, int state) {
    jtext = (*jniEnv)->NewStringUTF(jniEnv,text);
  
    if (jniMethodId == 0) {
@@ -9,10 +9,26 @@
    }
  
    completion = (*jniEnv)->CallObjectMethod(jniEnv, jniObject,
-@@ -560,6 +560,21 @@ JNIEXPORT void JNICALL 
- #endif
- 
- /* -------------------------------------------------------------------------- */
+@@ -472,11 +472,7 @@ JNIEXPORT void JNICALL Java_org_gnu_readline_Readline_
+       rl_completion_entry_function = NULL;
+       return;
+     }
+-#ifdef JavaEditline
+-    rl_completion_entry_function = (CPFunction *) java_completer;
+-#else
+     rl_completion_entry_function = (rl_compentry_func_t *) java_completer;
+-#endif
+   }
+   else {
+     rl_completion_entry_function = NULL;
+@@ -556,6 +552,21 @@ JNIEXPORT void JNICALL 
+     return;    
+   }
+   rl_completer_word_break_characters = word_break_buffer;
++}
++#endif
++
++/* -------------------------------------------------------------------------- */
 +/* Sets/gets rl_completion_append_character                                   */
 +/* -------------------------------------------------------------------------- */
 +
@@ -24,10 +40,6 @@
 +
 +	rl_completion_append_character = (int)appendCharacter;
 +	return (jchar)previous_setting;
-+}
-+#endif
-+
-+/* -------------------------------------------------------------------------- */
- /* Convert utf8-string to ucs1-string                   .                     */
- /* -------------------------------------------------------------------------- */
+ }
+ #endif
  
