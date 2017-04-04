c566a59a5b0e811c9f82e1af1a296120da150b0e
Thomas Zander <riggs@FreeBSD.org>
2017-04-04 17:48:24 +0000

    Update to upstream version 1.0.3
    PR:		218288
    Submitted by:	naito.yuichiro@gmail.com
    Reviewed by:	riggs

---
+++ multimedia/handbrake/files/patch-scripts_repo-info.sh
@@ -0,0 +1,8 @@
+--- scripts/repo-info.sh.orig	2017-02-25 20:56:19 UTC
++++ scripts/repo-info.sh
+@@ -1,4 +1,4 @@
+-#! /bin/bash
++#!/usr/bin/env bash
+ #
+ # Retrieves git repository info for directory ${1} using command ${2}
+ 
