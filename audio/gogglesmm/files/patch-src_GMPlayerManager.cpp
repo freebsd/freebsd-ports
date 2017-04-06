a675c8cda220015218760b0ad0f1808133639ba3
jhale <jhale@FreeBSD.org>
2017-04-06 22:28:08 +0000

    Update to 1.1.7
    Changes:	https://github.com/gogglesmm/gogglesmm/compare/1.1.5...1.1.7

--- audio/gogglesmm/files/patch-src_GMPlayerManager.cpp
+++ audio/gogglesmm/files/patch-src_GMPlayerManager.cpp
@@ -1,9 +1,18 @@
 Re-enable tray icon for KDE4 until KDE5 is in the ports tree. Mpris plugin
 works, but tray icon might be wanted to minimize app in the time being.
 
---- src/GMPlayerManager.cpp.orig	2016-10-22 17:13:46 UTC
+--- src/GMPlayerManager.cpp.orig	2017-02-08 01:48:39 UTC
 +++ src/GMPlayerManager.cpp
-@@ -741,7 +741,7 @@ FXint GMPlayerManager::run(int& argc,cha
+@@ -294,7 +294,7 @@ GMPlayerManager::~GMPlayerManager() {
+ 
+   myself=nullptr;
+ 
+-#ifdef HAVE_SSSION
++#ifdef HAVE_SESSION
+   delete session;
+ #endif
+   delete application;
+@@ -747,7 +747,7 @@ FXint GMPlayerManager::run(int& argc,cha
      // KDE5 comes with mpris plugin on the toolbar, no need for
      // tray icon
      if (gm_desktop_session()==DESKTOP_SESSION_KDE_PLASMA) {
