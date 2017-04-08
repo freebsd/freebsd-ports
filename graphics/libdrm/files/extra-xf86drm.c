b29ddf3ae375eb1da69b8c1a0f9fe4f7581318a7
Matthew Rezny <rezny@FreeBSD.org>
2017-04-08 15:49:25 +0000

    Update to 2.4.78 and remove the dependency on libdevq
    The functionality previously provided by libdevq is now implemented directly
    in libdrm. This direct implementation should be more robust than the prior
    integration. Initial testing has shown improvements in some scenarios while
    remaining on-par elsewhere. The ultimate goal of this effort is to have our
    platform support integrated upstream, which will be easier without the extra
    dependency. With that in mind, please quickly report any regressions found.
    
    Reviewed by:	dumbbell (x11), bapt (x11)
    Approved by:	swills (mentor), dumbbell (x11)
    Differential Revision:	https://reviews.freebsd.org/D10184

--- graphics/libdrm/files/extra-xf86drm.c
+++ graphics/libdrm/files/extra-xf86drm.c
@@ -4,7 +4,7 @@ for now. Newer kernel modules will have this fixed included.
 
 dragonfly fixed this issue in,
 http://gitweb.dragonflybsd.org/dragonfly.git/commit/b922632f623ee2cc2c1346bb3a6894a7756676aa
-which will be included in the 4.4 release when it is released.
+which has been included since the 4.4 release.
 
 --- xf86drm.c.orig	2017-01-30 13:59:15.919081000 +0100
 +++ xf86drm.c
