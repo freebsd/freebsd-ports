eccd7e0720929161ada13ee7d3a003eb8ecdf4b9
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2017-04-08 22:23:19 +0000

    databases/freetds: Fix build with libressl
    PR:		217549
    Submitted by:	w.schwarzenfeld@utanet.at

--- databases/freetds/files/patch-src_tds_tls.c
+++ databases/freetds/files/patch-src_tds_tls.c
@@ -11,3 +11,16 @@
  #include <freetds/tds.h>
  #include <freetds/string.h>
  #include <freetds/tls.h>
+@@ -72,6 +76,12 @@
+ #define SSL_PTR ptr
+ #else
+ 
++#ifdef LIBRESSL_VERSION_NUMBER
++static pthread_mutex_t *openssllocks;
++#undef OPENSSL_VERSION_NUMBER
++#define OPENSSL_VERSION_NUMBER 0x1000107fL
++#endif
++
+ /* some compatibility layer */
+ #if OPENSSL_VERSION_NUMBER < 0x1010000FL
+ static inline void
