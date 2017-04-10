e385730f9c4538176f514dac5d02b064e6c4cc14
Dmitry Marakasov <amdmi3@FreeBSD.org>
2017-04-10 15:50:23 +0000

    - Add LICENSE_FILE - Remove always true OSVERSION condition, make patch unconditional
    Approved by:	portmgr blanket

--- sysutils/fusefs-curlftpfs/files/patch-ftpfs.h
+++ sysutils/fusefs-curlftpfs/files/patch-ftpfs.h
@@ -1,11 +1,11 @@
---- ftpfs.h.orig	2008-04-25 19:32:30.000000000 +0900
-+++ ftpfs.h	2012-12-21 18:18:20.000000000 +0900
-@@ -75,7 +75,7 @@
+--- ftpfs.h.orig	2008-04-25 10:32:30 UTC
++++ ftpfs.h
+@@ -75,7 +75,7 @@ extern struct ftpfs ftpfs;
          do { if (level <= ftpfs.debug) {\
                 int i = 0; \
                 while (++i < level) fprintf(stderr, " "); \
 -               fprintf(stderr, "%ld ", time(NULL));\
-+               fprintf(stderr, "%d ", time(NULL));\
++               fprintf(stderr, "%lld ", time(NULL));\
                 fprintf(stderr, __FILE__ ":%d ", __LINE__);\
                 fprintf(stderr, args);\
               }\
