527c1314b26434ecf329afad2e565bfdf66ec071
jbeich <jbeich@FreeBSD.org>
2017-04-11 21:39:47 +0000

    www/firefox: drop broken GTK2 option following upstream
    https://www.mail-archive.com/dev-platform@lists.mozilla.org/msg20523.html
    
    PR:		218569
    Reported by:	Walter Schwarzenfeld, Jakub Lach <jakub_lach@mailplus.pl>
    Submitted by:	rezny (based on)
    MFH:		2017Q2 (r436907 followup)

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -249,7 +249,7 @@ BUILD_DEPENDS+=	${-${dep}_BUILD_DEPENDS}
 
 # Standard options
 MOZ_CHROME?=	omni
-MOZ_TOOLKIT?=	cairo-gtk2
+MOZ_TOOLKIT?=	cairo-gtk3
 MOZ_CHANNEL?=	${PKGNAMESUFFIX:Urelease:S/^-//}
 MOZ_OPTIONS+=	\
 		--enable-chrome-format=${MOZ_CHROME} \
@@ -278,8 +278,8 @@ MOZ_OPTIONS+=	--with-system-zlib		\
 # please get your own set of keys.
 MOZ_EXPORT+=	MOZ_GOOGLE_API_KEY=AIzaSyBsp9n41JLW8jCokwn7vhoaMejDFRd1mp8
 
-.if ${PORT_OPTIONS:MGTK3}
-MOZ_TOOLKIT=	cairo-gtk3
+.if ${PORT_OPTIONS:MGTK2}
+MOZ_TOOLKIT=	cairo-gtk2
 .endif
 
 .if ${MOZ_TOOLKIT:Mcairo-gtk3}
