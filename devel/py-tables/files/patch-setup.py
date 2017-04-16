9bf0020439a63223c420610174e03fc6f457bd07
wen <wen@FreeBSD.org>
2017-04-16 01:07:46 +0000

    - Fix typo in patch file

--- devel/py-tables/files/patch-setup.py
+++ devel/py-tables/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2017-04-16 08:22:17 UTC
+--- setup.py.orig	2017-04-12 12:02:34 UTC
 +++ setup.py
 @@ -115,8 +115,8 @@ with open('VERSION') as fd:
  debug = '--debug' in sys.argv
@@ -6,7 +6,7 @@
  # Global variables
 -lib_dirs = []
 -inc_dirs = [os.path.join('hdf5-blosc', 'src')]
-+lib_dirs = ['%%LOCALBASE/lib']
++lib_dirs = ['%%LOCALBASE%%/lib']
 +inc_dirs = [os.path.join('hdf5-blosc', 'src'),'%%LOCALBASE%%/include']
  optional_libs = []
  data_files = []    # list of data files to add to packages (mainly for DLL's)
