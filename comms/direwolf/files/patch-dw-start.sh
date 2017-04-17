f81323edcc86cdc521bd876f8ced434f6b93096b
db <db@FreeBSD.org>
2017-04-17 12:58:33 +0000

    MFH: r438662
    This got lost due to confusion over who was handling what.
    
    > > Attached is a revised patch, s/STAGEDIR/DESTDIR/ and the dw-start.sh
    > > script was getting installed with the wrong permissions because I
    > > overlooked the fact it's installed in the examples/ with a bunch of
    > > datafiles.
    
    PR:		ports/436814
    Submitted by:	Kyle Evans <bsdports@kyle-evans.net>
    Reported by:	danfe
    
    Approved by:	ports-secteam (junovitch)

--- comms/direwolf/files/patch-dw-start.sh
+++ comms/direwolf/files/patch-dw-start.sh
@@ -5,20 +5,20 @@
  
  
 -if [ -x /usr/bin/lxterminal ]
-+if [ -x /usr/local/bin/lxterminal ]
++if [ -x %%PREFIX%%/bin/lxterminal ]
  then
 -  /usr/bin/lxterminal -t "Dire Wolf" -e "$DWCMD" &
 -elif [ -x /usr/bin/xterm ] 
-+  /usr/local/bin/lxterminal -t "Dire Wolf" -e "$DWCMD" &
-+elif [ -x /usr/local/bin/xterm ] 
++  %%PREFIX%%/bin/lxterminal -t "Dire Wolf" -e "$DWCMD" &
++elif [ -x %%PREFIX%%/bin/xterm ] 
  then
 -  /usr/bin/xterm -bg white -fg black -e "$DWCMD" &
 -elif [ -x /usr/bin/x-terminal-emulator ]
-+  /usr/local/bin/xterm -bg white -fg black -e "$DWCMD" &
-+elif [ -x /usr/local/bin/x-terminal-emulator ]
++  %%PREFIX%%/bin/xterm -bg white -fg black -e "$DWCMD" &
++elif [ -x %%PREFIX%%/bin/x-terminal-emulator ]
  then
 -  /usr/bin/x-terminal-emulator -e "$DWCMD" &
-+  /usr/local/bin/x-terminal-emulator -e "$DWCMD" &
++  %%PREFIX%%/bin/x-terminal-emulator -e "$DWCMD" &
  else
    echo "Did not find an X terminal emulator."
  fi
