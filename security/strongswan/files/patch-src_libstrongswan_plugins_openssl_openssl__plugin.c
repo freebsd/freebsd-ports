dcce12d9c6511babdebc56ce6d4a3fa67f35f5df
Bernard Spil <bernard.spil@hardenedbsd.org>
2017-04-20 21:33:07 +0200

    HBSD: security/strongswan: Fix runtime issues with LibreSSL
     - Ugly but effective, set OPENSSL_VERSION_NUMBER
    
    Signed-off-by: Bernard Spil <bernard.spil@hardenedbsd.org>

--- security/strongswan/files/patch-src_libstrongswan_plugins_openssl_openssl__plugin.c
+++ security/strongswan/files/patch-src_libstrongswan_plugins_openssl_openssl__plugin.c
@@ -1,6 +1,17 @@
 --- src/libstrongswan/plugins/openssl/openssl_plugin.c.orig	2016-10-08 12:17:09 UTC
 +++ src/libstrongswan/plugins/openssl/openssl_plugin.c
-@@ -623,7 +623,7 @@ plugin_t *openssl_plugin_create()
+@@ -20,6 +20,10 @@
+ #include <threading/mutex.h>
+ #include <threading/thread_value.h>
+ 
++#ifdef LIBRESSL_VERSION_NUMBER
++#define OPENSSL_VERSION_NUMBER 0x1000107fL
++#endif
++
+ #include <openssl/err.h>
+ #include <openssl/evp.h>
+ #include <openssl/conf.h>
+@@ -623,7 +627,7 @@ plugin_t *openssl_plugin_create()
  		},
  	);
  
