460a0d8a1cdea35874a355dd20ea1d4ebe305e33
Matthew Rezny <rezny@FreeBSD.org>
2017-04-24 17:43:08 +0000

    Update to 3.3.12
    PR:		218817
    Approved by:	yuri@rawbw.com (maintainer), swills (mentor)
    Differential Revision:	https://reviews.freebsd.org/D10464

--- net-p2p/qbittorrent/files/patch-src_base_utils_string.cpp
+++ net-p2p/qbittorrent/files/patch-src_base_utils_string.cpp
@@ -1,10 +1,8 @@
 # Avoid use of thread local storage on versions prior to which it was supported.
 #
-# Qt::escape is part of QtGui, upstream patch replaces it for webui
-#
---- src/base/utils/string.cpp.orig	2017-03-03 23:20:21 UTC
+--- src/base/utils/string.cpp.orig	2017-04-06 16:53:25 UTC
 +++ src/base/utils/string.cpp
-@@ -37,7 +37,7 @@
+@@ -38,7 +38,7 @@
  #ifdef QBT_USES_QT5
  #include <QCollator>
  #endif
@@ -13,7 +11,7 @@
  #include <QThreadStorage>
  #endif
  
-@@ -146,7 +146,8 @@ bool Utils::String::naturalCompareCaseSe
+@@ -147,7 +147,8 @@ bool Utils::String::naturalCompareCaseSe
  {
      // provide a single `NaturalCompare` instance for easy use
      // https://doc.qt.io/qt-5/threads-reentrancy.html
@@ -23,7 +21,7 @@
      static QThreadStorage<NaturalCompare> nCmp;
      if (!nCmp.hasLocalData()) nCmp.setLocalData(NaturalCompare(true));
      return (nCmp.localData())(left, right);
-@@ -160,7 +161,8 @@ bool Utils::String::naturalCompareCaseIn
+@@ -161,7 +162,8 @@ bool Utils::String::naturalCompareCaseIn
  {
      // provide a single `NaturalCompare` instance for easy use
      // https://doc.qt.io/qt-5/threads-reentrancy.html
@@ -33,28 +31,3 @@
      static QThreadStorage<NaturalCompare> nCmp;
      if (!nCmp.hasLocalData()) nCmp.setLocalData(NaturalCompare(false));
      return (nCmp.localData())(left, right);
-@@ -217,6 +219,23 @@ QString Utils::String::toHtmlEscaped(con
- #ifdef QBT_USES_QT5
-     return str.toHtmlEscaped();
- #else
--    return Qt::escape(str);
-+    // code from Qt
-+    QString rich;
-+    const int len = str.length();
-+    rich.reserve(int(len * 1.1));
-+    for (int i = 0; i < len; ++i) {
-+        if (str.at(i) == QLatin1Char('<'))
-+            rich += QLatin1String("&lt;");
-+        else if (str.at(i) == QLatin1Char('>'))
-+            rich += QLatin1String("&gt;");
-+        else if (str.at(i) == QLatin1Char('&'))
-+            rich += QLatin1String("&amp;");
-+        else if (str.at(i) == QLatin1Char('"'))
-+            rich += QLatin1String("&quot;");
-+        else
-+            rich += str.at(i);
-+    }
-+    rich.squeeze();
-+    return rich;
- #endif
- }
