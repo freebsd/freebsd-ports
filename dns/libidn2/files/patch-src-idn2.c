6f510c76516732b595a41cd94077c20063310f7b
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2017-04-25 14:22:45 +0000

    Update to 2.0.1
    Changes:	https://gitlab.com/libidn/libidn2/blob/master/NEWS
    PR:		218833
    Submitted by:	tijl

--- dns/libidn2/files/patch-src-idn2.c
+++ dns/libidn2/files/patch-src-idn2.c
@@ -1,7 +1,7 @@
---- src/idn2.c.orig	2016-12-26 21:00:11 UTC
+--- src/idn2.c.orig	2017-04-12 18:43:32 UTC
 +++ src/idn2.c
-@@ -31,7 +31,7 @@
- #include <unistr.h>
+@@ -33,7 +33,7 @@
+ #include <unistring/localcharset.h>
  
  /* Gnulib headers. */
 -#include "error.h"
@@ -9,16 +9,16 @@
  #include "gettext.h"
  #define _(String) dgettext (PACKAGE, String)
  #include "progname.h"
-@@ -167,7 +167,7 @@ process_input (char *readbuf, int flags)
+@@ -176,7 +176,7 @@ process_input (char *readbuf, int flags)
        free (output);
      }
    else
--    error (EXIT_FAILURE, 0, "%s: %s",
-+    errc (EXIT_FAILURE, 0, "%s: %s",
- 	   args_info.register_given ? "register" : "lookup",
- 	   idn2_strerror (rc));
+-    error (EXIT_FAILURE, 0, "%s: %s", tag, idn2_strerror (rc));
++    errc (EXIT_FAILURE, 0, "%s: %s", tag, idn2_strerror (rc));
  }
-@@ -228,7 +228,7 @@ main (int argc, char *argv[])
+ 
+ int
+@@ -237,7 +237,7 @@ main (int argc, char *argv[])
      }
  
    if (ferror (stdin))
