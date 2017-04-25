de39fb7df15e486a788b6a1c46ee92f7d0ff1512
Thomas Zander <riggs@FreeBSD.org>
2017-04-25 08:11:11 +0000

    Update to upstream version 3.3
    PR:		218658
    Submitted by:	riggs

--- multimedia/ffmpeg/files/patch-libavformat_tls__openssl.c
+++ multimedia/ffmpeg/files/patch-libavformat_tls__openssl.c
@@ -1,4 +1,4 @@
---- libavformat/tls_openssl.c.orig	2016-10-27 16:17:40 UTC
+--- libavformat/tls_openssl.c.orig	2017-04-13 01:55:55 UTC
 +++ libavformat/tls_openssl.c
 @@ -43,7 +43,7 @@ typedef struct TLSContext {
      TLSShared tls_shared;
@@ -24,10 +24,10 @@
  
 -#if OPENSSL_VERSION_NUMBER >= 0x1010000fL
 +#if OPENSSL_VERSION_NUMBER >= 0x1010000fL && !defined(LIBRESSL_VERSION_NUMBER)
- #define GET_BIO_DATA(x) BIO_get_data(x);
+ #define GET_BIO_DATA(x) BIO_get_data(x)
  #else
- #define GET_BIO_DATA(x) (x)->ptr;
-@@ -133,7 +133,7 @@ static int url_bio_bputs(BIO *b, const c
+ #define GET_BIO_DATA(x) (x)->ptr
+@@ -129,7 +129,7 @@ static int url_bio_bputs(BIO *b, const c
      return url_bio_bwrite(b, str, strlen(str));
  }
  
@@ -36,7 +36,7 @@
  static BIO_METHOD url_bio_method = {
      .type = BIO_TYPE_SOURCE_SINK,
      .name = "urlprotocol bio",
-@@ -212,7 +212,7 @@ static int tls_close(URLContext *h)
+@@ -208,7 +208,7 @@ static int tls_close(URLContext *h)
          SSL_CTX_free(c->ctx);
      if (c->tls_shared.tcp)
          ffurl_close(c->tls_shared.tcp);
@@ -45,7 +45,7 @@
      if (c->url_bio_method)
          BIO_meth_free(c->url_bio_method);
  #endif
-@@ -265,7 +265,7 @@ static int tls_open(URLContext *h, const
+@@ -266,7 +266,7 @@ static int tls_open(URLContext *h, const
          ret = AVERROR(EIO);
          goto fail;
      }
