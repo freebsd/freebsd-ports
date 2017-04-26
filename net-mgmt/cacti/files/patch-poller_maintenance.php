ee18491770df5734aeb281d3b8246a600f7e8b29
Kurt Jaeger <pi@FreeBSD.org>
2017-04-26 07:35:06 +0000

    net-mgmt/cacti: update 1.0.4 -> 1.1.4
    PR:		217707
    Changes:	http://www.cacti.net/changelog.php
    Submitted by:	Daniel Austin <freebsd-ports@dan.me.uk> (maintainer)

--- net-mgmt/cacti/files/patch-poller_maintenance.php
+++ net-mgmt/cacti/files/patch-poller_maintenance.php
@@ -1,14 +1,17 @@
---- poller_maintenance.php.orig	2017-02-04 14:23:37.951265000 +0000
-+++ poller_maintenance.php	2017-02-04 14:24:49.403929000 +0000
-@@ -172,10 +172,10 @@
-  */
- function logrotate_rotatenow () {
- 	global $config;
--	$log = $config['base_path'] . '/log/cacti.log';
-+	$log = '/var/log/cacti/log';
+--- poller_maintenance.php.orig	2017-04-24 03:25:50.000000000 +0100
++++ poller_maintenance.php	2017-04-25 14:27:06.327800000 +0100
+@@ -175,13 +175,13 @@
+ 
+ 	$log = read_config_option('path_cactilog');
+ 	if ($log == '') {
+-		$log = $config['base_path'] . '/log/cacti.log';
++		$log = '/var/log/cacti/log';
+ 	}
+ 
  	set_config_option('logrotate_lastrun', time());
  	clearstatcache();
--	if (is_writable($config['base_path'] . '/log/') && is_writable($log)) {
+ 
+-	if (is_writable(dirname($log) . '/') && is_writable($log)) {
 +	if (is_writable('/var/log/cacti') && is_writable($log)) {
  		$perms = octdec(substr(decoct( fileperms($log) ), 2));
  		$owner = fileowner($log);
