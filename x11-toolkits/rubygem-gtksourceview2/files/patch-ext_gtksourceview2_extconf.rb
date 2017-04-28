dce800b7a5242be93ff6e680a148cc40bb697d27
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2017-04-28 20:44:05 +0000

    Update to 3.1.3
    - Add USES=gnome
    
    Changes:	https://github.com/ruby-gnome2/ruby-gnome2/blob/master/NEWS

--- x11-toolkits/rubygem-gtksourceview2/files/patch-ext_gtksourceview2_extconf.rb
+++ x11-toolkits/rubygem-gtksourceview2/files/patch-ext_gtksourceview2_extconf.rb
@@ -1,11 +1,10 @@
---- ext/gtksourceview2/extconf.rb.orig	2016-01-19 22:46:22 UTC
+--- ext/gtksourceview2/extconf.rb.orig	2017-04-28 17:05:47 UTC
 +++ ext/gtksourceview2/extconf.rb
-@@ -30,7 +30,7 @@ rescue LoadError
-   require 'mkmf-gnome2'
+@@ -31,6 +31,7 @@ rescue LoadError
  end
  
--["glib2", "atk", "pango", "gdk_pixbuf2", "gtk2"].each do |package|
-+["cairo", "glib2", "atk", "pango", "gdk_pixbuf2", "gtk2"].each do |package|
-   directory = "#{package}#{version_suffix}"
-   build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{package}/#{RUBY_VERSION}"
-   add_depend_package(package, "#{directory}/ext/#{package}",
+ [
++  "cairo",
+   "glib2",
+   "gobject-introspection",
+   "gio2",
