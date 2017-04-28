1834ef854f0f95da07d2d4ff246792fe964cf593
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2017-04-28 20:24:28 +0000

    Use SO_REUSEPORT instead of SO_REUSEADDR
    - Bump PORTREVISION for package change
    
    PR:		215545
    Submitted by:	Kirill Pertsev <kika@kikap.com>

--- net/mDNSResponder/files/patch-mDNSPosix-mDNSPosix.c
+++ net/mDNSResponder/files/patch-mDNSPosix-mDNSPosix.c
@@ -1,6 +1,15 @@
---- mDNSPosix/mDNSPosix.c.orig	2015-03-13 00:15:32 UTC
+--- mDNSPosix/mDNSPosix.c.orig	2016-09-13 22:37:18 UTC
 +++ mDNSPosix/mDNSPosix.c
-@@ -740,7 +741,13 @@ mDNSlocal int SetupSocket(struct sockadd
+@@ -654,7 +654,7 @@ mDNSlocal int SetupSocket(struct sockadd
+         // to bind to the socket. Our suggestion was to switch the order in which
+         // SO_REUSEPORT and SO_REUSEADDR was tested so that SO_REUSEADDR stays on
+         // top and SO_REUSEPORT to be used only if SO_REUSEADDR doesn't exist.
+-        #if defined(SO_REUSEADDR) && !defined(__MAC_OS_X_VERSION_MIN_REQUIRED)
++        #if defined(SO_REUSEADDR) && !defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && !defined(__FreeBSD__)
+         err = setsockopt(*sktPtr, SOL_SOCKET, SO_REUSEADDR, &kOn, sizeof(kOn));
+         #elif defined(SO_REUSEPORT)
+         err = setsockopt(*sktPtr, SOL_SOCKET, SO_REUSEPORT, &kOn, sizeof(kOn));
+@@ -755,7 +755,13 @@ mDNSlocal int SetupSocket(struct sockadd
      {
          struct ipv6_mreq imr6;
          struct sockaddr_in6 bindAddr6;
@@ -15,7 +24,7 @@
          if (err == 0)
          {
              err = setsockopt(*sktPtr, IPPROTO_IPV6, IPV6_2292_PKTINFO, &kOn, sizeof(kOn));
-@@ -749,7 +756,13 @@ mDNSlocal int SetupSocket(struct sockadd
+@@ -764,7 +770,13 @@ mDNSlocal int SetupSocket(struct sockadd
      #else
          #warning This platform has no way to get the destination interface information for IPv6 -- will only work for single-homed hosts
      #endif
