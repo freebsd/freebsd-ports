0760b9d934680089d78abba586a420f240fae413
rene <rene@FreeBSD.org>
2017-04-28 11:16:01 +0000

    graphics/urt: save from deprecation
    - merge all separate distribution files and patches into one archive
      maintained the submitter
    - pass maintainership to submitter
    - add LICENSE (BSD2CLAUSE)
    
    PR:		218016
    Submitted by:	Chris Hutchinson (portmaster@bsdforge.com)

--- graphics/urt/files/patch-get__getx11__XGetHClrs.c
+++
@@ -1,8 +0,0 @@
---- get/getx11/XGetHClrs.c.orig	Sun Jan 30 15:48:57 2005
-+++ get/getx11/XGetHClrs.c	Sun Jan 30 15:49:38 2005
-@@ -1,5 +1,4 @@
- #ifndef XLIBINT_H_NOT_AVAILABLE
--#include <X11/copyright.h>
- 
- /* $XConsortium: XGetHClrs.c,v 11.10 88/09/06 16:07:50 martin Exp $ */
- /* Copyright    Massachusetts Institute of Technology    1986	*/
