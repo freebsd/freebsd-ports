fb788017cd8925a764a0a085f3bc0ef0d455e390
Tobias Kortkamp <tobik@FreeBSD.org>
2017-05-02 12:22:21 +0000

    Update to 1.11
    - Prevent linking with libcrypto when a pkg-config file for it exists
      on the system [1]
    - Bump PORTREVISION of dependent ports
    
    PR:		218996, 218688 [1]
    Reported by:	rsmith@xs4all.nl [1]
    Submitted by:	Zsolt Udvari <udvzsolt@gmail.com> (maintainer)
    Approved by:	lme (mentor)
    Differential Revision:	https://reviews.freebsd.org/D10561

--- graphics/mupdf/files/patch-scripts_hexdump.c
+++ graphics/mupdf/files/patch-scripts_hexdump.c
@@ -1,9 +1,9 @@
---- scripts/fontdump.c.orig	2016-11-28 13:34:04 UTC
-+++ scripts/fontdump.c
+--- scripts/hexdump.c.orig	2017-04-05 11:02:21 UTC
++++ scripts/hexdump.c
 @@ -50,7 +50,6 @@ main(int argc, char **argv)
  	fprintf(fo, "#ifndef __STRICT_ANSI__\n");
  	fprintf(fo, "#if defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__)\n");
- 	fprintf(fo, "#if !defined(__ICC)\n");
+ 	fprintf(fo, "#if !defined(__ICC) && !defined(__ANDROID__)\n");
 -	fprintf(fo, "#define HAVE_INCBIN\n");
  	fprintf(fo, "#endif\n");
  	fprintf(fo, "#endif\n");
