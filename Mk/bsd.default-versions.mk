df1ade811419fac89385612edf295c0ecc68df9f
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2017-05-05 12:30:55 +0000

    Update comment: remove ruby 2.1

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -81,7 +81,7 @@ PYTHON_DEFAULT?=	2.7
 PYTHON2_DEFAULT?=	2.7
 # Possible values: 3.3, 3.4, 3.5, 3.6
 PYTHON3_DEFAULT?=	3.6
-# Possible values: 2.1, 2.2, 2.3, 2.4
+# Possible values: 2.2, 2.3, 2.4
 RUBY_DEFAULT?=		2.3
 # Possible values: 4.2, 4.3, 4.4, 4.5, 4.6
 SAMBA_DEFAULT?=		4.4
