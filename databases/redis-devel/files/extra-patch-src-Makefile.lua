dd7d3065f21eb7b223652d892e7eb49486511e89
osa <osa@FreeBSD.org>
2017-05-06 18:03:15 +0000

    Upgrade from 4.0-rc2 to 4.0-rc3.

--- databases/redis-devel/files/extra-patch-src-Makefile.lua
+++ databases/redis-devel/files/extra-patch-src-Makefile.lua
@@ -1,15 +1,15 @@
---- src/Makefile.orig	2016-12-04 19:56:10.186400000 -0500
-+++ src/Makefile	2016-12-04 20:01:02.427294000 -0500
-@@ -15,7 +15,7 @@
- release_hdr := $(shell sh -c './mkreleasehdr.sh')
+--- src/Makefile.orig	2017-05-06 13:55:29.224222000 -0400
++++ src/Makefile	2017-05-06 13:57:41.129934000 -0400
+@@ -16,7 +16,7 @@
  uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
+ uname_M := $(shell sh -c 'uname -m 2>/dev/null || echo not')
  OPTIMIZATION?=-O2
 -DEPENDENCY_TARGETS=hiredis linenoise lua
 +DEPENDENCY_TARGETS=hiredis linenoise
  NODEPS:=clean distclean
  
  # Default settings
-@@ -54,8 +54,8 @@
+@@ -58,8 +58,8 @@
  # Override default settings if possible
  -include .make-settings
  
@@ -20,7 +20,7 @@
  FINAL_LIBS=-lm
  
  ifeq ($(uname_S),SunOS)
-@@ -96,7 +96,7 @@
+@@ -107,7 +107,7 @@
  endif
  endif
  # Include paths to dependencies
@@ -29,7 +29,7 @@
  
  ifeq ($(MALLOC),tcmalloc)
  	FINAL_CFLAGS+= -DUSE_TCMALLOC
-@@ -114,6 +114,9 @@
+@@ -125,6 +125,9 @@
  	FINAL_LIBS+= ../deps/jemalloc/lib/libjemalloc.a
  endif
  
@@ -39,15 +39,15 @@
  REDIS_CC=$(QUIET_CC)$(CC) $(FINAL_CFLAGS)
  REDIS_LD=$(QUIET_LINK)$(CC) $(FINAL_LDFLAGS)
  REDIS_INSTALL=$(QUIET_INSTALL)$(INSTALL)
-@@ -134,6 +137,7 @@
+@@ -145,6 +148,7 @@
  REDIS_SERVER_NAME=redis-server
  REDIS_SENTINEL_NAME=redis-sentinel
- REDIS_SERVER_OBJ=adlist.o quicklist.o ae.o anet.o dict.o server.o sds.o zmalloc.o lzf_c.o lzf_d.o pqsort.o zipmap.o sha1.o ziplist.o release.o networking.o util.o object.o db.o replication.o rdb.o t_string.o t_list.o t_set.o t_zset.o t_hash.o config.o aof.o pubsub.o multi.o debug.o sort.o intset.o syncio.o cluster.o crc16.o endianconv.o slowlog.o scripting.o bio.o rio.o rand.o memtest.o crc64.o bitops.o sentinel.o notify.o setproctitle.o blocked.o hyperloglog.o latency.o sparkline.o redis-check-rdb.o geo.o lazyfree.o module.o evict.o expire.o geohash.o geohash_helper.o childinfo.o
+ REDIS_SERVER_OBJ=adlist.o quicklist.o ae.o anet.o dict.o server.o sds.o zmalloc.o lzf_c.o lzf_d.o pqsort.o zipmap.o sha1.o ziplist.o release.o networking.o util.o object.o db.o replication.o rdb.o t_string.o t_list.o t_set.o t_zset.o t_hash.o config.o aof.o pubsub.o multi.o debug.o sort.o intset.o syncio.o cluster.o crc16.o endianconv.o slowlog.o scripting.o bio.o rio.o rand.o memtest.o crc64.o bitops.o sentinel.o notify.o setproctitle.o blocked.o hyperloglog.o latency.o sparkline.o redis-check-rdb.o geo.o lazyfree.o module.o evict.o expire.o geohash.o geohash_helper.o childinfo.o defrag.o siphash.o rax.o
 +REDIS_SERVER_OBJ+=fpconv.o lua_bit.o lua_cjson.o lua_cmsgpack.o lua_struct.o strbuf.o
  REDIS_CLI_NAME=redis-cli
  REDIS_CLI_OBJ=anet.o adlist.o redis-cli.o zmalloc.o release.o anet.o ae.o crc64.o
  REDIS_BENCHMARK_NAME=redis-benchmark
-@@ -186,7 +190,7 @@
+@@ -197,7 +201,7 @@
  
  # redis-server
  $(REDIS_SERVER_NAME): $(REDIS_SERVER_OBJ)
