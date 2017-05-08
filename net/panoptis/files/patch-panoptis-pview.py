9bd12f06b1a365e7c8aca842104e3e7048d39409
miwi <miwi@FreeBSD.org>
2017-05-08 09:45:18 +0000

    - Fix shebangs

--- net/panoptis/files/patch-panoptis-pview.py
+++
@@ -1,8 +0,0 @@
---- panoptis/pview.py.orig	Tue Nov 28 14:57:01 2006
-+++ panoptis/pview.py	Tue Nov 28 14:57:10 2006
-@@ -1,4 +1,4 @@
--#!/usr/bin/python
-+#!/usr/bin/env python
- 
- # This CGI script connects to Panoptis' web port, makes a request,
- # accepts data and formats it to show it to the user.
