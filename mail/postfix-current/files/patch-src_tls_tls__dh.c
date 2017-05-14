5e468e329243ef95e64a9affc2c7ab2852dd5029
Bernard Spil <bernard.spil@hardenedbsd.org>
2017-05-14 11:19:41 +0200

    HBSD: mail/postfix-current: Fix x25519 Kex with LibreSSL
    See also #50
    
    Signed-off-by: Bernard Spil <bernard.spil@hardenedbsd.org>

--- mail/postfix-current/files/patch-src_tls_tls__dh.c
+++ mail/postfix-current/files/patch-src_tls_tls__dh.c
@@ -1,48 +1,15 @@
-PR 216732: Fix build with libressl < 2.5.1
-=========================================================
+$OpenBSD: patch-src_tls_tls_dh_c,v 1.1 2017/03/04 22:09:43 sthen Exp $
+
+Fix building with LibreSSL
+
 --- src/tls/tls_dh.c.orig	2016-12-26 23:47:24 UTC
 +++ src/tls/tls_dh.c
-@@ -94,7 +94,7 @@
- #define TLS_INTERNAL
- #include <tls.h>
- #include <openssl/dh.h>
--#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL && !defined(OPENSSL_NO_ECDH)
-+#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL && !defined(OPENSSL_NO_ECDH) && !defined(LIBRESSL_VERSION_NUMBER)
- #include <openssl/ec.h>
- #endif
- 
-@@ -244,7 +244,7 @@ DH     *tls_tmp_dh_cb(SSL *unused_ssl, i
- 
- void    tls_auto_eecdh_curves(SSL_CTX *ctx)
- {
--#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL && !defined(OPENSSL_NO_ECDH)
-+#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL && !defined(OPENSSL_NO_ECDH) && !defined(LIBRESSL_VERSION_NUMBER)
-     SSL_CTX *tmpctx;
-     int    *nids;
-     int     space = 5;
-@@ -337,14 +337,14 @@ void    tls_set_eecdh_curve(SSL_CTX *ser
- #define TLS_EECDH_NONE		1
- #define TLS_EECDH_STRONG	2
- #define TLS_EECDH_ULTRA		3
--#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL
-+#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL && !defined(LIBRESSL_VERSION_NUMBER)
- #define TLS_EECDH_AUTO		4
- #endif
-     static NAME_CODE eecdh_table[] = {
- 	"none", TLS_EECDH_NONE,
- 	"strong", TLS_EECDH_STRONG,
- 	"ultra", TLS_EECDH_ULTRA,
--#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL
-+#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL && !defined(LIBRESSL_VERSION_NUMBER)
- 	"auto", TLS_EECDH_AUTO,
- #endif
- 	0, TLS_EECDH_INVALID,
-@@ -364,7 +364,7 @@ void    tls_set_eecdh_curve(SSL_CTX *ser
-     case TLS_EECDH_ULTRA:
- 	curve = var_tls_eecdh_ultra;
- 	break;
--#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL
-+#if OPENSSL_VERSION_NUMBER >= 0x1000200fUL && !defined(LIBRESSL_VERSION_NUMBER)
-     case TLS_EECDH_AUTO:
- 	tls_auto_eecdh_curves(server_ctx);
- 	return;
+@@ -314,7 +314,7 @@ void    tls_auto_eecdh_curves(SSL_CTX *c
+      * This is a NOP in OpenSSL 1.1.0 and later, where curves are always
+      * auto-negotiated.
+      */
+-#if OPENSSL_VERSION_NUMBER < 0x10100000UL
++#if OPENSSL_VERSION_NUMBER < 0x10100000UL || defined(LIBRESSL_VERSION_NUMBER)
+     if (SSL_CTX_set_ecdh_auto(ctx, 1) <= 0) {
+ 	msg_warn("failed to enable automatic ECDHE curve selection");
+ 	tls_print_errors();
