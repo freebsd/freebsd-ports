c6a395f73b39cc583f07a68ddcb5b9ffca2dfad1
Jung-uk Kim <jkim@FreeBSD.org>
2017-05-19 23:48:29 +0000

    Revert r441276 and properly fix build failures with JAVA option.

--- editors/libreoffice/files/patch-sc_Module__sc.mk
+++ editors/libreoffice/files/patch-sc_Module__sc.mk
@@ -1,16 +1,6 @@
 --- sc/Module_sc.mk.orig	2017-05-03 16:46:29 UTC
 +++ sc/Module_sc.mk
-@@ -45,7 +45,8 @@ $(eval $(call gb_Module_add_check_targets,sc,\
- 	$(if $(and $(filter $(COM),MSC),$(MERGELIBS)),, \
- 		CppunitTest_sc_ucalc) \
- 	CppunitTest_sc_bugfix_test \
--	CppunitTest_sc_filters_test \
-+	$(if $(filter FREEBSD,$(OS)),, \
-+		CppunitTest_sc_filters_test) \
- 	CppunitTest_sc_rangelst_test \
- 	CppunitTest_sc_mark_test \
- 	CppunitTest_sc_core \
-@@ -60,8 +61,9 @@ endif
+@@ -60,8 +60,9 @@ endif
  $(eval $(call gb_Module_add_slowcheck_targets,sc, \
  	CppunitTest_sc_condformats \
  	CppunitTest_sc_new_cond_format_api \
@@ -22,7 +12,7 @@
  	CppunitTest_sc_html_export_test \
  	CppunitTest_sc_opencl_test \
  	CppunitTest_sc_copypaste \
-@@ -71,7 +73,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sc, \
+@@ -71,7 +72,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sc, \
  # point weirdness (x87, registers, compiler optimization, ... whatever),
  # disable them until someone finds a real cure.
  
