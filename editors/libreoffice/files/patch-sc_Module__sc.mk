16dfa15ae4858d3e66774d58bd6882360947dd15
Jung-uk Kim <jkim@FreeBSD.org>
2017-05-19 20:14:54 +0000

    Work around a build failure on i386.

--- editors/libreoffice/files/patch-sc_Module__sc.mk
+++ editors/libreoffice/files/patch-sc_Module__sc.mk
@@ -1,6 +1,6 @@
---- sc/Module_sc.mk.orig	2017-01-12 00:54:33 UTC
+--- sc/Module_sc.mk.orig	2017-05-03 16:46:29 UTC
 +++ sc/Module_sc.mk
-@@ -54,8 +54,9 @@ $(eval $(call gb_Module_add_check_target
+@@ -60,8 +60,9 @@ endif
  $(eval $(call gb_Module_add_slowcheck_targets,sc, \
  	CppunitTest_sc_condformats \
  	CppunitTest_sc_new_cond_format_api \
@@ -12,3 +12,12 @@
  	CppunitTest_sc_html_export_test \
  	CppunitTest_sc_opencl_test \
  	CppunitTest_sc_copypaste \
+@@ -71,7 +72,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sc, \
+ # point weirdness (x87, registers, compiler optimization, ... whatever),
+ # disable them until someone finds a real cure.
+ 
+-ifneq ($(PLATFORMID),linux_x86)
++ifeq ($(filter freebsd_x86 linux_x86,$(PLATFORMID)),)
+ $(eval $(call gb_Module_add_slowcheck_targets,sc, \
+ 	CppunitTest_sc_functions_test \
+ ))
