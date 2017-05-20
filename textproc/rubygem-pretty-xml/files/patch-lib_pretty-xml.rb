849e09b466b27a9265f6fdfdd05a09203e023916
tcberner <tcberner@FreeBSD.org>
2017-05-20 18:06:38 +0000

    Fix mangled shebang.
    Approved by:	rakuco (mentor, implicit)

---
+++ textproc/rubygem-pretty-xml/files/patch-lib_pretty-xml.rb
@@ -0,0 +1,15 @@
+--- lib/pretty-xml.rb.orig	2017-05-20 17:50:05 UTC
++++ lib/pretty-xml.rb
+@@ -1,4 +1,4 @@
+-#!/bin/usr/env ruby
++#!/usr/bin/env ruby
+ 
+ # file: pretty-xml.rb
+ 
+@@ -66,4 +66,4 @@ XSL
+   module_function :write
+   
+   alias print write
+-end
+\ No newline at end of file
++end
