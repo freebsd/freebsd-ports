bc00fe5968b09520b66745f1dfbb222f4256198d
Tobias C. Berner <tcberner@FreeBSD.org>
2017-05-20 12:40:08 +0000

    MFH: r441311
    Update _KDE_APPLICATIONS_ATTIC_VERSION to 16.12.3 to fix fetching of distfiles
    
    KDE has moved distfiles for applications 16.12.3 to Attic/ on their mirros.
    
    Reported by:	Matthias Apitz <guru@unixarea.de>
    Reviewed by:	rakuco
    Approved by:	rakuco (mentor, implicit)
    Differential Revision:	https://reviews.freebsd.org/D10830
    
    Approved by:	ports-secteam (blanket), rakuco (mentor)

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -73,7 +73,7 @@ KDE_APPLICATIONS_VERSION?=      16.12.3
 KDE_APPLICATIONS_BRANCH?=       stable
 # Upstream moves old software to Attic/. Specify the newest applications release there.
 # Only the major version is used for the comparison.
-_KDE_APPLICATIONS_ATTIC_VERSION=	15.12.3
+_KDE_APPLICATIONS_ATTIC_VERSION=	16.12.3
 
 # Extended KDE universe applications.
 CALLIGRA_VERSION?=		2.9.11
