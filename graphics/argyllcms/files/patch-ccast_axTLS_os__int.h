0b2f48dc3dbc41e36e345d987c317481b211b50f
Koop Mast <kwm@FreeBSD.org>
2017-05-22 19:28:52 +0000

    Fix hardcoded /usr/local.
    * Un-mute install of License.txt
    
    PR:		216236
    Submitted by:	John Hein <z7dr6ut7gs@snkmail.com>

--- graphics/argyllcms/files/patch-ccast_axTLS_os__int.h
+++ graphics/argyllcms/files/patch-ccast_axTLS_os__int.h
@@ -1,6 +1,6 @@
---- ./ccast/axTLS/os_int.h.orig
-+++ ./ccast/axTLS/os_int.h
-@@ -34,6 +34,7 @@
+--- ccast/axTLS/os_int.h.orig	2016-09-28 03:00:00 UTC
++++ ccast/axTLS/os_int.h
+@@ -34,6 +34,7 @@ extern "C" {
  #if (__STDC_VERSION__ >= 199901L)	/* C99 */		\
   || defined(_STDINT_H_) || defined(_STDINT_H)		\
   || defined(_SYS_TYPES_H)							\
@@ -8,4 +8,3 @@
   || (defined(__APPLE__) && defined(__POWERPC__))
  
  #include <stdint.h> 
-
