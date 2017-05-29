a0d2d2913eaf81bb0ca140f182f6af3099a022d3
crees <crees@FreeBSD.org>
2017-05-29 20:05:21 +0000

    CDE - The Common Desktop Environment is X Windows desktop environment that was commonly used on commercial UNIX variants such as Sun Solaris, HP-UX and IBM AIX. Developed between 1993 and 1999, it has now been released under an Open Source licence by The Open Group.
    WWW: https://sourceforge.net/p/cdesktopenv/wiki/Home/

---
+++ x11-wm/cde/files/patch-programs_dtcm_dtcm_calendarA.c
@@ -0,0 +1,16 @@
+$FreeBSD$
+
+We don't use inetd-- that involves messing with inetd.conf.  May as well
+just launch as a daemon, which also deals with rpcbind dependency.
+
+--- programs/dtcm/dtcm/calendarA.c.orig	2016-09-14 10:18:38 UTC
++++ programs/dtcm/dtcm/calendarA.c
+@@ -2051,7 +2051,7 @@ error_open(Calendar *c) {
+ 	set_message(c->message_text, buf);
+ 
+ 	sprintf(buf, "%s\n%s: %s, %s: %s", catgets(c->DT_catd, 1, 91,
+-				   		"rpc.cmsd is not responding for your user name.\nMake sure the inetd process is running and the entry\nin inetd.conf for rpc.cmsd is correct for your host."),
++				   		"rpc.cmsd is not responding for your user name.\nMake sure that rpc.cmsd is running and enabled\nin /etc/rc.conf (dtcms_enable=yes)."),
+ 		nl_user, name,
+ 		nl_host, host);
+ 
