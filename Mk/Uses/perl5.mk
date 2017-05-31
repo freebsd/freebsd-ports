941d49b55e4c0a1e6dac5de36f53d18a4dd36b26
mat <mat@FreeBSD.org>
2017-05-31 09:02:11 +0000

    Introduce Perl 5.26.
    A few important changes:
    
     - '.' is no longer in @INC.
     - "do" now gives a deprecation warning when it fails to load a file
       which it would have loaded had "." been in @INC.
     - In regular expression patterns, a literal left brace "{" should be
       escaped.
    
    Changes:	https://metacpan.org/pod/release/XSAWYERX/perl-5.26.0/pod/perldelta.pod
    Sponsored by:	Absolight

--- Mk/Uses/perl5.mk
+++ Mk/Uses/perl5.mk
@@ -57,6 +57,8 @@ THIS_IS_OLD_PERL=	yes
 .include "${PORTSDIR}/lang/perl5.22/version.mk"
 .    elif ${PERL5_DEFAULT} == 5.24
 .include "${PORTSDIR}/lang/perl5.24/version.mk"
+.    elif ${PERL5_DEFAULT} == 5.26
+.include "${PORTSDIR}/lang/perl5.26/version.mk"
 .    elif ${PERL5_DEFAULT} == devel
 .include "${PORTSDIR}/lang/perl5-devel/version.mk"
 # Force PERL_PORT here in case two identical PERL_VERSION.
@@ -91,7 +93,9 @@ PERL_ARCH?=	mach
 # perl5_default file, or up there in the default versions selection.
 # When adding a version, please keep the comment in
 # Mk/bsd.default-versions.mk in sync.
-.  if   ${PERL_LEVEL} >= 502400
+.  if   ${PERL_LEVEL} >= 502600
+PERL_PORT?=	perl5.26
+.  elif   ${PERL_LEVEL} >= 502400
 PERL_PORT?=	perl5.24
 .  elif   ${PERL_LEVEL} >= 502200
 PERL_PORT?=	perl5.22
