92a9b2e4326ee37668fd7c71fbdfb1b1efd65010
pi <pi@FreeBSD.org>
2017-05-31 13:23:26 +0000

    net/kamailio: update 4.4.3 -> 5.0.1
    This is a major release, introducing a consistent set of new features
    as well as improvements to existing components.
    
    Reviewed by:	Alexey V. Panfilov <ports@subnets.ru>
    Relnotes:	https://www.kamailio.org/w/kamailio-v5-0-0-release-notes/

--- net/kamailio/files/patch-src_core_ip__addr.h
+++ net/kamailio/files/patch-src_core_ip__addr.h
@@ -1,7 +1,7 @@
---- ip_addr.h.orig	2015-09-05 21:17:34 UTC
-+++ ip_addr.h
-@@ -53,6 +53,10 @@ enum comp_methods { COMP_NONE, COMP_SIGC
- #define INADDR_LOOPBACK         (u_int32_t)0x7f000001
+--- src/core/ip_addr.h.orig	2017-04-21 12:46:05 UTC
++++ src/core/ip_addr.h
+@@ -49,6 +49,10 @@ enum sip_protos { PROTO_NONE, PROTO_UDP, PROTO_TCP, PR
+ enum comp_methods { COMP_NONE, COMP_SIGCOMP, COMP_SERGZ };
  #endif
  
 +#ifndef INADDR_LOOPBACK
--- net/kamailio/files/patch-src_modules_websocket_ws__frame.c
+++ net/kamailio/files/patch-src_modules_websocket_ws__frame.c
@@ -1,5 +1,5 @@
---- modules/websocket/ws_frame.c.orig	2015-09-05 21:18:25 UTC
-+++ modules/websocket/ws_frame.c
+--- src/modules/websocket/ws_frame.c.orig	2017-04-21 12:46:06 UTC
++++ src/modules/websocket/ws_frame.c
 @@ -29,7 +29,7 @@
  #ifdef EMBEDDED_UTF8_DECODE
  #include "utf8_decode.h"
@@ -8,4 +8,4 @@
 +/* #include <unistr.h> */
  #endif
  
- #include "../../events.h"
+ #include "../../core/events.h"
