4d2a883e0e6febdfe030e365136533e78bcd6248
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2017-06-02 18:14:53 +0000

    Update to 1.4.12
    Changes:	https://github.com/rubys/nokogumbo/commits/master

--- textproc/rubygem-nokogumbo/files/patch-ext_nokogumboc_extconf.rb
+++ textproc/rubygem-nokogumbo/files/patch-ext_nokogumboc_extconf.rb
@@ -1,15 +1,15 @@
---- ext/nokogumboc/extconf.rb.orig	2015-07-26 21:04:54 UTC
+--- ext/nokogumboc/extconf.rb.orig	2017-06-02 16:28:15 UTC
 +++ ext/nokogumboc/extconf.rb
 @@ -1,3 +1,4 @@
 +require 'rubygems'
  require 'mkmf'
- $CFLAGS += " -std=c99"
+ $CFLAGS += ' -std=c99'
  
-@@ -10,7 +11,7 @@ if have_library('xml2', 'xmlNewDoc')
-     select { |name| name.match(%r{gems/nokogiri-([\d.]+)/lib/nokogiri}) }.
-     sort_by {|name| name[/nokogiri-([\d.]+)/,1].split('.').map(&:to_i)}.last
+@@ -12,7 +13,7 @@ if have_library('xml2', 'xmlNewDoc')
+                  .sort_by { |name| name[/nokogiri-([\d.]+)/, 1].split('.').map(&:to_i) }
+                  .last
    if nokogiri_lib
--    nokogiri_ext = nokogiri_lib.sub(%r(lib/nokogiri(.rb)?$), 'ext/nokogiri')
+-    nokogiri_ext = nokogiri_lib.sub(%r{lib/nokogiri(.rb)?$}, 'ext/nokogiri')
 +    nokogiri_ext = Gem::Specification.find_by_name('nokogiri').extension_dir + "/nokogiri"
  
      # if that doesn't work, try workarounds found in Nokogiri's extconf
