2a50f547193a527f8c0c7a92d44549cf4fe12068
tcberner <tcberner@FreeBSD.org>
2017-06-06 05:00:56 +0000

    Fix qt4 ports on armv6.
    Due to a misspelling in GCC [1] (probably) the check for the ARMv6KZ platform
    used ARM_ARCH_6ZK instead of ARM_ARCH_6KZ.
    
    [1] https://gcc.gnu.org/ml/gcc-patches/2015-06/msg01679.html
    
    PR:		210027
    Submitted by:	Mikaël Urankar <mikael.urankar@gmail.com>
    Reviewed by:	rakuco
    Differential Revision:	https://reviews.freebsd.org/D8322

--- Mk/bsd.qt.mk
+++ Mk/bsd.qt.mk
@@ -175,6 +175,8 @@ CONFIGURE_ARGS+=-verbose
 .  if ${_QT_VERSION:M4*}
 _EXTRA_PATCHES_QT4=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-src-corelib-global-qglobal.h \
 					${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-libtool
+# Patch in proper name for armv6 architecture: https://gcc.gnu.org/ml/gcc-patches/2015-06/msg01679.html 
+_EXTRA_PATCHES_QT4+=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-armv6
 .  else
 _EXTRA_PATCHES_QT5=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-mkspecs_features_create__cmake.prf \
 					${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-mkspecs_features_qt__module.prf
