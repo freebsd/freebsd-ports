17a34717e1497d2a2602aaae5dcf6409bc037a11
bofh <bofh@FreeBSD.org>
2017-06-06 13:38:52 +0000

    net-mgmt/librenms: Update version 1.26=>1.28
    - Fixed an issue where services-wrapper wasn't working (by adding the PATH
      line to the crontab) [1]
    - Fixed a permissions issue with some directories [1]
    - Added patch to validate.php which allows it to work correctly
      (accidentally ommitted from previous commit) [1]
    
    PR:		218608[1]
    Submitted by:	freebsd@jonathanprice.org[1]

--- net-mgmt/librenms/files/patch-includes_common.php
+++ net-mgmt/librenms/files/patch-includes_common.php
@@ -1,7 +1,7 @@
---- includes/common.php.orig	2017-01-29 06:54:29 UTC
+--- includes/common.php.orig	2017-03-26 12:06:00 UTC
 +++ includes/common.php
-@@ -1095,10 +1095,8 @@ function version_info($remote = true)
-             curl_setopt($api, CURLOPT_RETURNTRANSFER, 1);
+@@ -1098,10 +1098,8 @@ function version_info($remote = true)
+             curl_setopt($api, CURLOPT_CONNECTTIMEOUT, 5);
              $output['github'] = json_decode(curl_exec($api), true);
          }
 -        list($local_sha, $local_date) = explode('|', rtrim(`git show --pretty='%H|%ct' -s HEAD`));
