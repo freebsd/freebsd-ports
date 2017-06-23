ccbd78d2942f68f3f34072f0165c556a26438665
cpm <cpm@FreeBSD.org>
2017-06-23 17:45:26 +0000

    www/chromium: update to 59.0.3071.109
    - Fix TEST option
    
    Changelog: https://chromium.googlesource.com/chromium/src/+log/59.0.3071.104..59.0.3071.109?pretty=fuller&n=10000
    
    MFH:		2017Q2

--- www/chromium/files/patch-components_os__crypt_os__crypt.h
+++ www/chromium/files/patch-components_os__crypt_os__crypt.h
@@ -1,5 +1,5 @@
---- components/os_crypt/os_crypt.h.orig	2017-04-19 19:06:32 UTC
-+++ components/os_crypt/os_crypt.h
+--- components/os_crypt/os_crypt.h.orig	2017-06-15 21:03:04.000000000 +0200
++++ components/os_crypt/os_crypt.h	2017-06-21 19:21:25.185823000 +0200
 @@ -13,7 +13,7 @@
  #include "base/strings/string16.h"
  #include "build/build_config.h"
@@ -18,3 +18,12 @@
    // If |store_type| is a known password store, we will attempt to use it.
    // In any other case, we default to auto-detecting the store.
    // This should not be changed after OSCrypt has been used.
+@@ -71,7 +71,7 @@
+   DISALLOW_IMPLICIT_CONSTRUCTORS(OSCrypt);
+ };
+ 
+-#if defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(UNIT_TEST)
++#if (defined(OS_LINUX) || defined(OS_BSD)) && !defined(OS_CHROMEOS) && defined(UNIT_TEST)
+ // For unit testing purposes, inject methods to be used.
+ // |get_key_storage_mock| provides the desired |KeyStorage| implementation.
+ // If the provider returns |nullptr|, a hardcoded password will be used.
