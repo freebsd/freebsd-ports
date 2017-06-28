b309301127355508db866899af15ed3d41b7f9b5
amdmi3 <amdmi3@FreeBSD.org>
2017-06-28 12:46:46 +0000

    - Update to 9.4g

--- games/hyperrogue/files/patch-sound.cpp
+++ games/hyperrogue/files/patch-sound.cpp
@@ -1,6 +1,6 @@
---- graph.cpp.orig	2016-08-26 16:36:03 UTC
-+++ graph.cpp
-@@ -5595,7 +5595,7 @@ bool loadMusicInfo(string dir) {
+--- sound.cpp.orig	2017-04-15 02:52:00 UTC
++++ sound.cpp
+@@ -122,7 +122,7 @@ bool loadMusicInfo(string dir) {
      for(int i=0; i<size(dir); i++) if(dir[i] == '/' || dir[i] == '\\')
        dir2 = dir.substr(0, i+1);
      char buf[1000];
