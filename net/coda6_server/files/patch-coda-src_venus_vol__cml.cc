885fca7b6e13b3cd952efeb3e293cbb12bba0fc1
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-06-30 16:52:44 +0000

    Fix coda6_client and coda6_server after ino64 changes.

--- net/coda6_server/files/patch-coda-src_venus_vol__cml.cc
+++ net/coda6_server/files/patch-coda-src_venus_vol__cml.cc
@@ -1,6 +1,6 @@
 --- coda-src/venus/vol_cml.cc.orig	2008-10-06 16:52:54 UTC
 +++ coda-src/venus/vol_cml.cc
-@@ -3606,7 +3606,8 @@ static int WriteLinks(struct DirEntry *d
+@@ -3606,7 +3606,8 @@ static int WriteLinks(struct DirEntry *de, void *arg)
  {
      VnodeId vnode;
      Unique_t vunique;
