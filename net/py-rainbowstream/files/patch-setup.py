f1a1de7f56b8bd7c6095fdddba378a10a71e7615
Kubilay Kocak <koobs@FreeBSD.org>
2017-07-03 11:22:52 +0000

    net/py-rainbowstream: Make pocket (really) optional
    rainbowstream has configurable Pocket support (via settings) but declares
    the dependency in install_requires, making it a compulsory run-time dependency
    checked/varified by setuptools.
    
    When first committed, given the pocket Python package had not yet been ported,
    it was (obviously) not included in RUN_DEPENDS, but it was not removed from
    setup.py:install_requires, resulting in the following runtime error:
    
      pkg_resources.DistributionNotFound: The 'pocket' distribution was not found
      and is required by rainbowstream
    
    This could be rectified by the user by installing the package from pip, which
    is convenient, but which we explicitly recommend against [1].
    
    This commit patches setup.py moving pocket to setup.py:extras_require and the
    main module to conditionally import pocket, instead of failing as follows:
    
        from pocket import Pocket
      ImportError: No module named pocket
    
    While I'm here:
    
    - Remove upper bound/limit on 3.x version support
    
    [1] See devel/py-pip/pkg-message
    
    PR:		220312
    Reported by:	Petr Fischer <petr.fischer me com>
    MFH:		2017Q3

--- net/py-rainbowstream/files/patch-setup.py
+++ net/py-rainbowstream/files/patch-setup.py
@@ -1,6 +1,9 @@
---- setup.py.orig	2016-08-04 08:41:32 UTC
+# Make pocket actually optional
+# https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=220312
+
+--- setup.py.orig	2016-08-16 14:49:45 UTC
 +++ setup.py
-@@ -9,7 +9,7 @@ version = '1.3.3'
+@@ -15,14 +15,17 @@ version = '1.3.5'
  install_requires = [
      "python-dateutil",
      "arrow",
@@ -9,3 +12,22 @@
      "pyfiglet",
      "twitter",
      "Pillow",
+     "PySocks",
+-    "pocket"
+ ]
+ 
++extras_require = {
++    'pocket': ["pocket"],
++}
++
+ # Default user (considers non virtualenv method)
+ user = os.environ.get('SUDO_USER', os.environ['USER'])
+ 
+@@ -65,6 +68,7 @@ setup(name='rainbowstream',
+       include_package_data=True,
+       zip_safe=True,
+       install_requires=install_requires,
++      extras_require=extras_require,
+       entry_points="""
+       # -*- Entry points: -*-
+       [console_scripts]
