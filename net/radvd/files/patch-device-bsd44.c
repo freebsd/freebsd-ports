10abcdcdd27e5cfa9b857880dc7a8309e8e49547
Babak Farrokhi <farrokhi@FreeBSD.org>
2017-07-04 06:08:18 +0000

    Update net/radvd to 2.17

--- net/radvd/files/patch-device-bsd44.c
+++ net/radvd/files/patch-device-bsd44.c
@@ -1,9 +1,12 @@
---- device-bsd44.c.orig	2015-11-19 13:58:52 UTC
+--- device-bsd44.c.orig	2017-06-29 04:32:29 UTC
 +++ device-bsd44.c
-@@ -116,6 +116,24 @@ int update_device_info(int sock, struct 
+@@ -126,7 +126,28 @@ ret:
+ 	return -1;
+ }
  
- int setup_allrouters_membership(int sock, struct Interface *iface)
- {
+-int setup_allrouters_membership(int sock, struct Interface *iface) { return 0; }
++int setup_allrouters_membership(int sock, struct Interface *iface) 
++{
 +	struct ipv6_mreq mreq;
 +
 +	memset(&mreq, 0, sizeof(mreq));
@@ -15,13 +18,15 @@
 +		flog(LOG_ERR, "inet_pton failed");
 +		return (-1);
 +	}
-+
++       
 +	if (setsockopt(sock, IPPROTO_IPV6, IPV6_JOIN_GROUP,
 +			&mreq, sizeof(mreq)) < 0) {
 +		flog(LOG_ERR, "can't join ipv6-allrouters on %s", iface->props.name);
 +		return (-1);
 +	}
 +
- 	return 0;
- }
++	return 0; 
++}
  
+ int set_interface_linkmtu(const char *iface, uint32_t mtu)
+ {
