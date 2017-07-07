7be7de93053a006475729800f9c565a51311009f
Torsten Zuehlsdorff <tz@FreeBSD.org>
2017-07-07 08:27:24 +0000

    MFH: r445153
    www/gitlab: Update from 9.0.10 to 9.1.7
    
    Changelog: https://github.com/gitlabhq/gitlabhq/blob/v9.1.7/CHANGELOG.md
    
    Reviewed by: Matthias Fechner <idefix@fechner.net>
    
    Approved by: ports-secteam (junovitch)

--- www/gitlab/files/patch-config_initializers_1__settings.rb
+++ www/gitlab/files/patch-config_initializers_1__settings.rb
@@ -1,8 +1,8 @@
---- config/initializers/1_settings.rb.orig	2017-06-07 20:58:14 UTC
+--- config/initializers/1_settings.rb.orig	2017-06-07 20:48:31 UTC
 +++ config/initializers/1_settings.rb
-@@ -203,12 +203,8 @@ Settings.gitlab['email_subject_suffix'] 
- Settings.gitlab['base_url']   ||= Settings.send(:build_base_gitlab_url)
- Settings.gitlab['url']        ||= Settings.send(:build_gitlab_url)
+@@ -215,12 +215,8 @@ Settings.gitlab['email_subject_suffix'] 
+ Settings.gitlab['base_url']   ||= Settings.__send__(:build_base_gitlab_url)
+ Settings.gitlab['url']        ||= Settings.__send__(:build_gitlab_url)
  Settings.gitlab['user']       ||= 'git'
 -Settings.gitlab['user_home']  ||= begin
 -  Etc.getpwnam(Settings.gitlab['user']).dir
@@ -14,4 +14,4 @@
 +Settings.gitlab['time_zone']  ||= nil
  Settings.gitlab['signup_enabled'] ||= true if Settings.gitlab['signup_enabled'].nil?
  Settings.gitlab['signin_enabled'] ||= true if Settings.gitlab['signin_enabled'].nil?
- Settings.gitlab['restricted_visibility_levels'] = Settings.send(:verify_constant_array, Gitlab::VisibilityLevel, Settings.gitlab['restricted_visibility_levels'], [])
+ Settings.gitlab['restricted_visibility_levels'] = Settings.__send__(:verify_constant_array, Gitlab::VisibilityLevel, Settings.gitlab['restricted_visibility_levels'], [])
