6a9bf12ab1626d398b215362d1fcb2c10497b747
thierry <thierry@FreeBSD.org>
2017-07-17 17:43:23 +0000

    Upgrade Kicad to 4.0.7 (the latest stable release).
    The libraries are released separately: create a new port cad/kicad-library.

--- cad/kicad/files/patch-CMakeModules_config.h.cmake
+++ cad/kicad/files/patch-CMakeModules_config.h.cmake
@@ -1,6 +1,6 @@
---- ./CMakeModules/config.h.cmake.orig	2010-04-13 20:11:02.000000000 +0200
-+++ ./CMakeModules/config.h.cmake	2010-05-23 14:54:37.000000000 +0200
-@@ -32,7 +32,7 @@
+--- CMakeModules/config.h.cmake.orig	2017-07-14 19:17:29 UTC
++++ CMakeModules/config.h.cmake
+@@ -36,7 +36,7 @@
  #cmakedefine HAVE_ISO646_H
  
  #if defined( HAVE_ISO646_H )
@@ -8,4 +8,4 @@
 +#include <ciso646>
  #endif
  
- #cmakedefine HAVE_STRINGS_H
+ #if defined( HAVE_STRCASECMP )
