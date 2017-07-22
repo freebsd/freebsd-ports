8fa85c744abb2617016bea9e0e3591409eb184cf
jbeich <jbeich@FreeBSD.org>
2017-07-22 19:33:39 +0000

    multimedia/assimp: add patches for 4.0.0 forgotten in r446390
    PR:		220889
    Reported by:	vvd@unislabs.com
    Pointy hat to:	jbeich
    Submitted by:	Yuri Victorovich <yuri@rawbw.com> (maintainer)

---
+++ multimedia/assimp/files/patch-code_D3MFOpcPackage.cpp
@@ -0,0 +1,11 @@
+--- code/D3MFOpcPackage.cpp.orig	2017-07-18 17:03:26 UTC
++++ code/D3MFOpcPackage.cpp
+@@ -55,6 +55,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY O
+ #include <algorithm>
+ #include <cassert>
+ 
++#include <stdlib.h>
++
+ #include <contrib/unzip/unzip.h>
+ 
+ namespace Assimp {
