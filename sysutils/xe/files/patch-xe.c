dfe92b184ed6c428d23b62635ae08eaf9026c576
tobik <tobik@FreeBSD.org>
2017-07-25 11:46:38 +0000

    sysutils/xe: Update to 0.9
    Changes:	https://github.com/chneukirchen/xe/blob/v0.9/NEWS.md

--- sysutils/xe/files/patch-xe.c
+++ sysutils/xe/files/patch-xe.c
@@ -1,6 +1,6 @@
---- xe.c.orig	2017-04-13 15:28:38 UTC
+--- xe.c.orig	2017-07-17 14:20:26 UTC
 +++ xe.c
-@@ -12,6 +12,7 @@
+@@ -13,6 +13,7 @@
  
  #include <limits.h>
  #include <errno.h>
@@ -8,16 +8,16 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <string.h>
-@@ -99,7 +100,7 @@ mywait()
+@@ -114,7 +115,7 @@ my_child:
  		} else if (WEXITSTATUS(status) > 125) {
  			exit(WEXITSTATUS(status));
  		}
 -	} else if (WIFSIGNALED(status)) {
 +	} else if (WIFSIGNALED(status) && !(fflag && WTERMSIG(status) == SIGPIPE)) {
- 		fprintf(stderr, "xe: pid %d terminated by signal %d\n",
- 		    pid, WTERMSIG(status));
+ 		fprintf(stderr, "xe: job %ld [pid %ld] terminated by signal %d\n",
+ 		    children[i].iter, (long)pid, WTERMSIG(status));
  		exit(125);
-@@ -358,6 +359,7 @@ main(int argc, char *argv[], char *envp[
+@@ -472,6 +473,7 @@ main(int argc, char *argv[], char *envp[])
  			pusharg("/bin/sh");
  			pusharg("-c");
  			pusharg(sflag);
