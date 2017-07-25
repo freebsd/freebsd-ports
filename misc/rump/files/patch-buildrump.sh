a1d7299e08a23b04dba53b7da49ca64cbd1a543f
vanilla <vanilla@FreeBSD.org>
2017-07-25 15:07:27 +0000

    Fix building on gcc6
    PR:		219292
    Submitted by:	jbeich@, rakuco@
    Obtained from:	NetBSD

--- misc/rump/files/patch-buildrump.sh
+++ misc/rump/files/patch-buildrump.sh
@@ -1,5 +1,5 @@
---- buildrump.sh.orig	2013-12-14 17:51:57.000000000 +0800
-+++ buildrump.sh	2014-02-08 23:21:05.880728426 +0800
+--- buildrump.sh.orig	2013-12-14 09:51:57 UTC
++++ buildrump.sh
 @@ -893,6 +893,7 @@ evaltarget ()
  		;;
  	"freebsd")
