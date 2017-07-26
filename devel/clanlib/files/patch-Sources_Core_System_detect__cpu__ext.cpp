99aa507affd1a805981a868775dc0d525043d430
jbeich <jbeich@FreeBSD.org>
2017-07-26 18:51:40 +0000

    devel/clanlib: unbreak runtime on non-x86 after r446663
    $ methane
    /usr/local/lib/libclan23SWRender-2.3.so.1: Undefined symbol "_ZN9CL_System20detect_cpu_extensionENS_19CL_CPU_ExtensionX86E"
    
    Pointy hat to:	jbeich

--- devel/clanlib/files/patch-Sources_Core_System_detect__cpu__ext.cpp
+++ devel/clanlib/files/patch-Sources_Core_System_detect__cpu__ext.cpp
@@ -1,15 +1,11 @@
 --- Sources/Core/System/detect_cpu_ext.cpp.orig	2011-11-15 12:15:42 UTC
 +++ Sources/Core/System/detect_cpu_ext.cpp
-@@ -30,6 +30,7 @@
+@@ -30,7 +30,7 @@
  #include "Core/precomp.h"
  #include "API/Core/System/system.h"
  
-+#ifndef CL_DISABLE_SSE2
- #ifdef CL_ARM_PLATFORM
+-#ifdef CL_ARM_PLATFORM
++#if defined(CL_ARM_PLATFORM) || defined(CL_DISABLE_SSE2)
  bool CL_System::detect_cpu_extension(CL_CPU_ExtensionPPC ext)
  {
-@@ -183,3 +184,4 @@ bool CL_System::detect_cpu_extension(CL_CPU_ExtensionX
- }
- 
- #endif
-+#endif // not CL_DISABLE_SSE2
+ 	throw ("Congratulations, you've just been selected to code this feature!");
