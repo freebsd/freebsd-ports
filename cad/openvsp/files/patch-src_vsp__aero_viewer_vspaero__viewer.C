0723d7344257f1b8bc02b4989690babd929c1976
tz <tz@FreeBSD.org>
2017-07-27 13:41:31 +0000

    cad/openvsp: Update from 3.12.1 to 3.13.0
    Features:
     - Update VSPAERO to v4.1 -- unsteady formulation and lots of fixes
     - Update VSPAERO integration to support v4.1
     - Add ellipsoid geom type
     - Add von Karman-Trefftz airfoil type
     - Add CST Airfoils to Fuselage and Stack
    
    Bug Fixes:
     - Fix bugs in parasite drag
     - Fix split surface problem for IGES/STEP export
     - Fix VSPAERO rotor induced velocity terms
     - Fix VSPAERO integration issues
     - Simplify file insert to avoid crashes
     - Fix bug where DisplayNames sometimes don't get updated
    
    Changelog: https://github.com/OpenVSP/OpenVSP/releases/tag/OpenVSP_3.13.0
    
    PR:           220951
    Submitted by: fernando.apesteguia@gmail.com

---
+++ cad/openvsp/files/patch-src_vsp__aero_viewer_vspaero__viewer.C
@@ -0,0 +1,11 @@
+--- src/vsp_aero/viewer/vspaero_viewer.C.orig	2017-07-22 07:05:17 UTC
++++ src/vsp_aero/viewer/vspaero_viewer.C
+@@ -7,7 +7,7 @@
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <FL/Fl.H>
+-#include "viewerUI.H"
++#include "viewerUI.h"
+ 
+ int ForceView = 0;
+ int TimeStamp = 0;
