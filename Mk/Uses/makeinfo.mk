f872930d0e52c8802d4085a53fd91d3af6e0ddad
Jung-uk Kim <jkim@FreeBSD.org>
2017-07-28 21:45:20 +0000

    Set MAKEINFO to the full path of makeinfo when USES=makeinfo is set.
    PR:		221027
    Reviewed by:	mat
    Approved by:	portmgr (antoine)

--- Mk/Uses/makeinfo.mk
+++ Mk/Uses/makeinfo.mk
@@ -15,8 +15,11 @@ _INCLUDE_USES_MAKEINFO_MK=	yes
 IGNORE=	USES=makeinfo - expects no arguments
 .endif
 
-.if !exists(/usr/bin/makeinfo)
+.if exists(/usr/bin/makeinfo)
+MAKEINFO?=	/usr/bin/makeinfo
+.else
 BUILD_DEPENDS+=	makeinfo:print/texinfo
+MAKEINFO?=	${LOCALBASE}/bin/makeinfo
 .endif
 
 .endif
