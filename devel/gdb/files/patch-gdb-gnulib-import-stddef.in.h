6f7a127b29dd4426af1d50845ec378ca570d10e5
jhb <jhb@FreeBSD.org>
2017-07-28 21:20:47 +0000

    Upgrade to GDB 8.0.
    - Commit b5430a3ced fixes parsing of 32-bit MIPS process cores.
    - kgdb has been ported to 8.0 including renaming the new OSABI constant,
      using gdb_bfd_openr, new return type of ldirname, and solib_add
      changed.
    - GDB 8.0 requires C++11, however the port is marked as requiring C++14
      as a workaround for bugs in clang 10.3.
    - patch-armfbsd is updated for the new OSABI constant names (a.out was
      removed in 8.0) and new filename convention (arm-fbsd-*)
    - patch-gdb-amd64-bsd-nat.c (renamed from patch-gdbamd64-bsd-nat.c) is
      simplified to just store the r_flags value before collecting registers
      from the register cache rather than a full copy of the entire register
      set.
    - patch-gdb-corelow.c has been removed.  It isn't really correct and if
      arm cores still cause core dumps the error is in arm-fbsd-tdep.c.
    - patch-gdb-i386-fbsd-nat.c renamed to account for rename of patched
      file.
    - patch-gdb-x86bsd-nat.c has been removed.  The file was renamed, but the
      stock file also compiled fine for me on i386.
    
    PR:		221025 (exp-run for 10.3)
    Submitted by:	luca.pizzamiglio@gmail.com (original version)
    Reviewed by:	luca.pizzamiglio@gmail.com (maintainer)
    Approved by:	brooks
    Differential Revision:	https://reviews.freebsd.org/D11465

--- devel/gdb/files/patch-gdb-gnulib-import-stddef.in.h
+++ devel/gdb/files/patch-gdb-gnulib-import-stddef.in.h
@@ -1,10 +1,12 @@
---- gdb/gnulib/import/stddef.in.h.orig	2016-10-07 23:33:10.529558000 -0700
-+++ gdb/gnulib/import/stddef.in.h	2016-10-07 23:33:23.824676000 -0700
-@@ -82,7 +82,7 @@
- #endif
- 
- /* Some platforms lack max_align_t.  */
--#if !@HAVE_MAX_ALIGN_T@
+diff --git gdb/gnulib/import/stddef.in.h gdb/gnulib/import/stddef.in.h
+index f4c4a1070d..023ea2064d 100644
+--- gdb/gnulib/import/stddef.in.h
++++ gdb/gnulib/import/stddef.in.h
+@@ -84,7 +84,7 @@
+ /* Some platforms lack max_align_t.  The check for _GCC_MAX_ALIGN_T is
+    a hack in case the configure-time test was done with g++ even though
+    we are currently compiling with gcc.  */
+-#if ! (@HAVE_MAX_ALIGN_T@ || defined _GCC_MAX_ALIGN_T)
 +#if 0
  /* On the x86, the maximum storage alignment of double, long, etc. is 4,
     but GCC's C11 ABI for x86 says that max_align_t has an alignment of 8,
