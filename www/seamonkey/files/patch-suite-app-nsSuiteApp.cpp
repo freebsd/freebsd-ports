5d0858b3111afc808b33210553b2df0d62baa22f
Mark Felder <feld@FreeBSD.org>
2017-08-03 14:53:52 +0000

    MFH: r446510
    www/seamonkey: update to 2.48
    
    Changes:	http://www.seamonkey-project.org/news
    Security:	d1853110-07f4-4645-895b-6fd462ad0589
    Security:	512c0ffd-cd39-4da4-b2dc-81ff4ba8e238
    Security:	e60169c4-aa86-46b0-8ae2-0d81f683df09
    
    Approved by:	port-secteam (with hat)

--- www/seamonkey/files/patch-suite-app-nsSuiteApp.cpp
+++ www/seamonkey/files/patch-suite-app-nsSuiteApp.cpp
@@ -1,12 +1,12 @@
---- suite/app/nsSuiteApp.cpp~
+--- suite/app/nsSuiteApp.cpp.orig	2017-07-07 05:15:27 UTC
 +++ suite/app/nsSuiteApp.cpp
-@@ -331,6 +331,9 @@ int main(int argc, char* argv[])
-   TriggerQuirks();
- #endif
+@@ -318,6 +318,9 @@ int main(int argc, char* argv[], char* envp[])
+ {
+   mozilla::TimeStamp start = mozilla::TimeStamp::Now();
  
 +  setenv("MOZ_GMP_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
 +  setenv("MOZ_PLUGIN_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
-+  
-   int gotCounters;
- #if defined(XP_UNIX)
-   struct rusage initialRUsage;
++
+ #ifdef HAS_DLL_BLOCKLIST
+   DllBlocklist_Initialize();
+ 
