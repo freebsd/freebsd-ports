3c8c48233d6f9576cfe1bf445231c9d624d7eb2b
Jason E. Hale <jhale@FreeBSD.org>
2017-08-04 08:44:56 +0000

    Update to 3.13.1
    PR:		221204
    Submitted by:	<fernando.apesteguia@gmail.com> (maintainer)

--- cad/openvsp/files/patch-src_vsp__graphic_src_Shader.cpp
+++
@@ -1,9 +0,0 @@
---- src/vsp_graphic/src/Shader.cpp.orig	2017-01-08 05:14:57 UTC
-+++ src/vsp_graphic/src/Shader.cpp
-@@ -1,5 +1,6 @@
- #include <assert.h>
- #include <string>
-+#include <stdlib.h>
- 
- #include "Shader.h"
- 
