00e134f83a40f61a57df32fe3f4ebf9846ec5d23
Antoine Brodin <antoine@FreeBSD.org>
2017-08-06 17:32:17 +0000

    Update to 20170802

--- sysutils/py-pytsk/files/patch-setup.py
+++ sysutils/py-pytsk/files/patch-setup.py
@@ -1,44 +1,43 @@
 # Unbundle sleuthkit and talloc
 
---- setup.py.orig	2017-01-28 14:19:35 UTC
+--- setup.py.orig	2017-08-01 06:49:05 UTC
 +++ setup.py
-@@ -169,9 +169,9 @@ class BuildExtCommand(build_ext):
+@@ -178,9 +178,9 @@ class BuildExtCommand(build_ext):
+   def run(self):
+     compiler = new_compiler(compiler=self.compiler)
+     # pylint: disable=attribute-defined-outside-init
+-    self.define = self.configure_source_tree(compiler)
++    self.define = [("HAVE_TSK_LIBTSK_H", "")]
  
-     def run(self):
-         compiler = new_compiler(compiler=self.compiler)
--        self.define = self.configure_source_tree(compiler)
-+        self.define = [("HAVE_TSK_LIBTSK_H", "")]
+-    libtsk_path = os.path.join("sleuthkit", "tsk")
++    libtsk_path = os.path.join("%%LOCALBASE%%", "include", "tsk")
  
--        libtsk_path = "sleuthkit/tsk"
-+        libtsk_path = "%%LOCALBASE%%/include/tsk"
+     if not os.access("pytsk3.c", os.R_OK):
+       # Generate the Python binding code (pytsk3.c).
+@@ -390,7 +390,7 @@ class ProjectBuilder(object):
+     self._argv = argv
  
-         if not os.access("pytsk3.c", os.R_OK):
-             # Generate the Python binding code (pytsk3.c).
-@@ -325,7 +325,7 @@ class ProjectBuilder(object):
-         self._argv = argv
+     # The path to the sleuthkit/tsk directory.
+-    self._libtsk_path = os.path.join("sleuthkit", "tsk")
++    self._libtsk_path = os.path.join("%%LOCALBASE%%", "include", "tsk")
  
-         # The path to the "tsk" directory.
--        self._libtsk_path = "sleuthkit/tsk"
-+        self._libtsk_path = "%%LOCALBASE%%/include/tsk"
+     # Paths under the sleuthkit/tsk directory which contain files we need
+     # to compile.
+@@ -400,13 +400,13 @@ class ProjectBuilder(object):
+     # The args for the extension builder.
+     self.extension_args = {
+         "define_macros": [],
+-        "include_dirs": ["talloc", self._libtsk_path, "sleuthkit", "."],
+-        "library_dirs": [],
+-        "libraries": []}
++        "include_dirs": [".", os.path.join("%%LOCALBASE%%", "include")],
++        "library_dirs": [os.path.join("%%LOCALBASE%%","lib")],
++        "libraries": ["tsk", "talloc"]}
  
-         # paths under the tsk/ directory which contain files we need to compile.
-         self._sub_library_names = "auto  base  docs  fs  hashdb  img vs".split()
-@@ -333,14 +333,14 @@ class ProjectBuilder(object):
-         # The args for the extension builder.
-         self.extension_args = dict(
-             define_macros=[],
--            include_dirs=["talloc", "sleuthkit/tsk", "sleuthkit", "."],
--            library_dirs=[],
--            libraries=[],
-+            include_dirs=[".", "%%LOCALBASE%%/include"],
-+            library_dirs=["%%LOCALBASE%%/lib"],
-+            libraries=["tsk", "talloc"],
-         )
+     # The sources to build.
+     self._source_files = [
+-        "class.c", "error.c", "tsk3.c", "pytsk3.c", "talloc/talloc.c"]
++        "class.c", "error.c", "tsk3.c", "pytsk3.c"]
  
-         # The sources to build.
-         self._source_files = ["class.c", "error.c", "tsk3.c",
--                              "pytsk3.c", "talloc/talloc.c"]
-+                              "pytsk3.c"]
- 
-         # Path to the top of the unpacked sleuthkit sources.
-         self._sleuthkit_path = "sleuthkit"
+     # Path to the top of the unpacked sleuthkit sources.
+     self._sleuthkit_path = "sleuthkit"
