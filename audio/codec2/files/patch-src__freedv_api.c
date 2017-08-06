ced2e5871947af9f89879e366b0ca6867567fdc5
Diane Bruce <db@FreeBSD.org>
2017-08-06 01:07:22 +0000

    Update to 0.7 1. Make the 700 bit/s codec sound better, to improve speech quality on    low SNR HF channels (beneath 0dB). 2. Develop a higher quality mode in the 2000 to 3000 bit/s range, that     can be used on HF channels with modest SNRs (around 10dB)

--- audio/codec2/files/patch-src__freedv_api.c
+++ audio/codec2/files/patch-src__freedv_api.c
@@ -1,4 +1,4 @@
---- src/freedv_api.c.orig	2017-02-07 04:36:38 UTC
+--- src/freedv_api.c.orig	2017-03-30 20:09:46 UTC
 +++ src/freedv_api.c
 @@ -36,7 +36,7 @@
  #ifdef __APPLE__
@@ -9,7 +9,7 @@
  #endif /* __APPLE__ */
  
  #include "fsk.h"
-@@ -258,7 +258,7 @@ struct freedv *freedv_open(int mode) {
+@@ -260,7 +260,7 @@ struct freedv *freedv_open(int mode) {
          f->n_codec_bits = codec2_bits_per_frame(f->codec2);
          nbit = f->n_codec_bits;
          nbyte = (nbit + 7) / 8;
@@ -18,7 +18,7 @@
          f->n_speech_samples = 2*codec2_samples_per_frame(f->codec2);
          f->n_codec_bits = codec2_bits_per_frame(f->codec2);
          nbit = f->n_codec_bits;
-@@ -504,7 +504,7 @@ void freedv_tx(struct freedv *f, short m
+@@ -506,7 +506,7 @@ void freedv_tx(struct freedv *f, short m
       
      if((f->mode == FREEDV_MODE_2400A) || (f->mode == FREEDV_MODE_2400B) || (f->mode == FREEDV_MODE_800XA)){
          /* 800XA has two codec frames per modem frame */
