08d630621c5e925670c1cef4ae285a2bb9aea637
Ben Woods <woodsb02@FreeBSD.org>
2017-08-06 00:31:24 +0000

    net/freerdp: Update to 2.0.0 RC0
    - Fix patches and post-patch to apply against new release
    - Bump PORTREVISION of dependent ports
    
    PR:		221240
    Approved by:	kevans (maintainer)

--- net/freerdp/files/patch-libfreerdp_crypto_tls.c
+++ net/freerdp/files/patch-libfreerdp_crypto_tls.c
@@ -1,32 +1,27 @@
---- libfreerdp/crypto/tls.c.orig	2016-11-24 14:45:22 UTC
+--- libfreerdp/crypto/tls.c.orig	2017-07-24 08:46:54 UTC
 +++ libfreerdp/crypto/tls.c
-@@ -218,9 +218,9 @@ static long bio_rdp_tls_ctrl(BIO* bio, i
- 		case BIO_CTRL_RESET:
- 			SSL_shutdown(tls->ssl);
+@@ -853,13 +853,13 @@ int tls_connect(rdpTls* tls, BIO* underlying)
+ 	if (!tls_prepare(tls, underlying, SSLv23_client_method(), options, TRUE))
+ 		return FALSE;
  
--			if (tls->ssl->handshake_func == tls->ssl->method->ssl_connect)
-+			if (SSL_in_connect_init(tls->ssl))
- 				SSL_set_connect_state(tls->ssl);
--			else if (tls->ssl->handshake_func == tls->ssl->method->ssl_accept)
-+			else if (SSL_in_accept_init(tls->ssl))
- 				SSL_set_accept_state(tls->ssl);
- 
- 			SSL_clear(tls->ssl);
-@@ -805,7 +805,7 @@ int tls_connect(rdpTls* tls, BIO* underl
+-#ifndef OPENSSL_NO_TLSEXT
++#if !defined(OPENSSL_NO_TLSEXT) && !defined(LIBRESSL_VERSION_NUMBER)
+ 	SSL_set_tlsext_host_name(tls->ssl, tls->hostname);
+ #endif
  	return tls_do_handshake(tls, TRUE);
  }
  
--#ifndef OPENSSL_NO_TLSEXT
-+#if !defined(OPENSSL_NO_TLSEXT) && !defined(LIBRESSL_VERSION_NUMBER)
+-#if defined(MICROSOFT_IOS_SNI_BUG) && !defined(OPENSSL_NO_TLSEXT)
++#if defined(MICROSOFT_IOS_SNI_BUG) && !defined(OPENSSL_NO_TLSEXT) && !defined(LIBRESSL_VERSION_NUMBER)
  static void tls_openssl_tlsext_debug_callback(SSL* s, int client_server,
          int type, unsigned char* data, int len, void* arg)
  {
-@@ -949,7 +949,7 @@ BOOL tls_accept(rdpTls* tls, BIO* underl
+@@ -1002,7 +1002,7 @@ BOOL tls_accept(rdpTls* tls, BIO* underlying, rdpSetti
  		return FALSE;
  	}
  
--#ifndef OPENSSL_NO_TLSEXT
-+#if !defined(OPENSSL_NO_TLSEXT) && !defined(LIBRESSL_VERSION_NUMBER)
- 	/**
- 	 * The Microsoft iOS clients eventually send a null or even double null
- 	 * terminated hostname in the SNI TLS extension!
+-#if defined(MICROSOFT_IOS_SNI_BUG) && !defined(OPENSSL_NO_TLSEXT)
++#if defined(MICROSOFT_IOS_SNI_BUG) && !defined(OPENSSL_NO_TLSEXT) && !defined(LIBRESSL_VERSION_NUMBER)
+ 	SSL_set_tlsext_debug_callback(tls->ssl, tls_openssl_tlsext_debug_callback);
+ #endif
+ 	return tls_do_handshake(tls, FALSE) > 0;
