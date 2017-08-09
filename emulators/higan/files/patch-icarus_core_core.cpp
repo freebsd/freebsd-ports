4c03e78cf67fed5eb5b9a8c5424893ebbe465220
Richard Gallamore <ultima@FreeBSD.org>
2017-08-09 07:41:23 +0000

    * Updated to 103 * Cleanup Makefile
    Changes: https://byuu.org/
    
    PR:		221136
    Submitted by:	Naram Qashat (maintainer)
    Reviewed by:	matthew (mentor), mat
    Approved by:	matthew (mentor)
    Differential Revision:	https://reviews.freebsd.org/D11891

--- emulators/higan/files/patch-icarus_core_core.cpp
+++ emulators/higan/files/patch-icarus_core_core.cpp
@@ -1,12 +1,13 @@
---- icarus/core/core.cpp.orig	2017-01-11 13:27:39 UTC
+--- icarus/core/core.cpp.orig	2017-01-23 13:51:10 UTC
 +++ icarus/core/core.cpp
-@@ -1,17 +1,17 @@
+@@ -1,18 +1,18 @@
  Icarus::Icarus() {
 -  database.famicom = BML::unserialize(string::read(locate("Database/Famicom.bml")));
 -  database.superFamicom = BML::unserialize(string::read(locate("Database/Super Famicom.bml")));
 -  database.masterSystem = BML::unserialize(string::read(locate("Database/Master System.bml")));
 -  database.megaDrive = BML::unserialize(string::read(locate("Database/Mega Drive.bml")));
 -  database.pcEngine = BML::unserialize(string::read(locate("Database/PC Engine.bml")));
+-  database.superGrafx = BML::unserialize(string::read(locate("Database/SuperGrafx.bml")));
 -  database.gameBoy = BML::unserialize(string::read(locate("Database/Game Boy.bml")));
 -  database.gameBoyColor = BML::unserialize(string::read(locate("Database/Game Boy Color.bml")));
 -  database.gameBoyAdvance = BML::unserialize(string::read(locate("Database/Game Boy Advance.bml")));
@@ -20,6 +21,7 @@
 +  database.masterSystem = BML::unserialize(string::read(locateShared("Database/Master System.bml")));
 +  database.megaDrive = BML::unserialize(string::read(locateShared("Database/Mega Drive.bml")));
 +  database.pcEngine = BML::unserialize(string::read(locateShared("Database/PC Engine.bml")));
++  database.superGrafx = BML::unserialize(string::read(locateShared("Database/SuperGrafx.bml")));
 +  database.gameBoy = BML::unserialize(string::read(locateShared("Database/Game Boy.bml")));
 +  database.gameBoyColor = BML::unserialize(string::read(locateShared("Database/Game Boy Color.bml")));
 +  database.gameBoyAdvance = BML::unserialize(string::read(locateShared("Database/Game Boy Advance.bml")));
