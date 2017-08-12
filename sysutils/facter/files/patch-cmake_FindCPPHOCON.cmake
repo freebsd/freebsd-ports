00ebb8887303ea338d8c0c55c350e4c4ac235ddb
romain <romain@FreeBSD.org>
2017-08-12 13:09:34 +0000

    Update to 3.7.1
    - Prepare the port to have a slave-port: java-facter;
    - Add a bunch of patches that where merged upstream but not yet released so
      that facter can be a drop-in replacement of rubygem-facter.
    
    With hat:	puppet

---
+++ sysutils/facter/files/patch-cmake_FindCPPHOCON.cmake
@@ -0,0 +1,12 @@
+
+$FreeBSD$
+
+--- cmake/FindCPPHOCON.cmake.orig	2017-06-27 17:42:22 UTC
++++ cmake/FindCPPHOCON.cmake
+@@ -1,5 +1,5 @@
+ include(FindDependency)
+-find_dependency(CPPHOCON DISPLAY "cpp-hocon" HEADERS "hocon/config.hpp" LIBRARIES "libcpp-hocon.a")
++find_dependency(CPPHOCON DISPLAY "cpp-hocon" HEADERS "hocon/config.hpp" LIBRARIES "liblibcpp-hocon.so")
+ 
+ include(FeatureSummary)
+ set_package_properties(CPPHOCON PROPERTIES DESCRIPTION "A C++ parser for the HOCON configuration language" URL "https://github.com/puppetlabs/cpp-hocon")
