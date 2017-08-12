4ca519aedb2470f8968241bd7684594a3e78e131
pi <pi@FreeBSD.org>
2017-08-12 18:05:42 +0000

    math/dynare: update 4.4.3 -> 4.5.1
    PR:		221342
    Relnotes:	https://forum.dynare.org/t/release-of-dynare-4-5-1/10431
    Submitted by:	Fernando Apesteguia <fernando.apesteguia@gmail.com> (maintainer)

--- math/dynare/files/patch-dynare++_parser_cc_parser__exception.h
+++ math/dynare/files/patch-dynare++_parser_cc_parser__exception.h
@@ -1,4 +1,4 @@
---- dynare++/parser/cc/parser_exception.h.orig	2013-11-08 16:37:59 UTC
+--- dynare++/parser/cc/parser_exception.h.orig	2017-07-24 09:58:59 UTC
 +++ dynare++/parser/cc/parser_exception.h
 @@ -6,6 +6,7 @@
  #define OG_FORMULA_PARSER_H
@@ -6,5 +6,5 @@
  #include <string>
 +#include <stdio.h>
  
- namespace ogp {
- 	using std::string;
+ namespace ogp
+ {
