f9a16e069c6724a3ac88928f4495e8148c5304e3
Kurt Jaeger <pi@FreeBSD.org>
2017-08-12 18:12:38 +0000

    MFH: r447843 r447844
    math/dynare: Add missing dependencies
    
    - Regenerated patches with "make makepatch" to avoid portlint warnings
    - Added LICENSE
    - Added missing library dependencies
    
    PR:		221316
    Submitted by:	Fernando Apesteguia <fernando.apesteguia@gmail.com> (maintainer)
    Approved by:	portmgr (fix dependencies blanket)

---
+++ math/dynare/files/patch-dynare++_parser_cc_parser__exception.h
@@ -0,0 +1,10 @@
+--- dynare++/parser/cc/parser_exception.h.orig	2013-11-08 16:37:59 UTC
++++ dynare++/parser/cc/parser_exception.h
+@@ -6,6 +6,7 @@
+ #define OG_FORMULA_PARSER_H
+ 
+ #include <string>
++#include <stdio.h>
+ 
+ namespace ogp {
+ 	using std::string;
