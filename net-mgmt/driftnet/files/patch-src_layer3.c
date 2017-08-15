ee5773d5747daa69908a1f3c7d5ef7528ea2ff4c
Emanuel Haupt <ehaupt@FreeBSD.org>
2017-08-15 13:18:28 +0000

    Update to 1.1.5

--- net-mgmt/driftnet/files/patch-src_layer3.c
+++ net-mgmt/driftnet/files/patch-src_layer3.c
@@ -1,15 +1,16 @@
---- src/layer3.c.orig	2013-05-12 15:16:43 UTC
+--- src/layer3.c.orig	2014-09-14 19:16:31 UTC
 +++ src/layer3.c
-@@ -3,8 +3,12 @@
+@@ -10,6 +10,13 @@
  #include <string.h>
  #include <assert.h>
  
++#ifdef __FreeBSD__
 +#include <sys/types.h>
 +#include <netinet/in_systm.h>
 +#include <netinet/in.h>
++#include <sys/socket.h>
++#endif
++
  #include <netinet/ip.h>
  #include <netinet/ip6.h>
-+#include <sys/socket.h>
  
- #include "log.h"
- #include "layer3.h"
