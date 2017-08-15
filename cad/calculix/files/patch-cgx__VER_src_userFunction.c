0796f58254927f7d6931fee5528ceeb46bc80815
Pedro F. Giffuni <pfg@FreeBSD.org>
2017-08-15 00:18:22 +0000

    cad/calculix: Split the ccx analyzer out of the main port.
    CalculiX has alwasy been comoposed of two independent packages: ccx and
    cgx. Ccx, the main tool for doing FEA, depends on fortran and currently
    has to be built with GCC. Cgx, on the other hand, uses OpenGL and the C++
    libraries it depends on are built with our native clang so buuilding it
    with GCC causes C++ library conflicts.
    
    By making ccx an independent port we can better focus on the specific
    requirements of each tool and avoid compiler-related conflicts. This also
    helps the port work again with newer versions GCC.
    
    PR:	219290. 22043, 220434
    Approved by:	gerald

---
+++ cad/calculix/files/patch-cgx__VER_src_userFunction.c
@@ -0,0 +1,28 @@
+--- cgx_2.12/src/userFunction.c.orig	2017-07-01 17:49:32 UTC
++++ cgx_2.12/src/userFunction.c
+@@ -57,7 +57,12 @@ void sendFacesNodes( char *setname );
+ /*                                                                       */
+ /*                                                                       */
+ /* --------------------------------------------------------------------  */
+-                                                                
++
++  int assembleFEM(char *parameters, Summen   *sum, SumGeo   *sumGeo ) { return(0); }
++  int assembleFEMVane(char *parameters, Summen   *sum, SumGeo   *sumGeo ) { return(0); }
++  int createAF(char *sections, char *parameters, Summen   *sum, SumGeo   *sumGeo ){ return(0); }
++  int createAF_nlb(char *sections, char *parameters, Summen   *sum, SumGeo   *sumGeo ){ return(0); }
++  int sendFlutter(Summen *sum, char string[2][MAX_LINE_LENGTH] ) { return(0); }                                                                  
+ void userFunction(char *string, Summen   *sum, SumGeo   *sumGeo )
+ {                                                               
+   int i,j,k,l,n;
+@@ -89,11 +94,6 @@ void userFunction(char *string, Summen  
+   // aflib addons
+   char buf[12][MAX_LINE_LENGTH];                                            
+ 
+-  int assembleFEM(char *parameters, Summen   *sum, SumGeo   *sumGeo ) { return(0); }
+-  int assembleFEMVane(char *parameters, Summen   *sum, SumGeo   *sumGeo ){ return(0); }
+-  int createAF(char *sections, char *parameters, Summen   *sum, SumGeo   *sumGeo ){ return(0); }
+-  int createAF_nlb(char *sections, char *parameters, Summen   *sum, SumGeo   *sumGeo ){ return(0); }
+-  int sendFlutter(Summen *sum, char string[2][MAX_LINE_LENGTH] ) { return(0); }                  
+   // end
+ 
+   /* list the implemented user-functions if no parameter was provided for the "call" command */
