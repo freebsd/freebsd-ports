16be2daabd64f8e2e610d69a37c16a8f749f46b6
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2017-08-20 12:26:34 +0000

    Pet portlint: move BROKEN_* upward
    - Update patch file

--- devel/protobuf/files/patch-src-google-protobuf-stubs-atomicops.h
+++ devel/protobuf/files/patch-src-google-protobuf-stubs-atomicops.h
@@ -1,11 +1,11 @@
---- src/google/protobuf/stubs/atomicops.h.orig	2016-09-24 02:12:45 UTC
+--- src/google/protobuf/stubs/atomicops.h.orig	2017-08-15 23:39:12 UTC
 +++ src/google/protobuf/stubs/atomicops.h
-@@ -202,6 +202,8 @@ Atomic64 Release_Load(volatile const Ato
+@@ -198,6 +198,8 @@ Atomic64 Release_Load(volatile const Ato
  #elif defined(__GNUC__)
  #if defined(GOOGLE_PROTOBUF_ARCH_IA32) || defined(GOOGLE_PROTOBUF_ARCH_X64)
  #include <google/protobuf/stubs/atomicops_internals_x86_gcc.h>
 +#elif defined(GOOGLE_PROTOBUF_ARCH_ARM) && defined(__FreeBSD__)
 +#include <google/protobuf/stubs/atomicops_internals_generic_gcc.h>
  #elif defined(GOOGLE_PROTOBUF_ARCH_ARM) && defined(__linux__)
- #include <google/protobuf/stubs/atomicops_internals_arm_gcc.h>
- #elif defined(GOOGLE_PROTOBUF_ARCH_AARCH64)
+ #if (((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7)) || (__GNUC__ > 4))
+ #include <google/protobuf/stubs/atomicops_internals_generic_gcc.h>
