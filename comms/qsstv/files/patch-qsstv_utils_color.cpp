dfc5c78aeb12173dec7c045af80fd68906d8d112
sunpoet <sunpoet@FreeBSD.org>
2017-08-20 09:48:39 +0000

    Update graphics/openjpeg to 2.2.0
    Changes:	https://github.com/uclouvain/openjpeg/blob/master/CHANGELOG.md
    PR:		221478
    Exp-run by:	antoine

---
+++ comms/qsstv/files/patch-qsstv_utils_color.cpp
@@ -0,0 +1,11 @@
+--- qsstv/utils/color.cpp.orig	2016-08-23 19:39:12 UTC
++++ qsstv/utils/color.cpp
+@@ -42,7 +42,7 @@
+ #include <assert.h>
+ 
+ //#include "opj_apps_config.h"
+-#include "openjpeg-2.1/openjpeg.h"
++#include "openjpeg-2.2/openjpeg.h"
+ #include "color.h"
+ 
+ 
