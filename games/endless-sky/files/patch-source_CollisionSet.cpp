830f9fabcb9a9782658fe9df19c5a21ab19cbf5b
amdmi3 <amdmi3@FreeBSD.org>
2017-08-21 12:44:09 +0000

    - Update to 0.9.8

--- games/endless-sky/files/patch-source_CollisionSet.cpp
+++ games/endless-sky/files/patch-source_CollisionSet.cpp
@@ -8,14 +8,3 @@
  
  using namespace std;
  
-@@ -179,8 +180,8 @@ Body *CollisionSet::Line(const Projectil
- 	int stepX = (x <= endX ? 1 : -1);
- 	int stepY = (y <= endY ? 1 : -1);
- 	// Calculate the slope of the line, shifted so it is positive in both axes.
--	int mx = abs(endX - x);
--	int my = abs(endY - y);
-+	int mx = std::abs(endX - x);
-+	int my = std::abs(endY - y);
- 	// Behave as if each grid cell has this width and height. This guarantees
- 	// that we only need to work with integer coordinates.
- 	int scale = max(mx, 1) * max(my, 1);
