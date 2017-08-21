ca33aa17f371e9b24bce9d7a26703a6f9ae8d218
Romain Tartière <romain@FreeBSD.org>
2017-08-21 18:47:52 +0000

    Update to 3.8.0
    Remove a bunch of patches merged upstream.
    Add some new patches to use paths that better fit FreeBSD:
      Configuration -> /usr/local/etc/facter/facter.conf
      Custom facts  -> /usr/local/etc/facter/facts.d/
      Cached facts  -> /var/facter/cache/cached_facts/

---
+++ sysutils/facter/files/patch-lib_src_util_config_posix_config.cc
@@ -0,0 +1,13 @@
+
+$FreeBSD$
+
+--- lib/src/util/config/posix/config.cc.orig
++++ lib/src/util/config/posix/config.cc
+@@ -7,6 +7,6 @@
+     }
+ 
+     std::string default_config_location() {
+-        return "/etc/puppetlabs/facter/facter.conf";
++        return "%%PREFIX%%/etc/facter/facter.conf";
+     }
+ }}}  // namespace facter::util::config
