212056367b28056afc7702fbe11fef0d918cf928
Jan Beich <jbeich@FreeBSD.org>
2017-08-22 13:01:20 +0000

    devel/android-tools-{adb,fastboot}: update to 8.0.0.r4 (oreo)
    Changes:	https://android.googlesource.com/platform/system/core/+log/android-7.1.2_r17..android-8.0.0_r4/adb
    Changes:	https://android.googlesource.com/platform/system/core/+log/android-7.1.2_r17..android-8.0.0_r4/fastboot

--- devel/android-tools-adb-devel/files/patch-adb_client_usb__libusb.cpp
+++ devel/android-tools-adb-devel/files/patch-adb_client_usb__libusb.cpp
@@ -1,26 +1,5 @@
 --- adb/client/usb_libusb.cpp.orig	2017-06-20 10:50:27 UTC
 +++ adb/client/usb_libusb.cpp
-@@ -28,7 +28,7 @@
- #include <thread>
- #include <unordered_map>
- 
--#include <libusb/libusb.h>
-+#include <libusb.h>
- 
- #include <android-base/file.h>
- #include <android-base/logging.h>
-@@ -89,7 +89,11 @@ struct transfer_info {
- };
- 
- namespace libusb {
-+#if defined(__linux__) || defined(__APPLE__)
- struct usb_handle : public ::usb_handle {
-+#else
-+struct usb_handle {
-+#endif
-     usb_handle(const std::string& device_address, const std::string& serial,
-                unique_device_handle&& device_handle, uint8_t interface, uint8_t bulk_in,
-                uint8_t bulk_out, size_t zero_mask, size_t max_packet_size)
 @@ -152,7 +156,14 @@ struct usb_handle : public ::usb_handle {
  static auto& usb_handles = *new std::unordered_map<std::string, std::unique_ptr<usb_handle>>();
  static auto& usb_handles_mutex = *new std::mutex();
