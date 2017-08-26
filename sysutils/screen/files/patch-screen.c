243e8be544e795700ed4802f29529a1a8b7a2310
Cy Schubert <cy@FreeBSD.org>
2017-08-26 03:50:33 +0000

    Update 4.5.0 --> 4.6.1

--- sysutils/screen/files/patch-screen.c
+++ sysutils/screen/files/patch-screen.c
@@ -1,21 +1,6 @@
---- screen.c.orig	2017-01-17 11:28:29.397404660 -0800
-+++ screen.c	2017-01-24 17:31:24.342944000 -0800
-@@ -674,11 +674,14 @@
-               if (strlen(screenlogfile) > PATH_MAX)
-                 Panic(0, "-L: logfile name too long. (max. %d char)", PATH_MAX);
- 
-+#if 0
-+/* see https://lists.gnu.org/archive/html/screen-devel/2017-01/msg00025.html */
-               FILE *w_check;
-               if ((w_check = fopen(screenlogfile, "w")) == NULL)
-                 Panic(0, "-L: logfile name access problem");
-               else
-                 fclose(w_check);
-+#endif
-             }
-             nwin_options.Lflag = 1;
-             break;
-@@ -2222,7 +2225,7 @@
+--- screen.c.orig	2017-07-10 12:26:25.000000000 -0700
++++ screen.c	2017-08-25 20:20:20.471073000 -0700
+@@ -2214,7 +2214,7 @@
    pn2 = pn = p + padlen;
    r = winmsg_numrend;
    while (p >= buf) {
