1d47d649aaf13f7f4906c01df488bace675adbe1
lifanov <lifanov@FreeBSD.org>
2017-09-05 13:31:26 +0000

    update sysutils/docker-compose to 1.16.1

--- sysutils/docker-compose/files/patch-setup.py
+++ sysutils/docker-compose/files/patch-setup.py
@@ -1,13 +1,13 @@
---- setup.py.orig	2017-07-25 23:05:03 UTC
+--- setup.py.orig	2017-09-05 13:29:08 UTC
 +++ setup.py
-@@ -34,8 +34,8 @@ install_requires = [
-     'colorama >= 0.3.7, < 0.4',
+@@ -33,8 +33,8 @@ install_requires = [
+     'cached-property >= 1.2.0, < 2',
      'docopt >= 0.6.1, < 0.7',
      'PyYAML >= 3.10, < 4',
 -    'requests >= 2.6.1, != 2.11.0, < 2.12',
--    'texttable >= 0.8.1, < 0.9',
+-    'texttable >= 0.9.0, < 0.10',
 +    'requests >= 2.6.1, != 2.11.0, < 3',
-+    'texttable >= 0.8.1, < 1',
++    'texttable >= 0.9.0, < 1',
      'websocket-client >= 0.32.0, < 1.0',
-     'docker >= 2.4.2, < 3.0',
+     'docker >= 2.5.1, < 3.0',
      'dockerpty >= 0.4.1, < 0.5',
