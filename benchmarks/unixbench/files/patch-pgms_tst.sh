1c9863b704528439a4827b751d10f2e411f5f71e
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2017-09-05 07:53:13 +0000

    benchmarks/unixbench: updating to version 5.1.3
    While I'm here:
    * updating MAINTAINER entry
    * adding the proper dependencies
    
    Reviewed by:	mat
    Approved by:	olivier (mentor)
    Differential Revision:	https://reviews.freebsd.org/D12224

--- benchmarks/unixbench/files/patch-pgms_tst.sh
+++ benchmarks/unixbench/files/patch-pgms_tst.sh
@@ -1,11 +1,11 @@
---- pgms/tst.sh.orig	1995-11-27 17:07:03 UTC
+--- pgms/tst.sh.orig	2015-06-04 17:20:18 UTC
 +++ pgms/tst.sh
 @@ -14,7 +14,7 @@
  #
  ###############################################################################
  ID="@(#)tst.sh:3.4 -- 5/15/91 19:30:24";
 -sort >sort.$$ <sort.src
-+sort >sort.$$ < ${TESTDIR}/sort.src
- od sort.$$ | sort -n +1 > od.$$
++sort >sort.$$ < ${UB_TESTDIR}/sort.src
+ od sort.$$ | sort -n -k 1 > od.$$
  grep the sort.$$ | tee grep.$$ | wc > wc.$$
  rm sort.$$ grep.$$ od.$$ wc.$$
