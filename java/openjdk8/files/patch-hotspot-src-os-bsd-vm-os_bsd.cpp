d804ee50fd813231d464f47e85f71732cde0145b
jkim <jkim@FreeBSD.org>
2017-09-07 00:41:34 +0000

    Fix available memory calculation on head after r309017.
    Note PORTREVISION is not bumped because it only affects a kernel without
    "options COMPAT_FREEBSD11".

--- java/openjdk8/files/patch-hotspot-src-os-bsd-vm-os_bsd.cpp
+++ java/openjdk8/files/patch-hotspot-src-os-bsd-vm-os_bsd.cpp
@@ -1,6 +1,16 @@
---- ./hotspot/src/os/bsd/vm/os_bsd.cpp	2016-06-01 20:57:38.276230000 -0500
-+++ ./hotspot/src/os/bsd/vm/os_bsd.cpp	2016-06-01 20:56:46.380667000 -0500
-@@ -767,6 +767,11 @@
+--- hotspot/src/os/bsd/vm/os_bsd.cpp.orig	2017-09-06 22:12:44 UTC
++++ hotspot/src/os/bsd/vm/os_bsd.cpp
+@@ -189,7 +189,9 @@ julong os::Bsd::available_memory() {
+ #ifdef __FreeBSD__
+   static const char *vm_stats[] = {
+     "vm.stats.vm.v_free_count",
++#if __FreeBSD_version < 1200016
+     "vm.stats.vm.v_cache_count",
++#endif
+     "vm.stats.vm.v_inactive_count"
+   };
+   size_t size;
+@@ -775,6 +777,11 @@ static void *java_start(Thread *thread)
    guarantee(unique_thread_id != 0, "unique thread id was not found");
    osthread->set_unique_thread_id(unique_thread_id);
  #endif
@@ -12,7 +22,7 @@
    // initialize signal mask for this thread
    os::Bsd::hotspot_sigmask(thread);
  
-@@ -930,6 +935,11 @@
+@@ -938,6 +945,11 @@ bool os::create_attached_thread(JavaThre
    guarantee(unique_thread_id != 0, "just checking");
    osthread->set_unique_thread_id(unique_thread_id);
  #endif
