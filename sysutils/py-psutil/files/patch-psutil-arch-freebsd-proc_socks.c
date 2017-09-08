f185307ed437c62059bfb54cd474288a1358bd43
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2017-09-08 21:51:56 +0000

    Update ino64 support
    Submitted by:	Andrey V. Elsukov <bu7cher@yandex.ru>

--- sysutils/py-psutil/files/patch-psutil-arch-freebsd-proc_socks.c
+++ sysutils/py-psutil/files/patch-psutil-arch-freebsd-proc_socks.c
@@ -1,6 +1,6 @@
 --- psutil/arch/freebsd/proc_socks.c.orig	2017-08-03 16:14:27 UTC
 +++ psutil/arch/freebsd/proc_socks.c
-@@ -136,7 +136,11 @@ psutil_search_tcplist(char *buf, struct 
+@@ -136,20 +136,36 @@ psutil_search_tcplist(char *buf, struct 
          if (kif->kf_sock_domain == AF_INET) {
              if (!psutil_sockaddr_matches(
                      AF_INET, inp->inp_lport, &inp->inp_laddr,
@@ -12,7 +12,12 @@
                  continue;
              if (!psutil_sockaddr_matches(
                      AF_INET, inp->inp_fport, &inp->inp_faddr,
-@@ -145,7 +149,11 @@ psutil_search_tcplist(char *buf, struct 
++#if __FreeBSD_version >= 1200031
++                    &kif->kf_un.kf_sock.kf_sa_peer))
++#else
+                     &kif->kf_sa_peer))
++#endif
+                 continue;
          } else {
              if (!psutil_sockaddr_matches(
                      AF_INET6, inp->inp_lport, &inp->in6p_laddr,
@@ -24,7 +29,15 @@
                  continue;
              if (!psutil_sockaddr_matches(
                      AF_INET6, inp->inp_fport, &inp->in6p_faddr,
-@@ -243,7 +251,11 @@ psutil_proc_connections(PyObject *self, 
++#if __FreeBSD_version >= 1200031
++                    &kif->kf_un.kf_sock.kf_sa_peer))
++#else
+                     &kif->kf_sa_peer))
++#endif
+                 continue;
+         }
+ 
+@@ -243,19 +259,35 @@ psutil_proc_connections(PyObject *self, 
                  inet_ntop(
                      kif->kf_sock_domain,
                      psutil_sockaddr_addr(kif->kf_sock_domain,
@@ -36,7 +49,13 @@
                      lip,
                      sizeof(lip));
                  inet_ntop(
-@@ -253,7 +265,11 @@ psutil_proc_connections(PyObject *self, 
+                     kif->kf_sock_domain,
+                     psutil_sockaddr_addr(kif->kf_sock_domain,
++#if __FreeBSD_version >= 1200031
++                                         &kif->kf_un.kf_sock.kf_sa_peer),
++#else
+                                          &kif->kf_sa_peer),
++#endif
                      rip,
                      sizeof(rip));
                  lport = htons(psutil_sockaddr_port(kif->kf_sock_domain,
@@ -46,9 +65,15 @@
                                                     &kif->kf_sa_local));
 +#endif
                  rport = htons(psutil_sockaddr_port(kif->kf_sock_domain,
++#if __FreeBSD_version >= 1200031
++                                                   &kif->kf_un.kf_sock.kf_sa_peer));
++#else
                                                     &kif->kf_sa_peer));
++#endif
  
-@@ -287,7 +303,11 @@ psutil_proc_connections(PyObject *self, 
+                 // construct python tuple/list
+                 py_laddr = Py_BuildValue("(si)", lip, lport);
+@@ -287,7 +319,11 @@ psutil_proc_connections(PyObject *self, 
              else if (kif->kf_sock_domain == AF_UNIX) {
                  struct sockaddr_un *sun;
  
