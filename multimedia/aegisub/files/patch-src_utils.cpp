c9cfb3a7f7905855bf8453f138a163dcdcc43373
jbeich <jbeich@FreeBSD.org>
2017-09-12 22:23:17 +0000

    multimedia/aegisub: unbreak with ICU 59.1
    src/utils.cpp:269:17: error: variable has incomplete type 'icu::UnicodeString'
                    UnicodeString ustr;
                                  ^
    /usr/local/include/unicode/locid.h:50:7: note: forward declaration of 'icu::UnicodeString'
    class UnicodeString;
          ^
    
    PR:		218788
    Obtained from:	upstream

---
+++ multimedia/aegisub/files/patch-src_utils.cpp
@@ -0,0 +1,12 @@
+https://github.com/Aegisub/Aegisub/commit/dd67db47cb22
+
+--- src/utils.cpp.orig	2014-12-08 00:07:09 UTC
++++ src/utils.cpp
+@@ -44,6 +44,7 @@
+ #include <boost/filesystem/path.hpp>
+ #include <map>
+ #include <unicode/locid.h>
++#include <unicode/unistr.h>
+ #include <wx/clipbrd.h>
+ #include <wx/filedlg.h>
+ #include <wx/stdpaths.h>
