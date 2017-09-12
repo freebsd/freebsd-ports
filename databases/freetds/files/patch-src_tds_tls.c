8b021350469744d3feb14c08c52aaf34f1f4ed1b
Bernard Spil <bernard.spil@hardenedbsd.org>
2017-09-12 21:18:03 +0200

    HBSD: databases/freetds: Fix build with LibreSSL
    Fixes #93
    
    Signed-off-by: Bernard Spil <bernard.spil@hardenedbsd.org>

--- databases/freetds/files/patch-src_tds_tls.c
+++ databases/freetds/files/patch-src_tds_tls.c
@@ -1,26 +1,20 @@
---- src/tds/tls.c.orig	2015-06-09 10:38:40 UTC
+--- src/tds/tls.c.orig	2017-02-15 09:00:01 UTC
 +++ src/tds/tls.c
-@@ -46,6 +46,10 @@
- #include <sys/stat.h>
- #endif /* HAVE_SYS_STAT_H */
- 
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
-+#include <sys/socket.h>
-+#endif
-+
- #include <freetds/tds.h>
- #include <freetds/string.h>
- #include <freetds/tls.h>
-@@ -72,6 +76,12 @@
- #define SSL_PTR ptr
+@@ -73,7 +73,7 @@
  #else
  
-+#ifdef LIBRESSL_VERSION_NUMBER
-+static pthread_mutex_t *openssllocks;
-+#undef OPENSSL_VERSION_NUMBER
-+#define OPENSSL_VERSION_NUMBER 0x1000107fL
-+#endif
-+
  /* some compatibility layer */
- #if OPENSSL_VERSION_NUMBER < 0x1010000FL
+-#if OPENSSL_VERSION_NUMBER < 0x1010000FL
++#if OPENSSL_VERSION_NUMBER < 0x1010000FL || defined(LIBRESSL_VERSION_NUMBER)
  static inline void
+ BIO_set_init(BIO *b, int init)
+ {
+@@ -590,7 +590,7 @@ tds_ssl_free(BIO *a)
+ 	return 1;
+ }
+ 
+-#if OPENSSL_VERSION_NUMBER < 0x1010000FL
++#if OPENSSL_VERSION_NUMBER < 0x1010000FL || defined(LIBRESSL_VERSION_NUMBER)
+ static BIO_METHOD tds_method_login[1] = {
+ {
+ 	BIO_TYPE_MEM,
