331bb14fb342f95b3aa3583ab2515665a9b1cac8
gahr <gahr@FreeBSD.org>
2017-09-13 11:49:01 +0000

    lang/io: add forgotten patch

---
+++ lang/io/files/patch-libs_iovm_source_IoVersion.h
@@ -0,0 +1,7 @@
+--- libs/iovm/source/IoVersion.h.orig	2017-09-13 11:47:59 UTC
++++ libs/iovm/source/IoVersion.h
+@@ -1,3 +1,3 @@
+ #ifndef IO_VERSION_STRING
+-#define IO_VERSION_STRING "20151111"
++#define IO_VERSION_STRING "20170906"
+ #endif
