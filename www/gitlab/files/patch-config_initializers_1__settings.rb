4a9989723db6937418568c5b47c809ac3955641f
Torsten Zuehlsdorff <tz@FreeBSD.org>
2017-09-13 14:15:08 +0000

    www/gitlab: Update from 9.2.10 to 9.3.10
    Changelog: https://github.com/gitlabhq/gitlabhq/blob/v9.3.10/CHANGELOG.md
    
    We now add full support for gitaly and add the basics for later support
    of gitlab-pages.
    
    Reviewed by: Matthias Fechner <idefix@fechner.net>

--- www/gitlab/files/patch-config_initializers_1__settings.rb
+++ www/gitlab/files/patch-config_initializers_1__settings.rb
@@ -1,6 +1,6 @@
---- config/initializers/1_settings.rb.orig	2017-06-07 20:48:31 UTC
+--- config/initializers/1_settings.rb.orig	2017-08-09 13:53:30 UTC
 +++ config/initializers/1_settings.rb
-@@ -215,12 +215,8 @@ Settings.gitlab['email_subject_suffix'] 
+@@ -216,12 +216,8 @@ Settings.gitlab['email_subject_suffix'] 
  Settings.gitlab['base_url']   ||= Settings.__send__(:build_base_gitlab_url)
  Settings.gitlab['url']        ||= Settings.__send__(:build_gitlab_url)
  Settings.gitlab['user']       ||= 'git'
