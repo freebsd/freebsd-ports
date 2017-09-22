e998e7666d0f8be38c5178214711f59aa41922ba
romain <romain@FreeBSD.org>
2017-09-22 12:28:15 +0000

    Update to 3.9.0
    While here, regenerate patches.
    
    With hat:	puppet

--- sysutils/facter/files/patch-lib_src_util_config_posix_config.cc
+++ sysutils/facter/files/patch-lib_src_util_config_posix_config.cc
@@ -1,6 +1,6 @@
---- lib/src/util/config/posix/config.cc.orig
+--- lib/src/util/config/posix/config.cc.orig	2017-09-12 21:43:10 UTC
 +++ lib/src/util/config/posix/config.cc
-@@ -7,6 +7,6 @@
+@@ -7,6 +7,6 @@ namespace facter { namespace util { name
      }
  
      std::string default_config_location() {
