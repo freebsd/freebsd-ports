1faef9ef46b8b7ce198603c54216c6b8fbeaabbd
Larry Rosenman <ler@FreeBSD.org>
2017-09-22 16:11:54 +0000

    security/clamav: Compile-time test fails during portinstall
    Apply patch from upstream.
    
    PR:	221513
    Submitted by:	jny@jny.dk, fsbruva@yahoo.com

--- security/clamav/files/patch-libclamav_c++_llvm_include_llvm_Support_CFG.h
+++ security/clamav/files/patch-libclamav_c++_llvm_include_llvm_Support_CFG.h
@@ -1,6 +1,6 @@
---- libclamav/c++/llvm/include/llvm/Support/CFG.h.orig
+--- libclamav/c++/llvm/include/llvm/Support/CFG.h.orig	2016-04-22 15:02:19 UTC
 +++ libclamav/c++/llvm/include/llvm/Support/CFG.h
-@@ -27,8 +27,9 @@
+@@ -27,8 +27,9 @@ namespace llvm {
  
  template <class Ptr, class USE_iterator> // Predecessor Iterator
  class PredIterator : public std::iterator<std::forward_iterator_tag,
@@ -12,15 +12,15 @@
    typedef PredIterator<Ptr, USE_iterator> Self;
    USE_iterator It;
  
-@@ -40,6 +41,7 @@
+@@ -40,6 +41,7 @@ class PredIterator : public std::iterator<std::forward
  
  public:
    typedef typename super::pointer pointer;
 +  typedef typename super::reference reference;
  
-   PredIterator() {}
    explicit inline PredIterator(Ptr *bb) : It(bb->use_begin()) {
-@@ -50,7 +52,7 @@
+     advancePastNonTerminators();
+@@ -49,7 +51,7 @@ class PredIterator : public std::iterator<std::forward
    inline bool operator==(const Self& x) const { return It == x.It; }
    inline bool operator!=(const Self& x) const { return !operator==(x); }
  
@@ -29,7 +29,7 @@
      assert(!It.atEnd() && "pred_iterator out of range!");
      return cast<TerminatorInst>(*It)->getParent();
    }
-@@ -100,10 +102,11 @@
+@@ -87,10 +89,11 @@ inline const_pred_iterator pred_end(const BasicBlock *
  
  template <class Term_, class BB_>           // Successor Iterator
  class SuccIterator : public std::iterator<std::bidirectional_iterator_tag,
@@ -43,7 +43,7 @@
    typedef SuccIterator<Term_, BB_> Self;
  
    inline bool index_is_valid(int idx) {
-@@ -112,6 +115,7 @@
+@@ -99,6 +102,7 @@ class SuccIterator : public std::iterator<std::bidirec
  
  public:
    typedef typename super::pointer pointer;
@@ -51,7 +51,7 @@
    // TODO: This can be random access iterator, only operator[] missing.
  
    explicit inline SuccIterator(Term_ T) : Term(T), idx(0) {// begin iterator
-@@ -142,7 +146,7 @@
+@@ -122,7 +126,7 @@ class SuccIterator : public std::iterator<std::bidirec
    inline bool operator==(const Self& x) const { return idx == x.idx; }
    inline bool operator!=(const Self& x) const { return !operator==(x); }
  
