7b6015bee43a91422b260b4ce8b16beb1275ea80
Mathieu Arnold <mat@FreeBSD.org>
2017-09-26 20:28:53 +0000

    Fix post-clean.
    Submitted by:	antoine
    Sponsored by:	Absolight

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -3730,7 +3730,7 @@ do-clean-noflavor:
 	@cd ${.CURDIR} && ${SETENV} ${MAKE} do-clean
 .if target(post-clean)
 CLEAN_DEPENDENCIES+=	post-clean-noflavor
-post-clean-${_f}:
+post-clean-noflavor:
 	@cd ${.CURDIR} &&  ${SETENV} ${MAKE} post-clean
 .endif
 .ORDER: ${CLEAN_DEPENDENCIES}
