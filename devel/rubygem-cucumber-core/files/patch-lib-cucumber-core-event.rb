04e207c2b4a58f7325e4929925fabfbb992d7df5
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2017-09-28 10:40:55 +0000

    Update to 3.0.0
    Changes:	https://github.com/cucumber/cucumber-ruby-core/blob/master/CHANGELOG.md
    		https://github.com/cucumber/cucumber-ruby-core/commits/master

---
+++ devel/rubygem-cucumber-core/files/patch-lib-cucumber-core-event.rb
@@ -0,0 +1,8 @@
+--- lib/cucumber/core/event.rb.orig	2017-09-28 08:34:06 UTC
++++ lib/cucumber/core/event.rb
+@@ -1,5 +1,3 @@
+-require 'backports/2.1.0/array/to_h'
+-
+ module Cucumber
+   module Core
+     class Event
