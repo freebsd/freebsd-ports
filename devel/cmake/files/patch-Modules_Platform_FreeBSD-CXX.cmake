e95a7286197a9e6683d0dca0e972ab042952b18b
Adriaan de Groot <adridg@FreeBSD.org>
2017-10-01 16:26:39 +0000

    Update CMake to 3.9.3
    This includes the upstream patches for the new Boost version (PR 222207, r450301).
    Thanks to antoine@ for the exp-run.
    
    PR:		222513
    Approved by:	rakuco (mentor), tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D12427

--- devel/cmake/files/patch-Modules_Platform_FreeBSD-CXX.cmake
+++ devel/cmake/files/patch-Modules_Platform_FreeBSD-CXX.cmake
@@ -1,5 +1,8 @@
 This prefers c++ over CC as the C++ compiler command. See PR 203793.
 
+Upstreamed as 6f666f4335c7f6b44fc4ccdc886590094fdfa16d
+Will be included in CMake 3.10
+
 --- Modules/Platform/FreeBSD-CXX.cmake.orig	2017-06-14 21:54:30 UTC
 +++ Modules/Platform/FreeBSD-CXX.cmake
 @@ -0,0 +1,3 @@
