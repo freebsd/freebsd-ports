6bd063cbac7a7a4fcf1dc9a52866235c8fc2da33
vanilla <vanilla@FreeBSD.org>
2017-10-06 05:47:18 +0000

    Update to 1.6.5.

--- devel/grpc/files/patch-src_core_tsi_ssl__transport__security.c
+++ devel/grpc/files/patch-src_core_tsi_ssl__transport__security.c
@@ -1,6 +1,6 @@
---- src/core/tsi/ssl_transport_security.c.orig	2017-08-05 03:36:13 UTC
+--- src/core/tsi/ssl_transport_security.c.orig	2017-10-03 19:53:08 UTC
 +++ src/core/tsi/ssl_transport_security.c
-@@ -35,6 +35,7 @@
+@@ -20,6 +20,7 @@
  
  #include <grpc/support/port_platform.h>
  
