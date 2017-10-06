9024ea4ea90673c263e47dd63c62c4ab53c8fea8
tobik <tobik@FreeBSD.org>
2017-10-06 11:18:35 +0000

    graphics/netpbm: Update to 10.80.00
    - This moves netpbm to the advanced release series, which brings the
      port closer to what is available on OpenBSD, Fedora, Arch Linux,
      openSUSE, etc. and will let us get rid of patches for supporting
      newer libpng versions.
    - The advanced release series is only available via an SVN checkout.
      Add two maintenance targets netpbm-version-check and netpbm-fetch
      to make fetching them easier.  To generate release tarballs the sources
      are then tagged and committed to https://github.com/t6/netpbm
    - Add STATIC option for linking all programs statically to allow running them
      in small chroots
    - Add option to disable adding a Perl run dependency.  Only a fraction
      of all programs need it.  Some are compatibility shims for newer
      programs which should be used instead.  The following programs require
      Perl: pamfixtrunc pbmtox10bm pgmcrater pnmflip pnmquant pnmquantall ppmfade
      ppmquant ppmrainbow ppmshadow
    - Disable building svgtopam by default.  It's the only program that
      requires libxml2 and is obsolete according to the author.
    - Install manpages by default
    - Allow running of the test suite via `make test`
    - Headers have been moved to ${LOCALBASE}/include/netpbm.  Fix ports
      that expect them to be in ${LOCALBASE}/include
    
    Changes:	https://raw.githubusercontent.com/t6/netpbm/v10.80.00/doc/HISTORY
    PR:		219982
    Reviewed by:	mat
    Exp-run by:	antoine
    Differential Revision:	https://reviews.freebsd.org/D11157

---
+++ graphics/netpbm/files/patch-lib_libpm.c
@@ -0,0 +1,19 @@
+--- lib/libpm.c.orig	2017-06-10 22:23:50 UTC
++++ lib/libpm.c
+@@ -18,6 +17,7 @@
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <stdarg.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <errno.h>
+ #include <setjmp.h>
+@@ -824,7 +824,7 @@ pm_arg0toprogname(const char arg0[]) {
+ unsigned int
+ pm_randseed(void) {
+ 
+-    return time(NULL) ^ getpid();
++    return arc4random();
+ 
+ }
+ 
---
+++ graphics/ppminfo/files/patch-pi.c
@@ -0,0 +1,11 @@
+--- pi.c.orig	2007-08-06 18:51:13 UTC
++++ pi.c
+@@ -1,6 +1,7 @@
+ #include <stdio.h>
+ #include <math.h>
+ #include <unistd.h>
++#include <sys/param.h>
+ #include <ppm.h>
+ 
+ #include "error.h"
+
