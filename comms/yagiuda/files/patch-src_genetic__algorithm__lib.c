7f932a1d601a60d1575e2952c4dff7dbc65cf4a4
Diane Bruce <db@FreeBSD.org>
2017-10-07 01:24:16 +0000

    Remove the two gets() calls and replace with fgets() https://reviews.freebsd.org/D12298 Whilst here redo the patches with make makepatch
    PR:		ports/222796
    Reported by:	emaste@

--- comms/yagiuda/files/patch-src_genetic__algorithm__lib.c
+++ comms/yagiuda/files/patch-src_genetic__algorithm__lib.c
@@ -1,17 +1,17 @@
---- src/genetic_algorithm_lib.c.orig	Tue Jul 19 12:25:24 2005
-+++ src/genetic_algorithm_lib.c	Tue Jul 19 12:35:51 2005
+--- src/genetic_algorithm_lib.c.orig	2000-10-28 13:04:39 UTC
++++ src/genetic_algorithm_lib.c
 @@ -3,9 +3,8 @@
  #endif
  #include <string.h>
-
+ 
 -#include <values.h>
 -#include <math.h>
  #include <sys/types.h>
 +#include <math.h>
  #include <errno.h>
  #include "yagi.h"
-
-@@ -245,7 +244,7 @@
+ 
+@@ -245,7 +244,7 @@ int Selection(FILE *fd, int gen)
  	GeneRecord *temp ;
  	double minfit,maxfit ;
  	sigma=0.0 ;
@@ -19,4 +19,4 @@
 +	minfit=MAXFLOAT;
  	maxfit=-minfit ;
  	/* minfit=1e308; maxfit=-1e308; XXXXXXXXXXXXXXXXXXXXXX */
- 	for(a=0 ; a<population_size ; a++ )
+ 	for(a=0 ; a<population_size ; a++ ) 
