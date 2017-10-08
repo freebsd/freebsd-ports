dd3acf914801f770a53b9176241a8192ff677015
madpilot <madpilot@FreeBSD.org>
2017-10-08 12:40:34 +0000

    - Update cad/freecad to 0.17.12018 - Use upstream version numbering - Reset maintainership on his request - While here, silence portlint warning about USES variable position
    PR:		222195
    Submitted by:	Thibault Jouan <tj+freebsd_ports@a13.fr> (maintainer)

---
+++ cad/freecad/files/version.h
@@ -0,0 +1,12 @@
+// Version Number
+#define FCVersionMajor "0"
+#define FCVersionMinor "17"
+#define FCVersionName  "Vulcan"
+// test: $Format:Hash (%H), Date: %ci$
+#define FCRevision      "12018 (Git)"      //Highest committed revision number
+#define FCRevisionDate  "2017/09/08 12:52:12"     //Date of highest committed revision
+#define FCRepositoryURL "git://github.com/FreeCAD/FreeCAD.git master"      //Repository URL of the working copy
+
+// Git relevant stuff
+#define FCRepositoryHash   "f27617e63e4cd0ea00dd307577abe465d9a709fa"
+#define FCRepositoryBranch "master"
