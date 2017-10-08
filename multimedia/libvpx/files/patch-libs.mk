242d0fcee77d1763edc50e05f32fa6c658d7eb94
jbeich <jbeich@FreeBSD.org>
2017-10-08 18:57:40 +0000

    multimedia/libvpx: unblock build on mips*
    mips* cannot use lang/gcc*, so let it use GCC 4.2 in base.
    Other archs may work as well but x86 won't per r422956.

--- multimedia/libvpx/files/patch-libs.mk
+++ multimedia/libvpx/files/patch-libs.mk
@@ -1,8 +1,19 @@
---- libs.mk.orig
+--- libs.mk.orig	2017-01-12 20:27:27 UTC
 +++ libs.mk
-@@ -232,8 +232,8 @@
- 	$(qexec)echo 'Libs: -L$${libdir} -lvpx' >> $@
- 	$(qexec)echo 'Libs.private: -lm -pthread' >> $@
+@@ -117,8 +117,8 @@ endif
+ $(BUILD_PFX)third_party/googletest/%.cc.o: CXXFLAGS += -Wno-missing-field-initializers
+ # Suppress -Wextra warnings in first party code pending investigation.
+ # https://bugs.chromium.org/p/webm/issues/detail?id=1069
+-$(BUILD_PFX)vp8/encoder/onyx_if.c.o: CFLAGS += -Wno-unknown-warning-option -Wno-clobbered
+-$(BUILD_PFX)vp8/decoder/onyxd_if.c.o: CFLAGS += -Wno-unknown-warning-option -Wno-clobbered
++$(BUILD_PFX)vp8/encoder/onyx_if.c.o: CFLAGS += -Wno-extra
++$(BUILD_PFX)vp8/decoder/onyxd_if.c.o: CFLAGS += -Wno-extra
+ 
+ ifeq ($(CONFIG_MSVS),yes)
+ CODEC_LIB=$(if $(CONFIG_STATIC_MSVCRT),vpxmt,vpxmd)
+@@ -339,8 +339,8 @@ else
+ 	$(qexec)echo 'Libs.private: -lm' >> $@
+ endif
  	$(qexec)echo 'Cflags: -I$${includedir}' >> $@
 -INSTALL-LIBS-yes += $(LIBSUBDIR)/pkgconfig/vpx.pc
 -INSTALL_MAPS += $(LIBSUBDIR)/pkgconfig/%.pc %.pc
