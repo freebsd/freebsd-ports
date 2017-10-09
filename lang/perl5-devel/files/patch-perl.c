2b27acf4b6a2bcda108a458b2a56d357ee38e71a
mat <mat@FreeBSD.org>
2017-10-09 15:57:06 +0000

    Update to v5.27.4-72-g1fd9130.
    Changes:	https://github.com/Perl/perl5/compare/v5.27.4-36-g95db2ef...v5.27.4-72-g1fd9130
    Sponsored by:	Absolight

--- lang/perl5-devel/files/patch-perl.c
+++ lang/perl5-devel/files/patch-perl.c
@@ -1,6 +1,6 @@
---- perl.c.orig	2017-09-11 05:29:19 UTC
+--- perl.c.orig	2017-10-09 11:42:02 UTC
 +++ perl.c
-@@ -1928,23 +1928,7 @@ S_Internals_V(pTHX_ CV *cv)
+@@ -1932,23 +1932,7 @@ S_Internals_V(pTHX_ CV *cv)
      PUSHs(Perl_newSVpvn_flags(aTHX_ non_bincompat_options,
  			      sizeof(non_bincompat_options) - 1, SVs_TEMP));
  
