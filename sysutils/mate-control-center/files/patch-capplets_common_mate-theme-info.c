f76e449c9891ebc08e56d6414995e8b89c779e99
kwm <kwm@FreeBSD.org>
2017-10-14 20:51:48 +0000

    Minor MATE 1.18 update.
    Apart from version updates, the highlights are:
     * fix orphaned file issues in atril/atril-lite.
     * Add licenses where required.
     * make sure the mime xml gets installed in mate-control-center.
     * regen the m-c-c patch, with make makepatch.

--- sysutils/mate-control-center/files/patch-capplets_common_mate-theme-info.c
+++ sysutils/mate-control-center/files/patch-capplets_common_mate-theme-info.c
@@ -1,7 +1,7 @@
---- capplets/common/mate-theme-info.c.orig	2009-05-03 20:30:56.000000000 +0200
-+++ capplets/common/mate-theme-info.c	2009-07-19 12:54:25.000000000 +0200
-@@ -1876,7 +1876,7 @@ mate_theme_init ()
- #ifdef XCURSOR_ICONDIR
+--- capplets/common/mate-theme-info.c.orig	2017-05-28 07:59:23 UTC
++++ capplets/common/mate-theme-info.c
+@@ -1819,7 +1819,7 @@ mate_theme_init ()
+ 
    /* if there's a separate xcursors dir, add that as well */
    if (strcmp (XCURSOR_ICONDIR, top_theme_dir_string) &&
 -      strcmp (XCURSOR_ICONDIR, "/usr/share/icons")) {
