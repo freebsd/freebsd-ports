1e51c028157a71c6148d78e48110b4723bf2fbe3
Alex Kozlov <ak@FreeBSD.org>
2017-10-19 13:47:42 +0000

    - Fix buffer overflow (CVE-2015-2782) - Fix absolute path directory traversal (CVE-2015-0557) - Fix symlink directory traversal (CVE-2015-0556) - Fix build on armv6 - Fix parallel build - Make build reproducible
    PR:	221589
    Submitted by:	mikael.urankar@gmail.com
    Obtained from:	debian patchset 16
    Approved by:	garga (maintainer)

---
+++ archivers/arj/files/patch-arjtypes.c
@@ -0,0 +1,15 @@
+--- arjtypes.c	2005-06-23 UTC
++++ arjtypes.c
+@@ -138,8 +138,11 @@ static int isleapyear(int year)
+ static unsigned long ts_unix2dos(const long ts)
+ {
+  struct tm *stm;
++ time_t _ts;
+ 
+- stm=arj_localtime((time_t*)&ts);
++ _ts = ts;
++
++ stm=arj_localtime(&_ts);
+  return(get_tstamp(stm->tm_year+1900, stm->tm_mon+1, stm->tm_mday,
+         stm->tm_hour, stm->tm_min, stm->tm_sec));
+ }
