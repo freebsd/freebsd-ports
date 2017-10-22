686811a35a49c5fbfad7839b8bbce55d132853f8
olgeni <olgeni@FreeBSD.org>
2017-10-22 08:08:27 +0000

    Update devel/elixir-decimal to version 1.4.1.

--- devel/elixir-decimal/files/patch-mix.exs
+++ devel/elixir-decimal/files/patch-mix.exs
@@ -1,10 +1,10 @@
---- mix.exs.orig	2016-10-03 08:27:45 UTC
+--- mix.exs.orig	2017-10-22 08:06:47 UTC
 +++ mix.exs
-@@ -7,7 +7,6 @@ defmodule Decimal.Mixfile do
-     [app: :decimal,
-      version: @version,
-      elixir: "~> 1.0",
--     deps: deps(),
-      name: "Decimal",
-      source_url: "https://github.com/ericmj/decimal",
-      docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
+@@ -8,7 +8,6 @@ defmodule Decimal.Mixfile do
+       app: :decimal,
+       version: @version,
+       elixir: "~> 1.0",
+-      deps: deps(),
+       name: "Decimal",
+       source_url: "https://github.com/ericmj/decimal",
+       docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
