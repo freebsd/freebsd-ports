b3b9dbd218022c62f3a6998d7e2b090960d15e2f
jbeich <jbeich@FreeBSD.org>
2017-10-24 18:52:05 +0000

    MFH: r451280 r451633 r452187
    www/seamonkey: update to 2.49.1
    
    - linux- version cannot be updated due to lack of gtk3 package
    
    Changes:	http://www.seamonkey-project.org/news
    PR:		222464
    Security:	5e0a038a-ca30-416d-a2f5-38cbf5e7df33
    Security:	6cec1b0a-da15-467d-8691-1dea392d4c8d
    Security:	555b244e-6b20-4546-851f-d8eb7d6c1ffa
    Security:	1098a15b-b0f6-42b7-b5c7-8a8646e8be07
    Approved by:	ports-secteam (swills)

--- Mk/Uses/gecko.mk
+++ Mk/Uses/gecko.mk
@@ -51,8 +51,8 @@ _GECKO_INSTALLED_VER:=	${_GECKO_INSTALLED_VER:M[0-9][0-9]*:C/([0-9][0-9]).*/\1/g
 
 .elif ${gecko_ARGS:Mseamonkey}
 
-_GECKO_DEFAULT_VERSION=	48
-_GECKO_VERSIONS=		48
+_GECKO_DEFAULT_VERSION=	49
+_GECKO_VERSIONS=		49
 _GECKO_TYPE=	seamonkey
 
 .if exists(${LOCALBASE}/bin/seamonkey)
@@ -61,7 +61,7 @@ _GECKO_INSTALLED_VER:=	${_GECKO_INSTALLED_VER:M[0-9]*:C/[0-9].([0-9][0-9]).*/\1/
 .endif
 
 # Dependence lines for different Seamonkey versions
-48_DEPENDS=		${LOCALBASE}/lib/seamonkey/seamonkey:www/seamonkey
+49_DEPENDS=		${LOCALBASE}/lib/seamonkey/seamonkey:www/seamonkey
 
 .elif ${gecko_ARGS:Mthunderbird}
 
