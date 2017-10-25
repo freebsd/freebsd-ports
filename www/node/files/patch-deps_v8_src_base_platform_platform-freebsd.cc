77fd25a9b66545c7788dcaa8c99ec9b7d28b60c6
bhughes <bhughes@FreeBSD.org>
2017-10-25 18:43:33 +0000

    www/node: Update 8.6.0 -> 8.8.0
    Update to the latest upstream v8.x release to address a remote DoS
    vulnerability.
    
    https://nodejs.org/en/blog/vulnerability/oct-2017-dos/
    https://nodejs.org/en/blog/release/v8.8.0/
    
    Remove post-patch rules to replace various occurrences of 'python' with
    PYTHON_CMD. Upstream has done a change to ensure that the "correct"
    python version is called at build time, making our post-patch steps
    unnecessary.
    
    Re-roll patches with 'make makepatch'.
    
    Approved by:	robak (mentor)
    MFH:		2017Q4
    Security:	CVE-2017-14919
    Differential Revision:	https://reviews.freebsd.org/D12689

--- www/node/files/patch-deps_v8_src_base_platform_platform-freebsd.cc
+++ www/node/files/patch-deps_v8_src_base_platform_platform-freebsd.cc
@@ -1,6 +1,6 @@
---- deps/v8/src/base/platform/platform-freebsd.cc.orig	2017-08-09 18:48:10 UTC
+--- deps/v8/src/base/platform/platform-freebsd.cc.orig	2017-10-24 19:40:14 UTC
 +++ deps/v8/src/base/platform/platform-freebsd.cc
-@@ -48,6 +48,48 @@ void* OS::Allocate(const size_t requested, size_t* all
+@@ -51,6 +51,48 @@ void* OS::Allocate(const size_t requested, size_t* all
    return mbase;
  }
  
