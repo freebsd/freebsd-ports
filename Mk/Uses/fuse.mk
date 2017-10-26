9a0d161937c4949b45016d47755cd98cdcb6267a
se <se@FreeBSD.org>
2017-10-26 15:37:02 +0000

    Add support for libfuse3 (sysutils/fusefs-libs3).
    USES=fuse:3 does add a dependency on version 3 of libfuse, which is not
    compatible with version 2 (which remains default version for now). Both
    versions can co-exist at build and run time.
    
    Reviewed by:	mat
    Approved by:	mat (portmgr)
    Differential Revision:	https://reviews.freebsd.org/D12694

--- Mk/Uses/fuse.mk
+++ Mk/Uses/fuse.mk
@@ -1,20 +1,26 @@
 # $FreeBSD$
 #
-# handle dependency on the fuse port
+# Handle dependency on the fuse port
 #
 # Feature:	fuse
-# Usage:	USES=fuse
-# Valid ARGS:	does not require args
-#
-# MAINTAINER: portmgr@FreeBSD.org
+# Usage:	USES=fuse[:version]
+# Valid ARGS:	2 [default], 3
+# MAINTAINER:	portmgr@FreeBSD.org
 
 .if !defined(_INCLUDE_USES_FUSE_MK)
 _INCLUDE_USES_FUSE_MK=	yes
 
 .if !empty(fuse_ARGS)
-IGNORE=	USES=fuse does not require args
+LIBFUSE_VER=	${fuse_ARGS}
 .endif
+LIBFUSE_VER?=	2
 
+.if ${LIBFUSE_VER} == 2
 LIB_DEPENDS+=	libfuse.so:sysutils/fusefs-libs
+.elif ${LIBFUSE_VER} == 3
+LIB_DEPENDS+=	libfuse3.so:sysutils/fusefs-libs3
+.else
+IGNORE=		cannot install: unknown FUSE library version: ${LIBFUSE_VERSION}
+.endif
 
 .endif
