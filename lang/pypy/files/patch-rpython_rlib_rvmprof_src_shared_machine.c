30aa1218a3ee992ef348c9e5ac07394ab50532ff
dbn <dbn@FreeBSD.org>
2017-11-04 09:33:51 +0000

    lang/pypy(3): update to 5.9
     - add lubunwind as dependency: required by vmprof
     - disable vmprof on FreeBSD 10: missing base support
     - implement support for handles with ctypes.CDLL: fix dynamic loading of
       shared objects
     - enable various tests that were not working on FreeBSD 9
     - mark get_profile_path as xfail on FreeBSD
     - handle definition of char16_t and char32_t on libc++ for
       __cplusplus < 201103L (libstdc++ does not do this)
     - other minor fixes
    
    ChangeLog:
     - NumPy and Pandas now work on lang/pypy
     - Cython 0.27.1 supports more projects with PyPy
     - JSON parser improvements decrease memory by up to 50% and increase speed
       by up to 15%
     - CFFI updated to 1.11.1

--- lang/pypy/files/patch-rpython_rlib_rvmprof_src_shared_machine.c
+++ lang/pypy/files/patch-rpython_rlib_rvmprof_src_shared_machine.c
@@ -1,20 +1,11 @@
---- rpython/rlib/rvmprof/src/shared/machine.c.orig	2017-06-05 20:40:44 UTC
+--- rpython/rlib/rvmprof/src/shared/machine.c.orig	2017-10-03 10:49:20 UTC
 +++ rpython/rlib/rvmprof/src/shared/machine.c
-@@ -28,6 +28,8 @@ const char * vmp_machine_os_name(void)
-     #endif
+@@ -28,7 +28,7 @@ const char * vmp_machine_os_name(void)
  #elif __linux__
      return "linux";
-+#elif __FreeBSD__
+ #elif __FreeBSD__
+-    return "freebsd"
 +    return "freebsd";
  #else
      #error "Unknown compiler"
- #endif
-@@ -39,7 +41,7 @@ long vmp_fd_to_path(int fd, char * buffe
-     char proffs[24];
-     (void)snprintf(proffs, 24, "/proc/self/fd/%d", fd);
-     return readlink(proffs, buffer, buffer_len);
--#elif defined(VMPROF_UNIX)
-+#elif defined(VMPROF_UNIX) && !defined(__FreeBSD__)
-     fcntl(fd, F_GETPATH, buffer);
-     return strlen(buffer);
  #endif
