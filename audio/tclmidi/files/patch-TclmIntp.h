d04163bbadc37610518d32752c923496f13225b4
rene <rene@FreeBSD.org>
2017-11-11 10:30:10 +0000

    Remove expired ports: 2017-11-04 lang/ats: Unmaintained, outdated, needs lots of refreshment 2017-11-09 games/lordsawar: Broken for more than 6 months 2017-11-09 databases/elixir-ecto_migrate: Broken for more than 6 months 2017-11-09 audio/gspeakers: Broken for more than 6 months 2017-11-09 net/p5-Filesys-SmbClient: Broken for more than 6 months 2017-11-09 audio/tclmidi: Broken for more than 6 months 2017-11-09 security/py-xmlsec: Broken for more than 6 months 2017-11-09 security/samba-virusfilter: Broken for more than 6 months 2017-11-09 textproc/ocaml-pxp: Broken for more than 6 months 2017-11-09 audio/sooperlooper: Broken for more than 6 months 2017-11-09 devel/dwarves: Broken for more than 6 months 2017-11-09 devel/cl-cffi-sbcl: Broken for more than 6 months 2017-11-09 devel/pinba_engine: Broken for more than 6 months 2017-11-09 devel/py-event: Broken for more than 6 months

--- audio/tclmidi/files/patch-TclmIntp.h
+++
@@ -1,9 +0,0 @@
---- TclmIntp.h.orig	1996-07-28 18:53:08 UTC
-+++ TclmIntp.h
-@@ -64,6 +64,5 @@ private:
- 	int current_song;
- 	int current_dev;
- 	int current_patch;
--	Event *next_event;
- };
- #endif
