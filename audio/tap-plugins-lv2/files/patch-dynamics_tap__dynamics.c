ac9b9476ae0e27e008f7d7a0df6e053e73c46a54
yuri <yuri@FreeBSD.org>
2017-11-12 10:11:32 +0000

    audio/tap-plugins-lv2: Removed unused SIMD flags.
    Also, moved patches under files/
    
    PR:		223614
    Submitted by:	Jan Beich <jbeich@FreeBSD.org>
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D13051

---
+++ audio/tap-plugins-lv2/files/patch-dynamics_tap__dynamics.c
@@ -0,0 +1,11 @@
+--- dynamics/tap_dynamics.c.orig	2016-01-25 10:55:48 UTC
++++ dynamics/tap_dynamics.c
+@@ -267,7 +267,7 @@ rms_env_process(rms_env *r, const rms_t 
+ 
+ 
+ 
+-inline
++
+ float
+ get_table_gain(int mode, float level) {
+ 
