1da217351f0bf2d0db5626db4754f7a97359d070
Rene Ladan <rene@FreeBSD.org>
2017-11-12 17:21:38 +0000

    Remove expired ports: 2017-11-12 audio/smasher: Does not build with modern csound 2017-11-12 audio/csound: Not supported upstream anymore, use csound6

--- audio/smasher/files/patch-src_replace.cpp
+++
@@ -1,9 +0,0 @@
---- src/replace.cpp.orig	2010-06-02 19:10:54 UTC
-+++ src/replace.cpp
-@@ -1,5 +1,6 @@
- #include <cstdio>
- #include <cstring>
-+#include <cstdio>
- #include "replace.h"
- 
- int Replace::run (
