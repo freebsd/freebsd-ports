3452330225db92eb43026ba79f7e9d28e1eaf43a
David Thiel <lx@FreeBSD.org>
2017-11-17 18:39:05 +0000

    Update to 0.10.0.
    PR:	223637
    Submitted by:  Jan Kokemüller

---
+++ net-im/prosody/files/patch-util-src_pposix.c
@@ -0,0 +1,10 @@
+--- util-src/pposix.c.orig	2017-11-11 08:52:04 UTC
++++ util-src/pposix.c
+@@ -21,7 +21,6 @@
+ #else
+ #define _DEFAULT_SOURCE
+ #endif
+-#define _POSIX_C_SOURCE 200809L
+ 
+ #include <stdlib.h>
+ #include <math.h>
