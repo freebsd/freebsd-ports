7fc25c874cfce929ce6de55098ecc7810723af08
Rene Ladan <rene@FreeBSD.org>
2017-11-21 18:50:58 +0000

    Remove expired ports: 2017-11-20 news/brag: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 textproc/srilm: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 x11-toolkits/tk84: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 devel/tkinspect: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 games/bogged: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 games/ttraffic: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 games/ifm: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 games/tksol: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 lang/tcl84: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 mail/tkrat2: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 multimedia/dtv: Tcl/Tk 8.4 is no longer maintained, please use 8.5+ 2017-11-20 net-mgmt/netwag: Tcl/Tk 8.4 is no longer maintained, please use 8.5+

--- devel/tkinspect/files/patch-install.tcl
+++
@@ -1,9 +0,0 @@
-Index: install.tcl
-@@ -1,6 +1,6 @@
- #!/bin/sh
- # \
--exec wish "$0" ${1+"$@"}
-+exec wish8.4 "$0" ${1+"$@"}
- #
- # $Id: install.tcl,v 1.20 2002/03/26 00:10:17 patthoyts Exp $
- #
