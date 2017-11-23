c9ca1f69ca50add0157259aa7c6d02186f49f21e
ale <ale@FreeBSD.org>
2017-11-23 13:14:48 +0000

    Update to 1.1.0 release.

--- devel/jakarta-commons-daemon/files/patch-native_jsvc-unix.c
+++ devel/jakarta-commons-daemon/files/patch-native_jsvc-unix.c
@@ -1,6 +1,6 @@
---- native/jsvc-unix.c.orig	2012-02-24 00:24:02.000000000 +0100
-+++ native/jsvc-unix.c	2012-06-13 12:40:54.000000000 +0200
-@@ -621,18 +621,13 @@
+--- native/jsvc-unix.c.orig	2017-11-15 11:51:22 UTC
++++ native/jsvc-unix.c
+@@ -717,18 +717,13 @@ static void remove_tmp_file(arg_data *ar
   */
  static int wait_child(arg_data *args, int pid)
  {
@@ -21,7 +21,7 @@
      while (count > 0) {
          sleep(1);
          /* check if the controler is still running */
-@@ -671,7 +666,6 @@
+@@ -767,7 +762,6 @@ static int wait_child(arg_data *args, in
                  }
              }
          }
@@ -29,7 +29,7 @@
          count--;
      }
      /* It takes more than the wait time to start,
-@@ -801,11 +795,11 @@
+@@ -901,11 +895,11 @@ static int child(arg_data *args, home_da
      create_tmp_file(args);
      while (!stopping) {
  #if defined(OSD_POSIX)
@@ -43,7 +43,7 @@
  #endif
          if(doreopen) {
              doreopen = false;
-@@ -824,7 +818,7 @@
+@@ -924,7 +918,7 @@ static int child(arg_data *args, home_da
          return 6;
  
      if (doreload == true)
@@ -52,19 +52,25 @@
      else
          ret = 0;
  
-@@ -1212,12 +1206,12 @@
+@@ -1341,10 +1335,10 @@ static int run_controller(arg_data *args
              if (args->vers != true && args->chck != true && status != 122)
-                 unlink(args->pidf);
+                 remove_pid_file(args, pid);
  
 -            /* If the child got out with 123 he wants to be restarted */
 +            /* If the child got out with 123 or 124 he wants to be restarted */
              /* See java_abort123 (we use this return code to restart when the JVM aborts) */
--            if (status == 123) {
-+            if (status == 123 || status == 124) {
-                 log_debug("Reloading service");
-                 /* prevent looping */
--                if (laststart + 60 > time(NULL)) {
-+                if (status == 123 && laststart + 60 > time(NULL)) {
-                     log_debug("Waiting 60 s to prevent looping");
-                     sleep(60);
-                 }
+             if (!stopping) {
+-                if (status == 123) {
++                if (status == 123 || status == 124) {
+                     if (args->restarts == 0) {
+                         log_debug("Service failure, restarts disabled");
+                         return 1;
+@@ -1356,7 +1350,7 @@ static int run_controller(arg_data *args
+                     log_debug("Reloading service");
+                     restarts++;
+                     /* prevent looping */
+-                    if (laststart + 60 > time(NULL)) {
++                    if (status == 123 && laststart + 60 > time(NULL)) {
+                         log_debug("Waiting 60 s to prevent looping");
+                         sleep(60);
+                     }
