bddedb62944edee28eb3ce75a7274b28dad591c2
Bernard Spil <brnrd@FreeBSD.org>
2017-11-26 21:07:20 +0000

    security/acme-client: Chase upstream URL update
     - Unbreaks users fresh installing port
    
    Reported by:	David Gessel <gessel blackrosetech com>
    MFH:		2017Q4

---
+++ security/acme-client/files/patch-main.c
@@ -0,0 +1,11 @@
+--- main.c.orig	2017-02-01 15:20:14 UTC
++++ main.c
+@@ -33,7 +33,7 @@
+ #include "extern.h"
+ 
+ #define AGREEMENT "https://letsencrypt.org" \
+-		  "/documents/LE-SA-v1.1.1-August-1-2016.pdf"
++		  "/documents/LE-SA-v1.2-November-15-2017.pdf"
+ #define SSL_DIR "/etc/ssl/acme"
+ #define SSL_PRIV_DIR "/etc/ssl/acme/private"
+ #define ETC_DIR "/etc/acme"
