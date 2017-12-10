a512c645e30b03b75a49480789b641783bee07ea
Dmitry Marakasov <amdmi3@FreeBSD.org>
2017-12-10 11:22:29 +0000

    - Remove arch limitations

--- games/legesmotus/files/patch-common.mk
+++ games/legesmotus/files/patch-common.mk
@@ -8,7 +8,21 @@
  #UNIVERSAL = 1
  #NOBUNDLE = 1
  
-@@ -174,12 +173,10 @@ CXXFLAGS += -Wnon-virtual-dtor
+@@ -84,13 +83,6 @@ ifeq ($(ARCHS),)
+  endif
+ endif
+ 
+-# Only Mac OS X can use fat binaries
+-ifneq ($(filter $(ARCH),$(ARCHS)),$(ARCH))
+- ifneq ($(MACHINE)-$(ARCH),Darwin-universal)
+-  $(error Disallowed architecture for this platform: $(ARCH))
+- endif
+-endif
+-
+ ifeq ($(MACHINE)$(NOBUNDLE),Darwin)
+  export MACOSX_DEPLOYMENT_TARGET=10.4
+  FLAGS_SDL  = -I$(FRAMEWORKS)/SDL.framework/Headers
+@@ -174,12 +166,10 @@ CXXFLAGS += -Wnon-virtual-dtor
  
  ifeq ($(DEBUG),1)
   RELEASE = debug
