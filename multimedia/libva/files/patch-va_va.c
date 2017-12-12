449af9e6fba8571d4b2b208d7c8553b21bb3716c
madpilot <madpilot@FreeBSD.org>
2017-12-12 21:29:57 +0000

    - Update libva to 2.0.0 - Update libva-intel-driver to 2.0.0 - Update libva-utils to 2.0.0 - Bump dependent ports due to shlib bump - Import upstream patch to fix multimedia/mpv [1] - Import upstream patch to fix multimedia/gstreamer1-vaapi [2]
    Approved by:	cpm [1], multimedia (timeout) [2]
    Obtained from:	https://github.com/mpv-player/mpv/commit/2ecf240b1cd20875991a5b18efafbe799864ff7f [1]
    		https://cgit.freedesktop.org/gstreamer/gstreamer-vaapi/commit/gst-libs/gst/vaapi/gstvaapiutils.c?id=777bba473e3ed1c7b8b7f7f0322892851c70dfd9 [2]
    Differential Revision:	https://reviews.freebsd.org/D12761

--- multimedia/libva/files/patch-va_va.c
+++ multimedia/libva/files/patch-va_va.c
@@ -1,9 +1,9 @@
---- va/va.c.orig	2014-09-30 01:48:34 UTC
+--- va/va.c.orig	2017-10-21 04:49:28 UTC
 +++ va/va.c
-@@ -227,7 +227,7 @@
+@@ -350,7 +350,7 @@ static VAStatus va_openDriver(VADisplay dpy, char *dri
          strncat( driver_path, DRIVER_EXTENSION, strlen(DRIVER_EXTENSION) );
          
-         va_infoMessage("Trying to open %s\n", driver_path);
+         va_infoMessage(dpy, "Trying to open %s\n", driver_path);
 -#ifndef ANDROID
 +#if !defined(ANDROID) && defined(RTLD_NODELETE)
          handle = dlopen( driver_path, RTLD_NOW | RTLD_GLOBAL | RTLD_NODELETE );
