6a644144c79b2beeb9de558054def3bdcf1790bc
sunpoet <sunpoet@FreeBSD.org>
2017-12-12 13:32:53 +0000

    Update to 1.9.2
    - Update MASTER_SITES
    - Update WWW
    
    Changes:	https://code.mpimet.mpg.de/news/392
    		https://code.mpimet.mpg.de/news/387

--- science/cdo/files/patch-src-pthread_debug.h
+++ science/cdo/files/patch-src-pthread_debug.h
@@ -1,18 +1,27 @@
---- src/pthread_debug.h.orig	2017-06-16 08:43:18 UTC
+--- src/pthread_debug.h.orig	2017-11-21 10:40:41 UTC
 +++ src/pthread_debug.h
-@@ -8,15 +8,17 @@ int Pthread_create(const char *caller, p
+@@ -11,20 +11,22 @@ int Pthread_create(const char *caller, p
  
  int Pthread_join(const char *caller, pthread_t th, void **thread_return);
  
 -void Pthread_mutex_lock(const char *caller, pthread_mutex_t *mutex);
--void Pthread_mutex_unlock(const char *caller, pthread_mutex_t *mutex);
+-void Pthread_mutex_lock(const char *caller, std::mutex &p_mutex);
 +int Pthread_mutex_lock(const char *caller, pthread_mutex_t *mutex);
++int Pthread_mutex_lock(const char *caller, std::mutex &p_mutex);
+ 
+-void Pthread_mutex_unlock(const char *caller, pthread_mutex_t *mutex);
+-void Pthread_mutex_unlock(const char *caller, std::mutex& p_mutex);
 +int Pthread_mutex_unlock(const char *caller, pthread_mutex_t *mutex);
++int Pthread_mutex_unlock(const char *caller, std::mutex& p_mutex);
  
 -void Pthread_cond_signal(const char *caller, pthread_cond_t *cond);
+-void Pthread_cond_signal(const char *caller,std::condition_variable &p_cond_var);
 -void Pthread_cond_wait(const char *caller, pthread_cond_t *cond, pthread_mutex_t *mutex);
+-void Pthread_cond_wait(const char *caller, std::condition_variable &p_cond_var, std::unique_lock<std::mutex>& p_mutex);
 +int Pthread_cond_signal(const char *caller, pthread_cond_t *cond);
++int Pthread_cond_signal(const char *caller,std::condition_variable &p_cond_var);
 +int Pthread_cond_wait(const char *caller, pthread_cond_t *cond, pthread_mutex_t *mutex);
++int Pthread_cond_wait(const char *caller, std::condition_variable &p_cond_var, std::unique_lock<std::mutex>& p_mutex);
  
  void print_pthread_attr(const char *caller, pthread_attr_t *attr);
 +#if defined (PTHREAD_MUTEXATTR_CONDATTR)
