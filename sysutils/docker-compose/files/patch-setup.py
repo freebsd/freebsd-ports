733359453a9178de9cbf7afd50e004384db88f1b
lifanov <lifanov@FreeBSD.org>
2017-12-19 14:58:15 +0000

    update sysutils/docker-compose to 0.18.0
    Changes: https://github.com/docker/compose/blob/1.18.0/CHANGELOG.md

--- sysutils/docker-compose/files/patch-setup.py
+++ sysutils/docker-compose/files/patch-setup.py
@@ -1,13 +1,11 @@
---- setup.py.orig	2017-09-05 13:29:08 UTC
+--- setup.py.orig	2017-12-19 14:49:12 UTC
 +++ setup.py
-@@ -33,8 +33,8 @@ install_requires = [
-     'cached-property >= 1.2.0, < 2',
+@@ -34,7 +34,7 @@ install_requires = [
      'docopt >= 0.6.1, < 0.7',
      'PyYAML >= 3.10, < 4',
--    'requests >= 2.6.1, != 2.11.0, < 2.12',
+     'requests >= 2.6.1, != 2.11.0, != 2.12.2, != 2.18.0, < 2.19',
 -    'texttable >= 0.9.0, < 0.10',
-+    'requests >= 2.6.1, != 2.11.0, < 3',
 +    'texttable >= 0.9.0, < 2',
      'websocket-client >= 0.32.0, < 1.0',
-     'docker >= 2.5.1, < 3.0',
+     'docker >= 2.6.1, < 3.0',
      'dockerpty >= 0.4.1, < 0.5',
