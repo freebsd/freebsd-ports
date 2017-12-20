0a89159f7aec94ccc7a0d529aac87c2ddccfa17c
Mathieu Arnold <mat@FreeBSD.org>
2017-12-20 19:20:20 +0000

    Fix build on 11 and 12.
    While there, sort the Makefile.
    
    Sponsored by:	Absolight

--- security/dsniff/files/patch-pcaputil.c
+++ security/dsniff/files/patch-pcaputil.c
@@ -1,15 +1,36 @@
---- ./pcaputil.c.orig	2001-03-15 09:33:04.000000000 +0100
-+++ ./pcaputil.c	2014-07-22 13:20:14.000000000 +0200
-@@ -16,9 +16,10 @@
- #include <stdlib.h>
+--- pcaputil.c.orig	2001-03-15 08:33:04 UTC
++++ pcaputil.c
+@@ -17,20 +17,9 @@
  #include <string.h>
  #include <err.h>
--#include <pcap.h>
-+#include <net/bpf.h>
-+#include </usr/include/pcap.h>
- #ifdef BSD
+ #include <pcap.h>
+-#ifdef BSD
 -#include <pcap-int.h>
-+#include </usr/include/pcap-int.h>
- #endif
+-#endif
  
  #include "pcaputil.h"
+ 
+-#ifdef BSD
+-static int
+-bpf_immediate(int fd, int on)
+-{
+-	return (ioctl(fd, BIOCIMMEDIATE, &on));
+-}
+-#endif
+-
+ int
+ pcap_dloff(pcap_t *pd)
+ {
+@@ -87,12 +76,6 @@ pcap_init(char *intf, char *filter, int 
+ 		pcap_perror(pd, "pcap_compile");
+ 		return (NULL);
+ 	}
+-#ifdef BSD
+-	if (bpf_immediate(pd->fd, 1) < 0) {
+-		perror("ioctl");
+-		return (NULL);
+-	}
+-#endif
+ 	return (pd);
+ }
+ 
