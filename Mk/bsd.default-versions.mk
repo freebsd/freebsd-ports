ddd8c8640e6bf56980b2ae40cc52b03550db34f4
Antoine Brodin <antoine@FreeBSD.org>
2017-12-23 07:03:21 +0000

    Change default version of samba from 4.4 to 4.6
    Reviewed by:	mat
    With hat:	portmgr
    Differential Revision:	https://reviews.freebsd.org/D13529

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -86,7 +86,7 @@ PYTHON3_DEFAULT?=	3.6
 # Possible values: 2.2, 2.3, 2.4
 RUBY_DEFAULT?=		2.3
 # Possible values: 4.4, 4.5, 4.6, 4.7
-SAMBA_DEFAULT?=		4.4
+SAMBA_DEFAULT?=		4.6
 # Possible values: base, openssl, openssl-devel, libressl, libressl-devel
 .if !defined(SSL_DEFAULT)
 #	If no preference was set, check for an installed base version
