359cd903e4177f51cb5f1caecccc01fe7a4bc5ed
woodsb02 <woodsb02@FreeBSD.org>
2017-12-24 07:10:45 +0000

    print/hplip and print/hplip-plugin: Update to 3.17.11
    Changes this release:
      https://developers.hp.com/hp-linux-imaging-and-printing/release_notes

---
+++ print/hplip/files/patch-prnt_hpcups_genJPEGStrips.cpp
@@ -0,0 +1,10 @@
+--- prnt/hpcups/genJPEGStrips.cpp.orig	2017-12-19 19:39:29 UTC
++++ prnt/hpcups/genJPEGStrips.cpp
+@@ -28,7 +28,6 @@ extern "C"
+ 
+ #include <setjmp.h>
+ 
+-#include <malloc.h>
+ #include <memory.h>
+ 
+ 
