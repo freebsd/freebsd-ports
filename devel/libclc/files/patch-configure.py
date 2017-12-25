1211d5f1a04cc2596d03c7c45267db8b9a50a7d0
Matthew Rezny <rezny@FreeBSD.org>
2017-12-25 23:23:24 +0000

    Update to the current snapshot and switch to LLVM5 by default

--- devel/libclc/files/patch-configure.py
+++ devel/libclc/files/patch-configure.py
@@ -1,12 +1,23 @@
+Fix use of execinfo from llvm-config --system-libs
+
 The LLVM_TOOL_LINK builds a helper tool named utils/prepare-builtins that
 requires libedit which in turn pulls in libtinfo (part of libncurses).
 In order to support builds on platforms without base ncurses (e.g. DF),
 require base libedit.  This requires an additional -L LDFLAGS to search
 ${LOCALBASE}/lib for the needed libs.  (prepare-builtins is not installed)
 
---- configure.py.orig	2015-07-10 13:37:04 UTC
+--- configure.py.orig	2017-11-27 11:14:06 UTC
 +++ configure.py
-@@ -118,7 +118,8 @@ b.rule("OPT", command = llvm_opt + " -O3
+@@ -76,7 +76,7 @@ if llvm_int_version < 390:
+     print("libclc requires LLVM >= 3.9")
+     sys.exit(1)
+ 
+-llvm_system_libs = llvm_config(['--system-libs'])
++llvm_system_libs = (llvm_config(['--system-libs'])).replace("-l/usr/lib/libexecinfo.so","-lexecinfo")
+ llvm_bindir = llvm_config(['--bindir'])
+ llvm_core_libs = llvm_config(['--libs', 'core', 'bitreader', 'bitwriter']) + ' ' + \
+                  llvm_system_libs + ' ' + \
+@@ -131,7 +131,8 @@ b = metabuild.from_name(options.g)
         description = 'OPT $out')
  
  c_compiler_rule(b, "LLVM_TOOL_CXX", 'CXX', cxx_compiler, llvm_cxxflags)
@@ -16,7 +27,7 @@ ${LOCALBASE}/lib for the needed libs.  (prepare-builtins is not installed)
  
  prepare_builtins = os.path.join('utils', 'prepare-builtins')
  b.build(os.path.join('utils', 'prepare-builtins.o'), "LLVM_TOOL_CXX",
-@@ -264,7 +265,7 @@ install_cmd = ' && '.join(['mkdir -p ${D
+@@ -290,7 +291,7 @@ install_cmd = ' && '.join(['mkdir -p ${D
                             {'src': file,
                              'dst': libexecdir}
                             for (file, dest) in install_files_bc])
