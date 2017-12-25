d187fb4a66357210d89738aff1e235a279b0c64b
rezny <rezny@FreeBSD.org>
2017-12-25 23:30:54 +0000

    Update the Mesa ports to 17.3.1 and switch to LLVM5 by default
    Correct dependencies: remove the cruft added to mesa-dri in the previous
    commit (those are mesa-libs deps), add a RUN_DEPENDS for libclc to clover
    
    Clean up patches, remove one stale patch

--- graphics/mesa-dri/files/patch-src_egl_drivers_dri2_platform__x11.c
+++ graphics/mesa-dri/files/patch-src_egl_drivers_dri2_platform__x11.c
@@ -1,14 +1,16 @@
 # work-around for https://bugs.freedesktop.org/show_bug.cgi?id=100627
 #
---- src/egl/drivers/dri2/platform_x11.c.orig	2017-10-23 13:21:18 UTC
+--- src/egl/drivers/dri2/platform_x11.c.orig	2017-12-21 17:31:21 UTC
 +++ src/egl/drivers/dri2/platform_x11.c
-@@ -1462,6 +1462,9 @@ dri2_initialize_x11(_EGLDriver *drv, _EGLDisplay *disp
+@@ -1462,7 +1462,11 @@ dri2_initialize_x11(_EGLDriver *drv, _EG
  
     if (!disp->Options.UseFallback) {
  #ifdef HAVE_DRI3
 +#if (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) && !defined(__DRM_NEXT__)
-+      if (env_var_as_boolean("LIBGL_DRI3_ENABLE", false))
-+#endif
++      if (env_var_as_boolean("LIBGL_DRI3_ENABLE", false) && !env_var_as_boolean("LIBGL_DRI3_DISABLE", false))
++#else
        if (!env_var_as_boolean("LIBGL_DRI3_DISABLE", false))
++#endif
           initialized = dri2_initialize_x11_dri3(drv, disp);
  #endif
+ 
