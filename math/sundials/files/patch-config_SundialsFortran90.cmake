2a3f43cb6b9c094d6cfecae772117a63620777f7
Yuri Victorovich <yuri@FreeBSD.org>
2017-12-27 07:47:30 +0000

    New port: math/sundials: SUite of Nonlinear and DIfferential/ALgebraic Equation Solvers
    This port was originally created by Pedro Giffuni. He offered me to take over it.
    I updated it to 3.1.0, added many options, etc.
    
    The option FCMIX is broken upstream, so it is not present in the port.
    
    Approved by:	adamw (mentor)
    Differential Revision:	https://reviews.freebsd.org/D13647

---
+++ math/sundials/files/patch-config_SundialsFortran90.cmake
@@ -0,0 +1,15 @@
+--- config/SundialsFortran90.cmake.orig	2017-11-07 23:47:45 UTC
++++ config/SundialsFortran90.cmake
+@@ -35,9 +35,9 @@ if(CMAKE_Fortran_COMPILER)
+       (NOT "${ENV_FCFLAGS}" STREQUAL "") AND
+       ("${CMAKE_Fortran_FLAGS}" STREQUAL ""))
+     # check if environment variables are equal
+-    IF (NOT "${ENV_FFLAGS}" STREQUAL "${ENV_FCFLAGS}")
+-      PRINT_ERROR("FFLAGS='${ENV_FFLAGS}' and FCFLAGS='${ENV_FCFLAGS}' are both set but are not equal.")
+-    ENDIF()
++    #IF (NOT "${ENV_FFLAGS}" STREQUAL "${ENV_FCFLAGS}")
++    #  PRINT_ERROR("FFLAGS='${ENV_FFLAGS}' and FCFLAGS='${ENV_FCFLAGS}' are both set but are not equal.")
++    #ENDIF()
+   ENDIF()
+ 
+   # Enable the language for next steps
