bf4acd346ee5e16404bc4acd10547d3b90ebb095
jbeich <jbeich@FreeBSD.org>
2017-12-27 15:19:04 +0000

    lang/beignet: workaround broken llvm-config50 --system-libs
    /usr/bin/ld: cannot find -l/usr/lib/libexecinfo.so
    
    Inspired by:	devel/libclc fix from r457263

--- lang/beignet/files/patch-CMake_FindLLVM.cmake
+++ lang/beignet/files/patch-CMake_FindLLVM.cmake
@@ -28,3 +28,13 @@
  endif (LLVM_INSTALL_DIR)
  
  execute_process(
+@@ -100,6 +100,9 @@ execute_process(
+   OUTPUT_STRIP_TRAILING_WHITESPACE
+ )
+ if (LLVM_SYSTEM_LIBS_ORIG)
++# https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=223191#c3
++string(REPLACE "-l/usr/lib/libexecinfo.so" "-lexecinfo"
++       LLVM_SYSTEM_LIBS_ORIG ${LLVM_SYSTEM_LIBS_ORIG})
+ string(REGEX REPLACE " *\n" "" LLVM_SYSTEM_LIBS ${LLVM_SYSTEM_LIBS_ORIG})
+ endif (LLVM_SYSTEM_LIBS_ORIG)
+ endif (LLVM_VERSION_NODOT VERSION_GREATER 34)
