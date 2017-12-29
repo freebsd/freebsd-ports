b0de3baec2642d36df278dceb8c3c4b30284cd15
sunpoet <sunpoet@FreeBSD.org>
2017-12-29 18:47:37 +0000

    Update to 0.09
    - Remove USE_PERL5=patch
    - Strip shared library
    - Regenerate patch file with makepatch:
    
    Changes:	http://search.cpan.org/dist/X11-IdleTime/Changes

--- x11/p5-X11-IdleTime/files/patch-IdleTime.pm
+++ x11/p5-X11-IdleTime/files/patch-IdleTime.pm
@@ -1,10 +1,12 @@
---- IdleTime.pm.orig	Fri Feb  7 23:22:42 2003
-+++ IdleTime.pm	Wed Jul  9 18:23:01 2003
-@@ -12,6 +12,7 @@
+--- IdleTime.pm.orig	2008-07-04 17:02:22 UTC
++++ IdleTime.pm
+@@ -13,7 +13,8 @@ use Inline (
  	C => 'DATA',
- 	VERSION => '0.01',
+ 	VERSION => '0.08',
  	NAME => 'X11::IdleTime',
-+	INC => '-I/usr/X11R6/include/',
- 	LIBS => '-L/usr/X11R6/lib/ -lX11 -lXext -lXss',
+-	LIBS => '-L/usr/X11R6/lib/ -lX11 -lXext -lXss',
++	INC => '-I/usr/local/include/',
++	LIBS => '-L/usr/local/lib/ -lX11 -lXext -lXss',
  	);
  
+ 1;
