440cc1a47645762a9caa0dd233332a02a06fb287
swills <swills@FreeBSD.org>
2017-12-30 23:36:07 +0000

    x11-fm/doublecmd: update to version 0.8.1
    PR:		224736
    Submitted by:	Beñat Gonzalez Etxepare <bbtruk@users.sourceforge.net> (maintainer)

--- x11-fm/doublecmd/files/patch-src_uGlobsPaths.pas
+++ x11-fm/doublecmd/files/patch-src_uGlobsPaths.pas
@@ -1,6 +1,6 @@
---- src/uGlobsPaths.pas.orig	2016-01-13 13:34:19 UTC
+--- src/uGlobsPaths.pas.orig	2017-12-11 10:45:31 UTC
 +++ src/uGlobsPaths.pas
-@@ -3,13 +3,14 @@ unit uGlobsPaths;
+@@ -3,7 +3,7 @@ unit uGlobsPaths;
  interface
  
  var
@@ -9,14 +9,15 @@
    gpCfgDir : String = '';  // directory from which configuration files are used
    gpGlobalCfgDir : String = '';  // config dir global for all user
    gpCmdLineCfgDir : String = ''; // config dir passed on the command line
-   gpLngDir : String = '';  // path to language *.po files
+@@ -11,6 +11,7 @@ var
    gpPixmapPath : String = '';  // path to pixmaps
-   gpCacheDir : String = ''; // cache directory
+   gpHighPath : String = ''; // editor highlighter directory
+   gpThumbCacheDir : String = ''; // thumbnails cache directory
 +  gpDocsDir : String = '%%DOCSDIR%%'; // directory for help files
  
  //Global Configuration Filename
  const
-@@ -36,7 +37,7 @@ end;
+@@ -37,7 +38,7 @@ end;
  procedure LoadPaths;
  begin
    OnGetApplicationName := @GetAppName;
