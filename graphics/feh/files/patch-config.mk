e844f14c3a2b4e3cf0db141ccb91fcd4f4a051ca
zeising <zeising@FreeBSD.org>
2017-12-31 11:08:25 +0000

    Update to 2.23
    Use our own getopt instead of bundled version.
    
    ChangeLog: https://feh.finalrewind.org/archive/2.23/

--- graphics/feh/files/patch-config.mk
+++ graphics/feh/files/patch-config.mk
@@ -1,6 +1,6 @@
---- config.mk.orig	2015-05-24 09:46:21 UTC
+--- config.mk.orig	2017-12-28 18:28:35 UTC
 +++ config.mk
-@@ -6,12 +6,12 @@ PREFIX ?= /usr/local
+@@ -22,12 +22,12 @@ endif
  
  # Directories for manuals, executables, docs, data, etc.
  main_dir = ${DESTDIR}${PREFIX}
@@ -13,5 +13,14 @@
 -example_dir = ${main_dir}/share/doc/feh/examples
 +example_dir = ${main_dir}/share/examples/feh
  desktop_dir = ${main_dir}/share/applications
+ icon_dir = ${ICON_PREFIX}/hicolor
+ 48_icon_dir = ${icon_dir}/48x48/apps
+@@ -38,7 +38,7 @@ CFLAGS ?= -g -O2
+ CFLAGS += -Wall -Wextra -pedantic
  
- # default CFLAGS
+ # Settings for glibc >= 2.19 - may need to be adjusted for other systems
+-CFLAGS += -std=c11 -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=500
++CFLAGS += -std=c11
+ 
+ ifeq (${curl},1)
+ 	CFLAGS += -DHAVE_LIBCURL
