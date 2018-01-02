bfbf6537e2e3ddc0dfdd8a3f5b172afd1c5c7f13
Mark Linimon <linimon@FreeBSD.org>
2018-01-02 00:35:25 +0000

    Repairs to MASTER_SITE_DEBIAN_NON_US:
     - ftp.df.lth.se has gone away.
     - some of the "ftp\." sites now only accept http:// addresses.

--- Mk/bsd.qt.mk
+++ Mk/bsd.qt.mk
@@ -306,9 +306,12 @@ CONFIGURE_ARGS+=--with-qt-includes=${QT_INCDIR} \
 _USE_QT_ALL=	assistant dbus declarative designer doc gui help \
 				imageformats l10n linguist linguisttools multimedia \
 				network opengl pixeltool qdbusviewer qmake script \
-				scripttools sql sql-ibase sql-mysql sql-odbc sql-pgsql \
+				scripttools sql sql-mysql sql-odbc sql-pgsql \
 				sql-sqlite2 sql-sqlite3 svg testlib webkit \
 				xml xmlpatterns
+.if ${ARCH} == amd64 || ${ARCH} == i386
+_USE_QT_ALL+=	sql-ibase
+.endif
 
 _USE_QT4_ONLY=	accessible assistant-adp assistantclient clucene codecs-cn codecs-jp \
 				codecs-kr codecs-tw corelib demo graphicssystems-opengl \
