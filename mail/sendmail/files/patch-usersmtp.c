1c6661563a583041da18d61c142ce35ac0415e15
Mark Felder <feld@FreeBSD.org>
2018-01-02 22:12:14 +0000

    MFH: r457930
    - fix regression introduced by r457706
    PR: 224452
    
    - add option BLACKLISTD
    https://reviews.freebsd.org/D13475

--- mail/sendmail/files/patch-usersmtp.c
+++ mail/sendmail/files/patch-usersmtp.c
@@ -1,26 +1,23 @@
---- sendmail/usersmtp.c.orig	2014-12-05 10:42:28.000000000 -0500
-+++ sendmail/usersmtp.c	2016-06-12 18:35:25.940865000 -0400
-@@ -1825,6 +1825,10 @@
+--- sendmail/usersmtp.c.orig	2014-12-05 15:42:28 UTC
++++ sendmail/usersmtp.c
+@@ -1825,6 +1825,9 @@ attemptauth(m, mci, e, sai)
  
  		if (saslresult != SASL_OK && saslresult != SASL_CONTINUE)
  		{
-+#ifdef USE_BLACKLIST
 +			int fd = sm_io_getinfo(mci->mci_in, SM_IO_WHAT_FD, NULL);
-+			blacklist_notify(1, fd, "AUTH FAIL");
-+#endif
++			BLACKLIST_NOTIFY(BLACKLIST_AUTH_FAIL, fd, "AUTH FAIL");
++
  			if (tTd(95, 5))
  				sm_dprintf("AUTH FAIL=%s (%d)\n",
  					sasl_errstring(saslresult, NULL, NULL),
-@@ -1970,9 +1974,13 @@
+@@ -1970,9 +1973,11 @@ smtpauth(m, mci, e)
  	do
  	{
  		result = attemptauth(m, mci, e, &(mci->mci_sai));
 -		if (result == EX_OK)
 +		if (result == EX_OK) {
-+#ifdef USE_BLACKLIST
 +			int fd = sm_io_getinfo(mci->mci_in, SM_IO_WHAT_FD, NULL);
-+			blacklist_notify(0, fd, "AUTH OK");
-+#endif
++			BLACKLIST_NOTIFY(BLACKLIST_AUTH_OK, fd, "AUTH OK");
  			mci->mci_sasl_auth = true;
 -		else if (result == EX_TEMPFAIL || result == EX_NOPERM)
 +		} else if (result == EX_TEMPFAIL || result == EX_NOPERM)
