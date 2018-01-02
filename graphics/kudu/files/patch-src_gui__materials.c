7efbe52294532302db995c4347834088605b3aee
amdmi3 <amdmi3@FreeBSD.org>
2018-01-02 20:06:38 +0000

    - Limit python version - Switch to USES=localbase - Regenerate patches

--- graphics/kudu/files/patch-src_gui__materials.c
+++ graphics/kudu/files/patch-src_gui__materials.c
@@ -1,7 +1,7 @@
 Clang compatibility
---- src/gui_materials.c.orig	2006-01-22 12:37:31.000000000 +0300
-+++ src/gui_materials.c	2013-06-13 20:40:43.037204137 +0400
-@@ -66,7 +66,7 @@
+--- src/gui_materials.c.orig	2006-01-22 09:37:31 UTC
++++ src/gui_materials.c
+@@ -66,7 +66,7 @@ static gboolean kudu_gui_materials_edit_
  	return TRUE;
  }
  
