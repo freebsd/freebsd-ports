33402138034605394b506d84a99a209c530e2386
swills <swills@FreeBSD.org>
2018-01-02 16:06:59 +0000

    net/rubygem-grpc: update to 1.8.0
    PR:		224800
    Approved by:	Matthias Fechner <idefix@fechner.net> (maintainer)

--- net/rubygem-grpc/files/patch-src_ruby_lib_grpc.rb
+++ net/rubygem-grpc/files/patch-src_ruby_lib_grpc.rb
@@ -1,11 +1,11 @@
---- src/ruby/lib/grpc.rb.orig	2017-06-28 17:25:39 UTC
+--- src/ruby/lib/grpc.rb.orig	2017-12-31 07:02:12 UTC
 +++ src/ruby/lib/grpc.rb
-@@ -27,7 +27,7 @@
- # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+@@ -12,7 +12,7 @@
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
  
 -ssl_roots_path = File.expand_path('../../../../etc/roots.pem', __FILE__)
-+ssl_roots_path = '/usr/local/etc/ssl/cert.pem'
++ssl_roots_path = '%%LOCALBASE%%/etc/ssl/cert.pem'
  
  require_relative 'grpc/errors'
  require_relative 'grpc/grpc'
