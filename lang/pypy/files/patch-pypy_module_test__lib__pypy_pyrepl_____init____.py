ab8ddde14792e3580ead3a0b031bde52391c2262
dbn <dbn@FreeBSD.org>
2018-01-03 18:42:15 +0000

    lang/pypy(3): update to 5.10, add flavors to ancillary ports
     - Consistently use DISTVERSION instead of PORTVERSION (the former is set, not the latter)
     - Only depend on libunwind on x86 arch with FreeBSD >= 11 (fixes ignored build on arm/ppc)
     - Fix python dependency with depreciation of python meta ports
     - Remove PYPY_BITS from pkg-plist target, no longer used
     - Remove setting of PORTVERSION, DISTVERSION is set in bsd.pypy.mk
     - bsd.pypy.mk:
       - Define DISTVERSION once, to ensure consistency
       - PYPY_DIR: Handle versions with multiple digits
       - PYPY_CFFI_VER now differs in value between pypy and pypy3
     - bsd.pypy.cffi.mk:
       - Add FLAVORS for pypy and pypy3 [1]
       - Consistently use PYTHON_IMPL instead of the hard coded "pypy"
       - PLIST_FILES: use direct substitution instead of PLIST_SUB
    
    Approved by:		portmgr (mat) [1]
    Differential Revision:	https://reviews.freebsd.org/D13675

--- lang/pypy/files/patch-pypy_module_test__lib__pypy_pyrepl_____init____.py
+++
@@ -1,9 +0,0 @@
---- pypy/module/test_lib_pypy/pyrepl/__init__.py.orig	2017-10-03 10:49:20 UTC
-+++ pypy/module/test_lib_pypy/pyrepl/__init__.py
-@@ -1,6 +1,3 @@
- import sys
- import lib_pypy.pyrepl
- sys.modules['pyrepl'] = sys.modules['lib_pypy.pyrepl']
--
--if sys.platform.startswith('freebsd'):
--    raise Exception('XXX seems to hangs on FreeBSD9')
