2c8b87f83771cbd0274f9af83550447ec149f96f
bofh <bofh@FreeBSD.org>
2018-01-08 21:58:18 +0000

    sysutils/fusefs-sshfs: Update version 3.3.0=>3.3.1
    - Add NDEBUG [1]
    
    PR:		223338 [1]
    Submitted by:	rozhuk.im@gmail.com [1]
    Relnotes:	https://github.com/libfuse/sshfs/releases

--- sysutils/fusefs-sshfs/files/patch-sshfs.c
+++ sysutils/fusefs-sshfs/files/patch-sshfs.c
@@ -1,6 +1,6 @@
---- sshfs.c.orig	2017-09-20 17:08:27 UTC
+--- sshfs.c.orig	2018-01-08 21:05:08 UTC
 +++ sshfs.c
-@@ -965,7 +965,7 @@ static int pty_master(char **name)
+@@ -971,7 +971,7 @@ static int pty_master(char **name)
  {
  	int mfd;
  
