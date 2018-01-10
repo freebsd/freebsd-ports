0d70553a4fba47fffd8869cf6f65ca3d21ec90d9
jkim <jkim@FreeBSD.org>
2018-01-10 21:12:42 +0000

    Update to 0.3.13.

--- textproc/libmwaw03/files/patch-src_lib_libmwaw__internal.hxx
+++ textproc/libmwaw03/files/patch-src_lib_libmwaw__internal.hxx
@@ -1,17 +1,11 @@
---- src/lib/libmwaw_internal.hxx.orig	2017-07-03 08:10:43 UTC
+--- src/lib/libmwaw_internal.hxx.orig	2017-11-13 09:08:03 UTC
 +++ src/lib/libmwaw_internal.hxx
-@@ -119,9 +119,11 @@ struct MWAW_shared_ptr_noop_deleter {
- /** fall through attributes */
- #define MWAW_FALLTHROUGH
- #if defined(__clang__)
--#  if defined(__has_cpp_attribute) && __has_cpp_attribute(clang::fallthrough)
--#    undef MWAW_FALLTHROUGH
--#    define MWAW_FALLTHROUGH [[clang::fallthrough]]
-+#  if defined(__has_cpp_attribute)
-+#    if __has_cpp_attribute(clang::fallthrough)
-+#      undef MWAW_FALLTHROUGH
-+#      define MWAW_FALLTHROUGH [[clang::fallthrough]]
-+#    endif
- #  endif
- #elif defined(__GNUC__)
- #  if __GNUC__>=7
+@@ -1027,7 +1027,7 @@ template <class T> class MWAWBox2 (public)
+   }
+   //! generic constructor
+   template <class U> explicit MWAWBox2(MWAWBox2<U> const &p)
+-    : m_data(p.min(), p.max())
++    : m_data(MWAWVec2<T>(p.min()), MWAWVec2<T>(p.max()))
+   {
+   }
+ 
