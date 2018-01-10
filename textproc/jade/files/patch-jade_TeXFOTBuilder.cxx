66c9d73006e24b07ce3a79e5676c93fa5cc21198
jbeich <jbeich@FreeBSD.org>
2018-01-10 14:50:52 +0000

    textproc/jade: regen via "make makepatch"
    $ portlint -C
    WARN: /usr/ports/textproc/jade/files/patch-assert.cxx: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-CharsetRegistry.cxx: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-CharsetRegistry.h: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-CodingSystemKit.cxx: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-config.h: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-koi8-r.h: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-koi8r.dcl: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-lib__instmac.m4: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-Makefile.in: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-Ptr.cxx: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-Ptr.h: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-TeXFOTBuilder.cxx: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    WARN: /usr/ports/textproc/jade/files/patch-TransformFOTBuilder.cxx: patch was not generated using ``make makepatch''.  It is recommended to use ``make makepatch'' when you need to [re-]generate a patch to ensure proper patch format.
    0 fatal errors and 13 warnings found.
    
    Approved by:	portmgr blanket

--- textproc/jade/files/patch-jade_TeXFOTBuilder.cxx
+++ textproc/jade/files/patch-jade_TeXFOTBuilder.cxx
@@ -1,6 +1,6 @@
---- jade/TeXFOTBuilder.cxx.orig	2011-09-24 14:17:02.000000000 +0000
-+++ jade/TeXFOTBuilder.cxx	2011-09-24 14:19:42.000000000 +0000
-@@ -83,6 +83,8 @@
+--- jade/TeXFOTBuilder.cxx.orig	1998-10-07 05:16:34 UTC
++++ jade/TeXFOTBuilder.cxx
+@@ -83,6 +83,8 @@ class TeXFOTBuilder : public SerialFOTBuilder { (publi
        value.convertString(nic_.placement);
        }
      ExtensionFlowObj *copy() const { return new PageFloatFlowObj(*this); }
@@ -9,7 +9,7 @@
    private:
      PageFloatNIC nic_;
      StringC name_;
-@@ -96,6 +98,8 @@
+@@ -96,6 +98,8 @@ class TeXFOTBuilder : public SerialFOTBuilder { (publi
        fotb.endPageFootnote();
      }
      ExtensionFlowObj *copy() const { return new PageFootnoteFlowObj(*this); }
