acf09f669bb126754981110046702d71789177ff
Jan Beich <jbeich@FreeBSD.org>
2018-01-10 14:17:13 +0000

    textproc/jade: unbreak build with Clang 6 (C++14 by default)
    In file included from EntityCatalog.cxx:9:
    In file included from ./../include/EntityDecl.h:12:
    ./../include/Ptr.h:73:11: error: ISO C++11 does not allow access declarations; use using declarations
          instead
      Ptr<T>::isNull;
              ^
      using
    ./../include/Ptr.h:74:11: error: ISO C++11 does not allow access declarations; use using declarations
          instead
      Ptr<T>::clear;
              ^
      using
    
    In file included from DescriptorManager.cxx:5:
    In file included from ./../include/DescriptorManager.h:8:
    In file included from ./../include/List.h:7:
    ./../include/IList.h:29:14: error: ISO C++11 does not allow access declarations; use using
          declarations instead
      IListBase::clear;
                 ^
      using
    ./../include/IList.h:30:14: error: ISO C++11 does not allow access declarations; use using
          declarations instead
      IListBase::empty;
                 ^
      using
    In file included from DescriptorManager.cxx:6:
    In file included from ./../include/ListIter.h:8:
    ./../include/IListIter.h:20:18: error: ISO C++11 does not allow access declarations; use using
          declarations instead
      IListIterBase::next;
                     ^
      using
    ./../include/IListIter.h:21:18: error: ISO C++11 does not allow access declarations; use using
          declarations instead
      IListIterBase::done;
                     ^
      using
    
    In file included from parseSd.cxx:5:
    ./Parser.h:65:16: error: ISO C++11 does not allow access declarations; use using declarations instead
      ParserState::sdPointer;
                   ^
      using
    ./Parser.h:66:16: error: ISO C++11 does not allow access declarations; use using declarations instead
      ParserState::instanceSyntaxPointer;
                   ^
      using
    ./Parser.h:67:16: error: ISO C++11 does not allow access declarations; use using declarations instead
      ParserState::prologSyntaxPointer;
                   ^
      using
    ./Parser.h:68:16: error: ISO C++11 does not allow access declarations; use using declarations instead
      ParserState::activateLinkType;
                   ^
      using
    ./Parser.h:69:16: error: ISO C++11 does not allow access declarations; use using declarations instead
      ParserState::allLinkTypesActivated;
                   ^
      using
    ./Parser.h:70:16: error: ISO C++11 does not allow access declarations; use using declarations instead
      ParserState::entityManager;
                   ^
      using
    ./Parser.h:71:16: error: ISO C++11 does not allow access declarations; use using declarations instead
      ParserState::entityCatalog;
                   ^
      using
    ./Parser.h:72:16: error: ISO C++11 does not allow access declarations; use using declarations instead
      ParserState::baseDtd;
                   ^
      using
    ./Parser.h:73:16: error: ISO C++11 does not allow access declarations; use using declarations instead
      ParserState::options;
                   ^
      using
    
    PR:		224929
    Reported by:	antoine (via bug 224669)
    Reviewed by:	dim
    Approved by:	portmgr blanket

--- textproc/jade/files/patch-Ptr.h
+++ textproc/jade/files/patch-Ptr.h
@@ -8,3 +8,14 @@
  
  // T must have Resource as a public base class
  // T may be an incomplete type
+@@ -69,8 +70,8 @@ class ConstPtr : private Ptr<T> { (public)
+   const T *operator->() const { return Ptr<T>::pointer(); }
+   const T &operator*() const { return *Ptr<T>::pointer(); }
+   void swap(ConstPtr<T> &p) { Ptr<T>::swap(p); }
+-  Ptr<T>::isNull;
+-  Ptr<T>::clear;
++  using Ptr<T>::isNull;
++  using Ptr<T>::clear;
+   Boolean operator==(const Ptr<T> &p) const { return Ptr<T>::operator==(p); }
+   Boolean operator!=(const Ptr<T> &p) const { return Ptr<T>::operator!=(p); }
+   Boolean operator==(const ConstPtr<T> &p) const {
