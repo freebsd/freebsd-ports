9f2bb92d56d6969bb04a9daf048e299c971132e9
dbn <dbn@FreeBSD.org>
2018-01-10 18:39:31 +0000

    devel/eric6: update to 17.11.1, add FLAVORS
     - add FLAVORS for the following combinations [1]:
       - python: 2.7 and 3.6
       - qt: 4 and 5
     - simplify Makefiles:
       - all common macros between eric6 and the language packages are in
         devel/eric6/bsd.eric6.mk
       - devel/eric6/Makefile.inc only includes language package specific
         macros
       - eric6 specific macros are now moved into devel/eric6/Makefile
     - spelling error fixed in Makefile.inc
     - dependency on eric6 (for language packages) removed as the addition
       of flavors removes the ability to depend on a single port origin
     - install language files directly, since setup.py fails without eric6
       installed
    
    Approved by:	portmgr (mat) [1]
    Differential Revision:	https://reviews.freebsd.org/D13448

---
+++ devel/eric6/bsd.eric6.mk
@@ -0,0 +1,22 @@
+# Created by: Boris Samorodov <bsam@FreeBSD.org>
+# $FreeBSD$
+#
+# This is a set of Makefile macros that are common between devel/eric6 and
+# localized language packages for devel/eric6:
+# chinese/eric6, french/eric6, german/eric6, portuguese/eric6, russian/eric6
+# devel/cs-eric6, devel/es-eric6, devel/it-eric6, devel/tr-eric6
+
+PORTNAME=	eric6
+PORTVERSION=	17.11.1
+MASTER_SITES=	SF/eric-ide/${PORTNAME}/stable/${PORTVERSION}
+WRKSRC=		${WRKDIR}/${PORTNAME}-${PORTVERSION}
+
+MAINTAINER=	dbn@FreeBSD.org
+COMMENT=	Full featured Python and Ruby editor and IDE based on Qt
+
+LICENSE=	GPLv3
+
+DESCR=		${.CURDIR}/../../devel/eric6/pkg-descr
+MAKE_ENV+=	"HOME=${WRKDIR}"
+
+NO_BUILD=	yes
