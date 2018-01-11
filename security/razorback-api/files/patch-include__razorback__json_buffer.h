8d5d0af4832cfb15b0f5c377558c8ba1c3f396c7
Richard Gallamore <ultima@FreeBSD.org>
2018-01-11 01:45:36 +0000

    MFH: r457965 r458047
    Update devel/json-c to 0.13
    
    - Add TEST_TARGET
    - While I'm here, fix shebang for net/opensips
    - Bump PORTREVISION of dependent ports for shlib change
    
    Changes:	https://github.com/json-c/json-c/blob/master/ChangeLog
    PR:		224675
    Exp-run by:	antoine
    
    * Revision bump for libevhtp
    * Fix build errors for updated libevhtp
    
    Approved by:	ports-secteam (swills)

--- security/razorback-api/files/patch-include__razorback__json_buffer.h
+++ security/razorback-api/files/patch-include__razorback__json_buffer.h
@@ -1,11 +1,12 @@
---- ./include/razorback/json_buffer.h.orig	2014-08-20 10:25:03.000000000 -0400
-+++ ./include/razorback/json_buffer.h	2014-08-20 10:25:26.000000000 -0400
-@@ -9,7 +9,7 @@
+--- include/razorback/json_buffer.h.orig	2012-01-09 19:40:46 UTC
++++ include/razorback/json_buffer.h
+@@ -9,6 +9,7 @@
  #include <razorback/messages.h>
  #include <razorback/ntlv.h>
  
 -#include <json/json.h>
 +#include <json-c/json.h>
++#include <json-c/bits.h>
  
  #ifdef __cplusplus
  extern "C" {
