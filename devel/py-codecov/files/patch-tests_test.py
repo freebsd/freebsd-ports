88f94a5d12fb23420d7d92f3aaaadacf4d2f5f03
cpm <cpm@FreeBSD.org>
2018-01-13 11:33:45 +0000

    Fix python test suite

--- devel/py-codecov/files/patch-tests_test.py
+++ devel/py-codecov/files/patch-tests_test.py
@@ -1,11 +1,22 @@
---- tests/test.py.orig	2018-01-08 10:57:15 UTC
+--- tests/test.py.orig	2018-01-13 11:23:57 UTC
 +++ tests/test.py
+@@ -2,9 +2,9 @@ import os
+ import sys
+ import pickle
+ import itertools
++import unittest
+ from ddt import ddt, data
+ from mock import patch, Mock
+-import unittest2 as unittest
+ 
+ import subprocess
+ 
 @@ -134,7 +134,7 @@ class TestUploader(unittest.TestCase):
                  self.assertEqual(codecov.main(), None)
                  assert post.called and put.called
  
 -    @unittest.skipIf(os.getenv('CI') == "True" and os.getenv('APPVEYOR') == 'True', 'Skip AppVeyor CI test')
-+    @unittest.skipIf(sys.platform.startswith('freebsd'),'Skip test')
++    @unittest.skip('Skip AppVeyor test')
      def test_send(self):
          with patch('requests.post') as post:
              with patch('requests.put') as put:
@@ -14,7 +25,7 @@
              raise Exception("Did not raise AssertionError")
  
 -    @unittest.skipIf(os.getenv('CI') == "True" and os.getenv('APPVEYOR') == 'True', 'Skip AppVeyor CI test')
-+    @unittest.skipIf(sys.platform.startswith('freebsd'),'Skip test')
++    @unittest.skip('Skip AppVeyor test')
      def test_bowerrc_none(self):
          with open(self.bowerrc, 'w+') as f:
              f.write('{"other_key": "tests"}')
