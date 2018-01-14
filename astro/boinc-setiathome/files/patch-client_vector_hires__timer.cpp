3be55871f7b1ed611cc8dad737b33ec23bc9fa92
Larry Rosenman <ler@FreeBSD.org>
2018-01-14 23:38:11 +0000

    astro/boinc-astropulse: catch astro/boinc-setiathome rename, fix -lexecinfo astro/boinc-setiathome-v8 -> astro/boinc-setiathome per adamw - reroll local distfile to reflect real SVN revision - fix sah_config.h C++ errors

--- astro/boinc-setiathome/files/patch-client_vector_hires__timer.cpp
+++ astro/boinc-setiathome/files/patch-client_vector_hires__timer.cpp
@@ -1,4 +1,4 @@
---- client/vector/hires_timer.cpp.orig	2018-01-14 21:26:29 UTC
+--- client/vector/hires_timer.cpp.orig	2018-01-14 23:02:12 UTC
 +++ client/vector/hires_timer.cpp
 @@ -267,7 +267,7 @@ hires_timer::hires_timer() : rollover(0),last_ticks(0)
  #endif
