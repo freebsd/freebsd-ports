c7794c96e42df31491a4d604eba30d7e1ad1edf8
Mark Felder <feld@FreeBSD.org>
2018-01-18 20:23:31 +0000

    MFH: r459372
    net-mgmt/librenms: Update to 1.35, many improvements
    
    Improvements:
    
    -    All files should be owned root:wheel except logs and rrd which need to be writable by the app
    -    Add missing php posix extension
    -    Do not install config.php by default. This breaks the install process which won't run if this file exists
    -    Clean up automatic PLIST creation: don't install .orig or .bak files, don't add @dir as they aren't needed
    -    Patch LibreNMS to make /validate/ page not produce warnings about files not being writable (for git updates)
    -    Remove the Updates validation check altogether as we won't be using git to update
    -    Patch the User validation check to only check the logs and rrd dir and ensure the correct user owns them
    -    Change the default user in the generated config to "www"
    -    Patch the File Lock code to put the lock file in /tmp and not in the WWWDIR which should not be writable
    -    Update message in installer to use WWWDIR as suggested path for config.php
    -    Use shebangfix instead of patch where applicable
    -    Fix APACHEMOD port option and declaration of the USES=php
    
    PR:		225161
    Differential Revision:	https://reviews.freebsd.org/D13907

--- net-mgmt/librenms/files/patch-poller-wrapper.py
+++ net-mgmt/librenms/files/patch-poller-wrapper.py
@@ -1,11 +1,5 @@
---- poller-wrapper.py.orig	2017-05-28 21:54:20 UTC
+--- poller-wrapper.py.orig	2018-01-14 15:32:19 UTC
 +++ poller-wrapper.py
-@@ -1,4 +1,4 @@
--#! /usr/bin/env python2
-+#! %%LOCALBASE%%/bin/python
- """
-  poller-wrapper A small tool which wraps around the poller and tries to
-                 guide the polling process with a more modern approach with a
 @@ -36,6 +36,8 @@ except:
      print "threading, Queue, sys, subprocess, time, os, json"
      sys.exit(2)
