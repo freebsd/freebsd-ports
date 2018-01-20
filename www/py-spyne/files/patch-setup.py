f9fc48f36e4409421d4bd71976fe184983139432
sunpoet <sunpoet@FreeBSD.org>
2018-01-20 12:37:48 +0000

    Use patch from upstream

--- www/py-spyne/files/patch-setup.py
+++ www/py-spyne/files/patch-setup.py
@@ -1,19 +1,13 @@
+Obtained from:  https://github.com/arskom/spyne/commit/4b89a21f9af72f07628d2ecd114c308987acc0e1
+
 --- setup.py.orig	2016-10-28 11:15:20 UTC
 +++ setup.py
-@@ -3,6 +3,7 @@
- 
- from __future__ import print_function
- 
-+import codecs
- import os
- import re
- import sys
-@@ -48,7 +49,7 @@ protocols and transports.
+@@ -48,7 +48,7 @@ protocols and transports.
  
  try:
      os.stat('CHANGELOG.rst')
 -    LONG_DESC += "\n\n" + open('CHANGELOG.rst', 'r').read()
-+    LONG_DESC += "\n\n" + codecs.open('CHANGELOG.rst', 'r', encoding='utf-8').read()
++    LONG_DESC += u"\n\n" + open('CHANGELOG.rst', 'rb').read().decode('utf8')
  except OSError:
      pass
  
