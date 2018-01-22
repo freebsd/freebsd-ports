c0027b0b41256f0fa64562f2d78dd909e22db25c
mat <mat@FreeBSD.org>
2018-01-22 13:45:04 +0000

    Update to v5.27.8-12-gb404a7f.
    Sponsored by:	Absolight

--- lang/perl5-devel/files/patch-perl.c
+++ lang/perl5-devel/files/patch-perl.c
@@ -1,6 +1,6 @@
---- perl.c.orig	2017-12-30 17:41:59 UTC
+--- perl.c.orig	2018-01-22 01:13:24 UTC
 +++ perl.c
-@@ -2016,23 +2016,7 @@ S_Internals_V(pTHX_ CV *cv)
+@@ -2019,23 +2019,7 @@ S_Internals_V(pTHX_ CV *cv)
      PUSHs(Perl_newSVpvn_flags(aTHX_ non_bincompat_options,
  			      sizeof(non_bincompat_options) - 1, SVs_TEMP));
  
