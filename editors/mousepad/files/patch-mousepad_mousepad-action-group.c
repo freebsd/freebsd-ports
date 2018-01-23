68c97ff2762e6d0a2f27637c2bb44869b5d77944
Guido Falsi <madpilot@FreeBSD.org>
2018-01-23 13:10:32 +0000

    MFH: r459693
    Import code from mousepad development repository which fixes a dbus
    messages storm causing heavy CPU usage, memory allocation and disk
    usage when working with multiple windows.
    
    While here also import a GTK3 specific fix.
    
    Ref.:
    https://bugzilla.xfce.org/show_bug.cgi?id=14184
    https://bugzilla.xfce.org/show_bug.cgi?id=12134
    
    Reported by:	Erich Dollansky <freebsd.ed.lists@sumeritec.com>
    Obtained from:	https://git.xfce.org/apps/mousepad/
    
    Approved by:	ports-secteam (swills)

--- editors/mousepad/files/patch-mousepad_mousepad-action-group.c
+++ editors/mousepad/files/patch-mousepad_mousepad-action-group.c
@@ -1,25 +1,48 @@
 --- mousepad/mousepad-action-group.c.orig	2014-09-01 20:50:07 UTC
 +++ mousepad/mousepad-action-group.c
-@@ -283,7 +283,8 @@ void
- mousepad_action_group_set_active_style_scheme (MousepadActionGroup  *self,
-                                                GtkSourceStyleScheme *scheme)
- {
--  GtkAction *action;
-+  GtkAction   *action;
-+  const gchar *scheme_name = NULL;
+@@ -124,7 +124,7 @@ mousepad_action_group_class_init (MousepadActionGroupC
+                 G_SIGNAL_RUN_LAST,
+                 0, NULL, NULL,
+                 g_cclosure_marshal_VOID__OBJECT,
+-                G_TYPE_NONE, 0,
++                G_TYPE_NONE, 1,
+                 GTK_SOURCE_TYPE_LANGUAGE);
+ }
  
-   g_return_if_fail (MOUSEPAD_IS_ACTION_GROUP (self));
- 
-@@ -303,8 +304,11 @@ mousepad_action_group_set_active_style_s
+@@ -302,11 +302,6 @@ mousepad_action_group_set_active_style_scheme (Mousepa
+   gtk_toggle_action_set_active (GTK_TOGGLE_ACTION (action), TRUE);
    self->locked = FALSE;
  
-   /* update the setting when the active action is changed */
-+  if (scheme != NULL)
-+    scheme_name = gtk_source_style_scheme_get_id(scheme);
-+
-   self->locked = TRUE;
+-  /* update the setting when the active action is changed */
+-  self->locked = TRUE;
 -  MOUSEPAD_SETTING_SET_STRING (COLOR_SCHEME, gtk_source_style_scheme_get_id (scheme));
-+  MOUSEPAD_SETTING_SET_STRING (COLOR_SCHEME, scheme_name);
-   self->locked = FALSE;
- 
+-  self->locked = FALSE;
+-
    g_object_notify (G_OBJECT (self), "active-style-scheme");
+ }
+ 
+@@ -473,6 +468,8 @@ static void
+ mousepad_action_group_style_scheme_action_activate (MousepadActionGroup       *self,
+                                                     MousepadStyleSchemeAction *action)
+ {
++  const gchar *scheme_name = NULL;
++
+   /* only update the active action if we're not already in the process of
+    * setting it and the sender action is actually active */
+   if (! self->locked &&
+@@ -481,7 +478,14 @@ mousepad_action_group_style_scheme_action_activate (Mo
+       GtkSourceStyleScheme *scheme;
+ 
+       scheme = mousepad_style_scheme_action_get_style_scheme (action);
+-      mousepad_action_group_set_active_style_scheme (self, scheme);
++
++      /* update the setting when the active action is changed */
++      if (scheme != NULL)
++        scheme_name = gtk_source_style_scheme_get_id(scheme);
++
++      self->locked = TRUE;
++      MOUSEPAD_SETTING_SET_STRING (COLOR_SCHEME, scheme_name);
++      self->locked = FALSE;
+     }
+ }
+ 
