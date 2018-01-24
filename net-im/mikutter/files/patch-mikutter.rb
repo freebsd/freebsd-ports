6b8f29c9719740f9e6db2f667505488f18515bbc
TAKATSU Tomonari <tota@FreeBSD.org>
2018-01-24 10:38:50 +0000

    - Update to 3.6.0
    Submitted by:	Koichiro IWAO <meta+ports_AT_vmeta_DOT_jp>
    PR:		224923

--- net-im/mikutter/files/patch-mikutter.rb
+++ net-im/mikutter/files/patch-mikutter.rb
@@ -1,15 +1,15 @@
---- mikutter.rb.orig	2017-10-08 12:29:16 UTC
+--- mikutter.rb.orig	2017-12-24 15:00:56 UTC
 +++ mikutter.rb
-@@ -11,7 +11,7 @@ This software is released under the MIT 
- http://opensource.org/licenses/mit-license.php
- 
+@@ -13,7 +13,7 @@ http://opensource.org/licenses/mit-licen
  =end
+ module Mikutter; end
+ 
 -require_relative 'core/boot/option'
 +require_relative '%%RUBY_SITELIBDIR%%/mikutter/core/boot/option'
  Mopt.parse exec_command: true
  
  if !ENV['DISABLE_BUNDLER_SETUP'] || ['', '0'].include?(ENV['DISABLE_BUNDLER_SETUP'].to_s)
-@@ -31,7 +31,7 @@ require 'webrick'
+@@ -33,7 +33,7 @@ require 'webrick'
  require 'thread'
  require 'fileutils'
  
@@ -17,4 +17,4 @@
 +require_relative '%%RUBY_SITELIBDIR%%/mikutter/core/utils'
  
  miquire :boot, 'check_config_permission', 'mainloop', 'delayer'
- miquire :core, 'service', 'environment'
+ miquire :core, 'environment'
