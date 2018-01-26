db161a265454a9f1c095716f48d140d8adcc2b66
Don Lewis <truckman@FreeBSD.org>
2018-01-26 18:33:08 +0000

    Don't enable crashdumps by default.  This build knob causes the build to be done with debug symbols, which greatly increase the size of the build, but this is all in vain because the executables and libraries are subsequently stripped.
    Build with debug symbols if WITH_DEBUG is set.
    
    Build with -fstack-protector.
    
    Only fetch unowinreg.dll when building the SDK.
    
    Add three new options:
    
      CRASHDUMP - Enable crashdumps, requires WITH_DEBUG
    
      DBGUTIL   - Enable assertions, object counting, and other
                  non-production runtime debugging.
    
      DEBUG     - Compile with -O0 for better debugability.  The ports
                  framework also sets WITH_DEBUG.
    
    Fold a couple long lines in the Makefile.

--- editors/openoffice-4/files/patch-solenv_gbuild_platform_freebsd.mk
+++ editors/openoffice-4/files/patch-solenv_gbuild_platform_freebsd.mk
@@ -8,3 +8,16 @@
  	-pipe
  ifeq ($(COM),CLANG)
  gb_CXXFLAGS += -DHAVE_STL_INCLUDE_PATH
+@@ -142,6 +143,12 @@ gb_LinkTarget_LDFLAGS += \
+ 	-Wl,--dynamic-list-cpp-typeinfo \
+ 	-Wl,-Bsymbolic-functions \
+ 
++endif
++
++ifneq ($(filter $(CPUNAME),INTEL X86_64),)
++gb_CFLAGS += -fstack-protector
++gb_CXXFLAGS += -fstack-protector
++gb_LinkTarget_LDFLAGS += -fstack-protector
+ endif
+ 
+ ifeq ($(gb_DEBUGLEVEL),2)
