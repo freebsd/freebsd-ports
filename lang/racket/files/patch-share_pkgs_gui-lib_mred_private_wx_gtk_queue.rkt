9f0efe388b146af0d0295c77bef01c763815a19a
Jimmy Olgeni <olgeni@FreeBSD.org>
2018-01-27 19:40:37 +0000

    Update lang/racket to version 6.12.

--- lang/racket/files/patch-share_pkgs_gui-lib_mred_private_wx_gtk_queue.rkt
+++ lang/racket/files/patch-share_pkgs_gui-lib_mred_private_wx_gtk_queue.rkt
@@ -1,6 +1,6 @@
---- share/pkgs/gui-lib/mred/private/wx/gtk/queue.rkt.orig	2016-04-22 21:19:48 UTC
+--- share/pkgs/gui-lib/mred/private/wx/gtk/queue.rkt.orig	2018-01-27 18:25:11 UTC
 +++ share/pkgs/gui-lib/mred/private/wx/gtk/queue.rkt
-@@ -9,7 +9,6 @@
+@@ -11,7 +11,6 @@
           "clipboard.rkt"
           "const.rkt"
  	 "w32.rkt"
@@ -8,12 +8,12 @@
           "../common/keep-forever.rkt")
  
  (provide (protect-out gtk-start-event-pump
-@@ -83,8 +82,6 @@
+@@ -86,8 +85,6 @@
            (error (format
                    "Gtk initialization failed for display ~s"
                    (or display ":0"))))
 -        (when single-instance?
 -          (do-single-instance))
-         (let ([v (scheme_register_process_global "Racket-GUI-wm-class" #f)])
+         (let ([v (register-process-global #"Racket-GUI-wm-class" #f)])
            (when v
              (gdk_set_program_class (cast v _pointer _string))))
