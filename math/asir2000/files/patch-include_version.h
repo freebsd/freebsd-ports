b72a3f9fe8a319257344187aa3d2de59c3fe2a1e
rene <rene@FreeBSD.org>
2018-01-28 01:39:12 +0000

    Remove expired ports: 2018-01-27 emulators/raine: Broken for more than 6 months 2018-01-27 mail/libmapi: Broken for more than 6 months 2018-01-27 www/lessc: Broken for more than 6 months 2018-01-27 www/uglifyjs: Broken for more than 6 months 2018-01-27 www/py-octoprint: Broken for more than 6 months 2018-01-27 www/xpi-noscript: Broken for more than 6 months 2018-01-27 www/npm-less-plugin-clean-css: Broken for more than 6 months 2018-01-27 www/caudium14: Depends on expiring lang/pike78 2018-01-27 www/hinventory-client: Broken for more than 6 months 2018-01-27 security/tclgpg: Broken for more than 6 months 2018-01-27 security/pond: Broken for more than 6 months 2018-01-27 security/mdcrack: Broken for more than 6 months 2018-01-27 devel/npm-commander: Broken for more than 6 months 2018-01-27 devel/py-protocols-devel: Broken for more than 6 months 2018-01-27 devel/py-InlineEgg: Broken for more than 6 months 2018-01-27 devel/npm-amdefine: Broken for more than 6 months 2018-01-27 devel/npm-source-map: Broken for more than 6 months 2018-01-27 devel/libical-glib: Broken for more than 6 months 2018-01-27 devel/npm-graceful-readlink: Broken for more than 6 months 2018-01-27 devel/npm-clean-css: Broken for more than 6 months 2018-01-27 devel/py-ruledispatch: Depends on expiring devel/py-protocols-devel 2018-01-27 games/balazarbrothers: Broken for more than 6 months 2018-01-27 games/libretro-cores: Broken for more than 6 months 2018-01-27 games/gish-demo: Broken for more than 6 months 2018-01-27 games/cheech: Broken for more than 6 months 2018-01-27 audio/mbrolavox: Broken for more than 6 months 2018-01-27 audio/linux-mbrola: Depends on expiring audio/mbrolavox 2018-01-27 audio/gigedit: Broken for more than 6 months 2018-01-27 audio/gnomoradio: Broken for more than 6 months 2018-01-27 math/asir2000: Broken for more than 6 months 2018-01-27 multimedia/librespot: Broken for more than 6 months 2018-01-27 multimedia/bombono: Broken for more than 6 months 2018-01-27 lang/gcl: Broken for more than 6 months 2018-01-27 lang/pike78: Broken for more than 6 months 2018-01-27 shells/ambit: Broken for more than 6 months

--- math/asir2000/files/patch-include_version.h
+++
@@ -1,8 +0,0 @@
---- include/version.h.orig	2014-08-19 15:35:02.000000000 +0900
-+++ include/version.h	2014-08-25 02:37:52.000000000 +0900
-@@ -48,4 +48,4 @@
-  * $OpenXM: OpenXM_contrib2/asir2000/include/version.h,v 1.366 2014/08/19 06:35:02 noro Exp $ 
- */
- #define ASIR_VERSION 20140819
--#define ASIR_DISTRIBUTION "Kobe"
-+#define ASIR_DISTRIBUTION "Plotting Group"
