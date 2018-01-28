5f396595f20e25d0b29de07baf81602eacf98362
riggs <riggs@FreeBSD.org>
2018-01-28 15:16:37 +0000

    Update to upstream version 1.5.0 (features and fixes)
    Detailed upstream release notes see
    https://github.com/containous/traefik/releases/tag/v1.5.0

--- net/traefik/files/patch-generate.go
+++ net/traefik/files/patch-generate.go
@@ -1,10 +1,14 @@
---- generate.go.orig	2017-09-07 20:04:03 UTC
+--- generate.go.orig	2018-01-23 16:34:04 UTC
 +++ generate.go
-@@ -4,6 +4,6 @@ Copyright
- 
- //go:generate rm -vf autogen/gen.go
+@@ -1,9 +1,9 @@
+ //go:generate rm -vf autogen/gentemplates/gen.go
+ //go:generate rm -vf autogen/genstatic/gen.go
  //go:generate mkdir -p static
--//go:generate go-bindata -pkg autogen -o autogen/gen.go ./static/... ./templates/...
-+//go:generate src/github.com/jteeuwen/go-bindata/go-bindata/go-bindata -pkg autogen -o autogen/gen.go ./static/... ./templates/...
+-//go:generate go-bindata -pkg gentemplates -nometadata -nocompress -o autogen/gentemplates/gen.go ./templates/...
++//go:generate src/github.com/jteeuwen/go-bindata/go-bindata/go-bindata -pkg gentemplates -nometadata -nocompress -o autogen/gentemplates/gen.go ./templates/...
+ //go:generate gofmt -s -w autogen/gentemplates/gen.go
+-//go:generate go-bindata -pkg genstatic -nocompress -o autogen/genstatic/gen.go ./static/...
++//go:generate src/github.com/jteeuwen/go-bindata/go-bindata/go-bindata -pkg genstatic -nocompress -o autogen/genstatic/gen.go ./static/...
  
  package main
+ 
