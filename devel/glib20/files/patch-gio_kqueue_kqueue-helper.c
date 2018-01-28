aa9ac80b479820be2cfed954ee6c031d35308135
Koop Mast <kwm@FreeBSD.org>
2018-01-28 20:29:16 +0000

    Fix another crash bug in the kqueue backend.
    PR:		199872 217946

--- devel/glib20/files/patch-gio_kqueue_kqueue-helper.c
+++ devel/glib20/files/patch-gio_kqueue_kqueue-helper.c
@@ -4,8 +4,11 @@ and
 https://bugzilla.gnome.org/show_bug.cgi?id=739424
 https://bug739424.bugzilla-attachments.gnome.org/attachment.cgi?id=351191
 
---- gio/kqueue/kqueue-helper.c.orig	2018-01-15 21:22:08.234860000 +0100
-+++ gio/kqueue/kqueue-helper.c	2018-01-15 21:21:54.143656000 +0100
+https://bugzilla.gnome.org/show_bug.cgi?id=776147
+https://git.gnome.org/browse/glib/commit/?id=76072a2dde4a4acc8be8d3c47efbc6811ebe0c1e
+
+--- gio/kqueue/kqueue-helper.c.orig	2018-01-28 21:18:25.213606000 +0100
++++ gio/kqueue/kqueue-helper.c	2018-01-28 21:18:34.964780000 +0100
 @@ -43,7 +43,7 @@ static gboolean kh_debug_enabled = FALSE;
  #define KH_W if (kh_debug_enabled) g_warning
  
@@ -15,7 +18,19 @@ https://bug739424.bugzilla-attachments.gnome.org/attachment.cgi?id=351191
  
  static int kqueue_descriptor = -1;
  static int kqueue_socket_pair[] = {-1, -1};
-@@ -291,10 +291,10 @@ process_kqueue_notifications (GIOChannel   *gioc,
+@@ -97,8 +97,10 @@ convert_kqueue_events_to_gio (uint32_t flags, gboolean
+     }
+   if (flags & NOTE_RENAME)
+     {
++      /* Since there’s apparently no way to get the new name of the file out of
++       * kqueue(), all we can do is say that this one has been deleted. */
+       *done = TRUE;
+-      return G_FILE_MONITOR_EVENT_MOVED;
++      return G_FILE_MONITOR_EVENT_DELETED;
+     }
+   if (flags & NOTE_REVOKE)
+     {
+@@ -291,10 +293,10 @@ process_kqueue_notifications (GIOChannel   *gioc,
  
    G_LOCK (hash_lock);
    sub = (kqueue_sub *) g_hash_table_lookup (subs_hash_table, GINT_TO_POINTER (n.fd));
@@ -27,7 +42,7 @@ https://bug739424.bugzilla-attachments.gnome.org/attachment.cgi?id=351191
        KH_W ("Got a notification for a deleted or non-existing subscription %d",
               n.fd);
        return TRUE;
-@@ -336,6 +336,7 @@ process_kqueue_notifications (GIOChannel   *gioc,
+@@ -336,6 +338,7 @@ process_kqueue_notifications (GIOChannel   *gioc,
          g_file_monitor_source_handle_event (source, mask, NULL, NULL, NULL, g_get_monotonic_time ());
      }
  
@@ -35,7 +50,7 @@ https://bug739424.bugzilla-attachments.gnome.org/attachment.cgi?id=351191
    return TRUE;
  }
  
-@@ -451,13 +452,14 @@ _kh_start_watching (kqueue_sub *sub)
+@@ -451,13 +454,14 @@ _kh_start_watching (kqueue_sub *sub)
  
    G_LOCK (hash_lock);
    g_hash_table_insert (subs_hash_table, GINT_TO_POINTER (sub->fd), sub);
@@ -51,7 +66,7 @@ https://bug739424.bugzilla-attachments.gnome.org/attachment.cgi?id=351191
    return TRUE;
  }
  
-@@ -498,22 +500,15 @@ _kh_add_sub (kqueue_sub *sub)
+@@ -498,22 +502,15 @@ _kh_add_sub (kqueue_sub *sub)
  gboolean
  _kh_cancel_sub (kqueue_sub *sub)
  {
