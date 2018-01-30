35b3fa6b575f5a78d5d2aeb01949fb5dd0d1e2d7
Joseph Mingrone <jrm@FreeBSD.org>
2018-01-30 09:10:35 +0000

    net-im/mastodon: Update to 2.2.0
    Upstream changes: https://github.com/tootsuite/mastodon/releases/tag/v2.2.0

--- net-im/mastodon/files/patch-config_boot.rb
+++ net-im/mastodon/files/patch-config_boot.rb
@@ -1,27 +1,11 @@
---- config/boot.rb.orig	2017-05-20 22:05:08 UTC
+--- config/boot.rb.orig	2018-01-24 12:29:46 UTC
 +++ config/boot.rb
-@@ -1,14 +1,14 @@
- ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+@@ -4,7 +4,7 @@ require 'bundler/setup' # Set up gems li
+ require 'bootsnap'
  
- require 'bundler/setup' # Set up gems listed in the Gemfile.
--require 'bootsnap'
-+#require 'bootsnap'
- 
--Bootsnap.setup(
+ Bootsnap.setup(
 -  cache_dir:            'tmp/cache',
--  development_mode:     ENV['RAILS_ENV'] == 'development',
--  load_path_cache:      true,
--  autoload_paths_cache: true,
--  disable_trace:        true,
--  compile_cache_iseq:   false,
--  compile_cache_yaml:   false
--)
-+# Bootsnap.setup(
-+#   cache_dir:            'tmp/cache',
-+#   development_mode:     ENV['RAILS_ENV'] == 'development',
-+#   load_path_cache:      true,
-+#   autoload_paths_cache: true,
-+#   disable_trace:        true,
-+#   compile_cache_iseq:   false,
-+#   compile_cache_yaml:   false
-+# )
++  cache_dir:            '/var/cache/mastodon',
+   development_mode:     ENV['RAILS_ENV'] == 'development',
+   load_path_cache:      true,
+   autoload_paths_cache: true,
