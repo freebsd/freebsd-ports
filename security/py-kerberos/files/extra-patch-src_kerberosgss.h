0f6dd9c8eb35b155f3fa4806c3b30017490d68a9
Dan Langille <dvl@FreeBSD.org>
2018-01-30 02:20:10 +0000

    Fix for use with either GSSAPI_BASE or GSSAPI_HEIMDAL
    Without, the first invocation of authGSSClientStep raises kerberos.GSSError
    after failing to acquire a ticket for the krbtgt service on the intended host
    rather than the specified service.
    
    PR:		204899
    Submitted by:	John W. O'Brien <john@saltant.com>

--- security/py-kerberos/files/extra-patch-src_kerberosgss.h
+++ security/py-kerberos/files/extra-patch-src_kerberosgss.h
@@ -1,11 +1,10 @@
 --- src/kerberosgss.h.orig	2015-03-29 03:41:32 UTC
 +++ src/kerberosgss.h
-@@ -14,8 +14,9 @@
+@@ -14,8 +14,8 @@
   * limitations under the License.
   **/
  
 +#include <krb5.h>
-+#define gss_krb5_nt_service_name GSS_KRB5_NT_PRINCIPAL_NAME
  #include <gssapi/gssapi.h>
 -#include <gssapi/gssapi_generic.h>
  #include <gssapi/gssapi_krb5.h>
