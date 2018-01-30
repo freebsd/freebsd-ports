d4b3731e8b3931cca5f8d14a419daa2119db3297
zeising <zeising@FreeBSD.org>
2018-01-30 17:28:34 +0000

    Add a configuration knob to allow the usage of the super key instead of alt as modifier.
    Strip binary, fix whitespace.
    
    PR:		225227
    Submitted by:	Mateusz Piotrowski
    Approved by:	maintainer timeout (2 weeks)

---
+++ x11-wm/dwm/files/extra-patch-config.def.h
@@ -0,0 +1,11 @@
+--- config.def.h.orig	2018-01-16 16:05:12 UTC
++++ config.def.h
+@@ -42,7 +42,7 @@ static const Layout layouts[] = {
+ };
+ 
+ /* key definitions */
+-#define MODKEY Mod1Mask
++#define MODKEY Mod4Mask
+ #define TAGKEYS(KEY,TAG) \
+ 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+ 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
