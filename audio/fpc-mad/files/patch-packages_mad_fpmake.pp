f68185c820376bad8f6a9d1b2726036cf10c5dea
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2018-02-01 07:14:41 +0000

    - Update to 3.0.4 - Connect www/fpc-libmicrohttpd

--- audio/fpc-mad/files/patch-packages_mad_fpmake.pp
+++ audio/fpc-mad/files/patch-packages_mad_fpmake.pp
@@ -1,11 +1,11 @@
---- packages/mad/fpmake.pp.orig	2015-11-13 21:15:29 UTC
-+++ packages/mad/fpmake.pp
-@@ -17,7 +17,7 @@ begin
+--- packages/mad/fpmake.pp	2017-09-02 11:48:24.000000000 -0500
++++ packages/mad/fpmake.pp	2017-12-12 12:22:21.182111000 -0500
+@@ -17,7 +17,7 @@
      P.Directory:=ADirectory;
  {$endif ALLPACKAGES}
-     P.Version:='3.0.2';
+     P.Version:='3.0.4';
 -    P.OSes := [linux,win32,win64,wii];
-+    P.OSes := [linux,win32,win64,wii,freebsd,dragonfly];
++    P.OSes := [linux,win32,win64,wii,dragonfly,freebsd];
      P.SourcePath.Add('src');
  
      T:=P.Targets.AddUnit('mad.pas');
