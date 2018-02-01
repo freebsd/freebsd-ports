e9becaff3af6b30faf5b27483e12afd1dcb901b3
zeising <zeising@FreeBSD.org>
2018-02-01 13:23:40 +0000

    Complete fix for CVE-2017-15132
    Complete fix for CVE-2017-15132, the previous fix was not enough, and caused
    the request to remain after an abort, causing a use-after-free later on.
    
    PR:		225585
    Submitted by:	Vladimir Krstulja
    Approved by:	adamw (maintainer)
    MFH:		2018Q1

--- mail/dovecot/files/patch-src_lib-auth_auth-client-request.c
+++ mail/dovecot/files/patch-src_lib-auth_auth-client-request.c
@@ -1,10 +1,12 @@
 --- src/lib-auth/auth-client-request.c.orig	2017-10-05 17:10:44 UTC
 +++ src/lib-auth/auth-client-request.c
-@@ -180,6 +180,7 @@ void auth_client_request_abort(struct auth_client_requ
+@@ -180,6 +180,9 @@ void auth_client_request_abort(struct auth_client_requ
  
  	auth_client_send_cancel(request->conn->client, request->id);
  	call_callback(request, AUTH_REQUEST_STATUS_ABORT, NULL, NULL);
-+	pool_unref(&request->pool);
++	/* remove the request */
++	auth_server_connection_remove_request(request->conn, request->id);
++ 	pool_unref(&request->pool);
  }
  
  unsigned int auth_client_request_get_id(struct auth_client_request *request)
