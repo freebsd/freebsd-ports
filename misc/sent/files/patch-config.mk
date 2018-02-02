d2c401f0be4a79aafa22d311cb45a9c078daef1b
tobik <tobik@FreeBSD.org>
2018-02-02 04:06:07 +0000

    misc/sent: Update to 1
    Changes:	https://git.suckless.org/sent/log/?h=1
    PR:		225607
    Submitted by:	Mateusz Piotrowski <mpp302@gmail.com> (based on)
    Approved by:	Neel Chauhan <neel@neelc.org> (maintainer)

--- misc/sent/files/patch-config.mk
+++ misc/sent/files/patch-config.mk
@@ -1,19 +1,13 @@
---- config.mk.orig	2015-11-19 18:37:08 UTC
+--- config.mk.orig	2018-02-01 17:23:54 UTC
 +++ config.mk
-@@ -7,12 +7,12 @@ VERSION = 0.2
- PREFIX = /usr/local
- MANPREFIX = ${PREFIX}/share/man
- 
--X11INC = /usr/X11R6/include
--X11LIB = /usr/X11R6/lib
-+X11INC = /usr/local/X11R6/include
-+X11LIB = /usr/local/X11R6/lib
- 
- # includes and libs
--INCS = -I. -I/usr/include -I/usr/include/freetype2 -I${X11INC}
--LIBS = -L/usr/lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11 -lpng
+@@ -16,8 +16,8 @@ LIBS = -L/usr/lib -lc -lm -L${X11LIB} -lXft -lfontconf
+ # OpenBSD (uncomment)
+ #INCS = -I. -I${X11INC} -I${X11INC}/freetype2
+ # FreeBSD (uncomment)
+-#INCS = -I. -I/usr/local/include -I/usr/local/include/freetype2 -I${X11INC}
+-#LIBS = -L/usr/local/lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11
 +INCS = -I. -I/usr/local/include -I/usr/local/include/freetype2 -I${X11INC}
-+LIBS = -L/usr/local/lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11 -lpng
++LIBS = -L/usr/local/lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11
  
  # flags
  CPPFLAGS = -DVERSION=\"${VERSION}\" -D_XOPEN_SOURCE=600
