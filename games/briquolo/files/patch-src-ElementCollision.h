ab8768cb02e365181fe24ad66e5cc26fc4dfa149
Ganael LAPLANCHE <martymac@FreeBSD.org>
2018-02-02 12:12:31 +0000

    Fix build on -CURRENT

---
+++ games/briquolo/files/patch-src-ElementCollision.h
@@ -0,0 +1,11 @@
+--- ./src/ElementCollision.h.orig	2018-01-29 21:56:58.057957000 +0100
++++ ./src/ElementCollision.h	2018-01-29 22:04:08.902625000 +0100
+@@ -30,7 +30,7 @@
+ using namespace std;
+ 
+ class Balle;
+-class Struct_Collision;
++struct Struct_Collision;
+ 
+ class Point
+ {
