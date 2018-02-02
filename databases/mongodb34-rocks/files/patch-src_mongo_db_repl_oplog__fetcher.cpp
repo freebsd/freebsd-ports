18d0d795585cc214d8b4386814bf15fa8659f20a
Jan Beich <jbeich@FreeBSD.org>
2018-02-02 09:55:51 +0000

    databases/mongodb34{,-rocks}: switch to upstream Boost 1.66 fix
    PR:		224090 224091

--- databases/mongodb34-rocks/files/patch-src_mongo_db_repl_oplog__fetcher.cpp
+++ databases/mongodb34-rocks/files/patch-src_mongo_db_repl_oplog__fetcher.cpp
@@ -1,5 +1,5 @@
-Drop unnecessary use of boost::make_optional
 https://jira.mongodb.org/browse/SERVER-32516
+https://github.com/mongodb/mongo/commit/3c34eda8d8a3
 
 --- src/mongo/db/repl/oplog_fetcher.cpp.orig	2017-09-07 19:28:48 UTC
 +++ src/mongo/db/repl/oplog_fetcher.cpp
@@ -8,7 +8,7 @@ https://jira.mongodb.org/browse/SERVER-32516
              return metadataResult.getStatus();
          }
 -        oqMetadata = boost::make_optional<rpc::OplogQueryMetadata>(metadataResult.getValue());
-+        oqMetadata = metadataResult.getValue();
++        oqMetadata = boost::make_optional(metadataResult.getValue());
      }
      return oqMetadata;
  }
