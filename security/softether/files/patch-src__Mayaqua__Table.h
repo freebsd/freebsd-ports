e9904586ee10d09c18d85f83fc041467c580f888
pi <pi@FreeBSD.org>
2018-02-02 18:01:36 +0000

    security/softether: update 4.20.9608 -> 4.25.9656
    - There are 11 vulnerabilities on SoftEther VPN. There vulnerabilities
      are found by the source code audit process conducted by Max Planck
      Institute for Molecular Genetics and Mr. Guido Vranken in late 2017.
      This build fixes all of these vulnerabilities.
    
    PR:		225618
    Submitted by:	net@arrishq.net (maintainer)
    MFH:		2018Q1
    Relnotes:	http://www.softether.org/5-download/history

--- security/softether/files/patch-src__Mayaqua__Table.h
+++ security/softether/files/patch-src__Mayaqua__Table.h
@@ -1,9 +1,9 @@
 --- src/Mayaqua/Table.h.orig	2016-05-03 06:16:53 UTC
 +++ src/Mayaqua/Table.h
-@@ -117,7 +117,7 @@
- #define	UNICODE_CACHE_FILE		L".unicode_cache_%s.dat"
- 
+@@ -119,7 +119,7 @@
  #define	LANGLIST_FILENAME		"|languages.txt"
+ #define	LANGLIST_FILENAME_WINE	"|languages_wine.txt"
+ 
 -#define	LANG_CONFIG_FILENAME	L"@lang.config"
 +#define	LANG_CONFIG_FILENAME	L"/var/db/softether/lang.config"
  #define	LANG_CONFIG_TEMPLETE	"|lang.config"
