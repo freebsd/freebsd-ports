a911671e0bb663a5c1b49f01960da46eff5aba6c
nobutaka <nobutaka@FreeBSD.org>
2018-02-03 14:00:47 +0000

    - Update to 3.8.4. - Regenerate patches with make makepatch.

--- x11/mlterm/files/patch-inputmethod_ibus_im__ibus.c
+++ x11/mlterm/files/patch-inputmethod_ibus_im__ibus.c
@@ -1,11 +1,11 @@
---- inputmethod/ibus/im_ibus.c.orig	2017-01-07 22:24:38 UTC
+--- inputmethod/ibus/im_ibus.c.orig	2017-12-02 16:03:23 UTC
 +++ inputmethod/ibus/im_ibus.c
-@@ -1008,7 +1008,7 @@ im_info_t* im_ibus_get_info(char* locale
+@@ -1033,7 +1033,7 @@ im_info_t *im_ibus_get_info(char *locale
    }
  
    result->id = strdup("ibus");
 -  result->name = strdup("iBus");
 +  result->name = strdup("ibus");
-   result->num_of_args = 0;
+   result->num_args = 0;
    result->args = NULL;
    result->readable_args = NULL;
