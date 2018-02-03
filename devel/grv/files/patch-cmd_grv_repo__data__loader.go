fe7a0ac929a93311f5e63a78665a89c23349ce38
yuri <yuri@FreeBSD.org>
2018-02-03 20:20:49 +0000

    New port: devel/grv: Terminal interface for viewing git repositories
    Screenshot:
    https://raw.githubusercontent.com/rgburke/grv/master/doc/grv-history-view.png
    
    PR:		225617
    Submitted by:	Dmitri Goutnik <dg@syrec.org>
    Approved by:	adamw (mentor)
    Differential Revision:	https://reviews.freebsd.org/D14165

---
+++ devel/grv/files/patch-cmd_grv_repo__data__loader.go
@@ -0,0 +1,11 @@
+--- cmd/grv/repo_data_loader.go.orig	2018-02-01 15:59:52 UTC
++++ cmd/grv/repo_data_loader.go
+@@ -9,7 +9,7 @@ import (
+ 
+ 	log "github.com/Sirupsen/logrus"
+ 	slice "github.com/bradfitz/slice"
+-	git "gopkg.in/libgit2/git2go.v25"
++	git "gopkg.in/libgit2/git2go.v26"
+ )
+ 
+ const (
