431d173580c4722d1d33cb09755559b5c9f7c9e6
madpilot <madpilot@FreeBSD.org>
2018-02-04 11:11:47 +0000

    - Update deskutils/owncloudclient to 2.4.0 - Regenerate patches
    PR:		225583
    Submitted by:	me
    Approved by:	yonas@fizk.net (maintainer)

--- deskutils/owncloudclient/files/patch-src_gui_generalsettings.cpp
+++ deskutils/owncloudclient/files/patch-src_gui_generalsettings.cpp
@@ -1,14 +1,14 @@
---- src/gui/generalsettings.cpp.orig	2016-05-12 07:59:12 UTC
+--- src/gui/generalsettings.cpp.orig	2017-12-20 15:03:02 UTC
 +++ src/gui/generalsettings.cpp
-@@ -124,6 +124,7 @@
+@@ -117,6 +117,7 @@ void GeneralSettings::loadMiscSettings()
  
  void GeneralSettings::slotUpdateInfo()
  {
 +#if 0
      // Note: the sparkle-updater is not an OCUpdater
-     OCUpdater *updater = qobject_cast<OCUpdater*>(Updater::instance());
+     OCUpdater *updater = qobject_cast<OCUpdater *>(Updater::instance());
      if (ConfigFile().skipUpdateCheck()) {
-@@ -140,6 +141,7 @@
+@@ -133,6 +134,7 @@ void GeneralSettings::slotUpdateInfo()
          // can't have those infos from sparkle currently
          _ui->updatesGroupBox->setVisible(false);
      }
