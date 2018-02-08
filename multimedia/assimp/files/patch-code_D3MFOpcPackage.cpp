9cbdc5114ee8c46e5dd813d80b1da07e6ae56ff1
yuri <yuri@FreeBSD.org>
2018-02-08 22:09:15 +0000

    multimedia/assimp: Update to 4.1.0
    Change log:
    https://github.com/assimp/assimp/releases/tag/v4.1.0
    
    Additional port changes:
    * Removed CMAKE_SKIP_RPATH, the default behavior should suffice
    * Changed to CMAKE_OFF
    * Removed the unnecessary .if/gcc clause
    * Improved PLIST_SUB tokens
    * Added the patch for #include that is already in the upstream
      repository now
    
    Also tried to add the double-precision option, but it fails in build,
    so I reported this to the upsream.
    
    Bumped PORTREVISION: games/pioneer games/doomsday graphics/qt5-3d
    
    Submitted by:	myself
    Reported by:	portscout
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D14253

--- multimedia/assimp/files/patch-code_D3MFOpcPackage.cpp
+++ multimedia/assimp/files/patch-code_D3MFOpcPackage.cpp
@@ -1,11 +1,10 @@
---- code/D3MFOpcPackage.cpp.orig	2017-07-18 17:03:26 UTC
+--- code/D3MFOpcPackage.cpp.orig	2017-12-11 13:22:45 UTC
 +++ code/D3MFOpcPackage.cpp
-@@ -55,6 +55,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY O
+@@ -55,6 +55,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ #include <map>
  #include <algorithm>
  #include <cassert>
- 
 +#include <stdlib.h>
-+
  #include <contrib/unzip/unzip.h>
+ #include "3MFXmlTags.h"
  
- namespace Assimp {
