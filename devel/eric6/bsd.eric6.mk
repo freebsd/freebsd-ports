614abb4fab35354284d46152fda8c065d2e64736
dbn <dbn@FreeBSD.org>
2018-02-08 15:11:50 +0000

    devel/eric6: update to 18.02
     - remove translation packages no longer shipped by upstream:
         cs-eric6
         fr-eric6
         it-eric6
         pt-eric6
         tr-eric6
         zh_CN-eric6
       upstream considers these translations too incomplete.
     - add pkg-message explaining that there are translation available.

--- devel/eric6/bsd.eric6.mk
+++ devel/eric6/bsd.eric6.mk
@@ -7,7 +7,7 @@
 # devel/cs-eric6, devel/es-eric6, devel/it-eric6, devel/tr-eric6
 
 PORTNAME=	eric6
-PORTVERSION=	17.12
+PORTVERSION=	18.02
 MASTER_SITES=	SF/eric-ide/${PORTNAME}/stable/${PORTVERSION}
 WRKSRC=		${WRKDIR}/${PORTNAME}-${PORTVERSION}
 
