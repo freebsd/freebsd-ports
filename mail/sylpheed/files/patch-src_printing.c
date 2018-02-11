a9e170954440c00c453efdf8af29eba68152bfa0
Emanuel Haupt <ehaupt@FreeBSD.org>
2018-02-11 12:22:50 +0000

    Update to 3.7.0

--- mail/sylpheed/files/patch-src_printing.c
+++ mail/sylpheed/files/patch-src_printing.c
@@ -1,15 +1,15 @@
---- src/printing.c.orig	2013-05-15 04:23:40 UTC
+--- src/printing.c.orig	2017-11-15 04:12:56 UTC
 +++ src/printing.c
-@@ -151,8 +151,6 @@ static gint layout_set_headers(PangoLayo
+@@ -154,8 +154,6 @@ static gint layout_set_headers(PangoLayo
  
  	desc = pango_font_description_from_string(prefs_common_get()->textfont);
  	size = pango_font_description_get_size(desc);
 -	pango_font_description_free(desc);
 -	desc = gtkut_get_default_font_desc();
+ 	debug_print("layout_set_headers: using font %s (style %d, %g pt)\n", pango_font_description_get_family(desc), pango_font_description_get_style(desc), (gdouble)size / PANGO_SCALE);
  	pango_font_description_set_size(desc, size);
  	pango_layout_set_font_description(layout, desc);
- 	pango_font_description_free(desc);
-@@ -417,10 +415,6 @@ static void draw_page(GtkPrintOperation 
+@@ -433,10 +431,6 @@ static void draw_page(GtkPrintOperation 
  	}
  	debug_print("count = %d\n", count);
  
