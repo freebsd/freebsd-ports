f5cd2301bd7f9b223ac58d4dd44d55aface0b397
sunpoet <sunpoet@FreeBSD.org>
2018-02-15 12:48:04 +0000

    Fix build on FreeBSD 10
    PR:		225785
    Submitted by:	lbartoletti <lbartoletti@tuxfamily.org>
    MFH:		2018Q1

---
+++ databases/rocksdb/files/patch-util-arena.cc
@@ -0,0 +1,11 @@
+--- util/arena.cc.orig	2018-01-31 01:15:39 UTC
++++ util/arena.cc
+@@ -33,7 +33,7 @@ const size_t Arena::kInlineSize;
+ 
+ const size_t Arena::kMinBlockSize = 4096;
+ const size_t Arena::kMaxBlockSize = 2u << 30;
+-static const int kAlignUnit = alignof(max_align_t);
++static const int kAlignUnit = alignof(std::max_align_t);
+ 
+ size_t OptimizeBlockSize(size_t block_size) {
+   // Make sure block_size is in optimal range
