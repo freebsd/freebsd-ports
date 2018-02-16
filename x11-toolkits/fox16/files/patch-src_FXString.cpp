61a5293329cc253ba176035d78c135c03a172a8a
Jason E. Hale <jhale@FreeBSD.org>
2018-02-16 11:01:40 +0000

    Update to 1.6.56 Silence patching

--- x11-toolkits/fox16/files/patch-src_FXString.cpp
+++ x11-toolkits/fox16/files/patch-src_FXString.cpp
@@ -1,6 +1,6 @@
---- src/FXString.cpp.orig	2017-04-21 00:34:42 UTC
+--- src/FXString.cpp.orig	2018-01-17 00:37:42 UTC
 +++ src/FXString.cpp
-@@ -2353,7 +2353,7 @@ FXString& FXString::vformat(const FXchar
+@@ -2353,7 +2353,7 @@ FXString& FXString::vformat(const FXchar* fmt,va_list 
      length(result);
      vsnprintf(str,length()+1,fmt,args);
  #elif defined(HAVE_VSNPRINTF)           // Have vsnprintf()
@@ -8,4 +8,4 @@
 +#if (__GLIBC__ > 2) || ((__GLIBC__ == 2) && (__GLIBC_MINOR__ >= 1)) || defined(__FreeBSD__)
      va_list ag;
      va_copy(ag,args);
-     result=vsnprintf(str,length(),fmt,ag);
+     result=vsnprintf(str,empty()?0:length()+1,fmt,ag);
