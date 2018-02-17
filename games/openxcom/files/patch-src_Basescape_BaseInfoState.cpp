221df844c8fd9b1b51cc89c05116c1ce0c9404af
madpilot <madpilot@FreeBSD.org>
2018-02-17 10:12:29 +0000

    - Update devel/yaml-cpp to 0.6.1 - This new version drops dependency on boost-libs - Fix openxcom to build with this new version [1] - Add explicit dependency on boost to net-mgmt/openbmp, now that   yamp-cpp does not pull that in for it [2]
    Approved by:		pizzamig [2],
    			Maintainer timeout [1]
    Differential Revision:	https://reviews.freebsd.org/D14155

---
+++ games/openxcom/files/patch-src_Basescape_BaseInfoState.cpp
@@ -0,0 +1,10 @@
+--- src/Basescape/BaseInfoState.cpp.orig	2014-06-13 19:14:43 UTC
++++ src/Basescape/BaseInfoState.cpp
+@@ -17,6 +17,7 @@
+  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
+  */
+ #include "BaseInfoState.h"
++#include <cmath>
+ #include <sstream>
+ #include "../Engine/Game.h"
+ #include "../Engine/Action.h"
