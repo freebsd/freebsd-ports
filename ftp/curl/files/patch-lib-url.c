35ac31876452088104fe45a36d0ae80cdb074780
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2018-02-17 17:44:07 +0000

    Fix build with CURL_DEBUG enabled
    lib/checksrc.pl is run when CURL_DEBUG is enabled.
    The build failed due to the long line in patched lib/url.c.
    
    ./url.c:593:82: warning: Longer than 79 columns (LONGLINE)
           data->set.no_signal = TRUE; /* different handling of signals and threads */
    checksrc: 0 errors and 1 warnings
    checksrc: 0 errors and 5 warnings suppressed
    gmake[3]: *** [Makefile:3935: checksrc] Error 5
    
    MFH:		2018Q1

--- ftp/curl/files/patch-lib-url.c
+++ ftp/curl/files/patch-lib-url.c
@@ -3,14 +3,15 @@ Forwarded: not-needed
 Author: Peter Pentchev <roam@FreeBSD.org>
 Last-Update: 2010-12-18
 
---- lib/url.c.orig	2017-11-27 12:50:52 UTC
+--- lib/url.c.orig	2018-01-23 07:55:37 UTC
 +++ lib/url.c
-@@ -591,6 +591,9 @@ CURLcode Curl_open(struct Curl_easy **cu
+@@ -589,6 +589,10 @@ CURLcode Curl_open(struct Curl_easy **cu
        data->state.current_speed = -1; /* init to negative == impossible */
        data->set.fnmatch = ZERO_NULL;
        data->set.maxconnects = DEFAULT_CONNCACHE_SIZE; /* for easy handles */
 +#if defined(__FreeBSD_version)
-+      data->set.no_signal = TRUE; /* different handling of signals and threads */
++      /* different handling of signals and threads */
++      data->set.no_signal = TRUE;
 +#endif /* __FreeBSD_version */
  
        Curl_http2_init_state(&data->state);
