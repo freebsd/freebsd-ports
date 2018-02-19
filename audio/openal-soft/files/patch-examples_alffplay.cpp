e6b93d9fc12a0a67565aeb551de44a587f6a8cad
Ken Moore <ken@ixsystems.com>
2018-02-19 18:20:20 -0500

    Add a couple more patches to audio/openal-soft: This was submitted by "strangeqargo" on the TrueOS Community chat (Telegram) and fixes a couple typos in source preventing proper functionality on FreeBSD.

---
+++ audio/openal-soft/files/patch-examples_alffplay.cpp
@@ -0,0 +1,9 @@
+--- examples/alffplay.cpp.orig 2018-02-19 01:00:00 UTC
++++ examples/alffplay.cpp
+@@ -18,6 +18,7 @@
+ #include <mutex>
+ #include <deque>
+ #include <array>
++#include <cmath>
+ 
+ extern "C" {
