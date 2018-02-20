b8938cf4a6e664ab00341b4db0852694f4e537f9
Jan Beich <jbeich@FreeBSD.org>
2018-02-20 00:09:17 +0000

    games/openbor: update to 4601
    Changes:	https://github.com/DCurrent/openbor/compare/35968d0...ab066c1

--- games/openbor/files/patch-source_gamelib_packfile.c
+++ games/openbor/files/patch-source_gamelib_packfile.c
@@ -1,12 +1,12 @@
-Reset seek position for BGM with more than 64 tracks
+Reset seek position for BGM with more than 128 tracks
 
---- source/gamelib/packfile.c.orig	2015-04-18 21:22:03 UTC
+--- source/gamelib/packfile.c.orig	2018-02-19 15:02:50 UTC
 +++ source/gamelib/packfile.c
-@@ -1399,7 +1399,7 @@ void packfile_music_read(fileliststruct 
+@@ -1408,7 +1408,7 @@ void packfile_music_read(fileliststruct *filelist, int
          getBasePath(packfile, filelist[i].filename, 1);
          if(stristr(packfile, ".pak"))
          {
--            memset(filelist[i].bgmTracks, 0, 256);
+-            memset(filelist[i].bgmTracks, 0, PACKFILE_PATH_MAX);
 +            memset(filelist[i].bgmTracks, 0, sizeof(filelist[i].bgmTracks));
              filelist[i].nTracks = 0;
              fd = fopen(packfile, "rb");
