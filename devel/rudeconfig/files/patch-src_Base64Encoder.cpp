6455457f9f96c54e7f93107bfdff50a4e15e5fc0
Alexey Elymanov <strangeqargo@gmail.com>
2018-02-21 18:15:39 +0300

    fix src/Base64Encoder.cpp:170:25: error: cast from pointer to smaller type 'char' loses information

---
+++ devel/rudeconfig/files/patch-src_Base64Encoder.cpp
@@ -0,0 +1,11 @@
+--- src/Base64Encoder.cpp.orig	2018-02-21 15:13:36 UTC
++++ src/Base64Encoder.cpp
+@@ -167,7 +167,7 @@ char * Base64Encoder::encode(const char *data, int dat
+                   linelength=0;
+             }
+       };
+-      retval[outlength]=(char) NULL;
++      retval[outlength]='\0';
+       return retval;
+ }
+ 
