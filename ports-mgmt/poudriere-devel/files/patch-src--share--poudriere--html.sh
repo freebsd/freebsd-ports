6817bcae902ab14f55e4f6de09400f18285503f4
bdrewery <bdrewery@FreeBSD.org>
2018-02-22 05:52:57 +0000

    Fix jail cleanup waiting too long on HTML JSON process to exit

--- ports-mgmt/poudriere-devel/files/patch-src--share--poudriere--html.sh
+++ ports-mgmt/poudriere-devel/files/patch-src--share--poudriere--html.sh
@@ -4,21 +4,25 @@ Date:   Sat Feb 3 16:39:51 2018 -0800
 
     HTML: Hide harmless shared lock failures
 
-diff --git src/share/poudriere/include/html.sh src/share/poudriere/include/html.sh
-index c56cb6520..98bcd84f4 100644
---- src/share/poudriere/include/html.sh
+commit 129a3b0bfde6bf77982ebece039aaa745ead8b2a
+Author: Bryan Drewery <bryan@shatow.net>
+Date:   Sat Feb 3 17:58:53 2018 -0800
+
+    HTML: Only wait for shared locks for 2 seconds each
+
+--- src/share/poudriere/include/html.sh.orig	2018-02-22 05:50:35 UTC
 +++ src/share/poudriere/include/html.sh
 @@ -78,11 +78,11 @@ html_json_main() {
  build_all_json() {
  	critical_start
  	build_json
 -	if slock_acquire "json_jail_${MASTERNAME}"; then
-+	if slock_acquire "json_jail_${MASTERNAME}" 2>/dev/null; then
++	if slock_acquire "json_jail_${MASTERNAME}" 2 2>/dev/null; then
  		build_jail_json
  		slock_release "json_jail_${MASTERNAME}"
  	fi
 -	if slock_acquire "json_top"; then
-+	if slock_acquire "json_top" 2>/dev/null; then
++	if slock_acquire "json_top" 2 2>/dev/null; then
  		build_top_json
  		slock_release "json_top"
  	fi
@@ -27,7 +31,7 @@ index c56cb6520..98bcd84f4 100644
  	local dest="$3"
  
 -	slock_acquire html_base || return 0
-+	slock_acquire html_base 2>/dev/null || return 0
++	slock_acquire html_base 2>/dev/null 2 || return 0
  
  	# Update the base copy
  	mkdir -p "${base}"
