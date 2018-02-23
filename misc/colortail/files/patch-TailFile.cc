87f2caa2be66b01664322065f5bd8740f2daaf3b
sunpoet <sunpoet@FreeBSD.org>
2018-02-23 19:25:09 +0000

    Change MASTER_SITES to GitHub
    - Convert to options target helper
    - Regenerate patch files with makepatch:
    - Update pkg-descr
    - Update WWW
    - Take maintainership

--- misc/colortail/files/patch-TailFile.cc
+++ misc/colortail/files/patch-TailFile.cc
@@ -1,14 +1,16 @@
---- TailFile.cc.bak	2010-07-31 14:12:15.145254811 +0200
-+++ TailFile.cc	2010-07-31 14:13:31.996950208 +0200
-@@ -352,7 +352,10 @@
+--- TailFile.cc.orig	2010-06-02 20:41:36 UTC
++++ TailFile.cc
+@@ -352,8 +352,11 @@ void TailFile::follow_print(int n, int v
        int ch = fgetc(m_file);
  
        // add the character to the string
 -      m_follow_buffer->put(ch);
+-      
 +      if (ch == EOF)
 +          clearerr(m_file);     // required by POSIX and ANSI
 +      else
 +          m_follow_buffer->put(ch);
-       
++    
        // check if return
        if (ch == '\n')
+       {
