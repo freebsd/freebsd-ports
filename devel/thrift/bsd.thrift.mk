2f7ba43aec7fb6744063691b5db83ca98eae1345
lx <lx@FreeBSD.org>
2018-02-24 20:40:27 +0000

    Update thrift ports to 0.11.0.
    PR:		225798
    Approved by:	maintainer timeout

--- devel/thrift/bsd.thrift.mk
+++ devel/thrift/bsd.thrift.mk
@@ -5,8 +5,7 @@
 # in your makefile, set:
 # PORTVERSION=	${THRIFT_PORTVERSION}
 # see $PORTSDIR/devel/thrift for examples 
-THRIFT_PORTVERSION=	0.9.3
-THRIFT_PORTREVISION=	1
+THRIFT_PORTVERSION=	0.11.0
 
 CONFIGURE_ARGS+=	\
 		--without-tests
