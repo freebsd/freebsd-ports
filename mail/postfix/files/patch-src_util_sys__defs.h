de0cd10fe2df46eaebd18405a6924031137aaa54
Olli Hauer <ohauer@FreeBSD.org>
2018-02-25 15:27:30 +0000

    - update to 3.3.0 - add patch for MySQL 8.x [1} - add blacklistd support [2]
    additional:
    - regenerate patch for src/util/sys_defs.h
    
    Release Notes:
    ftp://ftp.porcupine.org/mirrors/postfix-release/official/postfix-3.3.0.RELEASE_NOTES
    
    PR:		220224 [1], 225664 [2]
    Submitted by:	Klaus Mayr [1], Koichiro IWAO [2]

--- mail/postfix/files/patch-src_util_sys__defs.h
+++ mail/postfix/files/patch-src_util_sys__defs.h
@@ -1,6 +1,6 @@
---- src/util/sys_defs.h.orig	2016-05-15 16:39:11 UTC
+--- src/util/sys_defs.h.orig	2017-06-18 19:30:20 UTC
 +++ src/util/sys_defs.h
-@@ -26,6 +26,7 @@
+@@ -30,6 +30,7 @@
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
      || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
