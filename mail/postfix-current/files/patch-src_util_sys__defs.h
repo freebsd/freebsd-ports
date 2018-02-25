38c3eb84c8afed2311afdfe8fa80694b1b858250
Olli Hauer <ohauer@FreeBSD.org>
2018-02-25 15:39:07 +0000

    - update to 3.4-20180222 - add patch for MySQL 8.x [1} - add blacklistd support [2]
    additional:
     regenerate patch for src/util/sys_defs.h
    
    PR:           220224 [1], 225664 [2]
    Submitted by: Klaus Mayr [1], Koichiro IWAO [2]

--- mail/postfix-current/files/patch-src_util_sys__defs.h
+++ mail/postfix-current/files/patch-src_util_sys__defs.h
@@ -1,6 +1,6 @@
---- src/util/sys_defs.h.orig	2016-07-09 20:09:34 UTC
+--- src/util/sys_defs.h.orig	2017-06-18 19:30:20 UTC
 +++ src/util/sys_defs.h
-@@ -27,6 +27,7 @@
+@@ -30,6 +30,7 @@
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
      || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
