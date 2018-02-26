e44fdb0735070b500835a2ba5d938de90d7483df
jhale <jhale@FreeBSD.org>
2018-02-26 13:35:00 +0000

    Fix stage-qa errors when building a non-default Python flavor when DOCS option is enabled. For example, if py27 is default and try to build the py36 flavor, it was using the py27 flavor of sphinx for the docs build and leaving behind python 2.7 bytecode in STAGEDIR:
    ====> Running Q/A tests (stage-qa)
    Error: 'lib/python3.6/site-packages/cssselect/parser.pyc' is referring to /wrkdirs/usr/ports/www/py-cssselect/work-py36/stage
    Error: 'lib/python3.6/site-packages/cssselect/xpath.pyc' is referring to /wrkdirs/usr/ports/www/py-cssselect/work-py36/stage
    Error: 'lib/python3.6/site-packages/cssselect/__init__.pyc' is referring to /wrkdirs/usr/ports/www/py-cssselect/work-py36/stage
    *** Error code 1
    
    While here:
    Add LICENSE_FILE
    Add NO_ARCH
    Pet portlint
    
    Approved by:	portmgr (blanket)

--- www/py-cssselect/files/patch-docs__conf.py
+++ www/py-cssselect/files/patch-docs__conf.py
@@ -1,6 +1,6 @@
---- ./docs/conf.py.orig	2014-04-01 22:42:52.000000000 +0200
-+++ ./docs/conf.py	2014-04-01 22:43:15.000000000 +0200
-@@ -244,7 +244,3 @@
+--- docs/conf.py.orig	2013-09-15 08:25:25 UTC
++++ docs/conf.py
+@@ -244,7 +244,3 @@ texinfo_documents = [
  
  # How to display URL addresses: 'footnote', 'no', or 'inline'.
  #texinfo_show_urls = 'footnote'
