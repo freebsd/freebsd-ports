a115caf44f2feac28fb5e6cc6d5c2a8923fcfb84
Emanuel Haupt <ehaupt@FreeBSD.org>
2018-02-26 16:11:04 +0000

    Provide a patch that removes the hard coded 5% cpu cycle donation minimum but keep the configurable default value at 5%.
    This gives the users the choice to go lower than 5% if desired.

---
+++ net-p2p/xmrig/files/patch-src_donate.h
@@ -0,0 +1,11 @@
+--- src/donate.h.orig	2018-02-18 21:31:50 UTC
++++ src/donate.h
+@@ -37,7 +37,7 @@
+  * XMR: 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
+  * BTC: 1P7ujsXeX7GxQwHNnJsRMgAdNkFZmNVqJT
+  */
+-constexpr const int kDonateLevel = 5;
++constexpr const int kDonateLevel = 0;
+ 
+ 
+ #endif /* __DONATE_H__ */
