055ff1837765f0b5fe2a3f1022c187b7aefeee75
Mathieu Arnold <mat@FreeBSD.org>
2018-02-26 12:39:59 +0000

    Update to latest commit.
    Sponsored by:	Absolight

--- dns/bind9-devel/files/patch-bin_named_include_named_globals.h
+++ dns/bind9-devel/files/patch-bin_named_include_named_globals.h
@@ -1,8 +1,8 @@
 We reference the pid file as being run/named/pid everywere else.
 
---- bin/named/include/named/globals.h.orig	2018-01-17 06:56:09 UTC
+--- bin/named/include/named/globals.h.orig	2018-02-26 06:30:28 UTC
 +++ bin/named/include/named/globals.h
-@@ -125,7 +125,7 @@ EXTERN isc_boolean_t		named_g_forcelock	
+@@ -128,7 +128,7 @@ EXTERN isc_boolean_t		named_g_forcelock	
  #if NAMED_RUN_PID_DIR
  EXTERN const char *		named_g_defaultpidfile 	INIT(NAMED_LOCALSTATEDIR
  							     "/run/named/"
