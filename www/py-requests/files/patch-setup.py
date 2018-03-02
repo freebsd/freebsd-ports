5f5e57a271da0f107d24d58147099cab27685b6c
koobs <koobs@FreeBSD.org>
2018-03-02 11:53:35 +0000

    www/py-requests: Update to 2.18.4
    While I'm here:
    
    Add note about test suite issue. There's no clear & quick way to resolve this
    in the short term without further investigation/yak shaving.
    
    Changelog:
    
      https://github.com/requests/requests/blob/v2.18.4/HISTORY.rst
    
    PR:		226078
    Submitted by:	yuri

--- www/py-requests/files/patch-setup.py
+++ www/py-requests/files/patch-setup.py
@@ -1,22 +1,8 @@
-From 8d1228cd1943bfe89ffaae32e15b2feb3f8479f5 Mon Sep 17 00:00:00 2001
-From: Cory Benfield <lukasaoz@gmail.com>
-Date: Thu, 20 Jul 2017 10:08:50 +0100
-Subject: [PATCH] Allow urllib3 1.22
-
----
- setup.py | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
 # pytest-cov and its dependencies are not compulsory
 
---- setup.py.orig	2017-06-14 17:44:15 UTC
+--- setup.py.orig	2018-02-24 01:49:05 UTC
 +++ setup.py
-@@ -44,11 +44,11 @@ packages = ['requests']
- requires = [
-     'chardet>=3.0.2,<3.1.0',
-     'idna>=2.5,<2.6',
--    'urllib3>=1.21.1,<1.22',
-+    'urllib3>=1.21.1,<1.23',
+@@ -48,7 +48,7 @@ requires = [
      'certifi>=2017.4.17'
  
  ]
