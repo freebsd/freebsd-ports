9b61d442c10bb79e74b06953030e4d906903eabd
joneum <joneum@FreeBSD.org>
2018-03-03 19:54:26 +0000

    www/nginx: Update passenger module to 5.2.1
    Changelog: https://github.com/phusion/passenger/commits/stable-5.2

--- www/nginx/files/extra-patch-passenger-build-nginx.rb
+++ www/nginx/files/extra-patch-passenger-build-nginx.rb
@@ -1,6 +1,6 @@
---- ../passenger-5.1.12/build/nginx.rb.orig	2013-10-26 18:00:00.000000000 -0400
-+++ ../passenger-5.1.12/build/nginx.rb	2016-05-09 18:21:22.426777000 -0400
-@@ -33,13 +33,12 @@
+--- ../passenger-5.2.1/build/nginx.rb.orig	2018-03-03 21:21:23.833026000 +0100
++++ ../passenger-5.2.1/build/nginx.rb	2018-03-03 21:25:02.268013000 +0100
+@@ -39,13 +39,12 @@ auto_generated_sources = %w(
  desc "Build Nginx support files"
  task :nginx => [
    :nginx_without_native_support,
@@ -15,7 +15,7 @@
  ].compact
  
  # Workaround for https://github.com/jimweirich/rake/issues/274
-@@ -47,7 +46,6 @@
+@@ -53,7 +52,6 @@ task :_nginx => :nginx
  
  task :nginx_without_native_support => [
    auto_generated_sources,
@@ -23,7 +23,7 @@
    COMMON_LIBRARY.only(*NGINX_LIBS_SELECTOR).link_objects
  ].flatten
  
-@@ -55,7 +53,6 @@
+@@ -61,7 +59,6 @@ task :nginx_without_native_support => [
  # it also creates a namespace:clean task to clean up the output_dir
  task :nginx_dynamic_without_native_support => [
    auto_generated_sources,
