484332e24f42927203a93d96a3c3ec72bb2d7227
Thomas Zander <riggs@FreeBSD.org>
2018-03-04 16:53:36 +0000

    Fix build with clang 6.0.0

---
+++ net/minisapserver/files/patch-sapserver.cpp
@@ -0,0 +1,11 @@
+--- sapserver.cpp.orig	2009-03-01 19:58:22 UTC
++++ sapserver.cpp
+@@ -128,7 +128,7 @@ help(void)
+ static void
+ version(void)
+ {
+-    puts(PACKAGE_NAME" version "PACKAGE_VERSION"\n"
++    puts(PACKAGE_NAME " version " PACKAGE_VERSION"\n"
+     "Copyright (C) 1999-2005 VideoLAN project\n"
+     "This is free software; see the source for copying conditions.\n"
+     "There is NO warranty; not even for MERCHANTABILITY or\n"
