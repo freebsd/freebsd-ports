6d7a3eb2b11e259380477a2acbe3556fba6c57c1
Yuri Victorovich <yuri@FreeBSD.org>
2018-03-04 04:11:49 +0000

    net-mgmt/cacti: Update to 1.1.36
    This is a bugfix release.
    
    PR:		226334
    Submitted by:	Daniel Austin <freebsd-ports@dan.me.uk> (maintainer)
    Approved by:	tcberner (mentor, implicit)

--- net-mgmt/cacti/files/patch-include__global_settings.php
+++ net-mgmt/cacti/files/patch-include__global_settings.php
@@ -1,10 +1,10 @@
---- include/global_settings.php.orig	2017-08-13 04:06:37 UTC
+--- include/global_settings.php.orig	2018-02-26 01:12:03 UTC
 +++ include/global_settings.php
-@@ -144,9 +144,9 @@ $settings = array(
+@@ -143,9 +143,9 @@ $settings = array(
  			),
  		'path_cactilog' => array(
  			'friendly_name' => __('Cacti Log Path'),
--			'description' => __('The path to your Cacti log file (if blank, defaults to <path_cacti>/log/cacti.log)'),
+-			'description' => __('The path to your Cacti log file (if blank, defaults to &lt;path_cacti&gt;/log/cacti.log)'),
 +			'description' => __('The path to your Cacti log file (if blank, defaults to /var/log/cacti/log)'),
  			'method' => 'filepath',
 -			'default' => $config['base_path'] . '/log/cacti.log',
--- net-mgmt/cacti/files/patch-poller_maintenance.php
+++ net-mgmt/cacti/files/patch-poller_maintenance.php
@@ -1,19 +1,11 @@
---- poller_maintenance.php.orig	2017-08-13 04:06:37 UTC
+--- poller_maintenance.php.orig	2018-02-26 01:12:04 UTC
 +++ poller_maintenance.php
-@@ -197,14 +197,14 @@ function logrotate_rotatenow () {
+@@ -223,7 +223,7 @@ function logrotate_rotatenow () {
  
  	$log = read_config_option('path_cactilog');
  	if ($log == '') {
 -		$log = $config['base_path'] . '/log/cacti.log';
-+		$log = '/var/log/cacti/log';
++		$log = /var/log/cacti/log';
  	}
  
- 	set_config_option('logrotate_lastrun', time());
- 
- 	clearstatcache();
- 
--	if (is_writable(dirname($log) . '/') && is_writable($log)) {
-+	if (is_writable('/var/log/cacti') && is_writable($log)) {
- 		$perms = octdec(substr(decoct( fileperms($log) ), 2));
- 		$owner = fileowner($log);
- 		$group = filegroup($log);
+ 	$run_time = time();
