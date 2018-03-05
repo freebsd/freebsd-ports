a18bcddba14b725e86b8afde82cc760feb2b216a
Jan Beich <jbeich@FreeBSD.org>
2018-03-05 02:06:52 +0000

    www/domoticz: regen patches via "make makepatch"
    PR:		224181
    Submitted by:	Xavier Beaudouin (maintainer)

--- www/domoticz/files/patch-getgit.cmake
+++ www/domoticz/files/patch-getgit.cmake
@@ -1,5 +1,5 @@
---- getgit.cmake.orig	2016-04-01 15:27:18.517929000 +0200
-+++ getgit.cmake	2016-04-01 15:27:52.572836000 +0200
+--- getgit.cmake.orig	2017-07-30 10:19:41 UTC
++++ getgit.cmake
 @@ -1,66 +1,7 @@
  # this macro gets called as a custom build step by running make
  # please take into account, that the variable 'SOURCE_DIR' has been defined by the caller
