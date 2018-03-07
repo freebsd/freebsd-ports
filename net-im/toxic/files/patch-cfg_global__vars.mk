188e3582337c54a220e3de4047935adbd07911e2
thierry <thierry@FreeBSD.org>
2018-03-07 20:57:27 +0000

    Upgrade to 0.8.2 and chase the new toxcore.
    PR:		226311

--- net-im/toxic/files/patch-cfg_global__vars.mk
+++ net-im/toxic/files/patch-cfg_global__vars.mk
@@ -1,14 +1,15 @@
---- cfg/global_vars.mk.orig	2017-11-17 22:44:56 UTC
+--- cfg/global_vars.mk.orig	2018-02-27 23:46:56 UTC
 +++ cfg/global_vars.mk
 @@ -1,11 +1,7 @@
  # Version
- TOXIC_VERSION = 0.8.1
+-TOXIC_VERSION = 0.8.1
 -REV = $(shell git rev-list HEAD --count 2>/dev/null || echo -n "error")
 -ifneq (, $(findstring error, $(REV)))
 -    VERSION = $(TOXIC_VERSION)
 -else
 -    VERSION = $(TOXIC_VERSION)_r$(REV)
 -endif
++TOXIC_VERSION = 0.8.2
 +#REV = $(shell git rev-list HEAD --count 2>/dev/null || echo -n "error")
 +VERSION = $(TOXIC_VERSION)
  
