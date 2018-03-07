fa7115d57ddca1e6317aed68182c86388f17e22c
pizzamig <pizzamig@FreeBSD.org>
2018-03-07 12:41:33 +0000

    net-mgmt/openbmp: Update to 0.14.0
    Release notes: https://github.com/OpenBMP/openbmp/releases/tag/v0.14.0
    Sponsored by:	trivago N.V.

--- net-mgmt/openbmp/files/patch-Server_src_client__thread.cpp
+++ net-mgmt/openbmp/files/patch-Server_src_client__thread.cpp
@@ -1,9 +1,9 @@
 --- Server/src/client_thread.cpp.orig	2017-09-14 09:34:45 UTC
 +++ Server/src/client_thread.cpp
-@@ -227,7 +227,7 @@ void *ClientThread(void *arg) {
+@@ -242,7 +242,7 @@ void *ClientThread(void *arg) {
+         LOG_INFO("%s: %s - Thread for sock [%d] ended", cInfo.client->c_ip, str, cInfo.client->c_sock);
          close(sock_fds[0]);
          close(sock_fds[1]);
- 
 -#ifndef __APPLE__
 +#if !defined(__APPLE__) and !defined(__FreeBSD__)
      } catch (abi::__forced_unwind&) {
