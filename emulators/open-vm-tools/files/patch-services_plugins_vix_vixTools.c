445f4b448a6d6ef64f7a3530d531cd177e916429
Josh Paetzel <jpaetzel@FreeBSD.org>
2018-03-10 22:56:54 +0000

    Fix bug that causes crashes when running commands from the host
    PR:	226478
    Submitted by:	german.mb@gmail.com
    Reviewed by:	John Wolfe <jwolfe@vmware.com>

--- emulators/open-vm-tools/files/patch-services_plugins_vix_vixTools.c
+++ emulators/open-vm-tools/files/patch-services_plugins_vix_vixTools.c
@@ -1,29 +1,29 @@
---- services/plugins/vix/vixTools.c.orig	2017-12-15 21:11:49 UTC
+--- services/plugins/vix/vixTools.c.orig	2018-02-24 17:50:37 UTC
 +++ services/plugins/vix/vixTools.c
-@@ -52,6 +52,7 @@
- #include <stdarg.h>
- #include <fcntl.h>
- #include <errno.h>
-+#include <limits.h>
+@@ -10135,7 +10135,7 @@ abort:
+    struct passwd pwd;
+    struct passwd *ppwd = &pwd;
+    char *buffer = NULL; // a pool of memory for Posix_Getpwnam_r() to use.
+-   size_t bufferSize;
++   long bufferSize;
  
- #ifdef _WIN32
- #include <WTypes.h>
-@@ -7330,7 +7331,7 @@ VixToolsRunScript(VixCommandRequestHeader *requestMsg,
-          goto abort;
-       }
-    }
--   for (var = 0; var <= 0xFFFFFFFF; var++) {
-+   for (var = 0; ; var++) {
-       free(tempScriptFilePath);
-       tempScriptFilePath = Str_SafeAsprintf(NULL,
-                                             "%s"DIRSEPS"%s%d%s",
-@@ -7372,6 +7373,9 @@ VixToolsRunScript(VixCommandRequestHeader *requestMsg,
-           * deletes it), we should not try 4+ billion times.
-           */
-          break;
-+      }
-+      if (var == INT_MAX) {
-+          break;
-       }
-    }
-    if (fd < 0) {
+    /*
+     * For POSIX systems, look up the uid of 'username', and compare
+@@ -10148,9 +10148,15 @@ abort:
+     * Multiply by 4 to compensate for the conversion to UTF-8 by
+     * the Posix_Getpwnam_r() wrapper.
+     */
+-   bufferSize = (size_t) sysconf(_SC_GETPW_R_SIZE_MAX) * 4;
++   errno = 0;
++   bufferSize = sysconf(_SC_GETPW_R_SIZE_MAX);
++   if ((errno != 0) || (bufferSize <= 0)) {
++      bufferSize = 16 * 1024;  // Unlimited; pick something reasonable
++   }
+ 
+-   buffer = Util_SafeMalloc(bufferSize);
++   bufferSize *= 4;
++
++   buffer = Util_SafeMalloc((size_t)bufferSize);
+ 
+    if (Posix_Getpwnam_r(username, &pwd, buffer, bufferSize, &ppwd) != 0 ||
+        NULL == ppwd) {
