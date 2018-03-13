b61c22d6e735eec4875bdab8a205a566159c0c39
timur <timur@FreeBSD.org>
2018-03-13 21:00:04 +0000

    Security update for samba45, samba46, samba47 ports to address CVE-2018-1050 and CVE-2018-1057.
    As AD DC doesn't work in samba46 and was broken again in samba 4.7.4 - the attack vector is quite narrow.
    
    Security:	CVE-2018-1050
    		CVE-2018-1057
    Sponsored by:	iXsystems Inc.

--- net/samba47/files/patch-buildtools__wafsamba__samba_autoconf.py
+++ net/samba47/files/patch-buildtools__wafsamba__samba_autoconf.py
@@ -1,6 +1,47 @@
---- buildtools/wafsamba/samba_autoconf.py.orig	2017-07-04 10:05:25 UTC
+--- buildtools/wafsamba/samba_autoconf.py.orig	2016-10-07 04:45:35 UTC
 +++ buildtools/wafsamba/samba_autoconf.py
-@@ -873,7 +873,7 @@ def SAMBA_CHECK_UNDEFINED_SYMBOL_FLAGS(c
+@@ -548,7 +548,7 @@ def library_flags(self, libs):
+ 
+ 
+ @conf
+-def CHECK_LIB(conf, libs, mandatory=False, empty_decl=True, set_target=True, shlib=False):
++def CHECK_LIB(conf, libs, mandatory=False, empty_decl=True, set_target=True, shlib=False, msg=None):
+     '''check if a set of libraries exist as system libraries
+ 
+     returns the sublist of libs that do exist as a syslib or []
+@@ -568,11 +568,29 @@ int foo()
+             ret.append(lib)
+             continue
+ 
++        if msg is None:
++            msg = 'Checking for library %s' % lib
++
+         (ccflags, ldflags, cpppath) = library_flags(conf, lib)
++
+         if shlib:
+-            res = conf.check(features='c cshlib', fragment=fragment, lib=lib, uselib_store=lib, ccflags=ccflags, ldflags=ldflags, uselib=lib.upper(), mandatory=False)
++            res = conf.check(features='c cshlib',
++                             fragment=fragment,
++                             lib=lib,
++                             uselib_store=lib,
++                             ccflags=ccflags,
++                             ldflags=ldflags,
++                             uselib=lib.upper(),
++                             mandatory=False,
++                             msg=msg)
+         else:
+-            res = conf.check(lib=lib, uselib_store=lib, ccflags=ccflags, ldflags=ldflags, uselib=lib.upper(), mandatory=False)
++            res = conf.check(lib=lib,
++                             uselib_store=lib,
++                             ccflags=ccflags,
++                             ldflags=ldflags,
++                             uselib=lib.upper(),
++                             mandatory=False,
++                             msg=msg)
+ 
+         if not res:
+             if mandatory:
+@@ -873,7 +891,7 @@ def SAMBA_CHECK_UNDEFINED_SYMBOL_FLAGS(c
          conf.env.undefined_ldflags = conf.ADD_LDFLAGS('-Wl,-no-undefined', testflags=True)
  
      if not sys.platform.startswith("openbsd") and conf.env.undefined_ignore_ldflags == []:
