153125ed76cb0ba0c6daf7f617762b7a514b261e
Emanuel Haupt <ehaupt@FreeBSD.org>
2018-03-13 10:09:09 +0000

    Update to 3.1.1

--- finance/electrum/files/patch-setup.py
+++ finance/electrum/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2017-03-23 01:56:41 UTC
+--- setup.py.orig	2018-03-12 17:25:11 UTC
 +++ setup.py
-@@ -21,12 +21,6 @@ if platform.system() in ['Linux', 'FreeB
+@@ -27,12 +27,6 @@ if platform.system() in ['Linux', 'FreeB
      parser.add_argument('--root=', dest='root_path', metavar='dir', default='/')
      opts, _ = parser.parse_known_args(sys.argv[1:])
      usr_share = os.path.join(sys.prefix, "share")
@@ -13,3 +13,12 @@
      data_files += [
          (os.path.join(usr_share, 'applications/'), ['electrum.desktop']),
          (os.path.join(usr_share, 'pixmaps/'), ['icons/electrum.png'])
+@@ -86,8 +80,3 @@ setup(
+     url="https://electrum.org",
+     long_description="""Lightweight Bitcoin Wallet"""
+ )
+-
+-# Optional modules (not required to run Electrum)
+-import pip
+-opt_modules = requirements_hw + ['pycryptodomex']
+-[ pip.main(['install', m]) for m in opt_modules ]
