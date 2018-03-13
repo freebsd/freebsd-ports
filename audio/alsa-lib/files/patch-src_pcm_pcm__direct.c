5949e05a7f1c2de749c61f9f864373465467cb8b
jbeich <jbeich@FreeBSD.org>
2018-03-13 14:11:42 +0000

    audio/alsa-lib: drop _WANT_SEMUN

--- audio/alsa-lib/files/patch-src_pcm_pcm__direct.c
+++ audio/alsa-lib/files/patch-src_pcm_pcm__direct.c
@@ -1,17 +1,18 @@
 --- src/pcm/pcm_direct.c.orig	2016-03-31 13:10:39 UTC
 +++ src/pcm/pcm_direct.c
-@@ -44,12 +44,16 @@
+@@ -44,12 +44,17 @@
   *
   */
   
-+#if defined(__NetBSD__)
-+typedef __semun semun;
-+#elif !defined(__DragonFly__) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
++#if !defined(__DragonFly__) && !defined(__OpenBSD__) \
++  && !(defined(__FreeBSD__) && __FreeBSD__ < 12)
  union semun {
  	int              val;    /* Value for SETVAL */
  	struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
  	unsigned short  *array;  /* Array for GETALL, SETALL */
++#ifdef __linux__
  	struct seminfo  *__buf;  /* Buffer for IPC_INFO (Linux specific) */
++#endif
  };
 +#endif
   
