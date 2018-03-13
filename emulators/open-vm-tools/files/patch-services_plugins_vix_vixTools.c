c589e60b6de42399f9d656e9025caab6c81fe761
jpaetzel <jpaetzel@FreeBSD.org>
2018-03-13 14:20:24 +0000

    Reintegrate accidentally removed patch
    Along the way remove a patch that is no longer
    needed.
    
    Reported by:	John Wolfe <jwolfe@vmware.com>
    Pointyhat:	jpaetzel

--- emulators/open-vm-tools/files/patch-services_plugins_vix_vixTools.c
+++ emulators/open-vm-tools/files/patch-services_plugins_vix_vixTools.c
@@ -1,6 +1,33 @@
---- services/plugins/vix/vixTools.c.orig	2018-02-24 17:50:37 UTC
+--- services/plugins/vix/vixTools.c.orig	2017-12-15 21:11:49 UTC
 +++ services/plugins/vix/vixTools.c
-@@ -10135,7 +10135,7 @@ abort:
+@@ -52,6 +52,7 @@
+ #include <stdarg.h>
+ #include <fcntl.h>
+ #include <errno.h>
++#include <limits.h>
+ 
+ #ifdef _WIN32
+ #include <WTypes.h>
+@@ -7330,7 +7331,7 @@ VixToolsRunScript(VixCommandRequestHeader *requestMsg,
+          goto abort;
+       }
+    }
+-   for (var = 0; var <= 0xFFFFFFFF; var++) {
++   for (var = 0; ; var++) {
+       free(tempScriptFilePath);
+       tempScriptFilePath = Str_SafeAsprintf(NULL,
+                                             "%s"DIRSEPS"%s%d%s",
+@@ -7373,6 +7374,9 @@ VixToolsRunScript(VixCommandRequestHeader *requestMsg,
+           */
+          break;
+       }
++      if (var == INT_MAX) {
++         break;
++      }
+    }
+    if (fd < 0) {
+       /*
+@@ -10131,7 +10135,7 @@ abort:
     struct passwd pwd;
     struct passwd *ppwd = &pwd;
     char *buffer = NULL; // a pool of memory for Posix_Getpwnam_r() to use.
@@ -9,7 +36,7 @@
  
     /*
      * For POSIX systems, look up the uid of 'username', and compare
-@@ -10148,9 +10148,15 @@ abort:
+@@ -10144,9 +10148,15 @@ abort:
      * Multiply by 4 to compensate for the conversion to UTF-8 by
      * the Posix_Getpwnam_r() wrapper.
      */
