6b7727dafda85af3155fdda2978bad0d96f2d11c
lwhsu <lwhsu@FreeBSD.org>
2018-03-17 06:31:11 +0000

    - Update LibreOffice to 6.0.2 [1] - Build offline help when DOCS option enabled [2] - Fix bad symlink and add missing dependencies [3]
    PR:		224288 [1] 197071 [2] 226344 [3]
    Submitted by:	Greg V <greg@unrelenting.technology> [1]
    		bergerkos@yahoo.co.uk [2]
    Reported by:	joneum [3]
    Helped & tested by:	Daniel Ebdrup
    			Greg Veldman <freebsd@gregv.net>
    			Per olof Ljungmark <peo@nethead.se>
    			StariKarp <starikarp@yandex.com>
    			pi
    Security:	289269f1-0def-11e8-99b0-d017c2987f9a

--- editors/libreoffice/files/patch-shell_source_unix_exec_shellexec.cxx
+++ editors/libreoffice/files/patch-shell_source_unix_exec_shellexec.cxx
@@ -1,11 +1,11 @@
---- shell/source/unix/exec/shellexec.cxx.orig	2017-01-12 00:54:33 UTC
-+++ shell/source/unix/exec/shellexec.cxx
-@@ -157,7 +157,7 @@ void SAL_CALL ShellExec::execute( const 
-         if (std::getenv("LIBO_FLATPAK") != nullptr) {
-             aBuffer.append("/app/bin/xdg-open");
-         } else {
--            aBuffer.append("/usr/bin/xdg-open");
-+            aBuffer.append("%%LOCALBASE%%/bin/xdg-open");
-         }
+--- shell/source/unix/exec/shellexec.cxx.orig	2017-12-08 02:26:19.000000000 +0300
++++ shell/source/unix/exec/shellexec.cxx	2017-12-12 15:51:21.551684000 +0300
+@@ -145,7 +145,7 @@
+         aBuffer.append("open --");
+ #else
+         // Just use xdg-open on non-Mac
+-        aBuffer.append("/usr/bin/xdg-open");
++        aBuffer.append("%%LOCALBASE%%/bin/xdg-open");
  #endif
          aBuffer.append(" ");
+         escapeForShell(aBuffer, OUStringToOString(aURL, osl_getThreadTextEncoding()));
