12cceed83618ad4cc63d3859fc94b83a7ff8ea6d
dbaio <dbaio@FreeBSD.org>
2018-03-17 12:15:31 +0000

    textproc/fzf: Fix patches
    The patches for this port got mangled in the last update.
    
    This patch corrects that so the completion for kill(1) works again
    if one is using the example shell completion files.
    
    PR:		226654
    Submitted by:	Sascha Holzleiter <sascha@root-login.org> (maintainer)

--- textproc/fzf/files/patch-shell_completion.bash
+++ textproc/fzf/files/patch-shell_completion.bash
@@ -1,12 +1,11 @@
 --- shell/completion.bash.orig	2017-12-03 14:55:24 UTC
 +++ shell/completion.bash
-@@ -219,7 +219,8 @@ _fzf_complete_kill() {
+@@ -219,7 +219,7 @@ _fzf_complete_kill() {
  
    local selected fzf
    fzf="$(__fzfcmd_complete)"
 -  selected=$(command ps -ef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" $fzf -m | awk '{print $2}' | tr '\n' ' ')
-+  selected=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAU
-+LT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" $fzf -m | awk '{print $2}' | tr '\n' ' ')
++  selected=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" $fzf -m | awk '{print $2}' | tr '\n' ' ')
    printf '\e[5n'
  
    if [ -n "$selected" ]; then
--- textproc/fzf/files/patch-shell_completion.zsh
+++ textproc/fzf/files/patch-shell_completion.zsh
@@ -1,13 +1,11 @@
 --- shell/completion.zsh.orig	2017-12-03 14:55:24 UTC
 +++ shell/completion.zsh
-@@ -163,7 +163,9 @@ fzf-completion() {
+@@ -163,7 +163,7 @@ fzf-completion() {
    # Kill completion (do not require trigger sequence)
    if [ $cmd = kill -a ${LBUFFER[-1]} = ' ' ]; then
      fzf="$(__fzfcmd_complete)"
 -    matches=$(command ps -ef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" ${=fzf} -m | awk '{print $2}' | tr '\n' ' ')
-+    matches=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFA
-+ULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" ${=fzf} -m | awk '{print $2}' | tr '\n'
-+' ')
++    matches=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" ${=fzf} -m | awk '{print $2}' | tr '\n' ' ')
      if [ -n "$matches" ]; then
        LBUFFER="$LBUFFER$matches"
      fi
