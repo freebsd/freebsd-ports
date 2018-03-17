e9e8480fb924db5d5e15d697c38f114715c43bf7
bar <bar@FreeBSD.org>
2018-03-17 06:48:03 +0000

    Attempt to fix on HEAD.

--- games/vavoom/files/patch-utils_vcc_vcc.cpp
+++ games/vavoom/files/patch-utils_vcc_vcc.cpp
@@ -1,5 +1,5 @@
---- utils/vcc/vcc.cpp.orig	2017-06-04 09:44:06.817573000 +0200
-+++ utils/vcc/vcc.cpp	2017-06-04 10:05:59.278454000 +0200
+--- utils/vcc/vcc.cpp.orig	2018-03-17 07:31:45.795910000 +0100
++++ utils/vcc/vcc.cpp	2018-03-17 07:37:38.864263000 +0100
 @@ -122,7 +122,7 @@
  	}
  	catch (VException& e)
@@ -9,3 +9,12 @@
  	}
  	return 0;
  }
+@@ -171,7 +171,7 @@
+ {
+ 	//	Print usage.
+ 	printf("\n");
+-	printf("VCC Version 1.%d. Copyright (c)2000-2001 by JL.          ("__DATE__" "__TIME__")\n", PROG_VERSION);
++	printf("VCC Version 1.%d. Copyright (c)2000-2001 by JL.          (" __DATE__ " " __TIME__ ")\n", PROG_VERSION);
+ 	printf("Usage: vcc [options] source[.c] [object[.dat]]\n");
+ 	printf("    -d<file>     Output debugging information into specified file\n");
+ 	printf("    -a<function> Output function's ASM statements into debug file\n");
