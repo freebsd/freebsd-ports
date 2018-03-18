a820042f299e706dd18de29d68943c0d85a9b590
Barbara Guida <bar@FreeBSD.org>
2018-03-18 06:20:50 +0000

    Attempt to fix on HEAD.

--- games/vavoom/files/patch-source_host.cpp
+++ games/vavoom/files/patch-source_host.cpp
@@ -1,5 +1,14 @@
---- source/host.cpp.orig	2017-06-04 12:56:34.440473000 +0200
-+++ source/host.cpp	2017-06-04 13:10:16.524114000 +0200
+--- source/host.cpp.orig	2018-03-18 07:16:51.331022000 +0100
++++ source/host.cpp	2018-03-18 07:17:13.897333000 +0100
+@@ -523,7 +523,7 @@
+ COMMAND(Version)
+ {
+ 	GCon->Log("VAVOOM version " VERSION_TEXT" (r" SVN_REVISION_STRING ").");
+-	GCon->Log("Compiled "__DATE__" "__TIME__".");
++	GCon->Log("Compiled " __DATE__ " " __TIME__ ".");
+ }
+ 
+ //==========================================================================
 @@ -706,5 +706,5 @@
  
  	SAFE_SHUTDOWN(VObject::StaticExit, ())
