5112d3ae21cde1b659b1f61b15a8104e7711cac0
yuri <yuri@FreeBSD.org>
2018-03-18 23:35:36 +0000

    archivers/unalz: Update to 0.66
    Port changes:
    * Change MASTER_SITES and WWW to https
    * Remove USES=dos2unix
    
    PR:		226227
    Submitted by:	Chris Hutchinson <portmaster@bsdforge.com> (maintainer)

--- archivers/unalz/files/patch-UnAlz.cpp
+++ archivers/unalz/files/patch-UnAlz.cpp
@@ -1,4 +1,4 @@
---- UnAlz.cpp.orig	2016-07-26 12:39:47 UTC
+--- UnAlz.cpp.orig	2018-02-26 21:09:31 UTC
 +++ UnAlz.cpp
 @@ -37,7 +37,7 @@
  #	include <errno.h>
@@ -7,9 +7,18 @@
 -#if defined(__NetBSD__)
 +#if defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
  #	include <sys/param.h>	// __NetBSD_Version__
- #	include <errno.h>		// iconv.h 때문에 필요 
+ #	include <errno.h>		// iconv.h
  #endif
-@@ -455,7 +455,7 @@ BOOL CUnAlz::ReadLocalFileheader()
+@@ -432,7 +432,7 @@ BOOL CUnAlz::ReadLocalFileheader()
+ 	FRead(zipHeader.fileName, zipHeader.head.fileNameLength);
+ 	if(zipHeader.head.fileNameLength > MAX_PATH - 5)
+ 		zipHeader.head.fileNameLength = MAX_PATH - 5;
+-	zipHeader.fileName[zipHeader.head.fileNameLength] = (CHAR)NULL;
++	zipHeader.fileName[zipHeader.head.fileNameLength] = (CHAR)0;
+ 
+ 
+ #ifdef _UNALZ_ICONV		// codepage convert
+@@ -447,7 +447,7 @@ BOOL CUnAlz::ReadLocalFileheader()
  	size_t size;
  	char inbuf[ICONV_BUF_SIZE];
  	char outbuf[ICONV_BUF_SIZE];
@@ -18,7 +27,16 @@
  	const char *inptr = inbuf;
  #else
  	char *inptr = inbuf;
-@@ -1907,7 +1907,11 @@ void CUnAlz::DecryptingData(int nSize, B
+@@ -937,7 +937,7 @@ BOOL CUnAlz::DigPath(const char* szPathN
+ 	{
+ 		if(*last=='/' || *last=='\\') 
+ 		{
+-			*last = (char)NULL;
++			*last = (char)0;
+ 			break;
+ 		}
+ 		last --;
+@@ -1870,7 +1870,11 @@ void CUnAlz::DecryptingData(int nSize, B
  ////////////////////////////////////////////////////////////////////////////////////////////////////
  UINT32 CUnAlz::CRC32(UINT32 l, BYTE c)
  {
