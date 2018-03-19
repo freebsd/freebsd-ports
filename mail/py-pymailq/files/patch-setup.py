9015977953eb66e490f443952e2c7dc0acb95c31
loader <loader@FreeBSD.org>
2018-03-19 07:36:57 +0000

    mail/py-pymailq: Update to 0.9.0
    - Use setup.py to install the man page
    
    Reviewed by: koobs
    Approved by: koobs (ports)
    Differential Revision: https://reviews.freebsd.org/D14722

--- mail/py-pymailq/files/patch-setup.py
+++ mail/py-pymailq/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2017-08-31 10:22:19 UTC
+--- setup.py.orig	2018-03-06 17:14:54 UTC
 +++ setup.py
-@@ -39,11 +39,10 @@ if __name__ == '__main__':
+@@ -39,11 +39,11 @@ if __name__ == '__main__':
          scripts=['bin/pqshell'],
          packages=['pymailq'],
          package_dir={'pymailq': 'pymailq'},
@@ -9,9 +9,9 @@
 +        data_files=[('%%DOCSDIR%%', ['README.rst', 'LICENSE', 'CHANGES']),
 +                    ('%%EXAMPLESDIR%%', [
                          'share/doc/examples/pymailq.ini'
--                    ]),
+                     ]),
 -                    ('share/man/man1', ['man/pqshell.1'])],
-+                    ])],
++                    ('%%MANPREFIX%%/man/man1', ['man/pqshell.1'])],
          keywords=['postfix', 'shell', 'mailq', 'python', 'pqshell', 'postqueue'],
          classifiers=[
              'Development Status :: 5 - Production/Stable',
