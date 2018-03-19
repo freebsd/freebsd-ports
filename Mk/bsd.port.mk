645eb83be24ffd9c3fe1d459bf5915a3a9f4d651
bdrewery <bdrewery@FreeBSD.org>
2018-03-19 21:39:40 +0000

    Fix some recursive targets using the wrong FLAVOR.
    Reported by:	Andrzej Tobola <ato@iem.pw.edu.pl>, koobs
    Approved by:	portmgr (implicit)

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -4053,6 +4053,7 @@ _FLAVOR_RECURSIVE_SH= \
 		${FALSE}; \
 	fi; \
 	for dir in $${recursive_dirs}; do \
+		unset flavor; \
 		case $${dir} in \
 			*@*) \
 				flavor=$${dir\#*@}; \
@@ -4240,6 +4241,7 @@ PACKAGE-DEPENDS-LIST?= \
 	fi; \
 	checked="${PARENT_CHECKED}"; \
 	for dir in ${_LIB_RUN_DEPENDS:C,[^:]*:([^:]*):?.*,\1,}; do \
+		unset flavor; \
 		case $${dir} in \
 		*@*) \
 			flavor=$${dir\#*@}; \
