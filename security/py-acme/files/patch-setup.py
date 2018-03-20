f82285841d7afdf0d708870f8e5c96694dd7c0ad
Mathieu Arnold <mat@FreeBSD.org>
2018-03-20 12:03:59 +0000

    Update acme/certbot ports to 0.22.2.
    Sponsored by:	Absolight

--- security/py-acme/files/patch-setup.py
+++ security/py-acme/files/patch-setup.py
@@ -1,15 +1,15 @@
---- setup.py.orig	2017-12-07 17:15:47 UTC
+--- setup.py.orig	2018-03-20 00:33:22 UTC
 +++ setup.py
-@@ -12,7 +12,6 @@ install_requires = [
-     # rsa_recover_prime_factors (>=0.8)
-     'cryptography>=0.8',
+@@ -14,7 +14,6 @@ install_requires = [
+     # formerly known as acme.jose:
+     'josepy>=1.0.0',
      # Connection.set_tlsext_host_name (>=0.13)
 -    'mock',
      'PyOpenSSL>=0.13',
      'pyrfc3339',
      'pytz',
-@@ -31,6 +30,7 @@ if sys.version_info < (2, 7):
-     ])
+@@ -24,6 +23,7 @@ install_requires = [
+ ]
  
  dev_extras = [
 +    'mock',
