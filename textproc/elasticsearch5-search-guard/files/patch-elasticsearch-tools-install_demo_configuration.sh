25e0502addf72e4da8492f76efc0c5fc1cd87e24
feld <feld@FreeBSD.org>
2018-03-20 16:52:01 +0000

    textproc/elasticsearch5-search-guard: Update to 5.6.8
    - Automate pkg-plist as well

--- textproc/elasticsearch5-search-guard/files/patch-elasticsearch-tools-install_demo_configuration.sh
+++ textproc/elasticsearch5-search-guard/files/patch-elasticsearch-tools-install_demo_configuration.sh
@@ -1,9 +1,9 @@
---- elasticsearch/tools/install_demo_configuration.sh.orig	2017-09-15 11:37:44 UTC
+--- elasticsearch/tools/install_demo_configuration.sh.orig	2018-03-20 16:29:13 UTC
 +++ elasticsearch/tools/install_demo_configuration.sh
-@@ -17,25 +17,14 @@ fi
- 
- set -e
- BASE_DIR="$DIR/../../../"
+@@ -26,34 +26,14 @@ if [ -d "$BASE_DIR" ]; then
+ else
+     echo "DEBUG: basedir does not exist"
+ fi
 -ES_CONF_FILE="$BASE_DIR/config/elasticsearch.yml"
 +ES_CONF_FILE="%%PREFIX%%/etc/elasticsearch/elasticsearch.yml"
  ES_BIN_DIR="$BASE_DIR/bin"
@@ -16,39 +16,38 @@
  
 -#Check if its a rpm/deb install
 -if [ -f /usr/share/elasticsearch/bin/elasticsearch ]; then
--    ES_CONF_FILE="/etc/elasticsearch/elasticsearch.yml"
+-    ES_CONF_FILE="/usr/share/elasticsearch/config/elasticsearch.yml"
+-
+-    if [ ! -f "$ES_CONF_FILE" ]; then
+-        ES_CONF_FILE="/etc/elasticsearch/elasticsearch.yml"
+-    fi
+-
 -    ES_BIN_DIR="/usr/share/elasticsearch/bin"
 -    ES_PLUGINS_DIR="/usr/share/elasticsearch/plugins"
 -    ES_LIB_PATH="/usr/share/elasticsearch/lib"
--    SUDO_CMD="sudo"
+-
+-    if [ -x "$(command -v sudo)" ]; then
+-        SUDO_CMD="sudo"
+-        echo "This script maybe require your root password for 'sudo' privileges"
+-    fi
+-
 -    ES_INSTALL_TYPE="rpm/deb"
--    echo "This script maybe require your root password for 'sudo' privileges"
 -fi
 -
- if $SUDO_CMD test -f "$ES_CONF_FILE"; then
-     :
- else
-@@ -58,12 +47,8 @@ if [ ! -d $ES_LIB_PATH ]; then
- 	exit -1
- fi
- 
--if [ "$ES_INSTALL_TYPE" != "rpm/deb" ];then
--    ES_CONF_DIR=$(dirname "${ES_CONF_FILE}")
--    ES_CONF_DIR=`cd "$ES_CONF_DIR" ; pwd`
--else
--    ES_CONF_DIR="/etc/elasticsearch"    
--fi
-+ES_CONF_DIR=$(dirname "${ES_CONF_FILE}")
-+ES_CONF_DIR=`cd "$ES_CONF_DIR" ; pwd`
- 
- ES_CONF_FILE="$ES_CONF_DIR/elasticsearch.yml"
- 
-@@ -123,7 +108,7 @@ ES_PLUGINS_DIR=`cd "$ES_PLUGINS_DIR" ; p
+ if [ $SUDO_CMD ]; then
+     if ! [ -x "$(command -v $SUDO_CMD)" ]; then
+         echo "Unable to locate 'sudo' command. Quit."
+@@ -168,10 +148,10 @@ ES_PLUGINS_DIR=`cd "$ES_PLUGINS_DIR" ; p
  echo "### Success"
  echo "### Execute this script now on all your nodes and then start all nodes"
  echo "### After the whole cluster is up execute: "
 -echo "#!/bin/bash" | $SUDO_CMD tee sgadmin_demo.sh > /dev/null 
-+echo "#!/usr/local/bin/bash" | $SUDO_CMD tee sgadmin_demo.sh > /dev/null 
- echo $SUDO_CMD "$ES_PLUGINS_DIR/search-guard-5/tools/sgadmin.sh" -cd "$ES_PLUGINS_DIR/search-guard-5/sgconfig" -cn searchguard_demo -ks "$ES_CONF_DIR/kirk.jks" -ts "$ES_CONF_DIR/truststore.jks" -nhnv | $SUDO_CMD tee -a sgadmin_demo.sh > /dev/null
++echo "%%BASH%%" | $SUDO_CMD tee sgadmin_demo.sh > /dev/null 
+ echo $SUDO_CMD "$ES_PLUGINS_DIR/search-guard-5/tools/sgadmin.sh" -cd "$ES_PLUGINS_DIR/search-guard-5/sgconfig" -icl -ks "$ES_CONF_DIR/kirk.jks" -ts "$ES_CONF_DIR/truststore.jks" -nhnv | $SUDO_CMD tee -a sgadmin_demo.sh > /dev/null
  $SUDO_CMD chmod +x sgadmin_demo.sh
  $SUDO_CMD cat sgadmin_demo.sh | tail -1
+ echo "### or run ./sgadmin_demo.sh"
+ echo "### Then open https://localhost:9200 an login with admin/admin"
+-echo "### (Just ignore the ssl certificate warning because we installed a self signed demo certificate)"
+\ No newline at end of file
++echo "### (Just ignore the ssl certificate warning because we installed a self signed demo certificate)"
