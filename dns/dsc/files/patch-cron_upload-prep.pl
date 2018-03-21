cc3c2817feada6d538a49253544539e21d93baa9
Kurt Jaeger <pi@FreeBSD.org>
2018-03-21 20:14:11 +0000

    New port: dns/dsc
    DSC (originally developed by The Measurement Factory and now developed
    by DNS-OARC) is a system for collecting and exploring statistics from
    busy DNS servers. It uses a distributed architecture with collectors
    running on or near nameservers sending their data to one or more
    central presenters for display and archiving. Collectors use pcap to
    sniff network traffic. They transmit aggregated data to the presenter
    as XML data. dsc is configurable to allow the administrator to capture
    any kind of data that he or she chooses.
    
    WWW: https://www.dns-oarc.net/dsc/
    
    PR:		226631
    Submitted by:	Leo Vandewoestijne <freebsd@dns.company>

---
+++ dns/dsc/files/patch-cron_upload-prep.pl
@@ -0,0 +1,8 @@
+--- cron/upload-prep.pl.orig	2018-03-13 22:05:23 UTC
++++ cron/upload-prep.pl
+@@ -1,4 +1,4 @@
+-#!/usr/bin/perl
++#!/usr/local/bin/perl
+ #
+ # Copyright (c) 2016-2017, OARC, Inc.
+ # Copyright (c) 2007, The Measurement Factory, Inc.
