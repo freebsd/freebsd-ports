8e3098e06cbb15ee4857948bcff6ae92ef2fce18
Steve Wills <swills@FreeBSD.org>
2018-03-22 17:15:23 +0000

    www/py-graphite-web: update to 1.1.2

--- www/py-graphite-web/files/patch-bin__build-index.sh
+++ www/py-graphite-web/files/patch-bin__build-index.sh
@@ -1,48 +1,9 @@
---- bin/build-index.sh.orig	2014-02-14 15:05:38.180621787 +0000
-+++ bin/build-index.sh	2014-02-14 15:31:54.580513137 +0000
-@@ -1,17 +1,19 @@
--#!/bin/bash
-+#!/bin/sh
+--- bin/build-index.sh.orig	2018-02-13 06:01:34.000000000 -0500
++++ bin/build-index.sh	2018-03-22 08:58:11.785051000 -0400
+@@ -1,5 +1,5 @@
+ #!/bin/bash
+-export PYTHONPATH="/opt/graphite/webapp/:$PYTHONPATH"
++export PYTHONPATH="%%PREFIX%%/graphite/webapp/:$PYTHONPATH"
+ BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
  
- if [ "$GRAPHITE_ROOT" = "" ]
- then
--  GRAPHITE_ROOT="/opt/graphite"
-+  GRAPHITE_ROOT="%%PREFIX%%/graphite"
- fi
- 
- if [ "$GRAPHITE_STORAGE_DIR" = "" ]
- then
--  GRAPHITE_STORAGE_DIR="${GRAPHITE_ROOT}/storage"
-+  GRAPHITE_STORAGE_DIR="/var/db/graphite"
- fi
- 
--
--WHISPER_DIR="${GRAPHITE_STORAGE_DIR}/whisper"
-+if [ "$WHISPER_DIR" = "" ]
-+then
-+  WHISPER_DIR="/var/db/carbon/whisper/"
-+fi
- 
- if [ ! -d "$WHISPER_DIR" ]
- then
-@@ -19,13 +21,17 @@
-   exit 1
- fi
- 
--INDEX_FILE="${GRAPHITE_STORAGE_DIR}/index"
--TMP_INDEX="${GRAPHITE_STORAGE_DIR}/.index.tmp"
-+if [ "$INDEX_FILE" = "" ]
-+then
-+  INDEX_FILE="${GRAPHITE_STORAGE_DIR}/index"
-+fi
-+
-+TMP_INDEX=${INDEX_FILE%%index}.index.tmp
- 
- rm -f $TMP_INDEX
- cd $WHISPER_DIR
- touch $INDEX_FILE
- echo "[`date`]  building index..."
--find -L . -name '*.wsp' | perl -pe 's!^[^/]+/(.+)\.wsp$!$1!; s!/!.!g' > $TMP_INDEX
-+find -L . -name '*.wsp' | sed -E 's!^[^/]+/(.+)\.wsp$!\1!; s!/!.!g' > $TMP_INDEX
- echo "[`date`]  complete, switching to new index file"
- mv -f $TMP_INDEX $INDEX_FILE
+ ${BINDIR}/build-index
