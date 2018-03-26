3147bb6c6b50283c03880d7e81033e282aac97f3
Baptiste Daroussin <bapt@FreeBSD.org>
2018-03-26 12:38:56 +0000

    Make libgnome and libgnomeui not implying esound dependency
    It has been a while both does not depend anymore on esound, before this change
    esound dependency was wrongly added to plenty of packages which actually does
    not depend on it.
    
    While here:
    
    - audio/glame: remove esound support
    - games/monkeybubble: add an explicit dependency on esound
    - sysutils/gnome-schedule: add en explicit dependency on pkg-config
    - comms/kb: add missing dependency on pkgconfig
    - x11-toolkits/libgnomeui:
      * update to 2.4.5
      * use USES=localbase

--- Mk/Uses/gnome.mk
+++ Mk/Uses/gnome.mk
@@ -265,7 +265,7 @@ libgnomeprintui_LIB_DEPENDS=	libgnomeprintui-2-2.so:x11-toolkits/libgnomeprintui
 libgnomeprintui_USE_GNOME_IMPL=	libgnomeprint libgnomecanvas
 
 libgnome_LIB_DEPENDS=	libgnome-2.so:x11/libgnome
-libgnome_USE_GNOME_IMPL=gnomevfs2 esound libbonobo
+libgnome_USE_GNOME_IMPL=gnomevfs2 libbonobo
 
 libbonoboui_LIB_DEPENDS=	libbonoboui-2.so:x11-toolkits/libbonoboui
 libbonoboui_USE_GNOME_IMPL=	libgnomecanvas libgnome
