691d254ed6df477fa904fad47231268fa4d0327e
brnrd <brnrd@FreeBSD.org>
2018-03-26 17:13:09 +0000

    irc/irssi: Fix build with LibreSSL 2.7
     - LibreSSL 2.7 added OpenSSL 1.1 API
    
    PR:		226925
    Submitted by:	Sergey Akhmatov <sergey akhmatov ru>
    Approved by:	David O'Rourke <dor.bsd xm0 uk> (maintainer)

--- irc/irssi/files/patch-src_core_network-openssl.c
+++ irc/irssi/files/patch-src_core_network-openssl.c
@@ -1,6 +1,16 @@
---- src/core/network-openssl.c.orig	2017-03-10 16:31:03 UTC
+--- src/core/network-openssl.c.orig	2018-02-14 23:53:44 UTC
 +++ src/core/network-openssl.c
-@@ -434,6 +434,7 @@ static GIOChannel *irssi_ssl_get_iochann
+@@ -47,7 +47,8 @@
+ 
+ /* OpenSSL 1.1.0 also introduced some useful additions to the api */
+ #if (OPENSSL_VERSION_NUMBER >= 0x10002000L)
+-#if (OPENSSL_VERSION_NUMBER < 0x10100000L) || defined (LIBRESSL_VERSION_NUMBER)
++#if (OPENSSL_VERSION_NUMBER < 0x10100000L) || \ 
++    (defined (LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x2070000fL)
+ static int X509_STORE_up_ref(X509_STORE *vfy)
+ {
+     int n;
+@@ -476,6 +477,7 @@ static GIOChannel *irssi_ssl_get_iochann
  		if (SSL_CTX_set_cipher_list(ctx, ciphers) != 1)
  			g_warning("No valid SSL cipher suite could be selected");
  	}
