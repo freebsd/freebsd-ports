f465239a9793dc48b42227fbd8b514612a9e4760
Lev A. Serebryakov <lev@FreeBSD.org>
2018-03-26 14:33:11 +0000

    Fix patches.

--- devel/msp430-debug-stack/files/patch-DLL430_v3_src_DLL430_OldApiV3.cpp
+++ devel/msp430-debug-stack/files/patch-DLL430_v3_src_DLL430_OldApiV3.cpp
@@ -1,6 +1,6 @@
---- DLL430_v3/src/DLL430_OldApiV3.cpp.orig	2017-12-05 18:38:46.037302000 +0300
-+++ DLL430_v3/src/DLL430_OldApiV3.cpp	2017-12-05 18:40:21.357777000 +0300
-@@ -191,7 +191,7 @@ void DLL430_OldApiV3::event(DebugEventTa
+--- DLL430_v3/src/DLL430_OldApiV3.cpp.orig	2018-03-26 17:21:18.500487000 +0300
++++ DLL430_v3/src/DLL430_OldApiV3.cpp	2018-03-26 17:22:50.850255000 +0300
+@@ -191,7 +191,7 @@
  			//With trace/variable watch enabled, give storage events on same trigger time to be reported
  			if (trace_storage.trControl == TR_ENABLE)
  			{
@@ -9,7 +9,25 @@
  			}
  			db_man->pausePolling();
  		}
-@@ -1524,7 +1524,7 @@ bool DLL430_OldApiV3::Erase(int32_t type
+@@ -330,7 +330,7 @@
+ 	catch (const std::runtime_error& e)
+ 	{
+ #ifndef NDEBUG
+-		ofstream("xml_error.log") << e.what() << endl;
++		std::ofstream("xml_error.log") << e.what() << endl;
+ #endif
+ 		log(LogTarget::ERR, DEVICE_DB_ERR, e.what());
+ 		return false;
+@@ -356,7 +356,7 @@
+ 	catch (const std::runtime_error& e)
+ 	{
+ #ifndef NDEBUG
+-		ofstream("xml_error.log") << e.what() << endl;
++		std::ofstream("xml_error.log") << e.what() << endl;
+ #endif
+ 		log(LogTarget::ERR, DEVICE_DB_ERR, e.what());
+ 		return false;
+@@ -1569,7 +1569,7 @@
  				log(LogTarget::ERR, ERASE_ERR, "");
  				return false;
  			}
@@ -18,7 +36,7 @@
  			return true;
  			
  		}
-@@ -1538,7 +1538,7 @@ bool DLL430_OldApiV3::Erase(int32_t type
+@@ -1583,7 +1583,7 @@
  			}
  			if (singleDevice)
  			{
@@ -27,7 +45,7 @@
  				return true;
  			}
  		}
-@@ -1923,7 +1923,7 @@ bool DLL430_OldApiV3::Reset(int32_t meth
+@@ -1987,7 +1987,7 @@
  		if (devArchitecture != MSP432_M4)
  		{
  			db_man->resumePolling();
@@ -36,16 +54,25 @@
  			db_man->pausePolling();
  		}
  		if (!State(&state, true, &pCPUCycles))
-@@ -3781,7 +3781,7 @@ bool DLL430_OldApiV3::FET_FwUpdate(
+@@ -3848,7 +3848,7 @@
+ 				int timeout = 50;
+ 				while (timeout-- && !this->Initialize(&port[0], &version))
+ 				{
+-					this_thread::sleep_for(chrono::seconds(1));
++					this_thread::sleep_for(std::chrono::seconds(1));
+ 				}
+ 				if (!handle)
+ 				{
+@@ -3873,7 +3873,7 @@
+ #endif
+ 						UpdateLog.append("\n---------------------Firmware upate end--------------------------\n");
  
- 			if (!handle)
- 			{
--				this_thread::sleep_for(chrono::seconds(3));
-+				this_thread::sleep_for(std::chrono::seconds(3));
- 				this->Initialize(&port[0], &version);
+-						ofstream(logfile.c_str(), ios::app | ios::out) << UpdateLog;
++						std::ofstream(logfile.c_str(), ios::app | ios::out) << UpdateLog;
+ 					}
+ 				}
  			}
- 
-@@ -4179,7 +4179,7 @@ bool DLL430_OldApiV3::writeToExternalMem
+@@ -4272,7 +4272,7 @@
  
  	Run(FREE_RUN, true);					// start execution of loader code
  
--- devel/msp430-debug-stack/files/patch-DLL430_v3_src_TI_DLL430_UpdateManagerMSP_FET430.cpp
+++ devel/msp430-debug-stack/files/patch-DLL430_v3_src_TI_DLL430_UpdateManagerMSP_FET430.cpp
@@ -1,29 +1,29 @@
---- DLL430_v3/src/TI/DLL430/UpdateManagerMSP_FET430.cpp.orig	2017-12-05 18:38:46.041601000 +0300
-+++ DLL430_v3/src/TI/DLL430/UpdateManagerMSP_FET430.cpp	2017-12-05 18:41:45.548342000 +0300
-@@ -212,7 +212,7 @@ bool UpdateManagerMSP_FET430::firmWareUp
- 		control->sendData(data_55);
- 		control->clearResponse();
+--- DLL430_v3/src/TI/DLL430/UpdateManagerMSP_FET430.cpp.orig	2018-03-26 17:14:12.210044000 +0300
++++ DLL430_v3/src/TI/DLL430/UpdateManagerMSP_FET430.cpp	2018-03-26 17:15:34.167641000 +0300
+@@ -226,7 +226,7 @@
  
--		this_thread::sleep_for(chrono::seconds(8));
-+		this_thread::sleep_for(std::chrono::seconds(8));
- 	}
- 
- 	MemoryContent firmware;
-@@ -282,7 +282,7 @@ bool UpdateManagerMSP_FET430::firmWareUp
+ 		for (int i = 0; i < 8; ++i)
+ 		{
+-			this_thread::sleep_for(chrono::seconds(1));
++			this_thread::sleep_for(std::chrono::seconds(1));
+ 			if (callback)
+ 			{
+ 				--requiredUpdates;
+@@ -313,7 +313,7 @@
  	const bool initFailed = !this->fetHandle->send(initCmd);
  
  	// give the firmware time to execute initialisation
 -	this_thread::sleep_for(chrono::seconds(1));
 +	this_thread::sleep_for(std::chrono::seconds(1));
- 
- 	//Only an error if no user specified file is used (ie. init will fail for a downgrade)
- 	if (initFailed && !fname)
-@@ -300,7 +300,7 @@ bool UpdateManagerMSP_FET430::firmWareUp
- 		cmd.elements.emplace_back(el);
- 
+ 	if (callback)
+ 	{
+ 		--requiredUpdates;
+@@ -338,7 +338,7 @@
  		fetHandle->send(cmd);
--		this_thread::sleep_for(chrono::seconds(2));
-+		this_thread::sleep_for(std::chrono::seconds(2));
- 
- 		if (callback)
- 			callback(BL_UPDATE_DONE, 0, 0);
+ 		for (int i = 0; i < 2; ++i)
+ 		{
+-			this_thread::sleep_for(chrono::seconds(1));
++			this_thread::sleep_for(std::chrono::seconds(1));
+ 			if (callback)
+ 			{
+ 				--requiredUpdates;
