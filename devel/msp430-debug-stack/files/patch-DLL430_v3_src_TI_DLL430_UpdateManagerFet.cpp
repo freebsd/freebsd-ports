c65671794ea1894b51b150079403a26cf9cd86b7
lev <lev@FreeBSD.org>
2018-03-26 14:33:11 +0000

    Fix patches.

--- devel/msp430-debug-stack/files/patch-DLL430_v3_src_TI_DLL430_UpdateManagerFet.cpp
+++ devel/msp430-debug-stack/files/patch-DLL430_v3_src_TI_DLL430_UpdateManagerFet.cpp
@@ -1,7 +1,15 @@
---- DLL430_v3/src/TI/DLL430/UpdateManagerFet.cpp.orig	2017-12-05 18:47:14.471473000 +0300
-+++ DLL430_v3/src/TI/DLL430/UpdateManagerFet.cpp	2017-12-05 18:47:14.610002000 +0300
-@@ -128,7 +128,7 @@ bool UpdateManagerFet::updateCore(Memory
- 			intCallback(BL_DATA_BLOCK_PROGRAMMED, (uint32_t)( 100-(requiredCoreUpdates)*requiredCoreUpdates), 0);
+--- DLL430_v3/src/TI/DLL430/UpdateManagerFet.cpp.orig	2018-03-26 17:23:13.431436000 +0300
++++ DLL430_v3/src/TI/DLL430/UpdateManagerFet.cpp	2018-03-26 17:25:33.815661000 +0300
+@@ -68,6 +68,7 @@
+ 
+ #include "../../../../Bios/include/MSP_FetDcdc.h"
+ #include "../../../../Bios/include/MSP_FetHal.h"
++#include "../../../../Bios/include/MSP_FetFpgaHal.h"
+ #include "../../../../Bios/include/MSP_FetHil.h"
+ #include "../../../../Bios/include/MSP_FetCore.h"
+ #include "../../../../Bios/include/MSP_FetComChannel.h"
+@@ -131,7 +132,7 @@
+ 			intCallback(BL_DATA_BLOCK_PROGRAMMED, (uint32_t)(100 - (requiredCoreUpdates)*percentCore), 0);
  		}
  
 -		this_thread::sleep_for(chrono::seconds(4));
@@ -9,7 +17,7 @@
  
  		fetHandle->shutdown();
  
-@@ -141,7 +141,7 @@ bool UpdateManagerFet::updateCore(Memory
+@@ -144,7 +145,7 @@
  		uint32_t countedHidDevices = 0, timeout = 50;
  		do
  		{
@@ -18,7 +26,7 @@
  			countedHidDevices = HidUpdateManager::countHidDevices(currentPid);
  
  			if (intCallback)
-@@ -635,7 +635,7 @@ bool UpdateManagerFet::updateFpga()
+@@ -648,7 +649,7 @@
  	// Start the FPGA update
  	this->upInit(3);
  
@@ -27,16 +35,25 @@
  
  	// Restore the normal HAL module
  	if (returnValue && !updateHal())
-@@ -956,7 +956,7 @@ bool UpdateManagerFet::firmWareUpdate(co
+@@ -839,7 +840,7 @@
+ #endif
+ 			UpdateLog.append("\n---------------------Firmware upate end--------------------------\n");
  
+-			ofstream(logfile.c_str(), ios::app | ios::out) << UpdateLog;
++			std::ofstream(logfile.c_str(), ios::app | ios::out) << UpdateLog;
+ 		}
+ 
+ 		return retval;
+@@ -1020,7 +1021,7 @@
+ #endif
  		UpdateLog.append("\n---------------------Firmware upate end--------------------------\n");
  
 -		ofstream(logfile.c_str(), ios::app | ios::out) << UpdateLog;
 +		std::ofstream(logfile.c_str(), ios::app | ios::out) << UpdateLog;
- #endif
  	}
  	return returnValue;
-@@ -1063,7 +1063,7 @@ bool UpdateManagerFet::updateFirmware(co
+ }
+@@ -1134,7 +1135,7 @@
  	this->upInit(0);
  
  	// give the firmware time to execute initialisation
