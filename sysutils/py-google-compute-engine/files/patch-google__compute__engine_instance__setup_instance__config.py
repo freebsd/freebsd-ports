3fbcc5b4dfcb609d022b7cf038ab278d9791d4d4
yuri <yuri@FreeBSD.org>
2018-03-27 18:55:37 +0000

    sysutils/py-google-compute-engine: Update 2.6.1 -> 2.7.6
    Patch updates.
    
    PR:		227006
    Submitted by:	Helen Koike <helen.koike@collabora.com> (maintainer)

--- sysutils/py-google-compute-engine/files/patch-google__compute__engine_instance__setup_instance__config.py
+++ sysutils/py-google-compute-engine/files/patch-google__compute__engine_instance__setup_instance__config.py
@@ -1,9 +1,9 @@
---- google_compute_engine/instance_setup/instance_config.py.orig	2017-09-29 00:21:28 UTC
+--- google_compute_engine/instance_setup/instance_config.py.orig	2018-02-22 19:44:17 UTC
 +++ google_compute_engine/instance_setup/instance_config.py
-@@ -72,11 +72,11 @@ class InstanceConfig(config_manager.Conf
-           'instance_id': '0',
+@@ -73,11 +73,11 @@ class InstanceConfig(config_manager.Conf
        },
        'InstanceSetup': {
+           'host_key_types': 'ecdsa,ed25519,rsa',
 -          'optimize_local_ssd': 'true',
 +          'optimize_local_ssd': 'false',
            'network_enabled': 'true',
--- sysutils/py-google-compute-engine/files/patch-setup.py
+++ sysutils/py-google-compute-engine/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2017-09-14 18:08:49 UTC
+--- setup.py.orig	2018-02-22 19:44:17 UTC
 +++ setup.py
-@@ -30,7 +30,6 @@ setuptools.setup(
+@@ -34,7 +34,6 @@ setuptools.setup(
      long_description='Google Compute Engine guest environment.',
      name='google-compute-engine',
      packages=setuptools.find_packages(),
 -    scripts=glob.glob('scripts/*'),
      url='https://github.com/GoogleCloudPlatform/compute-image-packages',
-     version='2.6.1',
+     version='2.7.6',
      # Entry points create scripts in /usr/bin that call a function.
