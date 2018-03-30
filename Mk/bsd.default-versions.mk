17390d808c90f76f6b0a386d1defba1de174eaa5
Mathieu Arnold <mat@FreeBSD.org>
2018-03-30 12:15:43 +0000

    Switch the default Perl to 5.26.
    PR:		226817
    Exp-run by:	antoine
    Sponsored by:	Absolight

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -60,7 +60,7 @@ MYSQL_DEFAULT?=		5.6
 # Possible values: 5.22, 5.24, 5.26, devel
 .if !exists(${LOCALBASE}/bin/perl) || (!defined(_PORTS_ENV_CHECK) && \
     defined(PACKAGE_BUILDING))
-PERL5_DEFAULT?=		5.24
+PERL5_DEFAULT?=		5.26
 .elif !defined(PERL5_DEFAULT)
 # There's no need to replace development versions, like "5.23" with "devel"
 # because 1) nobody is supposed to use it outside of poudriere, and 2) it must
