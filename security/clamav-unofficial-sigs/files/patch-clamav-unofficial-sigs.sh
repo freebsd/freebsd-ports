b18b537831cc3944a4dfaa412555d71e86e87d09
Kurt Jaeger <pi@FreeBSD.org>
2018-03-30 11:08:35 +0000

    security/clamav-unofficial-sigs: update 5.3.2 -> 5.6.2
    PR:		227057, 222032
    Submitted by:	Max Kostikov <max@kostikov.co>, Marko Njezic <sf@maxempire.com> (maintainer)
    Relnotes:	https://github.com/extremeshok/clamav-unofficial-sigs

--- security/clamav-unofficial-sigs/files/patch-clamav-unofficial-sigs.sh
+++ security/clamav-unofficial-sigs/files/patch-clamav-unofficial-sigs.sh
@@ -1,43 +1,43 @@
---- clamav-unofficial-sigs.sh.orig	2016-11-18 10:05:40 UTC
+--- clamav-unofficial-sigs.sh.orig	2018-03-27 15:43:43 UTC
 +++ clamav-unofficial-sigs.sh
-@@ -753,6 +753,8 @@ function make_signature_database_from_as
- #Remove the clamav-unofficial-sigs script
- function remove_script () {
-   echo ""
-+  pkg_mgr="FreeBSD ports"
-+  pkg_rm="pkg delete"
-   if [ -n "$pkg_mgr" ] && [ -n "$pkg_rm" ] ; then
-     echo "This script (clamav-unofficial-sigs) was installed on the system via '$pkg_mgr'"
-     echo "use '$pkg_rm' to remove the script and all of its associated files and databases from the system."
-@@ -1151,7 +1153,7 @@ minimum_required_config_version="65"
+@@ -1290,7 +1290,7 @@ minimum_required_config_version="72"
  minimum_yara_clamav_version="0.99"
  
- #default config files
+ # Default config files
 -config_dir="/etc/clamav-unofficial-sigs"
 +config_dir="%%PREFIX%%/etc/clamav-unofficial-sigs"
- config_files=("$config_dir/master.conf" "$config_dir/os.conf" "$config_dir/user.conf")
+ config_files=( "$config_dir/master.conf" "$config_dir/os.conf" "$config_dir/user.conf" )
  
- #Initialise 
-@@ -1879,20 +1881,11 @@ if [ "$sanesecurity_enabled" == "yes" ] 
+ # Initialise
+@@ -1302,6 +1302,8 @@ force_updates="no"
+ enable_log="no"
+ custom_config="no"
+ we_have_a_config="0"
++pkg_mgr="FreeBSD ports"
++pkg_rm="pkg delete"
+ 
+ # Solaris which function returns garbage when the program is not found
+ # only define the new which function if running under Solaris
+@@ -2131,20 +2133,11 @@ if [ "$sanesecurity_enabled" == "yes" ] 
          xshok_pretty_echo_and_log "Sanesecurity Database & GPG Signature File Updates" "="
          xshok_pretty_echo_and_log "Checking for Sanesecurity updates..."
  
--        sanesecurity_mirror_ips=$(dig +ignore +short "$sanesecurity_url")
--        #add fallback to host if dig returns no records
--        if [ "$(xshok_array_count  "$sanesecurity_mirror_ips")" -lt 1 ] ; then
--          sanesecurity_mirror_ips=$(host -t A "$sanesecurity_url" | sed -n '/has address/{s/.*address \([^ ]*\).*/\1/;p;}')
+-        sanesecurity_mirror_ips="$(dig +ignore +short "$sanesecurity_url")"
+-        # Add fallback to host if dig returns no records
+-        if [ ${#sanesecurity_mirror_ips} -lt 1 ] ; then
+-          sanesecurity_mirror_ips="$(host -t A "$sanesecurity_url" | sed -n '/has address/{s/.*address \([^ ]*\).*/\1/;p;}')"
 -        fi
 +        sanesecurity_mirror_ips=$(host "$sanesecurity_url" | sed 's/.*[[:space:]]//')
  
-         if [ "$(xshok_array_count  "$sanesecurity_mirror_ips")" -ge "1" ] ; then
+         if [ ${#sanesecurity_mirror_ips} -ge 1 ] ; then
            for sanesecurity_mirror_ip in $sanesecurity_mirror_ips ; do
 -            sanesecurity_mirror_name=""
--            sanesecurity_mirror_name=$(dig +short -x "$sanesecurity_mirror_ip" | command sed 's/\.$//')
--            #add fallback to host if dig returns no records
--            if [ "$sanesecurity_mirror_name" == "" ] ; then
--              sanesecurity_mirror_name=$(host "$sanesecurity_mirror_ip" | sed -n '/name pointer/{s/.*pointer \([^ ]*\).*\.$/\1/;p;}')
+-            sanesecurity_mirror_name="$(dig +short -x "$sanesecurity_mirror_ip" | command sed 's/\.$//')"
+-            # Add fallback to host if dig returns no records
+-            if [ -z "$sanesecurity_mirror_name" ] ; then
+-              sanesecurity_mirror_name="$(host "$sanesecurity_mirror_ip" | sed -n '/name pointer/{s/.*pointer \([^ ]*\).*\.$/\1/;p;}')"
 -            fi
 +            sanesecurity_mirror_name=$(host "$sanesecurity_mirror_ip" | sed 's/.*[[:space:]]//' | sed 's/\.$//')
              sanesecurity_mirror_site_info="$sanesecurity_mirror_name $sanesecurity_mirror_ip"
              xshok_pretty_echo_and_log "Sanesecurity mirror site used: $sanesecurity_mirror_site_info"
-             $rsync_bin $rsync_output_level $no_motd --files-from="$sanesecurity_include_dbs" -ctuz $connect_timeout --timeout="$rsync_max_time" "rsync://$sanesecurity_mirror_ip/sanesecurity" "$work_dir_sanesecurity" 2>/dev/null
+             # shellcheck disable=SC2086
