fc56cb26ab65149e088437fc2664d6956bbb45f4
Joseph Mingrone <jrm@FreeBSD.org>
2018-04-02 21:19:51 +0000

    devel/magit: Update to version 2.12.1
    Upstream changes: https://raw.githubusercontent.com/magit/magit/master/Documentation/RelNotes/2.12.1.txt
    
    PR:		227141
    Submitted by:	yasu@utahime.org (maintainer)

--- devel/magit/files/patch-default.mk
+++ devel/magit/files/patch-default.mk
@@ -1,17 +1,11 @@
---- default.mk.orig	2017-09-13 10:42:03 UTC
+--- default.mk.orig	2018-03-29 20:07:19 UTC
 +++ default.mk
-@@ -87,14 +87,6 @@ ELGS = magit-autoloads.el magit-version.
+@@ -89,8 +89,6 @@ ELGS = magit-autoloads.el magit-version.
  
  ## Versions ##########################################################
  
--VERSION := $(shell \
--  test -e $(TOP).git\
--  && git describe --tags --dirty 2> /dev/null\
--  || $(BATCH) --eval "(progn\
--  (fset 'message (lambda (&rest _)))\
--  (load-file \"magit-version.el\")\
--  (princ magit-version))")
+-VERSION ?= $(shell test -e $(TOP).git && git describe --tags --abbrev=0)
 -
- MAGIT_VERSION       = 2.11
  ASYNC_VERSION       = 1.9.2
  DASH_VERSION        = 2.13.0
+ GHUB_VERSION        = 2.0.0
