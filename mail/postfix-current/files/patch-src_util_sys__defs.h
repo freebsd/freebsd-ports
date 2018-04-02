3482ab88d299c33ad5ff601b6cf23ca8be080e7e
Olli Hauer <ohauer@FreeBSD.org>
2018-04-02 07:37:29 +0000

    - update to 3.4-20180401
    Changelog:
    20180224
      Workaround: postconf build did not abort if the m4 command
      is not installed (on a system that does have the make command,
      the awk command, the perl command, and the C compiler?!).
      File: postconf/extract_cfg.sh.
    
    20180303
      Portability: slight differences between MySQL and MariaDB.
      Olli Hauer. File: global/dict_mysql.c.
    
    20180306
      Bugfix (introduced: 19990302): when luser_relay specifies
      a non-existent local address, the luser_relay feature becomes
      a black hole. Reported by Juergen Thomsen. File: local/unknown.c.
    
      Portability: FreeBSD 11 is supported. Files: makedefs,
      util/sys_defs.h.

--- mail/postfix-current/files/patch-src_util_sys__defs.h
+++ mail/postfix-current/files/patch-src_util_sys__defs.h
@@ -1,9 +1,10 @@
---- src/util/sys_defs.h.orig	2017-06-18 19:30:20 UTC
+--- src/util/sys_defs.h.orig	2018-03-04 19:01:27 UTC
 +++ src/util/sys_defs.h
-@@ -30,6 +30,7 @@
+@@ -30,7 +30,7 @@
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
      || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
+-    || defined(FREEBSD11) \
 +    || defined(FREEBSD11) || defined(FREEBSD12) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
