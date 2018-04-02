35f4091109a062cc745f3a037f52626759ae3b42
Bernard Spil <brnrd@FreeBSD.org>
2018-04-02 14:18:03 +0200

    databases/freetds: Fix build with LibreSSL 2.7
     - LibreSSL 2.7 implements OpenSSL 1.1 API
    
    See also: https://bugs.freebsd.org/226911
    Upstream: https://github.com/FreeTDS/freetds/pull/204
    Signed-off-by: Bernard Spil <brnrd@FreeBSD.org>

--- databases/freetds/files/patch-src_tds_sec_negotiate_openssl.h
+++ databases/freetds/files/patch-src_tds_sec_negotiate_openssl.h
@@ -1,10 +1,10 @@
---- src/tds/sec_negotiate_openssl.h.orig	2017-03-06 16:07:14 UTC
+--- src/tds/sec_negotiate_openssl.h.orig	2017-02-15 09:00:01 UTC
 +++ src/tds/sec_negotiate_openssl.h
 @@ -37,6 +37,11 @@
  #error HAVE_OPENSSL not defines, this file should not be included
  #endif
  
-+#ifdef LIBRESSL_VERSION_NUMBER
++#if defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x2070000fL
 +#undef OPENSSL_VERSION_NUMBER
 +#define OPENSSL_VERSION_NUMBER 0x1000107fL
 +#endif
