03bcd309c789e5cad260b4fe6596de28d291adb7
Bernard Spil <brnrd@FreeBSD.org>
2018-04-02 13:47:17 +0200

    security/wpa_supplicant: Fix build with LibreSSL 2.7
     - LibreSSL 2.7 implements OpenSSL 1.1 API
    
    See also: https://bugs.freebsd.org/227173
    Upstream issue: http://lists.infradead.org/pipermail/hostap/2018-April/038375.html
    Signed-off-by: Bernard Spil <brnrd@FreeBSD.org>

--- security/wpa_supplicant/files/patch-src_crypto_crypto__openssl.c
+++ security/wpa_supplicant/files/patch-src_crypto_crypto__openssl.c
@@ -1,6 +1,16 @@
---- src/crypto/crypto_openssl.c.orig	2016-11-02 18:04:18 UTC
+--- src/crypto/crypto_openssl.c.orig	2016-10-02 18:51:11 UTC
 +++ src/crypto/crypto_openssl.c
-@@ -611,7 +611,7 @@ void crypto_cipher_deinit(struct crypto_
+@@ -33,7 +33,8 @@
+ #include "aes_wrap.h"
+ #include "crypto.h"
+ 
+-#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
++#if OPENSSL_VERSION_NUMBER < 0x10100000L || \
++	(defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
+ /* Compatibility wrappers for older versions. */
+ 
+ static HMAC_CTX * HMAC_CTX_new(void)
+@@ -611,7 +612,7 @@ void crypto_cipher_deinit(struct crypto_
  
  void * dh5_init(struct wpabuf **priv, struct wpabuf **publ)
  {
@@ -9,7 +19,7 @@
  	DH *dh;
  	struct wpabuf *pubkey = NULL, *privkey = NULL;
  	size_t publen, privlen;
-@@ -712,7 +712,7 @@ err:
+@@ -712,7 +713,7 @@ err:
  
  void * dh5_init_fixed(const struct wpabuf *priv, const struct wpabuf *publ)
  {
