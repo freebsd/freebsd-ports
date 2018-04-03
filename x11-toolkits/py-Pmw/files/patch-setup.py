51001404ccfb59636c3638bda0faf41282b3a877
sunpoet <sunpoet@FreeBSD.org>
2018-04-03 09:52:11 +0000

    Update to 2.0.1
    - Change MASTER_SITES to CHEESESHOP
    - Sort *_DEPENDS
    - Use USES=dos2unix
    - Relax USES=python
    - Convert to options target helper
    - Do not silence installation messages
    - Clean up Makefile
    - Change WWW to PyPI
    
    PR:		226782
    Submitted by:	sunpoet (myself)
    Approved by:	maintainer (timeout, 14 days)

--- x11-toolkits/py-Pmw/files/patch-setup.py
+++ x11-toolkits/py-Pmw/files/patch-setup.py
@@ -1,14 +1,26 @@
---- setup.py.orig	2007-09-24 17:30:50.000000000 +0900
-+++ setup.py	2008-03-05 20:02:42.000000000 +0900
-@@ -16,11 +16,6 @@
- 		'Pmw.Pmw_1_3.lib',],
- 
-       package_data={'Pmw': ['Pmw_1_3/lib/Pmw.def',
--			    'Pmw_1_3/doc/*',
--	                    'Pmw_1_3/contrib/*',
--	                    'Pmw_1_3/demos/*',
--	                    'Pmw_1_3/tests/*',
--                            'Pmw_1_3/bin/*',
- 			   ]
-                    },
-       
+--- setup.py.orig	2014-03-20 14:26:53 UTC
++++ setup.py
+@@ -6,11 +6,6 @@ if sys.version_info[0]<3:
+     version='2.0.1' # really '1.3.3'
+     packages=['Pmw', 'Pmw.Pmw_1_3_3', 'Pmw.Pmw_1_3_3.lib',]
+     package_data={'Pmw': ['Pmw_1_3_3/lib/Pmw.def',
+-                        'Pmw_1_3_3/doc/*',
+-                        'Pmw_1_3_3/contrib/*',
+-                        'Pmw_1_3_3/demos/*',
+-                        'Pmw_1_3_3/tests/*',
+-                            'Pmw_1_3_3/bin/*',
+                         ]
+                     }
+     development_status = 'Beta'
+@@ -18,11 +13,6 @@ else:
+     version='2.0.1'
+     packages=['Pmw', 'Pmw.Pmw_2_0_1', 'Pmw.Pmw_2_0_1.lib',]
+     package_data={'Pmw': ['Pmw_2_0_1/lib/Pmw.def',
+-                            'Pmw_2_0_1/doc/*',
+-                            'Pmw_2_0_1/contrib/*',
+-                            'Pmw_2_0_1/demos/*',
+-                            'Pmw_2_0_1/tests/*',
+-                            'Pmw_2_0_1/bin/*',
+                             ],
+                     }
+     development_status = 'Alpha'
