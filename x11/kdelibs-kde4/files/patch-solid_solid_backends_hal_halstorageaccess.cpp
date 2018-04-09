00e5768b2f4b5e56c9776aaf52a0685a69e50675
adridg <adridg@FreeBSD.org>
2018-04-09 08:57:19 +0000

    'large' is no longer a valid msdosfs mount option in head after r319735.
    Removes the -o large passed to mount, when kdelibs-kde4 is built on
    a recent-ish 12-CURRENT system. At the same time, fix a logic error
    regarding the handling of the -L flag reported by HAL.
    
    PR:		223125
    Submitted by:	rakuco
    Reported by:	avg
    Approved by:	tcberner (mentor, implicit)

--- x11/kdelibs-kde4/files/patch-solid_solid_backends_hal_halstorageaccess.cpp
+++ x11/kdelibs-kde4/files/patch-solid_solid_backends_hal_halstorageaccess.cpp
@@ -1,16 +1,27 @@
---- ./solid/solid/backends/hal/halstorageaccess.cpp.orig	2009-12-11 02:14:57.000000000 +0300
-+++ ./solid/solid/backends/hal/halstorageaccess.cpp	2010-04-11 14:13:21.934934943 +0400
-@@ -317,11 +317,17 @@
+--- solid/solid/backends/hal/halstorageaccess.cpp.orig	2017-11-05 01:51:22 UTC
++++ solid/solid/backends/hal/halstorageaccess.cpp
+@@ -38,6 +38,7 @@
+ #include <stdlib.h>
+ 
+ #ifdef Q_OS_FREEBSD
++#include <sys/param.h>  // For __FreeBSD_version
+ #include <langinfo.h>
+ #endif
+ 
+@@ -399,11 +400,20 @@ bool StorageAccess::callHalVolumeMount()
  
  #ifdef Q_OS_FREEBSD
      char *cType;
 -    if ( fstype=="vfat" && halOptions.contains("-L=")) {
 -        if ( (cType = getenv("LC_ALL")) || (cType = getenv("LC_CTYPE")) || (cType = getenv("LANG")) )
 +    if ( fstype=="vfat" ) {
-+        if ( halOptions.contains("-L=") && (cType = getenv("LC_ALL")) || (cType = getenv("LC_CTYPE")) || (cType = getenv("LANG")) )
++        if ( halOptions.contains("-L=") && ((cType = getenv("LC_ALL")) || (cType = getenv("LC_CTYPE")) || (cType = getenv("LANG"))) )
                options << "-L="+QString(cType);
++#if __FreeBSD_version < 1200033
++        // The "large" option was removed in src r319735.
 +        if ( halOptions.contains("large") )
 +              options << "large";
++#endif
 +        if ( halOptions.contains("-m=") )
 +              options << "-m=644";
 +        if ( halOptions.contains("-M=") )
@@ -21,7 +32,7 @@
          if ((cType = getenv("LC_ALL")) || (cType = getenv("LC_CTYPE")) || (cType = getenv("LANG")) )
              options << "-C="+QString(nl_langinfo(CODESET));
      }
-@@ -336,6 +342,7 @@
+@@ -418,6 +428,7 @@ bool StorageAccess::callHalVolumeMount()
          if (halOptions.contains("flush"))
              options<<"flush";
      }
@@ -29,7 +40,7 @@
      // pass our locale to the ntfs-3g driver so it can translate local characters
      else if ( halOptions.contains("locale=") ) {
          // have to obtain LC_CTYPE as returned by the `locale` command
-@@ -345,7 +352,6 @@
+@@ -427,7 +438,6 @@ bool StorageAccess::callHalVolumeMount()
              options << "locale="+QString(cType);
          }
      }
