85a79fab40d543764005acb6de12fec02b304fb0
Tobias Kortkamp <tobik@FreeBSD.org>
2018-04-10 08:13:16 +0000

    security/testssl.sh: Update to 2.9.5-4
    Changes:	https://github.com/drwetter/testssl.sh/releases/tag/v2.9.5-4

--- security/testssl.sh/files/patch-testssl.sh
+++ security/testssl.sh/files/patch-testssl.sh
@@ -10,12 +10,3 @@
  declare -x OPENSSL OPENSSL_TIMEOUT
  FAST_SOCKET=${FAST_SOCKET:-false}       # EXPERIMENTAL feature to accelerate sockets -- DO NOT USE it for production
  COLOR=${COLOR:-2}                       # 2: Full color, 1: b/w+positioning, 0: no ESC at all
-@@ -6221,7 +6224,7 @@ certificate_info() {
-           # best we are able to do under OpenBSD
-           days2expire=""
-      else
--          days2expire=$(( $(parse_date "$enddate" "+%s" "%F %H:%M %z") - $(LC_ALL=C date "+%s") ))    # in seconds
-+          days2expire=$(( $(parse_date "$enddate" "+%s" "%F %H:%M") - $(LC_ALL=C date "+%s") ))    # in seconds
-           days2expire=$((days2expire  / 3600 / 24 ))
- 
-           if grep -q "^Let's Encrypt Authority" <<< "$issuer_CN"; then          # we take the half of the thresholds for LE certificates
