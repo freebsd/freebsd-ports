7534e56f6792ba640ede540c8d9ff819a2898ac0
jbeich <jbeich@FreeBSD.org>
2018-04-10 00:13:43 +0000

    multimedia/libvpx: enable NEON on armv6 with more CPUTYPE values
    PR:		224107
    Submitted by:	Mikaël Urankar

--- multimedia/libvpx/files/patch-build_make_configure.sh
+++ multimedia/libvpx/files/patch-build_make_configure.sh
@@ -1,7 +1,23 @@
+- Add armv6 targets where NEON depends on CFLAGS
 - Recognize DragonFly as x86_64-linux-gcc target by default
 
 --- build/make/configure.sh.orig	2018-01-24 22:25:44 UTC
 +++ build/make/configure.sh
+@@ -696,6 +696,14 @@ process_common_toolchain() {
+       aarch64*)
+         tgt_isa=arm64
+         ;;
++      armv6*-gnueabihf)
++        tgt_isa=armv6
++        float_abi=hard
++        ;;
++      armv6*-gnueabi)
++        tgt_isa=armv6
++        float_abi=softfp
++        ;;
+       armv7*-hardfloat* | armv7*-gnueabihf | arm-*-gnueabihf)
+         tgt_isa=armv7
+         float_abi=hard
 @@ -767,7 +767,7 @@ process_common_toolchain() {
          [ -z "$tgt_isa" ] && tgt_isa=x86
          tgt_os=win32
