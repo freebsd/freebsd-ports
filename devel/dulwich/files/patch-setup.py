3686c737e094d61d242b71462271800c788642bb
wg <wg@FreeBSD.org>
2018-04-12 16:21:27 +0000

    devel/dulwich: update to 0.18.6
    Approved by:	maintainer timeout (email and last couple PRs)

--- devel/dulwich/files/patch-setup.py
+++ devel/dulwich/files/patch-setup.py
@@ -1,10 +1,11 @@
---- setup.py.orig	2016-07-05 22:58:08 UTC
-+++ setup.py
-@@ -83,7 +83,7 @@ setup(name='dulwich',
-       The project is named after the part of London that Mr. and Mrs. Git live in
-       in the particular Monty Python sketch.
+--- setup.py.orig	2018-01-24 16:10:15.448366000 -0600
++++ setup.py	2018-01-24 16:10:49.157689000 -0600
+@@ -91,8 +91,7 @@
+       The project is named after the part of London that Mr. and Mrs. Git live
+       in in the particular Monty Python sketch.
        """,
--      packages=['dulwich', 'dulwich.tests', 'dulwich.tests.compat', 'dulwich.contrib'],
+-      packages=['dulwich', 'dulwich.tests', 'dulwich.tests.compat',
+-                'dulwich.contrib'],
 +      packages=['dulwich', 'dulwich.contrib'],
        package_data={'': ['../docs/tutorial/*.txt']},
        scripts=['bin/dulwich', 'bin/dul-receive-pack', 'bin/dul-upload-pack'],
