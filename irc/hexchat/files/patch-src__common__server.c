5b9d7ef28a24114089ae53ff5fe5ffb25579f6a7
Ashish SHUKLA <ashish@FreeBSD.org>
2018-04-13 13:11:07 +0000

    - Update to 2.14.1 - Add dependency on iso-codes, and meson (new build system) - Remove OPTIONS: NLS SOCKS - Remove hardcoded CA bundle path in patch with a variable   replaced at post-patch - Add workaround for detecting system's OpenSSL version - Update WWW
    PR:		227324
    Approved by:	Piotr Kubaj <pkubaj at anongoth dot pl> (maintainer)

--- irc/hexchat/files/patch-src__common__server.c
+++ irc/hexchat/files/patch-src__common__server.c
@@ -1,11 +1,11 @@
---- src/common/server.c.orig	2016-12-10 14:30:51 UTC
+--- src/common/server.c.orig	2018-03-14 02:26:31 UTC
 +++ src/common/server.c
-@@ -743,7 +743,7 @@ server_connect_success (server *serv)
+@@ -749,7 +749,7 @@ server_connect_success (server *serv)
  		/* it'll be a memory leak, if connection isn't terminated by
  		   server_cleanup() */
  		serv->ssl = _SSL_socket (serv->ctx, serv->sok);
 -		if ((err = _SSL_set_verify (serv->ctx, ssl_cb_verify, NULL)))
-+		if ((err = _SSL_set_verify (serv->ctx, ssl_cb_verify, "/usr/local/share/certs/ca-root-nss.crt")))
++		if ((err = _SSL_set_verify (serv->ctx, ssl_cb_verify, "%%CA_BUNDLE%%")))
  		{
  			EMIT_SIGNAL (XP_TE_CONNFAIL, serv->server_session, err, NULL,
  							 NULL, NULL, 0);
