305c4394a7d937e4e6aa716260109e4ddfdbb94d
Mark Felder <feld@FreeBSD.org>
2018-04-14 17:14:12 +0000

    mail/archiveopteryx: Fix build with modern clang
    MFH:		2018Q2

--- mail/archiveopteryx/files/patch-core__md5.cpp
+++ mail/archiveopteryx/files/patch-core__md5.cpp
@@ -1,6 +1,15 @@
---- ./core/md5.cpp.orig	2014-03-06 13:46:23.918398965 -0600
-+++ ./core/md5.cpp	2014-03-06 13:46:30.784414914 -0600
-@@ -149,12 +149,9 @@
+--- core/md5.cpp.orig	2014-03-10 13:44:59 UTC
++++ core/md5.cpp
+@@ -52,7 +52,7 @@ void MD5::init()
+ 
+ void MD5::add( const char *str, uint len )
+ {
+-    register uint32 t;
++    uint32 t;
+ 
+     /* It's not possible to add() data, call hash(), then add more data
+        and call hash() again, because hash() destroys the accumulated
+@@ -149,12 +149,9 @@ EString MD5::hash()
      }
      swapBytes( in, 14 );
  
@@ -13,3 +22,12 @@
      transform();
      swapBytes( (char *)buf, 4 );
  
+@@ -247,7 +244,7 @@ EString MD5::HMAC( const EString &secret
+ 
+ void MD5::transform()
+ {
+-    register uint32 a, b, c, d;
++    uint32 a, b, c, d;
+     uint32 *inw = (uint32 *)in;
+ 
+     a = buf[0];
