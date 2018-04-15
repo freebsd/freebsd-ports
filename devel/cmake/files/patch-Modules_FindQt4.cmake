29caeeebb58c3224b7a8244f21b00bfac0698f29
Adriaan de Groot <adridg@FreeBSD.org>
2018-04-15 21:43:57 +0000

    Update CMake to 3.11.0. Thanks to antoine@ for the exp-run.
    In the run-up to this commit, many other ports were pre-emptively fixed.
    The only issue still known is math/kig, which had a build failure in
    the exp-run, but which isn't reproducible across multiple 11.1 {i386,amd64}
    machines and poudriere builds. We've decided to forge ahead.
    
    The new CMake version:
     - drops FreeBSD patches that have been incorporated upstream,
     - re-shuffles patches to FindQt4, since upstream has made some changes
       which break FindQt4 in new ways on FreeBSD (while fixing the old ones),
     - has new patches to make OpenMP and BLAS findable on FreeBSD,
     - drops ports libarchive in favor of the version in base, to avoid
       overlinking for the pkg(8) support in CPack (this makes portlint
       complain, and we have decided to ignore it).
    
    PR:		227372 226959 223678
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D14506

--- devel/cmake/files/patch-Modules_FindQt4.cmake
+++ devel/cmake/files/patch-Modules_FindQt4.cmake
@@ -1,36 +1,30 @@
---- Modules/FindQt4.cmake.orig	2017-05-31 14:00:09 UTC
+--- Modules/FindQt4.cmake.orig	2018-03-28 11:16:49 UTC
 +++ Modules/FindQt4.cmake
-@@ -706,14 +706,7 @@ if (QT_QMAKE_EXECUTABLE AND
-   # ask qmake for the plugins directory
-   if (QT_LIBRARY_DIR AND NOT QT_PLUGINS_DIR  OR  QT_QMAKE_CHANGED)
-     _qt4_query_qmake(QT_INSTALL_PLUGINS qt_plugins_dir)
--    set(QT_PLUGINS_DIR NOTFOUND)
--    foreach(qt_cross_path ${CMAKE_FIND_ROOT_PATH})
--      set(qt_cross_paths ${qt_cross_paths} "${qt_cross_path}/plugins")
--    endforeach()
+@@ -713,10 +713,15 @@ if (QT_QMAKE_EXECUTABLE AND
+     foreach(qt_cross_path ${CMAKE_FIND_ROOT_PATH})
+       set(qt_cross_paths ${qt_cross_paths} "${qt_cross_path}/plugins")
+     endforeach()
 -    find_path(QT_PLUGINS_DIR NAMES accessible imageformats sqldrivers codecs designer
--      HINTS ${qt_cross_paths} ${qt_plugins_dir}
--      DOC "The location of the Qt plugins"
++    find_path(QT_PLUGINS_DIR NAMES accessible imageformats sqldrivers codecs
+       HINTS ${qt_cross_paths} ${qt_plugins_dir}
+       DOC "The location of the Qt plugins"
 -      NO_CMAKE_FIND_ROOT_PATH)
-+    set(QT_PLUGINS_DIR ${qt_plugins_dir} CACHE PATH "The location of the Qt plugins" FORCE)
++      NO_CMAKE_FIND_ROOT_PATH
++      NO_SYSTEM_ENVIRONMENT_PATH
++      NO_CMAKE_SYSTEM_PATH)
++    if(NOT QT_PLUGINS_DIR AND qt_plugins_dir)
++      set(QT_PLUGINS_DIR ${qt_plugins_dir})
++    endif()
    endif ()
  
    # ask qmake for the translations directory
-@@ -726,16 +719,7 @@ if (QT_QMAKE_EXECUTABLE AND
-   if (QT_LIBRARY_DIR AND NOT QT_IMPORTS_DIR OR QT_QMAKE_CHANGED)
-     _qt4_query_qmake(QT_INSTALL_IMPORTS qt_imports_dir)
-     if(qt_imports_dir)
--      set(QT_IMPORTS_DIR NOTFOUND)
--      foreach(qt_cross_path ${CMAKE_FIND_ROOT_PATH})
--        set(qt_cross_paths ${qt_cross_paths} "${qt_cross_path}/imports")
--      endforeach()
--      find_path(QT_IMPORTS_DIR NAMES Qt
--        HINTS ${qt_cross_paths} ${qt_imports_dir}
--        DOC "The location of the Qt imports"
--        NO_CMAKE_FIND_ROOT_PATH
--        NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH
--        NO_CMAKE_SYSTEM_PATH)
-+      set(QT_IMPORTS_DIR ${qt_imports_dir} CACHE PATH "The location of the Qt imports" FORCE)
+@@ -740,6 +745,9 @@ if (QT_QMAKE_EXECUTABLE AND
+         NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH
+         NO_CMAKE_SYSTEM_PATH)
        mark_as_advanced(QT_IMPORTS_DIR)
++      if(NOT QT_IMPORTS_DIR AND qt_imports_dir)
++        set(QT_IMPORTS_DIR ${qt_imports_dir})
++      endif()
      endif()
    endif ()
+ 
