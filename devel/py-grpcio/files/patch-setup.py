8e5f5f7d8407f7eca45e062d697f203d5092f34b
vanilla <vanilla@FreeBSD.org>
2018-04-15 02:01:44 +0000

    Update to 1.11.0.

--- devel/py-grpcio/files/patch-setup.py
+++ devel/py-grpcio/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2018-01-02 23:09:14 UTC
+--- setup.py.orig	2018-04-13 19:56:08 UTC
 +++ setup.py
 @@ -37,17 +37,6 @@ PYTHON_STEM = os.path.join('src', 'pytho
  CORE_INCLUDE = ('include', '.',)
@@ -15,10 +15,10 @@
 -  CARES_INCLUDE += (os.path.join('third_party', 'cares', 'config_linux'),)
 -if 'openbsd' in sys.platform:
 -  CARES_INCLUDE += (os.path.join('third_party', 'cares', 'config_openbsd'),)
+ ADDRESS_SORTING_INCLUDE = (os.path.join('third_party', 'address_sorting', 'include'),)
  README = os.path.join(PYTHON_STEM, 'README.rst')
  
- # Ensure we're in the proper directory whether or not we're being used by pip.
-@@ -143,12 +132,9 @@ CYTHON_EXTENSION_MODULE_NAMES = ('grpc._
+@@ -145,12 +134,10 @@ CYTHON_EXTENSION_MODULE_NAMES = ('grpc._
  CYTHON_HELPER_C_FILES = ()
  
  CORE_C_FILES = tuple(grpc_core_dependencies.CORE_SOURCE_FILES)
@@ -26,9 +26,9 @@
 -  CORE_C_FILES = filter(lambda x: 'third_party/cares' not in x, CORE_C_FILES)
  
  EXTENSION_INCLUDE_DIRECTORIES = (
--    (PYTHON_STEM,) + CORE_INCLUDE + BORINGSSL_INCLUDE + ZLIB_INCLUDE +
--    CARES_INCLUDE)
-+    (PYTHON_STEM,) + CORE_INCLUDE + BORINGSSL_INCLUDE + ZLIB_INCLUDE)
+     (PYTHON_STEM,) + CORE_INCLUDE + BORINGSSL_INCLUDE + ZLIB_INCLUDE +
+-    CARES_INCLUDE + ADDRESS_SORTING_INCLUDE)
++    ADDRESS_SORTING_INCLUDE)
  
  EXTENSION_LIBRARIES = ()
  if "linux" in sys.platform:
--- devel/py-grpcio/files/patch-third__party_boringssl_include_openssl_base.h
+++ devel/py-grpcio/files/patch-third__party_boringssl_include_openssl_base.h
@@ -1,6 +1,6 @@
---- third_party/boringssl/include/openssl/base.h.orig	2018-01-02 23:09:20 UTC
+--- third_party/boringssl/include/openssl/base.h.orig	2018-04-13 19:56:11 UTC
 +++ third_party/boringssl/include/openssl/base.h
-@@ -328,9 +328,6 @@ typedef void *OPENSSL_BLOCK;
+@@ -367,9 +367,6 @@ typedef void *OPENSSL_BLOCK;
  // MSVC doesn't set __cplusplus to 201103 to indicate C++11 support (see
  // https://connect.microsoft.com/VisualStudio/feedback/details/763051/a-value-of-predefined-macro-cplusplus-is-still-199711l)
  // so MSVC is just assumed to support C++11.
