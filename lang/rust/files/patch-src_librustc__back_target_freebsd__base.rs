4eb4c9a18495f472f0384e710c566e9ec76485ed
Bryan Drewery <bdrewery@FreeBSD.org>
2018-04-16 21:37:21 +0000

    MFH: r467538
    Update to 1.25.0.
    
    - The ABI patch is no longer needed on head due to fixes being upstreamed
      to use the pre-ino64 symbols.  The ABI patch is still needed for the
      bootstrap but should be removable for 1.26.0 if it uses beta 2018-03-18.
    
    PR:		227130 [based on]
    Tested by:	dumbbell, Charlie Li
    Submitted by:	riggs [based on]
    Differential Revision:	https://reviews.freebsd.org/D14921 [based on]
    
    Approved by:	portmgr (implicit)

--- lang/rust/files/patch-src_librustc__back_target_freebsd__base.rs
+++ lang/rust/files/patch-src_librustc__back_target_freebsd__base.rs
@@ -1,13 +1,12 @@
 Return struct like OpenBSD, see src/librustc_trans/cabi_x86.rs
 
---- src/librustc_back/target/freebsd_base.rs.orig	2017-11-22 21:33:00 UTC
+--- src/librustc_back/target/freebsd_base.rs.orig	2018-03-25 14:26:14 UTC
 +++ src/librustc_back/target/freebsd_base.rs
-@@ -31,6 +31,8 @@ pub fn opts() -> TargetOptions {
+@@ -31,6 +31,7 @@ pub fn opts() -> TargetOptions {
          target_family: Some("unix".to_string()),
          linker_is_gnu: true,
          has_rpath: true,
 +        is_like_openbsd: true,
-+        eliminate_frame_pointer: false,
          pre_link_args: args,
          position_independent_executables: true,
-         relro_level: RelroLevel::Full,
+         eliminate_frame_pointer: false, // FIXME 43575
