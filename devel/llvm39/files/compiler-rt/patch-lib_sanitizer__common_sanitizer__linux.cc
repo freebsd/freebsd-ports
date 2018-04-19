d6a8d47bdbc687ffe1580ad1668ca946954ef5f1
Brooks Davis <brooks@FreeBSD.org>
2018-04-19 17:02:25 +0000

    Remove a needless include of vm/pmap.h which broke the i386 build with COMPILER_RT enabled on recent CURRENT.
    Rename files as if EXTRA_PATCHS+=compiler-rt were used, but don't use it
    due to compiler-rt's odd location in WRKDIR.
    
    No functional change.

--- devel/llvm39/files/compiler-rt/patch-lib_sanitizer__common_sanitizer__linux.cc
+++ devel/llvm39/files/compiler-rt/patch-lib_sanitizer__common_sanitizer__linux.cc
@@ -1,8 +1,14 @@
-diff --git lib/sanitizer_common/sanitizer_linux.cc lib/sanitizer_common/sanitizer_linux.cc
-index 7328a5c0ac1..4b59a39ee27 100644
---- lib/sanitizer_common/sanitizer_linux.cc
-+++ lib/sanitizer_common/sanitizer_linux.cc
-@@ -223,7 +223,8 @@ static void kernel_stat_to_stat(struct kernel_stat *in, struct stat *out) {
+--- lib/sanitizer_common/sanitizer_linux.cc.orig	2017-01-06 20:57:47.000000000 +0000
++++ lib/sanitizer_common/sanitizer_linux.cc	2018-04-19 17:03:38.293789000 +0100
+@@ -63,7 +63,6 @@
+ #include <sys/exec.h>
+ #include <sys/sysctl.h>
+ #include <vm/vm_param.h>
+-#include <vm/pmap.h>
+ #include <machine/atomic.h>
+ extern "C" {
+ // <sys/umtx.h> must be included after <errno.h> and <sys/types.h> on
+@@ -223,7 +222,8 @@ static void kernel_stat_to_stat(struct k
  
  uptr internal_stat(const char *path, void *buf) {
  #if SANITIZER_FREEBSD
@@ -12,7 +18,7 @@ index 7328a5c0ac1..4b59a39ee27 100644
  #elif SANITIZER_USES_CANONICAL_LINUX_SYSCALLS
    return internal_syscall(SYSCALL(newfstatat), AT_FDCWD, (uptr)path,
                            (uptr)buf, 0);
-@@ -247,7 +248,8 @@ uptr internal_stat(const char *path, void *buf) {
+@@ -247,7 +247,8 @@ uptr internal_stat(const char *path, voi
  
  uptr internal_lstat(const char *path, void *buf) {
  #if SANITIZER_FREEBSD
@@ -22,7 +28,7 @@ index 7328a5c0ac1..4b59a39ee27 100644
  #elif SANITIZER_USES_CANONICAL_LINUX_SYSCALLS
    return internal_syscall(SYSCALL(newfstatat), AT_FDCWD, (uptr)path,
                           (uptr)buf, AT_SYMLINK_NOFOLLOW);
-@@ -590,7 +592,9 @@ uptr internal_getppid() {
+@@ -590,7 +591,9 @@ uptr internal_getppid() {
  }
  
  uptr internal_getdents(fd_t fd, struct linux_dirent *dirp, unsigned int count) {
