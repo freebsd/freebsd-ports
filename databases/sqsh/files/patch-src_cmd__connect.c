38e700b635170646c4a1e583e3b189c679a9f074
Mikhail Teterin <mi@FreeBSD.org>
2018-04-20 21:10:25 +0000

    Upgrade from 2.1.8 to 2.5.6.1

--- databases/sqsh/files/patch-src_cmd__connect.c
+++ databases/sqsh/files/patch-src_cmd__connect.c
@@ -1,27 +1,36 @@
---- src/cmd_connect.c.orig	2012-03-14 09:17:51 UTC
-+++ src/cmd_connect.c
-@@ -805,9 +805,11 @@ int cmd_connect( argc, argv )
- 	/* Then we use freetds */
+--- src/cmd_connect.c	2014-04-04 08:22:38
++++ src/cmd_connect.c	2018-04-20 18:26:38
+@@ -861,6 +861,14 @@
          else if (strcmp(tds_version, "7.0") == 0)
              version = CS_TDS_70;
-+#if defined(CS_TDS_80)
++        else if (strcmp(tds_version, "7.1") == 0)
++            version = CS_TDS_71;
++        else if (strcmp(tds_version, "7.2") == 0)
++            version = CS_TDS_72;
++        else if (strcmp(tds_version, "7.3") == 0)
++            version = CS_TDS_73;
++        else if (strcmp(tds_version, "7.4") == 0)
++            version = CS_TDS_74;
          else if (strcmp(tds_version, "8.0") == 0)
-             version = CS_TDS_80;
+-            version = CS_TDS_80;
++            version = CS_TDS_71; /* https://sourceforge.net/p/sqsh/discussion/365924/thread/29ae1664/#7f59/707f */
  #endif
-+#endif
          else version = CS_TDS_50; /* default version */
- 
- 
-@@ -1061,10 +1063,12 @@ int cmd_connect( argc, argv )
- 			case CS_TDS_70:
-                         	env_set( g_env, "tds_version", "7.0" );
- 				break;
-+#if defined(CS_TDS_80)
- 			case CS_TDS_80:
-                         	env_set( g_env, "tds_version", "8.0" );
- 				break;
+@@ -1259,7 +1267,16 @@
+                     env_set( g_env, "tds_version", "7.0" );
+                     break;
+-                case CS_TDS_80:
++                case CS_TDS_71:
+                     env_set( g_env, "tds_version", "8.0" );
+                     break;
++                case CS_TDS_72:
++                    env_set( g_env, "tds_version", "7.2" );
++                    break;
++                case CS_TDS_73:
++                    env_set( g_env, "tds_version", "7.2" );
++                    break;
++                case CS_TDS_74:
++                    env_set( g_env, "tds_version", "7.2" );
++                    break;
  #endif
-+#endif
- 			default:
-                         	env_set( g_env, "tds_version", "unknown" );
- 		}
+                 default:
