96978f5239837c1e45ebeca78160596e82abf9a0
tobik <tobik@FreeBSD.org>
2018-04-21 18:51:13 +0000

    java/openjfx8-devel: Update to 8u172-b11

--- java/openjfx8-devel/files/patch-modules_web_src_main_native_Source_WebCore_PlatformJava.cmake
+++ java/openjfx8-devel/files/patch-modules_web_src_main_native_Source_WebCore_PlatformJava.cmake
@@ -1,6 +1,17 @@
---- modules/web/src/main/native/Source/WebCore/PlatformJava.cmake.orig	2018-01-16 01:40:56 UTC
+--- modules/web/src/main/native/Source/WebCore/PlatformJava.cmake.orig	2018-03-23 23:53:57 UTC
 +++ modules/web/src/main/native/Source/WebCore/PlatformJava.cmake
-@@ -242,7 +242,7 @@ set(WebCore_FORWARDING_HEADERS_FILES
+@@ -188,8 +188,8 @@ elseif (APPLE)
+ endif ()
+ 
+ list(APPEND WebCore_LIBRARIES
+-    XMLJava
+-    XSLTJava
++    ${CMAKE_INSTALL_PREFIX}/lib/libxml2.so
++    ${CMAKE_INSTALL_PREFIX}/lib/libxslt.so
+ )
+ 
+ list(APPEND WebCore_USER_AGENT_STYLE_SHEETS
+@@ -225,7 +225,7 @@ set(WebCore_FORWARDING_HEADERS_FILES
  set(WebCore_USER_AGENT_SCRIPTS_DEPENDENCIES ${WEBCORE_DIR}/platform/java/RenderThemeJava.cpp)
  
  list(APPEND WebCore_LIBRARIES
