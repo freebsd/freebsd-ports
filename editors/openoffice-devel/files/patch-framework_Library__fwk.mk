f5949a1c46edf14c0ec3b57d59a66a1416e05255
Don Lewis <truckman@FreeBSD.org>
2018-04-22 17:09:14 +0000

    Upgrade openoffice-devel to upstream SVN r1829757.
    The fix for compatibility with boost 1.67 has been upstreamed, so
    remove BROKEN.
    
    Upstream has switched from gstreamer 0.10 to gstreamer 1.x, so change
    our dependency to match.  Don't specify an explicit LIB_DEPENDS in
    addition to USE_GSTREAMER1.
    
    Most of the patches to work around various compiler issues have been
    upstreamed, so remove them here.
    
    Modernize patch-framework_Library__fwk.mk.

--- editors/openoffice-devel/files/patch-framework_Library__fwk.mk
+++ editors/openoffice-devel/files/patch-framework_Library__fwk.mk
@@ -1,14 +1,15 @@
---- framework/Library_fwk.mk.orig	2016-08-29 00:45:25 UTC
+--- framework/Library_fwk.mk.orig	2018-04-13 00:55:26 UTC
 +++ framework/Library_fwk.mk
-@@ -190,4 +190,11 @@ $(eval $(call gb_Library_add_exception_objects,fwk,\
- 	framework/source/xml/imagesdocumenthandler \
+@@ -196,5 +196,12 @@ $(eval $(call gb_LinkTarget_set_cxx_optimization, \
+ 	framework/source/loadenv/loadenv, $(gb_COMPILEROPT1FLAGS) \
  ))
- 
-+# i126622 - Base 4.1.2 does not open Tables and Queries in Mac OSX
+ endif
 +# Also affects FreeBSD 10.3 with clang 3.4.1.
 +# Appears to be a clang optimization bug in versions less than 3.8.0
 +ifeq ($(COM)$(CPUNAME),CLANGX86_64)
-+$(call gb_CxxObject_get_target,framework/source/loadenv/loadenv):       T_CXXFLAGS := $(gb_LinkTarget_CXXFLAGS) $(gb_LinkTarget_EXCEPTIONFLAGS) $(gb_COMPILERNOOPTFLAGS)
++$(eval $(call gb_LinkTarget_set_cxx_optimization, \
++	framework/source/loadenv/loadenv, $(gb_COMPILEROPT1FLAGS) \
++))
 +endif
-+
+ 
  # vim: set noet sw=4 ts=4:
