b73d3c140ffb5309605ae3eb3fd34c227dd02300
mr <mr@FreeBSD.org>
2018-04-22 18:14:40 +0000

    Add openscad-devel as of 2018.04.19
    PR:		227581

---
+++ cad/openscad-devel/files/patch-src-ModuleCache.h
@@ -0,0 +1,8 @@
+--- src/ModuleCache.h.orig	2018-04-22 17:10:59.553926000 +0000
++++ src/ModuleCache.h	2018-04-22 17:11:26.751065000 +0000
+@@ -2,4 +2,5 @@
+ 
+ #include <string>
++#include <time.h>
+ #include <unordered_map>
+ 
