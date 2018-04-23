12bdfaf12d7b654a1d6979c5b9c8fd5efb949c0d
swills <swills@FreeBSD.org>
2018-04-23 17:30:01 +0000

    www/py-graphite-web: update to 1.1.3

--- www/py-graphite-web/files/patch-bin__build-index.sh
+++ www/py-graphite-web/files/patch-bin__build-index.sh
@@ -1,7 +1,7 @@
---- bin/build-index.sh.orig	2018-02-13 06:01:34.000000000 -0500
-+++ bin/build-index.sh	2018-03-22 08:58:11.785051000 -0400
+--- bin/build-index.sh.orig	2018-04-23 17:11:55 UTC
++++ bin/build-index.sh
 @@ -1,5 +1,5 @@
- #!/bin/bash
+ #!/usr/local/bin/bash
 -export PYTHONPATH="/opt/graphite/webapp/:$PYTHONPATH"
 +export PYTHONPATH="%%PREFIX%%/graphite/webapp/:$PYTHONPATH"
  BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
