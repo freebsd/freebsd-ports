f1eb8e0f95902852cb71a1b858b6b70b1c3ab5e5
Steve Wills <swills@FreeBSD.org>
2018-04-23 17:11:13 +0000

    databases/py-carbon: update to 1.1.3

--- databases/py-carbon/files/patch-setup.py
+++ databases/py-carbon/files/patch-setup.py
@@ -1,20 +1,17 @@
---- setup.py.orig	2017-07-11 17:42:10.000000000 -0400
-+++ setup.py	2017-10-18 10:23:03.662885000 -0400
-@@ -50,12 +50,11 @@
-   from distutils.core import setup
+--- setup.py.orig	2018-04-04 03:38:32.000000000 -0400
++++ setup.py	2018-04-23 12:08:38.358102000 -0400
+@@ -58,10 +58,10 @@ else:
    setup_kwargs = dict()
  
--
--storage_dirs = [ ('storage/ceres', []), ('storage/whisper',[]),
--                 ('storage/lists',[]), ('storage/log',[]),
--                 ('storage/rrd',[]) ]
+ 
+-storage_dirs = [ ('storage/ceres/dummy.txt', []), ('storage/whisper/dummy.txt',[]),
+-                 ('storage/lists',[]), ('storage/log/dummy.txt',[]),
+-                 ('storage/rrd/dummy.txt',[]) ]
 -conf_files = [ ('conf', glob('conf/*.example')) ]
--
 +storage_dirs = [ ('/var/db/carbon/ceres',[]),  ('/var/db/carbon/whisper',[]),
 +                 ('/var/db/carbon/lists',[]), ('/var/db/carbon/log',[]),
 +                 ('/var/db/carbon/rrd',[]) ]
 +conf_files = [ ('%%PREFIX%%/etc/carbon', glob('conf/*.example')) ]
-+ 
+ 
  install_files = storage_dirs + conf_files
  
- # Let's include redhat init scripts, despite build platform
