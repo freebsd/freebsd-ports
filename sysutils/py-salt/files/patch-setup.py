f1feaf13e921e3a40ed75d06339b9b78a49ec290
mat <mat@FreeBSD.org>
2018-04-23 16:53:36 +0000

    Update to 2018.3.0
    PR:		227721
    Submitted by:	maintainer
    Sponsored by:	Absolight

--- sysutils/py-salt/files/patch-setup.py
+++ sysutils/py-salt/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2016-11-22 16:50:54 UTC
+--- setup.py.orig	2018-04-02 16:35:13 UTC
 +++ setup.py
-@@ -988,8 +988,8 @@ class SaltDistribution(distutils.dist.Di
+@@ -992,8 +992,8 @@ class SaltDistribution(distutils.dist.Di
      def _property_data_files(self):
          # Data files common to all scenarios
          data_files = [
