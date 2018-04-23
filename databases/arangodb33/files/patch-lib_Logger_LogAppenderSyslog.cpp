01c8f600d103034255887830e14aee3c7f1f6e43
Yuri Victorovich <yuri@FreeBSD.org>
2018-04-23 15:49:47 +0000

    databases/arangodb33: Update 3.3.3 -> 3.3.7
    Port changes:
    * Update maintaner e-mail galu@packetdam.com -> dev@dudu.ro
    * Patch updates and additions
    
    PR:		227637
    Submitted by:	Vlad Galu <dev@dudu.ro> (mantainer)

--- databases/arangodb33/files/patch-lib_Logger_LogAppenderSyslog.cpp
+++ databases/arangodb33/files/patch-lib_Logger_LogAppenderSyslog.cpp
@@ -1,11 +1,11 @@
---- lib/Logger/LogAppenderSyslog.cpp.orig	2017-11-15 11:23:07 UTC
+--- lib/Logger/LogAppenderSyslog.cpp.orig	2018-04-12 15:37:36 UTC
 +++ lib/Logger/LogAppenderSyslog.cpp
-@@ -64,7 +64,7 @@ LogAppenderSyslog::LogAppenderSyslog(std
+@@ -62,7 +62,7 @@ LogAppenderSyslog::LogAppenderSyslog(std
    if ('0' <= facility[0] && facility[0] <= '9') {
      value = StringUtils::int32(facility);
    } else {
--    CODE* ptr = reinterpret_cast<CODE*>(TRI_facilitynames);
-+    const CODE* ptr = reinterpret_cast<const CODE*>(TRI_facilitynames);
+-    CODE* ptr = reinterpret_cast<CODE*>(facilitynames);
++    const CODE* ptr = reinterpret_cast<const CODE*>(facilitynames);
  
      while (ptr->c_name != 0) {
        if (strcmp(ptr->c_name, facility.c_str()) == 0) {
