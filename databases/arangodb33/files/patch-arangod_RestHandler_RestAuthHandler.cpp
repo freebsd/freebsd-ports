8be88bd66edac7df56a925e3fb2029ee4f6ed8e1
yuri <yuri@FreeBSD.org>
2018-04-23 15:49:47 +0000

    databases/arangodb33: Update 3.3.3 -> 3.3.7
    Port changes:
    * Update maintaner e-mail galu@packetdam.com -> dev@dudu.ro
    * Patch updates and additions
    
    PR:		227637
    Submitted by:	Vlad Galu <dev@dudu.ro> (mantainer)

--- databases/arangodb33/files/patch-arangod_RestHandler_RestAuthHandler.cpp
+++ databases/arangodb33/files/patch-arangod_RestHandler_RestAuthHandler.cpp
@@ -1,11 +1,11 @@
---- arangod/RestHandler/RestAuthHandler.cpp.orig	2017-11-15 11:23:07 UTC
+--- arangod/RestHandler/RestAuthHandler.cpp.orig	2018-04-06 22:44:29 UTC
 +++ arangod/RestHandler/RestAuthHandler.cpp
-@@ -59,7 +59,7 @@ std::string RestAuthHandler::generateJwt
+@@ -54,7 +54,7 @@ std::string RestAuthHandler::generateJwt
      VPackObjectBuilder p(&bodyBuilder);
      bodyBuilder.add("preferred_username", VPackValue(username));
      bodyBuilder.add("iss", VPackValue("arangodb"));
 -    bodyBuilder.add("exp", VPackValue(exp.count()));
 +    bodyBuilder.add("exp", VPackValue((int64_t)exp.count()));
    }
-   return authentication->authInfo()->generateJwt(bodyBuilder);
- }
+   AuthenticationFeature* af = AuthenticationFeature::instance();
+   TRI_ASSERT(af != nullptr);
