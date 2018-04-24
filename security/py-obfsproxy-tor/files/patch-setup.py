1443d2969850d4e29e5a6d25865080c505a268c5
loader <loader@FreeBSD.org>
2018-04-24 06:30:56 +0000

    security/py-obfsproxy-tor: Fix runtime dependency error
    - Change runtime dependency entry point Twisted_Core back to Twisted
    - Update checksum for CHEESESHOP
    - Add test target
    
    Reviewed by:	cs, koobs
    Approved by:	cs (maintainer), koobs (ports)
    Differential Revision:	https://reviews.freebsd.org/D13452
    MFH:		2018Q2

--- security/py-obfsproxy-tor/files/patch-setup.py
+++ security/py-obfsproxy-tor/files/patch-setup.py
@@ -1,19 +1,10 @@
---- setup.py.orig	2014-12-31 15:34:31 UTC
+--- setup.py.orig	2014-08-21 09:28:57 UTC
 +++ setup.py
-@@ -31,12 +31,14 @@ setup(
-     install_requires = [
+@@ -32,7 +32,6 @@ setup(
          'setuptools',
          'PyCrypto',
--        'Twisted',
+         'Twisted',
 -        'argparse',
-+        'Twisted_Core',
          'pyptlib >= 0.0.6',
          'pyyaml'
          ],
- 
-+    #if sys.version_info < (2, 7):
-+    #    install_requires.append('argparse')
-+
-     extras_require = {
-         'SOCKS': ["txsocksx"]
-         }
