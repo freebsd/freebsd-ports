f472052dba562032e7e793ba85c6c01a385df37e
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2018-04-25 02:10:32 +0000

    devel/gsoap: Update version 2.8.59=>2.8.66
    Relnotes:	https://www.genivia.com/changelog.html#latest

--- devel/gsoap/files/patch-gsoap_samples_calc__vs2005_calc__vs2005_stdsoap2.cpp
+++ devel/gsoap/files/patch-gsoap_samples_calc__vs2005_calc__vs2005_stdsoap2.cpp
@@ -1,6 +1,6 @@
---- gsoap/samples/calc_vs2005/calc_vs2005/stdsoap2.cpp.orig	2017-04-07 18:39:02 UTC
+--- gsoap/samples/calc_vs2005/calc_vs2005/stdsoap2.cpp.orig	2018-04-08 14:22:45 UTC
 +++ gsoap/samples/calc_vs2005/calc_vs2005/stdsoap2.cpp
-@@ -3735,7 +3735,7 @@ ssl_auth_init(struct soap *soap)
+@@ -4281,7 +4281,7 @@ ssl_auth_init(struct soap *soap)
    ERR_clear_error();
    if (!soap->ctx)
    {
@@ -9,7 +9,7 @@
      /* TLS_method: a TLS/SSL connection established may understand the SSLv3, TLSv1, TLSv1.1 and TLSv1.2 protocols. */
      soap->ctx = SSL_CTX_new(TLS_method());
  #else
-@@ -3812,7 +3812,7 @@ ssl_auth_init(struct soap *soap)
+@@ -4369,7 +4369,7 @@ ssl_auth_init(struct soap *soap)
  #endif
    if ((soap->ssl_flags & SOAP_SSL_RSA))
    {
@@ -17,8 +17,8 @@
 +#if (OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER))
      if (SSL_CTX_need_tmp_RSA(soap->ctx))
  #endif
-     { RSA *rsa = RSA_generate_key(SOAP_SSL_RSA_BITS, RSA_F4, NULL, NULL);
-@@ -5501,7 +5501,7 @@ tcp_disconnect(struct soap *soap)
+     {
+@@ -6390,7 +6390,7 @@ tcp_disconnect(struct soap *soap)
              DBGLOG(TEST, SOAP_MESSAGE(fdebug, "Connection lost...\n"));
              soap->fclosesocket(soap, soap->socket);
              soap->socket = SOAP_INVALID_SOCKET;
@@ -27,7 +27,7 @@
              ERR_clear_error();
  #else
              ERR_remove_state(0);
-@@ -5525,7 +5525,7 @@ tcp_disconnect(struct soap *soap)
+@@ -6416,7 +6416,7 @@ tcp_disconnect(struct soap *soap)
      }
      SSL_free(soap->ssl);
      soap->ssl = NULL;
@@ -36,7 +36,7 @@
      ERR_clear_error();
  #else
      ERR_remove_state(0);
-@@ -6146,7 +6146,7 @@ soap_done(struct soap *soap)
+@@ -7096,7 +7096,7 @@ soap_done(struct soap *soap)
        soap->ctx = NULL;
      }
    }
