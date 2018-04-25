55caf841166c6b623c560ed35330457f97311a2e
Michael Reifenberger <mr@FreeBSD.org>
2018-04-25 16:29:09 +0000

    Enable experimental features. Make portlint happy. Mark Broken for FreeBSD 10 due to compile/link error.

--- cad/openscad-devel/files/patch-src-ModuleCache.h
+++ cad/openscad-devel/files/patch-src-ModuleCache.h
@@ -1,8 +1,10 @@
---- src/ModuleCache.h.orig	2018-04-22 17:10:59.553926000 +0000
-+++ src/ModuleCache.h	2018-04-22 17:11:26.751065000 +0000
-@@ -2,4 +2,5 @@
+--- src/ModuleCache.h.orig	2018-04-19 22:08:08 UTC
++++ src/ModuleCache.h
+@@ -1,6 +1,7 @@
+ #pragma once
  
  #include <string>
 +#include <time.h>
  #include <unordered_map>
  
+ /*!
