337979002e65a98986d887da6fa4c6c0d2e466d8
jbeich <jbeich@FreeBSD.org>
2018-04-27 23:09:59 +0000

    graphics/mesa-dri: switch to llvm60
    PR:		227682

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -106,7 +106,7 @@ USE_XORG+=	xcb
 .endif
 
 .if ${MOZILLA_VER:R:R} >= 56
-MESA_LLVM_VER?=	50
+MESA_LLVM_VER?=	60
 BUILD_DEPENDS+=	llvm${MESA_LLVM_VER}>0:devel/llvm${MESA_LLVM_VER}
 MOZ_EXPORT+=	LLVM_CONFIG=llvm-config${MESA_LLVM_VER}
 .endif
