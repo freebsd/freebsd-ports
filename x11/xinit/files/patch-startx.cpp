57ab0f3c7888b5d69af6227ed4afd034f93fce51
yuri <yuri@FreeBSD.org>
2018-04-29 21:51:53 +0000

    x11/xinit: Update 1.3.4 -> 1.4.0
    Also add LICENSE_FILE.
    
    PR:		227075
    Submitted by:	Dmitri Goutnik <dg@syrec.org>
    Approved by:	maintainer's timeout (x11@FreeBSD.org; 31 days)

--- x11/xinit/files/patch-startx.cpp
+++ x11/xinit/files/patch-startx.cpp
@@ -4,42 +4,31 @@
 #
 # generate displayname as dictated in the xauth manpage
 #
---- startx.cpp.orig	2014-09-11 17:31:42 UTC
+--- startx.cpp.orig	2018-03-10 01:46:03 UTC
 +++ startx.cpp
-@@ -154,7 +154,7 @@ while [ x"$1" != x ]; do
+@@ -166,7 +166,7 @@ while [ x"$1" != x ]; do
  	else
  	    XCOMM display must be the FIRST server argument
  	    if [ x"$serverargs" = x ] && @@
 -		 expr "$1" : ':[0-9][0-9]*$' > /dev/null 2>&1; then
-+		expr \( "$1" \) : ':[0-9][0-9]*$' > /dev/null 2>&1; then
++		 expr \( "$1" \) : ':[0-9][0-9]*$' > /dev/null 2>&1; then
  		display="$1"
  	    else
  		serverargs="$serverargs $1"
-@@ -193,7 +193,7 @@ if [ x"$server" = x ]; then
-     XCOMM the startx session being seen as inactive:
-     XCOMM "https://bugzilla.redhat.com/show_bug.cgi?id=806491"
-     tty=$(tty)
--    if expr match "$tty" '^/dev/tty[0-9]\+$' > /dev/null; then
-+    if expr "$tty" : '/dev/tty[0-9][0-9]*$' > /dev/null; then
-         tty_num=$(echo "$tty" | grep -oE '[0-9]+$')
-         vtarg="vt$tty_num"
-     fi
-@@ -217,7 +217,7 @@ fi
+@@ -229,7 +229,7 @@ fi
  XCOMM if no vt is specified add vtarg (which may be empty)
  have_vtarg="no"
  for i in $serverargs; do
--    if expr match "$i" '^vt[0-9]\+$' > /dev/null; then
+-    if expr "$i" : 'vt[0-9][0-9]*$' > /dev/null; then
 +    if expr \( "$i" \) : 'vt[0-9][0-9]*$' > /dev/null; then
          have_vtarg="yes"
      fi
  done
-@@ -283,10 +283,10 @@ EOF
- 
+@@ -296,9 +296,9 @@ EOF
      XCOMM now add the same credentials to the client authority file
      XCOMM if '$displayname' already exists do not overwrite it as another
--    XCOMM server man need it. Add them to the '$xserverauthfile' instead.
+     XCOMM server may need it. Add them to the '$xserverauthfile' instead.
 -    for displayname in $authdisplay $hostname$authdisplay; do
-+    XCOMM server may need it. Add them to the '$xserverauthfile' instead.
 +    for displayname in $authdisplay $hostname/unix$authdisplay; do
          authcookie=`XAUTH list "$displayname" @@
 -        | sed -n "s/.*$displayname[[:space:]*].*[[:space:]*]//p"` 2>/dev/null;
