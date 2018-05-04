1bad291d8bf670e0462afee63e30eb822fbfce8d
Jan Beich <jbeich@FreeBSD.org>
2018-05-04 11:40:14 +0000

    multimedia/ccextractor: unbreak with ffmpeg 4.0
    src/lib_ccx/ffmpeg_intgr.c:5:10: fatal error: 'libavfilter/avfiltergraph.h' file not found
             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    PR:		227726
    Reported by:	antoine (via exp-run)

--- multimedia/ccextractor/files/patch-src_lib__ccx_ffmpeg__intgr.c
+++ multimedia/ccextractor/files/patch-src_lib__ccx_ffmpeg__intgr.c
@@ -1,9 +1,10 @@
 --- src/lib_ccx/ffmpeg_intgr.c.orig	2017-01-23 20:33:53 UTC
 +++ src/lib_ccx/ffmpeg_intgr.c
-@@ -3,7 +3,7 @@
+@@ -2,8 +2,7 @@
+ 
  #ifdef ENABLE_FFMPEG
  #include <libavformat/avformat.h>
- #include <libavfilter/avfiltergraph.h>
+-#include <libavfilter/avfiltergraph.h>
 -#include <libavfilter/avcodec.h>
 +#include <libavcodec/avcodec.h>
  #include <libavfilter/buffersink.h>
