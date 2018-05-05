518ef677fadda04fc579509938a445639e1535f9
antoine <antoine@FreeBSD.org>
2018-05-05 08:57:37 +0000

    Note that samba 4.8 is supported and samba 4.4 no longer exists

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -84,7 +84,7 @@ PYTHON2_DEFAULT?=	2.7
 PYTHON3_DEFAULT?=	3.6
 # Possible values: 2.3, 2.4, 2.5
 RUBY_DEFAULT?=		2.4
-# Possible values: 4.4, 4.5, 4.6, 4.7
+# Possible values: 4.5, 4.6, 4.7, 4.8
 SAMBA_DEFAULT?=		4.6
 # Possible values: base, openssl, openssl-devel, libressl, libressl-devel
 .if !defined(SSL_DEFAULT)
