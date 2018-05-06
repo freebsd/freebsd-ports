c97f4de4fa47f4d1df5df0547b805fc8e2399ea4
stephen <stephen@FreeBSD.org>
2018-05-06 00:24:00 +0000

    - Fixed error in which -qt4 is passed to QT tools.  It should be -qt=qt4. - Set MKINFO in configure_args so that makeinfo works. - Since plist changed, bump portrevision.
    PR:		ports/228004
    Submitted by:	Robert Cina <transitive@gmail.com>

--- math/octave/files/patch-libgnu-math.in.h
+++ math/octave/files/patch-libgnu-math.in.h
@@ -1,8 +1,8 @@
---- libgnu/math.in.h.orig	2017-02-22 18:17:36 UTC
+--- libgnu/math.in.h.orig	2018-04-30 17:03:56 UTC
 +++ libgnu/math.in.h
 @@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
  
 -#ifndef _@GUARD_PREFIX@_MATH_H
 +#if 1
