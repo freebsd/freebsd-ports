36de9dfdcc7f13518373f19f56b3bc852a8ddcb0
Kurt Jaeger <pi@FreeBSD.org>
2018-05-06 12:55:59 +0000

    MFH: r469180
    net/kamailio: update 5.0.1 -> 5.1.3
    
    - lots of changes
    
    PR:		227677
    Submitted by:	Ben Hood <ben@relops.com>
    Relnotes:	https://www.kamailio.org/w/kamailio-v5-1-0-release-notes/
    		https://www.kamailio.org/pub/kamailio/5.1.3/ChangeLog
    		https://www.kamailio.org/pub/kamailio/5.1.2/ChangeLog
    		https://www.kamailio.org/pub/kamailio/5.1.1/ChangeLog
    		https://www.kamailio.org/pub/kamailio/5.1.0/ChangeLog
    		https://www.kamailio.org/pub/kamailio/5.0.6/ChangeLog
    Security:	CVE-2018-8828
    Approved by:	ports-secteam (riggs)

--- net/kamailio/files/patch-src_modules_websocket_ws__frame.c
+++ net/kamailio/files/patch-src_modules_websocket_ws__frame.c
@@ -1,4 +1,4 @@
---- src/modules/websocket/ws_frame.c.orig	2017-04-21 12:46:06 UTC
+--- src/modules/websocket/ws_frame.c.orig	2018-03-01 16:24:10 UTC
 +++ src/modules/websocket/ws_frame.c
 @@ -29,7 +29,7 @@
  #ifdef EMBEDDED_UTF8_DECODE
