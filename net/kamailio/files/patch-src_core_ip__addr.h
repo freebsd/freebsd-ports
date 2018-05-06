8d3f713b85a4b44e80f1dd84f54fad53d51f2a61
pi <pi@FreeBSD.org>
2018-05-06 12:55:59 +0000

    MFH: r469180
    net/kamailio: update 5.0.1 -> 5.1.3
    
    - lots of changes
    
    PR:		227677
    Submitted by:	Ben Hood <ben@relops.com>
    Relnotes:	https://www.kamailio.org/w/kamailio-v5-1-0-release-notes/
    		https://www.kamailio.org/pub/kamailio/5.1.3/ChangeLog
    		https://www.kamailio.org/pub/kamailio/5.1.2/ChangeLog
    		https://www.kamailio.org/pub/kamailio/5.1.1/ChangeLog
    		https://www.kamailio.org/pub/kamailio/5.1.0/ChangeLog
    		https://www.kamailio.org/pub/kamailio/5.0.6/ChangeLog
    Security:	CVE-2018-8828
    Approved by:	ports-secteam (riggs)

--- net/kamailio/files/patch-src_core_ip__addr.h
+++ net/kamailio/files/patch-src_core_ip__addr.h
@@ -1,6 +1,6 @@
---- src/core/ip_addr.h.orig	2017-04-21 12:46:05 UTC
+--- src/core/ip_addr.h.orig	2018-03-01 16:24:06 UTC
 +++ src/core/ip_addr.h
-@@ -49,6 +49,10 @@ enum sip_protos { PROTO_NONE, PROTO_UDP, PROTO_TCP, PR
+@@ -49,6 +49,10 @@ enum sip_protos { PROTO_NONE, PROTO_UDP,
  enum comp_methods { COMP_NONE, COMP_SIGCOMP, COMP_SERGZ };
  #endif
  
@@ -8,6 +8,6 @@
 +#define INADDR_LOOPBACK         (u_int32_t)0x7f000001
 +#endif
 +
- struct ip_addr{
- 	unsigned int af; /* address family: AF_INET6 or AF_INET */
- 	unsigned int len;    /* address len, 16 or 4 */
+ typedef struct ip_addr {
+ 	unsigned int af;	/* address family: AF_INET6 or AF_INET */
+ 	unsigned int len;	/* address len, 16 or 4 */
