a3cc3730ee8f067c851eb76587bac4d42bd3cf25
Alexey Dokuchaev <danfe@FreeBSD.org>
2018-05-06 09:02:02 +0000

    - Update `databases/iowow' to version 1.1.0 [*] - Remove ONLY_FOR_ARCHS (x64-specific bits are guarded now) - Cleanup up patch hunks that were included upstream - Fix a typo and generally include port description
    Requested by:	upstream [*]

--- databases/iowow/files/patch-src_fs_iwfile.c
+++ databases/iowow/files/patch-src_fs_iwfile.c
@@ -1,11 +1,11 @@
---- src/fs/iwfile.c.orig	2018-04-12 13:48:26 UTC
+--- src/fs/iwfile.c.orig	2018-05-02 10:28:14 UTC
 +++ src/fs/iwfile.c
-@@ -92,7 +92,7 @@ static iwrc _iwfs_sync(struct IWFS_FILE *f, iwfs_sync_
-     if (fcntl(wf->fh, F_FULLFSYNC) == -1) {
+@@ -97,7 +97,7 @@ static iwrc _iwfs_sync(struct IWFS_FILE *f, iwfs_sync_
+     if (fcntl(impl->fh, F_FULLFSYNC) == -1) {
        return iwrc_set_errno(IW_ERROR_IO_ERRNO, errno);
      }
 -#else
 +#elif defined(__FreeBSD_version) && __FreeBSD_version > 1100501
-     if (fdatasync(wf->fh) == -1) {
+     if (fdatasync(impl->fh) == -1) {
        return iwrc_set_errno(IW_ERROR_IO_ERRNO, errno);
      }
