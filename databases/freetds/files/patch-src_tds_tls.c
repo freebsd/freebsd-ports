a5aa6390074f77b94dfe90b036bbde6833ee6cec
brnrd <brnrd@FreeBSD.org>
2018-05-07 18:44:50 +0000

    databases/freetds: Fix build with LibreSSL 2.7
     - LibreSSL 2.7 introduces the OpenSSL 1.1 API
    
    PR:		226911
    Approved by:	maintainer time-out

--- databases/freetds/files/patch-src_tds_tls.c
+++ databases/freetds/files/patch-src_tds_tls.c
@@ -1,8 +1,8 @@
---- src/tds/tls.c.orig	2015-06-09 10:38:40 UTC
+--- src/tds/tls.c.orig	2017-11-30 09:00:01 UTC
 +++ src/tds/tls.c
-@@ -46,6 +46,10 @@
- #include <sys/stat.h>
- #endif /* HAVE_SYS_STAT_H */
+@@ -50,6 +50,10 @@
+ #include <sys/socket.h>
+ #endif
  
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
 +#include <sys/socket.h>
@@ -11,14 +11,17 @@
  #include <freetds/tds.h>
  #include <freetds/string.h>
  #include <freetds/tls.h>
-@@ -72,6 +76,12 @@
+@@ -72,6 +76,15 @@
  #define SSL_PTR ptr
  #else
  
 +#ifdef LIBRESSL_VERSION_NUMBER
++#if LIBRESSL_VERSION_NUMBER < 0x2070000FL
 +static pthread_mutex_t *openssllocks;
 +#undef OPENSSL_VERSION_NUMBER
 +#define OPENSSL_VERSION_NUMBER 0x1000107fL
++#endif
++#define TLS_ST_OK SSL_ST_OK
 +#endif
 +
  /* some compatibility layer */
