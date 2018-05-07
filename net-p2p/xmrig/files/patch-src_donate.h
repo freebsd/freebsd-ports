9c6fad036cf5ee89d70c11d50f8665b893dac550
ehaupt <ehaupt@FreeBSD.org>
2018-05-07 08:04:03 +0000

    Update to 2.6.2

--- net-p2p/xmrig/files/patch-src_donate.h
+++ net-p2p/xmrig/files/patch-src_donate.h
@@ -1,11 +1,13 @@
---- src/donate.h.orig	2018-02-18 21:31:50 UTC
+--- src/donate.h.orig	2018-05-06 19:17:20 UTC
 +++ src/donate.h
-@@ -37,7 +37,7 @@
+@@ -43,8 +43,8 @@
   * XMR: 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
   * BTC: 1P7ujsXeX7GxQwHNnJsRMgAdNkFZmNVqJT
   */
--constexpr const int kDonateLevel = 5;
-+constexpr const int kDonateLevel = 0;
+-constexpr const int kDefaultDonateLevel = 5;
+-constexpr const int kMinimumDonateLevel = 1;
++constexpr const int kDefaultDonateLevel = 0;
++constexpr const int kMinimumDonateLevel = 0;
  
  
  #endif /* __DONATE_H__ */
