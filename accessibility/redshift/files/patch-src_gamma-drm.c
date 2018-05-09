5d35dffde9e169b068feef23fd0d50dc88b2c073
Steve Wills <swills@FreeBSD.org>
2018-05-09 21:32:05 +0000

    accessibility/redshift: update to 1.11

---
+++ accessibility/redshift/files/patch-src_gamma-drm.c
@@ -0,0 +1,10 @@
+--- src/gamma-drm.c.orig	2018-05-09 16:23:00 UTC
++++ src/gamma-drm.c
+@@ -21,7 +21,6 @@
+ #include <stdlib.h>
+ #include <stdint.h>
+ #include <string.h>
+-#include <alloca.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
