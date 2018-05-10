f5cbde7ae515a31121d4d527faf65162a8cfd814
sunpoet <sunpoet@FreeBSD.org>
2018-05-10 23:14:57 +0000

    Fix device scan
    - Bump PORTREVISION for package change
    
    PR:		227252
    Reported by:	doktornotor <doktornotor@mailinator.com>
    Tested by:	<vidwer@gmail.com>
    Obtained from:	https://github.com/pciutils/pciutils/commit/58d9f25f25c0070c3c3df3073f375d87a9163e88

--- devel/libpci/files/patch-lib-fbsd-device.c
+++ devel/libpci/files/patch-lib-fbsd-device.c
@@ -1,5 +1,14 @@
 --- lib/fbsd-device.c.orig	2017-11-17 12:57:00 UTC
 +++ lib/fbsd-device.c
+@@ -143,7 +143,7 @@ fbsd_scan(struct pci_access *a)
+ 	  t = pci_alloc_dev(a);
+ 	  t->bus = matches[i].pc_sel.pc_bus;
+ 	  t->dev = matches[i].pc_sel.pc_dev;
+-	  t->dev = matches[i].pc_sel.pc_dev;
++	  t->func = matches[i].pc_sel.pc_func;
+ 	  t->domain = matches[i].pc_sel.pc_domain;
+ 	  t->domain_16 = matches[i].pc_sel.pc_domain;
+ 	  t->vendor_id = matches[i].pc_vendor;
 @@ -254,7 +254,7 @@ fbsd_read(struct pci_dev *d, int pos, by
  
    if (d->access->fd_rw < 0)
