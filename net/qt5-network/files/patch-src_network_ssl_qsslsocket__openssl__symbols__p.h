2d6132fc21deaa9507de284cd7331092161a2d93
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2018-05-12 18:04:45 +0000

    Drop some patches for old LibreSSL versions.
    The ports tree currently has LibreSSL 2.7.3, so remove some patches added to
    support LibreSSL < 2.5 (also, according to brnrd@, they are currently broken).
    Cleaning up our existing patches also helps us move to Qt 5.10.
    
    Reviewed by:	tcberner
    Differential Revision:	https://reviews.freebsd.org/D15338

--- net/qt5-network/files/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
+++ net/qt5-network/files/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
@@ -1,7 +1,4 @@
-* Boilerplate for SSL_CTX_set1_groups() used in qsslcontext_openssl.cpp
-*
-* check macro is defined instead of version, LibreSSL < 2.5 doesn't have SSL_CTRL_GET_SERVER_TMP_KEY
-*
+Boilerplate for SSL_CTX_set1_groups() used in qsslcontext_openssl.cpp.
 --- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2018-01-16 06:53:43 UTC
 +++ src/network/ssl/qsslsocket_openssl_symbols_p.h
 @@ -228,7 +228,7 @@ int q_BIO_read(BIO *a, void *b, int c);
@@ -13,7 +10,7 @@
  int q_BN_is_word(BIGNUM *a, BN_ULONG w);
  #else
  // BN_is_word is implemented purely as a
-@@ -511,11 +511,14 @@ void q_EC_KEY_free(EC_KEY *ecdh);
+@@ -511,6 +511,9 @@ void q_EC_KEY_free(EC_KEY *ecdh);
  size_t q_EC_get_builtin_curves(EC_builtin_curve *r, size_t nitems);
  #if OPENSSL_VERSION_NUMBER >= 0x10002000L
  int q_EC_curve_nist2nid(const char *name);
@@ -22,11 +19,4 @@
 +#endif // defined(LIBRESSL_VERSION_NUMBER)
  #endif // OPENSSL_VERSION_NUMBER >= 0x10002000L
  #endif // OPENSSL_NO_EC
--#if OPENSSL_VERSION_NUMBER >= 0x10002000L
-+#if defined(SSL_CTRL_GET_SERVER_TMP_KEY)
- #define q_SSL_get_server_tmp_key(ssl, key) q_SSL_ctrl((ssl), SSL_CTRL_GET_SERVER_TMP_KEY, 0, (char *)key)
--#endif // OPENSSL_VERSION_NUMBER >= 0x10002000L
-+#endif // defined(SSL_CTRL_GET_SERVER_TMP_KEY)
- 
- // PKCS#12 support
- int q_PKCS12_parse(PKCS12 *p12, const char *pass, EVP_PKEY **pkey, X509 **cert, STACK_OF(X509) **ca);
+ #if OPENSSL_VERSION_NUMBER >= 0x10002000L
