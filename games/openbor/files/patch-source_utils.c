038b0a3e2e3c165ae81f5e7045e0cbfdc3c777e8
jbeich <jbeich@FreeBSD.org>
2018-05-14 12:09:24 +0000

    MFH: r469877
    games/openbor: update to 6138
    
    Changes:	https://github.com/DCurrent/openbor/compare/0b077e46...5673214c
    Approved by:	ports-secteam (feld, implicit for snapshots)

--- games/openbor/files/patch-source_utils.c
+++ games/openbor/files/patch-source_utils.c
@@ -1,6 +1,14 @@
---- source/utils.c.bak	2017-04-22 14:20:08 UTC
+source/utils.c:303:54: error: implicit declaration of function 'mallinfo' is invalid in C99
+      [-Werror,-Wimplicit-function-declaration]
+        writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
+                                                     ^
+source/utils.c:303:64: error: member reference base type 'int' is not a structure or union
+        writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
+                                                     ~~~~~~~~~~^~~~~~
+
+--- source/utils.c.orig	2017-04-22 14:20:08 UTC
 +++ source/utils.c
-@@ -295,7 +295,7 @@ void *checkAlloc(void *ptr, size_t size, const char *f
+@@ -299,7 +299,7 @@ void *checkAlloc(void *ptr, size_t size, const char *f
                         "\n*            Shutting Down            *\n\n");
          writeToLogFile("Out of memory!\n");
          writeToLogFile("Allocation of size %i failed in function '%s' at %s:%i.\n", size, func, file, line);
