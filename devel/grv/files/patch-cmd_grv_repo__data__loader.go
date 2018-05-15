cf34e3d54bd5343d775d3498ed8063eb19c39650
pi <pi@FreeBSD.org>
2018-05-15 20:07:43 +0000

    devel/grv: Fix build with devel/libgit2 0.27
    PR:		228273
    Submitted by:	Dmitri Goutnik <dg@syrec.org> (maintainer)

---
+++ devel/grv/files/patch-cmd_grv_repo__data__loader.go
@@ -0,0 +1,11 @@
+--- cmd/grv/repo_data_loader.go.orig	2018-05-15 13:55:57 UTC
++++ cmd/grv/repo_data_loader.go
+@@ -9,7 +9,7 @@ import (
+ 
+ 	log "github.com/Sirupsen/logrus"
+ 	slice "github.com/bradfitz/slice"
+-	git "gopkg.in/libgit2/git2go.v26"
++	git "gopkg.in/libgit2/git2go.v27"
+ )
+ 
+ const (
---
+++ devel/grv/files/patch-src_gopkg.in_libgit2_git2go.v27_diff.go
@@ -0,0 +1,20 @@
+--- src/gopkg.in/libgit2/git2go.v27/diff.go.orig	2018-05-15 14:06:24 UTC
++++ src/gopkg.in/libgit2/git2go.v27/diff.go
+@@ -14,7 +14,7 @@ import (
+ 	"unsafe"
+ )
+ 
+-type DiffFlag int
++type DiffFlag int64
+ 
+ const (
+ 	DiffFlagBinary    DiffFlag = C.GIT_DIFF_FLAG_BINARY
+@@ -405,7 +405,7 @@ func (diff *Diff) Patch(deltaIndex int) 
+ 	return newPatchFromC(patchPtr), nil
+ }
+ 
+-type DiffOptionsFlag int
++type DiffOptionsFlag int64
+ 
+ const (
+ 	DiffNormal                 DiffOptionsFlag = C.GIT_DIFF_NORMAL
