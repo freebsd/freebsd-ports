c6723c34853d705313dec11adb323c0b24a7d23f
timur <timur@FreeBSD.org>
2018-05-17 02:24:59 +0000

    Update Samba supplimentary libraries, fixed (new) portlint errors, changed Python3 version knob.

--- databases/ldb13/files/patch-ldb_cache.c
+++ databases/ldb13/files/patch-ldb_cache.c
@@ -1,5 +1,5 @@
---- ldb_tdb/ldb_cache.c.orig	2018-03-04 05:41:25.313506000 +0100
-+++ ldb_tdb/ldb_cache.c	2018-03-04 05:46:09.353115000 +0100
+--- ldb_tdb/ldb_cache.c.orig	2018-03-04 05:41:25 UTC
++++ ldb_tdb/ldb_cache.c
 @@ -91,7 +91,9 @@ static int ldb_schema_attribute_compare(
  {
  	const struct ldb_schema_attribute *sa1 = (const struct ldb_schema_attribute *)p1;
