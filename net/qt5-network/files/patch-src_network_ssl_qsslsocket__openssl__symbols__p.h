fca547d7c97a143c5651ce25f326217356497a0f
rakuco <rakuco@FreeBSD.org>
2018-05-18 12:27:43 +0000

    Update the Qt5 ports to 5.10.1.
    The work was done by tcberner and myself, with thanks to antoine for the
    exp-run.
    
    Not a lot to report compared to other Qt5 updates:
    * net/qt5-network is still broken with LibreSSL. I said this in a commit
      message ages ago but it bears repeating: upstream is open to adding support
      for LibreSSL, but someone needs to step up to maintain it upstream, otherwise
      things will continue to be broken all the time.
    * www/qt5-webengine is a huge monster that is terrible to update, just like
      www/chromium itself is. We (kde@) have decided to keep using the 5.9 series
      for the time being, as it should be compatible with the rest of Qt anyway. It
      was updated to 5.9.5, the latest 5.9 release at the time of writing.
    
    PR:		228213

--- net/qt5-network/files/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
+++ net/qt5-network/files/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
@@ -1,8 +1,8 @@
 Boilerplate for SSL_CTX_set1_groups() used in qsslcontext_openssl.cpp.
---- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2018-01-16 06:53:43 UTC
+--- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2017-11-30 13:49:46 UTC
 +++ src/network/ssl/qsslsocket_openssl_symbols_p.h
-@@ -228,7 +228,7 @@ int q_BIO_read(BIO *a, void *b, int c);
- Q_AUTOTEST_EXPORT BIO_METHOD *q_BIO_s_mem();
+@@ -232,7 +232,7 @@ BIO *q_BIO_new_mem_buf(void *a, int b);
+ int q_BIO_read(BIO *a, void *b, int c);
  Q_AUTOTEST_EXPORT int q_BIO_write(BIO *a, const void *b, int c);
  int q_BN_num_bits(const BIGNUM *a);
 -#if OPENSSL_VERSION_NUMBER >= 0x10100000L
@@ -10,7 +10,7 @@ Boilerplate for SSL_CTX_set1_groups() used in qsslcontext_openssl.cpp.
  int q_BN_is_word(BIGNUM *a, BN_ULONG w);
  #else
  // BN_is_word is implemented purely as a
-@@ -511,6 +511,9 @@ void q_EC_KEY_free(EC_KEY *ecdh);
+@@ -454,6 +454,9 @@ void q_EC_KEY_free(EC_KEY *ecdh);
  size_t q_EC_get_builtin_curves(EC_builtin_curve *r, size_t nitems);
  #if OPENSSL_VERSION_NUMBER >= 0x10002000L
  int q_EC_curve_nist2nid(const char *name);
