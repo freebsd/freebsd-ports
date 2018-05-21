9613f01eda8d0427aded4454002d5ebe9a65d759
jwb <jwb@FreeBSD.org>
2018-05-21 20:56:17 +0000

    biology/clustalx: Multiple alignment of nucleic acid and protein sequences with GUI
    Approved by:    jrm
    Differential Revision:  https://reviews.freebsd.org/D15505

---
+++ biology/clustalx/files/patch-clustalW_general_VectorOutOfRange.h
@@ -0,0 +1,11 @@
+--- clustalW/general/VectorOutOfRange.h.orig	2018-05-20 18:08:08 UTC
++++ clustalW/general/VectorOutOfRange.h
+@@ -5,6 +5,8 @@
+  */
+ #include <stdexcept>
+ #include <exception>
++#include <string>
++
+ namespace clustalw
+ {
+ 
