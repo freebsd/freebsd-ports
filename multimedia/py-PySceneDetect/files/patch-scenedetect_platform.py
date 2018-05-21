55fe2d7ad9caa24893a2ab09c6ec2ff71c56bff4
swills <swills@FreeBSD.org>
2018-05-21 22:52:23 +0000

    multimedia/py-PySceneDetect: create port
    PySceneDetect is a command-line application and a Python library for detecting
    scene changes in videos, and automatically splitting the video into separate
    clips.
    
    WWW: https://pyscenedetect.readthedocs.io/en/latest/

---
+++ multimedia/py-PySceneDetect/files/patch-scenedetect_platform.py
@@ -0,0 +1,10 @@
+--- scenedetect/platform.py.orig	2018-04-27 12:19:46 UTC
++++ scenedetect/platform.py
+@@ -26,6 +26,7 @@
+ #
+ 
+ # Third-Party Library Imports
++import numpy
+ import cv2
+ 
+ # Compatibility fix for OpenCV < 3.0
