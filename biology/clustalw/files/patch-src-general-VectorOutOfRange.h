773e00abecd0c1fe489600cfa5197749520dd341
jwb <jwb@FreeBSD.org>
2018-05-21 01:13:40 +0000

    biology/clustalw: Fix license, take maintainership
    Approved by:    jrm (mentor)
    Differential Revision:  https://reviews.freebsd.org/D15503

--- biology/clustalw/files/patch-src-general-VectorOutOfRange.h
+++ biology/clustalw/files/patch-src-general-VectorOutOfRange.h
@@ -1,5 +1,5 @@
---- src/general/VectorOutOfRange.h.orig	2013-12-20 23:16:13.000000000 +0800
-+++ src/general/VectorOutOfRange.h	2013-12-20 23:16:37.000000000 +0800
+--- src/general/VectorOutOfRange.h.orig	2010-09-09 14:26:01 UTC
++++ src/general/VectorOutOfRange.h
 @@ -5,6 +5,7 @@
   */
  #include <stdexcept>
