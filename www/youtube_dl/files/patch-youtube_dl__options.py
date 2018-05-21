52ca72bf3da2998f0919ed2f53ceed28bbbdae26
araujo <araujo@FreeBSD.org>
2018-05-21 17:43:16 +0000

    Update to 2018.05.09
    PR:		ports/228238
    Submitted by:	takefu@airport.fm

--- www/youtube_dl/files/patch-youtube_dl__options.py
+++ www/youtube_dl/files/patch-youtube_dl__options.py
@@ -1,6 +1,6 @@
---- youtube_dl/options.py.orig	2016-04-28 23:04:36 UTC
+--- youtube_dl/options.py.orig	2018-05-09 02:36:28 UTC
 +++ youtube_dl/options.py
-@@ -127,10 +127,6 @@ def parseOpts(overrideArguments=None):
+@@ -138,10 +138,6 @@ def parseOpts(overrideArguments=None):
          action='version',
          help='Print program version and exit')
      general.add_option(
