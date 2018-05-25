e98b6cdca6169cfefabcb96b1838adadf75481e9
Adriaan de Groot <adridg@FreeBSD.org>
2018-05-25 11:51:39 +0000

    Update devel/qgit to latest upstream 2.8
    	https://github.com/tibirna/qgit/releases/tag/qgit-2.8
    
    	Version 2.8 releases fixes for a set of issues that were
    	reported and/or solved by QGit contributors in the last year.
    
    	Most notably, the file browsing was fixed. A set of issues
    	with compilation on Windows was also addressed. Breakage of
    	keybindings and menu item accelerators was also sorted out.
    
    The new patch in files/ is there because the release forgot to update
    the version number (reported upstream).
    
    Reported by:	portsmon

---
+++ devel/qgit/files/patch-src_config.h
@@ -0,0 +1,12 @@
+--- src/config.h.orig	2018-05-21 16:21:27 UTC
++++ src/config.h
+@@ -17,7 +17,7 @@
+ #define PACKAGE_TARNAME "qgit"
+ 
+ /* Define to the version of this package. */
+-#define PACKAGE_VERSION "2.7"
++#define PACKAGE_VERSION "2.8"
+ 
+ /* Version number of package */
+-#define VERSION "2.7"
++#define VERSION "2.8"
