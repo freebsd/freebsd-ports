ae635a951dd3d7170fad43cfc661de04960c82cd
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2018-05-25 16:31:21 +0000

    - Moved ports to Uses/lazarus.mk - Add FLAVORS
    Approved by:	portmgr
    Differential Revision:	https://reviews.freebsd.org/D14661

--- multimedia/winff/files/patch-winff_unit1.pas
+++ multimedia/winff/files/patch-winff_unit1.pas
@@ -1,6 +1,15 @@
---- winff/unit1.pas.orig	2013-08-16 03:55:46 UTC
-+++ winff/unit1.pas
-@@ -1228,13 +1228,13 @@ end;
+--- winff/unit1.pas	2016-09-21 16:40:54.000000000 -0500
++++ winff/unit1.pas	2018-03-07 17:35:42.310241000 -0500
+@@ -27,7 +27,7 @@
+   {$IFDEF WINDOWS} windows, shellapi, dos,{$endif}
+   {$IFDEF unix} baseunix, unix, {$endif}
+   laz_xmlcfg, dom, xmlread, xmlwrite, StdCtrls, Buttons, ActnList, Menus, unit2, unit3,
+-  unit4, unit5, gettext, translations, process
++  unit4, unit5, gettext, translations, process, LazFileUtils, LazUTF8
+   {$IFDEF TRANSLATESTRING}, DefaultTranslator{$ENDIF}, ExtCtrls, ComCtrls, Spin, UTF8Process,
+   PoTranslator, types, FileUtil, regexpr;
+ 
+@@ -1241,13 +1241,13 @@
  
  // launch browser
  procedure TfrmMain.launchbrowser(URL:string);
@@ -16,7 +25,7 @@
    s:='';
    if fileexists('/usr/bin/konqueror') then s:='/usr/bin/konqueror';
    if fileexists('/usr/bin/mozilla-firefox') then s:='/usr/bin/mozilla-firefox';
-@@ -1369,13 +1369,13 @@ end;
+@@ -1382,13 +1382,13 @@
  
  // launch pdf
  procedure TfrmMain.LaunchPdf(pdffile:string);
@@ -32,7 +41,7 @@
    s:='';
    if fileexists('/usr/bin/evince') then s:='/usr/bin/evince';
    if fileexists('/usr/bin/kpdf') then s:='/usr/bin/kpdf';
-@@ -1953,7 +1953,7 @@ var s : string;
+@@ -1982,7 +1982,7 @@
  language: string;
  begin
    language:=leftstr(lang,2);
