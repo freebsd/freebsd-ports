e796077c5943c4d0dd8e39e6dc2979a446440056
Yuri Victorovich <yuri@FreeBSD.org>
2018-05-27 20:56:47 +0000

    multimedia/gaupol: Plist problem fixed by the upstream
    Followup to r470990:
    They quickly fixed the plist problem that I've reported.
    
    PR:		226704

--- multimedia/gaupol/files/patch-setup.py
+++ multimedia/gaupol/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2017-11-12 01:10:33 UTC
+--- setup.py.orig	2018-05-27 20:40:27 UTC
 +++ setup.py
-@@ -235,7 +235,10 @@ class InstallData(install_data):
+@@ -232,7 +232,10 @@ class InstallData(install_data):
          data_dir = get_command_obj("install_data").install_dir
          data_dir = os.path.join(data_dir, "share", "gaupol")
          files = glob.glob("{}/extensions/*/*.py".format(data_dir))
@@ -9,6 +9,6 @@
 +                                    base_dir=%%PREFIX%%,
 +                                    prefix=%%STAGEDIR%%,
 +                                    dry_run=self.dry_run)
- 
-     def __generate_linguas(self):
-         """Generate LINGUAS file needed by msgfmt."""
+         # Figure out paths of the compiled files and add them to
+         # self.outfiles so that 'setup.py --record' works correctly.
+         def get_cache_pattern(path):
