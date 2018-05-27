37acad120f870a74e43f733632de53a83cc0ae2e
yuri <yuri@FreeBSD.org>
2018-05-27 19:05:00 +0000

    multimedia/gaupol: Update 0.19.2 -> 1.4; Change of maintainership
    Port changes:
    * greg@unrelenting.technology takes maintainership
    * Change PORTVERSION to DISTVERSION
    * Switch to github
    * Now use distutils/plist/etc
    * Adjust USE_GNOME
    * Delete --single-version-externally-managed in PYDISTUTILS_INSTALLARGS since PYDISTUTILS_INSTALLNOSINGLE is deprecated
    * Change WWW URL
    
    PR:		226704
    Submitted by:	Greg V <greg@unrelenting.technology>

--- multimedia/gaupol/files/patch-setup.py
+++ multimedia/gaupol/files/patch-setup.py
@@ -1,13 +1,14 @@
---- setup.py.orig	2011-09-11 16:26:55.000000000 +0200
-+++ setup.py	2014-06-22 15:30:04.801515555 +0200
-@@ -284,7 +284,9 @@
+--- setup.py.orig	2017-11-12 01:10:33 UTC
++++ setup.py
+@@ -235,7 +235,10 @@ class InstallData(install_data):
          data_dir = get_command_obj("install_data").install_dir
          data_dir = os.path.join(data_dir, "share", "gaupol")
-         files = glob.glob("%s/extensions/*/*.py" % data_dir)
+         files = glob.glob("{}/extensions/*/*.py".format(data_dir))
 -        distutils.util.byte_compile(files, optimize, self.force, self.dry_run)
-+        prefix = %%STAGEDIR%%
-+        base_dir = %%PREFIX%%
-+        distutils.util.byte_compile(files, optimize, self.force, prefix = prefix, base_dir = base_dir, dry_run = self.dry_run)
++        distutils.util.byte_compile(files, optimize, self.force,
++                                    base_dir=%%PREFIX%%,
++                                    prefix=%%STAGEDIR%%,
++                                    dry_run=self.dry_run)
  
-     def __get_desktop_file(self):
-         """Return a tuple for translated desktop file."""
+     def __generate_linguas(self):
+         """Generate LINGUAS file needed by msgfmt."""
