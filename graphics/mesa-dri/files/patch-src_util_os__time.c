ff52187624ead731e335e02c198bd031da277006
Niclas Zeising <zeising@FreeBSD.org>
2018-05-30 19:18:01 +0000

    graphics/mesa-libs, graphics/mesa-dri: Update to 18.1.0
    Update mesa ports to 18.1
    
    Changes:	https://www.mesa3d.org/relnotes/18.1.0.html
    
    PR:		227685
    Submitted by:	jbeich
    Tested by:	Greg V, Carlos J. Puga Medina

--- graphics/mesa-dri/files/patch-src_util_os__time.c
+++ graphics/mesa-dri/files/patch-src_util_os__time.c
@@ -1,16 +1,7 @@
 - Use monotonic clock for timeouts
 
---- src/util/os_time.c.orig	2018-01-23 18:08:50 UTC
+--- src/util/os_time.c.orig	2018-04-21 05:48:25 UTC
 +++ src/util/os_time.c
-@@ -55,7 +55,7 @@
- int64_t
- os_time_get_nano(void)
- {
--#if defined(PIPE_OS_LINUX)
-+#if defined(PIPE_OS_BSD) || defined(PIPE_OS_LINUX)
- 
-    struct timespec tv;
-    clock_gettime(CLOCK_MONOTONIC, &tv);
 @@ -95,7 +95,7 @@ os_time_get_nano(void)
  void
  os_time_sleep(int64_t usecs)
