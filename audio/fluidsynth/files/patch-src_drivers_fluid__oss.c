59c613cbf7bebbdc023bf8a991ec226bf42938b5
Thomas Zander <riggs@FreeBSD.org>
2018-06-01 21:44:32 +0000

    Updtate to upstrem version.
    PR:		228265
    Submitted by:	takefu@airport.fm
    Reviewed by:	riggs
    Approved by:	multimedia@ (riggs)

--- audio/fluidsynth/files/patch-src_drivers_fluid__oss.c
+++ audio/fluidsynth/files/patch-src_drivers_fluid__oss.c
@@ -1,13 +1,13 @@
---- src/drivers/fluid_oss.c.orig	2012-08-16 04:01:13 UTC
+--- src/drivers/fluid_oss.c.orig	2018-05-06 07:05:11 UTC
 +++ src/drivers/fluid_oss.c
 @@ -45,9 +45,9 @@
  #define BUFFER_LENGTH 512
  
  // Build issue on some systems (OSS 4.0)?
--#ifdef SNDCTL_DSP_CHANNELS
+-#if !defined(SOUND_PCM_WRITE_CHANNELS) && defined(SNDCTL_DSP_CHANNELS)
 -  #define SOUND_PCM_WRITE_CHANNELS        SNDCTL_DSP_CHANNELS
 -#endif
-+// #ifdef SNDCTL_DSP_CHANNELS
++// #if !defined(SOUND_PCM_WRITE_CHANNELS) && defined(SNDCTL_DSP_CHANNELS)
 +//   #define SOUND_PCM_WRITE_CHANNELS        SNDCTL_DSP_CHANNELS
 +// #endif
  
