6ee07a4827de5ed3560e547fb4abe2bb0e7e84d4
xride <xride@FreeBSD.org>
2018-06-03 03:25:10 +0000

    Update to 14Aug2014

--- astro/phoon/files/patch-astro.h
+++
@@ -1,8 +0,0 @@
---- astro.h.orig	2003-01-28 00:55:32 UTC
-+++ astro.h
-@@ -43,4 +43,4 @@ extern void phasehunt2( double sdate, do
-  */
- extern double phase( double pdate, double* pphase, double* mage, double* dist, double* angdia, double* sudist, double* suangdia );
- 
--#endif _ASTRO_H_
-+#endif
--- astro/phoon/files/patch-phoon.c
+++ astro/phoon/files/patch-phoon.c
@@ -1,11 +1,11 @@
---- phoon.c.orig	2005-05-20 19:10:24 UTC
+--- phoon.c.orig	2014-06-29 23:07:16 UTC
 +++ phoon.c
-@@ -354,7 +354,7 @@ putmoon( time_t t, int numlines, char* a
-     struct tm* tmP;
-     double jd, pctphase, angphase, cphase, aom, cdist, cangdia, csund, csuang;
-     double phases[2], which[2];
--    long clocknow;
-+    time_t clocknow;
-     int atflrlen, atflridx, numcols, lin, col, midlin;
-     double mcap, yrad, xrad, y, xright, xleft;
-     int colright, colleft;
+@@ -534,7 +534,7 @@ main( int argc, char** argv )
+     if ( argc - argn == 0 )
+ 	{
+ 	/* No arguments present - use the current date and time. */
+-	t = time( (time_t) 0 );
++	t = time( NULL );
+ 	}
+     else if ( argc - argn == 1 || argc - argn == 2 || argc - argn == 3 )
+ 	{
