582ee0978f2616577148ad1dc7b21c63a7a79962
tz <tz@FreeBSD.org>
2018-06-05 13:42:17 +0000

    devel/magit: Update from 2.12.1 to 2.13.0
    Changelog: https://raw.githubusercontent.com/magit/magit/master/Documentation/RelNotes/2.13.0.txt
    
    PR:		228717
    Submitted by:	Yasuhiro KIMURA <yasu@utahime.org>
    Approved by:	Yasuhiro KIMURA (maintainer)

--- devel/magit/files/patch-default.mk
+++ devel/magit/files/patch-default.mk
@@ -1,4 +1,4 @@
---- default.mk.orig	2018-03-29 20:07:19 UTC
+--- default.mk.orig	2018-06-03 00:38:37 UTC
 +++ default.mk
 @@ -89,8 +89,6 @@ ELGS = magit-autoloads.el magit-version.
  
@@ -6,6 +6,6 @@
  
 -VERSION ?= $(shell test -e $(TOP).git && git describe --tags --abbrev=0)
 -
- ASYNC_VERSION       = 1.9.2
- DASH_VERSION        = 2.13.0
- GHUB_VERSION        = 2.0.0
+ ASYNC_VERSION       = 1.9.3
+ DASH_VERSION        = 2.14.1
+ GHUB_VERSION        = 2.0.1
