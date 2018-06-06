e01d3d1c22cbcb6177f6e26494a1526420712bbf
Olivier Cochard <olivier@FreeBSD.org>
2018-06-06 18:40:18 +0000

    Update to 2.3.12

--- graphics/natron/files/patch-Engine_AppManagerPrivate.cpp
+++ graphics/natron/files/patch-Engine_AppManagerPrivate.cpp
@@ -1,24 +1,24 @@
---- Engine/AppManagerPrivate.cpp.orig	2017-08-13 15:26:35 UTC
-+++ Engine/AppManagerPrivate.cpp
-@@ -190,7 +190,7 @@ AppManagerPrivate::createBreakpadHandler(const QString
-                                                                       0,
-                                                                       true,
-                                                                       breakpadPipePath.toStdString().c_str() ) );
+--- Engine/AppManagerPrivate.cpp.orig	2018-05-23 13:32:30.000000000 +0200
++++ Engine/AppManagerPrivate.cpp	2018-06-06 10:16:23.029583000 +0200
+@@ -191,7 +191,7 @@ AppManagerPrivate::createBreakpadHandler(const QString
+                                                                                  (void*)NULL,
+                                                                                  true,
+                                                                                  breakpadPipePath.toStdString().c_str() );
 -#elif defined(Q_OS_LINUX)
 +#elif defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
          Q_UNUSED(breakpadPipePath);
-         breakpadHandler.reset( new google_breakpad::ExceptionHandler( google_breakpad::MinidumpDescriptor( dumpPath.toStdString() ),
-                                                                       0,
-@@ -607,7 +607,7 @@ AppManagerPrivate::initGLAPISpecific()
- #ifdef Q_OS_WIN32
+         breakpadHandler = boost::make_shared<google_breakpad::ExceptionHandler>( google_breakpad::MinidumpDescriptor( dumpPath.toStdString() ),
+                                                                                  google_breakpad::ExceptionHandler::FilterCallback(NULL),
+@@ -616,7 +616,7 @@ AppManagerPrivate::initGLAPISpecific()
+     // scoped_ptr
      wglInfo.reset(new OSGLContext_wgl_data);
      OSGLContext_win::initWGLData( wglInfo.get() );
 -#elif defined(Q_OS_LINUX)
 +#elif defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
+     // scoped_ptr
      glxInfo.reset(new OSGLContext_glx_data);
      OSGLContext_x11::initGLXData( glxInfo.get() );
- 
-@@ -870,7 +870,7 @@ AppManagerPrivate::tearDownGL()
+@@ -994,7 +994,7 @@ AppManagerPrivate::tearDownGL()
      if (wglInfo) {
          OSGLContext_win::destroyWGLData( wglInfo.get() );
      }
