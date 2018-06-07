84b6c91edc38c8b314bffec8095719e0f485b749
Dmitry Marakasov <amdmi3@FreeBSD.org>
2018-06-07 12:54:42 +0000

    mptsd receives mpegts streams from multicast (udp/rtp) or http and combines them into one multiple program stream that is suitable for outputing to DVB-C modulator.
    WWW: https://github.com/gfto/mptsd

---
+++ net/mptsd/files/patch-libtsfuncs_tsfuncs.h
@@ -0,0 +1,10 @@
+--- libtsfuncs/tsfuncs.h.orig	2012-01-05 12:41:01 UTC
++++ libtsfuncs/tsfuncs.h
+@@ -8,6 +8,7 @@
+ #ifndef LIBTS_TSFUNCS_H
+ #define LIBTS_TSFUNCS_H
+ 
++#include <stdint.h>
+ #include <time.h>
+ #include <netdb.h>
+ 
