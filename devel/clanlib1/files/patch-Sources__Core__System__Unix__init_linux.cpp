26edb5d3ce9178a86c629717c1dbf70a3da87148
amdmi3 <amdmi3@FreeBSD.org>
2018-06-09 16:07:10 +0000

    - Add LICENSE_FILE - Add missing depends - Regenerate patches - Revision will be bumped along with other ports for sdl_gfx update

--- devel/clanlib1/files/patch-Sources__Core__System__Unix__init_linux.cpp
+++ devel/clanlib1/files/patch-Sources__Core__System__Unix__init_linux.cpp
@@ -1,6 +1,6 @@
---- ./Sources/Core/System/Unix/init_linux.cpp.orig	2012-09-26 07:30:30.000000000 -0400
-+++ ./Sources/Core/System/Unix/init_linux.cpp	2012-09-26 07:29:01.000000000 -0400
-@@ -131,7 +131,7 @@
+--- Sources/Core/System/Unix/init_linux.cpp.orig	2009-03-23 00:40:42 UTC
++++ Sources/Core/System/Unix/init_linux.cpp
+@@ -131,7 +131,7 @@ std::string CL_System::get_exe_path()
  
  #else
  #ifndef PROC_EXE_PATH
--- devel/clanlib1/files/patch-Sources__Display__Providers__png_provider_generic.cpp
+++ devel/clanlib1/files/patch-Sources__Display__Providers__png_provider_generic.cpp
@@ -1,6 +1,6 @@
---- ./Sources/Display/Providers/png_provider_generic.cpp.orig	2012-09-26 07:31:33.000000000 -0400
-+++ ./Sources/Display/Providers/png_provider_generic.cpp	2012-09-26 07:32:16.000000000 -0400
-@@ -107,7 +107,7 @@
+--- Sources/Display/Providers/png_provider_generic.cpp.orig	2006-10-27 01:55:01 UTC
++++ Sources/Display/Providers/png_provider_generic.cpp
+@@ -107,7 +107,7 @@ void CL_PNGProvider_Generic::init()
  		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp) NULL);
  		cl_assert(false);
  	}   
