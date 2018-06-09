17a875bf968587b7c7eaf2f79dad06cf635bab19
amdmi3 <amdmi3@FreeBSD.org>
2018-06-09 16:14:58 +0000

    - Add LICENSE - Regenerate patches

--- games/chapping/files/patch-game-choosestadium.cpp
+++ games/chapping/files/patch-game-choosestadium.cpp
@@ -1,5 +1,5 @@
---- game/choosestadium.cpp.orig	2013-09-13 21:14:11.171227695 +0400
-+++ game/choosestadium.cpp	2013-09-13 21:14:29.875227107 +0400
+--- game/choosestadium.cpp.orig	2008-05-06 17:38:43 UTC
++++ game/choosestadium.cpp
 @@ -5,6 +5,7 @@
  #include <fstream>
  #include <sys/stat.h>
--- games/chapping/files/patch-game-soccergame.cpp
+++ games/chapping/files/patch-game-soccergame.cpp
@@ -1,5 +1,5 @@
---- game/soccergame.cpp.orig	2013-09-13 20:44:15.075226353 +0400
-+++ game/soccergame.cpp	2013-09-13 20:45:02.462227890 +0400
+--- game/soccergame.cpp.orig	2008-05-06 17:38:43 UTC
++++ game/soccergame.cpp
 @@ -1,3 +1,5 @@
 +#include <unistd.h> // for chdir
 +
