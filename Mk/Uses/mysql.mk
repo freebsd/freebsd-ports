2ec514f10eceda27c806046f30f31096c4cbba7e
Bernard Spil <brnrd@FreeBSD.org>
2018-06-09 16:40:54 +0000

    databases/mariadb103-server: Add MariaDB 10.3 port

--- Mk/Uses/mysql.mk
+++ Mk/Uses/mysql.mk
@@ -72,6 +72,7 @@ MYSQL57_LIBVER=		20
 MYSQL57p_LIBVER=	20
 MYSQL80_LIBVER=		21
 MYSQL102m_LIBVER=	3
+MYSQL103m_LIBVER=	3
 
 # Setting/finding MySQL version we want.
 .if exists(${LOCALBASE}/bin/mysql)
