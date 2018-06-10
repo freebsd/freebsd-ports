88d425306184b1d4754ffda4abfa41eb0dfbf020
yuri <yuri@FreeBSD.org>
2018-06-10 17:17:38 +0000

    audio/fluidplug-lv2: Unbreak on non-Intel/AMD platforms
    Make SSE flags depend on the option SSE that defaults to 'on' only on amd64/i386 architectures.

---
+++ audio/fluidplug-lv2/files/patch-Makefile.mk
@@ -0,0 +1,11 @@
+--- Makefile.mk.orig	2018-06-10 17:12:48 UTC
++++ Makefile.mk
+@@ -34,7 +34,7 @@ CXX ?= g++
+ # Set build and link flags
+ 
+ BASE_FLAGS = -Wall -Wextra -Wshadow -pipe
+-BASE_OPTS  = -O2 -ffast-math -mtune=generic -msse -msse2 -mfpmath=sse -fdata-sections -ffunction-sections
++BASE_OPTS  = -O2 -ffast-math -mtune=generic $(PORT_SIMD_FLAGS) -fdata-sections -ffunction-sections
+ 
+ ifeq ($(MACOS),true)
+ # MacOS linker flags
