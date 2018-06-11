86becf0d5643c1c97f788482ee7fb9933ffc4286
Stephen Hurd <shurd@FreeBSD.org>
2018-06-11 22:57:26 +0000

    Much better way to ensure a UTF-8 CODESET in the locale
    No longer force language or country codes, always call setlocale()

--- comms/quisk/files/patch-quisk.py
+++ comms/quisk/files/patch-quisk.py
@@ -1,13 +1,15 @@
---- quisk.py.orig	2018-06-11 21:58:22 UTC
+--- quisk.py.orig	2016-11-30 18:26:59 UTC
 +++ quisk.py
-@@ -21,6 +21,11 @@ os.chdir(os.path.normpath(os.path.dirnam
+@@ -21,6 +21,13 @@ os.chdir(os.path.normpath(os.path.dirnam
  if sys.path[0] != "'.'":		# Make sure the current working directory is on path
    sys.path.insert(0, '.')
  
 +# Hack to ensure unicode is available.
 +import locale
-+if locale.nl_langinfo(locale.CODESET) == 'US-ASCII':
-+        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
++locale.setlocal(locale.LC_ALL, '')
++cloc = locale.getdefaultlocale()
++if locale.nl_langinfo(locale.CODESET) != 'UTF-8':
++        locale.setlocale(locale.LC_ALL, (cloc[0], 'UTF-8'))
 +
  import wxversion				# Thanks to Mario, DH5YM
  wxversion.ensureMinimal('2.8')
