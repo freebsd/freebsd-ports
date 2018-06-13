33b79d9ef66404b7c30c5e80133c98e356f91963
arved <arved@FreeBSD.org>
2018-06-13 15:06:42 +0000

    Unbreak port
    PR:		228737
    Submitted by:	maintainer

--- japanese/yc.el/files/patch-yc.el
+++ japanese/yc.el/files/patch-yc.el
@@ -1,5 +1,5 @@
---- yc.el.orig	2010-09-30 13:22:22.000000000 +0900
-+++ yc.el	2010-09-30 13:26:09.000000000 +0900
+--- yc.el.orig	2018-06-04 13:46:33.083915000 +0900
++++ yc.el	2018-06-04 14:48:21.116298000 +0900
 @@ -1736,6 +1736,7 @@
  				   (error nil))))
  	    (yc-eval-sexp (car expr)))))
@@ -8,3 +8,12 @@
      (if romkana-table
  	(setq yc-rH-conv-dic (yc-search-file-first-in-path
  			      romkana-table (list "." (getenv "HOME")
+@@ -2071,7 +2072,7 @@
+ ;; 文節を指定しない場合、現在の文節が対象となる
+ ;; 読みを取得した文節はその読みをキャッシュする
+ ;; cut が 非nil の場合、指定文節以降の読みを削除する
+-(defun yc-yomi (&optional idx &optional cut)
++(defun yc-yomi (&optional idx cut)
+   (if (integerp idx)
+       (yc-put-bunsetsu-yomi idx (yc-get-bunsetsu-yomi idx cut) cut)
+     (yc-put-bunsetsu-yomi yc-mark (yc-get-bunsetsu-yomi yc-mark cut) cut)))
