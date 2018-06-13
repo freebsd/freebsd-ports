456170ef7d37b11f58bd59bc56688b3c92903a74
swills <swills@FreeBSD.org>
2018-06-13 13:12:44 +0000

    sysutils/google-compute-engine-oslogin: fix oslogin and update to 1.3.0
    PR:		228949
    Submitted by:	Helen Koike <helen.koike@collabora.com> (maintainer)
    MFH:		2018Q2

--- sysutils/google-compute-engine-oslogin/files/patch-utils_oslogin__utils.cc
+++ sysutils/google-compute-engine-oslogin/files/patch-utils_oslogin__utils.cc
@@ -1,6 +1,6 @@
---- utils/oslogin_utils.cc.orig	2018-02-27 22:53:04 UTC
+--- utils/oslogin_utils.cc.orig	2018-06-11 17:16:50 UTC
 +++ utils/oslogin_utils.cc
-@@ -222,7 +222,14 @@ bool ValidatePasswd(struct passwd* resul
+@@ -255,7 +255,14 @@ bool ValidatePasswd(struct passwd* resul
      }
    }
    if (strlen(result->pw_shell) == 0) {
