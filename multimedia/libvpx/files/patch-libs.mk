a7f4a4383a31e0c51c9005c962bf338fcca0a30c
jbeich <jbeich@FreeBSD.org>
2018-06-13 12:33:00 +0000

    multimedia/libvpx: unbreak on powerpc*
    - newer libstdc++ ABI is required by many consumers
    - -mspe and -maltivec are mutually exclusive
    - implement VSX detection for powerpc and powerpc64
    
    PR:		228586
    Submitted by:	jhibbits (based on)

--- multimedia/libvpx/files/patch-libs.mk
+++ multimedia/libvpx/files/patch-libs.mk
@@ -1,16 +1,5 @@
 --- libs.mk.orig	2017-01-12 20:27:27 UTC
 +++ libs.mk
-@@ -117,8 +117,8 @@ endif
- $(BUILD_PFX)third_party/googletest/%.cc.o: CXXFLAGS += -Wno-missing-field-initializers
- # Suppress -Wextra warnings in first party code pending investigation.
- # https://bugs.chromium.org/p/webm/issues/detail?id=1069
--$(BUILD_PFX)vp8/encoder/onyx_if.c.o: CFLAGS += -Wno-unknown-warning-option -Wno-clobbered
--$(BUILD_PFX)vp8/decoder/onyxd_if.c.o: CFLAGS += -Wno-unknown-warning-option -Wno-clobbered
-+$(BUILD_PFX)vp8/encoder/onyx_if.c.o: CFLAGS += -Wno-extra
-+$(BUILD_PFX)vp8/decoder/onyxd_if.c.o: CFLAGS += -Wno-extra
- 
- ifeq ($(CONFIG_MSVS),yes)
- CODEC_LIB=$(if $(CONFIG_STATIC_MSVCRT),vpxmt,vpxmd)
 @@ -339,8 +339,8 @@ else
  	$(qexec)echo 'Libs.private: -lm' >> $@
  endif
