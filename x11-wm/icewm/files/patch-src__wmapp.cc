7d6bd1f9aa0f2f3a404853f4ae1e48032464b8b7
tobik <tobik@FreeBSD.org>
2018-06-16 02:49:07 +0000

    x11-wm/icewm: Fix build with Clang 6
    While here
    
    - Fix license
    - Add missing dependencies
    
    PR:		228317
    Submitted by:	Chris Hutchinson <portmaster@bsdforge.com> (based on)
    Kudos to:	krion (for patiently test building many iterations of it)

--- x11-wm/icewm/files/patch-src__wmapp.cc
+++ x11-wm/icewm/files/patch-src__wmapp.cc
@@ -1,6 +1,15 @@
---- src/wmapp.cc.orig
+--- src/wmapp.cc.orig	2013-11-17 16:54:39 UTC
 +++ src/wmapp.cc
-@@ -337,7 +337,7 @@
+@@ -221,7 +221,7 @@ static void registerProtocols2(Window xid) {
+ #endif
+ 
+     long pid = getpid();
+-    const char wmname[] = "IceWM "VERSION" ("HOSTOS"/"HOSTCPU")";
++    const char wmname[] = "IceWM " VERSION " (" HOSTOS "/" HOSTCPU ")";
+ 
+ #ifdef GNOME1_HINTS
+     XChangeProperty(xapp->display(), xid, 
+@@ -337,7 +337,7 @@ static void initFontPath(IApp *app) {
              int ndirs; // ------------------- retrieve the old X's font path ---
              char ** fontPath(XGetFontPath(xapp->display(), &ndirs));
  
@@ -9,7 +18,7 @@
              newFontPath[ndirs] = fontsdir;
  
              if (fontPath)
-@@ -383,7 +383,7 @@
+@@ -383,7 +383,7 @@ static void initFontPath(IApp *app) {
              XChangeProperty(xapp->display(), manager->handle(),
                              XA_ICEWM_FONT_PATH, XA_STRING, 8, PropModeReplace,
                              (unsigned char *) fontsdir, strlen(fontsdir));
