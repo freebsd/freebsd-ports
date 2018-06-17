62aa0c3ef519d145d7c7194d76d437ed7e85fc37
Diane Bruce <db@FreeBSD.org>
2018-06-17 14:42:24 +0000

    Fix typo of locale.setlocal which should be locale.setlocale
    Pointy hat to:	@shurd

--- comms/quisk/files/patch-quisk.py
+++ comms/quisk/files/patch-quisk.py
@@ -1,12 +1,12 @@
---- quisk.py.orig	2016-11-30 18:26:59 UTC
-+++ quisk.py
-@@ -21,6 +21,13 @@ os.chdir(os.path.normpath(os.path.dirnam
+--- quisk.py.orig	2016-11-30 13:26:59.000000000 -0500
++++ quisk.py	2018-06-17 10:30:03.031192000 -0400
+@@ -21,6 +21,13 @@
  if sys.path[0] != "'.'":		# Make sure the current working directory is on path
    sys.path.insert(0, '.')
  
 +# Hack to ensure unicode is available.
 +import locale
-+locale.setlocal(locale.LC_ALL, '')
++locale.setlocale(locale.LC_ALL, '')
 +cloc = locale.getdefaultlocale()
 +if locale.nl_langinfo(locale.CODESET) != 'UTF-8':
 +        locale.setlocale(locale.LC_ALL, (cloc[0], 'UTF-8'))
