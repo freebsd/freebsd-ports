2b1893c226cadd7e551c707a7890c8c95480f164
Bryan Drewery <bdrewery@FreeBSD.org>
2018-06-19 15:44:42 +0000

    MFH: r472796 r472797 r472798
    Add lost metadata on why this patch exists
    
    Fix nologin check when PAM option is disabled in the port.
    
    PR:		229147
    Submitted by:	Robert Schulze <rs@bytecamp.net>
    
    Forgot PORTREVISION bump for r472797.
    
    PR:		229147
    
    Approved by:	portmgr (implicit)

--- security/openssh-portable/files/patch-servconf.c
+++ security/openssh-portable/files/patch-servconf.c
@@ -1,23 +1,34 @@
---- servconf.c.orig	2015-08-17 20:37:29.913831000 UTC
-+++ servconf.c	2015-08-17 20:37:29.950132000 -0700
-@@ -57,6 +57,7 @@
+r99048 | des | 2002-06-29 05:51:56 -0500 (Sat, 29 Jun 2002) | 4 lines
+Changed paths:
+   M /head/crypto/openssh/myproposal.h
+   M /head/crypto/openssh/readconf.c
+   M /head/crypto/openssh/servconf.c
+
+Apply FreeBSD's configuration defaults.
+
+--- servconf.c.orig	2018-06-19 09:26:26 UTC
++++ servconf.c
+@@ -63,6 +63,7 @@
  #include "auth.h"
  #include "myproposal.h"
  #include "digest.h"
 +#include "version.h"
  
- static void add_listen_addr(ServerOptions *, char *, int);
- static void add_one_listen_addr(ServerOptions *, char *, int);
-@@ -193,7 +194,7 @@ fill_default_server_options(ServerOption
+ static void add_listen_addr(ServerOptions *, const char *,
+     const char *, int);
+@@ -240,7 +241,11 @@ fill_default_server_options(ServerOption
  
  	/* Portable-specific options */
  	if (options->use_pam == -1)
--		options->use_pam = 0;
++#ifdef USE_PAM
 +		options->use_pam = 1;
++#else
+ 		options->use_pam = 0;
++#endif
  
  	/* Standard Options */
- 	if (options->protocol == SSH_PROTO_UNKNOWN)
-@@ -242,7 +243,7 @@ fill_default_server_options(ServerOption
+ 	if (options->num_host_key_files == 0) {
+@@ -280,7 +285,7 @@ fill_default_server_options(ServerOption
  	if (options->print_lastlog == -1)
  		options->print_lastlog = 1;
  	if (options->x11_forwarding == -1)
@@ -26,9 +37,9 @@
  	if (options->x11_display_offset == -1)
  		options->x11_display_offset = 10;
  	if (options->x11_use_localhost == -1)
-@@ -288,7 +289,11 @@ fill_default_server_options(ServerOption
+@@ -320,7 +325,11 @@ fill_default_server_options(ServerOption
  	if (options->gss_strict_acceptor == -1)
- 		options->gss_strict_acceptor = 0;
+ 		options->gss_strict_acceptor = 1;
  	if (options->password_authentication == -1)
 +#ifdef USE_PAM
 +		options->password_authentication = 0;
