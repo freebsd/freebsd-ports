78fc2389396571897986cf811b109565ee8ef221
Dmitry Marakasov <amdmi3@FreeBSD.org>
2018-06-22 21:28:52 +0000

    - Pet portlint - Fix build with clang 6 - Regenerate patches

--- games/ivan/files/patch-FeLib-Source-festring.cpp
+++ games/ivan/files/patch-FeLib-Source-festring.cpp
@@ -1,5 +1,5 @@
---- FeLib/Source/festring.cpp.orig	2004-11-11 20:27:44.000000000 +0300
-+++ FeLib/Source/festring.cpp	2013-11-08 16:54:46.925772448 +0400
+--- FeLib/Source/festring.cpp.orig	2004-11-11 17:27:44 UTC
++++ FeLib/Source/festring.cpp
 @@ -11,6 +11,7 @@
   */
  
@@ -8,7 +8,7 @@
  
  #include "festring.h"
  #include "allocate.h"
-@@ -116,7 +117,7 @@
+@@ -116,7 +117,7 @@ void festring::CreateOwnData(const char*
  {
    Size = N;
    Reserved = N|FESTRING_PAGE;
@@ -17,7 +17,7 @@
    REFS(Ptr) = 0;
    Data = Ptr;
    memcpy(Ptr, CStr, N);
-@@ -137,7 +138,7 @@
+@@ -137,7 +138,7 @@ void festring::SlowAppend(char Char)
        DeletePtr = &REFS(OldPtr);
  
      Reserved = NewSize|FESTRING_PAGE;
@@ -26,7 +26,7 @@
      REFS(NewPtr) = 0;
      Data = NewPtr;
      memcpy(NewPtr, OldPtr, OldSize);
-@@ -150,7 +151,7 @@
+@@ -150,7 +151,7 @@ void festring::SlowAppend(char Char)
    {
      Size = 1;
      Reserved = FESTRING_PAGE;
@@ -35,7 +35,7 @@
      REFS(Ptr) = 0;
      Ptr[0] = Char;
      Data = Ptr;
-@@ -174,7 +175,7 @@
+@@ -174,7 +175,7 @@ void festring::SlowAppend(const char* CS
        DeletePtr = &REFS(OldPtr);
  
      Reserved = NewSize|FESTRING_PAGE;
@@ -44,7 +44,7 @@
      REFS(NewPtr) = 0;
      Data = NewPtr;
      memcpy(NewPtr, OldPtr, OldSize);
-@@ -205,7 +206,7 @@
+@@ -205,7 +206,7 @@ void festring::Assign(sizetype N, char C
    }
  
    Reserved = N|FESTRING_PAGE;
@@ -53,7 +53,7 @@
    REFS(Ptr) = 0;
    Data = Ptr;
    memset(Ptr, C, N);
-@@ -240,7 +241,7 @@
+@@ -240,7 +241,7 @@ void festring::Resize(sizetype N, char C
      }
  
      Reserved = N|FESTRING_PAGE;
@@ -62,7 +62,7 @@
      REFS(NewPtr) = 0;
      Data = NewPtr;
      memcpy(NewPtr, OldPtr, OldSize);
-@@ -259,7 +260,7 @@
+@@ -259,7 +260,7 @@ void festring::Resize(sizetype N, char C
  	--REFS(OldPtr);
  
      Reserved = N|FESTRING_PAGE;
@@ -71,7 +71,7 @@
      REFS(NewPtr) = 0;
      Data = NewPtr;
      memcpy(NewPtr, OldPtr, N);
-@@ -362,7 +363,7 @@
+@@ -362,7 +363,7 @@ void festring::Erase(sizetype Pos, sizet
        sizetype NewSize = MoveReq ? OldSize - Length : Pos;
        Size = NewSize;
        Reserved = NewSize|FESTRING_PAGE;
@@ -80,7 +80,7 @@
        REFS(Ptr) = 0;
        Data = Ptr;
        OwnsData = true;
-@@ -411,7 +412,7 @@
+@@ -411,7 +412,7 @@ void festring::Insert(sizetype Pos, cons
        }
  
        Reserved = NewSize|FESTRING_PAGE;
