d0bac3ed2041117af405c33c2d9ba88d40b565f9
amdmi3 <amdmi3@FreeBSD.org>
2018-06-23 21:18:20 +0000

    - Update to 3.0.3 - Fix conflicts for some system libraries due to incorrect include path order - Pet portlint - Fix openssl detection
    PR:		228764
    Submitted by:	FreeBSD@ShaneWare.Biz (maintainer)

--- devel/godot/files/patch-platform_x11_detect.py
+++ devel/godot/files/patch-platform_x11_detect.py
@@ -1,7 +1,7 @@
---- platform/x11/detect.py.orig	2017-12-28 05:14:45 UTC
+--- platform/x11/detect.py.orig	2018-03-04 00:52:04 UTC
 +++ platform/x11/detect.py
 @@ -52,7 +52,9 @@ def get_opts():
-         BoolVariable('use_static_cpp', 'Link stdc++ statically', False),
+         BoolVariable('use_static_cpp', 'Link libgcc and libstdc++ statically for better portability', False),
          BoolVariable('use_sanitizer', 'Use LLVM compiler address sanitizer', False),
          BoolVariable('use_leak_sanitizer', 'Use LLVM compiler memory leaks sanitizer (implies use_sanitizer)', False),
 -        BoolVariable('pulseaudio', 'Detect & use pulseaudio', True),
@@ -11,16 +11,21 @@
          BoolVariable('udev', 'Use udev for gamepad connection callbacks', False),
          EnumVariable('debug_symbols', 'Add debug symbols to release version', 'yes', ('yes', 'no', 'full')),
          BoolVariable('separate_debug_symbols', 'Create a separate file with the debug symbols', False),
-@@ -154,7 +156,7 @@ def configure(env):
+@@ -154,7 +156,12 @@ def configure(env):
      # FIXME: Check for existence of the libs before parsing their flags with pkg-config
  
      if not env['builtin_openssl']:
 -        env.ParseConfig('pkg-config openssl --cflags --libs')
-+        env.ParseConfig('echo -lssl -lcrypto')
++        try:
++            # try to find a port installed openssl
++            env.ParseConfig('pkg-config openssl --cflags --libs')
++        except:
++            # if none found use base system openssl
++            env.ParseConfig('echo -lssl -lcrypto')
  
      if not env['builtin_libwebp']:
          env.ParseConfig('pkg-config libwebp --cflags --libs')
-@@ -226,12 +228,18 @@ def configure(env):
+@@ -226,12 +233,18 @@ def configure(env):
  
      ## Flags
  
