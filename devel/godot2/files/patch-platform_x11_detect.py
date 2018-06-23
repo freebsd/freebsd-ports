00215eaa951768c1d8377cc2c504ede8d6234bf7
Dmitry Marakasov <amdmi3@FreeBSD.org>
2018-06-23 21:18:26 +0000

    - Fix conflicts for some system libraries due to incorrect include path order - Pet portlint - Fix openssl detection
    PR:		228764
    Submitted by:	FreeBSD@ShaneWare.Biz (maintainer)

--- devel/godot2/files/patch-platform_x11_detect.py
+++ devel/godot2/files/patch-platform_x11_detect.py
@@ -1,5 +1,5 @@
---- ./platform/x11/detect.py.orig	2017-08-27 16:25:12 UTC
-+++ ./platform/x11/detect.py
+--- platform/x11/detect.py.orig	2017-08-28 01:55:12 UTC
++++ platform/x11/detect.py
 @@ -56,7 +56,9 @@ def get_opts():
          ('use_sanitizer', 'Use llvm compiler sanitize address', 'no'),
          ('use_leak_sanitizer', 'Use llvm compiler sanitize memory leaks', 'no'),
@@ -46,24 +46,32 @@
  
      env.ParseConfig('pkg-config x11 --cflags --libs')
      env.ParseConfig('pkg-config xinerama --cflags --libs')
-@@ -148,13 +143,14 @@ def configure(env):
+@@ -148,13 +143,22 @@ def configure(env):
          # Currently not compatible with OpenSSL 1.1.0+
          # https://github.com/godotengine/godot/issues/8624
          import subprocess
 -        openssl_version = subprocess.check_output(['pkg-config', 'openssl', '--modversion']).strip('\n')
-+        openssl_version = '1.0.2' #subprocess.check_output(['pkg-config', 'openssl', '--modversion']).strip('\n')
++        try:
++            openssl_version = subprocess.check_output(['pkg-config', 'openssl', '--modversion']).strip('\n')
++        except:
++            # approx base openssl version
++            openssl_version = '1.0.2'
          if (openssl_version >= "1.1.0"):
              print("Error: Found system-installed OpenSSL %s, currently only supporting version 1.0.x." % openssl_version)
              print("Aborting.. You can compile with 'builtin_openssl=yes' to use the bundled version.\n")
              sys.exit(255)
  
 -        env.ParseConfig('pkg-config openssl --cflags --libs')
-+        #env.ParseConfig('pkg-config openssl --cflags --libs')
-+        env.ParseConfig('echo -lssl -lcrypto')
++        try:
++            # try to find a port installed openssl
++            env.ParseConfig('pkg-config openssl --cflags --libs')
++        except:
++            # if none found use base system openssl
++            env.ParseConfig('echo -lssl -lcrypto')
  
      if (env['builtin_libwebp'] == 'no'):
          env.ParseConfig('pkg-config libwebp --cflags --libs')
-@@ -199,12 +195,13 @@ def configure(env):
+@@ -199,12 +203,13 @@ def configure(env):
      if (env['builtin_glew'] == 'no'):
          env.ParseConfig('pkg-config glew --cflags --libs')
  
@@ -83,7 +91,7 @@
  
      if (platform.system() == "Linux"):
          env.Append(CPPFLAGS=["-DJOYDEV_ENABLED"])
-@@ -219,6 +216,11 @@ def configure(env):
+@@ -219,6 +224,11 @@ def configure(env):
          else:
              print("libudev development libraries not found, disabling udev support")
  
