04e573a0ace94c59911a98fd5edd7fd06f70546b
Adam Weinberger <adamw@FreeBSD.org>
2018-06-24 01:06:40 +0000

    Neutralize Canary::Stability entirely
    As time goes on, the uselessness of this module only increases.
    Previously, a short-circuit was used to return early from import(),
    but there's no point in even wasting time to compile it anymore.
    Instead, when ACTIVATE is off, replace the module entirely with
    a much more functional stub.

---
+++ devel/p5-Canary-Stability/files/Stability.pm
@@ -0,0 +1,19 @@
+=head1 NAME
+
+Canary::Stability - Stub to prevent checking perl compatibility for "schmorp" modules
+
+=head1 SYNOPSIS
+
+  use Canary::Stability;
+
+=head1 PURPOSE
+
+This is a stub module that neutralizes the original Canary::Stability
+module. That module is a relic of a strange notion that "schmorp" should
+prevent people from using any Perl released after 2015.
+
+=cut
+
+package Canary::Stability;
+
+1;
