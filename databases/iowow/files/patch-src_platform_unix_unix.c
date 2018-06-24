4929a7073f21581f2d70998f52a454bb7c211c4c
danfe <danfe@FreeBSD.org>
2018-06-24 06:35:38 +0000

    Update `databases/iowow' to version 1.2.12.
    Reported by:	portscout

--- databases/iowow/files/patch-src_platform_unix_unix.c
+++ databases/iowow/files/patch-src_platform_unix_unix.c
@@ -1,5 +1,5 @@
---- src/platform/linux/linux.c.orig	2018-05-19 14:09:04 UTC
-+++ src/platform/linux/linux.c
+--- src/platform/unix/unix.c.orig	2018-05-19 14:09:04 UTC
++++ src/platform/unix/unix.c
 @@ -314,7 +314,7 @@ iwrc iwp_fdatasync(HANDLE fh) {
    if (fcntl(fh, F_FULLFSYNC) == -1) {
      return iwrc_set_errno(IW_ERROR_IO_ERRNO, errno);
