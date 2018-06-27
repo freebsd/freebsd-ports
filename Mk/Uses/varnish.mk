0885335da2241c87c3f60e04f08d8d56341195f7
Mark Felder <feld@FreeBSD.org>
2018-06-27 15:48:17 +0000

    Add Varnish 6 to the framework
    Reported by:	vanilla

--- Mk/Uses/varnish.mk
+++ Mk/Uses/varnish.mk
@@ -4,7 +4,7 @@
 #
 # Feature:      varnish
 # Usage:        USES=varnish
-# Valid ARGS:   4, 5, run
+# Valid ARGS:   4, 5, 6, run
 #
 # MAINTAINER: ports@FreeBSD.org
 
@@ -17,6 +17,8 @@ VARNISH_VERSION=	${VARNISH_DEFAULT}
 VARNISH_VERSION=	4
 . elif ${varnish_ARGS:M5}
 VARNISH_VERSION=	5
+. elif ${varnish_ARGS:M6}
+VARNISH_VERSION=	6
 . elif defined(VARNISH_DEFAULT)
 . endif
 
