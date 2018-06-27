1cbe60cb91e4f5e0d302dcd31d7a2cda53546631
Yuri Victorovich <yuri@FreeBSD.org>
2018-06-27 05:09:28 +0000

    comms/airspy: Fix build due to the MMX-related errors
    Fix errors like this:
    error: always_inline function '_mm_loadu_ps' requires target feature 'mmx',
           but would be inlined into function 'x' that is compiled without support for 'mmx'
    
    Reported by:	fallout

---
+++ comms/airspy/files/patch-libairspy_src_iqconverter__float.c
@@ -0,0 +1,12 @@
+--- libairspy/src/iqconverter_float.c.orig	2018-06-27 04:58:05 UTC
++++ libairspy/src/iqconverter_float.c
+@@ -39,7 +39,9 @@ THE SOFTWARE.
+   #define _inline inline
+   #define FIR_STANDARD
+ #elif defined(__FreeBSD__)
++# if defined(FREEBSD_USE_SIMD)
+   #define USE_SSE2
++# endif
+ #include <immintrin.h>
+   #define _inline inline
+   #define _aligned_free(mem) free(mem)
