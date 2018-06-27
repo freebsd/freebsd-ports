637586287af33c98b8dc1ee5c3a4e3eb260e9778
Jason W. Bacon <jwb@FreeBSD.org>
2018-06-27 00:04:28 +0000

    science/paraview: Fix protobuf conflict, upgrade to 5.5.1, modernize options
    PR:             229116
    Reported by:    jwb
    Approved by:    jrm (mentor)
    Differential Revision:  https://reviews.freebsd.org/D16024

--- science/paraview/files/patch-Plugins_CDIReader_cdilib.c
+++ science/paraview/files/patch-Plugins_CDIReader_cdilib.c
@@ -1,5 +1,5 @@
---- Plugins/CDIReader/cdilib.c.orig	2018-06-02 11:15:13.052742000 +0700
-+++ Plugins/CDIReader/cdilib.c	2018-06-02 11:15:49.323204000 +0700
+--- Plugins/CDIReader/cdilib.c.orig	2018-06-01 19:23:15 UTC
++++ Plugins/CDIReader/cdilib.c
 @@ -47,7 +47,7 @@
  #define _XOPEN_SOURCE 600
  #endif
--- science/paraview/files/patch-VTK_IO_EnSight_vtkEnSightGoldBinaryReader.cxx
+++ science/paraview/files/patch-VTK_IO_EnSight_vtkEnSightGoldBinaryReader.cxx
@@ -1,6 +1,6 @@
---- VTK/IO/EnSight/vtkEnSight6BinaryReader.cxx.orig	2018-05-30 20:27:53.507473000 +0700
-+++ VTK/IO/EnSight/vtkEnSight6BinaryReader.cxx	2018-05-30 20:34:17.396774000 +0700
-@@ -36,7 +36,7 @@
+--- VTK/IO/EnSight/vtkEnSightGoldBinaryReader.cxx.orig	2018-06-01 19:23:15 UTC
++++ VTK/IO/EnSight/vtkEnSightGoldBinaryReader.cxx
+@@ -37,7 +37,7 @@
  #if defined(_WIN32)
  # define VTK_STAT_STRUCT struct _stat64
  # define VTK_STAT_FUNC _stat64
