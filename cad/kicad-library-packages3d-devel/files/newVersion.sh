8d4e94c8f5eec06875b69935504f386ec3f3935d
mr <mr@FreeBSD.org>
2018-07-06 08:24:33 +0000

    Follow upstream and split the kicad-library-devel port into kicad-library-footprints-devel kicad-library-packages3d-devel and kicad-library-symbols-devel

---
+++ cad/kicad-library-packages3d-devel/files/newVersion.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#GIT_SRC_HASH=`cd kicad-repo; git log -n 1 --pretty=format:"%H"`
+#GIT_SRC_DATE=`cd kicad-repo; git log -n 1 --date=iso-local -n 1 --pretty=format:"%cd" | \
+#        cut -c '1-19' | sed -e 's,-,,g' -e 's,:,,g' -e 's, ,,g'`
+
+CURDIR=`pwd`
+WRKDIR=`pwd`/work
+#mkdir -p "${WRKDIR}"
+
+if [ \! -d "${CURDIR}/kicad-packages3D" ]; then
+ git clone https://github.com/KiCad/kicad-packages3D.git -b master "${CURDIR}/kicad-packages3D";
+else
+  cd "${CURDIR}/kicad-packages3D"
+  git pull;
+fi
+cd "${CURDIR}/kicad-packages3D"
+GIT_P3D_HASH=`git log -n 1 --pretty=format:"%H"`;
+GIT_P3D_DATE=`git log -n 1 --date=iso-local -n 1 --pretty=format:"%cd" | \
+    cut -c '1-19' | sed -e 's,-,,g' -e 's,:,,g' -e 's, ,,g'`
+
+echo "$GIT_P3D_HASH, $GIT_P3D_DATE"
+echo "GIT_P3D_HASH=  ${GIT_P3D_HASH}"    > ${CURDIR}/Makefile.git_rev
+echo "GIT_P3D_DATE=  ${GIT_P3D_DATE}"   >> ${CURDIR}/Makefile.git_rev
+
