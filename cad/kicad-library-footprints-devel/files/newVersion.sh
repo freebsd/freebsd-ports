3ca16fb3311195e285a82b5b3225c55d45b6c94b
Michael Reifenberger <mr@FreeBSD.org>
2018-07-06 08:24:33 +0000

    Follow upstream and split the kicad-library-devel port into kicad-library-footprints-devel kicad-library-packages3d-devel and kicad-library-symbols-devel

---
+++ cad/kicad-library-footprints-devel/files/newVersion.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#GIT_SRC_HASH=`cd kicad-repo; git log -n 1 --pretty=format:"%H"`
+#GIT_SRC_DATE=`cd kicad-repo; git log -n 1 --date=iso-local -n 1 --pretty=format:"%cd" | \
+#        cut -c '1-19' | sed -e 's,-,,g' -e 's,:,,g' -e 's, ,,g'`
+
+CURDIR=`pwd`
+WRKDIR=`pwd`/work
+#mkdir -p "${WRKDIR}"
+
+if [ \! -d "${CURDIR}/kicad-footprints" ]; then
+ git clone https://github.com/KiCad/kicad-footprints.git -b master "${CURDIR}/kicad-footprints";
+else
+  cd "${CURDIR}/kicad-footprints"
+  git pull;
+fi
+cd "${CURDIR}/kicad-footprints"
+GIT_FP_HASH=`git log -n 1 --pretty=format:"%H"`;
+GIT_FP_DATE=`git log -n 1 --date=iso-local -n 1 --pretty=format:"%cd" | \
+    cut -c '1-19' | sed -e 's,-,,g' -e 's,:,,g' -e 's, ,,g'`
+
+echo "$GIT_FP_HASH, $GIT_FP_DATE"
+echo "GIT_FP_HASH=  ${GIT_FP_HASH}"    > ${CURDIR}/Makefile.git_rev
+echo "GIT_FP_DATE=  ${GIT_FP_DATE}"   >> ${CURDIR}/Makefile.git_rev
+
---
+++ cad/kicad-library-symbols-devel/files/newVersion.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#GIT_SRC_HASH=`cd kicad-repo; git log -n 1 --pretty=format:"%H"`
+#GIT_SRC_DATE=`cd kicad-repo; git log -n 1 --date=iso-local -n 1 --pretty=format:"%cd" | \
+#        cut -c '1-19' | sed -e 's,-,,g' -e 's,:,,g' -e 's, ,,g'`
+
+CURDIR=`pwd`
+WRKDIR=`pwd`/work
+#mkdir -p "${WRKDIR}"
+
+if [ \! -d "${CURDIR}/kicad-symbols" ]; then
+ git clone https://github.com/KiCad/kicad-symbols.git -b master "${CURDIR}/kicad-symbols";
+else
+  cd "${CURDIR}/kicad-symbols"
+  git pull;
+fi
+cd "${CURDIR}/kicad-symbols"
+GIT_SYM_HASH=`git log -n 1 --pretty=format:"%H"`;
+GIT_SYM_DATE=`git log -n 1 --date=iso-local -n 1 --pretty=format:"%cd" | \
+    cut -c '1-19' | sed -e 's,-,,g' -e 's,:,,g' -e 's, ,,g'`
+
+echo "$GIT_SYM_HASH, $GIT_SYM_DATE"
+echo "GIT_SYM_HASH=  ${GIT_SYM_HASH}"    > ${CURDIR}/Makefile.git_rev
+echo "GIT_SYM_DATE=  ${GIT_SYM_DATE}"   >> ${CURDIR}/Makefile.git_rev
+
