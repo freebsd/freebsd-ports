872a354bcbe24403aea346dbf4f199fc1b5938b8
mr <mr@FreeBSD.org>
2018-07-07 18:28:47 +0000

    Add two missing patches.

---
+++ cad/qcad/files/patch-src_zip_RZip.cpp
@@ -0,0 +1,11 @@
+--- src/zip/RZip.cpp.orig	2018-07-01 14:53:24 UTC
++++ src/zip/RZip.cpp
+@@ -19,7 +19,7 @@
+ //#include <QTextStream>
+ //#include <QResource>
+ 
+-#include "JlCompress.h"
++#include <quazip5/JlCompress.h>
+ 
+ #include "RZip.h"
+ 
