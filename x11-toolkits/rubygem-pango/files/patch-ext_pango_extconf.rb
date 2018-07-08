476ed7d2a1802141a074a05994b3f5ecd61185de
miwi <miwi@FreeBSD.org>
2018-07-08 10:34:36 +0000

    - Update to 3.2.7
    Changes:	https://ruby-gnome2.osdn.jp/?News_20180607_1#Changes
    
    Sponsored by:	iXsystems Inc.

--- x11-toolkits/rubygem-pango/files/patch-ext_pango_extconf.rb
+++ x11-toolkits/rubygem-pango/files/patch-ext_pango_extconf.rb
@@ -1,11 +1,11 @@
---- ext/pango/extconf.rb.orig	2017-04-27 18:04:28 UTC
+--- ext/pango/extconf.rb.orig	2018-07-07 04:30:00 UTC
 +++ ext/pango/extconf.rb
 @@ -39,7 +39,7 @@ package_id = "pango"
  require "mkmf-gnome2"
  
  depended_packages = [
 -  "glib2",
-+  "cairo", "glib2", "pango"
++  "cairo", "glib2", "pango",
+   "gobject-introspection",
  ]
  depended_packages.each do |package|
-   directory = "#{package}#{version_suffix}"
