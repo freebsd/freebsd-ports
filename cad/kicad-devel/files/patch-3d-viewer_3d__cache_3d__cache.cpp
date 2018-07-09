604a8ef04dbf2a2e92c7d28143303db8ba57844f
jbeich <jbeich@FreeBSD.org>
2018-07-09 13:26:24 +0000

    cad/kicad-devel: unbreak with boost 1.68
    3d-viewer/3d_cache/3d_cache.cpp:37:10: fatal error: 'boost/uuid/sha1.hpp' file not found
     #include <boost/uuid/sha1.hpp>
              ^
    
    PR:		229569

---
+++ cad/kicad-devel/files/patch-3d-viewer_3d__cache_3d__cache.cpp
@@ -0,0 +1,21 @@
+After boostorg/uuid@33da3e2a5b87 (and boostorg/uuid@3d2f7758e9e1) build fails:
+
+3d-viewer/3d_cache/3d_cache.cpp:37:10: fatal error: 'boost/uuid/sha1.hpp' file not found
+#include <boost/uuid/sha1.hpp>
+         ^
+
+--- 3d-viewer/3d_cache/3d_cache.cpp.orig	2018-07-03 15:23:16 UTC
++++ 3d-viewer/3d_cache/3d_cache.cpp
+@@ -34,7 +34,12 @@
+ #include <wx/log.h>
+ #include <wx/stdpaths.h>
+ 
++#include <boost/version.hpp>
++#if (BOOST_VERSION >= 106600)
++#include <boost/uuid/detail/sha1.hpp>
++#else
+ #include <boost/uuid/sha1.hpp>
++#endif
+ 
+ #include <glm/glm.hpp>
+ #include <glm/ext.hpp>
