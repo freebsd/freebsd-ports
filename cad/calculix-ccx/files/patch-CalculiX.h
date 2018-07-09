ff783a1280688ebacb9133efc4da7c083f90355d
Thomas Zander <riggs@FreeBSD.org>
2018-07-09 21:05:27 +0000

    Update to upstream version 2.14
    Detailed maintainer log:
    - New version with many enhancements.
      See http://www.dhondt.de/new_calc.htm
    - Drop metis-edf: metid-edf will be deprecated and it's less
      confusing to just use metis4.
    
    PR:		228259
    Submitted by:	pfg (maintainer)

--- cad/calculix-ccx/files/patch-CalculiX.h
+++ cad/calculix-ccx/files/patch-CalculiX.h
@@ -1,6 +1,6 @@
---- CalculiX.h.orig	2017-10-08 20:06:28 UTC
+--- CalculiX.h.orig	2018-04-28 14:05:18 UTC
 +++ CalculiX.h
-@@ -15,12 +15,14 @@
+@@ -16,12 +16,14 @@
  /*     along with this program; if not, write to the Free Software       */
  /*     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.         */
  
@@ -17,7 +17,7 @@
  #define FORTRAN(A,B) A##_  B
  #elif ARCH == IRIX || ARCH == IRIX64
  #define FORTRAN(A,B) A##_##B
-@@ -28,7 +30,7 @@
+@@ -29,7 +31,7 @@
  #define FORTRAN(A,B) A##B
  #endif
  
