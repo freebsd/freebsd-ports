3e65d2ad369ec002a8ff15cd928d48dbbe44f141
mat <mat@FreeBSD.org>
2018-07-11 09:36:54 +0000

    Update BIND9 ports to 9.11.4, 9.12.2 and 9.13.2.
    Sponsored by:	Absolight

--- dns/bind911/files/patch-bin_named_include_named_globals.h
+++ dns/bind911/files/patch-bin_named_include_named_globals.h
@@ -1,8 +1,8 @@
 We reference the pid file as being run/named/pid everywere else.
 
---- bin/named/include/named/globals.h.orig	2018-01-04 05:28:11 UTC
+--- bin/named/include/named/globals.h.orig	2018-06-10 06:06:33 UTC
 +++ bin/named/include/named/globals.h
-@@ -135,7 +135,7 @@ EXTERN isc_boolean_t		ns_g_forcelock		IN
+@@ -138,7 +138,7 @@ EXTERN isc_boolean_t		ns_g_forcelock		IN
  #if NS_RUN_PID_DIR
  EXTERN const char *		ns_g_defaultpidfile 	INIT(NS_LOCALSTATEDIR
  							     "/run/named/"
