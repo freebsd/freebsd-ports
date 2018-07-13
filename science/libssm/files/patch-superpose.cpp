b14502d893591d52b37d061522f61f88f917b182
Yuri Victorovich <yuri@FreeBSD.org>
2018-07-13 04:58:15 +0000

    science/libssm: Fix build: put spaces between quotedstrings and defined values in C
    Reported by:	fallout

--- science/libssm/files/patch-superpose.cpp
+++ science/libssm/files/patch-superpose.cpp
@@ -1,6 +1,6 @@
 https://bugs.launchpad.net/ssm/+bug/1780908
 
---- superpose.cpp.orig	2018-07-10 06:15:41 UTC
+--- superpose.cpp.orig	2015-10-05 00:41:45 UTC
 +++ superpose.cpp
 @@ -114,7 +114,7 @@ void printInstructions ( char *argv0 )  
  #endif
@@ -11,3 +11,12 @@ https://bugs.launchpad.net/ssm/+bug/1780908
      "MMDB v.%i.%i.%i\n"
      "\n"
      " USAGE:\n"
+@@ -295,7 +295,7 @@ int             argNo,i,nStructures,rc;
+   }
+ 
+   printf ( "\n"
+-    " Superpose v."superpose_version" from "superpose_date" "
++    " Superpose v." superpose_version " from " superpose_date " "
+                                         "(based on SSM algorithm)\n"
+     " ---------------------------------------------------------\n\n"
+    );
