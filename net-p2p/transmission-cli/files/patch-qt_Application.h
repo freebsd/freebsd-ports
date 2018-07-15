3bda71c261bd41979696b485b5ee6398cdcbdf78
tcberner <tcberner@FreeBSD.org>
2018-07-15 17:01:05 +0000

    net-p2p/transmission* -- update and falvorize
    * update to 2.94
    * flavorize the Qt frontends
    * give maintainership to lebarondemerde@privacychain.ch
    
    PR:		226710
    Approved by:	portmgr (mat), crees (previous maintainer)
    Differential Revision:	https://reviews.freebsd.org/D14699

---
+++ net-p2p/transmission-cli/files/patch-qt_Application.h
@@ -0,0 +1,10 @@
+--- qt/Application.h.orig	2018-07-04 17:44:30 UTC
++++ qt/Application.h
+@@ -15,6 +15,7 @@
+ #include <QTimer>
+ #include <QTranslator>
+ 
++#include <time.h>
+ #include "FaviconCache.h"
+ 
+ class AddData;
