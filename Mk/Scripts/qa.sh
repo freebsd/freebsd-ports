42e89e30feb3d3d257a04b990fd9e6645d1ee231
mat <mat@FreeBSD.org>
2018-07-18 11:13:06 +0000

    Add a license qa check to tell porters when their ports will be mostly useless.
    Reviewed by:	antoine
    Sponsored by:	Absolight
    Differential Revision:	https://reviews.freebsd.org/D16103

--- Mk/Scripts/qa.sh
+++ Mk/Scripts/qa.sh
@@ -914,9 +914,34 @@ flavors()
 	return ${rc}
 }
 
+license()
+{
+	local autoaccept pkgmirror #distsell distmirror pkgsell
+
+	if [ -n "$LICENSE" ]; then
+		case "$LICENSE_PERMS" in
+			auto-accept) autoaccept=1 ;;
+			#dist-mirror) distmirror=1 ;;
+			#dist-sell)   distsell=1   ;;
+			pkg-mirror)  pkgmirror=1  ;;
+			#pkg-sell)    pkgsell=1    ;;
+		esac
+
+		if [ -z "$autoaccept" ]; then
+			warn "License is not auto-accepted, packages will not be built, ports depending on this one will be ignored."
+		fi
+		if [ -z "$pkgmirror" ]; then
+			warn "License does not allow package to be distributed, ports depending on this one will be ignored"
+		fi
+	fi
+
+	return 0
+}
+
 checks="shebang symlinks paths stripped desktopfileutils sharedmimeinfo"
 checks="$checks suidfiles libtool libperl prefixvar baselibs terminfo"
 checks="$checks proxydeps sonames perlcore no_arch gemdeps gemfiledeps flavors"
+checks="$checks license"
 
 ret=0
 cd ${STAGEDIR} || exit 1
