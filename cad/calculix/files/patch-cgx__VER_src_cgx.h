0d80ae46852255f7b7f5d7e5b3d37d2d671167c8
Steve Wills <swills@FreeBSD.org>
2018-07-18 16:42:31 +0000

    cad/calculix: update to version 2.14
    PR:		228260
    Submitted by:	pfg (maintainer)

--- cad/calculix/files/patch-cgx__VER_src_cgx.h
+++ cad/calculix/files/patch-cgx__VER_src_cgx.h
@@ -1,11 +1,11 @@
---- cgx_2.13/src/cgx.h.orig	2017-04-28 10:12:50 UTC
-+++ cgx_2.13/src/cgx.h
+--- cgx_2.14/src/cgx.h.orig	2017-04-28 10:12:50 UTC
++++ cgx_2.14/src/cgx.h
 @@ -89,8 +89,8 @@ rendering is done in the index-mode.
-     "/usr/local/CalculiX/ccx_2.13/doc/ccx/ccx.html",\
-     "/usr/local/CalculiX/cgx_2.13/doc/aflib/aflib.pdf"}
+     "/usr/local/CalculiX/ccx_2.14/doc/ccx/ccx.html",\
+     "/usr/local/CalculiX/cgx_2.14/doc/aflib/aflib.pdf"}
  #else
--  #define     HELPFILE        {"/usr/local/CalculiX/cgx_2.13/doc/cgx/cgx.html",\
--    "/usr/local/CalculiX/ccx_2.13/doc/ccx/ccx.html"}
+-  #define     HELPFILE        {"/usr/local/CalculiX/cgx_2.14/doc/cgx/cgx.html",\
+-    "/usr/local/CalculiX/ccx_2.14/doc/ccx/ccx.html"}
 +  #define     HELPFILE        {"/usr/local/share/doc/CalculiX/cgx/cgx.html",\
 +    "/usr/local/share/doc/CalculiX/ccx/ccx.html"}
  #endif
