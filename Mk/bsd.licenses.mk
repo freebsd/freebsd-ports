10b05e19370b3aee655ef29df8f33113210f4490
Mathieu Arnold <mat@FreeBSD.org>
2018-07-18 15:08:44 +0000

    Fix copy&paste error.
    Reported by:	koobs
    Sponsored by:	Absolight

--- Mk/bsd.licenses.mk
+++ Mk/bsd.licenses.mk
@@ -591,7 +591,7 @@ debug-license: check-license
 .if ${_LICENSE_PERMS:Mpkg-mirror}
 	@${ECHO_MSG} "===>  License allows mirroring pre-built packages"
 .else
-	@${ECHO_MSG} "===>  License does not allows selling pre-build packages"
+	@${ECHO_MSG} "===>  License does not allows mirroring pre-build packages"
 .endif
 .if ${_LICENSE_PERMS:Mpkg-sell}
 	@${ECHO_MSG} "===>  License allows selling pre-build packages"
