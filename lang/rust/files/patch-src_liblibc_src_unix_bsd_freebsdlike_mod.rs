80e30d61332368ec2c604a0d0ea2e5248d2726d4
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2018-07-19 20:57:09 +0000

    lang/rust: Update to 1.27.1
    Release notes:
    * https://blog.rust-lang.org/2018/06/21/Rust-1.27.html
    * https://blog.rust-lang.org/2018/07/10/Rust-1.27.1.html
    
    Rust is marked as broken on aarch64. The reason is the bootstrap
    compiler crashes currently. See PR 228892 which tracks the issue.
    
    A patch for aarch64 is still included. It fixes the sign for unqualified
    C char. This patch still needs to be upstream, but for that, the
    compiler needs to work again first.
    
    PR:		228892

---
+++ lang/rust/files/patch-src_liblibc_src_unix_bsd_freebsdlike_mod.rs
@@ -0,0 +1,18 @@
+--- src/liblibc/src/unix/bsd/freebsdlike/mod.rs.orig	2018-06-19 04:36:14 UTC
++++ src/liblibc/src/unix/bsd/freebsdlike/mod.rs
+@@ -1,4 +1,3 @@
+-pub type c_char = i8;
+ pub type dev_t = u32;
+ pub type mode_t = u16;
+ pub type pthread_attr_t = *mut ::c_void;
+@@ -84,7 +83,10 @@ s! {
+ 
+     pub struct stack_t {
+         // In FreeBSD 11 and later, ss_sp is actually a void*
++        #[cfg(not(target_arch = "aarch64"))]
+         pub ss_sp: *mut ::c_char,
++        #[cfg(target_arch = "aarch64")]
++        pub ss_sp: *mut ::c_void,
+         pub ss_size: ::size_t,
+         pub ss_flags: ::c_int,
+     }
