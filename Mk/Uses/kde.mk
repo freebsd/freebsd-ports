0c7ee9ff1b423687137217da2357a41e460e34cc
tcberner <tcberner@FreeBSD.org>
2018-07-19 20:45:03 +0000

    Update KDE Frameworks to 5.48.0
    Exp-run by:	antoine
    PR:		229769

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -70,7 +70,7 @@ KDE_PLASMA_VERSION?=		5.12.5
 KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
-KDE_FRAMEWORKS_VERSION?=	5.47.0
+KDE_FRAMEWORKS_VERSION?=	5.48.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 # Current KDE applications.
