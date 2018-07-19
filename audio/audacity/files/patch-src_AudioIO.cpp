c6b6cf046bc34951612a27463735d69ab99698d3
Tobias Kortkamp <tobik@FreeBSD.org>
2018-07-19 03:18:53 +0000

    audio/audacity: Unbundle portaudio and use audio/portaudio instead
    PR:		229853
    Submitted by:	tobik
    Approved by:	xxjack12xx@gmail.com (maintainer)

--- audio/audacity/files/patch-src_AudioIO.cpp
+++ audio/audacity/files/patch-src_AudioIO.cpp
@@ -1,6 +1,29 @@
---- src/AudioIO.cpp.orig	2018-02-22 14:12:03.835431000 -0800
-+++ src/AudioIO.cpp	2018-02-22 14:01:49.487127000 -0800
-@@ -985,7 +985,7 @@
+--- src/AudioIO.cpp.orig	2018-07-17 20:25:01 UTC
++++ src/AudioIO.cpp
+@@ -465,7 +465,6 @@ TimeTrack and AudioIOListener and whether the playback
+    #define ROUND(x) (int) ((x)+0.5)
+    //#include <string.h>
+    #include "../lib-src/portmidi/pm_common/portmidi.h"
+-   #include "../lib-src/portaudio-v19/src/common/pa_util.h"
+    #include "NoteTrack.h"
+ #endif
+ 
+@@ -979,13 +978,22 @@ struct AudioIO::ScrubQueue (private)
+ // return the system time as a double
+ static double streamStartTime = 0; // bias system time to small number
+ 
++// PaUtil_GetTime is an internal PortAudio function.  Unfortunately
++// it's used twice in AudioIO.cpp.  It's a simple function so just
++// provide the implementation here.
++static double PaUtil_GetTime(void) {
++   struct timespec tp;
++   clock_gettime(CLOCK_REALTIME, &tp);
++   return (double)(tp.tv_sec + tp.tv_nsec * 1e-9);
++}
++
+ static double SystemTime(bool usingAlsa)
+ {
+ #ifdef __WXGTK__
     if (usingAlsa) {
        struct timespec now;
        // CLOCK_MONOTONIC_RAW is unaffected by NTP or adj-time
