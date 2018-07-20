46ed0f3242e7ce6c186618e25c7772432a841275
Koichiro Iwao <meta@FreeBSD.org>
2018-07-20 08:17:05 +0000

    net/tigervnc: update 1.8.0 -> 1.9.0
    While here,
    * adjust USE_XORG
    * bring IGNORE line earlier (-devel port)
    * remove tridiavnc from CONFLICTS since it no longer exists
    * some cosmetic changes on Makefile
    
    Approved by:	pi (mentor)
    Relnotes:	https://github.com/TigerVNC/tigervnc/releases/tag/v1.9.0
    Differential Revision:	https://reviews.freebsd.org/D16340

--- net/tigervnc/files/patch-cmake_BuildPackages.cmake
+++ net/tigervnc/files/patch-cmake_BuildPackages.cmake
@@ -1,10 +1,10 @@
---- cmake/BuildPackages.cmake.orig	2017-05-16 13:53:28 UTC
+--- cmake/BuildPackages.cmake.orig	2017-04-19 13:27:31 UTC
 +++ cmake/BuildPackages.cmake
 @@ -86,5 +86,5 @@ endif() #UNIX
  # Common
  #
  
 -install(FILES ${CMAKE_SOURCE_DIR}/LICENCE.TXT DESTINATION ${DOC_DIR})
--install(FILES ${CMAKE_SOURCE_DIR}/README.txt DESTINATION ${DOC_DIR})
+-install(FILES ${CMAKE_SOURCE_DIR}/README.rst DESTINATION ${DOC_DIR})
 +install(FILES ${CMAKE_SOURCE_DIR}/LICENCE.TXT DESTINATION share/doc/tigervnc)
-+install(FILES ${CMAKE_SOURCE_DIR}/README.txt DESTINATION share/doc/tigervnc)
++install(FILES ${CMAKE_SOURCE_DIR}/README.rst DESTINATION share/doc/tigervnc)
