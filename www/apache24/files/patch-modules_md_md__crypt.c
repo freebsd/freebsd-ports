fbaa195ad50b2d7e1c6c3dcc7dd2cdac03410a7e
joneum <joneum@FreeBSD.org>
2018-07-20 14:26:43 +0000

    www/apache24: Update to 2.4.34
     - fixes vulns in mod_http2 and mod_md
     - include SSL_* options in alphabetic ordering
     - Remove unneeded SSL_CFLAGS and _LDFLAGS
     - Remove WITH_HTTP_PORT and WITH_SSL_PORT
     - Remove trailing whitespace
     - Fix build with HTTP2 but without SSL [1]
    
    PR:		229802, 227944 [1]
    With hat:	apache
    Approved by:	brnrd (apache)
    MFH:		2018Q3
    Security:	8b1a50ab-8a8e-11e8-add2-b499baebfeaf
    Differential Revision:	https://reviews.freebsd.org/D16294

--- www/apache24/files/patch-modules_md_md__crypt.c
+++ www/apache24/files/patch-modules_md_md__crypt.c
@@ -1,11 +1,19 @@
---- modules/md/md_crypt.c.orig	2018-01-30 10:20:50 UTC
+Upstream https://svn.apache.org/viewvc?view=revision&revision=1836096
+
+On the 2.4.x branch:
+
+merge r1836095 from trunk:
+* using the, hopefully correct, ever elusive libressl version numbering check
+  for the new openssl API calls, fixes PR 62548.
+
+--- modules/md/md_crypt.c.orig	2018-06-29 11:53:50 UTC
 +++ modules/md/md_crypt.c
-@@ -471,7 +471,7 @@ apr_status_t md_pkey_gen(md_pkey_t **ppk
-     }
- }
+@@ -52,7 +52,7 @@
  
--#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
-+#if OPENSSL_VERSION_NUMBER < 0x10100000L || (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
- 
- #ifndef NID_tlsfeature
- #define NID_tlsfeature          1020
+ #if defined(LIBRESSL_VERSION_NUMBER)
+ /* Missing from LibreSSL */
+-#define MD_USE_OPENSSL_PRE_1_1_API (LIBRESSL_VERSION_NUMBER < 0x2080000f)
++#define MD_USE_OPENSSL_PRE_1_1_API (LIBRESSL_VERSION_NUMBER < 0x2070000f)
+ #else /* defined(LIBRESSL_VERSION_NUMBER) */
+ #define MD_USE_OPENSSL_PRE_1_1_API (OPENSSL_VERSION_NUMBER < 0x10100000L)
+ #endif
