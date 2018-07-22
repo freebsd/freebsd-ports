824be7ba82a8fddae218c58e846874f5735bd0cf
bofh <bofh@FreeBSD.org>
2018-07-22 02:27:07 +0000

    emulators/qemu: Update version 2.9.0>=2.12.0
    - Pet portlint
    
    Relnotes:	https://wiki.qemu.org/ChangeLog/2.12

--- emulators/qemu/files/pcap-patch-net_clients.h
+++ emulators/qemu/files/pcap-patch-net_clients.h
@@ -1,13 +1,13 @@
---- net/clients.h.orig	2017-03-05 10:36:13.475005000 -0700
-+++ net/clients.h	2017-03-05 10:35:30.412539000 -0700
-@@ -61,4 +61,10 @@
- 
+--- net/clients.h.orig	2018-04-24 16:30:47 UTC
++++ net/clients.h
+@@ -61,4 +61,10 @@ int net_init_netmap(const Netdev *netdev, const char *
  int net_init_vhost_user(const Netdev *netdev, const char *name,
                          NetClientState *peer, Error **errp);
-+
+ 
 +#ifdef CONFIG_PCAP
 +int net_init_pcap(const Netdev *netdev, const char *name,
 +                    NetClientState *peer, Error **errp);
 +#endif
++
 +
  #endif /* QEMU_NET_CLIENTS_H */
