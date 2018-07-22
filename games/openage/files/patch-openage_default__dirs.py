6d27c847613677d8edf84e324bc867a24fce05fb
yuri <yuri@FreeBSD.org>
2018-07-22 17:11:40 +0000

    New port: games/openage: Free engine clone of the Genie Engine
    PR:		229787
    Submitted by:	Martin Filla <martinfilla@post.cz>

---
+++ games/openage/files/patch-openage_default__dirs.py
@@ -0,0 +1,27 @@
+--- openage/default_dirs.py.orig	2018-07-15 14:29:49 UTC
++++ openage/default_dirs.py
+@@ -33,6 +33,14 @@ LINUX_DIRS = {
+     "runtime_dir": ("XDG_RUNTIME_DIR", ("/run/user/$UID")),
+ }
+ 
++FREEBSD_DIRS = {
++    "config_home": ("XDG_CONFIG_HOME", ("{HOME}/.config", {"HOME"})),
++    "data_home": ("XDG_DATA_HOME", ("{HOME}/.local/share", {"HOME"})),
++    "data_dirs": ("XDG_DATA_DIRS", ("/usr/local/share/", {})),
++    "config_dirs": ("XDG_CONFIG_DIRS", ("/etc/xdg", {})),
++    "cache_home": ("XDG_CACHE_HOME", ("{HOME}/.cache", {"HOME"})),
++    "runtime_dir": ("XDG_RUNTIME_DIR", ("/run/user/$UID")),
++}
+ 
+ # Windows-specific paths
+ WINDOWS_DIRS = {
+@@ -60,6 +68,9 @@ def get_dir(which):
+     elif sys.platform.startswith("win32"):
+         platform_table = WINDOWS_DIRS
+ 
++    elif sys.platform.startswith("freebsd"):
++    	platform_table = FREEBSD_DIRS
++
+     else:
+         raise Exception("unsupported platform: '%s'" % sys.platform)
+ 
