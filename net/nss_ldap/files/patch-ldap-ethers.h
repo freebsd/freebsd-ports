1d66eabb6711be3ac82d2f9048ac8d2c9ac97757
des <des@FreeBSD.org>
2018-07-23 14:11:46 +0000

    Normalize patches.

--- net/nss_ldap/files/patch-ldap-ethers.h
+++ net/nss_ldap/files/patch-ldap-ethers.h
@@ -1,5 +1,5 @@
---- ldap-ethers.h.orig	2009-11-06 10:28:08.000000000 +0000
-+++ ldap-ethers.h	2009-12-23 17:02:06.000000000 +0000
+--- ldap-ethers.h.orig
++++ ldap-ethers.h
 @@ -32,7 +32,7 @@
  
  #ifndef HAVE_STRUCT_ETHER_ADDR
