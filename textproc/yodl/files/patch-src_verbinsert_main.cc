ff6580850d2f2fe5fd9a5570b8387edd805ef0c9
bofh <bofh@FreeBSD.org>
2018-07-24 13:24:09 +0000

    textproc/yodl: Fix patches

--- textproc/yodl/files/patch-src_verbinsert_main.cc
+++ textproc/yodl/files/patch-src_verbinsert_main.cc
@@ -1,34 +1,34 @@
---- src/verbinsert/verbinsert.cc.orig	2014-09-23 19:16:48 UTC
-+++ src/verbinsert/verbinsert.cc
+--- src/verbinsert/main.cc.orig	2018-07-22 13:07:15 UTC
++++ src/verbinsert/main.cc
 @@ -2,6 +2,7 @@
  #include <fstream>
  #include <iomanip>
  #include <string>
 +#include <cstdlib>
  #include <unistd.h>
+ #include <regex>
  
- using namespace std;
-@@ -37,19 +38,19 @@ int main(int argc, char **argv)
+@@ -43,19 +44,19 @@ int main(int argc, char **argv)
              continue;
  
              case 's':
 -                indent.append(stoul(optarg), ' ');
-+                indent.append(strtoul(optarg, NULL, 0), ' ');
++                indent.append(stoul(optarg, NULL, 0), ' ');
              continue;
  
              case 't':
 -                indent.insert(0, stoul(optarg), '\t');
-+                indent.insert(0, strtoul(optarg, NULL, 0), '\t');
++                indent.insert(0, stoul(optarg, NULL, 0), '\t');
              continue;
  
              case 'S':
 -                vindent.append(stoul(optarg), ' ');
-+                vindent.append(strtoul(optarg, NULL, 0), ' ');
++                vindent.append(stoul(optarg, NULL, 0), ' ');
              continue;
  
              case 'T':
 -                vindent.insert(0, stoul(optarg), '\t');
-+                vindent.insert(0, strtoul(optarg, NULL, 0), '\t');
++                vindent.insert(0, stoul(optarg, NULL, 0), '\t');
              continue;
  
              case 'N':
