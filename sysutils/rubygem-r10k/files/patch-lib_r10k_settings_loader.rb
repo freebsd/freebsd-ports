d6763248d5337fc4aa502b9f53cb5289a43e5cc1
romain <romain@FreeBSD.org>
2018-07-24 06:47:48 +0000

    Add a patch to search for configuration files under PREFIX, e.g.
    /usr/local/etc/r10k/r10k.yaml
    /usr/local/etc/r10k.yaml
    
    While here, pass maintainership to puppet@
    
    PR:		227341
    Submitted by:	romain
    Approved by:	swills (with ruby hat)

---
+++ sysutils/rubygem-r10k/files/patch-lib_r10k_settings_loader.rb
@@ -0,0 +1,24 @@
+--- lib/r10k/settings/loader.rb.orig	2018-04-07 11:13:36 UTC
++++ lib/r10k/settings/loader.rb
+@@ -8,8 +8,8 @@ module R10K
+     #
+     # r10k.yaml is checked for in the following locations:
+     #   - $PWD/r10k.yaml
+-    #   - /etc/puppetlabs/r10k/r10k.yaml
+-    #   - /etc/r10k.yaml
++    #   - %%PREFIX%%/etc/r10k/r10k.yaml
++    #   - %%PREFIX%%/etc/r10k.yaml
+     class Loader
+ 
+       def self.search(override = nil)
+@@ -21,8 +21,8 @@ module R10K
+       attr_reader :loadpath
+ 
+       CONFIG_FILE = 'r10k.yaml'
+-      DEFAULT_LOCATION = File.join('/etc/puppetlabs/r10k', CONFIG_FILE)
+-      OLD_DEFAULT_LOCATION = File.join('/etc', CONFIG_FILE)
++      DEFAULT_LOCATION = File.join('%%PREFIX%%/etc/r10k', CONFIG_FILE)
++      OLD_DEFAULT_LOCATION = File.join('%%PREFIX%%/etc', CONFIG_FILE)
+ 
+       def initialize
+         @loadpath = []
