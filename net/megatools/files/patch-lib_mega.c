bb98a543f936d40896b62f4abb5ff9c3129a23b1
Steve Wills <swills@FreeBSD.org>
2018-07-25 12:52:24 +0000

    net/megatools: update to 1.10.0
    PR:		230033
    Submitted by:	Max Kostikov <max@kostikov.co> (maintainer)

--- net/megatools/files/patch-lib_mega.c
+++ net/megatools/files/patch-lib_mega.c
@@ -1,33 +1,46 @@
-Fix build for FreeBSD 9.x base OpenSSL and LibreSSL.  Fix for LibreSSL is
-already in upstream repository, so this file can be removed once FreeBSD 9.x
-support is dropped AND the next version of megatools is released.
---- lib/mega.c.orig	2016-11-03 13:45:42 UTC
+--- lib/mega.c.orig	2018-07-22 21:41:55 UTC
 +++ lib/mega.c
-@@ -29,7 +29,9 @@
- #include <string.h>
- #include <time.h>
- #include <openssl/aes.h>
-+#if OPENSSL_VERSION_NUMBER >= 0x101000000L && !defined(LIBRESSL_VERSION_NUMBER)
- #include <openssl/modes.h>
-+#endif
- #include <openssl/bn.h>
- #include <openssl/rsa.h>
- #include <openssl/rand.h>
-@@ -803,7 +805,7 @@ static gboolean rsa_key_gen(rsa_key* k)
-     return FALSE;
-   }
+@@ -3172,6 +3172,7 @@ static void tman_worker_upload_chunk(struct transfer_c
+ 	GError *local_err = NULL;
+ 	gc_free gchar *url = NULL;
+ 	gc_string_free GString *response = NULL;
++	gc_free gchar* chksum = NULL;
  
--#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if OPENSSL_VERSION_NUMBER >= 0x101000000L && !defined(LIBRESSL_VERSION_NUMBER)
-   RSA_get0_key(key, &k->m, &k->e, &k->d);
-   RSA_get0_factors(key, &k->q, &k->p);
-   RSA_get0_crt_params(key, NULL, NULL, &k->u);
-@@ -2988,7 +2990,7 @@ struct _put_data
-   GByteArray* buffer;
- };
+ 	tman_debug("W[%d]: started for chunk %d\n", worker->index, c->index);
  
--#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if OPENSSL_VERSION_NUMBER >= 0x101000000L && !defined(LIBRESSL_VERSION_NUMBER)
- void AES_ctr128_encrypt(
-   const unsigned char *in, 
-   unsigned char *out,
+@@ -3217,7 +3218,7 @@ static void tman_worker_upload_chunk(struct transfer_c
+ 	}
+ 
+ 	// prepare URL including chunk offset
+-	gc_free gchar* chksum = upload_checksum(buf, c->size);
++	chksum = upload_checksum(buf, c->size);
+ 	url = g_strdup_printf("%s/%" G_GOFFSET_FORMAT "?c=%s", t->upload_url, c->offset, chksum);
+ 
+ 	// perform upload POST
+@@ -4636,6 +4637,26 @@ gboolean mega_node_get_path(struct mega_node *n, gchar
+ gboolean mega_node_is_container(struct mega_node *n)
+ {
+ 	return n && n->type != MEGA_NODE_FILE;
++}
++
++// }}}
++// {{{ mega_node_has_ancestor
++
++gboolean mega_node_has_ancestor(struct mega_node *n, struct mega_node *ancestor)
++{
++	g_return_val_if_fail(n != NULL, FALSE);
++	g_return_val_if_fail(ancestor != NULL, FALSE);
++
++	struct mega_node* it = n->parent;
++
++	while (it) {
++		if (it == ancestor)
++			return TRUE;
++
++		it = it->parent;
++	}
++
++	return FALSE;
+ }
+ 
+ // }}}
