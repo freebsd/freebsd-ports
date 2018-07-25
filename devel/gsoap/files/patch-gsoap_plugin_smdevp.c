335a5ae0924b34037b24e56778a5557f005bb150
bofh <bofh@FreeBSD.org>
2018-07-25 01:54:02 +0000

    devel/gsoap: Update version 2.8.66=>2.9.69
    Relnotes:	https://www.genivia.com/changelog.html

--- devel/gsoap/files/patch-gsoap_plugin_smdevp.c
+++ devel/gsoap/files/patch-gsoap_plugin_smdevp.c
@@ -1,6 +1,6 @@
---- gsoap/plugin/smdevp.c.orig	2018-04-08 14:22:44 UTC
+--- gsoap/plugin/smdevp.c.orig	2018-07-22 18:18:22 UTC
 +++ gsoap/plugin/smdevp.c
-@@ -479,7 +479,7 @@ soap_smd_init(struct soap *soap, struct soap_smd_data 
+@@ -480,7 +480,7 @@ soap_smd_init(struct soap *soap, struct soap_smd_data 
    /* allocate and init the OpenSSL HMAC or EVP_MD context */
    if ((alg & SOAP_SMD_ALGO) == SOAP_SMD_HMAC)
    {
@@ -9,7 +9,7 @@
      data->ctx = (void*)SOAP_MALLOC(soap, sizeof(HMAC_CTX));
      if (data->ctx)
        HMAC_CTX_init((HMAC_CTX*)data->ctx);
-@@ -489,7 +489,7 @@ soap_smd_init(struct soap *soap, struct soap_smd_data 
+@@ -490,7 +490,7 @@ soap_smd_init(struct soap *soap, struct soap_smd_data 
    }
    else
    {
@@ -18,16 +18,25 @@
      data->ctx = (void*)SOAP_MALLOC(soap, sizeof(EVP_MD_CTX));
      if (data->ctx)
        EVP_MD_CTX_init((EVP_MD_CTX*)data->ctx);
-@@ -642,7 +642,7 @@ soap_smd_final(struct soap *soap, struct soap_smd_data
-       *len = (int)n;
-   }
-   /* cleanup */
+@@ -510,7 +510,7 @@ soap_smd_init(struct soap *soap, struct soap_smd_data 
+     case SOAP_SMD_SHA1:
+       type = EVP_sha1();
+       break;
+-#if (OPENSSL_VERSION_NUMBER >= 0x0090800fL)
++#if (OPENSSL_VERSION_NUMBER >= 0x0090800fL || defined(LIBRESSL_VERSION_NUMBER))
+     case SOAP_SMD_SHA224:
+       type = EVP_sha224();
+       break;
+@@ -662,7 +662,7 @@ soap_smd_cleanup(struct soap *soap, struct soap_smd_da
+   (void)soap;
+   if (data->ctx)
+   {
 -#if (OPENSSL_VERSION_NUMBER < 0x10100000L)
 +#if (OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER))
-   if ((data->alg & SOAP_SMD_ALGO) == SOAP_SMD_HMAC)
-     HMAC_CTX_cleanup((HMAC_CTX*)data->ctx);
-   else
-@@ -687,7 +687,7 @@ soap_smd_check(struct soap *soap, struct soap_smd_data
+     if ((data->alg & SOAP_SMD_ALGO) == SOAP_SMD_HMAC)
+       HMAC_CTX_cleanup((HMAC_CTX*)data->ctx);
+     else
+@@ -706,7 +706,7 @@ soap_smd_check(struct soap *soap, struct soap_smd_data
      }
      if (data->ctx)
      {
