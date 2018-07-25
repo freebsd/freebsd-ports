1e6b2d8fd7c2cda22df01ebcbc712f9826b12526
fluffy <fluffy@FreeBSD.org>
2018-07-25 03:43:54 +0000

    - Update Battle of Wesnoth to 1.14.3 release
    PR:		228984 (based on)
    Submitted by:	Maik Fischer

---
+++ games/wesnoth/files/patch-src_replay.hpp
@@ -0,0 +1,10 @@
+--- src/replay.hpp.orig	2018-06-13 12:34:27 UTC
++++ src/replay.hpp
+@@ -21,6 +21,7 @@
+ 
+ #include "map/location.hpp"
+ 
++#include <time.h>
+ #include <deque>
+ #include <iterator>
+ #include <map>
