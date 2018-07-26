41119a60bcb6f030f1c4b49f070704c55386d064
mat <mat@FreeBSD.org>
2018-07-26 11:09:46 +0000

    Extract PLIST_SUB substitutions into a sed script.
    This fixes PLIST_SUB being too big and PLIST_SUB_SED getting bigger than
    _POSIX2_LINE_MAX.
    
    PR:		222355
    Reported by:	asomers
    Reviewed by:	asomers, mfechner
    Sponsored by:	Absolight
    Differential Revision:	 https://reviews.freebsd.org/D14014

--- Mk/Scripts/check_leftovers.sh
+++ Mk/Scripts/check_leftovers.sh
@@ -46,7 +46,9 @@ if [ -z "${CCACHE_DIR}" ]; then
 	CCACHE_DIR=$(make -C ${portdir} -VCCACHE_DIR)
 fi
 homedirs=$(awk -F: -v users="$(make -C ${portdir} -V USERS|sed -e 's, ,|,g;/^$/d;s,^,^(,;s,$,)$,')" 'users && $1 ~ users {print $9}' ${PORTSDIR}/UIDs|sort -u|sed -e "s|/usr/local|${PREFIX}|"|tr "\n" " ")
-plistsub_sed=$(make -C ${portdir} -VPLIST_SUB_SED | /bin/sh ${PORTSDIR}/Mk/Scripts/plist_sub_sed_sort.sh)
+plistsub_sed=$(mktemp -t plistsub_sed)
+trap "rm -f ${plistsub_sed}" EXIT 1
+make -C ${portdir} -VPLIST_SUB_SED | /bin/sh ${PORTSDIR}/Mk/Scripts/plist_sub_sed_sort.sh ${plistsub_sed}
 tmpplist=$(make -C ${portdir} -VTMPPLIST)
 
 while read -r modtype path extra; do
@@ -69,7 +71,7 @@ while read -r modtype path extra; do
 	esac
 
 	ignore_path=0
-	sub_path=$(echo "$path" | sed -e "s|^${PREFIX}/||" -e "${plistsub_sed}")
+	sub_path=$(echo "$path" | sed -e "s|^${PREFIX}/||" -f "${plistsub_sed}")
 	orig_sub_path="${sub_path}"
 	# If this is a directory, use @dir in output
 	is_dir=0
