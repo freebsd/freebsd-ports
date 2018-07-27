35c8be355a5524c80956318adb9642072eab3d62
netchild <netchild@FreeBSD.org>
2018-07-27 18:34:37 +0000

    Improve zoneminder ports:  - add patch to make it compile with a recent clang (from -current)  - add missing fcgiwrap config option to the FreeBSD readme  - sync pkg-descr and highlight difference between the ports in pkg-descr and COMMENT
    Approved by:	maintainer

--- multimedia/zoneminder-h264/files/patch-src_zmf.cpp
+++ multimedia/zoneminder-h264/files/patch-src_zmf.cpp
@@ -1,5 +1,14 @@
---- src/zmf.cpp.orig	2016-11-03 20:26:18 UTC
+--- src/zmf.cpp.orig	2017-05-09 14:54:48 UTC
 +++ src/zmf.cpp
+@@ -92,7 +92,7 @@ int OpenSocket( int monitor_id )
+   strncpy( addr.sun_path, sock_path, sizeof(addr.sun_path) );
+   addr.sun_family = AF_UNIX;
+ 
+-  if ( bind( sd, (struct sockaddr *)&addr, strlen(addr.sun_path)+sizeof(addr.sun_family)) < 0 )
++  if ( ::bind( sd, (struct sockaddr *)&addr, strlen(addr.sun_path)+sizeof(addr.sun_family)) < 0 )
+   {
+     Error( "Can't bind: %s", strerror(errno) );
+     exit( -1 );
 @@ -331,7 +331,7 @@ int main( int argc, char *argv[] )
      Debug( 1, "Got image, writing to %s", path );
  
