021a97a2a6b919cfe4440be3745e921ea65bfb59
Jason Helfman <jgh@FreeBSD.org>
2018-07-27 15:56:57 +0000

    - bring in missing dependencies, bump portrevision - modernize options helper usage - while here pet portlint

--- deskutils/xpad3/files/patch-src-xpad-pad.c
+++ deskutils/xpad3/files/patch-src-xpad-pad.c
@@ -1,6 +1,6 @@
---- src/xpad-pad.c.orig	2010-06-05 15:18:08.000000000 +0200
-+++ src/xpad-pad.c	2010-06-05 15:18:28.000000000 +0200
-@@ -274,7 +274,7 @@
+--- src/xpad-pad.c.orig	2009-01-11 16:27:39 UTC
++++ src/xpad-pad.c
+@@ -274,7 +274,7 @@ xpad_pad_init (XpadPad *pad)
  		"gravity", GDK_GRAVITY_STATIC, /* static gravity makes saving pad x,y work */
  		"skip-pager-hint", !xpad_settings_get_has_decorations (xpad_settings ()),
  		"skip-taskbar-hint", !xpad_settings_get_has_decorations (xpad_settings ()),
