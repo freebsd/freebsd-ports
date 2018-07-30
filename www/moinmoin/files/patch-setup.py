98c348e6e79151cd769ab872289ece4a8f2eae30
Kubilay Kocak <koobs@FreeBSD.org>
2018-07-30 00:05:20 +0000

    www/moinmoin: Add recaptcha v2 and email verification support
    Add patches to include:
    
       - reCAPTCHA v2 support [1]
       - Email verification for new account creation [2]
    
    Both patches are OPTION'al, both OPTIONS are enabled by default. Both features
    require additional configuration (within moinmoin) to enable them within wiki
    instances.
    
    While I'm here:
    
      - Use TLS for MASTER_SITES and pkg-descr WWW: URLs
      - Update COMMENT to match setup.py description
      - Enable NO_ARCH
      - Sort sections
    
    PR:			227394 [1]
    Submitted by:		<daz hackerspace pl> [1]
    Reviewed by:		0mp
    Tested by:		0mp (upgrade process)
    Approved by:		koobs (python, maintainer)
    Obtained from:		clusteradm (sbruno, custom patch to port) [2]
    Differential Revision:	D16315

--- www/moinmoin/files/patch-setup.py
+++ www/moinmoin/files/patch-setup.py
@@ -1,6 +1,6 @@
---- ./setup.py.orig	2010-04-30 10:09:50.411058590 -0400
-+++ ./setup.py	2010-04-30 10:10:32.801862294 -0400
-@@ -26,6 +26,7 @@
+--- setup.py.orig	2016-10-31 20:44:02 UTC
++++ setup.py
+@@ -26,6 +26,7 @@ def isbad(name):
      return (name.startswith('.') or
              name.startswith('#') or
              name.endswith('.pickle') or
