0edb553ded289ffdb19ee304fbb6f314a66a3dea
Jan Beich <jbeich@FreeBSD.org>
2018-08-01 22:32:04 +0000

    print/harfbuzz: update to 1.8.5
    Changes:	https://github.com/harfbuzz/harfbuzz/blob/1.8.5/NEWS
    ABI:		https://abi-laboratory.pro/tracker/timeline/harfbuzz/

--- print/harfbuzz/files/patch-src_hb-atomic-private.hh
+++ print/harfbuzz/files/patch-src_hb-atomic-private.hh
@@ -1,16 +1,15 @@
---- src/hb-atomic-private.hh.orig	2015-04-10 02:16:57 UTC
+--- src/hb-atomic-private.hh.orig	2018-08-01 21:15:00 UTC
 +++ src/hb-atomic-private.hh
-@@ -103,8 +103,12 @@ typedef int hb_atomic_int_impl_t;
- #define hb_atomic_int_impl_add(AI, V)		__sync_fetch_and_add (&(AI), (V))
+@@ -114,7 +114,12 @@ typedef LONG hb_atomic_int_impl_t;
+ typedef int hb_atomic_int_impl_t;
+ #define hb_atomic_int_impl_add(AI, V)		__sync_fetch_and_add ((AI), (V))
  
- #define hb_atomic_ptr_impl_get(P)		(void *) (__sync_synchronize (), *(P))
 +#if defined(__FreeBSD__) && defined(__mips__)
 +#include <machine/atomic.h>
 +#define hb_atomic_ptr_impl_cmpexch(P,O,N)	atomic_cmpset_rel_32 ((uint32_t *)(P), (uint32_t)(O), (uint32_t)(N))
 +#else
  #define hb_atomic_ptr_impl_cmpexch(P,O,N)	__sync_bool_compare_and_swap ((P), (O), (N))
--
 +#endif
  
- #elif !defined(HB_NO_MT) && defined(HAVE_SOLARIS_ATOMIC_OPS)
  
+ #elif !defined(HB_NO_MT) && defined(HAVE_SOLARIS_ATOMIC_OPS)
