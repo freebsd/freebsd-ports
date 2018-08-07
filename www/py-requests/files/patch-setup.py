aaaa0f6abfb33cc2af614ba25241257c589c9d74
Kubilay Kocak <koobs@FreeBSD.org>
2018-08-07 09:11:28 +0000

    devel/py-requests: Bump pinned idna dependency version
    py-idna was updated to 2.7 in ports r476560. py-requests had its idna dependency
    pinned to <2.7.
    
    That broke all py-requests dependents.
    
    This change backports part of an upstream commit [1] that bumped the dependency.
    
    Whisper of a dream: It would be really cool if *_DEPENDS could declare multiple
    versions in its scheme. We'd pick up these issues much earlier, and be able to
    declare dependencies more accurately.</pony>
    
    [1] https://github.com/requests/requests/commit/991e8b76b7a9d21f698b24fa0058d3d5968721bc
    
    PR:		230430
    Reported by:	<Trond Endrestol ximalas info>

--- www/py-requests/files/patch-setup.py
+++ www/py-requests/files/patch-setup.py
@@ -1,8 +1,16 @@
-# pytest-cov and its dependencies are not compulsory
+# 1: Backport bumping idna max version
+# https://github.com/requests/requests/commit/991e8b76b7a9d21f698b24fa0058d3d5968721bc
+# 2: pytest-cov and its dependencies are not compulsory
 
---- setup.py.orig	2018-02-24 01:49:05 UTC
+--- setup.py.orig	2017-08-15 13:19:39 UTC
 +++ setup.py
-@@ -48,7 +48,7 @@ requires = [
+@@ -43,12 +43,12 @@ packages = ['requests']
+ 
+ requires = [
+     'chardet>=3.0.2,<3.1.0',
+-    'idna>=2.5,<2.7',
++    'idna>=2.5,<2.8',
+     'urllib3>=1.21.1,<1.23',
      'certifi>=2017.4.17'
  
  ]
