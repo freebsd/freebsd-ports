f081518fa6d8ecd6445a8e85e960d05fcfae2dab
ale <ale@FreeBSD.org>
2018-08-09 08:01:28 +0000

    Update to 0.4.24 release and fix build with newer boost lib.

--- lang/solidity/files/patch-libdevcore_JSON.cpp
+++ lang/solidity/files/patch-libdevcore_JSON.cpp
@@ -1,4 +1,4 @@
---- libdevcore/JSON.cpp.orig	2018-04-19 21:36:04 UTC
+--- libdevcore/JSON.cpp.orig	2018-05-16 14:30:44 UTC
 +++ libdevcore/JSON.cpp
 @@ -27,10 +27,12 @@
  
@@ -6,8 +6,8 @@
  
 +/*
  static_assert(
- 	(JSONCPP_VERSION_MAJOR == 1) && (JSONCPP_VERSION_MINOR == 7) && (JSONCPP_VERSION_PATCH == 7),
- 	"Unexpected jsoncpp version: " JSONCPP_VERSION_STRING ". Expecting 1.7.7."
+ 	(JSONCPP_VERSION_MAJOR == 1) && (JSONCPP_VERSION_MINOR == 8) && (JSONCPP_VERSION_PATCH == 4),
+ 	"Unexpected jsoncpp version: " JSONCPP_VERSION_STRING ". Expecting 1.8.4."
  );
 +*/
  
