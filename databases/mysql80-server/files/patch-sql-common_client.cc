4858c5d94606f621d6fa80a1c3da83f8f0a2db0d
Mahdi Mokhtari <mmokhi@FreeBSD.org>
2018-08-14 14:44:29 +0000

    MFH: r476016 r476944 r477036 r477063 r477105
    databases/mysql80-client: Unbreak patching
    
    Broken after patch(1) changes in r326084
    
    PR:	228788
    Submitted by:	curtis@ipv6.occnc.com
    Approved by:	tcberner (mentor) mmokhi (maintainer)
    Differential Revision:	https://reviews.freebsd.org/D16532
    
    databases/mysql80-{client, server}: Update ports to 8.0.12
    This update includes Oracle Critical Patch Advisory published on July 2018
    More info:
    http://www.oracle.com/technetwork/security-advisory/cpujul2018-4258247.html#AppendixMSQL
    Also:
    Delete upstreamed hunks of patches.
    Change local patches regarding upstream changes.
    
    PR:		229861
    Reported by:	Markus Kohlmeyer <rootservice@gmail.com>
    Sponsored by:	The FreeBSD Foundation
    
    databases/mysql80-server: Fix build on default Clang of 10.4-base
    Add sse4.2 flag to CXXFLAGS on 10.4 case.
    The build-script needs it when using intrin lib on crc32
    
    Sponsored by:	The FreeBSD Foundation
    
    databases/mysql80-server: Use c++14-lang instead of `msse-4.2 in CXXFLAG`
    As suggested via replies to r477036 using `-msse-4.2` may break build on other
    platforms.
    
    Reported by:	linimon danfe jbeich
    Sponsored by:	The FreeBSD Foundation
    
    databases/mysql80-server: Delete no-longer-needed patch for sse4.2
    
    Reported by:	jbeich
    Sponsored by:	The FreeBSD Foundation
    
    Approved by:	ports-secteam (feld)

--- databases/mysql80-server/files/patch-sql-common_client.cc
+++ databases/mysql80-server/files/patch-sql-common_client.cc
@@ -1,21 +1,5 @@
 --- sql-common/client.cc.orig	2018-04-08 06:44:49 UTC
 +++ sql-common/client.cc
-@@ -2649,11 +2649,11 @@ static int ssl_verify_server_cert(Vio *v
-     goto error;
-   }
- 
--#if OPENSSL_VERSION_NUMBER < 0x10100000L
--  cn = (char *)ASN1_STRING_data(cn_asn1);
--#else  /* OPENSSL_VERSION_NUMBER < 0x10100000L */
-+#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
-   cn = (char *)ASN1_STRING_get0_data(cn_asn1);
--#endif /* OPENSSL_VERSION_NUMBER < 0x10100000L */
-+#else  /* OPENSSL_VERSION_NUMBER >= 0x10100000L */
-+  cn = (char *)ASN1_STRING_data(cn_asn1);
-+#endif /* OPENSSL_VERSION_NUMBER >= 0x10100000L */
- 
-   // There should not be any NULL embedded in the CN
-   if ((size_t)ASN1_STRING_length(cn_asn1) != strlen(cn)) {
 @@ -5596,7 +5596,7 @@ int STDCALL mysql_options(MYSQL *mysql, 
  #endif
        break;
