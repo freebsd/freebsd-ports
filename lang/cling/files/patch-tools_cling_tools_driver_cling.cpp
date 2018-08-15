2c43196d82398eefe566285e737a4841a0d48740
mmokhi <mmokhi@FreeBSD.org>
2018-08-15 13:16:31 +0000

    lang/cling: Update port to new release 0.5-2018-08-13 Also, Fix the problem of some non-header files as headers (Thanks to yuri@) Fix Jupyter integration
    PR:		230637
    Reported by:	yuri
    Sponsored by:	The FreeBSD Foundation

--- lang/cling/files/patch-tools_cling_tools_driver_cling.cpp
+++ lang/cling/files/patch-tools_cling_tools_driver_cling.cpp
@@ -1,11 +1,11 @@
---- tools/cling/tools/driver/cling.cpp.orig	2016-12-29 13:54:45 UTC
+--- tools/cling/tools/driver/cling.cpp.orig	2018-08-14 07:04:11 UTC
 +++ tools/cling/tools/driver/cling.cpp
-@@ -51,7 +51,7 @@ int main( int argc, char **argv ) {
+@@ -76,7 +76,7 @@ int main( int argc, char **argv ) {
  #endif
  
    // Set up the interpreter
--  cling::Interpreter interp(argc, argv);
-+  cling::Interpreter interp(argc, argv, "%%LLVM_PREFIX%%/");
-   if (interp.getOptions().Help) {
-     return 0;
-   }
+-  cling::Interpreter Interp(argc, argv);
++  cling::Interpreter Interp(argc, argv, "%%LLVM_PREFIX%%/");
+   const cling::InvocationOptions& Opts = Interp.getOptions();
+ 
+   if (!Interp.isValid()) {
