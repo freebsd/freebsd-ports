4ef4d386158ef37c272a2e06626ab27b4ea76fa4
jbeich <jbeich@FreeBSD.org>
2018-08-15 18:00:23 +0000

    devel/android-tools-adb: adjust for USB dispatch on Windows after r477245
    See https://android.googlesource.com/platform/system/core/+/9425996b4521%5E!/

--- devel/android-tools-adb/files/patch-adb_transport__usb.cpp
+++ devel/android-tools-adb/files/patch-adb_transport__usb.cpp
@@ -1,11 +1,11 @@
---- adb/transport_usb.cpp.orig	2017-06-20 10:50:27 UTC
+--- adb/transport_usb.cpp.orig	2018-06-21 00:58:42 UTC
 +++ adb/transport_usb.cpp
-@@ -191,6 +191,8 @@ int is_adb_interface(int usb_class, int usb_subclass, 
+@@ -186,6 +186,8 @@ int is_adb_interface(int usb_class, int usb_subclass, 
  bool should_use_libusb() {
- #if defined(_WIN32) || !ADB_HOST
+ #if !ADB_HOST
      return false;
-+#elif !defined(__linux__) && !defined(__APPLE__)
++#elif !defined(__linux__) && !defined(__APPLE__) && !defined(_WIN32)
 +    return true;
  #else
-     static bool disable = getenv("ADB_LIBUSB") && strcmp(getenv("ADB_LIBUSB"), "0") == 0;
-     return !disable;
+     static bool enable = getenv("ADB_LIBUSB") && strcmp(getenv("ADB_LIBUSB"), "1") == 0;
+     return enable;
