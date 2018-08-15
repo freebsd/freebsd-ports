44a5df37ac8578ca2d46bfe88e98c7050f50d6ea
cpm <cpm@FreeBSD.org>
2018-08-15 22:54:53 +0000

    www/chromium: Update to 68.0.3440.106
    - Update amount of free disk space required to build chromium
    - Implement GPU access set up for FreeBSD [1]
    - Remove the incorrect sugestion in pkg-message and remove the fix-hanging-tabs.sh script because the bug of hanging tabs has been fixed in r337328 improving the chromium stability. Thanks to markj@ [2]
    
    PR:		230450 [1], 212812 [2]
    Reported by:	Oleh Hushchenkov <gor@clogic.com.ua>
    MFH:		2018Q3

--- www/chromium/files/patch-gpu_config_gpu__info__collector__freebsd.cc
+++ www/chromium/files/patch-gpu_config_gpu__info__collector__freebsd.cc
@@ -1,22 +1,31 @@
---- gpu/config/gpu_info_collector_freebsd.cc.orig	2018-07-20 13:47:10.021258000 +0200
-+++ gpu/config/gpu_info_collector_freebsd.cc	2018-07-20 15:44:42.380444000 +0200
-@@ -0,0 +1,20 @@
+--- gpu/config/gpu_info_collector_freebsd.cc.orig	2018-08-13 21:50:38.978048000 +0200
++++ gpu/config/gpu_info_collector_freebsd.cc	2018-08-13 21:54:59.868827000 +0200
+@@ -0,0 +1,29 @@
 +// Copyright 2017 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
 +
++#include "base/trace_event/trace_event.h"
 +#include "gpu/config/gpu_info_collector.h"
++#include "third_party/angle/src/gpu_info_util/SystemInfo.h"
 +
 +namespace gpu {
 +
 +bool CollectContextGraphicsInfo(GPUInfo* gpu_info) {
-+  NOTIMPLEMENTED();
-+  return false;
++  DCHECK(gpu_info);
++
++  TRACE_EVENT0("gpu", "gpu_info_collector::CollectGraphicsInfo");
++
++  return CollectGraphicsInfoGL(gpu_info);
 +}
 +
 +bool CollectBasicGraphicsInfo(GPUInfo* gpu_info) {
-+  NOTIMPLEMENTED();
-+  return false;
++  DCHECK(gpu_info);
++
++  angle::SystemInfo system_info;
++  bool success = angle::GetSystemInfo(&system_info);
++  FillGPUInfoFromSystemInfo(gpu_info, &system_info);
++  return success;
 +}
 +
 +
