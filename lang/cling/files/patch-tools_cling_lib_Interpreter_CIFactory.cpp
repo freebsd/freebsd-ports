89a66e889669d60101c39b928ac06c5976194ff9
Mahdi Mokhtari <mmokhi@FreeBSD.org>
2018-08-15 13:16:31 +0000

    lang/cling: Update port to new release 0.5-2018-08-13 Also, Fix the problem of some non-header files as headers (Thanks to yuri@) Fix Jupyter integration
    PR:		230637
    Reported by:	yuri
    Sponsored by:	The FreeBSD Foundation

--- lang/cling/files/patch-tools_cling_lib_Interpreter_CIFactory.cpp
+++ lang/cling/files/patch-tools_cling_lib_Interpreter_CIFactory.cpp
@@ -1,13 +1,11 @@
---- tools/cling/lib/Interpreter/CIFactory.cpp.orig	2016-12-29 13:54:44 UTC
+--- tools/cling/lib/Interpreter/CIFactory.cpp.orig	2018-08-14 07:05:28 UTC
 +++ tools/cling/lib/Interpreter/CIFactory.cpp
-@@ -307,8 +307,8 @@ namespace {
-           // Note: On FreeBSD it uses getprogpath().
-           // Note: Otherwise it uses dladdr().
-           //
--          resourcePath
--            = CompilerInvocation::GetResourcesPath("cling",
-+          resourcePath = CompilerInvocation::GetResourcesPath(
-+                                        "%%LLVM_PREFIX%%/bin/cling",
-                                             (void*)intptr_t(GetExecutablePath));
-         } else {
-           llvm::SmallString<512> tmp(llvmdir);
+@@ -187,7 +187,7 @@ namespace {
+       // Note: Otherwise it uses dladdr().
+       //
+       return CompilerInvocation::GetResourcesPath(
+-          "cling", (void*)intptr_t(GetExecutablePath));
++          "%%LLVM_PREFIX%%/bin/cling", (void*)intptr_t(GetExecutablePath));
+     } else {
+       std::string resourcePath;
+       llvm::SmallString<512> tmp(llvmdir);
