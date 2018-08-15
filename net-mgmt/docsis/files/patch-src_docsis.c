308c03e75c535742187944c6f32ee33d349c79ff
Emanuel Haupt <ehaupt@FreeBSD.org>
2018-08-15 12:49:38 +0000

    - Update to 0.9.8 - Move from SF to GitHub - Update WWW - Pacify portlint - Unbreak on amd64, "Philippe Maechler" <pmaechler-ml@glattnet.ch> notified us   via ports@ that the produced config files work fine

--- net-mgmt/docsis/files/patch-src_docsis.c
+++ net-mgmt/docsis/files/patch-src_docsis.c
@@ -1,6 +1,6 @@
---- src/docsis.c.orig	Mon Aug 15 14:19:04 2005
-+++ src/docsis.c	Sun Jun 10 18:14:07 2007
-@@ -20,10 +20,6 @@
+--- src/docsis.c.orig	2015-10-18 20:28:25 UTC
++++ src/docsis.c
+@@ -21,10 +21,6 @@
   *  DOCSIS is a registered trademark of Cablelabs, http://www.cablelabs.com
   */
  
@@ -11,11 +11,10 @@
  #include <errno.h>
  #include <string.h>
  #include <sys/types.h>
-@@ -35,6 +31,16 @@
- #include <net-snmp/config_api.h>
+@@ -37,6 +33,16 @@
  #include <net-snmp/output_api.h>
  #include <net-snmp/mib_api.h>
-+
+ 
 +#undef PACKAGE_BUGREPORT
 +#undef PACKAGE_NAME
 +#undef PACKAGE_STRING
@@ -25,6 +24,7 @@
 +#ifdef HAVE_CONFIG_H
 +#include "config.h"
 +#endif /* HAVE_CONFIG_H  */
- 
++
  #include "docsis.h"
+ #include "docsis_globals.h"
  #include "docsis_symtable.h"
--- net-mgmt/docsis/files/patch-src_docsis__snmp.h
+++ net-mgmt/docsis/files/patch-src_docsis__snmp.h
@@ -1,6 +1,6 @@
---- src/docsis_snmp.h.orig	Wed Mar 17 08:01:37 2004
-+++ src/docsis_snmp.h	Sun Jun 10 18:27:16 2007
-@@ -24,11 +24,19 @@
+--- src/docsis_snmp.h.orig	2015-10-18 20:28:25 UTC
++++ src/docsis_snmp.h
+@@ -25,12 +25,20 @@
  #ifndef _DOCSIS_SNMP_H
  #define _DOCSIS_SNMP_H
  
@@ -11,12 +11,13 @@
  #include <net-snmp/config_api.h>
  #include <net-snmp/output_api.h>
  #include <net-snmp/mib_api.h>
-+
+ 
 +#undef PACKAGE_BUGREPORT
 +#undef PACKAGE_NAME
 +#undef PACKAGE_STRING
 +#undef PACKAGE_TARNAME
 +#undef PACKAGE_VERSION
- 
++
  #include "docsis.h"
  
+ unsigned int encode_vbind ( char *oid_string, char oid_asntype, union t_val *value,
