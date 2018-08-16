8ae626d827aa9405664872621e6a709f17b36091
Jan Beich <jbeich@FreeBSD.org>
2018-08-16 16:53:17 +0000

    devel/lua-luarocks: clean up cfg patch
    - Bitrig is dead, changes subsumed by OpenBSD (Clang/libc++, aarch64 support)
    - Overriding GNU make didn't work on BSDs and was rarely useful
    - Annotate changes in the patch to avoid losing them on update

--- devel/lua-luarocks/files/patch-src_luarocks_cfg.lua
+++ devel/lua-luarocks/files/patch-src_luarocks_cfg.lua
@@ -1,15 +1,16 @@
---- src/luarocks/cfg.lua.orig	2015-12-29 15:50:11 UTC
+- Add DragonFly support
+- Respect CC/CFLAGS/LD/LDFLAGS
+- Prefer system compiler over GCC on BSDs/Solaris
+- OpenBSD uses Clang by default, so don't pass GCC RPATH
+
+--- src/luarocks/cfg.lua.orig	2018-03-13 00:08:59 UTC
 +++ src/luarocks/cfg.lua
-@@ -88,7 +88,15 @@ elseif proc:match("Power Macintosh") the
+@@ -87,7 +87,11 @@ elseif proc:match("Power Macintosh") then
     cfg.target_cpu = proc
  end
  
 -if system == "FreeBSD" then
-+if system == "Bitrig" then
-+   cfg.platforms.unix = true
-+   cfg.platforms.bitrig = true
-+   cfg.platforms.bsd = true
-+elseif system == "DragonFly" then
++if system == "DragonFly" then
 +   cfg.platforms.unix = true
 +   cfg.platforms.dragonfly = true
 +   cfg.platforms.bsd = true
@@ -17,7 +18,7 @@
     cfg.platforms.unix = true
     cfg.platforms.freebsd = true
     cfg.platforms.bsd = true
-@@ -477,16 +485,18 @@ if cfg.platforms.unix then
+@@ -517,16 +521,17 @@ if cfg.platforms.unix then
     defaults.lib_extension = "so"
     defaults.external_lib_extension = "so"
     defaults.obj_extension = "o"
@@ -31,7 +32,6 @@
     defaults.cmake_generator = "Unix Makefiles"
 -   defaults.variables.CC = "gcc"
 -   defaults.variables.LD = "gcc"
-+   defaults.variables.MAKE = os.getenv("MAKE") or defaults.variables.MAKE
 +   -- Assume "cc" is a modern GCC-compatible compiler
 +   defaults.variables.CC = os.getenv("CC") or defaults.variables.CC
 +   defaults.variables.LD = os.getenv("CC") or defaults.variables.CC
@@ -41,14 +41,10 @@
     defaults.external_deps_patterns = {
        bin = { "?" },
        lib = { "lib?.a", "lib?.so", "lib?.so.*" },
-@@ -548,11 +558,17 @@ if cfg.platforms.linux then
+@@ -607,15 +612,18 @@ if cfg.platforms.linux then
     defaults.arch = "linux-"..cfg.target_cpu
  end
  
-+if cfg.platforms.bitrig then
-+   defaults.arch = "bitrig-"..cfg.target_cpu
-+end
-+
 +if cfg.platforms.dragonfly then
 +   defaults.arch = "dragonfly-"..cfg.target_cpu
 +end
@@ -61,3 +57,8 @@
  end
  
  if cfg.platforms.openbsd then
+    defaults.arch = "openbsd-"..cfg.target_cpu
++   defaults.gcc_rpath = false
+ end
+ 
+ if cfg.platforms.netbsd then
