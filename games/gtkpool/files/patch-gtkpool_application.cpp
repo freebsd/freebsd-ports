42438784608483a00ce08825abdbc9067814d478
tobik <tobik@FreeBSD.org>
2018-08-19 08:13:51 +0000

    games/gtkpool: Fix build with Clang

--- games/gtkpool/files/patch-gtkpool_application.cpp
+++ games/gtkpool/files/patch-gtkpool_application.cpp
@@ -1,6 +1,6 @@
---- gtkpool/application.cpp.orig	2002-08-06 13:02:45.000000000 +0900
-+++ gtkpool/application.cpp	2011-08-19 04:36:56.000000000 +0900
-@@ -67,11 +67,11 @@
+--- gtkpool/application.cpp.orig	2002-08-06 04:02:45 UTC
++++ gtkpool/application.cpp
+@@ -67,11 +67,11 @@ Application::Application(){
  		message_colours[14] = new GdkColor;
  
  		buf_pixmap = NULL;
@@ -17,7 +17,16 @@
  		running = true; placing_cue = false;
  		sunk_tf = collide_tf = bounce_tf = false;
  		connected = false;
-@@ -530,7 +530,7 @@
+@@ -234,7 +234,7 @@ reiterated until there are no more collisions left in 
+ 				}
+ 
+ 				update_rack();
+-				balls.erase(std::vector<Ball>::iterator(bnc)); // get rid of it
++				balls.erase(std::remove_if(balls.begin(), balls.end(), [bnc](Ball const &b) { return &b == bnc; }), balls.end()); // get rid of it
+ 				sunk_tf = true;
+ 			}
+ 			else             // otherwise bounce it off the bumper
+@@ -530,7 +530,7 @@ void Application::mouse_down (double x, double y) {
   	bb = find_if(balls.begin(), balls.end(), pointer_selects( x, y, hit_moving));
   	if(bb != balls.end())
   	{
@@ -26,7 +35,7 @@
  		// FIXME: horrible, non-portable, converting a vector iterator
  		// to a pointer using g++ 3.0 private interface :-(
  		// -- Philip Martin <philip_martin@ntlworld.com>
-@@ -815,12 +815,18 @@
+@@ -815,12 +815,18 @@ void Application::init_sound()
  		load_sounds();
  }
  
