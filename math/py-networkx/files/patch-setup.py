8e60aef42a9454adb82019a9b34b50e392076fdf
yuri <yuri@FreeBSD.org>
2018-08-20 07:39:32 +0000

    math/py-networkx: Update 1.10 -> 2.1
    PR:		229815
    Reported by:	jwb
    Approved by:	dikshie@sfc.wide.ad.jp (maintainer)

--- math/py-networkx/files/patch-setup.py
+++ math/py-networkx/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2015-11-15 23:06:39 UTC
+--- setup.py.orig	2018-08-20 04:50:16 UTC
 +++ setup.py
-@@ -59,27 +59,6 @@ packages=["networkx",
-           "networkx.testing",
-           "networkx.utils"]
+@@ -57,28 +57,6 @@ packages = ["networkx",
+             "networkx.testing",
+             "networkx.utils"]
  
--docdirbase  = 'share/doc/networkx-%s' % version
+-docdirbase = 'share/doc/networkx-%s' % version
 -# add basic documentation
 -data = [(docdirbase, glob("*.txt"))]
 -# add examples
@@ -14,25 +14,26 @@
 -          '3d_drawing',
 -          'drawing',
 -          'graph',
--          'multigraph',
+-          'javascript',
+-          'jit',
 -          'pygraphviz',
--          'readwrite']:
--    dd = os.path.join(docdirbase,'examples', d)
+-          'subclass']:
+-    dd = os.path.join(docdirbase, 'examples', d)
 -    pp = os.path.join('examples', d)
--    data.append((dd, glob(os.path.join(pp ,"*.py"))))
--    data.append((dd, glob(os.path.join(pp ,"*.bz2"))))
--    data.append((dd, glob(os.path.join(pp ,"*.gz"))))
--    data.append((dd, glob(os.path.join(pp ,"*.mbox"))))
--    data.append((dd, glob(os.path.join(pp ,"*.edgelist"))))
+-    data.append((dd, glob(os.path.join(pp, "*.py"))))
+-    data.append((dd, glob(os.path.join(pp, "*.bz2"))))
+-    data.append((dd, glob(os.path.join(pp, "*.gz"))))
+-    data.append((dd, glob(os.path.join(pp, "*.mbox"))))
+-    data.append((dd, glob(os.path.join(pp, "*.edgelist"))))
 -
  # add the tests
- package_data     = {
+ package_data = {
      'networkx': ['tests/*.py'],
-@@ -131,7 +110,6 @@ if __name__ == "__main__":
-         download_url     = release.download_url,
-         classifiers      = release.classifiers,
-         packages         = packages,
--        data_files       = data,
-         package_data     = package_data,
-         install_requires = install_requires,
-         test_suite       = 'nose.collector',
+@@ -130,7 +108,6 @@ if __name__ == "__main__":
+         download_url=release.download_url,
+         classifiers=release.classifiers,
+         packages=packages,
+-        data_files=data,
+         package_data=package_data,
+         install_requires=install_requires,
+         extras_require=extras_require,
