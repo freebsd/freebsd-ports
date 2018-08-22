948e7eaa714b5574a74458ba454d71830d5ba837
Carlos J. Puga Medina <cpm@FreeBSD.org>
2018-08-22 10:01:02 +0000

    MFH: r477781
    - Add more useful comments to make Makefile more readable
    - Fix TEST option allow running basic tests and skip tests that must be verified
      if they are supported before being added back to TEST_TARGETS
    - Add patches that fixes crash with non-MESA drivers fall back to scanning PCI
      devices. Ideally, we'd like to use libpci but the current version calculates
      device class wrong for non-root users (patch submitted to libpci author), so
      a workaround would be messy. Instead, use direct access to /dev/pci. Once the
      upstream fixes the bug generic libpci code can be used [1]
    
      Tested by: Oleh Hushchenkov <gor@clogic.com.ua>, Jonathan Chen <jonc@chen.org.nz>
    
    - Bump PORTREVISION
    
    PR:		230450 [1]
    Submitted by:	gonzo [1]
    Reported by:	Oleh Hushchenkov, Jonathan Chen [1]
    
    Approved by:	ports-secteam (blanket)

--- www/chromium/files/patch-chrome_test_base_in__process__browser__test.cc
+++ www/chromium/files/patch-chrome_test_base_in__process__browser__test.cc
@@ -1,13 +1,22 @@
---- chrome/test/base/in_process_browser_test.cc.orig	2017-06-05 19:03:04 UTC
-+++ chrome/test/base/in_process_browser_test.cc
-@@ -95,6 +95,10 @@
- 
- namespace {
+--- chrome/test/base/in_process_browser_test.cc.orig	2018-08-08 21:10:43.000000000 +0200
++++ chrome/test/base/in_process_browser_test.cc	2018-08-21 00:58:24.257483000 +0200
+@@ -69,6 +69,10 @@
+ #include "chrome/test/base/scoped_bundle_swizzler_mac.h"
+ #endif
  
 +#if defined(OS_FREEBSD)
 +#include <signal.h>
 +#endif
 +
- // Passed as value of kTestType.
- const char kBrowserTestType[] = "browser";
+ #if defined(OS_WIN)
+ #include "base/win/scoped_com_initializer.h"
+ #include "base/win/windows_version.h"
+@@ -88,7 +92,7 @@
+ #include "chrome/test/base/default_ash_event_generator_delegate.h"
+ #endif  // defined(OS_CHROMEOS)
+ 
+-#if !defined(OS_CHROMEOS) && defined(OS_LINUX)
++#if !defined(OS_CHROMEOS) && (defined(OS_LINUX) || defined(OS_BSD))
+ #include "ui/views/test/test_desktop_screen_x11.h"
+ #endif
  
