c26a6ef628bebf010d126b0de1be3a8321a5c9f0
Carlos J. Puga Medina <cpm@FreeBSD.org>
2018-08-22 10:11:13 +0000

    - Conveniently apply r477781 to iridium

--- www/iridium/files/patch-third__party_angle_src_gpu_info_util_SystemInfo_internal.h
+++ www/iridium/files/patch-third__party_angle_src_gpu_info_util_SystemInfo_internal.h
@@ -1,6 +1,6 @@
---- third_party/angle/src/gpu_info_util/SystemInfo_internal.h.orig	2017-09-10 02:31:40.635170000 +0200
-+++ third_party/angle/src/gpu_info_util/SystemInfo_internal.h	2017-09-10 02:32:27.153701000 +0200
-@@ -14,6 +14,8 @@
+--- third_party/angle/src/gpu_info_util/SystemInfo_internal.h.orig	2018-08-08 12:13:09.000000000 -0700
++++ third_party/angle/src/gpu_info_util/SystemInfo_internal.h	2018-08-20 09:43:56.141492000 -0700
+@@ -14,8 +14,13 @@
  namespace angle
  {
  
@@ -8,4 +8,9 @@
 +
  // Defined in SystemInfo_libpci when GPU_INFO_USE_LIBPCI is defined.
  bool GetPCIDevicesWithLibPCI(std::vector<GPUDeviceInfo> *devices);
++#if defined(__FreeBSD__)
++bool GetPCIDevicesFreeBSD(std::vector<GPUDeviceInfo> *devices);
++#endif
  // Defined in SystemInfo_x11 when GPU_INFO_USE_X11 is defined.
+ bool GetNvidiaDriverVersionWithXNVCtrl(std::string *version);
+ 
