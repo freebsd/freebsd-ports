7e7e56aaa67f9f8a67252c718967431f013b5c18
jbeich <jbeich@FreeBSD.org>
2018-08-22 19:38:08 +0000

    www/webkit2-gtk3, java/openjfx8-devel: unbreak with libc++ 7
    In file included from Source/JavaScriptCore/llint/LLIntOffsetsExtractor.cpp:28:
    In file included from Source/JavaScriptCore/bytecode/ArrayProfile.h:29:
    In file included from Source/JavaScriptCore/runtime/Structure.h:28:
    In file included from Source/JavaScriptCore/runtime/ClassInfo.h:25:
    In file included from Source/JavaScriptCore/interpreter/CallFrame.h:30:
    In file included from Source/JavaScriptCore/runtime/VM.h:40:
    In file included from Source/JavaScriptCore/runtime/FunctionHasExecutedCache.h:28:
    In file included from /usr/include/c++/v1/unordered_map:385:
    In file included from /usr/include/c++/v1/__node_handle:16:
    /usr/include/c++/v1/optional:171:29: error: redefinition of 'bad_optional_access'
    class _LIBCPP_EXCEPTION_ABI bad_optional_access
                                ^
    DerivedSources/ForwardingHeaders/wtf/Optional.h:295:7: note: previous definition is here
    class bad_optional_access : public std::logic_error {
          ^
    
    PR:		230464 230600
    Submitted by:	dim

---
+++ www/webkit2-gtk3/files/patch-Source_WTF_wtf_Optional.h
@@ -0,0 +1,17 @@
+--- Source/WTF/wtf/Optional.h.orig	2018-02-19 07:45:30 UTC
++++ Source/WTF/wtf/Optional.h
+@@ -291,12 +291,14 @@ struct nullopt_t
+ constexpr nullopt_t nullopt{nullopt_t::init()};
+ 
+ 
++# if !defined(_LIBCPP_VERSION) || _LIBCPP_VERSION < 7000
+ // 20.5.8, class bad_optional_access
+ class bad_optional_access : public std::logic_error {
+ public:
+   explicit bad_optional_access(const std::string& what_arg) : std::logic_error{what_arg} {}
+   explicit bad_optional_access(const char* what_arg) : std::logic_error{what_arg} {}
+ };
++# endif // _LIBCPP_VERSION < 7000
+ 
+ 
+ template <class T>
