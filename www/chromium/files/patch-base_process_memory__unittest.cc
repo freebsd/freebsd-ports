c03810c215b995f2e794cac71d97bb5b34e48393
Carlos J. Puga Medina <cpm@FreeBSD.org>
2018-08-22 09:58:18 +0000

    - Add more useful comments to make Makefile more readable - Fix TEST option allow running basic tests and skip tests that must be verified   if they are supported before being added back to TEST_TARGETS - Add patches that fixes crash with non-MESA drivers fall back to scanning PCI   devices. Ideally, we'd like to use libpci but the current version calculates   device class wrong for non-root users (patch submitted to libpci author), so   a workaround would be messy. Instead, use direct access to /dev/pci. Once the   upstream fixes the bug generic libpci code can be used [1]
      Tested by: Oleh Hushchenkov <gor@clogic.com.ua>, Jonathan Chen <jonc@chen.org.nz>
    
    - Bump PORTREVISION
    
    PR:		230450 [1]
    Submitted by:	gonzo [1]
    Reported by:	Oleh Hushchenkov, Jonathan Chen [1]

--- www/chromium/files/patch-base_process_memory__unittest.cc
+++ www/chromium/files/patch-base_process_memory__unittest.cc
@@ -1,5 +1,5 @@
---- base/process/memory_unittest.cc.orig	2017-09-05 21:05:11.000000000 +0200
-+++ base/process/memory_unittest.cc	2017-09-06 16:53:33.919374000 +0200
+--- base/process/memory_unittest.cc.orig	2018-08-08 21:10:31.000000000 +0200
++++ base/process/memory_unittest.cc	2018-08-20 13:06:25.303428000 +0200
 @@ -104,7 +104,7 @@
  // OpenBSD does not support these tests. Don't test these on ASan/TSan/MSan
  // configurations: only test the real allocator.
@@ -9,9 +9,9 @@
      !defined(MEMORY_TOOL_REPLACES_ALLOCATOR)
  
  namespace {
-@@ -507,5 +507,5 @@
+@@ -529,5 +529,5 @@
    EXPECT_FALSE(base::UncheckedCalloc(1, test_size_, &value_));
-   EXPECT_TRUE(value_ == NULL);
+   EXPECT_TRUE(value_ == nullptr);
  }
 -#endif  // !defined(OS_OPENBSD) && BUILDFLAG(ENABLE_WIN_ALLOCATOR_SHIM_TESTS) &&
 +#endif  // !defined(OS_BSD) && BUILDFLAG(ENABLE_WIN_ALLOCATOR_SHIM_TESTS) &&
