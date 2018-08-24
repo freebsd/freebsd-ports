5f73dbc9444948c5829aa77803df7f83b8d39acc
garga <garga@FreeBSD.org>
2018-08-24 12:46:07 +0000

    sysutils/openipmi: Update to 2.0.25
    Pass maintainership to submitter
    
    PR:		230052
    Submitted by:	Vinícius Zavam <egypcio@googlemail.com>
    Sponsored by:	Rubicon Communications, LLC (Netgate)

--- sysutils/openipmi/files/patch-lanserv_extcmd.c
+++ sysutils/openipmi/files/patch-lanserv_extcmd.c
@@ -1,14 +1,20 @@
---- lanserv/extcmd.c.orig	2015-07-30 03:31:16 UTC
+--- lanserv/extcmd.c.orig	2018-07-30 08:08:12 UTC
 +++ lanserv/extcmd.c
-@@ -52,10 +52,11 @@
-  *      products derived from this software without specific prior
+@@ -53,16 +53,11 @@
   *      written permission.
   */
+ #include <config.h>
 +#include <sys/types.h>
  #include <sys/socket.h>
  #include <netinet/in.h>
  #include <arpa/inet.h>
+-#ifdef HAVE_NETINET_ETHER_H
 -#include <netinet/ether.h>
+-#elif defined(HAVE_SYS_ETHERNET_H)
+-#include <sys/ethernet.h>
+-#else
+-#error "either netinet/ether.h or sys/ethernet.h must exist."
+-#endif
 +#include <net/ethernet.h>
  #include <ctype.h>
  #include <string.h>
