256f51071aaf478c4439b1c476a6cfac9fd51a5f
Jan Beich <jbeich@FreeBSD.org>
2018-08-27 17:29:28 +0000

    games/keeperrl: unbreak on FreeBSD 10
    stdafx.h:94:20: error: redefinition of 'unique'
    std::unique_ptr<T> unique(Args&&... args) { return std::unique_ptr<T>(new T(std::forward<Args>(args)...)); }
                       ^
    stdafx.h:94:20: note: previous definition is here
    std::unique_ptr<T> unique(Args&&... args) { return std::unique_ptr<T>(new T(std::forward<Args>(args)...)); }
                       ^
    stack_printer.cpp:329:18: error: assigning to 'char *' from incompatible type 'void *'
          ss.ss_sp = (void*)alternate_stack;
                     ^~~~~~~~~~~~~~~~~~~~~~

---
+++ games/keeperrl/files/patch-stack__printer.cpp
@@ -0,0 +1,20 @@
+https://svnweb.freebsd.org/changeset/base/294930
+
+stack_printer.cpp:329:18: error: assigning to 'char *' from incompatible type 'void *'
+      ss.ss_sp = (void*)alternate_stack;
+                 ^~~~~~~~~~~~~~~~~~~~~~
+
+--- stack_printer.cpp.orig	2017-10-24 09:45:42 UTC
++++ stack_printer.cpp
+@@ -326,7 +326,11 @@ int printStacktraceWithGdb() {
+       stack_t ss = {};
+       /* malloc is usually used here, I'm not 100% sure my static allocation
+          is valid but it seems to work just fine. */
++#if defined(__DragonFly__) || (defined(__FreeBSD__) && __FreeBSD__ < 11)
++      ss.ss_sp = (char*)alternate_stack;
++#else
+       ss.ss_sp = (void*)alternate_stack;
++#endif
+       ss.ss_size = SIGSTKSZ;
+       ss.ss_flags = 0;
+ 
