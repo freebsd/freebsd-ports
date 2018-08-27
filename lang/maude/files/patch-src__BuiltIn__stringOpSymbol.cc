e7def78e70a395739c15723aefa43a20a826292d
tobik <tobik@FreeBSD.org>
2018-08-27 21:08:25 +0000

    lang/maude: Fix build with Clang 6
    ../../src/Core/termSet.hh:35:15: error: ISO C++11 does not allow access declarations; use using declarations instead
      PointerSet::cardinality;
                  ^
    
    http://beefy11.nyi.freebsd.org/data/head-i386-default/p477696_s338122/logs/errors/maude-2.6_1.log

--- lang/maude/files/patch-src__BuiltIn__stringOpSymbol.cc
+++ lang/maude/files/patch-src__BuiltIn__stringOpSymbol.cc
@@ -1,11 +1,11 @@
---- ./src/BuiltIn/stringOpSymbol.cc.orig	2014-09-03 02:36:06.000000000 +0200
-+++ ./src/BuiltIn/stringOpSymbol.cc	2014-09-03 02:32:45.000000000 +0200
-@@ -472,7 +472,7 @@
+--- src/BuiltIn/stringOpSymbol.cc.orig	2008-09-12 01:03:36 UTC
++++ src/BuiltIn/stringOpSymbol.cc
+@@ -472,7 +472,7 @@ StringOpSymbol::eqRewrite(DagNode* subject, RewritingC
  		const mpz_class& n0 = succSymbol->getNat(a0);
  		if (n0 <= 255)
  		  {
 -		    char c = n0.get_si();
-+		    char c[2] = { n0.get_si(), 0 };
++		    char c[2] = { static_cast<char>(n0.get_si()), 0 };
  		    return rewriteToString(subject, context, crope(c));
  		  }
  	      }
