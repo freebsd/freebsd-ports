76ab1a5d911df1854494de7105a03b57f0013f86
woodsb02 <woodsb02@FreeBSD.org>
2018-08-27 13:05:13 +0000

    net/remmina: Update to 1.2.31.4
    Changes this release:
      https://gitlab.com/Remmina/Remmina/blob/v1.2.31.4/CHANGELOG.md

--- net/remmina-plugins/bsd.plugin.mk
+++ net/remmina-plugins/bsd.plugin.mk
@@ -15,6 +15,7 @@ RUN_DEPENDS+=	remmina>=${PORTVERSION}:net/remmina
 
 USES+=		cmake localbase:ldflags pkgconfig
 USE_GNOME+=	atk gdkpixbuf2 glib20 gtk30 pango
+USE_LDCONFIG=	yes
 INSTALLS_ICONS=	yes
 
 # disable appindicator as it is a feature of gnome 3 which isn't supported on FreeBSD
@@ -28,39 +29,41 @@ PLIST=		${.CURDIR}/pkg-plist
 
 post-patch:
 # Do not build remmina core program
-	${REINPLACE_CMD} -e 's|add_subdirectory(remmina)||' ${WRKSRC}/CMakeLists.txt
-	${REINPLACE_CMD} -e 's|find_suggested_package(AVAHI)||' ${WRKSRC}/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(src)/d' ${WRKSRC}/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(data)/d' ${WRKSRC}/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(po)/d' ${WRKSRC}/CMakeLists.txt
+	${REINPLACE_CMD} -e '/find_suggested_package(AVAHI)/d' ${WRKSRC}/CMakeLists.txt
 # Which plugins to build
 .if ${PKGNAMESUFFIX:S,-,,} != "exec"
-	${REINPLACE_CMD} -e 's|add_subdirectory(exec)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(exec)/d' ${WRKSRC}/plugins/CMakeLists.txt
 .endif
 .if ${PKGNAMESUFFIX:S,-,,} != "nx"
-	${REINPLACE_CMD} -e 's|find_suggested_package(LIBSSH)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
-	${REINPLACE_CMD} -e 's|find_required_package(XKBFILE)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
-	${REINPLACE_CMD} -e 's|add_subdirectory(nx)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/find_suggested_package(LIBSSH)/d' ${WRKSRC}/plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/find_required_package(XKBFILE)/d' ${WRKSRC}/plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(nx)/d' ${WRKSRC}/plugins/CMakeLists.txt
 .endif
 .if ${PKGNAMESUFFIX:S,-,,} != "rdp"
-	${REINPLACE_CMD} -e 's|find_suggested_package(FREERDP)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
-	${REINPLACE_CMD} -e 's|add_subdirectory(rdp)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/find_suggested_package(FREERDP)/d' ${WRKSRC}/plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(rdp)/d' ${WRKSRC}/plugins/CMakeLists.txt
 .endif
 .if ${PKGNAMESUFFIX:S,-,,} != "secret"
-	${REINPLACE_CMD} -e 's|add_subdirectory(remmina-plugins-secret)||' ${WRKSRC}/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(plugins\/secret)/d' ${WRKSRC}/CMakeLists.txt
 .endif
 .if ${PKGNAMESUFFIX:S,-,,} != "spice"
-	${REINPLACE_CMD} -e 's|find_suggested_package(SPICE)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
-	${REINPLACE_CMD} -e 's|add_subdirectory(spice)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/find_suggested_package(SPICE)/d' ${WRKSRC}/plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(spice)/d' ${WRKSRC}/plugins/CMakeLists.txt
 .endif
 .if ${PKGNAMESUFFIX:S,-,,} != "telepathy"
-	${REINPLACE_CMD} -e 's|find_suggested_package(TELEPATHY)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
-	${REINPLACE_CMD} -e 's|add_subdirectory(telepathy)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/find_suggested_package(TELEPATHY)/d' ${WRKSRC}/plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(telepathy)/d' ${WRKSRC}/plugins/CMakeLists.txt
 .endif
 .if ${PKGNAMESUFFIX:S,-,,} != "vnc"
-	${REINPLACE_CMD} -e 's|find_suggested_package(GCRYPT)||' ${WRKSRC}/CMakeLists.txt
-	${REINPLACE_CMD} -e' s|find_suggested_package(LIBVNCSERVER)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
-	${REINPLACE_CMD} -e 's|add_subdirectory(vnc)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/find_suggested_package(GCRYPT)/d' ${WRKSRC}/CMakeLists.txt
+	${REINPLACE_CMD} -e' /find_suggested_package(LIBVNCSERVER)/d' ${WRKSRC}/plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(vnc)/d' ${WRKSRC}/plugins/CMakeLists.txt
 .endif
 .if ${PKGNAMESUFFIX:S,-,,} != "xdmcp"
-	${REINPLACE_CMD} -e 's|add_subdirectory(xdmcp)||' ${WRKSRC}/remmina-plugins/CMakeLists.txt
+	${REINPLACE_CMD} -e '/add_subdirectory(xdmcp)/d' ${WRKSRC}/plugins/CMakeLists.txt
 .endif
 
 .include <bsd.port.post.mk>
