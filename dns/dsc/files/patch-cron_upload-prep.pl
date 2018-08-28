790acdb167d581302e562cb6f2889df6cbfc8cc9
Craig Leres <leres@FreeBSD.org>
2018-08-28 00:45:52 +0000

    Update to 2.7.0. Add --with-extra-cflags=-I${PREFIX}/include so configure finds pcap/sll.h. Add a rc.d script. Fix the path to the config directory in upload-prep.pl or else it silently exits without doing anything when it doesn't find any configs.
    PR:		230661
    Reviewed by:	ler (mentor)
    Approved by:	Leo Vandewoestijn (maintainer), ler (mentor)
    Differential Revision:	https://reviews.freebsd.org/D16925

--- dns/dsc/files/patch-cron_upload-prep.pl
+++ dns/dsc/files/patch-cron_upload-prep.pl
@@ -1,4 +1,4 @@
---- cron/upload-prep.pl.orig	2018-03-13 22:05:23 UTC
+--- cron/upload-prep.pl.orig	2017-08-21 08:14:08 UTC
 +++ cron/upload-prep.pl
 @@ -1,4 +1,4 @@
 -#!/usr/bin/perl
@@ -6,3 +6,12 @@
  #
  # Copyright (c) 2016-2017, OARC, Inc.
  # Copyright (c) 2007, The Measurement Factory, Inc.
+@@ -54,7 +54,7 @@ exit 0 if Proc::PID::File->running(dir =
+ sleep 3;
+ 
+ 
+-foreach my $conf (</usr/local/dsc/etc/*.conf>) {
++foreach my $conf (</usr/local/etc/dsc/*.conf>) {
+ 	next unless open (CONF, $conf);
+ 	my $rundir = undef;
+ 	while (<CONF>) {
