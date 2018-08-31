97479fb29df70fcbee37ecd48fc0af213d02a34f
Jason E. Hale <jhale@FreeBSD.org>
2018-08-31 09:40:13 +0000

    Fix build with Qt 5.11
    PR:		230884

---
+++ audio/sayonara/files/patch-src_GUI_InfoDialog_GUI__InfoDialog.h
@@ -0,0 +1,13 @@
+Fix build with Qt 5.11
+
+--- src/GUI/InfoDialog/GUI_InfoDialog.h.orig	2018-08-31 09:31:17 UTC
++++ src/GUI/InfoDialog/GUI_InfoDialog.h
+@@ -29,6 +29,8 @@
+ #include "Utils/MetaData/MetaDataFwd.h"
+ #include "Utils/Pimpl.h"
+ 
++#include <QTabBar>
++
+ class InfoDialogContainer;
+ namespace Cover
+ {
