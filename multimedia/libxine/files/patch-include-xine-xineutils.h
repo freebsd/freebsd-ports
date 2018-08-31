7216896a936ea943edf517ef7b01ebf39ef06c83
tobik <tobik@FreeBSD.org>
2018-08-31 08:16:18 +0000

    multimedia/libxine: Update to 1.2.9
    - Cleanup options
    - Enable IMAGEMAGICK by default
    - Make DVB support optional but leave it enabled by default
    - Add SNDIO option
    - Properly order pkg-plist
    - Unbreak on aarch64
    - Bump revision of dependent ports [1]
    
    Changes:	https://sourceforge.net/projects/xine/files/xine-lib/1.2.9/README.txt/view
    ABI:		https://abi-laboratory.pro/tracker/timeline/xine-lib/ [1]
    PR:		219921
    Reported by:	emorrasg@yahoo.es
    Submitted by:	w.schwarzenfeld@utanet.at (initial version), Nathan <ndowens@yahoo.com>
    Reviewed by:	mat, tobik
    Differential Revision:	https://reviews.freebsd.org/D16840

--- multimedia/libxine/files/patch-include-xine-xineutils.h
+++ multimedia/libxine/files/patch-include-xine-xineutils.h
@@ -1,3 +1,14 @@
+Unbreak build of phonon-xine
+
+In file included from xine/plugins.c:23:
+In file included from /usr/local/include/xine/input_plugin.h:27:
+/usr/local/include/xine/xineutils.h:197:8: error: unknown type name 'inline'
+static inline void _x_freep(void *ptr) {
+       ^
+/usr/local/include/xine/xineutils.h:197:15: error: expected identifier or '('
+static inline void _x_freep(void *ptr) {
+              ^
+
 --- include/xine/xineutils.h.orig
 +++ include/xine/xineutils.h
 @@ -146,7 +146,7 @@ void *xine_xcalloc(size_t nmemb, size_t 
--- multimedia/phonon-xine/files/patch-xine__kequalizer_plugin.cpp
+++ multimedia/phonon-xine/files/patch-xine__kequalizer_plugin.cpp
@@ -1,6 +1,21 @@
---- ./xine/kequalizer_plugin.cpp.orig	2011-01-19 20:20:53.000000000 +0100
-+++ ./xine/kequalizer_plugin.cpp	2012-03-12 11:12:12.244132405 +0100
-@@ -48,6 +48,9 @@
+In file included from xine/kequalizer_plugin.cpp:51:
+In file included from /usr/local/include/libavutil/mem.h:35:
+In file included from /usr/local/include/libavutil/avutil.h:296:
+/usr/local/include/libavutil/common.h:30:2: error: missing -D__STDC_CONSTANT_MACROS / #define __STDC_CONSTANT_MACROS
+#error missing -D__STDC_CONSTANT_MACROS / #define __STDC_CONSTANT_MACROS
+ ^
+
+--- xine/kequalizer_plugin.cpp.orig	2011-01-19 19:20:53 UTC
++++ xine/kequalizer_plugin.cpp
+@@ -37,6 +37,7 @@
+ #include <cmath>
+ 
+ #define __STDC_FORMAT_MACROS
++#define __STDC_CONSTANT_MACROS
+ #include <inttypes.h>
+ 
+ #include <xine.h>
+@@ -48,6 +49,9 @@ extern "C" {
  #include <xine/xineutils.h>
  #undef this
  
@@ -10,7 +25,7 @@
  #define KEQUALIZER_MAX_GAIN 12.0
  #define KEQUALIZER_L       2      // Storage for filter taps
  #define KEQUALIZER_KM      10     // Max number of bands 
-@@ -281,7 +284,7 @@
+@@ -281,7 +285,7 @@ static post_plugin_t *kequalizer_open_plugin(post_clas
      Q_UNUSED(video_target);
      kequalizer_plugin_t *that;
      //deprecated: kequalizer_plugin_t *that = static_cast<kequalizer_plugin_t *>(xine_xmalloc(sizeof(kequalizer_plugin_t)));
