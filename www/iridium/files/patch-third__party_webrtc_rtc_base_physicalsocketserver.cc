fd53ee54135f290f970e5bc6cc9db36a11446f20
Carlos J. Puga Medina <cpm@FreeBSD.org>
2018-09-01 13:24:31 +0000

    - USE_GNOME also requires USES=gnome - Switch to USES=localbase:ldflags - Update patch file with missing WEBRTC_BSD macro - Bump PORTREVISION

--- www/iridium/files/patch-third__party_webrtc_rtc_base_physicalsocketserver.cc
+++ www/iridium/files/patch-third__party_webrtc_rtc_base_physicalsocketserver.cc
@@ -1,6 +1,6 @@
---- third_party/webrtc/rtc_base/physicalsocketserver.cc.orig	2018-02-24 16:26:17.000000000 +0100
-+++ third_party/webrtc/rtc_base/physicalsocketserver.cc	2018-03-04 04:38:51.589837000 +0100
-@@ -60,7 +60,7 @@
+--- third_party/webrtc/rtc_base/physicalsocketserver.cc.orig	2018-08-08 21:13:16.000000000 +0200
++++ third_party/webrtc/rtc_base/physicalsocketserver.cc	2018-08-27 01:15:56.484919000 +0200
+@@ -68,7 +68,7 @@
  
  #endif  // WEBRTC_POSIX
  
@@ -9,7 +9,7 @@
  
  int64_t GetSocketRecvTimestamp(int socket) {
    struct timeval tv_ioctl;
-@@ -317,7 +317,7 @@
+@@ -295,7 +295,7 @@
    socklen_t optlen = sizeof(*value);
    int ret = ::getsockopt(s_, slevel, sopt, (SockOptArg)value, &optlen);
    if (ret != -1 && opt == OPT_DONTFRAGMENT) {
@@ -18,7 +18,7 @@
      *value = (*value != IP_PMTUDISC_DONT) ? 1 : 0;
  #endif
    }
-@@ -330,7 +330,7 @@
+@@ -308,7 +308,7 @@
    if (TranslateOption(opt, &slevel, &sopt) == -1)
      return -1;
    if (opt == OPT_DONTFRAGMENT) {
@@ -27,7 +27,7 @@
      value = (value) ? IP_PMTUDISC_DO : IP_PMTUDISC_DONT;
  #endif
    }
-@@ -340,7 +340,7 @@
+@@ -318,7 +318,7 @@
  int PhysicalSocket::Send(const void* pv, size_t cb) {
    int sent = DoSend(s_, reinterpret_cast<const char *>(pv),
        static_cast<int>(cb),
@@ -36,7 +36,16 @@
        // Suppress SIGPIPE. Without this, attempting to send on a socket whose
        // other end is closed will result in a SIGPIPE signal being raised to
        // our process, which by default will terminate the process, which we
-@@ -560,7 +560,7 @@
+@@ -347,7 +347,7 @@
+   size_t len = addr.ToSockAddrStorage(&saddr);
+   int sent = DoSendTo(
+       s_, static_cast<const char *>(buffer), static_cast<int>(length),
+-#if defined(WEBRTC_LINUX) && !defined(WEBRTC_ANDROID)
++#if defined(WEBRTC_LINUX) && !defined(WEBRTC_ANDROID) && !defined(WEBRTC_BSD)
+       // Suppress SIGPIPE. See above for explanation.
+       MSG_NOSIGNAL,
+ #else
+@@ -538,7 +538,7 @@
        *slevel = IPPROTO_IP;
        *sopt = IP_DONTFRAGMENT;
        break;
