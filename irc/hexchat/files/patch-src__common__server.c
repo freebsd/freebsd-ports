d9d8a3c60e9213fcb6478c89f404977123303e7a
swills <swills@FreeBSD.org>
2018-09-03 13:12:27 +0000

    irc/hexchat: update to 2.14.2
    PR:		231061
    Submitted by:	Piotr Kubaj <pkubaj@anongoth.pl> (maintainer)

--- irc/hexchat/files/patch-src__common__server.c
+++ irc/hexchat/files/patch-src__common__server.c
@@ -1,9 +1,9 @@
---- src/common/server.c.orig	2018-03-14 02:26:31 UTC
+--- src/common/server.c.orig	2018-08-29 16:41:08 UTC
 +++ src/common/server.c
-@@ -749,7 +749,7 @@ server_connect_success (server *serv)
+@@ -748,7 +748,7 @@ server_connect_success (server *serv)
+ 
  		/* it'll be a memory leak, if connection isn't terminated by
  		   server_cleanup() */
- 		serv->ssl = _SSL_socket (serv->ctx, serv->sok);
 -		if ((err = _SSL_set_verify (serv->ctx, ssl_cb_verify, NULL)))
 +		if ((err = _SSL_set_verify (serv->ctx, ssl_cb_verify, "%%CA_BUNDLE%%")))
  		{
