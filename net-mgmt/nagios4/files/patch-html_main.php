c284f36c59526af2972433763afd04b96f328ba4
mat <mat@FreeBSD.org>
2018-09-03 12:38:24 +0000

    Update to 4.4.2.

--- net-mgmt/nagios4/files/patch-html_main.php
+++ net-mgmt/nagios4/files/patch-html_main.php
@@ -3,7 +3,7 @@
 @@ -146,31 +146,6 @@ $this_year = '2018';
  	<div class="product">Nagios<sup><span style="font-size: small;">&reg;</span></sup> Core<sup><span style="font-size: small;">&trade;</span></sup></div>
  	<div class="version">Version <?php echo $this_version; ?></div>
- 	<div class="releasedate">June 25, 2018</div>
+ 	<div class="releasedate">August 16, 2018</div>
 -	<div class="checkforupdates"><a href="https://www.nagios.org/checkforupdates/?version=<?php echo $this_version; ?>&amp;product=nagioscore" target="_blank">Check for updates</a></div>
 -</div>
 -
