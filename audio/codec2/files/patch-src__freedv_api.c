800abf16811105022a3cc104f8f390cb1147434d
yuri <yuri@FreeBSD.org>
2018-09-04 06:34:50 +0000

    audio/codec2: Update 0.7 -> 0.8.1
    Port changes:
    * Change MASTER_SITES
    * Add LICENSE_FILE
    * Move examples to ${EXAMPLESDIR}, add PORTEXAMPLES=*, remove them from pkg-plist
    
    PR:		228281
    Submitted by:	takefu@airport.fm

--- audio/codec2/files/patch-src__freedv_api.c
+++ audio/codec2/files/patch-src__freedv_api.c
@@ -1,4 +1,4 @@
---- src/freedv_api.c.orig	2017-03-30 20:09:46 UTC
+--- src/freedv_api.c.orig	2018-05-24 09:41:22 UTC
 +++ src/freedv_api.c
 @@ -36,7 +36,7 @@
  #ifdef __APPLE__
@@ -9,21 +9,3 @@
  #endif /* __APPLE__ */
  
  #include "fsk.h"
-@@ -260,7 +260,7 @@ struct freedv *freedv_open(int mode) {
-         f->n_codec_bits = codec2_bits_per_frame(f->codec2);
-         nbit = f->n_codec_bits;
-         nbyte = (nbit + 7) / 8;
--    } else if ((mode == FREEDV_MODE_800XA)) {
-+    } else if ((mode = FREEDV_MODE_800XA)) {
-         f->n_speech_samples = 2*codec2_samples_per_frame(f->codec2);
-         f->n_codec_bits = codec2_bits_per_frame(f->codec2);
-         nbit = f->n_codec_bits;
-@@ -506,7 +506,7 @@ void freedv_tx(struct freedv *f, short m
-      
-     if((f->mode == FREEDV_MODE_2400A) || (f->mode == FREEDV_MODE_2400B) || (f->mode == FREEDV_MODE_800XA)){
-         /* 800XA has two codec frames per modem frame */
--        if((f->mode == FREEDV_MODE_800XA)){
-+        if((f->mode = FREEDV_MODE_800XA)){
-             codec2_encode(f->codec2, &f->packed_codec_bits[0], &speech_in[  0]);
-             codec2_encode(f->codec2, &f->packed_codec_bits[4], &speech_in[320]);
-         }else{
