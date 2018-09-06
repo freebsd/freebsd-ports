ecb70b414ae1a35618bac6e995e44b96986b688e
Steve Wills <swills@FreeBSD.org>
2018-09-06 15:44:12 +0000

    www/piwigo: update AdminTools plugin to 2.9.4.1
    PR:		231196
    Submitted by:	Ralf van der Enden <tremere@cainites.net> (maintainer)

---
+++ www/piwigo/files/patch-plugins_AdminTools_main.inc.php
@@ -0,0 +1,11 @@
+--- plugins/AdminTools/main.inc.php.orig	2018-07-13 13:24:01 UTC
++++ plugins/AdminTools/main.inc.php
+@@ -1,7 +1,7 @@
+ <?php
+ /*
+ Plugin Name: Admin Tools
+-Version: 2.9.4
++Version: 2.9.4.1
+ Description: Do some admin task from the public pages
+ Plugin URI: http://piwigo.org/ext/extension_view.php?eid=720
+ Author: Piwigo team
