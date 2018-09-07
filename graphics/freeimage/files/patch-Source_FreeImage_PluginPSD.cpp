b1c914e253cc8eafadde29094597f392fe5bc29a
tobik <tobik@FreeBSD.org>
2018-09-07 17:14:41 +0000

    graphics/freeimage: Amend patch introduced in r479150
    While there is an additional check on 'handle' in psdParser::Save()
    later it should be done earlier as intended and not just commented
    out because of Clang/libc++ regressions.

--- graphics/freeimage/files/patch-Source_FreeImage_PluginPSD.cpp
+++ graphics/freeimage/files/patch-Source_FreeImage_PluginPSD.cpp
@@ -1,3 +1,8 @@
+Source/FreeImage/PluginPSD.cpp:130:10: error: cannot initialize return object of type
+      'BOOL' (aka 'int') with an rvalue of type 'nullptr_t'
+                return NULL;
+                       ^~~~
+
 --- Source/FreeImage/PluginPSD.cpp.orig	2018-09-04 20:32:27 UTC
 +++ Source/FreeImage/PluginPSD.cpp
 @@ -127,7 +127,7 @@ Load(FreeImageIO *io, fi_handle handle, int page, int 
@@ -5,7 +10,7 @@
  Save(FreeImageIO *io, FIBITMAP *dib, fi_handle handle, int page, int flags, void *data) {
  	if(!handle) {
 -		return NULL;
-+	//return NULL;
++		return FALSE;
  	}
  	try {
  		psdParser parser;
