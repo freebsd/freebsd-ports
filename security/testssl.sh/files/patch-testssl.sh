5030a53256d379e6d6b2d3e6bd6f1a87bbd039b5
tobik <tobik@FreeBSD.org>
2018-09-08 11:33:55 +0000

    security/testssl.sh: Update to 3.0rc1

--- security/testssl.sh/files/patch-testssl.sh
+++ security/testssl.sh/files/patch-testssl.sh
@@ -1,12 +1,12 @@
---- testssl.sh.orig	2018-03-17 15:02:25 UTC
+--- testssl.sh.orig	2018-09-08 11:00:02 UTC
 +++ testssl.sh
-@@ -175,6 +175,9 @@ TERM_CURRPOS=0                          
- ## CONFIGURATION PART ##
- # following variables make use of $ENV, e.g. OPENSSL=<myprivate_path_to_openssl> ./testssl.sh <host>
- # 0 means (normally) true here. Some of the variables are also accessible with a command line switch, see --help
+@@ -188,6 +188,9 @@ TERM_CURRPOS=0                                        
+ ########### Defining (and presetting) variables which can be changed
+ #
+ # Following variables make use of $ENV and can be used like "OPENSSL=<myprivate_path_to_openssl> ./testssl.sh <URI>"
 +if [[ -z "$OPENSSL" ]] ; then
 +     OPENSSL=PREFIX/openssl-unsafe/bin/openssl
 +fi
  declare -x OPENSSL OPENSSL_TIMEOUT
+ PHONE_OUT=${PHONE_OUT:-false}           # Whether testssl can retrieve CRLs and OCSP
  FAST_SOCKET=${FAST_SOCKET:-false}       # EXPERIMENTAL feature to accelerate sockets -- DO NOT USE it for production
- COLOR=${COLOR:-2}                       # 2: Full color, 1: b/w+positioning, 0: no ESC at all
