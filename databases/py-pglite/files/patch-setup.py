11120e15d841602961ad76cf813d0b599e387389
sunpoet <sunpoet@FreeBSD.org>
2018-09-09 16:59:31 +0000

    Update to 1.0.10
    - While I'm here:
      - Change MASTER_SITES to CHEESESHOP
      - Change post-extract: to post-patch:
    
    Changes:	https://github.com/Oslandia/pglite/releases
    PR:		231246
    Submitted by:	lbartoletti <lbartoletti@tuxfamily.org> (maintainer)

---
+++ databases/py-pglite/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2018-09-08 06:36:41 UTC
++++ setup.py
+@@ -30,7 +30,7 @@ setup(
+     extras_require={
+         "develop": ()
+     },
+-    scripts=['script/pglite.bat', 'script/pglite']
++    scripts=['script/pglite']
+ 
+ #    entry_points=dict(console_scripts=[
+ #        'pglite=pglite.pglite:main',
