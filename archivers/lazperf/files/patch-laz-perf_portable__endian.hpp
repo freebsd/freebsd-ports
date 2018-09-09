657c9d6a3b6393eca4e814fd5efb790fb5a28998
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2018-09-09 16:59:37 +0000

    Update to 1.3.0
    - While I'm here:
      - Use CMAKE_OFF
      - Add NO_ARCH
      - Change post-build: to post-patch:
      - Silence patch message
      - Fix patch file
    
    Changes:	https://github.com/hobu/laz-perf/commits/master
    PR:		231247
    Submitted by:	lbartoletti <lbartoletti@tuxfamily.org> (maintainer)

--- archivers/lazperf/files/patch-laz-perf_portable__endian.hpp
+++ archivers/lazperf/files/patch-laz-perf_portable__endian.hpp
@@ -1,16 +1,16 @@
---- laz-perf/portable_endian.hpp.orig	2017-08-16 21:27:03 UTC
+--- laz-perf/portable_endian.hpp.orig	2018-07-12 13:01:41 UTC
 +++ laz-perf/portable_endian.hpp
-@@ -41,11 +41,11 @@
+@@ -43,11 +43,11 @@
  #   define __PDP_ENDIAN    PDP_ENDIAN
  **/
  
--#elif defined(__OpenBSD__) || defined(EMSCRIPTEN_OPEN_BSD)
-+#elif defined(__OpenBSD__) || defined(__FreeBSD__) || defined(EMSCRIPTEN_OPEN_BSD)
+-#elif defined(__OpenBSD__)
++#elif defined(__FreeBSD__) || defined(__OpenBSD__)
  
  #   include <sys/endian.h>
  
--#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(EMSCRIPTEN_BSD)
-+#elif defined(__NetBSD__) || defined(__DragonFly__) || defined(EMSCRIPTEN_BSD)
+-#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
++#elif defined(__NetBSD__) || defined(__DragonFly__)
  
- #   include <sys/endian.h>
- 
+ #   define be16toh betoh16
+ #   define le16toh letoh16
