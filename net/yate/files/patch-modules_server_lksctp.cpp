90cd403d0f4f15c40cf23d29c7e5682ec7e5150a
pi <pi@FreeBSD.org>
2018-09-10 16:51:53 +0000

    net/yate: update 6.0.0 -> 6.1.0
    PR:		231153
    Reviewed by:	tobik
    Submitted by:	koue@chaosophia.net (maintainer)
    Relnotes:	http://yate.null.ro/websvn/log.php?repname=yate&path=%2Ftrunk%2F&rev=0&isdir=1

--- net/yate/files/patch-modules_server_lksctp.cpp
+++ net/yate/files/patch-modules_server_lksctp.cpp
@@ -1,6 +1,6 @@
---- modules/server/lksctp.cpp.orig	2018-03-28 11:56:42 UTC
+--- modules/server/lksctp.cpp.orig	2018-08-30 14:23:27 UTC
 +++ modules/server/lksctp.cpp
-@@ -529,9 +529,10 @@ bool LKSocket::alive() const
+@@ -521,9 +521,10 @@ bool LKSocket::alive() const
  		    status.sstat_primary.spinfo_state);
  	    }
      }
@@ -8,8 +8,8 @@
 +    //static int s_lastSstatState = SCTP_EMPTY;
 +    static int s_lastSstatState = SCTP_CLOSED;
      switch (status.sstat_state) {
--	MAKE_CASE(EMPTY,Local);
-+	//MAKE_CASE(EMPTY,Local);
- 	MAKE_CASE(CLOSED,Local);
- 	MAKE_CASE(COOKIE_WAIT,Local);
- 	MAKE_CASE(COOKIE_ECHOED,Local);
+-	MAKE_CASE(EMPTY,Association);
++	//MAKE_CASE(EMPTY,Association);
+ 	MAKE_CASE(CLOSED,Association);
+ 	MAKE_CASE(COOKIE_WAIT,Association);
+ 	MAKE_CASE(COOKIE_ECHOED,Association);
