e22ceb6c310602ca96b63d35f6adc50e9b846999
brnrd <brnrd@FreeBSD.org>
2018-09-12 16:11:11 +0000

    security/openssl111: Re-add OpenSSL 1.1.1 port
     - Don't break systems currently using openssl-devel
     - Both 1.1.0 and 1.1.1 are release versions
     - Too many ports currently fail to build with 1.1.x
       see https://wiki.freebsd.org/OpenSSL/1.1.1
     - Allow porters to test their ports with 1.1.1
    
    PR:		230679
    Reported by:	mat

---
+++ security/openssl111/files/extra-patch-util_process__docs.pl
@@ -0,0 +1,20 @@
+--- util/process_docs.pl.orig	2018-09-11 12:48:25 UTC
++++ util/process_docs.pl
+@@ -43,7 +43,7 @@ GetOptions(\%options,
+           );
+ 
+ unless ($options{section}) {
+-    $options{section} = [ 1, 3, 5, 7 ];
++    $options{section} = [ 1, 5, 7 ];
+ }
+ unless ($options{sourcedir}) {
+     $options{sourcedir} = catdir($config{sourcedir}, "doc");
+@@ -98,7 +98,7 @@ foreach my $section (sort @{$options{sec
+         my $suffix = { man  => ".$podinfo{section}".($options{suffix} // ""),
+                        html => ".html" } -> {$options{type}};
+         my $generate = { man  => "pod2man --name=$name --section=$podinfo{section} --center=OpenSSL --release=$config{version} \"$podpath\"",
+-                         html => "pod2html \"--podroot=$options{sourcedir}\" --htmldir=$updir --podpath=man1:man3:man5:man7 \"--infile=$podpath\" \"--title=$podname\" --quiet"
++                         html => "pod2html \"--podroot=$options{sourcedir}\" --htmldir=$updir --podpath=man1:man5:man7 \"--infile=$podpath\" \"--title=$podname\" --quiet"
+                          } -> {$options{type}};
+         my $output_dir = catdir($options{destdir}, "man$podinfo{section}");
+         my $output_file = $podname . $suffix;
