53e540ba51788ccdeaf4e671ba982d5a2c0f694f
bhughes <bhughes@FreeBSD.org>
2018-09-14 03:49:54 +0000

    www/node8: Update 8.11.4_1 -> 8.12.0
    - drop b8f47b27571f.patch, which has been applied upstream
    - refresh patches with `make makepatch`
    
    https://nodejs.org/en/blog/release/v8.12.0/
    
    Sponsored by:	Miles AS

--- www/node8/files/patch-deps_v8_src_base_cpu.cc
+++ www/node8/files/patch-deps_v8_src_base_cpu.cc
@@ -1,4 +1,4 @@
---- deps/v8/src/base/cpu.cc.orig	2017-08-09 18:48:10 UTC
+--- deps/v8/src/base/cpu.cc.orig	2018-09-10 18:46:54 UTC
 +++ deps/v8/src/base/cpu.cc
 @@ -607,6 +607,7 @@ CPU::CPU()
  
@@ -8,12 +8,12 @@
    CPUInfo cpu_info;
  
    // Extract implementor from the "CPU implementer" field.
-@@ -640,6 +641,8 @@ CPU::CPU()
-     }
+@@ -641,6 +642,8 @@ CPU::CPU()
      delete[] part;
    }
-+
-+#endif  // V8_OS_LINUX
  
++#endif  // V8_OS_LINUX
++
  #elif V8_HOST_ARCH_PPC
  
+ #ifndef USE_SIMULATOR
