03f6c24e87819fe9a63cff1690308ad5ef1edcbf
Danilo Egea Gondolfo <danilo@FreeBSD.org>
2018-09-14 11:20:45 +0000

    - Fix a misbinding of <Bsp>
    When curses gives no code for Ctrl+Shift+Delete, do not fall back
    to KEY_BACKSPACE, because then ^H and/or <Backspace> get bound to
    'cutwordleft'.
    
    This fixes https://savannah.gnu.org/bugs/?54642.
    
    Bug was introduced with version 3.0, commit e6429e78.
    
    Reported by:	eadler

---
+++ editors/nano/files/patch-src_nano.h
@@ -0,0 +1,10 @@
+--- src/nano.h.orig	2018-09-14 11:02:29 UTC
++++ src/nano.h
+@@ -588,6 +588,7 @@ enum
+ #define SHIFT_CONTROL_DOWN 0x414
+ #define SHIFT_CONTROL_HOME 0x415
+ #define SHIFT_CONTROL_END 0x416
++#define CONTROL_SHIFT_DELETE 0x417
+ #define ALT_LEFT 0x421
+ #define ALT_RIGHT 0x422
+ #define ALT_UP 0x423
