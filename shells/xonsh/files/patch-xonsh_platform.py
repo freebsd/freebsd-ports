0110031aed218762d6e6a7c51fc97095f8435442
0mp <0mp@FreeBSD.org>
2018-09-15 12:34:47 +0000

    shells/xonsh: Upgrade to version 0.7.8
    Changelog: https://github.com/xonsh/xonsh/blob/0.7.8/CHANGELOG.rst
    
    PR:		230369
    Submitted by:	Roberto Fernandez Cueto <roberfern@gmail.com> (maintainer)
    Reported by:	Nathan <ndowens@yahoo.com>
    Reviewed by:	krion, mat
    Reviewed by:	Nathan <ndowens@yahoo.com> (previous version)
    Reviewed by:	John Hein <z7dr6ut7gs@snkmail.com> (previous version)
    Approved by:	krion (mentor)
    Differential Revision:	https://reviews.freebsd.org/D17152

--- shells/xonsh/files/patch-xonsh_platform.py
+++ shells/xonsh/files/patch-xonsh_platform.py
@@ -1,11 +1,11 @@
---- xonsh/platform.py.orig	2017-03-12 02:36:40 UTC
+--- xonsh/platform.py.orig	2018-09-05 14:16:47 UTC
 +++ xonsh/platform.py
-@@ -491,7 +491,7 @@ def LIBC():
-         libc = ctypes.CDLL('cygwin1.dll')
+@@ -571,7 +571,7 @@ def LIBC():
+         libc = ctypes.CDLL("msys-2.0.dll")
      elif ON_BSD:
          try:
--            libc = ctypes.CDLL('libc.so')
-+      	     libc = ctypes.CDLL(ctypes.util.find_library("c"))
+-            libc = ctypes.CDLL("libc.so")
++            libc = ctypes.CDLL(ctypes.util.find_library("c"))
          except AttributeError:
              libc = None
          except OSError:
