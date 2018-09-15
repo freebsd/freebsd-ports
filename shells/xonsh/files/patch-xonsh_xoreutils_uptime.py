83f0bf94f0007e4e7789e5847ef1267731fcb7aa
Mateusz Piotrowski <0mp@FreeBSD.org>
2018-09-15 12:34:47 +0000

    shells/xonsh: Upgrade to version 0.7.8
    Changelog: https://github.com/xonsh/xonsh/blob/0.7.8/CHANGELOG.rst
    
    PR:		230369
    Submitted by:	Roberto Fernandez Cueto <roberfern@gmail.com> (maintainer)
    Reported by:	Nathan <ndowens@yahoo.com>
    Reviewed by:	krion, mat
    Reviewed by:	Nathan <ndowens@yahoo.com> (previous version)
    Reviewed by:	John Hein <z7dr6ut7gs@snkmail.com> (previous version)
    Approved by:	krion (mentor)
    Differential Revision:	https://reviews.freebsd.org/D17152

--- shells/xonsh/files/patch-xonsh_xoreutils_uptime.py
+++ shells/xonsh/files/patch-xonsh_xoreutils_uptime.py
@@ -1,11 +1,18 @@
---- xonsh/xoreutils/uptime.py.orig	2017-02-11 16:24:07 UTC
+--- xonsh/xoreutils/uptime.py.orig	2018-09-05 14:16:47 UTC
 +++ xonsh/xoreutils/uptime.py
-@@ -100,7 +100,7 @@ def _uptime_bsd():
+@@ -100,13 +100,13 @@ def _uptime_bsd():
          return None
      # Determine how much space we need for the response.
      sz = ctypes.c_uint(0)
--    xp.LIBC.sysctlbyname('kern.boottime', None, ctypes.byref(sz), None, 0)
-+    xp.LIBC.sysctlbyname(b'kern.boottime', None, ctypes.byref(sz), None, 0)
-     if sz.value != struct.calcsize('@LL'):
+-    xp.LIBC.sysctlbyname("kern.boottime", None, ctypes.byref(sz), None, 0)
++    xp.LIBC.sysctlbyname(b"kern.boottime", None, ctypes.byref(sz), None, 0)
+     if sz.value != struct.calcsize("@LL"):
          # Unexpected, let's give up.
          return None
+     # For real now.
+     buf = ctypes.create_string_buffer(sz.value)
+-    xp.LIBC.sysctlbyname("kern.boottime", buf, ctypes.byref(sz), None, 0)
++    xp.LIBC.sysctlbyname(b"kern.boottime", buf, ctypes.byref(sz), None, 0)
+     sec, usec = struct.unpack_from("@LL", buf.raw)
+     # OS X disagrees what that second value is.
+     if usec > 1000000:
