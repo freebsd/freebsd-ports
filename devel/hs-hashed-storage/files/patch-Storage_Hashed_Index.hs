69cafd7d94a60c110b193e6122e8e808d47060e0
rene <rene@FreeBSD.org>
2018-09-16 09:14:45 +0000

    Remove expired ports: 2018-09-15 www/bookmarkbridge: Unmaintained. Unlikely to work correctly with modern browsers 2018-09-15 mail/annoyance-filter: Unmaintained upstream 2018-09-15 mail/pop3vscan: Unmaintained upstream 2018-09-15 audio/firefly: Unmaintained.  Use audio/forked-daapd instead 2018-09-15 www/hs-hS3: Not being developed upstream 2018-09-15 converters/hs-dataenc: Not being developed upstream 2018-09-15 devel/tinyq: Unmaintained upstream and unused in the ports tree 2018-09-15 devel/hs-hashed-storage: Not being developed upstream

--- devel/hs-hashed-storage/files/patch-Storage_Hashed_Index.hs
+++
@@ -1,8 +0,0 @@
---- Storage/Hashed/Index.hs.orig	2014-04-07 13:32:06 UTC
-+++ Storage/Hashed/Index.hs
-@@ -1,4 +1,4 @@
--{-# LANGUAGE CPP, ScopedTypeVariables, MultiParamTypeClasses #-}
-+{-# LANGUAGE CPP, ScopedTypeVariables, MultiParamTypeClasses, FlexibleContexts #-}
- 
- -- | This module contains plain tree indexing code. The index itself is a
- -- CACHE: you should only ever use it as an optimisation and never as a primary
--- devel/hs-hashed-storage/files/patch-Storage_Hashed_Monad.hs
+++
@@ -1,8 +0,0 @@
---- Storage/Hashed/Monad.hs.orig	2014-04-07 13:32:06 UTC
-+++ Storage/Hashed/Monad.hs
-@@ -1,4 +1,4 @@
--{-# LANGUAGE ScopedTypeVariables, BangPatterns, TypeSynonymInstances, UndecidableInstances, FlexibleInstances #-}
-+{-# LANGUAGE ScopedTypeVariables, BangPatterns, TypeSynonymInstances, UndecidableInstances, FlexibleInstances, FlexibleContexts #-}
- 
- -- | An experimental monadic interface to Tree mutation. The main idea is to
- -- simulate IO-ish manipulation of real filesystem (that's the state part of
