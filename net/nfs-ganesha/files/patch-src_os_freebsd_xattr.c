a4fb065f02d817c8c645e66da68127dc3b12bc7a
bapt <bapt@FreeBSD.org>
2018-09-17 14:38:06 +0000

    Update to 1.7.0 final

--- net/nfs-ganesha/files/patch-src_os_freebsd_xattr.c
+++ net/nfs-ganesha/files/patch-src_os_freebsd_xattr.c
@@ -1,15 +1,14 @@
---- src/os/freebsd/xattr.c.orig	2018-08-10 21:04:37 UTC
+diff --git a/src/os/freebsd/xattr.c b/src/os/freebsd/xattr.c
+index 5edb6f8b8..1d6275d61 100644
+--- src/os/freebsd/xattr.c
 +++ src/os/freebsd/xattr.c
-@@ -27,8 +27,12 @@
+@@ -26,6 +26,9 @@
+  */
  
  #include <os/freebsd/xattr.h>
- #include <sys/extattr.h>
-+#include <sys/param.h>
- 
-+
-+#ifndef EXTATTR_MAXNAMELEN
- #define EXTATTR_MAXNAMELEN 255
++#ifndef NAME_MAX
++#include <sys/syslimits.h>
 +#endif
+ #include <sys/extattr.h>
  
- ssize_t fgetxattr(int fd, const char *name, void *value, size_t size)
- {
+ #ifndef EXTATTR_MAXNAMELEN
