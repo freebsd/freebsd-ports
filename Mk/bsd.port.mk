635e6cc2e3f343d3970ed82bd55469234faaaadc
Ed Maste <emaste@FreeBSD.org>
2018-09-18 14:48:48 +0000

    Hoist LDFLAGS_${ARCH} logic into bsd.port.mk
    Approved by:	portmgr (bapt)
    Sponsored by:	The FreeBSD Foundation

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -363,6 +363,7 @@ FreeBSD_MAINTAINER=	portmgr@FreeBSD.org
 # CXXFLAGS_${ARCH}
 #				 Append the cxxflags to CXXFLAGS only on the specified architecture
 ##
+# LDFLAGS_${ARCH} Append the ldflags to LDFLAGS only on the specified architecture
 # USE_SDL		- If set, this port uses the sdl libraries.
 #				  See bsd.sdl.mk for more information.
 ##
@@ -2049,6 +2050,10 @@ ${lang}FLAGS:=	${${lang}FLAGS:N-std=*} -std=${USE_${lang}STD}
 ${lang}FLAGS+=	${${lang}FLAGS_${ARCH}}
 .endif
 .endfor
+
+.if defined(LDFLAGS_${ARCH})
+LDFLAGS+=	${LDFLAGS_${ARCH}}
+.endif
 
 # Multiple make jobs support
 .if defined(DISABLE_MAKE_JOBS) || defined(MAKE_JOBS_UNSAFE)
