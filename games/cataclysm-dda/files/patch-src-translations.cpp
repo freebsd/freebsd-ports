c6c25165a84f8ba1e8c0e875859c47715c98930c
swills <swills@FreeBSD.org>
2018-09-20 05:04:03 +0000

    games/cataclysm-dda: update, fix build, add SDL option
    While here, give maintainership to submitter
    
    PR:		226418
    Submitted by:	Greg V <greg@unrelenting.technology>

--- games/cataclysm-dda/files/patch-src-translations.cpp
+++ games/cataclysm-dda/files/patch-src-translations.cpp
@@ -1,11 +1,11 @@
---- src/translations.cpp.orig	2015-03-09 05:25:34 UTC
+--- src/translations.cpp.orig	2018-03-06 08:11:44 UTC
 +++ src/translations.cpp
-@@ -72,7 +72,7 @@ void set_language(bool reload_options)
+@@ -176,7 +176,7 @@ void set_language()
  
      // Step 2. Bind to gettext domain.
-     const char *locale_dir;
--#ifdef __linux__
-+#if (defined __linux__ || defined __FreeBSD__)
-     if (!FILENAMES["base_path"].empty()) {
-         locale_dir = std::string(FILENAMES["base_path"] + "share/locale").c_str();
+     std::string locale_dir;
+-#if (defined __linux__ || (defined MACOSX && !defined TILES))
++#if (defined __linux__ || defined __FreeBSD__ || (defined MACOSX && !defined TILES))
+     if( !FILENAMES["base_path"].empty() ) {
+         locale_dir = FILENAMES["base_path"] + "share/locale";
      } else {
