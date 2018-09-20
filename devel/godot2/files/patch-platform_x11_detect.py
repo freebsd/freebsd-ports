a41e02ac4a5bb39cb489591d3018cdcf07498105
swills <swills@FreeBSD.org>
2018-09-20 00:57:43 +0000

    devel/godot2: update to 2.1.5
    PR:		231316
    Submitted by:	Shane <FreeBSD@ShaneWare.Biz> (maintainer)

--- devel/godot2/files/patch-platform_x11_detect.py
+++ devel/godot2/files/patch-platform_x11_detect.py
@@ -1,4 +1,4 @@
---- platform/x11/detect.py.orig	2017-08-28 01:55:12 UTC
+--- platform/x11/detect.py.orig	2018-07-29 00:47:52 UTC
 +++ platform/x11/detect.py
 @@ -56,7 +56,9 @@ def get_opts():
          ('use_sanitizer', 'Use llvm compiler sanitize address', 'no'),
@@ -10,8 +10,8 @@
 +        ('pulseaudio', 'Detect & Use pulseaudio', 'no'),
          ('udev', 'Use udev for gamepad connection callbacks', 'no'),
          ('debug_release', 'Add debug symbols to release version', 'no'),
-     ]
-@@ -84,10 +86,6 @@ def configure(env):
+         ('touch', 'Enable touch events', 'yes'),
+@@ -85,10 +87,6 @@ def configure(env):
  
      env.Append(CPPPATH=['#platform/x11'])
      if (env["use_llvm"] == "yes"):
@@ -22,45 +22,34 @@
          env.Append(CPPFLAGS=['-DTYPED_METHOD_BIND'])
          env.extra_suffix = ".llvm"
      elif (os.system("gcc --version > /dev/null 2>&1") == 0): # GCC
-@@ -125,19 +123,16 @@ def configure(env):
+@@ -126,19 +124,14 @@ def configure(env):
      env.Append(LINKFLAGS=['-pipe'])
  
      if (env["target"] == "release"):
 -        env.Prepend(CCFLAGS=['-Ofast'])
 -        if (env["debug_release"] == "yes"):
 -            env.Prepend(CCFLAGS=['-g2'])
-+
 +        pass
  
      elif (env["target"] == "release_debug"):
- 
+-
 -        env.Prepend(CCFLAGS=['-O2', '-ffast-math', '-DDEBUG_ENABLED'])
 -        if (env["debug_release"] == "yes"):
 -            env.Prepend(CCFLAGS=['-g2'])
+-
 +        env.Prepend(CCFLAGS=['-DDEBUG_ENABLED'])
- 
++        
      elif (env["target"] == "debug"):
  
 -        env.Prepend(CCFLAGS=['-g2', '-DDEBUG_ENABLED', '-DDEBUG_MEMORY_ENABLED'])
 +        env.Prepend(CCFLAGS=['-DDEBUG_ENABLED', '-DDEBUG_MEMORY_ENABLED'])
+         env.Append(LINKFLAGS=['-rdynamic'])
  
      env.ParseConfig('pkg-config x11 --cflags --libs')
-     env.ParseConfig('pkg-config xinerama --cflags --libs')
-@@ -148,13 +143,22 @@ def configure(env):
-         # Currently not compatible with OpenSSL 1.1.0+
-         # https://github.com/godotengine/godot/issues/8624
-         import subprocess
--        openssl_version = subprocess.check_output(['pkg-config', 'openssl', '--modversion']).strip('\n')
-+        try:
-+            openssl_version = subprocess.check_output(['pkg-config', 'openssl', '--modversion']).strip('\n')
-+        except:
-+            # approx base openssl version
-+            openssl_version = '1.0.2'
-         if (openssl_version >= "1.1.0"):
-             print("Error: Found system-installed OpenSSL %s, currently only supporting version 1.0.x." % openssl_version)
-             print("Aborting.. You can compile with 'builtin_openssl=yes' to use the bundled version.\n")
-             sys.exit(255)
+@@ -155,7 +148,12 @@ def configure(env):
+         env.Append(CPPFLAGS=['-DTOUCH_ENABLED'])
  
+     if (env['builtin_openssl'] == 'no'):
 -        env.ParseConfig('pkg-config openssl --cflags --libs')
 +        try:
 +            # try to find a port installed openssl
@@ -71,7 +60,7 @@
  
      if (env['builtin_libwebp'] == 'no'):
          env.ParseConfig('pkg-config libwebp --cflags --libs')
-@@ -199,12 +203,13 @@ def configure(env):
+@@ -200,12 +198,13 @@ def configure(env):
      if (env['builtin_glew'] == 'no'):
          env.ParseConfig('pkg-config glew --cflags --libs')
  
@@ -91,7 +80,7 @@
  
      if (platform.system() == "Linux"):
          env.Append(CPPFLAGS=["-DJOYDEV_ENABLED"])
-@@ -219,6 +224,11 @@ def configure(env):
+@@ -220,6 +219,11 @@ def configure(env):
          else:
              print("libudev development libraries not found, disabling udev support")
  
