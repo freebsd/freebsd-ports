432694bd28c1d8c1f2525b311bc3e2f532b4dd58
mat <mat@FreeBSD.org>
2018-09-20 14:12:24 +0000

    Update to 9.13.3.
    Cleanup options.

--- dns/bind913/files/patch-bin_named_include_named_globals.h
+++ dns/bind913/files/patch-bin_named_include_named_globals.h
@@ -1,8 +1,8 @@
 We reference the pid file as being run/named/pid everywere else.
 
---- bin/named/include/named/globals.h.orig	2018-05-18 05:44:02 UTC
+--- bin/named/include/named/globals.h.orig	2018-09-06 00:15:26 UTC
 +++ bin/named/include/named/globals.h
-@@ -126,7 +126,7 @@ EXTERN isc_boolean_t		named_g_forcelock	
+@@ -128,7 +128,7 @@ EXTERN bool		named_g_forcelock	INIT(fals
  #if NAMED_RUN_PID_DIR
  EXTERN const char *		named_g_defaultpidfile 	INIT(NAMED_LOCALSTATEDIR
  							     "/run/named/"
