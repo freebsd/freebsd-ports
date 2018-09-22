d432fa6ec9405801b4c8e79d730490b24e92a6e7
Yuri Victorovich <yuri@FreeBSD.org>
2018-09-22 01:41:33 +0000

    New port: devel/vitables: Viewer and editor of files in both PyTables format and HDF5 format

---
+++ devel/vitables/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2018-09-21 23:38:58 UTC
++++ setup.py
+@@ -51,7 +51,6 @@ setup(name='ViTables',
+       ],
+       install_requires=[
+           'qtpy (>=1.2.1)',
+-          'PyQt5 (>=5.5.1)',
+           'numpy (>=1.4.1)',
+           'numexpr (>=2.0)',
+           'tables (>=3.0)'
