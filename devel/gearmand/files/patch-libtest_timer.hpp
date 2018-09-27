0ce715bd34e214a2005f8c582dc380db7d485759
Bryan Drewery <bdrewery@FreeBSD.org>
2018-09-27 16:55:05 +0000

    - Fix build on powerpc64
    PR:		231743
    Submitted by:	Piotr Kubaj <pkubaj@anongoth.pl>

---
+++ devel/gearmand/files/patch-libtest_timer.hpp
@@ -0,0 +1,10 @@
+--- libtest/timer.hpp.orig	2018-09-26 15:24:37 UTC
++++ libtest/timer.hpp
+@@ -39,6 +39,7 @@
+ #include <cstdlib>
+ #include <ctime>
+ #include <iostream>
++#include <stdint.h>
+ 
+ 
+ namespace libtest {
