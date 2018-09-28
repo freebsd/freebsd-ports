f0a027b4b6f4479da94454716926500258d26ce1
Martin Wilke <miwi@FreeBSD.org>
2018-09-28 18:18:57 +0000

    - Update to 2.0.2.18
    PR:		230684
    Sponsored by:	iXsystems Inc.

--- net/nuster/files/patch-include-common-hathreads.h
+++ net/nuster/files/patch-include-common-hathreads.h
@@ -1,6 +1,6 @@
---- include/common/hathreads.h.orig	2018-02-17 18:17:22.219400000 +0000
-+++ include/common/hathreads.h	2018-02-17 18:18:44.598422000 +0000
-@@ -104,7 +104,7 @@ extern THREAD_LOCAL unsigned long tid_bit; /* The bit 
+--- include/common/hathreads.h.orig	2018-08-15 11:14:35 UTC
++++ include/common/hathreads.h
+@@ -113,7 +113,7 @@ extern THREAD_LOCAL unsigned long tid_bi
  /* TODO: thread: For now, we rely on GCC builtins but it could be a good idea to
   * have a header file regrouping all functions dealing with threads. */
  
