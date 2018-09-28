1f57af0df996b424efa654d4af882fc722b274af
romain <romain@FreeBSD.org>
2018-09-28 10:47:01 +0000

    Update to 1.1.1
    With hat:	puppet

--- sysutils/rubygem-puppetserver-ca/files/patch-lib_puppetserver_ca_config_puppet.rb
+++ sysutils/rubygem-puppetserver-ca/files/patch-lib_puppetserver_ca_config_puppet.rb
@@ -1,15 +1,15 @@
---- lib/puppetserver/ca/config/puppet.rb.orig	2018-09-27 06:33:57 UTC
+--- lib/puppetserver/ca/config/puppet.rb.orig	2018-09-28 10:36:20 UTC
 +++ lib/puppetserver/ca/config/puppet.rb
-@@ -50,7 +50,7 @@ module Puppetserver
+@@ -48,7 +48,7 @@ module Puppetserver
          def user_specific_conf_dir
            @user_specific_conf_dir ||=
-             if running_as_root?
+             if Puppetserver::Ca::Utils::Config.running_as_root?
 -              '/etc/puppetlabs/puppet'
 +              '%%PREFIX%%/etc/puppet'
              else
                "#{ENV['HOME']}/.puppetlabs/etc/puppet"
              end
-@@ -102,7 +102,7 @@ module Puppetserver
+@@ -100,7 +100,7 @@ module Puppetserver
            # defaults below
            base_defaults = [
              [:confdir, user_specific_conf_dir],
