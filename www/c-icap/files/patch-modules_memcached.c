34ef2472882103e21c3db6563d2be62a804705a0
Rodrigo Osorio <rodrigo@FreeBSD.org>
2018-09-30 08:58:41 +0000

    Upgrade c-icap to 0.5.5
    - Update c-icap to 0.5.5
    - Move USES to a better place and silence portlint
    - Remove 'ListenAddress 127.0.0.1' parameter from sample c-icap.conf since it doesn't work. Instead, replace the change to 'Port 127.0.0.1:1344'. More references about this problem at https://redmine.pfsense.org/issues/8832#note-10
    - Regenerate patches with 'make makepatch'
    
    c-icap-0.5.5 changes
    - c-icap may crash with a SIGBUS while using mmap to map files to memory.
    - Fix multiple brotli decoding bugs
    - c-icap-client does not send the ";ieof" preview termination sequence when sends zero sized files
    
    PR:		231377
    Submitted by:	garga

--- www/c-icap/files/patch-modules_memcached.c
+++ www/c-icap/files/patch-modules_memcached.c
@@ -1,4 +1,4 @@
---- modules/memcached.c.orig	2016-04-30 12:42:52 UTC
+--- modules/memcached.c.orig	2017-10-21 06:57:48 UTC
 +++ modules/memcached.c
 @@ -47,7 +47,7 @@
  #include <libmemcached/memcached_pool.h>
@@ -8,4 +8,4 @@
 +/* #include <crypt.h> */
  
  int USE_MD5_SUM_KEYS = 1;
-    
+ 
