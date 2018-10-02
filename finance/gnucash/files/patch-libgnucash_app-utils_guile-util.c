af6dab9e1d749dad5793826d74cab1368cddcdc3
Guido Falsi <madpilot@FreeBSD.org>
2018-10-02 08:42:56 +0000

    - Update gnucash to 3.3 - Update gnucash-docs to 3.3 - Adapt gnucash-docs MASTER_SITES to new upstream distribution layout - Remove old unnecessary patch from gnucash port

--- finance/gnucash/files/patch-libgnucash_app-utils_guile-util.c
+++ finance/gnucash/files/patch-libgnucash_app-utils_guile-util.c
@@ -1,6 +1,6 @@
---- libgnucash/app-utils/guile-util.c.orig	2018-03-31 22:46:38 UTC
+--- libgnucash/app-utils/guile-util.c.orig	2018-09-04 21:53:12 UTC
 +++ libgnucash/app-utils/guile-util.c
-@@ -1095,6 +1095,8 @@ gnc_parse_time_to_time64 (const gchar *s, const gchar 
+@@ -996,6 +996,8 @@ gnc_parse_time_to_time64 (const gchar *s, const gchar 
  
      g_return_val_if_fail(s && format, -1);
  
