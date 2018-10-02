1e7103688e642ae76598c1f4edfdd4c504d7070e
Yuri Victorovich <yuri@FreeBSD.org>
2018-10-02 23:11:15 +0000

    science/PETSc: Update 3.9.4->3.10.1; math/py-petsc4py: Update 3.9.1->3.10.0; math/slepc: Update 3.9.2->3.10.0; math/py-slepc4py: Update 3.9.0->3.10.0
    Bump math/sundials and math/petiga.
    On commit because they depend on each other and don't build unless versions match.

--- math/slepc/files/patch-config_install.py
+++ math/slepc/files/patch-config_install.py
@@ -1,18 +1,21 @@
---- config/install.py.orig	2018-06-25 08:07:05 UTC
+--- config/install.py.orig	2018-10-02 22:50:33 UTC
 +++ config/install.py
-@@ -295,11 +295,11 @@ for dir in dirs:
+@@ -311,13 +311,13 @@ for dir in dirs:
  
    def installShare(self):
      self.copies.extend(self.copytree(self.rootShareDir, self.destShareDir))
+-    examplesdir=os.path.join(self.destShareDir,'slepc','examples')
 -    if os.path.exists(os.path.join(self.destShareDir,'slepc','examples')):
 -      shutil.rmtree(os.path.join(self.destShareDir,'slepc','examples'))
 -    os.mkdir(os.path.join(self.destShareDir,'slepc','examples'))
 -    self.copyExamples(self.rootDir,os.path.join(self.destShareDir,'slepc','examples'))
--    self.fixExamplesMakefile(os.path.join(self.destShareDir,'slepc','examples','makefile'))
++    examplesdir=os.path.join(self.destShareDir,'examples','slepc')
 +    if os.path.exists(os.path.join(self.destShareDir,'examples','slepc')):
 +      shutil.rmtree(os.path.join(self.destShareDir,'examples','slepc'))
 +    os.mkdir(os.path.join(self.destShareDir,'examples','slepc'))
 +    self.copyExamples(self.rootDir,os.path.join(self.destShareDir,'examples','slepc'))
+     self.copyConfig(self.rootDir,examplesdir)
+-    self.fixExamplesMakefile(os.path.join(self.destShareDir,'slepc','examples','makefile'))
 +    self.fixExamplesMakefile(os.path.join(self.destShareDir,'examples','slepc','makefile'))
      return
  
