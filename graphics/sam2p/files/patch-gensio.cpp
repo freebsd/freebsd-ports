f9f781293fe4f1f33db9b17f4051c6e1a52f1d91
Dag-Erling Smørgrav <des@FreeBSD.org>
2018-10-03 16:41:35 +0000

    Upgrade to 0.49.4 with re-rolled tarball.

--- graphics/sam2p/files/patch-gensio.cpp
+++ graphics/sam2p/files/patch-gensio.cpp
@@ -1,23 +1,5 @@
 --- gensio.cpp.orig	2014-01-19 19:50:35 UTC
 +++ gensio.cpp
-@@ -494,7 +494,7 @@ Filter::PipeE::PipeE(GenBuffer::Writable
- 
-  #if HAVE_PTS_POPEN
-   if (!tmpsname) {
--    if (NULLP==(p=popen(redir_cmd(), "w"CFG_PTS_POPEN_B))) Error::sev(Error::EERROR) << "Filter::PipeE" << ": popen() failed: " << (SimBuffer::B().appendDumpC(redir_cmd)) << (Error*)0;
-+    if (NULLP==(p=popen(redir_cmd(), "w" CFG_PTS_POPEN_B))) Error::sev(Error::EERROR) << "Filter::PipeE" << ": popen() failed: " << (SimBuffer::B().appendDumpC(redir_cmd)) << (Error*)0;
-     signal(SIGPIPE, SIG_IGN); /* Don't abort process with SIGPIPE signals if child cannot read our data */
-   } else {
-  #else
-@@ -622,7 +622,7 @@ slen_t Filter::PipeD::vi_read(char *tobu
-   if (state==0) { /* Read the whole stream from `in', write it to `tmpsname' */
-    #if HAVE_PTS_POPEN
-     if (!tmpsname) {
--      if (NULLP==(p=popen(redir_cmd(), "w"CFG_PTS_POPEN_B))) Error::sev(Error::EERROR) << "Filter::PipeD" << ": popen() failed: " << (SimBuffer::B().appendDumpC(redir_cmd)) << (Error*)0;
-+      if (NULLP==(p=popen(redir_cmd(), "w" CFG_PTS_POPEN_B))) Error::sev(Error::EERROR) << "Filter::PipeD" << ": popen() failed: " << (SimBuffer::B().appendDumpC(redir_cmd)) << (Error*)0;
-       signal(SIGPIPE, SIG_IGN); /* Don't abort process with SIGPIPE signals if child cannot read our data */
-       vi_precopy();
-       in.vi_read(0,0);
 @@ -731,11 +731,7 @@ slen_t Filter::FlatD::vi_read(char *to_b
  /* --- */
  
--- graphics/sam2p/files/patch-ps__tiny.c
+++ graphics/sam2p/files/patch-ps__tiny.c
@@ -11,30 +11,3 @@
  /* ps_tiny.c -- read .pin files, write short PostScript code
   * by pts@fazekas.hu at Sat Aug 24 12:46:14 CEST 2002
   * -- Sat Aug 24 18:23:08 CEST 2002
-@@ -151,7 +144,7 @@ static void ungetcc(int c) {
- }
- 
- static void erri(char const*msg1, char const*msg2) {
--  fprintf(stderr, "%s: error at %"SLEN_P"u.%"SLEN_P"u.%"SLEN_P"u: %s%s\n",
-+  fprintf(stderr, "%s: error at %" SLEN_P "u.%" SLEN_P "u.%" SLEN_P "u: %s%s\n",
-     PROGNAME, curline, curofs-leftofs+1, curofs, msg1, msg2?msg2:"");
-   exit(3);
- }
-@@ -230,7 +223,7 @@ static sbool is_wordx(char const *s) {
- /** @param b: assume null-terminated @return true on error */
- static /*inline*/ sbool toInteger(char *s, psint_t *ret) {
-   int n=0; /* BUGFIX?? found by __CHECKER__ */
--  return sscanf(s, "%"SLEN_P"i%n", ret, &n)<1 || s[n]!='\0';
-+  return sscanf(s, "%" SLEN_P "i%n", ret, &n)<1 || s[n]!='\0';
- }
- 
- /** @param b: assume null-terminated @return true on error */
-@@ -700,7 +693,7 @@ int main(int argc, char**argv) {
-   getotag("Abbr");
-   getkey("acount");  acount=getuintval();
-   getkey("xcount");  xcount=getuintval();
--  sprintf(tmp,"%"SLEN_P"u dict%%</I>", acount+xcount);
-+  sprintf(tmp,"%" SLEN_P "u dict%%</I>", acount+xcount);
-   setifmt(tmp,0); copy("I"); noifmt();
-   gettagbeg();
- 
