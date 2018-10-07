ac3f6ce7eb7a75c098027a9680a9d4542911c5a7
Tobias C. Berner <tcberner@FreeBSD.org>
2018-10-07 12:00:18 +0000

    graphics/qcomicbook: update to 0.9.1 and switch to Qt5
    - A future poppler update will remove poppler-qt4 (no longer supported)
      so switch this port to Qt5 as preparation.

--- graphics/qcomicbook/files/patch-src__FrameDetect__CompareFrames.h
+++ graphics/qcomicbook/files/patch-src__FrameDetect__CompareFrames.h
@@ -1,5 +1,5 @@
---- ./src/FrameDetect/CompareFrames.h.orig	2012-05-30 20:32:02.000000000 +0000
-+++ ./src/FrameDetect/CompareFrames.h	2014-05-13 08:15:11.199946861 +0000
+--- src/FrameDetect/CompareFrames.h.orig	2016-11-22 17:45:10 UTC
++++ src/FrameDetect/CompareFrames.h
 @@ -14,6 +14,7 @@
  #define __COMPARE_FRAMES_H
  
