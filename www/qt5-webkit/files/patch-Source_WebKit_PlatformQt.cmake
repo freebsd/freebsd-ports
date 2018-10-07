7d507d6846c25bb2c05a7dc55470a5369a841ab8
Tobias C. Berner <tcberner@FreeBSD.org>
2018-10-07 07:53:36 +0000

    www/qt5-webkit: fix installed pkg config files
    PR:		231108
    Obtained from:	https://src.mouf.net/area51/view/branches/plasma5/PORTS/www/qt5-webkit-annulen-wk2/files/patch-Source_WebKit_PlatformQt.cmake?rev=13782

--- www/qt5-webkit/files/patch-Source_WebKit_PlatformQt.cmake
+++ www/qt5-webkit/files/patch-Source_WebKit_PlatformQt.cmake
@@ -1,18 +1,42 @@
---- Source/WebKit/PlatformQt.cmake.orig	2017-06-15 23:36:48 UTC
+commit 14947c04b3c19b5eb04a683081742b14c1d32786
+Author: Konstantin Tokarev <annulen@yandex.ru>
+Date:   Sat Oct 7 17:38:32 2017 +0300
+
+    [cmake] Fix build with CMake 3.10: -frtti was enabled for wrong file path
+    
+    Change-Id: I65d4f491f21c17f6d99a14259988d2ddb0d67105
+
+--- Source/WebKit/PlatformQt.cmake.orig	2018-10-07 07:18:38 UTC
 +++ Source/WebKit/PlatformQt.cmake
 @@ -481,6 +481,7 @@ if (NOT MACOS_BUILD_FRAMEWORKS)
          BASE_NAME Qt5WebKit
          DEPS "${WEBKIT_PKGCONGIG_DEPS}"
          FILENAME_VAR WebKit_PKGCONFIG_FILENAME
-+        DESCRIPTION "Qt WebKit module"
++        INCLUDE_INSTALL_DIR "${KDE_INSTALL_INCLUDEDIR}/QtWebKit"
+         DESCRIPTION "Qt WebKit module"
      )
-     set(ECM_PKGCONFIG_INSTALL_DIR "${LIB_INSTALL_DIR}/pkgconfig" CACHE PATH "The directory where pkgconfig will be installed to.")
-     install(FILES ${WebKit_PKGCONFIG_FILENAME} DESTINATION ${ECM_PKGCONFIG_INSTALL_DIR} COMPONENT Data)
-@@ -671,6 +672,7 @@ if (NOT MACOS_BUILD_FRAMEWORKS)
+     set(ECM_PKGCONFIG_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/libdata/pkgconfig" CACHE PATH "The directory where pkgconfig will be installed to.")
+@@ -672,6 +673,7 @@ if (NOT MACOS_BUILD_FRAMEWORKS)
          BASE_NAME Qt5WebKitWidgets
          DEPS "${WEBKITWIDGETS_PKGCONFIG_DEPS}"
          FILENAME_VAR WebKitWidgets_PKGCONFIG_FILENAME
-+        DESCRIPTION "Qt WebKitWidgets module"
++        INCLUDE_INSTALL_DIR "${KDE_INSTALL_INCLUDEDIR}/QtWebKitWidgets"
+         DESCRIPTION "Qt WebKitWidgets module"
      )
      install(FILES ${WebKitWidgets_PKGCONFIG_FILENAME} DESTINATION ${ECM_PKGCONFIG_INSTALL_DIR} COMPONENT Data)
- endif ()
+@@ -788,6 +790,7 @@ if (COMPILER_IS_GCC_OR_CLANG)
+     set_source_files_properties(
+         qt/Api/qwebdatabase.cpp
+         qt/Api/qwebelement.cpp
++        qt/Api/qwebfullscreenrequest.cpp
+         qt/Api/qwebhistory.cpp
+         qt/Api/qwebhistoryinterface.cpp
+         qt/Api/qwebpluginfactory.cpp
+@@ -797,7 +800,6 @@ if (COMPILER_IS_GCC_OR_CLANG)
+ 
+         qt/WidgetApi/qgraphicswebview.cpp
+         qt/WidgetApi/qwebframe.cpp
+-        qt/WidgetApi/qwebfullscreenrequest.cpp
+         qt/WidgetApi/qwebinspector.cpp
+         qt/WidgetApi/qwebpage.cpp
+         qt/WidgetApi/qwebview.cpp
