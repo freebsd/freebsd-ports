7f3734e674a0446440a934f8dd246bd708c2bca7
tijl <tijl@FreeBSD.org>
2018-10-10 12:11:03 +0000

    - Update to 1.21.3. - Reorganise options to enable configurations that don't depend on   Ghostscript or Poppler.

--- print/cups-filters/files/patch-cupsfilters_colormanager.c
+++ print/cups-filters/files/patch-cupsfilters_colormanager.c
@@ -1,6 +1,6 @@
---- cupsfilters/colormanager.c.orig	2015-06-26 15:45:26 UTC
+--- cupsfilters/colormanager.c.orig	2018-10-03 20:12:51 UTC
 +++ cupsfilters/colormanager.c
-@@ -26,6 +26,7 @@ MIT Open Source License  -  http://www.o
+@@ -26,6 +26,7 @@ MIT Open Source License  -  http://www.opensource.org/
  */
  
  
@@ -8,12 +8,12 @@
  #include "colormanager.h"
  #include <cupsfilters/colord.h>
  //#include <cupsfilters/kmdevices.h>
-@@ -323,7 +324,7 @@ _get_ppd_icc_fallback (ppd_file_t *ppd, 
+@@ -327,7 +328,7 @@ _get_ppd_icc_fallback (ppd_file_t *ppd, char **qualifi
      /* expand to a full path if not already specified */
      if (attr->value[0] != '/')
        snprintf(full_path, sizeof(full_path),
 -               "%s/profiles/%s", CUPSDATA, attr->value);
 +               "%s/profiles/%s", CUPS_DATADIR, attr->value);
-     else
+     else {
        strncpy(full_path, attr->value, sizeof(full_path));
- 
+       if (strlen(attr->value) > 1023)
