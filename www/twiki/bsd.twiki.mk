12f8effcc8e2eee36340f0b0e4b9588cc6bd1154
Jochen Neumeister <joneum@FreeBSD.org>
2018-10-13 17:55:13 +0000

    Update bsd.twiki.mk

--- www/twiki/bsd.twiki.mk
+++ www/twiki/bsd.twiki.mk
@@ -26,7 +26,7 @@ WWWDIR?=	${PREFIX}/www/twiki
 PLIST_SUB+=	TWDIR="share/twiki/${PNAME}"
 SUB_LIST+=	TWDIR="${TWDIR}" WWWOWN="${WWWOWN}" WWWGRP="${WWWGRP}"
 SUB_FILES+=	pkg-install pkg-deinstall
-MAINTAINER?=	ports@FreeBSD.org
+MAINTAINER?=	joneum@FreeBSD.org
 DIST_SUBDIR?=	twiki
 RUN_DEPENDS+=	${TWDEP:C/([^=<>]*)([=<>]*)(.*)/twiki-\1\20.0.\3:www\/twiki-\1/}
 
