5c8086b156c022b546afa26115f16a77fe4db2de
swills <swills@FreeBSD.org>
2018-10-14 19:35:18 +0000

    net-im/telegram-desktop: update to 1.4.2
    PR:		232212
    Submitted by:	Henry Hu <henry.hu.sh@gmail.com> (maintainer)

--- net-im/telegram-desktop/files/patch-Telegram_SourceFiles_core_utils.h
+++ net-im/telegram-desktop/files/patch-Telegram_SourceFiles_core_utils.h
@@ -1,6 +1,6 @@
---- Telegram/SourceFiles/core/utils.h.orig	2018-09-28 13:08:38 UTC
+--- Telegram/SourceFiles/core/utils.h.orig	2018-10-09 21:21:48 UTC
 +++ Telegram/SourceFiles/core/utils.h
-@@ -411,6 +411,7 @@ enum DBIPlatform {
+@@ -406,6 +406,7 @@ enum DBIPlatform {
  	dbipLinux64 = 2,
  	dbipLinux32 = 3,
  	dbipMacOld = 4,
--- net-im/telegram-desktop/files/patch-Telegram_SourceFiles_logs.cpp
+++ net-im/telegram-desktop/files/patch-Telegram_SourceFiles_logs.cpp
@@ -1,16 +1,16 @@
---- Telegram/SourceFiles/logs.cpp.orig	2018-09-28 13:08:38 UTC
+--- Telegram/SourceFiles/logs.cpp.orig	2018-10-09 21:21:48 UTC
 +++ Telegram/SourceFiles/logs.cpp
-@@ -332,7 +332,7 @@ void start(not_null<Core::Launcher*> lau
- 
+@@ -333,7 +333,7 @@ void start(not_null<Core::Launcher*> lau
  	if (cAlphaVersion()) {
  		workingDirChosen = true;
+ 
 -#if defined Q_OS_MAC || defined Q_OS_LINUX
 +#if defined Q_OS_MAC || defined Q_OS_LINUX || defined Q_OS_FREEBSD
  	} else {
- #ifdef _DEBUG
- 		cForceWorkingDir(cExeDir());
-@@ -346,15 +346,15 @@ void start(not_null<Core::Launcher*> lau
- #endif // !_DEBUG
+ 		if (!cWorkingDir().isEmpty()) {
+ 			// This value must come from TelegramForcePortable
+@@ -348,16 +348,16 @@ void start(not_null<Core::Launcher*> lau
+ 		}
  		workingDirChosen = true;
  
 -#if defined Q_OS_LINUX && !defined _DEBUG // fix first version
@@ -24,15 +24,16 @@
  	} else {
  		cForceWorkingDir(psAppDataPath());
  		workingDirChosen = true;
+ 
 -#elif defined OS_WIN_STORE // Q_OS_MAC || Q_OS_LINUX || Q_OS_WINRT
 +#elif defined OS_WIN_STORE // Q_OS_MAC || Q_OS_LINUX || Q_OS_WINRT || Q_OS_FREEBSD
  #ifdef _DEBUG
  		cForceWorkingDir(cExeDir());
  #else // _DEBUG
-@@ -368,7 +368,7 @@ void start(not_null<Core::Launcher*> lau
- 			cForceWorkingDir(cWorkingDir());
+@@ -374,7 +374,7 @@ void start(not_null<Core::Launcher*> lau
  			workingDirChosen = true;
  		}
+ 
 -#endif // Q_OS_MAC || Q_OS_LINUX || Q_OS_WINRT || OS_WIN_STORE
 +#endif // Q_OS_MAC || Q_OS_LINUX || Q_OS_WINRT || OS_WIN_STORE || Q_OS_FREEBSD
  	}
