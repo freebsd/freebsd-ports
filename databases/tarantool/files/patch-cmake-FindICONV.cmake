a7d36fc9405226d35dbcafa6c39f020a1ddfc614
sunpoet <sunpoet@FreeBSD.org>
2018-10-14 16:27:45 +0000

    Update to 1.10.2
    Changes:	https://github.com/tarantool/tarantool/releases

--- databases/tarantool/files/patch-cmake-FindICONV.cmake
+++ databases/tarantool/files/patch-cmake-FindICONV.cmake
@@ -1,18 +1,11 @@
---- cmake/FindICONV.cmake.orig	2018-09-06 16:31:05 UTC
+--- cmake/FindICONV.cmake.orig	2018-10-12 19:53:35 UTC
 +++ cmake/FindICONV.cmake
-@@ -4,14 +4,8 @@
+@@ -4,7 +4,7 @@
  # ICONV_LIBRARIES
  #
  
 -if (TARGET_OS_LINUX)
 +if (TARGET_OS_FREEBSD OR TARGET_OS_LINUX)
      set(ICONV_LIBRARY "")
--else()
--    find_library(ICONV_LIBRARY iconv)
--    if(NOT ICONV_LIBRARY)
--        message(WARNING "iconv library not found")
--        set(ICONV_LIBRARY "")
--    endif()
- endif()
- find_path(ICONV_INCLUDE_DIR iconv.h)
- if(NOT ICONV_INCLUDE_DIR)
+ else()
+     find_library(ICONV_LIBRARY iconv)
