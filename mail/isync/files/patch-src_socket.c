04007e91b5153e5431cdaf375488bc6325cc2207
Emanuel Haupt <ehaupt@FreeBSD.org>
2018-10-16 07:45:53 +0000

    MFH: r482205
    - Fix build with openssl 1.1.1 / unbreak on 12.0-ALPHA9 [1]
    - Merge a patch from upstream as some providers seem to require SNI when
      TLSv1.3 is used
    - Pacify portlint
    
    PR:		231776
    Submitted by:	Sascha Holzleiter <sascha@root-login.org>
    Notified by:	pkg-fallout [1]
    Approved by:	ports-secteam (blanket)

--- mail/isync/files/patch-src_socket.c
+++ mail/isync/files/patch-src_socket.c
@@ -1,4 +1,4 @@
---- src/socket.c.orig	2017-08-13 23:04:40 UTC
+--- src/socket.c.orig	2017-10-01 08:42:35 UTC
 +++ src/socket.c
 @@ -40,7 +40,7 @@
  # include <openssl/ssl.h>
@@ -9,3 +9,12 @@
  #  define X509_OBJECT_get0_X509(o) ((o)->data.x509)
  #  define X509_STORE_get0_objects(o) ((o)->objs)
  # endif
+@@ -270,6 +270,8 @@ socket_start_tls( conn_t *conn, void (*c
+ 
+ 	init_wakeup( &conn->ssl_fake, ssl_fake_cb, conn );
+ 	conn->ssl = SSL_new( ((server_conf_t *)conn->conf)->SSLContext );
++	if (ssl_return( "set server name", conn, SSL_set_tlsext_host_name( conn->ssl, conn->conf->host ) ) < 0)
++		return;
+ 	SSL_set_fd( conn->ssl, conn->fd );
+ 	SSL_set_mode( conn->ssl, SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER );
+ 	socket_expect_read( conn, 1 );
