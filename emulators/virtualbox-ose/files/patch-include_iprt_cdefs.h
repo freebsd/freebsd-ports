27785eaba9c34c0d0af2dc972bd071ebe88cfb11
Jung-uk Kim <jkim@FreeBSD.org>
2018-10-17 06:49:30 +0000

    Update to 5.2.20.
    https://www.virtualbox.org/wiki/Changelog#20

--- emulators/virtualbox-ose/files/patch-include_iprt_cdefs.h
+++ emulators/virtualbox-ose/files/patch-include_iprt_cdefs.h
@@ -1,50 +1,6 @@
---- include/iprt/cdefs.h.orig	2018-01-15 14:49:42 UTC
+--- include/iprt/cdefs.h.orig	2018-10-15 14:24:52 UTC
 +++ include/iprt/cdefs.h
-@@ -2214,6 +2214,8 @@
-  */
- #if defined(__cplusplus) && RT_GNUC_PREREQ(4, 4)
- # define RT_OFFSETOF(type, member)              ( (int)(uintptr_t)&( ((type *)(void *)0x1000)->member) - 0x1000 )
-+#elif RT_CLANG_PREREQ(6, 0)
-+# define RT_OFFSETOF(type, member)              ( (int)__builtin_offsetof(type, member) )
- #else
- # define RT_OFFSETOF(type, member)              ( (int)(uintptr_t)&( ((type *)(void *)0)->member) )
- #endif
-@@ -2232,6 +2234,8 @@
-  */
- #if defined(__cplusplus) && RT_GNUC_PREREQ(4, 4)
- # define RT_UOFFSETOF(type, member)             ( (uintptr_t)&( ((type *)(void *)0x1000)->member) - 0x1000 )
-+#elif RT_CLANG_PREREQ(6, 0)
-+# define RT_UOFFSETOF(type, member)             ( (unsigned)__builtin_offsetof(type, member) )
- #else
- # define RT_UOFFSETOF(type, member)             ( (uintptr_t)&( ((type *)(void *)0)->member) )
- #endif
-@@ -2244,7 +2248,11 @@
-  * @param   member  Member.
-  * @param   addend  The addend to add to the offset.
-  */
--#define RT_OFFSETOF_ADD(type, member, addend)   ( (int)RT_UOFFSETOF_ADD(type, member, addend) )
-+#if RT_CLANG_PREREQ(6, 0)
-+# define RT_OFFSETOF_ADD(type, member, addend)  ( (int)(__builtin_offsetof(type, member) + addend) )
-+#else 
-+# define RT_OFFSETOF_ADD(type, member, addend)  ( (int)RT_UOFFSETOF_ADD(type, member, addend) )
-+#endif
- 
- /** @def RT_UOFFSETOF_ADD
-  * RT_UOFFSETOF with an addend.
-@@ -2254,7 +2262,11 @@
-  * @param   member  Member.
-  * @param   addend  The addend to add to the offset.
-  */
--#define RT_UOFFSETOF_ADD(type, member, addend)  ( (uintptr_t)&( ((type *)(void *)(uintptr_t)(addend))->member) )
-+#if RT_CLANG_PREREQ(6, 0)
-+# define RT_UOFFSETOF_ADD(type, member, addend) ( (unsigned)(__builtin_offsetof(type, member) + addend) )
-+#else
-+# define RT_UOFFSETOF_ADD(type, member, addend) ( (uintptr_t)&( ((type *)(void *)(uintptr_t)(addend))->member) )
-+#endif
- 
- /** @def RT_SIZEOFMEMB
-  * Get the size of a structure member.
-@@ -2517,28 +2529,28 @@
+@@ -2611,28 +2611,28 @@
  
  /** @def RT_BYTE1
   * Gets the first byte of something. */
