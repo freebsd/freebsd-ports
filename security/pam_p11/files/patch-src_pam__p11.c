0554340259f7ba500f1d940ca9f6950ef11823c0
ale <ale@FreeBSD.org>
2018-10-18 08:00:37 +0000

    Fix build on i386.

--- security/pam_p11/files/patch-src_pam__p11.c
+++ security/pam_p11/files/patch-src_pam__p11.c
@@ -1,4 +1,4 @@
---- src/pam_p11.c.orig	2018-10-16 10:00:15 UTC
+--- src/pam_p11.c.orig	2018-05-04 14:52:04 UTC
 +++ src/pam_p11.c
 @@ -53,7 +53,7 @@
  #include <security/pam_ext.h>
@@ -9,3 +9,12 @@
  		const char *fmt, va_list args)
  {
  	int r = PAM_CRED_INSUFFICIENT;
+@@ -549,7 +549,7 @@ static int key_find(pam_handle_t *pamh, 
+ 	return 0;
+ }
+ 
+-static int randomize(pam_handle_t *pamh, unsigned char *r, unsigned int r_len)
++static int randomize(pam_handle_t *pamh, unsigned char *r, int r_len)
+ {
+ 	int ok = 0;
+ 	int fd = open("/dev/urandom", O_RDONLY);
