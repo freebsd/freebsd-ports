d32f47f8288193d64eff89f0a244159860205d5c
Matthias Fechner <mfechner@FreeBSD.org>
2018-10-20 10:19:29 +0000

    Fixed problems with OpenSSL 1.1.1 and an error message regarding lua.
    PR:		223568
    Submitted by:	freebsd-ports@dan.me.uk
    Reported by:	pi@FreeBSD.org
    Reviewed by:	mfechner@FreeBSD.org
    Approved by:	mentors (implicit)

--- mail/opendkim/files/patch-libopendkim_dkim-canon.c
+++ mail/opendkim/files/patch-libopendkim_dkim-canon.c
@@ -1,6 +1,6 @@
---- libopendkim/dkim-canon.c.orig	2015-08-26 08:37:01 UTC
+--- libopendkim/dkim-canon.c.orig	2015-05-11 03:56:13 UTC
 +++ libopendkim/dkim-canon.c
-@@ -388,7 +388,7 @@ dkim_canon_header_string(struct dkim_dst
+@@ -388,7 +388,7 @@ dkim_canon_header_string(struct dkim_dstring *dstr, dk
  		}
  
  		/* skip all spaces before first word */
