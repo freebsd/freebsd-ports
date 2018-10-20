c170a2fa7b388913c2dc54d60bbb2a7eae83de57
Yuri Victorovich <yuri@FreeBSD.org>
2018-10-20 16:10:00 +0000

    www/py-pywikibot: Update 3.0.20180403 -> 3.0.20180922
    Reported by:	repology

--- www/py-pywikibot/files/patch-setup.py
+++ www/py-pywikibot/files/patch-setup.py
@@ -1,25 +1,31 @@
---- setup.py.orig	2018-03-05 08:16:49 UTC
+--- setup.py.orig	2018-10-20 16:02:34 UTC
 +++ setup.py
-@@ -213,14 +213,14 @@ from setuptools import setup, find_packa
- name = 'pywikibot'
- version = '3.0'
+@@ -190,17 +190,17 @@ else:
+ def get_version():
+     """Get a valid pywikibot module version string."""
+     version = '3.0'
+-    try:
+-        import subprocess
+-        date = subprocess.check_output(
+-            ['git', 'log', '-1', '--format=%ci']).strip()
+-        date = date.decode().split(' ')[0].replace('-', '')
+-        version += '.' + date
+-        if 'sdist' not in sys.argv:
+-            version += '.dev0'
+-    except Exception as e:
+-        print(e)
+-        version += '.dev0'
++    #try:
++    #    import subprocess
++    #    date = subprocess.check_output(
++    #        ['git', 'log', '-1', '--format=%ci']).strip()
++    #    date = date.decode().split(' ')[0].replace('-', '')
++    #    version += '.' + date
++    #    if 'sdist' not in sys.argv:
++    #        version += '.dev0'
++    #except Exception as e:
++    #    print(e)
++    #    version += '.dev0'
+     return version
  
--try:
--    import subprocess
--    date = subprocess.check_output(['git', 'log', '-1', '--format=%ci']).strip()
--    date = date.decode().split(' ')[0].replace('-', '')
--    version = version + "." + date
--except Exception as e:
--    print(e)
--    version = version + "-dev"
-+#try:
-+#    import subprocess
-+#    date = subprocess.check_output(['git', 'log', '-1', '--format=%ci']).strip()
-+#    date = date.decode().split(' ')[0].replace('-', '')
-+#    version = version + "." + date
-+#except Exception as e:
-+#    print(e)
-+#    version = version + "-dev"
  
- 
- def read_desc(filename):
