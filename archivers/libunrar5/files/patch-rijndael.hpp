0adbf447a156079ebfe81ad4c17eeeed32505aa9
Antoine Brodin <antoine@FreeBSD.org>
2018-10-20 05:38:06 +0000

    MFH: r481583 r481881
    archivers/libunrar5: Update to 5.6.8
    
    Bump PORTREVISION on sysutils/fusefs-rar2fs which builds against the source
    
    Fix build with OpenSSL 1.1.x
    
    Reported by:	pkg-fallout (head)

--- archivers/libunrar5/files/patch-rijndael.hpp
+++ archivers/libunrar5/files/patch-rijndael.hpp
@@ -1,16 +1,20 @@
---- rijndael.hpp.orig	2017-04-28 17:28:47 UTC
+--- rijndael.hpp.orig	2018-06-24 15:10:31 UTC
 +++ rijndael.hpp
-@@ -16,6 +16,9 @@
+@@ -16,6 +16,13 @@
  class Rijndael
  { 
    private:
 +#ifdef OPENSSL_AES
++#if OPENSSL_VERSION_NUMBER < 0x10100000L
 +    EVP_CIPHER_CTX ctx;
++#else
++    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
++#endif
 +#else // OPENSSL_AES
  #ifdef USE_SSE
      void blockEncryptSSE(const byte *input,size_t numBlocks,byte *outBuffer);
      void blockDecryptSSE(const byte *input, size_t numBlocks, byte *outBuffer);
-@@ -25,6 +28,7 @@ class Rijndael
+@@ -25,6 +32,7 @@ class Rijndael
      void keySched(byte key[_MAX_KEY_COLUMNS][4]);
      void keyEncToDec();
      void GenerateTables();
