9b238fe9cd99162374fd80d76d0fd10b13aea53b
Matthias Fechner <mfechner@FreeBSD.org>
2018-10-20 11:33:29 +0000

    Add PHP flavor support. Add LICENSE Fix portlint warnings (strip .so file, fix @exec in plist) Regenerate patches with makepatch Change maintainer as requested.
    PR:		227378
    Submitted by:	freebsd-ports@dan.me.uk
    Reported by:	freebsd-ports@dan.me.uk
    Reviewed by:	mfechner@FreeBSD.org
    Approved by:	maintainer timeout

--- www/suphp/files/patch-src_SmartPtr.hpp
+++ www/suphp/files/patch-src_SmartPtr.hpp
@@ -1,8 +1,4 @@
-$NetBSD: patch-src_SmartPtr.hpp,v 1.1 2011/09/21 12:54:16 adam Exp $
-
-Fix C++ syntax
-
---- src/SmartPtr.hpp.orig	2011-09-21 12:48:02.000000000 +0000
+--- src/SmartPtr.hpp.orig	2007-03-13 14:17:02 UTC
 +++ src/SmartPtr.hpp
 @@ -150,7 +150,7 @@ namespace suPHP {
      template<class T>
