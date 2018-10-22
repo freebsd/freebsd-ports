ab62af51ae36448946479e767d7e72f38a4ab87b
thierry <thierry@FreeBSD.org>
2018-10-22 20:08:54 +0000

    Upgrade to 0.7.1.
    Approved by:	maintainer time-out

--- korean/hunspell/files/patch-tests_runtest.py
+++ korean/hunspell/files/patch-tests_runtest.py
@@ -1,11 +1,11 @@
---- tests/runtest.py.orig	2012-10-06 23:29:32 UTC
+--- tests/runtest.py.orig	2017-12-25 07:30:13 UTC
 +++ tests/runtest.py
-@@ -19,7 +19,7 @@ def main():
+@@ -27,7 +27,7 @@ def main():
          hunspell_cmd = './hunspell'
      else:
          hunspell_cmd = 'hunspell'
--    hunspell = subprocess.Popen([hunspell_cmd, '-i', 'UTF-8', '-a', '-d', '../ko'],
-+    hunspell = subprocess.Popen([hunspell_cmd, '-i', 'UTF-8', '-a', '-d', '../ko_KR'],
+-    args = [hunspell_cmd, '-i', 'UTF-8', '-d', '../ko']
++    args = [hunspell_cmd, '-i', 'UTF-8', '-d', '../ko_KR']
+     hunspell = subprocess.Popen(args,
                                  stdin=subprocess.PIPE, stdout=subprocess.PIPE)
      hunspell.stdout.readline()  # the first line "Hunspell 1.2.8 ..."
-     lineno = 0
