d0a9b751ca76ad17d0d1de586e2f41c5b68e79f7
Olli Hauer <ohauer@FreeBSD.org>
2018-10-22 16:41:14 +0000

    - fix build on FreeBSD 13-CURRENT
    PR:		232502
    Submitted by:	Kai

--- mail/postfix-current/files/patch-src_util_sys__defs.h
+++ mail/postfix-current/files/patch-src_util_sys__defs.h
@@ -5,7 +5,7 @@
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
      || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
 -    || defined(FREEBSD11) \
-+    || defined(FREEBSD11) || defined(FREEBSD12) \
++    || defined(FREEBSD11) || defined(FREEBSD12) || defined(FREEBSD13) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(OPENBSD5) || defined(OPENBSD6) \
