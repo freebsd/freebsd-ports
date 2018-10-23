eca83706aad236974ce7d53053a2dfa14fd47abb
rene <rene@FreeBSD.org>
2018-10-23 10:14:09 +0000

    Remove expired ports: 2018-10-22 devel/hs-enumerator: Doesn't build with recent GHC version 2018-10-22 net-im/hs-haskell-xmpp: Doesn't build with recent GHC version 2018-10-22 security/hs-certificate: Doesn't build with recent GHC version 2018-10-22 textproc/hs-attoparsec-enumerator: Doesn't build with recent GHC version 2018-10-22 textproc/hs-HaXml: Doesn't build with recent GHC version 2018-10-22 www/hs-happstack: Doesn't build with recent GHC version 2018-10-22 x11-toolkits/hs-wxc: Doesn't build with recent GHC version 2018-10-22 x11-toolkits/hs-wx: Doesn't build with recent GHC version 2018-10-22 archivers/hs-zlib-enum: Doesn't build with recent GHC version 2018-10-22 x11-toolkits/hs-wxcore: Doesn't build with recent GHC version 2018-10-22 devel/hs-TypeCompose: Doesn't build with recent GHC version 2018-10-22 devel/hs-PSQueue: Doesn't build with recent GHC version 2018-10-22 devel/hs-blaze-builder-enumerator: Doesn't build with recent GHC version

--- net-im/hs-haskell-xmpp/files/patch-src_Network_XMPP_Stream.hs
+++
@@ -1,7 +0,0 @@
---- src/Network/XMPP/Stream.hs.orig	2012-08-30 21:44:36 UTC
-+++ src/Network/XMPP/Stream.hs
-@@ -1,3 +1,4 @@
-+{-# LANGUAGE FlexibleContexts #-}
- -----------------------------------------------------------------------------
- -- |
- -- Module      :  Network.XMPP.Stream
--- x11-toolkits/hs-wx/files/patch-src_Graphics_UI_WX_Window.hs
+++
@@ -1,8 +0,0 @@
---- src/Graphics/UI/WX/Window.hs.orig	2017-11-09 22:05:23 UTC
-+++ src/Graphics/UI/WX/Window.hs
-@@ -1,4 +1,4 @@
--{-# LANGUAGE FlexibleContexts      #-}
-+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, FlexibleContexts #-}
- {-# LANGUAGE FlexibleInstances     #-}
- {-# LANGUAGE TypeSynonymInstances  #-}
- --------------------------------------------------------------------------------
