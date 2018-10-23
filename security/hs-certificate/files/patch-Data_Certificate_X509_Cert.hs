72aaa3f72fa328203435e51a1885239fe5d700bc
Rene Ladan <rene@FreeBSD.org>
2018-10-23 10:14:09 +0000

    Remove expired ports: 2018-10-22 devel/hs-enumerator: Doesn't build with recent GHC version 2018-10-22 net-im/hs-haskell-xmpp: Doesn't build with recent GHC version 2018-10-22 security/hs-certificate: Doesn't build with recent GHC version 2018-10-22 textproc/hs-attoparsec-enumerator: Doesn't build with recent GHC version 2018-10-22 textproc/hs-HaXml: Doesn't build with recent GHC version 2018-10-22 www/hs-happstack: Doesn't build with recent GHC version 2018-10-22 x11-toolkits/hs-wxc: Doesn't build with recent GHC version 2018-10-22 x11-toolkits/hs-wx: Doesn't build with recent GHC version 2018-10-22 archivers/hs-zlib-enum: Doesn't build with recent GHC version 2018-10-22 x11-toolkits/hs-wxcore: Doesn't build with recent GHC version 2018-10-22 devel/hs-TypeCompose: Doesn't build with recent GHC version 2018-10-22 devel/hs-PSQueue: Doesn't build with recent GHC version 2018-10-22 devel/hs-blaze-builder-enumerator: Doesn't build with recent GHC version

--- security/hs-certificate/files/patch-Data_Certificate_X509_Cert.hs
+++
@@ -1,7 +0,0 @@
---- Data/Certificate/X509/Cert.hs.orig	2013-10-07 02:22:00 UTC
-+++ Data/Certificate/X509/Cert.hs
-@@ -1,3 +1,4 @@
-+{-# LANGUAGE FlexibleContexts #-}
- module Data.Certificate.X509.Cert
-         (
-         -- * Data Structure
--- security/hs-certificate/files/patch-Data_Certificate_X509_Ext.hs
+++
@@ -1,7 +0,0 @@
---- Data/Certificate/X509/Ext.hs.orig	2013-10-07 02:22:00 UTC
-+++ Data/Certificate/X509/Ext.hs
-@@ -1,3 +1,4 @@
-+{-# LANGUAGE FlexibleContexts #-}
- -- |
- -- Module      : Data.Certificate.X509.Ext
- -- License     : BSD-style
