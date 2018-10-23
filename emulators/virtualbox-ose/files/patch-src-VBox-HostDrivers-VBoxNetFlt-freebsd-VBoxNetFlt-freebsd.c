ee997424aa569fc6ad48fe84b5efe7d461c5a42c
jkim <jkim@FreeBSD.org>
2018-10-23 19:41:50 +0000

    MFH:	r482464
    Support SMAP for amd64.
    
    This should stop kernel panics on SMAP supported CPUs after r336876.
    
    PR:		230460
    Approved by:	ports-secteam (miwi)

--- emulators/virtualbox-ose/files/patch-src-VBox-HostDrivers-VBoxNetFlt-freebsd-VBoxNetFlt-freebsd.c
+++ emulators/virtualbox-ose/files/patch-src-VBox-HostDrivers-VBoxNetFlt-freebsd-VBoxNetFlt-freebsd.c
@@ -1,11 +1,6 @@
-Add VLAN trunking support to vboxnetflt
-
-See:		http://lists.freebsd.org/pipermail/freebsd-emulation/2012-April/009698.html
-See:		http://lists.freebsd.org/pipermail/freebsd-emulation/2013-May/010605.html
-Submitted by:	Landon J Fuller <landonf at plausible.coop>
---- src/VBox/HostDrivers/VBoxNetFlt/freebsd/VBoxNetFlt-freebsd.c.orig	2016-01-19 19:18:38 UTC
+--- src/VBox/HostDrivers/VBoxNetFlt/freebsd/VBoxNetFlt-freebsd.c.orig	2018-10-15 14:30:58 UTC
 +++ src/VBox/HostDrivers/VBoxNetFlt/freebsd/VBoxNetFlt-freebsd.c
-@@ -51,6 +51,7 @@
+@@ -52,6 +52,7 @@
  #include <net/if_dl.h>
  #include <net/if_types.h>
  #include <net/ethernet.h>
@@ -13,7 +8,15 @@ Submitted by:	Landon J Fuller <landonf at plausible.coop>
  
  #include <netgraph/ng_message.h>
  #include <netgraph/netgraph.h>
-@@ -369,7 +370,11 @@ static int ng_vboxnetflt_rcvdata(hook_p 
+@@ -73,6 +74,7 @@
+ 
+ #define VBOXNETFLT_OS_SPECFIC 1
+ #include "../VBoxNetFltInternal.h"
++#include "freebsd/the-freebsd-kernel.h"
+ 
+ static int vboxnetflt_modevent(struct module *, int, void *);
+ static ng_constructor_t    ng_vboxnetflt_constructor;
+@@ -370,7 +372,11 @@ static int ng_vboxnetflt_rcvdata(hook_p hook, item_p i
          mtx_lock_spin(&pThis->u.s.inq.ifq_mtx);
          _IF_ENQUEUE(&pThis->u.s.inq, m);
          mtx_unlock_spin(&pThis->u.s.inq.ifq_mtx);
@@ -25,7 +28,7 @@ Submitted by:	Landon J Fuller <landonf at plausible.coop>
      }
      /*
       * Handle mbufs on the outgoing hook, frames going to the interface
-@@ -387,7 +392,11 @@ static int ng_vboxnetflt_rcvdata(hook_p 
+@@ -388,7 +394,11 @@ static int ng_vboxnetflt_rcvdata(hook_p hook, item_p i
          mtx_lock_spin(&pThis->u.s.outq.ifq_mtx);
          _IF_ENQUEUE(&pThis->u.s.outq, m);
          mtx_unlock_spin(&pThis->u.s.outq.ifq_mtx);
@@ -37,7 +40,7 @@ Submitted by:	Landon J Fuller <landonf at plausible.coop>
      }
      else
      {
-@@ -427,6 +436,8 @@ static void vboxNetFltFreeBSDinput(void 
+@@ -428,6 +438,8 @@ static void vboxNetFltFreeBSDinput(void *arg, int pend
      struct ifnet *ifp = pThis->u.s.ifp;
      unsigned int cSegs = 0;
      bool fDropIt = false, fActive;
@@ -46,7 +49,7 @@ Submitted by:	Landon J Fuller <landonf at plausible.coop>
      PINTNETSG pSG;
  
      VBOXCURVNET_SET(ifp->if_vnet);
-@@ -439,6 +450,19 @@ static void vboxNetFltFreeBSDinput(void 
+@@ -440,6 +452,19 @@ static void vboxNetFltFreeBSDinput(void *arg, int pend
          if (m == NULL)
              break;
  
@@ -66,7 +69,7 @@ Submitted by:	Landon J Fuller <landonf at plausible.coop>
          for (m0 = m; m0 != NULL; m0 = m0->m_next)
              if (m0->m_len > 0)
                  cSegs++;
-@@ -453,6 +477,27 @@ static void vboxNetFltFreeBSDinput(void 
+@@ -454,6 +479,27 @@ static void vboxNetFltFreeBSDinput(void *arg, int pend
          vboxNetFltFreeBSDMBufToSG(pThis, m, pSG, cSegs, 0);
          fDropIt = pThis->pSwitchPort->pfnRecv(pThis->pSwitchPort, NULL /* pvIf */, pSG, INTNETTRUNKDIR_WIRE);
          RTMemTmpFree(pSG);
@@ -94,3 +97,177 @@ Submitted by:	Landon J Fuller <landonf at plausible.coop>
          if (fDropIt)
              m_freem(m);
          else
+@@ -513,6 +559,7 @@ static void vboxNetFltFreeBSDoutput(void *arg, int pen
+  */
+ int vboxNetFltPortOsXmit(PVBOXNETFLTINS pThis, void *pvIfData, PINTNETSG pSG, uint32_t fDst)
+ {
++    IPRT_FREEBSD_SAVE_EFL_AC();
+     NOREF(pvIfData);
+ 
+     void (*input_f)(struct ifnet *, struct mbuf *);
+@@ -529,10 +576,16 @@ int vboxNetFltPortOsXmit(PVBOXNETFLTINS pThis, void *p
+     {
+         m = vboxNetFltFreeBSDSGMBufFromSG(pThis, pSG);
+         if (m == NULL)
++        {
++            IPRT_FREEBSD_RESTORE_EFL_AC();
+             return VERR_NO_MEMORY;
++        }
+         m = m_pullup(m, ETHER_HDR_LEN);
+         if (m == NULL)
++        {
++            IPRT_FREEBSD_RESTORE_EFL_AC();
+             return VERR_NO_MEMORY;
++        }
+ 
+         m->m_flags |= M_PKTHDR;
+         ether_output_frame(ifp, m);
+@@ -542,10 +595,16 @@ int vboxNetFltPortOsXmit(PVBOXNETFLTINS pThis, void *p
+     {
+         m = vboxNetFltFreeBSDSGMBufFromSG(pThis, pSG);
+         if (m == NULL)
++        {
++            IPRT_FREEBSD_RESTORE_EFL_AC();
+             return VERR_NO_MEMORY;
++        }
+         m = m_pullup(m, ETHER_HDR_LEN);
+         if (m == NULL)
++        {
++            IPRT_FREEBSD_RESTORE_EFL_AC();
+             return VERR_NO_MEMORY;
++        }
+         /*
+          * Delivering packets to the host will be captured by the
+          * input hook. Tag the packet with a mbuf tag so that we
+@@ -556,6 +615,7 @@ int vboxNetFltPortOsXmit(PVBOXNETFLTINS pThis, void *p
+         if (mtag == NULL)
+         {
+             m_freem(m);
++            IPRT_FREEBSD_RESTORE_EFL_AC();
+             return VERR_NO_MEMORY;
+         }
+ 
+@@ -566,6 +626,7 @@ int vboxNetFltPortOsXmit(PVBOXNETFLTINS pThis, void *p
+         ifp->if_input(ifp, m);
+     }
+     VBOXCURVNET_RESTORE();
++    IPRT_FREEBSD_RESTORE_EFL_AC();
+     return VINF_SUCCESS;
+ }
+ 
+@@ -578,6 +639,7 @@ static bool vboxNetFltFreeBsdIsPromiscuous(PVBOXNETFLT
+ 
+ int vboxNetFltOsInitInstance(PVBOXNETFLTINS pThis, void *pvContext)
+ {
++    IPRT_FREEBSD_SAVE_EFL_AC();
+     char nam[NG_NODESIZ];
+     struct ifnet *ifp;
+     node_p node;
+@@ -586,7 +648,10 @@ int vboxNetFltOsInitInstance(PVBOXNETFLTINS pThis, voi
+     NOREF(pvContext);
+     ifp = ifunit(pThis->szName);
+     if (ifp == NULL)
++    {
++        IPRT_FREEBSD_RESTORE_EFL_AC();
+         return VERR_INTNET_FLT_IF_NOT_FOUND;
++    }
+ 
+     /* Create a new netgraph node for this instance */
+     if (ng_make_node_common(&ng_vboxnetflt_typestruct, &node) != 0)
+@@ -630,12 +695,14 @@ int vboxNetFltOsInitInstance(PVBOXNETFLTINS pThis, voi
+         vboxNetFltRelease(pThis, true /*fBusy*/);
+     }
+     VBOXCURVNET_RESTORE();
++    IPRT_FREEBSD_RESTORE_EFL_AC();
+ 
+     return VINF_SUCCESS;
+ }
+ 
+ bool vboxNetFltOsMaybeRediscovered(PVBOXNETFLTINS pThis)
+ {
++    IPRT_FREEBSD_SAVE_EFL_AC();
+     struct ifnet *ifp, *ifp0;
+ 
+     ifp = ASMAtomicUoReadPtrT(&pThis->u.s.ifp, struct ifnet *);
+@@ -652,6 +719,7 @@ bool vboxNetFltOsMaybeRediscovered(PVBOXNETFLTINS pThi
+         pThis->u.s.node = NULL;
+     }
+     VBOXCURVNET_RESTORE();
++    IPRT_FREEBSD_RESTORE_EFL_AC();
+ 
+     if (ifp0 != NULL)
+     {
+@@ -664,6 +732,7 @@ bool vboxNetFltOsMaybeRediscovered(PVBOXNETFLTINS pThi
+ 
+ void vboxNetFltOsDeleteInstance(PVBOXNETFLTINS pThis)
+ {
++    IPRT_FREEBSD_SAVE_EFL_AC();
+ 
+     taskqueue_drain(taskqueue_fast, &pThis->u.s.tskin);
+     taskqueue_drain(taskqueue_fast, &pThis->u.s.tskout);
+@@ -676,6 +745,7 @@ void vboxNetFltOsDeleteInstance(PVBOXNETFLTINS pThis)
+         ng_rmnode_self(pThis->u.s.node);
+     VBOXCURVNET_RESTORE();
+     pThis->u.s.node = NULL;
++    IPRT_FREEBSD_RESTORE_EFL_AC();
+ }
+ 
+ int vboxNetFltOsPreInitInstance(PVBOXNETFLTINS pThis)
+@@ -689,6 +759,7 @@ int vboxNetFltOsPreInitInstance(PVBOXNETFLTINS pThis)
+ 
+ void vboxNetFltPortOsSetActive(PVBOXNETFLTINS pThis, bool fActive)
+ {
++    IPRT_FREEBSD_SAVE_EFL_AC();
+     struct ifnet *ifp;
+     struct ifreq ifreq;
+     int error;
+@@ -722,7 +793,10 @@ void vboxNetFltPortOsSetActive(PVBOXNETFLTINS pThis, b
+         NG_MKMESSAGE(msg, NGM_GENERIC_COOKIE, NGM_CONNECT,
+             sizeof(struct ngm_connect), M_NOWAIT);
+         if (msg == NULL)
++        {
++            IPRT_FREEBSD_RESTORE_EFL_AC();
+             return;
++        }
+         con = (struct ngm_connect *)msg->data;
+         snprintf(con->path, NG_PATHSIZ, "vboxnetflt_%s:", ifp->if_xname);
+         strlcpy(con->ourhook, "lower", NG_HOOKSIZ);
+@@ -736,7 +810,10 @@ void vboxNetFltPortOsSetActive(PVBOXNETFLTINS pThis, b
+         NG_MKMESSAGE(msg, NGM_GENERIC_COOKIE, NGM_CONNECT,
+             sizeof(struct ngm_connect), M_NOWAIT);
+         if (msg == NULL)
++        {
++            IPRT_FREEBSD_RESTORE_EFL_AC();
+             return;
++        }
+         con = (struct ngm_connect *)msg->data;
+         snprintf(con->path, NG_PATHSIZ, "vboxnetflt_%s:",
+             ifp->if_xname);
+@@ -759,7 +836,10 @@ void vboxNetFltPortOsSetActive(PVBOXNETFLTINS pThis, b
+         NG_MKMESSAGE(msg, NGM_GENERIC_COOKIE, NGM_RMHOOK,
+             sizeof(struct ngm_rmhook), M_NOWAIT);
+         if (msg == NULL)
++        {
++            IPRT_FREEBSD_RESTORE_EFL_AC();
+             return;
++        }
+         rm = (struct ngm_rmhook *)msg->data;
+         strlcpy(rm->ourhook, "input", NG_HOOKSIZ);
+         NG_SEND_MSG_PATH(error, node, msg, path, 0);
+@@ -770,12 +850,16 @@ void vboxNetFltPortOsSetActive(PVBOXNETFLTINS pThis, b
+         NG_MKMESSAGE(msg, NGM_GENERIC_COOKIE, NGM_RMHOOK,
+             sizeof(struct ngm_rmhook), M_NOWAIT);
+         if (msg == NULL)
++        {
++            IPRT_FREEBSD_RESTORE_EFL_AC();
+             return;
++        }
+         rm = (struct ngm_rmhook *)msg->data;
+         strlcpy(rm->ourhook, "output", NG_HOOKSIZ);
+         NG_SEND_MSG_PATH(error, node, msg, path, 0);
+     }
+     VBOXCURVNET_RESTORE();
++    IPRT_FREEBSD_RESTORE_EFL_AC();
+ }
+ 
+ int vboxNetFltOsDisconnectIt(PVBOXNETFLTINS pThis)
--- emulators/virtualbox-ose/files/patch-src_VBox_Runtime_r0drv_freebsd_the-freebsd-kernel.h
+++ emulators/virtualbox-ose/files/patch-src_VBox_Runtime_r0drv_freebsd_the-freebsd-kernel.h
@@ -1,4 +1,4 @@
---- src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h.orig	2017-04-18 09:53:54 UTC
+--- src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h.orig	2018-10-15 14:31:31 UTC
 +++ src/VBox/Runtime/r0drv/freebsd/the-freebsd-kernel.h
 @@ -60,6 +60,7 @@
  #include <sys/smp.h>
@@ -16,3 +16,28 @@
  #include <sys/resourcevar.h>
  #include <machine/cpu.h>
  
+@@ -114,6 +114,24 @@
+  */
+ #if 0 /** @todo Not available yet. */
+ # define USE_KMEM_ALLOC_PROT
++#endif
++
++/** 
++ * Macros for preserving EFLAGS.AC (despair / paranoid)
++ */
++#if __FreeBSD_version >= 1200076 && defined(RT_ARCH_AMD64)
++# include <iprt/asm-amd64-x86.h>
++# include <iprt/x86.h>
++# define IPRT_FREEBSD_SAVE_EFL_AC()             RTCCUINTREG const fSavedEfl = ASMGetFlags()
++# define IPRT_FREEBSD_RESTORE_EFL_AC()          ASMSetFlags(fSavedEfl)
++# define IPRT_FREEBSD_RESTORE_EFL_ONLY_AC()     ASMChangeFlags(~X86_EFL_AC, fSavedEfl & X86_EFL_AC)
++# define IPRT_FREEBSD_RESTORE_EFL_ONLY_AC_EX(f) ASMChangeFlags(~X86_EFL_AC, (f) & X86_EFL_AC)
++# define VBOX_WITH_EFLAGS_AC_SET_IN_VBOXDRV
++#else
++# define IPRT_FREEBSD_SAVE_EFL_AC()             do { } while (0)
++# define IPRT_FREEBSD_RESTORE_EFL_AC()          do { } while (0)
++# define IPRT_FREEBSD_RESTORE_EFL_ONLY_AC()     do { } while (0)
++# define IPRT_FREEBSD_RESTORE_EFL_ONLY_AC_EX(f) do { } while (0)
+ #endif
+ 
+ #endif
