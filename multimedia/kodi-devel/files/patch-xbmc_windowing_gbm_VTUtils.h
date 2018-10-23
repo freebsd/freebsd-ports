95624a1fc9505f49c9cb8b5128fd36e68386dce1
tobik <tobik@FreeBSD.org>
2018-10-23 14:16:15 +0000

    Update multimedia/kodi-devel to 18.0b4

---
+++ multimedia/kodi-devel/files/patch-xbmc_windowing_gbm_VTUtils.h
@@ -0,0 +1,11 @@
+--- xbmc/windowing/gbm/VTUtils.h.orig	2018-10-23 13:18:04 UTC
++++ xbmc/windowing/gbm/VTUtils.h
+@@ -10,6 +10,8 @@
+ 
+ #include "platform/posix/utils/FileHandle.h"
+ 
++#include <string>
++
+ namespace KODI
+ {
+ namespace WINDOWING
