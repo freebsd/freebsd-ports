a99259713338cfc099c588d123d7fa1caeb10670
sunpoet <sunpoet@FreeBSD.org>
2018-10-24 18:36:59 +0000

    MFH: r482556
    Update to 2.3.8
    
    - Move BROKEN_* upward
    - Move USES upward
    - Remove no-op MLINKS and create symlinks for manpages
    - Sort INSTALLED_SCRIPTS
    - Regenerate patch files with makepatch:
    
    Changes:	https://www.ruby-lang.org/en/news/2018/10/17/ruby-2-3-8-released/
    PR:		232438
    Submitted by:	Yasuhiro KIMURA <yasu@utahime.org>
    Security:	afc60484-0652-440e-b01a-5ef814747f06
    
    Approved by:	ports-secteam

--- lang/ruby23/files/patch-lib_rdoc_generator_json__index.rb
+++ lang/ruby23/files/patch-lib_rdoc_generator_json__index.rb
@@ -1,6 +1,6 @@
---- ./lib/rdoc/generator/json_index.rb.orig	2017-11-12 19:38:46.598341000 -0500
-+++ ./lib/rdoc/generator/json_index.rb	2017-11-12 19:38:59.759125000 -0500
-@@ -175,7 +175,7 @@
+--- lib/rdoc/generator/json_index.rb.orig	2015-12-22 12:08:13 UTC
++++ lib/rdoc/generator/json_index.rb
+@@ -175,7 +175,7 @@ class RDoc::Generator::JsonIndex
      debug_msg "Writing gzipped search index to %s" % outfile
  
      Zlib::GzipWriter.open(outfile) do |gz|
@@ -9,7 +9,7 @@
        gz.orig_name = search_index_file.basename.to_s
        gz.write search_index
        gz.close
-@@ -193,7 +193,7 @@
+@@ -193,7 +193,7 @@ class RDoc::Generator::JsonIndex
          debug_msg "Writing gzipped file to %s" % outfile
  
          Zlib::GzipWriter.open(outfile) do |gz|
