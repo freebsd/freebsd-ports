9568de505752b4be07621efb8f1aa25add3bac42
mat <mat@FreeBSD.org>
2018-10-24 08:05:48 +0000

    Update to 9.11.5.

--- dns/bind911/files/patch-bin_named_include_named_globals.h
+++ dns/bind911/files/patch-bin_named_include_named_globals.h
@@ -1,8 +1,8 @@
 We reference the pid file as being run/named/pid everywere else.
 
---- bin/named/include/named/globals.h.orig	2018-06-10 06:06:33 UTC
+--- bin/named/include/named/globals.h.orig	2018-10-06 01:36:17 UTC
 +++ bin/named/include/named/globals.h
-@@ -138,7 +138,7 @@ EXTERN isc_boolean_t		ns_g_forcelock		IN
+@@ -139,7 +139,7 @@ EXTERN bool		ns_g_forcelock		INIT(false)
  #if NS_RUN_PID_DIR
  EXTERN const char *		ns_g_defaultpidfile 	INIT(NS_LOCALSTATEDIR
  							     "/run/named/"
