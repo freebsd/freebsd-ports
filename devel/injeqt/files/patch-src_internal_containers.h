19fb9b432be999e8d61c7892e6e3a884bf85ff4f
Tobias C. Berner <tcberner@FreeBSD.org>
2018-10-27 11:50:58 +0000

    Fix build with GCC-based architectures for various kde@ ports.
    - Qt5 required c++11 support.
    
    Submitted by:	Piotr Kubaj <pkubaj@anongoth.pl>
    PR:		232726
    PR:		232725
    PR:		232723
    PR:		232722
    PR:		232721
    PR:		232720
    PR:		232719
    PR:		232697
    PR:		232682
    PR:		232669
    PR:		232668
    PR:		232667
    PR:		232666
    PR:		232665
    PR:		232664
    PR:		232662
    PR:		232661
    PR:		232655
    PR:		232654
    PR:		232652
    PR:		232648
    PR:		232647
    PR:		232641
    PR:		232640
    PR:		232638
    PR:		232635
    PR:		232634
    PR:		232632
    PR:		232631
    PR:		232630
    PR:		232629
    PR:		232628
    PR:		232627
    PR:		232625
    PR:		232624
    PR:		232623
    PR:		232614
    PR:		232613
    PR:		232612
    PR:		232611
    PR:		232610
    PR:		232609
    PR:		232607
    PR:		232606
    PR:		232605
    PR:		232604
    PR:		232603
    PR:		232602
    PR:		232597
    PR:		232589
    PR:		232585
    PR:		232584
    PR:		232583
    PR:		232581
    PR:		232580
    PR:		232579
    PR:		232578
    PR:		232576
    PR:		232575
    PR:		232574
    PR:		232573
    PR:		232570

---
+++ devel/injeqt/files/patch-src_internal_containers.h
@@ -0,0 +1,10 @@
+--- src/internal/containers.h.orig	2018-10-26 17:05:42 UTC
++++ src/internal/containers.h
+@@ -23,6 +23,7 @@
+ #include "internal.h"
+ 
+ #include <algorithm>
++#include <functional>
+ #include <vector>
+ 
+ /**
