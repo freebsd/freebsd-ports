2b397c318882906ed6f595dd7f6744de4a4f3873
brnrd <brnrd@FreeBSD.org>
2018-10-27 14:36:41 +0000

    www/apache24: Update to 2.4.37
     - Adds TLSv1.3 support with security/openssl111
    
    PR:		232687
    Submitted by:	Pascal Christen <pascal christen hostpoint.ch>
    Reported by:	Markus Kohlmeyer <rootservice gmail com>
    Reviewed by:	ohauer
    Approved by:	joneum
    Differential Revision:	https://reviews.freebsd.org/D17668

--- www/apache24/files/patch-modules_ssl_ssl__engine__init.c
+++ www/apache24/files/patch-modules_ssl_ssl__engine__init.c
@@ -1,6 +1,6 @@
 --- modules/ssl/ssl_engine_init.c.orig	2018-02-13 23:43:36 UTC
 +++ modules/ssl/ssl_engine_init.c
-@@ -546,7 +546,7 @@ static apr_status_t ssl_init_ctx_protoco
+@@ -547,7 +547,7 @@ static apr_status_t ssl_init_ctx_protoco
      char *cp;
      int protocol = mctx->protocol;
      SSLSrvConfigRec *sc = mySrvConfig(s);
@@ -9,4 +9,12 @@
      int prot;
  #endif
  
+@@ -1492,7 +1492,7 @@ static apr_status_t ssl_init_proxy_certs
+     X509_STORE_CTX *sctx;
+     X509_STORE *store = SSL_CTX_get_cert_store(mctx->ssl_ctx);
  
+-#if OPENSSL_VERSION_NUMBER >= 0x1010100fL
++#if OPENSSL_VERSION_NUMBER >= 0x1010100fL && !defined(LIBRESSL_VERSION_NUMBER)
+     /* For OpenSSL >=1.1.1, turn on client cert support which is
+      * otherwise turned off by default (by design).
+      * https://github.com/openssl/openssl/issues/6933 */
