b1378e64a3064d1825c06f78e98f7599e895f773
Tobias C. Berner <tcberner@FreeBSD.org>
2018-10-27 18:14:44 +0000

    www/qt5-webkit: fix build with powerpc64
    PR:		232257
    Submitted by:	Piotr Kubaj <pkubaj@anongoth.pl>

--- www/qt5-webkit/files/patch-Source_WTF_wtf_Platform.h
+++ www/qt5-webkit/files/patch-Source_WTF_wtf_Platform.h
@@ -6,7 +6,24 @@ Add proper architecture name:
 
 --- Source/WTF/wtf/Platform.h.orig	2017-06-04 20:16:06 UTC
 +++ Source/WTF/wtf/Platform.h
-@@ -226,6 +226,7 @@
+@@ -105,11 +105,15 @@
+ 
+ /* CPU(PPC64) - PowerPC 64-bit Big Endian */
+ #if (  defined(__ppc64__)      \
+-    || defined(__PPC64__))     \
++    || defined(__PPC64__)      \
++    || defined(__powerpc64__)) \
+     && defined(__BYTE_ORDER__) \
+     && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+ #define WTF_CPU_PPC64 1
+ #define WTF_CPU_BIG_ENDIAN 1
++#define ENABLE_ASSEMBLER 0
++#define ENABLE_JIT 0
++#define ENABLE_SAMPLING_PROFILER 0
+ #endif
+ 
+ /* CPU(PPC64) - PowerPC 64-bit Little Endian */
+@@ -226,6 +230,7 @@
  #elif defined(__ARM_ARCH_6__) \
      || defined(__ARM_ARCH_6J__) \
      || defined(__ARM_ARCH_6K__) \
@@ -14,8 +31,8 @@ Add proper architecture name:
      || defined(__ARM_ARCH_6Z__) \
      || defined(__ARM_ARCH_6ZK__) \
      || defined(__ARM_ARCH_6T2__) \
-@@ -272,6 +273,7 @@
-
+@@ -272,6 +277,7 @@
+ 
  #elif defined(__ARM_ARCH_6J__) \
      || defined(__ARM_ARCH_6K__) \
 +    || defined(__ARM_ARCH_6KZ__) \
