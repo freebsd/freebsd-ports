428f39f7dc8f46ee31dd7e104841d969192d44ef
linimon <linimon@FreeBSD.org>
2018-10-28 09:49:51 +0000

    Unbreak on powerpc64.
    PR:		231722
    Submitted by:	Piotr Kubaj
    Approved by:	portmgr (tier-2 blanket)

---
+++ math/gringo/files/patch-libgringo_src_term.cc
@@ -0,0 +1,10 @@
+--- libgringo/src/term.cc.orig	2018-09-25 16:02:56 UTC
++++ libgringo/src/term.cc
+@@ -21,6 +21,7 @@
+ #include "gringo/term.hh"
+ #include "gringo/logger.hh"
+ #include "gringo/graph.hh"
++#include <cmath>
+ 
+ namespace Gringo {
+ 
