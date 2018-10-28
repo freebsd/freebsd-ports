84ca75066ff222f433692208b02ca5112192acba
linimon <linimon@FreeBSD.org>
2018-10-28 13:48:26 +0000

    Fix build with GCC-based architectures.
    PR:		232009
    Submitted by:	Piotr Kubaj
    Approved by:	maintainer

---
+++ audio/drumgizmo/files/patch-drumgizmo_output_oss.cc
@@ -0,0 +1,20 @@
+--- drumgizmo/output/oss.cc.orig	2018-10-06 16:27:27 UTC
++++ drumgizmo/output/oss.cc
+@@ -30,6 +30,7 @@
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <iostream>
++#include <cstring>
+ 
+ 
+ OSSOutputEngine::OSSOutputEngine()
+--- drumgizmo/output/oss.cc.orig	2018-10-06 16:27:27 UTC
++++ drumgizmo/output/oss.cc
+@@ -30,6 +30,7 @@
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <iostream>
++#include <cstring>
+ 
+ 
+ OSSOutputEngine::OSSOutputEngine()
