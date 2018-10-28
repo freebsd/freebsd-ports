a909b6e4cb3e0bfcfdf1f74a3157e79f1685a6c7
brnrd <brnrd@FreeBSD.org>
2018-10-28 08:26:44 +0000

    MFH: r482974 r483143
    databases/mariadb103-server: Update to 10.3.10
    
     - Fix build on aarch64 [1]
     - Not broken in i386 [2]
     - WSREP only on amd64
    
    PR:		230682 [1], 230027 [2]
    Submitted by:	Naram Qashat <cyberbotx cyberbotx com> [1]
    Reported by:	<dewayne heuristicsystems com.au> [2]
    Security:	909be51b-9b3b-11e8-add2-b499baebfeaf
    
    databases/mariadb103-server: Fix plist issues
    
     - Resurect mysql_upgrade [1]
     - Move some files between -client and -server
    
    PR:		232735 [1]
    Reported by:	Naram Qashat <cyberbotx cyberbotx com> [1]
    
    Approved by:	ports-secteam (miwi)

--- databases/mariadb103-server/files/patch-storage_rocksdb_build__rocksdb.cmake
+++ databases/mariadb103-server/files/patch-storage_rocksdb_build__rocksdb.cmake
@@ -1,6 +1,6 @@
---- storage/rocksdb/build_rocksdb.cmake.orig	2018-06-25 11:13:28 UTC
+--- storage/rocksdb/build_rocksdb.cmake.orig	2018-10-02 09:45:43 UTC
 +++ storage/rocksdb/build_rocksdb.cmake
-@@ -69,15 +69,9 @@ if(ZSTD_FOUND AND (NOT WITH_ROCKSDB_ZSTD
+@@ -69,10 +69,6 @@ if(ZSTD_FOUND AND (NOT WITH_ROCKSDB_ZSTD
    SET(CMAKE_REQUIRED_LIBRARIES zstd)
    CHECK_FUNCTION_EXISTS(ZDICT_trainFromBuffer ZSTD_VALID)
    UNSET(CMAKE_REQUIRED_LIBRARIES)
@@ -8,11 +8,6 @@
 -    MESSAGE(FATAL_ERROR
 -      "WITH_ROCKSDB_ZSTD is ON and ZSTD library was found, but the version needs to be >= 1.1.3")
 -  endif()
--  if (ZSTD_VALID)
+   if (ZSTD_VALID)
      add_definitions(-DZSTD)
      include_directories(${ZSTD_INCLUDE_DIR})
-     list(APPEND THIRDPARTY_LIBS ${ZSTD_LIBRARY})
--  endif()
- endif()
- 
- add_definitions(-DZLIB)
