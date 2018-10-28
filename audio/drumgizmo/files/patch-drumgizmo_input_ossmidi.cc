a02d71abad5813a7e649426754efe95086776feb
Mark Linimon <linimon@FreeBSD.org>
2018-10-28 13:48:26 +0000

    Fix build with GCC-based architectures.
    PR:		232009
    Submitted by:	Piotr Kubaj
    Approved by:	maintainer

---
+++ audio/drumgizmo/files/patch-drumgizmo_input_ossmidi.cc
@@ -0,0 +1,20 @@
+--- drumgizmo/input/ossmidi.cc.orig	2018-10-06 16:29:02 UTC
++++ drumgizmo/input/ossmidi.cc
+@@ -30,6 +30,7 @@
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <iostream>
++#include <cstring>
+ 
+ 
+ static int const NOTE_ON = 0x90;
+--- drumgizmo/input/ossmidi.cc.orig	2018-10-06 16:29:02 UTC
++++ drumgizmo/input/ossmidi.cc
+@@ -30,6 +30,7 @@
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <iostream>
++#include <cstring>
+ 
+ 
+ static int const NOTE_ON = 0x90;
