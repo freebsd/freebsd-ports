1eeda723b8069cf5eddec72e35dca0262d3704d9
linimon <linimon@FreeBSD.org>
2018-10-28 13:44:02 +0000

    Fix build with GCC-based architectures.
    PR:		231939
    Submitted by:	Piotr Kubaj
    Approved by:	portmgr (tier-2 blanket)

---
+++ math/spot/files/patch-spot_misc_tmpfile.cc
@@ -0,0 +1,10 @@
+--- spot/misc/tmpfile.cc.orig	2018-10-04 12:40:02 UTC
++++ spot/misc/tmpfile.cc
+@@ -23,6 +23,7 @@
+ #include <cstdlib>
+ #include <unistd.h>
+ #include <string.h>
++#include <stdlib.h>
+ 
+ namespace spot
+ {
