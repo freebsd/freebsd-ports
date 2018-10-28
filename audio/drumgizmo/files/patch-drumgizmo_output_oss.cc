95bfbf4763f168281fcf157bd08cda471b05d251
linimon <linimon@FreeBSD.org>
2018-10-28 14:17:19 +0000

    Fix mismerge.
    Pointy hat to:	linimon

--- audio/drumgizmo/files/patch-drumgizmo_output_oss.cc
+++ audio/drumgizmo/files/patch-drumgizmo_output_oss.cc
@@ -8,13 +8,3 @@
  
  
  OSSOutputEngine::OSSOutputEngine()
---- drumgizmo/output/oss.cc.orig	2018-10-06 16:27:27 UTC
-+++ drumgizmo/output/oss.cc
-@@ -30,6 +30,7 @@
- #include <stdlib.h>
- #include <unistd.h>
- #include <iostream>
-+#include <cstring>
- 
- 
- OSSOutputEngine::OSSOutputEngine()
