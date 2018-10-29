b945281c836abe0db2602fb3e14cf9c0f7b66bea
Renato Botelho <garga@FreeBSD.org>
2018-10-29 19:36:04 +0000

    Update net/xmlrpc to 1.43.08
    Sponsored by:	Rubicon Communications, LLC (Netgate)

--- net/xmlrpc-c/files/patch-test_cpp_server__abyss.cpp
+++ net/xmlrpc-c/files/patch-test_cpp_server__abyss.cpp
@@ -5,8 +5,8 @@ server_abyss.cpp:88:14: error: assigning to 'int' from incompatible type '__bind
         rc = bind(this->fd, (struct sockaddr *)&sockAddr, sizeof(sockAddr));
              ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
---- test/cpp/server_abyss.cpp~	2018-01-05 09:20:59.546269035 +0000
-+++ test/cpp/server_abyss.cpp	2018-01-05 09:23:10.542713023 +0000
+--- test/cpp/server_abyss.cpp.orig	2014-08-29 18:59:00 UTC
++++ test/cpp/server_abyss.cpp
 @@ -85,7 +85,7 @@ class boundSocket { (public)
          sockAddr.sin_port   = htons(portNumber);
          sockAddr.sin_addr.s_addr = 0;
