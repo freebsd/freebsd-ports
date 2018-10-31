1aebcf8032ac15971a8c9b273aa1b1449745797d
Thierry Thomas <thierry@FreeBSD.org>
2018-10-31 18:49:19 +0000

    Upgrade to 0.8.3.
    Release notes at <https://github.com/JFreegman/toxic/releases/tag/v0.8.3>.

--- net-im/toxic/files/patch-cfg_global__vars.mk
+++ net-im/toxic/files/patch-cfg_global__vars.mk
@@ -1,17 +1,15 @@
---- cfg/global_vars.mk.orig	2018-02-27 23:46:56 UTC
+--- cfg/global_vars.mk.orig	2018-10-30 22:03:44 UTC
 +++ cfg/global_vars.mk
-@@ -1,11 +1,7 @@
+@@ -1,11 +1,6 @@
  # Version
--TOXIC_VERSION = 0.8.1
+ TOXIC_VERSION = 0.8.3
 -REV = $(shell git rev-list HEAD --count 2>/dev/null || echo -n "error")
 -ifneq (, $(findstring error, $(REV)))
 -    VERSION = $(TOXIC_VERSION)
 -else
 -    VERSION = $(TOXIC_VERSION)_r$(REV)
 -endif
-+TOXIC_VERSION = 0.8.2
 +#REV = $(shell git rev-list HEAD --count 2>/dev/null || echo -n "error")
-+VERSION = $(TOXIC_VERSION)
  
  # Project directories
  BUILD_DIR = $(BASE_DIR)/build
