bb84a5a771b299158b3fd62743ae336c718eb4e1
swills <swills@FreeBSD.org>
2018-11-01 15:10:29 +0000

    games/bsdgames: update to 5.2.2
    Added bcd, boggle, ppt, tetris
    Pet portlint
    While here, pass maintainership to submitter
    
    PR:		232051
    Submitted by:	Joachim Werner <jockl@pianojockl.org>

---
+++ games/bsdgames/files/patch-boggle-boggle-extern.h
@@ -0,0 +1,10 @@
+--- boggle/boggle/extern.h.orig	2018-06-15 14:40:00 UTC
++++ boggle/boggle/extern.h
+@@ -33,6 +33,7 @@
+  */
+ 
+ #include <setjmp.h>
++#include <time.h>
+ 
+ extern int tlimit;
+ extern time_t start_t;
