f61de22a8df6e4a575ab7ef9e7e48a1fffe7bb99
Kurt Jaeger <pi@FreeBSD.org>
2018-11-01 08:15:59 +0000

    net/kamailio: update 5.1.5 -> 5.1.6, fix build with newer mysql clients
    PR:		232868
    Submitted by:	flo
    Relnotes:	https://www.kamailio.org/w/2018/10/kamailio-v5-1-6-released/

--- net/kamailio/files/patch-src_core_ip__addr.h
+++ net/kamailio/files/patch-src_core_ip__addr.h
@@ -1,6 +1,6 @@
---- src/core/ip_addr.h.orig	2018-03-01 16:24:06 UTC
+--- src/core/ip_addr.h.orig	2018-08-22 09:38:42 UTC
 +++ src/core/ip_addr.h
-@@ -49,6 +49,10 @@ enum sip_protos { PROTO_NONE, PROTO_UDP,
+@@ -49,6 +49,10 @@ enum sip_protos { PROTO_NONE, PROTO_UDP, PROTO_TCP, PR
  enum comp_methods { COMP_NONE, COMP_SIGCOMP, COMP_SERGZ };
  #endif
  
--- net/kamailio/files/patch-src_main.c
+++ net/kamailio/files/patch-src_main.c
@@ -1,6 +1,6 @@
---- src/main.c.orig	2018-03-01 16:24:07 UTC
+--- src/main.c.orig	2018-08-22 09:38:44 UTC
 +++ src/main.c
-@@ -362,7 +362,7 @@ int received_dns = 0;
+@@ -361,7 +361,7 @@ int received_dns = 0;
  int sr_auto_aliases=1;
  char* working_dir = 0;
  char* chroot_dir = 0;
