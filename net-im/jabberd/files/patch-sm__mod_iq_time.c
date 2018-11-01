56bd95183602554468200b7cbf12169ef5cead6a
antoine <antoine@FreeBSD.org>
2018-11-01 18:16:33 +0000

    MFH: r482797 r482800 r482837 r482994
    net-im/jabberd:
    - add OpenSSL 1.1 patches from upstream
    - use autoreconf
    - make portlint happier
    
    net-im/jabberd: fix build errors with MariaDB 10.2
    
    PR:		228382
    Submitted by:	Fabian Wenk <fabian@wenks.ch>
    
    net-im/jabberd: remove dependency on autoconf-archive
    
    net-im/jabberd: add missing build dependency on pkgconfig

--- net-im/jabberd/files/patch-sm__mod_iq_time.c
+++ net-im/jabberd/files/patch-sm__mod_iq_time.c
@@ -1,6 +1,6 @@
 --- sm/mod_iq_time.c.orig	2016-02-09 09:47:02 UTC
 +++ sm/mod_iq_time.c
-@@ -77,7 +77,7 @@ static mod_ret_t _iq_time_pkt_sm(mod_ins
+@@ -77,7 +77,7 @@ static mod_ret_t _iq_time_pkt_sm(mod_instance_t mi, pk
  
      datetime_out(t, dt_DATETIME, buf, 64);
      nad_insert_elem(pkt->nad, 2, NAD_ENS(pkt->nad, 1), "utc", buf);
