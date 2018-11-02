c693cb2d3440e4ab090c9e95052ec2456e2cd186
linimon <linimon@FreeBSD.org>
2018-11-02 15:12:08 +0000

    This port needs USES=c++11-lang.  Additionally, it unconditionally uses libc++ if it's compiled on FreeBSD, so fix that too.
    PR:		231714
    Submitted by:	Piotr Kubaj
    Approved by:	portmgr (tier-2 blanket)

--- www/py-libsass/files/patch-setup.py
+++ www/py-libsass/files/patch-setup.py
@@ -1,11 +1,29 @@
---- setup.py.orig	2017-11-16 05:28:26 UTC
+--- setup.py.orig	2018-09-16 19:57:43 UTC
 +++ setup.py
 @@ -37,7 +37,7 @@ def _maybe_clang(flags):
          compiler.linker_so[0] = os.environ['CXX']
          return compiler
      distutils.sysconfig.customize_compiler = customize_compiler
 -    flags[:] = ['-c', '-O3'] + flags + ['-stdlib=libc++']
-+    flags[:] = ['-c'] + flags + ['-stdlib=libc++']
++    flags[:] = ['-c'] + flags
  
  
  def _maybe_macos(flags):
+@@ -59,7 +59,7 @@ if system_sass:
+     _maybe_macos(flags)
+ 
+     if platform.system() == 'FreeBSD':
+-        link_flags = ['-fPIC', '-lc++']
++        link_flags = ['-fPIC']
+     else:
+         link_flags = ['-fPIC', '-lstdc++']
+     libraries = ['sass']
+@@ -173,7 +173,7 @@ else:
+                         f.write(cencode_body)
+ 
+         if platform.system() == 'FreeBSD':
+-            link_flags = ['-fPIC', '-lc++']
++            link_flags = ['-fPIC']
+         else:
+             link_flags = ['-fPIC', '-lstdc++']
+ 
