45b0a0268c6d78a964f0be34e22b4693a53fded4
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2018-11-03 22:50:40 +0000

    Fix build with curl 7.62.0
    - While I'm here, add USES=gnome

--- net-mgmt/seafile-client/files/patch-daemon_http-tx-mgr.c
+++ net-mgmt/seafile-client/files/patch-daemon_http-tx-mgr.c
@@ -1,7 +1,7 @@
---- daemon/http-tx-mgr.c.orig	2015-09-21 03:42:11 UTC
+--- daemon/http-tx-mgr.c.orig	2018-02-02 05:55:07 UTC
 +++ daemon/http-tx-mgr.c
-@@ -58,6 +58,22 @@
- #define USER_AGENT_OS "Linux"
+@@ -68,6 +68,22 @@
+ #define USER_AGENT_OS "BSD"
  #endif
  
 +#ifdef __FreeBSD__
@@ -23,3 +23,13 @@
  struct _Connection {
      CURL *curl;
      gint64 ctime;               /* Used to clean up unused connection. */
+@@ -1091,7 +1107,9 @@ curl_error_to_http_task_error (int curl_
+     case CURLE_SSL_CONNECT_ERROR:
+     case CURLE_PEER_FAILED_VERIFICATION:
+     case CURLE_SSL_CERTPROBLEM:
++#if LIBCURL_VERSION_NUM < 0x073e00
+     case CURLE_SSL_CACERT:
++#endif
+     case CURLE_SSL_CACERT_BADFILE:
+     case CURLE_SSL_ISSUER_ERROR:
+         return HTTP_TASK_ERR_SSL;
