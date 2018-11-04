28afc4dc6a7f55fe35b631b1a8c85a8b3d84ccaa
Mark Felder <feld@FreeBSD.org>
2018-11-04 16:58:30 +0000

    ElasticSearch6: Update to 6.4.2

---
+++ textproc/kibana6/files/patch-src_setup__node__env_index.js
@@ -0,0 +1,11 @@
+--- src/setup_node_env/index.js.orig	2018-11-04 14:48:57 UTC
++++ src/setup_node_env/index.js
+@@ -19,5 +19,5 @@
+  * under the License.
+  */
+ 
+-require('./node_version_validator');
+-require('./babel_register');
+\ No newline at end of file
++//require('./node_version_validator');
++require('./babel_register');
