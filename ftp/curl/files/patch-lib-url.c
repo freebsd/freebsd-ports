1239456e4f28aa35767ad38f39df38771cb846be
sunpoet <sunpoet@FreeBSD.org>
2018-11-05 16:28:01 +0000

    MFH: r483704 r483944 r483945 r483947 r483948 r483949 r483950 r483952
    Update to 7.62.0
    
    Changes:	https://curl.haxx.se/changes.html
    Security:	e0ab1773-07c1-46c6-9170-4c5e81c00927
    
    Fix build with curl 7.62.0
    
    - While I'm here, add USES=gnome
    
    Approved by:	ports-secteam (miwi)

--- ftp/curl/files/patch-lib-url.c
+++ ftp/curl/files/patch-lib-url.c
@@ -3,16 +3,16 @@ Forwarded: not-needed
 Author: Peter Pentchev <roam@FreeBSD.org>
 Last-Update: 2010-12-18
 
---- lib/url.c.orig	2018-01-23 07:55:37 UTC
+--- lib/url.c.orig	2018-10-30 06:47:16 UTC
 +++ lib/url.c
-@@ -589,6 +589,10 @@ CURLcode Curl_open(struct Curl_easy **cu
-       data->state.current_speed = -1; /* init to negative == impossible */
-       data->set.fnmatch = ZERO_NULL;
-       data->set.maxconnects = DEFAULT_CONNCACHE_SIZE; /* for easy handles */
+@@ -536,6 +536,10 @@ CURLcode Curl_init_userdefined(struct Cu
+   set->fnmatch = ZERO_NULL;
+   set->upkeep_interval_ms = CURL_UPKEEP_INTERVAL_DEFAULT;
+   set->maxconnects = DEFAULT_CONNCACHE_SIZE; /* for easy handles */
 +#if defined(__FreeBSD_version)
-+      /* different handling of signals and threads */
-+      data->set.no_signal = TRUE;
-+#endif /* __FreeBSD_version */
- 
-       Curl_http2_init_state(&data->state);
-     }
++  /* different handling of signals and threads */
++  set->no_signal = TRUE;
++#endif
+   set->httpversion =
+ #ifdef USE_NGHTTP2
+     CURL_HTTP_VERSION_2TLS
