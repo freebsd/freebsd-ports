99266506c0f33a19ad3bec88e6f04f383f049860
acm <acm@FreeBSD.org>
2018-11-06 18:08:37 +0000

    - Fix build on 12-BETA3
    Reported by:	pkg-fallout

--- www/sogo4/files/patch-UI-MailPartViewers_UIxMailPartSignedViewer.m
+++ www/sogo4/files/patch-UI-MailPartViewers_UIxMailPartSignedViewer.m
@@ -1,21 +1,18 @@
---- UI/MailPartViewers/UIxMailPartSignedViewer.m	2018-11-04 23:25:20.140369000 -0500
-+++ UI/MailPartViewers/UIxMailPartSignedViewer.m	2018-11-04 23:28:00.684899000 -0500
-@@ -173,15 +173,15 @@
+--- UI/MailPartViewers/UIxMailPartSignedViewer.m	2018-10-23 14:22:36.000000000 -0500
++++ UI/MailPartViewers/UIxMailPartSignedViewer.m	2018-11-06 13:02:05.090224000 -0500
+@@ -169,13 +169,13 @@
+       if (err)
+         {
+ #ifdef HAVE_GNUTLS
+-          const char* sslError;
++          NSString* sslError;
  	  ERR_load_crypto_strings();
            SSL_load_error_strings();
            sslError = ERR_reason_error_string(err);
--          validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : @"No error information available"]] retain];
-+          validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : "No error information available"]] retain];
+           validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : @"No error information available"]] retain];
  #elif OPENSSL_VERSION_NUMBER < 0x10100000L
-           const char* sslError;
+-          const char* sslError;
++          NSString* sslError;
  	  ERR_load_crypto_strings();
            SSL_load_error_strings();
            sslError = ERR_reason_error_string(err);
--          validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : @"No error information available"]] retain];
-+          validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : "No error information available"]] retain];
- #else
--	  validationMessage = [[self labelForKey: @"No error information available"] retain];
-+	  validationMessage = [[self labelForKey: "No error information available"] retain];
- #endif /* HAVE_GNUTLS */
-       }
-     }
