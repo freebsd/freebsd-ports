02cbe9e0304a77408642dc211c59d41b811c0a49
Kubilay Kocak <koobs@FreeBSD.org>
2018-11-07 03:24:59 +0000

    devel/py-buildbot{-*}: Update to 1.5.0
    All ports (where necessary):
    
      - Fix and equalize *_DEPENDS (and specified versions) to setup.py.
      - Match COMMENT to setup.py:description.
      - Add commented LICENSE_FILE describing why its not defined.
    
    py-buildbot{-worker}:
    
      - Enable concurrent (Multiple Python version) installation.
      - Update test targets to set PYTHONPATH, so that the package in WRKSRC.
        is tested, not installed packages.
    
    py-buildbot:
    
      - Remove post-patch target, no longer necessary.
      - Add test dependency not declared in setup.py:test_requires, that cause
        tests to fail when not installed, unlike other dependencies that are
        skipped.
      - Add an un-referenced compulsory RUN_DEPENDS on pyyaml reported and
        resolved upstream [1].
    
    py-buildbot-worker:
    
      - Update patch-setup.py to actually fix (package/install) the VERSION
        file, rather than just not installing it. The worker passes this files
        contents to the master for display in the frontend if it exists,
        otherwise sending the string 'latest' or the modification datestamp of
        another file. [1]
      - Fix startup script to use the filename of itself (the executed script),
        not a filename that uses the ${name} variable, which doesnt exist as it
        contains an underscore (not a dash), causing the following error when
        executed:
    
        /usr/local/etc/rc.d/buildbot-worker: /usr/local/etc/rc.d/buildbot_worker: not found
    
    Changelog:
    
      http://docs.buildbot.net/current/relnotes/index.html#buildbot-1-5-0-2018-10-09
    
    [1] https://github.com/buildbot/buildbot/pull/4394
    
    Requested by:		Tao Zhou <tao ish com au>
    Reviewed_by:		Nathan Owens <ndowens yahoo com>, 0mp
    Differential Revision:	D17821

--- devel/py-buildbot-worker/files/patch-setup.py
+++ devel/py-buildbot-worker/files/patch-setup.py
@@ -1,12 +1,46 @@
---- setup.py.orig	2017-07-01 19:16:48 UTC
+# modernize buildbot_worker setup.py with package_data
+# https://github.com/buildbot/buildbot/pull/4394
+
+--- setup.py.orig	2018-10-09 18:37:18 UTC
 +++ setup.py
-@@ -106,9 +106,6 @@ setup_args = {
+@@ -31,23 +31,6 @@ from distutils.core import setup
+ from buildbot_worker import version
+ 
+ 
+-class our_install_data(install_data):
+-
+-    def finalize_options(self):
+-        self.set_undefined_options('install',
+-                                   ('install_lib', 'install_dir'),
+-                                   )
+-        install_data.finalize_options(self)
+-
+-    def run(self):
+-        install_data.run(self)
+-        # ensure there's a buildbot_worker/VERSION file
+-        fn = os.path.join(self.install_dir, 'buildbot_worker', 'VERSION')
+-        with open(fn, 'w') as f:
+-            f.write(version)
+-        self.outfiles.append(fn)
+-
+-
+ class our_sdist(sdist):
+ 
+     def make_release_tree(self, base_dir, files):
+@@ -105,11 +88,12 @@ setup_args = {
          "buildbot_worker.test.unit",
          "buildbot_worker.test.util",
      ],
 -    # mention data_files, even if empty, so install_data is called and
 -    # VERSION gets copied
 -    'data_files': [("buildbot_worker", [])],
++    'package_data': {
++        '': [
++            'VERSION',
++        ]
++    },
      'cmdclass': {
-         'install_data': our_install_data,
+-        'install_data': our_install_data,
          'sdist': our_sdist
+     },
+     'entry_points': {
