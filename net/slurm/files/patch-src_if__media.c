266495ae1ca8e417c22d01644daa28cb0c4938ce
Michael Landin <mich@FreeBSD.org>
2018-11-07 10:02:00 +0000

    - fix build on 12.x (remove obsolete network support / IFM_FDDI) - add LICENSE - update patch format to make portlint happy - bump port revision
    Reported by:	pkg-fallout@

--- net/slurm/files/patch-src_if__media.c
+++ net/slurm/files/patch-src_if__media.c
@@ -1,6 +1,6 @@
---- src/if_media.c.orig
+--- src/if_media.c.orig	2004-09-27 13:08:59 UTC
 +++ src/if_media.c
-@@ -95,8 +95,13 @@
+@@ -95,8 +95,13 @@ int get_if_speed (char *ifstring)
       *
       */
  
@@ -14,3 +14,26 @@
  
  #ifdef MEDIADEBUG
      printf("      all: %6d\n", ifmr.ifm_current);
+@@ -160,22 +165,6 @@ int get_if_speed (char *ifstring)
+                     break;
+             } /* end switch physical */
+             break;
+-        /* FDDI interfaces */
+-        /* fpa doesn't seem to support SIOCGIFMEDIA on FreeBSD
+-         * so we won't get here but anyway ...
+-         */
+-        case IFM_FDDI:
+-            switch (physical)
+-            {
+-                case IFM_FDDI_SMF:
+-                case IFM_FDDI_MMF:
+-                case IFM_FDDI_UTP:
+-                    speed = 100 * 1000;
+-                    break;
+-                default:
+-                    speed = ERR_IFACE_NO_SPEED;
+-            }
+-            break;
+ #if WIRELESS
+         /* IEEE 802.11 wireless interfaces */
+         case IFM_IEEE80211:
