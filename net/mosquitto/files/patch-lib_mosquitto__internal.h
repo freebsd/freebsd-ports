dde5632d3970783ab9af5671ba0b92573fb0c240
antoine <antoine@FreeBSD.org>
2018-11-09 22:52:39 +0000

    MFH: r481153 r483677
    - Update to 1.5.3
    
    PR:		231047
    Submitted by:	Iblis Lin <iblis@hs.ntnu.edu.tw>
    Approved by:	joe@thrallingpenguin.com (maintainer)
    
    net/mosquitto: Fix OpenSSL build
    
    PR:		232168
    Submitted by:	Nathan <ndowens@yahoo.com>
    Approved by:	joe@thrallingpenguin.com (maintainer)

--- net/mosquitto/files/patch-lib_mosquitto__internal.h
+++ net/mosquitto/files/patch-lib_mosquitto__internal.h
@@ -1,25 +1,33 @@
-From 9f74e09373f3be7b77f510ef8b0ae2c887f7b325 Mon Sep 17 00:00:00 2001
-From: Natanael Copa <ncopa@alpinelinux.org>
-Date: Tue, 4 Oct 2016 14:35:33 +0200
-Subject: [PATCH] [280] Fix build with libressl
-
-Closes #280
-
-Fix building with libressl which broke in commit fff741613 (Support for
-openssl 1.1.0).
-
-Bug: #280
-
-Signed-off-by: Natanael Copa <ncopa@alpinelinux.org>
---- lib/mosquitto_internal.h.orig	2017-07-10 22:46:01 UTC
+--- lib/mosquitto_internal.h.orig	2018-09-25 15:27:43 UTC
 +++ lib/mosquitto_internal.h
-@@ -25,6 +25,9 @@ Contributors:
- 
- #ifdef WITH_TLS
- #  include <openssl/ssl.h>
-+#  if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
-+#    define HAVE_OPENSSL_OPAQUE_STRUCTS
-+#  endif
- #else
- #  include <time.h>
+@@ -31,11 +31,7 @@ Contributors:
  #endif
+ #include <stdlib.h>
+ 
+-#if defined(WITH_THREADING) && !defined(WITH_BROKER)
+-#  include <pthread.h>
+-#else
+-#  include <dummypthread.h>
+-#endif
++#include <pthread.h>
+ 
+ #ifdef WITH_SRV
+ #  include <ares.h>
+@@ -194,7 +190,7 @@ struct mosquitto {
+ #endif
+ 	bool want_write;
+ 	bool want_connect;
+-#if defined(WITH_THREADING) && !defined(WITH_BROKER)
++
+ 	pthread_mutex_t callback_mutex;
+ 	pthread_mutex_t log_callback_mutex;
+ 	pthread_mutex_t msgtime_mutex;
+@@ -205,7 +201,7 @@ struct mosquitto {
+ 	pthread_mutex_t out_message_mutex;
+ 	pthread_mutex_t mid_mutex;
+ 	pthread_t thread_id;
+-#endif
++
+ 	bool clean_session;
+ #ifdef WITH_BROKER
+ 	char *old_id; /* for when a duplicate client connects, but we still want to
