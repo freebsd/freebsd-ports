b6d13d54691262c6a0fec705097a26dcf3561976
arved <arved@FreeBSD.org>
2018-11-10 21:28:05 +0000

    Update to 1.15.1
    As Upstream seems unresponsive, maintainer created a fork.
    
    This fixes build with OpenSSL 1.1
    All patches from the port have been incorporated
    
    PR:             232219
    Submitted by:   maintainer

--- sysutils/radmind/files/patch-command.h
+++
@@ -1,8 +0,0 @@
---- ./command.h.orig	2010-12-13 04:42:49.000000000 +0100
-+++ ./command.h	2014-06-29 12:15:04.244544161 +0200
-@@ -13,3 +13,5 @@
-     char	*c_name;
-     int		(*c_func)( SNET *, int, char *[] );
- };
-+
-+#define RADMIND_STAT_FMT "%s %s %o %d %d %" PRItimet "d %" PRIofft "d %s\r\n"
