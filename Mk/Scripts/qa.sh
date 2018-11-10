1b0417656969bd1c326432fe019d9a6f6bf42997
mat <mat@FreeBSD.org>
2018-11-10 10:09:48 +0000

    security/openssl-devel was removed, but there is a security/openssl111 now.

--- Mk/Scripts/qa.sh
+++ Mk/Scripts/qa.sh
@@ -611,7 +611,7 @@ proxydeps_suggest_uses() {
 	elif [ ${pkg} = "devel/readline" ]; then
 		warn "you need USES+=readline"
 	# ssl
-	elif [ ${pkg} = "security/openssl" -o ${pkg} = "security/openssl-devel" \
+	elif [ ${pkg} = "security/openssl" -o ${pkg} = "security/openssl111" \
 	  -o ${pkg} = "security/libressl" -o ${pkg} = "security/libressl-devel" \
 	  ]; then
 		warn "you need USES=ssl"
