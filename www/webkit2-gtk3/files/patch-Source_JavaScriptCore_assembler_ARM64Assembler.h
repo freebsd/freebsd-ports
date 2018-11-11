320948405935e42ac65ef3aa9c972983cf431bd9
kwm <kwm@FreeBSD.org>
2018-11-11 20:53:34 +0000

    Update Webkit2-gtk3 to 2.22.3.
    * Regen patches
    
    Obtained from:	GNOME devel repo

--- www/webkit2-gtk3/files/patch-Source_JavaScriptCore_assembler_ARM64Assembler.h
+++ www/webkit2-gtk3/files/patch-Source_JavaScriptCore_assembler_ARM64Assembler.h
@@ -1,11 +1,11 @@
---- Source/JavaScriptCore/assembler/ARM64Assembler.h.orig	2018-03-05 09:28:05 UTC
+--- Source/JavaScriptCore/assembler/ARM64Assembler.h.orig	2018-08-21 16:06:10 UTC
 +++ Source/JavaScriptCore/assembler/ARM64Assembler.h
-@@ -2856,6 +2856,8 @@ class ARM64Assembler { (public)
+@@ -2863,6 +2863,8 @@ class ARM64Assembler { (public)
      {
  #if OS(IOS)
          sys_cache_control(kCacheFunctionPrepareForExecution, code, size);
 +#elif OS(FREEBSD) || OS(NETBSD)
 +        __clear_cache(code, reinterpret_cast<char*>(code) + size);
+ #elif OS(FUCHSIA)
+         zx_cache_flush(code, size, ZX_CACHE_FLUSH_INSN);
  #elif OS(LINUX)
-         size_t page = pageSize();
-         uintptr_t current = reinterpret_cast<uintptr_t>(code);
--- www/webkit2-gtk3/files/patch-Source_WTF_wtf_Platform.h
+++ www/webkit2-gtk3/files/patch-Source_WTF_wtf_Platform.h
@@ -1,6 +1,6 @@
---- Source/WTF/wtf/Platform.h.orig	2018-02-20 09:12:50 UTC
+--- Source/WTF/wtf/Platform.h.orig	2018-09-21 19:59:47 UTC
 +++ Source/WTF/wtf/Platform.h
-@@ -182,6 +182,7 @@
+@@ -183,6 +183,7 @@
  #elif defined(__ARM_ARCH_6__) \
      || defined(__ARM_ARCH_6J__) \
      || defined(__ARM_ARCH_6K__) \
@@ -8,7 +8,7 @@
      || defined(__ARM_ARCH_6Z__) \
      || defined(__ARM_ARCH_6ZK__) \
      || defined(__ARM_ARCH_6T2__) \
-@@ -229,6 +230,7 @@
+@@ -230,6 +231,7 @@
  
  #elif defined(__ARM_ARCH_6J__) \
      || defined(__ARM_ARCH_6K__) \
