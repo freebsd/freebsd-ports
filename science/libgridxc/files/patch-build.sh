565d6b725da7ad91a65eaad9d47e0411733a1625
yuri <yuri@FreeBSD.org>
2018-11-15 02:56:34 +0000

    science/libgridxc: Update 0.8.0 -> 0.8.3
    Reported by:	portscout

--- science/libgridxc/files/patch-build.sh
+++ science/libgridxc/files/patch-build.sh
@@ -1,10 +1,10 @@
---- build.sh.orig	2018-02-01 10:48:08 UTC
+--- build.sh.orig	2018-11-01 22:36:58 UTC
 +++ build.sh
-@@ -39,14 +39,17 @@
- # libGridXC installations in the old style.
+@@ -40,14 +40,16 @@
  #
  #
--inPREFIX=${PREFIX}
+ # Set to "." if empty or unset
+-inPREFIX=${PREFIX:-.}
 +inPREFIX=${INSTALL_DIR}
  #
  # Build first without MPI
@@ -16,7 +16,6 @@
 +if [ "${WITH_MPI}" = "" ]
 +then
 +  echo "==> make WITH_LIBXC=${WITH_LIBXC} WITH_MPI= PREFIX=${inPREFIX}/serial"
-+  #sleep 1
 +  make clean
 +  make WITH_LIBXC=${WITH_LIBXC} WITH_MPI= PREFIX=${inPREFIX}/serial
 +fi
