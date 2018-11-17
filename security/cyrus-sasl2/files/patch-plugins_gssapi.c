e3a074111a62a01f4e944abaf36788ec1dffa500
ume <ume@FreeBSD.org>
2018-11-17 18:47:45 +0000

    Update to 2.1.27.

--- security/cyrus-sasl2/files/patch-plugins_gssapi.c
+++ security/cyrus-sasl2/files/patch-plugins_gssapi.c
@@ -1,11 +1,11 @@
---- plugins/gssapi.c.orig
+--- plugins/gssapi.c.orig	2016-01-30 14:06:08 UTC
 +++ plugins/gssapi.c
-@@ -1490,8 +1490,10 @@
+@@ -1531,8 +1531,10 @@ static int gssapi_client_mech_step(void 
      *clientout = NULL;
      *clientoutlen = 0;
      
 +#if 0
-     params->utils->log(NULL, SASL_LOG_DEBUG,
+     params->utils->log(params->utils->conn, SASL_LOG_DEBUG,
  		       "GSSAPI client step %d", text->state);
 +#endif
  
