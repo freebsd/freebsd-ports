36b4e3e7eaf2ca7bff66330d32b28a0c3a78bb47
Yuri Victorovich <yuri@FreeBSD.org>
2018-11-19 21:56:09 +0000

    New port: science/berkeleygw: Scientific program aimed at the ab initio virtual experimentation
    Only a serial version for now.

---
+++ science/berkeleygw/files/patch-bin_assemble__manual.sh
@@ -0,0 +1,8 @@
+--- bin/assemble_manual.sh.orig	2018-11-19 21:30:43 UTC
++++ bin/assemble_manual.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash -l
++#!/bin/sh
+ 
+ # Assemble README and .inp files into a crude manual.
+ # Usage: bin/assemble_manual.sh > manual.html
