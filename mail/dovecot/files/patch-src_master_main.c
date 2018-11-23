218e60f0658b3db951a78c84615ae8a12aa6e9e2
Larry Rosenman <ler@FreeBSD.org>
2018-11-23 15:12:44 +0000

    mail/dovecot update to 2.3.4, mail/dovecot-pigeonhole to 0.5.4
    dovecot change log:
    * The default postmaster_address is now "postmaster@<user domain or
       server hostname>". If username contains the @domain part, that's
       used. If not, then the server's hostname is used.
    * "doveadm stats dump" now returns two decimals for the "avg" field.
    
    + Added push notification driver that uses a Lua script
    + Added new SQL, DNS and connection events.
       See https://wiki2.dovecot.org/Events
    + Added "doveadm mailbox cache purge" command.
    + Added events API support for Lua scripts
    + doveadm force-resync -f parameter performs "index fsck" while opening
       the index. This may be useful to fix some types of broken index files.
       This may become the default behavior in a later version.
    - director: Kicking a user crashes if login process is very slow
    - pop3_no_flag_updates=no: Don't expunge DELEted and RETRed messages
       unless QUIT is sent.
    - auth: Fix crypt() segfault with glibc-2.28+
    - imap: Running UID FILTER script with errors assert-crashes
    - dsync, pop3-migration: POP3 UIDLs weren't added to
       dovecot.index.cache while mails were saved.
    - dict clients may have been using 100% CPU while waiting for dict
       server to finish commands.
    - doveadm user: Fixed user listing via HTTP API
    - All levels of Cassandra log messages were logged as Dovecot errors.
    - http/smtp client may have crashed after SSL handshake
    - Lua auth converted strings that looked like numbers into numbers.
    
    pigeonhole change log:
      * Adjustments to several changes in Dovecot v2.3.4 make this Pigeonhole
        release dependent on that Dovecot release; it will not compile against
        older Dovecot versions. And, conversely, you need to upgrade
        Pigeonhole when upgrading Dovecot to v2.3.4.
      * The changes regarding the default postmaster_address in Dovecot v2.3.4
        mainly apply to Pigeonhole. The new default should work for all
        existing installations, thereby fixing several reported v2.3/v0.5
        migration problems.
    
      - IMAP FILTER=SIEVE capability: Fix assert crash occurring when running
        UID FILTER on a Sieve script with errors.

--- mail/dovecot/files/patch-src_master_main.c
+++ mail/dovecot/files/patch-src_master_main.c
@@ -1,6 +1,6 @@
---- src/master/main.c.orig	2018-03-20 03:15:40.000000000 -0700
-+++ src/master/main.c	2018-05-05 18:57:32.485732000 -0700
-@@ -861,6 +861,8 @@
+--- src/master/main.c.orig	2018-11-23 11:06:45 UTC
++++ src/master/main.c
+@@ -862,6 +862,8 @@ int main(int argc, char *argv[])
  		i_fatal("dup2(dev_null_fd) failed: %m");
  	if (!foreground && dup2(dev_null_fd, STDOUT_FILENO) < 0)
  		i_fatal("dup2(dev_null_fd) failed: %m");
