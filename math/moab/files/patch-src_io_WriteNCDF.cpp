ed411ada6fd23e1dd9363b7f96786df9ecbb66a8
Yuri Victorovich <yuri@FreeBSD.org>
2018-11-24 03:10:29 +0000

    math/moab: Update 4.6.3 -> 5.0.0
    Port changes:
    * Change MASTER_SITES to bitbucket where official releases are
    * Change to CMAKE
    * Add MPI option
    * Fix WWW
    
    Reported by:	portscout

--- math/moab/files/patch-src_io_WriteNCDF.cpp
+++ math/moab/files/patch-src_io_WriteNCDF.cpp
@@ -1,13 +1,13 @@
---- src/io/WriteNCDF.cpp.orig	2018-11-05 01:47:40 UTC
+--- src/io/WriteNCDF.cpp.orig	2018-11-23 06:26:09 UTC
 +++ src/io/WriteNCDF.cpp
-@@ -154,8 +154,8 @@ void WriteNCDF::time_and_date(char* time
+@@ -161,8 +161,8 @@ void WriteNCDF::time_and_date(char* time
     strftime(date_string, TIME_STR_LEN, "%m/%d/%Y", local_time);
  
-      // terminate with NULL character
+    // Terminate with NULL character
 -   time_string[10] = (char)NULL;
 -   date_string[10] = (char)NULL;
 +   time_string[10] = (char)0;
 +   date_string[10] = (char)0;
  }
  
- ErrorCode WriteNCDF::write_file(const char *exodus_file_name, 
+ ErrorCode WriteNCDF::write_file(const char *exodus_file_name,
