a7380f59670db7bbd558c3363895996f35e1398f
Tilman Keskinoz <arved@FreeBSD.org>
2018-11-25 21:01:10 +0000

    Unbreak by updating to 4.0.1
    PR:		232964
    Submitted by:	 w.schwarzenfeld@utanet.at

--- audio/terminatorx/files/patch-src_tX__mouse.cc
+++ audio/terminatorx/files/patch-src_tX__mouse.cc
@@ -1,11 +1,13 @@
---- src/tX_mouse.cc.orig	2011-01-30 21:01:32 UTC
+--- src/tX_mouse.cc.orig	2018-11-07 18:49:45 UTC
 +++ src/tX_mouse.cc
-@@ -47,7 +47,8 @@
+@@ -37,9 +37,9 @@
  #include "tX_global.h"
  #include "tX_engine.h"
  #include "tX_vtt.h"
 -#include <stdlib.h>
 +#include <cstdlib>
+ #include <string.h>
+-#include <math.h>
 +#include <cmath>
  
  #define TX_MOUSE_SPEED_NORMAL 0.05
