074b0cd0a6a407f10352d14f23134b24906f654c
ohauer <ohauer@FreeBSD.org>
2018-11-26 05:11:03 +0000

    - fix build with libressl 2.8.2
    PR:		233517
    Submitted by:	Andrej Ebert

--- mail/postfix-current/files/patch-src_tls_tls__server.c
+++ mail/postfix-current/files/patch-src_tls_tls__server.c
@@ -1,4 +1,4 @@
---- src/tls/tls_server.c.orig	2016-08-27 20:27:50 UTC
+--- src/tls/tls_server.c.orig	2018-11-17 14:59:22 UTC
 +++ src/tls/tls_server.c
 @@ -174,7 +174,7 @@ static const char server_session_id_cont
  #endif					/* OPENSSL_VERSION_NUMBER */
@@ -18,7 +18,16 @@
  
      /*
       * Initialize the OpenSSL library by the book! To start with, we must
-@@ -588,7 +588,7 @@ TLS_APPL_STATE *tls_server_init(const TL
+@@ -486,7 +486,7 @@ TLS_APPL_STATE *tls_server_init(const TL
+      * OpenSSL 0.9.8h, while SSL_NO_TICKET was added in 0.9.8f.
+      */
+ #ifdef SSL_OP_NO_TICKET
+-#if !defined(OPENSSL_NO_TLSEXT) && OPENSSL_VERSION_NUMBER >= 0x0090808fL
++#if !defined(OPENSSL_NO_TLSEXT) && OPENSSL_VERSION_NUMBER >= 0x0090808fL && !defined(LIBRESSL_VERSION_NUMBER)
+     ticketable = (*var_tls_tkt_cipher && scache_timeout > 0
+ 		  && !(off & SSL_OP_NO_TICKET));
+     if (ticketable) {
+@@ -595,7 +595,7 @@ TLS_APPL_STATE *tls_server_init(const TL
      /*
       * 2015-12-05: Ephemeral RSA removed from OpenSSL 1.1.0-dev
       */
