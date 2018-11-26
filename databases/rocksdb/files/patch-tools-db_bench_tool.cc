e351c7490b8bc8b3b2fec08d965b841c28da74d7
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2018-11-26 18:59:28 +0000

    Fix build and unbreak this port
    Approved by:	portmgr (blanket)

--- databases/rocksdb/files/patch-tools-db_bench_tool.cc
+++ databases/rocksdb/files/patch-tools-db_bench_tool.cc
@@ -1,6 +1,16 @@
 --- tools/db_bench_tool.cc.orig	2018-11-12 19:57:32 UTC
 +++ tools/db_bench_tool.cc
-@@ -4229,7 +4229,7 @@ void VerifyDBFromDB(std::string& truth_d
+@@ -1973,7 +1973,9 @@ class Benchmark {
+   int64_t max_num_range_tombstones_;
+   WriteOptions write_options_;
+   Options open_options_;  // keep options around to properly destroy db later
++#ifndef ROCKSDB_LITE
+   TraceOptions trace_options_;
++#endif
+   int64_t reads_;
+   int64_t deletes_;
+   double read_random_exp_range_;
+@@ -4229,7 +4231,7 @@ void VerifyDBFromDB(std::string& truth_d
          }
          if (levelMeta.level == 0) {
            for (auto& fileMeta : levelMeta.files) {
