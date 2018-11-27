d44588a7855ebcb58195190fce6a04da4bd6b7f7
ehaupt <ehaupt@FreeBSD.org>
2018-11-27 22:31:52 +0000

    Update to 0.1.b120

--- audio/fasttracker2/files/patch-src_ft2__main.c
+++ audio/fasttracker2/files/patch-src_ft2__main.c
@@ -1,4 +1,4 @@
---- src/ft2_main.c.orig	2018-11-25 14:01:25 UTC
+--- src/ft2_main.c.orig	2018-11-27 19:40:20 UTC
 +++ src/ft2_main.c
 @@ -187,15 +187,6 @@ int main(int argc, char *argv[])
          enterFullscreen();
@@ -6,7 +6,7 @@
  
 -    //benchmarkAudioChannelMixer(); /* for development testing */
 -
--    /* set up MIDI input (in a thread because it can take quite a while on f.ex. macOS) */  
+-    /* set up MIDI input (in a thread because it can take quite a while on f.ex. macOS) */
 -    initMidiThread = SDL_CreateThread(initMidiFunc, "FT2 Clone MIDI Initialization Thread", NULL);
 -    if (initMidiThread == NULL)
 -    {
