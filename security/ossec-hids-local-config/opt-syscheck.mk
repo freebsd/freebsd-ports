ff67580177763b62a16748d3792be994d02ab81f
swills <swills@FreeBSD.org>
2018-11-27 01:58:02 +0000

    security/ossec-hids-local: build fix as user [1]
    While here:
    
    - add new option PGSQL_SC ossec-hids-*-config [1]
    - improved option descriptions [1]
    - fix build with GCC-based architectures [2]
    
    PR:		233168 [1]
    PR:		233291 [2]
    Submitted by:	Dominik Lisiak <dominik.lisiak@bemsoft.pl> (maintainer) [1]
    Submitted by:	Piotr Kubaj <pkubaj@anongoth.pl> [2]
    Approved by:	Dominik Lisiak <dominik.lisiak@bemsoft.pl> (maintainer) [2]

--- security/ossec-hids-local-config/opt-syscheck.mk
+++ security/ossec-hids-local-config/opt-syscheck.mk
@@ -3,22 +3,6 @@ SYSCHECK_LOCAL_CONF=		530.syscheck.local.conf
 
 SYSCHECK_DESC=			File Integrity Checking (syscheck)
 
-# Default direcotries
-SYSCHECK_BASIC_OPTION=		BASIC_SC
-SYSCHECK_BASIC_PROFILE=		basic
-SYSCHECK_BASIC_DESC=		"bin", "sbin" and "etc"
-SYSCHECK_BASIC_DEFINE=		server local agent pushed
-SYSCHECK_BASIC_DEFAULT=		server local pushed
-SYSCHECK_OPTIONS+=		SYSCHECK_BASIC
-
-# OSSEC directories
-SYSCHECK_OSSEC_OPTION=		OSSEC_SC
-SYSCHECK_OSSEC_PROFILE=		ossec
-SYSCHECK_OSSEC_DESC=		OSSEC directories
-SYSCHECK_OSSEC_DEFINE=		server local agent pushed
-SYSCHECK_OSSEC_DEFAULT=		server local pushed
-SYSCHECK_OPTIONS+=		SYSCHECK_OSSEC
-
 # Alert new files
 SYSCHECK_NEWFILES_OPTION=	NEWFILES_SC
 SYSCHECK_NEWFILES_DESC=		Alert on new files created
@@ -40,3 +24,27 @@ SYSCHECK_HOSTDENY_DESC=		Ignore access control files
 SYSCHECK_HOSTDENY_DEFINE=	server local agent pushed
 SYSCHECK_HOSTDENY_DEFAULT=	server local pushed
 SYSCHECK_OPTIONS+=		SYSCHECK_HOSTDENY
+
+# Default direcotries
+SYSCHECK_BASIC_OPTION=		BASIC_SC
+SYSCHECK_BASIC_PROFILE=		basic
+SYSCHECK_BASIC_DESC=		"bin", "sbin" and "etc" directories
+SYSCHECK_BASIC_DEFINE=		server local agent pushed
+SYSCHECK_BASIC_DEFAULT=		server local pushed
+SYSCHECK_OPTIONS+=		SYSCHECK_BASIC
+
+# OSSEC directories
+SYSCHECK_OSSEC_OPTION=		OSSEC_SC
+SYSCHECK_OSSEC_PROFILE=		ossec
+SYSCHECK_OSSEC_DESC=		OSSEC directories
+SYSCHECK_OSSEC_DEFINE=		server local agent pushed
+SYSCHECK_OSSEC_DEFAULT=		server local pushed
+SYSCHECK_OPTIONS+=		SYSCHECK_OSSEC
+
+# PostgreSQL directory
+SYSCHECK_PGSQL_OPTION=		PGSQL_SC
+SYSCHECK_PGSQL_PROFILE=		postgresql
+SYSCHECK_PGSQL_DESC=		PostgreSQL configuration files
+SYSCHECK_PGSQL_DEFINE=		server local agent pushed
+SYSCHECK_PGSQL_DEFAULT=		pushed
+SYSCHECK_OPTIONS+=		SYSCHECK_PGSQL
