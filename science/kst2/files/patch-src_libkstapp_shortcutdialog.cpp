ed79ffeef878643525abd0787a3fb1cf264d29de
makc <makc@FreeBSD.org>
2018-11-28 12:40:23 +0000

    science/kst2: - Update to 2018.11.23 snapshot - Add LICENSE - Pass additional args to cmake instead of patching CMakeLists.txt

---
+++ science/kst2/files/patch-src_libkstapp_shortcutdialog.cpp
@@ -0,0 +1,10 @@
+--- src/libkstapp/shortcutdialog.cpp.orig	2018-11-23 19:35:50 UTC
++++ src/libkstapp/shortcutdialog.cpp
+@@ -17,6 +17,7 @@
+ #include <QDebug>
+ #include <QTextEdit>
+ #include <QFile>
++#include <QStyle>
+ 
+ #include <QStringList>
+ 
