93012956ae33e707616d64b4edab27e0a9d2f3b1
Thierry Thomas <thierry@FreeBSD.org>
2018-12-04 07:22:02 +0000

    Upgrade to v0.9.9.

--- science/gnudatalanguage/files/patch-CMakeModules_Findlibps.cmake
+++
@@ -1,8 +0,0 @@
---- CMakeModules/Findlibps.cmake.orig	2016-04-29 17:03:25 UTC
-+++ CMakeModules/Findlibps.cmake
-@@ -1,4 +1,4 @@
--find_path(LIBPSPKG libps.pc PATH_SUFFIXES lib lib/pkgconfig lib64/pkgconfig)
-+find_path(LIBPSPKG libps.pc PATH_SUFFIXES lib libdata/pkgconfig lib/pkgconfig lib64/pkgconfig)
- include(FindPackageHandleStandardArgs)
- if(LIBPSPKG)
- 	set(ENV{PKG_CONFIG_PATH} ${LIBPSPKG}) # pkg search path
