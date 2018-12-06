7c64203f67e1b9da62dbb9529801a35a5afdcbce
Jan Beich <jbeich@FreeBSD.org>
2018-12-06 17:11:14 +0000

    lang/rust: update to 1.31.0
    - devel/racer no longer needs to dowgrade rustc-ap-syntax
    - Force rebuild all consumers to catch regressions early
    
    Changes:	https://blog.rust-lang.org/2018/12/06/Rust-1.31.0.html
    Approved by:	rust (tobik)
    Differential Revision:	https://reviews.freebsd.org/D18427

--- lang/rust/files/patch-src_vendor_openssl-sys_src_dtls1.rs
+++
@@ -1,6 +0,0 @@
---- src/vendor/openssl-sys/src/dtls1.rs.orig	2018-09-16 20:29:19 UTC
-+++ src/vendor/openssl-sys/src/dtls1.rs
-@@ -0,0 +1,3 @@
-+use libc::*;
-+
-+pub const DTLS1_COOKIE_LENGTH: c_uint = 256;
--- lang/rust/files/patch-src_vendor_openssl-sys_src_safestack.rs
+++
@@ -1,4 +0,0 @@
---- src/vendor/openssl-sys/src/safestack.rs.orig	2018-09-16 20:29:19 UTC
-+++ src/vendor/openssl-sys/src/safestack.rs
-@@ -0,0 +1 @@
-+stack!(stack_st_OPENSSL_STRING);
