5c4ad83f25079344f209435daae2087a0f2673df
0mp <0mp@FreeBSD.org>
2018-12-07 13:41:26 +0000

    New port: devel/pmdk: Libraries and tools to manage and access persistent memory devices
    The Persistent Memory Development Kit (PMDK) is a collection of libraries
    and tools for System Administrators and Application Developers to simplify
    managing and accessing persistent memory devices.
    
    WWW: https://pmem.io/pmdk/
    
    PR:		232690
    Submitted by:	D Scott Phillips <d.scott.phillips@intel.com>
    Reviewed by:	mat, ndowens@yahoo.com (previous version)
    Approved by:	mat (mentor)
    Differential Revision:	https://reviews.freebsd.org/D18009

---
+++ devel/pmdk/files/patch-src_common_queue.h
@@ -0,0 +1,13 @@
+--- src/common/queue.h.orig	2018-10-24 02:57:34 UTC
++++ src/common/queue.h
+@@ -32,8 +32,8 @@
+  *	@(#)queue.h	8.5 (Berkeley) 8/20/94
+  */
+ 
+-#ifndef	_SYS_QUEUE_H_
+-#define	_SYS_QUEUE_H_
++#ifndef	_COPIED_GLIBC_SYS_QUEUE_H_
++#define	_COPIED_GLIBC_SYS_QUEUE_H_
+ 
+ /*
+  * This file defines five types of data structures: singly-linked lists,
