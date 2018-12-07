91c70b2aa9ec578373e6e53d2f66a1658e562f8b
yuri <yuri@FreeBSD.org>
2018-12-07 04:29:45 +0000

    New port: math/barvinok: Library for counting the number of integer points in polytopes

---
+++ math/barvinok/files/patch-interface_all__barvinok.h
@@ -0,0 +1,6 @@
+--- interface/all_barvinok.h.orig	2018-12-07 04:15:52 UTC
++++ interface/all_barvinok.h
+@@ -1,2 +1,2 @@
+-#include <interface/all.h>
++//#include <interface/all.h>
+ #include <barvinok/isl.h>
