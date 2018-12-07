ee9ab2281bc787cd040cd4bc29dbc897e6bec801
linimon <linimon@FreeBSD.org>
2018-12-07 15:59:48 +0000

    To build on powerpc64, assembler, JIT, YARR JIT and Sampling Profiler need to be disabled.
    PR:		233731
    Submitted by:	Piotr Kubaj
    Approved by:	portmgr (tier-2 blanket)

--- www/webkit-gtk3/files/patch-Source_WTF_wtf_Platform.h
+++ www/webkit-gtk3/files/patch-Source_WTF_wtf_Platform.h
@@ -1,7 +1,25 @@
-This patch reportedly fixes build for ARM. See PR 208569
+This patch reportedly fixes build for ARM and powerpc64. See PR 208569
 
 --- Source/WTF/wtf/Platform.h.orig	2016-04-10 06:48:36 UTC
 +++ Source/WTF/wtf/Platform.h
+@@ -119,11 +119,16 @@
+ 
+ /* CPU(PPC64) - PowerPC 64-bit Big Endian */
+ #if (  defined(__ppc64__)      \
+-    || defined(__PPC64__))     \
++    || defined(__PPC64__)      \
++    || defined(__powerpc64__))   \
+     && defined(__BYTE_ORDER__) \
+     && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+ #define WTF_CPU_PPC64 1
+ #define WTF_CPU_BIG_ENDIAN 1
++#define ENABLE_ASSEMBLER 0
++#define ENABLE_JIT 0
++#define ENABLE_YARR_JIT 0
++#define ENABLE_SAMPLING_PROFILER 0
+ #endif
+ 
+ /* CPU(PPC64) - PowerPC 64-bit Little Endian */
 @@ -218,6 +218,7 @@
  #elif defined(__ARM_ARCH_6__) \
      || defined(__ARM_ARCH_6J__) \
