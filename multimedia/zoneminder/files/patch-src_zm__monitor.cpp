f9e7f55b3a20d67e4c7f0b5af4482cb33fe732f0
Kurt Jaeger <pi@FreeBSD.org>
2018-12-08 22:28:06 +0000

    multimedia/zoneminder: update 1.30.4 -> 1.32.2
    PR:		233289
    Submitted by:	Ivan <bsd@abinet.ru> (maintainer)
    Reviewed by:	lme
    Relnotes:	http://www.zoneminder.com/blog/2018/10/13/tuesdays-gone-release.html
    		https://github.com/ZoneMinder/zoneminder/releases/tag/1.32.0

--- multimedia/zoneminder/files/patch-src_zm__monitor.cpp
+++ multimedia/zoneminder/files/patch-src_zm__monitor.cpp
@@ -1,29 +1,20 @@
---- src/zm_monitor.cpp.orig	2016-07-28 01:45:26 UTC
+--- src/zm_monitor.cpp.orig	2018-10-13 18:36:15 UTC
 +++ src/zm_monitor.cpp
-@@ -160,7 +160,7 @@ bool Monitor::MonitorLink::connect()
-       return( false );
-     }
-     mem_ptr = (unsigned char *)shmat( shm_id, 0, 0 );
--    if ( mem_ptr < 0 )
-+    if ( mem_ptr < (void *)0 )
-     {
-       Debug( 3, "Can't shmat link memory: %s", strerror(errno) );
-       connected = false;
-@@ -194,7 +194,7 @@ bool Monitor::MonitorLink::disconnect()
-     connected = false;
+@@ -2110,6 +2110,7 @@ Monitor *Monitor::Load(MYSQL_ROW dbrow, bool load_zone
+   Camera *camera = 0;
+   if ( type == "Local" ) {
  
- #if ZM_MEM_MAPPED
--    if ( mem_ptr > 0 )
-+    if ( mem_ptr > (void *)0 )
-     {
-       msync( mem_ptr, mem_size, MS_ASYNC );
-       munmap( mem_ptr, mem_size );
-@@ -558,7 +558,7 @@ bool Monitor::connect() {
-     exit( -1 );
-   }
-   mem_ptr = (unsigned char *)shmat( shm_id, 0, 0 );
--  if ( mem_ptr < 0 )
-+  if ( mem_ptr < (void *)0 )
-   {
-     Error( "Can't shmat: %s", strerror(errno));
-     exit( -1 );
++#if ZM_HAS_V4L
+     int extras = (deinterlacing>>24)&0xff;
+ 
+     camera = new LocalCamera(
+@@ -2132,6 +2133,9 @@ Monitor *Monitor::Load(MYSQL_ROW dbrow, bool load_zone
+         record_audio,
+         extras
+         );
++#else
++    Fatal("ZoneMinder not built with Local Camera support");
++#endif
+   } else if ( type == "Remote" ) {
+     if ( protocol == "http" ) {
+       camera = new RemoteCameraHttp(
