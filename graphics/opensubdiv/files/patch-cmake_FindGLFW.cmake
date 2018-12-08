d08fd124a27fb1927130a50b99962d4e514bf8be
Kurt Jaeger <pi@FreeBSD.org>
2018-12-08 17:20:36 +0000

    graphics/opensubdiv: update 3.3.0 -> 3.3.3
    PR:		225940
    Submitted by:	Shane Ambler <FreeBSD@ShaneWare.Biz> (maintainer)
    Relnotes:	http://graphics.pixar.com/opensubdiv/docs/release_notes.html

--- graphics/opensubdiv/files/patch-cmake_FindGLFW.cmake
+++ graphics/opensubdiv/files/patch-cmake_FindGLFW.cmake
@@ -1,11 +1,11 @@
---- cmake/FindGLFW.cmake.orig	2017-06-28 18:58:17 UTC
+--- cmake/FindGLFW.cmakeorig	2018-02-08 09:39:46 UTC
 +++ cmake/FindGLFW.cmake
-@@ -162,7 +162,7 @@ else ()
-             message(FATAL_ERROR "Xi library not found - required for GLFW")
-         endif()
+@@ -174,7 +174,7 @@ else ()
+                 message(FATAL_ERROR "Xi library not found - required for GLFW")
+             endif()
  
--        list(APPEND GLFW_x11_LIBRARY "${X11_Xrandr_LIB}" "${X11_Xxf86vm_LIB}" "${X11_Xcursor_LIB}" "${X11_Xinerama_LIB}" "${X11_Xi_LIB}" "${X11_LIBRARIES}" "${CMAKE_THREAD_LIBS_INIT}" -lrt -ldl)
-+        list(APPEND GLFW_x11_LIBRARY "${X11_Xrandr_LIB}" "${X11_Xxf86vm_LIB}" "${X11_Xcursor_LIB}" "${X11_Xinerama_LIB}" "${X11_Xi_LIB}" "${X11_LIBRARIES}" "${CMAKE_THREAD_LIBS_INIT}" -lrt ${CMAKE_DL_LIBS})
+-            list(APPEND GLFW_x11_LIBRARY "${X11_Xrandr_LIB}" "${X11_Xxf86vm_LIB}" "${X11_Xcursor_LIB}" "${X11_Xinerama_LIB}" "${X11_Xi_LIB}" "${X11_LIBRARIES}" "${CMAKE_THREAD_LIBS_INIT}" -lrt -ldl)
++            list(APPEND GLFW_x11_LIBRARY "${X11_Xrandr_LIB}" "${X11_Xxf86vm_LIB}" "${X11_Xcursor_LIB}" "${X11_Xinerama_LIB}" "${X11_Xi_LIB}" "${X11_LIBRARIES}" "${CMAKE_THREAD_LIBS_INIT}" -lrt ${CMAKE_DL_LIBS})
+         endif (NOT NO_GLFW_X11)
  
          find_library( GLFW_glfw_LIBRARY
-             NAMES 
