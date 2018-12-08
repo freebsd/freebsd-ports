9a41b2a7281d3fa6e76d2ed4c1a91594122d4a9e
swills <swills@FreeBSD.org>
2018-12-08 18:41:11 +0000

    devel/jfrog-cli: update to 1.22.1

--- devel/jfrog-cli/files/patch-jfrog-cli_utils_lock_utils__unix.go
+++
@@ -1,8 +0,0 @@
---- jfrog-cli/utils/lock/utils_unix.go.orig	2018-11-09 16:18:19 UTC
-+++ jfrog-cli/utils/lock/utils_unix.go
-@@ -1,4 +1,4 @@
--// +build linux darwin
-+// +build linux darwin freebsd
- 
- package lock
- 
