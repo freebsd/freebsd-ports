2bcb43f78f0c40b6d9f6c7aea976501bbc08a995
swills <swills@FreeBSD.org>
2018-12-08 18:08:08 +0000

    net/ndisc6 update to 1.0.3
    PR:		233854
    Submitted by:	m.tsatsenko@gmail.com (maintainer)

--- net/ndisc6/files/patch-rdnssd__rdnssd.h
+++ net/ndisc6/files/patch-rdnssd__rdnssd.h
@@ -1,16 +1,25 @@
---- rdnssd/rdnssd.h.orig    2011-07-23 01:03:38.000000000 +0400
-+++ rdnssd/rdnssd.h 2011-07-23 01:03:56.000000000 +0400
-@@ -32,15 +32,6 @@
- /* Belongs in <netinet/icmp6.h> */
+--- rdnssd/rdnssd.h.orig	2011-10-17 15:35:11 UTC
++++ rdnssd/rdnssd.h
+@@ -32,24 +32,6 @@ extern const rdnss_src_t rdnss_netlink, 
  #define ND_OPT_RDNSS 25
+ #define ND_OPT_DNSSL 31
  
 -struct nd_opt_rdnss
 -{
 -	uint8_t nd_opt_rdnss_type;
 -	uint8_t nd_opt_rdnss_len;
--	uint16_t nd_opt_rdnss_resserved1;
+-	uint16_t nd_opt_rdnss_reserved;
 -	uint32_t nd_opt_rdnss_lifetime;
 -	/* followed by one or more IPv6 addresses */
+-};
+-
+-struct nd_opt_dnssl
+-{
+-	uint8_t nd_opt_dnssl_type;
+-	uint8_t nd_opt_dnssl_len;
+-	uint16_t nd_opt_dnssl_reserved;
+-	uint32_t nd_opt_dnssl_lifetime;
+-	/* followed by one or more domain names */
 -};
 -
  # ifdef __cplusplus
--- net/ndisc6/files/patch-src_tcpspray.c
+++ net/ndisc6/files/patch-src_tcpspray.c
@@ -1,6 +1,6 @@
---- src/tcpspray.c.orig	2010-05-01 01:08:34.000000000 -0700
-+++ src/tcpspray.c	2015-02-15 15:56:55.904785304 -0800
-@@ -34,6 +34,7 @@
+--- src/tcpspray.c.orig	2011-09-22 16:00:30 UTC
++++ src/tcpspray.c
+@@ -33,6 +33,7 @@
  # define SIZE_MAX SIZE_T_MAX // FreeBSD 4.x workaround
  #endif
  #include <stdlib.h>
