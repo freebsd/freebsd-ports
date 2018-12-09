38423ea5de40b9d5caaff7e6597bacbe4a72d976
madpilot <madpilot@FreeBSD.org>
2018-12-09 17:29:13 +0000

    - Add gtk2 and gtk3 flavors to geany ports - Make gtk3 the default
    NOTE: some plugins are gtk2 only, please check UPDATING.
    
    PR:			226523
    Submitted by:		Greg V <greg@unrelenting.technology>
    Reviewed by:		mat
    Approved by:		portmgr (mat)
    Differential Revision:	https://reviews.freebsd.org/D18018

--- devel/geany-plugins/files/bsd.geany-plugins.mk
+++ devel/geany-plugins/files/bsd.geany-plugins.mk
@@ -1,18 +1,21 @@
 # $FreeBSD$
 
 GEANY_VER=		1.33
+GEANY_PLUGINS_GTK2=	debugger \
+			devhelp \
+			geanypy \
+			multiterm \
+			scope
+
 GEANY_PLUGINS_ALL=	addons \
 			autoclose \
 			automark \
 			codenav \
 			commander \
 			ctags \
-			debugger \
 			defineformat \
-			devhelp \
 			doc \
 			extrasel \
-			geanypy \
 			gendoc \
 			geniuspaste \
 			git-changebar \
@@ -24,7 +27,6 @@ GEANY_PLUGINS_ALL=	addons \
 			macro \
 			markdown \
 			miniscript \
-			multiterm \
 			numberedbookmarks \
 			overview \
 			pairtaghighlighter \
@@ -33,7 +35,6 @@ GEANY_PLUGINS_ALL=	addons \
 			pretty-printer \
 			projectorganizer \
 			prj \
-			scope \
 			sendmail \
 			shiftcolumn \
 			spellcheck \
