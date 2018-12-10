79de4e15082416eda51f9b87a0f1ad43924fb978
jwb <jwb@FreeBSD.org>
2018-12-10 21:40:55 +0000

    sysutils/condor: Permanent fix for FreeBSD 13 and all future versions
    Previous HTCondor code checks for each FreeBSD version individually.
    Add a generic patch to cover all current and future FreeBSD versions.
    
    Approved by:    jrm (mentor, implicit)

---
+++ sysutils/condor/files/patch-src_condor__includes_config.h.cmake
@@ -0,0 +1,13 @@
+--- src/condor_includes/config.h.cmake.orig	2018-12-10 14:54:32 UTC
++++ src/condor_includes/config.h.cmake
+@@ -49,10 +49,6 @@
+ #cmakedefine CONDOR_FREEBSD6
+ /* Define if on FreeBSD 7 */
+ #cmakedefine CONDOR_FREEBSD7
+-///* Define if on FreeBSD 8 */
+-#cmakedefine CONDOR_FREEBSD8
+-///* Define if on FreeBSD 9 */
+-#cmakedefine CONDOR_FREEBSD9
+ ///* Define if on FreeBSD 9 or later, which use utmpx insead of utmp */
+ #cmakedefine CONDOR_UTMPX
+ ///* Define if on FreeBSD */
