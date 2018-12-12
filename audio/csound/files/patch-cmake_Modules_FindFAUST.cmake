3045b6c9a466394b52eac0e45594174b56303790
yuri <yuri@FreeBSD.org>
2018-12-12 19:57:07 +0000

    audio/csound: 6.12.2 -> 6.12.2-83
    Reported by:	GH issue notification

--- audio/csound/files/patch-cmake_Modules_FindFAUST.cmake
+++ audio/csound/files/patch-cmake_Modules_FindFAUST.cmake
@@ -1,4 +1,4 @@
---- cmake/Modules/FindFAUST.cmake.orig	2018-11-17 16:56:13 UTC
+--- cmake/Modules/FindFAUST.cmake.orig	2018-12-12 19:23:43 UTC
 +++ cmake/Modules/FindFAUST.cmake
 @@ -27,7 +27,7 @@ if(FAUST_FOUND)
          # This is a static build of faust, hence
@@ -7,5 +7,5 @@
 -        find_program(LLVM_CONFIG llvm-config HINTS /usr/bin /usr/local/bin /usr/local/opt/llvm/bin)
 +        find_program(LLVM_CONFIG llvm-config${FAUST_LLVM_VERSION} HINTS /usr/bin /usr/local/bin /usr/local/opt/llvm/bin)
          if(NOT LLVM_CONFIG)
-             message(FATAL_ERROR "Using a static Faust library requires LLVM tooling to be present in the path")
-         endif()
+             message(WARNING "Using a static Faust library requires LLVM tooling to be present in the path.")
+             UNSET(FAUST_FOUND)
