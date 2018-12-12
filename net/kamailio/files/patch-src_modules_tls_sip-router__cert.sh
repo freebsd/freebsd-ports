05a7fc95fba8ce177d466f76580f8945256ab946
Kurt Jaeger <pi@FreeBSD.org>
2018-12-12 17:59:31 +0000

    net/kamailio: update 5.1.6 -> 5.2.0, fix build with libressl
    PR:		230202
    Submitted by:	dewayne@heuristicsystems.com.au
    Relnotes:	https://www.kamailio.org/w/kamailio-v5-2-0-release-notes/

--- net/kamailio/files/patch-src_modules_tls_sip-router__cert.sh
+++
@@ -1,9 +0,0 @@
---- src/modules/tls/sip-router_cert.sh.orig	2018-03-01 16:24:10 UTC
-+++ src/modules/tls/sip-router_cert.sh
-@@ -197,5 +197,6 @@ if [ $? != 0 ] ; then
- else
- 	echo "Private key stored in '$DIR/$KEY_FILENAME'."
- 	echo "Certificate stored in '$DIR/$CERT_FILENAME'."
-+	rm ~/.rnd
- 	exit 0;
- fi
