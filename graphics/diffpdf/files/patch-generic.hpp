1326f5eb26db1d3037ae6c7c0ac4d333efe83fee
Dirk Meyer <dinoex@FreeBSD.org>
2018-12-13 17:10:52 +0000

    - use qt5 Obtained from:	debian

---
+++ graphics/diffpdf/files/patch-generic.hpp
@@ -0,0 +1,11 @@
+--- generic.hpp.orig	2013-10-15 07:01:22 UTC
++++ generic.hpp
+@@ -12,7 +12,7 @@
+     for more details.
+ */
+ 
+-#include <poppler-qt4.h>
++#include <poppler-qt5.h>
+ #include <QMetaType>
+ #include <QPair>
+ #include <QPixmap>
