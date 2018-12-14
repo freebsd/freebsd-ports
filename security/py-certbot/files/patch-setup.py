5aad0be669789d99756a2fae83ccabb6719171d7
Matthew Seaman <matthew@FreeBSD.org>
2018-12-14 09:18:33 +0000

    Fix setting FreeBSD specific paths since upstream change
    https://github.com/certbot/certbot/commit/a23d76beb0e2c9539670766045314a5d50f582a2#diff-64ccdc74e8a07f9b039a6254093f1d0d
    
    as part of the update to 0.29.1
    
    PR:		233909
    Reported by:	Paul Macdonald
    Approved by:	koobs (python)

--- security/py-certbot/files/patch-setup.py
+++ security/py-certbot/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2018-03-08 22:20:34 UTC
+--- setup.py.orig	2018-12-05 23:47:59 UTC
 +++ setup.py
-@@ -44,7 +44,6 @@ install_requires = [
+@@ -39,7 +39,6 @@ install_requires = [
      'configobj',
      'cryptography>=1.2',  # load_pem_x509_certificate
      'josepy',
@@ -8,7 +8,7 @@
      'parsedatetime>=1.3',  # Calendar.parseDT
      'pyrfc3339',
      'pytz',
-@@ -58,6 +57,7 @@ dev_extras = [
+@@ -53,6 +52,7 @@ dev_extras = [
      'astroid==1.3.5',
      'coverage',
      'ipdb',
