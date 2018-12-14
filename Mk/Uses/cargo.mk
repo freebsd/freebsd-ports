6f0e774d272698e7c283945dcd74f77f964d22ba
jbeich <jbeich@FreeBSD.org>
2018-12-14 18:20:35 +0000

    USES=cargo to respect CC for linking
    Reported by:	tijl
    Approved by:	rust (tobik)
    Differential Revision:	https://reviews.freebsd.org/D18528

--- Mk/Uses/cargo.mk
+++ Mk/Uses/cargo.mk
@@ -68,7 +68,7 @@ CARGO_ENV+= \
 	CARGO_TARGET_DIR=${CARGO_TARGET_DIR} \
 	RUSTC=${LOCALBASE}/bin/rustc \
 	RUSTDOC=${LOCALBASE}/bin/rustdoc \
-	RUSTFLAGS="${RUSTFLAGS} ${LDFLAGS:S/^/-C link-arg=/}"
+	RUSTFLAGS="${RUSTFLAGS} -C linker=${CC:Q} ${LDFLAGS:S/^/-C link-arg=/}"
 
 # Adjust -C target-cpu if -march/-mcpu is set by bsd.cpu.mk
 .if ${ARCH} == amd64 || ${ARCH} == i386
