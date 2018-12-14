dae73343c3fa55323131714bebe19131a9329faa
Tijl Coosemans <tijl@FreeBSD.org>
2018-12-14 16:24:02 +0000

    Install both 32 bit and 64 bit Linux libraries from the official Linux driver distribution instead of the ones bundled with the FreeBSD driver which are incomplete and 32 bit only.  This makes amd64 Linux OpenGL programs work on FreeBSD 12.0 and recent 11-STABLE.  This does not enable CUDA.
    - Initialise DISTFILES so a file can be added by the LINUX option.
    - Set EXTRACT_ONLY because the file added by the LINUX option is a
      self-extracting archive handled in post-extract.
    - The patches for <355.06 and >=355.06 are identical except for the path.
      Keep only one set (listed in SUB_PATCHES) because more patches have been
      added.  Modify their path in pre-patch.  The patches have been renamed
      from extra-patch-* to extra-patch-*.in.
    - Set OPTIONS_SUB and use option helpers for the LINUX option.
    - Define LINUX_ARCH_SUFX like ARCH_SUFX because LINUX_ARCH can be i386 on
      amd64.
    - Set LINUX_LIBDIR to the main lib directory: (/compat/linux)/usr/lib on
      i386 and (/compat/linux)/usr/lib64 on amd64.  (The compat32 directory is
      (/compat/linux)/usr/lib.)
    - Define LINUX_LIBS, LINUX_LINKS, LINUX32_LIBS, LINUX32_LINKS, LINUX_PROGS,
      LINUX_MANS, and LINUX_DATA for all driver versions from 304.137 to 390.87.
      LINUX_LIBS and LINUX32_LIBS contain main libraries (i386/amd64) and
      compat32 libraries respectively.  These are installed with INSTALL_LIB in
      post-install.  LINUX_LINKS and LINUX32_LINKS contain symbolic link sources
      and their target.  LINUX_PROGS are binaries installed with
      INSTALL_PROGRAM.  LINUX_MANS are manuals installed with INSTALL_MAN.
      LINUX_DATA are data files and their destination, installed with
      INSTALL_DATA.  The .if blocks for the different versions are sorted
      alphabetically on the first file.
    - Define PLIST_RE for all versions.  These are sed commands applied to
      TMPPLIST in post-install.
    - Include bsd.port.pre.mk instead of bsd.port.options.mk to obtain
      LINUX_ARCH.
    - Adjust post-patch to disable installation of Linux libraries bundled with
      the FreeBSD driver.
    - Add post-patch-LINUX-on to setup nvidia_icd.json like nvidia-installer
      does on Linux.
    - Adjust existing post-install TMPPLIST edits to apply only to non-Linux
      files.
    - Add post-install-LINUX-on to install the Linux files.
    - Add files/extra-patch-src-nvidia_ctl.c.in and
      extra-patch-src-nvidia_dev.c.in.  These patches make Linux stat(2) return
      the right major and minor number for /dev/nvidiactl and /dev/nvidia[0-9]+.
      Recent versions of the Linux libraries verify these. [1]  Define the
      struct passed to linux_device_register_handler on the stack in
      nvidia_*_attach instead of as a global variable in case multiple graphics
      cards are attached in parallel.
    - Add files/extra-patch-src-nvidia_linux.c.in.  It rewrites the Linux ioctl
      handler.  The current implementation does not copyin arguments leading to
      kernel panics on FreeBSD 12 on machines that support Supervisor Mode
      Access Prevention (SMAP).
    - Add files/extra-patch-src-nvidia_pci.c.in.  It adds a dependency on the
      linux_common kernel module for linux_device_register_handler.
    - Modify files/extra-patch-src_nvidia-modeset_nvidia-modeset-freebsd.c to
      add major/minor numbers (global struct here, because it is constant),
      rewrite ioctl, and add a dependency on linux_common for
      /dev/nvidia-modeset.
    
    PR:		217901
    Analysed by:	Alex S <iwtcex@gmail.com> [1]
    Tested by:	Hannes Hauswedell <h2+fbsdports@fsfe.org> (390.87)
    Tested by:	tijl (304.137)
    Approved by:	maintainer timeout (15 days)

--- x11/nvidia-driver/files/extra-patch-src_nvidia-modeset_nvidia-modeset-freebsd.c
+++ x11/nvidia-driver/files/extra-patch-src_nvidia-modeset_nvidia-modeset-freebsd.c
@@ -1,6 +1,22 @@
---- src/nvidia-modeset/nvidia-modeset-freebsd.c.orig	2016-05-28 15:44:11 UTC
+--- src/nvidia-modeset/nvidia-modeset-freebsd.c.orig	2018-08-21 23:09:28 UTC
 +++ src/nvidia-modeset/nvidia-modeset-freebsd.c
-@@ -245,7 +245,7 @@ struct nvkms_ref_ptr {
+@@ -25,6 +25,7 @@
+ #include <sys/poll.h>
+ #include <sys/file.h>
+ #include <sys/proc.h>
++#include <sys/sysproto.h>
+ 
+ #include "nvkms-ioctl.h"
+ #include "nvidia-modeset-os-interface.h"
+@@ -48,6 +49,7 @@
+     #include "machine/../linux32/linux32_proto.h"
+   #endif
+   #include <compat/linux/linux_ioctl.h>
++  #include <compat/linux/linux_util.h>
+ #endif
+ 
+ 
+@@ -250,7 +252,7 @@ struct nvkms_ref_ptr {
  
  struct nvkms_ref_ptr* NVKMS_API_CALL nvkms_alloc_ref_ptr(void *ptr)
  {
@@ -9,3 +25,80 @@
      if (ref_ptr) {
          mtx_init(&ref_ptr->lock, "nvkms-ref-ptr-lock", NULL, MTX_SPIN);
          // The ref_ptr owner counts as a reference on the ref_ptr itself.
+@@ -867,33 +869,31 @@ static int nvkms_poll(
+  *************************************************************************/
+ 
+ #if defined(NVKMS_SUPPORT_LINUX_COMPAT)
++static struct linux_device_handler nvkms_linux_device_handler = {
++    .bsd_driver_name = "nvidia-modeset",
++    .linux_driver_name = "nvidia-modeset",
++    .bsd_device_name = "nvidia-modeset",
++    .linux_device_name = "nvidia-modeset",
++    .linux_major = 195,
++    .linux_minor = 254,
++    .linux_char_device = 1
++};
+ 
+ static int nvkms_linux_ioctl_function(
+     struct thread *td,
+     struct linux_ioctl_args *args
+ )
+ {
+-    struct file *fp;
+-    int status;
+-    u_long cmd;
++    static const uint32_t dir[4] = { IOC_VOID, IOC_IN, IOC_OUT, IOC_INOUT };
+ 
+-#if NV_FGET_HAS_CAP_RIGHTS_T_ARG
+-    cap_rights_t rights;
+-    status = fget(td, args->fd, cap_rights_init(&rights, CAP_IOCTL), &fp);
+-#else
+-    status = fget(td, args->fd, &fp);
+-#endif
+-
+-    if (status != 0) {
+-        return status;
++    if ((args->cmd & (1<<29)) != 0) {
++        /* FreeBSD has only 13 bits to encode the size. */
++        printf("nvidia: pid %d (%s): ioctl cmd=0x%x size too large\n",
++            (int)td->td_proc->p_pid, td->td_proc->p_comm, args->cmd);
++        return (EINVAL);
+     }
+-
+-    cmd = args->cmd;
+-
+-    status = fo_ioctl(fp, cmd, (caddr_t)args->arg, td->td_ucred, td);
+-    fdrop(fp, td);
+-
+-    return status;
++    args->cmd = (args->cmd & ~IOC_DIRMASK) | dir[args->cmd >> 30];
++    return (sys_ioctl(td, (struct ioctl_args *)args));
+ }
+ 
+ #define NVKMS_LINUX_IOCTL_MIN _IOC(0, NVKMS_IOCTL_MAGIC, NVKMS_IOCTL_CMD, 0)
+@@ -909,6 +909,7 @@ static struct linux_ioctl_handler nvkms_linux_ioctl_ha
+ static void nvkms_linux_compat_load(void)
+ {
+ #if defined(NVKMS_SUPPORT_LINUX_COMPAT)
++    linux_device_register_handler(&nvkms_linux_device_handler);
+     linux_ioctl_register_handler(&nvkms_linux_ioctl_handler);
+ #endif
+ }
+@@ -917,6 +918,7 @@ static void nvkms_linux_compat_unload(void)
+ {
+ #if defined(NVKMS_SUPPORT_LINUX_COMPAT)
+     linux_ioctl_unregister_handler(&nvkms_linux_ioctl_handler);
++    linux_device_unregister_handler(&nvkms_linux_device_handler);
+ #endif
+ }
+ 
+@@ -1100,4 +1102,9 @@ MODULE_DEPEND(nvidia_modeset,               /* module 
+ MODULE_DEPEND(nvidia_modeset,               /* module name */
+               linux,                        /* prerequisite module */
+               1, 1, 1);                     /* vmin, vpref, vmax */
++#if defined(NVCPU_X86_64)
++MODULE_DEPEND(nvidia_modeset,               /* module name */
++              linux_common,                 /* prerequisite module */
++              1, 1, 1);                     /* vmin, vpref, vmax */
++#endif
+ #endif
