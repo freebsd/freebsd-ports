f52512fba7c3847cc777c41cf592c545ab80c3db
Jochen Neumeister <joneum@FreeBSD.org>
2018-12-14 13:57:09 +0000

    www:mod_fastcgi: Update to 2.4.7.1
    The website of fastcgi has been down for a long time.
    The project is now in a GitHub archive: https://github.com/FastCGI-Archives
    
    Also switch to GitHub with www/mod_fastcgi
    
    Changelog:
    *) cast from pointer to integer of different size fix
    *) fix prinf %d warning
    *) .cvsignore to .gitignore
    *) apache 2.4 compile fix
    
    - While I am here, use "make makepatch" for the patches in /files
    
    Approved by:	brnrd (apache)
    With hat:	apache
    Sponsored by:	Netzkommune GmbH
    Differential Revision:	https://reviews.freebsd.org/D18471

--- www/mod_fastcgi/files/patch-fcgi_protocol.c
+++ www/mod_fastcgi/files/patch-fcgi_protocol.c
@@ -1,10 +1,10 @@
---- fcgi_protocol.c.orig
+--- fcgi_protocol.c.orig	2017-03-29 21:11:44 UTC
 +++ fcgi_protocol.c
-@@ -475,7 +475,7 @@
+@@ -475,7 +475,7 @@ int fcgi_protocol_dequeue(pool *p, fcgi_request *fr)
                          ap_log_rerror(FCGI_LOG_ERR_NOERRNO, fr->r,
                              "FastCGI: comm with server \"%s\" aborted: protocol error: "
                              "invalid FCGI_END_REQUEST size: "
--                            "%d != sizeof(FCGI_EndRequestBody)(%d)",
+-                            "%d != sizeof(FCGI_EndRequestBody)(%ld)",
 +                            "%d != sizeof(FCGI_EndRequestBody)(%zu)",
                              fr->fs_path, fr->dataLen, sizeof(FCGI_EndRequestBody));
                          return HTTP_INTERNAL_SERVER_ERROR;
--- www/mod_fastcgi/files/patch-mod_fastcgi.c
+++ www/mod_fastcgi/files/patch-mod_fastcgi.c
@@ -1,18 +1,6 @@
---- mod_fastcgi.c.orig
+--- mod_fastcgi.c.orig	2017-03-29 21:11:44 UTC
 +++ mod_fastcgi.c
-@@ -753,6 +753,11 @@
-             	continue;
-             }
- 
-+            if (strcasecmp(name, "Content-Length") == 0) {
-+                ap_set_content_length(r, strtol(value, NULL, 10));
-+                continue;
-+            }
-+
-             /* If the script wants them merged, it can do it */
-             ap_table_add(r->err_headers_out, name, value);
-             continue;
-@@ -2215,7 +2220,7 @@
+@@ -2219,7 +2219,7 @@ SERVER_SEND:
          if (select_status < 0)
          {
              ap_log_rerror(FCGI_LOG_ERR_ERRNO, r, "FastCGI: comm with server "
@@ -21,12 +9,12 @@
              state = STATE_ERROR;
              break;
          }
-@@ -2298,7 +2303,7 @@
+@@ -2302,7 +2302,7 @@ SERVER_SEND:
              	else 
              	{
-             		ap_log_rerror(FCGI_LOG_ERR, r, "FastCGI: comm with server "
--            				"\"%s\" aborted: read failed", fr->fs_path);
-+            				"\"%s\" aborted: read failed: \"%s\"", fr->fs_path, strerror(errno));
-             		state = STATE_ERROR;
-             		break;
-             	}
+                 ap_log_rerror(FCGI_LOG_ERR, r, "FastCGI: comm with server "
+-                    "\"%s\" aborted: read failed", fr->fs_path);
++                    "\"%s\" aborted: read failed: \"%s\"", fr->fs_path, strerror(errno));
+                 state = STATE_ERROR;
+                 break;
+             }
