64125521bd6054c37fcdb22e08cd495e6c6a2ae9
Matthew Seaman <matthew@FreeBSD.org>
2018-12-15 09:40:42 +0000

    Update to 8.0.2
    ChangeLog:	https://github.com/citusdata/citus/blob/v8.0.2/CHANGELOG.md

--- databases/pg_citus/files/patch-src_backend_distributed_executor_multi__real__time__executor.c
+++ databases/pg_citus/files/patch-src_backend_distributed_executor_multi__real__time__executor.c
@@ -1,8 +1,8 @@
---- src/backend/distributed/executor/multi_real_time_executor.c.orig	2018-01-17 22:05:28 UTC
+--- src/backend/distributed/executor/multi_real_time_executor.c.orig	2018-12-13 13:22:48 UTC
 +++ src/backend/distributed/executor/multi_real_time_executor.c
-@@ -20,6 +20,7 @@
- #include "miscadmin.h"
+@@ -21,6 +21,7 @@
  
+ #include <sys/stat.h>
  #include <unistd.h>
 +#include <sys/stat.h>
  
