7a49012806b2fcdcc5e554f50d79819f7ba4d040
tobik <tobik@FreeBSD.org>
2018-12-21 10:37:09 +0000

    lang/rust: Update to 1.31.1
    - Simplify .cargo-checksum.json handling [1].  Similar to USES=cargo
      we disable vendor checksum checking by clearing the list of files
      in all .cargo-checksum.json, so that cargo does not complain when
      we patch something.  The overall distfile is already checked by
      us which should be enough.
    - Add an overridable CARGO_VENDOR_DIR variable to set the crate
      vendor directory which has moved location in nightly
    - Add upstream openssl-sys patch to fix build with LibreSSL 2.9.0 [2]
    
    Changes:	https://blog.rust-lang.org/2018/12/20/Rust-1.31.1.html
    Submitted by:	mikael.urankar@gmail.com [1], ml@vishwin.info [2]
    Differential Revision:	https://reviews.freebsd.org/D18610

--- lang/rust/files/patch-src_vendor_openssl-sys_build_main.rs
+++ lang/rust/files/patch-src_vendor_openssl-sys_build_main.rs
@@ -1,6 +1,7 @@
 https://github.com/sfackler/rust-openssl/commit/9fd7584a8416
+https://github.com/sfackler/rust-openssl/commit/e75793e8962d
 
---- src/vendor/openssl-sys/build/main.rs.orig	2018-12-03 20:08:24 UTC
+--- src/vendor/openssl-sys/build/main.rs.orig	2018-12-05 01:24:32 UTC
 +++ src/vendor/openssl-sys/build/main.rs
 @@ -1,9 +1,9 @@
  extern crate cc;
@@ -45,11 +46,21 @@ https://github.com/sfackler/rust-openssl/commit/9fd7584a8416
      use std::process::{self, Command};
  
      use super::env;
-@@ -500,6 +503,7 @@ See rust-openssl README for more information:
+@@ -500,6 +503,8 @@ See rust-openssl README for more information:
              (7, _) => ('7', 'x'),
              (8, 0) => ('8', '0'),
              (8, 1) => ('8', '1'),
 +            (8, _) => ('8', 'x'),
++            (9, 0) => ('9', '0'),
              _ => version_error(),
          };
  
+@@ -540,7 +545,7 @@ fn version_error() -> ! {
+         "
+ 
+ This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
+-through 2.8.1, but a different version of OpenSSL was found. The build is now aborting
++through 2.9.0, but a different version of OpenSSL was found. The build is now aborting
+ due to this version mismatch.
+ 
+ "
