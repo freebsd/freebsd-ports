4ed8d3970d805c5c1eb52fb657e9f1d4c6affbb8
mmokhi <mmokhi@FreeBSD.org>
2018-12-22 17:16:56 +0000

    databases/mysql57-client: Fix the build with GCC-based architectures This patch adds a condition to FreeBSD part of MySQL CMake-scripts to recognize gcc as well as Clang, while taking care of Unsupported compilers too.
    This is an enhancement on the initial patch by Piotr Kubaj <pkubaj@anongoth.pl> that
    committed by linimon on r488087.
    
    PR:		234172
    Sponsored by:	The FreeBSD Foundation

--- databases/mysql57-client/files/patch-cmake_os_FreeBSD.cmake
+++ databases/mysql57-client/files/patch-cmake_os_FreeBSD.cmake
@@ -1,11 +1,23 @@
 --- cmake/os/FreeBSD.cmake.orig	2018-12-19 17:43:09 UTC
 +++ cmake/os/FreeBSD.cmake
-@@ -30,8 +30,6 @@ IF(NOT FORCE_UNSUPPORTED_COMPILER)
+@@ -30,6 +30,12 @@ IF(NOT FORCE_UNSUPPORTED_COMPILER)
      IF(NOT HAVE_SUPPORTED_CLANG_VERSION)
        MESSAGE(FATAL_ERROR "Clang 3.3 or newer is required!")
      ENDIF()
--  ELSE()
--    MESSAGE(FATAL_ERROR "Unsupported compiler!")
++  ELSEIF(CMAKE_COMPILER_IS_GNUCC)
++    EXECUTE_PROCESS(COMMAND ${CMAKE_C_COMPILER} -dumpversion
++                    OUTPUT_VARIABLE GCC_VERSION)
++    IF(GCC_VERSION VERSION_LESS 4.4)
++      MESSAGE(FATAL_ERROR "GCC 4.4 or newer is required!")
++    ENDIF()
+   ELSE()
+     MESSAGE(FATAL_ERROR "Unsupported compiler!")
    ENDIF()
- ENDIF()
- 
+@@ -38,4 +44,6 @@ ENDIF()
+ # Should not be needed any more, but kept for easy resurrection if needed
+ #   #Legacy option, maybe not needed anymore , taken as is from autotools build
+ #   ADD_DEFINITIONS(-DNET_RETRY_COUNT=1000000)
+-
++# For GCC maybe it's also good idea to use
++#   ADD_DEFINITIONS(-D_GNU_SOURCE)
++    
