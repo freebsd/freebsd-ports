c6f9b46efe11c10fdd46e67cfe61be6f170c4bf4
mfechner <mfechner@FreeBSD.org>
2018-12-23 12:49:25 +0000

    Upgrade to 11.6.0. https://about.gitlab.com/2018/12/22/gitlab-11-6-released/
    Approved by:	mentors (implicit)

--- www/gitlab-ce/files/patch-config_initializers_1__settings.rb
+++ www/gitlab-ce/files/patch-config_initializers_1__settings.rb
@@ -1,6 +1,6 @@
---- config/initializers/1_settings.rb.orig	2018-08-22 22:40:32 UTC
+--- config/initializers/1_settings.rb.orig	2018-12-21 14:23:37 UTC
 +++ config/initializers/1_settings.rb
-@@ -126,12 +126,8 @@ Settings.gitlab['email_subject_suffix'] 
+@@ -126,12 +126,8 @@ Settings.gitlab['email_subject_suffix'] ||= ENV['GITLA
  Settings.gitlab['base_url']   ||= Settings.__send__(:build_base_gitlab_url)
  Settings.gitlab['url']        ||= Settings.__send__(:build_gitlab_url)
  Settings.gitlab['user']       ||= 'git'
