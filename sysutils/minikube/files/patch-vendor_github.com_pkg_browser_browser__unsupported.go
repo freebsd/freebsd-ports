53de4e4b5da6c6fe45ef75eccfd069ea184cb430
Danilo Egea Gondolfo <danilo@FreeBSD.org>
2018-12-25 22:04:20 +0000

    - Update to 0.32.0 - Use symlinks for OS dependent files which don't need changes instead of keep them in files/

---
+++ sysutils/minikube/files/patch-vendor_github.com_pkg_browser_browser__unsupported.go
@@ -0,0 +1,8 @@
+--- vendor/github.com/pkg/browser/browser_unsupported.go.orig	2018-12-25 00:08:26 UTC
++++ vendor/github.com/pkg/browser/browser_unsupported.go
+@@ -1,4 +1,4 @@
+-// +build !linux,!windows,!darwin
++// +build !linux,!windows,!darwin,!freebsd
+ 
+ package browser
+ 
