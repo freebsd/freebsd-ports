836c3e08d5e1027dca2e4d0319c4abfeb97c7922
Ryan Steinmetz <zi@FreeBSD.org>
2018-12-25 15:37:09 +0000

    - Add 13.x support
    PR:		234395
    Submitted by:	Mark Linimon <linimon@freebsd.org>

---
+++ net-mgmt/net-snmp/files/patch-include_net-snmp_system_freebsd13.h
@@ -0,0 +1,6 @@
+--- include/net-snmp/system/freebsd13.h.orig	2018-12-25 15:35:28 UTC
++++ include/net-snmp/system/freebsd13.h
+@@ -0,0 +1,3 @@
++/* freebsd13 is a superset of freebsd12 */
++#include "freebsd12.h"
++#define freebsd13 freebsd13
