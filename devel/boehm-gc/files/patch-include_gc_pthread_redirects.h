f69daacc69ee23e3983143936b5bc426470dae95
swills <swills@FreeBSD.org>
2018-12-26 22:35:30 +0000

    devel/boehm-gc: Update to 8.0.2
    (Use correct patch this time)
    
    PR:		233976
    Submitted by:	Yasuhiro KIMURA <yasu@utahime.org> (maintainer)
    Pointyhat to:	swills

--- devel/boehm-gc/files/patch-include_gc_pthread_redirects.h
+++ devel/boehm-gc/files/patch-include_gc_pthread_redirects.h
@@ -1,9 +1,9 @@
---- include/gc_pthread_redirects.h.orig	2018-01-04 14:41:42 UTC
+--- include/gc_pthread_redirects.h.orig	2018-12-23 21:28:41 UTC
 +++ include/gc_pthread_redirects.h
-@@ -44,12 +44,8 @@
+@@ -53,12 +53,8 @@
+ # endif /* !GC_NO_DLOPEN */
  
  # ifndef GC_NO_PTHREAD_SIGMASK
- #   include <signal.h>  /* needed anyway for proper redirection */
 -#   if defined(GC_PTHREAD_SIGMASK_NEEDED) \
 -        || defined(_BSD_SOURCE) || defined(_GNU_SOURCE) \
 -        || (_POSIX_C_SOURCE >= 199506L) || (_XOPEN_SOURCE >= 500)
