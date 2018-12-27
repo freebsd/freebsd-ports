862cb2d249ec5bf8137415f2062887cc5463ae44
Wen Heping <wen@FreeBSD.org>
2018-12-27 07:58:47 +0000

    - Update to 0.3.6
    PR:		233372
    Submitted by:	numisemis@yahoo.com
    Approved by:	maintainer(timeout, > 4 weeks)

--- www/py-kallithea/files/patch-setup.py
+++ www/py-kallithea/files/patch-setup.py
@@ -1,18 +1,20 @@
---- setup.py.orig	2016-05-02 16:57:01 UTC
+--- setup.py.orig	2018-11-04 20:39:44 UTC
 +++ setup.py
-@@ -35,36 +35,36 @@ __platform__ = platform.system()
+@@ -35,38 +35,38 @@ __platform__ = platform.system()
  is_windows = __platform__ in ['Windows']
  
  requirements = [
+-    "setuptools<34", # setuptools==34 has an undeclared requirement of pyparsing >=2.1, but celery<2.3 requires pyparsing<2
 -    "waitress==0.8.8",
 -    "webob>=1.0.8,<=1.1.1",
 -    "webtest==1.4.3",
--    "Pylons>=1.0.0,<=1.0.2",
+-    "Pylons>=1.0.0,<=1.0.3",
 -    "Beaker==1.6.4",
 -    "WebHelpers==1.3",
 -    "formencode>=1.2.4,<=1.2.6",
 -    "SQLAlchemy==0.7.10",
 -    "Mako>=0.9.0,<=1.0.0",
++    "setuptools",
 +    "waitress>=0.8.8",
 +    "webob>=1.0.8",
 +    "webtest>=1.4.3",
@@ -39,11 +41,13 @@
 -    "URLObject==2.3.4",
 -    "Routes==1.13",
 -    "dulwich>=0.9.9,<=0.9.9",
--    "mercurial>=2.9,<3.8",
+-    "mercurial>=2.9,<4.3",
+-    "bleach >= 3.0, < 3.1",
 +    "URLObject>=2.3.4",
 +    "Routes>=1.13",
 +    "dulwich>=0.9.9",
 +    "mercurial>=2.9",
++    "bleach>=2.1.3",
  ]
  
  if sys.version_info < (2, 7):
@@ -54,7 +58,7 @@
  
  if not is_windows:
 -    requirements.append("py-bcrypt>=0.3.0,<=0.4")
-+    requirements.append("py-bcrypt>=0.3.0")
++    requirements.append("bcrypt>=0.3.0")
  
  
  dependency_links = [
