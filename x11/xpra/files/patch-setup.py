d79b3b6f4c0161b8300fc96cc5a79b16d0ea4a9e
Steve Wills <swills@FreeBSD.org>
2018-12-28 03:06:57 +0000

    x11/xpra: update to 2.4.2

--- x11/xpra/files/patch-setup.py
+++ x11/xpra/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2018-05-08 17:35:32 UTC
+--- setup.py.orig	2018-10-14 16:59:57 UTC
 +++ setup.py
-@@ -1487,14 +1487,14 @@ else:
+@@ -1508,12 +1508,12 @@ else:
          if libexec_scripts:
              add_data_files("%s/xpra/" % libexec, libexec_scripts)
      man_path = "share/man"
@@ -8,16 +8,14 @@
 +    if OPENBSD or FREEBSD:
          man_path = "man"
      add_data_files("%s/man1" % man_path,  ["man/xpra.1", "man/xpra_launcher.1", "man/xpra_browser.1"])
-     add_data_files("share/xpra",          ["README", "COPYING"])
-     add_data_files("share/xpra/icons",    glob.glob("icons/*"))
-     add_data_files("share/applications",  ["xdg/xpra-launcher.desktop", "xdg/xpra-browser.desktop", "xdg/xpra.desktop"])
+     add_data_files("share/applications",  ["xdg/xpra-shadow.desktop", "xdg/xpra-launcher.desktop", "xdg/xpra-browser.desktop", "xdg/xpra.desktop"])
      add_data_files("share/mime/packages", ["xdg/application-x-xpraconfig.xml"])
--    add_data_files("share/icons",         ["xdg/xpra.png", "xdg/xpra-mdns.png"])
-+    add_data_files("share/pixmaps",         ["xdg/xpra.png", "xdg/xpra-mdns.png"])
+-    add_data_files("share/icons",         ["xdg/xpra.png", "xdg/xpra-mdns.png", "xdg/xpra-shadow.png"])
++    add_data_files("share/pixmaps",       ["xdg/xpra.png", "xdg/xpra-mdns.png", "xdg/xpra-shadow.png"])
      add_data_files("share/appdata",       ["xdg/xpra.appdata.xml"])
-     add_data_files('share/xpra/',         ["bell.wav"])
-     add_data_files('share/xpra/http-headers', glob.glob("http-headers/*"))
-@@ -1545,7 +1545,7 @@ else:
+ 
+     #here, we override build and install so we can
+@@ -1562,7 +1562,7 @@ else:
  
              if printing_ENABLED and POSIX:
                  #install "/usr/lib/cups/backend" with 0700 permissions:
@@ -26,7 +24,7 @@
  
              if x11_ENABLED:
                  #install xpra_Xdummy if we need it:
-@@ -1617,8 +1617,8 @@ else:
+@@ -1634,8 +1634,8 @@ else:
          add_packages("xpra.platform.xposix")
          remove_packages("xpra.platform.win32", "xpra.platform.darwin")
          #not supported by all distros, but doesn't hurt to install them anyway:
