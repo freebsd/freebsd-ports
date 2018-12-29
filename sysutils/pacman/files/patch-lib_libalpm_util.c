c18df55aebd9ebe03c5408898041e79f104086d0
amdmi3 <amdmi3@FreeBSD.org>
2018-12-29 23:13:15 +0000

    - Update to 5.1.2

--- sysutils/pacman/files/patch-lib_libalpm_util.c
+++ sysutils/pacman/files/patch-lib_libalpm_util.c
@@ -1,10 +1,19 @@
---- lib/libalpm/util.c.orig	2017-05-18 11:10:47 UTC
+--- lib/libalpm/util.c.orig	2018-11-19 01:25:18 UTC
 +++ lib/libalpm/util.c
-@@ -32,6 +32,7 @@
- #include <sys/wait.h>
+@@ -33,6 +33,7 @@
+ #include <sys/socket.h>
  #include <fnmatch.h>
  #include <poll.h>
 +#include <signal.h>
  
  /* libarchive */
  #include <archive.h>
+@@ -556,7 +557,7 @@ static void _alpm_reset_signals(void)
+ 	int *i, signals[] = {
+ 		SIGABRT, SIGALRM, SIGBUS, SIGCHLD, SIGCONT, SIGFPE, SIGHUP, SIGILL,
+ 		SIGINT, SIGKILL, SIGPIPE, SIGQUIT, SIGSEGV, SIGSTOP, SIGTERM, SIGTSTP,
+-		SIGTTIN, SIGTTOU, SIGUSR1, SIGUSR2, SIGPOLL, SIGPROF, SIGSYS, SIGTRAP,
++		SIGTTIN, SIGTTOU, SIGUSR1, SIGUSR2, SIGIO, SIGPROF, SIGSYS, SIGTRAP,
+ 		SIGURG, SIGVTALRM, SIGXCPU, SIGXFSZ,
+ 		0
+ 	};
