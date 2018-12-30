a779b4ec603a3775d017def3df655a247ab8d9ae
riggs <riggs@FreeBSD.org>
2018-12-30 07:59:57 +0000

    Update to upstream version 3.0.5

--- multimedia/vlc3/files/patch-modules_codec_aom.c
+++ multimedia/vlc3/files/patch-modules_codec_aom.c
@@ -1,10 +1,10 @@
---- modules/codec/aom.c.orig	2018-07-24 15:13:43 UTC
+--- modules/codec/aom.c.orig	2018-12-19 00:05:20 UTC
 +++ modules/codec/aom.c
-@@ -96,7 +96,6 @@ static const struct
-     { VLC_CODEC_I444, AOM_IMG_FMT_I444, 8, 0 },
+@@ -98,7 +98,6 @@ uint8_t          i_needs_hack;
+ { VLC_CODEC_I444, AOM_IMG_FMT_I444, 8, 0 },
  
-     { VLC_CODEC_YV12, AOM_IMG_FMT_YV12, 8, 0 },
--    { VLC_CODEC_YUVA, AOM_IMG_FMT_444A, 8, 0 },
+ { VLC_CODEC_YV12, AOM_IMG_FMT_YV12, 8, 0 },
+-{ VLC_CODEC_YUVA, AOM_IMG_FMT_444A, 8, 0 },
  
-     { VLC_CODEC_GBR_PLANAR, AOM_IMG_FMT_I444, 8, 1 },
-     { VLC_CODEC_GBR_PLANAR_10L, AOM_IMG_FMT_I44416, 10, 1 },
+ { VLC_CODEC_GBR_PLANAR, AOM_IMG_FMT_I444, 8, 1 },
+ { VLC_CODEC_GBR_PLANAR_10L, AOM_IMG_FMT_I44416, 10, 1 },
