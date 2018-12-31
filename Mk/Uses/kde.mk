69d02e1cbe7f93348405d9c902800dfc1787b0ad
tcberner <tcberner@FreeBSD.org>
2018-12-31 06:38:42 +0000

    Update KDE Plasma Desktop to 5.14.4
    https://www.kde.org/announcements/plasma-5.14.4.php
    
    PR:		222905

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -66,7 +66,7 @@ KDE4_APPLICATIONS_VERSION?=	15.04.3
 KDE4_BRANCH?=			stable
 
 # Current KDE desktop.
-KDE_PLASMA_VERSION?=		5.12.7
+KDE_PLASMA_VERSION?=		5.14.4
 KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
@@ -283,8 +283,8 @@ _USE_PLASMA_ALL=	activitymanagerd breeze breeze-gtk \
 			kdeplasma-addons kgamma5 kmenuedit kscreen \
 			kscreenlocker ksshaskpass ksysguard kwallet-pam \
 			kwayland-integration kwin kwrited libkscreen \
-			libksysguard milou oxygen plasma-desktop \
-			plasma-integration plasma-pa \
+			libksysguard milou oxygen plasma-browser-integration \
+			plasma-desktop plasma-integration plasma-pa \
 			plasma-sdk plasma-workspace plasma-workspace-wallpapers \
 			polkit-kde-agent-1 powerdevil systemsettings \
 			user-manager
@@ -708,6 +708,9 @@ milou_LIB=		libmilou.so.5
 
 oxygen_PORT= 		x11-themes/plasma5-oxygen
 oxygen_LIB=		liboxygenstyle5.so
+
+plasma-browser-integration_PORT=	www/plasma5-plasma-browser-integration
+plasma-browser-integration_PATH=	${KDE_PREFIX}/bin/plasma-browser-integration-host		
 
 plasma-desktop_PORT=	x11/plasma5-plasma-desktop
 plasma-desktop_PATH=	${KDE_PREFIX}/bin/krdb
