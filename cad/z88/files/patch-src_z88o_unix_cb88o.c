696cfdbd3831ee5fed5d2fe9b53fe9c50691a241
pi <pi@FreeBSD.org>
2018-12-31 13:33:55 +0000

    cad/z88: update 13.0 -> 15
    - Changes PORTVERSION to DISTVERSION
    - Sets submitter as MAINTAINER.
    - Uses USE_GITHUB instead of MASTER_SITES
    - Sets LICENSE
    - Sets appropriate dependencies, USES, USE_X
    - Adds an EXAMPLES option
    - Adds a pkg-message to explain how the port must be used, as it
      is not self-evident (The software assumes all files in the same
      directory instead of distributed into bin/, ${DATADIR} etc so I had
      to find a work around: the solution is easy for the user).
    
    PR:		233772
    Submitted by:	Lorenzo Salvadore <phascolarctos@protonmail.ch>
    Reviewed by:	chitty_cloud@me.com
    Relnotes:	https://forum.z88.de/viewtopic.php?f=18&t=885

---
+++ cad/z88/files/patch-src_z88o_unix_cb88o.c
@@ -0,0 +1,37 @@
+--- src/z88o/unix/cb88o.c.orig	2018-12-05 16:59:00 UTC
++++ src/z88o/unix/cb88o.c
+@@ -1164,9 +1164,9 @@ const gchar *ccx,*ccy,*ccz;
+ /*---------------------------------------------------------------------------
+ * beim allerersten Start gehen fux, fuy & fuz mit je 100. rein
+ *--------------------------------------------------------------------------*/
+-gcvt(fux,idigit,cfux); 
+-gcvt(fuy,idigit,cfuy); 
+-gcvt(fuz,idigit,cfuz); 
++snprintf(cfux, idigit, "%g", fux);
++snprintf(cfuy, idigit, "%g", fuy);
++snprintf(cfuz, idigit, "%g",cfuz);
+ 
+ if(LANG == 1) strcpy(cstring,"VERGROESSERUNG VER.");
+ if(LANG == 2) strcpy(cstring,"MAGNIFICATION DEFL.");
+@@ -1234,9 +1234,9 @@ const gchar *ccx,*ccy,*ccz;
+ /*---------------------------------------------------------------------------
+ * beim allerersten Start gehen rotx, roty & rotz mit je 0. rein
+ *--------------------------------------------------------------------------*/
+-gcvt(rotx,idigit,crotx); 
+-gcvt(roty,idigit,croty); 
+-gcvt(rotz,idigit,crotz); 
++snprintf(crotx, idigit, "%g", rotx);
++snprintf(croty, idigit, "%g", roty);
++snprintf(crotz, idigit, "%g", rotz);
+ 
+ if(LANG == 1) strcpy(cstring,"ROTATIONEN 3-D");
+ if(LANG == 2) strcpy(cstring,"ROTATIONS 3-D");
+@@ -1307,7 +1307,7 @@ const gchar *ccm;
+ /*---------------------------------------------------------------------------
+ * beim allerersten Start geht zm mit -100. rein
+ *--------------------------------------------------------------------------*/
+-gcvt(zm,idigit,czm); 
++snprintf(czm, idigit, "%g", zm); 
+ 
+ if(LANG == 1) strcpy(cstring,"Z-Limit zum Betrachter");
+ if(LANG == 2) strcpy(cstring,"Z Limit towards you");
