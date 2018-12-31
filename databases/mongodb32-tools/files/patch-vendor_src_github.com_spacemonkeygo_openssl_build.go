d1a5353b9cccb568b52fb525175941ad0b8a0345
Rene Ladan <rene@FreeBSD.org>
2018-12-31 13:37:58 +0000

    Remove expired ports: 2018-12-31 net/libsrtp: Superseded by libsrtp2 2018-12-31 x11-toolkits/wlc: Deprecated upstream 2018-12-31 japanese/spamassassin: does not build 2018-12-31 www/spdylay: SPDY is deprecated, use HTTP/2 instead 2018-12-31 www/moodle33: Upstream no longer maintained 2018-12-31 security/krb5-114: EOL twelve months after release of krb5-1.16 2018-12-31 databases/mongodb32: EOL upstream since September 2018 2018-12-31 databases/mongodb32-tools: EOL upstream since September 2018 2018-12-31 audio/creox: Qt4 has been EOL since december 2015 2018-12-31 audio/terminatorx: Unmaintained, broken too often 2018-12-31 audio/shoutcast: Upstream vendor no longer provides releases 2018-12-31 dns/powerdns-recursor40: FreeBSD 10.3 has reached EoL since 2018-04-30; users should upgrade and use dns/powerdns-recursor instead 2018-12-31 sysutils/ataidle: obsoleted by camcontrol(8) 2018-12-31 sysutils/syslog-ng37: No longer officially supported by Balabit, does not support openssl111 2018-12-31 sysutils/cfengine22: No longeer supported by cfengine.com 2018-12-31 sysutils/syslog-ng36: No longer officially supported by Balabit, does not support openssl111

--- databases/mongodb32-tools/files/patch-vendor_src_github.com_spacemonkeygo_openssl_build.go
+++
@@ -1,9 +0,0 @@
---- vendor/src/github.com/spacemonkeygo/openssl/build.go.orig	2016-08-30 17:14:55 UTC
-+++ vendor/src/github.com/spacemonkeygo/openssl/build.go
-@@ -19,5 +19,6 @@ package openssl
- // #cgo linux pkg-config: openssl
- // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
- // #cgo darwin CFLAGS: -Wno-deprecated-declarations
-+// #cgo freebsd LDFLAGS: -lssl -lcrypto
- // #cgo darwin LDFLAGS: -lssl -lcrypto
- import "C"
