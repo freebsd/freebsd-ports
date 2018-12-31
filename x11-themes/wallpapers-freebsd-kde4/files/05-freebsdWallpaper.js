a0d6e04cd972fee7c427788e16be1425ecea3fc1
rene <rene@FreeBSD.org>
2018-12-31 18:39:25 +0000

    Remove some more KDE4 ports and hopefully fix INDEX again 2019-03-15 textproc/uim-kde4: Qt4 has been EOL since december 2015 misc/kde4-xdg-env x11-themes/wallpapers-freebsd-kde4

--- x11-themes/wallpapers-freebsd-kde4/files/05-freebsdWallpaper.js
+++
@@ -1,8 +0,0 @@
-var activitiesList = activities();
-for (var i in activitiesList) {
-  var activity = activitiesList[i];
-  activity.wallpaperPlugin = "image";
-  activity.wallpaperMode = "SingleImage";
-  activity.currentConfigGroup = new Array("Wallpaper", "image");
-  activity.writeConfig("wallpaper", "stripes-freebsd-blue");
-}
