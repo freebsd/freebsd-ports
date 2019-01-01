98873c29a80f6a68d26052e14cb46f93125d6011
sunpoet <sunpoet@FreeBSD.org>
2019-01-01 20:34:49 +0000

    Update to 0.40
    Changes:	https://metacpan.org/changes/distribution/Math-GSL

--- math/p5-Math-GSL/files/patch-inc-GSLBuilder.pm
+++ math/p5-Math-GSL/files/patch-inc-GSLBuilder.pm
@@ -1,15 +1,15 @@
---- inc/GSLBuilder.pm.orig	2014-09-25 17:36:46 UTC
+--- inc/GSLBuilder.pm.orig	2018-12-10 17:11:25 UTC
 +++ inc/GSLBuilder.pm
-@@ -266,7 +266,7 @@ sub link_c {
+@@ -262,7 +262,7 @@ sub link_c {
        }
      }
  
 -    my @lddlflags = $self->split_like_shell($cf->{lddlflags});
 +    my @lddlflags = $self->split_like_shell($Config{lddlflags});
-     my @shrp = $self->split_like_shell($cf->{shrpenv});
-     my @ld = $self->split_like_shell($cf->{ld} || $Config{cc});
+     my @shrp      = $self->split_like_shell($cf->{shrpenv});
+     my @ld        = $self->split_like_shell($cf->{ld} || $Config{cc});
  
-@@ -298,7 +298,7 @@ sub compile_c {
+@@ -294,7 +294,7 @@ sub compile_c {
  
    $cf->{installarchlib} = $Config{archlib};
  
