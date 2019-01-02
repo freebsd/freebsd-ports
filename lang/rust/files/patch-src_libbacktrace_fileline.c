701b2fb530976be6b0928c625e1eac85c5c1f8ef
Jan Beich <jbeich@FreeBSD.org>
2019-01-02 16:37:02 +0000

    lang/rust: disclaim /proc fix

--- lang/rust/files/patch-src_libbacktrace_fileline.c
+++ lang/rust/files/patch-src_libbacktrace_fileline.c
@@ -1,3 +1,5 @@
+This file is in the public domain.
+
 --- src/libbacktrace/fileline.c.orig	2017-06-06 00:42:59 UTC
 +++ src/libbacktrace/fileline.c
 @@ -38,8 +38,39 @@ POSSIBILITY OF SUCH DAMAGE.  */
