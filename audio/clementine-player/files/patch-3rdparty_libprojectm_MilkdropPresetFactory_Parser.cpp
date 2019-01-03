bb8cd08fc9bba16e4a7975a0d27846e4acf45a3c
Sean Bruno <sbruno@FreeBSD.org>
2019-01-03 21:43:57 +0000

    audio/clementine-player:  Jump to qt5 - regenerate some of the patch files for this new branch - cleanup and try to adjust Makefile
    audio/libechonest: Jump to qt5
    - clementine-player is the only port that I could find that has an explicit
      dependency on libechonest.  Commit this update so there is no window for
      build failures.
    
    Test built on 13-CURRENT, 12-STABLE, 11-STABLE across i386 and amd64.
    Run-time tested on 13-CURRENT, amd64.
    
    PR:		234534 226529
    Submitted by:	greg@unrelenting.technology

--- audio/clementine-player/files/patch-3rdparty_libprojectm_MilkdropPresetFactory_Parser.cpp
+++ audio/clementine-player/files/patch-3rdparty_libprojectm_MilkdropPresetFactory_Parser.cpp
@@ -1,6 +1,6 @@
---- 3rdparty/libprojectm/MilkdropPresetFactory/Parser.cpp.orig	2018-01-31 22:06:57.261592000 +0000
-+++ 3rdparty/libprojectm/MilkdropPresetFactory/Parser.cpp	2018-01-31 22:07:38.081299000 +0000
-@@ -1373,7 +1373,7 @@
+--- 3rdparty/libprojectm/MilkdropPresetFactory/Parser.cpp.orig	2018-12-05 17:16:34 UTC
++++ 3rdparty/libprojectm/MilkdropPresetFactory/Parser.cpp
+@@ -1373,7 +1373,7 @@ PerFrameEqn * Parser::parse_implicit_per_frame_eqn(std
    PerFrameEqn * per_frame_eqn;
    GenExpr * gen_expr;
  
@@ -9,7 +9,7 @@
      return NULL;
    if (param_string == NULL)
      return NULL;
-@@ -1528,7 +1528,7 @@
+@@ -1528,7 +1528,7 @@ InitCond * Parser::parse_per_frame_init_eqn(std::istre
  
    if (preset == NULL)
      return NULL;
@@ -18,7 +18,7 @@
      return NULL;
  
    if ((token = parseToken(fs, name)) != tEq)
-@@ -1842,7 +1842,7 @@
+@@ -1842,7 +1842,7 @@ int Parser::parse_shapecode(char * token, std::istream
    /* Null argument checks */
    if (preset == NULL)
      return PROJECTM_FAILURE;
@@ -27,7 +27,7 @@
      return PROJECTM_FAILURE;
    if (token == NULL)
      return PROJECTM_FAILURE;
-@@ -2133,7 +2133,7 @@
+@@ -2133,7 +2133,7 @@ int Parser::parse_wave(char * token, std::istream &  f
  
    if (token == NULL)
      return PROJECTM_FAILURE;
@@ -36,7 +36,7 @@
      return PROJECTM_FAILURE;
    if (preset == NULL)
      return PROJECTM_FAILURE;
-@@ -2315,7 +2315,7 @@
+@@ -2315,7 +2315,7 @@ int Parser::parse_shape(char * token, std::istream &  
    if (token == NULL)
  
      return PROJECTM_FAILURE;
