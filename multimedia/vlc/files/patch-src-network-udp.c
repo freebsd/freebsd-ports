143bd119c461f6141b3385dc416814123e7ddd2a
riggs <riggs@FreeBSD.org>
2019-01-03 14:12:03 +0000

    Retire multimedia/vlc3; update multimedia/vlc to latest upstream 3.0.5

--- multimedia/vlc/files/patch-src-network-udp.c
+++ multimedia/vlc/files/patch-src-network-udp.c
@@ -1,11 +1,11 @@
---- src/network/udp.c.orig
+--- src/network/udp.c.orig	2018-01-17 13:29:46 UTC
 +++ src/network/udp.c
-@@ -383,7 +383,7 @@ int net_Subscribe (vlc_object_t *obj, in
+@@ -315,7 +315,7 @@ net_SourceSubscribe (vlc_object_t *obj, 
  {
- /* MCAST_JOIN_GROUP was introduced to OS X in v10.7, but it doesn't work,
-  * so ignore it to use the same code as on 10.5 or 10.6 */
--#if defined (MCAST_JOIN_GROUP) && !defined (__APPLE__)
-+#if defined (MCAST_JOIN_GROUP) && !defined (__APPLE__) && !defined (__FreeBSD__)
-     /* Agnostic SSM multicast join */
+ /* MCAST_JOIN_SOURCE_GROUP was introduced to OS X in v10.7, but it doesn't work,
+  * so ignore it to use the same code path as on 10.5 or 10.6 */
+-#if defined (MCAST_JOIN_SOURCE_GROUP) && !defined (__APPLE__)
++#if defined (MCAST_JOIN_SOURCE_GROUP) && !defined (__APPLE__) && !defined (__FreeBSD__)
+     /* Family-agnostic Source-Specific Multicast join */
      int level;
-     struct group_req gr;
+     struct group_source_req gsr;
