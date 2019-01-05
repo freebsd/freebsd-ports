2ea601c279f1be27a654489ae30046352572feb1
vanilla <vanilla@FreeBSD.org>
2019-01-05 03:41:42 +0000

    Update to 1.9.0.

--- security/elixir-jose/files/patch-mix.exs
+++ security/elixir-jose/files/patch-mix.exs
@@ -1,10 +1,10 @@
---- mix.exs.orig	2017-02-02 15:47:04 UTC
+--- mix.exs.orig	2018-12-31 15:47:40 UTC
 +++ mix.exs
-@@ -8,7 +8,6 @@ defmodule JOSE.Mixfile do
-      erlc_options: erlc_options(),
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
--     deps: deps(),
-      name: "JOSE",
-      source_url: "https://github.com/potatosalad/erlang-jose",
-      docs: fn ->
+@@ -9,7 +9,6 @@ defmodule JOSE.Mixfile do
+       erlc_options: erlc_options(),
+       build_embedded: Mix.env() == :prod,
+       start_permanent: Mix.env() == :prod,
+-      deps: deps(),
+       name: "JOSE",
+       source_url: "https://github.com/potatosalad/erlang-jose",
+       docs: fn ->
