a6885ac1ef7b1602378b13250c8232d7cd7da814
Larry Rosenman <ler@FreeBSD.org>
2019-01-06 17:46:53 +0000

    mail/dovecot: Pick up mailing list patch for imap-preauth vs. stats-writer.
    see the dovecot mailing list thread on imap-preauth and stats-writer between
    Stephan Bosch and a FreeBSD user
    
    Obtained from:	upstream mailing list.

---
+++ mail/dovecot/files/patch-src_lib-master_master-service.c
@@ -0,0 +1,11 @@
+--- src/lib-master/master-service.c.orig	2019-01-06 17:42:52 UTC
++++ src/lib-master/master-service.c
+@@ -341,7 +341,7 @@ master_service_init(const char *name, enum master_serv
+ 	if ((flags & MASTER_SERVICE_FLAG_DONT_SEND_STATS) == 0) {
+ 		/* Initialize stats-client early so it can see all events. */
+ 		value = getenv(DOVECOT_STATS_WRITER_SOCKET_PATH);
+-		if (value != NULL)
++		if (value != NULL && *value = '\0')
+ 			service->stats_client = stats_client_init(value, FALSE);
+ 	}
+ 
