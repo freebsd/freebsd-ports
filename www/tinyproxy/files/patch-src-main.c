5e4fb3aeb515b9a070b3b5a2d17fb49caec8f2ed
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-01-06 20:39:53 +0000

    Update to 1.10.0
    - Update manpages
    
    Note that the security fix for CVE-2017-11747 to generate pid file before
    dropping privilege is already done in the src/main.c patch.
    
    Changes:	https://github.com/tinyproxy/tinyproxy/releases

--- www/tinyproxy/files/patch-src-main.c
+++ www/tinyproxy/files/patch-src-main.c
@@ -1,32 +1,26 @@
---- src/main.c.orig	2014-12-13 00:57:23 UTC
+--- src/main.c.orig	2018-09-01 02:06:59 UTC
 +++ src/main.c
-@@ -417,12 +417,9 @@ main (int argc, char **argv)
-                 exit (EX_OSERR);
-         }
- 
--        /* Switch to a different user if we're running as root */
--        if (geteuid () == 0)
--                change_user (argv[0]);
--        else
--                log_message (LOG_WARNING,
--                             "Not running as root, so not changing UID/GID.");
-+	/* To get this to run correctly on FreeBSD, we must
-+	   create the PID and log files before we drop privileges.
-+	   Other ideas are welcome */
- 
-         /* Create log file after we drop privileges */
-         if (setup_logging ()) {
-@@ -438,6 +435,13 @@ main (int argc, char **argv)
+@@ -395,6 +395,11 @@ main (int argc, char **argv)
                  }
          }
  
-+        /* Switch to a different user if we're running as root */
-+        if (geteuid () == 0)
-+                change_user (argv[0]);
-+        else
-+                log_message (LOG_WARNING,
-+                             "Not running as root, so not changing UID/GID.");
++        /* Create log file before we drop privileges */
++        if (setup_logging ()) {
++                exit (EX_SOFTWARE);
++        }
 +
+         /* Switch to a different user if we're running as root */
+         if (geteuid () == 0)
+                 change_user (argv[0]);
+@@ -402,11 +407,6 @@ main (int argc, char **argv)
+                 log_message (LOG_WARNING,
+                              "Not running as root, so not changing UID/GID.");
+ 
+-        /* Create log file after we drop privileges */
+-        if (setup_logging ()) {
+-                exit (EX_SOFTWARE);
+-        }
+-
          if (child_pool_create () < 0) {
                  fprintf (stderr,
                           "%s: Could not create the pool of children.\n",
