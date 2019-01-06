d4148ae4d2e85148031b66eae8f8a810bd467685
Conrad Meyer <cem@FreeBSD.org>
2019-01-06 07:21:24 +0000

    mail/fetchmail: backport less-old TLS support from Fedora
    Allows options like 'sslproto "TLS1.2+"', for example.
    
    PR:		233247
    Submitted by:	Corey Halpin <chalpin@cs.wisc.edu> (maintainer)
    Approved by:	koobs

--- mail/fetchmail/files/patch-socket.c
+++ mail/fetchmail/files/patch-socket.c
@@ -1,6 +1,6 @@
---- socket.c.orig	2013-04-23 20:00:45 UTC
-+++ socket.c
-@@ -907,14 +907,19 @@ int SSLOpen(int sock, char *mycert, char
+--- socket.c.orig	2018-12-30 11:43:19.042365000 -0600
++++ socket.c	2018-12-30 12:46:39.062201000 -0600
+@@ -910,7 +910,7 @@
  	_ssl_context[sock] = NULL;
  	if(myproto) {
  		if(!strcasecmp("ssl2",myproto)) {
@@ -8,17 +8,13 @@
 +#if (HAVE_DECL_SSLV2_CLIENT_METHOD + 0 > 0) && !defined(OPENSSL_NO_SSL2)
  			_ctx[sock] = SSL_CTX_new(SSLv2_client_method());
  #else
--			report(stderr, GT_("Your operating system does not support SSLv2.\n"));
-+			report(stderr, GT_("Your OpenSSL version does not support SSLv2.\n"));
- 			return -1;
+ 			report(stderr, GT_("Your OpenSSL version does not support SSLv2.\n"));
+@@ -918,7 +918,7 @@
  #endif
+ 			avoid_ssl_versions &= ~SSL_OP_NO_SSLv2;
  		} else if(!strcasecmp("ssl3",myproto)) {
-+#ifndef OPENSSL_NO_SSL3_METHOD
+-#if HAVE_DECL_SSLV3_CLIENT_METHOD + 0 > 0
++#if (HAVE_DECL_SSLV3_CLIENT_METHOD + 0 > 0) && !defined(OPENSSL_NO_SSL3_METHOD)
  			_ctx[sock] = SSL_CTX_new(SSLv3_client_method());
-+#else
-+			report(stderr, GT_("Your OpenSSL version does not support SSLv3.\n"));
-+			return -1;
-+#endif
- 		} else if(!strcasecmp("tls1",myproto)) {
- 			_ctx[sock] = SSL_CTX_new(TLSv1_client_method());
- 		} else if (!strcasecmp("ssl23",myproto)) {
+ #else
+ 			report(stderr, GT_("Your OpenSSL version does not support SSLv3.\n"));
