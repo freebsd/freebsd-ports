e2c3fdc1fdeca1ffbdfef680e3e712ddc2e7e06f
Gleb Popov <arrowd@FreeBSD.org>
2019-01-06 12:18:06 +0000

    lang/ghc: Update to 8.6.3 and bump PORTREVISION's of all Haskell ports.
    As usual, it is recommended to rebuild or reinstall all the
    dependent ports and the lang/ghc port itself in one of the following
    ways:
    
    # portmaster -w -r ghc
      or
    # portupgrade -fr lang/ghc
    
    In case of pkg(8), it is probably safer to remove all the GHC-dependent
    packages along with GHC and reinstall everything from scratch.  For
    example:
    
    # pkg query "%ro" ghc > ghc-pkgs.txt
    # pkg delete -y lang/ghc
    # pkg install -y `cat ghc-pkgs.txt`
    
    During update some hs-* ports got two PORTREVISION bumps in a row. Other ports
    got a PORTVERSION update together with one PORTREVISION bump. This is caused
    by bulk-bumping PORTREVISION of all hs-* ports. There are a lot of them updated,
    so figuring out which ones require a bump and which are not is too tedious.
    
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D18707

--- lang/ghc/files/patch-ghc.mk
+++ lang/ghc/files/patch-ghc.mk
@@ -1,4 +1,4 @@
---- ghc.mk.orig	2017-01-03 15:59:18 UTC
+--- ghc.mk.orig 2018-09-16 20:53:54 UTC
 +++ ghc.mk
 @@ -96,6 +96,7 @@ endif
  # Catch make if it runs away into an infinite loop
@@ -8,14 +8,13 @@
  else
  $(error Make has restarted itself $(MAKE_RESTARTS) times; is there a makefile bug? See http://ghc.haskell.org/trac/ghc/wiki/Building/Troubleshooting#Makehasrestarteditself3timesisthereamakefilebug for details)
  endif
-@@ -459,10 +460,7 @@ PACKAGES_STAGE1 += ghc-boot-th
- PACKAGES_STAGE1 += ghc-boot
- PACKAGES_STAGE1 += template-haskell
+@@ -461,9 +462,7 @@ PACKAGES_STAGE1 += template-haskell
  PACKAGES_STAGE1 += ghc-compact
--
+ PACKAGES_STAGE1 += ghc-heap
+
 -ifeq "$(HADDOCK_DOCS)" "YES"
  PACKAGES_STAGE1 += xhtml
 -endif
- 
+
  ifeq "$(WITH_TERMINFO)" "YES"
  PACKAGES_STAGE1 += terminfo
