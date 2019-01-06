eb76055960c952be94b4f153a6cbcf99dbc88518
Steve Wills <swills@FreeBSD.org>
2019-01-06 01:11:32 +0000

    devel/py-pid: Assign maintainer, fix minor issues
    PR:		234283
    Submitted by:	Mattias Lindvall <mattias.lindvall@gmail.com>

---
+++ devel/py-pid/files/patch-setup.py
@@ -0,0 +1,8 @@
+--- setup.py.orig	2018-12-27 13:15:31 UTC
++++ setup.py
+@@ -54,5 +54,4 @@ setup(
+     packages=["pid"],
+     install_requires=[],
+     test_suite='nose.collector',
+-    setup_requires=['nose>=1.0'],
+ )
