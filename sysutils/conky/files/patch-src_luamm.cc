9ddf0e55f636a90d568ea25f815cb832b08cc966
Guido Falsi <madpilot@FreeBSD.org>
2019-01-07 23:27:11 +0000

    - Update conky to 1.11.1 - Silence some portlint warnings

--- sysutils/conky/files/patch-src_luamm.cc
+++ sysutils/conky/files/patch-src_luamm.cc
@@ -1,39 +1,38 @@
---- src/luamm.cc.orig	2016-10-04 07:49:19 UTC
+--- src/luamm.cc.orig	2018-12-02 18:26:02 UTC
 +++ src/luamm.cc
-@@ -57,7 +57,8 @@ namespace lua {
- 				lua_pushstring(l, e.what());
- 			}
- 			catch(...) {
--				lua_pushstring(l, ptr->__cxa_exception_type()->name());
-+//				lua_pushstring(l, ptr->__cxa_exception_type()->name());
-+				lua_pushstring(l, "Unknown exception");
- 			}
- 			return 1;
- 		}
-@@ -344,7 +345,7 @@ namespace lua {
- 		pushinteger(what);
- 		pushinteger(data);
- 		call(2, 1, 0);
--		assert(isnumber(-1));
-+		assert(isanumber(-1));
- 		int r = tointeger(-1);
- 		pop();
- 		return r;
-@@ -427,7 +428,7 @@ namespace lua {
+@@ -56,7 +56,7 @@ int exception_to_string(lua_State *l) {
+   } catch (std::exception &e) {
+     lua_pushstring(l, e.what());
+   } catch (...) {
+-#if defined(__APPLE__) && defined(__MACH__)
++#if (defined(__APPLE__) && defined(__MACH__)) || defined(__FreeBSD__)
+     // lua_pushstring(l, ptr->__cxa_exception_type()->name());
+     printf(
+         "%s: FIXME: no member named '__cxa_exception_type' in "
+@@ -345,7 +345,7 @@ int state::gc(int what, int data) {
+   pushinteger(what);
+   pushinteger(data);
+   call(2, 1, 0);
+-  assert(isnumber(-1));
++  assert(isanumber(-1));
+   int r = tointeger(-1);
+   pop();
+   return r;
+@@ -420,7 +420,7 @@ bool state::next(int index) {
  
- 		call(2, MULTRET, 0);
+   call(2, MULTRET, 0);
  
--		assert(isnumber(-1));
-+		assert(isanumber(-1));
- 		int r = tointeger(-1);
- 		pop();
- 		return r;
-@@ -466,7 +467,7 @@ namespace lua {
- 		pushvalue(index1);
- 		pushvalue(index2);
- 		call(2, 1, 0);
--		assert(isnumber(-1));
-+		assert(isanumber(-1));
- 		int r = tointeger(-1);
- 		pop();
- 		return r;
+-  assert(isnumber(-1));
++  assert(isanumber(-1));
+   int r = tointeger(-1);
+   pop();
+   return r != 0;
+@@ -460,7 +460,7 @@ bool state::safe_compare(lua_CFunction trampoline, int
+   pushvalue(index1);
+   pushvalue(index2);
+   call(2, 1, 0);
+-  assert(isnumber(-1));
++  assert(isanumber(-1));
+   int r = tointeger(-1);
+   pop();
+   return r != 0;
--- sysutils/conky/files/patch-src_luamm.hh
+++ sysutils/conky/files/patch-src_luamm.hh
@@ -1,11 +1,11 @@
---- src/luamm.hh.orig	2016-10-04 07:49:19 UTC
+--- src/luamm.hh.orig	2018-12-02 18:26:02 UTC
 +++ src/luamm.hh
-@@ -212,7 +212,7 @@ namespace lua {
- 		bool islightuserdata(int index) throw() { return lua_islightuserdata(cobj.get(), index); }
- 		bool isnil(int index) throw() { return lua_isnil(cobj.get(), index); }
- 		bool isnone(int index) throw() { return lua_isnone(cobj.get(), index); }
--		bool isnumber(int index) throw() { return lua_isnumber(cobj.get(), index); }
-+		bool isanumber(int index) throw() { return lua_isnumber(cobj.get(), index); }
- 		bool isstring(int index) throw() { return lua_isstring(cobj.get(), index); }
- 		void pop(int n = 1) throw() { lua_pop(cobj.get(), n); }
- 		void pushboolean(bool b) throw() { lua_pushboolean(cobj.get(), b); }
+@@ -204,7 +204,7 @@ class state : private std::mutex {
+   }
+   bool isnil(int index) throw() { return lua_isnil(cobj.get(), index); }
+   bool isnone(int index) throw() { return lua_isnone(cobj.get(), index); }
+-  bool isnumber(int index) throw() { return lua_isnumber(cobj.get(), index); }
++  bool isanumber(int index) throw() { return lua_isnumber(cobj.get(), index); }
+   bool isstring(int index) throw() { return lua_isstring(cobj.get(), index); }
+   void pop(int n = 1) throw() { lua_pop(cobj.get(), n); }
+   void pushboolean(bool b) throw() { lua_pushboolean(cobj.get(), b); }
