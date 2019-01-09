5ab01967747d61ca1dca9cb4a5d7e5f4a181f124
Carlos J. Puga Medina <cpm@FreeBSD.org>
2019-01-09 16:38:15 +0000

    - Add USES=gl - Enable jumbo build to improve compilation times - Fix loading of the chromium binary on FreeBSD-HEAD - Fix jumbo build with the SNDIO option enabled - Add -Wl,-znotext for i386 - Bump PORTREVISION
    MFH:		2019Q1

--- www/iridium/files/patch-chrome_app_chrome__main__delegate.cc
+++ www/iridium/files/patch-chrome_app_chrome__main__delegate.cc
@@ -1,6 +1,6 @@
---- chrome/app/chrome_main_delegate.cc.orig	2018-02-24 16:25:09.000000000 +0100
-+++ chrome/app/chrome_main_delegate.cc	2018-03-03 20:07:40.484969000 +0100
-@@ -98,7 +98,7 @@
+--- chrome/app/chrome_main_delegate.cc.orig	2018-05-09 21:05:35.000000000 +0200
++++ chrome/app/chrome_main_delegate.cc	2019-01-07 21:51:11.921543000 +0100
+@@ -97,7 +97,7 @@
  #include "chrome/app/shutdown_signal_handlers_posix.h"
  #endif
  
@@ -9,7 +9,7 @@
  #include "components/nacl/common/nacl_paths.h"
  #include "components/nacl/zygote/nacl_fork_delegate_linux.h"
  #endif
-@@ -133,7 +133,7 @@
+@@ -131,7 +131,7 @@
  #include "v8/include/v8.h"
  #endif
  
@@ -18,7 +18,7 @@
  #include "base/environment.h"
  #endif
  
-@@ -238,7 +238,7 @@
+@@ -236,7 +236,7 @@
  
  #endif  // defined(OS_WIN)
  
@@ -27,7 +27,7 @@
  void AdjustLinuxOOMScore(const std::string& process_type) {
    // Browsers and zygotes should still be killable, but killed last.
    const int kZygoteScore = 0;
-@@ -407,7 +407,7 @@
+@@ -404,7 +404,7 @@
    std::string process_type =
        command_line->GetSwitchValueASCII(switches::kProcessType);
  
@@ -36,7 +36,7 @@
    // On Linux, Chrome does not support running multiple copies under different
    // DISPLAYs, so the profile directory can be specified in the environment to
    // support the virtual desktop use-case.
-@@ -581,7 +581,7 @@
+@@ -576,7 +576,7 @@
  #if defined(OS_CHROMEOS)
    chromeos::RegisterPathProvider();
  #endif
@@ -45,25 +45,7 @@
    nacl::RegisterPathProvider();
  #endif
  
-@@ -603,7 +603,7 @@
-       std::string format_str =
-           command_line.GetSwitchValueASCII(switches::kDiagnosticsFormat);
-       if (format_str == "machine") {
--        format = diagnostics::DiagnosticsWriter::MACHINE;
-+        format = diagnostics::DiagnosticsWriter::THEMACHINE;
-       } else if (format_str == "log") {
-         format = diagnostics::DiagnosticsWriter::LOG;
-       } else {
-@@ -653,7 +653,7 @@
-       std::string format_str =
-           command_line.GetSwitchValueASCII(switches::kDiagnosticsFormat);
-       if (format_str == "machine") {
--        format = diagnostics::DiagnosticsWriter::MACHINE;
-+        format = diagnostics::DiagnosticsWriter::THEMACHINE;
-       } else if (format_str == "human") {
-         format = diagnostics::DiagnosticsWriter::HUMAN;
-       } else {
-@@ -915,7 +915,7 @@
+@@ -893,7 +893,7 @@
    InitializePDF();
  #endif
  
@@ -72,7 +54,7 @@
    // Zygote needs to call InitCrashReporter() in RunZygote().
    if (process_type != switches::kZygoteProcess) {
  #if defined(OS_ANDROID)
-@@ -931,7 +931,7 @@
+@@ -909,7 +909,7 @@
      breakpad::InitCrashReporter(process_type);
  #endif  // defined(OS_ANDROID)
    }
@@ -81,7 +63,7 @@
  
    // After all the platform Breakpads have been initialized, store the command
    // line for crash reporting.
-@@ -941,7 +941,7 @@
+@@ -919,7 +919,7 @@
  void ChromeMainDelegate::SandboxInitialized(const std::string& process_type) {
    // Note: If you are adding a new process type below, be sure to adjust the
    // AdjustLinuxOOMScore function too.
