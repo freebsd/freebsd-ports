a013f9b55aace5e50fda5a0fd462a1bef7a30233
fernape <fernape@FreeBSD.org>
2019-01-12 23:04:06 +0000

    audio/oss: Fix breakage after r489819
    The patch in r489819 broke the build in 11.
    
    Address that and make sure this one builds in {11.2,12}{amd64,i386} and
    13-CURRENT i386
    
    PR:	234909
    Reported by:	leres@

--- audio/oss/files/patch-kernel_OS_FreeBSD_wrapper_bsddefs.h
+++ audio/oss/files/patch-kernel_OS_FreeBSD_wrapper_bsddefs.h
@@ -1,13 +1,12 @@
---- kernel/OS/FreeBSD/wrapper/bsddefs.h.orig	2019-01-06 18:38:02 UTC
+--- kernel/OS/FreeBSD/wrapper/bsddefs.h.orig	2019-01-12 19:57:42 UTC
 +++ kernel/OS/FreeBSD/wrapper/bsddefs.h
-@@ -91,7 +91,10 @@ typedef int ddi_iblock_cookie_t;
+@@ -91,6 +91,9 @@ typedef int ddi_iblock_cookie_t;
  extern void oss_udelay (unsigned long t);
  
  #ifdef _KERNEL
 +#ifdef memset
 +#undef memset
- #define memset oss_memset
 +#endif
+ #define memset oss_memset
  extern void *oss_memset (void *t, int val, int l);
  #endif
- 
