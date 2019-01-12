4975cbead91be0700f0aa00a66cbe957a4d143bc
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2019-01-12 11:14:13 +0000

    net-mgmt/librenms: Update version 1.45=>1.47
    PR:		234645
    Submitted by:	dvl
    Relnotes:	https://github.com/librenms/librenms/releases

--- net-mgmt/librenms/files/patch-LibreNMS_Util_FileLock.php
+++ net-mgmt/librenms/files/patch-LibreNMS_Util_FileLock.php
@@ -1,10 +1,9 @@
---- LibreNMS/Util/FileLock.php.orig	2018-01-14 15:32:35 UTC
+--- LibreNMS/Util/FileLock.php.orig	2018-12-30 14:29:16 UTC
 +++ LibreNMS/Util/FileLock.php
-@@ -45,7 +45,7 @@ class FileLock implements Lock
-         global $config;
+@@ -47,6 +47,7 @@ class FileLock implements Lock
  
          $this->name = $lock_name;
--        $this->file = "$config[install_dir]/.$lock_name.lock";
+         $this->file = "$install_dir/.$lock_name.lock";
 +        $this->file = "/tmp/.$lock_name.lock";
          $this->handle = fopen($this->file, "w+");
      }
