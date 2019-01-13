7ba47cf58207757ae7af8b03418af162a69c3755
Ruslan Makhmatkhanov <rm@FreeBSD.org>
2019-01-13 18:22:54 +0000

    security/hydra: update to 8.8
    - switch to github
    - update to 8.8
    - pet portlint

--- security/hydra/files/patch-hydra.h
+++ security/hydra/files/patch-hydra.h
@@ -1,13 +1,13 @@
---- hydra.h.orig	2016-06-16 14:57:15 UTC
+--- hydra.h.orig	2019-01-02 12:38:23 UTC
 +++ hydra.h
-@@ -31,10 +31,6 @@
- #endif
+@@ -38,10 +38,6 @@
+   #endif
  #endif
  
 -#ifdef LIBSSH
--#include <libssh/libssh.h>
+-  #include <libssh/libssh.h>
 -#endif
 -
  #ifdef HAVE_ZLIB
- #include <zlib.h>
+   #include <zlib.h>
  #endif
