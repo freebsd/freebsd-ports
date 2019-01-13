017db20ddf2c32ea444a09a2eeb61ee7853ec529
Mahdi Mokhtari <mmokhi@FreeBSD.org>
2019-01-13 23:24:27 +0000

    databases/mysql80-{client, server}: Update ports to latest version 8.0.13 This update fixes several issues including CVEs. Bug-fixes: 	- Return better error messages for OpenSSL errors 	- Incorrect copying of an integer value by X Plugin caused an 	    error relating to misaligned memory access Improvement: 	- Important Change: X Protocol now provides a connection pooling option
    Upstream notes: https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-13
    
    While here, fix the build with GCC-based architectures too (ported from fix on MySQL57)
    
    Sponsored by:	The FreeBSD Foundation

--- databases/mysql80-server/files/patch-sql_mysqld.cc
+++ databases/mysql80-server/files/patch-sql_mysqld.cc
@@ -1,15 +1,15 @@
---- sql/mysqld.cc.orig	2018-04-08 06:44:49 UTC
+--- sql/mysqld.cc.orig	2018-10-07 08:44:22 UTC
 +++ sql/mysqld.cc
-@@ -4309,7 +4309,7 @@ static int warn_self_signed_ca() {
- static void init_ssl() {
- #ifdef HAVE_OPENSSL
- #ifndef HAVE_WOLFSSL
+@@ -4493,7 +4493,7 @@ static int warn_self_signed_ca() {
+ 
+ static PSI_memory_key key_memory_openssl = PSI_NOT_INSTRUMENTED;
+ 
 -#if OPENSSL_VERSION_NUMBER < 0x10100000L
 +#if defined(LIBRESSL_VERSION_NUMBER) || OPENSSL_VERSION_NUMBER < 0x10100000L
-   CRYPTO_malloc_init();
- #else  /* OPENSSL_VERSION_NUMBER < 0x10100000L */
-   OPENSSL_malloc_init();
-@@ -4321,7 +4321,7 @@ static void init_ssl() {
+ #define FILE_LINE_ARGS
+ #else
+ #define FILE_LINE_ARGS , const char *, int
+@@ -4530,7 +4530,7 @@ static void init_ssl() {
  
  static int init_ssl_communication() {
  #ifdef HAVE_OPENSSL
@@ -18,7 +18,7 @@
    char ssl_err_string[OPENSSL_ERROR_LENGTH] = {'\0'};
    int ret_fips_mode = set_fips_mode(opt_ssl_fips_mode, ssl_err_string);
    if (ret_fips_mode != 1) {
-@@ -8647,7 +8647,7 @@ bool mysqld_get_one_option(int optid,
+@@ -8919,7 +8919,7 @@ bool mysqld_get_one_option(int optid,
          One can disable SSL later by using --skip-ssl or --ssl=0.
        */
        opt_use_ssl = true;
