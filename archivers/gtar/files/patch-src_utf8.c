e53051cd0293e4b59623c611521359c71b2f3f07
naddy <naddy@FreeBSD.org>
2019-01-13 15:33:22 +0000

    Update to 1.31.  Noteworthy changes: * Support for zstd compression (if zstd is installed). * The -K option interacts properly with member names given on the command line. * Fix handling of file shrinkage during read access when --sparse is used.

--- archivers/gtar/files/patch-src_utf8.c
+++ archivers/gtar/files/patch-src_utf8.c
@@ -1,14 +1,14 @@
 Work around an iconv(3) portability issue that trips up sparse07.at.
-https://lists.gnu.org/archive/html/bug-tar/2018-04/threads.html
+https://lists.gnu.org/archive/html/bug-tar/2018-04/msg00019.html
 
---- src/utf8.c.orig	2018-04-30 17:25:34 UTC
+--- src/utf8.c.orig	2019-01-11 17:46:41 UTC
 +++ src/utf8.c
-@@ -85,7 +85,7 @@ utf8_convert (bool to_utf, char const *i
+@@ -81,7 +81,7 @@ utf8_convert (bool to_utf, char const *input, char **o
+   outlen = inlen * MB_LEN_MAX + 1;
+   ob = ret = xmalloc (outlen);
    ib = (char ICONV_CONST *) input;
-   rc = iconv (cd, &ib, &inlen, &ob, &outlen);
-   *ob = 0;
--  return rc != -1;
-+  return rc == 0;
- }
- 
- 
+-  if (iconv (cd, &ib, &inlen, &ob, &outlen) == -1)
++  if (iconv (cd, &ib, &inlen, &ob, &outlen) != 0)
+     {
+       free (ret);
+       return false;
