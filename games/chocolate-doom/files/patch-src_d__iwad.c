fde04a3f58c6444a6d5f02caace64cf558f9e7d1
swills <swills@FreeBSD.org>
2019-01-15 14:22:14 +0000

    games/chocolate-doom: update to 3.0.0
    While here:
    
    * add rc script for server [1]
    * add pkg-message on music configuration [2]
    
    PR:		225453
    Submitted by:	jakub_lach@mailplus.pl (maintainer)
    Submitted by:	Shirkdog <mshirk@daemon-security.com> (update and [1])
    Submitted by:	Patrick McMunn <doctorwhoguy@gmail.com> [2]

--- games/chocolate-doom/files/patch-src_d__iwad.c
+++ games/chocolate-doom/files/patch-src_d__iwad.c
@@ -1,11 +1,11 @@
---- src/d_iwad.c.orig	2015-09-26 11:05:54 UTC
-+++ src/d_iwad.c
-@@ -632,7 +632,7 @@ static void AddXdgDirs(void)
+--- src/d_iwad.c.orig	2017-12-30 16:20:28.000000000 -0500
++++ src/d_iwad.c	2018-01-25 09:43:00.567417000 -0500
+@@ -658,7 +658,7 @@
      // The "standard" location for IWADs on Unix that is supported by most
      // source ports is /usr/share/games/doom - we support this through the
      // XDG_DATA_DIRS mechanism, through which it can be overridden.
 -    AddIWADPath(env, "/games/doom");
 +    AddIWADPath(env, "/doom");
- }
  
- //
+     // The convention set by RBDOOM-3-BFG is to install Doom 3: BFG
+     // Edition into this directory, under which includes the Doom
