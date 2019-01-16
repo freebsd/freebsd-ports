65add84902dcc2b6ce3cc7f3b9dfbad14e799f4a
Mark Linimon <linimon@FreeBSD.org>
2019-01-16 05:34:20 +0000

    kadu-core/plugin/dependency-graph/plugin-dependency-graph-builder.h needs to include <functional> to fix build on GCC-based architectures.
    PR:		234948
    Submitted by:	Piotr Kubaj

---
+++ polish/kadu/files/patch-kadu-core_plugin_dependency-graph_plugin-dependency-graph-builder.h
@@ -0,0 +1,10 @@
+--- kadu-core/plugin/dependency-graph/plugin-dependency-graph-builder.h.orig	2019-01-14 15:34:25 UTC
++++ kadu-core/plugin/dependency-graph/plugin-dependency-graph-builder.h
+@@ -20,6 +20,7 @@
+ #pragma once
+ 
+ #include <QtCore/QObject>
++#include <functional>
+ #include <map>
+ #include <set>
+ 
