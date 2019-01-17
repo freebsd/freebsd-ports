8d2ec5c9c07c79fd9da8065f8f7b8254cda0263f
jbeich <jbeich@FreeBSD.org>
2019-01-17 19:50:38 +0000

    lang/rust: update to 1.32.0
    - devel/racer no longer needs to dowgrade rustc-ap-syntax
    - Force rebuild all consumers to catch regressions early
    
    Changes:	https://blog.rust-lang.org/2019/01/17/Rust-1.32.0.html
    Approved by:	rust (tobik)
    Differential Revision:	https://reviews.freebsd.org/D18858

--- lang/rust/files/patch-src_vendor_libc_src_unix_bsd_freebsdlike_dragonfly_mod.rs
+++ lang/rust/files/patch-src_vendor_libc_src_unix_bsd_freebsdlike_dragonfly_mod.rs
@@ -1,5 +1,5 @@
---- src/vendor/libc/src/unix/bsd/freebsdlike/dragonfly/mod.rs.orig	2018-06-19 05:52:22 UTC
-+++ src/vendor/libc/src/unix/bsd/freebsdlike/dragonfly/mod.rs
+--- vendor/libc/src/unix/bsd/freebsdlike/dragonfly/mod.rs.orig	2018-06-19 05:52:22 UTC
++++ vendor/libc/src/unix/bsd/freebsdlike/dragonfly/mod.rs
 @@ -1,3 +1,4 @@
 +pub type c_char = i8;
  pub type clock_t = u64;
