f00aa0d930420f6a73b732151bdb0be37b57c707
danfe <danfe@FreeBSD.org>
2019-01-17 13:53:43 +0000

    Unbreak the build against modern compilers caused by some sloppy C++ coding.

--- x11/metisse/files/patch-FvwmCompositor_main_AScreen.cxx
+++ x11/metisse/files/patch-FvwmCompositor_main_AScreen.cxx
@@ -1,4 +1,4 @@
---- FvwmCompositor/main/AScreen.cxx.orig	2008-11-25 10:22:45.000000000 +0000
+--- FvwmCompositor/main/AScreen.cxx.orig	2008-11-25 10:22:45 UTC
 +++ FvwmCompositor/main/AScreen.cxx
 @@ -10,6 +10,7 @@
   *
@@ -8,3 +8,12 @@
  #ifdef HAVE_CONFIG_H
  #include "config-not-xserver.h"
  #endif
+@@ -5159,7 +5160,7 @@ AScreen::AScreen(MetisseDesktop *metisseDesktop, bool 
+ 	_clipOpWaitForCtrlReleased = false;
+ 	_clipAutoRepeatOff = false;
+ 	_miIn = false;
+-	_motionInterceptTk = false;
++	_motionInterceptTk = 0;
+ 	_miTkcount = 0;
+ 	_miFeed = 0;
+ 	_setupRightLongPressTimeOut();
