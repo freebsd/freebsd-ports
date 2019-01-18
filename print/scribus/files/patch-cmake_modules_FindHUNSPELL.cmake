91ea5c3247ec607f48809a35593bf5580ed7d455
Jan Beich <jbeich@FreeBSD.org>
2019-01-18 21:14:41 +0000

    textproc/hunspell: update to 1.7.0
    - Drop const optimization as v2 API moved to nuspell
    
    Changes:	https://github.com/hunspell/hunspell/releases/tag/v1.7.0

--- print/scribus/files/patch-cmake_modules_FindHUNSPELL.cmake
+++ print/scribus/files/patch-cmake_modules_FindHUNSPELL.cmake
@@ -5,7 +5,7 @@
  
  FIND_PATH(HUNSPELL_INCLUDE_DIR hunspell/hunspell.hxx )
 -FIND_LIBRARY(HUNSPELL_LIBRARIES NAMES hunspell-1.4 hunspell-1.3 hunspell-1.2 PATHS /opt/local/lib /usr/local/lib /usr/lib )
-+FIND_LIBRARY(HUNSPELL_LIBRARIES NAMES hunspell-1.6
++FIND_LIBRARY(HUNSPELL_LIBRARIES NAMES hunspell-1.7 hunspell-1.6
 +             hunspell-1.5 hunspell-1.4 hunspell-1.3 hunspell-1.2 hunspell
 +  PATHS /opt/local/lib /usr/local/lib /usr/lib )
  
