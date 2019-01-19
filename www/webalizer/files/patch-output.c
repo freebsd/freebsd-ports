646449a4bdccd7bccebcc75de8d253027824e5d3
Dirk Meyer <dinoex@FreeBSD.org>
2019-01-19 07:08:22 +0000

    - fix option GEOIP to use GeoLite2-Country

--- www/webalizer/files/patch-output.c
+++ www/webalizer/files/patch-output.c
@@ -1,5 +1,14 @@
 --- output.c.orig	2013-02-26 05:37:27 UTC
 +++ output.c
+@@ -54,7 +54,7 @@
+ #endif
+ 
+ #ifdef USE_GEOIP
+-#include <GeoIP.h>
++#include <maxminddb.h>
+ #endif
+ 
+ #include "webalizer.h"                        /* main header              */
 @@ -992,6 +992,20 @@ int all_sites_page(u_int64_t h_reg, u_in
        hptr=*pointer++;
        if (hptr->flag == OBJ_REG)
@@ -21,3 +30,46 @@
           fprintf(out_fp,
              "%-8llu %6.02f%%  %8llu %6.02f%%  %8.0f %6.02f%%  "            \
              "%8llu %6.02f%%  %s\n",
+@@ -1948,7 +1962,7 @@ void top_ctry_table()
+ 
+ #ifdef USE_GEOIP
+    extern int    geoip;
+-   extern GeoIP  *geo_fp;
++   extern MMDB_s mmdb;
+    const  char   *geo_rc=NULL;
+ #endif
+    char          geo_ctry[3]="--";
+@@ -1981,19 +1995,26 @@ void top_ctry_table()
+                if (geoip)
+                {
+                   /* Lookup IP address here,  turn into idx  */
+-                  geo_rc=GeoIP_country_code_by_addr(geo_fp, hptr->string);
+-                  if (geo_rc==NULL||geo_rc[0]=='\0'||geo_rc[0]=='-')
++                  MMDB_lookup_result_s result;
++                  MMDB_entry_data_s entry_data;
++                  int gai_error, mmdb_error, rc;
++                  result=MMDB_lookup_string(&mmdb, hptr->string, &gai_error, &mmdb_error);
++                  if (gai_error!=0||mmdb_error!=MMDB_SUCCESS||!result.found_entry)
+                   {
+                      if (debug_mode)
+                         fprintf(stderr,"GeoIP: %s unknown (returns '%s')\n",
+-                                hptr->string,(geo_rc==NULL)?"null":geo_rc);
++                                hptr->string,MMDB_strerror(mmdb_error));
+                   }
+                   else
+                   {
+-                     /* index returned geo_ctry */
+-                     geo_ctry[0]=tolower(geo_rc[0]);
+-                     geo_ctry[1]=tolower(geo_rc[1]);
+-                     idx=ctry_idx(geo_ctry);
++                     rc = MMDB_get_value(&result.entry, &entry_data, "country", "iso_code", NULL);
++                     if (rc==MMDB_SUCCESS&&entry_data.has_data)
++                     {
++                       /* index returned geo_ctry */
++                       geo_ctry[0]=tolower(entry_data.utf8_string[0]);
++                       geo_ctry[1]=tolower(entry_data.utf8_string[1]);
++                       idx=ctry_idx(geo_ctry);
++                     }
+                   }
+                }
+ #endif /* USE_GEOIP */
--- www/webalizer/files/patch-webalizer.c
+++ www/webalizer/files/patch-webalizer.c
@@ -1,6 +1,49 @@
 --- webalizer.c.orig	2013-08-26 04:52:15 UTC
 +++ webalizer.c
-@@ -942,7 +942,11 @@ int main(int argc, char *argv[])
+@@ -72,7 +72,7 @@
+ #endif  /* USE_DNS */
+ 
+ #ifdef USE_GEOIP
+-#include <GeoIP.h>
++#include <maxminddb.h>
+ #endif
+ 
+ #ifdef USE_BZIP
+@@ -172,7 +172,8 @@ char    *flag_dir    = "flags";         
+ #ifdef USE_GEOIP
+ int     geoip        = 0;                     /* Use GeoIP (0=no)         */
+ char    *geoip_db    = NULL;                  /* GeoIP database filename  */
+-GeoIP   *geo_fp      = NULL;                  /* GeoIP database handle    */
++int	mmdb_open    = MMDB_FILE_OPEN_ERROR;  /* GeoIP database open      */
++MMDB_s  mmdb;                                 /* GeoIP database handle    */
+ #endif
+ 
+ int     ntop_sites   = 30;                    /* top n sites to display   */
+@@ -619,20 +620,17 @@ int main(int argc, char *argv[])
+    if (geoip)
+    {
+       if (geoip_db!=NULL)
+-         geo_fp=GeoIP_open(geoip_db, GEOIP_MEMORY_CACHE);
++         mmdb_open = MMDB_open(geoip_db, MMDB_MODE_MMAP, &mmdb);
+       else
+-         geo_fp=GeoIP_new(GEOIP_MEMORY_CACHE);
++         mmdb_open = MMDB_open("GeoLite2-Country.mmdb", MMDB_MODE_MMAP, &mmdb);
+ 
+       /* Did we open one? */
+-      if (geo_fp==NULL)
++      if (mmdb_open!=MMDB_SUCCESS)
+       {
+          /* couldn't open.. warn user */
+          if (verbose) printf("GeoIP %s\n",msg_geo_nolu);
+          geoip=0;
+       }
+-      else if (verbose>1) printf("%s %s (%s)\n",msg_geo_use,
+-         GeoIPDBDescription[(int)geo_fp->databaseType],
+-         (geoip_db==NULL)?msg_geo_dflt:geo_fp->file_path);
+    }
+ #endif /* USE_GEOIP */
+ 
+@@ -942,7 +940,11 @@ int main(int argc, char *argv[])
                 {
                    /* Save query portion in log.rec.srchstr */
                    strncpy(log_rec.srchstr,(char *)cp1,MAXSRCH);
@@ -12,7 +55,7 @@
                    break;
                 }
                 else cp1++;
-@@ -1089,6 +1093,13 @@ int main(int argc, char *argv[])
+@@ -1089,6 +1091,13 @@ int main(int argc, char *argv[])
              {
                 freeaddrinfo(ares);
                 resolve_dns(&log_rec);
@@ -26,3 +69,15 @@
              }
           }
  #endif
+@@ -1487,7 +1496,10 @@ int main(int argc, char *argv[])
+ 
+ #ifdef USE_GEOIP
+       /* Close GeoIP database */
+-      if (geo_fp) GeoIP_delete(geo_fp);
++      if (mmdb_open==MMDB_SUCCESS) {
++         MMDB_close(&mmdb);
++         mmdb_open=MMDB_FILE_OPEN_ERROR;
++      }
+ #endif
+ 
+       /* Whew, all done! Exit with completion status (0) */
