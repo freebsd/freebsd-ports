5242423c2d3af227c02e4985318a7b3713e9dbe7
jbeich <jbeich@FreeBSD.org>
2019-01-21 10:42:44 +0000

    net-im/psi: unbreak HUNSPELL=on after r490671
    CMake Error at /usr/local/share/cmake/Modules/FindPackageHandleStandardArgs.cmake:137 (message):
      Could NOT find Hunspell (missing: HUNSPELL_LIBRARY)
    
    PR:		235104
    Submitted by:	VVD
    Pointy hat to:	jbeich

---
+++ net-im/psi/files/patch-cmake_modules_FindHunspell.cmake
@@ -0,0 +1,10 @@
+--- cmake/modules/FindHunspell.cmake.orig	2017-08-19 09:50:06 UTC
++++ cmake/modules/FindHunspell.cmake
+@@ -64,6 +64,7 @@ set(HUNSPELL_NAMES
+ 	hunspell-1.4
+ 	hunspell-1.5
+ 	hunspell-1.6
++	hunspell-1.7
+ 	libhunspell${d}
+ )
+ find_library(
