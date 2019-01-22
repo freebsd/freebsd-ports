6ef36886061a51acf31bf272bf7514390df57d61
koobs <koobs@FreeBSD.org>
2019-01-22 10:46:11 +0000

    www/py-requests: Update to 2.21.0
     - Update USES comment (Python 3.3 support dropped)
     - Rebase setup.py patch (idna change released)
     - Remove comment about failing tests due to httpbin issue which seems
       to now be fixed.
    
    This update includes a pinned urllib3 version bump to < 1.25, which paves
    the way for a net/urllib3 update to 1.24 [1].
    
    Note: 2.20.0 includes a security vulnerability fix for CVE-2018-18074
    
    Changelog:
    
      https://github.com/requests/requests/blob/v2.21.0/HISTORY.md
    
    PR: 		229322 [1]
    Security:	50ad9a9a-1e28-11e9-98d7-0050562a4d7b
    MFH:		2019Q1

--- www/py-requests/files/patch-setup.py
+++ www/py-requests/files/patch-setup.py
@@ -1,21 +1,12 @@
-# 1: Backport bumping idna max version
-# https://github.com/requests/requests/commit/991e8b76b7a9d21f698b24fa0058d3d5968721bc
-# 2: pytest-cov and its dependencies are not compulsory
+# pytest-cov and its dependencies are not compulsory
 
---- setup.py.orig	2017-08-15 13:19:39 UTC
+--- setup.py.orig	2019-01-22 02:52:54 UTC
 +++ setup.py
-@@ -43,12 +43,12 @@ packages = ['requests']
- 
- requires = [
-     'chardet>=3.0.2,<3.1.0',
--    'idna>=2.5,<2.7',
-+    'idna>=2.5,<2.8',
-     'urllib3>=1.21.1,<1.23',
-     'certifi>=2017.4.17'
- 
+@@ -50,7 +50,6 @@ requires = [
  ]
--test_requirements = ['pytest-httpbin==0.0.7', 'pytest-cov', 'pytest-mock', 'pytest-xdist', 'PySocks>=1.5.6, !=1.5.7', 'pytest>=2.8.0']
-+test_requirements = ['pytest-httpbin==0.0.7', 'pytest-mock', 'pytest-xdist', 'PySocks>=1.5.6, !=1.5.7', 'pytest>=2.8.0']
- 
- about = {}
- with open(os.path.join(here, 'requests', '__version__.py'), 'r', 'utf-8') as f:
+ test_requirements = [
+     'pytest-httpbin==0.0.7',
+-    'pytest-cov',
+     'pytest-mock',
+     'pytest-xdist',
+     'PySocks>=1.5.6, !=1.5.7',
