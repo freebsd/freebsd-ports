1d3f87546490b09c8c769b07c9e9a8c3879cd02e
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-01-23 00:01:07 +0000

    Update to 1.1
    Changes:	https://github.com/klausman/pymetar/commits/master

--- astro/py-pymetar/files/patch-setup.py
+++ astro/py-pymetar/files/patch-setup.py
@@ -1,11 +1,13 @@
---- setup.py.orig	2015-04-20 08:17:26 UTC
+--- setup.py.orig	2018-12-25 17:48:03 UTC
 +++ setup.py
-@@ -26,7 +26,5 @@ setup(name="pymetar", version=pymetar.__
-       packages=[""],
-       py_modules=["pymetar"],
-       scripts=["bin/pymetar"],
--      data_files=[("share/doc/pymetar-%s" % pymetar.__version__,
--                   ['README', 'COPYING', 'THANKS', 'librarydoc.txt']),
--                  ("share/man/man1", ['pymetar.1'])]
-+      data_files=[("man/man1", ['pymetar.1'])]
-       )
+@@ -31,9 +31,7 @@ setuptools.setup(
+     packages=setuptools.find_packages(),
+     py_modules=["pymetar"],
+     scripts=["bin/pymetar"],
+-    data_files=[("share/doc/pymetar-%s" % pymetar.__version__,
+-                 ['README.md', 'COPYING', 'THANKS']),
+-                ("share/man/man1", ['pymetar.1'])],
++    data_files=[("man/man1", ['pymetar.1'])],
+     classifiers=(
+     "Programming Language :: Python :: 3",
+     "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)", 
