996aa8dcf43e956fd9c070d83b13b94ddc34212d
dbaio <dbaio@FreeBSD.org>
2019-01-26 12:52:06 +0000

    Add USES azurepy: Shared namespace for py-azure-* ports
    USES=azurepy provides common files that are shared among all
    py-azure-* ports:
      azure/__init__.py
      azure/cli/__init__.py
      azure/cli/command_modules/__init__.py
      azure/mgmt/__init__.py
      azure/mgmt/datalake/__init__.py
    
    Reviewed by:	antoine, mat
    Approved by:	portmgr (mat)
    Differential Revision:	https://reviews.freebsd.org/D18573

---
+++ Mk/Uses/azurepy.mk
@@ -0,0 +1,41 @@
+# $FreeBSD$
+#
+# Shared namespace for py-azure-* ports
+# Feature:	azurepy
+# Usage:	USES=	azurepy
+# MAINTAINER:	dbaio@FreeBSD.org
+
+.if !defined(_INCLUDE_USES_AZUREPY_MK)
+_INCLUDE_USES_AZUREPY_MK=	yes
+
+RUN_DEPENDS+=	${PYTHON_PKGNAMEPREFIX}azure-cli-base>=0:sysutils/py-azure-cli-base@${PY_FLAVOR}
+
+POST_PLIST=	trim-backports-namespace
+
+_USES_install+=	701:remove-common-files
+remove-common-files:
+	@${RM} ${STAGEDIR}${PYTHON_SITELIBDIR}/azure/__init__.py* \
+		${STAGEDIR}${PYTHON_SITELIBDIR}/azure/cli/__init__.py* \
+		${STAGEDIR}${PYTHON_SITELIBDIR}/azure/cli/command_modules/__init__.py* \
+		${STAGEDIR}${PYTHON_SITELIBDIR}/azure/mgmt/__init__.py* \
+		${STAGEDIR}${PYTHON_SITELIBDIR}/azure/mgmt/datalake/__init__.py*
+	@${RM} -r ${STAGEDIR}${PYTHON_SITELIBDIR}/azure/__pycache__ \
+		${STAGEDIR}${PYTHON_SITELIBDIR}/azure/cli/__pycache__ \
+		${STAGEDIR}${PYTHON_SITELIBDIR}/azure/cli/command_modules/__pycache__ \
+		${STAGEDIR}${PYTHON_SITELIBDIR}/azure/mgmt/__pycache__ \
+		${STAGEDIR}${PYTHON_SITELIBDIR}/azure/mgmt/datalake/__pycache__
+
+trim-backports-namespace:
+	@${REINPLACE_CMD} -e '/azure\/__init__.py*/d' \
+		-e '/azure\/cli\/__init__.py*/d' \
+		-e '/azure\/cli\/command_modules\/__init__.py*/d' \
+		-e '/azure\/mgmt\/__init__.py*/d' \
+		-e '/azure\/mgmt\/datalake\/__init__.py*/d' \
+		-e '/azure\/__pycache__*/d' \
+		-e '/azure\/cli\/__pycache__*/d' \
+		-e '/azure\/cli\/command_modules\/__pycache__*/d' \
+		-e '/azure\/mgmt\/__pycache__*/d' \
+		-e '/azure\/mgmt\/datalake\/__pycache__*/d' \
+		${TMPPLIST}
+
+.endif
