eab6087aaa7da0e10450dc7a27a52b6dc6c8e36c
swills <swills@FreeBSD.org>
2019-01-27 23:57:42 +0000

    editors/scite: update to 4.1.0
    PR:		228909
    Submitted by:	Naram Qashat <cyberbotx@cyberbotx.com> (maintainer)

---
+++ editors/scite/files/patch-src_EditorConfig.cxx
@@ -0,0 +1,10 @@
+--- src/EditorConfig.cxx.orig	2018-05-06 22:32:10 UTC
++++ src/EditorConfig.cxx
+@@ -13,6 +13,7 @@
+ #include <map>
+ #include <algorithm>
+ #include <memory>
++#include <sys/time.h>
+ 
+ #include "Scintilla.h"
+ 
