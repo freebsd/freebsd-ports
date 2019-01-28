f02184df4eb7f18f1e172fa25aadfaf67cc2fc31
tobik <tobik@FreeBSD.org>
2019-01-28 19:24:50 +0000

    mail/pop3proxy: Fix build with OpenSSL 1.1.1
    - Add license
    - Fix config file location
    - Reorder some things to pet portlint
    - Mark it deprecated too as it appears to have no upstream anymore
      and only works properly with unencrypted traffic
    
    PR:		232134
    Submitted by:	freebsd_ports@k-worx.org
    MFH:		2019Q1

--- mail/pop3proxy/files/patch-src_imapcommon.c
+++ mail/pop3proxy/files/patch-src_imapcommon.c
@@ -1,10 +1,31 @@
---- src/imapcommon.c.orig
+--- src/imapcommon.c.orig	2005-02-01 15:21:28 UTC
 +++ src/imapcommon.c
-@@ -121,6 +121,7 @@
- #include <errno.h>
+@@ -295,16 +295,24 @@ extern ICD_Struct *Get_Server_conn( char *Username, 
+     int rc;
+     unsigned int Expiration;
  
- #include <openssl/evp.h>
-+#include <openssl/md5.h>
+-    EVP_MD_CTX mdctx;
++    EVP_MD_CTX *mdctx;
+     int md_len;
  
- #include <pthread.h>
- #include <sys/types.h>
++#if OPENSSL_VERSION_NUMBER < 0x10100000L
++    EVP_MD_CTX mdctx_;
++#define EVP_MD_CTX_new(x) &mdctx_
++#define EVP_MD_CTX_free(x)
++#endif
++
+     Expiration = PC_Struct.cache_expiration_time;
+     memset( &Server, 0, sizeof Server );
+     
+     /* need to md5 the passwd regardless, so do that now */
+-    EVP_DigestInit(&mdctx, EVP_md5());
+-    EVP_DigestUpdate(&mdctx, Password, strlen(Password));
+-    EVP_DigestFinal(&mdctx, md5pw, &md_len);
++    mdctx = EVP_MD_CTX_new();
++    EVP_DigestInit(mdctx, EVP_md5());
++    EVP_DigestUpdate(mdctx, Password, strlen(Password));
++    EVP_DigestFinal(mdctx, md5pw, &md_len);
++    EVP_MD_CTX_free(mdctx);
+     
+     /* see if we have a reusable connection available */
+     ICC_Active = NULL;
