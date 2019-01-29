39c22e3ff066e2a15ad1b91bc51a0744fbf5c5ac
tobik <tobik@FreeBSD.org>
2019-01-29 12:31:51 +0000

    graphics/sane-backends: Actually update to 1.0.27
    The current version is 1.0.25 with a wrongly named distfile which
    fails to fetch from the fossies.org mirror with a size mismatch.
    The update to 1.0.27 in r444767 also shows that the checksum and
    size have not changed in distinfo in that commit.  Full changes
    between the distfiles from 1.0.27_4 & 1.0.27_5 can be found at [1].
    
    - Remove dead alioth.debian.org mirror
    - Remove some no longer needed patches
    
    [1] https://people.freebsd.org/~tobik/logs/sane-backends-1.0.27.diff
    
    ABI:		https://abi-laboratory.pro/tracker/timeline/sane-backends/
    PR:		229653
    Submitted by:	Peter Wullinger <freebsd@dhke.de>

--- graphics/sane-backends/files/patch-backend__v4l.c
+++ graphics/sane-backends/files/patch-backend__v4l.c
@@ -1,4 +1,4 @@
---- backend/v4l.c.orig	2015-09-20 11:55:39 UTC
+--- backend/v4l.c.orig	2016-10-06 00:02:57 UTC
 +++ backend/v4l.c
 @@ -72,7 +72,10 @@
  #include "../include/sane/saneopts.h"
@@ -11,3 +11,12 @@
  
  #define BACKEND_NAME v4l
  #include "../include/sane/sanei_backend.h"
+@@ -1046,7 +1049,7 @@ sane_start (SANE_Handle handle)
+   /* v4l1 actually returns BGR when we ask for RGB, so convert it */
+   if (s->pict.palette == VIDEO_PALETTE_RGB24)
+     {
+-      __u32 loop;
++      uint32_t loop;
+       DBG (3, "sane_start: converting from BGR to RGB\n");
+       for (loop = 0; loop < (s->window.width * s->window.height * 3); loop += 3)
+         {
