fdc1ad42544deb1ef78ca58df88f8132e79d6ca3
David Thiel <lx@FreeBSD.org>
2019-01-29 23:15:55 +0000

    Update to 0.11.2.
    PR:	233692
    Submitted by:	Sascha Biberhofer

---
+++ net-im/prosody/files/patch-util-src_time.c
@@ -0,0 +1,9 @@
+--- util-src/time.c.orig	2018-12-01 10:50:51.975557000 +0000
++++ util-src/time.c	2018-12-01 10:51:05.667316000 +0000
+@@ -1,5 +1,5 @@
+ #ifndef _POSIX_C_SOURCE
+-#define _POSIX_C_SOURCE 199309L
++#define _POSIX_C_SOURCE 200809L
+ #endif
+
+ #include <time.h>
