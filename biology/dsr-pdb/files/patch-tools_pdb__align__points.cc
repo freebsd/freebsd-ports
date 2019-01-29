80435b015bc77f399a3f5d40247d7de5f5344ccc
Yuri Victorovich <yuri@FreeBSD.org>
2019-01-29 07:56:30 +0000

    New port: biology/dsr-pdb: Simple C++ PDB reader (reads the protein description format)

---
+++ biology/dsr-pdb/files/patch-tools_pdb__align__points.cc
@@ -0,0 +1,10 @@
+--- tools/pdb_align_points.cc.orig	2019-01-29 07:00:56 UTC
++++ tools/pdb_align_points.cc
+@@ -23,6 +23,7 @@
+ #include <vector>
+ #include <iterator>
+ #include <fstream>
++#include <sstream>
+ #include <dsrpdb/align.h>
+ #include <dsrpdb/distance.h>
+ 
