d06b7d37b905ca5848afab4d929a3b7d876451b9
Steve Wills <swills@FreeBSD.org>
2019-01-30 19:22:53 +0000

    sysutils/ipfs-go: fix rc script
    * Fix the rc script when default ipfs_go_path is used
    * Fix the logging by switching to uses the syslog logging in daemon
    * Switch the init to using the --init flag to the ipfs daemon command
    * Fixes the version string since the tarball provided by upstream
      doesn't match the tag for this release (has version string wrong)
    * Allow overriding IPFS_USER/GROUP/HOME at build time
    * Remove unnecessary BEFORE: LOGIN from rc script
    
    PR:		235222
    Approved by:	jhixson (maintainer)

---
+++ sysutils/ipfs-go/files/patch-version.go
@@ -0,0 +1,10 @@
+--- version.go.orig	2019-01-26 18:22:10 UTC
++++ version.go
+@@ -4,6 +4,6 @@ package ipfs
+ var CurrentCommit string
+ 
+ // CurrentVersionNumber is the current application's version literal
+-const CurrentVersionNumber = "0.4.19-dev"
++const CurrentVersionNumber = "0.4.18"
+ 
+ const ApiVersion = "/go-ipfs/" + CurrentVersionNumber + "/"
