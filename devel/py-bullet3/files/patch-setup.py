006712a504a6b3e9644326acd6bddd065cd4487a
Yuri Victorovich <yuri@FreeBSD.org>
2019-01-31 06:54:03 +0000

    devel/py-bullet3: Update 2.87 -> 2.88
    Reported by:	portscout

--- devel/py-bullet3/files/patch-setup.py
+++ devel/py-bullet3/files/patch-setup.py
@@ -1,11 +1,6 @@
---- setup.py.orig	2017-09-29 23:20:39 UTC
+--- setup.py.orig	2018-12-31 20:42:41 UTC
 +++ setup.py
-@@ -409,11 +409,12 @@ elif _platform == "darwin":
-     +["examples/OpenGLWindow/MacOpenGLWindowObjC.m"]
- else:
-     print("bsd!")
-+    libraries = ['GL','GLEW','pthread']
-     os.environ['LDFLAGS'] = '-L/usr/X11R6/lib'
+@@ -535,7 +535,7 @@ else:
      CXX_FLAGS += '-D_BSD '
      CXX_FLAGS += '-I/usr/X11R6/include '
      CXX_FLAGS += '-DHAS_SOCKLEN_T '
@@ -13,4 +8,4 @@
 +    #CXX_FLAGS += '-fno-inline-functions-called-once'
      sources = ["examples/ThirdPartyLibs/enet/unix.c"]\
      +["examples/OpenGLWindow/X11OpenGLWindow.cpp"]\
-     +["examples/ThirdPartyLibs/Glew/glew.c"]\
+     +["examples/ThirdPartyLibs/glad/gl.c"]\
