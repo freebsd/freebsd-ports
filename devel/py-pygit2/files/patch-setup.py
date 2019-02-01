d160bd5efa3a4469f0a2b08644b4fe73d18343e8
Matthias Fechner <mfechner@FreeBSD.org>
2019-02-01 19:48:38 +0000

    MFH: r491724
    Upgrade devel/libgit2 to 0.27.8 and required ports.
    
    Reported by:	PORTSCOUT, swills
    
    Approved by:	ports-secteam (miwi)

--- devel/py-pygit2/files/patch-setup.py
+++ devel/py-pygit2/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2018-09-17 11:22:42 UTC
+--- setup.py.orig	2019-01-30 21:54:31 UTC
 +++ setup.py
-@@ -195,7 +195,7 @@ setup(name='pygit2',
-       long_description=long_description,
+@@ -154,7 +154,7 @@ setup(name='pygit2',
        packages=['pygit2'],
        package_data={'pygit2': ['decl.h']},
--      setup_requires=['cffi', 'pycparser<2.18'],
-+      setup_requires=['cffi', 'pycparser<2.19'],
-       install_requires=['cffi', 'six'],
+       setup_requires=['cffi'],
+-      install_requires=['cffi', 'six', 'pycparser<2.18'],
++      install_requires=['cffi', 'six', 'pycparser<2.19'],
        zip_safe=False,
        cmdclass=cmdclass,
+       **extra_args)
