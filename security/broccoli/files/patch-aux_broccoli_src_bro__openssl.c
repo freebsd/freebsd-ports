7ef95f7e6fc8a6d1199fb9d0a53ab1e3bdfe6cca
Craig Leres <leres@FreeBSD.org>
2019-02-02 21:22:58 +0000

    Update to 1.101.
    PR:		235139
    Submitted by:	bofh
    Approved by:	ler (mentor, implicit)

--- security/broccoli/files/patch-aux_broccoli_src_bro__openssl.c
+++ security/broccoli/files/patch-aux_broccoli_src_bro__openssl.c
@@ -1,13 +1,14 @@
---- aux/broccoli/src/bro_openssl.c.orig	2015-09-06 19:43:23 UTC
+--- aux/broccoli/src/bro_openssl.c.orig	2018-12-19 16:36:18 UTC
 +++ aux/broccoli/src/bro_openssl.c
 @@ -302,7 +302,11 @@ __bro_openssl_init(void)
     * to set up an SSL connection now and abort if this fails in any way.
     */
  
+-  if (! (ctx = SSL_CTX_new(SSLv23_method())))
 +#ifndef OPENSSL_NO_SSL3
-   if (! (ctx = SSL_CTX_new(SSLv3_method())))
++   if (! (ctx = SSL_CTX_new(SSLv3_method())))
 +#else
-+  if (! (ctx = SSL_CTX_new(SSLv23_method())))
++   if (! (ctx = SSL_CTX_new(SSLv23_method())))
 +#endif
      D_RETURN_(FALSE);
    
