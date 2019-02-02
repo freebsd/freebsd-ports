57aa33279ca97ed5077572a9b485836c233d9b99
lifanov <lifanov@FreeBSD.org>
2019-02-02 00:03:06 +0000

    update Docker Compose to 1.23.2
    https://github.com/docker/compose/releases/tag/1.23.2
    
    PR:		235409
    Submitted by:	Dmitri Goutnik <dg@syrec.org>

--- sysutils/docker-compose/files/patch-setup.py
+++ sysutils/docker-compose/files/patch-setup.py
@@ -1,11 +1,13 @@
---- setup.py.orig	2017-12-19 14:49:12 UTC
+--- setup.py.orig	2018-11-28 22:24:54 UTC
 +++ setup.py
-@@ -34,7 +34,7 @@ install_requires = [
+@@ -33,8 +33,8 @@ install_requires = [
+     'cached-property >= 1.2.0, < 2',
      'docopt >= 0.6.1, < 0.7',
      'PyYAML >= 3.10, < 4',
-     'requests >= 2.6.1, != 2.11.0, != 2.12.2, != 2.18.0, < 2.19',
+-    'requests >= 2.6.1, != 2.11.0, != 2.12.2, != 2.18.0, < 2.21',
 -    'texttable >= 0.9.0, < 0.10',
++    'requests >= 2.6.1, != 2.11.0, != 2.12.2, != 2.18.0, < 3',
 +    'texttable >= 0.9.0, < 2',
      'websocket-client >= 0.32.0, < 1.0',
-     'docker >= 2.6.1, < 3.0',
+     'docker >= 3.6.0, < 4.0',
      'dockerpty >= 0.4.1, < 0.5',
