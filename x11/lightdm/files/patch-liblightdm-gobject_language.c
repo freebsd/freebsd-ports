b27824336f1f001688ab47f308956391c421b729
Ben Woods <woodsb02@FreeBSD.org>
2019-02-04 10:28:46 +0000

    x11/lightdm: Update to 1.28.0
    - Also pet portlint
    
    Submitted by:	madpilot (partly)

--- x11/lightdm/files/patch-liblightdm-gobject_language.c
+++ x11/lightdm/files/patch-liblightdm-gobject_language.c
@@ -1,8 +1,8 @@
 https://bugs.launchpad.net/lightdm/+bug/790186
 
---- liblightdm-gobject/language.c.orig	2016-12-09 01:04:48 UTC
+--- liblightdm-gobject/language.c.orig	2018-08-29 22:30:07 UTC
 +++ liblightdm-gobject/language.c
-@@ -57,6 +57,12 @@ G_DEFINE_TYPE (LightDMLanguage, lightdm_language, G_TY
+@@ -57,6 +57,12 @@ G_DEFINE_TYPE_WITH_PRIVATE (LightDMLanguage, lightdm_l
  static gboolean have_languages = FALSE;
  static GList *languages = NULL;
  
@@ -15,7 +15,7 @@ https://bugs.launchpad.net/lightdm/+bug/790186
  static void
  update_languages (void)
  {
-@@ -93,7 +99,7 @@ update_languages (void)
+@@ -83,7 +89,7 @@ update_languages (void)
                  continue;
  
              /* Ignore the non-interesting languages */
@@ -23,8 +23,8 @@ https://bugs.launchpad.net/lightdm/+bug/790186
 +            if (strcmp (command, "locale -a") == 0 && !is_utf8 (code))
                  continue;
  
-             language = g_object_new (LIGHTDM_TYPE_LANGUAGE, "code", code, NULL);
-@@ -109,12 +115,6 @@ update_languages (void)
+             LightDMLanguage *language = g_object_new (LIGHTDM_TYPE_LANGUAGE, "code", code, NULL);
+@@ -94,12 +100,6 @@ update_languages (void)
      have_languages = TRUE;
  }
  
@@ -37,33 +37,33 @@ https://bugs.launchpad.net/lightdm/+bug/790186
  /* Get a valid locale name that can be passed to setlocale(), so we always can use nl_langinfo() to get language and country names. */
  static gchar *
  get_locale_name (const gchar *code)
-@@ -154,7 +154,7 @@ get_locale_name (const gchar *code)
-         for (i = 0; avail_locales[i]; i++)
-         {
-             gchar *loc = avail_locales[i];
--            if (!g_strrstr (loc, ".utf8"))
-+            if (!is_utf8 (loc))
-                 continue;
-             if (g_str_has_prefix (loc, language))
-             {
-@@ -248,10 +248,16 @@ lightdm_language_get_name (LightDMLanguage *language)
+@@ -131,7 +131,7 @@ get_locale_name (const gchar *code)
+     for (gint i = 0; avail_locales[i]; i++)
+     {
+         const gchar *loc = avail_locales[i];
+-        if (!g_strrstr (loc, ".utf8"))
++        if (!is_utf8 (loc))
+             continue;
+         if (g_str_has_prefix (loc, language))
+             return g_strdup (loc);
+@@ -214,10 +214,16 @@ lightdm_language_get_name (LightDMLanguage *language)
          if (locale)
          {
-             gchar *current = setlocale (LC_ALL, NULL);
+             const gchar *current = setlocale (LC_ALL, NULL);
 +#ifdef LC_IDENTIFICATION
              setlocale (LC_IDENTIFICATION, locale);
 +#endif
              setlocale (LC_MESSAGES, "");
  
 +#ifdef _NL_IDENTIFICATION_LANGUAGE
-             gchar *language_en = nl_langinfo (_NL_IDENTIFICATION_LANGUAGE);
+             const gchar *language_en = nl_langinfo (_NL_IDENTIFICATION_LANGUAGE);
 +#else
-+            gchar *language_en = "Unknown";
++            const gchar *language_en = "Unknown";
 +#endif
              if (language_en && strlen (language_en) > 0)
                  priv->name = g_strdup (dgettext ("iso_639_3", language_en));
  
-@@ -291,10 +297,16 @@ lightdm_language_get_territory (LightDMLanguage *langu
+@@ -254,10 +260,16 @@ lightdm_language_get_territory (LightDMLanguage *langu
          if (locale)
          {
              gchar *current = setlocale (LC_ALL, NULL);
