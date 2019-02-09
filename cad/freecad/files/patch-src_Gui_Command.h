978fb00c2925f70e14f8d429125ff70c1dc3b0b8
cmt <cmt@FreeBSD.org>
2019-02-09 21:00:11 +0000

    switch cad/freecad to Qt5

---
+++ cad/freecad/files/patch-src_Gui_Command.h
@@ -0,0 +1,12 @@
+--- src/Gui/Command.h.orig	2019-02-08 22:48:01 UTC
++++ src/Gui/Command.h
+@@ -30,6 +30,9 @@
+ #include <string>
+ #include <vector>
+ 
++#include <QtCore/QtCore>
++#include <QtWidgets/QtWidgets>
++
+ #include <Base/Type.h>
+ 
+ class QWidget;
