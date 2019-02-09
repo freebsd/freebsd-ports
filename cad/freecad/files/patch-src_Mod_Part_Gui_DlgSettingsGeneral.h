c5326368bead9e83eabc23bd8b182297c69ca3a7
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2019-02-09 21:00:11 +0000

    switch cad/freecad to Qt5

---
+++ cad/freecad/files/patch-src_Mod_Part_Gui_DlgSettingsGeneral.h
@@ -0,0 +1,10 @@
+--- src/Mod/Part/Gui/DlgSettingsGeneral.h.orig	2019-02-08 22:48:01 UTC
++++ src/Mod/Part/Gui/DlgSettingsGeneral.h
+@@ -25,6 +25,7 @@
+ #define PARTGUI_DLGSETTINGSGENERAL_H
+ 
+ #include <Gui/PropertyPage.h>
++#include <QtWidgets/QtWidgets>
+ 
+ class QButtonGroup;
+ 
