11a49ac470ff788fec9c75ad4729e44d608a5d14
tcberner <tcberner@FreeBSD.org>
2019-02-10 21:04:35 +0000

    deskutils/kdeconnect-kde: use security/sshpass to pass passwords to sshfs
    On FreeBSD sshfs does not seem to support the "-o password_stdin" option.
    Work around this limitation by using security/sshpass. This should be considered
    a temporary work around until a better solution is found.
    
    With this fix sharing of files from the device should finally work.
    
    PR:             25303
    Submitted by:   Stefan Rumetshofer <sterum77@gmail.com>

---
+++ deskutils/kdeconnect-kde/files/patch-plugins_sftp_mounter.cpp
@@ -0,0 +1,22 @@
+--- plugins/sftp/mounter.cpp.orig	2018-11-06 14:32:08.000000000 +0100
++++ plugins/sftp/mounter.cpp	2019-01-29 18:43:47.324002000 +0100
+@@ -139,7 +139,7 @@
+         << QStringLiteral("-o") << QStringLiteral("gid=") + QString::number(getgid())
+         << QStringLiteral("-o") << QStringLiteral("reconnect")
+         << QStringLiteral("-o") << QStringLiteral("ServerAliveInterval=30")
+-        << QStringLiteral("-o") << QStringLiteral("password_stdin")
++        << QStringLiteral("-o") << QStringLiteral("ssh_command=sshpass -p ") + np.get<QString>("password").toLatin1() + (" ssh")
+         ;
+ 
+     m_proc->setProgram(program, arguments);
+@@ -148,8 +148,8 @@
+     m_proc->start();
+ 
+     //qCDebug(KDECONNECT_PLUGIN_SFTP) << "Passing password: " << np.get<QString>("password").toLatin1();
+-    m_proc->write(np.get<QString>(QStringLiteral("password")).toLatin1());
+-    m_proc->write("\n");
++    //m_proc->write(np.get<QString>(QStringLiteral("password")).toLatin1());
++    //  m_proc->write("\n");
+ 
+ }
+ 
