8d9b1a8a65550fc143b7fdd8990744a1549c373b
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-02-10 19:45:12 +0000

    Update to 0.31.0
    Changes:	https://github.com/certbot/certbot/blob/master/CHANGELOG.md

--- security/py-acme/files/patch-setup.py
+++ security/py-acme/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2018-03-20 00:33:22 UTC
+--- setup.py.orig	2019-02-07 21:20:31 UTC
 +++ setup.py
-@@ -14,7 +14,6 @@ install_requires = [
+@@ -13,7 +13,6 @@ install_requires = [
      # formerly known as acme.jose:
      'josepy>=1.0.0',
      # Connection.set_tlsext_host_name (>=0.13)
 -    'mock',
-     'PyOpenSSL>=0.13',
+     'PyOpenSSL>=0.13.1',
      'pyrfc3339',
      'pytz',
 @@ -24,6 +23,7 @@ install_requires = [
