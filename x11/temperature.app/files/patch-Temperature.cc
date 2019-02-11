cb9f5bae2440ac152c4c58fd4b78a6a1ece60fb8
Koichiro Iwao <meta@FreeBSD.org>
2019-02-11 04:39:47 +0000

    x11/temperature.app: Update to 1.5
    While here,
    - Update MASTER_SITES and WWW
    
    PR:		235608
    Submitted by:	Marco Beishuizen <mbeis@xs4all.nl>  (maintainer)

--- x11/temperature.app/files/patch-Temperature.cc
+++ x11/temperature.app/files/patch-Temperature.cc
@@ -1,16 +1,12 @@
---- Temperature.cc.bak	2016-08-24 21:28:41.551324000 -0400
-+++ Temperature.cc	2016-08-24 21:26:25.860220000 -0400
-@@ -20,24 +20,29 @@
- //
- 
+--- Temperature.cc.orig	2010-02-07 06:43:52 UTC
++++ Temperature.cc
+@@ -22,22 +22,27 @@
  #include <X11/Xlib.h>
--#include <iostream.h>
--#include <fstream.h>
+ #include <iostream>
+ #include <fstream>
 -#include <stdlib.h>
 -#include <stdio.h>
 -#include <string.h>
-+#include <iostream>
-+#include <fstream>
 +#include <cstdlib>
 +#include <cstdio>
 +#include <cstring>
@@ -39,12 +35,12 @@
  volatile static ChildStatus childStatus;
  
  static void catchBrokenPipe(int sig) 
-@@ -415,7 +420,7 @@
+@@ -415,7 +420,7 @@ void Temperature::run() 
              const char* verbose = (mVerbose ? "--verbose" : "--quiet");
              char* URL = new char[strlen(METAR_URL) + strlen(mStationId) + 1];
              sprintf(URL, METAR_URL, mStationId);
 -            execlp("wget", "wget", "--cache=off", "--tries=0", verbose, "-O", tmpFile, URL, 0);
 +            execlp("wget", "wget", "--no-cache", "--tries=0", verbose, "-O", tmpFile, URL, NULL);
-             cerr << APPNAME << ": could not fetch temperature (wget not found in $PATH)" << endl;
+             std::cerr << APPNAME << ": could not fetch temperature (wget not found in $PATH)" << std::endl;
              remove(tmpFile);
              exit(0);
--- x11/temperature.app/files/patch-Xpm.cc
+++ x11/temperature.app/files/patch-Xpm.cc
@@ -1,22 +1,19 @@
---- Xpm.cc.bak	2002-04-06 08:44:36.000000000 +0900
-+++ Xpm.cc	2013-12-07 17:25:00.000000000 +0900
-@@ -22,12 +22,16 @@
- #include <X11/Xlib.h>
+--- Xpm.cc.orig	2010-02-07 06:48:35 UTC
++++ Xpm.cc
+@@ -23,10 +23,14 @@
  #include <X11/xpm.h>
  #include <X11/extensions/shape.h>
--#include <iostream.h>
+ #include <iostream>
 -#include <stdlib.h>
 -#include <string.h>
-+#include <iostream>
 +#include <cstdlib>
 +#include <cstring>
  #include "Temperature.h"
  #include "Xpm.h"
- 
++
 +using std::cerr;
 +using std::cout;
 +using std::endl;
-+
+ 
  Xpm::Xpm(Display* display, Window root, char** data) 
  {
-    int error;
