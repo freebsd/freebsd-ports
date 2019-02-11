6698dbd89491ff513ca130db84656d615cfc28ef
meta <meta@FreeBSD.org>
2019-02-11 04:39:47 +0000

    x11/temperature.app: Update to 1.5
    While here,
    - Update MASTER_SITES and WWW
    
    PR:		235608
    Submitted by:	Marco Beishuizen <mbeis@xs4all.nl>  (maintainer)

--- x11/temperature.app/files/patch-Temperature.h
+++ x11/temperature.app/files/patch-Temperature.h
@@ -1,17 +1,7 @@
---- Temperature.h.bak	2016-08-24 21:28:32.665119000 -0400
-+++ Temperature.h	2016-08-24 21:26:20.577933000 -0400
-@@ -22,14 +22,16 @@
- #ifndef _TEMPERATURE_H_
- #define _TEMPERATURE_H_
- 
--#include <fstream.h>
-+#include <fstream>
- #include <X11/Xlib.h>
- 
-+using std::ifstream;
-+
- #define APPNAME                 "Temperature.app"
- #define VERSION                 "1.4"
+--- Temperature.h.orig	2010-02-07 07:03:11 UTC
++++ Temperature.h
+@@ -29,7 +29,7 @@
+ #define VERSION                 "1.5"
  #define INSTANCENAME            "temperature_app"
  #define CLASSNAME               "Temperature_app"
 -#define METAR_URL               "http://weather.noaa.gov/pub/data/observations/metar/decoded/%s.TXT"
