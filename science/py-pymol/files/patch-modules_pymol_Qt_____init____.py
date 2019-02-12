b3c79063dfc9e69486f92f725fcf33ecb81af1fe
yuri <yuri@FreeBSD.org>
2019-02-12 06:52:57 +0000

    science/py-pymol: Update 2.2.0 -> 2.3.0
    Reported by:	portscout

--- science/py-pymol/files/patch-modules_pymol_Qt_____init____.py
+++ science/py-pymol/files/patch-modules_pymol_Qt_____init____.py
@@ -1,15 +1,15 @@
---- modules/pymol/Qt/__init__.py.orig	2018-03-22 05:10:42 UTC
+--- modules/pymol/Qt/__init__.py.orig	2019-02-11 14:08:10 UTC
 +++ modules/pymol/Qt/__init__.py
-@@ -28,7 +28,7 @@ if not PYQT_NAME:
+@@ -29,7 +29,7 @@ if not PYQT_NAME:
          if DEBUG:
              print('import PyQt5 failed')
  
 -if not PYQT_NAME:
 +if False:
      try:
-         __import__("sip").setapi("QString", 2)
-         from PyQt4 import QtGui, QtCore, QtOpenGL
-@@ -37,7 +37,7 @@ if not PYQT_NAME:
+         from PySide2 import QtGui, QtCore, QtOpenGL, QtWidgets
+         PYQT_NAME = 'PySide2'
+@@ -46,7 +46,7 @@ if not PYQT_NAME:
          if DEBUG:
              print('import PyQt4 failed')
  
--- science/py-pymol/files/patch-setup.py
+++ science/py-pymol/files/patch-setup.py
@@ -1,19 +1,11 @@
---- setup.py.orig	2018-07-24 16:14:37 UTC
+--- setup.py.orig	2019-02-12 06:00:47 UTC
 +++ setup.py
-@@ -54,6 +54,8 @@ try:
-             "keyword support.")
+@@ -52,6 +52,8 @@ try:
+             "shared library; no: disable fast MMTF load support")
      parser.add_argument('--help-distutils', action="store_true",
              help="show help for distutils options and exit")
 +    parser.add_argument('--single-version-externally-managed', action="store_true",
 +            help="single-version-externally-managed")
+     parser.add_argument('--testing', action="store_true",
+             help="Build C-level tests")
      options, sys.argv[1:] = parser.parse_known_args(namespace=options)
- except ImportError:
-     print("argparse not available")
-@@ -278,7 +280,6 @@ ext_comp_args = [
-     # optimizations
-     "-ffast-math",
-     "-funroll-loops",
--    "-O3",
-     "-fcommon",
- ]
- ext_link_args = []
