3113d712b23fb2a32d080ca504cf6ab9d539f7d6
gahr <gahr@FreeBSD.org>
2019-02-12 15:34:14 +0000

    devel/dparser: update to 1.31

--- devel/dparser/files/patch-arg.c
+++ devel/dparser/files/patch-arg.c
@@ -1,11 +1,20 @@
---- arg.c.orig	2013-01-23 02:14:58 UTC
+--- arg.c.orig	2019-01-10 00:20:05 UTC
 +++ arg.c
-@@ -40,7 +40,7 @@ process_arg(ArgumentState *arg_state, in
+@@ -31,7 +31,7 @@ void process_arg(ArgumentState *arg_state, int i, char
            *(double *)desc[i].location = atof(arg);
            break;
          case 'L':
 -          *(int64 *)desc[i].location = atoll(arg);
 +          *(int64 *)desc[i].location = strtoll(arg,NULL,10);
            break;
-         case 'S': strncpy((char *)desc[i].location,arg, atoi(desc[i].type+1));
-           break;
+         case 'S':
+           strncpy((char *)desc[i].location, arg, atoi(desc[i].type + 1));
+@@ -143,7 +143,7 @@ void usage(ArgumentState *arg_state, char *arg_unused)
+ #if defined(__alpha)
+                 " %-9ld",
+ #else
+-#if defined(FreeBSD)
++#if defined(__FreeBSD__)
+                 " %-9qd",
+ #else
+                 " %-9" PRId64,
