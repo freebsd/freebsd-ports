c0949316431dabb34effa12aeca749a3a1c68df5
rodrigo <rodrigo@FreeBSD.org>
2019-02-13 07:29:04 +0000

    Upgrade net-mgmt/smokeping to 2.7.3
    - Use GITHUB to get distfiles
    - Redo the patches
    - Rework options
    
    Changelog: https://oss.oetiker.ch/smokeping/pub/CHANGES

--- net-mgmt/smokeping/files/patch-lib_Smokeping_probes_AnotherDNS.pm
+++ net-mgmt/smokeping/files/patch-lib_Smokeping_probes_AnotherDNS.pm
@@ -1,6 +1,6 @@
---- lib/Smokeping/probes/AnotherDNS.pm.orig	2014-10-29 09:03:10.000000000 +0100
-+++ lib/Smokeping/probes/AnotherDNS.pm	2015-02-12 19:43:50.000000000 +0100
-@@ -25,6 +25,8 @@
+--- lib/Smokeping/probes/AnotherDNS.pm.orig	2018-02-02 21:14:55 UTC
++++ lib/Smokeping/probes/AnotherDNS.pm
+@@ -25,6 +25,8 @@ use IO::Socket;
  use IO::Select;
  use Net::DNS;
  
@@ -9,7 +9,7 @@
  sub pod_hash {
  	return {
  		name => <<DOC,
-@@ -108,7 +110,7 @@
+@@ -111,7 +113,7 @@ sub pingone ($) {
          $elapsed = tv_interval( $t0, $t1 );
          if ( defined $ready ) {
              my $buf = '';
