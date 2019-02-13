2900541294a485917c859bbbfbaacfa233fee7c8
Yuri Victorovich <yuri@FreeBSD.org>
2019-02-13 07:00:58 +0000

    New port: science/molscript: Display 3D molecules in schematic and detailed representations

---
+++ science/molscript/files/patch-clib_ogl__bitmap__character.c
@@ -0,0 +1,11 @@
+--- clib/ogl_bitmap_character.c.orig	2019-02-13 05:26:19 UTC
++++ clib/ogl_bitmap_character.c
+@@ -23,7 +23,7 @@
+ 
+ #include <assert.h>
+ 
+-#include <../lib/glut/glutbitmap.h>
++#include "glutbitmap.h"
+ 
+ 
+ /*------------------------------------------------------------*/
