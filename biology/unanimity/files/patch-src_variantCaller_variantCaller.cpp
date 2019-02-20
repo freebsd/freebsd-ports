61be8e00f88687869190f6a4a2d1f4a0fc19869c
jwb <jwb@FreeBSD.org>
2019-02-20 01:36:51 +0000

    biology/unanimity: Upgrade to 3.4.0
    Switch from cmake to new meson build system
    Tests disabled for now pending work on unbundling googletest

---
+++ biology/unanimity/files/patch-src_variantCaller_variantCaller.cpp
@@ -0,0 +1,9 @@
+--- src/variantCaller/variantCaller.cpp.orig	2019-02-17 23:15:10 UTC
++++ src/variantCaller/variantCaller.cpp
+@@ -1,5 +1,6 @@
+ // Author: David Seifert
+ 
++#include <iostream>
+ #include <fstream>
+ #include <string>
+ 
