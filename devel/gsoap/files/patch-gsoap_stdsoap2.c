8c28e6b7935f448071999538fbbc1fd97893fddc
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2019-02-20 17:55:28 +0000

    devel/gsoap: Update version 2.8.78=>2.8.79
    - Fix build with libressl [1]
    
    PR:		235506 [1]
    Submitted by:	rozhuk.im@gmail.com [1]

--- devel/gsoap/files/patch-gsoap_stdsoap2.c
+++ devel/gsoap/files/patch-gsoap_stdsoap2.c
@@ -1,6 +1,6 @@
---- gsoap/stdsoap2.c.orig	2019-01-27 15:50:05 UTC
+--- gsoap/stdsoap2.c.orig	2019-02-10 15:19:37 UTC
 +++ gsoap/stdsoap2.c
-@@ -4302,7 +4302,7 @@ ssl_auth_init(struct soap *soap)
+@@ -4306,7 +4306,7 @@ ssl_auth_init(struct soap *soap)
    ERR_clear_error();
    if (!soap->ctx)
    {
@@ -9,16 +9,29 @@
      /* TLS_method: a TLS/SSL connection established may understand the SSLv3, TLSv1, TLSv1.1 and TLSv1.2 protocols. */
      soap->ctx = SSL_CTX_new(TLS_method());
  #else
-@@ -4390,7 +4390,7 @@ ssl_auth_init(struct soap *soap)
+@@ -4394,7 +4394,7 @@ ssl_auth_init(struct soap *soap)
  #endif
    if ((soap->ssl_flags & SOAP_SSL_RSA))
    {
--#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if (OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER))
+-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
++#if (OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER))
      if (SSL_CTX_need_tmp_RSA(soap->ctx))
- #endif
      {
-@@ -6682,6 +6682,9 @@ soap_bind(struct soap *soap, const char *host, int por
+       unsigned long e = RSA_F4;
+@@ -4473,10 +4473,12 @@ ssl_auth_init(struct soap *soap)
+     minv = TLS1_1_VERSION;
+   else if ((soap->ssl_flags & SOAP_TLSv1_2))
+     minv = TLS1_2_VERSION;
++#ifdef TLS1_3_VERSION
+   else if ((soap->ssl_flags & SOAP_TLSv1_3))
+     minv = TLS1_3_VERSION;
+   if ((soap->ssl_flags & SOAP_TLSv1_3) && OpenSSL_version_num() >= 0x10101000L)
+     maxv = TLS1_3_VERSION;
++#endif
+   else if ((soap->ssl_flags & SOAP_TLSv1_2))
+     maxv = TLS1_2_VERSION;
+   else if ((soap->ssl_flags & SOAP_TLSv1_1))
+@@ -6714,6 +6716,9 @@ soap_bind(struct soap *soap, const char *host, int por
    }
  #endif
  #ifdef TCP_FASTOPEN
