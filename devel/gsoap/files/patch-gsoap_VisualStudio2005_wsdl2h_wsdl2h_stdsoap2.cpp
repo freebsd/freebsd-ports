87d6485d824bfae94e0338e26f37fcf5051ce22e
bofh <bofh@FreeBSD.org>
2019-02-20 17:55:28 +0000

    devel/gsoap: Update version 2.8.78=>2.8.79
    - Fix build with libressl [1]
    
    PR:		235506 [1]
    Submitted by:	rozhuk.im@gmail.com [1]

--- devel/gsoap/files/patch-gsoap_VisualStudio2005_wsdl2h_wsdl2h_stdsoap2.cpp
+++ devel/gsoap/files/patch-gsoap_VisualStudio2005_wsdl2h_wsdl2h_stdsoap2.cpp
@@ -1,6 +1,6 @@
---- gsoap/VisualStudio2005/wsdl2h/wsdl2h/stdsoap2.cpp.orig	2019-01-27 15:50:05 UTC
+--- gsoap/VisualStudio2005/wsdl2h/wsdl2h/stdsoap2.cpp.orig	2019-02-10 15:19:37 UTC
 +++ gsoap/VisualStudio2005/wsdl2h/wsdl2h/stdsoap2.cpp
-@@ -4302,7 +4302,7 @@ ssl_auth_init(struct soap *soap)
+@@ -4306,7 +4306,7 @@ ssl_auth_init(struct soap *soap)
    ERR_clear_error();
    if (!soap->ctx)
    {
@@ -9,12 +9,12 @@
      /* TLS_method: a TLS/SSL connection established may understand the SSLv3, TLSv1, TLSv1.1 and TLSv1.2 protocols. */
      soap->ctx = SSL_CTX_new(TLS_method());
  #else
-@@ -4390,7 +4390,7 @@ ssl_auth_init(struct soap *soap)
+@@ -4394,7 +4394,7 @@ ssl_auth_init(struct soap *soap)
  #endif
    if ((soap->ssl_flags & SOAP_SSL_RSA))
    {
--#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if (OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER))
+-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
++#if (OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER))
      if (SSL_CTX_need_tmp_RSA(soap->ctx))
- #endif
      {
+       unsigned long e = RSA_F4;
