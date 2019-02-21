3eca0068dc216f18e426fcf46ff80f1000c95766
sunpoet <sunpoet@FreeBSD.org>
2019-02-21 16:11:06 +0000

    Update to 1.16.1
    - Move TEST_DEPENDS upward
    - Simplify Makefile
    
    Changes:	https://github.com/numpy/numpy/releases
    PR:		235721
    Exp-run by:	antoine

--- math/py-numpy/files/patch-numpy-f2py-f2py2e.py
+++ math/py-numpy/files/patch-numpy-f2py-f2py2e.py
@@ -1,14 +1,14 @@
---- numpy/f2py/f2py2e.py.orig	2014-10-26 10:36:14.000000000 -0400
-+++ numpy/f2py/f2py2e.py	2014-12-11 07:52:11.000000000 -0500
-@@ -516,6 +516,7 @@
+--- numpy/f2py/f2py2e.py.orig	2019-01-31 02:31:08 UTC
++++ numpy/f2py/f2py2e.py
+@@ -599,6 +599,7 @@ def run_compile():
      include_dirs, sources = filter_files('-I', '', sources, remove_prefix=1)
      library_dirs, sources = filter_files('-L', '', sources, remove_prefix=1)
      libraries, sources = filter_files('-l', '', sources, remove_prefix=1)
 +    extra_link_args, sources = filter_files('-Wl,|-B|-f','',sources,remove_prefix=0)
      undef_macros, sources = filter_files('-U', '', sources, remove_prefix=1)
      define_macros, sources = filter_files('-D', '', sources, remove_prefix=1)
-     using_numarray = 0
-@@ -551,6 +552,7 @@
+     for i in range(len(define_macros)):
+@@ -625,6 +626,7 @@ def run_compile():
                  'undef_macros': undef_macros,
                  'extra_objects': extra_objects,
                  'f2py_options': f2py_flags,
