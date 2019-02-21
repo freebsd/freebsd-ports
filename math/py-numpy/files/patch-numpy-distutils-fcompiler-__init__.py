f9d7c5e40df67b32de23f894524a6bd311c50007
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-02-21 16:11:06 +0000

    Update to 1.16.1
    - Move TEST_DEPENDS upward
    - Simplify Makefile
    
    Changes:	https://github.com/numpy/numpy/releases
    PR:		235721
    Exp-run by:	antoine

--- math/py-numpy/files/patch-numpy-distutils-fcompiler-__init__.py
+++ math/py-numpy/files/patch-numpy-distutils-fcompiler-__init__.py
@@ -1,11 +1,11 @@
---- numpy/distutils/fcompiler/__init__.py.orig	2008-04-06 23:03:15.000000000 +0900
-+++ numpy/distutils/fcompiler/__init__.py	2008-04-06 23:08:56.000000000 +0900
-@@ -179,7 +179,7 @@
+--- numpy/distutils/fcompiler/__init__.py.orig	2019-01-31 02:31:08 UTC
++++ numpy/distutils/fcompiler/__init__.py
+@@ -177,7 +177,7 @@ class FCompiler(CCompiler):
      # gnu95 compiler type when there are F90 sources.
      suggested_f90_compiler = None
  
 -    compile_switch = "-c"
-+    compile_switch = "-c -fPIC"
++    compile_switch = "-fPIC"
      object_switch = "-o "   # Ending space matters! It will be stripped
                              # but if it is missing then object_switch
                              # will be prefixed to object file name by
--- math/py-numpy/files/patch-numpy-distutils-system_info.py
+++ math/py-numpy/files/patch-numpy-distutils-system_info.py
@@ -1,6 +1,15 @@
---- numpy/distutils/system_info.py.orig	2016-10-03 23:42:24 UTC
+--- numpy/distutils/system_info.py.orig	2019-01-31 02:31:08 UTC
 +++ numpy/distutils/system_info.py
-@@ -1038,8 +1038,8 @@ class atlas_info(system_info):
+@@ -169,6 +169,8 @@ def _c_string_literal(s):
+     Convert a python string into a literal suitable for inclusion into C code
+     """
+     # only these three characters are forbidden in C strings
++    if s is None:
++        return '"None"'
+     s = s.replace('\\', r'\\')
+     s = s.replace('"',  r'\"')
+     s = s.replace('\n', r'\n')
+@@ -1109,8 +1111,8 @@ class atlas_info(system_info):
      dir_env_var = 'ATLAS'
      _lib_names = ['f77blas', 'cblas']
      if sys.platform[:7] == 'freebsd':
@@ -11,8 +20,8 @@
      else:
          _lib_atlas = ['atlas']
          _lib_lapack = ['lapack']
-@@ -1574,11 +1574,6 @@ class blas_opt_info(system_info):
-             self.set_info(**blas_mkl_info)
+@@ -1630,11 +1632,6 @@ class blas_opt_info(system_info):
+             self.set_info(**blis_info)
              return
  
 -        openblas_info = get_info('openblas')
