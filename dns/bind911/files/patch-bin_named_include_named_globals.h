4ab34dc21c3d8623d80b1bc6417a6b6b6abc5c39
Mathieu Arnold <mat@FreeBSD.org>
2019-02-22 08:52:52 +0000

    Update to 9.11.5-P4.
    MFH:		2019Q1 (blanket)
    Security:	CVE-2018-5744 CVE-2018-5745 CVE-2019-6465

--- dns/bind911/files/patch-bin_named_include_named_globals.h
+++ dns/bind911/files/patch-bin_named_include_named_globals.h
@@ -1,8 +1,8 @@
 We reference the pid file as being run/named/pid everywere else.
 
---- bin/named/include/named/globals.h.orig	2018-10-06 01:36:17 UTC
+--- bin/named/include/named/globals.h.orig	2019-02-05 00:06:43 UTC
 +++ bin/named/include/named/globals.h
-@@ -139,7 +139,7 @@ EXTERN bool		ns_g_forcelock		INIT(false)
+@@ -139,7 +139,7 @@ EXTERN bool		ns_g_forcelock		INIT(false);
  #if NS_RUN_PID_DIR
  EXTERN const char *		ns_g_defaultpidfile 	INIT(NS_LOCALSTATEDIR
  							     "/run/named/"
