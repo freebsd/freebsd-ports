01a78ff8af12961190d6c8dfd4a76aa4161232f5
tobik <tobik@FreeBSD.org>
2019-02-22 08:04:25 +0000

    sysutils/solaar: Update to 0.9.2-259-gc07c115
    This make Solaar compatible with Python 3.7 where 'async' has become
    a reserved keyword.
    
    Changes:	https://github.com/pwr/Solaar/compare/0.9.2-221-g1a67b33...0.9.2-259-gc07c115

--- sysutils/solaar/files/patch-setup.py
+++ sysutils/solaar/files/patch-setup.py
@@ -1,19 +1,21 @@
---- setup.py.orig	2017-03-20 23:07:26 UTC
+--- setup.py.orig	2019-01-30 15:58:00 UTC
 +++ setup.py
-@@ -6,10 +6,8 @@ from distutils.core import setup
+@@ -6,14 +6,9 @@ from distutils.core import setup
  autostart_path = '/etc/xdg/autostart'
  
  import sys
 -backup_path_0 = sys.path[0]
 -sys.path[0] = backup_path_0 + '/lib'
--from solaar import NAME, __version__
--sys.path[0] = backup_path_0
-+NAME = 'Solaar'
-+__version__ = '0.9.2'
+-#from solaar import NAME, __version__
+ __version__ = '0.9.2'
+ NAME = 'Solaar'
  
+-sys.path[0] = backup_path_0
+-
  if 'install' in sys.argv:
  	# naively guess where the autostart .desktop file should be installed
-@@ -22,7 +20,7 @@ if 'install' in sys.argv:
+ 	if '--prefix' in sys.argv or any(x.startswith('--prefix=') for x in sys.argv) or '--home' in sys.argv:
+@@ -25,7 +20,7 @@ if 'install' in sys.argv:
  		autostart_path = path.join(xdg_config_home, 'autostart')
  		del environ, path, xdg_config_home
  
@@ -22,7 +24,7 @@
  
  
  def _data_files():
-@@ -36,7 +34,6 @@ def _data_files():
+@@ -39,7 +34,6 @@ def _data_files():
  		yield _dirname(mo), [mo]
  
  	yield 'share/applications', ['share/applications/solaar.desktop']
