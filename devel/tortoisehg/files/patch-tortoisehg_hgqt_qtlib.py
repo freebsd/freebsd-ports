8c814f4d87ecaedda9458f5d619a4b6b26cadad1
Gleb Popov <arrowd@FreeBSD.org>
2019-02-23 16:22:48 +0000

    devel/tortoisehg: Update to 4.8.2.
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D19304

--- devel/tortoisehg/files/patch-tortoisehg_hgqt_qtlib.py
+++
@@ -1,8 +0,0 @@
---- tortoisehg/hgqt/qtlib.py.orig	2018-11-19 10:53:15 UTC
-+++ tortoisehg/hgqt/qtlib.py
-@@ -1470,5 +1470,3 @@ class PaletteSwitcher(object):
- def setContextMenuShortcut(action, shortcut):
-     """Set shortcut for a context menu action, making sure it's visible"""
-     action.setShortcut(shortcut)
--    if QT_VERSION >= 0x50a00:
--        action.setShortcutVisibleInContextMenu(True)
