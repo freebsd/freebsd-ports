d77fc6ab7046a18dbb2e35fd02dca612818b322e
Thierry Thomas <thierry@FreeBSD.org>
2019-02-23 16:57:01 +0000

    Unbreak video recording.
    Since I'm here, make some cleaning and add some explicit dependencies.
    
    PR:		235951
    Submitted by:	romain

--- sysutils/xvidcap/files/patch-src_gnome__ui.c
+++ sysutils/xvidcap/files/patch-src_gnome__ui.c
@@ -1,6 +1,6 @@
---- ./src/gnome_ui.c.orig	2008-06-12 22:48:03.000000000 +0200
-+++ ./src/gnome_ui.c	2014-04-30 22:44:15.000000000 +0200
-@@ -1679,7 +1679,7 @@
+--- src/gnome_ui.c.orig	2008-06-12 20:48:03 UTC
++++ src/gnome_ui.c
+@@ -1679,7 +1679,7 @@ xvc_init_pre (int argc, char **argv)
  {
  #define DEBUGFUNCTION "xvc_init_pre()"
      g_thread_init (NULL);
