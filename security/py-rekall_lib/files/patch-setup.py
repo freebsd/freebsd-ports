f7660a9d5b84421bee2dcf435cc2ebc819731a37
Antoine Brodin <antoine@FreeBSD.org>
2019-02-24 20:16:08 +0000

    Update rekall to 1.7.2rc1-52

--- security/py-rekall_lib/files/patch-setup.py
+++ security/py-rekall_lib/files/patch-setup.py
@@ -1,12 +1,14 @@
---- setup.py.orig	2017-11-05 21:38:24 UTC
+--- setup.py.orig	2019-01-04 00:51:27 UTC
 +++ setup.py
-@@ -41,9 +41,9 @@ def find_data_files(source):
+@@ -41,11 +41,11 @@ def find_data_files(source):
      return result
  
  install_requires = [
 -    "arrow==0.10.0",
 +    "arrow>=0.10.0",
      "future",
+     # We need to upgrade but this seems to break Rekall.
+     #    "sortedcontainers >= 2.0, < 3.0",
 -    "sortedcontainers==1.5.7",
 +    "sortedcontainers>=1.5.7",
  ]
