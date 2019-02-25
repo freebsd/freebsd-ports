32e3a68d62a8ee3ab3052582e3bb11610130a993
Mark Linimon <linimon@FreeBSD.org>
2019-02-25 15:23:26 +0000

    powerpc* platforms use uc_mcontext.mc_srr0 to access PC.
    Upstream PR at https://github.com/GenericMappingTools/gmt/pull/444 .
    
    PR:		236016
    Submitted by:	Piotr Kubaj
    Approved by:	maintainer

---
+++ graphics/gmt5/files/patch-src_common__sighandler.c
@@ -0,0 +1,11 @@
+--- src/common_sighandler.c.orig	2019-02-25 06:44:38 UTC
++++ src/common_sighandler.c
+@@ -69,6 +69,8 @@ void backtrace_symbols_fd(void *const *buffer, int siz
+ #  define UC_IP(uc) ((void *) (uc)->uc_mcontext.mc_rip)
+ # elif defined( __arm__)
+ #  define UC_IP(uc) ((void *) (uc)->uc_mcontext.arm_pc)
++# elif defined( __ppc__)
++#  define UC_IP(uc) ((void *) (uc)->uc_mcontext.mc_srr0)
+ # else
+ #  define UC_IP(uc) ((void *) (uc)->uc_mcontext.mc_eip)
+ # endif
