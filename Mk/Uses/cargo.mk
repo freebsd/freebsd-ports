f8eead936c09b69171b6fdb83823ed3d9973b5af
Jan Beich <jbeich@FreeBSD.org>
2019-02-28 18:47:28 +0000

    USES=cargo to update libc to 0.2.49 for Tier2
    PR:		235063
    Differential Revision:	https://reviews.freebsd.org/D19309

--- Mk/Uses/cargo.mk
+++ Mk/Uses/cargo.mk
@@ -139,6 +139,9 @@ _libc_VER=	${libc:C/.*-//}
 . if ${_libc_VER:R:R} == 0 && (${_libc_VER:R:E} < 2 || ${_libc_VER:R:E} == 2 && ${_libc_VER:E} < 38)
 DEV_WARNING+=	"CARGO_CRATES=${libc} may be unstable on FreeBSD 12.0. Consider updating to the latest version (higher than 0.2.37)."
 . endif
+. if ${_libc_VER:R:R} == 0 && (${_libc_VER:R:E} < 2 || ${_libc_VER:R:E} == 2 && ${_libc_VER:E} < 49)
+DEV_WARNING+=	"CARGO_CRATES=${libc} may be unstable on aarch64 or not build on armv6, armv7, powerpc64. Consider updating to the latest version (higher than 0.2.49)."
+. endif
 .undef _libc_VER
 .endfor
 
