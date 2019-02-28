14ff569c1372dc638c3676ef9ba20afa8acd37b3
Kai Knoblich <kai@FreeBSD.org>
2019-02-28 15:36:27 +0000

    devel/py-gitosis: Update to 0.3 and limit to Python 2.7
    Python 3 handles octal numbers in a different way as with Python 2.7. The
    latter one uses leading zeroes (e.g. 0755) to signify that a number is an
    octal number but this is invalid for Python 3 where the required syntax is
    0o755 for that case.
    
    Also there are some references to the "cStringIO" module that is gone in
    Python 3. Thus limit this port to Python 2.7 only.
    
    While I'm here:
    
    * Add location to license file
    * Update URL in pkg-message
    * Add test target with the required dependencies
    * Pet portlint
    
    Changelog:
    
    https://github.com/tv42/gitosis/releases/tag/release%2F0.3
    
    PR:		235441 [1], 235894 [2]
    Submitted by:	Keith Gaughan <k@stereochro.me> [2]
    Reported by:	Dan McGrath <danmcgrath.ca@gmail.com> [1]
    Approved by:	miwi (mentor), maintainer timeout (> ~25 days) [1], maintainer [2]
    Differential Revision:	https://reviews.freebsd.org/D19379

---
+++ devel/py-gitosis/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2019-02-18 05:26:38 UTC
++++ setup.py
+@@ -18,7 +18,7 @@ def subdir_contents(path):
+ 
+ setup(
+     name = "gitosis",
+-    version = "0.2",
++    version = "0.3",
+     packages = find_packages(),
+ 
+     author = "Tommi Virtanen",
