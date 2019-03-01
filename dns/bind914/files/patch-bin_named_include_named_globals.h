083d84bbcfb7de1a37d492a4cc708b32de71f632
mat <mat@FreeBSD.org>
2019-03-01 10:10:03 +0000

    Add BIND 9.14.0 first release candidate.

---
+++ dns/bind914/files/patch-bin_named_include_named_globals.h
@@ -0,0 +1,13 @@
+We reference the pid file as being run/named/pid everywere else.
+
+--- bin/named/include/named/globals.h.orig	2019-02-06 23:44:09 UTC
++++ bin/named/include/named/globals.h
+@@ -129,7 +129,7 @@ EXTERN bool		named_g_forcelock	INIT(false);
+ #if NAMED_RUN_PID_DIR
+ EXTERN const char *		named_g_defaultpidfile 	INIT(NAMED_LOCALSTATEDIR
+ 							     "/run/named/"
+-							     "named.pid");
++							     "pid");
+ #else
+ EXTERN const char *		named_g_defaultpidfile 	INIT(NAMED_LOCALSTATEDIR
+ 							     "/run/named.pid");
