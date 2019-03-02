19e6920222a6b85dd1851c78287309398a82fdb3
hrs <hrs@FreeBSD.org>
2019-03-02 23:48:27 +0000

    Update to v0.5.0 which supports OpenSSL 1.0.x/1.1.x.
    This is a forked version of OpenSSL TPM engine from the original
    upstream, TrouSerS project.

--- security/openssl_tpm_engine/files/patch-src-e_tpm.h
+++ security/openssl_tpm_engine/files/patch-src-e_tpm.h
@@ -1,8 +1,6 @@
-http://sourceforge.net/mailarchive/message.php?msg_name=4C0E2D48.20803%40sirrix.com
-
---- e_tpm.h.orig	2012-09-12 15:32:53 UTC
-+++ e_tpm.h
-@@ -66,6 +66,8 @@ void ERR_TSS_error(int function, int rea
+--- src/e_tpm.h.orig	2017-12-18 15:45:34 UTC
++++ src/e_tpm.h
+@@ -66,6 +66,8 @@ void ERR_TSS_error(int function, int reason, char *fil
  #define TPM_F_TPM_FILL_RSA_OBJECT		116
  #define TPM_F_TPM_ENGINE_GET_AUTH		117
  #define TPM_F_TPM_CREATE_SRK_POLICY		118
@@ -11,7 +9,7 @@ http://sourceforge.net/mailarchive/message.php?msg_name=4C0E2D48.20803%40sirrix.
  
  /* Reason codes. */
  #define TPM_R_ALREADY_LOADED			100
-@@ -96,6 +98,7 @@ void ERR_TSS_error(int function, int rea
+@@ -96,6 +98,7 @@ void ERR_TSS_error(int function, int reason, char *fil
  #define TPM_R_ID_INVALID			125
  #define TPM_R_UI_METHOD_FAILED			126
  #define TPM_R_UNKNOWN_SECRET_MODE		127
@@ -19,10 +17,12 @@ http://sourceforge.net/mailarchive/message.php?msg_name=4C0E2D48.20803%40sirrix.
  
  /* structure pointed to by the RSA object's app_data pointer */
  struct rsa_app_data
-@@ -107,6 +110,25 @@ struct rsa_app_data
+@@ -105,6 +108,25 @@ struct rsa_app_data
+ 	TSS_HENCDATA hEncData;
+ 	UINT32 encScheme;
  	UINT32 sigScheme;
- };
- 
++};
++
 +/* Added by c.hol...@sirrix.com */
 +struct quote_request
 +{
@@ -40,8 +40,6 @@ http://sourceforge.net/mailarchive/message.php?msg_name=4C0E2D48.20803%40sirrix.
 +{
 +	unsigned int index;
 +	unsigned int length;
-+};
-+
- #define TPM_ENGINE_EX_DATA_UNINIT		-1
- #define RSA_PKCS1_OAEP_PADDING_SIZE		(2 * SHA_DIGEST_LENGTH + 2)
+ };
  
+ #define TPM_ENGINE_EX_DATA_UNINIT		-1
