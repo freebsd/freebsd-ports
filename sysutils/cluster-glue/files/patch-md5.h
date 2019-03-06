3e688ca0a4324d12998d2c00eba3a8c9bb646c10
Rene Ladan <rene@FreeBSD.org>
2019-03-06 10:16:17 +0000

    sysutils/cluster-glue: unbreak and modernize port
    - add USES=gnome
    - sort Makefile
    - disable HPI option because that port is currently broken
    
    PR:		231097
    Submitted by:	w.schwarzenfeld@utanet.at
    MFH:		2019Q1

---
+++ sysutils/cluster-glue/files/patch-md5.h
@@ -0,0 +1,11 @@
+--- include/clplumbing/md5.h.orig	2018-09-02 16:05:48 UTC
++++ include/clplumbing/md5.h
+@@ -21,7 +21,7 @@
+  */
+ 
+ #ifndef _MD5_H_
+-#define _MD5_H__
++#define _MD5_H_
+ 
+ /*
+  *      MD5:		The MD5 Message-Digest Algorithm ( RFC 1321 )
