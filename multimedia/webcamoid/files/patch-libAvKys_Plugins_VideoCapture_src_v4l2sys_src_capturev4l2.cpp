f459433d442a88786a0720ba7012247c191ad29f
Yuri Victorovich <yuri@FreeBSD.org>
2019-03-06 22:30:49 +0000

    multimedia/webcamoid: Update 8.1.0 -> 8.5.0
    Reported by:	portscout

--- multimedia/webcamoid/files/patch-libAvKys_Plugins_VideoCapture_src_v4l2sys_src_capturev4l2.cpp
+++ multimedia/webcamoid/files/patch-libAvKys_Plugins_VideoCapture_src_v4l2sys_src_capturev4l2.cpp
@@ -1,53 +1,51 @@
---- libAvKys/Plugins/VideoCapture/src/v4l2sys/src/capturev4l2.cpp.orig	2017-10-09 06:45:30 UTC
+--- libAvKys/Plugins/VideoCapture/src/v4l2sys/src/capturev4l2.cpp.orig	2019-03-06 17:26:33 UTC
 +++ libAvKys/Plugins/VideoCapture/src/v4l2sys/src/capturev4l2.cpp
-@@ -60,12 +60,15 @@ inline IoMethodMap initIoMethodMap()
- 
- Q_GLOBAL_STATIC_WITH_ARGS(IoMethodMap, ioMethodToStr, (initIoMethodMap()))
+@@ -162,10 +162,13 @@ class CaptureV4L2Private
+                             const QVariantMap &map2) const;
+ };
  
 +#define FREEBSD_BUG224011_VIDEO0
 +
  CaptureV4L2::CaptureV4L2(QObject *parent):
      Capture(parent)
  {
-     this->m_id = -1;
-     this->m_ioMethod = IoMethodUnknown;
-     this->m_nBuffers = 32;
+     this->d = new CaptureV4L2Private(this);
 +#if !defined(FREEBSD_BUG224011_VIDEO0)
-     this->m_fsWatcher = new QFileSystemWatcher({"/dev"}, this);
+     this->d->m_fsWatcher = new QFileSystemWatcher({"/dev"}, this);
  
-     QObject::connect(this->m_fsWatcher,
-@@ -76,13 +79,15 @@ CaptureV4L2::CaptureV4L2(QObject *parent
+     QObject::connect(this->d->m_fsWatcher,
+@@ -176,13 +179,16 @@ CaptureV4L2::CaptureV4L2(QObject *parent
                       &QFileSystemWatcher::fileChanged,
                       this,
                       &CaptureV4L2::onFileChanged);
--
 +#endif
+ 
      this->updateDevices();
  }
  
  CaptureV4L2::~CaptureV4L2()
  {
 +#if !defined(FREEBSD_BUG224011_VIDEO0)
-     delete this->m_fsWatcher;
+     delete this->d->m_fsWatcher;
 +#endif
+     delete this->d;
  }
  
- QStringList CaptureV4L2::webcams() const
-@@ -1176,13 +1181,16 @@ void CaptureV4L2::updateDevices()
-     this->m_cameraControls = cameraControls;
+@@ -1302,13 +1308,17 @@ void CaptureV4L2::updateDevices()
+     this->d->m_devicesCaps = devicesCaps;
  
-     if (this->m_devices != devices) {
+     if (this->d->m_devices != devices) {
 +#if !defined(FREEBSD_BUG224011_VIDEO0)
-         if (!this->m_devices.isEmpty())
-             this->m_fsWatcher->removePaths(this->m_devices);
--
-+#endif
-         this->m_devices = devices;
- 
-+#if !defined(FREEBSD_BUG224011_VIDEO0)
-         if (!this->m_devices.isEmpty())
-             this->m_fsWatcher->addPaths(this->m_devices);
+         if (!this->d->m_devices.isEmpty())
+             this->d->m_fsWatcher->removePaths(this->d->m_devices);
 +#endif
  
-         emit this->webcamsChanged(this->m_devices);
+         this->d->m_devices = devices;
+ 
++#if !defined(FREEBSD_BUG224011_VIDEO0)
+         if (!this->d->m_devices.isEmpty())
+             this->d->m_fsWatcher->addPaths(this->d->m_devices);
++#endif
+ 
+         emit this->webcamsChanged(this->d->m_devices);
      }
