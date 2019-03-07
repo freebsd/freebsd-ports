66ffc2ce737578ac97edcc416b7538ac791e2c32
Alexey Dokuchaev <danfe@FreeBSD.org>
2019-03-07 11:37:55 +0000

    - Drop PFX support code: incompatible with newer OpenSSL versions - Remove `ssh_fmt_plug.c' file (should've been part of r494696)
    Obtained from:	https://github.com/magnumripper/JohnTheRipper/commit/12e8d57

--- security/john/files/patch-john.c
+++ security/john/files/patch-john.c
@@ -1,22 +1,31 @@
 --- john.c.orig	2014-12-18 07:59:02 UTC
 +++ john.c
-@@ -153,7 +153,6 @@ extern int base64conv(int argc, char **argv);
+@@ -153,8 +153,6 @@ extern int base64conv(int argc, char **argv);
  extern int hccap2john(int argc, char **argv);
  extern int zip2john(int argc, char **argv);
  extern int gpg2john(int argc, char **argv);
 -extern int ssh2john(int argc, char **argv);
- extern int pfx2john(int argc, char **argv);
+-extern int pfx2john(int argc, char **argv);
  extern int keychain2john(int argc, char **argv);
  extern int kwallet2john(int argc, char **argv);
-@@ -1540,11 +1539,6 @@ int main(int argc, char **argv)
- 	if (!strcmp(name, "unique")) {
- 		CPU_detect_or_fallback(argv, 0);
+ extern int keepass2john(int argc, char **argv);
+@@ -1542,19 +1540,9 @@ int main(int argc, char **argv)
  		return unique(argc, argv);
--	}
--
+ 	}
+ 
 -	if (!strcmp(name, "ssh2john")) {
 -		CPU_detect_or_fallback(argv, 0);
 -		return ssh2john(argc, argv);
+-	}
+-
+ 	if (!strcmp(name, "putty2john")) {
+ 		CPU_detect_or_fallback(argv, 0);
+ 		return putty2john(argc, argv);
+-	}
+-
+-	if (!strcmp(name, "pfx2john")) {
+-		CPU_detect_or_fallback(argv, 0);
+-		return pfx2john(argc, argv);
  	}
  
- 	if (!strcmp(name, "putty2john")) {
+ 	if (!strcmp(name, "keychain2john")) {
