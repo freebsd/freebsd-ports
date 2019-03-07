8e61f6eadbb7cc0d703dbdb16b00150340bc5923
acm <acm@FreeBSD.org>
2019-03-07 06:09:27 +0000

    - Update to 1.14.0

--- devel/dub/files/patch-build.sh
+++ devel/dub/files/patch-build.sh
@@ -1,12 +1,12 @@
---- build.sh.orig	2018-05-01 13:43:29 UTC
-+++ build.sh
+--- build.sh.orig	2019-03-01 15:41:12.000000000 +0000
++++ build.sh	2019-03-06 05:49:55.312072000 +0000
 @@ -1,4 +1,4 @@
 -#!/usr/bin/env bash
 +#!/bin/sh
  set -e
  
  if [ "$DMD" = "" ]; then
-@@ -41,9 +41,6 @@ if [[ $VERSION < 2.069.0 ]]; then
+@@ -41,9 +41,6 @@
  	LIBS=`echo "$LIBS" | sed 's/^-L/-L-L/; s/ -L/ -L-L/g; s/^-l/-L-l/; s/ -l/ -L-l/g'`
  fi
  
@@ -16,12 +16,12 @@
  if [ "$GITVER" != "" ]; then
  	echo Generating version file...
  	echo "module dub.version_;" > source/dub/version_.d
-@@ -56,7 +53,7 @@ fi
+@@ -56,7 +53,7 @@
  MACOSX_DEPLOYMENT_TARGET=10.8
  
  echo Running $DMD...
--$DMD -ofbin/dub -g -O -w -version=DubUseCurl -Isource $* $LIBS @build-files.txt
-+$DMD -ofbin/dub -O -w -version=DubUseCurl -Isource $* $LIBS @build-files.txt
+-$DMD -ofbin/dub -g -O -w -version=DubUseCurl -version=DubApplication -Isource $* $LIBS @build-files.txt
++$DMD -ofbin/dub -O -w -version=DubUseCurl -version=DubApplication -Isource $* $LIBS @build-files.txt
  bin/dub --version
  echo DUB has been built as bin/dub.
  echo
