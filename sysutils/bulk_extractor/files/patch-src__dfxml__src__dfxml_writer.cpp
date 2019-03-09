9444597d5cca50eb95d8bbdb71f5df08acad6243
Tobias C. Berner <tcberner@FreeBSD.org>
2019-03-09 08:15:19 +0000

    sysutils/bulk_extractor: prepare for exiv2-0.27
    PR:		235943

--- sysutils/bulk_extractor/files/patch-src__dfxml__src__dfxml_writer.cpp
+++ sysutils/bulk_extractor/files/patch-src__dfxml__src__dfxml_writer.cpp
@@ -1,6 +1,14 @@
---- src/dfxml/src/dfxml_writer.cpp.orig	2014-01-18 09:18:03.000000000 +0900
-+++ src/dfxml/src/dfxml_writer.cpp	2014-01-18 09:18:26.000000000 +0900
-@@ -657,7 +657,7 @@
+--- src/dfxml/src/dfxml_writer.cpp.orig	2014-09-16 18:34:02 UTC
++++ src/dfxml/src/dfxml_writer.cpp
+@@ -646,6 +646,7 @@ void dfxml_writer::xmlout(const string &tag,const stri
+ #include <exiv2/image.hpp>
+ #include <exiv2/exif.hpp>
+ #include <exiv2/error.hpp>
++#include <exiv2/exiv2.hpp>
+ #endif
+ 
+ #ifdef HAVE_HASHDB
+@@ -653,7 +654,7 @@ void dfxml_writer::xmlout(const string &tag,const stri
  #endif
  
  #ifdef HAVE_AFFLIB_AFFLIB_H
