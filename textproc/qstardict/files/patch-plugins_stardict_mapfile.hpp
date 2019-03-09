bb85caa197a6d688d37e991fe748aa88e1cb4764
Tobias C. Berner <tcberner@FreeBSD.org>
2019-03-09 16:30:06 +0000

    textproc/qstardict: update to 1.3 (with Qt5)

--- textproc/qstardict/files/patch-plugins_stardict_mapfile.hpp
+++ textproc/qstardict/files/patch-plugins_stardict_mapfile.hpp
@@ -1,10 +1,10 @@
---- plugins/stardict/mapfile.hpp.orig	2011-12-05 12:40:49 UTC
+--- plugins/stardict/mapfile.hpp.orig	2019-03-09 16:13:21 UTC
 +++ plugins/stardict/mapfile.hpp
-@@ -9,6 +9,7 @@
- #  include <sys/types.h>
- #  include <fcntl.h>
- #  include <sys/mman.h>
-+#  include <unistd.h>
- #endif
- #ifdef _WIN32
+@@ -14,6 +14,7 @@
  #  include <windows.h>
+ #endif
+ #include <glib.h>
++#include <unistd.h>
+ 
+ class MapFile
+ {
