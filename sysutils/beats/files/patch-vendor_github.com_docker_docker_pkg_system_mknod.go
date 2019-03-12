1d822b795da44ff931bf7b03a43805f976fd4292
Ryan Steinmetz <zi@FreeBSD.org>
2019-03-12 17:06:08 +0000

    - Fix build with go 1.12

---
+++ sysutils/beats/files/patch-vendor_github.com_docker_docker_pkg_system_mknod.go
@@ -0,0 +1,11 @@
+--- vendor/github.com/docker/docker/pkg/system/mknod.go.orig	2019-03-12 15:33:33 UTC
++++ vendor/github.com/docker/docker/pkg/system/mknod.go
+@@ -9,7 +9,7 @@ import (
+ // Mknod creates a filesystem node (file, device special file or named pipe) named path
+ // with attributes specified by mode and dev.
+ func Mknod(path string, mode uint32, dev int) error {
+-	return syscall.Mknod(path, mode, dev)
++	return syscall.Mknod(path, mode, uint64(dev))
+ }
+ 
+ // Mkdev is used to build the value of linux devices (in /dev/) which specifies major
