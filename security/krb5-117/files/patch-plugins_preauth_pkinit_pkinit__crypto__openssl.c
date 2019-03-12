64bc6a15ce43a980d0aa41b616999479e8da9980
cy <cy@FreeBSD.org>
2019-03-12 04:18:32 +0000

    Fix build with LibreSSL 2.9.
    PR:		234064
    Submitted by:	Charlie Li <ml+freebsd@vishwin.info>

--- security/krb5-117/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.c
+++ security/krb5-117/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.c
@@ -1,6 +1,6 @@
---- plugins/preauth/pkinit/pkinit_crypto_openssl.c.orig	2018-05-03 07:34:47.000000000 -0700
-+++ plugins/preauth/pkinit/pkinit_crypto_openssl.c	2018-06-12 18:38:30.978823000 -0700
-@@ -188,7 +188,8 @@
+--- plugins/preauth/pkinit/pkinit_crypto_openssl.c.orig	2018-11-01 23:51:07 UTC
++++ plugins/preauth/pkinit/pkinit_crypto_openssl.c
+@@ -188,7 +188,8 @@ pkinit_pkcs11_code_to_text(int err);
      (*_x509_pp) = PKCS7_cert_from_signer_info(_p7,_si)
  #endif
  
@@ -10,13 +10,14 @@
  
  /* 1.1 standardizes constructor and destructor names, renaming
   * EVP_MD_CTX_{create,destroy} and deprecating ASN1_STRING_data. */
-@@ -3040,7 +3041,8 @@
+@@ -3040,7 +3041,9 @@ cleanup:
      return retval;
  }
  
 -#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if (defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x10100000L) && \
-+     !defined(LIBRESSL_VERSION_NUMBER)
++#if ((defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x10100000L) && \
++     !defined(LIBRESSL_VERSION_NUMBER)) || \
++     (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20900000L)
  
  /*
   * We need to decode DomainParameters from RFC 3279 section 2.3.3.  We would
--- security/krb5-devel/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.c
+++ security/krb5-devel/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.c
@@ -1,20 +1,23 @@
---- plugins/preauth/pkinit/pkinit_crypto_openssl.c.orig	2016-12-01 22:31:25 UTC
+--- plugins/preauth/pkinit/pkinit_crypto_openssl.c.orig	2018-11-01 23:51:07 UTC
 +++ plugins/preauth/pkinit/pkinit_crypto_openssl.c
-@@ -191,7 +191,7 @@ pkinit_pkcs11_code_to_text(int err);
+@@ -188,7 +188,8 @@ pkinit_pkcs11_code_to_text(int err);
      (*_x509_pp) = PKCS7_cert_from_signer_info(_p7,_si)
  #endif
  
 -#if OPENSSL_VERSION_NUMBER < 0x10100000L
-+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
++#if (defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER < 0x10100000L) || \
++     defined(LIBRESSL_VERSION_NUMBER)
  
  /* 1.1 standardizes constructor and destructor names, renaming
   * EVP_MD_CTX_{create,destroy} and deprecating ASN1_STRING_data. */
-@@ -3059,7 +3059,7 @@ cleanup:
+@@ -3040,7 +3041,9 @@ cleanup:
      return retval;
  }
  
 -#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
++#if ((defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x10100000L) && \
++     !defined(LIBRESSL_VERSION_NUMBER)) || \
++     (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20900000L)
  
  /*
   * We need to decode DomainParameters from RFC 3279 section 2.3.3.  We would
