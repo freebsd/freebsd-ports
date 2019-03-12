f892bd7ca12babc3415bafb454cf1e8a582b3a3e
Alan Somers <asomers@FreeBSD.org>
2019-03-12 20:51:49 +0000

    shells/fish: fix the build when fish is already installed
    https://github.com/fish-shell/fish-shell/issues/5735
    
    Submitted by:	Stephen <stephen@tungol.org>

--- shells/fish/files/patch-cmake_Install.cmake
+++ shells/fish/files/patch-cmake_Install.cmake
@@ -1,15 +1,28 @@
---- cmake/Install.cmake.orig	2018-12-29 16:18:12 UTC
+--- cmake/Install.cmake.orig	2019-02-19 13:39:17 UTC
 +++ cmake/Install.cmake
-@@ -113,7 +113,7 @@ INSTALL(FILES share/config.fish
+@@ -53,9 +53,7 @@ ENDIF()
+ # Define a function to help us create directories.
+ FUNCTION(FISH_CREATE_DIRS)
+   FOREACH(dir ${ARGV})
+-      IF(NOT EXISTS ${CMAKE_INSTALL_PREFIX}/${dir})
+-        INSTALL(DIRECTORY DESTINATION ${dir})
+-      ENDIF()
++    INSTALL(DIRECTORY DESTINATION ${dir})
+   ENDFOREACH(dir)
+ ENDFUNCTION(FISH_CREATE_DIRS)
+ 
+@@ -113,9 +111,9 @@ INSTALL(FILES share/config.fish
  # -$v $(INSTALL) -m 755 -d $(DESTDIR)$(extra_completionsdir)
  # -$v $(INSTALL) -m 755 -d $(DESTDIR)$(extra_functionsdir)
  # -$v $(INSTALL) -m 755 -d $(DESTDIR)$(extra_confdir)
 -FISH_CREATE_DIRS(${rel_datadir}/pkgconfig)
-+FISH_CREATE_DIRS(${rel_datadir}/../libdata/pkgconfig)
  # Don't try too hard to create these directories as they may be outside our writeable area
  # https://github.com/Homebrew/homebrew-core/pull/2813
++FISH_TRY_CREATE_DIRS(${rel_datadir}/../libdata/pkgconfig)
  FISH_TRY_CREATE_DIRS(${extra_completionsdir} ${extra_functionsdir} ${extra_confdir})
-@@ -132,7 +132,7 @@ ADD_CUSTOM_COMMAND(OUTPUT fish.pc
+ 
+ # @echo "Installing pkgconfig file"
+@@ -132,7 +130,7 @@ ADD_CUSTOM_COMMAND(OUTPUT fish.pc
  ADD_CUSTOM_TARGET(build_fish_pc ALL DEPENDS fish.pc)
  
  INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/fish.pc
