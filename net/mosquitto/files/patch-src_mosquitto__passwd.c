6df390e34d68335535ba5f80b20b34a38bb26f4d
Alexey Dokuchaev <danfe@FreeBSD.org>
2019-03-12 07:24:48 +0000

    - Update to version 1.5.8 and fix "null" username and password bug - Drop `textproc/libxslt' dependency -- only needed when building   from git sources, distfiles come with pre-generated manpages - Make `dns/c-ares' dependency optional, but keep it on by default - Add missing run-time dependency on `security/ca_root_nss' - Fix excessive use of capital letters in COMMENT, wrap some overly   long lines so they fit standard terminal width (80 characters)
    Reported by:	Jeremiah Gillis
    PR:		236459
    Submitted by:	Joseph Benden (maintainer)

--- net/mosquitto/files/patch-src_mosquitto__passwd.c
+++ net/mosquitto/files/patch-src_mosquitto__passwd.c
@@ -1,33 +1,20 @@
- lib/mosquitto_internal.h | 3 +++
- src/mosquitto_passwd.c   | 4 ++--
- src/security_default.c   | 2 +-
- 3 files changed, 6 insertions(+), 3 deletions(-)
-
---- src/mosquitto_passwd.c.orig	2018-10-11 00:27:05 UTC
+--- src/mosquitto_passwd.c.orig	2019-02-28 17:18:59 UTC
 +++ src/mosquitto_passwd.c
-@@ -17,6 +17,7 @@ Contributors:
- #include "config.h"
- 
- #include <errno.h>
-+#include <openssl/opensslv.h>
- #include <openssl/evp.h>
- #include <openssl/rand.h>
- #include <openssl/buffer.h>
-@@ -99,7 +100,7 @@ int output_new_password(FILE *fptr, const char *userna
+@@ -100,7 +100,7 @@ int output_new_password(FILE *fptr, const char *userna
  	unsigned char hash[EVP_MAX_MD_SIZE];
  	unsigned int hash_len;
  	const EVP_MD *digest;
 -#if OPENSSL_VERSION_NUMBER < 0x10100000L
-+#ifdef HAVE_OPENSSL_OPAQUE_STRUCTS 
++#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(HAVE_OPENSSL_OPAQUE_STRUCTS)
  	EVP_MD_CTX context;
  #else
  	EVP_MD_CTX *context;
-@@ -126,7 +127,7 @@ int output_new_password(FILE *fptr, const char *userna
+@@ -127,7 +127,7 @@ int output_new_password(FILE *fptr, const char *userna
  		return 1;
  	}
  
 -#if OPENSSL_VERSION_NUMBER < 0x10100000L
-+#if OPENSSL_VERSION_NUMBER < 0x10100000L 
++#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(HAVE_OPENSSL_OPAQUE_STRUCTS)
  	EVP_MD_CTX_init(&context);
  	EVP_DigestInit_ex(&context, digest, NULL);
  	EVP_DigestUpdate(&context, password, strlen(password));
