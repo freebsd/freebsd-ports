da6f8877990dd298ca9d59020a6620cc42a26498
Cy Schubert <cy@FreeBSD.org>
2019-03-12 04:18:32 +0000

    Fix build with LibreSSL 2.9.
    PR:		234064
    Submitted by:	Charlie Li <ml+freebsd@vishwin.info>

--- security/krb5-117/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.h
+++ security/krb5-117/files/patch-plugins_preauth_pkinit_pkinit__crypto__openssl.h
@@ -1,12 +1,13 @@
---- plugins/preauth/pkinit/pkinit_crypto_openssl.h.orig	2018-05-03 07:34:47.000000000 -0700
-+++ plugins/preauth/pkinit/pkinit_crypto_openssl.h	2018-06-12 18:35:23.603366000 -0700
-@@ -46,7 +46,8 @@
+--- plugins/preauth/pkinit/pkinit_crypto_openssl.h.orig	2018-11-01 23:51:07 UTC
++++ plugins/preauth/pkinit/pkinit_crypto_openssl.h
+@@ -46,7 +46,9 @@
  #include <openssl/asn1.h>
  #include <openssl/pem.h>
  
 -#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if (defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x10100000L) && \
-+     !defined(LIBRESSL_VERSION_NUMBER)
++#if ((defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x10100000L) && \
++     !defined(LIBRESSL_VERSION_NUMBER)) || \
++     (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20900000L)
  #include <openssl/asn1t.h>
  #else
  #include <openssl/asn1_mac.h>
