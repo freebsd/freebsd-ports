39a3574d5048925794011b117f75158816b0ea9c
Mathieu Arnold <mat@FreeBSD.org>
2019-03-14 21:13:01 +0000

    Put back a default non dumb pod formatter [1], and fix a probable bug in the mandoc formatter.
    PR:		236092 [1]
    Reported by:	Andrew Daugherity

--- lang/perl5.28/files/patch-cpan_Pod-Perldoc_lib_Pod_Perldoc_ToMan.pm
+++ lang/perl5.28/files/patch-cpan_Pod-Perldoc_lib_Pod_Perldoc_ToMan.pm
@@ -1,4 +1,7 @@
---- cpan/Pod-Perldoc/lib/Pod/Perldoc/ToMan.pm.orig	2017-04-03 07:28:44 UTC
+- Handle MANWIDTH=tty
+- Reset $? before opening mandoc so that the outputter works.
+
+--- cpan/Pod-Perldoc/lib/Pod/Perldoc/ToMan.pm.orig	2019-03-11 04:03:26 UTC
 +++ cpan/Pod-Perldoc/lib/Pod/Perldoc/ToMan.pm
 @@ -108,6 +108,8 @@ sub _get_columns_from_manwidth {
  
@@ -9,3 +12,13 @@
  	unless( $ENV{MANWIDTH} =~ m/\A\d+\z/ ) {
  		$self->warn( "Ignoring non-numeric MANWIDTH ($ENV{MANWIDTH})\n" );
  		return 0;
+@@ -309,6 +311,9 @@ sub _filter_through_nroff {
+ 	require Symbol;
+ 	require IPC::Open3;
+ 	require IO::Handle;
++
++	# Reset this???
++	$?=0;
+ 
+ 	my $pid = IPC::Open3::open3(
+ 		my $writer,
