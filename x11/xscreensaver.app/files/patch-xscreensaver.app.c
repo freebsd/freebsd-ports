285184c82d488255ecf6c51bc1da89ecf5357337
Kai Knoblich <kai@FreeBSD.org>
2019-03-14 20:53:31 +0000

    x11-wm/libdockapp: Update to 0.7.2
    While I'm here:
    
    * Change MASTER_SITES to upstream's new location
    * Add license and path to the license file
    * Sort variables a bit according to the PHB
    * Simplify pkg-plist substitution for the installation of example files
    
    Add some patches as well as just bumping the port revision for the following
    ports due changed location of the header files:
    
    * net/wmnetload
    * x11/xscreensaver.app
    
    Changelog:
    
    https://github.com/d-torrance/dockapps/blob/master/libdockapp/ChangeLog
    
    PR:		235923
    Approved by:	miwi (mentor), maintainer timeout
    Differential Revision:	https://reviews.freebsd.org/D19508

---
+++ x11/xscreensaver.app/files/patch-xscreensaver.app.c
@@ -0,0 +1,11 @@
+--- xscreensaver.app.c.orig	2003-05-30 01:36:59 UTC
++++ xscreensaver.app.c
+@@ -16,7 +16,7 @@
+  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  */
+ 
+-#include <dockapp.h>
++#include <libdockapp/dockapp.h>
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <ctype.h>
