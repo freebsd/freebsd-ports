63eb6efe2242bf83813f6e4f9062d9bfcd6086b4
Steve Wills <swills@FreeBSD.org>
2019-03-18 23:21:27 +0000

    audio/quimup: update to 1.4.4
    PR:		236437
    Submitted by:	Chris <cmangin@arobas.net> (maintainer)

--- audio/quimup/files/patch-src_qm__config.cpp
+++ audio/quimup/files/patch-src_qm__config.cpp
@@ -1,6 +1,6 @@
---- src/qm_config.cpp.orig	2016-02-28 09:52:29 UTC
+--- src/qm_config.cpp.orig	2018-11-28 18:19:33 UTC
 +++ src/qm_config.cpp
-@@ -168,9 +168,9 @@ void qm_config::load_config()
+@@ -172,9 +172,9 @@ void qm_config::load_config()
      version       	    = sts.value("version",			    "0.0" ).toString();  
      // server related values
      quitMPD_onquit    	= sts.value("quitMPD_onquit",		false ).toBool();
@@ -10,5 +10,5 @@
 -    onstart_mpd_command = sts.value("onstart_mpd_command",	"mpd"  ).toString();
 +    onstart_mpd_command = sts.value("onstart_mpd_command",	"musicpd"  ).toString();
      auto_connect     	= sts.value("auto_connect",			true   ).toBool();
+     start_minimized     = sts.value("start_minimized",		false   ).toBool();
      
-     profile    			= sts.value("profile",				0   ).toInt();
--- audio/quimup/files/patch-src_qm__mpdcom.cpp
+++ audio/quimup/files/patch-src_qm__mpdcom.cpp
@@ -1,6 +1,39 @@
---- src/qm_mpdcom.cpp.orig	2016-02-28 09:52:46 UTC
+--- src/qm_mpdcom.cpp.orig	2018-12-11 21:49:38 UTC
 +++ src/qm_mpdcom.cpp
-@@ -2500,17 +2500,10 @@ bool qm_mpdCom::is_mpd_running()
+@@ -367,16 +367,16 @@ QString qm_mpdCom::get_from_etcdefaultmpd()
+     QFile file;
+     QString line = "";
+ 
+-    file.setFileName("/etc/default/mpd");
++    file.setFileName("%%PREFIX%%/etc/musicpd.conf");
+     if ( !file.exists() )
+     {
+-        printf ("MPD's config : Tried /etc/default/mpd but it does not exist\n");
++        printf ("MPD's config : Tried %%PREFIX%%/etc/musicpd.conf but it does not exist\n");
+         return line;
+     }
+ 
+     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+     {
+-        printf ("MPD's config : Tried /etc/default/mpd but it is not readable\n");
++        printf ("MPD's config : Tried %%PREFIX%%/etc/musicpd.conf but it is not readable\n");
+         return line;
+     }
+ 
+@@ -393,10 +393,10 @@ QString qm_mpdCom::get_from_etcdefaultmpd()
+     file.close();
+ 
+     if (line.isEmpty())
+-        printf ("MPD's config : Tried /etc/default/mpd but MPDCONF is not set\n");
++        printf ("MPD's config : Tried %%PREFIX%%/etc/musicpd.conf but MPDCONF is not set\n");
+     else
+     {
+-        QString msg  = "MPD's config found in /etc/default/mpd : " + line + "\n";
++        QString msg  = "MPD's config found in %%PREFIX%%/etc/musicpd.conf : " + line + "\n";
+         printf ("%s", msg.toUtf8().constData());
+     }
+ 
+@@ -2710,17 +2710,10 @@ bool qm_mpdCom::is_mpd_running()
      struct stat sts;
      bool b_isrunning = false;
  
