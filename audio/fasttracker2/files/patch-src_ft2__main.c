2de3148744ab2a406af514eb5a221f32a12f1a80
Emanuel Haupt <ehaupt@FreeBSD.org>
2019-03-19 07:21:33 +0000

    Update to 0.1.b142

--- audio/fasttracker2/files/patch-src_ft2__main.c
+++ audio/fasttracker2/files/patch-src_ft2__main.c
@@ -1,18 +1,18 @@
---- src/ft2_main.c.orig	2018-12-18 11:08:11 UTC
+--- src/ft2_main.c.orig	2019-03-19 07:15:21 UTC
 +++ src/ft2_main.c
-@@ -190,6 +190,7 @@ int main(int argc, char *argv[])
+@@ -188,6 +188,7 @@ int main(int argc, char *argv[])
  
-     //benchmarkAudioChannelMixer(); /* for development testing */
+ 	//benchmarkAudioChannelMixer(); // for development testing
  
 +#ifndef __FreeBSD__
-     /* set up MIDI input (in a thread because it can take quite a while on f.ex. macOS) */
-     initMidiThread = SDL_CreateThread(initMidiFunc, "FT2 Clone MIDI Initialization Thread", NULL);
-     if (initMidiThread == NULL)
-@@ -199,6 +200,7 @@ int main(int argc, char *argv[])
-         return (1);
-     }
-     SDL_DetachThread(initMidiThread); /* don't wait for this thread, let it clean up when done */
+ 	// set up MIDI input (in a thread because it can take quite a while on f.ex. macOS)
+ 	initMidiThread = SDL_CreateThread(initMidiFunc, NULL, NULL);
+ 	if (initMidiThread == NULL)
+@@ -197,6 +198,7 @@ int main(int argc, char *argv[])
+ 		return (1);
+ 	}
+ 	SDL_DetachThread(initMidiThread); // don't wait for this thread, let it clean up when done
 +#endif
  
-     setupWaitVBL();
-     while (editor.programRunning)
+ 	SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
+ 
