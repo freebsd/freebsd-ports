445ef2cce0485409fccf1676e09071bad7194682
rakuco <rakuco@FreeBSD.org>
2019-03-19 22:41:33 +0000

    Reland r496041 and r496054.
    With r496296 we no longer have naming conflicts and can define the wayland
    component in qt.mk again.

--- Mk/Uses/qt.mk
+++ Mk/Uses/qt.mk
@@ -317,6 +317,9 @@ qt-uitools_PATH=	${LOCALBASE}/${QT_LIBDIR_REL}/libQt${_QT_LIBVER}UiTools.a
 qt-virtualkeyboard_PORT=	x11-toolkits/${_QT_RELNAME}-virtualkeyboard
 qt-virtualkeyboard_PATH=	${LOCALBASE}/${QT_PLUGINDIR_REL}/platforminputcontexts/libqtvirtualkeyboardplugin.so
 
+qt-wayland_PORT=	graphics/${_QT_RELNAME}-wayland
+qt-wayland_LIB=		libQt${_QT_LIBVER}WaylandClient.so
+
 qt-webchannel_PORT=	www/${_QT_RELNAME}-webchannel
 qt-webchannel_LIB=	libQt${_QT_LIBVER}WebChannel.so
 
