75fd8e4962ec6e78d3b61fbb052a263ed52cd3a0
brooks <brooks@FreeBSD.org>
2019-03-19 16:52:53 +0000

    Upgrade to a new LLVM 9-pre based snapshot.
     - Update LICENSE data for new project license
     - Switch from seperate git repos to the new mono-repo.

--- devel/llvm-cheri/files/patch-utils_llvm-build_llvmbuild_main.py
+++ devel/llvm-cheri/files/patch-utils_llvm-build_llvmbuild_main.py
@@ -1,5 +1,5 @@
---- utils/llvm-build/llvmbuild/main.py.orig	2015-07-20 23:51:12 UTC
-+++ utils/llvm-build/llvmbuild/main.py
+--- llvm/utils/llvm-build/llvmbuild/main.py.orig	2015-07-20 23:51:12 UTC
++++ llvm/utils/llvm-build/llvmbuild/main.py
 @@ -718,7 +718,17 @@ def add_magic_target_components(parser, 
  
      # We handle a few special cases of target names here for historical
