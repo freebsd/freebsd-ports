26b0a45e9c6fd6391a6bd9061520c3bcf8fc72cb
wen <wen@FreeBSD.org>
2019-03-19 13:50:38 +0000

    - Update to 3.5.1

--- devel/py-tables/files/patch-setup.py
+++ devel/py-tables/files/patch-setup.py
@@ -1,13 +1,14 @@
---- setup.py.orig	2017-04-12 12:02:34 UTC
+--- setup.py.orig	2019-03-19 12:02:31 UTC
 +++ setup.py
-@@ -115,8 +115,8 @@ with open('VERSION') as fd:
- debug = '--debug' in sys.argv
+@@ -124,8 +124,9 @@ if __name__ == '__main__':
+     debug = '--debug' in sys.argv
  
- # Global variables
--lib_dirs = []
--inc_dirs = [os.path.join('hdf5-blosc', 'src')]
-+lib_dirs = ['%%LOCALBASE%%/lib']
-+inc_dirs = [os.path.join('hdf5-blosc', 'src'),'%%LOCALBASE%%/include']
- optional_libs = []
- data_files = []    # list of data files to add to packages (mainly for DLL's)
+     # Global variables
+-    lib_dirs = []
+-    inc_dirs = [os.path.join('hdf5-blosc', 'src')]
++    lib_dirs = ['%%LOCALBASE%%/lib']
++    inc_dirs = [os.path.join('hdf5-blosc', 'src'),'%%LOCALBASE%%/include']
++
+     optional_libs = []
+     data_files = []    # list of data files to add to packages (mainly for DLL's)
  
