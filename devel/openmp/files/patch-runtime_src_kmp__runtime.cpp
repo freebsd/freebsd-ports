114b32c5a072cfd46882f788e6c65f259bdf8baa
Jan Beich <jbeich@FreeBSD.org>
2019-03-20 14:53:17 +0000

    devel/openmp: update to 8.0.0
    Changes:	http://llvm.org/viewvc/llvm-project/openmp/tags/RELEASE_800/final/?view=log
    ABI:		https://abi-laboratory.pro/tracker/timeline/llvm_openmp/

--- devel/openmp/files/patch-runtime_src_kmp__runtime.cpp
+++ devel/openmp/files/patch-runtime_src_kmp__runtime.cpp
@@ -1,11 +1,11 @@
---- runtime/src/kmp_runtime.cpp.orig	2017-07-17 09:03:14 UTC
+--- runtime/src/kmp_runtime.cpp.orig	2019-01-15 15:36:53 UTC
 +++ runtime/src/kmp_runtime.cpp
-@@ -7535,7 +7535,7 @@ __kmp_determine_reduction_method(
+@@ -8104,7 +8104,7 @@ __kmp_determine_reduction_method(
  
  #elif KMP_ARCH_X86 || KMP_ARCH_ARM || KMP_ARCH_AARCH || KMP_ARCH_MIPS
  
--#if KMP_OS_LINUX || KMP_OS_WINDOWS
-+#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_WINDOWS
+-#if KMP_OS_LINUX || KMP_OS_WINDOWS || KMP_OS_HURD
++#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_WINDOWS || KMP_OS_HURD
  
      // basic tuning
  
