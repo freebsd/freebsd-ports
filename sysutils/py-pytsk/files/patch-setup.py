49ea1c33363829db9e790c3a108888bf68370113
antoine <antoine@FreeBSD.org>
2019-03-21 12:40:48 +0000

    Update to 20190316

--- sysutils/py-pytsk/files/patch-setup.py
+++ sysutils/py-pytsk/files/patch-setup.py
@@ -1,8 +1,8 @@
 # Unbundle sleuthkit and talloc
 
---- setup.py.orig	2019-01-21 19:31:43 UTC
+--- setup.py.orig	2019-03-04 18:14:03 UTC
 +++ setup.py
-@@ -230,9 +230,9 @@ class BuildExtCommand(build_ext):
+@@ -232,9 +232,9 @@ class BuildExtCommand(build_ext):
    def run(self):
      compiler = new_compiler(compiler=self.compiler)
      # pylint: disable=attribute-defined-outside-init
@@ -14,7 +14,7 @@
  
      if not os.access("pytsk3.c", os.R_OK):
        # Generate the Python binding code (pytsk3.c).
-@@ -402,7 +402,7 @@ class ProjectBuilder(object):
+@@ -404,7 +404,7 @@ class ProjectBuilder(object):
      self._argv = argv
  
      # The path to the sleuthkit/tsk directory.
@@ -23,7 +23,7 @@
  
      # Paths under the sleuthkit/tsk directory which contain files we need
      # to compile.
-@@ -412,13 +412,13 @@ class ProjectBuilder(object):
+@@ -414,13 +414,13 @@ class ProjectBuilder(object):
      # The args for the extension builder.
      self.extension_args = {
          "define_macros": [],
