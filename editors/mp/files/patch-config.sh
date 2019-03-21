09002a14acd3126ed3155763be84e5ba4fe1c654
jhale <jhale@FreeBSD.org>
2019-03-21 23:53:16 +0000

    editors/mp: Update to 5.31
    - LICENSE was changed from GPLv2 to a Public Domain license
    - UI options have been reconfigured so that if the user installs a GUI
      (GTK2, GTK3, or QT5), mp-5 is also usable in the console via the
      NCURSES or ANSI options
    - mpsl and supporting data are no longer installed, but rather compiled into
      the mp-5 binary
    - Add pkg-message to explain the GUI color scheme change and how to revert
      to the previous color scheme if so desired

--- editors/mp/files/patch-config.sh
+++ editors/mp/files/patch-config.sh
@@ -1,121 +1,56 @@
---- config.sh.orig	2018-02-19 15:16:29 UTC
+--- config.sh.orig	2019-02-15 07:23:10 UTC
 +++ config.sh
-@@ -28,9 +28,11 @@ while [ $# -gt 0 ] ; do
+@@ -38,6 +38,7 @@ while [ $# -gt 0 ] ; do
      case $1 in
-     --without-curses)   WITHOUT_CURSES=1 ;;
-     --without-gtk)      WITHOUT_GTK=1 ;;
-+    --with-gtk=*)       GTKVER=`echo $1 | sed -e 's/--with-gtk=//'` ;;
-     --without-win32)    WITHOUT_WIN32=1 ;;
-     --with-kde4)        WITHOUT_KDE4=0 ;;
-     --without-qt4)      WITHOUT_QT4=1 ;;
-+    --without-qt5)      WITHOUT_QT5=1 ;;
-     --without-ansi)     WITHOUT_ANSI=1 ;;
-     --help)             CONFIG_HELP=1 ;;
- 
-@@ -59,9 +61,11 @@ if [ "$CONFIG_HELP" = "1" ] ; then
+     --without-curses)    WITHOUT_CURSES=1 ;;
+     --without-gtk)       WITHOUT_GTK=1 ;;
++    --with-gtk=*)        GTKVER=`echo $1 | sed -e 's/--with-gtk=//'` ;;
+     --without-win32)     WITHOUT_WIN32=1 ;;
+     --with-kde4)         WITHOUT_KDE4=0 ;;
+     --without-qt)        WITHOUT_QT5=1 && WITHOUT_QT4=1 ;;
+@@ -73,6 +74,7 @@ if [ "$CONFIG_HELP" = "1" ] ; then
      echo "--prefix=PREFIX       Installation prefix ($PREFIX)."
      echo "--without-curses      Disable curses (text) interface detection."
      echo "--without-gtk         Disable GTK interface detection."
 +    echo "--with-gtk=GTKVER     Enable GTK where GTKVER=(2|3)."
      echo "--without-win32       Disable win32 interface detection."
      echo "--with-kde4           Enable KDE4 interface detection."
-     echo "--without-qt4         Disable Qt4 interface detection."
-+    echo "--without-qt5         Disable Qt5 interface detection."
-     echo "--with-moc            Path to your QT4 moc. Ie: --with-moc=/usr/lib64/qt4/bin/moc"
-     echo "--without-ansi        Disable ANSI terminal interface detection."
-     echo "--without-unix-glob   Disable glob.h usage (use workaround)."
-@@ -223,36 +227,11 @@ echo -n "Testing for ncursesw... "
+     echo "--without-qt          Disable Qt interface detection."
+@@ -250,11 +252,11 @@ echo -n "Testing for ncursesw... "
  if [ "$WITHOUT_CURSES" = "1" ] ; then
      echo "Disabled"
  else
 -    echo "#include <ncursesw/ncurses.h>" > .tmp.c
--    echo "int main(void) { initscr(); endwin(); return 0; }" >> .tmp.c
--
--    TMP_CFLAGS="-I/usr/local/include -I/usr/include/ncurses -I/usr/include/ncursesw"
--    TMP_LDFLAGS="-L/usr/local/lib -lncursesw"
--
--    $CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
--    if [ $? = 0 ] ; then
--        echo "#define CONFOPT_CURSES 1" >> config.h
--        echo $TMP_CFLAGS >> config.cflags
--        echo $TMP_LDFLAGS >> config.ldflags
--        echo "OK (ncursesw)"
--        DRIVERS="ncursesw $DRIVERS"
--        DRV_OBJS="mpv_curses.o $DRV_OBJS"
--        WITHOUT_ANSI=1
--    else
--        echo "No"
--        WITHOUT_CURSES=1
--    fi
--fi
--
--if [ "$WITHOUT_CURSES" = "1" ] ; then
--    # test for curses / ncurses library
--    echo -n "Testing for recent ncurses... "
--
-     echo "#include <ncurses.h>" > .tmp.c
++    echo "#include <ncurses.h>" > .tmp.c
      echo "int main(void) { initscr(); endwin(); return 0; }" >> .tmp.c
  
--    TMP_CFLAGS="-I/usr/local/include"
+-    TMP_CFLAGS="-I/usr/local/include -I/usr/include/ncurses -I/usr/include/ncursesw"
 -    TMP_LDFLAGS="-L/usr/local/lib -lncursesw"
 +    TMP_CFLAGS="-I${NCURSESINC}"
 +    TMP_LDFLAGS="${LDFLAGS} -L${NCURSESLIB} -lncursesw"
  
      $CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
      if [ $? = 0 ] ; then
-@@ -384,19 +363,22 @@ if [ "$WITHOUT_QT5" = "1" ] ; then
+@@ -384,14 +386,14 @@ if [ "$WITHOUT_QT5" = "1" ] ; then
  else
      if which pkg-config > /dev/null 2>&1
      then
--        if which moc-qt5 > /dev/null 2>&1
--        then
--            MOC=moc-qt5
--            echo "MOC=$MOC" >> makefile.opts
-+	if [ ! $QT4MOC ]; then
-+		MOC="moc"
-+	else
-+        	MOC="$QT4MOC"
-+	fi
-+        which moc-qt5 > /dev/null 2>&1 && MOC=moc-qt5
-+        echo "MOC=$MOC" >> makefile.opts
+-        TMP_CFLAGS="`sh -c 'pkg-config --cflags Qt5Widgets' 2>/dev/null` -fPIC"
+-        TMP_LDFLAGS="`sh -c 'pkg-config --libs Qt5Widgets' 2>/dev/null`"
++        TMP_CFLAGS="`sh -c 'pkg-config --cflags Qt5Widgets gl' 2>/dev/null` -fPIC"
++        TMP_LDFLAGS="`sh -c 'pkg-config --libs Qt5Widgets gl' 2>/dev/null`"
  
--            TMP_CFLAGS="$(pkg-config --cflags Qt5Widgets) -fPIC"
--            TMP_LDFLAGS=$(pkg-config --libs Qt5Widgets)
-+            TMP_CFLAGS="$(pkg-config --cflags Qt5Widgets) $(pkg-config --cflags gl) -fPIC"
-+            TMP_LDFLAGS="$(pkg-config --libs Qt5Widgets) $(pkg-config --libs gl)"
- 
-             echo "#include <QtWidgets>" > .tmp.cpp
-             echo "int main(int argc, char *argv[]) { new QApplication(argc, argv) ; return 0; } " >> .tmp.cpp
-  
--            echo "$CPP $TMP_CFLAGS .tmp.cpp $TMP_LDFLAGS -o .tmp.o" >> .config.log
--            $CPP $TMP_CFLAGS .tmp.cpp $TMP_LDFLAGS -o .tmp.o 2>> .config.log
-+            echo "$CPP $CXXFLAGS $TMP_CFLAGS .tmp.cpp $TMP_LDFLAGS -o .tmp.o" >> .config.log
-+            $CPP $CXXFLAGS $TMP_CFLAGS .tmp.cpp $TMP_LDFLAGS -o .tmp.o 2>> .config.log
- 
-             if [ $? = 0 ] ; then
-                 echo $TMP_CFLAGS >> config.cflags
-@@ -416,9 +398,6 @@ else
-             else
-                 echo "No"
-             fi
--        else
--            echo "No"
--        fi
-     else
-         echo "No"
-     fi
-@@ -441,8 +420,8 @@ else
-         which moc-qt4 > /dev/null 2>&1 && MOC=moc-qt4
-         echo "MOC=$MOC" >> makefile.opts
- 
--        TMP_CFLAGS=$(pkg-config --cflags QtGui)
--        TMP_LDFLAGS="$(pkg-config --libs QtGui) -lX11"
-+        TMP_CFLAGS="$(pkg-config --cflags QtGui) $(pkg-config --cflags x11)"
-+        TMP_LDFLAGS="$(pkg-config --libs QtGui) $(pkg-config --libs x11)"
- 
-         echo "#include <QtGui>" > .tmp.cpp
+         echo "#include <QtWidgets>" > .tmp.cpp
          echo "int main(int argc, char *argv[]) { new QApplication(argc, argv) ; return 0; } " >> .tmp.cpp
-@@ -483,15 +462,15 @@ else
+  
+-        echo "$CPP $TMP_CFLAGS .tmp.cpp $TMP_LDFLAGS -o .tmp.o" >> .config.log
+-        $CPP $TMP_CFLAGS .tmp.cpp $TMP_LDFLAGS -o .tmp.o 2>> .config.log
++        echo "$CPP $CXXFLAGS $TMP_CFLAGS .tmp.cpp $TMP_LDFLAGS -o .tmp.o" >> .config.log
++        $CPP $CXXFLAGS $TMP_CFLAGS .tmp.cpp $TMP_LDFLAGS -o .tmp.o 2>> .config.log
+ 
+         if [ $? = 0 ] ; then
+             echo $TMP_CFLAGS >> config.cflags
+@@ -475,15 +477,15 @@ else
      echo "int main(void) { gtk_main(); return 0; } " >> .tmp.c
  
      # Try first GTK 3.0
--- editors/mp/files/patch-mpdm_config.sh
+++ editors/mp/files/patch-mpdm_config.sh
@@ -1,49 +1,49 @@
---- mpdm/config.sh.orig	2015-05-05 07:29:54 UTC
+--- mpdm/config.sh.orig	2019-02-11 11:53:55 UTC
 +++ mpdm/config.sh
-@@ -191,8 +191,8 @@ echo -n "Testing for regular expressions... "
+@@ -194,8 +194,8 @@ echo -n "Testing for regular expressions... "
  
  if [ "$WITH_PCRE" = 1 ] ; then
- 	# try first the pcre library
--	TMP_CFLAGS="-I/usr/local/include"
--	TMP_LDFLAGS="-L/usr/local/lib -lpcre -lpcreposix"
-+	TMP_CFLAGS="-I$LOCALBASE/include"
-+	TMP_LDFLAGS="-L$LOCALBASE/lib -lpcre -lpcreposix"
- 	echo "#include <pcreposix.h>" > .tmp.c
- 	echo "int main(void) { regex_t r; regmatch_t m; regcomp(&r,\".*\",REG_EXTENDED|REG_ICASE); return 0; }" >> .tmp.c
+     # try first the pcre library
+-    TMP_CFLAGS="-I/usr/local/include"
+-    TMP_LDFLAGS="-L/usr/local/lib -lpcre -lpcreposix"
++    TMP_CFLAGS="-I$LOCALBASE/include"
++    TMP_LDFLAGS="-L$LOCALBASE/lib -lpcre -lpcreposix"
+     echo "#include <pcreposix.h>" > .tmp.c
+     echo "int main(void) { regex_t r; regmatch_t m; regcomp(&r,\".*\",REG_EXTENDED|REG_ICASE); return 0; }" >> .tmp.c
  
-@@ -370,13 +370,15 @@ else
- 		echo "#define CONFOPT_GETTEXT 1" >> config.h
- 	else
- 		# try now with -lintl
--		TMP_LDFLAGS="-lintl"
-+		TMP_CFLAGS="-I$LOCALBASE/include"
-+		TMP_LDFLAGS="-L$LOCALBASE/lib -lintl"
+@@ -373,13 +373,15 @@ else
+         echo "#define CONFOPT_GETTEXT 1" >> config.h
+     else
+         # try now with -lintl
+-        TMP_LDFLAGS="-lintl"
++        TMP_CFLAGS="-I$LOCALBASE/include"
++        TMP_LDFLAGS="-L$LOCALBASE/lib -lintl"
  
--		$CC .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
-+		$CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
+-        $CC .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
++        $CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
  
- 		if [ $? = 0 ] ; then
- 			echo "OK (libintl needed)"
- 			echo "#define CONFOPT_GETTEXT 1" >> config.h
-+			echo "$TMP_CFLAGS" >> config.cflags
- 			echo "$TMP_LDFLAGS" >> config.ldflags
- 		else
- 			echo "No"
-@@ -403,13 +405,15 @@ else
- 		echo "#define CONFOPT_ICONV 1" >> config.h
- 	else
- 		# try now with -liconv
--		TMP_LDFLAGS="-liconv"
-+		TMP_CFLAGS="-I$ICONV_PREFIX/include"
-+		TMP_LDFLAGS="-L$ICONV_PREFIX/lib $ICONV_LIB"
+         if [ $? = 0 ] ; then
+             echo "OK (libintl needed)"
+             echo "#define CONFOPT_GETTEXT 1" >> config.h
++            echo "$TMP_CFLAGS" >> config.cflags
+             echo "$TMP_LDFLAGS" >> config.ldflags
+         else
+             echo "No"
+@@ -406,13 +408,15 @@ else
+         echo "#define CONFOPT_ICONV 1" >> config.h
+     else
+         # try now with -liconv
+-        TMP_LDFLAGS="-liconv"
++        TMP_CFLAGS="-I$ICONV_PREFIX/include"
++        TMP_LDFLAGS="-L$ICONV_PREFIX/lib $ICONV_LIB"
  
--		$CC .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
-+		$CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
+-        $CC .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
++        $CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
  
- 		if [ $? = 0 ] ; then
- 			echo "OK (libiconv needed)"
- 			echo "#define CONFOPT_ICONV 1" >> config.h
-+			echo "$TMP_CFLAGS" >> config.cflags
- 			echo "$TMP_LDFLAGS" >> config.ldflags
- 		else
- 			echo "No"
+         if [ $? = 0 ] ; then
+             echo "OK (libiconv needed)"
+             echo "#define CONFOPT_ICONV 1" >> config.h
++            echo "$TMP_CFLAGS" >> config.cflags
+             echo "$TMP_LDFLAGS" >> config.ldflags
+         else
+             echo "No"
