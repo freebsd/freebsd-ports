ad828a3f04984e974818d76cc7e27ee7df2297be
Jason E. Hale <jhale@FreeBSD.org>
2019-03-22 19:04:31 +0000

    finance/libofx: Update to 0.9.14

--- finance/libofx/files/patch-lib_ofx__utilities.cpp
+++ finance/libofx/files/patch-lib_ofx__utilities.cpp
@@ -1,14 +1,24 @@
---- lib/ofx_utilities.cpp.orig	2013-04-16 19:50:51 UTC
+Emulate the behavior of the USG UXIX daylight variable implemented in glibc.
+It is supposed to be 0 if the time zone does not have any daylight saving time
+rules and non-zero if there is a time during the year when daylight saving
+time applies. [1]
+
+In FreeBSD, tzname[1] should be set to "   " (three spaces) if DST is never
+observed. [2]
+
+[1] https://www.gnu.org/software/libc/manual/html_node/Time-Zone-Functions.html#Time-Zone-Functions
+[2] https://svnweb.freebsd.org/base/head/contrib/tzcode/stdtime/localtime.c?revision=313774&view=markup#l84
+
+--- lib/ofx_utilities.cpp.orig	2018-05-02 19:39:38 UTC
 +++ lib/ofx_utilities.cpp
-@@ -115,6 +115,11 @@ time_t ofxdate_to_time_t(const string ofxdate)
-   char time_zone_specified = false;
-   string ofxdate_whole;
-   time_t temptime;
-+  int daylight = 0;
-+
-+  tzset();
-+  if(tzname[1][0] != ' ')
-+    daylight = 1;
- 
-   time.tm_isdst = daylight; // initialize dst setting
+@@ -142,6 +142,10 @@ time_t ofxdate_to_time_t(const string ofxdate)
    std::time(&temptime);
+   local_offset = difftime(mktime(localtime(&temptime)), mktime(gmtime(&temptime)));
+   /* daylight is set to 1 if the timezone indicated by the environment (either TZ or /etc/localtime) uses daylight savings time (aka summer time). We use it here to provisionally set tm_isdst. */
++#if defined(__FreeBSD__)
++  tzset();
++  int daylight = ((tzname[1][0] == ' ') ? 0 : 1);
++#endif
+   time.tm_isdst = daylight;
+ 
+   if (ofxdate.size() != 0)
