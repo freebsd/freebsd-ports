c2e272a602861c672a9487fbdedcf14188bfb991
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2019-03-24 18:22:19 +0000

    graphics/ipe: Update version 7.2.8=>7.2.11

--- graphics/ipe/files/patch-ipelib_ipeplatform.cpp
+++ graphics/ipe/files/patch-ipelib_ipeplatform.cpp
@@ -1,12 +1,13 @@
---- ipelib/ipeplatform.cpp.orig	2019-01-12 09:37:35 UTC
+--- ipelib/ipeplatform.cpp.orig	2019-03-09 12:21:05 UTC
 +++ ipelib/ipeplatform.cpp
-@@ -44,7 +44,9 @@
- #include <mach-o/dyld.h>
- #include <xlocale.h>
+@@ -48,7 +48,10 @@
+ #include <sys/param.h>
+ #include <sys/errno.h>
  #endif
 -
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
 +#include <xlocale.h>
++#include <sys/errno.h>
 +#endif
  #include <cstdlib>
  #include <sys/types.h>
