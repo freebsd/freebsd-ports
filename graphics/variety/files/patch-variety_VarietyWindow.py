55164eddff9fc04c704d0d748be47690971bf629
kwm <kwm@FreeBSD.org>
2019-03-24 20:34:22 +0000

    Update variety to 0.7.1 and unbreak.
    PR:		236180 (based on)
    Submitted by:	Hiroki Tagato <tagattie@yandex.com>
    Approved by:	maintainer timeout (3 weeks)

--- graphics/variety/files/patch-variety_VarietyWindow.py
+++ graphics/variety/files/patch-variety_VarietyWindow.py
@@ -1,11 +1,11 @@
---- variety/VarietyWindow.py.orig	2015-09-06 14:49:07 UTC
+--- variety/VarietyWindow.py.orig	2018-10-23 15:56:25 UTC
 +++ variety/VarietyWindow.py
-@@ -2339,7 +2339,7 @@ To set a specific wallpaper: %prog /some
+@@ -2351,7 +2351,7 @@ To set a specific wallpaper: %prog /some/local/image.j
                  ("auto" if refresh_level == VarietyWindow.RefreshLevel.ALL else "refresh")
              logger.debug(lambda: "Running set_wallpaper script with parameters: %s, %s, %s" % (wallpaper, auto, original_file))
              try:
 -                subprocess.check_call(["timeout", "--kill-after=5", "10", script, wallpaper, auto, original_file])
 +                subprocess.check_call(["gtimeout", "--kill-after=5", "10", script, wallpaper, auto, original_file])
-             except subprocess.CalledProcessError, e:
+             except subprocess.CalledProcessError as e:
                  if e.returncode == 124:
                      logger.error(lambda: "Timeout while running set_wallpaper script, killed")
