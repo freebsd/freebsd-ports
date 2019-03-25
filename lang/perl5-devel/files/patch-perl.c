9279e221ce94ee59c21a449def86fecc3f74f936
Mathieu Arnold <mat@FreeBSD.org>
2019-03-25 09:56:49 +0000

    Update to v5.29.9-19-g25d7b7aa37.

--- lang/perl5-devel/files/patch-perl.c
+++ lang/perl5-devel/files/patch-perl.c
@@ -1,6 +1,6 @@
---- perl.c.orig	2019-02-18 07:36:46 UTC
+--- perl.c.orig	2019-03-25 03:23:51 UTC
 +++ perl.c
-@@ -1985,23 +1985,7 @@ S_Internals_V(pTHX_ CV *cv)
+@@ -1990,23 +1990,7 @@ S_Internals_V(pTHX_ CV *cv)
      PUSHs(Perl_newSVpvn_flags(aTHX_ non_bincompat_options,
  			      sizeof(non_bincompat_options) - 1, SVs_TEMP));
  
