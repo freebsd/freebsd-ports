03f48ac87a6a0144520598d10c69dc68af0d6daf
meta <meta@FreeBSD.org>
2019-03-26 05:31:27 +0000

    net/tightvnc: Switch to options helpers, etc
    - Switch to options helpers
    - Regenerate patches
    - Adjust CONFLICTS since some conflicting ports removed from ports tree
    - Pet portlint (bring USES earlier)
    - Add missing USES,USE_XORG
    - Bump PORTREVISION due to USES change
    
    Sponsored by:	HAW International, Inc.

--- net/tightvnc/files/patch-vncviewer__rfbproto.c
+++ net/tightvnc/files/patch-vncviewer__rfbproto.c
@@ -1,6 +1,6 @@
---- vncviewer/rfbproto.c~	2006-12-08 18:20:03.000000000 +1100
-+++ vncviewer/rfbproto.c	2013-09-29 09:19:34.782746129 +1000
-@@ -569,7 +569,7 @@
+--- vncviewer/rfbproto.c.orig	2006-12-08 07:20:03 UTC
++++ vncviewer/rfbproto.c
+@@ -569,7 +569,7 @@ AuthenticateVNC(void)
      passwd = getpass("Password: ");
    }
  
@@ -9,4 +9,3 @@
      fprintf(stderr, "Reading password failed\n");
      return False;
    }
-
