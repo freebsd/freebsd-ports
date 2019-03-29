6643d16017b6be0979108d40de3b49fdafad9a0e
Mark Linimon <linimon@FreeBSD.org>
2019-03-29 16:08:54 +0000

    lib/inc/internal/util/freebsd/geom.hpp needs to include stdexcept to build on GCC-based architectures.
    PR:		235601
    Submitted by:	Piotr Kubaj
    Approved by:	portmgr (tier-2 blanket)

---
+++ sysutils/facter/files/patch-lib_inc_internal_util_freebsd_geom.hpp
@@ -0,0 +1,9 @@
+--- lib/inc/internal/util/freebsd/geom.hpp.orig	2019-02-08 14:24:43 UTC
++++ lib/inc/internal/util/freebsd/geom.hpp
+@@ -4,6 +4,7 @@
+  */
+ #pragma once
+ 
++#include <stdexcept>
+ #include <string>
+ #include <vector>
