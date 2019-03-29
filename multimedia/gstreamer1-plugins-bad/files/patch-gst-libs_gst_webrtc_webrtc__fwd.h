182347b3c6fbe27b5518f4b3b33f3b20d8987148
Mark Linimon <linimon@FreeBSD.org>
2019-03-29 17:30:07 +0000

    Fix the following two build errors on GCC-based architectures, similar to other previous gstreamer1-plugins-* commits:
      rtpreceiver.h:38: error: redefinition of typedef 'GstWebRTCRTPReceiver'
      rtpreceiver.h:39: error: redefinition of typedef 'GstWebRTCRTPReceiverClass'
    
    Tested on both amd64 and powerpc64.
    
    Approved by:	portmgr (tier-2 blanket)

---
+++ multimedia/gstreamer1-plugins-bad/files/patch-gst-libs_gst_webrtc_webrtc__fwd.h
@@ -0,0 +1,12 @@
+--- gst-libs/gst/webrtc/webrtc_fwd.h.orig	2018-07-18 14:46:42 UTC
++++ gst-libs/gst/webrtc/webrtc_fwd.h
+@@ -39,9 +39,6 @@ typedef struct _GstWebRTCDTLSTransportCl
+ typedef struct _GstWebRTCICETransport GstWebRTCICETransport;
+ typedef struct _GstWebRTCICETransportClass GstWebRTCICETransportClass;
+ 
+-typedef struct _GstWebRTCRTPReceiver GstWebRTCRTPReceiver;
+-typedef struct _GstWebRTCRTPReceiverClass GstWebRTCRTPReceiverClass;
+-
+ typedef struct _GstWebRTCRTPSender GstWebRTCRTPSender;
+ typedef struct _GstWebRTCRTPSenderClass GstWebRTCRTPSenderClass;
+ 
