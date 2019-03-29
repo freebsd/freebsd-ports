c0fb366e5d7b3764657f609a01cdfdba263584b0
linimon <linimon@FreeBSD.org>
2019-03-29 17:23:44 +0000

    Fix build on GCC-based architectures, similar to other previous gstreamer1-plugins-* commits:
      ./gstv4l2allocator.h:53: error: redefinition of typedef 'GstV4l2Object'
    
    Tested on both amd64 and powerpc64.
    
    Approved by:	portmgr (tier-2 blanket)

---
+++ multimedia/gstreamer1-plugins-v4l2/files/patch-sys_v4l2_gstv4l2allocator.h
@@ -0,0 +1,10 @@
+--- sys/v4l2/gstv4l2allocator.h.orig	2018-03-23 20:44:48 UTC
++++ sys/v4l2/gstv4l2allocator.h
+@@ -50,7 +50,6 @@ typedef struct _GstV4l2MemoryGroup GstV4
+ typedef struct _GstV4l2Memory GstV4l2Memory;
+ typedef enum _GstV4l2Capabilities GstV4l2Capabilities;
+ typedef enum _GstV4l2Return GstV4l2Return;
+-typedef struct _GstV4l2Object GstV4l2Object;
+ 
+ enum _GstV4l2AllocatorFlags
+ {
