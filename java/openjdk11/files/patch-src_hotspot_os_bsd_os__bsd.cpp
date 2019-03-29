c067269eed43729d60d5492b41bbb35b50c2b5b5
glewis <glewis@FreeBSD.org>
2019-03-29 01:05:26 +0000

    . Update to 11.0.2.9.4 . Sort LIB_DEPENDS and USE_XORG . Add a dependency in alsa-lib now that sound is supported on FreeBSD. . Don't disable dtrace now that it is supported on FreeBSD. . Less intrusive iconv change.

--- java/openjdk11/files/patch-src_hotspot_os_bsd_os__bsd.cpp
+++ java/openjdk11/files/patch-src_hotspot_os_bsd_os__bsd.cpp
@@ -1,13 +1,10 @@
---- src/hotspot/os/bsd/os_bsd.cpp
-+++ src/hotspot/os/bsd/os_bsd.cpp
-@@ -377,8 +377,10 @@ void os::init_system_properties_values() {
- #ifndef DEFAULT_LIBPATH
- #ifdef __APPLE__
-   #define DEFAULT_LIBPATH "/lib:/usr/lib"
-+#elif defined(__NetBSD__)
-+  #define DEFAULT_LIBPATH "/usr/lib:/usr/pkg/lib"
+--- src/hotspot/os/bsd/os_bsd.cpp.orig	2019-03-27 13:28:54.000000000 -0700
++++ src/hotspot/os/bsd/os_bsd.cpp	2019-03-28 16:46:31.000797000 -0700
+@@ -380,7 +380,7 @@
+ #elif defined(__NetBSD__)
+   #define DEFAULT_LIBPATH "/usr/lib:/usr/pkg/lib"
  #else
--  #define DEFAULT_LIBPATH "/usr/lib:" PACKAGE_PATH "/lib"
+-  #define DEFAULT_LIBPATH "/usr/lib:/usr/local/lib"
 +  #define DEFAULT_LIBPATH "/usr/lib:%%LOCALBASE%%/lib"
  #endif
  #endif
