6396901a4f485cb77318584c672985a6c961e4c3
kai <kai@FreeBSD.org>
2019-03-29 16:36:45 +0000

    games/anki: Unbreak
    Unbreak since www/py-qt5-webengine is usable again since r497003.
    
    Also update the patch for aqt/qt.py to load PyQt5.QtWebChannel explicitly.
    Otherwise Anki will crash upon invocation.
    
    PR:		233192
    Approved by:	mentors (implicit)

--- games/anki/files/patch-aqt_qt.py
+++ games/anki/files/patch-aqt_qt.py
@@ -1,6 +1,14 @@
 --- aqt/qt.py.orig	2019-03-09 22:22:42 UTC
 +++ aqt/qt.py
-@@ -42,7 +42,7 @@ qtmajor = (QT_VERSION & 0xff0000) >> 16
+@@ -14,6 +14,7 @@ from anki.utils import isWin, isMac
+ from PyQt5.Qt import *
+ # trigger explicit message in case of missing libraries
+ # instead of silently failing to import
++from PyQt5.QtWebChannel import *
+ from PyQt5.QtWebEngineWidgets import *
+ try:
+     from PyQt5 import sip
+@@ -42,7 +43,7 @@ qtmajor = (QT_VERSION & 0xff0000) >> 16
  qtminor = (QT_VERSION & 0x00ff00) >> 8
  qtpoint = QT_VERSION & 0xff
  
