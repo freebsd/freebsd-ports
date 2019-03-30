b848b67995ebb6bc7dd73693783bca6b008bb556
cy <cy@FreeBSD.org>
2019-03-30 18:36:47 +0000

    Fix rc script: remove extraneous &&.

--- net/pipsecd/files/patch-pipsecd.sh
+++ net/pipsecd/files/patch-pipsecd.sh
@@ -2,4 +2,4 @@
 +++ pipsecd.sh	Thu Oct 14 13:00:38 1999
 @@ -0,0 +1,2 @@
 +#!/bin/sh
-+cd /usr/local; [ -x sbin/pipsecd -a -f etc/ipsec/pipsecd.conf ] && sbin/pipsecd & && echo -n ' pipsecd'
++cd /usr/local; [ -x sbin/pipsecd -a -f etc/ipsec/pipsecd.conf ] && sbin/pipsecd & echo -n ' pipsecd'
