ad40a5554b408a58e643b6a54f6ff84652cd53d2
ohauer <ohauer@FreeBSD.org>
2019-03-31 16:09:51 +0000

    - update to 3.4-20190106

--- mail/postfix-current/files/patch-src_tls_tls__server.c
+++ mail/postfix-current/files/patch-src_tls_tls__server.c
@@ -1,38 +1,12 @@
---- src/tls/tls_server.c.orig	2018-11-17 14:59:22 UTC
+--- src/tls/tls_server.c.orig	2018-12-26 19:21:49 UTC
 +++ src/tls/tls_server.c
-@@ -174,7 +174,7 @@ static const char server_session_id_cont
- #endif					/* OPENSSL_VERSION_NUMBER */
- 
-  /* OpenSSL 1.1.0 bitrot */
--#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
- typedef const unsigned char *session_id_t;
- 
- #else
-@@ -377,7 +377,7 @@ TLS_APPL_STATE *tls_server_init(const TL
-      */
-     tls_check_version();
- 
--#if OPENSSL_VERSION_NUMBER < 0x10100000L
-+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
- 
-     /*
-      * Initialize the OpenSSL library by the book! To start with, we must
-@@ -486,7 +486,7 @@ TLS_APPL_STATE *tls_server_init(const TL
-      * OpenSSL 0.9.8h, while SSL_NO_TICKET was added in 0.9.8f.
-      */
- #ifdef SSL_OP_NO_TICKET
--#if !defined(OPENSSL_NO_TLSEXT) && OPENSSL_VERSION_NUMBER >= 0x0090808fL
-+#if !defined(OPENSSL_NO_TLSEXT) && OPENSSL_VERSION_NUMBER >= 0x0090808fL && !defined(LIBRESSL_VERSION_NUMBER)
-     ticketable = (*var_tls_tkt_cipher && scache_timeout > 0
- 		  && !(off & SSL_OP_NO_TICKET));
-     if (ticketable) {
-@@ -595,7 +595,7 @@ TLS_APPL_STATE *tls_server_init(const TL
-     /*
-      * 2015-12-05: Ephemeral RSA removed from OpenSSL 1.1.0-dev
-      */
--#if OPENSSL_VERSION_NUMBER < 0x10100000L
-+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
- 
-     /*
-      * According to OpenSSL documentation, a temporary RSA key is needed when
+@@ -518,7 +518,9 @@ TLS_APPL_STATE *tls_server_init(const TL
+ 	 * ticket decryption callback already (since 2.11) asks OpenSSL to
+ 	 * avoid issuing new tickets when the presented ticket is re-usable.
+ 	 */
++#ifndef LIBRESSL_VERSION_NUMBER
+ 	SSL_CTX_set_num_tickets(server_ctx, 1);
++#endif
+     }
+ #endif
+     if (!ticketable)
