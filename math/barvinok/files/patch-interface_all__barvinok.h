e6e6971bfca537a59e1ab33b3c64382eb58f1ae9
yuri <yuri@FreeBSD.org>
2019-03-31 16:47:12 +0000

    math/barvinok: Update 0.41.1 -> 0.41.2
    Reported by:	portscout

--- math/barvinok/files/patch-interface_all__barvinok.h
+++
@@ -1,6 +0,0 @@
---- interface/all_barvinok.h.orig	2018-12-07 04:15:52 UTC
-+++ interface/all_barvinok.h
-@@ -1,2 +1,2 @@
--#include <interface/all.h>
-+//#include <interface/all.h>
- #include <barvinok/isl.h>
