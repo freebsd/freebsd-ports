7465ded192c3a7ee8020121761e56a2b44435ef4
ohauer <ohauer@FreeBSD.org>
2019-03-31 13:08:05 +0000

    - update to postfix-3.3.4
    Changelog:
    20190312
      Bugfix (introduced: Postfix 2.2): reject_multi_recipient_bounce
      has been producing false rejects starting with the Postfix
      2.2 smtpd_end_of_data_restrictons, and for the same reasons,
      did the same with the Postfix 3.4 BDAT command. The latter
      was reported by Andreas Schulze. File: smtpd/smtpd_check.c.
    
    20190328
      Bugfix (introduced: Postfix 3.0): LMTP connections over
      UNIX-domain sockets were cached but not reused, due to a
      cache lookup key mismatch. Therefore, idle cached connections
      could exhaust LMTP server resources, resulting in two-second
      pauses between email deliveries. This problem was investigated
      by Juliana Rodrigueiro. File: smtp/smtp_connect.c.

--- mail/postfix/files/patch-src_tls_tls__client.c
+++ mail/postfix/files/patch-src_tls_tls__client.c
@@ -9,12 +9,3 @@
  
      /*
       * Initialize the OpenSSL library by the book! To start with, we must
-@@ -441,7 +441,7 @@ TLS_APPL_STATE *tls_client_init(const TL
-     /*
-      * 2015-12-05: Ephemeral RSA removed from OpenSSL 1.1.0-dev
-      */
--#if OPENSSL_VERSION_NUMBER < 0x10100000L
-+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
- 
-     /*
-      * According to the OpenSSL documentation, temporary RSA key is needed
