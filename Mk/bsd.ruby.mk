2fd85770a36c52fec0736fc911b154758a8c8879
sunpoet <sunpoet@FreeBSD.org>
2019-03-31 13:48:32 +0000

    Remove ruby 2.3 support

--- Mk/bsd.ruby.mk
+++ Mk/bsd.ruby.mk
@@ -150,17 +150,7 @@ RUBY?=			${LOCALBASE}/bin/${RUBY_NAME}
 .if defined(RUBY_VER)
 # When adding a version, please keep the comment in
 # Mk/bsd.default-versions.mk in sync.
-. if ${RUBY_VER} == 2.3
-#
-# Ruby 2.3
-#
-RUBY_RELVERSION=	2.3.8
-RUBY_PORTREVISION=	1
-RUBY_PORTEPOCH=		1
-RUBY_PATCHLEVEL=	0
-RUBY23=			""	# PLIST_SUB helpers
-
-. elif ${RUBY_VER} == 2.4
+. if ${RUBY_VER} == 2.4
 #
 # Ruby 2.4
 #
@@ -186,14 +176,13 @@ RUBY25=			""	# PLIST_SUB helpers
 #
 # Other versions
 #
-IGNORE=	Only ruby 2.3, 2.4 and 2.5 are supported
+IGNORE=	Only ruby 2.4 and 2.5 are supported
 _INVALID_RUBY_VER=	1
 . endif
 .endif # defined(RUBY_VER)
 
 .if !defined(_INVALID_RUBY_VER)
 
-RUBY23?=		"@comment "
 RUBY24?=		"@comment "
 RUBY25?=		"@comment "
 
@@ -310,7 +299,6 @@ PLIST_SUB+=		${PLIST_RUBY_DIRS:C,DIR="(${LOCALBASE}|${PREFIX})/,DIR=",} \
 			RUBY_SUFFIX="${RUBY_SUFFIX}" \
 			RUBY_NAME="${RUBY_NAME}" \
 			RUBY_DEFAULT_SUFFIX="${RUBY_DEFAULT_SUFFIX}" \
-			RUBY23=${RUBY23} \
 			RUBY24=${RUBY24} \
 			RUBY25=${RUBY25}
 
