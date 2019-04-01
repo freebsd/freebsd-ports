9450905cf5f80e259ad08e4126bfc1ed93165f7e
meta <meta@FreeBSD.org>
2019-04-01 15:52:53 +0000

    Fix the issue tigervnc-server and -viewer cannot be installed together
    when DOCS=on because both ports install LICENSE_FILE into the same place.
    Also, following changes are made.
    
    net/tigervnc-{server,viewer}:
    
    - bump PORTREVISION due to plist & dependency change
    - do build tests, it introduces unnecessary dependencies on fltk
    
    net/tigervnc-server:
    
    - add missing dependency on devel/m4
    - remove unnecessary dependency on x11-toolkits/fltk
    
    MFH:		2019Q2

--- net/tigervnc/Makefile.common.mk
+++ net/tigervnc/Makefile.common.mk
@@ -14,14 +14,22 @@ USES+=		cmake:insource,noninja
 USE_GITHUB=	yes
 
 GH_ACCOUNT=	TigerVNC
-
 CONFLICTS=	tightvnc-[0-9]*
+PLIST_SUB+=	TIGERVNC_COMPONENT=${PORTNAME}${PKGNAMESUFFIX}-
 
 CMAKE_ARGS+=	-G "Unix Makefiles"
 MAKE_ARGS+=	TIGERVNC_SRCDIR=${WRKSRC}
 
+DOCS=	LICENCE.TXT README.rst
+
 .include <bsd.port.pre.mk>
 
 PLIST=	${.CURDIR}/pkg-plist
+
+do-install-DOCS-on:
+	${MKDIR} ${STAGEDIR}${DOCSDIR}
+.for f in ${DOCS}
+	(cd ${WRKSRC} && ${INSTALL_DATA} ${f} ${STAGEDIR}${DOCSDIR}/${PORTNAME}${PKGNAMESUFFIX}-${f})
+.endfor
 
 .include <bsd.port.post.mk>
--- net/tigervnc/files/patch-cmake_BuildPackages.cmake
+++ net/tigervnc/files/patch-cmake_BuildPackages.cmake
@@ -1,10 +1,8 @@
---- cmake/BuildPackages.cmake.orig	2017-04-19 13:27:31 UTC
+--- cmake/BuildPackages.cmake.orig	2018-07-16 14:08:55 UTC
 +++ cmake/BuildPackages.cmake
-@@ -86,5 +86,5 @@ endif() #UNIX
+@@ -86,5 +86,3 @@ endif() #UNIX
  # Common
  #
  
 -install(FILES ${CMAKE_SOURCE_DIR}/LICENCE.TXT DESTINATION ${DOC_DIR})
 -install(FILES ${CMAKE_SOURCE_DIR}/README.rst DESTINATION ${DOC_DIR})
-+install(FILES ${CMAKE_SOURCE_DIR}/LICENCE.TXT DESTINATION share/doc/tigervnc)
-+install(FILES ${CMAKE_SOURCE_DIR}/README.rst DESTINATION share/doc/tigervnc)
