89dc5a1a541b26a8ba082eff68bbe13c8281f9b0
Jan Beich <jbeich@FreeBSD.org>
2019-04-05 19:23:59 +0000

    MFH: r497877
    www/qt5-webengine: unbreak on aarch64 and armv7 after r496989
    
    PR:		236855
    Reported by:	pkg-fallout
    Submitted by:	Mikaël Urankar
    Approved by:	kde (tcberner)
    Approved by:	ports-secteam blanket

--- www/qt5-webengine/files/patch-src_3rdparty_chromium_third__party_skia_src_core_SkCpu.cpp
+++ www/qt5-webengine/files/patch-src_3rdparty_chromium_third__party_skia_src_core_SkCpu.cpp
@@ -1,19 +1,36 @@
---- src/3rdparty/chromium/third_party/skia/src/core/SkCpu.cpp.orig	2018-11-13 18:25:11 UTC
+--- src/3rdparty/chromium/third_party/skia/src/core/SkCpu.cpp.orig	2019-01-16 10:59:47 UTC
 +++ src/3rdparty/chromium/third_party/skia/src/core/SkCpu.cpp
-@@ -74,6 +74,8 @@
-     #include <sys/auxv.h>
- 
-     static uint32_t read_cpu_features() {
-+return 0;
-+#if 0
-         const uint32_t kHWCAP_CRC32   = (1<< 7),
-                        kHWCAP_ASIMDHP = (1<<10);
- 
-@@ -82,6 +84,7 @@
-         if (hwcaps & kHWCAP_CRC32  ) { features |= SkCpu::CRC32; }
-         if (hwcaps & kHWCAP_ASIMDHP) { features |= SkCpu::ASIMDHP; }
+@@ -70,6 +70,20 @@
          return features;
-+#endif
      }
  
- #elif defined(SK_CPU_ARM32) && __has_include(<sys/auxv.h>) && \
++#elif defined(SK_CPU_ARM64) && defined(__FreeBSD__)
++    #include <machine/armreg.h>
++
++    static uint32_t read_cpu_features() {
++        uint32_t features = 0;
++        uint64_t id_aa64isar0;
++
++        id_aa64isar0 = READ_SPECIALREG(ID_AA64ISAR0_EL1);
++        if (ID_AA64ISAR0_CRC32(id_aa64isar0) == ID_AA64ISAR0_CRC32_BASE) {
++            features |= SkCpu::CRC32;
++        }
++        return features;
++    }
++
+ #elif defined(SK_CPU_ARM64) && __has_include(<sys/auxv.h>)
+     #include <sys/auxv.h>
+ 
+@@ -95,7 +109,12 @@
+         const uint32_t kHWCAP_VFPv4 = (1<<16);
+ 
+         uint32_t features = 0;
++#if defined(__FreeBSD__)
++        uint32_t hwcaps = 0;
++        elf_aux_info(AT_HWCAP, &hwcaps, sizeof(hwcaps));
++#else
+         uint32_t hwcaps = getauxval(AT_HWCAP);
++#endif
+         if (hwcaps & kHWCAP_NEON ) {
+             features |= SkCpu::NEON;
+             if (hwcaps & kHWCAP_VFPv4) { features |= SkCpu::NEON_FMA|SkCpu::VFP_FP16; }
