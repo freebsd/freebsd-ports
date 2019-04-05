6562b094e9dccc102ef29dc612660513287eddef
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-04-05 22:45:52 +0000

    Fix build on powerpc
    PR:		237049
    Submitted by:	Piotr Kubaj <pkubaj@anongoth.pl>

---
+++ net/libusipp/files/patch-ipcomp.cc
@@ -0,0 +1,10 @@
+--- ipcomp.cc.orig	2017-10-05 11:20:24 UTC
++++ ipcomp.cc
+@@ -18,6 +18,7 @@
+  * along with libusi++.  If not, see <http://www.gnu.org/licenses/>.
+  */
+ 
++#include <cstring>
+ #include "usi++/usi++.h"
+ #include "usi++/ipcomp.h"
+ #include "usi++/RX.h"
