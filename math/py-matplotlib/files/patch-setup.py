fb459ec6ffba1ba90f2b22e08a6f491f39772ae1
sunpoet <sunpoet@FreeBSD.org>
2019-04-06 14:46:04 +0000

    Remove unnecessary jquery-ui-1.12.1.zip from DISTFILES
    jquery-ui-1.21.1 is already bundled in the matplot 2.2.4 tarball [1].
    This patch fixes r496681.
    
    [1] https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=236455#c4
    
    PR:		236740
    Submitted by:	sunpoet (myself)
    Approved by:	maintainer (timeout, 14 days)

--- math/py-matplotlib/files/patch-setup.py
+++ math/py-matplotlib/files/patch-setup.py
@@ -1,22 +1,10 @@
---- setup.py.orig	2019-03-22 05:20:14 UTC
+--- setup.py.orig	2019-02-26 00:18:32 UTC
 +++ setup.py
-@@ -153,12 +153,13 @@ def _download_jquery_to(dest):
-     sha = 'f8233674366ab36b2c34c577ec77a3d70cac75d2e387d8587f3836345c0f624d'
-     if not os.path.exists(os.path.join(dest, "jquery-ui-1.12.1")):
-         _makedirs(dest, exist_ok=True)
--        try:
--            buff = download_or_cache(url, sha)
--        except Exception:
--            raise IOError("Failed to download jquery-ui.  Please download " +
--                          "{url} and extract it to {dest}.".format(
--                              url=url, dest=dest))
-+        buff = os.path.join("%%DISTDIR%%", "jquery-ui-1.12.1.zip")
-+#        try:
-+#            buff = download_or_cache(url, sha)
-+#        except Exception:
-+#            raise IOError("Failed to download jquery-ui.  Please download " +
-+#                          "{url} and extract it to {dest}.".format(
-+#                              url=url, dest=dest))
-         with ZipFile(buff) as zf:
-             zf.extractall(dest)
+@@ -189,7 +189,6 @@ class develop_with_jquery(DevelopCommand
+ 
+ 
+ cmdclass['sdist'] = sdist_with_jquery
+-cmdclass['install_lib'] = install_lib_with_jquery
+ cmdclass['develop'] = develop_with_jquery
+ 
  
