f92f9ea7f6a9d264bcfee9706b49ef8caa749a86
Yuri Victorovich <yuri@FreeBSD.org>
2019-04-08 01:13:21 +0000

    audio/caps-lv2: Update g20170626 -> g20190321

--- audio/caps-lv2/files/patch-dsp_v4f__IIR2.h
+++ audio/caps-lv2/files/patch-dsp_v4f__IIR2.h
@@ -1,6 +1,18 @@
---- dsp/v4f_IIR2.h.orig
+--- dsp/v4f_IIR2.h.orig	2019-03-22 09:42:59 UTC
 +++ dsp/v4f_IIR2.h
-@@ -45,8 +45,8 @@
+@@ -32,9 +32,9 @@
+ 
+ namespace DSP {
+ 
+-#ifdef __APPLE__
++#ifdef __FreeBSD__
+ 
+-inline float exp10f(float f) {return __exp10f(f);}
++inline float exp10f(float f) {return powf(10., f);}
+ 
+ #endif
+ 
+@@ -47,8 +47,8 @@ class RBJv4
  			{
  				v4f_t w = v4f_2pi * f;
  
@@ -11,7 +23,7 @@
  
  				alpha = sin / (v4f_2 * Q);
  			}
-@@ -549,9 +549,9 @@
+@@ -551,9 +551,9 @@ class Resonator4fBank
  			{
  				v4f_t * a = state + i*Item;
  				f *= v4f_2pi;
