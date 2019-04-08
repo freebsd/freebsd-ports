f2da585b666b9d1d6ff807eeb86715a93c271540
Matthias Fechner <mfechner@FreeBSD.org>
2019-04-08 11:19:55 +0000

    Changed the build order that the port also works with bundler version 2.

--- devel/gitaly/files/patch-__support_makegen.go
+++ devel/gitaly/files/patch-__support_makegen.go
@@ -1,4 +1,4 @@
---- _support/makegen.go.orig	2019-03-11 20:46:03 UTC
+--- _support/makegen.go.orig	2019-03-25 09:02:26 UTC
 +++ _support/makegen.go
 @@ -253,7 +253,7 @@ var templateText = `
  # These variables may be overriden at runtime by top-level make
@@ -9,12 +9,13 @@
  ASSEMBLY_ROOT ?= {{ .BuildDir }}/assembly
  BUILD_TAGS := tracer_static tracer_static_jaeger
  
-@@ -274,7 +274,7 @@ build: ../.ruby-bundle
+@@ -273,8 +273,8 @@ build: ../.ruby-bundle
+ # step. Both Omnibus and CNG assume it is in the Gitaly root, not in
  # _build. Hence the '../' in front.
  ../.ruby-bundle:  {{ .GitalyRubyDir }}/Gemfile.lock  {{ .GitalyRubyDir }}/Gemfile
++	cd  {{ .GitalyRubyDir }} && rm Gemfile.lock && bundle install $(BUNDLE_FLAGS)
  	cd  {{ .GitalyRubyDir }} && bundle config # for debugging
 -	cd  {{ .GitalyRubyDir }} && bundle install $(BUNDLE_FLAGS)
-+	cd  {{ .GitalyRubyDir }} && rm Gemfile.lock && bundle install $(BUNDLE_FLAGS)
  	cd  {{ .GitalyRubyDir }} && bundle show gitaly-proto # sanity check
  	touch $@
  
