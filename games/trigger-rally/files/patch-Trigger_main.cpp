053140ed0ed471e083c391ef34bda80218b2ef93
Steve Wills <swills@FreeBSD.org>
2019-04-11 20:07:34 +0000

    games/trigger-rally: update to 0.6.6.1
    PR:		236817
    Submitted by:	Alfonso S. Siciliano <alfix86@gmail.com> (maintainer)

--- games/trigger-rally/files/patch-Trigger_main.cpp
+++ games/trigger-rally/files/patch-Trigger_main.cpp
@@ -1,11 +1,11 @@
---- Trigger/main.cpp.orig	2017-03-02 19:21:27 UTC
+--- Trigger/main.cpp.orig	2019-03-26 21:41:24 UTC
 +++ Trigger/main.cpp
-@@ -469,7 +469,7 @@ void MainApp::loadConfig()
+@@ -471,7 +471,7 @@ void MainApp::loadConfig()
    if (!PHYSFS_exists(cfgfilename.c_str())) {
  #ifdef UNIX
      const std::vector<std::string> cfghidingplaces {
 -        "/usr/share/games/trigger-rally/"
-+        "/usr/local/share/games/trigger-rally/"
++        "%%PREFIX%%/share/games/trigger-rally/"
      };
  
      for (const std::string &cfgpath: cfghidingplaces)
