7831a0f03d51f3eae23d47f24be38af72674f7b9
bhughes <bhughes@FreeBSD.org>
2019-04-13 20:33:18 +0000

    www/node: Update 11.13.0 -> 11.14.0
    https://nodejs.org/en/blog/release/v11.14.0/
    
    Sponsored by:	Miles AS

--- www/node/files/patch-deps_openssl_config_opensslconf__no-asm.h
+++ www/node/files/patch-deps_openssl_config_opensslconf__no-asm.h
@@ -1,9 +1,9 @@
---- deps/openssl/config/opensslconf_no-asm.h.orig	2018-05-09 01:59:18 UTC
+--- deps/openssl/config/opensslconf_no-asm.h.orig	2019-04-11 12:07:35 UTC
 +++ deps/openssl/config/opensslconf_no-asm.h
-@@ -16,8 +16,8 @@
- # include "./archs/VC-WIN32/no-asm/include/openssl/opensslconf.h"
- #elif defined(_WIN32) && defined(_M_X64)
+@@ -18,8 +18,8 @@
  # include "./archs/VC-WIN64A/no-asm/include/openssl/opensslconf.h"
+ #elif defined(_WIN32) && defined(_M_ARM64)
+ # include "./archs/VC-WIN64-ARM/no-asm/include/openssl/opensslconf.h"
 -#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__i386__)
 -# include "./archs/BSD-x86/no-asm/include/openssl/opensslconf.h"
 +//#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__i386__)
