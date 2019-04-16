e02798cba1dd2b4e9e9a25503abec24fdbf063a6
tcberner <tcberner@FreeBSD.org>
2019-04-16 09:58:50 +0000

    Update KDE Frameworks to 5.57
    Announcement:
    	https://kde.org/announcements/kde-frameworks-5.57.0.php
    
    PR:		237253
    Exp-run by:	antoine

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -58,7 +58,7 @@ KDE_PLASMA_VERSION?=		5.15.4
 KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
-KDE_FRAMEWORKS_VERSION?=	5.56.0
+KDE_FRAMEWORKS_VERSION?=	5.57.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 # Current KDE applications.
