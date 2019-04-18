ade9d9c8b4afd45955e05735429d5b7ae4904b77
swills <swills@FreeBSD.org>
2019-04-18 19:45:12 +0000

    devel/godot: update to 3.1
    PR:		236731
    Submitted by:	Shane <FreeBSD@ShaneWare.Biz> (maintainer)

---
+++ devel/godot/files/patch-thirdparty_libvpx_vpx_vpx__codec.h
@@ -0,0 +1,20 @@
+--- thirdparty/libvpx/vpx/vpx_codec.h.orig  2019-03-23 14:24:15 UTC
++++ thirdparty/libvpx/vpx/vpx_codec.h
+@@ -36,8 +36,8 @@
+  * Once initialized, the instance is manged using other functions from
+  * the vpx_codec_* family.
+  */
+-#ifndef VPX_VPX_CODEC_H_
+-#define VPX_VPX_CODEC_H_
++#ifndef VPX_VPX_VPX_CODEC_H_
++#define VPX_VPX_VPX_CODEC_H_
+
+ #ifdef __cplusplus
+ extern "C" {
+@@ -475,5 +475,5 @@ extern "C" {
+ #ifdef __cplusplus
+ }
+ #endif
+-#endif  // VPX_VPX_CODEC_H_
++#endif  // VPX_VPX_VPX_CODEC_H_
+
