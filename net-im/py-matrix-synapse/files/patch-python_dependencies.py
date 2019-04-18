6ba29cefe788a2c83fbe80014cfda2292a826a9d
Steve Wills <swills@FreeBSD.org>
2019-04-18 20:33:46 +0000

    net-im/py-matrix-synapse: update to v0.99.3
    PR:		236260
    Submitted by:	Sascha Biberhofer <ports@skyforge.at> (maintainer)
    Reported by:	Christian Schwarz <me@cschwarz.com>

--- net-im/py-matrix-synapse/files/patch-python_dependencies.py
+++ net-im/py-matrix-synapse/files/patch-python_dependencies.py
@@ -1,11 +1,11 @@
---- synapse/python_dependencies.py.orig	2019-01-10 17:51:27.362204447 +0100
-+++ synapse/python_dependencies.py	2019-01-10 17:51:47.559224870 +0100
-@@ -58,7 +58,7 @@
+--- synapse/python_dependencies.py.orig	2019-02-14 17:20:02.000000000 +0000
++++ synapse/python_dependencies.py	2019-02-28 20:31:10.838648000 +0000
+@@ -62,7 +62,7 @@
      "six>=1.10",
      # prometheus_client 0.4.0 changed the format of counter metrics
      # (cf https://github.com/matrix-org/synapse/issues/4001)
 -    "prometheus_client>=0.0.18,<0.4.0",
 +    "prometheus_client>=0.0.18",
+ 
      # we use attr.s(slots), which arrived in 16.0.0
-     "attrs>=16.0.0",
-     "netaddr>=0.7.18",
+     # Twisted 18.7.0 requires attrs>=17.4.0
