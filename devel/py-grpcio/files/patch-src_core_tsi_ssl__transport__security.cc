60e1188416037bece48e813e8fd7247b9679479a
vanilla <vanilla@FreeBSD.org>
2019-04-18 09:23:09 +0000

    Update to 1.20.0.

--- devel/py-grpcio/files/patch-src_core_tsi_ssl__transport__security.cc
+++ devel/py-grpcio/files/patch-src_core_tsi_ssl__transport__security.cc
@@ -1,6 +1,6 @@
---- src/core/tsi/ssl_transport_security.cc.orig	2018-04-13 19:56:08 UTC
+--- src/core/tsi/ssl_transport_security.cc.orig	2019-04-15 23:12:51 UTC
 +++ src/core/tsi/ssl_transport_security.cc
-@@ -20,6 +20,7 @@
+@@ -22,6 +22,7 @@
  
  #include "src/core/tsi/ssl_transport_security.h"
  
