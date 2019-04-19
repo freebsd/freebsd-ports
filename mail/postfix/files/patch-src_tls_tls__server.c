f69a4a10f87ca8b8c64d06d7421f795bbaa68ee0
ohauer <ohauer@FreeBSD.org>
2019-04-19 18:53:13 +0000

    - update to 3.4.5 - adopt libressl patches from OpenBSD

--- mail/postfix/files/patch-src_tls_tls__server.c
+++ mail/postfix/files/patch-src_tls_tls__server.c
@@ -1,6 +1,6 @@
---- src/tls/tls_server.c.orig	2018-11-17 22:33:15 UTC
+--- src/tls/tls_server.c.orig	2019-02-18 23:03:54 UTC
 +++ src/tls/tls_server.c
-@@ -517,7 +517,9 @@ TLS_APPL_STATE *tls_server_init(const TL
+@@ -518,7 +518,9 @@ TLS_APPL_STATE *tls_server_init(const TL
  	 * ticket decryption callback already (since 2.11) asks OpenSSL to
  	 * avoid issuing new tickets when the presented ticket is re-usable.
  	 */
