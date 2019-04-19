e2680d8f4280af02f0f789079de3b15df1be846a
Stephen Hurd <shurd@FreeBSD.org>
2019-04-19 15:59:37 +0000

    Update to 4.1.26

--- comms/quisk/files/patch-microphone.c
+++ comms/quisk/files/patch-microphone.c
@@ -1,6 +1,6 @@
---- microphone.c.orig	2016-10-25 20:57:54 UTC
+--- microphone.c.orig	2018-11-21 19:08:22 UTC
 +++ microphone.c
-@@ -56,7 +56,7 @@ static int timeVOX = 2000;				// VOX han
+@@ -58,7 +58,7 @@ static int timeVOX = 2000;				// VOX han
  
  static int doTxCorrect = 0;				// Corrections for UDP sample transmit
  static double TxCorrectLevel;
--- comms/quisk/files/patch-setup.py
+++ comms/quisk/files/patch-setup.py
@@ -1,22 +1,51 @@
---- setup.py.orig	2016-12-08 22:14:02 UTC
+--- setup.py.orig	2018-11-22 14:56:57 UTC
 +++ setup.py
-@@ -12,8 +12,8 @@ fp.write("#Quisk version %s\n" % Version
- fp.close()
+@@ -18,18 +18,18 @@ if sys.platform != "win32":
+   except ImportError:
+     print ("Please install the package python-wxgtk3.0 or later")
+     missing = True
+-  if not os.path.isfile("/usr/include/fftw3.h"):
+-    print ("Please install the package libfftw3-dev")
+-    missing = True
+-  if not os.path.isdir("/usr/include/alsa"):
+-    print ("Please install the package libasound2-dev")
+-    missing = True
+-  if not os.path.isfile("/usr/include/portaudio.h"):
+-    print ("Please install the package portaudio19-dev")
+-    missing = True
+-  if not os.path.isdir("/usr/include/pulse"):
+-    print ("please install the package libpulse-dev")
+-    missing = True
++#  if not os.path.isfile("/usr/include/fftw3.h"):
++#    print ("Please install the package libfftw3-dev")
++#    missing = True
++#  if not os.path.isdir("/usr/include/alsa"):
++#    print ("Please install the package libasound2-dev")
++#    missing = True
++#  if not os.path.isfile("/usr/include/portaudio.h"):
++#    print ("Please install the package portaudio19-dev")
++#    missing = True
++#  if not os.path.isdir("/usr/include/pulse"):
++#    print ("please install the package libpulse-dev")
++#    missing = True
+   if missing:
+     ans = raw_input("Do you want to continue (y, N):  ")
+     ans = ans.strip()
+@@ -37,6 +37,8 @@ if sys.platform != "win32":
+       sys.exit(1)
  
  module1 = Extension ('quisk._quisk',
--	#include_dirs = ['.'],
--	#library_dirs = ['.'],
 +	include_dirs = ['%%LOCALBASE%%/include'],
 +	library_dirs = ['%%LOCALBASE%%/lib'],
  	libraries = ['asound', 'portaudio', 'pulse', 'fftw3', 'm'],
  	sources = ['quisk.c', 'sound.c', 'sound_alsa.c', 'sound_portaudio.c', 'sound_pulseaudio.c',
  		'is_key_down.c', 'microphone.c', 'utility.c',
-@@ -116,7 +116,7 @@ control the N8LP LP-PAN panadapter and t
- 	author_email = 'jahlstr@gmail.com',
- 	url = 'http://james.ahlstrom.name/quisk/',
- 	download_url = 'http://james.ahlstrom.name/quisk/',
--	packages = ['quisk', 'quisk.sdriqpkg', 'quisk.n2adr', 'quisk.softrock', 'quisk.usb'],
-+	packages = ['quisk', 'quisk.sdriqpkg', 'quisk.n2adr', 'quisk.softrock', 'quisk.usb', 'quisk.hermes'],
- 	package_dir =  {'quisk' : '.'},
- 	package_data = {'quisk' : ['*.txt', '*.html']},
- 	ext_modules = Modules,
+@@ -69,8 +71,6 @@ modulew2 = Extension ('quisk.sdriqpkg.sd
+ 
+ # Changes for MacOS support thanks to Mario, DL3LSM.
+ modulem1 = Extension ('quisk._quisk',
+-	#include_dirs = ['.'],
+-	#library_dirs = ['.'],
+ 	libraries = ['portaudio', 'fftw3', 'm', 'pulse'],
+ 	sources = ['quisk.c', 'sound.c', 'sound_portaudio.c',
+ 		'is_key_down.c', 'microphone.c', 'utility.c',
