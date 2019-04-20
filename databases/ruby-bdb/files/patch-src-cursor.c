7ac70f2cc03e765a06381cd4a0d2bb0db8fee721
mfechner <mfechner@FreeBSD.org>
2019-04-20 19:55:08 +0000

    Fix build problem with ruby 2.5. This patch should be included upstream.
    PR:		237410
    Submitted by:	ume@
    Reported by:	mbeis@xs4all.nl
    Reviewed by:	mbeis@xs4all.nl, w.schwarzenfeld@utanet.at

--- databases/ruby-bdb/files/patch-src-cursor.c
+++ databases/ruby-bdb/files/patch-src-cursor.c
@@ -1,13 +1,13 @@
---- src/cursor.c.orig	2015-01-22 17:37:51.000000000 +0000
-+++ src/cursor.c	2015-01-22 17:38:17.000000000 +0000
-@@ -29,8 +29,8 @@
+--- src/cursor.c.orig	2011-04-06 19:35:39 UTC
++++ src/cursor.c
+@@ -29,8 +29,8 @@ bdb_cursor(int argc, VALUE *argv, VALUE 
      flags = 0;
      if (argc && TYPE(argv[argc - 1]) == T_HASH) {
  	VALUE g, f = argv[argc - 1];
 -	if ((g = rb_hash_aref(f, rb_intern("flags"))) != RHASH(f)->ifnone ||
 -	    (g = rb_hash_aref(f, rb_str_new2("flags"))) != RHASH(f)->ifnone) {
-+	if ((g = rb_hash_aref(f, rb_intern("flags"))) != rb_hash_ifnone(f) ||
-+	    (g = rb_hash_aref(f, rb_str_new2("flags"))) != rb_hash_ifnone(f)) {
++	if ((g = rb_hash_lookup(f, rb_intern("flags"))) != Qnil ||
++	    (g = rb_hash_lookup(f, rb_str_new2("flags"))) != Qnil) {
  	    flags = NUM2INT(g);
  	}
  	argc--;
--- databases/ruby-bdb/files/patch-src-recnum.c
+++ databases/ruby-bdb/files/patch-src-recnum.c
@@ -1,22 +1,22 @@
---- src/recnum.c.orig	2011-04-06 19:35:39.000000000 +0000
-+++ src/recnum.c	2015-01-22 17:16:32.000000000 +0000
-@@ -17,7 +17,7 @@
+--- src/recnum.c.orig	2011-04-06 19:35:39 UTC
++++ src/recnum.c
+@@ -17,7 +17,7 @@ bdb_recnum_init(int argc, VALUE *argv, V
  	argc++;
      }
      rb_hash_aset(argv[argc - 1], array, INT2FIX(0));
 -    if (rb_hash_aref(argv[argc - 1], sarray) != RHASH(argv[argc - 1])->ifnone) {
-+    if (rb_hash_aref(argv[argc - 1], sarray) != rb_hash_ifnone(argv[argc - 1])) {
++    if (rb_hash_lookup(argv[argc - 1], sarray) != Qnil) {
  	rb_hash_aset(argv[argc - 1], sarray, INT2FIX(0));
      }
      rb_hash_aset(argv[argc - 1], rb_str_new2("set_flags"), INT2FIX(DB_RENUMBER));
-@@ -697,8 +697,8 @@
+@@ -697,8 +697,8 @@ bdb_sary_clear(int argc, VALUE *argv, VA
  
      if (argc && TYPE(argv[argc - 1]) == T_HASH) {
  	VALUE f = argv[argc - 1];
 -	if ((g = rb_hash_aref(f, rb_intern("flags"))) != RHASH(f)->ifnone ||
 -	    (g = rb_hash_aref(f, rb_str_new2("flags"))) != RHASH(f)->ifnone) {
-+	if ((g = rb_hash_aref(f, rb_intern("flags"))) != rb_hash_ifnone(f) ||
-+	    (g = rb_hash_aref(f, rb_str_new2("flags"))) != rb_hash_ifnone(f)) {
++	if ((g = rb_hash_lookup(f, rb_intern("flags"))) != Qnil ||
++	    (g = rb_hash_lookup(f, rb_str_new2("flags"))) != Qnil) {
  	    flags = NUM2INT(g);
  	}
  	argc--;
