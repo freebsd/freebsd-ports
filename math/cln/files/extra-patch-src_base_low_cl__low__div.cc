6049e5cc65a3822fc181de698b22028b8e0d025c
wen <wen@FreeBSD.org>
2019-04-23 23:44:25 +0000

    - Unbreak on aarch64
    PR:		237439
    Submitted by:	greg@unrelenting.technology

--- math/cln/files/extra-patch-src_base_low_cl__low__div.cc
+++ math/cln/files/extra-patch-src_base_low_cl__low__div.cc
@@ -1,11 +1,11 @@
---- src/base/low/cl_low_div.cc.orig	2018-06-23 21:13:46 UTC
+--- src/base/low/cl_low_div.cc.orig	2011-04-08 21:07:28 UTC
 +++ src/base/low/cl_low_div.cc
 @@ -207,7 +207,7 @@ uint32 divu_6432_3232_(uint32 xhi, uint32 xlo, uint32 
  #endif
  
  #ifdef NEED_VAR_divu_64_rest
 -uint64 divu_64_rest;
-+extern "C" uint64 divu_64_rest;
++extern "C" uint64 divu_64_rest = 0;
  #endif
  
  #ifdef NEED_FUNCTION_divu_6464_6464_
--- math/cln/files/extra-patch-src_base_low_cl__low__mul.cc
+++ math/cln/files/extra-patch-src_base_low_cl__low__mul.cc
@@ -1,4 +1,4 @@
---- src/base/low/cl_low_mul.cc.orig	2018-06-23 21:15:19 UTC
+--- src/base/low/cl_low_mul.cc.orig	2011-04-08 21:06:30 UTC
 +++ src/base/low/cl_low_mul.cc
 @@ -50,11 +50,11 @@ uint64 mulu32_w (uint32 arg1, uint32 arg2)
  
@@ -10,7 +10,7 @@
  
  #ifdef NEED_FUNCTION_mulu64_
 -uint64 mulu64_high;
-+extern "C" uint64 mulu64_high;
++extern "C" uint64 mulu64_high = 0;
  namespace cln {
  extern "C" uint64 mulu64_ (uint64 x, uint64 y);
  uint64 mulu64_ (uint64 x, uint64 y)
