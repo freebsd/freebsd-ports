213fd6e5a2ceeefee266f1d9f91990a79d504972
mat <mat@FreeBSD.org>
2019-04-23 12:09:04 +0000

    Update to 5.28.2.

--- lang/perl5.28/files/patch-cpan_Pod-Perldoc_lib_Pod_Perldoc_ToTerm.pm
+++ lang/perl5.28/files/patch-cpan_Pod-Perldoc_lib_Pod_Perldoc_ToTerm.pm
@@ -1,3 +1,5 @@
+Handle MANWIDTH=tty
+
 --- cpan/Pod-Perldoc/lib/Pod/Perldoc/ToTerm.pm.orig	2017-04-03 07:28:44 UTC
 +++ cpan/Pod-Perldoc/lib/Pod/Perldoc/ToTerm.pm
 @@ -69,6 +69,8 @@ sub _get_columns_from_manwidth {
