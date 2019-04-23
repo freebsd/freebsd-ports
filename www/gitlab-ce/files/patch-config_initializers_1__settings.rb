c32d5f531fc680064e3a8ee7322e1eb16c39c7b0
mfechner <mfechner@FreeBSD.org>
2019-04-23 08:27:13 +0000

    Upgrade to 11.10. For full log see: https://about.gitlab.com/2019/04/22/gitlab-11-10-released/
    WARNING, this release switches the path were the repositories are
    stored back to the default git home dir. Make sure you read the
    UPDATING 20190423 entry and follow the update maual very carefully!

--- www/gitlab-ce/files/patch-config_initializers_1__settings.rb
+++ www/gitlab-ce/files/patch-config_initializers_1__settings.rb
@@ -1,6 +1,6 @@
---- config/initializers/1_settings.rb.orig	2018-12-21 14:23:37 UTC
+--- config/initializers/1_settings.rb.orig	2019-04-19 09:51:35 UTC
 +++ config/initializers/1_settings.rb
-@@ -126,12 +126,8 @@ Settings.gitlab['email_subject_suffix'] ||= ENV['GITLA
+@@ -145,12 +145,8 @@ Settings.gitlab['email_subject_suffix'] ||= ENV['GITLA
  Settings.gitlab['base_url']   ||= Settings.__send__(:build_base_gitlab_url)
  Settings.gitlab['url']        ||= Settings.__send__(:build_gitlab_url)
  Settings.gitlab['user']       ||= 'git'
