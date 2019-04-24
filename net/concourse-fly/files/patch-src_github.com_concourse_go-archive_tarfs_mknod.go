4896ac2f7b28120c4fdde315d0754d275561088d
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2019-04-24 17:20:06 +0000

    net/concourse-fly: Update to 5.1.0
    Release notes:
    * https://concourse-ci.org/download.html#v500
    * https://concourse-ci.org/download.html#v501
    * https://concourse-ci.org/download.html#v510
    
    Sponsored by:	Pivotal Software, Inc.

--- net/concourse-fly/files/patch-src_github.com_concourse_go-archive_tarfs_mknod.go
+++
@@ -1,8 +0,0 @@
---- src/github.com/concourse/go-archive/tarfs/mknod.go.orig	2019-03-04 10:29:20 UTC
-+++ src/github.com/concourse/go-archive/tarfs/mknod.go
-@@ -1,4 +1,4 @@
--// +build !windows
-+// +build !windows,!freebsd
- 
- package tarfs
- 
