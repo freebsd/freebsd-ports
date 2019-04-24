32daaae72c4bea6711020b6b7a68acbaf0af386e
Jan Beich <jbeich@FreeBSD.org>
2019-04-24 19:15:05 +0000

    Switch to -fstack-protector-strong and enable on more architectures
    PR:		237273
    Exp-run by:	antoine
    Approved by:	portmgr (antoine)
    Differential Revision:	https://reviews.freebsd.org/D19907

--- Mk/bsd.ssp.mk
+++ Mk/bsd.ssp.mk
@@ -4,14 +4,9 @@
 SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 
 .if !defined(SSP_UNSAFE) && \
-    (${ARCH} == i386 || ${ARCH} == amd64)
+    (${MACHINE} != "mips")
 # Overridable as a user may want to use -fstack-protector-all
-SSP_CFLAGS?=	-fstack-protector
+SSP_CFLAGS?=	-fstack-protector-strong
 CFLAGS+=	${SSP_CFLAGS}
-LDFLAGS+=	-fstack-protector
-# -lssp_nonshared is needed on i386 where /usr/lib/libc.so is not an ldscript
-# This is currently unused XXX
-.	if defined(SSP_NEED_NONSHARED)
-LDFLAGS+=	-lssp_nonshared
-.	endif
+LDFLAGS+=	${SSP_CFLAGS}
 .endif
