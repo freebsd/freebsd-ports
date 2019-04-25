66761cb3a679db0db5423575ded9b7c4f8279839
koobs <koobs@FreeBSD.org>
2019-04-25 09:51:48 +0000

    devel/py-freezegun: Update to 0.3.11
    Changelog:
    
      https://github.com/spulec/freezegun/blob/0.3.11/CHANGELOG

--- devel/py-freezegun/files/patch-setup.py
+++ devel/py-freezegun/files/patch-setup.py
@@ -1,23 +1,10 @@
---- setup.py.orig	2016-03-03 08:44:23 UTC
+--- setup.py.orig	2019-04-25 09:34:13 UTC
 +++ setup.py
-@@ -11,6 +11,11 @@ else:
-     # Py3k
-     requires += ['python-dateutil>=2.0']
- 
-+tests_require = [
-+    'nose',
-+    'mock',
-+]
-+
- setup(
-     name='freezegun',
-     version='0.3.6',
-@@ -20,6 +25,8 @@ setup(
-     url='https://github.com/spulec/freezegun',
+@@ -27,6 +27,7 @@ setup(
      packages=['freezegun'],
      install_requires=requires,
-+    tests_require=tests_require,
+     tests_require=tests_require,
 +    test_suite='nose.collector',
      include_package_data=True,
      license='Apache 2.0',
-     classifiers=[
+     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
