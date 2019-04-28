8e888a4ed1a9bf9dd39792131036d87092c88946
mmokhi <mmokhi@FreeBSD.org>
2019-04-28 21:24:35 +0000

    databases/mysql56-{client, server}: Update to latest release 5.6.44 This update includes Bugfix: - InnoDB: The INDEX_LENGTH value in INFORMATION_SCHEMA.TABLES     was not updated when adding an index - MySQL 5.6 did not build with maintainer mode enabled with GCC 7 - A damaged mysql.user table could cause a server exit - mysqladmin shutdown did not wait for mysqld to shut down More info: https://dev.mysql.com/doc/relnotes/mysql/5.6/en/news-5-6-44.html
    Security Fix:
    CVE-2019-1559, CVE-2018-3123 and other fixes.
    More info: https://www.oracle.com/technetwork/security-advisory/cpuapr2019-5072813.html#AppendixMSQL<Paste>
    
    PR:		237399
    Reported by:	Brent Busby <brent@jfi.uchicago.edu>
    Sponsored by:	The FreeBSD Foundation

--- databases/mysql56-server/files/patch-cmake_plugin.cmake
+++ databases/mysql56-server/files/patch-cmake_plugin.cmake
@@ -1,10 +1,12 @@
---- cmake/plugin.cmake.orig	2011-09-09 17:56:39.000000000 +0200
-+++ cmake/plugin.cmake	2011-10-07 10:56:37.000000000 +0200
-@@ -195,7 +195,6 @@
-       OUTPUT_NAME "${ARG_MODULE_OUTPUT_NAME}")  
-     # Install dynamic library
-     MYSQL_INSTALL_TARGETS(${target} DESTINATION ${INSTALL_PLUGINDIR} COMPONENT Server)
--    INSTALL_DEBUG_TARGET(${target} DESTINATION ${INSTALL_PLUGINDIR}/debug)
-     # Add installed files to list for RPMs
-     FILE(APPEND ${CMAKE_BINARY_DIR}/support-files/plugins.files
-             "%attr(755, root, root) %{_prefix}/${INSTALL_PLUGINDIR}/${ARG_MODULE_OUTPUT_NAME}.so\n"
+--- cmake/plugin.cmake.orig	2019-03-15 07:38:15 UTC
++++ cmake/plugin.cmake
+@@ -212,9 +212,6 @@ MACRO(MYSQL_ADD_PLUGIN)
+       MYSQL_INSTALL_TARGETS(${target}
+         DESTINATION ${INSTALL_PLUGINDIR}
+         COMPONENT ${INSTALL_COMPONENT})
+-      INSTALL_DEBUG_TARGET(${target}
+-        DESTINATION ${INSTALL_PLUGINDIR}/debug
+-        COMPONENT ${INSTALL_COMPONENT})
+       # Add installed files to list for RPMs
+       FILE(APPEND ${CMAKE_BINARY_DIR}/support-files/plugins.files
+               "%attr(755, root, root) %{_prefix}/${INSTALL_PLUGINDIR}/${ARG_MODULE_OUTPUT_NAME}.so\n"
