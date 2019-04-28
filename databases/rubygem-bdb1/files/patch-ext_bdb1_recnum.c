73c4c3043f0a00e12eadd63461e51626902aeb30
Dirk Meyer <dinoex@FreeBSD.org>
2019-04-28 12:47:14 +0000

    - fix build with ruby25

--- databases/rubygem-bdb1/files/patch-ext_bdb1_recnum.c
+++ databases/rubygem-bdb1/files/patch-ext_bdb1_recnum.c
@@ -5,7 +5,7 @@
      }
      rb_hash_aset(argv[argc - 1], array, INT2FIX(0));
 -    if (rb_hash_aref(argv[argc - 1], sarray) != RHASH(argv[argc - 1])->ifnone) {
-+    if (rb_hash_aref(argv[argc - 1], sarray) != rb_hash_ifnone(argv[argc - 1])) {
++    if (rb_hash_lookup2(argv[argc - 1], sarray, Qundef) != Qundef) {
  	rb_hash_aset(argv[argc - 1], sarray, INT2FIX(0));
      }
      rb_hash_aset(argv[argc - 1], rb_str_new2("set_flags"), INT2FIX(DB_RENUMBER));
