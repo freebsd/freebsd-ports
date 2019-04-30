aa1f68c3d4c1b88d2db9eaa2a722bcdc67259ccd
pkubaj <pkubaj@FreeBSD.org>
2019-04-30 18:51:53 +0000

    games/openjk: Community effort to maintain Jedi Academy + Jedi Outcast
    OpenJK is an effort by the JACoders group to maintain and improve the game
    engines on which the Jedi Academy (JA) and Jedi Outcast (JO) games run on,
    while maintaining full backwards compatibility with the existing games.
    This project does not attempt to rebalance or otherwise modify core gameplay.
    
    WWW: https://openjk.org
    
    PR:		227265
    Approved by:	mat (mentor)
    Differential Revision:	https://reviews.freebsd.org/D19955

---
+++ games/openjk/files/patch-CMakeModules_InstallConfig.cmake
@@ -0,0 +1,13 @@
+--- CMakeModules/InstallConfig.cmake.orig	2019-04-07 09:22:21 UTC
++++ CMakeModules/InstallConfig.cmake
+@@ -17,8 +17,8 @@
+ #============================================================================
+ 
+ # Subdirectories to package JK2 and JKA into
+-set(JKAInstallDir "JediAcademy")
+-set(JK2InstallDir "JediOutcast")
++set(JKAInstallDir "share/JediAcademy")
++set(JK2InstallDir "share/JediOutcast")
+ 
+ # Install components
+ set(JKAMPCoreComponent "JKAMPCore")
