7857ef97e99abea7c665ba1c2c1255bd290f47c5
danfe <danfe@FreeBSD.org>
2019-05-03 17:50:35 +0000

    - Update to version 0.9.2 and make it build on both amd64 and i386 by   defining __LONG_LONG_SUPPORTED macro, which allows to pull llabs(3)   for 64-bit numbers; was not a problem on amd64 because sizeof(long)   == 8 there and thus no `long long' help needed
    - Bump required C++ standard to C++03; it's not quite ready for later
      ones due to the this:
    
        bardecode/code25i.hh:34:29: error: in-class initializer for static
        data member of type 'const double' requires 'constexpr' specifier
    
    - GC no-op `post-extract' target, patches that were applied upstream,
      and no longer relevant libungif -> libgif fixups in the Makefile

--- graphics/exact-image/files/patch-codecs__gif.cc
+++ graphics/exact-image/files/patch-codecs__gif.cc
@@ -1,4 +1,4 @@
---- codecs/gif.cc.orig	2010-03-03 21:04:44 UTC
+--- codecs/gif.cc.orig	2015-03-28 15:35:58 UTC
 +++ codecs/gif.cc
 @@ -22,6 +22,24 @@
  
@@ -25,26 +25,26 @@
  /* The way Interlaced image should. */
  static const int InterlacedOffset[] = { 0, 4, 2, 1 };
  
-@@ -60,9 +78,13 @@ int GIFCodec::readImage (std::istream* s
-   ColorMapObject *ColorMap = NULL;
-   int ExtCode;
+@@ -60,9 +78,13 @@ int GIFCodec::readImage (std::istream* stream, Image& 
+   ColorMapObject *ColorMap = 0;
+   int GifError, ExtCode;
    
 +#if GIFLIB_MAJOR >= 5
-+  if ((GifFile = DGifOpen (stream, &GIFInputFunc, NULL)) == NULL)
+   if ((GifFile = DGifOpen (stream, &GIFInputFunc, &GifError)) == 0)
 +#else
-   if ((GifFile = DGifOpen (stream, &GIFInputFunc)) == NULL)
++  if ((GifFile = DGifOpen (stream, &GIFInputFunc)) == 0)
 +#endif
      {
--      PrintGifError();
+-      //PrintGifError();
 +      std::cerr << "\nGIF-LIB error: can't open GIF file" << std::endl;
        return false;
      }
    
-@@ -74,7 +96,11 @@ int GIFCodec::readImage (std::istream* s
+@@ -74,7 +96,11 @@ int GIFCodec::readImage (std::istream* stream, Image& 
    /* Scan the content of the GIF file and load the image(s) in: */
    do {
      if (DGifGetRecordType(GifFile, &RecordType) == GIF_ERROR) {
--      PrintGifError();
+-      //PrintGifError();
 +#if GIFLIB_MAJOR >= 5
 +      ExactImagePrintGifError(GifFile->Error);
 +#else
@@ -53,11 +53,11 @@
        return false;
      }
      
-@@ -83,7 +109,11 @@ int GIFCodec::readImage (std::istream* s
+@@ -83,7 +109,11 @@ int GIFCodec::readImage (std::istream* stream, Image& 
      switch (RecordType) {
      case IMAGE_DESC_RECORD_TYPE:
        if (DGifGetImageDesc(GifFile) == GIF_ERROR) {
--	PrintGifError();
+-	//PrintGifError();
 +#if GIFLIB_MAJOR >= 5
 +	ExactImagePrintGifError(GifFile->Error);
 +#else
@@ -66,11 +66,11 @@
  	return false;
        }
        
-@@ -104,7 +134,11 @@ int GIFCodec::readImage (std::istream* s
+@@ -104,7 +134,11 @@ int GIFCodec::readImage (std::istream* stream, Image& 
  	       j += InterlacedJumps[i]) {
  	    if (DGifGetLine(GifFile, &image.getRawData()[j*image.stride()+Col],
  			    Width) == GIF_ERROR) {
--	      PrintGifError();
+-	      //PrintGifError();
 +#if GIFLIB_MAJOR >= 5
 +	      ExactImagePrintGifError(GifFile->Error);
 +#else
@@ -79,11 +79,11 @@
  	      return false;
  	    }
  	  }
-@@ -113,7 +147,11 @@ int GIFCodec::readImage (std::istream* s
+@@ -113,7 +147,11 @@ int GIFCodec::readImage (std::istream* stream, Image& 
  	for (int i = 0; i < Height; ++i) {
  	  if (DGifGetLine(GifFile, &image.getRawData()[Row++ * image.stride()+Col],
  			  Width) == GIF_ERROR) {
--	    PrintGifError();
+-	    //PrintGifError();
 +#if GIFLIB_MAJOR >= 5
 +	    ExactImagePrintGifError(GifFile->Error);
 +#else
@@ -92,11 +92,11 @@
  	    return false;
  	  }
  	}
-@@ -122,12 +160,20 @@ int GIFCodec::readImage (std::istream* s
+@@ -122,12 +160,20 @@ int GIFCodec::readImage (std::istream* stream, Image& 
      case EXTENSION_RECORD_TYPE:
        /* Skip any extension blocks in file: */
        if (DGifGetExtension(GifFile, &ExtCode, &Extension) == GIF_ERROR) {
--	PrintGifError();
+-	//PrintGifError();
 +#if GIFLIB_MAJOR >= 5
 +	ExactImagePrintGifError(GifFile->Error);
 +#else
@@ -104,9 +104,9 @@
 +#endif
  	return false;
        }
-       while (Extension != NULL) {
+       while (Extension != 0) {
  	if (DGifGetExtensionNext(GifFile, &Extension) == GIF_ERROR) {
--	  PrintGifError();
+-	  //PrintGifError();
 +#if GIFLIB_MAJOR >= 5
 +	  ExactImagePrintGifError(GifFile->Error);
 +#else
@@ -115,74 +115,62 @@
  	  return false;
  	}
        }
-@@ -155,7 +201,11 @@ int GIFCodec::readImage (std::istream* s
+@@ -155,7 +201,11 @@ int GIFCodec::readImage (std::istream* stream, Image& 
    // convert colormap to our 16bit "TIFF"format
    colorspace_de_palette (image, ColorMap->ColorCount, rmap, gmap, bmap);
    
 +#if GIFLIB_MAJOR == 5 && GIFLIB_MINOR >= 1 || GIFLIB_MAJOR > 5
-+  EGifCloseFile(GifFile, NULL);
+   EGifCloseFile(GifFile, &GifError);
 +#else
-   EGifCloseFile(GifFile);
++  EGifCloseFile(GifFile);
 +#endif
  
    return true;
  }
-@@ -166,7 +216,11 @@ bool GIFCodec::writeImage (std::ostream*
-   GifFileType* GifFile;
+@@ -167,7 +217,11 @@ bool GIFCodec::writeImage (std::ostream* stream, Image
    GifByteType* Ptr;
+   int GifError;
    
 +#if GIFLIB_MAJOR >= 5
-+  if ((GifFile = EGifOpen (stream, &GIFOutputFunc, NULL)) == NULL)
+   if ((GifFile = EGifOpen(stream, &GIFOutputFunc, &GifError)) == 0)
 +#else
-   if ((GifFile = EGifOpen (stream, &GIFOutputFunc)) == NULL)
++  if ((GifFile = EGifOpen(stream, &GIFOutputFunc)) == 0)
 +#endif
      {
        std::cerr << "Error preparing GIF file for writing." << std::endl;
        return false;
-@@ -175,7 +229,11 @@ bool GIFCodec::writeImage (std::ostream*
+@@ -176,7 +230,11 @@ bool GIFCodec::writeImage (std::ostream* stream, Image
    int ColorMapSize = 256;
    
    // later use our own colormap generation
 +#if GIFLIB_MAJOR >= 5
-+  ColorMapObject* OutputColorMap = GifMakeMapObject(ColorMapSize, NULL);
+   ColorMapObject* OutputColorMap = GifMakeMapObject(ColorMapSize, 0);
 +#else
-   ColorMapObject* OutputColorMap = MakeMapObject(ColorMapSize, NULL);
++  ColorMapObject* OutputColorMap = MakeMapObject(ColorMapSize, 0);
 +#endif
    if (!OutputColorMap)
      return false;
    
-@@ -203,7 +261,11 @@ bool GIFCodec::writeImage (std::ostream*
+@@ -204,7 +262,11 @@ bool GIFCodec::writeImage (std::ostream* stream, Image
    }
     
    
 +#if GIFLIB_MAJOR >= 5
-+  if (GifQuantizeBuffer(image.w, image.h, &ColorMapSize,
+   if (GifQuantizeBuffer(image.w, image.h, &ColorMapSize,
 +#else
-   if (QuantizeBuffer(image.w, image.h, &ColorMapSize,
++  if (QuantizeBuffer(image.w, image.h, &ColorMapSize,
 +#endif
  		     RedBuffer, GreenBuffer, BlueBuffer,
  		     OutputBuffer, OutputColorMap->Colors) == GIF_ERROR) {
      return false;
-@@ -215,7 +277,7 @@ bool GIFCodec::writeImage (std::ostream*
-   if (EGifPutScreenDesc(GifFile, image.w, image.h,
- 			ColorMapSize, 0, OutputColorMap) == GIF_ERROR ||
-       EGifPutImageDesc(GifFile, 0, 0, image.w, image.h,
--		       FALSE, NULL) == GIF_ERROR)
-+		       false, NULL) == GIF_ERROR)
-     {
-       std::cerr << "Error writing GIF header." << std::endl;
-       return false;
-@@ -232,9 +294,13 @@ bool GIFCodec::writeImage (std::ostream*
-   }
-   free (OutputBuffer);
+@@ -235,7 +297,11 @@ bool GIFCodec::writeImage (std::ostream* stream, Image
  
--  delete (RedBuffer); delete (GreenBuffer); delete (BlueBuffer);
-+  delete[] RedBuffer; delete[] GreenBuffer; delete[] BlueBuffer;
+   delete[] RedBuffer; delete[] GreenBuffer; delete[] BlueBuffer;
  
 +#if GIFLIB_MAJOR == 5 && GIFLIB_MINOR >= 1 || GIFLIB_MAJOR > 5
-+  EGifCloseFile(GifFile, NULL);
+   EGifCloseFile(GifFile, &GifError);
 +#else
-   EGifCloseFile(GifFile);
++  EGifCloseFile(GifFile);
 +#endif
    return true;
  }
