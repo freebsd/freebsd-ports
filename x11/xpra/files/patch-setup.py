6376be15bba83213caddc5c5151205f415a8efd6
Steve Wills <swills@FreeBSD.org>
2019-05-03 21:32:50 +0000

    x11/xpra: update to 2.5.1
    While here, clean up a bit

--- x11/xpra/files/patch-setup.py
+++ x11/xpra/files/patch-setup.py
@@ -1,21 +1,21 @@
---- setup.py.orig	2018-10-14 16:59:57 UTC
+--- setup.py.orig	2019-03-19 15:05:07 UTC
 +++ setup.py
-@@ -1508,12 +1508,12 @@ else:
-         if libexec_scripts:
+@@ -1551,12 +1551,12 @@ else:
              add_data_files("%s/xpra/" % libexec, libexec_scripts)
-     man_path = "share/man"
--    if OPENBSD:
-+    if OPENBSD or FREEBSD:
-         man_path = "man"
-     add_data_files("%s/man1" % man_path,  ["man/xpra.1", "man/xpra_launcher.1", "man/xpra_browser.1"])
-     add_data_files("share/applications",  ["xdg/xpra-shadow.desktop", "xdg/xpra-launcher.desktop", "xdg/xpra-browser.desktop", "xdg/xpra.desktop"])
-     add_data_files("share/mime/packages", ["xdg/application-x-xpraconfig.xml"])
--    add_data_files("share/icons",         ["xdg/xpra.png", "xdg/xpra-mdns.png", "xdg/xpra-shadow.png"])
-+    add_data_files("share/pixmaps",       ["xdg/xpra.png", "xdg/xpra-mdns.png", "xdg/xpra-shadow.png"])
-     add_data_files("share/appdata",       ["xdg/xpra.appdata.xml"])
+     if data_ENABLED:
+         man_path = "share/man"
+-        if OPENBSD:
++        if OPENBSD or FREEBSD:
+             man_path = "man"
+         add_data_files("%s/man1" % man_path,  ["man/xpra.1", "man/xpra_launcher.1"])
+         add_data_files("share/applications",  glob.glob("xdg/*.desktop"))
+         add_data_files("share/mime/packages", ["xdg/application-x-xpraconfig.xml"])
+-        add_data_files("share/icons",         ["xdg/xpra.png", "xdg/xpra-mdns.png", "xdg/xpra-shadow.png"])
++        add_data_files("share/pixmaps",       ["xdg/xpra.png", "xdg/xpra-mdns.png", "xdg/xpra-shadow.png"])
+         add_data_files("share/appdata",       ["xdg/xpra.appdata.xml"])
  
      #here, we override build and install so we can
-@@ -1562,7 +1562,7 @@ else:
+@@ -1605,7 +1605,7 @@ else:
  
              if printing_ENABLED and POSIX:
                  #install "/usr/lib/cups/backend" with 0700 permissions:
@@ -24,14 +24,14 @@
  
              if x11_ENABLED:
                  #install xpra_Xdummy if we need it:
-@@ -1634,8 +1634,8 @@ else:
-         add_packages("xpra.platform.xposix")
+@@ -1678,8 +1678,8 @@ else:
          remove_packages("xpra.platform.win32", "xpra.platform.darwin")
-         #not supported by all distros, but doesn't hurt to install them anyway:
--        for x in ("tmpfiles.d", "sysusers.d"):
--            add_data_files("lib/%s" % x, ["%s/xpra.conf" % x])
-+#        for x in ("tmpfiles.d", "sysusers.d"):
-+#            add_data_files("lib/%s" % x, ["%s/xpra.conf" % x])
-         if uinput_ENABLED:
-             add_data_files("lib/udev/rules.d/", ["udev/rules.d/71-xpra-virtual-pointer.rules"])
+         if data_ENABLED:
+             #not supported by all distros, but doesn't hurt to install them anyway:
+-            for x in ("tmpfiles.d", "sysusers.d"):
+-                add_data_files("lib/%s" % x, ["%s/xpra.conf" % x])
++#            for x in ("tmpfiles.d", "sysusers.d"):
++#                add_data_files("lib/%s" % x, ["%s/xpra.conf" % x])
+             if uinput_ENABLED:
+                 add_data_files("lib/udev/rules.d/", ["udev/rules.d/71-xpra-virtual-pointer.rules"])
  
--- x11/xpra/files/patch-xpra_scripts_config.py
+++ x11/xpra/files/patch-xpra_scripts_config.py
@@ -1,6 +1,6 @@
---- xpra/scripts/config.py.orig	2018-12-28 02:45:20 UTC
+--- xpra/scripts/config.py.orig	2019-04-16 04:57:10 UTC
 +++ xpra/scripts/config.py
-@@ -123,7 +123,6 @@ def detect_xvfb_command(conf_dir="/etc/xpra/", bin_dir
+@@ -126,7 +126,6 @@ def detect_xvfb_command(conf_dir="/etc/xpra/", bin_dir
      if OSX:
          return get_Xvfb_command()
      if sys.platform.find("bsd")>=0 and Xdummy_ENABLED is None:
