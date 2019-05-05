bc22c4bbfd5d0e651f90998e347485972d66a5b1
amdmi3 <amdmi3@FreeBSD.org>
2019-05-05 09:01:29 +0000

    - Add LICENSE - Pet portlint - Regenerate patches - Update WWW
    Approved by:	portmgr blanket

--- emulators/parallels-tools/files/patch-pvmnet-if_pvmnet.c
+++ emulators/parallels-tools/files/patch-pvmnet-if_pvmnet.c
@@ -1,6 +1,6 @@
---- pvmnet/if_pvmnet.c.orig	2014-10-02 22:19:21.000000000 +0400
-+++ pvmnet/if_pvmnet.c	2014-10-02 22:21:10.000000000 +0400
-@@ -364,12 +364,20 @@
+--- pvmnet/if_pvmnet.c.orig	2014-02-14 14:09:06 UTC
++++ pvmnet/if_pvmnet.c
+@@ -364,12 +364,20 @@ pvmnetintr(void *arg)
  		for (offset = io_get_rcv_offset(sc);
  		    (size = io_get_packet_size(sc, offset)); offset += FULL_PACKET_SIZE(size)) {
  			if (size > ETHER_MAX_LEN) {
@@ -21,7 +21,7 @@
  				continue;
  			}
  			m->m_pkthdr.rcvif = ifp;
-@@ -379,7 +387,11 @@
+@@ -379,7 +387,11 @@ pvmnetintr(void *arg)
  			m->m_pkthdr.len = m->m_len = size;
  			io_read_data(sc, offset, m);
  
@@ -33,7 +33,7 @@
  
  			PVMNET_UNLOCK(sc);
  			(*ifp->if_input)(ifp, m);
-@@ -538,7 +550,11 @@
+@@ -538,7 +550,11 @@ pvmnet_start_locked(struct ifnet *ifp)
  			io_notify_sndbuf_full(sc);
  			break;
  		}
