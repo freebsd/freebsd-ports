494c4e584f2f58d7ced8e315a8b563687cd12c7b
Kurt Jaeger <pi@FreeBSD.org>
2019-05-05 05:57:18 +0000

    graphics/qgis-ltr: upgrade 3.4.5 -> 3.4.7
    - Update several Python dependencies
    - Add USE_PYQT=webkitwidgets for plugins like 'OpenLayers Plugin'
    - Add USES=gnome, USE_GNOME=libxml2, USE_QT=gamepad, and
      LIB_DEPENDS=libsz.so:science/szip to pet 'make DEVELOPER=yes'
    - Add OPTIONS_DEFAULT=SERVER
    - Change
      files/patch-python_plugins_processing_algs_saga_SagaAlgorithmProvider.py
      to work with REQUIRED_VERSION
    - Remove files/patch-python_plugins_processing_algs_saga_SagaUtils.py
      and files/patch-src_app_main.cpp, after they are upstream now
    - Remove 'world writeable rights' in pkg-plist (not necessary)
    - Update pkg-plist
    
    PR:		237685
    Submitted by:	Rainer Hurling <rhurlin@gwdg.de> (maintainer)

--- graphics/qgis-ltr/files/patch-python_plugins_processing_algs_saga_SagaAlgorithmProvider.py
+++ graphics/qgis-ltr/files/patch-python_plugins_processing_algs_saga_SagaAlgorithmProvider.py
@@ -1,11 +1,20 @@
---- python/plugins/processing/algs/saga/SagaAlgorithmProvider.py.orig	2018-11-23 12:08:36 UTC
+--- python/plugins/processing/algs/saga/SagaAlgorithmProvider.py.orig	2019-03-02 02:07:46 UTC
 +++ python/plugins/processing/algs/saga/SagaAlgorithmProvider.py
+@@ -82,7 +82,7 @@ class SagaAlgorithmProvider(QgsProcessingProvider):
+ 
+     def canBeActivated(self):
+         version = SagaUtils.getInstalledVersion(True)
+-        if version is not None and version.startswith(REQUIRED_VERSION):
++        if version is not None and version >= REQUIRED_VERSION:
+             return True
+         return False
+ 
 @@ -93,7 +93,7 @@ class SagaAlgorithmProvider(QgsProcessingProvider):
                                       self.tr('Processing'), Qgis.Critical)
              return
  
 -        if not version.startswith(REQUIRED_VERSION):
-+        if version < '2.3':
++        if version < REQUIRED_VERSION:
              QgsMessageLog.logMessage(self.tr('Problem with SAGA installation: unsupported SAGA version (found: {}, required: {}).').format(version, REQUIRED_VERSION),
                                       self.tr('Processing'),
                                       Qgis.Critical)
