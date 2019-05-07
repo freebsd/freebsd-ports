7a16d47d11832824f2711358624518fd034252f4
Yuri Victorovich <yuri@FreeBSD.org>
2019-05-07 05:51:28 +0000

    audio/linuxsampler: Update 2.0.0 -> 2.1.0
    * Change to option helpers
    * Update http -> https
    
    Reported by:	protonlucifer <protonlucifer@protonmail.com>

--- audio/linuxsampler/files/patch-src_common_Mutex.cpp
+++ audio/linuxsampler/files/patch-src_common_Mutex.cpp
@@ -1,10 +1,10 @@
---- src/common/Mutex.cpp.orig	2007-11-14 23:42:15 UTC
+--- src/common/Mutex.cpp.orig	2019-05-07 05:18:11 UTC
 +++ src/common/Mutex.cpp
-@@ -36,7 +36,6 @@
+@@ -40,7 +40,6 @@
  #if !defined(WIN32)
- #if !defined(_XOPEN_SOURCE) || _XOPEN_SOURCE < 500
- # undef _XOPEN_SOURCE
--# define _XOPEN_SOURCE 500 /* to define PTHREAD_MUTEX_ERRORCHECK */
- # warning "Seems you don't have a UNIX98 compatible system."
- # warning "Please run LinuxSampler's selftest to make sure this won't be a problem!"
- # warning "(compile tests with 'make tests', run them with 'src/testcases/linuxsamplertest')"
+ # if !defined(_XOPEN_SOURCE) || _XOPEN_SOURCE < 500
+ #  undef _XOPEN_SOURCE
+-#  define _XOPEN_SOURCE 500 /* to define PTHREAD_MUTEX_RECURSIVE */
+ #  if (!defined(POSIX_C_SOURCE) || POSIX_C_SOURCE < 199801L) && !__DARWIN_UNIX03
+ #   warning "Seems you don't have a UNIX98 compatible system."
+ #   warning "Please run LinuxSampler's selftest to make sure this won't be a problem!"
