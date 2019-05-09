a3e2e77ad564fd605d71e6ff244a64ca3e679045
rene <rene@FreeBSD.org>
2019-05-09 15:03:38 +0000

    Remove expired ports: 2019-05-08 databases/ruby-odbc: Broken for more than 6 months 2019-05-08 databases/rubygem-dbd-odbc: Broken for more than 6 months 2019-05-08 devel/dlangui: Broken for more than 6 months 2019-05-08 editors/dlangide: Broken for more than 6 months 2019-05-08 emulators/desmume: Broken for more than 6 months 2019-05-08 emulators/yabause: Broken for more than 6 months 2019-05-08 emulators/yape: Broken for more than 6 months 2019-05-08 games/armagetron: Broken for more than 6 months 2019-05-08 games/boswars: Broken for more than 6 months 2019-05-08 games/ceferino: Broken for more than 6 months 2019-05-08 games/chanta: Broken for more than 6 months 2019-05-08 games/d2x-xl: Broken for more than 6 months 2019-05-08 games/drcreep: Broken for more than 6 months 2019-05-08 games/frobtads: Broken for more than 6 months 2019-05-08 games/paintown: Broken for more than 6 months 2019-05-08 games/pykawari: Broken for more than 6 months 2019-05-08 games/stepmania-devel: Broken for more than 6 months 2019-05-08 games/tinymux: Broken for more than 6 months 2019-05-08 games/voxelands: Broken for more than 6 months 2019-05-08 games/voxelands-server: Broken for more than 6 months 2019-05-08 games/warsow: Broken for more than 6 months 2019-05-08 graphics/appleseed: Broken for more than 6 months 2019-05-08 graphics/apvlv: Broken for more than 6 months 2019-05-08 graphics/qslim: Broken for more than 6 months 2019-05-08 graphics/rawstudio: Broken for more than 6 months 2019-05-08 graphics/tulip: Broken for more than 6 months 2019-05-08 lang/qore: Broken for more than 6 months 2019-05-08 mail/milter-manager: Broken for more than 6 months 2019-05-08 math/goblin: Broken for more than 6 months 2019-05-08 math/mosesdecoder: Broken for more than 6 months 2019-05-08 multimedia/asdcplib: Broken for more than 6 months 2019-05-08 net/crtmpserver: Broken for more than 6 months 2019-05-08 net/linuxigd: Abandonware; use net/miniupnpd instead 2019-05-08 net/openafs: Broken for more than 6 months 2019-05-08 security/quantis: Broken for more than 6 months 2019-05-08 sysutils/boxbackup: Broken for more than 6 months 2019-05-08 sysutils/grub2-efi: Broken for more than 6 months 2019-05-08 sysutils/grub2-pcbsd: Broken for more than 6 months 2019-05-08 sysutils/mdcp: Broken for more than 6 months 2019-05-08 sysutils/sbsigntool: Broken for more than 6 months 2019-05-08 www/py-cherrypy-old: Lates version is in tree and no dependent ports

--- mail/milter-manager/files/patch-binding_ruby_test_run-test.sh
+++
@@ -1,8 +0,0 @@
---- binding/ruby/test/run-test.sh.orig	2015-06-15 05:13:40 UTC
-+++ binding/ruby/test/run-test.sh
-@@ -1,4 +1,4 @@
--#!/bin/bash
-+#!/bin/sh
- #
- # Copyright (C) 2011  Kouhei Sutou <kou@clear-code.com>
- #
--- net/linuxigd/files/patch-gatedevice.c
+++
@@ -1,8 +0,0 @@
---- gatedevice.c.orig	2010-11-26 17:54:07.000000000 +0300
-+++ gatedevice.c	2010-11-26 17:55:02.000000000 +0300
-@@ -1,3 +1,5 @@
-+#include <stdio.h>
-+#include <string.h>
- #include <syslog.h>
- #include <stdlib.h>
- #include <upnp/ixml.h>
