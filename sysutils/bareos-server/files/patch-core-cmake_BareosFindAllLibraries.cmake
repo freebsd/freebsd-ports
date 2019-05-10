b08635660d11a90edf826c5d7dceb55426eb0358
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2019-05-10 20:19:42 +0000

    - Fix pthread error [1] - Fix build when python 2.x is not default version [2] - Fix build when NLS option is not selected [3] - Fix pkg-plist - Few other modifications
    PR:		237818 237823 237825
    Submitted by:	Trond.Endrestol _ at _ ximalas.info [1][3], beldin _ at _ beldin.org [2]
    Reported by:	pkg-fallout

---
+++ sysutils/bareos-server/files/patch-core-cmake_BareosFindAllLibraries.cmake
@@ -0,0 +1,24 @@
+--- core/cmake/BareosFindAllLibraries.cmake	2019-05-10 00:27:21.421777000 -0500
++++ core/cmake/BareosFindAllLibraries.cmake	2019-05-10 00:28:47.016424000 -0500
+@@ -25,13 +25,15 @@
+ 
+ 
+ if (NOT ${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+-   # make sure we get python 2 not 3
+-   set(Python_ADDITIONAL_VERSIONS 2.5 2.6 2.7 2.8 2.9)
+-   find_package(PythonInterp)
+-   INCLUDE(FindPythonLibs)
++   if (python)
++     # make sure we get python 2 not 3
++     set(Python_ADDITIONAL_VERSIONS 2.5 2.6 2.7 2.8 2.9)
++     find_package(PythonInterp)
++     INCLUDE(FindPythonLibs)
+ 
+-   if (${PYTHONLIBS_FOUND})
+-      SET(HAVE_PYTHON 1)
++     if (${PYTHONLIBS_FOUND})
++        SET(HAVE_PYTHON 1)
++     endif()
+    endif()
+ 
+    INCLUDE(FindPostgreSQL)
