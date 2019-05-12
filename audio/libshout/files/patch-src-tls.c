f96992d253e6d1a41a3422cdc1d8661348387914
sunpoet <sunpoet@FreeBSD.org>
2019-05-12 16:33:58 +0000

    Update to 2.4.2
    Changes:	https://icecast.org/news/libshout-release-2_4_2/

--- audio/libshout/files/patch-src-tls.c
+++ audio/libshout/files/patch-src-tls.c
@@ -1,18 +1,18 @@
---- src/tls.c.orig	2015-06-18 04:27:45 UTC
+--- src/tls.c.orig	2019-04-29 10:00:11 UTC
 +++ src/tls.c
-@@ -63,10 +63,15 @@ static inline int tls_setup(shout_tls_t 
+@@ -88,10 +88,15 @@ static inline int tls_setup(shout_tls_t 
  {
- 	SSL_METHOD *meth;
+     SSL_METHOD *meth;
  
 +#if OPENSSL_VERSION_NUMBER < 0x10100000L
- 	SSL_library_init();
- 	SSL_load_error_strings();
- 	SSLeay_add_all_algorithms();
-  	SSLeay_add_ssl_algorithms();
+     SSL_library_init();
+     SSL_load_error_strings();
+     SSLeay_add_all_algorithms();
+     SSLeay_add_ssl_algorithms();
 +#else
-+	OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS, NULL);
-+	OPENSSL_init_crypto(OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
++    OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS, NULL);
++    OPENSSL_init_crypto(OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
 +#endif
  
- 	meth = TLSv1_client_method();
- 	if (!meth)
+     meth = TLSv1_client_method();
+     if (!meth)
