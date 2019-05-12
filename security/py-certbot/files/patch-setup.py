b439b5be8d6a99eb6218c350047b6522eefe11f4
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-05-12 16:38:53 +0000

    Update to 0.34.2
    Changes:	https://github.com/certbot/certbot/blob/master/CHANGELOG.md

--- security/py-certbot/files/patch-setup.py
+++ security/py-certbot/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2019-04-04 22:02:09 UTC
+--- setup.py.orig	2019-05-07 19:17:33 UTC
 +++ setup.py
 @@ -43,7 +43,6 @@ install_requires = [
      # 1.1.0+ is required to avoid the warnings described at
@@ -8,8 +8,8 @@
      'parsedatetime>=1.3',  # Calendar.parseDT
      'pyrfc3339',
      'pytz',
-@@ -57,6 +56,7 @@ dev_extras = [
-     'astroid==1.3.5',
+@@ -56,6 +55,7 @@ dev_extras = [
+     'astroid==1.6.5',
      'coverage',
      'ipdb',
 +    'mock',
