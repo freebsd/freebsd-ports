a4c89c7bee7452475435a7eb8dd23a331276c2b4
swills <swills@FreeBSD.org>
2019-05-14 13:38:50 +0000

    audio/audacity: update to 2.3.2
    While here, improve formatting
    
    PR:		237891
    Submitted by:	Jack <xxjack12xx@gmail.com> (maintainer)

--- audio/audacity/files/patch-src_MemoryX.h
+++ audio/audacity/files/patch-src_MemoryX.h
@@ -1,6 +1,6 @@
 --- src/MemoryX.h.orig	2019-03-09 11:55:09 UTC
 +++ src/MemoryX.h
-@@ -628,7 +628,7 @@ make_iterator_range( const Container &container )
+@@ -641,7 +641,7 @@ make_iterator_range( const Container &container )
  // For using std::unordered_map on wxString
  namespace std
  {
