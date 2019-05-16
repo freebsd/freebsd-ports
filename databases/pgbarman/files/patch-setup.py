2cc1d3ddbcafcf0acdf9910d0492c35f96596054
Matthew Seaman <matthew@FreeBSD.org>
2019-05-16 15:33:32 +0000

    Update to 2.8
    Regenerate patches
    
    ChangeLog: https://sourceforge.net/projects/pgbarman/files/2.8/

--- databases/pgbarman/files/patch-setup.py
+++ databases/pgbarman/files/patch-setup.py
@@ -1,13 +1,17 @@
---- setup.py.orig	2016-09-23 15:43:57 UTC
+--- setup.py.orig	2019-05-14 16:37:28 UTC
 +++ setup.py
-@@ -69,8 +69,8 @@ setup(
-     packages=['barman', ],
-     scripts=['bin/barman', ],
+@@ -71,10 +71,10 @@ setup(
+     url='http://www.pgbarman.org/',
+     packages=find_packages(exclude=["tests"]),
      data_files=[
--        ('share/man/man1', ['doc/barman.1']),
+-        ('share/man/man1', ['doc/barman.1',
+-                            'doc/barman-wal-archive.1',
+-                            'doc/barman-wal-restore.1']),
 -        ('share/man/man5', ['doc/barman.5']),
-+        ('man/man1', ['doc/barman.1']),
++        ('man/man1', ['doc/barman.1',
++                      'doc/barman-wal-archive.1',
++                      'doc/barman-wal-restore.1']),
 +        ('man/man5', ['doc/barman.5']),
      ],
-     license='GPL-3.0',
-     description=__doc__.split("\n")[0],
+     entry_points={
+         'console_scripts': [
