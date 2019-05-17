bba9bc0881fa860088a4f59556c4c4a3b0253205
Jan Beich <jbeich@FreeBSD.org>
2019-05-17 12:02:08 +0000

    www/firefox: restore SafeBrowsing v4 after r495403
    Required for phishing protection. Not covered by uBlock Origin lists.
    Looks like #Mozilla didn't announce the POLA-violating change anywhere
    public despite backporting to ESR60.
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1531176

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -236,7 +236,8 @@ MOZ_OPTIONS+=	--with-system-zlib		\
 # http://www.chromium.org/developers/how-tos/api-keys
 # Note: these are for FreeBSD use ONLY. For your own distribution,
 # please get your own set of keys.
-MOZ_EXPORT+=	MOZ_GOOGLE_API_KEY=AIzaSyBsp9n41JLW8jCokwn7vhoaMejDFRd1mp8
+MOZ_EXPORT+=	MOZ_GOOGLE_LOCATION_SERVICE_API_KEY=AIzaSyBsp9n41JLW8jCokwn7vhoaMejDFRd1mp8
+MOZ_EXPORT+=	MOZ_GOOGLE_SAFEBROWSING_API_KEY=AIzaSyBsp9n41JLW8jCokwn7vhoaMejDFRd1mp8
 
 .if ${PORT_OPTIONS:MGTK2}
 MOZ_TOOLKIT=	cairo-gtk2
