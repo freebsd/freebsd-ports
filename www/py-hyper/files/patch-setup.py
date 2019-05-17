03fa42d47c191dfd8b5a2c772721a264027fbd97
Kubilay Kocak <koobs@FreeBSD.org>
2019-05-17 12:43:14 +0000

    wwww/py-hyper: Update to 0.8.0.dev0 (g20171217)
    Latest PyPI released (0.7.0) fails several tests that have been resolved
    in branch:development but not yet released.
    
      - Add patches to make hyper work with h2 > 3.0 [1]
    
    Changelog:
    
      https://github.com/Lukasa/hyper/blob/development/HISTORY.rst
    
    [1] https://github.com/Lukasa/hyper/pull/402

--- www/py-hyper/files/patch-setup.py
+++ www/py-hyper/files/patch-setup.py
@@ -1,44 +1,11 @@
---- setup.py.orig	2015-10-28 10:21:14 UTC
+--- setup.py.orig	2019-05-17 11:59:54 UTC
 +++ setup.py
-@@ -7,6 +7,7 @@ import sys
- 
- try:
-     from setuptools import setup
-+    from setuptools.command.test import test as TestCommand
- except ImportError:
-     from distutils.core import setup
- 
-@@ -48,6 +49,24 @@ def resolve_install_requires():
-         return deps
-     return []
- 
-+class PyTest(TestCommand):
-+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-+
-+    def initialize_options(self):
-+        TestCommand.initialize_options(self)
-+        self.pytest_args = []
-+
-+    def finalize_options(self):
-+        TestCommand.finalize_options(self)
-+        self.test_args = []
-+        self.test_suite = True
-+
-+    def run_tests(self):
-+        #import here, cause outside the eggs aren't loaded
-+        import pytest
-+        errno = pytest.main(self.pytest_args)
-+        sys.exit(errno)
-+
- packages = [
-     'hyper',
-     'hyper.http20',
-@@ -90,5 +109,7 @@ setup(
-     },
-     extras_require={
-         'fast': ['pycohttpparser'],
--    }
-+    },
-+    tests_require=['pytest'],
-+    cmdclass = {'test': PyTest},
- )
+@@ -77,7 +77,7 @@ setup(
+         'Programming Language :: Python :: Implementation :: CPython',
+     ],
+     install_requires=[
+-        'h2>=2.4,<3.0,!=2.5.0', 'hyperframe>=3.2,<4.0', 'rfc3986>=1.1.0,<2.0', 'brotlipy>=0.7.0,<1.0'
++        'h2>=2.4,!=2.5.0', 'hyperframe>=3.2', 'rfc3986>=1.1.0,<2.0', 'brotlipy>=0.7.0,<1.0'
+     ],
+     tests_require=['pytest', 'requests', 'mock'],
+     cmdclass={'test': PyTest},
