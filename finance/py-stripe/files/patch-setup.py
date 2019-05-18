ccf33088f3828c99201e5686287fd9f687a6bb57
Kubilay Kocak <koobs@FreeBSD.org>
2019-05-18 07:48:03 +0000

    finance/py-stripe: Update to 2.28.0
    Changelog:
    
      https://github.com/stripe/stripe-python/blob/v2.28.0/CHANGELOG.md

--- finance/py-stripe/files/patch-setup.py
+++ finance/py-stripe/files/patch-setup.py
@@ -1,24 +1,17 @@
---- setup.py.orig	2016-03-03 09:42:16 UTC
+# pytest-cov is not a compulsory test dependency
+# TODO: Upstream
+
+--- setup.py.orig	2019-04-08 18:44:53 UTC
 +++ setup.py
-@@ -28,6 +28,11 @@ if sys.version_info < (2, 6):
- else:
-     install_requires.append('requests >= 0.8.8')
- 
-+tests_require = [
-+    'unittest2',
-+    'pycurl>=7.19',
-+    'mock',
-+]
- 
- with open('LONG_DESCRIPTION.rst') as f:
-     long_description = f.read()
-@@ -56,8 +61,8 @@ setup(
-     packages=['stripe', 'stripe.test', 'stripe.test.resources'],
-     package_data={'stripe': ['data/ca-certificates.crt']},
-     install_requires=install_requires,
-+    tests_require=tests_require,
-     test_suite='stripe.test.all',
--    tests_require=['unittest2', 'mock'],
-     use_2to3=True,
-     classifiers=[
-         "Development Status :: 5 - Production/Stable",
+@@ -54,8 +54,10 @@ setup(
+         "pytest >= 3.4",
+         "pytest-mock >= 1.7",
+         "pytest-xdist >= 1.22",
+-        "pytest-cov >= 2.5",
+     ],
++    extras_require={
++        'dev': ['pytest-cov >= 2.5'],
++    },
+     cmdclass={"test": PyTest},
+     project_urls={
+         "Bug Tracker": "https://github.com/stripe/stripe-python/issues",
