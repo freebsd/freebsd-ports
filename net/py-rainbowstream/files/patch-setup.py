f1bb9d14019f1cea2125efe8e38d66f14fdfec68
Kubilay Kocak <koobs@FreeBSD.org>
2019-05-18 10:10:50 +0000

    net/py-rainbowstream: Update to 1.4.0

--- net/py-rainbowstream/files/patch-setup.py
+++ net/py-rainbowstream/files/patch-setup.py
@@ -1,9 +1,9 @@
 # Make pocket actually optional
 # https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=220312
 
---- setup.py.orig	2016-08-16 14:49:45 UTC
+--- setup.py.orig	2018-08-30 11:36:46 UTC
 +++ setup.py
-@@ -15,14 +15,17 @@ version = '1.3.5'
+@@ -15,14 +15,17 @@ version = '1.4.0'
  install_requires = [
      "python-dateutil",
      "arrow",
@@ -21,9 +21,18 @@
 +}
 +
  # Default user (considers non virtualenv method)
- user = os.environ.get('SUDO_USER', os.environ['USER'])
+ user = os.environ.get('SUDO_USER', os.environ.get('USER', None))
  
-@@ -65,6 +68,7 @@ setup(name='rainbowstream',
+@@ -41,7 +44,7 @@ if not os.path.isfile(default):
+ setup(name='rainbowstream',
+       version=version,
+       description="A smart and nice Twitter client on terminal.",
+-      long_description=open("./README.rst", "r").read(),
++      long_description=open("./README.rst", "rb").read().decode("utf-8"),
+       classifiers=[
+           "Development Status :: 5 - Production/Stable",
+           "Environment :: Console",
+@@ -68,6 +71,7 @@ setup(name='rainbowstream',
        include_package_data=True,
        zip_safe=True,
        install_requires=install_requires,
