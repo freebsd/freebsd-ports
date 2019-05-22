73eb992926b876b24be7a81665f5fe81281ec88c
Jason W. Bacon <jwb@FreeBSD.org>
2019-05-22 12:44:17 +0000

    science/afni: Upgrade to 19.1.10
    Rapid response from upstream on reported issue.  Additional patches to be
    upstreamed in next release.

---
+++ science/afni/files/patch-SUMA_gts_examples_happrox.c
@@ -0,0 +1,11 @@
+--- SUMA/gts/examples/happrox.c.orig	2019-05-21 18:01:41 UTC
++++ SUMA/gts/examples/happrox.c
+@@ -20,7 +20,7 @@
+ #include <math.h>
+ #include <stdlib.h>
+ #include <string.h>
+-#include <pgm.h>
++#include <netpbm/pgm.h>
+ #include "config.h"
+ #ifdef HAVE_GETOPT_H
+ #  include <getopt.h>
