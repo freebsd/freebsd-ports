cc8aeb88a0fffff5acf7bda9f0e2bd4251ea7a59
ler <ler@FreeBSD.org>
2019-05-22 14:38:58 +0000

    sysutils/lsof: fix patchfile from previous commit

--- sysutils/lsof/files/patch-dialects-freebsd-dlsof.h
+++ sysutils/lsof/files/patch-dialects-freebsd-dlsof.h
@@ -1,21 +1,15 @@
-Index: sysutils/lsof/files/patch-dialects-freebsd-dlsof.h
-===================================================================
---- /dev/null
-+++ sysutils/lsof/files/patch-dialects-freebsd-dlsof.h
-@@ -0,0 +1,16 @@
-+--- dialects/freebsd/dlsof.h.orig	2019-05-08 07:32:25 UTC
-++++ dialects/freebsd/dlsof.h
-+@@ -45,6 +45,13 @@
-+ #include <signal.h>
-+ #include <unistd.h>
-+ 
-++#if	FREEBSDV>=13000
-++/* This header is a huge mess.  Please don't support EOL FreeBSD releases. */
-++#define	_KERNEL	1
-++#include <sys/_lock.h>
-++#undef	_KERNEL
-++#endif		/* FREEBSDV>=13000 */
-++
-+ # if	FREEBSDV>=4000
-+ #  if	FREEBSDV>=5000
-+ #   if	FREEBSDV<6020
+--- dialects/freebsd/dlsof.h.orig	2019-05-22 14:37:20 UTC
++++ dialects/freebsd/dlsof.h
+@@ -45,6 +45,12 @@
+ #include <signal.h>
+ #include <unistd.h>
+ 
++#if    FREEBSDV>=13000
++/* This header is a huge mess.  Please don't support EOL FreeBSD releases. */
++#define        _KERNEL 1
++#include <sys/_lock.h>
++#undef _KERNEL
++#endif         /* FREEBSDV>=13000 */
+ # if	FREEBSDV>=4000
+ #  if	FREEBSDV>=5000
+ #   if	FREEBSDV<6020
