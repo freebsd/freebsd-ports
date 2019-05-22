1a6876d001752ed26a36f8c2d4aca5ca75eb9891
Kubilay Kocak <koobs@FreeBSD.org>
2019-05-22 08:27:09 +0000

    deskutils/py-bugwarrior: Update to 1.7.0
    Changelog:
    
      https://github.com/ralphbean/bugwarrior/blob/1.7.0/CHANGELOG.rst

--- deskutils/py-bugwarrior/files/patch-setup.py
+++ deskutils/py-bugwarrior/files/patch-setup.py
@@ -1,24 +1,11 @@
---- setup.py.orig	2017-05-25 15:38:59 UTC
+--- setup.py.orig	2019-05-22 04:44:47 UTC
 +++ setup.py
-@@ -38,7 +38,7 @@ setup(name='bugwarrior',
-           "dogpile.cache>=0.5.3",
-           "lockfile>=0.9.1",
-           "click",
--          "future!=0.16.0",
-+          "future",
-       ],
-       extras_require=dict(
-           keyring=["keyring", "dbus-python"],
-@@ -50,9 +50,11 @@ setup(name='bugwarrior',
-           bugzilla=["python-bugzilla"],
-       ),
-       tests_require=[
--          "Mock",
-+          "mock",
+@@ -54,6 +54,8 @@ setup(name='bugwarrior',
+           "Mock",
            "nose",
            "responses",
-+          "pypandoc",
-+          "pyac",
++           "pypandoc",
++           "pyac",
            "bugwarrior[jira]",
            "bugwarrior[megaplan]",
            "bugwarrior[activecollab]",
