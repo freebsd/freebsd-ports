f83106b6c1265d9fa396ec7162235eaeb50edb77
Baptiste Daroussin <bapt@FreeBSD.org>
2019-05-23 17:05:47 +0000

    Update to 0.7.0-81
    Upstream changes fixes a conflict with textproc/scim
    Some cleanups
    
    PR:		237839
    Approved by:	Samy Mahmoudi <samy.mahmoudi@gmail.com>

--- math/sc-im/files/patch-cmds__command.c
+++ math/sc-im/files/patch-cmds__command.c
@@ -1,6 +1,6 @@
---- cmds_command.c.orig	2018-08-03 06:04:39 UTC
+--- cmds_command.c.orig	2019-02-22 18:41:46 UTC
 +++ cmds_command.c
-@@ -826,7 +826,7 @@ void do_commandmode(struct block * sb) {
+@@ -853,7 +853,7 @@ void do_commandmode(struct block * sb) {
              exec_cmd(line);
  
          } else if ( inputline[0] == L'w' ) {
