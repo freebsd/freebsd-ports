67df7fbcc8b548923d141df8a6fe025c51a3c61d
tobik <tobik@FreeBSD.org>
2019-05-24 03:25:11 +0000

    lang/rust: Update to 1.35.0
    - Force rebuild all consumers to catch regressions early
    
    Thanks to Mikaël Urankar for providing updated bootstraps for
    aarch64, armv6, armv7, powerpc64.
    
    Changes:	https://blog.rust-lang.org/2019/05/23/Rust-1.35.0.html
    Reviewed by:	jbeich
    Tested by:	pkubaj (powerpc64), mikael.urankar@gmail.com (aarch64)
    With hat:	rust
    Differential Revision:	https://reviews.freebsd.org/D20332

--- lang/rust/files/patch-src_bootstrap_native.rs
+++ lang/rust/files/patch-src_bootstrap_native.rs
@@ -1,6 +1,28 @@
---- src/bootstrap/native.rs.orig	2019-04-08 12:42:31 UTC
+Avoid unnecessarily restarting the LLVM build and installing it
+into STAGEDIR during the install phase.
+
+--- src/bootstrap/native.rs.orig	2019-05-22 05:39:52 UTC
 +++ src/bootstrap/native.rs
-@@ -209,6 +209,10 @@ impl Step for Llvm {
+@@ -92,17 +92,9 @@ impl Step for Llvm {
+             .join(exe("llvm-config", &*builder.config.build));
+         let done_stamp = out_dir.join("llvm-finished-building");
+ 
+-        if let Some(llvm_commit) = llvm_info.sha() {
+             if done_stamp.exists() {
+-                let done_contents = t!(fs::read(&done_stamp));
+-
+-                // If LLVM was already built previously and the submodule's commit didn't change
+-                // from the previous build, then no action is required.
+-                if done_contents == llvm_commit.as_bytes() {
+-                    return build_llvm_config
+-                }
++                return build_llvm_config
+             }
+-        }
+ 
+         let _folder = builder.fold_output(|| "llvm");
+         let descriptor = if emscripten { "Emscripten " } else { "" };
+@@ -218,6 +210,10 @@ impl Step for Llvm {
              }
          }
  
@@ -11,3 +33,14 @@
          // http://llvm.org/docs/HowToCrossCompileLLVM.html
          if target != builder.config.build && !emscripten {
              builder.ensure(Llvm {
+@@ -283,9 +279,7 @@ impl Step for Llvm {
+ 
+         cfg.build();
+ 
+-        if let Some(llvm_commit) = llvm_info.sha() {
+-            t!(fs::write(&done_stamp, llvm_commit));
+-        }
++        t!(fs::write(&done_stamp, "done building LLVM"));
+ 
+         build_llvm_config
+     }
--- lang/rust/files/patch-vendor_libc_src_unix_bsd_freebsdlike_freebsd_x86.rs
+++
@@ -1,7 +0,0 @@
---- vendor/libc/src/unix/bsd/freebsdlike/freebsd/x86.rs.orig	2018-06-19 05:52:22 UTC
-+++ vendor/libc/src/unix/bsd/freebsdlike/freebsd/x86.rs
-@@ -1,3 +1,4 @@
-+pub type c_char = i8;
- pub type c_long = i32;
- pub type c_ulong = u32;
- pub type time_t = i32;
