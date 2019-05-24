658e110b8b2004b5ba98ae82f9d2c5235f0bf336
Kubilay Kocak <koobs@FreeBSD.org>
2019-05-24 10:00:13 +0000

    MFH: r499978 devel/py-posix_ipc: Update to 1.0.4
    Changelog:
    
      http://semanchuk.com/philip/posix_ipc/history.html
    
    Approved by: ports-secteam (bugfix release(s))

--- devel/py-posix_ipc/files/patch-setup.py
+++ devel/py-posix_ipc/files/patch-setup.py
@@ -1,18 +1,18 @@
---- setup.py.orig	2016-02-21 15:13:28 UTC
+--- setup.py.orig	2018-08-07 00:29:38 UTC
 +++ setup.py
-@@ -32,6 +32,7 @@ classifiers = [ "Development Status :: 5
-                 "Topic :: Utilities" ]
- license = "http://creativecommons.org/licenses/BSD/"
+@@ -39,6 +39,7 @@ license = "http://creativecommons.org/licenses/BSD/"
  keywords = "ipc inter-process communication semaphore shared memory shm message queue"
+ 
+ libraries = []
 +test_suite = "tests"
  
- libraries = [ ]
+ d = prober.probe()
  
-@@ -65,5 +66,6 @@ duc.setup(name = name,
-           classifiers = classifiers,
-           license = license,
-           keywords = keywords,
--          ext_modules = ext_modules
-+          ext_modules = ext_modules,
-+          test_suite = test_suite,
-          )
+@@ -69,5 +70,6 @@ distutools.setup(name=name,
+                  classifiers=classifiers,
+                  license=license,
+                  keywords=keywords,
+-                 ext_modules=ext_modules
++                 ext_modules=ext_modules,
++                 test_suite=test_suite
+                  )
