aabf55262cd29134b78975bbaf98f9004ab7db95
Tobias C. Berner <tcberner@FreeBSD.org>
2019-05-25 20:53:13 +0000

    MFH: r499101 r499116 r502628
    Fix build when using GCC. Needed by GCC architectures.
    
    Tested for no breakage on amd64 with Clang.
    
    PR:	235921
    Reviewed by:	tcberner
    Approved by:	tcberner (mentor)
    
    devel/qt5: Follow-up to r499101
    
    - Move the GCC related path fix into freebsd-g++/qmake.conf
      Otherwise the inclusion of the GCC path would break clang
      based systems when gcc-${GCC_DEFAULT} was installed.
    
    - This might break GCC architectures again, and if so, that
      will be fixed in a follow up commit.
    
    PR:		235921
    
    qt5: fix build on gcc architectures
    
    - the sed call was only replacing one of the %%LOCALBASE%% by ${LOCALBASE},
      due to the missing 'g' flag to the sed call.
    
    - with this change the gcc architectures should be able to build Qt5 again.
    
    PR:		237745
    Submitted by:	Mark Millard <marklmi26-fbsd@yahoo.com>, jwb
    Reported by:	pkubaj
    
    Approved by:	ports-secteam (joneum via irc)

--- Mk/Uses/qt-dist.mk
+++ Mk/Uses/qt-dist.mk
@@ -172,7 +172,8 @@ _EXTRA_PATCHES_QT5=	${PORTSDIR}/devel/${_QT_RELNAME}/files/extrapatch-mkspecs_fe
 			${PORTSDIR}/devel/${_QT_RELNAME}/files/extrapatch-mkspecs_common_bsd_bsd.conf
 .        if ${ARCH:Mmips*} || ${ARCH:Mpowerpc*} || ${ARCH} == sparc64
 _EXTRA_PATCHES_QT5+=	${PORTSDIR}/devel/${_QT_RELNAME}/files/extra-patch-mkspecs_common_g++-base.conf \
-			${PORTSDIR}/devel/${_QT_RELNAME}/files/extra-patch-mkspecs_common_gcc-base.conf
+			${PORTSDIR}/devel/${_QT_RELNAME}/files/extra-patch-mkspecs_common_gcc-base.conf \
+			${PORTSDIR}/devel/${_QT_RELNAME}/files/extrapatch-mkspecs_freebsd-g++_qmake.conf
 USE_GCC=		yes
 .    endif
 EXTRA_PATCHES?=		${PORTSDIR}/devel/${_QT_RELNAME}/files/extrapatch-configure \
@@ -249,9 +250,14 @@ _QT5_BASE=		core dbus gui network sql widgets
 .if ${_QT_VER:M5}
 post-patch: gcc-post-patch
 gcc-post-patch:
-	${REINPLACE_CMD} 's|%%LOCALBASE%%|${LOCALBASE}|' ${WRKSRC}/mkspecs/common/gcc-base.conf
-	${REINPLACE_CMD} 's|%%GCC_DEFAULT%%|${GCC_DEFAULT}|' ${WRKSRC}/mkspecs/common/gcc-base.conf \
-		${WRKSRC}/mkspecs/common/g++-base.conf
+	${REINPLACE_CMD} 's|%%LOCALBASE%%|${LOCALBASE}|g' \
+		${WRKSRC}/mkspecs/common/gcc-base.conf \
+		${WRKSRC}/mkspecs/freebsd-g++/qmake.conf
+	${REINPLACE_CMD} 's|%%GCC_DEFAULT%%|${GCC_DEFAULT}|g' \
+		${WRKSRC}/mkspecs/common/gcc-base.conf \
+		${WRKSRC}/mkspecs/common/g++-base.conf \
+		${WRKSRC}/mkspecs/common/bsd/bsd.conf \
+		${WRKSRC}/mkspecs/freebsd-g++/qmake.conf
 .endif
 
 pre-configure: qtbase-pre-configure
@@ -321,8 +327,8 @@ qt5-pre-configure:
 # occurrences of ${WRKSRC}/lib from .pc and .prl files when installing them.
 # See QTBUG-40825 and ports bugs 194088, 195105 and 198720.
 	${ECHO_CMD} 'QMAKE_LIBDIR_FLAGS = -L${CONFIGURE_WRKSRC}/lib' >> ${CONFIGURE_WRKSRC}/.qmake.cache
-	${ECHO_CMD} 'QMAKE_DEFAULT_LIBDIRS = ${LOCALBASE}/lib' >> ${CONFIGURE_WRKSRC}/.qmake.cache
-	${ECHO_CMD} 'QMAKE_DEFAULT_INCDIRS = ${LOCALBASE}/include' >> ${CONFIGURE_WRKSRC}/.qmake.cache
+	${ECHO_CMD} 'QMAKE_DEFAULT_LIBDIRS += ${LOCALBASE}/lib /usr/lib /lib' >> ${CONFIGURE_WRKSRC}/.qmake.cache
+	${ECHO_CMD} 'QMAKE_DEFAULT_INCDIRS += ${LOCALBASE}/include /usr/include' >> ${CONFIGURE_WRKSRC}/.qmake.cache
 
 post-install: qt-post-install
 qt-post-install:
