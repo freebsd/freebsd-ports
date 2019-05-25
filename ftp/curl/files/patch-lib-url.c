990655bebae9e776ea5d95872c78550e87d49a28
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-05-25 22:02:33 +0000

    Update to 7.65.0
    Changes:	https://curl.haxx.se/changes.html
    Security:	dd343a2b-7ee7-11e9-a290-8ddc52868fa9
    MFH:		2019Q2

--- ftp/curl/files/patch-lib-url.c
+++ ftp/curl/files/patch-lib-url.c
@@ -3,16 +3,16 @@ Forwarded: not-needed
 Author: Peter Pentchev <roam@FreeBSD.org>
 Last-Update: 2010-12-18
 
---- lib/url.c.orig	2019-02-05 14:00:12 UTC
+--- lib/url.c.orig	2019-05-21 17:57:39 UTC
 +++ lib/url.c
-@@ -536,6 +536,10 @@ CURLcode Curl_init_userdefined(struct Cu
-   set->fnmatch = ZERO_NULL;
-   set->upkeep_interval_ms = CURL_UPKEEP_INTERVAL_DEFAULT;
-   set->maxconnects = DEFAULT_CONNCACHE_SIZE; /* for easy handles */
+@@ -552,6 +552,10 @@ CURLcode Curl_init_userdefined(struct Cu
+     CURL_HTTP_VERSION_1_1
+ #endif
+     ;
 +#if defined(__FreeBSD_version)
 +  /* different handling of signals and threads */
 +  set->no_signal = TRUE;
 +#endif
-   set->http09_allowed = TRUE;
-   set->httpversion =
- #ifdef USE_NGHTTP2
+   Curl_http2_init_userset(set);
+   return result;
+ }
