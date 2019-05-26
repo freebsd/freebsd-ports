47bd1ce25587570c44e5e8f23a697ffd3739531a
Guido Falsi <madpilot@FreeBSD.org>
2019-05-26 08:43:03 +0000

    Update corsix-th to 0.63

--- games/corsix-th/files/patch-CorsixTH_Lua_config__finder.lua
+++ games/corsix-th/files/patch-CorsixTH_Lua_config__finder.lua
@@ -1,11 +1,11 @@
---- CorsixTH/Lua/config_finder.lua.orig	2015-08-30 20:29:45 UTC
+--- CorsixTH/Lua/config_finder.lua.orig	2019-05-24 00:36:47 UTC
 +++ CorsixTH/Lua/config_finder.lua
-@@ -133,7 +133,7 @@ local config_defaults = {
+@@ -137,7 +137,7 @@ local config_defaults = {
    shift_scroll_speed = 4,
    new_graphics_folder = nil,
    use_new_graphics = false,
--  check_for_updates = true
-+  check_for_updates = false
+-  check_for_updates = true,
++  check_for_updates = false,
  }
- local fi = io.open(config_filename, "r")
- local config_values = {}
+ 
+ fi = io.open(config_filename, "r")
