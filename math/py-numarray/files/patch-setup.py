bb9f108e70eb3e5028a0fc675c59d1d0b8b50b84
rene <rene@FreeBSD.org>
2019-05-30 21:39:16 +0000

    Remove expired ports: 2019-05-30 math/gracetmpl: Long unmaintained abandonware; depends on stale numarray 2019-05-30 math/py-numarray: Obsoleted upstream in favor of NumPy 2019-05-30 net/neubot: neubot client has been retired upstream

--- math/py-numarray/files/patch-setup.py
+++
@@ -1,9 +0,0 @@
---- setup.py.orig	2005-11-17 23:12:49 UTC
-+++ setup.py
-@@ -227,6 +227,4 @@ def main():
- # Do the installation.
- #
- if __name__ == "__main__":
--  if "config" not in sys.argv and ("build" in sys.argv or "install" in sys.argv):
--    sys.argv.insert(sys.argv.index("setup.py")+1, "config")
-   main()
