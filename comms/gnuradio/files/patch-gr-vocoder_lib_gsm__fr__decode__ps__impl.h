5a9c663edb57b881f8e1e8576d4e7bb824b950c5
Yuri Victorovich <yuri@FreeBSD.org>
2019-06-03 08:39:07 +0000

    comms/gnuradio: Update to 3.8tech-preview-381
    The previous commit kept GH_TAGNAME, as jbeich@ noted.
    
    * Rebuilt pkg-plist
    * Use GH_TUPLE for volk
    
    Reported by:	jbeich

--- comms/gnuradio/files/patch-gr-vocoder_lib_gsm__fr__decode__ps__impl.h
+++ comms/gnuradio/files/patch-gr-vocoder_lib_gsm__fr__decode__ps__impl.h
@@ -1,4 +1,4 @@
---- gr-vocoder/lib/gsm_fr_decode_ps_impl.h.orig	2019-03-13 15:13:10 UTC
+--- gr-vocoder/lib/gsm_fr_decode_ps_impl.h.orig	2019-05-29 03:34:08 UTC
 +++ gr-vocoder/lib/gsm_fr_decode_ps_impl.h
 @@ -26,7 +26,11 @@
  #include <gnuradio/vocoder/gsm_fr_decode_ps.h>
@@ -7,7 +7,7 @@
 +#ifdef __FreeBSD__
 +#include <gsm.h>
 +#else
- #include "gsm/gsm.h"
+ #include "gsm.h"
 +#endif
  }
  
--- comms/gnuradio/files/patch-gr-vocoder_lib_gsm__fr__encode__sp__impl.h
+++ comms/gnuradio/files/patch-gr-vocoder_lib_gsm__fr__encode__sp__impl.h
@@ -1,4 +1,4 @@
---- gr-vocoder/lib/gsm_fr_encode_sp_impl.h.orig	2019-03-13 15:22:30 UTC
+--- gr-vocoder/lib/gsm_fr_encode_sp_impl.h.orig	2019-05-29 03:34:08 UTC
 +++ gr-vocoder/lib/gsm_fr_encode_sp_impl.h
 @@ -26,7 +26,11 @@
  #include <gnuradio/vocoder/gsm_fr_encode_sp.h>
@@ -7,7 +7,7 @@
 +#ifdef __FreeBSD__
 +#include <gsm.h>
 +#else
- #include "gsm/gsm.h"
+ #include "gsm.h"
 +#endif
  }
  
