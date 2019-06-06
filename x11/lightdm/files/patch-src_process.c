a0aeb683db136aa59abcc04ea5b77f1fc678b3f4
Ben Woods <woodsb02@FreeBSD.org>
2019-06-06 23:05:21 +0000

    x11/lightdm: Update to 1.30.0, fix crash and change default config
    This update addresses a crash in lightdm, related to the way the
    user environment for the lightdm process is setup in src/process.c.
    
    The configuration option in lightdm.conf for locked memory is now
    honoured - the default configuration has been updated to disable this,
    and pkg-message has been updated to reflect this change.
    
    Changes this release:
      https://github.com/CanonicalLtd/lightdm/releases/tag/1.30.0
    
    PR:		238083
    Submitted by:	madpilot
    Reported by:	David.Boyd49@twc.com
    Differential Revision:	https://reviews.freebsd.org/D20541

--- x11/lightdm/files/patch-src_process.c
+++ x11/lightdm/files/patch-src_process.c
@@ -1,20 +1,11 @@
 --- src/process.c.orig	2018-08-30 00:28:55 UTC
 +++ src/process.c
-@@ -238,11 +238,16 @@ process_start (Process *process, gboolean block)
+@@ -73,6 +73,8 @@ static GHashTable *processes = NULL;
+ static pid_t signal_pid;
+ static int signal_pipe[2];
  
-         /* Set environment */
-         if (priv->clear_environment)
-+        {
- #ifdef HAVE_CLEARENV
-             clearenv ();
- #else
--            environ = NULL;
-+            extern char **environ;
-+            char *cleanenv[1];
-+            environ = cleanenv;
-+            cleanenv[0] = NULL;
- #endif
-+        }
-         for (guint i = 0; i < env_length; i++)
-             setenv (env_keys[i], env_values[i], TRUE);
- 
++extern char **environ;
++
+ Process *
+ process_get_current (void)
+ {
