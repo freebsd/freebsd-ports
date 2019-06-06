05f7853628f5694ad05f0937411d3d7acae5c954
mat <mat@FreeBSD.org>
2019-06-06 11:46:06 +0000

    Put back the php extension installation message, now that it can be shown on installation only.
    Differential Revision:	https://reviews.freebsd.org/D20510

--- Mk/Uses/php.mk
+++ Mk/Uses/php.mk
@@ -300,6 +300,8 @@ _INCLUDE_USES_PHP_POST_MK=yes
 
 .  if ${php_ARGS:Mext} || ${php_ARGS:Mzend}
 PHP_MODNAME?=	${PORTNAME}
+PHP_EXT_PKGMESSAGE=	${WRKDIR}/php-ext-pkg-message
+_PKGMESSAGES+=	${PHP_EXT_PKGMESSAGE}
 PHP_HEADER_DIRS+=	.
 # If there is no priority defined, we wing it.
 .    if !defined(PHP_MOD_PRIO)
@@ -348,6 +350,25 @@ add-plist-phpext:
 		>> ${TMPPLIST}
 	@${ECHO_CMD} "${PHP_EXT_INI_FILE}" \
 		>> ${TMPPLIST}
+	@${ECHO_CMD} "[" > ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "{" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "  message: <<EOD" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "****************************************************************************" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "The following line has been added to your ${PREFIX}/${PHP_EXT_INI_FILE}" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "configuration file to automatically load the installed extension:" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "" >> ${PHP_EXT_PKGMESSAGE}
+.    if ${php_ARGS:Mzend}
+	@${ECHO_CMD} "zend_extension=${PHP_MODNAME}.so" >> ${PHP_EXT_PKGMESSAGE}
+.    else
+	@${ECHO_CMD} "extension=${PHP_MODNAME}.so" >> ${PHP_EXT_PKGMESSAGE}
+.    endif
+	@${ECHO_CMD} "" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "****************************************************************************" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "EOD" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "  type: install" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "}" >> ${PHP_EXT_PKGMESSAGE}
+	@${ECHO_CMD} "]" >> ${PHP_EXT_PKGMESSAGE}
 .  endif
 
 # Extensions
