5f4a3e14e3993da529c3ea8bd9528752d8c6bfb9
Cy Schubert <cy@FreeBSD.org>
2019-06-07 11:54:01 +0000

    Update  4.3.95 --> 4.3.97
    Reported by:	portscout

--- net/ntp-devel/files/patch-ntpd__refclock__mx4200.c
+++ net/ntp-devel/files/patch-ntpd__refclock__mx4200.c
@@ -1,6 +1,6 @@
---- ntpd/refclock_mx4200.c.orig	2014-12-19 11:56:52 UTC
-+++ ntpd/refclock_mx4200.c
-@@ -1572,7 +1572,7 @@ mx4200_debug(struct peer *peer, char *fm
+--- ntpd/refclock_mx4200.c.orig	2019-06-06 17:17:04.000000000 -0700
++++ ntpd/refclock_mx4200.c	2019-06-07 04:44:07.786152000 -0700
+@@ -1572,7 +1572,7 @@
  		 * Print debug message to stdout
  		 * In the future, we may want to get get more creative...
  		 */
@@ -9,17 +9,20 @@
  
  		va_end(ap);
  	}
-@@ -1613,11 +1613,11 @@ mx4200_send(peer, fmt, va_alist)
+@@ -1614,14 +1614,14 @@
+ #else
+ 	va_start(ap);
+ #endif /* __STDC__ */
+-	n = VSNPRINTF((cp, (size_t)(ep - cp), fmt, ap));
++	n = vsnprintf((cp, (size_t)(ep - cp), fmt, ap));
+ 	va_end(ap);
+ 	if (n < 0 || (size_t)n >= (size_t)(ep - cp))
+ 		goto overflow;
  
- 	cp = buf;
- 	*cp++ = '$';
--	n = VSNPRINTF((cp, sizeof(buf) - 1, fmt, ap));
-+	n = vsnprintf(cp, sizeof(buf) - 1, fmt, ap);
  	ck = mx4200_cksum(cp, n);
+ 	cp += n;	    
+-	n = SNPRINTF((cp, (size_t)(ep - cp), "*%02X\r\n", ck));
++	n = snprintf((cp, (size_t)(ep - cp), "*%02X\r\n", ck));
+ 	if (n < 0 || (size_t)n >= (size_t)(ep - cp))
+ 		goto overflow;
  	cp += n;
- 	++n;
--	n += SNPRINTF((cp, sizeof(buf) - n - 5, "*%02X\r\n", ck));
-+	n += snprintf(cp, sizeof(buf) - n - 5, "*%02X\r\n", ck);
- 
- 	m = write(pp->io.fd, buf, (unsigned)n);
- 	if (m < 0)
