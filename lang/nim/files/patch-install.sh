c45bbb87fa604ac255e84c30a80c16eed6d66139
swills <swills@FreeBSD.org>
2019-06-07 23:07:57 +0000

    lang/nim: update to 0.20
    PR:		238390
    Submitted by:	Neal Nelson <ports@nicandneal.net> (maintainer)

--- lang/nim/files/patch-install.sh
+++ lang/nim/files/patch-install.sh
@@ -19,7 +19,7 @@
 -      libdir=/usr/lib/nim
 -      docdir=/usr/share/nim/doc
 -      datadir=/usr/share/nim/data
--      nimbleDir="/opt/nimble/pkgs/compiler-0.19.6"
+-      nimbleDir="/opt/nimble/pkgs/compiler-0.20.0"
 -      ;;
 -    "/usr/local/bin")
 -      bindir=/usr/local/bin
@@ -27,7 +27,7 @@
 -      libdir=/usr/local/lib/nim
 -      docdir=/usr/local/share/nim/doc
 -      datadir=/usr/local/share/nim/data
--      nimbleDir="/opt/nimble/pkgs/compiler-0.19.6"
+-      nimbleDir="/opt/nimble/pkgs/compiler-0.20.0"
 -      ;;
 -    "/opt")
 -      bindir="/opt/nim/bin"
@@ -35,7 +35,7 @@
 -      libdir="/opt/nim/lib"
 -      docdir="/opt/nim/doc"
 -      datadir="/opt/nim/data"
--      nimbleDir="/opt/nimble/pkgs/compiler-0.19.6"
+-      nimbleDir="/opt/nimble/pkgs/compiler-0.20.0"
 -      mkdir -p /opt/nim
 -      mkdir -p $bindir
 -      mkdir -p $configdir
