a4841f94f92948454c68ed3e286dfcd867905066
tcberner <tcberner@FreeBSD.org>
2019-06-07 04:08:25 +0000

    Update KDE Applications to 19.04.2
    https://kde.org/announcements/announce-applications-19.04.2.php

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -62,8 +62,8 @@ KDE_FRAMEWORKS_VERSION?=	5.58.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 # Current KDE applications.
-KDE_APPLICATIONS_VERSION?=	19.04.1
-KDE_APPLICATIONS_SHLIB_VER?=	5.11.1
+KDE_APPLICATIONS_VERSION?=	19.04.2
+KDE_APPLICATIONS_SHLIB_VER?=	5.11.2
 KDE_APPLICATIONS_BRANCH?=	stable
 # Upstream moves old software to Attic/. Specify the newest applications release there.
 # Only the major version is used for the comparison.
