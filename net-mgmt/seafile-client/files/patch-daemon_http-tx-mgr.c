5fc6d5ece971294b863b6898128a51beace4f8a5
ultima <ultima@FreeBSD.org>
2019-06-10 07:51:26 +0000

    Updated to 7.0.1 Port cleanup, ccnet-client will be deleted in the future.
    Changes: https://manual.seafile.com/changelog/client-changelog.html

--- net-mgmt/seafile-client/files/patch-daemon_http-tx-mgr.c
+++ net-mgmt/seafile-client/files/patch-daemon_http-tx-mgr.c
@@ -1,6 +1,6 @@
---- daemon/http-tx-mgr.c.orig	2018-02-02 05:55:07 UTC
+--- daemon/http-tx-mgr.c.orig	2019-06-04 02:14:53 UTC
 +++ daemon/http-tx-mgr.c
-@@ -68,6 +68,22 @@
+@@ -69,6 +69,22 @@
  #define USER_AGENT_OS "BSD"
  #endif
  
@@ -23,13 +23,3 @@
  struct _Connection {
      CURL *curl;
      gint64 ctime;               /* Used to clean up unused connection. */
-@@ -1091,7 +1107,9 @@ curl_error_to_http_task_error (int curl_
-     case CURLE_SSL_CONNECT_ERROR:
-     case CURLE_PEER_FAILED_VERIFICATION:
-     case CURLE_SSL_CERTPROBLEM:
-+#if LIBCURL_VERSION_NUM < 0x073e00
-     case CURLE_SSL_CACERT:
-+#endif
-     case CURLE_SSL_CACERT_BADFILE:
-     case CURLE_SSL_ISSUER_ERROR:
-         return HTTP_TASK_ERR_SSL;
