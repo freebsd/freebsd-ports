c56f7f4a0257fb2cea88b1f957a04528edfe0907
Rodrigo Osorio <rodrigo@FreeBSD.org>
2019-06-11 22:30:14 +0000

    Update print/fontforge to 20190413
    * Rework FREETYPE option using print/freetype2 to get
      freetype files instead of fetching distfile directly,
      removing the need of chasing the port's version.
    * Shift some things around in Makefile to appease portlint
    * Regenerate patches and remove patch-fontforge_splinefont.h
    
    Changelog: https://github.com/fontforge/fontforge/releases/tag/20190317
               https://github.com/fontforge/fontforge/releases/tag/20190413
    
    PR:		237855
    Submitted by:	kunda <chitty_cloud@me.com>
    Approved by:	<cyberbotx@cyberbotx.com> (maintainer)

--- print/fontforge/files/patch-fontforge_cvexport.c
+++ print/fontforge/files/patch-fontforge_cvexport.c
@@ -1,6 +1,6 @@
---- fontforge/cvexport.c.orig	2014-11-27 00:40:08 UTC
+--- fontforge/cvexport.c.orig	2019-04-13 07:38:39 UTC
 +++ fontforge/cvexport.c
-@@ -213,20 +213,7 @@ int _ExportPDF(FILE *pdf,SplineChar *sc,
+@@ -232,20 +232,7 @@ int _ExportPDF(FILE *pdf,SplineChar *sc,int layer) {
      fprintf( pdf, "    /CreationDate (D:%04d%02d%02d%02d%02d%02d",
  	    1900+tm->tm_year, tm->tm_mon+1, tm->tm_mday,
  	    tm->tm_hour, tm->tm_min, tm->tm_sec );
@@ -8,7 +8,7 @@
      fprintf( pdf, "Z)\n" );
 -#else
 -    tzset();
--    if ( timezone==0 )
+-    if ( timezone==0  || getenv("SOURCE_DATE_EPOCH") )
 -	fprintf( pdf, "Z)\n" );
 -    else {
 -	if ( timezone<0 ) /* fprintf bug - this is a kludge to print +/- in front of a %02d-padded value */
--- print/fontforge/files/patch-fontforge_print.c
+++ print/fontforge/files/patch-fontforge_print.c
@@ -1,13 +1,13 @@
---- fontforge/print.c.orig	2014-11-27 00:40:08 UTC
+--- fontforge/print.c.orig	2019-04-13 07:38:39 UTC
 +++ fontforge/print.c
-@@ -1148,19 +1148,7 @@ static void dump_pdfprologue(PI *pi) {
-     tm = localtime(&now);
+@@ -1164,19 +1164,7 @@ static void dump_pdfprologue(PI *pi) {
+     }
      fprintf( pi->out, "    /CreationDate (D:%04d%02d%02d%02d%02d%02d",
  	    tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec );
 -#ifdef _NO_TZSET
      fprintf( pi->out, "Z)\n" );
 -#else
--    if ( timezone==0 )
+-    if ( timezone==0 || getenv("SOURCE_DATE_EPOCH") )
 -	fprintf( pi->out, "Z)\n" );
 -    else {
 -	if ( timezone<0 ) /* fprintf bug - this is a kludge to print +/- in front of a %02d-padded value */
