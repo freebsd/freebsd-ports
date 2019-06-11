88b0b8e17dbf3ec9c9a440d6786c3e6c3876c470
bofh <bofh@FreeBSD.org>
2019-06-11 18:47:33 +0000

    graphics/ipe: fix build with GCC-based architectures
    PR:		238066
    Submitted by:	pkubaj

--- graphics/ipe/files/patch-ipelib_ipeplatform.cpp
+++ graphics/ipe/files/patch-ipelib_ipeplatform.cpp
@@ -1,11 +1,12 @@
---- ipelib/ipeplatform.cpp.orig	2019-03-09 12:21:05 UTC
+--- ipelib/ipeplatform.cpp.orig	2019-05-02 09:29:29 UTC
 +++ ipelib/ipeplatform.cpp
-@@ -48,7 +48,10 @@
+@@ -48,7 +48,11 @@
  #include <sys/param.h>
  #include <sys/errno.h>
  #endif
 -
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
++#include <limits.h>
 +#include <xlocale.h>
 +#include <sys/errno.h>
 +#endif
