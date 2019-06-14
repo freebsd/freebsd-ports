0e0f841f14289c744d7092150ee42331df8bf077
Steve Wills <swills@FreeBSD.org>
2019-06-14 19:52:28 +0000

    www/minio: update to 2019.06.13.01.41.13

---
+++ www/minio/files/patch-vendor_github.com_minio_parquet-go_column.go
@@ -0,0 +1,11 @@
+--- vendor/github.com/minio/parquet-go/column.go.orig	2019-06-14 18:44:52 UTC
++++ vendor/github.com/minio/parquet-go/column.go
+@@ -21,7 +21,7 @@ import (
+ 	"strings"
+ 
+ 	"git.apache.org/thrift.git/lib/go/thrift"
+-	"github.com/minio/minio-go/pkg/set"
++	"github.com/swills/minio-go/pkg/set"
+ 	"github.com/minio/parquet-go/gen-go/parquet"
+ )
+ 
