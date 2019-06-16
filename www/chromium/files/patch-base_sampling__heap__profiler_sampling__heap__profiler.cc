659bcd8163808339de9a714e9880d6d9f739e9ec
cpm <cpm@FreeBSD.org>
2019-06-16 02:17:02 +0000

    www/chromium: Unbreak build on < 11.3
    In file included from gen/base/base_jumbo_17.cc:8:
    ./../../base/sampling_heap_profiler/sampling_heap_profiler.cc:74:3: error: use of undeclared identifier 'pthread_get_name_np'
      pthread_get_name_np(pthread_self(), name, kBufferLen);
      ^
    
    PR:		238563
    Reported by:	kib@

--- www/chromium/files/patch-base_sampling__heap__profiler_sampling__heap__profiler.cc
+++ www/chromium/files/patch-base_sampling__heap__profiler_sampling__heap__profiler.cc
@@ -1,5 +1,5 @@
---- base/sampling_heap_profiler/sampling_heap_profiler.cc.orig	2019-06-04 18:55:15 UTC
-+++ base/sampling_heap_profiler/sampling_heap_profiler.cc
+--- base/sampling_heap_profiler/sampling_heap_profiler.cc.orig	2019-06-13 09:11:51.000000000 +0200
++++ base/sampling_heap_profiler/sampling_heap_profiler.cc	2019-06-14 21:28:04.089347000 +0200
 @@ -30,6 +30,10 @@
  #include <sys/prctl.h>
  #endif
@@ -11,12 +11,12 @@
  #if defined(OS_ANDROID) && BUILDFLAG(CAN_UNWIND_WITH_CFI_TABLE) && \
      defined(OFFICIAL_BUILD)
  #include "base/trace_event/cfi_backtrace_android.h"
-@@ -65,6 +69,10 @@ const char* GetAndLeakThreadName() {
+@@ -65,6 +69,10 @@
  #elif defined(OS_MACOSX)
    int err = pthread_getname_np(pthread_self(), name, kBufferLen);
    if (err == 0 && *name != '\0')
 +    return strdup(name);
-+#elif defined(OS_BSD)
++#elif defined(OS_BSD) && defined(HAVE_PTHREAD_GET_NAME_NP)
 +  pthread_get_name_np(pthread_self(), name, kBufferLen);
 +  if (*name != '\0')
      return strdup(name);
