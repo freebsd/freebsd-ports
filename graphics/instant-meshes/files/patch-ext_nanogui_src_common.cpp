05a1988c95d32eec7920af39073d193403b8bab0
rene <rene@FreeBSD.org>
2019-06-17 16:52:50 +0000

    graphics/instant-meshes: include dirent.h instead of sys/dir.h which is scheduled for removal.
    While here modernize USES
    
    PR:		238620
    Submitted by:	rene
    Approved by:	Greg V (maintainer)
    Event:		Berlin Hackathon 2019 (generating the patch)

--- graphics/instant-meshes/files/patch-ext_nanogui_src_common.cpp
+++ graphics/instant-meshes/files/patch-ext_nanogui_src_common.cpp
@@ -1,6 +1,15 @@
 --- ext/nanogui/src/common.cpp.orig	2016-10-15 20:12:12 UTC
 +++ ext/nanogui/src/common.cpp
-@@ -248,7 +248,7 @@ std::string file_dialog(const std::vecto
+@@ -24,7 +24,7 @@
+ #if !defined(_WIN32)
+     #include <locale.h>
+     #include <signal.h>
+-    #include <sys/dir.h>
++    #include <dirent.h>
+ #endif
+ 
+ NAMESPACE_BEGIN(nanogui)
+@@ -248,7 +248,7 @@ std::string file_dialog(const std::vector<std::pair<st
      return std::string(ofn.lpstrFile);
  #else
      char buffer[FILE_DIALOG_MAX_BUFFER];
