5429ac505a0580fbef4711191685d1a70b397905
mich <mich@FreeBSD.org>
2019-06-17 13:37:52 +0000

    - update to -> 1.3.0 - cleanup Makefile to keep portlint happy - regenerate patches to keep portlint happy
    PR:		238610
    Submitted by:	Thomas Merkel <tm@NetBSD.org>

--- comms/tits/files/patch-dispatcher.c
+++ comms/tits/files/patch-dispatcher.c
@@ -1,11 +1,11 @@
---- dispatcher.c.orig	Tue Sep 24 13:21:42 2002
-+++ dispatcher.c	Tue Sep 24 13:23:15 2002
-@@ -131,7 +131,7 @@
+--- dispatcher.c.orig	2006-03-19 11:34:47 UTC
++++ dispatcher.c
+@@ -237,7 +237,7 @@ dispatcher_mainloop(void)
  	struct context *ctx;
  	struct client_ctx *cc;
  	struct pollfd *pf;
 -	nfds_t pfds;
 +	unsigned int pfds;
  	int nfds, errcnt = 0;
- 
- 	while (!TAILQ_EMPTY(&contexts)) {
+ 	int timeout;
+ 	time_t delta = 0;
--- comms/tits/files/patch-listener.c
+++ comms/tits/files/patch-listener.c
@@ -1,12 +1,13 @@
---- listener.c.orig
+--- listener.c.orig	2009-11-16 14:30:13 UTC
 +++ listener.c
-@@ -189,10 +189,6 @@
+@@ -189,11 +189,6 @@ listener_destroy(struct client_ctx *cc)
  	    NULL)
  		context_del_client(cc->cc_ctx, ccc);
  
 -	if (lc->lc_args.la_address)
 -		(void) free(lc->lc_args.la_address);
--	(void) free(lc->lc_args.la_port);
+-	if (lc->lc_args.la_port)
+-		(void) free(lc->lc_args.la_port);
 -
  	(void) free(lc);
  }
