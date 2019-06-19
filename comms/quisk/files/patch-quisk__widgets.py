3544dc3c8b8a5b6191e7678481148a45682eef4a
Stephen Hurd <shurd@FreeBSD.org>
2019-06-19 06:52:31 +0000

    Fix error in positioning sliders when the font size is larger than the slider size introduced by r504514.

--- comms/quisk/files/patch-quisk__widgets.py
+++ comms/quisk/files/patch-quisk__widgets.py
@@ -1,6 +1,6 @@
---- quisk_widgets.py.orig	2019-06-19 06:35:39 UTC
+--- quisk_widgets.py.orig	2019-05-28 12:22:59 UTC
 +++ quisk_widgets.py
-@@ -248,7 +248,16 @@ class SliderBoxH:
+@@ -248,7 +248,15 @@ class SliderBoxH:
      else:
        self.text_ctrl = wx.StaticText(parent, -1, t, pos=pos)
        w2, h2 = self.text_ctrl.GetSize()
@@ -10,10 +10,9 @@
 +      p2 = pos[1]
 +      if h3 > h2:
 +        p2 -= (h3 - h2) / 2
-+        h2 = h3
 +      else:
 +        p2 += (h2 - h3) / 2
-+      self.slider.SetSize((width - w2, h2))
++      self.slider.SetSize((width - w2, h3))
 +      self.slider.SetPosition((pos[0] + w2, p2))
      self.slider.Bind(wx.EVT_SCROLL, self.OnScroll)
      self.text_ctrl.SetForegroundColour(parent.GetForegroundColour())
