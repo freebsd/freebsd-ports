1928c1c28e0b07eb139cf66537994d5e08a244b7
rodrigo <rodrigo@FreeBSD.org>
2019-06-20 08:19:04 +0000

    use dirent.h instead of deprecated sys/dir.h
    tilda currently includes sys/dir.h, but this header is scheduled for removal.
    Its successor is dirent.h. Update this port accordingly.
    
    Poudriere build run with Freebsd 11 and 12
    
    PR:		238695
    Submitted by:	rene

--- x11/tilda/files/patch-tilda.c
+++ x11/tilda/files/patch-tilda.c
@@ -1,5 +1,5 @@
---- src/tilda.c.orig	2017-09-08 22:19:35.318278000 +0300
-+++ src/tilda.c	2017-09-08 22:21:22.752550000 +0300
+--- src/tilda.c.orig	2018-02-08 19:55:42 UTC
++++ src/tilda.c
 @@ -12,6 +12,7 @@
   * You should have received a copy of the GNU Library General Public
   * License along with this library. If not, see <http://www.gnu.org/licenses/>.
@@ -8,7 +8,16 @@
  #define _POSIX_SOURCE /* feature test macro for signal functions */
  #define _XOPEN_SOURCE /* feature test macro for popen */
  
-@@ -230,7 +231,7 @@
+@@ -52,7 +53,7 @@
+ #include <sys/stat.h>
+ #include <sys/file.h>
+ #include <errno.h>
+-#include <sys/dir.h>
++#include <dirent.h>
+ #include <errno.h>
+ #include <fcntl.h>
+ #include <string.h>
+@@ -230,7 +231,7 @@ nomatch:
  static GSList *getPids() {
      GSList *pids = NULL;
      FILE *ps_output;
