49dec4e2277b363a145b6d0e3df22b369c1b2610
Philip Paeps <philip@FreeBSD.org>
2019-06-21 20:10:36 +0000

    www/cliqz: update to 1.27.3
    PR:		238410
    Submitted by:	Santhosh Raju <santhosh.raju@gmail.com> (maintainer)

--- www/cliqz/files/patch-cliqz__env.sh
+++ www/cliqz/files/patch-cliqz__env.sh
@@ -1,9 +1,9 @@
 Commented out the various wget(1) to prevent http traffic during build
 phase.
 
---- cliqz_env.sh.orig	2018-11-16 13:55:29 UTC
+--- cliqz_env.sh.orig	2019-05-29 12:52:42 UTC
 +++ cliqz_env.sh
-@@ -78,9 +78,9 @@ export S3_BUCKET=repository.cliqz.com
+@@ -101,9 +101,9 @@ export S3_BUCKET=repository.cliqz.com
  export S3_BUCKET_SERVICE=cliqz-browser-data
  
  # check CQZ_BUILD_ID and try to obtain, if not specified
@@ -16,7 +16,7 @@ phase.
  
  if [ -z $CQZ_BUILD_ID ]; then
    echo "CQZ_BUILD_ID not specified and can not be obtain from "$S3_BUCKET
-@@ -103,7 +103,7 @@ OBJ_DIR=$MOZ_OBJDIR
+@@ -131,7 +131,7 @@ OBJ_DIR=$MOZ_OBJDIR
  SRC_BASE=mozilla-release
  
  # automatic forget tab - start
