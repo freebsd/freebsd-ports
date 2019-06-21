0e0a9357e18735e45d9f0fb0baf61f97d08ee1ec
linimon <linimon@FreeBSD.org>
2019-06-21 21:49:02 +0000

    Remove stray ";" which was mistakenly added during editing.
    Reported by:	jbeich

--- math/clblas/files/patch-src_library_blas_xgemm.cc
+++ math/clblas/files/patch-src_library_blas_xgemm.cc
@@ -24,12 +24,3 @@
  
  static char *getKernelName(cl_kernel clKernel)
  {
-@@ -534,7 +534,7 @@ clblasGemm(
-   if (needRowKernel)    makeGemmKernel(   rowClKernel, commandQueues[0],    rowKernelSource, sourceBuildOptions,    &rowKernelBinary,    rowKernelBinarySize, binaryBuildOptions);
-   if (needColKernel)    makeGemmKernel(   colClKernel, commandQueues[0],    colKernelSource, sourceBuildOptions,    &colKernelBinary,    colKernelBinarySize, binaryBuildOptions);
-   if (needCornerKernel) makeGemmKernel(cornerClKernel, commandQueues[0], cornerKernelSource, sourceBuildOptions, &cornerKernelBinary, cornerKernelBinarySize, binaryBuildOptions);
--  const size_t localWorkSize[2] = { workGroupNumRows, workGroupNumCols };
-+  const size_t localWorkSize[2] = { workGroupNumRows, workGroupNumCols };;
-   unsigned int numKernelsEnqueued = 0;
- 
- /******************************************************************************
