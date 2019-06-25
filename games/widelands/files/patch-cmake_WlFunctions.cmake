bff4c8bc2b12baf8fd770cb3c3256ed04c0aa2fb
Dmitry Marakasov <amdmi3@FreeBSD.org>
2019-06-25 15:35:21 +0000

    - Update to build20

--- games/widelands/files/patch-cmake_WlFunctions.cmake
+++ games/widelands/files/patch-cmake_WlFunctions.cmake
@@ -1,6 +1,6 @@
---- cmake/WlFunctions.cmake.orig	2016-11-10 07:28:01 UTC
+--- cmake/WlFunctions.cmake.orig	2019-05-02 05:23:39 UTC
 +++ cmake/WlFunctions.cmake
-@@ -276,5 +276,5 @@ function(wl_binary NAME)
+@@ -270,5 +270,5 @@ function(wl_binary NAME)
  
    #Quoting the CMake documentation on DESTINATION:
    #"If a relative path is given it is interpreted relative to the value of CMAKE_INSTALL_PREFIX"
