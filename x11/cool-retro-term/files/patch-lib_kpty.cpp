021c1b5416fc1b8a18b856eae6acc064fea94e0c
danfe <danfe@FreeBSD.org>
2019-06-25 16:22:21 +0000

    - Update to version 1.1.1, install manual page - Add run-time dependency on sql-sqlite3 to enable save/restore   of profile and other settings [*] - Fix some stage Q/A warnings while here
    Reported by:	Lundin Cahill [*]

--- x11/cool-retro-term/files/patch-lib_kpty.cpp
+++ x11/cool-retro-term/files/patch-lib_kpty.cpp
@@ -3,7 +3,7 @@
 @@ -28,7 +28,7 @@
  
  
- #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+ #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
 -#define HAVE_LOGIN
 +//#define HAVE_LOGIN
  #define HAVE_LIBUTIL_H
@@ -21,15 +21,6 @@
  # endif
  # if !defined(_PATH_UTMPX) && defined(_UTMPX_FILE)
  #  define _PATH_UTMPX _UTMPX_FILE
-@@ -503,7 +504,7 @@ void KPty::login(const char * user, cons
-     // note: strncpy without terminators _is_ correct here. man 4 utmp
- 
-     if (user) {
--        strncpy(l_struct.ut_name, user, sizeof(l_struct.ut_name));
-+        strncpy(l_struct.ut_user, user, sizeof(l_struct.ut_user));
-     }
- 
-     if (remotehost) {
 @@ -550,7 +551,7 @@ void KPty::login(const char * user, cons
  #   endif
  #  endif
@@ -48,12 +39,3 @@
      setutxent();
      if ((ut = getutxline(&l_struct))) {
  #  else
-@@ -614,7 +615,7 @@ void KPty::logout()
-     setutent();
-     if ((ut = getutline(&l_struct))) {
- #  endif
--        memset(ut->ut_name, 0, sizeof(*ut->ut_name));
-+        memset(ut->ut_user, 0, sizeof(*ut->ut_user));
-         memset(ut->ut_host, 0, sizeof(*ut->ut_host));
- #  ifdef HAVE_STRUCT_UTMP_UT_SYSLEN
-         ut->ut_syslen = 0;
