a64534e590dce87e13a1b9631cec6058c4a9c014
sunpoet <sunpoet@FreeBSD.org>
2019-06-27 20:04:20 +0000

    Update to 0.7.2
    Changes:	https://github.com/davidfraser/WSGIUtils/releases

--- devel/py-wsgiutils/files/patch-setup.py
+++ devel/py-wsgiutils/files/patch-setup.py
@@ -1,12 +1,10 @@
---- setup.py.orig	2018-08-14 08:49:36 UTC
+--- setup.py.orig	2019-06-26 18:43:04 UTC
 +++ setup.py
-@@ -33,8 +33,8 @@ setup(name="WSGIUtils",
- 	packages=[
+@@ -34,6 +34,7 @@ setup(name="WSGIUtils",
  		'wsgiutils',
  	],
--	data_files = [('', ['LICENSE.txt', 'README.txt'])],
  	package_dir = {'': 'lib'},
-+        use_2to3=True,
++       use_2to3=True,
  	classifiers = [
  		'Development Status :: 5 - Production/Stable',
  		'Intended Audience :: Developers',
