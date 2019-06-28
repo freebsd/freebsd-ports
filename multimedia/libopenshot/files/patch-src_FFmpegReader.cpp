d2952870588af9b693af822d021485999801c863
Dmitry Marakasov <amdmi3@FreeBSD.org>
2019-06-28 15:49:00 +0000

    - Add multimedia/libopenshot
    OpenShot Video Library (libopenshot) is a free, open-source project dedicated
    to delivering high quality video editing, animation, and playback solutions to
    the world. API currently supports C++, Python, and Ruby.
    
    WWW: https://www.openshot.org/
    
    PR:		237575
    Submitted by:	tatsuki_makino@hotmail.com

---
+++ multimedia/libopenshot/files/patch-src_FFmpegReader.cpp
@@ -0,0 +1,35 @@
+--- src/FFmpegReader.cpp.orig	2019-05-08 19:16:50 UTC
++++ src/FFmpegReader.cpp
+@@ -159,7 +159,7 @@ static enum AVPixelFormat get_hw_dec_format(AVCodecCon
+ 
+ 	for (p = pix_fmts; *p != AV_PIX_FMT_NONE; p++) {
+ 		switch (*p) {
+-#if defined(__linux__)
++#if defined(__unix__)
+ 			// Linux pix formats
+ 			case AV_PIX_FMT_VAAPI:
+ 				hw_de_av_pix_fmt_global = AV_PIX_FMT_VAAPI;
+@@ -312,7 +312,7 @@ void FFmpegReader::Open() {
+ 					pCodecCtx->get_format = get_hw_dec_format;
+ 
+ 					if (adapter_num < 3 && adapter_num >=0) {
+-#if defined(__linux__)
++#if defined(__unix__)
+ 						snprintf(adapter,sizeof(adapter),"/dev/dri/renderD%d", adapter_num+128);
+ 						adapter_ptr = adapter;
+ 						i_decoder_hw = openshot::Settings::Instance()->HARDWARE_DECODER;
+@@ -375,11 +375,13 @@ void FFmpegReader::Open() {
+ 					}
+ 
+ 					// Check if it is there and writable
+-#if defined(__linux__)
++#if defined(__unix__)
+ 					if( adapter_ptr != NULL && access( adapter_ptr, W_OK ) == 0 ) {
+ #elif defined(_WIN32)
+ 					if( adapter_ptr != NULL ) {
+ #elif defined(__APPLE__)
++					if( adapter_ptr != NULL ) {
++#else
+ 					if( adapter_ptr != NULL ) {
+ #endif
+ 						ZmqLogger::Instance()->AppendDebugMethod("Decode Device present using device", "", -1, "", -1, "", -1, "", -1, "", -1, "", -1);
