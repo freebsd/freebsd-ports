c8bee7bdaf95dbf68544ebb7c308257dba6ea465
Jan Beich <jbeich@FreeBSD.org>
2019-06-30 01:27:17 +0000

    emulators/rpcs3: oops, r505399 didn't build

--- emulators/rpcs3/files/patch-rpcs3_Emu_RSX_VK_VKHelpers.h
+++ emulators/rpcs3/files/patch-rpcs3_Emu_RSX_VK_VKHelpers.h
@@ -7,11 +7,16 @@ rpcs3/Emu/RSX/VK/VKHelpers.h:465:3: error: unknown type name 'VkPhysicalDeviceDr
 
 --- rpcs3/Emu/RSX/VK/VKHelpers.h.orig	2019-06-29 15:48:42 UTC
 +++ rpcs3/Emu/RSX/VK/VKHelpers.h
-@@ -47,6 +47,35 @@
+@@ -47,6 +47,48 @@
  #define FRAME_PRESENT_TIMEOUT 1000000ull // 1 second
  #define GENERAL_WAIT_TIMEOUT  100000ull  // 100ms
  
 +#if VK_HEADER_VERSION < 92
++#define VK_MAX_DRIVER_NAME_SIZE_KHR       256
++#define VK_MAX_DRIVER_INFO_SIZE_KHR       256
++
++#define VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DRIVER_PROPERTIES_KHR (VkStructureType)1000196000
++
 +typedef enum VkDriverIdKHR {
 +    VK_DRIVER_ID_AMD_PROPRIETARY_KHR = 1,
 +    VK_DRIVER_ID_AMD_OPEN_SOURCE_KHR = 2,
@@ -30,6 +35,14 @@ rpcs3/Emu/RSX/VK/VKHelpers.h:465:3: error: unknown type name 'VkPhysicalDeviceDr
 +    VK_DRIVER_ID_RANGE_SIZE_KHR = (VK_DRIVER_ID_BROADCOM_PROPRIETARY_KHR - VK_DRIVER_ID_AMD_PROPRIETARY_KHR + 1),
 +    VK_DRIVER_ID_MAX_ENUM_KHR = 0x7FFFFFFF
 +} VkDriverIdKHR;
++
++typedef struct VkConformanceVersionKHR {
++    uint8_t    major;
++    uint8_t    minor;
++    uint8_t    subminor;
++    uint8_t    patch;
++} VkConformanceVersionKHR;
++
 +typedef struct VkPhysicalDeviceDriverPropertiesKHR {
 +    VkStructureType            sType;
 +    void*                      pNext;
