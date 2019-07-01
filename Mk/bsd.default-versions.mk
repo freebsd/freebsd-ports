7d84bff13f6af1ac9283e0596cfe1104d79c8d8b
ale <ale@FreeBSD.org>
2019-07-01 07:43:32 +0000

    Update default MySQL version to 5.7.
    PR:		238365
    Submitted by:	ale
    Exp-run by:	antoine
    Approved by:	portmgr (antoine)

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -65,7 +65,7 @@ LLVM_DEFAULT?=		80
 # Possible values: 5.1, 5.2, 5.3
 LUA_DEFAULT?=		5.2
 # Possible values: 5.5, 5.6, 5.7, 8.0, 5.5m, 10.0m, 10.1m, 10.2m, 10.3m, 5.5p, 5.6p, 5.7p, 5.6w
-MYSQL_DEFAULT?=		5.6
+MYSQL_DEFAULT?=		5.7
 # Possible values: 5.26, 5.28, 5.30, devel
 .if !exists(${LOCALBASE}/bin/perl) || (!defined(_PORTS_ENV_CHECK) && \
     defined(PACKAGE_BUILDING))
