6d67ff8844abdc3b3097eb6d9e9f8d384861d8cd
tobik <tobik@FreeBSD.org>
2019-07-04 14:41:57 +0000

    lang/rust: Update to 1.36.0
    - Force rebuild all consumers to catch regressions early
    
    Thanks to Mikaël Urankar for providing updated bootstraps for
    aarch64, armv6, armv7, powerpc64.
    
    Changes:	https://blog.rust-lang.org/2019/07/04/Rust-1.36.0.html
    Tested by:	jbeich, mikael.urankar@gmail.com
    With hat:	rust
    Differential Revision:	https://reviews.freebsd.org/D20810

--- Mk/Uses/cargo.mk
+++ Mk/Uses/cargo.mk
@@ -43,7 +43,7 @@ DISTFILES+=	${CARGO_DIST_SUBDIR}/${_crate}.tar.gz:cargo_${_crate:S/-//g:S/.//g}
 
 CARGO_BUILDDEP?=	yes
 .if ${CARGO_BUILDDEP:tl} == "yes"
-BUILD_DEPENDS+=	${RUST_DEFAULT}>=1.35.0:lang/${RUST_DEFAULT}
+BUILD_DEPENDS+=	${RUST_DEFAULT}>=1.36.0:lang/${RUST_DEFAULT}
 .endif
 
 # Location of cargo binary (default to lang/rust's Cargo binary)
