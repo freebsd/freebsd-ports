07b66317470afc5f99d5fa25ddcdd65805a62409
jbeich <jbeich@FreeBSD.org>
2019-07-05 14:45:08 +0000

    multimedia/libva: update to 2.5.0
    Changes:	https://github.com/intel/libva/releases/tag/2.5.0
    ABI:		https://abi-laboratory.pro/tracker/timeline/libva/
    Reported by:	GitHub (watch releases), portscout

--- multimedia/libva/files/patch-va_va__trace.c
+++ multimedia/libva/files/patch-va_va__trace.c
@@ -1,6 +1,6 @@
 - Implement gettid() for BSDs
 
---- va/va_trace.c.orig	2018-02-12 06:32:11 UTC
+--- va/va_trace.c.orig	2019-07-05 13:14:31 UTC
 +++ va/va_trace.c
 @@ -48,12 +48,36 @@
  #include <unistd.h>
@@ -9,7 +9,7 @@
 -#include <sys/syscall.h>
  #include <pthread.h>
  #include <unistd.h>
- #include <time.h>
+ #include <sys/time.h>
  #include <errno.h>
  
 +#if defined(__linux__)
@@ -40,7 +40,7 @@
  /*
   * Env. to debug some issue, e.g. the decode/encode issue in a video conference scenerio:
   * .LIBVA_TRACE=log_file: general VA parameters saved into log_file
-@@ -290,7 +318,7 @@ static void add_trace_config_info(
+@@ -290,7 +314,7 @@ static void add_trace_config_info(
  {
      struct trace_config_info *pconfig_info;
      int idx = 0;
@@ -49,7 +49,7 @@
  
      LOCK_RESOURCE(pva_trace);
  
-@@ -668,7 +696,7 @@ static struct trace_log_file *start_tracing2log_file(
+@@ -668,7 +692,7 @@ static struct trace_log_file *start_tracing2log_file(
  {
      struct trace_log_files_manager *plog_files_mgr = NULL;
      struct trace_log_file *plog_file = NULL;
@@ -58,7 +58,7 @@
      int i = 0;
  
      LOCK_RESOURCE(pva_trace);
-@@ -707,7 +735,7 @@ static void refresh_log_file(
+@@ -707,7 +731,7 @@ static void refresh_log_file(
      struct trace_context *ptra_ctx)
  {
      struct trace_log_file *plog_file = NULL;
@@ -67,7 +67,7 @@
      int i = 0;
  
      plog_file = ptra_ctx->plog_file;
-@@ -1230,7 +1258,7 @@ static void internal_TraceUpdateContext (
+@@ -1238,7 +1262,7 @@ static void internal_TraceUpdateContext (
  {
      struct trace_context *trace_ctx = NULL;
      int i = 0, delete = 1;
