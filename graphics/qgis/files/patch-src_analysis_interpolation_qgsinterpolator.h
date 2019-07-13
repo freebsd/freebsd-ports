5609ce6bf678beecbdf919761e3413542cdf01b2
tcberner <tcberner@FreeBSD.org>
2019-07-13 13:06:48 +0000

    graphics/qgis: prepare for Qt 5.13
    PR:		238782

---
+++ graphics/qgis/files/patch-src_analysis_interpolation_qgsinterpolator.h
@@ -0,0 +1,10 @@
+--- src/analysis/interpolation/qgsinterpolator.h.orig	2019-07-13 11:21:02 UTC
++++ src/analysis/interpolation/qgsinterpolator.h
+@@ -18,6 +18,7 @@
+ #ifndef QGSINTERPOLATOR_H
+ #define QGSINTERPOLATOR_H
+ 
++#include <QList>
+ #include <QVector>
+ #include "qgis_sip.h"
+ #include "qgis_analysis.h"
