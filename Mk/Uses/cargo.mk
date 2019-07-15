fbe6dc595d34fab04a65220fe297e7eaf390daa3
Tobias Kortkamp <tobik@FreeBSD.org>
2019-07-15 08:46:13 +0000

    Mk/Uses/cargo.mk: Handle distfile group names properly
    Crates like openssl-src-111.3.0+1.1.1c have characters not in
    [a-zA-Z0-9].  Use the complement of [a-zA-Z0-9] instead of adding
    + to the list of chars [-.] we currently handle.

--- Mk/Uses/cargo.mk
+++ Mk/Uses/cargo.mk
@@ -35,8 +35,8 @@ CARGO_DIST_SUBDIR?=	rust/crates
 
 # Generate list of DISTFILES.
 .for _crate in ${CARGO_CRATES}
-MASTER_SITES+=	CRATESIO/${_crate:C/^(.*)-[0-9].*/\1/}/${_crate:C/^.*-([0-9].*)/\1/}:cargo_${_crate:S/-//g:S/.//g}
-DISTFILES+=	${CARGO_DIST_SUBDIR}/${_crate}.tar.gz:cargo_${_crate:S/-//g:S/.//g}
+MASTER_SITES+=	CRATESIO/${_crate:C/^(.*)-[0-9].*/\1/}/${_crate:C/^.*-([0-9].*)/\1/}:cargo_${_crate:C/[^a-zA-Z0-9_]//g}
+DISTFILES+=	${CARGO_DIST_SUBDIR}/${_crate}.tar.gz:cargo_${_crate:C/[^a-zA-Z0-9_]//g}
 .endfor
 
 # Build dependencies.
