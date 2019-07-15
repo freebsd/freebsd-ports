44b223eddf1475806998572c884958a0e1b0f4cc
Mathieu Arnold <mat@FreeBSD.org>
2019-07-15 13:01:40 +0000

    Update to v5.31.1-148-gdc9ac3ee56.

--- lang/perl5-devel/files/patch-perl.c
+++ lang/perl5-devel/files/patch-perl.c
@@ -1,8 +1,8 @@
 Remove compile time information (reproducible builds)
 
---- perl.c.orig	2019-06-25 08:41:51 UTC
+--- perl.c.orig	2019-07-14 14:40:03 UTC
 +++ perl.c
-@@ -2005,23 +2005,7 @@ S_Internals_V(pTHX_ CV *cv)
+@@ -1994,23 +1994,7 @@ S_Internals_V(pTHX_ CV *cv)
      PUSHs(Perl_newSVpvn_flags(aTHX_ non_bincompat_options,
  			      sizeof(non_bincompat_options) - 1, SVs_TEMP));
  
