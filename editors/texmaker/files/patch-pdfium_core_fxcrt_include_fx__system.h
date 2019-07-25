7d534ce6499949188212c43032a257a0e0c23694
Max Brazhnikov <makc@FreeBSD.org>
2019-07-25 18:20:23 +0000

    editors/texmaker: - Update to 5.0.3 - Remove dependency on poppler, texmaker uses pdfium since 5.0 - Update WWW
    PR:		238752
    Approved by:	maintainer timeout (4 weeks)

---
+++ editors/texmaker/files/patch-pdfium_core_fxcrt_include_fx__system.h
@@ -0,0 +1,11 @@
+--- pdfium/core/fxcrt/include/fx_system.h.orig	2017-04-16 14:53:52 UTC
++++ pdfium/core/fxcrt/include/fx_system.h
+@@ -40,7 +40,7 @@
+ #elif defined(_WIN64)
+ #define _FX_OS_ _FX_WIN64_DESKTOP_
+ #define _FXM_PLATFORM_ _FXM_PLATFORM_WINDOWS_
+-#elif defined(__linux__)
++#elif defined(__linux__) || defined(__FreeBSD__)
+ #define _FX_OS_ _FX_LINUX_DESKTOP_
+ #define _FXM_PLATFORM_ _FXM_PLATFORM_LINUX_
+ #elif defined(__APPLE__)
