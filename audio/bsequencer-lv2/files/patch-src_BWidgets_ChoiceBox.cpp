a9e5ffdac8460247d388632fff9d720cf10880ab
Piotr Kubaj <pkubaj@FreeBSD.org>
2019-08-01 19:21:09 +0000

    audio/bsequencer-lv2: fix build with GCC architectures
    C++11 compiler is required:
    cc1plus: error: unrecognized command line option "-std=c++11"
    
    include sys/types.h is required by GCC for uint in src/BWidgets/ChoiceBox.cpp, src/BWidgets/ImageIcon.cpp and src/BWidgets/ListBox.cpp.
    
    PR:		239440
    Approved by:	linimon (mentor), yuri (maintainer)
    Differential Revision:	https://reviews.freebsd.org/D21066

---
+++ audio/bsequencer-lv2/files/patch-src_BWidgets_ChoiceBox.cpp
@@ -0,0 +1,10 @@
+--- src/BWidgets/ChoiceBox.cpp.orig	2019-07-25 10:03:27 UTC
++++ src/BWidgets/ChoiceBox.cpp
+@@ -16,6 +16,7 @@
+  */
+ 
+ #include "ChoiceBox.hpp"
++#include <sys/types.h>
+ 
+ namespace BWidgets
+ {
