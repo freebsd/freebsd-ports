f783260c1c4d7bacbb6d36cab209eab28da49d4e
Jan Beich <jbeich@FreeBSD.org>
2019-08-03 21:14:43 +0000

    x11/libxshmfence: prefer SHM_ANON over mksotemp()
    SHM_ANON is faster and more similar to memfd_create()
    
    PR:		239630
    Approved by:	jkim

--- x11/libxshmfence/files/patch-src__xshmfence_alloc.c
+++ x11/libxshmfence/files/patch-src__xshmfence_alloc.c
@@ -1,6 +1,16 @@
 --- src/xshmfence_alloc.c.orig	2015-03-04 15:28:23 UTC
 +++ src/xshmfence_alloc.c
-@@ -79,7 +79,11 @@ xshmfence_alloc_shm(void)
+@@ -73,13 +73,21 @@ xshmfence_alloc_shm(void)
+ 	fd = memfd_create("xshmfence", MFD_CLOEXEC|MFD_ALLOW_SEALING);
+ 	if (fd < 0)
+ #endif
++#ifdef SHM_ANON
++	fd = shm_open(SHM_ANON, O_RDWR|O_CLOEXEC, 0600);
++	if (fd < 0)
++#endif
+ 	{
+ #ifdef O_TMPFILE
+ 		fd = open(SHMDIR, O_TMPFILE|O_RDWR|O_CLOEXEC|O_EXCL, 0666);
  		if (fd < 0)
  #endif
  		{
