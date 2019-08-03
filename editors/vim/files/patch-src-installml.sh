24478a28af09fbe674ad8f19ded48a128af6f4b6
Adam Weinberger <adamw@FreeBSD.org>
2019-08-03 17:33:02 +0000

    vim: Simplify patch

--- editors/vim/files/patch-src-installml.sh
+++ editors/vim/files/patch-src-installml.sh
@@ -1,31 +1,25 @@
---- src/installml.sh.orig	2015-05-14 03:56:21 UTC
+--- src/installml.sh.orig	2019-08-03 17:29:51 UTC
 +++ src/installml.sh
-@@ -53,16 +53,16 @@ if test $what = "install" -a \( -f $dest
+@@ -52,18 +52,6 @@ if test $what = "install" -a \( -f $destdir/$vimname.1
+       /bin/sh install-sh -c -d $destdir
     fi
  
-    # ex
+-   # ex
 -   if test ! -f $destdir/$exname.1 -a -f $destdir/$vimname.1; then
 -      echo creating link $destdir/$exname.1
 -      cd $destdir; ln -s $vimname.1 $exname.1
 -   fi
-+   #if test ! -f $destdir/$exname.1 -a -f $destdir/$vimname.1; then
-+   #   echo creating link $destdir/$exname.1
-+   #   cd $destdir; ln -s $vimname.1 $exname.1
-+   #fi
- 
-    # view
+-
+-   # view
 -   if test ! -f $destdir/$viewname.1 -a -f $destdir/$vimname.1; then
 -      echo creating link $destdir/$viewname.1
 -      cd $destdir; ln -s $vimname.1 $viewname.1
 -   fi
-+   #if test ! -f $destdir/$viewname.1 -a -f $destdir/$vimname.1; then
-+   #   echo creating link $destdir/$viewname.1
-+   #   cd $destdir; ln -s $vimname.1 $viewname.1
-+   #fi
- 
+-
     # rvim
     if test ! -f $destdir/$rvimname.1 -a -f $destdir/$vimname.1; then
-@@ -119,14 +119,14 @@ fi
+       echo creating link $destdir/$rvimname.1
+@@ -119,14 +107,6 @@ fi
  if test $what = "uninstall"; then
     echo Checking for Vim manual page links in $destdir...
  
@@ -37,14 +31,6 @@
 -      echo deleting $destdir/$viewname.1
 -      rm -f $destdir/$viewname.1
 -   fi
-+   #if test -L $destdir/$exname.1; then
-+   #   echo deleting $destdir/$exname.1
-+   #   rm -f $destdir/$exname.1
-+   #fi
-+   #if test -L $destdir/$viewname.1; then
-+   #   echo deleting $destdir/$viewname.1
-+   #   rm -f $destdir/$viewname.1
-+   #fi
     if test -L $destdir/$rvimname.1; then
        echo deleting $destdir/$rvimname.1
        rm -f $destdir/$rvimname.1
