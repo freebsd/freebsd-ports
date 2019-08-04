6c85984ae9fd33f96ddccd8932f0c99eb13ee827
kai <kai@FreeBSD.org>
2019-08-04 21:12:28 +0000

    www/mitmproxy: Prepare for py-openssl 19.0.0 and unbreak at runtime
    Relax the version ranges for security/py-openssl to avoid breakage at
    runtime when release 19.0.0 lands into the Ports tree.
    
    Also unbreak the port as it's already broken at runtime because the given
    requirements for security/py-cryptography are too narrow.
    
    PR:		239540
    Approved by:	Hung-Yi Chen <gaod@hychen.org> (maintainer)
    MFH:		2019Q3 (runtime fix)

--- www/mitmproxy/files/patch-setup.py
+++ www/mitmproxy/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2019-02-14 12:22:24 UTC
+--- setup.py.orig	2019-07-30 16:02:34 UTC
 +++ setup.py
-@@ -61,7 +61,7 @@ setup(
+@@ -61,24 +61,24 @@ setup(
      # It is not considered best practice to use install_requires to pin dependencies to specific versions.
      install_requires=[
          "blinker>=1.4",
@@ -8,8 +8,16 @@
 +        "brotli>=0.7.0",
          "certifi>=2015.11.20.1",  # no semver here - this should always be on the last release!
          "click>=6.2",
-         "cryptography>=2.1.4,<2.4",
-@@ -75,10 +75,10 @@ setup(
+-        "cryptography>=2.1.4,<2.4",
++        "cryptography>=2.1.4",
+         "h2>=3.0.1,<4",
+         "hyperframe>=5.1.0,<6",
+         "kaitaistruct>=0.7,<0.9",
+         "ldap3>=2.5,<2.6",
+         "passlib>=1.6.5",
+         "pyasn1>=0.3.1,<0.5",
+-        "pyOpenSSL>=17.5,<18.1",
++        "pyOpenSSL>=17.5",
          "pyparsing>=2.1.3",
          "pyperclip>=1.6.0",
          "ruamel.yaml>=0.13.2",
