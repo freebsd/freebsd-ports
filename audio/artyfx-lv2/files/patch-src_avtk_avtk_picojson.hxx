807c89db58719382498c4be9d729573776015833
Piotr Kubaj <pkubaj@FreeBSD.org>
2019-08-05 10:59:39 +0000

    audio/artyfx-lv2: fix build with GCC-based architectures
    The fix for clang in files/patch-src_avtk_avtk_picojson.hxx breaks GCC. Have two versions for two compilers.
    
    Fix altivec in src/avtk/libs/pffft.cxx and add -maltivec to CXXFLAGS on powerpc64.
    
    Also use new compilers.
    
    PR:		239518
    Approved by:	yuri (maintainer), tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D21141

--- audio/artyfx-lv2/files/patch-src_avtk_avtk_picojson.hxx
+++ audio/artyfx-lv2/files/patch-src_avtk_avtk_picojson.hxx
@@ -1,11 +1,15 @@
---- src/avtk/avtk/picojson.hxx.orig	2018-05-29 06:05:03 UTC
+--- src/avtk/avtk/picojson.hxx.orig	2018-02-25 11:56:24 UTC
 +++ src/avtk/avtk/picojson.hxx
-@@ -204,7 +204,7 @@ inline value::value(double n) : type_(nu
+@@ -204,7 +204,11 @@ inline value::value(double n) : type_(number_type)
  #ifdef _MSC_VER
  	        ! _finite(n)
  #elif __cplusplus>=201103L || !(defined(isnan) && defined(isinf))
 -	        std::isnan(n) || std::isinf(n)
++#ifdef __GNUC__
++		std::isnan(n) || std::isinf(n)
++#else
 +	        isnanf(n) || isinf(n)
++#endif
  #else
  	        isnan(n) || isinf(n)
  #endif
