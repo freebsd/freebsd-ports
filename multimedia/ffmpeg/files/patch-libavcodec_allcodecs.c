76a8e58f8ee2be9d62f2ff53c7fb8b257c25b2ed
jbeich <jbeich@FreeBSD.org>
2019-08-05 23:06:50 +0000

    multimedia/ffmpeg: update to 4.2
    Changes:	https://git.ffmpeg.org/gitweb/ffmpeg.git/blob/n4.2:/Changelog
    ABI:		https://abi-laboratory.pro/tracker/timeline/ffmpeg/

--- multimedia/ffmpeg/files/patch-libavcodec_allcodecs.c
+++ multimedia/ffmpeg/files/patch-libavcodec_allcodecs.c
@@ -2,13 +2,14 @@
 
 --- libavcodec/allcodecs.c.orig	2018-11-05 23:22:25 UTC
 +++ libavcodec/allcodecs.c
-@@ -672,12 +672,12 @@ extern AVCodec ff_pcm_mulaw_at_encoder;
+@@ -678,13 +678,13 @@ extern AVCodec ff_pcm_mulaw_at_encoder;
  extern AVCodec ff_pcm_mulaw_at_decoder;
  extern AVCodec ff_qdmc_at_decoder;
  extern AVCodec ff_qdm2_at_decoder;
 +extern AVCodec ff_libdav1d_decoder;
  extern AVCodec ff_libaom_av1_decoder;
  extern AVCodec ff_libaom_av1_encoder;
+ extern AVCodec ff_libaribb24_decoder;
  extern AVCodec ff_libcelt_decoder;
  extern AVCodec ff_libcodec2_encoder;
  extern AVCodec ff_libcodec2_decoder;
