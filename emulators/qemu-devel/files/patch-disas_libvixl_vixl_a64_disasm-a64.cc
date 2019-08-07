24a647098ebdb4af8a035d6439d04388ed357a73
bofh <bofh@FreeBSD.org>
2019-08-07 09:58:15 +0000

    emulators/qemu-devel: Update version 3.1.0=>4.0.0
    - USES=gl is not always needed [1]
    
    PR:		238364 [1]
    Submitted by:	pizzamig [1]
    Reported by:	bz

--- emulators/qemu-devel/files/patch-disas_libvixl_vixl_a64_disasm-a64.cc
+++ emulators/qemu-devel/files/patch-disas_libvixl_vixl_a64_disasm-a64.cc
@@ -1,6 +1,6 @@
---- disas/libvixl/vixl/a64/disasm-a64.cc.orig	2016-04-14 20:19:53 UTC
+--- disas/libvixl/vixl/a64/disasm-a64.cc.orig	2019-04-23 18:14:45 UTC
 +++ disas/libvixl/vixl/a64/disasm-a64.cc
-@@ -2693,7 +2693,7 @@ void Disassembler::AppendPCRelativeOffse
+@@ -2693,7 +2693,7 @@ void Disassembler::AppendPCRelativeOffsetToOutput(cons
    if (offset < 0) {
      abs_offset = -abs_offset;
    }
