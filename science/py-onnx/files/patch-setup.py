d3f552d9f109fa49e34765a04458f025a216d4c7
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-08-08 19:22:20 +0000

    Fix RUN_DEPENDS: typing is used for Python 3.4 and below
    - Bump PORTREVISION for dependency change
    
    With hat:	python

--- science/py-onnx/files/patch-setup.py
+++ science/py-onnx/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2019-08-08 17:17:08 UTC
+--- setup.py.orig	2019-04-24 06:21:37 UTC
 +++ setup.py
 @@ -52,11 +52,12 @@ COVERAGE = bool(os.getenv('COVERAGE'))
  # Version
@@ -18,3 +18,12 @@
  
  with open(os.path.join(TOP_DIR, 'VERSION_NUMBER')) as version_file:
      VersionInfo = namedtuple('VersionInfo', ['version', 'git_version'])(
+@@ -286,7 +287,7 @@ install_requires.extend([
+     'protobuf',
+     'numpy',
+     'six',
+-    'typing>=3.6.4',
++    'typing>=3.6.4; python_version < 3.5',
+     'typing-extensions>=3.6.2.1',
+ ])
+ 
