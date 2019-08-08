c6ce1c0b390f172ca0b40c1913b04ddfcd3f78cc
Vsevolod Stakhov <vsevolod@FreeBSD.org>
2019-08-08 15:08:31 +0000

    - Update to 2.1 - Use autoreconf - Add pango dependency - Remove obsoleted master site - Add patches to avoid malloc.h include
    PR:		234081
    Submitted by:		ManFree <roman_romul at mail.ru>

---
+++ cad/tkgate/files/patch-src_common_list.c
@@ -0,0 +1,10 @@
+--- src/common/list.c.orig	2019-08-08 12:43:30 UTC
++++ src/common/list.c
+@@ -16,7 +16,6 @@
+     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ****************************************************************************/
+ #include <stdlib.h>
+-#include <malloc.h>
+ #include "misc.h"
+ #include "list.h"
+ 
