0d751561e8ff37101d3c8df46fd7c033a9fd2918
yuri <yuri@FreeBSD.org>
2019-08-08 17:53:24 +0000

    New port: science/py-onnx: Open Neural Network eXchange

---
+++ science/py-onnx/files/patch-setup.py
@@ -0,0 +1,20 @@
+--- setup.py.orig	2019-08-08 17:17:08 UTC
++++ setup.py
+@@ -52,11 +52,12 @@ COVERAGE = bool(os.getenv('COVERAGE'))
+ # Version
+ ################################################################################
+ 
+-try:
+-    git_version = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
+-                                          cwd=TOP_DIR).decode('ascii').strip()
+-except (OSError, subprocess.CalledProcessError):
+-    git_version = None
++#try:
++#    git_version = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
++#                                          cwd=TOP_DIR).decode('ascii').strip()
++#except (OSError, subprocess.CalledProcessError):
++#    git_version = None
++git_version = None
+ 
+ with open(os.path.join(TOP_DIR, 'VERSION_NUMBER')) as version_file:
+     VersionInfo = namedtuple('VersionInfo', ['version', 'git_version'])(
