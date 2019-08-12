0fcdb51b41e35a4e320a6959b56e9585fcc520e9
Kubilay Kocak <koobs@FreeBSD.org>
2019-08-12 01:14:45 +0000

    MFH: r508699 textproc/py-sphinxcontrib-bitbucket: Fix Python 3 build/run
    Fix Python 3 build, identified while testing devel/py-virtualenvwrapper:
    
    Exception occurred:
      File "/usr/local/lib/python3.7/site-packages/sphinx/registry.py", line 196, in load_extension
        mod = __import__(extname, None, None, ['setup'])
      File "/usr/local/lib/python3.7/site-packages/sphinxcontrib/bitbucket.py", line 26
        except AttributeError, err:
                             ^
    SyntaxError: invalid syntax
    
    While I'm here, level up ports/python compliance:
    
      - Add python to CATEGORIES
      - Add LICENSE/LICENSE_FILE
      - Add NO_ARCH
      - Add missing RUN_DEPENDS
      - Correct RUN_DEPENDS version-specs
    
    Approved by:	portmgr (blanket(s): build fix, ports compliance, dependencies)
    
    Approved by:	ports-secteam (blanket: ports fixes)

--- textproc/py-sphinxcontrib-bitbucket/files/patch-setup.py
+++ textproc/py-sphinxcontrib-bitbucket/files/patch-setup.py
@@ -1,5 +1,5 @@
---- ./setup.py.orig	2012-08-20 21:53:58.000000000 +0200
-+++ ./setup.py	2012-08-20 21:54:10.000000000 +0200
+--- setup.py.orig	2011-04-24 16:19:59 UTC
++++ setup.py
 @@ -1,9 +1,5 @@
  # -*- coding: utf-8 -*-
  
@@ -10,7 +10,7 @@
  from setuptools import setup, find_packages
  
  try:
-@@ -43,5 +39,4 @@
+@@ -43,5 +39,4 @@ setup(
      include_package_data=True,
      install_requires=requires,
      namespace_packages=['sphinxcontrib'],
