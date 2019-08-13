8f0834395d4fdc728d242bf6564fb58b4bd45bd4
tobik <tobik@FreeBSD.org>
2019-08-13 17:43:08 +0000

    graphics/svgbob: Unbreak with Rust 1.37.0
    error: trait objects without an explicit `dyn` are deprecated
       --> src/main.rs:163:47
        |
    163 | fn build(args: &ArgMatches) -> Result<(), Box<Error>> {
        |                                               ^^^^^ help: use `dyn`: `dyn Error`
        |
    note: lint level defined here
       --> src/main.rs:1:9
        |
    1   | #![deny(warnings)]
        |         ^^^^^^^^
        = note: #[deny(bare_trait_objects)] implied by #[deny(warnings)]
    
    error: trait objects without an explicit `dyn` are deprecated
       --> src/main.rs:224:68
        |
    224 | fn convert_file(input: PathBuf, output: PathBuf) -> Result<(), Box<Error>> {
        |                                                                    ^^^^^ help: use `dyn`: `dyn Error`
    
    error: aborting due to 2 previous errors
    
    Remove #![deny(warnings)] (basically the equivalent to -Werror in
    C/C++) to fix the build with Rust 1.37.0 or greater.

--- graphics/svgbob/files/patch-src_main.rs
+++ graphics/svgbob/files/patch-src_main.rs
@@ -10,7 +10,12 @@ error: the item `exit` is imported redundantly
 
 --- src/main.rs.orig	2019-04-12 08:16:01 UTC
 +++ src/main.rs
-@@ -83,7 +83,6 @@ fn main() {
+@@ -1,4 +1,3 @@
+-#![deny(warnings)]
+ #[macro_use]
+ extern crate clap;
+ 
+@@ -83,7 +82,6 @@ fn main() {
                  }
                  Err(e) => {
                      use std::io::Write;
@@ -18,7 +23,7 @@ error: the item `exit` is imported redundantly
  
                      writeln!(
                          &mut std::io::stderr(),
-@@ -124,7 +123,6 @@ fn main() {
+@@ -124,7 +122,6 @@ fn main() {
      if let Some(file) = args.value_of("output") {
          if let Err(e) = svg::save(file, &svg) {
              use std::io::Write;
@@ -26,7 +31,7 @@ error: the item `exit` is imported redundantly
  
              writeln!(
                  &mut std::io::stderr(),
-@@ -148,7 +146,6 @@ where
+@@ -148,7 +145,6 @@ where
              Ok(a) => Some(a),
              Err(e) => {
                  use std::io::Write;
