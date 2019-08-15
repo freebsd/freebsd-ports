4b5930e48a6f56f3b6ed302f77a648181f48eaf3
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2019-08-15 16:11:27 +0000

    - Add new fpc bootstrap (i386 and x86_64). It could need less COMPATX   dependencies in kernel - Add patch file to solve some linking problem - Remove LD_UNSAFE. Now it can be defined via LDPATH (/usr/bin/ld.bfd by   default). fpc is not working with lld. We will use binutils-ports when   ld.bfd is moved from base if lld not working on future version   of fpc - Bump PORTREVISION

--- lang/fpc/files/patch-compiler_systems_t__bsd.pas
+++ lang/fpc/files/patch-compiler_systems_t__bsd.pas
@@ -1,6 +1,6 @@
---- compiler/systems/t_bsd.pas.orig	2015-11-13 21:15:29 UTC
-+++ compiler/systems/t_bsd.pas
-@@ -192,6 +192,8 @@ begin
+--- compiler/systems/t_bsd.pas	2014-12-07 15:27:02.000000000 -0500
++++ compiler/systems/t_bsd.pas	2019-08-15 01:12:52.940972000 -0500
+@@ -192,6 +192,8 @@
        DynamicLinker:='/usr/libexec/ld.so'
       else if target_info.system in systems_netbsd then
        DynamicLinker:='/usr/libexec/ld.elf_so'
@@ -9,13 +9,24 @@
       else
         DynamicLinker:='';
     end;
-@@ -718,7 +720,10 @@ begin
+@@ -555,7 +557,7 @@
+      While not SharedLibFiles.Empty do
+       begin
+         S:=SharedLibFiles.GetFirst;
+-        if (s<>'c') or reorder then
++        if ((((((s<>'c') and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) or reorder then
+          begin
+            i:=Pos(target_info.sharedlibext,S);
+            if i>0 then
+@@ -718,7 +720,12 @@
    Replace(cmdstr,'$DYNLINK',DynLinkStr);
    if (target_info.system in systems_darwin) then
      Replace(cmdstr,'$PRTOBJ',GetDarwinPrtobjName(false));
 -  BinStr:=FindUtil(utilsprefix+BinStr);
 +  if target_info.system=system_x86_64_dragonfly then
-+     BinStr:='/usr/libexec/binutils225/elf/ld.bfd'
++     BinStr:='/usr/libexec/binutils225/elf/ld.bfd';
++  if target_info.system in systems_freebsd then
++     BinStr:='%%LDPATH%%'
 +  else
 +     BinStr:=FindUtil(utilsprefix+BinStr);
  
