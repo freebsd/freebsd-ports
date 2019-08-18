f8c6624f529f0703e7b1e5ece555725ae0900f36
zeising <zeising@FreeBSD.org>
2019-08-18 15:08:02 +0000

    x11/xdm: Update to 1.1.12
    Update x11/xdm to 1.1.12.
    This is a big release, incorporating over 7 years worth of changes.
    
    Changelog:
    https://lists.x.org/archives/xorg-announce/2019-March/002959.html
    
    Obtained from:	FreeBSD Graphics Team development repo
    		https://github.com/FreeBSDDesktop/freebsd-ports/tree/feature/xorglibs
    MFH:		2019Q3
    Seucrity:	CVE-2013-2179
    Sponsored by:	B3 Init

--- x11/xdm/files/patch-xdm_session.c
+++ x11/xdm/files/patch-xdm_session.c
@@ -1,6 +1,6 @@
---- xdm/session.c.orig	2011-09-25 07:35:47 UTC
+--- xdm/session.c.orig	2019-03-02 22:06:13 UTC
 +++ xdm/session.c
-@@ -575,6 +575,7 @@ StartClient (
+@@ -594,6 +594,7 @@ StartClient (
      pid_t	pid;
  #ifdef HAVE_SETUSERCONTEXT
      struct passwd* pwd;
@@ -8,7 +8,7 @@
  #endif
  #ifdef USE_PAM
      pam_handle_t *pamh = thepamh ();
-@@ -695,6 +696,8 @@ StartClient (
+@@ -678,6 +679,8 @@ StartClient (
  	 * Set the user's credentials: uid, gid, groups,
  	 * environment variables, resource limits, and umask.
  	 */
@@ -17,7 +17,7 @@
  	pwd = getpwnam(name);
  	if (pwd) {
  	    if (setusercontext(NULL, pwd, pwd->pw_uid, LOGIN_SETALL) < 0) {
-@@ -702,6 +705,7 @@ StartClient (
+@@ -685,6 +688,7 @@ StartClient (
  			  name, _SysErrorMsg (errno));
  		return (0);
  	    }
