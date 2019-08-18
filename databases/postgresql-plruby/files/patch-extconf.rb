ebfce890367e73e4fd1c714fea32c0f34b111f70
Tobias Kortkamp <tobik@FreeBSD.org>
2019-08-18 13:23:40 +0000

    databases/postgresql-plruby: Unbreak build with PostgreSQL 11
    ============================================================
    PostgreSQL 11.4
     is unsupported.  Try plruby-0.4.2.
    ============================================================
    *** Error code 1
    
    http://package18.nyi.freebsd.org/data/120amd64-default-PR239514/2019-07-29_20h41m52s/logs/postgresql-plruby-0.5.7_4.log
    
    While here deprecate it since it seems to have been abandoned in
    2010.
    
    PR:		239639
    Approved by:	knu (maintainer timeout, 2 weeks)

--- databases/postgresql-plruby/files/patch-extconf.rb
+++ databases/postgresql-plruby/files/patch-extconf.rb
@@ -1,9 +1,11 @@
---- extconf.rb.orig	2017-08-31 10:56:23 UTC
+--- extconf.rb.orig	2017-05-28 08:47:01 UTC
 +++ extconf.rb
-@@ -122,6 +122,8 @@ end
+@@ -122,6 +122,10 @@ end
  case version_str = `#{pg_config} --version`
  when /^PostgreSQL ([7-9])\.([0-9]{1,3})(\.[0-9]{1,3})?$/
     version = 10 * $1.to_i + $2.to_i
++when /^PostgreSQL 11/
++   version = 110
 +when /^PostgreSQL 10/
 +   version = 100
  else
