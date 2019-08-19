a4f8c42fb07a27d16998d2e6ab5c3d02be3adce7
kai <kai@FreeBSD.org>
2019-08-19 20:25:28 +0000

    net-mgmt/cacti: Update to 1.2.5
    * Remove the SNMPBIN option that was introduced with r490477 and which is no
      longer required since the 1.2.4 release.
    
      It was used as a workaround to override the internal PHP SNMP calls which
      led to graphs that didn't update within cacti.
    
      The PHP SNMP extension can now be enabled/disabled by a re-purposed
      variable in the configuration file.
    
    While I'm here:
    
    * Remove superfluous "+=" from PLIST_SUB and SUB_LIST because there are no
      previous definitions of those variables.
    
    * Remove CONFLICTS_INSTALL as net-mgmt/cacti88 is no longer present in the
      Ports tree since the end of 2018.
    
    Changes since 1.2.3:
    
    https://github.com/Cacti/cacti/blob/release/1.2.5/CHANGELOG
    
    PR:		238434
    Submitted by:	Michael Muenz <m.muenz@gmail.com>
    Reported by:	Matthew Horan <matt@matthoran.com>
    Approved by:	maintainer timeout (2+ months)
    MFH:		2019Q3

--- net-mgmt/cacti/files/patch-install__functions.php
+++ net-mgmt/cacti/files/patch-install__functions.php
@@ -1,11 +1,11 @@
---- install/functions.php.orig	2019-03-30 23:40:23 UTC
+--- install/functions.php.orig	2019-07-15 19:23:30 UTC
 +++ install/functions.php
-@@ -533,7 +533,7 @@ function install_file_paths() {
- 	if (!config_value_exists('path_cactilog')) {
- 		$input['path_cactilog'] = $settings['path']['path_cactilog'];
- 		if (empty($input['path_cactilog']['default'])) {
--			$input['path_cactilog']['default'] = $config['base_path'] . '/log/cacti.log';
-+			$input['path_cactilog']['default'] = '/var/log/cacti/log';
- 		}
- 	} else {
- 		$input['path_cactilog'] = $settings['path']['path_cactilog'];
+@@ -538,7 +538,7 @@ function install_file_paths() {
+ 	}
+ 
+ 	if (empty($input['path_cactilog']['default'])) {
+-		$input['path_cactilog']['default'] = $config['base_path'] . '/log/cacti.log';
++		$input['path_cactilog']['default'] = '/var/log/cacti/log';
+ 	}
+ 
+ 	/* stderr log file path */
