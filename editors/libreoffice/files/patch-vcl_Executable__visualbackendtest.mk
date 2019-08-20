f8ee82d199540436901f32987c32d8f06ba2b3ab
lwhsu <lwhsu@FreeBSD.org>
2019-08-20 02:18:04 +0000

    - Update to 6.3.0

--- editors/libreoffice/files/patch-vcl_Executable__visualbackendtest.mk
+++ editors/libreoffice/files/patch-vcl_Executable__visualbackendtest.mk
@@ -1,6 +1,6 @@
---- vcl/Executable_visualbackendtest.mk.orig	2017-05-03 16:46:29 UTC
-+++ vcl/Executable_visualbackendtest.mk
-@@ -41,13 +41,12 @@ $(eval $(call gb_Executable_use_static_libraries,visua
+--- vcl/Executable_visualbackendtest.mk.orig	2019-08-07 01:26:35.000000000 +0800
++++ vcl/Executable_visualbackendtest.mk	2019-08-08 20:07:11.830059000 +0800
+@@ -41,11 +41,10 @@
      vclmain \
  ))
  
@@ -9,9 +9,8 @@
  $(eval $(call gb_Executable_add_libs,visualbackendtest,\
 -	-lm \
 -	-ldl \
-+	-lm $(DLOPEN_LIBS) \
- 	-lpthread \
 -    -lX11 \
++	-lm $(DLOPEN_LIBS) \
 +	-lX11 \
  ))
  
