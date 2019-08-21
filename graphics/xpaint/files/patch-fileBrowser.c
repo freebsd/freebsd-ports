a11b067171daab1da7b0c8c92fad9288d54499eb
rene <rene@FreeBSD.org>
2019-08-21 20:04:18 +0000

    graphics/xpaint: fix build after removal of sys/dir.h in HEAD

--- graphics/xpaint/files/patch-fileBrowser.c
+++ graphics/xpaint/files/patch-fileBrowser.c
@@ -1,20 +1,26 @@
 --- fileBrowser.c.orig	2014-05-08 13:50:48 UTC
 +++ fileBrowser.c
-@@ -47,7 +47,7 @@ extern Xaw3dXftData *xaw3dxft_data;
+@@ -47,11 +47,7 @@ extern Xaw3dXftData *xaw3dxft_data;
  #include <stdio.h>
  #include <sys/types.h>
  #include <sys/stat.h>
 -#if defined(SYSV) || defined(SVR4) || defined(__CYGWIN__) || defined(__VMS )
-+#if defined(SYSV) || defined(SVR4) || defined(__CYGWIN__) || defined(__VMS ) || defined(BSD)
  #include <dirent.h>
- #else
- #include <sys/dir.h>
-@@ -562,7 +562,7 @@ static void 
+-#else
+-#include <sys/dir.h>
+-#endif
+ #include <pwd.h>
+ 
+ #include "rw/rwTable.h"
+@@ -562,11 +558,7 @@ static void 
  setCWD(arg_t * arg, char *dir)
  {
      DIR *dirp;
 -#if defined(SYSV) || defined(SVR4) || defined(__alpha) || defined(__CYGWIN__)
-+#if defined(SYSV) || defined(SVR4) || defined(__alpha) || defined(__CYGWIN__) || defined(BSD)
      struct dirent *e;
- #else
-     struct direct *e;
+-#else
+-    struct direct *e;
+-#endif
+     int count = 0, i = 0;
+     int dirCount = 0, fileCount = 0;
+     String *list;
