3ab4e3fe003b46a3dbb70c7177a30cb9ab542e14
Kai Knoblich <kai@FreeBSD.org>
2019-08-21 07:47:44 +0000

    MFH: r509310
    net-mgmt/cacti: Update to 1.2.5
    
    * Remove the SNMPBIN option that was introduced with r490477 and which is no
      longer required since the 1.2.4 release.
    
      It was used as a workaround to override the internal PHP SNMP calls which
      led to graphs that didn't update within cacti.
    
      The PHP SNMP extension can now be enabled/disabled by a re-purposed
      variable in the configuration file.
    
    While I'm here:
    
    * Remove superfluous "+=" from PLIST_SUB and SUB_LIST because there are no
      previous definitions of those variables.
    
    * Remove CONFLICTS_INSTALL as net-mgmt/cacti88 is no longer present in the
      Ports tree since the end of 2018.
    
    Changes since 1.2.3:
    
    https://github.com/Cacti/cacti/blob/release/1.2.5/CHANGELOG
    
    * Merge the UCL convertions from r508903 while I'm here.
    
    PR:		238434
    Submitted by:	Michael Muenz <m.muenz@gmail.com>
    Reported by:	Matthew Horan <matt@matthoran.com>
    Approved by:	maintainer timeout (2+ months)
    Approved by:	ports-secteam (joneum)

--- net-mgmt/cacti/files/patch-lib_installer.php
+++ net-mgmt/cacti/files/patch-lib_installer.php
@@ -1,11 +1,11 @@
---- lib/installer.php.orig	2019-01-06 07:22:22 UTC
+--- lib/installer.php.orig	2019-07-26 03:05:22 UTC
 +++ lib/installer.php
-@@ -3222,7 +3222,7 @@ class Installer implements JsonSerializable {
- 
- 	public static function getInstallLog() {
+@@ -3216,7 +3216,7 @@ class Installer implements JsonSerializable {
  		global $config;
--		$logcontents = tail_file($config['base_path'] . '/log/cacti.log', 100, -1, ' INSTALL:' , 1, $total_rows);
-+		$logcontents = tail_file('/var/log/cacti/log', 100, -1, ' INSTALL:' , 1, $total_rows);
+ 
+ 		$page_nr = 1;
+-		$logcontents = tail_file($config['base_path'] . '/log/cacti.log', 100, -1, ' INSTALL:' , $page_no, $total_rows);
++		$logcontents = tail_file('/var/log/cacti/log', 100, -1, ' INSTALL:' , $page_no, $total_rows);
  
  		$output_log = '';
  		foreach ($logcontents as $logline) {
