d5873280d25d6d30745a8eea220a72118552bc56
yuri <yuri@FreeBSD.org>
2019-08-31 14:55:12 +0000

    devel/emscripten: Update 1.38.41 -> 1.38.43
    Reported by:	portscout

--- devel/emscripten/files/patch-tools_settings__template__readonly.py
+++ devel/emscripten/files/patch-tools_settings__template__readonly.py
@@ -1,6 +1,6 @@
---- tools/settings_template_readonly.py.orig	2019-05-09 05:05:10 UTC
+--- tools/settings_template_readonly.py.orig	2019-08-31 14:51:24 UTC
 +++ tools/settings_template_readonly.py
-@@ -17,6 +17,10 @@ EMSCRIPTEN_ROOT = os.path.expanduser(os.
+@@ -17,6 +17,10 @@ EMSCRIPTEN_ROOT = os.path.expanduser(os.getenv('EMSCRI
  LLVM_ROOT = os.path.expanduser(os.getenv('LLVM', '{{{ LLVM_ROOT }}}')) # directory
  BINARYEN_ROOT = os.path.expanduser(os.getenv('BINARYEN', '')) # if not set, we will use it from ports
  
@@ -8,6 +8,6 @@
 +LLVM_ADD_VERSION = 'devel'
 +CLANG_ADD_VERSION = 'devel'
 +
- # If not specified, defaults to sys.executable.
- # PYTHON = 'python'
- 
+ # Add this if you have manually built the JS optimizer executable (in
+ # Emscripten/tools/optimizer) and want to run it from a custom location.
+ # Alternatively, you can set this as the environment variable
