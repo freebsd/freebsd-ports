d3354f8dab64a06f8b6034e3edd93a8101e0e458
Kurt Jaeger <pi@FreeBSD.org>
2019-09-01 15:12:50 +0000

    net/kamailio: refactoring
    - improved options handling
    - improved building
    - fixed plist
    
    PR:		240206
    Submitted by:	Dmitry Wagin <dmitry.wagin@ya.ru>

--- net/kamailio/files/patch-src_modules_tls_tls__bio.c
+++ net/kamailio/files/patch-src_modules_tls_tls__bio.c
@@ -1,6 +1,6 @@
---- src/modules/tls/tls_bio.c.orig	2018-11-28 11:08:42.000000000 +0100
-+++ src/modules/tls/tls_bio.c	2018-12-12 18:28:16.580475000 +0100
-@@ -63,7 +63,7 @@
+--- src/modules/tls/tls_bio.c.orig	2019-08-14 09:43:03 UTC
++++ src/modules/tls/tls_bio.c
+@@ -63,7 +63,7 @@ static int tls_bio_mbuf_puts(BIO* b, const char* s);
  static long tls_bio_mbuf_ctrl(BIO* b, int cmd, long arg1, void* arg2);
  
  
@@ -9,7 +9,7 @@
  static BIO_METHOD tls_mbuf_method = {
  	BIO_TYPE_TLS_MBUF,	/* type */
  	"sr_tls_mbuf",		/* name */
-@@ -109,7 +109,7 @@
+@@ -109,7 +109,7 @@ static BIO_METHOD *tls_mbuf_method = NULL;
  /** returns a custom tls_mbuf BIO. */
  BIO_METHOD* tls_BIO_mbuf(void)
  {
