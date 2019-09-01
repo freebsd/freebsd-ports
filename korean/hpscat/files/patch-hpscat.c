562051b9e35ff9b1f55042ca0d701d8a2fcd724b
Li-Wen Hsu <lwhsu@FreeBSD.org>
2019-09-01 16:42:05 +0000

    Add a patch to remove use of gets(3)
    PR:		238690
    Submitted by:	Walter Schwarzenfeld <w.schwarzenfeld at utanet.at>
    Reported by:	emaste
    Sponsored by:	The FreeBSD Foundation

--- korean/hpscat/files/patch-hpscat.c
+++ korean/hpscat/files/patch-hpscat.c
@@ -1,38 +1,11 @@
---- hpscat.c.orig	1995-11-23 07:46:12.000000000 -0500
-+++ hpscat.c	2013-06-12 17:10:59.000000000 -0400
-@@ -12,13 +12,14 @@
- bool cheat;
- FILE *fp, *ofp;
- 
-+static int open_outfile(char *);
-+
- /****************
-    main routine
-  ****************/
- 
--main(ac,av)
--  int  ac;
--  char *av[];
-+int
-+main(int ac, char *av[])
- {
-     int  i;
-     extern void showfile PROT((char *));
-@@ -53,13 +54,13 @@
-     exit(0);
- }
- 
--int
--open_outfile(s)    /*  returns -1 if can't write */
--  char *s;
-+static int
-+open_outfile(char *s)    /*  returns -1 if can't write */
- {
-     char ans[80], buf[256];
-     sprintf(buf,"%s.ps", s);
--    if(ofp=fopen(buf,"r")) { /* file exists */
-+    ofp=fopen(buf,"r");
-+    if(ofp) { /* file exists */
+--- hpscat.c.orig	2019-08-03 11:25:44 UTC
++++ hpscat.c
+@@ -62,7 +62,7 @@ open_outfile(s)    /*  returns -1 if can
+     if(ofp=fopen(buf,"r")) { /* file exists */
  	fclose(ofp);
  	fprintf(stderr,"output file %s exists...overwrite? ", buf);
- 	gets(ans);
+-	gets(ans);
++	fgets(buf,256,ans);
+ 	if(ans[0]=='y')
+ 	    unlink(buf);
+ 	else 
