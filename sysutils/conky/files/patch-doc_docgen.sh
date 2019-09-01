6df27236b177541fc3e165df29e3c55a33412b0a
madpilot <madpilot@FreeBSD.org>
2019-09-01 19:23:53 +0000

    Fix document generation.
    PR:		240244
    Submitted by:	James Stark <james.e.stark@gmail.com>
    MFH:		2019Q3

--- sysutils/conky/files/patch-doc_docgen.sh
+++ sysutils/conky/files/patch-doc_docgen.sh
@@ -1,16 +1,13 @@
---- doc/docgen.sh.orig	2016-07-20 16:53:48 UTC
+--- doc/docgen.sh.orig	2019-08-12 21:53:54 UTC
 +++ doc/docgen.sh
-@@ -1,8 +1,11 @@
+@@ -1,6 +1,9 @@
  #!/bin/sh
  
 -xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl docs.xml > docs.html && \
-+xsltproc LOCALBASE/share/xsl/docbook/manpages/docbook.xsl docs.xml > docs.html && \
-+xsltproc LOCALBASE/share/xsl/docbook/manpages/docbook.xsl variables.xml > variables.html && \
-+xsltproc LOCALBASE/share/xsl/docbook/manpages/docbook.xsl config_settings.xml > config_settings.html && \
-+xsltproc LOCALBASE/share/xsl/docbook/manpages/docbook.xsl lua.xml > lua.html && \
++xsltproc LOCALBASE/share/xsl/docbook/html/docbook.xsl docs.xml > docs.html && \
++xsltproc LOCALBASE/share/xsl/docbook/html/docbook.xsl variables.xml > variables.html && \
++xsltproc LOCALBASE/share/xsl/docbook/html/docbook.xsl config_settings.xml > config_settings.html && \
++xsltproc LOCALBASE/share/xsl/docbook/html/docbook.xsl lua.xml > lua.html && \
  db2x_xsltproc -s man docs.xml -o docs.mxml && \
--db2x_manxml docs.mxml && \
-+db2x_manxml docs.mxml
+ db2x_manxml docs.mxml && \
  { echo ".TH CONKY 1 \"August 2005\" \"conky compiled August 2005\" \"User Commands\""; sed 1d < conky.1; } > conky.2 && \
- mv conky.2 conky.1 && \
- man ./conky.1 | col -b > README && \
