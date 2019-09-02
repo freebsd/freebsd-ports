4cecbbcf472d7b8ae1c9626b39497d565a39e87e
Jan Beich <jbeich@FreeBSD.org>
2019-09-02 15:43:57 +0000

    MFH: r509978 r510809
    www/firefox: update to 69.0
    
    Changes:	https://www.mozilla.org/firefox/69.0/releasenotes/
    PR:		239155
    Security:	05463e0a-abd3-4fa4-bd5f-cd5ed132d4c6
    Approved by:	ports-secteam (joneum)
    Differential Revision:	https://reviews.freebsd.org/D20932

--- www/firefox/files/patch-dom_media_flac_FlacDecoder.cpp
+++ www/firefox/files/patch-dom_media_flac_FlacDecoder.cpp
@@ -12,13 +12,14 @@ index 53fc3c9937f7..b23771ab80fa 100644
  
  namespace mozilla {
  
-@@ -15,6 +16,10 @@ FlacDecoder::IsEnabled()
- {
+@@ -14,6 +15,11 @@ namespace mozilla {
+ bool FlacDecoder::IsEnabled() {
  #ifdef MOZ_FFVPX
-   return StaticPrefs::MediaFlacEnabled();
+   return StaticPrefs::media_flac_enabled();
 +#elif defined(MOZ_FFMPEG)
 +  RefPtr<PDMFactory> platform = new PDMFactory();
-+  return StaticPrefs::MediaFlacEnabled() && platform->SupportsMimeType(NS_LITERAL_CSTRING("audio/flac"),
++  return StaticPrefs::media_flac_enabled() &&
++         platform->SupportsMimeType(NS_LITERAL_CSTRING("audio/flac"),
 +                                    /* DecoderDoctorDiagnostics* */ nullptr);
  #else
    // Until bug 1295886 is fixed.
