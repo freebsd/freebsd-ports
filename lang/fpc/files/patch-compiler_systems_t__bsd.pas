41baa577bb8d8907dd6de1a9cd29b5390a2c8d88
acm <acm@FreeBSD.org>
2019-09-03 15:51:50 +0000

    - Rebuild bootstrap (ppcx64 and ppc386) with ld patches - Fix build on 12-STABLE and CURRENT (amd64 and i386) - Bump PORTREVISION for all ports that depends of lang/fpc - Add USE_BINUTILS to fpc and lazarus based ports - Add binutils dependency to Uses/fpc.mk and Uses/lazarus.mk
    PR:		240293 239934 233413 214864
    Exp-run by:	antoine

--- lang/fpc/files/patch-compiler_systems_t__bsd.pas
+++ lang/fpc/files/patch-compiler_systems_t__bsd.pas
@@ -1,5 +1,45 @@
 --- compiler/systems/t_bsd.pas	2014-12-07 15:27:02.000000000 -0500
-+++ compiler/systems/t_bsd.pas	2019-08-15 01:12:52.940972000 -0500
++++ compiler/systems/t_bsd.pas	2019-09-02 21:10:43.316197000 -0500
+@@ -146,8 +146,8 @@
+        begin
+          if not(target_info.system in systems_darwin) then
+            begin
+-             ExeCmd[1]:='ld $TARGET $EMUL $OPT $DYNLINK $STATIC $GCSECTIONS $STRIP -L. -o $EXE $CATRES';
+-             DllCmd[1]:='ld $TARGET $EMUL $OPT -shared -L. -o $EXE $CATRES'
++             ExeCmd[1]:='%%LDPATH%% $TARGET $EMUL $OPT $DYNLINK $STATIC $GCSECTIONS $STRIP -L. -o $EXE $CATRES';
++             DllCmd[1]:='%%LDPATH%% $TARGET $EMUL $OPT -shared -L. -o $EXE $CATRES'
+            end
+          else
+            begin
+@@ -166,22 +166,22 @@
+                programs with problems that require Valgrind will have more
+                than 60KB of data (first 4KB of address space is always invalid)
+              }
+-               ExeCmd[1]:='ld $PRTOBJ $TARGET $EMUL $OPT $DYNLINK $STATIC $GCSECTIONS $STRIP -multiply_defined suppress -L. -o $EXE $CATRES';
++               ExeCmd[1]:='%%LDPATH%% $PRTOBJ $TARGET $EMUL $OPT $DYNLINK $STATIC $GCSECTIONS $STRIP -multiply_defined suppress -L. -o $EXE $CATRES';
+              if not(cs_gdb_valgrind in current_settings.globalswitches) then
+                ExeCmd[1]:=ExeCmd[1]+' -pagezero_size 0x10000';
+ {$else ndef cpu64bitaddr}
+-             ExeCmd[1]:='ld $PRTOBJ $TARGET $EMUL $OPT $DYNLINK $STATIC $GCSECTIONS $STRIP -multiply_defined suppress -L. -o $EXE $CATRES';
++             ExeCmd[1]:='%%LDPATH%% $PRTOBJ $TARGET $EMUL $OPT $DYNLINK $STATIC $GCSECTIONS $STRIP -multiply_defined suppress -L. -o $EXE $CATRES';
+ {$endif ndef cpu64bitaddr}
+              if (apptype<>app_bundle) then
+-               DllCmd[1]:='ld $PRTOBJ $TARGET $EMUL $OPT $GCSECTIONS -dynamic -dylib -multiply_defined suppress -L. -o $EXE $CATRES'
++               DllCmd[1]:='%%LDPATH%% $PRTOBJ $TARGET $EMUL $OPT $GCSECTIONS -dynamic -dylib -multiply_defined suppress -L. -o $EXE $CATRES'
+              else
+-               DllCmd[1]:='ld $PRTOBJ $TARGET $EMUL $OPT $GCSECTIONS -dynamic -bundle -multiply_defined suppress -L. -o $EXE $CATRES'
++               DllCmd[1]:='%%LDPATH%% $PRTOBJ $TARGET $EMUL $OPT $GCSECTIONS -dynamic -bundle -multiply_defined suppress -L. -o $EXE $CATRES'
+            end
+        end
+      else
+        begin
+-         ExeCmd[1]:='ld $TARGET $EMUL $OPT $DYNLINK $STATIC  $GCSECTIONS $STRIP -L. -o $EXE $RES';
+-         DllCmd[1]:='ld $TARGET $EMUL $OPT $INIT $FINI $SONAME -shared -L. -o $EXE $RES';
++         ExeCmd[1]:='%%LDPATH%% $TARGET $EMUL $OPT $DYNLINK $STATIC  $GCSECTIONS $STRIP -L. -o $EXE $RES';
++         DllCmd[1]:='%%LDPATH%% $TARGET $EMUL $OPT $INIT $FINI $SONAME -shared -L. -o $EXE $RES';
+        end;
+      if not(target_info.system in systems_darwin) then
+        DllCmd[2]:='strip --strip-unneeded $EXE'
 @@ -192,6 +192,8 @@
        DynamicLinker:='/usr/libexec/ld.so'
       else if target_info.system in systems_netbsd then
@@ -14,7 +54,7 @@
        begin
          S:=SharedLibFiles.GetFirst;
 -        if (s<>'c') or reorder then
-+        if ((((((s<>'c') and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) or reorder then
++        if (((((((s<>'c') and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) and (s<>'iconv')) or reorder then
           begin
             i:=Pos(target_info.sharedlibext,S);
             if i>0 then
