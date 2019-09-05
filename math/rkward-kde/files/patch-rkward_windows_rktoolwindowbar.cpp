970c23e3a81690ed40c6d463c37cb3083cedd534
Tobias C. Berner <tcberner@FreeBSD.org>
2019-09-05 17:26:20 +0000

    math/rkward-kde: add missing includes
    PR:		238782

---
+++ math/rkward-kde/files/patch-rkward_windows_rktoolwindowbar.cpp
@@ -0,0 +1,10 @@
+--- rkward/windows/rktoolwindowbar.cpp.orig	2019-09-04 19:20:47 UTC
++++ rkward/windows/rktoolwindowbar.cpp
+@@ -24,6 +24,7 @@
+ #include <kparts/partmanager.h>
+ #include <kselectaction.h>
+ 
++#include <QLayout>
+ #include <QSplitter>
+ #include <QContextMenuEvent>
+ 
