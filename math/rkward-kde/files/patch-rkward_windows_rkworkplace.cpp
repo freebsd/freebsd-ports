0789a544d2f8f4dc47b16c090183dd756796d8d7
tcberner <tcberner@FreeBSD.org>
2019-09-05 17:26:20 +0000

    math/rkward-kde: add missing includes
    PR:		238782

---
+++ math/rkward-kde/files/patch-rkward_windows_rkworkplace.cpp
@@ -0,0 +1,10 @@
+--- rkward/windows/rkworkplace.cpp.orig	2019-09-04 19:06:32 UTC
++++ rkward/windows/rkworkplace.cpp
+@@ -31,6 +31,7 @@
+ #include <QDir>
+ #include <QApplication>
+ #include <QMimeDatabase>
++#include <QVBoxLayout>
+ 
+ #include "detachedwindowcontainer.h"
+ #include "rkcommandeditorwindow.h"
