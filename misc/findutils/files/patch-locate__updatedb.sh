17057c81a5860d62e88672cb28ccda9679d2167f
dbaio <dbaio@FreeBSD.org>
2019-09-08 19:40:14 +0000

    misc/findutils: Update to 4.7.0
    Changelog:	https://lists.gnu.org/archive/html/bug-findutils/2019-08/msg00027.html
    
    PR:		240420
    Submitted by:	Klaus Aehlig <aehlig@linta.de> (maintainer)

--- misc/findutils/files/patch-locate__updatedb.sh
+++ misc/findutils/files/patch-locate__updatedb.sh
@@ -1,6 +1,6 @@
---- locate/updatedb.sh.orig	2011-04-03 23:53:12.000000000 +0100
-+++ locate/updatedb.sh	2011-05-17 18:20:29.000000000 +0100
-@@ -250,7 +250,7 @@
+--- locate/updatedb.sh.orig	2019-06-23 09:00:40 UTC
++++ locate/updatedb.sh
+@@ -292,7 +292,7 @@ cd "$changeto"
  if test -n "$SEARCHPATHS"; then
    if [ "$LOCALUSER" != "" ]; then
      # : A1
@@ -9,28 +9,10 @@
      "$find $SEARCHPATHS $FINDOPTIONS \
       \\( $prunefs_exp \
       -type d -regex '$PRUNEREGEX' \\) -prune -o $print_option"
-@@ -266,7 +266,7 @@
+@@ -308,7 +308,7 @@ if test -n "$NETPATHS"; then
  myuid=`getuid`
  if [ "$myuid" = 0 ]; then
      # : A3
--    su $NETUSER `select_shell $NETUSER` -c \
-+    su -m $NETUSER -c \
-      "$find $NETPATHS $FINDOPTIONS \\( -type d -regex '$PRUNEREGEX' -prune \\) -o $print_option" ||
-     exit $?
-   else
-@@ -319,7 +319,7 @@
- if test -n "$SEARCHPATHS"; then
-   if [ "$LOCALUSER" != "" ]; then
-     # : A5
--    su $LOCALUSER `select_shell $LOCALUSER` -c \
-+    su -m $LOCALUSER -c \
-     "$find $SEARCHPATHS $FINDOPTIONS \
-      \( $prunefs_exp \
-      -type d -regex '$PRUNEREGEX' \) -prune -o $print_option" || exit $?
-@@ -335,7 +335,7 @@
-   myuid=`getuid`
-   if [ "$myuid" = 0 ]; then
-     # : A7
 -    su $NETUSER `select_shell $NETUSER` -c \
 +    su -m $NETUSER -c \
       "$find $NETPATHS $FINDOPTIONS \\( -type d -regex '$PRUNEREGEX' -prune \\) -o $print_option" ||
