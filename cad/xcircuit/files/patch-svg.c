137b178d775cc82871e7e5c92cb59bc054773fdf
Danilo Egea Gondolfo <danilo@FreeBSD.org>
2019-09-09 11:58:29 +0000

    - Update to 3.10.13

--- cad/xcircuit/files/patch-svg.c
+++ cad/xcircuit/files/patch-svg.c
@@ -1,11 +1,12 @@
---- svg.c.orig	2009-09-23 21:50:37.000000000 +0900
-+++ svg.c	2009-11-09 22:27:21.000000000 +0900
-@@ -8,6 +8,7 @@
+--- svg.c.orig	2019-09-08 12:19:55 UTC
++++ svg.c
+@@ -7,9 +7,7 @@
+ #include <stdlib.h>
+ #include <stdint.h>
  #include <string.h>
+-#ifndef XC_WIN32
+ #include <unistd.h>
+-#endif
  #include <math.h>
  #include <limits.h>
-+#include <unistd.h>
  #include <sys/stat.h>
- #include <sys/wait.h>
- 
-
