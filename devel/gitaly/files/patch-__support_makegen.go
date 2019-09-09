bec00067e2ee33465435290c39350bd34d675947
Matthias Fechner <mfechner@FreeBSD.org>
2019-09-09 10:26:45 +0000

    devel/gitaly: upgrade to 1.59.2 which is required for gitlab-ce 12.2.

--- devel/gitaly/files/patch-__support_makegen.go
+++ devel/gitaly/files/patch-__support_makegen.go
@@ -1,6 +1,6 @@
---- _support/makegen.go.orig	2019-06-18 09:53:31 UTC
+--- _support/makegen.go.orig	2019-08-22 16:24:24 UTC
 +++ _support/makegen.go
-@@ -282,7 +282,7 @@ var templateText = `
+@@ -320,7 +320,7 @@ var templateText = `
  # These variables may be overridden at runtime by top-level make
  PREFIX ?= /usr/local
  INSTALL_DEST_DIR := $(DESTDIR)$(PREFIX)/bin/
@@ -9,13 +9,13 @@
  ASSEMBLY_ROOT ?= {{ .BuildDir }}/assembly
  BUILD_TAGS := tracer_static tracer_static_jaeger
  
-@@ -330,8 +330,8 @@ build: ../.ruby-bundle
+@@ -368,8 +368,8 @@ build: ../.ruby-bundle
  # step. Both Omnibus and CNG assume it is in the Gitaly root, not in
  # _build. Hence the '../' in front.
  ../.ruby-bundle:  {{ .GitalyRubyDir }}/Gemfile.lock  {{ .GitalyRubyDir }}/Gemfile
 +	cd  {{ .GitalyRubyDir }} && rm Gemfile.lock && bundle install $(BUNDLE_FLAGS)
  	cd  {{ .GitalyRubyDir }} && bundle config # for debugging
 -	cd  {{ .GitalyRubyDir }} && bundle install $(BUNDLE_FLAGS)
- 	cd  {{ .GitalyRubyDir }} && bundle show gitaly-proto # sanity check
  	touch $@
  
+ .PHONY: install
