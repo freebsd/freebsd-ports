175b46ed83900b522e4160f1acdea8b16bc420dc
brnrd <brnrd@FreeBSD.org>
2019-09-11 09:38:31 +0000

    security/openssl111: Security update to 1.1.1d
     - Fix RC2 and RC4 options [1]
     - Add man7 to API documentation knob MAN3
    
    PR:		240283 [1]
    Submitted by:	adamw
    MFH:		2019Q3
    Security:	9e0c6f7a-d46d-11e9-a1c7-b499baebfeaf

--- security/openssl111/files/extra-patch-util_process__docs.pl
+++ security/openssl111/files/extra-patch-util_process__docs.pl
@@ -5,7 +5,7 @@
  
  unless ($options{section}) {
 -    $options{section} = [ 1, 3, 5, 7 ];
-+    $options{section} = [ 1, 5, 7 ];
++    $options{section} = [ 1, 5 ];
  }
  unless ($options{sourcedir}) {
      $options{sourcedir} = catdir($config{sourcedir}, "doc");
@@ -14,7 +14,7 @@
                         html => ".html" } -> {$options{type}};
          my $generate = { man  => "pod2man --name=$name --section=$podinfo{section} --center=OpenSSL --release=$config{version} \"$podpath\"",
 -                         html => "pod2html \"--podroot=$options{sourcedir}\" --htmldir=$updir --podpath=man1:man3:man5:man7 \"--infile=$podpath\" \"--title=$podname\" --quiet"
-+                         html => "pod2html \"--podroot=$options{sourcedir}\" --htmldir=$updir --podpath=man1:man5:man7 \"--infile=$podpath\" \"--title=$podname\" --quiet"
++                         html => "pod2html \"--podroot=$options{sourcedir}\" --htmldir=$updir --podpath=man1:man5 \"--infile=$podpath\" \"--title=$podname\" --quiet"
                           } -> {$options{type}};
          my $output_dir = catdir($options{destdir}, "man$podinfo{section}");
          my $output_file = $podname . $suffix;
