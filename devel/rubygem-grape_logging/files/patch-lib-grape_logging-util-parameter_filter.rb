9b1f3c4fdba14a6a2a4029a8f095c116b9a3255f
sunpoet <sunpoet@FreeBSD.org>
2019-09-14 21:10:58 +0000

    Update to 1.8.3
    Changes:	https://github.com/aserafin/grape_logging/releases

--- devel/rubygem-grape_logging/files/patch-lib-grape_logging-util-parameter_filter.rb
+++
@@ -1,9 +0,0 @@
---- lib/grape_logging/util/parameter_filter.rb.orig	2019-09-07 15:56:34 UTC
-+++ lib/grape_logging/util/parameter_filter.rb
-@@ -1,5 +1,5 @@
- if defined?(::Rails.application)
--  class ParameterFilter < ActiveSupport::ParameterFilter
-+  class ParameterFilter < ActionDispatch::Http::ParameterFilter
-     def initialize(_replacement, filter_parameters)
-       super(filter_parameters)
-     end
