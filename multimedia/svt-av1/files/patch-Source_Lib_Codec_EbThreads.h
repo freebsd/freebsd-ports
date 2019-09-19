baba89b62dbe561d3de3cb32a31dcea4cdea098d
Jan Beich <jbeich@FreeBSD.org>
2019-09-19 00:46:10 +0000

    multimedia/svt-av1: update to 0.6.0.358
    Changes:	https://github.com/OpenVisualCloud/SVT-AV1/compare/4f180cce...48c155aa

--- multimedia/svt-av1/files/patch-Source_Lib_Codec_EbThreads.h
+++ multimedia/svt-av1/files/patch-Source_Lib_Codec_EbThreads.h
@@ -5,12 +5,12 @@ Source/Lib/Codec/EbThreads.h:100:11: error: unknown type name 'cpu_set_t'
 extern    cpu_set_t                   groupAffinity;
           ^
 
---- Source/Lib/Common/Codec/EbThreads.h.orig	2019-02-14 00:36:54 UTC
+--- Source/Lib/Common/Codec/EbThreads.h.orig	2019-09-18 21:33:26 UTC
 +++ Source/Lib/Common/Codec/EbThreads.h
-@@ -95,7 +95,14 @@ extern    EB_BOOL                  alternateGroups;
- #else
- #define __USE_GNU
+@@ -99,8 +99,15 @@ extern "C" {
+ #ifndef _GNU_SOURCE
  #define _GNU_SOURCE
+ #endif
 +#ifdef __FreeBSD__
 +#define cpu_set_t cpuset_t
 +#else
@@ -20,5 +20,6 @@ extern    cpu_set_t                   groupAffinity;
 +#if defined(__DragonFly__) || defined(__FreeBSD__)
 +#include <pthread_np.h>
 +#endif
- extern    cpu_set_t                   groupAffinity;
- #define EB_CREATETHREAD(type, pointer, nElements, pointerClass, threadFunction, threadContext) \
+ extern    cpu_set_t                   group_affinity;
+ #define EB_CREATE_THREAD(pointer, thread_function, thread_context) \
+     do { \
