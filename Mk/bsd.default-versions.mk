3f1915711db5e32dd082295bb1a3e4eb36969d34
Jan Beich <jbeich@FreeBSD.org>
2019-09-20 19:58:36 +0000

    Switch default devel/llvm* to 90
    PR:		239682
    Reviewed by:	tobik
    Differential Revision:	https://reviews.freebsd.org/D21172

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -57,10 +57,10 @@ JULIA_DEFAULT?=		1.0
 LAZARUS_DEFAULT?=	2.0.4
 # Possible values: c6, c7
 LINUX_DEFAULT?=		c7
-# Possible values: 60, 70, 80, -devel (to be used when non-base compiler is required)
+# Possible values: 60, 70, 80, 90, -devel (to be used when non-base compiler is required)
 # Please give notice to the Graphics Team (x11@FreeBSD.org) in advance before 
 # bumping the LLVM version.
-LLVM_DEFAULT?=		80
+LLVM_DEFAULT?=		90
 # Possible values: 5.1, 5.2, 5.3
 LUA_DEFAULT?=		5.2
 # Possible values: 5.5, 5.6, 5.7, 8.0, 5.5m, 10.0m, 10.1m, 10.2m, 10.3m, 5.5p, 5.6p, 5.7p, 5.6w
