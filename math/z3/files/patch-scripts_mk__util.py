1ba6693a6a6d692b2659a29a68dae32a5e732850
arrowd <arrowd@FreeBSD.org>
2019-09-21 20:32:14 +0000

    math/z3: Update to 4.8.6.

--- math/z3/files/patch-scripts_mk__util.py
+++ math/z3/files/patch-scripts_mk__util.py
@@ -1,19 +1,12 @@
---- scripts/mk_util.py.orig	2017-12-18 14:18:30 UTC
+--- scripts/mk_util.py.orig	2019-09-19 23:43:06 UTC
 +++ scripts/mk_util.py
-@@ -49,7 +49,7 @@ C_COMPILERS=['gcc', 'clang']
- CSC_COMPILERS=['csc', 'mcs']
- JAVAC=None
- JAR=None
--PYTHON_PACKAGE_DIR=distutils.sysconfig.get_python_lib()
-+PYTHON_PACKAGE_DIR=distutils.sysconfig.get_python_lib(prefix=getenv("PREFIX", None))
- BUILD_DIR='build'
- REV_BUILD_DIR='..'
- SRC_DIR='src'
-@@ -2441,7 +2441,7 @@ def mk_config():
+@@ -2508,8 +2508,8 @@ def mk_config():
          check_ar()
          CXX = find_cxx_compiler()
          CC  = find_c_compiler()
 -        SLIBEXTRAFLAGS = ''
+-#       SLIBEXTRAFLAGS = '%s -Wl,-soname,libz3.so.0' % LDFLAGS
++#       SLIBEXTRAFLAGS = ''
 +        SLIBEXTRAFLAGS = '%s -Wl,-soname,libz3.so.0' % LDFLAGS
          EXE_EXT = ''
          LIB_EXT = '.a'
