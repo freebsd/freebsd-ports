9e0bc9533359ec4266e5ae5672016589fd627209
Greg Lewis <glewis@FreeBSD.org>
2019-09-21 22:03:06 +0000

    Make it possible to enable dump logs
    * Enable the DUMPLOG configuration option
    * Configure an appropriate pattern for the dump log file name.
    
    Note, to actually enable dump logs you will need to edit sysconf and
    uncomment the DUMPLOGFILE name variable.

--- games/nethack36/files/patch-include-config.h
+++ games/nethack36/files/patch-include-config.h
@@ -1,6 +1,6 @@
---- include/config.h.orig	2015-11-17 05:58:34 UTC
-+++ include/config.h
-@@ -250,11 +250,11 @@
+--- include/config.h.orig	2018-04-27 05:07:22.000000000 -0700
++++ include/config.h	2019-04-26 07:32:12.113908000 -0700
+@@ -256,11 +256,11 @@
  
  #if defined(UNIX) && !defined(ZLIB_COMP) && !defined(COMPRESS)
  /* path and file name extension for compression program */
@@ -16,7 +16,7 @@
  #endif
  
  #ifndef COMPRESS
-@@ -317,7 +317,7 @@
+@@ -345,7 +345,7 @@
   * otherwise it will be the current directory.
   */
  #ifndef HACKDIR
@@ -25,3 +25,21 @@
  #endif
  
  /*
+@@ -520,7 +520,7 @@
+    (within the same session) */
+ /* #define EDIT_GETLIN */
+ 
+-/* #define DUMPLOG */  /* End-of-game dump logs */
++#define DUMPLOG /* End-of-game dump logs */
+ #ifdef DUMPLOG
+ 
+ #ifndef DUMPLOG_MSG_COUNT
+@@ -528,7 +528,7 @@
+ #endif
+ 
+ #ifndef DUMPLOG_FILE
+-#define DUMPLOG_FILE        "/tmp/nethack.%n.%d.log"
++#define DUMPLOG_FILE        "/tmp/nethack.%v.%u.%n.%D.log"
+ /* DUMPLOG_FILE allows following placeholders:
+    %% literal '%'
+    %v version (eg. "3.6.1-0")
