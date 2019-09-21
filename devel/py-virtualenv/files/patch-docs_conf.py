a9957d36119cb95677dcea4610ca80f48515d183
antoine <antoine@FreeBSD.org>
2019-09-21 06:05:53 +0000

    Unbreak py27 FLAVOR
    Reported by:	pkg-fallout

--- devel/py-virtualenv/files/patch-docs_conf.py
+++ devel/py-virtualenv/files/patch-docs_conf.py
@@ -1,9 +1,14 @@
 --- docs/conf.py.orig	2019-09-03 16:32:54 UTC
 +++ docs/conf.py
-@@ -5,6 +5,7 @@ import re
+@@ -4,7 +4,11 @@ import os
+ import re
  import subprocess
  import sys
- from pathlib import Path
+-from pathlib import Path
++if sys.version_info[0] == 2:
++    from pathlib2 import Path
++else:
++    from pathlib import Path
 +sys.path.insert(0, os.path.abspath('../build/lib'))
  
  from virtualenv import __version__
