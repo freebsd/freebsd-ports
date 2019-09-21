69b8c7de1b1d92446456a6adc3fcda1189389706
pkubaj <pkubaj@FreeBSD.org>
2019-09-21 16:06:24 +0000

    audio/bsequencer-lv2: fix build on GCC architectures
    src/ButtonBox.hpp needs include sys/types.h for uint.
    
    PR:		240663
    Approved by:	yuri (maintainer), linimon (mentor)
    Differential Revision:	https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=240663

---
+++ audio/bsequencer-lv2/files/patch-src_ButtonBox.hpp
@@ -0,0 +1,10 @@
+--- src/ButtonBox.hpp.orig	2019-09-18 08:50:30 UTC
++++ src/ButtonBox.hpp
+@@ -1,6 +1,7 @@
+ #ifndef BUTTONBOX_HPP_
+ #define BUTTONBOX_HPP_
+ 
++#include <sys/types.h>
+ #include <vector>
+ #include <cmath>
+ #include "BWidgets/BColors.hpp"
