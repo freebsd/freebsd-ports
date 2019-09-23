f45575b26d57fa9f1c8243881884170648fd7124
Jan Beich <jbeich@FreeBSD.org>
2019-09-23 17:02:26 +0000

    games/xray_re-tools: unbreak with clang 9
    ld: error: utils/converter/level_mesh.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/Ice/IceAABB.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/Ice/IceContainer.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/Ice/IceHPoint.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/Ice/IceMatrix4x4.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/Ice/IcePoint.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/Ice/IceRandom.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/OPC_AABBTree.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/OPC_BaseModel.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/OPC_Collider.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/OPC_MeshInterface.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/OPC_Model.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/OPC_OptimizedTree.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/OPC_TreeBuilders.o: unable to find library from dependent library specifier: Opcode.lib
    ld: error: utils/converter/opcode/OPC_TreeCollider.o: unable to find library from dependent library specifier: Opcode.lib
    
    Reported by:	antoine (via bug 240629 exp-run)
    Regressed by:	https://reviews.llvm.org/rL360984

--- games/xray_re-tools/files/patch-sources_utils_converter_opcode_Opcode.h
+++ games/xray_re-tools/files/patch-sources_utils_converter_opcode_Opcode.h
@@ -1,5 +1,14 @@
 --- sources/utils/converter/opcode/Opcode.h.orig	2018-09-02 12:42:44 UTC
 +++ sources/utils/converter/opcode/Opcode.h
+@@ -28,7 +28,7 @@
+ 	#pragma message("Using OPCODE")
+ 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 	// Automatic linking
+-	#ifndef BAN_OPCODE_AUTOLINK
++	#if defined(_WIN32) && !defined(BAN_OPCODE_AUTOLINK)
+ 		#ifdef _DEBUG
+ 			#pragma comment(lib, "Opcode_D.lib")
+ 		#else
 @@ -41,9 +41,17 @@
  // Preprocessor
  #ifndef ICE_NO_DLL
