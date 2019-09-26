2a236fe50ee95f5b786b70ae05773194706f39fe
Dan Langille <dvl@FreeBSD.org>
2019-09-26 17:50:43 +0000

    Update to 1.55
    re: https://github.com/librenms/librenms/releases/tag/1.55

--- net-mgmt/librenms/files/patch-includes_defaults.inc.php
+++ net-mgmt/librenms/files/patch-includes_defaults.inc.php
@@ -1,6 +1,6 @@
---- includes/defaults.inc.php.orig	2018-10-17 10:33:28 UTC
+--- includes/defaults.inc.php.orig	2019-09-03 04:19:02 UTC
 +++ includes/defaults.inc.php
-@@ -49,19 +49,19 @@ $config['own_hostname'] = 'localhost';
+@@ -49,20 +49,20 @@ $config['own_hostname'] = 'localhost';
  $config['fping_options']['timeout'] = 500;
  $config['fping_options']['count']   = 3;
  $config['fping_options']['interval'] = 500;
@@ -16,19 +16,20 @@
 -$config['ping']           = '/bin/ping';
 -$config['mtr']            = '/usr/bin/mtr';
 -$config['nmap']           = '/usr/bin/nmap';
--$config['nagios_plugins'] = '/usr/lib/nagios/plugins';
++$config['ping']           = '/dbin/ping';
++$config['mtr']            = '/usr/local/bin/mtr';
++$config['nmap']           = '/usr/local/bin/nmap';
+ $config['nagios_plugins'] = '/usr/lib/nagios/plugins';
 -$config['ipmitool']       = '/usr/bin/ipmitool';
 -$config['virsh']          = '/usr/bin/virsh';
 -$config['dot']            = '/usr/bin/dot';
 -$config['sfdp']           = '/usr/bin/sfdp';
-+$config['ping']           = '/sbin/ping';
-+$config['mtr']            = '/usr/local/sbin/mtr';
-+$config['nmap']           = '/usr/local/bin/nmap';
-+$config['nagios_plugins'] = '/usr/local/libexec/nagios';
+-$config['nfdump']         = '/usr/bin/nfdump';
 +$config['ipmitool']       = '/usr/local/bin/ipmitool';
 +$config['virsh']          = '/usr/local/bin/virsh';
 +$config['dot']            = '/usr/local/bin/dot';
 +$config['sfdp']           = '/usr/local/bin/sfdp';
++$config['nfdump']         = '/usr/local/bin/nfdump';
  
  $config['slow_statistics'] = true;
  // THIS WILL CHANGE TO FALSE IN FUTURE
