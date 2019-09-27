4d59d9e3aa37b1bd489653b9537b8bff651792b2
danfe <danfe@FreeBSD.org>
2019-09-27 11:05:57 +0000

    - Update to version 1.4.3 (upstream does not embed version in the source   file, but comparing both files' contents shows that it is legit update) - Convert rather cumbersome `post-patch' target to patches, removing the   changes that were integrated upstream - Actualize USES and USE_XORG lists, reword the COMMENT text while here

--- editors/e93/files/patch-shell.c
+++ editors/e93/files/patch-shell.c
@@ -1,19 +1,10 @@
---- shell.c.orig	2013-05-22 09:35:54.000000000 +0200
-+++ shell.c	2013-05-22 09:44:04.000000000 +0200
-@@ -992,7 +992,7 @@
- 		Tcl_SetVar(interpreter,"SCRIPTPATH",scriptPath,TCL_LEAVE_ERR_MSG);
- 		if(Tcl_EvalFile(interpreter,scriptPath)!=TCL_OK)
- 		{
--			SetError("%s:%d:%s",scriptPath,interpreter->errorLine,Tcl_GetStringResult(interpreter));
-+			SetError("%s:%d:%s",scriptPath,Tcl_GetErrorLine(interpreter),Tcl_GetStringResult(interpreter));
- 			fail=true;
- 		}
- 	}
-@@ -1066,7 +1066,7 @@
+--- shell.c.orig	2009-12-26 20:34:58 UTC
++++ shell.c
+@@ -1070,7 +1070,7 @@ static bool InitTcl(char *pathName)
  			}
  			else
  			{
--				SetError("Failed to Tcl_Init(): %s",tclInterpreter->result);
+-				SetError("Failed to Tcl_Init(): %s",Tcl_GetResult(tclInterpreter));
 +				SetError("Failed to Tcl_Init(): %s",Tcl_GetStringResult(tclInterpreter));
  			}
  			UnInitChannels(tclInterpreter);
