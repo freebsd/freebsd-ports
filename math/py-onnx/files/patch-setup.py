5150114b100dbdc8e8d4140c57bb17e731d12d01
Yuri Victorovich <yuri@FreeBSD.org>
2019-09-28 15:08:27 +0000

    math/py-onnx: Update 1.5.0 -> 1.6.0
    Reported by:	portscout

--- math/py-onnx/files/patch-setup.py
+++ math/py-onnx/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2019-04-24 06:21:37 UTC
+--- setup.py.orig	2019-09-25 20:37:27 UTC
 +++ setup.py
-@@ -52,11 +52,12 @@ COVERAGE = bool(os.getenv('COVERAGE'))
+@@ -53,11 +53,12 @@ COVERAGE = bool(os.getenv('COVERAGE'))
  # Version
  ################################################################################
  
@@ -18,12 +18,3 @@
  
  with open(os.path.join(TOP_DIR, 'VERSION_NUMBER')) as version_file:
      VersionInfo = namedtuple('VersionInfo', ['version', 'git_version'])(
-@@ -286,7 +287,7 @@ install_requires.extend([
-     'protobuf',
-     'numpy',
-     'six',
--    'typing>=3.6.4',
-+    'typing>=3.6.4; python_version < "3.5"',
-     'typing-extensions>=3.6.2.1',
- ])
- 
