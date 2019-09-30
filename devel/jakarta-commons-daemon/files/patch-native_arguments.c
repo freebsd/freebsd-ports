fc570106dfed7706f92568948f43927c358031be
ale <ale@FreeBSD.org>
2019-09-30 14:55:22 +0000

    Update to 1.2.1 release.

--- devel/jakarta-commons-daemon/files/patch-native_arguments.c
+++ devel/jakarta-commons-daemon/files/patch-native_arguments.c
@@ -1,15 +1,15 @@
---- native/arguments.c.orig	2017-11-02 13:10:04 UTC
+--- native/arguments.c.orig	2019-09-04 15:44:04 UTC
 +++ native/arguments.c
-@@ -157,7 +157,7 @@ static arg_data *parse(int argc, char *a
-     args->help    = false;        /* Don't display help */
-     args->chck    = false;        /* Don't do a check-only startup */
-     args->stop    = false;        /* Stop a running jsvc */
--    args->wait    = 0;            /* Wait until jsvc has started the JVM */
-+    args->wait    = 10;           /* Wait until jsvc has started the JVM */
-     args->restarts = -1;          /* Infinite restarts by default */
-     args->install = false;        /* Don't install as a service */
-     args->remove  = false;        /* Don't remove the installed service */
-@@ -270,10 +270,6 @@ static arg_data *parse(int argc, char *a
+@@ -157,7 +157,7 @@ static arg_data *parse(int argc, char *argv[])
+     args->help = false;                /* Don't display help */
+     args->chck = false;                /* Don't do a check-only startup */
+     args->stop = false;                /* Stop a running jsvc */
+-    args->wait = 0;                    /* Wait until jsvc has started the JVM */
++    args->wait = 10;                   /* Wait until jsvc has started the JVM */
+     args->restarts = -1;               /* Infinite restarts by default */
+     args->install = false;             /* Don't install as a service */
+     args->remove = false;              /* Don't remove the installed service */
+@@ -266,10 +266,6 @@ static arg_data *parse(int argc, char *argv[])
              temp = optional(argc, argv, x++);
              if (temp)
                  args->wait = atoi(temp);
