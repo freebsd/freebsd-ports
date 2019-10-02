4e795cc7ba81226360e1ce22858e0cfa5f4ffe33
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2019-10-02 18:38:26 +0000

    devel/pyside2 and friends: update to 5.13.1
    Announcement:
      https://lists.qt-project.org/pipermail/pyside/2019-September/002786.html
    
    While here, pet portlint. Bump FreeCAD, pyside2's consumer

--- devel/shiboken2/files/patch-libshiboken_helper.h
+++ devel/shiboken2/files/patch-libshiboken_helper.h
@@ -1,11 +1,11 @@
---- libshiboken/helper.h.orig	2018-12-17 07:28:47 UTC
+--- libshiboken/helper.h.orig	2019-10-02 11:12:45 UTC
 +++ libshiboken/helper.h
 @@ -90,7 +90,7 @@ class AutoArrayPointer
-         T* data;
+         T *data;
  };
  
--typedef unsigned long long ThreadId;
-+typedef pthread_t ThreadId;
+-using ThreadId = unsigned long long;
++using ThreadId = pthread_t;
  LIBSHIBOKEN_API ThreadId currentThreadId();
  LIBSHIBOKEN_API ThreadId mainThreadId();
  
