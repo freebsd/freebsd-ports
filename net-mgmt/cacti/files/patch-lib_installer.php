b307bd0bc36ba16148abf71a1e33a7440ba4f106
kai <kai@FreeBSD.org>
2019-10-02 14:53:51 +0000

    net-mgmt/cacti: Update to 1.2.7
    Changelog since: 1.2.5:
    
    * Fixes CVE-2019-16723 that allowed unrestricted access to graphs
    * Various bugfixes
    
    https://github.com/Cacti/cacti/blob/release/1.2.7/CHANGELOG
    
    PR:		204999
    Submitted by:	Michael Muenz <m.muenz@gmail.com>
    Approved by:	Daniel Austin <freebsd-ports@dan.me.uk> (maintainer)
    MFH:		2019Q4
    Security:	ed18aa92-e4f4-11e9-b6fa-3085a9a95629

--- net-mgmt/cacti/files/patch-lib_installer.php
+++ net-mgmt/cacti/files/patch-lib_installer.php
@@ -1,11 +1,11 @@
---- lib/installer.php.orig	2019-07-26 03:05:22 UTC
+--- lib/installer.php.orig	2019-09-29 18:36:29 UTC
 +++ lib/installer.php
-@@ -3216,7 +3216,7 @@ class Installer implements JsonSerializable {
+@@ -3226,7 +3226,7 @@ class Installer implements JsonSerializable {
  		global $config;
  
  		$page_nr = 1;
--		$logcontents = tail_file($config['base_path'] . '/log/cacti.log', 100, -1, ' INSTALL:' , $page_no, $total_rows);
-+		$logcontents = tail_file('/var/log/cacti/log', 100, -1, ' INSTALL:' , $page_no, $total_rows);
+-		$logcontents = tail_file($config['base_path'] . '/log/cacti.log', 100, -1, ' INSTALL:' , $page_nr, $total_rows);
++		$logcontents = tail_file('/var/log/cacti/log', 100, -1, ' INSTALL:' , $page_nr, $total_rows);
  
  		$output_log = '';
  		foreach ($logcontents as $logline) {
