7d128a3dc7327ccda08818299e78e46b1e1b2d80
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-10-03 18:23:58 +0000

    Update to 1.0.1
    Changes:	https://github.com/Zulko/moviepy/releases

--- multimedia/py-moviepy/files/patch-moviepy-config_defaults.py
+++ multimedia/py-moviepy/files/patch-moviepy-config_defaults.py
@@ -1,12 +1,10 @@
---- moviepy/config_defaults.py.orig	2018-12-15 16:06:35 UTC
+--- moviepy/config_defaults.py.orig	2019-10-01 15:30:18 UTC
 +++ moviepy/config_defaults.py
 @@ -50,5 +50,6 @@ IMAGEMAGICK_BINARY
  
  import os
  
 -FFMPEG_BINARY = os.getenv('FFMPEG_BINARY', 'ffmpeg-imageio')
--IMAGEMAGICK_BINARY = os.getenv('IMAGEMAGICK_BINARY', 'auto-detect')
-\ No newline at end of file
 +# Use ffmpeg from ports
 +FFMPEG_BINARY = "ffmpeg"
-+IMAGEMAGICK_BINARY = os.getenv('IMAGEMAGICK_BINARY', 'auto-detect')
+ IMAGEMAGICK_BINARY = os.getenv('IMAGEMAGICK_BINARY', 'auto-detect')
