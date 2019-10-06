b9be7991655fa1cfda428858c9eb7d7a91ea5cb2
Rene Ladan <rene@FreeBSD.org>
2019-10-06 10:55:57 +0000

    Remove expired port:
    2019-10-05 x11-wm/fvwm: Unfetchable, unmaintained, unsupported upstream

--- x11-wm/fvwm/files/patch-FvwmScroll.h
+++
@@ -1,8 +0,0 @@
---- ./modules/FvwmScroll/FvwmScroll.h.orig	1994-09-14 14:11:35.000000000 +0000
-+++ ./modules/FvwmScroll/FvwmScroll.h	2009-03-11 09:42:48.000000000 +0000
-@@ -1,5 +1,4 @@
- #include "../../libs/fvwmlib.h"       
--char *safemalloc(int length);
- void DeadPipe(int nonsense);
- void GetTargetWindow(Window *app_win);
- void sleep_a_little(int n);
