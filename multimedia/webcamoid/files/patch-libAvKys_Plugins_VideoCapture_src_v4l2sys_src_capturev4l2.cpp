f634cbf6eecee6d21e884b345636426f8b8c969c
Yuri Victorovich <yuri@FreeBSD.org>
2019-10-08 05:04:06 +0000

    multimedia/webcamoid: Update 8.6.1 -> 8.7.0
    Reported by:	portscout

--- multimedia/webcamoid/files/patch-libAvKys_Plugins_VideoCapture_src_v4l2sys_src_capturev4l2.cpp
+++ multimedia/webcamoid/files/patch-libAvKys_Plugins_VideoCapture_src_v4l2sys_src_capturev4l2.cpp
@@ -1,26 +1,18 @@
---- libAvKys/Plugins/VideoCapture/src/v4l2sys/src/capturev4l2.cpp.orig	2019-03-01 19:22:43 UTC
+--- libAvKys/Plugins/VideoCapture/src/v4l2sys/src/capturev4l2.cpp.orig	2019-09-30 15:37:45 UTC
 +++ libAvKys/Plugins/VideoCapture/src/v4l2sys/src/capturev4l2.cpp
-@@ -162,10 +162,13 @@ class CaptureV4L2Private
-                             const QVariantMap &map2) const;
- };
- 
-+#define FREEBSD_BUG224011_VIDEO0
-+
- CaptureV4L2::CaptureV4L2(QObject *parent):
+@@ -237,18 +237,22 @@ CaptureV4L2::CaptureV4L2(QObject *parent):
      Capture(parent)
  {
      this->d = new CaptureV4L2Private(this);
 +#if !defined(FREEBSD_BUG224011_VIDEO0)
      this->d->m_fsWatcher = new QFileSystemWatcher({"/dev"}, this);
- 
      QObject::connect(this->d->m_fsWatcher,
-@@ -176,13 +179,16 @@ CaptureV4L2::CaptureV4L2(QObject *parent
-                      &QFileSystemWatcher::fileChanged,
-                      this,
-                      &CaptureV4L2::onFileChanged);
+                      &QFileSystemWatcher::directoryChanged,
+                      [this] () {
+                          this->d->updateDevices();
+                      });
 +#endif
- 
-     this->updateDevices();
+     this->d->updateDevices();
  }
  
  CaptureV4L2::~CaptureV4L2()
@@ -31,18 +23,18 @@
      delete this->d;
  }
  
-@@ -1301,11 +1307,13 @@ void CaptureV4L2::updateDevices()
-     this->d->m_devicesCaps = devicesCaps;
+@@ -1145,11 +1149,13 @@ void CaptureV4L2Private::updateDevices()
+     this->m_devicesCaps = devicesCaps;
  
-     if (this->d->m_devices != devices) {
+     if (this->m_devices != devices) {
 +#if !defined(FREEBSD_BUG224011_VIDEO0)
-         if (!this->d->m_devices.isEmpty())
-             this->d->m_fsWatcher->removePaths(this->d->m_devices);
+         if (!this->m_devices.isEmpty())
+             this->m_fsWatcher->removePaths(this->m_devices);
 +#endif
  
-         this->d->m_devices = devices;
+         this->m_devices = devices;
 -#ifndef Q_OS_BSD4
 +#if !defined(FREEBSD_BUG224011_VIDEO0)
-         if (!this->d->m_devices.isEmpty())
-             this->d->m_fsWatcher->addPaths(this->d->m_devices);
+         if (!this->m_devices.isEmpty())
+             this->m_fsWatcher->addPaths(this->m_devices);
  #endif
