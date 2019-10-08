c1e6d7243177f1752f9152c73136072e679ab469
Jan Beich <jbeich@FreeBSD.org>
2019-10-08 00:48:49 +0000

    multimedia/aom: update to 1.0.0.2474
    Changes:	https://aomedia.googlesource.com/aom/+log/fdd0ab4b3..60a00de69

--- multimedia/aom/files/patch-build_cmake_aom__configure.cmake
+++ multimedia/aom/files/patch-build_cmake_aom__configure.cmake
@@ -1,17 +1,8 @@
-- uname -p returns amd64 on FreeBSD/OpenBSD but x86_64 on DragonFly/NetBSD
 - More ELF platforms can use GNU assembler on non-x86
 
---- build/cmake/aom_configure.cmake.orig	2019-05-24 23:33:39 UTC
+--- build/cmake/aom_configure.cmake.orig	2019-10-07 23:31:01 UTC
 +++ build/cmake/aom_configure.cmake
-@@ -37,6 +37,7 @@ string(STRIP "${AOM_CMAKE_CONFIG}" AOM_CMAKE_CONFIG)
- # Detect target CPU.
- if(NOT AOM_TARGET_CPU)
-   if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "AMD64"
-+     OR "${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "amd64"
-      OR "${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86_64")
-     if(${CMAKE_SIZEOF_VOID_P} EQUAL 4)
-       set(AOM_TARGET_CPU "x86")
-@@ -137,21 +138,15 @@ elseif("${AOM_TARGET_CPU}" MATCHES "arm")
+@@ -154,21 +154,15 @@ elseif("${AOM_TARGET_CPU}" MATCHES "arm")
    if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
      set(AS_EXECUTABLE as)
      set(AOM_AS_FLAGS -arch ${AOM_TARGET_CPU} -isysroot ${CMAKE_OSX_SYSROOT})
