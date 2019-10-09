834ed590099848188670e4826fab4667daeebc89
amdmi3 <amdmi3@FreeBSD.org>
2019-10-09 20:05:18 +0000

    - Update to 3.3.1

--- games/instead/files/patch-src_SDL__anigif.h
+++
@@ -1,9 +0,0 @@
---- src/SDL_anigif.h.orig	2019-03-08 09:20:22 UTC
-+++ src/SDL_anigif.h
-@@ -56,6 +56,5 @@ extern DECLSPEC int		AG_LoadGIF_RW( SDL_RWops* src, AG
- #ifdef __cplusplus
- 	}
- #endif
--#include "close_code.h"
- 
- #endif /* _SDL_ANIGIF_H */
