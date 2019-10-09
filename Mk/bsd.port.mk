399ae34706ab590fb7bcbc6f97639669680d48d5
bapt <bapt@FreeBSD.org>
2019-10-09 12:28:26 +0000

    Remove the ipv6 virtual category
    in 2019 the norm is for applications to support both v4 and v6 version of the ip
    stack. Furthermore, the ipv6 category was clearly not consistently spread over
    the ports tree defeating the goal of the category.

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -2564,7 +2564,7 @@ VALID_CATEGORIES+= accessibility afterstep arabic archivers astro audio \
 	benchmarks biology cad chinese comms converters databases \
 	deskutils devel docs dns editors elisp emulators enlightenment finance french ftp \
 	games geography german gnome gnustep graphics hamradio haskell hebrew hungarian \
-	ipv6 irc japanese java kde ${_KDE_CATEGORIES_SUPPORTED} kld korean lang linux lisp \
+	irc japanese java kde ${_KDE_CATEGORIES_SUPPORTED} kld korean lang linux lisp \
 	mail mate math mbone misc multimedia net net-im net-mgmt net-p2p net-vpn news \
 	palm parallel pear perl5 plan9 polish portuguese ports-mgmt \
 	print python ruby rubygems russian \
