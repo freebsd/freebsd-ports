0ae3d94da83e1f869c5e08b3e1b8a748f0728af3
Adriaan de Groot <adridg@FreeBSD.org>
2019-10-10 20:38:42 +0000

    Fix build of x11-toolkits/qt5-gui
    <linux/kd.h> isn't needed here.
    
    Pointy hat to adridg@, for mixing up pou-build tree and the SVN tree.
    
    PR:		240964
    Reported by:	vvd@unislabs.com

--- x11-toolkits/qt5-gui/files/patch-src_platformsupport_input_evdevmouse_qevdevmousehandler.cpp
+++ x11-toolkits/qt5-gui/files/patch-src_platformsupport_input_evdevmouse_qevdevmousehandler.cpp
@@ -1,13 +1,13 @@
 --- src/platformsupport/input/evdevmouse/qevdevmousehandler.cpp.orig	2019-06-13 04:18:18 UTC
 +++ src/platformsupport/input/evdevmouse/qevdevmousehandler.cpp
-@@ -53,12 +53,8 @@
+@@ -53,12 +53,7 @@
  
  #include <errno.h>
  
 -#ifdef Q_OS_FREEBSD
 -#include <dev/evdev/input.h>
 -#else
- #include <linux/kd.h>
+-#include <linux/kd.h>
  #include <linux/input.h>
 -#endif
  
