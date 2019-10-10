fbd2f7ab9caebd4349ae1f5f9277b5dbfe41f3ee
Adriaan de Groot <adridg@FreeBSD.org>
2019-10-10 20:19:13 +0000

    Update astro/kstars to latest upstream release
    Release notes:
    	http://knro.blogspot.com/2019/09/kstars-v336-is-released.html
    says "lots of little improvements". One translation has gone away.

--- astro/kstars/files/patch-kstars_auxiliary_ksutils.cpp
+++ astro/kstars/files/patch-kstars_auxiliary_ksutils.cpp
@@ -1,10 +1,10 @@
---- kstars/auxiliary/ksutils.cpp.orig	2019-09-09 16:36:17 UTC
+--- kstars/auxiliary/ksutils.cpp.orig	2019-09-08 19:12:11 UTC
 +++ kstars/auxiliary/ksutils.cpp
-@@ -1325,7 +1325,7 @@ bool copyRecursively(QString sourceFolder, QString des
+@@ -1323,7 +1323,7 @@ bool copyRecursively(QString sourceFolder, QString des
  //One is createLocalAstrometryConf and the other is configureAstrometry
  bool configureLocalAstrometryConfIfNecessary()
  {
--#ifdef Q_OS_LINUX
+-#if defined(Q_OS_LINUX)
 +#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
      QString confPath = KSPaths::writableLocation(QStandardPaths::GenericDataLocation) + QLatin1Literal("astrometry") + QLatin1Literal("/astrometry.cfg");
      if (QFileInfo(confPath).exists() == false)
