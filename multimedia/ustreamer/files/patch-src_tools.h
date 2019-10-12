08aef2b2b8000168405597c50f7c068710c506c2
Yuri Victorovich <yuri@FreeBSD.org>
2019-10-12 07:26:44 +0000

    multimedia/ustreamer: Update 1.0 -> 1.9
    Reported by:	portscout

--- multimedia/ustreamer/files/patch-src_tools.h
+++ multimedia/ustreamer/files/patch-src_tools.h
@@ -1,8 +1,8 @@
---- src/tools.h.orig	2019-04-27 06:12:11 UTC
+--- src/tools.h.orig	2019-10-11 20:37:25 UTC
 +++ src/tools.h
-@@ -36,6 +36,13 @@
- #include <sys/ioctl.h>
- #include <sys/syscall.h>
+@@ -40,6 +40,13 @@
+ #define INLINE inline __attribute__((always_inline))
+ #define UNUSED __attribute__((unused))
  
 +#ifndef CLOCK_MONOTONIC_RAW
 +# ifdef CLOCK_MONOTONIC_FAST
@@ -12,15 +12,5 @@
 +# endif
 +#endif
  
- #define A_THREAD_CREATE(_tid, _func, _arg)	assert(!pthread_create(_tid, NULL, _func, _arg))
- #define A_THREAD_JOIN(_tid)					assert(!pthread_join(_tid, NULL))
-@@ -106,5 +113,9 @@ INLINE long double get_now_real(void) {
- }
- 
- INLINE pid_t get_thread_id(void) {
-+#if defined(__FreeBSD__)
-+	return pthread_getthreadid_np();
-+#else
- 	return syscall(SYS_gettid);
-+#endif
- }
+ INLINE char *bool_to_string(bool flag) {
+ 	return (flag ? "true" : "false");
