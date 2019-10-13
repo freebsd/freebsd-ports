63d3daeb1cd2d23bc1ce41144c25cabf60ca9a8e
Jan Beich <jbeich@FreeBSD.org>
2019-10-13 13:07:28 +0000

    ftp/omi: drop FreeBSD < 7.2 support

--- ftp/omi/files/patch-utils.c
+++ ftp/omi/files/patch-utils.c
@@ -1,20 +1,17 @@
 --- utils/utils.c.orig	2009-02-23 16:51:16.000000000 +0100
 +++ utils/utils.c	2009-02-23 16:53:54.000000000 +0100
-@@ -31,6 +31,9 @@
+@@ -31,6 +31,7 @@
  	return (mem);
  }
  
-+#if defined(__FreeBSD__)
-+#include <osreldate.h>
-+#if __FreeBSD_version <= 800057 && __FreeBSD_version > 800000 || __FreeBSD_version <= 701100
++#if !defined(__FreeBSD__)
  char *strndup (const char *src, int num)
  {
  	char *dst;
-@@ -40,6 +43,8 @@
+@@ -40,6 +43,7 @@
  	dst[num] = '\0';
  	return (strncpy(dst, src, num));
  }
-+#endif
 +#endif
  
  char *justify (char *str)
--- ftp/omi/files/patch-utils.h
+++ ftp/omi/files/patch-utils.h
@@ -1,20 +1,17 @@
 --- utils/utils.h.orig	2009-02-23 16:51:20.000000000 +0100
 +++ utils/utils.h	2009-02-23 16:54:21.000000000 +0100
-@@ -42,6 +42,9 @@
+@@ -42,6 +42,7 @@
   *   allocated.
   */
  
-+#if defined(__FreeBSD__)
-+#include <osreldate.h>
-+#if __FreeBSD_version <= 800057 && __FreeBSD_version > 800000 || __FreeBSD_version <= 701100
++#if !defined(__FreeBSD__)
  char *strndup (const char *src, int num);
  /*
   *   Like strdup(), but limits the string length to at most
-@@ -50,6 +53,8 @@
+@@ -50,6 +53,7 @@
   *   Always allocates <num>+1 bytes, even if less space would
   *   be sufficient to store <src>.
   */
-+#endif
 +#endif
  
  char *justify (char *str);
