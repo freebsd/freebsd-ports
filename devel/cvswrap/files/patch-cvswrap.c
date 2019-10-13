aa7a90a8f443a7d44924ac661baa3ea72d66226a
jbeich <jbeich@FreeBSD.org>
2019-10-13 13:10:45 +0000

    devel/cvswrap: drop FreeBSD < 7.2 support

--- devel/cvswrap/files/patch-cvswrap.c
+++ devel/cvswrap/files/patch-cvswrap.c
@@ -1,33 +1,27 @@
 --- cvswrap.c.orig	2009-02-24 15:40:55.000000000 +0100
 +++ cvswrap.c	2009-02-24 15:38:24.000000000 +0100
-@@ -94,7 +94,12 @@
+@@ -94,7 +94,9 @@
      char **cvsrootp);
  static void	slashfix(char *);
  static int	strings_have_line(char * const *strings, const char *line);
-+#if defined(__FreeBSD__)
-+#include <osreldate.h>
-+#if __FreeBSD_version <= 800057 && __FreeBSD_version > 800000 || __FreeBSD_version <= 701100
++#if !defined(__FreeBSD__)
  static char *	strndup(const char *str, size_t len);
-+#endif
 +#endif
  static int	timeval_diff(const struct timeval *tv1,
      const struct timeval *tv2);
  static int	writebuf(int fd, const char *buf, size_t len);
-@@ -324,6 +329,9 @@
+@@ -324,6 +329,7 @@
  	return (0);
  }
  
-+#if defined(__FreeBSD__)
-+#include <osreldate.h>
-+#if __FreeBSD_version <= 800057 && __FreeBSD_version > 800000 || __FreeBSD_version <= 701100
++#if !defined(__FreeBSD__)
  /*
   * return: a copy of "len" bytes from byte string "str" and NULL terminate.
   */
-@@ -340,6 +348,8 @@
+@@ -340,6 +348,7 @@
  		*(ret + len) = '\0';
  	return (ret);
  }
-+#endif
 +#endif
  
  /*
