882a402d7682a77edc2880a461701af028082c76
Yuri Victorovich <yuri@FreeBSD.org>
2019-10-14 05:46:10 +0000

    security/zeronet: Update 0.6.5-25 -> 0.7.1
    ZeroNet now supports python3.
    ZeroNet is nothing less than the alternative model of the internet, where websites are hosted by users.

--- security/zeronet/files/patch-src_Ui_UiServer.py
+++ security/zeronet/files/patch-src_Ui_UiServer.py
@@ -1,18 +1,20 @@
---- src/Ui/UiServer.py.orig	2016-06-11 08:29:47 UTC
+--- src/Ui/UiServer.py.orig	2019-09-04 18:16:57 UTC
 +++ src/Ui/UiServer.py
-@@ -107,7 +107,7 @@ class UiServer:
+@@ -133,7 +133,7 @@ class UiServer:
                  from werkzeug.debug import DebuggedApplication
                  handler = DebuggedApplication(self.handleRequest, evalex=True)
-             except Exception, err:
+             except Exception as err:
 -                self.log.info("%s: For debugging please download Werkzeug (http://werkzeug.pocoo.org/)" % err)
 +                self.log.info("%s: For debugging please enable the port option: DEBUG=on" % err)
                  from Debug import DebugReloader
          self.log.write = lambda msg: self.log.debug(msg.strip())  # For Wsgi access.log
          self.log.info("--------------------------------------")
-@@ -155,4 +155,4 @@ class UiServer:
+@@ -192,7 +192,7 @@ class UiServer:
  
          self.server.socket.close()
          self.server.stop()
--        time.sleep(1)
-\ No newline at end of file
+-        self.running = False
 +        time.sleep(1)
+         time.sleep(1)
+ 
+     def updateWebsocket(self, **kwargs):
