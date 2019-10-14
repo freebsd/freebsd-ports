768bbbcbaf27f8289c40296a78ed6f683c452134
garga <garga@FreeBSD.org>
2019-10-14 18:40:51 +0000

    devel/cvsps:
    * Regenerate patches with make makepatch
    * Remove dead MASTER_SITES
    * Take maintainership

--- devel/cvsps/files/patch-cvsps.c
+++ devel/cvsps/files/patch-cvsps.c
@@ -1,6 +1,6 @@
---- cvsps.c.orig	Wed Aug  9 21:59:54 2006
-+++ cvsps.c	Wed Aug  9 22:00:17 2006
-@@ -2551,8 +2551,8 @@
+--- cvsps.c.orig	2005-05-26 03:39:40 UTC
++++ cvsps.c
+@@ -2551,8 +2551,8 @@ static void determine_branch_ancestor(PatchSet * ps, P
      for (next = ps->members.next; next != &ps->members; next = next->next) 
      {
  	PatchSetMember * psm = list_entry(next, PatchSetMember, link);
