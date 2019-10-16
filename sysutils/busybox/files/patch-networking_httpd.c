bfd2fd846262cfb6c78f046364b75867a72553d7
Oleksii Samorukov <samm@FreeBSD.org>
2019-10-16 19:07:42 +0000

    sysutils/busybox: update to the 1.31.0 version
     - Update to the 1.31.0
    
    Approved by:    araujo (mentor)
    Differential Revision:  https://reviews.freebsd.org/D22022

--- sysutils/busybox/files/patch-networking_httpd.c
+++ sysutils/busybox/files/patch-networking_httpd.c
@@ -1,17 +1,17 @@
---- networking/httpd.c.orig	2018-12-05 14:44:34 UTC
+--- networking/httpd.c.orig	2019-06-10 09:09:31 UTC
 +++ networking/httpd.c
-@@ -2109,10 +2109,10 @@ static void handle_incoming_and_exit(con
+@@ -2135,10 +2135,10 @@ static void handle_incoming_and_exit(con
  	}
  #if ENABLE_FEATURE_IPV6
  	if (fromAddr->u.sa.sa_family == AF_INET6
 -	 && fromAddr->u.sin6.sin6_addr.s6_addr32[0] == 0
 -	 && fromAddr->u.sin6.sin6_addr.s6_addr32[1] == 0
 -	 && ntohl(fromAddr->u.sin6.sin6_addr.s6_addr32[2]) == 0xffff)
--		rmt_ip = ntohl(fromAddr->u.sin6.sin6_addr.s6_addr32[3]);
+-		remote_ip = ntohl(fromAddr->u.sin6.sin6_addr.s6_addr32[3]);
 +	 && fromAddr->u.sin6.sin6_addr.s6_addr[0] == 0
 +	 && fromAddr->u.sin6.sin6_addr.s6_addr[1] == 0
 +	 && ntohl(fromAddr->u.sin6.sin6_addr.s6_addr[2]) == 0xffff)
-+		rmt_ip = ntohl(fromAddr->u.sin6.sin6_addr.s6_addr[3]);
++		remote_ip = ntohl(fromAddr->u.sin6.sin6_addr.s6_addr[3]);
  #endif
  	if (ENABLE_FEATURE_HTTPD_CGI || DEBUG || verbose) {
  		/* NB: can be NULL (user runs httpd -i by hand?) */
