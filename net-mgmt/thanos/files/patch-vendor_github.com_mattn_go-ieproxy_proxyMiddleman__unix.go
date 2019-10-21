377f6990cbdaf9e9ca01546aef9c2db50aed4b0a
ler <ler@FreeBSD.org>
2019-10-21 02:43:54 +0000

    net-mgmt/thanos: upgrade to 0.8.1.
    Release Notes: https://github.com/thanos-io/thanos/releases
    
    Assist from: dmgk

--- net-mgmt/thanos/files/patch-vendor_github.com_mattn_go-ieproxy_proxyMiddleman__unix.go
+++
@@ -1,8 +0,0 @@
---- vendor/github.com/mattn/go-ieproxy/proxyMiddleman_unix.go.orig	2019-08-27 16:11:57 UTC
-+++ vendor/github.com/mattn/go-ieproxy/proxyMiddleman_unix.go
-@@ -1,4 +1,4 @@
--//+build darwin unix linux
-+//+build !windows
- 
- package ieproxy
- 
