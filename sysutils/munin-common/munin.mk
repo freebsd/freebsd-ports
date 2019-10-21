18f8a545dad84dc33c345e46c033380d739be59c
mat <mat@FreeBSD.org>
2019-10-21 13:54:56 +0000

    Update to 2.0.51.

--- sysutils/munin-common/munin.mk
+++ sysutils/munin-common/munin.mk
@@ -1,6 +1,6 @@
 LICENSE=	GPLv2
 
-MUNIN_VERSION=	2.0.49
+MUNIN_VERSION=	2.0.51
 MUNIN_SITES=	http://downloads.munin-monitoring.org/munin/stable/${MUNIN_VERSION}/
 DISTINFO_FILE=	${.CURDIR}/../../sysutils/munin-common/distinfo
 PATCHDIR=	${.CURDIR}/../../sysutils/munin-common/files
