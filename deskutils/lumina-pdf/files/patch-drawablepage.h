f61cdaf31274693a9c75141f735de193aee355c7
pkubaj <pkubaj@FreeBSD.org>
2019-10-21 10:09:16 +0000

    deskutils/lumina-pdf: fix build on GCC architectures
    Add include memory to drawablepage.h:
    In file included from Renderer-poppler.cpp:2:
    drawablepage.h:34:8: error: 'unique_ptr' in namespace 'std' does not name a template type
       34 |   std::unique_ptr<Poppler::Page> page;
          |        ^~~~~~~~~~
    drawablepage.h:7:1: note: 'std::unique_ptr' is defined in header '<memory>'; did you forget to '#include <memory>'?
        6 | #include <poppler/qt5/poppler-qt5.h>
      +++ |+#include <memory>
        7 |
    
    PR:		241002
    Approved by:	ken@ixsystems.com (maintainer timeout), linimon (mentor)

---
+++ deskutils/lumina-pdf/files/patch-drawablepage.h
@@ -0,0 +1,10 @@
+--- drawablepage.h.orig	2019-10-02 09:07:11 UTC
++++ drawablepage.h
+@@ -1,6 +1,7 @@
+ #ifndef LUMINA_PDF_DRAWABLEPAGE_H
+ #define LUMINA_PDF_DRAWABLEPAGE_H
+ 
++#include <memory>
+ #include <QImage>
+ #include <QSize>
+ #include <poppler/qt5/poppler-qt5.h>
