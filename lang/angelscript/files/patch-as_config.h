2c0daee4ad73a3ab870bed68b41d9f858f35dace
Li-Wen Hsu <lwhsu@FreeBSD.org>
2019-10-22 09:26:00 +0000

    - Update to 2.34.0 - Change maintainer email address
    PR:		241378
    Submitted by:	Nathan <ndowens04@gmail.com> (maintainer)

--- lang/angelscript/files/patch-as_config.h
+++ lang/angelscript/files/patch-as_config.h
@@ -1,11 +1,15 @@
---- ../../source/as_config.h.orig	2019-07-28 15:58:25.283690000 +0200
-+++ ../../source/as_config.h	2019-07-28 16:08:34.592212000 +0200
-@@ -953,6 +953,8 @@
+diff --git sdk/angelscript/source/as_config.h sdk/angelscript/source/as_config.h
+index e17d1ee..f0e49b4 100644
+--- ../../source/as_config.h
++++ ../../source/as_config.h
+@@ -953,7 +953,9 @@
  			#define COMPLEX_RETURN_MASK (asOBJ_APP_CLASS_DESTRUCTOR | asOBJ_APP_CLASS_COPY_CONSTRUCTOR | asOBJ_APP_ARRAY)
  			#define THISCALL_PASS_OBJECT_POINTER_ON_THE_STACK
  			#define AS_X86
+-		#elif defined(__x86_64__)
 +		#elif defined(__powerpc64__)
 +			#define AS_MAX_PORTABILITY
- 		#elif defined(__LP64__)
++		#elif defined(__x86_64__) || defined(__LP64__)
  			#define AS_X64_GCC
  			#define HAS_128_BIT_PRIMITIVES
+ 			#define SPLIT_OBJS_BY_MEMBER_TYPES
