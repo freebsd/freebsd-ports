361c2522a3f390754876ccf08f845477f3e6313e
Dmitry Marakasov <amdmi3@FreeBSD.org>
2019-10-24 14:30:30 +0000

    - Add missing USES and dependency on libX11 - Simplify hash_map handling, C++11 way (fixed build on CURRENT)

--- games/xmoto/files/patch-src-include-xm__hashmap.h
+++ games/xmoto/files/patch-src-include-xm__hashmap.h
@@ -1,28 +1,33 @@
---- src/include/xm_hashmap.h.orig	2011-10-11 20:18:17 UTC
+--- src/include/xm_hashmap.h.orig	2019-10-23 11:51:33 UTC
 +++ src/include/xm_hashmap.h
-@@ -13,13 +13,18 @@
- #include <hash_map>
- namespace HashNamespace=std;
- #endif
+@@ -1,25 +1,11 @@
+ #ifndef __XMHASHMAP_H__
+ #define __XMHASHMAP_H__
+ 
+-#ifdef __GNUC__
+-#if (__GNUC__ >= 3)
+-#include <ext/hash_map>
+-  namespace HashNamespace=__gnu_cxx;
+-#else
+-#include <hash_map>
+-#define HashNamespace std
+-#endif
+-#else // #ifdef __GNUC__
+-#include <hash_map>
+-namespace HashNamespace=std;
+-#endif
 -struct hashcmp_str {
 -  bool operator()(const char* s1, const char* s2) {
 -    if(s1 == NULL || s2 == NULL) {
 -      return false;
-+
-+#ifdef _LIBCPP_VERSION
-+namespace __gnu_cxx {
-+  template<> struct hash<std::string>
-+    : public unary_function<std::string, size_t>
-+  {
-+    size_t operator()(const std::string& s) const
-+    {
-+      return hash<const char*>()(s.c_str());
-     }
+-    }
 -    return strcmp(s1, s2) == 0;
 -  }
--};
-+  };
-+}
-+#endif // _LIBCPP_VERSION
++#include <unordered_map>
++
++namespace HashNamespace {
++	template <class Key, class T, class Hash = std::hash<Key>, class Pred = std::equal_to<Key>, class Allocator = std::allocator< std::pair<const Key, T> >>
++	using hash_map = std::unordered_map<Key, T, Hash, Pred, Allocator>;
+ };
  
  #endif
