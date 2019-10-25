ad1c285efcc33dee07f29e9cbf5298e532a08ce2
Yuri Victorovich <yuri@FreeBSD.org>
2019-10-25 06:21:03 +0000

    Re-add port: misc/gpsim: Software simulator for Microchip PIC microcontrollers

---
+++ misc/gpsim/files/patch-src_modules.cc
@@ -0,0 +1,11 @@
+--- src/modules.cc.orig	2019-10-25 05:47:58 UTC
++++ src/modules.cc
+@@ -47,7 +47,7 @@ License along with this library; if not, see
+ #undef interface
+ #endif
+ 
+-#include "error.h"
++//#include "error.h"
+ #include "gpsim_interface.h"
+ #include "stimuli.h"
+ #include "symbol.h"
