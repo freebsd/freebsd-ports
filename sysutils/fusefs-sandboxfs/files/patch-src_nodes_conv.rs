56b25375fdf4763ee55b7e9b44fcdef38d27aea7
Tobias Kortkamp <tobik@FreeBSD.org>
2019-10-26 14:10:27 +0000

    sysutils/fusefs-sandboxfs: Update to sandboxfs-0.1.1
    Changes:	https://github.com/bazelbuild/sandboxfs/releases/tag/sandboxfs-0.1.1

--- sysutils/fusefs-sandboxfs/files/patch-src_nodes_conv.rs
+++ sysutils/fusefs-sandboxfs/files/patch-src_nodes_conv.rs
@@ -6,7 +6,7 @@ error[E0308]: mismatched types
 72 |     Timespec::new(val.tv_sec() as sys::time::time_t, usec)
    |                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ expected i64, found i32
 
---- src/nodes/conv.rs.orig	2019-02-06 10:25:59 UTC
+--- src/nodes/conv.rs.orig	2019-10-24 19:42:29 UTC
 +++ src/nodes/conv.rs
 @@ -69,7 +69,7 @@ pub fn timeval_to_timespec(val: sys::time::TimeVal) ->
      } else {
@@ -17,3 +17,25 @@ error[E0308]: mismatched types
  }
  
  /// Converts a file type as returned by the file system to a FUSE file type.
+@@ -313,6 +313,10 @@ mod tests {
+         // modified and may not be queryable, so stub them out.
+         attr.ctime = BAD_TIME;
+         attr.crtime = BAD_TIME;
++        // Ignore rdev too
++        attr.rdev = 0;
++        // XXX: gid is always 0 when the tests run on tmpfs(5)
++        attr.gid = exp_attr.gid;
+         assert!(fileattrs_eq(&exp_attr, &attr));
+     }
+ 
+@@ -350,6 +354,10 @@ mod tests {
+         // modified and may not be queryable, so stub them out.
+         attr.ctime = BAD_TIME;
+         attr.crtime = BAD_TIME;
++        // Ignore rdev too
++        attr.rdev = 0;
++        // XXX: gid is always 0 when the tests run on tmpfs(5)
++        attr.gid = exp_attr.gid;
+         assert!(fileattrs_eq(&exp_attr, &attr));
+     }
+ 
