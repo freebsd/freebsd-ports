582b451a40d2f905ba6ebbc2f4df81b9dc6e68b6
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2019-10-29 15:12:51 +0000

    Update to 3.4.1
    Changes:	https://github.com/ruby-gnome/ruby-gnome/blob/master/NEWS

--- devel/rubygem-glib2/files/patch-lib_mkmf-gnome.rb
+++ devel/rubygem-glib2/files/patch-lib_mkmf-gnome.rb
@@ -1,7 +1,7 @@
---- lib/mkmf-gnome2.rb.orig	2017-07-08 14:30:53 UTC
-+++ lib/mkmf-gnome2.rb
-@@ -160,7 +160,7 @@ def add_depend_package(target_name, targ
- 
+--- lib/mkmf-gnome.rb.orig	2019-10-28 15:13:19 UTC
++++ lib/mkmf-gnome.rb
+@@ -156,7 +156,7 @@ setup_homebrew
+ def add_depend_package(target_name, target_srcdir, top_srcdir, options={})
    gem_spec = find_gem_spec(target_name)
    if gem_spec
 -    target_source_dir = File.join(gem_spec.full_gem_path, "ext/#{target_name}")
