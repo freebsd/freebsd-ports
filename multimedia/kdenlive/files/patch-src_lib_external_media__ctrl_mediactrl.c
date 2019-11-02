63e51a8e47ad3773def7d817ddf6caaa04b4b004
Tobias C. Berner <tcberner@FreeBSD.org>
2019-11-02 20:11:52 +0000

    multimedia/kdenlive: update to 19.08.2
    - we were holding back the update of this due to crashes on startup,
      I no longer have these crashes, so let's hope for the best...

---
+++ multimedia/kdenlive/files/patch-src_lib_external_media__ctrl_mediactrl.c
@@ -0,0 +1,10 @@
+--- src/lib/external/media_ctrl/mediactrl.c.orig	2019-11-02 19:21:10 UTC
++++ src/lib/external/media_ctrl/mediactrl.c
+@@ -18,7 +18,6 @@
+  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  */
+ 
+-#include <asm/types.h>
+ #include <errno.h>
+ #include <fcntl.h>
+ #include <stdint.h>
