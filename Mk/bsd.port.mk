ee06ec39ce0f2b4e530bfe69c604c35ef3f35c2b
Rene Ladan <rene@FreeBSD.org>
2019-11-07 16:30:12 +0000

    Clean up support for FreeBSD 11.2.
    While here, modernize some comments in Mk/bsd.*.mk.
    
    Note that graphics/drm-fbsd11.2-kmod is not renamed yet, this was somewhat
    under discussion.
    
    Submitted by:	rene
    Reviewed by:	antoine, jbeich, mat, zeising
    Differential Revision:	https://reviews.freebsd.org/D21974

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -162,12 +162,12 @@ FreeBSD_MAINTAINER=	portmgr@FreeBSD.org
 # IGNORE_${ARCH} - Port should be ignored on ${ARCH}.
 # IGNORE_${OPSYS} - Port should be ignored on ${OPSYS}.
 # IGNORE_${OPSYS}_${OSREL:R} -  Port should be ignored on a single
-#				  release of ${OPSYS}, e.g IGNORE_FreeBSD_8
-#				  would affect all point releases of FreeBSD 8.
+#				  release of ${OPSYS}, e.g IGNORE_FreeBSD_13
+#				  would affect all point releases of FreeBSD 13.
 # IGNORE_${OPSYS}_${OSREL:R}_${ARCH} -  Port should be ignored on a
 #				  single release of ${OPSYS} and specific architecture,
-#				  e.g IGNORE_FreeBSD_8_i386 would affect all point
-#				  releases of FreeBSD 8 in i386.
+#				  e.g IGNORE_FreeBSD_13_i386 would affect all point
+#				  releases of FreeBSD 13 in i386.
 # BROKEN		- Port is believed to be broken.  Package builds can
 # 				  still be attempted using TRYBROKEN to test this
 #				  assumption.
@@ -178,13 +178,13 @@ FreeBSD_MAINTAINER=	portmgr@FreeBSD.org
 #				  can still be attempted using TRYBROKEN to
 #				  test this assumption.
 # BROKEN_${OPSYS}_${OSREL:R} -  Port is believed to be broken on a single
-#				  release of ${OPSYS}, e.g BROKEN_FreeBSD_8
-#				  would affect all point releases of FreeBSD 8
+#				  release of ${OPSYS}, e.g BROKEN_FreeBSD_13
+#				  would affect all point releases of FreeBSD 13
 #				  unless TRYBROKEN is also set.
 # BROKEN_${OPSYS}_${OSREL:R}_${ARCH} -  Port is believed to be broken on a
 #				  single release of ${OPSYS} and specific architecture,
-#				  e.g BROKEN_FreeBSD_8_i386 would affect all point
-#				  releases of FreeBSD 8 in i386
+#				  e.g BROKEN_FreeBSD_13 would affect all point
+#				  releases of FreeBSD 13 in i386
 #				  unless TRYBROKEN is also set.
 # DEPRECATED	- Port is deprecated to install. Advisory only.
 # EXPIRATION_DATE
