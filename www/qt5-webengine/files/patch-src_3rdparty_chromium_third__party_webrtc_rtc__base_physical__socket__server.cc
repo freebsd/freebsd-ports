3aa2ae4295c7f0164a59b717f100257bfafad546
tcberner <tcberner@FreeBSD.org>
2019-11-08 16:59:58 +0000

    Update Qt5 to 5.13.2
    Announcement:
            https://www.qt.io/blog/qt-5.13.2-released
    
    Thanks to kai@ for updating webengine.
    
    PR:		241649
    Exp-run by:	antoine

--- www/qt5-webengine/files/patch-src_3rdparty_chromium_third__party_webrtc_rtc__base_physical__socket__server.cc
+++ www/qt5-webengine/files/patch-src_3rdparty_chromium_third__party_webrtc_rtc__base_physical__socket__server.cc
@@ -1,6 +1,15 @@
---- src/3rdparty/chromium/third_party/webrtc/rtc_base/physical_socket_server.cc.orig	2019-05-23 12:39:34 UTC
+--- src/3rdparty/chromium/third_party/webrtc/rtc_base/physical_socket_server.cc.orig	2019-10-21 08:14:54 UTC
 +++ src/3rdparty/chromium/third_party/webrtc/rtc_base/physical_socket_server.cc
-@@ -66,7 +66,7 @@ typedef void* SockOptArg;
+@@ -51,7 +51,7 @@
+ #include "rtc_base/null_socket_server.h"
+ #include "rtc_base/time_utils.h"
+ 
+-#if defined(WEBRTC_LINUX)
++#if defined(WEBRTC_LINUX) && !defined(WEBRTC_BSD)
+ #include <linux/sockios.h>
+ #endif
+ 
+@@ -70,7 +70,7 @@ typedef void* SockOptArg;
  
  #endif  // WEBRTC_POSIX
  
@@ -9,7 +18,7 @@
  
  int64_t GetSocketRecvTimestamp(int socket) {
    struct timeval tv_ioctl;
-@@ -286,7 +286,7 @@ int PhysicalSocket::GetOption(Option opt, int* value) 
+@@ -290,7 +290,7 @@ int PhysicalSocket::GetOption(Option opt, int* value) 
    socklen_t optlen = sizeof(*value);
    int ret = ::getsockopt(s_, slevel, sopt, (SockOptArg)value, &optlen);
    if (ret != -1 && opt == OPT_DONTFRAGMENT) {
@@ -18,7 +27,7 @@
      *value = (*value != IP_PMTUDISC_DONT) ? 1 : 0;
  #endif
    }
-@@ -299,7 +299,7 @@ int PhysicalSocket::SetOption(Option opt, int value) {
+@@ -303,7 +303,7 @@ int PhysicalSocket::SetOption(Option opt, int value) {
    if (TranslateOption(opt, &slevel, &sopt) == -1)
      return -1;
    if (opt == OPT_DONTFRAGMENT) {
@@ -27,7 +36,7 @@
      value = (value) ? IP_PMTUDISC_DO : IP_PMTUDISC_DONT;
  #endif
    }
-@@ -309,7 +309,7 @@ int PhysicalSocket::SetOption(Option opt, int value) {
+@@ -313,7 +313,7 @@ int PhysicalSocket::SetOption(Option opt, int value) {
  int PhysicalSocket::Send(const void* pv, size_t cb) {
    int sent = DoSend(
        s_, reinterpret_cast<const char*>(pv), static_cast<int>(cb),
@@ -36,7 +45,7 @@
        // Suppress SIGPIPE. Without this, attempting to send on a socket whose
        // other end is closed will result in a SIGPIPE signal being raised to
        // our process, which by default will terminate the process, which we
-@@ -338,7 +338,7 @@ int PhysicalSocket::SendTo(const void* buffer,
+@@ -342,7 +342,7 @@ int PhysicalSocket::SendTo(const void* buffer,
    size_t len = addr.ToSockAddrStorage(&saddr);
    int sent =
        DoSendTo(s_, static_cast<const char*>(buffer), static_cast<int>(length),
@@ -45,7 +54,7 @@
                 // Suppress SIGPIPE. See above for explanation.
                 MSG_NOSIGNAL,
  #else
-@@ -529,7 +529,7 @@ int PhysicalSocket::TranslateOption(Option opt, int* s
+@@ -533,7 +533,7 @@ int PhysicalSocket::TranslateOption(Option opt, int* s
        *slevel = IPPROTO_IP;
        *sopt = IP_DONTFRAGMENT;
        break;
--- www/qt5-webengine/files/patch-src_core_core__module.pro
+++ www/qt5-webengine/files/patch-src_core_core__module.pro
@@ -1,8 +1,4 @@
-* Do not pass --no-undefined and -z,defs to the linker because of environ(7).
-* Extend some Linux-only compiler arguments to FreeBSD.
-* Generate split out debug files on FreeBSD too.
-
---- src/core/core_module.pro.orig	2019-05-24 09:01:39 UTC
+--- src/core/core_module.pro.orig	2019-10-23 10:30:33 UTC
 +++ src/core/core_module.pro
 @@ -4,6 +4,9 @@ include(core_common.pri)
  # Needed to set a CFBundleIdentifier
@@ -14,7 +10,7 @@
  linking_pri = $$OUT_PWD/$$getConfigDir()/$${TARGET}.pri
  
  !include($$linking_pri) {
-@@ -30,21 +33,21 @@ RSP_ARCHIVE_FILE = $$OUT_PWD/$$getConfigDir()/$${TARGE
+@@ -30,10 +33,10 @@ RSP_ARCHIVE_FILE = $$OUT_PWD/$$getConfigDir()/$${TARGE
  for(archive, NINJA_ARCHIVES): RSP_A_CONTENT += $$archive
  write_file($$RSP_ARCHIVE_FILE, RSP_A_CONTENT)
  macos:LIBS_PRIVATE += -Wl,-filelist,$$shell_quote($$RSP_OBJECT_FILE)
@@ -27,20 +23,16 @@
  else: LIBS_PRIVATE += $$NINJA_ARCHIVES
  LIBS_PRIVATE += $$NINJA_LIB_DIRS $$NINJA_LIBS
  # GN's LFLAGS doesn't always work across all the Linux configurations we support.
- # The Windows and macOS ones from GN does provide a few useful flags however
+@@ -41,7 +44,7 @@ LIBS_PRIVATE += $$NINJA_LIB_DIRS $$NINJA_LIBS
  
+ unix:qtConfig(webengine-noexecstack): \
+     QMAKE_LFLAGS += -Wl,-z,noexecstack
 -linux {
 +unix {
-     QMAKE_LFLAGS += -Wl,--gc-sections -Wl,-O1 -Wl,-z,now
-     # Embedded address sanitizer symbols are undefined and are picked up by the dynamic link loader
-     # at runtime. Thus we do not to pass the linker flag below, because the linker would complain
-     # about the undefined sanitizer symbols.
--    !sanitizer: QMAKE_LFLAGS += -Wl,-z,defs
-+    #!sanitizer: QMAKE_LFLAGS += -Wl,-z,defs
- } else {
-     QMAKE_LFLAGS += $$NINJA_LFLAGS
- }
-@@ -85,7 +88,7 @@ win32 {
+     # add chromium flags
+     for(flag, NINJA_LFLAGS) {
+         # filter out some flags
+@@ -91,7 +94,7 @@ win32 {
  # and doesn't let Chromium get access to libc symbols through dlsym.
  CONFIG -= bsymbolic_functions
  
