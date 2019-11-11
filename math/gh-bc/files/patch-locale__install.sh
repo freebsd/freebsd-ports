64c3a642b6d170b3a89748d084b1c1574519c032
Stefan Eßer <se@FreeBSD.org>
2019-11-11 14:33:58 +0000

    Update to version 2.3.0
    Approved by:	antoine (implicit)

--- math/gh-bc/files/patch-locale__install.sh
+++ math/gh-bc/files/patch-locale__install.sh
@@ -1,6 +1,6 @@
---- locale_install.sh.orig	2019-05-29 01:32:06 UTC
+--- locale_install.sh.orig	2019-11-09 19:04:17 UTC
 +++ locale_install.sh
-@@ -114,6 +114,7 @@ for file in $locales_dir/*.msg; do
+@@ -193,6 +193,7 @@ for file in $locales_dir/*.msg; do
  		continue
  	fi
  
@@ -8,11 +8,18 @@
  	gencatfile "$loc" "$file"
  
  done
-@@ -143,7 +144,8 @@ for file in $locales_dir/*.msg; do
+@@ -217,14 +218,13 @@ for file in $locales_dir/*.msg; do
+ 		linkdir=$(dirname "$file")
+ 		locale=$(basename "$link" .msg)
+ 		linksrc=$(gen_nlspath "$nlspath" "$locale" "$main_exec")
+-		relloc="${loc##$destdir/}"
+-		rel=$(relpath "$linksrc" "$relloc")
+ 
+ 		if [ ! -f "$destdir/$linksrc" ]; then
  			gencatfile "$destdir/$linksrc" "$linkdir/$link"
  		fi
  
--		ln -s "$linksrc" "$loc"
+-		ln -fs "$rel" "$loc"
 +		echo ln "$linksrc" "$loc"
 +		ln "$linksrc" "$loc"
  	fi
