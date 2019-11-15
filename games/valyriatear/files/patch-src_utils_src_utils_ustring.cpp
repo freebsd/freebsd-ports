c92883d2d1e1901cc7052a6b503b079085f9ce41
Dmitry Marakasov <amdmi3@FreeBSD.org>
2019-11-15 23:39:01 +0000

    - Update to 1.1.0

--- games/valyriatear/files/patch-src_utils_src_utils_ustring.cpp
+++ games/valyriatear/files/patch-src_utils_src_utils_ustring.cpp
@@ -1,11 +1,11 @@
---- src/utils/ustring.cpp.orig	2013-09-24 11:33:12 UTC
-+++ src/utils/ustring.cpp
-@@ -203,7 +203,7 @@ static bool UTF8ToUTF16(const char *sour
+--- src/utils/src/utils/ustring.cpp.orig	2018-10-19 17:18:23 UTC
++++ src/utils/src/utils/ustring.cpp
+@@ -171,7 +171,7 @@ static bool UTF8ToUTF16(const std::string& source, uin
          return false;
      }
  
 -#if (defined(_LIBICONV_VERSION) && _LIBICONV_VERSION == 0x0109) || defined(__FreeBSD__)
 +#if defined(_LIBICONV_VERSION) && _LIBICONV_VERSION == 0x0109
      // We are using an iconv API that uses const char*
-     const char *sourceChar = source;
+     const char *sourceChar = source.c_str();
  #else
