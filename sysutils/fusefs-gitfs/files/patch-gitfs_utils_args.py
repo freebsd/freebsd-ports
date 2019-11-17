afc215ee9e8134793cdfb686b285174b16c42689
jbeich <jbeich@FreeBSD.org>
2019-11-17 10:23:44 +0000

    sysutils/fusefs-gitfs: update to 0.5.2
    Changes:	https://github.com/PressLabs/gitfs/compare/0.4.5.1...0.5.2
    Reported by:	GitHub (watch releases)

--- sysutils/fusefs-gitfs/files/patch-gitfs_utils_args.py
+++ sysutils/fusefs-gitfs/files/patch-gitfs_utils_args.py
@@ -1,18 +1,18 @@
 https://github.com/PressLabs/gitfs/issues/258
 https://github.com/PressLabs/gitfs/issues/257
 
---- gitfs/utils/args.py.orig	2016-10-19 15:12:59 UTC
+--- gitfs/utils/args.py.orig	2019-10-20 11:00:10 UTC
 +++ gitfs/utils/args.py
-@@ -43,7 +43,7 @@ class Args(object):
-             ("foreground", (False, "bool")),
-             ("branch", ("master", "string")),
-             ("allow_other", (False, "bool")),
--            ("allow_root", (True, "bool")),
-+            ("allow_root", (False, "bool")),
-             ("commiter_name", (self.get_commiter_user, "string")),
-             ("commiter_email", (self.get_commiter_email, "string")),
-             ("max_size", (10, "float")),
-@@ -71,12 +71,6 @@ class Args(object):
+@@ -44,7 +44,7 @@ class Args(object):
+                 ("foreground", (False, "bool")),
+                 ("branch", ("master", "string")),
+                 ("allow_other", (False, "bool")),
+-                ("allow_root", (True, "bool")),
++                ("allow_root", (False, "bool")),
+                 ("commiter_name", (self.get_commiter_user, "string")),
+                 ("commiter_email", (self.get_commiter_email, "string")),
+                 ("max_size", (10, "float")),
+@@ -76,12 +76,6 @@ class Args(object):
          return self.check_args(self.set_defaults(args))
  
      def check_args(self, args):
@@ -24,8 +24,8 @@ https://github.com/PressLabs/gitfs/issues/257
 -
          # check log_level
          if args.debug:
-             args.log_level = 'debug'
-@@ -169,7 +163,7 @@ class Args(object):
+             args.log_level = "debug"
+@@ -182,7 +176,7 @@ class Args(object):
          return "{}@{}".format(args.user, socket.gethostname())
  
      def get_repo_path(self, args):
