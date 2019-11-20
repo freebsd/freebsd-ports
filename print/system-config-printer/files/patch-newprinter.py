9ab8bf675303ee8171f87c41a014e21ab538a9be
Eric Turgeon <ericbsd@FreeBSD.org>
2019-11-20 15:23:09 +0000

    update print/system-config-printer to 1.5.12
    Approved by:	mentors (implicit)

--- print/system-config-printer/files/patch-newprinter.py
+++ print/system-config-printer/files/patch-newprinter.py
@@ -1,15 +1,24 @@
---- newprinter.py.orig	2015-01-13 11:56:34.450684352 +0100
-+++ newprinter.py	2015-01-13 11:57:28.467679877 +0100
-@@ -1067,7 +1067,7 @@
- 
-                             (self._host == 'localhost' or
-                              self._host[0] == '/') and
--                            not os.access ("/usr/lib/cups/backend/smb", os.F_OK)):
-+                            not os.access ("%%PREFIX%%/libexec/cups/backend/smb", os.F_OK)):
-                             debugprint ("No smb backend so attempting install")
-                             try:
-                                 pk = installpackage.PackageKit ()
-@@ -1909,7 +1909,7 @@
+--- newprinter.py.orig	2019-11-19 17:32:12.439573000 -0400
++++ newprinter.py	2019-11-19 17:37:00.087735000 -0400
+@@ -1163,7 +1163,7 @@
+                 if (self.device.id and
+                     not self.device.type in ("socket", "lpd", "ipp",
+                                              "http", "https", "bluetooth")):
+-                    name = "%s %s" % (self.device.id_dict["MFG"], 
++                    name = "%s %s" % (self.device.id_dict["MFG"],
+                                       self.device.id_dict["MDL"])
+             except:
+                 nonfatalException ()
+@@ -1411,7 +1411,7 @@
+         # Does the backend need to be installed?
+         if (self.nextnptab_rerun == False and not self.searchedfordriverpackages and
+                (self._host == 'localhost' or self._host[0] == '/') and
+-               not os.access ("/usr/lib/cups/backend/smb", os.F_OK)):
++               not os.access ("%%PREFIX%%/libexec/cups/backend/smb", os.F_OK)):
+             debugprint ("No smb backend so attempting install")
+             try:
+                 pk = installpackage.PackageKit ()
+@@ -2174,7 +2174,7 @@
                  host = device.uri[s:s+e]
          # Try to get make and model via SNMP
          if host:
@@ -18,7 +27,16 @@
              debugprint (host + ": " + repr(args))
              stdout = None
              try:
-@@ -3842,7 +3842,7 @@
+@@ -2382,7 +2382,7 @@
+                     else:
+                         device2.uri = "delete"
+         devices = [x for x in devices if x.uri not in ("hp", "hpfax",
+-                                                       "hal", "beh", "smb", 
++                                                       "hal", "beh", "smb",
+                                                        "scsi", "http", "bjnp",
+                                                        "delete")]
+         newdevices = []
+@@ -4212,7 +4212,7 @@
                  try:
                      # We want this to be in the current natural language,
                      # so we intentionally don't set LC_ALL=C here.
@@ -26,4 +44,4 @@
 +                    p = subprocess.Popen (['%%PREFIX%%/bin/cupstestppd',
                                             '-rvv', filename],
                                            close_fds=True,
-                                           stdin=file("/dev/null"),
+                                           stdin=subprocess.DEVNULL,
