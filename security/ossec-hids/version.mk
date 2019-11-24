97ffa77964c9648b271c9cd9f0db7c19ff67ac3d
fernape <fernape@FreeBSD.org>
2019-11-24 17:58:28 +0000

    security/ossec-hids*: update to 3.5.0
    ChangeLogs:
            https://github.com/ossec/ossec-hids/releases/tag/3.4.0
            https://github.com/ossec/ossec-hids/releases/tag/3.5.0
    
    PR:     242110
    Submitted by:   dominik.lisiak@bemsoft.pl (maintainer)

---
+++ security/ossec-hids/version.mk
@@ -0,0 +1,9 @@
+PORTNAME=	ossec-hids
+PORTVERSION=	3.5.0
+PORTREVISION?=	0
+CATEGORIES=	security
+
+MAINTAINER=	dominik.lisiak@bemsoft.pl
+COMMENT?=	Security tool to monitor and check logs and intrusions
+
+LICENSE=	GPLv2
