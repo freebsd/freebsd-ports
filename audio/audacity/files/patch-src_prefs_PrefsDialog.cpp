3438fddffd07af0484be7cc60ee063d4c6fec59f
Thomas Zander <riggs@FreeBSD.org>
2019-11-24 20:08:49 +0000

    MFH: r514755 r514770 r518301
    audio/audacity: Remove bogus STATIC_WX option
    
    This was accepted by me in r495224.  STATIC_WX_ENABLE was probably
    supposed to be STATIC_WX_CONFIGURE_ENABLE but the entire idea of
    the option is flawed.  wxgtk31 does not provide static libraries.
    
    While here drop unhooked JACK_LIB_DEPENDS options helper.  Audacity
    has been using the system Portaudio for a while and JACK support
    can be enabled there.
    
    Remove a bunch of unused *_DESC
    
    Update to upstream version 2.3.3
    
    PR:		242169
    Submitted by:	xxjack12xx@gmail.com (maintainer)
    
    Approved by:	ports-secteam (riggs)

--- audio/audacity/files/patch-src_prefs_PrefsDialog.cpp
+++ audio/audacity/files/patch-src_prefs_PrefsDialog.cpp
@@ -1,6 +1,6 @@
 --- src/prefs/PrefsDialog.cpp.orig	2019-05-13 18:00:33.704997000 -0700
 +++ src/prefs/PrefsDialog.cpp	2019-05-13 17:52:51.702814000 -0700
-@@ -29,6 +29,7 @@
+@@ -30,6 +30,7 @@
  #include <wx/intl.h>
  #include <wx/listbox.h>
  #include <wx/sizer.h>
