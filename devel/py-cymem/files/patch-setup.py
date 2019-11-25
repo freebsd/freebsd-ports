ae3dfb002af2154ddff7baca543b3980b6ba9152
Yuri Victorovich <yuri@FreeBSD.org>
2019-11-25 01:19:46 +0000

    devel/py-cymem: Update 2.0.2 -> 2.0.3
    Reported by:	portscout

--- devel/py-cymem/files/patch-setup.py
+++ devel/py-cymem/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2019-06-16 03:00:04 UTC
+--- setup.py.orig	2019-11-13 12:12:11 UTC
 +++ setup.py
-@@ -119,7 +119,7 @@ def setup_package():
-             url=about['__uri__'],
-             license=about['__license__'],
+@@ -121,7 +121,7 @@ def setup_package():
+             url=about["__uri__"],
+             license=about["__license__"],
              ext_modules=ext_modules,
--            setup_requires=['wheel>=0.32.0,<0.33.0'],
+-            setup_requires=["wheel>=0.32.0,<0.33.0"],
 +            setup_requires=[],
              classifiers=[
-                 'Environment :: Console',
-                 'Intended Audience :: Developers',
+                 "Environment :: Console",
+                 "Intended Audience :: Developers",
