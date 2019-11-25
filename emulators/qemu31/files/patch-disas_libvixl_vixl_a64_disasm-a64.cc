d5083d08c4bdf6db1f8270010527c9c42525074e
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2019-11-25 09:43:08 +0000

    [NEW] emulators/qemu31: QEMU CPU Emulator - 3.1.X version
    QEMU is a FAST! processor emulator using dynamic translation to achieve
    good emulation speed.
    QEMU has two operating modes:
    
    * Full system emulation. In this mode, QEMU emulates a full system
      (for example a PC), including a processor and various peripherials.
      It can be used to launch different Operating Systems without rebooting
      the PC or to debug system code.
    * User mode emulation (Linux host only). In this mode, QEMU can launch
      Linux processes compiled for one CPU on another CPU. It can be used to
      launch the Wine Windows API emulator or to ease cross-compilation and
      cross-debugging.
    
      As QEMU requires no host kernel patches to run, it is very safe and easy to use.
    
      See also the preconfigured system images on http://oszoo.org/
      Many live cd isos also work.
    
    WWW: http://wiki.qemu.org/Main_Page
    
    This is a repocopy of emulators/qemu as qemu will be updated to latest
    STABLE 4.1.X branch

---
+++ emulators/qemu31/files/patch-disas_libvixl_vixl_a64_disasm-a64.cc
@@ -0,0 +1,11 @@
+--- disas/libvixl/vixl/a64/disasm-a64.cc.orig	2019-04-12 03:28:28 UTC
++++ disas/libvixl/vixl/a64/disasm-a64.cc
+@@ -2693,7 +2693,7 @@ void Disassembler::AppendPCRelativeOffsetToOutput(cons
+   if (offset < 0) {
+     abs_offset = -abs_offset;
+   }
+-  AppendToOutput("#%c0x%" PRIx64, sign, abs_offset);
++  AppendToOutput("#%c0x%" PRIx64, sign, offset < 0 ? -offset : offset);
+ }
+ 
+ 
