fb383d32ee8df2f463f32e68352c88bf9fed17b7
Bradley T. Hughes <bhughes@FreeBSD.org>
2019-11-29 05:24:38 +0000

    www/node: Update 13.1.0 -> 13.2.0
    https://nodejs.org/en/blog/release/v13.2.0/
    
    Sponsored by:	Miles AS

--- www/node/files/patch-deps_v8_src_execution_simulator.h
+++ www/node/files/patch-deps_v8_src_execution_simulator.h
@@ -1,9 +1,9 @@
---- deps/v8/src/execution/simulator.h.orig	2019-04-26 17:47:06 UTC
+--- deps/v8/src/execution/simulator.h.orig	2019-11-21 21:14:01 UTC
 +++ deps/v8/src/execution/simulator.h
-@@ -124,7 +124,7 @@ class GeneratedCode {
- 
-   DISABLE_CFI_ICALL Return Call(Args... args) {
-     // When running without a simulator we call the entry directly.
+@@ -128,7 +128,7 @@ class GeneratedCode {
+ #if defined(V8_TARGET_OS_WIN) && !defined(V8_OS_WIN)
+     FATAL("Generated code execution not possible during cross-compilation.");
+ #endif  // defined(V8_TARGET_OS_WIN) && !defined(V8_OS_WIN)
 -#if V8_OS_AIX
 +#if V8_TARGET_ARCH_PPC_BE
      // AIX ABI requires function descriptors (FD).  Artificially create a pseudo
