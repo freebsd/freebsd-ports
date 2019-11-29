585529b5a7b2817ea141f07295f6fb23e7015f63
bofh <bofh@FreeBSD.org>
2019-11-29 08:54:56 +0000

    mail/imapfilter: Update version 2.6.14=>2.6.16

--- mail/imapfilter/files/patch-src_imapfilter.c
+++ mail/imapfilter/files/patch-src_imapfilter.c
@@ -1,4 +1,4 @@
---- src/imapfilter.c.orig	2017-02-27 08:19:40 UTC
+--- src/imapfilter.c.orig	2019-11-22 22:07:11 UTC
 +++ src/imapfilter.c
 @@ -21,7 +21,7 @@
  
@@ -9,7 +9,7 @@
  extern SSL_CTX *sslctx;
  #else
  extern SSL_CTX *ssl23ctx;
-@@ -131,7 +131,7 @@ main(int argc, char *argv[])
+@@ -132,7 +132,7 @@ main(int argc, char *argv[])
  
  	SSL_library_init();
  	SSL_load_error_strings();
@@ -18,16 +18,16 @@
  	sslctx = SSL_CTX_new(TLS_method());
  #else
  	ssl23ctx = SSL_CTX_new(SSLv23_client_method());
-@@ -152,7 +152,7 @@ main(int argc, char *argv[])
- 		capath = opts.truststore;
- 	else if (exists_file(opts.truststore))
+@@ -156,7 +156,7 @@ main(int argc, char *argv[])
  		cafile = opts.truststore;
+ 		capath = NULL;
+ 	}
 -#if OPENSSL_VERSION_NUMBER >= 0x1010000fL
 +#if OPENSSL_VERSION_NUMBER >= 0x1010000fL || LIBRESSL_VERSION_NUMBER
  	if (sslctx)
  		SSL_CTX_load_verify_locations(sslctx, cafile, capath);
  #else
-@@ -193,7 +193,7 @@ main(int argc, char *argv[])
+@@ -197,7 +197,7 @@ main(int argc, char *argv[])
  #endif
  	stop_lua();
  
