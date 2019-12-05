1423b465c937a45e4a460579445c8999c2f6806f
joneum <joneum@FreeBSD.org>
2019-12-05 18:12:07 +0000

    Update to 5.7.28
    Changelog: https://dev.mysql.com/doc/relnotes/mysql/5.7/en/news-5-7-28.html
    
    PR:		241920
    Approved by:	mmokhi (maintainer, implicit)
    Sponsored by:	Netzkommune GmbH

--- databases/mysql57-server/files/patch-sql_mysqld.cc
+++ databases/mysql57-server/files/patch-sql_mysqld.cc
@@ -1,9 +1,9 @@
---- sql/mysqld.cc.orig	2018-06-08 11:56:29.000000000 +0200
-+++ sql/mysqld.cc	2018-08-11 13:38:48.782595000 +0200
-@@ -3408,7 +3408,7 @@
+--- sql/mysqld.cc.orig	2019-09-27 09:00:15.000000000 +0200
++++ sql/mysqld.cc	2019-11-24 17:47:52.395539000 +0100
+@@ -3419,7 +3419,7 @@
+ static int init_ssl()
  {
  #ifdef HAVE_OPENSSL
- #ifndef HAVE_YASSL
 -#if OPENSSL_VERSION_NUMBER < 0x10100000L
 +#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
    CRYPTO_malloc_init();
