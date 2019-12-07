666ee148510b2b5f5c13b20de777106bdee872ae
Xin LI <delphij@FreeBSD.org>
2019-12-07 22:47:43 +0000

    - Drop support of FreeBSD < 11.0  - Make use of robust mutexes.

--- databases/lmdb/files/patch-mdb.c
+++ databases/lmdb/files/patch-mdb.c
@@ -1,11 +1,14 @@
---- mdb.c.orig	2017-06-01 16:51:10 UTC
+--- mdb.c.orig	2019-07-19 16:41:12 UTC
 +++ mdb.c
-@@ -125,6 +125,8 @@ typedef SSIZE_T	ssize_t;
- # define MDB_FDATASYNC		fsync
- #elif defined(ANDROID)
- # define MDB_FDATASYNC		fsync
-+#elif defined(__FreeBSD_version) && __FreeBSD_version < 1101000
-+# define MDB_FDATASYNC		fsync
+@@ -124,7 +124,10 @@ typedef SSIZE_T	ssize_t;
+ #include <resolv.h>	/* defines BYTE_ORDER on HPUX and Solaris */
  #endif
  
- #ifndef _WIN32
+-#if defined(__APPLE__) || defined (BSD) || defined(__FreeBSD_kernel__)
++#if defined(__FreeBSD__)
++#  define MDB_USE_POSIX_MUTEX	1
++#  define MDB_USE_ROBUST	1
++#elif defined(__APPLE__) || defined (BSD) || defined(__FreeBSD_kernel__)
+ # define MDB_USE_POSIX_SEM	1
+ # define MDB_FDATASYNC		fsync
+ #elif defined(ANDROID)
