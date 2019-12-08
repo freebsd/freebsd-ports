20b064f81439dcc4f63ae21ab00b23c17428bfc3
truckman <truckman@FreeBSD.org>
2019-12-08 22:49:11 +0000

    Attempt to fix gcc builds on powerpc, that were broken by a boost upgrade.
    Clang builds using -std=gnu++98 can use the STL headers in
    /usr/include/c++/v1, but recent versions of the gcc headers forbid this.
    As a fallback, OpenOffice tries to use the TR1 headers supplied by boost,
    but recent versions of boost no longer have those headers.  In theory
    the gcc TR1 headers should work, but I was not able to make those work.
    Solve this problem by doing gcc builds using the "bundled" version of
    boost which is much older rather that boost from ports.
    
    Patch the "bundled" boost to fix the compiler feature detection logic
    to get rid of a bunch of compiler warnings.
    
    The headers in the ports version of vigra has some C++11 stuff that
    gcc also complains about, so use the "bundled" version of vigra for
    gcc builds as well.
    
    The icu patch may only be needed for non-default values of LOCALBASE.
    
    Comment out an unused method in basebmp/inc/basebmp/packedpixeliterator.hxx
    that contains some broken code that causes gcc to error out.
    
    Add a missing include to l10ntools/source/inireader.cxx to resolve
    a compile error.
    
    Add a missing include to scripting/source/protocolhandler/scripthandler.cxx
    to resolve a compile error.
    
    No PORTREVISION bump since amd64 and i386 builds should be unchanged.
    
    Tested by:	Curtis Hamilton clhamilto AT gmail.com

---
+++ editors/openoffice-4/files/patch-l10ntools_source_inireader.cxx
@@ -0,0 +1,10 @@
+--- l10ntools/source/inireader.cxx.orig	2019-08-14 17:32:51 UTC
++++ l10ntools/source/inireader.cxx
+@@ -21,6 +21,7 @@
+ 
+ #include <unicode/regex.h>
+ #include <unicode/unistr.h>
++#include <cstdlib>
+ #include <string>
+ #include <fstream>
+ #include <iostream>
