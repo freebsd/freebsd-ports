94108af78bab9a7002d363b35421bf1f6ac69205
Alexey Dokuchaev <danfe@FreeBSD.org>
2019-12-08 12:15:29 +0000

    Belatedly update Doom Legacy to version 1.47.2 (r1412) and unbreak.
    The new features include MBF (Marine's Best Friend) compatibility, support
    for sprites with 16 rotation positions, the possibility of multiple clients
    and the server running on the same computer, plus many bug fixes.
    
    While here, move ${DMDIR} up in WAD search list (DEFWADS20 -> DEFWADS04),
    add ASM_BROKEN and missing `gl' to USE_GL, improve diagnostics and fix
    potential segmentation fault in the owner_wad_search_order() function.

--- games/doomlegacy/files/patch-src_d__main.c
+++ games/doomlegacy/files/patch-src_d__main.c
@@ -1,27 +1,26 @@
---- src/d_main.c.orig	2017-03-03 19:49:10 UTC
+--- src/d_main.c.orig	2018-07-25 10:14:34 UTC
 +++ src/d_main.c
-@@ -439,11 +439,13 @@ extern char mac_user_home[FILENAME_SIZE]
- void  owner_wad_search_order( void )
- {
-     // Wad search order.
-+#if 0
-     if( defdir_stat )
-     {
-         // Search current dir near first, for other wad searches.
-         doomwaddir[1] = defdir;
+@@ -454,18 +454,20 @@ void  owner_wad_search_order( void )
+         }
+         else
+         if(    (strcmp( defdir, cv_home.string ) != 0) // not home directory
+-	    && (strcmp( defdir, progdir ) != 0)        // not program directory
+-            && (strcmp( defdir, progdir_wads ) != 0) ) // not wads directory
+-        {
++          ) { if( verbose )
++                GenPrintf( EMSG_ver, "User's home is default dir, not searched.\n");
++        } else {
+             defdir_search = 1;
+             // Search current dir near first, for other wad searches.
+             doomwaddir[1] = defdir;
+ 	}
      }
-+#endif
++#if 0 // only useful for developers, missing NULL checks
      // Search progdir/wads early, for other wad searches.
      doomwaddir[2] = progdir_wads;
      // Search last, for other wad searches.
-@@ -1160,8 +1162,10 @@ void  Print_search_directories( byte emf
-     // Verbose only. For IWAD or legacy.wad they are in doomwaddir entries.
-     if( (enables==0x0F) && progdir_wads )
-         GenPrintf(emf, "        : %s\n", progdir_wads );
-+#if 0
-     if( (enables==0x0F) && defdir )
-         GenPrintf(emf, " defdir: %s\n", defdir );
+     doomwaddir[MAX_NUM_DOOMWADDIR-1] = progdir;
 +#endif
- #ifdef LEGACYWADDIR
-     GenPrintf(emf, " LEGACYWADDIR: %s\n", LEGACYWADDIR );
- #endif
+ }
+ 
+ 
