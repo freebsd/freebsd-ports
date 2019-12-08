18e18777a7ffcb86925564efdb6a0b6b4cb92edb
tcberner <tcberner@FreeBSD.org>
2019-12-08 11:28:16 +0000

    multimedia/libopenshot: Update to 0.2.3.490
    PR:		241912
    Submitted by:	Tatsuki Makino <tatsuki_makino@hotmail.com> (maintainer)

--- multimedia/libopenshot/files/patch-src_FFmpegWriter.cpp
+++ multimedia/libopenshot/files/patch-src_FFmpegWriter.cpp
@@ -1,33 +1,36 @@
---- src/FFmpegWriter.cpp.orig	2019-05-08 19:16:50 UTC
+--- src/FFmpegWriter.cpp.orig	2019-11-04 12:14:04 UTC
 +++ src/FFmpegWriter.cpp
-@@ -166,7 +166,7 @@ void FFmpegWriter::SetVideoOptions(bool has_video, str
+@@ -172,7 +172,7 @@ void FFmpegWriter::SetVideoOptions(bool has_video, str
  		AVCodec *new_codec;
  		// Check if the codec selected is a hardware accelerated codec
  #if IS_FFMPEG_3_2
--		#if defined(__linux__)
+-#if defined(__linux__)
 +#if defined(__unix__)
- 		if ( (strcmp(codec.c_str(),"h264_vaapi") == 0)) {
+ 		if (strstr(codec.c_str(), "_vaapi") != NULL) {
  			new_codec = avcodec_find_encoder_by_name(codec.c_str());
  			hw_en_on = 1;
-@@ -225,7 +225,7 @@ void FFmpegWriter::SetVideoOptions(bool has_video, str
+@@ -220,9 +220,9 @@ void FFmpegWriter::SetVideoOptions(bool has_video, str
+ 			hw_en_on = 0;
+ 			hw_en_supported = 0;
  		}
- #else  // is FFmpeg 3 but not linux
+-	#else  // is FFmpeg 3 but not linux
++#else  // is FFmpeg 3 but not linux
  		new_codec = avcodec_find_encoder_by_name(codec.c_str());
--#endif //__linux__
+-	#endif //__linux__
 +#endif //__unix__
  #else // not ffmpeg 3
  		new_codec = avcodec_find_encoder_by_name(codec.c_str());
  #endif //IS_FFMPEG_3_2
-@@ -888,7 +888,7 @@ void FFmpegWriter::close_video(AVFormatContext *oc, AV
- 	AV_FREE_CONTEXT(video_codec);
- 	video_codec = NULL;
+@@ -956,7 +956,7 @@ void FFmpegWriter::flush_encoders() {
+ void FFmpegWriter::close_video(AVFormatContext *oc, AVStream *st)
+ {
  #if IS_FFMPEG_3_2
 -	//  #if defined(__linux__)
 +	//  #if defined(__unix__)
  		if (hw_en_on && hw_en_supported) {
  			if (hw_device_ctx) {
  				av_buffer_unref(&hw_device_ctx);
-@@ -1290,7 +1290,7 @@ void FFmpegWriter::open_video(AVFormatContext *oc, AVS
+@@ -1351,7 +1351,7 @@ void FFmpegWriter::open_video(AVFormatContext *oc, AVS
  		adapter_num = openshot::Settings::Instance()->HW_EN_DEVICE_SET;
  		fprintf(stderr, "\n\nEncodiing Device Nr: %d\n", adapter_num);
  		if (adapter_num < 3 && adapter_num >=0) {
@@ -36,7 +39,7 @@
  				snprintf(adapter,sizeof(adapter),"/dev/dri/renderD%d", adapter_num+128);
  				// Maybe 127 is better because the first card would be 1?!
  				adapter_ptr = adapter;
-@@ -1298,17 +1298,21 @@ void FFmpegWriter::open_video(AVFormatContext *oc, AVS
+@@ -1359,17 +1359,21 @@ void FFmpegWriter::open_video(AVFormatContext *oc, AVS
  				adapter_ptr = NULL;
  #elif defined(__APPLE__)
  				adapter_ptr = NULL;
@@ -58,4 +61,4 @@
 +#else
  		if( adapter_ptr != NULL ) {
  #endif
- 				ZmqLogger::Instance()->AppendDebugMethod("Encode Device present using device", "", -1, "", -1, "", -1, "", -1, "", -1, "", -1);
+ 				ZmqLogger::Instance()->AppendDebugMethod("Encode Device present using device", "adapter", adapter_num);
