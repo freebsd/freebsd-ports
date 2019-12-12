936b4eeb1b4c2871d0bfe2e85df24bd2a3a43cb1
Piotr Kubaj <pkubaj@FreeBSD.org>
2019-12-12 14:57:24 +0000

    devel/avr-gcc: fix build on powerpc64 elfv2
    This port needs a patch similar to other GCC ports to avoid namespace collision on powerpc64 elfv2.
    
    PR:		242284
    Approved by:	joerg (maintainer timeout)

---
+++ devel/avr-gcc/files/patch-gcc_tree-vect-loop.c
@@ -0,0 +1,11 @@
+--- gcc/tree-vect-loop.c.orig	2019-11-28 10:47:59 UTC
++++ gcc/tree-vect-loop.c
+@@ -55,6 +55,8 @@ along with GCC; see the file COPYING3.  If not see
+ #include "vec-perm-indices.h"
+ #include "tree-eh.h"
+ 
++#define vec_step vec_step_
++
+ /* Loop Vectorization Pass.
+ 
+    This pass tries to vectorize loops.
