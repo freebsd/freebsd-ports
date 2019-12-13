faf3f2b8c06410089ffea0bec5a4769f25986018
tobik <tobik@FreeBSD.org>
2019-12-13 06:02:07 +0000

    security/testssl.sh: Update to 3.0rc6
    - Hook up tests
    
    Changes:	https://github.com/drwetter/testssl.sh/releases/tag/3.0rc6

--- security/testssl.sh/files/patch-testssl.sh
+++ security/testssl.sh/files/patch-testssl.sh
@@ -1,12 +1,12 @@
---- testssl.sh.orig	2018-09-08 11:00:02 UTC
+--- testssl.sh.orig	2019-12-11 11:41:31 UTC
 +++ testssl.sh
-@@ -188,6 +188,9 @@ TERM_CURRPOS=0                                        
+@@ -190,6 +190,9 @@ TERM_CURRPOS=0                                        
  ########### Defining (and presetting) variables which can be changed
  #
  # Following variables make use of $ENV and can be used like "OPENSSL=<myprivate_path_to_openssl> ./testssl.sh <URI>"
 +if [[ -z "$OPENSSL" ]] ; then
 +     OPENSSL=PREFIX/openssl-unsafe/bin/openssl
 +fi
- declare -x OPENSSL OPENSSL_TIMEOUT
- PHONE_OUT=${PHONE_OUT:-false}           # Whether testssl can retrieve CRLs and OCSP
- FAST_SOCKET=${FAST_SOCKET:-false}       # EXPERIMENTAL feature to accelerate sockets -- DO NOT USE it for production
+ declare -x OPENSSL
+ OPENSSL_TIMEOUT=${OPENSSL_TIMEOUT:-""}  # Default connect timeout with openssl before we call the server side unreachable
+ CONNECT_TIMEOUT=${CONNECT_TIMEOUT:-""}  # Default connect timeout with sockets before we call the server side unreachable
