93e488a57522f63129694776776b9f931c1c17dd
Tobias Kortkamp <tobik@FreeBSD.org>
2019-12-17 06:14:49 +0000

    multimedia/kodi-devel: Update to newer snapshot
    - Switch to upstreamed patches
    - Add patches for improved CPUInfo support [1]
    
    [1] https://github.com/xbmc/xbmc/pull/16779
    
    Submitted by:	rozhuk.im@gmail.com [1]

--- multimedia/kodi-devel/files/patch-cmake_platform_freebsd_gbm.cmake
+++
@@ -1,5 +0,0 @@
---- cmake/platform/freebsd/gbm.cmake.orig	2019-05-08 08:47:45 UTC
-+++ cmake/platform/freebsd/gbm.cmake
-@@ -1 +1,2 @@
- include(cmake/platform/linux/gbm.cmake)
-+list(APPEND PLATFORM_REQUIRED_DEPS EpollShim)
