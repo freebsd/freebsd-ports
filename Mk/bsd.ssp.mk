0850d05fc0ca447aebf677d6209da71105729741
John Baldwin <jhb@FreeBSD.org>
2019-12-20 01:11:41 +0000

    Use ARCH instead of MACHINE to check for mips.
    Cross-building ports via CROSS_TOOLCHAIN and CROSS_SYSROOT only set
    ARCH to the target arch.  MACHINE still references the host.  This
    unbreaks cross-building ports for mips.
    
    Reviewed by:	jbeich, portmgr (bapt)
    Differential Revision:	https://reviews.freebsd.org/D22592

--- Mk/bsd.ssp.mk
+++ Mk/bsd.ssp.mk
@@ -4,7 +4,7 @@
 SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 
 .if !defined(SSP_UNSAFE) && \
-    (${MACHINE} != "mips")
+    (! ${ARCH:Mmips*})
 # Overridable as a user may want to use -fstack-protector-all
 SSP_CFLAGS?=	-fstack-protector-strong
 CFLAGS+=	${SSP_CFLAGS}
