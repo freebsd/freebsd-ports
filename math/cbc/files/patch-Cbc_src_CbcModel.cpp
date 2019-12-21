5c6e1265ffa0f36389d2f6209efc34b498be9bc6
yuri <yuri@FreeBSD.org>
2019-12-21 22:58:54 +0000

    math/cbc: Update 2.9.9 -> 2.10.3

--- math/cbc/files/patch-Cbc_src_CbcModel.cpp
+++ math/cbc/files/patch-Cbc_src_CbcModel.cpp
@@ -1,11 +1,11 @@
---- Cbc/src/CbcModel.cpp.orig	2018-04-16 07:45:54 UTC
+--- Cbc/src/CbcModel.cpp.orig	2019-12-21 21:30:53 UTC
 +++ Cbc/src/CbcModel.cpp
-@@ -5338,7 +5338,7 @@ void CbcModel::branchAndBound(int doStat
-         OsiClpSolverInterface * clpSolver
-         = dynamic_cast<OsiClpSolverInterface *> (solver_);
-         if (clpSolver)
--            clpSolver->setFakeObjective(reinterpret_cast<double *> (NULL));
-+            clpSolver->setFakeObjective((double*)nullptr);
-     }
+@@ -5417,7 +5417,7 @@ void CbcModel::branchAndBound(int doStatistics)
+     OsiClpSolverInterface *clpSolver
+       = dynamic_cast< OsiClpSolverInterface * >(solver_);
+     if (clpSolver)
+-      clpSolver->setFakeObjective(reinterpret_cast< double * >(NULL));
++      clpSolver->setFakeObjective((double*)nullptr);
+   }
  #endif
-     moreSpecialOptions_ = saveMoreSpecialOptions;
+   moreSpecialOptions_ = saveMoreSpecialOptions;
