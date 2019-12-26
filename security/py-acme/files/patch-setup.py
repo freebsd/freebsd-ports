ba9c25b4d3847a6332a44aa7c0eb28046b63d934
Kurt Jaeger <pi@FreeBSD.org>
2019-12-26 19:25:00 +0000

    security/py-{acme|certbot*}: update 0.39 -> 1.0.0
    PR:		242458
    Submitted by:	Yasuhiro KIMURA <yasu@utahime.org>
    Relnotes:	https://github.com/certbot/certbot/releases/tag/v1.0.0

--- security/py-acme/files/patch-setup.py
+++ security/py-acme/files/patch-setup.py
@@ -1,14 +1,14 @@
---- setup.py.orig	2019-02-07 21:20:31 UTC
+--- setup.py.orig	2019-12-05 17:30:21 UTC
 +++ setup.py
-@@ -13,7 +13,6 @@ install_requires = [
-     # formerly known as acme.jose:
-     'josepy>=1.0.0',
-     # Connection.set_tlsext_host_name (>=0.13)
+@@ -14,7 +14,6 @@ install_requires = [
+     # 1.1.0+ is required to avoid the warnings described at
+     # https://github.com/certbot/josepy/issues/13.
+     'josepy>=1.1.0',
 -    'mock',
+     # Connection.set_tlsext_host_name (>=0.13)
      'PyOpenSSL>=0.13.1',
      'pyrfc3339',
-     'pytz',
-@@ -24,6 +23,7 @@ install_requires = [
+@@ -26,6 +25,7 @@ install_requires = [
  ]
  
  dev_extras = [
