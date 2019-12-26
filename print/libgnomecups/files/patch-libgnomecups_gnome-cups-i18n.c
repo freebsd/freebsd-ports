c585611c0475ff5129f55022b1adbfd4054021ce
bapt <bapt@FreeBSD.org>
2019-12-26 21:59:06 +0000

    garbage collect libgnomecups, which should have been collected along with libgnomeprint

--- print/libgnomecups/files/patch-libgnomecups_gnome-cups-i18n.c
+++
@@ -1,9 +0,0 @@
---- libgnomecups/gnome-cups-i18n.c.orig	2012-06-07 10:55:30.000000000 +0200
-+++ libgnomecups/gnome-cups-i18n.c	2012-06-07 10:55:37.000000000 +0200
-@@ -1,5 +1,5 @@
- #include <config.h>
--#include <glib/gmacros.h>
-+#include <glib.h>
- #include "gnome-cups-i18n.h"
- 
- #ifdef ENABLE_NLS
