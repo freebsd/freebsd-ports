f6733154e530cbfd5bd33efea0afc3c93c3a333e
Yuri Victorovich <yuri@FreeBSD.org>
2019-12-27 05:04:29 +0000

    devel/vitables: Update 3.0.0-55 -> 3.0.2
    Reported by:	portscout

--- devel/vitables/files/patch-setup.py
+++ devel/vitables/files/patch-setup.py
@@ -1,10 +1,13 @@
---- setup.py.orig	2018-09-21 23:38:58 UTC
+--- setup.py.orig	2019-12-27 04:39:05 UTC
 +++ setup.py
-@@ -51,7 +51,6 @@ setup(name='ViTables',
-       ],
-       install_requires=[
-           'qtpy (>=1.2.1)',
--          'PyQt5 (>=5.5.1)',
-           'numpy (>=1.4.1)',
-           'numexpr (>=2.0)',
-           'tables (>=3.0)'
+@@ -31,8 +31,8 @@ def read(fname):
+ 
+ install_requires = ['qtpy (>=1.2.1)', 'numpy (>=1.4.1)', 'numexpr (>=2.0)',
+                     'tables (>=3.0)']
+-if 'CONDA_PREFIX' not in os.environ:
+-    install_requires.append('PyQt5 (>=5.5.1)')
++#if 'CONDA_PREFIX' not in os.environ:
++#    install_requires.append('PyQt5 (>=5.5.1)')
+ 
+ setup(name='ViTables',
+       version=read('VERSION'),
