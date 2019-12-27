a8ee87b62c2c4d8dfe75e2be75a03d72d75f1a92
Baptiste Daroussin <bapt@FreeBSD.org>
2019-12-27 16:28:41 +0000

    Ensure to be able to build with only one libncurses

--- sysutils/dvtm/files/patch-config.mk
+++ sysutils/dvtm/files/patch-config.mk
@@ -1,6 +1,6 @@
 --- config.mk.orig	2016-01-09 11:40:56 UTC
 +++ config.mk
-@@ -4,14 +4,14 @@ VERSION = 0.15
+@@ -4,14 +4,19 @@ VERSION = 0.15
  # Customize below to fit your system
  
  PREFIX ?= /usr/local
@@ -11,8 +11,14 @@
  TERMINFO := ${DESTDIR}${PREFIX}/share/terminfo
  
  INCS = -I.
- LIBS = -lc -lutil -lncursesw
+-LIBS = -lc -lutil -lncursesw
 -CPPFLAGS = -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_XOPEN_SOURCE_EXTENDED
++LIBS = -lc -lutil
++.if exists(/usr/lib/libncursesw.so)
++LIBS += -lncursesw
++.else
++LIBS += -lncurses
++.endif
 +CPPFLAGS = -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_XOPEN_SOURCE_EXTENDED -D__BSD_VISIBLE
  CFLAGS += -std=c99 ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG ${CPPFLAGS}
  LDFLAGS += ${LIBS}
