d4b5cafeae0551d6e9103d7558f5b0ba2f1db992
Koichiro Iwao <meta@FreeBSD.org>
2019-12-31 14:07:41 +0000

    databases/rubygem-dbm: Update to 1.1.0
    PR:		242868
    Submitted by:	maintainer

--- databases/rubygem-dbm/files/patch-ext_dbm_extconf.rb
+++ databases/rubygem-dbm/files/patch-ext_dbm_extconf.rb
@@ -1,26 +1,24 @@
---- ext/dbm/extconf.rb.orig	2018-08-01 14:46:21 UTC
+--- ext/dbm/extconf.rb.orig	2019-12-01 06:24:29 UTC
 +++ ext/dbm/extconf.rb
-@@ -23,7 +23,7 @@ dir_config("dbm")
+@@ -24,7 +24,7 @@ dir_config("dbm")
  if dblib = with_config("dbm-type", nil)
    dblib = dblib.split(/[ ,]+/)
  else
--  dblib = %w(libc db db2 db1 db5 db4 db3 gdbm_compat gdbm qdbm)
+-  dblib = %w(libc db db2 db1 db6 db5 db4 db3 gdbm_compat gdbm qdbm)
 +  dblib = %w(libc db db2 db1 db6 db5 db48 db3 gdbm_compat gdbm qdbm)
  end
  
  headers = {
-@@ -32,8 +32,9 @@ headers = {
+@@ -33,7 +33,7 @@ headers = {
    "db1" => ["db1/ndbm.h", "db1.h", "ndbm.h"],
    "db2" => ["db2/db.h", "db2.h", "db.h"],
    "db3" => ["db3/db.h", "db3.h", "db.h"],
 -  "db4" => ["db4/db.h", "db4.h", "db.h"],
 +  "db48" => ["db48/db.h", "db4.h", "db.h"],
    "db5" => ["db5/db.h", "db5.h", "db.h"],
-+  "db6" => ["db6/db.h", "db6.h", "db.h"],
+   "db6" => ["db6/db.h", "db6.h", "db.h"],
    "gdbm_compat" => ["gdbm-ndbm.h", "gdbm/ndbm.h", "ndbm.h"], # GDBM since 1.8.1
-   "gdbm" => ["gdbm-ndbm.h", "gdbm/ndbm.h", "ndbm.h"], # GDBM until 1.8.0
-   "qdbm" => ["qdbm/relic.h", "relic.h"],
-@@ -125,13 +126,26 @@ end
+@@ -127,6 +127,19 @@ end
  
  
  def headers.db_check2(db, hdr)
@@ -40,15 +38,7 @@
    $defs.push(%{-DRUBYDBM_DBM_HEADER='"#{hdr}"'})
    $defs.push(%{-DRUBYDBM_DBM_TYPE='"#{db}"'})
  
-   hsearch = nil
- 
-   case db
--  when /^db[2-5]?$/
-+  when /^db[2-6].*$/
-     hsearch = "-DDB_DBM_HSEARCH"
-   when "gdbm_compat"
-     have_library("gdbm") or return false
-@@ -145,7 +159,7 @@ def headers.db_check2(db, hdr)
+@@ -147,7 +160,7 @@ def headers.db_check2(db, hdr)
    # 4.3BSD original ndbm is contained in libc.
    # 4.4BSD (and its derivatives such as NetBSD) contains Berkeley DB 1 in libc.
    if !(db == 'libc' ? have_func('dbm_open("", 0, 0)', hdr, hsearch) :
