6d484806df3ed5f48462fa05ba63f2cce8fd36b3
rene <rene@FreeBSD.org>
2019-12-31 17:07:55 +0000

    Remove expired linux-c6 from Mk

--- Mk/Uses/linux.mk
+++ Mk/Uses/linux.mk
@@ -4,8 +4,7 @@
 #
 # Feature:	linux:args
 # Usage:	USES=linux or USES=linux:args
-# Valid args:	c6	Depend on CentOS 6 packages (default)
-#		c7	Depend on CentOS 7 packages
+# Valid args:	c7	Depend on CentOS 7 packages (default)
 # Additional variables:
 # USE_LINUX	List of Linux packages to depend on.
 # USE_LINUX_RPM	When defined, additional variables and targets useful to Linux
@@ -27,18 +26,14 @@ _INCLUDE_USES_LINUX_MK=	yes
 _USES_POST+=		linux
 
 .if empty(linux_ARGS)
-.if exists(${LINUXBASE}/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6)
-linux_ARGS=		c6
-.elif exists(${LINUXBASE}/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7)
+.if exists(${LINUXBASE}/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7)
 linux_ARGS=		c7
 .else
 linux_ARGS=		${LINUX_DEFAULT}
 .endif
 .endif
 
-.if ${linux_ARGS} == c6
-LINUX_DIST_VER?=	6.10
-.elif ${linux_ARGS} == c7
+.if ${linux_ARGS} == c7
 LINUX_DIST_VER?=	7.7.1908
 .else
 IGNORE=			Invalid Linux distribution: ${linux_ARGS}
@@ -57,7 +52,6 @@ _linux_c7_at-spi2-atk=			linux-c7-at-spi2-atk>0:accessibility/linux-c7-at-spi2-a
 _linux_c7_at-spi2-core=			linux-c7-at-spi2-core>0:accessibility/linux-c7-at-spi2-core
 _linux_${linux_ARGS}_atk=		linux-${linux_ARGS}-atk>0:accessibility/linux-${linux_ARGS}-atk
 _linux_${linux_ARGS}_avahi-libs=	linux-${linux_ARGS}-avahi-libs>0:net/linux-${linux_ARGS}-avahi-libs
-_linux_c6_base=				linux_base-c6>=6.10:emulators/linux_base-c6
 _linux_c7_base=				linux_base-c7>=7.6.1810_7:emulators/linux_base-c7
 _linux_${linux_ARGS}_cairo=		linux-${linux_ARGS}-cairo>0:graphics/linux-${linux_ARGS}-cairo
 _linux_c7_cairo-gobject=		linux-c7-cairo-gobject>0:graphics/linux-c7-cairo-gobject
@@ -71,7 +65,6 @@ _linux_${linux_ARGS}_dri=		linux-${linux_ARGS}-dri>0:graphics/linux-${linux_ARGS
 _linux_${linux_ARGS}_elfutils-libelf=	linux-${linux_ARGS}-elfutils-libelf>0:devel/linux-${linux_ARGS}-elfutils-libelf
 _linux_c7_elfutils-libs=		linux-c7-elfutils-libs>0:devel/linux-c7-elfutils-libs
 _linux_${linux_ARGS}_expat=		linux-${linux_ARGS}-expat>0:textproc/linux-${linux_ARGS}-expat
-_linux_c6_flac=				linux-c6-flac>0:audio/linux-c6-flac
 _linux_c7_flac=				linux-c7-flac-libs>0:audio/linux-c7-flac
 _linux_${linux_ARGS}_fontconfig=	linux-${linux_ARGS}-fontconfig>0:x11-fonts/linux-${linux_ARGS}-fontconfig
 _linux_c7_freetype=			linux-c7-freetype>0:print/linux-c7-freetype
@@ -87,7 +80,6 @@ _linux_${linux_ARGS}_jasper=		linux-${linux_ARGS}-jasper-libs>0:graphics/linux-$
 _linux_c7_jbigkit=			linux-c7-jbigkit-libs>0:graphics/linux-c7-jbigkit
 _linux_${linux_ARGS}_jpeg=		linux-${linux_ARGS}-jpeg>0:graphics/linux-${linux_ARGS}-jpeg
 _linux_${linux_ARGS}_libasyncns=	linux-${linux_ARGS}-libasyncns>0:dns/linux-${linux_ARGS}-libasyncns
-_linux_c6_libaudiofile=			linux-c6-audiofile>0:audio/linux-c6-libaudiofile
 _linux_c7_libaudiofile=			linux-c7-audiofile>0:audio/linux-c7-audiofile
 _linux_c7_libdrm=			linux-c7-libdrm>0:graphics/linux-c7-libdrm
 _linux_c7_libepoxy=			linux-c7-libepoxy>0:graphics/linux-c7-libepoxy
@@ -98,7 +90,6 @@ _linux_${linux_ARGS}_libgpg-error=	linux-${linux_ARGS}-libgpg-error>0:security/l
 _linux_${linux_ARGS}_libogg=		linux-${linux_ARGS}-libogg>0:audio/linux-${linux_ARGS}-libogg
 _linux_${linux_ARGS}_libpciaccess=	linux-${linux_ARGS}-libpciaccess>0:devel/linux-${linux_ARGS}-libpciaccess
 _linux_${linux_ARGS}_libsndfile=	linux-${linux_ARGS}-libsndfile>0:audio/linux-${linux_ARGS}-libsndfile
-_linux_c6_libsoup=			linux-c6-libsoup>0:devel/linux-c6-libsoup
 _linux_${linux_ARGS}_libssh2=		linux-${linux_ARGS}-libssh2>0:security/linux-${linux_ARGS}-libssh2
 _linux_${linux_ARGS}_libtasn1=		linux-${linux_ARGS}-libtasn1>0:security/linux-${linux_ARGS}-libtasn1
 _linux_${linux_ARGS}_libthai=		linux-${linux_ARGS}-libthai>0:devel/linux-${linux_ARGS}-libthai
@@ -111,27 +102,20 @@ _linux_${linux_ARGS}_libxml2=		linux-${linux_ARGS}-libxml2>0:textproc/linux-${li
 _linux_${linux_ARGS}_lttng-ust=		linux-${linux_ARGS}-lttng-ust>0:sysutils/linux-${linux_ARGS}-lttng-ust
 _linux_c7_lz4=				linux-c7-lz4>0:archivers/linux-c7-lz4
 _linux_c7_make=				linux-c7-make>0:devel/linux-c7-make
-_linux_c6_mikmod=			linux-c6-libmikmod>0:audio/linux-c6-mikmod
-_linux_c6_naslibs=			linux-c6-nas>0:audio/linux-c6-nas-libs
 _linux_c7_nettle=			linux-c7-nettle>0:security/linux-c7-nettle
 _linux_${linux_ARGS}_nspr=		linux-${linux_ARGS}-nspr>0:devel/linux-${linux_ARGS}-nspr
 _linux_${linux_ARGS}_nss=		linux-${linux_ARGS}-nss>0:security/linux-${linux_ARGS}-nss
 _linux_${linux_ARGS}_openal-soft=	linux-${linux_ARGS}-openal-soft>0:audio/linux-${linux_ARGS}-openal-soft
 _linux_${linux_ARGS}_openldap=		linux-${linux_ARGS}-openldap>0:net/linux-${linux_ARGS}-openldap
-_linux_c6_openmotif=			linux-c6-openmotif>0:x11-toolkits/linux-c6-openmotif
 _linux_c7_openmotif=			linux-c7-motif>0:x11-toolkits/linux-c7-openmotif
-_linux_c6_openssl=			linux-c6-openssl>0:security/linux-c6-openssl
 _linux_c7_openssl=			${_linux_c7_base}
 _linux_c7_p11-kit=			linux-c7-p11-kit>0:security/linux-c7-p11-kit
 _linux_${linux_ARGS}_pango=		linux-${linux_ARGS}-pango>0:x11-toolkits/linux-${linux_ARGS}-pango
 _linux_${linux_ARGS}_pixman=		linux-${linux_ARGS}-pixman>0:x11/linux-${linux_ARGS}-pixman
 _linux_${linux_ARGS}_png=		linux-${linux_ARGS}-libpng>0:graphics/linux-${linux_ARGS}-png
 _linux_${linux_ARGS}_pulseaudio-libs=	linux-${linux_ARGS}-pulseaudio-libs>0:audio/linux-${linux_ARGS}-pulseaudio-libs
-_linux_c6_qt=				linux-c6-qt47>0:devel/linux-c6-qt47
 _linux_c7_qt=				linux-c7-qt>0:devel/linux-c7-qt
-_linux_c6_qt-x11=			linux-c6-qt47-x11>0:x11-toolkits/linux-c6-qt47-x11
 _linux_c7_qt-x11=			linux-c7-qt-x11>0:x11-toolkits/linux-c7-qt-x11
-_linux_c6_qtwebkit=			linux-c6-qt47-webkit>0:www/linux-c6-qt47-webkit
 _linux_c7_qtwebkit=			linux-c7-qtwebkit>0:www/linux-c7-qtwebkit
 _linux_${linux_ARGS}_sdl12=		linux-${linux_ARGS}-sdl>0:devel/linux-${linux_ARGS}-sdl12
 _linux_${linux_ARGS}_sdlimage=		linux-${linux_ARGS}-sdl_image>0:graphics/linux-${linux_ARGS}-sdl_image
@@ -146,7 +130,6 @@ _linux_${linux_ARGS}_tk85=		linux-${linux_ARGS}-tk85>0:x11-toolkits/linux-${linu
 _linux_c7_trousers=			linux-c7-trousers>0:security/linux-c7-trousers
 _linux_${linux_ARGS}_userspace-rcu=	linux-${linux_ARGS}-userspace-rcu>0:sysutils/linux-${linux_ARGS}-userspace-rcu
 _linux_c7_wayland=			linux-c7-wayland>0:graphics/linux-c7-wayland
-_linux_c6_xorglibs=			linux-c6-xorg-libs>0:x11/linux-c6-xorg-libs
 _linux_c7_xorglibs=			linux-c7-xorg-libs>=7.7_7:x11/linux-c7-xorg-libs
 
 USE_LINUX?=		base
@@ -168,20 +151,7 @@ DEV_ERROR+=		"USE_LINUX=${i}: package does not exist"
 
 .ifdef USE_LINUX_RPM
 
-.if ${linux_ARGS} == c6
-.ifndef MASTER_SITES
-MASTER_SITES=		${MASTER_SITE_CENTOS_LINUX}
-MASTER_SITE_SUBDIR=	centos/${LINUX_DIST_VER}/os/x86_64/Packages/:DEFAULT,amd64 \
-			centos/${LINUX_DIST_VER}/updates/x86_64/Packages/:DEFAULT,amd64 \
-			centos/${LINUX_DIST_VER}/os/i386/Packages/:DEFAULT,i386 \
-			centos/${LINUX_DIST_VER}/updates/i386/Packages/:DEFAULT,i386 \
-			centos/${LINUX_DIST_VER}/os/Source/SPackages/:SOURCE \
-			centos/${LINUX_DIST_VER}/updates/Source/SPackages/:SOURCE
-.endif
-DIST_SUBDIR?=		centos
-DEPRECATED=		Superseded by CentOS 7
-EXPIRATION_DATE=	2019-12-31
-.elif ${linux_ARGS} == c7
+.if ${linux_ARGS} == c7
 .ifndef MASTER_SITES
 MASTER_SITES=		${MASTER_SITE_CENTOS_LINUX}
 MASTER_SITE_SUBDIR=	centos/${LINUX_DIST_VER}/os/x86_64/Packages/:DEFAULT,amd64 \
