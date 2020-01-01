cb24bd26f82a89678bcbe6d52d72c67a43dd913d
Tobias C. Berner <tcberner@FreeBSD.org>
2020-01-01 20:52:54 +0000

    cad/lepton-eda: Update to 1.9.9
    PR:		241289
    Submitted by:	dmn <graahnul.grom@gmail.com> (maintainer)

--- cad/lepton-eda/files/patch-version.h
+++ cad/lepton-eda/files/patch-version.h
@@ -1,11 +1,11 @@
---- version.h.orig	2018-12-13 02:49:55 UTC
+--- version.h.orig	2019-10-04 15:45:44 UTC
 +++ version.h
 @@ -0,0 +1,8 @@
 +/* Date part of package version string. */
-+#define PACKAGE_DATE_VERSION "20181211"
++#define PACKAGE_DATE_VERSION "20191003"
 +
 +/* Dotted part of package version string. */
-+#define PACKAGE_DOTTED_VERSION "1.9.7"
++#define PACKAGE_DOTTED_VERSION "1.9.9"
 +
 +/* Current git commit. */
-+#define PACKAGE_GIT_COMMIT "f3a8714fdfd6dae491ebfff69f0fd9aa4899f9a6"
++#define PACKAGE_GIT_COMMIT "3139e7782564ff4bf76963984a735fbe638e43a0"
