6121857a625dbf03399701f8f1f18ed0dcafa860
joneum <joneum@FreeBSD.org>
2020-01-02 20:00:50 +0000

    databases/mysql56-{client, server}: Update to latest release 5.6.46
    Bugfix:
     -  Improper locking during storage engine initialization could cause a server exit.
     - A query with a WHERE clause whose predicate contained a numeric value in scientific notation was not handled correctly.
     - VS2019 produced compilation errors with debug compilation selected due to use of the /ZI flag. Now /Z7 is used instead.
     - For MySQL Community Edition, the cipher order specified by the client was used in preference to the order on the server side, unless the server was confi
     - Password masking was incomplete for SHOW PROCESSLIST and some INFORMATION_SCHEMA and Performance Schema tables.
     - The -DWITH_EXAMPLE_STORAGE_ENGINE=1 CMake option was ignored but should not have been. If -DWITH_EXAMPLE_STORAGE_ENGINE=0 is given, the EXAMPLE storage e
    
    More Info: https://dev.mysql.com/doc/relnotes/mysql/5.6/en/news-5-6-46.html
    
    Approved by:	mmokhi (maintainer, implicit)
    MFH:		2020Q1
    Security:	fc91f2ef-fd7b-11e9-a1c7-b499baebfeaf
    Sponsored by:	Netzkommune GmbH

--- databases/mysql56-client/files/patch-cmake_build_configurations_compiler_options.cmake
+++ databases/mysql56-client/files/patch-cmake_build_configurations_compiler_options.cmake
@@ -1,6 +1,6 @@
---- cmake/build_configurations/compiler_options.cmake.orig	2016-03-03 16:35:33.000000000 +0100
-+++ cmake/build_configurations/compiler_options.cmake	2016-04-19 17:30:13.345558666 +0200
-@@ -25,7 +25,7 @@ IF(UNIX)  
+--- cmake/build_configurations/compiler_options.cmake.orig	2020-01-01 21:24:49.736077000 +0100
++++ cmake/build_configurations/compiler_options.cmake	2020-01-01 21:25:22.307418000 +0100
+@@ -32,7 +32,7 @@ IF(UNIX)  
  
    # Default GCC flags
    IF(CMAKE_COMPILER_IS_GNUCC)
@@ -9,10 +9,10 @@
      # Disable inline optimizations for valgrind testing to avoid false positives
      IF(WITH_VALGRIND)
        SET(COMMON_C_FLAGS             "-fno-inline ${COMMON_C_FLAGS}")
-@@ -34,7 +34,7 @@ IF(UNIX)  
-     SET(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 ${COMMON_C_FLAGS}")
-   ENDIF()
-   IF(CMAKE_COMPILER_IS_GNUCXX)
+@@ -47,7 +47,7 @@ IF(UNIX)  
+     IF(GXX_VERSION VERSION_EQUAL 6.0 OR GXX_VERSION VERSION_GREATER 6.0)
+       SET(COMMON_CXX_FLAGS             "${COMMON_CXX_FLAGS} -std=gnu++03")
+     ENDIF()
 -    SET(COMMON_CXX_FLAGS               "-g -fabi-version=2 -fno-omit-frame-pointer -fno-strict-aliasing")
 +    SET(COMMON_CXX_FLAGS               "-g -fno-omit-frame-pointer -fno-strict-aliasing")
      # Disable inline optimizations for valgrind testing to avoid false positives
