e59a5a71a792b6d02312910eba560e795c7f8663
thierry <thierry@FreeBSD.org>
2020-01-02 21:50:40 +0000

    - Do not try to get VmSize on -CURRENT - Add a dependency on xterm + Some minor fixes.

--- french/aster/files/patch-products__data.py
+++ french/aster/files/patch-products__data.py
@@ -1,11 +1,7 @@
---- products_data.py.orig	2017-08-26 13:56:41 UTC
+--- products_data.py.orig	2019-10-01 12:45:34 UTC
 +++ products_data.py
-@@ -107,13 +107,11 @@ def configure(self):
-     ])
- 
-     self.env.append_value('INCLUDES', [
--        '%(HOME_HDF)s/include',
-+        '%(HOME_HDF)s/include/hdf5-18',
+@@ -110,10 +110,8 @@ def configure(self):
+         '%(HOME_HDF)s/include',
          '%(HOME_MED)s/include',
          '%(HOME_MUMPS)s/include',
 -        '%(HOME_MUMPS)s/include_seq',
