4b063220f445033b23e709fe47ca714adbefbd49
dmgk <dmgk@FreeBSD.org>
2020-01-04 12:17:07 +0000

    sysutils/ctop: Update to 0.7.3
    Changes:	https://github.com/bcicen/ctop/releases/tag/v0.7.3

--- sysutils/ctop/files/patch-vendor_github.com_docker_docker_pkg_system_mknod.go
+++ sysutils/ctop/files/patch-vendor_github.com_docker_docker_pkg_system_mknod.go
@@ -1,11 +1,11 @@
---- vendor/github.com/docker/docker/pkg/system/mknod.go.orig	2019-03-01 16:45:25 UTC
+--- vendor/github.com/docker/docker/pkg/system/mknod.go.orig	2020-01-04 11:57:57 UTC
 +++ vendor/github.com/docker/docker/pkg/system/mknod.go
 @@ -9,7 +9,7 @@ import (
  // Mknod creates a filesystem node (file, device special file or named pipe) named path
  // with attributes specified by mode and dev.
  func Mknod(path string, mode uint32, dev int) error {
--	return syscall.Mknod(path, mode, dev)
-+	return syscall.Mknod(path, mode, uint64(dev))
+-	return unix.Mknod(path, mode, dev)
++	return unix.Mknod(path, mode, uint64(dev))
  }
  
  // Mkdev is used to build the value of linux devices (in /dev/) which specifies major
