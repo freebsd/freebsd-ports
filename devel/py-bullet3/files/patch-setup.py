3b177b112081760e6dfe08d60752143816a81033
yuri <yuri@FreeBSD.org>
2020-01-04 10:11:31 +0000

    devel/py-bullet3: Update 2.88 -> 2.89
    Reported by:	portscout

--- devel/py-bullet3/files/patch-setup.py
+++ devel/py-bullet3/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2018-12-31 20:42:41 UTC
+--- setup.py.orig	2020-01-04 10:04:32 UTC
 +++ setup.py
-@@ -535,7 +535,7 @@ else:
-     CXX_FLAGS += '-D_BSD '
-     CXX_FLAGS += '-I/usr/X11R6/include '
-     CXX_FLAGS += '-DHAS_SOCKLEN_T '
--    CXX_FLAGS += '-fno-inline-functions-called-once'
-+    #CXX_FLAGS += '-fno-inline-functions-called-once'
-     sources = ["examples/ThirdPartyLibs/enet/unix.c"]\
-     +["examples/OpenGLWindow/X11OpenGLWindow.cpp"]\
-     +["examples/ThirdPartyLibs/glad/gl.c"]\
+@@ -434,7 +434,7 @@ else:
+   CXX_FLAGS += '-D_BSD '
+   CXX_FLAGS += '-I/usr/X11R6/include '
+   CXX_FLAGS += '-DHAS_SOCKLEN_T '
+-  CXX_FLAGS += '-fno-inline-functions-called-once'
++  #CXX_FLAGS += '-fno-inline-functions-called-once'
+   sources = ["examples/ThirdPartyLibs/enet/unix.c"]\
+   +["examples/OpenGLWindow/X11OpenGLWindow.cpp"]\
+   +["examples/ThirdPartyLibs/glad/gl.c"]\
