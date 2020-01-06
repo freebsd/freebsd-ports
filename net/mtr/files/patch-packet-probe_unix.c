f2f98719f9f04265c6136f17d44f366fc79eceb6
sunpoet <sunpoet@FreeBSD.org>
2020-01-06 14:45:39 +0000

    Fix "mtr-packet: Invalid argument" runtime error
    - Bump PORTREVISION for package change
    
    PR:		243121
    Reported by:	kib

--- net/mtr/files/patch-packet-probe_unix.c
+++ net/mtr/files/patch-packet-probe_unix.c
@@ -1,14 +1,14 @@
 --- packet/probe_unix.c.orig	2019-08-03 08:31:26 UTC
 +++ packet/probe_unix.c
-@@ -798,6 +798,7 @@ void receive_replies_from_recv_socket(
- #endif
+@@ -795,7 +795,6 @@ void receive_replies_from_recv_socket(
+                 memcpy(&remote_addr, SO_EE_OFFENDER(ee), sizeof(remote_addr));
+             }
+         }
+-#endif
  
  #ifdef SO_PROTOCOL
-+#ifdef HAVE_LINUX_ERRQUEUE_H
          if (icmp_connrefused_received) {
-             /* using ICMP type ICMP_ECHOREPLY is not a bug, it is an
-                indication of successfully reaching dst host.
-@@ -815,11 +816,14 @@ void receive_replies_from_recv_socket(
+@@ -815,11 +814,14 @@ void receive_replies_from_recv_socket(
                      packet, packet_length, &timestamp);
          } else {
  #endif
