911755000e0b9bf374725dac95e7cf74a93a4a03
Dima Panov <fluffy@FreeBSD.org>
2020-01-11 05:04:15 +0000

    net-mgmt/observium: unbreak and update to 19.8 community release
    Community edition release 19.8 from 2019-08-22
    
    PR:		243031
    Submitted by:	fluffy
    Approved by:	maintainer timeout (>1w)
    MFH:		2020Q1

--- net-mgmt/observium/files/patch-includes_defaults.inc.php
+++ net-mgmt/observium/files/patch-includes_defaults.inc.php
@@ -1,6 +1,6 @@
---- includes/defaults.inc.php.orig	2017-09-07 22:26:04 UTC
+--- includes/defaults.inc.php.orig	2019-08-23 07:09:46 UTC
 +++ includes/defaults.inc.php
-@@ -36,7 +36,7 @@ $config['db']['debug']      = TRUE;     
+@@ -57,7 +57,7 @@ $config['db']['debug']      = TRUE;        // If TRUE 
  
  // Default directories
  
@@ -9,7 +9,7 @@
  #$config['html_dir']      = $config['install_dir'] . "/html";
  #$config['rrd_dir']       = $config['install_dir'] . "/rrd";
  #$config['template_dir']  = $config['install_dir'] . "/templates";
-@@ -50,29 +50,29 @@ $config['db']['debug']      = TRUE;     
+@@ -71,30 +71,30 @@ $config['db']['debug']      = TRUE;        // If TRUE 
  
  // Location of executables
  
@@ -18,6 +18,7 @@
 -$config['fping6']         = "/usr/bin/fping6";
 -$config['snmpwalk']       = "/usr/bin/snmpwalk";
 -$config['snmpget']        = "/usr/bin/snmpget";
+-$config['snmpgetnext']    = "/usr/bin/snmpgetnext";
 -$config['snmpbulkget']    = "/usr/bin/snmpbulkget";
 -$config['snmpbulkwalk']   = "/usr/bin/snmpbulkwalk";
 -$config['snmptranslate']  = "/usr/bin/snmptranslate";
@@ -26,6 +27,7 @@
 +$config['fping6']         = "/usr/local/sbin/fping6";
 +$config['snmpwalk']       = "/usr/local/bin/snmpwalk";
 +$config['snmpget']        = "/usr/local/bin/snmpget";
++$config['snmpgetnext']    = "/usr/local/bin/snmpgetnext";
 +$config['snmpbulkget']    = "/usr/local/bin/snmpbulkget";
 +$config['snmpbulkwalk']   = "/usr/local/bin/snmpbulkwalk";
 +$config['snmptranslate']  = "/usr/local/bin/snmptranslate";
