8ca9d8a85ccaca4960c100ac3bd04d4b99103f2c
rene <rene@FreeBSD.org>
2020-01-11 11:29:03 +0000

    Remove expired port:
    2020-01-10 sysutils/docker-freebsd: Broken for more than 9 months

--- sysutils/docker-freebsd/files/patch-pkg_system_meminfo__unsupported.go
+++
@@ -1,9 +0,0 @@
---- pkg/system/meminfo_unsupported.go.orig	2015-06-08 13:34:30 UTC
-+++ pkg/system/meminfo_unsupported.go
-@@ -3,5 +3,5 @@
- package system
- 
- func ReadMemInfo() (*MemInfo, error) {
--	return nil, ErrNotSupportedPlatform
-+	return &MemInfo{}, ErrNotSupportedPlatform
- }
