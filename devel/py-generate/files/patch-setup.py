88f033168fd7bd8b110d89c66b367de186ea036b
Kubilay Kocak <koobs@FreeBSD.org>
2020-01-12 05:35:01 +0000

    MFH: r522753 devel/py-generate: Fix MASTER_SITES, Remove BROKEN [1]
    While I'm here, address other ports bugs/issues [2]:
    
      - gmake is not required (build uses distutils/setuptools:Extension)
      - patch setup.py:version (was 2.2) to match PORTVERSION (2.8)
      - Remove unecessary DISTNAME override
      - Strip installed extension/library
      - Correctly declare USES=python:<version-spec>, limited to 2.7. Fails to
        import on Python 3:
    
          ImportError: ... /site-packages/generate.so: Undefined symbol "PyString_FromString
    
    PR:		241966
    Submitted by:	D'Arcy J.M. Cain <darcy druid net> [1]
    Approved by:	portmgr (blanket(s): ports compliance, dependencies) [2]
    
    Approved by:	ports-secteam ((blanket(s): build (fetch) fix, ports compliance) [1][2]

---
+++ devel/py-generate/files/patch-setup.py
@@ -0,0 +1,14 @@
+# Correct version <> PORTVERSION
+# TODO: Upstream
+
+--- setup.py.orig	2020-01-12 03:43:46 UTC
++++ setup.py
+@@ -4,7 +4,7 @@
+ from distutils.core import setup, Extension
+ 
+ setup(name="generate",
+-	version="2.2",
++	version="2.8",
+ 	description="Text pre-processor",
+ 	author="D'Arcy J.M. Cain",
+ 	author_email="darcy@druid.net",
