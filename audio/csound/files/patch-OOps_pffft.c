d5640cca10d941e3ba548b9c8a4c2aa252ef2810
Yuri Victorovich <yuri@FreeBSD.org>
2020-01-12 07:54:29 +0000

    audio/csound: Update 6.12.2-83 -> 6.13.0-244
    Reviewed by:	portscout

--- audio/csound/files/patch-OOps_pffft.c
+++ audio/csound/files/patch-OOps_pffft.c
@@ -1,4 +1,4 @@
---- OOps/pffft.c.orig	2019-03-05 17:24:02 UTC
+--- OOps/pffft.c.orig	2019-07-12 21:54:19 UTC
 +++ OOps/pffft.c
 @@ -63,6 +63,9 @@
  #include <stdio.h>
@@ -10,23 +10,3 @@
  
  /* detect compiler flavour */
  #if defined(_MSC_VER)
-@@ -114,8 +117,8 @@ inline v4sf ld_ps1(const float *p) { v4sf v=vec_lde(0,
- #  define LD_PS1(p) ld_ps1(&p)
- #  define INTERLEAVE2(in1, in2, out1, out2) { v4sf tmp__ = vec_mergeh(in1, in2); out2 = vec_mergel(in1, in2); out1 = tmp__; }
- #  define UNINTERLEAVE2(in1, in2, out1, out2) {                           \
--    vector unsigned char vperm1 =  (vector unsigned char)(0,1,2,3,8,9,10,11,16,17,18,19,24,25,26,27); \
--    vector unsigned char vperm2 =  (vector unsigned char)(4,5,6,7,12,13,14,15,20,21,22,23,28,29,30,31); \
-+    vector unsigned char vperm1 =  (vector unsigned char){0,1,2,3,8,9,10,11,16,17,18,19,24,25,26,27}; \
-+    vector unsigned char vperm2 =  (vector unsigned char){4,5,6,7,12,13,14,15,20,21,22,23,28,29,30,31}; \
-     v4sf tmp__ = vec_perm(in1, in2, vperm1); out2 = vec_perm(in1, in2, vperm2); out1 = tmp__; \
-   }
- #  define VTRANSPOSE4(x0,x1,x2,x3) {              \
-@@ -128,7 +131,7 @@ inline v4sf ld_ps1(const float *p) { v4sf v=vec_lde(0,
-     x2 = vec_mergeh(y1, y3);                    \
-     x3 = vec_mergel(y1, y3);                    \
-   }
--#  define VSWAPHL(a,b) vec_perm(a,b, (vector unsigned char)(16,17,18,19,20,21,22,23,8,9,10,11,12,13,14,15))
-+#  define VSWAPHL(a,b) vec_perm(a,b, (vector unsigned char){16,17,18,19,20,21,22,23,8,9,10,11,12,13,14,15})
- #  define VALIGNED(ptr) ((((uintptr_t)(ptr)) & 0xF) == 0)
- 
- /*
--- audio/csound/files/patch-Top_csound.c
+++ audio/csound/files/patch-Top_csound.c
@@ -1,6 +1,6 @@
---- Top/csound.c.orig	2018-11-07 14:05:23 UTC
+--- Top/csound.c.orig	2020-01-11 09:18:32 UTC
 +++ Top/csound.c
-@@ -60,10 +60,14 @@
+@@ -61,10 +61,14 @@
  //#include "cs_par_dispatch.h"
  #include "find_opcode.h"
  
@@ -16,16 +16,7 @@
  #include "csound_standard_types.h"
  
  #include "csdebug.h"
-@@ -405,7 +409,7 @@ static const CSOUND cenviron_ = {
-     rewriteheader,
-     csoundLoadSoundFile,
-     fdrecord,
--    fdclose,
-+    fd__close,
-     csoundCreateFileHandle,
-     csoundGetFileName,
-     csoundFileClose,
-@@ -3755,7 +3759,7 @@ void csoundNotifyFileOpened(CSOUND* csou
+@@ -3773,7 +3777,7 @@ void csoundNotifyFileOpened(CSOUND* csound, const char
  /* ------------------------------------ */
  
  #if defined(HAVE_RDTSC)
@@ -34,7 +25,7 @@
  #undef HAVE_RDTSC
  #endif
  #endif
-@@ -3768,6 +3772,13 @@ static double timeResolutionSeconds = -1
+@@ -3786,6 +3790,13 @@ static double timeResolutionSeconds = -1.0;
  static int getTimeResolution(void)
  {
  #if defined(HAVE_RDTSC)
@@ -48,7 +39,7 @@
      FILE    *f;
      char    buf[256];
  
-@@ -3803,9 +3814,14 @@ static int getTimeResolution(void)
+@@ -3821,9 +3832,14 @@ static int getTimeResolution(void)
        }
      }
      fclose(f);
