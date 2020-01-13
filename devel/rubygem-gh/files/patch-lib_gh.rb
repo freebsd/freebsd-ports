334f5c08d3f97efa8b41bcbded7e916428c27507
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-01-13 17:49:34 +0000

    Clean up patch-gemspec and patch

--- devel/rubygem-gh/files/patch-lib_gh.rb
+++
@@ -1,8 +0,0 @@
---- lib/gh.rb.orig	2019-12-03 16:28:14 UTC
-+++ lib/gh.rb
-@@ -1,5 +1,4 @@
- require 'gh/version'
--require 'backports' if RUBY_VERSION < '2.1'
- require 'forwardable'
- 
- module GH
