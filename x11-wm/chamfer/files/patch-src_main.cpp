8824681a43bce2aab80ae9c907cadca8026812e7
jbeich <jbeich@FreeBSD.org>
2020-01-14 01:00:17 +0000

    x11-wm/chamfer: update to s20200112
    Changes:	https://github.com/jaelpark/chamferwm/compare/ed8b707...78168e0

---
+++ x11-wm/chamfer/files/patch-src_main.cpp
@@ -0,0 +1,11 @@
+--- src/main.cpp.orig	2020-01-11 22:22:06 UTC
++++ src/main.cpp
+@@ -159,7 +159,7 @@ class RunBackend : public Config::BackendConfig{ (publ
+ 		Config::ContainerInterface &containerInt = SetupContainer<T,U>(pcreateInfo);
+ 		containerInt.OnSetupContainer();
+ 
+-		Compositor::CompositorInterface *pcompInt = dynamic_cast<Compositor::CompositorInterface *>(pcomp);
++		Compositor::CompositorInterface *pcompInt = reinterpret_cast<Compositor::CompositorInterface *>(pcomp);
+ 
+ 		WManager::Container::Setup setup;
+ 		if(containerInt.floatingMode == Config::ContainerInterface::FLOAT_ALWAYS ||
