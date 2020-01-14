58df1fa043fcec69c63adf29c31c932095497dd3
Yuri Victorovich <yuri@FreeBSD.org>
2020-01-14 08:51:55 +0000

    audio/csoundqt: Update 0.9.5 -> 0.9.7-rc1-4
    Reported by:	portscout

--- audio/csoundqt/files/patch-src_qutecsound.cpp
+++ audio/csoundqt/files/patch-src_qutecsound.cpp
@@ -1,6 +1,6 @@
---- src/qutecsound.cpp.orig	2017-11-19 10:59:36 UTC
+--- src/qutecsound.cpp.orig	2020-01-12 07:45:31 UTC
 +++ src/qutecsound.cpp
-@@ -267,6 +267,9 @@ CsoundQt::CsoundQt(QStringList fileNames
+@@ -279,6 +279,9 @@ CsoundQt::CsoundQt(QStringList fileNames)
  #ifdef Q_OS_LINUX
          possibleDirectories  << "/usr/share/doc/csound-manual/html/" << "/usr/share/doc/csound-doc/html/";
  #endif
@@ -10,7 +10,7 @@
  #ifdef Q_OS_WIN
          QString programFilesPath = QDir::fromNativeSeparators(getenv("PROGRAMFILES"));
          QString programFilesPathx86 = QDir::fromNativeSeparators(getenv("PROGRAMFILES(X86)"));
-@@ -1255,6 +1258,8 @@ void CsoundQt::createApp()
+@@ -1299,6 +1302,8 @@ void CsoundQt::createApp()
  #ifdef USE_DOUBLE
  #ifdef Q_OS_LINUX
  #endif
@@ -19,7 +19,7 @@
  #ifdef Q_OS_SOLARIS
  #endif
  #ifdef Q_OS_WIN32
-@@ -1267,6 +1272,8 @@ void CsoundQt::createApp()
+@@ -1311,6 +1316,8 @@ void CsoundQt::createApp()
  
  #ifdef Q_OS_LINUX
  #endif
@@ -28,7 +28,7 @@
  #ifdef Q_OS_SOLARS
  #endif
  #ifdef Q_OS_WIN32
-@@ -1314,10 +1321,10 @@ void CsoundQt::createApp()
+@@ -1358,10 +1365,10 @@ void CsoundQt::createApp()
          wizard.setField("targetDir", appDir);
          if (m_options->sdkDir.isEmpty()) { // No sdk,
              wizard.setField("customPaths", true);
@@ -42,7 +42,7 @@
              }
  #endif
  #ifdef Q_OS_WIN32
-@@ -1810,6 +1817,9 @@ void CsoundQt::runInTerm(bool realtime)
+@@ -1899,6 +1906,9 @@ void CsoundQt::runInTerm(bool realtime)
  #ifdef Q_OS_LINUX
      options = "-e " + scriptFileName;
  #endif
@@ -52,7 +52,7 @@
  #ifdef Q_OS_SOLARIS
      options = "-e " + scriptFileName;
  #endif
-@@ -2797,6 +2807,9 @@ void CsoundQt::runUtility(QString flags)
+@@ -2896,6 +2906,9 @@ void CsoundQt::runUtility(QString flags)
  #ifdef Q_OS_LINUX
          options = "-e " + SCRIPT_NAME;
  #endif
@@ -62,20 +62,18 @@
  #ifdef Q_OS_SOLARIS
          options = "-e " + SCRIPT_NAME;
  #endif
-@@ -3911,10 +3924,10 @@ QString CsoundQt::getExamplePath(QString
+@@ -4024,8 +4037,8 @@ QString CsoundQt::getExamplePath(QString dir)
      examplePath = qApp->applicationDirPath() + "/../Resources/" + dir;
      qDebug() << examplePath;
  #endif
 -#ifdef Q_OS_LINUX
+-    examplePath = QString(); //qApp->applicationDirPath() + "/Examples/" + dir;
 +#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
-     examplePath = qApp->applicationDirPath() + "/Examples/" + dir;
-     if (!QDir(examplePath).exists()) {
--        examplePath = qApp->applicationDirPath() + "/../src/Examples/" + dir;
-+        examplePath = qApp->applicationDirPath() + "/../share/examples/CsoundQt/" + dir;
-     }
-     if (!QDir(examplePath).exists()) { // for out of tree builds
-         examplePath = qApp->applicationDirPath() + "/../../csoundqt/src/Examples/" + dir;
-@@ -4380,7 +4393,7 @@ void CsoundQt::fillFileMenu()
++    examplePath = qApp->applicationDirPath() + "/../share/examples/CsoundQt/" + dir;
+     QStringList possiblePaths;
+     possiblePaths << qApp->applicationDirPath() + "/Examples/" << "~/.local/share/csoundqt/Examples/"
+                   << "/usr/share/csoundqt/Examples/" << qApp->applicationDirPath() + "/../src/Examples/"
+@@ -4507,7 +4520,7 @@ void CsoundQt::fillFileMenu()
          templatePath = qApp->applicationDirPath() + "/../templates/";
          qDebug() << templatePath;
  #endif
@@ -84,13 +82,13 @@
          templatePath = qApp->applicationDirPath() + "/templates/";
          if (!QDir(templatePath).exists()) {
              templatePath = qApp->applicationDirPath() + "/../templates/";
-@@ -5058,6 +5071,9 @@ int CsoundQt::execute(QString executable
- #ifdef Q_OS_LINUX
-     QString commandLine = "\"" + executable + "\" " + options;
+@@ -5196,6 +5209,9 @@ int CsoundQt::execute(QString executable, QString opti
+     QString commandLine = "open -a \"" + executable + "\" " + options;
  #endif
-+#ifdef Q_OS_FREEBSD
+ #ifdef Q_OS_LINUX
 +    QString commandLine = "\"" + executable + "\" " + options;
 +#endif
- #ifdef Q_OS_HAIKU
++#ifdef Q_OS_FREEBSD
      QString commandLine = "\"" + executable + "\" " + options;
  #endif
+ #ifdef Q_OS_HAIKU
