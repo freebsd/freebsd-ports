5c0f583393e53a6817e69d070b1bd88c7f93a3f1
Antoine Brodin <antoine@FreeBSD.org>
2020-01-14 09:27:18 +0000

    Switch version of python used in USES=waf to default version of python
    Reviewed by:	bapt
    With hat:	portmgr
    Differential Revision:	https://reviews.freebsd.org/D23143

--- Mk/Uses/waf.mk
+++ Mk/Uses/waf.mk
@@ -22,7 +22,7 @@ IGNORE=	Incorrect 'USES+= waf:${waf_ARGS}' waf takes no arguments
 .endif
 
 .if !${USES:Mpython*}
-python_ARGS=	2.7,build
+python_ARGS=	build
 .include "${USESDIR}/python.mk"
 .endif
 
