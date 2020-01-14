78c1e9617a00eadd6ec1d2ccce3011ba05d983be
jbeich <jbeich@FreeBSD.org>
2020-01-14 01:03:05 +0000

    x11-wm/chamfer: add missing patch comment

--- x11-wm/chamfer/files/patch-src_main.cpp
+++ x11-wm/chamfer/files/patch-src_main.cpp
@@ -1,3 +1,10 @@
+../src/main.cpp:162:47: error: 'RunCompositor' is an incomplete type
+                Compositor::CompositorInterface *pcompInt = dynamic_cast<Compositor::CompositorInterface *>(pcomp);
+                                                            ^                                               ~~~~~
+../src/main.cpp:109:27: note: forward declaration of 'RunCompositor'
+        void SetCompositor(class RunCompositor *pcomp){
+                                 ^
+
 --- src/main.cpp.orig	2020-01-11 22:22:06 UTC
 +++ src/main.cpp
 @@ -159,7 +159,7 @@ class RunBackend : public Config::BackendConfig{ (publ
