2a0367fb9fcc0149e64c54c7f9a5e4a08bd9699a
Jochen Neumeister <joneum@FreeBSD.org>
2020-01-15 21:02:12 +0000

    MFH: r523155
    databases/mysql80-{client, server}: Update to latest release 8.0.19
    
    - New FPROFILE_GENERATE and FPROFILE_USE CMake options are available for experimenting with profile guided optimization (PGO) with GCC. See the cmake/fprofile.cmake in a MySQL source distribution for information about using them. These options have been tested with GCC 8 and 9, and with Clang.
    - Enabling FPROFILE_USE also enables WITH_LTO (link time optimization).
    - Innodb_system_rows_read, Innodb_system_rows_inserted, Innodb_system_rows_deleted status variables were added for counting row operations on InnoDB tables that belong to system-created schemas. The new status variables are similar to the existing Innodb_rows_read, Innodb_rows_inserted, Innodb_rows_deleted status variables, which count operations on InnoDB tables that belong to both user-created and system-created schemas.
    - The new status variables are useful in replication environments where relay_log_info_repository and master_info_repository variables are set to TABLE, resulting in higher row operation counts on slaves due to operations performed on the slave_master_info, slave_replay_log_info, and slave_worker_info tables, which belong to the system-created mysql schema. For a valid comparison of master and slave row operation counts, operations on tables in system-created schemas can now be excluded using the count data provided by the new status variables.
    
    More infos: https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-19.html
    
    Submitted by:	fluffy
    Approved by:	mmokhi (maintainer, implicit)
    Sponsored by:	Netzkommune GmbH
    Security: 	a6cf65ad-37d2-11ea-a1c7-b499baebfeaf
    
    Approved by:	ports-secteam (with hat)

--- databases/mysql80-server/files/patch-vio_viossl.cc
+++ databases/mysql80-server/files/patch-vio_viossl.cc
@@ -1,5 +1,15 @@
 --- vio/viossl.cc.orig	2019-09-20 08:30:51 UTC
 +++ vio/viossl.cc
+@@ -45,7 +45,8 @@
+   BIO_set_callback_ex was added in openSSL 1.1.1
+   For older openSSL, use the deprecated BIO_set_callback.
+ */
+-#if OPENSSL_VERSION_NUMBER >= 0x10101000L
++#if OPENSSL_VERSION_NUMBER >= 0x10101000L && \
++    !defined(LIBRESSL_VERSION_NUMBER)
+ #define HAVE_BIO_SET_CALLBACK_EX
+ #endif
+ 
 @@ -490,7 +490,7 @@ static int ssl_do(struct st_VioSSLFd *ptr, Vio *vio, l
  #if !defined(DBUG_OFF)
      {
