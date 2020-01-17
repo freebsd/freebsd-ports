d3d28252a0bc26625f8497d181b343829a918ba3
cmt <cmt@FreeBSD.org>
2020-01-17 22:16:25 +0000

    update the Coin3D stack to it's latest release
    Coin has moved to a new place on the web https://coin3d.github.io/
    and the project has rolled new releases. Additionally, it's undergoing
    relicensing (most parts are now licensed user BSD 3-clause).
    
    This updates
      graphics/Coin to 4.0
      graphics/py-pivy to 0.6.5
      x11-toolkits/soqt to 1.6.0
    
    Further, graphics/py-pivy-devel and x11-toolkits/soqt-devel have been
    renamed to graphics/py-pivy and x11-toolkits/soqt respectively (losing
    their -devel suffixes). Previously, these ports had been forks and/or
    pre-releases of the old Qt4-based pivy and soqt ports. As these old ports
    are long gone and these new versions have been updated to officially
    released versions, it seemed prudent to remove the devel from their names.
    
    Bumping FreeCAD for dependency change.
    Adding PORTEPOCH to soqt to prevent PKGVERSION going backwards.
    
    Thanks to pfg@, who contributed the update to graphics/Coin and reviewed
    my patches.
    
    While here, I'm taking over maintainership of these ports.
    
    Submitted by:	pfg@ (graphics/Coin)
    Reviewed by:	pfg@

--- graphics/py-pivy/files/patch-setup.py
+++ graphics/py-pivy/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2018-06-08 08:15:18 UTC
+--- setup.py.orig	2020-01-12 21:51:59 UTC
 +++ setup.py
-@@ -203,7 +203,7 @@ class pivy_build(build):
+@@ -201,7 +201,7 @@ class pivy_build(build):
          print(yellow('calling: ' + cmake_command[0] + ' ' + cmake_command[1]))
-         cmake = subprocess.Popen(cmake_command, stdout=subprocess.PIPE)
-         cmake_out, _ = cmake.communicate()
+         cmake = subprocess.Popen(cmake_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+         cmake_out, cmake_err = cmake.communicate()
 -        coin_vars = ['COIN_FOUND', 'COIN_VERSION', 'COIN_INCLUDE_DIR', 'COIN_LIB_DIR']
 +        coin_vars = ['COIN_FOUND', 'COIN_INCLUDE_DIR', 'COIN_LIB_DIR']
          soqt_vars = ['SOQT_FOUND', 'SOQT_VERSION', 'SOQT_INCLUDE_DIR', 'SOQT_LIB_DIR']
          config_dict = {}
-         if cmake.returncode == 0:
+         print(yellow(cmake_out.decode("utf-8")))
