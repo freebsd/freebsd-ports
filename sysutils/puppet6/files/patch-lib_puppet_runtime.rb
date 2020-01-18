c39c8be09caddcb7e4d485fdbf4dfd581fae581b
romain <romain@FreeBSD.org>
2020-01-18 02:56:49 +0000

    Update to 6.12.0
    Release notes:
    https://puppet.com/docs/puppet/latest/release_notes_puppet.html#puppet-release-notes-x.12.0
    
    With hat:	puppet

--- sysutils/puppet6/files/patch-lib_puppet_runtime.rb
+++
@@ -1,8 +0,0 @@
---- lib/puppet/runtime.rb.orig	2019-11-26 01:40:01 UTC
-+++ lib/puppet/runtime.rb
-@@ -1,4 +1,5 @@
- require 'puppet/http'
-+require 'singleton'
- 
- class Puppet::Runtime
-   include Singleton
