bf52a01924239c5c24904436346ce073d98b3f11
kbowling <kbowling@FreeBSD.org>
2020-01-19 20:45:42 +0000

    net/nsscache: Update to 0.39
    The big change here is a move to Python 3 upstream and in the port.
    
    Approved by:	krion (mentor)
    Sponsored by:	BBOX.io
    Differential Revision:	https://reviews.freebsd.org/D23269

--- net/nsscache/files/patch-setup.py
+++ net/nsscache/files/patch-setup.py
@@ -1,9 +1,10 @@
---- setup.py.orig	2014-10-21 10:32:31 UTC
+--- setup.py.orig	2019-11-11 16:27:27 UTC
 +++ setup.py
-@@ -48,5 +48,4 @@ as LDAP.''',
-                                'nss_cache.util',
-                                'nss_cache.update',
-                                'nss_cache.sources'],
--                     scripts=['nsscache'],
--                     data_files=[('/etc', ['nsscache.conf'])])
-+                     scripts=['nsscache'])
+@@ -46,7 +46,6 @@ as LDAP.""",
+         'nss_cache.update', 'nss_cache.sources'
+     ],
+     scripts=['nsscache'],
+-    data_files=[('/etc', ['nsscache.conf'])],
+     python_requires='~=3.4',
+     setup_requires=['pytest-runner'],
+     tests_require=['pytest', 'mox3', 'pytest-cov', 'python-coveralls'],
