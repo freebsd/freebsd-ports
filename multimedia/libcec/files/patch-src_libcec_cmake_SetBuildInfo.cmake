5c3e9ddf20a760abdd5d1fec95b8c6e43b123370
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-01-20 20:09:45 +0000

    Fix hardcoded version in src/libcec/cmake/SetBuildInfo.cmake patch
    - Use USES=pathfix
    - Convert to options helper
    - Sort PLIST
    - Take maintainership

--- multimedia/libcec/files/patch-src_libcec_cmake_SetBuildInfo.cmake
+++ multimedia/libcec/files/patch-src_libcec_cmake_SetBuildInfo.cmake
@@ -1,18 +1,13 @@
---- src/libcec/cmake/SetBuildInfo.cmake.orig	2017-01-28 22:51:30 UTC
+--- src/libcec/cmake/SetBuildInfo.cmake.orig	2018-12-21 21:34:45 UTC
 +++ src/libcec/cmake/SetBuildInfo.cmake
-@@ -14,14 +14,7 @@ else()
-   set(LIB_INFO "")
+@@ -15,10 +15,6 @@ else()
  
    # add git revision to compile info
--  find_program(HAVE_GIT_BIN git /bin /usr/bin /usr/local/bin)
+   find_program(HAVE_GIT_BIN git /bin /usr/bin /usr/local/bin)
 -  if(HAVE_GIT_BIN)
 -    exec_program(${CMAKE_CURRENT_SOURCE_DIR}/cmake/git-rev.sh HEAD OUTPUT_VARIABLE GIT_REVISION)
 -    message(STATUS "git found: ${GIT_REVISION}")
 -  endif()
--  if (GIT_REVISION)
--    set(LIB_INFO "git revision: ${GIT_REVISION},")
--  endif()
-+  set(LIB_INFO "git revision: 4.0.4,")
- 
-   # add compilation date to compile info
-   find_program(HAVE_DATE_BIN date /bin /usr/bin /usr/local/bin)
+   if (GIT_REVISION)
+     set(LIB_INFO "git revision: ${GIT_REVISION},")
+   endif()
