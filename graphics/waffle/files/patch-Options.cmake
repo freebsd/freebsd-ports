124c46441711269cffbd044ff644441c749ba516
Tobias Kortkamp <tobik@FreeBSD.org>
2020-01-23 12:42:55 +0000

    graphics/waffle: Update to 1.6.0-7
    - Waffle moved to freedesktop gitlab
    - Now supports building with Meson, which allows us to remove all
      the CMake patches from the port.  The Meson build however still
      expects that cmake is available in the build environment.
    - New version generates CMake config with 'Waffle' named variables
      (which devel/apitrace expects) instead of 'WAFFLE'
    - Extra xdg-shell patch to make devel/apitrace 'eglretrace' work
      on modern Wayland compositors (wlroots has removed the old wl-shell
      protocol, so this is required to make apitrace's debug trace
      replays work on sway/wayfire/...)
    - All backends are now options (on by default)
    - Manpages are now built
    - graphics/piglit needs a small patch to build
    
    PR:		242892, 242893
    Submitted by:	Greg V <greg@unrelenting.technology> (based on)
    Approved by:	x11 (maintainer timeout, 27 days)

--- graphics/waffle/files/patch-Options.cmake
+++
@@ -1,8 +0,0 @@
---- Options.cmake.orig	2015-01-22 20:00:17 UTC
-+++ Options.cmake
-@@ -1,4 +1,4 @@
--if(waffle_on_linux)
-+if(waffle_on_linux OR waffle_on_freebsd)
-     if(gl_FOUND AND x11-xcb_FOUND)
-         set(glx_default ON)
-     else()
