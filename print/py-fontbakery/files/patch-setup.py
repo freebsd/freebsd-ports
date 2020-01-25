234aeb07879833219c71ed02a8c31b5eac92794a
sunpoet <sunpoet@FreeBSD.org>
2020-01-25 18:18:00 +0000

    Add py-fontbakery 0.7.17
    Font Bakery is a command-line tool for checking the quality of font projects.
    
    WWW: https://github.com/googlefonts/fontbakery

---
+++ print/py-fontbakery/files/patch-setup.py
@@ -0,0 +1,20 @@
+--- setup.py.orig	2019-12-21 04:47:47 UTC
++++ setup.py
+@@ -23,7 +23,7 @@ except IOError:
+ 
+ setup(
+     name="fontbakery",
+-    use_scm_version={"write_to": "Lib/fontbakery/_version.py"},
++    use_scm_version={"fallback_version": "%%PORTVERSION%%", "write_to": "Lib/fontbakery/_version.py"},
+     url='https://github.com/googlefonts/fontbakery/',
+     description='Well designed Font QA tool, written in Python 3',
+     long_description=readme,
+@@ -72,7 +72,7 @@ setup(
+         'ufolint',
+ 
+     ],
+-    extras_requires={
++    extras_require={
+         'docs': [
+             'sphinx >= 1.4',
+             'sphinx_rtd_theme',
