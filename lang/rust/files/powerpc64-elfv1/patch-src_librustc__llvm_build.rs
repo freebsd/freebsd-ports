3679ebcd64f88cb8880e6bf10190c109fbc8e0c0
Tobias Kortkamp <tobik@FreeBSD.org>
2020-01-25 05:41:19 +0000

    lang/rust: Apply some of the patches for lang/rust-bootstrap (D23133)
    They should not affect the regular lang/rust build.

--- lang/rust/files/powerpc64-elfv1/patch-src_librustc__llvm_build.rs
+++ lang/rust/files/powerpc64-elfv1/patch-src_librustc__llvm_build.rs
@@ -1,12 +1,16 @@
---- src/librustc_llvm/build.rs.orig	2019-11-04 15:45:21 UTC
+--- src/librustc_llvm/build.rs.orig	2019-12-16 15:38:05 UTC
 +++ src/librustc_llvm/build.rs
-@@ -273,7 +273,10 @@ fn main() {
+@@ -273,7 +273,14 @@ fn main() {
      };
  
      // C++ runtime library
 -    if !target.contains("msvc") {
 +    if target == "powerpc64-unknown-freebsd" {
-+        println!("cargo:rustc-link-search=native=/usr/local/lib/%CC%");
++        if is_crossed {
++            println!("cargo:rustc-link-search=native=%WRKDIR%/usr/local/lib/%CC%");
++        } else {
++            println!("cargo:rustc-link-search=native=/usr/local/lib/%CC%");
++        }
 +        println!("cargo:rustc-link-lib=static=stdc++");
 +    } else if !target.contains("msvc") {
          if let Some(s) = llvm_static_stdcpp {
