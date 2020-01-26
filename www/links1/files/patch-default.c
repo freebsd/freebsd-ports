c246a8c74d773679fc12c08e1bc47075bd558496
pkubaj <pkubaj@FreeBSD.org>
2020-01-26 15:40:37 +0000

    www/links1: update to 1.04, move USES block

--- www/links1/files/patch-default.c
+++ www/links1/files/patch-default.c
@@ -1,7 +1,7 @@
---- default.c.orig	2011-11-23 00:54:28 UTC
+--- default.c.orig	2018-11-27 22:19:36 UTC
 +++ default.c
-@@ -361,7 +361,7 @@ void init_home()
- 	get_system_name();
+@@ -785,7 +785,7 @@ void init_home()
+ 	get_compiler_name();
  	links_home = get_home(&first_use);
  	if (!links_home) {
 -		fprintf(stderr, "Unable to find or create links config directory. Please check, that you have $HOME variable set correctly and that you have write permission to your home directory.\n\007");
