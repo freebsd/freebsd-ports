94016cb7be5a879094ab5887076bf17495a85c75
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-01-26 16:15:02 +0000

    Update to 1.1.0
    Changes:	https://github.com/certbot/certbot/releases

--- security/py-certbot/files/patch-setup.py
+++ security/py-certbot/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2019-05-07 19:17:33 UTC
+--- setup.py.orig	2020-01-14 18:41:32 UTC
 +++ setup.py
-@@ -43,7 +43,6 @@ install_requires = [
+@@ -47,7 +47,6 @@ install_requires = [
      # 1.1.0+ is required to avoid the warnings described at
      # https://github.com/certbot/josepy/issues/13.
      'josepy>=1.1.0',
@@ -8,8 +8,8 @@
      'parsedatetime>=1.3',  # Calendar.parseDT
      'pyrfc3339',
      'pytz',
-@@ -56,6 +55,7 @@ dev_extras = [
-     'astroid==1.6.5',
+@@ -76,6 +75,7 @@ elif os.name == 'nt':
+ dev_extras = [
      'coverage',
      'ipdb',
 +    'mock',
