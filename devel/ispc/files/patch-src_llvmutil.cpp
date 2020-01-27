9d27312278aa0e10a2dff2b3e5ad3cfa7be2b8ef
Yuri Victorovich <yuri@FreeBSD.org>
2020-01-27 02:49:13 +0000

    devel/ispc: Update 1.11.0 -> 1.12.0-116-g98cd1326

--- devel/ispc/files/patch-src_llvmutil.cpp
+++ devel/ispc/files/patch-src_llvmutil.cpp
@@ -1,9 +1,8 @@
 ISPC devs insist on using dump() functions which LLVM devs consider a debug functionality, and it isn't available in the LLVM packages.
 
---- src/llvmutil.cpp.orig	2019-03-02 06:20:14 UTC
---- src/llvmutil.cpp.orig	2019-04-20 16:08:37 UTC
+--- src/llvmutil.cpp.orig	2020-01-25 02:15:35 UTC
 +++ src/llvmutil.cpp
-@@ -1349,7 +1349,7 @@ static void lDumpValue(llvm::Value *v, s
+@@ -1344,7 +1344,7 @@ static void lDumpValue(llvm::Value *v, std::set<llvm::
          return;
  
      fprintf(stderr, "  ");
