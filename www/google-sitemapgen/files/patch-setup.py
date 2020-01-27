e397bb0e19920719e5cfaf3198707e61143c9ffa
Rene Ladan <rene@FreeBSD.org>
2020-01-27 17:18:16 +0000

    Remove expired ports (part 2):
    2020-01-27 security/kedpm: Unmaintained, uses EOLed python27
    2020-01-27 security/parano: Unmaintained, uses EOLed python27
    2020-01-27 security/py-cryptkit: Unmaintained, uses EOLed python27
    2020-01-27 security/py-fchksum: Unmaintained, uses EOLed python27
    2020-01-27 security/py-mhash: Unmaintained, uses EOLed python27
    2020-01-27 security/py-pow: Unmaintained, uses EOLed python27
    2020-01-27 security/py-pycryptopp: Unmaintained, uses EOLed python27
    2020-01-27 security/py-trustedpickle: Unmaintained, uses EOLed python27
    2020-01-27 security/py-twofish: Unmaintained, uses EOLed python27
    2020-01-27 security/revelation: Unmaintained, uses EOLed python27
    2020-01-27 security/spike-proxy: Unmaintained, uses EOLed python27
    2020-01-27 security/umit: Unmaintained, uses EOLed python27
    2020-01-27 sysutils/feather: Unmaintained, uses EOLed python27
    2020-01-27 sysutils/py-bcfg2: Unmaintained, uses EOLed python27
    2020-01-27 sysutils/rdiff-backup-devel: Unmaintained, uses EOLed python27
    2020-01-27 sysutils/tentakel: Unmaintained, uses EOLed python27
    2020-01-27 textproc/archmage: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-asv: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-dsv: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-excelerator: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-jaxml: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-libtre: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-pyctpp2: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-rstyoutube: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-rxp: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-syck: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-tvgrab: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py-xlwriter: Unmaintained, uses EOLed python27
    2020-01-27 textproc/py2html: Unmaintained, uses EOLed python27
    2020-01-27 textproc/queequeg: Unmaintained, uses EOLed python27
    2020-01-27 textproc/rubber: Unmaintained, uses EOLed python27
    2020-01-27 textproc/silvercity: Unmaintained, uses EOLed python27
    2020-01-27 textproc/xsv: Unmaintained, uses EOLed python27
    2020-01-27 textproc/yould: Unmaintained, uses EOLed python27
    2020-01-27 www/cherokee: Unmaintained, uses EOLed python27
    2020-01-27 www/cppcms: Unmaintained, uses EOLed python27
    2020-01-27 www/gaeutilities: Unmaintained, uses EOLed python27
    2020-01-27 www/google-sitemapgen: Unmaintained, uses EOLed python27
    2020-01-27 www/jtoolkit: Unmaintained, uses EOLed python27
    2020-01-27 www/linkchecker: Unmaintained, uses EOLed python27
    2020-01-27 www/ljdump: Unmaintained, uses EOLed python27
    2020-01-27 www/monast: Unmaintained, uses EOLed python27
    2020-01-27 www/py-django-paging: Unmaintained, uses EOLed python27
    2020-01-27 www/py-fcgi: Unmaintained, uses EOLed python27
    2020-01-27 www/py-http-parser: Unmaintained, uses EOLed python27
    2020-01-27 www/py-meld: Unmaintained, uses EOLed python27
    2020-01-27 www/py-mt: Unmaintained, uses EOLed python27
    2020-01-27 www/py-rhodecode: Unmaintained, uses EOLed python27
    2020-01-27 www/qooxdoo: Unmaintained, uses EOLed python27
    2020-01-27 www/varnish-libvmod-maxminddb: Unmaintained, uses EOLed python27
    2020-01-27 x11/metisse: Unmaintained, uses EOLed python27
    2020-01-27 x11/ooxcb: Unmaintained, uses EOLed python27
    2020-01-27 x11-toolkits/diorite: Unmaintained, uses EOLed python27
    2020-01-27 x11-toolkits/nucleo: Unmaintained, only used by deprecated x11/metisse
    2020-01-27 x11-toolkits/py-gui: Unmaintained, uses EOLed python27
    2020-01-27 x11-wm/ede: Unmaintained, uses EOLed python27
    2020-01-27 x11-wm/obapps: Unmaintained, uses EOLed python27
    2020-01-27 x11-wm/obkey: Unmaintained, uses EOLed python27
    2020-01-27 x11-wm/obmenu: Unmaintained, uses EOLed python27
    2020-01-27 x11-wm/plwm: Unmaintained, uses EOLed python27

--- www/google-sitemapgen/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
---- setup.py.orig	2009-05-27 17:43:25.000000000 +1000
-+++ setup.py	2009-05-27 17:45:52.000000000 +1000
-@@ -9,4 +9,5 @@
-       author='Google Inc.',
-       author_email='opensource@google.com',
-       url='http://code.google.com/p/sitemap-generators/',
-+      py_modules = [ 'sitemap_gen' ]
-      )
