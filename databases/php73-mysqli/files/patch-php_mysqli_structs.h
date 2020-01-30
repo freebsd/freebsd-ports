1e2feeb91003ab56045131687bea4833b110030c
fluffy <fluffy@FreeBSD.org>
2020-01-30 09:44:31 +0000

    databases/php7[234]-mysqli: unbreak build with system mysql when MYSQLND is disabled
    This is follow-up to r522540 which introduced ability to build with MYSQLND=off
    
    PR:		243643
    Submitted by:	fluffy
    Approved by:	maintainer (tz)

--- databases/php73-mysqli/files/patch-php_mysqli_structs.h
+++ databases/php73-mysqli/files/patch-php_mysqli_structs.h
@@ -1,5 +1,14 @@
 --- php_mysqli_structs.h.orig	2010-07-26 15:52:54.000000000 +0200
 +++ php_mysqli_structs.h	2010-07-26 15:53:14.000000000 +0200
+@@ -36,7 +36,7 @@
+ #define FALSE 0
+ #endif
+ 
+-#ifdef MYSQLI_USE_MYSQLND
++#if defined(MYSQLI_USE_MYSQLND)
+ #include "ext/mysqlnd/mysqlnd.h"
+ #include "mysqli_mysqlnd.h"
+ #else
 @@ -54,6 +54,7 @@
  #define WE_HAD_MBSTATE_T
  #endif
