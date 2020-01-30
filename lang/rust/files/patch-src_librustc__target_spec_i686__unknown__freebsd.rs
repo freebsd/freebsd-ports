83209114b829704b4d3c22ef757562bf4dccb87f
Tobias Kortkamp <tobik@FreeBSD.org>
2020-01-30 20:00:06 +0000

    lang/rust: Update to 1.41.0
    - Force rebuild all consumers to catch regressions early
    - Switch to cross-compiled (from amd64) bootstraps for all
      architectures generated with the incoming lang/rust-bootstrap
    - Update cargo-c to 0.5.2 to unbreak librav1e build
    - Make use of regular MAKE_ENV/TEST_ENV in lang/rust
    - Turn on RUST_BACKTRACE in lang/rust and USES=cargo to hopefully
      produce more useful failure logs when something panics during
      builds
    
    Changes:	https://blog.rust-lang.org/2020/01/30/Rust-1.41.0.html
    Tested by:	mikael, tobik
    With hat:	rust
    Differential Revision:	https://reviews.freebsd.org/D23385

--- lang/rust/files/patch-src_librustc__target_spec_i686__unknown__freebsd.rs
+++ lang/rust/files/patch-src_librustc__target_spec_i686__unknown__freebsd.rs
@@ -1,11 +1,16 @@
---- src/librustc_target/spec/i686_unknown_freebsd.rs.orig	2019-04-08 12:42:31 UTC
+--- src/librustc_target/spec/i686_unknown_freebsd.rs.orig	2019-12-16 15:38:05 UTC
 +++ src/librustc_target/spec/i686_unknown_freebsd.rs
-@@ -2,7 +2,7 @@ use crate::spec::{LinkerFlavor, Target, TargetResult};
+@@ -2,9 +2,11 @@ use crate::spec::{LinkerFlavor, Target, TargetResult};
  
  pub fn target() -> TargetResult {
      let mut base = super::freebsd_base::opts();
 -    base.cpu = "pentium4".to_string();
 +    base.cpu = "pentiumpro".to_string();
      base.max_atomic_width = Some(64);
-     base.pre_link_args.get_mut(&LinkerFlavor::Gcc).unwrap().push("-m32".to_string());
+-    base.pre_link_args.get_mut(&LinkerFlavor::Gcc).unwrap().push("-m32".to_string());
++    let pre_link_args = base.pre_link_args.get_mut(&LinkerFlavor::Gcc).unwrap();
++    pre_link_args.push("-m32".to_string());
++    pre_link_args.push("-Wl,-znotext".to_string());
      base.stack_probes = true;
+ 
+     Ok(Target {
