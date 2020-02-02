80db07ac7286d4d579b53336509e69eb5f2fb312
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-02-02 11:00:02 +0000

    Update to 0.5
    - Add LICENSE
    - Remove DOCS option:
      - README.md is the same as pkg-descr
      - Changelog is almost 10-years old
    - Update pkg-descr
    - Update WWW
    
    Changes:	https://github.com/candlepin/python-iniparse/commits/master

--- devel/py-iniparse/files/patch-setup.py
+++ devel/py-iniparse/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig
+--- setup.py.orig	2020-01-29 13:22:11 UTC
 +++ setup.py
-@@ -30,12 +30,5 @@
-         'Topic :: Software Development :: Libraries :: Python Modules',
+@@ -34,12 +34,5 @@ use.''',
        ],
        packages = ['iniparse'],
+       install_requires=['six'],
 -      data_files = [
--        ('share/doc/iniparse-%s' % VERSION, ['README', 'LICENSE-PSF',
+-        ('share/doc/iniparse-%s' % VERSION, ['README.md', 'LICENSE-PSF',
 -                                             'LICENSE', 'Changelog',
 -                                             'html/index.html',
 -                                             'html/style.css',
