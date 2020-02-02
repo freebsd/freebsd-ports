15f8149045e8a99b94c68bdeb7ca7c071df0e381
Bernard Spil <brnrd@FreeBSD.org>
2020-02-02 19:54:15 +0000

    Mk/bsd.default-versions.mk
     - Remove MariaDB 10.0 (removed from ports 2019-04-19)
     - Add MariaDB 10.4 (added to ports 2019-06-30)
    
    PR:		240254
    Reported by:	Dries Michiels <driesm michiels gmail com>
    Approved by:	portmgr (rene)
    Differential Revision:	https://reviews.freebsd.org/D21511

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -63,7 +63,7 @@ LINUX_DEFAULT?=		c7
 LLVM_DEFAULT?=		90
 # Possible values: 5.1, 5.2, 5.3
 LUA_DEFAULT?=		5.2
-# Possible values: 5.5, 5.6, 5.7, 8.0, 5.5m, 10.0m, 10.1m, 10.2m, 10.3m, 5.5p, 5.6p, 5.7p, 5.6w
+# Possible values: 5.5, 5.6, 5.7, 8.0, 5.5m, 10.1m, 10.2m, 10.3m, 10.4m, 5.5p, 5.6p, 5.7p, 5.6w
 MYSQL_DEFAULT?=		5.7
 # Possible values: 5.26, 5.28, 5.30, devel
 .if !exists(${LOCALBASE}/bin/perl) || (!defined(_PORTS_ENV_CHECK) && \
