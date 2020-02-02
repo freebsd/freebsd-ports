4aa19fdc20b1441d26d7b80931823f96d7cdffcb
swills <swills@FreeBSD.org>
2020-02-02 05:37:22 +0000

    devel/premake5: Build script creation tool
    Describe your software project just once, using Premake's
    simple and easy to read syntax, and build it everywhere.
    
    WWW: https://premake.github.io/
    
    This is version 5 alpha
    
    PR:		241365
    Submitted by:	Martin Filla <freebsd@sysctl.cz>

---
+++ devel/premake5/files/patch-tests___tests.lua
@@ -0,0 +1,12 @@
+--- tests/_tests.lua.orig	2019-11-16 06:02:45 UTC
++++ tests/_tests.lua
+@@ -57,9 +57,4 @@ return {
+ 	-- Control system tests
+ 	"test_premake.lua",
+ 	"base/test_validation.lua",
+-
+-	-- -- Toolset tests
+-	"tools/test_dotnet.lua",
+-	"tools/test_gcc.lua",
+-	"tools/test_msc.lua",
+ }
