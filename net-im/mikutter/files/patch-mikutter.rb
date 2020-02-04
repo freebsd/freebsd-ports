6a8b4e9223c6000a3a164a051a6991b551f87452
Koichiro Iwao <meta@FreeBSD.org>
2020-02-04 02:33:41 +0000

    net-im/mikutter: Update to 4.0.4
    While here,
    - Always use system ruby even if devel/rbenv is installed
    
    Relnotes:	https://mikutter.hatenablog.com/entry/2019/12/25/001520
    		https://mikutter.hatenablog.com/entry/2020/01/01/124344
    		https://mikutter.hatenablog.com/entry/2020/01/05/194602
    		https://mikutter.hatenablog.com/entry/2020/01/12/184122
    		https://mikutter.hatenablog.com/entry/2020/02/01/173247

--- net-im/mikutter/files/patch-mikutter.rb
+++ net-im/mikutter/files/patch-mikutter.rb
@@ -1,6 +1,14 @@
---- mikutter.rb.orig	2017-12-24 15:00:56 UTC
+--- mikutter.rb.orig	2020-02-04 02:25:25 UTC
 +++ mikutter.rb
-@@ -13,7 +13,7 @@ http://opensource.org/licenses/mit-licen
+@@ -1,6 +1,6 @@
+ #!/bin/sh
+ # -*- coding: utf-8 -*-
+-exec ruby -x "$0" "$@"
++exec %%RUBY_WITH_SUFFIX%% -x "$0" "$@"
+ #!ruby
+ =begin rdoc
+ = mikutter - simple, powerful and moeful Mastodon client
+@@ -13,7 +13,7 @@ http://opensource.org/licenses/mit-license.php
  =end
  module Mikutter; end
  
@@ -13,8 +21,17 @@
  require 'thread'
  require 'fileutils'
  
+-require_relative 'core/miquire'
++require_relative '%%RUBY_SITELIBDIR%%/mikutter/core/miquire'
+ 
+ require 'lib/diva_hacks'
+ require 'lib/lazy'
+@@ -42,7 +42,7 @@ require 'lib/timelimitedqueue'
+ require 'lib/uithreadonly'
+ require 'lib/weakstorage'
+ 
 -require_relative 'core/utils'
 +require_relative '%%RUBY_SITELIBDIR%%/mikutter/core/utils'
  
- miquire :boot, 'check_config_permission', 'mainloop', 'delayer'
- miquire :core, 'environment'
+ require 'boot/check_config_permission'
+ require 'boot/mainloop'
