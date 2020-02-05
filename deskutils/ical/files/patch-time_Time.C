2ef85c50eeaa51a2252b23e88163be7b01f8fecb
fernape <fernape@FreeBSD.org>
2020-02-05 21:59:13 +0000

    deskutils/ical: udpate to 3.0.4
    PR:     243520
    Submitted by:   fbsd@opal.com (maintainer)

---
+++ deskutils/ical/files/patch-time_Time.C
@@ -0,0 +1,10 @@
+--- time/Time.C.orig	2012-06-02 18:44:03 UTC
++++ time/Time.C
+@@ -4,7 +4,6 @@
+ #include <sys/types.h>
+ #include <math.h>
+ #include <string.h>
+-#include <alloca.h>
+ #include <stdlib.h>
+ 
+ #include "config.h"
