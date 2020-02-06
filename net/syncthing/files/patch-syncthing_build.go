57c40367e17c4fe20c71930b4d0b624d94027646
Steve Wills <swills@FreeBSD.org>
2020-02-06 15:23:27 +0000

    net/syncthing: udpate to 1.3.4

--- net/syncthing/files/patch-syncthing_build.go
+++ net/syncthing/files/patch-syncthing_build.go
@@ -1,11 +1,11 @@
---- syncthing/build.go.orig	2019-04-04 16:46:36 UTC
+--- syncthing/build.go.orig	2020-02-05 04:20:26 UTC
 +++ syncthing/build.go
-@@ -398,7 +398,7 @@ func build(target target, tags []string) {
+@@ -443,7 +443,7 @@ func build(target target, tags []string) {
+ 	}
  
- 	rmr(target.BinaryName())
+ 	for _, pkg := range target.buildPkgs {
+-		args := []string{"build", "-v"}
++		args := []string{"build"}
+ 		args = appendParameters(args, tags, pkg)
  
--	args := []string{"build", "-v"}
-+	args := []string{"build"}
- 	args = appendParameters(args, tags, target)
- 
- 	os.Setenv("GOOS", goos)
+ 		runPrint(goCmd, args...)
