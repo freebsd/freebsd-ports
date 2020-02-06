3637fc0fe9b75e95e76e4a244a3dc75293ce35be
dinoex <dinoex@FreeBSD.org>
2020-02-06 13:15:20 +0000

    - fix build on i386

---
+++ sysutils/xen-guest-tools/files/patch-libxl_create.c
@@ -0,0 +1,38 @@
+--- tools/libxl/libxl_create.c.orig	2019-12-17 14:23:09 UTC
++++ tools/libxl/libxl_create.c
+@@ -459,7 +459,7 @@ int libxl__domain_build(libxl__gc *gc,
+         vments[2] = "image/ostype";
+         vments[3] = "hvm";
+         vments[4] = "start_time";
+-        vments[5] = GCSPRINTF("%lu.%02d", start_time.tv_sec,(int)start_time.tv_usec/10000);
++        vments[5] = GCSPRINTF("%lu.%02d", (unsigned long)start_time.tv_sec,(int)start_time.tv_usec/10000);
+ 
+         localents = libxl__calloc(gc, 13, sizeof(char *));
+         i = 0;
+@@ -498,7 +498,7 @@ int libxl__domain_build(libxl__gc *gc,
+         vments[i++] = "image/kernel";
+         vments[i++] = (char *) state->pv_kernel.path;
+         vments[i++] = "start_time";
+-        vments[i++] = GCSPRINTF("%lu.%02d", start_time.tv_sec,(int)start_time.tv_usec/10000);
++        vments[i++] = GCSPRINTF("%lu.%02d", (unsigned long)start_time.tv_sec,(int)start_time.tv_usec/10000);
+         if (state->pv_ramdisk.path) {
+             vments[i++] = "image/ramdisk";
+             vments[i++] = (char *) state->pv_ramdisk.path;
+@@ -1342,7 +1342,7 @@ static void domcreate_stream_done(libxl__egc *egc,
+         vments[2] = "image/ostype";
+         vments[3] = "hvm";
+         vments[4] = "start_time";
+-        vments[5] = GCSPRINTF("%lu.%02d", start_time.tv_sec,(int)start_time.tv_usec/10000);
++        vments[5] = GCSPRINTF("%lu.%02d", (unsigned long)start_time.tv_sec,(int)start_time.tv_usec/10000);
+         break;
+     case LIBXL_DOMAIN_TYPE_PV:
+         vments = libxl__calloc(gc, 11, sizeof(char *));
+@@ -1352,7 +1352,7 @@ static void domcreate_stream_done(libxl__egc *egc,
+         vments[i++] = "image/kernel";
+         vments[i++] = (char *) state->pv_kernel.path;
+         vments[i++] = "start_time";
+-        vments[i++] = GCSPRINTF("%lu.%02d", start_time.tv_sec,(int)start_time.tv_usec/10000);
++        vments[i++] = GCSPRINTF("%lu.%02d", (unsigned long)start_time.tv_sec,(int)start_time.tv_usec/10000);
+         if (state->pv_ramdisk.path) {
+             vments[i++] = "image/ramdisk";
+             vments[i++] = (char *) state->pv_ramdisk.path;
