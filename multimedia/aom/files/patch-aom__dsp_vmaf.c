1350a7062aa469dad84fab46c5d53f1c4705ba36
jbeich <jbeich@FreeBSD.org>
2020-02-07 23:18:55 +0000

    multimedia/aom: update to 1.0.0.3059
    Changes:	https://aomedia.googlesource.com/aom/+log/bfcd04a71..fc038664e

--- multimedia/aom/files/patch-aom__dsp_vmaf.c
+++ multimedia/aom/files/patch-aom__dsp_vmaf.c
@@ -4,62 +4,17 @@ aom_dsp/vmaf.c:12:10: fatal error: 'libvmaf/libvmaf.h' file not found
 #include <libvmaf/libvmaf.h>
          ^~~~~~~~~~~~~~~~~~~
 
-Don't use CLI symbols in shared library
-
-  /usr/lib/libaom.so: undefined reference to `fatal'
-
---- aom_dsp/vmaf.c.orig	2020-01-17 20:13:03 UTC
+--- aom_dsp/vmaf.c.orig	2020-02-07 16:59:05 UTC
 +++ aom_dsp/vmaf.c
-@@ -9,13 +9,17 @@
-  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+@@ -10,7 +10,11 @@
   */
+ 
  #include <assert.h>
 +#if __has_include(<libvmaf/libvmaf.h>)
  #include <libvmaf/libvmaf.h>
 +#else
 +#include <libvmaf.h>
 +#endif
-+#include <stdio.h>
+ #include <stdio.h>
  #include <stdlib.h>
  #include <string.h>
- 
- #include "aom_dsp/vmaf.h"
- #include "aom_ports/system_state.h"
--#include "common/tools_common.h"
- 
- typedef struct FrameData {
-   const YV12_BUFFER_CONFIG *source;
-@@ -82,7 +86,10 @@ void aom_calc_vmaf(const char *model_path, const YV12_
- 
-   aom_clear_system_state();
-   *vmaf = vmaf_score;
--  if (ret) fatal("Failed to compute VMAF scores.");
-+  if (ret) {
-+    fprintf(stderr, "Failed to compute VMAF scores.\n");
-+    exit(EXIT_FAILURE);
-+  }
- }
- 
- void aom_calc_vmaf_multi_frame(
-@@ -102,7 +109,10 @@ void aom_calc_vmaf_multi_frame(
-       /*do_ms_ssim=*/0, /*pool_method=*/NULL, /*n_thread=*/0,
-       /*n_subsample=*/1, /*enable_conf_interval=*/0);
-   FILE *vmaf_log = fopen("vmaf_scores.xml", "r");
--  if (vmaf_log == NULL || ret) fatal("Failed to compute VMAF scores.");
-+  if (vmaf_log == NULL || ret) {
-+    fprintf(stderr, "Failed to compute VMAF scores.\n");
-+    exit(EXIT_FAILURE);
-+  }
- 
-   int frame_index = 0;
-   char buf[512];
-@@ -114,7 +124,8 @@ void aom_calc_vmaf_multi_frame(
-         *p2 = '\0';
-         const double score = atof(&p[6]);
-         if (score < 0.0 || score > 100.0) {
--          fatal("Failed to compute VMAF scores.");
-+          fprintf(stderr, "Failed to compute VMAF scores.\n");
-+          exit(EXIT_FAILURE);
-         }
-         vmaf[frame_index++] = score;
-       }
--- multimedia/aom/files/patch-build_cmake_aom__configure.cmake
+++ multimedia/aom/files/patch-build_cmake_aom__configure.cmake
@@ -1,30 +1,22 @@
-- More ELF platforms can use GNU assembler on non-x86
+- ARM uses compiler intrinsics, so don't require GNU as
 
---- build/cmake/aom_configure.cmake.orig	2019-10-07 23:31:01 UTC
+--- build/cmake/aom_configure.cmake.orig	2020-02-07 16:59:05 UTC
 +++ build/cmake/aom_configure.cmake
-@@ -154,21 +154,15 @@ elseif("${AOM_TARGET_CPU}" MATCHES "arm")
-   if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
-     set(AS_EXECUTABLE as)
-     set(AOM_AS_FLAGS -arch ${AOM_TARGET_CPU} -isysroot ${CMAKE_OSX_SYSROOT})
--  elseif("${AOM_TARGET_SYSTEM}" STREQUAL "Linux")
--    if(NOT AS_EXECUTABLE)
--      set(AS_EXECUTABLE as)
--    endif()
-   elseif("${AOM_TARGET_SYSTEM}" STREQUAL "Windows")
-     if(NOT AS_EXECUTABLE)
-       set(AS_EXECUTABLE ${CMAKE_C_COMPILER} -c -mimplicit-it=always)
+@@ -162,17 +162,6 @@ elseif("${AOM_TARGET_CPU}" MATCHES "arm")
+       set(AS_EXECUTABLE as)
      endif()
-+  else()
-+    if(NOT AS_EXECUTABLE)
-+      set(AS_EXECUTABLE as)
-+    endif()
    endif()
--  if(NOT AS_EXECUTABLE)
+-  find_program(as_executable_found ${AS_EXECUTABLE})
+-  if(NOT as_executable_found)
 -    message(
 -      FATAL_ERROR
--        "Unknown assembler for: ${AOM_TARGET_CPU}-${AOM_TARGET_SYSTEM}")
+-        "Unable to find assembler and optimizations are enabled."
+-        "Searched for ${AS_EXECUTABLE}. Install it, add it to your path, or "
+-        "set the assembler directly by adding -DAS_EXECUTABLE=<assembler path> "
+-        "to your CMake command line."
+-        "To build without optimizations, add -DAOM_TARGET_CPU=generic to your "
+-        "cmake command line.")
 -  endif()
--
    string(STRIP "${AOM_AS_FLAGS}" AOM_AS_FLAGS)
  endif()
  
