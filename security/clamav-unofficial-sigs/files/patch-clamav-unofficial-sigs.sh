04cab8dbd3cbe0a8cf48be498008db302c7ee4b9
Matthias Fechner <mfechner@FreeBSD.org>
2020-02-09 19:15:01 +0000

    Update to 7.0.1. They next version will change the way config files can be used to configure this package. Users should use user.conf to do custom configuration. Changed maintainer.
    PR:		243556
    Submitted by:	sf@maxempire.com
    Reported by:	freebsd@mnd.sc

--- security/clamav-unofficial-sigs/files/patch-clamav-unofficial-sigs.sh
+++ security/clamav-unofficial-sigs/files/patch-clamav-unofficial-sigs.sh
@@ -1,6 +1,6 @@
---- clamav-unofficial-sigs.sh.orig	2019-09-02 22:41:48 UTC
+--- clamav-unofficial-sigs.sh.orig	2020-02-08 22:48:12 UTC
 +++ clamav-unofficial-sigs.sh
-@@ -1422,25 +1422,14 @@ force_wget="no"
+@@ -1541,48 +1541,17 @@ force_wget="no"
  enable_log="no"
  custom_config="no"
  we_have_a_config="0"
@@ -13,25 +13,47 @@
 -  config_dir="/etc/clamav-unofficial-sigs"
 -elif [ -f "/usr/local/etc/clamav-unofficial-sigs/master.conf" ] ; then
 -  config_dir="/usr/local/etc/clamav-unofficial-sigs/"
--elif [ -f "/opt/zimbra/config/clamav-unofficial-sigs/master.conf" ] ; then
--  config_dir="/opt/zimbra/config/clamav-unofficial-sigs/"
+-elif [ -f "/opt/zimbra/conf/clamav-unofficial-sigs/master.conf" ] ; then
+-  config_dir="/opt/zimbra/conf/clamav-unofficial-sigs/"
 -else
 -  xshok_pretty_echo_and_log "ERROR: config_dir (/etc/clamav-unofficial-sigs/master.conf) could not be found"
 -  exit 1
 -fi
  # Default config files
+-if [ -r "${config_dir}/master.conf" ] ; then
+-	config_files+=( "${config_dir}/master.conf" )
+-else
+-	xshok_pretty_echo_and_log "ERROR: ${config_dir}/master.conf is not readable"
+-	exit 1
+-fi
 +config_dir="%%PREFIX%%/etc/clamav-unofficial-sigs"
- config_files=("${config_dir}/master.conf")
--#find the a suitable os.conf or os.*.conf file
--config_file="$(find "$config_dir" -type f -iname "os.conf" -o -iname "os.*.conf" | tail -n1)"
--if [ -r "${config_file}" ]; then
--	config_files+=( "${config_file}" )
-+if [ -r "${config_dir}/os.conf" ] ; then
-+	config_files+=( "${config_dir}/os.conf" )
++config_files=("${config_dir}/master.conf")
+ if [ -r "${config_dir}/os.conf" ] ; then
+ 	config_files+=( "${config_dir}/os.conf" )
+-else
+-	#find the a suitable os.*.conf file
+-	os_config_number=$(find "$config_dir" -type f -iname "os.*.conf" | wc -l)
+-	if [ "$os_config_number" == "0" ] ; then
+-		xshok_pretty_echo_and_log "WARNING: no os.conf or os.*.conf found"
+-	elif [ "$os_config_number" == "1" ] ; then
+-		config_file="$(find "$config_dir" -type f -iname "os.*.conf" | head -n1)"
+-		if [ -r "${config_file}" ]; then
+-			config_files+=( "${config_file}" )
+-		else
+-			xshok_pretty_echo_and_log "WARNING: ${config_file} is not readable"
+-		fi
+-	else
+-		xshok_pretty_echo_and_log "WARNING: Too many os.*.conf configs found"
+-	fi
  fi
  if [ -r "${config_dir}/user.conf" ] ; then
  	config_files+=( "${config_dir}/user.conf" )
-@@ -1576,10 +1565,8 @@ if [ "$custom_config" != "no" ] ; then
+-else
+-	xshok_pretty_echo_and_log "WARNING: ${config_dir}/user.conf is not readable"
+ fi
+ 
+ # Solaris command -v function returns garbage when the program is not found
+@@ -1718,10 +1687,8 @@ if [ "$custom_config" != "no" ] ; then
  		else
  			xshok_pretty_echo_and_log "WARNING: ${config_dir}/master.conf not found"
  		fi
@@ -44,7 +66,7 @@
  		else
  			xshok_pretty_echo_and_log "WARNING: ${config_dir}/os.conf not found"
  		fi
-@@ -1625,7 +1612,7 @@ for config_file in "${config_files[@]}" ; do
+@@ -1767,7 +1734,7 @@ for config_file in "${config_files[@]}" ; do
        # Delete both trailing and leading whitespace
        # Delete all trailing whitespace
        # Delete all empty lines
@@ -53,7 +75,7 @@
      fi
  
      #fix eval of |
-@@ -1640,7 +1627,7 @@ for config_file in "${config_files[@]}" ; do
+@@ -1782,7 +1749,7 @@ for config_file in "${config_files[@]}" ; do
      fi
  
      # Check there is an = for every set of "" optional whitespace \s* between = and "
@@ -62,7 +84,7 @@
  
      if [ $(( ${#config_check} / 2 )) -ne "$config_check_vars" ] ; then
        xshok_pretty_echo_and_log "ERROR: Your configuration has errors, every = requires a pair of \"\""
-@@ -2316,24 +2303,12 @@ if [ "$sanesecurity_enabled" == "yes" ] ; then
+@@ -2504,24 +2471,12 @@ if [ "$sanesecurity_enabled" == "yes" ] ; then
          echo "$current_time" > "${work_dir_work_configs}/last-ss-update.txt"
          xshok_pretty_echo_and_log "Sanesecurity Database & GPG Signature File Updates" "="
          xshok_pretty_echo_and_log "Checking for Sanesecurity updates..."
@@ -90,3 +112,12 @@
              sanesecurity_mirror_site_info="$sanesecurity_mirror_name $sanesecurity_mirror_ip"
              xshok_pretty_echo_and_log "Sanesecurity mirror site used: ${sanesecurity_mirror_site_info}"
              # shellcheck disable=SC2086
+@@ -2834,7 +2789,7 @@ if [ "$linuxmalwaredetect_enabled" == "yes" ] ; then
+           ret="$?"
+           if [ "$ret" -eq 0 ] ; then
+ 						# shellcheck disable=SC2035
+-            $tar_bin --strip-components=1 --wildcards --overwrite -xzf "${work_dir_linuxmalwaredetect}/sigpack.tgz" --directory "${work_dir_linuxmalwaredetect}" */rfxn.*
++            $tar_bin --strip-components=1 -xzf "${work_dir_linuxmalwaredetect}/sigpack.tgz" --directory "${work_dir_linuxmalwaredetect}" "*/rfxn.*"
+             for db_file in "${linuxmalwaredetect_dbs[@]}" ; do
+               if [ "$loop" == "1" ] ; then
+                 xshok_pretty_echo_and_log "---"
