266bf590e7288dbb2bb055db228d44c908a487f4
Danilo G. Baio <dbaio@FreeBSD.org>
2020-02-10 22:39:15 +0000

    sysutils/py-azure-cli: Update to 2.0.81
    Changelog:	https://pypi.org/project/azure-cli/2.0.81/
    Reported by:	portscout

--- sysutils/py-azure-cli/files/patch-azure_cli_command__modules_storage_azcopy_util.py
+++ sysutils/py-azure-cli/files/patch-azure_cli_command__modules_storage_azcopy_util.py
@@ -1,15 +1,15 @@
---- azure/cli/command_modules/storage/azcopy/util.py.orig	2019-12-04 23:30:55 UTC
+--- azure/cli/command_modules/storage/azcopy/util.py.orig	2020-01-31 12:42:39 UTC
 +++ azure/cli/command_modules/storage/azcopy/util.py
-@@ -41,6 +41,8 @@ class AzCopy(object):
-                 file_url = base_url.format('linux', 'tar.gz')
-             elif self.system == 'Darwin':
-                 file_url = base_url.format('darwin', 'zip')
-+            elif self.system == 'FreeBSD':
-+                raise CLIError('Azcopy ({}) binary not available, follow instructions at https://wiki.freebsd.org/Ports/sysutils/py-azure-cli'.format(self.system))
-             else:
-                 raise CLIError('Azcopy ({}) does not exist.'.format(self.system))
-             try:
-@@ -163,7 +165,7 @@ def _get_default_install_location():
+@@ -50,6 +50,8 @@ class AzCopy(object):
+             file_url = base_url.format('linux', 'amd64', AZCOPY_VERSION, 'tar.gz')
+         elif self.system == 'Darwin':
+             file_url = base_url.format('darwin', 'amd64', AZCOPY_VERSION, 'zip')
++        elif self.system == 'FreeBSD':
++            raise CLIError('Azcopy ({}) binary not available, follow instructions at https://wiki.freebsd.org/Ports/sysutils/py-azure-cli'.format(self.system))
+         else:
+             raise CLIError('Azcopy ({}) does not exist.'.format(self.system))
+         try:
+@@ -180,7 +182,7 @@ def _get_default_install_location():
          if not home_dir:
              return None
          install_location = os.path.join(home_dir, r'.azcopy\azcopy.exe')
