3633b138ce9b1b35d03e8223fb72ebaac362fb35
mandree <mandree@FreeBSD.org>
2020-02-10 20:40:41 +0000

    graphics/rawtherapee: update to 5.8, and more
    * Update rawtherapee to 5.8.
    - New Capture Sharpening tool (on the "Raw" tab)
    - CR3 support for decoding (not metadata yet)
    - Improvements of various camera models.
    - http://rawtherapee.com/downloads/5.8/
    
    * Add two post-release patches cherry-picked from Git
    - The Canon sRAW/mRAW black level computation fix, 4221fdf3
    - The fix to install 32/64/SVG icons, 37bfa83 (291abfd)
    
    * Switch MASTER_SITES to https://rawtherapee.com (from http://)
    
    * Add local MASTER_SITES and mirror 5.8 source.
    
    * Add a TCMALLOC option for i386/amd64, and by default enable it.
      This requires google-perftools, which isn't available on all other
      platforms, but if your platform does provide it let me know,
      so I can whitelist it.
    
    * Restrict 11.x to amd64, it is known to malfunction for instance
      on 11.3 i386 with TIFF export.
    
    * Limit LIB_DEPENDS+=devel/openmp to 11.2 and 12.0 and early 13-CURRENT.
      11.3, 12.1 have OpenMP in base. Note that 12.0 will be EOL soon.
    
    * Under WITH_DEBUG=yes, use -O1 and RelWithDebInfo. -O0 is just
      no fun to run, way too slow.
    
    * Improve the post-install self-tests
    - Make them work even if no older rawtherapee is installed, by setting
      up an RT_SETTINGS folder. This still causes complaints by the test
      suite, but it works nonetheless.
    - More thorough error detection (tracks exit codes)
    - Run TIFF export, too (rawtherapee-cli -tz)
    
    * NOTES:
    - Getting this port to work reliably across versions and architectures
      has taken several days and many compiles to get right.
      - liblensfun 0.3.95 needed fixing against memory corruption when
        loading the database (committed as r525547/lensfun 0.3.95_1)
    - Attempting base clang on 12.1/11.3 builds has failed again, so
      we'll continue with GCC for now. This rules out MIPS.
    - You really really really only want to run RawTherapee on a system
      with 64-bit address space and sufficient RAM. On 32-bit, it will
      run out of memory all too easily.
    - You also really only want to run RawTherapee on a platform that
      supports OpenMP and with multi-core processors with sufficient
      horsepower.
    - On the single attempt to get ARM64 working, the self-tests fail.
      Run at your own risk on Tier 2/3/4 architectures.
    - Known good FreeBSD versions and architectures
      - 11.3: amd64
      - 12.0: amd64, i386 (12.0 EOL soon)
      - 12.1: amd64, i386

--- graphics/rawtherapee/files/patch-rtengine_imagedata.cc
+++ graphics/rawtherapee/files/patch-rtengine_imagedata.cc
@@ -1,6 +1,6 @@
---- rtengine/imagedata.cc.orig	2018-12-18 19:12:08 UTC
+--- rtengine/imagedata.cc.orig	2020-02-04 06:39:52 UTC
 +++ rtengine/imagedata.cc
-@@ -413,7 +413,7 @@ FrameData::FrameData (rtexif::TagDirectory* frameRootD
+@@ -506,7 +506,7 @@ FrameData::FrameData(rtexif::TagDirectory* frameRootDi
                          if (baseIsoTag) {
                              std::string isoData = baseIsoTag->valueToString();
                              if (isoData.size() > 1) {
