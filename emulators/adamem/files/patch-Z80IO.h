8544327c4048918fbb5ec4a019126d4b6dba27d8
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-02-15 00:19:45 +0000

    emulators/adamem: fix build on powerpc64 elfv2
    regparm is only supported on x86.

--- emulators/adamem/files/patch-Z80IO.h
+++ emulators/adamem/files/patch-Z80IO.h
@@ -1,13 +1,17 @@
-Index: Z80IO.h
-@@ -11,7 +11,6 @@
+--- Z80IO.h.orig	1999-02-14 19:45:30 UTC
++++ Z80IO.h
+@@ -11,9 +11,8 @@
  /****************************************************************************/
  
  #define INLINE_OP                     /* Inline Z80_RDOP()                  */
 -#define INLINE_MEM                    /* Inline Z80_RDMEM() and Z80_WRMEM() */
  
- #ifdef __GNUC__
+-#ifdef __GNUC__
++#if defined(__GNUC__) && (defined(__amd64__) || defined(__i386__))
  #define FASTCALL        __attribute__ ((regparm(3)))
-@@ -36,11 +35,11 @@
+ #ifdef INLINE_MEM
+ #define INLINE_MEM_GNU
+@@ -36,11 +35,11 @@ void Z80_Out (unsigned Port,byte Value) FASTCALL;
   /***************************************************************************/
   /* Read a byte from given memory location                                  */
   /***************************************************************************/
