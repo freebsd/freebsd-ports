a7ff60d604477e68ad4582aa29a5ca03b39dee1c
tobik <tobik@FreeBSD.org>
2020-02-16 06:15:58 +0000

    graphics/sane-backends: Update to 1.0.29
    - Rename legacy patches
    - Clean up post-patch
    - Pet portfmt/portclippy
    
    Changes:	https://gitlab.com/sane-project/backends/-/tags/1.0.29

--- graphics/sane-backends/files/patch-backend_ricoh2__buffer.c
+++ graphics/sane-backends/files/patch-backend_ricoh2__buffer.c
@@ -1,11 +1,11 @@
---- backend/ricoh2_buffer.c.orig	2019-10-09 14:45:51 UTC
+--- backend/ricoh2_buffer.c.orig	2020-02-02 11:58:09 UTC
 +++ backend/ricoh2_buffer.c
-@@ -46,7 +46,7 @@
- 
- #include <memory.h>
- #include <assert.h>
+@@ -50,7 +50,7 @@
+ #if defined(__APPLE__) && defined(__MACH__)
+ #include <malloc/malloc.h>
+ #else
 -#include <malloc.h>
 +#include <stdlib.h>
+ #endif
  
  #include "../include/sane/sanei_debug.h"
- 
--- graphics/sane-backends/files/patch-tools_sane-desc.c
+++ graphics/sane-backends/files/patch-tools_sane-desc.c
@@ -1,8 +1,8 @@
---- tools/sane-desc.c.orig	2015-09-01 00:31:54 UTC
+--- tools/sane-desc.c.orig	2020-02-02 11:58:09 UTC
 +++ tools/sane-desc.c
 @@ -2816,8 +2816,8 @@ html_print_footer (void)
       "<address>\n"
-      "<a href=\"http://www.sane-project.org/imprint.html\"\n"
+      "<a href=\"imprint.html\"\n"
       ">Contact</a>\n" "</address>\n" "<font size=-1>\n");
 -  printf ("This page was last updated on %s by sane-desc %s from %s\n",
 -	  asctime (localtime (&current_time)), SANE_DESC_VERSION, PACKAGE_STRING);
