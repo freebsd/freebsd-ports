859a85de8e97a85107954a40c497003e743ca629
Tobias C. Berner <tcberner@FreeBSD.org>
2020-02-19 18:30:26 +0000

    Update KDE Plasma Desktop to 5.18.1
    Tuesday, 18 February 2020. Today KDE releases a feature update to KDE Plasma 5,
    versioned 5.18.1. Plasma 5.18 was released in February 2020 with many feature
    refinements and new modules to complete the desktop experience.
    
    This release adds a week's worth of new translations and fixes from KDE's
    contributors. The bugfixes are typically small but important and include:
    
    Changelog:
    	https://kde.org/announcements/plasma-5.18.0-5.18.1-changelog.php

--- x11/plasma5-plasma-desktop/files/patch-desktoppackage_contents_updates_unlock_widgets.js
+++
@@ -1,8 +0,0 @@
-diff --git desktoppackage/contents/updates/unlock_widgets.js desktoppackage/contents/updates/unlock_widgets.js
-new file mode 100644
-index 000000000..c3322704b
---- /dev/null
-+++ desktoppackage/contents/updates/unlock_widgets.js
-@@ -0,0 +1,2 @@
-+
-+__AppInterface.locked = false;
