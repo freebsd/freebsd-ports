75aba49519dc852c465cdbe71e8cc6350287e298
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-02-21 18:57:00 +0000

    Update to 0.4.0
    Changes:	https://github.com/imageio/imageio-ffmpeg/releases

--- graphics/py-imageio-ffmpeg/files/patch-setup.py
+++ graphics/py-imageio-ffmpeg/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2019-04-12 09:07:51 UTC
+--- setup.py.orig	2020-02-18 12:37:49 UTC
 +++ setup.py
 @@ -57,7 +57,6 @@ setup(
      platforms="any",
      provides=["imageio_ffmpeg"],
      python_requires=">=3.4",
 -    setup_requires=["pip>19"],
-     install_requires=[],  # todo: maybe numpy
+     install_requires=[],
      packages=["imageio_ffmpeg"],
      package_dir={"imageio_ffmpeg": "imageio_ffmpeg"},
