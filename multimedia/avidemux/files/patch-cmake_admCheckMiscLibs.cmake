208aa248e567bbf04f55cd554c5c4b1168d3f2f3
tcberner <tcberner@FreeBSD.org>
2020-02-23 09:04:44 +0000

    multimedia/avidemux: update to 2.7.4 and readd Qt gui port
    - multimedia/avidemux-qt5 repo copied from multimedia/avidemux-qt4
    - The port as a whole is in need of a little modernization
    
    PR:		242634
    Submitted by:	Lars Herschke <lhersch@dssgmbh.de>,tcberner
    Tested by:	rkoberman@gmail.com
    Reported by:	VVD <vvd@unislabs.com>

--- multimedia/avidemux/files/patch-cmake_admCheckMiscLibs.cmake
+++ multimedia/avidemux/files/patch-cmake_admCheckMiscLibs.cmake
@@ -1,11 +1,11 @@
---- cmake/admCheckMiscLibs.cmake.orig	2015-05-30 14:09:29 UTC
+--- cmake/admCheckMiscLibs.cmake.orig	2019-12-14 18:40:01 UTC
 +++ cmake/admCheckMiscLibs.cmake
-@@ -67,7 +67,7 @@ ENDIF (UNIX AND NOT APPLE)
+@@ -69,7 +69,7 @@ ENDIF (UNIX AND NOT APPLE)
  MESSAGE(STATUS "Checking for execinfo")
  MESSAGE(STATUS "*********************")
  
--FIND_HEADER_AND_LIB(execinfo execinfo.h execinfo backtrace_symbols)
+-FIND_HEADER_AND_LIB(EXECINFO execinfo.h c backtrace_symbols)
 +FIND_HEADER_AND_LIB(EXECINFO execinfo.h execinfo backtrace_symbols)
- PRINT_LIBRARY_INFO("execinfo" EXECINFO_FOUND "${LIBEXECINFO_INCLUDE_DIR}" "${LIBEXECINFO_LIBRARY_DIR}")
+ PRINT_LIBRARY_INFO("execinfo" EXECINFO_FOUND "${EXECINFO_INCLUDE_DIR}" "${EXECINFO_LIBRARY_DIR}")
  
- IF (LIBEXECINFO_INCLUDE_DIR)
+ IF (EXECINFO_INCLUDE_DIR)
