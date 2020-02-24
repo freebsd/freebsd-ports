16b08b022363c98d1b65f688c2e6f5db42c78f8f
Dima Panov <fluffy@FreeBSD.org>
2020-02-24 15:12:22 +0000

    net/freeradius-client: unbreak fetch, update to 1.1.7 release
    Maintainer didn't unbreak the port over 3 months since it was marked unfetchable,
    version 1.1.7 was released 26.05.2017
    
    Approved by:	maintainer (timeout)
    MFH:		2020Q1

--- net/freeradius-client/files/patch-lib__options.h
+++ net/freeradius-client/files/patch-lib__options.h
@@ -1,6 +1,6 @@
---- lib/options.h
+--- lib/options.h.orig	2015-01-19 16:18:26 UTC
 +++ lib/options.h
-@@ -50,6 +50,7 @@
+@@ -50,6 +50,7 @@ static OPTION config_options_default[] = {
  {"radius_retries",	OT_INT,	ST_UNDEF, NULL},
  {"radius_deadtime",	OT_INT, ST_UNDEF, NULL},
  {"bindaddr",		OT_STR, ST_UNDEF, NULL},
