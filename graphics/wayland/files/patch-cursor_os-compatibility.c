972ed3346ee6def0ee384dca82eaacf79177654f
jbeich <jbeich@FreeBSD.org>
2020-02-28 10:23:24 +0000

    graphics/wayland: disable posix_fallocate on FreeBSD < 13 after r527045
    On FreeBSD < 13 neither memfd_create exists nor posix_fallocate works
    with file descriptors returned by shm_open. As SHM_ANON code wasn't
    upstreamed and is not used on FreeBSD 13 just disable posix_fallocate
    without version checks.
    
    Reported by:	Pete Wright, raichoo
    Tested by:	Pete Wright, raichoo
    Approved by:	zeising (x11), Greg V
    Differential Revision:	https://reviews.freebsd.org/D23856

--- graphics/wayland/files/patch-cursor_os-compatibility.c
+++ graphics/wayland/files/patch-cursor_os-compatibility.c
@@ -9,11 +9,13 @@
  #include <sys/mman.h>
  #endif
  
-@@ -132,6 +132,9 @@ os_create_anonymous_file(off_t size)
+@@ -131,6 +131,11 @@ os_create_anonymous_file(off_t size)
  		 */
  		fcntl(fd, F_ADD_SEALS, F_SEAL_SHRINK | F_SEAL_SEAL);
  	} else
 +#elif defined(__FreeBSD__)
++/* posix_fallocate returns ENODEV before https://svnweb.freebsd.org/changeset/base/356512 */
++#undef HAVE_POSIX_FALLOCATE
 +	fd = shm_open(SHM_ANON, O_CREAT | O_RDWR | O_CLOEXEC, 0600); // shm_open is always CLOEXEC
 +	if (fd < 0)
  #endif
