68a8c2922d5cf23f7c0247daa9827402c23a29ca
dbaio <dbaio@FreeBSD.org>
2020-02-29 16:10:18 +0000

    devel/py-pysparklines: Update to 1.0
    Drop support for Python 2.7

--- devel/py-pysparklines/files/patch-setup.py
+++ devel/py-pysparklines/files/patch-setup.py
@@ -1,9 +1,9 @@
---- setup.py.orig	2013-08-22 21:35:52 UTC
+--- setup.py.orig	2020-02-22 20:09:57 UTC
 +++ setup.py
-@@ -15,6 +15,7 @@ setup(
+@@ -15,6 +15,7 @@ setuptools.setup(
      author_email="redkrieg@gmail.com",
      url="https://github.com/RedKrieg/pysparklines",
-     packages=find_packages(),
+     packages=setuptools.find_packages(),
 +    data_files=[("man/man1", ["man/sparkline.1"])],
      entry_points={
          'console_scripts': [
