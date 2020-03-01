3483efab863d94b77f476099bd257976b33062d5
antoine <antoine@FreeBSD.org>
2020-03-01 08:25:45 +0000

    Update to 3.0.3

--- security/wapiti/files/patch-setup.py
+++ security/wapiti/files/patch-setup.py
@@ -1,14 +1,5 @@
 --- setup.py.orig	2019-09-04 12:31:34 UTC
 +++ setup.py
-@@ -50,7 +50,7 @@ doc_and_conf_files = [
-         ]
-     ),
-     (
--        "share/man/man1",
-+        "man/man1",
-         [
-             "doc/wapiti.1",
-             "doc/wapiti-getcookie.1"
 @@ -122,6 +122,5 @@ if a script is vulnerable.""",
      },
      # https://buildmedia.readthedocs.org/media/pdf/pytest/3.6.0/pytest.pdf
