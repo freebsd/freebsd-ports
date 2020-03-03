98041571e58934eb7c5bbe73659a9dabae9ebafd
0mp <0mp@FreeBSD.org>
2020-03-03 10:45:56 +0000

    Update to 1.45
    While here:
    - Update license information.
    - Sort variables.
    
    Reported by:	portscout

--- sysutils/sg3_utils/files/patch-lib_sg__pt__freebsd.c
+++ sysutils/sg3_utils/files/patch-lib_sg__pt__freebsd.c
@@ -1,13 +1,14 @@
---- lib/sg_pt_freebsd.c.orig	2018-06-29 09:46:13 UTC
+--- lib/sg_pt_freebsd.c.orig	2019-02-11 01:43:25 UTC
 +++ lib/sg_pt_freebsd.c
-@@ -1115,7 +1115,11 @@ nvme_pt_low(struct freebsd_dev_channel *fdc_p, void * 
+@@ -1181,8 +1181,12 @@ nvme_pt_low(struct freebsd_dev_channel *fdc_p, void * 
      err = ioctl(fdc_p->dev_fd, NVME_PASSTHROUGH_CMD, npcp);
      if (err < 0)
          return -errno;  /* Assume Unix error in normal place */
 +#if __FreeBSD_version <= 1200058
-     sct_sc = ((npcp->cpl.status.sct << 8) | npcp->cpl.status.sc);
++    sct_sc = ((npcp->cpl.status.sct << 8) | npcp->cpl.status.sc);
 +#else
-+    sct_sc = (NVME_STATUS_GET_SCT(npcp->cpl.status) << 8 | NVME_STATUS_GET_SC(npcp->cpl.status));
+     sct_sc = (NVME_STATUS_GET_SCT(npcp->cpl.status) << 8) |
+              NVME_STATUS_GET_SC(npcp->cpl.status);
 +#endif
      fdc_p->nvme_result = npcp->cpl.cdw0;
      sg_put_unaligned_le32(npcp->cpl.cdw0,
