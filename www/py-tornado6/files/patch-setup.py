74f9d4983383fef95ad13089305f77718cd96196
sunpoet <sunpoet@FreeBSD.org>
2020-03-04 16:17:05 +0000

    Update to 6.0.4
    - Use USES=dos2unix to convert CRLF to LF
    
    Changes:	https://www.tornadoweb.org/en/stable/releases.html

--- www/py-tornado6/files/patch-setup.py
+++ www/py-tornado6/files/patch-setup.py
@@ -1,12 +1,12 @@
---- setup.py.orig	2015-11-06 20:08:26 UTC
+--- setup.py.orig	2020-03-04 14:48:15 UTC
 +++ setup.py
-@@ -147,9 +147,6 @@ setup(
-         # in the sdist tarball)
+@@ -150,9 +150,6 @@ setup(
+         "tornado": ["py.typed"],
          "tornado.test": [
              "README",
 -            "csv_translations/fr_FR.csv",
 -            "gettext_translations/fr_FR/LC_MESSAGES/tornado_test.mo",
 -            "gettext_translations/fr_FR/LC_MESSAGES/tornado_test.po",
              "options_test.cfg",
-             "static/robots.txt",
-             "static/sample.xml",
+             "options_test_types.cfg",
+             "options_test_types_str.cfg",
