893110422e1fe1293cd656afd4c4b80971ff9213
pi <pi@FreeBSD.org>
2020-03-04 19:47:53 +0000

    net-im/prosody: refactor and move to lua 5.2
    - Use Lua 5.2 and drop support for the dead LuaJIT.
    - Explicitly state all required configure options
      as the "freebsd" preset has some of them wrong.
    - Do not install the example certs as they clutter the
      config directory and nobody should ever use them.
      They can be recreated with prosodyctl if needed.
    - Remove the broken "daemonize" option from the rc script,
      handle all actions with "prosodyctl" instead.
    - Change data directory from /usr/local/var/lib/prosody to
      /var/db/prosody and include instructions in UPDATING
    - Install the prosody migrator
    - Use arc4random
    
    PR:		241619
    Submitted by:	thomas@beingboiled.info
    Reviewed by:	lx (maintainer), Sascha Biberhofer <ports@skyforge.at>
    Approved by:	lx (maintainer timeout)

--- net-im/prosody/files/patch-util-src_time.c
+++ net-im/prosody/files/patch-util-src_time.c
@@ -1,9 +1,9 @@
---- util-src/time.c.orig	2018-12-01 10:50:51.975557000 +0000
-+++ util-src/time.c	2018-12-01 10:51:05.667316000 +0000
+--- util-src/time.c.orig	2020-01-02 09:49:37 UTC
++++ util-src/time.c
 @@ -1,5 +1,5 @@
  #ifndef _POSIX_C_SOURCE
 -#define _POSIX_C_SOURCE 199309L
 +#define _POSIX_C_SOURCE 200809L
  #endif
-
+ 
  #include <time.h>
