3bb8e1fd96b62ef0ff41b3ad41c2f13396ef245b
jbeich <jbeich@FreeBSD.org>
2020-03-05 02:03:14 +0000

    www/firefox: update to 74.0
    Changes:	https://www.mozilla.org/firefox/74.0/releasenotes/
    PR:		244310
    Security:	9f900456-0bfa-4da4-ad59-14b2933259a2
    Differential Revision:	https://reviews.freebsd.org/D23646

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -76,7 +76,7 @@ LDFLAGS+=		-Wl,--as-needed
 BUNDLE_LIBS=	yes
 
 BUILD_DEPENDS+=	llvm${LLVM_DEFAULT}>0:devel/llvm${LLVM_DEFAULT} \
-				rust-cbindgen>=0.12.0:devel/rust-cbindgen \
+				rust-cbindgen>=0.13.0:devel/rust-cbindgen \
 				${RUST_DEFAULT}>=1.39:lang/${RUST_DEFAULT} \
 				${LOCALBASE}/bin/python${PYTHON3_DEFAULT}:lang/python${PYTHON3_DEFAULT:S/.//g} \
 				node:www/node
