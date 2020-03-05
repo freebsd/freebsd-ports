40dd9a7508b3efea439f8cfb337c1793dcc88ce0
Mateusz Piotrowski <0mp@FreeBSD.org>
2020-03-05 14:24:55 +0000

    New port: devel/py-cle
    CLE loads binaries and their associated libraries, resolves imports and
    provides an abstraction of process memory the same way as if it was loader
    by the OS's loader.
    
    WWW: https://github.com/angr/cle
    
    PR:		244562

---
+++ devel/py-cle/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2020-03-05 14:08:36 UTC
++++ setup.py
+@@ -22,6 +22,6 @@ setup(
+         'pyvex==8.20.1.7',
+         'pefile',
+         'sortedcontainers>=2.0',
+-        'minidump==0.0.10',
++        'minidump>=0.0.10',
+     ]
+ )
