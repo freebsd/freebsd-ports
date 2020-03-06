eae2871da669cfad96847165723c263f07c1517c
mandree <mandree@FreeBSD.org>
2020-03-06 01:44:53 +0000

    dns/dnsmasq-devel: Update to new upstream release candidate #2.
    Changelog (from Git):
    * fc19399 2020-03-05 | Fix compiler warning. (HEAD -> master, tag: v2.81rc2, origin/master, origin/HEAD) [Simon Kelley]
    * 980b14f 2020-03-05 | Compiler warning. [Simon Kelley]
    * 1df73fe 2020-03-05 | Remove compiler warnings in IPv6 checksum code in dump.c [Simon Kelley]
    * c125c1d 2020-03-05 | Update decline address handling in DHCPv6 for new multi-address world. [Simon Kelley]
    * e39c484 2020-03-05 | Fix parameters to setsockopt() for TCP_FASTOPEN. [Matthias Andree]
    * 977a5a2 2020-03-02 | Merge i18n messages. [Simon Kelley]
    * 02df000 2020-03-02 | Trivial formatting fix. [Simon Kelley]
    
    Drop previous patches, and add two new ones,
    one to fix pkgconfig for lua,
    one to fix the bitfield flag testing in rfc3315.c/DHCPv6 decline handling.

--- dns/dnsmasq-devel/files/patch-src_rfc3315.c
+++ dns/dnsmasq-devel/files/patch-src_rfc3315.c
@@ -1,11 +1,11 @@
---- src/rfc3315.c.orig	2020-03-02 18:11:22 UTC
+--- src/rfc3315.c.orig	2020-03-05 22:13:45 UTC
 +++ src/rfc3315.c
-@@ -1190,7 +1190,7 @@ static int dhcp6_no_relay(struct state *state, int msg
- 		/* align */
- 		memcpy(&addr, opt6_ptr(ia_option, 0), IN6ADDRSZ);
+@@ -1708,7 +1708,7 @@ static int config_valid(struct dhcp_config *config, st
+     return 0;
  
--		if (have_config(config, CONFIG_ADDR6) && IN6_ARE_ADDR_EQUAL(&config->addr6, &addr))
-+		if (have_config(config, CONFIG_ADDR6) && IN6_ARE_ADDR_EQUAL(&config->addr6->addr.addr6, &addr))
- 		  {
- 		    prettyprint_time(daemon->dhcp_buff3, DECLINE_BACKOFF);
- 		    inet_ntop(AF_INET6, &addr, daemon->addrbuff, ADDRSTRLEN);
+   for (addr_list = config->addr6; addr_list; addr_list = addr_list->next)
+-    if (!(addr_list->flags && ADDRLIST_DECLINED) ||
++    if (!(addr_list->flags & ADDRLIST_DECLINED) ||
+ 	difftime(now, addr_list->decline_time) >= (float)DECLINE_BACKOFF)
+       {
+ 	addrpart = addr6part(&addr_list->addr.addr6);
