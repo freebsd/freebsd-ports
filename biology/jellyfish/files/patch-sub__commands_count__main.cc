4906705775ced663d5a202536b7ef38cedb63ef7
Jason W. Bacon <jwb@FreeBSD.org>
2020-03-07 19:25:54 +0000

    biology/jellyfish: Patch for clang 10
    Add cast to silence implicit conversion error
    
    PR:             ports/244645
    Submitted by:   dim

--- biology/jellyfish/files/patch-sub__commands_count__main.cc
+++ biology/jellyfish/files/patch-sub__commands_count__main.cc
@@ -1,6 +1,6 @@
---- sub_commands/count_main.cc.orig	2019-05-13 07:42:50 UTC
+--- sub_commands/count_main.cc.orig	2018-04-16 12:46:09 UTC
 +++ sub_commands/count_main.cc
-@@ -330,7 +330,7 @@ int count_main(int argc, char *argv[])
+@@ -360,7 +360,7 @@ int count_main(int argc, char *argv[])
          uint64_t max = args.upper_count_given ? args.upper_count_arg : std::numeric_limits<uint64_t>::max();
          try {
            merge_files(files, args.output_arg, header, min, max);
