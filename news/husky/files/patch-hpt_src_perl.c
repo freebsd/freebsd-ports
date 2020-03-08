68989143cabc7f84be643a91626a17ef4842182d
tcberner <tcberner@FreeBSD.org>
2020-03-08 19:09:14 +0000

    news/husky*: Update ports to latest versions
    PR:		243030
    Submitted by:	Oleg Sharoyko <osharoiko@gmail.com> (maintainer)
    Reported by:	Ruslan Suleimanov <rs@paket.ua>

--- news/husky/files/patch-hpt_src_perl.c
+++ news/husky/files/patch-hpt_src_perl.c
@@ -1,14 +1,15 @@
---- hpt/src/perl.c.orig	2018-03-28 16:01:18 UTC
+--- hpt/src/perl.c.orig	2020-01-16 21:50:04 UTC
 +++ hpt/src/perl.c
-@@ -109,9 +109,9 @@ extern "C" {
+@@ -109,10 +109,12 @@ extern "C" {
  # define min(a, b)      ((a) < (b) ? (a) : (b))
  #endif
  
--#ifdef __GNUC__
-+/*#ifdef __GNUC__
- # define Perl___notused Perl___notused __attribute__ ((unused))
--#endif
-+#endif*/
++#if 0
+ #ifdef __GNUC__
+ # ifdef _OLDPERL_
+ #  define Perl___notused Perl___notused __attribute__ ((unused))
+ # endif
++#endif
+ #endif
  
  #ifndef LL_PERL
- # define LL_PERL LL_EXEC
