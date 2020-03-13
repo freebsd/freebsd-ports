02efecff0255a68bdec5927e702905c75ad211d2
danfe <danfe@FreeBSD.org>
2020-03-13 20:15:41 +0000

    Unbreak against Clang 10 and Python 3.
    Reported by:	pkg-fallout

--- games/gtkradiant/files/patch-install.py
+++ games/gtkradiant/files/patch-install.py
@@ -1,5 +1,14 @@
 --- ./install.py.orig	Sun Feb 12 16:47:01 2006
 +++ ./install.py	Thu Mar 16 16:09:46 2006
+@@ -38,7 +38,7 @@
+   targetFile = target
+   if os.path.isdir(targetFile):
+     targetFile = os.path.join(target, os.path.basename(source))
+-  print source, "->", targetFile
++  print(source, "->", targetFile)
+   shutil.copyfile(source, targetFile)
+   
+ def copyFileIfExists(source, target):
 @@ -98,8 +98,6 @@
  ]
  
