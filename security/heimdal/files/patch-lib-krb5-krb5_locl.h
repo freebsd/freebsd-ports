f60311d8713f586ae10525060d82fcb9bdb3e6e0
hrs <hrs@FreeBSD.org>
2020-03-13 17:18:40 +0000

    Regen patches.

--- security/heimdal/files/patch-lib-krb5-krb5_locl.h
+++ security/heimdal/files/patch-lib-krb5-krb5_locl.h
@@ -1,6 +1,6 @@
---- lib/krb5/krb5_locl.h.orig	2020-03-13 06:00:08.405783000 +0900
-+++ lib/krb5/krb5_locl.h	2020-03-13 13:19:46.263840000 +0900
-@@ -143,7 +143,7 @@
+--- lib/krb5/krb5_locl.h.orig	2019-06-07 06:21:39 UTC
++++ lib/krb5/krb5_locl.h
+@@ -143,7 +143,7 @@ struct _krb5_krb_auth_data;
  #include <krb5.h>
  #include <krb5_err.h>
  #include <asn1_err.h>
@@ -9,7 +9,7 @@
  #include <hx509.h>
  #endif
  
-@@ -271,7 +271,7 @@
+@@ -271,7 +271,7 @@ typedef struct krb5_context_data {
  #define KRB5_CTX_F_RD_REQ_IGNORE		16
  #define KRB5_CTX_F_FCACHE_STRICT_CHECKING	32
      struct send_to_kdc *send_to_kdc;
