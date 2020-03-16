0fcb44d42e5e6494d5f4d0e19a0cf1a355fdbaf4
swills <swills@FreeBSD.org>
2020-03-16 20:01:55 +0000

    devel/ocaml-lacaml: fix patch to make REINPLACE_CMD effective

--- devel/ocaml-lacaml/files/patch-myocamlbuild.ml
+++ devel/ocaml-lacaml/files/patch-myocamlbuild.ml
@@ -5,14 +5,14 @@
              [
                 (OASISExpr.EBool true,
 -                 S [A "-cclib"; A "-lblas"; A "-cclib"; A "-llapack"]);
-+                 S [A "-cclib"; A "-L/usr/local/lib"; A "-cclib"; A "-lblas"; A "-cclib"; A "-llapack"]);
++                 S [A "-cclib"; A "-L%%LOCALBASE%%/lib"; A "-cclib"; A "%%BLASLIB%%"; A "-cclib"; A "%%LAPACKLIB%%"]);
                 (OASISExpr.ETest ("system", "macosx"),
                   S [A "-cclib"; A "-framework"; A "-cclib"; A "Accelerate"])
              ]);
            (["oasis_library_lacaml_cclib"; "ocamlmklib"; "c"],
              [
 -               (OASISExpr.EBool true, S [A "-lblas"; A "-llapack"]);
-+               (OASISExpr.EBool true, S [A "-L/usr/local/lib"; A "-lblas"; A "-llapack"]);
++               (OASISExpr.EBool true, S [A "-L%%LOCALBASE%%/lib"; A "%%BLASLIB%%"; A "%%LAPACKLIB%%"]);
                 (OASISExpr.ETest ("system", "macosx"),
                   S [A "-framework"; A "Accelerate"])
              ])
