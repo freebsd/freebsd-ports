2384cc5a6058ce3e4900c6d2768749e9a3894627
pkubaj <pkubaj@FreeBSD.org>
2020-03-17 19:25:19 +0000

    lang/rust: fix build on powerpc64 elfv2
    After upgrade to LLVM 10, core in stage 1 fails to compile (no clear reason, rustc crashes):
    pid 26828 (rustc), jid 0, uid 0: exited on signal 11 (core dumped)
    
    Compilation with GCC works fine, so switch to GCC for the time being.
    
    PR:		244813
    Approved by:	mikael

---
+++ lang/rust/files/powerpc64-elfv2/patch-src_librustc__llvm_build.rs
@@ -0,0 +1,18 @@
+--- src/librustc_llvm/build.rs.orig	2019-12-16 15:38:05 UTC
++++ src/librustc_llvm/build.rs
+@@ -273,7 +273,14 @@ fn main() {
+     };
+ 
+     // C++ runtime library
+-    if !target.contains("msvc") {
++    if target == "powerpc64-unknown-freebsd" {
++        if is_crossed {
++            println!("cargo:rustc-link-search=native=%WRKDIR%/usr/local/lib/%CC%");
++        } else {
++            println!("cargo:rustc-link-search=native=/usr/local/lib/%CC%");
++        }
++        println!("cargo:rustc-link-lib=static=stdc++");
++    } else if !target.contains("msvc") {
+         if let Some(s) = llvm_static_stdcpp {
+             assert!(!cxxflags.contains("stdlib=libc++"));
+             let path = PathBuf::from(s);
