8e83020a81b6e9be59ae9c1868f96b0bd06d9170
Jung-uk Kim <jkim@FreeBSD.org>
2020-03-19 06:41:25 +0000

    Add an upstream patch to fix build with Clang 10.0.
    Note PORTREVISION is bumped because the patch actually fixes a bug.
    
    https://bugs.launchpad.net/dee/+bug/1579529
    https://bazaar.launchpad.net/~unity-team/dee/trunk/revision/439

--- devel/dee/files/patch-src_dee-transaction.c
+++ devel/dee/files/patch-src_dee-transaction.c
@@ -1,5 +1,5 @@
---- src/dee-transaction.c.orig	2012-11-28 09:46:41.000000000 +0100
-+++ src/dee-transaction.c	2016-01-17 15:50:39.997965000 +0100
+--- src/dee-transaction.c.orig	2012-11-28 08:46:41 UTC
++++ src/dee-transaction.c
 @@ -233,7 +233,7 @@ journal_iter_free (JournalIter *jiter)
    g_slice_free (JournalIter, jiter);
  }
