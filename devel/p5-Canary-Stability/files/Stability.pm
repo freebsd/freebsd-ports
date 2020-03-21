536134639f9dce2fe225abf439597a69be9d4d0e
adamw <adamw@FreeBSD.org>
2020-03-21 00:44:25 +0000

    p5-Canary::Stability: Provide only the FreeBSD stub
    Canary::Stability continues to provide the same harmful instructions
    that it has since 2013, including instructing users to uninstall Perl
    and install one from 2015 (it also advises users to switch to an
    unmaintained fork provided by the author).
    
    Previously, there was an OPTION to install the actual module. There
    is no scenario under which that should ever happen, so I'm removing
    that entirely. This port now only provides a no-op stub.

--- devel/p5-Canary-Stability/files/Stability.pm
+++ devel/p5-Canary-Stability/files/Stability.pm
@@ -1,6 +1,6 @@
 =head1 NAME
 
-Canary::Stability - Stub to prevent checking perl compatibility for "schmorp" modules
+Canary::Stability - Stub to avoid harmful instructions
 
 =head1 SYNOPSIS
 
@@ -8,9 +8,14 @@ Canary::Stability - Stub to prevent checking perl compatibility for "schmorp" mo
 
 =head1 PURPOSE
 
-This is a stub module that neutralizes the original Canary::Stability
-module. That module is a relic of a strange notion that "schmorp" should
-prevent people from using any Perl released after 2015.
+The original Canary::Stability is a bizarre set of system checks
+that provide dangerous instructions. Among those instructions is
+never to use any Perl released after 2015, and to use a fork of
+Perl from a person who calls himself "schmorp."
+
+This stub exists only to satisfy modules (all of which are released
+by "schmorp") that list it as a dependency. It does nothing, which
+is considered far more useful than the original.
 
 =cut
 
