652daca4de00f6f10755fc39430bcf82efcd8207
tcberner <tcberner@FreeBSD.org>
2020-03-22 06:43:19 +0000

    multimedia/mlt: prepare for Qt5 upgrade

---
+++ multimedia/mlt/files/patch-src_modules_qt_producer__qtext.cpp
@@ -0,0 +1,10 @@
+--- src/modules/qt/producer_qtext.cpp.orig	2020-03-22 06:32:03 UTC
++++ src/modules/qt/producer_qtext.cpp
+@@ -26,6 +26,7 @@
+ #include <QImage>
+ #include <QColor>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QFont>
+ #include <QString>
+ #include <QTextCodec>
