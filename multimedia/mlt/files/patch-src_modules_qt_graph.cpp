6113f26707b4db670210de68c4b91ddbed2a99ac
Tobias C. Berner <tcberner@FreeBSD.org>
2020-03-22 06:43:19 +0000

    multimedia/mlt: prepare for Qt5 upgrade

---
+++ multimedia/mlt/files/patch-src_modules_qt_graph.cpp
@@ -0,0 +1,10 @@
+--- src/modules/qt/graph.cpp.orig	2020-03-22 06:36:38 UTC
++++ src/modules/qt/graph.cpp
+@@ -18,6 +18,7 @@
+  */
+ 
+ #include "graph.h"
++#include <QPainterPath>
+ #include <QVector>
+ #include <math.h>
+ 
---
+++ multimedia/mlt/files/patch-src_modules_qt_kdenlivetitle__wrapper.cpp
@@ -0,0 +1,10 @@
+--- src/modules/qt/kdenlivetitle_wrapper.cpp.orig	2020-03-22 06:37:07 UTC
++++ src/modules/qt/kdenlivetitle_wrapper.cpp
+@@ -28,6 +28,7 @@
+ #include <QGraphicsScene>
+ #include <QGraphicsTextItem>
+ #include <QGraphicsSvgItem>
++#include <QPainterPath>
+ #include <QSvgRenderer>
+ #include <QTextCursor>
+ #include <QTextDocument>
