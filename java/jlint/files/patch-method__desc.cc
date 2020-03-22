2162d07aedf86833acc7076d01f1080f182ca442
Tobias Kortkamp <tobik@FreeBSD.org>
2020-03-22 06:08:32 +0000

    java/jlint: Unbreak on CURRENT
    In file included from access_desc.cc:1:
    In file included from ./access_desc.hh:8:
    In file included from ./class_desc.hh:4:
    In file included from ./types.hh:41:
    ./jlint.d:117:16: error: redefinition of 'drem'
    JAVA_INSN(115, drem, 1)
                   ^
    /usr/include/math.h:329:8: note: previous definition is here
    double  drem(double, double);
            ^

--- java/jlint/files/patch-method__desc.cc
+++ java/jlint/files/patch-method__desc.cc
@@ -1,6 +1,6 @@
 --- method_desc.cc.orig	2003-06-09 09:29:14 UTC
 +++ method_desc.cc
-@@ -767,7 +767,7 @@ void method_desc::parse_code(constant** 
+@@ -767,7 +767,7 @@ void method_desc::parse_code(constant** constant_pool,
            sp->mask = var_desc::vs_not_null;
          }
          sp->index = NO_ASSOC_VAR;
@@ -9,7 +9,16 @@
          sp += 1;
        }
        break;
-@@ -2381,7 +2381,7 @@ void method_desc::parse_code(constant** 
+@@ -1906,7 +1906,7 @@ void method_desc::parse_code(constant** constant_pool,
+     case dsub:
+     case dmul:
+     case ddiv:
+-    case drem:
++    case Jdrem:
+       sp -= 2;
+       break;
+     case ineg:
+@@ -2381,7 +2381,7 @@ void method_desc::parse_code(constant** constant_pool,
              aux->name_and_type = nt;
              sp->equals = aux;
              equal_descs.push_back(aux);*/
