b3b534a92e7952201e765d3574aefbad62b5ff78
mikael <mikael@FreeBSD.org>
2020-03-24 20:01:46 +0000

    devel/lazygit: unbreak on aarch64
    # github.com/jesseduffield/pty
    vendor/github.com/jesseduffield/pty/pty_freebsd.go:50:20: undefined: fiodgnameArg
    vendor/github.com/jesseduffield/pty/pty_freebsd.go:63:12: undefined: _C_SPECNAMELEN
    vendor/github.com/jesseduffield/pty/pty_freebsd.go:66:9: undefined: fiodgnameArg
    
    Add the missing ztypes_freebsd_arm64.go file for aarch64
    
    PR:		245013
    Approved by:	meta (maintainer), manu (mentor, implicit)

---
+++ devel/lazygit/files/patch-vendor_github.com_jesseduffield_pty_ztypes__freebsd__arm64.go
@@ -0,0 +1,16 @@
+--- vendor/github.com/jesseduffield/pty/ztypes_freebsd_arm64.go.orig	2019-12-23 19:57:30 UTC
++++ vendor/github.com/jesseduffield/pty/ztypes_freebsd_arm64.go
+@@ -0,0 +1,13 @@
++// Created by cgo -godefs - DO NOT EDIT
++// cgo -godefs types_freebsd.go
++
++package pty
++
++const (
++	_C_SPECNAMELEN = 0x3f
++)
++
++type fiodgnameArg struct {
++	Len       int32
++	Buf       *byte
++}
