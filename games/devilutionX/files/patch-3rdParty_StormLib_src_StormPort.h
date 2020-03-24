1b1700ade3d33a552a16ad9876f5ffb1b76fb159
fernape <fernape@FreeBSD.org>
2020-03-24 17:48:45 +0000

    games/devilutionX: Update to 1.0.1
    ChangeLog: https://github.com/diasurgical/devilutionX/releases/tag/1.0.1
    
    PR:	244722
    Submitted by:	benny.goemans@gmail.com (maintainer)

--- games/devilutionX/files/patch-3rdParty_StormLib_src_StormPort.h
+++ games/devilutionX/files/patch-3rdParty_StormLib_src_StormPort.h
@@ -1,6 +1,14 @@
---- 3rdParty/StormLib/src/StormPort.h.orig	2019-10-11 11:58:58 UTC
+--- 3rdParty/StormLib/src/StormPort.h.orig	2020-03-10 14:00:26 UTC
 +++ 3rdParty/StormLib/src/StormPort.h
 @@ -165,6 +165,7 @@
+   #include <ctype.h>
+   #include <assert.h>
+   #include <errno.h>
++  #include <strings.h>
+ 
+   #ifndef __BIG_ENDIAN__
+     #define PLATFORM_LITTLE_ENDIAN
+@@ -193,6 +194,7 @@
    #include <ctype.h>
    #include <assert.h>
    #include <errno.h>
