3318196f67a3ff6bd1dc168fa4af484934a2c160
koobs <koobs@FreeBSD.org>
2020-03-25 04:16:25 +0000

    devel/py-nose2: Update to 0.9.2 [1]
      - Add TEST_DEPENDS / test target to improve QA
      - Add comment for setup.py patch to describe why its there
    
    Changelog:
    
      https://github.com/nose-devs/nose2/blob/0.9.2/docs/changelog.rst
    
    PR:		245030 [1]
    Submitted by:	Daniel Ylitalo <daniel blodan se> [1]

--- devel/py-nose2/files/patch-setup.py
+++ devel/py-nose2/files/patch-setup.py
@@ -1,5 +1,6 @@
-Allow USE_PYTHON=concurrent to handle script renaming/linking for multiple
-Python versions.
+# Upstream creates a nose2-X.Y script name already
+# Port uses concurrent, which creates nose2-X.Y-X.Y
+# duplicate suffix
 
 --- setup.py.orig	2019-04-02 15:52:32 UTC
 +++ setup.py
