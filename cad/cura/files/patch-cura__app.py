de4ef25addf8fb56df35f41e0e040c82b3668bc0
db <db@FreeBSD.org>
2020-03-26 00:03:41 +0000

    This is the new, shiny frontend for Cura. Check daid/LegacyCura for the legacy Cura that everyone knows and loves/hates. We re-worked the whole GUI code at Ultimaker, because the old code started to become unmaintainable.
    WWW: https://github.com/Ultimaker/Cura/wiki

---
+++ cad/cura/files/patch-cura__app.py
@@ -0,0 +1,8 @@
+--- cura_app.py.orig	2020-02-28 16:06:57 UTC
++++ cura_app.py
+@@ -1,4 +1,4 @@
+-#!/usr/bin/env python3
++#!/usr/bin/env %%PYTHON_CMD%%
+ 
+ # Copyright (c) 2019 Ultimaker B.V.
+ # Cura is released under the terms of the LGPLv3 or higher.
