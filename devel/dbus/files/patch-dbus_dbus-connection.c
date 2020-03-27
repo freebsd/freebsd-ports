35ed8ec99cdc81aea5bf7c4206cfcfc91c8cd91a
Mateusz Piotrowski <0mp@FreeBSD.org>
2020-03-27 09:59:14 +0000

    Substitute REINPLACE_CMD with a patch
    While here, mute MKDIR and regenerate patches.
    
    Approved by:	portmgr blanket

--- devel/dbus/files/patch-dbus_dbus-connection.c
+++ devel/dbus/files/patch-dbus_dbus-connection.c
@@ -1,6 +1,6 @@
---- dbus/dbus-connection.c.orig	2010-08-31 21:44:19.000000000 +0000
-+++ dbus/dbus-connection.c	2010-09-18 18:01:19.000000000 +0000
-@@ -2364,7 +2364,7 @@ _dbus_connection_block_pending_call (DBu
+--- dbus/dbus-connection.c.orig	2018-02-08 14:07:21 UTC
++++ dbus/dbus-connection.c
+@@ -2385,7 +2385,7 @@ _dbus_connection_block_pending_call (DBusPendingCall *
    DBusConnection *connection;
    dbus_uint32_t client_serial;
    DBusTimeout *timeout;
@@ -9,8 +9,8 @@
  
    _dbus_assert (pending != NULL);
  
-@@ -2445,7 +2445,11 @@ _dbus_connection_block_pending_call (DBu
-   _dbus_get_current_time (&tv_sec, &tv_usec);
+@@ -2466,7 +2466,11 @@ _dbus_connection_block_pending_call (DBusPendingCall *
+   _dbus_get_monotonic_time (&tv_sec, &tv_usec);
    elapsed_milliseconds = (tv_sec - start_tv_sec) * 1000 +
  	  (tv_usec - start_tv_usec) / 1000;
 -  
@@ -22,7 +22,7 @@
    if (!_dbus_connection_get_is_connected_unlocked (connection))
      {
        DBusMessage *error_msg;
-@@ -2471,7 +2475,7 @@ _dbus_connection_block_pending_call (DBu
+@@ -2494,7 +2498,7 @@ _dbus_connection_block_pending_call (DBusPendingCall *
             */
            _dbus_verbose ("dbus_connection_send_with_reply_and_block() waiting for more memory\n");
  
@@ -31,7 +31,7 @@
          }
        else
          {          
-@@ -2480,7 +2484,7 @@ _dbus_connection_block_pending_call (DBu
+@@ -2503,7 +2507,7 @@ _dbus_connection_block_pending_call (DBusPendingCall *
                                                    pending,
                                                    DBUS_ITERATION_DO_READING |
                                                    DBUS_ITERATION_BLOCK,
@@ -40,7 +40,7 @@
          }
  
        goto recheck_status;
-@@ -2489,7 +2493,7 @@ _dbus_connection_block_pending_call (DBu
+@@ -2512,7 +2516,7 @@ _dbus_connection_block_pending_call (DBusPendingCall *
      _dbus_verbose ("dbus_connection_send_with_reply_and_block(): clock set backward\n");
    else if (elapsed_milliseconds < timeout_milliseconds)
      {
@@ -49,7 +49,7 @@
        
        if (status == DBUS_DISPATCH_NEED_MEMORY)
          {
-@@ -2499,7 +2503,7 @@ _dbus_connection_block_pending_call (DBu
+@@ -2522,7 +2526,7 @@ _dbus_connection_block_pending_call (DBusPendingCall *
             */
            _dbus_verbose ("dbus_connection_send_with_reply_and_block() waiting for more memory\n");
  
@@ -58,8 +58,8 @@
          }
        else
          {          
-@@ -2508,7 +2512,7 @@ _dbus_connection_block_pending_call (DBu
-                                                   NULL,
+@@ -2531,7 +2535,7 @@ _dbus_connection_block_pending_call (DBusPendingCall *
+                                                   pending,
                                                    DBUS_ITERATION_DO_READING |
                                                    DBUS_ITERATION_BLOCK,
 -                                                  timeout_milliseconds - elapsed_milliseconds);
--- devel/dbus/files/patch-dbus_dbus-sysdeps-util-unix.c
+++ devel/dbus/files/patch-dbus_dbus-sysdeps-util-unix.c
@@ -1,10 +1,10 @@
---- dbus/dbus-sysdeps-util-unix.c.orig	2008-12-21 14:51:17.000000000 -0500
-+++ dbus/dbus-sysdeps-util-unix.c	2008-12-21 14:51:37.000000000 -0500
-@@ -38,6 +38,7 @@
+--- dbus/dbus-sysdeps-util-unix.c.orig	2019-06-09 12:08:49 UTC
++++ dbus/dbus-sysdeps-util-unix.c
+@@ -42,6 +42,7 @@
  #include <stdio.h>
  #include <errno.h>
  #include <fcntl.h>
 +#include <syslog.h>
  #include <sys/stat.h>
- #include <grp.h>
- #include <sys/socket.h>
+ #ifdef HAVE_SYS_RESOURCE_H
+ #include <sys/resource.h>
