934218c6a671600a2e04d5912242523907492ed7
Steve Wills <swills@FreeBSD.org>
2020-03-29 15:56:38 +0000

    databases/memcached: update to 1.6.2

--- databases/memcached/files/patch-memcached.c
+++ databases/memcached/files/patch-memcached.c
@@ -1,15 +1,6 @@
---- memcached.c.orig	2018-02-13 05:09:37 UTC
+--- memcached.c.orig	2020-03-23 17:30:29 UTC
 +++ memcached.c
-@@ -316,7 +316,7 @@ static int add_msghdr(conn *c)
-     c->msgbytes = 0;
-     c->msgused++;
- 
--    if (IS_UDP(c->transport)) {
-+    if IS_UDP(c->transport) {
-         /* Leave room for the UDP header, which we'll fill in later. */
-         return add_iov(c, NULL, UDP_HEADER_SIZE);
-     }
-@@ -582,7 +582,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
+@@ -647,7 +647,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
          if (init_state == conn_listening) {
              fprintf(stderr, "<%d server listening (%s)\n", sfd,
                  prot_text(c->protocol));
@@ -18,7 +9,16 @@
              fprintf(stderr, "<%d server listening (udp)\n", sfd);
          } else if (c->protocol == negotiating_prot) {
              fprintf(stderr, "<%d new auto-negotiating client connection\n",
-@@ -752,7 +752,7 @@ static void conn_cleanup(conn *c) {
+@@ -708,7 +708,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
+         c->write = tcp_write;
+     }
+ 
+-    if (IS_UDP(transport)) {
++    if IS_UDP(transport) {
+         c->try_read_command = try_read_command_udp;
+     } else {
+         switch (c->protocol) {
+@@ -876,7 +876,7 @@ static void conn_cleanup(conn *c) {
          c->sasl_conn = NULL;
      }
  
@@ -27,25 +27,25 @@
          conn_set_state(c, conn_read);
      }
  }
-@@ -822,7 +822,7 @@ static void conn_close(conn *c) {
- static void conn_shrink(conn *c) {
-     assert(c != NULL);
- 
--    if (IS_UDP(c->transport))
-+    if IS_UDP(c->transport)
-         return;
- 
-     if (c->rsize > READ_BUFFER_HIGHWAT && c->rbytes < DATA_BUFFER_SIZE) {
-@@ -5253,7 +5253,7 @@ static enum transmit_result transmit(conn *c) {
-         if (settings.verbose > 0)
-             perror("Failed to write, and not due to blocking");
- 
--        if (IS_UDP(c->transport))
-+        if IS_UDP(c->transport)
-             conn_set_state(c, conn_read);
-         else
-             conn_set_state(c, conn_closing);
-@@ -5694,7 +5694,7 @@ static void drive_machine(conn *c) {
+@@ -1038,7 +1038,7 @@ static bool resp_start(conn *c) {
+         c->resp->next = resp;
+         c->resp = resp;
+     }
+-    if (IS_UDP(c->transport)) {
++    if IS_UDP(c->transport) {
+         // need to hold on to some data for async responses.
+         c->resp->request_id = c->request_id;
+         c->resp->request_addr = c->request_addr;
+@@ -3443,7 +3443,7 @@ static void process_stats_conns(ADD_STAT add_stats, vo
+              * output -- not worth the complexity of the locking that'd be
+              * required to prevent it.
+              */
+-            if (IS_UDP(conns[i]->transport)) {
++            if IS_UDP(conns[i]->transport) {
+                 APPEND_NUM_STAT(i, "UDP", "%s", "UDP");
+             }
+             if (conns[i]->state != conn_closed) {
+@@ -7315,7 +7315,7 @@ static void drive_machine(conn *c) {
              break;
  
          case conn_closing:
@@ -54,7 +54,7 @@
                  conn_cleanup(c);
              else
                  conn_close(c);
-@@ -5858,7 +5858,7 @@ static int server_socket(const char *interface,
+@@ -7479,7 +7479,7 @@ static int server_socket(const char *interface,
  #endif
  
          setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, (void *)&flags, sizeof(flags));
@@ -63,7 +63,7 @@
              maximize_sndbuf(sfd);
          } else {
              error = setsockopt(sfd, SOL_SOCKET, SO_KEEPALIVE, (void *)&flags, sizeof(flags));
-@@ -5913,7 +5913,7 @@ static int server_socket(const char *interface,
+@@ -7534,7 +7534,7 @@ static int server_socket(const char *interface,
              }
          }
  
