5592a99bba078763a39bde2515f64d07da2da1cf
Tobias Kortkamp <tobik@FreeBSD.org>
2020-03-30 09:12:30 +0000

    graphics/gtimelapse: Unbreak build on FreeBSD >= 12.1
    gtimelapse.cpp:753:2: error: reference to 'thread' is ambiguous
            thread = CreateThread();
            ^
    ./globals.h:23:18: note: candidate found by name lookup is 'thread'
    extern MyThread *thread;
                     ^
    /usr/include/c++/v1/__threading_support:397:24: note: candidate found by name lookup is 'std::__1::thread'
    class _LIBCPP_TYPE_VIS thread;
                           ^
    
    http://beefy6.nyi.freebsd.org/data/121amd64-default/529299/logs/errors/gtimelapse-0.1_16.log

--- graphics/gtimelapse/files/patch-src__gtimelapse.cpp
+++ graphics/gtimelapse/files/patch-src__gtimelapse.cpp
@@ -1,6 +1,6 @@
 --- src/gtimelapse.cpp.orig	2009-06-08 12:13:59 UTC
 +++ src/gtimelapse.cpp
-@@ -155,7 +155,11 @@ wxPanel *display_widgets (wxPanel *panel
+@@ -155,7 +155,11 @@ wxPanel *display_widgets (wxPanel *panel, CameraWidget
  					//cout << "type: RADIO" << endl;
  					//cout << "current: " << current << endl;
  
@@ -12,7 +12,7 @@
  					
  					for ( i=0; i<cnt; i++) {
  						const char *choice;
-@@ -165,6 +169,10 @@ wxPanel *display_widgets (wxPanel *panel
+@@ -165,6 +169,10 @@ wxPanel *display_widgets (wxPanel *panel, CameraWidget
  						choices[i] = mystring;
  					}
  			
@@ -23,3 +23,15 @@
  					wxString title(label, wxConvUTF8);
  					wxString default_choice(current, wxConvUTF8);
  					wxString choice_label(uselabel, wxConvUTF8);
+@@ -742,9 +750,9 @@ void MyFrame::StartCapture(wxCommandEvent& event){
+ 
+ 	button2->Disable();
+ 	button4->Enable();
+-	thread = CreateThread();
++	::thread = CreateThread();
+ 
+-	if ( thread->Run() != wxTHREAD_NO_ERROR ){
++	if ( ::thread->Run() != wxTHREAD_NO_ERROR ){
+ 		wxLogError(wxT("Can't start thread!"));
+     	}else{
+ 		//cout << "Thread started." << endl;
