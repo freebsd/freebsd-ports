53ddb57aac69b9bbf4881109001286fc9174dda9
Mathieu Arnold <mat@FreeBSD.org>
2020-03-30 19:28:27 +0000

    Update to 2.0.59.

--- sysutils/munin-common/files/patch-node_lib_Munin_Node_Server.pm
+++ sysutils/munin-common/files/patch-node_lib_Munin_Node_Server.pm
@@ -1,9 +1,9 @@
 Backporting: https://github.com/munin-monitoring/munin/commit/8ff5d5a9ec8b7ffd99dce61fee42defe7bda3b29
 See also: http://munin-monitoring.org/ticket/1451
 
---- node/lib/Munin/Node/Server.pm.orig	2020-03-09 01:59:44 UTC
+--- node/lib/Munin/Node/Server.pm.orig	2020-03-24 12:31:00 UTC
 +++ node/lib/Munin/Node/Server.pm
-@@ -343,7 +343,7 @@ sub _print_service {
+@@ -341,7 +341,7 @@ sub _print_service {
  sub _list_services {
      my ($session, $node) = @_;
  
