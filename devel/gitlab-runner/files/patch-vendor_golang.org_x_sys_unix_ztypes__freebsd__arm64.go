ba5540d80e7ae48f4471a904cbd9f3e3cf0c4cc8
Mikael Urankar <mikael@FreeBSD.org>
2020-03-30 15:06:49 +0000

    devel/gitlab-runner: fix patch files
    The patch were applied twice.
    
    Reported by:	mfechner
    Approved by:	manu (mentor, implicit)

--- devel/gitlab-runner/files/patch-vendor_golang.org_x_sys_unix_ztypes__freebsd__arm64.go
+++ devel/gitlab-runner/files/patch-vendor_golang.org_x_sys_unix_ztypes__freebsd__arm64.go
@@ -22,27 +22,3 @@ Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
  	Fp_sr uint32
  	Fp_cr uint32
  }
-From 33540a1f603772f9d4b761f416f5c10dade23e96 Mon Sep 17 00:00:00 2001
-From: Tobias Klauser <tklauser@distanz.ch>
-Date: Fri, 25 Oct 2019 21:13:51 +0200
-Subject: [PATCH] unix: don't use non-existing uint128 type on freebsd/arm64
-
-CL 179099 introduced type FpReg on freebsd, on freebsd/arm64 using a
-type uint128. This type doesn't exist in Go.
-
-Change-Id: Ie7f065f38f2cc2c163abdfa7dddad886b8edbe3b
-Reviewed-on: https://go-review.googlesource.com/c/sys/+/203419
-Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
-TryBot-Result: Gobot Gobot <gobot@golang.org>
-Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>
---- vendor/golang.org/x/sys/unix/ztypes_freebsd_arm64.go.orig	2019-08-18 13:42:05 UTC
-+++ vendor/golang.org/x/sys/unix/ztypes_freebsd_arm64.go
-@@ -397,7 +397,7 @@ type Reg struct {
- }
- 
- type FpReg struct {
--	Fp_q  [32]uint128
-+	Fp_q  [512]uint8
- 	Fp_sr uint32
- 	Fp_cr uint32
- }
