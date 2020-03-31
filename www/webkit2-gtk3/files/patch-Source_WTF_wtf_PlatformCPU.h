7f952c966f8ff64bcae27bd65f71fa398a83b7aa
Koop Mast <kwm@FreeBSD.org>
2020-03-31 20:01:41 +0000

    Update webkit2-gtk3 to 2.28.0.

--- www/webkit2-gtk3/files/patch-Source_WTF_wtf_PlatformCPU.h
+++ www/webkit2-gtk3/files/patch-Source_WTF_wtf_PlatformCPU.h
@@ -1,6 +1,6 @@
---- Source/WTF/wtf/Platform.h.orig	2019-01-14 07:57:51 UTC
-+++ Source/WTF/wtf/Platform.h
-@@ -182,6 +182,7 @@
+--- Source/WTF/wtf/PlatformCPU.h.orig	2020-03-16 20:02:18.816718000 +0100
++++ Source/WTF/wtf/PlatformCPU.h	2020-03-16 20:02:49.520094000 +0100
+@@ -155,6 +155,7 @@
  #elif defined(__ARM_ARCH_6__) \
      || defined(__ARM_ARCH_6J__) \
      || defined(__ARM_ARCH_6K__) \
@@ -8,7 +8,7 @@
      || defined(__ARM_ARCH_6Z__) \
      || defined(__ARM_ARCH_6ZK__) \
      || defined(__ARM_ARCH_6T2__) \
-@@ -229,6 +230,7 @@
+@@ -196,6 +197,7 @@
  
  #elif defined(__ARM_ARCH_6J__) \
      || defined(__ARM_ARCH_6K__) \
