fd7ef5102563e7fbb1360095516028c7db0d0822
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2020-04-01 19:59:45 +0000

    databases/grass7, math/saga, graphics/qgis and graphics/qgis-ltr: update to latest versions
        - math/saga:
            - Update to 7.6.2
            - Use wxWidgets 3.0+ instead of 3.1+ since 3.1 is not a mandatory version and this reduces the number of packages installed when you install saga and qgis/grass (which requires version 3.0).
    
        - databases/grass7:
            - Update to 7.8.2 first version for python3 only
            - Use ${PY_NUMPY} variable
            - Readd openmp
            - Use wxPython40 for python3
            - Remove bash dependency. You can use any shell.
            - Rework the workaround in post-install (for / touch files). Fix the long (random) standing issue with pkg-plist
            - Alphabetize USES and OPTIONS
            - Replace XX_configure_on by XX_configure_with as possible
    
        - graphics/qgis:
            - Update to 3.12.1
            - Remove patches (applied upstream or fixed in ports: pyqt5 / sip)
    
        - graphics/qgis-ltr:
            - Update to 3.10.4
    
    PR:             241892, 244979
    Reported by:    rainer hurling (maintainer), lbartoletti (maintainer)
    Reviewed by:    tcberner (mentor), rainer hurling (maintainer)
    Approved by:    tcberner (mentor), rainer hurling (maintainer)
    Differential Revision:  https://reviews.freebsd.org/D24198

--- graphics/qgis/files/patch-python_plugins_processing_algs_saga_SagaAlgorithmProvider.py
+++ graphics/qgis/files/patch-python_plugins_processing_algs_saga_SagaAlgorithmProvider.py
@@ -1,19 +1,19 @@
---- python/plugins/processing/algs/saga/SagaAlgorithmProvider.py.orig	2019-03-02 02:07:46 UTC
+--- python/plugins/processing/algs/saga/SagaAlgorithmProvider.py.orig	2020-03-27 05:50:09 UTC
 +++ python/plugins/processing/algs/saga/SagaAlgorithmProvider.py
-@@ -82,7 +82,7 @@ class SagaAlgorithmProvider(QgsProcessingProvider):
+@@ -79,7 +79,7 @@ class SagaAlgorithmProvider(QgsProcessingProvider):
  
      def canBeActivated(self):
          version = SagaUtils.getInstalledVersion(True)
--        if version is not None and version.startswith(REQUIRED_VERSION):
+-        if version is not None and (version.startswith(REQUIRED_VERSION) or version.startswith(BETA_SUPPORT_VERSION)):
 +        if version is not None and version >= REQUIRED_VERSION:
              return True
          return False
  
-@@ -93,7 +93,7 @@ class SagaAlgorithmProvider(QgsProcessingProvider):
+@@ -96,7 +96,7 @@ class SagaAlgorithmProvider(QgsProcessingProvider):
                                       self.tr('Processing'), Qgis.Critical)
              return
  
--        if not version.startswith(REQUIRED_VERSION):
+-        if not version.startswith(REQUIRED_VERSION) and not version.startswith(BETA_SUPPORT_VERSION):
 +        if version < REQUIRED_VERSION:
              QgsMessageLog.logMessage(self.tr('Problem with SAGA installation: unsupported SAGA version (found: {}, required: {}).').format(version, REQUIRED_VERSION),
                                       self.tr('Processing'),
