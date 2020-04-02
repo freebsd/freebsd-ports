3c68bea70034e94bf6369d3b8510f6a5279594f3
Jason W. Bacon <jwb@FreeBSD.org>
2020-04-02 13:41:03 +0000

    MFH: r530250
    kallisto: Patch for build with GCC
    
    Fixes broken build on GCC-based platforms
    
    PR:             ports/245240
    Submitted by:   pkubaj
    
    Approved by:	portmgr (fix build blanket)

---
+++ biology/kallisto/files/patch-src_Bootstrap.h
@@ -0,0 +1,10 @@
+--- src/Bootstrap.h.orig	2020-04-01 16:56:42 UTC
++++ src/Bootstrap.h
+@@ -1,6 +1,7 @@
+ #ifndef KALLISTO_BOOTSTRAP_H
+ #define KALLISTO_BOOTSTRAP_H
+ 
++#include <sys/types.h>
+ #include <mutex>
+ #include <thread>
+ 
