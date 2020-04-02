5d749b104341c1ffbcef04fd389db5a1301e64f4
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2020-04-02 11:56:27 +0000

    astro/qmapshack: Update version 1.14.0=>1.14.1
    - Add USE_QT=help
    
    Relnotes:	https://github.com/Maproom/qmapshack/releases

--- astro/qmapshack/files/patch-cmake_Modules_DefineInstallationPaths.cmake
+++ astro/qmapshack/files/patch-cmake_Modules_DefineInstallationPaths.cmake
@@ -1,5 +1,14 @@
---- cmake/Modules/DefineInstallationPaths.cmake.orig	2018-09-08 09:17:56 UTC
+--- cmake/Modules/DefineInstallationPaths.cmake.orig	2020-03-27 08:07:31 UTC
 +++ cmake/Modules/DefineInstallationPaths.cmake
+@@ -59,7 +59,7 @@ if (UNIX)
+     FORCE
+   )
+   SET(HTML_INSTALL_DIR
+-    "${DATA_INSTALL_PREFIX}/doc/HTML"
++	  "${DATA_INSTALL_PREFIX}/doc/${_PROJECT_NAME}/HTML"
+     CACHE PATH "The HTML install dir for documentation (default data/doc/html)"
+     FORCE
+   )
 @@ -97,7 +97,7 @@ if (UNIX)
      FORCE
    )
