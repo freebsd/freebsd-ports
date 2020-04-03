2b4c6cc6a65a4a4ea6970252c83c4fe6941281f6
Steve Wills <swills@FreeBSD.org>
2020-04-03 13:51:51 +0000

    japanese/scim-skk: Fix REINPLACE_CMD

--- japanese/scim-skk/files/patch-src-scim_skk_prefs.h
+++ japanese/scim-skk/files/patch-src-scim_skk_prefs.h
@@ -5,7 +5,7 @@
  #define SCIM_SKK_CONFIG_SELECTION_STYLE_DEFAULT      "Qwerty"
  
 -#define SCIM_SKK_CONFIG_SYSDICT_DEFAULT          "DictFile:/usr/share/skk/SKK-JISYO.L"
-+#define SCIM_SKK_CONFIG_SYSDICT_DEFAULT          "DictFile:/usr/local/share/skk/SKK-JISYO.L"
++#define SCIM_SKK_CONFIG_SYSDICT_DEFAULT          "DictFile:%%LOCALBASE%%/share/skk/SKK-JISYO.L"
  #define SCIM_SKK_CONFIG_USERDICT_DEFAULT         ".skk-scim-jisyo"
  #define SCIM_SKK_CONFIG_CANDVEC_SIZE_DEFAULT      4
  #define SCIM_SKK_CONFIG_ANNOT_VIEW_DEFAULT    true
