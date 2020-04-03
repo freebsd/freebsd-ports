f517659df5f852b95cfe7940b3d5f6c1ba7bb21c
Yuri Victorovich <yuri@FreeBSD.org>
2020-04-03 09:26:08 +0000

    MFH: r530454
    multimedia/obs-studio: Fix the procfs(5) link for the executable path: /proc/self/exe -> /proc/curproc/file
    
    PR:		245299
    Reported by:	Keith Hellman <khellman@mcprogramming.com> (the reported issue seems to be the same)
    
    Approved by:	ports-secteam@FreeBSD.org

--- multimedia/obs-studio/files/patch-libobs_util_platform-nix.c
+++ multimedia/obs-studio/files/patch-libobs_util_platform-nix.c
@@ -1,10 +1,22 @@
---- libobs/util/platform-nix.c.orig	2017-10-29 09:19:47 UTC
+--- libobs/util/platform-nix.c.orig	2020-03-19 16:51:06 UTC
 +++ libobs/util/platform-nix.c
-@@ -38,6 +38,7 @@
- #include <sys/queue.h>
+@@ -40,6 +40,7 @@
  #include <sys/socket.h>
+ #include <sys/sysctl.h>
  #include <sys/user.h>
 +#include <sys/sysctl.h>
  #include <unistd.h>
  #include <libprocstat.h>
  #else
+@@ -275,7 +276,11 @@ char *os_get_program_data_path_ptr(const char *name)
+ char *os_get_executable_path_ptr(const char *name)
+ {
+ 	char exe[PATH_MAX];
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
++	ssize_t count = readlink("/proc/curproc/file", exe, PATH_MAX);
++#else
+ 	ssize_t count = readlink("/proc/self/exe", exe, PATH_MAX);
++#endif
+ 	const char *path_out = NULL;
+ 	struct dstr path;
+ 
