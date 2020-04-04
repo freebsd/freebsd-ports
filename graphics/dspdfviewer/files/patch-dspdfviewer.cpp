eeee6534ff32da51c9c39934aeab2b60b1b34f7b
Tobias C. Berner <tcberner@FreeBSD.org>
2020-04-04 13:10:57 +0000

    graphics/dspdfviewer: Prepare for Qt5-5.14
    PR:		244964

--- graphics/dspdfviewer/files/patch-dspdfviewer.cpp
+++ graphics/dspdfviewer/files/patch-dspdfviewer.cpp
@@ -6,6 +6,19 @@ Fix build with Clang 8:
 
 --- dspdfviewer.cpp.orig	2016-09-13 13:18:47 UTC
 +++ dspdfviewer.cpp
+@@ -292,10 +292,10 @@ void DSPDFViewer::sendAllClockSignals() const
+   emit presentationClockUpdate(presentationClock());
+ }
+ 
+-QTime DSPDFViewer::timeSince(const QTime& startPoint) const
++QTime DSPDFViewer::timeSince(const QElapsedTimer& startPoint) const
+ {
+   QTime result(0,0);
+-  result = result.addMSecs(startPoint.elapsed());
++  result = result.addMSecs(static_cast<int>(startPoint.elapsed()));
+   return result;
+ }
+ 
 @@ -308,7 +308,7 @@ RenderingIdentifier DSPDFViewer::toRenderIdent(unsigne
  
  RenderingIdentifier DSPDFViewer::toThumbnailRenderIdent(unsigned int pageNumber, PDFViewerWindow& window)
