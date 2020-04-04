1f4f336323aecb5f8b6a575e4c55dffeccb0ec17
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2020-04-04 12:44:03 +0000

    security/snort3: UNBREAK
    - Update version 3.0.0-258=>3.0.0-270
    - Remove BUILD_DEPENDS and RUN_DEPENDS into LIB_DEPENDS
    - Change compiler to use c++14-lang
    - Make HYPERSCAN default for amd64
    - Remove DEBUG_DESC and DOCS_DESC for the defaults DESC
    
    net/libdaq: Update version 3.0.0-alpha3=>3.0.0-alpha4
    
    Submitted by:	dvl
    MFH:		2020Q2 (buildtime fix)
    Differential Revision:	https://reviews.freebsd.org/D24263

--- security/snort3/files/patch-src_main_analyzer.cc
+++ security/snort3/files/patch-src_main_analyzer.cc
@@ -1,11 +1,20 @@
---- src/main/analyzer.cc.orig	2019-07-31 10:41:21 UTC
+--- src/main/analyzer.cc.orig	2020-03-25 14:13:20 UTC
 +++ src/main/analyzer.cc
-@@ -94,7 +94,7 @@ class RetryQueue (public)
+@@ -95,7 +95,7 @@ class RetryQueue (public)
      RetryQueue(unsigned interval_ms)
      {
          assert(interval_ms > 0);
--        interval = { interval_ms / 1000, (interval_ms % 1000) * 1000 };
+-        interval = { interval_ms / 1000, static_cast<suseconds_t>((interval_ms % 1000) * 1000) };
 +        interval = { static_cast<time_t>(interval_ms / 1000), static_cast<suseconds_t>( (interval_ms % 1000) * 1000 )};
      }
  
      ~RetryQueue()
+@@ -546,7 +546,7 @@ void Analyzer::idle()
+     struct timeval now, increment;
+     unsigned int timeout = SnortConfig::get_conf()->daq_config->timeout;
+     packet_gettimeofday(&now);
+-    increment = { timeout / 1000, static_cast<suseconds_t>((timeout % 1000) * 1000) };
++    increment = { static_cast<time_t>(timeout / 1000), static_cast<suseconds_t>((timeout % 1000) * 1000) };
+     timeradd(&now, &increment, &now);
+     packet_time_update(&now);
+ 
