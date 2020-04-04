2f73e9a39658df65f569b1f4f6fb42aadea205cc
Timur I. Bakeyev <timur@FreeBSD.org>
2020-04-04 01:12:04 +0000

    Add databases/ldb20 port, needed for samba411.

---
+++ databases/ldb20/files/patch-buildtools_wafsamba_samba__autoconf.py
@@ -0,0 +1,50 @@
+--- buildtools/wafsamba/samba_autoconf.py.orig	2019-09-04 10:53:16 UTC
++++ buildtools/wafsamba/samba_autoconf.py
+@@ -573,7 +573,7 @@ def library_flags(self, libs):
+ 
+ 
+ @conf
+-def CHECK_LIB(conf, libs, mandatory=False, empty_decl=True, set_target=True, shlib=False):
++def CHECK_LIB(conf, libs, mandatory=False, empty_decl=True, set_target=True, shlib=False, msg=None):
+     '''check if a set of libraries exist as system libraries
+ 
+     returns the sublist of libs that do exist as a syslib or []
+@@ -593,11 +593,29 @@ int foo()
+             ret.append(lib)
+             continue
+ 
++        if msg is None:
++            msg = 'Checking for library %s' % lib
++
+         (ccflags, ldflags, cpppath) = library_flags(conf, lib)
++
+         if shlib:
+-            res = conf.check(features='c cshlib', fragment=fragment, lib=lib, uselib_store=lib, cflags=ccflags, ldflags=ldflags, uselib=lib.upper(), mandatory=False)
++            res = conf.check(features='c cshlib',
++                             fragment=fragment,
++                             lib=lib,
++                             uselib_store=lib,
++                             cflags=ccflags,
++                             ldflags=ldflags,
++                             uselib=lib.upper(),
++                             mandatory=False,
++                             msg=msg)
+         else:
+-            res = conf.check(lib=lib, uselib_store=lib, cflags=ccflags, ldflags=ldflags, uselib=lib.upper(), mandatory=False)
++            res = conf.check(lib=lib,
++                             uselib_store=lib,
++                             cflags=ccflags,
++                             ldflags=ldflags,
++                             uselib=lib.upper(),
++                             mandatory=False,
++                             msg=msg)
+ 
+         if not res:
+             if mandatory:
+@@ -944,5 +962,5 @@ def SAMBA_CHECK_UNDEFINED_SYMBOL_FLAGS(c
+         conf.env.undefined_ldflags = conf.ADD_LDFLAGS('-Wl,-no-undefined', testflags=True)
+ 
+         if (conf.env.undefined_ignore_ldflags == [] and
+-            conf.CHECK_LDFLAGS(['-undefined', 'dynamic_lookup'])):
++            conf.CHECK_LDFLAGS(['-undefined', 'dynamic_lookup'] + conf.env.WERROR_CFLAGS)):
+             conf.env.undefined_ignore_ldflags = ['-undefined', 'dynamic_lookup']
