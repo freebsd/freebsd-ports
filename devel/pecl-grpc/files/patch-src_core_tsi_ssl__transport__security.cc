6655744dcf88e6810a975bb2f87ccf446784fe10
Vanilla I. Shu <vanilla@FreeBSD.org>
2020-04-04 11:19:42 +0000

    Upgrade to 1.28.0.

--- devel/pecl-grpc/files/patch-src_core_tsi_ssl__transport__security.cc
+++ devel/pecl-grpc/files/patch-src_core_tsi_ssl__transport__security.cc
@@ -1,6 +1,6 @@
---- src/core/tsi/ssl_transport_security.cc.orig	2018-04-16 17:48:12 UTC
+--- src/core/tsi/ssl_transport_security.cc.orig	2020-04-02 22:15:03 UTC
 +++ src/core/tsi/ssl_transport_security.cc
-@@ -20,6 +20,7 @@
+@@ -22,6 +22,7 @@
  
  #include "src/core/tsi/ssl_transport_security.h"
  
