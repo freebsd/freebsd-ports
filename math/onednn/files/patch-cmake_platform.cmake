107ad6127b980a1d3ff1d84102af874b342d898a
Yuri Victorovich <yuri@FreeBSD.org>
2020-04-04 07:13:35 +0000

    math/mkl-dnn: Rename to math/onednn; Update 1.2.2 -> 1.3
    The project is renamed by the upstream.

--- math/onednn/files/patch-cmake_platform.cmake
+++ math/onednn/files/patch-cmake_platform.cmake
@@ -1,6 +1,6 @@
---- cmake/platform.cmake.orig	2019-12-24 01:47:09 UTC
+--- cmake/platform.cmake.orig	2020-03-31 16:33:27 UTC
 +++ cmake/platform.cmake
-@@ -89,7 +89,6 @@ elseif(UNIX OR MINGW)
+@@ -95,7 +95,6 @@ elseif(UNIX OR MINGW)
      append(CMAKE_CCXX_NOEXCEPT_FLAGS "-fno-exceptions")
      # compiler specific settings
      if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
@@ -8,11 +8,12 @@
          # Clang cannot vectorize some loops with #pragma omp simd and gets
          # very upset. Tell it that it's okay and that we love it
          # unconditionally.
-@@ -133,7 +132,6 @@ elseif(UNIX OR MINGW)
-             append(CMAKE_CCXX_SANITIZER_FLAGS "-g -fno-omit-frame-pointer")
+@@ -144,7 +143,7 @@ elseif(UNIX OR MINGW)
+              set(DNNL_ENABLE_JIT_PROFILING CACHE BOOL "OFF" FORCE)
+              message(WARNING "AArch64 build, DNNL_ENABLE_JIT_PROFILING is OFF")
+         else()
+-             set(DEF_ARCH_OPT_FLAGS "-msse4.1")
++             #set(DEF_ARCH_OPT_FLAGS "-msse4.1")
          endif()
-     elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
--        set(DEF_ARCH_OPT_FLAGS "-msse4.1")
          # suppress warning on assumptions made regarding overflow (#146)
          append(CMAKE_CCXX_NOWARN_FLAGS "-Wno-strict-overflow")
-     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
