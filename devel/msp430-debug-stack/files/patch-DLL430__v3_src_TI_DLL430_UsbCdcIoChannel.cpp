9f51adcdfaeb3abc4d72e466b36aaf151e8eeeb6
Lev A. Serebryakov <lev@FreeBSD.org>
2020-04-04 15:32:39 +0000

    Update to latest version.

--- devel/msp430-debug-stack/files/patch-DLL430__v3_src_TI_DLL430_UsbCdcIoChannel.cpp
+++ devel/msp430-debug-stack/files/patch-DLL430__v3_src_TI_DLL430_UsbCdcIoChannel.cpp
@@ -1,6 +1,6 @@
---- DLL430_v3/src/TI/DLL430/UsbCdcIoChannel.cpp.orig	2017-12-05 19:25:19.220234000 +0300
-+++ DLL430_v3/src/TI/DLL430/UsbCdcIoChannel.cpp	2017-12-05 19:31:57.808647000 +0300
-@@ -309,6 +309,22 @@
+--- DLL430_v3/src/TI/DLL430/UsbCdcIoChannel.cpp.orig	2020-02-14 09:38:32 UTC
++++ DLL430_v3/src/TI/DLL430/UsbCdcIoChannel.cpp
+@@ -310,6 +310,22 @@ void UsbCdcIoChannel::createCdcPortList(const uint16_t
  	}
  #else
  	stringstream cdcIdStream;
@@ -23,7 +23,7 @@
  	cdcIdStream << hex << setfill('0') << "usb:v" << setw(4) << vendorId << "p" << setw(4) << productId;
  
  	path p("/sys/class/tty/");
-@@ -330,6 +346,7 @@
+@@ -331,6 +347,7 @@ void UsbCdcIoChannel::createCdcPortList(const uint16_t
  				ifNumStream >> interfaceNumber;
  				if (modalias.find(cdcIdStream.str()) == 0 && interfaceNumber == 0)
  				{
@@ -31,33 +31,3 @@
  					const string filename = it->path().filename().string();
  					const string portPath = string("/dev/") + filename;
  
-@@ -375,7 +392,11 @@
- 
- bool UsbCdcIoChannel::openPort()
- {
-+#if BOOST_VERSION < 106600
- 	ioService = new boost::asio::io_service;
-+#else
-+	ioService = new boost::asio::io_context;
-+#endif
- 	port = new boost::asio::serial_port(*ioService);
- 	timer = new boost::asio::deadline_timer(*ioService);
- 
-@@ -384,7 +405,7 @@
- 		int retry = 5;
- 		while (ec && --retry )
- 		{
--			this_thread::sleep_for(chrono::milliseconds(5));
-+			this_thread::sleep_for(std::chrono::milliseconds(5));
- 			ec = port->open(portInfo.path, ec);
- 		}
- 
-@@ -409,7 +430,7 @@
- 	{
- 		openPort();
- 		//Seeing issues on some platforms (eg. Ubuntu) when port is immediately closed again
--		this_thread::sleep_for(chrono::milliseconds(100));
-+		this_thread::sleep_for(std::chrono::milliseconds(100));
- 		close();
- 	}
- }
