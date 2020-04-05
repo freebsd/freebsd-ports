14aae109df4ffd8ba04bfa9382aeff4307c725f9
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-04-05 19:54:43 +0000

    Update to 1.0.2
    Changes:	https://github.com/Zulko/moviepy/releases

--- multimedia/py-moviepy/files/patch-moviepy-editor.py
+++ multimedia/py-moviepy/files/patch-moviepy-editor.py
@@ -1,6 +1,6 @@
---- moviepy/editor.py.orig	2019-02-14 01:02:48 UTC
+--- moviepy/editor.py.orig	2020-03-24 20:20:06 UTC
 +++ moviepy/editor.py
-@@ -20,15 +20,6 @@ clip.preview().
+@@ -20,18 +20,6 @@ clip.preview().
  import os
  import sys
  
@@ -8,10 +8,13 @@
 -import imageio
 -# Checks to see if the user has set a place for their own version of ffmpeg
 -
--if os.getenv('FFMPEG_BINARY', 'ffmpeg-imageio') == 'ffmpeg-imageio':
+-if os.getenv('FFMPEG_BINARY') is None:
 -    if sys.version_info < (3, 4):
 -        #uses an old version of imageio with ffmpeg.download.
 -        imageio.plugins.ffmpeg.download()
+-
+-# Hide the welcome message from pygame: https://github.com/pygame/pygame/issues/542
+-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
 -
  # Clips
  from .video.io.VideoFileClip import VideoFileClip
