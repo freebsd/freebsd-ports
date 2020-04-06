d9fd17a8be708ae3da8b4c66b6d0f7b0c85e5a49
Alexey Dokuchaev <danfe@FreeBSD.org>
2020-04-06 15:42:20 +0000

    Unbreak the build: as of recently, <assert.h> header file must be included before <Magick++.h> because of the following conflict:
      [...]: error: use of undeclared identifier '__assert'; did you mean
            'MagickCore::__assert'?
              assert( file.colorSpace() == Magick::sRGBColorspace );
              ^
      /usr/include/assert.h:56:36: note: expanded from macro 'assert'
      #define assert(e)       ((e) ? (void)0 : __assert(__func__, __FILE__, \
    
      /usr/include/assert.h:77:6: note: 'MagickCore::__assert' declared here
      void __assert(const char *, const char *, int, const char *) __dead2;
    
    Reported by:	pkg-fallout

---
+++ graphics/hdr_tools/files/patch-headers.hh
@@ -0,0 +1,18 @@
+--- headers.hh.orig	2010-11-23 21:22:52 UTC
++++ headers.hh
+@@ -30,6 +30,7 @@
+ 
+ 
+ 
++#include <assert.h>
+ #include <popt.h>
+ #include <tiff.h>
+ #include <tiffio.h>
+@@ -61,7 +62,6 @@
+ #include <cmath>
+ 
+ 
+-#include <assert.h>
+ #include <stdlib.h>
+ #include <math.h>
+ #include <stdint.h>
