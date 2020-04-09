4290d4a833c1a31eb35f3d8a1045b185e6ecc453
Yuri Victorovich <yuri@FreeBSD.org>
2020-04-09 06:41:59 +0000

    textproc/py-citeproc-py: Fix build
    Reported by:	fallout

--- textproc/py-citeproc-py/files/patch-setup.py
+++ textproc/py-citeproc-py/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2017-06-23 13:08:27 UTC
+--- setup.py.orig	2020-04-06 16:34:05 UTC
 +++ setup.py
-@@ -29,7 +29,7 @@ os.chdir(BASE_PATH)
+@@ -30,7 +30,7 @@ os.chdir(BASE_PATH)
  # inspired by http://dcreager.net/2010/02/10/setuptools-git-version-numbers/
  try:
      print('Attempting to get version number from git...')
@@ -9,11 +9,18 @@
                  stdout=PIPE, stderr=sys.stderr)
      if git.wait() != 0:
          raise OSError
-@@ -85,7 +85,6 @@ setup(
+@@ -85,13 +85,12 @@ setup(
      name='citeproc-py',
      version=__version__,
      cmdclass = dict(build_py=custom_build_py, develop=custom_develop),
 -    packages=find_packages(),
      package_data={PACKAGE: ['data/locales/*.xml',
+                             'data/locales/locales.json',
                              'data/schema/*.rng',
                              'data/styles/*.csl']},
+     scripts=['bin/csl_unsorted'],
+-    setup_requires=['rnc2rng>=2.6.1,!=2.6.2'],
++    setup_requires=['rnc2rng'],
+     install_requires=['lxml'],
+     provides=[PACKAGE],
+     #test_suite='nose.collector',
