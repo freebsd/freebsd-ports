5102cb74ca32627d3202fd72ab2520806b0da2cb
Kurt Jaeger <pi@FreeBSD.org>
2020-04-11 05:49:24 +0000

    net/whois: update 5.2.17 -> 5.5.6
    PR:		243230
    Submitted by:	Juraj Lutter <juraj@lutter.sk>
    Reviewed by:	fernape
    Approved by:	coco@executive-computing.de (maintainer timeout)
    Relnotes:	https://github.com/rfc1036/whois/blob/next/debian/changelog

--- net/whois/files/patch-config.h
+++ net/whois/files/patch-config.h
@@ -1,11 +1,41 @@
---- config.h.orig	2014-06-08 18:25:19.000000000 +0200
-+++ config.h	2014-06-08 18:25:39.000000000 +0200
-@@ -8,7 +8,7 @@
+--- config.h.orig	2020-01-09 21:40:46 UTC
++++ config.h
+@@ -7,9 +7,7 @@
+ #define DEFAULTSERVER   "whois.arin.net"
  
  /* Configuration file */
- /*
+-/*
 -#define CONFIG_FILE "/etc/whois.conf"
+-*/
 +#define CONFIG_FILE "%%MWHOISCONF%%"
- */
  
  
+ /* autoconf in cpp macros */
+@@ -17,17 +15,12 @@
+ # include <sys/param.h>
+ #endif
+ 
+-#ifdef __GLIBC__
+-# define ENABLE_NLS
+-#endif
+-
+ #ifdef __FreeBSD__
+ /* which versions? */
+ # define HAVE_GETOPT_LONG
+ # define HAVE_GETADDRINFO
+-# define ENABLE_NLS
+ # ifndef LOCALEDIR
+-#  define LOCALEDIR "/usr/local/share/locale"
++#  define LOCALEDIR "%%PREFIX%%/share/locale"
+ # endif
+ #endif
+ 
+@@ -116,7 +109,7 @@
+ 
+ #ifdef ENABLE_NLS
+ # ifndef NLS_CAT_NAME
+-#  define NLS_CAT_NAME   "whois"
++#  define NLS_CAT_NAME   "%%MWHOIS%%"
+ # endif
+ # ifndef LOCALEDIR
+ #  define LOCALEDIR     "/usr/share/locale"
--- net/whois/files/patch-mkpasswd.c
+++ net/whois/files/patch-mkpasswd.c
@@ -1,12 +1,10 @@
---- mkpasswd.orig	2014-06-08 17:57:46.000000000 +0200
-+++ mkpasswd.c	2014-06-08 17:58:02.000000000 +0200
-@@ -17,7 +17,9 @@
+--- mkpasswd.c.orig	2020-01-09 21:08:11 UTC
++++ mkpasswd.c
+@@ -17,7 +17,6 @@
   */
  
  /* for crypt, snprintf and strcasecmp */
-+#ifndef __FreeBSD__
- #define _XOPEN_SOURCE 500
-+#endif
+-#define _XOPEN_SOURCE 500
  #define _BSD_SOURCE 1
+ #define _DEFAULT_SOURCE 1
  #define __EXTENSIONS__ 1
- 
