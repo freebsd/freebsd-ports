211d0079ac86648f494945b49204fec911ba2dc6
Matthias Fechner <mfechner@FreeBSD.org>
2020-04-15 20:36:06 +0000

    Fixed dependency problems that caused gitaly/gitlab-ce build to fail.
    PR:		245559
    Reported by:	freebsd at wb9.se

--- net/rubygem-grpc124/files/patch-src_ruby_ext_grpc_extconf.rb
+++ net/rubygem-grpc124/files/patch-src_ruby_ext_grpc_extconf.rb
@@ -13,7 +13,7 @@
  ENV['ARCH_FLAGS'] = RbConfig::CONFIG['ARCH_FLAG']
  ENV['ARCH_FLAGS'] = '-arch i386 -arch x86_64' if RUBY_PLATFORM =~ /darwin/
  ENV['CPPFLAGS'] = '-DGPR_BACKWARDS_COMPATIBILITY_MODE'
-@@ -50,22 +50,22 @@ output_dir = File.expand_path(RbConfig::
+@@ -50,22 +50,23 @@ output_dir = File.expand_path(RbConfig::
  grpc_lib_dir = File.join(output_dir, 'libs', grpc_config)
  ENV['BUILDDIR'] = output_dir
  
@@ -38,8 +38,11 @@
 +#$CFLAGS << ' -I' + File.join(grpc_root, 'include')
  
  ext_export_file = File.join(grpc_root, 'src', 'ruby', 'ext', 'grpc', 'ext-export')
- $LDFLAGS << ' -Wl,--version-script="' + ext_export_file + '.gcc"' if RUBY_PLATFORM =~ /linux/
- $LDFLAGS << ' -Wl,-exported_symbols_list,"' + ext_export_file + '.clang"' if RUBY_PLATFORM =~ /darwin/
+-$LDFLAGS << ' -Wl,--version-script="' + ext_export_file + '.gcc"' if RUBY_PLATFORM =~ /linux/
+-$LDFLAGS << ' -Wl,-exported_symbols_list,"' + ext_export_file + '.clang"' if RUBY_PLATFORM =~ /darwin/
++#$LDFLAGS << ' -Wl,--version-script="' + ext_export_file + '.gcc"' if RUBY_PLATFORM =~ /linux/
++#$LDFLAGS << ' -Wl,-exported_symbols_list,"' + ext_export_file + '.clang"' if RUBY_PLATFORM =~ /darwin/
++$LDFLAGS << ' -lgrpc' unless windows
  
 -$LDFLAGS << ' ' + File.join(grpc_lib_dir, 'libgrpc.a') unless windows
 +#$LDFLAGS << ' ' + File.join(grpc_lib_dir, 'libgrpc.a') unless windows
