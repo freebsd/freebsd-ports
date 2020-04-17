54f81f82919c2c89153c144c80dde31f80e8f6c4
Tobias Kortkamp <tobik@FreeBSD.org>
2020-04-17 10:53:18 +0000

    lang/rust-nightly: Update to 1.44.0.20200417
    - Add workaround to fix build when CC/CXX have "clang" in them [1]
    - Add patch to allow build with outdated libgit2 0.99.0
    
    Changes:	https://github.com/rust-lang/rust/compare/485c5fb6e1...7f3df57724
    PR:		238556 [1]

---
+++ lang/rust-nightly/files/patch-vendor_cc_src_lib.rs
@@ -0,0 +1,42 @@
+When the compiler has "clang" in its name the cc crate will pass
+an LLVM target triple to it.  Rust uses a triple that lacks the OS
+version and LLVM will default to FreeBSD 9 behavior, i.e., it will
+default to libstdc++ which is no longer available in newer releases.
+
+To avoid this issue assume we have a GNU compatible toolchain instead
+until LLVM can be updated to use libc++ by default.
+
+https://reviews.llvm.org/D77776
+
+--- vendor/cc/src/lib.rs.orig	2020-04-14 08:55:10 UTC
++++ vendor/cc/src/lib.rs
+@@ -2353,28 +2353,7 @@ impl Tool {
+     }
+ 
+     fn with_features(path: PathBuf, clang_driver: Option<&str>, cuda: bool) -> Self {
+-        // Try to detect family of the tool from its name, falling back to Gnu.
+-        let family = if let Some(fname) = path.file_name().and_then(|p| p.to_str()) {
+-            if fname.contains("clang-cl") {
+-                ToolFamily::Msvc { clang_cl: true }
+-            } else if fname.contains("cl")
+-                && !fname.contains("cloudabi")
+-                && !fname.contains("uclibc")
+-                && !fname.contains("clang")
+-            {
+-                ToolFamily::Msvc { clang_cl: false }
+-            } else if fname.contains("clang") {
+-                match clang_driver {
+-                    Some("cl") => ToolFamily::Msvc { clang_cl: true },
+-                    _ => ToolFamily::Clang,
+-                }
+-            } else {
+-                ToolFamily::Gnu
+-            }
+-        } else {
+-            ToolFamily::Gnu
+-        };
+-
++        let family = ToolFamily::Gnu;
+         Tool {
+             path: path,
+             cc_wrapper_path: None,
