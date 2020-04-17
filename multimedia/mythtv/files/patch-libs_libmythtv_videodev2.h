9aa37199261bf213752c9868b3d48ca5fac666d9
Hans Petter Selasky <hselasky@FreeBSD.org>
2020-04-17 08:35:59 +0000

    Fix build after LibV4L upgrade to v1.18.0.
    PR:		245501
    Approved by:	pi (implicit)

--- multimedia/mythtv/files/patch-libs_libmythtv_videodev2.h
+++ multimedia/mythtv/files/patch-libs_libmythtv_videodev2.h
@@ -1,34 +1,32 @@
---- libs/libmythtv/videodev2.h.orig      2018-01-11 12:39:22 UTC
+--- libs/libmythtv/videodev2.h.orig	2019-01-14 11:53:51 UTC
 +++ libs/libmythtv/videodev2.h
-@@ -60,6 +60,32 @@
- #ifdef __FreeBSD__
- #include <linux/input.h>	// For __[us][0-9]+ types
-+#ifndef __u64
-+typedef uint64_t __u64;
-+#endif
-+#ifndef __u32
-+typedef uint32_t __u32;
-+#endif
-+#ifndef __u16
-+typedef uint16_t __u16;
-+#endif
-+#ifndef __u8
-+typedef uint8_t __u8;
-+#endif
+@@ -53,25 +53,14 @@
+  *              Hans Verkuil <hverkuil@xs4all.nl>
+  *		et al.
+  */
+-#ifndef __LINUX_VIDEODEV2_H
 +
-+#ifndef __s64
-+typedef int64_t __s64;
-+#endif
-+#ifndef __s32
-+typedef int32_t __s32;
-+#endif
-+#ifndef __s16
-+typedef int16_t __s16;
-+#endif
-+#ifndef __s8
-+typedef int8_t __s8;
-+#endif
-+
- #define __le64 __u64
- #define __le32 __u32
- #define __le16 __u16
++#ifdef __FreeBSD__
++#include <linux/videodev2.h>
++#elif !defined(__LINUX_VIDEODEV2_H)
+ #define __LINUX_VIDEODEV2_H
+ 
+ #include <sys/time.h>
+ 
+-#ifdef __FreeBSD__
+-#include <linux/input.h>	// For __[us][0-9]+ types
+-#define __le64 __u64
+-#define __le32 __u32
+-#define __le16 __u16
+-#define __le8  __u8
+-#define __be64 __u64
+-#define __be32 __u32
+-#define __be16 __u16
+-#define __be8  __u8
+-#else
+-#include <linux/ioctl.h>
+-#include <linux/types.h>
+-#endif
+ #include <linux/v4l2-common.h>
+ #include <linux/v4l2-controls.h>
+ 
--- multimedia/w_scan/files/types.sed
+++
@@ -1,6 +0,0 @@
-/^#include <linux\/dvb\/frontend.h>/i\
-#include <stdint.h>
-s/^#include <linux\/types.h>/#include <stdint.h>/
-s/__u8/uint8_t/
-s/__u16/uint16_t/
-s/__u32/uint32_t/
