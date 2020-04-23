cb08bca15936aeeedd43ceb0371f94d7ca7c6a66
Tobias C. Berner <tcberner@FreeBSD.org>
2020-04-23 17:00:52 +0000

    Update KDE Applications (release-service) to 20.04
    KDE's April 2020 Apps Update
    
    A new bundle of KDE applications is here! In these releases, you can expect to
    find more features, stability improvements, and more user-friendly tools that
    will help you work more effectively.
    
    There are dozens of changes to look forward to in most of your favorite
    applications. Take Dolphin, for example. Windows Samba shares are now fully
    discoverable.
    
    On the topic of playing music: the Elisa music player is adding features by
    leaps and bounds. This release brings a new “Now Playing” view, easy
    accessibility through the system tray, and an option to minimize the playlist
    whenever you want. Thanks to the recently-added visual shuffle mode, it’s much
    easier to rearrange your music in the playlists.
    
    These are just the highlights of what’s new in KDE’s applications this month.
    Read on to find out about everything we’ve prepared for you.
    
    Announcement:
    	https://kde.org/announcements/releases/2020-04-apps-update/

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -62,8 +62,8 @@ KDE_FRAMEWORKS_VERSION?=	5.69.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 # Current KDE applications.
-KDE_APPLICATIONS_VERSION?=	19.12.3
-KDE_APPLICATIONS_SHLIB_VER?=	5.13.3
+KDE_APPLICATIONS_VERSION?=	20.04.0
+KDE_APPLICATIONS_SHLIB_VER?=	5.14.0
 KDE_APPLICATIONS_BRANCH?=	stable
 # Upstream moves old software to Attic/. Specify the newest applications release there.
 # Only the major version is used for the comparison.
@@ -669,7 +669,7 @@ kde-kontactinterface_PORT=	net/kontactinterface
 kde-kontactinterface_LIB=	libKF5KontactInterface.so
 
 kde-kpimdav_PORT=		net/kdav
-kde-kpimdav_LIB=		libKPimKDAV.so
+kde-kpimdav_LIB=		libKF5DAV.so
 
 kde-kpkpass_PORT=		security/kpkpass
 kde-kpkpass_LIB=		libKPimPkPass.so
