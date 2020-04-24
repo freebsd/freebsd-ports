58f92e48968427f318dc8ec745976fc6ca6febd3
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2020-04-24 17:06:33 +0000

    net-mgmt/centreon-clib: Update version 19.10.0=>20.04.0
    Relnotes:	https://github.com/centreon/centreon-clib/releases

--- net-mgmt/centreon-clib/files/patch-src_process__manager.cc
+++ net-mgmt/centreon-clib/files/patch-src_process__manager.cc
@@ -1,12 +1,12 @@
---- src/process_manager_posix.cc.orig	2019-10-25 18:23:17 UTC
-+++ src/process_manager_posix.cc
-@@ -22,6 +22,9 @@
+--- src/process_manager.cc.orig	2020-04-24 11:44:54 UTC
++++ src/process_manager.cc
+@@ -24,6 +24,9 @@
  #include <sys/types.h>
  #include <sys/wait.h>
  #include <unistd.h>
 +#if defined(__FreeBSD__) || defined (__DragonFly__)
 +#include <signal.h>
 +#endif
- #include "com/centreon/concurrency/locker.hh"
  #include "com/centreon/exceptions/basic.hh"
  #include "com/centreon/logging/logger.hh"
+ #include "com/centreon/process.hh"
