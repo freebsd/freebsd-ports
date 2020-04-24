5c17e9625960c4038f8a1e179b74232d9bf16e4b
Tobias Kortkamp <tobik@FreeBSD.org>
2020-04-24 04:45:57 +0000

    sysutils/fusefs-sandboxfs: Update to sandboxfs-0.2.0
    Changes:	https://github.com/bazelbuild/sandboxfs/releases/tag/sandboxfs-0.2.0

--- sysutils/fusefs-sandboxfs/files/patch-src_nodes_conv.rs
+++ sysutils/fusefs-sandboxfs/files/patch-src_nodes_conv.rs
@@ -6,7 +6,7 @@ error[E0308]: mismatched types
 72 |     Timespec::new(val.tv_sec() as sys::time::time_t, usec)
    |                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ expected i64, found i32
 
---- src/nodes/conv.rs.orig	2019-10-24 19:42:29 UTC
+--- src/nodes/conv.rs.orig	2020-04-20 18:46:46 UTC
 +++ src/nodes/conv.rs
 @@ -69,7 +69,7 @@ pub fn timeval_to_timespec(val: sys::time::TimeVal) ->
      } else {
@@ -16,8 +16,8 @@ error[E0308]: mismatched types
 +    Timespec::new((val.tv_sec() as sys::time::time_t).into(), usec)
  }
  
- /// Converts a file type as returned by the file system to a FUSE file type.
-@@ -313,6 +313,10 @@ mod tests {
+ /// Converts a `sys::time::TimeVal` object into a `sys::time::TimeSpec`.
+@@ -328,6 +328,10 @@ mod tests {
          // modified and may not be queryable, so stub them out.
          attr.ctime = BAD_TIME;
          attr.crtime = BAD_TIME;
@@ -28,7 +28,7 @@ error[E0308]: mismatched types
          assert!(fileattrs_eq(&exp_attr, &attr));
      }
  
-@@ -350,6 +354,10 @@ mod tests {
+@@ -365,6 +369,10 @@ mod tests {
          // modified and may not be queryable, so stub them out.
          attr.ctime = BAD_TIME;
          attr.crtime = BAD_TIME;
