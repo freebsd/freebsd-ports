0b47db8de09b2a68b9601d97fb718f61cb9fb7d6
Matthias Andree <mandree@FreeBSD.org>
2020-04-25 23:21:10 +0000

    New port: textproc/py-iso-639: Python library for ISO 639 standard
    PR:		241564
    Submitted by:	takefu@airport.fm (maintainer)

---
+++ textproc/py-iso-639/files/patch-setup.py
@@ -0,0 +1,18 @@
+--- setup.py.orig	2016-04-29 14:33:19 UTC
++++ setup.py
+@@ -32,13 +32,11 @@ setup(
+         'Operating System :: OS Independent',
+         'Programming Language :: Python',
+         'Programming Language :: Python :: 2',
+-        'Programming Language :: Python :: 2.6',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+-        'Programming Language :: Python :: 3.2',
+-        'Programming Language :: Python :: 3.3',
+-        'Programming Language :: Python :: 3.4',
+         'Programming Language :: Python :: 3.5',
++        'Programming Language :: Python :: 3.6',
++        'Programming Language :: Python :: 3.7',
+         'Programming Language :: Python :: Implementation :: CPython',
+         'Programming Language :: Python :: Implementation :: PyPy',
+         'Topic :: Text Processing :: Linguistic',
