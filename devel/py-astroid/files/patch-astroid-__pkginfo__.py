37fd3c5cf08aa2327f0aaa5d90b3b58e65e34faa
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-04-28 22:55:20 +0000

    Update to 2.4.0
    Changes:	https://github.com/PyCQA/astroid/blob/master/ChangeLog

--- devel/py-astroid/files/patch-astroid-__pkginfo__.py
+++ devel/py-astroid/files/patch-astroid-__pkginfo__.py
@@ -1,10 +1,10 @@
---- astroid/__pkginfo__.py.orig	2019-11-06 18:41:21 UTC
+--- astroid/__pkginfo__.py.orig	2020-04-27 09:43:18 UTC
 +++ astroid/__pkginfo__.py
-@@ -24,7 +24,7 @@ extras_require = {}
+@@ -28,7 +28,7 @@ extras_require = {}
  install_requires = [
      "lazy_object_proxy==1.4.*",
      "six~=1.12",
--    "wrapt==1.11.*",
+-    "wrapt~=1.11",
 +    "wrapt>=1.11.0,<1.13",
      'typed-ast>=1.4.0,<1.5;implementation_name== "cpython" and python_version<"3.8"',
  ]
