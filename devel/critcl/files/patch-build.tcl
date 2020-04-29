e0f76f72639b719855f0c37fd26a6486db4b3227
Pietro Cerutti <gahr@FreeBSD.org>
2020-04-29 10:00:26 +0000

    devel/critcl: better fix for build-as-user

--- devel/critcl/files/patch-build.tcl
+++ devel/critcl/files/patch-build.tcl
@@ -1,6 +1,14 @@
---- build.tcl.orig	2020-02-19 04:07:06 UTC
-+++ build.tcl
-@@ -507,6 +507,7 @@ proc _install {args} {
+--- build.tcl.orig	2020-02-19 04:07:06.000000000 +0000
++++ build.tcl	2020-04-29 09:52:24.419446000 +0000
+@@ -507,6 +507,7 @@
+ 	lappend cmd exec >@ stdout 2>@ stderr
+ 	lappend cmd [info nameofexecutable]
+ 	lappend cmd $theapp
++	lappend cmd -cache [file join $selfdir cache]
+ 	if {$target ne {}} {
+ 	    lappend cmd -target $target
+ 	}
+@@ -541,6 +542,7 @@
  	lappend cmd exec >@ stdout 2>@ stderr
  	lappend cmd [info nameofexecutable]
  	lappend cmd $theapp
