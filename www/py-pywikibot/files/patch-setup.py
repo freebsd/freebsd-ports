47cea69cc5633c2ebe57e8bd2ad09d104fb88358
Yuri Victorovich <yuri@FreeBSD.org>
2020-04-29 19:06:22 +0000

    www/py-pywikibot: Update 3.0.20190722 -> 3.0.20200405

--- www/py-pywikibot/files/patch-setup.py
+++ www/py-pywikibot/files/patch-setup.py
@@ -1,31 +1,41 @@
---- setup.py.orig	2018-10-20 16:02:34 UTC
+--- setup.py.orig	2020-04-29 18:50:22 UTC
 +++ setup.py
-@@ -190,17 +190,17 @@ else:
- def get_version():
-     """Get a valid pywikibot module version string."""
+@@ -168,22 +168,22 @@ def get_version(name):
+     @rtype: str
+     """
      version = '3.0'
 -    try:
 -        import subprocess
 -        date = subprocess.check_output(
 -            ['git', 'log', '-1', '--format=%ci']).strip()
--        date = date.decode().split(' ')[0].replace('-', '')
+-        date = date.decode().split(' ', 1)[0].replace('-', '')
 -        version += '.' + date
 -        if 'sdist' not in sys.argv:
 -            version += '.dev0'
 -    except Exception as e:
 -        print(e)
--        version += '.dev0'
+-        from pkg_resources import get_distribution, DistributionNotFound
+-        try:
+-            version = get_distribution(name).version
+-        except DistributionNotFound as e:
+-            print(e)
+-            version += '.dev0'
 +    #try:
 +    #    import subprocess
 +    #    date = subprocess.check_output(
 +    #        ['git', 'log', '-1', '--format=%ci']).strip()
-+    #    date = date.decode().split(' ')[0].replace('-', '')
++    #    date = date.decode().split(' ', 1)[0].replace('-', '')
 +    #    version += '.' + date
 +    #    if 'sdist' not in sys.argv:
 +    #        version += '.dev0'
 +    #except Exception as e:
 +    #    print(e)
-+    #    version += '.dev0'
++    #    from pkg_resources import get_distribution, DistributionNotFound
++    #    try:
++    #        version = get_distribution(name).version
++    #    except DistributionNotFound as e:
++    #        print(e)
++    #        version += '.dev0'
      return version
  
  
