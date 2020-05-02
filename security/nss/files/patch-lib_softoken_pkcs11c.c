9cd0a807cf46e524953f5f30e681aee4befdc982
Jan Beich <jbeich@FreeBSD.org>
2020-05-02 00:54:30 +0000

    security/nss: update to 3.52
    Changes:	https://developer.mozilla.org/docs/Mozilla/Projects/NSS/NSS_3.52_release_notes
    Changes:	https://hg.mozilla.org/projects/nss/shortlog/NSS_3_52_RTM
    ABI:		https://abi-laboratory.pro/tracker/timeline/nss/

--- security/nss/files/patch-lib_softoken_pkcs11c.c
+++ security/nss/files/patch-lib_softoken_pkcs11c.c
@@ -1,20 +1,20 @@
---- lib/softoken/pkcs11c.c.orig	2018-08-31 12:55:53 UTC
+--- lib/softoken/pkcs11c.c.orig	2020-05-01 21:08:55 UTC
 +++ lib/softoken/pkcs11c.c
-@@ -5772,9 +5772,6 @@ sftk_unwrapPrivateKey(SFTKObject *key, SECItem *bpki)
+@@ -6106,9 +6106,6 @@ sftk_unwrapPrivateKey(SFTKObject *key, SECItem *bpki)
              break;
          case NSSLOWKEYDSAKey:
              keyType = CKK_DSA;
--            crv = (sftk_hasAttribute(key, CKA_NETSCAPE_DB)) ? CKR_OK : CKR_KEY_TYPE_INCONSISTENT;
+-            crv = (sftk_hasAttribute(key, CKA_NSS_DB)) ? CKR_OK : CKR_KEY_TYPE_INCONSISTENT;
 -            if (crv != CKR_OK)
 -                break;
              crv = sftk_AddAttributeType(key, CKA_KEY_TYPE, &keyType,
                                          sizeof(keyType));
              if (crv != CKR_OK)
-@@ -5814,9 +5811,6 @@ sftk_unwrapPrivateKey(SFTKObject *key, SECItem *bpki)
+@@ -6148,9 +6145,6 @@ sftk_unwrapPrivateKey(SFTKObject *key, SECItem *bpki)
          /* what about fortezza??? */
          case NSSLOWKEYECKey:
              keyType = CKK_EC;
--            crv = (sftk_hasAttribute(key, CKA_NETSCAPE_DB)) ? CKR_OK : CKR_KEY_TYPE_INCONSISTENT;
+-            crv = (sftk_hasAttribute(key, CKA_NSS_DB)) ? CKR_OK : CKR_KEY_TYPE_INCONSISTENT;
 -            if (crv != CKR_OK)
 -                break;
              crv = sftk_AddAttributeType(key, CKA_KEY_TYPE, &keyType,
