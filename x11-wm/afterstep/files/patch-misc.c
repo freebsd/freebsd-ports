541e9f271b61c7208c16ae2470e634d0cde8cc10
Dirk Meyer <dinoex@FreeBSD.org>
2020-05-04 19:38:50 +0000

    - fix build with -fno-common

---
+++ x11-wm/afterstep/files/patch-misc.c
@@ -0,0 +1,11 @@
+--- afterstep/misc.c.orig	1997-04-26 12:21:37 UTC
++++ afterstep/misc.c
+@@ -58,8 +58,6 @@
+ #include "screen.h"
+ #include "module.h"
+ 
+-XGCValues Globalgcv;
+-unsigned long Globalgcm;
+ 
+ 
+ /**************************************************************************
