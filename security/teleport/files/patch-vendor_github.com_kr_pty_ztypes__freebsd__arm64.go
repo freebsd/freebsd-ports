a7122e3c853d6361f47d2a34f2d98229562efb03
Mikael Urankar <mikael@FreeBSD.org>
2020-05-05 16:25:55 +0000

    security/teleport: update to 4.2.8
     - update to 4.2.8
     - fix build on aarch64
    
    PR:		245670 245940

---
+++ security/teleport/files/patch-vendor_github.com_kr_pty_ztypes__freebsd__arm64.go
@@ -0,0 +1,51 @@
+--- vendor/github.com/kr/pty/ztypes_freebsd_arm64.go.orig	2019-12-23 19:57:30 UTC
++++ vendor/github.com/kr/pty/ztypes_freebsd_arm64.go
+@@ -0,0 +1,13 @@
++// Created by cgo -godefs - DO NOT EDIT
++// cgo -godefs types_freebsd.go
++
++package pty
++
++const (
++	_C_SPECNAMELEN = 0x3f
++)
++
++type fiodgnameArg struct {
++	Len       int32
++	Buf       *byte
++}
+
+
+--- ../src/github.com/gravitational/teleport/vendor/github.com/kr/pty/ztypes_freebsd_arm64.go.orig	2019-12-23 19:57:30 UTC
++++ ../src/github.com/gravitational/teleport/vendor/github.com/kr/pty/ztypes_freebsd_arm64.go
+@@ -0,0 +1,13 @@
++// Created by cgo -godefs - DO NOT EDIT
++// cgo -godefs types_freebsd.go
++
++package pty
++
++const (
++	_C_SPECNAMELEN = 0x3f
++)
++
++type fiodgnameArg struct {
++	Len       int32
++	Buf       *byte
++}
+
+--- ../src/golang.org/x/sys/unix/ztypes_freebsd_arm64.go.orig	2019-12-23 19:57:30 UTC
++++ ../src/golang.org/x/sys/unix/ztypes_freebsd_arm64.go
+@@ -0,0 +1,13 @@
++// Created by cgo -godefs - DO NOT EDIT
++// cgo -godefs types_freebsd.go
++
++package pty
++
++const (
++	_C_SPECNAMELEN = 0x3f
++)
++
++type fiodgnameArg struct {
++	Len       int32
++	Buf       *byte
++}
