2444cc556b98e1e8068bb778c0d2a5a3df82ec45
Hiroki Tagato <tagattie@FreeBSD.org>
2020-05-06 05:48:16 +0000

    - Update to 4.3.0 - Add LICENSE_FILE - Create directory for sockets and pidfile - Update pkg-descr with a more descriptive text from website
    PR:		244657
    Submitted by:	yds <yds@Necessitu.de>
    Approved by:	tremere@cainites.net (maintainer), ehaupt (mentor)
    Changelog:	https://blog.powerdns.com/2020/03/03/powerdns-recursor-4-3-0-released/

--- dns/powerdns-recursor/files/patch-dnsname.hh
+++ dns/powerdns-recursor/files/patch-dnsname.hh
@@ -1,6 +1,6 @@
---- dnsname.hh.orig	2019-01-31 19:43:44 UTC
-+++ dnsname.hh
-@@ -30,7 +30,7 @@
+--- dnsname.hh.orig	2020-03-02 07:49:54.000000000 -0500
++++ dnsname.hh	2020-03-02 07:49:54.000000000 -0500
+@@ -33,7 +33,7 @@
  #include <boost/version.hpp>
  
  // it crashes on OSX and doesn't compile on OpenBSD
@@ -9,7 +9,7 @@
  #include <boost/container/string.hpp>
  #endif
  
-@@ -135,7 +135,7 @@ class DNSName (public)
+@@ -138,7 +138,7 @@
    inline bool canonCompare(const DNSName& rhs) const;
    bool slowCanonCompare(const DNSName& rhs) const;  
  
