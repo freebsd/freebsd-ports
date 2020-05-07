bf1573f316a97c72448d8c1a4b1b9eba6fa90c3f
Antoine Brodin <antoine@FreeBSD.org>
2020-05-07 21:09:25 +0000

    Revert r534292, this was not properly tested
    With hat:	portmgr

--- math/py-matplotlib/files/patch-setup.py
+++ math/py-matplotlib/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2020-03-18 07:54:08 UTC
+--- setup.py.orig	2019-02-26 00:18:32 UTC
 +++ setup.py
-@@ -165,7 +165,6 @@ class develop_with_jquery(DevelopCommand):
+@@ -189,7 +189,6 @@ class develop_with_jquery(DevelopCommand
  
  
  cmdclass['sdist'] = sdist_with_jquery
