bc3bbaabb09b330f47e7cec28c7751c99f95f46a
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-05-08 20:53:46 +0000

    Update to 2.9
    Changes:	https://docs.wagtail.io/en/latest/releases/index.html
    		https://github.com/wagtail/wagtail/releases

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,13 +1,11 @@
 --- setup.py.orig	2020-04-14 08:29:39 UTC
 +++ setup.py
-@@ -26,8 +26,8 @@ install_requires = [
+@@ -26,7 +26,7 @@ install_requires = [
      "django-taggit>=1.0,<2.0",
      "django-treebeard>=4.2.0,<5.0",
      "djangorestframework>=3.7.4,<4.0",
 -    "draftjs_exporter>=2.1.5,<3.0",
--    "Pillow>=4.0.0,<7.0.0",
 +    "draftjs_exporter>=2.1.5",
-+    "Pillow>=4.0.0,<8.0.0",
-     "beautifulsoup4>=4.5.1,<4.6.1",
+     "Pillow>=4.0.0,<8.0.0",
+     "beautifulsoup4>=4.8,<4.9",
      "html5lib>=0.999,<2",
-     "Unidecode>=0.04.14,<2.0",
