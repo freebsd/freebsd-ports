be7d93cd66b4ef5c0010642618cf28be0ac81361
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2020-05-08 05:35:11 +0000

    smath/cgal: Update to 5.0.2
    The new version of CGAL brings improvements and a lot of changes, including 2 major ones [1](header only[2] and require c++14) requiring to patch all software depending on it.
    
    [1] https://www.cgal.org/2019/11/08/cgal50/
    [2] https://doc.cgal.org/latest/Manual/installation.html
    
    math/cgal:
    
       - Update to 5.0.2
    
    databases/sfcgal:
    
       - Fix with cgal5 : use c++14 and add gmpxx link
    
    databases/postgis*:
    
       - bump portversion needed due to cgal and sfcgal changes
    
    cad/openscad:
    
       - remove LIB_DEPENDS and add BUILD_DEPENDS
       - Use c++14
    
    cad/openscad-devel:
    
       - remove LIB_DEPENDS and add BUILD_DEPENDS
       - Use c++14
    
    games/flightgear-terragear:
    
       - remove LIB_DEPENDS and add BUILD_DEPENDS
       - fix Boost error (ld: error: undefined symbol: boost::thread::hardware_concurrency())
    
    graphics/colmap:
    
       - remove LIB_DEPENDS and add BUILD_DEPENDS
       - fix CMAKE_CXX_FLAGS with c++14
    
    PR:		245824
    Approved by:	wen, jbeich, tcberner (mentor)
    Differential Revision:	D24578

---
+++ databases/sfcgal/files/patch-src_config.h.cmake
@@ -0,0 +1,10 @@
+--- src/config.h.cmake.orig	2019-06-03 10:11:36 UTC
++++ src/config.h.cmake
+@@ -21,7 +21,6 @@
+ #define _SFCGAL_CONFIG_H_
+ 
+ #define CGAL_DO_NOT_USE_BOOST_MP 1
+-#define CGAL_DO_NOT_USE_GMPXX 1
+ 
+ #include <SFCGAL/export.h>
+ 
