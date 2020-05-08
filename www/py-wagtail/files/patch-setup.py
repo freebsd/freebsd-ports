3c25e54bd4f2e3f611047c4e22a75d71dddee808
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-05-08 08:50:16 +0000

    Convert REINPLACE_CMD to patch file

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,10 +1,12 @@
---- setup.py.orig	2020-01-08 11:30:31 UTC
+--- setup.py.orig	2020-04-14 08:29:39 UTC
 +++ setup.py
-@@ -27,7 +27,7 @@ install_requires = [
+@@ -26,8 +26,8 @@ install_requires = [
+     "django-taggit>=1.0,<2.0",
      "django-treebeard>=4.2.0,<5.0",
      "djangorestframework>=3.7.4,<4.0",
-     "draftjs_exporter>=2.1.5,<3.0",
+-    "draftjs_exporter>=2.1.5,<3.0",
 -    "Pillow>=4.0.0,<7.0.0",
++    "draftjs_exporter>=2.1.5",
 +    "Pillow>=4.0.0,<8.0.0",
      "beautifulsoup4>=4.5.1,<4.6.1",
      "html5lib>=0.999,<2",
