2f8c589c819928570c31af5c12b8a855e46a7d2b
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-05-12 22:52:43 +0000

    games/fs2open: fix build
    Build with Clang 10 fails.

--- games/fs2open/files/patch-code_model_modelread.cpp
+++ games/fs2open/files/patch-code_model_modelread.cpp
@@ -1,4 +1,4 @@
---- code/model/modelread.cpp.orig	2020-02-10 13:38:06 UTC
+--- code/model/modelread.cpp.orig	2020-01-25 16:45:09 UTC
 +++ code/model/modelread.cpp
 @@ -5341,7 +5341,7 @@ void swap_bsp_sortnorms( polymodel * pm, ubyte * p )
  }
@@ -9,7 +9,13 @@
  {
  #if BYTE_ORDER == BIG_ENDIAN
  	ubyte *p = (ubyte *)model_ptr;
-@@ -5397,7 +5397,7 @@ void swap_bsp_data( polymodel *  /*pm*/, void * /*mode
+@@ -5394,10 +5394,13 @@ void swap_bsp_data( polymodel *  /*pm*/, void * /*mode
+ 	}
+ 
+ 	return;
++#else
++(void)pm;
++(void)model_ptr;
  #endif
  }
  
@@ -18,3 +24,12 @@
  {
  #if BYTE_ORDER == BIG_ENDIAN
  	char *type_p = (char *)(buffer);
+@@ -5439,6 +5442,8 @@ void swap_sldc_data(ubyte * /*buffer*/)
+ 			shld_polys[i] = INTEL_INT(shld_polys[i]);
+ 		}			
+ 	}
++#else
++(void)buffer;
+ #endif
+ }
+ 
