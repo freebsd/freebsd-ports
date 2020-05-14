7d7a2b69382e8a4b549924befe45826b840a6bb3
Yuri Victorovich <yuri@FreeBSD.org>
2020-05-14 01:05:34 +0000

    New port: science/ergo: Quantum chemistry program for large-scale atomic calculations

---
+++ science/ergo/files/patch-source_dft_fun-tester.cc
@@ -0,0 +1,11 @@
+--- source/dft/fun-tester.cc.orig	2020-05-14 00:03:22 UTC
++++ source/dft/fun-tester.cc
+@@ -42,7 +42,7 @@
+    the TEST_BUILD. 
+  */
+ 
+-#define _POSIX_SOURCE 1
++//#define _POSIX_SOURCE 1
+ 
+ #define __CVERSION__
+ #include <cmath>
