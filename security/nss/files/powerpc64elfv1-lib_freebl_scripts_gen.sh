fb69e0909c3ac160f6bb35be8a9a95ddf9f1ebd1
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-05-15 18:31:16 +0000

    MFH: r535312
    security/nss: fix build on powerpc and powerpc64 with gcc
    
    PR:             246419
    Approved by:    jbeich (maintainer)
    
    Approved by:	portmgr (fix build blanket)

---
+++ security/nss/files/powerpc64elfv1-lib_freebl_scripts_gen.sh
@@ -0,0 +1,11 @@
+--- lib/freebl/scripts/gen.sh.orig	2020-05-12 17:17:31 UTC
++++ lib/freebl/scripts/gen.sh
+@@ -3,7 +3,7 @@
+ # Run the scripts in this folder, generating the assembly,
+ #
+ 
+-perl sha512p8-ppc.pl linux64le sha512-p8.s
++perl sha512p8-ppc.pl linux64 sha512-p8.s
+ 
+ # Add the license mention
+ cat > hdr << "EOF"
