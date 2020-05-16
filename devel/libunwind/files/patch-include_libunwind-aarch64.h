20dc92cfa1f72daf407e88b0cc2db22018fbfd3c
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-05-16 10:51:12 +0000

    Update to 20200331 (1.4.0)
    - Update MASTER_SITES
    
    Changes:	https://github.com/libunwind/libunwind/releases
    PR:		246390
    Exp-run by:	antoine

--- devel/libunwind/files/patch-include_libunwind-aarch64.h
+++ devel/libunwind/files/patch-include_libunwind-aarch64.h
@@ -1,11 +1,11 @@
---- include/libunwind-aarch64.h.orig	2018-06-15 23:58:09 UTC
+--- include/libunwind-aarch64.h.orig	2018-12-20 18:10:32 UTC
 +++ include/libunwind-aarch64.h
-@@ -177,7 +177,7 @@ typedef ucontext_t unw_tdep_context_t;
+@@ -212,7 +212,7 @@ typedef struct
  
  #define unw_tdep_getcontext(uc) (({					\
    unw_tdep_context_t *unw_ctx = (uc);					\
--  register uint64_t *unw_base asm ("x0") = (uint64_t*) unw_ctx->uc_mcontext.regs;		\
-+  register uint64_t *unw_base asm ("x0") = (uint64_t*) unw_ctx->uc_mcontext.mc_gpregs.gp_x;		\
+-  register uint64_t *unw_base __asm__ ("x0") = (uint64_t*) unw_ctx->uc_mcontext.regs;		\
++  register uint64_t *unw_base __asm__ ("x0") = (uint64_t*) unw_ctx->uc_mcontext.mc_gpregs.gp_x;		\
    __asm__ __volatile__ (						\
       "stp x0, x1, [%[base], #0]\n" \
       "stp x2, x3, [%[base], #16]\n" \
--- devel/libunwind/files/patch-src_aarch64_Ginit.c
+++ devel/libunwind/files/patch-src_aarch64_Ginit.c
@@ -1,11 +1,11 @@
---- src/aarch64/Ginit.c.orig	2018-06-15 23:58:20 UTC
+--- src/aarch64/Ginit.c.orig	2020-03-31 15:35:32 UTC
 +++ src/aarch64/Ginit.c
 @@ -44,7 +44,7 @@ static inline void *
- uc_addr (ucontext_t *uc, int reg)
+ uc_addr (unw_tdep_context_t *uc, int reg)
  {
-   if (reg >= UNW_AARCH64_X0 && reg <= UNW_AARCH64_V31)
+   if (reg >= UNW_AARCH64_X0 && reg < UNW_AARCH64_V0)
 -    return &uc->uc_mcontext.regs[reg];
 +    return &uc->uc_mcontext.mc_gpregs.gp_x[reg];
+   else if (reg >= UNW_AARCH64_V0 && reg <= UNW_AARCH64_V31)
+     return &GET_FPCTX(uc)->vregs[reg - UNW_AARCH64_V0];
    else
-     return NULL;
- }
