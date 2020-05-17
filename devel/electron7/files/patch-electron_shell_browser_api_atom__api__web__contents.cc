260e9bf9e625ab5c72ef62352078336cf415cbb6
Hiroki Tagato <tagattie@FreeBSD.org>
2020-05-17 02:27:09 +0000

    Update to 7.3.0
    Approved by:	ehaupt (mentor)
    Changelog:	https://github.com/electron/electron/releases/tag/v7.3.0

--- devel/electron7/files/patch-electron_shell_browser_api_atom__api__web__contents.cc
+++ devel/electron7/files/patch-electron_shell_browser_api_atom__api__web__contents.cc
@@ -1,6 +1,6 @@
---- electron/shell/browser/api/atom_api_web_contents.cc.orig	2020-04-14 14:59:21 UTC
+--- electron/shell/browser/api/atom_api_web_contents.cc.orig	2020-05-15 00:25:58 UTC
 +++ electron/shell/browser/api/atom_api_web_contents.cc
-@@ -105,11 +105,11 @@
+@@ -106,11 +106,11 @@
  #include "ui/base/cocoa/defaults_utils.h"
  #endif
  
@@ -14,16 +14,16 @@
  #include "third_party/blink/public/mojom/renderer_preferences.mojom.h"
  #include "ui/gfx/font_render_params.h"
  #endif
-@@ -483,7 +483,7 @@ void WebContents::InitWithSessionAndOptions(
-   auto* prefs = web_contents()->GetMutableRendererPrefs();
-   prefs->accept_languages = g_browser_process->GetApplicationLocale();
+@@ -499,7 +499,7 @@ void WebContents::InitWithSessionAndOptions(
+   accept_languages.pop_back();
+   prefs->accept_languages = accept_languages;
  
 -#if defined(OS_LINUX) || defined(OS_WIN)
 +#if defined(OS_LINUX) || defined(OS_WIN) || defined(OS_BSD)
    // Update font settings.
    static const base::NoDestructor<gfx::FontRenderParams> params(
        gfx::GetFontRenderParams(gfx::FontRenderParamsQuery(), nullptr));
-@@ -500,7 +500,7 @@ void WebContents::InitWithSessionAndOptions(
+@@ -516,7 +516,7 @@ void WebContents::InitWithSessionAndOptions(
    base::TimeDelta interval;
    if (ui::TextInsertionCaretBlinkPeriod(&interval))
      prefs->caret_blink_interval = interval;
