df7aee1b6c062f877c274e41133393bf9151d498
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-18 14:47:59 +0000

    net-p2p/qtum: prepare for Qt5-5.15

---
+++ net-p2p/qtum/files/patch-src_qt_splashscreen.cpp
@@ -0,0 +1,10 @@
+--- src/qt/splashscreen.cpp.orig	2020-05-18 14:21:32 UTC
++++ src/qt/splashscreen.cpp
+@@ -24,6 +24,7 @@
+ #include <QApplication>
+ #include <QCloseEvent>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QRadialGradient>
+ #include <QScreen>
+ 
---
+++ net-p2p/qtum/files/patch-src_qt_trafficgraphwidget.cpp
@@ -0,0 +1,10 @@
+--- src/qt/trafficgraphwidget.cpp.orig	2020-05-18 14:24:29 UTC
++++ src/qt/trafficgraphwidget.cpp
+@@ -7,6 +7,7 @@
+ #include <qt/clientmodel.h>
+ 
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QColor>
+ #include <QTimer>
+ 
