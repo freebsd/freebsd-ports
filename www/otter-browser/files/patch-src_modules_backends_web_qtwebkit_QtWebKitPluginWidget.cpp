6de53f8dec7efb9c025a0af12c6fa1838efa8bd6
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-18 16:42:00 +0000

    www/otter-browser: prepare for Qt5-5.15

---
+++ www/otter-browser/files/patch-src_modules_backends_web_qtwebkit_QtWebKitPluginWidget.cpp
@@ -0,0 +1,10 @@
+--- src/modules/backends/web/qtwebkit/QtWebKitPluginWidget.cpp.orig	2020-05-18 16:31:44 UTC
++++ src/modules/backends/web/qtwebkit/QtWebKitPluginWidget.cpp
+@@ -22,6 +22,7 @@
+ 
+ #include <QtGui/QMouseEvent>
+ #include <QtGui/QPainter>
++#include <QtGui/QPainterPath>
+ 
+ namespace Otter
+ {
---
+++ www/otter-browser/files/patch-src_modules_windows_web_StartPageWidget.cpp
@@ -0,0 +1,10 @@
+--- src/modules/windows/web/StartPageWidget.cpp.orig	2020-05-18 16:31:40 UTC
++++ src/modules/windows/web/StartPageWidget.cpp
+@@ -41,6 +41,7 @@
+ #include <QtGui/QGuiApplication>
+ #include <QtGui/QMouseEvent>
+ #include <QtGui/QPainter>
++#include <QtGui/QPainterPath>
+ #include <QtGui/QPixmapCache>
+ #include <QtWidgets/QDesktopWidget>
+ #include <QtWidgets/QGridLayout>
