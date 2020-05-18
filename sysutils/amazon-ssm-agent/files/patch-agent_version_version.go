45bbfd169712351dd63068125ee263c1df429778
Oleksii Samorukov <samm@FreeBSD.org>
2020-05-18 22:27:17 +0000

    sysutils/amazon-ssm-agent: update to 2.3.1205.0
    Changelog - https://github.com/aws/amazon-ssm-agent/releases
    
    Approved by: cperciva (maintainer)
    Differential Revision: https://reviews.freebsd.org/D24872

--- sysutils/amazon-ssm-agent/files/patch-agent_version_version.go
+++
@@ -1,8 +0,0 @@
---- agent/version/version.go.orig	2019-05-08 20:19:03 UTC
-+++ agent/version/version.go
-@@ -19,4 +19,4 @@
- package version
- 
- // Version is the version of the Agent
--const Version = "2.3.0.0"
-+const Version = "2.3.612.0"
