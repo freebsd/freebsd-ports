2e3e4653f7f5a1a9826408717e9cda2c5c00c1f9
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-18 16:42:25 +0000

    science/kst2: prepare for Qt5-5.15

---
+++ science/kst2/files/patch-src_widgets_gradienteditor.h
@@ -0,0 +1,10 @@
+--- src/widgets/gradienteditor.h.orig	2020-05-18 16:26:46 UTC
++++ src/widgets/gradienteditor.h
+@@ -16,6 +16,7 @@
+ #include <QHash>
+ #include <QWidget>
+ #include <QGradient>
++#include <QPainterPath>
+ 
+ #include "kstwidgets_export.h"
+ 
