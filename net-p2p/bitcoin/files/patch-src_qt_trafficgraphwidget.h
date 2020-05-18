53c504f813248633b1458701849321c2bc8340e6
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-18 18:11:48 +0000

    net-p2p/bitcoin: prepare for Qt5-5.15

---
+++ net-p2p/bitcoin/files/patch-src_qt_trafficgraphwidget.h
@@ -0,0 +1,10 @@
+--- src/qt/trafficgraphwidget.h.orig	2020-05-18 18:05:29 UTC
++++ src/qt/trafficgraphwidget.h
+@@ -7,6 +7,7 @@
+ 
+ #include <QWidget>
+ #include <QQueue>
++#include <QPainterPath>
+ 
+ class ClientModel;
+ 
