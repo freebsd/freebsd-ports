1a2924f406b28a92383e22a04502da9436673668
Kurt Jaeger <pi@FreeBSD.org>
2020-05-18 09:09:06 +0000

    www/squid: add patch to fix kerberos_ldap_group helper, fix pinger
    - add patch to fix kerberos_ldap_group helper work with heimdal
    - regenerate two patches to follow upstreamed versions
    - fix pinger permissions
    
    PR:		245861, 246410
    Submitted by:	Pavel Timofeev <timp87@gmail.com> (maintainer)

--- www/squid/files/patch-src_acl_external_eDirectory__userip_ext__edirectory__userip__acl.cc
+++ www/squid/files/patch-src_acl_external_eDirectory__userip_ext__edirectory__userip__acl.cc
@@ -1,12 +1,15 @@
 --- src/acl/external/eDirectory_userip/ext_edirectory_userip_acl.cc.orig	2020-04-19 12:38:51 UTC
 +++ src/acl/external/eDirectory_userip/ext_edirectory_userip_acl.cc
-@@ -69,6 +69,9 @@
+@@ -69,6 +69,12 @@
  #ifdef HAVE_NETDB_H
  #include <netdb.h>
  #endif
-+#include <sys/types.h>
++#ifdef HAVE_SYS_SOCKET_H
 +#include <sys/socket.h>
++#endif
++#ifdef HAVE_NETINET_IN_H
 +#include <netinet/in.h>
++#endif
  
  #ifdef HELPER_INPUT_BUFFER
  #define EDUI_MAXLEN     HELPER_INPUT_BUFFER
