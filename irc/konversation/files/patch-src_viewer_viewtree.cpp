f0c3c04d18c0f2cdde7b27a3924d6b57c649da27
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-18 15:34:54 +0000

    irc/konversation: prepare for Qt5-5.15

---
+++ irc/konversation/files/patch-src_viewer_viewtree.cpp
@@ -0,0 +1,10 @@
+--- src/viewer/viewtree.cpp.orig	2020-05-18 15:18:30 UTC
++++ src/viewer/viewtree.cpp
+@@ -21,6 +21,7 @@
+ #include <QFontDatabase>
+ #include <QGuiApplication>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QItemSelectionModel>
+ #include <QStyleHints>
+ #include <QToolTip>
