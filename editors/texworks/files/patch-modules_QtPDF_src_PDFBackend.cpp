a3ba8a0170291f0afe7d348be9cf661f0e270ed2
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-18 16:22:53 +0000

    editors/texworks: prepare for Qt5-5.15

---
+++ editors/texworks/files/patch-modules_QtPDF_src_PDFBackend.cpp
@@ -0,0 +1,10 @@
+--- modules/QtPDF/src/PDFBackend.cpp.orig	2020-05-18 16:06:39 UTC
++++ modules/QtPDF/src/PDFBackend.cpp
+@@ -14,6 +14,7 @@
+ 
+ #include <PDFBackend.h>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QApplication>
+ 
+ namespace QtPDF {
