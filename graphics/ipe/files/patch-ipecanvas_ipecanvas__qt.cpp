8ed8a4fd33265d0ac29085cfb321d464177c235b
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-18 16:04:44 +0000

    graphics/ipe: prepare for Qt5-5.15

---
+++ graphics/ipe/files/patch-ipecanvas_ipecanvas__qt.cpp
@@ -0,0 +1,10 @@
+--- ipecanvas/ipecanvas_qt.cpp.orig	2020-05-18 15:58:39 UTC
++++ ipecanvas/ipecanvas_qt.cpp
+@@ -36,6 +36,7 @@
+ 
+ #include <QPainter>
+ #include <QPaintEvent>
++#include <QPainterPath>
+ 
+ using namespace ipe;
+ 
