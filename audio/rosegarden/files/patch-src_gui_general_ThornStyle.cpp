2681c607a37f45a36cefeb03ed7cbd2573fa79cf
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-18 18:21:51 +0000

    audio/rosegarden: prepare for Qt5-5.15

---
+++ audio/rosegarden/files/patch-src_gui_general_ThornStyle.cpp
@@ -0,0 +1,10 @@
+--- src/gui/general/ThornStyle.cpp.orig	2020-05-18 16:28:59 UTC
++++ src/gui/general/ThornStyle.cpp
+@@ -34,6 +34,7 @@
+ #include <QLabel>
+ #include <QLayout>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QRadioButton>
+ #include <QStyleFactory>
+ #include <QStyleOption>
---
+++ audio/rosegarden/files/patch-src_gui_widgets_Panner.cpp
@@ -0,0 +1,10 @@
+--- src/gui/widgets/Panner.cpp.orig	2020-05-18 16:29:03 UTC
++++ src/gui/widgets/Panner.cpp
+@@ -26,6 +26,7 @@
+ 
+ #include <QPolygon>
+ #include <QMouseEvent>
++#include <QPainterPath>
+ 
+ #include <iostream>
+ 
