4f1b5b292cdcdcc653f3c450b874425241e53d64
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-19 18:52:07 +0000

    emulators/dolphpin-emu: prepare for Qt5-5.15

---
+++ emulators/dolphin-emu/files/patch-Source_Core_DolphinQt_Config_Mapping_MappingIndicator.cpp
@@ -0,0 +1,10 @@
+--- Source/Core/DolphinQt/Config/Mapping/MappingIndicator.cpp.orig	2020-05-19 18:47:30 UTC
++++ Source/Core/DolphinQt/Config/Mapping/MappingIndicator.cpp
+@@ -11,6 +11,7 @@
+ #include <QAction>
+ #include <QDateTime>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QTimer>
+ 
+ #include "Common/MathUtil.h"
