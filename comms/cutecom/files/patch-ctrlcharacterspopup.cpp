f481298a500abe39381a90ed424bde8f6ef59fda
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-19 18:06:11 +0000

    comms/cutecom: prepare for Qt5-5.15

---
+++ comms/cutecom/files/patch-ctrlcharacterspopup.cpp
@@ -0,0 +1,10 @@
+--- ctrlcharacterspopup.cpp.orig	2020-05-19 17:54:37 UTC
++++ ctrlcharacterspopup.cpp
+@@ -23,6 +23,7 @@
+ #include <QApplication>
+ #include <QKeyEvent>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QPoint>
+ #include <QPushButton>
+ #include <QSignalMapper>
