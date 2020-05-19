95426ee112263ea6b44e01d787371af0b8d636b9
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-05-19 05:10:42 +0000

    Add py-copier 3.1.0
    Copier is a library for rendering project templates.
    - Works with local paths and git URLs.
    - Your project can include any file and Copier can dynamically replace values in
      any kind of text file.
    - It generates a beautiful output and takes care of not overwrite existing files
      unless instructed to do so.
    
    WWW: https://github.com/pykong/copier

---
+++ devel/py-copier/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-05-11 07:54:59 UTC
++++ setup.py
+@@ -11,7 +11,7 @@ install_requires = \
+ ['colorama>=0.4.0,<1.0.0',
+  'jinja2>=2.10.0,<3.0.0',
+  'packaging>=20.1,<21.0',
+- 'pathspec>=0.7.0,<0.8.0',
++ 'pathspec>=0.7.0,<0.9.0',
+  'plumbum>=1.6.0,<2.0.0',
+  'pydantic>=1.0b1',
+  'pyyaml-include>=1.2,<2.0',
