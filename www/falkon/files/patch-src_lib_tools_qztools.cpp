b0910aebfb4b75c50f2478e756aba4448ad90580
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-19 19:06:44 +0000

    www/falkon: prepare for Qt5-5.15

---
+++ www/falkon/files/patch-src_lib_tools_qztools.cpp
@@ -0,0 +1,10 @@
+--- src/lib/tools/qztools.cpp.orig	2020-05-19 18:59:17 UTC
++++ src/lib/tools/qztools.cpp
+@@ -25,6 +25,7 @@
+ #include <QByteArray>
+ #include <QPixmap>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QBuffer>
+ #include <QFile>
+ #include <QDir>
