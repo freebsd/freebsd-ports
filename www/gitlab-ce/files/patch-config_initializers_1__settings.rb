34032f2dc124a8c869111fd6e9b49f83e04ade69
Matthias Fechner <mfechner@FreeBSD.org>
2020-05-20 06:14:24 +0000

    Update to 12.10.6. Changelogs: https://about.gitlab.com/releases/2020/04/22/gitlab-12-10-released/ https://about.gitlab.com/releases/2020/04/24/gitlab-12-10-1-released/ https://about.gitlab.com/releases/2020/04/30/security-release-12-10-2-released/ https://about.gitlab.com/releases/2020/05/04/gitlab-12-10-3-released/ https://about.gitlab.com/releases/2020/05/13/gitlab-12-10-5-released/ https://about.gitlab.com/releases/2020/05/15/gitlab-12-10-6-released/

--- www/gitlab-ce/files/patch-config_initializers_1__settings.rb
+++ www/gitlab-ce/files/patch-config_initializers_1__settings.rb
@@ -1,6 +1,6 @@
---- config/initializers/1_settings.rb.orig	2020-03-26 12:19:15 UTC
+--- config/initializers/1_settings.rb.orig	2020-04-21 23:01:33 UTC
 +++ config/initializers/1_settings.rb
-@@ -182,11 +182,7 @@ Settings.gitlab['url'] ||= Settings.__send__(:build_gi
+@@ -173,11 +173,7 @@ Settings.gitlab['url'] ||= Settings.__send__(:build_gi
  Settings.gitlab['user'] ||= 'git'
  # External configuration may cause the ssh user to differ from the GitLab user
  Settings.gitlab['ssh_user'] ||= Settings.gitlab.user
