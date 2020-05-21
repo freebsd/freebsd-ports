921199ea1647e6d75d5f9e264ca2b0b6cec83790
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 12:01:50 +0000

    multimedia/QtAV: prepare for Qt5-5.15

---
+++ multimedia/QtAV/files/patch-src_QtAV_FilterContext.h
@@ -0,0 +1,10 @@
+--- src/QtAV/FilterContext.h.orig	2020-05-21 11:58:15 UTC
++++ src/QtAV/FilterContext.h
+@@ -26,6 +26,7 @@
+ #include <QtCore/QByteArray>
+ #include <QtCore/QRect>
+ #include <QtGui/QPainter>
++#include <QtGui/QPainterPath>
+ /*
+  * QPainterFilterContext, D2DFilterContext, ...
+  */
