b515564642b5724c9a70cfb3d8f6c13676cb2f00
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 11:05:11 +0000

    audio/sayonara: prepare for Qt5-5.15

---
+++ audio/sayonara/files/patch-src_Gui_Utils_Widgets_Slider.cpp
@@ -0,0 +1,10 @@
+--- src/Gui/Utils/Widgets/Slider.cpp.orig	2020-05-21 10:59:45 UTC
++++ src/Gui/Utils/Widgets/Slider.cpp
+@@ -22,6 +22,7 @@
+ #include "Gui/Utils/Style.h"
+ 
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QMouseEvent>
+ 
+ using Gui::Slider;
