a3720ba17c2a288c914796b746fb774c19f9c0d4
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 07:17:53 +0000

    deskutils/nextcloudclient: prepare for Qt5-5.15

---
+++ deskutils/nextcloudclient/files/patch-src_libsync_networkjobs.cpp
@@ -0,0 +1,10 @@
+--- src/libsync/networkjobs.cpp.orig	2020-05-21 07:13:57 UTC
++++ src/libsync/networkjobs.cpp
+@@ -31,6 +31,7 @@
+ #include <QJsonDocument>
+ #include <QJsonObject>
+ #include <QPainter>
++#include <QPainterPath>
+ 
+ #include "networkjobs.h"
+ #include "account.h"
