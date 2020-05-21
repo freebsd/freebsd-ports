48c15d3bbdbf1f27d860c8260b38040da4868d36
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 07:44:55 +0000

    editors/ghostwriter: prepare for Qt5-5.15

---
+++ editors/ghostwriter/files/patch-src_MarkdownEditor.cpp
@@ -0,0 +1,10 @@
+--- src/MarkdownEditor.cpp.orig	2020-05-21 07:40:36 UTC
++++ src/MarkdownEditor.cpp
+@@ -34,6 +34,7 @@
+ #include <QUrl>
+ #include <QPixmap>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QFileInfo>
+ #include <QDir>
+ 
