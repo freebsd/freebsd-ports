da6bf65c95f0eccd666520ca578fff73ac024ff8
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 07:34:54 +0000

    games/freeciv: prepare for Qt5-5.15

---
+++ games/freeciv/files/patch-client_gui-qt_canvas.cpp
@@ -0,0 +1,10 @@
+--- client/gui-qt/canvas.cpp.orig	2020-05-21 07:25:19 UTC
++++ client/gui-qt/canvas.cpp
+@@ -18,6 +18,7 @@
+ // Qt
+ #include <QFontMetrics>
+ #include <QPainter>
++#include <QPainterPath>
+ 
+ // qt-client
+ #include "canvas.h"
---
+++ games/freeciv/files/patch-client_gui-qt_dialogs.cpp
@@ -0,0 +1,10 @@
+--- client/gui-qt/dialogs.cpp.orig	2020-05-21 07:25:26 UTC
++++ client/gui-qt/dialogs.cpp
+@@ -24,6 +24,7 @@
+ #include <QMessageBox>
+ #include <QMouseEvent>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QRadioButton>
+ #include <QRect>
+ #include <QSignalMapper>
