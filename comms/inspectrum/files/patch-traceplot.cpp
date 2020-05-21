26312a5e4c0fec217573113471186bd66739e57e
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 10:03:54 +0000

    comms/inspectrum: prepare for Qt5-5.15

---
+++ comms/inspectrum/files/patch-traceplot.cpp
@@ -0,0 +1,10 @@
+--- traceplot.cpp.orig	2020-05-21 09:59:50 UTC
++++ traceplot.cpp
+@@ -20,6 +20,7 @@
+ #include <QPixmapCache>
+ #include <QTextStream>
+ #include <QtConcurrent>
++#include <QPainterPath>
+ #include "samplesource.h"
+ #include "traceplot.h"
+ 
