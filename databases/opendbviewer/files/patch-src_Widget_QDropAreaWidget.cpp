6ccaee6005d4835ce6f32629e585b97e3e5be5be
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 08:21:17 +0000

    databases/opendbviewer: prepare for Qt5-5.15

---
+++ databases/opendbviewer/files/patch-src_Widget_QDropAreaWidget.cpp
@@ -0,0 +1,10 @@
+--- src/Widget/QDropAreaWidget.cpp.orig	2020-05-21 08:16:02 UTC
++++ src/Widget/QDropAreaWidget.cpp
+@@ -6,6 +6,7 @@
+  */
+ 
+ #include <QPainter>
++#include <QPainterPath>
+ 
+ #include "QDropAreaWidget.h"
+ 
