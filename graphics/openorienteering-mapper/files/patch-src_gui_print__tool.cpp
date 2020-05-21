e16be1d855619853c22ac11fb94607810f901312
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 09:10:27 +0000

    graphics/openorienteering-mapper: prepare for Qt5-5.15

---
+++ graphics/openorienteering-mapper/files/patch-src_gui_print__tool.cpp
@@ -0,0 +1,10 @@
+--- src/gui/print_tool.cpp.orig	2020-05-21 09:05:59 UTC
++++ src/gui/print_tool.cpp
+@@ -25,6 +25,7 @@
+ 
+ #include <QMouseEvent>
+ #include <QPainter>
++#include <QPainterPath>
+ 
+ #include "core/map.h"
+ #include "core/map_printer.h"
