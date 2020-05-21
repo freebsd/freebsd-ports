44c1dc2ec6b46ef0138f40981bf2188bdd943161
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 09:13:03 +0000

    polish/kadu: prepare for Qt5-5.15

---
+++ polish/kadu/files/patch-kadu-core_gui_widgets_avatar-painter.cpp
@@ -0,0 +1,10 @@
+--- kadu-core/gui/widgets/avatar-painter.cpp.orig	2020-05-21 09:00:13 UTC
++++ kadu-core/gui/widgets/avatar-painter.cpp
+@@ -22,6 +22,7 @@
+ 
+ #include <QtCore/QModelIndex>
+ #include <QtGui/QPainter>
++#include <QtGui/QPainterPath>
+ #include <QtGui/QPixmap>
+ #include <QtGui/QPixmapCache>
+ #include <QtWidgets/QStyleOptionViewItem>
