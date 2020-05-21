1a658498bc25c2aa7b2b0d2e9c9144757d055ed4
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 07:35:09 +0000

    games/hdegewars: pepare for Qt5-5.15

---
+++ games/hedgewars/files/patch-QTfrontend_drawmapscene.cpp
@@ -0,0 +1,10 @@
+--- QTfrontend/drawmapscene.cpp.orig	2020-05-21 07:21:20 UTC
++++ QTfrontend/drawmapscene.cpp
+@@ -19,6 +19,7 @@
+ #include <QGraphicsSceneMouseEvent>
+ #include <QGraphicsPathItem>
+ #include <QtEndian>
++#include <QPainterPath>
+ #include <QDebug>
+ #include <QTransform>
+ #include <math.h>
---
+++ games/hedgewars/files/patch-QTfrontend_ui_page_pagegamestats.cpp
@@ -0,0 +1,10 @@
+--- QTfrontend/ui/page/pagegamestats.cpp.orig	2020-05-21 07:21:30 UTC
++++ QTfrontend/ui/page/pagegamestats.cpp
+@@ -22,6 +22,7 @@
+ #include <QGraphicsScene>
+ #include <QGroupBox>
+ #include <QSizePolicy>
++#include <QPainterPath>
+ 
+ #include "pagegamestats.h"
+ #include "team.h"
