11d8060789a65b17c35b54caba5a0b23e09f250a
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 10:54:54 +0000

    x11/radare-cutter: prepare for Qt5-5.15

---
+++ x11/radare-cutter/files/patch-widgets_ColorPicker.cpp
@@ -0,0 +1,10 @@
+--- widgets/ColorPicker.cpp.orig	2020-05-21 10:52:24 UTC
++++ widgets/ColorPicker.cpp
+@@ -3,6 +3,7 @@
+ 
+ #include <QPaintEvent>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QMouseEvent>
+ #include <QDesktopWidget>
+ #include <QPixmap>
---
+++ x11/radare-cutter/files/patch-widgets_ColorThemeListView.cpp
@@ -0,0 +1,10 @@
+--- widgets/ColorThemeListView.cpp.orig	2020-05-21 10:52:24 UTC
++++ widgets/ColorThemeListView.cpp
+@@ -3,6 +3,7 @@
+ #include <QJsonArray>
+ #include <QMap>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QFontMetrics>
+ #include <QScreen>
+ #include <QJsonArray>
