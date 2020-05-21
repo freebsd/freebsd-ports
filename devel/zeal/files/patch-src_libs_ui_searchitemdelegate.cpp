f7bb2be2c41d881fd1330e8c1ebdfa70aa5bcaef
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 11:33:09 +0000

    devel/zeal: prepare for Qt5-5.15

---
+++ devel/zeal/files/patch-src_libs_ui_searchitemdelegate.cpp
@@ -0,0 +1,10 @@
+--- src/libs/ui/searchitemdelegate.cpp.orig	2020-05-21 11:29:50 UTC
++++ src/libs/ui/searchitemdelegate.cpp
+@@ -27,6 +27,7 @@
+ #include <QFontMetrics>
+ #include <QHelpEvent>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QToolTip>
+ 
+ using namespace Zeal::WidgetUi;
