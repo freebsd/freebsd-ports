ddf7002d922a3957cd33e3a0079e198c371b0844
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 09:59:14 +0000

    games/pentobi: prepare for Qt5-5.15

---
+++ games/pentobi/files/patch-src_pentobi_RatingGraph.cpp
@@ -0,0 +1,10 @@
+--- src/pentobi/RatingGraph.cpp.orig	2020-05-21 09:54:38 UTC
++++ src/pentobi/RatingGraph.cpp
+@@ -13,6 +13,7 @@
+ #include <QApplication>
+ #include <QDesktopWidget>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QPen>
+ 
+ //-----------------------------------------------------------------------------
