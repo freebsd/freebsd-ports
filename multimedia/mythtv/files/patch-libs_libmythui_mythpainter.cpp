c01ca375822caa875847646dbb6e7f2f4c7d6ac8
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 12:54:50 +0000

    multimedia/mythtv: prepare for Qt5-5.15

---
+++ multimedia/mythtv/files/patch-libs_libmythui_mythpainter.cpp
@@ -0,0 +1,10 @@
+--- libs/libmythui/mythpainter.cpp.orig	2020-05-21 12:31:44 UTC
++++ libs/libmythui/mythpainter.cpp
+@@ -5,6 +5,7 @@
+ // QT headers
+ #include <QRect>
+ #include <QPainter>
++#include <QPainterPath>
+ 
+ // libmythbase headers
+ #include "mythlogging.h"
