7ac40436c7d3d8406209040392e8663b2c8abf55
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 08:33:08 +0000

    net-mgmt/seafile-gui: prepare for Qt5-5.15

---
+++ net-mgmt/seafile-gui/files/patch-src_ui_private-share-dialog.cpp
@@ -0,0 +1,10 @@
+--- src/ui/private-share-dialog.cpp.orig	2020-05-21 08:29:40 UTC
++++ src/ui/private-share-dialog.cpp
+@@ -2,6 +2,7 @@
+ #include <QCompleter>
+ #include <QLineEdit>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QResizeEvent>
+ #include <QStringList>
+ #include <QStringListModel>
