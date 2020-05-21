e1299426b7d9b6b9b91504d2c19bc326e6aae3b9
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 11:10:15 +0000

    graphics/zint: prepare for Qt5-5.15

---
+++ graphics/zint/files/patch-backend__qt_qzint.cpp
@@ -0,0 +1,10 @@
+--- backend_qt/qzint.cpp.orig	2020-05-21 11:07:30 UTC
++++ backend_qt/qzint.cpp
+@@ -17,6 +17,7 @@
+ 
+ #include "qzint.h"
+ #include <stdio.h>
++#include <QPainterPath>
+ 
+ namespace Zint {
+ 
