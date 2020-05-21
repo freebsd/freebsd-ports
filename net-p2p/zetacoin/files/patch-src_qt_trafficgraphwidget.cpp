c577355c7d56650153f25a0318965016ce179c6c
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 11:08:40 +0000

    net-p2p/zetacoin: prepare for Qt5-5.15

---
+++ net-p2p/zetacoin/files/patch-src_qt_trafficgraphwidget.cpp
@@ -0,0 +1,10 @@
+--- src/qt/trafficgraphwidget.cpp.orig	2020-05-21 11:02:42 UTC
++++ src/qt/trafficgraphwidget.cpp
+@@ -6,6 +6,7 @@
+ #include "clientmodel.h"
+ 
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QColor>
+ #include <QTimer>
+ 
