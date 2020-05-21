bbc0b2886776adab88d49541c7558d15a2b1328e
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 11:57:50 +0000

    net/smb4k: prepare for Qt5-5.15

---
+++ net/smb4k/files/patch-smb4k_smb4ktooltip.cpp
@@ -0,0 +1,10 @@
+--- smb4k/smb4ktooltip.cpp.orig	2020-05-21 11:52:53 UTC
++++ smb4k/smb4ktooltip.cpp
+@@ -48,6 +48,7 @@
+ #include <QStyleOptionFrame>
+ #include <QAbstractScrollArea>
+ #include <QScreen>
++#include <QPainterPath>
+ 
+ // KDE includes
+ #include <KI18n/KLocalizedString>
