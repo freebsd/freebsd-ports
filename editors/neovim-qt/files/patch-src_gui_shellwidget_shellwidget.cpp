7c76a748937493b8ae60399766152cc638fcb4c8
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 10:08:32 +0000

    editors/neovim-qt: prepare for Qt5-5.15

---
+++ editors/neovim-qt/files/patch-src_gui_shellwidget_shellwidget.cpp
@@ -0,0 +1,8 @@
+--- src/gui/shellwidget/shellwidget.cpp.orig	2020-05-21 10:04:36 UTC
++++ src/gui/shellwidget/shellwidget.cpp
+@@ -1,4 +1,5 @@
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QPaintEvent>
+ #include <QDebug>
+ #include "shellwidget.h"
