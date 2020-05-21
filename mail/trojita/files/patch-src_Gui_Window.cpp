247330e2833390d14664b9815b526c2d67376c6e
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-21 12:18:16 +0000

    mail/trojita: prepare for Qt5-5.15

---
+++ mail/trojita/files/patch-src_Gui_Window.cpp
@@ -0,0 +1,10 @@
+--- src/Gui/Window.cpp.orig	2020-05-21 12:12:17 UTC
++++ src/Gui/Window.cpp
+@@ -45,6 +45,7 @@
+ #include <QToolTip>
+ #include <QUrl>
+ #include <QWheelEvent>
++#include <QPainterPath>
+ 
+ #include "configure.cmake.h"
+ #include "Common/Application.h"
