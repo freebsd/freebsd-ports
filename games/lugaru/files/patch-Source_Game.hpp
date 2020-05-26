cae178df783e3e81ee41b5711bf29727152d5f7c
Lorenzo Salvadore <salvadore@FreeBSD.org>
2020-05-26 23:04:48 +0000

    games/lugaru: Update to 1.2
    - Switch to gitlab repo, where the game is actually maintained.
    - Bump PORTEPOCH as the versioning scheme changed.
    - Update license information.
    - Switch to sdl2.
    - Remove obsolete DESKTOP_ENTRIES and link from share/pixmaps: the
      software now provides a lugaru.desktop file and an icon in share/icons.
    
    Reviewed by:	tcberner (co-mentor)
    Approved by:	gerald (mentor)
    Differential Revision:	https://reviews.freebsd.org/D24992

---
+++ games/lugaru/files/patch-Source_Game.hpp
@@ -0,0 +1,10 @@
+--- Source/Game.hpp.orig	2020-05-23 00:03:09 UTC
++++ Source/Game.hpp
+@@ -42,6 +42,7 @@ along with Lugaru.  If not, see <http://www.gnu.org/li
+ 
+ #include <SDL.h>
+ #include <fstream>
++#include <unistd.h>
+ 
+ #define NB_CAMPAIGN_MENU_ITEM 7
+ 
