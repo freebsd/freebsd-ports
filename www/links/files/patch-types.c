30ca82a7b6fd78434ba755192ebda2d356664756
Jimmy Olgeni <olgeni@FreeBSD.org>
2020-05-28 08:01:47 +0000

    Update www/links to version 2.20.2.
    PR:           242036
    Submitted by: olgeni
    Approved by:  maintainer

--- www/links/files/patch-types.c
+++ www/links/files/patch-types.c
@@ -1,12 +1,12 @@
---- types.c.orig	2016-06-17 20:33:57 UTC
+--- types.c.orig	2019-11-17 18:26:27 UTC
 +++ types.c
-@@ -849,7 +849,8 @@ unsigned char *get_content_type_by_exten
+@@ -872,7 +872,8 @@ unsigned char *get_content_type_by_extension(unsigned 
  			ext = NULL;
  		}
  	if (ext) while (ext[extl] && ext[extl] != '.' && !dir_sep(ext[extl]) && !end_of_dir(url, ext[extl])) extl++;
 -	if ((extl == 3 && !casecmp(ext, cast_uchar "htm", 3)) ||
-+	if (force_html || 
++ 	if (force_html || 
 +	    (extl == 3 && !casecmp(ext, cast_uchar "htm", 3)) ||
- 	    (extl == 4 && !casecmp(ext, cast_uchar "html", 4))) return stracpy(cast_uchar "text/html");
- 	foreach(e, extensions) {
- 		unsigned char *fname = NULL;
+ 	    (extl == 4 && !casecmp(ext, cast_uchar "html", 4)) ||
+ 	    (extl == 5 && !casecmp(ext, cast_uchar "xhtml", 5))) return stracpy(cast_uchar "text/html");
+ 	foreach(struct list, l, ll, extensions.list_entry) {
