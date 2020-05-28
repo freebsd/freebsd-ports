102086eb378c3b90caba45355db732ddf2b89aa2
Jimmy Olgeni <olgeni@FreeBSD.org>
2020-05-28 07:50:25 +0000

    Update textproc/fzf to version 0.21.1.
    PR:           246739
    Submitted by: olgeni
    Approved by:  maintainer

--- textproc/fzf/files/patch-shell_completion.bash
+++ textproc/fzf/files/patch-shell_completion.bash
@@ -1,11 +1,11 @@
---- shell/completion.bash.orig	2017-12-03 14:55:24 UTC
+--- shell/completion.bash.orig	2020-05-25 18:52:57 UTC
 +++ shell/completion.bash
-@@ -219,7 +219,7 @@ _fzf_complete_kill() {
+@@ -252,7 +252,7 @@ _fzf_complete_kill() {
+   [ -n "${COMP_WORDS[COMP_CWORD]}" ] && return 1
  
-   local selected fzf
-   fzf="$(__fzfcmd_complete)"
--  selected=$(command ps -ef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" $fzf -m | awk '{print $2}' | tr '\n' ' ')
-+  selected=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" $fzf -m | awk '{print $2}' | tr '\n' ' ')
+   local selected
+-  selected=$(command ps -ef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS --preview 'echo {}' --preview-window down:3:wrap" __fzf_comprun "kill" -m | awk '{print $2}' | tr '\n' ' ')
++  selected=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS --preview 'echo {}' --preview-window down:3:wrap" __fzf_comprun "kill" -m | awk '{print $2}' | tr '\n' ' ')
+   selected=${selected% }
    printf '\e[5n'
  
-   if [ -n "$selected" ]; then
--- textproc/fzf/files/patch-shell_completion.zsh
+++ textproc/fzf/files/patch-shell_completion.zsh
@@ -1,11 +1,11 @@
---- shell/completion.zsh.orig	2017-12-03 14:55:24 UTC
+--- shell/completion.zsh.orig	2020-05-25 18:53:01 UTC
 +++ shell/completion.zsh
-@@ -163,7 +163,7 @@ fzf-completion() {
+@@ -276,7 +276,7 @@ fzf-completion() {
+   tail=${LBUFFER:$(( ${#LBUFFER} - ${#trigger} ))}
    # Kill completion (do not require trigger sequence)
    if [ $cmd = kill -a ${LBUFFER[-1]} = ' ' ]; then
-     fzf="$(__fzfcmd_complete)"
--    matches=$(command ps -ef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" ${=fzf} -m | awk '{print $2}' | tr '\n' ' ')
-+    matches=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" ${=fzf} -m | awk '{print $2}' | tr '\n' ' ')
+-    matches=$(command ps -ef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS --preview 'echo {}' --preview-window down:3:wrap" __fzf_comprun "$cmd" -m | awk '{print $2}' | tr '\n' ' ')
++    matches=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS --preview 'echo {}' --preview-window down:3:wrap" __fzf_comprun "$cmd" -m | awk '{print $2}' | tr '\n' ' ')
      if [ -n "$matches" ]; then
        LBUFFER="$LBUFFER$matches"
      fi
