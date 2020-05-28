48be3955e839e3c37da1353ba619253502fde929
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-05-28 19:13:55 +0000

    Update RUN_DEPENDS for py-willow 1.4 update

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2020-04-14 08:29:39 UTC
+--- setup.py.orig	2020-05-04 10:55:13 UTC
 +++ setup.py
-@@ -26,7 +26,7 @@ install_requires = [
+@@ -26,12 +26,12 @@ install_requires = [
      "django-taggit>=1.0,<2.0",
      "django-treebeard>=4.2.0,<5.0",
      "djangorestframework>=3.7.4,<4.0",
@@ -9,3 +9,9 @@
      "Pillow>=4.0.0,<8.0.0",
      "beautifulsoup4>=4.8,<4.9",
      "html5lib>=0.999,<2",
+     "Unidecode>=0.04.14,<2.0",
+-    "Willow>=1.3,<1.4",
++    "Willow>=1.3",
+     "requests>=2.11.1,<3.0",
+     "l18n>=2018.5",
+     "xlsxwriter>=1.2.8,<2.0",
