e0ad71a1b4a2d8712072c783e62d5bab687433ca
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-29 16:39:43 +0000

    graphics/drawpile: prepare for Qt5-5.15

---
+++ graphics/drawpile/files/patch-src_desktop_bundled_QtColorWidgets_color__wheel.cpp
@@ -0,0 +1,10 @@
+--- src/desktop/bundled/QtColorWidgets/color_wheel.cpp.orig	2020-05-29 16:35:46 UTC
++++ src/desktop/bundled/QtColorWidgets/color_wheel.cpp
+@@ -25,6 +25,7 @@
+ #include <QtMath>
+ #include <QMouseEvent>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QLineF>
+ #include <QDragEnterEvent>
+ #include <QMimeData>
---
+++ graphics/drawpile/files/patch-src_desktop_widgets_popupmessage.h
@@ -0,0 +1,10 @@
+--- src/desktop/widgets/popupmessage.h.orig	2020-05-29 16:32:32 UTC
++++ src/desktop/widgets/popupmessage.h
+@@ -20,6 +20,7 @@
+ #define POPUPMESSAGE_H
+ 
+ #include <QWidget>
++#include <QPainterPath>
+ 
+ class QTextDocument;
+ class QTimer;
