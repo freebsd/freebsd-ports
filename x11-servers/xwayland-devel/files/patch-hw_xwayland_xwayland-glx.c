4bdb3aa74e913bd541dc413f7bda841d6f1c2a0d
Jan Beich <jbeich@FreeBSD.org>
2020-05-29 10:40:38 +0000

    x11-servers/xwayland-devel: simplify r536952 again

--- x11-servers/xwayland-devel/files/patch-hw_xwayland_xwayland-glx.c
+++ x11-servers/xwayland-devel/files/patch-hw_xwayland_xwayland-glx.c
@@ -2,21 +2,16 @@ https://gitlab.freedesktop.org/xorg/xserver/-/issues/1032
 
 --- hw/xwayland/xwayland-glx.c.orig	2020-05-20 07:13:28 UTC
 +++ hw/xwayland/xwayland-glx.c
-@@ -343,6 +343,9 @@ egl_screen_probe(ScreenPtr pScreen)
-     struct xwl_screen *xwl_screen = xwl_screen_get(pScreen);
-     __GLXscreen *base;
- 
-+    if (xwl_screen->egl_backend == &xwl_screen->eglstream_backend)
-+        return NULL;
-+
-     if (enableIndirectGLX)
-         return NULL; /* not implemented */
- 
-@@ -353,6 +356,7 @@ egl_screen_probe(ScreenPtr pScreen)
-     base->destroy = egl_screen_destroy;
+@@ -354,6 +354,12 @@ egl_screen_probe(ScreenPtr pScreen)
      base->createDrawable = egl_create_glx_drawable;
      /* base.swapInterval = NULL; */
-+    base->glvnd = strdup("mesa");
  
++    /* GlxVendorLibrary is queried in DRI2, so assume Mesa for now */
++    if (xwl_screen->egl_backend == &xwl_screen->eglstream_backend)
++        base->glvnd = strdup("nvidia");
++    else
++        base->glvnd = strdup("mesa");
++
      screen->display = xwl_screen->glamor_ctx->display;
  
+     __glXInitExtensionEnableBits(screen->base.glx_enable_bits);
