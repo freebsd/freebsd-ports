a2587c2df291ede275724de9d3252b3df8d18994
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2020-05-29 18:41:55 +0000

    net-im/telegram-desktop: prepare for Qt5-5.15
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D25051

---
+++ net-im/telegram-desktop/files/patch-Telegram_lib__ui_ui_widgets_input__fields.h
@@ -0,0 +1,10 @@
+--- Telegram/lib_ui/ui/widgets/input_fields.h.orig	2020-05-29 11:22:48 UTC
++++ Telegram/lib_ui/ui/widgets/input_fields.h
+@@ -16,6 +16,7 @@
+ #include <QtWidgets/QLineEdit>
+ #include <QtWidgets/QTextEdit>
+ #include <QtCore/QTimer>
++#include <QtGui/QPainterPath>
+ 
+ class QTouchEvent;
+ class Painter;
