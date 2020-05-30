9cef17e95a49e6ebecd135937e561d5399917e4f
Tobias C. Berner <tcberner@FreeBSD.org>
2020-05-30 09:26:01 +0000

    editors/calligraplan: prepare for Qt5-5.15

---
+++ editors/calligraplan/files/patch-src_libs_ui_kptganttitemdelegate.cpp
@@ -0,0 +1,10 @@
+--- src/libs/ui/kptganttitemdelegate.cpp.orig	2020-05-30 09:10:49 UTC
++++ src/libs/ui/kptganttitemdelegate.cpp
+@@ -28,6 +28,7 @@
+ #include <QModelIndex>
+ #include <QApplication>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QLocale>
+ 
+ #include <KLocalizedString>
