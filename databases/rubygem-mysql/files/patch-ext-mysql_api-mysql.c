260f794134d4cd5e1f59a3d67f86578ce02c4cfa
Dirk Meyer <dinoex@FreeBSD.org>
2020-05-30 08:57:01 +0000

    - fix build with mariadb 10.4

--- databases/rubygem-mysql/files/patch-ext-mysql_api-mysql.c
+++ databases/rubygem-mysql/files/patch-ext-mysql_api-mysql.c
@@ -1,6 +1,50 @@
---- ext/mysql_api/mysql.c.orig	2018-01-03 15:47:52 UTC
+--- ext/mysql_api/mysql.c.orig	2020-05-29 23:08:40 UTC
 +++ ext/mysql_api/mysql.c
-@@ -1317,7 +1317,7 @@ static VALUE stmt_bind_result(int argc, 
+@@ -273,7 +273,10 @@ static VALUE real_connect(int argc, VALUE* argv, VALUE
+     rb_thread_start_timer();
+ #endif
+ 
++#if MYSQL_VERSION_ID >= 100400
++#else
+     myp->handler.reconnect = 0;
++#endif
+     myp->connection = Qtrue;
+     myp->query_with_result = Qtrue;
+     rb_obj_call_init(obj, argc, argv);
+@@ -347,7 +350,10 @@ static VALUE real_connect2(int argc, VALUE* argv, VALU
+ #ifdef HAVE_RB_THREAD_START_TIMER
+     rb_thread_start_timer();
+ #endif
++#if MYSQL_VERSION_ID >= 100400
++#else
+     m->reconnect = 0;
++#endif
+     GetMysqlStruct(obj)->connection = Qtrue;
+ 
+     return obj;
+@@ -917,13 +923,21 @@ static VALUE query_with_result_set(VALUE obj, VALUE fl
+ /*	reconnect()	*/
+ static VALUE reconnect(VALUE obj)
+ {
++#if MYSQL_VERSION_ID >= 100400
++    return Qfalse;
++#else
+     return GetHandler(obj)->reconnect ? Qtrue : Qfalse;
++#endif
+ }
+ 
+ /*	reconnect=(flag)	*/
+ static VALUE reconnect_set(VALUE obj, VALUE flag)
+ {
++#if MYSQL_VERSION_ID >= 100400
++    return Qfalse;
++#else
+     GetHandler(obj)->reconnect = (flag == Qnil || flag == Qfalse) ? 0 : 1;
++#endif
+     return flag;
+ }
+ 
+@@ -1317,7 +1331,7 @@ static VALUE stmt_bind_result(int argc, VALUE *argv, V
  	}
  	else if (argv[i] == rb_cString)
  	    s->result.bind[i].buffer_type = MYSQL_TYPE_STRING;
