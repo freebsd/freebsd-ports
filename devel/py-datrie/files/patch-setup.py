3dd9016e8e67025c771f4c7637839d9ee67e77f7
Yuri Victorovich <yuri@FreeBSD.org>
2020-05-30 05:31:44 +0000

    devel/py-datrie: Update 0.8 -> 0.8.2

--- devel/py-datrie/files/patch-setup.py
+++
@@ -1,9 +0,0 @@
---- setup.py.orig	2018-11-27 07:47:36 UTC
-+++ setup.py
-@@ -54,5 +54,5 @@ setup(name="datrie",
-           ], include_dirs=[LIBDATRIE_DIR])
-       ],
- 
--      setup_requires=["pytest-runner"],
-+      setup_requires=[],
-       tests_require=["pytest", "hypothesis"])
