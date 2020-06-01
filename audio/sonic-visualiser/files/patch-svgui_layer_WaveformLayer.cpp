a3e0aed9e89dda4a12e5b5e354be016c9e56d0e1
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2020-06-01 06:37:52 +0000

    audio/sonic-visualiser: prepare for Qt5-5.15
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D25054

---
+++ audio/sonic-visualiser/files/patch-svgui_layer_WaveformLayer.cpp
@@ -0,0 +1,10 @@
+--- svgui/layer/WaveformLayer.cpp.orig	2020-05-29 17:17:11 UTC
++++ svgui/layer/WaveformLayer.cpp
+@@ -27,6 +27,7 @@
+ #include "data/model/WaveformOversampler.h"
+ 
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QPixmap>
+ #include <QTextStream>
+ 
