7f82d79532692fd3dc373b0f469bf082378f03cf
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2020-06-01 06:37:14 +0000

    print/scribus-devel: prepare for Qt5-5.15
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D25063

---
+++ print/scribus-devel/files/patch-scribus_ui_scresizecursor.cpp
@@ -0,0 +1,10 @@
+--- scribus/ui/scresizecursor.cpp.orig	2020-05-29 20:30:14 UTC
++++ scribus/ui/scresizecursor.cpp
+@@ -12,6 +12,7 @@
+ 
+ #include <QDebug>
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QPixmap>
+ #include <QPen>
+ #include <QBrush>
