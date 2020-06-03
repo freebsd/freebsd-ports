a72f32a06f184502651439f4cfbb410f13b9832f
Lev A. Serebryakov <lev@FreeBSD.org>
2020-06-03 15:16:51 +0000

    Update to latest version 1.6.1

--- devel/stlink/files/patch-cmake_modules_c__flags.cmake
+++ devel/stlink/files/patch-cmake_modules_c__flags.cmake
@@ -1,6 +1,6 @@
---- cmake/CFlags.cmake.orig	2017-07-01 17:41:54.000000000 +0300
-+++ cmake/CFlags.cmake	2017-08-21 18:28:07.994387000 +0300
-@@ -27,6 +27,7 @@ add_cflag_if_supported("-Wmaybe-uninitia
+--- cmake/modules/c_flags.cmake.orig	2020-05-31 22:00:20 UTC
++++ cmake/modules/c_flags.cmake
+@@ -30,6 +30,7 @@ add_cflag_if_supported("-Wmaybe-uninitialized")
  add_cflag_if_supported("-Wmissing-variable-declarations")
  add_cflag_if_supported("-Wshorten-64-to-32")
  add_cflag_if_supported("-Wimplicit-function-declaration")
