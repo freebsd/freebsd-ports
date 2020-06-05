ae49030f4bd5b6f837f3770d9b8656a006271a52
Fernando Apesteguía <fernape@FreeBSD.org>
2020-06-05 09:08:11 +0000

    cad/lepton-eda: Update to 1.9.11
    ChangeLog:
    https://github.com/lepton-eda/lepton-eda/blob/master/NEWS.md#notable-changes-in-lepton-eda-1911-20200604
    
    PR:	246991
    Submitted by:	graahnul.grom@gmail.com (maintainer)

--- cad/lepton-eda/files/patch-version.h
+++ cad/lepton-eda/files/patch-version.h
@@ -1,11 +1,11 @@
---- version.h.orig	2020-03-25 05:39:40 UTC
+--- version.h.orig	2020-06-04 15:22:30 UTC
 +++ version.h
 @@ -0,0 +1,8 @@
 +/* Date part of package version string. */
-+#define PACKAGE_DATE_VERSION "20200319"
++#define PACKAGE_DATE_VERSION "20200604"
 +
 +/* Dotted part of package version string. */
-+#define PACKAGE_DOTTED_VERSION "1.9.10"
++#define PACKAGE_DOTTED_VERSION "1.9.11"
 +
 +/* Current git commit. */
-+#define PACKAGE_GIT_COMMIT "c69155b34b82d2b288f4787f92fe930112e73c72"
++#define PACKAGE_GIT_COMMIT "219f66e870008a2bbc575b5b5f9499bf6c4a9b1f"
