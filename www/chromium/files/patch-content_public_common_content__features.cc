0884aebd18601b90ab0fc152c7862d6ffd042671
Joseph Mingrone <jrm@FreeBSD.org>
2020-06-06 15:34:27 +0000

    www/chromium: Update to version 83.0.4103.97
    Submitted by:	Matthias Wolf <freebsd@rheinwolf.de> via [1]
    Approved by:	chromium@ (rene)
    MFH:            2020Q2
    Security:	https://chromereleases.googleblog.com/2020/06/stable-channel-update-for-desktop.html
    
    [1] https://github.com/gliaskos/freebsd-chromium

--- www/chromium/files/patch-content_public_common_content__features.cc
+++ www/chromium/files/patch-content_public_common_content__features.cc
@@ -1,4 +1,4 @@
---- content/public/common/content_features.cc.orig	2020-05-13 18:40:32 UTC
+--- content/public/common/content_features.cc.orig	2020-06-03 18:40:24 UTC
 +++ content/public/common/content_features.cc
 @@ -48,7 +48,7 @@ const base::Feature kAudioServiceLaunchOnStartup{
  const base::Feature kAudioServiceOutOfProcess {
@@ -9,7 +9,7 @@
        base::FEATURE_ENABLED_BY_DEFAULT
  #else
        base::FEATURE_DISABLED_BY_DEFAULT
-@@ -707,7 +707,7 @@ const base::Feature kWebAssemblyThreads {
+@@ -711,7 +711,7 @@ const base::Feature kWebAssemblyThreads {
  };
  
  // Enable WebAssembly trap handler.
@@ -18,7 +18,7 @@
      defined(ARCH_CPU_X86_64)
  const base::Feature kWebAssemblyTrapHandler{"WebAssemblyTrapHandler",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
-@@ -737,7 +737,7 @@ const base::Feature kWebAuth{"WebAuthentication",
+@@ -741,7 +741,7 @@ const base::Feature kWebAuth{"WebAuthentication",
  // https://w3c.github.io/webauthn
  const base::Feature kWebAuthCable {
    "WebAuthenticationCable",
