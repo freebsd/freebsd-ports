e94cc77846ad285726a013a17cffec2bfbc55d3a
Rene Ladan <rene@FreeBSD.org>
2020-06-06 10:21:03 +0000

    Remove expired ports:
    2020-06-06 net/freerdp1: Superseded by the freerdp port/package
    2020-05-05 irc/psybnc: Broken for more than 6 months

--- net/freerdp1/files/patch-client_X11_ModuleOptions.cmake
+++
@@ -1,8 +0,0 @@
---- client/X11/ModuleOptions.cmake.orig	2014-09-11 22:46:32 UTC
-+++ client/X11/ModuleOptions.cmake
-@@ -1,4 +1,4 @@
- 
--set(FREERDP_CLIENT_NAME "xfreerdp")
-+set(FREERDP_CLIENT_NAME "xfreerdp1")
- set(FREERDP_CLIENT_PLATFORM "X11")
- set(FREERDP_CLIENT_VENDOR "FreeRDP")
