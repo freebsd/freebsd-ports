1d6dda8d0a27ee34575efa059de3543b8743f52e
Dima Panov <fluffy@FreeBSD.org>
2020-06-10 16:41:49 +0000

    devel/eric6: update to 20.6 release
    - pass all python scripts via shebangfix (env python3 is not looks good)
    - rename variable INSTALL_CMD to INSTALL_PY to avoid stage error at pkg build cluster
    - install qsci files under share/qt5 subdir instead of share/qt4

--- devel/eric6/bsd.eric6.mk
+++ devel/eric6/bsd.eric6.mk
@@ -6,7 +6,8 @@
 # german/eric6, russian/eric6, devel/es-eric6
 
 PORTNAME=	eric6
-PORTVERSION=	20.5
+PORTVERSION=	20.6
+PORTREVISION?=	0
 MASTER_SITES=	SF/eric-ide/${PORTNAME}/stable/${PORTVERSION}
 WRKSRC=		${WRKDIR}/${PORTNAME}-${PORTVERSION}
 
@@ -18,4 +19,5 @@ LICENSE=	GPLv3
 DESCR=		${.CURDIR}/../../devel/eric6/pkg-descr
 MAKE_ENV+=	"HOME=${WRKDIR}"
 
+NO_ARCH=	yes
 NO_BUILD=	yes
