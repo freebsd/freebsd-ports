e0604362603db1f0bfde4b102bd3f226ec4cf78c
Mark Linimon <linimon@FreeBSD.org>
2020-06-13 17:16:43 +0000

    Remove logic for specifying version of gcc-aux.  It has been OBE since lang/gcc5-aux was removed 2017-11-01.
    Test by 'make index'.

--- Mk/Uses/ada.mk
+++ Mk/Uses/ada.mk
@@ -1,11 +1,11 @@
 # $FreeBSD$
 #
 # Establish Ada-capable compiler as a build dependency
-# To change default compiler, define ADA_DEFAULT in make.conf to 5
+# To change default compiler, define ADA_DEFAULT in make.conf
 #
 # Feature:      ada
 # Usage:        USES=ada
-# Valid ARGS:   5, 6, run
+# Valid ARGS:   run
 #
 # MAINTAINER: ports@FreeBSD.org
 
@@ -14,16 +14,6 @@ _INCLUDE_USES_ADA_MK=    yes
 
 CC=	ada
 ADAXX=	gcc6	# framework default
-
-. if ${ada_ARGS:M5}
-ADAXX=	gcc5
-. elif ${ada_ARGS:M6}
-ADAXX=	gcc6
-. elif defined(ADA_DEFAULT)
-.  if ${ADA_DEFAULT} == 5
-ADAXX=	gcc5
-.  endif
-. endif
 
 . if ${ada_ARGS:Mrun}
 RUN_DEPENDS+=	${LOCALBASE}/${ADAXX}-aux/bin/ada:lang/${ADAXX}-aux
