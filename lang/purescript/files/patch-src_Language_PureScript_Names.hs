4926ea68829d3632bd818f274da54ff7d2ef4eda
Gleb Popov <arrowd@FreeBSD.org>
2020-06-15 18:47:12 +0000

    lang/purescript: Update to 0.13.8

--- lang/purescript/files/patch-src_Language_PureScript_Names.hs
+++ lang/purescript/files/patch-src_Language_PureScript_Names.hs
@@ -1,6 +1,6 @@
---- src/Language/PureScript/Names.hs.orig	2019-11-02 17:39:14 UTC
+--- src/Language/PureScript/Names.hs.orig	2020-05-23 12:43:53 UTC
 +++ src/Language/PureScript/Names.hs
-@@ -126,6 +126,9 @@ data OpNameType = ValueOpName | TypeOpName | AnyOpName
+@@ -132,6 +132,9 @@ data OpNameType = ValueOpName | TypeOpName | AnyOpName
  eraseOpName :: OpName a -> OpName 'AnyOpName
  eraseOpName = OpName . runOpName
  
