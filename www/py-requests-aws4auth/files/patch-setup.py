ee0884d6448174295f2f9d987c8f13a311369aec
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-06-15 18:50:33 +0000

    Update to 1.0
    Changes:	https://github.com/tedder/requests-aws4auth/blob/master/HISTORY.md

--- www/py-requests-aws4auth/files/patch-setup.py
+++ www/py-requests-aws4auth/files/patch-setup.py
@@ -1,12 +1,12 @@
---- setup.py.orig	2016-02-07 23:17:13 UTC
+--- setup.py.orig	2020-06-06 19:44:57 UTC
 +++ setup.py
 @@ -45,9 +45,7 @@ setup(
-     license='MIT License',
      keywords='requests authentication amazon web services aws s3 REST',
      install_requires=['requests'],
+     packages=['requests_aws4auth'],
 -    package_data={'requests_aws4auth': ['test/requests_aws4auth_test.py',
--                                        '../README.rst', '../LICENSE',
--                                        '../NOTICE', '../HISTORY.rst']},
+-                                        '../README.md', '../LICENSE',
+-                                        '../HISTORY.md']},
 +    package_data={'requests_aws4auth': ['test/requests_aws4auth_test.py']},
      classifiers=[
          'Development Status :: 3 - Alpha',
