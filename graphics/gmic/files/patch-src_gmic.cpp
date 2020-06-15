98c81e32734f0edf525a5a34413addddabcd8b56
Yuri Victorovich <yuri@FreeBSD.org>
2020-06-15 07:49:40 +0000

    graphics/gmic: Update 2.9.0 -> 2.9.1
    Reported by:	portscout

--- graphics/gmic/files/patch-src_gmic.cpp
+++ graphics/gmic/files/patch-src_gmic.cpp
@@ -1,6 +1,6 @@
---- src/gmic.cpp.orig	2018-12-03 08:57:03 UTC
+--- src/gmic.cpp.orig	2020-06-10 13:43:16 UTC
 +++ src/gmic.cpp
-@@ -2029,7 +2029,7 @@ double gmic::mp_ext(char *const str, voi
+@@ -2320,7 +2320,7 @@ double gmic::mp_setname(const unsigned int ind, const 
  // Manage correspondence between abort pointers and thread ids.
  CImgList<void*> gmic::list_p_is_abort = CImgList<void*>();
  bool *gmic::abort_ptr(bool *const p_is_abort) {
@@ -9,9 +9,9 @@
    void* tid = (void*)(cimg_ulong)getpid();
  #elif cimg_OS==1
    void* tid = (void*)(cimg_ulong)syscall(SYS_gettid);
-@@ -2362,7 +2362,7 @@ gmic::~gmic() {
- #endif // #if cimg_display!=0
- 
+@@ -2660,7 +2660,7 @@ gmic::~gmic() {
+   cimg::exception_mode(cimg_exception_mode);
+   cimg_forX(display_windows,l) delete &display_window(l);
    cimg::mutex(21);
 -#if defined(__MACOSX__) || defined(__APPLE__)
 +#if defined(__MACOSX__) || defined(__APPLE__) || defined(__FreeBSD__)
