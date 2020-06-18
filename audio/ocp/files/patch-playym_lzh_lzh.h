12a4b55f68e5f3eac63482d03e27ba87afb9bceb
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-06-18 15:41:32 +0000

    audio/ocp: fix build on powerpc64 with gcc
    GCC is still used on stable/11 and stable/12. It needs sys/types.h in lzh.h, because some types are not recognized.
    
    Force newer compiler due to:
    pfilesel.c:3312: error: #pragma GCC diagnostic not allowed inside functions
    
    Because of libstdc++ ABI changes due to forcing new compiler, audio/libadplug and devel/libbinio in order for audio/ocp to build.
    
    PR:		247377
    Approved by:	ehaupt (maintainer)

---
+++ audio/ocp/files/patch-playym_lzh_lzh.h
@@ -0,0 +1,10 @@
+--- playym/lzh/lzh.h.orig	2020-06-17 18:26:50 UTC
++++ playym/lzh/lzh.h
+@@ -41,6 +41,7 @@
+ #ifndef LZH_H
+ #define LZH_H
+ 
++#include <sys/types.h>
+ 
+ #define BUFSIZE (1024 * 4)
+ 
