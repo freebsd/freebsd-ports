770c4bc3cf82a48a113d0e3af61c584a6ab1d785
Kirill Ponomarev <krion@FreeBSD.org>
2020-06-18 15:03:31 +0000

    Update to 3001 Sodium. Changes: https://salt.tips/whats-new-in-salt-sodium/
    PR:		247372
    Approved by:	maintainer

--- sysutils/py-salt/files/patch-salt_loader.py
+++ sysutils/py-salt/files/patch-salt_loader.py
@@ -1,11 +1,13 @@
---- salt/loader.py
+--- salt/loader.py.orig	2020-06-18 07:20:08 UTC
 +++ salt/loader.py
-@@ -1240,7 +1240,7 @@
+@@ -1383,9 +1383,7 @@ class LazyLoader(salt.utils.lazy.LazyDict):
                  try:
                      pycache_files = [
-                         os.path.join('__pycache__', x) for x in
--                        sorted(os.listdir(os.path.join(mod_dir, '__pycache__')))
-+                        sorted(os.listdir(os.path.join(mod_dir, '__pycache__'))) if not x.endswith('opt-1.pyc') and not x.endswith('opt-2.pyc')
+                         os.path.join("__pycache__", x)
+-                        for x in sorted(
+-                            os.listdir(os.path.join(mod_dir, "__pycache__"))
+-                        )
++                        for x in sorted(os.listdir(os.path.join(mod_dir, '__pycache__'))) if not x.endswith('opt-1.pyc') and not x.endswith('opt-2.pyc')
                      ]
                  except OSError:
                      pass
--- sysutils/py-salt/files/patch-setup.py
+++ sysutils/py-salt/files/patch-setup.py
@@ -1,13 +1,13 @@
---- setup.py.orig	2018-04-02 16:35:13 UTC
+--- setup.py.orig	2020-06-18 07:24:31 UTC
 +++ setup.py
-@@ -992,8 +992,8 @@ class SaltDistribution(distutils.dist.Di
+@@ -1119,8 +1119,8 @@ class SaltDistribution(distutils.dist.Distribution):
      def _property_data_files(self):
          # Data files common to all scenarios
          data_files = [
--            ('share/man/man1', ['doc/man/salt-call.1', 'doc/man/salt-run.1']),
--            ('share/man/man7', ['doc/man/salt.7'])
+-            ("share/man/man1", ["doc/man/salt-call.1", "doc/man/salt-run.1"]),
+-            ("share/man/man7", ["doc/man/salt.7"]),
 +            ('man/man1', ['doc/man/salt-call.1', 'doc/man/salt-run.1']),
 +            ('man/man7', ['doc/man/salt.7'])
          ]
          if self.ssh_packaging or PACKAGED_FOR_SALT_SSH:
-             data_files[0][1].append('doc/man/salt-ssh.1')
+             data_files[0][1].append("doc/man/salt-ssh.1")
