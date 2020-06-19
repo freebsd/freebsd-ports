1428b4435d47c3506f2309e0f5c9557945a29a29
Niclas Zeising <zeising@FreeBSD.org>
2020-06-19 09:28:25 +0000

    x11-wm/spectrwm: Update to 3.4.0
    Update x11-wm/spectrwm to 3.4.0
    Changelog:
    https://github.com/conformal/spectrwm/releases/tag/SPECTRWM_3_4_0

--- x11-wm/spectrwm/files/patch-spectrwm.c
+++ x11-wm/spectrwm/files/patch-spectrwm.c
@@ -1,11 +1,11 @@
---- spectrwm.c.orig	2017-10-04 18:36:20 UTC
+--- spectrwm.c.orig	2020-06-17 19:26:40 UTC
 +++ spectrwm.c
-@@ -7960,7 +7960,7 @@ validate_spawns(void)
+@@ -8913,7 +8913,7 @@ validate_spawns(void)
  void
  setup_spawn(void)
  {
--	setconfspawn("lock",		"xlock",		0);
-+	setconfspawn("lock",		"xlock",		SWM_SPAWN_OPTIONAL);
+-	setconfspawn("lock", "xlock", 0, NULL);
++	setconfspawn("lock", "xlock", SWM_SPAWN_OPTIONAL, NULL);
  
- 	setconfspawn("term",		"xterm",		0);
- 	setconfspawn("spawn_term",	"xterm",		0);
+ 	setconfspawn("term", "xterm", 0, NULL);
+ 	setconfspawn("spawn_term", "xterm", 0, NULL);
