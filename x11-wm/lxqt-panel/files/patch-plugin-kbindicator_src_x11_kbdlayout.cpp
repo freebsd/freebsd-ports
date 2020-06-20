115f99186954d50853a9093bda3a36b9c9bf7aad
Jesper Schmitz Mouridsen <jsm@FreeBSD.org>
2020-06-20 09:18:08 +0000

    Upgrade to 0.15.0
    Approved by: swills (mentor)
    Differential Revision: https://reviews.freebsd.org/D24720

--- x11-wm/lxqt-panel/files/patch-plugin-kbindicator_src_x11_kbdlayout.cpp
+++ x11-wm/lxqt-panel/files/patch-plugin-kbindicator_src_x11_kbdlayout.cpp
@@ -1,15 +1,15 @@
---- plugin-kbindicator/src/x11/kbdlayout.cpp.orig	2018-08-01 13:39:24 UTC
+--- plugin-kbindicator/src/x11/kbdlayout.cpp.orig	2020-04-24 02:39:01 UTC
 +++ plugin-kbindicator/src/x11/kbdlayout.cpp
-@@ -245,10 +245,10 @@ private:
-         static LangInfo def{"Unknown", "??", "None"};
+@@ -245,10 +245,10 @@ class X11Kbd: public QAbstractNativeEventFilter (priva
+         static LangInfo def{QStringLiteral("Unknown"), QStringLiteral("??"), QStringLiteral("None")};
          static QHash<QString, LangInfo> names;
          if (names.empty()){
--            if(QFile::exists("/usr/share/X11/xkb/rules/evdev.xml")){
-+            if(QFile::exists("/usr/local/share/X11/xkb/rules/evdev.xml")){
+-            if(QFile::exists(QStringLiteral("/usr/share/X11/xkb/rules/evdev.xml"))){
++            if(QFile::exists(QStringLiteral("/usr/local/share/X11/xkb/rules/evdev.xml"))){
                  QDomDocument doc;
  
--                QFile file("/usr/share/X11/xkb/rules/evdev.xml");
-+                QFile file("/usr/local/share/X11/xkb/rules/evdev.xml");
+-                QFile file(QStringLiteral("/usr/share/X11/xkb/rules/evdev.xml"));
++                QFile file(QStringLiteral("/usr/local/share/X11/xkb/rules/evdev.xml"));
                  if (file.open(QIODevice::ReadOnly)){
                      if (doc.setContent(&file)) {
                          QDomElement docElem = doc.documentElement();
