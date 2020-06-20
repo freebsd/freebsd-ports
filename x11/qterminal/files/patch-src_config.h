a97a7d748405db39f8a27c07f7ebd5487fdac2f5
Jesper Schmitz Mouridsen <jsm@FreeBSD.org>
2020-06-20 09:13:53 +0000

    Upgrade to 0.15.0
    Approved by:	swills (mentor)
    Differential Revision: https://reviews.freebsd.org/D24719

--- x11/qterminal/files/patch-src_config.h
+++ x11/qterminal/files/patch-src_config.h
@@ -1,15 +1,11 @@
---- src/config.h.orig	2014-10-22 08:13:14 UTC
+--- src/config.h.orig	2020-05-02 17:22:39 UTC
 +++ src/config.h
-@@ -60,9 +60,9 @@
- 
- /* Some defaults for QTerminal application */
- 
--#define DEFAULT_WIDTH                  800
--#define DEFAULT_HEIGHT                 600
--#define DEFAULT_FONT                   "Monospace"
-+#define DEFAULT_WIDTH                  820
-+#define DEFAULT_HEIGHT                 597
-+#define DEFAULT_FONT                   "Liberation Mono"
- 
- // ACTIONS
- #define CLEAR_TERMINAL_SHORTCUT        "Ctrl+Shift+X"
+@@ -74,6 +74,8 @@
+ #ifdef Q_OS_MACOS
+ // Qt does not support fontconfig on macOS, so we need to use a "real" font name.
+ #define DEFAULT_FONT                   "Menlo"
++#elif defined(Q_OS_FREEBSD)
++#define DEFAULT_FONT	    	       "Liberation Mono"
+ #else
+ #define DEFAULT_FONT                   "Monospace"
+ #endif
