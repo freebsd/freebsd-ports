aebfb01c2e9b20389f6db099028a9c5baabdb602
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2020-06-24 12:52:49 +0000

    emulators/qmc2: : Prepare for Qt5.15
    PR:             247010
    Approved by:    tcberner (mentor, implicit)

---
+++ emulators/qmc2/files/patch-qmc2_softwareimagewidget.cpp
@@ -0,0 +1,10 @@
+--- qmc2/softwareimagewidget.cpp.orig	2020-06-24 12:40:06 UTC
++++ qmc2/softwareimagewidget.cpp
+@@ -3,6 +3,7 @@
+ #include <QHash>
+ #include <QDir>
+ #include <QClipboard>
++#include <QPainterPath>
+ 
+ #include "softwarelist.h"
+ #include "softwaresnapshot.h"
