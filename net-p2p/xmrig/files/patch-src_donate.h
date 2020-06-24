f1a24525be9bdd5e66c2b6e52a421b14465d558c
Emanuel Haupt <ehaupt@FreeBSD.org>
2020-06-24 06:55:32 +0000

    Update to 6.2.2

--- net-p2p/xmrig/files/patch-src_donate.h
+++ net-p2p/xmrig/files/patch-src_donate.h
@@ -1,13 +1,13 @@
---- src/donate.h.orig	2018-05-06 19:17:20 UTC
+--- src/donate.h.orig	2020-06-23 09:17:06 UTC
 +++ src/donate.h
-@@ -43,8 +43,8 @@
+@@ -44,8 +44,8 @@
   * XMR: 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
   * BTC: 1P7ujsXeX7GxQwHNnJsRMgAdNkFZmNVqJT
   */
--constexpr const int kDefaultDonateLevel = 5;
+-constexpr const int kDefaultDonateLevel = 1;
 -constexpr const int kMinimumDonateLevel = 1;
 +constexpr const int kDefaultDonateLevel = 0;
 +constexpr const int kMinimumDonateLevel = 0;
  
  
- #endif /* __DONATE_H__ */
+ #endif /* XMRIG_DONATE_H */
