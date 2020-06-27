ce5c41d70262c1880693524fd0b0e8e2dfca8588
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-06-27 09:44:58 +0000

    Remove LICENSE from /usr/local
    - Bump PORTREVISION for package change
    
    PR:		247574
    Reported by:	Shane <FreeBSD@ShaneWare.Biz>

---
+++ textproc/py-sphinx-tabs/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2019-09-10 15:58:49 UTC
++++ setup.py
+@@ -14,7 +14,6 @@ setup(
+             'semantic-ui-2.4.1/*',
+         ],
+     },
+-    data_files = [("", ["LICENSE"])],
+     url = 'https://github.com/djungelorm/sphinx-tabs',
+     license = 'MIT',
+     description = 'Tab views for Sphinx',
