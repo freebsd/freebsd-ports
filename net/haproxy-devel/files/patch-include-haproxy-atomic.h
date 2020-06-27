6ea34b90da510d9744ad70e45e773f55db676ab8
Dmitry Sivachenko <demon@FreeBSD.org>
2020-06-27 08:21:26 +0000

    Update to -dev11.

--- net/haproxy-devel/files/patch-include-haproxy-atomic.h
+++ net/haproxy-devel/files/patch-include-haproxy-atomic.h
@@ -1,5 +1,5 @@
---- include/common/hathreads.h.orig	2018-02-17 18:17:22.219400000 +0000
-+++ include/common/hathreads.h	2018-02-17 18:18:44.598422000 +0000
+--- include/haproxy/atomic.h.orig	2018-02-17 18:17:22.219400000 +0000
++++ include/haproxy/atomic.h	2018-02-17 18:18:44.598422000 +0000
 @@ -104,7 +104,7 @@ extern THREAD_LOCAL unsigned long tid_bit; /* The bit 
  /* TODO: thread: For now, we rely on GCC builtins but it could be a good idea to
   * have a header file regrouping all functions dealing with threads. */
