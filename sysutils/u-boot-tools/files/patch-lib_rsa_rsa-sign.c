26c48fcca5568d7cba9ea71de2c07710425c1a3d
Emmanuel Vadot <manu@FreeBSD.org>
2020-06-27 12:44:31 +0000

    sysutils/u-boot-tools: Unbreak on <13
    rsa tools needs malloc.h, remove this include for now, a patch
    will be sent upstream.
    While here pet portlint.

---
+++ sysutils/u-boot-tools/files/patch-lib_rsa_rsa-sign.c
@@ -0,0 +1,10 @@
+--- lib/rsa/rsa-sign.c.orig	2020-06-27 12:26:29 UTC
++++ lib/rsa/rsa-sign.c
+@@ -4,7 +4,6 @@
+  */
+ 
+ #include "mkimage.h"
+-#include <malloc.h>
+ #include <stdio.h>
+ #include <string.h>
+ #include <image.h>
