c5c1c02dbf9fdbb5b82c5e225afcd3b2fd19c22f
Jason E. Hale <jhale@FreeBSD.org>
2020-06-29 20:09:53 +0000

    Update editors/mp to 5.41

--- editors/mp/files/patch-config.sh
+++ editors/mp/files/patch-config.sh
@@ -1,6 +1,6 @@
---- config.sh.orig	2019-07-02 05:57:45 UTC
+--- config.sh.orig	2020-05-16 07:31:22 UTC
 +++ config.sh
-@@ -40,6 +40,7 @@ while [ $# -gt 0 ] ; do
+@@ -37,6 +37,7 @@ while [ $# -gt 0 ] ; do
      case $1 in
      --without-curses)    WITHOUT_CURSES=1 ;;
      --without-gtk)       WITHOUT_GTK=1 ;;
@@ -8,7 +8,7 @@
      --without-win32)     WITHOUT_WIN32=1 ;;
      --with-kde4)         WITHOUT_KDE4=0 ;;
      --without-qt)        WITHOUT_QT5=1 && WITHOUT_QT4=1 ;;
-@@ -75,6 +76,7 @@ if [ "$CONFIG_HELP" = "1" ] ; then
+@@ -71,6 +72,7 @@ if [ "$CONFIG_HELP" = "1" ] ; then
      echo "--prefix=PREFIX         Installation prefix ($PREFIX)."
      echo "--without-curses        Disable curses (text) interface detection."
      echo "--without-gtk           Disable GTK interface detection."
@@ -16,7 +16,17 @@
      echo "--without-win32         Disable win32 interface detection."
      echo "--with-kde4             Enable KDE4 interface detection."
      echo "--without-qt            Disable Qt interface detection."
-@@ -250,11 +252,11 @@ echo -n "Testing for ncursesw... "
+@@ -203,9 +205,6 @@ echo
+ (cd $MPSL && ./config.sh --prefix=$PREFIX --docdir=$PREFIX/share/doc/$APPNAME $CONF_ARGS)
+ echo
+ 
+-# import MPSL build configuration
+-[ -f "$MPSL.build.sh" ] && . $MPSL.build.sh
+-
+ cat $MPDM/config.ldflags >> config.ldflags
+ cat $MPSL/config.ldflags >> config.ldflags
+ 
+@@ -244,11 +243,11 @@ echo -n "Testing for ncursesw... "
  if [ "$WITHOUT_CURSES" = "1" ] ; then
      echo "Disabled"
  else
@@ -29,9 +39,9 @@
 +    TMP_CFLAGS="-I${NCURSESINC}"
 +    TMP_LDFLAGS="${LDFLAGS} -L${NCURSESLIB} -lncursesw"
  
-     $CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
+     $CC $CFLAGS $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
      if [ $? = 0 ] ; then
-@@ -384,14 +386,14 @@ if [ "$WITHOUT_QT5" = "1" ] ; then
+@@ -382,14 +381,14 @@ if [ "$WITHOUT_QT5" = "1" ] ; then
  else
      if which pkg-config > /dev/null 2>&1
      then
@@ -50,7 +60,7 @@
  
          if [ $? = 0 ] ; then
              echo $TMP_CFLAGS >> config.cflags
-@@ -475,15 +477,15 @@ else
+@@ -475,15 +474,15 @@ else
      echo "int main(void) { gtk_main(); return 0; } " >> .tmp.c
  
      # Try first GTK 3.0
@@ -59,7 +69,7 @@
 +    TMP_CFLAGS=`sh -c "pkg-config --cflags gtk+-$GTKVER.0" 2>/dev/null`
 +    TMP_LDFLAGS=`sh -c "pkg-config --libs gtk+-$GTKVER.0" 2>/dev/null`
  
-     $CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
+     $CC $CFLAGS $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
      if [ $? = 0 ] ; then
 -        echo "#define CONFOPT_GTK 3" >> config.h
 +        echo "#define CONFOPT_GTK $GTKVER" >> config.h
@@ -69,4 +79,4 @@
 +        echo "OK ($GTKVER.0)"
          DRIVERS="gtk $DRIVERS"
          DRV_OBJS="mpv_gtk.o $DRV_OBJS"
-     else
+         CFLAGS="$CFLAGS $TMP_CFLAGS"
--- editors/mp/files/patch-mpdm_config.sh
+++ editors/mp/files/patch-mpdm_config.sh
@@ -1,6 +1,6 @@
---- mpdm/config.sh.orig	2019-02-11 11:53:55 UTC
+--- mpdm/config.sh.orig	2020-05-10 08:05:32 UTC
 +++ mpdm/config.sh
-@@ -194,8 +194,8 @@ echo -n "Testing for regular expressions... "
+@@ -190,8 +190,8 @@ echo -n "Testing for regular expressions... "
  
  if [ "$WITH_PCRE" = 1 ] ; then
      # try first the pcre library
@@ -11,7 +11,7 @@
      echo "#include <pcreposix.h>" > .tmp.c
      echo "int main(void) { regex_t r; regmatch_t m; regcomp(&r,\".*\",REG_EXTENDED|REG_ICASE); return 0; }" >> .tmp.c
  
-@@ -373,13 +373,15 @@ else
+@@ -371,13 +371,15 @@ else
          echo "#define CONFOPT_GETTEXT 1" >> config.h
      else
          # try now with -lintl
@@ -19,17 +19,17 @@
 +        TMP_CFLAGS="-I$LOCALBASE/include"
 +        TMP_LDFLAGS="-L$LOCALBASE/lib -lintl"
  
--        $CC .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
-+        $CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
+-        $CC $CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
++        $CC $CFLAGS $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
  
          if [ $? = 0 ] ; then
              echo "OK (libintl needed)"
              echo "#define CONFOPT_GETTEXT 1" >> config.h
 +            echo "$TMP_CFLAGS" >> config.cflags
              echo "$TMP_LDFLAGS" >> config.ldflags
+             LDFLAGS="$LDFLAGS $TMP_LDFLAGS"
          else
-             echo "No"
-@@ -406,13 +408,15 @@ else
+@@ -405,13 +407,15 @@ else
          echo "#define CONFOPT_ICONV 1" >> config.h
      else
          # try now with -liconv
@@ -37,13 +37,13 @@
 +        TMP_CFLAGS="-I$ICONV_PREFIX/include"
 +        TMP_LDFLAGS="-L$ICONV_PREFIX/lib $ICONV_LIB"
  
--        $CC .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
-+        $CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
+-        $CC $CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
++        $CC $CFLAGS $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
  
          if [ $? = 0 ] ; then
              echo "OK (libiconv needed)"
              echo "#define CONFOPT_ICONV 1" >> config.h
 +            echo "$TMP_CFLAGS" >> config.cflags
              echo "$TMP_LDFLAGS" >> config.ldflags
+             LDFLAGS="$LDFLAGS $TMP_LDFLAGS"
          else
-             echo "No"
