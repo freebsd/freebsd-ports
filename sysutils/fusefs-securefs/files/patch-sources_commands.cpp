0157b050b1696113e2849ffdd6eecd42741c2278
Serhii (Sergey) Kozlov <skozlov@FreeBSD.org>
2020-06-29 22:06:57 +0000

    New port: sysutils/fusefs-securefs
    securefs is a filesystem in userspace (FUSE) with transparent encryption
    (when writing) and decryption (when reading).
    
    securefs mounts a regular directory onto a mount point. The mount point appears
    as a regular filesystem, where one can read/write/create files, directories and
    symbolic links. The underlying directory will be automatically updated to
    contain the encrypted and authenticated contents.
    
    WWW: https://github.com/netheril96/securefs

---
+++ sysutils/fusefs-securefs/files/patch-sources_commands.cpp
@@ -0,0 +1,11 @@
+--- sources/commands.cpp.orig	2020-06-29 17:57:42 UTC
++++ sources/commands.cpp
+@@ -14,7 +14,7 @@
+ #include <fuse.h>
+ #include <json/json.h>
+ #include <tclap/CmdLine.h>
+-#include <utf8proc/utf8proc.h>
++#include <utf8proc.h>
+ 
+ #include <algorithm>
+ #include <memory>
---
+++ sysutils/fusefs-securefs/files/patch-sources_mystring.cpp
@@ -0,0 +1,11 @@
+--- sources/mystring.cpp.orig	2020-06-29 17:57:55 UTC
++++ sources/mystring.cpp
+@@ -3,7 +3,7 @@
+ #include "logger.h"
+ #include "myutils.h"
+ 
+-#include <utf8proc/utf8proc.h>
++#include <utf8proc.h>
+ 
+ #include <ctype.h>
+ #include <errno.h>
