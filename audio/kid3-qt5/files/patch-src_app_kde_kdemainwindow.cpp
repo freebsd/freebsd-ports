46d1cef65accccbd6749288a5bd71ca7296ebb9e
Jason E. Hale <jhale@FreeBSD.org>
2020-06-30 20:14:27 +0000

    audio/kid3-*: Update to 3.8.3

--- audio/kid3-qt5/files/patch-src_app_kde_kdemainwindow.cpp
+++ audio/kid3-qt5/files/patch-src_app_kde_kdemainwindow.cpp
@@ -5,27 +5,27 @@ Shortcut for action  "reload" "Re&load" set with QAction::setShortcut()! Use KAc
 Shortcut for action  "filelist_rename" "&Rename" set with QAction::setShortcut()! Use KActionCollection::setDefaultShortcut(s) instead.
 Shortcut for action  "filelist_delete" "&Move to Trash" set with QAction::setShortcut()! Use KActionCollection::setDefaultShortcut(s) instead.
 
---- src/app/kde/kdemainwindow.cpp.orig	2018-12-21 05:40:14 UTC
+--- src/app/kde/kdemainwindow.cpp.orig	2020-05-09 12:12:37 UTC
 +++ src/app/kde/kdemainwindow.cpp
-@@ -180,7 +180,7 @@ void KdeMainWindow::initActions()
+@@ -189,7 +189,7 @@ void KdeMainWindow::initActions()
    action = new QAction(QIcon::fromTheme(QLatin1String("document-open")),
-                        tr("O&pen Directory..."), this);
-   action->setStatusTip(tr("Opens a directory"));
+                        tr("O&pen Folder..."), this);
+   action->setStatusTip(tr("Opens a folder"));
 -  action->setShortcut(QKeySequence(QLatin1String("Ctrl+D")));
 +  collection->setDefaultShortcut(action, QKeySequence(Qt::CTRL + Qt::Key_D));
    collection->addAction(QLatin1String("open_directory"), action);
    connect(action, &QAction::triggered,
            impl(), &BaseMainWindowImpl::slotFileOpenDirectory);
-@@ -188,7 +188,7 @@ void KdeMainWindow::initActions()
+@@ -197,7 +197,7 @@ void KdeMainWindow::initActions()
    action = new QAction(QIcon::fromTheme(QLatin1String("view-refresh")),
                         tr("Re&load"), this);
-   action->setStatusTip(tr("Reload directory"));
+   action->setStatusTip(tr("Reload folder"));
 -  action->setShortcut(QKeySequence::Refresh);
 +  collection->setDefaultShortcut(action, QKeySequence::Refresh);
    collection->addAction(QLatin1String("reload"), action);
    connect(action, &QAction::triggered,
            impl(), &BaseMainWindowImpl::slotFileReload);
-@@ -401,13 +401,13 @@ void KdeMainWindow::initActions()
+@@ -415,13 +415,13 @@ void KdeMainWindow::initActions()
    collection->addAction(QLatin1String("filelist_focus"), action);
    connect(action, &QAction::triggered, form(), &Kid3Form::setFocusFileList);
    action = new QAction(tr("&Rename"), this);
--- audio/kid3-qt5/files/patch-src_test_testmusicbrainzreleaseimporter.cpp
+++ audio/kid3-qt5/files/patch-src_test_testmusicbrainzreleaseimporter.cpp
@@ -1,17 +1,17 @@
 Fix test to reflect the current data in the MusicBrainz database.
 
---- src/test/testmusicbrainzreleaseimporter.cpp.orig	2018-12-21 05:40:14 UTC
+--- src/test/testmusicbrainzreleaseimporter.cpp.orig	2019-08-23 09:52:05 UTC
 +++ src/test/testmusicbrainzreleaseimporter.cpp
 @@ -38,7 +38,7 @@ void TestMusicBrainzReleaseImporter::testQueryAlbums()
  {
    queryAlbums(QLatin1String("Wizard"), QLatin1String("Odin"));
-   QStandardItemModel* albumModel = m_importer->getAlbumListModel();
+   AlbumListModel* albumModel = m_importer->getAlbumListModel();
 -  QCOMPARE(albumModel->rowCount(), 3);
 +  QCOMPARE(albumModel->rowCount(), 4);
-   auto  item = static_cast<AlbumListItem*>(albumModel->item(0, 0));
-   QVERIFY(item);
-   QVERIFY(item->type() == AlbumListItem::Type);
-@@ -54,9 +54,9 @@ void TestMusicBrainzReleaseImporter::testQueryTracks()
+   QString text, category, id;
+   albumModel->getItem(0, text, category, id);
+   QCOMPARE(text, QString(QLatin1String("Wizard - Odin")));
+@@ -53,9 +53,9 @@ void TestMusicBrainzReleaseImporter::testQueryTracks()
    QStringList titles;
    titles << QLatin1String("The Prophecy") << QLatin1String("Betrayer")
           << QLatin1String("Dead Hope") << QLatin1String("Dark God")
@@ -23,7 +23,7 @@ Fix test to reflect the current data in the MusicBrainz database.
           << QLatin1String("Hall of Odin") << QLatin1String("The Powergod")
           << QLatin1String("March of the Einheriers")
           << QLatin1String("End of All");
-@@ -66,7 +66,7 @@ void TestMusicBrainzReleaseImporter::testQueryTracks()
+@@ -65,7 +65,7 @@ void TestMusicBrainzReleaseImporter::testQueryTracks()
            << QLatin1String("5:08") << QLatin1String("4:01")
            << QLatin1String("5:01") << QLatin1String("5:06")
            << QLatin1String("5:21") << QLatin1String("5:40")
