dbc5577117accb7f0b4f3e667c2ca79ff2672236
John Hixson <jhixson@FreeBSD.org>
2020-07-02 17:38:07 +0000

    sysutils/mtpfs: update to use github
    PR:	200078
    Reported by:	jensh604@gmail.com

--- sysutils/mtpfs/files/patch-mtpfs.c
+++ sysutils/mtpfs/files/patch-mtpfs.c
@@ -1,45 +1,11 @@
---- mtpfs.c.orig	2012-02-27 00:59:39 UTC
+--- mtpfs.c.orig	2016-12-15 23:55:52 UTC
 +++ mtpfs.c
-@@ -1270,7 +1270,7 @@ mtpfs_rename (const char *oldname, const
- }
+@@ -1774,7 +1774,7 @@ main (int argc, char *argv[])
  
- static int
--mtpfs_statfs (const char *path, struct statfs *stbuf)
-+mtpfs_statfs (const char *path, struct statvfs *stbuf)
- {
-     DBG("mtpfs_statfs");
-     stbuf->f_bsize=1024;
-@@ -1282,7 +1282,7 @@ mtpfs_statfs (const char *path, struct s
- }
+   DBG ("Start fuse");
  
- void *
--mtpfs_init ()
-+mtpfs_init (struct fuse_conn_info *conn)
- {
-     LIBMTP_devicestorage_t *storage;
-     DBG("mtpfs_init");
-@@ -1298,8 +1298,14 @@ mtpfs_blank()
-     // Do nothing
- }
- 
-+int
-+mtpfs_blank_chmod(const char *path, mode_t mode)
-+{
-+	return (0);
-+}
-+
- static struct fuse_operations mtpfs_oper = {
--    .chmod   = mtpfs_blank,
-+    .chmod   = mtpfs_blank_chmod,
-     .release = mtpfs_release,
-     .readdir = mtpfs_readdir,
-     .getattr = mtpfs_getattr,
-@@ -1428,7 +1434,7 @@ main (int argc, char *argv[])
- 
-     DBG("Start fuse");
- 
--    fuse_stat=fuse_main (argc, argv, &mtpfs_oper);
-+    fuse_stat=fuse_main (argc, argv, &mtpfs_oper, NULL);
-     DBG("fuse_main returned %d\n", fuse_stat);
-     return fuse_stat;
+-  fuse_stat = fuse_main (argc, argv, &mtpfs_oper);
++  fuse_stat = fuse_main (argc, argv, &mtpfs_oper, NULL);
+   DBG ("fuse_main returned %d\n", fuse_stat);
+   return fuse_stat;
  }
--- sysutils/mtpfs/files/patch-mtpfs.h
+++ sysutils/mtpfs/files/patch-mtpfs.h
@@ -1,31 +1,11 @@
---- mtpfs.h.orig	2012-02-24 04:37:14 UTC
+--- mtpfs.h.orig	2016-12-15 23:55:52 UTC
 +++ mtpfs.h
-@@ -18,7 +18,9 @@
+@@ -17,7 +17,7 @@
  #include <fcntl.h>
  #include <dirent.h>
  #include <errno.h>
 -#include <sys/statfs.h>
-+#include <sys/param.h>
 +#include <sys/mount.h>
-+#include <sys/statvfs.h>
  
  #include <libmtp.h>
  #include <glib.h>
-@@ -49,7 +51,7 @@ static int find_storage(const gchar * pa
- 
- 
-     /* fuse functions */
--static void * mtpfs_init (void);
-+static void * mtpfs_init (struct fuse_conn_info *);
- static int mtpfs_blank ();
- static int mtpfs_release (const char *path, struct fuse_file_info *fi);
- void mtpfs_destroy ();
-@@ -62,7 +64,7 @@ static int mtpfs_write (const gchar * pa
- static int mtpfs_unlink (const gchar * path);
- static int mtpfs_mkdir (const char *path, mode_t mode);
- static int mtpfs_rmdir (const char *path);
--static int mtpfs_statfs (const char *path, struct statfs *stbuf);
-+static int mtpfs_statfs (const char *path, struct statvfs *stbuf);
- int calc_length(int f);
- 
- static LIBMTP_mtpdevice_t *device;
