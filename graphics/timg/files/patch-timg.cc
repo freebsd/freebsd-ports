5e2e62accee3fe262c327b022846aeeab79e4a45
Li-Wen Hsu <lwhsu@FreeBSD.org>
2020-07-03 08:35:14 +0000

    Update to 0.9.8
    PR:		247732
    Submitted by:	Zsolt Udvari <uzsolt@uzsolt.hu> (maintainer)

--- graphics/timg/files/patch-timg.cc
+++ graphics/timg/files/patch-timg.cc
@@ -1,6 +1,6 @@
---- timg.cc.orig	2016-06-27 04:44:39 UTC
+--- timg.cc.orig	2020-04-30 05:40:03 UTC
 +++ timg.cc
-@@ -66,9 +66,9 @@ void CopyToCanvas(const Magick::Image &i
+@@ -66,9 +66,9 @@ void CopyToCanvas(const Magick::Image &img, TerminalCa
              if (c.alphaQuantum() >= 255)
                  continue;
              result->SetPixel(x, y,
@@ -13,7 +13,7 @@
          }
      }
  }
-@@ -222,9 +222,9 @@ void DisplayScrolling(const Magick::Imag
+@@ -271,9 +271,9 @@ void DisplayScrolling(const Magick::Image &img, int sc
              if (src.alphaQuantum() >= 255)
                  continue;
              RGBCol &dest = fast_image[y * img_width + x];
