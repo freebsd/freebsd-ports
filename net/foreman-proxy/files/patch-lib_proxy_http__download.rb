b476e222143ee970b422a0bbef19021acecdce8c
Li-Wen Hsu <lwhsu@FreeBSD.org>
2020-07-03 16:55:58 +0000

    Update to 1.21.2
    PR:		237289
    Submitted by:	Frank Wall <fw@moov.de>

--- net/foreman-proxy/files/patch-lib_proxy_http__download.rb
+++ net/foreman-proxy/files/patch-lib_proxy_http__download.rb
@@ -1,11 +1,11 @@
---- lib/proxy/http_download.rb.orig	2017-05-08 15:18:42 UTC
-+++ lib/proxy/http_download.rb
-@@ -6,7 +6,7 @@ module Proxy
+--- lib/proxy/http_download.rb.orig	2018-12-06 13:30:25.000000000 +0100
++++ lib/proxy/http_download.rb	2019-02-11 22:33:58.455890000 +0100
+@@ -9,7 +9,7 @@
  
-     def initialize(src, dst)
+     def initialize(src, dst, read_timeout = nil, connect_timeout = nil, dns_timeout = nil)
        @dst = dst
 -      wget = which("wget")
 +      wget = "%%LOCALBASE%%/bin/wget"
-       super("#{wget} --timeout=10 --tries=3 --no-check-certificate -nv -c \"#{escape_for_shell(src.to_s)}\" -O \"#{escape_for_shell(dst.to_s)}\"")
-     end
- 
+       read_timeout ||= DEFAULT_READ_TIMEOUT
+       dns_timeout ||= DEFAULT_CONNECT_TIMEOUT
+       connect_timeout ||= DEFAULT_DNS_TIMEOUT
