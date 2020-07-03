945c4e5ade40198be54cf4e6bb186bb5a2436dc7
Tobias C. Berner <tcberner@FreeBSD.org>
2020-07-03 06:16:08 +0000

    audio/muse-sequencer: re-prepare for Qt5-5.15
    PR:		247010

---
+++ audio/muse-sequencer/files/patch-muse_mixer_routedialog.cpp
@@ -0,0 +1,10 @@
+--- muse/mixer/routedialog.cpp.orig	2020-07-03 05:46:17 UTC
++++ muse/mixer/routedialog.cpp
+@@ -35,6 +35,7 @@
+ #include <QLayout>
+ #include <QFlags>
+ #include <QVariant>
++#include <QPainterPath>
+ 
+ #include "routedialog.h"
+ #include "globaldefs.h"
