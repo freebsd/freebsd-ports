15a8b1c66357c31fbe5daf6aaa670eed5a2081e1
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-07-04 12:47:46 +0000

    www/node12: fix runtime on powerpc64 elfv1, build / runtime on powerpc64 elfv2
    Patches are backported from www/node and changed where necessary.
    
    PR:             247433
    Approved by:    bhughes (maintainer timeout)
    MFH:            2020Q3 (fix build / runtime blanket)

--- www/node12/files/patch-deps_v8_src_execution_simulator.h
+++ www/node12/files/patch-deps_v8_src_execution_simulator.h
@@ -1,11 +1,11 @@
---- deps/v8/src/execution/simulator.h.orig	2020-04-28 09:45:23 UTC
+--- deps/v8/src/execution/simulator.h.orig	2019-11-21 21:14:01 UTC
 +++ deps/v8/src/execution/simulator.h
-@@ -122,7 +122,7 @@ class GeneratedCode {
+@@ -122,7 +122,7 @@
  
    DISABLE_CFI_ICALL Return Call(Args... args) {
      // When running without a simulator we call the entry directly.
 -#if V8_OS_AIX
-+#if V8_TARGET_ARCH_PPC_BE
++#if V8_OS_AIX || (V8_TARGET_ARCH_PPC_BE && (!defined(_CALL_ELF) || _CALL_ELF == 1))
      // AIX ABI requires function descriptors (FD).  Artificially create a pseudo
      // FD to ensure correct dispatch to generated code.  The 'volatile'
      // declaration is required to avoid the compiler from not observing the
