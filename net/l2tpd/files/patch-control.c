88b1f4328d007c13d2f039bc7cb1952c40a00611
Tobias Kortkamp <tobik@FreeBSD.org>
2020-07-05 09:46:37 +0000

    net/l2tpd: Unbreak build with GCC 10 (-fno-common default)
    /usr/local/bin/ld: pty.o:(.bss+0x0): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: misc.o:(.bss+0x0): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: control.o:(.bss+0x0): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: avp.o:(.bss+0x0): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: call.o:(.bss+0x0): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: network.o:(.bss+0x140): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: avpsend.o:(.bss+0x0): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: scheduler.o:(.bss+0x8): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: file.o:(.bss+0x228): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: aaa.o:(.bss+0x0): multiple definition of `rand_source'; l2tpd.o:(.bss+0x530): first defined here
    /usr/local/bin/ld: control.o: in function `handle_special':
    control.c:(.text+0x220e): undefined reference to `check_control'
    /usr/local/bin/ld: control.o: in function `handle_packet':
    control.c:(.text+0x22d0): undefined reference to `check_payload'
    /usr/local/bin/ld: control.c:(.text+0x22e6): undefined reference to `expand_payload'
    /usr/local/bin/ld: control.c:(.text+0x231a): undefined reference to `check_control'
    /usr/local/bin/ld: control.c:(.text+0x2414): undefined reference to `write_packet'
    
    http://package18.nyi.freebsd.org/data/121amd64-default-PR244494/2020-06-10_18h04m44s/logs/errors/l2tpd-0.69_10.log
    
    PR:		246700

--- net/l2tpd/files/patch-control.c
+++ net/l2tpd/files/patch-control.c
@@ -1,5 +1,5 @@
---- control.c.orig.o	Thu Apr 11 00:09:33 2002
-+++ control.c	Tue Aug  9 13:20:07 2005
+--- control.c.orig	2002-04-10 20:09:33 UTC
++++ control.c
 @@ -16,8 +16,6 @@
  #include <errno.h>
  #include <string.h>
@@ -9,7 +9,43 @@
  #include <unistd.h>
  #include <stdlib.h>
  #include "l2tp.h"
-@@ -1582,6 +1580,12 @@
+@@ -1076,7 +1074,7 @@ int control_finish (struct tunnel *t, struct call *c)
+     return 0;
+ }
+ 
+-inline int check_control (const struct buffer *buf, struct tunnel *t,
++static inline int check_control (const struct buffer *buf, struct tunnel *t,
+                           struct call *c)
+ {
+     /*
+@@ -1212,7 +1210,7 @@ inline int check_control (const struct buffer *buf, st
+     return 0;
+ }
+ 
+-inline int check_payload (struct buffer *buf, struct tunnel *t,
++static inline int check_payload (struct buffer *buf, struct tunnel *t,
+                           struct call *c)
+ {
+     /*
+@@ -1318,7 +1316,7 @@ inline int check_payload (struct buffer *buf, struct t
+ #endif
+     return 0;
+ }
+-inline int expand_payload (struct buffer *buf, struct tunnel *t,
++static inline int expand_payload (struct buffer *buf, struct tunnel *t,
+                            struct call *c)
+ {
+     /*
+@@ -1498,7 +1496,7 @@ void send_zlb (void *data)
+     toss (buf);
+ }
+ 
+-inline int write_packet (struct buffer *buf, struct tunnel *t, struct call *c,
++static inline int write_packet (struct buffer *buf, struct tunnel *t, struct call *c,
+                          int convert)
+ {
+     /*
+@@ -1582,6 +1580,12 @@ inline int write_packet (struct buffer *buf, struct tu
      wbuf[pos++] = e;
      for (x = 0; x < buf->len; x++)
      {
@@ -22,3 +58,12 @@
          e = *((char *) buf->start + x);
          if ((e < 0x20) || (e == PPP_ESCAPE) || (e == PPP_FLAG))
          {
+@@ -1657,7 +1661,7 @@ void handle_special (struct buffer *buf, struct call *
+     }
+ }
+ 
+-inline int handle_packet (struct buffer *buf, struct tunnel *t,
++int handle_packet (struct buffer *buf, struct tunnel *t,
+                           struct call *c)
+ {
+     int res;
