f89602260d39d382f05aa7a2d70dfbad029f5f16
Yuri Victorovich <yuri@FreeBSD.org>
2020-07-07 08:58:22 +0000

    ecurity/helib: Update 1.0.1 -> 1.0.2
    Reported by:	portscout

--- security/helib/files/patch-src_timing.cpp
+++ security/helib/files/patch-src_timing.cpp
@@ -1,11 +1,11 @@
---- src/timing.cpp.orig	2020-01-16 05:36:23 UTC
+--- src/timing.cpp.orig	2020-07-06 09:01:51 UTC
 +++ src/timing.cpp
-@@ -23,7 +23,7 @@ unsigned long GetTimerClock()
+@@ -22,7 +22,7 @@ unsigned long GetTimerClock()
+ {
    timespec ts;
- 
  
 -  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
 +  clock_gettime(CLOCK_MONOTONIC, &ts);
-   //clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts);
-   //clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts);
+   // clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts);
+   // clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts);
  
