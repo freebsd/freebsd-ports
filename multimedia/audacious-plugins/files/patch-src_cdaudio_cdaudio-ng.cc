ec6a4ada8e6c39d48a9d1d176be9dd14550ea721
Christian Weisgerber <naddy@FreeBSD.org>
2020-07-07 14:47:51 +0000

    Switch the CDDB default to gnudb.org. The freedb.org CD track database has been discontinued.

---
+++ multimedia/audacious-plugins/files/patch-src_cdaudio_cdaudio-ng.cc
@@ -0,0 +1,11 @@
+--- src/cdaudio/cdaudio-ng.cc.orig	2020-07-07 14:41:19 UTC
++++ src/cdaudio/cdaudio-ng.cc
+@@ -126,7 +126,7 @@ const char * const CDAudio::defaults[] = {
+  "use_cdtext", "TRUE",
+  "use_cddb", "TRUE",
+  "cddbhttp", "FALSE",
+- "cddbserver", "freedb.org",
++ "cddbserver", "gnudb.gnudb.org",
+  "cddbport", "8880",
+  nullptr};
+ 
