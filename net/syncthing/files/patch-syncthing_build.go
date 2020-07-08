42cd59e5e43b3fc202aacee92cd69c3c28630c4e
Steve Wills <swills@FreeBSD.org>
2020-07-08 14:03:50 +0000

    net/syncthing: update to 1.6.1

--- net/syncthing/files/patch-syncthing_build.go
+++ net/syncthing/files/patch-syncthing_build.go
@@ -1,11 +1,11 @@
---- syncthing/build.go.orig	2020-02-05 04:20:26 UTC
+--- syncthing/build.go.orig	2020-06-02 09:38:39 UTC
 +++ syncthing/build.go
-@@ -443,7 +443,7 @@ func build(target target, tags []string) {
+@@ -404,7 +404,7 @@
+ 		defer shouldCleanupSyso(sysoPath)
  	}
  
- 	for _, pkg := range target.buildPkgs {
--		args := []string{"build", "-v"}
-+		args := []string{"build"}
- 		args = appendParameters(args, tags, pkg)
- 
- 		runPrint(goCmd, args...)
+-	args := []string{"install", "-v"}
++	args := []string{"install"}
+ 	args = appendParameters(args, tags, target.buildPkgs...)
+ 	runPrint(goCmd, args...)
+ }
