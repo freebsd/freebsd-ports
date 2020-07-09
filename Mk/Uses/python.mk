0b6652315b101f12c0097caf4cbec0289b04db07
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-07-09 18:11:01 +0000

    Update PYNUMPY
    science/py-geometer requires math/py-numpy between 1.15 and 1.20 (>=1.15,<1.20)

--- Mk/Uses/python.mk
+++ Mk/Uses/python.mk
@@ -632,7 +632,7 @@ CMAKE_ARGS+=	-DPython_ADDITIONAL_VERSIONS=${PYTHON_VER}
 
 # Python 3rd-party modules
 PYGAME=		${PYTHON_PKGNAMEPREFIX}game>0:devel/py-game@${PY_FLAVOR}
-PYNUMPY=	${PYTHON_PKGNAMEPREFIX}numpy>=1.16,1<1.19,1:math/py-numpy@${PY_FLAVOR}
+PYNUMPY=	${PYTHON_PKGNAMEPREFIX}numpy>=1.16,1<1.20,1:math/py-numpy@${PY_FLAVOR}
 
 # Common Python modules that can be needed but only for some versions of Python.
 .if ${PYTHON_REL} < 3500
