0b6e9181ba750e9431546e0c9af4e36ad8b94b66
Boris Samorodov <bsam@FreeBSD.org>
2020-07-12 13:02:20 +0000

    devel/libftdi1: Update to version 1.5 and:
    - mark dual licensed;
    - regenerate patches;
    - pet portlint (shuffle some Makefile lines).
    
    New in 1.5 - 2020-07-07
    -----------------------
    * Implement tc[io]flush methods & deprecate broken purge_buffers methods
    
      Please check your code for ftdi_usb_purge_rx_buffer(),
      ftdi_usb_purge_tx_buffer() and ftdi_usb_purge_buffers()
      and migrate to the new ftdi_tc[io]flush() methods.
    
      Old code will continue to function, but you'll get
      a deprecation warning during compilation.
    
    * Add program to test buffer flush (purge) functionality
    * Add kernel driver auto attach/detach.
      See new AUTO_DETACH_REATACH_SIO_MODULE option
    * Add ftdi_setflowctrl_xonxoff()
    * ftdi_eeprom / eeprom handling:
      * Unify handling of all boolean eeprom flags
      * Add device release number support
      * Add channel_a_driver support for type xxR chips
      * Add support for group0 drive levels on x232H chips
      * Fix handling of high_current_drive parameter
      * Fix inverted handling of VCP driver field for TYPE_R chips
      * New --verbose option for eeprom decode operation
    * Add example code for async mode
    * Add SPDX license identifiers to the core library & ftdi_eeprom
    * Various python SWIG wrapper improvements
    * Various cmake file improvements
    * Fix small bugs in error code paths

--- devel/libftdi1/files/patch-cmake__FindUSB1.cmake
+++ devel/libftdi1/files/patch-cmake__FindUSB1.cmake
@@ -1,11 +1,11 @@
---- cmake/FindUSB1.cmake.orig	2014-08-02 12:05:59.000000000 +0200
-+++ cmake/FindUSB1.cmake	2014-08-02 12:06:34.000000000 +0200
-@@ -26,7 +26,7 @@
+--- cmake/FindUSB1.cmake.orig	2020-07-07 19:32:55 UTC
++++ cmake/FindUSB1.cmake
+@@ -26,7 +26,7 @@ else (LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
      PATH_SUFFIXES libusb-1.0
      PATHS ${PC_LIBUSB_INCLUDEDIR} ${PC_LIBUSB_INCLUDE_DIRS})
-
+ 
 -  FIND_LIBRARY(LIBUSB_LIBRARIES NAMES usb-1.0
 +  FIND_LIBRARY(LIBUSB_LIBRARIES NAMES usb-1.0 usb
      PATHS ${PC_LIBUSB_LIBDIR} ${PC_LIBUSB_LIBRARY_DIRS})
-
+ 
    include(FindPackageHandleStandardArgs)
