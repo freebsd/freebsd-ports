b9a4f58f7d6271396528acf3f28e271093ebe74e
Yuri Victorovich <yuri@FreeBSD.org>
2020-07-16 06:06:58 +0000

    databases/arrow: Update 0.17.1 -> 1.0.0
    Reported by:	portscout

--- databases/arrow/files/patch-cmake__modules_SetupCxxFlags.cmake
+++ databases/arrow/files/patch-cmake__modules_SetupCxxFlags.cmake
@@ -1,11 +1,11 @@
---- cmake_modules/SetupCxxFlags.cmake.orig	2020-06-15 16:23:49 UTC
+--- cmake_modules/SetupCxxFlags.cmake.orig	2020-07-15 21:43:39 UTC
 +++ cmake_modules/SetupCxxFlags.cmake
-@@ -27,7 +27,7 @@ if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch
-   set(ARROW_CPU_FLAG "armv8")
- elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "armv7")
-   set(ARROW_CPU_FLAG "armv7")
--elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "ppc")
-+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "powerpc|powerpc64|ppc")
-   set(ARROW_CPU_FLAG "ppc")
- elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "s390x")
-   set(ARROW_CPU_FLAG "s390x")
+@@ -27,7 +27,7 @@ if(NOT DEFINED ARROW_CPU_FLAG)
+     set(ARROW_CPU_FLAG "armv8")
+   elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "armv7")
+     set(ARROW_CPU_FLAG "armv7")
+-  elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "ppc")
++  elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "powerpc|powerpc64|ppc")
+     set(ARROW_CPU_FLAG "ppc")
+   elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "s390x")
+     set(ARROW_CPU_FLAG "s390x")
