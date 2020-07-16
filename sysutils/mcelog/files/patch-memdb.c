2ad48d29685f1eca43116f3d00a90127c7a07423
Richard Gallamore <ultima@FreeBSD.org>
2020-07-16 08:12:05 +0000

    Updated to 170

--- sysutils/mcelog/files/patch-memdb.c
+++ sysutils/mcelog/files/patch-memdb.c
@@ -1,11 +1,11 @@
---- memdb.c.orig	2018-05-22 17:06:36 UTC
+--- memdb.c.orig	2020-07-16 03:44:57 UTC
 +++ memdb.c
-@@ -172,7 +172,9 @@ void memdb_trigger(char *msg, struct memdimm *md,  tim
+@@ -173,7 +173,9 @@ void memdb_trigger(char *msg, struct memdimm *md,  tim
  	xasprintf(&env[ei++], "THRESHOLD_COUNT=%d", bucket->count);
  	env[ei] = NULL;	
  	assert(ei < MAX_ENV);
 +#ifdef __Linux__
- 	run_trigger(bc->trigger, args, env, sync);
+ 	run_trigger(bc->trigger, args, env, sync, reporter);
 +#endif
  	for (i = 0; i < ei; i++)
  		free(env[i]);
