88700ad75af90b7ed391a06e691b9ddb2a8f9c97
Bernhard Froehlich <decke@FreeBSD.org>
2020-07-17 06:42:47 +0000

    multimedia/kodi-addon-pvr-hts: Update to 4.4.21 to chase kodi update

---
+++ multimedia/kodi-addon-pvr-hts/files/patch-src_tvheadend_entity_Event.h
@@ -0,0 +1,30 @@
+[ 74%] Building CXX object CMakeFiles/pvr.hts.dir/src/tvheadend/entity/RecordingBase.cpp.o
+/usr/bin/c++  -DADDON_GLOBAL_VERSION_GUI_USED -DADDON_GLOBAL_VERSION_MAIN_USED -DADDON_INSTANCE_VERSION_PVR_USED -DBUILD_KODI_ADDON -DUSE_DEMUX -Dpvr_hts_EXPORTS -I/usr/local/include -I/usr/local/include/kodi -I/usr/local/include/p8-platform -I/usr/home/decke/dev/ports/multimedia/kodi-addon-pvr-hts/work/pvr.hts-4.4.2-Leia/lib -std=c++11 -O2 -pipe -fstack-protector -fno-strict-aliasing -fPIC   -D_LINUX -DTARGET_POSIX -DTARGET_FREEBSD -DHAVE_SSE=1 -DHAVE_SSE2=1 -DHAVE_SSE3=1 -DHAVE_SSSE3=1 -DHAVE_SSE4_1=1 -DHAVE_SSE4_2=1 -o CMakeFiles/pvr.hts.dir/src/tvheadend/entity/RecordingBase.cpp.o -c /usr/home/decke/dev/ports/multimedia/kodi-addon-pvr-hts/work/pvr.hts-4.4.2-Leia/src/tvheadend/entity/RecordingBase.cpp
+--- CMakeFiles/pvr.hts.dir/src/tvheadend/entity/Event.cpp.o ---
+In file included from /usr/home/decke/dev/ports/multimedia/kodi-addon-pvr-hts/work/pvr.hts-4.4.2-Leia/src/tvheadend/entity/Event.cpp:22:
+/usr/home/decke/dev/ports/multimedia/kodi-addon-pvr-hts/work/pvr.hts-4.4.2-Leia/src/tvheadend/entity/Event.h:105:7: error: unknown type name 'time_t'
+      time_t GetStart() const { return m_start; }
+      ^
+/usr/home/decke/dev/ports/multimedia/kodi-addon-pvr-hts/work/pvr.hts-4.4.2-Leia/src/tvheadend/entity/Event.h:106:21: error: unknown type name 'time_t'
+      void SetStart(time_t start) { m_start = start; }
+                    ^
+/usr/home/decke/dev/ports/multimedia/kodi-addon-pvr-hts/work/pvr.hts-4.4.2-Leia/src/tvheadend/entity/Event.h:108:7: error: unknown type name 'time_t'
+      time_t GetStop() const { return m_stop; }
+      ^
+/usr/home/decke/dev/ports/multimedia/kodi-addon-pvr-hts/work/pvr.hts-4.4.2-Leia/src/tvheadend/entity/Event.h:109:20: error: unknown type name 'time_t'
+      void SetStop(time_t stop) { m_stop = stop; }
+                   ^
+/usr/home/decke/dev/ports/multimedia/kodi-addon-pvr-hts/work/pvr.hts-4.4.2-Leia/src/tvheadend/entity/Event.h:117:7: error: unknown type name 'time_t'
+      time_t GetAired() const { return m_aired; }
+
+Submitted by:	Bernhard Froehlich <decke@FreeBSD.org>
+--- src/tvheadend/entity/Event.h.orig	2018-10-27 09:20:29 UTC
++++ src/tvheadend/entity/Event.h
+@@ -22,6 +22,7 @@
+  */
+ 
+ #include "Entity.h"
++#include <sys/types.h>
+ #include <map>
+ #include <vector>
+ #include <cstdint>
