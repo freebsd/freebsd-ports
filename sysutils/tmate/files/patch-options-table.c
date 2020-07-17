f70cd751cd72ffe720b26c50511b82b6c1578dd3
Steve Wills <swills@FreeBSD.org>
2020-07-17 04:54:36 +0000

    sysutils/tmate: update to 2.4.0

--- sysutils/tmate/files/patch-options-table.c
+++ sysutils/tmate/files/patch-options-table.c
@@ -1,40 +1,25 @@
---- options-table.c.orig	2016-03-29 03:30:07 UTC
+--- options-table.c.orig	2020-07-17 03:22:27 UTC
 +++ options-table.c
-@@ -925,15 +925,34 @@ const struct options_table_entry options_table[] = {
+@@ -925,19 +925,19 @@ const struct options_table_entry options_table[] = {
  	{ .name = "tmate-server-rsa-fingerprint",
  	  .type = OPTIONS_TABLE_STRING,
  	  .scope = OPTIONS_TABLE_SERVER,
--	  .default_str = "af:2d:81:c1:fe:49:70:2d:7f:09:a9:d7:4b:32:e3:be"
-+	  .default_str = "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00"
+-	  .default_str = "SHA256:Hthk2T/M/Ivqfk1YYUn5ijC2Att3+UPzD7Rn72P5VWs"
++	  .default_str = "SHA256:0000000000000000000000000000000000000000000"
  	},
  
  	{ .name = "tmate-server-ecdsa-fingerprint",
  	  .type = OPTIONS_TABLE_STRING,
  	  .scope = OPTIONS_TABLE_SERVER,
--	  .default_str = "c7:a1:51:36:d2:bb:35:4b:0a:1a:c0:43:97:74:ea:42"
-+	  .default_str = "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00"
+-	  .default_str = "SHA256:8GmKHYHEJ6n0TEdciHeEGkKOigQfCFuBULdt6vZIhDc"
++	  .default_str = "SHA256:0000000000000000000000000000000000000000000"
  	},
--
-+	{ .name = "tmate-server-dss-fingerprint",
-+	  .type = OPTIONS_TABLE_STRING,
-+	  .scope = OPTIONS_TABLE_SERVER,
-+	  .default_str = "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00"
-+	},
-+	{ .name = "tmate-server-ed25519-fingerprint",
-+	  .type = OPTIONS_TABLE_STRING,
-+	  .scope = OPTIONS_TABLE_SERVER,
-+	  .default_str = "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00"
-+	},
-+	{ .name = "tmate-server-dss-cert01-fingerprint",
-+	  .type = OPTIONS_TABLE_STRING,
-+	  .scope = OPTIONS_TABLE_SERVER,
-+	  .default_str = "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00"
-+	},
-+	{ .name = "tmate-server-rsa-cert01-fingerprint",
-+	  .type = OPTIONS_TABLE_STRING,
-+	  .scope = OPTIONS_TABLE_SERVER,
-+	  .default_str = "00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00"
-+	},
+ 
+ 	{ .name = "tmate-server-ed25519-fingerprint",
+ 	  .type = OPTIONS_TABLE_STRING,
+ 	  .scope = OPTIONS_TABLE_SERVER,
+-	  .default_str = "SHA256:jfttvoypkHiQYUqUCwKeqd9d1fJj/ZiQlFOHVl6E9sI"
++	  .default_str = "SHA256:0000000000000000000000000000000000000000000"
+ 	},
+ 
  	{ .name = "tmate-display-time",
- 	  .type = OPTIONS_TABLE_NUMBER,
- 	  .scope = OPTIONS_TABLE_SESSION,
