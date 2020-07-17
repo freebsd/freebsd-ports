b0f1d6430efe60a7d9a3aa11e8ed28a80842469b
Tobias Kortkamp <tobik@FreeBSD.org>
2020-07-17 06:44:04 +0000

    lang/rust: Update to 1.45.0
    - This Rust release comes with LLVM 10.  Drop the LLVM patches we
      no longer need.
    - On FreeBSD 13.0 powerpc64 set the elfv2 ABI explicitly in the
      Rust target instead of patching LLVM.
    - Build LLVM with Ninja.  It can shorten the build time slightly.
    - Force rebuild all consumers to catch regressions early
    
    Changes:	https://blog.rust-lang.org/2020/07/16/Rust-1.45.0.html
    Tested by:	mikael, pkubaj, tobik
    With hat:	rust
    Differential Revision:	https://reviews.freebsd.org/D25658

---
+++ lang/rust/files/powerpc64-elfv2/patch-src_librustc__target_spec_powerpc64__unknown__freebsd.rs
@@ -0,0 +1,14 @@
+--- src/librustc_target/spec/powerpc64_unknown_freebsd.rs.orig	2020-07-15 11:31:58 UTC
++++ src/librustc_target/spec/powerpc64_unknown_freebsd.rs
+@@ -17,6 +17,10 @@ pub fn target() -> TargetResult {
+         target_env: String::new(),
+         target_vendor: "unknown".to_string(),
+         linker_flavor: LinkerFlavor::Gcc,
+-        options: TargetOptions { target_mcount: "_mcount".to_string(), ..base },
++        options: TargetOptions {
++            target_mcount: "_mcount".to_string(),
++            llvm_abiname: "elfv2".to_string(),
++            ..base
++        },
+     })
+ }
