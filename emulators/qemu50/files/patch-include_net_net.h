09d18ea16822a118571e227746c5a8c6467a75a2
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2020-07-18 22:48:23 +0000

    [NEW] emulators/qemu50: QEMU CPU Emulator - 5.0.X branch
    QEMU is a FAST! processor emulator using dynamic translation to achieve
    good emulation speed.
    QEMU has two operating modes:
    
    * Full system emulation. In this mode, QEMU emulates a full system
      (for example a PC), including a processor and various peripherials.
      It can be used to launch different Operating Systems without rebooting
      the PC or to debug system code.
    * User mode emulation (Linux host only). In this mode, QEMU can launch
      Linux processes compiled for one CPU on another CPU. It can be used to
      launch the Wine Windows API emulator or to ease cross-compilation and
      cross-debugging.
    
    As QEMU requires no host kernel patches to run, it is very safe and easy to use.
    
    See also the preconfigured system images on http://oszoo.org/
    Many live cd isos also work.
    
    WWW: http://wiki.qemu.org/Main_Page

---
+++ emulators/qemu50/files/patch-include_net_net.h
@@ -0,0 +1,13 @@
+--- include/net/net.h.orig	2020-04-28 16:49:24 UTC
++++ include/net/net.h
+@@ -208,8 +208,8 @@ void netdev_add(QemuOpts *opts, Error **errp);
+ int net_hub_id_for_client(NetClientState *nc, int *id);
+ NetClientState *net_hub_port_find(int hub_id);
+ 
+-#define DEFAULT_NETWORK_SCRIPT "/etc/qemu-ifup"
+-#define DEFAULT_NETWORK_DOWN_SCRIPT "/etc/qemu-ifdown"
++#define DEFAULT_NETWORK_SCRIPT PREFIX "/etc/qemu-ifup"
++#define DEFAULT_NETWORK_DOWN_SCRIPT PREFIX "/etc/qemu-ifdown"
+ #define DEFAULT_BRIDGE_HELPER CONFIG_QEMU_HELPERDIR "/qemu-bridge-helper"
+ #define DEFAULT_BRIDGE_INTERFACE "br0"
+ 
