215732021ecfa8101d1b0e31037587a8dc28a094
Matthew Seaman <matthew@FreeBSD.org>
2020-07-18 16:18:15 +0000

    Update to 2.11
    ChangeLog:	https://sourceforge.net/projects/pgbarman/files/2.11/

--- databases/pgbarman/files/patch-setup.py
+++ databases/pgbarman/files/patch-setup.py
@@ -1,18 +1,24 @@
---- setup.py.orig	2019-12-02 10:19:28 UTC
+--- setup.py.orig	2020-07-08 09:12:30 UTC
 +++ setup.py
-@@ -70,12 +70,12 @@ setup(
+@@ -70,15 +70,15 @@ setup(
      url='http://www.pgbarman.org/',
      packages=find_packages(exclude=["tests"]),
      data_files=[
 -        ('share/man/man1', ['doc/barman.1',
 -                            'doc/barman-cloud-backup.1',
+-                            'doc/barman-cloud-backup-list.1',
+-                            'doc/barman-cloud-restore.1',
 -                            'doc/barman-cloud-wal-archive.1',
+-                            'doc/barman-cloud-wal-restore.1',
 -                            'doc/barman-wal-archive.1',
 -                            'doc/barman-wal-restore.1']),
 -        ('share/man/man5', ['doc/barman.5']),
 +        ('man/man1', ['doc/barman.1',
 +                      'doc/barman-cloud-backup.1',
++                      'doc/barman-cloud-backup-list.1',
++                      'doc/barman-cloud-restore.1',
 +                      'doc/barman-cloud-wal-archive.1',
++                      'doc/barman-cloud-wal-restore.1',
 +                      'doc/barman-wal-archive.1',
 +                      'doc/barman-wal-restore.1']),
 +        ('man/man5', ['doc/barman.5']),
