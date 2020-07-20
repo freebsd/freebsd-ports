734ab49edaa9ae20b8195e0a8104f226f4b9fd08
Li-Wen Hsu <lwhsu@FreeBSD.org>
2020-07-20 19:03:24 +0000

    Update to 3.4.1
    PR:		248117
    Submitted by:	Klaus Aehlig <aehlig@linta.de> (maintainer)

--- devel/bazel/files/patch-src_main_java_com_google_devtools_build_lib_bazel_rules_python_BazelPythonSemantics.java
+++ devel/bazel/files/patch-src_main_java_com_google_devtools_build_lib_bazel_rules_python_BazelPythonSemantics.java
@@ -1,21 +1,14 @@
---- src/main/java/com/google/devtools/build/lib/bazel/rules/python/BazelPythonSemantics.java.orig	2020-03-03 17:09:23 UTC
-+++ src/main/java/com/google/devtools/build/lib/bazel/rules/python/BazelPythonSemantics.java
-@@ -237,15 +237,15 @@ public class BazelPythonSemantics implem
+--- src/main/java/com/google/devtools/build/lib/bazel/rules/python/BazelPythonSemantics.java.orig	2020-07-03 15:30:52.313535000 +0200
++++ src/main/java/com/google/devtools/build/lib/bazel/rules/python/BazelPythonSemantics.java	2020-07-03 15:31:54.048865000 +0200
+@@ -247,9 +247,9 @@
          PathFragment shExecutable = ShToolchain.getPathOrError(ruleContext);
          // TODO(#8685): Remove this special-case handling as part of making the proper shebang a
          // property of the Python toolchain configuration.
 -        String pythonExecutableName = OS.getCurrent() == OS.OPENBSD ? "python3" : "python";
-+        String pythonExecutable = "%%PYTHON_CMD%%";
++        String pythonExecutableName = "%%PYTHON_CMD%%";
+         // NOTE: keep the following line intact to support nix builds
+-        String pythonShebang = "#!/usr/bin/env " + pythonExecutableName;
++        String pythonShebang = "#!" + pythonExecutableName;
          ruleContext.registerAction(
              new SpawnAction.Builder()
                  .addInput(zipFile)
-                 .addOutput(executable)
-                 .setShellCommand(
-                     shExecutable,
--                    "echo '#!/usr/bin/env "
--                        + pythonExecutableName
-+                    "echo '#!"
-+                        + pythonExecutable
-                         + "' | cat - "
-                         + zipFile.getExecPathString()
-                         + " > "
