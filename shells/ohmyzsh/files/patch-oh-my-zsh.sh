29b0ff23a5afb25a64dbc4d31f92c5789f3e6806
Serhii (Sergey) Kozlov <skozlov@FreeBSD.org>
2020-07-22 19:46:00 +0000

    Update and adjust shells/ohmyzsh
    - Update to 2020-07-22
    - Add comments to the patches in files/ [1]
    - Use XDG cache directory if present [1]
    - Rewrite port description to have something more factual with a bit
      less of hype [1]
    
    PR:		247887 [1]
    Submitted by:	fernape, mat [1]

--- shells/ohmyzsh/files/patch-oh-my-zsh.sh
+++ shells/ohmyzsh/files/patch-oh-my-zsh.sh
@@ -1,8 +1,11 @@
---- oh-my-zsh.sh.orig	2020-07-06 15:31:40 UTC
+Disable auto update
+Respect XDG cache directory if present.
+--- oh-my-zsh.sh.orig	2020-07-18 17:06:08 UTC
 +++ oh-my-zsh.sh
-@@ -1,12 +1,7 @@
+@@ -1,12 +1,8 @@
  # Set ZSH_CACHE_DIR to the path where cache files should be created
- # or else we will use the default cache/
+-# or else we will use the default cache/
++# or else we will use the default .cache/
  if [[ -z "$ZSH_CACHE_DIR" ]]; then
 -  ZSH_CACHE_DIR="$ZSH/cache"
 -fi
@@ -10,7 +13,8 @@
 -# Check for updates on initial load...
 -if [ "$DISABLE_AUTO_UPDATE" != "true" ]; then
 -  source $ZSH/tools/check_for_upgrade.sh
-+  ZSH_CACHE_DIR="$HOME/.oh-my-zsh/cache"
++  ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/oh-my-zsh"
++  mkdir -p ${ZSH_CACHE_DIR}
  fi
  
  # Initializes Oh My Zsh
