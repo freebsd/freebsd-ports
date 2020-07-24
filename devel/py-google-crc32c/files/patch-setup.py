db12deba961b4253daf89dd876a3114929debdab
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-07-24 07:07:54 +0000

    Add py-google-crc32c 0.1.0
    google-crc32c wraps the google/crc32c hardware-based implementation of the
    CRC32C hashing algorithm. Multiple wheels are distributed as well as source. If
    a wheel is not published for the python version and platform you are using, you
    will need to compile crc32c using a C toolchain.
    
    WWW: https://github.com/googleapis/python-crc32c

---
+++ devel/py-google-crc32c/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2020-03-21 00:48:10 UTC
++++ setup.py
+@@ -54,7 +54,6 @@ def main(build_cffi=True):
+     setuptools.setup(
+         packages=["crc32c"],
+         package_dir={"": "src"},
+-        package_data={"crc32c": [os.path.join(_EXTRA_DLL, _DLL_FILENAME)]},
+         setup_requires=[cffi_dep] if build_cffi else [],
+         cffi_modules=[builder] if build_cffi else [],
+         install_requires=[cffi_dep] if build_cffi else [],
