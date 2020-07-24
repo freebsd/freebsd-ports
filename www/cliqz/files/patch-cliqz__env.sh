1d68bed6389295f32bf15aa7932b6f2aee1f0dab
Santhosh Raju <fox@FreeBSD.org>
2020-07-24 16:30:40 +0000

    www/cliqz: Updates to 1.38.0
    - Bumps versions of dependencies.
    
    Changes since 1.36.0:
    
    Merge with Firefox 78.0.2

--- www/cliqz/files/patch-cliqz__env.sh
+++ www/cliqz/files/patch-cliqz__env.sh
@@ -1,9 +1,9 @@
 Commented out the various wget(1) to prevent http traffic during build
 phase.
 
---- cliqz_env.sh.orig	2019-11-13 13:22:25 UTC
+--- cliqz_env.sh.orig	2020-07-17 14:00:07 UTC
 +++ cliqz_env.sh
-@@ -105,9 +105,9 @@ export S3_BUCKET=repository.cliqz.com
+@@ -109,9 +109,9 @@ export S3_BUCKET=repository.cliqz.com
  export S3_BUCKET_SERVICE=cliqz-browser-data
  
  # check CQZ_BUILD_ID and try to obtain, if not specified
@@ -16,12 +16,12 @@ phase.
  
  if [ -z $CQZ_BUILD_ID ]; then
    echo "CQZ_BUILD_ID not specified and can not be obtain from "$S3_BUCKET
-@@ -135,7 +135,7 @@ OBJ_DIR=$MOZ_OBJDIR
+@@ -134,7 +134,7 @@ OBJ_DIR=$MOZ_OBJDIR
  SRC_BASE=mozilla-release
  
  # automatic forget tab - start
--wget -O adult-domains.bin https://s3.amazonaws.com/cdn.cliqz.com/browser-f/APT/adult-domains.bin
-+#wget -O adult-domains.bin https://s3.amazonaws.com/cdn.cliqz.com/browser-f/APT/adult-domains.bin
- export CQZ_ADULT_DOMAINS_BF=../adult-domains.bin
+-wget -O mozilla-release/browser/adult-domains.bin https://s3.amazonaws.com/cdn.cliqz.com/browser-f/APT/adult-domains.bin
++#wget -O mozilla-release/browser/adult-domains.bin https://s3.amazonaws.com/cdn.cliqz.com/browser-f/APT/adult-domains.bin
+ export CQZ_ADULT_DOMAINS_BF="adult-domains.bin"
  # automatic forget tab - end
  
