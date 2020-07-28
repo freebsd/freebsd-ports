778f7a3e70be5208e8b36ea3917acf77a231b7d9
Yuri Victorovich <yuri@FreeBSD.org>
2020-07-28 05:03:27 +0000

    audio/sfizz: 0.3.2 -> 0.4.0
    Reported by:	portscout

--- audio/sfizz/files/patch-cmake_SfizzConfig.cmake
+++ audio/sfizz/files/patch-cmake_SfizzConfig.cmake
@@ -1,11 +1,11 @@
---- cmake/SfizzConfig.cmake.orig	2020-04-20 01:18:25 UTC
+--- cmake/SfizzConfig.cmake.orig	2020-07-23 22:01:34 UTC
 +++ cmake/SfizzConfig.cmake
-@@ -67,7 +67,7 @@ if (USE_LIBCPP)
- add_compile_options(-stdlib=libc++)
- # Presumably need the above for linking too, maybe other options missing as well
- add_link_options(-stdlib=libc++)   # New command on CMake master, not in 3.12 release
--add_link_options(-lc++abi)   # New command on CMake master, not in 3.12 release
-+#add_link_options(-lc++abi)   # New command on CMake master, not in 3.12 release
- endif()
+@@ -69,7 +69,7 @@ if (USE_LIBCPP)
+     add_compile_options(-stdlib=libc++)
+     # Presumably need the above for linking too, maybe other options missing as well
+     add_link_options(-stdlib=libc++)   # New command on CMake master, not in 3.12 release
+-    add_link_options(-lc++abi)   # New command on CMake master, not in 3.12 release
++    #add_link_options(-lc++abi)   # New command on CMake master, not in 3.12 release
  endif()
  
+ add_library(sfizz-pugixml STATIC "src/external/pugixml/src/pugixml.cpp")
