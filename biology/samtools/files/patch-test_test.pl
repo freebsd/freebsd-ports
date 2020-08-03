7197facae51c8ea545727b67e95ffbb063781754
Jason W. Bacon <jwb@FreeBSD.org>
2020-08-03 18:22:06 +0000

    biology/samtools: Upgrade to 1.10
    Numerous bug fixes and enhancements:
    https://github.com/samtools/samtools/releases/tag/1.10
    
    Take maintainership per ports/248089
    
    PR:             ports/248089

--- biology/samtools/files/patch-test_test.pl
+++ biology/samtools/files/patch-test_test.pl
@@ -1,20 +1,20 @@
---- test/test.pl.orig	2017-10-18 06:08:39 UTC
+--- test/test.pl.orig	2020-07-21 20:48:50 UTC
 +++ test/test.pl
-@@ -636,7 +636,7 @@ sub test_usage
+@@ -907,7 +907,7 @@ sub test_usage
      my $command = $args{cmd};
      my $commandpath = $$opts{bin}."/".$command;
      my ($ret,$out,$err) = _cmd("$commandpath $args{redirection}");
 -    if ( $err =~ m/\/bin\/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath: $out"); return; }
-+    if ( $err =~ m/\/usr\/local\/bin\/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath: $out"); return; }
++    if ( $err =~ m/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath: $out"); return; }
  
      my @sections = ($err =~ m/(^[A-Za-z]+.*?)(?:(?=^[A-Za-z]+:)|\z)/msg);
  
-@@ -694,7 +694,7 @@ sub test_usage_subcommand
+@@ -965,7 +965,7 @@ sub test_usage_subcommand
      my $commandpath = $$opts{bin}."/".$command;
      my ($ret,$out,$err) = _cmd("$commandpath $subcommand $args{redirection}");
  
 -    if ( $err =~ m/\/bin\/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath $subcommand: $out"); return; }
-+    if ( $err =~ m/\/usr\/local\/bin\/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath $subcommand: $out"); return; }
++    if ( $err =~ m/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath $subcommand: $out"); return; }
  
      if ( $err =~ m/not.*implemented/is ) { failed($opts,msg=>$test,reason=>"subcommand indicates it is not implemented",expect_fail=>1); return; }
  
