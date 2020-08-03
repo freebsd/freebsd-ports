3d154d8bbc74c28b45b481d466c6339f7f226206
Jason W. Bacon <jwb@FreeBSD.org>
2020-08-03 18:16:54 +0000

    biology/bcftools: Upgrade to 1.10.2
    Numerous bug fixes and enhancements since 1.9:
    https://github.com/samtools/bcftools/tags
    
    Take maintainership per ports/248089.
    
    PR:             ports/248089

--- biology/bcftools/files/patch-test_test.pl
+++ biology/bcftools/files/patch-test_test.pl
@@ -1,29 +1,38 @@
---- test/test.pl.orig	2017-10-18 09:33:24 UTC
+--- test/test.pl.orig	2020-07-21 20:49:56 UTC
 +++ test/test.pl
-@@ -381,7 +381,7 @@ sub _cmd
+@@ -610,7 +610,7 @@ sub _cmd
      else
      {
          # child
 -        exec('/bin/bash', '-o','pipefail','-c', $cmd) or error("Cannot execute the command [/bin/sh -o pipefail -c $cmd]: $!");
-+        exec('/usr/local/bin/bash', '-o','pipefail','-c', $cmd) or error("Cannot execute the command [/usr/local/bin/sh -o pipefail -c $cmd]: $!");
++        exec('bash', '-o','pipefail','-c', $cmd) or error("Cannot execute the command [/usr/local/bin/sh -o pipefail -c $cmd]: $!");
      }
      return ($? >> 8, join('',@out));
  }
-@@ -782,7 +782,7 @@ sub test_usage
+@@ -622,7 +622,7 @@ sub _cmd3
+     my $pid = fork();
+     if ( !$pid )
+     {
+-        exec('/bin/bash', '-o','pipefail','-c', "($cmd) 2>$tmp.e >$tmp.o");
++        exec('bash', '-o','pipefail','-c', "($cmd) 2>$tmp.e >$tmp.o");
+     }
+     waitpid($pid,0);
+ 
+@@ -1063,7 +1063,7 @@ sub test_usage
      my $command = $args{cmd};
      my $commandpath = $$opts{bin}."/".$command;
      my ($ret,$out) = _cmd("$commandpath $args{redirection} 2>&1");
 -    if ( $out =~ m/\/bin\/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath: $out"); return; }
-+    if ( $out =~ m/\/usr\/local\/bin\/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath: $out"); return; }
++    if ( $out =~ m/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath: $out"); return; }
  
      my @sections = ($out =~ m/(^[A-Za-z]+.*?)(?:(?=^[A-Za-z]+:)|\z)/msg);
  
-@@ -833,7 +833,7 @@ sub test_usage_subcommand
+@@ -1118,7 +1118,7 @@ sub test_usage_subcommand
      my $subcommand = $args{subcmd};
      my $commandpath = $$opts{bin}."/".$command;
      my ($ret,$out) = _cmd("$commandpath $subcommand $args{redirection} 2>&1");
 -    if ( $out =~ m/\/bin\/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath $subcommand: $out"); return; }
-+    if ( $out =~ m/\/usr\/local\/bin\/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath $subcommand: $out"); return; }
++    if ( $out =~ m/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath $subcommand: $out"); return; }
  
      my @sections = ($out =~ m/(^[A-Za-z]+.*?)(?:(?=^[A-Za-z]+:)|\z)/msg);
  
