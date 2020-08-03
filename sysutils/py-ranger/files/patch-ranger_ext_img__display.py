4e25ff4752c932685e9d71508b9adc4b6e61a4f0
Kai Knoblich <kai@FreeBSD.org>
2020-08-03 10:46:41 +0000

    sysutils/py-ranger: Update to 1.9.3
    While I'm here:
    
    * Backport a runtime fix for environments that don't have sysutils/screen
      installed but have TERM set to "screen".
    
    * Reduce patching by switching manpages to the preferred share/man location.
    
    * Rebase and document the patch that avoids hardcoded paths to w3mimgdisplay
      (which is supplied via www/w3m-img).
    
    Changelog:
    
    https://github.com/ranger/ranger/blob/v1.9.3/CHANGELOG.md
    
    PR:		247938
    Submitted by:	Greg Fitzgerald <gregf@hugops.pw> (based on)
    Approved by:	maintainer timeout (3+ weeks)
    MFH:		No (contains more additions/improvements than bugfixes)

--- sysutils/py-ranger/files/patch-ranger_ext_img__display.py
+++ sysutils/py-ranger/files/patch-ranger_ext_img__display.py
@@ -1,10 +1,17 @@
---- ranger/ext/img_display.py.orig	2017-02-20 16:56:49.277878000 +0800
-+++ ranger/ext/img_display.py	2017-02-20 17:00:01.700857000 +0800
-@@ -25,6 +25,7 @@
+Avoid hardcoded paths to w3mimgdisplay
+
+--- ranger/ext/img_display.py.orig	2019-12-31 15:07:45 UTC
++++ ranger/ext/img_display.py
+@@ -35,11 +35,7 @@ from ranger.core.shared import FileManagerAware
  W3MIMGDISPLAY_ENV = "W3MIMGDISPLAY_PATH"
  W3MIMGDISPLAY_OPTIONS = []
  W3MIMGDISPLAY_PATHS = [
+-    '/usr/lib/w3m/w3mimgdisplay',
+-    '/usr/libexec/w3m/w3mimgdisplay',
+-    '/usr/lib64/w3m/w3mimgdisplay',
+-    '/usr/libexec64/w3m/w3mimgdisplay',
+-    '/usr/local/libexec/w3m/w3mimgdisplay',
 +    '%%LOCALBASE%%/libexec/w3m/w3mimgdisplay',
-     '/usr/lib/w3m/w3mimgdisplay',
-     '/usr/libexec/w3m/w3mimgdisplay',
-     '/usr/lib64/w3m/w3mimgdisplay',
+ ]
+ 
+ # Helper functions shared between the previewers (make them static methods of the base class?)
