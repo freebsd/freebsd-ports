51309f0503e757ce4a0314af5a20522d8aaa0eca
Li-Wen Hsu <lwhsu@FreeBSD.org>
2020-08-04 08:21:27 +0000

    Update to 0.22.0
    PR:		248447
    Submitted by:	Sascha Holzleiter <sascha@root-login.org> (maintainer)

--- textproc/fzf/files/patch-shell_completion.bash
+++ textproc/fzf/files/patch-shell_completion.bash
@@ -1,11 +1,11 @@
---- shell/completion.bash.orig	2020-05-25 18:52:57 UTC
+--- shell/completion.bash.orig	2020-08-03 09:46:24 UTC
 +++ shell/completion.bash
-@@ -252,7 +252,7 @@ _fzf_complete_kill() {
-   [ -n "${COMP_WORDS[COMP_CWORD]}" ] && return 1
+@@ -262,7 +262,7 @@ _fzf_complete_kill() {
  
-   local selected
--  selected=$(command ps -ef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS --preview 'echo {}' --preview-window down:3:wrap" __fzf_comprun "kill" -m | awk '{print $2}' | tr '\n' ' ')
-+  selected=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS --preview 'echo {}' --preview-window down:3:wrap" __fzf_comprun "kill" -m | awk '{print $2}' | tr '\n' ' ')
-   selected=${selected% }
-   printf '\e[5n'
+ _fzf_proc_completion() {
+   _fzf_complete -m --preview 'echo {}' --preview-window down:3:wrap --min-height 15 -- "$@" < <(
+-    command ps -ef | sed 1d
++    command ps -uef | sed 1d
+   )
+ }
  
--- textproc/fzf/files/patch-shell_completion.zsh
+++ textproc/fzf/files/patch-shell_completion.zsh
@@ -1,11 +1,11 @@
---- shell/completion.zsh.orig	2020-05-25 18:53:01 UTC
+--- shell/completion.zsh.orig	2020-08-03 09:46:30 UTC
 +++ shell/completion.zsh
-@@ -276,7 +276,7 @@ fzf-completion() {
-   tail=${LBUFFER:$(( ${#LBUFFER} - ${#trigger} ))}
-   # Kill completion (do not require trigger sequence)
-   if [ $cmd = kill -a ${LBUFFER[-1]} = ' ' ]; then
--    matches=$(command ps -ef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS --preview 'echo {}' --preview-window down:3:wrap" __fzf_comprun "$cmd" -m | awk '{print $2}' | tr '\n' ' ')
-+    matches=$(ps -uef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-50%} --min-height 15 --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS --preview 'echo {}' --preview-window down:3:wrap" __fzf_comprun "$cmd" -m | awk '{print $2}' | tr '\n' ' ')
-     if [ -n "$matches" ]; then
-       LBUFFER="$LBUFFER$matches"
-     fi
+@@ -252,7 +252,7 @@ _fzf_complete_unalias() {
+ 
+ _fzf_complete_kill() {
+   _fzf_complete -m --preview 'echo {}' --preview-window down:3:wrap --min-height 15 -- "$@" < <(
+-    command ps -ef | sed 1d
++    command ps -uef | sed 1d
+   )
+ }
+ 
