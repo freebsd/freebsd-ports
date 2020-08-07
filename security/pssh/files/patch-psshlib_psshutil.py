20753135023863642fa4bf7442eecb68454031cf
Fernando Apesteguía <fernape@FreeBSD.org>
2020-08-07 18:41:57 +0000

    security/pssh: update to 2.3.4
    ChangeLog https://github.com/lilydjwg/pssh/blob/master/ChangeLog
    
    * switch to the most viable fork
    * Set MAINTAINER
    
    PR:	248208
    Submitted by:	timp87@gmail.com

--- security/pssh/files/patch-psshlib_psshutil.py
+++ security/pssh/files/patch-psshlib_psshutil.py
@@ -1,6 +1,6 @@
---- psshlib/psshutil.py.orig
+--- psshlib/psshutil.py.orig	2020-04-05 06:12:36 UTC
 +++ psshlib/psshutil.py
-@@ -27,7 +27,10 @@
+@@ -28,7 +28,10 @@ def read_host_file(path, host_glob, default_user=None,
      Returns a list of (host, port, user) triples.
      """
      lines = []
