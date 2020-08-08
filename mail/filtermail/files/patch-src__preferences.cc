df754b64ae7e4cbdd4d5abe1f39c9df65bd0d2b6
Kurt Jaeger <pi@FreeBSD.org>
2020-08-08 15:16:07 +0000

    MFH: r544378
    mail/filtermail: update 0.8.3 -> 0.8.6
    
    - fix build with more recent flex versions
    - pet portlint
    
    PR:		247771
    Submitted by:	Mike Clarke <jmc-freebsd2@milibyte.co.uk> (maintainer)
    Relnotes:	https://raw.githubusercontent.com/nondeterministic/mailfilter/master/NEWS
    Approved by:	portmgr (joneum)

--- mail/filtermail/files/patch-src__preferences.cc
+++ mail/filtermail/files/patch-src__preferences.cc
@@ -1,5 +1,5 @@
---- src/preferences.cc.orig	2007-01-22 23:36:04.000000000 +0100
-+++ src/preferences.cc	2008-04-08 17:00:17.000000000 +0200
+--- src/preferences.cc.orig	2014-05-17 17:19:41 UTC
++++ src/preferences.cc
 @@ -24,6 +24,7 @@
  #include <vector>
  #include <stdexcept>
