acaebbf9d799337c9912e0b7785c3d45d5e5fec0
Steve Wills <swills@FreeBSD.org>
2020-08-10 17:59:11 +0000

    devel/libzim: update to 6.1.8
    PR:		248055

--- devel/libzim/files/patch-src_buffer.cpp
+++ devel/libzim/files/patch-src_buffer.cpp
@@ -1,11 +1,11 @@
---- src/buffer.cpp.orig	2018-09-04 16:23:32 UTC
+--- src/buffer.cpp.orig	2020-07-15 14:24:54 UTC
 +++ src/buffer.cpp
-@@ -49,7 +49,7 @@ MMapBuffer::MMapBuffer(int fd, offset_t offset, zsize_
- #if defined(__APPLE__)
+@@ -48,6 +48,8 @@ MMapBuffer::MMapBuffer(int fd, offset_t offset, zsize_
+   _offset = offset-pa_offset;
+ #if defined(__APPLE__) || defined(__OpenBSD__)
    #define MAP_FLAGS MAP_PRIVATE
- #else
--  #define MAP_FLAGS MAP_PRIVATE|MAP_POPULATE
++#elif defined(__FreeBSD__)
 +  #define MAP_FLAGS MAP_PRIVATE|MAP_PREFAULT_READ
+ #else
+   #define MAP_FLAGS MAP_PRIVATE|MAP_POPULATE
  #endif
- #if !MMAP_SUPPORT_64
-   if(pa_offset.v >= INT32_MAX) {
--- devel/libzim/files/patch-src_fs__unix.cpp
+++ devel/libzim/files/patch-src_fs__unix.cpp
@@ -1,11 +1,11 @@
---- src/fs_unix.cpp.orig	2018-12-04 23:56:33 UTC
+--- src/fs_unix.cpp.orig	2020-07-15 14:24:54 UTC
 +++ src/fs_unix.cpp
 @@ -34,7 +34,7 @@ namespace unix {
  
  zsize_t FD::readAt(char* dest, zsize_t size, offset_t offset) const
  {
--#ifdef __APPLE__
-+#if defined(__APPLE__) || defined(__FreeBSD__)
+-#if defined(__APPLE__) || defined(__OpenBSD__)
++#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__)
  # define PREAD pread
  #else
  # define PREAD pread64
