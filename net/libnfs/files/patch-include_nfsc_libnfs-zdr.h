12677ba70e1024ef100c81b7df8c8ef071e16546
Alan Somers <asomers@FreeBSD.org>
2020-08-10 20:48:57 +0000

    net/libnfs: fix build error regarding "redefinition of typedef"
    libnfs was redefining u_int, causing programs using libnfs to get
    "error: redefinition of typedef 'u_int' is a C11 feature" errors.
    
    Upstream PR: https://github.com/sahlberg/libnfs/pull/326
    
    PR:		248296
    Approved by:	Shannon Noe <snoe925@gmail.com> (maintainer)

---
+++ net/libnfs/files/patch-include_nfsc_libnfs-zdr.h
@@ -0,0 +1,10 @@
+--- include/nfsc/libnfs-zdr.h.orig	2020-07-26 21:16:08 UTC
++++ include/nfsc/libnfs-zdr.h
+@@ -92,7 +92,6 @@ struct ZDR {
+ typedef struct ZDR ZDR;
+ 
+ 
+-typedef uint32_t u_int;
+ typedef uint32_t enum_t;
+ typedef uint32_t bool_t;
+ 
