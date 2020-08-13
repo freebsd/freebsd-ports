6219baaaa60ced910f1b4a5f832fbe11c2a15dcd
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-08-13 21:43:07 +0000

    MFH: r544844
    misc/nn-insight: fix build with GCC and on head
    
    Add missing includes. This fixes build on stable/12 for powerpc64 (with GCC) and on head with powerpc64. Looking at https://www.freshports.org/misc/nn-insight/, it seems currently doesn't build even on head/amd64, so those includes should fix it.
    
    The original error that made me look into it was:
    /wrkdirs/usr/ports/misc/nn-insight/work/nn-insight-1.0.5/tensor.h:18:75: error: 'std::shared_ptr' has not been declared
       18 | bool readTensorDataAsJson(const char *fileName, const TensorShape &shape, std::shared_ptr<const float> &tensorData);
          |                                                                           ^~~
    
    Approved by:	portmgr (fix build blanket)

---
+++ misc/nn-insight/files/patch-3rdparty_tensorflow_tflite-reference-implementation.cpp
@@ -0,0 +1,10 @@
+--- 3rdparty/tensorflow/tflite-reference-implementation.cpp.orig	2020-08-13 21:04:29 UTC
++++ 3rdparty/tensorflow/tflite-reference-implementation.cpp
+@@ -7,6 +7,7 @@
+ #include <array>
+ #include <algorithm>
+ #include <cstdint>
++#include <cstring>
+ #include <vector>
+ #include <cmath>
+ 
