59c22beddccbc660993e0719467546514751d727
Joseph Mingrone <jrm@FreeBSD.org>
2020-08-15 21:02:33 +0000

    MFH: r545067 r545068
    www/chromium: Update to version 84.0.4147.125
    
    Upstream changes:
    https://chromereleases.googleblog.com/2020/08/stable-channel-update-for-desktop.html
    
    --
    
    www/chromium: Fix U2F authentication
    
    PR:		247744
    Submitted by:	gonzo
    Reported by:	Henry Hu <henry.hu.sh@gmail.com>
    
    Approved by:	ports-secteam (blanket)

--- www/chromium/files/patch-services_device_hid_hid__connection__freebsd.h
+++ www/chromium/files/patch-services_device_hid_hid__connection__freebsd.h
@@ -1,6 +1,6 @@
---- services/device/hid/hid_connection_freebsd.h.orig	2019-05-01 15:21:27 UTC
-+++ services/device/hid/hid_connection_freebsd.h
-@@ -0,0 +1,68 @@
+--- services/device/hid/hid_connection_freebsd.h.orig	2020-07-18 19:40:14.412550000 -0700
++++ services/device/hid/hid_connection_freebsd.h	2020-07-21 19:15:28.613027000 -0700
+@@ -0,0 +1,65 @@
 +// Copyright (c) 2014 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
@@ -13,10 +13,9 @@
 +
 +#include "base/files/scoped_file.h"
 +#include "base/macros.h"
-+#include "base/memory/ptr_util.h"
-+#include "base/memory/ref_counted_memory.h"
 +#include "base/memory/weak_ptr.h"
-+#include "base/sequence_checker.h"
++#include "base/memory/ref_counted_memory.h"
++#include "base/sequenced_task_runner.h"
 +#include "services/device/hid/hid_connection.h"
 +
 +namespace base {
@@ -38,7 +37,7 @@
 +
 + private:
 +  friend class base::RefCountedThreadSafe<HidConnectionFreeBSD>;
-+  class BlockingTaskHelper;
++  class BlockingTaskRunnerHelper;
 +
 +  ~HidConnectionFreeBSD() override;
 +
@@ -54,14 +53,12 @@
 +  // |helper_| lives on the sequence to which |blocking_task_runner_| posts
 +  // tasks so all calls must be posted there including this object's
 +  // destruction.
-+  std::unique_ptr<BlockingTaskHelper> helper_;
++  std::unique_ptr<BlockingTaskRunnerHelper, base::OnTaskRunnerDeleter> helper_;
 +
 +  const scoped_refptr<base::SequencedTaskRunner> blocking_task_runner_;
 +  const scoped_refptr<base::SequencedTaskRunner> task_runner_;
 +
-+  SEQUENCE_CHECKER(sequence_checker_);
-+
-+  base::WeakPtrFactory<HidConnectionFreeBSD> weak_factory_;
++  base::WeakPtrFactory<HidConnectionFreeBSD> weak_factory_{this};
 +
 +  DISALLOW_COPY_AND_ASSIGN(HidConnectionFreeBSD);
 +};
--- www/chromium/files/patch-services_device_hid_hid__service__freebsd.h
+++ www/chromium/files/patch-services_device_hid_hid__service__freebsd.h
@@ -1,6 +1,6 @@
---- services/device/hid/hid_service_freebsd.h.orig	2020-03-17 15:11:01 UTC
-+++ services/device/hid/hid_service_freebsd.h
-@@ -0,0 +1,47 @@
+--- services/device/hid/hid_service_freebsd.h.orig	2020-07-18 19:40:14.418200000 -0700
++++ services/device/hid/hid_service_freebsd.h	2020-07-21 19:52:08.796704000 -0700
+@@ -0,0 +1,46 @@
 +// Copyright 2014 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
@@ -30,17 +30,16 @@
 +
 + private:
 +  struct ConnectParams;
-+  class BlockingTaskHelper;
++  class BlockingTaskRunnerHelper;
 +
 +  static void OpenOnBlockingThread(std::unique_ptr<ConnectParams> params);
 +  static void FinishOpen(std::unique_ptr<ConnectParams> params);
 +
-+  const scoped_refptr<base::SequencedTaskRunner> task_runner_;
 +  const scoped_refptr<base::SequencedTaskRunner> blocking_task_runner_;
 +  // |helper_| lives on the sequence |blocking_task_runner_| posts to and holds
 +  // a weak reference back to the service that owns it.
-+  std::unique_ptr<BlockingTaskHelper> helper_;
-+  base::WeakPtrFactory<HidServiceFreeBSD> weak_factory_;
++  std::unique_ptr<BlockingTaskRunnerHelper, base::OnTaskRunnerDeleter> helper_;
++  base::WeakPtrFactory<HidServiceFreeBSD> weak_factory_{this};
 +
 +  DISALLOW_COPY_AND_ASSIGN(HidServiceFreeBSD);
 +};
