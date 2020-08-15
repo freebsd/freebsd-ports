4f09c274050a00f9bab644c47e38e1eeed3d821b
Rene Ladan <rene@FreeBSD.org>
2020-08-15 12:29:48 +0000

    Remove expired ports:
    2020-08-15 audio/py-cddb: Uses deprecated version of python
    2020-08-15 databases/py-sybase: Uses deprecated version of python
    2020-08-15 devel/py-pp: Uses deprecated version of python
    2020-08-15 security/py-keyczar: Uses deprecated version of python
    2020-08-15 math/py-pysparse: Uses deprecated version of python
    2020-08-15 net/py-beanstalkc: Uses deprecated version of python
    2020-08-15 x11-toolkits/py-wxPython30: Uses deprecated version of python
    2020-08-15 net-im/py-skype4py: Uses deprecated version of python
    2020-08-15 net-im/telepathy-haze: Uses deprecated version of python

--- net/py-beanstalkc/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
---- ./setup.py.orig	2014-03-08 14:16:00.573786621 +1100
-+++ ./setup.py	2014-03-08 14:16:32.606016182 +1100
-@@ -29,4 +29,5 @@
-         'Operating System :: OS Independent',
-         'Topic :: Software Development :: Libraries :: Python Modules',
-     ],
-+    test_suite = 'nose.collector',
- )
