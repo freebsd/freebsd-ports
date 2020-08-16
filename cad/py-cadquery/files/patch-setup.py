8398ee71c0202e6a6f8eaefbf2abda43c9549c22
Steve Wills <swills@FreeBSD.org>
2020-08-16 12:54:13 +0000

    cad/py-cadquery: create port
    CadQuery is an intuitive, easy-to-use Python module for building parametric 3D
    CAD models. Using CadQuery, you can write short, simple scripts that produce
    high quality CAD models. It is easy to make many different objects using a
    single script that can be customized.
    
    WWW: https://github.com/CadQuery/cadquery
    
    PR:		248210
    Submitted by:	Neal Nelson <ports@nicandneal.net>

---
+++ cad/py-cadquery/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-06-25 14:36:51 UTC
++++ setup.py
+@@ -35,7 +35,7 @@ setup(
+         "cadquery.contrib",
+         "cadquery.occ_impl",
+         "cadquery.plugins",
+-        "tests",
++        "cadquery.tests",
+     ],
+     include_package_data=True,
+     zip_safe=False,
