8423f117e5bfb2c6cfc2f41ac2ecdefbd92a377f
Florian Smeets <flo@FreeBSD.org>
2020-08-16 20:58:01 +0000

    - update to 3.0.0 - pass maintainership to submitter
    PR:		248525
    Submitted by:	James French <james@french.id.au>
    Approved by:	joshruehlig@gmail.com (previous maintainer)

--- news/sabnzbdplus/files/patch-SABnzbd.py
+++ news/sabnzbdplus/files/patch-SABnzbd.py
@@ -1,17 +1,7 @@
---- SABnzbd.py.orig	2016-06-04 11:45:01 UTC
+--- SABnzbd.py.orig	2020-08-08 04:14:33 UTC
 +++ SABnzbd.py
-@@ -51,6 +51,9 @@ except:
-     print "The Python module Cheetah is required"
-     sys.exit(1)
+@@ -930,7 +930,7 @@ def main():
  
-+# Force python to load the patched version of cherrypy included with the port,
-+# instead of any version that may be installed otherwise.
-+sys.path.insert(0,%%DATADIR%%)
- import cherrypy
- if [int(n) for n in cherrypy.__version__.split('.')] < [3, 8, 0]:
-     print 'Sorry, requires Python module Cherrypy 3.8.0+ (use the included version)'
-@@ -1006,7 +1009,7 @@ def main():
-             
      sabnzbd.MY_FULLNAME = os.path.normpath(os.path.abspath(sabnzbd.MY_FULLNAME))
      sabnzbd.MY_NAME = os.path.basename(sabnzbd.MY_FULLNAME)
 -    sabnzbd.DIR_PROG = os.path.dirname(sabnzbd.MY_FULLNAME)
