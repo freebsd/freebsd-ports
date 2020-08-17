7b2f8aa7d5f42c40a4ece7a4b6ee9ab3da7888b0
Steve Wills <swills@FreeBSD.org>
2020-08-17 03:39:04 +0000

    devel/athens: create port
    Athens provides a server for Go Modules that you can run. It serves public code
    and your private code for you, so you don't have to pull directly from a
    version control system (VCS) like GitHub or GitLab.
    
    WWW: https://github.com/gomods/athens

---
+++ devel/athens/files/patch-vendor_github.com_mattn_go-ieproxy_proxyMiddleman__unix.go
@@ -0,0 +1,8 @@
+--- vendor/github.com/mattn/go-ieproxy/proxyMiddleman_unix.go.orig	2020-08-17 02:41:36 UTC
++++ vendor/github.com/mattn/go-ieproxy/proxyMiddleman_unix.go
+@@ -1,4 +1,4 @@
+-//+build darwin unix linux
++//+build darwin unix linux freebsd
+ 
+ package ieproxy
+ 
