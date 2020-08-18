f49807023dd62994553672acea224c92da8d699d
Greg Lewis <glewis@FreeBSD.org>
2020-08-18 21:24:49 +0000

    Attempt to fix the build on HEAD

--- sysutils/beats7/files/patch-vendor_github.com_godbus_dbus_transport__unixcred__freebsd.go
+++ sysutils/beats7/files/patch-vendor_github.com_godbus_dbus_transport__unixcred__freebsd.go
@@ -1,14 +1,16 @@
 --- vendor/github.com/godbus/dbus/transport_unixcred_freebsd.go.orig	2020-05-28 07:16:25.000000000 -0700
 +++ vendor/github.com/godbus/dbus/transport_unixcred_freebsd.go	2020-06-10 23:26:07.955802000 -0700
-@@ -8,7 +8,7 @@
+@@ -8,8 +8,9 @@
  package dbus
  
  /*
 -const int sizeofPtr = sizeof(void*);
 +const int sizeOfPtr = sizeof(void*);
  #define _WANT_UCRED
++#include <sys/types.h>
  #include <sys/ucred.h>
  */
+ import "C"
 @@ -38,7 +38,7 @@
  
  // http://golang.org/src/pkg/syscall/sockcmsg_unix.go
