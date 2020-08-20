f11f67649197fd8697774fdaa81e0bc533053b30
Xin LI <delphij@FreeBSD.org>
2020-08-20 04:44:42 +0000

    MFH: r545496
    Fix build.
    
    Approved by:	ports-secteam (build fix blanket)

--- archivers/arc/files/patch-arclzw.c
+++ archivers/arc/files/patch-arclzw.c
@@ -1,6 +1,21 @@
 --- arclzw.c.orig	2010-08-07 13:06:42 UTC
 +++ arclzw.c
-@@ -560,7 +560,7 @@ decomp(squash, f, t)		/* decompress a fi
+@@ -57,9 +57,11 @@ static VOID     putcode();
+ #define NOT_FND  0xFFFF
+ 
+ extern u_char	*pinbuf;
+-u_char		*inbeg, *inend;
+-u_char          *outbuf;
+-u_char          *outbeg, *outend; 
++u_char		*inbeg;
++u_char		*inend;
++extern u_char	*outbuf;
++u_char		*outbeg;
++extern u_char	*outend; 
+ 
+ static int      sp;		/* current stack pointer */
+ static int	inflag;
+@@ -560,7 +562,7 @@ decomp(squash, f, t)		/* decompress a file */
  		 */
  		if (code >= free_ent) {
  			if (code > free_ent) {
