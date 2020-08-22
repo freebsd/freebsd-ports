69490264a5e44cf85ad5519e5c83324b813710ea
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-08-22 23:05:38 +0000

    net/appkonference: fix build on powerpc64
    MFH:		2020Q3 (fix build blanket)

---
+++ net/appkonference/files/patch-konference_libwebrtc_typedefs.h
@@ -0,0 +1,23 @@
+--- libwebrtc/typedefs.h.orig	2019-08-04 15:52:12 UTC
++++ libwebrtc/typedefs.h
+@@ -48,7 +48,19 @@
+ #define WEBRTC_ARCH_32_BITS
+ #define WEBRTC_ARCH_LITTLE_ENDIAN
+ #else
+-#error Please add support for your architecture in typedefs.h
++/* instead of failing, use typical unix defines... */
++#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
++#define WEBRTC_ARCH_LITTLE_ENDIAN
++#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
++#define WEBRTC_ARCH_BIG_ENDIAN
++#else
++#error __BYTE_ORDER__ is not defined
++#endif
++#if defined(__LP64__)
++#define WEBRTC_ARCH_64_BITS
++#else
++#define WEBRTC_ARCH_32_BITS
++#endif
+ #endif
+ 
+ #if !(defined(WEBRTC_ARCH_LITTLE_ENDIAN) ^ defined(WEBRTC_ARCH_BIG_ENDIAN))
