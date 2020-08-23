7d6e9df4a5b283a7f3bf3a7ce9df26c7ed879201
Hiroki Sato <hrs@FreeBSD.org>
2020-08-23 19:46:58 +0000

    - Fix a build breakage when -fno-common is specified. - Minor style fixes.

--- security/ipv6toolkit/files/patch-tools-scan6.c
+++ security/ipv6toolkit/files/patch-tools-scan6.c
@@ -1,5 +1,16 @@
 --- tools/scan6.c.orig	2015-04-05 14:03:21 UTC
 +++ tools/scan6.c
+@@ -251,8 +251,8 @@ uint16_t				service_ports_dec[]={21, 22, 23, 25, 49, 5
+ 
+ 
+ /* IPv6 Address Resolution */
+-sigjmp_buf				env;
+-unsigned int			canjump;
++extern sigjmp_buf				env;
++extern unsigned int			canjump;
+ 
+ int main(int argc, char **argv){
+ 	extern char				*optarg;
 @@ -1504,9 +1504,11 @@ int main(int argc, char **argv){
  	if(!pps_f && !bps_f)
  		pktinterval= 1000;
