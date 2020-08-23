017aff1f8359fe19bf6d708eb825bffb1fe5d414
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-08-23 00:43:24 +0000

    math/onednn: enable native powerpc64
    In version 1.6, upstream added native support for powerpc64, without using generic code. Enable that instead of using generic code.
    
    Also patch platform.cmake not to add -mcpu=native for aarch64.

--- math/onednn/files/patch-cmake_platform.cmake
+++ math/onednn/files/patch-cmake_platform.cmake
@@ -1,7 +1,14 @@
---- cmake/platform.cmake.orig	2020-06-05 21:56:56 UTC
+--- cmake/platform.cmake.orig	2020-08-06 03:36:08 UTC
 +++ cmake/platform.cmake
-@@ -104,10 +104,10 @@ elseif(UNIX OR MINGW)
+@@ -104,16 +104,16 @@ elseif(UNIX OR MINGW)
          if(DNNL_TARGET_ARCH STREQUAL "AARCH64")
+              set(DEF_ARCH_OPT_FLAGS "-O3")
+              # For native compilation tune for the host processor
+-             if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
++             if (FALSE AND CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
+                  append(DEF_ARCH_OPT_FLAGS "-mcpu=native")
+              endif()
+         elseif(DNNL_TARGET_ARCH STREQUAL "PPC64")
               set(DEF_ARCH_OPT_FLAGS "-O3")
               # For native compilation tune for the host processor
 -             if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
@@ -13,8 +20,20 @@
               set(DEF_ARCH_OPT_FLAGS "-msse4.1")
          endif()
          # Clang cannot vectorize some loops with #pragma omp simd and gets
-@@ -159,7 +159,7 @@ elseif(UNIX OR MINGW)
-              if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
+@@ -162,17 +162,17 @@ elseif(UNIX OR MINGW)
+         if(DNNL_TARGET_ARCH STREQUAL "AARCH64")
+              set(DEF_ARCH_OPT_FLAGS "-O3")
+              # For native compilation tune for the host processor
+-             if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
++             if (FALSE AND CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
+                  append(DEF_ARCH_OPT_FLAGS "-mcpu=native")
+              endif()
+         elseif(DNNL_TARGET_ARCH STREQUAL "PPC64")
+              set(DEF_ARCH_OPT_FLAGS "-O3")
+              # In GCC, -ftree-vectorize is turned on under -O3 since 2007.
+              # For native compilation tune for the host processor
+-             if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
++             if (FALSE AND CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
                   append(DEF_ARCH_OPT_FLAGS "-mcpu=native")
               endif()
 -        elseif(DNNL_TARGET_ARCH STREQUAL "X64")
