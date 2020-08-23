965715bca6fb883632afa41f69c5000f4be86bfe
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-08-23 19:35:57 +0000

    Fix build with py-lazy-object-proxy 1.5.0 update
    - Bump PORTREVISION for package change

--- devel/py-astroid/files/patch-astroid-__pkginfo__.py
+++ devel/py-astroid/files/patch-astroid-__pkginfo__.py
@@ -1,11 +1,11 @@
---- astroid/__pkginfo__.py.orig	2020-04-27 09:43:18 UTC
+--- astroid/__pkginfo__.py.orig	2020-06-08 06:51:41 UTC
 +++ astroid/__pkginfo__.py
-@@ -28,7 +28,7 @@ extras_require = {}
- install_requires = [
-     "lazy_object_proxy==1.4.*",
-     "six~=1.12",
--    "wrapt~=1.11",
-+    "wrapt>=1.11.0,<1.13",
-     'typed-ast>=1.4.0,<1.5;implementation_name== "cpython" and python_version<"3.8"',
- ]
+@@ -26,7 +26,7 @@ numversion = tuple(int(elem) for elem in version.split
  
+ extras_require = {}
+ install_requires = [
+-    "lazy_object_proxy==1.4.*",
++    "lazy_object_proxy>=1.4",
+     "six~=1.12",
+     "wrapt~=1.11",
+     'typed-ast>=1.4.0,<1.5;implementation_name== "cpython" and python_version<"3.8"',
