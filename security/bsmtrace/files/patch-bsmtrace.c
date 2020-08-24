c6372e46360e9723d252a32e93ff3b3a2b174c11
Kyle Evans <kevans@FreeBSD.org>
2020-08-24 00:28:44 +0000

    security/bsmtrace: backport the -fno-common fix
    This port is the older version of bsmtrace and should go away in due time,
    but it's easy enough to backport the fix in the meantime. This version did
    need an additional fix to log.{c,h} that was no longer needed in 3.x.
    
    PR:		248756
    Approved by:	csjp (maintainer, also discussed with)
    MFH:		2020Q3 (-fno-common build fix)

--- security/bsmtrace/files/patch-bsmtrace.c
+++ security/bsmtrace/files/patch-bsmtrace.c
@@ -1,5 +1,13 @@
 --- bsmtrace.c.orig	2014-01-14 22:51:19 UTC
 +++ bsmtrace.c
+@@ -30,6 +30,7 @@
+ #include "includes.h"
+ 
+ static int	daemonized;	/* daemonized or not? */
++int	audit_pipe_fd;
+ 
+ /*
+  * If we are going to be daemonized, write out a pid file to
 @@ -92,7 +92,7 @@ bsmtrace_exit(int x)
  }
  
