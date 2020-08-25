9dcc41034f75bd980ca5fa4bf85009592e1db41c
Diane Bruce <db@FreeBSD.org>
2020-08-25 23:27:40 +0000

    Thanks to Dima Panov <fluffy@FreeBSD.org> for pointing out it was older compilers not clang 11 failing this. *sigh*
    PR:		ports/248894
    Submitted by:	vvd@unislabs.com

--- audio/codec2/files/patch-src__freedv_api.c
+++ audio/codec2/files/patch-src__freedv_api.c
@@ -1,11 +1,20 @@
---- src/freedv_api.c.orig	2020-08-24 17:11:35 UTC
+--- src/freedv_api.c.orig	2018-05-29 05:59:17 UTC
 +++ src/freedv_api.c
-@@ -76,6 +76,8 @@
+@@ -36,7 +36,7 @@
+ #ifdef __APPLE__
+ #include <malloc/malloc.h>
+ #else
+-#include <malloc.h>
++#include <stdlib.h>
+ #endif /* __APPLE__ */
+ 
+ #include "fsk.h"
+@@ -75,6 +75,8 @@
+ #define NORM_PWR_OFDM    1.00
  
  /* OFDM payload data test frame for 700D */
- 
-+extern const struct OFDM_CONFIG * OFDM_CONFIG_700D;
 +
++extern const struct OFDM_CONFIG * OFDM_CONFIG_700D;
+ 
  extern int payload_data_bits[];
  
- /*---------------------------------------------------------------------------*\
