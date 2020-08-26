eb8b7fc1d8da757fb46512d0f2780cc4b7e2662a
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-08-26 11:38:37 +0000

    games/avp: fix build on current

---
+++ games/avp/files/patch-src_stubs.c
@@ -0,0 +1,10 @@
+--- src/stubs.c.orig	2020-08-26 10:46:52 UTC
++++ src/stubs.c
+@@ -109,7 +109,6 @@ void ReloadImageIntoD3DImmediateSurface(IMAGEHEADER* i
+ int NumberOfLandscapePolygons;
+ int FMVParticleColour;
+ int WireFrameMode;
+-int WaterFallBase;
+ 
+ void InitDrawTest()
+ {
