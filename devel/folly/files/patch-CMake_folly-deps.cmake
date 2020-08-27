35182f24ae12325f5079eb3f1a616245091d3dba
Yuri Victorovich <yuri@FreeBSD.org>
2020-08-27 07:06:13 +0000

    devel/folly: Update 2020.08.17.00 -> 2020.08.24.00
    Reported by:	portscout

--- devel/folly/files/patch-CMake_folly-deps.cmake
+++ devel/folly/files/patch-CMake_folly-deps.cmake
@@ -1,9 +1,9 @@
---- CMake/folly-deps.cmake.orig	2020-05-29 02:22:17 UTC
+--- CMake/folly-deps.cmake.orig	2020-08-22 08:44:17 UTC
 +++ CMake/folly-deps.cmake
-@@ -155,7 +155,7 @@ endif()
+@@ -157,7 +157,7 @@ endif()
  find_package(Backtrace)
  set(FOLLY_HAVE_BACKTRACE ${Backtrace_FOUND})
- if (FOLLY_HAVE_ELF_H AND FOLLY_HAVE_BACKTRACE AND LIBDWARF_FOUND)
+ if (FOLLY_HAVE_ELF AND FOLLY_HAVE_BACKTRACE AND LIBUNWIND_FOUND AND LIBDWARF_FOUND)
 -  set(FOLLY_USE_SYMBOLIZER ON)
 +  set(FOLLY_USE_SYMBOLIZER ${FREEBSD_FOLLY_USE_SYMBOLIZER})
  endif()
