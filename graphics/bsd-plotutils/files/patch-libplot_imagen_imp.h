f849872d7ff8644475dc5fa38d3626e1e263b640
Tobias C. Berner <tcberner@FreeBSD.org>
2020-08-28 11:02:20 +0000

    graphics/bsd-plotutils: fix build on recent current

---
+++ graphics/bsd-plotutils/files/patch-libplot_imagen_imp.h
@@ -0,0 +1,10 @@
+--- libplot/imagen/imp.h.orig	2020-08-28 11:01:25 UTC
++++ libplot/imagen/imp.h
+@@ -13,6 +13,6 @@ extern float botx;
+ extern float oboty;
+ extern float scalex;
+ extern float scaley;
+-int imPx, imPy;
++extern int imPx, imPy;
+ extern int imPcsize;
+ extern char *imP_charset;
