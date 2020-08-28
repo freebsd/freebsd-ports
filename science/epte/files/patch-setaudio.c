29ee96d0dca283c394ff9fe5b4c4c5a2bde14643
Tobias C. Berner <tcberner@FreeBSD.org>
2020-08-28 20:59:08 +0000

    science/epte: fix build on recent current

--- science/epte/files/patch-setaudio.c
+++ science/epte/files/patch-setaudio.c
@@ -1,5 +1,14 @@
---- setaudio.c.orig	2010-09-01 10:55:30.665968356 +0200
-+++ setaudio.c	2010-09-01 10:56:04.763401409 +0200
+--- setaudio.c.orig	1999-04-07 20:06:21 UTC
++++ setaudio.c
+@@ -297,7 +297,7 @@ modification follow.
+ #include <fcntl.h>
+ #include "periodic.h"
+ 
+-FILE *Audio;
++extern FILE *Audio;
+ int AudioFiles;
+ 
+ extern char *program_root;
 @@ -312,7 +312,7 @@ void setaudio ( void )
  char File_Name[1024];
  char *file_name;
