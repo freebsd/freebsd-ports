5f357bba59a3f5a6818134491a84699efc6d035f
Tobias C. Berner <tcberner@FreeBSD.org>
2020-08-28 17:08:37 +0000

    devel/cligen: fix build on recent current

---
+++ devel/cligen/files/patch-cligen__getline.h
@@ -0,0 +1,20 @@
+--- cligen_getline.h.orig	2020-08-28 17:05:52 UTC
++++ cligen_getline.h
+@@ -48,11 +48,11 @@ void	gl_redraw(cligen_handle h);	/* issue \n and redra
+ int     gl_regfd(int, cligen_fd_cb_t *, void *);
+ int     gl_unregfd(int);
+ 
+-int 	(*gl_in_hook)(void *, char *);
+-int 	(*gl_out_hook)(void*, char *);
+-int	(*gl_tab_hook)(cligen_handle, int *);
+-cligen_susp_cb_t *gl_susp_hook;
+-cligen_interrupt_cb_t *gl_interrupt_hook;
+-int	(*gl_qmark_hook)(cligen_handle, char *);
++extern int 	(*gl_in_hook)(void *, char *);
++extern int 	(*gl_out_hook)(void*, char *);
++extern int	(*gl_tab_hook)(cligen_handle, int *);
++extern cligen_susp_cb_t *gl_susp_hook;
++extern cligen_interrupt_cb_t *gl_interrupt_hook;
++extern int	(*gl_qmark_hook)(cligen_handle, char *);
+ 
+ #endif /* CLIGEN_GETLINE_H */
