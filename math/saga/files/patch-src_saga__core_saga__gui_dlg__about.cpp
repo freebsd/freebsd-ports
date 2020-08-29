840978d42ec1f8528503eed8b7c07fa9c2d5c050
Fernando Apesteguía <fernape@FreeBSD.org>
2020-08-29 16:56:00 +0000

    math/saga: Update to 7.7.0
    ChangeLog: https://sourceforge.net/p/saga-gis/wiki/Changelog%207.7.0/attachment/changelog_saga_7.7.0.txt
    
    PR:	248949
    Submitted by:	rhurlin@gwdg.de (maintainer)

--- math/saga/files/patch-src_saga__core_saga__gui_dlg__about.cpp
+++ math/saga/files/patch-src_saga__core_saga__gui_dlg__about.cpp
@@ -1,6 +1,6 @@
---- src/saga_core/saga_gui/dlg_about.cpp.orig	2018-11-22 07:53:06 UTC
+--- src/saga_core/saga_gui/dlg_about.cpp.orig	2020-08-25 19:21:15 UTC
 +++ src/saga_core/saga_gui/dlg_about.cpp
-@@ -188,6 +188,7 @@ wxString CDLG_About::_Get_Version(void)
+@@ -177,6 +177,7 @@ wxString CDLG_About::_Get_Version(void)
  	s	+= "http://www.saga-gis.org\n";
  	s	+= "\n";
  	s	+= "Version: " + wxString(SAGA_VERSION) + "\n";
@@ -8,12 +8,73 @@
  
  #ifdef _SAGA_MSW
  	#ifdef _WIN64
-@@ -263,7 +264,47 @@ wxString CDLG_About::_Get_Version(void)
+@@ -254,13 +255,107 @@ wxString CDLG_About::_Get_Version(void)
+ 		#elif	defined(__PGI)
+ 			"Portland PGCC/PGCPP"
+ 		#elif	defined(__clang__)
+-			"Clang/LLVM"
++			#if   (__clang_major__ == 11 && __clang_minor__ == 0 && __clang_patchlevel__ == 1)
++				"Clang/LLVM 11.0.1"
++			#elif (__clang_major__ == 11 && __clang_minor__ == 0 && __clang_patchlevel__ == 0)
++				"Clang/LLVM 11.0.0"
++			#elif (__clang_major__ == 10 && __clang_minor__ == 0 && __clang_patchlevel__ == 1)
++				"Clang/LLVM 10.0.1"
++			#elif (__clang_major__ == 10 && __clang_minor__ == 0 && __clang_patchlevel__ == 0)
++				"Clang/LLVM 10.0.0"
++			#elif (__clang_major__ == 9 && __clang_minor__ == 0 && __clang_patchlevel__ == 1)
++				"Clang/LLVM 9.0.1"
++			#elif (__clang_major__ == 9 && __clang_minor__ == 0 && __clang_patchlevel__ == 0)
++				"Clang/LLVM 9.0.0"
++			#elif (__clang_major__ == 8 && __clang_minor__ == 0 && __clang_patchlevel__ == 1)
++				"Clang/LLVM 8.0.1"
++			#elif (__clang_major__ == 8 && __clang_minor__ == 0 && __clang_patchlevel__ == 0)
++				"Clang/LLVM 8.0.0"
++			#elif (__clang_major__ == 7 && __clang_minor__ == 0 && __clang_patchlevel__ == 1)
++				"Clang/LLVM 7.0.1"
++			#elif (__clang_major__ == 7 && __clang_minor__ == 0 && __clang_patchlevel__ == 0)
++				"Clang/LLVM 7.0.0"
++			#elif (__clang_major__ == 6 && __clang_minor__ == 0 && __clang_patchlevel__ == 1)
++				"Clang/LLVM 6.0.1"
++			#elif (__clang_major__ == 6 && __clang_minor__ == 0 && __clang_patchlevel__ == 0)
++				"Clang/LLVM 6.0.0"
++			#else
++				"Clang/LLVM"
++			#endif
+ 		#elif	defined(__MINGW32__)
+ 			"MinGW"
  		#elif	defined(__GNUWIN32__)
  			"Gnu-Win32 compiler"
  		#elif	defined(__GNUG__)
 -			"Gnu C++"
-+			#if   wxCHECK_GCC_VERSION(6,4)
++			#if   wxCHECK_GCC_VERSION(9,4)
++				"Gnu C++ 9.4"
++			#elif wxCHECK_GCC_VERSION(9,3)
++				"Gnu C++ 9.3"
++			#elif wxCHECK_GCC_VERSION(9,2)
++				"Gnu C++ 9.2"
++			#elif wxCHECK_GCC_VERSION(9,1)
++				"Gnu C++ 9.1"
++			#elif wxCHECK_GCC_VERSION(9,0)
++				"Gnu C++ 9.0"
++			#elif wxCHECK_GCC_VERSION(8,3)
++				"Gnu C++ 8.3"
++			#elif wxCHECK_GCC_VERSION(8,2)
++				"Gnu C++ 8.2"
++			#elif wxCHECK_GCC_VERSION(8,1)
++				"Gnu C++ 8.1"
++			#elif wxCHECK_GCC_VERSION(8,0)
++				"Gnu C++ 8.0"
++			#elif wxCHECK_GCC_VERSION(7,4)
++				"Gnu C++ 7.4"
++			#elif wxCHECK_GCC_VERSION(7,3)
++				"Gnu C++ 7.3"
++			#elif wxCHECK_GCC_VERSION(7,2)
++				"Gnu C++ 7.2"
++			#elif wxCHECK_GCC_VERSION(7,1)
++				"Gnu C++ 7.1"
++			#elif wxCHECK_GCC_VERSION(7,0)
++				"Gnu C++ 7.0"
++			#elif wxCHECK_GCC_VERSION(6,4)
 +				"Gnu C++ 6.4"
 +			#elif wxCHECK_GCC_VERSION(6,3)
 +				"Gnu C++ 6.3"
