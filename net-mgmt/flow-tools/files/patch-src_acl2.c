687cea0eaa7784a69755de2fa6f9a642d32838a1
Tobias C. Berner <tcberner@FreeBSD.org>
2020-08-29 06:33:20 +0000

    net-mgmt/flow-tools: fix build on recent current

---
+++ net-mgmt/flow-tools/files/patch-src_acl2.c
@@ -0,0 +1,11 @@
+--- src/acl2.c.orig	2020-08-29 06:28:09 UTC
++++ src/acl2.c
+@@ -46,7 +46,7 @@
+ 
+ #include "acl2.h"
+ 
+-struct acl_list acl_list;
++// struct acl_list acl_list;
+ 
+ #define E acl_list.acl_ip_ext[index]
+ 
