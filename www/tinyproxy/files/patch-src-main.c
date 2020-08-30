6fca5d6a53e7ac4c69e7faf796a4680398c46b83
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-08-30 07:14:42 +0000

    Use upstream release tarball

--- www/tinyproxy/files/patch-src-main.c
+++ www/tinyproxy/files/patch-src-main.c
@@ -1,6 +1,6 @@
---- src/main.c.orig	2018-09-01 02:06:59 UTC
+--- src/main.c.orig	2018-09-01 00:55:39 UTC
 +++ src/main.c
-@@ -395,6 +395,11 @@ main (int argc, char **argv)
+@@ -395,17 +395,17 @@ main (int argc, char **argv)
                  }
          }
  
@@ -12,15 +12,14 @@
          /* Switch to a different user if we're running as root */
          if (geteuid () == 0)
                  change_user (argv[0]);
-@@ -402,11 +407,6 @@ main (int argc, char **argv)
+         else
                  log_message (LOG_WARNING,
                               "Not running as root, so not changing UID/GID.");
- 
+-
 -        /* Create log file after we drop privileges */
 -        if (setup_logging ()) {
 -                exit (EX_SOFTWARE);
 -        }
--
+ 
          if (child_pool_create () < 0) {
                  fprintf (stderr,
-                          "%s: Could not create the pool of children.\n",
