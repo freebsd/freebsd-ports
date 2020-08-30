1cbce2fe1571269350caa91f028c521e5f5e69af
Josh Paetzel <jpaetzel@FreeBSD.org>
2020-08-30 03:11:46 +0000

    Patch source code to fix the problem -fno-common tripped over
    Along the way take maintainership.  I have a soft spot for WM
    
    Fixing this was faster than replying to the thread caused by r546907

--- x11-clocks/wmtime/files/patch-wmtime.c
+++ x11-clocks/wmtime/files/patch-wmtime.c
@@ -1,5 +1,5 @@
---- wmtime.c	Wed May 20 06:34:53 1998
-+++ wmtime.c2	Sat Nov 28 18:44:59 1998
+--- wmtime.c.orig	1998-05-19 21:04:53 UTC
++++ wmtime.c
 @@ -89,7 +89,7 @@
  char	*ProgName;
  int		digital = 0;
@@ -9,3 +9,11 @@
  
  /* functions */
  void usage(void);
+@@ -166,6 +166,7 @@ void get_lang(){
+ char		*left_action = NULL;
+ char		*right_action = NULL;
+ char		*middle_action = NULL;
++Display		*display;
+ 
+ void DrawTime(int, int, int);
+ void DrawWijzer(int, int, int);
