fb4dae6760d3d5cea20cb8c43753152573ae78d1
Boris Samorodov <bsam@FreeBSD.org>
2020-08-30 19:57:40 +0000

    x11/fbpanel: revert previous commit. The port gas been already fixed.

--- x11/fbpanel/files/patch-panel_plugin.h
+++ x11/fbpanel/files/patch-panel_plugin.h
@@ -1,11 +1,11 @@
---- panel/plugin.h.orig	2020-08-30 19:42:52 UTC
+--- panel/plugin.h.orig	2020-08-28 22:09:44 UTC
 +++ panel/plugin.h
 @@ -9,7 +9,7 @@
  #include <stdio.h>
  #include "panel.h"
  
 -struct _plugin_instance *stam;
-+struct _plugin_instance;
++extern struct _plugin_instance *stam;
  
  typedef struct {
      /* common */
