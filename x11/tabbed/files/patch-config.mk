e5622822e187c55f2c8fae882bb93a382501066f
Alexey Dokuchaev <danfe@FreeBSD.org>
2020-09-01 08:41:23 +0000

    Convert the patch in the "context diff" format which no longer correctly applies by the modern FreeBSD patch(1).  Patch was applied on the FreeBSD 8.4 and then regenerated with ``make makepatch''.  While here, replace hardcoded ``/usr/local'' with ${LOCALBASE} as appropriate, merge in the change made with sed(1), and typofix + shorten the COMMENT text.
    Reported by:	pkg-fallout

--- x11/tabbed/files/patch-config.mk
+++ x11/tabbed/files/patch-config.mk
@@ -1,39 +1,29 @@
-*** config.mk.orig	2014-01-19 21:53:24.000000000 +0400
---- config.mk	2014-01-19 22:40:13.000000000 +0400
-***************
-*** 5,15 ****
-  
-  # paths
-  PREFIX = /usr/local
-! MANPREFIX = ${PREFIX}/share/man
-  
-  # includes and libs
-! INCS = -I. -I/usr/include
-! LIBS = -L/usr/lib -lc -lX11
-  
-  # flags
-  CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE
---- 5,15 ----
-  
-  # paths
-  PREFIX = /usr/local
-! MANPREFIX = ${PREFIX}/man
-  
-  # includes and libs
-! INCS = -I. -I/usr/local/include
-! LIBS = -L/usr/local/lib -lc -lX11
-  
-  # flags
-  CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE
-***************
-*** 21,25 ****
-  #LDFLAGS = ${LIBS}
-  
-  # compiler and linker
-! CC = cc
-  
---- 21,25 ----
-  #LDFLAGS = ${LIBS}
-  
-  # compiler and linker
-! CC?= cc
+--- config.mk.orig	2014-01-21 18:22:03 UTC
++++ config.mk
+@@ -5,15 +5,15 @@ VERSION = 0.6
+ 
+ # paths
+ PREFIX = /usr/local
+-MANPREFIX = ${PREFIX}/share/man
++MANPREFIX = ${PREFIX}/man
+ 
+ # includes and libs
+-INCS = -I. -I/usr/include
+-LIBS = -L/usr/lib -lc -lX11
++INCS = -I. -I${LOCALBASE}/include
++LIBS = -L${LOCALBASE}/lib -lX11
+ 
+ # flags
+ CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE
+-CFLAGS = -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
++CFLAGS += -Wall ${INCS} ${CPPFLAGS}
+ LDFLAGS = -s ${LIBS}
+ 
+ # Solaris
+@@ -21,5 +21,5 @@ LDFLAGS = -s ${LIBS}
+ #LDFLAGS = ${LIBS}
+ 
+ # compiler and linker
+-CC = cc
++CC?= cc
+ 
