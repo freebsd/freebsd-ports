df129e029eb8ce067dcc7c19d3ca3bbe33b49639
Dima Panov <fluffy@FreeBSD.org>
2020-09-02 14:39:50 +0000

    databases/percona57-*: update to 5.7.31-34 release

--- databases/percona57-pam-for-mysql/files/patch-cmake_build__configurations_compiler__options.cmake
+++ databases/percona57-pam-for-mysql/files/patch-cmake_build__configurations_compiler__options.cmake
@@ -1,20 +1,20 @@
 --- cmake/build_configurations/compiler_options.cmake.orig	2016-08-25 11:52:06 UTC
 +++ cmake/build_configurations/compiler_options.cmake
-@@ -29,7 +29,7 @@ IF(UNIX)  
- 
-   # Default GCC flags
+@@ -50,7 +50,7 @@ IF(UNIX)  
    IF(CMAKE_COMPILER_IS_GNUCC)
--    SET(COMMON_C_FLAGS               "-g -fabi-version=2 -fno-omit-frame-pointer -fno-strict-aliasing")
-+    SET(COMMON_C_FLAGS               "-g -fno-omit-frame-pointer -fno-strict-aliasing")
+     SET(COMMON_C_FLAGS               "-g -fno-omit-frame-pointer -fno-strict-aliasing")
+     IF(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 10.0)  # gcc-9 or older
+-      SET(COMMON_C_FLAGS             "-fabi-version=2 ${COMMON_C_FLAGS}")
++      SET(COMMON_C_FLAGS             "${COMMON_C_FLAGS}")
+     ENDIF()
      # Disable inline optimizations for valgrind testing to avoid false positives
      IF(WITH_VALGRIND)
-       SET(COMMON_C_FLAGS             "-fno-inline ${COMMON_C_FLAGS}")
-@@ -42,7 +42,7 @@ IF(UNIX)  
-     SET(CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 ${COMMON_C_FLAGS}")
-   ENDIF()
+@@ -77,7 +77,7 @@ IF(UNIX)  
    IF(CMAKE_COMPILER_IS_GNUCXX)
--    SET(COMMON_CXX_FLAGS               "-g -fabi-version=2 -fno-omit-frame-pointer -fno-strict-aliasing")
-+    SET(COMMON_CXX_FLAGS               "-g -fno-omit-frame-pointer -fno-strict-aliasing")
+     SET(COMMON_CXX_FLAGS               "-g -fno-omit-frame-pointer -fno-strict-aliasing")
+     IF(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 10.0)  # gcc-9 or older
+-      SET(COMMON_CXX_FLAGS             "-fabi-version=2 ${COMMON_CXX_FLAGS}")
++      SET(COMMON_CXX_FLAGS             "${COMMON_CXX_FLAGS}")
+     ENDIF()
      # GCC 6 has C++14 as default, set it explicitly to the old default.
      EXECUTE_PROCESS(COMMAND ${CMAKE_CXX_COMPILER} -dumpversion
-                     OUTPUT_VARIABLE GXX_VERSION)
