87064b991117c491ea59292a4263146dbb3ac7eb
Stefan Eßer <se@FreeBSD.org>
2020-09-03 14:31:51 +0000

    Fix build with -fno-common

---
+++ games/libretro-cap32/files/patch-libretro_retro__events.c
@@ -0,0 +1,11 @@
+--- libretro/retro_events.c.orig	2020-04-17 23:23:41 UTC
++++ libretro/retro_events.c
+@@ -56,6 +56,8 @@ const uint8_t bit_values[8] = {0x01, 0x02, 0x04, 0x08,
+ #define MAX_BUTTONS 14
+ #define MAX_PADCFG 4
+ 
++bool (*ev_events)(void);
++
+ static uint8_t keyboard_translation[MAX_KEYSYMS];
+ unsigned int last_input[PORTS_NUMBER] = {0,0};
+ uint32_t padnum = 0;
