2a344f800adb7c78cd410ca7bc5dddd802420d51
Yuri Victorovich <yuri@FreeBSD.org>
2020-09-05 06:13:37 +0000

    www/py-pywikibot: Update 3.0.20200703 -> 4.3.0

--- www/py-pywikibot/files/patch-setup.py
+++ www/py-pywikibot/files/patch-setup.py
@@ -1,41 +1,21 @@
---- setup.py.orig	2020-04-29 18:50:22 UTC
+--- setup.py.orig	2020-09-05 06:11:40 UTC
 +++ setup.py
-@@ -168,22 +168,22 @@ def get_version(name):
-     @rtype: str
-     """
-     version = '3.0'
+@@ -173,12 +173,12 @@ def get_validated_version():  # pragma: no cover
+     # validate version for sdist
+     from contextlib import suppress
+     from subprocess import run, PIPE
 -    try:
--        import subprocess
--        date = subprocess.check_output(
--            ['git', 'log', '-1', '--format=%ci']).strip()
--        date = date.decode().split(' ', 1)[0].replace('-', '')
--        version += '.' + date
--        if 'sdist' not in sys.argv:
--            version += '.dev0'
+-        tags = run(['git', 'tag'], check=True, stdout=PIPE,
+-                   universal_newlines=True).stdout.splitlines()
 -    except Exception as e:
 -        print(e)
--        from pkg_resources import get_distribution, DistributionNotFound
--        try:
--            version = get_distribution(name).version
--        except DistributionNotFound as e:
--            print(e)
--            version += '.dev0'
+-        sys.exit('Creating source distribution canceled.')
 +    #try:
-+    #    import subprocess
-+    #    date = subprocess.check_output(
-+    #        ['git', 'log', '-1', '--format=%ci']).strip()
-+    #    date = date.decode().split(' ', 1)[0].replace('-', '')
-+    #    version += '.' + date
-+    #    if 'sdist' not in sys.argv:
-+    #        version += '.dev0'
++    #    tags = run(['git', 'tag'], check=True, stdout=PIPE,
++    #               universal_newlines=True).stdout.splitlines()
 +    #except Exception as e:
 +    #    print(e)
-+    #    from pkg_resources import get_distribution, DistributionNotFound
-+    #    try:
-+    #        version = get_distribution(name).version
-+    #    except DistributionNotFound as e:
-+    #        print(e)
-+    #        version += '.dev0'
-     return version
++    #    sys.exit('Creating source distribution canceled.')
  
- 
+     for tag in ('stable', 'python2'):
+         with suppress(ValueError):
