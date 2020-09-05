f9dae2fc7cf7bc0a93724ad31b3f7b41a344bbce
Baptiste Daroussin <bapt@FreeBSD.org>
2020-09-05 20:43:23 +0000

    Revert r547341 tcberner have been faster than I did to address the problem in r546805
    Somehow I missed his commit and worked on an outdated tree when I worked on
    r547341 apparently

--- x11/dmenu-wayland/files/patch-draw.h
+++
@@ -1,8 +0,0 @@
---- draw.h.orig	2019-06-25 12:55:46 UTC
-+++ draw.h
-@@ -96,4 +96,4 @@ void eprintf(const char *fmt, ...);
- void weprintf(const char *fmt, ...);
- int32_t round_to_int(double val);
- 
--const char *progname;
-+extern const char *progname;
