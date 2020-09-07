410edceabd8c40821eefafa40fef9c00b1b97d53
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-09-07 00:58:07 +0000

    Mk/bsd.gecko.mk: fix build with defined CPUTYPE on powerpc64 (and possibly others)
    In /etc/make.conf:
    CPUTYPE?=power9
    
    When trying to build www/firefox:
    make: "/usr/ports/Mk/bsd.gecko.mk" line 265: Malformed conditional (${ARCH:Maarch64} || ${MACHINE_CPU:Msse2})
    make: Fatal errors encountered -- cannot continuemake: "/usr/ports/Mk/bsd.gecko.mk" line 265: Malformed conditional (${ARCH:Maarch64} || ${MACHINE_CPU:Msse2})
    make: "/usr/ports/Mk/bsd.port.mk" line 4517: warning: duplicate script for target "/tmp/usr/ports/www/firefox/work-make" ignored
    make: "/usr/ports/Mk/bsd.port.mk" line 3423: warning: using previous script for "/tmp/usr/ports/www/firefox/work-make" defined here
    make: "/usr/ports/Mk/bsd.port.mk" line 5203: warning: duplicate script for target "/tmp/usr/ports/www/firefox/work-make" ignored
    make: "/usr/ports/Mk/bsd.port.mk" line 3423: warning: using previous script for "/tmp/usr/ports/www/firefox/work-make" defined here
    make: "/usr/ports/Mk/bsd.port.mk" line 5364: warning: duplicate script for target "/tmp/usr/ports/www/firefox/work-make" ignored
    make: "/usr/ports/Mk/bsd.port.mk" line 3423: warning: using previous script for "/tmp/usr/ports/www/firefox/work-make" defined here
    make: "/usr/ports/Mk/bsd.port.mk" line 5364: warning: duplicate script for target "/tmp/usr/ports/www/firefox/work-make" ignored
    make: "/usr/ports/Mk/bsd.port.mk" line 3423: warning: using previous script for "/tmp/usr/ports/www/firefox/work-make" defined here
    make: "/usr/ports/Mk/bsd.port.mk" line 5364: warning: duplicate script for target "/tmp/usr/ports/www/firefox/work-make" ignored
    make: "/usr/ports/Mk/bsd.port.mk" line 3423: warning: using previous script for "/tmp/usr/ports/www/firefox/work-make" defined here
    make: "/usr/ports/Mk/bsd.port.mk" line 5364: warning: duplicate script for target "/tmp/usr/ports/www/firefox/work-make" ignored
    make: "/usr/ports/Mk/bsd.port.mk" line 3423: warning: using previous script for "/tmp/usr/ports/www/firefox/work-make" defined here
    make: "/usr/ports/Mk/bsd.port.mk" line 5364: warning: duplicate script for target "/tmp/usr/ports/www/firefox/work-make" ignored
    make: "/usr/ports/Mk/bsd.port.mk" line 3423: warning: using previous script for "/tmp/usr/ports/www/firefox/work-make" defined here
    make: "/usr/ports/Mk/bsd.port.mk" line 5364: warning: duplicate script for target "/tmp/usr/ports/www/firefox/work-make" ignored
    make: "/usr/ports/Mk/bsd.port.mk" line 3423: warning: using previous script for "/tmp/usr/ports/www/firefox/work-make" defined here
    make: "/usr/ports/Mk/bsd.port.mk" line 5364: warning: duplicate script for target "/tmp/usr/ports/www/firefox/work-make" ignored
    make: "/usr/ports/Mk/bsd.port.mk" line 3423: warning: using previous script for "/tmp/usr/ports/www/firefox/work-make" defined here
    make: Fatal errors encountered -- cannot continuemake: "/usr/ports/Mk/bsd.gecko.mk" line 265: Malformed conditional (${ARCH:Maarch64} || ${MACHINE_CPU:Msse2})
    make: Fatal errors encountered -- cannot continuemake: "/usr/ports/Mk/bsd.gecko.mk" line 265: Malformed conditional (${ARCH:Maarch64} || ${MACHINE_CPU:Msse2})
    make: Fatal errors encountered -- cannot continuemake: "/usr/ports/Mk/bsd.gecko.mk" line 265: Malformed conditional (${ARCH:Maarch64} || ${MACHINE_CPU:Msse2})
    make: Fatal errors encountered -- cannot continue
    
    PR:		248826
    Approved by:	gecko (maintainer timeout)

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -262,7 +262,7 @@ MOZ_OPTIONS+=	--enable-debug --disable-release
 STRIP=	# ports/184285
 .else
 MOZ_OPTIONS+=	--disable-debug --disable-debug-symbols --enable-release
-. if ${ARCH:Maarch64} || ${MACHINE_CPU:Msse2}
+. if ${ARCH:Maarch64} || (defined(MACHINE_CPU) && ${MACHINE_CPU:Msse2})
 MOZ_OPTIONS+=	--enable-rust-simd
 . endif
 .endif
