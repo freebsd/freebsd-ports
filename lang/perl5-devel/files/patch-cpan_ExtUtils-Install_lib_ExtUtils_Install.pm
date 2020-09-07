e519ec0dbb7d7a273130755b12dff33e47e65223
Mathieu Arnold <mat@FreeBSD.org>
2020-09-07 15:13:13 +0000

    Update to v5.33.1-184-g2bd36f42f4.

--- lang/perl5-devel/files/patch-cpan_ExtUtils-Install_lib_ExtUtils_Install.pm
+++ lang/perl5-devel/files/patch-cpan_ExtUtils-Install_lib_ExtUtils_Install.pm
@@ -1,9 +1,9 @@
 Make installed files writable by the installing user.
 
---- cpan/ExtUtils-Install/lib/ExtUtils/Install.pm.orig	2017-06-05 16:14:15 UTC
+--- cpan/ExtUtils-Install/lib/ExtUtils/Install.pm.orig	2020-09-07 12:26:35 UTC
 +++ cpan/ExtUtils-Install/lib/ExtUtils/Install.pm
 @@ -825,7 +825,7 @@ sub install { #XXX OS-SPECIFIC
-                 utime($atime,$mtime + Is_VMS,$targetfile) unless $dry_run>1;
+                 utime($atime,$mtime + _Is_VMS,$targetfile) unless $dry_run>1;
  
  
 -                $mode = 0444 | ( $mode & 0111 ? 0111 : 0 );
