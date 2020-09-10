e75f64e1bd0ba53358cd6b22c8b1d96196eeb2bc
Joseph Mingrone <jrm@FreeBSD.org>
2020-09-10 13:52:10 +0000

    textproc/kibana7: Update to version 7.9.1
    Changelog:
    * Notable issues fixed:
      - Fixes in Docker image configuration of Monitoring and Central
        Management:
        - As more and more users adopt our docker images, we’ve been getting
          reports on gaps where these images don’t provide all the configuration
          knobs the other artifacts do. This release exposes more environment
          variables for configuring proxy support and certificate verification
          mode for monitoring and central management , and for configuring
          verification_mode
      - Pipeline execution fixes to flushing and shutdown
        - Since 7.2.0, a change caused terminating pipelines to not request
          input plugins to cleanup before shutdown, which could cause leaks in
          resources that weren’t freed during pipeline reloads.
        - Logstash supports ordered execution for pipelines with a single
          worker. A bug was found in this mode where the flushing mechanism
          wasn’t working, preventing plugins like the aggregate filter from
          working correctly. This has been fixed.
    
    * Consistent Fingerprinting:
      - Our fingerprint filter is a popular solution to perform deduplication
        of data in downstream systems like Elasticsearch, by computing a hash
        value based on data from each event. Users reported that this filter
        could produced different values for events containing the same data
        since it didn’t ensure the order in which Hash Maps/Objects/Ruby Hashes
        processed their key/value pairs. This has now been fixed.
    
    * Updated JRuby to 9.2.13.0:
      - The new JRuby release brings greater stability to its code
        optimizations in multithreaded workloads and a fix to exception
        handling in Windows environments, both issues that could affect our
        users. See the JRuby release notes for more information.
    
    * Plugins:
      - Avro Codec - 3.2.4
        - [DOC] Add clarifications on partial deserialization
      - Fingerprint Filter - 3.2.2
        - Fixed lack of consistent fingerprints on Hash/Map objects
      - Kv Filter - 4.4.1
        - Fixed issue where a field_split_pattern containing a literal
          backslash failed to match correctly
      - Elasticsearch Input - 4.7.1
        - [DOC] Updated sliced scroll link to resolve to correct location after
          doc structure change
        - [DOC] Added usage example of docinfo metadata
      - Http_poller Input - 5.0.2
        - [DOC]Expanded url option to include Manticore keys
      - Snmp Input - 1.2.5
        - Updated snmp4j library to v2.8.4
        - Fixed: support SNMPv3 multiple identical security name with different
          credentials
        - Fixed: multithreading problem when using multiple snmp inputs with
          multiple v3 credentials
      - Syslog Input - 3.4.4
        - Refactor: avoid global side-effect + cleanup
        - avoid setting BasicSocket.do_not_reverse_lookup as it has side
          effects for others
      - Jdbc Integration - 5.0.6
        - DOC:Replaced plugin_header file with plugin_header-integration file.
      - Rabbitmq Integration - 7.1.1
        - DOC:Replaced plugin_header file with plugin_header-integration file.
      - Elasticsearch Output - 10.6.2
        - [DOC] Added clarifying info on http compression settings and behaviors
        - [DOC] Fixed entry for ilm_policy default value
    
    PR:		249108
    Submitted by:	Juraj Lutter <juraj@lutter.sk>

---
+++ textproc/kibana7/files/patch-src_setup__node__env_prebuilt__dev__only__entry.js
@@ -0,0 +1,9 @@
+--- src/setup_node_env/prebuilt_dev_only_entry.js.orig	2020-08-22 12:22:07 UTC
++++ src/setup_node_env/prebuilt_dev_only_entry.js
+@@ -28,4 +28,4 @@ require('symbol-observable');
+ 
+ require('./root');
+ 
+-require('./node_version_validator');
+\ No newline at end of file
++//require('./node_version_validator');
