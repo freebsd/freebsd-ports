51bf6f116cfbb80048761504c16ff35856d12008
Baptiste Daroussin <bapt@FreeBSD.org>
2020-09-17 20:28:06 +0000

    Update to 1.24.1

--- sysutils/mate-control-center/files/patch-capplets_common_mate-theme-info.c
+++ sysutils/mate-control-center/files/patch-capplets_common_mate-theme-info.c
@@ -1,11 +1,11 @@
---- capplets/common/mate-theme-info.c.orig	2017-05-28 07:59:23 UTC
+--- capplets/common/mate-theme-info.c.orig	2020-08-15 13:10:32 UTC
 +++ capplets/common/mate-theme-info.c
-@@ -1819,7 +1819,7 @@ mate_theme_init ()
+@@ -1824,7 +1824,7 @@ mate_theme_init ()
+   g_strfreev (search_path);
  
    /* if there's a separate xcursors dir, add that as well */
-   if (strcmp (XCURSOR_ICONDIR, top_theme_dir_string) &&
--      strcmp (XCURSOR_ICONDIR, "/usr/share/icons")) {
-+      strcmp (XCURSOR_ICONDIR, "%%LOCALBASE%%/share/icons")) {
+-  if (strcmp (XCURSOR_ICONDIR, "/usr/share/icons")) {
++  if (strcmp (XCURSOR_ICONDIR, "%%LOCALBASE%%/share/icons")) {
      top_theme_dir = g_file_new_for_path (XCURSOR_ICONDIR);
      add_top_icon_theme_dir_monitor (top_theme_dir, 1, NULL);
      g_object_unref (top_theme_dir);
