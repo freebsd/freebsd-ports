8d29214ebea5ae31ba7920939e11b77373ee414b
Bradley T. Hughes <bhughes@FreeBSD.org>
2020-09-18 04:50:56 +0000

    MFH: r541372 r543678 r543682 r545547 r545560 r547570 r548046 r548047 r548707 r548802
    www/node: Update 14.4.0 -> 14.5.0
    
    https://nodejs.org/en/blog/release/v14.5.0/
    
    Refresh patches with `make makepatch`, and adapt them to the changes
    from upstream due to the V8 upgrade.
    
    Sponsored by:	Miles AS
    
    www/node10: Update 10.21.0 -> 10.22.0
    
    https://nodejs.org/en/blog/release/v10.22.0/
    
    Sponsored by:	Miles AS
    
    www/node: Update 14.6.0 -> 14.6.0_1
    
    Bump PORTREVISION after updating the minimum version requirements for
    libuv, libnghttp2, and libcares.
    
    The previous change for www/node erroneously mentioned the most recent
    www/node10 update. The correct changelog should have read:
    
    www/node: Update 14.5.0 -> 14.6.0
    
    https://nodejs.org/en/blog/release/v14.6.0/
    
    Sponsored by:	Miles AS
    
    www/node: Update 14.6.0_1 -> 14.7.0
    
    https://nodejs.org/en/blog/release/v14.7.0/
    
    Sponsored by:	Miles AS
    
    www/node: Update 14.7.0 -> 14.8.0
    
    https://nodejs.org/en/blog/release/v14.8.0/
    
    Sponsored by:	Miles AS
    
    www/node: Update 14.8.0 -> 14.9.0
    
    https://nodejs.org/en/blog/release/v14.9.0/
    
    Sponsored by:	Miles AS
    
    www/node: Update 14.9.0 -> 14.10.0
    
    https://nodejs.org/en/blog/release/v14.10.0/
    
    While here, refresh patches with `make makepatch`.
    
    Sponsored by:	Miles AS
    
    www/node: add dependency on archivers/brotli
    
    ... instead of building the version bundled with Node.js. Bump
    PORTREVISION due to the new dependency.
    
    Sponsored by:	Miles AS
    
    www/node: Update 14.10.0_1 -> 14.10.1
    
    https://nodejs.org/en/blog/release/v14.10.1/
    
    Sponsored by:	Miles AS
    
    www/node: Update 14.10.1 -> 14.11.0
    
    https://nodejs.org/en/blog/release/v14.11.0/
    
    This is a security release. See
    https://nodejs.org/en/blog/vulnerability/september-2020-security-releases/
    
    Security:	4ca5894c-f7f1-11ea-8ff8-0022489ad614
    Sponsored by:	Miles AS
    
    Approved by:	ports-secteam (fluffy)

--- www/node/files/patch-deps_v8_src_compiler_backend_instruction-selector.cc
+++ www/node/files/patch-deps_v8_src_compiler_backend_instruction-selector.cc
@@ -1,6 +1,6 @@
---- deps/v8/src/compiler/backend/instruction-selector.cc.orig	2020-04-21 11:38:50 UTC
+--- deps/v8/src/compiler/backend/instruction-selector.cc.orig	2020-07-20 22:18:45 UTC
 +++ deps/v8/src/compiler/backend/instruction-selector.cc
-@@ -2783,7 +2783,7 @@ void InstructionSelector::VisitCall(Node* node, BasicB
+@@ -2808,7 +2808,7 @@ void InstructionSelector::VisitCall(Node* node, BasicB
    switch (call_descriptor->kind()) {
      case CallDescriptor::kCallAddress: {
        int misc_field = static_cast<int>(call_descriptor->ParameterCount());
