f6267aba13ed929fa415cc7f14fb69581e3e9b93
Yuri Victorovich <yuri@FreeBSD.org>
2020-09-19 04:54:45 +0000

    audio/surge-synthesizer-lv2: Fix build on 11 by adding #define _WITH_GETLINE
    Reported by:	fallout

--- audio/surge-synthesizer-lv2/files/patch-vstgui.surge_vstgui_lib_platform_linux_x11fileselector.cpp
+++ audio/surge-synthesizer-lv2/files/patch-vstgui.surge_vstgui_lib_platform_linux_x11fileselector.cpp
@@ -1,10 +1,11 @@
---- vstgui.surge/vstgui/lib/platform/linux/x11fileselector.cpp.orig	2020-06-11 05:42:27 UTC
+--- vstgui.surge/vstgui/lib/platform/linux/x11fileselector.cpp.orig	2020-06-23 22:22:42 UTC
 +++ vstgui.surge/vstgui/lib/platform/linux/x11fileselector.cpp
-@@ -3,6 +3,7 @@
+@@ -3,6 +3,8 @@
  // distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
  
  #include "../../cfileselector.h"
-+#include <stdio.h>
++#define _WITH_GETLINE // for FreeBSD 11
++#include <stdio.h> // https://github.com/surge-synthesizer/vstgui/issues/7
  #include <unistd.h>
  #include <string>
  
