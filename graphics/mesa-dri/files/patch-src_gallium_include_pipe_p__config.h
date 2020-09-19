a55db457da83f8c69590ee9f4b08efc753c98b08
Emmanuel Vadot <manu@FreeBSD.org>
2020-09-19 09:33:33 +0000

    mesa: Remove sparc64 patches
    No one who cares about graphics have a sparc64 machine.
    Next update to 20.2 will not contain any patches too.
    
    Reviewed by:	zeising
    Approved by:	x11 (zeising@)
    Differential Revision:	https://reviews.freebsd.org/D26460

--- graphics/mesa-dri/files/patch-src_gallium_include_pipe_p__config.h
+++ graphics/mesa-dri/files/patch-src_gallium_include_pipe_p__config.h
@@ -1,29 +1,6 @@
---- src/gallium/include/pipe/p_config.h.orig	2017-10-02 15:49:02 UTC
+--- src/gallium/include/pipe/p_config.h.orig	2019-06-26 20:14:08 UTC
 +++ src/gallium/include/pipe/p_config.h
-@@ -77,6 +77,13 @@
- #define PIPE_CC_ICL
- #endif
- 
-+#if defined(__sparc__) || defined(__sparc64__)
-+#define PIPE_ARCH_SPARC
-+#if defined(__sparc64__)
-+#define PIPE_ARCH_SPARC_64
-+#endif
-+#endif
-+
- 
- /*
-  * Processor architecture
-@@ -131,7 +138,7 @@
- 
- #if defined(PIPE_ARCH_X86) || defined(PIPE_ARCH_X86_64) || defined(PIPE_ARCH_ARM) || defined(PIPE_ARCH_AARCH64)
- #define PIPE_ARCH_LITTLE_ENDIAN
--#elif defined(PIPE_ARCH_PPC) || defined(PIPE_ARCH_PPC_64) || defined(PIPE_ARCH_S390)
-+#elif defined(PIPE_ARCH_PPC) || defined(PIPE_ARCH_PPC_64) || defined(PIPE_ARCH_S390) || defined(PIPE_ARCH_SPARC) || defined(PIPE_ARCH_SPARC_64)
- #define PIPE_ARCH_BIG_ENDIAN
- #endif
- 
-@@ -160,6 +167,12 @@
+@@ -160,6 +160,12 @@
  #define PIPE_OS_ANDROID
  #endif
  
