8bd840bc1c5262a03044f85c1c4e7b893f7ff627
Stefan Eßer <se@FreeBSD.org>
2020-09-21 19:47:50 +0000

    Fix build by including stdlib.h for the definition of alloca()

---
+++ devel/ncc/files/patch-dbstree.C
@@ -0,0 +1,10 @@
+--- dbstree.C.orig	2004-12-01 03:54:40 UTC
++++ dbstree.C
+@@ -12,6 +12,7 @@
+ *****************************************************************************/
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <assert.h>
+ #include <string.h>
+ #include "dbstree.h"
