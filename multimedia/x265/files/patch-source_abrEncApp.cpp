dbaa3f3f8e63b4a3f7201a785938bbe8bcb3d469
Mikhail Teterin <mi@FreeBSD.org>
2020-09-21 04:47:33 +0000

    While we were testing the new code, upstream have removed the 3.4 release from download area. Change our port to download using Bitbucket's method.
    Although the upstream branch Release_3.4 is very similar to what
    used to be available as 3.4, there are a few improvements -- in
    particular, one of our patches is no longer necessary, which is
    nice.
    
    PR:		247035
    Reported by:	John Kennedy, Tomoaki AOKI
    Sponsored by:	United Marsupials

--- multimedia/x265/files/patch-source_abrEncApp.cpp
+++
@@ -1,9 +0,0 @@
---- source/abrEncApp.cpp	2020-05-29 13:39:35.000000000 -0400
-+++ source/abrEncApp.cpp	2020-07-19 17:37:32.365958000 -0400
-@@ -815,5 +815,5 @@
-             if (m_param->csvfn && !b_ctrl_c)
- #if ENABLE_LIBVMAF
--                api->vmaf_encoder_log(m_encoder, m_cliopt.argCount, m_cliopt.argString, m_cliopt.param, vmafdata);
-+                api->vmaf_encoder_log(m_encoder, m_cliopt.argCnt, m_cliopt.argString, m_cliopt.param, vmafdata);
- #else
-                 api->encoder_log(m_encoder, m_cliopt.argCnt, m_cliopt.argString);
