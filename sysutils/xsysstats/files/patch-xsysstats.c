af5c72ccd0319187bda6fd117b1a3c87e4e03394
Stefan Eßer <se@FreeBSD.org>
2020-09-22 11:56:27 +0000

    Fix build with -fno-common
    While here add a license (extracted from the README file).
    Redistribution is only allowed free of charge.

--- sysutils/xsysstats/files/patch-xsysstats.c
+++ sysutils/xsysstats/files/patch-xsysstats.c
@@ -1,8 +1,16 @@
---- xsysstats.c.orig	Sat Nov  9 08:44:11 2002
-+++ xsysstats.c	Sat Nov  9 08:44:45 2002
+--- xsysstats.c.orig	2000-05-30 06:39:48 UTC
++++ xsysstats.c
 @@ -1,5 +1,4 @@
  #include "xsysstats.h"
 -#include "headers.h"
  #include "patchlevel.h"
  
  struct base_types {
+@@ -48,6 +47,7 @@ struct base_types types[NUM_TYPES] = {
+ {"icpu", "icpu", 100}, 
+ };
+ 
++struct graph_info *graphs;
+ 
+ /* Various notes:
+  * we store the values in the 'points' variable.  They are stored in the
--- sysutils/xsysstats/files/patch-xsysstats.h
+++ sysutils/xsysstats/files/patch-xsysstats.h
@@ -1,5 +1,5 @@
---- xsysstats.h.orig	Tue May 30 08:46:20 2000
-+++ xsysstats.h	Sat Nov  9 08:43:29 2002
+--- xsysstats.h.orig	2000-05-30 06:46:20 UTC
++++ xsysstats.h
 @@ -15,12 +15,9 @@
  #include <stdio.h>
  #include <sys/param.h>
@@ -13,7 +13,19 @@
  #include <netdb.h>
  #include <rpcsvc/rstat.h>
  
-@@ -138,7 +135,7 @@
+@@ -115,8 +112,10 @@ struct graph_info {
+ 	short	host_offset;
+ 	short	window;		/* What window to put this graph in */
+ 	int	max_val;	/* maximum value this graph ever reached */
+-} *graphs;
++};
+ 
++extern struct graph_info *graphs;
++
+ struct Xss_Window {
+ 	short	x, y;		/* Upper left corner of window */
+ 	short	width,height;	/* width and height of the window.  height
+@@ -138,7 +137,7 @@ struct Host_Info {
      char    *name;	/*Name of the host */
  #ifdef USE_NEW_RSTAT
      CLIENT  *client;	/* client connection for rstat */
