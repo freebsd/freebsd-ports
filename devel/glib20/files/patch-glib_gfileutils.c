527499e0ed62a2c89e0991d61e9056bcdc3b4525
Baptiste Daroussin <bapt@FreeBSD.org>
2020-09-23 15:35:28 +0000

    Actually commit the glib 2.66.0 update
    Reported by:	fluffy
    Differential Revision:	https://reviews.freebsd.org/D25279

---
+++ devel/glib20/files/patch-glib_gfileutils.c
@@ -0,0 +1,42 @@
+https://gitlab.gnome.org/GNOME/glib/merge_requests/832
+
+Index: glib/gfileutils.c
+--- glib/gfileutils.c.orig
++++ glib/gfileutils.c
+@@ -259,7 +259,7 @@ g_mkdir_with_parents (const gchar *pathname,
+       
+       if (!g_file_test (fn, G_FILE_TEST_EXISTS))
+ 	{
+-	  if (g_mkdir (fn, mode) == -1 && errno != EEXIST)
++	  if (g_mkdir (fn, mode) == -1 && errno != EEXIST && (p ? (errno != ENOENT) : (-1)))
+ 	    {
+ 	      int errno_save = errno;
+ 	      if (errno != ENOENT || !p)
+https://gitlab.gnome.org/GNOME/glib/merge_requests/832
+
+Index: glib/gfileutils.c
+--- glib/gfileutils.c.orig
++++ glib/gfileutils.c
+@@ -259,7 +259,7 @@ g_mkdir_with_parents (const gchar *pathname,
+       
+       if (!g_file_test (fn, G_FILE_TEST_EXISTS))
+ 	{
+-	  if (g_mkdir (fn, mode) == -1 && errno != EEXIST)
++	  if (g_mkdir (fn, mode) == -1 && errno != EEXIST && (p ? (errno != ENOENT) : (-1)))
+ 	    {
+ 	      int errno_save = errno;
+ 	      if (errno != ENOENT || !p)
+https://gitlab.gnome.org/GNOME/glib/merge_requests/832
+
+Index: glib/gfileutils.c
+--- glib/gfileutils.c.orig
++++ glib/gfileutils.c
+@@ -259,7 +259,7 @@ g_mkdir_with_parents (const gchar *pathname,
+       
+       if (!g_file_test (fn, G_FILE_TEST_EXISTS))
+ 	{
+-	  if (g_mkdir (fn, mode) == -1 && errno != EEXIST)
++	  if (g_mkdir (fn, mode) == -1 && errno != EEXIST && (p ? (errno != ENOENT) : (-1)))
+ 	    {
+ 	      int errno_save = errno;
+ 	      if (errno != ENOENT || !p)
--- devel/glib20/files/patch-glib_gutils.c
+++ devel/glib20/files/patch-glib_gutils.c
@@ -1,20 +1,11 @@
---- glib/gutils.c.orig	Wed Mar 23 09:55:02 2005
-+++ glib/gutils.c	Thu Apr  7 01:06:16 2005
-@@ -1929,7 +1929,7 @@ g_get_system_data_dirs (void)
-       data_dirs = (gchar *) g_getenv ("XDG_DATA_DIRS");
+--- glib/gutils.c.orig	2018-12-23 14:10:41.000000000 +0100
++++ glib/gutils.c	2018-12-27 00:18:23.471950000 +0100
+@@ -2219,7 +2219,7 @@
+     }
+ #else
+   if (!conf_dirs || !conf_dirs[0])
+-    conf_dirs = "/etc/xdg";
++    conf_dirs = "/usr/local/etc/xdg:/etc/xdg";
  
-       if (!data_dirs || !data_dirs[0])
--          data_dirs = "/usr/local/share/:/usr/share/";
-+          data_dirs = "/usr/local/share/";
- 
-       data_dir_vector = g_strsplit (data_dirs, G_SEARCHPATH_SEPARATOR_S, 0);
- #endif
-@@ -1983,7 +1983,7 @@ g_get_system_config_dirs (void)
-       conf_dirs = (gchar *) g_getenv ("XDG_CONFIG_DIRS");
- 
-       if (!conf_dirs || !conf_dirs[0])
--          conf_dirs = "/etc/xdg";
-+          conf_dirs = "/usr/local/etc/xdg:/etc/xdg";
- 
-       conf_dir_vector = g_strsplit (conf_dirs, G_SEARCHPATH_SEPARATOR_S, 0);
+   conf_dir_vector = g_strsplit (conf_dirs, G_SEARCHPATH_SEPARATOR_S, 0);
  #endif
