c572c8166229b67002dddc2d8b6f0309ab9a5ba0
Stefan Eßer <se@FreeBSD.org>
2020-09-25 18:42:14 +0000

    Fix build with -fno-common

---
+++ emulators/nonpareil/files/patch-src_asm.h
@@ -0,0 +1,11 @@
+--- src/asm.h.orig	2008-08-24 00:37:18 UTC
++++ src/asm.h
+@@ -22,7 +22,7 @@ MA 02111, USA.
+ 
+ extern int arch;
+ 
+-int pass;
++extern int pass;
+ extern int lineno;
+ extern int errors;
+ 
