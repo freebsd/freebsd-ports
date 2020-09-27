0e5bdc3ed6164e4b5231a6c8a829cd0a7126e495
Nicola Vitale <nivit@FreeBSD.org>
2020-09-27 13:54:24 +0000

    - Update to 2.67 - Enable test target

--- math/asymptote/files/patch-xstream.h
+++ math/asymptote/files/patch-xstream.h
@@ -1,23 +1,11 @@
---- xstream.h.orig	2018-06-10 15:14:47 UTC
+--- xstream.h.orig	2020-08-06 02:32:59 UTC
 +++ xstream.h
-@@ -31,7 +31,7 @@
- #define quad_t long long
- #define u_quad_t unsigned long long
-   
--#if defined(__CYGWIN__) || defined(__FreeBSD__)
-+#if defined(__CYGWIN__)
- #include <sys/select.h>
- #define u_char unsigned char
- #define u_int unsigned int
-@@ -41,6 +41,11 @@ extern "C" int fseeko(FILE *, off_t, int
+@@ -41,7 +41,7 @@ extern "C" int fseeko(FILE *, off_t, int);
  extern "C" off_t ftello(FILE *);
- #endif  
+ #endif
  
-+#ifdef __FreeBSD__
-+#define xdr_longlong_t xdr_int64_t
-+#define xdr_u_longlong_t xdr_u_int64_t
-+#endif
-+
- #ifdef _POSIX_SOURCE
- #undef _POSIX_SOURCE
- #include <rpc/rpc.h>
+-#ifdef __APPLE__
++if defined(__APPLE__) || defined(__FreeBSD__)
+ #include <rpc/xdr.h>
+ 
+ inline bool_t xdr_long(XDR *__xdrs, long *__lp) {
