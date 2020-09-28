64ce1620cdf6b4326fc1dfa56df896ad04572439
Timur I. Bakeyev <timur@FreeBSD.org>
2020-09-28 00:16:08 +0000

    Actually allow to select from the varity of Samba ports.

--- Mk/Uses/samba.mk
+++ Mk/Uses/samba.mk
@@ -23,7 +23,7 @@ IGNORE=		USES=samba has invalid arguments: ${samba_ARGS:Nbuild:Nenv:Nlib:Nrun}
 
 SAMBAPORT=	net/samba${SAMBA_DEFAULT:S/.//}
 SAMBAINCLUDES=	${LOCALBASE}/include/samba4
-.if ${SAMBA_DEFAULT} == 4.10 || ${SAMBA_DEFAULT} == 4.11
+.if ${SAMBA_DEFAULT} == 4.10 || ${SAMBA_DEFAULT} == 4.11 || ${SAMBA_DEFAULT} == 4.12 || ${SAMBA_DEFAULT} == 4.13
 SAMBALIBS=	${LOCALBASE}/lib/samba4
 .else
 IGNORE=		Invalid version of samba: ${SAMBA_DEFAULT}
