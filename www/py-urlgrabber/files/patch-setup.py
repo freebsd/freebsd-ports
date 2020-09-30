ffb42a7be174daf68c2cf6e3d9d1fb4cddefa52d
Nicola Vitale <nivit@FreeBSD.org>
2020-09-30 07:41:14 +0000

    - Update to 4.0.0 - Remove dependency on Python 2.X (reported by [1]) - Use autoplist facility
    PR:	249799
    Submitted by:	swills  [1]

--- www/py-urlgrabber/files/patch-setup.py
+++ www/py-urlgrabber/files/patch-setup.py
@@ -1,20 +1,22 @@
---- setup.py.orig	2017-07-23 09:57:14 UTC
+--- setup.py.orig	2019-02-25 13:49:04 UTC
 +++ setup.py
-@@ -32,7 +32,6 @@ packages = ['urlgrabber']
+@@ -1,4 +1,5 @@
+ # urlgrabber distutils setup
++import os
+ import re as _re
+ import sys as _sys
+ 
+@@ -32,9 +33,12 @@ packages = ['urlgrabber']
  package_dir = {'urlgrabber':'urlgrabber'}
  scripts = ['scripts/urlgrabber']
  data_files = [
 -    ('share/doc/' + name + '-' + version, ['README','LICENSE', 'TODO', 'ChangeLog']),
      ('libexec', ['scripts/urlgrabber-ext-down']),
  ]
- options = { 'clean' : { 'all' : 1 } }
-@@ -59,5 +58,9 @@ if __name__ == '__main__':
-     #print '%-20s -> %s' % (k, config[k])
-     if k.startswith('_'): del config[k]
- 
-+  import os
-+  if os.environ.has_key('INSTALL_DOCS'):
++if (sys.version_info.major == 2 and os.environ.has_key('INSTALL_DOCS')) or \
++        (sys.version_info.major == 3 and os.environ.get('INSTALL_DOCS')):
 +    data_files.append(('%%DOCSDIR%%', ['README', 'TODO', 'ChangeLog']))
 +
-   from distutils.core import setup
-   setup(**config)
+ setup_requires = ['six']
+ install_requires = ['pycurl', 'six']
+ options = { 'clean' : { 'all' : 1 } }
