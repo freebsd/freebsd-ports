a330e0ebc7e1d7fae1bac4f0afa255626f64354b
Tobias C. Berner <tcberner@FreeBSD.org>
2020-10-04 06:50:47 +0000

    MFH: r551336
    devel/upnp: update to 1.14.0
    
    Update to newer release which contains a fix for CVE-2020-13848:
    	https://github.com/pupnp/pupnp/commit/c805c1de1141cb22f74c0d94dd5664bda37398e0
    
    Security:	CVE-2020-13848
    
    Approved by:	ports-secteam (joneum)

---
+++ devel/upnp/files/patch-upnp_src_genlib_net_http_httpreadwrite.c
@@ -0,0 +1,10 @@
+--- upnp/src/genlib/net/http/httpreadwrite.c.orig	2020-10-03 17:28:40 UTC
++++ upnp/src/genlib/net/http/httpreadwrite.c
+@@ -55,7 +55,6 @@
+ #include "uri.h"
+ #include "webserver.h"
+ 
+-#include <alloca.h>
+ #include <assert.h>
+ #include <stdarg.h>
+ #include <string.h>
