38d01d4f0b302a0558d76e8f01b8ecc6410f011e
Yuri Victorovich <yuri@FreeBSD.org>
2020-10-09 20:20:01 +0000

    science/nwchem: Update 7.0.0 -> 7.0.1
    Also update WWW.
    
    Reported by:	portscout

--- science/nwchem/files/patch-config_makefile.h
+++ science/nwchem/files/patch-config_makefile.h
@@ -1,6 +1,6 @@
---- config/makefile.h.orig	2019-11-23 16:35:56 UTC
+--- config/makefile.h.orig	2020-10-08 04:03:38 UTC
 +++ config/makefile.h
-@@ -988,9 +988,9 @@ ifeq ($(TARGET),MACX)
+@@ -1027,9 +1027,9 @@ ifeq ($(TARGET),MACX)
          FDEBUG = -O0 -g
          FOPTIMIZE  = -O2 -ffast-math -Wuninitialized 
          DEFINES  += -DGFORTRAN
@@ -12,7 +12,7 @@
          GNU_GE_4_6 = $(shell [ $(GNUMAJOR) -gt 4 -o \( $(GNUMAJOR) -eq 4 -a $(GNUMINOR) -ge 6 \) ] && echo true)
          GNU_GE_4_8 = $(shell [ $(GNUMAJOR) -gt 4 -o \( $(GNUMAJOR) -eq 4 -a $(GNUMINOR) -ge 8 \) ] && echo true)
          GNU_GE_6 = $(shell [ $(GNUMAJOR) -ge 6  ] && echo true)
-@@ -1156,9 +1156,9 @@ endif
+@@ -1195,9 +1195,9 @@ endif
  #
           FOPTIMIZE+= -funroll-all-loops -mtune=native 
           FVECTORIZE=-O3 -ffast-math -mtune=native -mfpmath=sse -msse3 -ftree-vectorize -ftree-vectorizer-verbose=1   -fprefetch-loop-arrays  -funroll-all-loops 
@@ -24,7 +24,7 @@
          GNU_GE_4_6 = $(shell [ $(GNUMAJOR) -gt 4 -o \( $(GNUMAJOR) -eq 4 -a $(GNUMINOR) -ge 6 \) ] && echo true)
          GNU_GE_4_8 = $(shell [ $(GNUMAJOR) -gt 4 -o \( $(GNUMAJOR) -eq 4 -a $(GNUMINOR) -ge 8 \) ] && echo true)
          GNU_GE_6 = $(shell [ $(GNUMAJOR) -ge 6  ] && echo true)
-@@ -1339,7 +1339,7 @@ ifeq ($(TARGET),$(findstring $(TARGET),LINUX CYGNUS CY
+@@ -1378,7 +1378,7 @@ ifeq ($(TARGET),$(findstring $(TARGET),LINUX CYGNUS CY
          FOPTIONS   = # -Wextra -Wunused  
          FOPTIMIZE  += -ffast-math -Wuninitialized
          DEFINES  += -DGFORTRAN
@@ -33,15 +33,3 @@
          ifdef GNUMAJOR
            GNUMINOR=$(shell $(FC) -dM -E - < /dev/null 2> /dev/null | egrep __VERS | cut -c24)
            GNU_GE_4_6 = $(shell [ $(GNUMAJOR) -gt 4 -o \( $(GNUMAJOR) -eq 4 -a $(GNUMINOR) -ge 6 \) ] && echo true)
-@@ -1768,9 +1768,9 @@ endif
-          CFLAGS_FORGA = -mcmodel=medium
-          FFLAGS_FORGA = -mcmodel=medium
-         else
--        GNUMAJOR=$(shell $(_FC) -dM -E - < /dev/null 2> /dev/null | grep __GNUC__ |cut -c18-)
-+        GNUMAJOR=$(shell $(FC) -dM -E - < /dev/null 2> /dev/null | grep __GNUC__ |cut -c18-)
-         ifdef GNUMAJOR
--        GNUMINOR=$(shell $(_FC) -dM -E - < /dev/null 2> /dev/null | egrep __GNUC_MINOR | cut -c24)
-+        GNUMINOR=$(shell $(FC) -dM -E - < /dev/null 2> /dev/null | egrep __GNUC_MINOR | cut -c24)
-         GNU_GE_4_6 = $(shell [ $(GNUMAJOR) -gt 4 -o \( $(GNUMAJOR) -eq 4 -a $(GNUMINOR) -ge 6 \) ] && echo true)
-         GNU_GE_4_8 = $(shell [ $(GNUMAJOR) -gt 4 -o \( $(GNUMAJOR) -eq 4 -a $(GNUMINOR) -ge 8 \) ] && echo true)
-         endif
