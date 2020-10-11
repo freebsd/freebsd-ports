ecdac088eaff358125fd99207360b9cb66e4913d
Rainer Hurling <rhurlin@FreeBSD.org>
2020-10-11 13:26:50 +0000

    math/saga: Update to 7.8.0
    Minor release with several bug fixes and some enhancements like[1]:
    - Added OpenCV version 4 support
    - Separate grid system for weather data in sim_hydrology/overland_flow
    - Reworked, free positioning for layout items (map, legend, titel, ...)
      in print layout of saga_gui
    - Added SAGA4QGIS, updated for inno-setup v6 in accessories/helper
    
    [1] https://sourceforge.net/p/saga-gis/wiki/Changelog%207.8.0/attachment/changelog_saga_7.8.0.txt
    
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D26737

--- math/saga/files/patch-src_tools_imagery_imagery__svm_svm__grids.h
+++ math/saga/files/patch-src_tools_imagery_imagery__svm_svm__grids.h
@@ -1,11 +1,11 @@
---- src/tools/imagery/imagery_svm/svm_grids.h.orig	2018-11-22 07:53:06 UTC
+--- src/tools/imagery/imagery_svm/svm_grids.h.orig	2020-08-07 13:46:39 UTC
 +++ src/tools/imagery/imagery_svm/svm_grids.h
-@@ -72,7 +72,7 @@
- #include "MLB_Interface.h"
+@@ -61,7 +61,7 @@
  
+ //---------------------------------------------------------
  #if defined(SYSTEM_SVM)
--#include <libsvm/svm.h>
-+#include <svm.h>
+-	#include <libsvm/svm.h>
++	#include <svm.h>
  #else
- #include "svm/svm.h"
+ 	#include "svm/svm.h"
  #endif
