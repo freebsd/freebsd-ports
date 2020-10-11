95a7a8b5baada80df3d2c8896ee05d5613aa1a53
Yuri Victorovich <yuri@FreeBSD.org>
2020-10-11 04:34:14 +0000

    multimedia/obs-streamfx: Update 0.8.3 -> 0.9.0
    Reported by:	portscout

---
+++ multimedia/obs-streamfx/files/patch-source_util_util-curl.hpp
@@ -0,0 +1,10 @@
+--- source/util/util-curl.hpp.orig	2020-10-11 04:20:03 UTC
++++ source/util/util-curl.hpp
+@@ -24,6 +24,7 @@
+ #include <functional>
+ #include <map>
+ #include <string>
++#include <vector>
+ 
+ extern "C" {
+ #ifndef NOMINMAX
