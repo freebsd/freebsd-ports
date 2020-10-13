dab61e0251e90601d1472090f577b2a9fdd91492
Koichiro Iwao <meta@FreeBSD.org>
2020-10-13 02:25:16 +0000

    databases/ruby-bdb: Fix build with upcoming databases/db18
    While here, return to pool due to more than 3 years of consecutive
    maintainer timeouts.
    
    PR:		248416
    Submitted by:	Yasuhiro KIMURA <yasu@utahime.org>
    Approved by:	maintainer timeout (> 2 months)

--- databases/ruby-bdb/files/patch-src-extconf.rb
+++ databases/ruby-bdb/files/patch-src-extconf.rb
@@ -1,6 +1,6 @@
---- src/extconf.rb.orig	2015-01-22 16:09:31 UTC
+--- src/extconf.rb.orig	2011-04-06 19:35:39 UTC
 +++ src/extconf.rb
-@@ -36,16 +36,16 @@
+@@ -36,16 +36,16 @@ end
  
  if unknown = enable_config("unknown")
     libs = if CONFIG.key?("LIBRUBYARG_STATIC")
@@ -21,3 +21,27 @@
  when /solaris2/
     $DLDFLAGS ||= ""
     $DLDFLAGS += " -R#{lib_dir}"
+@@ -63,10 +63,12 @@ end
+ if csv = with_config('db-version')
+    version = csv.split(',', -1)
+    version << '' if version.empty?
+-elsif m = lib_dir.match(%r{/db(?:([2-9])|([2-9])([0-9])|-([2-9]).([0-9]))(?:$|/)}) ||
+-          inc_dir.match(%r{/db(?:([2-9])|([2-9])([0-9])|-([2-9]).([0-9]))(?:$|/)})
++elsif m = lib_dir.match(%r{/db(?:([2-9])|([2-9])([0-9])|-([2-9]).([0-9]))|([1-9][0-9]+)(?:$|/)}) ||
++          inc_dir.match(%r{/db(?:([2-9])|([2-9])([0-9])|-([2-9]).([0-9]))(|([1-9][0-9]+)?:$|/)})
+    if m[1]
+       version = [m[1], '']
++   elsif m[6]
++      version = [m[6], '']
+    else
+       if m[2]
+          major, minor = m[2], m[3]
+@@ -94,7 +96,7 @@ catch(:done) do
+          end
+          next if with_ver.empty?
+          if !unique.is_a?(String) || unique.empty?
+-            m = with_ver.match(/^[^0-9]*([2-9])\.?([0-9]{0,3})/)
++            m = with_ver.match(/^[^0-9]*([1-9][0-9]*)\.?([0-9]{0,3})/)
+             major = m[1].to_i
+             minor = m[2].to_i
+             db_version = "db_version_" + (1000 * major + minor).to_s
