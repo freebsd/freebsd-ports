06fcbb3587b754a7acbfde9f2129980eba223b12
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-10-13 18:42:03 +0000

    Allow build with py-sqlparse 0.4.0
    - Update WWW
    - Bump PORTREVISION for package change

---
+++ databases/py-mycli/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-07-25 03:46:06 UTC
++++ setup.py
+@@ -21,7 +21,7 @@ install_requirements = [
+     'Pygments >= 1.6',
+     'prompt_toolkit>=3.0.0,<4.0.0',
+     'PyMySQL >= 0.9.2',
+-    'sqlparse>=0.3.0,<0.4.0',
++    'sqlparse>=0.3.0',
+     'configobj >= 5.0.5',
+     'cryptography >= 1.0.0',
+     'cli_helpers[styles] > 1.1.0',
