e3beab9016cc015477685d5d63c534d31518c6ab
Jason E. Hale <jhale@FreeBSD.org>
2020-10-13 22:26:54 +0000

    audio/kid3-*: Update to 3.8.4

--- audio/kid3-qt5/files/patch-src_app_kde_kdemainwindow.cpp
+++ audio/kid3-qt5/files/patch-src_app_kde_kdemainwindow.cpp
@@ -5,9 +5,9 @@ Shortcut for action  "reload" "Re&load" set with QAction::setShortcut()! Use KAc
 Shortcut for action  "filelist_rename" "&Rename" set with QAction::setShortcut()! Use KActionCollection::setDefaultShortcut(s) instead.
 Shortcut for action  "filelist_delete" "&Move to Trash" set with QAction::setShortcut()! Use KActionCollection::setDefaultShortcut(s) instead.
 
---- src/app/kde/kdemainwindow.cpp.orig	2020-05-09 12:12:37 UTC
+--- src/app/kde/kdemainwindow.cpp.orig	2020-09-25 07:02:19 UTC
 +++ src/app/kde/kdemainwindow.cpp
-@@ -189,7 +189,7 @@ void KdeMainWindow::initActions()
+@@ -190,7 +190,7 @@ void KdeMainWindow::initActions()
    action = new QAction(QIcon::fromTheme(QLatin1String("document-open")),
                         tr("O&pen Folder..."), this);
    action->setStatusTip(tr("Opens a folder"));
@@ -16,16 +16,16 @@ Shortcut for action  "filelist_delete" "&Move to Trash" set with QAction::setSho
    collection->addAction(QLatin1String("open_directory"), action);
    connect(action, &QAction::triggered,
            impl(), &BaseMainWindowImpl::slotFileOpenDirectory);
-@@ -197,7 +197,7 @@ void KdeMainWindow::initActions()
-   action = new QAction(QIcon::fromTheme(QLatin1String("view-refresh")),
-                        tr("Re&load"), this);
-   action->setStatusTip(tr("Reload folder"));
--  action->setShortcut(QKeySequence::Refresh);
-+  collection->setDefaultShortcut(action, QKeySequence::Refresh);
+@@ -203,7 +203,7 @@ void KdeMainWindow::initActions()
+   // Avoid this by assigning Qt::Key_F5 instead of QKeySequence::Refresh.
+   // The section "Standard Shortcuts" in the QKeySequence documentation lists
+   // F5 as a key for "Refresh" on all platforms.
+-  action->setShortcut(Qt::Key_F5);
++  collection->setDefaultShortcut(action, QKeySequence(Qt::Key_F5));
    collection->addAction(QLatin1String("reload"), action);
    connect(action, &QAction::triggered,
            impl(), &BaseMainWindowImpl::slotFileReload);
-@@ -415,13 +415,13 @@ void KdeMainWindow::initActions()
+@@ -421,7 +421,7 @@ void KdeMainWindow::initActions()
    collection->addAction(QLatin1String("filelist_focus"), action);
    connect(action, &QAction::triggered, form(), &Kid3Form::setFocusFileList);
    action = new QAction(tr("&Rename"), this);
@@ -33,11 +33,13 @@ Shortcut for action  "filelist_delete" "&Move to Trash" set with QAction::setSho
 +  collection->setDefaultShortcut(action, QKeySequence(Qt::Key_F2));
    action->setShortcutContext(Qt::WidgetShortcut);
    connect(action, &QAction::triggered, impl(), &BaseMainWindowImpl::renameFile);
-   collection->addAction(QLatin1String("filelist_rename"), action);
+   // This action is not made configurable because its shortcut F2 conflicts
+@@ -430,7 +430,7 @@ void KdeMainWindow::initActions()
+   // collection->addAction(QLatin1String("filelist_rename"), action);
    form()->getFileList()->setRenameAction(action);
    action = new QAction(tr("&Move to Trash"), this);
 -  action->setShortcut(QKeySequence::Delete);
 +  collection->setDefaultShortcut(action, QKeySequence::Delete);
    action->setShortcutContext(Qt::WidgetShortcut);
    connect(action, &QAction::triggered, impl(), &BaseMainWindowImpl::deleteFile);
-   collection->addAction(QLatin1String("filelist_delete"), action);
+   // collection->addAction(QLatin1String("filelist_delete"), action);
