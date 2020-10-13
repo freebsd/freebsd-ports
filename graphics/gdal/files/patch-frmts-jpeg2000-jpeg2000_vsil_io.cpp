90a1fdb8241fa5e415f4964a1668e9c3a16fe67e
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-10-13 18:41:43 +0000

    Update to 3.1.3
    Changes:	https://github.com/OSGeo/gdal/blob/release/3.1/gdal/NEWS
    PR:		250102
    Exp-run by:	antoine

--- graphics/gdal/files/patch-frmts-jpeg2000-jpeg2000_vsil_io.cpp
+++ graphics/gdal/files/patch-frmts-jpeg2000-jpeg2000_vsil_io.cpp
@@ -1,28 +1,11 @@
-Obtained from:	https://github.com/OSGeo/gdal/commit/e236eeaed1be45a4af457565085e3db1f2fc489f
-
---- frmts/jpeg2000/jpeg2000_vsil_io.cpp.orig	2020-05-04 09:21:31 UTC
+--- frmts/jpeg2000/jpeg2000_vsil_io.cpp.orig	2020-09-01 08:39:25 UTC
 +++ frmts/jpeg2000/jpeg2000_vsil_io.cpp
-@@ -94,13 +94,23 @@ typedef struct {
- * File stream object.
- \******************************************************************************/
- 
-+// PRIjas_seqent macro is defined since Jasper 2.0.17
-+
-+#ifndef PRIjas_seqent
- static int JPEG2000_VSIL_read(jas_stream_obj_t *obj, char *buf, int cnt)
-+#else
-+static int JPEG2000_VSIL_read(jas_stream_obj_t *obj, char *buf, unsigned cnt)
-+#endif
- {
-     jas_stream_VSIFL_t *fileobj = JAS_CAST(jas_stream_VSIFL_t *, obj);
-     return static_cast<int>(VSIFReadL(buf, 1, cnt, fileobj->fp));
- }
- 
-+#ifndef PRIjas_seqent
+@@ -109,7 +109,7 @@ static int JPEG2000_VSIL_read(jas_stream_obj_t *obj, c
+ #ifndef PRIjas_seqent
  static int JPEG2000_VSIL_write(jas_stream_obj_t *obj, char *buf, int cnt)
-+#else
+ #else
+-static int JPEG2000_VSIL_write(jas_stream_obj_t *obj, char *buf, unsigned int cnt)
 +static int JPEG2000_VSIL_write(jas_stream_obj_t *obj, const char *buf, unsigned int cnt)
-+#endif
+ #endif
  {
      jas_stream_VSIFL_t *fileobj = JAS_CAST(jas_stream_VSIFL_t *, obj);
-     return static_cast<int>(VSIFWriteL(buf, 1, cnt, fileobj->fp));
