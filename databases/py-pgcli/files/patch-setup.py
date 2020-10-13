a08c195e72b7c103a8a711b347458ebcb64fa24c
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-10-13 18:42:09 +0000

    Allow build with py-sqlparse 0.4.0
    - Update WWW
    - Bump PORTREVISION for package change

---
+++ databases/py-pgcli/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-04-10 21:58:56 UTC
++++ setup.py
+@@ -11,7 +11,7 @@ install_requirements = [
+     "Pygments >= 2.0",  # Pygments has to be Capitalcased. WTF?
+     "prompt_toolkit>=3.0.0,<4.0.0",
+     "psycopg2 >= 2.8",
+-    "sqlparse >=0.3.0,<0.4",
++    "sqlparse >=0.3.0",
+     "configobj >= 5.0.6",
+     "humanize >= 0.5.1",
+     "cli_helpers[styles] >= 1.2.0",
