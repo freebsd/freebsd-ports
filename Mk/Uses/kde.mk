80e1253bfff17c5251e341e1126bf31724f9c730
Tobias C. Berner <tcberner@FreeBSD.org>
2020-10-15 17:31:12 +0000

    Update KDE Frameworks to 5.75.0
    October 10, 2020. KDE today announces the release of KDE Frameworks 5.75.0.
    
    KDE Frameworks are over 70 addon libraries to Qt which provide a wide variety
    of commonly needed functionality in mature, peer reviewed and well tested
    libraries with friendly licensing terms. For an introduction see the KDE
    Frameworks web page.
    
    This release is part of a series of planned monthly releases making
    improvements available to developers in a quick and predictable manner.
    
    Full Announcement & Changelog:
    	https://kde.org/announcements/kde-frameworks-5.75.0
    
    PR:		250251
    Exp-run by:	antoine

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -58,7 +58,7 @@ KDE_PLASMA_VERSION?=		5.20.0
 KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
-KDE_FRAMEWORKS_VERSION?=	5.74.0
+KDE_FRAMEWORKS_VERSION?=	5.75.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 # Current KDE applications.
