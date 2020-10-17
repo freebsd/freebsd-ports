57dce831551b792d762bdab2dde8f18f7729643a
Danilo G. Baio <dbaio@FreeBSD.org>
2020-10-17 17:29:04 +0000

    MFH: r552582
    net-im/py-matrix-synapse: Update to 1.21.2, Fix security issue
    
    Changelog:	https://github.com/matrix-org/synapse/blob/v1.21.2/CHANGES.md
    
    PR:		249948
    Submitted by:	Sascha Biberhofer <ports@skyforge.at> (maintainer)
    Security:	5f39d80f-107c-11eb-8b47-641c67a117d8
    
    Approved by:	ports-secteam (joneum)

--- net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
+++ net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
@@ -1,11 +1,11 @@
---- synapse/python_dependencies.py.orig	2020-07-15 20:12:36 UTC
+--- synapse/python_dependencies.py.orig	2020-10-15 15:59:57 UTC
 +++ synapse/python_dependencies.py
-@@ -66,7 +66,7 @@ REQUIREMENTS = [
-     "pymacaroons>=0.13.0",
-     "msgpack>=0.5.2",
-     "phonenumbers>=8.2.0",
--    "prometheus_client>=0.0.18,<0.9.0",
-+    "prometheus_client>=0.0.18",
-     # we use attr.validators.deep_iterable, which arrived in 19.1.0
-     "attrs>=19.1.0",
-     "netaddr>=0.7.18",
+@@ -72,7 +72,7 @@ REQUIREMENTS = [
+     # prom-client has a history of breaking backwards compatibility between
+     # minor versions (https://github.com/prometheus/client_python/issues/317),
+     # so we also pin the minor version.
+-    "prometheus_client>=0.4.0,<0.9.0",
++    "prometheus_client>=0.4.0",
+     # we use attr.validators.deep_iterable, which arrived in 19.1.0 (Note:
+     # Fedora 31 only has 19.1, so if we want to upgrade we should wait until 33
+     # is out in November.)
