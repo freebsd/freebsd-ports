d6332b1700ced83d0f22e1f76f773c51212af3b1
Kirill Ponomarev <krion@FreeBSD.org>
2020-10-22 14:19:09 +0000

    MFH: r552978
    Update to 3002
    
    Changes: https://salt.tips/whats-new-in-salt-magnesium
    
    PR:		250519
    Approved by:	maintainer
    
    Approved by:	portmgr (blanket)

--- sysutils/py-salt/files/patch-salt_loader.py
+++ sysutils/py-salt/files/patch-salt_loader.py
@@ -1,13 +1,11 @@
---- salt/loader.py.orig	2020-06-18 07:20:08 UTC
+--- salt/loader.py.orig	2020-10-22 05:33:25 UTC
 +++ salt/loader.py
-@@ -1383,9 +1383,7 @@ class LazyLoader(salt.utils.lazy.LazyDict):
-                 try:
-                     pycache_files = [
-                         os.path.join("__pycache__", x)
--                        for x in sorted(
--                            os.listdir(os.path.join(mod_dir, "__pycache__"))
--                        )
-+                        for x in sorted(os.listdir(os.path.join(mod_dir, '__pycache__'))) if not x.endswith('opt-1.pyc') and not x.endswith('opt-2.pyc')
-                     ]
-                 except OSError:
-                     pass
+@@ -1468,7 +1468,7 @@ class LazyLoader(salt.utils.lazy.LazyDict):
+             try:
+                 pycache_files = [
+                     os.path.join("__pycache__", x)
+-                    for x in sorted(os.listdir(os.path.join(mod_dir, "__pycache__")))
++                    for x in sorted(os.listdir(os.path.join(mod_dir, '__pycache__'))) if not x.endswith('opt-1.pyc') and not x.endswith('opt-2.pyc')
+                 ]
+             except OSError:
+                 pass
