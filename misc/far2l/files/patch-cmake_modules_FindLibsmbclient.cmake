051bd96f0cf527e9a4477c5337318005b3f2f05d
Alexey Dokuchaev <danfe@FreeBSD.org>
2020-10-22 09:07:31 +0000

    - Update to the latest snapshot (still alpha quality) - Now compatible with wxWidgets version 3.0 and 3.1 - Reformat port description for somewhat better looks

--- misc/far2l/files/patch-cmake_modules_FindLibsmbclient.cmake
+++ misc/far2l/files/patch-cmake_modules_FindLibsmbclient.cmake
@@ -1,6 +1,6 @@
---- cmake/modules/FindLibsmbclient.cmake.orig	2020-06-22 23:39:14 UTC
+--- cmake/modules/FindLibsmbclient.cmake.orig	2020-10-21 22:24:08 UTC
 +++ cmake/modules/FindLibsmbclient.cmake
-@@ -21,23 +21,17 @@ else (LIBSMBCLIENT_LIBRARIES AND LIBSMBCLIENT_INCLUDE_
+@@ -21,11 +21,6 @@ else (LIBSMBCLIENT_LIBRARIES AND LIBSMBCLIENT_INCLUDE_
    find_path(LIBSMBCLIENT_INCLUDE_DIR
      NAMES
        libsmbclient.h
@@ -11,20 +11,4 @@
 -      /sw/include
      PATH_SUFFIXES
        samba-4.0
-+      samba4
-   )
- 
-   find_library(SMBCLIENT_LIBRARY
-     NAMES
-       smbclient
--    PATHS
--      /usr/lib
--      /usr/local/lib
--      /opt/local/lib
--      /sw/lib
-+    PATH_SUFFIXES
-+      samba-4.0
-+      samba4
-   )
- 
-   if (SMBCLIENT_LIBRARY)
+       samba4
