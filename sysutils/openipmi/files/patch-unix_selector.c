72a95d640f476fa0d80354b1eb464c8ce09948ff
Vinícius Zavam <egypcio@FreeBSD.org>
2020-10-24 11:32:30 +0000

    sysutils/openipmi: update 2.0.28 to 2.0.29
      * sourceforge.net/p/openipmi/code/commit_browser
        - 2.0.28 == 1666c8d5a4ee8874
        - 2.0.29 == 7a1d1ce556ce5d3d
    
      files/patch-unix_selector.c:
        - upstream reworked previous patch changes, we removed those changes;
        - patch was updated to handle the non-existing 'EBADFD' errno;
        - 'EBADFD' is Solaris/Linux specific, as per "bsm/audit_errno.h"

--- sysutils/openipmi/files/patch-unix_selector.c
+++ sysutils/openipmi/files/patch-unix_selector.c
@@ -1,16 +1,14 @@
---- unix/selector.c.orig	2018-12-28 13:36:00 UTC
+--- unix/selector.c.orig	2020-10-24 10:59:38 UTC
 +++ unix/selector.c
-@@ -459,7 +459,13 @@ i_sel_clear_fd_handler(struct selector_s
- 	fdc->state = NULL;
- 
- 	sel_update_epoll(sel, fd, EPOLL_CTL_DEL, 0);
-+	/* We do not set HAVE_EPOLL_PWAIT, so 'fd_control_s' will never
-+	 * have 'saved_events', and sel_update_epoll() will always return 1.
-+	 * 
-+	 * See lines 340 (HAVE_EPOLL_PWAIT), and 369 (no HAVE_EPOLL_PWAIT).
-+	 *
- 	fdc->saved_events = 0;
+@@ -1030,7 +1030,10 @@ process_fds(struct selector_s	    *sel,
+ 		  &tmp_except_set,
+ 		  &ts, &sigmask);
+     if (err < 0) {
+-	if (errno == EBADF || errno == EBADFD)
++	/* We do not have EBADFD, as it is Solaris and Linux specific;
++	 * if (errno == EBADF || errno == EBADFD)
 +	 */
-     }
- 
-     init_fd(fdc);
++	if (errno == EBADF)
+ 	    /* We raced, just retry it. */
+ 	    goto retry;
+ 	goto out;
