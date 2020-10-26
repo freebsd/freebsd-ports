5a6fdde102199772752251d5e158fa3f43ac3b91
Yuri Victorovich <yuri@FreeBSD.org>
2020-10-26 21:38:05 +0000

    misc/py-xgboost: Update 1.2.0 -> 1.2.1
    PR:		247408
    MFH:		2020Q4 (fixes py-xgboost failure, see bug#247408)

--- misc/py-xgboost/files/patch-xgboost_libpath.py
+++ misc/py-xgboost/files/patch-xgboost_libpath.py
@@ -1,10 +1,18 @@
---- xgboost/libpath.py.orig	2020-05-25 18:09:22 UTC
+--- xgboost/libpath.py.orig	2020-10-12 22:10:16 UTC
 +++ xgboost/libpath.py
-@@ -20,6 +20,8 @@ def find_lib_path():
+@@ -4,6 +4,7 @@
+ import os
+ import platform
+ import sys
++import sysconfig # from https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=247408#c0
+ 
+ 
+ class XGBoostLibraryNotFound(Exception):
+@@ -20,6 +21,8 @@ def find_lib_path():
      """
      curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
      dll_path = [
-+        os.getenv('FREEBSD_LIBRARY_PATH'),
++	sysconfig.get_config_var('LIBDIR'), # from https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=247408#c0
 +	os.path.join(sys.prefix, 'xgboost'),
          # normal, after installation `lib` is copied into Python package tree.
          os.path.join(curr_path, 'lib'),
