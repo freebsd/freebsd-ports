3759080d14b0a1ce2394c3a9a1f1bcd407bf3838
Alexey Dokuchaev <danfe@FreeBSD.org>
2020-10-27 06:27:56 +0000

    - Update to version 1.0.2 (project had moved to GitHub) - Give a better COMMENT (gets propagated to the .desktop file) - GC no longer needed -fno-common patches and fix the logic   for the ``struct stereo_volume vol'' definition and symbol - Install more complete set of documentation files, clean up   Makefile, remove redundant wording from the port description

--- audio/gvolwheel/files/patch-src_oss.c
+++ audio/gvolwheel/files/patch-src_oss.c
@@ -1,6 +1,6 @@
---- src/oss.c.orig	2012-04-07 21:18:50 UTC
+--- src/oss.c.orig	2020-08-01 09:32:59 UTC
 +++ src/oss.c
-@@ -17,13 +17,15 @@
+@@ -17,11 +17,14 @@
   * along with this program. If not, see <http://www.gnu.org/licenses/>.
   */
  
@@ -11,35 +11,8 @@
  #include <sys/ioctl.h>
  
  #include "oss.h"
++
++struct stereo_volume vol;
  
- static char *default_device = "/dev/mixer"; 
-+struct stereo_volume oss_vol;
+ static char *default_device = "/dev/mixer";
  
- int mixer_fd;
- 
-@@ -38,17 +40,17 @@ int vol_backend_init (char *device)
- int vol_backend_get(int mixer)
- {
- 	if (mixer == 0)
--		ioctl(mixer_fd, MIXER_READ(SOUND_MIXER_VOLUME), &vol);
-+		ioctl(mixer_fd, MIXER_READ(SOUND_MIXER_VOLUME), &oss_vol);
- 	else if (mixer == 1)	
--		ioctl(mixer_fd, MIXER_READ(SOUND_MIXER_PCM), &vol);
--	return(vol.left);
-+		ioctl(mixer_fd, MIXER_READ(SOUND_MIXER_PCM), &oss_vol);
-+	return(oss_vol.left);
- }
- 
- void vol_backend_set(int mixer, int value)
- {
--	vol.left=vol.right = value;
-+	oss_vol.left=oss_vol.right = value;
- 	if (mixer == 0)	
--		ioctl(mixer_fd, MIXER_WRITE(SOUND_MIXER_VOLUME), &vol);
-+		ioctl(mixer_fd, MIXER_WRITE(SOUND_MIXER_VOLUME), &oss_vol);
- 	else if (mixer == 1)	
--		ioctl(mixer_fd, MIXER_WRITE(SOUND_MIXER_PCM), &vol);
--}
-\ No newline at end of file
-+		ioctl(mixer_fd, MIXER_WRITE(SOUND_MIXER_PCM), &oss_vol);
-+}
--- audio/gvolwheel/files/patch-src_oss.h
+++ audio/gvolwheel/files/patch-src_oss.h
@@ -1,14 +1,11 @@
---- src/oss.h.orig	2020-08-29 07:49:49 UTC
+--- src/oss.h.orig	2020-08-01 09:32:59 UTC
 +++ src/oss.h
-@@ -23,8 +23,8 @@ struct stereo_volume
+@@ -23,7 +23,7 @@ struct stereo_volume
  	unsigned char right;
  };
  
 -struct stereo_volume vol;
-+extern struct stereo_volume oss_vol;
++extern struct stereo_volume vol;
  
  int vol_backend_init(char *device);
  void vol_backend_set(int mixer, int value);
--int vol_backend_get(int mixer);
-\ No newline at end of file
-+int vol_backend_get(int mixer);
