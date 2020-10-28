d6b509bcaea39f09b92946871f0fb4cd9d4b17ee
Mikael Urankar <mikael@FreeBSD.org>
2020-10-28 12:49:16 +0000

    www/deno: Update to 1.5.0
    Changelog:
      https://github.com/denoland/deno/releases/tag/v1.5.0

--- www/deno/files/patch-cargo-crates_rusty__v8_build_linux_unbundle_replace__gn__files.py
+++ www/deno/files/patch-cargo-crates_rusty__v8_build_linux_unbundle_replace__gn__files.py
@@ -1,5 +1,5 @@
---- cargo-crates/rusty_v8-0.11.0/build/linux/unbundle/replace_gn_files.py.orig	2020-06-26 16:27:54 UTC
-+++ cargo-crates/rusty_v8-0.11.0/build/linux/unbundle/replace_gn_files.py
+--- cargo-crates/rusty_v8-0.12.0/build/linux/unbundle/replace_gn_files.py.orig	2020-06-26 16:27:54 UTC
++++ cargo-crates/rusty_v8-0.12.0/build/linux/unbundle/replace_gn_files.py
 @@ -27,6 +27,7 @@ REPLACEMENTS = {
    'libevent': 'base/third_party/libevent/BUILD.gn',
    'libjpeg': 'third_party/libjpeg.gni',
