9981c7141c5148e26674ca3394f07ba2b3a36b56
Stefan Eßer <se@FreeBSD.org>
2020-11-01 09:33:28 +0000

    Pass _PATH_LOCALBASE correctly quoted to the build

--- deskutils/calendar/files/patch-pathnames.h
+++ deskutils/calendar/files/patch-pathnames.h
@@ -1,12 +1,8 @@
 --- pathnames.h.orig	2020-10-18 03:01:26 UTC
 +++ pathnames.h
-@@ -34,4 +34,9 @@
+@@ -34,4 +34,5 @@
  
  #include <paths.h>
  
-+#ifndef	_PATH_LOCALBASE
-+#define	_PATH_LOCALBASE	"/usr/local"
-+#endif
-+
  #define	_PATH_INCLUDE	"/usr/share/calendar"
 +#define	_PATH_INCLUDE_LOCAL	_PATH_LOCALBASE "/share/calendar"
