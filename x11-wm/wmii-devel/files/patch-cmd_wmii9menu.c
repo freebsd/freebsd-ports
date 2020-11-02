16b2a474d112633745ed751b208e0d38e05106e6
Rodrigo Osorio <rodrigo@FreeBSD.org>
2020-11-02 14:39:51 +0000

    Fix whith -fno-common in FreeBSD 13
    Lastest version of llvm introduced in
    FreeBSD 13 has the flag -fno-common
    enabled by default. Fix the issue.

---
+++ x11-wm/wmii-devel/files/patch-cmd_wmii9menu.c
@@ -0,0 +1,10 @@
+--- cmd/wmii9menu.c.orig	2020-10-18 16:18:05 UTC
++++ cmd/wmii9menu.c
+@@ -35,6 +35,7 @@
+  * Heavily modified by Kris Maglione for use with wmii.
+  */
+ 
++#define EXTERN
+ #define IXP_NO_P9_
+ #define IXP_P9_STRUCTS
+ #include <fmt.h>
