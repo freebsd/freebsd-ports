7b30849d66a6affc4a39cda75bd381d1ded5baef
Yuri Victorovich <yuri@FreeBSD.org>
2020-11-03 19:43:22 +0000

    devel/py-murmurhash: Update 1.0.2 -> 1.0.3
    Reported by:	portscout

--- devel/py-murmurhash/files/patch-setup.py
+++ devel/py-murmurhash/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2019-06-16 03:40:21 UTC
+--- setup.py.orig	2020-10-29 12:35:41 UTC
 +++ setup.py
-@@ -126,7 +126,7 @@ def setup_package():
-             url=about['__uri__'],
-             license=about['__license__'],
+@@ -137,7 +137,7 @@ def setup_package():
+             url=about["__uri__"],
+             license=about["__license__"],
              ext_modules=ext_modules,
--            setup_requires=['wheel>=0.32.0,<0.33.0'],
+-            setup_requires=["wheel>=0.32.0,<0.33.0"],
 +            setup_requires=[],
              classifiers=[
-                 'Development Status :: 5 - Production/Stable',
-                 'Environment :: Console',
+                 "Development Status :: 5 - Production/Stable",
+                 "Environment :: Console",
