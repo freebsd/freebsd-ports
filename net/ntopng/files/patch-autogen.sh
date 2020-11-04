aee305e1ad233f4ec290adb4e565484604dc34c2
Guido Falsi <madpilot@FreeBSD.org>
2020-11-04 08:51:34 +0000

    - Update nDPI to latest upstream stable snapshot - Update ntopng to latest upsstream snapshot of new 4.2 stable   version

--- net/ntopng/files/patch-autogen.sh
+++ net/ntopng/files/patch-autogen.sh
@@ -1,6 +1,38 @@
---- autogen.sh.orig	2018-06-04 09:56:33 UTC
+--- autogen.sh.orig	2020-11-02 21:27:19 UTC
 +++ autogen.sh
-@@ -38,8 +38,3 @@ cat configure.seed | sed \
+@@ -8,31 +8,6 @@ LIBTOOLIZE=$(command -v libtoolize)
+ AUTORECONF=$(command -v autoreconf)
+ PKG_CONFIG=$(command -v pkg-config)
+ 
+-if test -z $AUTOCONF; then
+-    echo "autoconf is missing: please install it and try again"
+-    exit
+-fi
+-
+-if test -z $AUTOMAKE; then
+-    echo "automake is missing: please install it and try again"
+-    exit
+-fi
+-
+-if test -z $LIBTOOL && test -z $LIBTOOLIZE ; then
+-    echo "libtool and libtoolize is missing: please install it and try again"
+-    exit
+-fi
+-
+-if test -z $AUTORECONF; then
+-    echo "autoreconf is missing: please install it and try again"
+-    exit
+-fi
+-
+-if test -z $PKG_CONFIG; then
+-    echo "pkg-config is missing: please install it (apt-get install pkg-config) and try again"
+-    exit
+-fi
+-
+ ##########################################
+ 
+ TODAY=`date +%y%m%d`
+@@ -73,8 +48,3 @@ cat configure.seed | sed \
      > configure.ac
  
  rm -f config.h config.h.in *~ #*
