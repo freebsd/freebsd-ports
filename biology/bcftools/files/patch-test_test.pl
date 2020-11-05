b0ab3ea009b66fa94cdce2b7178313c21ad621e5
Jason W. Bacon <jwb@FreeBSD.org>
2020-11-05 20:34:03 +0000

    biology/bcftools: Upgrade to 1.11
    Numerous new features, bug fixes and performance enhancements
    https://github.com/samtools/bcftools/releases/tag/1.11
    
    Reported by:    portscout

--- biology/bcftools/files/patch-test_test.pl
+++ biology/bcftools/files/patch-test_test.pl
@@ -1,24 +1,6 @@
---- test/test.pl.orig	2020-07-21 20:49:56 UTC
+--- test/test.pl.orig	2020-10-18 13:36:42 UTC
 +++ test/test.pl
-@@ -610,7 +610,7 @@ sub _cmd
-     else
-     {
-         # child
--        exec('/bin/bash', '-o','pipefail','-c', $cmd) or error("Cannot execute the command [/bin/sh -o pipefail -c $cmd]: $!");
-+        exec('bash', '-o','pipefail','-c', $cmd) or error("Cannot execute the command [/usr/local/bin/sh -o pipefail -c $cmd]: $!");
-     }
-     return ($? >> 8, join('',@out));
- }
-@@ -622,7 +622,7 @@ sub _cmd3
-     my $pid = fork();
-     if ( !$pid )
-     {
--        exec('/bin/bash', '-o','pipefail','-c', "($cmd) 2>$tmp.e >$tmp.o");
-+        exec('bash', '-o','pipefail','-c', "($cmd) 2>$tmp.e >$tmp.o");
-     }
-     waitpid($pid,0);
- 
-@@ -1063,7 +1063,7 @@ sub test_usage
+@@ -1236,7 +1236,7 @@ sub test_usage
      my $command = $args{cmd};
      my $commandpath = $$opts{bin}."/".$command;
      my ($ret,$out) = _cmd("$commandpath $args{redirection} 2>&1");
@@ -27,7 +9,7 @@
  
      my @sections = ($out =~ m/(^[A-Za-z]+.*?)(?:(?=^[A-Za-z]+:)|\z)/msg);
  
-@@ -1118,7 +1118,7 @@ sub test_usage_subcommand
+@@ -1291,7 +1291,7 @@ sub test_usage_subcommand
      my $subcommand = $args{subcmd};
      my $commandpath = $$opts{bin}."/".$command;
      my ($ret,$out) = _cmd("$commandpath $subcommand $args{redirection} 2>&1");
