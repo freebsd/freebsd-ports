f599ab18a98cec0fd0e45c0d6f2b42b43f13dbd8
Yuri Victorovich <yuri@FreeBSD.org>
2020-11-05 02:02:36 +0000

    math/onednn: Update 1.6.5 -> 1.7
    Reported by:	portscout

--- math/onednn/files/patch-cmake_platform.cmake
+++ math/onednn/files/patch-cmake_platform.cmake
@@ -1,6 +1,6 @@
---- cmake/platform.cmake.orig	2020-08-06 03:36:08 UTC
+--- cmake/platform.cmake.orig	2020-10-28 05:50:44 UTC
 +++ cmake/platform.cmake
-@@ -104,16 +104,16 @@ elseif(UNIX OR MINGW)
+@@ -104,22 +104,22 @@ elseif(UNIX OR MINGW)
          if(DNNL_TARGET_ARCH STREQUAL "AARCH64")
               set(DEF_ARCH_OPT_FLAGS "-O3")
               # For native compilation tune for the host processor
@@ -14,27 +14,22 @@
 -             if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
 +             if (FALSE AND CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
                   append(DEF_ARCH_OPT_FLAGS "-mcpu=native")
+              endif()
+         elseif(DNNL_TARGET_ARCH STREQUAL "S390X")
+              set(DEF_ARCH_OPT_FLAGS "-O3")
+              # For native compilation tune for the host processor
+-             if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
++             if (FALSE AND CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
+                  append(DEF_ARCH_OPT_FLAGS "-march=native")
               endif()
 -        elseif(DNNL_TARGET_ARCH STREQUAL "X64")
 +        elseif(FALSE AND DNNL_TARGET_ARCH STREQUAL "X64")
               set(DEF_ARCH_OPT_FLAGS "-msse4.1")
          endif()
          # Clang cannot vectorize some loops with #pragma omp simd and gets
-@@ -162,17 +162,17 @@ elseif(UNIX OR MINGW)
-         if(DNNL_TARGET_ARCH STREQUAL "AARCH64")
-              set(DEF_ARCH_OPT_FLAGS "-O3")
-              # For native compilation tune for the host processor
--             if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
-+             if (FALSE AND CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
-                  append(DEF_ARCH_OPT_FLAGS "-mcpu=native")
-              endif()
-         elseif(DNNL_TARGET_ARCH STREQUAL "PPC64")
-              set(DEF_ARCH_OPT_FLAGS "-O3")
-              # In GCC, -ftree-vectorize is turned on under -O3 since 2007.
-              # For native compilation tune for the host processor
--             if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
-+             if (FALSE AND CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
-                  append(DEF_ARCH_OPT_FLAGS "-mcpu=native")
+@@ -203,7 +203,7 @@ elseif(UNIX OR MINGW)
+              if (CMAKE_SYSTEM_PROCESSOR STREQUAL CMAKE_HOST_SYSTEM_PROCESSOR)
+                  append(DEF_ARCH_OPT_FLAGS "-march=native")
               endif()
 -        elseif(DNNL_TARGET_ARCH STREQUAL "X64")
 +        elseif(FALSE AND DNNL_TARGET_ARCH STREQUAL "X64")
