83998103e636505e27e4a9004ab8c903c81f1cb0
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-11-06 11:25:53 +0000

    Fix build with PHP 8.0

--- graphics/php-geos/files/patch-geos.c
+++ graphics/php-geos/files/patch-geos.c
@@ -1,6 +1,20 @@
 --- geos.c.orig	2016-12-24 10:53:24 UTC
 +++ geos.c
-@@ -863,7 +863,7 @@ PHP_METHOD(Geometry, buffer)
+@@ -33,6 +33,13 @@
+ /* Own stuff */
+ #include "php_geos.h"
+ 
++#if PHP_MAJOR_VERSION >= 8
++#define TSRMLS_C
++#define TSRMLS_CC
++#define TSRMLS_DC
++#define TSRMLS_FETCH()
++#endif
++
+ static ZEND_DECLARE_MODULE_GLOBALS(geos);
+ static PHP_GINIT_FUNCTION(geos);
+ 
+@@ -863,7 +870,7 @@ PHP_METHOD(Geometry, buffer)
      GEOS_PHP_ZVAL data;
      HashTable *style;
      zend_string *key;
@@ -9,7 +23,7 @@
  
      this = (GEOSGeometry*)getRelay(getThis(), Geometry_ce_ptr);
  
-@@ -956,7 +956,7 @@ PHP_METHOD(Geometry, offsetCurve)
+@@ -956,7 +963,7 @@ PHP_METHOD(Geometry, offsetCurve)
      GEOS_PHP_ZVAL data;
      HashTable *style;
      zend_string *key;
