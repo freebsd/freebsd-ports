74e85aadbd630158478f70d8348e50800bfa8fae
Romain Tartière <romain@FreeBSD.org>
2020-11-06 22:10:34 +0000

    Update to 2.0.0
    With hat:	puppet

--- sysutils/rubygem-puppetserver-ca/files/patch-lib_puppetserver_ca_config_puppet.rb
+++ sysutils/rubygem-puppetserver-ca/files/patch-lib_puppetserver_ca_config_puppet.rb
@@ -1,20 +1,11 @@
---- lib/puppetserver/ca/config/puppet.rb.orig	2018-09-28 10:36:20 UTC
+--- lib/puppetserver/ca/config/puppet.rb.orig	2020-11-06 21:46:06 UTC
 +++ lib/puppetserver/ca/config/puppet.rb
-@@ -48,7 +48,7 @@ module Puppetserver
-         def user_specific_conf_dir
-           @user_specific_conf_dir ||=
-             if Puppetserver::Ca::Utils::Config.running_as_root?
--              '/etc/puppetlabs/puppet'
-+              '%%PREFIX%%/etc/puppet'
-             else
-               "#{ENV['HOME']}/.puppetlabs/etc/puppet"
-             end
-@@ -100,7 +100,7 @@ module Puppetserver
+@@ -97,7 +97,7 @@ module Puppetserver
            # defaults below
            base_defaults = [
-             [:confdir, user_specific_conf_dir],
+             [:confdir, user_specific_puppet_confdir],
 -            [:ssldir,'$confdir/ssl'],
 +            [:ssldir,'/var/puppet/ssl'],
-             [:cadir, '$ssldir/ca'],
              [:certdir, '$ssldir/certs'],
              [:certname, default_certname],
+             [:server, 'puppet'],
