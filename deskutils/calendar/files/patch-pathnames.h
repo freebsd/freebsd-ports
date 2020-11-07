1a3491f23b6647effb6b448d9a2276b57923430e
Stefan Eßer <se@FreeBSD.org>
2020-11-07 11:41:24 +0000

    Fetch from my repository to simplify port
    Instead of fetching from a repository that was created based on a version
    that did not include the pre-processor enhancements applied to -CURRENT
    and patching to include them, fetch from a repository that has a copy of
    the latest sources.
    
    This change allows to remove the large number of complex patch files from
    the port and will simplify further updates.
    
    It is planned to make the port fetch from an official FreeBSD repository
    as soon as it is available with up-to-date sources from -CURRENT.
    
    No port revision bump since this is only a change to the build process and
    not the generated package.

--- deskutils/calendar/files/patch-pathnames.h
+++
@@ -1,7 +0,0 @@
---- pathnames.h.orig	2020-10-18 03:01:26 UTC
-+++ pathnames.h
-@@ -35,3 +35,4 @@
- #include <paths.h>
- 
- #define	_PATH_INCLUDE	"/usr/share/calendar"
-+#define	_PATH_INCLUDE_LOCAL	_PATH_LOCALBASE "/share/calendar"
