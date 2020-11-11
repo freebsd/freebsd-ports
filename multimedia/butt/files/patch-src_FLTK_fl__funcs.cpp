5468b4d6b0bd14c72b8500ac2f65162af7c3164b
Kurt Jaeger <pi@FreeBSD.org>
2020-11-11 06:14:11 +0000

    multimedia/butt: update 0.1.22 -> 0.1.25
    PR:		250993
    Submitted by:	Per Gunnarsson <per.gunnarsson@yandex.com>
    Relnotes:	https://danielnoethen.de/butt/Changelog.html

--- multimedia/butt/files/patch-src_FLTK_fl__funcs.cpp
+++ multimedia/butt/files/patch-src_FLTK_fl__funcs.cpp
@@ -1,6 +1,6 @@
---- src/FLTK/fl_funcs.cpp.orig	2020-07-26 17:47:54 UTC
-+++ src/FLTK/fl_funcs.cpp
-@@ -147,7 +147,7 @@ void fill_cfg_widgets(void)
+--- src/FLTK/fl_funcs.cpp.orig	2020-11-09 19:05:50.529619000 +0100
++++ src/FLTK/fl_funcs.cpp	2020-11-09 19:14:30.905597000 +0100
+@@ -155,7 +155,7 @@
      fl_g->choice_cfg_app->add("VOX");
      fl_g->check_cfg_use_app->value(cfg.main.app_update);
      fl_g->choice_cfg_app->value(cfg.main.app_update_service);
