47b1e9c0c27eb7f248e2e146ac5996222b3731d8
Mathieu Arnold <mat@FreeBSD.org>
2020-11-11 13:30:03 +0000

    Ignore grep return value.
    Most of the time, we use grep in a test, where its return value is
    important.  In this instance, it is only used for filtering, and we do
    not care about its return value, so ignore it.
    
    PR:             250723
    Exp-run by:     antoine
    Differential Revision:  https://reviews.freebsd.org/D27007

--- Mk/Scripts/create-manifest.sh
+++ Mk/Scripts/create-manifest.sh
@@ -63,7 +63,8 @@ EOT
 
 # Then the key/values sections
 echo "deps: { "
-eval ${dp_ACTUAL_PACKAGE_DEPENDS} | grep -v -E ${dp_PKG_IGNORE_DEPENDS} | sort -u
+# Ignore grep's return value.
+eval ${dp_ACTUAL_PACKAGE_DEPENDS} | { grep -v -E ${dp_PKG_IGNORE_DEPENDS} || :; } | sort -u
 echo "}"
 
 echo "options: {"
