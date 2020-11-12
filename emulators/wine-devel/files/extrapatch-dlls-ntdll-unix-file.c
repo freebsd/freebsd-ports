658e5248dd6a2664a9cf6ddc72978945e0d30707
Gerald Pfeifer <gerald@FreeBSD.org>
2020-11-12 13:31:01 +0000

    Update to Wine 5.21.  This includes the following changes:  - GDI32 library converted to PE.  - More fixes for windowless RichEdit.  - A number of timezone updates.  - Various bug fixes.
    files/extrapatch-server-file.c that I added to work around upstream
    breakage with the Wine Staging patchset is not necessary any longer,
    nor is half of files/extrapatch-dlls-ntdll-unix-file.c.

--- emulators/wine-devel/files/extrapatch-dlls-ntdll-unix-file.c
+++ emulators/wine-devel/files/extrapatch-dlls-ntdll-unix-file.c
@@ -1,13 +1,9 @@
---- dlls/ntdll/unix/file.c.orig	2020-10-24 12:22:03.186448000 +0000
-+++ dlls/ntdll/unix/file.c	2020-10-24 13:14:00.020964000 +0000
-@@ -390,6 +390,22 @@
- #define XATTR_USER_PREFIX "user."
+--- dlls/ntdll/unix/file.c.orig	2020-11-07 09:33:27.083812000 +0000
++++ dlls/ntdll/unix/file.c	2020-11-07 09:53:55.641522000 +0000
+@@ -394,6 +394,18 @@
+ #define XATTR_USER_PREFIX_LEN (sizeof(XATTR_USER_PREFIX) - 1)
  #endif
  
-+#ifndef XATTR_USER_PREFIX_LEN
-+#define XATTR_USER_PREFIX_LEN (sizeof(XATTR_USER_PREFIX) - 1)
-+#endif
-+
 +#ifdef HAVE_SYS_EXTATTR_H
 +static inline int xattr_valid_namespace( const char *name )
 +{
