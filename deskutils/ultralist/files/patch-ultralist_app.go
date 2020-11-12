70e28c79e71a97b1275babf7ceea669693c73419
Fernando Apesteguía <fernape@FreeBSD.org>
2020-11-12 14:22:55 +0000

    MFH: r554964
    deskutils/ultralist: Update to 1.6.3
    
    ChangeLog: https://github.com/ultralist/ultralist/releases/1.6.3
    
    PR:	251070
    Submitted by:	vulcan@wired.sh (maintainer)
    
    Approved by:	ports-secteam (blanket, bugfix release)

---
+++ deskutils/ultralist/files/patch-ultralist_app.go
@@ -0,0 +1,11 @@
+--- ultralist/app.go.orig	2020-11-12 12:22:43 UTC
++++ ultralist/app.go
+@@ -14,7 +14,7 @@ import (
+ 
+ // Current version of ultralist.
+ const (
+-	VERSION     string = "1.6.2"
++	VERSION     string = "1.6.3"
+ 	DATE_FORMAT string = "2006-01-02"
+ )
+ 
