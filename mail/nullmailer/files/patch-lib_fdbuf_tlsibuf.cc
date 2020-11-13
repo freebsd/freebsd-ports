4d5c0461dcd02fec3bc09d8fe8d5ae9537c5d484
Fernando Apesteguía <fernape@FreeBSD.org>
2020-11-13 07:12:18 +0000

    mail/nullmailer: update to 2.2
    ChangeLog: http://untroubled.org/nullmailer/ChangeLog
    
    PR:	251074
    Submitted by:	uffe@uffe.org

--- mail/nullmailer/files/patch-lib_fdbuf_tlsibuf.cc
+++ mail/nullmailer/files/patch-lib_fdbuf_tlsibuf.cc
@@ -1,14 +1,16 @@
---- lib/fdbuf/tlsibuf.cc.orig
+--- lib/fdbuf/tlsibuf.cc.orig	2018-10-12 20:49:30 UTC
 +++ lib/fdbuf/tlsibuf.cc
-@@ -27,5 +27,10 @@
+@@ -27,7 +27,12 @@ tlsibuf::tlsibuf(gnutls_session_t s, unsigned bufsz)
  
  ssize_t tlsibuf::_read(char* buf, ssize_t len)
  {
--  return gnutls_record_recv(session, buf, len);
+-  int rc;
 +  ssize_t rc;
 +  do
 +  {
 +    rc = gnutls_record_recv(session, buf, len);
 +  } while (rc == GNUTLS_E_AGAIN || rc == GNUTLS_E_INTERRUPTED);
 +  return rc;
- }
+   do
+   {
+     rc = gnutls_record_recv(session, buf, len);
--- mail/nullmailer/files/patch-lib_fdbuf_tlsobuf.cc
+++ mail/nullmailer/files/patch-lib_fdbuf_tlsobuf.cc
@@ -1,6 +1,6 @@
---- lib/fdbuf/tlsobuf.cc.orig
+--- lib/fdbuf/tlsobuf.cc.orig	2018-10-12 20:49:30 UTC
 +++ lib/fdbuf/tlsobuf.cc
-@@ -27,5 +27,10 @@
+@@ -27,5 +27,10 @@ tlsobuf::tlsobuf(gnutls_session_t s, unsigned bufsz)
  
  ssize_t tlsobuf::_write(const char* buf, ssize_t len)
  {
