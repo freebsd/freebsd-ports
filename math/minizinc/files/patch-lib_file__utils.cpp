cea61a5fa00939f043ae177fa78cbcb8629ba936
Yuri Victorovich <yuri@FreeBSD.org>
2020-11-13 01:21:48 +0000

    math/minizinc: Update 2.5.1 -> 2.5.2

--- math/minizinc/files/patch-lib_file__utils.cpp
+++ math/minizinc/files/patch-lib_file__utils.cpp
@@ -1,16 +1,16 @@
---- lib/file_utils.cpp.orig	2020-10-25 18:29:35 UTC
+--- lib/file_utils.cpp.orig	2020-11-09 03:26:53 UTC
 +++ lib/file_utils.cpp
-@@ -310,6 +310,9 @@ std::string share_directory() {
-     return std::string(MZNSTDLIBDIR);
+@@ -315,6 +315,9 @@ std::string share_directory() {
+   if (FileUtils::file_exists(static_stdlib_dir + "/std/stdlib.mzn")) {
+     return static_stdlib_dir;
    }
- #endif
 +#if defined(__FreeBSD__)
 +  return FREEBSD_MZN_STDLIB_DIR;
 +#else
    std::string mypath = FileUtils::progpath();
    int depth = 0;
    for (char i : mypath) {
-@@ -324,6 +327,7 @@ std::string share_directory() {
+@@ -329,6 +332,7 @@ std::string share_directory() {
      mypath += "/..";
    }
    return "";
