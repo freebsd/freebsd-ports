ea792bc0213520af566bbe8e58f654654c09f1f0
Alexey Dokuchaev <danfe@FreeBSD.org>
2020-11-13 03:13:52 +0000

    Fix a silly typo in the comment.

--- databases/xtrabackup8/files/patch-storage_temptable_include_temptable_lock_free_type.h
+++ databases/xtrabackup8/files/patch-storage_temptable_include_temptable_lock_free_type.h
@@ -5,7 +5,7 @@
  #include "storage/temptable/include/temptable/constants.h"
  
 +#if defined(__i386__) //&& defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_8)
-+/* Fix for clang setting __GCC_ATOMIC_LLONG_LOCK_FREE incorecctly for x86
++/* Fix for clang setting __GCC_ATOMIC_LLONG_LOCK_FREE incorrectly for x86
 + * https://llvm.org/bugs/show_bug.cgi?id=19355
 + */
 +#undef ATOMIC_LLONG_LOCK_FREE
