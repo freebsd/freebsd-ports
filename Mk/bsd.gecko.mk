e379690328aa44292633f1ca4533e21c74260ab3
Jan Beich <jbeich@FreeBSD.org>
2020-11-13 11:52:06 +0000

    gecko: bump LLVM_DEFAULT to 11
    Detach from tree-wide default as coordinating with other maintainers
    is expensive for little gain. User's choice is still respected.
    
    llvm11 has better Tier2 support, more optimizations, reduces package size
    and aligns with what contributors dogfood.

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -92,6 +92,10 @@ MOZ_OPTIONS+=	--with-libclang-path="${LOCALBASE}/llvm${LLVM_DEFAULT}/lib"
 .if !exists(/usr/bin/llvm-objdump)
 MOZ_EXPORT+=	LLVM_OBJDUMP="${LOCALBASE}/bin/llvm-objdump${LLVM_DEFAULT}"
 .endif
+# Ignore Mk/bsd.default-versions.mk but respect make.conf(5)
+.if !defined(DEFAULT_VERSIONS) || ! ${DEFAULT_VERSIONS:Mllvm*}
+LLVM_DEFAULT=	11 # bump if not latest release
+.endif
 # Require newer Clang than what's in base system unless user opted out
 . if ${CC} == cc && ${CXX} == c++ && exists(/usr/lib/libc++.so)
 BUILD_DEPENDS+=	${LOCALBASE}/bin/clang${LLVM_DEFAULT}:devel/llvm${LLVM_DEFAULT}
