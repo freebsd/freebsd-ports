3c7c4cb53c9475851571a6738e154b1d8ceae878
Joseph Mingrone <jrm@FreeBSD.org>
2020-11-14 15:08:00 +0000

    www/chromium: Make online detection always positive
    Summary: navigator.onLine is currently broken in www/chromium. This change
    introduce a workaround that always return positive for online detection.
    
    Sponsored by:   The FreeBSD Foundation
    Submitted by:	Ka Ho Ng <khng300@gmail.com>
    Differential Revision:   https://reviews.freebsd.org/D26490

--- www/chromium/files/patch-net_base_network__change__notifier.cc
+++ www/chromium/files/patch-net_base_network__change__notifier.cc
@@ -1,4 +1,4 @@
---- net/base/network_change_notifier.cc.orig	2020-02-03 21:52:49 UTC
+--- net/base/network_change_notifier.cc.orig	2020-08-10 18:39:31 UTC
 +++ net/base/network_change_notifier.cc
 @@ -35,7 +35,7 @@
  #include "net/base/network_change_notifier_linux.h"
@@ -9,18 +9,14 @@
  #include "net/base/network_change_notifier_posix.h"
  #elif defined(OS_FUCHSIA)
  #include "net/base/network_change_notifier_fuchsia.h"
-@@ -229,7 +229,7 @@ std::unique_ptr<NetworkChangeNotifier> NetworkChangeNo
-   // service in a separate process.
-   return std::make_unique<NetworkChangeNotifierPosix>(initial_type,
-                                                       initial_subtype);
--#elif defined(OS_CHROMEOS)
-+#elif defined(OS_CHROMEOS) || defined(OS_BSD)
-   return std::make_unique<NetworkChangeNotifierPosix>(initial_type,
-                                                       initial_subtype);
- #elif defined(OS_LINUX)
-@@ -241,7 +241,6 @@ std::unique_ptr<NetworkChangeNotifier> NetworkChangeNo
+@@ -240,8 +240,11 @@ std::unique_ptr<NetworkChangeNotifier> NetworkChangeNo
+ #elif defined(OS_FUCHSIA)
    return std::make_unique<NetworkChangeNotifierFuchsia>(
        0 /* required_features */);
++#elif defined(OS_BSD)
++  return std::make_unique<MockNetworkChangeNotifier>(
++		        std::make_unique<SystemDnsConfigChangeNotifier>(
++				          nullptr /* task_runner */, nullptr /* dns_config_service */));
  #else
 -  NOTIMPLEMENTED();
    return NULL;
