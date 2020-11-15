d26b131060f255c6861e573567c2d6a3e774cdc9
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-11-15 02:00:13 +0000

    games/ioquake3: fix build on powerpc64
    MFH:		2020Q4 (fix build blanket)

--- games/ioquake3/files/patch-code-qcommon-q_platform.h
+++ games/ioquake3/files/patch-code-qcommon-q_platform.h
@@ -1,6 +1,6 @@
 --- code/qcommon/q_platform.h.orig	2009-03-02 17:29:30 UTC
 +++ code/qcommon/q_platform.h
-@@ -199,6 +199,8 @@ Foundation, Inc., 51 Franklin St, Fifth 
+@@ -199,8 +199,14 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
  
  #ifdef __i386__
  #define ARCH_STRING "i386"
@@ -8,4 +8,10 @@
 +#define ARCH_STRING "amd64"
  #elif defined __axp__
  #define ARCH_STRING "alpha"
++#elif defined __powerpc64__
++#define ARCH_STRING "ppc64"
++#elif defined __powerpc__
++#define ARCH_STRING "ppc"
  #endif
+ 
+ #if BYTE_ORDER == BIG_ENDIAN
