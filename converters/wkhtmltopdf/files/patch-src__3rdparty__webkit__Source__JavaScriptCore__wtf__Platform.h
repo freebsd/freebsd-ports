36cc6cffde4d5732fe60fb5ab8eb5893cab37999
Kurt Jaeger <pi@FreeBSD.org>
2020-11-15 10:23:01 +0000

    converters/wkhtmltopdf: fix build on arm64/aarch64
    PR:		250952
    Submitted by:	rasmus@gal.dk
    Reviewed by:	linimon
    MFH:		2020Q4

---
+++ converters/wkhtmltopdf/files/patch-src__3rdparty__webkit__Source__JavaScriptCore__wtf__Platform.h
@@ -0,0 +1,12 @@
+--- src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h.orig	2020-11-08 17:05:56.503694000 +0100
++++ src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h	2020-11-08 17:06:42.105176000 +0100
+@@ -1003,7 +1003,8 @@
+     || CPU(SPARC64) \
+     || CPU(S390X) \
+     || CPU(PPC64) \
+-    || CPU(MIPS64)
++    || CPU(MIPS64) \
++    || defined(__aarch64__)
+ #define WTF_USE_JSVALUE64 1
+ #else
+ #define WTF_USE_JSVALUE32_64 1
