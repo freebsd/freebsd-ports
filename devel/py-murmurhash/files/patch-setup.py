078552ccbb7fd7da808440643fc06091d87b3fe3
Yuri Victorovich <yuri@FreeBSD.org>
2020-11-15 19:31:02 +0000

    devel/py-murmurhash: Update 1.0.3 -> 1.0.4

--- devel/py-murmurhash/files/patch-setup.py
+++ devel/py-murmurhash/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2020-10-29 12:35:41 UTC
+--- setup.py.orig	2020-11-02 09:42:20 UTC
 +++ setup.py
 @@ -137,7 +137,7 @@ def setup_package():
              url=about["__uri__"],
              license=about["__license__"],
              ext_modules=ext_modules,
--            setup_requires=["wheel>=0.32.0,<0.33.0"],
+-            setup_requires=["wheel"],
 +            setup_requires=[],
              classifiers=[
                  "Development Status :: 5 - Production/Stable",
