e9e80cf5d1af9d8a1364285a98fb7f28697ad1af
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-11-17 20:07:37 +0000

    x11-toolkits/pangolin: fix build on GCC architectures
    /wrkdirs/usr/ports/x11-toolkits/pangolin/work/Pangolin-0.5-431-g86eb497/src/utils/file_utils.cpp: In function 'std::string pangolin::FindPath(const string&, const string&)':
    /wrkdirs/usr/ports/x11-toolkits/pangolin/work/Pangolin-0.5-431-g86eb497/src/utils/file_utils.cpp:194:19: error: 'PATH_MAX' was not declared in this scope
      194 |     char abs_path[PATH_MAX];
          |                   ^~~~~~~~
    /wrkdirs/usr/ports/x11-toolkits/pangolin/work/Pangolin-0.5-431-g86eb497/src/utils/file_utils.cpp:195:32: error: 'abs_path' was not declared in this scope
      195 |     if (realpath(path.c_str(), abs_path)) {

---
+++ x11-toolkits/pangolin/files/patch-src_utils_file__utils.cpp
@@ -0,0 +1,10 @@
+--- src/utils/file_utils.cpp.orig	2020-11-17 16:53:53 UTC
++++ src/utils/file_utils.cpp
+@@ -54,6 +54,7 @@
+ #include <algorithm>
+ #include <sstream>
+ #include <list>
++#include <climits>
+ 
+ namespace pangolin
+ {
