bcb1c9b4debee92c2606714c52e8e62f4d9af45f
Palle Girgensohn <girgen@FreeBSD.org>
2020-11-17 09:50:16 +0000

    Fix SIGSEGV fault on FreeBSD 12+
    PR:	250825
    Submitted by:	Artyom Davidov

---
+++ sysutils/fluent-bit/files/patch-lib_flb__libco_settings.h
@@ -0,0 +1,11 @@
+--- lib/flb_libco/settings.h.orig	2020-11-16 20:38:14 UTC
++++ lib/flb_libco/settings.h
+@@ -42,6 +42,8 @@
+   #define text_section __declspec(allocate(".text"))
+ #elif defined(__APPLE__) && defined(__MACH__)
+   #define text_section __attribute__((section("__TEXT,__text")))
++#elif defined(__clang__)
++  #define text_section __attribute__((section(".text")))
+ #else
+   #define text_section __attribute__((section(".text#")))
+ #endif
