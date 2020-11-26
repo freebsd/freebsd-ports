e4de351588a5e8640d3ebea26acd5e0dddf1fce7
Danilo G. Baio <dbaio@FreeBSD.org>
2020-11-26 00:16:38 +0000

    net-im/py-matrix-synapse: Update to 1.23.0
    Changelog:	https://github.com/matrix-org/synapse/blob/v1.23.0/CHANGES.md
    
    PR:		250965
    Submitted by:	Sascha Biberhofer <ports@skyforge.at> (maintainer)

--- net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
+++ net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
@@ -1,9 +1,9 @@
---- synapse/python_dependencies.py.orig	2020-10-15 15:59:57 UTC
+--- synapse/python_dependencies.py.orig	2020-11-18 13:34:04 UTC
 +++ synapse/python_dependencies.py
-@@ -72,7 +72,7 @@ REQUIREMENTS = [
-     # prom-client has a history of breaking backwards compatibility between
-     # minor versions (https://github.com/prometheus/client_python/issues/317),
-     # so we also pin the minor version.
+@@ -76,7 +76,7 @@ REQUIREMENTS = [
+     # Note that we replicate these constraints in the Synapse Dockerfile while
+     # pre-installing dependencies. If these constraints are updated here, the
+     # same change should be made in the Dockerfile.
 -    "prometheus_client>=0.4.0,<0.9.0",
 +    "prometheus_client>=0.4.0",
      # we use attr.validators.deep_iterable, which arrived in 19.1.0 (Note:
