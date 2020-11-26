f3637be01ef527ecd3f31e7d628ef8dad98ede93
Jan Beich <jbeich@FreeBSD.org>
2020-11-26 00:30:14 +0000

    devel/vulkan-*: update to 1.2.154
    Changes:	https://github.com/KhronosGroup/Vulkan-Headers/compare/sdk-1.2.135.0...sdk-1.2.154.0
    Changes:	https://github.com/KhronosGroup/Vulkan-Loader/compare/sdk-1.2.135.0...sdk-1.2.154.1
    Changes:	https://github.com/KhronosGroup/Vulkan-Tools/compare/sdk-1.2.135.0...sdk-1.2.154.0
    Changes:	https://github.com/KhronosGroup/Vulkan-ValidationLayers/compare/sdk-1.2.135.0...sdk-1.2.154.0
    Changes:	https://github.com/KhronosGroup/SPIRV-Tools/compare/v2020.2...v2020.4
    PR:		251345
    Approved by:	Greg V <greg@unrelenting.technology> (maintainer, earlier version)
    Submitted by:	Austin Shafer <ashafer@badland.io> (spirv-tools, vulkan-validation-layers)

--- devel/vulkan-headers/files/patch-include_vulkan_vulkan.hpp
+++ devel/vulkan-headers/files/patch-include_vulkan_vulkan.hpp
@@ -1,47 +1,56 @@
---- include/vulkan/vulkan.hpp.orig	2020-05-14 18:48:51 UTC
+--- include/vulkan/vulkan.hpp.orig	2020-09-21 09:26:07 UTC
 +++ include/vulkan/vulkan.hpp
-@@ -65,7 +65,7 @@
+@@ -66,7 +66,7 @@
  #endif
  
  #if VULKAN_HPP_ENABLE_DYNAMIC_LOADER_TOOL == 1
--#  if defined(__linux__) || defined(__APPLE__)
-+#  if defined(__unix__)
- #   include <dlfcn.h>
- #  endif
- 
-@@ -81153,7 +81153,7 @@ namespace VULKAN_HPP_NAMESPACE
-     DynamicLoader() : m_success( false )
- #endif
+-#  if defined( __linux__ ) || defined( __APPLE__ )
++#  if defined( __unix__ ) || defined( __APPLE__ )
+ #    include <dlfcn.h>
+ #  elif defined( _WIN32 )
+ typedef struct HINSTANCE__ * HINSTANCE;
+@@ -90062,7 +90062,7 @@ namespace VULKAN_HPP_NAMESPACE
      {
--#if defined(__linux__)
-+#if defined(__unix__)
-       m_library = dlopen( "libvulkan.so", RTLD_NOW | RTLD_LOCAL );
- #elif defined(__APPLE__)
-       m_library = dlopen( "libvulkan.dylib", RTLD_NOW | RTLD_LOCAL );
-@@ -81195,7 +81195,7 @@ namespace VULKAN_HPP_NAMESPACE
+       if ( !vulkanLibraryName.empty() )
+       {
+-#  if defined( __linux__ ) || defined( __APPLE__ )
++#  if defined( __unix__ ) || defined( __APPLE__ )
+         m_library = dlopen( vulkanLibraryName.c_str(), RTLD_NOW | RTLD_LOCAL );
+ #  elif defined( _WIN32 )
+         m_library = ::LoadLibraryA( vulkanLibraryName.c_str() );
+@@ -90072,7 +90072,7 @@ namespace VULKAN_HPP_NAMESPACE
+       }
+       else
+       {
+-#  if defined( __linux__ )
++#  if defined( __unix__ )
+         m_library = dlopen( "libvulkan.so", RTLD_NOW | RTLD_LOCAL );
+         if ( m_library == nullptr )
+         {
+@@ -90119,7 +90119,7 @@ namespace VULKAN_HPP_NAMESPACE
      {
        if ( m_library )
        {
--#if defined(__linux__) || defined(__APPLE__)
-+#if defined(__unix__)
+-#  if defined( __linux__ ) || defined( __APPLE__ )
++#  if defined( __unix__ ) || defined( __APPLE__ )
          dlclose( m_library );
- #elif defined(_WIN32)
-         FreeLibrary( m_library );
-@@ -81206,7 +81206,7 @@ namespace VULKAN_HPP_NAMESPACE
+ #  elif defined( _WIN32 )
+         ::FreeLibrary( m_library );
+@@ -90132,7 +90132,7 @@ namespace VULKAN_HPP_NAMESPACE
      template <typename T>
      T getProcAddress( const char* function ) const VULKAN_HPP_NOEXCEPT
      {
--#if defined(__linux__) || defined(__APPLE__)
-+#if defined(__unix__)
+-#  if defined( __linux__ ) || defined( __APPLE__ )
++#  if defined( __unix__ ) || defined( __APPLE__ )
        return (T)dlsym( m_library, function );
- #elif defined(_WIN32)
-       return (T)GetProcAddress( m_library, function );
-@@ -81217,7 +81217,7 @@ namespace VULKAN_HPP_NAMESPACE
+ #  elif defined( _WIN32 )
+       return (T)::GetProcAddress( m_library, function );
+@@ -90145,7 +90145,7 @@ namespace VULKAN_HPP_NAMESPACE
  
    private:
      bool m_success;
--#if defined(__linux__) || defined(__APPLE__)
-+#if defined(__unix__)
-     void *m_library;
- #elif defined(_WIN32)
-     HMODULE m_library;
+-#  if defined( __linux__ ) || defined( __APPLE__ )
++#  if defined( __unix__ ) || defined( __APPLE__ )
+     void * m_library;
+ #  elif defined( _WIN32 )
+     ::HINSTANCE m_library;
--- graphics/vulkan-loader/files/patch-loader_vk__loader__platform.h
+++ graphics/vulkan-loader/files/patch-loader_vk__loader__platform.h
@@ -1,4 +1,4 @@
---- loader/vk_loader_platform.h.orig	2020-04-02 16:20:56 UTC
+--- loader/vk_loader_platform.h.orig	2020-10-07 20:39:07 UTC
 +++ loader/vk_loader_platform.h
 @@ -31,7 +31,7 @@
  #include "vulkan/vk_platform.h"
@@ -9,3 +9,30 @@
  /* Linux-specific common code: */
  
  // Headers:
+@@ -116,6 +116,26 @@ static inline char *loader_platform_executable_path(ch
+     int ret = proc_pidpath(pid, buffer, size);
+     if (ret <= 0) return NULL;
+     buffer[ret] = '\0';
++    return buffer;
++}
++#elif defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__)
++#include <sys/sysctl.h>
++static inline char *loader_platform_executable_path(char *buffer, size_t size) {
++    int mib[] = {
++        CTL_KERN,
++#if defined(__NetBSD__)
++        KERN_PROC_ARGS,
++        -1,
++        KERN_PROC_PATHNAME,
++#else
++        KERN_PROC,
++        KERN_PROC_PATHNAME,
++        -1,
++#endif
++    };
++    if (sysctl(mib, sizeof(mib) / sizeof(mib[0]), buffer, &size, NULL, 0) < 0)
++        return NULL;
++
+     return buffer;
+ }
+ #endif  // defined (__APPLE__)
