494b0275ddc55c281cbfc307e6c2f2fe7c24def6
Rainer Hurling <rhurlin@FreeBSD.org>
2020-11-29 18:58:09 +0000

    graphics/qgis-ltr: Update to 3.10.12
    Bugfix release [1][2].
    
    [1] https://qgis.org/en/site/forusers/visualchangelog310/index.html
    [2] https://github.com/qgis/QGIS/compare/final-3_10_11...final-3_10_12
    
    If you miss features in QGIS-LTR that are actually
    built into QGIS-LTR, you may need to enable additional
    options in graphics/gdal, see pkg-message.
    
    Approved by:	arrowd, tcberner (mentors, implicit)

--- graphics/qgis-ltr/files/patch-python_plugins_processing_algs_saga_SagaAlgorithmProvider.py
+++ graphics/qgis-ltr/files/patch-python_plugins_processing_algs_saga_SagaAlgorithmProvider.py
@@ -1,20 +1,27 @@
---- python/plugins/processing/algs/saga/SagaAlgorithmProvider.py.orig	2020-03-27 05:50:09 UTC
+--- python/plugins/processing/algs/saga/SagaAlgorithmProvider.py.orig	2019-12-07 13:18:21 UTC
 +++ python/plugins/processing/algs/saga/SagaAlgorithmProvider.py
-@@ -79,7 +79,7 @@ class SagaAlgorithmProvider(QgsProcessingProvider):
+@@ -79,13 +79,13 @@ class SagaAlgorithmProvider(QgsProcessingProvider):
  
      def canBeActivated(self):
          version = SagaUtils.getInstalledVersion(True)
 -        if version is not None and (version.startswith(REQUIRED_VERSION) or version.startswith(BETA_SUPPORT_VERSION)):
-+        if version is not None and version >= REQUIRED_VERSION:
++        if version is not None and (version.startswith(REQUIRED_VERSION) or version >= BETA_SUPPORT_VERSION):
              return True
          return False
+ 
+     def warningMessage(self):
+         version = SagaUtils.getInstalledVersion(True)
+-        if version is not None and version.startswith(BETA_SUPPORT_VERSION):
++        if version is not None and version >= BETA_SUPPORT_VERSION:
+             return self.tr('SAGA version {} is not officially supported - algorithms may encounter issues').format(version)
+         return ''
  
 @@ -96,7 +96,7 @@ class SagaAlgorithmProvider(QgsProcessingProvider):
                                       self.tr('Processing'), Qgis.Critical)
              return
  
 -        if not version.startswith(REQUIRED_VERSION) and not version.startswith(BETA_SUPPORT_VERSION):
-+        if version < REQUIRED_VERSION:
++        if not version.startswith(REQUIRED_VERSION) and not version >= BETA_SUPPORT_VERSION:
              QgsMessageLog.logMessage(self.tr('Problem with SAGA installation: unsupported SAGA version (found: {}, required: {}).').format(version, REQUIRED_VERSION),
                                       self.tr('Processing'),
                                       Qgis.Critical)
