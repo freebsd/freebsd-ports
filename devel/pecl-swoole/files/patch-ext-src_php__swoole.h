b457c3eae705c102041b0ef25cecf9b94219a8d8
Vanilla I. Shu <vanilla@FreeBSD.org>
2020-11-30 08:19:09 +0000

    Update to 4.5.9.

--- devel/pecl-swoole/files/patch-ext-src_php__swoole.h
+++ devel/pecl-swoole/files/patch-ext-src_php__swoole.h
@@ -1,6 +1,6 @@
---- ext-src/php_swoole.h.orig	2020-11-22 13:36:39 UTC
-+++ ext-src/php_swoole.h
-@@ -912,10 +912,11 @@ static sw_inline zend_bool sw_zend_is_callable_at_fram
+--- ext-src/php_swoole.h.orig	2020-11-27 11:02:33.000000000 +0800
++++ ext-src/php_swoole.h	2020-11-30 16:16:31.186209000 +0800
+@@ -912,7 +912,7 @@ static sw_inline zend_bool sw_zend_is_callable_at_fram
  {
      zend_string *name;
      zend_bool ret;
@@ -8,9 +8,4 @@
 +#if PHP_VERSION_ID < 80001
      ret = zend_is_callable_ex(zcallable, zobject ? Z_OBJ_P(zobject) : NULL, check_flags, &name, fci_cache, error);
  #else
--    ret = zend_is_callable_at_frame(zcallable, zobject ? Z_OBJ_P(zobject) : NULL, frame, check_flags, &name, fci_cache, error);
-+    ret = zend_is_callable_at_frame(zcallable, zobject ? Z_OBJ_P(zobject) : NULL, frame, check_flags, fci_cache, error);
-+    name = zend_get_callable_name_ex(zcallable, zobject);
- #endif
-     if (callable_name)
-     {
+     ret = zend_is_callable_at_frame(zcallable, zobject ? Z_OBJ_P(zobject) : NULL, frame, check_flags, fci_cache, error);
