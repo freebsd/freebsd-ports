41f9c083716f1d8b81ee941b0d48b9cab0fc18b6
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-11-30 02:00:14 +0000

    security/highwayhash: remove sys/types.h include, move sys/param.h higher
    Reported by:	jbeich

--- security/highwayhash/files/patch-highwayhash_os__specific.cc
+++ security/highwayhash/files/patch-highwayhash_os__specific.cc
@@ -1,10 +1,12 @@
 --- highwayhash/os_specific.cc.orig	2020-11-29 23:19:18 UTC
 +++ highwayhash/os_specific.cc
-@@ -53,6 +53,7 @@
+@@ -53,8 +53,8 @@
  
  #ifdef __FreeBSD__
  #define OS_FREEBSD 1
-+#include <sys/types.h>
- #include <sys/cpuset.h>
+-#include <sys/cpuset.h>
  #include <sys/param.h>
++#include <sys/cpuset.h>
  #include <unistd.h>
+ #else
+ #define OS_FREEBSD 0
