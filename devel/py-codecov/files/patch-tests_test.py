77538b91b96f90482e2d66dc6ab00585795fc810
Guangyuan Yang <ygy@FreeBSD.org>
2020-12-01 11:38:28 +0000

    devel/py-codecov: Update to 2.1.10 and take maintainership
    PR:		251392
    Approved by:	lwhsu

--- devel/py-codecov/files/patch-tests_test.py
+++ devel/py-codecov/files/patch-tests_test.py
@@ -1,39 +1,121 @@
---- tests/test.py.orig	2018-01-29 15:52:17 UTC
-+++ tests/test.py
-@@ -2,9 +2,9 @@ import os
- import sys
- import pickle
- import itertools
-+import unittest
- from ddt import ddt, data
- from mock import patch, Mock
--import unittest2 as unittest
+--- tests/test.py.orig	2020-11-25 20:02:03.000000000 +0800
++++ tests/test.py	2020-11-25 20:04:45.000000000 +0800
+@@ -231,10 +231,7 @@
+         else:
+             raise Exception("did not exit")
  
- import subprocess
- 
-@@ -134,7 +134,7 @@ class TestUploader(unittest.TestCase):
+-    @unittest.skipIf(
+-        os.getenv("CI") == "True" and os.getenv("APPVEYOR") == "True",
+-        "Skip AppVeyor CI test",
+-    )
++    @unittest.skip("Skip tests dependent on .git directory")
+     def test_returns_none(self):
+         with patch("requests.post") as post:
+             with patch("requests.put") as put:
+@@ -251,10 +248,7 @@
                  self.assertEqual(codecov.main(), None)
                  assert post.called and put.called
  
--    @unittest.skipIf(os.getenv('CI') == "True" and os.getenv('APPVEYOR') == 'True', 'Skip AppVeyor CI test')
-+    @unittest.skip('Skip AppVeyor test')
+-    @unittest.skipIf(
+-        os.getenv("CI") == "True" and os.getenv("APPVEYOR") == "True",
+-        "Skip AppVeyor CI test",
+-    )
++    @unittest.skip("Skip tests dependent on .git directory")
      def test_send(self):
-         with patch('requests.post') as post:
-             with patch('requests.put') as put:
-@@ -204,6 +204,7 @@ class TestUploader(unittest.TestCase):
+         with patch("requests.post") as post:
+             with patch("requests.put") as put:
+@@ -294,10 +288,7 @@
+             else:
+                 raise Exception("400 never raised")
+ 
+-    @unittest.skipIf(
+-        os.getenv("CI") == "True" and os.getenv("APPVEYOR") == "True",
+-        "Skip AppVeyor CI test",
+-    )
++    @unittest.skip("Skip tests dependent on .git directory")
+     def test_read_token_file(self):
+         with open(self.token, "w+") as f:
+             f.write("a")
+@@ -306,6 +297,7 @@
+         res = self.run_cli(token="@" + self.token, commit="a", branch="b")
+         self.assertIn("token=a", res["urlargs"])
+ 
++    @unittest.skip("Skip tests dependent on .git directory")
+     def test_bowerrc(self):
+         with open(self.bowerrc, "w+") as f:
+             f.write('{"directory": "tests"}')
+@@ -318,6 +310,7 @@
          else:
              raise Exception("Did not raise AssertionError")
  
-+    @unittest.skip('Skip test')
++    @unittest.skip("Skip tests dependent on .git directory")
+     def test_disable_search(self):
+         self.fake_report()
+         try:
+@@ -327,10 +320,7 @@
+         else:
+             raise Exception("Did not raise AssertionError")
+ 
+-    @unittest.skipIf(
+-        os.getenv("CI") == "True" and os.getenv("APPVEYOR") == "True",
+-        "Skip AppVeyor CI test",
+-    )
++    @unittest.skip("Skip tests dependent on .git directory")
      def test_prefix(self):
          self.fake_report()
-         res = self.run_cli(prefix='/foo/bar/', dump=True, token='a', branch='b', commit='c')
-@@ -254,7 +255,7 @@ class TestUploader(unittest.TestCase):
+         res = self.run_cli(
+@@ -400,10 +390,7 @@
          else:
              raise Exception("Did not raise AssertionError")
  
--    @unittest.skipIf(os.getenv('CI') == "True" and os.getenv('APPVEYOR') == 'True', 'Skip AppVeyor CI test')
-+    @unittest.skip('Skip AppVeyor test')
+-    @unittest.skipIf(
+-        os.getenv("CI") == "True" and os.getenv("APPVEYOR") == "True",
+-        "Skip AppVeyor CI test",
+-    )
++    @unittest.skip("Skip tests dependent on .git directory")
      def test_bowerrc_none(self):
-         with open(self.bowerrc, 'w+') as f:
+         with open(self.bowerrc, "w+") as f:
              f.write('{"other_key": "tests"}')
+@@ -412,10 +399,7 @@
+         res = self.run_cli(**self.defaults)
+         self.assertIn("tests/test.py", res["reports"])
+ 
+-    @unittest.skipIf(
+-        os.getenv("CI") == "True" and os.getenv("APPVEYOR") == "True",
+-        "Skip AppVeyor CI test",
+-    )
++    @unittest.skip("Skip tests dependent on .git directory")
+     def test_discovers(self):
+         with open(self.jacoco, "w+") as f:
+             f.write("<jacoco></jacoco>")
+@@ -427,6 +411,7 @@
+         self.assertIn("jacoco.xml", res["reports"])
+         self.assertIn("<jacoco></jacoco>", res["reports"])
+ 
++    @unittest.skip("Skip tests dependent on .git directory")
+     def test_not_jacoco(self):
+         with open(self.filepath, "w+") as f:
+             f.write("<data>")
+@@ -442,6 +427,7 @@
+         res = self.run_cli(**self.defaults)
+         self.assertIn('<?xml version="1.0" ?>', res["reports"])
+ 
++    @unittest.skip("Skip tests dependent on .git directory")
+     def test_run_coverage_fails(self):
+         with open(self.coverage, "w+") as f:
+             f.write("bad data")
+@@ -452,12 +438,14 @@
+         else:
+             raise Exception("Did not raise AssertionError")
+ 
++    @unittest.skip("Skip tests dependent on .git directory")
+     def test_include_env(self):
+         self.set_env(HELLO="WORLD")
+         self.fake_report()
+         res = self.run_cli(env="HELLO", file=self.filepath, **self.defaults)
+         self.assertIn("HELLO=WORLD", res["reports"])
+ 
++    @unittest.skip("Skip tests dependent on .git directory")
+     def test_none_found(self):
+         try:
+             self.run_cli(**self.defaults)
