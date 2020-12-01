612299b4c1030fbc3c64e2abbcc20be8f1e9f4a7
Mikael Urankar <mikael@FreeBSD.org>
2020-12-01 16:51:38 +0000

    dns/encrypted-dns-server: Fix build on build on 11.4
    Switch to a modern linker
    
    PR:		251273
    Approved by:	Felix Hanley (maintainer)

---
+++ dns/encrypted-dns-server/files/patch-cargo-crates_libsodium-sys-stable-1.19.11_build.rs
@@ -0,0 +1,10 @@
+--- cargo-crates/libsodium-sys-stable-1.19.11/build.rs.orig     2020-11-19 15:20:25 UTC
++++ cargo-crates/libsodium-sys-stable-1.19.11/build.rs
+@@ -214,6 +214,7 @@ fn make_libsodium(target: &str, source_dir: &Path, ins
+         cross_compiling = true;
+         help = "";
+     } else {
++        cflags += " -fuse-ld=lld";
+         if target.contains("i686") {
+             compiler += " -m32 -maes";
+             cflags += " -march=i686 -fuse-ld=lld";
