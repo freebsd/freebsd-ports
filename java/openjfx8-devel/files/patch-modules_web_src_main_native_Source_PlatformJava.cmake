6ac46b8d4eda46efbae5a8491764d3684545c09b
Rene Ladan <rene@FreeBSD.org>
2020-12-01 20:09:51 +0000

    Remove expired ports:
    2020-11-30 databases/datamodeler: depends on expiring java/openjfx8-devel
    2020-11-30 devel/gradle4: only needed for expiring biology/igv and java/openjfx8-devel
    2020-11-30 devel/py-astroid1: Use devel/py-astroid instead
    2020-11-30 devel/pylint1: Use devel/pylint instead
    2020-11-30 german/mediathekview: depends on expiring java/openjfx8-devel
    2020-11-30 java/openjfx8-devel: No longer maintained upstream
    2020-11-30 java/openjfx8-scenebuilder: depends on expiring java/openjfx8-devel
    2020-11-30 net-im/py-sleekxmpp: Upstream archived and advices to use net-im/py-slixmpp
    2020-12-01 net-mgmt/zabbix44-agent: End of Life
    2020-12-01 net-mgmt/zabbix44-frontend: End of Life
    2020-12-01 net-mgmt/zabbix44-java: End of Life
    2020-12-01 net-mgmt/zabbix44-proxy: End of Life
    2020-12-01 net-mgmt/zabbix44-server: End of Life
    2020-11-30 print/jabref: depends on expiring java/openjfx8-devel
    2020-11-30 science/datawarrior: depends on expiring java/openjfx8-devel

--- java/openjfx8-devel/files/patch-modules_web_src_main_native_Source_PlatformJava.cmake
+++
@@ -1,8 +0,0 @@
---- modules/web/src/main/native/Source/PlatformJava.cmake.orig	2018-03-23 23:53:57 UTC
-+++ modules/web/src/main/native/Source/PlatformJava.cmake
-@@ -1,4 +1 @@
--add_subdirectory(ThirdParty/sqlite)
--add_subdirectory(ThirdParty/icu)
--add_subdirectory(ThirdParty/libxml)
--add_subdirectory(ThirdParty/libxslt)
-+
