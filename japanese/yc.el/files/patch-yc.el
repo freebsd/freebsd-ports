df9a2fa31cc785cc95ed8bf8c70a8d1167ff79cf
Rainer Hurling <rhurlin@FreeBSD.org>
2020-12-04 12:41:08 +0000

    japanese/yc.el: Adapt to newer emacs versions
    The "process-kill-without-query" function was made
    obsolete in emacs 27.1 [1]. Therefore the function
    should be replaced in japanese/yc.el by
    "set-process-query-on-exit-flag" function.
    
    [1] http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=f1c48b0
    
    PR:		249873
    Submitted by:	Takayuki Nakao <t@nakao.org> (maintainer)
    Approved by:	tcberner (mentor)
    Differential Revision:	https://reviews.freebsd.org/D27473

--- japanese/yc.el/files/patch-yc.el
+++ japanese/yc.el/files/patch-yc.el
@@ -1,6 +1,17 @@
---- yc.el.orig	2018-06-04 13:46:33.083915000 +0900
-+++ yc.el	2018-06-04 14:48:21.116298000 +0900
-@@ -1736,6 +1736,7 @@
+--- yc.el.orig	2020-09-25 04:51:12 UTC
++++ yc.el
+@@ -393,7 +393,9 @@ OBJ を返却する。"
+ 		       (error nil)))))))
+   (when (processp yc-server)
+     (put 'yc-server 'init nil)
+-    (process-kill-without-query yc-server)
++    (if (boundp 'process-kill-without-query)
++	(process-kill-without-query yc-server)
++      (set-process-query-on-exit-flag yc-server nil))
+     (when yc-debug
+       (unwind-protect
+ 	  (progn
+@@ -1736,6 +1738,7 @@ OBJ を返却する。"
  				   (error nil))))
  	    (yc-eval-sexp (car expr)))))
        (setq files (cdr files)))
@@ -8,7 +19,7 @@
      (if romkana-table
  	(setq yc-rH-conv-dic (yc-search-file-first-in-path
  			      romkana-table (list "." (getenv "HOME")
-@@ -2071,7 +2072,7 @@
+@@ -2071,7 +2074,7 @@ OBJ を返却する。"
  ;; 文節を指定しない場合、現在の文節が対象となる
  ;; 読みを取得した文節はその読みをキャッシュする
  ;; cut が 非nil の場合、指定文節以降の読みを削除する
