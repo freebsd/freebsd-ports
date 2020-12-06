bce768706a8ac9e977c351ba45464035e4d76651
Steve Wills <swills@FreeBSD.org>
2020-12-06 01:38:42 +0000

    databases/memcached: update to 1.6.9

--- databases/memcached/files/patch-memcached.c
+++ databases/memcached/files/patch-memcached.c
@@ -1,6 +1,6 @@
---- memcached.c.orig	2020-03-23 17:30:29 UTC
+--- memcached.c.orig	2020-11-21 00:10:41 UTC
 +++ memcached.c
-@@ -647,7 +647,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
+@@ -676,7 +676,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
          if (init_state == conn_listening) {
              fprintf(stderr, "<%d server listening (%s)\n", sfd,
                  prot_text(c->protocol));
@@ -9,7 +9,7 @@
              fprintf(stderr, "<%d server listening (udp)\n", sfd);
          } else if (c->protocol == negotiating_prot) {
              fprintf(stderr, "<%d new auto-negotiating client connection\n",
-@@ -708,7 +708,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
+@@ -736,7 +736,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
          c->write = tcp_write;
      }
  
@@ -18,7 +18,7 @@
          c->try_read_command = try_read_command_udp;
      } else {
          switch (c->protocol) {
-@@ -876,7 +876,7 @@ static void conn_cleanup(conn *c) {
+@@ -820,7 +820,7 @@ static void conn_cleanup(conn *c) {
          c->sasl_conn = NULL;
      }
  
@@ -27,7 +27,7 @@
          conn_set_state(c, conn_read);
      }
  }
-@@ -1038,7 +1038,7 @@ static bool resp_start(conn *c) {
+@@ -1116,7 +1116,7 @@ bool resp_start(conn *c) {
          c->resp->next = resp;
          c->resp = resp;
      }
@@ -36,7 +36,7 @@
          // need to hold on to some data for async responses.
          c->resp->request_id = c->request_id;
          c->resp->request_addr = c->request_addr;
-@@ -3443,7 +3443,7 @@ static void process_stats_conns(ADD_STAT add_stats, vo
+@@ -2046,7 +2046,7 @@ void process_stats_conns(ADD_STAT add_stats, void *c) 
               * output -- not worth the complexity of the locking that'd be
               * required to prevent it.
               */
@@ -45,7 +45,7 @@
                  APPEND_NUM_STAT(i, "UDP", "%s", "UDP");
              }
              if (conns[i]->state != conn_closed) {
-@@ -7315,7 +7315,7 @@ static void drive_machine(conn *c) {
+@@ -3242,7 +3242,7 @@ static void drive_machine(conn *c) {
              break;
  
          case conn_closing:
@@ -54,7 +54,7 @@
                  conn_cleanup(c);
              else
                  conn_close(c);
-@@ -7479,7 +7479,7 @@ static int server_socket(const char *interface,
+@@ -3425,7 +3425,7 @@ static int server_socket(const char *interface,
  #endif
  
          setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, (void *)&flags, sizeof(flags));
@@ -63,7 +63,7 @@
              maximize_sndbuf(sfd);
          } else {
              error = setsockopt(sfd, SOL_SOCKET, SO_KEEPALIVE, (void *)&flags, sizeof(flags));
-@@ -7534,7 +7534,7 @@ static int server_socket(const char *interface,
+@@ -3480,7 +3480,7 @@ static int server_socket(const char *interface,
              }
          }
  
