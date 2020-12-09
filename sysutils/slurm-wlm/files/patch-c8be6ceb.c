freebsd/freebsd-ports

commit 2af8c95e5d23f4ca68a7e565b67f0bd5eeffa273

sysutils/slurm-wlm: Fix build with -fno-common
Add two patches based on upstream changes to fix the build of
sysutils/slurm-wlm with -fno-common, which is the default with llvm 11.

MFH:		2020Q3 (implicit, -fno-common fixes, ok by joenum)


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@546150 35697150-7ecd-e111-bb59-0022644237b5

sysutils/slurm-wlm/files/patch-c8be6ceb.c
@@ -1,0 +1,25 @@
+diff --git a/src/scancel/scancel.c b/src/scancel/scancel.c
+index 4448b75599..23594fe0bf 100644
+--- src/scancel/scancel.c
++++ src/scancel/scancel.c
+@@ -99,6 +99,7 @@ static	pthread_cond_t   num_active_threads_cond;
+ static	pthread_mutex_t  max_delay_lock;
+ static	uint32_t max_resp_time = 0;
+ static	int request_count = 0;
++opt_t opt;
+ 
+ int
+ main (int argc, char **argv)
+diff --git a/src/scancel/scancel.h b/src/scancel/scancel.h
+index a0c9d8a829..678bcd7616 100644
+--- src/scancel/scancel.h
++++ src/scancel/scancel.h
+@@ -75,7 +75,7 @@ typedef struct scancel_options {
+ 	bool *job_pend;		/* Set fi job is pending	*/
+ } opt_t;
+ 
+-opt_t opt;
++extern opt_t opt;
+ 
+ /* process options:
+  * 1. set defaults

sysutils/slurm-wlm/files/patch-src_plugins_select_cons__tres_job__test.c
@@ -1,0 +1,26 @@
+--- src/plugins/select/cons_tres/job_test.c.orig	2020-03-26 21:44:05 UTC
++++ src/plugins/select/cons_tres/job_test.c
+@@ -41,23 +41,6 @@
+ 
+ #define _DEBUG 0	/* Enables module specific debugging */
+ 
+-/*
+- * These symbols are defined here so when we link with something other
+- * than the slurmctld we will have these symbols defined. They will get
+- * overwritten when linking with the slurmctld.
+- */
+-#if defined (__APPLE__)
+-extern slurmctld_config_t slurmctld_config __attribute__((weak_import));
+-extern bitstr_t *idle_node_bitmap __attribute__((weak_import));
+-extern node_record_t *node_record_table_ptr __attribute__((weak_import));
+-extern List job_list __attribute__((weak_import));
+-#else
+-slurmctld_config_t slurmctld_config;
+-bitstr_t *idle_node_bitmap;
+-node_record_t *node_record_table_ptr;
+-List job_list;
+-#endif
+-
+ typedef struct node_weight_struct {
+ 	bitstr_t *node_bitmap;	/* bitmap of nodes with this weight */
+ 	uint32_t weight;	/* priority of node for scheduling work on */

commit 1366853366e3327ac6a063a756d624af18044a53

MFH: r556796 r557295
sysutils/slurm-wlm: Patch in lua 5.4 support

PR opened upstream
Make sure patch files have meaningful names

PR:             251471
Reported by:    greg@unrelenting.technology

sysutils/slurm-wlm: Back out -fno-common patches, add -fcommon flag

Fixes runtime regression caused by r546150

Reported by:    jrm

Approved by:	blanket bug fix

sysutils/slurm-wlm/Makefile
@@ -2,7 +2,7 @@
 
 PORTNAME=	slurm
 DISTVERSION=	20.02.1
-PORTREVISION=	2
+PORTREVISION=	4
 CATEGORIES=	sysutils python
 MASTER_SITES=	https://download.schedmd.com/slurm/
 PKGNAMESUFFIX=	-wlm
@@ -82,8 +82,8 @@ RRD_CONFIGURE_WITH=	rrdtool
 CONFIGURE_ARGS=	--without-ofed
 DOCSDIR=	${PREFIX}/share/doc/${PORTNAME}-${DISTVERSION}
 
-CFLAGS+=	-I${WRKSRC}/slurm -I${LOCALBASE}/include
-LDFLAGS+=	-L${LOCALBASE}/lib -lsysinfo -lprocstat
+CFLAGS+=	-I${WRKSRC}/slurm -I${LOCALBASE}/include -fcommon
+LDFLAGS+=	-L${LOCALBASE}/lib -lsysinfo -lprocstat -fcommon
 
 # Hack around nonfunctional --disable-gtktest flag
 pre-configure-GUI-off:

sysutils/slurm-wlm/files/patch-c8be6ceb.c
@@ -1,25 +1,0 @@
-diff --git a/src/scancel/scancel.c b/src/scancel/scancel.c
-index 4448b75599..23594fe0bf 100644
---- src/scancel/scancel.c
-+++ src/scancel/scancel.c
-@@ -99,6 +99,7 @@ static	pthread_cond_t   num_active_threads_cond;
- static	pthread_mutex_t  max_delay_lock;
- static	uint32_t max_resp_time = 0;
- static	int request_count = 0;
-+opt_t opt;
- 
- int
- main (int argc, char **argv)
-diff --git a/src/scancel/scancel.h b/src/scancel/scancel.h
-index a0c9d8a829..678bcd7616 100644
---- src/scancel/scancel.h
-+++ src/scancel/scancel.h
-@@ -75,7 +75,7 @@ typedef struct scancel_options {
- 	bool *job_pend;		/* Set fi job is pending	*/
- } opt_t;
- 
--opt_t opt;
-+extern opt_t opt;
- 
- /* process options:
-  * 1. set defaults

sysutils/slurm-wlm/files/patch-configure
@@ -1,0 +1,38 @@
+--- configure.orig	2020-11-30 15:56:36 UTC
++++ configure
+@@ -24075,8 +24075,24 @@ fi
+ 
+ 
+ 	x_ac_lua_pkg_name="lua"
+-	#check for 5.3 then 5.2 then 5.1
+-	if test -n "$PKG_CONFIG" && \
++	#check for 5.4 then 5.3 then 5.2 then 5.1
++if test -n "$PKG_CONFIG" && \
++    { { $as_echo "$as_me:${as_lineno-$LINENO}: \$PKG_CONFIG --exists --print-errors \"lua5.4\""; } >&5
++  ($PKG_CONFIG --exists --print-errors "lua5.4") 2>&5
++  ac_status=$?
++  $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
++  test $ac_status = 0; }; then
++  x_ac_lua_pkg_name=lua5.4
++else
++  if test -n "$PKG_CONFIG" && \
++    { { $as_echo "$as_me:${as_lineno-$LINENO}: \$PKG_CONFIG --exists --print-errors \"lua-5.4\""; } >&5
++  ($PKG_CONFIG --exists --print-errors "lua-5.4") 2>&5
++  ac_status=$?
++  $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
++  test $ac_status = 0; }; then
++  x_ac_lua_pkg_name=lua-5.4
++else
++  if test -n "$PKG_CONFIG" && \
+     { { $as_echo "$as_me:${as_lineno-$LINENO}: \$PKG_CONFIG --exists --print-errors \"lua5.3\""; } >&5
+   ($PKG_CONFIG --exists --print-errors "lua5.3") 2>&5
+   ac_status=$?
+@@ -24125,6 +24141,8 @@ else
+   x_ac_lua_pkg_name=lua-5.1
+ else
+   x_ac_lua_pkg_name="lua >= 5.1"
++fi
++fi
+ fi
+ fi
+ fi

sysutils/slurm-wlm/files/patch-src_plugins_select_cons__tres_job__test.c
@@ -1,26 +1,0 @@
---- src/plugins/select/cons_tres/job_test.c.orig	2020-03-26 21:44:05 UTC
-+++ src/plugins/select/cons_tres/job_test.c
-@@ -41,23 +41,6 @@
- 
- #define _DEBUG 0	/* Enables module specific debugging */
- 
--/*
-- * These symbols are defined here so when we link with something other
-- * than the slurmctld we will have these symbols defined. They will get
-- * overwritten when linking with the slurmctld.
-- */
--#if defined (__APPLE__)
--extern slurmctld_config_t slurmctld_config __attribute__((weak_import));
--extern bitstr_t *idle_node_bitmap __attribute__((weak_import));
--extern node_record_t *node_record_table_ptr __attribute__((weak_import));
--extern List job_list __attribute__((weak_import));
--#else
--slurmctld_config_t slurmctld_config;
--bitstr_t *idle_node_bitmap;
--node_record_t *node_record_table_ptr;
--List job_list;
--#endif
--
- typedef struct node_weight_struct {
- 	bitstr_t *node_bitmap;	/* bitmap of nodes with this weight */
- 	uint32_t weight;	/* priority of node for scheduling work on */