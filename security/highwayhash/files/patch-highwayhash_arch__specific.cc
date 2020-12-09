0be729d74350f672eacdf631ec1bbd0358ff9b60
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-12-09 16:29:49 +0000

    security/highwayhash: remove obviously wrong hunk that I somehow missed before
    The function reads TimeBase frequency, not the CPU frequency.

--- security/highwayhash/files/patch-highwayhash_arch__specific.cc
+++ security/highwayhash/files/patch-highwayhash_arch__specific.cc
@@ -10,12 +10,3 @@
  #endif
  #endif
  
-@@ -174,7 +174,7 @@ double InvariantTicksPerSecond() {
- #elif __FreeBSD__
-   static double cycles_per_second = 0;
-   size_t length = sizeof(cycles_per_second);
--  sysctlbyname("kern.timecounter.tc.timebase.frequency", &cycles_per_second,
-+  sysctlbyname("dev.cpu.0.freq", &cycles_per_second,
-                &length, NULL, 0);
- #endif
-   return cycles_per_second;
