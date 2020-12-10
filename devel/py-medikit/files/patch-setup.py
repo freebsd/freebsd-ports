97b3a3a1942c44b3e5963f93cf63a21e5dc8f9e9
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2020-12-10 21:34:27 +0000

    Update to 0.7.5
    Changes:	https://github.com/python-medikit/medikit/blob/master/docs/changelog.rst

--- devel/py-medikit/files/patch-setup.py
+++ devel/py-medikit/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2019-09-10 15:03:00 UTC
+--- setup.py.orig	2020-04-04 17:34:58 UTC
 +++ setup.py
 @@ -55,7 +55,7 @@ setup(
      include_package_data=True,
      install_requires=[
-         'git-semver ~= 0.3.1', 'jinja2 ~= 2.10', 'mondrian ~= 0.8',
--        'packaging ~= 19.0', 'pip-tools ~= 4.0', 'semantic-version ~= 2.8',
-+        'packaging >= 19.0', 'pip-tools ~= 4.0', 'semantic-version ~= 2.8',
+         'git-semver ~= 0.3.2', 'jinja2 ~= 2.10', 'mondrian ~= 0.8',
+-        'packaging ~= 20.0', 'pip-tools ~= 4.5.0', 'semantic_version < 2.7',
++        'packaging ~= 20.0', 'pip-tools ~= 4.5.0', 'semantic_version ~= 2.8',
          'stevedore ~= 1.28', 'whistle ~= 1.0', 'yapf ~= 0.20'
      ],
      extras_require={
