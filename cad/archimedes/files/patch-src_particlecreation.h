60e4d17b87f28db35b5b68464c0d65a386652491
Yuri Victorovich <yuri@FreeBSD.org>
2020-12-10 05:25:32 +0000

    New port: cad/archimedes: Semiconductor device simulation software

---
+++ cad/archimedes/files/patch-src_particlecreation.h
@@ -0,0 +1,11 @@
+--- src/particlecreation.h.orig	2020-12-10 05:08:02 UTC
++++ src/particlecreation.h
+@@ -34,7 +34,7 @@
+ // edge = 2 Upper edge
+ // edge = 3 Left edge
+ 
+-inline
++//inline
+ void
+ creation(int i,real t,int edge)
+ {
