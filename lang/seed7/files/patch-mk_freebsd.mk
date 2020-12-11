db7fc24e0e8f2af062f8a2041d0522e3fb0d8833
Pietro Cerutti <gahr@FreeBSD.org>
2020-12-11 17:29:54 +0000

    lang/seed7: update to 05_20201208
    Relnotes:	https://github.com/ThomasMertes/seed7/blob/a19763a/doc/chlog.txt

--- lang/seed7/files/patch-mk_freebsd.mk
+++ lang/seed7/files/patch-mk_freebsd.mk
@@ -1,10 +1,10 @@
---- mk_freebsd.mk.orig	2020-04-15 07:58:59.000000000 +0000
-+++ mk_freebsd.mk	2020-05-06 10:16:02.032695000 +0000
-@@ -154,6 +154,7 @@
+--- mk_freebsd.mk.orig	2020-05-21 06:01:46 UTC
++++ mk_freebsd.mk
+@@ -153,6 +153,7 @@ chkccomp.h:
  	echo "#define SQL_SERVER_LIBRARY_PATH \"-L%%LOCALBASE%%/lib\"" >> chkccomp.h
  	echo "#define TDS_INCLUDE_OPTIONS \"-I%%LOCALBASE%%/include\"" >> chkccomp.h
  	echo "#define TDS_LIBRARY_PATH \"-L%%LOCALBASE%%/lib\"" >> chkccomp.h
 +	echo "#define X11_LIBRARY_PATH \"%%LOCALBASE%%/lib\"" >> chkccomp.h
  
- version.h: chkccomp.h
- 	echo "#define PATH_DELIMITER '/'" > version.h
+ base.h:
+ 	echo "#define PATH_DELIMITER '/'" > base.h
