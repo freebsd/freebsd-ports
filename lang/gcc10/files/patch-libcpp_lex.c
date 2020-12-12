0670ef14f1e392b7bbb02737f96d512e9b6597f3
Gerald Pfeifer <gerald@FreeBSD.org>
2020-12-12 22:06:42 +0000

    Add a bit of meta data so that everyone possibly touching this has sufficient background.

--- lang/gcc10/files/patch-libcpp_lex.c
+++ lang/gcc10/files/patch-libcpp_lex.c
@@ -1,3 +1,12 @@
+This is
+
+  commit cd547f0ddcd3a54e5b73bcda5ac0f0c46808db8b
+  Author: Jakub Jelinek <jakub@redhat.com>
+  Date:   Sat Sep 26 10:07:41 2020 +0200
+
+which is on the GCC 10 release branch and thus part of lang/gcc10-devel;
+we should automatically get it with GCC 10.3.
+
 --- libcpp/lex.c.orig	2020-11-02 10:24:50 UTC
 +++ libcpp/lex.c
 @@ -531,11 +531,11 @@ init_vectorized_lexer (void)
