19066be21b2109d5c575e4d9b27826bfbe0725a7
Mikael Urankar <mikael@FreeBSD.org>
2020-12-12 18:51:33 +0000

    lang/rust: Fix build with libressl 3.2.3
    PR:		251766
    Submitted by:	John Kennedy

--- lang/rust/files/patch-vendor_openssl-sys_build_main.rs
+++ lang/rust/files/patch-vendor_openssl-sys_build_main.rs
@@ -1,10 +1,10 @@
---- vendor/openssl-sys/build/main.rs.ORIG	2020-10-07 02:33:31.000000000 -0700
-+++ vendor/openssl-sys/build/main.rs	2020-10-23 20:45:37.047602000 -0700
+--- vendor/openssl-sys/build/main.rs.orig	2020-11-16 06:27:50.000000000 -0800
++++ vendor/openssl-sys/build/main.rs	2020-12-11 12:47:11.685331000 -0800
 @@ -221,6 +221,7 @@
              (3, 1, 0) => ('3', '1', '0'),
              (3, 1, _) => ('3', '1', 'x'),
              (3, 2, 0) => ('3', '2', '0'),
-+            (3, 2, 2) => ('3', '2', '2'),
++            (3, 2, _) => ('3', '2', 'x'),
              _ => version_error(),
          };
  
@@ -13,7 +13,7 @@
  
  This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
 -through 3.2.0, but a different version of OpenSSL was found. The build is now aborting
-+through 3.2.2, but a different version of OpenSSL was found. The build is now aborting
++through 3.2.x, but a different version of OpenSSL was found. The build is now aborting
  due to this version mismatch.
  
  "
