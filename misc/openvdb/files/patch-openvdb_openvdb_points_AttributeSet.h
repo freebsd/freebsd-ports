cbb085f1dbfcdd055fd3786b173eef01b6632f00
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-12-14 11:09:51 +0000

    misc/openvdb: fix build on GCC architectures
    /wrkdirs/usr/ports/misc/openvdb/work/openvdb-7.1.0/openvdb/points/AttributeSet.h:461:60:
    error: 'CHAR_BIT' was not declared in this scope
      461 |     static size_t groupBits() { return sizeof(GroupType) * CHAR_BIT; }
          |                                                            ^~~~~~~~
    /wrkdirs/usr/ports/misc/openvdb/work/openvdb-7.1.0/openvdb/points/AttributeSet.h:1:1:
    note: 'CHAR_BIT' is defined in header '<climits>'; did you forget to '#include <climits>'?
      +++ |+#include <climits>
        1 | // Copyright Contributors to the OpenVDB Project

---
+++ misc/openvdb/files/patch-openvdb_openvdb_points_AttributeSet.h
@@ -0,0 +1,10 @@
+--- openvdb/openvdb/points/AttributeSet.h.orig	2020-12-14 01:40:20 UTC
++++ openvdb/openvdb/points/AttributeSet.h
+@@ -14,6 +14,7 @@
+ #include <openvdb/version.h>
+ #include <openvdb/MetaMap.h>
+ 
++#include <climits>
+ #include <limits>
+ #include <memory>
+ #include <vector>
