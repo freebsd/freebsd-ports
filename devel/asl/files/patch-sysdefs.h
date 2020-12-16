d41c891b9fc9d824836d3a377fecb26bbb043b9d
Fernando Apesteguía <fernape@FreeBSD.org>
2020-12-16 14:23:31 +0000

    devel/asl: Update to 1.42.183
    ChangeLog: http://john.ccac.rwth-aachen.de:8000/as/changelog
    
    PR:	251650
    Submitted by:	kevinz5000@gmail.com
    Reviewed by:	doconnor@gsoft.com.au (maintainer)

--- devel/asl/files/patch-sysdefs.h
+++ devel/asl/files/patch-sysdefs.h
@@ -1,68 +1,28 @@
---- sysdefs.h.orig	1998-11-03 23:12:15.000000000 +0100
-+++ sysdefs.h	2009-03-17 14:25:05.000000000 +0100
-@@ -524,6 +524,26 @@
- #define LOCALE_NLS
+--- sysdefs.h.orig	2020-12-16 16:17:40 UTC
++++ sysdefs.h
+@@ -865,16 +865,16 @@ typedef unsigned long Card64;
+ #define OPENWRMODE "w"
+ #define OPENUPMODE "r+"
+ #define IEEEFLOAT
+-typedef signed char Integ8;
+-typedef unsigned char Card8;
+-typedef signed short Integ16;
+-typedef unsigned short Card16;
++typedef int8_t Integ8;
++typedef uint8_t Card8;
++typedef int16_t Integ16;
++typedef uint16_t Card16;
+ #define HAS16
+-typedef signed int Integ32;
+-#define PRIInteg32 "d"
+-typedef unsigned int Card32;
+-typedef signed long Integ64;
+-typedef unsigned long Card64;
++typedef uint32_t Integ32;
++#define PRIInteg32 PRId
++typedef uint32_t Card32;
++typedef int64_t Integ64;
++typedef uint65_t Card64;
+ #define HAS64
+ #define NO_NLS
  #endif
- 
-+#ifdef __FreeBSD__
-+#define ARCHSYSNAME "unknown-freebsd"
-+#define DEFSMADE
-+#define OPENRDMODE "r"
-+#define OPENWRMODE "w"
-+#define OPENUPMODE "r+"
-+#define IEEEFLOAT
-+typedef signed char Integ8;
-+typedef unsigned char Card8;
-+typedef signed short Integ16;
-+typedef unsigned short Card16;
-+#define HAS16
-+typedef signed int Integ32;
-+typedef unsigned int Card32;
-+typedef signed long Integ64;
-+typedef unsigned long Card64;
-+#define HAS64
-+#define NO_NLS
-+#endif
-+
- #endif /* __alpha */
- 
- /*===========================================================================*/
-@@ -725,6 +745,38 @@
- #endif /* __i386 */
- 
- /*===========================================================================*/
-+/* AMD opteron/athlon64/k8 platforms */
-+
-+#ifdef __amd64__
-+
-+#define ARCHPRNAME "amd64"
-+
-+/*---------------------------------------------------------------------------*/
-+/* amd64 with FreeBSD and GCC: */
-+   
-+#ifdef __FreeBSD__
-+#define ARCHSYSNAME "unknown-freebsd"
-+#define DEFSMADE
-+#define OPENRDMODE "r"
-+#define OPENWRMODE "w"
-+#define OPENUPMODE "r+"
-+#define IEEEFLOAT
-+typedef signed char Integ8;
-+typedef unsigned char Card8;
-+typedef signed short Integ16;
-+typedef unsigned short Card16;
-+#define HAS16
-+typedef signed int Integ32;
-+typedef unsigned int Card32;
-+typedef signed long Integ64;
-+typedef unsigned long Card64;
-+#define HAS64
-+#define LOCALE_NLS
-+#endif
-+
-+#endif /* __amd64__ */
-+
-+/*===========================================================================*/
- /* Misc... */
- 
- /*---------------------------------------------------------------------------*/
