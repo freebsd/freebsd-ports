9932c6b2bb986c0ae500470f078ac6152cdadd38
Koichiro Iwao <meta@FreeBSD.org>
2020-12-17 02:04:47 +0000

    net/xrdp-devel: resurrect and update to 0.9.14

---
+++ net/xrdp-devel/files/startwm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# This script is an example. Edit this to suit your needs.
+# If ${HOME}/startwm.sh exists, xrdp-sesman will execute it instead of this.
+
+#### set environment variables here if you want
+# export LANG=en_US.UTF-8
+
+#### start desktop environment
+# exec gnome-session
+# exec mate-session
+# exec start-lumina-desktop
+# exec startkde
+# exec startxfce4
+exec xterm
