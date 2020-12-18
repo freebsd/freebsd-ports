45bec4845a3c20292c1bacbe136c49a9b3d74673
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-12-18 14:50:39 +0000

    devel/py-pyinstaller: fix build on powerpc64
    Include sys/cdefs.h to make __BEGIN_DECLS available.
    
    Approved by:	tier 2 blanket

--- devel/py-pyinstaller/files/patch-bootloader_src_main.c
+++ devel/py-pyinstaller/files/patch-bootloader_src_main.c
@@ -1,17 +1,18 @@
---- bootloader/src/main.c.orig	2018-09-22 05:16:07 UTC
+--- bootloader/src/main.c.orig	2019-07-09 19:14:04 UTC
 +++ bootloader/src/main.c
-@@ -16,6 +16,10 @@
+@@ -16,6 +16,11 @@
   * main: For OS X and Linux
   */
  
 +#ifdef __FreeBSD__
++    #include <sys/cdefs.h>
 +    #include <floatingpoint.h>
 +    #include <ieeefp.h>
 +#endif
  #ifdef _WIN32
      #include <windows.h>
      #include <wchar.h>
-@@ -33,9 +37,6 @@
+@@ -33,9 +38,6 @@
  #include "pyi_global.h"
  #include "pyi_win32_utils.h"
  
@@ -21,7 +22,7 @@
  
  #if defined(_WIN32)
      #define MS_WINDOWS
-@@ -87,19 +88,12 @@ main(int argc, char **argv)
+@@ -87,19 +89,12 @@ main(int argc, char **argv)
  {
      int res;
  
