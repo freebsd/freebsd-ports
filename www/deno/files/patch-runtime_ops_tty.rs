d06c1cb92d9e3d34409cd12d6ebe76ec639b1ce5
Mikael Urankar <mikael@FreeBSD.org>
2020-12-20 13:20:39 +0000

    www/deno: Update to 1.6.1
    Changelog: https://github.com/denoland/deno/releases/tag/v1.6.1

--- www/deno/files/patch-runtime_ops_tty.rs
+++ www/deno/files/patch-runtime_ops_tty.rs
@@ -1,11 +1,11 @@
---- cli/ops/tty.rs.orig	2020-09-18 21:28:11 UTC
-+++ cli/ops/tty.rs
-@@ -311,7 +311,7 @@ fn op_console_size(
+--- runtime/ops/tty.rs.orig	2020-12-14 21:25:05 UTC
++++ runtime/ops/tty.rs
+@@ -315,7 +315,7 @@ fn op_console_size(
          let fd = std_file.as_raw_fd();
          unsafe {
            let mut size: libc::winsize = std::mem::zeroed();
 -          if libc::ioctl(fd, libc::TIOCGWINSZ, &mut size as *mut _) != 0 {
 +          if libc::ioctl(fd, libc::TIOCGWINSZ.into(), &mut size as *mut _) != 0 {
-             return Err(last_os_error());
+             return Err(Error::last_os_error().into());
            }
  
