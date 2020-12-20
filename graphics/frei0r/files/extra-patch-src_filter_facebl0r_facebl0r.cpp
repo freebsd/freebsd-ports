cdd929a0b99258aa6ae8c1a6ace113d7e8280585
Tobias C. Berner <tcberner@FreeBSD.org>
2020-12-20 09:06:19 +0000

    graphics/frei0r-plugins-opencv: prepare for OpenCV 3.4.7
    PR:		237135
    Obtained from:	https://github.com/dyne/frei0r/commit/b0a06d52e39438fae2afbf98bafe6c794d13b83e

---
+++ graphics/frei0r/files/extra-patch-src_filter_facebl0r_facebl0r.cpp
@@ -0,0 +1,10 @@
+--- src/filter/facebl0r/facebl0r.cpp.orig	2020-12-20 08:53:32 UTC
++++ src/filter/facebl0r/facebl0r.cpp
+@@ -19,6 +19,7 @@
+ #include <stdlib.h>
+ #include <opencv/cv.h>
+ #include <opencv/highgui.h>
++#include <opencv2/imgproc.hpp>
+ 
+ #include <frei0r.hpp>
+ #include <frei0r_math.h>
