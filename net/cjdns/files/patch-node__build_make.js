cd9d58d0f46849802afabaddbfab62359ea2c31b
Yuri Victorovich <yuri@FreeBSD.org>
2020-12-20 19:50:28 +0000

    net/cjdns: Update 21 -> 21.1

--- net/cjdns/files/patch-node__build_make.js
+++ net/cjdns/files/patch-node__build_make.js
@@ -1,11 +1,18 @@
---- node_build/make.js.orig	2018-07-12 12:41:34 UTC
+--- node_build/make.js.orig	2020-10-05 21:46:19 UTC
 +++ node_build/make.js
-@@ -29,7 +29,7 @@ var GCC = process.env['CC'];
+@@ -27,7 +27,7 @@ const GetVersion = require('./GetVersion');
+ 
  var CFLAGS = process.env['CFLAGS'];
  var LDFLAGS = process.env['LDFLAGS'];
- 
 -var NO_MARCH_FLAG = ['arm', 'ppc', 'ppc64'];
 +var NO_MARCH_FLAG = ['arm', 'arm64', 'ppc', 'ppc64'];
  
- if (GCC) {
-     // Already specified.
+ Builder.configure({
+     systemName:     process.env['SYSTEM'] || process.platform,
+@@ -471,4 +471,4 @@ Builder.configure({
+     console.log('\x1b[1;31mFailed to build cjdns.\x1b[0m');
+     process.exit(1);
+ 
+-});
+\ No newline at end of file
++});
