bd5a415767e994a7e91270887bd9b2b5605ab08b
Jochen Neumeister <joneum@FreeBSD.org>
2020-12-23 20:12:07 +0000

    Update 3rd party Module nchan
    PR:		252050
    Sponsored by:	Netzkommune GmbH

--- www/nginx/files/extra-patch-nchan-store-private.h
+++ www/nginx/files/extra-patch-nchan-store-private.h
@@ -1,5 +1,5 @@
---- ../nchan-1.1.14/src/store/memory/store-private.h.orig	2020-09-20 13:11:33.781569000 +0200
-+++ ../nchan-1.1.14/src/store/memory/store-private.h	2020-09-20 13:11:48.679855000 +0200
+--- ../nchan-1.2.7/src/store/memory/store-private.h.orig	2020-09-20 13:11:33.781569000 +0200
++++ ../nchan-1.2.7/src/store/memory/store-private.h	2020-09-20 13:11:48.679855000 +0200
 @@ -172,6 +172,6 @@
  void memstore_chanhead_release(memstore_channel_head_t *ch, char *label);
  void memstore_chanhead_reserve(memstore_channel_head_t *ch, const char *label);
