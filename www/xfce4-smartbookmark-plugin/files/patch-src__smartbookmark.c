a6bf09f7eb49442f86ecdc46b628c6407c967c7e
Guido Falsi <madpilot@FreeBSD.org>
2020-12-24 21:19:38 +0000

    Update xfce4-smartbookmark-plugin to 0.5.2

--- www/xfce4-smartbookmark-plugin/files/patch-src__smartbookmark.c
+++ www/xfce4-smartbookmark-plugin/files/patch-src__smartbookmark.c
@@ -1,10 +1,10 @@
---- src/smartbookmark.c.orig	2013-02-18 21:14:36 UTC
+--- src/smartbookmark.c.orig	2020-12-24 16:01:51 UTC
 +++ src/smartbookmark.c
-@@ -182,10 +182,10 @@ static t_search *search_new(XfcePanelPlu
-     gtk_container_add(GTK_CONTAINER(align), search->box);
+@@ -173,10 +173,10 @@ static t_search *search_new(XfcePanelPlugin *plugin)
+     gtk_widget_set_valign(GTK_WIDGET(search->box), GTK_ALIGN_CENTER);
  
      /* default options */
--    search->url = "http://bugs.debian.org/";
+-    search->url = "https://bugs.debian.org/";
 -    search->label_text = "BTS";
 -    search->size = 5;
 -    search->hide_label = FALSE;
@@ -15,11 +15,11 @@
      /* read config file options */
      filename = xfce_panel_plugin_save_location(plugin, TRUE);
      search_read_config(search, filename);
-@@ -218,10 +218,10 @@ static void search_read_config(t_search 
+@@ -209,10 +209,10 @@ static void search_read_config(t_search *search, const
      if( (rcfile = xfce_rc_simple_open(filename, TRUE) ))
      {
          xfce_rc_set_group(rcfile, NULL);
--        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","http://bugs.debian.org/"));
+-        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","https://bugs.debian.org/"));
 -        search->label_text = g_strdup(xfce_rc_read_entry(rcfile,"value","DBS"));
 -        search->size = xfce_rc_read_int_entry(rcfile, "size", 5);
 -        search->hide_label = xfce_rc_read_bool_entry(rcfile, "hidelabel", FALSE);
