2638058d728b14eed97b92fa3f6111e7d0d31fcb
Piotr Kubaj <pkubaj@FreeBSD.org>
2020-12-24 13:50:01 +0000

    games/libretro-play: fix build on powerpc64
    SSE is not available on powerpc64.
    
    Patches are already sent upstream.
    
    Approved by:	tier 2 blanket

--- games/libretro-play/files/patch-deps_Dependencies_cmake-modules_Header.cmake
+++ games/libretro-play/files/patch-deps_Dependencies_cmake-modules_Header.cmake
@@ -1,6 +1,13 @@
 --- deps/Dependencies/cmake-modules/Header.cmake.orig	2020-07-29 22:03:00 UTC
 +++ deps/Dependencies/cmake-modules/Header.cmake
-@@ -31,8 +31,8 @@ if(CMAKE_CURRENT_SOURCE_DIR STREQUAL "${CMAKE_SOURCE_D
+@@ -27,12 +27,15 @@ if(CMAKE_CURRENT_SOURCE_DIR STREQUAL "${CMAKE_SOURCE_D
+ 		if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "^(aarch64.*|AARCH64.*)")
+ 			message("-- Arch: aarch64 --")
+ 			set(TARGET_PLATFORM_UNIX_AARCH64 TRUE)
++		elseif(${CMAKE_SYSTEM_PROCESSOR} MATCHES "^(ppc64.*|powerpc64.*)")
++			message("-- Arch: powerpc64 --")
++			set(TARGET_PLATFORM_UNIX_POWERPC64 TRUE)
+ 		endif()
  
  	endif()
  	
