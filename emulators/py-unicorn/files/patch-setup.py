fed1690859b80709afc90dcb4c5712327c1be99f
Antoine Brodin <antoine@FreeBSD.org>
2020-12-27 20:11:58 +0000

    Update to 1.0.2

--- emulators/py-unicorn/files/patch-setup.py
+++ emulators/py-unicorn/files/patch-setup.py
@@ -1,15 +1,15 @@
 # Do not bundle libunicorn.so
 
---- setup.py.orig	2020-05-29 03:56:24 UTC
+--- setup.py.orig	2020-09-21 18:11:43 UTC
 +++ setup.py
-@@ -289,11 +289,4 @@ setup(
+@@ -281,11 +281,4 @@ setup(
          'Programming Language :: Python :: 3',
      ],
      requires=['ctypes'],
--    cmdclass=cmdclass,
--    zip_safe=True,
+-    cmdclass={'build': custom_build, 'develop': custom_develop, 'sdist': custom_sdist, 'bdist_egg': custom_bdist_egg},
+-    zip_safe=False,
 -    include_package_data=True,
--    is_pure=True,
+-    is_pure=False,
 -    package_data={
 -        'unicorn': ['lib/*', 'include/unicorn/*']
 -    }
