71a61cc61902bf287657802b60091d0dd9a44591
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2020-12-29 22:34:29 +0000

    security/sslproxy: Update version 0.7.0=>0.8.2
    Relnotes:	https://github.com/sonertari/SSLproxy/blob/master/NEWS.md

---
+++ security/sslproxy/files/patch-Mk_main.mk
@@ -0,0 +1,11 @@
+--- Mk/main.mk.orig	2020-12-15 14:12:50 UTC
++++ Mk/main.mk
+@@ -201,7 +201,7 @@ endif
+ 
+ PREFIX?=	/usr/local
+ MANDIR?=	share/man
+-EXAMPLESDIR?=	share/examples
++EXAMPLESDIR?=	etc
+ 
+ INSTALLUID?=	0
+ INSTALLGID?=	0
