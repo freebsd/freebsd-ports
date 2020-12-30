767d3c1c35f0d4d7c16d4f99a9f812c051daa595
Yuri Victorovich <yuri@FreeBSD.org>
2020-12-30 17:54:19 +0000

    math/py-or-tools: Update 7.7 -> 8.1

--- math/py-or-tools/files/patch-cmake_python.cmake
+++ math/py-or-tools/files/patch-cmake_python.cmake
@@ -1,4 +1,4 @@
---- cmake/python.cmake.orig	2020-08-03 18:30:02 UTC
+--- cmake/python.cmake.orig	2020-12-09 16:30:21 UTC
 +++ cmake/python.cmake
 @@ -23,7 +23,7 @@ if(UNIX AND NOT APPLE)
  endif()
@@ -8,4 +8,13 @@
 +find_package(Python ${FREEBSD_PYTHON_VER} EXACT REQUIRED COMPONENTS Interpreter Development)
  
  if(Python_VERSION VERSION_GREATER_EQUAL 3)
-   list(APPEND CMAKE_SWIG_FLAGS "-py3")
+   list(APPEND CMAKE_SWIG_FLAGS "-py3" "-DPY3")
+@@ -206,7 +206,7 @@ if(BUILD_TESTING)
+     COMMAND ${VENV_EXECUTABLE} ${VENV_DIR}
+     # Must not call it in a folder containing the setup.py otherwise pip call it
+     # (i.e. "python setup.py bdist") while we want to consume the wheel package
+-    COMMAND ${VENV_Python_EXECUTABLE} -m pip install --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PROJECT_NAME}
++    COMMAND ${VENV_Python_EXECUTABLE} -m pip install --no-index --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PROJECT_NAME}
+     COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/test.py.in ${VENV_DIR}/test.py
+     BYPRODUCTS ${VENV_DIR}
+     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} VERBATIM)
--- math/py-or-tools/files/patch-ortools_util_zvector.h
+++ math/py-or-tools/files/patch-ortools_util_zvector.h
@@ -1,7 +1,7 @@
---- ortools/util/zvector.h.orig	2020-07-20 23:36:37 UTC
+--- ortools/util/zvector.h.orig	2020-12-09 16:30:21 UTC
 +++ ortools/util/zvector.h
 @@ -17,7 +17,11 @@
- #if defined(__APPLE__) && defined(__GNUC__)
+ #if (defined(__APPLE__) || defined(__FreeBSD__)) && defined(__GNUC__)
  #include <machine/endian.h>
  #elif !defined(_MSC_VER)
 +#if defined(__FreeBSD__)
