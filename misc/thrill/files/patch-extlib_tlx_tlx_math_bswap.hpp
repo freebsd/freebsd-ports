91f8e2e1761368e8c79e38b2e358b051c745d6eb
Yuri Victorovich <yuri@FreeBSD.org>
2020-12-31 18:59:28 +0000

    misc/thrill: Fix build on big-endian architectures
    PR:		252290
    Submitted by:	pkubaj

--- misc/thrill/files/patch-extlib_tlx_tlx_math_bswap.hpp
+++ misc/thrill/files/patch-extlib_tlx_tlx_math_bswap.hpp
@@ -1,6 +1,17 @@
---- extlib/tlx/tlx/math/bswap.hpp.orig	2020-12-12 21:48:10 UTC
+--- extlib/tlx/tlx/math/bswap.hpp.orig	2019-10-28 13:53:21 UTC
 +++ extlib/tlx/tlx/math/bswap.hpp
-@@ -35,9 +35,11 @@ static inline uint16_t bswap16_generic(const uint16_t&
+@@ -19,6 +19,10 @@
+ #include <cstdlib>
+ #endif
+ 
++#ifdef __FreeBSD__
++#include <sys/endian.h>
++#endif
++
+ namespace tlx {
+ 
+ //! \addtogroup tlx_math
+@@ -35,9 +39,11 @@ static inline uint16_t bswap16_generic(const uint16_t&
  #if defined(__GNUC__) || defined(__clang__)
  
  //! bswap16 - gcc/clang intrinsic
@@ -12,7 +23,7 @@
  
  #elif defined(_MSC_VER)
  
-@@ -67,9 +69,11 @@ static inline uint32_t bswap32_generic(const uint32_t&
+@@ -67,9 +73,11 @@ static inline uint32_t bswap32_generic(const uint32_t&
  #if defined(__GNUC__) || defined(__clang__)
  
  //! bswap32 - gcc/clang intrinsic
@@ -24,7 +35,7 @@
  
  #elif defined(_MSC_VER)
  
-@@ -105,9 +109,11 @@ static inline uint64_t bswap64_generic(const uint64_t&
+@@ -105,9 +113,11 @@ static inline uint64_t bswap64_generic(const uint64_t&
  #if defined(__GNUC__) || defined(__clang__)
  
  //! bswap64 - gcc/clang intrinsic
