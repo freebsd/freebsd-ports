6b2060a94271e3a79adf6ec454f3b650488c5b89
Yuri Victorovich <yuri@FreeBSD.org>
2021-01-02 00:42:00 +0000

    science/abinit: Update 8.10.3 -> 9.2.2
    PR:		252221
    Submitted by:	thierry

--- science/abinit/files/patch-shared__common__src__02_clib__md5.c
+++ science/abinit/files/patch-shared__common__src__02_clib__md5.c
@@ -1,5 +1,5 @@
---- src/02_clib/md5.c.orig	2017-02-28 12:02:53 UTC
-+++ src/02_clib/md5.c
+--- shared/common/src/02_clib/md5.c.orig	2017-02-28 12:02:53 UTC
++++ shared/common/src/02_clib/md5.c
 @@ -47,7 +47,7 @@
  #ifdef HAVE_MALLOC_MALLOC_H
  #  include <malloc/malloc.h>
