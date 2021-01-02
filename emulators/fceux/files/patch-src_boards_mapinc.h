28ee39466918415876c222d93310195e291bc33a
Rene Ladan <rene@FreeBSD.org>
2021-01-02 14:05:27 +0000

    Remove some expired ports:
    2020-12-31 emulators/fceux: Uses Python 2.7 version of scons, and Python 2.7 which is EOLed upstream
    2020-12-31 emulators/gem5: Uses Python 2.7 version of scons, and Python 2.7 which is EOLed upstream
    2020-12-31 emulators/nonpareil: Uses Python 2.7 version of scons, and Python 2.7 which is EOLed upstream
    2020-12-31 games/freera: Uses Python 2.7 version of scons, and Python 2.7 which is EOLed upstream
    2020-12-31 games/glob2: Uses Python 2.7 version of scons, and Python 2.7 which is EOLed upstream
    2020-12-31 games/marsnomercy: Uses Python 2.7 version of scons, and Python 2.7 which is EOLed upstream
    2020-12-31 games/netpanzer: Uses Python 2.7 version of scons, and Python 2.7 which is EOLed upstream
    2020-12-31 games/pingus: Uses Python 2.7 version of scons, and Python 2.7 which is EOLed upstream
    2020-12-31 graphics/goxel: Uses Python 2.7 version of scons, and Python 2.7 which is EOLed upstream
    2020-12-31 graphics/sk1libs: Uses Python 2.7 which is EOLed upstream
    2020-12-31 graphics/uniconvertor: Uses deprecated version of python

--- emulators/fceux/files/patch-src_boards_mapinc.h
+++
@@ -1,8 +0,0 @@
---- src/boards/mapinc.h.orig	2017-02-26 13:37:11 UTC
-+++ src/boards/mapinc.h
-@@ -9,4 +9,5 @@
- #include "../cheat.h"
- #include "../unif.h"
- #include <stdio.h>
-+#include <stdlib.h>
- #include <string.h>
