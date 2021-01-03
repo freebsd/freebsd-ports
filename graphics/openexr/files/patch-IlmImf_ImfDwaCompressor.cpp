c77c064297efe1486dfdc1841a15df8f6c354230
Matthias Andree <mandree@FreeBSD.org>
2021-01-03 16:57:29 +0000

    graphics/openexr: really commit v2.5.4 update.
    Unfortunately, this was missed with the ilmbase update, and
    causes yet another PORTREVISION bump on all dependent ports.
    
    repeating ilmbase's commit log here:
    
    "Patch release with various bug/sanitizer/security fixes, primarily related to
    reading corrupted input files."
    <https://github.com/AcademySoftwareFoundation/openexr/releases/tag/v2.5.4>
    
    Cherry-pick upstream commit for GCC 11 compatibility:
    <https://github.com/AcademySoftwareFoundation/openexr/commit/a40a6151c4e746eb8f93c7c06992d0ec11289fdc>
    
    Since this changes the patchlevel of the solibs, bump PORTREVISION of dependent
    ports just to be sure.
    (graphics/py-openshadinglanguage is unaltered and .includes the revision bump
    from .../openshadinglanguage.)
    
    MFH:		2021Q1 (this requires discussion by ports-secteam@!)
    
    Reported by:	VVD (IRC, #bsdports)

---
+++ graphics/openexr/files/patch-IlmImf_ImfDwaCompressor.cpp
@@ -0,0 +1,10 @@
+--- IlmImf/ImfDwaCompressor.cpp.orig	2020-12-30 23:09:11 UTC
++++ IlmImf/ImfDwaCompressor.cpp
+@@ -156,6 +156,7 @@
+ #include <cctype>
+ #include <cassert>
+ #include <algorithm>
++#include <limits>
+ 
+ #include <cstddef>
+ 
---
+++ graphics/openexr/files/patch-IlmImf_ImfTiledMisc.cpp
@@ -0,0 +1,10 @@
+--- IlmImf/ImfTiledMisc.cpp.orig	2020-12-30 23:09:11 UTC
++++ IlmImf/ImfTiledMisc.cpp
+@@ -45,6 +45,7 @@
+ #include <ImfChannelList.h>
+ #include <ImfTileDescription.h>
+ #include <algorithm>
++#include <limits>
+ 
+ #include "ImfNamespace.h"
+ 
