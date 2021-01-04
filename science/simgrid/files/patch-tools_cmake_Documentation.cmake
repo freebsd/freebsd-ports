fbe4bdc450fe11dc53a9c7a55d485a898e2acc3c
Yuri Victorovich <yuri@FreeBSD.org>
2021-01-04 22:59:09 +0000

    science/simgrid: Update 3.25 -> 3.26

--- science/simgrid/files/patch-tools_cmake_Documentation.cmake
+++
@@ -1,8 +0,0 @@
---- tools/cmake/Documentation.cmake.orig	2019-05-16 09:00:44 UTC
-+++ tools/cmake/Documentation.cmake
-@@ -137,4 +137,4 @@ install(FILES
-   ${CMAKE_HOME_DIRECTORY}/docs/manpages/smpif90.1
-   ${CMAKE_HOME_DIRECTORY}/docs/manpages/smpiff.1
-   ${CMAKE_HOME_DIRECTORY}/docs/manpages/smpirun.1
--  DESTINATION $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/share/man/man1)
-+  DESTINATION $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/man/man1)
