862860932dcb1bba261f463f33226180136c5389
Dima Panov <fluffy@FreeBSD.org>
2021-01-05 01:40:48 +0000

    devel/eric6: update to 20.1 release

--- devel/eric6/bsd.eric6.mk
+++ devel/eric6/bsd.eric6.mk
@@ -6,7 +6,7 @@
 # german/eric6, russian/eric6, devel/es-eric6
 
 PORTNAME=	eric6
-PORTVERSION=	20.12.1
+PORTVERSION=	21.1
 PORTREVISION?=	0
 MASTER_SITES=	SF/eric-ide/${PORTNAME}/stable/${PORTVERSION}
 WRKSRC=		${WRKDIR}/${PORTNAME}-${PORTVERSION}
