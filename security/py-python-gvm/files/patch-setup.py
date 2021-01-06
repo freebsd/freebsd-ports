21640666a6101ed83263201cb885fcdca4a58ad3
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2021-01-06 08:24:12 +0000

    - New port: security/py-python-gvm
    The Greenbone Vulnerability Management Python API library (python-gvm) is a
    collection of APIs that help with remote controlling a Greenbone Security
    Manager (GSM) appliance and its underlying Greenbone Vulnerability Manager
    (GVM).
    
    The library essentially abstracts accessing the communication protocols
    Greenbone Management Protocol (GMP) and Open Scanner Protocol (OSP)
    
    WWW: https://github.com/greenbone/python-gvm

---
+++ security/py-python-gvm/files/patch-setup.py
@@ -0,0 +1,34 @@
+--- setup.py.orig	2021-01-04 18:38:41.394413000 -0500
++++ setup.py	2021-01-04 18:38:57.320954000 -0500
+@@ -8,30 +8,7 @@
+  'gvm.protocols.gmpv214',
+  'gvm.protocols.gmpv7',
+  'gvm.protocols.gmpv8',
+- 'gvm.protocols.gmpv9',
+- 'tests',
+- 'tests.connections',
+- 'tests.protocols',
+- 'tests.protocols.gmp',
+- 'tests.protocols.gmpv208',
+- 'tests.protocols.gmpv208.testcmds',
+- 'tests.protocols.gmpv208.testtypes',
+- 'tests.protocols.gmpv214',
+- 'tests.protocols.gmpv214.testcmds',
+- 'tests.protocols.gmpv214.testtypes',
+- 'tests.protocols.gmpv7',
+- 'tests.protocols.gmpv7.testcmds',
+- 'tests.protocols.gmpv7.testtypes',
+- 'tests.protocols.gmpv8',
+- 'tests.protocols.gmpv8.testcmds',
+- 'tests.protocols.gmpv8.testtypes',
+- 'tests.protocols.gmpv9',
+- 'tests.protocols.gmpv9.testcmds',
+- 'tests.protocols.gmpv9.testtypes',
+- 'tests.protocols.osp',
+- 'tests.transforms',
+- 'tests.utils',
+- 'tests.xml']
++ 'gvm.protocols.gmpv9']
+ 
+ package_data = \
+ {'': ['*']}
