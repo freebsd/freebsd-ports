a798515415faedb3e670d78108a9b2597902b5c5
Mikael Urankar <mikael@FreeBSD.org>
2021-01-06 10:13:58 +0000

    lang/rust: Update to 1.49.0
    Changes: https://github.com/rust-lang/rust/releases/tag/1.49.0
    
    Tested by:	mikael, pkubaj
    Differential Revision:	https://reviews.freebsd.org/D27940

--- lang/rust/files/powerpc64-elfv2/patch-compiler_rustc__target_src_spec_powerpc64__unknown__freebsd.rs
+++ lang/rust/files/powerpc64-elfv2/patch-compiler_rustc__target_src_spec_powerpc64__unknown__freebsd.rs
@@ -1,14 +1,14 @@
---- compiler/rustc_target/src/spec/powerpc64_unknown_freebsd.rs.orig	2020-07-15 11:31:58 UTC
+--- compiler/rustc_target/src/spec/powerpc64_unknown_freebsd.rs.orig	2020-12-29 04:03:08 UTC
 +++ compiler/rustc_target/src/spec/powerpc64_unknown_freebsd.rs
-@@ -17,6 +17,10 @@ pub fn target() -> TargetResult {
-         target_env: String::new(),
-         target_vendor: "unknown".to_string(),
-         linker_flavor: LinkerFlavor::Gcc,
--        options: TargetOptions { target_mcount: "_mcount".to_string(), ..base },
+@@ -11,6 +11,10 @@ pub fn target() -> Target {
+         pointer_width: 64,
+         data_layout: "E-m:e-i64:64-n32:64".to_string(),
+         arch: "powerpc64".to_string(),
+-        options: TargetOptions { endian: "big".to_string(), mcount: "_mcount".to_string(), ..base },
 +        options: TargetOptions {
-+            target_mcount: "_mcount".to_string(),
++            endian: "big".to_string(),
++            mcount: "_mcount".to_string(),
 +            llvm_abiname: "elfv2".to_string(),
-+            ..base
-+        },
-     })
++            ..base },
+     }
  }
