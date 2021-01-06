9bcd917b66d65bc8c47252cc8a34640f4e932fba
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2021-01-06 09:01:33 +0000

    - New port: securty/py-gvm-tools
    The Greenbone Vulnerability Management Tools gvm-tools are a collection of
    tools that help with remote controlling a Greenbone Security Manager (GSM)
    appliance and its underlying Greenbone Vulnerability Manager (GVM). The tools
    aid in accessing the communication protocols GMP (Greenbone Management
    Protocol) and OSP (Open Scanner Protocol).
    
    This module is comprised of interactive and non-interactive clients. The
    programming language Python is supported directly for interactive scripting.
    But it is also possible to issue remote GMP/OSP commands without programming
    in Python.
    
    WWW: https://github.com/greenbone/gvm-tools

---
+++ security/py-gvm-tools/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2021-01-04 15:47:34.037289000 -0500
++++ setup.py	2021-01-04 15:47:58.669103000 -0500
+@@ -2,7 +2,7 @@
+ from setuptools import setup
+ 
+ packages = \
+-['gvmtools', 'scripts', 'tests']
++['gvmtools', 'scripts']
+ 
+ package_data = \
+ {'': ['*']}
