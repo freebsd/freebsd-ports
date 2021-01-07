39a783569020ff68f22c2282e8968d7c2edd8094
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-01-07 21:17:22 +0000

    lang/rust: fix build on powerpc64le

--- lang/rust/files/powerpc64le/patch-compiler_rustc__target_src_spec_powerpc64le__unknown__freebsd.rs
+++ lang/rust/files/powerpc64le/patch-compiler_rustc__target_src_spec_powerpc64le__unknown__freebsd.rs
@@ -1,25 +1,19 @@
---- /dev/null	2020-09-30 18:09:21.093949000 -0500
-+++ compiler/rustc_target/src/spec/powerpc64le_unknown_freebsd.rs	2020-09-30 18:08:47.737833000 -0500
-@@ -0,0 +1,22 @@
-+use crate::spec::{LinkerFlavor, Target, TargetOptions, TargetResult};
+--- compiler/rustc_target/src/spec/powerpc64le_unknown_freebsd.rs.orig	2021-01-07 03:05:53 UTC
++++ compiler/rustc_target/src/spec/powerpc64le_unknown_freebsd.rs
+@@ -0,0 +1,16 @@
++use crate::spec::{LinkerFlavor, Target, TargetOptions};
 +
-+pub fn target() -> TargetResult {
++pub fn target() -> Target {
 +    let mut base = super::freebsd_base::opts();
 +    base.cpu = "ppc64le".to_string();
 +    base.pre_link_args.get_mut(&LinkerFlavor::Gcc).unwrap().push("-m64".to_string());
 +    base.max_atomic_width = Some(64);
 +
-+    Ok(Target {
++    Target {
 +        llvm_target: "powerpc64le-unknown-freebsd".to_string(),
-+        target_endian: "little".to_string(),
-+        target_pointer_width: "64".to_string(),
-+        target_c_int_width: "32".to_string(),
++	pointer_width: 64,
 +        data_layout: "e-m:e-i64:64-n32:64".to_string(),
 +        arch: "powerpc64".to_string(),
-+        target_os: "freebsd".to_string(),
-+        target_env: String::new(),
-+        target_vendor: "unknown".to_string(),
-+        linker_flavor: LinkerFlavor::Gcc,
-+        options: TargetOptions { target_mcount: "_mcount".to_string(), ..base },
-+    })
++        options: TargetOptions { endian: "little".to_string(), mcount: "_mcount".to_string(), ..base },
++    }
 +}
