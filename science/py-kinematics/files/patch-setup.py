57a53c376f0f5673484161269470760b6c234d4a
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-01-10 23:14:07 +0000

    Remove py-pytest-runner from BUILD_DEPENDS

---
+++ science/py-kinematics/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2018-12-08 03:30:44 UTC
++++ setup.py
+@@ -88,7 +88,6 @@ setup(
+     },
+ 
+     # testing requires for pytest
+-    setup_requires=['pytest-runner'],
+ 
+     tests_require=['pytest'],
+ 
