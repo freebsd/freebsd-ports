e76b7da17b87aed8be12c96c5230dfb9b84341c6
Dmitry Marakasov <amdmi3@FreeBSD.org>
2021-01-11 09:41:03 +0000

    - Update WWW - Regenerate patches

--- ftp/oftpd/files/patch-src_ftp__session.c
+++ ftp/oftpd/files/patch-src_ftp__session.c
@@ -1,6 +1,6 @@
---- src/ftp_session.c.orig	Sun Jun 17 03:45:08 2001
-+++ src/ftp_session.c	Sun Jun 17 03:46:26 2001
-@@ -1165,7 +1165,7 @@
+--- src/ftp_session.c.orig	2004-03-25 20:46:40 UTC
++++ src/ftp_session.c
+@@ -1165,7 +1165,7 @@ static void do_retr(ftp_session_t *f, const ftp_comman
          /* for sendfile(), we still have to use a loop to avoid 
             having our watchdog time us out on large files - it does
             allow us to avoid an extra copy to/from user space */
@@ -9,7 +9,7 @@
          offset = f->file_offset;
          file_size = stat_buf.st_size - offset;
          while (offset < stat_buf.st_size) {
-@@ -1200,7 +1200,7 @@
+@@ -1200,7 +1200,7 @@ static void do_retr(ftp_session_t *f, const ftp_comman
  
  	    watchdog_defer_watched(f->watched);
          }
@@ -18,7 +18,7 @@
          for (;;) {
              read_ret = read(file_fd, buf, sizeof(buf));
  	    if (read_ret == -1) {
-@@ -1219,7 +1219,7 @@
+@@ -1219,7 +1219,7 @@ static void do_retr(ftp_session_t *f, const ftp_comman
  
  	    watchdog_defer_watched(f->watched);
  	}
