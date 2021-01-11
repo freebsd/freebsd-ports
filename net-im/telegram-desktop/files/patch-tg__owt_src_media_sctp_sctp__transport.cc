39fb66171b4547488d48a133cefbb8f481d4bf3c
Yuri Victorovich <yuri@FreeBSD.org>
2021-01-11 19:22:21 +0000

    net-im/telegram-desktop: Fix the build failure caused by net/libusrsctp update
    libusrsctp significantly changed API without bumping SONAME.
    
    The upstream is notified.
    
    PR:		252581
    Submitted by:	madpilot

--- net-im/telegram-desktop/files/patch-tg__owt_src_media_sctp_sctp__transport.cc
+++ net-im/telegram-desktop/files/patch-tg__owt_src_media_sctp_sctp__transport.cc
@@ -1,6 +1,6 @@
---- tg_owt/src/media/sctp/sctp_transport.cc.orig	2020-08-30 09:41:57 UTC
+--- tg_owt/src/media/sctp/sctp_transport.cc.orig	2020-10-07 14:11:02 UTC
 +++ tg_owt/src/media/sctp/sctp_transport.cc
-@@ -43,7 +43,7 @@ enum PreservedErrno {
+@@ -43,7 +43,7 @@
  #include "rtc_base/thread_annotations.h"
  #include "rtc_base/thread_checker.h"
  #include "rtc_base/trace_event.h"
@@ -9,3 +9,12 @@
  
  namespace {
  
+@@ -470,7 +470,7 @@
+     return transport;
+   }
+ 
+-  static int SendThresholdCallback(struct socket* sock, uint32_t sb_free) {
++  static int SendThresholdCallback(struct socket* sock, uint32_t sb_free, void *ulp_info) {
+     // Fired on our I/O thread. SctpTransport::OnPacketReceived() gets
+     // a packet containing acknowledgments, which goes into usrsctp_conninput,
+     // and then back here.
