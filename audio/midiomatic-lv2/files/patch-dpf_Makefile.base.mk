f803ae1b81d59efab2d256526841cf1fd5bbdb64
Yuri Victorovich <yuri@FreeBSD.org>
2021-01-17 22:12:03 +0000

    audio/midiomatic-lv2: Update 0.1.1-beta -> 0.2.0
    Reported by:	portscout

--- audio/midiomatic-lv2/files/patch-dpf_Makefile.base.mk
+++ audio/midiomatic-lv2/files/patch-dpf_Makefile.base.mk
@@ -1,11 +1,11 @@
---- dpf/Makefile.base.mk.orig	2019-11-04 21:45:33 UTC
+--- dpf/Makefile.base.mk.orig	2021-01-17 22:01:08 UTC
 +++ dpf/Makefile.base.mk
-@@ -129,7 +129,7 @@ endif
- # Set build and link flags
- 
- BASE_FLAGS = -Wall -Wextra -pipe -MD -MP
--BASE_OPTS  = -O3 -ffast-math -mtune=generic -fdata-sections -ffunction-sections
-+BASE_OPTS  = -O3 -ffast-math -fdata-sections -ffunction-sections
+@@ -132,7 +132,7 @@ BASE_FLAGS = -Wall -Wextra -pipe -MD -MP
+ BASE_OPTS  = -O3 -ffast-math -fdata-sections -ffunction-sections
  
  ifeq ($(CPU_I386_OR_X86_64),true)
- BASE_OPTS += -msse -msse2
+-BASE_OPTS += -mtune=generic -msse -msse2
++BASE_OPTS += ${PORT_SIMD_FLAGS}
+ endif
+ 
+ ifeq ($(CPU_ARM),true)
