1acb11a2f38c720d5297f29fd1a84f0ae79b6cee
Neel Chauhan <nc@FreeBSD.org>
2021-01-17 16:48:33 +0000

    emulators/bochs: Update to 2.6.11
    Changes: http://svn.code.sf.net/p/bochs/code/tags/REL_2_6_11_FINAL/bochs/CHANGES
    
    Reviewed by:		0mp (mentor)
    Approved by:		0mp (mentor)
    Differential Revision:	https://reviews.freebsd.org/D28204

--- emulators/bochs/files/patch-iodev_network_eth__socket.cc
+++ emulators/bochs/files/patch-iodev_network_eth__socket.cc
@@ -1,16 +1,6 @@
---- iodev/network/eth_socket.cc.orig	2017-11-15 14:29:17 UTC
+--- iodev/network/eth_socket.cc.orig	2019-12-09 16:29:23 UTC
 +++ iodev/network/eth_socket.cc
-@@ -78,16 +78,21 @@ extern "C" {
- #ifdef WIN32
- #include <winsock2.h>
- #else
-+#include <sys/param.h>
- #include <errno.h>
- #include <fcntl.h>
- #include <sys/types.h>
- #include <sys/socket.h>
- #include <sys/ioctl.h>
--#include <netpacket/packet.h>
+@@ -95,7 +95,12 @@ extern "C" {
  #include <netinet/in.h>
  #include <net/ethernet.h>
  #include <net/if.h>
