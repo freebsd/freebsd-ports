ff6758abfc690b8c5fe4e8658e2fd35843c1d625
Tobias C. Berner <tcberner@FreeBSD.org>
2021-01-18 12:03:48 +0000

    Update KDE Frameworks to 5.78.0
    January 09, 2021. KDE today announces the release of KDE Frameworks 5.78.0.
    
    KDE Frameworks are 83 addon libraries to Qt which provide a wide variety of
    commonly needed functionality in mature, peer reviewed and well tested
    libraries with friendly licensing terms. For an introduction see the KDE
    Frameworks release announcement.
    
    This release is part of a series of planned monthly releases making
    improvements available to developers in a quick and pred
    
    Full Changelog:
    	https://kde.org/announcements/kde-frameworks-5.78.0
    
    PR:		252591
    Exp-run by:	antoine

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -58,7 +58,7 @@ KDE_PLASMA_VERSION?=		5.20.5
 KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
-KDE_FRAMEWORKS_VERSION?=	5.77.0
+KDE_FRAMEWORKS_VERSION?=	5.78.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 # Current KDE applications.
