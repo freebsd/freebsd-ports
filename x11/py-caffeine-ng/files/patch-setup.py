fc644a2c986cd742c2c45b802a644bfad1e43ceb
Hiroki Tagato <tagattie@FreeBSD.org>
2021-01-19 02:12:51 +0000

    Update to 3.5.0
    Changelog:
    https://github.com/caffeine-ng/caffeine-ng/compare/v3.4.2...v3.5.0

--- x11/py-caffeine-ng/files/patch-setup.py
+++ x11/py-caffeine-ng/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2018-05-17 01:08:07 UTC
+--- setup.py.orig	2021-01-01 14:03:23 UTC
 +++ setup.py
-@@ -7,14 +7,18 @@ from setuptools import find_packages, setup
+@@ -7,13 +7,16 @@ from setuptools import setup
  
  
  def get_data_files():
@@ -8,17 +8,14 @@
 +    black_list = ['net.launchpad.caffeine.gschema.xml']
      data_files = []
  
-     for path, dirs, files in walk('share'):
-         target_path = os.path.join('/usr', path)
+     for path, _dirs, files in walk("share"):
+         target_path = os.path.join("/usr", path)
  
--        data_files.append((
--            target_path, [os.path.join(path, f) for f in files]
--        ))
+-        if files:
+-            data_files.append((target_path, [os.path.join(path, f) for f in files]))
 +        tl = [os.path.join(path, f) for f in files if f not in black_list]
-+        if tl != []:
-+            data_files.append((
-+                target_path, tl
-+            ))
++        if tl:
++            data_files.append((target_path, tl))
  
-     data_files.append(
-         ("/etc/xdg/autostart", ["share/applications/caffeine.desktop"])
+     # Install all icons for the package into /usr/share as well.
+     # This is because the .desktop file actually uses them too.
