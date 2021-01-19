e5ca7a7dfd3d9449378fb4e17056a9306d469d47
Joseph Mingrone <jrm@FreeBSD.org>
2021-01-19 17:40:41 +0000

    multimedia/kodi: Update to version 19.0RC1
    PR:		252791
    Submitted by:	yzrh@noema.org (maintainer)

--- multimedia/kodi/files/patch-cmake_scripts_freebsd_ArchSetup.cmake
+++ multimedia/kodi/files/patch-cmake_scripts_freebsd_ArchSetup.cmake
@@ -1,6 +1,6 @@
---- cmake/scripts/freebsd/ArchSetup.cmake.orig	2020-12-15 16:30:44 UTC
+--- cmake/scripts/freebsd/ArchSetup.cmake.orig	2021-01-15 12:42:30 UTC
 +++ cmake/scripts/freebsd/ArchSetup.cmake
-@@ -19,6 +19,10 @@ else()
+@@ -20,6 +20,10 @@ else()
    elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL aarch64)
      set(ARCH aarch64-freebsd)
      set(NEON True)
--- multimedia/kodi/files/patch-xbmc_dbwrappers_mysqldataset.cpp
+++ multimedia/kodi/files/patch-xbmc_dbwrappers_mysqldataset.cpp
@@ -1,9 +1,9 @@
---- xbmc/dbwrappers/mysqldataset.cpp.orig	2020-10-04 17:34:28 UTC
+--- xbmc/dbwrappers/mysqldataset.cpp.orig	2021-01-18 00:42:21 UTC
 +++ xbmc/dbwrappers/mysqldataset.cpp
-@@ -17,10 +17,8 @@
- #include "utils/StringUtils.h"
- 
- #include "mysqldataset.h"
+@@ -18,10 +18,8 @@
+ #include <iostream>
+ #include <set>
+ #include <string>
 -#ifdef HAS_MYSQL
 +#if defined(HAS_MYSQL) || defined(HAS_MARIADB)
  #include <mysql/errmsg.h>
