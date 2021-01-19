9e4bc6e64aabb61ed7dd906183aef0d76ae7327b
Yuri Victorovich <yuri@FreeBSD.org>
2021-01-19 02:10:32 +0000

    audio/surge-synthesizer-lv2: Update 1.7.1 -> 1.8.0
    Reported by:	portscout

--- audio/surge-synthesizer-lv2/files/patch-cmake_versiontools.cmake
+++ audio/surge-synthesizer-lv2/files/patch-cmake_versiontools.cmake
@@ -1,11 +1,18 @@
---- cmake/versiontools.cmake.orig	2020-07-29 15:54:49 UTC
+--- cmake/versiontools.cmake.orig	2021-01-17 13:26:30 UTC
 +++ cmake/versiontools.cmake
-@@ -1,7 +1,7 @@
+@@ -1,5 +1,5 @@
  
- find_package(Git)
+-find_package(Git)
++#find_package(Git)
  
--if( Git_FOUND )
-+if( FALSE AND Git_FOUND )
+ if( EXISTS ${SURGESRC}/VERSION_GIT_INFO )
+   message( STATUS "VERSION_GIT_INFO file is present; using that rather than git query" )
+@@ -18,7 +18,7 @@ if( EXISTS ${SURGESRC}/VERSION_GIT_INFO )
+     OUTPUT_STRIP_TRAILING_WHITESPACE
+     )
+ 
+-elseif( Git_FOUND )
++elseif( FALSE AND Git_FOUND )
    execute_process(
      COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
      WORKING_DIRECTORY ${SURGESRC}
--- audio/surge-synthesizer-lv2/files/patch-vstgui.surge_vstgui_lib_platform_linux_x11fileselector.cpp
+++ audio/surge-synthesizer-lv2/files/patch-vstgui.surge_vstgui_lib_platform_linux_x11fileselector.cpp
@@ -1,4 +1,4 @@
---- vstgui.surge/vstgui/lib/platform/linux/x11fileselector.cpp.orig	2020-06-23 22:22:42 UTC
+--- vstgui.surge/vstgui/lib/platform/linux/x11fileselector.cpp.orig	2020-09-24 01:49:47 UTC
 +++ vstgui.surge/vstgui/lib/platform/linux/x11fileselector.cpp
 @@ -3,6 +3,8 @@
  // distribution and at http://github.com/steinbergmedia/vstgui/LICENSE
@@ -8,4 +8,4 @@
 +#include <stdio.h> // https://github.com/surge-synthesizer/vstgui/issues/7
  #include <unistd.h>
  #include <string>
- 
+ #include <stdio.h> // Since we use getline below
