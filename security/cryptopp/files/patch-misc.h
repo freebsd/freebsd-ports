54557556437e82b0ffd1912584b4895cfe301ba9
Jason E. Hale <jhale@FreeBSD.org>
2021-01-19 04:26:12 +0000

    security/cryptopp: Update to 8.4.0
    Bump PORTREVSION on all consumers. ABI-breaking changes were introduced and
    recompile of consumers is necessary.
    
    Remove unused cryptopp dependency from devel/xeus. Upstream migrated to OpenSSL
    several releases ago. [1]
    
    [1] https://github.com/jupyter-xeus/xeus/commit/c98c44c174cafcf424f4203dc3c61564118feb3e
    
    Changes:	https://www.cryptopp.com/release840.html

--- security/cryptopp/files/patch-misc.h
+++ security/cryptopp/files/patch-misc.h
@@ -2,12 +2,12 @@ This fixes a warning triggered by testing an unsigned parameter
 against 0.  The patch solves this by creating a different template
 for signed case. (PR: 178827)
 
---- misc.h.orig	2018-04-08 08:47:11 UTC
+--- misc.h.orig	2021-01-01 17:03:59 UTC
 +++ misc.h
-@@ -572,8 +572,10 @@ inline bool SafeConvert(T1 from, T2 &to)
+@@ -700,8 +700,10 @@ inline bool SafeConvert(T1 from, T2 &to)
  /// \param value the value to convert
  /// \param base the base to use during the conversion
- /// \returns the string representation of value in base.
+ /// \return the string representation of value in base.
 +template<bool> struct IsUnsigned {};
 +
  template <class T>
@@ -15,8 +15,8 @@ for signed case. (PR: 178827)
 +std::string IntToStringImpl(T value, unsigned int base, IsUnsigned<true>)
  {
  	// Hack... set the high bit for uppercase.
- 	static const unsigned int HIGH_BIT = (1U << 31);
-@@ -584,12 +586,6 @@ std::string IntToString(T value, unsigned int base = 1
+ 	const unsigned int HIGH_BIT = (1U << 31);
+@@ -712,12 +714,6 @@ std::string IntToString(T value, unsigned int base = 1
  	if (value == 0)
  		return "0";
  
@@ -29,7 +29,7 @@ for signed case. (PR: 178827)
  	std::string result;
  	while (value > 0)
  	{
-@@ -597,9 +593,28 @@ std::string IntToString(T value, unsigned int base = 1
+@@ -725,9 +721,28 @@ std::string IntToString(T value, unsigned int base = 1
  		result = char((digit < 10 ? '0' : (CH - 10)) + digit) + result;
  		value /= base;
  	}
