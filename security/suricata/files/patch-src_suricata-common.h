c6ecb1db81599192b5a1cccc44ae0ad3b8854fc0
Fernando Apesteguía <fernape@FreeBSD.org>
2021-01-20 14:02:18 +0000

    security/suricata: simplify Makefile
    Add USES=localbase so we can get rid of *-includes and *-libraries.
    Reorder some variables too.
    Regenerate patch.
    
    PR:	252571
    Submitted by:	fernape@FreeBSD.org
    Reviewed by:	franco@opnsense.org (maintainer)

--- security/suricata/files/patch-src_suricata-common.h
+++ security/suricata/files/patch-src_suricata-common.h
@@ -1,5 +1,5 @@
---- src/suricata-common.h-orig	2020-10-25 16:56:49.454317000 +0100
-+++ src/suricata-common.h	2020-10-25 16:57:06.035153000 +0100
+--- src/suricata-common.h.orig	2020-12-04 07:11:05 UTC
++++ src/suricata-common.h
 @@ -36,6 +36,8 @@
  #define _GNU_SOURCE
  #define __USE_GNU
