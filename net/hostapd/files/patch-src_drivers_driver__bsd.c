74e55da319b00e9dc38d622070fd9c35ab1515c6
Cy Schubert <cy@FreeBSD.org>
2021-01-20 17:14:16 +0000

    This is the ports version of src commit d70886d063166786ded0007af8cdcbf57b7b4827.
    wpa_supplicant uses PF_ROUTE to return the routing table in order to
    determine the length of the routing table buffer. As of 81728a538d24
    wpa_supplicant is started before the routing table has been populated
    resulting in the length of zero to be returned. This causes
    wpa_supplicant to loop endlessly. (The workaround is to kill and restart
    wpa_supplicant as by the time it is restarted the routing table is
    populated.)
    
    (Personally, I was not able to reproduce this unless wlan0 was a member of
    lagg0. However, others experienced this problem on standalone wlan0.)
    
    PR:		252844
    Submitted by:	shu <ankohuu _ outlook.com>
    Reported by:	shu <ankohuu _ outlook.com>
    Reviewed by:	cy
    Differential Revision:	https://reviews.freebsd.org/D28249

--- net/hostapd/files/patch-src_drivers_driver__bsd.c
+++ net/hostapd/files/patch-src_drivers_driver__bsd.c
@@ -1,5 +1,14 @@
---- src/drivers/driver_bsd.c.orig2	2019-08-07 06:25:25.000000000 -0700
-+++ src/drivers/driver_bsd.c	2020-05-19 21:11:18.891164000 -0700
+--- src/drivers/driver_bsd.c.orig	2019-08-07 06:25:25.000000000 -0700
++++ src/drivers/driver_bsd.c	2021-01-20 08:04:07.589603000 -0800
+@@ -649,7 +649,7 @@
+ 		len = 2048;
+ 	}
+ 
+-	return len;
++	return (len == 0) ? 2048 : len;
+ }
+ 
+ #ifdef HOSTAPD
 @@ -665,7 +665,11 @@
  static int bsd_sta_deauth(void *priv, const u8 *own_addr, const u8 *addr,
  			  u16 reason_code);
--- security/wpa_supplicant/files/patch-src_drivers_driver__bsd.c
+++ security/wpa_supplicant/files/patch-src_drivers_driver__bsd.c
@@ -1,5 +1,14 @@
 --- src/drivers/driver_bsd.c.orig	2019-08-07 06:25:25.000000000 -0700
-+++ src/drivers/driver_bsd.c	2020-05-19 18:17:48.607660000 -0700
++++ src/drivers/driver_bsd.c	2021-01-20 08:00:59.210974000 -0800
+@@ -649,7 +649,7 @@
+ 		len = 2048;
+ 	}
+ 
+-	return len;
++	return (len == 0) ? 2048 : len;
+ }
+ 
+ #ifdef HOSTAPD
 @@ -1336,14 +1336,18 @@
  		drv = bsd_get_drvindex(global, ifm->ifm_index);
  		if (drv == NULL)
