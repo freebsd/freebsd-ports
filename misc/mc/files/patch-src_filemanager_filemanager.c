4c1ebe9463769823c81a52b4aaade14b7a43de7e
Ben Woods <woodsb02@FreeBSD.org>
2021-01-21 23:21:34 +0000

    misc/mc: Update to 4.8.26
    Changes this release:
      https://midnight-commander.org/wiki/NEWS-4.8.26

--- misc/mc/files/patch-src_filemanager_filemanager.c
+++ misc/mc/files/patch-src_filemanager_filemanager.c
@@ -1,7 +1,7 @@
---- src/filemanager/midnight.c.orig	2017-03-04 17:51:38 UTC
-+++ src/filemanager/midnight.c
-@@ -879,7 +879,7 @@ setup_mc (void)
-         add_select_channel (mc_global.tty.subshell_pty, load_prompt, 0);
+--- src/filemanager/filemanager.c.orig	2021-01-10 11:50:16 UTC
++++ src/filemanager/filemanager.c
+@@ -863,7 +863,7 @@ setup_mc (void)
+         add_select_channel (mc_global.tty.subshell_pty, load_prompt, NULL);
  #endif /* !ENABLE_SUBSHELL */
  
 -    if ((tty_baudrate () < 9600) || mc_global.tty.slow_terminal)
