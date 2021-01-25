641427224c752413c6bd1d47605b04f61909ee75
Dmitry Marakasov <amdmi3@FreeBSD.org>
2021-01-25 18:15:13 +0000

    - Update to 1.5.0

--- astro/libosmpbf/files/patch-tools_osmpbf-outline.cpp
+++ astro/libosmpbf/files/patch-tools_osmpbf-outline.cpp
@@ -1,9 +1,14 @@
---- tools/osmpbf-outline.cpp.orig	2014-03-15 15:11:32 UTC
+--- tools/osmpbf-outline.cpp.orig	2021-01-06 13:47:22 UTC
 +++ tools/osmpbf-outline.cpp
-@@ -1,3 +1,6 @@
-+// for isatty()
-+#include <unistd.h>
-+
- // used for va_list in debug-print methods
- #include <stdarg.h>
+@@ -10,6 +10,11 @@
+ // zlib compression is used inside the pbf blobs
+ #include <zlib.h>
  
++#ifndef WIN32
++	// used for isatty
++	#include <unistd.h>
++#endif
++
+ // netinet or winsock2 provides the network-byte-order conversion function
+ #ifdef D_HAVE_WINSOCK
+     #include <winsock2.h>
