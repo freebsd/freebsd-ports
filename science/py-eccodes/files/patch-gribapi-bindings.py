45fadfdc4eaa588f20fa7875a994d49005639f73
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-01-26 17:37:28 +0000

    Update to 1.1.0
    Changes:	https://github.com/ecmwf/eccodes-python/blob/develop/CHANGELOG.rst

--- science/py-eccodes/files/patch-gribapi-bindings.py
+++ science/py-eccodes/files/patch-gribapi-bindings.py
@@ -1,19 +1,19 @@
---- gribapi/bindings.py.orig	2020-10-15 15:56:17 UTC
+--- gribapi/bindings.py.orig	2021-01-20 16:01:53 UTC
 +++ gribapi/bindings.py
-@@ -34,7 +34,7 @@ except ModuleNotFoundError:
-         + pkgutil.get_data(__name__, "eccodes.h").decode("utf-8")
-     )
+@@ -33,7 +33,7 @@ except ModuleNotFoundError:
+     CDEF += pkgutil.get_data(__name__, "eccodes.h")
+     ffi.cdef(CDEF.decode("utf-8").replace("\r", "\n"))
  
 -    LIBNAMES = ["eccodes", "libeccodes.so", "libeccodes"]
 +    LIBNAMES = ["libeccodes.so"]
  
      try:
          import ecmwflibs
-@@ -46,7 +46,6 @@ except ModuleNotFoundError:
+@@ -45,7 +45,6 @@ except ModuleNotFoundError:
      if os.environ.get("ECCODES_DIR"):
          eccdir = os.environ["ECCODES_DIR"]
          LIBNAMES.insert(0, os.path.join(eccdir, "lib/libeccodes.so"))
--        LIBNAMES.insert(0, os.path.join(eccdir, "lib64/libeccodes.so"))
+-        LIBNAMES.insert(1, os.path.join(eccdir, "lib64/libeccodes.so"))
  
+     lib = None
      for libname in LIBNAMES:
-         try:
