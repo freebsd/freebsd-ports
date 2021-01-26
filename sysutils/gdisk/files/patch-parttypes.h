5d53474a4db67ba60b830b760a33d5265157accb
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-01-26 17:22:19 +0000

    Update to 1.0.6
    Changes:	http://www.rodsbooks.com/gdisk/revisions.html

--- sysutils/gdisk/files/patch-parttypes.h
+++ sysutils/gdisk/files/patch-parttypes.h
@@ -1,7 +1,7 @@
---- parttypes.h.orig	2018-07-05 20:19:46 UTC
+--- parttypes.h.orig	2021-01-13 22:14:27 UTC
 +++ parttypes.h
 @@ -16,6 +16,9 @@
- #define __PARTITION_TYPES
+ #include "guid.h"
  
  using namespace std;
 +#ifdef USE_UTF16
