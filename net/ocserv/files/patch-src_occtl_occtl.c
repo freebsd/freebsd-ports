9ea5b8940196dd031430c084ff2f1d1a168e52fb
Juraj Lutter <otis@FreeBSD.org>
2021-01-29 21:26:11 +0000

    net/ocserv: Update to 1.1.2
    - Update to 1.1.2
    - Reformat Makefile according to portclippy/portfmt
    - Install sample config with PREFIX-ized values where
      apropriate.
    - Take MAINTAINERship
    
    Reviewed by:	osa (mentor)
    Approved by:	osa (mentor)
    MFH:		2021Q1
    Differential Revision:	https://reviews.freebsd.org/D28346

--- net/ocserv/files/patch-src_occtl_occtl.c
+++ net/ocserv/files/patch-src_occtl_occtl.c
@@ -1,6 +1,6 @@
---- src/occtl/occtl.c.orig	2018-01-14 16:25:24 UTC
+--- src/occtl/occtl.c.orig	2020-08-06 18:51:31 UTC
 +++ src/occtl/occtl.c
-@@ -249,7 +249,7 @@ static int handle_help_cmd(CONN_TYPE * conn, const cha
+@@ -264,7 +264,7 @@ static int handle_help_cmd(CONN_TYPE * conn, const cha
  static int handle_reset_cmd(CONN_TYPE * conn, const char *arg, cmd_params_st *params)
  {
  	rl_reset_terminal(NULL);
