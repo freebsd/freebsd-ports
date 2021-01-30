f0c841a87b4d348bba5169396015358a39d6cd7f
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-01-30 12:23:07 +0000

    Update to 1.23.0
    Changes:	https://github.com/dbcli/mycli/blob/master/changelog.md

--- databases/py-mycli/files/patch-setup.py
+++ databases/py-mycli/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2020-07-25 03:46:06 UTC
+--- setup.py.orig	2021-01-07 01:54:09 UTC
 +++ setup.py
 @@ -21,7 +21,7 @@ install_requirements = [
      'Pygments >= 1.6',
-     'prompt_toolkit>=3.0.0,<4.0.0',
+     'prompt_toolkit>=3.0.6,<4.0.0',
      'PyMySQL >= 0.9.2',
 -    'sqlparse>=0.3.0,<0.4.0',
 +    'sqlparse>=0.3.0',
      'configobj >= 5.0.5',
      'cryptography >= 1.0.0',
-     'cli_helpers[styles] > 1.1.0',
+     'cli_helpers[styles] >= 2.0.1',
