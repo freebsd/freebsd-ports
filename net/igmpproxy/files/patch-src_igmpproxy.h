a98326bf91bb556fd0b6ca0476e21576b8b32147
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2021-01-31 14:33:08 +0000

    net/igmpproxy: Update to 0.3
    igmpproxy update to 0.3, mostly fixing previous FreeBSD build issues.
    
    MAX_IF patch kept and raised further to retain operation when many addresses are found in the system.  (On OPNsense/pfSense 40 addresses can be too low.)
    
    PR:		252857
    Submitted by:	Franco Fichtner <franco@opnsense.org> (maintainer)

--- net/igmpproxy/files/patch-src_igmpproxy.h
+++ net/igmpproxy/files/patch-src_igmpproxy.h
@@ -1,31 +1,11 @@
---- src/igmpproxy.h.orig	2018-02-13 19:17:30 UTC
+--- src/igmpproxy.h.orig	2021-01-12 08:24:34 UTC
 +++ src/igmpproxy.h
-@@ -35,9 +35,6 @@
- *   igmpproxy.h - Header file for common includes.
- */
- 
--#include "config.h"
--#include "os.h"
--
- #include <errno.h>
- #include <stdarg.h>
- #include <stdio.h>
-@@ -60,6 +57,9 @@
- #include <netinet/in.h>
- #include <arpa/inet.h>
- 
-+#include "os.h"
-+#include "config.h"
-+
- /*
-  * Limit on length of route data
-  */
-@@ -113,7 +113,7 @@ void my_log( int Serverity, int Errno, c
+@@ -112,7 +112,7 @@ void my_log( int Serverity, int Errno, const char *Fmt
  
  /* ifvc.c
   */
 -#define MAX_IF         40     // max. number of interfaces recognized
-+#define MAX_IF         100     // max. number of interfaces recognized
++#define MAX_IF         140     // max. number of interfaces recognized
  
  // Interface states
  #define IF_STATE_DISABLED      0   // Interface should be ignored.
