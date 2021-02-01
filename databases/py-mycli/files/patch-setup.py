79f02373bcefd8184fc0ba2f69d0f0562ed0e8bf
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-02-01 19:25:54 +0000

    Fix r563317
    Relax pyperclip version requirement to 1.7.0 temporarily.

--- databases/py-mycli/files/patch-setup.py
+++ databases/py-mycli/files/patch-setup.py
@@ -1,6 +1,6 @@
 --- setup.py.orig	2021-01-07 01:54:09 UTC
 +++ setup.py
-@@ -21,7 +21,7 @@ install_requirements = [
+@@ -21,11 +21,11 @@ install_requirements = [
      'Pygments >= 1.6',
      'prompt_toolkit>=3.0.6,<4.0.0',
      'PyMySQL >= 0.9.2',
@@ -9,3 +9,8 @@
      'configobj >= 5.0.5',
      'cryptography >= 1.0.0',
      'cli_helpers[styles] >= 2.0.1',
+-    'pyperclip >= 1.8.1'
++    'pyperclip >= 1.7.0'
+ ]
+ 
+ 
