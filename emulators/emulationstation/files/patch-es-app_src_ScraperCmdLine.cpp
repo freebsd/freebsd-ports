cd99973f7cbc8dd2858a75e1161db0e81d4d4c40
Fernando Apesteguía <fernape@FreeBSD.org>
2021-02-01 17:49:58 +0000

    emulators/emulationstation: Update to 2.9.4
    ChangeLog: https://github.com/RetroPie/EmulationStation/compare/v2.9.4...master
    
    PR:	251474
    Submitted by:	freebsd@coombscloud.com (maintainer)

--- emulators/emulationstation/files/patch-es-app_src_ScraperCmdLine.cpp
+++ emulators/emulationstation/files/patch-es-app_src_ScraperCmdLine.cpp
@@ -1,6 +1,4 @@
-Use POSIX sleep funtion, instead of Windows
-
---- es-app/src/ScraperCmdLine.cpp.orig	2018-08-11 01:17:21 UTC
+--- es-app/src/ScraperCmdLine.cpp.orig	2020-11-29 11:43:15 UTC
 +++ es-app/src/ScraperCmdLine.cpp
 @@ -5,17 +5,13 @@
  #include "SystemData.h"
@@ -14,7 +12,7 @@ Use POSIX sleep funtion, instead of Windows
  
  std::ostream& out = std::cout;
  
- void handle_interrupt_signal(int p)
+ void handle_interrupt_signal(int /*p*/)
  {
 -	sleep(50);
 +	usleep(50);
