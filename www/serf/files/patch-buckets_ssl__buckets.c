973ad92f2c0dc135c0e444ba14b59da14ba73b56
Lev A. Serebryakov <lev@FreeBSD.org>
2021-02-04 11:38:06 +0000

    MFH: r563962
     Fix incompatibility with KTLS.
    
    PR:		253214
    Submitted by:	madpilot@FreeBSD.org
    
    Approved by:	portmgr (blanket, FAQ 20.6.2)

--- www/serf/files/patch-buckets_ssl__buckets.c
+++ www/serf/files/patch-buckets_ssl__buckets.c
@@ -1,6 +1,23 @@
 --- buckets/ssl_buckets.c.orig	2016-06-30 15:45:07 UTC
 +++ buckets/ssl_buckets.c
-@@ -1156,7 +1156,7 @@ static void init_ssl_libraries(void)
+@@ -407,7 +407,7 @@ static int bio_bucket_destroy(BIO *bio)
+ 
+ static long bio_bucket_ctrl(BIO *bio, int cmd, long num, void *ptr)
+ {
+-    long ret = 1;
++    long ret = 0;
+ 
+     switch (cmd) {
+     default:
+@@ -415,6 +415,7 @@ static long bio_bucket_ctrl(BIO *bio, int cmd, long nu
+         break;
+     case BIO_CTRL_FLUSH:
+         /* At this point we can't force a flush. */
++        ret = 1;
+         break;
+     case BIO_CTRL_PUSH:
+     case BIO_CTRL_POP:
+@@ -1156,7 +1157,7 @@ static void init_ssl_libraries(void)
          }
  #endif
  
