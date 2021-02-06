c07f3fbf62ab1d878049d817a28b18b60375d138
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-02-06 20:43:43 +0000

    Update to 7.75.0
    Changes:	https://curl.haxx.se/changes.html

--- ftp/curl/files/patch-lib-url.c
+++ ftp/curl/files/patch-lib-url.c
@@ -1,5 +1,3 @@
-Obtained from:	https://github.com/curl/curl/commit/0936ecd0ee5e3e28c098fefc9e2c0e6847cb7a82
-
 Description: Different handling of signals and threads.
 Forwarded: not-needed
 Author: Peter Pentchev <roam@FreeBSD.org>
@@ -18,24 +16,3 @@ Last-Update: 2010-12-18
    Curl_http2_init_userset(set);
    return result;
  }
-@@ -3940,20 +3944,6 @@ CURLcode Curl_setup_conn(struct connectdata *conn,
-      lingering set from a previous invoke */
-   conn->bits.proxy_connect_closed = FALSE;
- #endif
--  /*
--   * Set user-agent. Used for HTTP, but since we can attempt to tunnel
--   * basically anything through a http proxy we can't limit this based on
--   * protocol.
--   */
--  if(data->set.str[STRING_USERAGENT]) {
--    Curl_safefree(data->state.aptr.uagent);
--    data->state.aptr.uagent =
--      aprintf("User-Agent: %s\r\n", data->set.str[STRING_USERAGENT]);
--    if(!data->state.aptr.uagent)
--      return CURLE_OUT_OF_MEMORY;
--  }
--
--  data->req.headerbytecount = 0;
- 
- #ifdef CURL_DO_LINEEND_CONV
-   data->state.crlf_conversions = 0; /* reset CRLF conversion counter */
