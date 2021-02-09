46abde0a25ee7bd21f4145fb4096206df1615f25
Fernando Apesteguía <fernape@FreeBSD.org>
2021-02-09 06:56:15 +0000

    net/open-isns: Update to 0.101
    ChangeLog: https://github.com/open-iscsi/open-isns/blob/master/ChangeLog
    
    PR:	253343
    Submitted by:	pavelivolkov@gmail.com (maintainer)

--- net/open-isns/files/patch-attrs.c
+++ net/open-isns/files/patch-attrs.c
@@ -1,6 +1,6 @@
---- attrs.c.orig	2018-10-28 09:10:34 UTC
+--- attrs.c.orig	2021-02-06 13:40:34 UTC
 +++ attrs.c
-@@ -777,7 +777,7 @@ isns_attr_decode(buf_t *bp, isns_attr_t 
+@@ -777,7 +777,7 @@ isns_attr_decode(buf_t *bp, isns_attr_t **result)
  {
  	isns_attr_t	*attr = NULL;
  	isns_value_t	*value;
