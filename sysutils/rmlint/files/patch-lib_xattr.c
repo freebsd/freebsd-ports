03016e1aefe461a43e10d1a54b3c6a07587309bd
Joseph Mingrone <jrm@FreeBSD.org>
2021-02-10 03:53:52 +0000

    sysutils/rmlint: Clean up some cruft in initial commit
    Reported by:	danfe
    Pointy hat:	jrm

--- sysutils/rmlint/files/patch-lib_xattr.c
+++ sysutils/rmlint/files/patch-lib_xattr.c
@@ -26,21 +26,3 @@
 +int rm_xattr_clear_hash(__attribute__((unused)) RmFile *file, __attribute__((unused)) RmSession *session) {
      g_assert(file);
      g_assert(session);
- 
-@@ -316,6 +316,8 @@ int rm_xattr_clear_hash(RmFile *file, RmSession *sessi
- 
- #if HAVE_XATTR
- 
-+#if HAVE_XATTR
-+
- GHashTable *rm_xattr_list(const char *path, bool follow_symlinks) {
-     const size_t buf_size = 4096;
-     const size_t val_size = 1024;
-@@ -484,5 +486,7 @@ int rm_xattr_mark_deduplicated(const char *path, bool 
-     g_hash_table_destroy(map);
-     return result;
- }
-+
-+#endif
- 
- #endif
