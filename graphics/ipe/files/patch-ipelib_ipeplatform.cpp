a96165d3035a019ea2da4422eb3a313ca7663acc
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2021-02-11 16:57:37 +0000

    graphics/ipe: Update version 7.2.21=>7.2.23

--- graphics/ipe/files/patch-ipelib_ipeplatform.cpp
+++ graphics/ipe/files/patch-ipelib_ipeplatform.cpp
@@ -1,15 +1,14 @@
---- ipelib/ipeplatform.cpp.orig	2019-05-02 09:29:29 UTC
+--- ipelib/ipeplatform.cpp.orig	2020-12-24 12:20:13 UTC
 +++ ipelib/ipeplatform.cpp
-@@ -48,7 +48,11 @@
+@@ -49,6 +49,11 @@
  #include <sys/param.h>
  #include <sys/errno.h>
  #endif
--
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
 +#include <limits.h>
 +#include <xlocale.h>
 +#include <sys/errno.h>
 +#endif
- #include <cstdlib>
- #include <sys/types.h>
- #include <sys/stat.h>
+ 
+ #ifdef IPE_GSL
+ #include <gsl/gsl_errno.h>
