75d6bc8de824cec008d1b7dbc3caf09d8a35cdcf
Li-Wen Hsu <lwhsu@FreeBSD.org>
2021-02-11 12:32:32 +0000

    net-im/telegram-desktop: update to 2.5.8
    PR:		253336
    Submitted by:	Henry Hu <henry.hu.sh@gmail.com> (maintainer)

--- net-im/telegram-desktop/files/patch-Telegram_lib__base_base_platform_linux_base__info__linux.cpp
+++ net-im/telegram-desktop/files/patch-Telegram_lib__base_base_platform_linux_base__info__linux.cpp
@@ -1,50 +1,41 @@
---- Telegram/lib_base/base/platform/linux/base_info_linux.cpp.orig	2020-10-20 16:29:09 UTC
+--- Telegram/lib_base/base/platform/linux/base_info_linux.cpp.orig	2021-01-28 15:30:24 UTC
 +++ Telegram/lib_base/base/platform/linux/base_info_linux.cpp
-@@ -11,7 +11,6 @@
- #include <QtCore/QVersionNumber>
+@@ -14,11 +14,6 @@
  #include <QtCore/QDate>
  #include <QtGui/QGuiApplication>
+ 
+-// this file is used on both Linux & BSD
+-#ifdef Q_OS_LINUX
 -#include <gnu/libc-version.h>
- 
- namespace Platform {
- 
-@@ -32,11 +31,9 @@ QString SystemVersionPretty() {
- 	const auto value = result ? QString::fromLatin1(result) : QString();
- 	const auto list = value.split(':', QString::SkipEmptyParts);
- 
--	return "Linux "
-+	return "FreeBSD "
- 		+ (list.isEmpty() ? QString() : list[0] + ' ')
--		+ (IsWayland() ? "Wayland " : "X11 ")
--		+ "glibc "
--		+ GetGlibCVersion();
-+		+ (IsWayland() ? "Wayland " : "X11 ");
- }
- 
- QString SystemCountry() {
-@@ -54,10 +51,6 @@ QString SystemLanguage() {
- QDate WhenSystemBecomesOutdated() {
- 	if (IsLinux32Bit()) {
- 		return QDate(2020, 9, 1);
--	} else if (const auto version = GetGlibCVersion(); !version.isEmpty()) {
--		if (QVersionNumber::fromString(version) < QVersionNumber(2, 23)) {
--			return QDate(2020, 9, 1); // Older than Ubuntu 16.04.
--		}
- 	}
- 	return QDate();
- }
-@@ -78,14 +71,6 @@ QString AutoUpdateKey() {
- 	} else {
- 		Unexpected("Platform in AutoUpdateKey.");
- 	}
--}
+-#endif // Q_OS_LINUX
 -
--QString GetGlibCVersion() {
--	static const auto result = [&] {
--		const auto version = QString::fromLatin1(gnu_get_libc_version());
--		return QVersionNumber::fromString(version).isNull() ? QString() : version;
--	}();
--	return result;
+ namespace Platform {
+ namespace {
+ 
+@@ -103,7 +98,7 @@ QString SystemVersionPretty() {
+ 	static const auto result = [&] {
+ 		QStringList resultList{};
+ 
+-#ifdef Q_OS_LINUX
++#if 0
+ 		resultList << "Linux";
+ #else // Q_OS_LINUX
+ 		resultList << QSysInfo::kernelType();
+@@ -186,7 +181,7 @@ QString AutoUpdateKey() {
  }
  
- bool IsWayland() {
+ QString GetLibcName() {
+-#ifdef Q_OS_LINUX
++#if 0
+ 	return "glibc";
+ #endif // Q_OS_LINUX
+ 
+@@ -194,7 +189,7 @@ QString GetLibcName() {
+ }
+ 
+ QString GetLibcVersion() {
+-#ifdef Q_OS_LINUX
++#if 0
+ 	static const auto result = [&] {
+ 		const auto version = QString::fromLatin1(gnu_get_libc_version());
+ 		return QVersionNumber::fromString(version).isNull() ? QString() : version;
