b434cb6ac7072369691b513fffcb59f7c34cd32a
Fernando Apesteguía <fernape@FreeBSD.org>
2021-02-12 15:56:07 +0000

    games/retroarch: update to 1.9.0
    ChangeLog: https://www.libretro.com/index.php/retroarch-1-9-0-released/
    
    PR:	252189
    Submitted by:	beyert@cs.ucr.edu
    Approved by:	greg@unrelenting.technology (maintainer)

--- games/retroarch/files/patch-libretro-common_net_net__ifinfo.c
+++ games/retroarch/files/patch-libretro-common_net_net__ifinfo.c
@@ -1,8 +1,8 @@
---- libretro-common/net/net_ifinfo.c.orig	2016-07-04 20:02:01 UTC
+--- libretro-common/net/net_ifinfo.c.orig	2020-08-07 22:06:53 UTC
 +++ libretro-common/net/net_ifinfo.c
-@@ -44,6 +44,8 @@
- 
- #include <net/net_ifinfo.h>
+@@ -50,6 +50,8 @@
+ #include <netinet/in.h>
+ #endif
  
 +#include <netinet/in.h>
 +
