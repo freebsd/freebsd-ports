82eb4ffa9704430d0cd67ed105d0cd329600b5ee
Yuri Victorovich <yuri@FreeBSD.org>
2021-02-12 11:13:40 +0000

    science/chrono: Update 5.0.1 -> 6.0.0
    Also add port options for optional modules defined by the project.

---
+++ science/chrono/files/patch-src_chrono__synchrono_flatbuffer_message_SynFlatBuffers__generated.h
@@ -0,0 +1,11 @@
+--- src/chrono_synchrono/flatbuffer/message/SynFlatBuffers_generated.h.orig	2021-02-11 13:43:25 UTC
++++ src/chrono_synchrono/flatbuffer/message/SynFlatBuffers_generated.h
+@@ -4,7 +4,7 @@
+ #ifndef FLATBUFFERS_GENERATED_SYNFLATBUFFERS_SYNFLATBUFFERS_H_
+ #define FLATBUFFERS_GENERATED_SYNFLATBUFFERS_SYNFLATBUFFERS_H_
+ 
+-#include "flatbuffers/flatbuffers.h"
++#include <flatbuffers/flatbuffers.h>
+ 
+ namespace SynFlatBuffers {
+ namespace SPAT {
