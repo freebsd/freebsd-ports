98c24badd0f2908e39151be41b0873ac3e59ae1a
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-02-15 21:21:04 +0000

    www/webkit2-gtk3: fix build on powerpc64
    ar is now required for build.
    
    Include ExecutableBaseInlines.h in NativeExecutable.cpp to fix:
    Source/JavaScriptCore/CMakeFiles/JavaScriptCore.dir/__/__/DerivedSources/JavaScriptCore/unified-sources/UnifiedSource-f2e18ffc-27.cpp.o(.text+0x5300): error: undefined reference to 'JSC::ExecutableBase::hasJITCodeForCall() const'

---
+++ www/webkit2-gtk3/files/patch-Source_JavaScriptCore_runtime_NativeExecutable.cpp
@@ -0,0 +1,10 @@
+--- Source/JavaScriptCore/runtime/NativeExecutable.cpp.orig	2021-02-15 20:45:27 UTC
++++ Source/JavaScriptCore/runtime/NativeExecutable.cpp
+@@ -24,6 +24,7 @@
+  */
+ 
+ #include "config.h"
++#include "ExecutableBaseInlines.h"
+ #include "NativeExecutable.h"
+ 
+ #include "JSCInlines.h"
