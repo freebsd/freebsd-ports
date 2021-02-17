64150803e71ea0f0f1a9d7dd716899098cb5561e
Tobias C. Berner <tcberner@FreeBSD.org>
2021-02-17 14:47:02 +0000

    www/youtube_dlc: Update to 2021.02.15
    - enable ffmpeg by default for DASH video support
    
    PR:		253582
    Submitted by:	daniel.engberg.lists@pyret.net

--- www/youtube_dlc/files/patch-youtube__dlc_____init____.py
+++ www/youtube_dlc/files/patch-youtube__dlc_____init____.py
@@ -1,27 +1,27 @@
---- youtube_dlc/__init__.py.orig	2021-01-16 18:54:52 UTC
+--- youtube_dlc/__init__.py.orig	2021-02-17 11:58:10 UTC
 +++ youtube_dlc/__init__.py
-@@ -39,7 +39,6 @@ from .utils import (
+@@ -38,7 +38,6 @@ from .utils import (
+     std_headers,
      write_string,
-     render_table,
  )
 -from .update import update_self
  from .downloader import (
      FileDownloader,
  )
-@@ -499,17 +498,13 @@ def _real_main(argv=None):
-     }
- 
-     with YoutubeDL(ydl_opts) as ydl:
--        # Update version
--        if opts.update_self:
--            update_self(ydl.to_screen, opts.verbose, ydl._opener)
--
-         # Remove cache dir
+@@ -555,17 +554,9 @@ def _real_main(argv=None):
          if opts.rm_cachedir:
              ydl.cache.remove()
  
+-        # Update version
+-        if opts.update_self:
+-            # If updater returns True, exit. Required for windows
+-            if update_self(ydl.to_screen, opts.verbose, ydl._opener):
+-                if actual_use:
+-                    parser.error('The program must exit for the update to complete')
+-                sys.exit()
+-
          # Maybe do nothing
-         if (len(all_urls) < 1) and (opts.load_info_filename is None):
+         if not actual_use:
 -            if opts.update_self or opts.rm_cachedir:
 +            if opts.rm_cachedir:
                  sys.exit()
--- www/youtube_dlc/files/patch-youtube__dlc_options.py
+++ www/youtube_dlc/files/patch-youtube__dlc_options.py
@@ -1,13 +1,13 @@
---- youtube_dlc/options.py.orig	2021-01-17 22:30:37 UTC
+--- youtube_dlc/options.py.orig	2021-02-17 12:04:51 UTC
 +++ youtube_dlc/options.py
-@@ -131,10 +131,6 @@ def parseOpts(overrideArguments=None):
+@@ -151,10 +151,6 @@ def parseOpts(overrideArguments=None):
          action='version',
          help='Print program version and exit')
      general.add_option(
 -        '-U', '--update',
 -        action='store_true', dest='update_self',
--        help='[BROKEN] Update this program to latest version. Make sure that you have sufficient permissions (run with sudo if needed)')
+-        help='Update this program to latest version. Make sure that you have sufficient permissions (run with sudo if needed)')
 -    general.add_option(
          '-i', '--ignore-errors', '--no-abort-on-error',
          action='store_true', dest='ignoreerrors', default=True,
-         help='Continue on download errors, for example to skip unavailable videos in a playlist (default)')
+         help='Continue on download errors, for example to skip unavailable videos in a playlist (default) (Alias: --no-abort-on-error)')
