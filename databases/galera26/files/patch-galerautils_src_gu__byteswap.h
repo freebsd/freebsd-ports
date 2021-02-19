11718094e20bddb8188b82875fa762dccaf1b511
Fernando Apesteguía <fernape@FreeBSD.org>
2021-02-19 12:46:26 +0000

    databases/galera26: Fix build on FreeBSD13
    Use bswap macros as provided by FreeBSD.
    
    While here regenerate some patches and tidy Makefile up.
    
    PR:	253612
    Submitted by:	devin@sevenlayer.studio
    Approved by:	devel@galeracluster.com (maintainer)

--- databases/galera26/files/patch-galerautils_src_gu__byteswap.h
+++ databases/galera26/files/patch-galerautils_src_gu__byteswap.h
@@ -1,22 +1,14 @@
 --- galerautils/src/gu_byteswap.h.orig	2020-10-12 06:33:51 UTC
 +++ galerautils/src/gu_byteswap.h
-@@ -54,10 +54,15 @@ static GU_FORCE_INLINE uint64_t GU_ROTL64 (uint64_t x,
+@@ -53,11 +53,6 @@ static GU_FORCE_INLINE uint64_t GU_ROTL64 (uint64_t x,
+ #  define gu_bswap16 _OSSwapInt16
  #  define gu_bswap32 _OSSwapInt32
  #  define gu_bswap64 _OSSwapInt64
- #elif defined(__FreeBSD__)
+-#elif defined(__FreeBSD__)
 -/* do not use bswapXX, because gcc44 gives old-style cast warnings */
 -#  define gu_bswap16 __bswap16_var
 -#  define gu_bswap32 __bswap32_var
 -#  define gu_bswap64 __bswap64_var
-+#  if defined(__aarch64__) || defined(__arm__)
-+#    define gu_bswap16 bswap16
-+#    define gu_bswap32 bswap32
-+#    define gu_bswap64 bswap64
-+#  else
-+#    define gu_bswap16 __bswap16_var
-+#    define gu_bswap32 __bswap32_var
-+#    define gu_bswap64 __bswap64_var
-+#  endif // __FreeBSD__
  #elif defined(__sun__)
  #  define gu_bswap16 BSWAP_16
  #  define gu_bswap32 BSWAP_32
