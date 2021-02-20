89db97cf930dd126db22b12cff5e99d35b4c82a5
Thierry Thomas <thierry@FreeBSD.org>
2021-02-20 09:09:31 +0000

    Upgrade to 2.5.
    Release notes at
    <https://networkx.org/documentation/stable/release/release_2.5.html>.
    
    PR:		253540
    Submitted by:	/me
    Approved by:	dikshie (maintainer)

--- math/py-networkx/files/patch-setup.py
+++ math/py-networkx/files/patch-setup.py
@@ -1,36 +1,44 @@
---- setup.py.orig	2018-08-20 04:50:16 UTC
+--- setup.py.orig	2021-02-15 16:30:03 UTC
 +++ setup.py
-@@ -57,28 +57,6 @@ packages = ["networkx",
-             "networkx.testing",
-             "networkx.utils"]
+@@ -64,36 +64,6 @@ packages = [
+     "networkx.utils",
+ ]
  
--docdirbase = 'share/doc/networkx-%s' % version
+-docdirbase = "share/doc/networkx-%s" % version
 -# add basic documentation
 -data = [(docdirbase, glob("*.txt"))]
 -# add examples
--for d in ['advanced',
--          'algorithms',
--          'basic',
--          '3d_drawing',
--          'drawing',
--          'graph',
--          'javascript',
--          'jit',
--          'pygraphviz',
--          'subclass']:
--    dd = os.path.join(docdirbase, 'examples', d)
--    pp = os.path.join('examples', d)
+-for d in [
+-    ".",
+-    "advanced",
+-    "algorithms",
+-    "basic",
+-    "3d_drawing",
+-    "drawing",
+-    "graph",
+-    "javascript",
+-    "jit",
+-    "pygraphviz",
+-    "subclass",
+-]:
+-    dd = os.path.join(docdirbase, "examples", d)
+-    pp = os.path.join("examples", d)
+-    data.append((dd, glob(os.path.join(pp, "*.txt"))))
 -    data.append((dd, glob(os.path.join(pp, "*.py"))))
 -    data.append((dd, glob(os.path.join(pp, "*.bz2"))))
 -    data.append((dd, glob(os.path.join(pp, "*.gz"))))
 -    data.append((dd, glob(os.path.join(pp, "*.mbox"))))
 -    data.append((dd, glob(os.path.join(pp, "*.edgelist"))))
+-# add js force examples
+-dd = os.path.join(docdirbase, "examples", "javascript/force")
+-pp = os.path.join("examples", "javascript/force")
+-data.append((dd, glob(os.path.join(pp, "*"))))
 -
  # add the tests
  package_data = {
-     'networkx': ['tests/*.py'],
-@@ -130,7 +108,6 @@ if __name__ == "__main__":
-         download_url=release.download_url,
+     "networkx": ["tests/*.py"],
+@@ -169,7 +139,6 @@ if __name__ == "__main__":
+         project_urls=release.project_urls,
          classifiers=release.classifiers,
          packages=packages,
 -        data_files=data,
