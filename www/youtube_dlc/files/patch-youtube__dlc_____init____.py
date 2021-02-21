1501b3de0f3b2b050c9767f0f69e4d0a781809e3
Tobias C. Berner <tcberner@FreeBSD.org>
2021-02-21 06:23:42 +0000

    www/youtube_dlc: Update to 2021.02.19
    PR:		253732
    Submitted by:	daniel.engberg.lists@pyret.net

--- www/youtube_dlc/files/patch-youtube__dlc_____init____.py
+++ www/youtube_dlc/files/patch-youtube__dlc_____init____.py
@@ -1,4 +1,4 @@
---- youtube_dlc/__init__.py.orig	2021-02-17 11:58:10 UTC
+--- youtube_dlc/__init__.py.orig	2021-02-19 21:44:01 UTC
 +++ youtube_dlc/__init__.py
 @@ -38,7 +38,6 @@ from .utils import (
      std_headers,
@@ -8,7 +8,7 @@
  from .downloader import (
      FileDownloader,
  )
-@@ -555,17 +554,9 @@ def _real_main(argv=None):
+@@ -556,17 +555,9 @@ def _real_main(argv=None):
          if opts.rm_cachedir:
              ydl.cache.remove()
  
@@ -17,7 +17,7 @@
 -            # If updater returns True, exit. Required for windows
 -            if update_self(ydl.to_screen, opts.verbose, ydl._opener):
 -                if actual_use:
--                    parser.error('The program must exit for the update to complete')
+-                    sys.exit('ERROR: The program must exit for the update to complete')
 -                sys.exit()
 -
          # Maybe do nothing
