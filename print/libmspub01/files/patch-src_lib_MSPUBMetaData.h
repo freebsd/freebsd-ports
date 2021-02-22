5f19d593b30fce505bf355f4e8ca70585ab59d19
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-02-22 20:54:46 +0000

    print/libmspub01: fix build on GCC architectures
    Include sys/types.h:
    In file included from MSPUBMetaData.cpp:10:
    MSPUBMetaData.h:38:60: error: 'uint32_t' has not been declared
       38 |   void readPropertySet(librevenge::RVNGInputStream *input, uint32_t offset, char *FMTID);

---
+++ print/libmspub01/files/patch-src_lib_MSPUBMetaData.h
@@ -0,0 +1,10 @@
+--- src/lib/MSPUBMetaData.h.orig	2021-02-22 20:49:29 UTC
++++ src/lib/MSPUBMetaData.h
+@@ -13,6 +13,7 @@
+ #include <map>
+ #include <utility>
+ #include <vector>
++#include <sys/types.h>
+ 
+ #include <librevenge/librevenge.h>
+ 
