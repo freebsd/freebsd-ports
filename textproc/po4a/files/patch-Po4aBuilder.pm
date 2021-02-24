cf341c22eaa59d494f59cd9d642a53523f871c47
Danilo G. Baio <dbaio@FreeBSD.org>
2021-02-24 00:21:55 +0000

    textproc/po4a: Update to 0.63
    Take MAINTAINER'ship.
    
    Changelog:	https://github.com/mquinson/po4a/blob/v0.63/NEWS
    
    Approved by:	garga (maintainer)
    Differential Revision:	https://reviews.freebsd.org/D28875

--- textproc/po4a/files/patch-Po4aBuilder.pm
+++ textproc/po4a/files/patch-Po4aBuilder.pm
@@ -1,6 +1,6 @@
---- Po4aBuilder.pm.orig	2017-03-29 14:02:01 UTC
+--- Po4aBuilder.pm.orig	2021-02-21 00:56:23 UTC
 +++ Po4aBuilder.pm
-@@ -140,8 +140,8 @@ sub ACTION_install {
+@@ -118,8 +118,8 @@ sub ACTION_install {
      $mandir =~ s,/man3$,,;
      $self->install_path(man => $mandir);
  
