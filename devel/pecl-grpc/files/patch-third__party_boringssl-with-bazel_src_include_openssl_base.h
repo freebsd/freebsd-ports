658b75716b18623038da1aafb07e99c17f9e4572
Vanilla I. Shu <vanilla@FreeBSD.org>
2021-03-02 14:39:47 +0000

    Update to 1.36.0.

--- devel/pecl-grpc/files/patch-third__party_boringssl-with-bazel_src_include_openssl_base.h
+++ devel/pecl-grpc/files/patch-third__party_boringssl-with-bazel_src_include_openssl_base.h
@@ -1,17 +1,17 @@
---- third_party/boringssl-with-bazel/src/include/openssl/base.h.orig	2020-04-04 11:13:35 UTC
+--- third_party/boringssl-with-bazel/src/include/openssl/base.h.orig	2021-03-01 21:34:01 UTC
 +++ third_party/boringssl-with-bazel/src/include/openssl/base.h
 @@ -96,9 +96,13 @@ extern "C" {
- #elif defined(__arm) || defined(__arm__) || defined(_M_ARM)
+ #elif defined(__ARMEL__) || defined(_M_ARM)
  #define OPENSSL_32_BIT
  #define OPENSSL_ARM
 -#elif (defined(__PPC64__) || defined(__powerpc64__)) && defined(_LITTLE_ENDIAN)
-+#elif (defined(__PPC64__) || defined(__powerpc64__)) && defined(__LITTLE_ENDIAN__)
++#elif (defined(__PPC64__) || defined(__powerpc64__)) && defined(_LITTLE_ENDIAN__)
  #define OPENSSL_64_BIT
  #define OPENSSL_PPC64LE
 +#elif (defined(__PPC64__) || defined(__powerpc64__))
 +#define OPENSSL_64_BIT
 +#define OPENSSL_PPC64
 +#define OPENSSL_BIGENDIAN
- #elif defined(__mips__) && !defined(__LP64__)
+ #elif defined(__MIPSEL__) && !defined(__LP64__)
  #define OPENSSL_32_BIT
  #define OPENSSL_MIPS
