4e3370f404e9b3b131eab1e1ba4cc711684c7783
Sunpoet Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-03-03 18:13:01 +0000

    Move tests under tomlkit namespace to avoid conflicts with other ports
    - Bump PORTREVISION for package change

---
+++ textproc/py-tomlkit/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-07-31 10:23:07 UTC
++++ setup.py
+@@ -2,7 +2,7 @@
+ from setuptools import setup
+ 
+ packages = \
+-['tests', 'tomlkit']
++['tomlkit']
+ 
+ package_data = \
+ {'': ['*'],
