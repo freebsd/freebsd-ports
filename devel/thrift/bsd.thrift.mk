3005bc54ded5ed378f6b6b3904f5362c8fb79efd
Matthias Fechner <mfechner@FreeBSD.org>
2021-03-03 09:25:38 +0000

    Update thrift ports to 0.13.0 which is required for gitlab-ce upgrade.
    PR:		253804
    Approved by:	maintainer timeout

--- devel/thrift/bsd.thrift.mk
+++ devel/thrift/bsd.thrift.mk
@@ -5,7 +5,7 @@
 # in your makefile, set:
 # PORTVERSION=	${THRIFT_PORTVERSION}
 # see $PORTSDIR/devel/thrift for examples 
-THRIFT_PORTVERSION=	0.11.0
+THRIFT_PORTVERSION=	0.13.0
 
 CONFIGURE_ARGS+=	\
 		--without-tests
