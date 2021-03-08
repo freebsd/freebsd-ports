fa23000273c95286d1437d365930301fd1b1a7ec
Brooks Davis <brooks@FreeBSD.org>
2021-03-08 21:01:41 +0000

    Update to a new snapshot (now on the 13.0.0 line).
    Disable LLDB docs due to build issues.
    
    Reduce dependencies in core LLVM and Clang bits.
    
    Sponsored by:	DARPA

--- devel/llvm-devel/files/patch-cmake_modules_AddSphinxTarget.cmake
+++ devel/llvm-devel/files/patch-cmake_modules_AddSphinxTarget.cmake
@@ -1,11 +1,11 @@
 --- llvm/cmake/modules/AddSphinxTarget.cmake.orig
 +++ llvm/cmake/modules/AddSphinxTarget.cmake
-@@ -29,6 +29,8 @@
+@@ -39,6 +39,8 @@
    endif()
  
    add_custom_target(${SPHINX_TARGET_NAME}
-+		    COMMAND ${CMAKE_COMMAND}
++                    COMMAND ${CMAKE_COMMAND}
 +                            -E make_directory ${SPHINX_BUILD_DIR}
-                     COMMAND ${SPHINX_EXECUTABLE}
+                     COMMAND ${CMAKE_COMMAND} -E env ${ARG_ENV_VARS}
+                             ${SPHINX_EXECUTABLE}
                              -b ${builder}
-                             -d "${SPHINX_DOC_TREE_DIR}"
---
+++ devel/llvm-devel/files/patch-lldb_docs_conf.py
@@ -0,0 +1,10 @@
+--- lldb/docs/conf.py.orig
++++ lldb/docs/conf.py
+@@ -130,7 +130,6 @@
+ # further.  For a list of options available for each theme, see the
+ # documentation.
+ html_theme_options = {
+-    'font_size': '11pt',
+     # Don't generate any links to GitHub.
+     'github_button' : 'false',
+ }
