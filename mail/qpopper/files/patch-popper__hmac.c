b79a15d92b819759340147d6aa3b7a6757cc3884
Eugene Grosbein <eugen@FreeBSD.org>
2021-03-10 06:08:24 +0000

    mail/qpopper: small patch cleanup
    Avoid duplicate #include <sys/types.h>

--- mail/qpopper/files/patch-popper__hmac.c
+++ mail/qpopper/files/patch-popper__hmac.c
@@ -1,15 +1,14 @@
---- popper/hmac.c.orig	2008-06-27 17:40:15.000000000 -0700
-+++ popper/hmac.c	2011-06-08 21:57:42.000000000 -0700
-@@ -13,7 +13,11 @@
+--- popper/hmac.c.orig	2008-06-28 08:40:15.000000000 +0800
++++ popper/hmac.c	2021-03-10 13:06:10.554159000 +0700
+@@ -13,7 +13,10 @@
  #include <stdio.h>
  #include <string.h>
  #include <sys/types.h>
 -#include "md5.h"
-+#include <sys/types.h>
 +#include <md5.h>
 +
 +#define UINT4 u_int32_t
 +
  #include "hmac-md5.h"
  
-
+ 
