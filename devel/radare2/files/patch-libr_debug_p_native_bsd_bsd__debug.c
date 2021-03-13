a52023e4c99a868db6ce55fb80985a97296e199a
Steve Wills <swills@FreeBSD.org>
2021-03-13 05:51:26 +0000

    devel/radare2: keep KF_TYPE_CRYPTO where possible [1]
    While here, clean up a bit
    
    PR:		253923 [1]
    Submitted by:	Michael Zhilin <mizhka@gmail.com> [1]

--- devel/radare2/files/patch-libr_debug_p_native_bsd_bsd__debug.c
+++ devel/radare2/files/patch-libr_debug_p_native_bsd_bsd__debug.c
@@ -1,10 +1,12 @@
---- libr/debug/p/native/bsd/bsd_debug.c.orig	2021-02-13 20:27:22 UTC
+--- libr/debug/p/native/bsd/bsd_debug.c.orig	2021-02-11 00:59:05 UTC
 +++ libr/debug/p/native/bsd/bsd_debug.c
-@@ -488,7 +488,6 @@ RList *bsd_desc_list(int pid) {
+@@ -488,7 +488,9 @@ RList *bsd_desc_list(int pid) {
  		case KF_TYPE_PIPE: type = 'p'; break;
  		case KF_TYPE_FIFO: type = 'f'; break;
  		case KF_TYPE_KQUEUE: type = 'k'; break;
--		case KF_TYPE_CRYPTO: type = 'c'; break;
++#if __FreeBSD_version < 1300130
+ 		case KF_TYPE_CRYPTO: type = 'c'; break;
++#endif
  		case KF_TYPE_MQUEUE: type = 'm'; break;
  		case KF_TYPE_SHM: type = 'h'; break;
  		case KF_TYPE_PTS: type = 't'; break;
