09aa78f86789a71c77a6fc3cefc5862dda35911a
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2021-03-13 16:13:22 +0000

    update FreeCAD to 0.19.1
    https://wiki.freecadweb.org/Release_notes_0.19

--- cad/freecad/files/patch-cMake_FindPySide2Tools.cmake
+++ cad/freecad/files/patch-cMake_FindPySide2Tools.cmake
@@ -1,21 +1,19 @@
---- cMake/FindPySide2Tools.cmake.orig	2019-10-22 16:53:35 UTC
+--- cMake/FindPySide2Tools.cmake.orig	2021-03-12 22:05:53 UTC
 +++ cMake/FindPySide2Tools.cmake
-@@ -42,7 +42,7 @@ MACRO(PYSIDE_WRAP_UI outfiles)
+@@ -58,7 +58,7 @@
          # we follow the tool command with in-place sed.
          ADD_CUSTOM_COMMAND(OUTPUT ${outfile}
-           COMMAND "${PYSIDE2UICBINARY}" "${infile}" -o "${outfile}"
+           COMMAND "${PYSIDE2UICBINARY}" ${UICOPTIONS} "${infile}" -o "${outfile}"
 -          COMMAND sed -i "/^# /d" "${outfile}"
 +          COMMAND sed -i \"\" "/^# /d" "${outfile}"
            MAIN_DEPENDENCY "${infile}"
          )
      endif()
-@@ -68,8 +68,8 @@ MACRO(PYSIDE_WRAP_RC outfiles)
-         # pyside-rcc generates in comments at beginning, which is why
+@@ -85,7 +85,7 @@
          # we follow the tool command with in-place sed.
          ADD_CUSTOM_COMMAND(OUTPUT "${outfile}"
--          COMMAND "${PYSIDE2RCCBINARY}" "${infile}" ${PY_ATTRIBUTE} -o "${outfile}"
+           COMMAND "${PYSIDE2RCCBINARY}" ${RCCOPTIONS} "${infile}" ${PY_ATTRIBUTE} -o "${outfile}"
 -          COMMAND sed -i "/^# /d" "${outfile}"
-+          COMMAND "${PYSIDE2RCCBINARY}" "${infile}" --no-compress --generator python -o "${outfile}"
 +          COMMAND sed -i \"\" "/^# /d" "${outfile}"
            MAIN_DEPENDENCY "${infile}"
          )
