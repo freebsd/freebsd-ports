f333b2924814e9e3b0aa9ae0dd58890f869871cb
Dmitry Marakasov <amdmi3@FreeBSD.org>
2021-03-14 19:26:52 +0000

    - Update to 2021-03-07

---
+++ graphics/bonzomatic/files/patch-src_platform__x11_Misc.cpp
@@ -0,0 +1,10 @@
+--- src/platform_x11/Misc.cpp.orig	2021-03-07 12:52:50 UTC
++++ src/platform_x11/Misc.cpp
+@@ -7,6 +7,7 @@
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <limits.h>
++#include <locale.h>
+ 
+ #include <fontconfig/fontconfig.h>
+ 
