ca01e8ba1b92f70d06f9f1e28b041e2a1b8d636a
Bernard Spil <brnrd@FreeBSD.org>
2021-03-14 19:42:04 +0000

    sysutils/passwordsafe: Update to 1.12.0

--- sysutils/passwordsafe/files/patch-src_ui_wxWidgets_wxUtilities.cpp
+++ sysutils/passwordsafe/files/patch-src_ui_wxWidgets_wxUtilities.cpp
@@ -5,20 +5,14 @@ Subject: [PATCH] Unbreak build on FreeBSD
 
 The function wxLinuxDistributionInfo is only available on Linux. Add check for LINUX symbol as per [documentation](http://docs.wxwidgets.org/3.0/group__group__funcmacro__networkuseros.html#ga06f6fb212c396bd20865ee4e2f69aa1f)
 `This function is Linux-specific and is only available when the LINUX symbol is defined. `
----
- src/ui/wxWidgets/wxutils.cpp | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/src/ui/wxWidgets/wxutils.cpp b/src/ui/wxWidgets/wxutils.cpp
-index 25faa5310..027ee69b4 100644
---- src/ui/wxWidgets/wxutils.cpp.orig
-+++ src/ui/wxWidgets/wxutils.cpp
-@@ -203,7 +203,7 @@ int pless(int* first, int* second) { return *first - *second; }
+--- src/ui/wxWidgets/wxUtilities.cpp.orig	2020-10-28 19:48:24 UTC
++++ src/ui/wxWidgets/wxUtilities.cpp
+@@ -203,7 +203,7 @@ int pless(int* first, int* second) { return *first - *
  // on Fedora or Ubuntu
  bool IsTaskBarIconAvailable()
  {
--#ifdef __WXGTK__
+-#if defined(__WXGTK__)
 +#if defined(__WXGTK__) && defined(LINUX)
-   const wxLinuxDistributionInfo ldi = wxGetLinuxDistributionInfo();
-   if (ldi.Id.IsEmpty() || ldi.Id == wxT("Ubuntu") || ldi.Id == wxT("Fedora"))
-     return false;
+   const wxVersionInfo verInfo = wxGetLibraryVersionInfo();
+   int major = verInfo.GetMajor();
+   int minor = verInfo.GetMinor();
