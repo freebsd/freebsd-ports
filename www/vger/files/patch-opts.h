ec10202a7c27fb6b0b5bb5f9817d9408648e7ee6
Neel Chauhan <nc@FreeBSD.org>
2021-03-15 19:58:59 +0000

    www/vger: Use /usr/local/etc/gemini instead of /var/gemini

---
+++ www/vger/files/patch-opts.h
@@ -0,0 +1,11 @@
+--- opts.h.orig	2021-03-15 19:55:02 UTC
++++ opts.h
+@@ -2,7 +2,7 @@
+ 
+ #define DEFAULT_MIME	 "application/octet-stream"
+ #define DEFAULT_LANG	 ""
+-#define DEFAULT_CHROOT	 "/var/gemini"
++#define DEFAULT_CHROOT	 "/usr/local/etc/gemini"
+ #define DEFAULT_AUTOIDX  0
+ 
+  /* longest is 56 so 64 should be enough */
