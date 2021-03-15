a25ab1fd80178a79251d19b33cecc1bd28f3b8a4
Jason E. Hale <jhale@FreeBSD.org>
2021-03-15 14:41:40 +0000

    editors/mp: Update to 5.47

--- editors/mp/files/patch-mpdm_mpdm__f.c
+++ editors/mp/files/patch-mpdm_mpdm__f.c
@@ -1,5 +1,12 @@
-mpdm_f.c:1947:9: error: use of undeclared identifier 'environ'
-        environ = (char **) calloc(sizeof(char *), mpdm_size(v) + 1);
+mpdm_f.c:2395:24: error: variable has incomplete type 'struct sockaddr_in'
+    struct sockaddr_in host;
+                       ^
+mpdm_f.c:2395:12: note: forward declaration of 'struct sockaddr_in'
+    struct sockaddr_in host;
+           ^
+mpdm_f.c:2417:23: warning: implicit declaration of function 'htons' is invalid in C99 [-Wimplicit-function-declaration]
+    host.sin_port   = htons(p);
+                      ^
 
 --- mpdm/mpdm_f.c.orig	2020-11-22 08:58:48 UTC
 +++ mpdm/mpdm_f.c
@@ -12,11 +19,3 @@ mpdm_f.c:1947:9: error: use of undeclared identifier 'environ'
  #include "mpdm.h"
  
  #ifdef CONFOPT_ICONV
-@@ -1974,6 +1976,7 @@ static int sysdep_popen(mpdm_t v, char *prg, int rw)
- 
-         /* build the environment for the subprocess */
-         v = mpdm_join(mpdm_get_wcs(mpdm_root(), L"ENV"), MPDM_S(L"="));
-+        extern char **environ;
-         environ = (char **) calloc(sizeof(char *), mpdm_size(v) + 1);
- 
-         mpdm_ref(v);
