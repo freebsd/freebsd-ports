eb381407531f6439bf316724d4f44be0854e1d4e
Cy Schubert <cy@FreeBSD.org>
2021-03-16 16:39:41 +0000

    sysutils/mxkill x11/mxascii: Fix FreeBSD-11 build
    FreeBSD-11 still contains #error in malloc.h rather than include
    stdlib.h.
    
    Reported by:	linimon

---
+++ sysutils/mxkill/files/patch-xhead.h
@@ -0,0 +1,10 @@
+--- xhead.h.orig	1997-03-02 11:06:48.000000000 -0800
++++ xhead.h	2021-03-16 08:52:47.491466000 -0700
+@@ -26,7 +26,6 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <malloc.h>
+ #include <memory.h>
+ #include <ctype.h>
+ #include <string.h>
---
+++ x11/mxascii/files/patch-xhead.h
@@ -0,0 +1,10 @@
+--- xhead.h.orig	1996-06-18 15:52:31.000000000 -0700
++++ xhead.h	2021-03-16 08:52:44.972493000 -0700
+@@ -26,7 +26,6 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+-#include <malloc.h>
+ #include <memory.h>
+ #include <ctype.h>
+ #include <string.h>
