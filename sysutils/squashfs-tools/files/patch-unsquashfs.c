afcefdd40e21caa2be7372ec0d7a40e0d1651d67
Alexey Dokuchaev <danfe@FreeBSD.org>
2021-03-16 06:20:24 +0000

    - Update to version 4.4 and unforbid - Add LZ4 and ZSTD compression support - Improve or drop option descriptions - Convert to option helpers while here
    Security:	317487c6-85ca-11eb-80fa-14dae938ec40

--- sysutils/squashfs-tools/files/patch-unsquashfs.c
+++ sysutils/squashfs-tools/files/patch-unsquashfs.c
@@ -1,17 +1,16 @@
---- unsquashfs.c.orig	2014-05-12 22:18:35 UTC
+--- unsquashfs.c.orig	2019-08-29 01:58:04 UTC
 +++ unsquashfs.c
-@@ -31,7 +31,14 @@
- #include "unsquashfs_info.h"
+@@ -32,8 +32,13 @@
  #include "stdarg.h"
+ #include "fnmatch_compat.h"
  
 +#ifdef __linux__
  #include <sys/sysinfo.h>
+ #include <sys/sysmacros.h>
 +#endif
-+
 +#ifdef __FreeBSD__
 +#include <sys/sysctl.h>
 +#endif
-+
  #include <sys/types.h>
  #include <sys/time.h>
  #include <sys/resource.h>
