2633429ed5b4236d7488c351a10168431b821bbb
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2021-03-20 20:10:16 +0000

    update py-pivy to 0.6.6
    https://github.com/coin3d/pivy/releases/tag/0.6.6

--- graphics/py-pivy/files/patch-setup.py
+++ graphics/py-pivy/files/patch-setup.py
@@ -1,11 +1,22 @@
---- setup.py.orig	2020-01-12 21:51:59 UTC
+--- setup.py.orig	2021-03-19 15:41:45 UTC
 +++ setup.py
-@@ -201,7 +201,7 @@ class pivy_build(build):
+@@ -201,8 +201,8 @@ class pivy_build(build):
          print(yellow('calling: ' + cmake_command[0] + ' ' + cmake_command[1]))
          cmake = subprocess.Popen(cmake_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
          cmake_out, cmake_err = cmake.communicate()
 -        coin_vars = ['COIN_FOUND', 'COIN_VERSION', 'COIN_INCLUDE_DIR', 'COIN_LIB_DIR']
+-        soqt_vars = ['SOQT_FOUND', 'SOQT_VERSION', 'SOQT_INCLUDE_DIR', 'SOQT_LIB_DIR']
 +        coin_vars = ['COIN_FOUND', 'COIN_INCLUDE_DIR', 'COIN_LIB_DIR']
-         soqt_vars = ['SOQT_FOUND', 'SOQT_VERSION', 'SOQT_INCLUDE_DIR', 'SOQT_LIB_DIR']
++        soqt_vars = ['SOQT_VERSION', 'SOQT_INCLUDE_DIR', 'SOQT_LIB_DIR']
          config_dict = {}
          print(yellow(cmake_out.decode("utf-8")))
+         print(red(cmake_err.decode("utf-8")))
+@@ -228,7 +228,7 @@ class pivy_build(build):
+             if key in config_dict:
+                 print(blue(key + ': ' + config_dict[key]))
+ 
+-        if config_dict.get('SOQT_FOUND', 'false') == 'false':
++        if config_dict.get('SOQT_INCLUDE_DIR', 'false') == 'false':
+             pivy_build.MODULES.pop('soqt')
+             print(red("\ndisable soqt, because cmake couldn't find it"))
+         else:
