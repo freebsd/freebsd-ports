7d6cf6331c81463bebca7db6e7d5fe2b207df6fd
Neel Chauhan <nc@FreeBSD.org>
2021-03-23 16:55:27 +0000

    games/libretro-play: update to 0.38.0.20210320
    PR:		254471
    Submitted by:	rozhuk.im AT gmail DOT com (maintainer)

--- games/libretro-play/files/patch-deps_Dependencies_cmake-modules_Header.cmake
+++ games/libretro-play/files/patch-deps_Dependencies_cmake-modules_Header.cmake
@@ -1,20 +1,17 @@
 --- deps/Dependencies/cmake-modules/Header.cmake.orig	2020-07-29 22:03:00 UTC
 +++ deps/Dependencies/cmake-modules/Header.cmake
-@@ -27,12 +27,15 @@ if(CMAKE_CURRENT_SOURCE_DIR STREQUAL "${CMAKE_SOURCE_D
- 		if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "^(aarch64.*|AARCH64.*)")
- 			message("-- Arch: aarch64 --")
- 			set(TARGET_PLATFORM_UNIX_AARCH64 TRUE)
+@@ -30,11 +30,12 @@
+ 		elseif(${CMAKE_SYSTEM_PROCESSOR} MATCHES "^arm")
+ 			message("-- Arch: arm --")
+ 			set(TARGET_PLATFORM_UNIX_ARM TRUE)
 +		elseif(${CMAKE_SYSTEM_PROCESSOR} MATCHES "^(ppc64.*|powerpc64.*)")
 +			message("-- Arch: powerpc64 --")
 +			set(TARGET_PLATFORM_UNIX_POWERPC64 TRUE)
  		endif()
- 
  	endif()
  	
 -	set(CMAKE_CXX_STANDARD 17)
 -	set(CMAKE_CXX_STANDARD_REQUIRED ON)
-+	
-+	
  	if(TARGET_PLATFORM_WIN32)
  		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc /MP")
  	endif()
