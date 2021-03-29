16686cacc5192f2120615b0461d31e2d1590e3a9
Dan Langille <dvl@FreeBSD.org>
2021-03-29 14:41:29 +0000

    Update to 21.3.0
    Upstream has changed their release number, but I don't think that will affect
    us at all. It appears to follow year.month
    
    I have sent some newly added patches upstream
    (https://github.com/librenms/librenms/pull/12647) and they will be removed
    from here on the next release.
    
    This update jumps over 21.20, 21.1.0, & 17.0.1 for no reason.
    
    While here, include a patch from an outstanding PR
    
    PR:		252640
    Submitted by:	geoffroy desvernay <dgeo@centrale-marseille.fr>

--- net-mgmt/librenms/files/patch-poller-wrapper.py
+++ net-mgmt/librenms/files/patch-poller-wrapper.py
@@ -1,11 +1,11 @@
---- poller-wrapper.py.orig	2020-07-20 20:50:23 UTC
+--- poller-wrapper.py.orig	2021-01-14 02:21:36 UTC
 +++ poller-wrapper.py
-@@ -27,6 +27,8 @@
- 
- import LibreNMS.library as LNMS
+@@ -44,6 +44,8 @@ except ImportError as exc:
+     print('ERROR: %s' % exc)
+     sys.exit(2)
  
 +os.environ['PATH'] += ':/usr/local/sbin:/usr/local/bin'
 +
- try:
  
-     import json
+ APP_NAME = "poller_wrapper"
+ LOG_FILE = "logs/" + APP_NAME + ".log"
