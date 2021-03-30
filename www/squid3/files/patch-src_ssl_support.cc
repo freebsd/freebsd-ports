c8d44ceb8947dea57f6950f51bfd356208d4a96f
Alexey Dokuchaev <danfe@FreeBSD.org>
2021-03-30 12:10:31 +0000

    Resolve contradiction between BROKEN_SSL=openssl and advice to add DEFAULT_VERSIONS+=ssl=openssl to /etc/make.conf as a workaround.
    Squid 3.5 requires pre-1.1 versions of OpenSSL, and that can be only
    satisfied by the base system on 11.x versions of FreeBSD; doing the
    DEFAULT_VERSIONS+=ssl=openssl would not work.  Unfortunately, making
    proper conversion from CRYPTO_LOCK_SSL_* to new multi-threading API
    does not look feasible.  Fortunately, LibreSSL could be used as a
    drop-in replacement.
    
    That said, change the advice to use DEFAULT_VERSIONS+=ssl=libressl
    instead, and add a couple of minor patches to unbreak the build.

---
+++ www/squid3/files/patch-src_ssl_support.cc
@@ -0,0 +1,11 @@
+--- src/ssl/support.cc.orig	2018-07-15 20:46:55 UTC
++++ src/ssl/support.cc
+@@ -2159,7 +2159,7 @@ remove_session_cb(SSL_CTX *, SSL_SESSION *sessionID)
+ }
+ 
+ static SSL_SESSION *
+-get_session_cb(SSL *, unsigned char *sessionID, int len, int *copy)
++get_session_cb(SSL *, const unsigned char *sessionID, int len, int *copy)
+ {
+     if (!SslSessionCache)
+         return NULL;
