ff5d36478476044aef26d9cfa51f6d42d6cb50cd
Kevin Bowling <kbowling@FreeBSD.org>
2021-04-07 23:37:45 -0700

    devel/libegit2: fix with emacs-devel
    PR:		254685
    Reported by:	Sean Champ <lab+bsd@thinkum.space>
    Approved by:	Yasuhiro Kimura <yasu@utahime.org> (maintainer)

--- devel/libegit2/files/patch-libgit.el
+++ devel/libegit2/files/patch-libgit.el
@@ -1,11 +1,11 @@
---- libgit.el.orig	2020-05-15 17:59:08 UTC
-+++ libgit.el
+--- libgit.el.orig	2020-05-15 17:59:08.000000000 +0000
++++ libgit.el	2021-04-01 04:02:35.637193000 +0000
 @@ -41,7 +41,7 @@
    "Directory where the libegit2 dynamic module file should be built.")
  
  (defvar libgit--module-file
 -  (expand-file-name (concat "libegit2" module-file-suffix) libgit--build-dir)
-+  (expand-file-name (concat "libegit2" module-file-suffix) "/usr/local/share/emacs/27.1/site-lisp")
++  (expand-file-name (concat "libegit2" module-file-suffix) "%%LIBEGIT2_INSTALL_DIR%%")
    "Path to the libegit2 dynamic module file.")
  
  (defun libgit--configure ()
