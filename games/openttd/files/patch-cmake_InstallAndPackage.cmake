8c5a0d0eb2f465e003a8b7109131329f7f8bea3f
Alexey Dokuchaev <danfe@FreeBSD.org>
2021-04-08 02:48:56 +0000

    - Update OpenTTD to version 1.11.0 - Now requires C++17 and built with CMake - Finally, convert to options and helpers - Drop no longer needed Makefile targets

---
+++ games/openttd/files/patch-cmake_InstallAndPackage.cmake
@@ -0,0 +1,19 @@
+--- cmake/InstallAndPackage.cmake.orig	2021-04-01 12:33:44 UTC
++++ cmake/InstallAndPackage.cmake
+@@ -33,7 +33,6 @@ install(DIRECTORY
+         COMPONENT language_files)
+ 
+ install(FILES
+-                ${CMAKE_SOURCE_DIR}/COPYING.md
+                 ${CMAKE_SOURCE_DIR}/README.md
+                 ${CMAKE_SOURCE_DIR}/changelog.txt
+                 ${CMAKE_SOURCE_DIR}/docs/multiplayer.md
+@@ -58,7 +57,7 @@ if(OPTION_INSTALL_FHS)
+             COMPONENT manual)
+ endif()
+ 
+-if(UNIX AND NOT APPLE)
++if(UNIX AND NOT APPLE AND NOT OPTION_DEDICATED)
+     install(DIRECTORY
+                     ${CMAKE_BINARY_DIR}/media/icons
+                     ${CMAKE_BINARY_DIR}/media/pixmaps
