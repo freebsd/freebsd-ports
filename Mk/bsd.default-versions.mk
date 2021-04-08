930c93129234e5ed3f67be1b8795a5a20e2745db
Dmitry Marakasov <amdmi3@FreeBSD.org>
2021-04-09 01:31:49 +0300

    lang/python310: add python 3.10.0.beta4
    PR:		256150
    Approved by:	maintainer timeout (python@, 1 month)
    Differential Revision:	https://reviews.freebsd.org/D30475

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -105,11 +105,11 @@ PERL5_DEFAULT:=		${_PERL5_FROM_BIN:R}
 PGSQL_DEFAULT?=		12
 # Possible values: 7.3, 7.4, 8.0
 PHP_DEFAULT?=		7.4
-# Possible values: 2.7, 3.6, 3.7, 3.8, 3.9
+# Possible values: 2.7, 3.6, 3.7, 3.8, 3.9, 3.10
 PYTHON_DEFAULT?=	3.8
 # Possible values: 2.7
 PYTHON2_DEFAULT?=	2.7
-# Possible values: 3.6, 3.7, 3.8, 3.9
+# Possible values: 3.6, 3.7, 3.8, 3.9, 3.10
 PYTHON3_DEFAULT?=	3.8
 # Possible values: 2.6, 2.7, 3.0
 RUBY_DEFAULT?=		2.7
