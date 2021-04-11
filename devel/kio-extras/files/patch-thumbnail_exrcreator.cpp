6f771bd2f7b6d2bb78dc6cd35f74f5ad3b8808d1
Matthias Andree <mandree@FreeBSD.org>
2021-04-11 06:39:54 +0200

    devel/kio-extras: make compatible with OpenEXR/Imath 3.0

---
+++ devel/kio-extras/files/patch-thumbnail_exrcreator.cpp
@@ -0,0 +1,10 @@
+--- thumbnail/exrcreator.cpp.orig	2021-02-24 23:05:55 UTC
++++ thumbnail/exrcreator.cpp
+@@ -25,6 +25,7 @@
+ 
+ #include <ImfInputFile.h>
+ #include <ImfPreviewImage.h>
++#include <ImfHeader.h>
+ 
+ #include <ksharedconfig.h>
+ #include <kconfiggroup.h>
