9ab14902a13463a884c7446d358364ed5ee64088
Matthias Andree <mandree@FreeBSD.org>
2021-04-12 02:29:13 +0200

    graphics/gdal: make compatible with OpenEXR/Imath 3.0

---
+++ graphics/gdal/files/patch-frmts_exr_openexr__headers.h
@@ -0,0 +1,10 @@
+--- frmts/exr/openexr_headers.h.orig	2020-12-29 12:18:28 UTC
++++ frmts/exr/openexr_headers.h
+@@ -42,6 +42,7 @@
+ #include "ImfMatrixAttribute.h"
+ #include "ImfMultiPartInputFile.h"
+ #include "ImfMultiPartOutputFile.h"
++#include "ImfOutputPart.h"
+ #include "ImfPartType.h"
+ #include "ImfPreviewImage.h"
+ #include "ImfRgbaFile.h"
