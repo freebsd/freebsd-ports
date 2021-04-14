ef24a782a2a4f9dce3b37ecbbb6aa1f798cc96b3
Thierry Thomas <thierry@FreeBSD.org>
2021-04-14 17:44:51 +0200

    textproc/aspell: fix a bad regexp after location specifier
    PR:		253774

--- textproc/aspell/files/patch-modules__speller__default__affix.cpp
+++ textproc/aspell/files/patch-modules__speller__default__affix.cpp
@@ -1,6 +1,6 @@
---- modules/speller/default/affix.cpp.orig	2012-01-25 19:46:47.684690459 +0200
-+++ modules/speller/default/affix.cpp	2012-01-25 19:46:56.385691084 +0200
-@@ -796,7 +796,10 @@ bool AffixMgr::suffix_check (const Looku
+--- modules/speller/default/affix.cpp.orig	2019-10-08 00:15:21 UTC
++++ modules/speller/default/affix.cpp
+@@ -798,7 +798,10 @@ bool AffixMgr::suffix_check (const LookupInfo & linf, 
      if (se->check(linf, word, ci, gi, sfxopts, ppfx)) return true;
      se = se->next;
    }
--- textproc/aspell/files/patch-prog__aspell.cpp
+++ textproc/aspell/files/patch-prog__aspell.cpp
@@ -1,5 +1,5 @@
---- prog/aspell.cpp.orig	2011-07-04 11:13:58.000000000 +0200
-+++ prog/aspell.cpp	2014-02-11 22:42:04.000000000 +0100
+--- prog/aspell.cpp.orig	2019-10-08 00:15:21 UTC
++++ prog/aspell.cpp
 @@ -25,6 +25,7 @@
  # include <langinfo.h>
  #endif
@@ -7,8 +7,8 @@
 +#include "errors.hpp"
  #include "aspell.h"
  
- #ifdef USE_FILE_INO
-@@ -40,7 +41,6 @@
+ #include <sys/types.h>
+@@ -41,7 +42,6 @@
  #include "convert.hpp"
  #include "document_checker.hpp"
  #include "enumeration.hpp"
