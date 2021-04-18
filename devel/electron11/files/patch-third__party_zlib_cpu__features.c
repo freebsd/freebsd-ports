d06276deb159addfcf61a1ee598ae088d772e110
Hiroki Tagato <tagattie@FreeBSD.org>
2021-04-18 15:37:03 +0900

    devel/electron11: fix build on aarch64
    Submitted by:	mikael (via GitHub)

--- devel/electron11/files/patch-third__party_zlib_cpu__features.c
+++ devel/electron11/files/patch-third__party_zlib_cpu__features.c
@@ -23,6 +23,15 @@
  #include <cpu-features.h>
  #elif defined(ARMV8_OS_LINUX)
  #include <asm/hwcap.h>
+@@ -65,7 +65,7 @@ int ZLIB_INTERNAL x86_cpu_enable_simd = 0;
+ static void _cpu_check_features(void);
+ #endif
+ 
+-#if defined(ARMV8_OS_ANDROID) || defined(ARMV8_OS_LINUX) || defined(ARMV8_OS_MACOS) || defined(ARMV8_OS_FUCHSIA) || defined(X86_NOT_WINDOWS)
++#if defined(ARMV8_OS_ANDROID) || defined(ARMV8_OS_FREEBSD) || defined(ARMV8_OS_LINUX) || defined(ARMV8_OS_MACOS) || defined(ARMV8_OS_FUCHSIA) || defined(X86_NOT_WINDOWS)
+ #if !defined(ARMV8_OS_MACOS)
+ // _cpu_check_features() doesn't need to do anything on mac/arm since all
+ // features are known at build time, so don't call it.
 @@ -123,6 +132,13 @@ static void _cpu_check_features(void)
  #elif defined(ARMV8_OS_WINDOWS)
      arm_cpu_enable_crc32 = IsProcessorFeaturePresent(PF_ARM_V8_CRC32_INSTRUCTIONS_AVAILABLE);
