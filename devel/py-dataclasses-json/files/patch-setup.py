374b7d2c90f5ee227994cd7a43669eda7b3746dd
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-04-18 12:55:36 +0800

    devel/py-dataclasses-json: Add py-dataclasses-json 0.5.2
    This library provides a simple API for encoding and decoding dataclasses to and
    from JSON.
    
    WWW: https://github.com/lidatong/dataclasses-json

---
+++ devel/py-dataclasses-json/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-07-28 20:02:19 UTC
++++ setup.py
+@@ -21,7 +21,7 @@ setup(
+         'marshmallow>=3.3.0,<4.0.0',
+         'marshmallow-enum>=1.5.1,<2.0.0',
+         'typing-inspect>=0.4.0',
+-        'stringcase==1.2.0,<2.0.0'
++        'stringcase>=1.2.0,<2.0.0'
+     ],
+     python_requires='>=3.6',
+     extras_require={
