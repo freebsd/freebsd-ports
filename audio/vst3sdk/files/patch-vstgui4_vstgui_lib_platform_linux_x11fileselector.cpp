6e103bcb57b6011a7084e02622000166a41af1e9
Yuri Victorovich <yuri@FreeBSD.org>
2021-04-17 22:34:52 -0700

    audio/vst3sdk: Unbreak on 11 by adding #define _WITH_GETLINE for getline(3) to be visible
    Reported by:	fallout

--- audio/vst3sdk/files/patch-vstgui4_vstgui_lib_platform_linux_x11fileselector.cpp
+++ audio/vst3sdk/files/patch-vstgui4_vstgui_lib_platform_linux_x11fileselector.cpp
@@ -1,11 +1,12 @@
 - fix missing include, see https://github.com/steinbergmedia/vst3sdk/issues/80
 
---- vstgui4/vstgui/lib/platform/linux/x11fileselector.cpp.orig	2021-04-15 16:36:46 UTC
+--- vstgui4/vstgui/lib/platform/linux/x11fileselector.cpp.orig	2020-08-20 14:14:27 UTC
 +++ vstgui4/vstgui/lib/platform/linux/x11fileselector.cpp
-@@ -6,6 +6,8 @@
+@@ -6,6 +6,9 @@
  #include <unistd.h>
  #include <string>
  
++#define _WITH_GETLINE // this is still required on FreeBSD 11
 +#include <stdio.h>
 +
  //------------------------------------------------------------------------
