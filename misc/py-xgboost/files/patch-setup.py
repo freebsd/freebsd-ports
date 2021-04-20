97c28a6205418913c189cf2c1d28c5f5b4b212da
Yuri Victorovich <yuri@FreeBSD.org>
2021-04-20 13:47:07 -0700

    misc/py-xgboost: Update 1.2.1 -> 1.4.1
    Reported by:	portscout

--- misc/py-xgboost/files/patch-setup.py
+++ misc/py-xgboost/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2020-05-25 18:12:50 UTC
+--- setup.py.orig	2021-04-20 00:31:38 UTC
 +++ setup.py
-@@ -57,14 +57,14 @@ def copy_tree(src_dir, target_dir):
+@@ -60,14 +60,14 @@ def copy_tree(src_dir, target_dir):
  
      src = os.path.join(src_dir, 'src')
      inc = os.path.join(src_dir, 'include')
@@ -17,3 +17,21 @@
      clean_copy_tree(rabit, os.path.join(target_dir, 'rabit'))
      clean_copy_tree(cmake, os.path.join(target_dir, 'cmake'))
      clean_copy_tree(plugin, os.path.join(target_dir, 'plugin'))
+@@ -125,7 +125,7 @@ class BuildExt(build_ext.build_ext):  # pylint: disabl
+ 
+     def build_cmake_extension(self):
+         '''Configure and build using CMake'''
+-        if USER_OPTIONS['use-system-libxgboost'][2]:
++        if True or USER_OPTIONS['use-system-libxgboost'][2]: # see https://github.com/dmlc/xgboost/issues/6879
+             self.logger.info('Using system libxgboost.')
+             return
+ 
+@@ -217,7 +217,7 @@ class InstallLib(install_lib.install_lib):
+     def install(self):
+         outfiles = super().install()
+ 
+-        if USER_OPTIONS['use-system-libxgboost'][2] != 0:
++        if True or USER_OPTIONS['use-system-libxgboost'][2] != 0: # see https://github.com/dmlc/xgboost/issues/6879
+             self.logger.info('Using system libxgboost.')
+             lib_path = os.path.join(sys.prefix, 'lib')
+             msg = 'use-system-libxgboost is specified, but ' + lib_name() + \
--- misc/py-xgboost/files/patch-xgboost_libpath.py
+++ misc/py-xgboost/files/patch-xgboost_libpath.py
@@ -1,14 +1,14 @@
---- xgboost/libpath.py.orig	2020-10-12 22:10:16 UTC
+--- xgboost/libpath.py.orig	2021-04-20 00:31:38 UTC
 +++ xgboost/libpath.py
-@@ -4,6 +4,7 @@
- import os
+@@ -5,6 +5,7 @@ import os
  import platform
+ from typing import List
  import sys
 +import sysconfig # from https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=247408#c0
  
  
  class XGBoostLibraryNotFound(Exception):
-@@ -20,6 +21,8 @@ def find_lib_path():
+@@ -21,6 +22,8 @@ def find_lib_path() -> List[str]:
      """
      curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
      dll_path = [
