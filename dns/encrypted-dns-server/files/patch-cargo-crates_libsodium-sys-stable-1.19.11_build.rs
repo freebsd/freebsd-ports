45a4d45b166acd6956d4fb3ed7910a518c4595e4
Lewis Cook <lcook@FreeBSD.org>
2021-04-20 11:48:00 +0100

    dns/encrypted-dns-server: Update to 0.3.23
    Changes:	https://github.com/jedisct1/encrypted-dns-server/releases/tag/0.3.23
    PR:		255110
    Submitted by:	Felix Hanley <felix@userspace.com.au> (maintainer)

--- dns/encrypted-dns-server/files/patch-cargo-crates_libsodium-sys-stable-1.19.11_build.rs
+++ dns/encrypted-dns-server/files/patch-cargo-crates_libsodium-sys-stable-1.19.11_build.rs
@@ -1,5 +1,5 @@
---- cargo-crates/libsodium-sys-stable-1.19.11/build.rs.orig     2020-11-19 15:20:25 UTC
-+++ cargo-crates/libsodium-sys-stable-1.19.11/build.rs
+--- cargo-crates/libsodium-sys-stable-1.19.15/build.rs.orig     2020-11-19 15:20:25 UTC
++++ cargo-crates/libsodium-sys-stable-1.19.15/build.rs
 @@ -214,6 +214,7 @@ fn make_libsodium(target: &str, source_dir: &Path, ins
          cross_compiling = true;
          help = "";
