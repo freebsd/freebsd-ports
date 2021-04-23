d0d548bd3fc870e9eee050895da84528b2fa5521
Vanilla I. Shu <vanilla@FreeBSD.org>
2021-04-23 14:31:24 +0800

    devel/pecl-swoole: update to 4.6.6.

--- devel/pecl-swoole/files/patch-ext-src_php__swoole__private.h
+++ devel/pecl-swoole/files/patch-ext-src_php__swoole__private.h
@@ -1,7 +1,7 @@
---- ext-src/php_swoole.h.orig	2021-02-09 10:27:52 UTC
-+++ ext-src/php_swoole.h
-@@ -948,7 +948,7 @@ static sw_inline zend_bool sw_zend_is_callable_at_fram
- {
+--- ext-src/php_swoole_private.h.orig	2021-04-23 05:47:52 UTC
++++ ext-src/php_swoole_private.h
+@@ -894,7 +894,7 @@ static sw_inline zend_bool sw_zend_is_callable_at_fram
+                                                         char **error) {
      zend_string *name;
      zend_bool ret;
 -#if PHP_VERSION_ID < 80000
