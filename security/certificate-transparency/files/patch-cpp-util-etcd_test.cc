b661ebb122bf752933671f07435806ee5787acae
Rene Ladan <rene@FreeBSD.org>
2021-04-24 17:12:52 +0200

    Remove expired ports:
    2021-04-22 net/samba411: Security Support ends on 03 Dec 2020
    2021-04-22 net-im/cordless: Unmaintained and dead upstream, uses the old Discord API which notably has got users banned
    2021-04-18 news/plor: listed as "Alpha-release" but last update in 2001; unmaintained
    2021-04-20 security/certificate-transparency: Broken for more than 6 months

--- security/certificate-transparency/files/patch-cpp-util-etcd_test.cc
+++
@@ -1,9 +0,0 @@
---- cpp/util/etcd_test.cc.orig	2016-10-14 17:11:57 UTC
-+++ cpp/util/etcd_test.cc
-@@ -1,3 +1,6 @@
-+// Without this, build will fail due to clash between onigposix.h
-+// and regex.h; note it's only compiled with "make check".
-+#define GTEST_HAS_POSIX_RE 0
- #include <gmock/gmock.h>
- #include <gtest/gtest.h>
- #include <list>
