8df29ed6c0d4357eac11eb0ace0cb479bf44b117
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-04-26 04:16:18 +0800

    www/py-httpx: Fix build with py-httpcore 0.13 update
    - Bump PORTREVISION for package change

--- www/py-httpx/files/patch-setup.py
+++ www/py-httpx/files/patch-setup.py
@@ -1,7 +1,11 @@
---- setup.py.orig	2020-10-08 12:15:02 UTC
+--- setup.py.orig	2021-03-15 09:19:02 UTC
 +++ setup.py
-@@ -61,7 +61,7 @@ setup(
-         "httpcore==0.12.*",
+@@ -59,10 +59,10 @@ setup(
+         "certifi",
+         "sniffio",
+         "rfc3986[idna2008]>=1.3,<2",
+-        "httpcore>=0.12.1,<0.13",
++        "httpcore>=0.12.1,<0.14",
      ],
      extras_require={
 -        "http2": "h2==3.*",
