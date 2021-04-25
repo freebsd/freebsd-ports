361819c0b8aa82d2a3a5b7451333dd43093459af
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-04-26 04:16:26 +0800

    www/py-wagtail: Update to 2.11.7
    Changes:	https://github.com/wagtail/wagtail/releases
    		https://docs.wagtail.io/en/latest/releases/index.html

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,7 +1,7 @@
---- setup.py.orig	2020-11-02 15:07:59 UTC
+--- setup.py.orig	2021-04-19 09:14:58 UTC
 +++ setup.py
 @@ -26,7 +26,7 @@ install_requires = [
-     "django-treebeard>=4.2.0,<5.0",
+     "django-treebeard>=4.2.0,<5.0,!=4.5",
      "djangorestframework>=3.11.1,<4.0",
      "django-filter>=2.2,<3.0",
 -    "draftjs_exporter>=2.1.5,<3.0",
