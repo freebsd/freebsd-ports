44b57221ced803c65ed137d495fb787537915ee6
Alexey Dokuchaev <danfe@FreeBSD.org>
2021-04-26 08:48:12 +0000

    Update OpenBVE to version 1.8.0.2.
    Found out via:	repology.org

--- games/openbve/files/patch-source_OpenBVE_System_Program.cs
+++ games/openbve/files/patch-source_OpenBVE_System_Program.cs
@@ -1,11 +1,11 @@
---- source/OpenBVE/System/Program.cs.orig	2020-11-18 18:35:26 UTC
+--- source/OpenBVE/System/Program.cs.orig	2021-04-25 10:39:43 UTC
 +++ source/OpenBVE/System/Program.cs
-@@ -78,6 +78,8 @@ namespace OpenBve {
- 			Application.SetCompatibleTextRenderingDefault(false);
- 			CurrentHost = new Host();
- 			Joysticks = new JoystickManager();
+@@ -83,6 +83,8 @@ namespace OpenBve {
+ 			{
+ 				Joysticks = new JoystickManager64();	
+ 			}
 +			// XXX: crashes with System.PlatformNotSupportedException on the same calls below unless we call it here first
 +			Joysticks.RefreshJoysticks();
  			try {
- 				FileSystem = FileSystem.FromCommandLineArgs(args);
+ 				FileSystem = FileSystem.FromCommandLineArgs(args, CurrentHost);
  				FileSystem.CreateFileSystem();
