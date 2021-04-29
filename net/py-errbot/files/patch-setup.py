756466c9f47bfcd75ab204c5dbc0bcc463ace6b3
Neel Chauhan <nc@FreeBSD.org>
2021-04-29 11:52:42 -0700

    net/py-errbot: Update to 6.1.7
    Also, unlike the previous version, maintain compatability with
    py-markdown < 3.3.
    
    This is needed for the GTK+ 4 update.
    
    PR:		255278
    Approved by:	sunpoet (maintainer)

--- net/py-errbot/files/patch-setup.py
+++ net/py-errbot/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2020-10-10 16:25:15 UTC
+--- setup.py.orig	2020-12-19 05:26:11 UTC
 +++ setup.py
-@@ -36,7 +36,6 @@ deps = ['webtest',
-         'markdown<3.0',  # rendering stuff, 3.0+ deprecates 'safe()'
-         'ansi',
-         'Pygments>=2.0.2',
--        'pygments-markdown-lexer>=0.1.0.dev39',  # sytax coloring to debug md
-         'dulwich>=0.19.16',  # python implementation of git
-         'deepmerge>=0.1.0',
-         ]
+@@ -37,7 +37,6 @@ deps = [
+     'markdown>=3.3',
+     "ansi",
+     "Pygments>=2.0.2",
+-    "pygments-markdown-lexer>=0.1.0.dev39",  # sytax coloring to debug md
+     "dulwich>=0.19.16",  # python implementation of git
+     "deepmerge>=0.1.0",
+ ]
