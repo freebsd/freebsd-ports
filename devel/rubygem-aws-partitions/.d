05419abaa6b9b2b3c12dfde3a610f2517173bfc2
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-04-29 22:19:25 +0800

    devel/rubygem-aws-partitions: Update to 1.446.0
    Changes:	https://github.com/aws/aws-sdk-ruby/blob/master/gems/aws-partitions/CHANGELOG.md

---
+++ devel/rubygem-aws-partitions/.d
@@ -0,0 +1,3 @@
+TIMESTAMP = 1619458608
+SHA256 (rubygem/aws-partitions-1.446.0.gem) = 3ebf35a077b5062e602f329a036365a27a32bee67c9679f36075bb9309c9ecd8
+SIZE (rubygem/aws-partitions-1.446.0.gem) = 34304
