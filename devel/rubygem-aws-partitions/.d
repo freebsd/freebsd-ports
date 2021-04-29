74509bea0ca3f7ed4b33c15d13ddfd95f6632637
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-04-29 22:53:00 +0800

    devel/rubygem-aws-partitions: Remove accidentally-added files

--- devel/rubygem-aws-partitions/.d
+++
@@ -1,3 +0,0 @@
-TIMESTAMP = 1619458608
-SHA256 (rubygem/aws-partitions-1.446.0.gem) = 3ebf35a077b5062e602f329a036365a27a32bee67c9679f36075bb9309c9ecd8
-SIZE (rubygem/aws-partitions-1.446.0.gem) = 34304
