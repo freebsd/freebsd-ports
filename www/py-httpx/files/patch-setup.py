99bad053582902c573692706965af0ab16fde38a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-05-03 03:46:08 +0800

    www/py-httpx: Update to 0.18.1
    Changes:	https://github.com/encode/httpx/releases

--- www/py-httpx/files/patch-setup.py
+++ www/py-httpx/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2021-04-27 14:22:26 UTC
+--- setup.py.orig	2021-04-29 12:57:41 UTC
 +++ setup.py
 @@ -63,7 +63,7 @@ setup(
          "async_generator; python_version < '3.7'"
@@ -6,6 +6,6 @@
      extras_require={
 -        "http2": "h2==3.*",
 +        "http2": "h2>=3,<5",
-         "brotli": "brotlipy==0.7.*",
+         "brotli": "brotlicffi==1.*",
      },
      classifiers=[
