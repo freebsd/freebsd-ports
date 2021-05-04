46ee17ded040af73e358ad2c15d84a39728c3b32
Neel Chauhan <nc@FreeBSD.org>
2021-05-04 09:27:53 -0700

    devel/py-prettyprinter: Fix pytest-runner dependency
    PR:		255555
    Reported by:	Patrice Clement <monsieurp@gentoo.org> (maintainer)

---
+++ devel/py-prettyprinter/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2019-06-22 07:04:23 UTC
++++ setup.py
+@@ -17,7 +17,7 @@ requirements = [
+ ]
+ 
+ setup_requirements = [
+-    'pytest-runner==3.0',
++    'pytest-runner',
+     # TODO(tommikaikkonen): put setup requirements (distutils extensions, etc.) here
+ ]
+ 
