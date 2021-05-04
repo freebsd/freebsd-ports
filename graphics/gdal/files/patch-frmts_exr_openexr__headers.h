66a03470a2924840d5ff13a874d93f95ed832d7b
John Hein <jcfyecrayz@liamekaens.com>
2021-05-05 05:40:05 +0800

    graphics/gdal: Fix build with OpenEXR/Imath 3.0 (should be part of 9ab14902a13463a884c7446d358364ed5ee64088)
    - Remove MAKE_JOBS_UNSAFE [1]
    
    PR:		255216
    Reported by:	O. Hartmann <ohartmann@walstatt.org>
    Tested by:	John Hein <jcfyecrayz@liamekaens.com> [1]

--- graphics/gdal/files/patch-frmts_exr_openexr__headers.h
+++ graphics/gdal/files/patch-frmts_exr_openexr__headers.h
@@ -1,6 +1,14 @@
---- frmts/exr/openexr_headers.h.orig	2020-12-29 12:18:28 UTC
+--- frmts/exr/openexr_headers.h.orig	2021-03-05 04:40:24 UTC
 +++ frmts/exr/openexr_headers.h
-@@ -42,6 +42,7 @@
+@@ -35,6 +35,7 @@
+ #pragma GCC system_header
+ #endif
+ 
++#include "ImathInt64.h"
+ #include "ImathMatrix.h"
+ #include "ImfChannelList.h"
+ #include "ImfFloatAttribute.h"
+@@ -42,6 +43,7 @@
  #include "ImfMatrixAttribute.h"
  #include "ImfMultiPartInputFile.h"
  #include "ImfMultiPartOutputFile.h"
