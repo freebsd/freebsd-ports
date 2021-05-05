4f84f5901de0e189e1bdc01f4be86bd022e40b96
Bernhard Froehlich <decke@FreeBSD.org>
2021-05-05 17:37:40 +0000

    multimedia/minisatip: Update to 1.0.6
    (cherry picked from commit 348acda125ee227962a6c71c4d69193ade1e59cd)

--- multimedia/minisatip/files/patch-src_socketworks.c
+++ multimedia/minisatip/files/patch-src_socketworks.c
@@ -1,21 +1,24 @@
---- src/socketworks.c.orig	2020-08-16 05:56:14 UTC
+--- src/socketworks.c.orig	2021-05-05 05:43:10 UTC
 +++ src/socketworks.c
-@@ -1023,7 +1023,7 @@ void set_sockets_rtime(int i, int r)
- 	if (ss)
- 		ss->rtime = r;
+@@ -968,7 +968,7 @@ void set_sockets_rtime(int i, int r) {
+     if (ss)
+         ss->rtime = r;
  }
 -#ifndef __APPLE__
 +#ifdef __linux__
  
- int get_mac_address(char *mac)
- {
-@@ -1658,7 +1658,9 @@ void set_socket_dscp(int id, int dscp, int prio)
- 	setsockopt(id, IPPROTO_IP, IP_TOS, &d, sizeof(d));
+ int get_mac_address(char *mac) {
+     struct ifreq ifr;
+@@ -1562,8 +1562,12 @@ void set_socket_dscp(int id, int dscp, int prio) {
+         LOG("%s: setsockopt IP_TOS failed", __FUNCTION__);
  
- 	d = prio;
+     d = prio;
 +#if defined(SO_PRIORITY)
- 	setsockopt(id, SOL_SOCKET, SO_PRIORITY, &d, sizeof(d));
+     if (setsockopt(id, SOL_SOCKET, SO_PRIORITY, &d, sizeof(d)))
+         LOG("%s: setsockopt SO_PRIORITY failed", __FUNCTION__);
++#else
++    LOG("%s: setsockopt SO_PRIORITY not implemented", __FUNCTION__);
 +#endif
  }
  
- void sockets_set_opaque(int id, void *opaque, void *opaque2, void *opaque3)
+ void sockets_set_opaque(int id, void *opaque, void *opaque2, void *opaque3) {
