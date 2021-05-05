d9cf7e20fe3dbf481ec207ef6809f2f2017a8281
Neel Chauhan <nc@FreeBSD.org>
2021-05-05 15:50:47 -0700

    textproc/py-markdown: Update to 3.3.4
    Approved by:	koobs (maintainer)
    MFH:		No (backward incompatible)

--- textproc/py-markdown/files/patch-setup.py
+++ textproc/py-markdown/files/patch-setup.py
@@ -1,9 +1,9 @@
---- setup.py.orig	2017-01-04 05:33:56 UTC
+--- setup.py.orig	2021-02-24 19:57:42 UTC
 +++ setup.py
-@@ -270,5 +270,6 @@ setup(
-         'Topic :: Software Development :: Libraries :: Python Modules',
+@@ -129,5 +129,6 @@ setup(
          'Topic :: Text Processing :: Filters',
-         'Topic :: Text Processing :: Markup :: HTML'
+         'Topic :: Text Processing :: Markup :: HTML',
+         'Topic :: Text Processing :: Markup :: Markdown'
 -    ]
 +    ],
 +    test_suite='nose.collector',
