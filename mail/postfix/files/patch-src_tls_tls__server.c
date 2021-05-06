917dd373939553b4304d35cff0c5d2dbc9c5786a
Olli Hauer <ohauer@FreeBSD.org>
2021-05-06 19:37:03 +0200

    mail/postfix: update to 3.6.0
    Postfix 3.6 requires minimum OpenSSL 1.1.1, so
    mark broken for FreeBSD 11 with 'OpenSSL base'

--- mail/postfix/files/patch-src_tls_tls__server.c
+++ mail/postfix/files/patch-src_tls_tls__server.c
@@ -1,6 +1,6 @@
---- src/tls/tls_server.c.orig	2019-02-18 23:03:54 UTC
+--- src/tls/tls_server.c.orig	2020-07-25 23:49:16 UTC
 +++ src/tls/tls_server.c
-@@ -518,7 +518,9 @@ TLS_APPL_STATE *tls_server_init(const TL
+@@ -502,7 +502,9 @@ TLS_APPL_STATE *tls_server_init(const TLS_SERVER_INIT_
  	 * ticket decryption callback already (since 2.11) asks OpenSSL to
  	 * avoid issuing new tickets when the presented ticket is re-usable.
  	 */
--- mail/postfix/files/patch-src_util_sys__defs.h
+++ mail/postfix/files/patch-src_util_sys__defs.h
@@ -1,10 +1,10 @@
---- src/util/sys_defs.h.orig	2019-10-13 15:32:18 UTC
+--- src/util/sys_defs.h.orig	2020-05-21 18:34:23 UTC
 +++ src/util/sys_defs.h
 @@ -30,7 +30,8 @@
  #if defined(FREEBSD2) || defined(FREEBSD3) || defined(FREEBSD4) \
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
      || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
--    || defined(FREEBSD11) \
+-    || defined(FREEBSD11) || defined(FREEBSD12) \
 +    || defined(FREEBSD11) || defined(FREEBSD12) || defined(FREEBSD13) \
 +    || defined(FREEBSD14) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
