76f2c0607d29e6aeaf7fb43dfb74846b50571b68
Rainer Hurling <rhurlin@FreeBSD.org>
2021-05-06 16:59:30 +0200

    games/eboard: Take maintainership and rework the port a bit
    - Add license
    - Remove nonfunctional joystick option
    - Comment out timeseal link; remove pkg-message for now
    - Remake all patchfiles
    - Change PORT_OPTIONS:MDOCS into post-install-DOCS-on target
    - pkg-descr: Use secured URL
    
    PR:		254278

--- games/eboard/files/patch-network.cc
+++ games/eboard/files/patch-network.cc
@@ -1,6 +1,15 @@
---- network.cc.orig	2008-02-22 16:51:22.000000000 +0100
-+++ network.cc	2008-12-29 11:24:10.000000000 +0100
-@@ -566,6 +566,7 @@
+--- network.cc.orig	2018-07-31 18:02:19 UTC
++++ network.cc
+@@ -501,7 +501,7 @@ int IncomingConnection::createSocket() {
+   sin.sin_addr.s_addr = INADDR_ANY;
+   sin.sin_port = htons(Port);
+     
+-  if (bind(netsocket,(struct sockaddr *) &sin,sizeof(sin))==-1) {
++  if (::bind(netsocket,(struct sockaddr *) &sin,sizeof(sin))==-1) {
+     snprintf(errorMessage,128,_("Unable to bind on port %d."),Port);
+     return(-1);
+   }
+@@ -604,6 +604,7 @@ PipeConnection::PipeConnection(int _pin,int _pout) {
    strcpy(HostName,"local pipe");
    snprintf(HostAddress,96,"pipe[%d,%d]",pin,pout);
    Quiet=0;
@@ -8,7 +17,7 @@
    MaxWaitTime = 60000.0; // 1 minute
  }
  
-@@ -577,6 +578,7 @@
+@@ -615,6 +616,7 @@ void PipeConnection::init() {
    strcpy(HostAddress,"unknown");
    memset(HelperBin,0,512);
    Quiet=0;
@@ -16,7 +25,7 @@
    handshake.erase();
    MaxWaitTime = 60000.0; // 1 minute
  }
-@@ -612,6 +614,25 @@
+@@ -652,6 +654,25 @@ PipeConnection::PipeConnection(const char *host,int po
    Port=port;
    g_strlcpy(HostName,host,128);
  
@@ -42,7 +51,7 @@
    // build helper path
    if (helpersuffix)
      snprintf(z,256,"%s.%s",helperbin,helpersuffix);
-@@ -728,7 +749,10 @@
+@@ -768,7 +789,10 @@ int PipeConnection::open() {
      dup2(1,2);
  
      setpgid(getpid(),0); // to broadcast SIGKILL later
