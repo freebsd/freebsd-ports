78e9016fd75efa29be61efbab34d724050b39530
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-05-07 17:23:41 +0800

    www/py-uvicorn: Fix build after de9f2bce3fb33121e676fae95aba97368beb8f51
    I've run the tests and the results are the same (3 failed, 212 passed) except
    "DeprecationWarning: websockets.handshake is deprecated".

--- www/py-uvicorn/files/patch-setup.py
+++ www/py-uvicorn/files/patch-setup.py
@@ -1,10 +1,12 @@
---- setup.py.orig	2021-02-20 16:49:00 UTC
+--- setup.py.orig	2021-02-20 16:48:34 UTC
 +++ setup.py
-@@ -52,7 +52,7 @@ minimal_requirements = [
+@@ -51,8 +51,8 @@ minimal_requirements = [
+ 
  
  extra_requirements = [
-     "websockets==8.*",
+-    "websockets==8.*",
 -    "httptools==0.1.* ;" + env_marker_cpython,
++    "websockets>=8.0",
 +    "httptools>=0.1.0 ;" + env_marker_cpython,
      "uvloop>=0.14.0,!=0.15.0,!=0.15.1; " + env_marker_cpython,
      "colorama>=0.4;" + env_marker_win,
