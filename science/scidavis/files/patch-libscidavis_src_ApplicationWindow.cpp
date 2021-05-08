ea54bf4ceaddba5b829938d619358a80eae4bef1
Max Brazhnikov <makc@FreeBSD.org>
2021-05-08 20:52:31 +0300

    science/scidavis: update to 2.4.0
    - Switch to cmake build system
    - Remove stale patches
    - Enable Python support by default

--- science/scidavis/files/patch-libscidavis_src_ApplicationWindow.cpp
+++ science/scidavis/files/patch-libscidavis_src_ApplicationWindow.cpp
@@ -1,31 +1,31 @@
---- libscidavis/src/ApplicationWindow.cpp.orig	2020-05-10 23:27:36 UTC
+--- libscidavis/src/ApplicationWindow.cpp.orig	2021-05-05 01:51:49 UTC
 +++ libscidavis/src/ApplicationWindow.cpp
-@@ -4253,7 +4253,7 @@ void ApplicationWindow::readSettings()
- 	settings.endGroup(); // Colors
+@@ -4200,7 +4200,7 @@ void ApplicationWindow::readSettings()
+     settings.endGroup(); // Colors
  
- 	settings.beginGroup("/Paths");
--	workingDir = settings.value("/WorkingDir", qApp->applicationDirPath()).toString();
-+	workingDir = settings.value("/WorkingDir", QDir::homePath()).toString();
+     settings.beginGroup("/Paths");
+-    workingDir = settings.value("/WorkingDir", qApp->applicationDirPath()).toString();
++    workingDir = settings.value("/WorkingDir", QDir::homePath()).toString();
      helpFilePath = settings.value("/HelpFile", "").toString();
  #ifdef PLUGIN_PATH
- 	QString defaultFitPluginsPath = PLUGIN_PATH;
-@@ -8211,6 +8211,10 @@ void ApplicationWindow::showWindowTitleBarMenu()
+     QString defaultFitPluginsPath = PLUGIN_PATH;
+@@ -8062,6 +8062,10 @@ void ApplicationWindow::showWindowTitleBarMenu()
  void ApplicationWindow::chooseHelpFolder()
  {
- // TODO: move all paths & location handling to anothor class  
+ // TODO: move all paths & location handling to anothor class
 +#ifdef DOC_PATH
-+	const QString locateDefaultHelp = DOC_PATH +
-+	    QDir::toNativeSeparators("/manual/index.html");
++    const QString locateDefaultHelp = DOC_PATH +
++        QDir::toNativeSeparators("/manual/index.html");
 +#else // defined DOC_PATH
  #if defined(Q_OS_WIN)
- 	const QString locateDefaultHelp = qApp->applicationDirPath() +
-         QDir::toNativeSeparators("/manual/index.html");
-@@ -8218,6 +8222,8 @@ void ApplicationWindow::chooseHelpFolder()
      const QString locateDefaultHelp =
-         QDir::toNativeSeparators("/usr/share/doc/scidavis/manual/index.html");
+             qApp->applicationDirPath() + QDir::toNativeSeparators("/manual/index.html");
+@@ -8069,6 +8073,8 @@ void ApplicationWindow::chooseHelpFolder()
+     const QString locateDefaultHelp =
+             QDir::toNativeSeparators("/usr/share/doc/scidavis/manual/index.html");
  #endif
 +#endif // defined DOC_PATH
 +
-     if (QFile(locateDefaultHelp).exists())
-     {
+     if (QFile(locateDefaultHelp).exists()) {
          helpFilePath = locateDefaultHelp;
+     } else {
