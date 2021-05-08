4c7a3f76ab2dd2b4e07c6765a3b441e0225e8c19
Tobias Kortkamp <tobik@FreeBSD.org>
2021-05-08 10:48:00 +0200

    games/veloren: Unbreak build with Rust 1.52.0 (D30099)
    error[E0635]: unknown feature `const_checked_int_methods`
      --> common/src/lib.rs:10:5
       |
    10 |     const_checked_int_methods,
       |     ^^^^^^^^^^^^^^^^^^^^^^^^^
    
    http://package18.nyi.freebsd.org/data/122amd64-default-foo/2021-05-07_12h05m44s/logs/errors/veloren-0.9.0_1.log
    
    PR:		255608

---
+++ games/veloren/files/patch-common_src_lib.rs
@@ -0,0 +1,10 @@
+--- common/src/lib.rs.orig	2021-05-08 07:26:33 UTC
++++ common/src/lib.rs
+@@ -7,7 +7,6 @@
+     arbitrary_enum_discriminant,
+     associated_type_defaults,
+     bool_to_option,
+-    const_checked_int_methods,
+     const_generics,
+     fundamental,
+     iter_map_while,
