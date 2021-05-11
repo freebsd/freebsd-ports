37bea0822892d0d7390729377c44a5fdee9b29d2
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-05-12 05:15:04 +0800

    security/keybase: Convert REINPLACE_CMD to patch file

---
+++ security/keybase/files/patch-vendor-github.com-keybase-go-ps-process_freebsd.go
@@ -0,0 +1,8 @@
+--- vendor/github.com/keybase/go-ps/process_freebsd.go.orig	2021-01-25 16:28:40 UTC
++++ vendor/github.com/keybase/go-ps/process_freebsd.go
+@@ -1,4 +1,4 @@
+-// +build freebsd,amd64
++// +build freebsd
+ 
+ package ps
+ 
