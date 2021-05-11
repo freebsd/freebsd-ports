44c14328c08e0ba92855367dba48ad9e06dc4b25
Greg V <greg@unrelenting.technology>
2021-05-11 19:49:32 +0300

    audio/alsa-lib, audio/alsa-plugins, audio/alsa-utils: Update to 1.2.2
    PR:		245321
    Reviewed by:	swills
    Tested by:	Oleh Hushchenkov <o.hushchenkov@gmail.com>

--- audio/alsa-lib/files/linux/types.h
+++ audio/alsa-lib/files/linux/types.h
@@ -10,6 +10,16 @@
 
 typedef pid_t __kernel_pid_t;
 typedef off_t __kernel_off_t;
+typedef long __kernel_long_t;
 
+typedef uint8_t __u8;
+typedef uint16_t __u16;
+typedef uint32_t __u32;
 typedef uint64_t __u64;
+
+typedef int8_t __s8;
+typedef int16_t __s16;
+typedef int32_t __s32;
+typedef int64_t __s64;
+
 #endif /* _LINUX_TYPES_H */
--- audio/alsa-utils/files/patch-aplay_aplay.c
+++ audio/alsa-utils/files/patch-aplay_aplay.c
@@ -1,23 +1,23 @@
---- aplay/aplay.c.orig	2016-03-31 14:37:02 UTC
+--- aplay/aplay.c.orig	2020-02-19 12:02:10 UTC
 +++ aplay/aplay.c
-@@ -48,7 +48,6 @@
+@@ -49,7 +49,6 @@
  #include <sys/time.h>
  #include <sys/stat.h>
  #include <sys/types.h>
 -#include <endian.h>
- #include "aconfig.h"
  #include "gettext.h"
  #include "formats.h"
+ #include "version.h"
 @@ -70,6 +69,12 @@
+ #define be16toh(x) __be16_to_cpu(x)
+ #define le32toh(x) __le32_to_cpu(x)
  #define be32toh(x) __be32_to_cpu(x)
- #endif
- 
++#endif
++
 +#ifndef _LARGEFILE64_SOURCE
 +/* assume modern system */
 +typedef off_t off64_t;
 +#define lseek64 lseek
-+#endif
-+
- #define DEFAULT_FORMAT		SND_PCM_FORMAT_U8
- #define DEFAULT_SPEED 		8000
+ #endif
  
+ #define DEFAULT_FORMAT		SND_PCM_FORMAT_U8
