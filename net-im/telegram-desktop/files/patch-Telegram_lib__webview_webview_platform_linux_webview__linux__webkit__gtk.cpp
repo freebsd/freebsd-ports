ec8a052e5d1c8fb8c9440a879de5735c1e899766
Charlie Li <vishwin@FreeBSD.org>
2021-05-14 14:47:28 -0400

    net-im/telegram-desktop: fix build with GTK option (again)
    Remove https://github.com/desktop-app/lib_webview/commit/48d70e5258754b2415b07c88f0376212596e09a7,
    as it depends on function changes from https://github.com/desktop-app/lib_base/commit/1b949f5ca250a47cd1261dd27fd83c174665f8fb
    
    Reported by:	madpilot
    Fixes:		80762637bb7d net-im/telegram-desktop: fix build with GTK option
    Pointy hat to:	vishwin
    Approved by:	Henry Hu <henry.hu.sh@gmail.com> (maintainer, implicit),
    		arrowd (mentor, implicit), fluffy (mentor, implicit)
    PR:		255719

--- net-im/telegram-desktop/files/patch-Telegram_lib__webview_webview_platform_linux_webview__linux__webkit__gtk.cpp
+++ net-im/telegram-desktop/files/patch-Telegram_lib__webview_webview_platform_linux_webview__linux__webkit__gtk.cpp
@@ -1,20 +1,11 @@
 --- Telegram/lib_webview/webview/platform/linux/webview_linux_webkit_gtk.cpp.orig	2021-04-14 05:42:51 UTC
 +++ Telegram/lib_webview/webview/platform/linux/webview_linux_webkit_gtk.cpp
-@@ -26,7 +26,7 @@ bool Resolve() {
- 	auto &gtk = BaseGtkIntegration::Instance()->library();
- 
- 	auto webkit2gtk = QLibrary();
--	const auto result =  LOAD_GTK_SYMBOL(gtk, gtk_widget_get_type)
-+	const auto result = LOAD_GTK_SYMBOL(gtk, gtk_widget_get_type)
- 		&& LOAD_GTK_SYMBOL(gtk, gtk_widget_grab_focus)
- 		&& LOAD_GTK_SYMBOL(gtk, gtk_container_get_type)
- 		&& LOAD_GTK_SYMBOL(gtk, gtk_container_add)
 @@ -38,7 +38,7 @@ bool Resolve() {
  		&& LOAD_GTK_SYMBOL(gtk, gtk_window_get_type)
  		&& LOAD_GTK_SYMBOL(gtk, gtk_window_set_decorated)
  		&& LOAD_GTK_SYMBOL(gtk, gdk_x11_window_get_xid)
 -		&& base::Platform::Gtk::LoadLibrary(webkit2gtk, "libwebkit2gtk-4.0.so.37", 0)
-+		&& base::Platform::Gtk::LoadGtkLibrary(webkit2gtk, "libwebkit2gtk-4.0.so.37")
++		&& base::Platform::Gtk::LoadLibrary(webkit2gtk, "libwebkit2gtk-4.0.so.37", 37)
  		&& LOAD_GTK_SYMBOL(webkit2gtk, webkit_web_view_new)
  		&& LOAD_GTK_SYMBOL(webkit2gtk, webkit_web_view_get_type)
  		&& LOAD_GTK_SYMBOL(webkit2gtk, webkit_web_view_get_user_content_manager)
