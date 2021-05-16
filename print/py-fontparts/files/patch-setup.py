d4ca6956380f540c2f19ad811a05c28d66b9632c
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-05-16 23:12:49 +0800

    print/py-fontparts: Add py-fontparts 0.9.10
    FontParts provides an API for interacting with the parts of fonts during the
    font development process. FontParts is the replacement for RoboFab.
    
    WWW: https://github.com/robotools/fontParts

---
+++ print/py-fontparts/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2021-03-09 14:24:32 UTC
++++ setup.py
+@@ -28,7 +28,7 @@ setup_params = dict(
+         "FontTools[ufo,lxml,unicode]>=3.32.0",
+         "fontMath>=0.4.8",
+         "defcon[pens]>=0.6.0",
+-        "booleanOperations>=0.9.0",
++        "booleanOperations>=0.8.2",
+     ],
+     classifiers=[
+         "Development Status :: 4 - Beta",
