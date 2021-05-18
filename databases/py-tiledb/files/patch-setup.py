296ef56a0b7afc0b5af1a7ea7ef1720da5e7eaad
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-05-19 03:28:11 +0800

    databases/py-tiledb: Update to 0.8.9
    Changes:	https://github.com/TileDB-Inc/TileDB-Py/releases

--- databases/py-tiledb/files/patch-setup.py
+++ databases/py-tiledb/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2021-04-30 20:41:45 UTC
+--- setup.py.orig	2021-05-13 21:29:51 UTC
 +++ setup.py
 @@ -487,11 +487,9 @@ def cmake_available():
  def setup_requires():
@@ -18,6 +18,6 @@
      install_requires=[
          "numpy>=1.16",
 -        "wheel>=0.30",
+         "contextvars ;python_version<'3.7'",
          "dataclasses ;python_version<'3.7'",
      ],
-     packages=find_packages(),
