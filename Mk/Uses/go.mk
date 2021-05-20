475a23ad78aa18be05071e841fc82894711c085c
Serhii (Sergey) Kozlov <skozlov@FreeBSD.org>
2021-05-20 10:59:27 -0500

    Mk/Uses/go.mk: Add -trimpath to GO_BUILDFLAGS
    Differential Revision:	https://reviews.freebsd.org/D30321

--- Mk/Uses/go.mk
+++ Mk/Uses/go.mk
@@ -82,7 +82,7 @@ GO_PKGNAME=	${PORTNAME}
 GO_TARGET?=	${GO_PKGNAME}
 GO_TESTTARGET?=	./...
 
-GO_BUILDFLAGS+=	-v -buildmode=exe
+GO_BUILDFLAGS+=	-v -buildmode=exe -trimpath
 .if !defined(WITH_DEBUG) && empty(GO_BUILDFLAGS:M-ldflags*)
 GO_BUILDFLAGS+=	-ldflags=-s
 .endif
