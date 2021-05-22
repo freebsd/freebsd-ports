37ecec030264ebe6625c14a8fa9edf0d7dfd937d
Hans Petter Selasky <hselasky@FreeBSD.org>
2021-05-22 14:57:35 +0200

    multimedia/libv4l: Update webcamd and Linux source tree dependencies.
    Approved by:	pi (implicit)

---
+++ multimedia/libv4l/files/patch-utils_common_fwht-ctrls.h
@@ -0,0 +1,24 @@
+--- utils/common/fwht-ctrls.h.orig	2021-05-22 12:52:28 UTC
++++ utils/common/fwht-ctrls.h
+@@ -11,21 +11,6 @@
+ #ifndef _FWHT_CTRLS_H_
+ #define _FWHT_CTRLS_H_
+ 
+-#define V4L2_CTRL_TYPE_FWHT_PARAMS 0x0105
+-
+ #define V4L2_CID_MPEG_VIDEO_FWHT_PARAMS	(V4L2_CID_MPEG_BASE + 292)
+-
+-struct v4l2_ctrl_fwht_params {
+-	__u64 backward_ref_ts;
+-	__u32 version;
+-	__u32 width;
+-	__u32 height;
+-	__u32 flags;
+-	__u32 colorspace;
+-	__u32 xfer_func;
+-	__u32 ycbcr_enc;
+-	__u32 quantization;
+-};
+-
+ 
+ #endif
