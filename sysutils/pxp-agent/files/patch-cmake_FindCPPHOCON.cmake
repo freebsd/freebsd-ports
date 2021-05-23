af41f55753385d93f35fbdf3ad7fecf23a9c8721
Romain Tartière <romain@FreeBSD.org>
2021-05-23 09:32:31 -1000

    sysutils/pxp-agent: New port
    This is the agent for the PCP Execution Protocol (PXP), based on the the Puppet
    Communications Protocol (PCP). It enables the execution of actions on remote
    nodes.
    
    WWW: https://github.com/puppetlabs/pxp-agent
    
    With hat:	puppet

---
+++ sysutils/pxp-agent/files/patch-cmake_FindCPPHOCON.cmake
@@ -0,0 +1,9 @@
+--- cmake/FindCPPHOCON.cmake.orig	2018-09-28 17:04:38 UTC
++++ cmake/FindCPPHOCON.cmake
+@@ -1,5 +1,5 @@
+ include(FindDependency)
+-find_dependency(CPPHOCON DISPLAY "cpp-hocon" HEADERS "hocon/config.hpp" LIBRARIES "libcpp-hocon.a")
++find_dependency(CPPHOCON DISPLAY "cpp-hocon" HEADERS "hocon/config.hpp" LIBRARIES "liblibcpp-hocon.so")
+ 
+ include(FeatureSummary)
+ set_package_properties(CPPHOCON PROPERTIES DESCRIPTION "A C++ parser for the HOCON configuration language" URL "https://github.com/puppetlabs/cpp-hocon")
