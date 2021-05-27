507359c509389602b4060b2c5e203c99911c3578
Neel Chauhan <nc@FreeBSD.org>
2021-05-26 19:41:16 -0700

    devel/glib20: Update to 2.68.2

--- devel/glib20/files/patch-glib_tests_gdatetime.c
+++ devel/glib20/files/patch-glib_tests_gdatetime.c
@@ -1,20 +1,20 @@
---- glib/tests/gdatetime.c.orig	2014-03-09 10:29:13.925231275 +0000
-+++ glib/tests/gdatetime.c	2014-03-09 10:30:18.235227107 +0000
-@@ -1322,7 +1322,7 @@
+--- glib/tests/gdatetime.c.orig	2021-05-26 19:32:37.257501000 -0700
++++ glib/tests/gdatetime.c	2021-05-26 19:32:53.078466000 -0700
+@@ -2377,7 +2377,7 @@
    gint i1, i2;
  
  #ifdef G_OS_UNIX
--  tz = g_time_zone_new ("America/Toronto");
-+  tz = g_time_zone_new ("America/Toronto Eastern");
+-  tz = g_time_zone_new_identifier ("America/Toronto");
++  tz = g_time_zone_new_identifier ("America/Toronto Eastern");
  #elif defined G_OS_WIN32
-   tz = g_time_zone_new ("Eastern Standard Time");
+   tz = g_time_zone_new_identifier ("Eastern Standard Time");
  #endif
-@@ -1355,7 +1355,7 @@
+@@ -2411,7 +2411,7 @@
    gint i1, i2;
  
  #ifdef G_OS_UNIX
--  tz = g_time_zone_new ("America/Toronto");
-+  tz = g_time_zone_new ("America/Toronto Eastern");
+-  tz = g_time_zone_new_identifier ("America/Toronto");
++  tz = g_time_zone_new_identifier ("America/Toronto Eastern");
  #elif defined G_OS_WIN32
-   tz = g_time_zone_new ("Eastern Standard Time");
+   tz = g_time_zone_new_identifier ("Eastern Standard Time");
  #endif
