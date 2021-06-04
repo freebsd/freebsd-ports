aa0c36b1aad1f978d2f7c2398bf9b546612bcdb5
Sascha Biberhofer <ports@skyforge.at>
2021-06-04 10:05:32 +0100

    net-im/py-matrix-synapse: Update to 1.35.1
    ChangeLog: https://github.com/matrix-org/synapse/releases/tag/v1.35.0
    
    PR:		256380
    Approved by:	dbaio, garga (mentors, implicit)

--- net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
+++ net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
@@ -1,11 +1,11 @@
---- synapse/python_dependencies.py.orig	2021-05-07 09:20:49 UTC
+--- synapse/python_dependencies.py.orig	2021-06-01 19:28:25 UTC
 +++ synapse/python_dependencies.py
 @@ -86,7 +86,7 @@ REQUIREMENTS = [
      "typing-extensions>=3.7.4",
      # We enforce that we have a `cryptography` version that bundles an `openssl`
      # with the latest security patches.
 -    "cryptography>=3.4.7",
-+    #"cryptography>=3.4.7",
++    "cryptography",
+     "ijson>=3.0",
  ]
  
- CONDITIONAL_REQUIREMENTS = {
