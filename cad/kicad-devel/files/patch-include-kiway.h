ed1548773960b674f97d72ce5066e896ebbdc1b7
Michael Reifenberger <mr@FreeBSD.org>
2021-06-10 14:50:29 +0200

    cad/kicad-devel: Update to latest commit

--- cad/kicad-devel/files/patch-include-kiway.h
+++ cad/kicad-devel/files/patch-include-kiway.h
@@ -1,9 +1,11 @@
---- include/kiway.h.orig	2015-01-10 18:11:39.000000000 +0000
-+++ include/kiway.h	2015-01-11 10:51:52.458174797 +0000
-@@ -115,5 +115,5 @@
+--- include/kiway.h.orig	2021-06-08 00:34:09 UTC
++++ include/kiway.h
+@@ -113,7 +113,7 @@
  #define KIFACE_INSTANCE_NAME_AND_VERSION   "KIFACE_1"
  
+ #ifndef SWIG
 -#if defined(__linux__) || defined(__FreeBSD__)
 +#if defined(__linux__) || defined(__FreeBSD__) || defined(__DragonFly__)
   #define LIB_ENV_VAR    wxT( "LD_LIBRARY_PATH" )
  #elif defined(__WXMAC__)
+  #define LIB_ENV_VAR    wxT( "DYLD_LIBRARY_PATH" )
