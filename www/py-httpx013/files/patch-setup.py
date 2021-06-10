355fc057d311fd9509653c34142aa26e5d00356e
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-06-10 22:30:21 +0800

    www/py-httpx013: Fix build with py-chardet 4.0.0
    - Bump PORTREVISION for package change

--- www/py-httpx013/files/patch-setup.py
+++ www/py-httpx013/files/patch-setup.py
@@ -1,7 +1,11 @@
 --- setup.py.orig	2020-05-29 10:29:22 UTC
 +++ setup.py
-@@ -61,7 +61,7 @@ setup(
-         "chardet==3.*",
+@@ -58,10 +58,10 @@ setup(
+         "certifi",
+         "hstspreload",
+         "sniffio",
+-        "chardet==3.*",
++        "chardet>=3.0",
          "idna==2.*",
          "rfc3986>=1.3,<2",
 -        "httpcore==0.9.*",
