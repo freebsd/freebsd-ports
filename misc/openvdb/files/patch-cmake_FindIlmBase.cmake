b5163fd6722acb0627f2e8c028ccd7e2c77a6b41
Yuri Victorovich <yuri@FreeBSD.org>
2021-06-12 20:46:23 -0700

    misc/openvdb: Update 8.0.1 -> 8.1.0
    Reported by:	portscout

--- misc/openvdb/files/patch-cmake_FindIlmBase.cmake
+++ misc/openvdb/files/patch-cmake_FindIlmBase.cmake
@@ -1,5 +1,14 @@
---- cmake/FindIlmBase.cmake.orig	2021-02-05 19:21:38 UTC
+--- cmake/FindIlmBase.cmake.orig	2021-06-11 21:38:41 UTC
 +++ cmake/FindIlmBase.cmake
+@@ -10,7 +10,7 @@ Find IlmBase include dirs and libraries
+ 
+ Use this module by invoking find_package with the form::
+ 
+-  find_package(IlmBase
++  find_package(Imath
+     [version] [EXACT]      # Minimum or EXACT version
+     [REQUIRED]             # Fail with error if IlmBase is not found
+     [COMPONENTS <libs>...] # IlmBase libraries by their canonical name
 @@ -36,11 +36,11 @@ Result Variables
  
  This will define the following variables:
@@ -13,9 +22,9 @@
 -``IlmBase_INCLUDE_DIRS``
 +``S``
    Include directories needed to use IlmBase.
- ``IlmBase_LIBRARIES``
-   Libraries needed to link to IlmBase.
-@@ -54,7 +54,7 @@ Cache Variables
+ ``IlmBase_RELEASE_LIBRARIES``
+   Libraries needed to link to the release version of IlmBase.
+@@ -65,7 +65,7 @@ Cache Variables
  
  The following cache variables may also be set:
  
@@ -23,8 +32,8 @@
 +````
    The directory containing ``IlmBase/config-auto.h``.
  ``IlmBase_{COMPONENT}_LIBRARY``
-   Individual component libraries for IlmBase
-@@ -85,7 +85,7 @@ include(GNUInstallDirs)
+   Individual component libraries for IlmBase. may include target_link_libraries() debug/optimized keywords.
+@@ -102,7 +102,7 @@ include(GNUInstallDirs)
  
  
  mark_as_advanced(
@@ -33,7 +42,7 @@
    IlmBase_LIBRARY
  )
  
-@@ -95,10 +95,6 @@ if(DISABLE_CMAKE_SEARCH_PATHS)
+@@ -112,10 +112,6 @@ if(DISABLE_CMAKE_SEARCH_PATHS)
  endif()
  
  set(_ILMBASE_COMPONENT_LIST
@@ -44,16 +53,7 @@
    Imath
  )
  
-@@ -142,7 +138,7 @@ if(USE_PKGCONFIG)
-   if(NOT DEFINED PKG_CONFIG_FOUND)
-     find_package(PkgConfig)
-   endif()
--  pkg_check_modules(PC_IlmBase QUIET IlmBase)
-+  pkg_check_modules(PC_IlmBase QUIET Imath)
- endif()
- 
- # ------------------------------------------------------------------------
-@@ -158,15 +154,15 @@ list(APPEND _ILMBASE_INCLUDE_SEARCH_DIRS
+@@ -177,15 +173,15 @@ list(APPEND _ILMBASE_INCLUDE_SEARCH_DIRS
  )
  
  # Look for a standard IlmBase header file.
@@ -72,7 +72,7 @@
      _ilmbase_version_major_string REGEX "#define ILMBASE_VERSION_MAJOR "
    )
    string(REGEX REPLACE "#define ILMBASE_VERSION_MAJOR" ""
-@@ -174,7 +170,7 @@ if(EXISTS "${IlmBase_INCLUDE_DIR}/IlmBaseConfig.h")
+@@ -193,7 +189,7 @@ if(EXISTS "${IlmBase_INCLUDE_DIR}/IlmBaseConfig.h")
    )
    string(STRIP "${_ilmbase_version_major_string}" IlmBase_VERSION_MAJOR)
  
@@ -81,7 +81,7 @@
       _ilmbase_version_minor_string REGEX "#define ILMBASE_VERSION_MINOR "
    )
    string(REGEX REPLACE "#define ILMBASE_VERSION_MINOR" ""
-@@ -255,16 +251,16 @@ unset(_ILMBASE_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES)
+@@ -324,16 +320,16 @@ endforeach()
  # ------------------------------------------------------------------------
  
  include(FindPackageHandleStandardArgs)
@@ -91,40 +91,53 @@
 +  FOUND_VAR Imath_FOUND
    REQUIRED_VARS
 -    IlmBase_INCLUDE_DIR
-+    
-     IlmBase_LIB_COMPONENTS
-   VERSION_VAR IlmBase_VERSION
+-    IlmBase_LIB_COMPONENTS
+-  VERSION_VAR IlmBase_VERSION
++#    IlmBase_INCLUDE_DIR
++    Imath_LIB_COMPONENTS
++  VERSION_VAR Imath_VERSION
    HANDLE_COMPONENTS
  )
  
--if(IlmBase_FOUND)
-+if(Imath_FOUND)
-   set(IlmBase_LIBRARIES ${IlmBase_LIB_COMPONENTS})
+-if(NOT IlmBase_FOUND)
++if(NOT Imath_FOUND)
+   if(IlmBase_FIND_REQUIRED)
+     message(FATAL_ERROR "Unable to find IlmBase")
+   endif()
+@@ -348,8 +344,8 @@ set(IlmBase_DEBUG_LIBRARIES "")
+ set(IlmBase_DEBUG_LIBRARY_DIRS "")
+ foreach(LIB ${IlmBase_LIB_COMPONENTS_RELEASE})
+   get_filename_component(_ILM_LIBDIR ${LIB} DIRECTORY)
+-  list(APPEND IlmBase_RELEASE_LIBRARIES ${LIB})
+-  list(APPEND IlmBase_RELEASE_LIBRARY_DIRS ${_ILM_LIBDIR})
++  list(APPEND Imath_RELEASE_LIBRARIES ${LIB})
++  list(APPEND Imath_RELEASE_LIBRARY_DIRS ${_ILM_LIBDIR})
+ endforeach()
  
-   # We have to add both include and include/OpenEXR to the include
-@@ -280,12 +276,12 @@ if(IlmBase_FOUND)
+ foreach(LIB ${IlmBase_LIB_COMPONENTS_DEBUG})
+@@ -377,12 +373,12 @@ set(IlmBase_LIBRARY_DIRS ${IlmBase_RELEASE_LIBRARY_DIR
  
-   set(_IlmBase_Parent_Dir "")
-   get_filename_component(_IlmBase_Parent_Dir
--    ${IlmBase_INCLUDE_DIR}/../ ABSOLUTE)
-+    ${}/../ ABSOLUTE)
+ set(_IlmBase_Parent_Dir "")
+ get_filename_component(_IlmBase_Parent_Dir
+-  ${IlmBase_INCLUDE_DIR}/../ ABSOLUTE)
++  ${}/../ ABSOLUTE)
  
--  set(IlmBase_INCLUDE_DIRS)
--  list(APPEND IlmBase_INCLUDE_DIRS
-+  set(S)
-+  list(APPEND S
-     ${_IlmBase_Parent_Dir}
--    ${IlmBase_INCLUDE_DIR}
-+    ${}
-   )
-   unset(_IlmBase_Parent_Dir)
+-set(IlmBase_INCLUDE_DIRS)
+-list(APPEND IlmBase_INCLUDE_DIRS
++set(S)
++list(APPEND S
+   ${_IlmBase_Parent_Dir}
+-  ${IlmBase_INCLUDE_DIR}
++  ${}
+ )
+ unset(_IlmBase_Parent_Dir)
  
-@@ -330,7 +326,7 @@ if(IlmBase_FOUND)
-         IMPORTED_LOCATION "${IlmBase_${COMPONENT}_LIBRARY}"
-         INTERFACE_COMPILE_OPTIONS "${PC_IlmBase_CFLAGS_OTHER}"
-         INTERFACE_COMPILE_DEFINITIONS "${IlmBase_${COMPONENT}_DEFINITIONS}"
--        INTERFACE_INCLUDE_DIRECTORIES "${IlmBase_INCLUDE_DIRS}"
-+        INTERFACE_INCLUDE_DIRECTORIES "${S}"
-       )
-     endif()
-   endforeach()
+@@ -419,7 +415,7 @@ foreach(COMPONENT ${IlmBase_FIND_COMPONENTS})
+     set_target_properties(IlmBase::${COMPONENT} PROPERTIES
+       INTERFACE_COMPILE_OPTIONS "${PC_IlmBase_CFLAGS_OTHER}"
+       INTERFACE_COMPILE_DEFINITIONS "${IlmBase_${COMPONENT}_DEFINITIONS}"
+-      INTERFACE_INCLUDE_DIRECTORIES "${IlmBase_INCLUDE_DIRS}")
++      INTERFACE_INCLUDE_DIRECTORIES "${S}")
+ 
+     # Standard location
+     set_target_properties(IlmBase::${COMPONENT} PROPERTIES
