0c5491c62a091b3024af54e3db272898dfc0b1ed
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-06-16 23:45:47 +0000

    games/openarena: fix build on powerpc
    Similar fix to powerpc64*.

--- games/openarena/files/patch-code_qcommon_q__platform.h
+++ games/openarena/files/patch-code_qcommon_q__platform.h
@@ -1,11 +1,13 @@
 --- code/qcommon/q_platform.h.orig	2020-11-23 13:12:36 UTC
 +++ code/qcommon/q_platform.h
-@@ -235,6 +235,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+@@ -235,6 +235,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
  #define ARCH_STRING "amd64"
  #elif defined __axp__
  #define ARCH_STRING "alpha"
 +#elif defined __powerpc64__
 +#define ARCH_STRING "ppc64"
++#elif defined __powerpc__
++#define ARCH_STRING "ppc"
  #endif
  
  #if BYTE_ORDER == BIG_ENDIAN
