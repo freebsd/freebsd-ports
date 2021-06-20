8d5ae5fc00c1b94519780eda7f873fab021adaa1
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-06-20 19:37:24 +0800

    devel/py-prance: Update to 0.21.2
    Changes:	https://github.com/RonnyPfannschmidt/prance/blob/main/CHANGES.rst

--- devel/py-prance/files/patch-setup.py
+++
@@ -1,9 +0,0 @@
---- setup.py.orig	2020-12-08 10:10:33 UTC
-+++ setup.py
-@@ -98,6 +98,5 @@ if __name__ == '__main__':
-       },
-       zip_safe = True,
-       test_suite = 'tests',
--      setup_requires = ['pytest-runner'],
-       tests_require = dev_require,
-   )
