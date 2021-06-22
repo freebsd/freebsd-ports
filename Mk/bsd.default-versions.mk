0c2741ce9b9fa81fd21719794f9a31f82ddb57e1
Gerald Pfeifer <gerald@FreeBSD.org>
2021-06-22 13:25:10 +0000

    Mk/bsd.default-versions.mk: Add version 11 to GCC_DEFAULT
    Now that GCC 11.1 has been released and lang/gcc11 is in place,
    explicitly list version 11 as an option for GCC_DEFAULT.
    
    Technically USE_GCC=11 was already working, using lang/gcc11-devel,
    alas intentionally undocumented (and definitely unsupported).
    
    Add an explicit note that versions of GCC not listed here are not
    supposed to be used. [1]
    
    Suggested by:	mandree [1]

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -45,7 +45,8 @@ FIREBIRD_DEFAULT?=	2.5
 FORTRAN_DEFAULT?=	gfortran
 # Possible values: 3.2.0
 FPC_DEFAULT?=		3.2.0
-# Possible values: 8 (last to support powerpcspe), 9, 10
+# Possible values: 8 (last to support powerpcspe), 9, 10, 11
+# (Any other version is completely unsupported and not meant for general use.)
 .if ${ARCH} == "powerpcspe"
 GCC_DEFAULT?=		8
 .else
