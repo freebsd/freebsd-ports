b03649227fd333cad8521db8efae2d28b2acdb53
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2021-06-27 18:26:17 -0500

    Mk/Uses/firebird.mk: Prepare support for firebird 3.0 and 4.0
    PR:		256846
    Approved by:	rene

--- Mk/Uses/firebird.mk
+++ Mk/Uses/firebird.mk
@@ -8,15 +8,26 @@ _INCLUDE_USES_FIREBIRD_MK=	yes
 
 .if !empty(firebird_ARGS)
 FIREBIRD_VER=	${firebird_ARGS}
+.else
+FIREBIRD_VER=	${FIREBIRD_DEFAULT}
 .endif
-
-FIREBIRD_VER?=	${FIREBIRD_DEFAULT:S/.//}
 
 # When adding a version, please keep the comment in
 # Mk/bsd.default-versions.mk in sync.
-.if ${FIREBIRD_VER} == 25
-LIB_DEPENDS+=	libfbclient.so:databases/firebird25-client
-.else
-IGNORE=		cannot install: unknown Firebird version: ${FIREBIRD_VER}
-.endif
+VALID_FIREBIRD_VER=        2.5 3.0 4.0
+
+.for v in ${FIREBIRD_DEFAULT}
+.  if ! ${VALID_FIREBIRD_VER:M$v}
+IGNORE=		Invalid Firebird default version ${FIREBIRD_DEFAULT}; valid versions are ${VALID_FIREBIRD_VER}
+.  endif
+.endfor
+
+.for w in ${FIREBIRD_VER}
+.  if ! ${VALID_FIREBIRD_VER:M$w}
+IGNORE=		Invalid Firebird version ${FIREBIRD_VER}; valid versions are ${VALID_FIREBIRD_VER}
+.  endif
+.endfor
+
+LIB_DEPENDS+=	libfbclient.so:databases/firebird${FIREBIRD_VER:S/.//}-client
+
 .endif
