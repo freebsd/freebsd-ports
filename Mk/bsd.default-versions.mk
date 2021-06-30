be6fbf890b684ddd3d591e0a4c9dcbdffde4b43a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-07-01 04:40:51 +0800

    Mk/bsd.default-versions.mk: Cosmetic change

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -39,7 +39,7 @@ BDB_DEFAULT?=		5
 COROSYNC_DEFAULT?=	2
 # Possible_values: full canna nox devel_full devel_nox
 #EMACS_DEFAULT?=	let the flavor be the default if not explicitly set
-# Possible values: 2.5 3.0 4.0
+# Possible values: 2.5, 3.0, 4.0
 FIREBIRD_DEFAULT?=	2.5
 # Possible values: flang (experimental), gfortran
 FORTRAN_DEFAULT?=	gfortran
