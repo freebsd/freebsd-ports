c7bab0df6c8375d5f110ad467b07f3cb7667c327
Alex Dupre <ale@FreeBSD.org>
2021-06-30 09:29:07 +0200

    www/mitmproxy: update to 6.0.2 release and unbreak.
    PR:		250799
    Reported by:	p5B2E9A8F@t-online.de
    Patch by:	maintainer, lwhsu, ale

--- www/mitmproxy/files/patch-setup.py
+++ www/mitmproxy/files/patch-setup.py
@@ -1,34 +1,11 @@
---- setup.py.orig	2019-12-09 13:30:10 UTC
+--- setup.py.orig	2021-06-30 07:08:47 UTC
 +++ setup.py
-@@ -61,24 +61,24 @@ setup(
-     # It is not considered best practice to use install_requires to pin dependencies to specific versions.
-     install_requires=[
-         "blinker>=1.4",
--        "brotli>=0.7.0,<0.8",
-+        "brotli>=0.7.0",
-         "certifi>=2015.11.20.1",  # no semver here - this should always be on the last release!
-         "click>=6.2",
--        "cryptography>=2.1.4,<2.4",
-+        "cryptography>=2.1.4",
-         "h2>=3.0.1,<4",
-         "hyperframe>=5.1.0,<6",
-         "kaitaistruct>=0.7,<0.9",
--        "ldap3>=2.5,<2.6",
-+        "ldap3>=2.5",
-         "passlib>=1.6.5",
-         "pyasn1>=0.3.1,<0.5",
--        "pyOpenSSL>=17.5,<18.1",
-+        "pyOpenSSL>=17.5",
-         "pyparsing>=2.1.3",
-         "pyperclip>=1.6.0",
-         "ruamel.yaml>=0.13.2",
--        "sortedcontainers>=1.5.4,<2.1",
-+        "sortedcontainers>=1.5.4",
-         "tornado>=4.3,<5.2",
--        "urwid>=2.0.1,<2.1",
--        "wsproto>=0.11.0,<0.12.0",
-+        "urwid>=2.0.1",
-+        "wsproto>=0.13.0",
+@@ -91,7 +91,7 @@ setup(
+         "urwid>=2.1.1,<2.2",
+         "wsproto>=1.0,<1.1",
+         "publicsuffix2>=2.20190812,<3",
+-        "zstandard>=0.11,<0.15",
++        "zstandard>=0.11,<0.16",
      ],
      extras_require={
          ':sys_platform == "win32"': [
