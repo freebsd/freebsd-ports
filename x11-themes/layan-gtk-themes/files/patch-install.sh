6dd8d9eba1ab00ac65219fbc380080131fb57ece
Alexander Vereeken <Alexander88207@protonmail.com>
2021-07-03 23:51:08 +0100

    x11-themes/layan-gtk-themes: Update to 2021-06-30
     * pet portclippy
     * pet portfmt
    
    ChangeLog:	https://github.com/vinceliuice/Layan-gtk-theme/releases/tag/2021-06-30
    
    PR:		256943
    Approved by:	dbaio, garga (mentors, implicit)

--- x11-themes/layan-gtk-themes/files/patch-install.sh
+++ x11-themes/layan-gtk-themes/files/patch-install.sh
@@ -1,6 +1,6 @@
---- install.sh.orig	2021-02-13 19:28:54 UTC
+--- install.sh.orig	2021-07-02 19:37:34 UTC
 +++ install.sh
-@@ -42,8 +42,8 @@ install() {
+@@ -54,8 +54,8 @@ install() {
    echo "Installing '${THEME_DIR}'..."
  
    mkdir -p                                                                           ${THEME_DIR}
@@ -11,20 +11,26 @@
  
    echo "[Desktop Entry]" >> ${THEME_DIR}/index.theme
    echo "Type=X-GNOME-Metatheme" >> ${THEME_DIR}/index.theme
-@@ -59,30 +59,30 @@ install() {
+@@ -71,41 +71,41 @@ install() {
    echo "ButtonLayout=menu:minimize,maximize,close" >> ${THEME_DIR}/index.theme
  
    mkdir -p                                                                           ${THEME_DIR}/gnome-shell
--  cp -ur ${SRC_DIR}/src/gnome-shell/{*.svg,extensions,noise-texture.png,pad-osd.css} ${THEME_DIR}/gnome-shell
+-  cp -ur ${SRC_DIR}/src/gnome-shell/{*.svg,noise-texture.png,pad-osd.css}            ${THEME_DIR}/gnome-shell
 -  cp -ur ${SRC_DIR}/src/gnome-shell/gnome-shell-theme.gresource.xml                  ${THEME_DIR}/gnome-shell
 -  cp -ur ${SRC_DIR}/src/gnome-shell/assets${ELSE_DARK}                               ${THEME_DIR}/gnome-shell/assets
--  cp -ur ${SRC_DIR}/src/gnome-shell/common-assets/{*.svg,dash}                       ${THEME_DIR}/gnome-shell/assets
--  cp -ur ${SRC_DIR}/src/gnome-shell/gnome-shell${ELSE_DARK}.css                      ${THEME_DIR}/gnome-shell/gnome-shell.css
-+  cp -pr ${SRC_DIR}/src/gnome-shell/{*.svg,extensions,noise-texture.png,pad-osd.css} ${THEME_DIR}/gnome-shell
+-  cp -ur ${SRC_DIR}/src/gnome-shell/common-assets/*.svg                              ${THEME_DIR}/gnome-shell/assets
++  cp -pr ${SRC_DIR}/src/gnome-shell/{*.svg,noise-texture.png,pad-osd.css}            ${THEME_DIR}/gnome-shell
 +  cp -pr ${SRC_DIR}/src/gnome-shell/gnome-shell-theme.gresource.xml                  ${THEME_DIR}/gnome-shell
 +  cp -pr ${SRC_DIR}/src/gnome-shell/assets${ELSE_DARK}                               ${THEME_DIR}/gnome-shell/assets
-+  cp -pr ${SRC_DIR}/src/gnome-shell/common-assets/{*.svg,dash}                       ${THEME_DIR}/gnome-shell/assets
-+  cp -pr ${SRC_DIR}/src/gnome-shell/gnome-shell${ELSE_DARK}.css                      ${THEME_DIR}/gnome-shell/gnome-shell.css
++  cp -pr ${SRC_DIR}/src/gnome-shell/common-assets/*.svg                              ${THEME_DIR}/gnome-shell/assets
+ 
+   if [[ "${GS_VERSION:-}" == 'new' ]]; then
+-    cp -ur ${SRC_DIR}/src/gnome-shell/shell-40-0/gnome-shell${ELSE_DARK}.css         ${THEME_DIR}/gnome-shell/gnome-shell.css
++    cp -pr ${SRC_DIR}/src/gnome-shell/shell-40-0/gnome-shell${ELSE_DARK}.css         ${THEME_DIR}/gnome-shell/gnome-shell.css
+   else
+-    cp -ur ${SRC_DIR}/src/gnome-shell/shell-3-36/gnome-shell${ELSE_DARK}.css         ${THEME_DIR}/gnome-shell/gnome-shell.css
++    cp -pr ${SRC_DIR}/src/gnome-shell/shell-3-36/gnome-shell${ELSE_DARK}.css         ${THEME_DIR}/gnome-shell/gnome-shell.css
+   fi
  
    mkdir -p                                                                           ${THEME_DIR}/gtk-2.0
 -  cp -ur ${SRC_DIR}/src/gtk-2.0/{apps.rc,hacks.rc,main.rc,panel.rc}                  ${THEME_DIR}/gtk-2.0
@@ -39,11 +45,19 @@
  
    mkdir -p                                                                           ${THEME_DIR}/gtk-3.0
    ln -sf ../gtk-assets                                                               ${THEME_DIR}/gtk-3.0/assets
--  cp -ur ${SRC_DIR}/src/gtk/gtk${color}${solid}.css                                  ${THEME_DIR}/gtk-3.0/gtk.css
-+  cp -pr ${SRC_DIR}/src/gtk/gtk${color}${solid}.css                                  ${THEME_DIR}/gtk-3.0/gtk.css
+-  cp -ur ${SRC_DIR}/src/gtk/3.0/gtk${color}${solid}.css                              ${THEME_DIR}/gtk-3.0/gtk.css
++  cp -pr ${SRC_DIR}/src/gtk/3.0/gtk${color}${solid}.css                              ${THEME_DIR}/gtk-3.0/gtk.css
    [[ ${color} != '-dark' ]] && \
--  cp -ur ${SRC_DIR}/src/gtk/gtk-dark${solid}.css                                     ${THEME_DIR}/gtk-3.0/gtk-dark.css
-+  cp -pr ${SRC_DIR}/src/gtk/gtk-dark${solid}.css                                     ${THEME_DIR}/gtk-3.0/gtk-dark.css
+-  cp -ur ${SRC_DIR}/src/gtk/3.0/gtk-dark${solid}.css                                 ${THEME_DIR}/gtk-3.0/gtk-dark.css
++  cp -pr ${SRC_DIR}/src/gtk/3.0/gtk-dark${solid}.css                                 ${THEME_DIR}/gtk-3.0/gtk-dark.css
+ 
+   mkdir -p                                                                           ${THEME_DIR}/gtk-4.0
+   ln -sf ../gtk-assets                                                               ${THEME_DIR}/gtk-4.0/assets
+-  cp -ur ${SRC_DIR}/src/gtk/4.0/gtk${color}${solid}.css                              ${THEME_DIR}/gtk-4.0/gtk.css
++  cp -pr ${SRC_DIR}/src/gtk/4.0/gtk${color}${solid}.css                              ${THEME_DIR}/gtk-4.0/gtk.css
+   [[ ${color} != '-dark' ]] && \
+-  cp -ur ${SRC_DIR}/src/gtk/4.0/gtk-dark${solid}.css                                 ${THEME_DIR}/gtk-4.0/gtk-dark.css
++  cp -pr ${SRC_DIR}/src/gtk/4.0/gtk-dark${solid}.css                                 ${THEME_DIR}/gtk-4.0/gtk-dark.css
  
    mkdir -p                                                                           ${THEME_DIR}/xfwm4
 -  cp -ur ${SRC_DIR}/src/xfwm4/assets${ELSE_LIGHT}/*.png                              ${THEME_DIR}/xfwm4
