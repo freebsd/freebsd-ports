562f7a0402ea5aa193f618ed6da3775a631f249a
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2021-07-04 18:08:38 +0200

    databases/grass7: unbreak after gdal 3.3 update
    GDAL 3.3.0 introduces the use of standard C bool type.
    Redefining bool locally, as in vector/v.hull/chull.c causes failure.
    
    Obtained from:	https://github.com/OSGeo/grass/pull/1567

---
+++ databases/grass7/files/patch-lib_lidar_lidar.h
@@ -0,0 +1,12 @@
+--- lib/lidar/lidar.h.orig	2021-07-04 15:30:31 UTC
++++ lib/lidar/lidar.h
+@@ -62,9 +62,6 @@
+     /* INTERPOLATOR */
+ #define P_BILINEAR 		1
+ #define P_BICUBIC 		0
+-    /* Boolean definitions */
+-#define TRUE 			1
+-#define FALSE 			0
+ 
+ /*----------------------------------------------------------------------------------------------------------*/
+     /*STRUCTS DECLARATION */
