93b1d6256e8a7dc49074970f68fb585749848ea1
Yuri Victorovich <yuri@FreeBSD.org>
2021-07-04 10:15:51 -0700

    net/clash: Update 1.6.0 -> 1.6.5
    Reported by:	portscout

--- net/clash/files/patch-listener_redir_tcp__freebsd.go
+++ net/clash/files/patch-listener_redir_tcp__freebsd.go
@@ -1,7 +1,7 @@
---- proxy/redir/tcp_freebsd.go.orig	2018-12-08 09:12:52 UTC
-+++ proxy/redir/tcp_freebsd.go
-@@ -38,7 +38,8 @@ func parserPacket(conn net.Conn) (socks.
- func getorigdst(fd uintptr) (socks.Addr, error) {
+--- listener/redir/tcp_freebsd.go.orig	2021-07-04 17:12:45 UTC
++++ listener/redir/tcp_freebsd.go
+@@ -38,7 +38,8 @@ func parserPacket(conn net.Conn) (socks5.Addr, error) 
+ func getorigdst(fd uintptr) (socks5.Addr, error) {
  	raw := syscall.RawSockaddrInet4{}
  	siz := unsafe.Sizeof(raw)
 -	if err := socketcall(GETSOCKOPT, fd, syscall.IPPROTO_IP, SO_ORIGINAL_DST, uintptr(unsafe.Pointer(&raw)), uintptr(unsafe.Pointer(&siz)), 0); err != nil {
