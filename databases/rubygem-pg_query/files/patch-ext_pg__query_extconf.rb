6bd317c1fceee41e30770e5e1dd1ba3ae65e8a89
VVD <vvd@unislabs.com>
2021-07-05 08:00:01 +0200

    databases/rubygem-pg_query: Fix unknown type name 'PgQueryProtobuf'
    Make sure to use internal pg_query lib.
    Patch is already included upstream in version 2.1.0:
    https://github.com/pganalyze/pg_query/pull/216/files
    
    This should fix the build outside of poudriere.
    
    PR:		256741
    Reported by:	VVD <vvd@unislabs.com>
    
    (cherry picked from commit 4bbd995b742469b4d3c5a37f761dfaa6c32f3d76)

--- databases/rubygem-pg_query/files/patch-ext_pg__query_extconf.rb
+++ databases/rubygem-pg_query/files/patch-ext_pg__query_extconf.rb
@@ -1,17 +1,18 @@
---- ext/pg_query/extconf.rb.orig	2021-05-19 21:51:31 UTC
+--- ext/pg_query/extconf.rb.orig	2021-07-05 05:53:16 UTC
 +++ ext/pg_query/extconf.rb
-@@ -7,13 +7,6 @@ require 'pathname'
+@@ -7,13 +7,8 @@ require 'pathname'
  
  $objs = Dir.glob(File.join(__dir__, '*.c')).map { |f| Pathname.new(f).sub_ext('.o').to_s }
  
 -$CFLAGS << " -I#{File.join(__dir__, 'include')} -O3 -Wall -fno-strict-aliasing -fwrapv -fstack-protector -Wno-unused-function -Wno-unused-variable -g"
--
++$CFLAGS << " -O3 -Wall -fno-strict-aliasing -fwrapv -fstack-protector -Wno-unused-function -Wno-unused-variable -DHAVE_STRCHRNUL -g"
+ 
 -SYMFILE = File.join(__dir__, 'pg_query_ruby.sym')
 -if RUBY_PLATFORM =~ /darwin/
 -  $DLDFLAGS << " -Wl,-exported_symbols_list #{SYMFILE}" unless defined?(::Rubinius)
 -else
 -  $DLDFLAGS << " -Wl,--retain-symbols-file=#{SYMFILE}"
 -end
-+$CFLAGS << " -I#{File.join(__dir__, 'include')} -O3 -Wall -fno-strict-aliasing -fwrapv -fstack-protector -Wno-unused-function -Wno-unused-variable -DHAVE_STRCHRNUL -g"
++$INCFLAGS = "-I#{File.join(__dir__, 'include')} " + $INCFLAGS
  
  create_makefile 'pg_query/pg_query'
