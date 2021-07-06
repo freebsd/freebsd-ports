024aa85be65d88f91d6952633ba0337ab1ddd4f2
Matthias Fechner <mfechner@FreeBSD.org>
2021-07-06 11:48:06 +0200

    databases/rubygem-pg_query: Update to 2.1.0
    This upgrade included patches upstream.
    Changelog:
    https://github.com/pganalyze/pg_query/blob/main/CHANGELOG.md
    
    (cherry picked from commit 943cc2e9f0474989ac172ceeef8994265e3dddb6)

--- databases/rubygem-pg_query/files/patch-ext_pg__query_extconf.rb
+++ databases/rubygem-pg_query/files/patch-ext_pg__query_extconf.rb
@@ -1,18 +1,19 @@
---- ext/pg_query/extconf.rb.orig	2021-07-05 05:53:16 UTC
+--- ext/pg_query/extconf.rb.orig	2021-07-06 09:35:46 UTC
 +++ ext/pg_query/extconf.rb
-@@ -7,13 +7,8 @@ require 'pathname'
+@@ -7,15 +7,8 @@ require 'pathname'
  
  $objs = Dir.glob(File.join(__dir__, '*.c')).map { |f| Pathname.new(f).sub_ext('.o').to_s }
  
--$CFLAGS << " -I#{File.join(__dir__, 'include')} -O3 -Wall -fno-strict-aliasing -fwrapv -fstack-protector -Wno-unused-function -Wno-unused-variable -g"
+-$CFLAGS << " -O3 -Wall -fno-strict-aliasing -fwrapv -fstack-protector -Wno-unused-function -Wno-unused-variable -g"
 +$CFLAGS << " -O3 -Wall -fno-strict-aliasing -fwrapv -fstack-protector -Wno-unused-function -Wno-unused-variable -DHAVE_STRCHRNUL -g"
  
+ $INCFLAGS = "-I#{File.join(__dir__, 'include')} " + $INCFLAGS
+-
 -SYMFILE = File.join(__dir__, 'pg_query_ruby.sym')
 -if RUBY_PLATFORM =~ /darwin/
 -  $DLDFLAGS << " -Wl,-exported_symbols_list #{SYMFILE}" unless defined?(::Rubinius)
 -else
 -  $DLDFLAGS << " -Wl,--retain-symbols-file=#{SYMFILE}"
 -end
-+$INCFLAGS = "-I#{File.join(__dir__, 'include')} " + $INCFLAGS
  
  create_makefile 'pg_query/pg_query'
