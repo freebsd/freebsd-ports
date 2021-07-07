f3c231d641aa6fa2a29dc5d7767eb22caf3c4b5b
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2021-07-07 09:54:41 +0200

    cad/opencascade: switch to vtk9
    - switches opencascade to vtk9 to enable upcoming import of
      cad/py-ocp
    - cad/freecad has to switch vtk8 -> vtk9, too
      - this requires upstream commit 0cfea3fee3e7848bbf043d2b1a19f6405d7ebe25
        "Make smesh compile with vtk9"
      - while touching this, fixes vtk module detection
      - clean up VTK_DIR usage: that variable does not exist in FreeCAD's
        build system anymore (for quite some time, actually)
    
    Obtained from:	opencascade upstream: Kirill Gavrilov
    Obtained from:	freecad upstream: committed by github/wwmayer
    Differential Revision:	D30934
    Reported by:	thierry@
    Submitted by:	thierry@

--- cad/opencascade/files/patch-src_Standard_Standard__CString.cxx
+++ cad/opencascade/files/patch-src_Standard_Standard__CString.cxx
@@ -1,6 +1,6 @@
---- src/Standard/Standard_CString.cxx.orig	2017-08-30 13:28:31 UTC
+--- src/Standard/Standard_CString.cxx.orig	2020-11-03 14:50:08 UTC
 +++ src/Standard/Standard_CString.cxx
-@@ -81,7 +81,7 @@ Standard_Integer HashCodes (const Standa
+@@ -83,7 +83,7 @@ Standard_Integer HashCodes (const Standard_CString the
      // strtod, strtol, strtoll functions. For other system with locale-depended
      // implementations problems may appear if "C" locale is not set explicitly.
      #if !defined(__ANDROID__) && !defined(__QNX__) && !defined(__MINGW32__)
