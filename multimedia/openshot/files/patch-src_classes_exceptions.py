8577317159a90cd5e6c5a4a549d90b9718e8b490
Fernando Apesteguía <fernape@FreeBSD.org>
2021-07-08 09:49:08 +0200

    multimedia/openshot: Update to 2.5.1.891
    ChangeLog:
    https://www.openshot.org/blog/2020/03/03/openshot-251-released-optimized-effects-improved-performance/
    
    PR:	256641
    Reported by:	tatsuki_makino@hotmail.com (maintainer)

--- multimedia/openshot/files/patch-src_classes_exceptions.py
+++ multimedia/openshot/files/patch-src_classes_exceptions.py
@@ -1,6 +1,6 @@
---- src/classes/exceptions.py.orig	2021-01-30 23:50:27 UTC
+--- src/classes/exceptions.py.orig	2021-06-09 04:36:57 UTC
 +++ src/classes/exceptions.py
-@@ -120,6 +120,8 @@ def libopenshot_crash_recovery():
+@@ -141,6 +141,8 @@ def libopenshot_crash_recovery():
          # Format last log line based on OS (since each OS can be formatted differently)
          if platform.system() == "Darwin":
              last_log_line = "mac-%s" % last_log_line[58:].strip()
