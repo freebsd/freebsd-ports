b275a10c2b0eb472235faf136a58f1a580578509
Alexey Dokuchaev <danfe@FreeBSD.org>
2021-07-13 10:19:12 +0000

    net/openmdns: fix daemon memory leaks in various expected paths.
    PR:	249296

--- net/openmdns/files/patch-mdnsd_mdns.c
+++ net/openmdns/files/patch-mdnsd_mdns.c
@@ -10,3 +10,33 @@
  /*
   * RR cache
   */
+@@ -155,8 +158,10 @@ cache_process(struct rr *rr)
+ 					/* TODO Cancel possible deletion */
+ 					log_warnx("cache_process: recover %s",
+ 					    rrs_str(&rr->rrs));
++					free(rr);
+ 					return (0);
+ 				}
++				free(rr);
+ 				return (0);
+ 			}
+ 			/*
+@@ -186,14 +191,17 @@ cache_process(struct rr *rr)
+ 				rr_aux->ttl = rr->ttl;
+ 				rr_aux->revision = 0;
+ 				cache_schedrev(rr_aux);
++				free(rr);
+ 
+ 				return (0);
+ 			}
+ 		}
+ 	}
+ 	/* Got a goodbye for a record we don't have */
+-	if (rr->ttl == 0)
++	if (rr->ttl == 0) {
++		free(rr);
+ 		return (0);
++	}
+ 
+ 	return (cache_insert(rr));
+ }
