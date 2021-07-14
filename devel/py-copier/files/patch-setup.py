2928c0831f588ae829b7d97c20f7803d24f61f77
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-07-14 23:52:23 +0800

    devel/py-copier: Fix build with py-packaging 21.0
    - Bump PORTREVISION for package change

---
+++ devel/py-copier/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-08-17 07:46:54 UTC
++++ setup.py
+@@ -10,7 +10,7 @@ package_data = \
+ install_requires = \
+ ['colorama>=0.4.3,<0.5.0',
+  'jinja2>=2.11.2,<3.0.0',
+- 'packaging>=20.4,<21.0',
++ 'packaging>=20.4',
+  'pathspec>=0.8.0,<0.9.0',
+  'plumbum>=1.6.9,<2.0.0',
+  'pydantic>=1.5.1,<2.0.0',
