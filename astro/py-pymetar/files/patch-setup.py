458e93a3203559fafb1f93bc5b2b5bf3cfda9378
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-07-14 23:52:21 +0800

    astro/py-pymetar: Update to 1.3
    Changes:	https://github.com/klausman/pymetar/commits/master

--- astro/py-pymetar/files/patch-setup.py
+++ astro/py-pymetar/files/patch-setup.py
@@ -1,12 +1,9 @@
---- setup.py.orig	2018-12-25 17:48:03 UTC
+--- setup.py.orig	2021-07-06 11:19:31 UTC
 +++ setup.py
-@@ -31,9 +31,7 @@ setuptools.setup(
+@@ -31,6 +31,7 @@ setuptools.setup(
      packages=setuptools.find_packages(),
      py_modules=["pymetar"],
      scripts=["bin/pymetar"],
--    data_files=[("share/doc/pymetar-%s" % pymetar.__version__,
--                 ['README.md', 'COPYING', 'THANKS']),
--                ("share/man/man1", ['pymetar.1'])],
 +    data_files=[("share/man/man1", ['pymetar.1'])],
      classifiers=(
      "Programming Language :: Python :: 3",
