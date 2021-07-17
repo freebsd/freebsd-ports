a8825ced6ef6b02befb50f332be4dd7c1b534c5a
Matthias Andree <mandree@FreeBSD.org>
2021-07-17 11:07:32 +0200

    graphics/*openimageio: chase Imath, update->2.2.16
    openimageio upstream versions received lots of build fixes for
    Imath compatibility, so update to 2.2.16 from 2.2.13.
    (oiio 2.2.13 does not build with Imath 3.1.0)
    
    The patch fiddling is due to Ptex-related changes in openimageio's cmake
    build system, https://github.com/OpenImageIO/oiio/pull/3001
    
    Changelogs (one document to read through):
    https://github.com/OpenImageIO/oiio/blob/v2.2.16.0/CHANGES.md#release-2216-1-jul-2021----compared-to-2215
    
    or with clickable links to issues/merges:
    https://github.com/OpenImageIO/oiio/releases/tag/v2.2.16.0
    https://github.com/OpenImageIO/oiio/releases/tag/v2.2.15.1
    https://github.com/OpenImageIO/oiio/releases/tag/v2.2.15.0
    https://github.com/OpenImageIO/oiio/releases/tag/v2.2.14.0

--- graphics/openimageio/files/patch-src_cmake_modules_FindPtex.cmake
+++ graphics/openimageio/files/patch-src_cmake_modules_FindPtex.cmake
@@ -1,5 +1,5 @@
---- src/cmake/modules/FindPTex.cmake.orig	2020-05-11 03:43:52 UTC
-+++ src/cmake/modules/FindPTex.cmake
+--- src/cmake/modules/FindPtex.cmake.orig	2021-07-01 18:54:28 UTC
++++ src/cmake/modules/FindPtex.cmake
 @@ -70,6 +70,7 @@ else ()
          PATHS
              /usr/include
--- graphics/openimageio/files/patch-src_libutil_ustring.cpp
+++ graphics/openimageio/files/patch-src_libutil_ustring.cpp
@@ -1,6 +1,6 @@
---- src/libutil/ustring.cpp.orig	2020-01-27 21:27:51 UTC
+--- src/libutil/ustring.cpp.orig	2021-07-01 18:54:28 UTC
 +++ src/libutil/ustring.cpp
-@@ -314,7 +314,7 @@ struct libcpp_string__long {
+@@ -308,7 +308,7 @@ struct libcpp_string__long {
      std::string::size_type __size_;
      std::string::size_type __cap_;
  };
@@ -9,7 +9,7 @@
  enum { libcpp_string__long_mask = 0x1ul };
  #    else   // _LIBCPP_BIG_ENDIAN
  enum { libcpp_string__long_mask = ~(std::string::size_type(~0) >> 1) };
-@@ -325,7 +325,7 @@ struct libcpp_string__long {
+@@ -319,7 +319,7 @@ struct libcpp_string__long {
      std::string::size_type __size_;
      std::string::pointer __data_;
  };
