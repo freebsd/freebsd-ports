ae1fa233bc568a939c326256ea4d533cf878a800
Tobias C. Berner <tcberner@FreeBSD.org>
2021-07-21 09:30:05 +0200

    graphics/gmic-qt: update to 2.9.8

--- graphics/gmic-qt/files/patch-gmic_src_gmic.cpp
+++ graphics/gmic-qt/files/patch-gmic_src_gmic.cpp
@@ -1,6 +1,6 @@
---- gmic/src/gmic.cpp.orig	2018-08-27 18:23:05 UTC
+--- gmic/src/gmic.cpp.orig	2021-07-16 09:15:32 UTC
 +++ gmic/src/gmic.cpp
-@@ -1993,7 +1993,7 @@ double gmic::mp_ext(char *const str, void *const p_lis
+@@ -2443,7 +2443,7 @@ double gmic::mp_store(const Ts *const ptr, const unsig
  // Manage correspondence between abort pointers and thread ids.
  CImgList<void*> gmic::list_p_is_abort = CImgList<void*>();
  bool *gmic::abort_ptr(bool *const p_is_abort) {
@@ -9,9 +9,9 @@
    void* tid = (void*)(cimg_ulong)getpid();
  #elif cimg_OS==1
    void* tid = (void*)(cimg_ulong)syscall(SYS_gettid);
-@@ -2326,7 +2326,7 @@ gmic::~gmic() {
- #endif // #if cimg_display!=0
- 
+@@ -2781,7 +2781,7 @@ gmic::~gmic() {
+   cimg::exception_mode(cimg_exception_mode);
+   cimg_forX(display_windows,l) delete &display_window(l);
    cimg::mutex(21);
 -#if defined(__MACOSX__) || defined(__APPLE__)
 +#if defined(__MACOSX__) || defined(__APPLE__) || defined(__FreeBSD__)
