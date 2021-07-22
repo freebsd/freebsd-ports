a31c989be86c0896a98932337a3a4c1c57fa5080
Tobias C. Berner <tcberner@FreeBSD.org>
2021-07-22 15:33:25 +0200

    kde.mk: fix component for x11/plasma-wayland-protocols and make accessible

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -255,7 +255,8 @@ _USE_FRAMEWORKS_ALL=	ecm \
 			${_USE_FRAMEWORKS_TIER4} \
 			${_USE_FRAMEWORKS_PORTING} \
 			${_USE_FRAMEWORKS_EXTRA} \
-			kpublictransport kosm
+			kpublictransport kosm \
+			plasma-wayland-protocols
 
 # List of components of the KDE Plasma distribution.
 _USE_PLASMA_ALL=	activitymanagerd breeze breeze-gtk \
@@ -436,9 +437,6 @@ kde-kquickcharts_PATH=		${QT_QMLDIR}/org/kde/quickcharts/controls/libchartscontr
 kde-kross_PORT=			lang/kf5-kross
 kde-kross_LIB=			libKF5KrossCore.so
 
-kde-kwayland-protocols_PORT=	x11/plasma-wayland-protocols
-kde-kwayland-protocols_LIB=	${KDE_PREFIX}/lib/cmake/PlasmaWaylandProtocols/PlasmaWaylandProtocolsConfig.cmake
-
 kde-kwayland-server_PORT=	x11/plasma5-kwayland-server
 kde-kwayland-server_LIB=	libKWaylandServer.so
 
@@ -472,6 +470,9 @@ kde-people_LIB=			libKF5People.so
 
 kde-plasma-framework_PORT=	x11/kf5-plasma-framework
 kde-plasma-framework_LIB=	libKF5Plasma.so
+
+kde-plasma-wayland-protocols_PORT=	x11/plasma-wayland-protocols
+kde-plasma-wayland-protocols_PATH=	${KDE_PREFIX}/lib/cmake/PlasmaWaylandProtocols/PlasmaWaylandProtocolsConfig.cmake
 
 kde-plotting_PORT=		graphics/kf5-kplotting
 kde-plotting_LIB=		libKF5Plotting.so
