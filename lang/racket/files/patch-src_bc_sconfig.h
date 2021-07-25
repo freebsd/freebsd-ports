e772c7109606d234120ea01c556ec04758e3eda5
Jimmy Olgeni <olgeni@FreeBSD.org>
2021-07-25 11:48:14 +0200

    lang/racket: Update to version 8.2, with default ChezScheme support.
    PR:		257367
    Reported by:	Philipp Ost <bsd@philippost.de>

--- lang/racket/files/patch-src_bc_sconfig.h
+++ lang/racket/files/patch-src_bc_sconfig.h
@@ -1,9 +1,9 @@
---- src/bc/sconfig.h.orig	2020-10-13 22:27:26 UTC
+--- src/bc/sconfig.h.orig	2021-07-08 17:51:36 UTC
 +++ src/bc/sconfig.h
-@@ -357,7 +357,6 @@
- #  define SCHEME_PLATFORM_LIBRARY_SUBPATH "aarch64-freebsd"
+@@ -368,7 +368,6 @@
+ #  define SCHEME_ARCH "aarch64"
  # elif defined(__powerpc__)
- #  define SCHEME_PLATFORM_LIBRARY_SUBPATH "ppc-freebsd"
+ #  define SCHEME_ARCH "ppc"
 -#  define MZ_USE_JIT_PPC
  # else
  #  error Unported platform.
