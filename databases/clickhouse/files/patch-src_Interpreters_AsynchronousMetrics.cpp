f9f9e99ef15337fc2bb7ba46b54c5d432869b2ef
Kirill Ponomarev <krion@FreeBSD.org>
2021-07-28 16:00:34 +0200

    databases/clickhouse: update to 21.7.4.18
    PR:		257474
    Reported by:	maintainer
    
    (cherry picked from commit 509cf82860669420f1e77b339490f13021407281)

--- databases/clickhouse/files/patch-src_Interpreters_AsynchronousMetrics.cpp
+++ databases/clickhouse/files/patch-src_Interpreters_AsynchronousMetrics.cpp
@@ -1,7 +1,7 @@
---- src/Interpreters/AsynchronousMetrics.cpp.orig	2021-03-19 11:39:14 UTC
+--- src/Interpreters/AsynchronousMetrics.cpp.orig	2021-07-17 10:04:30 UTC
 +++ src/Interpreters/AsynchronousMetrics.cpp
-@@ -196,7 +196,7 @@ void AsynchronousMetrics::update()
-     new_values["Uptime"] = global_context.getUptimeSeconds();
+@@ -207,7 +207,7 @@ void AsynchronousMetrics::update()
+     new_values["Uptime"] = getContext()->getUptimeSeconds();
  
      /// Process memory usage according to OS
 -#if defined(OS_LINUX)
