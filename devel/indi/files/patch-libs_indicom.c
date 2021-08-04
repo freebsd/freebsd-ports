ef8f58197aaa874dc087fe0d9f7942a2f12922fd
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2021-08-03 19:42:54 -0500

    devel/indi: update to 1.9.1
    * Bump PORTREVISION of some ports depends of indi

--- devel/indi/files/patch-libs_indicom.c
+++ devel/indi/files/patch-libs_indicom.c
@@ -1,5 +1,5 @@
---- libs/indicom.c	2021-04-22 12:28:35.000000000 -0500
-+++ libs/indicom.c	2021-05-11 12:29:20.391397000 -0500
+--- libs/indicom.c.orig	2021-06-26 09:52:40 UTC
++++ libs/indicom.c
 @@ -32,6 +32,7 @@
  #include "base64.h"
  
@@ -18,12 +18,12 @@
  #include <sys/ioctl.h>
  #endif
  
-@@ -763,7 +766,7 @@
+@@ -763,7 +766,7 @@ int tty_connect(const char *device, int bit_rate, int 
      case 57600:  bps = B57600;  break;
      case 115200: bps = B115200; break;
      case 230400: bps = B230400; break;
 -#ifndef __APPLE__
-+#if !defined(__FreeBSD__)
++#if !defined __APPLE__ && !defined(__FreeBSD__)
      case 460800: bps = B460800; break;
      case 576000: bps = B576000; break;
      case 921600: bps = B921600; break;
