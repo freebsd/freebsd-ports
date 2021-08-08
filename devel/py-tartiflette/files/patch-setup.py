95275a526e33c105afe7813c7e7b75c1b33b93b5
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-08-09 05:36:35 +0800

    devel/py-tartiflette: Update to 1.4.0
    - Add LICENSE_FILE
    
    Changes:	https://github.com/tartiflette/tartiflette/releases

--- devel/py-tartiflette/files/patch-setup.py
+++ devel/py-tartiflette/files/patch-setup.py
@@ -1,25 +1,23 @@
---- setup.py.orig	2020-05-02 18:17:15 UTC
+--- setup.py.orig	2021-08-02 14:49:15 UTC
 +++ setup.py
-@@ -38,13 +38,11 @@ def _build_libgraphqlparser():
- 
- class BuildExtCmd(build_ext):
-     def run(self):
--        _build_libgraphqlparser()
-         build_ext.run(self)
- 
+@@ -42,7 +42,6 @@ class BuildExtCmd(build_ext):
  
  class BuildPyCmd(build_py):
      def run(self):
 -        _build_libgraphqlparser()
-         build_py.run(self)
+         super().run()
  
  
-@@ -88,7 +86,7 @@ setup(
+@@ -91,10 +90,9 @@ setup(
      ],
      keywords="api graphql protocol api rest relay tartiflette dailymotion",
      packages=_PACKAGES,
--    install_requires=["cffi>=1.0.0,<2.0.0", "lark-parser==0.11.2", "pytz"],
-+    install_requires=["cffi>=1.0.0,<2.0.0", "lark-parser>=0.11.2", "pytz"],
+-    install_requires=["cffi>=1.0.0,<2.0.0", "lark-parser==0.11.3", "pytz"],
++    install_requires=["cffi>=1.0.0,<2.0.0", "lark-parser>=0.11.3", "pytz"],
      tests_require=_TEST_REQUIRE,
      extras_require={"test": _TEST_REQUIRE, "benchmark": _BENCHMARK_REQUIRE},
-     cmdclass={"build_ext": BuildExtCmd, "build_py": BuildPyCmd},
+-    cmdclass={"build_ext": BuildExtCmd, "build_py": BuildPyCmd},
+-    ext_modules=[LibGraphQLParserExtension()],
++    cmdclass={"build_py": BuildPyCmd},
+     include_package_data=True,
+ )
