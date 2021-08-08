dd6cd778b430f47d984976590a4b4e6857f3ecc6
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-08-08 22:36:56 +0800

    science/py-pysal: Update to 2.4.0
    Changes:	https://github.com/pysal/pysal/releases

--- science/py-pysal/files/patch-setup.py
+++ science/py-pysal/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2020-02-01 23:18:07 UTC
+--- setup.py.orig	2021-01-31 21:39:53 UTC
 +++ setup.py
-@@ -54,7 +54,7 @@ def setup_package():
+@@ -65,7 +65,7 @@ def setup_package():
          download_url='https://pypi.python.org/pypi/pysal',
          license='BSD',
          packages=find_packages(),
