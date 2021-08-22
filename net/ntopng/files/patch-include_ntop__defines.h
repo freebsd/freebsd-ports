84c9db61e287493d33f24c5cb108908f56a801ca
Guido Falsi <madpilot@FreeBSD.org>
2021-08-22 19:24:19 +0200

    net/ntopng: Update to 5.0
    - Use ports provided lua library

--- net/ntopng/files/patch-include_ntop__defines.h
+++ net/ntopng/files/patch-include_ntop__defines.h
@@ -1,11 +1,11 @@
---- include/ntop_defines.h.orig	2018-12-21 18:58:17 UTC
+--- include/ntop_defines.h.orig	2021-08-20 09:38:15 UTC
 +++ include/ntop_defines.h
-@@ -416,7 +416,7 @@
- #define NTOPNG_NDPI_OS_PROTO_ID      (NDPI_LAST_IMPLEMENTED_PROTOCOL+NDPI_MAX_NUM_CUSTOM_PROTOCOLS-2)
- #define CONST_DEFAULT_HOME_NET       "192.168.1.0/24"
- #define CONST_OLD_DEFAULT_DATA_DIR   "/var/tmp/ntopng"
+@@ -523,7 +523,7 @@
+ #define CONST_SHARE_DIR              "/usr/share"
+ #define CONST_SHARE_DIR_2            "/usr/local/share"
+ #define CONST_ETC_DIR                "/etc"
 -#define CONST_DEFAULT_DATA_DIR       "/var/lib/ntopng"
 +#define CONST_DEFAULT_DATA_DIR       "/var/db/ntopng"
- #define CONST_DEFAULT_MAX_UI_STRLEN  24
- #define CONST_DEFAULT_IS_AUTOLOGOUT_ENABLED               1
- #define CONST_DEFAULT_IS_IDLE_LOCAL_HOSTS_CACHE_ENABLED   1
+ #endif
+ #define CONST_ALT_INSTALL_DIR        CONST_SHARE_DIR   "/ntopng"
+ #define CONST_ALT2_INSTALL_DIR       CONST_SHARE_DIR_2 "/usr/share/ntopng"
