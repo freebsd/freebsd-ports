8d78b2ea798690afa69d251bb26febd5213e4546
Cy Schubert <cy@FreeBSD.org>
2021-08-23 14:09:59 -0700

    security/libgcrypt: Update to 1.9.4

--- security/libgcrypt/files/patch-random_jitterentropy-base.c
+++ security/libgcrypt/files/patch-random_jitterentropy-base.c
@@ -1,10 +1,10 @@
---- random/jitterentropy-base.c.orig	2021-03-16 20:10:57.561791000 -0700
-+++ random/jitterentropy-base.c	2021-02-12 03:23:24.000000000 -0800
-@@ -51,6 +51,7 @@
- 
+--- random/jitterentropy-base.c.orig	2021-08-22 05:57:34.000000000 -0700
++++ random/jitterentropy-base.c	2021-08-23 14:05:29.630091000 -0700
+@@ -52,6 +52,7 @@
  #undef _FORTIFY_SOURCE
+ #ifdef __OPTIMIZE__
  #pragma GCC optimize ("O0")
 +#pragma clang optimize off
+ #endif
  
  #include "jitterentropy.h"
- 
