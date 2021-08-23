44f511b489902a937bff3d677f03c37d3628ea52
Hiroki Sato <hrs@FreeBSD.org>
2021-08-24 04:01:11 +0900

    mail/mu4e-maildirs: update to 0.8.20201028

---
+++ mail/mu4e-maildirs/files/patch-mu4e-maildirs-extension.el
@@ -0,0 +1,10 @@
+--- mu4e-maildirs-extension.el.orig	2020-10-28 09:21:07 UTC
++++ mu4e-maildirs-extension.el
+@@ -34,6 +34,7 @@
+ 
+ ;;; Code:
+ (require 'mu4e)
++(require 'mu4e-meta)
+ (require 'dash)
+ 
+ (defgroup mu4e-maildirs-extension nil
