2f757d6c4401f88ceef7ed05d9583de44159fb62
Matthias Fechner <mfechner@FreeBSD.org>
2021-08-31 11:53:21 +0200

    www/gitlab-ce: Update to 14.2.1
    Changelog:
    https://about.gitlab.com/releases/2021/08/22/gitlab-14-2-released/
    https://about.gitlab.com/releases/2021/08/23/gitlab-14-2-1-released/

--- www/gitlab-ce/files/patch-config_initializers_1__settings.rb
+++ www/gitlab-ce/files/patch-config_initializers_1__settings.rb
@@ -1,6 +1,6 @@
---- config/initializers/1_settings.rb.orig	2021-04-22 21:13:23 UTC
+--- config/initializers/1_settings.rb.orig	2021-08-23 16:27:44 UTC
 +++ config/initializers/1_settings.rb
-@@ -182,11 +182,7 @@ Settings.gitlab['url'] ||= Settings.__send__(:build_gi
+@@ -184,11 +184,7 @@ Settings.gitlab['url'] ||= Settings.__send__(:build_gi
  Settings.gitlab['user'] ||= 'git'
  # External configuration may cause the ssh user to differ from the GitLab user
  Settings.gitlab['ssh_user'] ||= Settings.gitlab.user
