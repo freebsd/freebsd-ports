f5fc27fb0878ad6c90a7f5e9936616b624e30783
Kai Knoblich <kai@FreeBSD.org>
2021-09-01 20:37:17 +0200

    games/pysolfc: Update to 2.12.0
    * Make the port less restrictive and thus distributable again by using
      a reduced collection of cardsets, which contains clearly defined
      licenses.  It's also bundled by upstream for the distributed binary
      packages of PySolFC.
    
    * Introduce PILLOW/SOUND options which enables sound and enhanced
      graphics support.
    
    Changelog since 2.6.2:
    
    https://github.com/shlomif/PySolFC/blob/pysolfc-2.12.0/NEWS.asciidoc

---
+++ games/pysolfc/files/patch-setup.py
@@ -0,0 +1,36 @@
+* Avoid staging issues by skipping directories that only contain
+  subdirectories.
+
+* Skip locale files if NLS is disabled (otherwise enable it via
+  post-patch target).
+
+* The pycotap package is only required for testing.
+
+--- setup.py.orig	2021-06-13 03:25:15 UTC
++++ setup.py
+@@ -14,6 +14,8 @@ def get_data_files(source, destination):
+     destination using the data_files keyword of setuptools.setup."""
+     for path, _, files in os.walk(source):
+         files = [os.path.join(path, f) for f in files]
++        if (len(files) == 0):
++            continue
+         path = path.replace(source, destination, 1)
+         yield (path, files)
+ 
+@@ -44,7 +46,7 @@ for d in ddirs:
+     data_files += get_data_files(os.path.join('data', d),
+                                  os.path.join(data_dir, d))
+ 
+-data_files += get_data_files('locale', locale_dir)
++#%%NLS%%data_files += get_data_files('locale', locale_dir)
+ 
+ if os.name == 'posix':
+     for size in os.listdir('data/images/icons'):
+@@ -74,7 +76,6 @@ kw = {
+     'install_requires': [
+         'attrs',
+         'configobj',
+-        'pycotap',
+         'pysol_cards',
+         'random2',
+         'six',
