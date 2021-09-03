602c26c14599438bee8f29d7bdaf0f445ff7eca8
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-09-03 13:19:12 +0000

    java/openjdk8: fix runtime issues on powerpc64*
    Running java or javac executables fails with:
    Error: dl failure on line 910
    Error: failed /usr/local/openjdk8/jre/lib/ppc64le/server/libjvm.so, because /usr/local/openjdk8/jre/lib/ppc64le/server/libjvm.so: Undefined symbol "_ZN5frameC1EPlPh"

--- java/openjdk8/files/patch-hotspot_src_os__cpu_bsd__ppc_vm_thread__bsd__ppc.cpp
+++ java/openjdk8/files/patch-hotspot_src_os__cpu_bsd__ppc_vm_thread__bsd__ppc.cpp
@@ -1,5 +1,14 @@
 --- hotspot/src/os_cpu/bsd_ppc/vm/thread_bsd_ppc.cpp.orig	2021-08-04 17:59:22 UTC
 +++ hotspot/src/os_cpu/bsd_ppc/vm/thread_bsd_ppc.cpp
+@@ -24,7 +24,7 @@
+  */
+ 
+ #include "precompiled.hpp"
+-#include "runtime/frame.hpp"
++#include "runtime/frame.inline.hpp"
+ #include "runtime/thread.hpp"
+ 
+ bool JavaThread::pd_get_top_frame_for_profiling(frame* fr_addr, void* ucontext, bool isInJava) {
 @@ -42,7 +42,7 @@ bool JavaThread::pd_get_top_frame_for_profiling(frame*
    // if we were running Java code when SIGPROF came in.
    if (isInJava) {
