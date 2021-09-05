1af1b4caa25a2d4dcee07939369e988c996bffe5
Jan Beich <jbeich@FreeBSD.org>
2021-09-05 18:44:57 +0000

    www/webkit2-gtk3: convert 3f23fec4c93a to upstream fix

--- www/webkit2-gtk3/files/patch-Source_WebCore_platform_graphics_x11_PlatformDisplayX11.cpp
+++ www/webkit2-gtk3/files/patch-Source_WebCore_platform_graphics_x11_PlatformDisplayX11.cpp
@@ -1,4 +1,5 @@
 Regressed by https://github.com/KhronosGroup/EGL-Registry/commit/64aa561f1971
+Part of https://github.com/WebKit/WebKit/commit/29421afdcd64
 
 Source/WebCore/platform/graphics/x11/PlatformDisplayX11.cpp:132:5: error: unknown type name 'XVisualInfo'
     XVisualInfo visualTemplate;
@@ -12,11 +13,11 @@ Source/WebCore/platform/graphics/x11/PlatformDisplayX11.cpp:136:57: error: use o
 
 --- Source/WebCore/platform/graphics/x11/PlatformDisplayX11.cpp.orig	2020-08-12 09:17:55 UTC
 +++ Source/WebCore/platform/graphics/x11/PlatformDisplayX11.cpp
-@@ -36,6 +36,7 @@
+@@ -32,6 +32,7 @@
+ #include <X11/Xlib.h>
+ #include <X11/extensions/Xcomposite.h>
+ #if PLATFORM(GTK)
++#include <X11/Xutil.h>
+ #include <X11/extensions/Xdamage.h>
  #endif
  
- #if USE(EGL)
-+#define USE_X11
- #include <EGL/egl.h>
- #include <EGL/eglext.h>
- #endif
