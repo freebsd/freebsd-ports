74c66d50fd5f3f12469881d4f9c5634f8722ccc5
Mikael Urankar <mikael@FreeBSD.org>
2021-09-06 15:14:54 +0200

    lang/rust: Fix for missing getauxval function on FreeBSD
    Use FreeBSD's elf_aux_info for detecting ARM HW features
    
    PR:		258198
    Tested by:	Bob Prohaska <fbsd@www.zefox.net>

---
+++ lang/rust/files/patch-src_llvm-project_compiler-rt_lib_builtins_cpu__model.c
@@ -0,0 +1,16 @@
+--- src/llvm-project/compiler-rt/lib/builtins/cpu_model.c.orig	2021-05-22 10:27:43 UTC
++++ src/llvm-project/compiler-rt/lib/builtins/cpu_model.c
+@@ -775,7 +775,12 @@ _Bool __aarch64_have_lse_atomics
+ #define HWCAP_ATOMICS (1 << 8)
+ #endif
+ static void CONSTRUCTOR_ATTRIBUTE init_have_lse_atomics(void) {
+-  unsigned long hwcap = getauxval(AT_HWCAP);
++  unsigned long hwcap = 0;
++#if defined(__linux__)
++  hwcap = getauxval(AT_HWCAP);
++#elif defined(__FreeBSD__)
++  hwcap = elf_aux_info(AT_HWCAP, &hwcap, sizeof(unsigned long));
++#endif
+   __aarch64_have_lse_atomics = (hwcap & HWCAP_ATOMICS) != 0;
+ }
+ #endif // defined(__has_include)
