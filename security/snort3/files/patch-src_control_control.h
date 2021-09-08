fb0f6cdfd018b7f9ca2fc2a10ddf6dc25b771077
Dan Langille <dvl@FreeBSD.org>
2021-09-08 20:30:46 +0000

    security/snort3: Update to 3.1.12.0
    Include a patch for a build issue on 12.2 and 11.4, sent upstream.
    
    re: https://github.com/snort3/snort3/issues/210
    
    Approved by:	maintainer (via private email)

---
+++ security/snort3/files/patch-src_control_control.h
@@ -0,0 +1,10 @@
+--- src/control/control.h.orig	2021-09-08 19:39:41 UTC
++++ src/control/control.h
+@@ -26,6 +26,7 @@
+ #include <cstdarg>
+ #include <queue>
+ #include <string>
++#include <ctime>
+ 
+ #include "main/snort_types.h"
+ 
