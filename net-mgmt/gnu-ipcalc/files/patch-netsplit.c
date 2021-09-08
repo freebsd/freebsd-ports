5b60fcdb18e3e5bb5ae813ee4e82dfb7de997df2
Emanuel Haupt <ehaupt@FreeBSD.org>
2021-09-08 23:28:02 +0200

    net-mgmt/gnu-ipcalc: Update to 1.0.1
    - Upstream has move to GitLab
    - Use meson instead of gmake
    - Optionally support GeoIP1 + GeoIP2 via net/libmaxminddb
    
    PR:		258363 (based on)
    Reported by:	freebsd@ohreally.nl

--- net-mgmt/gnu-ipcalc/files/patch-netsplit.c
+++ net-mgmt/gnu-ipcalc/files/patch-netsplit.c
@@ -1,4 +1,4 @@
---- netsplit.c.orig	2017-05-15 12:33:58 UTC
+--- netsplit.c.orig	2021-06-06 18:56:02 UTC
 +++ netsplit.c
 @@ -32,9 +32,19 @@
  #include <netinet/in.h>
@@ -6,8 +6,9 @@
  #include <stdint.h>
 +#include <sys/socket.h>
  
+ #include "ipv6.h"
  #include "ipcalc.h"
- 
++
 +#if defined(__FreeBSD__) || defined(__darwin__) || defined(__APPLE__)
 +#ifndef s6_addr16
 +#define s6_addr16 __u6_addr.__u6_addr16
@@ -16,7 +17,6 @@
 +#define s6_addr32 __u6_addr.__u6_addr32
 +#endif
 +#endif
-+
+ 
  static const char *numtoquad(uint32_t num)
  {
- 	static char quad[64];
