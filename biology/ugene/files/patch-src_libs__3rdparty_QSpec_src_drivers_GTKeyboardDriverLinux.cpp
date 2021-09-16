299862ff2afc17f5a415843ba9fc87ee6074fa87
Alexey Dokuchaev <danfe@FreeBSD.org>
2021-09-16 09:48:26 +0000

    biology/ugene: update Unipro UGENE to version 40.0.

--- biology/ugene/files/patch-src_libs__3rdparty_QSpec_src_drivers_GTKeyboardDriverLinux.cpp
+++ biology/ugene/files/patch-src_libs__3rdparty_QSpec_src_drivers_GTKeyboardDriverLinux.cpp
@@ -1,22 +1,21 @@
 Use the Linux driver for FreeBSD as well. Fixes crash on startup.
 
---- src/libs_3rdparty/QSpec/src/drivers/GTKeyboardDriverLinux.cpp.orig	2018-09-02 00:22:54 UTC
+--- src/libs_3rdparty/QSpec/src/drivers/GTKeyboardDriverLinux.cpp.orig	2021-09-08 14:18:53 UTC
 +++ src/libs_3rdparty/QSpec/src/drivers/GTKeyboardDriverLinux.cpp
-@@ -23,7 +23,7 @@
+@@ -21,14 +21,14 @@
  
  #include "GTKeyboardDriver.h"
  
 -#if defined __linux__
 +#if defined(__linux__) || defined(__FreeBSD__)
- #    define XK_LATIN1    // for latin symbol
- #    define XK_MISCELLANY    // for action keys
  #    include <X11/extensions/XTest.h>
-@@ -32,7 +32,7 @@
+ #    include <X11/keysym.h>
+ #endif
  
  namespace HI {
  
 -#if defined __linux__
 +#if defined(__linux__) || defined(__FreeBSD__)
  
- #    define GT_CLASS_NAME "GTKeyboardDriverLinux"
- 
+ bool GTKeyboardDriver::keyPress(char key, Qt::KeyboardModifiers modifiers) {
+     DRIVER_CHECK(key != 0, "key = 0");
