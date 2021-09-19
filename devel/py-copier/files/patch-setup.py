084d01a539a7ab2a73772163c3bb3bd9d9833d05
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-09-19 13:30:22 +0800

    devel/py-copier: Fix build after py-Jinja2 3.0.0 update (04ca9b0f4970d8d47accc9e31ef0c91b1a9d01f0)
    - Bump PORTREVISION for package change

--- devel/py-copier/files/patch-setup.py
+++ devel/py-copier/files/patch-setup.py
@@ -1,11 +1,13 @@
 --- setup.py.orig	2020-08-17 07:46:54 UTC
 +++ setup.py
-@@ -10,8 +10,8 @@ package_data = \
+@@ -9,9 +9,9 @@ package_data = \
+ 
  install_requires = \
  ['colorama>=0.4.3,<0.5.0',
-  'jinja2>=2.11.2,<3.0.0',
+- 'jinja2>=2.11.2,<3.0.0',
 - 'packaging>=20.4,<21.0',
 - 'pathspec>=0.8.0,<0.9.0',
++ 'jinja2>=2.11.2',
 + 'packaging>=20.4',
 + 'pathspec>=0.8.0',
   'plumbum>=1.6.9,<2.0.0',
