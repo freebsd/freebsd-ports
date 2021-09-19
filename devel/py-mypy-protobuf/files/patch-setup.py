d5a0e373a255d107765a8aa80f32f0749a8f1d76
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-09-19 13:30:26 +0800

    devel/py-mypy-protobuf: Update to 2.9
    Changes:	https://github.com/dropbox/mypy-protobuf/blob/master/CHANGELOG.md

--- devel/py-mypy-protobuf/files/patch-setup.py
+++
@@ -1,9 +0,0 @@
---- setup.py.orig	2021-07-15 00:12:15 UTC
-+++ setup.py
-@@ -34,6 +34,5 @@ setup(
-             "protoc-gen-mypy_grpc = mypy_protobuf.main:grpc",
-         ],
-     },
--    scripts=["mypy_protobuf/protoc_gen_mypy.bat"],
-     python_requires=">=3.6",
- )
