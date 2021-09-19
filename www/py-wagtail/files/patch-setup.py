edcf831573e60b027a1598c7275701fb08f75f57
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-09-19 13:30:44 +0800

    www/py-wagtail: Fix build after py-beautifulsoup 4.10.0 update (8407165700d4ec423f9a41549f2ebb4cc7b912a2)
    - Bump PORTREVISION for package change

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,11 +1,14 @@
---- setup.py.orig	2021-04-19 09:14:58 UTC
+--- setup.py.orig	2021-07-13 11:20:32 UTC
 +++ setup.py
-@@ -26,7 +26,7 @@ install_requires = [
+@@ -26,9 +26,9 @@ install_requires = [
      "django-treebeard>=4.2.0,<5.0,!=4.5",
      "djangorestframework>=3.11.1,<4.0",
      "django-filter>=2.2,<3.0",
 -    "draftjs_exporter>=2.1.5,<3.0",
 +    "draftjs_exporter>=2.1.5",
      "Pillow>=4.0.0,<9.0.0",
-     "beautifulsoup4>=4.8,<4.9",
+-    "beautifulsoup4>=4.8,<4.10",
++    "beautifulsoup4>=4.8,<4.11",
      "html5lib>=0.999,<2",
+     "Willow>=1.4,<1.5",
+     "requests>=2.11.1,<3.0",
