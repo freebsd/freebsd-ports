1abbc5aa310aa58e909f856829559cf9b8ad4a3a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-09-21 11:27:46 +0800

    textproc/py-Tempita: Fix build with py-setuptools 58.0.0+
    ===>  Configuring for py38-Tempita-0.5.2
    error in Tempita setup command: use_2to3 is invalid.
    *** Error code 1
    
    Stop.
    make: stopped in /usr/ports/textproc/py-Tempita
    
    Reference:	https://github.com/pypa/setuptools/blob/main/CHANGES.rst

---
+++ textproc/py-Tempita/files/patch-setup.py
@@ -0,0 +1,8 @@
+--- setup.py.orig	2013-12-17 04:07:27 UTC
++++ setup.py
+@@ -36,5 +36,4 @@ more to learn about it.
+       test_suite='nose.collector',
+       include_package_data=True,
+       zip_safe=True,
+-      use_2to3=True,
+       )
