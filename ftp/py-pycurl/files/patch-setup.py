48f80e888963f69018f38dff19c2ae59c97b49eb
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-09-22 06:13:37 +0800

    ftp/py-pycurl: Update to 7.44.1
    Changes:	https://github.com/pycurl/pycurl/blob/master/ChangeLog
    		http://pycurl.io/docs/latest/release-notes.html

--- ftp/py-pycurl/files/patch-setup.py
+++ ftp/py-pycurl/files/patch-setup.py
@@ -1,17 +1,9 @@
---- setup.py.orig	2020-09-02 05:16:18 UTC
+--- setup.py.orig	2021-08-15 19:30:48 UTC
 +++ setup.py
-@@ -923,7 +923,6 @@ in COPYING-LGPL_ and COPYING-MIT_ files in the source 
-     ],
-     packages=[PY_PACKAGE],
-     package_dir={ PY_PACKAGE: os.path.join('python', 'curl') },
--    python_requires='>=3.5',
- )
- 
- if sys.platform == "win32":
-@@ -980,7 +979,6 @@ if __name__ == "__main__":
-     elif len(sys.argv) > 1 and sys.argv[1] == 'docstrings-sources':
-         gen_docstrings_sources()
+@@ -990,7 +990,6 @@ if __name__ == "__main__":
      else:
+         convert_docstrings()
+ 
 -        setup_args['data_files'] = get_data_files()
          if 'PYCURL_RELEASE' in os.environ and os.environ['PYCURL_RELEASE'].lower() in ['1', 'yes', 'true']:
              split_extension_source = False
