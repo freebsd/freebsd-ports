c02ea4f1b269245cafbd5975b8b1a92b9f3a661e
Mikael Urankar <mikael@FreeBSD.org>
2021-09-22 19:18:42 +0200

    devel/py-maturin: Update to 0.11.3
    * Simplify installation of the "maturin" binary.
    
    Changelog since 0.8.3:
    
    https://github.com/PyO3/maturin/blob/v0.11.3/Changelog.md
    
    PR:		258390
    MFH:		No (requires Rust >= 1.55 which isn't present in 2021Q3)

--- devel/py-maturin/files/patch-setup.py
+++ devel/py-maturin/files/patch-setup.py
@@ -1,29 +1,39 @@
 Skip the "cargo" command as the "maturin" binary is pre-built via the Port
-itself.  Also change the destination directory to honor ${PREFIX}.
+itself.
 
---- setup.py.orig	2020-08-17 14:45:47 UTC
+--- setup.py.orig	2021-08-25 08:52:22 UTC
 +++ setup.py
-@@ -55,10 +55,10 @@ class PostInstallCommand(install):
-                     "cargo not found in PATH. Please install rust "
-                     "(https://www.rust-lang.org/tools/install) and try again"
+@@ -74,15 +74,15 @@ class PostInstallCommand(install):
+                     ["--no-default-features", "--features=upload,log,human-panic"]
                  )
--            subprocess.check_call(
--                ["cargo", "rustc", "--bin", "maturin", "--", "-C", "link-arg=-s"]
--            )
--            source = os.path.join(source_dir, "target", "debug", executable_name)
-+            source = os.path.join(source_dir, "target", os.getenv("CARGO_BUILD_TARGET", ""), "release", executable_name)
-+            #subprocess.check_call(
-+            #    ["cargo", "rustc", "--bin", "maturin", "--", "-C", "link-arg=-s"]
-+            #)
+ 
+-            try:
+-                metadata = json.loads(
+-                    subprocess.check_output(cargo_args).splitlines()[-2]
+-                )
+-            except subprocess.CalledProcessError as exc:
+-                raise RuntimeError("build maturin failed:\n" + exc.output.decode())
+-            print(metadata)
+-            assert metadata["target"]["name"] == "maturin"
+-            source = metadata["filenames"][0]
++            #try:
++            #    metadata = json.loads(
++            #        subprocess.check_output(cargo_args).splitlines()[-2]
++            #    )
++            #except subprocess.CalledProcessError as exc:
++            #    raise RuntimeError("build maturin failed:\n" + exc.output.decode())
++            #print(metadata)
++            #assert metadata["target"]["name"] == "maturin"
++            #source = metadata["filenames"][0]
+ 
          # run this after trying to build with cargo (as otherwise this leaves
          # venv in a bad state: https://github.com/benfred/py-spy/issues/69)
-         install.run(self)
-@@ -68,7 +68,7 @@ class PostInstallCommand(install):
-         self.copy_file(source, target)
+@@ -90,7 +90,7 @@ class PostInstallCommand(install):
+ 
+         target = os.path.join(self.install_scripts, executable_name)
+         os.makedirs(self.install_scripts, exist_ok=True)
+-        self.copy_file(source, target)
++        #self.copy_file(source, target)
          self.copy_tree(
              os.path.join(source_dir, "maturin"),
--            os.path.join(self.root or self.install_lib, "maturin"),
-+            os.path.join(self.install_lib, "maturin"),
-         )
- 
- 
+             os.path.join(self.install_lib, "maturin"),
