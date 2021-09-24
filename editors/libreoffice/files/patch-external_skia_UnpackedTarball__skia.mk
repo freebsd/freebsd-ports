51a8c14d5209368d6223649b7a3a919c9eccf8d4
Tobias C. Berner <tcberner@FreeBSD.org>
2021-09-24 18:54:57 +0200

    editors/libreoffice: fix build against freetype2-2.11
    - Build was broken by the update of freetype2 to 2.11. This was not
      caught by the exp-run as libreoffice was updated after the exp-run
      had been done, but prior to the commit.
    
    Obtained from:	https://github.com/archlinux/svntogit-packages/commit/1db0f16aa08d3d7c13ecca8fd1c2c20c4b255504
    PR:		251512
    Reported by:	jrm

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,11 @@
---- external/skia/UnpackedTarball_skia.mk.orig	2021-02-05 14:00:19 UTC
+--- external/skia/UnpackedTarball_skia.mk.orig	2021-09-08 17:53:20 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -39,6 +39,7 @@ skia_patches := \
+@@ -39,6 +39,8 @@ skia_patches := \
      fast-png-write.patch.1 \
      skia_sk_cpu_sse_level_0_by_default.patch.1 \
      fix-warnings.patch.1 \
 +    gfx-skia-skia-src-core-SkCpu.cpp.patch.0 \
++    skia-freetype2.11.patch \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
+ 
