85ba1d2e9531b9bb6236ac0611294c11047f4b71
Yuri Victorovich <yuri@FreeBSD.org>
2021-09-25 22:02:16 -0700

    www/yt-dlp: Update 2021.09.02 -> 2021.09.25
    The MUTAGEN option is removed because mutagen listed in yt-dlp requirements - reported by sunpoet@.

--- www/yt-dlp/files/patch-yt__dlp_options.py
+++ www/yt-dlp/files/patch-yt__dlp_options.py
@@ -1,6 +1,6 @@
---- yt_dlp/options.py.orig	2021-03-18 03:13:34 UTC
+--- yt_dlp/options.py.orig	2021-09-25 00:59:59 UTC
 +++ yt_dlp/options.py
-@@ -151,10 +151,6 @@ def parseOpts(overrideArguments=None):
+@@ -202,10 +202,6 @@ def parseOpts(overrideArguments=None):
          action='version',
          help='Print program version and exit')
      general.add_option(
@@ -8,6 +8,6 @@
 -        action='store_true', dest='update_self',
 -        help='Update this program to latest version. Make sure that you have sufficient permissions (run with sudo if needed)')
 -    general.add_option(
-         '-i', '--ignore-errors', '--no-abort-on-error',
-         action='store_true', dest='ignoreerrors', default=True,
-         help='Continue on download errors, for example to skip unavailable videos in a playlist (default) (Alias: --no-abort-on-error)')
+         '-i', '--ignore-errors',
+         action='store_true', dest='ignoreerrors',
+         help='Ignore download and postprocessing errors. The download will be considered successfull even if the postprocessing fails')
