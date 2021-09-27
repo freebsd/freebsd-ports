d08cccaa487fc24db7a691ef29ae48a01dbef2d2
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-09-28 03:44:07 +0800

    textproc/py-atoml: Add py-atoml 1.0.3
    ATOML is a 1.0.0rc1-compliant TOML library.
    
    It includes a parser that preserves all comments, indentations, whitespace and
    internal element ordering, and makes them accessible and editable via an
    intuitive API.
    
    You can also create new TOML documents from scratch using the provided helpers.
    
    WWW: https://github.com/frostming/atoml

---
+++ textproc/py-atoml/files/setup.py
@@ -0,0 +1,39 @@
+
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+import codecs
+
+with codecs.open('README.md', encoding="utf-8") as fp:
+    long_description = fp.read()
+
+setup_kwargs = {
+    'name': 'atoml',
+    'version': '1.0.3',
+    'description': 'Yet another style preserving TOML library',
+    'long_description': long_description,
+    'license': 'MIT',
+    'author': '',
+    'author_email': 'Frost Ming <mianghong@gmail.com>,Sébastien Eustace <sebastien@eustace.io>',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': '',
+    'packages': [
+        'atoml',
+    ],
+    'package_data': {'': ['*']},
+    'long_description_content_type': 'text/markdown',
+    'classifiers': [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    'python_requires': '>=3.6',
+
+}
+
+
+setup(**setup_kwargs)
