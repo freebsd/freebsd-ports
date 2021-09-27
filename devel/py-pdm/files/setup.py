ae7e9d3094f54e8cf55185312933bc69c2d0e7bf
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-09-28 03:44:00 +0800

    devel/py-pdm: Add py-pdm 1.8.5
    PDM is meant to be a next generation Python package management tool. It was
    originally built for personal use. If you feel you are going well with Pipenv or
    Poetry and don't want to introduce another package manager, just stick to it.
    But if you are missing something that is not present in those tools, you can
    probably find some goodness in pdm.
    
    WWW: https://github.com/pdm-project/pdm

---
+++ devel/py-pdm/files/setup.py
@@ -0,0 +1,80 @@
+
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+import codecs
+
+with codecs.open('README.md', encoding="utf-8") as fp:
+    long_description = fp.read()
+INSTALL_REQUIRE = [
+    'appdirs',
+    'atoml>=1.0.3',
+    'click>=7',
+    'importlib-metadata; python_version < "3.8"',
+    'pdm-pep517>=0.8.3,<0.9',
+    'pep517>=0.11.0',
+    'pip>=20.1',
+    'python-dotenv~=0.15',
+    'pythonfinder',
+    'resolvelib>=0.7.0,<0.8.0',
+    'shellingham<2.0.0,>=1.3.2',
+    'wheel<1.0.0,>=0.36.2',
+    'tomli>=1.1.0,<2.0.0',
+    'installer~=0.2.3',
+]
+ENTRY_POINTS = {
+    'console_scripts': [
+        'pdm = pdm.core:main',
+    ],
+}
+
+setup_kwargs = {
+    'name': 'pdm',
+    'version': '1.8.5',
+    'description': 'Python Development Master',
+    'long_description': long_description,
+    'license': 'MIT',
+    'author': '',
+    'author_email': 'frostming <mianghong@gmail.com>',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://pdm.fming.dev',
+    'packages': [
+        'pdm',
+        'pdm._vendor',
+        'pdm._vendor.colorama',
+        'pdm._vendor.halo',
+        'pdm._vendor.log_symbols',
+        'pdm._vendor.spinners',
+        'pdm.builders',
+        'pdm.cli',
+        'pdm.cli.commands',
+        'pdm.cli.completions',
+        'pdm.formats',
+        'pdm.installers',
+        'pdm.models',
+        'pdm.models.in_process',
+        'pdm.pep582',
+        'pdm.project',
+        'pdm.resolver',
+    ],
+    'package_data': {'': ['*']},
+    'long_description_content_type': 'text/markdown',
+    'keywords': ['packaging', 'dependency', 'workflow'],
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    'install_requires': INSTALL_REQUIRE,
+    'python_requires': '>=3.7',
+    'entry_points': ENTRY_POINTS,
+
+}
+
+
+setup(**setup_kwargs)
