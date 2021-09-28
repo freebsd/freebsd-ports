8a681529a547c681de95f51107e8b1cc599c79b4
Dag-Erling Smørgrav <des@FreeBSD.org>
2021-09-28 04:22:52 +0000

    mail/spampd: Upgrade to 2.61.

--- mail/spampd/files/patch-spampd.pl
+++ mail/spampd/files/patch-spampd.pl
@@ -1,10 +1,10 @@
---- spampd.pl.orig	2019-03-12 15:58:04 UTC
+--- spampd.pl.orig	2021-09-28 04:11:50 UTC
 +++ spampd.pl
-@@ -877,6 +877,7 @@ my $sa_options = {
-   'debug'                => $debug,
-   'local_tests_only'     => $sa_local_only,
-   'home_dir_for_helpers' => $sa_home_dir,
-+  'user_dir'             => (getpwnam($user))[7],
-   'userstate_dir'        => $sa_home_dir,
-   'username'             => $user
- };
+@@ -470,6 +470,7 @@ sub new {
+       userstate_dir        =>
+         '/var/spool/spamassassin/spampd',         # home directory for SA files and plugins (--homedir option)
+       home_dir_for_helpers => '',                 # this will be set to the same as userstate_dir once options are parsed
++      user_dir             => (getpwnam($user))[7],
+       username             => '',                 # this will be set to the same user as we're running as once options are parsed
+       userprefs_filename   => undef,              # add this config file for SA "user_prefs" settings (--saconfig option)
+       dont_copy_prefs      => 1,                  # tell SA not to copy user pref file into its working dir
