197b71f9917728c95f6fb30d5a40cbaff72321a1
Neel Chauhan <nc@FreeBSD.org>
2021-09-28 15:05:48 -0700

    www/epiphany: Update to 41.0

--- www/epiphany/files/patch-lib_ephy-sync-utils.c
+++ www/epiphany/files/patch-lib_ephy-sync-utils.c
@@ -1,9 +1,9 @@
---- lib/ephy-sync-utils.c.orig	2020-09-11 23:16:40 UTC
+--- lib/ephy-sync-utils.c.orig	2021-09-28 21:57:11 UTC
 +++ lib/ephy-sync-utils.c
 @@ -30,9 +30,9 @@
- #include <libsoup/soup.h>
  #include <stdio.h>
  #include <string.h>
+ #include <webkit2/webkit2.h>
 -#if defined(__linux__)
 +#if defined(__linux__) || defined(__FreeBSD__)
  #include <sys/random.h>
