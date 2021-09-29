670c3c8cd9bde6a4ebc7d79a0087e98f0e3446b9
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-09-29 21:55:53 +0000

    java/openjdk17: port to powerpc64le

--- java/openjdk17/files/patch-src_hotspot_os_bsd_os__bsd.cpp
+++ java/openjdk17/files/patch-src_hotspot_os_bsd_os__bsd.cpp
@@ -1,5 +1,13 @@
---- src/hotspot/os/bsd/os_bsd.cpp.orig	2019-03-28 21:47:08.000000000 -0700
-+++ src/hotspot/os/bsd/os_bsd.cpp	2019-03-28 23:08:55.590922000 -0700
+--- src/hotspot/os/bsd/os_bsd.cpp.orig	2021-09-29 21:25:10 UTC
++++ src/hotspot/os/bsd/os_bsd.cpp
+@@ -58,6 +58,7 @@
+ #include "runtime/thread.inline.hpp"
+ #include "runtime/threadCritical.hpp"
+ #include "runtime/timer.hpp"
++#include "runtime/vm_version.hpp"
+ #include "services/attachListener.hpp"
+ #include "services/memTracker.hpp"
+ #include "services/runtimeService.hpp"
 @@ -361,7 +361,7 @@
      #elif defined(__NetBSD__)
        #define DEFAULT_LIBPATH "/usr/lib:/usr/pkg/lib"
