ab1c2c47aba883c63f6763fbee1d9a1bf86ed2a0
Felix Palmen <felix@palmen-it.de>
2021-09-29 12:17:24 -0500

    devel/gsoap: Add SSL path to CFLAGS and LDFLAGS
    - Remove old libressl-related changes
    
    PR: 258182
    Reported by: rozhuk.im@gmail.com

--- devel/gsoap/files/patch-gsoap_stdsoap2.cpp
+++ devel/gsoap/files/patch-gsoap_stdsoap2.cpp
@@ -1,23 +1,5 @@
 --- gsoap/stdsoap2.cpp.orig	2019-02-10 15:19:37 UTC
 +++ gsoap/stdsoap2.cpp
-@@ -4306,7 +4306,7 @@ ssl_auth_init(struct soap *soap)
-   ERR_clear_error();
-   if (!soap->ctx)
-   {
--#if OPENSSL_VERSION_NUMBER >= 0x10100000L
-+#if (OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER))
-     /* TLS_method: a TLS/SSL connection established may understand the SSLv3, TLSv1, TLSv1.1 and TLSv1.2 protocols. */
-     soap->ctx = SSL_CTX_new(TLS_method());
- #else
-@@ -4394,7 +4394,7 @@ ssl_auth_init(struct soap *soap)
- #endif
-   if ((soap->ssl_flags & SOAP_SSL_RSA))
-   {
--#if OPENSSL_VERSION_NUMBER >= 0x10002000L
-+#if (OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER))
-     if (SSL_CTX_need_tmp_RSA(soap->ctx))
-     {
-       unsigned long e = RSA_F4;
 @@ -4473,10 +4473,12 @@ ssl_auth_init(struct soap *soap)
      minv = TLS1_1_VERSION;
    else if ((soap->ssl_flags & SOAP_TLSv1_2))
