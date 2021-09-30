479829cc0f4e9c54c7b5941a2c0dd490cbfc54c3
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-09-30 21:33:59 +0000

    Mk/bsd.lto.mk: also add -flto=thin to LDFLAGS
    Reported by: jbeich

--- Mk/bsd.lto.mk
+++ Mk/bsd.lto.mk
@@ -4,7 +4,8 @@ LTO_Include_MAINTAINER=	pkubaj@FreeBSD.org
 
 .if !defined(LTO_UNSAFE)
 # Overridable as a user may want to use -flto
-LTO_CFLAGS?=	-flto=thin
-CFLAGS+=	${LTO_CFLAGS}
-CXXFLAGS+=	${LTO_CFLAGS}
+LTO_FLAGS?=	-flto=thin
+CFLAGS+=	${LTO_FLAGS}
+CXXFLAGS+=	${LTO_FLAGS}
+LDFLAGS+=	${LTO_FLAGS}
 .endif
