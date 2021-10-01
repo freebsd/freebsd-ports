b8b32707f322a10b98d9cec7f818954e65d7d57b
Mikael Urankar <mikael@FreeBSD.org>
2021-10-01 14:46:23 +0200

    devel/stack: Add aarch64 support.
    Approved by:		arrowd
    Differential Revision:	https://reviews.freebsd.org/D32221

---
+++ devel/stack/files/patch-__cabal__deps_cryptonite-0.29_cbits_decaf_include_word.h
@@ -0,0 +1,13 @@
+https://github.com/haskell-crypto/cryptonite/issues/350
+
+--- _cabal_deps/cryptonite-0.29/cbits/decaf/include/word.h.orig	2019-09-08 00:46:48 UTC
++++ _cabal_deps/cryptonite-0.29/cbits/decaf/include/word.h
+@@ -238,7 +238,7 @@ malloc_vector(size_t size) {
+ /* PERF: vectorize vs unroll */
+ #ifdef __clang__
+ #if 100*__clang_major__ + __clang_minor__ > 305
+-#define UNROLL _Pragma("clang loop unroll(full)")
++//#define UNROLL _Pragma("clang loop unroll(full)")
+ #endif
+ #endif
+ 
