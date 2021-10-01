a79bfc5158678afcd04177d26c327b8af57745f1
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-10-01 10:28:25 +0000

    graphics/librsvg2-rust: fix build on big-endian
    patch-vendor_crossbeam-* patches are necessary on powerpc. They may be removed
    after Rust 1.56.0 is released, upstream for crossbeam* crates regenerates
    no_atomic.rs and those changes flow into ports.
    
    Same for the patch for builtns.rs (except that it also affects powerpc64le
    which is already in the upstream cfg_expr crate, but powerpc has not yet landed
    there).
    
    To fix build generally on big-endian (so powerpc and powerpc64 at the moment),
    merging https://gitlab.gnome.org/GNOME/librsvg/-/commit/92df18a3faf80c1d6d35899d9a5d22c74adfb8f8
    is also necessary.

--- graphics/librsvg2-rust/files/patch-vendor_cfg-expr_src_targets_builtins.rs
+++ graphics/librsvg2-rust/files/patch-vendor_cfg-expr_src_targets_builtins.rs
@@ -1,11 +1,30 @@
 https://github.com/EmbarkStudios/cfg-expr/commit/f8c59a8dccd0
 
---- vendor/cfg-expr/src/targets/builtins.rs.orig	2021-09-30 14:33:28 UTC
+--- vendor/cfg-expr/src/targets/builtins.rs.orig	2021-09-16 01:15:26 UTC
 +++ vendor/cfg-expr/src/targets/builtins.rs
-@@ -1004,6 +1004,16 @@ pub const ALL_BUILTINS: &[TargetInfo<'static>] = &[
-         endian: Endian::big,
+@@ -894,6 +894,16 @@ pub const ALL_BUILTINS: &[TargetInfo<'static>] = &[
+         endian: Endian::little,
      },
      TargetInfo {
++        triple: "powerpc-unknown-freebsd",
++        os: Some(Os::freebsd),
++        arch: Arch::powerpc,
++        env: None,
++        vendor: Some(Vendor::unknown),
++        family: Some(Family::unix),
++        pointer_width: 32,
++        endian: Endian::big,
++    },
++    TargetInfo {
+         triple: "powerpc-unknown-linux-gnu",
+         os: Some(Os::linux),
+         arch: Arch::powerpc,
+@@ -1002,6 +1012,16 @@ pub const ALL_BUILTINS: &[TargetInfo<'static>] = &[
+         family: Some(Family::unix),
+         pointer_width: 64,
+         endian: Endian::big,
++    },
++    TargetInfo {
 +        triple: "powerpc64le-unknown-freebsd",
 +        os: Some(Os::freebsd),
 +        arch: Arch::powerpc64,
@@ -14,8 +33,6 @@ https://github.com/EmbarkStudios/cfg-expr/commit/f8c59a8dccd0
 +        family: Some(Family::unix),
 +        pointer_width: 64,
 +        endian: Endian::little,
-+    },
-+    TargetInfo {
+     },
+     TargetInfo {
          triple: "powerpc64le-unknown-linux-gnu",
-         os: Some(Os::linux),
-         arch: Arch::powerpc64,
