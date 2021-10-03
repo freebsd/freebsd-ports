4c2db26adf7d81372f9c708118ecbe6ea38784b4
Thomas Zander <riggs@FreeBSD.org>
2021-10-03 12:16:04 +0200

    audio/fluidsynth: Update to upstream version 2.2.3
    MFH:		2021Q4

--- audio/fluidsynth/files/patch-src_drivers_fluid__adriver.c
+++ audio/fluidsynth/files/patch-src_drivers_fluid__adriver.c
@@ -1,11 +1,11 @@
---- src/drivers/fluid_adriver.c.orig	2018-12-30 11:42:00 UTC
+--- src/drivers/fluid_adriver.c.orig	2021-09-11 16:26:35 UTC
 +++ src/drivers/fluid_adriver.c
-@@ -39,6 +39,16 @@ struct _fluid_audriver_definition_t
+@@ -40,6 +40,16 @@ struct _fluid_audriver_definition_t
  /* Available audio drivers, listed in order of preference */
  static const fluid_audriver_definition_t fluid_audio_drivers[] =
  {
 +#if SNDIO_SUPPORT
-+    { 
++    {
 +       "sndio",
 +       new_fluid_sndio_audio_driver,
 +       new_fluid_sndio_audio_driver2,
@@ -14,6 +14,6 @@
 +    },
 +#endif
 +
- #if JACK_SUPPORT
+ #if ALSA_SUPPORT
      {
-         "jack",
+         "alsa",
