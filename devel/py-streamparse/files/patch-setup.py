e959ca709bb76186dddf74c7e5a03893028e9d19
Vinícius Zavam <egypcio@FreeBSD.org>
2021-10-04 10:04:00 +0000

    devel/py-streamparse: update 3.16.0 to 4.0.0
      - github.com/Parsely/streamparse/releases/tag/v4.0.0
    
    Reported by:	ygy@
    PR:		257428

--- devel/py-streamparse/files/patch-setup.py
+++ devel/py-streamparse/files/patch-setup.py
@@ -1,14 +1,14 @@
---- setup.py.orig	2019-01-13 16:56:06 UTC
+--- setup.py.orig	2021-10-04 08:45:30 UTC
 +++ setup.py
-@@ -16,6 +16,7 @@ limitations under the License.
+@@ -14,6 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either e
+ See the License for the specific language governing permissions and
+ limitations under the License.
  """
- import re
- import sys
 +import codecs
+ import re
  
  from setuptools import setup, find_packages
- 
-@@ -29,7 +30,7 @@ def get_version():
+@@ -28,7 +29,7 @@ def get_version():
  
  def readme():
      """ Returns README.rst contents as str """
