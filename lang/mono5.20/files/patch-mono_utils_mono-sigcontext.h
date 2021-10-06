756e364d86114622cbb1b3b9e572e4c2e85f6cb6
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-10-06 20:52:48 +0000

    lang/mono5.20: port to powerpc64
    Same as lang/mono, except that BTLS is enabled.

--- lang/mono5.20/files/patch-mono_utils_mono-sigcontext.h
+++ lang/mono5.20/files/patch-mono_utils_mono-sigcontext.h
@@ -1,9 +1,24 @@
 --- mono/utils/mono-sigcontext.h.orig	2018-07-11 23:51:16 UTC
 +++ mono/utils/mono-sigcontext.h
-@@ -460,6 +460,13 @@ typedef struct ucontext {
-	#define UCONTEXT_REG_SP(ctx) (((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__sp)
-	#define UCONTEXT_REG_R0(ctx) (((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__x [ARMREG_R0])
-	#define UCONTEXT_GREGS(ctx) (&(((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__x))
+@@ -363,10 +363,10 @@
+ #elif defined(__FreeBSD__)
+ 	typedef ucontext_t os_ucontext;
+ 
+-	#define UCONTEXT_REG_Rn(ctx, n)   ((ctx)->uc_mcontext.mc_gpr [(n)])
+-	#define UCONTEXT_REG_FPRn(ctx, n) ((ctx)->uc_mcontext.mc_fpreg [(n)])
+-	#define UCONTEXT_REG_NIP(ctx)     ((ctx)->uc_mcontext.mc_srr0)
+-	#define UCONTEXT_REG_LNK(ctx)     ((ctx)->uc_mcontext.mc_lr)
++	#define UCONTEXT_REG_Rn(ctx, n)   (((os_ucontext*)(ctx))->uc_mcontext.mc_gpr [(n)])
++	#define UCONTEXT_REG_FPRn(ctx, n) (((os_ucontext*)(ctx))->uc_mcontext.mc_fpreg [(n)])
++	#define UCONTEXT_REG_NIP(ctx)     (((os_ucontext*)(ctx))->uc_mcontext.mc_srr0)
++	#define UCONTEXT_REG_LNK(ctx)     (((os_ucontext*)(ctx))->uc_mcontext.mc_lr)
+ #elif defined(_AIX)
+ 	typedef ucontext_t os_ucontext;
+ 
+@@ -467,6 +467,13 @@
+ 	#define UCONTEXT_REG_SP(ctx) (((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__sp)
+ 	#define UCONTEXT_REG_R0(ctx) (((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__x [ARMREG_R0])
+ 	#define UCONTEXT_GREGS(ctx) (&(((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__x))
 +#elif defined(__FreeBSD__)
 +#include <ucontext.h>
 +	/* https://lists.freebsd.org/pipermail/freebsd-arm/2017-February/015611.html */
@@ -13,4 +28,4 @@
 +	#define UCONTEXT_GREGS(ctx) (&(((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_x))
  #else
  #include <ucontext.h>
-	#define UCONTEXT_REG_PC(ctx) (((ucontext_t*)(ctx))->uc_mcontext.pc)
+ 	#define UCONTEXT_REG_PC(ctx) (((ucontext_t*)(ctx))->uc_mcontext.pc)
