036bda781b0b170e823e3cacc14f25e2bd9c2b3a
Ganael LAPLANCHE <martymac@FreeBSD.org>
2021-10-10 17:11:27 +0200

    devel/onetbb: Update to 2021.4.0
    Release notes: https://software.intel.com/content/www/us/en/develop/articles/intel-oneapi-threading-building-blocks-release-notes.html

--- devel/onetbb/files/patch-test-common-utils_concurrency_limit.h
+++ devel/onetbb/files/patch-test-common-utils_concurrency_limit.h
@@ -1,16 +1,21 @@
-Backport edc39b844a483b69c39c64a136c4d62342884892 and 5c5d500d4fd502baed5176a19306e70c72d9c4e4
+Fix build on FreeBSD
 
---- test/common/utils_concurrency_limit.h.orig	2021-06-30 08:19:55 UTC
+__unix__ is defined on FreeBSD and makes #elif __FreeBSD__ section skipped
+
+--- test/common/utils_concurrency_limit.h.orig	2021-10-04 09:50:18 UTC
 +++ test/common/utils_concurrency_limit.h
-@@ -166,9 +166,10 @@ int limit_number_of_threads( int max_threads ) {
+@@ -37,12 +37,11 @@
+ #endif
+ #include <string.h>
+ #include <sched.h>
+-#elif __FreeBSD__
+-#include <unistd.h>
++#if __FreeBSD__
+ #include <errno.h>
+-#include <string.h>
+ #include <sys/param.h>
+ #include <sys/cpuset.h>
++#endif
+ #endif
+ #include <thread>
  
- #endif // __TBB_TEST_SKIP_AFFINITY
- 
--#define OS_AFFINITY_SYSCALL_PRESENT ((__linux__ && !__ANDROID__) || (__FreeBSD_version >= 701000))
-+#define OS_AFFINITY_SYSCALL_PRESENT (__linux__ && !__ANDROID__)
- 
- #if OS_AFFINITY_SYSCALL_PRESENT
-+
- void get_thread_affinity_mask(std::size_t& ncpus, std::vector<int>& free_indexes) {
-     cpu_set_t* mask = nullptr;
-     ncpus = sizeof(cpu_set_t) * CHAR_BIT;
