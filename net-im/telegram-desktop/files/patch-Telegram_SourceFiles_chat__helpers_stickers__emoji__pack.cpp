2bc3e6229febd07dd7f2980cb7065573d689a0f5
Henry Hu <henry.hu.sh@gmail.com>
2021-10-12 21:55:54 +0800

    net-im/telegram-desktop: Update to 3.1.9
    Thanks to the work done by Gleb (glebius@), we are able to remove most
    of the patches. 11 patches are upstreamed, and 2 seem to be obsolete.
    
    Changelog: https://github.com/telegramdesktop/tdesktop/blob/dev/changelog.txt
    
    PR:		259014

--- net-im/telegram-desktop/files/patch-Telegram_SourceFiles_chat__helpers_stickers__emoji__pack.cpp
+++ net-im/telegram-desktop/files/patch-Telegram_SourceFiles_chat__helpers_stickers__emoji__pack.cpp
@@ -1,6 +1,6 @@
---- Telegram/SourceFiles/chat_helpers/stickers_emoji_pack.cpp.orig	2020-08-23 13:36:52 UTC
+--- Telegram/SourceFiles/chat_helpers/stickers_emoji_pack.cpp.orig	2021-10-08 09:51:22 UTC
 +++ Telegram/SourceFiles/chat_helpers/stickers_emoji_pack.cpp
-@@ -88,7 +88,7 @@ constexpr auto kRefreshTimeout = 7200 * crl::time(1000
+@@ -99,7 +99,7 @@ constexpr auto kRefreshTimeout = 7200 * crl::time(1000
  		},
  		5,
  	};
