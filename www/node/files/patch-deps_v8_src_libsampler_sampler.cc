25cf6ee9a998c92ffc86f8fa8e6bbd96d9d32e76
Bradley T. Hughes <bhughes@FreeBSD.org>
2021-10-14 13:53:30 +0000

    www/node: Update 16.10.0 -> 16.11.0
    MFH:	2021Q4
    Sponsored by:	Miles AS

--- www/node/files/patch-deps_v8_src_libsampler_sampler.cc
+++ www/node/files/patch-deps_v8_src_libsampler_sampler.cc
@@ -1,6 +1,6 @@
---- deps/v8/src/libsampler/sampler.cc.orig	2021-04-20 11:30:13 UTC
+--- deps/v8/src/libsampler/sampler.cc.orig	2021-10-08 13:38:46 UTC
 +++ deps/v8/src/libsampler/sampler.cc
-@@ -488,6 +488,10 @@ void SignalHandler::FillRegisterState(void* context, R
+@@ -490,6 +490,10 @@ void SignalHandler::FillRegisterState(void* context, R
    state->pc = reinterpret_cast<void*>(mcontext.__gregs[_REG_PC]);
    state->sp = reinterpret_cast<void*>(mcontext.__gregs[_REG_SP]);
    state->fp = reinterpret_cast<void*>(mcontext.__gregs[_REG_FP]);
