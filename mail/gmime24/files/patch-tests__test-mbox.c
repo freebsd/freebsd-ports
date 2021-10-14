c1400983b1806e52c094f84f166e263c76a5aa2b
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2021-10-14 12:47:22 -0500

    mail/gmime24: mark DEPRECATED
    - As more recent versions are in the tree
    - Set EXPIRATION_DATE on 2021-11-30
    - Make patches makepatch compatible

--- mail/gmime24/files/patch-tests__test-mbox.c
+++ mail/gmime24/files/patch-tests__test-mbox.c
@@ -1,10 +1,10 @@
---- tests/test-mbox.c.orig	Mon Feb 12 15:48:22 2007
-+++ tests/test-mbox.c	Mon Feb 12 15:48:54 2007
-@@ -26,6 +26,7 @@
+--- tests/test-mbox.c.orig	2012-10-22 06:07:54 UTC
++++ tests/test-mbox.c
+@@ -32,6 +32,7 @@
  #include <fcntl.h>
  #include <time.h>
  
 +#include <config.h>
  #include <gmime/gmime.h>
  
- #ifndef G_OS_WIN32
+ #include "testsuite.h"
--- mail/gmime24/files/patch-tests__test-parser.c
+++ mail/gmime24/files/patch-tests__test-parser.c
@@ -1,10 +1,10 @@
---- tests/test-parser.c.orig	Mon Feb 12 15:48:38 2007
-+++ tests/test-parser.c	Mon Feb 12 15:49:14 2007
-@@ -26,6 +26,7 @@
+--- tests/test-parser.c.orig	2011-08-09 02:23:26 UTC
++++ tests/test-parser.c
+@@ -30,6 +30,7 @@
  #include <fcntl.h>
  #include <time.h>
  
 +#include <config.h>
  #include <gmime/gmime.h>
  
- #ifndef G_OS_WIN32
+ #if !defined (G_OS_WIN32) || defined (__MINGW32__)
