95cd93086e6fb0a44865d86a95486dfa59c16a83
Yuri Victorovich <yuri@FreeBSD.org>
2021-10-15 08:47:33 -0700

    biology/treekin: Unbreak
    Recent Lapack API changes broke it.
    
    PR:		259179
    (cherry picked from commit c16b145a0f292e0aaf321c7b48681ccd63f7280b)

--- biology/treekin/files/patch-src_calcpp.h
+++ biology/treekin/files/patch-src_calcpp.h
@@ -1,3 +1,5 @@
+- patch for latest breaking Lapack API changes, see https://github.com/Reference-LAPACK/lapack/issues/604#issuecomment-944069793
+
 --- src/calcpp.h.orig	2019-06-13 14:11:19 UTC
 +++ src/calcpp.h
 @@ -49,6 +49,9 @@
@@ -10,3 +12,23 @@
  #   include <lapacke/lapacke.h>
  # else
  #   ifdef HAVE_OPENBLAS_LAPACKE_H
+@@ -311,7 +314,8 @@ Calccpp::Mx_Dsyevx(const char *jobz,
+       /*default standard lapack */
+       ::dsyevx_((char *)jobz, (char *)range, (char *)uplo, n, (double *)a, lda, (double *)vl,
+               (double *)vu, il, iu, (double *)abstol, m, (double *)w,
+-              (double *)z, ldz, (double *)work, lwork, iwork, ifail, info);
++              (double *)z, ldz, (double *)work, lwork, iwork, ifail, info,
++              int(0) /*jobz_int*/, int(0) /*range_int*/, int(0) /*uplo_int*/); // "int" after the "info" parameter for each "char*" argument in the list
+       break;
+   }
+ }
+@@ -367,7 +371,8 @@ Calccpp::Mx_Dgeevx(const char *balanc,
+               (double *)wr, (double *)wi, (double *)vl, ldvl,
+               (double *)vr, ldvr, ilo, ihi, (double *)scale, (double *)abnrm, (double *)rconde,
+               (double *)rcondv, (double *)work,
+-              lwork, iwork, info);
++              lwork, iwork, info,
++              int(0) /*balanc_int*/, int(0) /*jobvl_int*/, int(0) /*jobvr_int*/, int(0) /*sense_int*/); // "int" after the "info" parameter for each "char*" argument in the list
+       break;
+   }
+ }
