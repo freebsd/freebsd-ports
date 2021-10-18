f12a94a12cc8887c351cb1de265b854a5697d57b
Mikael Urankar <mikael@FreeBSD.org>
2021-10-18 08:25:18 +0200

    lang/mono: Fix build on aarch64.
    Approved by:	portmgr (build fix blanket)

--- lang/mono5.10/files/patch-mono_utils_mono-sigcontext.h
+++ lang/mono5.10/files/patch-mono_utils_mono-sigcontext.h
@@ -1,6 +1,6 @@
---- mono/utils/mono-sigcontext.h.orig	2018-08-24 15:17:13 UTC
-+++ mono/utils/mono-sigcontext.h
-@@ -363,10 +363,10 @@ typedef struct ucontext {
+--- mono/utils/mono-sigcontext.h.orig	2021-10-05 20:43:22.170433000 +0200
++++ mono/utils/mono-sigcontext.h	2021-10-05 20:43:36.848022000 +0200
+@@ -363,10 +363,10 @@
  #elif defined(__FreeBSD__)
  	typedef ucontext_t os_ucontext;
  
@@ -15,3 +15,17 @@
  #endif
  
  #elif defined(TARGET_ARM)
+@@ -460,6 +460,13 @@ typedef struct ucontext {
+ 	#define UCONTEXT_REG_SP(ctx) (((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__sp)
+ 	#define UCONTEXT_REG_R0(ctx) (((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__x [ARMREG_R0])
+ 	#define UCONTEXT_GREGS(ctx) (&(((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__x))
++#elif defined(__FreeBSD__)
++#include <ucontext.h>
++	/* https://lists.freebsd.org/pipermail/freebsd-arm/2017-February/015611.html */
++	#define UCONTEXT_REG_PC(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_elr)
++	#define UCONTEXT_REG_SP(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_sp)
++	#define UCONTEXT_REG_R0(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_x [ARMREG_R0])
++	#define UCONTEXT_GREGS(ctx) (&(((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_x))
+ #else
+ #include <ucontext.h>
+ 	#define UCONTEXT_REG_PC(ctx) (((ucontext_t*)(ctx))->uc_mcontext.pc)
