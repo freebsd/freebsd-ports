ada48bb23728ccccbbbf190064b5151d3ff9b888
Pedro F. Giffuni <pfg@FreeBSD.org>
2021-10-21 23:43:59 -0500

    cad/calculix{-ccx}: Update to version 2.18
    Check http://www.dhondt.de/new_calc.htm for a list of new features.
    
    Approved by:	thierry (mentor, implicit)

---
+++ cad/calculix/files/patch-cgx__2.18_src_pickFunktions.c
@@ -0,0 +1,11 @@
+--- cgx_2.18/src/pickFunktions.c.orig	2021-09-18 09:40:04 UTC
++++ cgx_2.18/src/pickFunktions.c
+@@ -4594,7 +4594,7 @@ void moveLineEndPoint(int lineNr, int pntNr, double ll
+     else
+     {
+       printf("ERROR: selected point:%s is no line endpoint\n", point[pntNr].name);
+-      return(-1);
++      return;
+     }
+     u=flag*llength;
+     u/=scale->w;
