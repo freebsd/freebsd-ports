12cce839321eee9e5c745a71af56c8bdf556da39
Matthias Fechner <mfechner@FreeBSD.org>
2021-10-28 16:43:31 +0200

    www/gitlab-ce: Fix sidekiq-cron problem disable log to console
    The FreeBSD ports is having sidekiq-cron gem version 1.2 which is not
    compatible with gitlab yet.
    See issue:
    https://gitlab.com/gitlab-org/gitlab/-/issues/343695
    Following MR is included:
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/73093

--- www/gitlab-ce/files/patch-lib_gitlab_sidekiq__logging_structured__logger.rb
+++ www/gitlab-ce/files/patch-lib_gitlab_sidekiq__logging_structured__logger.rb
@@ -1,10 +1,11 @@
---- lib/gitlab/sidekiq_logging/structured_logger.rb.orig	2021-10-26 19:13:52 UTC
+--- lib/gitlab/sidekiq_logging/structured_logger.rb.orig	2021-10-21 22:00:37 UTC
 +++ lib/gitlab/sidekiq_logging/structured_logger.rb
-@@ -2,6 +2,7 @@
+@@ -2,6 +2,8 @@
  
  require 'active_record'
  require 'active_record/log_subscriber'
 +require 'sidekiq/job_logger'
++require 'sidekiq/job_retry'
  
  module Gitlab
    module SidekiqLogging
