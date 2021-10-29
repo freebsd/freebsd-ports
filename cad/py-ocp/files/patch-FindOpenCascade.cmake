41443836f017f8c69a32dcac15479305aebc903e
Antoine Brodin <antoine@FreeBSD.org>
2021-10-29 18:21:42 +0000

    cad/py-ocp: Revert update to 7.5.2 to unbreak the ports tree
    This reverts commit 224b32fffa22fc1abd54938df081c92210b539ba.

--- cad/py-ocp/files/patch-FindOpenCascade.cmake
+++ cad/py-ocp/files/patch-FindOpenCascade.cmake
@@ -1,8 +1,12 @@
---- FindOpenCascade.cmake.orig	2021-04-19 17:00:03 UTC
+--- FindOpenCascade.cmake.orig	2020-06-12 09:48:04 UTC
 +++ FindOpenCascade.cmake
-@@ -76,4 +76,4 @@ endforeach()
+@@ -69,6 +69,6 @@ foreach( MOD ${OCCT_MODULES})
  
- target_include_directories( OPENCASCADE INTERFACE ${OPENCASCADE_INCLUDE_DIR})
+ endforeach()
+ 
+-target_include_directories( OPENCASCADE INTERFACE ${OPENCASCADE_INCLUDE_DIR})
++target_include_directories( OPENCASCADE INTERFACE OCCT ${OPENCASCADE_INCLUDE_DIR})
  
 -find_package_handle_standard_args( OPENCASCADE DEFAULT_MSG OPENCASCADE_LIBRARIES OPENCASCADE_INCLUDE_DIR )
-+find_package_handle_standard_args( OpenCascade DEFAULT_MSG OPENCASCADE_LIBRARIES OPENCASCADE_INCLUDE_DIR )
+\ No newline at end of file
++find_package_handle_standard_args( OPENCASCADE DEFAULT_MSG OPENCASCADE_LIBRARIES OPENCASCADE_INCLUDE_DIR )
