a9d52e329d39fb914170e582065f10f079eb4c5b
Greg Lewis <glewis@FreeBSD.org>
2021-10-31 10:59:31 -0700

    java/openjdk17: Update to 17.0.1

--- java/openjdk17/files/patch-src_hotspot_os_bsd_os__bsd.cpp
+++ java/openjdk17/files/patch-src_hotspot_os_bsd_os__bsd.cpp
@@ -1,13 +1,5 @@
 --- src/hotspot/os/bsd/os_bsd.cpp.orig	2021-09-29 21:25:10 UTC
 +++ src/hotspot/os/bsd/os_bsd.cpp
-@@ -58,6 +58,7 @@
- #include "runtime/thread.inline.hpp"
- #include "runtime/threadCritical.hpp"
- #include "runtime/timer.hpp"
-+#include "runtime/vm_version.hpp"
- #include "services/attachListener.hpp"
- #include "services/memTracker.hpp"
- #include "services/runtimeService.hpp"
 @@ -361,7 +361,7 @@
      #elif defined(__NetBSD__)
        #define DEFAULT_LIBPATH "/usr/lib:/usr/pkg/lib"
