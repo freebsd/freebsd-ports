eecaadf531d9675f5a82a2971271b71429f7f1e7
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-11-01 21:25:39 +0800

    devel/py-ddtrace: Update to 0.55.1
    Changes:	https://github.com/DataDog/dd-trace-py/releases

--- devel/py-ddtrace/files/patch-setup.py
+++ devel/py-ddtrace/files/patch-setup.py
@@ -1,7 +1,7 @@
---- setup.py.orig	2021-09-21 14:40:10 UTC
+--- setup.py.orig	2021-10-28 20:18:41 UTC
 +++ setup.py
-@@ -202,7 +202,7 @@ setup(
-         "Programming Language :: Python :: 3.9",
+@@ -203,7 +203,7 @@ setup(
+         "Programming Language :: Python :: 3.10",
      ],
      use_scm_version={"write_to": "ddtrace/_version.py"},
 -    setup_requires=["setuptools_scm[toml]>=4,<6.1", "cython"],
