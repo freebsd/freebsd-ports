e363da0a843e7ec714aa2b3706411ee0c05af82d
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-11-02 05:18:45 +0800

    devel/py-l18n: Fix build with Python 3.10

---
+++ devel/py-l18n/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-10-31 03:58:31 UTC
++++ setup.py
+@@ -69,7 +69,7 @@ class PredistBuild(object):
+         log.info('translation files built successfully')
+ 
+ cmd_classes = {}
+-for cmd in ('sdist', 'bdist', 'bdist_egg', 'bdist_rpm', 'bdist_wininst'):
++for cmd in ('sdist', 'bdist', 'bdist_egg'):
+     try:
+         cmd_module = getattr(__import__('setuptools.command', fromlist=[cmd]),
+                              cmd)
