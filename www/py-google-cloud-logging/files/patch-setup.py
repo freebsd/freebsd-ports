e3689fc34260c22ac63a7c13cadd0cada97302c0
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-11-01 21:28:27 +0800

    www/py-google-cloud-logging: Allow build with py-google-cloud-appengine-logging 1.1.0
    - Bump PORTREVISION for package change

--- www/py-google-cloud-logging/files/patch-setup.py
+++ www/py-google-cloud-logging/files/patch-setup.py
@@ -1,5 +1,3 @@
-Obtained from:	https://github.com/googleapis/python-logging/commit/3ab386102d06637c3b0ba100c7a36a30d0ada26e
-
 --- setup.py.orig	2021-07-28 20:06:37 UTC
 +++ setup.py
 @@ -33,7 +33,7 @@ dependencies = [
@@ -7,7 +5,7 @@ Obtained from:	https://github.com/googleapis/python-logging/commit/3ab386102d066
      # https://github.com/googleapis/google-cloud-python/issues/10566
      "google-api-core[grpc] >= 1.26.0, <3.0.0dev",
 -    "google-cloud-appengine-logging >= 0.1.0, < 1.0.0dev",
-+    "google-cloud-appengine-logging >= 0.1.0, < 1.0.1",
++    "google-cloud-appengine-logging >= 0.1.0, < 1.1.1",
      "google-cloud-audit-log >= 0.1.0, < 1.0.0dev",
      # NOTE: Maintainers, please do not require google-api-core>=2.x.x
      # Until this issue is closed
