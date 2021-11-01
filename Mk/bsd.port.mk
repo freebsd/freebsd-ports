freebsd/freebsd-ports

commit fba040e62bff04323a29d5ad2e21f516b18e9bb4

Reduce code duplication by calling fetch target
when converting the do-fetch target to proper scripting
we lost the ability to overwrite do-fetch when running make makesum.
as reported here: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=

Let's call again do-fetch directly instead of duplicating its content

PR:		215530
Reported by:	jbeich
Differential Revision:	https://reviews.freebsd.org/D21544

Mk/bsd.port.mk
@@ -3873,20 +3873,8 @@ _CHECKSUM_INIT_ENV= \
 # As we're fetching new distfiles, that are not in the distinfo file, disable
 # checksum and sizes checks.
 makesum: check-sanity
-.if !empty(DISTFILES)
-	@${SETENV} \
-			${_DO_FETCH_ENV} ${_MASTER_SITES_ENV} \
-			dp_NO_CHECKSUM=yes dp_DISABLE_SIZE=yes \
-			dp_SITE_FLAVOR=MASTER \
-			${SH} ${SCRIPTSDIR}/do-fetch.sh ${DISTFILES:C/.*/'&'/}
-.endif
-.if defined(PATCHFILES) && !empty(PATCHFILES)
-	@${SETENV} \
-			${_DO_FETCH_ENV} ${_PATCH_SITES_ENV} \
-			dp_NO_CHECKSUM=yes dp_DISABLE_SIZE=yes \
-			dp_SITE_FLAVOR=PATCH \
-			${SH} ${SCRIPTSDIR}/do-fetch.sh ${PATCHFILES:C/:-p[0-9]//:C/.*/'&'/}
-.endif
+	@cd ${.CURDIR} && ${MAKE} fetch NO_CHECKSUM=yes \
+			DISABLE_SIZE=yes
 	@${SETENV} \
 			${_CHECKSUM_INIT_ENV} \
 			dp_CHECKSUM_ALGORITHMS='${CHECKSUM_ALGORITHMS:tu}' \

commit 8d07ec826a7af0500b5982aed7327fa3f7efeca8

Mk/bsd.port.mk: Fix makesum for ports that override MASTER_SITES in make(makesum)
fba040e62bff04323a29d5ad2e21f516b18e9bb4 broke makesum for ports
that conditionally override MASTER_SITES in a make(makesum)
block to simplify maintenance like lang/python-doc-html or
devel/freebsd-sysroot where it is impractical to just statically
list all possible MASTER_SITES. It also broke USES=linux ports
that do the same for DISTFILES.

The breakage with DISTFILES was fixed in
50d2c82e016fd176868cdc6e4befa606fa61c50e by explicitly passing
it to the sub-make fetch. Do the same with MASTER_SITES and
PATCH_SITES so we do not have to workaround this with .MAKEFLAGS
or .export.

PR:		249537
Approved by:	portmgr (implicit)

Mk/bsd.port.mk
@@ -3979,7 +3979,9 @@ _CHECKSUM_INIT_ENV= \
 # checksum and sizes checks.
 makesum: check-sanity
 	@cd ${.CURDIR} && ${MAKE} fetch NO_CHECKSUM=yes \
-			DISABLE_SIZE=yes DISTFILES="${DISTFILES}"
+			DISABLE_SIZE=yes DISTFILES="${DISTFILES}" \
+			MASTER_SITES="${MASTER_SITES}" \
+			PATCH_SITES="${PATCH_SITES}"
 	@${SETENV} \
 			${_CHECKSUM_INIT_ENV} \
 			dp_CHECKSUM_ALGORITHMS='${CHECKSUM_ALGORITHMS:tu}' \