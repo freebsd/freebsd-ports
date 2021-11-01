f366d96316722f0acfe0183f844e6fd3d6a5d80a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-11-01 21:23:13 +0800

    lang/v8-beta: Add v8-beta 9.6.180.5
    Changes:	https://v8.dev/blog/v8-release-96
    		https://chromium.googlesource.com/v8/v8.git/+log/refs/heads/9.6-lkgr
    		https://github.com/v8/v8/commits/9.6-lkgr

---
+++ lang/v8-beta/files/patch-src_api_api.cc
@@ -0,0 +1,11 @@
+--- src/api/api.cc.orig	2021-10-06 18:22:58 UTC
++++ src/api/api.cc
+@@ -5886,7 +5886,7 @@ bool v8::V8::Initialize(const int build_config) {
+   return true;
+ }
+ 
+-#if V8_OS_LINUX || V8_OS_MACOSX
++#if V8_OS_LINUX || V8_OS_MACOSX || V8_OS_OPENBSD || V8_OS_FREEBSD
+ bool TryHandleWebAssemblyTrapPosix(int sig_code, siginfo_t* info,
+                                    void* context) {
+ #if V8_ENABLE_WEBASSEMBLY && V8_TRAP_HANDLER_SUPPORTED
