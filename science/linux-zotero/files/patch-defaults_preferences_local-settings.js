8c6cd1e9189cdfa6918c020fb0c1fb87fa3c03ee
Mateusz Piotrowski <0mp@FreeBSD.org>
2021-11-03 17:14:48 +0100

    science/linux-zotero: Add new port
    Zotero is a reference management software to manage bibliographic data and
    related research materials.
    
    WWW: https://www.zotero.org/
    
    MFH:		2021Q4

---
+++ science/linux-zotero/files/patch-defaults_preferences_local-settings.js
@@ -0,0 +1,6 @@
+--- defaults/preferences/local-settings.js.orig	2021-11-03 15:32:28 UTC
++++ defaults/preferences/local-settings.js
+@@ -0,0 +1,3 @@
++// Disable automatic updates.
++pref("general.config.obscure_value", 0);
++pref("general.config.filename", "mozilla.cfg");
