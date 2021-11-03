17200781a8b671fe38629ae61cc9d89f65ddfbb8
Lewis Cook <lcook@FreeBSD.org>
2021-11-03 22:57:15 +0000

    www/lagrange: Update to 1.7.3
    Changes:	https://github.com/skyjake/lagrange/compare/v1.6.5...v1.7.3

--- www/lagrange/files/patch-lib_the__Foundation_Depends.cmake
+++ www/lagrange/files/patch-lib_the__Foundation_Depends.cmake
@@ -1,14 +1,13 @@
---- lib/the_Foundation/Depends.cmake.orig	2021-01-28 11:44:52 UTC
+--- lib/the_Foundation/Depends.cmake.orig	2021-11-03 22:54:49 UTC
 +++ lib/the_Foundation/Depends.cmake
 @@ -10,9 +10,8 @@ if (NOT IOS)
          set (iHaveCurl NO)
      endif ()
      if (TFDN_ENABLE_TLSREQUEST)
 -        pkg_check_modules (OPENSSL eopenssl11)  # BSD
--	if (NOT OPENSSL_FOUND)
+         if (NOT OPENSSL_FOUND)
 -            pkg_check_modules (OPENSSL openssl)
-+   	if (NOT OPENSSL_FOUND)
-+		find_package (OpenSSL REQUIRED)
++		find_package (OpenSSL REQUIRED) 
          endif ()
      else ()
          set (OPENSSL_FOUND NO)
