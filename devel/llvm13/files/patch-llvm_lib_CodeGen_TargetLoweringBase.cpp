d326758c32dff2938c1da97c0df5926612c3efa2
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-11-08 15:39:54 +0000

    devel/llvm13: revert to the older powerpc64 fix
    The newer one apparently causes a regression on amd64 freestanding binaries.
    
    While here, fix some whitespace issues in Makefile.
    
    Submitted by:	adalava

--- devel/llvm13/files/patch-llvm_lib_CodeGen_TargetLoweringBase.cpp
+++ devel/llvm13/files/patch-llvm_lib_CodeGen_TargetLoweringBase.cpp
@@ -1,15 +1,14 @@
---- llvm/lib/CodeGen/TargetLoweringBase.cpp.orig	2021-09-24 16:18:10 UTC
+diff --git a/contrib/llvm-project/llvm/lib/CodeGen/TargetLoweringBase.cpp b/contrib/llvm-project/llvm/lib/CodeGen/TargetLoweringBase.cpp
+--- llvm/lib/CodeGen/TargetLoweringBase.cpp
 +++ llvm/lib/CodeGen/TargetLoweringBase.cpp
-@@ -1980,8 +1980,11 @@ void TargetLoweringBase::insertSSPDeclarations(Module 
-     auto *GV = new GlobalVariable(M, Type::getInt8PtrTy(M.getContext()), false,
+@@ -1961,7 +1961,8 @@
                                    GlobalVariable::ExternalLinkage, nullptr,
                                    "__stack_chk_guard");
-+
-+    // FreeBSD has "__stack_chk_guard" defined externally on libc.so
      if (TM.getRelocationModel() == Reloc::Static &&
 -        !TM.getTargetTriple().isWindowsGNUEnvironment())
 +        !TM.getTargetTriple().isWindowsGNUEnvironment() &&
-+        !TM.getTargetTriple().isOSFreeBSD())
++	!(TM.getTargetTriple().isPPC64() && TM.getTargetTriple().isOSFreeBSD()))
        GV->setDSOLocal(true);
    }
  }
+
