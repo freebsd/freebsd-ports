cf29dd6e4661ca8eb4eedcd5e50021c56255fe7d
Florian Walpen <dev@submerge.ch>
2021-11-15 21:54:01 +0100

    devel/qtcreator: Fix incomplete update to 5.0.3.
    A partial update to qtcreator 5.0.3 snuck in with the update to
    KDE Frameworks. Thank you to Florian Walpen for finishing the work.
    Pointy hat:	adridg
    
    PR:		259751
    Reported by:	Peter TKATCHENKO

--- devel/qtcreator/files/patch-src_libs_qmljs_qmljscontext.cpp
+++ devel/qtcreator/files/patch-src_libs_qmljs_qmljscontext.cpp
@@ -4,9 +4,9 @@ In default ports builds Without this spurious qDebug(), the pointer
 allocated by the program. This hack may be disguising a memory problem
 higher up in the application.
 
---- src/libs/qmljs/qmljscontext.cpp.orig	2018-02-07 14:23:39 UTC
+--- src/libs/qmljs/qmljscontext.cpp.orig	2021-11-03 12:01:09 UTC
 +++ src/libs/qmljs/qmljscontext.cpp
-@@ -175,6 +175,7 @@ ReferenceContext::ReferenceContext(const
+@@ -191,6 +191,7 @@ ReferenceContext::ReferenceContext(const ContextPtr &c
  const Value *ReferenceContext::lookupReference(const Value *value)
  {
      const Reference *reference = value_cast<Reference>(value);
