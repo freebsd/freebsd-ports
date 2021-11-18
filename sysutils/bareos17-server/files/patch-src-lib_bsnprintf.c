0f6c39590217a05c9b03f47b884571402106d4e5
Dmitry Marakasov <amdmi3@FreeBSD.org>
2021-11-18 19:17:04 +0300

    sysutils/bareos17-client: fix build
    Approved by:	porgmgr blanket

--- sysutils/bareos17-server/files/patch-src-lib_bsnprintf.c
+++ sysutils/bareos17-server/files/patch-src-lib_bsnprintf.c
@@ -1,6 +1,6 @@
---- src/lib/bsnprintf.c	2021-08-04 23:06:43.246164000 -0500
-+++ src/lib/bsnprintf.c	2021-08-04 23:07:16.549429000 -0500
-@@ -618,6 +618,7 @@
+--- src/lib/bsnprintf.c.orig	2021-11-18 14:05:23 UTC
++++ src/lib/bsnprintf.c
+@@ -618,6 +618,7 @@ static LDOUBLE pow10(int exp)
     return result;
  }
  
@@ -8,10 +8,12 @@
  static int64_t round(LDOUBLE value)
  {
     int64_t intpart;
-@@ -629,6 +630,7 @@
+@@ -629,6 +630,9 @@ static int64_t round(LDOUBLE value)
  
     return intpart;
  }
++#else
++#include <math.h>
 +#endif
  
  static int32_t fmtfp(char *buffer, int32_t currlen, int32_t maxlen,
