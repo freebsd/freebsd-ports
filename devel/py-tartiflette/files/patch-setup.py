f954e0a2b948453e72ef58675db9ce7c8d21e0bc
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-11-22 02:20:24 +0800

    devel/py-tartiflette: Update to 1.4.1
    Changes:	https://github.com/tartiflette/tartiflette/releases

--- devel/py-tartiflette/files/patch-setup.py
+++ devel/py-tartiflette/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2021-08-02 14:49:15 UTC
+--- setup.py.orig	2021-11-15 11:04:50 UTC
 +++ setup.py
 @@ -42,7 +42,6 @@ class BuildExtCmd(build_ext):
  
@@ -8,16 +8,11 @@
          super().run()
  
  
-@@ -91,10 +90,9 @@ setup(
-     ],
-     keywords="api graphql protocol api rest relay tartiflette dailymotion",
-     packages=_PACKAGES,
--    install_requires=["cffi>=1.0.0,<2.0.0", "lark-parser==0.11.3", "pytz"],
-+    install_requires=["cffi>=1.0.0,<2.0.0", "lark-parser>=0.11.3", "pytz"],
-     tests_require=_TEST_REQUIRE,
-     extras_require={"test": _TEST_REQUIRE, "benchmark": _BENCHMARK_REQUIRE},
+@@ -52,6 +51,5 @@ class LibGraphQLParserExtension(Extension):
+ 
+ 
+ setup(
 -    cmdclass={"build_ext": BuildExtCmd, "build_py": BuildPyCmd},
 -    ext_modules=[LibGraphQLParserExtension()],
 +    cmdclass={"build_py": BuildPyCmd},
-     include_package_data=True,
  )
