e4a4006f4ccb8277ec94a55f185dcb24476ef529
Yuri Victorovich <yuri@FreeBSD.org>
2021-11-23 02:09:09 -0800

    audio/inscore: Update 1.28 -> 1.31
    Reported by:	portscout

--- audio/inscore/files/patch-src_inscore_model_Modules.h
+++ audio/inscore/files/patch-src_inscore_model_Modules.h
@@ -1,4 +1,4 @@
---- src/inscore/model/Modules.h.orig	2021-02-23 07:27:14 UTC
+--- src/inscore/model/Modules.h.orig	2021-11-08 06:26:08 UTC
 +++ src/inscore/model/Modules.h
 @@ -56,7 +56,7 @@
  
@@ -7,5 +7,5 @@
 -# define HASQGuidoImporter		true
 +# define HASQGuidoImporter		false
  # define HASFileDownloader		true
- # define HASWSSupport			true
- # define HASHTTPSupport			true
+ //#ifndef ANDROID
+ #if 1
