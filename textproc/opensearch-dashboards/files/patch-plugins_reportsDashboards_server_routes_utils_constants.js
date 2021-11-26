35d87da9988c372f5d2866778efb0ade41d78e8a
Romain Tartière <romain@FreeBSD.org>
2021-11-26 13:07:39 -1000

    textproc/opensearch-dashboards: Update to 1.2.0
    Resease notes:
    https://opensearch.org/blog/releases/2021/11/launch-announcement-1-2-0/
    
    With hat:	opensearch

--- textproc/opensearch-dashboards/files/patch-plugins_reportsDashboards_server_routes_utils_constants.js
+++ textproc/opensearch-dashboards/files/patch-plugins_reportsDashboards_server_routes_utils_constants.js
@@ -1,11 +1,11 @@
---- plugins/reportsDashboards/server/routes/utils/constants.js.orig	2021-09-16 00:07:34 UTC
+--- plugins/reportsDashboards/server/routes/utils/constants.js.orig	2021-11-22 18:22:17 UTC
 +++ plugins/reportsDashboards/server/routes/utils/constants.js
-@@ -130,7 +130,7 @@ const SECURITY_CONSTANTS = {
-   PROXY_AUTH_IP_HEADER: 'x-forwarded-for'
- };
+@@ -107,7 +107,7 @@ const SECURITY_CONSTANTS = {
  exports.SECURITY_CONSTANTS = SECURITY_CONSTANTS;
+ const EXTRA_HEADERS = ['cookie', 'x-proxy-user', 'x-proxy-roles', 'x-forwarded-for'];
+ exports.EXTRA_HEADERS = EXTRA_HEADERS;
 -const CHROMIUM_PATH = `${__dirname}/../../../.chromium/headless_shell`;
-+const CHROMIUM_PATH = '%%LOCALBASE%%/bin/chrome';
++const CHROMIUM_PATH = `%%LOCALBASE%%/bin/chrome`;
  /**
   * Metric constants
   */
