5e543050a907914386b13078cdfda1eba7cdf95d
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2021-11-28 10:15:46 -0600

    sysutils/asmon: Mark DEPRECATED
    - Fails to build on modern compilers
    - Set EXPIRATION_DATE to 2021-12-27
    - Return to pool
    - Make patches makepatch compatible

--- sysutils/asmon/files/patch-asmon-asmon.c
+++ sysutils/asmon/files/patch-asmon-asmon.c
@@ -1,15 +1,11 @@
---- asmon/asmon.c.bak	2012-03-13 19:03:06.795252708 +0600
-+++ asmon/asmon.c	2012-03-13 19:04:16.395242371 +0600
-@@ -990,7 +990,11 @@
+--- asmon/asmon.c.orig	2000-07-20 11:08:47 UTC
++++ asmon/asmon.c
+@@ -990,7 +990,7 @@ void DrawXmem(int Xpid, float total) 
  
  	kproc = kvm_getprocs(kd, KERN_PROC_PID, Xpid, &kcnt);
  
 -	Xsize = kproc->kp_eproc.e_vm.vm_map.size / 1024;
-+	#if __FreeBSD_version < 500000
-+		Xsize = kproc->kp_eproc.e_vm.vm_map.size / 1024;
-+	#else
-+		Xsize = kproc->ki_size / 1024;
-+	#endif
++	Xsize = kproc->ki_size / 1024;
  #endif
          if(old_Xsize!=Xsize)
  	{
