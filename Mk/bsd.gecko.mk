a2bd8616995c26c95d8ac7cdf655f006931d3df5
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2021-11-29 21:22:44 +0100

    www/firefox: update to 95.0 (rc1)
    Release Notes (soon):
      https://www.mozilla.org/en-US/firefox/95.0/releasenotes/
    
    This firefox release includes a not-yet-released snapshot of dav1d,
    which is not fully API-compatible with the released version. Switch
    to the bundled dav1d/aom until dav1d released have caught up.
    
    (cherry picked from commit f4c07f1834f70e7359c290415aaf47727cda042a)

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -130,9 +130,12 @@ RUSTFLAGS+=	${CFLAGS:M-mcpu=*:S/-mcpu=/-C target-cpu=/}
 # Standard depends
 _ALL_DEPENDS=	av1 event ffi graphite harfbuzz icu jpeg nspr nss png pixman sqlite vpx webp
 
+# firefox 95 uses a dav1d snapshot > 0.9.2
+.if ${MOZILLA_VER:R:R} < 95
 .if exists(${FILESDIR}/patch-bug1559213)
 av1_LIB_DEPENDS=	libaom.so:multimedia/aom libdav1d.so:multimedia/dav1d
 av1_MOZ_OPTIONS=	--with-system-av1
+.endif
 .endif
 
 event_LIB_DEPENDS=	libevent.so:devel/libevent
