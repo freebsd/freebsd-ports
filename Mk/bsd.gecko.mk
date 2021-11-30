67d9f1559aa71f3a4e90b99387a11871d00b7d8d
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2021-11-30 19:22:07 +0100

    www/firefox: make dep on wasi-compiler-rt more flexible
    as firefox needs matching wasi-compiler-rt, embedded llvm in
    rust and llvm for building and wasi-compiler-rt installs it's files
    in a path depending on it's full distversion, we need to know
    llvm/clang's full version. it's not possible to derive that
    programatically, as the required llvm version may not be installed
    at the time firefox' Makefile is being parsed.
    
    (cherry picked from commit dcc6b13daedf095da6572c27af69713ee551cb81)

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -102,6 +102,7 @@ MOZ_EXPORT+=	LLVM_OBJDUMP="${LOCALBASE}/bin/llvm-objdump${LLVM_DEFAULT}"
 # Ignore Mk/bsd.default-versions.mk but respect make.conf(5) unless LTO is enabled
 .if !defined(DEFAULT_VERSIONS) || ! ${DEFAULT_VERSIONS:Mllvm*} || ${PORT_OPTIONS:MLTO}
 LLVM_DEFAULT=	12 # chase bundled LLVM in lang/rust for LTO
+LLVM_VERSION=	12.0.1 # keep in sync with devel/wasi-compiler-rt${LLVM_DEFAULT}
 .endif
 # Require newer Clang than what's in base system unless user opted out
 . if ${CC} == cc && ${CXX} == c++ && exists(/usr/lib/libc++.so)
