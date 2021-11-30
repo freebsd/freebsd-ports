ecfed7ee5cb8f6cef90628a3df1ace1011b5dc17
Renato Botelho <garga@FreeBSD.org>
2021-11-30 16:22:19 -0300

    emulators/open-vm-tools: Fix build after 1400043
    NDINIT() last parameter was removed after this osversion
    
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- emulators/open-vm-tools/files/patch-modules_freebsd_vmblock_vfsops.c
+++ emulators/open-vm-tools/files/patch-modules_freebsd_vmblock_vfsops.c
@@ -1,4 +1,4 @@
---- modules/freebsd/vmblock/vfsops.c.orig	2018-03-30 18:44:35 UTC
+--- modules/freebsd/vmblock/vfsops.c.orig	2021-09-24 04:19:18 UTC
 +++ modules/freebsd/vmblock/vfsops.c
 @@ -124,6 +124,11 @@ VMBlockVFSMount(struct mount *mp,        // IN: mount(
     char *pathname;
@@ -12,7 +12,19 @@
     VMBLOCKDEBUG("VMBlockVFSMount(mp = %p)\n", (void *)mp);
  
     /*
-@@ -276,6 +281,11 @@ VMBlockVFSUnmount(struct mount *mp,    // IN: filesyst
+@@ -171,7 +176,11 @@ VMBlockVFSMount(struct mount *mp,        // IN: mount(
+     * Find lower node and lock if not already locked.
+     */
+ 
++#if __FreeBSD_version >= 1400043
++   NDINIT(ndp, LOOKUP, FOLLOW|LOCKLEAF, UIO_SYSSPACE, target);
++#else
+    NDINIT(ndp, LOOKUP, FOLLOW|LOCKLEAF, UIO_SYSSPACE, target, compat_td);
++#endif
+    error = namei(ndp);
+    if (error) {
+       NDFREE(ndp, 0);
+@@ -276,6 +285,11 @@ VMBlockVFSUnmount(struct mount *mp,    // IN: filesyst
     void *mntdata;
     int error;
     int flags = 0, removed = 0;
