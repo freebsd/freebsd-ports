609caa70c35550b9f21a789feb8a1fc890dba1bf
Rainer Hurling <rhurlin@FreeBSD.org>
2021-11-30 18:22:45 +0100

    astro/py-ephem: Update to 4.1.1
    Changelog: https://github.com/brandon-rhodes/pyephem/compare/4.1...4.1.1
    
    Reported by:	Repology

--- astro/py-ephem/files/patch-extensions___libastro.c
+++
@@ -1,9 +0,0 @@
---- extensions/_libastro.c.orig	2021-06-12 19:13:26 UTC
-+++ extensions/_libastro.c
-@@ -1,5 +1,6 @@
- /* Python header files. */
- 
-+#define PY_SSIZE_T_CLEAN
- #include "Python.h"
- 
- #if PY_MAJOR_VERSION == 2
