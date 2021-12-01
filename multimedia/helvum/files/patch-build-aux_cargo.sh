ef60d3eaa1046df6695a1150a2677e95da2cf714
Jan Beich <jbeich@FreeBSD.org>
2021-12-01 10:19:23 +0000

    multimedia/helvum: update to 0.3.2
    Changes:	https://gitlab.freedesktop.org/ryuukyu/helvum/-/compare/0.3.1...0.3.2
    Reported by:	Repology

--- multimedia/helvum/files/patch-build-aux_cargo.sh
+++ multimedia/helvum/files/patch-build-aux_cargo.sh
@@ -1,16 +1,15 @@
---- build-aux/cargo.sh.orig	2021-09-30 06:45:11 UTC
+--- build-aux/cargo.sh.orig	2021-11-30 17:25:58 UTC
 +++ build-aux/cargo.sh
-@@ -10,11 +10,11 @@ then
+@@ -12,10 +12,10 @@ then
      echo "DEBUG MODE"
      cargo build --manifest-path \
-         "$MESON_SOURCE_ROOT"/Cargo.toml && \
--        cp "$CARGO_TARGET_DIR"/debug/$5 $3
-+        cp "$CARGO_TARGET_DIR"/$CARGO_BUILD_TARGET/debug/$5 $3
+         "$MESON_SOURCE_ROOT/Cargo.toml" && \
+-        cp "$CARGO_TARGET_DIR/debug/$5" "$3"
++        cp "$CARGO_TARGET_DIR/$CARGO_BUILD_TARGET/debug/$5" "$3"
  else
      echo "RELEASE MODE"
      cargo build --manifest-path \
-         "$MESON_SOURCE_ROOT"/Cargo.toml --release && \
--        cp "$CARGO_TARGET_DIR"/release/$5 $3
-+        cp "$CARGO_TARGET_DIR"/$CARGO_BUILD_TARGET/release/$5 $3
+         "$MESON_SOURCE_ROOT/Cargo.toml" --release && \
+-        cp "$CARGO_TARGET_DIR/release/$5" "$3"
++        cp "$CARGO_TARGET_DIR/$CARGO_BUILD_TARGET/release/$5" "$3"
  fi
- 
