95de1b454625c8c251fe0ee8109391efe26de43f
Rainer Hurling <rhurlin@FreeBSD.org>
2021-12-02 15:06:05 +0100

    graphics/qgis: Fix broken patch naming
    Accidentally, the patches in commit 75007752abec044026edbb53aa0742cc47b49a89 were named incorrectly.
    
    Reported by:	pkg-fallout
    Fixes:	75007752abec, Fix on CURRENT after llvm13 intro

--- graphics/qgis/files/patch-src_core_providers_ogr_qgsogrprovider.cpp
+++ graphics/qgis/files/patch-src_core_providers_ogr_qgsogrprovider.cpp
@@ -1,6 +1,6 @@
---- src/core/providers/ogr/qgsogrproviderutils.cpp.orig	2021-11-19 12:08:37 UTC
-+++ src/core/providers/ogr/qgsogrproviderutils.cpp
-@@ -2311,7 +2311,7 @@ void QgsOgrProviderUtils::release( QgsOgrLayer *&layer
+--- src/core/providers/ogr/qgsogrprovider.cpp.orig	2021-11-19 12:10:19 UTC
++++ src/core/providers/ogr/qgsogrprovider.cpp
+@@ -5934,7 +5934,7 @@ void QgsOgrProviderUtils::release( QgsOgrLayer *&layer
  }
  
  
@@ -9,7 +9,7 @@
  {
    if ( !ds )
      return;
-@@ -2319,7 +2319,6 @@ void QgsOgrProviderUtils::releaseDataset( QgsOgrDatase
+@@ -5942,7 +5942,6 @@ void QgsOgrProviderUtils::releaseDataset( QgsOgrDatase
    QMutexLocker locker( sGlobalMutex() );
    releaseInternal( ds->mIdent, ds->mDs, true );
    delete ds;
