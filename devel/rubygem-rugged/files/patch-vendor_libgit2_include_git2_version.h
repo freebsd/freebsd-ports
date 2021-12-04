57a2bff976f5361671b26d0d0660818776aef0fc
Matthias Fechner <mfechner@FreeBSD.org>
2021-12-04 09:50:37 +0100

    devel/rubygem-rugged: update to 1.2.0
    Required for gitlab-ce 14.5
    Changelog:
    https://github.com/libgit2/rugged/compare/v1.1.1...v1.2.0

---
+++ devel/rubygem-rugged/files/patch-vendor_libgit2_include_git2_version.h
@@ -0,0 +1,18 @@
+--- vendor/libgit2/include/git2/version.h.orig	2021-11-22 20:02:49 UTC
++++ vendor/libgit2/include/git2/version.h
+@@ -7,12 +7,12 @@
+ #ifndef INCLUDE_git_version_h__
+ #define INCLUDE_git_version_h__
+ 
+-#define LIBGIT2_VERSION "1.2.0"
++#define LIBGIT2_VERSION "1.3.0"
+ #define LIBGIT2_VER_MAJOR 1
+-#define LIBGIT2_VER_MINOR 2
++#define LIBGIT2_VER_MINOR 3
+ #define LIBGIT2_VER_REVISION 0
+ #define LIBGIT2_VER_PATCH 0
+ 
+-#define LIBGIT2_SOVERSION "1.2"
++#define LIBGIT2_SOVERSION "1.3"
+ 
+ #endif
