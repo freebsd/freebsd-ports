9159da406b5a1b829147ff97c748d9963c2d9c4a
Jan Beich <jbeich@FreeBSD.org>
2021-12-08 18:47:57 +0000

    audio/gnome-podcasts: update to 0.5.0

--- audio/gnome-podcasts/files/patch-scripts_cargo.sh
+++ audio/gnome-podcasts/files/patch-scripts_cargo.sh
@@ -1,8 +1,8 @@
---- scripts/cargo.sh.orig	2021-09-08 13:34:20 UTC
+--- scripts/cargo.sh.orig	2021-12-04 18:15:44 UTC
 +++ scripts/cargo.sh
-@@ -22,4 +22,4 @@ if test -d vendor; then
+@@ -24,4 +24,4 @@ if test -d vendor; then
  fi
  
- cargo build ${ARGS[@]} --manifest-path="$1"/Cargo.toml -p podcasts-gtk
+ cargo build ${ARGS[@]} --manifest-path="$MESON_SOURCE_ROOT"/Cargo.toml -p podcasts-gtk
 -cp "$CARGO_TARGET_DIR"/${TARGET}/podcasts-gtk "$OUTPUT"
 +cp "$CARGO_TARGET_DIR"/${CARGO_BUILD_TARGET}/${TARGET}/podcasts-gtk "$OUTPUT"
