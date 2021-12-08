849bacb55c2c38a41c485181bdb270dd46df60d2
Michiel van Baak <michiel@vanbaak.eu>
2021-12-08 16:07:29 +0100

    multimedia/tautulli: Update to 2.7.7
    While here switch to DISTVERSION, pet portclippy and portfmt.
    
    Changes:		https://github.com/Tautulli/Tautulli/releases/tag/v2.7.7
    PR:			259479
    Approved by:		portmgr (maintainer timeout), philip (mentor)
    Differential Revision:	https://reviews.freebsd.org/D33336

--- multimedia/tautulli/files/patch-plexpy_config.py
+++ multimedia/tautulli/files/patch-plexpy_config.py
@@ -1,11 +1,11 @@
---- plexpy/config.py.orig	2018-03-17 14:16:52 UTC
+--- plexpy/config.py.orig	2021-10-27 10:01:59 UTC
 +++ plexpy/config.py
-@@ -111,7 +111,7 @@ _CONFIG_DEFINITIONS = {
-     'CACHE_DIR': (str, 'General', ''),
+@@ -89,7 +89,7 @@ _CONFIG_DEFINITIONS = {
      'CACHE_IMAGES': (int, 'General', 1),
      'CACHE_SIZEMB': (int, 'Advanced', 32),
+     'CHECK_DOCKER_MOUNT': (int, 'Advanced', 1),
 -    'CHECK_GITHUB': (int, 'General', 1),
 +    'CHECK_GITHUB': (int, 'General', 0),
      'CHECK_GITHUB_INTERVAL': (int, 'General', 360),
      'CHECK_GITHUB_ON_STARTUP': (int, 'General', 1),
-     'CLEANUP_FILES': (int, 'General', 0),
+     'CHECK_GITHUB_CACHE_SECONDS': (int, 'Advanced', 3600),
