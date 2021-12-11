37f85ee5cf12715887ca04ad3c5e58856e987665
Tobias C. Berner <tcberner@FreeBSD.org>
2021-12-11 10:58:23 +0100

    math/p5-Math-GSL: prepare for math/gsl-2.7.1
    - Note, this project seems mostly dead upstream and the
      support for gsl 2.7.* is likely not really functional.
    
    PR:		260201

--- math/p5-Math-GSL/files/patch-inc_Ver2Func.pm
+++ math/p5-Math-GSL/files/patch-inc_Ver2Func.pm
@@ -1,11 +1,15 @@
---- inc/Ver2Func.pm.orig	2021-06-16 17:58:11 UTC
+--- inc/Ver2Func.pm.orig	2020-06-12 22:07:37 UTC
 +++ inc/Ver2Func.pm
-@@ -397,6 +397,8 @@ my @ver2func = (
+@@ -397,7 +397,12 @@ my @ver2func = (
                ^gsl_matrix_uint_scale_columns$
                /
              ]
 +    },
 +    "2.7" => {
++    },
++    "2.7.1" => {
      }
++
  );
  
+ my ( %index, @info, @versions );
