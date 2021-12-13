17d04a615efce5b8d70c1d3cacf70135a98bae2a
Alexander Sieg <ports@xanderio.de>
2021-12-13 16:57:37 +0900

    www/owncast: update to 0.0.10
    Changelog:
    - https://github.com/owncast/owncast/releases/tag/v0.0.8
    - https://github.com/owncast/owncast/releases/tag/v0.0.9
    - https://github.com/owncast/owncast/releases/tag/v0.0.10
    
    PR:		259934
    Approved by:	de-freebsd@ctseuro.com (maintainer timeout, >3 weeks)

---
+++ www/owncast/files/patch-config_constants.go
@@ -0,0 +1,11 @@
+--- config/constants.go.orig	2021-11-19 09:16:05 UTC
++++ config/constants.go
+@@ -4,7 +4,7 @@ import "path/filepath"
+ 
+ const (
+ 	// StaticVersionNumber is the version of Owncast that is used when it's not overwritten via build-time settings.
+-	StaticVersionNumber = "0.0.9" // Shown when you build from develop
++	StaticVersionNumber = "0.0.10" // Shown when you build from develop
+ 	// WebRoot is the web server root directory.
+ 	WebRoot = "webroot"
+ 	// FfmpegSuggestedVersion is the version of ffmpeg we suggest.
