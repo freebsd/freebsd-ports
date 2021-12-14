61dc3caeec62b43e14aedb7c32148b45830a8d26
Yuri Victorovich <yuri@FreeBSD.org>
2021-12-13 20:35:21 -0800

    databases/arrow: Update 4.0.1 -> 6.0.1

--- databases/arrow/files/patch-cmake__modules_SetupCxxFlags.cmake
+++ databases/arrow/files/patch-cmake__modules_SetupCxxFlags.cmake
@@ -1,10 +1,10 @@
---- cmake_modules/SetupCxxFlags.cmake.orig	2021-04-21 16:14:36 UTC
+--- cmake_modules/SetupCxxFlags.cmake.orig	2021-11-10 02:07:56 UTC
 +++ cmake_modules/SetupCxxFlags.cmake
 @@ -28,7 +28,7 @@ if(NOT DEFINED ARROW_CPU_FLAG)
      set(ARROW_CPU_FLAG "armv8")
    elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "armv7")
      set(ARROW_CPU_FLAG "armv7")
--  elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "ppc")
+-  elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "powerpc|ppc")
 +  elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "powerpc|powerpc64|ppc")
      set(ARROW_CPU_FLAG "ppc")
    elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "s390x")
