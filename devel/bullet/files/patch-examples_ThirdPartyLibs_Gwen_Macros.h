0ee4d03db38fe6b927e6531d039841c985306106
Dima Panov <fluffy@FreeBSD.org>
2021-12-14 20:58:11 +0300

    devel/bullet: update to 3.21 release
    While here, take maintainership
    
    Approved by:	acm (previous maintainer, by mail)
    Sponsored by:	Netzkommune GmbH

--- devel/bullet/files/patch-examples_ThirdPartyLibs_Gwen_Macros.h
+++ devel/bullet/files/patch-examples_ThirdPartyLibs_Gwen_Macros.h
@@ -1,21 +1,11 @@
---- examples/ThirdPartyLibs/Gwen/Macros.h.orig	2017-02-17 23:43:38 UTC
+--- examples/ThirdPartyLibs/Gwen/Macros.h.orig	2021-05-07 18:04:02 UTC
 +++ examples/ThirdPartyLibs/Gwen/Macros.h
-@@ -4,9 +4,6 @@
- #define GWEN_MACROS_H
- #include <stdlib.h>
- #include <stdarg.h>
--#ifndef __APPLE__
--#include <malloc.h>
--#endif //__APPLE__
- #include <memory.h>
- #include <algorithm>
+@@ -41,7 +41,7 @@
+ #define GwenUtil_OutputDebugWideString(lpOutputString)  //wprintf( lpOutputString  )
+ #define GwenUtil_WideStringToFloat(_Str) wcstof(_Str, NULL)
  
-@@ -38,7 +35,7 @@
- 	#define GwenUtil_OutputDebugWideString( lpOutputString ) //wprintf( lpOutputString  )
- 	#define GwenUtil_WideStringToFloat( _Str ) wcstof(_Str, NULL)
+-#elif defined(__linux__) || defined(__GNUC__)
++#elif defined(__linux__) || defined(__GNUC__) || defined(__FreeBSD__)
  
--#elif defined(__linux__)
-+#elif defined(__linux__) || defined(__FreeBSD__)
- 
- 	#define GwenUtil_VSNPrintFSafe( _DstBuf, _DstSize, _MaxCount, _Format, _ArgList ) vsnprintf( _DstBuf, _DstSize, _Format, _ArgList )
- 	#define GwenUtil_VSWPrintFSafe( _DstBuf, _SizeInWords, _Format, _ArgList ) vswprintf( _DstBuf, _SizeInWords, _Format, _ArgList )
+ #define GwenUtil_VSNPrintFSafe(_DstBuf, _DstSize, _MaxCount, _Format, _ArgList) vsnprintf(_DstBuf, _DstSize, _Format, _ArgList)
+ #ifdef _WIN32
