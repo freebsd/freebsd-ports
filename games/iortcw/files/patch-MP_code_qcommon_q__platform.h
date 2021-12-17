b788897da19b5a85343fb73040f85f2efb788d04
Piotr Kubaj <pkubaj@FreeBSD.org>
2021-12-17 15:05:48 +0100

    games/iortcw: port to riscv64

--- games/iortcw/files/patch-MP_code_qcommon_q__platform.h
+++ games/iortcw/files/patch-MP_code_qcommon_q__platform.h
@@ -12,7 +12,7 @@
  
  #if (defined(powerc) || defined(powerpc) || defined(ppc) || \
  	defined(__ppc) || defined(__ppc__)) && !defined(C_ONLY)
-@@ -224,11 +220,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+@@ -224,11 +220,15 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
  #ifdef __i386__
  #define ARCH_STRING "i386"
  #elif defined __amd64__
@@ -25,6 +25,8 @@
 +#define ARCH_STRING "powerpc64"
 +#elif defined __powerpc__
 +#define ARCH_STRING "powerpc"
++#elif defined __riscv
++#define ARCH_STRING "riscv64"
  #endif
  
  #if BYTE_ORDER == BIG_ENDIAN
