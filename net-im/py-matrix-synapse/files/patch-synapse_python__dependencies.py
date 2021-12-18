757030bdb44734a22319f624842be356e26dbd8e
Sascha Biberhofer <ports@skyforge.at>
2021-12-18 07:24:33 +0000

    net-im/py-matrix-synapse: Update to 1.49.0
    PR:		260502

--- net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
+++ net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
@@ -1,11 +1,11 @@
---- synapse/python_dependencies.py.orig	2021-06-01 19:28:25 UTC
+--- synapse/python_dependencies.py.orig	2021-12-17 15:03:44 UTC
 +++ synapse/python_dependencies.py
-@@ -86,7 +86,7 @@ REQUIREMENTS = [
+@@ -85,7 +85,7 @@ REQUIREMENTS = [
      "typing-extensions>=3.7.4",
      # We enforce that we have a `cryptography` version that bundles an `openssl`
      # with the latest security patches.
 -    "cryptography>=3.4.7",
 +    "cryptography",
-     "ijson>=3.0",
+     "ijson>=3.1",
  ]
  
