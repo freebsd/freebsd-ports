e721e5aeffbd529fb28aeae0bf0b90b128a965e8
Bernard Spil <brnrd@FreeBSD.org>
2021-12-20 17:23:35 +0000

    www/apache24: Update to 2.4.52
    Security:	ca982e2d-61a9-11ec-8be6-d4c9ef517024
    MFH:		2021Q4

--- www/apache24/files/patch-modules_ssl_ssl__engine__io.c
+++ www/apache24/files/patch-modules_ssl_ssl__engine__io.c
@@ -9,30 +9,3 @@
      bio_filter_out_ctx_t *outctx = (bio_filter_out_ctx_t *)BIO_get_data(bio);
  
      switch (cmd) {
-@@ -542,7 +542,7 @@ static int bio_filter_in_gets(BIO *bio, char *buf, int
- 
- static long bio_filter_in_ctrl(BIO *bio, int cmd, long num, void *ptr)
- {
--    return -1;
-+    return 0;
- }
- 
- #if MODSSL_USE_OPENSSL_PRE_1_1_API
-@@ -567,7 +567,7 @@ static BIO_METHOD bio_filter_in_method = {
-     bio_filter_in_read,
-     bio_filter_in_puts,         /* puts is never called */
-     bio_filter_in_gets,         /* gets is never called */
--    bio_filter_in_ctrl,         /* ctrl is never called */
-+    bio_filter_in_ctrl, 
-     bio_filter_create,
-     bio_filter_destroy,
-     NULL
-@@ -594,7 +594,7 @@ void init_bio_methods(void)
-     BIO_meth_set_read(bio_filter_in_method, &bio_filter_in_read);
-     BIO_meth_set_puts(bio_filter_in_method, &bio_filter_in_puts);   /* puts is never called */
-     BIO_meth_set_gets(bio_filter_in_method, &bio_filter_in_gets);   /* gets is never called */
--    BIO_meth_set_ctrl(bio_filter_in_method, &bio_filter_in_ctrl);   /* ctrl is never called */
-+    BIO_meth_set_ctrl(bio_filter_in_method, &bio_filter_in_ctrl);
-     BIO_meth_set_create(bio_filter_in_method, &bio_filter_create);
-     BIO_meth_set_destroy(bio_filter_in_method, &bio_filter_destroy);
- }
