d0a6c40cb51b8d750f6bd8cd8c9842b76dedc3ec
Yuri Victorovich <yuri@FreeBSD.org>
2021-12-19 23:28:06 -0800

    math/{,py-}or-tools: Update 9.1 -> 9.2
    Reported by:	portscout

--- math/py-or-tools/files/patch-cmake_python.cmake
+++ math/py-or-tools/files/patch-cmake_python.cmake
@@ -1,4 +1,4 @@
---- cmake/python.cmake.orig	2021-10-01 03:13:58 UTC
+--- cmake/python.cmake.orig	2021-12-14 20:57:53 UTC
 +++ cmake/python.cmake
 @@ -23,7 +23,7 @@ if(UNIX AND NOT APPLE)
  endif()
@@ -8,13 +8,16 @@
 +find_package(Python3 ${FREEBSD_PYTHON_VER} REQUIRED EXACT COMPONENTS Interpreter Development.Module)
  list(APPEND CMAKE_SWIG_FLAGS "-py3" "-DPY3")
  
- # Find if python module MODULE_NAME is available,
-@@ -214,7 +214,7 @@ if(BUILD_TESTING)
-     COMMAND ${VENV_EXECUTABLE} ${VENV_DIR}
-     # Must not call it in a folder containing the setup.py otherwise pip call it
+ # Find if the python module is available,
+@@ -294,9 +294,9 @@ if(BUILD_TESTING)
+     #COMMAND ${VENV_EXECUTABLE} ${VENV_DIR}
+     # Must NOT call it in a folder containing the setup.py otherwise pip call it
      # (i.e. "python setup.py bdist") while we want to consume the wheel package
--    COMMAND ${VENV_Python3_EXECUTABLE} -m pip install --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PROJECT_NAME}
-+    COMMAND ${VENV_Python3_EXECUTABLE} -m pip install --no-index --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PROJECT_NAME}
-     COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/test.py.in ${VENV_DIR}/test.py
+-    COMMAND ${VENV_Python3_EXECUTABLE} -m pip install --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PYTHON_PROJECT}
++    COMMAND ${VENV_Python3_EXECUTABLE} -m pip install --no-index --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PYTHON_PROJECT}
+     # install modules only required to run examples
+-    COMMAND ${VENV_Python3_EXECUTABLE} -m pip install pandas matplotlib
++    #COMMAND ${VENV_Python3_EXECUTABLE} -m pip install pandas matplotlib
      BYPRODUCTS ${VENV_DIR}
-     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} VERBATIM)
+     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+     COMMENT "Create venv and install ${PYTHON_PROJECT}"
