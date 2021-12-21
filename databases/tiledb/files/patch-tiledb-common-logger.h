4ce88fb09a378875f2e7dd46b03626c1ef59f420
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2021-12-22 02:46:19 +0800

    databases/tiledb: Update to 2.5.2
    Changes:	https://github.com/TileDB-Inc/TileDB/releases

--- databases/tiledb/files/patch-tiledb-common-logger.h
+++ databases/tiledb/files/patch-tiledb-common-logger.h
@@ -1,10 +1,10 @@
---- tiledb/common/logger.h.orig	2021-09-08 12:12:06 UTC
+--- tiledb/common/logger.h.orig	2021-11-25 02:05:56 UTC
 +++ tiledb/common/logger.h
 @@ -52,6 +52,7 @@
  #define TILEDB_LOGGER_H
  
  #include <spdlog/spdlog.h>
 +#include <spdlog/sinks/stdout_color_sinks.h>
+ #include <atomic>
  #include <sstream>
  
- #include "tiledb/common/heap_memory.h"
