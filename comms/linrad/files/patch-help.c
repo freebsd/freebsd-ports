e91198da77a6215d6ef7730cc16004b8e44bb58b
Diane Bruce <db@FreeBSD.org>
2021-12-23 13:01:04 -0500

    comms/linrad: Unbreak on current
    Unbreak on head of tree.
    The port itself needs a major update. Patches welcome.

--- comms/linrad/files/patch-help.c
+++ comms/linrad/files/patch-help.c
@@ -1,20 +1,20 @@
---- help.c.orig	2014-11-04 05:25:00.000000000 -0500
-+++ help.c	2020-08-28 11:17:46.820070000 -0400
-@@ -720,7 +720,7 @@
+--- help.c.orig	2014-11-04 10:25:00 UTC
++++ help.c
+@@ -720,7 +720,7 @@ else
    printf("\n%s",s);
    DEB"\n%s",s);
    }  
 -msg_filename="errors.lir";
-+msg_filename="%%DATADIR%%errors.lir";
++msg_filename="%%PREFIX%%/share/linrad/errors.lir";
  line=3;
  write_from_msg_file(&line, lir_errcod, screen_mode, ERROR_VERNR);
  }  
-@@ -730,7 +730,7 @@
+@@ -730,7 +730,7 @@ void help_message(int msg_no)
  {
  int i, line;
  if(msg_no < 0)return;
 -msg_filename="help.lir";
-+msg_filename="%%DATADIR%%help.lir";
++msg_filename="%%PREFIX%%/share/linrad/help.lir";
  line=0;
  settextcolor(15);
  clear_screen();
--- comms/linrad/files/patch-libfind1.c
+++ comms/linrad/files/patch-libfind1.c
@@ -1,12 +1,12 @@
 --- libfind1.c.orig	2014-11-04 10:25:00 UTC
 +++ libfind1.c
-@@ -38,7 +38,8 @@ char *dirs[]={"/lib/i386-linux-gnu",
-               "/usr/lib64/lib",
-               "/usr/lib32",
-               "/usr/lib64",
--              "/usr/local/lib",
-+              "%%LOCALBASE%%lib",
-+              "%%LOCALBASE%%lib32",
-               "/usr/local/lib64",
-               "/usr/local32/lib",
-               "/usr/lib",
+@@ -30,7 +30,8 @@
+ char *dirs[]={"/lib/i386-linux-gnu",
+               "/lib/x86_64-linux-gnu",
+               "/lib",
+-              "/lib32",
++              "%%PREFIX%%/lib",
++	      "/lib32",
+               "/lib64",
+               "/usr/lib/i386-linux-gnu",
+               "/usr/lib/x86_64-linux-gnu",
