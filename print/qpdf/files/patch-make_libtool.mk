42dc2225828fc5190c5635f074f81b02db1796e0
Wen Heping <wen@FreeBSD.org>
2021-12-24 16:02:27 +0800

    print/qpdf: Update to 10.5.0

--- print/qpdf/files/patch-make_libtool.mk
+++ print/qpdf/files/patch-make_libtool.mk
@@ -1,4 +1,4 @@
---- make/libtool.mk.orig	2019-09-03 07:26:46 UTC
+--- make/libtool.mk.orig	2021-12-24 07:35:47 UTC
 +++ make/libtool.mk
 @@ -1,7 +1,7 @@
  # --- Required interface definitions ---
@@ -10,15 +10,10 @@
  
  OBJ=o
  LOBJ=lo
-@@ -112,14 +112,14 @@ install: all
- 	./mkinstalldirs -m 0755 $(DESTDIR)$(includedir)/qpdf
+@@ -128,10 +128,10 @@ install: all install-libs
+ 	./mkinstalldirs -m 0755 $(DESTDIR)$(bindir)
  	./mkinstalldirs -m 0755 $(DESTDIR)$(docdir)
  	./mkinstalldirs -m 0755 $(DESTDIR)$(mandir)/man1
--	$(LIBTOOL) --mode=install ./install-sh \
-+	$(LIBTOOL) --mode=install ./install-sh -s \
- 		libqpdf/$(OUTPUT_DIR)/libqpdf.la \
- 		$(DESTDIR)$(libdir)/libqpdf.la
- 	$(LIBTOOL) --finish $(DESTDIR)$(libdir)
 -	$(LIBTOOL) --mode=install ./install-sh \
 +	$(LIBTOOL) --mode=install ./install-sh -s \
  		qpdf/$(OUTPUT_DIR)/qpdf \
@@ -27,4 +22,4 @@
 +	$(LIBTOOL) --mode=install ./install-sh -s \
  		zlib-flate/$(OUTPUT_DIR)/zlib-flate \
  		$(DESTDIR)$(bindir)/zlib-flate
- 	./install-sh -m 0755 qpdf/fix-qdf $(DESTDIR)$(bindir)
+ 	$(LIBTOOL) --mode=install ./install-sh \
