2fa35a912315ebc2be23e12bbb86def55919e732
Dmitry Marakasov <amdmi3@FreeBSD.org>
2021-12-29 15:15:27 +0300

    games/stratagus: update to 3.2.0

--- games/stratagus/files/patch-gameheaders_stratagus-tinyfiledialogs.h
+++ games/stratagus/files/patch-gameheaders_stratagus-tinyfiledialogs.h
@@ -1,13 +1,30 @@
-We don't currently have osascript in ports, but for the case when
-we actually do, make sure it's not suddenly called and prefer zenity.
+Break overly clever and inconsistent logic of detecting a tool to
+display a dialog box, always stick to zenity. Don't try to beep
+too.
 
 --- gameheaders/stratagus-tinyfiledialogs.h.orig	2021-07-17 05:26:07 UTC
 +++ gameheaders/stratagus-tinyfiledialogs.h
-@@ -2350,6 +2350,7 @@ static int gdialogPresent ( )
+@@ -3676,6 +3676,7 @@ static int gdialogPresent(void)
  
- static int osascriptPresent ( )
+ static int osascriptPresent(void)
  {
 +	return 0;
- 	static int lOsascriptPresent = -1 ;
- 	if ( lOsascriptPresent < 0 )
- 	{
+     static int lOsascriptPresent = -1 ;
+     if (lOsascriptPresent < 0) {
+         gWarningDisplayed |= !!getenv("SSH_TTY");
+@@ -3779,6 +3780,7 @@ int tfd_zenity3Present(void)
+ 
+ int tfd_kdialogPresent(void)
+ {
++	return 0;
+ 	static int lKdialogPresent = -1 ;
+ 	char lBuff[MAX_PATH_OR_CMD] ;
+     FILE *lIn ;
+@@ -3974,6 +3976,7 @@ static void sigHandler(int signum)
+ 
+ void tinyfd_beep(void)
+ {
++	return;
+     char lDialogString[256] ;
+     FILE *lIn ;
+ 
