d110fac03fba6114a8ad47a3ed45d9674afcc50e
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2022-01-05 07:17:59 +0100

    deskutils/freeplane: Fix Unable to launch freeplane
    Fix a typo in the launch script and add bash as a dependency.
    Create a new launch script.
    
    PR:		260753
    Reported by:	chris@crvintel.com

--- deskutils/freeplane/files/patch-freeplane.sh
+++ deskutils/freeplane/files/patch-freeplane.sh
@@ -1,17 +1,28 @@
---- freeplane.sh.orig	2018-01-07 18:16:56 UTC
+--- freeplane.sh.orig	2022-01-05 06:17:27 UTC
 +++ freeplane.sh
-@@ -1,4 +1,4 @@
--#!/bin/bash
-+#!/bin/sh
+@@ -57,7 +57,7 @@ findjava() {
+ 		fi
+ 	fi
  
- # we only want to test the script, not Freeplane itself
- if ( echo "${DEBUG}" | grep -qe "script" ); then
-@@ -123,7 +123,7 @@ if [ -x $(which readlink) ] && [ "`echo $OSTYPE | cut 
- 	# if we have 'readlink' we can use it to get an absolute path
- 	# -m should be faster and link does always resolve, else this script
- 	# wouldn't be called, would it?
+-	JAVA_VERSION=$(${JAVACMD} -version |& grep -E "[[:alnum:]]+ version" | awk '{print $3}' | tr -d '"')
++	JAVA_VERSION=$(${JAVACMD} -version | grep -E "[[:alnum:]]+ version" | awk '{print $3}' | tr -d '"')
+ 	JAVA_MAJOR_VERSION=$(echo $JAVA_VERSION | awk -F. '{print $1}')
+ 	if [ $JAVA_MAJOR_VERSION -ge 16 ]; then
+ 		if [ -z "${FREEPLANE_USE_UNSUPPORTED_JAVA_VERSION}" ]; then
+@@ -142,15 +142,7 @@ fi
+ 
+ output_debug_info
+ 
+-if [ -x $(which readlink) ] && [ "`echo $OSTYPE | cut -b1-6`" != "darwin" ]; then
+-	# if we have 'readlink' we can use it to get an absolute path
+-	# -m should be faster and link does always resolve, else this script
+-	# wouldn't be called, would it?
 -	freefile=$(readlink -mn "$0")
-+	freefile=$(readlink -fn "$0")
- 	_debug "Link '$0' resolved to '${freefile}'."
- else
- 	freefile="$0"
+-	_debug "Link '$0' resolved to '${freefile}'."
+-else
+-	freefile="$0"
+-fi
++freefile="$0"
+ 
+ if [ "`echo $OSTYPE | cut -b1-6`" == "darwin" ]; then
+ 	xdockname='-Xdock:name=Freeplane'
