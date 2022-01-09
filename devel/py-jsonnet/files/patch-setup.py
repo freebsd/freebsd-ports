f3f5e0bc31c1ebe3a80d2e30b4f60ea5a8f6d160
Pavel Timofeev <timp87@gmail.com>
2022-01-09 08:38:15 +0000

    devel/py-jsonnet: Update to 0.18.0
    PR:		261040

--- devel/py-jsonnet/files/patch-setup.py
+++ devel/py-jsonnet/files/patch-setup.py
@@ -1,9 +1,9 @@
---- setup.py.orig	2020-11-22 14:19:41 UTC
+--- setup.py.orig	2022-01-08 16:20:10 UTC
 +++ setup.py
-@@ -19,18 +19,6 @@ from setuptools.command.build_ext import build_ext as 
- from subprocess import Popen
- 
+@@ -21,30 +21,6 @@ from subprocess import Popen
  DIR = os.path.abspath(os.path.dirname(__file__))
+ # NOTE: If you are editing the array below then you probably also need
+ # to change MANIFEST.in.
 -LIB_OBJECTS = [
 -    'core/desugarer.o',
 -    'core/formatter.o',
@@ -14,12 +14,24 @@
 -    'core/static_analysis.o',
 -    'core/string_utils.o',
 -    'core/vm.o',
--    'third_party/md5/md5.o'
+-    'third_party/md5/md5.o',
+-    'third_party/rapidyaml/rapidyaml/ext/c4core/src/c4/char_traits.o',
+-    'third_party/rapidyaml/rapidyaml/ext/c4core/src/c4/base64.o',
+-    'third_party/rapidyaml/rapidyaml/ext/c4core/src/c4/language.o',
+-    'third_party/rapidyaml/rapidyaml/ext/c4core/src/c4/memory_util.o',
+-    'third_party/rapidyaml/rapidyaml/ext/c4core/src/c4/format.o',
+-    'third_party/rapidyaml/rapidyaml/ext/c4core/src/c4/time.o',
+-    'third_party/rapidyaml/rapidyaml/ext/c4core/src/c4/memory_resource.o',
+-    'third_party/rapidyaml/rapidyaml/ext/c4core/src/c4/error.o',
+-    'third_party/rapidyaml/rapidyaml/src/c4/yml/parse.o',
+-    'third_party/rapidyaml/rapidyaml/src/c4/yml/preprocess.o',
+-    'third_party/rapidyaml/rapidyaml/src/c4/yml/common.o',
+-    'third_party/rapidyaml/rapidyaml/src/c4/yml/tree.o',
 -]
  
  MODULE_SOURCES = ['python/_jsonnet.c']
  
-@@ -46,19 +34,10 @@ def get_version():
+@@ -60,19 +36,10 @@ def get_version():
                      v_code = v_code[1:]
                  return v_code
  
@@ -35,12 +47,12 @@
      '_jsonnet',
      sources=MODULE_SOURCES,
 -    extra_objects=LIB_OBJECTS,
--    include_dirs = ['include', 'third_party/md5', 'third_party/json'],
+-    include_dirs = ['include'],
 +    libraries = ['jsonnet'],
      language='c++'
  )
  
-@@ -68,9 +47,6 @@ setup(name='jsonnet',
+@@ -83,9 +50,6 @@ setup(name='jsonnet',
        author='David Cunningham',
        author_email='dcunnin@google.com',
        version=get_version(),
