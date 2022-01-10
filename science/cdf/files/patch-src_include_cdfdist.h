db48936a5f19da6a6f1e7df73f4219dab9d1de5b
Stephen Montgomery-Smith <stephen@FreeBSD.org>
2022-01-10 13:32:06 -0600

    science/cdf: Allow build for ARM.
    Obtained from:	Robert Clausecker <fuz@fuz.su>

--- science/cdf/files/patch-src_include_cdfdist.h
+++ science/cdf/files/patch-src_include_cdfdist.h
@@ -1,5 +1,14 @@
---- src/include/cdfdist.h.orig	2022-01-08 02:20:38 UTC
+--- src/include/cdfdist.h.orig	2019-10-16 16:22:18 UTC
 +++ src/include/cdfdist.h
+@@ -135,7 +135,7 @@
+ #  define unix
+ #endif
+ 
+-#if defined(__arm__)
++#if defined(__arm__) || defined (__aarch64__)
+ #  define ARM
+ #  if defined(__BYTE_ORDER__)
+ #     if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 @@ -567,7 +567,7 @@ typedef sChar sByte;
  #      if defined(__DARWIN_64_BIT_INO_T)
  #         define STAT     stat
