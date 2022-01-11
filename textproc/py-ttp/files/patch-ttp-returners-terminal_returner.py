1f5040115670861c17fd14408eddcb224e51650c
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-01-12 01:17:53 +0800

    textproc/py-ttp: Update to 0.8.2
    Changes:	https://github.com/dmulyalin/ttp/releases

--- textproc/py-ttp/files/patch-ttp-returners-terminal_returner.py
+++ textproc/py-ttp/files/patch-ttp-returners-terminal_returner.py
@@ -1,12 +1,14 @@
---- ttp/returners/terminal_returner.py.orig	2020-10-29 23:14:41 UTC
+--- ttp/returners/terminal_returner.py.orig	2021-12-17 23:00:10 UTC
 +++ ttp/returners/terminal_returner.py
-@@ -33,12 +33,12 @@ def terminal_returner(data, **kwargs):
+@@ -33,14 +33,14 @@ def terminal_returner(data, **kwargs):
          for yeallow_word in yeallow_words:
              data = data.replace(yeallow_word, fttr.format(Y, yeallow_word, N))
      # print output
 -    if _ttp_["python_major_version"] is 2:
 +    if _ttp_["python_major_version"] == 2:
-         if isinstance(data, str) or isinstance(data, unicode):
+         if isinstance(data, str) or isinstance(
+             data, unicode
+         ):  # pylint: disable=undefined-variable
              print(data)
          else:
              print(str(data).replace("\\n", "\n"))
