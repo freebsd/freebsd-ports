e045eef41af9f313508398ebebf119be3d9c5574
Diane Bruce <db@FreeBSD.org>
2022-01-11 14:02:43 -0500

    comms/nanovna-saver:  Update to 3.10
    - Fixed FreeBSD specifc Sysctl.py code so it will no longer core
      if no USB device plugged in
    - changed delay in NanoVNA__V2.py since too fast for some devices
      see https://github.com/NanoVNA-Saver/nanovna-saver/issues/441
    
    Changelog
    =========
    
    v0.3.10
    ------
    
    - Default Band ranges for 5 and 9cm
    - Layout should fit on smaller screens
    - Fixed fixed axis settings
    - Show VNA type in port selector
    - Recognise tinySA (screenshot only)
    - Some more cables in TDR
    - Reference plane applied after calibration
    - Calibration fixes by DiSlord
    
    v0.3.9
    ------
    
    - TX Power on V2
    - New analysis
    - Magnitude Z Chart
    - VSWR Chart improvements
    
    Reported by:	portscout

--- comms/nanovna-saver/files/patch-NanoVNASaver_Hardware_Hardware.py
+++ comms/nanovna-saver/files/patch-NanoVNASaver_Hardware_Hardware.py
@@ -1,31 +1,28 @@
---- NanoVNASaver/Hardware/Hardware.py.orig	2020-11-01 13:28:10 UTC
+--- NanoVNASaver/Hardware/Hardware.py.orig	2022-01-04 07:44:03 UTC
 +++ NanoVNASaver/Hardware/Hardware.py
-@@ -33,6 +33,7 @@ from NanoVNASaver.Hardware.NanoVNA_H import NanoVNA_H
- from NanoVNASaver.Hardware.NanoVNA_H4 import NanoVNA_H4
+@@ -34,6 +34,7 @@ from NanoVNASaver.Hardware.NanoVNA_H4 import NanoVNA_H
  from NanoVNASaver.Hardware.NanoVNA_V2 import NanoVNA_V2
+ from NanoVNASaver.Hardware.TinySA import TinySA
  from NanoVNASaver.Hardware.Serial import drain_serial, Interface
 +from NanoVNASaver.Hardware.Sysctl import usb_vid_pid
  
  
  logger = logging.getLogger(__name__)
-@@ -63,8 +64,12 @@ def get_interfaces() -> List[Interface]:
-     interfaces = []
-     # serial like usb interfaces
+@@ -80,6 +81,11 @@ def get_interfaces() -> List[Interface]:
      for d in list_ports.comports():
--        if platform.system() == 'Windows' and d.vid is None:
--            d = _fix_v2_hwinfo(d)
+         if platform.system() == 'Windows' and d.vid is None:
+             d = _fix_v2_hwinfo(d)
 +        if platform.system() == 'FreeBSD':
 +            logger.debug("Found FreeBSD USB port %s", d.device)
 +            vid_pid = usb_vid_pid(d.device)
 +            d.vid = vid_pid[0]
 +            d.pid = vid_pid[1]
-+                
          for t in USBDEVICETYPES:
              if d.vid != t.vid or d.pid != t.pid:
                  continue
-@@ -74,7 +79,6 @@ def get_interfaces() -> List[Interface]:
-             iface.port = d.device
-             interfaces.append(iface)
+@@ -94,7 +100,6 @@ def get_interfaces() -> List[Interface]:
+ 
+     logger.debug("Interfaces: %s", interfaces)
      return interfaces
 -
  
