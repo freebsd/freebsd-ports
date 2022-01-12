a569c6e6c3d41c84172699d08e170309d039bca9
Tobias C. Berner <tcberner@FreeBSD.org>
2022-01-12 20:31:31 +0100

    math/p5-Math-GSL: prepare again for gsl-2.7.1
    - Note, this project seems mostly dead upstream and the
      support for gsl 2.7.* is likely not really functional.
    
    - The initial fix was reverted in 1a2add4b174eac35a3a4c36eb8e653ab72eb87ae
    
    PR:		260201

---
+++ math/p5-Math-GSL/files/patch-inc_Ver2Func.pm
@@ -0,0 +1,13 @@
+--- inc/Ver2Func.pm.orig	2021-07-26 16:28:55 UTC
++++ inc/Ver2Func.pm
+@@ -526,8 +526,8 @@ my @ver2func = (
+ 
+               /
+         ]
+-    }
+-
++    },
++    "2.7.1" => {},
+ );
+ 
+ my ( %index, @info, @versions );
