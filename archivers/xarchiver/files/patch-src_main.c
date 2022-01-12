88cbdd9c25e9af87860167f9a705bdabd82ee5cc
Max Brazhnikov <makc@FreeBSD.org>
2022-01-13 00:24:02 +0300

    archivers/xarchiver: switch from p7zip to 7-zip
    PR:	260866

--- archivers/xarchiver/files/patch-src_main.c
+++ archivers/xarchiver/files/patch-src_main.c
@@ -1,6 +1,15 @@
---- src/main.c.orig	2020-05-30 14:10:55 UTC
+--- src/main.c.orig	2020-12-12 13:32:03 UTC
 +++ src/main.c
-@@ -387,7 +387,7 @@ static void xa_check_available_archivers ()
+@@ -103,7 +103,7 @@ static void xa_check_available_archivers ()
+ 
+ 	/* (un)compressors that can handle various types */
+ 
+-	sevenz = g_find_program_in_path("7z");
++	sevenz = g_find_program_in_path("7zz");
+ 
+ 	if (!sevenz)
+ 	{
+@@ -388,7 +388,7 @@ static void xa_check_available_archivers ()
  	/* GNU zip */
  
  	type = XARCHIVETYPE_GZIP;
@@ -9,7 +18,7 @@
  
  	if (path)
  		archiver[type].is_compressor = TRUE;
-@@ -799,11 +799,11 @@ static void xa_check_available_archivers ()
+@@ -812,11 +812,11 @@ static void xa_check_available_archivers ()
  	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(prefs_window->prefer_unzip)) ||
  	    (!is7za && !lsar))
  	{
