72811e6606a8b4b3a1bebd0867e2a20a1c2e60f9
Max Brazhnikov <makc@FreeBSD.org>
2022-01-12 21:49:30 +0300

    comms/minicom: update to 2.8
    - Switch to new upstream and update to the latest version
    - Assign maintainership to submitter
    
    PR:		260967
    Submitted by:	Dustin Marquess

--- comms/minicom/files/patch-src-script.c
+++ comms/minicom/files/patch-src-script.c
@@ -1,14 +1,6 @@
---- src/script.c.orig	2013-10-29 11:07:42 UTC
+--- src/script.c.orig	2022-01-06 04:57:01 UTC
 +++ src/script.c
-@@ -32,6 +32,7 @@
- #include <config.h>
- #endif
- 
-+#include <sys/wait.h>
- #include <stdarg.h>
- 
- #include "port.h"
-@@ -76,7 +77,7 @@ struct env {
+@@ -77,7 +77,7 @@ struct env {
  struct env *curenv;		/* Execution environment */
  int gtimeout = 120;		/* Global Timeout */
  int etimeout = 0;		/* Timeout in expect routine */
--- comms/minicom/files/patch-src-window.c
+++ comms/minicom/files/patch-src-window.c
@@ -1,4 +1,4 @@
---- src/window.c.orig	2010-12-12 13:56:25 UTC
+--- src/window.c.orig	2021-01-01 17:45:55 UTC
 +++ src/window.c
 @@ -66,6 +66,7 @@ static const char *CE, *Al, *Dl, *AL, *DL;
  static const char *CS, *SF, *SR, *VB, *BL;
@@ -8,7 +8,7 @@
  static const char *CR, *NL;
  #ifdef ST_LINE
  static const char *TS, *FS, *DS;
-@@ -335,8 +336,8 @@ static void _gotoxy(int x, int y)
+@@ -320,8 +321,8 @@ static void _gotoxy(int x, int y)
    else if (NL != NULL && x == 0 && x == curx && y == cury + 1)
      outstr(NL);
  #endif
@@ -19,7 +19,7 @@
    else
      outstr(tgoto(CM, x, y));
    curx = x;
-@@ -2000,7 +2001,7 @@ int win_init(int fg, int bg, int attr)
+@@ -1950,7 +1951,7 @@ int win_init(int fg, int bg, int attr)
    CL = tgetstr("cl", &_tptr);
    IC = tgetstr("ic", &_tptr);
    DC = tgetstr("dc", &_tptr);
@@ -28,7 +28,7 @@
    CR = tgetstr("cr", &_tptr);
    NL = tgetstr("nl", &_tptr);
    AC = tgetstr("ac", &_tptr);
-@@ -2059,11 +2060,11 @@ int win_init(int fg, int bg, int attr)
+@@ -2009,11 +2010,11 @@ int win_init(int fg, int bg, int attr)
    _has_am = tgetflag("am");
    _mv_standout = tgetflag("ms");
    if (tgetflag("bs")) {
