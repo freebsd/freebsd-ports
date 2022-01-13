7350f869b64883d1a142509b190f27c5d945e83a
Wen Heping <wen@FreeBSD.org>
2022-01-13 11:08:19 +0800

    textproc/py-sphinx-markdown-tables: do not install LICENSE into /usr/local
    PR:		261161
    Reported by:	se@

---
+++ textproc/py-sphinx-markdown-tables/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2022-01-13 02:59:07 UTC
++++ setup.py
+@@ -29,7 +29,7 @@ setup(
+     keywords='sphinx markdown tables',
+     packages=['sphinx_markdown_tables'],
+     install_requires=['markdown>=3.0.1'],
+-    data_files=[('', ['LICENSE'])],
++#    data_files=[('', ['LICENSE'])],
+     project_urls={
+         'Bug Reports': 'https://github.com/ryanfox/sphinx-markdown-tables/issues',
+         'Say Thanks!': 'https://foxrow.com',
