61dc21a3030c154c0723a3ec27a172193fb6e890
Yuri Victorovich <yuri@FreeBSD.org>
2022-01-14 11:22:18 -0800

    multimedia/obs-ndi: Fix libndi.so name in the code and mark BROKEN
    PR:		258694

---
+++ multimedia/obs-ndi/files/patch-lib_ndi_Processing.NDI.Lib.h
@@ -0,0 +1,11 @@
+--- lib/ndi/Processing.NDI.Lib.h.orig	2022-01-14 18:57:16 UTC
++++ lib/ndi/Processing.NDI.Lib.h
+@@ -60,7 +60,7 @@
+ #			define NDILIB_REDIST_FOLDER "NDI_RUNTIME_DIR_V4"
+ #			define NDILIB_REDIST_URL    "http://new.tk/NDIRedistV4Apple"
+ #		else // __APPLE__
+-#			define NDILIB_LIBRARY_NAME  "libndi.so.4"
++#			define NDILIB_LIBRARY_NAME  "libndi.so"
+ #			define NDILIB_REDIST_FOLDER "NDI_RUNTIME_DIR_V4"
+ #			define NDILIB_REDIST_URL    ""
+ #		endif // __APPLE__
