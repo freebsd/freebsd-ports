43e4463f3a97fae1410469f894dce5e0d018af53
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2022-01-16 12:16:00 +0100

    textproc/bat: Update to 0.19.0
    Changes: https://github.com/sharkdp/bat/releases/tag/v0.19.0

--- textproc/bat/files/patch-src_bin_bat_main.rs
+++ textproc/bat/files/patch-src_bin_bat_main.rs
@@ -1,10 +1,10 @@
---- src/bin/bat/main.rs.orig	2021-07-19 20:42:50 UTC
-+++ src/bin/bat/main.rs
-@@ -233,7 +233,6 @@ fn run() -> Result<bool> {
+--- src/bin/bat/main.rs.orig	2022-01-08 17:01:20.000000000 +0100
++++ src/bin/bat/main.rs	2022-01-13 23:14:55.825571000 +0100
+@@ -232,7 +232,6 @@
  
-         let report = bugreport!()
-             .info(SoftwareVersion::default())
--            .info(OperatingSystem::default())
-             .info(CommandLine::default())
-             .info(EnvironmentVariables::list(&[
-                 "SHELL",
+     let mut report = bugreport!()
+         .info(SoftwareVersion::default())
+-        .info(OperatingSystem::default())
+         .info(CommandLine::default())
+         .info(EnvironmentVariables::list(&[
+             "SHELL",
