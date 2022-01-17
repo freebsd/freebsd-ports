d3564c5610c4d94d97971a6b7e45a7c3e45454ee
Cy Schubert <cy@FreeBSD.org>
2022-01-17 08:14:05 -0800

    security/wpa_supplicant: Update to 2.10
    The long awaited hostapd 2.10 is finally here.

--- security/wpa_supplicant/files/patch-src_drivers_driver__bsd.c
+++ security/wpa_supplicant/files/patch-src_drivers_driver__bsd.c
@@ -1,30 +1,6 @@
---- src/drivers/driver_bsd.c.orig	2019-08-07 06:25:25.000000000 -0700
-+++ src/drivers/driver_bsd.c	2021-06-13 23:07:14.016849000 -0700
-@@ -649,7 +649,7 @@
- 		len = 2048;
- 	}
- 
--	return len;
-+	return (len == 0) ? 2048 : len;
- }
- 
- #ifdef HOSTAPD
-@@ -1080,7 +1080,14 @@
- 		mode = 0 /* STA */;
- 		break;
- 	case IEEE80211_MODE_IBSS:
-+		/*
-+		 * Ref bin/203086 - FreeBSD's net80211 currently uses
-+		 * IFM_IEEE80211_ADHOC.
-+		 */
-+#if 0
- 		mode = IFM_IEEE80211_IBSS;
-+#endif
-+		mode = IFM_IEEE80211_ADHOC;
- 		break;
- 	case IEEE80211_MODE_AP:
- 		mode = IFM_IEEE80211_HOSTAP;
-@@ -1336,14 +1343,18 @@
+--- src/drivers/driver_bsd.c.orig	2021-06-02 14:11:18.000000000 -0700
++++ src/drivers/driver_bsd.c	2021-06-13 23:08:54.275496000 -0700
+@@ -853,14 +853,18 @@
  		drv = bsd_get_drvindex(global, ifm->ifm_index);
  		if (drv == NULL)
  			return;
@@ -46,3 +22,18 @@
  			wpa_printf(MSG_DEBUG, "RTM_IFINFO: Interface '%s' UP",
  				   drv->ifname);
  			wpa_supplicant_event(drv->ctx, EVENT_INTERFACE_ENABLED,
+@@ -1220,7 +1224,14 @@
+ 		mode = 0 /* STA */;
+ 		break;
+ 	case IEEE80211_MODE_IBSS:
++		/*
++		 * Ref bin/203086 - FreeBSD's net80211 currently uses
++		 * IFM_IEEE80211_ADHOC.
++		 */
++#if 0
+ 		mode = IFM_IEEE80211_IBSS;
++#endif
++		mode = IFM_IEEE80211_ADHOC;
+ 		break;
+ 	case IEEE80211_MODE_AP:
+ 		mode = IFM_IEEE80211_HOSTAP;
