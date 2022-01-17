f5e549a9f44e992c0a2f34b2f7686198f5590bad
Cy Schubert <cy@FreeBSD.org>
2022-01-17 08:15:43 -0800

    net/hostapd: Update to 2.10
    The long awaited hostapd 2.10 is finally here.

--- net/hostapd/files/patch-src_wps_wps__upnp.c
+++ net/hostapd/files/patch-src_wps_wps__upnp.c
@@ -1,20 +1,12 @@
---- src/wps/wps_upnp.c.orig	2015-03-15 17:30:39 UTC
-+++ src/wps/wps_upnp.c
-@@ -837,7 +837,7 @@ fail:
- }
- 
- 
--#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
- #include <sys/sysctl.h>
- #include <net/route.h>
- #include <net/if_dl.h>
-@@ -924,7 +924,7 @@ int get_netif_info(const char *net_if, u
+--- src/wps/wps_upnp.c.orig	2021-03-16 13:50:10.000000000 -0700
++++ src/wps/wps_upnp.c	2021-03-18 12:49:19.537874000 -0700
+@@ -963,7 +963,8 @@
  		goto fail;
  	}
  	os_memcpy(mac, req.ifr_addr.sa_data, 6);
--#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
+-#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__APPLE__)
++#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__APPLE__) \
++	|| defined(__DragonFly__)
  	if (eth_get(net_if, mac) < 0) {
  		wpa_printf(MSG_ERROR, "WPS UPnP: Failed to get MAC address");
  		goto fail;
