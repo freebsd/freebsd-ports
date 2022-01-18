629197586cda1f5f9b5ae670605a93b03767a962
Sascha Biberhofer <ports@skyforge.at>
2022-01-18 17:37:18 +0000

    net-im/py-matrix-synapse: Unbreak the port
    - Remove the dependency constraint added by upstream for problem
      on Debian GNU/Linux, which does not occur on FreeBSD
    
    This commit resolves a merge conflict due to skipping 0290f284f.
    
    PR:		261232
    (cherry picked from commit e51b5b86095bdb962b05f3c1e504fb33a91d5c96)

--- net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
+++ net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
@@ -1,6 +1,15 @@
---- synapse/python_dependencies.py.orig	2021-12-17 15:03:44 UTC
+--- synapse/python_dependencies.py.orig	2022-01-15 14:42:17 UTC
 +++ synapse/python_dependencies.py
-@@ -85,7 +85,7 @@ REQUIREMENTS = [
+@@ -51,7 +51,7 @@ REQUIREMENTS = [
+     # we use the TYPE_CHECKER.redefine method added in jsonschema 3.0.0
+     "jsonschema>=3.0.0",
+     # frozendict 2.1.2 is broken on Debian 10: https://github.com/Marco-Sulla/python-frozendict/issues/41
+-    "frozendict>=1,<2.1.2",
++    "frozendict>=1",
+     "unpaddedbase64>=1.1.0",
+     "canonicaljson>=1.4.0",
+     # we use the type definitions added in signedjson 1.1.
+@@ -86,7 +86,7 @@ REQUIREMENTS = [
      "typing-extensions>=3.7.4",
      # We enforce that we have a `cryptography` version that bundles an `openssl`
      # with the latest security patches.
