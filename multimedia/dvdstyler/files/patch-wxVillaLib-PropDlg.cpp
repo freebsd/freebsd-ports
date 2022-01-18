a98d4ac05dbf4edc95ce1ae95d35fbd5cf3f7bc2
Danilo Egea Gondolfo <danilo@FreeBSD.org>
2022-01-18 19:37:46 +0000

    multimedia/dvdstyler: Update to 3.2.1
    PR:		259678

---
+++ multimedia/dvdstyler/files/patch-wxVillaLib-PropDlg.cpp
@@ -0,0 +1,10 @@
+--- wxVillaLib/PropDlg2.cpp	2021-07-28 16:49:08.802073000 +0100
++++ wxVillaLib/PropDlg.cpp	2021-07-28 16:49:33.888103000 +0100
+@@ -12,7 +12,6 @@
+ #include "utils.h"
+ #include <wx/fontdlg.h>
+ #include <wx/colordlg.h>
+-#include <wx/generic/colrdlgg.h>
+ #include <wx/filedlg.h>
+ #include <wx/dirdlg.h>
+ #include <wx/grid.h>
