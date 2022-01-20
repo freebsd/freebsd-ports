a38ab8dc52010630ef28e1a42952ac67f8a0703b
Ashish SHUKLA <ashish@FreeBSD.org>
2022-01-20 13:51:19 +0000

    devel/soft-serve: Unbreak on 32-bit platforms
    Reported by:	pkg-fallout

--- devel/soft-serve/files/extrapatch-vendor_github.com_charmbracelet_bubbletea_cancelreader__bsd.go
+++ devel/soft-serve/files/extrapatch-vendor_github.com_charmbracelet_bubbletea_cancelreader__bsd.go
@@ -1,9 +1,12 @@
---- vendor/github.com/charmbracelet/bubbletea/cancelreader_bsd.go.orig	2021-12-11 06:31:30 UTC
+--- vendor/github.com/charmbracelet/bubbletea/cancelreader_bsd.go.orig	2022-01-07 18:25:29 UTC
 +++ vendor/github.com/charmbracelet/bubbletea/cancelreader_bsd.go
-@@ -134,9 +134,9 @@ func (r *kqueueCancelReader) wait() error {
+@@ -134,11 +134,11 @@ func (r *kqueueCancelReader) wait() error {
+ 		break
  	}
  
- 	switch events[0].Ident {
+-	ident := uint64(events[0].Ident)
++	ident := uint32(events[0].Ident)
+ 	switch ident {
 -	case uint64(r.file.Fd()):
 +	case uint32(r.file.Fd()):
  		return nil
