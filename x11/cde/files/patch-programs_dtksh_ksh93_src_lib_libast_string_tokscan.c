181ced015360d1b86b57298da7be4422e0d28522
Cy Schubert <cy@FreeBSD.org>
2022-01-20 13:20:59 -0800

    x11/cde: MFH x11/cde fixes
    The following MFHs were requested by  Robert Clausecker <fuz@fuz.su>
    in PR/261359:
    
    Requested by:	Robert Clausecker <fuz@fuz.su>
    PR:		261359
    
    --
    x11/cde: Fix armv7 build
    
    In attempting to fix powerpc64le, c66a851d13783558a8fa17ffcf64759a0c1b5bab
    broke armv7. Fix it.
    
    PR:		261359
    Reported by:	Robert Clausecker <fuz@fuz.su>
    Fixes:		c66a851d13783558a8fa17ffcf64759a0c1b5bab
    
    (cherry picked from commit 47d8fb3af3f0e411edcb06a9c694813404c8fe39)
    
    --
    x11/cde: Mark BROKEN on armv7
    
    STAGEDIR contains numerous intermediate files likely used during the
    install process which are not in the final STAGEDIR on other platforms.
    An example of one of the 5830 orphaned files is:
    dt/app-defaults/C/ecp.2AeDmfIk.
    
    PR:		261359
    (cherry picked from commit 03c319e5e9f2bd0244819d0d74159d98c10842d2)
    
    --
    x11/cde: Handle undstrippable outputs
    
    Stripping unstrippable files results in ecp.* files. Deal with the
    fallout.
    
    PR:		261359, 261359
    (cherry picked from commit ba5de232fe34dc71085e96d1df7937bea080f4d6)
    
    --
    x11/cde: Fix a typo
    
    Reported by:	adridg
    
    (cherry picked from commit 235153c84f41c06ba188bf3df297a6e87b2fe339)

--- x11/cde/files/patch-programs_dtksh_ksh93_src_lib_libast_string_tokscan.c
+++ x11/cde/files/patch-programs_dtksh_ksh93_src_lib_libast_string_tokscan.c
@@ -1,20 +1,25 @@
---- programs/dtksh/ksh93/src/lib/libast/string/tokscan.c.orig	2021-12-13 22:46:02 UTC
-+++ programs/dtksh/ksh93/src/lib/libast/string/tokscan.c
-@@ -188,6 +188,7 @@ tokscan(register char* s, char** nxt, const char* fmt,
+--- programs/dtksh/ksh93/src/lib/libast/string/tokscan.c.orig	2022-01-20 12:56:38.679529000 -0800
++++ programs/dtksh/ksh93/src/lib/libast/string/tokscan.c	2022-01-20 13:02:26.179022000 -0800
+@@ -188,6 +188,9 @@
  	char**		p_string;
  	char*		prv_f = 0;
  	va_list		prv_ap;
++#ifdef POWERPC64LE
 +	va_listarg	tmpval;
++#endif
  
  	va_start(ap, fmt);
  	if (!*s || *s == '\n')
-@@ -233,7 +234,8 @@ tokscan(register char* s, char** nxt, const char* fmt,
+@@ -233,7 +236,12 @@
  			prv_f = f;
  			f = va_arg(ap, char*);
  			va_copy(prv_ap, ap);
--			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++#ifdef POWERPC64LE
 +			tmpval = va_listval(va_arg(ap, va_listarg));
 +			va_copy(ap, tmpval);
++#else
+ 			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++#endif
  			continue;
  		case 'c':
  			p_char = va_arg(ap, char*);
