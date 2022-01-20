freebsd/freebsd-ports

commit c66a851d13783558a8fa17ffcf64759a0c1b5bab

x11/cde: fix build on powerpc64le
1. Merge patches from shells/ast-ksh fixing ksh93 build.
2. Change endianness detection to what __BYTE_ORDER__ points to.
The way CDE detects endianness is not correct and I got errors about running
neither BE or LE.

This still fails to build on powerpc64, so BROKEN_powerpc64 is not removed.

x11/cde/files/patch-programs_dtksh_ksh93_src_lib_libast_hash_hashalloc.c
@@ -1,0 +1,20 @@
+--- programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c.orig	2021-12-13 19:03:46 UTC
++++ programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c
+@@ -49,6 +49,7 @@ hashalloc(Hash_table_t* ref, ...)
+ 	va_list*		vp = va;
+ 	Hash_region_f		region = 0;
+ 	void*			handle;
++	va_listarg		tmpval;
+ 
+ 	va_start(ap, ref);
+ 
+@@ -151,7 +152,8 @@ hashalloc(Hash_table_t* ref, ...)
+ 				va_copy(*vp, ap);
+ 				vp++;
+ 			}
+-			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++			tmpval = va_listval(va_arg(ap, va_listarg));
++			va_copy(ap, tmpval);
+ 			break;
+ 		case 0:
+ 			if (vp > va)

x11/cde/files/patch-programs_dtksh_ksh93_src_lib_libast_string_tokscan.c
@@ -1,0 +1,20 @@
+--- programs/dtksh/ksh93/src/lib/libast/string/tokscan.c.orig	2021-12-13 22:46:02 UTC
++++ programs/dtksh/ksh93/src/lib/libast/string/tokscan.c
+@@ -188,6 +188,7 @@ tokscan(register char* s, char** nxt, const char* fmt,
+ 	char**		p_string;
+ 	char*		prv_f = 0;
+ 	va_list		prv_ap;
++	va_listarg	tmpval;
+ 
+ 	va_start(ap, fmt);
+ 	if (!*s || *s == '\n')
+@@ -233,7 +234,8 @@ tokscan(register char* s, char** nxt, const char* fmt,
+ 			prv_f = f;
+ 			f = va_arg(ap, char*);
+ 			va_copy(prv_ap, ap);
+-			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++			tmpval = va_listval(va_arg(ap, va_listarg));
++			va_copy(ap, tmpval);
+ 			continue;
+ 		case 'c':
+ 			p_char = va_arg(ap, char*);

x11/cde/files/patch-programs_dtinfo_DtMmdb_btree__berkeley_compat.h
@@ -1,0 +1,16 @@
+--- programs/dtinfo/DtMmdb/btree_berkeley/compat.h.orig	2021-12-14 13:35:18 UTC
++++ programs/dtinfo/DtMmdb/btree_berkeley/compat.h
+@@ -147,11 +147,11 @@ void * __fix_realloc __P((void*, size_t));
+ #define LITTLE_ENDIAN	1234		/* LSB first: i386, vax */
+ #define BIG_ENDIAN	4321		/* MSB first: 68000, ibm, net */
+ 
+-#ifdef MMDB_LITTLE_ENDIAN
++#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+ #define BYTE_ORDER	LITTLE_ENDIAN	/* Set for your system. */
+ #endif
+ 
+-#ifdef MMDB_BIG_ENDIAN
++#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+ #define BYTE_ORDER	BIG_ENDIAN	/* Set for your system. */
+ #endif
+ 

x11/cde/files/patch-programs_dtinfo_DtMmdb_schema_store__desc.C
@@ -1,0 +1,30 @@
+--- programs/dtinfo/DtMmdb/schema/store_desc.C.orig	2021-12-14 19:59:51 UTC
++++ programs/dtinfo/DtMmdb/schema/store_desc.C
+@@ -66,11 +66,11 @@ store_desc::store_desc(const char* name) : desc(name)
+    mode_str = strdup("");
+    order_str = 0;
+ 
+-#ifdef MMDB_BIG_ENDIAN
++#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    order_str = strdup("big_endian");
+ #endif
+ 
+-#ifdef MMDB_LITTLE_ENDIAN
++#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    order_str = strdup("little_endian");
+ #endif
+ 
+@@ -87,11 +87,11 @@ store_desc::store_desc(int tp, const char* comment): 
+ 
+    order_str = 0;
+ 
+-#ifdef MMDB_BIG_ENDIAN
++#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    order_str = strdup("big_endian");
+ #endif
+ 
+-#ifdef MMDB_LITTLE_ENDIAN
++#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    order_str = strdup("little_endian");
+ #endif
+ 

x11/cde/files/patch-programs_dtinfo_DtMmdb_storage_spointer.h
@@ -1,0 +1,11 @@
+--- programs/dtinfo/DtMmdb/storage/spointer.h.orig	2021-12-14 10:42:39 UTC
++++ programs/dtinfo/DtMmdb/storage/spointer.h
+@@ -52,7 +52,7 @@
+ 
+ struct header_t 
+ {
+-#ifdef MMDB_BIG_ENDIAN
++#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    unsigned spointer    : 14;
+    unsigned length      : 14;
+    unsigned is_object   : 1;

x11/cde/files/patch-programs_dtinfo_DtMmdb_utility_atoi__pearson.C
@@ -1,0 +1,19 @@
+--- programs/dtinfo/DtMmdb/utility/atoi_pearson.C.orig	2021-12-14 01:08:51 UTC
++++ programs/dtinfo/DtMmdb/utility/atoi_pearson.C
+@@ -107,14 +107,14 @@ atoi_pearson::~atoi_pearson()
+ }
+ 
+ struct reg_t {
+-#ifdef MMDB_BIG_ENDIAN
++#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    unsigned b4: 8;
+    unsigned b3: 8;
+    unsigned b2: 8;
+    unsigned b1: 8;
+ #endif
+ 
+-#ifdef MMDB_LITTLE_ENDIAN
++#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    unsigned b1: 8;
+    unsigned b2: 8;
+    unsigned b3: 8;

commit 47d8fb3af3f0e411edcb06a9c694813404c8fe39

x11/cde: Fix armv7 build
In attempting to fix powerpc64le, c66a851d13783558a8fa17ffcf64759a0c1b5bab
broke armv7. Fix it.

PR:		261359
Reported by:	Robert Clausecker <fuz@fuz.su>
Fixes:		c66a851d13783558a8fa17ffcf64759a0c1b5bab

x11/cde/Makefile
@@ -40,6 +40,12 @@ MAKE_JOBS_UNSAFE=yes
 SUB_FILES=	pkg-message
 LDFLAGS+=	-z muldefs #--allow-multiple-definition
 
+.include <bsd.port.pre.mk>
+
+.if ${ARCH} == powerpc64le
+CPPFLAGS+=	-DPOWERPC64LE
+.endif
+
 post-patch:
 	@if [ -f /usr/include/iconv.h ]; then \
 	    ${REINPLACE_CMD} -e 's!^[[:space:]]*const char .ip = (const char .) .bp!char *ip = *bp!' \
@@ -116,4 +122,4 @@ do-install:
 post-install:
 	${FIND} ${STAGEDIR}/${PREFIX}/dt -type f -exec ${SH} -c '${STRIP_CMD} {} > /dev/null 2>&1' \;
 
-.include <bsd.port.mk>
+.include <bsd.port.post.mk>

x11/cde/files/patch-programs_dtksh_ksh93_src_lib_libast_hash_hashalloc.c
@@ -1,20 +1,25 @@
---- programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c.orig	2021-12-13 19:03:46 UTC
-+++ programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c
-@@ -49,6 +49,7 @@ hashalloc(Hash_table_t* ref, ...)
+--- programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c.orig	2022-01-20 12:56:31.275926000 -0800
++++ programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c	2022-01-20 12:59:47.461533000 -0800
+@@ -49,6 +49,9 @@
  	va_list*		vp = va;
  	Hash_region_f		region = 0;
  	void*			handle;
++#ifdef POWERPC64LE
 +	va_listarg		tmpval;
++#endif
  
  	va_start(ap, ref);
  
-@@ -151,7 +152,8 @@ hashalloc(Hash_table_t* ref, ...)
+@@ -151,7 +154,12 @@
  				va_copy(*vp, ap);
  				vp++;
  			}
--			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++#ifdef POWERPC64LE
 +			tmpval = va_listval(va_arg(ap, va_listarg));
 +			va_copy(ap, tmpval);
++#else
+ 			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++#endif
  			break;
  		case 0:
  			if (vp > va)

x11/cde/files/patch-programs_dtksh_ksh93_src_lib_libast_string_tokscan.c
@@ -1,20 +1,25 @@
---- programs/dtksh/ksh93/src/lib/libast/string/tokscan.c.orig	2021-12-13 22:46:02 UTC
-+++ programs/dtksh/ksh93/src/lib/libast/string/tokscan.c
-@@ -188,6 +188,7 @@ tokscan(register char* s, char** nxt, const char* fmt,
+--- programs/dtksh/ksh93/src/lib/libast/string/tokscan.c.orig	2022-01-20 12:56:38.679529000 -0800
++++ programs/dtksh/ksh93/src/lib/libast/string/tokscan.c	2022-01-20 13:02:26.179022000 -0800
+@@ -188,6 +188,9 @@
  	char**		p_string;
  	char*		prv_f = 0;
  	va_list		prv_ap;
++#ifdef POWERPC64LE
 +	va_listarg	tmpval;
++#endif
  
  	va_start(ap, fmt);
  	if (!*s || *s == '\n')
-@@ -233,7 +234,8 @@ tokscan(register char* s, char** nxt, const char* fmt,
+@@ -233,7 +236,12 @@
  			prv_f = f;
  			f = va_arg(ap, char*);
  			va_copy(prv_ap, ap);
--			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++#ifdef POWERPC64LE
 +			tmpval = va_listval(va_arg(ap, va_listarg));
 +			va_copy(ap, tmpval);
++#else
+ 			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++#endif
  			continue;
  		case 'c':
  			p_char = va_arg(ap, char*);
47d8fb3af3f0e411edcb06a9c694813404c8fe39
Cy Schubert <cy@FreeBSD.org>
2022-01-20 13:20:59 -0800

    x11/cde: Fix armv7 build
    In attempting to fix powerpc64le, c66a851d13783558a8fa17ffcf64759a0c1b5bab
    broke armv7. Fix it.
    
    PR:		261359
    Reported by:	Robert Clausecker <fuz@fuz.su>
    Fixes:		c66a851d13783558a8fa17ffcf64759a0c1b5bab

--- x11/cde/files/patch-programs_dtksh_ksh93_src_lib_libast_hash_hashalloc.c
+++ x11/cde/files/patch-programs_dtksh_ksh93_src_lib_libast_hash_hashalloc.c
@@ -1,20 +1,25 @@
---- programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c.orig	2021-12-13 19:03:46 UTC
-+++ programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c
-@@ -49,6 +49,7 @@ hashalloc(Hash_table_t* ref, ...)
+--- programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c.orig	2022-01-20 12:56:31.275926000 -0800
++++ programs/dtksh/ksh93/src/lib/libast/hash/hashalloc.c	2022-01-20 12:59:47.461533000 -0800
+@@ -49,6 +49,9 @@
  	va_list*		vp = va;
  	Hash_region_f		region = 0;
  	void*			handle;
++#ifdef POWERPC64LE
 +	va_listarg		tmpval;
++#endif
  
  	va_start(ap, ref);
  
-@@ -151,7 +152,8 @@ hashalloc(Hash_table_t* ref, ...)
+@@ -151,7 +154,12 @@
  				va_copy(*vp, ap);
  				vp++;
  			}
--			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++#ifdef POWERPC64LE
 +			tmpval = va_listval(va_arg(ap, va_listarg));
 +			va_copy(ap, tmpval);
++#else
+ 			va_copy(ap, va_listval(va_arg(ap, va_listarg)));
++#endif
  			break;
  		case 0:
  			if (vp > va)
