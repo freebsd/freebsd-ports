ca63a16f5ced25bd5dc58383609ed8f6b5762e2a
Hans Petter Selasky <hselasky@FreeBSD.org>
2022-01-25 17:50:35 +0100

    multimedia/libv4l: Update v4l_compat, libv4l and v4l-utils to 1.23.0
    NOTE: This version tag currently only exists for FreeBSD and has not yet
          been made upstream and is based on the latest available v4l-utils.
    
    Approved by:	pi (implicit)

--- multimedia/libv4l/files/patch-utils_v4l2-ctl_v4l2-ctl-overlay.cpp
+++ multimedia/libv4l/files/patch-utils_v4l2-ctl_v4l2-ctl-overlay.cpp
@@ -1,12 +1,13 @@
---- utils/v4l2-ctl/v4l2-ctl-overlay.cpp.orig	2020-05-21 11:22:05 UTC
+diff --git utils/v4l2-ctl/v4l2-ctl-overlay.cpp utils/v4l2-ctl/v4l2-ctl-overlay.cpp
+index 5493222dd..1f0a6f47f 100644
+--- utils/v4l2-ctl/v4l2-ctl-overlay.cpp
 +++ utils/v4l2-ctl/v4l2-ctl-overlay.cpp
-@@ -14,11 +14,13 @@
- #include <dirent.h>
- #include <math.h>
- 
--#include <linux/fb.h>
+@@ -1,10 +1,11 @@
+ #include <array>
  #include <vector>
  
+-#include <linux/fb.h>
+-
  #include "v4l2-ctl.h"
  
 +#ifndef __FreeBSD__
@@ -15,7 +16,7 @@
  static unsigned int set_fbuf;
  static unsigned int set_overlay_fmt;
  static struct v4l2_format overlay_fmt;	/* set_format/get_format video overlay */
-@@ -546,3 +548,24 @@ void overlay_list(cv4l_fd &fd)
+@@ -527,3 +528,24 @@ void overlay_list(cv4l_fd &fd)
  	if (options[OptFindFb])
  		find_fb(fd.g_fd());
  }
