b120a15fb70ee9eea782409e6d3a4e789e258828
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-01-26 03:25:32 +0800

    net-p2p/py-transmission-rpc: Allow build with py-typing-extensions 4.0.1
    Reference:	https://github.com/Trim21/transmission-rpc/commit/af38c4faf685cda70ef6643c018a1408318ecbf8

---
+++ net-p2p/py-transmission-rpc/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2021-09-29 06:26:44 UTC
++++ setup.py
+@@ -8,7 +8,7 @@ package_data = \
+ {'': ['*']}
+ 
+ install_requires = \
+-['requests>=2.23.0,<3.0.0', 'typing_extensions>=3.7.4.2,<4.0.0.0']
++['requests>=2.23.0,<3.0.0', 'typing_extensions>=3.7.4.2,<4.0.2.0']
+ 
+ extras_require = \
+ {'docs': ['sphinx==4.2.0', 'sphinx-rtd-theme==1.0.0']}
