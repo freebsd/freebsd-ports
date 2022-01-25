97108f5a3fca1ae8de3556eebae86de77622b7e2
Frank Wall <fw@moov.de>
2022-01-25 01:27:34 +0000

    net/foreman-proxy: update 2.5.4 -> 3.0.1
    * update to version 3.0.1
    * bump Puppet dependency to Puppet 7
    * fix path in pkg-message
    * take maintainership
    
    Changes:	https://github.com/theforeman/smart-proxy/compare/2.5.4...3.0.1
    PR:		260019
    Submitted by:	Frank Wall <fw@moov.de>; junovitch (rebase on incremental update)

--- net/foreman-proxy/files/patch-lib_proxy_http__download.rb
+++ net/foreman-proxy/files/patch-lib_proxy_http__download.rb
@@ -1,8 +1,8 @@
---- lib/proxy/http_download.rb.orig	2018-12-06 13:30:25.000000000 +0100
-+++ lib/proxy/http_download.rb	2019-02-11 22:33:58.455890000 +0100
+--- lib/proxy/http_download.rb.orig     2021-10-26 14:48:17.000000000 +0200
++++ lib/proxy/http_download.rb  2021-11-24 12:24:12.924526000 +0100
 @@ -9,7 +9,7 @@
  
-     def initialize(src, dst, read_timeout = nil, connect_timeout = nil, dns_timeout = nil)
+     def initialize(src, dst, read_timeout = nil, connect_timeout = nil, dns_timeout = nil, verify_server_cert = false)
        @dst = dst
 -      wget = which("wget")
 +      wget = "%%LOCALBASE%%/bin/wget"
