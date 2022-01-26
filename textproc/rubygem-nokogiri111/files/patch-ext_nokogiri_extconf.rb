5f255f62ce0cc4845b6531fec4ef614003bb8928
Matthias Fechner <mfechner@FreeBSD.org>
2022-01-26 10:48:04 +0100

    textproc/nokogiri111: make 1.11.x available and use it if required

---
+++ textproc/rubygem-nokogiri111/files/patch-ext_nokogiri_extconf.rb
@@ -0,0 +1,46 @@
+--- ext/nokogiri/extconf.rb.orig	2022-01-26 09:35:58 UTC
++++ ext/nokogiri/extconf.rb
+@@ -891,6 +891,43 @@ unless config_system_libraries?
+   end
+ end
+ 
++$INSTALLFILES = [
++  ['html_document.h', '$(archdir)'],
++  ['html_element_description.h', '$(archdir)'],
++  ['html_entity_lookup.h', '$(archdir)'],
++  ['html_sax_parser_context.h', '$(archdir)'],
++  ['html_sax_push_parser.h', '$(archdir)'],
++  ['nokogiri.h', '$(archdir)'],
++  ['xml_attr.h', '$(archdir)'],
++  ['xml_attribute_decl.h', '$(archdir)'],
++  ['xml_cdata.h', '$(archdir)'],
++  ['xml_comment.h', '$(archdir)'],
++  ['xml_document.h', '$(archdir)'],
++  ['xml_document_fragment.h', '$(archdir)'],
++  ['xml_dtd.h', '$(archdir)'],
++  ['xml_element_content.h', '$(archdir)'],
++  ['xml_element_decl.h', '$(archdir)'],
++  ['xml_encoding_handler.h', '$(archdir)'],
++  ['xml_entity_decl.h', '$(archdir)'],
++  ['xml_entity_reference.h', '$(archdir)'],
++  ['xml_io.h', '$(archdir)'],
++  ['xml_libxml2_hacks.h', '$(archdir)'],
++  ['xml_namespace.h', '$(archdir)'],
++  ['xml_node.h', '$(archdir)'],
++  ['xml_node_set.h', '$(archdir)'],
++  ['xml_processing_instruction.h', '$(archdir)'],
++  ['xml_reader.h', '$(archdir)'],
++  ['xml_relax_ng.h', '$(archdir)'],
++  ['xml_sax_parser.h', '$(archdir)'],
++  ['xml_sax_parser_context.h', '$(archdir)'],
++  ['xml_sax_push_parser.h', '$(archdir)'],
++  ['xml_schema.h', '$(archdir)'],
++  ['xml_syntax_error.h', '$(archdir)'],
++  ['xml_text.h', '$(archdir)'],
++  ['xml_xpath_context.h', '$(archdir)'],
++  ['xslt_stylesheet.h', '$(archdir)'],
++]
++
+ create_makefile('nokogiri/nokogiri')
+ 
+ if config_clean?
