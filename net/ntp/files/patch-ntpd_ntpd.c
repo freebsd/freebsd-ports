646499bcb3f4f8e8ba96b767d102d674091c674c
Cy Schubert <cy@FreeBSD.org>
2022-01-26 10:18:22 -0800

    net/ntp: Fix build on older FreeBSD
    Fix stackgap build on older FreeBSD.
    
    PR:		261491
    Reported by:	tomasz.sowinski@nucleus.malbork.pl
    MFH:		2022Q1

--- net/ntp/files/patch-ntpd_ntpd.c
+++ net/ntp/files/patch-ntpd_ntpd.c
@@ -1,5 +1,5 @@
 --- ntpd/ntpd.c.orig	2020-06-23 02:17:48.000000000 -0700
-+++ ntpd/ntpd.c	2022-01-19 20:36:53.477753000 -0800
++++ ntpd/ntpd.c	2022-01-26 10:14:00.828563000 -0800
 @@ -145,7 +145,9 @@
  # include <seccomp.h>
  #endif /* LIBSECCOMP and KERN_SECCOMP */
@@ -11,12 +11,14 @@
  #include <sys/procctl.h>
  #ifndef PROC_STACKGAP_CTL
  /*
-@@ -438,7 +440,7 @@
+@@ -438,7 +440,9 @@
  	char *argv[]
  	)
  {
 -#   ifdef __FreeBSD__
-+#   if defined(__FreeBSD_version) && __FreeBSD_version < 1400037
++#   if defined(__FreeBSD_version) && \
++       ((__FreeBSD_version > 1400000 && __FreeBSD_version < 1400038) || \
++       __FreeBSD_version < 1300524)
  	{
  		/*
  		 * We Must disable ASLR stack gap on FreeBSD to avoid a
