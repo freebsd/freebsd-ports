d763116bc08383f5b56ec687b64c053ae4163f71
James French <james@french.id.au>
2022-01-30 16:10:19 +0100

    news/sabnzbdplus: Update to 3.5.0
    PR:		261548

--- news/sabnzbdplus/files/patch-sabnzbd_newsunpack.py
+++ news/sabnzbdplus/files/patch-sabnzbd_newsunpack.py
@@ -1,11 +1,11 @@
---- sabnzbd/newsunpack.py.orig	2021-10-15 06:30:37 UTC
+--- sabnzbd/newsunpack.py.orig	2022-01-28 10:50:34 UTC
 +++ sabnzbd/newsunpack.py
-@@ -127,6 +127,8 @@ def find_programs(curdir):
-         if not sabnzbd.newsunpack.SEVEN_COMMAND:
-             sabnzbd.newsunpack.SEVEN_COMMAND = find_on_path("7za")
-         if not sabnzbd.newsunpack.SEVEN_COMMAND:
-+            sabnzbd.newsunpack.SEVEN_COMMAND = find_on_path("7zz")
-+        if not sabnzbd.newsunpack.SEVEN_COMMAND:
-             sabnzbd.newsunpack.SEVEN_COMMAND = find_on_path("7z")
+@@ -136,6 +136,8 @@ def find_programs(curdir: str):
+         if not sabnzbd.newsunpack.SEVENZIP_COMMAND:
+             sabnzbd.newsunpack.SEVENZIP_COMMAND = find_on_path("7za")  # 7za = 7z stand-alone executable
+         if not sabnzbd.newsunpack.SEVENZIP_COMMAND:
++            sabnzbd.newsunpack.SEVENZIP_COMMAND = find_on_path("7zz")
++        if not sabnzbd.newsunpack.SEVENZIP_COMMAND:
+             sabnzbd.newsunpack.SEVENZIP_COMMAND = find_on_path("7z")
  
      if not (sabnzbd.WIN32 or sabnzbd.DARWIN):
