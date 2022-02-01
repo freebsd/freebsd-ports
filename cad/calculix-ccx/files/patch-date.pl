5cf3fd43c61825465fad94736ea5e638926ab6c2
Pedro F. Giffuni <pfg@FreeBSD.org>
2022-02-01 17:36:48 -0500

    cad/calculix{-ccx}: Update to version 2.19
    Approved by:	thierry (mentor, implicit)

--- cad/calculix-ccx/files/patch-date.pl
+++ cad/calculix-ccx/files/patch-date.pl
@@ -1,4 +1,4 @@
---- date.pl.orig	2021-09-15 19:39:24 UTC
+--- date.pl.orig	2021-12-17 12:13:23 UTC
 +++ date.pl
 @@ -1,6 +1,7 @@
  #!/usr/bin/env perl
@@ -7,5 +7,5 @@
 +use POSIX qw(strftime);
 +chomp(my $date = strftime("%Y-%m-%d", gmtime($ENV{SOURCE_DATE_EPOCH} || time)));
  
- # inserting the date into ccx_2.18.c
+ # inserting the date into ccx_2.19.c
  
