37991a7381a067c2e744e7debafcf18aef664e35
Jose G. Juanino <jjuanino@gmail.com>
2022-02-01 11:54:48 +0100

    deskutils/recoll: update to 1.31.4 and set X11MON as default option
    ChangeLog: https://www.lesbonscomptes.com/recoll/pages/release-1.31.html
    
    This should fix PR260772 too.
    
    PR:	260162
    Reported by:	jjuanino@gmail.com

--- deskutils/recoll/files/patch-utils_fstreewalk.cpp
+++ deskutils/recoll/files/patch-utils_fstreewalk.cpp
@@ -1,4 +1,4 @@
---- utils/fstreewalk.cpp.orig	2020-08-30 18:31:25 UTC
+--- utils/fstreewalk.cpp.orig	2021-08-03 07:29:21 UTC
 +++ utils/fstreewalk.cpp
 @@ -17,6 +17,7 @@
  
@@ -6,5 +6,5 @@
  
 +#include <sys/types.h>
  #include <stdio.h>
- #include <dirent.h>
+ 
  #include <errno.h>
