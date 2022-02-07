3746559b47e10ec53bed09b2f9ac782d153bff14
Carlo Strub <cs@FreeBSD.org>
2022-02-07 20:51:52 +0100

    devel/pijul: Update to 1.0.0.b

--- devel/pijul/files/patch-cargo-crates_pager-0.16.0_src_utils.rs
+++ devel/pijul/files/patch-cargo-crates_pager-0.16.0_src_utils.rs
@@ -1,4 +1,4 @@
---- cargo-crates/pager-0.16.0/src/utils.rs.orig	2021-07-20 20:40:27 UTC
+--- cargo-crates/pager-0.16.0/src/utils.rs.orig	2022-02-06 16:47:07 UTC
 +++ cargo-crates/pager-0.16.0/src/utils.rs
 @@ -66,7 +66,7 @@ pub(crate) fn execvpe(cmd: &OsString, envs: &[OsString
      };
