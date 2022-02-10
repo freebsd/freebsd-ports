ec06613abe3ed1459d274fb28bd4776a6f874917
Mathieu Arnold <mat@FreeBSD.org>
2022-02-10 14:32:12 +0100

    dns/bind916: update to 9.16.25
    Add back the Administrator Reference Manual in the DOCS option.
    
    Changes:	https://downloads.isc.org/isc/bind9/9.16.25/doc/arm/html/notes.html#notes-for-bind-9-16-25
    (cherry picked from commit e84b73e5ff1fdb67c9479b0182aa2035e16aacad)

--- dns/bind916/files/patch-bin_named_include_named_globals.h
+++ dns/bind916/files/patch-bin_named_include_named_globals.h
@@ -1,8 +1,8 @@
 We reference the pid file as being run/named/pid everywere else.
 
---- bin/named/include/named/globals.h.orig	2020-02-12 20:03:44 UTC
+--- bin/named/include/named/globals.h.orig	2022-01-12 13:50:10 UTC
 +++ bin/named/include/named/globals.h
-@@ -127,7 +127,7 @@ EXTERN bool named_g_forcelock		   INIT(false);
+@@ -129,7 +129,7 @@ EXTERN bool named_g_forcelock		   INIT(false);
  
  #if NAMED_RUN_PID_DIR
  EXTERN const char *named_g_defaultpidfile INIT(NAMED_LOCALSTATEDIR "/run/named/"
