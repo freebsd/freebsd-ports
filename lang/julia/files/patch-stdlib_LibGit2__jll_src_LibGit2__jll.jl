eefeae155db9f350f9185915d31844c2965cf9fa
Eric Reardon <rizor.reardon@yahoo.com>
2022-02-09 23:25:26 -0500

    lang/julia: Update to 1.7.2
    - Fix a typo in the patch
    
    Changelog:	https://github.com/JuliaLang/julia/releases/tag/v1.7.2
    
    PR:		261795

--- lang/julia/files/patch-stdlib_LibGit2__jll_src_LibGit2__jll.jl
+++ lang/julia/files/patch-stdlib_LibGit2__jll_src_LibGit2__jll.jl
@@ -1,11 +1,11 @@
---- stdlib/LibGit2_jll/src/LibGit2_jll.jl.orig	2021-12-15 04:07:07 UTC
+--- stdlib/LibGit2_jll/src/LibGit2_jll.jl.orig	2021-11-29 02:21:22 UTC
 +++ stdlib/LibGit2_jll/src/LibGit2_jll.jl
 @@ -23,7 +23,7 @@ if Sys.iswindows()
  elseif Sys.isapple()
      const libgit2 = "@rpath/libgit2.1.1.dylib"
  else
 -    const libgit2 = "libgit2.so.1.1"
-+    const libgit2 = "libgit2.so.1"
++    const libgit2 = "libgit2.so"
  end
  
  function __init__()
