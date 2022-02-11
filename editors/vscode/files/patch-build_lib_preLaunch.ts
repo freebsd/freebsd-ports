b43816f0c8df9e2010fba85e56d0b0b75ad3128f
Hiroki Tagato <tagattie@FreeBSD.org>
2022-02-11 16:47:55 +0900

    editors/vscode: update to 1.64.2
    Changelog: https://code.visualstudio.com/updates/v1_64

--- editors/vscode/files/patch-build_lib_preLaunch.ts
+++ editors/vscode/files/patch-build_lib_preLaunch.ts
@@ -1,5 +1,18 @@
---- build/lib/preLaunch.ts.orig	2022-02-05 06:44:51 UTC
+--- build/lib/preLaunch.ts.orig	2022-02-09 21:24:27 UTC
 +++ build/lib/preLaunch.ts
+@@ -37,9 +37,9 @@ async function ensureNodeModules() {
+ 	}
+ }
+ 
+-async function getElectron() {
+-	await runProcess(yarn, ['electron']);
+-}
++// async function getElectron() {
++// 	await runProcess(yarn, ['electron']);
++// }
+ 
+ async function ensureCompiled() {
+ 	if (!(await exists('out'))) {
 @@ -49,7 +49,7 @@ async function ensureCompiled() {
  
  async function main() {
