dbced39a6b93061cddb846eed724fefe23c50090
Vanilla I. Shu <vanilla@FreeBSD.org>
2022-02-13 22:14:07 +0800

    www/janus: update to 0.11.8, and add a workaround for dlerror.
    PR:		261750
    Reported by:	ruben@verweg.com

--- www/janus/files/patch-janus.c
+++ www/janus/files/patch-janus.c
@@ -1,10 +1,18 @@
---- janus.c.orig	2021-10-18 15:46:53 UTC
+--- janus.c.orig	2022-02-11 08:56:29 UTC
 +++ janus.c
-@@ -4359,6 +4359,7 @@ gint main(int argc, char *argv[])
- 			if (!event) {
- 				JANUS_LOG(LOG_ERR, "\tCouldn't load logger plugin '%s': %s\n", eventent->d_name, dlerror());
+@@ -4423,6 +4423,7 @@ gint main(int argc, char *argv[])
+ 				if (exit_on_dl_error)
+ 					exit(1);
  			} else {
 +				dlerror();
  				create_l *create = (create_l*) dlsym(event, "create");
  				const char *dlsym_error = dlerror();
  				if (dlsym_error) {
+@@ -5447,6 +5448,7 @@ gint main(int argc, char *argv[])
+ 			if (exit_on_dl_error)
+ 				exit(1);
+ 		} else {
++			dlerror();
+ 			create_p *create = (create_p*) dlsym(plugin, "create");
+ 			const char *dlsym_error = dlerror();
+ 			if (dlsym_error) {
