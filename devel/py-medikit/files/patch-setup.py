5575e7076572b78a2fbff8143d032b41c269e5e8
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-02-14 07:32:09 +0100

    devel/py-medikit: Update to 0.8.0
    Changelog:
    
    https://github.com/python-medikit/medikit/blob/0.8.0/docs/changelog.rst
    
    PR:		261470
    Approved by:	sunpoet (maintainer)

--- devel/py-medikit/files/patch-setup.py
+++ devel/py-medikit/files/patch-setup.py
@@ -1,13 +1,16 @@
---- setup.py.orig	2020-04-04 17:34:58 UTC
+--- setup.py.orig	2020-11-25 07:42:37 UTC
 +++ setup.py
-@@ -54,8 +54,8 @@ setup(
-     packages=find_packages(exclude=['ez_setup', 'example', 'test']),
+@@ -56,11 +56,11 @@ setup(
      include_package_data=True,
      install_requires=[
--        'git-semver ~= 0.3.2', 'jinja2 ~= 2.10', 'mondrian ~= 0.8',
--        'packaging ~= 20.0', 'pip-tools ~= 4.5.0', 'semantic_version < 2.7',
-+        'git-semver ~= 0.3.2', 'jinja2 >= 2.10', 'mondrian ~= 0.8',
-+        'packaging >= 20.0', 'pip-tools ~= 4.5.0', 'semantic_version ~= 2.8',
-         'stevedore ~= 1.28', 'whistle ~= 1.0', 'yapf ~= 0.20'
-     ],
-     extras_require={
+         "git-semver ~= 0.3.2",
+-        "jinja2 ~= 2.10",
++        "jinja2 >= 2.10",
+         "mondrian ~= 0.8",
+         "packaging ~= 20.0",
+         "pip-tools ~= 4.5.0",
+-        "semantic_version < 2.7",
++        "semantic_version ~= 2.8",
+         "stevedore ~= 3.0",
+         "whistle ~= 1.0",
+         "yapf ~= 0.20",
