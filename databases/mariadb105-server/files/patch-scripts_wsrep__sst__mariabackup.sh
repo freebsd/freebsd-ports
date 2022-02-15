c5990d4968fa9eda882901fd921b1b1be5f8545a
Bernard Spil <brnrd@FreeBSD.org>
2022-02-15 11:47:37 +0000

    databases/mariadb105-server: Security update to 10.5.15
    security:	ff5606f7-8a45-11ec-8be6-d4c9ef517024
    MFH:		2022Q1
    (cherry picked from commit 4e7a17eee56a05ef998bf35a17453da5b6c96c8b)

--- databases/mariadb105-server/files/patch-scripts_wsrep__sst__mariabackup.sh
+++ databases/mariadb105-server/files/patch-scripts_wsrep__sst__mariabackup.sh
@@ -1,9 +1,9 @@
---- scripts/wsrep_sst_mariabackup.sh.orig	2021-06-24 09:33:04 UTC
+--- scripts/wsrep_sst_mariabackup.sh.orig	2022-02-14 08:10:09 UTC
 +++ scripts/wsrep_sst_mariabackup.sh
-@@ -696,7 +696,7 @@ recv_joiner()
+@@ -715,7 +715,7 @@ recv_joiner()
      local ltcmd="$tcmd"
      if [ $tmt -gt 0 ]; then
-         if [ -n "$(command -v timeout)" ]; then
+         if [ -n "$(commandex timeout)" ]; then
 -            if timeout --help | grep -qw -- '-k'; then
 +            if timeout --help 2>&1 | grep -qw -- '-k'; then
                  ltcmd="timeout -k $(( tmt+10 )) $tmt $tcmd"
