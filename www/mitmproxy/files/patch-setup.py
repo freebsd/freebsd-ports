60448a8aefe443645115a7a2b4a8f2845d660b62
Hung-Yi Chen <gaod@hychen.org>
2022-02-17 00:33:08 +0800

    www/mitmproxy: Fix build after net/py-h11 update
    PR:		261991

--- www/mitmproxy/files/patch-setup.py
+++ www/mitmproxy/files/patch-setup.py
@@ -1,6 +1,6 @@
 --- setup.py.orig	2021-09-28 16:43:29 UTC
 +++ setup.py
-@@ -67,7 +67,7 @@ setup(
+@@ -67,14 +67,14 @@ setup(
      # https://packaging.python.org/en/latest/requirements/#install-requires
      # It is not considered best practice to use install_requires to pin dependencies to specific versions.
      install_requires=[
@@ -9,6 +9,14 @@
          "blinker>=1.4, <1.5",
          "Brotli>=1.0,<1.1",
          "certifi>=2019.9.11",  # no semver here - this should always be on the last release!
+         "click>=7.0,<8.1",
+         "cryptography>=3.3,<3.5",
+         "flask>=1.1.1,<2.1",
+-        "h11>=0.11,<0.13",
++        "h11>=0.11,<0.14",
+         "h2>=4.0,<5",
+         "hyperframe>=6.0,<7",
+         "kaitaistruct>=0.7,<0.10",
 @@ -83,7 +83,7 @@ setup(
          "passlib>=1.6.5, <1.8",
          "protobuf>=3.14,<3.19",
