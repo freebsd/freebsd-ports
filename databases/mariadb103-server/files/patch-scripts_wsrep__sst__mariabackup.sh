b7f811a8ea8132998af511b9a105a25137dcb20d
Bernard Spil <brnrd@FreeBSD.org>
2022-02-17 10:09:09 +0000

    databases/mariadb103-server: Security update to 10.3.34
    PR:		261969
    Security:	ff5606f7-8a45-11ec-8be6-d4c9ef517024
    MFH:		2022Q1

--- databases/mariadb103-server/files/patch-scripts_wsrep__sst__mariabackup.sh
+++ databases/mariadb103-server/files/patch-scripts_wsrep__sst__mariabackup.sh
@@ -1,9 +1,9 @@
---- scripts/wsrep_sst_mariabackup.sh.orig	2021-06-09 14:23:11.000000000 +0200
+--- scripts/wsrep_sst_mariabackup.sh.orig	2022-02-16 20:18:40 UTC
 +++ scripts/wsrep_sst_mariabackup.sh
-@@ -696,7 +696,7 @@
+@@ -715,7 +715,7 @@ recv_joiner()
      local ltcmd="$tcmd"
      if [ $tmt -gt 0 ]; then
-         if [ -n "$(command -v timeout)" ]; then
+         if [ -n "$(commandex timeout)" ]; then
 -            if timeout --help | grep -qw -- '-k'; then
 +            if timeout --help 2>&1 | grep -qw -- '-k'; then
                  ltcmd="timeout -k $(( tmt+10 )) $tmt $tcmd"
