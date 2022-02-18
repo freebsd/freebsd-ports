6ad6ff76763ac0c33ba507b51faf52a70556dcb2
Piotr Kubaj <pkubaj@FreeBSD.org>
2022-02-18 15:26:07 +0000

    converters/wkhtmltopdf: fix build on riscv64 the same way it's done for aarch64

--- converters/wkhtmltopdf/files/patch-src__3rdparty__webkit__Source__JavaScriptCore__wtf__Platform.h
+++ converters/wkhtmltopdf/files/patch-src__3rdparty__webkit__Source__JavaScriptCore__wtf__Platform.h
@@ -1,12 +1,13 @@
 --- src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h.orig	2020-11-08 17:05:56.503694000 +0100
 +++ src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h	2020-11-08 17:06:42.105176000 +0100
-@@ -1003,7 +1003,8 @@
+@@ -1003,7 +1003,9 @@
      || CPU(SPARC64) \
      || CPU(S390X) \
      || CPU(PPC64) \
 -    || CPU(MIPS64)
 +    || CPU(MIPS64) \
-+    || defined(__aarch64__)
++    || defined(__aarch64__) \
++    || defined(__riscv)
  #define WTF_USE_JSVALUE64 1
  #else
  #define WTF_USE_JSVALUE32_64 1
