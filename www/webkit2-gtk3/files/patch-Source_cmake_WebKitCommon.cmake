8ac6e42420df96f3f4710ba713d3e3a208fdaf00
Piotr Kubaj <pkubaj@FreeBSD.org>
2022-02-18 19:47:38 +0000

    www/webkit2-gtk3: correctly detect powerpc64le

--- www/webkit2-gtk3/files/patch-Source_cmake_WebKitCommon.cmake
+++ www/webkit2-gtk3/files/patch-Source_cmake_WebKitCommon.cmake
@@ -1,6 +1,6 @@
---- Source/cmake/WebKitCommon.cmake.orig	2021-09-22 23:05:58 UTC
+--- Source/cmake/WebKitCommon.cmake.orig	2021-09-17 09:07:45 UTC
 +++ Source/cmake/WebKitCommon.cmake
-@@ -106,9 +106,9 @@ if (NOT HAS_RUN_WEBKIT_COMMON)
+@@ -106,11 +106,11 @@ if (NOT HAS_RUN_WEBKIT_COMMON)
          endif ()
      elseif (LOWERCASE_CMAKE_SYSTEM_PROCESSOR MATCHES "(i[3-6]86|x86)")
          set(WTF_CPU_X86 1)
@@ -10,5 +10,8 @@
 -    elseif (LOWERCASE_CMAKE_SYSTEM_PROCESSOR MATCHES "ppc64")
 +    elseif (LOWERCASE_CMAKE_SYSTEM_PROCESSOR MATCHES "(ppc64|powerpc64)")
          set(WTF_CPU_PPC64 1)
-     elseif (LOWERCASE_CMAKE_SYSTEM_PROCESSOR MATCHES "ppc64le")
+-    elseif (LOWERCASE_CMAKE_SYSTEM_PROCESSOR MATCHES "ppc64le")
++    elseif (LOWERCASE_CMAKE_SYSTEM_PROCESSOR MATCHES "(ppc64le|powerpc64le)")
          set(WTF_CPU_PPC64LE 1)
+     elseif (LOWERCASE_CMAKE_SYSTEM_PROCESSOR MATCHES "^riscv64")
+         set(WTF_CPU_RISCV64 1)
