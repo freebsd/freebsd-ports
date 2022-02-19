246082e073834b16b0721adc1234a2c340cd129d
Yasuhiro Kimura <yasu@FreeBSD.org>
2022-02-19 11:24:06 +0900

    databases/emacsql: Update to latest snapshot
    This fixes runtime error of devel/{forge,forge-devel}
    
    While here, pet portlint and portclippy
    
    Changes:	https://github.com/skeeto/emacsql/compare/3.0.0...3747260
    MFH:		2022Q1
    (cherry picked from commit 4aa107cc4566b0da08cfc6deac8fb50c0d59d106)

--- databases/emacsql/files/patch-emacsql-sqlite.el
+++ databases/emacsql/files/patch-emacsql-sqlite.el
@@ -1,19 +1,24 @@
---- emacsql-sqlite.el.orig	2018-10-31 17:50:07 UTC
+--- emacsql-sqlite.el.orig	2022-02-18 15:43:25 UTC
 +++ emacsql-sqlite.el
-@@ -27,11 +27,7 @@
-   (file-name-directory (or load-file-name buffer-file-name))
-   "Directory where EmacSQL is installed.")
+@@ -33,16 +33,7 @@
+     "sqlite/emacsql-sqlite")
+   "Relative path to emacsql executable.")
  
 -(defvar emacsql-sqlite-executable
--  (expand-file-name (if (memq system-type '(windows-nt cygwin ms-dos))
--                        "sqlite/emacsql-sqlite.exe"
--                      "sqlite/emacsql-sqlite")
--                    emacsql-sqlite-data-root)
+-  (expand-file-name emacsql-sqlite-executable-path
+-                    (if (or (file-writable-p emacsql-sqlite-data-root)
+-                            (file-exists-p (expand-file-name
+-                                            emacsql-sqlite-executable-path
+-                                            emacsql-sqlite-data-root)))
+-                        emacsql-sqlite-data-root
+-                      (expand-file-name
+-                       (concat "emacsql/" emacsql-version)
+-                       user-emacs-directory)))
 +(defvar emacsql-sqlite-executable "%%PREFIX%%/bin/emacsql-sqlite"
    "Path to the EmacSQL backend (this is not the sqlite3 shell).")
  
  (defvar emacsql-sqlite-reserved
-@@ -82,6 +78,7 @@ http://www.sqlite.org/lang_keywords.html")
+@@ -100,6 +91,7 @@ used.")
      (emacsql-wait connection)
      (emacsql connection [:pragma (= busy-timeout $s1)]
               (/ (* emacsql-global-timeout 1000) 2))
@@ -21,18 +26,19 @@
      (emacsql-register connection)))
  
  (cl-defun emacsql-sqlite (file &key debug)
-@@ -143,11 +140,11 @@ If called with non-nil ASYNC the return value is meani
-   (let* ((cc (executable-find "cc"))
+@@ -163,12 +155,12 @@ If called with non-nil ASYNC the return value is meani
+                       if path return it))
           (src (expand-file-name "sqlite" emacsql-sqlite-data-root))
           (files (mapcar (lambda (f) (expand-file-name f src))
 -                        '("sqlite3.c" "emacsql.c")))
 +                        '("emacsql.c")))
           (cflags (list (format "-I%s" src) (format "-O%d" (or o-level 2))))
-          (ldlibs (if (memq system-type '(windows-nt berkeley-unix))
--                     (list "-lm")
--                   (list "-lm" "-ldl")))
-+                     (list "-lm" "-lsqlite3")
-+                   (list "-lm" "-ldl" "-lsqlite3")))
+          (ldlibs (cl-case system-type
+                    (windows-nt (list))
+-                   (berkeley-unix (list "-lm"))
+-                   (otherwise (list "-lm" "-ldl"))))
++                   (berkeley-unix (list "-lm" "-lsqlite3"))
++                   (otherwise (list "-lm" "-ldl" "-lsqlite3"))))
           (options (emacsql-sqlite-compile-switches))
           (output (list "-o" emacsql-sqlite-executable))
           (arguments (nconc cflags options files ldlibs output)))
