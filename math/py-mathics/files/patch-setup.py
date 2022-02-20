ae2dc35e151567281d575c47fea8359d78d612b5
Thierry Thomas <thierry@FreeBSD.org>
2022-02-20 14:37:29 +0100

    math/py-mathics: upgrade to 4.0.0

---
+++ math/py-mathics/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2022-02-20 13:30:03 UTC
++++ setup.py
+@@ -105,7 +105,7 @@ else:
+ # General Requirements
+ INSTALL_REQUIRES += [
+     "Mathics_Scanner>=1.2.1,<1.3.0",
+-    "sympy>=1.8, <= 1.9dev",
++    "sympy>=1.8, <= 1.9",
+     "mpmath>=1.2.0",
+     "numpy",
+     "palettable",
