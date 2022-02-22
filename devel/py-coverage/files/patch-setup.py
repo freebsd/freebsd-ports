26c7cd14084c9cbd570963f9b9ebe3e2e6ec7f9e
Dima Panov <fluffy@FreeBSD.org>
2022-02-22 12:46:58 +0300

    devel/py-coverage: update to 6.3.2 release
    Changelog:	https://coverage.readthedocs.io/en/latest/changes.html#version-6-3-2-2022-02-20
    Approved by:	maintainer (lwhsu)
    Sponsored by:	Netzkommune GmbH

--- devel/py-coverage/files/patch-setup.py
+++ devel/py-coverage/files/patch-setup.py
@@ -1,15 +1,15 @@
 Allow USE_PYTHON=concurrent to handle script renaming/linking for multiple
 Python versions.
 
---- setup.py.orig	2018-01-29 11:46:04 UTC
+--- setup.py.orig	2022-02-20 22:41:35 UTC
 +++ setup.py
-@@ -86,12 +86,9 @@ setup_args = dict(
+@@ -97,12 +97,9 @@ setup_args = dict(
      },
  
      entry_points={
--        # Install a script as "coverage", and as "coverage[23]", and as
--        # "coverage-2.7" (or whatever).
-+        # Install a script as "coverage"
+-        # Install a script as "coverage", and as "coverage3", and as
+-        # "coverage-3.7" (or whatever).
++        # Install a script as "coverage".
          'console_scripts': [
              'coverage = coverage.cmdline:main',
 -            'coverage%d = coverage.cmdline:main' % sys.version_info[:1],
