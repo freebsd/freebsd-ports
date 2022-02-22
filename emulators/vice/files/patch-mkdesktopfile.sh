32fc1cac0c960a1aba928de0834b03b576b38acf
Dirk Meyer <dinoex@FreeBSD.org>
2022-02-22 16:06:38 +0100

    emulators/vice: update to 3.6.1

--- emulators/vice/files/patch-mkdesktopfile.sh
+++
@@ -1,8 +0,0 @@
---- src/arch/gtk3/data/unix/mkdesktopfile.sh.orig	2020-12-23 13:04:50 UTC
-+++ src/arch/gtk3/data/unix/mkdesktopfile.sh
-@@ -1,4 +1,4 @@
--#!/bin/bash
-+#!/bin/sh
- #
- # Script to generate .desktop files for the emu binaries on Linux
- #
