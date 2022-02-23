73fbbd6e9f7ae3369ca9510f0f9ddd90d87435bf
Robert Nagy <robert@openbsd.org>
2022-02-23 22:57:09 +0100

    www/chromium: fix GPU acceleration on NVIDIA
    PR:		262156
    Reported by:	o.hushchenkov@gmail.com
    Fixes:		a23dfd21 "www/chromium: update to 98.0.4758.102"

--- www/chromium/files/patch-third__party_angle_src_gpu__info__util_SystemInfo__internal.h
+++ www/chromium/files/patch-third__party_angle_src_gpu__info__util_SystemInfo__internal.h
@@ -1,11 +1,14 @@
---- third_party/angle/src/gpu_info_util/SystemInfo_internal.h.orig	2022-02-07 13:39:41 UTC
+--- third_party/angle/src/gpu_info_util/SystemInfo_internal.h.orig	2022-02-23 19:25:59 UTC
 +++ third_party/angle/src/gpu_info_util/SystemInfo_internal.h
-@@ -14,6 +14,10 @@
+@@ -14,6 +14,13 @@
  namespace angle
  {
  
 +#if defined(__OpenBSD__) || defined(__FreeBSD__)
 +bool CollectMesaCardInfo(std::vector<GPUDeviceInfo> *devices);                
++#if defined(__FreeBSD__)
++bool GetPCIDevicesFreeBSD(std::vector<GPUDeviceInfo> *devices);
++#endif
 +#endif
 +
  // Defined in SystemInfo_libpci when GPU_INFO_USE_LIBPCI is defined.
