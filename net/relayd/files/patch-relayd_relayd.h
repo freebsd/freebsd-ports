b4d94c7c3d864775002f92322cc978bf522d27b2
Steve Wills <swills@FreeBSD.org>
2022-02-25 14:19:12 +0100

    net/relayd: fix build
    While here, pet linters.
    
    PR:	262004
    Reported by:	swills@FreeBSD.org
    Approved by:	koue@chaosophia.net (maintainer)
    MFH:	2022Q1	build fix

--- net/relayd/files/patch-relayd_relayd.h
+++ net/relayd/files/patch-relayd_relayd.h
@@ -49,3 +49,14 @@
  
  /* Attach the control socket to the following process */
  #define PROC_CONTROL	PROC_PFE
+@@ -1242,10 +1252,6 @@ int	 ssl_load_pkey(const void *, size_t, char *, off_t
+ 	    X509 **, EVP_PKEY **);
+ int	 ssl_ctx_fake_private_key(SSL_CTX *, const void *, size_t,
+ 	    char *, off_t, X509 **, EVP_PKEY **);
+-
+-/* ssl_privsep.c */
+-int	 ssl_ctx_use_certificate_chain(SSL_CTX *, char *, off_t);
+-int	 ssl_ctx_load_verify_memory(SSL_CTX *, char *, off_t);
+ 
+ /* ca.c */
+ pid_t	 ca(struct privsep *, struct privsep_proc *);
