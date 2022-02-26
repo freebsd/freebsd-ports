6cacfe141ff16b041da0f4112e5e03bd726ca122
Vinícius Zavam <egypcio@FreeBSD.org>
2022-02-26 13:34:26 +0000

    emulators/hatari: update 2.2.1 to 2.3.1
      while here: pet portlint.
    
      * https://git.tuxfamily.org/hatari/hatari.git/tree/doc/release-notes.txt?id=v2.3.1
    
      Main changes since Hatari 2.3.0:
    
        Hatari could crash when switching to Falcon mode and no IDE drive was used
        Fix unneeded extra prefetch for movem in 68020/30 CPU
        Fix master clock use for crossbar/DSP Handshake mode
        Fix hostport PORTB interrupt handling for DSP
        FPU setting did not work in macOS version for french language
        Fix Hatari window disappearing in the python UI when changing resolution
    
      Major highlights since Hatari 2.2.1:
    
        Emulate more hardware register (DIP switches, DD/HD mode, more RTC, ...)
        Update CPU core to match latest WinUAE 4.4.0 beta
        Add a better filter for downsampling the internal 250 kHz signal,
          ... should give better results when the YM2149 outputs high frequency sounds
        Improved video timing emulation
        Add support for the TT's 2nd MFP (not all signals are emulated yet)
        Large rewrite of the blitter's core, improving accuracy
        Rewrote / improved parts of the GEMDOS HD emulation
        Improved zooming function of the window
    
     Fixed demos:
    
        Pacemaker STE demo (end part), when it's run from GEMDOS HD
        Fullscreen part in Hard As Ice STE demo by I.C.E. (screen with no vertical DE signal)
        Fullast Vinner by Troed/Sync (improved vblank position, partial fix for now)
        Electric Night Falcon demo by Dune (MFP DMA sound interrupt on timer A using AER)
        Oompa by No Extra (after greetings part)
        E.K.O System (music should now play in the racing scene)
    
     Fixed programs:
    
        MS Write (crash), when it's run from GEMDOS HD
        Akaisex program reading AKAI S1000 HD floppies converted to STX
        EmuTos drawing vertical lines with blitter and leaving some trails
          ... (blitter in non HOG mode using TAS instruction to restart)
        Cecile v2.22, now also works if no IDE drive has been configured
        Trans D-Bug Express by PHF (lock when running in megaSTE mode at 16 MHz)

--- emulators/hatari/files/patch-tools_atari-hd-image.sh
+++ emulators/hatari/files/patch-tools_atari-hd-image.sh
@@ -1,24 +1,24 @@
---- tools/atari-hd-image.sh.orig	2018-02-20 07:08:43 UTC
+--- tools/atari-hd-image.sh.orig	2022-02-26 12:11:11 UTC
 +++ tools/atari-hd-image.sh
 @@ -33,8 +33,8 @@ PATH=/sbin:$PATH
  export PATH
  
  # check tools
--if [ -z $(which mkdosfs) ] || [ -z $(which python) ]; then
+-if [ -z "$(which mkdosfs)" ] || [ -z "$(which python)" ]; then
 -	echo "ERROR: either mkdosfs or python tool missing!"
-+if [ -z $(which mkdosfs) ] || [ -z $(which %%PYTHON_VERSION%%) ]; then
++if [ -z "$(which mkdosfs)" ] || [ -z "$(which %%PYTHON_VERSION%%)" ]; then
 +	echo "ERROR: either mkdosfs or %%PYTHON_VERSION%% tool missing!"
  	exit 1
  fi
  
-@@ -147,8 +147,8 @@ echo "$step) Create DOS Master Boot Reco
+@@ -147,8 +147,8 @@ echo "$step) Create DOS Master Boot Record / partition
  # - http://en.wikipedia.org/wiki/File_Allocation_Table#Boot_Sector
  # For DOS MBR, the values are little endian.
  # -----------
 -python << EOF
--#!/usr/bin/env python
-+%%PYTHON_CMD%% << EOF
-+#!%%PYTHON_CMD%%
+-#!/usr/bin/env python3
++%%PYTHON_VERSION%% << EOF
++#!/usr/bin/env %%PYTHON_VERSION%%3
  mbr = bytearray(512)
  
  def set_long(idx, value):
