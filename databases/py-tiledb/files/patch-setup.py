63e74029fdc111d24f8979a7b03a4b78ce4fafd7
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-02-28 17:34:36 +0800

    databases/py-tiledb: Update to 0.12.1
    - Strip shared library
    
    Changes:	https://github.com/TileDB-Inc/TileDB-Py/releases

--- databases/py-tiledb/files/patch-setup.py
+++ databases/py-tiledb/files/patch-setup.py
@@ -1,24 +1,15 @@
---- setup.py.orig	2021-06-08 20:19:16 UTC
+--- setup.py.orig	2022-01-25 22:10:16 UTC
 +++ setup.py
-@@ -489,12 +489,9 @@ def cmake_available():
- def setup_requires():
-     req = [
-         "cython>=0.27",
--        "numpy==1.16.* ; python_version < '3.9' and 'arm' not in platform_machine",
--        "numpy ; python_version >= '3.9' and 'arm' not in platform_machine",
--        "numpy ; 'arm' in platform_machine",
-+        "numpy>=1.16",
-         "setuptools>=18.0",
-         "setuptools_scm>=1.5.4",
--        "wheel>=0.30",
-         "pybind11>=2.6.2",
-     ]
-     # Add cmake requirement if libtiledb is not found and cmake is not available.
-@@ -687,7 +684,6 @@ setup(
-     setup_requires=setup_requires(),
-     install_requires=[
-         "numpy>=1.16",
--        "wheel>=0.30",
-         "contextvars ;python_version<'3.7'",
-         "dataclasses ;python_version<'3.7'",
-     ],
+@@ -503,12 +503,6 @@ def setup_requires():
+         req = parse_requirements("requirements_dev.txt")
+         req = list(filter(lambda r: not r.startswith("-r"), req))
+ 
+-    req_cmake = list(filter(lambda r: "cmake" in r, req))[0]
+-
+-    # Add cmake requirement if libtiledb is not found and cmake is not available.
+-    if not libtiledb_exists(LIB_DIRS) and not cmake_available():
+-        req.append(req_cmake)
+-
+     return req
+ 
+ 
