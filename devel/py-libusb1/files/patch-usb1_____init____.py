cb33235b5dc2e8370752196c79441c193a53e833
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-02-28 17:34:56 +0800

    devel/py-libusb1: Update to 2.0.1
    Changes:	https://github.com/vpelletier/python-libusb1/blob/master/README.rst#history

--- devel/py-libusb1/files/patch-usb1_____init____.py
+++ devel/py-libusb1/files/patch-usb1_____init____.py
@@ -1,8 +1,8 @@
 https://lists.freebsd.org/pipermail/freebsd-usb/2015-January/013586.html
 
---- usb1/__init__.py.orig	2021-07-02 12:59:01 UTC
+--- usb1/__init__.py.orig	2021-09-24 22:30:54 UTC
 +++ usb1/__init__.py
-@@ -1864,8 +1864,10 @@ class USBDevice(object):
+@@ -1885,8 +1885,10 @@ class USBDevice:
          Get the port number of each hub toward device.
          """
          port_list = (c_uint8 * PATH_MAX_DEPTH)()
