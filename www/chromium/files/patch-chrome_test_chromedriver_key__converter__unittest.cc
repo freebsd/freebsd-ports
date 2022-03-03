2d34172bb7a46cec13b6e66bd7c9bcc6f426eda5
Robert Nagy <robert@openbsd.org>
2022-03-03 14:17:22 +0100

    www/chromium: unbreak chromedriver unittest by fixing a bad ifdef

--- www/chromium/files/patch-chrome_test_chromedriver_key__converter__unittest.cc
+++ www/chromium/files/patch-chrome_test_chromedriver_key__converter__unittest.cc
@@ -14,7 +14,7 @@
    ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
    const char kTextForKeys[] = {
 -#if defined(OS_LINUX) || defined(OS_CHROMEOS)
-+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_LINUX)
++#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_BSD)
        0, 0, 0, 0, '\t', 0, '\r', '\r', 0, 0, 0, 0, 0,
  #else
        0, 0, 0, 0, 0, 0, '\r', '\r', 0, 0, 0, 0, 0,
