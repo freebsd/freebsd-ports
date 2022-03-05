eca58e3768122a0ef093f62ad91760e4341f5df1
Yasuhiro Kimura <yasu@FreeBSD.org>
2022-03-05 13:08:26 +0900

    Mk/bsd.default-versions.mk: Switch default version of Ruby from 2.7 to 3.0
    How to upgrade is described in the entry of 20220421 in UPDATING.
    
    PR:		262350
    Exp-run by:	antoine

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -116,7 +116,7 @@ PYTHON2_DEFAULT?=	2.7
 # Possible values: 3.7, 3.8, 3.9, 3.10, 3.11
 PYTHON3_DEFAULT?=	3.8
 # Possible values: 2.7, 3.0, 3.1, 3.2
-RUBY_DEFAULT?=		2.7
+RUBY_DEFAULT?=		3.0
 # Possible values: rust, rust-nightly
 RUST_DEFAULT?=		rust
 # Possible values: 4.12, 4.13
