ff4d0ccb61b80dd846ba697f6cd30ecbfc167c4e
Jason Unovitch <junovitch@FreeBSD.org>
2022-03-05 02:44:40 +0000

    net/foreman-proxy: update 3.0.1 -> 3.1.2
    Changes:	https://github.com/theforeman/smart-proxy/compare/3.0.1...3.1.2
    PR:		263431

--- net/foreman-proxy/files/patch-lib_proxy_http__download.rb
+++ net/foreman-proxy/files/patch-lib_proxy_http__download.rb
@@ -1,11 +1,11 @@
---- lib/proxy/http_download.rb.orig     2021-10-26 14:48:17.000000000 +0200
-+++ lib/proxy/http_download.rb  2021-11-24 12:24:12.924526000 +0100
-@@ -9,7 +9,7 @@
+--- lib/proxy/http_download.rb.orig	2022-02-16 01:16:26 UTC
++++ lib/proxy/http_download.rb
+@@ -10,7 +10,7 @@ module Proxy
+       logger.warn('Deprecated: HttpDownload read_timeout is deprecated and will be removed in 4.0') if read_timeout
+       logger.warn('Deprecated: HttpDownload dns_timeout is deprecated and will be removed in 4.0') if dns_timeout
+       connect_timeout ||= DEFAULT_CONNECT_TIMEOUT
+-      args = [which('curl')]
++      args = ["%%LOCALBASE%%/bin/curl"]
  
-     def initialize(src, dst, read_timeout = nil, connect_timeout = nil, dns_timeout = nil, verify_server_cert = false)
-       @dst = dst
--      wget = which("wget")
-+      wget = "%%LOCALBASE%%/bin/wget"
-       read_timeout ||= DEFAULT_READ_TIMEOUT
-       dns_timeout ||= DEFAULT_CONNECT_TIMEOUT
-       connect_timeout ||= DEFAULT_DNS_TIMEOUT
+       # no cert verification if set
+       args << "--insecure" unless verify_server_cert
