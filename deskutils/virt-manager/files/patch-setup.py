775d857db4e7eb90ca78745da1e7f15fc8f2e50b
Roman Bogorodskiy <novel@FreeBSD.org>
2022-03-06 07:18:16 +0400

    deskutils/virt-manager: update to 4.0.0

--- deskutils/virt-manager/files/patch-setup.py
+++ deskutils/virt-manager/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2020-09-19 02:24:26 UTC
+--- setup.py.orig	2022-02-12 16:13:59 UTC
 +++ setup.py
-@@ -419,8 +419,8 @@ class VMMDistribution(distutils.dist.Distribution):
+@@ -427,8 +427,8 @@ class VMMDistribution(setuptools.dist.Distribution):
      ]
  
      def __init__(self, *args, **kwargs):
@@ -8,10 +8,10 @@
 -        self.no_compile_schemas = False
 +        self.no_update_icon_cache = True
 +        self.no_compile_schemas = True
-         distutils.dist.Distribution.__init__(self, *args, **kwargs)
+         setuptools.dist.Distribution.__init__(self, *args, **kwargs)
  
  
-@@ -492,11 +492,9 @@ distutils.core.setup(
+@@ -500,11 +500,9 @@ setuptools.setup(
          "build/virt-xml"]),
  
      data_files=[
@@ -24,10 +24,10 @@
              "man/virt-manager.1",
              "man/virt-install.1",
              "man/virt-clone.1",
-@@ -528,7 +526,6 @@ distutils.core.setup(
+@@ -535,7 +533,6 @@ setuptools.setup(
+         'build_i18n': my_build_i18n,
  
          'install': my_install,
-         'install_data': my_install_data,
 -        'install_egg_info': my_egg_info,
  
          'configure': configure,
