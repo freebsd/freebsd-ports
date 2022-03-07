0613e796c43709b1e1283588fc7b5695dba3a152
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-03-08 01:52:56 +0800

    devel/py-ddtrace: Update to 0.58.5
    Changes:	https://github.com/DataDog/dd-trace-py/releases

--- devel/py-ddtrace/files/patch-setup.py
+++ devel/py-ddtrace/files/patch-setup.py
@@ -1,11 +1,26 @@
---- setup.py.orig	2021-10-28 20:18:41 UTC
+--- setup.py.orig	2022-02-24 22:37:13 UTC
 +++ setup.py
-@@ -203,7 +203,7 @@ setup(
+@@ -258,7 +257,7 @@ setup(
          "Programming Language :: Python :: 3.10",
      ],
      use_scm_version={"write_to": "ddtrace/_version.py"},
--    setup_requires=["setuptools_scm[toml]>=4,<6.1", "cython"],
+-    setup_requires=["setuptools_scm[toml]>=4,<6.1", "cython", "cmake", "ninja"],
 +    setup_requires=["setuptools_scm[toml]>=4", "cython"],
      ext_modules=ext_modules
      + cythonize(
          [
+@@ -309,14 +308,6 @@ setup(
+                 "ddtrace.profiling._build",
+                 sources=["ddtrace/profiling/_build.pyx"],
+                 language="c",
+-            ),
+-            Cython.Distutils.Extension(
+-                "ddtrace.appsec._ddwaf",
+-                sources=["ddtrace/appsec/_ddwaf.pyx"],
+-                include_dirs=["ddtrace/appsec/include"],
+-                library_dirs=["ddtrace/appsec/lib"],
+-                libraries=ddwaf_libraries,
+-                language="c++",
+             ),
+         ],
+         compile_time_env={
