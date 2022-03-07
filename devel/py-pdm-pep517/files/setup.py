b92eceb2e03c4ab7b7b8507127c288aa8701c00d
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-03-08 01:53:31 +0800

    devel/py-pdm-pep517: Update to 0.11.2
    Changes:	https://github.com/pdm-project/pdm-pep517/blob/master/CHANGELOG.md

--- devel/py-pdm-pep517/files/setup.py
+++ devel/py-pdm-pep517/files/setup.py
@@ -5,13 +5,6 @@ import codecs
 
 with codecs.open('README.md', encoding="utf-8") as fp:
     long_description = fp.read()
-EXTRAS_REQUIRE = {
-    'setuptools': [
-        'setuptools>=42',
-        'wheel',
-        'editables==0.2',
-    ],
-}
 
 setup_kwargs = {
     'name': 'pdm-pep517',
@@ -29,7 +22,10 @@ setup_kwargs = {
         'pdm.pep517._vendor',
         'pdm.pep517._vendor.cerberus',
         'pdm.pep517._vendor.packaging',
-        'pdm.pep517._vendor.toml',
+        'pdm.pep517._vendor.pyparsing',
+        'pdm.pep517._vendor.pyparsing.diagram',
+        'pdm.pep517._vendor.tomli',
+        'pdm.pep517._vendor.tomli_w',
     ],
     'package_data': {'': ['*']},
     'long_description_content_type': 'text/markdown',
@@ -38,17 +34,13 @@ setup_kwargs = {
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Build Tools',
     ],
-    'extras_require': EXTRAS_REQUIRE,
-    'python_requires': '>=3.6',
-
+    'python_requires': '>=3.7',
 }
-
 
 setup(**setup_kwargs)
