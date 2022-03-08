38ab63405edb0ee89171e316268dfe78e05221e6
Dave Cottlehuber <dch@FreeBSD.org>
2022-03-08 11:56:34 +0000

    dns/py-ns1-python: update to 0.17.1

--- dns/py-ns1-python/files/patch-setup.py
+++ dns/py-ns1-python/files/patch-setup.py
@@ -1,11 +1,12 @@
---- setup.py.orig	2020-05-18 08:21:51 UTC
+--- setup.py.orig	2022-03-08 12:04:18 UTC
 +++ setup.py
-@@ -21,7 +21,7 @@ setup(
-     author_email="devteam@ns1.com",
+@@ -23,8 +23,7 @@ setup(
      url="https://github.com/ns1/ns1-python",
      packages=find_packages(exclude=["tests", "examples"]),
--    setup_requires=["pytest-runner", "wheel",],
-+    setup_requires=["pytest-runner"],
-     tests_require=["pytest", "pytest-pep8", "pytest-cov", "mock",],
-     keywords="dns development rest sdk ns1 nsone",
-     classifiers=[
+     setup_requires=[
+-        "pytest-runner",
+-        "wheel",
++        "pytest-runner"
+     ],
+     tests_require=[
+         "pytest",
