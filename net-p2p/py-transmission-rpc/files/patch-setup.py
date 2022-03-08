36a549c6ed68518b0c9386773a875db574b4a6eb
Jan Beich <jbeich@FreeBSD.org>
2022-03-08 12:42:43 +0000

    net-p2p/py-transmission-rpc: unbreak after f9fae7539193
    =======================<phase: run-depends    >============================
    ===>   py38-transmission-rpc-3.3.0 depends on package: py38-typing-extensions>=3.7.4.2<4.0.2.0 - not found
    ===>   Installing existing package /packages/All/py38-typing-extensions-4.1.1.pkg
    Installing py38-typing-extensions-4.1.1...
    Extracting py38-typing-extensions-4.1.1: .......... done
    ===>   py38-transmission-rpc-3.3.0 depends on package: py38-typing-extensions>=3.7.4.2<4.0.2.0 - not found
    *** Error code 1
    
    Reported by:	pkg-fallout

--- net-p2p/py-transmission-rpc/files/patch-setup.py
+++ net-p2p/py-transmission-rpc/files/patch-setup.py
@@ -1,3 +1,6 @@
+https://github.com/Trim21/transmission-rpc/commit/af38c4faf685
+https://github.com/Trim21/transmission-rpc/commit/9dafcea404b9
+
 --- setup.py.orig	2021-09-29 06:26:44 UTC
 +++ setup.py
 @@ -8,7 +8,7 @@ package_data = \
@@ -5,7 +8,7 @@
  
  install_requires = \
 -['requests>=2.23.0,<3.0.0', 'typing_extensions>=3.7.4.2,<4.0.0.0']
-+['requests>=2.23.0,<3.0.0', 'typing_extensions>=3.7.4.2,<4.0.2.0']
++['requests>=2.23.0,<3.0.0', 'typing_extensions>=3.7.4.2,<4.2.0.0']
  
  extras_require = \
  {'docs': ['sphinx==4.2.0', 'sphinx-rtd-theme==1.0.0']}
