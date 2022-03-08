b494b8bcd804a987898908be8de7cfb6580d3d2a
Yasuhiro Kimura <yasu@FreeBSD.org>
2022-03-08 16:31:16 +0900

    archivers/rubygem-libarchive: Fix build with Ruby 3.x
    PR:		262525
    Approved by:	maintainer timeout

---
+++ archivers/rubygem-libarchive/files/patch-ext_libarchive__internal.h
@@ -0,0 +1,10 @@
+--- ext/libarchive_internal.h.orig	2022-03-08 07:22:32 UTC
++++ ext/libarchive_internal.h
+@@ -28,7 +28,6 @@
+ #include <archive_entry.h>
+ 
+ #include <ruby.h>
+-#include <rubysig.h>
+ 
+ #ifdef _WIN32
+ #include "libarchive_win32.h"
