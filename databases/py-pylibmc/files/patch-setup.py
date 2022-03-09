0d1e1a1fadc90cc0bd7fee09f2181f6fb1d54c11
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2022-03-09 00:28:00 -0600

    databases/py-pylibmc: Update version 1.6.0=>1.6.1
    Relnotes: https://github.com/lericson/pylibmc/releases/tag/1.6.1

--- databases/py-pylibmc/files/patch-setup.py
+++ databases/py-pylibmc/files/patch-setup.py
@@ -1,8 +1,8 @@
---- setup.py.orig	2016-08-09 16:06:26 UTC
+--- setup.py.orig	2019-08-15 11:49:27 UTC
 +++ setup.py
-@@ -123,4 +123,5 @@ setup(
-         'Programming Language :: Python :: 3.4',
-         'Programming Language :: Python :: 3.5',
+@@ -125,4 +125,5 @@ setup(
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7',
      ],
 +    test_suite = 'nose.collector'
  )
