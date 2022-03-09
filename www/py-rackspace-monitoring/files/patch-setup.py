0d4ffd735d4b595cd6df668d0d5c65425a5710de
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2022-03-09 11:09:54 -0600

    www/py-rackspace-monitoring: Update version 0.6.5=>0.8.1
    - Mark DEPRECATED as it's compatibility defined by upstream is upto 3.2
      only and last release was 3 years ago
    - Set EXPIRATION_DATE to 2022-06-30
    Relnotes: https://github.com/racker/rackspace-monitoring/releases/tag/v0.8.1

--- www/py-rackspace-monitoring/files/patch-setup.py
+++ www/py-rackspace-monitoring/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2016-03-07 05:34:43 UTC
+--- setup.py.orig	2018-08-13 13:22:29 UTC
 +++ setup.py
 @@ -171,7 +171,7 @@ setup(
      description='Client library for Rackspace Cloud Monitoring',
      author='Rackspace, Inc.',
      author_email='monitoring@rackspace.com',
--    install_requires=['apache-libcloud >= 0.14, <0.16'],
-+    install_requires=['apache-libcloud >= 0.14'],
+-    install_requires=['apache-libcloud >= 0.17,<2.0',
++    install_requires=['apache-libcloud >= 0.17',
+                       'backports.ssl_match_hostname'],
      packages=[
          'rackspace_monitoring',
-         'rackspace_monitoring.drivers',
