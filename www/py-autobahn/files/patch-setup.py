8e8cb469063549780b40a6c6620f73d573b429b3
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2022-03-10 09:22:53 -0600

    www/py-autobahn: Update version 19.5.1=>21.2.1
    - Update WWW
    
    Relnotes:
    https://github.com/crossbario/autobahn-python/releases/tag/v21.2.1

--- www/py-autobahn/files/patch-setup.py
+++ www/py-autobahn/files/patch-setup.py
@@ -1,18 +1,26 @@
---- setup.py.orig	2016-01-24 16:10:22 UTC
+--- setup.py.orig	2022-03-10 14:28:18 UTC
 +++ setup.py
-@@ -32,12 +32,9 @@ import platform
- from setuptools import setup
- from setuptools.command.test import test as test_command
- 
--# remember if we already had six _before_ installation
+@@ -312,23 +312,3 @@ setup(
+                  "Topic :: Software Development :: Object Brokering"],
+     keywords='autobahn crossbar websocket realtime rfc6455 wamp rpc pubsub twisted asyncio xbr data-markets blockchain ethereum'
+ )
+-
+-
+-# regenerate Twisted plugin cache
 -try:
--    import six  # noqa
--    _HAD_SIX = True
+-    from twisted.internet import reactor
+-    print("Twisted found (default reactor is {0})".format(reactor.__class__))
 -except ImportError:
--    _HAD_SIX = False
-+# We don't want this package to regenerate the Twisted dropin.cache
-+# as it creates a filesystem violation by writing outside of STAGEDIR
-+_HAD_SIX = False
- 
- CPY = platform.python_implementation() == 'CPython'
- PY3 = sys.version_info >= (3,)
+-    # the user doesn't have Twisted, so skip
+-    pass
+-else:
+-    # Make Twisted regenerate the dropin.cache, if possible. This is necessary
+-    # because in a site-wide install, dropin.cache cannot be rewritten by
+-    # normal users.
+-    try:
+-        from twisted.plugin import IPlugin, getPlugins
+-        list(getPlugins(IPlugin))
+-    except Exception as e:
+-        print("Failed to update Twisted plugin cache: {0}".format(e))
+-    else:
+-        print("Twisted dropin.cache regenerated.")
