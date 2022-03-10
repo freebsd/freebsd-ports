aed79866d4c7fee24b9be37fcad41a82f330e14f
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2022-03-10 04:01:32 -0600

    net/py-twitter-tools: Update version 1.18.0=>1.19.3
    Relnotes:
    https://github.com/python-twitter-tools/twitter/releases/tag/twitter-1.19.3

--- net/py-twitter-tools/files/patch-setup.py
+++ net/py-twitter-tools/files/patch-setup.py
@@ -1,17 +1,27 @@
 # Fix UnicodeDecodeError: 'ascii' codec can't decode error
 
---- setup.py.orig	2019-04-25 10:41:18 UTC
+--- setup.py.orig	2021-06-18 10:28:47 UTC
 +++ setup.py
-@@ -1,11 +1,7 @@
- from setuptools import setup, find_packages
- import sys, os
+@@ -3,12 +3,8 @@ with open("README") as f:
+ with open("README") as f:
+     long_description = f.read()
  
--try:
--    import pypandoc
--    long_description = pypandoc.convert('README', 'rst', format='md')
--except ImportError:
--    long_description = open('./README', 'r').read()
-+long_description = open('./README', 'rb').read().decode("utf-8")
++version = '1.19.3'
  
- version = '1.18.0'
- 
+-def local_scheme(version):
+-    """Skip the local version (eg. +xyz of 0.6.1.dev4+gdf99fe2)
+-    to be able to upload to Test PyPI"""
+-    return ""
+-
+ setup(name='twitter',
+       description="An API and command-line toolset for Twitter (twitter.com)",
+       long_description=long_description,
+@@ -43,8 +39,6 @@ setup(name='twitter',
+       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+       include_package_data=True,
+       zip_safe=True,
+-      use_scm_version={"local_scheme": local_scheme},
+-      setup_requires=["setuptools_scm"],
+       entry_points="""
+       # -*- Entry points: -*-
+       [console_scripts]
