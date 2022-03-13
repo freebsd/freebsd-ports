de445d5dd99c6b2d2188f7f25d41c871a14e12db
Robert Clausecker <fuz@fuz.su>
2022-03-12 18:26:31 -0800

    math/oink: Update g20210704 -> g20210710 and make atomics portable
    PR:		262513

--- math/oink/files/patch-src_lace.h
+++ math/oink/files/patch-src_lace.h
@@ -1,13 +1,26 @@
---- src/lace.h.orig	2021-07-12 11:27:04 UTC
+--- src/lace.h.orig	2021-07-10 20:56:55 UTC
 +++ src/lace.h
-@@ -293,7 +293,11 @@ void lace_yield(WorkerP *__lace_worker, Task *__lace_d
+@@ -17,6 +17,11 @@
+  */
+ 
+ #include <unistd.h>
++#ifdef __cplusplus
++#include <atomic>
++#else
++#include <stdatomic.h>
++#endif
+ #include <stdint.h>
+ #include <stdio.h>
+ #include <pthread.h> /* for pthread_t */
+@@ -293,7 +298,11 @@ void lace_yield(WorkerP *__lace_worker, Task *__lace_d
  #endif
  
  #ifndef mfence
-+#if defined(__amd64__) || defined(__i386__)
- #define mfence() { asm volatile("mfence" ::: "memory"); }
-+#elif defined(__powerpc__)
-+#define mfence() { asm volatile("sync" ::: "memory"); }
+-#define mfence() { asm volatile("mfence" ::: "memory"); }
++#ifdef __cplusplus
++#define mfence() std::atomic_thread_fence(std::memory_order_seq_cst)
++#else
++#define mfence() atomic_thread_fence(memory_order_seq_cst)
 +#endif
  #endif
  
