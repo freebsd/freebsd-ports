7163a3858daeb67fb55dee1a94c2bf7487867908
Gleb Popov <arrowd@FreeBSD.org>
2022-03-15 22:39:07 +0300

    devel/tortoisehg: Update to 6.1
    Reported by:	portscout!

--- devel/tortoisehg/files/patch-tortoisehg_util_hgversion.py
+++ devel/tortoisehg/files/patch-tortoisehg_util_hgversion.py
@@ -1,20 +1,19 @@
---- tortoisehg/util/hgversion.py.orig	2020-04-08 09:44:12 UTC
+--- tortoisehg/util/hgversion.py.orig	2022-03-06 02:33:21 UTC
 +++ tortoisehg/util/hgversion.py
-@@ -19,16 +19,4 @@ except AttributeError:
- testedwith = b'5.2 5.3'
+@@ -33,15 +33,7 @@ def checkhgversion(v):
+     return vers
  
  def checkhgversion(v):
 -    """range check the Mercurial version"""
 -    reqvers = testedwith.split()
--    v = v.split(b'+')[0]
--    if not v or v == b'unknown' or len(v) >= 12:
--        # can't make any intelligent decisions about unknown or hashes
--        return
--    vers = re.split(br'\.|-|rc', v)[:2]
--    if len(vers) < 2:
+-    vers = _splitversion(v)
+-    if not vers:
 -        return
 -    if b'.'.join(vers) in reqvers:
 -        return
 -    return (b'This version of TortoiseHg requires Mercurial version %s.n to '
 -            b'%s.n, but found %s') % (reqvers[0], reqvers[-1], v)
 +    return
+ 
+ def checkminhgversion(v):
+     """Check if the given Mercurial version is not lower than the minimum
