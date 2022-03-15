7d96490834314e7e5853222464b25c792a46789c
Dmitry Marakasov <amdmi3@FreeBSD.org>
2022-03-15 21:57:10 +0300

    graphics/geomorph: update 0.60.1 → 0.63

--- graphics/geomorph/files/patch-src_app_main.c
+++ graphics/geomorph/files/patch-src_app_main.c
@@ -1,46 +1,50 @@
---- src/app/main.c.orig	2010-10-30 08:17:00.000000000 +0400
-+++ src/app/main.c	2013-08-28 03:51:05.535226256 +0400
-@@ -18,6 +18,7 @@
-  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-  */
+--- src/app/main.c.orig	2021-01-31 03:36:55 UTC
++++ src/app/main.c
+@@ -75,87 +75,22 @@ gchar *find_config_file() {
+ 	gboolean shortcuts;
+ 	glong pathmax;
  
-+#include <sys/param.h>
- #include "./main.h"
- #include "./globals.h"
- #include "./app.h"
-@@ -68,6 +69,12 @@
- 	if (command) x_free(command);
- }
- 
-+char *get_current_dir_name() {
-+	char *buf = malloc(MAXPATHLEN);
-+	getwd(buf);
-+	return buf;
-+}
-+
- gchar *find_config_file() {
- //	Returns the config file name (name OPTION_FILE, defined in globals.h),
- //	with its full path
-@@ -83,59 +90,23 @@
- //	if $HOME/DEF_DIR_NAME doesn't exist, we create the required files
- //	The directory where OPTION_FILE is found becomes the default one (DEF_DIR)
+-//	We try:
+-//	1. in the current directory. (./OPTION_FILE = ./geomorphrc)
+-//	2. if not found, we try ./DEF_DIR_NAME/OPTION_FILE (./geomorph/geomorphrc)
+-//	3. if not found, we try $HOME/OPTION_FILE (~/geomorphrc)
+-//	4. if not found, we try in $HOME/DEF_DIR_NAME (~/geomorph/geomorphrc) 
+-//	--> if OPTION_FILE was not found, and 
+-//	if $HOME/DEF_DIR_NAME doesn't exist, we create the required files
+-//	The directory where OPTION_FILE is found becomes the default one (DEF_DIR)
++	default_dir = concat_dname_fname(getenv("HOME"), DEF_DIR_NAME);
++	path_n_file = path_n_file = concat_dname_fname(default_dir, OPTION_FILE);
  
 -	// Try ./OPTION_FILE
--	default_dir = (gchar *) get_current_dir_name();
--	path_n_file = concat_dname_fname(default_dir,OPTION_FILE);
+-	pathmax = get_pathmax();
+-	default_dir = (gchar *) x_malloc(pathmax*sizeof(gchar),"gchar - default_dir in main.c");
+-	dummy = (gchar *) getcwd(default_dir,pathmax);
+-// printf("Dummy: %p; Current dir name for config file: %p;\n", dummy, default_dir);
+-	path_n_file = concat_dname_fname(dummy,OPTION_FILE);
 -	if (!filexists(path_n_file)) { // Try ./DEF_DIR_NAME/OPTION_FILE
--		default_dir = concat_dname_fname(default_dir,DEF_DIR_NAME);
+-		dummy = default_dir;
+-		default_dir = concat_dname_fname(dummy,DEF_DIR_NAME);
+-		x_free(dummy);
+-		dummy = NULL;
 -		if (path_n_file)
 -			x_free(path_n_file);
 -		path_n_file = concat_dname_fname(default_dir,OPTION_FILE);
 -		if (!filexists(path_n_file)) { // Try in the home directory - ~/OPTION_FILE
+-			x_free(default_dir);
+-#ifdef _WIN32
+-			default_dir = get_windows_home();
+-#else
 -			default_dir = getenv("HOME");
+-#endif
 -			if (path_n_file)
 -				x_free(path_n_file);
 -			path_n_file = concat_dname_fname(default_dir, OPTION_FILE);
 -			if (!filexists(path_n_file)) { // Try ~/DEFAULT_DIR/OPTION_FILE
--				default_dir = concat_dname_fname(default_dir,DEF_DIR_NAME);
--				if (path_n_file) x_free(path_n_file);
+-				dummy = default_dir;				
+-				default_dir = concat_dname_fname(dummy,DEF_DIR_NAME);
+-				// Here we don't free dummy because it comes from getenv
+-				if (path_n_file) 
+-					x_free(path_n_file);
 -				path_n_file = concat_dname_fname(default_dir, OPTION_FILE);
 -				if (directory_exists(default_dir)) {
 -					if (!filexists(path_n_file)) {
@@ -52,7 +56,7 @@
 -					// Geomorph directory ("default_dir") doesn't exist
 -					// Create one with default scenes and rc file
 -					buf = (gchar *) x_malloc(strlen(_("Creation of the default working directory?"))+1+strlen(default_dir), "const gchar (buf - Creation of the default working directory MSG)");
--					sprintf(buf,_("Creation of the default working directory?"),default_dir);
+-					sprintf(buf,_("Creation of the default working directory %s?"),default_dir);
 -					if (!yes_no(buf,TRUE))
 -						exit(0);
 -//					if (buf) x_free(buf);
@@ -76,23 +80,24 @@
 -				if (path_n_file) x_free(path_n_file);
 -				}
 -			}
-+	default_dir = concat_dname_fname(getenv("HOME"), DEF_DIR_NAME);
-+	path_n_file = path_n_file = concat_dname_fname(default_dir, OPTION_FILE);
-+
 +	if (directory_exists(default_dir)) {
 +		if (!filexists(path_n_file)) {
-+			// Big problem, create a default rc file
 +			create_config_file(path_n_file);
  		}
- 	}
-+	else {
++	} else {
 +		command = concat_dname_fname(get_data_dir(),"install-step1-dir");
 +		if (system(command))
 +			my_msg(_("Fatal error during the creation of the default working directory"),ABORT);
 +		if (command) free(command);
 +		create_config_file(path_n_file);
-+	}
+ 	}
+-	dummy = default_dir;
+-	default_dir = add_filesep(dummy);
+-	if (dummy != default_dir)
+-		x_free(dummy);
+-	return default_dir;
 +	if (path_n_file) free(path_n_file);
- 	return add_filesep(default_dir);
++	return add_filesep(default_dir);
  }
  
+ int main( int   argc,
