ff7f5c801fc60ea328d49af62f5e323b5ea3ff3c
Robert Nagy <robert@openbsd.org>
2022-03-19 18:06:32 +0100

    www/chromium: add forgotten patch in last commit
    (cherry picked from commit 064dbd0f3082fd1da6e11ba7386f3a9182543341)

---
+++ www/chromium/files/patch-ui_gl_gl__context.cc
@@ -0,0 +1,11 @@
+--- ui/gl/gl_context.cc.orig	2022-03-19 12:56:15 UTC
++++ ui/gl/gl_context.cc
+@@ -414,7 +414,7 @@ bool GLContext::MakeVirtuallyCurrent(
+   DCHECK(virtual_context->IsCurrent(surface));
+ 
+   if (switched_real_contexts || virtual_context != current_virtual_context_) {
+-#if DCHECK_IS_ON()
++#if DCHECK_IS_ON() && !BUILDFLAG(IS_BSD)
+     GLenum error = glGetError();
+     // Accepting a context loss error here enables using debug mode to work on
+     // context loss handling in virtual context mode.
