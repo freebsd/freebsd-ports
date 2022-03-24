ac83da8f0f647fdcfd6c5d8abf01fe09a7ed28fb
Sascha Biberhofer <ports@skyforge.at>
2022-03-24 19:23:56 +0000

    net-im/py-matrix-synapse: Update to 1.55.2

--- net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
+++ net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
@@ -1,11 +1,11 @@
---- synapse/python_dependencies.py.orig	2022-02-22 11:32:11 UTC
+--- synapse/python_dependencies.py.orig	2022-03-24 17:18:43 UTC
 +++ synapse/python_dependencies.py
-@@ -86,7 +87,7 @@ REQUIREMENTS = [
-     "typing-extensions>=3.7.4",
+@@ -80,7 +80,7 @@ REQUIREMENTS = [
+     "typing-extensions>=3.10.0",
      # We enforce that we have a `cryptography` version that bundles an `openssl`
      # with the latest security patches.
 -    "cryptography>=3.4.7",
 +    "cryptography",
-     "ijson>=3.1",
+     # ijson 3.1.4 fixes a bug with "." in property names
+     "ijson>=3.1.4",
      "matrix-common~=1.1.0",
- ]
