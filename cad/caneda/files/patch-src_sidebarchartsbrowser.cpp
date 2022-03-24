1a9d3bdee3d0896d043c363794a6f8a1ad6b1f4b
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2022-03-24 05:57:11 +0100

    cad/caneda: Prepare to Qwt 6.2.0

---
+++ cad/caneda/files/patch-src_sidebarchartsbrowser.cpp
@@ -0,0 +1,10 @@
+--- src/sidebarchartsbrowser.cpp.orig	2022-03-24 04:51:12 UTC
++++ src/sidebarchartsbrowser.cpp
+@@ -34,6 +34,7 @@
+ #include <QVBoxLayout>
+ 
+ #include <qwt_plot_curve.h>
++#include <qwt_text.h>
+ 
+ namespace Caneda
+ {
