792ff6045206a97a0ec3625a0417303b8156bcc3
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-03-25 21:10:55 +0800

    Mk/Uses/python.mk: Use lang/cython for all Python versions
    lang/cython has been updated to support lang/python311 (3.11.0a6) while
    lang/cython-devel does not support it yet (broken since 3.11.0a4).

--- Mk/Uses/python.mk
+++ Mk/Uses/python.mk
@@ -514,18 +514,12 @@ _PYTHONPKGLIST=	${WRKDIR}/.PLIST.pymodtmp
 # - it uses USE_PYTHON=distutils
 #
 
-.if ${PYTHON_REL} >= 31100
-_CYTHON_DEP=	cython-${PYTHON_VER}:lang/cython-devel@${PY_FLAVOR}
-.else
-_CYTHON_DEP=	cython-${PYTHON_VER}:lang/cython@${PY_FLAVOR}
-.endif
-
 .if defined(_PYTHON_FEATURE_CYTHON)
-BUILD_DEPENDS+=	${_CYTHON_DEP}
+BUILD_DEPENDS+=	cython-${PYTHON_VER}:lang/cython@${PY_FLAVOR}
 .endif
 
 .if defined(_PYTHON_FEATURE_CYTHON_RUN)
-RUN_DEPENDS+=	${_CYTHON_DEP}
+RUN_DEPENDS+=	cython-${PYTHON_VER}:lang/cython@${PY_FLAVOR}
 .endif
 
 .if defined(_PYTHON_FEATURE_CONCURRENT)
