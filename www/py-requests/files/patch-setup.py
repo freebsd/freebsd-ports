fb5f8f301150da3a6cbd7b395788cb057fca6e9a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-03-26 01:15:39 +0800

    www/py-requests: Fix build with py-idna 3.*
    - Update version requirement of *_DEPENDS
    - Clean up TEST_DEPENDS
    - Update do-test:
    - Bump PORTREVISION for package change

---
+++ www/py-requests/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-12-16 17:43:25 UTC
++++ setup.py
+@@ -43,7 +43,7 @@ packages = ['requests']
+ 
+ requires = [
+     'chardet>=3.0.2,<5',
+-    'idna>=2.5,<3',
++    'idna>=2.5,<4',
+     'urllib3>=1.21.1,<1.27',
+     'certifi>=2017.4.17'
+ 
