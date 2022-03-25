5d96194792aa1e3f1cae5ca0a71ba1c1372b71cc
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-03-25 21:05:47 +0800

    www/py-quilt3: Do not install tests module
    - Bump PORTREVISION for package change
    
    PR:		262759
    Reported by:	se

--- www/py-quilt3/files/patch-setup.py
+++ www/py-quilt3/files/patch-setup.py
@@ -1,19 +1,11 @@
 --- setup.py.orig	2021-10-15 18:41:22 UTC
 +++ setup.py
-@@ -64,13 +64,13 @@ setup(
-         'flask',
-         'flask_cors',
-         'flask_json',
--        'jsonlines==1.2.0',
-+        'jsonlines>=1.2.0',
-         'PyYAML>=5.1',
-         'requests>=2.12.4',
-         'tenacity>=5.1.1',
-         'tqdm>=4.32',
--        'requests_futures==1.0.0',
--        'jsonschema==3.*',
-+        'requests_futures>=1.0.0',
-+        'jsonschema>=3,<5',
-     ],
-     extras_require={
-         'pyarrow': [
+@@ -38,7 +38,7 @@ class VerifyVersionCommand(install):
+ setup(
+     name="quilt3",
+     version=VERSION,
+-    packages=find_packages(),
++    packages=find_packages(exclude=['tests*']),
+     description='Quilt: where data comes together',
+     long_description=readme(),
+     python_requires='>=3.6',
