683aeeebd2c496d01b936380aff2a4975016f57b
Neel Chauhan <nc@FreeBSD.org>
2022-03-27 08:19:29 -0700

    x11/gnome-shell: Fix crash with gnome-desktop 42.0

---
+++ x11/gnome-shell/files/patch-js_ui_environment.js
@@ -0,0 +1,10 @@
+--- js/ui/environment.js.orig	2022-03-27 15:10:52 UTC
++++ js/ui/environment.js
+@@ -6,6 +6,7 @@ imports.gi.versions.GdkPixbuf = '2.0';
+ imports.gi.versions.Clutter = Config.LIBMUTTER_API_VERSION;
+ imports.gi.versions.Gio = '2.0';
+ imports.gi.versions.GdkPixbuf = '2.0';
++imports.gi.versions.GnomeDesktop = '3.0';
+ imports.gi.versions.Gtk = '3.0';
+ imports.gi.versions.Soup = '3.0';
+ imports.gi.versions.TelepathyGLib = '0.12';
