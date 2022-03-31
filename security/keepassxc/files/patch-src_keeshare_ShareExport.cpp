05087b0b8a3f2a6314b3230eccb85130e5570343
Guido Falsi <madpilot@FreeBSD.org>
2022-04-01 01:52:17 +0800

    security/keepassxc: Update to 2.7.0
    PR:		262953
    Reported by:	courtney.hicks1@icloud.com

---
+++ security/keepassxc/files/patch-src_keeshare_ShareExport.cpp
@@ -0,0 +1,11 @@
+--- src/keeshare/ShareExport.cpp.orig	2022-03-21 22:47:20 UTC
++++ src/keeshare/ShareExport.cpp
+@@ -27,7 +27,7 @@
+ 
+ #include <QBuffer>
+ #include <botan/pubkey.h>
+-#include <zip.h>
++#include <minizip/zip.h>
+ 
+ namespace
+ {
---
+++ security/keepassxc/files/patch-src_keeshare_ShareImport.cpp
@@ -0,0 +1,11 @@
+--- src/keeshare/ShareImport.cpp.orig	2022-03-21 22:47:20 UTC
++++ src/keeshare/ShareImport.cpp
+@@ -21,7 +21,7 @@
+ #include "keys/PasswordKey.h"
+ 
+ #include <QBuffer>
+-#include <unzip.h>
++#include <minizip/unzip.h>
+ 
+ namespace
+ {
