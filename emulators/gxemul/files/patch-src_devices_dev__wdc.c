9478a8c169cd07d375213573bb139dce11f9ee19
Mitchell Clay <mclay@astate.edu>
2022-04-01 00:12:21 -0400

    emulators/gxemul: Update to 0.7.0 and take maintainership
    Changelog:	http://gavare.se/gxemul/gxemul-stable/doc/RELEASE.html
    
    PR:		261874

--- emulators/gxemul/files/patch-src_devices_dev__wdc.c
+++ emulators/gxemul/files/patch-src_devices_dev__wdc.c
@@ -1,6 +1,6 @@
---- src/devices/dev_wdc.cc.orig	2014-08-17 08:45:12 UTC
-+++ src/devices/dev_wdc.cc
-@@ -274,12 +274,12 @@ void wdc__read(struct cpu *cpu, struct w
+--- src/devices/dev_wdc.c.orig	2021-04-22 18:04:20 UTC
++++ src/devices/dev_wdc.c
+@@ -274,12 +274,12 @@ void wdc__read(struct cpu *cpu, struct wdc_data *d)
  	    + (int64_t)d->head * d->sectors_per_track[d->drive] +
  	    (int64_t)d->heads[d->drive] * d->sectors_per_track[d->drive] * cyl);
  
@@ -15,7 +15,7 @@
  #endif
  
  	while (count > 0) {
-@@ -320,12 +320,12 @@ void wdc__write(struct cpu *cpu, struct 
+@@ -320,12 +320,12 @@ void wdc__write(struct cpu *cpu, struct wdc_data *d)
  	uint64_t offset = 512 * (d->sector - 1
  	    + (int64_t)d->head * d->sectors_per_track[d->drive] +
  	    (int64_t)d->heads[d->drive] * d->sectors_per_track[d->drive] * cyl);
