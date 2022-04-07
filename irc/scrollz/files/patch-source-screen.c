2bd6d7151e1339c6d17db55cf5db020ed5e45243
Chris Petrik <Christopher.petrik@usm.edu>
2022-04-07 10:55:18 +0200

    irc/scrollz: Update to version 2.3.1
    ChangeLog: https://www.scrollz.info/download/ChangeLog.2.3.1.txt
    
    Use online commit to avoid gcc.
    
    PR:	262693
    Reported by:	Christopher.petrik@usm.edu
    Approved by:	freebsd@bitchx.org (maintainer, timeout > 2 weeks)
    Reviewed by:	mikael@
    MFH:	2022Q2 (bugfix release, security)
    Security:	CVE-2021-29376
    
    (cherry picked from commit c4a37205059c6f8e365b50ee29e82e15a3e41221)

--- irc/scrollz/files/patch-source-screen.c
+++ irc/scrollz/files/patch-source-screen.c
@@ -1,6 +1,6 @@
---- ./source/screen.c.orig	2011-04-29 03:21:18.000000000 -0500
-+++ ./source/screen.c	2013-07-13 07:45:53.092133202 -0500
-@@ -2424,6 +2424,10 @@
+--- source/screen.c.orig	2021-05-06 08:04:55 UTC
++++ source/screen.c
+@@ -2425,6 +2425,10 @@ create_additional_screen()
  	{
  		screen_type = ST_SCREEN;
  	}
@@ -11,7 +11,7 @@
  	else if ((char *) 0 != (displayvar = getenv("DISPLAY")))
  	{
  		if ((char *) 0 == (termvar = getenv("TERM")))
-@@ -2462,6 +2466,7 @@
+@@ -2463,6 +2467,7 @@ create_additional_screen()
  	say("Opening new %s...",
  		screen_type == ST_XTERM ?  "window" :
  		screen_type == ST_SCREEN ? "screen" :
@@ -19,10 +19,11 @@
  					   "wound" );
  	snprintf(sock.sun_path, sizeof sock.sun_path, "/tmp/irc_%08d_%x", (int) pid, mycycle);
  	sock.sun_family = AF_UNIX;
-@@ -2577,6 +2582,22 @@
+@@ -2577,6 +2582,22 @@ create_additional_screen()
+ 			args[i++] = error_sockaddr->sun_path;
  			args[i] = NULL;
  			execvp(xterm, args);
- 		}
++		}
 +		else if (screen_type == ST_TMUX)
 +		{
 +			char	*args[64],
@@ -38,7 +39,6 @@
 +			Debug((3, "added: %s %s '%s'", args[i-3], args[i-2], args[i-1]));
 +			args[i++] = NULL;
 +			execvp("tmux", args);
-+		}
+ 		}
  		perror("execve");
  		unlink(sockaddr->sun_path);
- 		unlink(error_sockaddr->sun_path);
