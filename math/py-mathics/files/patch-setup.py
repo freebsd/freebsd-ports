a6867d0979f4d1a7d24aca96263b9b14a4ab8c89
Dmitry Marakasov <amdmi3@FreeBSD.org>
2022-04-08 16:52:49 +0300

    math/py-mathics: remove bogus sympy max version limitation
    This fixes this and dependent ports, including math/mathicsscript,
    which is confirmed by `make test`
    
    Approved by:	portmgr blanket

--- math/py-mathics/files/patch-setup.py
+++ math/py-mathics/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2022-02-20 13:30:03 UTC
+--- setup.py.orig	2021-07-31 19:47:09 UTC
 +++ setup.py
 @@ -105,7 +105,7 @@ else:
  # General Requirements
  INSTALL_REQUIRES += [
      "Mathics_Scanner>=1.2.1,<1.3.0",
 -    "sympy>=1.8, <= 1.9dev",
-+    "sympy>=1.8, <= 1.9",
++    "sympy>=1.8",
      "mpmath>=1.2.0",
      "numpy",
      "palettable",
