9a15ef04b4cc306bf68abfcd474eb4a1f726ccf2
Ashish SHUKLA <ashish@FreeBSD.org>
2022-04-10 11:45:30 +0000

    net-im/py-matrix-synapse: Update to 1.56.0
    - Mention important changes related to the upgrade to UPDATING
    - Update the dependency constraints
    
    Co-authored-by:	Sascha Biberhofer <ports at skyforge dot at>
    PR:		263193
    Approved by:	Sascha Biberhofer <ports at skyforge dot at> (maintainer)
    
    (cherry picked from commit 76ad2b55b71f9ba18a205d9ecdb248b78d626a0c)

--- net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
+++ net-im/py-matrix-synapse/files/patch-synapse_python__dependencies.py
@@ -1,6 +1,15 @@
---- synapse/python_dependencies.py.orig	2022-03-24 17:18:43 UTC
+--- synapse/python_dependencies.py.orig	2022-04-12 08:11:46 UTC
 +++ synapse/python_dependencies.py
-@@ -80,7 +80,7 @@ REQUIREMENTS = [
+@@ -48,7 +48,7 @@ REQUIREMENTS = [
+     "unpaddedbase64>=1.1.0",
+     "canonicaljson>=1.4.0",
+     # we use the type definitions added in signedjson 1.1.
+-    "signedjson>=1.1.0,<=1.1.1",
++    "signedjson>=1.1.0",
+     "pynacl>=1.2.1",
+     "idna>=2.5",
+     # validating SSL certs for IP addresses requires service_identity 18.1.
+@@ -83,7 +83,7 @@ REQUIREMENTS = [
      "typing-extensions>=3.10.0",
      # We enforce that we have a `cryptography` version that bundles an `openssl`
      # with the latest security patches.
