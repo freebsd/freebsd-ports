a93edf31dc0589c814cb4aa617be4f9db0a8999c
Kai Knoblich <kai@FreeBSD.org>
2022-04-10 12:42:24 +0200

    textproc/py-pdfminer.six: Update to 20220319
    Changelog:
    
    https://github.com/pdfminer/pdfminer.six/releases/tag/20220319
    
    MFH:		2022Q2

--- textproc/py-pdfminer.six/files/patch-setup.py
+++ textproc/py-pdfminer.six/files/patch-setup.py
@@ -2,16 +2,16 @@ Provide the console scripts without their ".py" extension.
 
 See also: https://github.com/pdfminer/pdfminer.six/issues/724
 
---- setup.py.orig	2022-02-21 17:52:51 UTC
+--- setup.py.orig	2022-03-19 20:13:07 UTC
 +++ setup.py
-@@ -28,8 +28,8 @@ setup(
-     author_email='pdfminer@goulu.net',
-     url='https://github.com/pdfminer/pdfminer.six',
+@@ -33,8 +33,8 @@ setup(
+     author_email="pdfminer@goulu.net",
+     url="https://github.com/pdfminer/pdfminer.six",
      scripts=[
--        'tools/pdf2txt.py',
--        'tools/dumppdf.py',
-+        'tools/pdf2txt',
-+        'tools/dumppdf',
+-        "tools/pdf2txt.py",
+-        "tools/dumppdf.py",
++        "tools/pdf2txt",
++        "tools/dumppdf",
      ],
      keywords=[
-         'pdf parser',
+         "pdf parser",
