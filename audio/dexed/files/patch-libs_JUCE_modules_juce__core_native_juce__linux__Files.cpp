fddc379a5486c450653f2c1a4c34dd0ed5a7b54a
Yuri Victorovich <yuri@FreeBSD.org>
2022-04-13 12:47:39 -0700

    audio/dexed: Fix install on non-Intel architectures; add comment to patch

--- audio/dexed/files/patch-libs_JUCE_modules_juce__core_native_juce__linux__Files.cpp
+++ audio/dexed/files/patch-libs_JUCE_modules_juce__core_native_juce__linux__Files.cpp
@@ -1,3 +1,5 @@
+- otherwise it fails with unknown 'environ' symbol, see https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=263265
+
 --- libs/JUCE/modules/juce_core/native/juce_linux_Files.cpp.orig	2022-04-13 18:55:08 UTC
 +++ libs/JUCE/modules/juce_core/native/juce_linux_Files.cpp
 @@ -21,7 +21,7 @@
