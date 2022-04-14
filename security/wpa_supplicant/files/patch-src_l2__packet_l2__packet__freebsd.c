84877d0e0252072a222b3eb16a354f53b8f639fa
Cy Schubert <cy@FreeBSD.org>
2022-04-14 08:42:03 -0700

    */{wpa_supplicant*,hostapd*}: Fix wpa 100% CPU when USB wlan NIC removed
    hostapd calls pcap_next(3) to read the next packet off the wlan interface.
    pcap_next() returns a pointer to the packet header but does not indicate
    success or failure. Unfortunately this results in an infinite loop (100%
    CPU) when the wlan device disappears, i.e. when a USB wlan device is
    manually removed or a USB error results in the device removal. However
    pcap_next_ex(3) does return success or failure. To resolve this we use
    pcap_next_ex(), forcing hostapd to exit when the error is encountered.
    
    An error message is printed to syslog or stderr when debugging (-d flag)
    is enabled. Unfortunately wpa_printf() only works when debugging is enabled.
    
    PR:		253608
    Reported by:	Damjan Jovanovic <damjan.jov@gmail.com>,
    		bz (privately)
    MFH:		2022Q2
    
    (cherry picked from commit c586ac04eb662dea00ab81b226fa3e41a5110b21)

--- security/wpa_supplicant/files/patch-src_l2__packet_l2__packet__freebsd.c
+++ security/wpa_supplicant/files/patch-src_l2__packet_l2__packet__freebsd.c
@@ -1,5 +1,5 @@
---- src/l2_packet/l2_packet_freebsd.c.orig	2018-12-02 11:34:59.000000000 -0800
-+++ src/l2_packet/l2_packet_freebsd.c	2018-12-05 23:18:27.612433000 -0800
+--- src/l2_packet/l2_packet_freebsd.c.orig	2022-01-16 12:51:29.000000000 -0800
++++ src/l2_packet/l2_packet_freebsd.c	2022-04-14 07:21:15.259934000 -0700
 @@ -8,7 +8,8 @@
   */
  
@@ -10,3 +10,36 @@
  #include <net/bpf.h>
  #endif /* __APPLE__ */
  #include <pcap.h>
+@@ -76,24 +77,27 @@
+ {
+ 	struct l2_packet_data *l2 = eloop_ctx;
+ 	pcap_t *pcap = sock_ctx;
+-	struct pcap_pkthdr hdr;
++	struct pcap_pkthdr *hdr;
+ 	const u_char *packet;
+ 	struct l2_ethhdr *ethhdr;
+ 	unsigned char *buf;
+ 	size_t len;
+ 
+-	packet = pcap_next(pcap, &hdr);
++	if (pcap_next_ex(pcap, &hdr, &packet) == -1) {
++		wpa_printf(MSG_ERROR, "Error reading packet, has device disappeared?");
++		eloop_terminate();
++	}
+ 
+-	if (!l2->rx_callback || !packet || hdr.caplen < sizeof(*ethhdr))
++	if (!l2->rx_callback || !packet || hdr->caplen < sizeof(*ethhdr))
+ 		return;
+ 
+ 	ethhdr = (struct l2_ethhdr *) packet;
+ 	if (l2->l2_hdr) {
+ 		buf = (unsigned char *) ethhdr;
+-		len = hdr.caplen;
++		len = hdr->caplen;
+ 	} else {
+ 		buf = (unsigned char *) (ethhdr + 1);
+-		len = hdr.caplen - sizeof(*ethhdr);
++		len = hdr->caplen - sizeof(*ethhdr);
+ 	}
+ 	l2->rx_callback(l2->rx_callback_ctx, ethhdr->h_source, buf, len);
+ }
