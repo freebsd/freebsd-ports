958546bf98ff43aa07db9f195c4a2dc6bbdf74d3
Dima Panov <fluffy@FreeBSD.org>
2022-04-14 17:10:46 +0300

    graphics/openimageio: update to 2.3.14.0 release
    While here, unbreak build with py311
    
    ChangeLog:	https://github.com/OpenImageIO/oiio/releases/tag/v2.3.13.0
    		https://github.com/OpenImageIO/oiio/releases/tag/v2.3.14.0
    
    Approved by:	maintainer (implicit)

--- graphics/openimageio/files/patch-src_cmake_modules_FindPtex.cmake
+++ graphics/openimageio/files/patch-src_cmake_modules_FindPtex.cmake
@@ -1,10 +1,10 @@
---- src/cmake/modules/FindPtex.cmake.orig	2021-07-01 18:54:28 UTC
+--- src/cmake/modules/FindPtex.cmake.orig	2022-02-27 08:12:20 UTC
 +++ src/cmake/modules/FindPtex.cmake
 @@ -70,6 +70,7 @@ else ()
          PATHS
              /usr/include
              /usr/local/include
-+            %%LOCALBASE%%/include/ptex
++            /usr/local/include/ptex
              DOC "The directory where Ptexture.h resides")
      find_library( PTEX_LIBRARY
          NAMES
