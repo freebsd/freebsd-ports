14203704214f2ce60b5d95e39692560dd75d5e4f
Robert Nagy <robert@openbsd.org>
2022-04-15 20:43:13 +0200

    www/iridium: set SO_REUSEPORT on UDP multicast and broadcast sockets

--- www/iridium/files/patch-net_socket_udp__socket__posix.cc
+++ www/iridium/files/patch-net_socket_udp__socket__posix.cc
@@ -1,4 +1,4 @@
---- net/socket/udp_socket_posix.cc.orig	2022-04-01 07:48:30 UTC
+--- net/socket/udp_socket_posix.cc.orig	2022-04-15 05:34:28 UTC
 +++ net/socket/udp_socket_posix.cc
 @@ -574,7 +574,7 @@ int UDPSocketPosix::SetDoNotFragment() {
  }
@@ -9,3 +9,12 @@
    if (confirm) {
      sendto_flags_ |= MSG_CONFIRM;
    } else {
+@@ -595,7 +595,7 @@ int UDPSocketPosix::SetBroadcast(bool broadcast) {
+   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+   int value = broadcast ? 1 : 0;
+   int rv;
+-#if BUILDFLAG(IS_APPLE)
++#if BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_BSD)
+   // SO_REUSEPORT on OSX permits multiple processes to each receive
+   // UDP multicast or broadcast datagrams destined for the bound
+   // port.
