68f3c2ea05f7498ae188bdf35bef1b96abe2e1d0
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-04-18 07:42:59 +0800

    sysutils/gdisk: Update to 1.0.9
    - Reformat pkg-descr
    - Update WWW
    
    Changes:	https://www.rodsbooks.com/gdisk/revisions.html

--- sysutils/gdisk/files/patch-parttypes.h
+++ sysutils/gdisk/files/patch-parttypes.h
@@ -1,12 +1,13 @@
---- parttypes.h.orig	2021-01-13 22:14:27 UTC
+--- parttypes.h.orig	2022-04-14 23:17:12 UTC
 +++ parttypes.h
-@@ -16,6 +16,9 @@
- #include "guid.h"
+@@ -15,6 +15,10 @@
+ #define UnicodeString std::string
+ #endif
  
- using namespace std;
 +#ifdef USE_UTF16
 +using namespace icu;
 +#endif
- 
++
  // A partition type
  struct AType {
+    // I'm using a custom 16-bit extension of the original MBR 8-bit
