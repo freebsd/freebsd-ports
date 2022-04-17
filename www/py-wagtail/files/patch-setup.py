fbc123d75218b19115f38da6f309e20ff6d419ec
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-04-18 07:45:52 +0800

    www/py-wagtail: Update to 2.16.2
    - Change RUN_DEPENDS from py-beautifulsoup to py-beautifulsoup449
    
    Changes:	https://github.com/wagtail/wagtail/releases
    		https://docs.wagtail.org/en/latest/releases/index.html

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,20 +1,18 @@
---- setup.py.orig	2022-02-11 13:47:04 UTC
+--- setup.py.orig	2022-04-11 15:28:13 UTC
 +++ setup.py
-@@ -21,14 +21,14 @@ except ImportError:
+@@ -21,12 +21,12 @@ except ImportError:
  
  install_requires = [
      "Django>=3.2,<4.1",
 -    "django-modelcluster>=5.2,<6.0",
 +    "django-modelcluster>=5.2,<6.1",
      "django-taggit>=2.0,<3.0",
-     "django-treebeard>=4.2.0,<5.0,!=4.5",
+-    "django-treebeard>=4.5.1,<5.0",
++    "django-treebeard>=4.4.0,<5.0",
      "djangorestframework>=3.11.1,<4.0",
      "django-filter>=2.2,<22",
 -    "draftjs_exporter>=2.1.5,<3.0",
 +    "draftjs_exporter>=2.1.5",
      "Pillow>=4.0.0,<10.0.0",
--    "beautifulsoup4>=4.8,<4.10",
-+    "beautifulsoup4>=4.8,<4.11",
+     "beautifulsoup4>=4.8,<4.10",
      "html5lib>=0.999,<2",
-     "Willow>=1.4,<1.5",
-     "requests>=2.11.1,<3.0",
