9b89eb4df672f3909b54329f194f1d18d1c2f99b
Yuri Victorovich <yuri@FreeBSD.org>
2022-04-17 16:11:03 -0700

    audio/vst3sdk: Update 3.7.1_build_50-1 -> 3.7.4_build_25

--- audio/vst3sdk/files/patch-cmake_modules_SMTG__PlatformToolset.cmake
+++ audio/vst3sdk/files/patch-cmake_modules_SMTG__PlatformToolset.cmake
@@ -1,11 +1,11 @@
---- cmake/modules/SMTG_PlatformToolset.cmake.orig	2021-03-22 20:22:49 UTC
+--- cmake/modules/SMTG_PlatformToolset.cmake.orig	2021-12-16 12:29:25 UTC
 +++ cmake/modules/SMTG_PlatformToolset.cmake
-@@ -54,7 +54,7 @@ macro(smtg_setup_platform_toolset)
+@@ -60,7 +60,7 @@ macro(smtg_setup_platform_toolset)
                      set(CMAKE_ANDROID_STL_TYPE c++_static)
                      link_libraries(dl)
                   else()
 -                    link_libraries(stdc++fs pthread dl)
 +                    link_libraries(pthread dl)
-                 endif()
-             endif()
-         endif()
+                 endif(ANDROID)
+             endif(SMTG_MAC)
+         endif(XCODE)
--- audio/vst3sdk/files/patch-vstgui4_vstgui_lib_platform_linux_x11fileselector.cpp
+++ audio/vst3sdk/files/patch-vstgui4_vstgui_lib_platform_linux_x11fileselector.cpp
@@ -1,10 +1,10 @@
 - fix missing include, see https://github.com/steinbergmedia/vst3sdk/issues/80
 
---- vstgui4/vstgui/lib/platform/linux/x11fileselector.cpp.orig	2020-08-20 14:14:27 UTC
+--- vstgui4/vstgui/lib/platform/linux/x11fileselector.cpp.orig	2021-12-07 16:20:00 UTC
 +++ vstgui4/vstgui/lib/platform/linux/x11fileselector.cpp
-@@ -6,6 +6,8 @@
- #include <unistd.h>
- #include <string>
+@@ -14,6 +14,8 @@
+ #include <cassert>
+ extern "C" { extern char **environ; }
  
 +#include <stdio.h>
 +
