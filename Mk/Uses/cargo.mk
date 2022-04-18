85a16842b1c5958428b6c39582630545ee481906
Piotr Kubaj <pkubaj@FreeBSD.org>
2022-04-18 23:32:13 +0200

    Mk/Uses/cargo.mk: set correct target CPU on powerpc

--- Mk/Uses/cargo.mk
+++ Mk/Uses/cargo.mk
@@ -140,7 +140,7 @@ CARGO_ENV+=	RUST_BACKTRACE=1
 # Adjust -C target-cpu if -march/-mcpu is set by bsd.cpu.mk
 .if ${ARCH} == amd64 || ${ARCH} == i386
 RUSTFLAGS+=	${CFLAGS:M-march=*:S/-march=/-C target-cpu=/}
-.elif ${ARCH:Mpowerpc64*}
+.elif ${ARCH:Mpowerpc*}
 RUSTFLAGS+=	${CFLAGS:M-mcpu=*:S/-mcpu=/-C target-cpu=/:S/power/pwr/}
 .else
 RUSTFLAGS+=	${CFLAGS:M-mcpu=*:S/-mcpu=/-C target-cpu=/}
