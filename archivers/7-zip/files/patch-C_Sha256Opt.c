93a0e053b7f8dd451ae4b4ac377f20a45e8df188
Max Brazhnikov <makc@FreeBSD.org>
2022-04-18 14:40:48 +0300

    archivers/7-zip: fix build on armv6/armv7
    - Extend HWCAP detection patch to use correct auxvals on armv6/armv7.
    - Disable optimised AES, CRC, SHA-1, and SHA-256 kernels on armv6/armv7
      as they don't build with base clang.
    
    PR:		263244
    Submitted by:	Robert Clausecker
    
    (cherry picked from commit 96151ece4c41ec9a060a5347704f1a51323dd57f)

---
+++ archivers/7-zip/files/patch-C_Sha256Opt.c
@@ -0,0 +1,11 @@
+--- C/Sha256Opt.c.orig	2022-04-12 16:00:07 UTC
++++ C/Sha256Opt.c
+@@ -212,7 +212,7 @@ void MY_FAST_CALL Sha256_UpdateBlocks_HW(UInt32 state[
+ 
+ #endif // USE_HW_SHA
+ 
+-#elif defined(MY_CPU_ARM_OR_ARM64)
++#elif defined(MY_CPU_ARM64)
+ 
+   #if defined(__clang__)
+     #if (__clang_major__ >= 8) // fix that check
