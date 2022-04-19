9e1a841eb1cd21c5b790d628a4adf230ba99e972
Bruce Richardson <bruce.richardson@intel.com>
2022-04-19 17:04:01 +0100

    net/dpdk: update to 19.11.12 point release
    PR:	263423

--- net/dpdk/files/patch-lib_librte__eal_freebsd_eal_include_rte__os.h
+++ net/dpdk/files/patch-lib_librte__eal_freebsd_eal_include_rte__os.h
@@ -1,10 +1,9 @@
---- lib/librte_eal/freebsd/eal/include/rte_os.h.orig	2021-03-17 16:43:15 UTC
+--- lib/librte_eal/freebsd/eal/include/rte_os.h.orig	2021-03-08 17:40:08 UTC
 +++ lib/librte_eal/freebsd/eal/include/rte_os.h
-@@ -14,8 +14,30 @@
+@@ -14,6 +14,28 @@
  #include <pthread_np.h>
- 
+
  typedef cpuset_t rte_cpuset_t;
--#define RTE_CPU_AND(dst, src1, src2) do \
 +
 +/* FreeBSD 14 uses GLIBC compatible CPU_AND, CPU_OR, ... */
 +#ifdef CPU_ALLOC
@@ -12,7 +11,7 @@
 +#define RTE_CPU_AND(dst, src1, src2) CPU_AND(dst, src1, src2)
 +#define RTE_CPU_OR(dst, src1, src2) CPU_OR(dst, src1, src2)
 +#define RTE_CPU_FILL(set) do \
- { \
++{ \
 +	unsigned int i; \
 +	CPU_ZERO(set); \
 +	for (i = 0; i < CPU_SETSIZE; i++) \
@@ -27,35 +26,16 @@
 +
 +#else
 +
-+#define RTE_CPU_AND(dst, src1, src2) do		\
-+{ \
- 	cpuset_t tmp; \
- 	CPU_COPY(src1, &tmp); \
- 	CPU_AND(&tmp, src2); \
-@@ -29,6 +51,9 @@ typedef cpuset_t rte_cpuset_t;
- 	CPU_COPY(&tmp, dst); \
- } while (0)
- #define RTE_CPU_FILL(set) CPU_FILL(set)
-+
-+/* In FreeBSD 13 CPU_NAND macro is CPU_ANDNOT */
-+#ifdef CPU_NAND
- #define RTE_CPU_NOT(dst, src) do \
+ #define RTE_CPU_AND(dst, src1, src2) do \
  { \
  	cpuset_t tmp; \
-@@ -36,5 +61,16 @@ typedef cpuset_t rte_cpuset_t;
- 	CPU_NAND(&tmp, src); \
+@@ -47,6 +69,8 @@ typedef cpuset_t rte_cpuset_t;
+ 	CPU_ANDNOT(&tmp, src); \
  	CPU_COPY(&tmp, dst); \
  } while (0)
-+#else
-+#define RTE_CPU_NOT(dst, src) do \
-+{ \
-+	cpuset_t tmp; \
-+	CPU_FILL(&tmp); \
-+	CPU_ANDNOT(&tmp, src); \
-+	CPU_COPY(&tmp, dst); \
-+} while (0)
+-#endif
 +#endif /* CPU_NAND */
 +
 +#endif /* CPU_ALLOC */
- 
+
  #endif /* _RTE_OS_H_ */
