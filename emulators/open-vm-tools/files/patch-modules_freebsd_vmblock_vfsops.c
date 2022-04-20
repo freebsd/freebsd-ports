30dfe4eba542da9acfe9e6a6d738f1a24ec5a5b4
Renato Botelho <garga@FreeBSD.org>
2022-04-20 11:54:12 -0300

    emulators/open-vm-tools: Fix build on current
    Since 8b83d7e0ee544, -Wunused-but-set-variable became a fatal error on
    CURRENT.  Remove a couble of set-but-not-used variables and fix the
    build
    
    Sponsored by:	Rubicon Communications, LLC ("Netgate")

--- emulators/open-vm-tools/files/patch-modules_freebsd_vmblock_vfsops.c
+++ emulators/open-vm-tools/files/patch-modules_freebsd_vmblock_vfsops.c
@@ -1,18 +1,6 @@
---- modules/freebsd/vmblock/vfsops.c.orig	2021-09-24 04:19:18 UTC
+--- modules/freebsd/vmblock/vfsops.c.orig	2022-03-04 21:01:24 UTC
 +++ modules/freebsd/vmblock/vfsops.c
-@@ -124,6 +124,11 @@ VMBlockVFSMount(struct mount *mp,        // IN: mount(
-    char *pathname;
-    int len, error = 0;
- 
-+#if __FreeBSD_version >= 800087
-+   struct thread *td;
-+   td = curthread;
-+#endif
-+
-    VMBLOCKDEBUG("VMBlockVFSMount(mp = %p)\n", (void *)mp);
- 
-    /*
-@@ -171,14 +176,22 @@ VMBlockVFSMount(struct mount *mp,        // IN: mount(
+@@ -171,14 +171,22 @@ VMBlockVFSMount(struct mount *mp,        // IN: mount(
      * Find lower node and lock if not already locked.
      */
  
@@ -35,15 +23,3 @@
  
     /*
      * Check multi VMBlock mount to avoid `lock against myself' panic.
-@@ -276,6 +289,11 @@ VMBlockVFSUnmount(struct mount *mp,    // IN: filesyst
-    void *mntdata;
-    int error;
-    int flags = 0, removed = 0;
-+
-+#if __FreeBSD_version >= 800087
-+   struct thread *td;
-+   td = curthread;
-+#endif
- 
-    VMBLOCKDEBUG("VMBlockVFSUnmount: mp = %p\n", (void *)mp);
- 
