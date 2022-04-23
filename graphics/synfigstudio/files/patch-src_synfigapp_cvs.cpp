020c244359926d9f080a5e0c4eec8d4796255dc4
Chris Hutchinson <portmaster@bsdforge.com>
2022-04-23 12:36:27 -0700

    graphics/synfigstudio: Update to 1.4.2
    PR:	263462

--- graphics/synfigstudio/files/patch-src_synfigapp_cvs.cpp
+++ graphics/synfigstudio/files/patch-src_synfigapp_cvs.cpp
@@ -1,6 +1,6 @@
---- src/synfigapp/cvs.cpp.orig	2015-03-28 10:15:01 UTC
-+++ src/synfigapp/cvs.cpp
-@@ -62,7 +62,7 @@ using namespace synfigapp;
+--- src/synfigapp/cvs.cpp.orig	2022-04-20 22:29:43.537492000 -0700
++++ src/synfigapp/cvs.cpp	2022-04-20 22:30:03.473447000 -0700
+@@ -62,7 +62,7 @@
  #define HAVE_STRPTIME
  #endif
  
