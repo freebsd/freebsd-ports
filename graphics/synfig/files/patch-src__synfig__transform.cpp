c484be0f2e085c534aa6be8a2394a46b464a33f0
Chris Hutchinson <portmaster@bsdforge.com>
2022-04-23 12:36:01 -0700

    graphics/synfig: Update to 1.4.2
    PR:	263461

--- graphics/synfig/files/patch-src__synfig__transform.cpp
+++ graphics/synfig/files/patch-src__synfig__transform.cpp
@@ -1,5 +1,5 @@
---- src/synfig/transform.cpp.orig	2015-03-28 10:15:01 UTC
-+++ src/synfig/transform.cpp
+--- src/synfig/transform.cpp.orig	2022-04-20 19:16:57.130395000 -0700
++++ src/synfig/transform.cpp	2022-04-20 19:17:00.690569000 -0700
 @@ -31,6 +31,7 @@
  
  #include "transform.h"
