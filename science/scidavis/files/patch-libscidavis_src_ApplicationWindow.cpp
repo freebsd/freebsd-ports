94253f4442598128eb0d27ae175b05312d2a7f42
Max Brazhnikov <makc@FreeBSD.org>
2022-04-24 15:23:10 +0300

    science/scidavis: Update to 2.9.2

--- science/scidavis/files/patch-libscidavis_src_ApplicationWindow.cpp
+++ science/scidavis/files/patch-libscidavis_src_ApplicationWindow.cpp
@@ -1,6 +1,6 @@
---- libscidavis/src/ApplicationWindow.cpp.orig	2021-05-05 01:51:49 UTC
+--- libscidavis/src/ApplicationWindow.cpp.orig	2022-04-16 07:39:19 UTC
 +++ libscidavis/src/ApplicationWindow.cpp
-@@ -4200,7 +4200,7 @@ void ApplicationWindow::readSettings()
+@@ -4201,7 +4201,7 @@ void ApplicationWindow::readSettings()
      settings.endGroup(); // Colors
  
      settings.beginGroup("/Paths");
@@ -9,7 +9,7 @@
      helpFilePath = settings.value("/HelpFile", "").toString();
  #ifdef PLUGIN_PATH
      QString defaultFitPluginsPath = PLUGIN_PATH;
-@@ -8062,6 +8062,10 @@ void ApplicationWindow::showWindowTitleBarMenu()
+@@ -8063,6 +8063,10 @@ void ApplicationWindow::showWindowTitleBarMenu()
  void ApplicationWindow::chooseHelpFolder()
  {
  // TODO: move all paths & location handling to anothor class
@@ -20,7 +20,7 @@
  #if defined(Q_OS_WIN)
      const QString locateDefaultHelp =
              qApp->applicationDirPath() + QDir::toNativeSeparators("/manual/index.html");
-@@ -8069,6 +8073,8 @@ void ApplicationWindow::chooseHelpFolder()
+@@ -8070,6 +8074,8 @@ void ApplicationWindow::chooseHelpFolder()
      const QString locateDefaultHelp =
              QDir::toNativeSeparators("/usr/share/doc/scidavis/manual/index.html");
  #endif
