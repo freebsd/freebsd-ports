aa99d776c0e0edde6c799ca8936bcf214ab21bd1
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-04-26 22:53:44 +0800

    devel/py-typing-extensions: Update to 4.2.0
    Changes:	https://github.com/python/typing/blob/master/typing_extensions/CHANGELOG.md

--- devel/py-typing-extensions/files/setup.py
+++ devel/py-typing-extensions/files/setup.py
@@ -8,11 +8,11 @@ package_dir = \
 
 setup(name='typing_extensions',
       version='%%PORTVERSION%%',
-      description='Backported and Experimental Type Hints for Python 3.6+',
+      description='Backported and Experimental Type Hints for Python 3.7+',
       author=None,
       author_email='"Guido van Rossum, Jukka Lehtosalo, Łukasz Langa, Michael Lee" <levkivskyi@gmail.com>',
       url=None,
       py_modules=['typing_extensions'],
       package_dir=package_dir,
-      python_requires='>=3.6',
+      python_requires='>=3.7',
      )
