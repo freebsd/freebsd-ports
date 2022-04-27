801fb1d508f8ee4280cbaabd6ab6244ca232c472
Alessandro Sagratini <alessandro.sagratini@gmail.com>
2022-04-27 09:37:48 +0000

    Updating devel/py-oci to 2.65.0 and devel/oci-cli to 3.8.0

--- devel/oci-cli/files/patch-setup.py
+++ devel/oci-cli/files/patch-setup.py
@@ -1,11 +1,17 @@
---- setup.py.orig	2022-04-04 18:26:03 UTC
+--- setup.py.orig	2022-04-25 17:10:49 UTC
 +++ setup.py
-@@ -30,7 +30,7 @@ with open_relative("README.rst") as f:
- 
- requires = [
-     'oci==2.62.1',
--    'arrow>=1.0.0',
-+    'arrow>>=1.0.0',
+@@ -34,12 +34,12 @@ requires = [
      'certifi',
      'click==7.1.2',
      'cryptography>=3.2.1,<=3.4.7',
+-    'jmespath==0.10.0',
++    'jmespath>=0.10.0',
+     'python-dateutil>=2.5.3,<3.0.0',
+     'pytz>=2016.10',
+     'six>=1.15.0',
+-    'terminaltables==3.1.0',
+-    'pyOpenSSL==19.1.0',
++    'terminaltables>=3.1.0',
++    'pyOpenSSL>=19.1.0',
+     'PyYAML>=5.4,<6'
+ ]
