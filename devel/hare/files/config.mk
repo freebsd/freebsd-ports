998f34001daeb28c5c5d246564b5efb7bc9731ca
Stefan Eßer <se@FreeBSD.org>
2022-04-30 12:27:46 +0200

    devel/hare: add new port for the Hare build system and std library
    This is the build system for Hare language projects.
    
    The compiler currently supports amd64, aarch64, and riscv64, but the
    port is currently only provided for amd64 for lack of testing on other
    FreeBSD architectures.

---
+++ devel/hare/files/config.mk
@@ -0,0 +1,32 @@
+## Install configuration
+
+#PREFIX = /usr
+BINDIR = $(PREFIX)/bin
+MANDIR = $(PREFIX)/man
+DATADIR= $(PREFIX)/share/hare/
+SRCDIR = $(DATADIR)/src
+
+# Where to install the stdlib tree
+STDLIB = $(SRCDIR}/stdlib
+
+# Default HAREPATH
+LOCALSRCDIR = ${SRCDIR}/local-src
+HAREPATH = $(LOCALSRCDIR)/stdlib:$(LOCALSRCDIR)/third-party:$(DATADIR)/stdlib:$(DATADIR)/third-party
+
+## Build configuration
+
+# Platform to build for
+PLATFORM = freebsd
+#ARCH = x86_64
+
+# External tools and flags
+HAREC = harec
+HAREFLAGS =
+QBE = qbe
+AS = as
+LD = ld
+AR = ar
+SCDOC = scdoc
+
+# Where to store build artifacts
+HARECACHE = .cache
