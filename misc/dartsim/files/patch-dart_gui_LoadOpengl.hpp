6f1f155d61345e5d5178a35e3f411e83bd7c3017
Yuri Victorovich <yuri@FreeBSD.org>
2022-05-01 15:18:52 -0700

    misc/dartsim: Update 6.11.1 -> 6.12.1
    Reported by:	portscout

--- misc/dartsim/files/patch-dart_gui_LoadOpengl.hpp
+++ misc/dartsim/files/patch-dart_gui_LoadOpengl.hpp
@@ -1,12 +1,12 @@
---- dart/gui/LoadOpengl.hpp.orig	2021-07-15 08:12:45 UTC
+--- dart/gui/LoadOpengl.hpp.orig	2021-11-04 20:43:53 UTC
 +++ dart/gui/LoadOpengl.hpp
 @@ -46,6 +46,9 @@
  #elif defined(__linux__)
- #  include <GL/gl.h>
- #  include <GL/glu.h>
+   #include <GL/gl.h>
+   #include <GL/glu.h>
 +#elif defined(__FreeBSD__)
-+#  include <GL/gl.h>
-+#  include <GL/glu.h>
++  #include <GL/gl.h>
++  #include <GL/glu.h>
  #elif defined(__APPLE__)
- #  include <OpenGL/gl.h>
- #  include <OpenGL/glu.h>
+   #include <OpenGL/gl.h>
+   #include <OpenGL/glu.h>
