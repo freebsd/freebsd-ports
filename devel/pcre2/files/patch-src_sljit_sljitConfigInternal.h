4a3d3aa4748e76ab3e01b3e32fdf513e6c7554b4
Kirill Ponomarev <krion@FreeBSD.org>
2022-05-02 17:31:35 +0200

    devel/pcre2: Update to 10.40

--- devel/pcre2/files/patch-src_sljit_sljitConfigInternal.h
+++ devel/pcre2/files/patch-src_sljit_sljitConfigInternal.h
@@ -1,11 +1,11 @@
---- src/sljit/sljitConfigInternal.h.orig	2022-03-25 09:54:56 UTC
+--- src/sljit/sljitConfigInternal.h.orig	2022-05-02 15:22:09 UTC
 +++ src/sljit/sljitConfigInternal.h
-@@ -283,7 +283,7 @@
- /* Instruction cache flush. */
- /****************************/
- 
+@@ -333,7 +333,7 @@ extern "C" {
+  * beware APPLE is known to have removed the code in iOS so
+  * it will need to be excempted or result in broken builds
+  */
 -#if (!defined SLJIT_CACHE_FLUSH && defined __has_builtin)
 +#if (!defined SLJIT_CACHE_FLUSH && defined __has_builtin && !defined SLJIT_CONFIG_PPC_32)
- #if __has_builtin(__builtin___clear_cache)
+ #if __has_builtin(__builtin___clear_cache) && !defined(__clang__)
  
- #define SLJIT_CACHE_FLUSH(from, to) \
+ /*
