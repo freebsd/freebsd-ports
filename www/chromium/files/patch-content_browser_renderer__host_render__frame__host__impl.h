2c5fe353ed15f8a3e9ed97fdd6c28299116e76db
Robert Nagy <robert@openbsd.org>
2022-05-03 06:44:26 +0200

    www/chromium: update to 101.0.4951.54
    (cherry picked from commit 1821f674e65dd3bf1d992e1908368e4d3d97bfa8)

--- www/chromium/files/patch-content_browser_renderer__host_render__frame__host__impl.h
+++ www/chromium/files/patch-content_browser_renderer__host_render__frame__host__impl.h
@@ -1,6 +1,6 @@
---- content/browser/renderer_host/render_frame_host_impl.h.orig	2022-04-21 18:48:31 UTC
+--- content/browser/renderer_host/render_frame_host_impl.h.orig	2022-05-03 04:41:28 UTC
 +++ content/browser/renderer_host/render_frame_host_impl.h
-@@ -3314,7 +3314,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
+@@ -3301,7 +3301,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
    perfetto::protos::pbzero::RenderFrameHost::LifecycleState
    LifecycleStateToProto();
  
@@ -9,7 +9,7 @@
    void RunScreenAIAnnotator();
  #endif
  
-@@ -4154,7 +4154,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
+@@ -4141,7 +4141,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
    BackForwardCacheDisablingFeaturesCallback
        back_forward_cache_disabling_features_callback_for_testing_;
  
