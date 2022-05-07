b6aecffa15a20221cb45de272ba142e6c2eb03eb
Yuri Victorovich <yuri@FreeBSD.org>
2022-05-07 12:22:51 -0700

    misc/adios2: Update 2.7.1 -> 2.8.0
    Reported by:	portscout

---
+++ misc/adios2/files/patch-cmake_DetectOptions.cmake
@@ -0,0 +1,28 @@
+- fix the python version
+
+--- cmake/DetectOptions.cmake.orig	2022-03-29 11:54:45 UTC
++++ cmake/DetectOptions.cmake
+@@ -312,12 +312,12 @@ if(NOT SHARED_LIBS_SUPPORTED)
+ endif()
+ 
+ if(ADIOS2_USE_Python STREQUAL AUTO)
+-  find_package(Python 3 COMPONENTS Interpreter Development NumPy)
++  find_package(Python ${FREEBSD_PYTHON_VERSION} EXACT COMPONENTS Interpreter Development NumPy)
+   if(Python_FOUND AND ADIOS2_HAVE_MPI)
+     find_package(PythonModule COMPONENTS mpi4py mpi4py/mpi4py.h)
+   endif()
+ elseif(ADIOS2_USE_Python)
+-  find_package(Python 3 REQUIRED COMPONENTS Interpreter Development NumPy)
++  find_package(Python ${FREEBSD_PYTHON_VERSION} EXACT REQUIRED COMPONENTS Interpreter Development NumPy)
+   if(ADIOS2_HAVE_MPI)
+     find_package(PythonModule REQUIRED COMPONENTS mpi4py mpi4py/mpi4py.h)
+   endif()
+@@ -334,7 +334,7 @@ endif()
+ 
+ # Even if no python support, we still want the interpreter for tests
+ if(BUILD_TESTING AND NOT Python_Interpreter_FOUND)
+-  find_package(Python REQUIRED COMPONENTS Interpreter)
++  find_package(Python ${FREEBSD_PYTHON_VERSION} EXACT REQUIRED COMPONENTS Interpreter)
+ endif()
+ 
+ if(Python_Interpreter_FOUND)
