457d3d98e269fb634e2696de67df8bef1070a4b9
Neel Chauhan <nc@FreeBSD.org>
2022-05-06 21:00:35 -0700

    emulators/dosbox-x: Update to 0.83.25

--- emulators/dosbox-x/files/patch-src_gui_sdl__gui.cpp
+++ emulators/dosbox-x/files/patch-src_gui_sdl__gui.cpp
@@ -1,6 +1,6 @@
---- src/gui/sdl_gui.cpp.orig	2022-04-21 18:01:44 UTC
+--- src/gui/sdl_gui.cpp.orig	2022-05-01 06:25:42 UTC
 +++ src/gui/sdl_gui.cpp
-@@ -3022,7 +3022,7 @@ class ConfigurationWindow : public GUI::ToplevelWindow
+@@ -3059,7 +3059,7 @@ class ConfigurationWindow : public GUI::ToplevelWindow
              std::string url = "https://dosbox-x.com/";
  #if defined(WIN32)
              ShellExecute(NULL, "open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
@@ -9,19 +9,25 @@
              system(("xdg-open "+url).c_str());
  #elif defined(MACOSX)
              system(("open "+url).c_str());
-@@ -3436,7 +3436,7 @@ void GUI_Shortcut(int select) {
+@@ -3475,7 +3475,11 @@ void GUI_Shortcut(int select) {
          PIC_AddEvent(RunCfgTool, 100);
      } else
  #endif
--    RunCfgTool(NULL);
++#ifdef __FreeBSD__
 +    RunCfgTool(0);
++#else
+     RunCfgTool(NULL);
++#endif
  }
  
  void GUI_Run(bool pressed) {
-@@ -3458,5 +3458,5 @@ void GUI_Run(bool pressed) {
+@@ -3490,5 +3494,9 @@ void GUI_Run(bool pressed) {
          PIC_AddEvent(RunCfgTool, 100);
      } else
  #endif
--    RunCfgTool(NULL);
++#ifdef __FreeBSD__
 +    RunCfgTool(0);
++#else
+     RunCfgTool(NULL);
++#endif
  }
