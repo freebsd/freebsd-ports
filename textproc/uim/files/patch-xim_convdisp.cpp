77267e8a33ff905e2973eb9f75831a80bc7cddc9
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2022-05-08 16:24:16 +0900

    textproc/uim: Update to the snapshot on 2022-03-18
    - Update slave ports of textproc/uim
    - Bump PORTREVISION for the ports that depend on textproc/uim

--- textproc/uim/files/patch-xim_convdisp.cpp
+++ textproc/uim/files/patch-xim_convdisp.cpp
@@ -1,6 +1,6 @@
---- xim/convdisp.cpp.orig	2013-06-30 04:26:09 UTC
+--- xim/convdisp.cpp.orig	2022-03-18 00:54:17 UTC
 +++ xim/convdisp.cpp
-@@ -87,14 +87,14 @@ init_default_xftfont() {
+@@ -81,14 +81,14 @@ init_default_xftfont() {
      gXftFont = XftFontOpen(XimServer::gDpy, DefaultScreen(XimServer::gDpy),
  		    XFT_FAMILY, XftTypeString, fontname,
  		    XFT_PIXEL_SIZE, XftTypeDouble, (double)DEFAULT_FONT_SIZE,
@@ -17,7 +17,7 @@
      }
  }
  
-@@ -112,7 +112,7 @@ update_default_xftfont() {
+@@ -106,7 +106,7 @@ update_default_xftfont() {
  			DefaultScreen(XimServer::gDpy),
  			XFT_FAMILY, XftTypeString, fontname,
  			XFT_PIXEL_SIZE, XftTypeDouble, (double)DEFAULT_FONT_SIZE,
@@ -26,7 +26,7 @@
  	if (xftfont) {
  	    if (gXftFont)
  		XftFontClose(XimServer::gDpy, gXftFont);
-@@ -387,7 +387,7 @@ PeWin::PeWin(Window pw, const char *im_l
+@@ -385,7 +385,7 @@ PeWin::PeWin(Window pw, const char *im_lang, const cha
  			    DefaultScreen(XimServer::gDpy),
  			    XFT_FAMILY, XftTypeString, gXftFontName,
  			    XFT_PIXEL_SIZE, XftTypeDouble, (double)mXftFontSize,
@@ -35,7 +35,7 @@
  	}
  	mXftDraw = XftDrawCreate(XimServer::gDpy, mPixmap,
  			DefaultVisual(XimServer::gDpy, scr_num),
-@@ -557,7 +557,7 @@ void PeWin::set_xftfont(const char *xfld
+@@ -555,7 +555,7 @@ void PeWin::set_xftfont(const char *xfld)
  			    DefaultScreen(XimServer::gDpy),
  			    XFT_FAMILY, XftTypeString, gXftFontName,
  			    XFT_PIXEL_SIZE, XftTypeDouble, (double)size,
