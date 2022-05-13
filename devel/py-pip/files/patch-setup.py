ee3473c33ef5abd7dd898fb025cd7689f5cec8e0
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-05-13 19:35:42 +0800

    devel/py-pip: Update to 22.0.4
    Changes:	https://github.com/pypa/pip/blob/main/NEWS.rst
    		https://pip.pypa.io/en/stable/news/

--- devel/py-pip/files/patch-setup.py
+++ devel/py-pip/files/patch-setup.py
@@ -1,17 +1,15 @@
---- setup.py.orig	2020-08-11 11:26:41 UTC
+--- setup.py.orig	2022-03-06 23:51:55 UTC
 +++ setup.py
-@@ -76,10 +76,10 @@ setup(
+@@ -74,9 +74,9 @@ setup(
+     },
      entry_points={
          "console_scripts": [
-             "pip=pip._internal.cli.main:main",
+-            "pip=pip._internal.cli.main:main",
 -            "pip{}=pip._internal.cli.main:main".format(sys.version_info[0]),
--            "pip{}.{}=pip._internal.cli.main:main".format(
--                *sys.version_info[:2]
--            ),
-+            #"pip{}=pip._internal.cli.main:main".format(sys.version_info[0]),
-+            #"pip{}.{}=pip._internal.cli.main:main".format(
-+            #    *sys.version_info[:2]
-+            #),
+-            "pip{}.{}=pip._internal.cli.main:main".format(*sys.version_info[:2]),
++            # "pip=pip._internal.cli.main:main",
++            # "pip{}=pip._internal.cli.main:main".format(sys.version_info[0]),
++            # "pip{}.{}=pip._internal.cli.main:main".format(*sys.version_info[:2]),
          ],
      },
- 
+     zip_safe=False,
