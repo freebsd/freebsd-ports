8d2283b7663ce9116cd203e5752e89d2d466ca64
Guido Falsi <madpilot@FreeBSD.org>
2022-05-14 00:27:22 +0200

    net/asterisk18: Update to 18.12.0
    - Embedded pjproject updated from 2.10 to 2.12
    - Fix terminfo detection by adding the correct path to detection code [1]
    - Add note to pkg-descr to explain that the misc/terminfo-db port
      is required to get color in the console
    - Remove trailing whitespace
    
    Reported by:	ogogon@ogogon.org (via email) [1]

---
+++ net/asterisk18/files/patch-main_term.c
@@ -0,0 +1,10 @@
+--- main/term.c.orig	2022-04-14 21:53:34 UTC
++++ main/term.c
+@@ -46,6 +46,7 @@ static const char * const termpath[] = {
+ static char quitdata[80] = "";
+ 
+ static const char * const termpath[] = {
++	"%%LOCALBASE%%/share/terminfo",
+ 	"/usr/share/terminfo",
+ 	"/usr/local/share/misc/terminfo",
+ 	"/usr/lib/terminfo",
