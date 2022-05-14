b1e525f8607ab2c9605a68a8208e1c24bf41744a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-05-14 18:24:36 +0800

    devel/py-pip: Restore accidentally-removed bin/pip
    - Bump PORTREVISION for package change

--- devel/py-pip/files/patch-setup.py
+++ devel/py-pip/files/patch-setup.py
@@ -1,15 +1,11 @@
---- setup.py.orig	2022-03-06 23:51:55 UTC
+--- setup.py.orig	2022-05-11 19:03:44 UTC
 +++ setup.py
-@@ -74,9 +74,9 @@ setup(
-     },
+@@ -75,8 +75,6 @@ setup(
      entry_points={
          "console_scripts": [
--            "pip=pip._internal.cli.main:main",
+             "pip=pip._internal.cli.main:main",
 -            "pip{}=pip._internal.cli.main:main".format(sys.version_info[0]),
 -            "pip{}.{}=pip._internal.cli.main:main".format(*sys.version_info[:2]),
-+            # "pip=pip._internal.cli.main:main",
-+            # "pip{}=pip._internal.cli.main:main".format(sys.version_info[0]),
-+            # "pip{}.{}=pip._internal.cli.main:main".format(*sys.version_info[:2]),
          ],
      },
      zip_safe=False,
