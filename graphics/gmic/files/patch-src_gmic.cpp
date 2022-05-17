27089289ec6b94710cc90dffc076cca5bd99c134
Yuri Victorovich <yuri@FreeBSD.org>
2022-05-16 22:20:09 -0700

    graphics/gmic: Update 3.0.1 -> 3.1.2

--- graphics/gmic/files/patch-src_gmic.cpp
+++ graphics/gmic/files/patch-src_gmic.cpp
@@ -1,6 +1,6 @@
---- src/gmic.cpp.orig	2020-06-10 13:43:16 UTC
+--- src/gmic.cpp.orig	2022-05-11 08:15:20 UTC
 +++ src/gmic.cpp
-@@ -2320,7 +2320,7 @@ double gmic::mp_setname(const unsigned int ind, const 
+@@ -2516,7 +2516,7 @@ double gmic::mp_store(const Ts *const ptr, const unsig
  // Manage correspondence between abort pointers and thread ids.
  CImgList<void*> gmic::list_p_is_abort = CImgList<void*>();
  bool *gmic::abort_ptr(bool *const p_is_abort) {
@@ -9,7 +9,7 @@
    void* tid = (void*)(cimg_ulong)getpid();
  #elif cimg_OS==1
    void* tid = (void*)(cimg_ulong)syscall(SYS_gettid);
-@@ -2660,7 +2660,7 @@ gmic::~gmic() {
+@@ -2858,7 +2858,7 @@ gmic::~gmic() {
    cimg::exception_mode(cimg_exception_mode);
    cimg_forX(display_windows,l) delete &display_window(l);
    cimg::mutex(21);
