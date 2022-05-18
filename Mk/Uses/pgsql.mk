5b11f47f0d59c95f6dcf1ff75badede5ec84e72e
Palle Girgensohn <girgen@FreeBSD.org>
2022-05-18 21:11:48 +0200

    databases/postgresql??-*: add postgresql-15 to the ports tree
    Introduce PostgreSQL-15 to the ports tree.
    
    Make version 15 the master port, and add plist parameter for the
    postgresql version.
    
    Release notes:	https://www.postgresql.org/docs/devel/release.html

--- Mk/Uses/pgsql.mk
+++ Mk/Uses/pgsql.mk
@@ -39,7 +39,7 @@ _INCLUDE_USES_PGSQL_MK=	yes
 
 # When adding a version, please keep the comment in
 # Mk/bsd.default-versions.mk in sync.
-VALID_PGSQL_VER=	10 11 12 13 14
+VALID_PGSQL_VER=	10 11 12 13 14 15
 
 # Override non-default LIBVERS like this:
 #PGSQL99_LIBVER=6
