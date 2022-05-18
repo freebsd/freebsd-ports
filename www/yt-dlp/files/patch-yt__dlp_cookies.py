3ac953a950ac0061c20629ce051e80cccd66f94e
Yuri Victorovich <yuri@FreeBSD.org>
2022-05-17 21:30:29 -0700

    www/yt-dlp: Update 2022.04.08 -> 2022.05.18

--- www/yt-dlp/files/patch-yt__dlp_cookies.py
+++ www/yt-dlp/files/patch-yt__dlp_cookies.py
@@ -1,6 +1,6 @@
---- yt_dlp/cookies.py.orig	2022-05-14 11:34:22.365323000 -0400
-+++ yt_dlp/cookies.py	2022-05-14 11:34:52.544340000 -0400
-@@ -140,7 +140,7 @@
+--- yt_dlp/cookies.py.orig	2022-05-18 03:37:32 UTC
++++ yt_dlp/cookies.py
+@@ -156,7 +156,7 @@ def _extract_firefox_cookies(profile, logger):
  
  
  def _firefox_browser_dir():
@@ -8,4 +8,4 @@
 +    if sys.platform in ('linux', 'linux2','freebsd13','freebsd14'):
          return os.path.expanduser('~/.mozilla/firefox')
      elif sys.platform == 'win32':
-         return os.path.expandvars(r'%APPDATA%\Mozilla\Firefox\Profiles')
+         return os.path.expandvars(R'%APPDATA%\Mozilla\Firefox\Profiles')
