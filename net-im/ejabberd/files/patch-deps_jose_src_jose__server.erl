d44192b04ed363bba30bef0cd7ad18efe701730c
Ashish SHUKLA <ashish@FreeBSD.org>
2022-05-19 16:04:27 +0000

    net-im/ejabberd: Update to 22.05
    - Remove unneeded dependencies
    - Add upgrade note to pkg-message
    - Add a patch from upstream for a bug related to packaging

--- net-im/ejabberd/files/patch-deps_jose_src_jose__server.erl
+++ net-im/ejabberd/files/patch-deps_jose_src_jose__server.erl
@@ -1,8 +1,8 @@
 Source: https://github.com/potatosalad/erlang-jose/commit/833f81666a0f9fc31e7a3c2403d72e168a4a3209.patch
 
---- deps/jose/src/jose_server.erl.orig	2018-12-31 15:47:40 UTC
+--- deps/jose/src/jose_server.erl.orig	2020-12-23 13:18:03 UTC
 +++ deps/jose/src/jose_server.erl
-@@ -162,9 +162,7 @@ support_check() ->
+@@ -173,9 +173,7 @@ support_check() ->
  
  %% @private
  check_ec_key_mode(_Fallback, Entries) ->
@@ -13,7 +13,7 @@ Source: https://github.com/potatosalad/erlang-jose/commit/833f81666a0f9fc31e7a3c
  			48,119,2,1,1,4,32,104,152,88,12,19,82,251,156,171,31,222,207,
  			0,76,115,88,210,229,36,106,137,192,81,153,154,254,226,38,247,
  			70,226,157,160,10,6,8,42,134,72,206,61,3,1,7,161,68,3,66,0,4,
-@@ -173,13 +171,23 @@ check_ec_key_mode(_Fallback, Entries) ->
+@@ -184,13 +182,23 @@ check_ec_key_mode(_Fallback, Entries) ->
  			36,225,0,90,21,186,235,132,152,229,13,189,196,121,64,84,64,
  			229,173,12,24,23,127,175,67,247,29,139,91
  		>>,
