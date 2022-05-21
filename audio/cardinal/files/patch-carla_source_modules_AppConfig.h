935a195501a766c32ea67afd44e3b4b387a170c6
Yuri Victorovich <yuri@FreeBSD.org>
2022-05-21 00:23:44 -0700

    audio/cardinal: Update 22.02 -> 22.05

--- audio/cardinal/files/patch-carla_source_modules_AppConfig.h
+++ audio/cardinal/files/patch-carla_source_modules_AppConfig.h
@@ -1,6 +1,6 @@
 - for now treat FreeBSD as Linux for simplicity
 
---- carla/source/modules/AppConfig.h.orig	2022-01-30 04:43:57 UTC
+--- carla/source/modules/AppConfig.h.orig	2022-05-15 13:40:22 UTC
 +++ carla/source/modules/AppConfig.h
 @@ -27,7 +27,7 @@
  # define APPCONFIG_OS_WIN32
@@ -9,5 +9,5 @@
 -#elif defined(__linux__) || defined(__linux)
 +#elif defined(__linux__) || defined(__linux) || defined(__FreeBSD__)
  # define APPCONFIG_OS_LINUX
- #else
- # error Unsupported platform!
+ #elif defined(__FreeBSD__)
+ # define APPCONFIG_OS_FREEBSD
