5b718ffa36da92349f14039f9af5a4c7aa9b7d8e
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-05-22 13:08:55 +0800

    www/py-httpx: Allow build with py-httpcore 0.15.0
    - While I'm here, remove py-rfc3986 patch from setup.py and update RUN_DEPENDS
    - Bump PORTREVISION for package change
    
    Obtained from:	https://github.com/encode/httpx/commit/14a1704be507cf435fa84da75c249c2ecd1c3981

--- www/py-httpx/files/patch-setup.py
+++ www/py-httpx/files/patch-setup.py
@@ -1,14 +1,5 @@
 --- setup.py.orig	2022-01-26 14:49:48 UTC
 +++ setup.py
-@@ -59,7 +59,7 @@ setup(
-         "certifi",
-         "charset_normalizer",
-         "sniffio",
--        "rfc3986[idna2008]>=1.3,<2",
-+        "rfc3986[idna2008]>=1.3",
-         "httpcore>=0.14.5,<0.15.0",
-         "async_generator; python_version < '3.7'"
-     ],
 @@ -72,7 +72,7 @@ setup(
          ],
          "cli": [
