ae17808e4a1b635038b5a80216cbf98bafdbb2ef
Dima Panov <fluffy@FreeBSD.org>
2022-05-22 19:38:37 +0300

    devel/boost-all:	update Boost to 1.79.0 release (+)
    Please welcome a longawaited boost-* upgrade.
    It introduces a full support for OpenSSL3.0, Clang13+, C++17/C++20 standards and more.
    
    Since 1.74 release Boost library introduced some internal changes which breaks comsumers of outdated API.
    All failed consumers will be marked as BROKEN in separated commits.
    
    Special thanks for all community members who take a long survey with real environments testing.
    
    Changelogs:
    https://www.boost.org/users/history/version_1_73_0.html
    https://www.boost.org/users/history/version_1_74_0.html
    https://www.boost.org/users/history/version_1_75_0.html
    https://www.boost.org/users/history/version_1_76_0.html
    https://www.boost.org/users/history/version_1_77_0.html
    https://www.boost.org/users/history/version_1_78_0.html
    https://www.boost.org/users/history/version_1_79_0.html
    
    PR:	246106
    Exp-runs by:	antoine

--- devel/boost-all/common.mk
+++ devel/boost-all/common.mk
@@ -1,12 +1,12 @@
 
-PORTVERSION=	1.72.0
+PORTVERSION=	1.79.0
 PORTVERSUFFIX=	${PORTVERSION:C/\.[0-9]+$//}
 DISTNAME=	boost_${PORTVERSION:S/./_/g}
 
 CATEGORIES=	devel
 MAINTAINER=	office@FreeBSD.org
 
-MASTER_SITES=	https://dl.bintray.com/boostorg/release/${PORTVERSION}/source/ \
+MASTER_SITES=	https://boostorg.jfrog.io/artifactory/main/release/${PORTVERSION}/source/ \
 		SF/boost/boost/${PORTVERSION}
 
 USES+=		tar:bzip2
