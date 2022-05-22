cf66f761bd45f259bba88586f5939c5e6599246a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-05-22 12:22:59 +0800

    devel/py-copier: Update to 6.0.0
    - Update pkg-descr
    - Update WWW
    
    Changes:	https://github.com/copier-org/copier/releases

--- devel/py-copier/files/patch-setup.py
+++ devel/py-copier/files/patch-setup.py
@@ -1,15 +1,11 @@
---- setup.py.orig	2020-08-17 07:46:54 UTC
+--- setup.py.orig	2022-05-15 09:22:56 UTC
 +++ setup.py
-@@ -9,9 +9,9 @@ package_data = \
+@@ -8,7 +8,7 @@ package_data = \
+ {'': ['*']}
  
  install_requires = \
- ['colorama>=0.4.3,<0.5.0',
-- 'jinja2>=2.11.2,<3.0.0',
-- 'packaging>=20.4,<21.0',
-- 'pathspec>=0.8.0,<0.9.0',
-+ 'jinja2>=2.11.2',
-+ 'packaging>=20.4',
-+ 'pathspec>=0.8.0',
-  'plumbum>=1.6.9,<2.0.0',
-  'pydantic>=1.5.1,<2.0.0',
-  'pyyaml-include>=1.2,<2.0',
+-['Jinja2>=3.1.1',
++['Jinja2>=3.0.1',
+  'PyYAML>=5.3.1',
+  'Pygments>=2.7.1',
+  'colorama>=0.4.3',
