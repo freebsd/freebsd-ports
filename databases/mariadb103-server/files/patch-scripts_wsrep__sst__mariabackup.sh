f194935cd9b9c0b5aa196b4e8a0b7fcd1dc3fd65
Bernard Spil <brnrd@FreeBSD.org>
2022-05-24 15:40:12 +0000

    databases/mariadb103-server: Security update to 10.3.35
    PR:		264170
    Submitted by:	Vincent Jancso <vincent jancso outlook com>
    Security:	04fecc47-dad2-11ec-8fbd-d4c9ef517024
    MFH:		2022Q2
    
    (cherry picked from commit 6202520020d9c4f553a2c522a8004521dfa20418)

--- databases/mariadb103-server/files/patch-scripts_wsrep__sst__mariabackup.sh
+++ databases/mariadb103-server/files/patch-scripts_wsrep__sst__mariabackup.sh
@@ -1,11 +1,11 @@
---- scripts/wsrep_sst_mariabackup.sh.orig	2022-02-16 20:18:40 UTC
+--- scripts/wsrep_sst_mariabackup.sh.orig	2022-05-24 14:46:36 UTC
 +++ scripts/wsrep_sst_mariabackup.sh
-@@ -715,7 +715,7 @@ recv_joiner()
+@@ -725,7 +725,7 @@ recv_joiner()
      local ltcmd="$tcmd"
      if [ $tmt -gt 0 ]; then
          if [ -n "$(commandex timeout)" ]; then
--            if timeout --help | grep -qw -- '-k'; then
-+            if timeout --help 2>&1 | grep -qw -- '-k'; then
+-            if timeout --help | grep -qw -F -- '-k'; then
++            if timeout --help 2>&1 | grep -qw -F -- '-k'; then
                  ltcmd="timeout -k $(( tmt+10 )) $tmt $tcmd"
              else
                  ltcmd="timeout -s9 $tmt $tcmd"
