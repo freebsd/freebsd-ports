79ca5f26fcb2a5406d2b71db7483aad7f9fe7be6
Vanilla I. Shu <vanilla@FreeBSD.org>
2022-05-26 20:35:50 +0800

    www/janus: update to 1.0.2.

--- www/janus/files/patch-src__janus.c
+++ www/janus/files/patch-src__janus.c
@@ -1,6 +1,6 @@
---- src/janus.c.orig	2022-03-03 10:44:38 UTC
+--- src/janus.c.orig	2022-05-23 09:14:54 UTC
 +++ src/janus.c
-@@ -4435,6 +4435,7 @@ gint main(int argc, char *argv[])
+@@ -4449,6 +4449,7 @@ gint main(int argc, char *argv[]) {
  				if (exit_on_dl_error)
  					exit(1);
  			} else {
@@ -8,11 +8,11 @@
  				create_l *create = (create_l*) dlsym(event, "create");
  				const char *dlsym_error = dlerror();
  				if (dlsym_error) {
-@@ -5459,6 +5460,7 @@ gint main(int argc, char *argv[])
- 			if (exit_on_dl_error)
+@@ -5474,6 +5475,7 @@ gint main(int argc, char *argv[]) {
  				exit(1);
+ 			}
  		} else {
 +			dlerror();
  			create_p *create = (create_p*) dlsym(plugin, "create");
  			const char *dlsym_error = dlerror();
- 			if (dlsym_error) {
+ 			if(dlsym_error) {
