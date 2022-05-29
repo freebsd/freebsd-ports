3ddbd0b4b93b3631542014b681b67713e97d4cfb
Ashish SHUKLA <ashish@FreeBSD.org>
2022-05-29 09:55:58 +0000

    textproc/meilisearch: Update to 0.27.1

--- textproc/meilisearch/files/patch-meilisearch-http_build.rs
+++ textproc/meilisearch/files/patch-meilisearch-http_build.rs
@@ -1,14 +1,14 @@
---- meilisearch-http/build.rs.orig	2022-01-27 19:17:02 UTC
+--- meilisearch-http/build.rs.orig	2022-05-17 10:09:16 UTC
 +++ meilisearch-http/build.rs
-@@ -19,7 +19,7 @@ mod mini_dashboard {
-     use actix_web_static_files::resource_dir;
+@@ -18,7 +18,7 @@ mod mini_dashboard {
+ 
      use anyhow::Context;
      use cargo_toml::Manifest;
 -    use reqwest::blocking::get;
 +    use std::fs;
      use sha1::{Digest, Sha1};
+     use static_files::resource_dir;
  
-     pub fn setup_mini_dashboard() -> anyhow::Result<()> {
 @@ -51,12 +51,10 @@ mod mini_dashboard {
              }
          }
