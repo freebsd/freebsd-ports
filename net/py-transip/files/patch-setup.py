a0504ed8f952e186fd0fc2cbe1364a313d1be023
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-06-03 18:52:14 +0800

    net/py-transip: Change RUN_DEPENDS from py-suds-jurko to py-suds
    - Bump PORTREVISION for dependency change

---
+++ net/py-transip/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-03-29 13:27:50 UTC
++++ setup.py
+@@ -44,7 +44,7 @@ setup(
+     install_requires=[
+         'requests',
+         'cryptography',
+-        'suds-jurko',
++        'suds',
+     ],
+     classifiers=[
+         'Development Status :: 4 - Beta',
