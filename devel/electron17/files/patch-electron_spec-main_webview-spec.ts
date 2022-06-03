4001d5f952d29085f3130b231ed20e12142eef86
Hiroki Tagato <tagattie@FreeBSD.org>
2022-06-03 15:23:12 +0900

    devel/electron17: update to 17.4.7
    Changelog: https://github.com/electron/electron/releases/tag/v17.4.7

--- devel/electron17/files/patch-electron_spec-main_webview-spec.ts
+++ devel/electron17/files/patch-electron_spec-main_webview-spec.ts
@@ -1,11 +1,11 @@
---- electron/spec-main/webview-spec.ts.orig	2022-05-11 15:32:29 UTC
+--- electron/spec-main/webview-spec.ts.orig	2022-06-01 15:33:02 UTC
 +++ electron/spec-main/webview-spec.ts
-@@ -453,7 +453,7 @@ describe('<webview> tag', function () {
+@@ -456,7 +456,7 @@ describe('<webview> tag', function () {
+     });
  
      // FIXME(zcbenz): Fullscreen events do not work on Linux.
-     // This test is flaky on arm64 macOS.
--    ifit(process.platform !== 'linux' && process.arch !== 'arm64')('exiting fullscreen should unfullscreen window', async () => {
-+    ifit((process.platform !== 'linux' && process.platform !== 'freebsd') && process.arch !== 'arm64')('exiting fullscreen should unfullscreen window', async () => {
+-    ifit(process.platform !== 'linux')('exiting fullscreen should unfullscreen window', async () => {
++    ifit(process.platform !== 'linux' && process.platform !== 'freebsd')('exiting fullscreen should unfullscreen window', async () => {
        const [w, webview] = await loadWebViewWindow();
        const enterFullScreen = emittedOnce(w, 'enter-full-screen');
        await webview.executeJavaScript('document.getElementById("div").requestFullscreen()', true);
