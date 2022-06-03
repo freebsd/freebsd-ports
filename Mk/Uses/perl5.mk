699f62d696da389806fbd5f5bb86b1cc1f29f1ae
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-06-03 16:53:36 +0800

    Mk/Uses/perl5.mk: Remove outdated P5_POD_PARSER
    lang/perl5.30 has been removed in b3fb5d8b3c860bd3c3ebcffafe621aea61379539.

--- Mk/Uses/perl5.mk
+++ Mk/Uses/perl5.mk
@@ -166,12 +166,6 @@ IGNORE= has unknown USE_PERL5 components: ${_USE_PERL5_UNKNOWN}
 
 _USES_POST+=	perl5
 
-.  if   ${PERL_LEVEL} >= 503100
-P5_POD_PARSER=	p5-Pod-Parser>=1.63:textproc/p5-Pod-Parser
-.  else
-P5_POD_PARSER=	
-.  endif
-
 .endif
 
 .if defined(_POSTMKINCLUDED) && !defined(_INCLUDE_USES_PERL5_POST_MK)
