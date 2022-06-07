23941c954fd7a3e7730d499f76f2483c3c93ed1c
Jason W. Bacon <jwb@FreeBSD.org>
2022-06-07 10:54:22 -0500

    deskutils/coregarage: Update to latest commit
    Minor build system updates
    Changes:        https://gitlab.com/cubocore/coreapps/coregarage/-/tags

--- deskutils/coregarage/files/patch-settings.cpp
+++ deskutils/coregarage/files/patch-settings.cpp
@@ -1,11 +1,7 @@
---- settings.cpp.orig	2021-08-19 22:05:32 UTC
+--- settings.cpp.orig	2022-06-04 12:07:40 UTC
 +++ settings.cpp
-@@ -75,13 +75,13 @@ void settings::setDefaultSettings()
-     QFont genFont = QFontDatabase::systemFont(QFontDatabase::GeneralFont);
+@@ -85,10 +85,10 @@ void settings::setDefaultSettings()
      QFont monoFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
-     if ( not genFont.family().count() )
--        genFont = QFont("Cantarell", 9);
-+        genFont = QFont("Cantarell", 12);
  
      if ( not monoFont.family().count() )
 -        monoFont = QFont("monospace", 9);
@@ -16,4 +12,4 @@
 +        monoFont = QFont("monospace", 12);
      }
  
- 	// CoreAction
+ 
