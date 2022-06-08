b60ec1b0ab25f6ad0cb1e5f242f37d306e2bb884
Gleb Popov <arrowd@FreeBSD.org>
2022-06-08 14:28:35 +0300

    graphics/feh: Install manpages into ${LOCALBASE}/share/man.

--- graphics/feh/files/patch-config.mk
+++ graphics/feh/files/patch-config.mk
@@ -1,12 +1,7 @@
 s--- config.mk.orig	2017-12-28 18:28:35 UT2
+--- config.mk.orig	2022-01-03 10:34:57 UTC
 +++ config.mk
-@@ -22,12 +22,12 @@ endif
- 
- # Directories for manuals, executables, docs, data, etc.
- main_dir = ${DESTDIR}${PREFIX}
--man_dir = ${main_dir}/share/man
-+man_dir = ${main_dir}/man
- bin_dir = ${main_dir}/bin
+@@ -28,7 +28,7 @@ font_dir = ${main_dir}/share/feh/fonts
  doc_dir = ${main_dir}/share/doc/feh
  image_dir = ${main_dir}/share/feh/images
  font_dir = ${main_dir}/share/feh/fonts
