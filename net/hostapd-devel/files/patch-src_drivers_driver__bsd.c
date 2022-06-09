c86f32d652eb9dd023049122d8ca37cb13ed07b6
Cy Schubert <cy@FreeBSD.org>
2022-06-09 14:53:18 -0700

    */*: Restore non-IBSS part of wpa patches
    b8477825c2dc42f6c595697a36f593c71f39fbad removed some non-IBSS patches.
    Restore them. We only want to remove the patches that make IBSS use
    ADHOC mode.
    
    Fixes:		b8477825c2dc42f6c595697a36f593c71f39fbad

---
+++ net/hostapd-devel/files/patch-src_drivers_driver__bsd.c
@@ -0,0 +1,23 @@
+--- src/drivers/driver_bsd.c.orig	2021-06-02 14:11:18.000000000 -0700
++++ src/drivers/driver_bsd.c	2021-06-13 23:11:15.089256000 -0700
+@@ -853,14 +853,18 @@
+ 		drv = bsd_get_drvindex(global, ifm->ifm_index);
+ 		if (drv == NULL)
+ 			return;
+-		if ((ifm->ifm_flags & IFF_UP) == 0 &&
+-		    (drv->flags & IFF_UP) != 0) {
++		if (((ifm->ifm_flags & IFF_UP) == 0 ||
++		    (ifm->ifm_flags & IFF_RUNNING) == 0) &&
++		    (drv->flags & IFF_UP) != 0 &&
++		    (drv->flags & IFF_RUNNING) != 0) {
+ 			wpa_printf(MSG_DEBUG, "RTM_IFINFO: Interface '%s' DOWN",
+ 				   drv->ifname);
+ 			wpa_supplicant_event(drv->ctx, EVENT_INTERFACE_DISABLED,
+ 					     NULL);
+ 		} else if ((ifm->ifm_flags & IFF_UP) != 0 &&
+-		    (drv->flags & IFF_UP) == 0) {
++		    (ifm->ifm_flags & IFF_RUNNING) != 0 &&
++		    ((drv->flags & IFF_UP) == 0 ||
++		    (drv->flags & IFF_RUNNING)  == 0)) {
+ 			wpa_printf(MSG_DEBUG, "RTM_IFINFO: Interface '%s' UP",
+ 				   drv->ifname);
