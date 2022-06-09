db83728cb30ebd50576bd2e21091355af44fb041
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-06-09 18:58:07 +0800

    audio/py-audioread: Convert to USE_PYTHON=pytest
    - Remove py-pytest-runner from TEST_DEPENDS

---
+++ audio/py-audioread/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2019-09-01 00:41:41 UTC
++++ setup.py
+@@ -37,7 +37,6 @@ setup(name='audioread',
+       packages=['audioread'],
+ 
+       tests_require=[
+-          'pytest-runner',
+           'pytest'
+       ],
+ 
