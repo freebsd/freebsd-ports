0321fae503000c0b6d3815d518f477d68fa66e24
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2022-06-15 08:43:29 -0500

    net/bctoolbox: Update version 0.6.0=>5.1.38
    - Update MASTER_SITES
    - Change LICENSE from GPLv2+ to GPLv3
    - Take MAINTAINERSHIP
    - Update pkg-descr
    - Update WWW
    
    Relnotes: https://gitlab.linphone.org/BC/public/bctoolbox/-/tags/5.1.38

--- net/bctoolbox/files/patch-src-utils-port.c
+++ net/bctoolbox/files/patch-src-utils-port.c
@@ -1,6 +1,6 @@
---- src/utils/port.c.orig	2017-07-17 12:16:03 UTC
+--- src/utils/port.c.orig	2022-05-27 19:35:26 UTC
 +++ src/utils/port.c
-@@ -1331,7 +1331,7 @@ void bctbx_sockaddr_remove_nat64_mapping(const struct 
+@@ -1438,7 +1438,7 @@ void bctbx_sockaddr_remove_nat64_mapping(const struct 
  		if (htonl(0x0064ff9b) ==
  #ifdef _MSC_VER
  			((in6->sin6_addr.u.Word[0] << 16) & in6->sin6_addr.u.Word[1])
