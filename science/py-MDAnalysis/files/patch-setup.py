40568cac8e3e1931ca48becdfd18e2bf0aab1061
Thierry Thomas <thierry@FreeBSD.org>
2022-06-16 18:10:38 +0200

    science/py-MDAnalysis*: upgrade to 2.2.0
    Version 0.19.2 is too old and does not support Python-3.9.
    
    Remark: threadpoolctl is disabled in this port. See
    <https://github.com/MDAnalysis/mdanalysis/pull/2950> for the impacts on
    performance.
    
    Releases notes at <https://github.com/MDAnalysis/mdanalysis/releases>.
    
    PR:		264716
    Approved by:	yuri (maintainer)

--- science/py-MDAnalysis/files/patch-setup.py
+++ science/py-MDAnalysis/files/patch-setup.py
@@ -1,11 +1,19 @@
---- setup.py.orig	2018-05-13 20:38:06 UTC
+--- setup.py.orig	2022-06-02 18:49:09 UTC
 +++ setup.py
-@@ -253,7 +253,7 @@ def extensions(config):
-     use_cython = config.get('use_cython', default=not is_release)
+@@ -274,7 +274,7 @@ def extensions(config):
+     use_cython = config.get('use_cython', default=cython_found)
      use_openmp = config.get('use_openmp', default=True)
  
--    extra_compile_args = ['-std=c99', '-ffast-math', '-O3', '-funroll-loops']
-+    extra_compile_args = ['-std=c99', '-ffast-math', '-funroll-loops']
+-    extra_compile_args = ['-std=c99', '-ffast-math', '-O3', '-funroll-loops',
++    extra_compile_args = ['-std=c99', '-ffast-math', '-funroll-loops',
+                           '-fsigned-zeros'] # see #2722
      define_macros = []
      if config.get('debug_cflags', default=False):
-         extra_compile_args.extend(['-Wall', '-pedantic'])
+@@ -599,7 +599,6 @@ if __name__ == '__main__':
+           'scipy>=1.5.0',
+           'matplotlib>=1.5.1',
+           'tqdm>=4.43.0',
+-          'threadpoolctl',
+           'packaging',
+           'fasteners',
+           'gsd>=1.9.3',
