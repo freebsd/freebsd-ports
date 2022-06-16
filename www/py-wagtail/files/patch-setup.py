0d8f45d67ee29390d4ed91cc88a7adb8d1492622
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-06-16 22:01:09 +0800

    www/py-wagtail: Update to 3.0
    Changes:	https://github.com/wagtail/wagtail/releases

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,14 +1,7 @@
---- setup.py.orig	2022-04-11 15:28:13 UTC
+--- setup.py.orig	2022-05-16 13:46:09 UTC
 +++ setup.py
-@@ -21,12 +21,12 @@ except ImportError:
- 
- install_requires = [
-     "Django>=3.2,<4.1",
--    "django-modelcluster>=5.2,<6.0",
-+    "django-modelcluster>=5.2,<6.1",
-     "django-taggit>=2.0,<3.0",
--    "django-treebeard>=4.5.1,<5.0",
-+    "django-treebeard>=4.4.0,<5.0",
+@@ -26,7 +26,7 @@ install_requires = [
+     "django-treebeard>=4.5.1,<5.0",
      "djangorestframework>=3.11.1,<4.0",
      "django-filter>=2.2,<22",
 -    "draftjs_exporter>=2.1.5,<3.0",
