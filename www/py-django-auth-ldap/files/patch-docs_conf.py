b21507a27271b7c91d31e2723d558a35ea1864ae
Kai Knoblich <kai@FreeBSD.org>
2022-06-18 10:36:56 +0200

    www/py-django-auth-ldap: Update to 4.1.0
    Changelog:
    
    https://github.com/django-auth-ldap/django-auth-ldap/releases/tag/4.1.0

--- www/py-django-auth-ldap/files/patch-docs_conf.py
+++ www/py-django-auth-ldap/files/patch-docs_conf.py
@@ -2,14 +2,12 @@ Avoid the download of various intersphinx inventories during build.  Only two
 intersphinx mappings are used and the documentation can be built/used without
 them.
 
---- docs/conf.py.orig	2021-02-28 15:46:24 UTC
+--- docs/conf.py.orig	2022-06-12 10:26:47 UTC
 +++ docs/conf.py
-@@ -186,15 +186,3 @@ texinfo_documents = [
- 
- 
+@@ -188,13 +188,3 @@ texinfo_documents = [
  # -- Extension configuration -------------------------------------------------
--
--# -- Options for intersphinx extension ---------------------------------------
+ 
+ # -- Options for intersphinx extension ---------------------------------------
 -
 -intersphinx_mapping = {
 -    "python": ("https://docs.python.org/3/", None),
@@ -17,6 +15,6 @@ them.
 -        "https://docs.djangoproject.com/en/stable/",
 -        "https://docs.djangoproject.com/en/stable/_objects/",
 -    ),
--    "pythonldap": ("https://python-ldap.readthedocs.io/en/latest/", None),
--    "tox": ("https://tox.readthedocs.io/en/latest/", None),
+-    "pythonldap": ("https://www.python-ldap.org/en/latest/", None),
+-    "tox": ("https://tox.wiki/en/latest/", None),
 -}
