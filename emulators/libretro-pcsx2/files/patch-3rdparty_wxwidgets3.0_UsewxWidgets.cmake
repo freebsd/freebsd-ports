84f32169ed730eb0244ef01c59aea892ba1d3895
Timothy Beyer <beyert@cs.ucr.edu>
2022-06-27 15:09:34 +0800

    emulators/libretro-pcsx2: Fix 32-bit / i386 build
    PR:		264219

--- emulators/libretro-pcsx2/files/patch-3rdparty_wxwidgets3.0_UsewxWidgets.cmake
+++ emulators/libretro-pcsx2/files/patch-3rdparty_wxwidgets3.0_UsewxWidgets.cmake
@@ -1,6 +1,13 @@
 --- 3rdparty/wxwidgets3.0/UsewxWidgets.cmake.orig	2020-10-29 23:31:05 UTC
 +++ 3rdparty/wxwidgets3.0/UsewxWidgets.cmake
-@@ -11,7 +11,7 @@ if(UNIX)
+@@ -5,13 +5,13 @@ set(wxWidgets_LIBRARIES wxwidgets)
+ set(wxWidgets_CXX_FLAGS)
+ 
+ include_directories(SYSTEM ${wxWidgets_INCLUDE_DIRS})
+-add_definitions(-DwxUSE_GUI=0 -D_FILE_OFFSET_BITS=64)
++add_definitions(-DwxUSE_GUI=0 -D_FILE_OFFSET_BITS=64 -DwxSIZE_T_IS_UINT)
+ if(UNIX)
+    add_definitions(-DwxUSE_UNIX -D__UNIX__)
     if(APPLE)
        add_definitions(-D__DARWIN__)
     else()
