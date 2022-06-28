aaa201ef81ee4b302c46cd09160b583339c19747
Olivier Cochard <olivier@FreeBSD.org>
2022-06-28 23:15:26 +0200

    net/pkt-gen: fix build on armv6, armv7, i386
    PR:		264561
    Reported by:	Robert Clausecker <fuz@fuz.su>

--- net/pkt-gen/files/patch-apps_nmreplay_nmreplay.c
+++ net/pkt-gen/files/patch-apps_nmreplay_nmreplay.c
@@ -1,6 +1,6 @@
---- apps/nmreplay/nmreplay.c.orig	2021-11-25 23:16:06 UTC
+--- apps/nmreplay/nmreplay.c.orig	2022-02-11 07:45:02 UTC
 +++ apps/nmreplay/nmreplay.c
-@@ -1280,7 +1280,7 @@ main(int argc, char **argv)
+@@ -1273,7 +1273,7 @@ main(int argc, char **argv)
  	D("exiting on abort");
  	sleep(1);
  
