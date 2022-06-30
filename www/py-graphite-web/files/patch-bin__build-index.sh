346864723634e4d90e8c4655bc3ad8bcd1c6cd94
Kai Knoblich <kai@FreeBSD.org>
2022-06-30 08:59:01 +0200

    www/py-graphite-web: Remove port
    * The expiration date has been reached and on the part of upstream there
      isn't yet a new release that works with newer versions of Django.
    
      However, the port could come back again soon during 2022Q3 once it's
      ready for Django 3.2 or newer.

--- www/py-graphite-web/files/patch-bin__build-index.sh
+++
@@ -1,9 +0,0 @@
---- bin/build-index.sh.orig	2018-04-23 17:11:55 UTC
-+++ bin/build-index.sh
-@@ -1,5 +1,5 @@
- #!/usr/local/bin/bash
--export PYTHONPATH="/opt/graphite/webapp/:$PYTHONPATH"
-+export PYTHONPATH="%%PREFIX%%/graphite/webapp/:$PYTHONPATH"
- BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
- 
- ${BINDIR}/build-index
