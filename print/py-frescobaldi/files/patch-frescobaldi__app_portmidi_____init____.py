ccc49ad83534228911e1577e2ffa51ed04d639f2
Martin Neubauer <m.ne@gmx.net>
2022-07-04 07:51:12 +0100

    print/py-frescobaldi: Update to 3.2
    ChangeLog: https://github.com/frescobaldi/frescobaldi/blob/v3.2/ChangeLog
    
    PR:		264988

---
+++ print/py-frescobaldi/files/patch-frescobaldi__app_portmidi_____init____.py
@@ -0,0 +1,20 @@
+--- frescobaldi_app/portmidi/__init__.py.orig	2020-12-26 10:15:52 UTC
++++ frescobaldi_app/portmidi/__init__.py
+@@ -264,16 +264,7 @@ def _setup():
+ 
+     """
+     global pypm
+-    if pypm is None:
+-        for name in try_order:
+-            try:
+-                pypm = globals()['_do_import_' + name]()
+-                break
+-            except ImportError:
+-                continue
+-        else:
+-            pypm = False
+-    return bool(pypm)
++    return False
+ 
+ def _load_module(name):
+     """Loads and returns a single module.
