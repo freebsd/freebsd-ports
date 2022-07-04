e5c64542d1d2c62dac66e92e54d91eb9faaafa6f
Gleb Popov <arrowd@FreeBSD.org>
2022-07-04 19:44:18 +0300

    devel/stack: Update to 2.7.5

--- devel/stack/files/patch-__cabal__deps_cryptonite-0.29_cbits_decaf_include_word.h
+++ devel/stack/files/patch-__cabal__deps_cryptonite-0.29_cbits_decaf_include_word.h
@@ -1,7 +1,7 @@
 https://github.com/haskell-crypto/cryptonite/issues/350
 
---- _cabal_deps/cryptonite-0.29/cbits/decaf/include/word.h.orig	2019-09-08 00:46:48 UTC
-+++ _cabal_deps/cryptonite-0.29/cbits/decaf/include/word.h
+--- _cabal_deps/cryptonite-0.30/cbits/decaf/include/word.h.orig	2019-09-08 00:46:48 UTC
++++ _cabal_deps/cryptonite-0.30/cbits/decaf/include/word.h
 @@ -238,7 +238,7 @@ malloc_vector(size_t size) {
  /* PERF: vectorize vs unroll */
  #ifdef __clang__
