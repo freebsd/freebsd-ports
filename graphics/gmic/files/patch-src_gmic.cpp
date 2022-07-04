11422944c5b58d8e606c63e44bc0475f55df953d
Yuri Victorovich <yuri@FreeBSD.org>
2022-07-04 00:16:19 -0700

    graphics/gmic: Update 3.1.3 -> 3.1.5
    Reported by:	portscout

--- graphics/gmic/files/patch-src_gmic.cpp
+++ graphics/gmic/files/patch-src_gmic.cpp
@@ -1,20 +1,29 @@
---- src/gmic.cpp.orig	2022-05-11 08:15:20 UTC
+--- src/gmic.cpp.orig	2022-06-30 12:35:34 UTC
 +++ src/gmic.cpp
-@@ -2516,7 +2516,7 @@ double gmic::mp_store(const Ts *const ptr, const unsig
- // Manage correspondence between abort pointers and thread ids.
- CImgList<void*> gmic::list_p_is_abort = CImgList<void*>();
- bool *gmic::abort_ptr(bool *const p_is_abort) {
+@@ -2314,7 +2314,7 @@ inline CImgList<void*>& gmic_runs() {
+ }
+ 
+ inline void* get_tid() {
 -#if defined(__MACOSX__) || defined(__APPLE__)
 +#if defined(__MACOSX__) || defined(__APPLE__) || defined(__FreeBSD__)
    void* tid = (void*)(cimg_ulong)getpid();
  #elif cimg_OS==1
    void* tid = (void*)(cimg_ulong)syscall(SYS_gettid);
-@@ -2858,7 +2858,7 @@ gmic::~gmic() {
-   cimg::exception_mode(cimg_exception_mode);
-   cimg_forX(display_windows,l) delete &display_window(l);
-   cimg::mutex(21);
+@@ -2322,7 +2322,7 @@ inline void* get_tid() {
+   void* tid = (void*)(cimg_ulong)GetCurrentThreadId();
+ #else
+   void* tid = (void*)0;
+-#endif // #if defined(__MACOSX__) || defined(__APPLE__)
++#endif // #if defined(__MACOSX__) || defined(__APPLE__) || defined(__FreeBSD__)
+   return tid;
+ }
+ 
+@@ -10762,7 +10762,7 @@ gmic& gmic::_run(const CImgList<char>& commands_line, 
+ #ifdef gmic_is_parallel
+ #ifdef PTHREAD_CANCEL_ENABLE
+ 
 -#if defined(__MACOSX__) || defined(__APPLE__)
 +#if defined(__MACOSX__) || defined(__APPLE__) || defined(__FreeBSD__)
-   void* tid = (void*)(cimg_ulong)getpid();
- #elif cimg_OS==1
-   void* tid = (void*)(cimg_ulong)syscall(SYS_gettid);
+               const cimg_uint64 stacksize = (cimg_uint64)8*1024*1024;
+               pthread_attr_t thread_attr;
+               if (!pthread_attr_init(&thread_attr) && !pthread_attr_setstacksize(&thread_attr,stacksize))
