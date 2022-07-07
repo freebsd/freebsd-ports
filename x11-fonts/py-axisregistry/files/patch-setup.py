fd14dcc9c27daa8d42e357c4851d827b39de2fe3
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-07-08 05:28:03 +0800

    x11-fonts/py-axisregistry: Update to 0.3.5
    Changes:	https://github.com/googlefonts/axisregistry/releases

--- x11-fonts/py-axisregistry/files/patch-setup.py
+++ x11-fonts/py-axisregistry/files/patch-setup.py
@@ -1,10 +1,11 @@
---- setup.py.orig	2022-06-23 17:45:59 UTC
+--- setup.py.orig	2022-07-01 11:49:54 UTC
 +++ setup.py
 @@ -42,6 +42,6 @@ setup(
          "Programming Language :: Python :: 3",
      ],
      python_requires=">=3.7",
 -    setup_requires=["setuptools_scm>=4,<6.1"],
+-    install_requires=["protobuf==3.19.4", "fonttools"],
 +    setup_requires=["setuptools_scm>=4"],
-     install_requires=["protobuf"],
++    install_requires=["protobuf>=3.19.4", "fonttools"],
  )
