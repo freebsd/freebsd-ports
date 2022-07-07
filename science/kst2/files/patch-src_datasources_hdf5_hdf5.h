82633a6049d118789fd7afcb68e95811c4f1c827
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-07-07 20:23:47 +0800

    science/kst2: Fix build with newer HDF5 (1.10.8+ or 1.12.1+)
    (cherry picked from commit 8d1b0443daa1bca4e64b113714f9801e7f0f5a0b)

--- science/kst2/files/patch-src_datasources_hdf5_hdf5.h
+++ science/kst2/files/patch-src_datasources_hdf5_hdf5.h
@@ -1,6 +1,15 @@
---- src/datasources/hdf5/hdf5.h.orig	2021-01-24 05:51:19 UTC
+--- src/datasources/hdf5/hdf5.h.orig	2022-02-16 19:03:30 UTC
 +++ src/datasources/hdf5/hdf5.h
-@@ -18,7 +18,7 @@
+@@ -11,14 +11,14 @@
+  ***************************************************************************/
+ 
+ 
+-#ifndef HDF5_H
+-#define HDF5_H
++#ifndef KST2_HDF5_H
++#define KST2_HDF5_H
+ 
+ #include <datasource.h>
  #include <dataplugin.h>
  
  #include <QFileInfo>
