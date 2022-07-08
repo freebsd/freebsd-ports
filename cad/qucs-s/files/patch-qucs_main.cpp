7609fce6eb9f4ad90a9e871dca3dc8add86c64ce
Yuri Victorovich <yuri@FreeBSD.org>
2022-07-08 00:22:00 -0700

    cad/qucs-s: New port: Quite Universal Circuit Simulator: GUI for circuit simulation kernels

---
+++ cad/qucs-s/files/patch-qucs_main.cpp
@@ -0,0 +1,21 @@
+- this patch works together with post-patch: %%PREFIX%% are substituted there
+
+--- qucs/main.cpp.orig	2022-06-30 18:34:40 UTC
++++ qucs/main.cpp
+@@ -139,13 +139,13 @@ bool loadSettings()
+ #ifdef Q_OS_WIN
+         QucsSettings.XyceExecutable = "Xyce.exe";
+ #else
+-        QucsSettings.XyceExecutable = "/usr/local/Xyce-Release-6.8.0-OPENSOURCE/bin/Xyce";
++        QucsSettings.XyceExecutable = "%%PREFIX%%/bin/Xyce";
+ #endif
+     }
+     if(settings.contains("XyceParExecutable")) QucsSettings.XyceParExecutable = settings.value("XyceParExecutable").toString();
+-    else QucsSettings.XyceParExecutable = "mpirun -np %p /usr/local/Xyce-Release-6.8.0-OPENMPI-OPENSOURCE/bin/Xyce";
++    else QucsSettings.XyceParExecutable = "mpirun -np %p %%PREFIX%%/bin/Xyce";
+     if(settings.contains("SpiceOpusExecutable")) QucsSettings.SpiceOpusExecutable = settings.value("SpiceOpusExecutable").toString();
+-    else QucsSettings.SpiceOpusExecutable = "spiceopus";
++    else QucsSettings.SpiceOpusExecutable = "%%PREFIX%%/bin/spiceopus";
+     if(settings.contains("Nprocs")) QucsSettings.NProcs = settings.value("Nprocs").toInt();
+     else QucsSettings.NProcs = 4;
+     if(settings.contains("S4Q_workdir")) QucsSettings.S4Qworkdir = settings.value("S4Q_workdir").toString();
