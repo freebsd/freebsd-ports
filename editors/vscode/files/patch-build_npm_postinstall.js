5b2cbb7be2922fb35dd827a4a17ab1c04c23db58
Stefan Eßer <se@FreeBSD.org>
2022-07-09 10:51:16 +0900

    editors/vscode: fix build with ${LOCALBASE}/share/.yarnrc existing
    PR:		242936
    Reported by:	bourne.identity@hotmail.com,
    	 	jacques+freebsdbugzilla@foucry.net,
    		meta

--- editors/vscode/files/patch-build_npm_postinstall.js
+++ editors/vscode/files/patch-build_npm_postinstall.js
@@ -1,12 +1,13 @@
 --- build/npm/postinstall.js.orig	2022-06-08 11:20:55 UTC
 +++ build/npm/postinstall.js
-@@ -20,7 +20,8 @@ function yarnInstall(location, opts) {
+@@ -20,7 +20,9 @@ function yarnInstall(location, opts) {
  	const raw = process.env['npm_config_argv'] || '{}';
  	const argv = JSON.parse(raw);
  	const original = argv.original || [];
 -	const args = original.filter(arg => arg === '--ignore-optional' || arg === '--frozen-lockfile' || arg === '--check-files');
 +	const passargs = ['--ignore-optional', '--frozen-lockfile', '--check-files', '--offline', '--no-progress', '--verbose'];
 +	const args = original.filter(arg => passargs.includes(arg));
++	args.push('--use-yarnrc=' + process.env.PWD + '/.yarnrc');
  	if (opts.ignoreEngines) {
  		args.push('--ignore-engines');
  		delete opts.ignoreEngines;
