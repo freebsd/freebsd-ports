dedf16bf59c3a82ad7b886557d685845a5e7b18a
Oleksii Samorukov <samm@FreeBSD.org>
2022-07-12 12:39:03 +0000

    www/trac-devel: fix build with a recent Python version, patch adopted from the upstream
    (cherry picked from commit 038a08951124293872fa3f90afcc8d63c73e7600)

--- www/trac-devel/files/patch-trac_util_html.py
+++ www/trac-devel/files/patch-trac_util_html.py
@@ -1,11 +1,13 @@
 --- trac/util/html.py.orig	2021-05-09 23:08:49 UTC
 +++ trac/util/html.py
-@@ -24,7 +24,7 @@ import sys
- from html import entities
+@@ -25,6 +25,10 @@ from html import entities
  from html.parser import HTMLParser
  
--from markupsafe import Markup, escape as escape_quotes
-+from markupsafe import Markup, escape as escape_quotes, soft_unicode
+ from markupsafe import Markup, escape as escape_quotes
++try:
++    from markupsafe import soft_str as soft_unicode
++except ImportError:
++    from markupsafe import soft_unicode
  
  try:
      from babel.support import LazyProxy
