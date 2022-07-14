1b6930abbd1a04f78fa66cc3b451ea75acff1854
Yuri Victorovich <yuri@FreeBSD.org>
2022-07-14 13:00:00 -0700

    audio/zita-alsa-pcmi: Update 0.4.0 -> 0.5.1

--- audio/zita-alsa-pcmi/files/patch-source_zita-alsa-pcmi.cc
+++ audio/zita-alsa-pcmi/files/patch-source_zita-alsa-pcmi.cc
@@ -1,4 +1,4 @@
---- source/zita-alsa-pcmi.cc.orig	2021-11-26 09:24:45 UTC
+--- source/zita-alsa-pcmi.cc.orig	2022-07-14 07:07:32 UTC
 +++ source/zita-alsa-pcmi.cc
 @@ -18,9 +18,17 @@
  // ----------------------------------------------------------------------------
@@ -18,21 +18,3 @@
  #include <sys/time.h>
  #include "zita-alsa-pcmi.h"
  
-@@ -983,7 +991,7 @@ char *Alsa_pcmi::play_floatre (const float *src, char 
-     while (nfrm--)
-     {
-         d = *src;
--        *((float *) dst) = __bswap_32 (d);
-+        *((float *) dst) = bswap_32 (d);
-         dst += _play_step;
-         src += step;
-     }  
-@@ -1105,7 +1113,7 @@ const char *Alsa_pcmi::capt_floatre (const char *src, 
-     while (nfrm--)
-     {
-         d = *((float *) src);
--        *dst = __bswap_32 (d);
-+        *dst = bswap_32 (d);
-         dst += step;
-         src += _capt_step;
-     }  
