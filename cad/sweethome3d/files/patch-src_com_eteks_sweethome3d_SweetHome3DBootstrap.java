2dd141966bb51e13f8b0ba9601dba4283d7f25c3
Alexey Dokuchaev <danfe@FreeBSD.org>
2022-07-16 05:05:00 +0000

    cad/sweethome3d: update the port to version 7.0 with many improvements
    Read the blog article* to learn about most prominent changes.  As for
    the port itself, addition of YafaRay rendering engine complicates the
    build process because it's not part of the upstream build system (they
    ship precompiled *.so files for MacOS, GNU/Linux, and Windows) and we
    cannot make use of `graphics/yafaray' port because they use some fork
    with a slightly different API.  Drop NO_ARCH because the package now
    contains CPU-specific binary code.
    
    Reported by:	portscout
    
    *) http://www.sweethome3d.com/blog/2022/07/13/sweet_home_3d_7_0.html

--- cad/sweethome3d/files/patch-src_com_eteks_sweethome3d_SweetHome3DBootstrap.java
+++ cad/sweethome3d/files/patch-src_com_eteks_sweethome3d_SweetHome3DBootstrap.java
@@ -1,12 +1,14 @@
---- src/com/eteks/sweethome3d/SweetHome3DBootstrap.java.orig	2020-04-15 10:23:38 UTC
+--- src/com/eteks/sweethome3d/SweetHome3DBootstrap.java.orig	2022-07-13 08:03:06 UTC
 +++ src/com/eteks/sweethome3d/SweetHome3DBootstrap.java
-@@ -116,6 +116,9 @@ public class SweetHome3DBootstrap {
-         // Disable JOGL library loader
-         System.setProperty("jogamp.gluegen.UseTempJarCache", "false");
+@@ -135,6 +135,11 @@ public class SweetHome3DBootstrap {
+             "yafaray/macosx/libyafarayjni.dylib"}));
+         yafarayPluginsFolder = "yafaray/macosx/yafaray-plugins";
        }
 +    } else if ("FreeBSD".equals(operatingSystemName)) {
-+      extensionJarsAndDlls.add(System.getProperty("sun.boot.library.path")
-+        + "/libj3dcore-ogl.so");
++      extensionJarsAndDlls.addAll(Arrays.asList(new String[] {
++        System.getProperty("sun.boot.library.path") + "/libj3dcore-ogl.so",
++        "yafaray/libyafaray_v3_core.so", "yafaray/libyafarayjni.so" }));
++      yafarayPluginsFolder = "yafaray/yafaray-plugins";
      } else { // Other OS
        if ("1.5.2".equals(System.getProperty("com.eteks.sweethome3d.j3d.version", "1.6"))
            || "d3d".equals(System.getProperty("j3d.rend", "jogl"))
