045414389c9a66e77cf8a10eee3683b5f85c52f6
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-07-18 20:34:09 +0800

    devel/py-calver: Update to 2022.6.26
    Changes:	https://github.com/di/calver/releases

--- devel/py-calver/files/patch-setup.py
+++
@@ -1,9 +0,0 @@
---- setup.py.orig	2021-07-30 20:23:43 UTC
-+++ setup.py
-@@ -30,5 +30,5 @@ setup(
-             "use_calver = calver.integration:version",
-         ],
-     },
--    use_calver=True,
-+    version='%%PORTVERSION%%',
- )
