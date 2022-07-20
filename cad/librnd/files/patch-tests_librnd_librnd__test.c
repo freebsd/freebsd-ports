2cca79c47733ee05a2021788cd5b5e7e1a3eddde
Robert Clausecker <fuz@fuz.su>
2022-07-20 22:34:51 +0200

    cad/librnd: update to 3.2.0
    Previous patch in PR was incomplete. This one properly updates the port
    to 3.2.0.
    
    Fixes: 89b6b25d78b0

--- cad/librnd/files/patch-tests_librnd_librnd__test.c
+++ cad/librnd/files/patch-tests_librnd_librnd__test.c
@@ -1,10 +1,10 @@
 Index: tests/librnd/librnd_test.c
 ===================================================================
---- tests/librnd/librnd_test.c.orig	2021-06-11 15:14:52 UTC
+--- tests/librnd/librnd_test.c.orig	2022-05-11 07:25:34 UTC
 +++ tests/librnd/librnd_test.c
-@@ -22,12 +22,23 @@ static void poly_test()
- 	rnd_poly_valid(&pa);
- }
+@@ -15,6 +15,15 @@
+ 
+ /*** init test ***/
  
 +/* workaround for symbols missing out because of the static linking done with
 +   the test app. Won't ever happen to real apps as they don't static link */
@@ -15,14 +15,6 @@ Index: tests/librnd/librnd_test.c
 +	workaround_fn = rnd_pixmap_reg_import;
 +}
 +
- int main(int argc, char *argv[])
+ static void poly_test()
  {
- 	int n;
- 	rnd_main_args_t ga;
- 
- 	rnd_app.default_embedded_menu = "";
-+
-+	librnd_test_symbol_workaround();
- 
- 	rnd_fix_locale_and_env();
- 
+ 	rnd_polyarea_t pa;
