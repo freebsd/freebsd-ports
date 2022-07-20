5eb5735d03f90419dba9b0daa5747dc4d42a27ba
Reid Linnemann <rlinnemann@netgate.com>
2022-07-20 10:34:57 -0300

    net/pear-Net_IPv6: Update to version 1.3.0b4
    Summary:
    - Removed patches included in 1.3.0b4
    - Added php 8 compat patch to correct curly brace array accessor syntax
    
    Sponsored by:		Rubicon Communications, LLC ("Netgate")
    Differential Revision:	https://reviews.freebsd.org/D35818

--- net/pear-Net_IPv6/files/patch-Net_IPv6.php
+++ net/pear-Net_IPv6/files/patch-Net_IPv6.php
@@ -1,35 +1,15 @@
---- Net/IPv6.php.orig	2016-08-31 14:11:53 UTC
+--- Net/IPv6.php.orig	2018-07-09 11:55:41 UTC
 +++ Net/IPv6.php
-@@ -550,7 +550,7 @@ class Net_IPv6
- 
-         if (false !== strpos($uip, '::') ) {
- 
--            list($ip1, $ip2) = explode('::', $uip);
-+            list($ip1, $ip2) = explode('::', $uip, 2);
- 
-             if ("" == $ip1) {
- 
-@@ -601,12 +601,12 @@ class Net_IPv6
- 
-             } else if (-1 == $c1) {              // ::xxx
- 
--                $fill = str_repeat('0:', 7-$c2);
-+                $fill = str_repeat('0:', max(1, 7-$c2));
-                 $uip  = str_replace('::', $fill, $uip);
- 
-             } else if (-1 == $c2) {              // xxx::
- 
--                $fill = str_repeat(':0', 7-$c1);
-+                $fill = str_repeat(':0', max(1, 7-$c1));
-                 $uip  = str_replace('::', $fill, $uip);
- 
-             } else {                          // xxx::xxx
-@@ -912,7 +912,7 @@ class Net_IPv6
- 
+@@ -853,10 +853,10 @@ class Net_IPv6
+                 return array("", $ip);
              }
  
--            if (8 == $count) {
-+            if (8 == $count and empty($ipPart[1])) {
- 
-                 return true;
+-            $ip{$pos} = '_';
++            $ip[$pos] = '_';
+             $ipPart   = explode('_', $ip);
+             
+-            if ($ip{$pos-1} === ":") {
++            if ($ip[$pos-1] === ":") {
+                 $ipPart[0] .= ":";
+             }
  
