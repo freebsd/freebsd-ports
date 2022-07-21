510e77cbcd3eb4117352f5aa01921c41b4134a9f
Yuri Victorovich <yuri@FreeBSD.org>
2022-07-21 13:18:26 -0700

    audio/zita-alsa-pcmi: Correct patch
    ... such that it doesn't require infiniband/byteswap.h
    
    (cherry picked from commit 80122a73d56247a6df95419ab3d0144fd213bc78)

--- audio/zita-alsa-pcmi/files/patch-source_zita-alsa-pcmi.cc
+++ audio/zita-alsa-pcmi/files/patch-source_zita-alsa-pcmi.cc
@@ -1,18 +1,17 @@
 --- source/zita-alsa-pcmi.cc.orig	2022-07-14 07:07:32 UTC
 +++ source/zita-alsa-pcmi.cc
-@@ -18,9 +18,17 @@
+@@ -18,9 +18,16 @@
  // ----------------------------------------------------------------------------
  
  
 +#if defined(__FreeBSD__)
-+#include <machine/endian.h>
++#  include <machine/endian.h>
++#  define bswap_32(x) bswap32(x)
 +#else
  #include <endian.h>
 +#endif
  #include <stdint.h>
-+#if defined(__FreeBSD__)
-+#include <infiniband/byteswap.h>
-+#else
++#if !defined(__FreeBSD__)
  #include <byteswap.h>
 +#endif
  #include <sys/time.h>
