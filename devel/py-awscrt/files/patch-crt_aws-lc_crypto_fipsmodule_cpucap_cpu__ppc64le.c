f08b67611f0b19c0ee8d9053ee4d22e09b03f2b1
Piotr Kubaj <pkubaj@FreeBSD.org>
2022-07-25 11:27:02 +0200

    devel/py-awscrt: fix build on powerpc64le
      Unknown processor:powerpc64le

---
+++ devel/py-awscrt/files/patch-crt_aws-lc_crypto_fipsmodule_cpucap_cpu__ppc64le.c
@@ -0,0 +1,14 @@
+--- crt/aws-lc/crypto/fipsmodule/cpucap/cpu_ppc64le.c.orig	2020-10-26 11:38:52 UTC
++++ crt/aws-lc/crypto/fipsmodule/cpucap/cpu_ppc64le.c
+@@ -28,7 +28,11 @@
+ #endif
+ 
+ void OPENSSL_cpuid_setup(void) {
++#if defined(__linux__)
+   OPENSSL_ppc64le_hwcap2 = getauxval(AT_HWCAP2);
++#elif defined(__FreeBSD__)
++  elf_aux_info(AT_HWCAP2, &OPENSSL_ppc64le_hwcap2, sizeof(OPENSSL_ppc64le_hwcap2));
++#endif
+ }
+ 
+ int CRYPTO_is_PPC64LE_vcrypto_capable(void) {
