0dff6d6e2f1a5263710d7b8abf07ed9b64f0bbb3
Brooks Davis <brooks@one-eyed-alien.net>
2022-07-26 11:21:35 -0700

    bsd.ssp.mk: don't enable on CHERI systems
    Stack smashing protection don't make sense on CHERI and the compiler
    ignores the flag except for complaining about it so disable it.

--- Mk/bsd.ssp.mk
+++ Mk/bsd.ssp.mk
@@ -3,7 +3,7 @@
 SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 
 .if !defined(SSP_UNSAFE) && \
-    (! ${ARCH:Mmips*})
+    (! ${ARCH:Mmips*}) && ${ARCH} != aarch64c && ${ARCH} != riscv64c
 # Overridable as a user may want to use -fstack-protector-all
 SSP_CFLAGS?=	-fstack-protector-strong
 CFLAGS+=	${SSP_CFLAGS}
