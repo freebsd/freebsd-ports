08f749841e111185a2ce23d67cf5df4bf2a2e2bb
Andreas Bilke <andreas@bilke.org>
2022-07-26 11:07:56 +0100

    textproc/py-sphinx-autoapi: Update to 1.9.0
     - switch to DISTVERSION
    
    ChangeLog:	https://github.com/readthedocs/sphinx-autoapi/blob/v1.9.0/CHANGELOG.rst
    PR:		265445

--- textproc/py-sphinx-autoapi/files/patch-setup.py
+++
@@ -1,8 +0,0 @@
---- setup.py.orig	2021-04-05 03:23:24 UTC
-+++ setup.py
-@@ -1,2 +1,4 @@
-+# Upstream switched to PEP-517 packaging
- from setuptools import setup
--setup()
-+if __name__ == "__main__":
-+    setup(use_scm_version=True)
