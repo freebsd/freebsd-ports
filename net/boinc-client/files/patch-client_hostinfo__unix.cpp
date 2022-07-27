4668af6562433fc42f29d30f2dd02317f49ee6ad
Larry Rosenman <ler@FreeBSD.org>
2022-07-27 16:52:01 -0500

    net/boinc-client: upgrade to 7.20.2
    notified by: portscout

--- net/boinc-client/files/patch-client_hostinfo__unix.cpp
+++ net/boinc-client/files/patch-client_hostinfo__unix.cpp
@@ -1,13 +1,11 @@
---- client/hostinfo_unix.cpp.orig	2018-01-14 10:07:52 UTC
+--- client/hostinfo_unix.cpp.orig	2022-07-27 21:28:09 UTC
 +++ client/hostinfo_unix.cpp
-@@ -164,7 +164,9 @@ extern "C" {
+@@ -177,7 +177,7 @@ extern "C" {
  // The following is intended to be true both on Linux
  // and Debian GNU/kFreeBSD (see trac #521)
  //
--#define LINUX_LIKE_SYSTEM (defined(__linux__) || defined(__GNU__) || defined(__GLIBC__)) && !defined(__HAIKU__)
-+#if (defined(__linux__) || defined(__GNU__) || defined(__GLIBC__) || defined (__freebsd__)) && !defined(__HAIKU__) 
-+#define LINUX_LIKE_SYSTEM 1
-+#endif
+-#if (defined(__linux__) || defined(__GNU__) || defined(__GLIBC__))  && !defined(__HAIKU__)
++#if (defined(__linux__) || defined(__GNU__) || defined(__GLIBC__) || defined(__freebsd__))  && !defined(__HAIKU__)
+ #define LINUX_LIKE_SYSTEM 1
+ #endif
  
- // Returns the offset between LOCAL STANDARD TIME and UTC.
- // LOCAL_STANDARD_TIME = UTC_TIME + get_timezone().
