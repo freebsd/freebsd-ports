d60e95e52e7df0240c9fd0e3267e9752c6ab8bd0
Yuri Victorovich <yuri@FreeBSD.org>
2022-07-27 20:43:06 -0700

    biology/cufflinks: Unbreak
    (cherry picked from commit 8fd55d46d939263df2c327093c6bb21d74a29c0d)

--- biology/cufflinks/files/patch-src_GBase.h
+++ biology/cufflinks/files/patch-src_GBase.h
@@ -1,4 +1,4 @@
---- src/GBase.h.orig	2019-05-25 22:08:19 UTC
+--- src/GBase.h.orig	2019-07-06 16:28:01 UTC
 +++ src/GBase.h
 @@ -1,6 +1,6 @@
  #ifndef G_BASE_DEFINED
@@ -8,3 +8,12 @@
  //mostly for MinGW
  #define _POSIX_SOURCE
  #endif
+@@ -71,7 +71,7 @@ typedef int16_t int16;
+ typedef uint16_t uint16;
+ 
+ typedef unsigned char uchar;
+-typedef unsigned char byte;
++typedef unsigned char bytex;
+ 
+ #ifndef MAXUINT
+ #define MAXUINT ((unsigned int)-1)
