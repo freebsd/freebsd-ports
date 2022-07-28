f2697b1421211b2169e27d47c63a6704911dee1c
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-07-28 16:13:56 +0000

    Revert "Regenerate patches for lang/perl5.32."
    This reverts commit 3e2ace039173887c701d8222759623d4f98ca3d6.

--- lang/perl5.32/files/patch-hints_freebsd.sh
+++ lang/perl5.32/files/patch-hints_freebsd.sh
@@ -1,6 +1,6 @@
 Remove libs that are not here on FreeBSD.
 
---- hints/freebsd.sh.orig	2020-12-18 09:58:48 UTC
+--- hints/freebsd.sh.orig	2016-12-05 10:28:13 UTC
 +++ hints/freebsd.sh
 @@ -88,6 +88,8 @@ case "$osvers" in
  	esac
@@ -22,45 +22,3 @@ Remove libs that are not here on FreeBSD.
  	;;
  esac
  
-@@ -124,24 +130,24 @@ case "$osvers" in
- 	;;
- 
- 3*|4*|5*|6*)
--        objformat=`/usr/bin/objformat`
-+        objformat=`$sysroot/usr/bin/objformat`
-         if [ x$objformat = xaout ]; then
--            if [ -e /usr/lib/aout ]; then
--                libpth="/usr/lib/aout /usr/local/lib /usr/lib"
--                glibpth="/usr/lib/aout /usr/local/lib /usr/lib"
-+            if [ -e $sysroot/usr/lib/aout ]; then
-+                libpth="$sysroot/usr/lib/aout $sysroot/usr/local/lib $sysroot/usr/lib"
-+                glibpth="$sysroot/usr/lib/aout $sysroot/usr/local/lib $sysroot/usr/lib"
-             fi
-             lddlflags='-Bshareable'
-         else
--            libpth="/usr/lib /usr/local/lib"
--            glibpth="/usr/lib /usr/local/lib"
-+            libpth="$sysroot/usr/lib $sysroot/usr/local/lib"
-+            glibpth="$sysroot/usr/lib $sysroot/usr/local/lib"
-             ldflags="-Wl,-E "
-             lddlflags="-shared "
-         fi
-         cccdlflags='-DPIC -fPIC'
-         ;;
- *)
--       libpth="/usr/lib /usr/local/lib"
--       glibpth="/usr/lib /usr/local/lib"
-+       libpth="$sysroot/usr/lib $sysroot/usr/local/lib"
-+       glibpth="$sysroot/usr/lib $sysroot/usr/local/lib"
-        ldflags="-Wl,-E "
-         lddlflags="-shared "
-         cccdlflags='-DPIC -fPIC'
-@@ -153,7 +159,7 @@ case "$osvers" in
- 
- *)
- 	ccflags="${ccflags} -DHAS_FPSETMASK -DHAS_FLOATINGPOINT_H"
--	if /usr/bin/file -L /usr/lib/libc.so | /usr/bin/grep -vq "not stripped" ; then
-+	if /usr/bin/file -L $sysroot/usr/lib/libc.so | /usr/bin/grep -vq "not stripped" ; then
- 	    usenm=false
- 	fi
-         ;;
