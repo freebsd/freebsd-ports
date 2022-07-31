53e2145761760a52e1ac4f8e6e54be5ec4daee4c
Jan Beich <jbeich@FreeBSD.org>
2022-07-31 12:39:52 +0000

    print/py-uharfbuzz: update to 0.30.0
    Changes:	https://github.com/harfbuzz/uharfbuzz/releases/tag/v0.30.0
    PR:		265585
    Reported by:	GitHub (watch releases)
    Approved by:	maintainer timeout (3 weeks)

--- print/py-uharfbuzz/files/patch-setup.py
+++ print/py-uharfbuzz/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2022-03-22 22:29:38 UTC
+--- setup.py.orig	2022-07-31 12:39:52 UTC
 +++ setup.py
-@@ -39,11 +39,9 @@ if platform.system() == 'Darwin':
+@@ -39,12 +39,9 @@ extension = Extension(
  extension = Extension(
      'uharfbuzz._harfbuzz',
      define_macros=define_macros,
@@ -10,6 +10,7 @@
          'src/uharfbuzz/_harfbuzz.pyx',
 -        'harfbuzz/src/harfbuzz.cc',
 -        'harfbuzz/src/hb-subset-repacker.cc',
+-        'harfbuzz/src/graph/gsubgpos-context.cc',
+         'src/uharfbuzz/_draw_test_funcs.cc',
      ],
      language='c++',
-     libraries=libraries,
