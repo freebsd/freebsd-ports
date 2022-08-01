5f35bc58a18fababea079891628bfa1a7bade4ae
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2022-08-01 15:46:24 -0500

    security/gvmd: update to 22.4.0
    ChangeLog:	https://github.com/greenbone/gvmd/releases/tag/v22.4.0

--- security/gvmd/files/patch-src_gvmd.c
+++ security/gvmd/files/patch-src_gvmd.c
@@ -1,6 +1,6 @@
---- src/gvmd.c	2020-08-11 10:05:36.000000000 -0500
-+++ src/gvmd.c	2021-01-03 16:50:30.707314000 -0500
-@@ -1362,6 +1362,7 @@
+--- src/gvmd.c	2022-07-21 02:20:24.000000000 -0500
++++ src/gvmd.c	2022-07-31 19:20:20.775975000 -0500
+@@ -1558,6 +1558,7 @@
    struct sockaddr_un address_unix;
    struct sockaddr_storage address_tls;
    int address_size;
@@ -8,7 +8,7 @@
  
    memset (&address_tls, 0, sizeof (struct sockaddr_storage));
    memset (&address_unix, 0, sizeof (struct sockaddr_un));
-@@ -1375,9 +1376,8 @@
+@@ -1571,9 +1572,8 @@
        /* UNIX file socket. */
  
        address_unix.sun_family = AF_UNIX;
@@ -20,7 +20,7 @@
  
        g_debug ("%s: address_unix.sun_path: %s",
                 __func__,
-@@ -1398,7 +1398,7 @@
+@@ -1594,7 +1594,7 @@
          }
  
        address = (struct sockaddr *) &address_unix;
@@ -29,7 +29,7 @@
  
        /* Ensure the path of the socket exists. */
  
-@@ -1414,8 +1414,8 @@
+@@ -1610,8 +1610,8 @@
      }
    else if (address_str_tls)
      {
@@ -40,7 +40,7 @@
        int port, optval;
  
        /* TLS TCP socket. */
-@@ -1440,17 +1440,17 @@
+@@ -1636,17 +1636,17 @@
              port = htons (GVMD_PORT);
          }
  
@@ -60,7 +60,7 @@
          }
        else
          {
-@@ -1491,7 +1491,7 @@
+@@ -1687,7 +1687,7 @@
        return -1;
      }
  
@@ -69,3 +69,13 @@
      {
        g_warning ("Failed to bind manager socket: %s", strerror (errno));
        return -1;
+@@ -2326,7 +2326,9 @@
+ 
+   /* Set process title. */
+ 
++#if !defined(__FreeBSD__)
+   setproctitle_init (argc, argv, env);
++#endif
+   setproctitle ("gvmd: Initializing");
+ 
+   /* Setup initial signal handlers. */
