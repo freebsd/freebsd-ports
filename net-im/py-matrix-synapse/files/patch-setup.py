7abef63eaed2cf588cd1799a834ae75c643f78e9
Sascha Biberhofer <sascha.biberhofer@skyforge.at>
2022-08-02 15:30:54 +0000

    net-im/py-matrix-synapse: Update to 1.64.0

--- net-im/py-matrix-synapse/files/patch-setup.py
+++ net-im/py-matrix-synapse/files/patch-setup.py
@@ -1,9 +1,9 @@
---- setup.py.orig	2022-07-05 15:51:27 UTC
+--- setup.py.orig	2022-08-02 13:44:06 UTC
 +++ setup.py
 @@ -149,7 +149,7 @@ install_requires = \
   'bcrypt>=3.1.0',
   'bleach>=1.4.3',
-  'canonicaljson>=1.4.0,<2.0.0',
+  'canonicaljson>=1.5.0,<2.0.0',
 - 'cryptography>=3.4.7',
 + 'cryptography',
   'frozendict>=1,!=2.1.2',
