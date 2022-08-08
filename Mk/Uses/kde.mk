021c28353c05e6a1af27e3b9a25b197e17f6bb35
Tobias C. Berner <tcberner@FreeBSD.org>
2022-08-08 09:59:12 +0200

    KDE: Update KDE Frameworks to 5.97
    Sunday, 14 August 2022
    
    KDE today announces the release of KDE Frameworks 5.97.0.
    
    KDE Frameworks are 83 addon libraries to Qt which provide a wide variety
    of commonly needed functionality in mature, peer reviewed and well
    tested libraries with friendly licensing terms. For an introduction see
    the KDE Frameworks release announcement.
    
    This release is part of a series of planned monthly releases making
    improvements available to developers in a quick and predictable manner.
    
    Announcement:	https://kde.org/announcements/frameworks/5/5.97.0/
    
    PR:		265831
    Exp-run by:	antoine

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -79,7 +79,7 @@ KDE_PLASMA_VERSION?=		5.24.6
 KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
-KDE_FRAMEWORKS_VERSION?=	5.96.0
+KDE_FRAMEWORKS_VERSION?=	5.97.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 # Current KDE applications.
