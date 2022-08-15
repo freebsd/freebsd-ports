445b057b01a13e23470da9ab3f2c12096f2552cb
Cy Schubert <cy@FreeBSD.org>
2022-08-15 07:21:20 -0700

    sysutils/nut-devel: Update to the latest networkupstools/nut github commit

--- sysutils/nut-devel/files/patch-tools_nut-scanner_nutscan-init.c
+++ sysutils/nut-devel/files/patch-tools_nut-scanner_nutscan-init.c
@@ -1,16 +1,14 @@
-diff --git tools/nut-scanner/nutscan-init.c.orig tools/nut-scanner/nutscan-init.c
-index 9ea3f5d..13f4e78 100644
---- tools/nut-scanner/nutscan-init.c.orig
-+++ tools/nut-scanner/nutscan-init.c
-@@ -133,7 +133,11 @@ void nutscan_init(void)
+--- tools/nut-scanner/nutscan-init.c.orig	2022-08-15 05:15:58.000000000 -0700
++++ tools/nut-scanner/nutscan-init.c	2022-08-15 07:11:39.489088000 -0700
+@@ -135,7 +135,11 @@
  	char *libname = NULL;
  #ifdef WITH_USB
   #if WITH_LIBUSB_1_0
 +  #ifdef __FreeBSD__
 +	libname = get_libname("libusb.so");
 +  #else
- 	libname = get_libname("libusb-1.0.so");
+ 	libname = get_libname("libusb-1.0" SOEXT);
 +  #endif
   #else
- 	libname = get_libname("libusb-0.1.so");
- 	if (!libname) {
+ 	libname = get_libname("libusb-0.1" SOEXT);
+  #endif
