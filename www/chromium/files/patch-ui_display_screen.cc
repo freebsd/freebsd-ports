b108b8734f8256503267fd05c48c7bc22f033beb
Robert Nagy <robert@openbsd.org>
2022-08-15 18:50:19 +0200

    www/chromium: two fixes from upstream
    - std::is_same compiler error
    - X11 screensaver suspension
    
    commit 904c8cedf85c7223f96ea084953089fea9a36bdd
    Author: Ben Noordhuis <info@bnoordhuis.nl>
    Date: Thu Jul 07 10:28:11 2022
    Fix std::is_same compiler error
    
    From 8c1ebea5f601b0b5247535dcdfd01755f3e6e1a6 Mon Sep 17 00:00:00 2001
    From: Andrew Wolfers <aswolfers@chromium.org>
    Date: Tue, 19 Jul 2022 15:01:25 +0000
    Subject: [PATCH] [x11][ozone] Fix X11 screensaver suspension.

--- www/chromium/files/patch-ui_display_screen.cc
+++ www/chromium/files/patch-ui_display_screen.cc
@@ -1,11 +1,34 @@
---- ui/display/screen.cc.orig	2022-07-22 17:30:31 UTC
+--- ui/display/screen.cc.orig	2022-08-15 13:11:31 UTC
 +++ ui/display/screen.cc
-@@ -84,7 +84,7 @@ void Screen::SetDisplayForNewWindows(int64_t display_i
+@@ -84,27 +84,12 @@ void Screen::SetDisplayForNewWindows(int64_t display_i
    display_id_for_new_windows_ = display_id;
  }
  
 -#if BUILDFLAG(IS_CHROMEOS_LACROS) || BUILDFLAG(IS_LINUX)
+-std::unique_ptr<Screen::ScreenSaverSuspender> Screen::SuspendScreenSaver() {
+-  SetScreenSaverSuspended(true);
+-  screen_saver_suspension_count_++;
+-  return base::WrapUnique(new Screen::ScreenSaverSuspender(this));
+-}
 +#if BUILDFLAG(IS_CHROMEOS_LACROS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
- std::unique_ptr<Screen::ScreenSaverSuspender> Screen::SuspendScreenSaver() {
-   SetScreenSaverSuspended(true);
-   screen_saver_suspension_count_++;
++Screen::ScreenSaverSuspender::~ScreenSaverSuspender() = default;
+ 
+-Screen::ScreenSaverSuspender::~ScreenSaverSuspender() {
+-  // Check that this suspender still refers to the active screen. Particularly
+-  // in tests, the screen might be destructed before the suspender.
+-  if (screen_ == GetScreen()) {
+-    screen_->screen_saver_suspension_count_--;
+-    if (screen_->screen_saver_suspension_count_ == 0) {
+-      screen_->SetScreenSaverSuspended(false);
+-    }
+-  }
+-}
+-
+-bool Screen::SetScreenSaverSuspended(bool suspend) {
++std::unique_ptr<Screen::ScreenSaverSuspender> Screen::SuspendScreenSaver() {
+   NOTIMPLEMENTED_LOG_ONCE();
+-  return false;
++  return nullptr;
+ }
+ #endif  // BUILDFLAG(IS_CHROMEOS_LACROS) || BUILDFLAG(IS_LINUX)
+ 
