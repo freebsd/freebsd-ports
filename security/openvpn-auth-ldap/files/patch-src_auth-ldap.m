327c682e46761e2a9c06507dafb8fac49f564ce6
Ryan Steinmetz <zi@FreeBSD.org>
2022-08-17 13:49:46 -0400

    security/openvpn-auth-ldap: Fix crash/update MAINTAINER/bump PORTREVISION
    Approved by:	mandree@ (maintainer)

--- security/openvpn-auth-ldap/files/patch-src_auth-ldap.m
+++ security/openvpn-auth-ldap/files/patch-src_auth-ldap.m
@@ -1,8 +1,8 @@
 Fix crash on close if there is no ctx.
 
---- src/auth-ldap.m.orig	2012-08-15 13:55:46 UTC
+--- src/auth-ldap.m.orig	2018-07-26 20:17:21 UTC
 +++ src/auth-ldap.m
-@@ -245,6 +245,9 @@ OPENVPN_EXPORT void
+@@ -247,6 +247,9 @@ OPENVPN_EXPORT void
  {
      ldap_ctx *ctx = handle;
  
@@ -12,3 +12,20 @@ Fix crash on close if there is no ctx.
      /* Clean up the configuration file */
      [ctx->config release];
  
+@@ -545,7 +548,6 @@ openvpn_plugin_func_v1(openvpn_plugin_ha
+     pool = [[TRAutoreleasePool alloc] init];
+ 
+     username = get_env("username", envp);
+-    TRString *userName=[[TRString alloc]initWithCString: username];
+     password = get_env("password", envp);
+     remoteAddress = get_env("ifconfig_pool_remote_ip", envp);
+ 
+@@ -556,6 +558,8 @@ openvpn_plugin_func_v1(openvpn_plugin_ha
+         goto cleanup;
+     }
+ 
++    TRString *userName=[[TRString alloc]initWithCString: username];
++
+     /* Create an LDAP connection */
+     if (!(ldap = connect_ldap(ctx->config))) {
+         [TRLog error: "LDAP connect failed."];
