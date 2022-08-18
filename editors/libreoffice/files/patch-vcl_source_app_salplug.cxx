1fc4b7db57ed8620821a80c6041faced6146d19f
Dima Panov <fluffy@FreeBSD.org>
2022-08-18 15:25:31 +0300

    editors/libreoffice: update to 7.4.0 release (+)
    On behalf of Office team I proud to announce a major update: LibreOffice 7.4!
    
       * 16,384 columns in spreadsheets
       * Better change tracking
       * Document themes in presentations
    
    ...plus compatibility improvements, performance boosts and more.
    
    Read more:	https://blog.documentfoundation.org/blog/2022/08/18/libreoffice-7-4-community/

--- editors/libreoffice/files/patch-vcl_source_app_salplug.cxx
+++ editors/libreoffice/files/patch-vcl_source_app_salplug.cxx
@@ -1,6 +1,6 @@
---- vcl/source/app/salplug.cxx.orig	2020-05-27 12:17:53 UTC
+--- vcl/source/app/salplug.cxx.orig	2022-05-12 13:26:05 UTC
 +++ vcl/source/app/salplug.cxx
-@@ -152,6 +152,7 @@ SalInstance* autodetect_plugin()
+@@ -202,6 +202,7 @@ const char* const* autodetect_plugin_list()
  #if ENABLE_KF5
          "kf5",
  #endif
@@ -8,12 +8,11 @@
  #if ENABLE_GTK3_KDE5
          "gtk3_kde5",
  #endif
-@@ -160,7 +161,7 @@ SalInstance* autodetect_plugin()
+@@ -216,6 +217,7 @@ const char* const* autodetect_plugin_list()
  
      static const char* const pStandardFallbackList[] =
      {
--        "gtk3", "gen", nullptr
-+        "gtk3", "qt5", "gen", nullptr
-     };
- 
- #ifdef HEADLESS_VCLPLUG
++        "qt5",
+ #if ENABLE_GTK3
+         "gtk3",
+ #endif
