e05fc79418b1d8b13231587d89701daa74ff7846
Hiroki Tagato <tagattie@FreeBSD.org>
2022-08-22 14:25:00 +0900

    devel/electron19: update to 19.0.13
    Changelog: https://github.com/electron/electron/releases/tag/v19.0.13

--- devel/electron19/files/patch-electron_shell_common_node__bindings.cc
+++ devel/electron19/files/patch-electron_shell_common_node__bindings.cc
@@ -1,4 +1,4 @@
---- electron/shell/common/node_bindings.cc.orig	2022-06-08 15:30:58 UTC
+--- electron/shell/common/node_bindings.cc.orig	2022-08-17 15:33:41 UTC
 +++ electron/shell/common/node_bindings.cc
 @@ -38,7 +38,7 @@
  #include "shell/common/node_includes.h"
@@ -9,7 +9,7 @@
  #include "shell/common/crash_keys.h"
  #endif
  
-@@ -142,7 +142,7 @@ bool g_is_initialized = false;
+@@ -143,7 +143,7 @@ bool g_is_initialized = false;
  void V8FatalErrorCallback(const char* location, const char* message) {
    LOG(ERROR) << "Fatal error in V8: " << location << " " << message;
  
@@ -18,7 +18,7 @@
    electron::crash_keys::SetCrashKey("electron.v8-fatal.message", message);
    electron::crash_keys::SetCrashKey("electron.v8-fatal.location", location);
  #endif
-@@ -369,7 +369,7 @@ void NodeBindings::Initialize() {
+@@ -370,7 +370,7 @@ void NodeBindings::Initialize() {
    TRACE_EVENT0("electron", "NodeBindings::Initialize");
    // Open node's error reporting system for browser process.
  
