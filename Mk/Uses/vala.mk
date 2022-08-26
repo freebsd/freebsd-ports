19a9a1889f0e14b7ec179103da50cfd8b1216970
Tobias C. Berner <tcberner@FreeBSD.org>
2022-08-26 16:53:10 +0200

    framework: add vala.mk -- easily depend on lang/vala
    The vala library comes with the version in the name.
    Instead of updating the LIB_DEPENDS on every major vala update,
       LIB_DEPENDS=libvala-XX.so:lang/vala
    one can now just have a static
       USES=vala:lib
    
    For build-time dependencies on vala or valac, use
       USES=vala:build
    
    PR:		249940

---
+++ Mk/Uses/vala.mk
@@ -0,0 +1,26 @@
+# Handle dependency on lang/vala
+#
+# Valid args:
+# 	- lib:       add a lib depends
+# 	- build:     add a build depends
+# 	- no_depend: only used for lang/vala itself
+
+.if ! defined(_INCLUDE_USES_VALA_MK)
+_INCLUDE_USES_VALA_MK=	yes
+
+_VALA_VERSION=		0.56.2
+_VALA_LIB_VERSION=	${_VALA_VERSION:R}
+_VALA_LIBRARY=		libvala-${_VALA_LIB_VERSION}.so
+_VALA_BINARY=		valac
+_VALA_PORT=		lang/vala
+
+.  if empty(vala_ARGS:Mno_depend)
+.    if ! empty(vala_ARGS:Mlib)
+LIB_DEPENDS+=		${_VALA_LIBRARY}:${_VALA_PORT}
+.    endif
+.    if ! empty(vala_ARGS:Mbuild)
+BUILD_DEPENDS+=		${_VALA_BINARY}:${_VALA_PORT}
+.    endif
+.  endif
+
+.endif
