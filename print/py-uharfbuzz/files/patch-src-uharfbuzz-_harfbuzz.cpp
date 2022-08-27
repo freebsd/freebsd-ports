e46a2183265448e9d9bd01d1fb554e3cba137d00
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-08-27 18:24:51 +0800

    print/py-uharfbuzz: Update BUILD_DEPENDS: Add check for harfbuzz version

--- print/py-uharfbuzz/files/patch-src-uharfbuzz-_harfbuzz.cpp
+++ print/py-uharfbuzz/files/patch-src-uharfbuzz-_harfbuzz.cpp
@@ -1,6 +1,13 @@
---- src/uharfbuzz/_harfbuzz.cpp.orig	2022-07-31 12:40:22 UTC
+--- src/uharfbuzz/_harfbuzz.cpp.orig	2022-07-31 07:40:22 UTC
 +++ src/uharfbuzz/_harfbuzz.cpp
-@@ -36,9 +36,6 @@
+@@ -30,15 +30,12 @@
+             "-std=c++11"
+         ],
+         "include_dirs": [
+-            "harfbuzz/src"
++            "%%LOCALBASE%%/include/harfbuzz"
+         ],
+         "language": "c++",
          "name": "uharfbuzz._harfbuzz",
          "sources": [
              "src/uharfbuzz/_harfbuzz.pyx",
