404e702489272b4003a340a7189fc27537788182
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-08-27 18:25:35 +0800

    net/rubygem-grpc: Update to 1.48.0
    Changes:	https://github.com/grpc/grpc/releases
    		https://github.com/grpc/grpc/commits/master/src/ruby

--- net/rubygem-grpc/files/patch-src_ruby_ext_grpc_extconf.rb
+++ net/rubygem-grpc/files/patch-src_ruby_ext_grpc_extconf.rb
@@ -1,19 +1,21 @@
---- src/ruby/ext/grpc/extconf.rb.orig	2022-04-11 16:34:02 UTC
+--- src/ruby/ext/grpc/extconf.rb.orig	2022-07-30 16:45:00 UTC
 +++ src/ruby/ext/grpc/extconf.rb
-@@ -45,9 +45,9 @@ if darwin && !cross_compiling
-   ENV['ARFLAGS'] = '-o'
+@@ -68,11 +68,11 @@ if apple_toolchain && !cross_compiling
  end
  
--ENV['EMBED_OPENSSL'] = 'true'
--ENV['EMBED_ZLIB'] = 'true'
--ENV['EMBED_CARES'] = 'true'
+ # Don't embed on TruffleRuby (constant-time crypto is unsafe with Sulong, slow build times)
+-ENV['EMBED_OPENSSL'] = (RUBY_ENGINE != 'truffleruby').to_s
 +ENV['EMBED_OPENSSL'] = 'false'
+ # Don't embed on TruffleRuby (the system zlib is already linked for the zlib C extension, slow build times)
+-ENV['EMBED_ZLIB'] = (RUBY_ENGINE != 'truffleruby').to_s
 +ENV['EMBED_ZLIB'] = 'false'
+ 
+-ENV['EMBED_CARES'] = 'true'
 +ENV['EMBED_CARES'] = 'false'
  
  ENV['ARCH_FLAGS'] = RbConfig::CONFIG['ARCH_FLAG']
- if darwin && !cross_compiling
-@@ -66,29 +66,30 @@ output_dir = File.expand_path(RbConfig::CONFIG['topdir
+ if apple_toolchain && !cross_compiling
+@@ -93,30 +93,10 @@ output_dir = File.expand_path(RbConfig::CONFIG['topdir
  grpc_lib_dir = File.join(output_dir, 'libs', grpc_config)
  ENV['BUILDDIR'] = output_dir
  
@@ -32,33 +34,15 @@
 -  system(cmd)
 -  exit 1 unless $? == 0
 -end
-+#unless windows
-+#  puts 'Building internal gRPC into ' + grpc_lib_dir
-+#  nproc = 4
-+#  nproc = Etc.nprocessors if Etc.respond_to? :nprocessors
-+#  nproc_override = ENV['GRPC_RUBY_BUILD_PROCS']
-+#  unless nproc_override.nil? or nproc_override.size == 0
-+#    nproc = nproc_override
-+#    puts "Overriding make parallelism to #{nproc}"
-+#  end
-+#  make = bsd ? 'gmake' : 'make'
-+#  cmd = "#{make} -j#{nproc} -C #{grpc_root} #{grpc_lib_dir}/libgrpc.a CONFIG=#{grpc_config} Q="
-+#  puts "Building grpc native library: #{cmd}"
-+#  system(cmd)
-+#  exit 1 unless $? == 0
-+#end
- 
+-
 -$CFLAGS << ' -I' + File.join(grpc_root, 'include')
-+#$CFLAGS << ' -I' + File.join(grpc_root, 'include')
- 
+-
  ext_export_file = File.join(grpc_root, 'src', 'ruby', 'ext', 'grpc', 'ext-export')
+ ext_export_file += '-truffleruby' if RUBY_ENGINE == 'truffleruby'
 -$LDFLAGS << ' -Wl,--version-script="' + ext_export_file + '.gcc"' if linux
--$LDFLAGS << ' -Wl,-exported_symbols_list,"' + ext_export_file + '.clang"' if darwin
-+#$LDFLAGS << ' -Wl,--version-script="' + ext_export_file + '.gcc"' if linux
-+#$LDFLAGS << ' -Wl,-exported_symbols_list,"' + ext_export_file + '.clang"' if darwin
+-$LDFLAGS << ' -Wl,-exported_symbols_list,"' + ext_export_file + '.clang"' if apple_toolchain
  
 -$LDFLAGS << ' ' + File.join(grpc_lib_dir, 'libgrpc.a') unless windows
-+#$LDFLAGS << ' ' + File.join(grpc_lib_dir, 'libgrpc.a') unless windows
 +$LDFLAGS << ' -lgrpc' unless windows
  if grpc_config == 'gcov'
    $CFLAGS << ' -O0 -fprofile-arcs -ftest-coverage'
