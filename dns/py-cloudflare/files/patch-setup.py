a8af098b8c1cbb04e009208a1f384b0ef629b4a8
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-08-27 18:26:10 +0800

    dns/py-cloudflare: Update to 2.9.11
    Changes:	https://github.com/cloudflare/python-cloudflare/blob/master/CHANGELOG.md

--- dns/py-cloudflare/files/patch-setup.py
+++ dns/py-cloudflare/files/patch-setup.py
@@ -1,11 +1,10 @@
---- setup.py.orig	2020-08-13 01:09:48 UTC
+--- setup.py.orig	2022-03-30 20:48:59 UTC
 +++ setup.py
-@@ -31,7 +31,7 @@ def main():
+@@ -32,7 +32,6 @@ def main():
          #package_dir={'CloudFlare/examples': 'examples'},
          #package_data={'cloudflare-examples': ["examples/*"]},
          include_package_data=True,
--        data_files = [('man/man1', ['cli4/cli4.man'])],
-+        #data_files = [('man/man1', ['cli4/cli4.man'])],
+-        data_files = [('share/man/man1', ['cli4/cli4.1'])],
          install_requires=['requests', 'pyyaml', 'jsonlines', 'beautifulsoup4'],
          keywords='cloudflare',
          entry_points={
