4e63ad4a56160351807e64239bd6bd8217a5e999
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-08-27 18:23:32 +0800

    devel/xxhash: Add upstream link

--- devel/xxhash/files/patch-xxhash.h
+++ devel/xxhash/files/patch-xxhash.h
@@ -1,3 +1,5 @@
+Obtained from:	https://github.com/Cyan4973/xxHash/commit/6189ecd3d44a693460f86280ccf49d33cb4b18e1
+
 --- xxhash.h.orig	2022-08-13 18:58:45.930785000 +0200
 +++ xxhash.h	2022-08-13 18:59:08.416809000 +0200
 @@ -1546,8 +1546,7 @@
