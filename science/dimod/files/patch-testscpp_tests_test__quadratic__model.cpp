5b8dd2d0c6fcb225f099355a18e29f4736dffb57
Yuri Victorovich <yuri@FreeBSD.org>
2022-08-27 10:50:29 -0700

    science/dimod: Fix tests

---
+++ science/dimod/files/patch-testscpp_tests_test__quadratic__model.cpp
@@ -0,0 +1,11 @@
+--- testscpp/tests/test_quadratic_model.cpp.orig	2022-08-27 15:54:59 UTC
++++ testscpp/tests/test_quadratic_model.cpp
+@@ -14,7 +14,7 @@
+ 
+ #include <iostream>
+ 
+-#include "../Catch2/single_include/catch2/catch.hpp"
++#include <catch2/catch.hpp>
+ #include "dimod/quadratic_model.h"
+ 
+ namespace dimod {
