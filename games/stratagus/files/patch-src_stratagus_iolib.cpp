43ea5b1719282d1066794067392f2eaf10d31ef2
Dmitry Marakasov <amdmi3@FreeBSD.org>
2022-08-31 14:13:48 +0300

    games/stratagus: update 3.2.0 → 3.3.1

---
+++ games/stratagus/files/patch-src_stratagus_iolib.cpp
@@ -0,0 +1,10 @@
+--- src/stratagus/iolib.cpp.orig	2022-07-31 09:44:18 UTC
++++ src/stratagus/iolib.cpp
+@@ -47,6 +47,7 @@
+ #include "SDL.h"
+ 
+ #include <map>
++#include <unordered_map>
+ #include <stdarg.h>
+ #include <stdio.h>
+ 
