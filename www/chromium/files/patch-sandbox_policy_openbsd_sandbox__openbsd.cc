8327a5f1bca6db04ef8d2300def2a5c67627d745
Robert Nagy <robert@openbsd.org>
2022-09-03 13:30:36 +0200

    www/chromium: update to 105.0.5195.102 and fix gpu watchdog thread while here
    Security:	f38d25ac-2b7a-11ed-a1ef-3065ec8fd3ec
    (cherry picked from commit 96b9a69e70108efa8f5a4783a530b94f663958ce)

--- www/chromium/files/patch-sandbox_policy_openbsd_sandbox__openbsd.cc
+++ www/chromium/files/patch-sandbox_policy_openbsd_sandbox__openbsd.cc
@@ -1,6 +1,6 @@
---- sandbox/policy/openbsd/sandbox_openbsd.cc.orig	2022-04-21 18:48:31 UTC
+--- sandbox/policy/openbsd/sandbox_openbsd.cc.orig	2022-09-02 10:45:05 UTC
 +++ sandbox/policy/openbsd/sandbox_openbsd.cc
-@@ -0,0 +1,407 @@
+@@ -0,0 +1,413 @@
 +// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
@@ -36,6 +36,7 @@
 +#include "base/posix/eintr_wrapper.h"
 +#include "base/strings/string_number_conversions.h"
 +#include "base/system/sys_info.h"
++#include "base/threading/thread.h"
 +#include "base/time/time.h"
 +#include "build/build_config.h"
 +#include "sandbox/constants.h"
@@ -99,6 +100,11 @@
 +  SandboxLinux* instance = base::Singleton<SandboxLinux>::get();
 +  CHECK(instance);
 +  return instance;
++}
++
++void SandboxLinux::StopThread(base::Thread* thread) {
++  DCHECK(thread);
++  thread->Stop();
 +}
 +
 +void SandboxLinux::PreinitializeSandbox(sandbox::mojom::Sandbox sandbox_type) {
