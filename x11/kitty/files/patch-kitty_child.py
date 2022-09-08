3c40124e07293e6305ab4ea6a66951aaa1d2c320
Nuno Teixeira <eduardo@FreeBSD.org>
2022-09-08 11:35:26 +0100

    x11/kitty: reverts commit 9051f0ba8e211de002a1571417ea6cc302c9db44
    Revert "x11/kitty: Use upstream patch fix build on 12.x Release"
    
    This reverts commit 9051f0ba8e211de002a1571417ea6cc302c9db44.
    
    Fix wrong cherry-pick, forgot to commit past updates.

---
+++ x11/kitty/files/patch-kitty_child.py
@@ -0,0 +1,11 @@
+--- kitty/child.py.orig	2021-11-10 21:33:51 UTC
++++ kitty/child.py
+@@ -45,7 +45,7 @@ else:
+             return list(filter(None, f.read().decode('utf-8').split('\0')))
+ 
+     def cwd_of_process(pid: int) -> str:
+-        ans = f'/proc/{pid}/cwd'
++        ans = subprocess.run(["pwdx", pid], capture_output=True).stdout.split()[1].decode("utf-8")
+         return os.path.realpath(ans)
+ 
+     def _environ_of_process(pid: int) -> str:
