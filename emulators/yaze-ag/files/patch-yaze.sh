6faea7028ecb591e7702b5fccf4670a633c00893
Stefan Eßer <se@FreeBSD.org>
2022-09-09 23:59:51 +0200

    emulators/yaze-ag: upgrade to version 2.51.3

--- emulators/yaze-ag/files/patch-yaze.sh
+++ emulators/yaze-ag/files/patch-yaze.sh
@@ -7,15 +7,15 @@
 -S_CPMDSKS=/usr/local/lib/yaze/disks
 -S_DOCFILES=/usr/local/lib/yaze/doc
 -S_DOCFILES_html=/usr/local/lib/yaze/doc_html
-+S_YAZEEXECDIR=%%LIBEXECDIR%%
-+S_YAZEFILES=%%DATADIR%%
-+S_CPMDSKS=%%DATADIR%%/disks
-+S_DOCFILES=%%DOCSDIR%%
-+S_DOCFILES_html=%%DOCSDIR%%/html
++S_YAZEEXECDIR=/usr/local/libexec
++S_YAZEFILES=/usr/local/share/yaze-ag
++S_CPMDSKS=/usr/local/share/yaze-ag/disks
++S_DOCFILES=/usr/local/share/doc/yaze-ag
++S_DOCFILES_html=/usr/local/share/doc/yaze-ag/html
  
  if [ ! -f .yazerc ]
  then
-@@ -80,10 +81,10 @@ fi
+@@ -80,10 +81,10 @@ echo pwd=`pwd`
  echo
  echo pwd=`pwd`
  
