3f3cc7e11540c33d2c1a4ef4dbf1ad58bfd3d8d1
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-09-10 20:53:19 +0800

    databases/py-mycli: Update to 1.26.1
    Changes:	https://github.com/dbcli/mycli/blob/main/changelog.md

--- databases/py-mycli/files/patch-setup.py
+++ databases/py-mycli/files/patch-setup.py
@@ -1,7 +1,16 @@
---- setup.py.orig	2022-01-21 01:27:26 UTC
+--- setup.py.orig	2022-09-01 20:45:54 UTC
 +++ setup.py
-@@ -26,7 +26,7 @@ install_requirements = [
-     'sqlparse>=0.3.0,<0.5.0',
+@@ -20,7 +20,7 @@ install_requirements = [
+     'click >= 7.0',
+     # Temporary to suppress paramiko Blowfish warning which breaks CI.
+     # Pinning cryptography should not be needed after paramiko 2.11.0.
+-    'cryptography == 36.0.2',
++    'cryptography >= 1.0.0',
+     # 'Pygments>=1.6,<=2.11.1',
+     'Pygments>=1.6',
+     'prompt_toolkit>=3.0.6,<4.0.0',
+@@ -29,7 +29,7 @@ install_requirements = [
+     'sqlglot>=5.1.3',
      'configobj >= 5.0.5',
      'cli_helpers[styles] >= 2.2.1',
 -    'pyperclip >= 1.8.1',
@@ -9,7 +18,7 @@
      'pyaes >= 1.6.1'
  ]
  
-@@ -92,7 +92,7 @@ setup(
+@@ -95,7 +95,7 @@ setup(
      author_email='mycli-dev@googlegroups.com',
      version=version,
      url='http://mycli.net',
