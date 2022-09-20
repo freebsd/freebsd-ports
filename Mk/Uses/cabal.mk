eaf7a4ee5399b0cac5f21bdf0cec3b7c53c0ae86
Gleb Popov <arrowd@FreeBSD.org>
2022-09-20 16:07:09 +0300

    Uses/cabal.mk: Extend i386 brokeness to basement-0.0.15

--- Mk/Uses/cabal.mk
+++ Mk/Uses/cabal.mk
@@ -69,9 +69,9 @@ IGNORE=		USES=cabal: invalid arguments: ${arg}
 IGNORE=		CABAL_PROJECT: invalid value: ${CABAL_PROJECT}
 .  endif
 
-.  if ${ARCH} == i386 && defined(USE_CABAL) && ${USE_CABAL:Mbasement-0.0.14}
+.  if ${ARCH} == i386 && defined(USE_CABAL) && ${USE_CABAL:Mbasement-0.0.1[4-5]}
 # Upstream issue: https://github.com/haskell-foundation/foundation/issues/565
-BROKEN=		basement-0.0.14 package doesn't compile on i386
+BROKEN=		${USE_CABAL:Mbasement-0.0.1[4-5]} package doesn't compile on i386
 .  endif
 
 PKGNAMEPREFIX?=	hs-
