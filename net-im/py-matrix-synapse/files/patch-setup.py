167b320fbf10f720cd507107d1c9e1ae87b8506a
Sascha Biberhofer <sascha.biberhofer@skyforge.at>
2022-09-27 17:39:36 +0000

    net-im/py-matrix-synapse: Update to 1.68.0
    - This update introduces a new dependency on rust.
    
    - Testing now uses the staged build output to ensure tests
      run against the compiled rust module.
    
    Co-authored-by: Ashish SHUKLA <ashish@FreeBSD.org>

--- net-im/py-matrix-synapse/files/patch-setup.py
+++ net-im/py-matrix-synapse/files/patch-setup.py
@@ -1,7 +1,7 @@
---- setup.py.orig	2022-08-02 13:44:06 UTC
+--- setup.py.orig	1970-01-01 00:00:00 UTC
 +++ setup.py
 @@ -149,7 +149,7 @@ install_requires = \
-  'bcrypt>=3.1.0',
+  'bcrypt>=3.1.7',
   'bleach>=1.4.3',
   'canonicaljson>=1.5.0,<2.0.0',
 - 'cryptography>=3.4.7',
@@ -9,3 +9,11 @@
   'frozendict>=1,!=2.1.2',
   'ijson>=3.1.4',
   'jsonschema>=3.0.0',
+@@ -165,7 +165,6 @@ install_requires = \
+  'pydantic>=1.7.4',
+  'pymacaroons>=0.13.0',
+  'service-identity>=18.1.0',
+- 'setuptools_rust>=1.3',
+  'signedjson>=1.1.0,<2.0.0',
+  'sortedcontainers>=1.4.4',
+  'treq>=15.1',
