8ee6579926f8aeb687ce365fda5dab8d640fc104
Piotr Kubaj <pkubaj@FreeBSD.org>
2022-09-30 00:29:34 +0200

    devel/zydis: enable on powerpc / powerpc64

---
+++ devel/zydis/files/patch-dependencies_zycore_include_Zycore_Defines.h
@@ -0,0 +1,13 @@
+--- dependencies/zycore/include/Zycore/Defines.h.orig	2022-09-29 22:15:50 UTC
++++ dependencies/zycore/include/Zycore/Defines.h
+@@ -131,6 +131,10 @@
+ #   define ZYAN_AARCH64
+ #elif defined(_M_ARM) || defined(_M_ARMT) || defined(__arm__) || defined(__thumb__)
+ #   define ZYAN_ARM
++#elif defined(__powerpc64__)
++#   define ZYAN_PPC64
++#elif defined(__powerpc__)
++#   define ZYAN_PPC
+ #elif defined(__EMSCRIPTEN__)
+     // Nothing to do, `ZYAN_EMSCRIPTEN` is both platform and arch macro for this one.
+ #else
