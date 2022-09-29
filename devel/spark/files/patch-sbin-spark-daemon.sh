6d342acaa5f7575de2b92e68a46ccd85b47e0626
Stefan Eßer <se@FreeBSD.org>
2022-09-29 15:06:06 +0200

    devel/spark: fix multiple issues
    The devel/spark port had been resurrected by Neel Chauhan based on
    PR 266484, but that PR had become stale and did not follow current
    rules and conventions (and had some issues that needed to be fixed).
    
    Neel has run out of time fixing the issues and I have taken over and
    finished the patches he had been working on.
    
    This updated port has been tested with poudriere testport, but I do
    not have a suitable test environment to run functional tests on.
    
    Please address run-time issues that are detected to both the
    maintainer of the port and to me.
    
    Approved by:	portmgr (blanket)

--- devel/spark/files/patch-sbin-spark-daemon.sh
+++ devel/spark/files/patch-sbin-spark-daemon.sh
@@ -1,8 +1,8 @@
---- sbin/spark-daemon.sh.orig	2015-10-26 09:05:25.709519603 +0000
-+++ sbin/spark-daemon.sh	2015-10-26 09:07:31.767513027 +0000
-@@ -99,14 +99,6 @@
+--- sbin/spark-daemon.sh.orig	2022-06-09 18:11:30 UTC
++++ sbin/spark-daemon.sh
+@@ -108,14 +108,6 @@ fi
  if [ "$SPARK_LOG_DIR" = "" ]; then
-   export SPARK_LOG_DIR="$SPARK_HOME/logs"
+   export SPARK_LOG_DIR="${SPARK_HOME}/logs"
  fi
 -mkdir -p "$SPARK_LOG_DIR"
 -touch "$SPARK_LOG_DIR"/.spark_test > /dev/null 2>&1
@@ -15,12 +15,12 @@
  
  if [ "$SPARK_PID_DIR" = "" ]; then
    SPARK_PID_DIR=/tmp
-@@ -125,8 +117,6 @@
+@@ -161,8 +153,6 @@ run_command() {
+ run_command() {
    mode="$1"
    shift
- 
--  mkdir -p "$SPARK_PID_DIR"
 -
+-  mkdir -p "$SPARK_PID_DIR"
+ 
    if [ -f "$pid" ]; then
      TARGET_ID="$(cat "$pid")"
-     if [[ $(ps -p "$TARGET_ID" -o comm=) =~ "java" ]]; then
