5dd7c81df6eeb00025f5948c1e47209579e43899
Rene Ladan <rene@FreeBSD.org>
2022-09-30 22:27:51 +0200

    devel/electron17: remove expired port (2022-09-30)
    rmport chokes on this port.

--- devel/electron17/files/patch-electron_spec-main_fixtures_apps_crash_node-crash.js
+++
@@ -1,8 +0,0 @@
---- electron/spec-main/fixtures/apps/crash/node-crash.js.orig	2021-01-22 23:55:24 UTC
-+++ electron/spec-main/fixtures/apps/crash/node-crash.js
-@@ -1,4 +1,4 @@
--if (process.platform === 'linux') {
-+if (process.platform === 'linux' || process.platform === 'freebsd') {
-   process.crashReporter.start({
-     submitURL: process.argv[2],
-     productName: 'Zombies',
