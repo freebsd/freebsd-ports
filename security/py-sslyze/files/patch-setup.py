77b8e3cb4f13caf19f749454e9c4f61b52160a79
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-10-03 10:15:42 +0800

    security/py-sslyze: Unbreak this port after e4f5c37688a52c5c8f0b2f0292753db1c030405a
    - Allow build with py-pydantic 1.10+
    - Bump PORTREVISION for package change

---
+++ security/py-sslyze/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2022-05-14 09:12:21 UTC
++++ setup.py
+@@ -100,7 +100,7 @@ setup(
+         "nassl>=4.0.1,<5.0.0",
+         "cryptography>=2.6,<38.0.0",
+         "tls-parser>=2.0.0,<3.0.0",
+-        "pydantic>=1.7,<1.10",
++        "pydantic>=1.7,<1.11",
+     ],
+     # cx_freeze info for Windows builds with Python embedded
+     options={"build_exe": {"packages": ["cffi", "cryptography"], "include_files": get_include_files()}},
