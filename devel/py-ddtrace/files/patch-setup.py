114a043763a7199a9e4f0b818a8d4b9b42419144
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-10-04 01:05:04 +0800

    devel/py-ddtrace: Update to 1.5.0
    Changes:	https://github.com/DataDog/dd-trace-py/releases

--- devel/py-ddtrace/files/patch-setup.py
+++ devel/py-ddtrace/files/patch-setup.py
@@ -1,6 +1,14 @@
---- setup.py.orig	2022-02-24 22:37:13 UTC
+--- setup.py.orig	2022-09-23 19:22:12 UTC
 +++ setup.py
-@@ -258,7 +257,7 @@ setup(
+@@ -258,7 +258,6 @@ setup(
+         "pathlib2; python_version<'3.5'",
+         "jsonschema",
+         "xmltodict>=0.12",
+-        "ipaddress",
+         "envier",
+     ]
+     + bytecode,
+@@ -293,7 +292,7 @@ setup(
          "Programming Language :: Python :: 3.10",
      ],
      use_scm_version={"write_to": "ddtrace/_version.py"},
@@ -9,7 +17,7 @@
      ext_modules=ext_modules
      + cythonize(
          [
-@@ -309,14 +308,6 @@ setup(
+@@ -344,14 +343,6 @@ setup(
                  "ddtrace.profiling._build",
                  sources=["ddtrace/profiling/_build.pyx"],
                  language="c",
