b86bdb228d46512d36e5842f22df1fdcaa2ab4b1
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-10-04 01:06:12 +0800

    www/py-aiohttp: Update to 3.8.3
    - Convert to USE_PYTHON=pytest
    
    Changes:	https://github.com/aio-libs/aiohttp/releases

---
+++ www/py-aiohttp/files/patch-tests-test_proxy_functional.py
@@ -0,0 +1,11 @@
+--- tests/test_proxy_functional.py.orig	2022-09-21 13:09:27 UTC
++++ tests/test_proxy_functional.py
+@@ -7,7 +7,7 @@ from re import match as match_regex
+ from unittest import mock
+ from uuid import uuid4
+ 
+-import proxy
++#import proxy
+ import pytest
+ from yarl import URL
+ 
