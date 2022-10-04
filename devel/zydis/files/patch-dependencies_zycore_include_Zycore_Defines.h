13d2c5e0aec3dba44720c86b7cdd74f66d70279e
Martin Filla <freebsd@sysctl.cz>
2022-10-04 07:27:53 +0200

    devel/zydis: update to 3.2.1
    ChangeLog: https://github.com/zyantific/zydis/releases/tag/v3.2.1
    
    Fixed a bug where an internal formatter structure wasn't properly initialized.
    
    PR:		266766
    Reported by:	freebsd@sysctl.cz (maintainer)
    MFH:		2022Q4 (security release)
    Security:	CVE-2021-41253
    
    (cherry picked from commit 61392a0734a41440ae817a26016547ebd5ce0ac0)

--- devel/zydis/files/patch-dependencies_zycore_include_Zycore_Defines.h
+++ devel/zydis/files/patch-dependencies_zycore_include_Zycore_Defines.h
@@ -1,6 +1,6 @@
---- dependencies/zycore/include/Zycore/Defines.h.orig	2022-09-29 22:15:50 UTC
+--- dependencies/zycore/include/Zycore/Defines.h.orig	2022-10-03 17:54:40 UTC
 +++ dependencies/zycore/include/Zycore/Defines.h
-@@ -131,6 +131,12 @@
+@@ -134,6 +134,12 @@
  #   define ZYAN_AARCH64
  #elif defined(_M_ARM) || defined(_M_ARMT) || defined(__arm__) || defined(__thumb__)
  #   define ZYAN_ARM
@@ -10,6 +10,6 @@
 +#   define ZYAN_PPC
 +#elif defined(__riscv) && __riscv_xlen == 64
 +#   define ZYAN_RISCV64
- #elif defined(__EMSCRIPTEN__)
-     // Nothing to do, `ZYAN_EMSCRIPTEN` is both platform and arch macro for this one.
+ #elif defined(__EMSCRIPTEN__) || defined(__wasm__) || defined(__WASM__)
+ #   define ZYAN_WASM
  #else
