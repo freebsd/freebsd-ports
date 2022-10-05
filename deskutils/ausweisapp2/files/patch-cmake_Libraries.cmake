301ebd7c33a29d76302ca01f4528797c47cd229d
Martin Filla <freebsd@sysctl.cz>
2022-10-05 07:26:06 +0200

    deskutils/ausweisapp2: Update to 1.24.2
    PR:		266824
    Reported by:	freebsd@sysctl.cz (maintainer)

--- deskutils/ausweisapp2/files/patch-cmake_Libraries.cmake
+++ deskutils/ausweisapp2/files/patch-cmake_Libraries.cmake
@@ -1,17 +1,11 @@
---- cmake/Libraries.cmake.orig	2022-04-25 13:11:06 +0200
+--- cmake/Libraries.cmake.orig	2022-09-06 11:32:26 UTC
 +++ cmake/Libraries.cmake
-@@ -124,11 +124,9 @@
+@@ -144,7 +144,7 @@ elseif(MAC)
+ 	find_library(OSX_FOUNDATION Foundation)
+ 	find_library(OSX_SERVICEMANAGEMENT ServiceManagement)
  elseif(UNIX)
- 	find_package(PkgConfig REQUIRED)
- 
 -	if(LINUX)
--		pkg_check_modules(UDEV IMPORTED_TARGET libudev)
--		if(NOT TARGET PkgConfig::UDEV)
--			message(STATUS "Hardware detection disabled - Missing libudev")
--		endif()
-+	pkg_check_modules(UDEV IMPORTED_TARGET libudev)
-+	if(NOT TARGET PkgConfig::UDEV)
-+		message(STATUS "Hardware detection disabled - Missing libudev")
- 	endif()
- 
- 	pkg_check_modules(PCSC REQUIRED libpcsclite)
++	if(LINUX OR BSD)
+ 		find_package(PkgConfig REQUIRED)
+ 		pkg_check_modules(UDEV IMPORTED_TARGET libudev)
+ 		if(NOT TARGET PkgConfig::UDEV)
