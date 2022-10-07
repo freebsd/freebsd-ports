aad793666b9a5393ed258e35ae47fc5982de3531
Yuri Victorovich <yuri@FreeBSD.org>
2022-10-07 07:17:01 -0800

    science/dimod: Update 0.11.5 -> 0.11.6
    Reported by:	portscout

--- science/dimod/files/patch-testscpp_tests_test__quadratic__model.cpp
+++ science/dimod/files/patch-testscpp_tests_test__quadratic__model.cpp
@@ -1,10 +1,10 @@
---- testscpp/tests/test_quadratic_model.cpp.orig	2022-08-27 15:54:59 UTC
+--- testscpp/tests/test_quadratic_model.cpp.orig	2022-10-05 22:19:20 UTC
 +++ testscpp/tests/test_quadratic_model.cpp
 @@ -14,7 +14,7 @@
  
  #include <iostream>
  
--#include "../Catch2/single_include/catch2/catch.hpp"
+-#include "catch2/catch.hpp"
 +#include <catch2/catch.hpp>
  #include "dimod/quadratic_model.h"
  
