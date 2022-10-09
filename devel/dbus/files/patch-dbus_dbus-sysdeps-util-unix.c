e477580f73f11c0dc522ad9e32c0d2808b8325c5
Dima Panov <fluffy@FreeBSD.org>
2022-10-09 20:39:07 +0300

    devel/dbus: update to 1.14.4 release (+)
    Changelog:	https://gitlab.freedesktop.org/dbus/dbus/-/blob/dbus-1.14.4/NEWS
    MFH:		2022q4
    (cherry picked from commit be1c9a0f195b90007f76f211e85a63bcab1d0514)

--- devel/dbus/files/patch-dbus_dbus-sysdeps-util-unix.c
+++ devel/dbus/files/patch-dbus_dbus-sysdeps-util-unix.c
@@ -1,14 +1,14 @@
---- dbus/dbus-sysdeps-util-unix.c.orig	2019-06-09 12:08:49 UTC
+--- dbus/dbus-sysdeps-util-unix.c.orig	2022-09-14 12:12:21 UTC
 +++ dbus/dbus-sysdeps-util-unix.c
-@@ -42,6 +42,7 @@
- #include <stdio.h>
+@@ -44,6 +44,7 @@
  #include <errno.h>
  #include <fcntl.h>
+ #include <limits.h>
 +#include <syslog.h>
  #include <sys/stat.h>
  #ifdef HAVE_SYS_RESOURCE_H
  #include <sys/resource.h>
-@@ -1367,7 +1368,7 @@ out:
+@@ -1394,7 +1395,7 @@ out:
   * On UNIX this should be the standard xdg freedesktop.org data directories:
   *
   * XDG_DATA_HOME=${XDG_DATA_HOME-$HOME/.local/share}
@@ -17,7 +17,7 @@
   *
   * and
   *
-@@ -1424,7 +1425,7 @@ _dbus_get_standard_session_servicedirs (DBusList **dir
+@@ -1451,7 +1452,7 @@ _dbus_get_standard_session_servicedirs (DBusList **dir
      }
    else
      {
@@ -26,7 +26,7 @@
          goto oom;
      }
  
-@@ -1457,7 +1458,7 @@ _dbus_get_standard_session_servicedirs (DBusList **dir
+@@ -1484,7 +1485,7 @@ _dbus_get_standard_session_servicedirs (DBusList **dir
   *
   * On UNIX this should be the standard xdg freedesktop.org data directories:
   *
@@ -35,7 +35,7 @@
   *
   * and
   *
-@@ -1482,7 +1483,7 @@ _dbus_get_standard_system_servicedirs (DBusList **dirs
+@@ -1509,7 +1510,7 @@ _dbus_get_standard_system_servicedirs (DBusList **dirs
     * be available.
     */
    static const char standard_search_path[] =
