7c7cdf2dc584772b706cf5d1473229348bc62244
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-10-09 23:32:28 +0800

    security/py-sslyze: Allow build with py-cryptography 38.0.0+
    - Bump PORTREVISION for package change

--- security/py-sslyze/files/patch-setup.py
+++ security/py-sslyze/files/patch-setup.py
@@ -1,8 +1,11 @@
 --- setup.py.orig	2022-05-14 09:12:21 UTC
 +++ setup.py
-@@ -100,7 +100,7 @@ setup(
+@@ -98,9 +98,9 @@ setup(
+     # Dependencies
+     install_requires=[
          "nassl>=4.0.1,<5.0.0",
-         "cryptography>=2.6,<38.0.0",
+-        "cryptography>=2.6,<38.0.0",
++        "cryptography>=2.6,<39.0.0",
          "tls-parser>=2.0.0,<3.0.0",
 -        "pydantic>=1.7,<1.10",
 +        "pydantic>=1.7,<1.11",
