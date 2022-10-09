c7ef7f52414e22b0abb662f5934a136a0aba06e6
Yuri Victorovich <yuri@FreeBSD.org>
2022-10-09 10:31:03 -0700

    math/{,py-}spot: Update 2.10.6 -> 2.11
    Reported by:	portscout

--- math/spot/files/patch-spot_misc_tmpfile.cc
+++ math/spot/files/patch-spot_misc_tmpfile.cc
@@ -1,10 +1,10 @@
---- spot/misc/tmpfile.cc.orig	2018-10-04 12:40:02 UTC
+--- spot/misc/tmpfile.cc.orig	2022-10-08 19:11:32 UTC
 +++ spot/misc/tmpfile.cc
 @@ -23,6 +23,7 @@
- #include <cstdlib>
+ #include <stdlib.h>
  #include <unistd.h>
  #include <string.h>
 +#include <stdlib.h>
  
- namespace spot
- {
+ using namespace std::string_literals;
+ 
