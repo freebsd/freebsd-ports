dc9a49023b3ea9ef76fb336df78d8277e0e0b786
Pavel Balaev <balaev@tarantool.org>
2022-10-12 16:05:30 +0300

    databases/tarantool: Update to 2.10.3
    (cherry picked from commit 9143dba5a6c4df9a4f501efbc70099c93936d553)

--- databases/tarantool/files/patch-cmake-BuildLibUnwind.cmake
+++ databases/tarantool/files/patch-cmake-BuildLibUnwind.cmake
@@ -1,5 +1,5 @@
---- cmake/BuildLibUnwind.cmake.orig	2022-05-21 03:40:03.175123000 +0300
-+++ cmake/BuildLibUnwind.cmake	2022-05-21 03:40:11.327052000 +0300
+--- cmake/BuildLibUnwind.cmake.old	2022-10-12 15:38:58.856661600 +0300
++++ cmake/BuildLibUnwind.cmake	2022-10-12 15:38:42.031660740 +0300
 @@ -18,6 +18,11 @@
    The paths to the libunwind libraries.
  #]========================================================================]
@@ -9,21 +9,21 @@
 +	set(SYSTEM_ARCH x86_64)
 +endif()
 +
- macro(libunwind_build)
+ macro(libunwind_build CFLAGS)
      set(LIBUNWIND_SOURCE_DIR ${PROJECT_SOURCE_DIR}/third_party/libunwind)
      set(LIBUNWIND_BUILD_DIR ${PROJECT_BINARY_DIR}/build/libunwind)
-@@ -81,12 +86,12 @@
+@@ -91,12 +96,12 @@
      add_library(bundled-libunwind-platform STATIC IMPORTED GLOBAL)
      set_target_properties(bundled-libunwind-platform PROPERTIES
                            IMPORTED_LOCATION
 -                          ${LIBUNWIND_INSTALL_DIR}/lib/libunwind-${CMAKE_SYSTEM_PROCESSOR}.a)
-+			  ${LIBUNWIND_INSTALL_DIR}/lib/libunwind-${SYSTEM_ARCH}.a)
++                          ${LIBUNWIND_INSTALL_DIR}/lib/libunwind-${SYSTEM_ARCH}.a)
      add_dependencies(bundled-libunwind-platform bundled-libunwind-project)
  
      set(LIBUNWIND_INCLUDE_DIR ${LIBUNWIND_INSTALL_DIR}/include)
      set(LIBUNWIND_LIBRARIES
 -        ${LIBUNWIND_INSTALL_DIR}/lib/libunwind-${CMAKE_SYSTEM_PROCESSOR}.a
-+	    ${LIBUNWIND_INSTALL_DIR}/lib/libunwind-${SYSTEM_ARCH}.a
++        ${LIBUNWIND_INSTALL_DIR}/lib/libunwind-${SYSTEM_ARCH}.a
          ${LIBUNWIND_INSTALL_DIR}/lib/libunwind.a)
  
      message(STATUS "Using bundled libunwind")
