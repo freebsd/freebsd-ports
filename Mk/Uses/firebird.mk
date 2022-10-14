88b4424308540bab2199f3e0d8c31f82bfbece2d
Baptiste Daroussin <bapt@FreeBSD.org>
2022-10-14 18:08:49 +0200

    Mk/Uses/firebird.mk: cleanup
    - remove useless for loops
    - avoid IGNORE overwrite

--- Mk/Uses/firebird.mk
+++ Mk/Uses/firebird.mk
@@ -16,17 +16,11 @@ FIREBIRD_VER=	${FIREBIRD_DEFAULT}
 # Mk/bsd.default-versions.mk in sync.
 VALID_FIREBIRD_VER=        2.5 3.0 4.0
 
-.  for v in ${FIREBIRD_DEFAULT}
-.    if ! ${VALID_FIREBIRD_VER:M$v}
+.  if ! ${VALID_FIREBIRD_VER:M${FIREBIRD_DEFAULT}}
 IGNORE=		Invalid Firebird default version ${FIREBIRD_DEFAULT}; valid versions are ${VALID_FIREBIRD_VER}
-.    endif
-.  endfor
-
-.  for w in ${FIREBIRD_VER}
-.    if ! ${VALID_FIREBIRD_VER:M$w}
+.  elif ! ${VALID_FIREBIRD_VER:M${FIREBIRD_VER}}
 IGNORE=		Invalid Firebird version ${FIREBIRD_VER}; valid versions are ${VALID_FIREBIRD_VER}
-.    endif
-.  endfor
+.  endif
 
 LIB_DEPENDS+=	libfbclient.so:databases/firebird${FIREBIRD_VER:S/.//}-client
 
