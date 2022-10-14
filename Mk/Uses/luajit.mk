a6b06460a563185c4d13db874052ae989f8cb556
Adam Weinberger <adamw@FreeBSD.org>
2022-10-14 12:02:19 -0600

    Uses/luajit.mk: Set a path to the luajit headers

--- Mk/Uses/luajit.mk
+++ Mk/Uses/luajit.mk
@@ -3,6 +3,10 @@
 # Usage:      USES=  luajit[:version]
 # MAINTAINER: adamw@FreeBSD.org
 
+# After bsd.port.options.mk or bsd.port.pre.mk, the following are set:
+#   LUAJIT_VER:    The selected luajit version
+#   LUAJIT_INCDIR: The path to luajit's header files
+
 .if !defined(_INCLUDE_USES_LUAJIT_MK)
 _INCLUDE_USES_LUAJIT_MK=yes
 
@@ -20,6 +24,12 @@ VALID_LUAJIT_VER=	luajit luajit-devel luajit-openresty
 IGNORE=	Invalid luajit default version ${LUAJIT_DEFAULT}: valid versions are ${VALID_LUAJIT_VER}
 .elif empty(VALID_LUAJIT_VER:M${LUAJIT_VER})
 IGNORE=	Invalid luajit version ${LUAJIT_VER}: valid versions are ${VALID_LUAJIT_VER}
+.endif
+
+.if ${LUAJIT_VER} == luajit
+LUAJIT_INCDIR=	${LOCALBASE}/include/luajit-2.0
+.else
+LUAJIT_INCDIR=	${LOCALBASE}/include/luajit-2.1
 .endif
 
 LIB_DEPENDS+=	libluajit-5.1.so:lang/${LUAJIT_VER}
