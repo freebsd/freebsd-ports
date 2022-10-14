ed4bec1e01390965296c2f510e87de9ccf8cef63
Tobias C. Berner <tcberner@FreeBSD.org>
2022-10-14 15:38:20 +0200

    security/py-cryptography: revert Update to 38.0.1
    This reverts commit f5711e95bcd17b154bdd697cb3f1650a788fdf3c.
    
    Revert due to multiple breakage reports.

--- security/py-cryptography/files/patch-setup.py
+++ security/py-cryptography/files/patch-setup.py
@@ -1,8 +1,8 @@
---- setup.py.orig	2022-09-07 12:20:58 UTC
+--- setup.py.orig	2021-03-25 17:19:57 UTC
 +++ setup.py
-@@ -13,23 +13,6 @@ import sys
+@@ -10,23 +10,7 @@ import sys
  
- from setuptools import setup
+ from setuptools import find_packages, setup
  
 -try:
 -    from setuptools_rust import RustExtension
@@ -12,7 +12,7 @@
 -        =============================DEBUG ASSISTANCE==========================
 -        If you are seeing an error here please try the following to
 -        successfully install cryptography:
--
+ 
 -        Upgrade to the latest pip and try again. This will fix errors for most
 -        users. See: https://pip.pypa.io/en/stable/installing/#upgrading-pip
 -        =============================DEBUG ASSISTANCE==========================
@@ -24,33 +24,32 @@
  base_dir = os.path.dirname(__file__)
  src_dir = os.path.join(base_dir, "src")
  
-@@ -43,20 +26,6 @@ try:
-         cffi_modules=[
+@@ -41,9 +25,8 @@ with open(os.path.join(src_dir, "cryptography", "__abo
+ 
+ # `install_requirements` and `setup_requirements` must be kept in sync with
+ # `pyproject.toml`
+-setuptools_rust = "setuptools-rust>=0.11.4"
+ install_requirements = ["cffi>=1.12"]
+-setup_requirements = install_requirements + [setuptools_rust]
++setup_requirements = install_requirements
+ 
+ if os.environ.get("CRYPTOGRAPHY_DONT_BUILD_RUST"):
+     rust_extensions = []
+@@ -129,9 +112,6 @@ try:
+                 "twine >= 1.12.0",
+                 "sphinxcontrib-spelling >= 4.0.1",
+             ],
+-            "sdist": [
+-                setuptools_rust,
+-            ],
+             "pep8test": [
+                 "black",
+                 "flake8",
+@@ -149,7 +129,6 @@ try:
              "src/_cffi_src/build_openssl.py:ffi",
+             "src/_cffi_src/build_padding.py:ffi",
          ],
--        rust_extensions=[
--            RustExtension(
--                "cryptography.hazmat.bindings._rust",
--                "src/rust/Cargo.toml",
--                py_limited_api=True,
--                # Enable abi3 mode if we're not using PyPy.
--                features=(
--                    []
--                    if platform.python_implementation() == "PyPy"
--                    else ["pyo3/abi3-py36"]
--                ),
--                rust_version=">=1.48.0",
--            )
--        ],
+-        rust_extensions=rust_extensions,
      )
  except:  # noqa: E722
      # Note: This is a bare exception that re-raises so that we don't interfere
-@@ -86,7 +55,7 @@ except:  # noqa: E722
-     )
-     print(f"    Python: {'.'.join(str(v) for v in sys.version_info[:3])}")
-     print(f"    platform: {platform.platform()}")
--    for dist in ["pip", "setuptools", "setuptools_rust"]:
-+    for dist in ["pip", "setuptools"]:
-         try:
-             version = pkg_resources.get_distribution(dist).version
-         except pkg_resources.DistributionNotFound:
