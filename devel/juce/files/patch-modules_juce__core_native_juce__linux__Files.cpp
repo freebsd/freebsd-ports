00180663d1e163f50dfae6916e9eac4c4192338d
Yuri Victorovich <yuri@FreeBSD.org>
2022-10-16 15:11:00 -0700

    devel/juce: Add better workaround for broken ENVIRON(7) in shared libraries
    New workaround exactly mimics the effect of ENVIRON(7).

--- devel/juce/files/patch-modules_juce__core_native_juce__linux__Files.cpp
+++ devel/juce/files/patch-modules_juce__core_native_juce__linux__Files.cpp
@@ -1,13 +1,24 @@
 - workaround for https://reviews.freebsd.org/D30842
 
---- modules/juce_core/native/juce_linux_Files.cpp.orig	2022-10-16 19:17:29 UTC
+--- modules/juce_core/native/juce_linux_Files.cpp.orig	2022-08-15 18:29:17 UTC
 +++ modules/juce_core/native/juce_linux_Files.cpp
-@@ -229,7 +229,7 @@ bool Process::openDocument (const String& fileName, co
+@@ -21,7 +21,9 @@
+ */
+ 
+ #if JUCE_BSD
+-extern char** environ;
++//extern char** environ; // this is broken on FreeBSD, see https://reviews.freebsd.org/D30842
++#include <dlfcn.h>
++static char*** environ_ptr = (char***)dlsym(RTLD_DEFAULT, "environ"); // workaround for the above
+ #endif
+ 
+ namespace juce
+@@ -229,7 +231,7 @@ bool Process::openDocument (const String& fileName, co
          setsid();
  
          // Child process
 -        execve (argv[0], (char**) argv, environ);
-+        execv (argv[0], (char**) argv);
++        execve (argv[0], (char**) argv, *environ_ptr);
          exit (0);
      }
  
