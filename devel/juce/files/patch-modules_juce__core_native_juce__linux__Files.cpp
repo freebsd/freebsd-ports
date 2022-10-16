5a5cab3eaa7d0072b8589a88842b922c4b0aac29
Yuri Victorovich <yuri@FreeBSD.org>
2022-10-16 12:36:49 -0700

    devel/juce: Add BSD-related patches

---
+++ devel/juce/files/patch-modules_juce__core_native_juce__linux__Files.cpp
@@ -0,0 +1,13 @@
+- workaround for https://reviews.freebsd.org/D30842
+
+--- modules/juce_core/native/juce_linux_Files.cpp.orig	2022-10-16 19:17:29 UTC
++++ modules/juce_core/native/juce_linux_Files.cpp
+@@ -229,7 +229,7 @@ bool Process::openDocument (const String& fileName, co
+         setsid();
+ 
+         // Child process
+-        execve (argv[0], (char**) argv, environ);
++        execv (argv[0], (char**) argv);
+         exit (0);
+     }
+ 
