freebsd/freebsd-ports

commit 39e58330c84e4598164fe3585ed7260410d04f35

math/octave-forge-ponchon-savarit: New port.
A toolbox for the Ponchon-Savarit method for octave.

Had to make changes to Mk/bsd.octave.mk to accomodate this port.

Mk/bsd.octave.mk
@@ -6,15 +6,17 @@
 # It is, in effect, a wrapper using the package handling already built
 # into math/octave.
 
-.if !empty(MASTER_SITE_OLD:MR2009_05)
-MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Old%20Monolithic%20Releases/R2009-05-08
-.elif !empty(MASTER_SITE_OLD:MR2009_06)
-MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Old%20Monolithic%20Releases/R2009-06-07
-.else
+.ifndef MASTER_SITES
+  .if !empty(MASTER_SITE_OLD:MR2009_05)
+  MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Old%20Monolithic%20Releases/R2009-05-08
+  .elif !empty(MASTER_SITE_OLD:MR2009_06)
+  MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Old%20Monolithic%20Releases/R2009-06-07
+  .else
 MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Individual%20Package%20Releases
+  .endif
 .endif
 
-DISTNAME=	${OCTAVE_PKGNAME}-${DISTVERSIONFULL}
+DISTNAME?=	${OCTAVE_PKGNAME}-${DISTVERSIONFULL}
 
 bsd_octave_mk_MAINTAINER=	stephen@FreeBSD.org
 

math/Makefile
@@ -586,6 +586,7 @@
     SUBDIR += octave-forge-outliers
     SUBDIR += octave-forge-parallel
     SUBDIR += octave-forge-plot
+    SUBDIR += octave-forge-ponchon-savarit
     SUBDIR += octave-forge-quaternion
     SUBDIR += octave-forge-queueing
     SUBDIR += octave-forge-secs1d

math/octave-forge-ponchon-savarit/Makefile
@@ -1,0 +1,29 @@
+PORTNAME=	octave-forge-ponchon-savarit
+PORTVERSION=	0.1.0
+CATEGORIES=	math
+MASTER_SITES=	https://github.com/aumpierre-unb/Ponchon-Savarit-for-GNU-Octave/archive/refs/tags/
+DISTNAME=	v${PORVERSION}0.1.0
+
+MAINTAINER=	stephen@FreeBSD.org
+COMMENT=	Octave-forge package ${OCTAVE_PKGNAME}
+WWW=		http://octave.sourceforge.net/
+
+LICENSE=	GPLv3
+LICENSE_FILE=	${WRKDIR}/${OCTSRC}/COPYING
+
+# OCTSRC is the name of the directory of the package.
+# It is usually ${OCTAVE_PKGNAME} or ${DISTNAME}.
+OCTSRC=		Ponchon-Savarit-for-GNU-Octave-${PORTVERSION}
+
+WRKSRC=		${WRKDIR}/${OCTSRC}/src
+
+do-build:
+	@${DO_NADA}
+
+.include "${.CURDIR}/../../Mk/bsd.octave.mk"
+
+post-build:
+	${RM} ${WRKSRC}/Makefile ${WRKSRC}/configure
+	cd ${WRKDIR} && ${TAR} cfz ${DISTNAME}.tar.gz ${OCTSRC}
+
+.include <bsd.port.mk>

math/octave-forge-ponchon-savarit/distinfo
@@ -1,0 +1,3 @@
+TIMESTAMP = 1666204732
+SHA256 (octave-forge/v0.1.0.tar.gz) = 3a1dd359d63c67e9fc0b927c5e30a5b55422636d8b08dbeaa050dc2dade4e7d4
+SIZE (octave-forge/v0.1.0.tar.gz) = 151831

math/octave-forge-ponchon-savarit/pkg-descr
@@ -1,0 +1,8 @@
+The octave-forge package is the result of The GNU Octave Repositry project,
+which is intended to be a central location for custom scripts, functions and
+extensions for GNU Octave. contains the source for all the functions plus
+build and install scripts.
+
+This is ponchon-savarit.
+
+ A toolbox for the Ponchon-Savarit method.

commit f3bad4cab17c8d9eb179319f8893b4c4de7e6259

Mk/bsd.octave.mk: Fix error introduced in 39e58330c84e.

Mk/bsd.octave.mk
@@ -7,13 +7,13 @@
 # into math/octave.
 
 .ifndef MASTER_SITES
-  .if !empty(MASTER_SITE_OLD:MR2009_05)
-  MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Old%20Monolithic%20Releases/R2009-05-08
-  .elif !empty(MASTER_SITE_OLD:MR2009_06)
-  MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Old%20Monolithic%20Releases/R2009-06-07
-  .else
+.  if !empty(MASTER_SITE_OLD:MR2009_05)
+MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Old%20Monolithic%20Releases/R2009-05-08
+.  elif !empty(MASTER_SITE_OLD:MR2009_06)
+MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Old%20Monolithic%20Releases/R2009-06-07
+.  else
 MASTER_SITES=	SF/octave/Octave%20Forge%20Packages/Individual%20Package%20Releases
-  .endif
+.  endif
 .endif
 
 DISTNAME?=	${OCTAVE_PKGNAME}-${DISTVERSIONFULL}