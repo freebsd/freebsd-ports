0720e8460e93b225d3b95d268ecb097e850f981c
Alexis Praga <alexis.praga@proton.me>
2022-10-20 07:47:29 +0200

    x11/kitty: Update to 0.26.4
    ChangeLog: https://sw.kovidgoyal.net/kitty/changelog/#detailed-list-of-changes
    
     * Allow centering the background_image
     * X11: Fix a regression in the previous release that caused pasting from GTK
       based applications to have extra newlines
     * Tab bar: Improve empty space management when some tabs have short titles,
       allocate the saved space to the active tab
     * Fix background_tint not applying to window margins and padding
     * Wayland: Fix background image scaling using tiled mode on high DPI screens
     * Wayland: Fix an abort when changing background colors with
       wayland_titlebar_color set to background
     * Update to Unicode 15.0
     * GNOME Wayland: Fix a memory leak in gnome-shell when using client side
       decorations
     * Wayland: Mark windows in which a bell occurs as urgent on compositors that
       support the xdg-activation protocol
     * Allow passing null bytes through the system clipboard
     * ssh kitten: Fix KITTY_PUBLIC_KEY not being encoded properly when transmitting
     * Sessions: Allow controlling which OS Window is active via the
       focus_os_window directive
     * Wayland: Fix for bug in NVIDIA drivers that prevents transparency working
     * Wayland: Fix for a bug that could cause kitty to become non-responsive when
       using multiple OS windows in a single instance on some compositors
     * Wayland: Fix for a bug preventing kitty from starting on Hyprland when using
       a non-unit scale
     * Wayland: Generate a XDG_ACTIVATION_TOKEN when opening URLs or running
       programs in the background via the launch action
     * Fix a regression that caused kitty not to restore SIGPIPE after python nukes
       it when launching children. Affects bash which does not sanitize its signal
       mask.
     * Fix a use-after-free when handling fake mouse clicks and the action causes
    windows to be removed/re-allocated
    
    PR:		267209
    Reported by:	alexis.praga@proton.me (maintainer)

---
+++ x11/kitty/files/patch-setup.py
@@ -0,0 +1,31 @@
+--- setup.py.orig	2022-10-19 17:45:56 UTC
++++ setup.py
+@@ -130,25 +130,9 @@ def libcrypto_flags() -> Tuple[List[str], List[str]]:
+     # Apple use their special snowflake TLS libraries and additionally
+     # have an ancient broken system OpenSSL, so we need to check for one
+     # installed by all the various macOS package managers.
+-    extra_pc_dir = ''
+-
+-    try:
+-        cflags = pkg_config('libcrypto', '--cflags-only-I', fatal=False)
+-    except subprocess.CalledProcessError:
+-        if is_macos:
+-            import ssl
+-            v = ssl.OPENSSL_VERSION_INFO
+-            pats = f'{v[0]}.{v[1]}', f'{v[0]}'
+-            for pat in pats:
+-                q = f'opt/openssl@{pat}/lib/pkgconfig'
+-                openssl_dirs = glob.glob(f'/opt/homebrew/{q}') + glob.glob(f'/usr/local/{q}')
+-                if openssl_dirs:
+-                    break
+-            if not openssl_dirs:
+-                raise SystemExit(f'Failed to find OpenSSL version {v[0]}.{v[1]} on your system')
+-            extra_pc_dir = os.pathsep.join(openssl_dirs)
+-        cflags = pkg_config('libcrypto', '--cflags-only-I', extra_pc_dir=extra_pc_dir)
+-    return cflags, pkg_config('libcrypto', '--libs', extra_pc_dir=extra_pc_dir)
++    cflags = [f"-I{sys.prefix}/include"]
++    libs = [f"-L{sys.prefix}/lib", "-lcrypto"]
++    return cflags, libs
+ 
+ 
+ def at_least_version(package: str, major: int, minor: int = 0) -> None:
