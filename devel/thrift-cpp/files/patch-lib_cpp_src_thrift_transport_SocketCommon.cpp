519e762a6f42268d5aacc56c84636f1ad7a17496
Matthias Fechner <mfechner@FreeBSD.org>
2022-10-20 16:21:01 +0200

    devel/thrift: update to 0.16.0
    Changelog:
    https://github.com/apache/thrift/blob/master/CHANGES.md#0160
    This update to required for gitlab-ce 15.4.
    
    PR:		267228
    Approved by:	 komarov@valerka.net (maintainer timeout)
    Differential Revision:	https://reviews.freebsd.org/D37068

--- devel/thrift-cpp/files/patch-lib_cpp_src_thrift_transport_SocketCommon.cpp
+++ devel/thrift-cpp/files/patch-lib_cpp_src_thrift_transport_SocketCommon.cpp
@@ -1,14 +1,14 @@
---- lib/cpp/src/thrift/transport/SocketCommon.cpp.orig	2021-03-05 23:09:13 UTC
+--- lib/cpp/src/thrift/transport/SocketCommon.cpp.orig	2022-01-20 19:36:56 UTC
 +++ lib/cpp/src/thrift/transport/SocketCommon.cpp
-@@ -24,6 +24,11 @@
+@@ -26,6 +26,11 @@
  
- #ifndef _WIN32
+ #include <cstring>
  
 +#include <sys/types.h>
 +#include <sys/socket.h>
 +#include <netinet/in.h>
 +#include <arpa/inet.h>
 +
- #include <thrift/thrift-config.h>
- 
- #ifdef HAVE_UNISTD_H
+ namespace apache {
+ namespace thrift {
+ namespace transport {
