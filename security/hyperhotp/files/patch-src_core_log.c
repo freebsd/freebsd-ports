a5aabfcb3401a492ef2eeae1d49c0f9cf73ce5eb
Robert Clausecker <fuz@fuz.su>
2022-10-22 19:40:52 +0000

    security/hyperhotp: update to 0.1
    Upstream changes:
    
     - add man page
     - integrate FreeBSD patches
     - improve usability
    
    PR:		267275

--- security/hyperhotp/files/patch-src_core_log.c
+++
@@ -1,8 +0,0 @@
---- src/core/log.c.orig	2021-10-05 10:43:22 UTC
-+++ src/core/log.c
-@@ -1,4 +1,4 @@
--#include <libusb-1.0/libusb.h>
-+#include <libusb.h>
- #include <memory.h>
- #include <stdbool.h>
- #include <stdint.h>
