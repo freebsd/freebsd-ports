5c793405d0e9f0aa05553a2888d6e31b1a17edb1
Dima Panov <fluffy@FreeBSD.org>
2022-10-24 09:51:22 +0300

    graphics/ptex: update to 2.4.2 release (+)
    * Drop custom do-install target, cmake automation do it well
    * Bump direct consumers
    
    Changes sinve 2.3.2 release:
    v2.4.0	May 18, 2021	API: headers and DSOs hide non-API symbols by default.
    v2.4.1	Aug 3, 2021	API: Windows symbol visibility fixes.
    v2.4.2	Aug 4, 2022	API: Windows Clang/14 support, visibility fixes and cmake enhancements.
    
    Approved by:	maintaimer (implicit)

--- graphics/ptex/files/patch-src_ptex_PtexPlatform.h
+++ graphics/ptex/files/patch-src_ptex_PtexPlatform.h
@@ -1,6 +1,6 @@
---- src/ptex/PtexPlatform.h.orig	2015-02-12 16:37:58 UTC
+--- src/ptex/PtexPlatform.h.orig	2022-08-05 02:02:51 UTC
 +++ src/ptex/PtexPlatform.h
-@@ -60,7 +60,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+@@ -70,7 +70,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY O
  
  // linux/unix/posix
  #include <stdlib.h>
@@ -9,4 +9,4 @@
 +#endif
  #include <string.h>
  #include <pthread.h>
- // OS for spinlock
+ 
--- graphics/ptex/files/patch-src_ptex_PtexWriter.cpp
+++ graphics/ptex/files/patch-src_ptex_PtexWriter.cpp
@@ -1,6 +1,6 @@
---- src/ptex/PtexWriter.cpp.orig	2015-02-12 16:37:58 UTC
+--- src/ptex/PtexWriter.cpp.orig	2022-08-05 02:02:51 UTC
 +++ src/ptex/PtexWriter.cpp
-@@ -66,6 +66,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+@@ -66,6 +66,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY O
  #include <algorithm>
  #include <iostream>
  #include <sstream>
