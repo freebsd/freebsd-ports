a16d8f14ce3bbcae7b17b690069593b5b0c7fb9e
Dan Langille <dvl@FreeBSD.org>
2022-10-26 13:05:55 +0000

    security/py-keyrings.alt: Update to 4.2.0
    This undeprecates the prot by moving from the deprecated
    security/py-pycrypto to security/py-pycryptodome and
    security/py-pycryptodomex (both are needed for `make test`).
    
    PR:		267340

---
+++ security/py-keyrings.alt/files/patch-setup.py
@@ -0,0 +1,9 @@
+--- setup.py.orig	2022-10-24 19:25:10 UTC
++++ setup.py
+@@ -0,0 +1,6 @@
++#!/usr/bin/env python
++
++import setuptools
++
++if __name__ == "__main__":
++    setuptools.setup()
--- security/py-keyrings.alt/files/patch-tests_test__file.py
+++ security/py-keyrings.alt/files/patch-tests_test__file.py
@@ -2,14 +2,14 @@
 # but this package doesn't exist in the ports tree yet, so adjust the import
 # here to import 'mock' proper. TODO
 
---- tests/test_file.py.orig	2019-01-04 05:24:38 UTC
+--- tests/test_file.py.orig	2022-09-05 01:38:48 UTC
 +++ tests/test_file.py
-@@ -7,7 +7,7 @@ import unittest
- from six.moves import configparser
+@@ -6,7 +6,7 @@ import getpass
+ import configparser
  
  import pytest
 -from unittest import mock
 +import mock
  
- from keyring.tests.test_backend import BackendBasicTests
- from keyring.tests.util import random_string
+ from keyring.testing.backend import BackendBasicTests
+ from keyring.testing.util import random_string
