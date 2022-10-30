eed33d5e0327a740bffb6ace547e2913ed02f37b
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2022-10-30 16:35:07 -0500

    security/wazuh-*: update to 4.3.9
    ChangeLog at:	https://github.com/wazuh/wazuh/releases/tag/v4.3.9

---
+++ security/wazuh-manager/files/patch-src-rootcheck_os_string.c
@@ -0,0 +1,12 @@
+--- src/rootcheck/os_string.c	2022-10-06 08:35:58.000000000 -0500
++++ src/rootcheck/os_string.c	2022-10-30 14:31:56.614900000 -0500
+@@ -42,6 +42,9 @@
+ #include <locale.h>
+ #include <unistd.h>
+ #include <netinet/in.h>
++#ifdef __FreeBSD__
++#include <machine/exec.h>
++#endif
+ 
+ #ifdef SOLARIS
+ #include <sys/exechdr.h>
