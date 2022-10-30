d75e99a393bf5ed0b96ca77e8f185f3677b432b8
Robert Clausecker <fuz@fuz.su>
2022-10-30 11:39:50 -0700

    x11/hamclock: fix build on armv7
    PR:		267439
    (cherry picked from commit ba97dbab1eba786c0bedfe11d583f2e6dd4fd1b6)

--- x11/hamclock/files/patch-ESPHamClock.ino
+++ x11/hamclock/files/patch-ESPHamClock.ino
@@ -1,5 +1,5 @@
---- ESPHamClock.ino.orig	2022-04-30 15:37:10.367129000 -0500
-+++ ESPHamClock.ino	2022-04-30 15:37:48.165833000 -0500
+--- ESPHamClock.ino.orig	2022-07-02 11:59:16 UTC
++++ ESPHamClock.ino
 @@ -1,6 +1,9 @@
  /* HamClock
   */
@@ -10,3 +10,12 @@
  
  // glue
  #include "HamClock.h"
+@@ -1605,7 +1608,7 @@ static void drawUptime(bool force)
+     // draw two most significant units if change
+     if (upsecs < 60) {
+         prepUptime();
+-        tft.print(upsecs); tft.print(F("s "));
++        tft.print((long)upsecs); tft.print(F("s "));
+     } else if (upsecs < 3600) {
+         prepUptime();
+         tft.print(mins); tft.print(F("m "));
