1040fd38d58f27fd6897a72354ad980736395097
Yuri Victorovich <yuri@FreeBSD.org>
2022-11-01 17:02:54 -0700

    science/dd_package: New port: MQT decision diagram package tailored to quantum computing

---
+++ science/dd_package/files/patch-include_dd_Package.hpp
@@ -0,0 +1,10 @@
+--- include/dd/Package.hpp.orig	2022-11-01 23:39:28 UTC
++++ include/dd/Package.hpp
+@@ -39,6 +39,7 @@
+ #include <random>
+ #include <regex>
+ #include <set>
++#include <stack>
+ #include <stdexcept>
+ #include <string>
+ #include <type_traits>
