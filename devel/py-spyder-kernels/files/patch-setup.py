18ac8be53bef5758ea07c5d7490bc9ae808e6079
Rainer Hurling <rhurlin@FreeBSD.org>
2022-11-03 22:06:34 +0100

    devel/py-spyder-kernels: Allow py-ipykernel 6.16.0

---
+++ devel/py-spyder-kernels/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2022-11-02 23:49:28 UTC
++++ setup.py
+@@ -40,7 +40,7 @@ REQUIREMENTS = [
+     'backports.functools-lru-cache; python_version<"3"',
+     'cloudpickle',
+     'ipykernel<5; python_version<"3"',
+-    'ipykernel>=6.16.1,<7; python_version>="3"',
++    'ipykernel>=6.16.0,<7; python_version>="3"',
+     'ipython<6; python_version<"3"',
+     'ipython>=7.31.1,<8; python_version>="3"',
+     'jupyter-client>=5.3.4,<6; python_version<"3"',
