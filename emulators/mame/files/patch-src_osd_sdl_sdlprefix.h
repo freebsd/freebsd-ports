5fb3b275a32501e1df45a08b034b94b8eb93b9a2
Alastair Hogge <agh@riseup.net>
2022-11-04 01:25:22 +0800

    emulators/{mame,mess}: Update to 0.249
    Release note: https://www.mamedev.org/releases/whatsnew_0249.txt
    
    Also transfer maintainership.
    
    PR:		252362
    Reported by:	Stéphane D'Alu <sdalu@sdalu.com> (initial patches)
    Approved by:	manu

--- emulators/mame/files/patch-src_osd_sdl_sdlprefix.h
+++ emulators/mame/files/patch-src_osd_sdl_sdlprefix.h
@@ -1,7 +1,10 @@
---- src/osd/sdl/sdlprefix.h.orig	2018-02-27 15:59:06 UTC
+--- src/osd/sdl/sdlprefix.h.orig	2022-04-29 05:37:26 UTC
 +++ src/osd/sdl/sdlprefix.h
-@@ -48,6 +48,7 @@
- #define NO_AFFINITY_NP 1
+@@ -45,9 +45,9 @@
+ 
+ #elif defined(__FreeBSD__)
+ #define SDLMAME_FREEBSD 1
+-#define NO_AFFINITY_NP 1
  #elif defined(__DragonFly__)
  #define SDLMAME_DRAGONFLY 1
 +#define NO_AFFINITY_NP 1
