a3860f2310f6f27c764c8aa59ccc22b28583d50c
Christian Weisgerber <naddy@FreeBSD.org>
2022-11-03 18:38:42 +0000

    devel/got: update to 0.78
    User-visible changes:
    - provide a diff of changes in a temp file while editing a commit log message

--- devel/got/files/patch-regress_cmdline_common.sh
+++ devel/got/files/patch-regress_cmdline_common.sh
@@ -1,18 +1,9 @@
---- regress/cmdline/common.sh.orig	2022-08-23 09:11:50 UTC
+--- regress/cmdline/common.sh.orig	2022-10-24 22:00:47 UTC
 +++ regress/cmdline/common.sh
-@@ -27,6 +27,29 @@ export GOT_IGNORE_GITCONFIG=1
+@@ -27,6 +27,20 @@ export GOT_IGNORE_GITCONFIG=1
  
  export MALLOC_OPTIONS=S
  
-+# work around the fix for CVE-2022-39253 in Git 2.38.1
-+git()
-+(
-+	if [ "$1" = submodule ]; then
-+		set - -c protocol.file.allow=always "$@"
-+	fi
-+	exec git "$@"
-+)
-+
 +# compat wrapper
 +sed()
 +(
