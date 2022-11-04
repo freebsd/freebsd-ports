5d3d887e48141fd7fe390f5763e64c895f5ac456
Thierry Thomas <thierry@FreeBSD.org>
2022-11-04 15:02:13 +0100

    Mk/Uses/fortran.mk: set variables for F90
    Re-apply fa6fe3f8b6d7 now that net/mpich has been fixed.

--- Mk/Uses/fortran.mk
+++ Mk/Uses/fortran.mk
@@ -18,8 +18,10 @@ _GCC_VER=	${GCC_DEFAULT:S/.//}
 BUILD_DEPENDS+=	gfortran${_GCC_VER}:lang/gcc${_GCC_VER}
 RUN_DEPENDS+=	gfortran${_GCC_VER}:lang/gcc${_GCC_VER}
 F77=		gfortran${_GCC_VER}
+F90=		gfortran${_GCC_VER}
 FC=		gfortran${_GCC_VER}
 FFLAGS+=	-Wl,-rpath=${LOCALBASE}/lib/gcc${_GCC_VER}
+F90FLAGS+=	-Wl,-rpath=${LOCALBASE}/lib/gcc${_GCC_VER}
 FCFLAGS+=	-Wl,-rpath=${LOCALBASE}/lib/gcc${_GCC_VER}
 LDFLAGS+=	-Wl,-rpath=${LOCALBASE}/lib/gcc${_GCC_VER} \
 		-L${LOCALBASE}/lib/gcc${_GCC_VER} -B${LOCALBASE}/bin
@@ -28,7 +30,9 @@ IGNORE=		USES=fortran: invalid arguments: ${fortran_ARGS}
 .  endif
 
 USE_BINUTILS=	yes
-CONFIGURE_ENV+=	F77="${F77}" FC="${FC}" FFLAGS="${FFLAGS}" FCFLAGS="${FCFLAGS}"
-MAKE_ENV+=	F77="${F77}" FC="${FC}" FFLAGS="${FFLAGS}" FCFLAGS="${FCFLAGS}"
+CONFIGURE_ENV+=	F77="${F77}" F90="${FC}" FC="${FC}"	\
+		FFLAGS="${FFLAGS}" F90FLAGS="${FFLAGS}" FCFLAGS="${FCFLAGS}"
+MAKE_ENV+=	F77="${F77}" F90="${FC}" FC="${FC}"	\
+		FFLAGS="${FFLAGS}" F90FLAGS="${FFLAGS}" FCFLAGS="${FCFLAGS}"
 
 .endif
