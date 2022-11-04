750c5a75797564ffa39b66ac88a19d3c65f590be
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2022-11-04 16:26:57 -0500

    print/py-reportlab: Update version 3.5.68=>3.6.11
    PR:		267345
    Reported by:	osidorkin@gmail.com

--- print/py-reportlab/files/patch-docs-userguide-genuserguide.py
+++ print/py-reportlab/files/patch-docs-userguide-genuserguide.py
@@ -1,12 +1,12 @@
---- docs/userguide/genuserguide.py.orig	2014-02-14 14:22:37 UTC
+--- docs/userguide/genuserguide.py.orig	2022-06-08 08:27:36 UTC
 +++ docs/userguide/genuserguide.py
-@@ -8,7 +8,8 @@ This module contains the script for buil
+@@ -8,7 +8,8 @@ def run(pagesize=None, verbose=0, outDir=None):
  """
  
  def run(pagesize=None, verbose=0, outDir=None):
 -    import sys,os
 +    import sys,os,site
 +    site.addsitedir("%%STAGEDIR%%%%PYTHON_SITELIBDIR%%")
-     from reportlab.lib.utils import open_and_read, asUnicode
+     from reportlab.lib.utils import open_and_read
      cwd = os.getcwd()
      docsDir=os.path.dirname(os.path.dirname(sys.argv[0]) or cwd)
