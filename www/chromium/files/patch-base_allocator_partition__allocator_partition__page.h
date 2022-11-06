17f46f71982b5a498273b541a511e8b00b79241f
Robert Nagy <robert@openbsd.org>
2022-11-06 19:54:31 +0100

    www/chromium: unbreak on i386 by enforcing 8 byte alignment on i386 in partition alloc

--- www/chromium/files/patch-base_allocator_partition__allocator_partition__page.h
+++ www/chromium/files/patch-base_allocator_partition__allocator_partition__page.h
@@ -1,14 +1,6 @@
---- base/allocator/partition_allocator/partition_page.h.orig	2022-10-24 13:33:33 UTC
+--- base/allocator/partition_allocator/partition_page.h.orig	2022-11-06 16:08:29 UTC
 +++ base/allocator/partition_allocator/partition_page.h
-@@ -138,13 +138,14 @@ struct SlotSpanMetadata {
-   PartitionBucket<thread_safe>* const bucket = nullptr;
- 
-   // CHECK()ed in AllocNewSlotSpan().
--#if defined(PA_HAS_64_BITS_POINTERS) && BUILDFLAG(IS_APPLE)
-+#if (defined(PA_HAS_64_BITS_POINTERS) && BUILDFLAG(IS_APPLE)) || \
-+    (BUILDFLAG(IS_FREEBSD) && defined(__i386__))
-   // System page size is not a constant on Apple OSes, but is either 4 or 16kiB
-   // (1 << 12 or 1 << 14), as checked in PartitionRoot::Init(). And
+@@ -144,7 +144,7 @@ struct SlotSpanMetadata {
    // PartitionPageSize() is 4 times the OS page size.
    static constexpr size_t kMaxSlotsPerSlotSpan =
        4 * (1 << 14) / kSmallestBucket;
@@ -17,13 +9,3 @@
    // System page size can be 4, 16, or 64 kiB on Linux on arm64. 64 kiB is
    // currently (kMaxSlotsPerSlotSpanBits == 13) not supported by the code,
    // so we use the 16 kiB maximum (64 kiB will crash).
-@@ -158,7 +159,9 @@ struct SlotSpanMetadata {
- #endif  // defined(PA_HAS_64_BITS_POINTERS) && BUILDFLAG(IS_APPLE)
-   // The maximum number of bits needed to cover all currently supported OSes.
-   static constexpr size_t kMaxSlotsPerSlotSpanBits = 13;
-+#if !BUILDFLAG(IS_FREEBSD) && defined(__i386__)
-   static_assert(kMaxSlotsPerSlotSpan < (1 << kMaxSlotsPerSlotSpanBits), "");
-+#endif
- 
-   // |marked_full| isn't equivalent to being full. Slot span is marked as full
-   // iff it isn't on the active slot span list (or any other list).
