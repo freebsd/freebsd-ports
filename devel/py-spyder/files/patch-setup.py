0ae6b8dc3f2dfb0d0892ea5070d15c9823e2facb
Yuri Victorovich <yuri@FreeBSD.org>
2022-11-06 11:41:08 -0800

    devel/py-spyder: Update 5.3.3 -> 5.4.0
    Reported by:	portscout

--- devel/py-spyder/files/patch-setup.py
+++ devel/py-spyder/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2022-08-29 23:48:19 UTC
+--- setup.py.orig	2022-11-04 20:45:25 UTC
 +++ setup.py
 @@ -91,7 +91,7 @@ def get_data_files():
      """
@@ -17,25 +17,22 @@
  )
  
  
-@@ -229,17 +228,17 @@ install_requires = [
+@@ -229,13 +228,13 @@ install_requires = [
+     'pyls-spyder>=0.4.0',
      'pyqt5<5.16',
      'pyqtwebengine<5.16',
-     'python-lsp-server[all]>=1.5.0,<1.6.0',
+-    'python-lsp-server[all]>=1.6.0,<1.7.0',
 -    'pyxdg>=0.26;platform_system=="Linux"',
++    'python-lsp-server[all]>0', # maybe dangerous to ignore >=1.6.0,<1.7.0
 +    'pyxdg>=0.26',
      'pyzmq>=22.1.0',
 -    'qdarkstyle>=3.0.2,<3.1.0',
 +    'qdarkstyle>=3.0.2',
-     'qstylizer>=0.1.10',
-     'qtawesome>=1.0.2',
--    'qtconsole>=5.3.2,<5.4.0',
+     'qstylizer>=0.2.2',
+-    'qtawesome>=1.2.1',
+-    'qtconsole>=5.4.0,<5.5.0',
++    'qtawesome', # maybe dangerous to ignore >=1.2.1
 +    'qtconsole',
      'qtpy>=2.1.0',
      'rtree>=0.9.7',
      'setuptools>=49.6.0',
-     'sphinx>=0.6.6',
--    'spyder-kernels>=2.3.3,<2.4.0',
-+    'spyder-kernels>=2.3.3,<2.4.1',
-     'textdistance>=4.2.0',
-     'three-merge>=0.1.1',
-     'watchdog>=0.10.3'
--- devel/py-spyder/files/patch-spyder_dependencies.py
+++ devel/py-spyder/files/patch-spyder_dependencies.py
@@ -1,21 +1,22 @@
---- spyder/dependencies.py.orig	2022-08-29 23:48:19 UTC
+--- spyder/dependencies.py.orig	2022-11-04 20:45:25 UTC
 +++ spyder/dependencies.py
-@@ -57,15 +57,15 @@ PYZMQ_REQVER = '>=22.1.0'
+@@ -53,15 +53,15 @@ PSUTIL_REQVER = '>=5.3'
+ PYGMENTS_REQVER = '>=2.0'
+ PYLINT_REQVER = '>=2.5.0;<3.0'
+ PYLINT_VENV_REQVER = '>=2.1.1'
+-PYLSP_REQVER = '>=1.6.0;<1.7.0'
++PYLSP_REQVER = '>0' # until dependency is updated
+ PYLSP_BLACK_REQVER = '>=1.2.0'
  PYLS_SPYDER_REQVER = '>=0.4.0'
  PYXDG_REQVER = '>=0.26'
  PYZMQ_REQVER = '>=22.1.0'
 -QDARKSTYLE_REQVER = '>=3.0.2;<3.1.0'
 +QDARKSTYLE_REQVER = '>=3.0.2'
- QSTYLIZER_REQVER = '>=0.1.10'
- QTAWESOME_REQVER = '>=1.0.2'
--QTCONSOLE_REQVER = '>=5.3.2;<5.4.0'
-+QTCONSOLE_REQVER = '>0'
+ QSTYLIZER_REQVER = '>=0.2.2'
+-QTAWESOME_REQVER = '>=1.2.1'
+-QTCONSOLE_REQVER = '>=5.4.0;<5.5.0'
++QTAWESOME_REQVER = '>0' # until dependency is updated
++QTCONSOLE_REQVER = '>0' # until dependency is updated
  QTPY_REQVER = '>=2.1.0'
  RTREE_REQVER = '>=0.9.7'
  SETUPTOOLS_REQVER = '>=49.6.0'
- SPHINX_REQVER = '>=0.6.6'
--SPYDER_KERNELS_REQVER = '>=2.3.3;<2.4.0'
-+SPYDER_KERNELS_REQVER = '>=2.3.3;<2.4.1'
- TEXTDISTANCE_REQVER = '>=4.2.0'
- THREE_MERGE_REQVER = '>=0.1.1'
- # None for pynsist install for now
