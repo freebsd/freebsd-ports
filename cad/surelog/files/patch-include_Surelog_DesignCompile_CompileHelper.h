be73e47d1d3aff3c70e47a21ef351ac38bcac47d
Yuri Victorovich <yuri@FreeBSD.org>
2022-11-06 14:37:11 -0800

    cad/surelog: Update 1.36 -> 1.45
    Reported by:	portscout

---
+++ cad/surelog/files/patch-include_Surelog_DesignCompile_CompileHelper.h
@@ -0,0 +1,10 @@
+--- include/Surelog/DesignCompile/CompileHelper.h.orig	2022-09-09 21:48:59 UTC
++++ include/Surelog/DesignCompile/CompileHelper.h
+@@ -29,6 +29,7 @@
+ #include <Surelog/Expression/ExprBuilder.h>
+ #include <Surelog/SourceCompile/VObjectTypes.h>
+ 
++#include <filesystem>
+ #include <string>
+ #include <unordered_map>
+ 
---
+++ cad/surelog/files/patch-include_Surelog_Design_ModuleInstance.h
@@ -0,0 +1,10 @@
+--- include/Surelog/Design/ModuleInstance.h.orig	2022-09-09 21:24:46 UTC
++++ include/Surelog/Design/ModuleInstance.h
+@@ -30,6 +30,7 @@
+ #include <Surelog/Design/ValuedComponentI.h>
+ #include <Surelog/SourceCompile/VObjectTypes.h>
+ 
++#include <filesystem>
+ #include <string_view>
+ 
+ namespace SURELOG {
