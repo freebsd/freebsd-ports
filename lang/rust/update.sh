6420d69983d7ef615eb2a78aa64de64abf6a1a2f
Tobias Kortkamp <tobik@FreeBSD.org>
2022-11-08 10:12:59 +0100

    USES=cargo: Fix CARGO_BUILDDEP=any-version
    Fix update.sh to only replace the first version.
    
    (cherry picked from commit 922686e80097c77e700c1788dd78fbd3bd8777a0)

--- lang/rust/update.sh
+++ lang/rust/update.sh
@@ -13,8 +13,11 @@ fetch -qo - https://raw.githubusercontent.com/rust-lang/rust/${new_commit}/src/s
 	"RUST_BOOTSTRAP_VERSION=\(.compiler.version)"
 ' | portedit merge -i .
 
-sed -i '' -E -e "s,(\\$\\{RUST_DEFAULT\\}>=).*(:lang/\\$\\{RUST_DEFAULT\\}),\\1${version}\\2," \
-	../../Mk/Uses/cargo.mk ../../Mk/bsd.gecko.mk
+cat <<EOF | sed -i '' -E -f - ../../Mk/Uses/cargo.mk ../../Mk/bsd.gecko.mk
+1,/\\$\\{RUST_DEFAULT\\}>=/ {
+	s,(\\$\\{RUST_DEFAULT\\}>=).*(:lang/\\$\\{RUST_DEFAULT\\}),\\1${version}\\2,
+}
+EOF
 
 portedit set-version -i "${version}" .
 portedit set-version -i "${version}" ../rust-bootstrap
