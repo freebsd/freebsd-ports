23b7d54e92c90054095d1aabb168fe29e9cab851
Thierry Thomas <thierry@FreeBSD.org>
2022-11-12 11:44:01 +0100

    cad/opencascade: upgrade to 7.7.0
    Release notes at
    <https://www.opencascade.com/open-cascade-technology-7-7-0-released/>.
    
    Differential Revision:	https://reviews.freebsd.org/D36884

--- cad/opencascade/files/patch-src_Standard_Standard__CString.cxx
+++ cad/opencascade/files/patch-src_Standard_Standard__CString.cxx
@@ -1,6 +1,6 @@
---- src/Standard/Standard_CString.cxx.orig	2021-10-30 11:13:58 UTC
+--- src/Standard/Standard_CString.cxx.orig	2022-09-30 11:54:05 UTC
 +++ src/Standard/Standard_CString.cxx
-@@ -82,7 +82,7 @@ Standard_Integer HashCodes (const Standard_CString the
+@@ -80,7 +80,7 @@ Standard_Integer HashCodes (const Standard_CString the
      // strtod, strtol, strtoll functions. For other system with locale-depended
      // implementations problems may appear if "C" locale is not set explicitly.
      #if !defined(__ANDROID__) && !defined(__QNX__) && !defined(__MINGW32__)
