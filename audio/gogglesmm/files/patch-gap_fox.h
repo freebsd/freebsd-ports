015d36da886e30f7f81631032680cb5d51329d23
Jason E. Hale <jhale@FreeBSD.org>
2022-11-14 12:07:05 -0500

    audio/gogglesmm: Fix build with FOX 1.7.81

--- audio/gogglesmm/files/patch-gap_fox.h
+++ audio/gogglesmm/files/patch-gap_fox.h
@@ -1,18 +1,21 @@
-Fix build with FOX 1.7.80
+Fix build with FOX >= 1.7.80
 
 --- gap/fox.h.orig	2020-12-27 21:40:54 UTC
 +++ gap/fox.h
-@@ -35,6 +35,9 @@
+@@ -35,6 +35,12 @@
  #define FXVERSION(major,minor,release) ((release)+(minor*1000)+(major*100000))
  
  #include <fxdefs.h>
 +#if FOXVERSION >= FXVERSION(1, 7, 80)
 +#include <fxchar.h>
 +#endif
++#if FOXVERSION >= FXVERSION(1, 7, 81)
++#include <fxmath.h>
++#endif
  #include <fxendian.h>
  #include <fxascii.h>
  #include <fxunicode.h>
-@@ -121,6 +124,25 @@ constexpr FXTime operator"" _ms(unsigned long long int
+@@ -121,6 +127,25 @@ constexpr FXTime operator"" _ms(unsigned long long int
  {
    return value * NANOSECONDS_PER_MILLISECOND;
  }
