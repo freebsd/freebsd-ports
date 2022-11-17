268a15a15e036328ba29a86ab1670817e1195d99
Thierry Thomas <thierry@FreeBSD.org>
2022-11-17 21:41:22 +0100

    science/paraview: upgrade to 5.11.0

--- science/paraview/files/patch-Qt_Core_pqServerConfiguration.cxx
+++ science/paraview/files/patch-Qt_Core_pqServerConfiguration.cxx
@@ -1,6 +1,6 @@
---- Qt/Core/pqServerConfiguration.cxx.orig	2022-03-11 22:19:30 UTC
+--- Qt/Core/pqServerConfiguration.cxx.orig	2022-11-14 01:44:43 UTC
 +++ Qt/Core/pqServerConfiguration.cxx
-@@ -212,7 +212,7 @@ vtkPVXMLElement* pqServerConfiguration::startupXML() c
+@@ -229,7 +229,7 @@ QString pqServerConfiguration::termCommand()
  //-----------------------------------------------------------------------------
  QString pqServerConfiguration::termCommand()
  {
