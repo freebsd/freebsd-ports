fd34d28a01709696fb40781e122664b090684363
Dave Cottlehuber <dch@FreeBSD.org>
2022-11-17 01:29:06 +0000

    databases/foundationdb: update to 7.1.25
    - drop obsolete -devel variant
    - pet port with portfmt and portclippy
    
    Reported by:    portscout
    Sponsored by:   SkunkWerks, GmbH

--- databases/foundationdb/files/patch-cmake_CompileBoost.cmake
+++ databases/foundationdb/files/patch-cmake_CompileBoost.cmake
@@ -1,11 +1,11 @@
---- cmake/CompileBoost.cmake.orig	2022-04-19 01:08:48 UTC
+--- cmake/CompileBoost.cmake.orig	2022-09-19 11:42:41 UTC
 +++ cmake/CompileBoost.cmake
 @@ -113,7 +113,7 @@ endif()
    return()
  endif()
  
--find_package(Boost 1.72.0 EXACT QUIET COMPONENTS context CONFIG PATHS ${BOOST_HINT_PATHS})
-+find_package(Boost 1.72.0 COMPONENTS context CONFIG PATHS ${BOOST_HINT_PATHS})
+-find_package(Boost 1.78.0 EXACT QUIET COMPONENTS context CONFIG PATHS ${BOOST_HINT_PATHS})
++find_package(Boost 1.78.0 COMPONENTS context CONFIG PATHS ${BOOST_HINT_PATHS})
  set(FORCE_BOOST_BUILD OFF CACHE BOOL "Forces cmake to build boost and ignores any installed boost")
  
  if(Boost_FOUND AND NOT FORCE_BOOST_BUILD)
