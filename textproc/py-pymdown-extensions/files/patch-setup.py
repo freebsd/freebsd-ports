e360b8abfce055f3b674cfbad57d3314855cd701
Kai Knoblich <kai@FreeBSD.org>
2022-11-19 14:44:29 +0100

    textproc/py-pymdown-extensions: Update to 9.4
    Changelog:
    
    https://github.com/facelessuser/pymdown-extensions/releases/tag/9.4

---
+++ textproc/py-pymdown-extensions/files/patch-setup.py
@@ -0,0 +1,42 @@
+Generated via adding
+
+[tool.hatch.build.targets.sdist]
+support-legacy = true
+
+to setup.py and then run "hatchling build".
+
+--- setup.py.orig	2022-11-19 11:48:49 UTC
++++ setup.py
+@@ -0,0 +1,32 @@
++# -*- coding: utf-8 -*-
++from setuptools import setup
++
++setup(
++    name='pymdown-extensions',
++    version='9.4',
++    description='Extension pack for Python Markdown.',
++    long_description='',
++    author_email='Isaac Muse <Isaac.Muse@gmail.com>',
++    classifiers=[
++        'Development Status :: 5 - Production/Stable',
++        'Environment :: Console',
++        'Intended Audience :: Developers',
++        'License :: OSI Approved :: MIT License',
++        'Operating System :: OS Independent',
++        'Programming Language :: Python :: 3',
++        'Programming Language :: Python :: 3.7',
++        'Programming Language :: Python :: 3.8',
++        'Programming Language :: Python :: 3.9',
++        'Programming Language :: Python :: 3.10',
++        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
++        'Topic :: Software Development :: Libraries :: Python Modules',
++        'Topic :: Text Processing :: Filters',
++        'Topic :: Text Processing :: Markup :: HTML',
++    ],
++    install_requires=[
++        'markdown>=3.2',
++    ],
++    packages=[
++        'pymdownx',
++    ],
++)
