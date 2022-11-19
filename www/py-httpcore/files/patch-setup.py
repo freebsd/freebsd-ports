255ca6bac8ef59c39a98b96c5d92dc03cab97f77
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-11-19 16:13:09 +0800

    www/py-httpcore: Fix build after 7f9c0a770d4cb27d603401d526f22c3abc310db6 (py-h11 0.14.0 update)
    - Bump PORTREVISION for package change
    
    Obtained from:	https://github.com/encode/httpcore/commit/4cf288e0007cb73561b9020af9228f076ba2a94e
    Reference:	http://beefy18.nyi.freebsd.org/data/main-amd64-default/p97fda7e3a598_s813c5b75e6/logs/py39-httpcore-0.15.0.log

--- www/py-httpcore/files/patch-setup.py
+++ www/py-httpcore/files/patch-setup.py
@@ -1,11 +1,13 @@
---- setup.py.orig	2022-01-18 11:44:56 UTC
+Obtained from:	https://github.com/encode/httpcore/commit/4cf288e0007cb73561b9020af9228f076ba2a94e
+
+--- setup.py.orig	2022-05-17 12:45:06 UTC
 +++ setup.py
 @@ -54,7 +54,7 @@ setup(
      include_package_data=True,
      zip_safe=False,
      install_requires=[
 -        "h11>=0.11,<0.13",
-+        "h11>=0.11,<0.14",
++        "h11>=0.13,<0.15",
          "sniffio==1.*",
          "anyio==3.*",
          "certifi",
