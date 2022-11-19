bbe39ca3d74a4a7762a5f1145cd7abb87b02b59f
Mikael Urankar <mikael@FreeBSD.org>
2022-11-19 10:26:13 +0100

    www/deno: Update to 1.28.1
    Changes:
    https://github.com/denoland/deno/releases/tag/v1.26.2
    https://github.com/denoland/deno/releases/tag/v1.27.0
    https://github.com/denoland/deno/releases/tag/v1.27.1
    https://github.com/denoland/deno/releases/tag/v1.27.2
    https://github.com/denoland/deno/releases/tag/v1.28.0
    https://github.com/denoland/deno/releases/tag/v1.28.1

---
+++ www/deno/files/patch-runtime_ops_os_sys__info.rs
@@ -0,0 +1,32 @@
+--- runtime/ops/os/sys_info.rs.orig	2022-11-13 21:31:36 UTC
++++ runtime/ops/os/sys_info.rs
+@@ -56,7 +56,7 @@ pub fn os_release() -> String {
+       _ => String::from(""),
+     }
+   }
+-  #[cfg(target_vendor = "apple")]
++  #[cfg(any(target_vendor = "apple", target_os = "freebsd"))]
+   {
+     let mut s = [0u8; 256];
+     let mut mib = [libc::CTL_KERN, libc::KERN_OSRELEASE];
+@@ -296,6 +296,20 @@ pub fn mem_info() -> Option<MemInfo> {
+         mem_info.swap_free = mem_info.swap_total;
+       }
+     }
++  }
++  #[cfg(target_os = "freebsd")]
++  {
++    use sysinfo::{System, SystemExt};
++    let mut sys = System::new_all();
++
++    // First we update all information of our `System` struct.
++    sys.refresh_all();
++
++    mem_info.swap_total = sys.total_swap();
++    mem_info.swap_free = sys.free_swap();
++    mem_info.total = sys.total_memory();
++    mem_info.free = sys.free_memory();
++    mem_info.available = sys.available_memory();
+   }
+ 
+   Some(mem_info)
--- www/deno/files/patch-runtime_ops_tty.rs
+++ www/deno/files/patch-runtime_ops_tty.rs
@@ -1,11 +1,11 @@
 --- runtime/ops/tty.rs.orig	2020-12-14 21:25:05 UTC
 +++ runtime/ops/tty.rs
-@@ -227,7 +227,7 @@ fn op_console_size(
-       let fd = std_file.as_raw_fd();
-       unsafe {
-         let mut size: libc::winsize = std::mem::zeroed();
--        if libc::ioctl(fd, libc::TIOCGWINSZ, &mut size as *mut _) != 0 {
-+        if libc::ioctl(fd, libc::TIOCGWINSZ.into(), &mut size as *mut _) != 0 {
-           return Err(Error::last_os_error().into());
-         }
- 
+@@ -232,7 +232,7 @@ fn op_console_size(
+         #[allow(clippy::undocumented_unsafe_blocks)]
+         unsafe {
+           let mut size: libc::winsize = std::mem::zeroed();
+-          if libc::ioctl(fd, libc::TIOCGWINSZ, &mut size as *mut _) != 0 {
++          if libc::ioctl(fd, libc::TIOCGWINSZ.into(), &mut size as *mut _) != 0 {
+             return Err(Error::last_os_error().into());
+           }
+           result[0] = size.ws_col as u32;
