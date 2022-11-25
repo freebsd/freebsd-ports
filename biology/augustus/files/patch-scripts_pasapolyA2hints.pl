8b43ef57fd38af532b6619eaade5b35b053b4afd
Yuri Victorovich <yuri@FreeBSD.org>
2022-11-25 15:50:50 -0800

    biology/augustus: New port: Genome annotation tool

---
+++ biology/augustus/files/patch-scripts_pasapolyA2hints.pl
@@ -0,0 +1,8 @@
+--- scripts/pasapolyA2hints.pl.orig	2022-11-25 23:03:59 UTC
++++ scripts/pasapolyA2hints.pl
+@@ -1,4 +1,4 @@
+-#!/usr/bin/bin env
++#!/usr/bin/env perl
+ use strict;
+ use warnings;
+ use Getopt::Long;
