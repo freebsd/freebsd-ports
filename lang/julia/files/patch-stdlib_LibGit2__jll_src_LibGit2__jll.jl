cfd86e4d15036cc6c70b9e00f269c4b505881d88
Wen Heping <wen@FreeBSD.org>
2022-11-26 06:10:19 +0000

    lang/julia: Update to 1.8.3
    PR:		267889
    Reported by:	wen@
    Approved by:	maintainer
    
    (cherry picked from commit 2a86b2d73e641a22c7a78d2df93f3aa08080ec95)

--- lang/julia/files/patch-stdlib_LibGit2__jll_src_LibGit2__jll.jl
+++ lang/julia/files/patch-stdlib_LibGit2__jll_src_LibGit2__jll.jl
@@ -1,10 +1,10 @@
---- stdlib/LibGit2_jll/src/LibGit2_jll.jl.orig	2021-11-29 02:21:22 UTC
+--- stdlib/LibGit2_jll/src/LibGit2_jll.jl.orig	2022-11-21 00:23:36 UTC
 +++ stdlib/LibGit2_jll/src/LibGit2_jll.jl
-@@ -23,7 +23,7 @@ if Sys.iswindows()
+@@ -23,7 +23,7 @@ else
  elseif Sys.isapple()
-     const libgit2 = "@rpath/libgit2.1.1.dylib"
+     const libgit2 = "@rpath/libgit2.1.3.dylib"
  else
--    const libgit2 = "libgit2.so.1.1"
+-    const libgit2 = "libgit2.so.1.3"
 +    const libgit2 = "libgit2.so"
  end
  
--- lang/julia/files/patch-stdlib_MbedTLS__jll_src_MbedTLS__jll.jl
+++ lang/julia/files/patch-stdlib_MbedTLS__jll_src_MbedTLS__jll.jl
@@ -1,11 +1,11 @@
---- stdlib/MbedTLS_jll/src/MbedTLS_jll.jl.orig	2021-11-25 11:01:50 UTC
+--- stdlib/MbedTLS_jll/src/MbedTLS_jll.jl.orig	2022-11-21 01:45:43 UTC
 +++ stdlib/MbedTLS_jll/src/MbedTLS_jll.jl
-@@ -31,9 +31,9 @@ elseif Sys.isapple()
-     const libmbedtls = "@rpath/libmbedtls.13.dylib"
+@@ -31,9 +31,9 @@ else
+     const libmbedtls = "@rpath/libmbedtls.14.dylib"
      const libmbedx509 = "@rpath/libmbedx509.1.dylib"
  else
--    const libmbedcrypto = "libmbedcrypto.so.5"
--    const libmbedtls = "libmbedtls.so.13"
+-    const libmbedcrypto = "libmbedcrypto.so.7"
+-    const libmbedtls = "libmbedtls.so.14"
 -    const libmbedx509 = "libmbedx509.so.1"
 +    const libmbedcrypto = "libmbedcrypto.so"
 +    const libmbedtls = "libmbedtls.so"
