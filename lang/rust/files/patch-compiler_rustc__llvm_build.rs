d5e92a6ee45c44a0fb52cbba968d5d38b4279757
Piotr Kubaj <pkubaj@FreeBSD.org>
2022-11-26 01:03:15 +0100

    lang/rust: don't link to libatomic on armv7
    Reported by:	mikael

--- lang/rust/files/patch-compiler_rustc__llvm_build.rs
+++ lang/rust/files/patch-compiler_rustc__llvm_build.rs
@@ -1,18 +1,6 @@
-From cbeec86a5594ef6ca36d0ea24b4cb7e3dbaded87 Mon Sep 17 00:00:00 2001
-From: pkubaj <pkubaj@FreeBSD.org>
-Date: Fri, 18 Nov 2022 12:36:49 +0000
-Subject: [PATCH] Fix build on powerpc-unknown-freebsd
-
-Probably also fixes build on arm and mips*. Related to https://github.com/rust-lang/rust/issues/104220
----
- compiler/rustc_llvm/build.rs | 15 +++++++++------
- 1 file changed, 9 insertions(+), 6 deletions(-)
-
-diff --git a/compiler/rustc_llvm/build.rs b/compiler/rustc_llvm/build.rs
-index d35e4191cc0b1..afdf9ac8883e9 100644
 --- compiler/rustc_llvm/build.rs
 +++ compiler/rustc_llvm/build.rs
-@@ -237,18 +237,21 @@ fn main() {
+@@ -237,18 +237,20 @@
  
      if !is_crossed {
          cmd.arg("--system-libs");
@@ -22,10 +10,10 @@ index d35e4191cc0b1..afdf9ac8883e9 100644
 -    } else if target.contains("netbsd") || target.contains("haiku") || target.contains("darwin") {
 -        println!("cargo:rustc-link-lib=z");
 -    } else if target.starts_with("arm")
+-        || target.starts_with("mips-")
 +    }
 +
-+    if target.starts_with("arm")
-         || target.starts_with("mips-")
++    if target.starts_with("mips")
          || target.starts_with("mipsel-")
          || target.starts_with("powerpc-")
      {
