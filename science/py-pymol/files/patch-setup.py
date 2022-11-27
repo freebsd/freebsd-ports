a2b852ee70ee234f4b2360f72ea2d89f48943b37
Yuri Victorovich <yuri@FreeBSD.org>
2022-11-27 12:58:44 -0800

    science/py-pymol: Update 2.4.0 -> 2.5.0-97

--- science/py-pymol/files/patch-setup.py
+++ science/py-pymol/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2019-02-12 06:00:47 UTC
+--- setup.py.orig	2021-05-10 18:38:37 UTC
 +++ setup.py
-@@ -52,6 +52,8 @@ try:
+@@ -53,6 +53,8 @@ try:
              "shared library; no: disable fast MMTF load support")
      parser.add_argument('--help-distutils', action="store_true",
              help="show help for distutils options and exit")
@@ -8,4 +8,13 @@
 +            help="single-version-externally-managed")
      parser.add_argument('--testing', action="store_true",
              help="Build C-level tests")
-     options, sys.argv[1:] = parser.parse_known_args(namespace=options)
+     parser.add_argument('--openvr', dest='openvr', action='store_true')
+@@ -282,7 +284,7 @@ if DEBUG and not WIN:
+ libs = ["png", "freetype"]
+ lib_dirs = []
+ ext_comp_args = [
+-    "-Werror=return-type",
++    #"-Werror=return-type",
+     "-Wunused-variable",
+     "-Wno-switch",
+     "-Wno-narrowing",
