f91d5bcf05a78c2855a100e4e7bc192440043164
Naram Qashat <cyberbotx@cyberbotx.com>
2022-11-28 07:42:11 +0100

    x11/workrave: Update to 1.10.50
    Changelogs since last update:
    
    * https://github.com/rcaelers/workrave/releases/tag/v1_10_45
    * https://github.com/rcaelers/workrave/releases/tag/v1_10_47
    * https://github.com/rcaelers/workrave/releases/tag/v1_10_48
    * https://github.com/rcaelers/workrave/releases/tag/v1_10_49
    * https://github.com/rcaelers/workrave/releases/tag/v1_10_50
    
    PR:		268027
    Reported by:	cyberbotx@cyberbotx.com (maintainer)

--- x11/workrave/files/patch-backend_include_ICore.hh
+++ x11/workrave/files/patch-backend_include_ICore.hh
@@ -1,11 +1,11 @@
 This is needed for compiling on FreeBSD >= 11.0.
---- backend/include/ICore.hh.orig	2016-08-03 13:08:26 UTC
+--- backend/include/ICore.hh.orig	2022-04-01 16:28:54 UTC
 +++ backend/include/ICore.hh
 @@ -21,6 +21,7 @@
  #define ICORE_HH
  
  #include <string>
 +#include <ctime>
+ #include <iostream>
  
  #include "enum.h"
- 
