635f402b82017c34c6040ace1b590a6196e710da
Emanuel Haupt <ehaupt@FreeBSD.org>
2022-12-07 19:15:38 +0100

    deskutils/arttime: Update to 1.9.8

--- deskutils/arttime/files/patch-install.sh
+++ deskutils/arttime/files/patch-install.sh
@@ -1,6 +1,6 @@
---- install.sh.orig	2022-09-24 00:11:14 UTC
+--- install.sh.orig	2022-12-06 05:41:43 UTC
 +++ install.sh
-@@ -119,9 +119,9 @@ function printdirerror {
+@@ -142,9 +142,9 @@ function printdirerror {
      fi
  }
  
@@ -13,7 +13,7 @@
  
  printdirerror $installdircode $installdir
  printdirerror $bindircode $bindir
-@@ -139,8 +139,8 @@ installerdir="${0:a:h}"
+@@ -162,8 +162,8 @@ installerdir="${0:a:h}"
  
  # Copy bin files
  cd $installerdir/bin
@@ -24,7 +24,7 @@
  
  # Copy share files
  cd $installerdir/share/arttime/textart
-@@ -156,10 +156,10 @@ for ((i = 1; i <= $artfilearraysize; i++)); do
+@@ -179,10 +179,10 @@ for ((i = 1; i <= $artfilearraysize; i++)); do
          oldmessage='"Custom message for art goes here"'
          oldmessage="$(head -n1 $artdir/$file)"
          newart="$(tail -n +2 $file)"
@@ -38,9 +38,12 @@
      fi
      percentdone=$(((i-1.0)/(artfilearraysize-1.0)*100.0))
      [[ $percentdone -lt 1 ]] && percentdone="0"
-@@ -191,3 +191,5 @@ else
-     fi
+@@ -227,6 +227,8 @@ elif [[ $machine =~ ^Darwin.*$ ]]; then
+     echo "[4mNote[0m: Notification settings on macOS are not fully in control of an application.\n      To check if you have desired notification settings, open following link.\n      https://github.com/poetaman/arttime/issues/11"
  fi
- tput cnorm
-+
+ 
++echoti cnorm
 +exit 0
+ # Check if path to arttime excutable is on user's $PATH
+ if [[ ":$PATH:" == *":$bindir:"* ]]; then
+     echo "Installation complete!\nType 'arttime' and press Enter to start arttime."
