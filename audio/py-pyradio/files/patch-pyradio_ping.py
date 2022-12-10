584a88ad25752c63b8951cf38fe8ab418942fb2d
Nicola Vitale <nivit@FreeBSD.org>
2022-12-10 14:48:15 +0100

    audio/py-pyradio: Update to 0.8.9.32
    - Install additional files (.desktop and icon)
    - Add a post-patch target to set up default configuration
    - Add desktop-file-utils to USES
    
    Changelog:	https://github.com/coderholic/pyradio/releases/tag/0.8.9.32

--- audio/py-pyradio/files/patch-pyradio_ping.py
+++ audio/py-pyradio/files/patch-pyradio_ping.py
@@ -1,11 +1,11 @@
---- pyradio/ping.py.orig	2022-11-25 15:21:16 UTC
+--- pyradio/ping.py.orig	2022-12-09 15:36:15 UTC
 +++ pyradio/ping.py
-@@ -35,7 +35,7 @@ def linux_ping(server, count=1, timeout_in_seconds=1):
-             -1: error
-     '''
-     try:
--        r=subprocess.Popen(['ping', '-c', str(count), '-w', str(timeout_in_seconds), server ], stdout=subprocess.PIPE).stdout.read()
-+        r=subprocess.Popen(['ping', '-c', str(count), '-t', str(timeout_in_seconds), server ], stdout=subprocess.PIPE).stdout.read()
-         # print(r)
-         return 0 if '100%' in str(r) else 1
-     except:
+@@ -49,7 +49,7 @@ def linux_ping(server, count=1, timeout_in_seconds=1):
+             return 0 if '100%' in out[0] else 1
+         else:
+             r=subprocess.Popen(
+-                ['ping', '-c', str(count), '-w',
++                ['ping', '-c', str(count), '-t',
+                  str(timeout_in_seconds), server],
+                 stderr=subprocess.DEVNULL,
+                 stdout=subprocess.PIPE
