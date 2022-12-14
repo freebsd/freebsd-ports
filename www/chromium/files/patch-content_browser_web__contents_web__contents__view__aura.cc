ef010e428a842c90921884c571007413bd9a2153
Robert Nagy <robert@openbsd.org>
2022-12-14 12:39:54 +0100

    www/chromium: update to 108.0.5359.124
    (cherry picked from commit 7ae5e0e6d6eec52644461fd8cc1fcae368376202)

--- www/chromium/files/patch-content_browser_web__contents_web__contents__view__aura.cc
+++ www/chromium/files/patch-content_browser_web__contents_web__contents__view__aura.cc
@@ -1,4 +1,4 @@
---- content/browser/web_contents/web_contents_view_aura.cc.orig	2022-10-24 13:33:33 UTC
+--- content/browser/web_contents/web_contents_view_aura.cc.orig	2022-12-14 08:02:29 UTC
 +++ content/browser/web_contents/web_contents_view_aura.cc
 @@ -176,7 +176,7 @@ class WebDragSourceAura : public content::WebContentsO
    raw_ptr<aura::Window> window_;
@@ -18,7 +18,7 @@
    // We set the file contents before the URL because the URL also sets file
    // contents (to a .URL shortcut).  We want to prefer file content data over
    // a shortcut so we add it first.
-@@ -1365,7 +1365,7 @@ void WebContentsViewAura::OnMouseEvent(ui::MouseEvent*
+@@ -1366,7 +1366,7 @@ void WebContentsViewAura::OnMouseEvent(ui::MouseEvent*
      // Linux window managers like to handle raise-on-click themselves.  If we
      // raise-on-click manually, this may override user settings that prevent
      // focus-stealing.
