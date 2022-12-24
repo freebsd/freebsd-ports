caa4d8eb629255d8ee5faed2c78c55446ece76d2
Mikael Urankar <mikael@FreeBSD.org>
2022-12-24 18:23:16 +0100

    www/deno: Update to 1.29.1
    Changes:
      https://github.com/denoland/deno/releases/tag/v1.29.0
      https://github.com/denoland/deno/releases/tag/v1.29.1

--- www/deno/files/patch-runtime_ops_tty.rs
+++ www/deno/files/patch-runtime_ops_tty.rs
@@ -1,11 +1,11 @@
 --- runtime/ops/tty.rs.orig	2020-12-14 21:25:05 UTC
 +++ runtime/ops/tty.rs
-@@ -232,7 +232,7 @@ fn op_console_size(
-         #[allow(clippy::undocumented_unsafe_blocks)]
-         unsafe {
-           let mut size: libc::winsize = std::mem::zeroed();
--          if libc::ioctl(fd, libc::TIOCGWINSZ, &mut size as *mut _) != 0 {
-+          if libc::ioctl(fd, libc::TIOCGWINSZ.into(), &mut size as *mut _) != 0 {
-             return Err(Error::last_os_error().into());
-           }
-           result[0] = size.ws_col as u32;
+@@ -260,7 +260,7 @@ pub fn console_size(
+     // SAFETY: libc calls
+     unsafe {
+       let mut size: libc::winsize = std::mem::zeroed();
+-      if libc::ioctl(fd, libc::TIOCGWINSZ, &mut size as *mut _) != 0 {
++      if libc::ioctl(fd, libc::TIOCGWINSZ.into(), &mut size as *mut _) != 0 {
+         return Err(Error::last_os_error());
+       }
+       Ok(ConsoleSize {
