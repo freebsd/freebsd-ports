097770c474d4bb7b24cec37dc87439c1ed4344c7
Yasuhiro Kimura <yasu@FreeBSD.org>
2022-12-27 08:24:41 +0900

    Mk/bsd.default-versions.mk: Change default version of Ruby
    * Change default version of Ruby from 3.0 to 3.1.
    * Bump PORTREVISION of lang/ruby3[01]. They need to be rebuilt as only
      default ruby package includes ${PREFIX}/bin/ruby and
      ${PREFIX}/man/man1/ruby.1.gz symlinks.
    * Add entry about change of default Ruby version to UPDATING.
    
    PR:		268586
    Exp-run by:	antoine
    Approved by:	meta (ruby, maintainer)

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -123,7 +123,7 @@ PYTHON2_DEFAULT?=	2.7
 # Possible values: 3.7, 3.8, 3.9, 3.10, 3.11
 PYTHON3_DEFAULT?=	3.9
 # Possible values: 2.7, 3.0, 3.1, 3.2
-RUBY_DEFAULT?=		3.0
+RUBY_DEFAULT?=		3.1
 # Possible values: rust, rust-nightly
 RUST_DEFAULT?=		rust
 # Possible values: 4.13, 4.16
