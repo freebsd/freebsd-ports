1e4554730637ed854156dead62be601f37c4443f
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-12-30 16:42:42 +0800

    devel/py-medikit: Fix build after 6aaf332f9fe68aca8f8824a17cc18b1434f4bdba
    - Allow build with py-stevedore 4.0.0+
    - Bump PORTREVISION for package change

--- devel/py-medikit/files/patch-setup.py
+++ devel/py-medikit/files/patch-setup.py
@@ -1,6 +1,6 @@
 --- setup.py.orig	2020-11-25 07:42:37 UTC
 +++ setup.py
-@@ -56,11 +56,11 @@ setup(
+@@ -56,12 +56,12 @@ setup(
      include_package_data=True,
      install_requires=[
          "git-semver ~= 0.3.2",
@@ -10,7 +10,9 @@
          "packaging ~= 20.0",
          "pip-tools ~= 4.5.0",
 -        "semantic_version < 2.7",
+-        "stevedore ~= 3.0",
 +        "semantic_version ~= 2.8",
-         "stevedore ~= 3.0",
++        "stevedore >= 3.0",
          "whistle ~= 1.0",
          "yapf ~= 0.20",
+     ],
