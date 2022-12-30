f2d3d014c477858c2469328928193d29f1c11eb9
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-12-30 16:43:35 +0800

    devel/py-watchfiles: Update to 0.18.1
    Changes:	https://github.com/samuelcolvin/watchfiles/releases

--- devel/py-watchfiles/files/setup.py
+++ devel/py-watchfiles/files/setup.py
@@ -9,7 +9,7 @@ setup(
     url = 'https://github.com/samuelcolvin/watchfiles',
     packages = ['watchfiles'],
     package_data = {'': ['*']},
-    install_requires = ['anyio >= 3.0.0, <4'],
+    install_requires = ['anyio >= 3.0.0'],
     entry_points = {'console_scripts': ['watchfiles = watchfiles.cli:cli']},
     python_requires = '>= 3.7',
 )
