aa17ba01c99c79ec9ebfa4af466fae1631bacfd5
Li-Wen Hsu <lwhsu@FreeBSD.org>
2022-12-30 18:00:29 +0800

    Add chinese/fcitx5-mcbopomofo: McBopomofo for Fcitx5

---
+++ chinese/fcitx5-mcbopomofo/files/patch-src_McBopomofo.cpp
@@ -0,0 +1,10 @@
+--- src/McBopomofo.cpp.orig	2022-12-30 09:01:33 UTC
++++ src/McBopomofo.cpp
+@@ -31,6 +31,7 @@
+ #include <fmt/format.h>
+ 
+ #include <memory>
++#include <sstream>
+ #include <unordered_map>
+ #include <utility>
+ 
