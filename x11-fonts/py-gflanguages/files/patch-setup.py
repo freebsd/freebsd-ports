beafd31389ec70b7d58821b67617e7cf42d4be40
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-12-30 16:47:02 +0800

    x11-fonts/py-gflanguages: Update to 0.4.3
    Changes:	https://github.com/googlefonts/lang/releases

--- x11-fonts/py-gflanguages/files/patch-setup.py
+++ x11-fonts/py-gflanguages/files/patch-setup.py
@@ -1,11 +1,15 @@
---- setup.py.orig	2022-03-04 21:26:42 UTC
+--- setup.py.orig	2022-11-14 13:16:17 UTC
 +++ setup.py
-@@ -51,7 +51,7 @@ setup(
+@@ -51,10 +51,10 @@ setup(
          'Programming Language :: Python :: 3'
      ],
      python_requires=">=3.7",
 -    setup_requires=['setuptools_scm>=4,<6.1'],
 +    setup_requires=['setuptools_scm>=4'],
      install_requires=[
-         'protobuf'
+         # 3.7.0 fixed a bug on parsing some METADATA.pb files.
+         # We cannot use v4 because our protobuf files have been compiled with v3.
+-        'protobuf>=3.7.0, <4',
++        'protobuf>=3.7.0',
      ]
+ )
