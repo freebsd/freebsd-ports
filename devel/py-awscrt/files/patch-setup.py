25a370e86cbed229347b8358aacb14ed6f362c26
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2022-12-30 16:41:59 +0800

    devel/py-awscrt: Update to 0.15.3
    Changes:	https://github.com/awslabs/aws-crt-python/releases

--- devel/py-awscrt/files/patch-setup.py
+++ devel/py-awscrt/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2022-07-12 00:42:43 UTC
+--- setup.py.orig	2022-11-09 18:39:08 UTC
 +++ setup.py
-@@ -315,7 +315,7 @@ def awscrt_ext():
- 
-     if distutils.ccompiler.get_default_compiler() != 'msvc':
-         extra_compile_args += ['-Wextra', '-Werror', '-Wno-strict-aliasing', '-std=gnu99']
--        extra_link_args += ['-Wl,-fatal_warnings']
-+        extra_link_args += ['-Wl,-fatal-warnings']
+@@ -320,7 +320,7 @@ def awscrt_ext():
+             # a proper MacOS Universal2 binary. The linker warns us about this,
+             # but WHATEVER. Building everything twice (x86_64 and arm64) takes too long.
+             if not is_macos_universal2():
+-                extra_link_args += ['-Wl,-fatal_warnings']
++                extra_link_args += ['-Wl,-fatal-warnings']
  
      return setuptools.Extension(
          '_awscrt',
