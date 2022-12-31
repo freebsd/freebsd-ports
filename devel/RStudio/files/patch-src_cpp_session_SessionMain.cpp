eff3aa5c32f4d02d041aa04d67cf55bdc15b80eb
Yuri Victorovich <yuri@FreeBSD.org>
2022-12-31 02:05:28 -0800

    devel/RStudio: Update 2022.07.2+576 -> 2022.12.0+353

--- devel/RStudio/files/patch-src_cpp_session_SessionMain.cpp
+++ devel/RStudio/files/patch-src_cpp_session_SessionMain.cpp
@@ -1,7 +1,7 @@
---- src/cpp/session/SessionMain.cpp.orig	2020-01-23 23:30:24 UTC
+--- src/cpp/session/SessionMain.cpp.orig	2022-12-03 07:38:59 UTC
 +++ src/cpp/session/SessionMain.cpp
-@@ -1711,7 +1711,11 @@ int main (int argc, char * const argv[]) 
-       
+@@ -1947,7 +1947,11 @@ int main(int argc, char * const argv[])
+ 
        // move to own process group
  #ifndef _WIN32
 +#if defined(__FreeBSD__)
@@ -11,4 +11,4 @@
 +#endif
  #endif
  
-       // get main thread id (used to distinguish forks which occur
+       logStartingEnv();
