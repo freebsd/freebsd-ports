044c0a783c36b3ac2244c44e7af78fe158c599af
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2022-12-31 15:56:16 -0600

    emulators/qemu: Update version 7.1.0=>7.2.0
    - Disable guest-agent
    
    Relnotes: https://wiki.qemu.org/ChangeLog/7.2

--- emulators/qemu/files/patch-bsd-user_host_i386_host-signal.h
+++ emulators/qemu/files/patch-bsd-user_host_i386_host-signal.h
@@ -1,12 +1,10 @@
---- bsd-user/host/i386/host-signal.h.orig	2022-09-08 16:00:45 UTC
+--- bsd-user/host/i386/host-signal.h.orig	2022-12-14 16:28:45 UTC
 +++ bsd-user/host/i386/host-signal.h
-@@ -9,6 +9,9 @@
- #ifndef I386_HOST_SIGNAL_H
- #define I386_HOST_SIGNAL_H
- 
-+#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__)
-+#include <sys/param.h>
-+#endif
+@@ -13,6 +13,7 @@
  #include <sys/ucontext.h>
  #include <machine/trap.h>
  #include <vm/pmap.h>
++#include <vm/vm.h>
+ #include <machine/pmap.h>
+ 
+ static inline uintptr_t host_signal_pc(ucontext_t *uc)
