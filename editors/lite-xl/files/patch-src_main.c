25361c4c5ebb910763771b81d2ade58683f3a614
Alexey Dokuchaev <danfe@FreeBSD.org>
2023-01-03 04:06:22 +0000

    editors/lite-xl: the port had been updated to version 2.1.1
    - Pull plugins and color themes to their respective latest commits
    - Clean up the patch after most of it had been integrated upstream
    
    Reported by:	portscout

--- editors/lite-xl/files/patch-src_main.c
+++ editors/lite-xl/files/patch-src_main.c
@@ -1,39 +1,12 @@
---- src/main.c.orig	2022-11-02 02:38:58 UTC
+--- src/main.c.orig	2022-12-29 02:03:36 UTC
 +++ src/main.c
-@@ -7,11 +7,14 @@
- 
- #ifdef _WIN32
-   #include <windows.h>
--#elif __linux__ || __FreeBSD__
-+#elif __linux__
-   #include <unistd.h>
-   #include <signal.h>
- #elif __APPLE__
-   #include <mach-o/dyld.h>
-+#elif __FreeBSD__
-+  #include <sys/sysctl.h>
-+  #include <signal.h>
- #endif
- 
- 
-@@ -43,6 +46,10 @@ static void get_exe_filename(char *buf, int sz) {
-   char exepath[size];
-   _NSGetExecutablePath(exepath, &size);
-   realpath(exepath, buf);
-+#elif __FreeBSD__
-+  size_t len = sz;
-+  const int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
-+  sysctl(mib, 4, buf, &len, NULL, 0);
- #else
-   strcpy(buf, "./lite");
- #endif
-@@ -91,6 +98,10 @@ void set_macos_bundle_resources(lua_State *L);
+@@ -99,6 +99,10 @@ void set_macos_bundle_resources(lua_State *L);
      #define ARCH_PROCESSOR "aarch64"
-   #elif __arm__
+   #elif defined(__arm__) || defined(_M_ARM)
      #define ARCH_PROCESSOR "arm"
-+  #elif __powerpc64__
++  #elif defined(__powerpc64__)
 +    #define ARCH_PROCESSOR "ppc64"
-+  #elif __powerpc__
++  #elif defined(__powerpc__)
 +    #define ARCH_PROCESSOR "ppc"
    #endif
  
