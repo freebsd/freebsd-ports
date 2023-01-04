0ce58718554603ba9b178a44857e3b088ed0bb0f
Yuri Victorovich <yuri@FreeBSD.org>
2023-01-04 00:29:55 -0800

    math/symfpu: New port: Implementation of IEEE-754 / SMT-LIB floating-point

---
+++ math/symfpu/files/patch-baseTypes_simpleExecutable.cpp
@@ -0,0 +1,11 @@
+--- baseTypes/simpleExecutable.cpp.orig	2023-01-04 05:09:43 UTC
++++ baseTypes/simpleExecutable.cpp
+@@ -28,7 +28,7 @@
+ */
+ 
+ 
+-#include "symfpu/baseTypes/simpleExecutable.h"
++#include "baseTypes/simpleExecutable.h"
+ 
+ #include <assert.h>
+ #include <math.h>
