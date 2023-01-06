c447a044bbc1aa83319aef908ab81460ac726248
Daniel Engberg <diizzy@FreeBSD.org>
2023-01-06 20:26:29 +0100

    Mk/Features/lto.mk: Use DEV_WARNING instead of IGNORE
    Instead of using IGNORE issue a warning via DEV_WARNING instead.
    Change suggested by tobik
    
    Reviewed by:	pkubaj (via private mail)

--- Mk/Features/lto.mk
+++ Mk/Features/lto.mk
@@ -9,7 +9,7 @@ LTO_Include_MAINTAINER=	pkubaj@FreeBSD.org
 
 .  if !defined(LTO_UNSAFE) || defined(LTO_DISABLE_CHECK)
 .    if "${ARCH}" == "powerpc64" || "${ARCH}" == "riscv64" && !defined(LTO_DISABLE_CHECK)
-       IGNORE=	LTO is currently broken on ${ARCH}, to override set LTO_DISABLE_CHECK=yes
+       DEV_WARNING+=	"LTO is currently broken on powerpc64 and riscv64, to override set LTO_DISABLE_CHECK=yes"
 .    elif defined(_INCLUDE_USES_CARGO_MK)
    CARGO_ENV+=	CARGO_PROFILE_RELEASE_LTO="true" \
 		CARGO_PROFILE_RELEASE_PANIC="abort" \
