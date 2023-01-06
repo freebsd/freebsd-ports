c7ae8f5fe345c635b298b1aebc6ace0834c1a0ae
Yuri Victorovich <yuri@FreeBSD.org>
2023-01-06 07:31:22 -0800

    graphics/bgfx: New port: Cross-platform, graphics API agnostic rendering library

---
+++ graphics/bgfx/files/patch-bx_include_bx_bx.h
@@ -0,0 +1,11 @@
+--- bx/include/bx/bx.h.orig	2023-01-06 20:55:11 UTC
++++ bx/include/bx/bx.h
+@@ -6,7 +6,7 @@
+ #ifndef BX_H_HEADER_GUARD
+ #define BX_H_HEADER_GUARD
+ 
+-#include <alloca.h> // alloca
++//#include <alloca.h> // alloca
+ #include <stdarg.h> // va_list
+ #include <stdint.h> // uint32_t
+ #include <stdlib.h> // size_t
