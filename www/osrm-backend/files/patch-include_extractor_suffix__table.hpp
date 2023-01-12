da6ca1a63052adf4ee397bd775c42b794d3849ac
Dima Panov <fluffy@FreeBSD.org>
2023-01-12 18:50:15 +0300

    www/osrm-backend: prepare for upcoming Boost-1.81
    With hat:	office

---
+++ www/osrm-backend/files/patch-include_extractor_suffix__table.hpp
@@ -0,0 +1,10 @@
+--- include/extractor/suffix_table.hpp.orig	2021-09-21 21:49:41 UTC
++++ include/extractor/suffix_table.hpp
+@@ -2,6 +2,7 @@
+ #define OSRM_EXTRACTOR_SUFFIX_LIST_HPP_
+ 
+ #include <string>
++#include <vector>
+ #include <unordered_set>
+ 
+ #include "util/string_view.hpp"
