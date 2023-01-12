ab08d9f32d37cd259cb666d7b1d26d0337bb62c3
Dima Panov <fluffy@FreeBSD.org>
2023-01-12 18:23:57 +0300

    science/dakota: prepare for upcoming Boost-1.81
    With hat:	office

---
+++ science/dakota/files/patch-packages_external_muq2_MUQ_Modeling_WorkPiece.h
@@ -0,0 +1,10 @@
+--- packages/external/muq2/MUQ/Modeling/WorkPiece.h.orig	2022-05-09 04:14:50 UTC
++++ packages/external/muq2/MUQ/Modeling/WorkPiece.h
+@@ -9,6 +9,7 @@
+ #include<cassert>
+ #include<memory>
+ #include<string>
++#include<deque>
+ 
+ #include "boost/any.hpp"
+ #include "boost/optional.hpp"
