fec8825b724037d21d160618b314b66be29e4d3c
Alexey Dokuchaev <danfe@FreeBSD.org>
2023-01-12 02:44:44 +0000

    games/doomlegacy: update Doom Legacy to version 1.48.12 (rev 1643)
    Now have to unset SVN_ENABLE so that run-time SVN revision reporting
    works, and rename one patch file to match modern naming conventions.
    
    Reported by:	portscout

--- games/doomlegacy/files/patch-src_sdl_i__system.c
+++ games/doomlegacy/files/patch-src_sdl_i__system.c
@@ -1,11 +1,12 @@
---- src/sdl/i_system.c.orig	2014-05-16 20:11:49 UTC
+--- src/sdl/i_system.c.orig	2022-12-18 00:00:15 UTC
 +++ src/sdl/i_system.c
-@@ -508,6 +508,8 @@ void I_SysInit(void)
+@@ -932,6 +932,9 @@ void I_SysInit(void)
  
    // Enable unicode key conversion
    SDL_EnableUNICODE(1);
++
 +  // Enable key auto repeat
 +  SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
+ #endif
  
    // Initialize the joystick subsystem.
-   I_JoystickInit();
