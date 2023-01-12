1f1603eb7542655c5a5cb86119ffeb67354d2c01
Dima Panov <fluffy@FreeBSD.org>
2023-01-12 21:58:41 +0300

    graphics/gnash: prepare for upcoming Boost-1.81
    With hat:	office

---
+++ graphics/gnash/files/patch-libcore_asobj_Array__as.cpp
@@ -0,0 +1,10 @@
+--- libcore/asobj/Array_as.cpp.orig	2016-07-21 12:25:09 UTC
++++ libcore/asobj/Array_as.cpp
+@@ -25,6 +25,7 @@
+ #include <cmath>
+ #include <functional>
+ #include <iterator>
++#include <list>
+ #include <boost/algorithm/string/case_conv.hpp>
+ #include <boost/lexical_cast.hpp>
+ 
