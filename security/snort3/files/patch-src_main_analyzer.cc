80502c020b260f1dc88a57b21845d88054459195
Dan Langille <dvl@FreeBSD.org>
2023-01-13 17:23:42 +0000

    security/snort3: Update to 3.1.51.0
    re: https://github.com/snort3/snort3/releases/tag/3.1.51.0
    
    This new patch file is temporary and is known upstream.
    
    Approved by:	maintainer (via private email)

---
+++ security/snort3/files/patch-src_main_analyzer.cc
@@ -0,0 +1,10 @@
+--- src/main/analyzer.cc.orig	2023-01-13 17:19:37 UTC
++++ src/main/analyzer.cc
+@@ -67,6 +67,7 @@
+ #include "time/packet_time.h"
+ #include "trace/trace_api.h"
+ #include "utils/stats.h"
++#include "utils/util.h"
+ 
+ #include "analyzer_command.h"
+ #include "oops_handler.h"
