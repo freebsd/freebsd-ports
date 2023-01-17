8f5465b112babf5ad42ced7fa845876d8acc0210
Dima Panov <fluffy@FreeBSD.org>
2023-01-17 10:04:42 +0300

    devel/simgear: fix build with Boost-1.81
    Wit hat:	office

---
+++ devel/simgear/files/patch-simgear_scene_viewer_Compositor.hxx
@@ -0,0 +1,10 @@
+--- simgear/scene/viewer/Compositor.hxx
++++ simgear/scene/viewer/Compositor.hxx
+@@ -17,6 +17,7 @@
+ #ifndef SG_COMPOSITOR_HXX
+ #define SG_COMPOSITOR_HXX
+ 
++#include <array>
+ #include <unordered_map>
+ #include <vector>
+ 
