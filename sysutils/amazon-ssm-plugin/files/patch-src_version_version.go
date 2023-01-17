b3cf3d7a458712063e30e35c45b2adf886010656
Norikatsu Shigemura <nork@ninth-nine.com>
2023-01-17 18:05:01 +0100

    sysutils/amazon-ssm-plugin: Add new port
    This package provides Amazon SSM SessionManager and CLI for managing
    shell experience using SSM APIs.
    
    PR:		268142

---
+++ sysutils/amazon-ssm-plugin/files/patch-src_version_version.go
@@ -0,0 +1,8 @@
+--- src/version/version.go.orig	2022-06-17 20:08:49 UTC
++++ src/version/version.go
+@@ -9,4 +9,4 @@
+ package version
+ 
+ // Version is the version of the CLI
+-const Version = "1.2.0.0"
++const Version = "%%VERSION%%"
