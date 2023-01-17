564f2d010b63197481ea79a2307cc325d85948fb
Jan Beich <jbeich@FreeBSD.org>
2023-01-17 10:14:08 +0000

    accessibility/darkman: update to 1.5.2
    Changes:	https://gitlab.com/WhyNotHugo/darkman/-/compare/v1.5.0...v1.5.2
    (cherry picked from commit ae0285d3f6681d4a42bc8291859f65a16ae591eb)

--- accessibility/darkman/files/patch-boottimer_boottimer.go
+++ accessibility/darkman/files/patch-boottimer_boottimer.go
@@ -1,9 +1,8 @@
-Unbreak on FreeBSD < 13 and i386
+Unbreak on FreeBSD < 13
 
 boottimer/boottimer.go:35:17: could not determine kind of name for C.CLOCK_BOOTTIME
-boottimer/boottimer.go:47:13: cannot use _Ctype_long(seconds) (value of type _Ctype_long) as type _Ctype_int in struct literal
 
---- boottimer/boottimer.go.orig	2022-10-22 10:12:59 UTC
+--- boottimer/boottimer.go.orig	2023-01-17 10:14:08 UTC
 +++ boottimer/boottimer.go
 @@ -32,7 +32,7 @@ func SetTimer(d time.Duration) {
  // the same channel `Alarms` above.
@@ -14,12 +13,3 @@ boottimer/boottimer.go:47:13: cannot use _Ctype_long(seconds) (value of type _Ct
  
  	seconds := d.Round(time.Second).Seconds()
  	ns := (d - d.Truncate(time.Second)).Nanoseconds()
-@@ -44,7 +44,7 @@ func SetTimer(d time.Duration) {
- 			tv_nsec: 0,
- 		},
- 		it_value: C.struct_timespec{
--			tv_sec:  C.long(seconds),
-+			tv_sec:  C.time_t(seconds),
- 			tv_nsec: C.long(ns),
- 		},
- 	}
