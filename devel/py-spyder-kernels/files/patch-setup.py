26d35c8e0739007171cc5596cbf3360574875035
Rainer Hurling <rhurlin@FreeBSD.org>
2023-01-19 20:22:10 +0100

    devel/py-spyder-kernels: Update to 2.4.2
    Changes: https://github.com/spyder-ide/spyder-kernels/blob/master/CHANGELOG.md#version-241-2022-12-29
             https://github.com/spyder-ide/spyder-kernels/blob/master/CHANGELOG.md#version-242-2023-01-17
    
    PR:		269059
    Reported by:	yuri, portscout, Repology

--- devel/py-spyder-kernels/files/patch-setup.py
+++ devel/py-spyder-kernels/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2022-11-02 23:49:28 UTC
+--- setup.py.orig	2023-01-17 23:10:03 UTC
 +++ setup.py
-@@ -40,7 +40,7 @@ REQUIREMENTS = [
-     'backports.functools-lru-cache; python_version<"3"',
-     'cloudpickle',
-     'ipykernel<5; python_version<"3"',
--    'ipykernel>=6.16.1,<7; python_version>="3"',
-+    'ipykernel>=6.16.0,<7; python_version>="3"',
+@@ -44,7 +44,7 @@ REQUIREMENTS = [
      'ipython<6; python_version<"3"',
-     'ipython>=7.31.1,<8; python_version>="3"',
+     'ipython>=7.31.1,<9; python_version>="3"',
      'jupyter-client>=5.3.4,<6; python_version<"3"',
+-    'jupyter-client>=7.4.9,<8; python_version>="3"',
++    'jupyter-client>=7.4.7; python_version>="3"',
+     'pyzmq>=17,<20; python_version<"3"',
+     'pyzmq>=22.1.0; python_version>="3"',
+     'wurlitzer>=1.0.3;platform_system!="Windows"',
