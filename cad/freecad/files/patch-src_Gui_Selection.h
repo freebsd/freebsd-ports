4b02b30706b21f3986cc67f44888c267809266dc
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2023-01-21 23:42:53 +0100

    cad/freecad: fix build after ebbef4b5f8
    PR:		269018

---
+++ cad/freecad/files/patch-src_Gui_Selection.h
@@ -0,0 +1,10 @@
+--- src/Gui/Selection.h.orig	2023-01-21 23:23:47.647852000 +0100
++++ src/Gui/Selection.h	2023-01-21 23:24:04.167678000 +0100
+@@ -25,6 +25,7 @@
+ #define GUI_SELECTION_H
+ 
+ #include <deque>
++#include <array>
+ #include <list>
+ #include <string>
+ #include <vector>
---
+++ cad/freecad/files/patch-src_Gui_SoFCUnifiedSelection.h
@@ -0,0 +1,10 @@
+--- src/Gui/SoFCUnifiedSelection.h.orig	2023-01-21 23:07:09.710205000 +0100
++++ src/Gui/SoFCUnifiedSelection.h	2023-01-21 23:07:38.508972000 +0100
+@@ -23,6 +23,7 @@
+ #ifndef GUI_SOFCUNIFIEDSELECTION_H
+ #define GUI_SOFCUNIFIEDSELECTION_H
+ 
++#include <array>
+ #include <list>
+ #include <unordered_map>
+ #include <unordered_set>
