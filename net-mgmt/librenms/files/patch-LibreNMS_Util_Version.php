3e84726434c702950fd7597de82b5d4ff26e5575
Dan Langille <dvl@FreeBSD.org>
2023-01-25 13:58:08 +0000

    net-mgmt/librenms: Update to 23.1.0
    re: https://github.com/librenms/librenms/releases/tag/23.1.0
    
    While here, run the Makefile through portfmt and portlint

--- net-mgmt/librenms/files/patch-LibreNMS_Util_Version.php
+++ net-mgmt/librenms/files/patch-LibreNMS_Util_Version.php
@@ -1,8 +1,17 @@
---- LibreNMS/Util/Version.php.orig	2021-05-21 10:09:00 UTC
+--- LibreNMS/Util/Version.php.orig	2023-01-24 21:18:47 UTC
 +++ LibreNMS/Util/Version.php
-@@ -92,7 +92,7 @@ class Version
+@@ -56,7 +56,7 @@ class Version
  
-     public static function python()
+     public function date(string $format = 'c'): string
+     {
+-        return date($format, $this->git->commitDate() ?: filemtime(__FILE__));  // approximate date for non-git installs
++        return date($format, %%RELEASE_TIMESTAMP%%);  // obtained via git show --pretty='%H|%ct' -s %%PORTVERSION%%
+     }
+ 
+     public function name(): string
+@@ -123,7 +123,7 @@
+ 
+     public function python(): string
      {
 -        $proc = new Process(['python3', '--version']);
 +        $proc = new Process(['%%LOCALBASE%%/bin/python3', '--version']);
