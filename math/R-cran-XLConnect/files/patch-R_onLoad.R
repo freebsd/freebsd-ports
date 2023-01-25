fe81050c2fda6803c96300db9faf2ccbccd77c06
Thierry Thomas <thierry@FreeBSD.org>
2023-01-25 22:46:52 +0100

    math/R-cran-XLConnect: upgrade to 1.0.7
    Changelog at <https://cran.r-project.org/web/packages/XLConnect/NEWS>.

--- math/R-cran-XLConnect/files/patch-R_onLoad.R
+++ math/R-cran-XLConnect/files/patch-R_onLoad.R
@@ -1,4 +1,4 @@
---- R/onLoad.R.orig	2022-10-20 08:32:02 UTC
+--- R/onLoad.R.orig	2023-01-24 09:49:21 UTC
 +++ R/onLoad.R
 @@ -55,47 +55,7 @@
      repo <- "https://repo1.maven.org/maven2"
@@ -19,7 +19,7 @@
 -      paste0(apachePrefix, "/commons/commons-compress/1.21/commons-compress-1.21.jar"), "commons-compress.jar",
 -      "1\\.(1[8-9]|[2-9][0-9]).*",  libname, pkgname, debianpkg = "libcommons-compress-java", rpmpkg="apache-commons-compress"),
 -    xlcEnsureDependenciesFor(
--      paste0(apachePrefix, "/xmlbeans/xmlbeans/5.0.3/xmlbeans-5.0.3.jar"), "xmlbeans.jar",
+-      paste0(apachePrefix, "/xmlbeans/xmlbeans/5.1.1/xmlbeans-5.1.1.jar"), "xmlbeans.jar",
 -      "5\\..*",  libname, pkgname, debianpkg="libxmlbeans-java"),
 -    xlcEnsureDependenciesFor(
 -      paste0(apachePrefix, "/commons/commons-collections4/4.4/commons-collections4-4.4.jar"), "commons-collections4.jar",
@@ -28,7 +28,7 @@
 -      paste0(apachePrefix, "/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar"), "commons-math3.jar",
 -      "3\\.([6-9]|1[0-9]).*",  libname, pkgname, debianpkg="libcommons-math3-java"),
 -    xlcEnsureDependenciesFor(
--      paste0(apachePrefix, "/logging/log4j/log4j-api/2.17.2/log4j-api-2.17.2.jar"), "log4j-api.jar",
+-      paste0(apachePrefix, "/logging/log4j/log4j-api/2.18.0/log4j-api-2.18.0.jar"), "log4j-api.jar",
 -      "2.17.2",  libname, pkgname),
 -    xlcEnsureDependenciesFor(
 -      paste0(repo, "/commons-codec/commons-codec/1.15/commons-codec-1.15.jar"), "commons-codec-1.15.jar",
