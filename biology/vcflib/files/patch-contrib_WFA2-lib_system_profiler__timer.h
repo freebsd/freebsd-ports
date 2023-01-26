62e06491dd1a548fb95ab05880b0d5a5d422534c
Jason W. Bacon <jwb@FreeBSD.org>
2023-01-26 08:14:07 -0600

    biology/vcflib: Patch in time.h for struct timespec
    Forgot to add patch file to repo during upgrade
    Needed on 12.3-RELEASE

---
+++ biology/vcflib/files/patch-contrib_WFA2-lib_system_profiler__timer.h
@@ -0,0 +1,10 @@
+--- contrib/WFA2-lib/system/profiler_timer.h.orig	2023-01-26 13:43:09 UTC
++++ contrib/WFA2-lib/system/profiler_timer.h
+@@ -33,6 +33,7 @@
+ #ifndef PROFILER_TIMER_H
+ #define PROFILER_TIMER_H
+ 
++#include <time.h>
+ #include "profiler_counter.h"
+ 
+ /*
