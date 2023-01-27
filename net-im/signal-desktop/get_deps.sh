6caf783eef95f9dbe1f4e39a016a5d30cdb6ecfd
Mikael Urankar <mikael@FreeBSD.org>
2023-01-27 12:13:34 +0100

    net-im/signal-desktop: Update to 6.3.0
    Changes:
      https://github.com/signalapp/Signal-Desktop/releases/tag/v6.3.0

--- net-im/signal-desktop/get_deps.sh
+++ net-im/signal-desktop/get_deps.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-SIGNAL_VERS=v6.1.0-beta.1
+SIGNAL_VERS=v6.3.0-beta.1
 
-ringrtc_hash=$(fetch -qo - https://raw.githubusercontent.com/signalapp/Signal-Desktop/${SIGNAL_VERS}/package.json | grep '"ringrtc": "https' | awk -F "#" '{print $2}' | sed 's#",##')
-# echo "RINGRTC_HASH= ${ringrtc_hash} ; https://raw.githubusercontent.com/signalapp/Signal-Desktop/${SIGNAL_VERS}/package.json"
+ringrtc_version=$(fetch -qo - https://raw.githubusercontent.com/signalapp/Signal-Desktop/${SIGNAL_VERS}/package.json | grep '@signalapp/ringrtc"' | awk -F ":" '{print $2}' | sed -E 's#("|,| )##g')
+echo "RINGRTC_VERSION= ${ringrtc_version}"
 
-ringrtc_version=$(fetch -qo - https://raw.githubusercontent.com/signalapp/signal-ringrtc-node/${ringrtc_hash}/package.json | grep '"version":' | awk -F ":" '{print $2}' | sed -e 's# "##' -e 's#",##')
-echo "RINGRTC_VERSION= ${ringrtc_version} ; https://raw.githubusercontent.com/signalapp/signal-ringrtc-node/${ringrtc_hash}/package.json"
-
-webrtc_version=$(fetch -qo - https://raw.githubusercontent.com/signalapp/ringrtc/v${ringrtc_version}/config/version.sh | awk -F '=' /WEBRTC_VERSION/'{print $2}')
-echo "WEBRTC_REV= ${webrtc_version} ; https://raw.githubusercontent.com/signalapp/ringrtc/v${ringrtc_version}/config/version.sh"
+webrtc_version=$(fetch -qo - https://raw.githubusercontent.com/signalapp/ringrtc/v${ringrtc_version}/config/version.properties | grep 'webrtc.version' | awk -F '=' '{print $2}')
+echo "WEBRTC_REV= ${webrtc_version} ; https://raw.githubusercontent.com/signalapp/ringrtc/v${ringrtc_version}/config/version.properties"
 
 libsignalclient_version=$(fetch -qo - https://raw.githubusercontent.com/signalapp/Signal-Desktop/${SIGNAL_VERS}/yarn.lock | grep 'signalapp/libsignal-client@' | awk -F '@' '{print $3}' | sed 's#", "##')
 echo "LIBSIGNAL_VERSION= ${libsignalclient_version}"
