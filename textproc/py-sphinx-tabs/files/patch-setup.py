c10773f8a5d60feaac6582b360dac159ebba6138
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-01-30 20:34:46 +0800

    textproc/py-sphinx-tabs: Fix 7a4b1542eea74a66513b168436f47fd2a67d7f7d
    - Fix RUN_DEPENDS: Remove py-Jinja2
    - Update version requirement of RUN_DEPENDS
    - Bump PORTREVISION for package change

---
+++ textproc/py-sphinx-tabs/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2022-07-02 09:46:49 UTC
++++ setup.py
+@@ -25,7 +25,7 @@ setup(
+     url="https://github.com/executablebooks/sphinx-tabs",
+     license="MIT",
+     python_requires="~=3.7",
+-    install_requires=["sphinx", "pygments", "docutils~=0.18.0"],
++    install_requires=["sphinx", "pygments", "docutils>=0.18.0,<0.20.0"],
+     extras_require={
+         "testing": [
+             "coverage",
