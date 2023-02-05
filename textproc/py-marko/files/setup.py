e59d0443787278f6278e002f845d7f9151e80ec8
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-02-05 23:19:33 +0800

    textproc/py-marko: Update to 1.3.0
    Changes:	https://github.com/frostming/marko/releases
    		https://github.com/frostming/marko/blob/master/CHANGELOG.md

--- textproc/py-marko/files/setup.py
+++ textproc/py-marko/files/setup.py
@@ -42,14 +42,14 @@ setup_kwargs = {
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     'extras_require': EXTRAS_REQUIRE,
-    'python_requires': '>=3.6',
+    'python_requires': '>=3.7',
     'entry_points': ENTRY_POINTS,
 }
 
