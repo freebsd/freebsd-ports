3d2ef90d0f941012262285b93a854172a7dc3c79
Dmitri Goutnik <dmgk@FreeBSD.org>
2023-02-09 14:30:56 -0500

    Mk/bsd.default-versions.mk: Update GO_DEFAULT to 1.20
    PR:		269289
    Exp-run by:	antoine

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -58,7 +58,7 @@ GHOSTSCRIPT_DEFAULT?=	agpl
 # Possible values: mesa-libs, mesa-devel
 GL_DEFAULT?=		mesa-libs
 # Possible values: 1.18, 1.19, 1.20, 1.21-devel
-GO_DEFAULT?=		1.19
+GO_DEFAULT?=		1.20
 # Possible values: 6, 6-nox11, 7, 7-nox11
 IMAGEMAGICK_DEFAULT?=	7
 # Possible values: 7, 8, 11, 17, 18
