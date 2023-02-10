523bb16432aa57536536ba0ac54687718e011e02
Hiroki Tagato <tagattie@FreeBSD.org>
2023-02-10 19:54:54 +0900

    devel/electron22: update to 22.2.1
    Changelog: https://github.com/electron/electron/releases/tag/v22.2.1

--- devel/electron22/files/patch-electron_shell_common_node__bindings.cc
+++ devel/electron22/files/patch-electron_shell_common_node__bindings.cc
@@ -1,6 +1,6 @@
---- electron/shell/common/node_bindings.cc.orig	2023-01-24 16:58:16 UTC
+--- electron/shell/common/node_bindings.cc.orig	2023-02-08 13:34:58 UTC
 +++ electron/shell/common/node_bindings.cc
-@@ -39,7 +39,7 @@
+@@ -40,7 +40,7 @@
  #include "third_party/blink/renderer/bindings/core/v8/v8_initializer.h"  // nogncheck
  #include "third_party/electron_node/src/debug_utils.h"
  
@@ -9,7 +9,7 @@
  #include "shell/common/crash_keys.h"
  #endif
  
-@@ -148,7 +148,7 @@ bool g_is_initialized = false;
+@@ -157,7 +157,7 @@ bool g_is_initialized = false;
  void V8FatalErrorCallback(const char* location, const char* message) {
    LOG(ERROR) << "Fatal error in V8: " << location << " " << message;
  
@@ -18,7 +18,7 @@
    electron::crash_keys::SetCrashKey("electron.v8-fatal.message", message);
    electron::crash_keys::SetCrashKey("electron.v8-fatal.location", location);
  #endif
-@@ -391,7 +391,7 @@ void NodeBindings::Initialize() {
+@@ -412,7 +412,7 @@ void NodeBindings::Initialize() {
    TRACE_EVENT0("electron", "NodeBindings::Initialize");
    // Open node's error reporting system for browser process.
  
