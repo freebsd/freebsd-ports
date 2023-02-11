e83df13905e945f1b776a84ff8abfeda29f04743
Hiroki Sato <hrs@FreeBSD.org>
2023-02-11 14:34:26 +0900

    security/opencryptoki: Update to 3.19.0

--- security/opencryptoki/files/patch-usr-sbin-pkcsconf-pkcsconf.c
+++ security/opencryptoki/files/patch-usr-sbin-pkcsconf-pkcsconf.c
@@ -1,6 +1,6 @@
---- usr/sbin/pkcsconf/pkcsconf.c.orig	2022-04-25 11:04:51 UTC
+--- usr/sbin/pkcsconf/pkcsconf.c.orig	2022-09-30 07:45:52 UTC
 +++ usr/sbin/pkcsconf/pkcsconf.c
-@@ -548,7 +548,7 @@ CK_RV check_user_and_group(void)
+@@ -362,7 +362,7 @@ CK_RV check_user_and_group(void)
       * when forked). So we need to get the group information.
       * Really need to take the uid and map it to a name.
       */
@@ -9,12 +9,3 @@
      if (grp == NULL) {
          return CKR_FUNCTION_FAILED;
      }
-@@ -589,6 +589,8 @@ CK_RV display_pkcs11_info(void)
-     printf("\tLibrary Description: %.32s \n", CryptokiInfo.libraryDescription);
-     printf("\tLibrary Version: %d.%d \n", CryptokiInfo.libraryVersion.major,
-            CryptokiInfo.libraryVersion.minor);
-+
-+    cleanup();
- 
-     return rc;
- }
