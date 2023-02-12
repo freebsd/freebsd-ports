399d55596666c0cef64992f8eb7bee67db9a3fd1
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2023-02-12 18:02:56 +0100

    cad/kicad: update KiCad and libraries to 7.0.0
    Announcement soon on https://www.kicad.org/blog/

--- cad/kicad/files/patch-CMakeModules_KiCadFullVersion.cmake
+++ cad/kicad/files/patch-CMakeModules_KiCadFullVersion.cmake
@@ -1,11 +1,11 @@
---- CMakeModules/KiCadFullVersion.cmake.orig	2022-01-17 23:51:27 UTC
-+++ CMakeModules/KiCadFullVersion.cmake
+--- ./cmake/KiCadFullVersion.cmake.orig	2023-01-25 21:49:08.004539000 +0100
++++ ./cmake/KiCadFullVersion.cmake	2023-01-25 21:49:50.772285000 +0100
 @@ -27,10 +27,6 @@
  # repository), or the version from KiCadVersion.cmake. The user-provided
  # KICAD_VERSION_EXTRA is then appended to the base version string.
  
 -# Use git to determine the version string if it's available.
--include( ${CMAKE_MODULE_PATH}/CreateGitVersionHeader.cmake )
+-include( ${KICAD_CMAKE_MODULE_PATH}/CreateGitVersionHeader.cmake )
 -create_git_version_header( ${SRC_PATH} )
 -
  # $KICAD_VERSION is set in KiCadVersion.cmake or by git (if it is available).
--- cad/kicad/files/patch-eeschema_sim_sim__plot__frame.cpp
+++ cad/kicad/files/patch-eeschema_sim_sim__plot__frame.cpp
@@ -1,23 +1,11 @@
---- eeschema/sim/sim_plot_frame.cpp.orig	2022-09-14 16:35:24 UTC
-+++ eeschema/sim/sim_plot_frame.cpp
-@@ -167,15 +167,15 @@ SIM_PLOT_FRAME::SIM_PLOT_FRAME( KIWAY* aKiway, wxWindo
-     Bind( EVT_SIM_CURSOR_UPDATE, &SIM_PLOT_FRAME::onCursorUpdate, this );
+--- eeschema/sim/sim_plot_frame.cpp.orig	2023-01-25 22:50:55.894239000 +0100
++++ eeschema/sim/sim_plot_frame.cpp	2023-01-27 00:25:46.086158000 +0100
+@@ -27,6 +27,8 @@
+ #include <wx/debug.h>
+ #include <wx/stc/stc.h>
  
-     // Toolbar buttons
--    m_toolSimulate = m_toolBar->AddTool( ID_SIM_RUN, _( "Run/Stop Simulation" ),
-+    m_toolSimulate = m_toolBar->AddTool( ID_SIM_RUN, wxEmptyString,
-             KiBitmap( BITMAPS::sim_run ), _( "Run Simulation" ), wxITEM_NORMAL );
--    m_toolAddSignals = m_toolBar->AddTool( ID_SIM_ADD_SIGNALS, _( "Add Signals" ),
-+    m_toolAddSignals = m_toolBar->AddTool( ID_SIM_ADD_SIGNALS, wxEmptyString,
-             KiBitmap( BITMAPS::sim_add_signal ), _( "Add signals to plot" ), wxITEM_NORMAL );
--    m_toolProbe = m_toolBar->AddTool( ID_SIM_PROBE,  _( "Probe" ),
-+    m_toolProbe = m_toolBar->AddTool( ID_SIM_PROBE,  wxEmptyString,
-             KiBitmap( BITMAPS::sim_probe ), _( "Probe signals on the schematic" ), wxITEM_NORMAL );
--    m_toolTune = m_toolBar->AddTool( ID_SIM_TUNE, _( "Tune" ),
-+    m_toolTune = m_toolBar->AddTool( ID_SIM_TUNE, wxEmptyString,
-             KiBitmap( BITMAPS::sim_tune ), _( "Tune component values" ), wxITEM_NORMAL );
--    m_toolSettings = m_toolBar->AddTool( wxID_ANY, _( "Sim Parameters" ),
-+    m_toolSettings = m_toolBar->AddTool( wxID_ANY, wxEmptyString,
-             KiBitmap( BITMAPS::config ), _( "Simulation parameters and settings" ), wxITEM_NORMAL );
- 
-     // Start all toolbar buttons except settings as disabled
++#undef WXWIN_COMPATIBILITY_2_8
++
+ // For some obscure reason, needed on msys2 with some wxWidgets versions (3.0) to avoid
+ // undefined symbol at link stage (due to use of #include <pegtl.hpp>)
+ // Should not create issues on other platforms
