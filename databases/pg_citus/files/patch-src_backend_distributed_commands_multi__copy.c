9f3a6591c599ae3deb72c5e02f29b4457f0fecc4
Matthew Seaman <matthew@FreeBSD.org>
2023-02-15 11:35:58 +0000

    databases/pg_citus: fix build with recent postgresql versions
    Partially import commit
    https://github.com/citusdata/citus/commit/341fdb32fc07a3aea2dda2fa33a70d22819be37b
    from upstream (issue https://github.com/citusdata/citus/pull/6711)
    
    PG13 and PG14 have now backported the `RelationGetSmgr` function that
    Citus already backported, so on those PG versions use the function
    from the PG sources.
    
    Reported by:	pkg-fallout

--- databases/pg_citus/files/patch-src_backend_distributed_commands_multi__copy.c
+++ databases/pg_citus/files/patch-src_backend_distributed_commands_multi__copy.c
@@ -1,6 +1,6 @@
---- src/backend/distributed/commands/multi_copy.c.orig	2021-03-09 08:26:53 UTC
+--- src/backend/distributed/commands/multi_copy.c.orig	2023-02-03 08:13:35 UTC
 +++ src/backend/distributed/commands/multi_copy.c
-@@ -53,6 +53,7 @@
+@@ -50,6 +50,7 @@
  
  #include <arpa/inet.h> /* for htons */
  #include <netinet/in.h> /* for htons */
