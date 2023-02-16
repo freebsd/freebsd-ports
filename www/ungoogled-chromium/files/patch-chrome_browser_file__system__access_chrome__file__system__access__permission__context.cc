434cf16883861cf97d35426f1979394778ddeaa6
Robert Nagy <rnagy@FreeBSD.org>
2023-02-16 18:19:20 +0100

    www/ungoogled-chromium: update to 110.0.5481.100
    Approved by:	rene (mentor)

--- www/ungoogled-chromium/files/patch-chrome_browser_file__system__access_chrome__file__system__access__permission__context.cc
+++ www/ungoogled-chromium/files/patch-chrome_browser_file__system__access_chrome__file__system__access__permission__context.cc
@@ -1,6 +1,6 @@
---- chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc.orig	2023-02-11 09:11:04 UTC
+--- chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc.orig	2023-02-16 17:16:19 UTC
 +++ chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc
-@@ -253,7 +253,7 @@ const struct {
+@@ -302,7 +302,7 @@ const struct {
      {base::DIR_HOME, FILE_PATH_LITERAL("Library/CloudStorage"),
       kDontBlockChildren},
  #endif
