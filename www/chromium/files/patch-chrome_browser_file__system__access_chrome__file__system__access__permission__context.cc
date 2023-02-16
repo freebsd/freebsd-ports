09ffcc8674b1b43f463fa189faeadf58b4cc3d49
Robert Nagy <rnagy@FreeBSD.org>
2023-02-16 18:17:00 +0100

    www/chromium: update to 110.0.5481.100
    Approved by:	rene (mentor)
    
    (cherry picked from commit 614b1e4a8c5f65e3dc161f034fc24f7ad27b47f6)

--- www/chromium/files/patch-chrome_browser_file__system__access_chrome__file__system__access__permission__context.cc
+++ www/chromium/files/patch-chrome_browser_file__system__access_chrome__file__system__access__permission__context.cc
@@ -1,6 +1,6 @@
---- chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc.orig	2023-02-08 09:03:45 UTC
+--- chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc.orig	2023-02-16 17:16:19 UTC
 +++ chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc
-@@ -255,7 +255,7 @@ const struct {
+@@ -302,7 +302,7 @@ const struct {
      {base::DIR_HOME, FILE_PATH_LITERAL("Library/CloudStorage"),
       kDontBlockChildren},
  #endif
