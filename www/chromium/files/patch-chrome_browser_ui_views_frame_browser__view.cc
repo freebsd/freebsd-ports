e96ee1b79d2c591ff4d7c9e8e00041d20b72233d
Robert Nagy <rnagy@FreeBSD.org>
2023-02-22 20:52:14 +0100

    www/chromium: update to 110.0.5481.177
    Approved by:	rene (mentor)
    Security:	https://vuxml.freebsd.org/freebsd/4d6b5ea9-bc64-4e77-a7ee-d62ba68a80dd.html
    
    (cherry picked from commit 97b4cd7194245a56aefcedf4fcebdf2301f00145)

--- www/chromium/files/patch-chrome_browser_ui_views_frame_browser__view.cc
+++ www/chromium/files/patch-chrome_browser_ui_views_frame_browser__view.cc
@@ -1,6 +1,6 @@
---- chrome/browser/ui/views/frame/browser_view.cc.orig	2023-02-08 09:03:45 UTC
+--- chrome/browser/ui/views/frame/browser_view.cc.orig	2023-02-22 19:51:47 UTC
 +++ chrome/browser/ui/views/frame/browser_view.cc
-@@ -2115,7 +2115,7 @@ void BrowserView::TabDraggingStatusChanged(bool is_dra
+@@ -2117,7 +2117,7 @@ void BrowserView::TabDraggingStatusChanged(bool is_dra
    // CrOS cleanup is done.
  // TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
  // of lacros-chrome is complete.
