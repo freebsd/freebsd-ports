b5b6d5a2d469878c3109f7efeeb0f44b9a1ffa18
Mikael Urankar <mikael@FreeBSD.org>
2023-02-26 17:44:28 +0100

    www/deno: Update to 1.31.1
    Changes:
      https://github.com/denoland/deno/releases/tag/v1.31.0
      https://github.com/denoland/deno/releases/tag/v1.31.1

--- www/deno/files/patch-cargo-crates_v8_build.rs
+++ www/deno/files/patch-cargo-crates_v8_build.rs
@@ -1,13 +1,11 @@
---- cargo-crates/v8-0.60.1/build.rs.orig	2020-07-22 09:53:02 UTC
-+++ cargo-crates/v8-0.60.1/build.rs
-@@ -130,6 +132,10 @@ fn platform() -> &'static str {
-   #[cfg(target_os = "macos")]
-   {
-     "mac"
-+  }
+--- cargo-crates/v8-0.63.0/build.rs.orig	2020-07-22 09:53:02 UTC
++++ cargo-crates/v8-0.63.0/build.rs
+@@ -255,6 +255,8 @@ fn platform() -> String {
+   let os = "mac";
+   #[cfg(target_os = "windows")]
+   let os = "windows";
 +  #[cfg(target_os = "freebsd")]
-+  {
-+    "freebsd"
-   }
- }
++  let os = "freebsd";
  
+   #[cfg(target_arch = "x86_64")]
+   let arch = "amd64";
--- www/deno/files/patch-runtime_js_30__fs.js
+++ www/deno/files/patch-runtime_js_30__fs.js
@@ -1,11 +1,20 @@
 --- runtime/js/30_fs.js.orig	2022-07-08 08:14:40 UTC
 +++ runtime/js/30_fs.js
-@@ -178,7 +178,7 @@
-   }
+@@ -209,7 +209,7 @@ function createByteStruct(types) {
+   // `?` prefix means optional on windows.
+   let offset = 0;
+   let str =
+-    'const unix = Deno.build.os === "darwin" || Deno.build.os === "linux"; return {';
++    'const unix = Deno.build.os === "darwin" || Deno.build.os === "linux" || Deno.build.os === "freebsd"; return {';
+   const typeEntries = ObjectEntries(types);
+   for (let i = 0; i < typeEntries.length; ++i) {
+     let { 0: name, 1: type } = typeEntries[i];
+@@ -262,7 +262,7 @@ function parseFileInfo(response) {
+ });
  
-   function parseFileInfo(response) {
--    const unix = build.os === "darwin" || build.os === "linux";
-+    const unix = build.os === "darwin" || build.os === "freebsd" || build.os === "linux";
-     return {
-       isFile: response.isFile,
-       isDirectory: response.isDirectory,
+ function parseFileInfo(response) {
+-  const unix = build.os === "darwin" || build.os === "linux";
++  const unix = build.os === "darwin" || build.os === "linux" || build.os === "freebsd";
+   return {
+     isFile: response.isFile,
+     isDirectory: response.isDirectory,
