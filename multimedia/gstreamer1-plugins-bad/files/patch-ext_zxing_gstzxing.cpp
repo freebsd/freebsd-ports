4a8a17c8c628d4191373f06daedb9d83949d742e
Gleb Popov <arrowd@FreeBSD.org>
2023-02-28 09:09:44 +0300

    multimedia/gstreamer1: Update to 1.22, new stable branch.
    PR:		269895
    Exp-run by:	antoine
    Sponsored by:   Serenity Cybersecurity, LLC

--- multimedia/gstreamer1-plugins-bad/files/patch-ext_zxing_gstzxing.cpp
+++ multimedia/gstreamer1-plugins-bad/files/patch-ext_zxing_gstzxing.cpp
@@ -1,25 +1,20 @@
---- ext/zxing/gstzxing.cpp.orig	2022-12-19 23:34:46 UTC
+--- ext/zxing/gstzxing.cpp.orig	2023-01-23 19:29:34 UTC
 +++ ext/zxing/gstzxing.cpp
-@@ -368,8 +368,8 @@ gst_zxing_transform_frame_ip (GstVideoFilter * vfilter
-   auto result = ReadBarcode ({(unsigned char *)data, width, height, zxing->image_format}, hints);
+@@ -369,7 +369,7 @@ gst_zxing_transform_frame_ip (GstVideoFilter * vfilter
    if (result.isValid ()) {
      GST_DEBUG_OBJECT (zxing, "Symbol found. Text: %s Format: %s",
--        TextUtfEncoding::ToUtf8 (result.text ()).c_str (),
+         result.text ().c_str (),
 -        ToString (result.format ()));
-+        result.text ().c_str (),
 +        ToString (result.format ()).c_str());
    } else {
      goto out;
    }
-@@ -394,9 +394,9 @@ gst_zxing_transform_frame_ip (GstVideoFilter * vfilter
+@@ -394,7 +394,7 @@ gst_zxing_transform_frame_ip (GstVideoFilter * vfilter
          "timestamp", G_TYPE_UINT64, timestamp,
          "stream-time", G_TYPE_UINT64, stream_time,
          "running-time", G_TYPE_UINT64, running_time,
 -        "type", G_TYPE_STRING, ToString (result.format ()),
 +        "type", G_TYPE_STRING, ToString (result.format ()).c_str(),
          "symbol", G_TYPE_STRING,
--        TextUtfEncoding::ToUtf8 (result.text ()).c_str (), NULL);
-+        result.text ().c_str (), NULL);
+         result.text ().c_str (), NULL);
  
-     if (zxing->attach_frame) {
-       /* create a sample from image */
