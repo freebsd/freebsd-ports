35b0544d8e1149aecec2ed3b81a67d6e64601cb7
Jason E. Hale <jhale@FreeBSD.org>
2023-03-02 12:50:49 -0500

    graphics/libraw: Update to 0.21.1
    Bump PORTREVISION on dependent ports due to shared library increase.
    Add several fixes for compatability.
    
    https://www.libraw.org/news/libraw-0-21-release
    https://www.libraw.org/news/libraw-0-21-1-release

--- graphics/oyranos/files/patch-src_modules_color_modules_devices_oyranos__cmm__oyRE.cpp
+++ graphics/oyranos/files/patch-src_modules_color_modules_devices_oyranos__cmm__oyRE.cpp
@@ -1,4 +1,8 @@
---- src/modules/color/modules/devices/oyranos_cmm_oyRE.cpp.orig	2018-01-15 20:16:42 UTC
+Chunk 2: Fix build with libraw >= 0.21.0
+Based on upstream commit:
+https://gitlab.com/oyranos/oyranos/-/commit/7fd1eabdf9f59727c0f516d33663d895f812a449
+
+--- src/modules/color/modules/devices/oyranos_cmm_oyRE.cpp.orig	2016-12-09 10:36:53 UTC
 +++ src/modules/color/modules/devices/oyranos_cmm_oyRE.cpp
 @@ -482,7 +482,7 @@ oyProfile_s * createMatrixProfile      ( libraw_colord
        oyOptions_s * result = 0;
@@ -9,3 +13,12 @@
        oyOptions_Handle( reg, opts, "create_profile.icc_profile.color_matrix",
                          &result );
  
+@@ -1447,7 +1447,7 @@ int DeviceFromContext(oyConfig_s **config, libraw_outp
+    DFC_OPT_ADD_INT_ARR(greybox,1) //4
+    DFC_OPT_ADD_INT_ARR(greybox,2) //4
+    DFC_OPT_ADD_INT_ARR(greybox,3) //4
+-   DFC_OPT_ADD_INT(shot_select)
++   //DFC_OPT_ADD_INT(shot_select)
+ 
+    return error;
+ }
