002f89af8943866ce04f70983109fdedf1a2ed22
Dima Panov <fluffy@FreeBSD.org>
2023-03-03 21:05:21 +0300

    editors/libreoffice: update to 7.5.1 release (+)
    LibreOffice Suite 7.5.1 Community release is now available!
    Along with various fixes, it also includes a manual
    light/dark mode switch – regardless of your system settings.
    It's under "Tools > Options > View".
    
    Learn more: https://blog.documentfoundation.org/blog/2023/03/02/libreoffice-7-5-1-community/
    Sponsored by:	Serenity Cybersecurity, LLC

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,10 +1,11 @@
 --- external/skia/UnpackedTarball_skia.mk.orig	2022-05-12 13:26:05 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -40,6 +40,7 @@ skia_patches := \
-     tdf148624.patch.1 \
+@@ -41,6 +41,7 @@ skia_patches := \
      constexpr-template.patch.0 \
      missing-include.patch.0 \
+     tdf147342.patch.0 \
 +    gfx-skia-skia-src-core-SkCpu.cpp.patch.0 \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
+ 
  
