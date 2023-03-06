d627a614c29b0aa506ec12d10975a3fc3d12c05c
Alexey Dokuchaev <danfe@FreeBSD.org>
2023-03-06 06:06:38 +0000

    graphics/fotoxx: update the port to version 23.2.

--- graphics/fotoxx/files/patch-f.refine.cc
+++ graphics/fotoxx/files/patch-f.refine.cc
@@ -1,6 +1,6 @@
---- f.refine.cc.orig	2022-01-01 11:16:58 UTC
+--- f.refine.cc.orig	2023-03-01 07:32:06 UTC
 +++ f.refine.cc
-@@ -223,7 +223,7 @@ void m_edit_dist(GtkWidget *, cchar *menu)
+@@ -223,7 +223,7 @@ void m_edit_dist(GtkWidget *, ch  *menu)
     EFedit_dist.Farea = 2;                                                        //  select area usable
     EFedit_dist.Frestart = 1;                                                     //  restart allowed
     EFedit_dist.Fscript = 1;                                                      //  scripting supported 
@@ -9,21 +9,21 @@
     if (! edit_setup(EFedit_dist)) return;                                        //  setup edit
  
  /***
-@@ -589,7 +589,7 @@ void m_flatten_dist(GtkWidget *, cchar *menu)
-    EFflatten_dist.Frestart = 1;                                                  //  restartable
-    EFflatten_dist.Fpaintedits = 1;                                               //  use with paint edits OK
-    EFflatten_dist.Fscript = 1;                                                   //  scripting supported
--   EFflatten_dist.threadfunc = thread;
-+   EFflatten_dist.threadfunc = flatten_dist_names::thread;
-    if (! edit_setup(EFflatten_dist)) return;                                     //  setup edit
+@@ -589,7 +589,7 @@ void m_flatdist(GtkWidget *, ch  *menu)
+    EFflatdist.Frestart = 1;                                                      //  restartable
+    EFflatdist.Fpaintedits = 1;                                                   //  use with paint edits OK
+    EFflatdist.Fscript = 1;                                                       //  scripting supported
+-   EFflatdist.threadfunc = thread;
++   EFflatdist.threadfunc = flatdist_names::thread;
+    if (! edit_setup(EFflatdist)) return;                                         //  setup edit
  
     Eww = E0pxm->ww;
-@@ -1066,7 +1066,7 @@ void flatten_dist_func(int _NZ, int _flatten, int _deb
-    EFflatten_dist.menufunc = m_flatten_dist;
-    EFflatten_dist.Farea = 2;                                                     //  select area usable
-    EFflatten_dist.Frestart = 1;                                                  //  restartable
--   EFflatten_dist.threadfunc = thread;
-+   EFflatten_dist.threadfunc = flatten_dist_names::thread;
+@@ -1067,7 +1067,7 @@ void flatdist_func(int _radius, int _flatten, int _deb
+    EFflatdist.menufunc = m_flatdist;
+    EFflatdist.Farea = 2;                                                         //  select area usable
+    EFflatdist.Frestart = 1;                                                      //  restartable
+-   EFflatdist.threadfunc = thread;
++   EFflatdist.threadfunc = flatdist_names::thread;
  
-    if (! edit_setup(EFflatten_dist)) return;                                     //  setup edit
+    if (! edit_setup(EFflatdist)) return;                                         //  setup edit
  
