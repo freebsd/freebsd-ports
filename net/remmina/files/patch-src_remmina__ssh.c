d6a0a073f9f2a3968d7f6960bfbc4056dcf293e3
Fernando Apesteguía <fernape@FreeBSD.org>
2023-03-07 08:13:33 +0100

    net/remmina: Fix patch
    The version in main had the size in hexadecimal while in quarterly it was in
    decimal.
    
    Fixes:	a9cb9c75a020

--- net/remmina/files/patch-src_remmina__ssh.c
+++ net/remmina/files/patch-src_remmina__ssh.c
@@ -1,10 +1,10 @@
---- src/remmina_ssh.c.orig	2023-03-02 13:25:58 UTC
+--- src/remmina_ssh.c.orig	2022-06-20 10:37:40 UTC
 +++ src/remmina_ssh.c
-@@ -479,7 +479,7 @@ remmina_ssh_cp_to_ch_cb(int fd, int revents, void *use
+@@ -478,7 +478,7 @@ remimna_ssh_cp_to_ch_cb(int fd, int revents, void *use
  {
  	TRACE_CALL(__func__);
  	ssh_channel channel = (ssh_channel)userdata;
--	gchar buf[0x200000];
+-	gchar buf[2097152];
 +	gchar buf[524288];
  	gint sz = 0, ret = 0;
  
