47c9ea70d88e20522cdfbb4f124c0d67df8a62a9
Rene Ladan <rene@FreeBSD.org>
2023-03-07 22:15:51 +0100

    Mk: unregister expired PostgreSQL 10

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -112,7 +112,7 @@ _PERL5_FROM_BIN!=	${LOCALBASE}/bin/perl -e 'printf "%vd\n", $$^V;'
 _EXPORTED_VARS+=	_PERL5_FROM_BIN
 PERL5_DEFAULT:=		${_PERL5_FROM_BIN:R}
 .  endif
-# Possible values: 10, 11, 12, 13, 14, 15
+# Possible values: 11, 12, 13, 14, 15
 PGSQL_DEFAULT?=		13
 # Possible values: 8.0, 8.1, 8.2
 PHP_DEFAULT?=		8.1
