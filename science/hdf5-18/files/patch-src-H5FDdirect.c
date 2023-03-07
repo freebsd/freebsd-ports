3518336673991ed525d32647ec5ef7375bd96ce0
Thierry Thomas <thierry@FreeBSD.org>
2023-03-07 09:39:29 +0100

    science/hdf5-18: upgrade to 1.8.23
    This fixes the build with recent clang.

--- science/hdf5-18/files/patch-src-H5FDdirect.c
+++ science/hdf5-18/files/patch-src-H5FDdirect.c
@@ -1,4 +1,4 @@
---- src/H5FDdirect.c.orig	2017-06-15 15:47:13 UTC
+--- src/H5FDdirect.c.orig	2023-01-30 14:52:51 UTC
 +++ src/H5FDdirect.c
 @@ -27,6 +27,8 @@
   * this file. */
@@ -6,6 +6,6 @@
  
 +#define O_DIRECT	0x00010000
 +
- #include "H5private.h"    /* Generic Functions      */
- #include "H5Eprivate.h"    /* Error handling        */
- #include "H5Fprivate.h"    /* File access        */
+ #include "H5private.h"   /* Generic Functions        */
+ #include "H5Eprivate.h"  /* Error handling           */
+ #include "H5Fprivate.h"  /* File access              */
