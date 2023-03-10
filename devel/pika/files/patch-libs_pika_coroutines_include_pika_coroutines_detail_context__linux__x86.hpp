7fd1e8b14dbea49f23235e5b58bebac4f5552b26
Yuri Victorovich <yuri@FreeBSD.org>
2023-03-10 01:02:34 -0800

    devel/pika: Update 0.12.0 → 0.13.0
    Reported by:	portscout

--- devel/pika/files/patch-libs_pika_coroutines_include_pika_coroutines_detail_context__linux__x86.hpp
+++ devel/pika/files/patch-libs_pika_coroutines_include_pika_coroutines_detail_context__linux__x86.hpp
@@ -1,4 +1,4 @@
---- libs/pika/coroutines/include/pika/coroutines/detail/context_linux_x86.hpp.orig	2022-07-13 00:19:18 UTC
+--- libs/pika/coroutines/include/pika/coroutines/detail/context_linux_x86.hpp.orig	2023-03-08 10:50:55 UTC
 +++ libs/pika/coroutines/include/pika/coroutines/detail/context_linux_x86.hpp
 @@ -12,7 +12,7 @@
  
@@ -7,5 +7,5 @@
 -#if defined(__linux) || defined(linux) || defined(__linux__)
 +#if defined(__linux) || defined(linux) || defined(__linux__) || defined(__FreeBSD__)
  
- #include <pika/config.hpp>
- #include <pika/assert.hpp>
+ # include <pika/config.hpp>
+ # include <pika/assert.hpp>
