97edf5fd1950f9b18d927f96e4e96b56a0400c96
Ganael LAPLANCHE <martymac@FreeBSD.org>
2023-03-14 12:19:43 +0100

    x11/openrgb: Update to 0.8
    While here, fix COMMENT and re-generate patch.
    
    PR:	270174
    Approved by:	vidar@karlsen.tech (maintainer)

---
+++ x11/openrgb/files/patch-Controllers-LianLiController-LianLiUniHub_AL10Controller.h
@@ -0,0 +1,11 @@
+--- Controllers/LianLiController/LianLiUniHub_AL10Controller.h.orig	2022-11-28 03:44:33 UTC
++++ Controllers/LianLiController/LianLiUniHub_AL10Controller.h
+@@ -16,7 +16,7 @@
+ 
+ #include "RGBController.h"
+ 
+-#include <libusb-1.0/libusb.h>
++#include <libusb.h>
+ 
+ /*----------------------------------------------------------------------------*\
+ | Global definitions.                                                          |
