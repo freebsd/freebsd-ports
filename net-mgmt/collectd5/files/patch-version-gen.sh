910e52c9d15669b8caf7101011e86786e3b25f54
Krzysztof <ports@bsdserwis.com>
2023-03-14 10:35:49 +0100

    net-mgmt/collectd5: update varnish dependency, modify nut dependency
    As discussed in PR 265217 there is small patch for the nut (libups)
    dependency.  I also add the actual varnish depndency (6 instead of 4).
    
    PR:		268972, 265217

--- net-mgmt/collectd5/files/patch-version-gen.sh
+++ net-mgmt/collectd5/files/patch-version-gen.sh
@@ -1,8 +1,8 @@
---- version-gen.sh.orig	2020-03-19 09:52:56.313967100 +0100
-+++ version-gen.sh	2020-03-27 19:15:53.407855000 +0100
+--- version-gen.sh.orig	2020-09-03 11:26:25 UTC
++++ version-gen.sh
 @@ -2,10 +2,6 @@
  
- DEFAULT_VERSION="5.11.0.git"
+ DEFAULT_VERSION="5.12.0.git"
  
 -if [ -d .git ]; then
 -	VERSION="`git describe --dirty=+ --abbrev=7 2> /dev/null | sed -e '/^collectd-/!d' -e 's///' -e 'y/-/./'`"
