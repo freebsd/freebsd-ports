2f35240f67b54feb5c212d35cae3b80d24bf52ff
Adriaan de Groot <adridg@FreeBSD.org>
2023-03-16 22:21:10 +0100

    devel/cmake: update to 3.26.1, latest upstream
    See release notes at
    	https://cmake.org/cmake/help/v3.26/release/3.26.html
    
    Some "while we're at it" changes:
    - remove obsolete patch, reported by: diizzy (PR 270337)
    - set LOCALSTATEDIR to /var (PR 267748)
    
    Thanks antoine@ for the exp-run.
    
    PR:		270491 270337 267748

--- Mk/Uses/cmake.mk
+++ Mk/Uses/cmake.mk
@@ -50,7 +50,7 @@ _INCLUDE_USES_CMAKE_MK=	yes
 
 _valid_ARGS=		insource run noninja testing _internal
 
-_CMAKE_VERSION=		3.25.1
+_CMAKE_VERSION=		3.26.1
 
 # Sanity check
 .  for arg in ${cmake_ARGS}
