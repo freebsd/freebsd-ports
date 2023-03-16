41306deed97730e3e197fd6144570d0bfbea97dd
Mikael Urankar <mikael@FreeBSD.org>
2023-03-16 13:05:19 +0100

    net-im/signal-desktop: Update to 6.10.0
    Changes:
      https://github.com/signalapp/Signal-Desktop/releases/tag/v6.10.0

--- net-im/signal-desktop/get_deps.sh
+++ net-im/signal-desktop/get_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SIGNAL_VERS=v6.9.0
+SIGNAL_VERS=v6.10.0
 
 fetch -qo /tmp/package.json https://raw.githubusercontent.com/signalapp/Signal-Desktop/${SIGNAL_VERS}/package.json
 ringrtc_version=$(grep '@signalapp/ringrtc"' /tmp/package.json | awk -F ":" '{print $2}' | sed -E 's#("|,| )##g')
@@ -25,14 +25,14 @@ OPENSSL_VERSION=$(awk /"OPENSSL_VERSION ="/'{print $4}' /tmp/download.js | sed -
 TOKENIZER_VERSION=$(awk /"TOKENIZER_VERSION ="/'{print $4}' /tmp/download.js | sed -e 's#;##g' -e "s#'##g")
 TAG="${SQLCIPHER_VERSION}--${OPENSSL_VERSION}--${TOKENIZER_VERSION}"
 echo "Signal-FTS5-Extension= ${TOKENIZER_VERSION}"
-echo "SQLCIPHER= sqlcipher-${TAG}-${HASH}"
+echo "SQLCIPHER=sqlcipher-${TAG}-${HASH}" | portedit merge -i Makefile
 
 fetch -qo /tmp/yarn.lock https://raw.githubusercontent.com/signalapp/Signal-Desktop/${SIGNAL_VERS}/yarn.lock
 npm_signal_hash=$(grep libsignal-client /tmp/yarn.lock | awk -F '#' /resolved/'{print $2}' | sed 's/"//g')
-echo "NPM_SIGNAL_DIR= npm-@signalapp-libsignal-client-${libsignalclient_version}-${npm_signal_hash}-integrity"
+echo "NPM_SIGNAL_DIR=npm-@signalapp-libsignal-client-${libsignalclient_version}-${npm_signal_hash}-integrity" | portedit merge -i Makefile
 
 npm_ringrtc_hash=$(grep ringrtc /tmp/yarn.lock | awk -F '#' /resolved/'{print $2}' | sed 's/"//g')
-echo "NPM_RINGRTC_DIR= npm-@signalapp-ringrtc-${ringrtc_version}-${npm_ringrtc_hash}-integrity"
+echo "NPM_RINGRTC_DIR=npm-@signalapp-ringrtc-${ringrtc_version}-${npm_ringrtc_hash}-integrity" | portedit merge -i Makefile
 
 npm_bsqlite3_hash=$(grep better-sqlite3 /tmp/yarn.lock | awk -F '#' /resolved/'{print $2}' | sed 's/"//g')
-echo "NPM_SQLITE3_DIR= npm-@signalapp-better-sqlite3-${bsqlite3_version}-${npm_bsqlite3_hash}-integrity"
+echo "NPM_SQLITE3_DIR=npm-@signalapp-better-sqlite3-${bsqlite3_version}-${npm_bsqlite3_hash}-integrity" | portedit merge -i Makefile
