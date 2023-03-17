b03d6b0066f5cabaa9fd23f4b575ce046207b148
Thierry Thomas <thierry@FreeBSD.org>
2023-03-17 13:32:44 +0100

    math/py-mathics: upgrade to 6.0.1
    Release notes at
    	<https://github.com/Mathics3/mathics-core/releases/tag/6.0.1>.

--- math/py-mathics/files/patch-setup.py
+++ math/py-mathics/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2023-01-20 17:32:12 UTC
+--- setup.py.orig	2023-03-17 10:22:19 UTC
 +++ setup.py
-@@ -57,7 +57,7 @@ elif sys.version_info[:2] == (3, 6):
- elif sys.version_info[:2] == (3, 7):
-     INSTALL_REQUIRES += ["numpy<1.22", "llvmlite", "sympy>=1.8, < 1.11"]
+@@ -64,7 +64,7 @@ elif sys.version_info[:2] == (3, 6):
+         sys.exit(-1)
  else:
--    INSTALL_REQUIRES += ["numpy", "llvmlite", "sympy>=1.8, < 1.11"]
-+    INSTALL_REQUIRES += ["numpy", "llvmlite", "sympy>=1.8, < 1.11.2"]
- 
- if not is_PyPy:
-     INSTALL_REQUIRES += ["recordclass"]
+     INSTALL_REQUIRES += [
+-        "numpy<=1.24",
++        "numpy<=1.25",
+         "llvmlite",
+         "sympy>=1.8, < 1.12",
+         "pillow >= 9.2",
