1285536d2278724b5907973346a1f2ab5e961133
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-03-18 20:43:19 -0500

    multimedia/bcmatroska2: Update version 0.23=>5.2.1
    - Adopt port
    - Add LICENSE BSD3CLAUSE
    - Change MASTERSITES to https://gitlab.linphone.org/BC GITLAB
    
    Changelog:
    https://gitlab.linphone.org/BC/public/bcmatroska2/-/tags/5.2.1

--- multimedia/bcmatroska2/files/patch-corec-corec-helpers-file-file_libc.c
+++ multimedia/bcmatroska2/files/patch-corec-corec-helpers-file-file_libc.c
@@ -1,9 +1,9 @@
---- corec/corec/helpers/file/file_libc.c.orig	2017-07-20 13:52:29 UTC
+--- corec/corec/helpers/file/file_libc.c.orig	2022-11-29 10:25:07 UTC
 +++ corec/corec/helpers/file/file_libc.c
-@@ -40,7 +40,7 @@
- #include <unistd.h>
- #include <dirent.h>
- #include <errno.h>
+@@ -44,7 +44,7 @@
+ // Do not move. Define TARGET_*.
+ #include "file.h"
+ 
 -#if defined(TARGET_OSX)
 +#if defined(TARGET_OSX) || defined(__FreeBSD__)
  #include <sys/mount.h>
