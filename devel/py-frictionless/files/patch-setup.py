5c68940a30932c6a7867875a3d235051ef61dd11
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-03-22 02:43:55 +0800

    devel/py-frictionless: Update to 4.40.11
    Changes:	https://github.com/frictionlessdata/frictionless-py/releases
    		https://github.com/frictionlessdata/frictionless-py/blob/main/CHANGELOG.md
    		https://github.com/frictionlessdata/frictionless-py/commits/main

---
+++ devel/py-frictionless/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-02-17 16:01:29 UTC
++++ setup.py
+@@ -78,7 +78,7 @@ INSTALL_REQUIRES = [
+     "validators>=0.18",
+     "python-slugify>=1.2",
+     "python-dateutil>=2.8",
+-    "tabulate>=0.8.10",
++    "tabulate>=0.8.9",
+     "jinja2>=3.0.3",
+ ]
+ README = read("README.md")
