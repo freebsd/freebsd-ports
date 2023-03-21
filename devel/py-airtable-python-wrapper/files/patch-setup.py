cc4ae28438969833faf377ae503160f6caf5596a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-03-22 02:35:01 +0800

    devel/py-airtable-python-wrapper: Add py-airtable-python-wrapper 0.15.3
    Python Client for the Airtable API

---
+++ devel/py-airtable-python-wrapper/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2021-07-27 05:35:10 UTC
++++ setup.py
+@@ -7,7 +7,7 @@ about = {}
+ with open(os.path.join(here, "airtable", "__version__.py"), mode="r") as f:
+     exec(f.read(), about)
+ 
+-setup_requires = ["pytest-runner"]
++setup_requires = []
+ install_requires = ["requests>=2"]
+ tests_require = ["requests-mock", "requests", "mock", "pytest"]
+ 
