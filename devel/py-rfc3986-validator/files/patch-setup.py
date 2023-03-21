561df5ce2f4c9dc34236ed30d2a2cf6db26d4f80
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-03-22 02:35:15 +0800

    devel/py-rfc3986-validator: Add py-rfc3986-validator 0.1.1
    A pure python RFC3986 validator

---
+++ devel/py-rfc3986-validator/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2019-10-28 15:38:52 UTC
++++ setup.py
+@@ -10,7 +10,7 @@ with open('README.md') as readme_file:
+ 
+ requirements = []
+ 
+-setup_requirements = ['pytest-runner', ]
++setup_requirements = []
+ 
+ test_requirements = ['pytest>=3', ]
+ 
