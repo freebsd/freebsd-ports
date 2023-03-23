9cd34a82b423ff3f24d079edf17b674fb290d43e
Ganael LAPLANCHE <martymac@FreeBSD.org>
2023-03-23 12:36:14 +0100

    games/flightgear: Update to 2020.3.18
    While here, re-generate patch for devel/simgear

--- devel/simgear/files/patch-simgear_scene_viewer_Compositor.hxx
+++ devel/simgear/files/patch-simgear_scene_viewer_Compositor.hxx
@@ -1,4 +1,4 @@
---- simgear/scene/viewer/Compositor.hxx
+--- simgear/scene/viewer/Compositor.hxx.orig	2023-03-14 07:49:42 UTC
 +++ simgear/scene/viewer/Compositor.hxx
 @@ -17,6 +17,7 @@
  #ifndef SG_COMPOSITOR_HXX
