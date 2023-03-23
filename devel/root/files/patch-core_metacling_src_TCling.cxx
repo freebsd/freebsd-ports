7e15a860f43a56a7da9eb4b32e8c683548db7339
Erik Jensen <erik@tenku.dk>
2023-03-23 08:07:37 +0000

    devel/root: Update to 6.28/02
    ChangeLog:	https://root.cern/doc/v628/release-notes.html#release-6.2802
    PR:		270407

--- devel/root/files/patch-core_metacling_src_TCling.cxx
+++ devel/root/files/patch-core_metacling_src_TCling.cxx
@@ -1,4 +1,4 @@
---- core/metacling/src/TCling.cxx.orig	2023-02-13 15:21:06 UTC
+--- core/metacling/src/TCling.cxx.orig	2023-03-22 17:40:48 UTC
 +++ core/metacling/src/TCling.cxx
 @@ -1249,6 +1249,8 @@ static void RegisterCxxModules(cling::Interpreter &cli
           true
@@ -9,7 +9,7 @@
  #else // Windows
           false
  #endif
-@@ -3366,6 +3368,8 @@ void TCling::UpdateListOfLoadedSharedLibraries()
+@@ -3371,6 +3373,8 @@ void TCling::UpdateListOfLoadedSharedLibraries()
     dl_iterate_phdr(callback_for_dl_iterate_phdr, &newLibs);
     for (auto &&lib: newLibs)
        RegisterLoadedSharedLibrary(lib.c_str());
