f1f1a8be887ee2c5d75bec33cb8f8a89454e606b
Jan Beich <jbeich@FreeBSD.org>
2023-03-23 17:56:40 +0000

    devel/icu: update to 73.1
    - Temporarily switch to GitHub auto archive (release artifacts are N/A atm)
    
    Changes:	https://github.com/unicode-org/icu/releases/tag/release-73-1
    Reported by:	GitHub (watch releases)
    PR:		270422
    Exp-run by:	antoine

--- devel/icu/files/patch-common_putil.cpp
+++ devel/icu/files/patch-common_putil.cpp
@@ -1,15 +1,15 @@
---- common/putil.cpp.orig	2016-10-19 17:20:56 UTC
+--- common/putil.cpp.orig	2023-03-23 17:56:40 UTC
 +++ common/putil.cpp
-@@ -1789,7 +1789,7 @@ remapPlatformDependentCodepage(const cha
+@@ -2004,7 +2004,7 @@ remapPlatformDependentCodepage(const char *locale, con
           */
          name = "ISO-8859-1";
      }
 -#elif U_PLATFORM_IS_DARWIN_BASED
 +#elif U_PLATFORM_IS_DARWIN_BASED || U_PLATFORM == U_PF_BSD
-     if (locale == NULL && *name == 0) {
+     if (locale == nullptr && *name == 0) {
          /*
          No locale was specified, and an empty name was passed in.
-@@ -1808,11 +1808,6 @@ remapPlatformDependentCodepage(const cha
+@@ -2023,11 +2023,6 @@ remapPlatformDependentCodepage(const char *locale, con
           */
          name = "UTF-8";
      }
@@ -19,9 +19,9 @@
 -        name = "EUC-KR";
 -    }
  #elif U_PLATFORM == U_PF_HPUX
-     if (locale != NULL && uprv_strcmp(locale, "zh_HK") == 0 && uprv_strcmp(name, "big5") == 0) {
+     if (locale != nullptr && uprv_strcmp(locale, "zh_HK") == 0 && uprv_strcmp(name, "big5") == 0) {
          /* HP decided to extend big5 as hkbig5 even though it's not compatible :-( */
-@@ -1942,7 +1937,7 @@ int_getDefaultCodepage()
+@@ -2183,7 +2178,7 @@ int_getDefaultCodepage()
         nl_langinfo may use the same buffer as setlocale. */
      {
          const char *codeset = nl_langinfo(U_NL_LANGINFO_CODESET);
