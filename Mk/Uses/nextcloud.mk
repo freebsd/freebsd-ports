a1e75a92d14b8e22a2927e414832794346ee9f1c
Bernard Spil <brnrd@FreeBSD.org>
2023-03-24 10:15:55 +0000

    www/nextcloud: Mark 8.2 compatible for 26

--- Mk/Uses/nextcloud.mk
+++ Mk/Uses/nextcloud.mk
@@ -30,7 +30,6 @@ LICENSE?=	AGPLv3
 RUN_DEPENDS=	nextcloud${PHP_PKGNAMESUFFIX}>=${NEXTCLOUD_RUN_DEPENDS}:www/nextcloud@${PHP_FLAVOR}
 
 USES+=		php:flavors
-IGNORE_WITH_PHP=	82
 NO_BUILD=	yes
 NO_ARCH=	yes
 
