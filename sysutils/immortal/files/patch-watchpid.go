34f23989992d1e0a47273ba6aad165cb5d73622d
Robert Clausecker <fuz@FreeBSD.org>
2023-03-25 13:22:43 +0000

    sysutils/immortal: fix build on riscv64
    Patch build constraints to include riscv64 on some
    architecture-dependent source files.
    
    Sponsored by:	Berliner Linux User Group e.V.
    Approved by:	portmgr (build fix blanket)

---
+++ sysutils/immortal/files/patch-watchpid.go
@@ -0,0 +1,9 @@
+--- watchpid.go.orig	2023-03-25 13:20:09 UTC
++++ watchpid.go
+@@ -1,5 +1,5 @@
+ // +build freebsd netbsd openbsd dragonfly darwin
+-// +build amd64 arm64
++// +build amd64 arm64 riscv64
+ 
+ package immortal
+ 
