b16985093aa15fd4c39768d932fb632c6d162f23
Yuri Victorovich <yuri@FreeBSD.org>
2023-03-26 16:31:58 -0700

    devel/spyder: Update 5.4.0 → 5.4.2
    Reported by:	portscout

--- devel/spyder/files/patch-setup.py
+++ devel/spyder/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2022-11-04 20:45:25 UTC
+--- setup.py.orig	2023-01-18 21:16:10 UTC
 +++ setup.py
 @@ -91,7 +91,7 @@ def get_data_files():
      """
@@ -17,22 +17,16 @@
  )
  
  
-@@ -229,13 +228,13 @@ install_requires = [
+@@ -229,10 +228,10 @@ install_requires = [
      'pyls-spyder>=0.4.0',
      'pyqt5<5.16',
      'pyqtwebengine<5.16',
--    'python-lsp-server[all]>=1.6.0,<1.7.0',
--    'pyxdg>=0.26;platform_system=="Linux"',
-+    'python-lsp-server[all]>0', # maybe dangerous to ignore >=1.6.0,<1.7.0
-+    'pyxdg>=0.26',
+-    'python-lsp-server[all]>=1.7.1,<1.8.0',
++    'python-lsp-server[all]',
+     'pyxdg>=0.26;platform_system=="Linux"',
      'pyzmq>=22.1.0',
 -    'qdarkstyle>=3.0.2,<3.1.0',
 +    'qdarkstyle>=3.0.2',
      'qstylizer>=0.2.2',
--    'qtawesome>=1.2.1',
--    'qtconsole>=5.4.0,<5.5.0',
-+    'qtawesome', # maybe dangerous to ignore >=1.2.1
-+    'qtconsole',
-     'qtpy>=2.1.0',
-     'rtree>=0.9.7',
-     'setuptools>=49.6.0',
+     'qtawesome>=1.2.1',
+     'qtconsole>=5.4.0,<5.5.0',
--- devel/spyder/files/patch-spyder_dependencies.py
+++ devel/spyder/files/patch-spyder_dependencies.py
@@ -1,11 +1,11 @@
---- spyder/dependencies.py.orig	2022-11-04 20:45:25 UTC
+--- spyder/dependencies.py.orig	2023-01-18 21:16:10 UTC
 +++ spyder/dependencies.py
-@@ -53,15 +53,15 @@ PSUTIL_REQVER = '>=5.3'
+@@ -53,12 +53,12 @@ PSUTIL_REQVER = '>=5.3'
  PYGMENTS_REQVER = '>=2.0'
  PYLINT_REQVER = '>=2.5.0;<3.0'
  PYLINT_VENV_REQVER = '>=2.1.1'
--PYLSP_REQVER = '>=1.6.0;<1.7.0'
-+PYLSP_REQVER = '>0' # until dependency is updated
+-PYLSP_REQVER = '>=1.7.1;<1.8.0'
++PYLSP_REQVER = '>0'
  PYLSP_BLACK_REQVER = '>=1.2.0'
  PYLS_SPYDER_REQVER = '>=0.4.0'
  PYXDG_REQVER = '>=0.26'
@@ -13,10 +13,5 @@
 -QDARKSTYLE_REQVER = '>=3.0.2;<3.1.0'
 +QDARKSTYLE_REQVER = '>=3.0.2'
  QSTYLIZER_REQVER = '>=0.2.2'
--QTAWESOME_REQVER = '>=1.2.1'
--QTCONSOLE_REQVER = '>=5.4.0;<5.5.0'
-+QTAWESOME_REQVER = '>0' # until dependency is updated
-+QTCONSOLE_REQVER = '>0' # until dependency is updated
- QTPY_REQVER = '>=2.1.0'
- RTREE_REQVER = '>=0.9.7'
- SETUPTOOLS_REQVER = '>=49.6.0'
+ QTAWESOME_REQVER = '>=1.2.1'
+ QTCONSOLE_REQVER = '>=5.4.0;<5.5.0'
