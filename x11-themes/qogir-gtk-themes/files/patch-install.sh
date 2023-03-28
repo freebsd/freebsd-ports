51e410215358d50f9f3d19b9898649aa69e03b28
Olivier Duchateau <duchateau.olivier@gmail.com>
2023-03-28 11:11:25 +0900

    x11-themes/qogir-gtk-themes: update to 2023-02-27
    Changelog: https://github.com/vinceliuice/Qogir-theme/releases/tag/2023-02-27
    
    PR:		269880
    Reported by:	Olivier Duchateau <duchateau.olivier@gmail.com> (maintainer)

--- x11-themes/qogir-gtk-themes/files/patch-install.sh
+++ x11-themes/qogir-gtk-themes/files/patch-install.sh
@@ -1,6 +1,6 @@
---- install.sh.orig	2022-10-16 06:48:19 UTC
+--- install.sh.orig	2023-02-27 08:02:22 UTC
 +++ install.sh
-@@ -556,18 +556,13 @@ tweaks_temp() {
+@@ -637,18 +637,13 @@ tweaks_temp() {
    cp -rf ${SRC_DIR}/src/_sass/_tweaks.scss ${SRC_DIR}/src/_sass/_tweaks-temp.scss
  }
  
@@ -11,30 +11,27 @@
 -
  install_win_titlebutton() {
 -  sed -i "/\$titlebutton:/s/circle/square/" ${SRC_DIR}/src/_sass/_tweaks-temp.scss
-+  sed -i "" -e "s|titlebutton: 'circle|titlebutton: 'square|" ${SRC_DIR}/src/_sass/_tweaks-temp.scss
++  sed -i "" -e "s/titlebutton: 'circle/titlebutton: 'square/" ${SRC_DIR}/src/_sass/_tweaks-temp.scss
    echo -e "Install Square titlebutton version ..."
  }
  
  install_round_window() {
 -  sed -i "/\$window:/s/default/round/" ${SRC_DIR}/src/_sass/_tweaks-temp.scss
-+  sed -i "" -e "s|window: 'default|window: 'round|" ${SRC_DIR}/src/_sass/_tweaks-temp.scss
++  sed -i "" -e "s/window: 'default/window: 'round/" ${SRC_DIR}/src/_sass/_tweaks-temp.scss
    echo -e "Install Round window version ..."
  }
  
-@@ -581,20 +576,16 @@ install_theme_color() {
+@@ -662,7 +657,7 @@ install_theme_color() {
          theme_color='ubuntu'
          ;;
      esac
 -    sed -i "/\$theme:/s/default/${theme_color}/" ${SRC_DIR}/src/_sass/_tweaks-temp.scss
-+    sed -i "" -e "s|theme: 'default|theme: '${theme_color}|" ${SRC_DIR}/src/_sass/_tweaks-temp.scss
++    sed -i "" -e "s/theme: 'default/theme: '${theme_color}/" ${SRC_DIR}/src/_sass/_tweaks-temp.scss
    fi
  }
  
- theme_tweaks() {
--  if [[ "$image" == "true" || "$square" == "true" || "$accent" == 'true' || "$window" == 'round' ]]; then
-+  if [[ "$square" == "true" || "$accent" == 'true' || "$window" == 'round' ]]; then
-     tweaks='true'
-     install_package; tweaks_temp
+@@ -672,10 +667,6 @@ theme_tweaks() {
+     install_css_deps; tweaks_temp
    fi
  
 -  if [[ "$image" == "true" ]] ; then
@@ -44,7 +41,7 @@
    if [[ "$square" == "true" ]] ; then
      install_win_titlebutton
    fi
-@@ -635,8 +626,6 @@ uninstall_theme() {
+@@ -716,8 +707,6 @@ uninstall_theme() {
      done
    done
  }
