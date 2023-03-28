9ae57bb754413fc9394ccad44c97926175df61a0
Robert Clausecker <fuz@FreeBSD.org>
2023-03-28 14:58:28 +0200

    graphics/lux: update to 1.1.6
    Patch out an unused SIMD function that didn't build on i386
    as per upstream instructions.
    
    Changelog: https://groups.google.com/g/hugin-ptx/c/RFnsB3JyKpU/m/zrbdlQlDEQAJ

---
+++ graphics/lux/files/patch-vspline_hwy__simd__type.h
@@ -0,0 +1,10 @@
+--- vspline/hwy_simd_type.h.orig	2023-03-30 09:58:06 UTC
++++ vspline/hwy_simd_type.h
+@@ -694,7 +694,6 @@ PROMOTE(unsigned short,int)
+ PROMOTE(unsigned char,int)
+ PROMOTE(unsigned char,short)
+ 
+-PROMOTE(short,unsigned int)
+ PROMOTE(signed char,unsigned int)
+ PROMOTE(unsigned short,unsigned int)
+ PROMOTE(unsigned char,unsigned int)
