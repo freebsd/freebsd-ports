77cefb783868a018ff996012b46865e6e4c16f15
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-04-06 01:16:08 +0800

    devel/py-frictionless: Update to 5.10.1
    - Add API, GITHUB, MYSQL, PARQUET, POSTGRESQL, VISIDATA and WRK options
    - Rename S3 option to AWS option
    - Remove SERVER option
    
    Changes:	https://github.com/frictionlessdata/frictionless-py/releases
    		https://github.com/frictionlessdata/frictionless-py/blob/main/CHANGELOG.md
    		https://github.com/frictionlessdata/frictionless-py/commits/main

--- devel/py-frictionless/files/patch-setup.py
+++ devel/py-frictionless/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2023-02-17 16:01:29 UTC
+--- setup.py.orig	2023-03-20 17:33:32 UTC
 +++ setup.py
-@@ -78,7 +78,7 @@ INSTALL_REQUIRES = [
-     "validators>=0.18",
-     "python-slugify>=1.2",
-     "python-dateutil>=2.8",
+@@ -73,7 +73,7 @@ INSTALL_REQUIRES = [
+     "pydantic>=1.9",
+     "requests>=2.10",
+     "humanize>=4.2",
 -    "tabulate>=0.8.10",
 +    "tabulate>=0.8.9",
-     "jinja2>=3.0.3",
- ]
- README = read("README.md")
+     "jsonschema>=2.5",
+     "simpleeval>=0.9.11",
+     "stringcase>=1.2",
