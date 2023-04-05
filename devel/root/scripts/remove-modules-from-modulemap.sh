41a479ef801eb1f07456637e90afdecce951659b
Erik Jensen <erik@tenku.dk>
2023-04-05 10:59:00 +0100

    devel/root: Remove 12.3 workaround and clean up
    Remove build workaround for 12.3 re-added in last commit by mistake that
    was removed in 5693229 commit.
    Cleanup port and delete scripts dir used by workaround.
    
    PR:		270625
    MFH:		2023Q2 (buildfixes)

--- devel/root/scripts/remove-modules-from-modulemap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Script removes module definition blocks in LLVM/Clang .modulemap
-# files "in-place" in the file path stored in the variable MODULEMAP
-
-modules=$1
-tmp=${MODULEMAP}.tmp && awk -f ${SCRIPTDIR}/remove-modules-from-modulemap.awk -v exclude="${modules}" ${MODULEMAP} > ${tmp} && mv ${tmp} ${MODULEMAP} || rm ${tmp}
