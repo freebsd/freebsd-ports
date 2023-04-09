265945fa58c166aabc06e9cedeefe48cd5614ddf
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-04-10 04:39:08 +0800

    devel/py-livemark: Add py-livemark 0.110.5
    Data presentation framework for Python that generates static sites from extended
    Markdown with interactive charts, tables, scripts, and other features.
    
    Purpose:
    - Data Journalism: Livemark provides a complete toolset for working with data,
      starting from data extraction and ending with a published website containing
      interactive charts, tables, and other features.
    - Software Education: Livemark is perfectly suited for writing education
      materials as it uses code execution model in markdown documents. It solves a
      range of problems with testing and having your code example up-to-date.
    - Python Development: Livemark can be used in software development as a helper
      tool for working on Python projects. It provides an ability to create
      documentation sites and works as a task runner.

---
+++ devel/py-livemark/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-03-16 09:34:49 UTC
++++ setup.py
+@@ -40,7 +40,7 @@ INSTALL_REQUIRES = [
+     "marko>=1.0",
+     "pyyaml>=5.3",
+     "jinja2>=3.0",
+-    "pyquery==1.*",
++    "pyquery>=1.0",
+     "deepmerge>=0.3",
+     "gitpython>=3.1",
+     "jsonschema>=2.5",
