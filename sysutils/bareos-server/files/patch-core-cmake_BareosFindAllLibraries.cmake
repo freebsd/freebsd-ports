e106c20f319068e97a59f6fec7aa543b63cf42c5
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2023-04-10 15:27:13 -0500

    sysutils/bareos-*: update to 22.0.3
    - Remove obsolete patch files
    - Remove obsolete options

--- sysutils/bareos-server/files/patch-core-cmake_BareosFindAllLibraries.cmake
+++ sysutils/bareos-server/files/patch-core-cmake_BareosFindAllLibraries.cmake
@@ -1,24 +1,11 @@
---- core/cmake/BareosFindAllLibraries.cmake	2022-08-05 04:40:11.000000000 -0500
-+++ core/cmake/BareosFindAllLibraries.cmake	2022-10-31 16:28:14.422391000 -0500
-@@ -175,12 +175,18 @@
-   )
- endif()
+--- core/cmake/BareosFindAllLibraries.cmake	2023-03-24 08:05:20.000000000 -0500
++++ core/cmake/BareosFindAllLibraries.cmake	2023-04-05 16:21:00.503424000 -0500
+@@ -177,7 +177,9 @@
  
--bareosfindlibraryandheaders("rados" "rados/librados.h" "")
--bareosfindlibraryandheaders("radosstriper" "radosstriper/libradosstriper.h" "")
--bareosfindlibraryandheaders("cephfs" "cephfs/libcephfs.h" "")
-+if(rados)
-+  bareosfindlibraryandheaders("rados" "rados/librados.h" "")
-+  bareosfindlibraryandheaders("radosstriper" "radosstriper/libradosstriper.h" "")
-+endif()
-+if(cephfs)
-+  bareosfindlibraryandheaders("cephfs" "cephfs/libcephfs.h" "")
-+endif()
  bareosfindlibraryandheaders("pthread" "pthread.h" "")
  bareosfindlibraryandheaders("cap" "sys/capability.h" "")
--bareosfindlibraryandheaders("gfapi" "glusterfs/api/glfs.h" "")
 +if(gfapi)
-+  bareosfindlibraryandheaders("gfapi" "glusterfs/api/glfs.h" "")
+ bareosfindlibraryandheaders("gfapi" "glusterfs/api/glfs.h" "")
 +endif()
  
  bareosfindlibraryandheaders("pam" "security/pam_appl.h" "")
