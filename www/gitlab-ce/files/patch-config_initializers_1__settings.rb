f3e532069a198a3bd72cd8e688cda52a5925e3aa
Matthias Fechner <mfechner@FreeBSD.org>
2023-04-12 16:39:37 +0300

    www/gitlab-ce: update to 15.10.2
    Changelog:
    https://about.gitlab.com/releases/2023/04/05/gitlab-15-10-2-released/

--- www/gitlab-ce/files/patch-config_initializers_1__settings.rb
+++ www/gitlab-ce/files/patch-config_initializers_1__settings.rb
@@ -1,6 +1,6 @@
---- config/initializers/1_settings.rb.orig	2023-02-21 16:38:51 UTC
+--- config/initializers/1_settings.rb.orig	2023-04-05 16:11:51 UTC
 +++ config/initializers/1_settings.rb
-@@ -208,11 +208,7 @@ Settings.gitlab['url'] ||= Settings.__send__(:build_gi
+@@ -209,11 +209,7 @@ Settings.gitlab['url'] ||= Settings.__send__(:build_gi
  Settings.gitlab['user'] ||= 'git'
  # External configuration may cause the ssh user to differ from the GitLab user
  Settings.gitlab['ssh_user'] ||= Settings.gitlab.user
