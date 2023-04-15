f36df7ffe63167fa293bd89a072da8ba85289c09
Alexey Dokuchaev <danfe@FreeBSD.org>
2023-04-15 10:05:20 +0000

    misc/gedkeeper: update GEDKeeper to version 2.25.2
    Reported by:	portscout

--- misc/gedkeeper/files/patch-projects_GKCore_GKCore_GKUtils.cs
+++ misc/gedkeeper/files/patch-projects_GKCore_GKCore_GKUtils.cs
@@ -1,19 +1,6 @@
---- projects/GKCore/GKCore/GKUtils.cs.orig	2023-03-27 21:40:19 UTC
+--- projects/GKCore/GKCore/GKUtils.cs.orig	2023-04-13 18:03:47 UTC
 +++ projects/GKCore/GKCore/GKUtils.cs
-@@ -1758,7 +1758,11 @@ namespace GKCore
- 
-         public static string GetTempDir()
-         {
--            string tempPath = Environment.GetEnvironmentVariable("TEMP");
-+            string tempPath;
-+            if (SysUtils.IsUnix())
-+                tempPath = Environment.GetEnvironmentVariable("TMP") ?? "/tmp";
-+            else
-+                tempPath = Environment.GetEnvironmentVariable("TEMP");
-             return tempPath + Path.DirectorySeparatorChar;
-         }
- 
-@@ -1772,8 +1776,7 @@ namespace GKCore
+@@ -1777,8 +1777,7 @@ namespace GKCore
  
          public static string GetAppPath()
          {
