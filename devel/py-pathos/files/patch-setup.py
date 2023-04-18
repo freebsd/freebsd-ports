2186d1122c43fe61dc2e6fc47b47e2bdf5e0a361
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-04-19 01:57:28 +0800

    devel/py-pathos: Update to 0.3.0
    - Convert to USE_PYTHON=pep517
    
    Changes:	https://github.com/uqfoundation/pathos/releases

---
+++ devel/py-pathos/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2022-10-24 02:43:02 UTC
++++ setup.py
+@@ -91,7 +91,7 @@ class BinaryDistribution(Distribution):
+ ppft_version = 'ppft>=1.7.6.6'
+ dill_version = 'dill>=0.3.6'
+ pox_version = 'pox>=0.3.2'
+-mp_version = 'multiprocess>=0.70.14'
++mp_version = 'multiprocess>=0.70.12'
+ pyina_version = 'pyina>=0.2.6'
+ mystic_version = 'mystic>=0.3.9'
+ # add dependencies
