bc28f863e7880ff17f9a01e5f73d8f7107397365
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-04-19 01:58:10 +0800

    www/py-wagtail: Fix d7f9610cf9322ef01f626713e86fa443cf629398
    - Bump PORTREVISION for package change
    
    Restore accidentally-removed draftjs_exporter patch.
    
    (cherry picked from commit 35020f5e575e29f63a346b1c9c1e012a8f64125c)

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,8 +1,11 @@
 --- setup.py.orig	2023-02-06 13:19:58 UTC
 +++ setup.py
-@@ -28,7 +28,7 @@ install_requires = [
+@@ -26,9 +26,9 @@ install_requires = [
+     "django-treebeard>=4.5.1,<5.0",
+     "djangorestframework>=3.11.1,<4.0",
      "django-filter>=2.2,<23",
-     "draftjs_exporter>=2.1.5,<3.0",
+-    "draftjs_exporter>=2.1.5,<3.0",
++    "draftjs_exporter>=2.1.5",
      "Pillow>=4.0.0,<10.0.0",
 -    "beautifulsoup4>=4.8,<4.12",
 +    "beautifulsoup4>=4.8,<4.13",
