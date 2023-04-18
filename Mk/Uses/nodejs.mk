1d6547adc9d3d1667e42df453b7237701f0fa877
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-04-19 05:47:57 +0800

    Mk/Uses/nodejs.mk: Add node 20 as new current

--- Mk/Uses/nodejs.mk
+++ Mk/Uses/nodejs.mk
@@ -7,13 +7,13 @@
 #   - build     use node as build-time dependency
 #   - run       use node as runtime dependency
 #   - env       set the environment (NODEJS_VERSION and NODEJS_SUFFIX)
-#   - version   available version: lts, current, 14, 16, 18, 19
+#   - version   available version: lts, current, 14, 16, 18, 19, 20
 #
 # Note:
 # - The supported versions follow upstream release schedule
 #   https://github.com/nodejs/Release/blob/main/README.md#release-schedule
 #   - lts is 18 now
-#   - current is 19 now
+#   - current is 20 now
 # - USES=nodejs means USES=nodejs:build,run
 # - If you define a version, you must provide run and/or build
 #
@@ -22,14 +22,14 @@
 .if !defined(_INCLUDE_USES_NODEJS_MK)
 _INCLUDE_USES_NODEJS_MK=	yes
 
-_VALID_NODEJS_VERSIONS=	14 16 18 19 current lts
+_VALID_NODEJS_VERSIONS=	14 16 18 19 20 current lts
 
 .  if ! ${_VALID_NODEJS_VERSIONS:M${NODEJS_DEFAULT}}
 IGNORE=	Invalid default nodejs version ${NODEJS_DEFAULT}; valid versions are ${_VALID_NODEJS_VERSIONS}
 .  endif
 
-.  if !empty(nodejs_ARGS:Nbuild:Nenv:Nrun:Nlts:Ncurrent:N14:N16:N18:N19)
-IGNORE=		USES=nodejs has invalid arguments ${nodejs_ARGS}.
+.  if !empty(nodejs_ARGS:Nbuild:Nenv:Nrun:Nlts:Ncurrent:N14:N16:N18:N19:N20)
+IGNORE=		USES=nodejs has invalid arguments ${nodejs_ARGS}
 .  endif
 
 .  if empty(nodejs_ARGS)
@@ -47,7 +47,7 @@ _NODEJS_VER=	${version}
 _NODEJS_VER=	${NODEJS_DEFAULT}
 .  endif
 
-NODEJS_VERSION=	${_NODEJS_VER:S|current|19|:S|lts|18|}
+NODEJS_VERSION=	${_NODEJS_VER:S|current|20|:S|lts|18|}
 NODEJS_SUFFIX=	-node${NODEJS_VERSION}
 
 .  if ${nodejs_ARGS:M*build*}
