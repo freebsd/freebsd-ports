f1f155eea12c242e6670c7b0f80b25f280b207a9
Li-Wen Hsu <lwhsu@FreeBSD.org>
2023-04-28 02:24:28 +0800

    net-im/py-matrix-synapse: Update to 1.82.0
    PR:             270737
    Approved by:	maintainer timeout

--- net-im/py-matrix-synapse/files/patch-setup.py
+++ net-im/py-matrix-synapse/files/patch-setup.py
@@ -1,16 +1,5 @@
---- setup.py.orig	1970-01-01 00:00:00 UTC
+--- setup.py.orig	2023-04-21 21:41:33 UTC
 +++ setup.py
-@@ -154,8 +154,8 @@ install_requires = \
-  'bcrypt>=3.1.7',
-  'bleach>=1.4.3',
-  'canonicaljson>=1.5.0,<2.0.0',
-- 'cryptography>=3.4.7',
-- 'frozendict>=1,!=2.1.2,<2.3.5',
-+ 'cryptography',
-+ 'frozendict>=1',
-  'ijson>=3.1.4',
-  'jsonschema>=3.0.0',
-  'matrix-common>=1.3.0,<2.0.0',
 @@ -170,7 +170,6 @@ install_requires = \
   'pydantic>=1.7.4',
   'pymacaroons>=0.13.0',
