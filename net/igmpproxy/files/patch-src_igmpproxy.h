e9b7c7598b956dd924dc655c88026a17454ff2ae
Franco Fichtner <franco@opnsense.org>
2023-04-28 11:29:28 +0200

    net/igmpproxy: update to 0.4
    Changelog: https://github.com/pali/igmpproxy/releases/tag/0.4
    
    PR:		271093

--- net/igmpproxy/files/patch-src_igmpproxy.h
+++ net/igmpproxy/files/patch-src_igmpproxy.h
@@ -1,11 +1,20 @@
---- src/igmpproxy.h.orig	2021-01-12 08:24:34 UTC
+--- src/igmpproxy.h.orig	2022-10-29 10:01:46 UTC
 +++ src/igmpproxy.h
-@@ -112,7 +112,7 @@ void my_log( int Serverity, int Errno, const char *Fmt
+@@ -115,7 +115,7 @@ void my_log( int Serverity, int Errno, const char *Fmt
  
  /* ifvc.c
   */
 -#define MAX_IF         40     // max. number of interfaces recognized
-+#define MAX_IF         140     // max. number of interfaces recognized
++#define MAX_IF         240     // max. number of interfaces recognized
  
  // Interface states
  #define IF_STATE_DISABLED      0   // Interface should be ignored.
+@@ -183,7 +183,7 @@ struct Config {
+     unsigned short	defaultInterfaceState;	// 0: disable, 2: downstream
+     //~ aimwang added done
+     char                chroot[PATH_MAX];
+-    char                user[LOGIN_NAME_MAX];
++    char                user[MAXLOGNAME];
+ };
+ 
+ // Holds the indeces of the upstream IF...
