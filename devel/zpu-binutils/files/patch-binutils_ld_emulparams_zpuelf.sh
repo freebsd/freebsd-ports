2511a76e3799f56771affad70864969116168ac6
Rene Ladan <rene@FreeBSD.org>
2023-04-30 11:23:42 +0200

    devel/zpu-binutils/: Remove expired port:
    2023-04-30 devel/zpu-binutils: Broken with latest C compiler and port unused

--- devel/zpu-binutils/files/patch-binutils_ld_emulparams_zpuelf.sh
+++
@@ -1,9 +0,0 @@
---- binutils/ld/emulparams/zpuelf.sh.orig	2012-10-12 15:43:08.000000000 -0500
-+++ binutils/ld/emulparams/zpuelf.sh	2012-10-12 15:43:55.000000000 -0500
-@@ -27,4 +27,5 @@
- DTOR_START='___dtors = .;'
- DTOR_END='___dtors_end = .;'
- 
--STACK_ADDR=(0x1000000-0x10000)
-+STACK_ADDR=0x1000000-0x10000
-+#STACK_ADDR=(0x1000000-0x10000)
