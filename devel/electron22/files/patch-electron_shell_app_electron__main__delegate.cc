c131530b08a7524c20a46eda16509b831660d0ef
Hiroki Tagato <tagattie@FreeBSD.org>
2023-05-05 11:17:05 +0900

    devel/electron22: update to 22.3.8
    Changelog: https://github.com/electron/electron/releases/tag/v22.3.8
    
    Reported by:	GitHub (watch releases)

--- devel/electron22/files/patch-electron_shell_app_electron__main__delegate.cc
+++ devel/electron22/files/patch-electron_shell_app_electron__main__delegate.cc
@@ -1,6 +1,6 @@
---- electron/shell/app/electron_main_delegate.cc.orig	2023-01-24 16:58:16 UTC
+--- electron/shell/app/electron_main_delegate.cc.orig	2023-05-03 07:56:25 UTC
 +++ electron/shell/app/electron_main_delegate.cc
-@@ -56,13 +56,16 @@
+@@ -57,13 +57,16 @@
  #include "chrome/child/v8_crashpad_support_win.h"
  #endif
  
@@ -19,7 +19,7 @@
  #include "components/crash/core/app/crash_switches.h"  // nogncheck
  #include "components/crash/core/app/crashpad.h"        // nogncheck
  #include "components/crash/core/common/crash_key.h"
-@@ -159,7 +162,7 @@ bool ElectronPathProvider(int key, base::FilePath* res
+@@ -155,7 +158,7 @@ bool ElectronPathProvider(int key, base::FilePath* res
        create_dir = true;
        break;
      }
@@ -28,7 +28,7 @@
      case DIR_APP_DATA: {
        auto env = base::Environment::Create();
        cur = base::nix::GetXDGDirectory(
-@@ -293,7 +296,7 @@ absl::optional<int> ElectronMainDelegate::BasicStartup
+@@ -287,7 +290,7 @@ absl::optional<int> ElectronMainDelegate::BasicStartup
      base::win::PinUser32();
  #endif
  
@@ -37,7 +37,7 @@
    // Check for --no-sandbox parameter when running as root.
    if (getuid() == 0 && IsSandboxEnabled(command_line))
      LOG(FATAL) << "Running as root without --"
-@@ -345,7 +348,7 @@ void ElectronMainDelegate::PreSandboxStartup() {
+@@ -339,7 +342,7 @@ void ElectronMainDelegate::PreSandboxStartup() {
                                     process_type == ::switches::kZygoteProcess);
  #endif
  
@@ -46,7 +46,7 @@
    crash_reporter::InitializeCrashKeys();
  #endif
  
-@@ -380,7 +383,7 @@ void ElectronMainDelegate::PreSandboxStartup() {
+@@ -374,7 +377,7 @@ void ElectronMainDelegate::PreSandboxStartup() {
    }
  #endif
  
--- devel/electron22/files/patch-electron_shell_common_node__bindings.cc
+++ devel/electron22/files/patch-electron_shell_common_node__bindings.cc
@@ -1,4 +1,4 @@
---- electron/shell/common/node_bindings.cc.orig	2023-04-13 06:10:54 UTC
+--- electron/shell/common/node_bindings.cc.orig	2023-05-03 07:56:25 UTC
 +++ electron/shell/common/node_bindings.cc
 @@ -40,7 +40,7 @@
  #include "third_party/blink/renderer/bindings/core/v8/v8_initializer.h"  // nogncheck
@@ -18,7 +18,7 @@
    electron::crash_keys::SetCrashKey("electron.v8-fatal.message", message);
    electron::crash_keys::SetCrashKey("electron.v8-fatal.location", location);
  #endif
-@@ -429,7 +429,7 @@ void NodeBindings::Initialize() {
+@@ -428,7 +428,7 @@ void NodeBindings::Initialize() {
    TRACE_EVENT0("electron", "NodeBindings::Initialize");
    // Open node's error reporting system for browser process.
  
