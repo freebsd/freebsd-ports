05c4a05566903cad52b3bbb9b03da677ef2f7bd6
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-05-05 21:37:40 +0200

    emulators/qemu7: Update version 7.0.0=>7.2.1
    Merge the relevant port files from emulators/qemu. This is most probably
    going to be the last version update untill Qemu releases version 9 and
    we phase this out.

--- emulators/qemu7/files/patch-bsd-user_host_i386_host-signal.h
+++ emulators/qemu7/files/patch-bsd-user_host_i386_host-signal.h
@@ -1,12 +1,10 @@
---- bsd-user/host/i386/host-signal.h.orig	2022-09-08 16:00:45 UTC
+--- bsd-user/host/i386/host-signal.h.orig	2022-12-14 16:28:45 UTC
 +++ bsd-user/host/i386/host-signal.h
-@@ -9,6 +9,9 @@
- #ifndef I386_HOST_SIGNAL_H
- #define I386_HOST_SIGNAL_H
- 
-+#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__)
-+#include <sys/param.h>
-+#endif
+@@ -12,6 +12,7 @@
+ #include <sys/param.h>
  #include <sys/ucontext.h>
  #include <machine/trap.h>
++#include <vm/vm.h>
  #include <vm/pmap.h>
+ #include <machine/pmap.h>
+ 
