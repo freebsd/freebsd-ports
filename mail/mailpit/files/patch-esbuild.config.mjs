86768eb343516fd78f87ae149bbd2a636c79be24
Guido Falsi <madpilot@FreeBSD.org>
2023-05-09 11:36:42 +0200

    mail/mailpit: Update to 1.6.9
    - Added further patch to make it compile on all architectures.

---
+++ mail/mailpit/files/patch-esbuild.config.mjs
@@ -0,0 +1,8 @@
+--- esbuild.config.mjs.orig	2023-05-09 05:17:58 UTC
++++ esbuild.config.mjs
+@@ -1,4 +1,4 @@
+-import * as esbuild from 'esbuild'
++import * as esbuild from 'esbuild-wasm'
+ import pluginVue from 'esbuild-plugin-vue-next'
+ import { sassPlugin } from 'esbuild-sass-plugin'
+ 
