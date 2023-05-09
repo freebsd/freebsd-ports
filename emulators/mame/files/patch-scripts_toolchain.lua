3841ad2f6e56bcb80d80bcda7dd83fcb6fd9c3af
Alastair Hogge <agh@riseup.net>
2023-05-09 15:53:43 +0800

    emulators/mame: Update to 0.254
    emulators/mess has been merged to emulators/mame in the upstream.
    
    PR:		268691

---
+++ emulators/mame/files/patch-scripts_toolchain.lua
@@ -0,0 +1,12 @@
+--- scripts/toolchain.lua.orig	2023-05-06 02:56:32 UTC
++++ scripts/toolchain.lua
+@@ -60,7 +60,8 @@ function androidToolchainRoot()
+ 		local hostTags = {
+ 			windows = "windows-x86_64",
+ 			linux   = "linux-x86_64",
+-			macosx  = "darwin-x86_64"
++			macosx  = "darwin-x86_64",
++			bsd = "freebsd-x86_64"
+ 		}
+ 		android.toolchainRoot = (os.getenv("ANDROID_NDK_HOME") or "") .. "/toolchains/llvm/prebuilt/" .. hostTags[os.get()]
+ 	end
