89d7e1df96bfc29e6851e927d11b6f01f11cca3e
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-05-13 01:12:51 +0200

    net/libtnl: Unbreak on HEAD
    - Refresh patches
    - Pet portclippy
    - Adopt port

--- net/libtnl/files/patch-tnlTypes.h
+++ net/libtnl/files/patch-tnlTypes.h
@@ -3,9 +3,10 @@ other architectures is easy too.
 
 	-mi
 
---- tnlTypes.h	2004-09-28 14:45:25.000000000 -0400
-+++ tnlTypes.h	2008-08-17 10:30:26.000000000 -0400
-@@ -285,6 +285,6 @@
+--- tnlTypes.h.orig	2004-09-28 18:45:25 UTC
++++ tnlTypes.h
+@@ -284,8 +284,8 @@ typedef unsigned long long  U64;  ///< Compiler indepe
+ // Identify the target CPU and assembly language options
  //----------------------------------------------------------------------------------
  
 -#if defined(_M_IX86) || defined(i386)
@@ -14,10 +15,13 @@ other architectures is easy too.
 +#  define TNL_CPU_STRING "x86"
  #  define TNL_CPU_X86
  #  define TNL_LITTLE_ENDIAN
-@@ -294,5 +294,5 @@
+ #  define TNL_SUPPORTS_NASM
+@@ -293,7 +293,7 @@ typedef unsigned long long  U64;  ///< Compiler indepe
+ #  if defined (__GNUC__)
  #    if __GNUC__ == 2
  #      define TNL_GCC_2
 -#    elif __GNUC__ == 3
 +#    elif __GNUC__ >= 3
  #      define TNL_GCC_3
  #    else
+ #      error "TNL: Unsupported version of GCC (see tnlMethodDispatch.cpp)"
