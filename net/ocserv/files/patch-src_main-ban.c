39beea1060026e7f4751713a5719280698ccfb2b
Juraj Lutter <otis@FreeBSD.org>
2023-05-13 18:32:37 +0200

    net/ocserv: Update to 1.1.7
    - Update to 1.1.7
    - Regen patches

--- net/ocserv/files/patch-src_main-ban.c
+++ net/ocserv/files/patch-src_main-ban.c
@@ -1,20 +1,13 @@
---- src/main-ban.c.orig	2021-01-26 17:01:03 UTC
+--- src/main-ban.c.orig	2023-01-29 14:09:45 UTC
 +++ src/main-ban.c
-@@ -403,8 +403,8 @@ static bool test_local_ipv6(struct sockaddr_in6 * remo
+@@ -408,8 +408,8 @@ static bool test_local_ipv6(struct sockaddr_in6 * remo
  	unsigned index = 0;
- 	
+ 
  	for (index = 0; index < 4; index ++) {
 -		uint32_t l = local->sin6_addr.s6_addr32[index] & network->sin6_addr.s6_addr32[index];
 -		uint32_t r = remote->sin6_addr.s6_addr32[index] & network->sin6_addr.s6_addr32[index];
 +		uint32_t l = local->sin6_addr.__u6_addr.__u6_addr32[index] & network->sin6_addr.__u6_addr.__u6_addr32[index];
 +		uint32_t r = remote->sin6_addr.__u6_addr.__u6_addr32[index] & network->sin6_addr.__u6_addr.__u6_addr32[index];
- 		if (l != r) 
+ 		if (l != r)
  			return false;
  	}
-@@ -443,4 +443,4 @@ void if_address_cleanup(main_server_st * s)
- 
- 	s->if_addresses = NULL;
- 	s->if_addresses_count = 0;
--}
-\ No newline at end of file
-+}
