5c77d25480473e8fd9bd3a50b8a3e8c0cefead2b
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-05-16 21:13:55 +0200

    databases/memcached: Update version 1.6.14=>1.6.20
    Changelog: https://github.com/memcached/memcached/wiki/ReleaseNotes1620

--- databases/memcached/files/patch-memcached.c
+++ databases/memcached/files/patch-memcached.c
@@ -1,6 +1,6 @@
---- memcached.c.orig	2020-11-21 00:10:41 UTC
+--- memcached.c.orig	2023-03-08 21:34:27 UTC
 +++ memcached.c
-@@ -676,7 +676,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
+@@ -707,7 +707,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
          if (init_state == conn_listening) {
              fprintf(stderr, "<%d server listening (%s)\n", sfd,
                  prot_text(c->protocol));
@@ -9,7 +9,7 @@
              fprintf(stderr, "<%d server listening (udp)\n", sfd);
          } else if (c->protocol == negotiating_prot) {
              fprintf(stderr, "<%d new auto-negotiating client connection\n",
-@@ -736,7 +736,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
+@@ -772,7 +772,7 @@ conn *conn_new(const int sfd, enum conn_states init_st
          c->write = tcp_write;
      }
  
@@ -18,7 +18,7 @@
          c->try_read_command = try_read_command_udp;
      } else {
          switch (c->protocol) {
-@@ -820,7 +820,7 @@ static void conn_cleanup(conn *c) {
+@@ -871,7 +871,7 @@ static void conn_cleanup(conn *c) {
          c->sasl_conn = NULL;
      }
  
@@ -27,7 +27,7 @@
          conn_set_state(c, conn_read);
      }
  }
-@@ -1116,7 +1116,7 @@ bool resp_start(conn *c) {
+@@ -1171,7 +1171,7 @@ bool resp_start(conn *c) {
          c->resp->next = resp;
          c->resp = resp;
      }
@@ -36,7 +36,7 @@
          // need to hold on to some data for async responses.
          c->resp->request_id = c->request_id;
          c->resp->request_addr = c->request_addr;
-@@ -2046,7 +2046,7 @@ void process_stats_conns(ADD_STAT add_stats, void *c) 
+@@ -2166,7 +2166,7 @@ void process_stats_conns(ADD_STAT add_stats, void *c) 
               * output -- not worth the complexity of the locking that'd be
               * required to prevent it.
               */
@@ -45,7 +45,7 @@
                  APPEND_NUM_STAT(i, "UDP", "%s", "UDP");
              }
              if (conns[i]->state != conn_closed) {
-@@ -3242,7 +3242,7 @@ static void drive_machine(conn *c) {
+@@ -3361,7 +3361,7 @@ static void drive_machine(conn *c) {
              break;
  
          case conn_closing:
@@ -54,7 +54,7 @@
                  conn_cleanup(c);
              else
                  conn_close(c);
-@@ -3425,7 +3425,7 @@ static int server_socket(const char *interface,
+@@ -3552,7 +3552,7 @@ static int server_socket(const char *interface,
  #endif
  
          setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, (void *)&flags, sizeof(flags));
@@ -63,7 +63,7 @@
              maximize_sndbuf(sfd);
          } else {
              error = setsockopt(sfd, SOL_SOCKET, SO_KEEPALIVE, (void *)&flags, sizeof(flags));
-@@ -3480,7 +3480,7 @@ static int server_socket(const char *interface,
+@@ -3607,7 +3607,7 @@ static int server_socket(const char *interface,
              }
          }
  
