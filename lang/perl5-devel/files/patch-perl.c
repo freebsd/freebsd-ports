062338767ec5628d432222de01189a70eadbea6b
Mathieu Arnold <mat@FreeBSD.org>
2023-05-18 07:12:21 +0200

    lang/perl5-devel: update to latest commit
    (cherry picked from commit 9bd6ffdcb83d03f14dd9075b0d8967fe094cba59)

--- lang/perl5-devel/files/patch-perl.c
+++ lang/perl5-devel/files/patch-perl.c
@@ -1,8 +1,8 @@
 Remove compile time information (reproducible builds)
 
---- perl.c.orig	2022-06-01 02:05:01 UTC
+--- perl.c.orig	2023-05-16 23:36:32 UTC
 +++ perl.c
-@@ -2035,23 +2035,7 @@ S_Internals_V(pTHX_ CV *cv)
+@@ -2098,23 +2098,7 @@ S_Internals_V(pTHX_ CV *cv)
      PUSHs(Perl_newSVpvn_flags(aTHX_ non_bincompat_options,
                                sizeof(non_bincompat_options) - 1, SVs_TEMP));
  
