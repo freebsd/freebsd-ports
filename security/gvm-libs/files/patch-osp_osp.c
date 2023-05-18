4b312f7f6d1cbe0489a104130f35d0412d138212
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2023-05-18 15:35:14 -0500

    security/gvm: Update to 22.7.0
    - security/gvm-libs: Update to 22.6.0
    - security/gvmd: Bump PORTREVISION
    - security/openvas: Update to 22.7.0
    - security/py-notus-scanner: Update to 22.5.0
    - security/py-ospd-openvas: Update to 22.5.1
    - security/gsa: Bump PORTREVISION
    - security/gsad: Bump PORTREVISION
    - security/py-gvm-tools: Update to 23.4.0
    - security/py-python-gvm: Update to 23.5.0

--- security/gvm-libs/files/patch-osp_osp.c
+++ security/gvm-libs/files/patch-osp_osp.c
@@ -1,5 +1,5 @@
---- osp/osp.c	2022-07-18 03:40:56.000000000 -0500
-+++ osp/osp.c	2022-07-31 18:21:56.704338000 -0500
+--- osp/osp.c	2023-04-26 03:20:28.000000000 -0500
++++ osp/osp.c	2023-05-15 22:35:12.914724000 -0500
 @@ -141,7 +141,9 @@
    if (host && *host == '/')
      {
@@ -8,9 +8,9 @@
        int len;
 +#endif
  
-       connection = g_malloc0 (sizeof (*connection));
-       connection->socket = socket (AF_UNIX, SOCK_STREAM, 0);
-@@ -152,10 +154,15 @@
+       if (strlen (host) >= sizeof (addr.sun_path))
+         {
+@@ -159,10 +161,15 @@
          }
  
        addr.sun_family = AF_UNIX;
@@ -19,7 +19,7 @@
 +      if (connect (connection->socket, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)) == -1)
 +#else
        memset (addr.sun_path, 0, sizeof (addr.sun_path));
-       memcpy (addr.sun_path, host, sizeof (addr.sun_path) - 1);
+       memcpy (addr.sun_path, host, strlen (host));
        len = strlen (addr.sun_path) + sizeof (addr.sun_family);
        if (connect (connection->socket, (struct sockaddr *) &addr, len) == -1)
 +#endif
