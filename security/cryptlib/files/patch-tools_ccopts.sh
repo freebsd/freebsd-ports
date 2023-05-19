31a94c2e0067a73f26a4b039acfe85ab898b37fd
Piotr Kubaj <pkubaj@FreeBSD.org>
2023-05-19 02:45:58 +0200

    security/cryptlib: fix build on powerpc*
    Same as on armv7.

--- security/cryptlib/files/patch-tools_ccopts.sh
+++ security/cryptlib/files/patch-tools_ccopts.sh
@@ -5,7 +5,7 @@
  if [ $ISCLANG -gt 0 ] && [ $ISSPECIAL -eq 0 ] ; then
  	if [ $COMPILER_VER -ge 47 ] ; then
 -		if [ "$OSNAME" = "Darwin" ] || [ "$OSNAME" = "OpenBSD" ] ; then
-+		if [ "$OSNAME" = "Darwin" ] || [ "$OSNAME" = "OpenBSD" ] || [ "$OSNAME" = "FreeBSD" -a "$ARCH" = "arm" ] ; then
++		if [ "$OSNAME" = "Darwin" ] || [ "$OSNAME" = "OpenBSD" ] || [ "$OSNAME" = "FreeBSD" -a "$ARCH" = "arm" ] || [ "$OSNAME" = "FreeBSD" -a "$ARCH" = "powerpc" ]; then
  			# The versions of clang shipped with OS X or OpenBSD don't
  			# support -fsanitize=safe-stack even as late as clang 12, so
  			# there's not much that we can do.
