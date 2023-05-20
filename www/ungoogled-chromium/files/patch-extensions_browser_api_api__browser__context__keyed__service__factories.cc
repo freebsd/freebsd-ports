f832685f2296b170e72c8556a154073875db4a19
Robert Nagy <rnagy@FreeBSD.org>
2023-05-20 08:22:46 +0200

    www/ungoogled-chromium: update to 113.0.5672.126
    Approved by:	rene (mentor, implicit)
    Security:	https://vuxml.freebsd.org/freebsd/bea52545-f4a7-11ed-8290-a8a1599412c6.html
    
    (cherry picked from commit adb115316fe7dc24898bfa1297304018da32824c)

--- www/ungoogled-chromium/files/patch-extensions_browser_api_api__browser__context__keyed__service__factories.cc
+++ www/ungoogled-chromium/files/patch-extensions_browser_api_api__browser__context__keyed__service__factories.cc
@@ -1,6 +1,6 @@
---- extensions/browser/api/api_browser_context_keyed_service_factories.cc.orig	2023-04-08 11:38:38 UTC
+--- extensions/browser/api/api_browser_context_keyed_service_factories.cc.orig	2023-05-20 06:21:07 UTC
 +++ extensions/browser/api/api_browser_context_keyed_service_factories.cc
-@@ -94,7 +94,7 @@ void EnsureApiBrowserContextKeyedServiceFactoriesBuilt
+@@ -96,7 +96,7 @@ void EnsureApiBrowserContextKeyedServiceFactoriesBuilt
    MessageService::GetFactoryInstance();
    MessagingAPIMessageFilter::EnsureAssociatedFactoryBuilt();
  #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN) || \
