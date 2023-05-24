a8179bb67ad193b93e508a19094191f980ad70e8
Yuri Victorovich <yuri@FreeBSD.org>
2023-05-24 01:08:05 -0700

    net/clash: Update 1.12.0 → 1.16.0
    PR:		271597
    Reported by:	Yonas Yanfa <yonas.yanfa@gmail.com>

--- net/clash/files/patch-listener_redir_tcp__freebsd.go
+++ net/clash/files/patch-listener_redir_tcp__freebsd.go
@@ -1,12 +1,22 @@
---- listener/redir/tcp_freebsd.go.orig	2022-05-22 03:30:14 UTC
+--- listener/redir/tcp_freebsd.go.orig	2023-05-24 08:01:04 UTC
 +++ listener/redir/tcp_freebsd.go
-@@ -38,7 +38,8 @@ func parserPacket(conn net.Conn) (socks5.Addr, error) 
- func getorigdst(fd uintptr) (socks5.Addr, error) {
- 	raw := syscall.RawSockaddrInet4{}
- 	siz := uint32(unsafe.Sizeof(raw))
--	if err := socketcall(GETSOCKOPT, fd, syscall.IPPROTO_IP, SO_ORIGINAL_DST, uintptr(unsafe.Pointer(&raw)), uintptr(unsafe.Pointer(&siz)), 0); err != nil {
-+	_, _, err := syscall.Syscall6(syscall.SYS_GETSOCKOPT, fd, syscall.IPPROTO_IP, SO_ORIGINAL_DST, uintptr(unsafe.Pointer(&raw)), uintptr(unsafe.Pointer(&siz)), 0);
+@@ -46,7 +46,8 @@ func parserPacket(conn net.Conn) (socks5.Addr, error) 
+ func getorigdst(fd uintptr) (netip.AddrPort, error) {
+ 	addr := unix.RawSockaddrInet4{}
+ 	size := uint32(unsafe.Sizeof(addr))
+-	if err := socketcall(GETSOCKOPT, fd, syscall.IPPROTO_IP, SO_ORIGINAL_DST, uintptr(unsafe.Pointer(&addr)), uintptr(unsafe.Pointer(&size)), 0); err != nil {
++	 _, _, err := syscall.Syscall6(syscall.SYS_GETSOCKOPT, fd, syscall.IPPROTO_IP, SO_ORIGINAL_DST, uintptr(unsafe.Pointer(&addr)), uintptr(unsafe.Pointer(&size)), 0);
 +	if err != 0 {
- 		return nil, err
+ 		return netip.AddrPort{}, err
  	}
- 
+ 	port := binary.BigEndian.Uint16((*(*[2]byte)(unsafe.Pointer(&addr.Port)))[:])
+@@ -56,7 +57,8 @@ func getorigdst(fd uintptr) (netip.AddrPort, error) {
+ func getorigdst6(fd uintptr) (netip.AddrPort, error) {
+ 	addr := unix.RawSockaddrInet6{}
+ 	size := uint32(unsafe.Sizeof(addr))
+-	if err := socketcall(GETSOCKOPT, fd, syscall.IPPROTO_IPV6, IP6T_SO_ORIGINAL_DST, uintptr(unsafe.Pointer(&addr)), uintptr(unsafe.Pointer(&size)), 0); err != nil {
++	_, _, err := syscall.Syscall6(syscall.SYS_GETSOCKOPT, fd, syscall.IPPROTO_IPV6, IP6T_SO_ORIGINAL_DST, uintptr(unsafe.Pointer(&addr)), uintptr(unsafe.Pointer(&size)), 0);
++	if err != 0 {
+ 		return netip.AddrPort{}, err
+ 	}
+ 	port := binary.BigEndian.Uint16((*(*[2]byte)(unsafe.Pointer(&addr.Port)))[:])
