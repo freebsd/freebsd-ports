6225ae1731854190ed1ed89b49bfc3405bf35f10
Matthias Fechner <mfechner@FreeBSD.org>
2023-05-28 20:02:27 +0300

    www/gitlab-ce: fix problem with upgraded sidekiq version 6.5.9
    from revision 013014feef0787c5168fd727d8d72a0322da7495

--- www/gitlab-ce/files/patch-lib_gitlab_patch_sidekiq__cron__poller.rb
+++ www/gitlab-ce/files/patch-lib_gitlab_patch_sidekiq__cron__poller.rb
@@ -1,11 +1,11 @@
---- lib/gitlab/patch/sidekiq_cron_poller.rb.orig	2023-01-10 20:21:22 UTC
+--- lib/gitlab/patch/sidekiq_cron_poller.rb.orig	2023-05-22 18:44:12 UTC
 +++ lib/gitlab/patch/sidekiq_cron_poller.rb
 @@ -7,7 +7,7 @@
  require 'sidekiq/version'
  require 'sidekiq/cron/version'
  
 -if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.7')
-+if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.8')
++if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.9')
    raise 'New version of sidekiq detected, please remove or update this patch'
  end
  
