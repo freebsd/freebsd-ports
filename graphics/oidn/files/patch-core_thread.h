4b9936fd2f8629487ebd2414e7258c12f2c52bbd
Yuri Victorovich <yuri@FreeBSD.org>
2023-05-29 17:17:03 -0700

    graphics/oidn: Unbreak build on 12
    Submitted by:	Tatsuki Makino <tatsuki_makino@hotmail.com>

--- graphics/oidn/files/patch-core_thread.h
+++ graphics/oidn/files/patch-core_thread.h
@@ -1,6 +1,18 @@
 --- core/thread.h.orig	2023-05-24 17:03:47 UTC
 +++ core/thread.h
-@@ -130,7 +130,7 @@ OIDN_NAMESPACE_BEGIN
+@@ -8,6 +8,11 @@
+ #if !defined(_WIN32)
+   #include <pthread.h>
+   #include <sched.h>
++  #if __FreeBSD__ <= 12
++    #include <sys/cpuset.h>
++    struct _cpuset;
++    typedef struct _cpuset cpu_set_t;
++  #endif
+   #if defined(__APPLE__)
+     #include <mach/thread_policy.h>
+   #endif
+@@ -130,7 +135,7 @@ OIDN_NAMESPACE_BEGIN
      std::vector<GROUP_AFFINITY> oldAffinities; // original thread affinities
    };
  
@@ -9,7 +21,7 @@
  
    // -----------------------------------------------------------------------------------------------
    // ThreadAffinity: Linux
-@@ -153,8 +153,8 @@ OIDN_NAMESPACE_BEGIN
+@@ -153,8 +158,8 @@ OIDN_NAMESPACE_BEGIN
      void restore(int threadIndex);
  
    private:
