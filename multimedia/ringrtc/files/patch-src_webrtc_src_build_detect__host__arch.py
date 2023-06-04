3ae9f711af36d1810517da668bfd0edd47e99b7e
Mikael Urankar <mikael@FreeBSD.org>
2023-06-04 12:18:49 +0200

    multimedia/ringrtc: Update to 2.27.0
    Changes:
      https://github.com/signalapp/ringrtc/releases/tag/v2.27.0

--- multimedia/ringrtc/files/patch-src_webrtc_src_build_detect__host__arch.py
+++ multimedia/ringrtc/files/patch-src_webrtc_src_build_detect__host__arch.py
@@ -1,6 +1,6 @@
 --- ../../src/webrtc/src/build/detect_host_arch.py.orig	2019-07-24 18:58:02 UTC
 +++ ../../src/webrtc/src/build/detect_host_arch.py
-@@ -21,6 +21,8 @@ def HostArch():
+@@ -20,6 +20,8 @@ def HostArch():
      host_arch = 'ia32'
    elif host_arch in ['x86_64', 'amd64']:
      host_arch = 'x64'
--- multimedia/ringrtc/files/patch-src_webrtc_src_build_linux_unbundle_replace__gn__files.py
+++ multimedia/ringrtc/files/patch-src_webrtc_src_build_linux_unbundle_replace__gn__files.py
@@ -1,7 +1,7 @@
 --- ../../src/webrtc/src/build/linux/unbundle/replace_gn_files.py.orig	2021-11-15 18:52:18 UTC
 +++ ../../src/webrtc/src/build/linux/unbundle/replace_gn_files.py
-@@ -25,6 +25,7 @@ REPLACEMENTS = {
-   'libevent': 'base/third_party/libevent/BUILD.gn',
+@@ -54,6 +54,7 @@ REPLACEMENTS = {
+   'libevent': 'third_party/libevent/BUILD.gn',
    'libjpeg': 'third_party/libjpeg.gni',
    'libpng': 'third_party/libpng/BUILD.gn',
 +  'libusb': 'third_party/libusb/BUILD.gn',
