a5c66eb5d4dbc58e8403eb4db1d73d01d90e15aa
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-06-06 02:40:03 +0800

    print/py-fontbakery: Update to 0.8.12
    Changes:	https://github.com/googlefonts/fontbakery/releases

--- print/py-fontbakery/files/patch-setup.py
+++ print/py-fontbakery/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2023-03-03 17:20:04 UTC
+--- setup.py.orig	2023-06-01 00:42:56 UTC
 +++ setup.py
 @@ -23,7 +23,7 @@ except IOError:
  
@@ -9,16 +9,7 @@
      url='https://github.com/googlefonts/fontbakery/',
      description='Well designed Font QA tool, written in Python 3',
      long_description=readme,
-@@ -66,7 +66,7 @@ setup(
-         'beautifulsoup4',
-         'beziers>=0.5.0', # Uses new fontTools glyph outline access
-         'cmarkgfm',
--        'collidoscope==0.4.1', # 0.4.0 had a bug that failed to detect
-+        'collidoscope>=0.5.2', # 0.4.0 had a bug that failed to detect
-                                # an ïï collision on Nunito Black.
-                                # (see https://github.com/googlefonts/fontbakery/issues/3554)
-         'defcon',
-@@ -84,7 +84,7 @@ setup(
+@@ -83,7 +83,7 @@ setup(
          'pip-api',    # needed for checking Font Bakery's version
          # 3.7.0 fixed a bug on parsing some METADATA.pb files.
          # We cannot use v4 because our protobuf files have been compiled with v3.
