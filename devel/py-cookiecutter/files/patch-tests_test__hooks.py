126112cfe799f8a1597b5a2e2cdfbdc5af9895fb
Matthew Seaman <matthew@FreeBSD.org>
2023-06-08 15:21:41 +0100

    devel/py-cookiecutter: switch to USE_PYTHON=pytest
    Use the standard pytest setup.  Disable one windows specific test
    
    While here, allow py311 flavor.  Upstream has not specified 3.11 as
    supported, but there hasn't been any significant development on this
    module since before python-3.11 was released, and the code runs just
    fine.  This means that a py311 flavour of molecule should now be
    available.
    
    No portrevision bump, as this just adds a new flavour; existing
    flavours are unchanged.

--- devel/py-cookiecutter/files/patch-tests_test__hooks.py
+++ devel/py-cookiecutter/files/patch-tests_test__hooks.py
@@ -1,6 +1,6 @@
---- tests/test_hooks.py.orig	2020-12-28 09:57:55 UTC
+--- tests/test_hooks.py.orig	2022-06-01 17:00:44 UTC
 +++ tests/test_hooks.py
-@@ -42,7 +42,7 @@ def make_test_repo(name):
+@@ -41,7 +41,7 @@ def make_test_repo(name, multiple_hooks=False):
          post = 'post_gen_project.sh'
          filename = os.path.join(hook_dir, post)
          with open(filename, 'w') as f:
@@ -9,7 +9,16 @@
              f.write("\n")
              f.write("echo 'post generation hook';\n")
              f.write("touch 'shell_post.txt'\n")
-@@ -143,7 +143,7 @@ class TestExternalHooks(object):
+@@ -61,7 +61,7 @@ def make_test_repo(name, multiple_hooks=False):
+             pre = 'pre_gen_project.sh'
+             filename = os.path.join(hook_dir, pre)
+             with open(filename, 'w') as f:
+-                f.write("#!/bin/bash\n")
++                f.write("#!/bin/sh\n")
+                 f.write("\n")
+                 f.write("echo 'post generation hook';\n")
+                 f.write("touch 'shell_pre.txt'\n")
+@@ -189,7 +189,7 @@ class TestExternalHooks:
                  f.write("echo. >{{cookiecutter.file}}\n")
          else:
              with open(hook_path, 'w') as fh:
