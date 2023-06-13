aaa80b4f53b0fc7b0ae7cceeb765c7c2f260abb4
Wen Heping <wen@FreeBSD.org>
2023-06-13 12:07:57 +0800

    lang/julia: Update to 1.9.1
    PR:		271363
    Reported by:	wen@
    Approved by:	rizor.reardon@yahoo.com(maintainer)

--- lang/julia/files/patch-stdlib_LibGit2__jll_src_LibGit2__jll.jl
+++ lang/julia/files/patch-stdlib_LibGit2__jll_src_LibGit2__jll.jl
@@ -1,10 +1,10 @@
---- stdlib/LibGit2_jll/src/LibGit2_jll.jl.orig	2022-11-21 00:23:36 UTC
+--- stdlib/LibGit2_jll/src/LibGit2_jll.jl.orig	2023-05-09 10:56:52 UTC
 +++ stdlib/LibGit2_jll/src/LibGit2_jll.jl
 @@ -23,7 +23,7 @@ else
  elseif Sys.isapple()
-     const libgit2 = "@rpath/libgit2.1.3.dylib"
+     const libgit2 = "@rpath/libgit2.1.5.dylib"
  else
--    const libgit2 = "libgit2.so.1.3"
+-    const libgit2 = "libgit2.so.1.5"
 +    const libgit2 = "libgit2.so"
  end
  
