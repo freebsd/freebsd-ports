6298555d05162375ed42572626037b35f1dcbb07
Yuri Victorovich <yuri@FreeBSD.org>
2023-06-13 20:35:22 -0700

    science/py-dwave-system: Update 1.18.0 → 1.20.0

---
+++ science/py-dwave-system/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-06-14 03:20:31 UTC
++++ setup.py
+@@ -29,7 +29,7 @@ install_requires = ['dimod>=0.12.7,<0.14.0',
+                     'dwave-cloud-client>=0.9.1,<0.11.0',
+                     'dwave-networkx>=0.8.10',
+                     'dwave-preprocessing>=0.5.0',
+-                    'networkx>=2.0,<3.0',
++                    'networkx>=2.0',
+                     'homebase>=1.0.0,<2.0.0',
+                     'minorminer>=0.2.8,<0.3.0',
+                     'numpy>=1.20.0',
