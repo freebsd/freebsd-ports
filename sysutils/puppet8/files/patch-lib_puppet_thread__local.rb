dddf7ca1a971857dfe4173d7e3e0fbf4c2fb55af
Romain Tartière <romain@FreeBSD.org>
2023-06-16 18:20:44 -1000

    sysutils/puppet8: Update to 8.1.0
    Release notes:
    https://www.puppet.com/docs/puppet/8/release_notes_puppet.html#release_notes_puppet_x-8-1-0
    
    With hat:	puppet

--- sysutils/puppet8/files/patch-lib_puppet_thread__local.rb
+++
@@ -1,7 +0,0 @@
---- lib/puppet/thread_local.rb.orig	2023-04-08 20:42:06 UTC
-+++ lib/puppet/thread_local.rb
-@@ -1,3 +1,4 @@
-+gem 'concurrent-ruby', '< 1.2.0'
- require 'concurrent'
- 
- # We want to use the pure Ruby implementation even on JRuby. If we use the Java
