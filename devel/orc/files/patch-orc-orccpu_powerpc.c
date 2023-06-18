e8927d19c7e8a6fa3e12573f6ff0ff1d16050e22
Daniel Engberg <diizzy@FreeBSD.org>
2023-06-18 09:43:26 +0200

    devel/orc: Update to 0.4.34
    Changelog:
    https://gitlab.freedesktop.org/gstreamer/orc/-/blob/0.4.34/RELEASE
    
    PR:		271792
    Reviewed by:	bsdimp (partially)
    Approved by:	kwm (maintainer timeout, 2+ weeks)
    Sponsored by:	Blinkinblox

--- devel/orc/files/patch-orc-orccpu_powerpc.c
+++ devel/orc/files/patch-orc-orccpu_powerpc.c
@@ -1,9 +1,9 @@
---- orc/orccpu-powerpc.c.orig	2019-09-09 07:21:43 UTC
+--- orc/orccpu-powerpc.c.orig	2023-05-31 21:29:36 UTC
 +++ orc/orccpu-powerpc.c
 @@ -55,6 +55,7 @@
  #endif
  
- #if defined(__FreeBSD__) || defined(__APPLE__)
+ #if defined(__FreeBSD__) || defined(__APPLE__) || defined(__NetBSD__)
 +#include <stdio.h>
  #include <sys/types.h>
  #include <sys/sysctl.h>
