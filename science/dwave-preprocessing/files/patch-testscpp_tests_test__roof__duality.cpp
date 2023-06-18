fb1a3b688a473e392317c80915fee5f01e3d392e
Yuri Victorovich <yuri@FreeBSD.org>
2023-06-18 11:52:44 -0700

    science/dwave-preprocessing: Update 0.4.0 → 0.5.4
    Reported by:	portscout

--- science/dwave-preprocessing/files/patch-testscpp_tests_test__roof__duality.cpp
+++ science/dwave-preprocessing/files/patch-testscpp_tests_test__roof__duality.cpp
@@ -1,10 +1,10 @@
---- testscpp/tests/test_roof_duality.cpp.orig	2022-08-28 01:26:12 UTC
+--- testscpp/tests/test_roof_duality.cpp.orig	2022-12-19 19:58:20 UTC
 +++ testscpp/tests/test_roof_duality.cpp
 @@ -12,7 +12,7 @@
  // See the License for the specific language governing permissions and
  // limitations under the License.
  
--#include "../Catch2/single_include/catch2/catch.hpp"
+-#include "catch2/catch.hpp"
 +#include <catch2/catch.hpp>
  #include <dimod/quadratic_model.h>
  
