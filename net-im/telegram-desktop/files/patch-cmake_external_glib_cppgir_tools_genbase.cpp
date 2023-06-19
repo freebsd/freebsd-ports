1e0f6a02b5b8b59d3e130d22c2a2224bf5907871
Sergey A. Osokin <osa@FreeBSD.org>
2023-06-18 21:13:41 -0400

    net-im/telegram-desktop: update to 4.8.4

---
+++ net-im/telegram-desktop/files/patch-cmake_external_glib_cppgir_tools_genbase.cpp
@@ -0,0 +1,8 @@
+--- cmake/external/glib/cppgir/tools/genbase.cpp.orig	2023-06-15 00:42:39 UTC
++++ cmake/external/glib/cppgir/tools/genbase.cpp
+@@ -1,3 +1,5 @@
++#include <map>
++
+ #include "genbase.hpp"
+ 
+ static std::string
