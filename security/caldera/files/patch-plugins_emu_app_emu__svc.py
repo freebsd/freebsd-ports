7c963b3e3e45808196aa24de0b1db4afef283a6a
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2023-06-21 11:48:22 -0500

    security/caldera: Update to 4.2.0
    ChangeLog at:	https://github.com/mitre/caldera/releases/tag/4.2.0

--- security/caldera/files/patch-plugins_emu_app_emu__svc.py
+++ security/caldera/files/patch-plugins_emu_app_emu__svc.py
@@ -1,11 +1,11 @@
 --- plugins/emu/app/emu_svc.py.orig	2022-06-12 20:12:01 UTC
 +++ plugins/emu/app/emu_svc.py
-@@ -12,7 +12,7 @@ from app.utility.base_service import BaseService
+@@ -12,7 +12,7 @@
  
  
  class EmuService(BaseService):
 -    _dynamicically_compiled_payloads = {'sandcat.go-linux', 'sandcat.go-darwin', 'sandcat.go-windows'}
 +    _dynamicically_compiled_payloads = {'sandcat.go-linux', 'sandcat.go-darwin', 'sandcat.go-windows', 'sandcat.go-freebsd'}
+     _emu_config_path = "conf/default.yml"
  
      def __init__(self):
-         self.log = self.add_service('emu_svc', self)
