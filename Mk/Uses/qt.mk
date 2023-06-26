0222c44e03e178bf17380fdeaa9c85c52b334afc
Loïc Bartoletti <lbartoletti@FreeBSD.org>
2023-06-26 20:58:15 +0200

    Qt6: Update to 6.5.1
    Changes:
      - https://www.qt.io/blog/qt-6.5-lts-released
      - https://www.qt.io/blog/qt-6.5.1-released
    
    PR:	272422
    Exp-run by:	antoine

--- Mk/Uses/qt.mk
+++ Mk/Uses/qt.mk
@@ -23,7 +23,8 @@ _QT_MK_INCLUDED=	qt.mk
 # Qt versions currently supported by the framework.
 _QT_SUPPORTED?=		5 6
 QT5_VERSION?=		5.15.8
-QT6_VERSION?=		6.5.0
+QT6_VERSION?=		6.5.1
+PYSIDE6_VERSION?=	${QT6_VERSION}.1
 
 # We accept the Qt version to be passed by either or all of the three mk files.
 .  if empty(qt_ARGS) && empty(qmake_ARGS) && empty(qt-dist_ARGS)
