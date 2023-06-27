ed489937c40461862bb8c7d096be234b49e38e8c
Cy Schubert <cy@FreeBSD.org>
2023-06-26 21:54:11 -0700

    security/heimdal-devel: Update to the latest Heimdal github commit

--- security/heimdal-devel/files/patch-cf-make-proto.pl
+++ security/heimdal-devel/files/patch-cf-make-proto.pl
@@ -1,11 +1,11 @@
---- cf/make-proto.pl.orig	2019-06-07 06:21:35 UTC
-+++ cf/make-proto.pl
+--- cf/make-proto.pl.orig	2023-06-24 12:54:02.000000000 -0700
++++ cf/make-proto.pl	2023-06-26 21:14:16.008839000 -0700
 @@ -4,7 +4,7 @@
  use Getopt::Std;
  use File::Compare;
  
--use JSON;
-+# use JSON;
+-use JSON::PP
++# use JSON::PP
  
  my $comment = 0;
  my $doxygen = 0;
