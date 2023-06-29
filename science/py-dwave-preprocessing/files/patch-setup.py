9c2d1c3cda7f6b63139016b5206a9d551b744b0b
Yuri Victorovich <yuri@FreeBSD.org>
2023-06-29 08:44:43 -0700

    science/py-dwave-preprocessing: Update 0.5.4 → 0.6.0
    Reported by:	portscout

--- science/py-dwave-preprocessing/files/patch-setup.py
+++ science/py-dwave-preprocessing/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2022-12-25 07:50:44 UTC
+--- setup.py.orig	2023-06-29 15:35:53 UTC
 +++ setup.py
-@@ -26,6 +26,7 @@ extra_compile_args = {
-     'msvc': ['/std:c++17', '/EHsc'],
-     'unix': [
-         '-std=c++17', 
-+        '-DSPDLOG_FMT_EXTERNAL=1', 
-        ],
- }
- 
+@@ -63,7 +63,6 @@ setup(
+     include_dirs=[
+         numpy.get_include(),
+         dimod.get_include(),
+-        'extern/spdlog/include/',
+         ],
+     install_requires=[
+         'numpy>=1.17.3,<2.0.0',  # this is the oldest supported NumPy on Python 3.8
