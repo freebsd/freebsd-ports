211b01d0758aa5f8597db757992022ee6c77ff20
Stefan Eßer <se@FreeBSD.org>
2023-06-30 13:38:50 +0200

    devel/delta: fetch from GitHub
    Make port more compliant with style, fetch from GitHub instead of a
    local copy, respect port system CFLAGS, support installation of README
    files depending on DOCS, support testing of the compiled port, take
    maintainership from ports@.
    
    The installed programs are identical to those of the previous port
    version, except for CFLAGS from the ports system that are no longer
    ignored.

--- devel/delta/files/patch-Build.mk
+++ devel/delta/files/patch-Build.mk
@@ -1,9 +1,12 @@
---- ./Build.mk.orig	2013-09-19 09:41:07.282741683 -0400
-+++ ./Build.mk	2013-09-19 09:41:14.007747039 -0400
-@@ -1,6 +1,5 @@
+--- Build.mk.orig	2020-06-23 01:34:49 UTC
++++ Build.mk
+@@ -1,7 +1,7 @@
  # see License.txt for copyright and terms of use
  
 -CC := gcc
- CFLAGS := -Wall
+-CFLAGS := -Wall
++# CC := gcc
++# CFLAGS := -Wall
  # CFLAGS += -g
  FLEX := flex
+ 
