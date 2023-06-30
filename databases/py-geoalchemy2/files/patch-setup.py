5286a7c3ac1c8a5976dc5dd2eeabc923d79b1e78
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-06-30 15:03:00 +0800

    databases/py-geoalchemy2: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ databases/py-geoalchemy2/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-05-18 09:18:05 UTC
++++ setup.py
+@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages
+ from setuptools import setup
+ 
+ setup(
+-    name="GeoAlchemy2",
++    name="geoalchemy2",
+     use_scm_version=True,
+     description="Using SQLAlchemy with Spatial Databases",
+     long_description=open("README.rst", encoding="utf-8").read(),
