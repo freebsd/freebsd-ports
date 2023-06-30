2bcd791dc7c6338329aa9186e813b52781fe68fe
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-06-30 07:19:03 +0200

    */*: Cleanup pointers to EOL mariadb103
    Although mariadb103 is EOL there were some pointers to this version, so
    cleanup the necessaries.
    
    Approved by:    portmgr (blanket)

--- Mk/Uses/mysql.mk
+++ Mk/Uses/mysql.mk
@@ -67,7 +67,7 @@ DEFAULT_MYSQL_VER?=	${MYSQL_DEFAULT:S/.//}
 MYSQL57_LIBVER=		20
 MYSQL57p_LIBVER=	20
 MYSQL80_LIBVER=		21
-.  for v in 3 4 5 6 11
+.  for v in 4 5 6 11
 MYSQL10${v}m_LIBVER=	3
 .  endfor
 
@@ -129,7 +129,7 @@ _MYSQL_SHLIB=	libmysqlclient
 .  if (${MYSQL_VER:C/[0-9]*//} == "m")
 _MYSQL_SERVER_FLAVOUR=	mariadb
 _MYSQL_CLIENT_FLAVOUR=	mariadb
-.    if ${MYSQL_VER:C/m//} >= 103
+.    if ${MYSQL_VER:C/m//} >= 104
 _MYSQL_SHLIB=	libmariadb
 .    endif
 .  elif (${MYSQL_VER:C/[0-9]*//} == "p")
