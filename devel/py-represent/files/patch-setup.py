f51bf171c0e7c1a40a56b0b2f52a860cff306021
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-06-30 15:03:58 +0800

    devel/py-represent: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-represent/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-12-22 00:50:20 UTC
++++ setup.py
+@@ -31,7 +31,7 @@ extras_require = {
+ 
+ 
+ setup(
+-    name='Represent',
++    name='represent',
+     version=VERSION,
+     description=DESCRIPTION,
+     long_description=open('README.md').read(),
