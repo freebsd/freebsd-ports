51bdb2a78ecf30451d842c1bafbb886dbdf2e1ec
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-06-30 15:08:31 +0800

    devel/py-zope.testbrowser: Fix build with newer setuptools
    Approved by:	portmgr (blanket)

---
+++ devel/py-zope.testbrowser/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2019-11-12 09:52:53 UTC
++++ setup.py
+@@ -64,7 +64,7 @@ setup(
+         'zope.interface',
+         'zope.schema',
+         'zope.cachedescriptors',
+-        'pytz > dev',
++        'pytz',
+         'WebTest >= 2.0.30',
+         'BeautifulSoup4',
+         'SoupSieve >= 1.9.0',
