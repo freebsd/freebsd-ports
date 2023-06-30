e18d862b9d04efd14684af43da888c8df4c87ef9
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-06-30 15:05:00 +0800

    textproc/py-markdown: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ textproc/py-markdown/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2022-05-05 19:08:30 UTC
++++ setup.py
+@@ -56,7 +56,7 @@ with open('README.md') as f:
+     long_description = f.read()
+ 
+ setup(
+-    name='Markdown',
++    name='markdown',
+     version=__version__,
+     url='https://Python-Markdown.github.io/',
+     project_urls={
