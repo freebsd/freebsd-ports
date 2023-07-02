738d7427d67024949b1541cf2968f16aa3b6f384
Matthias Andree <mandree@FreeBSD.org>
2023-07-02 16:48:40 +0200

    graphics/photivo: make compatible with exiv2 0.28
    PR:		272311

---
+++ graphics/photivo/files/patch-Sources_ptMain.cpp
@@ -0,0 +1,11 @@
+--- Sources/ptMain.cpp.orig	2016-03-22 03:34:55 UTC
++++ Sources/ptMain.cpp
+@@ -5325,7 +5325,7 @@ void ReadSidecar(const QString& Sidecar)
+     return;
+   }
+ 
+-  Exiv2::Image::AutoPtr hImage = Exiv2::ImageFactory::open(Sidecar.toLocal8Bit().data());
++  Exiv2::Image::UniquePtr hImage = Exiv2::ImageFactory::open(Sidecar.toLocal8Bit().data());
+ 
+   if (!hImage.get()) {
+     return;
