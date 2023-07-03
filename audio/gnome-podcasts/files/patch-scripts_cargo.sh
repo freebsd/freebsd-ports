ba2d959246172c6b90b9a4e28f708a1950b19a3e
Jan Beich <jbeich@FreeBSD.org>
2023-07-03 23:02:22 +0000

    audio/gnome-podcasts: update to 0.6.0
    Changes:	https://gitlab.gnome.org/World/podcasts/-/releases/0.6.0
    Reported by:	Repology

--- audio/gnome-podcasts/files/patch-scripts_cargo.sh
+++
@@ -1,8 +0,0 @@
---- scripts/cargo.sh.orig	2021-12-04 18:15:44 UTC
-+++ scripts/cargo.sh
-@@ -24,4 +24,4 @@ if test -d vendor; then
- fi
- 
- cargo build ${ARGS[@]} --manifest-path="$MESON_SOURCE_ROOT"/Cargo.toml -p podcasts-gtk
--cp "$CARGO_TARGET_DIR"/${TARGET}/podcasts-gtk "$OUTPUT"
-+cp "$CARGO_TARGET_DIR"/${CARGO_BUILD_TARGET}/${TARGET}/podcasts-gtk "$OUTPUT"
