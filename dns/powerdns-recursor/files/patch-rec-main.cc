bbf3f2110b24b17eb4531cf9a0e8522a68850a97
Ralf van der Enden <tremere@cainites.net>
2023-07-04 08:19:42 +0200

    dns/powerdns-recursor: update to 4.9.0
    ChangeLog: https://blog.powerdns.com/2023/06/30/powerdns-recursor-4-9-0-released
    
     * The performance impact of metrics collection has been reduced by using
       lock-free non-atomic thread-local counters.
     * The packet cache is sharded and shared by all threads.
     * The TTL of negative answers in the packet cache can now be controlled
       separately from positive and failure answers.
     * The rec_control trace_regex command writes the generated trace information to
       a specified file instead of the general log. The trace information contains
       more precise timestamps and DNSSEC validation information.
     * If extended-resolution-errors is enabled EDNS errors are now generated in
       more cases, specifically when authoritative servers for a zone are
       unreachable or when synthesising answers by e.g. using the aggressive NSEC
       cache.
     * The aggressive NSEC cache has been changed not to store NSEC3 entries which
       cover only a small fraction of possible names. This also allows switching off
       the aggressive cache for NSEC3 only.
     * It is now possible to switch off root-refreshing completely.
     * Proper handling of security policies that restrict the use of specific DNSSEC
       algorithms on RHEL9 derived systems.
    
    PR:		272343
    Reported by:	tremere@cainites.net (maintainer)

--- dns/powerdns-recursor/files/patch-rec-main.cc
+++ dns/powerdns-recursor/files/patch-rec-main.cc
@@ -1,18 +1,18 @@
---- rec-main.cc.orig	2022-12-09 10:46:42 UTC
+--- rec-main.cc.orig	2023-06-30 06:58:36 UTC
 +++ rec-main.cc
-@@ -2609,13 +2609,13 @@ int main(int argc, char** argv)
-                 SYSTEMD_SETID_MSG
+@@ -2829,13 +2829,13 @@ static void initArgs()
+               SYSTEMD_SETID_MSG
  #endif
-                 )
--      = "";
-+      = "pdns";
-     ::arg().set("setuid", "If set, change user id to this uid for more security"
+               )
+-    = "";
++    = "pdns";
+   ::arg().set("setuid", "If set, change user id to this uid for more security"
  #ifdef HAVE_SYSTEMD
-                 SYSTEMD_SETID_MSG
+               SYSTEMD_SETID_MSG
  #endif
-                 )
--      = "";
-+      = "pdns_recursor";
-     ::arg().set("network-timeout", "Wait this number of milliseconds for network i/o") = "1500";
-     ::arg().set("threads", "Launch this number of threads") = "2";
-     ::arg().set("distributor-threads", "Launch this number of distributor threads, distributing queries to other threads") = "0";
+               )
+-    = "";
++    = "pdns_recursor";
+   ::arg().set("network-timeout", "Wait this number of milliseconds for network i/o") = "1500";
+   ::arg().set("threads", "Launch this number of threads") = "2";
+   ::arg().set("distributor-threads", "Launch this number of distributor threads, distributing queries to other threads") = "0";
