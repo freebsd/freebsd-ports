4de68f18b8bc43534a30b4a1a2cd35457b3a7476
Matthew Seaman <matthew@FreeBSD.org>
2023-07-08 07:27:34 +0100

    devel/py-cookiecutter: update to 2.2.0
    Changes:	https://github.com/cookiecutter/cookiecutter/blob/2.2.0/HISTORY.md
    Reported by:	portscout, repology

--- devel/py-cookiecutter/files/patch-tests_test__hooks.py
+++ devel/py-cookiecutter/files/patch-tests_test__hooks.py
@@ -1,9 +1,9 @@
---- tests/test_hooks.py.orig	2022-06-01 17:00:44 UTC
+--- tests/test_hooks.py.orig	2023-06-13 08:07:06 UTC
 +++ tests/test_hooks.py
 @@ -41,7 +41,7 @@ def make_test_repo(name, multiple_hooks=False):
          post = 'post_gen_project.sh'
          filename = os.path.join(hook_dir, post)
-         with open(filename, 'w') as f:
+         with Path(filename).open('w') as f:
 -            f.write("#!/bin/bash\n")
 +            f.write("#!/bin/sh\n")
              f.write("\n")
@@ -12,7 +12,7 @@
 @@ -61,7 +61,7 @@ def make_test_repo(name, multiple_hooks=False):
              pre = 'pre_gen_project.sh'
              filename = os.path.join(hook_dir, pre)
-             with open(filename, 'w') as f:
+             with Path(filename).open('w') as f:
 -                f.write("#!/bin/bash\n")
 +                f.write("#!/bin/sh\n")
                  f.write("\n")
@@ -21,7 +21,7 @@
 @@ -189,7 +189,7 @@ class TestExternalHooks:
                  f.write("echo. >{{cookiecutter.file}}\n")
          else:
-             with open(hook_path, 'w') as fh:
+             with Path(hook_path).open('w') as fh:
 -                fh.write("#!/bin/bash\n")
 +                fh.write("#!/bin/sh\n")
                  fh.write("\n")
