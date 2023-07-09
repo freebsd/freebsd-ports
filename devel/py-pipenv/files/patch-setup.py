1bb44cd3d55812fe96c018be03c7c6b0a9218159
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-07-09 18:19:11 +0800

    devel/py-pipenv: Update to 2023.6.26
    Changes:	https://github.com/pypa/pipenv/releases

---
+++ devel/py-pipenv/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-06-26 22:28:12 UTC
++++ setup.py
+@@ -21,7 +21,7 @@ if sys.argv[-1] == "publish":
+ 
+ required = [
+     "certifi",
+-    "setuptools>=67.0.0",
++    "setuptools>=61.0.0",
+     "virtualenv-clone>=0.2.5",
+     "virtualenv>=20.17.1",
+ ]
