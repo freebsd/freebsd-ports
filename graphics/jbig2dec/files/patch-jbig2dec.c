aeb01f3ee1e1a0c1cdee67e1c9b0c915561e6855
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-07-09 18:17:11 +0800

    graphics/jbig2dec: Use USES=pathfix to fix .pc installation
    - Update MASTER_SITES
    - Convert REINPLACE_CMD to patch files

--- graphics/jbig2dec/files/patch-jbig2dec.c
+++ graphics/jbig2dec/files/patch-jbig2dec.c
@@ -1,4 +1,4 @@
---- jbig2dec.c.orig	2020-02-11 16:28:00 UTC
+--- jbig2dec.c.orig	2020-09-10 15:14:14 UTC
 +++ jbig2dec.c
 @@ -33,7 +33,10 @@
  #endif
@@ -12,3 +12,12 @@
  
  #ifdef JBIG_EXTERNAL_MEMENTO_H
  #include JBIG_EXTERNAL_MEMENTO_H
+@@ -217,7 +220,7 @@ hash_print(jbig2dec_params_t *params, FILE *out)
+     char digest[2 * SHA1_DIGEST_SIZE + 1];
+     int i;
+ 
+-    SHA1_Final(params->hash_ctx, md);
++    SHA1_Final(md, params->hash_ctx);
+     for (i = 0; i < SHA1_DIGEST_SIZE; i++) {
+         snprintf(&(digest[2 * i]), 3, "%02x", md[i]);
+     }
