c8b47f91bca080d5a8212a8608f1b36d99de1893
Nick Hilliard <nick@foobar.org>
2023-07-11 10:36:18 +0200

    net-mgmt/rubygem-oxidized-web: update to support puma6
    The recent update from rubygem-puma5 to rubygem-puma6 broke
    rubygem-oxidized-web. This patch fixes it (+ has been submitted upstream).
    
    PR:		272441

--- net-mgmt/rubygem-oxidized-web/files/patch-lib_oxidized_web.rb
+++ net-mgmt/rubygem-oxidized-web/files/patch-lib_oxidized_web.rb
@@ -1,6 +1,17 @@
---- lib/oxidized/web.rb.orig	2022-04-22 14:40:24 UTC
-+++ lib/oxidized/web.rb
-@@ -26,7 +26,7 @@ module Oxidized
+--- lib/oxidized/web.rb.orig	2023-07-09 23:03:50.799687000 +0100
++++ lib/oxidized/web.rb	2023-07-09 23:04:50.410274000 +0100
+@@ -3,9 +3,8 @@
+ module Oxidized
+   module API
+     class Web
+-      require 'rack/handler'
++      require 'rack/handler/puma'
+       attr_reader :thread
+-      Rack::Handler::WEBrick = Rack::Handler.get(:puma)
+       def initialize nodes, listen
+         require 'oxidized/web/webapp'
+         listen, uri = listen.split '/'
+@@ -26,7 +25,7 @@
  
        def run
          @thread = Thread.new do
