df9b162e1d1f8030c02b32fae9359a4d0ee014d2
Yuri Victorovich <yuri@FreeBSD.org>
2023-07-11 21:14:01 -0700

    science/dwave-preprocessing: Update 0.5.4 → 0.6.2
    Reported by:	portscout

--- science/dwave-preprocessing/files/patch-testscpp_tests_test__roof__duality.cpp
+++ science/dwave-preprocessing/files/patch-testscpp_tests_test__roof__duality.cpp
@@ -1,11 +1,11 @@
---- testscpp/tests/test_roof_duality.cpp.orig	2022-12-19 19:58:20 UTC
+--- testscpp/tests/test_roof_duality.cpp.orig	2023-07-06 22:19:16 UTC
 +++ testscpp/tests/test_roof_duality.cpp
-@@ -12,7 +12,7 @@
- // See the License for the specific language governing permissions and
- // limitations under the License.
+@@ -14,7 +14,7 @@
+ 
+ #include <dimod/quadratic_model.h>
  
 -#include "catch2/catch.hpp"
 +#include <catch2/catch.hpp>
- #include <dimod/quadratic_model.h>
- 
  #include "dwave-preprocessing/fix_variables.hpp"
+ 
+ namespace fix_variables_ {
