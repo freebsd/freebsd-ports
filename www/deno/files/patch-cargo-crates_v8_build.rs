adda1473bd5320c059aeb80329ec8db9eb51910a
Mikael Urankar <mikael@FreeBSD.org>
2023-07-12 13:30:41 +0200

    www/deno: Update to 1.35.0
    Changes:
      https://github.com/denoland/deno/releases/tag/v1.35.0

--- www/deno/files/patch-cargo-crates_v8_build.rs
+++ www/deno/files/patch-cargo-crates_v8_build.rs
@@ -1,5 +1,5 @@
---- cargo-crates/v8-0.73.0/build.rs.orig	2020-07-22 09:53:02 UTC
-+++ cargo-crates/v8-0.73.0/build.rs
+--- cargo-crates/v8-0.74.1/build.rs.orig	2020-07-22 09:53:02 UTC
++++ cargo-crates/v8-0.74.1/build.rs
 @@ -266,10 +266,13 @@ fn platform() -> String {
    let os = "mac";
    #[cfg(target_os = "windows")]
