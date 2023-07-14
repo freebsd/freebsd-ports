7bc5bbbfdd926b8e6f00934abbd881fbd340a99d
Adriaan de Groot <adridg@FreeBSD.org>
2023-07-14 15:10:27 +0200

    audio/musescore: update to 4.1.0, latest release
    All of the audio-backend options have gone away -- I don't see
    MuseScore searching for them, either, and do not feel like digging
    too far into the configure-and-build of this package either.
    It bundles all kinds of stuff, where the most-worstest culprits
    are dealt with in post-stage: why this installs Google Test headers
    I really don't know.

---
+++ audio/musescore/files/patch-src_learn_view_learnpagemodel.h
@@ -0,0 +1,11 @@
+--- src/learn/view/learnpagemodel.h.orig	2023-07-14 10:22:05 UTC
++++ src/learn/view/learnpagemodel.h
+@@ -23,6 +23,8 @@
+ #define MU_LEARN_LEARNPAGEMODEL_H
+ 
+ #include <QObject>
++#include <QVariantMap>
++#include <QString>
+ 
+ #include "async/asyncable.h"
+ 
