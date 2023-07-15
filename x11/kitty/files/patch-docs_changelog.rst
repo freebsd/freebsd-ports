593b9705c8e6519948ee0ab17563662c06c97516
Nicola Vitale <nivit@FreeBSD.org>
2023-07-15 15:59:01 +0200

    x11/kitty: Update to 0.29.0
    - Remove patches merged by upstream
    - Update lib dependencies
    - Pet portfmt/portclippy/portlint
    - Add option DOCS
    - Don't install all files in $PREFIX/lib/kitty, but in the canonical
      directories DATADIR, DESKTOPDIR, etc.
    - Add a couple of patch to fix some links in the documentation.
      Error shown:
    
    fatal: ambiguous argument '8dea5b3': unknown revision or path not in the working tree.
    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'
    fatal: ambiguous argument 'ad1109b': unknown revision or path not in the working tree.
    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'
    fatal: ambiguous argument '889ca77': unknown revision or path not in the working tree.
    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'
    reading sources... [100%] protocol-extensions .. unscroll
    /usr/local/poudriere/ports/default/x11/kitty/work/github.com/kovidgoyal/kitty@v0.29.0/docs/changelog.rst:2940: ERROR: GitHub commit id "8dea5b3" not recognized.
    /usr/local/poudriere/ports/default/x11/kitty/work/github.com/kovidgoyal/kitty@v0.29.0/docs/changelog.rst:3055: ERROR: GitHub commit id "ad1109b" not recognized.
    /usr/local/poudriere/ports/default/x11/kitty/work/github.com/kovidgoyal/kitty@v0.29.0/docs/changelog.rst:3067: ERROR: GitHub commit id "889ca77" not recognized.
    
    Release changes: https://sw.kovidgoyal.net/kitty/changelog/#id1

---
+++ x11/kitty/files/patch-docs_changelog.rst
@@ -0,0 +1,29 @@
+--- docs/changelog.rst.orig	1979-11-29 23:00:00 UTC
++++ docs/changelog.rst
+@@ -2937,7 +2937,7 @@ Detailed list of changes
+ - A :doc:`new kitten <kittens/panel>` to create dock panels on X11 desktops
+   showing the output from arbitrary terminal programs.
+ 
+-- Reduce data sent to the GPU per render by 30% (:commit:`8dea5b3`)
++- Reduce data sent to the GPU per render by 30% (:commit:`8dea5b3e3ec1db8597f3a3649b5cefd52f41e409`)
+ 
+ - Implement changing the font size for individual top level (OS) windows
+   (:iss:`408`)
+@@ -3052,7 +3052,7 @@ Detailed list of changes
+ - Implement support for the SGR *faint* escape code to make text blend
+   into the background (:iss:`446`).
+ 
+-- Make the hints kitten a little smarter (:commit:`ad1109b`)
++- Make the hints kitten a little smarter (:commit:`ad1109b6fe0a6802ca4f77182a7a0b36086b3e9f`)
+   so that URLs that stretch over multiple lines are detected. Also improve
+   detection of surrounding brackets/quotes.
+ 
+@@ -3064,7 +3064,7 @@ Detailed list of changes
+ 
+ - Expand environment variables in config file include directives
+ 
+-- Allow programs running in kitty to read/write from the clipboard (:commit:`889ca77`).
++- Allow programs running in kitty to read/write from the clipboard (:commit:`889ca7791244253cb08fbc3eca8883a87fb943a7`).
+   By default only writing is allowed. This feature is supported in many
+   terminals, search for `OSC 52 clipboard` to find out more about using it.
+ 
--- x11/kitty/files/patch-setup.py
+++ x11/kitty/files/patch-setup.py
@@ -1,11 +1,14 @@
---- setup.py.orig	2023-04-08 12:02:15 UTC
+--- setup.py.orig	2023-07-13 16:10:52 UTC
 +++ setup.py
-@@ -126,25 +126,9 @@ def libcrypto_flags() -> Tuple[List[str], List[str]]:
+@@ -130,27 +130,11 @@ def libcrypto_flags() -> Tuple[List[str], List[str]]:
      # Apple use their special snowflake TLS libraries and additionally
      # have an ancient broken system OpenSSL, so we need to check for one
      # installed by all the various macOS package managers.
 -    extra_pc_dir = ''
--
++    cflags = [f"-I{sys.prefix}/include"]
++    libs = [f"-L{sys.prefix}/lib", "-lcrypto"]
++    return cflags, libs
+ 
 -    try:
 -        cflags = pkg_config('libcrypto', '--cflags-only-I', fatal=False)
 -    except subprocess.CalledProcessError:
@@ -23,9 +26,18 @@
 -            extra_pc_dir = os.pathsep.join(openssl_dirs)
 -        cflags = pkg_config('libcrypto', '--cflags-only-I', extra_pc_dir=extra_pc_dir)
 -    return cflags, pkg_config('libcrypto', '--libs', extra_pc_dir=extra_pc_dir)
-+    cflags = [f"-I{sys.prefix}/include"]
-+    libs = [f"-L{sys.prefix}/lib", "-lcrypto"]
-+    return cflags, libs
  
- 
+-
  def at_least_version(package: str, major: int, minor: int = 0) -> None:
+     q = f'{major}.{minor}'
+     if subprocess.run([PKGCONFIG, package, f'--atleast-version={q}']
+@@ -1163,7 +1147,8 @@ def create_linux_bundle_gunk(ddir: str, libdir_name: s
+         make = 'gmake' if is_freebsd else 'make'
+         run_tool([make, 'docs'])
+     copy_man_pages(ddir)
+-    copy_html_docs(ddir)
++    if os.getenv('BUILD_DOCS') == 'yes':
++        copy_html_docs(ddir)
+     for (icdir, ext) in {'256x256': 'png', 'scalable': 'svg'}.items():
+         icdir = os.path.join(ddir, 'share', 'icons', 'hicolor', icdir, 'apps')
+         safe_makedirs(icdir)
