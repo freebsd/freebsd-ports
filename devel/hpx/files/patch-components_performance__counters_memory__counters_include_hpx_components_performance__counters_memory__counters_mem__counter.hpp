7c29c7c157e4b61091afdf1463972c59afeb6966
Yuri Victorovich <yuri@FreeBSD.org>
2023-07-17 00:08:52 -0700

    devel/hpx: Update 1.8.0 → 1.9.0
    Reported by:	portscout

--- devel/hpx/files/patch-components_performance__counters_memory__counters_include_hpx_components_performance__counters_memory__counters_mem__counter.hpp
+++ devel/hpx/files/patch-components_performance__counters_memory__counters_include_hpx_components_performance__counters_memory__counters_mem__counter.hpp
@@ -1,5 +1,5 @@
---- components/performance_counters/memory/include/hpx/components/performance_counters/memory/mem_counter.hpp.orig	2022-05-24 18:40:32 UTC
-+++ components/performance_counters/memory/include/hpx/components/performance_counters/memory/mem_counter.hpp
+--- components/performance_counters/memory_counters/include/hpx/components/performance_counters/memory_counters/mem_counter.hpp.orig	2023-07-17 06:38:16 UTC
++++ components/performance_counters/memory_counters/include/hpx/components/performance_counters/memory_counters/mem_counter.hpp
 @@ -19,6 +19,7 @@ namespace hpx { namespace performance_counters { names
      std::uint64_t read_psm_resident(bool);
  
