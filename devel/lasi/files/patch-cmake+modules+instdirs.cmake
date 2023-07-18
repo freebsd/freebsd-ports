cfc60481ca907eb5c8b84f5aef6ad0ff9f986ac3
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-07-19 00:19:57 +0200

    devel/lasi: Fix build with llvm16
    - Pet portclippy
    - Fix builds with DOCS option instead of DOXYGEN
    - Adopt port
    
    Sponsored by:	The FreeBSD Foundation

--- devel/lasi/files/patch-cmake+modules+instdirs.cmake
+++ devel/lasi/files/patch-cmake+modules+instdirs.cmake
@@ -1,6 +1,20 @@
 --- cmake/modules/instdirs.cmake.orig	2019-01-31 07:34:50 UTC
 +++ cmake/modules/instdirs.cmake
-@@ -80,6 +80,6 @@ set(docdir ${CMAKE_INSTALL_DATADIR}/doc/libLASi-${VERS
+@@ -13,7 +13,7 @@
+ # install locations).
+ 
+ set(
+-CMAKE_INSTALL_EXEC_PREFIX 
++CMAKE_INSTALL_EXEC_PREFIX
+ ${CMAKE_INSTALL_PREFIX}
+ CACHE PATH "install location for architecture-dependent files"
+ )
+@@ -76,10 +76,10 @@ set(includedir ${CMAKE_INSTALL_INCLUDEDIR}
+ CACHE PATH "install location for headers"
+ )
+ 
+-set(docdir ${CMAKE_INSTALL_DATADIR}/doc/libLASi-${VERSION}
++set(docdir ${CMAKE_INSTALL_PREFIX}/share/doc/lasi
  CACHE PATH "install location for documentation"
  )
  
