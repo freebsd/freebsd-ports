93e702acbc369605aa637ca04bab6177db9fe410
Jan Beich <jbeich@FreeBSD.org>
2023-07-19 15:21:13 +0000

    devel/highway: update to 1.0.5
    Changes:	https://github.com/google/highway/releases/tag/1.0.5
    Reported by:	GitHub (watch releases)

--- graphics/lux/files/patch-vspline_hwy__simd__type.h
+++ graphics/lux/files/patch-vspline_hwy__simd__type.h
@@ -1,5 +1,15 @@
 --- vspline/hwy_simd_type.h.orig	2023-03-30 09:58:06 UTC
 +++ vspline/hwy_simd_type.h
+@@ -104,7 +104,9 @@
+ #include <hwy/contrib/math/math-inl.h>
+ #include <hwy/aligned_allocator.h>
+ #include <hwy/print-inl.h>
++#ifndef HWY_HAVE_ATAN2
+ #include "hwy_atan2.h"
++#endif
+ 
+ // lux uses it's own dispatching mechanism, but this code should also
+ // cooperate with highway's multi-platform strategy
 @@ -694,7 +694,6 @@ PROMOTE(unsigned short,int)
  PROMOTE(unsigned char,int)
  PROMOTE(unsigned char,short)
