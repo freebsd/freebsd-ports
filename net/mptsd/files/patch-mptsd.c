db63cc4d389920c499e8bf413a0e12227cd482f7
Alex Mironov <win2000rus@hotmail.com>
2023-07-20 08:20:26 +0200

    net/mptsd: update to 1.1-27 and fix for FreeBSD 14
    PR:		272603
    Reported by:	win2000rus@hotmail.com (maintainer)

--- net/mptsd/files/patch-mptsd.c
+++ net/mptsd/files/patch-mptsd.c
@@ -1,11 +1,14 @@
---- mptsd.c.orig	2011-09-16 12:37:04 UTC
+--- mptsd.c.orig	2018-05-07 06:42:40 UTC
 +++ mptsd.c
-@@ -34,7 +34,7 @@
- 
+@@ -35,9 +35,9 @@
  #define PROGRAM_NAME "ux-mptsd"
  
+ #ifdef BUILD_ID
 -const char *program_id = PROGRAM_NAME " " GIT_VER " build " BUILD_ID;
-+const char *program_id = PROGRAM_NAME " FreeBSD port";
++const char *program_id = PROGRAM_NAME " " GIT_VER " build " BUILD_ID " FreeBSD port";
+ #else
+-const char *program_id = PROGRAM_NAME " " GIT_VER;
++const char *program_id = PROGRAM_NAME " " GIT_VER " FreeBSD port";
+ #endif
  
  char *server_sig = PROGRAM_NAME;
- char *server_ver = GIT_VER;
