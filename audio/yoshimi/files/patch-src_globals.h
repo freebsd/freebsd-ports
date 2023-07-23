101fd3d99eab34afbbe31bffccdc412dcc231fcb
Yuri Victorovich <yuri@FreeBSD.org>
2023-07-22 19:56:49 -0700

    audio/yoshimi: Update 2.2.2.1 → 2.3.0.2
    Reported by:	portscout

--- audio/yoshimi/files/patch-src_globals.h
+++ audio/yoshimi/files/patch-src_globals.h
@@ -1,14 +1,12 @@
 - workaround for https://github.com/Yoshimi/yoshimi/issues/174
 
---- src/globals.h.orig	2023-02-19 18:22:47 UTC
+--- src/globals.h.orig	2023-05-29 08:38:23 UTC
 +++ src/globals.h
-@@ -126,8 +126,8 @@ typedef unsigned int  uint;
- #define DEFAULT_NAME "Simple Sound"
+@@ -117,7 +117,7 @@ typedef unsigned int  uint;
  #define UNTITLED "No Title"
  
--#define DEFAULT_AUDIO alsa_audio
+ #define DEFAULT_AUDIO jack_audio
 -#define DEFAULT_MIDI alsa_midi
-+#define DEFAULT_AUDIO jack_audio
 +#define DEFAULT_MIDI jack_midi
  
  #define FORCED_EXIT 16
