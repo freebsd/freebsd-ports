3cbdb6722c379fd8885ddb4def996a60862037d1
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2023-07-23 22:22:02 +0200

    net/librdkafka: update to 2.2.0
    Update patches, to re-enable atomic operations in 64bit

--- net/librdkafka/files/patch-src_rd.h
+++ net/librdkafka/files/patch-src_rd.h
@@ -1,8 +1,8 @@
---- src/rd.h.orig	2017-01-25 12:25:17 UTC
+--- src/rd.h.orig	2023-07-12 09:03:32 UTC
 +++ src/rd.h
-@@ -38,6 +38,12 @@
+@@ -43,6 +43,12 @@
  #ifndef _POSIX_C_SOURCE
- #define _POSIX_C_SOURCE 200809L  /* for timespec on solaris */
+ #define _POSIX_C_SOURCE 200809L /* for timespec on solaris */
  #endif
 +#ifndef __XSI_VISIBLE
 +#define __XSI_VISIBLE 700 /* for gettimeofday() */
