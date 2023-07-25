1a9ee421ca0b9b93886ce35808788289e02b2ce7
Thierry Thomas <thierry@FreeBSD.org>
2023-07-25 15:26:45 +0200

    math/py-mathics: upgrade to 6.0.2

--- math/py-mathics/files/patch-setup.py
+++ math/py-mathics/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2023-03-17 10:22:19 UTC
+--- setup.py.orig	2023-07-25 13:04:43 UTC
 +++ setup.py
 @@ -64,7 +64,7 @@ elif sys.version_info[:2] == (3, 6):
          sys.exit(-1)
  else:
      INSTALL_REQUIRES += [
--        "numpy<=1.24",
+-        "numpy<1.25",
 +        "numpy<=1.25",
          "llvmlite",
-         "sympy>=1.8, < 1.12",
+         "sympy>=1.8",
          "pillow >= 9.2",
