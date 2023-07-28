959ab8d38f30cb0e87e153bec93f1d000b3298c8
Hiroki Tagato <tagattie@FreeBSD.org>
2023-07-28 18:16:48 +0900

    devel/electron25: update to 25.3.2
    Changelog: https://github.com/electron/electron/releases/tag/v25.3.2
    
    Reported by:	GitHub (watch releases)

--- devel/electron25/files/patch-electron_shell_browser_native__window.cc
+++ devel/electron25/files/patch-electron_shell_browser_native__window.cc
@@ -1,4 +1,4 @@
---- electron/shell/browser/native_window.cc.orig	2023-07-19 13:20:24 UTC
+--- electron/shell/browser/native_window.cc.orig	2023-07-26 13:27:22 UTC
 +++ electron/shell/browser/native_window.cc
 @@ -197,7 +197,7 @@ void NativeWindow::InitFromOptions(const gin_helper::D
    } else {
@@ -6,6 +6,6 @@
    }
 -#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
 +#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
-   bool resizable;
-   if (options.Get(options::kResizable, &resizable)) {
-     SetResizable(resizable);
+   bool closable;
+   if (options.Get(options::kClosable, &closable)) {
+     SetClosable(closable);
