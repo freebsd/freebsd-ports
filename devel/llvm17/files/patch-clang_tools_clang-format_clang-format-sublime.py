f0d151bde81e0c846acf04d32a5ba6e31299fdbe
Brooks Davis <brooks@FreeBSD.org>
2023-08-01 00:44:58 +0100

    devel/llvm17: Welcome 17.0.0rc1
    The first release candidate of the LLVM 17 release.
    
    Polly is now enabled by default.

--- devel/llvm17/files/patch-clang_tools_clang-format_clang-format-sublime.py
+++ devel/llvm17/files/patch-clang_tools_clang-format_clang-format-sublime.py
@@ -4,8 +4,8 @@
  import subprocess
  
  # Change this to the full path if clang-format is not on the path.
--binary = 'clang-format'
-+binary = 'clang-format%%LLVM_SUFFIX%%'
+-binary = "clang-format"
++binary = "clang-format%%LLVM_SUFFIX%%"
  
  # Change this to format according to other formatting styles. See the output of
  # 'clang-format --help' for a list of supported styles. The default looks for
