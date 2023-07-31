759b2fb3bb384f1280fa8d4d12594e518af9b2d2
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-08-01 00:30:54 +0800

    graphics/p5-Geo-GDAL-FFI: Update to 0.1
    Changes:	https://metacpan.org/dist/Geo-GDAL-FFI/changes

--- graphics/p5-Geo-GDAL-FFI/files/patch-lib-Geo-GDAL-FFI.pm
+++ graphics/p5-Geo-GDAL-FFI/files/patch-lib-Geo-GDAL-FFI.pm
@@ -1,11 +1,10 @@
---- lib/Geo/GDAL/FFI.pm.orig	2021-03-16 05:39:02 UTC
+--- lib/Geo/GDAL/FFI.pm.orig	2023-07-10 15:47:31 UTC
 +++ lib/Geo/GDAL/FFI.pm
-@@ -433,7 +433,7 @@ sub new {
- 
-     my $ffi = FFI::Platypus->new;
-     $ffi->load_custom_type('::StringPointer' => 'string_pointer');
--    $ffi->lib(Alien::gdal->dynamic_libs);
-+    $ffi->lib(['%%LOCALBASE%%/lib/libgdal.so']);
- 
-     $ffi->type('(pointer,size_t,size_t,opaque)->size_t' => 'VSIWriteFunction');
-     $ffi->type('(int,int,string)->void' => 'CPLErrorHandler');
+@@ -1812,7 +1812,6 @@ BEGIN {
+         $gdal = Geo::GDAL::gdal->new();
+     };
+     if ($@) {
+-        require Alien::gdal;
+         no strict 'subs';
+         $gdal = Alien::gdal;
+     }
