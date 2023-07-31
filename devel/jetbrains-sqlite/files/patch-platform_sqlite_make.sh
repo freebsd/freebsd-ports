bd1ebaadf1d760f6446f2b23c9b5614e0ae2e0ec
Dmitry Wagin <dmitry.wagin@ya.ru>
2023-07-31 09:59:54 +0200

    devel/jetbrains-sqlite: new port. Native SQLite support for devel/jetbrains-*
    ChangeLog: https://github.com/JetBrains/intellij-community/tree/master/platform/sqlite
    
    This port is needed by the upcoming releases of devel/jetbrains-* ports.
    
    PR:		272819
    Reported by:	dmitry.wagin@ya.ru (maintainer)

---
+++ devel/jetbrains-sqlite/files/patch-platform_sqlite_make.sh
@@ -0,0 +1,22 @@
+--- platform/sqlite/make.sh.orig	2023-07-25 23:43:08 UTC
++++ platform/sqlite/make.sh
+@@ -23,6 +23,10 @@ if [ "$OS" == "mac" ]; then
+   if [ "$ARCH" == "x86_64" ]; then
+     cFlags+=" --target=x86_64-apple-darwin18.7.0"
+   fi
++elif [ "$OS" == "freebsd" ]; then
++  cFlags+=" -I$JAVA_HOME/include/freebsd"
++  linkFlags+=" -shared"
++  libFilename="libsqliteij.so"
+ elif [ "$OS" == "linux" ]; then
+   libFilename="libsqliteij.so"
+ 
+@@ -69,7 +73,7 @@ CC="${CC:-clang}"
+ 
+ libFile="$outDir/$libFilename"
+ "${CROSS_PREFIX}${CC}" $cFlags -o "$libFile" "$outDir/NativeDB.o" "$outDir/sqlite3.o" $linkFlags
+-shasum -a 256 "$libFile" | head -c 64 >"$libFile.sha256"
++sha256sum "$libFile" | head -c 64 >"$libFile.sha256"
+ 
+ unlink "$outDir/sqlite3.o"
+ unlink "$outDir/NativeDB.o"
