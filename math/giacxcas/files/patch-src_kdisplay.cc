2a427cef80628382a521f81033ee9c8fe9e4fe59
wen heping <wenheping2000@hotmail.com>
2023-08-02 21:32:48 -0700

    math/giacxcas: Update 1.9.0-21 → 1.9.0-55

---
+++ math/giacxcas/files/patch-src_kdisplay.cc
@@ -0,0 +1,10 @@
+--- src/kdisplay.cc.orig	2023-08-02 10:12:31 UTC
++++ src/kdisplay.cc
+@@ -29,7 +29,6 @@
+ #include <syscall.h>
+ #include "sha256.h"
+ #endif
+-#include <alloca.h>
+ #ifndef is_cx2
+ #define is_cx2 false
+ #endif
