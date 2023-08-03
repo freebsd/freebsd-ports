6483d7e6e5f341dbcc6483d30f353b3fb2dd2c45
Piotr Kubaj <pkubaj@FreeBSD.org>
2023-08-03 10:39:58 +0000

    games/oblige: fix build on powerpc*
    gui/sys_endian.h:66:58: error: unsupported inline asm: input with type 'int' matching output with type 'u16_t' (aka 'unsigned short')
      __asm__("rlwimi %0,%2,8,16,23" : "=&r" (result) : "0" (x >> 8), "r" (x));
                                              ~~~~~~         ^~~~~~

---
+++ games/oblige/files/patch-gui_sys__endian.h
@@ -0,0 +1,14 @@
+--- gui/sys_endian.h.orig	2023-07-31 15:43:29 UTC
++++ gui/sys_endian.h
+@@ -61,10 +61,7 @@ static inline u16_t UT_Swap16(u16_t x)
+ #elif defined(__GNUC__) && (defined(__powerpc__) || defined(__ppc__))
+ static inline u16_t UT_Swap16(u16_t x)
+ {
+-  u16_t result;
+-
+-  __asm__("rlwimi %0,%2,8,16,23" : "=&r" (result) : "0" (x >> 8), "r" (x));
+-  return result;
++  return __builtin_bswap16(x);
+ }
+ #else
+ static inline u16_t UT_Swap16(u16_t x) {
