2eff09595997f14e2a589568fdf3289643a093c1
Guido Falsi <madpilot@FreeBSD.org>
2023-08-06 17:23:28 +0200

    sysutils/catfish: Update to 4.18.0

--- sysutils/catfish/files/patch-catfish__lib_catfishconfig.py
+++ sysutils/catfish/files/patch-catfish__lib_catfishconfig.py
@@ -1,6 +1,6 @@
---- catfish_lib/catfishconfig.py.orig	2020-01-01 18:29:00 UTC
+--- catfish_lib/catfishconfig.py.orig	2023-08-06 08:56:49 UTC
 +++ catfish_lib/catfishconfig.py
-@@ -17,6 +17,7 @@
+@@ -17,6 +17,7 @@ import os
  #   with this program.  If not, see <https://www.gnu.org/licenses/>.
  
  import os
@@ -8,15 +8,15 @@
  
  __all__ = [
      'project_path_not_found',
-@@ -29,7 +30,10 @@ __all__ = [
+@@ -29,7 +30,10 @@ __catfish_data_directory__ = '../data/'
  # files). By default, this is ../data, relative your trunk layout
  __catfish_data_directory__ = '../data/'
  # Location of locate.db file
--__locate_db_path__ = '/var/lib/mlocate/mlocate.db'
+-__locate_db_paths__ = ('/var/lib/plocate/plocate.db', '/var/lib/mlocate/mlocate.db')
 +if sys.platform.startswith('linux'):
-+    __locate_db_path__ = '/var/lib/mlocate/mlocate.db'
++    __locate_db_paths__ = ('/var/lib/plocate/plocate.db', '/var/lib/mlocate/mlocate.db')
 +elif 'bsd' in sys.platform or sys.platform.startswith('dragonfly'):
-+    __locate_db_path__ = '/var/db/locate.database'
++    __locate_db_paths__ = ('/var/db/locate.database')
  __license__ = 'GPL-2+'
- __version__ = '1.4.12'
+ __version__ = '4.18.0'
  __url__ = 'https://docs.xfce.org/apps/catfish/start'
