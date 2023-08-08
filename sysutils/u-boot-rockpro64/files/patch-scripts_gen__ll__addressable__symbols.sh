92069368d7e07c7e6efb418bead6e2959ec4d66a
Minsoo Choo <minsoochoo0122@proton.me>
2023-08-08 09:43:19 +0200

    sysutils/u-boot-*: Update to 2023.07.02
    Differential Revision:	https://reviews.freebsd.org/D41193
    
    (cherry picked from commit c1baa2b4d8d63c4369e4e53422914ea515dbd1e2)

---
+++ sysutils/u-boot-rockpro64/files/patch-scripts_gen__ll__addressable__symbols.sh
@@ -0,0 +1,8 @@
+--- scripts/gen_ll_addressable_symbols.sh.orig	2023-07-27 20:45:26 UTC
++++ scripts/gen_ll_addressable_symbols.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/bin/sh
+ # SPDX-License-Identifier: GPL-2.0+
+ # Copyright (C) 2020 Marek Behún <kabel@kernel.org>
+ 
