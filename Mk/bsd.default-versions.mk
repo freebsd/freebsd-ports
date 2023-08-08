4f13ca16c773075e671c9b732cf452887596a402
Jan Beich <jbeich@FreeBSD.org>
2023-08-08 10:52:26 +0000

    lang/mono: switch to 5.20 by default
    PR:		238089

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -94,7 +94,7 @@ LUAJIT_DEFAULT?=	luajit-openresty
 LUAJIT_DEFAULT?=	luajit-devel
 .  endif
 # Possible values: 5.10, 5.20, 6.8
-MONO_DEFAULT?=		5.10
+MONO_DEFAULT?=		5.20
 # Possible values: 5.7, 8.0, 10.5m, 10.6m, 10.11m, 5.7p, 5.7w
 MYSQL_DEFAULT?=		8.0
 # Possible values: ninja, samurai
