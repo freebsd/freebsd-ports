dce7c84bbbee09a7d97c60af6aa11eb90e4c0aaa
Gleb Popov <arrowd@FreeBSD.org>
2023-08-12 21:19:00 +0300

    sysutils/accountsservice: Switch to my fork and update to 23.13.9
    My fork incorporates adapted patches from Olivier Duchateau.
    
    PR:		270881
    Sponsored by:	Serenity Cybersecurity, LLC

--- sysutils/accountsservice/files/patch-meson__post__install.py
+++ sysutils/accountsservice/files/patch-meson__post__install.py
@@ -1,11 +1,13 @@
---- meson_post_install.py.orig	2019-04-23 15:16:09 UTC
+--- meson_post_install.py.orig	2022-02-15 21:53:55 UTC
 +++ meson_post_install.py
-@@ -9,8 +9,8 @@ localstatedir = os.path.normpath(destdir + os.sep + sy
+@@ -9,9 +9,9 @@ localstatedir = os.path.normpath(destdir + os.sep + sy
  # FIXME: meson will not track the creation of these directories
  #        https://github.com/mesonbuild/meson/blob/master/mesonbuild/scripts/uninstall.py#L39
  dst_dirs = [
+-  (os.path.join(localstatedir, 'lib'), 0o755),
 -  (os.path.join(localstatedir, 'lib', 'AccountsService', 'icons'), 0o775),
 -  (os.path.join(localstatedir, 'lib', 'AccountsService', 'users'), 0o700),
++  (os.path.join(localstatedir, 'db'), 0o755),
 +  (os.path.join(localstatedir, 'db', 'AccountsService', 'icons'), 0o775),
 +  (os.path.join(localstatedir, 'db', 'AccountsService', 'users'), 0o700),
  ]
