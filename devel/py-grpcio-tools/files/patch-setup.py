a012c9c869ff13981f87a23f3a23a9d5492d2b56
Vanilla I. Shu <vanilla@FreeBSD.org>
2023-08-13 14:29:28 +0800

    devel/py-grpcio-tools: update t. 1.57.0.

--- devel/py-grpcio-tools/files/patch-setup.py
+++ devel/py-grpcio-tools/files/patch-setup.py
@@ -1,29 +1,29 @@
---- setup.py.orig	2022-10-01 13:52:51 UTC
+--- setup.py.orig	2023-08-08 21:49:44 UTC
 +++ setup.py
-@@ -149,7 +149,7 @@ if EXTRA_ENV_COMPILE_ARGS is None:
+@@ -160,7 +160,7 @@ if EXTRA_ENV_COMPILE_ARGS is None:
              # We need to statically link the C++ Runtime, only the C runtime is
              # available dynamically
-             EXTRA_ENV_COMPILE_ARGS += ' /MT'
+             EXTRA_ENV_COMPILE_ARGS += " /MT"
 -    elif "linux" in sys.platform or "darwin" in sys.platform:
 +    elif "linux" in sys.platform or "darwin" in sys.platform or "freebsd" in sys.platform:
-         EXTRA_ENV_COMPILE_ARGS += ' -fno-wrapv -frtti'
+         EXTRA_ENV_COMPILE_ARGS += " -fno-wrapv -frtti"
  if EXTRA_ENV_LINK_ARGS is None:
-     EXTRA_ENV_LINK_ARGS = ''
-@@ -175,7 +175,7 @@ if EXTRA_ENV_LINK_ARGS is None:
-     if "darwin" in sys.platform:
-         EXTRA_ENV_LINK_ARGS += ' -Wl,-exported_symbol,_{}'.format(
-             _EXT_INIT_SYMBOL)
+     EXTRA_ENV_LINK_ARGS = ""
+@@ -187,7 +187,7 @@ if EXTRA_ENV_LINK_ARGS is None:
+         EXTRA_ENV_LINK_ARGS += " -Wl,-exported_symbol,_{}".format(
+             _EXT_INIT_SYMBOL
+         )
 -    if "linux" in sys.platform or "darwin" in sys.platform:
 +    if "linux" in sys.platform or "darwin" in sys.platform or "freebsd" in sys.platform:
-         EXTRA_ENV_LINK_ARGS += ' -lpthread'
+         EXTRA_ENV_LINK_ARGS += " -lpthread"
          if check_linker_need_libatomic():
-             EXTRA_ENV_LINK_ARGS += ' -latomic'
-@@ -206,7 +206,7 @@ if "win32" in sys.platform:
-     DEFINE_MACROS += (('WIN32_LEAN_AND_MEAN', 1),)
-     if '64bit' in platform.architecture()[0]:
-         DEFINE_MACROS += (('MS_WIN64', 1),)
+             EXTRA_ENV_LINK_ARGS += " -latomic"
+@@ -225,7 +225,7 @@ if "win32" in sys.platform:
+     )
+     if "64bit" in platform.architecture()[0]:
+         DEFINE_MACROS += (("MS_WIN64", 1),)
 -elif "linux" in sys.platform or "darwin" in sys.platform:
 +elif "linux" in sys.platform or "darwin" in sys.platform or "freebsd" in sys.platform:
-     DEFINE_MACROS += (('HAVE_PTHREAD', 1),)
+     DEFINE_MACROS += (("HAVE_PTHREAD", 1),)
  
  # By default, Python3 distutils enforces compatibility of
