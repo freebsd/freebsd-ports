8a62a1d0d0b79ff977f3ff7fdef9d9272c42e40d
Hiroki Tagato <tagattie@FreeBSD.org>
2023-08-13 20:22:06 +0900

    devel/electron25: update to 25.5.0
    Changelog: https://github.com/electron/electron/releases/tag/v25.5.0
    
    Reported by:	GitHub (watch releases)
    
    (cherry picked from commit d2e8fa2be25e3efce7dd6a27758e05f1889861a9)

--- devel/electron25/files/patch-electron_shell_browser_net_system__network__context__manager.cc
+++ devel/electron25/files/patch-electron_shell_browser_net_system__network__context__manager.cc
@@ -1,6 +1,6 @@
---- electron/shell/browser/net/system_network_context_manager.cc.orig	2023-05-31 14:28:29 UTC
+--- electron/shell/browser/net/system_network_context_manager.cc.orig	2023-08-09 14:05:20 UTC
 +++ electron/shell/browser/net/system_network_context_manager.cc
-@@ -42,7 +42,7 @@
+@@ -41,7 +41,7 @@
  #include "shell/common/options_switches.h"
  #include "url/gurl.h"
  
