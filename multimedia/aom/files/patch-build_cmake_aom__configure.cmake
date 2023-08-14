f0f115af84c322c272f76bf279e1713494f2dc5b
Jan Beich <jbeich@FreeBSD.org>
2023-08-14 23:22:47 +0200

    multimedia/aom: update to 3.7.0.r1
    Changes:	https://aomedia.googlesource.com/aom/+log/v3.6.1..v3.7.0-rc1
    Reported by:	Repology, portscout

--- multimedia/aom/files/patch-build_cmake_aom__configure.cmake
+++ multimedia/aom/files/patch-build_cmake_aom__configure.cmake
@@ -1,9 +1,8 @@
 - FreeBSD and NetBSD powerpc* targets don't abbreviate to ppc*
-- ARM uses compiler intrinsics, so don't require GNU as
 
---- build/cmake/aom_configure.cmake.orig	2021-02-23 19:51:03 UTC
+--- build/cmake/aom_configure.cmake.orig	2023-08-14 21:22:47 UTC
 +++ build/cmake/aom_configure.cmake
-@@ -71,7 +71,7 @@ if(NOT AOM_TARGET_CPU)
+@@ -75,7 +75,7 @@ if(NOT AOM_TARGET_CPU)
      set(AOM_TARGET_CPU "${cpu_lowercase}")
    elseif(cpu_lowercase MATCHES "aarch64")
      set(AOM_TARGET_CPU "arm64")
@@ -12,21 +11,3 @@
      set(AOM_TARGET_CPU "ppc")
    else()
      message(WARNING "The architecture ${CMAKE_SYSTEM_PROCESSOR} is not "
-@@ -159,17 +159,6 @@ elseif(AOM_TARGET_CPU MATCHES "arm")
-     if(NOT AS_EXECUTABLE)
-       set(AS_EXECUTABLE as)
-     endif()
--  endif()
--  find_program(as_executable_found ${AS_EXECUTABLE})
--  if(NOT as_executable_found)
--    message(
--      FATAL_ERROR
--        "Unable to find assembler and optimizations are enabled."
--        "Searched for ${AS_EXECUTABLE}. Install it, add it to your path, or "
--        "set the assembler directly by adding -DAS_EXECUTABLE=<assembler path> "
--        "to your CMake command line."
--        "To build without optimizations, add -DAOM_TARGET_CPU=generic to your "
--        "cmake command line.")
-   endif()
-   string(STRIP "${AOM_AS_FLAGS}" AOM_AS_FLAGS)
- endif()
--- multimedia/aom/files/patch-build_cmake_cpu.cmake
+++ multimedia/aom/files/patch-build_cmake_cpu.cmake
@@ -1,6 +1,6 @@
 - Don't enable NEON on armv6 similar to build/cmake/rtcd.pl
 
---- build/cmake/cpu.cmake.orig	2018-11-16 20:24:20 UTC
+--- build/cmake/cpu.cmake.orig	2023-08-14 21:22:47 UTC
 +++ build/cmake/cpu.cmake
 @@ -9,7 +9,7 @@
  # can obtain it at www.aomedia.org/license/patent.
@@ -8,6 +8,6 @@
  
 -if("${AOM_TARGET_CPU}" MATCHES "^arm")
 +if("${AOM_TARGET_CPU}" MATCHES "^arm" AND NOT "${AOM_TARGET_CPU}" MATCHES "^armv6")
-   set(ARCH_ARM 1)
-   set(RTCD_ARCH_ARM "yes")
- 
+   set(AOM_ARCH_ARM 1)
+   if("${AOM_TARGET_CPU}" STREQUAL "arm64")
+     set(AOM_ARCH_AARCH64 1)
