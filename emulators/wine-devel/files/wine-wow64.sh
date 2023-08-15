freebsd/freebsd-ports

commit 3f914243d8179e0d1606c7d00ee2eb3498d0bb62

emulators/wine-devel: Add a note on Poudriere in the WoW case
This forward ports commit 9dc3d307de26ebb3ee31859fed8cc9dbc779dff5
from emulators/wine.

PR:		260407
Reported by:	Zane C. Bowers-Hadley <vvelox@vvelox.net>
Approved by:	maintainer

emulators/wine-devel/files/wine-wow64.sh
@@ -9,7 +9,17 @@ I386_ROOT="${WINE_i386_ROOT:-$HOME/.i386-wine-pkg}"
 if [ ! -f "$I386_ROOT/$PREFIX/bin/wine" ]
 then
   printf "%s doesn't exist!\n\n" "$I386_ROOT/$PREFIX/bin/wine"
-  printf "Try installing 32-bit Wine with\n\t%s\n" "$PREFIX/share/wine/pkg32.sh install wine-devel mesa-dri"
+  printf "Try installing 32-bit Wine with\n\t%s\n" "$PREFIX/share/wine/pkg32.sh install wine mesa-dri"
+  ABI=$(pkg config ABI | sed s/amd64/i386/)
+  FREEBSD_VERSION_MAJOR=`uname -r | sed "s/\..*//"`
+  cat <<- HERE
+
+	If using Poudriere, please make sure your repo is setup to use ${ABI}
+	and create symlinks for
+	  FreeBSD:$FREEBSD_VERSION_MAJOR:amd64 and
+	  FreeBSD:$FREEBSD_VERSION_MAJOR:i386
+	to the relevant output directories. See pkg.conf(5) for more info.
+HERE
   exit 1
 fi
 

commit 16d8a4b821d2db0134901589e22740290cc40020

emulators/wine-devel: Fix 32-bit pkg invocation for WoW64
This addresses a regression introduced with commit 3f914243d8179e0d
on 2022-03-12 which was a forward port from emulators/wine and not
sufficiently adjusted to this port).

Do not bump PORTREVISION since 32-bit builds are broken right now.

PR:	273137

emulators/wine-devel/files/wine-wow64.sh
@@ -9,7 +9,7 @@ I386_ROOT="${WINE_i386_ROOT:-$HOME/.i386-wine-pkg}"
 if [ ! -f "$I386_ROOT/$PREFIX/bin/wine" ]
 then
   printf "%s doesn't exist!\n\n" "$I386_ROOT/$PREFIX/bin/wine"
-  printf "Try installing 32-bit Wine with\n\t%s\n" "$PREFIX/share/wine/pkg32.sh install wine mesa-dri"
+  printf "Try installing 32-bit Wine with\n\t%s\n" "$PREFIX/share/wine/pkg32.sh install wine-devel mesa-dri"
   ABI=$(pkg config ABI | sed s/amd64/i386/)
   FREEBSD_VERSION_MAJOR=`uname -r | sed "s/\..*//"`
   cat <<- HERE