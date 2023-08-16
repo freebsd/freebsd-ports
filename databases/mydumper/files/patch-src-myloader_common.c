6a807d45ee340ae2805ea6c637a5cc0b8d3758df
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-08-17 02:07:53 +0800

    databases/mydumper: Update to 0.15.1-2
    Changes:	https://github.com/mydumper/mydumper/releases

--- databases/mydumper/files/patch-src-myloader_common.c
+++ databases/mydumper/files/patch-src-myloader_common.c
@@ -1,10 +1,11 @@
---- src/myloader_common.c.orig	2023-05-24 12:16:48 UTC
-+++ src/myloader_common.c
-@@ -21,6 +21,7 @@
- #include <stdlib.h>
- #include <string.h>
- #include <sys/wait.h>
-+#include <unistd.h>
- 
- #ifdef ZWRAP_USE_ZSTD
- #include "../zstd/zstd_zlibwrapper.h"
+--- src/mydumper_common.c.orig	2023-08-02 14:40:15 UTC
++++ src/mydumper_common.c
+@@ -123,7 +123,7 @@ int m_close_pipe(guint thread_id, void *file, gchar *f
+   g_mutex_lock(fifo_table_mutex);
+   struct fifo *f=g_hash_table_lookup(fifo_hash,file);
+   g_mutex_unlock(fifo_table_mutex);
+-  int r=close(fileno(file));
++  int r=close(fileno((FILE *) file));
+   if (f != NULL){
+     int status=0;
+     waitpid(f->pid, &status, 0);
