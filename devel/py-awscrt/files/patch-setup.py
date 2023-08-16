6ea42400ddfa7a7ddd4140497728469aade890fe
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-08-17 03:44:20 +0800

    devel/py-awscrt: Update to 0.16.26
    Changes:	https://github.com/awslabs/aws-crt-python/releases

--- devel/py-awscrt/files/patch-setup.py
+++ devel/py-awscrt/files/patch-setup.py
@@ -1,11 +1,44 @@
---- setup.py.orig	2022-11-09 18:39:08 UTC
+--- setup.py.orig	2023-07-19 00:27:11 UTC
 +++ setup.py
-@@ -320,7 +320,7 @@ def awscrt_ext():
+@@ -14,7 +14,6 @@ import shutil
+ import subprocess
+ import sys
+ import sysconfig
+-from wheel.bdist_wheel import bdist_wheel
+ 
+ 
+ def is_64bit():
+@@ -283,16 +282,6 @@ class awscrt_build_ext(setuptools.command.build_ext.bu
+         super().run()
+ 
+ 
+-class bdist_wheel_abi3(bdist_wheel):
+-    def get_tag(self):
+-        python, abi, plat = super().get_tag()
+-        if python.startswith("cp") and sys.version_info >= (3, 11):
+-            # on CPython, our wheels are abi3 and compatible back to 3.11
+-            return "cp311", "abi3", plat
+-
+-        return python, abi, plat
+-
+-
+ def awscrt_ext():
+     # fetch the CFLAGS/LDFLAGS from env
+     extra_compile_args = os.environ.get('CFLAGS', '').split()
+@@ -360,7 +349,7 @@ def awscrt_ext():
              # a proper MacOS Universal2 binary. The linker warns us about this,
              # but WHATEVER. Building everything twice (x86_64 and arm64) takes too long.
              if not is_macos_universal2():
 -                extra_link_args += ['-Wl,-fatal_warnings']
 +                extra_link_args += ['-Wl,-fatal-warnings']
  
-     return setuptools.Extension(
-         '_awscrt',
+     if sys.version_info >= (3, 11):
+         define_macros.append(('Py_LIMITED_API', '0x030B0000'))
+@@ -410,6 +399,6 @@ setuptools.setup(
+     ],
+     python_requires='>=3.7',
+     ext_modules=[awscrt_ext()],
+-    cmdclass={'build_ext': awscrt_build_ext, "bdist_wheel": bdist_wheel_abi3},
++    cmdclass={'build_ext': awscrt_build_ext},
+     test_suite='test',
+ )
