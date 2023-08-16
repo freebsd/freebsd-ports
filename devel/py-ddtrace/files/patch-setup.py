0c96adba0b2abfe6ead973a4d479fece30334ef3
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-08-17 02:11:45 +0800

    devel/py-ddtrace: Update to 1.17.2
    - Update WWW
    
    Changes:	https://github.com/DataDog/dd-trace-py/releases
    		https://ddtrace.readthedocs.io/en/stable/release_notes.html

--- devel/py-ddtrace/files/patch-setup.py
+++ devel/py-ddtrace/files/patch-setup.py
@@ -1,10 +1,19 @@
---- setup.py.orig	2023-02-17 21:33:11 UTC
+--- setup.py.orig	2023-07-25 21:04:28 UTC
 +++ setup.py
-@@ -366,7 +366,6 @@ setup(
+@@ -557,7 +557,6 @@ setup(
+     tests_require=["flake8"],
      cmdclass={
-         "test": Tox,
-         "build_ext": BuildExtCommand,
+         "build_ext": CMakeBuild,
 -        "build_py": LibraryDownloader,
          "clean": CleanLibraries,
      },
      entry_points={
+@@ -584,7 +583,7 @@ setup(
+         "Programming Language :: Python :: 3.11",
+     ],
+     use_scm_version={"write_to": "ddtrace/_version.py"},
+-    setup_requires=["setuptools_scm[toml]>=4", "cython<3", "cmake>=3.24.2; python_version>='3.6'"],
++    setup_requires=["setuptools_scm[toml]>=4", "cython<3"],
+     ext_modules=ext_modules
+     + cythonize(
+         [
