494ec7b5efee25a05d64e6e5ae282089d0f02400
Hiroki Tagato <tagattie@FreeBSD.org>
2023-08-18 19:37:22 +0900

    devel/electron24: update to 24.8.0
    Changelog: https://github.com/electron/electron/releases/tag/v24.8.0
    
    Reported by:	GitHub (watch releases)

--- devel/electron24/files/patch-electron_shell_common_node__bindings.cc
+++ devel/electron24/files/patch-electron_shell_common_node__bindings.cc
@@ -1,4 +1,4 @@
---- electron/shell/common/node_bindings.cc.orig	2023-06-13 21:28:30 UTC
+--- electron/shell/common/node_bindings.cc.orig	2023-08-16 07:45:30 UTC
 +++ electron/shell/common/node_bindings.cc
 @@ -37,7 +37,7 @@
  #include "third_party/blink/renderer/bindings/core/v8/v8_initializer.h"  // nogncheck
@@ -18,7 +18,7 @@
    electron::crash_keys::SetCrashKey("electron.v8-fatal.message", message);
    electron::crash_keys::SetCrashKey("electron.v8-fatal.location", location);
  #endif
-@@ -433,7 +433,7 @@ void NodeBindings::Initialize() {
+@@ -432,7 +432,7 @@ void NodeBindings::Initialize() {
    TRACE_EVENT0("electron", "NodeBindings::Initialize");
    // Open node's error reporting system for browser process.
  
