b717d346a0889c45acde65a91187f5dc559a4df1
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-08-19 04:51:09 +0200

    devel/cook: Mark BROKEN
    - Fails to build with:
      a space character is not allowed in an escape sequence parameter
    - Add LICENSE GPLv3
    - Utilize USES=localbase
    - Mark DEPRECATED as upstreeam last release was in 2008 and upstream
      site is vaporized
    - Set EXPIRATION_DATE to 2023-12-31
    - Refresh patches
    
    Approved by:	portmgr (blanket)
    Sponsored by:	The FreeBSD Foundation

--- devel/cook/files/patch-common_ac_time.h
+++ devel/cook/files/patch-common_ac_time.h
@@ -1,6 +1,6 @@
---- common/ac/time.h.orig	Fri Nov 29 23:15:13 2002
-+++ common/ac/time.h	Fri Nov 29 23:15:27 2002
-@@ -29,7 +29,7 @@
+--- common/ac/time.h.orig	2007-09-30 13:52:44 UTC
++++ common/ac/time.h
+@@ -27,7 +27,7 @@
   * Catch-22: Dec Alpha OSF/1: need to include time.h before sys/time.h
   * before time.h
   */
