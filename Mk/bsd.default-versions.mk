3b2ff2ef194c83835fe7dc6618427b8fd50ea31d
Jason E. Hale <jhale@FreeBSD.org>
2023-08-21 09:57:29 -0400

    Mk: Add Uses/ebur128.mk
    Handles dependencies for the chosen implementation of libebur128 set
    via DEFAULT_VERSIONS: audio/libebur128 (legacy) and audio/ebur128
    (rust).
    
    PR:		272843

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -17,7 +17,7 @@ _INCLUDE_BSD_DEFAULT_VERSIONS_MK=	yes
 
 LOCALBASE?=	/usr/local
 
-.  for lang in APACHE BDB COROSYNC EMACS FIREBIRD FORTRAN FPC GCC \
+.  for lang in APACHE BDB COROSYNC EBUR128 EMACS FIREBIRD FORTRAN FPC GCC \
 	GHOSTSCRIPT GL GO GUILE IMAGEMAGICK JAVA LAZARUS LIBRSVG2 LINUX LLVM \
 	LUA LUAJIT MONO MYSQL NINJA NODEJS OPENLDAP PERL5 PGSQL PHP \
 	PYCRYPTOGRAPHY PYTHON PYTHON2 PYTHON3 RUBY RUST SAMBA SSL TCLTK VARNISH
@@ -38,6 +38,12 @@ APACHE_DEFAULT?=	2.4
 BDB_DEFAULT?=		5
 # Possible values: 2, 3
 COROSYNC_DEFAULT?=	2
+# Possible values: rust, legacy
+.  if empty(ARCH:Naarch64:Namd64:Narmv7:Ni386:Npowerpc64:Npowerpc64le:Npowerpc:Nriscv64)
++EBUR128_DEFAULT?=	rust
+.  else
++EBUR128_DEFAULT?=	legacy
+.  endif
 # Possible_values: full canna nox devel_full devel_nox
 #EMACS_DEFAULT?=	let the flavor be the default if not explicitly set
 # Possible values: 3.0, 4.0
