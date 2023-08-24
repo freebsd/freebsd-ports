5958533fac2116c3ac7e3c48321c550bf9fcc562
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2023-08-24 22:59:29 +0200

    www/firefox: update to 117.0 (rc2)
    while here, tag patches so I don't lose track
    
    Release Notes (soon):
      https://www.mozilla.org/en-US/firefox/117.0/releasenotes/
    
    (cherry picked from commit 58b3862e67cb314301fe445f91f96b08242b2b00)

--- www/firefox/files/patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland__egl__dmabuf.cc
+++ www/firefox/files/patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland__egl__dmabuf.cc
@@ -1,3 +1,8 @@
+commit 3524023ca1d2184ab9a5a7cc61695923e6834cc1
+Author: Christoph Moench-Tegeder <cmt@burggraben.net>
+
+    avoid linux-only includes
+
 diff --git third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc
 index 6a019c64b4b3..18bb43d797dd 100644
 --- third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc
