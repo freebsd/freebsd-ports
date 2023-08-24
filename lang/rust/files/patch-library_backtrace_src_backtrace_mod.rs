fa874813924cb55095c731fcab6c31b036b38541
Mikael Urankar <mikael@FreeBSD.org>
2023-08-24 19:20:10 +0200

    lang/rust: Update to 1.72.0
    Announce:	https://blog.rust-lang.org/2023/08/24/Rust-1.72.0.html
    ChangeLog:	https://github.com/rust-lang/rust/releases/tag/1.72.0
    
    PR:		244316
    Tested by:	mikael,pkubaj

--- lang/rust/files/patch-library_backtrace_src_backtrace_mod.rs
+++ lang/rust/files/patch-library_backtrace_src_backtrace_mod.rs
@@ -1,10 +1,9 @@
 --- library/backtrace/src/backtrace/mod.rs.orig	2021-11-29 19:27:28 UTC
 +++ library/backtrace/src/backtrace/mod.rs
-@@ -138,6 +138,8 @@ cfg_if::cfg_if! {
-                 unix,
+@@ -139,6 +139,7 @@ cfg_if::cfg_if! {
                  not(target_os = "emscripten"),
                  not(all(target_os = "ios", target_arch = "arm")),
-+                not(all(target_os = "freebsd", target_arch = "arm")),
+                 not(all(target_os = "nto", target_env = "nto70")),
 +                not(all(target_os = "freebsd", target_arch = "riscv64")),
              ),
              all(
