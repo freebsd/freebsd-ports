e440162525f444d45c3928cea1958575aa756b65
Robert Nagy <rnagy@FreeBSD.org>
2023-08-25 14:33:29 +0200

    www/ungoogled-chromium: sync patches

--- www/ungoogled-chromium/files/patch-media_gpu_chromeos_video__decoder__pipeline.cc
+++ www/ungoogled-chromium/files/patch-media_gpu_chromeos_video__decoder__pipeline.cc
@@ -1,6 +1,6 @@
---- media/gpu/chromeos/video_decoder_pipeline.cc.orig	2023-07-21 09:49:17 UTC
+--- media/gpu/chromeos/video_decoder_pipeline.cc.orig	2023-08-25 12:24:14 UTC
 +++ media/gpu/chromeos/video_decoder_pipeline.cc
-@@ -979,14 +979,14 @@ VideoDecoderPipeline::PickDecoderOutputFormat(
+@@ -986,14 +986,14 @@ VideoDecoderPipeline::PickDecoderOutputFormat(
      }
    }
  
@@ -17,7 +17,7 @@
    // Linux w/ V4L2 should not use a custom allocator
    // Only tested with video_decode_accelerator_tests
    // TODO(wenst@) Test with full Chromium Browser
-@@ -1117,7 +1117,7 @@ VideoDecoderPipeline::PickDecoderOutputFormat(
+@@ -1124,7 +1124,7 @@ VideoDecoderPipeline::PickDecoderOutputFormat(
               << " VideoFrames";
      auxiliary_frame_pool_->set_parent_task_runner(decoder_task_runner_);
  
