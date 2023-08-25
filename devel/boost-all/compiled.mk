b4c46aa44c8d5ea87f00115ccb0057b8eedc4365
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2023-08-25 10:10:32 +0100

    devel/boost-all: use system compiler for CheriABI
    Don't use a specific LLVM version from ports for CheriABI builds.
    We could try to replace the LLVM version used by ports with llvm-morello
    but that must be first carefully tested also for hybrid ABI builds.
    
    Unfortunately, upstream doesn't explain [1] why they require a specific
    LLVM version from ports to build boost ports.
    
    [1] https://github.com/freebsd/freebsd-ports/commit/3efdfab9b59f2cd00beec5c863820f0057f1cfb3

--- devel/boost-all/compiled.mk
+++ devel/boost-all/compiled.mk
@@ -43,7 +43,7 @@ MAKE_ARGS+=	pch=off
 
 .include <bsd.port.options.mk>
 
-.if ${OPSYS} == FreeBSD && ${OSVERSION} >= 1400000
+.if ${OPSYS} == FreeBSD && ${OSVERSION} >= 1400000 && !${ABI:Mpurecap}
 USES+=	llvm:build
 CC=	${_LLVM_MK_PREFIX}/bin/clang
 CPP=	${_LLVM_MK_PREFIX}/bin/clang-cpp
