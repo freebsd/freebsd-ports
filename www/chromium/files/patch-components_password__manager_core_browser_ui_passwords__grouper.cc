f14f291a6dfea6d167aa9800a5ea05125c4b776f
Robert Nagy <rnagy@FreeBSD.org>
2023-08-28 14:57:23 +0200

    www/*chromium: bump _LIBCPP_VERSION check to 160000 and fallback to base::ranges
    the chromium source tree provides ranges functions so let's use them
    in case our libcxx is too old
    
    PR:	273376

--- www/chromium/files/patch-components_password__manager_core_browser_ui_passwords__grouper.cc
+++ www/chromium/files/patch-components_password__manager_core_browser_ui_passwords__grouper.cc
@@ -1,21 +1,14 @@
---- components/password_manager/core/browser/ui/passwords_grouper.cc.orig	2023-08-17 12:40:56 UTC
+--- components/password_manager/core/browser/ui/passwords_grouper.cc.orig	2023-08-28 12:53:33 UTC
 +++ components/password_manager/core/browser/ui/passwords_grouper.cc
-@@ -399,6 +399,7 @@ absl::optional<PasskeyCredential> PasswordsGrouper::Ge
-   if (group_id_iterator == map_signon_realm_to_group_id_.end()) {
-     return absl::nullopt;
-   }
-+#if BUILDFLAG(IS_OPENBSD) || (BUILDFLAG(IS_FREEBSD) && (_LIBCPP_VERSION > 15000))
-   // Find the passkey in the group.
+@@ -403,7 +403,11 @@ absl::optional<PasskeyCredential> PasswordsGrouper::Ge
    const std::vector<PasskeyCredential>& passkeys =
        map_group_id_to_credentials_[group_id_iterator->second].passkeys;
-@@ -410,6 +411,10 @@ absl::optional<PasskeyCredential> PasswordsGrouper::Ge
-     return absl::nullopt;
-   }
-   return *passkey_it;
+   const auto passkey_it =
++#if (_LIBCPP_VERSION >= 160000)
+       std::ranges::find_if(passkeys, [&credential](const auto& passkey) {
 +#else
-+  LOG(ERROR) << "Unable to lookup in password group";
-+  return absl::nullopt;
++      base::ranges::find_if(passkeys, [&credential](const auto& passkey) {
 +#endif
- }
- 
- void PasswordsGrouper::ClearCache() {
+         return credential.passkey_credential_id == passkey.credential_id();
+       });
+   if (passkey_it == passkeys.end()) {
