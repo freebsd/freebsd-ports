3c9a82210557fd280dff7216b48b50e9d090b55e
Robert Nagy <rnagy@FreeBSD.org>
2023-08-28 14:57:23 +0200

    www/*chromium: bump _LIBCPP_VERSION check to 160000 and fallback to base::ranges
    the chromium source tree provides ranges functions so let's use them
    in case our libcxx is too old
    
    PR:	273376
    (cherry picked from commit f14f291a6dfea6d167aa9800a5ea05125c4b776f)

--- www/chromium/files/patch-components_webauthn_core_browser_passkey__sync__bridge.cc
+++ www/chromium/files/patch-components_webauthn_core_browser_passkey__sync__bridge.cc
@@ -1,38 +1,26 @@
---- components/webauthn/core/browser/passkey_sync_bridge.cc.orig	2023-08-17 12:40:56 UTC
+--- components/webauthn/core/browser/passkey_sync_bridge.cc.orig	2023-08-28 12:53:33 UTC
 +++ components/webauthn/core/browser/passkey_sync_bridge.cc
-@@ -267,6 +267,7 @@ PasskeySyncBridge::GetAllPasskeys() const {
- 
+@@ -268,7 +268,11 @@ PasskeySyncBridge::GetAllPasskeys() const {
  bool PasskeySyncBridge::DeletePasskey(const std::string& credential_id) {
    // Find the credential with the given |credential_id|.
-+#if BUILDFLAG(IS_OPENBSD) || (BUILDFLAG(IS_FREEBSD) && (_LIBCPP_VERSION > 15000))
    const auto passkey_it =
++#if (_LIBCPP_VERSION >= 160000)
        std::ranges::find_if(data_, [&credential_id](const auto& passkey) {
-         return passkey.second.credential_id() == credential_id;
-@@ -313,11 +314,16 @@ bool PasskeySyncBridge::DeletePasskey(const std::strin
-                      weak_ptr_factory_.GetWeakPtr()));
-   NotifyPasskeysChanged();
-   return true;
 +#else
-+  LOG(ERROR) << "Unable to delete passkey"; 
-+  return false;
++      base::ranges::find_if(data_, [&credential_id](const auto& passkey) {
 +#endif
- }
- 
- bool PasskeySyncBridge::UpdatePasskey(const std::string& credential_id,
+         return passkey.second.credential_id() == credential_id;
+       });
+   if (passkey_it == data_.end()) {
+@@ -319,7 +323,11 @@ bool PasskeySyncBridge::UpdatePasskey(const std::strin
                                        PasskeyChange change) {
    // Find the credential with the given |credential_id|.
-+#if BUILDFLAG(IS_OPENBSD) || (BUILDFLAG(IS_FREEBSD) && (_LIBCPP_VERSION > 15000))
    const auto passkey_it =
++#if (_LIBCPP_VERSION >= 160000)
        std::ranges::find_if(data_, [&credential_id](const auto& passkey) {
-         return passkey.second.credential_id() == credential_id;
-@@ -341,6 +347,10 @@ bool PasskeySyncBridge::UpdatePasskey(const std::strin
-                      weak_ptr_factory_.GetWeakPtr()));
-   NotifyPasskeysChanged();
-   return true;
 +#else
-+  LOG(ERROR) << "Unable to update passkey"; 
-+  return false;
++      base::ranges::find_if(data_, [&credential_id](const auto& passkey) {
 +#endif
- }
- 
- std::string PasskeySyncBridge::AddNewPasskeyForTesting(
+         return passkey.second.credential_id() == credential_id;
+       });
+   if (passkey_it == data_.end()) {
