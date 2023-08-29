c196d1f5ffb8368b0fc062ee05456818a545fbac
Koichiro Iwao <meta@FreeBSD.org>
2023-08-29 10:45:51 +0900

    net/xrdp{,-devel}: Update startwm.sh script
    PR:		273191

--- net/xrdp/files/startwm.sh
+++ net/xrdp/files/startwm.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 #
-# This script is an example. Edit this to suit your needs.
+# This script is just an example. Edit this to suit your needs.
 # If ${HOME}/startwm.sh exists, xrdp-sesman will execute it instead of this.
 
-#### set environment variables here if you want
+#### load profile if necessary
+# source /etc/profile
+
+#### you can set environment variables here whatever you want
 # export LANG=en_US.UTF-8
+# export GTK_IM_MODULE=fcitx
+# export XMODIFIERS=@im=fcitx
 
 #### start desktop environment
 # exec gnome-session
 # exec mate-session
 # exec start-lumina-desktop
-# exec startkde
+# exec ck-launch-session startplasma-x11
 # exec startxfce4
 exec xterm
