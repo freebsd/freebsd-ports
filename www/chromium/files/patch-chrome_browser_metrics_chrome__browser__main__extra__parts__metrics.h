36df0e4cd12d20c3a159b6d6c60d6612319d168b
Robert Nagy <rnagy@FreeBSD.org>
2023-08-30 12:26:47 +0200

    www/chromium: update to 116.0.5845.140
    Security:	https://vuxml.freebsd.org/freebsd/22fffa69-46fa-11ee-8290-a8a1599412c6.html
    (cherry picked from commit f80e0e6a5faa2227af95330f61609a16bf1ac040)

--- www/chromium/files/patch-chrome_browser_metrics_chrome__browser__main__extra__parts__metrics.h
+++ www/chromium/files/patch-chrome_browser_metrics_chrome__browser__main__extra__parts__metrics.h
@@ -1,6 +1,6 @@
---- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h.orig	2023-07-16 15:47:57 UTC
+--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h.orig	2023-08-30 06:01:45 UTC
 +++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h
-@@ -32,7 +32,7 @@ class PowerMetricsReporter;
+@@ -33,7 +33,7 @@ class PowerMetricsReporter;
  class ProcessMonitor;
  #endif
  
@@ -9,7 +9,7 @@
  class PressureMetricsReporter;
  #endif  // BUILDFLAG(IS_LINUX)
  
-@@ -133,7 +133,7 @@ class ChromeBrowserMainExtraPartsMetrics : public Chro
+@@ -139,7 +139,7 @@ class ChromeBrowserMainExtraPartsMetrics : public Chro
    std::unique_ptr<BatteryDischargeReporter> battery_discharge_reporter_;
  #endif  // !BUILDFLAG(IS_ANDROID)
  
