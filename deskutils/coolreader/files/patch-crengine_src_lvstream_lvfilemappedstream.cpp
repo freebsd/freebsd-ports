4b64cf228ff8d9a57e902d06df776272474f750b
Kenneth Raplee <kenrap@kennethraplee.com>
2023-08-30 10:32:31 +0200

    deskutils/coolreader: Update to 3.2.59
    PR:		273420
    Reported by:	kenrap@kennethraplee.com
    Approved by:	mistresssilvara@hotmail.com (maintainer, timeout > 2 weeks)

---
+++ deskutils/coolreader/files/patch-crengine_src_lvstream_lvfilemappedstream.cpp
@@ -0,0 +1,11 @@
+--- crengine/src/lvstream/lvfilemappedstream.cpp.orig
++++ crengine/src/lvstream/lvfilemappedstream.cpp
+@@ -45,7 +45,7 @@ extern "C" {
+ // it is logical to define our own wrapper function 'lseek'.
+ static inline lvpos_t cr3_lseek(int fd, lvoffset_t offset, int whence) {
+ #if LVLONG_FILE_SUPPORT == 1
+-    return (lvpos_t)::lseek64(fd, (off64_t)offset, whence);
++    return (lvpos_t)::lseek(fd, (off64_t)offset, whence);
+ #else
+     return (lvpos_t)::lseek(fd, (off_t)offset, whence);
+ #endif
