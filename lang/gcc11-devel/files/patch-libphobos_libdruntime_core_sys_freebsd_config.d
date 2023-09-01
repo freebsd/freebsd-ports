929ca7ff802f14a2852122678d0c1b9e5a89be22
Lorenzo Salvadore <salvadore@FreeBSD.org>
2023-09-01 03:04:20 +0000

    lang/gcc11-devel: Update to 11.4.1.s20230831
    Also update files/patch-libphobos_libdruntime_core_sys_freebsd_config.d
    patch to take into account FreeBSD 15.

--- lang/gcc11-devel/files/patch-libphobos_libdruntime_core_sys_freebsd_config.d
+++ lang/gcc11-devel/files/patch-libphobos_libdruntime_core_sys_freebsd_config.d
@@ -1,12 +1,13 @@
---- libphobos/libdruntime/core/sys/freebsd/config.d.orig	2022-10-21 22:39:24 UTC
+--- libphobos/libdruntime/core/sys/freebsd/config.d.orig	2023-08-31 22:33:21 UTC
 +++ libphobos/libdruntime/core/sys/freebsd/config.d
-@@ -13,8 +13,9 @@ public import core.sys.posix.config;
+@@ -13,8 +13,10 @@ public import core.sys.posix.config;
  // __FreeBSD_version numbers are documented in the Porter's Handbook.
  // NOTE: When adding newer versions of FreeBSD, verify all current versioned
  // bindings are still compatible with the release.
 -     version (FreeBSD_13) enum __FreeBSD_version = 1300000;
 -else version (FreeBSD_12) enum __FreeBSD_version = 1202000;
-+     version (FreeBSD_14) enum __FreeBSD_version = 1400000;
++     version (FreeBSD_15) enum __FreeBSD_version = 1500000;
++else version (FreeBSD_14) enum __FreeBSD_version = 1400000;
 +else version (FreeBSD_13) enum __FreeBSD_version = 1302000;
 +else version (FreeBSD_12) enum __FreeBSD_version = 1204000;
  else version (FreeBSD_11) enum __FreeBSD_version = 1104000;
