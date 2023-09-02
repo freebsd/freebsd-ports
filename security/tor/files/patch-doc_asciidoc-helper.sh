94ed6cbc3e83edb1d7bc48ce7b6251b3cf87365a
Rene Ladan <rene@FreeBSD.org>
2023-09-01 17:47:16 -0700

    security/tor: update 0.4.7.13 → 0.4.8.5
    PR:	273471

--- security/tor/files/patch-doc_asciidoc-helper.sh
+++ security/tor/files/patch-doc_asciidoc-helper.sh
@@ -1,10 +1,12 @@
---- doc/asciidoc-helper.sh.orig	2022-02-04 16:24:11 UTC
+--- doc/asciidoc-helper.sh.orig	2023-08-30 12:58:59 UTC
 +++ doc/asciidoc-helper.sh
-@@ -12,7 +12,6 @@ if [ $# != 3 ]; then
-   exit 1;
+@@ -12,9 +12,6 @@ if [ $# != 3 ]; then
+     exit 1
  fi
  
--SOURCE_DATE_EPOCH="$(git show --no-patch --format='%ct')"
- export SOURCE_DATE_EPOCH
- 
+-SOURCE_DATE_EPOCH="$(git -C "$(dirname "$0")" show --no-patch --format='%ct')"
+-export SOURCE_DATE_EPOCH
+-
  output=$3
+ 
+ if [ "$1" = "html" ]; then
