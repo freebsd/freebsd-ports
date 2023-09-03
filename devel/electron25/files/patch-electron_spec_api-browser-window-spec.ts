73de6f2a40973acb59d8e7b74c95e862fb361c76
Hiroki Tagato <tagattie@FreeBSD.org>
2023-09-03 10:08:03 +0900

    devel/electron25: update to 25.8.0
    Changelog: https://github.com/electron/electron/releases/tag/v25.8.0
    
    Reported by:	GitHub (watch releases)
    MFH:		2023Q3
    Security:	970dcbe0-a947-41a4-abe9-7aaba87f41fe
    
    (cherry picked from commit 0b15a88279d0164d340d40f5a2769c223dedd5ef)

--- devel/electron25/files/patch-electron_spec_api-browser-window-spec.ts
+++ devel/electron25/files/patch-electron_spec_api-browser-window-spec.ts
@@ -1,4 +1,4 @@
---- electron/spec/api-browser-window-spec.ts.orig	2023-08-16 14:00:49 UTC
+--- electron/spec/api-browser-window-spec.ts.orig	2023-08-30 15:08:12 UTC
 +++ electron/spec/api-browser-window-spec.ts
 @@ -65,7 +65,7 @@ describe('BrowserWindow module', () => {
        }).not.to.throw();
@@ -144,7 +144,7 @@
      // Not implemented on Linux.
      afterEach(closeAllWindows);
  
-@@ -6214,7 +6214,7 @@ describe('BrowserWindow module', () => {
+@@ -6250,7 +6250,7 @@ describe('BrowserWindow module', () => {
    describe('"transparent" option', () => {
      afterEach(closeAllWindows);
  
