710a64c4471df9b78b96e7fc26e12c34b104baee
Cy Schubert <cy@FreeBSD.org>
2023-09-03 22:29:12 -0700

    net-mgmt/net-snmp: Fix build for 15-CURRENT
    Add support for 15-CURRENT by adding a freebsd15.h (header) file.
    
    Approved by:	portmgr (blanket)

---
+++ net-mgmt/net-snmp/files/patch-include_net-snmp_system_freebsd15.h
@@ -0,0 +1,6 @@
+--- /dev/null	2023-09-03 22:25:19.482823000 -0700
++++ include/net-snmp/system/freebsd15.h	2023-09-03 22:24:08.956666000 -0700
+@@ -0,0 +1,3 @@
++/* freebsd15 is a superset of freebsd14 */
++#include "freebsd14.h"
++#define freebsd14 freebsd14
