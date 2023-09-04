fc17b5d74f0ff1da995625fe9157a89ceb958c9d
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-09-04 17:06:03 +0800

    textproc/rubygem-yarp: Update to 0.9.0
    Changes:	https://github.com/ruby/yarp/releases

--- textproc/rubygem-yarp/files/patch-ext-yarp-extconf.rb
+++ textproc/rubygem-yarp/files/patch-ext-yarp-extconf.rb
@@ -1,11 +1,11 @@
---- ext/yarp/extconf.rb.orig	2023-08-18 17:14:49 UTC
+--- ext/yarp/extconf.rb.orig	2023-08-29 19:49:53 UTC
 +++ ext/yarp/extconf.rb
-@@ -70,7 +70,7 @@ module Yarp
-             Rake.sh("templates/template.rb")
+@@ -75,7 +75,7 @@ module Yarp
+             # normally we package up the configure and other files in the gem itself
+             system("templates/template.rb", exception: true)
            end
-           Rake.sh("sh", "configure") # explicit "sh" for Windows where shebangs are not supported
--          Rake.sh("make", target)
-+          Rake.sh("gmake", target)
+-          system("make", target, exception: true)
++          system("gmake", target, exception: true)
          end
        end
  
