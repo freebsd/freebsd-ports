40989fdf9b57507d77dbf470b6111250032e91f1
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-09-05 05:25:19 +0800

    databases/rocksdb: Update to 8.5.3
    - Install librocksdb_test.so: it is required by table_reader_bench
    (rocksdb-8.5.3) /wrkdirs/usr/ports/databases/rocksdb/work/stage//usr/local/bin/table_reader_bench - required shared library librocksdb_test.so not found
    
    Changes:	https://github.com/facebook/rocksdb/releases
    (cherry picked from commit 65d51f736c81c048cda3ba44c8495f57050f117f)

--- databases/rocksdb/files/patch-port-stack_trace.cc
+++ databases/rocksdb/files/patch-port-stack_trace.cc
@@ -1,29 +1,10 @@
---- port/stack_trace.cc.orig	2023-02-19 21:44:55 UTC
+--- port/stack_trace.cc.orig	2023-09-01 20:58:39 UTC
 +++ port/stack_trace.cc
-@@ -33,6 +33,8 @@ void* SaveStack(int* /*num_frames*/, int /*first_frame
- 
+@@ -37,6 +37,7 @@ void* SaveStack(int* /*num_frames*/, int /*first_frame
+ #endif  // OS_OPENBSD
  #ifdef OS_FREEBSD
  #include <sys/sysctl.h>
-+#include <sys/types.h>
 +#include <sys/wait.h>
  #endif  // OS_FREEBSD
  #ifdef OS_LINUX
  #include <sys/prctl.h>
-@@ -219,7 +221,7 @@ void PrintStack(int first_frames_to_skip) {
-   void* frames[kMaxFrames];
- 
-   auto num_frames = backtrace(frames, kMaxFrames);
--  PrintStack(&frames[first_frames_to_skip], num_frames - first_frames_to_skip);
-+  PrintStack(&frames[first_frames_to_skip], (int) (num_frames - first_frames_to_skip));
- }
- 
- void PrintAndFreeStack(void* callstack, int num_frames) {
-@@ -232,7 +234,7 @@ void* SaveStack(int* num_frames, int first_frames_to_s
-   void* frames[kMaxFrames];
- 
-   auto count = backtrace(frames, kMaxFrames);
--  *num_frames = count - first_frames_to_skip;
-+  *num_frames = (int) (count - first_frames_to_skip);
-   void* callstack = malloc(sizeof(void*) * *num_frames);
-   memcpy(callstack, &frames[first_frames_to_skip], sizeof(void*) * *num_frames);
-   return callstack;
