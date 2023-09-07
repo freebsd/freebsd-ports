4245767a21694d7351e2b0226b5585dd36041f11
Mikael Urankar <mikael@FreeBSD.org>
2023-09-07 19:52:10 +0200

    net-im/signal-desktop: Update to 6.30.0
    Changes:
      https://github.com/signalapp/Signal-Desktop/releases/tag/v6.30.0

--- net-im/signal-desktop/get_deps.sh
+++ net-im/signal-desktop/get_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SIGNAL_VERS=v6.29.0
+SIGNAL_VERS=v6.30.0
 
 fetch -qo /tmp/package.json https://raw.githubusercontent.com/signalapp/Signal-Desktop/${SIGNAL_VERS}/package.json
 node_version=$(awk /'"node":'/'{print $2}' package.json | sed 's/"//g')
