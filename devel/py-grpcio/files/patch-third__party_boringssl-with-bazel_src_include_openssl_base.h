44d2eabe98eb49443293633da4dd5e514082eb01
Vanilla I. Shu <vanilla@FreeBSD.org>
2023-09-08 22:55:23 +0800

    devel/py-grpcio: update to 1.58.0.

--- devel/py-grpcio/files/patch-third__party_boringssl-with-bazel_src_include_openssl_base.h
+++ devel/py-grpcio/files/patch-third__party_boringssl-with-bazel_src_include_openssl_base.h
@@ -1,6 +1,6 @@
---- third_party/boringssl-with-bazel/src/include/openssl/base.h.orig	2022-05-03 21:27:37 UTC
+--- third_party/boringssl-with-bazel/src/include/openssl/base.h.orig	2023-09-06 01:32:08 UTC
 +++ third_party/boringssl-with-bazel/src/include/openssl/base.h
-@@ -483,9 +483,6 @@ typedef void *OPENSSL_BLOCK;
+@@ -413,9 +413,6 @@ typedef void *OPENSSL_BLOCK;
  // MSVC doesn't set __cplusplus to 201103 to indicate C++11 support (see
  // https://connect.microsoft.com/VisualStudio/feedback/details/763051/a-value-of-predefined-macro-cplusplus-is-still-199711l)
  // so MSVC is just assumed to support C++11.
