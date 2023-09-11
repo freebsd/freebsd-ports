200b969fbb9e17d44f4c9df47767b60bff6bd2ae
Rainer Hurling <rhurlin@FreeBSD.org>
2023-09-11 07:11:14 +0200

    devel/spyder: Relax dependency restriction
    textproc/py-python-lsp-server is now at 1.8.0. Spyder seems to work well with it.

--- devel/spyder/files/patch-setup.py
+++ devel/spyder/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2023-04-05 02:35:28 UTC
+--- setup.py.orig	2023-08-29 21:53:14 UTC
 +++ setup.py
 @@ -91,7 +91,7 @@ def get_data_files():
      """
@@ -17,3 +17,12 @@
  )
  
  
+@@ -229,7 +228,7 @@ install_requires = [
+     'pyls-spyder>=0.4.0',
+     'pyqt5<5.16',
+     'pyqtwebengine<5.16',
+-    'python-lsp-server[all]>=1.7.4,<1.8.0',
++    'python-lsp-server[all]>=1.7.4',
+     'pyxdg>=0.26;platform_system=="Linux"',
+     'pyzmq>=22.1.0',
+     'qdarkstyle>=3.0.2,<3.2.0',
---
+++ devel/spyder/files/patch-spyder_dependencies.py
@@ -0,0 +1,11 @@
+--- spyder/dependencies.py.orig	2023-08-29 21:53:14 UTC
++++ spyder/dependencies.py
+@@ -54,7 +54,7 @@ PYLINT_VENV_REQVER = '>=3.0.2'
+ PYGMENTS_REQVER = '>=2.0'
+ PYLINT_REQVER = '>=2.5.0,<3.0'
+ PYLINT_VENV_REQVER = '>=3.0.2'
+-PYLSP_REQVER = '>=1.7.4,<1.8.0'
++PYLSP_REQVER = '>=1.7.4'
+ PYLSP_BLACK_REQVER = '>=1.2.0,<3.0.0'
+ PYLS_SPYDER_REQVER = '>=0.4.0'
+ PYXDG_REQVER = '>=0.26'
