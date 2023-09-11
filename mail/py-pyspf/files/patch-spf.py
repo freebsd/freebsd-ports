2920ddff74980fdebe12c0ff4d48b067c2e5ebe9
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-09-12 00:05:19 +0800

    mail/py-pyspf: Fix runtime on IPv6
    - Bump PORTREVISION for package change
    
    % spf.py 2610:1c1:1:606c::50:15 FreeBSD.org localhost
    /usr/local/bin/spf.py:131: DeprecationWarning: please use dns.resolver.resolve() instead
      answers = dns.resolver.query(name, qtype, lifetime=timeout)
    result: ('softfail', 250, 'domain owner discourages use of this host') ~all
    
    PR:		268030
    Submitted by:	<takefu@airport.fm>

---
+++ mail/py-pyspf/files/patch-spf.py
@@ -0,0 +1,11 @@
+--- spf.py.orig	2020-01-01 21:02:26 UTC
++++ spf.py
+@@ -128,7 +128,7 @@ def DNSLookup_dnspython(name, qtype, tcpfallback=True,
+     retVal = []
+     try:
+         # FIXME: how to disable TCP fallback in dnspython if not tcpfallback?
+-        answers = dns.resolver.query(name, qtype, lifetime=timeout)
++        answers = dns.resolver.resolve(name, qtype)
+         for rdata in answers:
+             if qtype == 'A' or qtype == 'AAAA':
+                 retVal.append(((name, qtype), rdata.address))
