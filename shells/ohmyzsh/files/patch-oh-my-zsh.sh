dd64a6e7730f8f6d5e261e01b5c7e7dbf4c7eb4a
Serhii (Sergey) Kozlov <skozlov@FreeBSD.org>
2023-09-16 14:07:43 +0000

    shells/ohmyzsh: update to 20230913

--- shells/ohmyzsh/files/patch-oh-my-zsh.sh
+++ shells/ohmyzsh/files/patch-oh-my-zsh.sh
@@ -1,14 +1,11 @@
-Disable auto update
---- oh-my-zsh.sh.orig	2022-01-28 23:08:18 UTC
+--- oh-my-zsh.sh.orig	2023-09-16 14:01:58 UTC
 +++ oh-my-zsh.sh
-@@ -56,11 +56,6 @@ fi
+@@ -65,9 +65,6 @@ mkdir -p "$ZSH_CACHE_DIR/completions"
  mkdir -p "$ZSH_CACHE_DIR/completions"
  (( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
  
 -# Check for updates on initial load...
--if [[ "$DISABLE_AUTO_UPDATE" != true ]]; then
--  source "$ZSH/tools/check_for_upgrade.sh"
--fi
+-source "$ZSH/tools/check_for_upgrade.sh"
 -
  # Initializes Oh My Zsh
  
