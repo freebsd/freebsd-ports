6a6a9ad85d351525e0a974e5e9ae36a30b30155a
Dima Panov <fluffy@FreeBSD.org>
2023-09-25 18:08:32 +0300

    net-im/telegram-desktop: update to 4.9.3 release (+)
    Telegram Desktop 4.9.3 is latest release which supports Qt 5.15.8 before migrating to Qt 5.15.10 API
    
    PR:	274043, 274064

--- net-im/telegram-desktop/files/patch-cmake_external_glib_cppgir_tools_function.hpp
+++
@@ -1,8 +0,0 @@
---- cmake/external/glib/cppgir/tools/function.hpp.orig	2023-06-15 00:41:50 UTC
-+++ cmake/external/glib/cppgir/tools/function.hpp
-@@ -1,3 +1,5 @@
-+#include <map>
-+
- #ifndef FUNCTION_HPP
- #define FUNCTION_HPP
- 
--- net-im/telegram-desktop/files/patch-cmake_external_glib_cppgir_tools_genbase.cpp
+++
@@ -1,8 +0,0 @@
---- cmake/external/glib/cppgir/tools/genbase.cpp.orig	2023-06-15 00:42:39 UTC
-+++ cmake/external/glib/cppgir/tools/genbase.cpp
-@@ -1,3 +1,5 @@
-+#include <map>
-+
- #include "genbase.hpp"
- 
- static std::string
