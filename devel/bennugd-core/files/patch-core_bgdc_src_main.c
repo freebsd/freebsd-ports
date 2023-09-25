6a93dda0a7104bee4b5fa0c78f579f57f7eaf52a
Dmitry Marakasov <amdmi3@FreeBSD.org>
2023-09-25 20:26:07 +0300

    devel/bennugd-core: fix build on 14.x

---
+++ devel/bennugd-core/files/patch-core_bgdc_src_main.c
@@ -0,0 +1,10 @@
+--- core/bgdc/src/main.c.orig	2013-01-06 01:50:56 UTC
++++ core/bgdc/src/main.c
+@@ -29,6 +29,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
++#include <unistd.h>
+ 
+ #include <time.h>
+ 
