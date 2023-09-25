2781931b6ffda70f3f1dc8123d06444d8a39f054
Guido Falsi <madpilot@FreeBSD.org>
2023-09-25 14:56:26 +0200

    finance/gnucash-docs: Update to 5.4
    - Remove unneeded dependency.

--- finance/gnucash-docs/files/patch-cmake_AddXdgHelpTarget.cmake
+++ finance/gnucash-docs/files/patch-cmake_AddXdgHelpTarget.cmake
@@ -1,17 +1,17 @@
---- cmake/AddXdgHelpTarget.cmake.orig	2022-12-17 21:04:07 UTC
+--- cmake/AddXdgHelpTarget.cmake.orig	2023-09-05 01:13:47 UTC
 +++ cmake/AddXdgHelpTarget.cmake
-@@ -44,8 +44,8 @@ function (add_xdghelp_target docname lang entities fig
+@@ -45,8 +45,8 @@ function (add_xdghelp_target targetbase lang entities 
        COMMAND ${CMAKE_COMMAND} -E make_directory "${BUILD_DIR}/figures"
  
        # Add links to make our documentation visible for KDE's help system
--      COMMAND ${CMAKE_COMMAND} -E make_directory "${BUILD_DIR_KDE_BASE}"
--      COMMAND ${CMAKE_COMMAND} -E create_symlink "${BUILD_DIR_BASE}" "${BUILD_DIR_KDE_BASE}/${kde_lang}"
-+      # COMMAND ${CMAKE_COMMAND} -E make_directory "${BUILD_DIR_KDE_BASE}"
-+      # COMMAND ${CMAKE_COMMAND} -E create_symlink "${BUILD_DIR_BASE}" "${BUILD_DIR_KDE_BASE}/${kde_lang}"
+-      COMMAND ${CMAKE_COMMAND} -E make_directory "${BUILD_DIR_KDE_BASE}/${kde_lang}"
+-      COMMAND ${CMAKE_COMMAND} -E create_symlink "${BUILD_DIR}" "${BUILD_DIR_KDE_BASE}/${kde_lang}/${docname}"
++      # COMMAND ${CMAKE_COMMAND} -E make_directory "${BUILD_DIR_KDE_BASE}/${kde_lang}"
++      # COMMAND ${CMAKE_COMMAND} -E create_symlink "${BUILD_DIR}" "${BUILD_DIR_KDE_BASE}/${kde_lang}/${docname}"
        COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/xdghelptrigger")
  
  
-@@ -90,8 +90,8 @@ function (add_xdghelp_target docname lang entities fig
+@@ -91,8 +91,8 @@ function (add_xdghelp_target targetbase lang entities 
  
      # Add links to make our documentation visible for KDE's help system
      set(doc_install_dir_kde_base "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_DATADIR}/doc/HTML")
