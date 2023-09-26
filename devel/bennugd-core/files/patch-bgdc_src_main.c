c4a9b245b9cad213011990cc07fa377cd101e0ef
Dmitry Marakasov <amdmi3@FreeBSD.org>
2023-09-26 21:35:34 +0300

    devel/bennugd-core: fix patch

--- devel/bennugd-core/files/patch-bgdc_src_main.c
+++ devel/bennugd-core/files/patch-bgdc_src_main.c
@@ -1,5 +1,5 @@
---- core/bgdc/src/main.c.orig	2013-01-06 01:50:56 UTC
-+++ core/bgdc/src/main.c
+--- bgdc/src/main.c.orig	2013-01-06 01:50:56 UTC
++++ bgdc/src/main.c
 @@ -29,6 +29,7 @@
  #include <stdio.h>
  #include <stdlib.h>
