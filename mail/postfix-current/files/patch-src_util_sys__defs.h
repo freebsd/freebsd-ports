f9772d176717f460b7342110dab86aa2393a5a93
Herbert J. Skuhra <herbert@gojira.at>
2023-09-27 18:44:39 +0200

    mail/postfix-current: Update to 3.9-20230924
    - Update to 3.9-20230924
    - Add FreeBSD 15 defines
    
    PR:		273784

---
+++ mail/postfix-current/files/patch-src_util_sys__defs.h
@@ -0,0 +1,11 @@
+--- src/util/sys_defs.h.orig	2023-09-27 18:45:38 UTC
++++ src/util/sys_defs.h
+@@ -31,7 +31,7 @@
+     || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
+     || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
+     || defined(FREEBSD11) || defined(FREEBSD12) || defined(FREEBSD13) \
+-    || defined(FREEBSD14) \
++    || defined(FREEBSD14) || defined(FREEBSD15) \
+     || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
+     || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
+     || defined(OPENBSD5) || defined(OPENBSD6) || defined(OPENBSD7) \
