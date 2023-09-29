c47361598b73b5e6e180b3ee1bbbaca833dbac63
Robert Nagy <rnagy@FreeBSD.org>
2023-09-29 12:30:53 +0200

    www/*chromium: update to 117.0.5938.132
    Security:	https://vuxml.freebsd.org/freebsd/6d9c6aae-5eb1-11ee-8290-a8a1599412c6.html
    PR:		274139, 274136

--- www/ungoogled-chromium/files/patch-net_dns_address__info.cc
+++ www/ungoogled-chromium/files/patch-net_dns_address__info.cc
@@ -1,11 +1,16 @@
---- net/dns/address_info.cc.orig	2023-09-17 07:59:53 UTC
+--- net/dns/address_info.cc.orig	2023-09-29 10:30:11 UTC
 +++ net/dns/address_info.cc
-@@ -77,7 +77,7 @@ AddressInfo::AddressInfoAndResult AddressInfo::Get(
+@@ -77,8 +77,12 @@ AddressInfo::AddressInfoAndResult AddressInfo::Get(
      // error.
      // http://crbug.com/134142
      err = ERR_NAME_NOT_RESOLVED;
 -#elif BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_FREEBSD)
 +#elif BUILDFLAG(IS_POSIX)
++#ifdef EAI_NODATA
      if (os_error != EAI_NONAME && os_error != EAI_NODATA)
++#else
++    if (os_error != EAI_NONAME)
++#endif
        err = ERR_NAME_RESOLUTION_FAILED;
  #endif
+ 
