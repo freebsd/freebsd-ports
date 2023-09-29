e55df4c8f8e0abf4f83177d75c24ffacfef2fd11
Robert Nagy <rnagy@FreeBSD.org>
2023-09-29 12:30:53 +0200

    www/*chromium: update to 117.0.5938.132
    Security:	https://vuxml.freebsd.org/freebsd/6d9c6aae-5eb1-11ee-8290-a8a1599412c6.html
    PR:		274139, 274136
    (cherry picked from commit c47361598b73b5e6e180b3ee1bbbaca833dbac63)

--- www/ungoogled-chromium/files/patch-components_password__manager_core_browser_login__database.cc
+++ www/ungoogled-chromium/files/patch-components_password__manager_core_browser_login__database.cc
@@ -1,6 +1,6 @@
---- components/password_manager/core/browser/login_database.cc.orig	2023-09-17 07:59:53 UTC
+--- components/password_manager/core/browser/login_database.cc.orig	2023-09-29 10:30:11 UTC
 +++ components/password_manager/core/browser/login_database.cc
-@@ -960,7 +960,7 @@ std::string GeneratePlaceholders(size_t count) {
+@@ -1046,7 +1046,7 @@ std::string GeneratePlaceholders(size_t count) {
    return result;
  }
  
@@ -9,7 +9,7 @@
  // Fills |form| with necessary data required to be removed from the database
  // and returns it.
  PasswordForm GetFormForRemoval(sql::Statement& statement) {
-@@ -977,7 +977,7 @@ PasswordForm GetFormForRemoval(sql::Statement& stateme
+@@ -1063,7 +1063,7 @@ PasswordForm GetFormForRemoval(sql::Statement& stateme
  // Whether we should try to return the decryptable passwords while the
  // encryption service fails for some passwords.
  bool ShouldReturnPartialPasswords() {
@@ -18,7 +18,7 @@
    return base::FeatureList::IsEnabled(features::kSkipUndecryptablePasswords);
  #else
    return false;
-@@ -1832,7 +1832,7 @@ bool LoginDatabase::DeleteAndRecreateDatabaseFile() {
+@@ -1918,7 +1918,7 @@ bool LoginDatabase::DeleteAndRecreateDatabaseFile() {
  }
  
  DatabaseCleanupResult LoginDatabase::DeleteUndecryptableLogins() {
