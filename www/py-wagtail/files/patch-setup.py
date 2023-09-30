a38956d6d26750edd89012690587771f69b5af32
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-09-30 08:23:15 +0800

    www/py-wagtail: Update to 5.0.3
    Changes:	https://github.com/wagtail/wagtail/releases

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,14 +1,14 @@
---- setup.py.orig	2023-06-21 13:35:13 UTC
+--- setup.py.orig	2023-09-25 12:43:44 UTC
 +++ setup.py
 @@ -26,9 +26,9 @@ install_requires = [
      "django-treebeard>=4.5.1,<5.0",
      "djangorestframework>=3.11.1,<4.0",
      "django-filter>=2.2,<23",
 -    "draftjs_exporter>=2.1.5,<3.0",
--    "Pillow>=4.0.0,<10.0.0",
+-    "Pillow>=9.1.0,<10.0.0",
 -    "beautifulsoup4>=4.8,<4.12",
 +    "draftjs_exporter>=2.1.5",
-+    "Pillow>=4.0.0,<11.0.0",
++    "Pillow>=9.1.0,<11.0.0",
 +    "beautifulsoup4>=4.8,<4.13",
      "html5lib>=0.999,<2",
      "Willow>=1.5,<1.6",
