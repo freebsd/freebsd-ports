c076d2ad15483c152049d2e80091a1ccd4c5242e
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2023-10-02 11:13:26 -0500

    net/py-impacket: Update to 0.11.0
    - Take maintainership. Maintainer not show activity from 2019 and this port was
      marked like vulnerable from 2023-04-09
    
    Approved by:	maintainer timeout

--- net/py-impacket/files/patch-setup.py
+++ net/py-impacket/files/patch-setup.py
@@ -1,22 +1,23 @@
---- setup.py.orig	2022-05-21 20:29:42 UTC
-+++ setup.py
-@@ -10,11 +10,6 @@ from setuptools import setup
- 
- PACKAGE_NAME = "impacket"
+--- setup.py.orig	2023-09-21 00:08:28.427651000 -0500
++++ setup.py	2023-09-21 00:09:29.055566000 -0500
+@@ -39,12 +39,6 @@
+ except Exception:
+     VER_LOCAL = ""
  
 -if platform.system() != 'Darwin':
 -    data_files = [(os.path.join('share', 'doc', PACKAGE_NAME), ['README.md', 'LICENSE']+glob.glob('doc/*'))]
 -else:
 -    data_files = []
 -
+-
  def read(fname):
      return open(os.path.join(os.path.dirname(__file__), fname)).read()
  
-@@ -36,7 +31,6 @@ setup(name = PACKAGE_NAME,
-                 'impacket.examples.ntlmrelayx.servers.socksplugins', 'impacket.examples.ntlmrelayx.utils',
-                 'impacket.examples.ntlmrelayx.attacks'],
-       scripts = glob.glob(os.path.join('examples', '*.py')),
--      data_files = data_files,
-       install_requires=['pyasn1>=0.2.3', 'pycrypto>=2.6.1', 'pyOpenSSL>=0.13.1', 'six', 'ldap3>=2.5.0', 'ldapdomaindump', 'flask'],
-       extras_require={
-                       'pyreadline:sys_platform=="win32"': [],
+@@ -66,7 +60,6 @@
+               'impacket.examples.ntlmrelayx.servers.socksplugins', 'impacket.examples.ntlmrelayx.utils',
+               'impacket.examples.ntlmrelayx.attacks', 'impacket.examples.ntlmrelayx.attacks.httpattacks'],
+     scripts=glob.glob(os.path.join('examples', '*.py')),
+-    data_files=data_files,
+     install_requires=['pyasn1>=0.2.3', 'pycryptodomex', 'pyOpenSSL>=21.0.0', 'six', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6',
+                       'ldapdomaindump>=0.9.0', 'flask>=1.0', 'future', 'charset_normalizer', 'dsinternals'],
+     extras_require={'pyreadline:sys_platform=="win32"': [],
