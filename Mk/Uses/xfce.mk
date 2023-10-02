0afc8661e4a56c97828d88575c4bf3e7f7d3a768
Guido Falsi <madpilot@FreeBSD.org>
2023-10-02 17:53:42 +0200

    XFCE: Cleanup dependencies
    - Update xfce.mk to avid forcing too many dependencies on consumers
    - Change dependency on garcon to reference libgarcon-1.so (main
      library)
    - Explicitly add USE_XFCE components to the ports actually needing
      them.
    - Cleanup dependencies, adding/removing missing/extra dependencies
    - Change the referenced library in some LIB_DEPENDS to the actually
      linked one (avoids false positives in poudriere Q/A tests)
    - Bump PORTREVISION on all touched ports, even indirectly due to
      the effect of removing forced dependencies on certain components
    - Remove USES=fuse from gigolo. I could not find any kind f reference
      to fuse in the code.
    - xfce4-bsdcpufreq-plugin: Also remove empty comment at the top of
      the Makefile

--- Mk/Uses/xfce.mk
+++ Mk/Uses/xfce.mk
@@ -23,7 +23,7 @@ LIBS+=	-L${LOCALBASE}/lib
 libmenu_LIB_DEPENDS=	libxfce4ui-2.so:x11/libxfce4menu
 libmenu_USE_XFCE_REQ=	libutil
 
-garcon_LIB_DEPENDS=	libgarcon-gtk3-1.so:sysutils/garcon
+garcon_LIB_DEPENDS=	libgarcon-1.so:sysutils/garcon
 garcon_USE_XFCE_REQ=	libmenu
 
 libexo_LIB_DEPENDS=	libexo-2.so:x11/libexo
@@ -31,16 +31,13 @@ libexo_USE_XFCE_REQ=	libmenu
 
 panel_LIB_DEPENDS=	libxfce4panel-2.0.so:x11-wm/xfce4-panel
 panel_RUN_DEPENDS=	xfce4-panel:x11-wm/xfce4-panel
-panel_USE_XFCE_REQ=	garcon libexo xfconf
 
 libutil_LIB_DEPENDS=	libxfce4util.so:x11/libxfce4util
 
 thunar_LIB_DEPENDS=	libthunarx-3.so:x11-fm/thunar
 thunar_RUN_DEPENDS=	Thunar:x11-fm/thunar
-thunar_USE_XFCE_REQ=	xfconf libmenu
 
 xfconf_LIB_DEPENDS=	libxfconf-0.so:x11/xfce4-conf
-xfconf_USE_XFCE_REQ=	libutil
 
 .  if defined(USE_XFCE)
 # First, expand all USE_XFCE_REQ recursively.
