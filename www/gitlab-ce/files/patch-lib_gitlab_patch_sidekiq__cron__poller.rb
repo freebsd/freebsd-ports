3083687cbace383da3247089d07360d4be36033d
Matthias Fechner <mfechner@FreeBSD.org>
2023-10-05 15:57:59 +0300

    www/gitlab-ce: fix problem with rubygem-sidekiq 6.5.10
    Fix a regression instroduced by:
    aa93117abe2109fc18dda351690beb5358bb8c95

--- www/gitlab-ce/files/patch-lib_gitlab_patch_sidekiq__cron__poller.rb
+++ www/gitlab-ce/files/patch-lib_gitlab_patch_sidekiq__cron__poller.rb
@@ -5,7 +5,7 @@
  require 'sidekiq/cron/version'
  
 -if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.7')
-+if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.9')
++if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.10')
    raise 'New version of sidekiq detected, please remove or update this patch'
  end
  
