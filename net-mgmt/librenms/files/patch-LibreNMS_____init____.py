73922d6b363a6902a05721d9102386ce657b03d4
Dan Langille <dvl@FreeBSD.org>
2023-10-07 11:49:41 +0000

    net-mgmt/librenms: Fix start on FreeBSD 14/nginx
    This may be a path issue or a shell issue, but nginx on FreeBSD 14
    is not finding /usr/local/bin/php. The fix: use /usr/local/bin/php
    as we have done with other patches.
    
    PR:		274248

--- net-mgmt/librenms/files/patch-LibreNMS_____init____.py
+++ net-mgmt/librenms/files/patch-LibreNMS_____init____.py
@@ -1,4 +1,4 @@
---- LibreNMS/__init__.py.orig	2023-10-04 13:52:17 UTC
+--- LibreNMS/__init__.py.orig	2023-09-19 01:59:06 UTC
 +++ LibreNMS/__init__.py
 @@ -167,7 +167,7 @@ def get_config_data(base_dir):
          )
@@ -9,3 +9,12 @@
      try:
          exit_code, output = command_runner(config_cmd, timeout=300, stderr=False)
          if exit_code != 0:
+@@ -194,7 +194,7 @@ def call_script(script, args=()):
+     """
+     if script.endswith(".php"):
+         # save calling the sh process
+-        base = ("/usr/bin/env", "php")
++        base = ("/usr/local/bin/php",)
+     else:
+         base = ()
+ 
