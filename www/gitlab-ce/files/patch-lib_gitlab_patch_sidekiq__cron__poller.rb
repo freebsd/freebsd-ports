aff9c1bfd6018221c078c4860dd9409cf651427e
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-10-08 19:56:45 +0800

    www/gitlab-ce: Allow run with rubygem-sidekiq6 6.5.11
    - Bump PORTREVISION for package change
    
    Note that it is an upstream issue.
    Upstream has conflicting requirement for sidekiq which causes runtime issue.
    
    from Gemfile:
    gem 'sidekiq', '~> 6.5.7'
    
    from lib/gitlab/patch/sidekiq_cron_poller.rb:
    if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.7')
      raise 'New version of sidekiq detected, please remove or update this patch'
    end

--- www/gitlab-ce/files/patch-lib_gitlab_patch_sidekiq__cron__poller.rb
+++ www/gitlab-ce/files/patch-lib_gitlab_patch_sidekiq__cron__poller.rb
@@ -5,7 +5,7 @@
  require 'sidekiq/cron/version'
  
 -if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.7')
-+if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.10')
++if Gem::Version.new(Sidekiq::VERSION) != Gem::Version.new('6.5.11')
    raise 'New version of sidekiq detected, please remove or update this patch'
  end
  
