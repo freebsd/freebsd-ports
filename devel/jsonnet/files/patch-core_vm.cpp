ec733213974264ea5602ec6f7aa7dad1046ec47f
Pavel Timofeev <timp87@gmail.com>
2023-10-11 16:03:08 +0200

    devel/jsonnet: update to v0.20.0
    ChangeLogs:
     https://github.com/google/jsonnet/releases/tag/v0.19.0
     https://github.com/google/jsonnet/releases/tag/v0.19.1
     https://github.com/google/jsonnet/releases/tag/v0.20.0
    
    PR:		274397
    Reported by:	timp87@gmail.com
    Approved by:	gasol.wu@gmail.com (maintainer)

--- devel/jsonnet/files/patch-core_vm.cpp
+++ devel/jsonnet/files/patch-core_vm.cpp
@@ -1,4 +1,4 @@
---- core/vm.cpp.orig	2021-12-21 17:55:49 UTC
+--- core/vm.cpp.orig	2023-04-17 19:02:16 UTC
 +++ core/vm.cpp
 @@ -23,7 +23,7 @@ limitations under the License.
  
@@ -9,3 +9,14 @@
  #include "md5.h"
  #include "parser.h"
  #include "ryml_std.hpp" // include this before any other ryml header
+@@ -1710,6 +1710,10 @@ class Interpreter {
+             } break;
+ 
+             case json::value_t::discarded: {
++                abort();
++            }
++
++            case json::value_t::binary: {
+                 abort();
+             }
+         }
