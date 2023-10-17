50936df61c8206049839a0040734059703e529b9
Wen Heping <wen@FreeBSD.org>
2023-10-16 19:25:49 -0700

    misc/py-pytorch: update 2.0.1 → 2.1.0
    PR:	274514

--- misc/py-pytorch/files/patch-aten_src_ATen_cpu_vec_vec256_vec256__bfloat16.h
+++ misc/py-pytorch/files/patch-aten_src_ATen_cpu_vec_vec256_vec256__bfloat16.h
@@ -1,11 +1,11 @@
---- aten/src/ATen/cpu/vec/vec256/vec256_bfloat16.h.orig	2023-05-07 16:59:15 UTC
+--- aten/src/ATen/cpu/vec/vec256/vec256_bfloat16.h.orig	2023-10-12 12:49:42 UTC
 +++ aten/src/ATen/cpu/vec/vec256/vec256_bfloat16.h
-@@ -206,7 +206,7 @@ template <> class Vectorized<BFloat16> { (public)
+@@ -266,7 +266,7 @@ static_assert( (public)
      }
      return b;
    }
--  Vectorized<BFloat16> map(const __m256 (*const vop)(__m256)) const {
-+  Vectorized<BFloat16> map(__m256 (*const vop)(__m256)) const {
+-  Vectorized<T> map(const __m256 (*const vop)(__m256)) const {
++  Vectorized<T> map(__m256 (*const vop)(__m256)) const {
      __m256 lo, hi;
-     cvtbf16_fp32(values, lo, hi);
+     cvt_to_fp32<T>(values, lo, hi);
      const auto o1 = vop(lo);
--- misc/py-pytorch/files/patch-aten_src_ATen_cpu_vec_vec512_vec512__bfloat16.h
+++ misc/py-pytorch/files/patch-aten_src_ATen_cpu_vec_vec512_vec512__bfloat16.h
@@ -1,11 +1,11 @@
---- aten/src/ATen/cpu/vec/vec512/vec512_bfloat16.h.orig	2023-05-07 17:07:36 UTC
+--- aten/src/ATen/cpu/vec/vec512/vec512_bfloat16.h.orig	2023-10-12 12:54:40 UTC
 +++ aten/src/ATen/cpu/vec/vec512/vec512_bfloat16.h
-@@ -283,7 +283,7 @@ template <> class Vectorized<BFloat16> { (public)
+@@ -345,7 +345,7 @@ static_assert( (public)
    }
    #pragma clang diagnostic push
    #pragma clang diagnostic ignored "-Wignored-qualifiers"
--  Vectorized<BFloat16> map(const __m512 (*const vop)(__m512)) const {
-+  Vectorized<BFloat16> map(__m512 (*const vop)(__m512)) const {
+-  Vectorized<T> map(const __m512 (*const vop)(__m512)) const {
++  Vectorized<T> map(__m512 (*const vop)(__m512)) const {
      __m512 lo, hi;
-     cvtbf16_fp32(values, lo, hi);
+     cvt_to_fp32<T>(values, lo, hi);
      const auto o1 = vop(lo);
