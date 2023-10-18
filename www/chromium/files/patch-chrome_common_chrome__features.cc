0b94f89b6a99c5f74658d7a282c31e9fcba505ec
Robert Nagy <rnagy@FreeBSD.org>
2023-10-18 15:31:49 +0200

    www/chromium: update to 118.0.5993.88

--- www/chromium/files/patch-chrome_common_chrome__features.cc
+++ www/chromium/files/patch-chrome_common_chrome__features.cc
@@ -1,5 +1,14 @@
---- chrome/common/chrome_features.cc.orig	2023-10-11 18:22:24 UTC
+--- chrome/common/chrome_features.cc.orig	2023-10-18 13:29:25 UTC
 +++ chrome/common/chrome_features.cc
+@@ -76,7 +76,7 @@ BASE_FEATURE(kAppShimNotificationAttribution,
+ BASE_FEATURE(kAsyncDns,
+              "AsyncDns",
+ #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_ANDROID) || \
+-    BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
++    BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+              base::FEATURE_ENABLED_BY_DEFAULT
+ #else
+              base::FEATURE_DISABLED_BY_DEFAULT
 @@ -84,7 +84,7 @@ BASE_FEATURE(kAsyncDns,
  );
  
