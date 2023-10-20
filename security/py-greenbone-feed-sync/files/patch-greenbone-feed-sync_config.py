bff9bc0e00371f32ea06bed1f78e09ec4ccbe826
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2023-10-20 18:49:47 -0500

    security/gvm: Update gvm-libs to 22.7.3 and openvas to 22.7.6
    - Bump PORTREVISION
    - Update security/py-python-gvm to 23.10.0
    - Fix issues with config path of security/py-notus-scanner and
      security/py-greenbone-feed-sync [1]
    - Improve pkg-message.in from security/gvm [1]
    
    Reported by:	Luciano Mannucci <luciano at_ vespaperitivo.it> [1]

--- security/py-greenbone-feed-sync/files/patch-greenbone-feed-sync_config.py
+++ security/py-greenbone-feed-sync/files/patch-greenbone-feed-sync_config.py
@@ -1,12 +1,13 @@
---- greenbone/feed/sync/config.py	2023-09-21 01:01:53.391475000 -0500
-+++ greenbone/feed/sync/config.py	2023-09-21 01:02:58.557438000 -0500
+--- greenbone/feed/sync/config.py	2023-08-31 07:57:04.000000000 -0500
++++ greenbone/feed/sync/config.py	2023-10-20 10:18:44.157521000 -0500
 @@ -69,10 +69,10 @@
  DEFAULT_GVMD_LOCK_FILE_PATH = "gvm/feed-update.lock"
  DEFAULT_OPENVAS_LOCK_FILE_PATH = "openvas/feed-update.lock"
  
 -DEFAULT_CONFIG_FILE = "/etc/gvm/greenbone-feed-sync.toml"
+-DEFAULT_USER_CONFIG_FILE = "~/.config/greenbone-feed-sync.toml"
 +DEFAULT_CONFIG_FILE = "%%LOCALBASE%%/etc/gvm/greenbone-feed-sync.toml"
- DEFAULT_USER_CONFIG_FILE = "~/.config/greenbone-feed-sync.toml"
++DEFAULT_USER_CONFIG_FILE = "%%LOCALBASE%%/etc/gvm/greenbone-feed-sync.toml"
  
 -DEFAULT_ENTERPRISE_KEY_PATH = "/etc/gvm/greenbone-enterprise-feed-key"
 +DEFAULT_ENTERPRISE_KEY_PATH = "%%LOCALBASE%%/etc/gvm/greenbone-enterprise-feed-key"
