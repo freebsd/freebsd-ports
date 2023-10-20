f851f5d9dac4f093ed436154538278805f31b662
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2023-10-20 18:49:47 -0500

    security/gvm: Update gvm-libs to 22.7.3 and openvas to 22.7.6
    - Bump PORTREVISION
    - Update security/py-python-gvm to 23.10.0
    - Fix issues with config path of security/py-notus-scanner and
      security/py-greenbone-feed-sync [1]
    - Improve pkg-message.in from security/gvm [1]
    
    Reported by:	Luciano Mannucci <luciano at_ vespaperitivo.it> [1]

--- security/py-notus-scanner/files/patch-notus-scanner-cli_parser.py
+++ security/py-notus-scanner/files/patch-notus-scanner-cli_parser.py
@@ -1,11 +1,14 @@
---- notus/scanner/cli/parser.py	2022-07-31 23:45:49.051030000 -0500
-+++ notus/scanner/cli/parser.py	2022-07-31 23:46:07.662584000 -0500
-@@ -29,7 +29,7 @@
+--- notus/scanner/cli/parser.py	2023-08-30 03:14:56.000000000 -0500
++++ notus/scanner/cli/parser.py	2023-10-20 18:26:27.530523000 -0500
+@@ -16,9 +16,8 @@
  
  logger = logging.getLogger(__name__)
  
 -DEFAULT_CONFIG_FILE = "/etc/gvm/notus-scanner.toml"
+-DEFAULT_USER_CONFIG_FILE = "~/.config/notus-scanner.toml"
+-
 +DEFAULT_CONFIG_FILE = "%%LOCALBASE%%/etc/gvm/notus-scanner.toml"
- DEFAULT_USER_CONFIG_FILE = "~/.config/notus-scanner.toml"
++DEFAULT_USER_CONFIG_FILE = "%%LOCALBASE%%/etc/gvm/notus-scanner.toml"
  
- 
+ def log_level(string: str) -> str:
+     """Check if provided string is a valid log level."""
