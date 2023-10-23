b47b76304ccebb0a40acd56f1687e232f0ebedeb
Mark Willson <cdr.nil@gmail.com>
2023-10-23 09:45:40 +0100

    editors/mg: Update to OpenBSD 7.4 version; modify location of source tarball
    Changes in this release:
    
      * New command set-tab-width to change the tabulator width on a per-buffer
        basis.
      * Let the space-to-tabstop command move to the right position even if
        the line contains tabs, control characters, or non-ASCII bytes.
      * Fall back to /bin/sh if $SHELL is undefined.
      * Fix parsing of tag files with duplicate entries. Instead of erroring
        out, ignore duplicates. Fixes using /var/db/libc.tags again.
      * Change the visit-tags-table command to immediately load the tag
        file, and drop the lazy mechanics.
      * Do not leak memory in pop-tag-mark if it fails to switch buffers.
      * Fix a read buffer overrun caused by -u arguments longer than 1023 bytes.
      * Fix a write buffer overrun on the stack caused by blink-and-insert
        matching a very long line that is not currently visible in the
        window.
      * Skip checking permissions of conffile with access(2).
      * Resurrect no-tab-mode and add it to the list of modes that can be
        set with set-default-mode.
    
    From: https://www.openbsd.org/74.html
    
    PR:		274611

---
+++ editors/mg/files/patch-tags.c
@@ -0,0 +1,11 @@
+--- tags.c.orig	2023-10-22 13:42:02 UTC
++++ tags.c
+@@ -18,7 +18,7 @@
+ #include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
+-#include <util.h>
++#include <libutil.h>
+ 
+ #include "def.h"
+ 
