272042874f2e478be2817fd397f12f9642fee1b4
Hiroki Tagato <tagattie@FreeBSD.org>
2023-10-23 18:00:44 +0900

    devel/electron26: update to 26.4.1
    Changelog: https://github.com/electron/electron/releases/tag/v26.4.1
    
    Reported by:	GitHub (watch releases)
    Security:	9000591b-483b-45ac-9c87-b3df3a4198ec

--- devel/electron26/files/patch-electron_shell_browser_api_electron__api__web__contents.cc
+++ devel/electron26/files/patch-electron_shell_browser_api_electron__api__web__contents.cc
@@ -1,4 +1,4 @@
---- electron/shell/browser/api/electron_api_web_contents.cc.orig	2023-10-10 23:44:27 UTC
+--- electron/shell/browser/api/electron_api_web_contents.cc.orig	2023-10-18 09:32:19 UTC
 +++ electron/shell/browser/api/electron_api_web_contents.cc
 @@ -155,11 +155,11 @@
  #include "ui/base/cocoa/defaults_utils.h"
@@ -14,7 +14,7 @@
  #include "ui/gfx/font_render_params.h"
  #endif
  
-@@ -491,7 +491,7 @@ absl::optional<base::TimeDelta> GetCursorBlinkInterval
+@@ -498,7 +498,7 @@ absl::optional<base::TimeDelta> GetCursorBlinkInterval
        ui::TextInsertionCaretBlinkPeriodFromDefaults());
    if (system_value)
      return *system_value;
@@ -23,7 +23,7 @@
    if (auto* linux_ui = ui::LinuxUi::instance())
      return linux_ui->GetCursorBlinkInterval();
  #elif BUILDFLAG(IS_WIN)
-@@ -946,7 +946,7 @@ void WebContents::InitWithSessionAndOptions(
+@@ -953,7 +953,7 @@ void WebContents::InitWithSessionAndOptions(
    accept_languages.pop_back();
    prefs->accept_languages = accept_languages;
  
@@ -32,7 +32,7 @@
    // Update font settings.
    static const gfx::FontRenderParams params(
        gfx::GetFontRenderParams(gfx::FontRenderParamsQuery(), nullptr));
-@@ -2619,7 +2619,7 @@ void WebContents::ForcefullyCrashRenderer() {
+@@ -2626,7 +2626,7 @@ void WebContents::ForcefullyCrashRenderer() {
  
    content::RenderProcessHost* rph = rwh->GetProcess();
    if (rph) {
@@ -41,7 +41,7 @@
      // A generic |CrashDumpHungChildProcess()| is not implemented for Linux.
      // Instead we send an explicit IPC to crash on the renderer's IO thread.
      rph->ForceCrash();
-@@ -3282,7 +3282,7 @@ void WebContents::CopyImageAt(int x, int y) {
+@@ -3289,7 +3289,7 @@ void WebContents::CopyImageAt(int x, int y) {
  void WebContents::Focus() {
    // Focusing on WebContents does not automatically focus the window on macOS
    // and Linux, do it manually to match the behavior on Windows.
@@ -50,7 +50,7 @@
    if (owner_window())
      owner_window()->Focus(true);
  #endif
-@@ -4096,7 +4096,7 @@ ui::ImageModel WebContents::GetDevToolsWindowIcon() {
+@@ -4097,7 +4097,7 @@ ui::ImageModel WebContents::GetDevToolsWindowIcon() {
  }
  #endif
  
