4fd9620ae253e35362b65f411408fc3fc1eaccde
Robert Nagy <rnagy@FreeBSD.org>
2023-10-27 20:35:46 +0200

    www/*chromium: update to 118.0.5993.117
    Security:	https://vuxml.freebsd.org/freebsd/db33e250-74f7-11ee-8290-a8a1599412c6.html

--- www/chromium/files/patch-components_password__manager_core_common_password__manager__features.cc
+++ www/chromium/files/patch-components_password__manager_core_common_password__manager__features.cc
@@ -1,4 +1,4 @@
---- components/password_manager/core/common/password_manager_features.cc.orig	2023-10-11 18:22:24 UTC
+--- components/password_manager/core/common/password_manager_features.cc.orig	2023-10-27 05:38:38 UTC
 +++ components/password_manager/core/common/password_manager_features.cc
 @@ -18,7 +18,7 @@ BASE_FEATURE(kEnableOverwritingPlaceholderUsernames,
               "EnableOverwritingPlaceholderUsernames",
@@ -9,12 +9,3 @@
  // When enabled, initial sync will be forced during startup if the password
  // store has encryption service failures.
  BASE_FEATURE(kForceInitialSyncWhenDecryptionFails,
-@@ -99,7 +99,7 @@ BASE_FEATURE(kRecoverFromNeverSaveAndroid,
-              "RecoverFromNeverSaveAndroid_LAUNCHED",
-              base::FEATURE_ENABLED_BY_DEFAULT);
- 
--#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
- // Displays at least the decryptable and never saved logins in the password
- // manager
- BASE_FEATURE(kSkipUndecryptablePasswords,
--- www/chromium/files/patch-components_password__manager_core_common_password__manager__features.h
+++ www/chromium/files/patch-components_password__manager_core_common_password__manager__features.h
@@ -1,4 +1,4 @@
---- components/password_manager/core/common/password_manager_features.h.orig	2023-10-11 18:22:24 UTC
+--- components/password_manager/core/common/password_manager_features.h.orig	2023-10-27 05:38:38 UTC
 +++ components/password_manager/core/common/password_manager_features.h
 @@ -24,7 +24,7 @@ namespace password_manager::features {
  // alongside the definition of their values in the .cc file.
@@ -9,12 +9,3 @@
  BASE_DECLARE_FEATURE(kForceInitialSyncWhenDecryptionFails);
  #endif
  BASE_DECLARE_FEATURE(kForgotPasswordFormSupport);
-@@ -43,7 +43,7 @@ BASE_DECLARE_FEATURE(kPasswordGenerationExperiment);
- #endif
- BASE_DECLARE_FEATURE(kPasswordsImportM2);
- BASE_DECLARE_FEATURE(kRecoverFromNeverSaveAndroid);
--#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
- BASE_DECLARE_FEATURE(kSkipUndecryptablePasswords);
- #endif
- #if BUILDFLAG(IS_ANDROID)
