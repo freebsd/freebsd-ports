f67381ff3f4b8712de4b89f280f50d9ccf0e37b3
Dan Langille <dvl@FreeBSD.org>
2023-10-27 17:31:13 +0000

    net-mgmt/librenms: Update to 23.10.0
    re: https://github.com/librenms/librenms/releases/tag/23.10.0
    
    See also UPDATING re crontab issue which this update fixes.

--- net-mgmt/librenms/files/patch-LibreNMS_wrapper.py
+++ net-mgmt/librenms/files/patch-LibreNMS_wrapper.py
@@ -1,11 +1,11 @@
---- LibreNMS/wrapper.py.orig	2023-08-21 15:25:48 UTC
+--- LibreNMS/wrapper.py.orig	2023-10-27 13:10:42 UTC
 +++ LibreNMS/wrapper.py
-@@ -280,7 +280,7 @@ def poll_worker(
+@@ -282,7 +282,7 @@ def poll_worker(
                      os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
                      wrappers[wrapper_type]["executable"],
                  )
--                command = "/usr/bin/env php {} -h {}".format(executable, device_id)
-+                command = "%%LOCALBASE%%/bin/php php {} -h {}".format(executable, device_id)
+-                command = "/usr/bin/env php {} {} {}".format(
++                command = ""%%LOCALBASE%%/bin/php {} {} {}".format(
+                     executable, wrappers[wrapper_type]["option"], device_id
+                 )
                  if modules is not None and len(str(modules).strip()):
-                     module_str = re.sub("\s", "", str(modules).strip())
-                     command = command + " -m {}".format(module_str)
--- net-mgmt/librenms/files/patch-includes_html_output_capture.inc.php
+++ net-mgmt/librenms/files/patch-includes_html_output_capture.inc.php
@@ -1,20 +1,20 @@
---- includes/html/output/capture.inc.php.orig	2021-05-25 19:32:48 UTC
+--- includes/html/output/capture.inc.php.orig	2023-10-27 13:17:37 UTC
 +++ includes/html/output/capture.inc.php
-@@ -31,7 +31,7 @@ $type = $_REQUEST['type'];
+@@ -32,7 +32,7 @@ $type = $_REQUEST['type'];
  
  switch ($type) {
      case 'poller':
--        $cmd = ['php', \LibreNMS\Config::get('install_dir') . '/poller.php', '-h', $hostname, '-r', '-f', '-d'];
-+        $cmd = ['%%LOCALBASE%%/bin/php', \LibreNMS\Config::get('install_dir') . '/poller.php', '-h', $hostname, '-r', '-f', '-d'];
+-        $cmd = ['php', \LibreNMS\Config::get('install_dir') . '/lnms', 'device:poll', $hostname, '--no-data', '-vv'];
++        $cmd = [''%%LOCALBASE%%/bin/php', \LibreNMS\Config::get('install_dir') . '/lnms', 'device:poll', $hostname, '--no-data', '-vv'];
          $filename = "poller-$hostname.txt";
          break;
      case 'snmpwalk':
-@@ -42,7 +42,7 @@ switch ($type) {
+@@ -43,7 +43,7 @@ switch ($type) {
          $filename = $device['os'] . '-' . $device['hostname'] . '.snmpwalk';
          break;
      case 'discovery':
 -        $cmd = ['php', \LibreNMS\Config::get('install_dir') . '/discovery.php', '-h', $hostname, '-d'];
-+        $cmd = ['%%LOCALBASE%%/bin/php', \LibreNMS\Config::get('install_dir') . '/discovery.php', '-h', $hostname, '-d'];
++        $cmd = [''%%LOCALBASE%%/bin/php', \LibreNMS\Config::get('install_dir') . '/discovery.php', '-h', $hostname, '-d'];
          $filename = "discovery-$hostname.txt";
          break;
      default:
