1534335108e8cc8932c59c5e84f0fc407a8310dc
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-11-01 09:50:08 +0800

    net/py-softlayer: Update to 6.1.10
    Changes:	https://github.com/softlayer/softlayer-python/releases

--- net/py-softlayer/files/patch-setup.py
+++ net/py-softlayer/files/patch-setup.py
@@ -4,8 +4,8 @@
          'prompt_toolkit >= 2',
          'pygments >= 2.0.0',
          'urllib3 >= 1.24',
--        'rich == 13.5.3'
-+        'rich >= 13.5.3'
+-        'rich == 13.6.0'
++        'rich >= 13.6.0'
      ],
      keywords=['softlayer', 'cloud', 'slcli'],
      classifiers=[
