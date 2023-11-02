941f6b702c4fb18bad8e15acc7884b946fa51d12
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2023-11-02 14:52:17 +0000

    Extend conditional statements to support aarch64cb
    * Replace conditions specific to CheriABIs with a single condition to
      match all CheriABIs;
    * Replace conditions specific to aarch64 and aarch64c with a single
      condition to match all AArch64 ABIs;
    * Add conditions specific to aarch64cb when there is a condition on
      aarch64c but not CheriABI in general.

--- Mk/Features/ssp.mk
+++ Mk/Features/ssp.mk
@@ -5,7 +5,7 @@ _SSP_MK_INCLUDED=	yes
 SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 
 .  if !defined(SSP_UNSAFE) && \
-    (! ${ARCH:Mmips*}) && ${ARCH} != aarch64c && ${ARCH} != riscv64c
+    (! ${ARCH:Mmips*}) && !${ABI:Mpurecap}
 # Overridable as a user may want to use -fstack-protector-all
 SSP_CFLAGS?=	-fstack-protector-strong
 CFLAGS+=	${SSP_CFLAGS}
