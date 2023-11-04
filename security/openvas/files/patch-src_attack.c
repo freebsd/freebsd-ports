c5f3ade2afe4807dfb9de757367c10aafb7b3500
Robert Clausecker <fuz@FreeBSD.org>
2023-11-04 07:19:31 +0100

    security/openvas: fix build on armv7
     - fix a wrong formatting specifier
     - mute a well-intended but irrelevant warning.  It would be better to
       just kill -Werror altogether as per policy, but I'll leave this up to
       the maintainer
     - remove a REINPLACE_CMD now that the same effect is achieved by patch
    
    Approved by:	portmgr (build fix blanket)
    MFH:		2023Q4

--- security/openvas/files/patch-src_attack.c
+++ security/openvas/files/patch-src_attack.c
@@ -1,6 +1,33 @@
---- src/attack.c.orig	2023-01-03 21:45:22 UTC
+--- src/attack.c.orig	2023-10-11 11:14:13 UTC
 +++ src/attack.c
-@@ -1493,13 +1493,13 @@ stop:
+@@ -31,7 +31,7 @@
+ #include "utils.h"
+ 
+ #include <arpa/inet.h> /* for inet_ntoa() */
+-#include <bsd/unistd.h>
++
+ #include <errno.h> /* for errno() */
+ #include <fcntl.h>
+ #include <glib.h>
+@@ -1095,7 +1095,7 @@ apply_hosts_reverse_lookup_preferences (gvm_hosts_t *h
+       gvm_hosts_t *excluded;
+ 
+       excluded = gvm_hosts_reverse_lookup_unify_excluded (hosts);
+-      g_debug ("reverse_lookup_unify: Skipped %lu host(s).", excluded->count);
++      g_debug ("reverse_lookup_unify: Skipped %zu host(s).", excluded->count);
+ 
+       // Get the amount of hosts which are excluded now for this option,
+       // but they are already in the exclude list.
+@@ -1111,7 +1111,7 @@ apply_hosts_reverse_lookup_preferences (gvm_hosts_t *h
+       gvm_hosts_t *excluded;
+ 
+       excluded = gvm_hosts_reverse_lookup_only_excluded (hosts);
+-      g_debug ("reverse_lookup_unify: Skipped %lu host(s).", excluded->count);
++      g_debug ("reverse_lookup_unify: Skipped %zu host(s).", excluded->count);
+       // Get the amount of hosts which are excluded now for this option,
+       // but they are already in the exclude list.
+       // This is to avoid issues with the scan progress calculation, since
+@@ -1614,13 +1614,13 @@ stop:
  
    gettimeofday (&now, NULL);
    if (test_alive_hosts_only)
