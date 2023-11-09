17e9764ab5ce1afc3ce20f496c45dddf1c075068
Hiroki Tagato <tagattie@FreeBSD.org>
2023-11-09 23:29:39 +0900

    devel/electron26: update to 26.5.0
    Changelog: https://github.com/electron/electron/releases/tag/v26.5.0
    
    Reported by:	GitHub (watch releases)
    Security:	5558dded-a870-4fbe-8b0a-ba198db47007

--- devel/electron26/files/patch-electron_shell_browser_api_electron__api__base__window.cc
+++ devel/electron26/files/patch-electron_shell_browser_api_electron__api__base__window.cc
@@ -1,6 +1,6 @@
---- electron/shell/browser/api/electron_api_base_window.cc.orig	2023-09-20 06:47:47 UTC
+--- electron/shell/browser/api/electron_api_base_window.cc.orig	2023-11-07 13:23:25 UTC
 +++ electron/shell/browser/api/electron_api_base_window.cc
-@@ -1063,7 +1063,7 @@ void BaseWindow::SetIconImpl(v8::Isolate* isolate,
+@@ -1071,7 +1071,7 @@ void BaseWindow::SetIconImpl(v8::Isolate* isolate,
    static_cast<NativeWindowViews*>(window_.get())
        ->SetIcon(native_image->GetHICON(GetSystemMetrics(SM_CXSMICON)),
                  native_image->GetHICON(GetSystemMetrics(SM_CXICON)));
