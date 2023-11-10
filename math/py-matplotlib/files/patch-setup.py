8e771b3ece92768c7413bc53d0c305c0ffc99e31
wen heping <wenheping2000@hotmail.com>
2023-11-10 05:31:47 +0100

    math/py-matplotlib: update to 3.8.0
     - Switch to pep517
    
    Changelog: https://github.com/matplotlib/matplotlib/releases/tag/v3.8.0
    
    Approved by:	mainland@apeiron.net (maintainer timeout)
    PR:		274571

--- math/py-matplotlib/files/patch-setup.py
+++ math/py-matplotlib/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2023-03-10 22:52:58 UTC
+--- setup.py.orig	2023-09-15 02:26:52 UTC
 +++ setup.py
-@@ -269,6 +269,7 @@ setup(  # Finally, pass this all along to setuptools t
+@@ -278,6 +278,7 @@ setup(  # Finally, pass this all along to setuptools t
  
  setup(  # Finally, pass this all along to setuptools to do the heavy lifting.
      name="matplotlib",
-+    version="3.7.1",
++    version="3.8.0",
      description="Python plotting package",
      author="John D. Hunter, Michael Droettboom",
      author_email="matplotlib-users@python.org",
