8077e8215d9028c70f9a6b5e8f30590eeccf1233
Li-Wen Hsu <lwhsu@FreeBSD.org>
2023-11-13 11:41:54 +0800

    net-im/py-matrix-synapse: Update to 1.95.1
    PR:		274758
    Approved by:	Sascha Biberhofer <ports@skyforge.at> (maintainer)

--- net-im/py-matrix-synapse/files/patch-setup.py
+++ net-im/py-matrix-synapse/files/patch-setup.py
@@ -1,7 +1,7 @@
 --- setup.py.orig	1970-01-01 00:00:00 UTC
 +++ setup.py
-@@ -169,7 +169,6 @@ install_requires = \
-  'pydantic>=1.7.4',
+@@ -175,7 +175,6 @@ install_requires = \
+  'pydantic>=1.7.4,<3',
   'pymacaroons>=0.13.0',
   'service-identity>=18.1.0',
 - 'setuptools_rust>=1.3',
