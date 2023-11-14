9dedefece66e56392e2266aeed4814a0b645d46b
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-11-15 05:03:37 +0800

    devel/py-voluptuous: Update to 0.14.0
    Changes:	https://github.com/alecthomas/voluptuous/releases

---
+++ devel/py-voluptuous/files/patch-setup.py
@@ -0,0 +1,8 @@
+--- setup.py.orig	2023-11-12 17:08:50 UTC
++++ setup.py
+@@ -40,5 +40,4 @@ setup(
+         'Programming Language :: Python :: 3.9',
+         'Programming Language :: Python :: 3.10',
+     ],
+-    setup_requires=['wheel']
+ )
