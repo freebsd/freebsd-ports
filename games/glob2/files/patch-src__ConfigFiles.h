512cec264164ff2c48c4237137f04cf990873256
Max Brazhnikov <makc@FreeBSD.org>
2023-11-16 16:13:56 +0300

    games/glob2: Update to 0.9.5.0 snapshot
     - Update to the latest snapshot. 'glob2 --version' reports 0.9.5.0
     - Clean up Makefile, drop stale patches, remove useless pkg-message.
     - Switch to SDL2.
     - Depend on libportaudio unconditionally.

--- games/glob2/files/patch-src__ConfigFiles.h
+++ games/glob2/files/patch-src__ConfigFiles.h
@@ -1,6 +1,6 @@
---- ./src/ConfigFiles.h.orig	2009-06-14 12:11:34.000000000 +0200
-+++ ./src/ConfigFiles.h	2013-10-28 01:26:26.000000000 +0100
-@@ -178,7 +178,11 @@
+--- src/ConfigFiles.h.orig	2023-06-13 07:14:47 UTC
++++ src/ConfigFiles.h
+@@ -178,7 +178,11 @@ class ConfigVector (public)
  		else
  		{
  			std::cerr << "ConfigVector::get(" << static_cast<unsigned int>(id) << ") : warning : id is not valid, returning default" << std::endl;
