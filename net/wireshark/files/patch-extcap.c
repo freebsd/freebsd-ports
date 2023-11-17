b8629f74283ee367b0eb27d410b22ab57d34819c
Joe Marcus Clarke <marcus@FreeBSD.org>
2023-11-17 14:50:46 -0500

    net/wireshark: Update to 4.2.0
    See https://www.wireshark.org/docs/relnotes/wireshark-4.2.0.html for a
    list of changes in this release.
    
    Of note to FreeBSD users, wireshark now uses Qt6 (rather than 5), and
    development files (e.g., headers), while installed by default can be
    disabled by unchecking the DEVELOPMENT option.

--- net/wireshark/files/patch-extcap.c
+++ net/wireshark/files/patch-extcap.c
@@ -1,6 +1,6 @@
---- extcap.c.orig	2019-02-28 17:56:38 UTC
+--- extcap.c.orig	2023-11-15 18:30:18 UTC
 +++ extcap.c
-@@ -21,6 +21,7 @@
+@@ -22,6 +22,7 @@
  #include <process.h>
  #include <time.h>
  #else
