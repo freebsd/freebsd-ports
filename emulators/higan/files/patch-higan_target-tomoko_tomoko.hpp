597ca5a01e3893880470afdd10a9cc4a0cdaa93a
Lorenzo Salvadore <salvadore@FreeBSD.org>
2023-11-18 12:02:56 +0000

    emulators/higan: Update to v110
    Moreover,
    
    - Switch to USE_GITHUB, since the sources are now available only there.
    - Replace WWW with the GitHub repository link, since the site in WWW
      does not exist anymore.
    - Change default sound option from OSS to OPENAL: OSS does not work.
    - Do not require GCC anymore.
    
    PR:		273678
    Approved by:	Naram Qashat <cyberbotx@cyberbotx.com> (maintainer)

--- emulators/higan/files/patch-higan_target-tomoko_tomoko.hpp
+++
@@ -1,8 +0,0 @@
---- higan/target-tomoko/tomoko.hpp.orig	2017-01-11 11:35:22 UTC
-+++ higan/target-tomoko/tomoko.hpp
-@@ -18,4 +18,5 @@ extern Emulator::Interface* emulator;
- #include "tools/tools.hpp"
- #include "presentation/presentation.hpp"
- 
-+auto locateShared(string name) -> string;
- auto locate(string name) -> string;
