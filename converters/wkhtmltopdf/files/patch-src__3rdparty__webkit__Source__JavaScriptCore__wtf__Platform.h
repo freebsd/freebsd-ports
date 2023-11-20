3f47a0b1eb91e6b5241cbe73a435aa1ea19e4f19
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-11-20 13:42:15 +0100

    converters/wkhtmltopdf: Fix build with gcc10
    The patch is accepted from Debian:
    https://salsa.debian.org/qt-kde-team/qt/qt4-x11/commit/0d4a3dd61ccb156dee556c214dbe91c04d44a717
    
    Still keep it DEPRECATED and see how it builds on 13.X series.
    
    PR:		269313
    Reported by:	r.quakkelaar@quaras.nl
    Approved by:	portmgr (just-fix-it)

--- converters/wkhtmltopdf/files/patch-src__3rdparty__webkit__Source__JavaScriptCore__wtf__Platform.h
+++ converters/wkhtmltopdf/files/patch-src__3rdparty__webkit__Source__JavaScriptCore__wtf__Platform.h
@@ -1,5 +1,5 @@
---- src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h.orig	2020-11-08 17:05:56.503694000 +0100
-+++ src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h	2020-11-08 17:06:42.105176000 +0100
+--- src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h.orig	2018-05-29 06:56:52 UTC
++++ src/3rdparty/webkit/Source/JavaScriptCore/wtf/Platform.h
 @@ -1003,7 +1003,9 @@
      || CPU(SPARC64) \
      || CPU(S390X) \
