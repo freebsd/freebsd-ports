eecd094e32a4fbc24d65197221242c29b83d3200
Alexey Dokuchaev <danfe@FreeBSD.org>
2023-11-22 12:16:30 +0000

    games/nehquake: unbreak, undeprecate, and assume maintainership
    Define LICENSE (GPLv2 or later), add missing USE_XORG components,
    do not allow ``make build'' to succeed when compilation actually
    fails, bump MAX_PRINTMSG value to accommodate long GL extensions
    string returned by modern video cards, amend post-install advice.
    
    Tested on:	13-STABLE/i386 + ATI Radeon HD 4350 (RV710)

--- games/nehquake/files/patch-src__glquake.h
+++ games/nehquake/files/patch-src__glquake.h
@@ -1,6 +1,6 @@
---- ./src/glquake.h.orig	Tue Sep 27 19:06:49 2005
-+++ ./src/glquake.h	Tue May 16 17:36:22 2006
-@@ -275,7 +275,6 @@
+--- src/glquake.h.orig	2005-09-27 22:06:49 UTC
++++ src/glquake.h
+@@ -275,7 +275,6 @@ extern GLenum gl_Texture1;
  #define    TEXTURE1_SGIS gl_Texture1
  
  #ifndef _WIN32
@@ -8,3 +8,9 @@
  #endif
  
  typedef void (APIENTRY *lpMTexFUNC) (GLenum, GLfloat, GLfloat);
+@@ -289,4 +288,4 @@ void GL_DisableMultitexture(void);
+ void GL_EnableMultitexture(void);
+ void GL_LightMaps_f (void);
+ 
+-extern   bspversion;
++extern int bspversion;
