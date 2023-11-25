b182c0864c3b5c4c0b9ac679913e163ca28eb338
Ryan Steinmetz <zi@FreeBSD.org>
2023-11-24 22:51:03 -0500

    Mk/bsd.sites.mk: Update zi mirror URL

--- Mk/bsd.sites.mk
+++ Mk/bsd.sites.mk
@@ -1069,7 +1069,7 @@ MASTER_SITE_KERNEL_ORG+= \
 
 .if !defined(IGNORE_MASTER_SITE_ZI)
 MASTER_SITE_ZI+= \
-	https://ftpmirror.your.org/zi/%SUBDIR%/ \
+	https://ftpmirror.your.org/pub/zi/%SUBDIR%/ \
 	https://mirrors.rit.edu/zi/%SUBDIR%/ \
 	https://www.zi0r.com/mirrors/%SUBDIR%/ \
 	${MASTER_SITE_LOCAL:S/%SUBDIR%/zi/}
