7668cedb379c55db16a7e132852802ffa8cdf8e8
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-11-27 18:03:06 +0800

    textproc/py-furo: Update to 2023.9.10
    Changes:	https://github.com/pradyunsg/furo/blob/main/docs/changelog.md
    		https://pradyunsg.me/furo/changelog/

---
+++ textproc/py-furo/files/patch-src-furo-__init__.py
@@ -0,0 +1,11 @@
+--- src/furo/__init__.py.orig	2023-09-10 14:57:28 UTC
++++ src/furo/__init__.py
+@@ -347,7 +347,7 @@ def _overwrite_pygments_css(
+ 
+ def setup(app: sphinx.application.Sphinx) -> Dict[str, Any]:
+     """Entry point for sphinx theming."""
+-    app.require_sphinx("6.0")
++    app.require_sphinx("5.0")
+ 
+     app.add_config_value(
+         "pygments_dark_style", default="native", rebuild="env", types=[str]
