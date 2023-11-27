189fd10b099b32db828a4e247499705438fd6a87
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-11-28 02:03:11 +0800

    devel/py-django-rq: Update to 2.9.0
    Changes:	https://github.com/rq/django-rq/releases

--- devel/py-django-rq/files/patch-setup.py
+++ devel/py-django-rq/files/patch-setup.py
@@ -4,8 +4,8 @@
      zip_safe=False,
      include_package_data=True,
      package_data={'': ['README.rst']},
--    install_requires=['django>=2.0', 'rq>=1.14', 'redis>=3'],
-+    install_requires=['django>=2.0', 'rq>=1.11', 'redis>=3'],
+-    install_requires=['django>=3.2', 'rq>=1.14', 'redis>=3'],
++    install_requires=['django>=3.2', 'rq>=1.11', 'redis>=3'],
      extras_require={
          'Sentry': ['raven>=6.1.0'],
          'testing': ['mock>=2.0.0'],
