f19cf1d2bc1c362f61dd9368b8ad156af3fedf7d
Rene Ladan <rene@FreeBSD.org>
2023-12-02 14:40:29 +0100

    science/libgeodecomp: Remove expired port
    2023-11-30 science/libgeodecomp: BROKEN for more than an year

--- science/libgeodecomp/files/patch-src_examples_globalreductions_main.cpp
+++
@@ -1,8 +0,0 @@
---- src/examples/globalreductions/main.cpp.orig	2019-07-28 15:27:06 UTC
-+++ src/examples/globalreductions/main.cpp
-@@ -1,4 +1,5 @@
- #include <libgeodecomp.h>
-+#include <libgeodecomp/io/serialbovwriter.h>
- 
- using namespace LibGeoDecomp;
- 
