5ad03236da0cc00391c3c7e2797095cb794e0236
Rene Ladan <rene@FreeBSD.org>
2023-12-03 13:48:00 +0100

    devel/electron24: Remove expired port
    2023-12-03 devel/electron24: Upstream EOL reached on 2023-10-10

--- devel/electron24/files/patch-third__party_boringssl_src_crypto_fipsmodule_aes_asm_aesni-x86__64.pl
+++
@@ -1,18 +0,0 @@
---- third_party/boringssl/src/crypto/fipsmodule/aes/asm/aesni-x86_64.pl.orig	2023-03-30 00:40:31 UTC
-+++ third_party/boringssl/src/crypto/fipsmodule/aes/asm/aesni-x86_64.pl
-@@ -3778,6 +3778,7 @@ ___
- }
- 
- $code.=<<___;
-+.rodata
- .align	64
- .Lbswap_mask:
- 	.byte	15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0
-@@ -3800,6 +3801,7 @@ $code.=<<___;
- 
- .asciz  "AES for Intel AES-NI, CRYPTOGAMS by <appro\@openssl.org>"
- .align	64
-+.previous
- ___
- 
- # EXCEPTION_DISPOSITION handler (EXCEPTION_RECORD *rec,ULONG64 frame,
