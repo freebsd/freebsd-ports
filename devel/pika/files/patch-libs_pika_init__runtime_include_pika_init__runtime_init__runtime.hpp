b986ed339ed718c3898b131796ee0afe81c0ee23
Yuri Victorovich <yuri@FreeBSD.org>
2023-12-06 23:02:23 -0800

    devel/pika: update 0.20.0 → 0.21.0
    Reported by:	portscout

--- devel/pika/files/patch-libs_pika_init__runtime_include_pika_init__runtime_init__runtime.hpp
+++ devel/pika/files/patch-libs_pika_init__runtime_include_pika_init__runtime_init__runtime.hpp
@@ -1,6 +1,6 @@
 - hack to work around https://github.com/pika-org/pika/issues/649
 
---- libs/pika/init_runtime/include/pika/init_runtime/init_runtime.hpp.orig	2023-04-10 00:11:12 UTC
+--- libs/pika/init_runtime/include/pika/init_runtime/init_runtime.hpp.orig	2023-12-06 08:42:57 UTC
 +++ libs/pika/init_runtime/include/pika/init_runtime/init_runtime.hpp
 @@ -44,6 +44,7 @@
  
@@ -10,12 +10,3 @@
  extern char** environ;
  #endif
  
-@@ -113,7 +114,7 @@ namespace pika {
-             }
- 
- #if defined(__FreeBSD__)
--            freebsd_environ = environ;
-+            //freebsd_environ = environ;
- #endif
-             // set a handler for std::abort
-             std::signal(SIGABRT, pika::detail::on_abort);
