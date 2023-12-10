f5562c0c4f6dd3c3718e9c3e3331b4c5ed914356
Dima Panov <fluffy@FreeBSD.org>
2023-12-10 23:00:23 +0300

    editors/libreoffice: update to 7.6.4 release (+)
    LibreOffice 7.6.4 Community and LibreOffice 7.5.9 Community are immediately available for FreeBSD users!
    
    This release brings up over 40 fixes since previous version.
    
    Changelog:	https://wiki.documentfoundation.org/Releases/7.6.4/RC1

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,9 @@
---- external/skia/UnpackedTarball_skia.mk.orig	2023-09-22 19:07:01 UTC
+--- external/skia/UnpackedTarball_skia.mk.orig	2023-11-30 13:04:41 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -39,6 +39,8 @@ skia_patches := \
-     tdf147342.patch.0 \
+@@ -40,6 +40,8 @@ skia_patches := \
      redefinition-of-op.patch.0 \
      0001-Added-missing-include-cstdio.patch \
+     0001-AvoidCombiningExtrememelyLargeMeshes.patch.1 \
 +    gfx-skia-skia-src-core-SkCpu.cpp.patch.0 \
 +    powerpc64le-skia.patch.0 \
  
