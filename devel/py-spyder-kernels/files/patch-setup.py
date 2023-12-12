f9aa5cdf7f96921d2951a9196e1e97df853a1217
Rainer Hurling <rhurlin@FreeBSD.org>
2023-12-12 18:10:20 +0100

    devel/py-spyder-kernels: Update to 2.5.0
    - Add support for chained exceptions to the debugger
    - Improve getting signatures from docstrings
    - Restore compatibility with Python 2
    
    Changelog: https://github.com/spyder-ide/spyder-kernels/blob/master/CHANGELOG.md#version-250-2023-11-06

--- devel/py-spyder-kernels/files/patch-setup.py
+++ devel/py-spyder-kernels/files/patch-setup.py
@@ -1,11 +1,19 @@
---- setup.py.orig	2023-06-29 15:09:47 UTC
+--- setup.py.orig	2023-11-06 14:56:06 UTC
 +++ setup.py
-@@ -40,7 +40,7 @@ REQUIREMENTS = [
-     'backports.functools-lru-cache; python_version<"3"',
+@@ -41,13 +41,13 @@ REQUIREMENTS = [
      'cloudpickle',
      'ipykernel>=4.5,<5; python_version<"3"',
--    'ipykernel>=6.23.2,<7; python_version>="3"',
-+    'ipykernel>=6.23.1,<7; python_version>="3"',
+     'ipykernel>=6.16.1,<6.17; python_version<"3.8"',
+-    'ipykernel>=6.23.2,<7; python_version>="3.8"',
++    'ipykernel>=6.23.2; python_version>="3.8"',
      'ipython<6; python_version<"3"',
-     'ipython>=7.31.1,<9,!=8.8.0,!=8.9.0,!=8.10.0,!=8.11.0,!=8.12.0,!=8.12.1; python_version>="3"',
+     'ipython>=7.31.1,<8; python_version<"3.8"',
+     'ipython>=8.12.2,<8.13; python_version=="3.8"',
+-    'ipython>=8.13.0,<9,!=8.17.1; python_version>"3.8"',
++    'ipython>=8.13.0,!=8.17.1; python_version>"3.8"',
      'jupyter-client>=5.3.4,<6; python_version<"3"',
+-    'jupyter-client>=7.4.9,<9; python_version>="3"',
++    'jupyter-client>=7.4.9; python_version>="3"',
+     'pyzmq>=17,<20; python_version<"3"',
+     'pyzmq>=22.1.0; python_version>="3"',
+     'wurlitzer>=1.0.3;platform_system!="Windows"',
