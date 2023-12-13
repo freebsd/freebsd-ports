1f36b409252779218185c9140c069689aa12d603
Hiroki Tagato <tagattie@FreeBSD.org>
2023-12-13 13:36:09 +0900

    editors/vscode: update to 1.85.0
    Changelog: https://code.visualstudio.com/updates/v1_85
    
    Reported by:	GitHub (watch releases)

--- editors/vscode/files/patch-build_lib_layersChecker.ts
+++ editors/vscode/files/patch-build_lib_layersChecker.ts
@@ -1,6 +1,6 @@
---- build/lib/layersChecker.ts.orig	2023-09-06 21:00:17 UTC
+--- build/lib/layersChecker.ts.orig	2023-12-06 10:36:56 UTC
 +++ build/lib/layersChecker.ts
-@@ -310,7 +310,7 @@ function checkFile(program: ts.Program, sourceFile: ts
+@@ -338,7 +338,7 @@ function createProgram(tsconfigPath: string): ts.Progr
  function createProgram(tsconfigPath: string): ts.Program {
  	const tsConfig = ts.readConfigFile(tsconfigPath, ts.sys.readFile);
  
--- editors/vscode/files/patch-src_vs_workbench_contrib_extensions_browser_extensionEditor.ts
+++ editors/vscode/files/patch-src_vs_workbench_contrib_extensions_browser_extensionEditor.ts
@@ -1,6 +1,6 @@
---- src/vs/workbench/contrib/extensions/browser/extensionEditor.ts.orig	2023-11-01 09:39:20 UTC
+--- src/vs/workbench/contrib/extensions/browser/extensionEditor.ts.orig	2023-12-06 10:36:56 UTC
 +++ src/vs/workbench/contrib/extensions/browser/extensionEditor.ts
-@@ -1778,7 +1778,8 @@ export class ExtensionEditor extends EditorPane {
+@@ -1801,7 +1801,8 @@ export class ExtensionEditor extends EditorPane {
  
  		switch (platform) {
  			case 'win32': key = rawKeyBinding.win; break;
