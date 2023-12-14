54c0f9f19acbb83625b6039789d8e8c9370fa45e
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2023-12-14 13:36:47 +0800

    print/py-fontbakery: Update to 0.10.6
    Changes:	https://github.com/fonttools/fontbakery/releases
    		https://github.com/fonttools/fontbakery/blob/main/CHANGELOG.md

--- print/py-fontbakery/files/patch-setup.py
+++ print/py-fontbakery/files/patch-setup.py
@@ -1,15 +1,15 @@
---- setup.py.orig	2023-09-12 12:07:05 UTC
+--- setup.py.orig	2023-12-01 14:46:39 UTC
 +++ setup.py
-@@ -55,7 +55,7 @@ googlefonts_extras = (
-         "gflanguages>=0.3.0",  # 0.3.0 had an api simplification/update
+@@ -72,7 +72,7 @@ googlefonts_extras = (
+         # FontVersion.get_name_id5_version_string which was added in version 0.6.0
+         f"fontTools[lxml,unicode]{FONTTOOLS_VERSION}",
          # (see https://github.com/googlefonts/gflanguages/pull/7)
-         "glyphsets>=0.5.0",
 -        "protobuf>=3.7.0, <4",  # 3.7.0 fixed a bug on parsing some METADATA.pb files.
 +        "protobuf>=3.7.0",  # 3.7.0 fixed a bug on parsing some METADATA.pb files.
          # We cannot use v4 because our protobuf files have been compiled with v3.
          # (see https://github.com/fonttools/fontbakery/issues/2200)
          f"vharfbuzz{VHARFBUZZ_VERSION}",
-@@ -96,7 +96,7 @@ all_extras = set(
+@@ -122,7 +122,7 @@ all_extras = set(
  
  setup(
      name="fontbakery",
@@ -18,9 +18,9 @@
      url="https://github.com/fonttools/fontbakery/",
      description="A font quality assurance tool for everyone",
      long_description=readme,
-@@ -148,7 +148,7 @@ setup(
+@@ -183,7 +183,7 @@ setup(
          # core dependencies
-         "babelfont",
+         "babelfont>=3.0.1",  # v3.0.1 needed by check/legacy_accents
          f"fontTools{FONTTOOLS_VERSION}",
 -        "freetype-py!=2.4.0",  # Avoiding 2.4.0 due to seg-fault described at
 +        "freetype-py",  # Avoiding 2.4.0 due to seg-fault described at
