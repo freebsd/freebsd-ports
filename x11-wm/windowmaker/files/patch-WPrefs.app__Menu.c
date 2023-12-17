9d85112e8000fa3355a6248f1d5bea4dc2aac882
Alexey Dokuchaev <danfe@FreeBSD.org>
2023-12-17 17:07:02 +0000

    x11-wm/{libwraster,windowmaker}: update Window Maker to version 0.96.0
    PR:		273739
    Approved by:	maintainer timeout (hrs; ~14 weeks)

--- x11-wm/windowmaker/files/patch-WPrefs.app__Menu.c
+++ x11-wm/windowmaker/files/patch-WPrefs.app__Menu.c
@@ -1,25 +1,30 @@
---- WPrefs.app/Menu.c.orig	2020-04-05 13:41:15 UTC
+--- WPrefs.app/Menu.c.orig	2023-03-13 14:40:46 UTC
 +++ WPrefs.app/Menu.c
-@@ -518,19 +518,19 @@ static void createPanel(_Panel * p)
+@@ -520,8 +520,7 @@ static void createPanel(_Panel * p)
  		data = putNewItem(panel, pad, DirectoryInfo, _("Themes"));
  		data->param.directory.command = "setstyle";
  		data->param.directory.directory =
--		    "/usr/share/WindowMaker/Themes /usr/local/share/WindowMaker/Themes $HOME/GNUstep/Library/WindowMaker/Themes";
-+		    "/usr/share/WindowMaker/Themes " PREFIX "/share/WindowMaker/Themes $HOME/GNUstep/Library/WindowMaker/Themes";
+-		    "/usr/share/" PACKAGE_TARNAME "/Themes"
+-		    " /usr/local/share/" PACKAGE_TARNAME "/Themes"
++		    PREFIX "/share/" PACKAGE_TARNAME "/Themes"
+ 		    " $HOME/" GSUSER_SUBDIR "/" USERDATA_SUBDIR "/" PACKAGE_TARNAME "/Themes";
  		data->param.directory.stripExt = 1;
  
- 		data = putNewItem(panel, pad, DirectoryInfo, _("Bg Images (scale)"));
+@@ -529,7 +528,7 @@ static void createPanel(_Panel * p)
  		data->param.directory.command = "wmsetbg -u -s";
  		data->param.directory.directory =
--		    "/opt/kde2/share/wallpapers /usr/share/WindowMaker/Backgrounds $HOME/GNUstep/Library/WindowMaker/Backgrounds";
-+		    "/opt/kde2/share/wallpapers " PREFIX "/share/WindowMaker/Backgrounds $HOME/GNUstep/Library/WindowMaker/Backgrounds";
+ 		    "/opt/kde2/share/wallpapers"
+-		    " /usr/share/" PACKAGE_TARNAME "/Backgrounds"
++		    " " PREFIX "/share/" PACKAGE_TARNAME "/Backgrounds"
+ 		    " $HOME/" GSUSER_SUBDIR "/" USERDATA_SUBDIR "/" PACKAGE_TARNAME "/Backgrounds";
  		data->param.directory.stripExt = 1;
  
- 		data = putNewItem(panel, pad, DirectoryInfo, _("Bg Images (tile)"));
+@@ -537,7 +536,7 @@ static void createPanel(_Panel * p)
  		data->param.directory.command = "wmsetbg -u -t";
  		data->param.directory.directory =
--		    "/opt/kde2/share/wallpapers /usr/share/WindowMaker/Backgrounds $HOME/GNUstep/Library/WindowMaker/Backgrounds";
-+		    "/opt/kde2/share/wallpapers " PREFIX "/share/WindowMaker/Backgrounds $HOME/GNUstep/Library/WindowMaker/Backgrounds";
+ 		    "/opt/kde2/share/wallpapers"
+-		    " /usr/share/" PACKAGE_TARNAME "/Backgrounds"
++		    " " PREFIX "/share/" PACKAGE_TARNAME "/Backgrounds"
+ 		    " $HOME/" GSUSER_SUBDIR "/" USERDATA_SUBDIR "/" PACKAGE_TARNAME "/Backgrounds";
  		data->param.directory.stripExt = 1;
  
- 		smenu = putNewSubmenu(pad, _("Assorted XTerms"));
--- x11-wm/windowmaker/files/patch-WPrefs.app__Paths.c
+++ x11-wm/windowmaker/files/patch-WPrefs.app__Paths.c
@@ -1,24 +1,25 @@
---- WPrefs.app/Paths.c.orig	2015-08-11 18:41:14 UTC
+--- WPrefs.app/Paths.c.orig	2023-01-06 21:34:50 UTC
 +++ WPrefs.app/Paths.c
-@@ -78,9 +78,9 @@ static void showData(_Panel * panel)
+@@ -78,10 +78,9 @@ static void showData(_Panel * panel)
  			wwarning(_("bad value in option IconPath. Using default path list"));
  		addPathToList(panel->icoL, -1, "~/pixmaps");
- 		addPathToList(panel->icoL, -1, "~/GNUstep/Library/Icons");
+ 		addPathToList(panel->icoL, -1, "~/" GSUSER_SUBDIR "/" USERDATA_SUBDIR "/Icons");
 -		addPathToList(panel->icoL, -1, "/usr/include/X11/pixmaps");
--		addPathToList(panel->icoL, -1, "/usr/local/share/WindowMaker/Icons");
--		addPathToList(panel->icoL, -1, "/usr/local/share/WindowMaker/Pixmaps");
+-		addPathToList(panel->icoL, -1, "/usr/local/share/" PACKAGE_TARNAME "/Icons");
+-		addPathToList(panel->icoL, -1, "/usr/local/share/" PACKAGE_TARNAME "/Pixmaps");
+-		addPathToList(panel->icoL, -1, "/usr/share/" PACKAGE_TARNAME "/Icons");
 +		addPathToList(panel->icoL, -1, PREFIX "/share/pixmaps");
-+		addPathToList(panel->icoL, -1, PREFIX "/share/WindowMaker/Icons");
-+		addPathToList(panel->icoL, -1, PREFIX "/share/WindowMaker/Pixmaps");
- 		addPathToList(panel->icoL, -1, "/usr/share/WindowMaker/Icons");
++		addPathToList(panel->icoL, -1, PREFIX "/share/" PACKAGE_TARNAME "/Icons");
++		addPathToList(panel->icoL, -1, PREFIX "/share/" PACKAGE_TARNAME "/Pixmaps");
  	} else {
  		for (i = 0; i < WMGetPropListItemCount(array); i++) {
-@@ -95,7 +95,7 @@ static void showData(_Panel * panel)
+ 			val = WMGetFromPLArray(array, i);
+@@ -95,7 +94,7 @@ static void showData(_Panel * panel)
  			wwarning(_("bad value in option PixmapPath. Using default path list"));
  		addPathToList(panel->pixL, -1, "~/pixmaps");
- 		addPathToList(panel->pixL, -1, "~/GNUstep/Library/WindowMaker/Pixmaps");
--		addPathToList(panel->pixL, -1, "/usr/local/share/WindowMaker/Pixmaps");
-+		addPathToList(panel->pixL, -1, PREFIX "/share/WindowMaker/Pixmaps");
+ 		addPathToList(panel->pixL, -1, "~/" GSUSER_SUBDIR "/" USERDATA_SUBDIR "/" PACKAGE_TARNAME "/Pixmaps");
+-		addPathToList(panel->pixL, -1, "/usr/local/share/" PACKAGE_TARNAME "/Pixmaps");
++		addPathToList(panel->pixL, -1, PREFIX "/share/" PACKAGE_TARNAME "/Pixmaps");
  	} else {
  		for (i = 0; i < WMGetPropListItemCount(array); i++) {
  			val = WMGetFromPLArray(array, i);
