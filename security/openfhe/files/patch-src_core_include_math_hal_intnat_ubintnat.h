8f94e4aaec0dbc90d3eb8a58e2029002895bae84
Yuri Victorovich <yuri@FreeBSD.org>
2023-12-16 23:16:56 -0800

    security/openfhe: update 1.0.4 → 1.1.2
    Reported by:	portscout

--- security/openfhe/files/patch-src_core_include_math_hal_intnat_ubintnat.h
+++ security/openfhe/files/patch-src_core_include_math_hal_intnat_ubintnat.h
@@ -1,6 +1,6 @@
---- src/core/include/math/hal/intnat/ubintnat.h.orig	2022-12-06 03:29:13 UTC
+--- src/core/include/math/hal/intnat/ubintnat.h.orig	2023-12-16 19:03:47 UTC
 +++ src/core/include/math/hal/intnat/ubintnat.h
-@@ -2152,17 +2152,17 @@ class NativeIntegerT : public lbcrypto::BigIntegerInte
+@@ -1935,16 +1935,16 @@ class NativeIntegerT final : public lbcrypto::BigInteg
          res.lo = x.lo * y;
          asm("umulh %0, %1, %2\n\t" : "=r"(res.hi) : "r"(x.lo), "r"(y));
          res.hi += x.hi * y;
@@ -10,15 +10,14 @@
 +        wres   = wa * wb;  // should give us 128 bits  of 64 * 64
 +        res.hi = (uint64_t)(wres >> 64);
 +        res.lo = (uint64_t)wres;
-+#elif defined(__arm__)  || defined(__powerpc__) // 32 bit processor
++#elif defined(__arm__) || defined(__powerpc__) // 32 bit processor
          uint64_t wres(0), wa(a), wb(b);
- 
-         wres   = wa * wb;  // should give us the lower 64 bits of 32*32
+         wres   = wa * wb;
          res.hi = wres >> 32;
          res.lo = (uint32_t)wres & 0xFFFFFFFF;
 -#elif __riscv
 -        U128BITS wres(0), wa(a), wb(b);
--        wres   = wa * wb;  // should give us 128 bits  of 64 * 64
+-        wres   = wa * wb;
 -        res.hi = (uint64_t)(wres >> 64);
 -        res.lo = (uint64_t)wres;
  #elif defined(__EMSCRIPTEN__)  // web assembly
