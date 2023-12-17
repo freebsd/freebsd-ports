7d0d0cf4403d5aedc84097a3b5a15f223702474a
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-12-17 13:55:08 +0100

    x11-wm/wmakerconf: Mark DEPRECATED
    - Last upstream release was in 2007
    - Set EXPIRATION_DATE 2024-01-17
    - Pet portclippy
    - Refresh patches
    - Add missing dependencies

--- x11-wm/wmakerconf/files/patch-scripts__wget.pl
+++ x11-wm/wmakerconf/files/patch-scripts__wget.pl
@@ -1,6 +1,6 @@
---- scripts/wget.pl.orig	Mon Jan 26 07:20:31 2004
-+++ scripts/wget.pl	Wed Jun  2 01:56:42 2004
-@@ -1,5 +1,5 @@
+--- scripts/wget.pl.orig	2004-01-25 22:20:31 UTC
++++ scripts/wget.pl
+@@ -1,5 +1,5 @@ quit ("Wget not installed") if $wwwget eq "";
  quit ("Wget not installed") if $wwwget eq "";
  
 -$rc = system ("$wwwget --quiet -c --tries=0 -O$tmpfile '$url'");
