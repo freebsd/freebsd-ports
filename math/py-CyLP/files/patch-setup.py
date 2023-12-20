50037fe84c6f20109e3e1c5aa1f37cc58876985f
Yuri Victorovich <yuri@FreeBSD.org>
2023-12-20 11:21:21 -0800

    math/py-CyLP: update 0.91.6 → 0.92.2
    Reported by:	portscout

--- math/py-CyLP/files/patch-setup.py
+++ math/py-CyLP/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2014-02-14 17:25:06 UTC
+--- setup.py.orig	2023-11-01 20:21:56 UTC
 +++ setup.py
-@@ -108,7 +108,7 @@ ext_modules = []
+@@ -124,7 +124,7 @@ ext_modules = []
  
  if operatingSystem == 'mac':
      extra_link_args = ['-Wl,-framework', '-Wl,Accelerate', '-headerpad_max_install_names']
@@ -9,3 +9,12 @@
      extra_link_args = ['-lrt']
  else:
      # Assuming Windows
+@@ -396,7 +396,7 @@ s_AUTHORS = open('AUTHORS').read()
+ 
+ extra_files = ['cpp/*.hpp', 'cpp/*.h', 'cy/*.pxd', 'VERSION']
+ 
+-setup(name='cylp',
++setup(name='CyLP',
+       version=VERSION,
+       description='A Python interface for CLP, CBC, and CGL',
+       long_description=s_README,
