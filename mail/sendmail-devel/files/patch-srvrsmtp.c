f65dcbde97ed01982df5ce7c5152c4f862d3db05
Dirk Meyer <dinoex@FreeBSD.org>
2023-12-22 08:01:36 +0100

    mail/sendmail-devel: Update to 8.18.0.2

--- mail/sendmail-devel/files/patch-srvrsmtp.c
+++ mail/sendmail-devel/files/patch-srvrsmtp.c
@@ -1,24 +1,34 @@
---- sendmail/srvrsmtp.c.orig	2019-02-15 10:30:19 UTC
+--- sendmail/srvrsmtp.c.orig	2023-12-05 09:53:22 UTC
 +++ sendmail/srvrsmtp.c
-@@ -878,6 +878,9 @@ smtp(nullserver, d_flags, e)
- #if _FFR_BADRCPT_SHUTDOWN
- 	int n_badrcpts_adj;
+@@ -921,6 +921,9 @@ do								\
+ # define SHOWCMDINREPLY(inp) inp
+ # define SHOWSHRTCMDINREPLY(inp) shortenstring(inp, MAXSHORTSTR)
  #endif
 +#ifdef USE_BLACKLIST
 +	int saved_bl_fd;
 +#endif
  
- 	RESET_AUTH_FAIL_LOG_USER;
- 	SevenBitInput_Saved = SevenBitInput;
-@@ -1377,6 +1380,7 @@ smtp(nullserver, d_flags, e)
- 					  (int) tp.tv_sec +
- 						(tp.tv_usec >= 500000 ? 1 : 0)
+ void
+ smtp(nullserver, d_flags, e)
+@@ -1504,6 +1507,8 @@ smtp(nullserver, d_flags, e)
+ 			/* check if data is on the socket during the pause */
+ 			if ((tp = channel_readable(InChannel, msecs)) != NULL)
+ 			{
++				int fd;
++
+ 				greetcode = "554";
+ 				nullserver = "Command rejected";
+ 				sm_syslog(LOG_INFO, e->e_id,
+@@ -1513,6 +1518,8 @@ smtp(nullserver, d_flags, e)
+ 					  (int) tp->tv_sec +
+ 						(tp->tv_usec >= 500000 ? 1 : 0)
  					 );
++				fd = sm_io_getinfo(InChannel, SM_IO_WHAT_FD, NULL);
 +				BLACKLIST_NOTIFY(BLACKLIST_AUTH_FAIL, fd, "pre-greeting traffic");
  			}
  		}
  	}
-@@ -1479,6 +1483,10 @@ smtp(nullserver, d_flags, e)
+@@ -1631,6 +1638,10 @@ smtp(nullserver, d_flags, e)
  		SmtpPhase = "server cmd read";
  		sm_setproctitle(true, e, "server %s cmd read", CurSmtpClient);
  
@@ -29,7 +39,7 @@
  		/* handle errors */
  		if (sm_io_error(OutChannel) ||
  		    (p = sfgets(inp, sizeof(inp), InChannel,
-@@ -1792,8 +1800,11 @@ smtp(nullserver, d_flags, e)
+@@ -1944,8 +1955,11 @@ smtp(nullserver, d_flags, e)
  #define LOGAUTHFAIL	\
  	do	\
  	{	\
@@ -41,7 +51,7 @@
  		if (LogLevel >= 9)	\
  			sm_syslog(LOG_WARNING, e->e_id,	\
  				  "AUTH failure (%s): %s (%d) %s%s%.*s, relay=%.100s",	\
-@@ -1943,6 +1954,9 @@ smtp(nullserver, d_flags, e)
+@@ -2095,6 +2109,9 @@ smtp(nullserver, d_flags, e)
  			DELAY_CONN("AUTH");
  			if (!sasl_ok || n_mechs <= 0)
  			{
@@ -51,7 +61,7 @@
  				message("503 5.3.3 AUTH not available");
  				break;
  			}
-@@ -3546,10 +3560,17 @@ doquit:
+@@ -3836,10 +3853,17 @@ smtp(nullserver, d_flags, e)
  				**  timeouts for the same connection.
  				*/
  
@@ -69,7 +79,7 @@
  			if (tTd(93, 100))
  			{
  				/* return to handle next connection */
-@@ -3607,7 +3628,10 @@ doquit:
+@@ -3921,7 +3945,10 @@ smtp(nullserver, d_flags, e)
  #if MAXBADCOMMANDS > 0
  			if (++n_badcmds > MAXBADCOMMANDS)
  			{
@@ -80,13 +90,13 @@
  				message("421 4.7.0 %s Too many bad commands; closing connection",
  					MyHostName);
  
-@@ -3659,6 +3683,9 @@ doquit:
+@@ -3975,6 +4002,9 @@ smtp(nullserver, d_flags, e)
+ 		}
  #if SASL
  		}
- #endif
++#endif
 +#ifdef USE_BLACKLIST
 +		close(saved_bl_fd);
-+#endif
+ #endif
  	    }
  	    SM_EXCEPT(exc, "[!F]*")
- 	    {
