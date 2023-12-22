ba388fc3bbfcc755e844c6bf99895c6b734b32b3
Mikael Urankar <mikael@FreeBSD.org>
2023-12-22 09:40:47 +0100

    lang/rust: Update to 1.75.0
    Announce:	https://blog.rust-lang.org/
    ChangeLog:	https://github.com/rust-lang/rust/releases/tag/1.75.0
    
    PR:		276033
    Tested by:	mikael,pkubaj

--- lang/rust/files/patch-compiler_rustc__target_src_spec_targets_powerpc64__unknown__freebsd.rs
+++ lang/rust/files/patch-compiler_rustc__target_src_spec_targets_powerpc64__unknown__freebsd.rs
@@ -1,5 +1,5 @@
---- compiler/rustc_target/src/spec/powerpc64_unknown_freebsd.rs.orig	2020-12-29 04:03:08 UTC
-+++ compiler/rustc_target/src/spec/powerpc64_unknown_freebsd.rs
+--- compiler/rustc_target/src/spec/targets/powerpc64_unknown_freebsd.rs.orig	2020-12-29 04:03:08 UTC
++++ compiler/rustc_target/src/spec/targets/powerpc64_unknown_freebsd.rs
 @@ -12,6 +12,10 @@ pub fn target() -> Target {
          pointer_width: 64,
          data_layout: "E-m:e-i64:64-n32:64".into(),
