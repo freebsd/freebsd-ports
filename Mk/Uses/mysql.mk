66f73a2a3fcdb3abb848be066aced6da352615e2
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2023-12-24 02:06:05 +0100

    Mk/Uses/mysql.mk: Remove percona57
    percona57-* was removed from the tree on 2023-10-01 as upstream support
    ended however the necessary codebase in the Mk infrastructure was still
    there. This commit removes thos unnecessary part and also syncs with
    bsd.default-version.mk.

--- Mk/Uses/mysql.mk
+++ Mk/Uses/mysql.mk
@@ -48,8 +48,6 @@ _MYSQL_ARGS:=	${_MYSQL_ARGS:Nclient}
 _WANT_MYSQL_VER=	${_MYSQL_ARGS}
 .      if   (${_WANT_MYSQL_VER:C/[0-9]*//} == "m")
 _WANT_MYSQL_FLAVOUR=	mariadb
-.      elif (${_WANT_MYSQL_VER:C/[0-9]*//} == "p")
-_WANT_MYSQL_FLAVOUR=	percona
 .      else
 _WANT_MYSQL_FLAVOUR=	mysql
 .      endif
@@ -65,7 +63,6 @@ DEFAULT_MYSQL_VER?=	${MYSQL_DEFAULT:S/.//}
 # When adding a version, please keep the comment in
 # Mk/bsd.default-versions.mk in sync.
 MYSQL57_LIBVER=		20
-MYSQL57p_LIBVER=	20
 MYSQL80_LIBVER=		21
 MYSQL81_LIBVER=		22
 .  for v in 5 6 11
@@ -75,13 +72,9 @@ MYSQL10${v}m_LIBVER=	3
 # Setting/finding MySQL version we want.
 .  if exists(${LOCALBASE}/bin/mysql)
 _MYSQL!=	${LOCALBASE}/bin/mysql_config --version | ${SED} -e 's/\([0-9]\{1,2\}\)\.\([0-9]*\).*/\1\2/'
-_PERCONA!=	${LOCALBASE}/bin/mysql --version | ${GREP} Percona | wc -l
 _MARIADB!=	${LOCALBASE}/bin/mysql --version | ${GREP} MariaDB | wc -l
 
-.    if ${_PERCONA} == 1
-_MYSQL_VER=	${_MYSQL}p
-_MYSQL_FLAVOUR=	percona
-.    elif ${_MARIADB} == 1
+.    if ${_MARIADB} == 1
 _MYSQL_VER=	${_MYSQL}m
 _MYSQL_FLAVOUR=	mariadb
 .    else
@@ -93,13 +86,11 @@ _MYSQL_FLAVOUR=	mysql
 .  if defined(_WANT_MYSQL_VER)
 .    if   defined(WITH_MYSQL_VER) && (${WITH_MYSQL_VER:C/[0-9]*//} == "m")
 WITH_MYSQL_FLAVOUR=	mariadb
-.    elif defined(WITH_MYSQL_VER) && (${WITH_MYSQL_VER:C/[0-9]*//} == "p")
-WITH_MYSQL_FLAVOUR=	percona
 .    else
 WITH_MYSQL_FLAVOUR=	mysql
 .    endif
 .    if defined(WITH_MYSQL_VER) && ${WITH_MYSQL_VER} != ${_WANT_MYSQL_VER}
-IGNORE=		cannot install: the port wants ${_WANT_MYSQL_FLAVOUR}${_WANT_MYSQL_VER:C/[mpw]//}-client and you try to install ${WITH_MYSQL_FLAVOUR}${WITH_MYSQL_VER:C/[mpw]//}-client
+IGNORE=		cannot install: the port wants ${_WANT_MYSQL_FLAVOUR}${_WANT_MYSQL_VER:C/[mw]//}-client and you try to install ${WITH_MYSQL_FLAVOUR}${WITH_MYSQL_VER:C/[mw]//}-client
 .    endif
 MYSQL_VER=	${_WANT_MYSQL_VER}
 .  elif defined(WITH_MYSQL_VER)
@@ -114,15 +105,13 @@ MYSQL_VER=	${DEFAULT_MYSQL_VER}
 
 .  if   ${MYSQL_VER:C/[0-9]*//} == "m"
 MYSQL_FLAVOUR=     mariadb
-.  elif ${MYSQL_VER:C/[0-9]*//} == "p"
-MYSQL_FLAVOUR=     percona
 .  else
 MYSQL_FLAVOUR=     mysql
 .  endif
 
 .  if defined(_MYSQL_VER)
 .    if ${_MYSQL_VER} != ${MYSQL_VER}
-IGNORE=		cannot install: MySQL versions mismatch: ${_MYSQL_FLAVOUR}${_MYSQL_VER:C/[mpw]//}-client is installed and wanted version is ${MYSQL_FLAVOUR}${MYSQL_VER:C/[mpw]//}-client
+IGNORE=		cannot install: MySQL versions mismatch: ${_MYSQL_FLAVOUR}${_MYSQL_VER:C/[mw]//}-client is installed and wanted version is ${MYSQL_FLAVOUR}${MYSQL_VER:C/[mw]//}-client
 .    endif
 .  endif
 
@@ -133,9 +122,6 @@ _MYSQL_CLIENT_FLAVOUR=	mariadb
 .    if ${MYSQL_VER:C/m//} >= 105
 _MYSQL_SHLIB=	libmariadb
 .    endif
-.  elif (${MYSQL_VER:C/[0-9]*//} == "p")
-_MYSQL_SERVER_FLAVOUR=	percona
-_MYSQL_CLIENT_FLAVOUR=	percona
 .  elif (${MYSQL_VER:C/[0-9]*//} == "w")
 _MYSQL_SERVER_FLAVOUR=	mysqlwsrep
 _MYSQL_CLIENT_FLAVOUR=	mysql
@@ -144,8 +130,8 @@ _MYSQL_SERVER_FLAVOUR=	mysql
 _MYSQL_CLIENT_FLAVOUR=	mysql
 .  endif
 
-_MYSQL_CLIENT=	databases/${_MYSQL_CLIENT_FLAVOUR}${MYSQL_VER:C/[mp]//}-client
-_MYSQL_SERVER=	databases/${_MYSQL_SERVER_FLAVOUR}${MYSQL_VER:C/[mp]//}-server
+_MYSQL_CLIENT=	databases/${_MYSQL_CLIENT_FLAVOUR}${MYSQL_VER:C/[m]//}-client
+_MYSQL_SERVER=	databases/${_MYSQL_SERVER_FLAVOUR}${MYSQL_VER:C/[m]//}-server
 
 # And now we are checking if we can use it
 .  if defined(MYSQL${MYSQL_VER}_LIBVER)
