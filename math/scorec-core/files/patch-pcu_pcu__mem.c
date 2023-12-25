e7d61ea50e17c79515f0cb34a5730398ba0b4d2f
Yuri Victorovich <yuri@FreeBSD.org>
2023-12-24 18:42:38 -0800

    math/scorec-core: update 2.2.7-55 → 2.2.8
    Reported by:	portscout

--- math/scorec-core/files/patch-pcu_pcu__mem.c
+++ math/scorec-core/files/patch-pcu_pcu__mem.c
@@ -1,6 +1,8 @@
---- pumi/pumi_sys.cc.orig	2022-12-05 09:50:18 UTC
-+++ pumi/pumi_sys.cc
-@@ -98,6 +98,8 @@ double pumi_getMem()
+- use of linux-specific function mallinfo(): https://github.com/SCOREC/core/issues/409
+
+--- pcu/pcu_mem.c.orig	2023-12-24 18:38:48 UTC
++++ pcu/pcu_mem.c
+@@ -44,6 +44,8 @@ double PCU_GetMem() {
    task_info(current_task(), TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count);
    size_t size = (resident ? t_info.resident_size : t_info.virtual_size);
    return (double)size/M;
