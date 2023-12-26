c48fd2014e721f0e4b3a76ef30c2592b75ac85e9
Lorenzo Salvadore <salvadore@FreeBSD.org>
2023-12-26 15:54:03 +0000

    emulators/emulationstation: Update to v2.11.2
    This also fixes the build with GCC 13 [1].
    
    Changes:
    https://github.com/RetroPie/EmulationStation/compare/v2.9.4...v2.11.2
    
    PR:		273676 [1]
    Approved by:	maintainer timeout (> 3 weeks)

--- emulators/emulationstation/files/patch-es-core_src_utils_FileSystemUtil.cpp
+++ emulators/emulationstation/files/patch-es-core_src_utils_FileSystemUtil.cpp
@@ -1,39 +1,85 @@
---- es-core/src/utils/FileSystemUtil.cpp.orig	2020-11-29 11:37:59 UTC
+--- es-core/src/utils/FileSystemUtil.cpp.orig	2023-12-18 20:00:52 UTC
 +++ es-core/src/utils/FileSystemUtil.cpp
-@@ -577,10 +577,10 @@ namespace Utils
- 		bool exists(const std::string& _path)
- 		{
- 			std::string path = getGenericPath(_path);
+@@ -578,10 +578,10 @@ namespace Utils
+ 				CloseHandle(hFile);
+ 			}
+ #else // _WIN32
 -			struct stat64 info;
 +			struct stat info;
  
- 			// check if stat64 succeeded
--			return (stat64(path.c_str(), &info) == 0);
-+			return (stat(path.c_str(), &info) == 0);
+-			// check if lstat64 succeeded
+-			if(lstat64(path.c_str(), &info) == 0)
++			// check if lstat succeeded
++			if(lstat(path.c_str(), &info) == 0)
+ 			{
+ 				resolved.resize(info.st_size);
+ 				if(readlink(path.c_str(), (char*)resolved.data(), resolved.size()) > 0)
+@@ -658,9 +658,9 @@ namespace Utils
+ 			if(pathExistsIndex.find(_path) == pathExistsIndex.cend())
+ 			{
+ 				const std::string path = getGenericPath(_path);
+-				struct stat64 info;
+-				// check if stat64 succeeded
+-				pathExistsIndex[_path] = (stat64(path.c_str(), &info) == 0);
++				struct stat info;
++				// check if stat succeeded
++				pathExistsIndex[_path] = (stat(path.c_str(), &info) == 0);
+ 			}
  
- 		} // exists
- 
-@@ -599,10 +599,10 @@ namespace Utils
+ 			return pathExistsIndex.at(_path);
+@@ -686,10 +686,10 @@ namespace Utils
  		bool isRegularFile(const std::string& _path)
  		{
- 			std::string path = getGenericPath(_path);
--			struct stat64 info;
-+			struct stat info;
+ 			const std::string path = getGenericPath(_path);
+-			struct stat64     info;
++			struct stat     info;
  
- 			// check if stat64 succeeded
+-			// check if stat64 succeeded
 -			if(stat64(path.c_str(), &info) != 0)
++			// check if stat succeeded
 +			if(stat(path.c_str(), &info) != 0)
  				return false;
  
  			// check for S_IFREG attribute
-@@ -670,8 +670,8 @@ namespace Utils
- 		} // isHidden
- #ifndef WIN32 // osx / linux
- 		bool isExecutable(const std::string& _path) {
--			struct stat64 st;
--			if(stat64(_path.c_str(), &st) == 0){
-+			struct stat st;
-+			if(stat(_path.c_str(), &st) == 0){
- 				mode_t perm = st.st_mode;
- 				// regular files and executables but not setuid, setgid, shared text (mode 0755)
- 				mode_t mask = S_IFREG | S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
+@@ -702,10 +702,10 @@ namespace Utils
+ 		bool isDirectory(const std::string& _path)
+ 		{
+ 			const std::string path = getGenericPath(_path);
+-			struct stat64     info;
++			struct stat     info;
+ 
+-			// check if stat64 succeeded
+-			if(stat64(path.c_str(), &info) != 0)
++			// check if stat succeeded
++			if(stat(path.c_str(), &info) != 0)
+ 				return false;
+ 
+ 			// check for S_IFDIR attribute
+@@ -725,10 +725,10 @@ namespace Utils
+ 			if((Attributes != INVALID_FILE_ATTRIBUTES) && (Attributes & FILE_ATTRIBUTE_REPARSE_POINT))
+ 				return true;
+ #else // _WIN32
+-			struct stat64 info;
++			struct stat info;
+ 
+-			// check if lstat64 succeeded
+-			if(lstat64(path.c_str(), &info) != 0)
++			// check if lstat succeeded
++			if(lstat(path.c_str(), &info) != 0)
+ 				return false;
+ 
+ 			// check for S_IFLNK attribute
+@@ -772,10 +772,10 @@ namespace Utils
+ 			// regular files and executables, but not setuid, setgid, shared text
+ 			const mode_t mask = S_IFREG;
+ 			const mode_t mask_exec = S_IXUSR | S_IXGRP | S_IXOTH;
+-			struct stat64 info;
++			struct stat info;
+ 
+-			// check if stat64 succeeded
+-			if(stat64(path.c_str(), &info) != 0)
++			// check if stat succeeded
++			if(stat(path.c_str(), &info) != 0)
+ 				return false;
+ 
+ 			// check for mask attributes
