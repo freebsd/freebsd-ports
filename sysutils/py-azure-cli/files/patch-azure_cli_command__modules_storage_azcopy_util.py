a8ec07669fc7850c8f9a84fa036ffe53cfe7f203
Danilo G. Baio <dbaio@FreeBSD.org>
2023-12-26 15:03:36 -0300

    sysutils/py-azure-cli: Update to 2.55.0
    Changelog:      https://github.com/Azure/azure-cli/blob/azure-cli-2.55.0/src/azure-cli/HISTORY.rst

--- sysutils/py-azure-cli/files/patch-azure_cli_command__modules_storage_azcopy_util.py
+++ sysutils/py-azure-cli/files/patch-azure_cli_command__modules_storage_azcopy_util.py
@@ -1,6 +1,6 @@
---- azure/cli/command_modules/storage/azcopy/util.py.orig	2020-01-31 12:42:39 UTC
+--- azure/cli/command_modules/storage/azcopy/util.py.orig	2023-11-29 03:41:09 UTC
 +++ azure/cli/command_modules/storage/azcopy/util.py
-@@ -50,6 +50,8 @@ class AzCopy(object):
+@@ -70,6 +70,8 @@ class AzCopy:
              file_url = base_url.format('linux', 'amd64', AZCOPY_VERSION, 'tar.gz')
          elif self.system == 'Darwin':
              file_url = base_url.format('darwin', 'amd64', AZCOPY_VERSION, 'zip')
@@ -9,12 +9,12 @@
          else:
              raise CLIError('Azcopy ({}) does not exist.'.format(self.system))
          try:
-@@ -180,7 +182,7 @@ def _get_default_install_location():
-         if not home_dir:
-             return None
-         install_location = os.path.join(home_dir, r'.azcopy\azcopy.exe')
+@@ -206,7 +208,7 @@ def _get_default_install_location():
+     _azcopy_installation_dir = os.path.join(_config_dir, "bin")
+     if system == 'Windows':
+         install_location = os.path.join(_azcopy_installation_dir, 'azcopy.exe')
 -    elif system in ('Linux', 'Darwin'):
 +    elif system in ('Linux', 'Darwin', 'FreeBSD'):
-         install_location = os.path.expanduser(os.path.join('~', 'bin/azcopy'))
+         install_location = os.path.join(_azcopy_installation_dir, 'azcopy')
      else:
-         install_location = None
+         raise CLIError('The {} platform is not currently supported. If you want to know which platforms are supported, '
