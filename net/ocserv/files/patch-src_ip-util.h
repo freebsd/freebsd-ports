7f0a801fe7c33d8ded65e5394daa861730bcf957
Juraj Lutter <otis@FreeBSD.org>
2023-12-27 20:08:56 +0100

    net/ocserv: Update to 1.2.3
    Release notes: https://gitlab.com/openconnect/ocserv/-/releases/1.2.3

--- net/ocserv/files/patch-src_ip-util.h
+++ net/ocserv/files/patch-src_ip-util.h
@@ -1,10 +1,10 @@
---- src/ip-util.h.orig	2023-08-15 11:26:31.522070000 +0300
-+++ src/ip-util.h	2023-08-15 11:28:31.360118000 +0300
+--- src/ip-util.h.orig	2023-12-16 05:18:58 UTC
++++ src/ip-util.h
 @@ -24,6 +24,7 @@
  
  #include <sys/socket.h>
  #include <netinet/in.h>
 +#include <sys/types.h>
  
- #define MAX_IP_STR 46
  // Lower MTU bound is the value defined in RFC 791
+ #define RFC_791_MTU (68)
--- net/ocserv/files/patch-src_occtl_occtl.c
+++ net/ocserv/files/patch-src_occtl_occtl.c
@@ -1,6 +1,6 @@
---- src/occtl/occtl.c.orig	2023-06-16 17:01:03 UTC
+--- src/occtl/occtl.c.orig	2023-12-17 10:19:23 UTC
 +++ src/occtl/occtl.c
-@@ -257,7 +257,7 @@ static int handle_help_cmd(CONN_TYPE * conn, const cha
+@@ -260,7 +260,7 @@ static int handle_help_cmd(CONN_TYPE * conn, const cha
  static int handle_reset_cmd(CONN_TYPE * conn, const char *arg, cmd_params_st *params)
  {
  	rl_reset_terminal(NULL);
