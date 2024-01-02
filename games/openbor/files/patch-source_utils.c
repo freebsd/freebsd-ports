ddacb9107505c843c5db2991219da87d7ec113fb
Jan Beich <jbeich@FreeBSD.org>
2024-01-02 07:34:24 +0100

    games/openbor: update to 7534
    Changes:	https://github.com/DCurrent/openbor/compare/3caaddd5...3bae1dd7

--- games/openbor/files/patch-source_utils.c
+++ games/openbor/files/patch-source_utils.c
@@ -6,14 +6,16 @@ source/utils.c:303:64: error: member reference base type 'int' is not a structur
         writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
                                                      ~~~~~~~~~~^~~~~~
 
---- source/utils.c.orig	2018-07-06 15:13:16 UTC
+--- source/utils.c.orig	2024-01-02 00:04:19 UTC
 +++ source/utils.c
-@@ -303,7 +303,7 @@ void *checkAlloc(void *ptr, size_t size, const char *f
+@@ -303,9 +303,7 @@ void *checkAlloc(void *ptr, size_t size, const char *f
                         "\n*            Shutting Down            *\n\n");
          writeToLogFile("Out of memory!\n");
          writeToLogFile("Allocation of size %i failed in function '%s' at %s:%i.\n", size, func, file, line);
 -#ifndef WIN
+-        writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
+-#elif LINUX
 +#if defined(__GLIBC__) || defined(ANDROID) || defined(VITA)
-         writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
+         writeToLogFile("Memory usage at exit: %u\n", mallinfo2().arena);
  #endif
          borExit(2);
