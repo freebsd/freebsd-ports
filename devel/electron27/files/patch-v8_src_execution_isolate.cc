8c5f09459a63a0875b7154b878fabbf304cc2692
Hiroki Tagato <tagattie@FreeBSD.org>
2024-01-04 23:29:21 +0900

    devel/electron27: update to 27.2.1
    Changelog: https://github.com/electron/electron/releases/tag/v27.2.1
    
    Reported by:	GitHub (watch releases)
    Security:	d1b20e09-dbdf-432b-83c7-89f0af76324a

--- devel/electron27/files/patch-v8_src_execution_isolate.cc
+++ devel/electron27/files/patch-v8_src_execution_isolate.cc
@@ -1,4 +1,4 @@
---- v8/src/execution/isolate.cc.orig	2023-10-19 20:00:39 UTC
+--- v8/src/execution/isolate.cc.orig	2024-01-04 08:55:24 UTC
 +++ v8/src/execution/isolate.cc
 @@ -147,6 +147,10 @@
  #include "src/execution/simulator-base.h"
@@ -11,7 +11,7 @@
  extern "C" const uint8_t v8_Default_embedded_blob_code_[];
  extern "C" uint32_t v8_Default_embedded_blob_code_size_;
  extern "C" const uint8_t v8_Default_embedded_blob_data_[];
-@@ -4034,6 +4038,11 @@ void Isolate::InitializeDefaultEmbeddedBlob() {
+@@ -4052,6 +4056,11 @@ void Isolate::InitializeDefaultEmbeddedBlob() {
    uint32_t code_size = DefaultEmbeddedBlobCodeSize();
    const uint8_t* data = DefaultEmbeddedBlobData();
    uint32_t data_size = DefaultEmbeddedBlobDataSize();
