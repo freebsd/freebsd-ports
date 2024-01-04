9d8c5004a4352e84f0e29327790885c858a5f534
Robert Nagy <rnagy@FreeBSD.org>
2024-01-04 13:59:43 +0100

    www/chromium: update to 120.0.6099.199
    Security:	https://vuxml.freebsd.org/freebsd/3ee577a9-aad4-11ee-86bb-a8a1599412c6.html

--- www/chromium/files/patch-third__party_sqlite_src_amalgamation_sqlite3.c
+++ www/chromium/files/patch-third__party_sqlite_src_amalgamation_sqlite3.c
@@ -1,6 +1,6 @@
---- third_party/sqlite/src/amalgamation/sqlite3.c.orig	2023-12-10 06:10:27 UTC
+--- third_party/sqlite/src/amalgamation/sqlite3.c.orig	2024-01-04 08:02:45 UTC
 +++ third_party/sqlite/src/amalgamation/sqlite3.c
-@@ -44312,7 +44312,12 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
+@@ -44313,7 +44313,12 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
    memset(zBuf, 0, nBuf);
    randomnessPid = osGetpid(0);
  #if !defined(SQLITE_TEST) && !defined(SQLITE_OMIT_RANDOMNESS)
@@ -13,7 +13,7 @@
      int fd, got;
      fd = robust_open("/dev/urandom", O_RDONLY, 0);
      if( fd<0 ){
-@@ -44327,6 +44332,7 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
+@@ -44328,6 +44333,7 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
        robust_close(0, fd, __LINE__);
      }
    }
