f1885a5e16f3ec91ade46d40035ae4c0a2a44cf8
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2024-01-05 15:46:58 -0500

    security/bareos-*: Update to 22.1.3
    - Fix pkg-plist [1]
    
    PR:		270967
    Reported by:	ddrinnon _at_ cdor.net [1]

--- sysutils/bareos-server/files/patch-core-cmake_BareosInstallConfigFiles.cmake
+++ sysutils/bareos-server/files/patch-core-cmake_BareosInstallConfigFiles.cmake
@@ -1,33 +1,41 @@
---- core/cmake/BareosInstallConfigFiles.cmake	2020-12-16 02:46:16.000000000 -0500
-+++ core/cmake/BareosInstallConfigFiles.cmake	2020-12-23 22:41:21.832081000 -0500
-@@ -55,20 +55,12 @@
-     get_filename_component(resname ${resdir} NAME)
-     foreach(configfile ${configfiles})
-       get_filename_component(fname ${configfile} NAME)
--      if(EXISTS ${DESTCONFDIR}/${resname}/${fname})
--        message(STATUS "${DESTCONFDIR}/${resname}/${fname} exists")
--        message(STATUS "rename ${configfile} to ${configfile}.new")
--        file(RENAME "${configfile}" "${configfile}.new")
--
--        message(STATUS "copy ${configfile}.new to ${DESTCONFDIR}/${resname}")
--        file(COPY "${configfile}.new" DESTINATION "${DESTCONFDIR}/${resname}")
--        file(RENAME "${configfile}.new" "${configfile}")
--      else()
--        message(
--          STATUS "${resname}/${fname} as ${resname}/${fname} (new installation)"
--        )
--        file(COPY "${configfile}" DESTINATION "${DESTCONFDIR}/${resname}")
--      endif()
-+      message(STATUS "${resname}/${fname} as ${resname}/${fname}.sample (new installation)")
-+      file(RENAME "${configfile}" "${configfile}.sample")
-+      file(
-+       COPY "${configfile}.sample"
-+       DESTINATION "${DESTCONFDIR}/${resname}"
-+      )
-     endforeach()
-   endforeach()
+--- core/cmake/BareosInstallConfigFiles.cmake	2023-12-13 07:48:37.000000000 -0500
++++ core/cmake/BareosInstallConfigFiles.cmake	2024-01-05 01:02:51.562611000 -0500
+@@ -27,27 +27,13 @@
+   endif()
  
-@@ -104,23 +96,12 @@
+   get_filename_component(fname ${configfile} NAME)
+-  if(EXISTS ${DESTCONFDIR}${additionalconfigdir}/${fname})
+-    message(
+-      STATUS "${DESTCONFDIR}${additionalconfigdir}/${fname} already exists"
+-    )
+-    message(
+-      STATUS
+-        "Installing config: ${DESTCONFDIR}${additionalconfigdir}/${fname}.new"
+-    )
+-    file(RENAME "${configfile}" "${configfile}.new")
+-    file(COPY "${configfile}.new"
+-         DESTINATION "${DESTCONFDIR}${additionalconfigdir}"
+-    )
+-    file(RENAME "${configfile}.new" "${configfile}")
+-  else()
+-    message(
+-      STATUS "Installing config: ${DESTCONFDIR}${additionalconfigdir}/${fname}"
+-    )
+-    file(COPY "${configfile}"
+-         DESTINATION "${DESTCONFDIR}${additionalconfigdir}"
+-    )
+-  endif()
++
++  message(STATUS "${DESTCONFDIR}${additionalconfigdir}/${fname} as ${DESTCONFDIR}${additionalconfigdir}/${fname}.sample (new installation)")
++  file(RENAME "${configfile}" "${configfile}.sample")
++  file(                                                             
++       COPY "${configfile}.sample"                                      
++       DESTINATION "${DESTCONFDIR}${additionalconfigdir}"
++  )
+ endfunction()
+ 
+ macro(BareosInstallConfigFiles CONFDIR CONFIGBASEDIRECTORY PLUGINS BACKENDS
+@@ -125,23 +111,12 @@
        get_filename_component(dir ${configfile} DIRECTORY)
        get_filename_component(fname ${configfile} NAME)
  
@@ -48,7 +56,7 @@
 -             DESTINATION "${DESTCONFDIR}/${dir}"
 -        )
 -      endif()
-+      message(STATUS "${configfile} as ${configfile}")
++      message(STATUS "${configfile} as ${configfile}.sample")
 +      file(RENAME "${BackendConfigSrcDir}/${configfile}" "${BackendConfigSrcDir}/${configfile}.sample")
 +      file(
 +       COPY "${BackendConfigSrcDir}/${configfile}.sample"
@@ -57,7 +65,7 @@
      endforeach()
  
      file(
-@@ -130,7 +111,8 @@
+@@ -151,7 +126,8 @@
      )
      foreach(configfile ${configfiles})
        get_filename_component(dir ${configfile} DIRECTORY)
@@ -67,22 +75,20 @@
  
        if(EXISTS ${DESTCONFDIR}/${configfile})
          message(STATUS "overwriting ${configfile}")
-@@ -138,9 +120,11 @@
+@@ -159,8 +135,10 @@
          message(STATUS "${configfile} as ${configfile}")
        endif()
  
 -      file(COPY "${BackendConfigSrcDir}/${configfile}"
 -           DESTINATION "${DESTCONFDIR}/${dir}"
--      )
 +      file(RENAME "${BackendConfigSrcDir}/${configfile}" "${BackendConfigSrcDir}/${fsname}.conf.sample")
-+       file(
++      file(
 +       COPY "${BackendConfigSrcDir}/${fsname}.conf.sample"
 +       DESTINATION "${DESTCONFDIR}/${dir}"
-+       )
+       )
      endforeach()
  
-   endforeach()
-@@ -167,20 +151,13 @@
+@@ -188,20 +166,13 @@
          string(REGEX MATCH "\\.in\$" IS_INFILE ${configfile})
          if(NOT "${IS_INFILE}" STREQUAL ".in")
            get_filename_component(fname ${configfile} NAME)
