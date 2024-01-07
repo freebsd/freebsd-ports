f7d0c259cce55b0da69350975d6f896f030eaa6f
Hiroki Sato <hrs@FreeBSD.org>
2024-01-08 02:30:34 +0900

    japanese/kterm: Resurrect kterm-6.2.0

---
+++ japanese/kterm/files/patch-VTparse.h
@@ -0,0 +1,7 @@
+--- VTparse.h.orig	2016-11-04 21:41:21 UTC
++++ VTparse.h
+@@ -118,3 +118,4 @@
+ #define CASE_SCS_STATE 77
+ #define CASE_GSET_VERSION_STATE 78
+ #define CASE_GSET_VERSION 79
++#define CASE_ECH 80
