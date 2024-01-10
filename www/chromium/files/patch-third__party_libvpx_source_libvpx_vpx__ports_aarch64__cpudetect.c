bb5b510ef5c64ddd9a9c3b465f18808900c5780b
Robert Nagy <rnagy@FreeBSD.org>
2024-01-10 16:47:13 +0100

    www/chromium: update to 120.0.6099.216
    include a better arm64 cpu feature detection patch from jbeich@ while here
    
    Security:	https://vuxml.freebsd.org/freebsd/ec8e4040-afcd-11ee-86bb-a8a1599412c6.html

--- www/chromium/files/patch-third__party_libvpx_source_libvpx_vpx__ports_aarch64__cpudetect.c
+++ www/chromium/files/patch-third__party_libvpx_source_libvpx_vpx__ports_aarch64__cpudetect.c
@@ -1,20 +1,27 @@
---- third_party/libvpx/source/libvpx/vpx_ports/aarch64_cpudetect.c.orig	2024-01-08 12:52:53 UTC
+--- third_party/libvpx/source/libvpx/vpx_ports/aarch64_cpudetect.c.orig	2024-01-09 18:29:07 UTC
 +++ third_party/libvpx/source/libvpx/vpx_ports/aarch64_cpudetect.c
-@@ -153,7 +153,16 @@ static int arm_get_cpu_caps(void) {
+@@ -91,9 +91,23 @@ static int arm_get_cpu_caps(void) {
    return flags;
  }
  
--#else  // end __Fuchsia__
-+#elif defined(__FreeBSD__)  // end __Fuchsia__
+-#elif defined(__linux__)  // end defined(VPX_USE_ANDROID_CPU_FEATURES)
++#elif defined(__linux__) || defined(__FreeBSD__)  // end defined(VPX_USE_ANDROID_CPU_FEATURES)
+ 
+ #include <sys/auxv.h>
 +
-+int arm_get_cpu_caps(void) {
-+  int flags = 0;
-+#if HAVE_NEON
-+  flags |= HAS_NEON;  // Neon is mandatory in Armv8.0-A.
-+#endif  // HAVE_NEON
-+  return flags;
++#if defined(__FreeBSD__)
++static unsigned long getauxval(unsigned long type)
++{
++    /* Only AT_HWCAP* return unsigned long */
++    if (type != AT_HWCAP && type != AT_HWCAP2) {
++        return 0;
++    }
++
++    unsigned long ret = 0;
++    elf_aux_info(type, &ret, sizeof(ret));
++    return ret;
 +}
-+#else  // end __FreeBSD__
- #error \
-     "Runtime CPU detection selected, but no CPU detection method available" \
- "for your platform. Rerun configure with --disable-runtime-cpu-detect."
++#endif  
+ 
+ // Define hwcap values ourselves: building with an old auxv header where these
+ // hwcap values are not defined should not prevent features from being enabled.
