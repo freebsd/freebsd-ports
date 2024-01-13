b57ae9b9d7196c40b44bfb7e06f58d34476b177e
Hiroki Tagato <tagattie@FreeBSD.org>
2024-01-13 22:37:16 +0900

    devel/electron27: update to 27.2.2
    Changelog: https://github.com/electron/electron/releases/tag/v27.2.2
    
    Reported by:	GitHub (watch releases)
    MFH:		2024Q1
    Security:	28b42ef5-80cd-440c-904b-b7fbca74c73d
    
    (cherry picked from commit 3f073f899db00433b9c67b61e48e8e4c07a48495)

--- devel/electron27/files/patch-electron_spec_api-browser-window-spec.ts
+++ devel/electron27/files/patch-electron_spec_api-browser-window-spec.ts
@@ -1,4 +1,4 @@
---- electron/spec/api-browser-window-spec.ts.orig	2023-12-21 18:00:06 UTC
+--- electron/spec/api-browser-window-spec.ts.orig	2024-01-10 11:28:08 UTC
 +++ electron/spec/api-browser-window-spec.ts
 @@ -64,7 +64,7 @@ describe('BrowserWindow module', () => {
        }).not.to.throw();
@@ -144,7 +144,7 @@
      // Not implemented on Linux.
      afterEach(closeAllWindows);
  
-@@ -6398,7 +6398,7 @@ describe('BrowserWindow module', () => {
+@@ -6411,7 +6411,7 @@ describe('BrowserWindow module', () => {
    describe('"transparent" option', () => {
      afterEach(closeAllWindows);
  
--- devel/electron27/files/patch-third__party_sqlite_src_amalgamation_sqlite3.c
+++ devel/electron27/files/patch-third__party_sqlite_src_amalgamation_sqlite3.c
@@ -1,6 +1,6 @@
---- third_party/sqlite/src/amalgamation/sqlite3.c.orig	2023-08-10 01:51:18 UTC
+--- third_party/sqlite/src/amalgamation/sqlite3.c.orig	2024-01-13 03:15:12 UTC
 +++ third_party/sqlite/src/amalgamation/sqlite3.c
-@@ -43843,7 +43843,12 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
+@@ -43844,7 +43844,12 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
    memset(zBuf, 0, nBuf);
    randomnessPid = osGetpid(0);
  #if !defined(SQLITE_TEST) && !defined(SQLITE_OMIT_RANDOMNESS)
@@ -13,7 +13,7 @@
      int fd, got;
      fd = robust_open("/dev/urandom", O_RDONLY, 0);
      if( fd<0 ){
-@@ -43858,6 +43863,7 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
+@@ -43859,6 +43864,7 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
        robust_close(0, fd, __LINE__);
      }
    }
