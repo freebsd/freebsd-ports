45ce0001fe3e1e35ccc5e5232db7149ca381c1ac
Jan Beich <jbeich@FreeBSD.org>
2024-01-18 04:04:00 +0100

    x11-toolkits/wlroots: prefix local variable in override.mk
    SOVERSION may be used in consumers for their own libraries.

--- x11-toolkits/wlroots015/override.mk
+++ x11-toolkits/wlroots015/override.mk
@@ -1,7 +1,7 @@
 WLR_SUFFIX=	015
-SOVERSION=	10
+WLR_SOVERSION=	10
 
-LIB_DEPENDS:=	${LIB_DEPENDS:S/wlroots.so/&.${SOVERSION}/:S/wlroots$/&${WLR_SUFFIX}/}
+LIB_DEPENDS:=	${LIB_DEPENDS:S/wlroots.so/&.${WLR_SOVERSION}/:S/wlroots$/&${WLR_SUFFIX}/}
 
 USES+=		localbase # -isystem
 CONFIGURE_ENV+=	PKG_CONFIG_PATH="${LOCALBASE}/wlroots${WLR_SUFFIX}/libdata/pkgconfig"
