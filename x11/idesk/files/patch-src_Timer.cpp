ff4bc28d1eb9df3c651af62568bd848fa59c0f67
Nuno Teixeira <eduardo@FreeBSD.org>
2024-01-18 22:05:42 +0000

    x11/idesk: Fix build with graphics/imlib2 1.12.1
    - Fix build with imlib2 1.12.1 due to imlib2-config drop in 1.7.5
      in favor of pkgconfig
    - Fix SN option build (lib depends)
    - Add license
    - Pet portlint, portclippy, portfmt and Q/A check
    
    PR:		276370

--- x11/idesk/files/patch-src_Timer.cpp
+++ x11/idesk/files/patch-src_Timer.cpp
@@ -1,5 +1,5 @@
---- src/Timer.cpp.orig	Mon Aug 15 07:26:39 2005
-+++ src/Timer.cpp	Tue Sep 13 23:14:32 2005
+--- src/Timer.cpp.orig	2005-08-15 05:26:39 UTC
++++ src/Timer.cpp
 @@ -26,6 +26,7 @@
  #include "Database.h"
  #include <X11/Xlib.h>
