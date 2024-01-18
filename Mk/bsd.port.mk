0592024317abf61f3b824e4413ca301386c20d80
Olivier Cochard <olivier@FreeBSD.org>
2024-01-18 21:07:45 +0100

    features: Add support to enable sanitizers
    Can be activated/deactivated globally via
    WITH_SANITIZE
    
    Can be activated individualy via
    WITH_SANITIZE_PORTS
    
    Each port can individually mark itself as not supporting the feature
    via SANITIZE_UNSAFE=
    
    Approved by:	bapt, arrowd
    Sponsored by:	Netflix
    Differential Revision:	https://reviews.freebsd.org/D43497

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1016,7 +1016,7 @@ LC_ALL=		C
 # These need to be absolute since we don't know how deep in the ports
 # tree we are and thus can't go relative.  They can, of course, be overridden
 # by individual Makefiles or local system make configuration.
-_LIST_OF_WITH_FEATURES=	debug lto ssp pie relro bind_now
+_LIST_OF_WITH_FEATURES=	bind_now debug lto pie relro sanitize ssp
 _DEFAULT_WITH_FEATURES=	ssp
 PORTSDIR?=		/usr/ports
 LOCALBASE?=		/usr/local
