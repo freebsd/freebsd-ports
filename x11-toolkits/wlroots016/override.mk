21a74b9aac80dc16a8b0392bb1cf9c410649c35c
Jan Beich <jbeich@FreeBSD.org>
2024-01-18 03:02:37 +0100

    x11-toolkits/wlroots: dedup *_DEPENDS in consumers
    LIB_DEPENDS against a port with non-default USE_LDCONFIG can desync
    RUN_DEPENDS and BUILD_DEPENDS, leading to incorrectly registered
    dependencies when built in a dirty environment.
    
    $ make install -C x11-toolkits/wlroots
    $ make install -C x11-wm/hikari
    $ pkg info -xr wlroots
    wlroots-0.17.1:
            hikari-2.3.3_4
    wlroots015-0.15.1_2:
    $ make run-depends-list -C x11-wm/hikari | fgrep wlroots
    /usr/ports/main/x11-toolkits/wlroots015
    $ make actual-package-depends -C x11-wm/hikari | fgrep wlroots
    "wlroots": {origin: "x11-toolkits/wlroots", version: "0.17.1"}

--- x11-toolkits/wlroots016/override.mk
+++ x11-toolkits/wlroots016/override.mk
@@ -1,7 +1,7 @@
 WLR_SUFFIX=	016
+SOVERSION=	11
 
-BUILD_DEPENDS:=	${BUILD_DEPENDS:S/wlroots/&${WLR_SUFFIX}/g}
-LIB_DEPENDS:=	${LIB_DEPENDS:S/wlroots$/&${WLR_SUFFIX}/}
+LIB_DEPENDS:=	${LIB_DEPENDS:S/wlroots.so/&.${SOVERSION}/:S/wlroots$/&${WLR_SUFFIX}/}
 
 USES+=		localbase # -isystem
 CONFIGURE_ENV+=	PKG_CONFIG_PATH="${LOCALBASE}/wlroots${WLR_SUFFIX}/libdata/pkgconfig"
