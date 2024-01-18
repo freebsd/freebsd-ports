08cdb353ba38378bf3aebddef2ad98ebb2ced2cd
Renato Botelho <garga@FreeBSD.org>
2024-01-18 14:41:50 -0300

    multimedia/blind: Move manpages to share/man
    Approved by:	portmgr (blanket)

--- multimedia/blind/files/patch-config.mk
+++ multimedia/blind/files/patch-config.mk
@@ -1,13 +1,12 @@
 --- config.mk.orig	2017-05-06 11:27:39 UTC
 +++ config.mk
-@@ -2,10 +2,10 @@
+@@ -2,10 +2,10 @@ VERSION = 1.1
  VERSION = 1.1
  
  # Paths
 -PREFIX = /usr/local
--MANPREFIX = $(PREFIX)/share/man
 +PREFIX ?= /usr/local
-+MANPREFIX = $(PREFIX)/man
+ MANPREFIX = $(PREFIX)/share/man
  
  # You may want to remove -DHAVE_PRCTL and -DHAVE_EPOLL from CPPFLAGS if you are not using Linux.
 -CFLAGS   = -std=c99 -Wall -pedantic -O2
