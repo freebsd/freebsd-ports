1b31dfe05a6f52ecbc1514868d723abb98b99d4f
Yuri Victorovich <yuri@FreeBSD.org>
2024-01-21 10:21:40 -0800

    science/spheral: update 2023.06.0 → 2024.01.0
    Reported by:	portscout

--- science/spheral/files/patch-cmake_SetupSpheral.cmake
+++ science/spheral/files/patch-cmake_SetupSpheral.cmake
@@ -1,13 +1,13 @@
 - eliminate the need to use git
 
---- cmake/SetupSpheral.cmake.orig	2023-07-06 16:44:17 UTC
+--- cmake/SetupSpheral.cmake.orig	2024-01-20 00:52:15 UTC
 +++ cmake/SetupSpheral.cmake
-@@ -176,7 +176,7 @@ if (ENABLE_TESTS)
- 
-   # Find the test files we want to install
-   execute_process(
--    COMMAND git ls-files tests
-+    COMMAND find tests -type f
-     WORKING_DIRECTORY ${SPHERAL_ROOT_DIR}
-     OUTPUT_VARIABLE test_files1)
-   string(REPLACE "\n" " " test_files ${test_files1})
+@@ -169,7 +169,7 @@ if (ENABLE_TESTS)
+   set(test_files1 "")
+   if (EXISTS "${CMAKE_SOURCE_DIR}/.git")
+     execute_process(
+-      COMMAND git ls-files tests
++      COMMAND find tests -type f
+       WORKING_DIRECTORY ${SPHERAL_ROOT_DIR}
+       OUTPUT_VARIABLE test_files1)
+   else()
