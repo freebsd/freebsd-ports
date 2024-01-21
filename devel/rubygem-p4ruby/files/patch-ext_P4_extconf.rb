2c2da76dd90366799ab5ee9dfa3748e14b2eb344
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2024-01-21 11:55:44 +0100

    devel/rubygem-p4ruby: Update version 2017.1.1599185=>2023.1.2544327
    - Fix build with ruby32
    - Adopt port
    
    Changelog: https://github.com/perforce/p4ruby/releases/tag/2023.1
    
    PR:		276466

--- devel/rubygem-p4ruby/files/patch-ext_P4_extconf.rb
+++ devel/rubygem-p4ruby/files/patch-ext_P4_extconf.rb
@@ -1,6 +1,6 @@
---- ext/P4/extconf.rb.orig	2016-03-25 23:50:09.485520000 +0000
-+++ ext/P4/extconf.rb	2016-03-25 23:52:32.288698000 +0000
-@@ -354,22 +354,8 @@
+--- ext/P4/extconf.rb.orig	2024-01-21 10:40:57 UTC
++++ ext/P4/extconf.rb
+@@ -384,23 +384,8 @@ def resolve_p4api_dir
  
    # When running rake compile, use this instead of other options, I'm not sure how
    # gem/bundler options are passed through via rake
@@ -10,7 +10,7 @@
 -  end
 -
 -  if !p4api_dir && !with_config('p4api-dir') && enable_config('p4api-download', true)
--    download_api_via_ftp
+-    download_api_via_https
 -    unzip_file
 -    p4api_dir = downloaded_p4api_dir
 -    dir_config('p4api', "#{p4api_dir}/include", "#{p4api_dir}/lib")
@@ -20,25 +20,33 @@
 -  elsif !p4api_dir
 -    raise '--with-p4api-dir option has not been specified, and --disable-p4api-download is in effect'
 -  end
+-
 +  p4api_dir = with_config('p4api-dir')
 +  dir_config('supp', "#{p4api_dir}/include/perforce", "#{p4api_dir}/lib/perforce")
- 
    p4api_dir
  end
-@@ -569,7 +555,8 @@
+ 
+@@ -455,12 +440,12 @@ def p4_platform_label
+       raise 'Automatic fetching of p4api from perforce FTP is not supported on Windows'
+     when /darwin19|darwin[2-9][0-9]/
+       "macosx1015#{p4_cpu(:darwin)}"
+-    when /darwin/      
++    when /darwin/
+       "darwin90#{p4_cpu(:darwin)}"
+     when /solaris/
+       "solaris10#{p4_cpu(:solaris)}"
+     when /linux/
+-      "linux26#{p4_cpu(:linux)}"    
++      "linux26#{p4_cpu(:linux)}"
+   end
+ end
+ 
+@@ -586,7 +571,7 @@ puts "$libs #{$libs}"
  puts "$libs #{$libs}"
  
  # Parse the Version file into a ruby structure
 -version_info = P4ApiVersion.load(p4api_dir)
-+#version_info = P4ApiVersion.load(p4api_dir)
 +version_info = P4ApiVersion.load("#{p4api_dir}/share/examples/p4api")
  create_p4rubyconf_header(version_info, $libs)
  
  # This will generate a standard extconf.h based on what we discover locally.
-@@ -577,4 +564,4 @@
- # don't believe we need to rely on actually.
- create_header
- 
--create_makefile('P4')
-\ No newline at end of file
-+create_makefile('P4')
