cea6be3b48fa041c65b505f8383f86bfb15b6ad1
Wen Heping <wen@FreeBSD.org>
2024-01-25 03:17:39 +0000

    graphics/mypaint: Fix build with python-3.11

--- graphics/mypaint/files/patch-setup.py
+++ graphics/mypaint/files/patch-setup.py
@@ -1,5 +1,14 @@
---- setup.py.orig	2022-01-09 11:24:00 UTC
+--- setup.py.orig	2020-05-29 15:18:55 UTC
 +++ setup.py
+@@ -632,7 +632,7 @@ class InstallScripts (install_scripts):
+         self.announce("installing %s as %s" % (src, targ_basename), level=2)
+         if self.dry_run:
+             return []
+-        with open(src, "rU") as in_fp:
++        with open(src, "r") as in_fp:
+             with open(targ, "w") as out_fp:
+                 line = in_fp.readline().rstrip()
+                 if line.startswith("#!"):
 @@ -867,7 +867,6 @@ def get_ext_modules():
          '-Wall',
          '-Wno-sign-compare',
