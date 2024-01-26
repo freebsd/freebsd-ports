4c5b01c37aa273ddd476f67ba63569a1e6c7eeda
Ryan Steinmetz <zi@FreeBSD.org>
2024-01-26 10:03:09 -0500

    databases/mysqltcl: Unbreak with MySQL 8.x, bump PORTREVISION

---
+++ databases/mysqltcl/files/patch-generic_mysqltcl.c
@@ -0,0 +1,11 @@
+--- generic/mysqltcl.c.orig	2024-01-26 14:58:40 UTC
++++ generic/mysqltcl.c
+@@ -911,7 +911,7 @@ static int Mysqltcl_Connect(ClientData clientData, Tcl
+ #if (MYSQL_VERSION_ID>=32350)
+   if(mysql_options_reconnect)
+   {
+-      my_bool reconnect = 1;
++      bool reconnect = 1;
+       mysql_options(handle->connection, MYSQL_OPT_RECONNECT, &reconnect);
+   }
+   mysql_options(handle->connection,MYSQL_READ_DEFAULT_GROUP,groupname);
