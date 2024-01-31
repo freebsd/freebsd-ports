b49fedd5dc6d00d8f130972c17d5cd75fbd718fb
Jan Beich <jbeich@FreeBSD.org>
2024-01-31 19:34:10 +0100

    games/openbor: update to 7610
    Changes:	https://github.com/DCurrent/openbor/compare/87ea891e...f4510854

--- games/openbor/files/patch-source_utils.c
+++ games/openbor/files/patch-source_utils.c
@@ -1,3 +1,6 @@
+source/utils.c:18:10: fatal error: 'features.h' file not found
+   18 | #include <features.h>
+      |          ^~~~~~~~~~~~
 source/utils.c:303:54: error: implicit declaration of function 'mallinfo' is invalid in C99
       [-Werror,-Wimplicit-function-declaration]
         writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
@@ -6,9 +9,18 @@ source/utils.c:303:64: error: member reference base type 'int' is not a structur
         writeToLogFile("Memory usage at exit: %u\n", mallinfo().arena);
                                                      ~~~~~~~~~~^~~~~~
 
---- source/utils.c.orig	2024-01-02 00:04:19 UTC
+--- source/utils.c.orig	2024-02-01 15:55:11 UTC
 +++ source/utils.c
-@@ -303,9 +303,7 @@ void *checkAlloc(void *ptr, size_t size, const char *f
+@@ -14,7 +14,7 @@
+ #include <locale.h>
+ #include <math.h>
+ 
+-#ifdef LINUX
++#if defined(__linux__)
+ #include <features.h>
+ #endif
+ 
+@@ -307,9 +307,7 @@ void *checkAlloc(void *ptr, size_t size, const char *f
                         "\n*            Shutting Down            *\n\n");
          writeToLogFile("Out of memory!\n");
          writeToLogFile("Allocation of size %i failed in function '%s' at %s:%i.\n", size, func, file, line);
