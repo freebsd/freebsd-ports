f8de28e40192090b9c4d1e3a7e5fb5c07322dd32
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2024-01-31 17:07:41 +0100

    net-mgmt/py-aggregate6: Moved man to share/man
    Approved by:    portmgr (blanket)

--- net-mgmt/py-aggregate6/files/patch-setup.py
+++ net-mgmt/py-aggregate6/files/patch-setup.py
@@ -1,6 +1,6 @@
 --- setup.py.orig	2017-12-01 09:46:38 UTC
 +++ setup.py
-@@ -70,8 +70,7 @@ setup(
+@@ -70,13 +70,12 @@ setup(
          'Programming Language :: Python :: 3',
          'Programming Language :: Python :: 3.6'
      ],
@@ -10,3 +10,9 @@
          ["future", "ipaddress"] if sys.version_info.major == 2 else []
      ),
      packages=find_packages(exclude=['tests', 'tests.*']),
+     entry_points={'console_scripts':
+                   ['aggregate6 = aggregate6.aggregate6:main']},
+-    data_files = [('man/man7', ['aggregate6.7'])],
++    data_files = [('share/man/man7', ['aggregate6.7'])],
+     test_suite='nose.collector'
+ )
