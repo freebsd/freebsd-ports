dd4b8a6c2444f98bbd28fc9257034bf67d06fd2c
Daniel Engberg <diizzy@FreeBSD.org>
2024-02-04 23:38:14 +0100

    dns/godns: Update to 3.0.7
    Changelog:
    https://github.com/TimothyYe/godns/releases/tag/v3.0.7

--- dns/godns/files/patch-cmd_godns_godns.go
+++ dns/godns/files/patch-cmd_godns_godns.go
@@ -1,12 +1,12 @@
---- cmd/godns/godns.go.orig	2023-01-19 15:20:32 UTC
+--- cmd/godns/godns.go.orig	2024-02-04 03:14:27 UTC
 +++ cmd/godns/godns.go
-@@ -16,11 +16,11 @@ import (
+@@ -22,11 +22,11 @@ var (
  
  var (
- 	configuration settings.Settings
--	optConf       = flag.String("c", "./config.json", "Specify a config file")
-+	optConf       = flag.String("c", "%%PREFIX%%/etc/godns-config.json", "Specify a config file")
- 	optHelp       = flag.Bool("h", false, "Show help")
+ 	config  settings.Settings
+-	optConf = flag.String("c", "./config.json", "Specify a config file")
++	optConf = flag.String("c", "%%PREFIX%%/etc/godns-config.json", "Specify a config file")
+ 	optHelp = flag.Bool("h", false, "Show help")
  
  	// Version is current version of GoDNS.
 -	Version = "0.1"
