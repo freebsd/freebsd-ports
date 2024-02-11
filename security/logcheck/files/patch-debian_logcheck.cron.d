4d161fff033445025c42bd05f0eba6aebed15563
Dan Langille <dvl@FreeBSD.org>
2024-02-11 11:19:54 +0900

    security/logcheck: Add patch to change crontab entry
    When the host is heavily loaded, the log files can grow incredibly and
    one logcheck invocation may not complete before it is invoked next
    time.
    
    This commit add patch to change crontab entries so it uses lockf(1) to
    avoid multiple invocation of logcheck under such situation as above.
    
    PR:		276023

--- security/logcheck/files/patch-debian_logcheck.cron.d
+++ security/logcheck/files/patch-debian_logcheck.cron.d
@@ -1,4 +1,4 @@
---- debian/logcheck.cron.d.orig	2017-01-25 21:08:04 UTC
+--- debian/logcheck.cron.d.orig	2023-07-18 10:54:00 UTC
 +++ debian/logcheck.cron.d
 @@ -1,9 +1,5 @@
 -# /etc/cron.d/logcheck: crontab entries for the logcheck package
@@ -12,5 +12,5 @@
 -2 * * * *       logcheck    if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck; fi
 -
 -# EOF
-+@reboot    if [ -x %%PREFIX%%/sbin/logcheck ]; then nice -n10 %%PREFIX%%/sbin/logcheck -R; fi
-+2 * * * *  if [ -x %%PREFIX%%/sbin/logcheck ]; then nice -n10 %%PREFIX%%/sbin/logcheck; fi
++@reboot    if [ -x %%PREFIX%%/sbin/logcheck ]; then /usr/bin/lockf -t 0 /tmp/.logcheck nice -n10 %%PREFIX%%/sbin/logcheck -R; fi
++2 * * * *  if [ -x %%PREFIX%%/sbin/logcheck ]; then /usr/bin/lockf -t 0 /tmp/.logcheck nice -n10 %%PREFIX%%/sbin/logcheck; fi
