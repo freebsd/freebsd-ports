811ac0d6f493d79212cce590f181b3c961c84b4f
Hiroki Tagato <tagattie@FreeBSD.org>
2024-02-11 23:30:01 +0900

    editors/vscode: update to 1.86.1
    Changelog: https://code.visualstudio.com/updates/v1_86
    
    Reported by:	GitHub (watch releases)

--- editors/vscode/files/patch-build_npm_postinstall.js
+++ editors/vscode/files/patch-build_npm_postinstall.js
@@ -1,4 +1,4 @@
---- build/npm/postinstall.js.orig	2024-01-31 22:36:21 UTC
+--- build/npm/postinstall.js.orig	2024-02-07 08:28:46 UTC
 +++ build/npm/postinstall.js
 @@ -41,8 +41,14 @@ function yarnInstall(dir, opts) {
  	const raw = process.env['npm_config_argv'] || '{}';
@@ -17,7 +17,7 @@
  	if (opts.ignoreEngines) {
  		args.push('--ignore-engines');
  		delete opts.ignoreEngines;
-@@ -116,5 +122,5 @@ for (let dir of dirs) {
+@@ -131,5 +137,5 @@ for (let dir of dirs) {
  	yarnInstall(dir, opts);
  }
  
