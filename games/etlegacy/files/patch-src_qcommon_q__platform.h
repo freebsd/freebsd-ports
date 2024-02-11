27fd645f259deb9bc33ab0eb7582b466b62bac92
Piotr Kubaj <pkubaj@FreeBSD.org>
2024-02-11 12:46:26 +0100

    games/etlegacy: fix build on armv7, powerpc and powerpc64

--- games/etlegacy/files/patch-src_qcommon_q__platform.h
+++ games/etlegacy/files/patch-src_qcommon_q__platform.h
@@ -1,6 +1,6 @@
---- src/qcommon/q_platform.h.orig	2024-01-16 23:51:21 UTC
+--- src/qcommon/q_platform.h.orig	2023-03-12 18:33:03 UTC
 +++ src/qcommon/q_platform.h
-@@ -264,6 +264,8 @@
+@@ -264,12 +264,20 @@
  
  #ifdef __i386__
  #define ARCH_STRING "i386"
@@ -9,3 +9,15 @@
  #elif defined __amd64__
  #undef idx64
  #define idx64 1
+ #define ARCH_STRING "x86_64"
+ #elif defined __axp__
+ #define ARCH_STRING "alpha"
++#elif defined __powerpc64__
++#define ARCH_STRING "ppc64"
++#elif defined __powerpc__
++#define ARCH_STRING "ppc"
++#elif defined __arm__
++#define ARCH_STRING "arm" // __ARM_ARCH_'V'__ FIXME: add ARM version to the ARCH_STRING
+ #endif
+ 
+ #if BYTE_ORDER == BIG_ENDIAN
