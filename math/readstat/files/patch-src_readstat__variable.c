013b958d94ac91a14d3d6b2b3db24c1ce88ee7ed
Jason E. Hale <jhale@FreeBSD.org>
2024-02-11 19:55:48 -0500

    math/readstat: Update to 1.1.9
    MFH:		2024Q1
    Security:	388eefc0-c93f-11ee-92ce-4ccc6adda413

--- math/readstat/files/patch-src_readstat__variable.c
+++ math/readstat/files/patch-src_readstat__variable.c
@@ -1,11 +1,7 @@
---- src/readstat_variable.c.orig	2020-08-25 16:29:50 UTC
+--- src/readstat_variable.c.orig	2023-02-20 19:09:20 UTC
 +++ src/readstat_variable.c
-@@ -2,10 +2,10 @@
- #include <stdlib.h>
- #include "readstat.h"
- 
--static readstat_value_t make_blank_value();
-+static readstat_value_t make_blank_value(void);
+@@ -5,7 +5,7 @@ static readstat_value_t make_double_value(double dval)
+ static readstat_value_t make_blank_value(void);
  static readstat_value_t make_double_value(double dval);
  
 -static readstat_value_t make_blank_value() {
--- math/readstat/files/patch-src_sas_ieee.c
+++ math/readstat/files/patch-src_sas_ieee.c
@@ -1,14 +1,5 @@
---- src/sas/ieee.c.orig	2018-12-15 15:27:55 UTC
+--- src/sas/ieee.c.orig	2023-02-20 19:09:20 UTC
 +++ src/sas/ieee.c
-@@ -16,7 +16,7 @@ static void ieee2xpt(unsigned char *ieee, unsigned cha
- 
- #ifndef FLOATREP
- #define FLOATREP get_native()
--int get_native();
-+int get_native(void);
- #endif
- 
- void memreverse(void *intp_void, int l) {
 @@ -96,7 +96,7 @@ int cnxptiee(const void *from_bytes, int fromtype, voi
      return(0);
  }
