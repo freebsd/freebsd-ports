40efec60a98d3360c1b6045adce159e3c53ebcca
Wen Heping <wen@FreeBSD.org>
2024-02-13 23:05:09 +0100

    print/py-reportlab: Update version 3.6.13=>4.1.0
    Changelog: https://hg.reportlab.com/hg-public/reportlab/file/tip/CHANGES.md
    
    PR:		277019

--- print/py-reportlab/files/patch-setup.py
+++ print/py-reportlab/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2022-06-08 08:27:36 UTC
+--- setup.py.orig	2024-02-13 07:54:28 UTC
 +++ setup.py
-@@ -443,9 +443,7 @@ def get_fonts(PACKAGE_DIR, reportlab_files):
+@@ -202,9 +202,7 @@ def get_fonts(PACKAGE_DIR, reportlab_files):
          xitmsg = "not downloading T1 font curve files"
      else:
          try:
 -            infoline("Downloading standard T1 font curves")
--            zipdata = url2data("http://www.reportlab.com/ftp/pfbfer-20180109.zip")
+-            zipdata = url2data("https://www.reportlab.com/ftp/pfbfer-20180109.zip")
 -            archive = zipfile.ZipFile(zipdata)
 +            archive = zipfile.ZipFile("%%DISTDIR%%/%%PFBFER%%")
              dst = pjoin(rl_dir, 'fonts')
