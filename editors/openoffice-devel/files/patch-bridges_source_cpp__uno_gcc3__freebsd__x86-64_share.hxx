14be24161950017f400a9e83560fbb71501e6d43
Don Lewis <truckman@FreeBSD.org>
2024-02-13 15:37:59 -0800

    editors/openoffice-*: Fix build
    Tweak our declaration for __cxa_allocate_exception() so that it matches
    the one that sometimes shows up in /usr/include/c++/v1/cxxabi.h.  Clang
    now treats the subtle differences as an error.
    
    No PORTREVISION bump for this build fix.  There should not be any
    functional change to the generated code if it was buildable before
    this fix.
    
    (cherry picked from commit dde6195c8d21cfcab6d1415d56a5f09b8d924452)

---
+++ editors/openoffice-devel/files/patch-bridges_source_cpp__uno_gcc3__freebsd__x86-64_share.hxx
@@ -0,0 +1,11 @@
+--- bridges/source/cpp_uno/gcc3_freebsd_x86-64/share.hxx.orig	2019-09-17 22:55:10 UTC
++++ bridges/source/cpp_uno/gcc3_freebsd_x86-64/share.hxx
+@@ -62,7 +62,7 @@ extern "C" void *__cxa_allocate_exception(
+ #endif /* __GLIBCXX__ */
+ 
+ extern "C" void *__cxa_allocate_exception(
+-    std::size_t thrown_size ) throw();
++    size_t thrown_size );
+ extern "C" void __cxa_throw (
+     void *thrown_exception, std::type_info *tinfo, void (*dest) (void *) ) __attribute__((noreturn));
+ 
