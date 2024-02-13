ae3d385524caa7950ae4b6f314c618a3df7cd9c9
Dima Panov <fluffy@FreeBSD.org>
2024-02-13 18:41:21 +0300

    editors/libreoffice: update to 24.2.0 release (+)
    LibreOffice 24.2 Community is here!
    
    The new major release of the complete, free, volunteer-supported office suite,
    with the new calendar-based numbering scheme (YY.M), many new useful features,
    and a focus on security and accessibility, is available.
    
    * Styles for comments
    * Row/column highlighting in Calc
    * A search field in the options dialog
    ...and many other new features
    
    Read more:	https://blog.documentfoundation.org/blog/2024/01/31/libreoffice-24-2/

--- editors/libreoffice/files/patch-vcl_qt5_QtInstance.cxx
+++ editors/libreoffice/files/patch-vcl_qt5_QtInstance.cxx
@@ -1,6 +1,16 @@
---- vcl/qt5/QtInstance.cxx.orig	2023-07-28 18:13:49 UTC
+--- vcl/qt5/QtInstance.cxx.orig	2023-11-23 12:14:48 UTC
 +++ vcl/qt5/QtInstance.cxx
-@@ -492,7 +492,7 @@ QtInstance::createPicker(css::uno::Reference<css::uno:
+@@ -220,7 +220,8 @@ QtInstance::QtInstance(std::unique_ptr<QApplication>& 
+ 
+ QtInstance::QtInstance(std::unique_ptr<QApplication>& pQApp)
+     : SalGenericInstance(std::make_unique<QtYieldMutex>())
+-    , m_bUseCairo(nullptr == getenv("SAL_VCL_QT_USE_QFONT"))
++//    , m_bUseCairo(nullptr == getenv("SAL_VCL_QT_USE_QFONT"))
++    , m_bUseCairo(true) // (nullptr != getenv("SAL_VCL_QT5_USE_CAIRO"));
+     , m_pTimer(nullptr)
+     , m_bSleeping(false)
+     , m_pQApplication(std::move(pQApp))
+@@ -492,7 +493,7 @@ QtInstance::createPicker(css::uno::Reference<css::uno:
      {
          SolarMutexGuard g;
          rtl::Reference<QtFilePicker> pPicker;
@@ -9,12 +19,3 @@
          assert(pPicker);
          return pPicker;
      }
-@@ -751,7 +751,7 @@ VCLPLUG_QT_PUBLIC SalInstance* create_SalInstance()
- extern "C" {
- VCLPLUG_QT_PUBLIC SalInstance* create_SalInstance()
- {
--    static const bool bUseCairo = (nullptr == getenv("SAL_VCL_QT_USE_QFONT"));
-+    static const bool bUseCairo = true; // (nullptr != getenv("SAL_VCL_QT5_USE_CAIRO"));
- 
-     std::unique_ptr<char* []> pFakeArgv;
-     std::unique_ptr<int> pFakeArgc;
