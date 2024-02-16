d0f54524687fbacb4ab4c2854f4f3856c3512bde
Jason E. Hale <jhale@FreeBSD.org>
2024-02-16 15:57:11 -0500

    Qt6: Update to 6.6.2
    As a patch release, Qt 6.6.2 does not introduce any new features but
    contains more than 400 bug fixes, security updates, and other
    improvements on top of Qt 6.6.1 release. See more information about the
    most important changes and bug fixes from the Qt 6.6.2 release note.
    
    Announcement: https://www.qt.io/blog/qt-6.6.2-released
    Release note: https://code.qt.io/cgit/qt/qtreleasenotes.git/about/qt/6.6.2/release-note.md
    
    MFH:		2024Q1

--- www/qt6-webengine/files/patch-src_3rdparty_chromium_build_config_linux_pkg-config.py
+++ www/qt6-webengine/files/patch-src_3rdparty_chromium_build_config_linux_pkg-config.py
@@ -1,11 +1,11 @@
---- src/3rdparty/chromium/build/config/linux/pkg-config.py.orig	2023-03-09 06:31:50 UTC
+--- src/3rdparty/chromium/build/config/linux/pkg-config.py.orig	2024-02-10 00:23:21 UTC
 +++ src/3rdparty/chromium/build/config/linux/pkg-config.py
 @@ -108,7 +108,7 @@ def main():
    # If this is run on non-Linux platforms, just return nothing and indicate
    # success. This allows us to "kind of emulate" a Linux build from other
    # platforms.
--  if "linux" not in sys.platform:
-+  if not sys.platform.startswith(tuple(['linux', 'openbsd', 'freebsd'])):
+-  if 'linux' not in sys.platform and 'darwin' not in sys.platform:
++  if not sys.platform.startswith(tuple(['linux', 'openbsd', 'freebsd', 'darwin'])):
      print("[[],[],[],[],[]]")
      return 0
  
