49bb1706880d668105c916d8a206442a8724987a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-02-21 22:47:48 +0800

    www/py-wagtail: Update to 5.2.3
    Changes:	https://github.com/wagtail/wagtail/releases

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2023-11-16 14:45:48 UTC
+--- setup.py.orig	2024-01-23 15:46:24 UTC
 +++ setup.py
-@@ -26,9 +26,9 @@ install_requires = [
+@@ -26,11 +26,11 @@ install_requires = [
      "django-treebeard>=4.5.1,<5.0",
      "djangorestframework>=3.11.1,<4.0",
      "django-filter>=23.3,<24",
@@ -10,5 +10,8 @@
 -    "beautifulsoup4>=4.8,<4.12",
 +    "beautifulsoup4>=4.8,<4.13",
      "html5lib>=0.999,<2",
-     "Willow[heif]>=1.6.2,<1.7",
+-    "Willow[heif]>=1.6.2,<1.7",
++    "Willow[heif]>=1.6.2,<2",
      "requests>=2.11.1,<3.0",
+     "l18n>=2018.5",
+     "openpyxl>=3.0.10,<4.0",
