76003b0a2b0057d8231a9b70b7e67d5ffc72c92e
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-02-21 22:44:57 +0800

    print/py-uharfbuzz: Update to 0.39.0
    Changes:	https://github.com/harfbuzz/uharfbuzz/releases

--- print/py-uharfbuzz/files/patch-setup.py
+++ print/py-uharfbuzz/files/patch-setup.py
@@ -1,16 +1,16 @@
 --- setup.py.orig	2023-03-13 09:18:30 UTC
 +++ setup.py
-@@ -39,12 +39,8 @@ if platform.system() == 'Darwin':
+@@ -39,12 +39,8 @@ if platform.system() == "Darwin":
  extension = Extension(
-     'uharfbuzz._harfbuzz',
+     "uharfbuzz._harfbuzz",
      define_macros=define_macros,
--    include_dirs=['harfbuzz/src'],
-+    include_dirs=['%%LOCALBASE%%/include/harfbuzz'],
+-    include_dirs=["harfbuzz/src"],
++    include_dirs=["%%LOCALBASE%%/include/harfbuzz"],
      sources=[
--        'harfbuzz/src/harfbuzz-subset.cc',
--        'harfbuzz/src/hb-coretext.cc',
--        'harfbuzz/src/hb-directwrite.cc',
--        'harfbuzz/src/hb-uniscribe.cc',
-         'src/uharfbuzz/_harfbuzz.pyx',
+-        "harfbuzz/src/harfbuzz-subset.cc",
+-        "harfbuzz/src/hb-coretext.cc",
+-        "harfbuzz/src/hb-directwrite.cc",
+-        "harfbuzz/src/hb-uniscribe.cc",
+         "src/uharfbuzz/_harfbuzz.pyx",
      ],
-     language='c++',
+     language="c++",
