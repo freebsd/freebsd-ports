561c3d00270e1e329c078cafb83ea401e15ff716
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-02-21 23:01:38 +0800

    textproc/p5-XML-Parser: Update to 2.47
    - Update LICENSE
    - Add LICENSE_FILE
    
    Changes:	https://metacpan.org/dist/XML-Parser/changes

--- textproc/p5-XML-Parser/files/patch-inc-Devel-CheckLib.pm
+++ textproc/p5-XML-Parser/files/patch-inc-Devel-CheckLib.pm
@@ -1,20 +1,11 @@
---- inc/Devel/CheckLib.pm.orig	2016-04-13 04:38:16 UTC
+--- inc/Devel/CheckLib.pm.orig	2023-12-28 19:47:02 UTC
 +++ inc/Devel/CheckLib.pm
-@@ -227,7 +227,7 @@ sub assert_lib {
-     # first figure out which headers we can't find ...
-     for my $header (@headers) {
-         push @use_headers, $header;
--        my ( $ch, $cfile ) = File::Temp::tempfile( 'assertlibXXXXXXXX', SUFFIX => '.c' );
-+        my ( $ch, $cfile ) = File::Temp::tempfile( 'assertlibXXXXXXXX', SUFFIX => '.c', TMPDIR => 1 );
-         my $ofile = $cfile;
-         $ofile =~ s/\.c$/$Config{_o}/;
-         print $ch qq{#include <$_>\n} for @use_headers;
-@@ -275,7 +275,7 @@ sub assert_lib {
+@@ -315,7 +315,7 @@ sub _make_cfile {
+ 	warn "# Code:\n$c\n";
      }
- 
-     # now do each library in turn with headers
--    my ( $ch, $cfile ) = File::Temp::tempfile( 'assertlibXXXXXXXX', SUFFIX => '.c' );
-+    my ( $ch, $cfile ) = File::Temp::tempfile( 'assertlibXXXXXXXX', SUFFIX => '.c', TMPDIR => 1 );
-     my $ofile = $cfile;
-     $ofile =~ s/\.c$/$Config{_o}/;
-     print $ch qq{#include <$_>\n} foreach (@headers);
+     my ($ch, $cfile) = File::Temp::tempfile(
+-	'assertlibXXXXXXXX', SUFFIX => '.c'
++	'assertlibXXXXXXXX', SUFFIX => '.c', TMPDIR => 1
+     );
+     print $ch $code;
+     close $ch;
