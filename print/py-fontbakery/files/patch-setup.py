02cd09dd0efb9586938b59ea49d26e6c5ec8d43f
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-02-21 22:44:56 +0800

    print/py-fontbakery: Update to 0.11.1
    - Remove ISO15008 option
    
    Changes:	https://github.com/fonttools/fontbakery/releases
    		https://github.com/fonttools/fontbakery/blob/main/CHANGELOG.md

--- print/py-fontbakery/files/patch-setup.py
+++ print/py-fontbakery/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2023-12-01 14:46:39 UTC
+--- setup.py.orig	2024-02-06 04:58:53 UTC
 +++ setup.py
-@@ -72,7 +72,7 @@ googlefonts_extras = (
+@@ -71,7 +71,7 @@ googlefonts_extras = (
          # FontVersion.get_name_id5_version_string which was added in version 0.6.0
          f"fontTools[lxml,unicode]{FONTTOOLS_VERSION}",
          # (see https://github.com/googlefonts/gflanguages/pull/7)
@@ -8,8 +8,8 @@
 +        "protobuf>=3.7.0",  # 3.7.0 fixed a bug on parsing some METADATA.pb files.
          # We cannot use v4 because our protobuf files have been compiled with v3.
          # (see https://github.com/fonttools/fontbakery/issues/2200)
-         f"vharfbuzz{VHARFBUZZ_VERSION}",
-@@ -122,7 +122,7 @@ all_extras = set(
+     ]
+@@ -116,7 +116,7 @@ setup(
  
  setup(
      name="fontbakery",
@@ -18,9 +18,9 @@
      url="https://github.com/fonttools/fontbakery/",
      description="A font quality assurance tool for everyone",
      long_description=readme,
-@@ -183,7 +183,7 @@ setup(
+@@ -179,7 +179,7 @@ setup(
+         # ---
          # core dependencies
-         "babelfont>=3.0.1",  # v3.0.1 needed by check/legacy_accents
          f"fontTools{FONTTOOLS_VERSION}",
 -        "freetype-py!=2.4.0",  # Avoiding 2.4.0 due to seg-fault described at
 +        "freetype-py",  # Avoiding 2.4.0 due to seg-fault described at
