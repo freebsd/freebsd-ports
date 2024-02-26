3b33ec1b0e1e9a723e3862359bc1081893ec091e
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2024-02-26 07:47:21 +0100

    sysutils/safecat: Moved man to share/man
    Approved by:    portmgr (blanket)

--- sysutils/safecat/files/patch-hier.c
+++ sysutils/safecat/files/patch-hier.c
@@ -1,6 +1,6 @@
---- hier.c	Mon Aug  6 23:27:18 2001
-+++ hier.c	Mon Aug  6 23:27:45 2001
-@@ -2,16 +2,13 @@
+--- hier.c.orig	2001-04-14 11:09:54 UTC
++++ hier.c
+@@ -2,16 +2,13 @@ void hier()
  
  void hier()
  {
@@ -14,10 +14,12 @@
 -  d(auto_home,"man",-1,-1,02755);
 -  d(auto_home,"man/man1",-1,-1,02755);
 -  d(auto_home,"man/cat1",-1,-1,02755);
-+  d(auto_home,"man",-1,-1,0755);
-+  d(auto_home,"man/man1",-1,-1,0755);
-   c(auto_home,"man/man1","safecat.1",-1,-1,0644);
+-  c(auto_home,"man/man1","safecat.1",-1,-1,0644);
 -  c(auto_home,"man/cat1","safecat.0",-1,-1,0644);
-   c(auto_home,"man/man1","maildir.1",-1,-1,0644);
+-  c(auto_home,"man/man1","maildir.1",-1,-1,0644);
 -  c(auto_home,"man/cat1","maildir.0",-1,-1,0644);
++  d(auto_home,"share/man",-1,-1,0755);
++  d(auto_home,"share/man/man1",-1,-1,0755);
++  c(auto_home,"share/man/man1","safecat.1",-1,-1,0644);
++  c(auto_home,"share/man/man1","maildir.1",-1,-1,0644);
  }
