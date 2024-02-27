71e92b26bd43763a7b82208625e628f043858fa7
Austin Shafer <ashafer@badland.io>
2024-02-27 18:46:23 +0300

    graphics/nvidia-drm-kmod: Update to 550.54.14
    Differential Revision: https://reviews.freebsd.org/D44073

--- graphics/nvidia-drm-61-kmod/files/patch-nvidia-drm-freebsd-lkpi.c
+++ graphics/nvidia-drm-61-kmod/files/patch-nvidia-drm-freebsd-lkpi.c
@@ -1,8 +1,8 @@
---- nvidia-drm-freebsd-lkpi.c.orig	2023-11-06 18:11:13 UTC
+--- nvidia-drm-freebsd-lkpi.c.orig	2024-02-22 01:03:15 UTC
 +++ nvidia-drm-freebsd-lkpi.c
-@@ -228,7 +228,6 @@ MODULE_DEPEND(nvidia_drm, linuxkpi, 1, 1, 1);
- 
+@@ -148,7 +148,6 @@ MODULE_DEPEND(nvidia_drm, linuxkpi, 1, 1, 1);
  LKPI_DRIVER_MODULE(nvidia_drm, nv_drm_init, nv_drm_exit);
+ LKPI_PNP_INFO(pci, nvidia_drm, nv_module_device_table);
  MODULE_DEPEND(nvidia_drm, linuxkpi, 1, 1, 1);
 -MODULE_DEPEND(nvidia_drm, linuxkpi_gplv2, 1, 1, 1);
  MODULE_DEPEND(nvidia_drm, drmn, 2, 2, 2);
