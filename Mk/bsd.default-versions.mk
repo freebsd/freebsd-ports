d0833d76bb573f8694627e82fe44e56df049d252
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2024-02-28 09:05:29 +0100

    */ruby: Switch DEFAULT to 3.2
    BREAKING CHANGES:
    www/redmine50 will stop working. At the moment there is no redmine 5.1.X
    in the tree. So users of redmine should utilize the following in their
    relevant make.conf file:
    DEFAULT_VERSIONS=ruby=3.1
    
    PR:		244427
    Approved by:	portmgr (exp-run)

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -139,7 +139,7 @@ PYTHON_DEFAULT?=	3.9
 # Possible values: 2.7
 PYTHON2_DEFAULT?=	2.7
 # Possible values: 3.0, 3.1, 3.2, 3.3
-RUBY_DEFAULT?=		3.1
+RUBY_DEFAULT?=		3.2
 # Possible values: rust, rust-nightly
 RUST_DEFAULT?=		rust
 # Possible values: 4.13, 4.16, 4.19
