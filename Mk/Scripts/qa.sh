b18af1280f1241d8c8a0622d7d29a47d0f4b656c
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2024-02-29 20:40:25 +0100

    Mk/Scripts: Add qa check for man pages
    portmgr@ has been actively working on moving man pages from
    ${PREFIX}/man to ${PREFIX}/share/man. Majority of the ports belonging to
    non-committers or aliases has been migrated to ${PREFIX}/share/man.
    There are still around 1000+ ports remaining mainly from the committers.
    portmgr@ has planned to make the hard change after the split of 2024Q3
    after which ports that installs man pages in ${PREFIX}/man will no
    longer build or will be BROKEN.
    
    Approved by:	portmgr

--- Mk/Scripts/qa.sh
+++ Mk/Scripts/qa.sh
@@ -1034,10 +1034,21 @@ reinplace()
 	fi
 }
 
+prefixman() {
+	local manlist
+
+	manlist=$(find ${STAGEDIR}${PREFIX}/man -type f)
+	if [ -n "${manlist}" ]; then
+		warn "Installing man files in ${PREFIX}/man is no longer supported. Consider installing these files in ${PREFIX}/share/man instead."
+		ls -liTd ${manlist}
+	fi
+	return 0
+}
+
 checks="shebang symlinks paths stripped desktopfileutils sharedmimeinfo"
 checks="$checks suidfiles libtool libperl prefixvar baselibs terminfo"
 checks="$checks proxydeps sonames perlcore no_arch gemdeps gemfiledeps flavors"
-checks="$checks license depends_blacklist pkgmessage reinplace"
+checks="$checks license depends_blacklist pkgmessage reinplace prefixman"
 
 ret=0
 cd ${STAGEDIR} || exit 1
