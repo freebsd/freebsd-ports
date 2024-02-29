ba50fad3e7c1cd8ab2db402e1ac43637683b24ec
Robert Nagy <rnagy@FreeBSD.org>
2024-02-29 10:02:04 +0100

    www/*chromium: udpate to 122.0.6261.94
    Security:	https://vuxml.freebsd.org/freebsd/31bb1b8d-d6dc-11ee-86bb-a8a1599412c6.html
    (cherry picked from commit 8e838d1e1c1813565d43e3c7be6477afae05aa74)

--- www/ungoogled-chromium/files/patch-components_search__engines_template__url__service.cc
+++ www/ungoogled-chromium/files/patch-components_search__engines_template__url__service.cc
@@ -1,6 +1,6 @@
---- components/search_engines/template_url_service.cc.orig	2024-02-25 20:22:18 UTC
+--- components/search_engines/template_url_service.cc.orig	2024-02-29 08:39:18 UTC
 +++ components/search_engines/template_url_service.cc
-@@ -2922,7 +2922,7 @@ bool TemplateURLService::MatchesDefaultSearchProvider(
+@@ -2930,7 +2930,7 @@ bool TemplateURLService::MatchesDefaultSearchProvider(
  std::unique_ptr<EnterpriseSiteSearchManager>
  TemplateURLService::GetEnterpriseSiteSearchManager(PrefService* prefs) {
  #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
