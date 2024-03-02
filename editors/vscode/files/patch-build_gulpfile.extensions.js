2260b20940fa21f21bca3911fdf0d801fbcef620
Hiroki Tagato <tagattie@FreeBSD.org>
2024-03-02 11:44:49 +0900

    editors/vscode: update to 1.87.0
    Changelog: https://code.visualstudio.com/updates/v1_87
    
    Reported by:	GitHub (watch releases)

--- editors/vscode/files/patch-build_gulpfile.extensions.js
+++ editors/vscode/files/patch-build_gulpfile.extensions.js
@@ -1,6 +1,6 @@
---- build/gulpfile.extensions.js.orig	2023-09-06 21:00:17 UTC
+--- build/gulpfile.extensions.js.orig	2024-02-27 22:50:23 UTC
 +++ build/gulpfile.extensions.js
-@@ -238,7 +238,7 @@ exports.compileExtensionMediaBuildTask = compileExtens
+@@ -239,7 +239,7 @@ const compileExtensionsBuildTask = task.define('compil
  const cleanExtensionsBuildTask = task.define('clean-extensions-build', util.rimraf('.build/extensions'));
  const compileExtensionsBuildTask = task.define('compile-extensions-build', task.series(
  	cleanExtensionsBuildTask,
