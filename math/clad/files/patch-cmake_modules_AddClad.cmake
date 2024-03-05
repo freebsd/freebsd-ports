4f63327c570c21ccad2b2c1ff15204409aedc791
Yuri Victorovich <yuri@FreeBSD.org>
2024-03-05 07:04:57 -0800

    math/clad: update 1.3 → 1.4
    Reported by:	portscout

--- math/clad/files/patch-cmake_modules_AddClad.cmake
+++ math/clad/files/patch-cmake_modules_AddClad.cmake
@@ -1,17 +1,18 @@
---- cmake/modules/AddClad.cmake.orig	2024-02-12 07:51:28 UTC
+--- cmake/modules/AddClad.cmake.orig	2024-02-29 15:28:44 UTC
 +++ cmake/modules/AddClad.cmake
-@@ -1,14 +1,14 @@
- # Find the current branch.
--execute_process(WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
--                COMMAND git rev-parse HEAD
--                OUTPUT_VARIABLE CURRENT_REPO_COMMIT
--                OUTPUT_STRIP_TRAILING_WHITESPACE)
--string(REPLACE "/" "" CURRENT_REPO_COMMIT ${CURRENT_REPO_COMMIT})
-+#execute_process(WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-+#                COMMAND git rev-parse HEAD
-+#                OUTPUT_VARIABLE CURRENT_REPO_COMMIT
-+#                OUTPUT_STRIP_TRAILING_WHITESPACE)
-+#string(REPLACE "/" "" CURRENT_REPO_COMMIT ${CURRENT_REPO_COMMIT})
+@@ -1,15 +1,15 @@ if (CLAD_ENABLE_BENCHMARKS)
+ if (CLAD_ENABLE_BENCHMARKS)
+   # Find the current branch.
+-  execute_process(WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+-                  COMMAND git rev-parse HEAD
+-                  OUTPUT_VARIABLE CURRENT_REPO_COMMIT
+-                  OUTPUT_STRIP_TRAILING_WHITESPACE)
+-  string(REPLACE "/" "" CURRENT_REPO_COMMIT ${CURRENT_REPO_COMMIT})
++  #execute_process(WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
++  #                COMMAND git rev-parse HEAD
++  #                OUTPUT_VARIABLE CURRENT_REPO_COMMIT
++  #                OUTPUT_STRIP_TRAILING_WHITESPACE)
++  #string(REPLACE "/" "" CURRENT_REPO_COMMIT ${CURRENT_REPO_COMMIT})
  
  # Ask cmake to reconfigure each time we change the branch so that it can change
  # the value of CURRENT_REPO_COMMIT.
@@ -20,5 +21,5 @@
 +#set_property(DIRECTORY APPEND PROPERTY
 +#             CMAKE_CONFIGURE_DEPENDS "${CMAKE_SOURCE_DIR}/.git/HEAD")
  
- #-------------------------------------------------------------------------------
- # function ENABLE_CLAD_FOR_EXECUTABLE(<executable>
+ endif(CLAD_ENABLE_BENCHMARKS)
+ 
