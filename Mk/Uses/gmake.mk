624b8729fc49aa22cca173aa1a6b69b65fa96f5a
Tijl Coosemans <tijl@FreeBSD.org>
2024-03-06 15:48:41 +0100

    Mk/bsd.commands.mk: Use absolute path to define GMAKE
    Same as BSDMAKE variable.  It is needed by some ports.
    
    Some ports needed fixes:
    - devel/rcs57: Regenerate configure with newer autoconf so it can deal
      with MAKE set to an absolute path.  While here fix issues with
      (un)escaping double quotes.  The warnings about this were in
      WRKSRC/src/conf.err.
    - lang/gnu-cobol: Patch configure.ac so the MAKE environment variable
      can contain an absolute path, and define MAKE before it is used in
      AM_INIT_AUTOMAKE.  Remove post-patch patching of configure.  It appears
      to have been fixed upstream.
    - math/unuran: Remove post-patch, fixed upstream.
    - net-mgmt/kismet: Fix post-patch.
    
    PR:		283534
    Exp-run by:	antoine

--- Mk/Uses/gmake.mk
+++ Mk/Uses/gmake.mk
@@ -13,7 +13,7 @@ IGNORE=	Incorrect 'USES+= gmake:${gmake_ARGS}' gmake takes no arguments
 .  endif
 
 BUILD_DEPENDS+=		gmake>=4.4.1:devel/gmake
-CONFIGURE_ENV+=		MAKE=gmake
-MAKE_CMD=		gmake
+CONFIGURE_ENV+=		MAKE=${GMAKE}
+MAKE_CMD=		${GMAKE}
 
 .endif
