4dea5aec7a8e24b5cc5572b4214719bdddfd1ba3
Baptiste Daroussin <bapt@FreeBSD.org>
2024-03-06 14:49:10 +0100

    Mk/bsd.port.mk: remove LEGAL related variables
    This is noop since the ports tree does nothing with the variables
    anymore for a while
    
    Reviewed by:	mat
    Differential Revision:	https://reviews.freebsd.org/D44252

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -155,8 +155,6 @@ FreeBSD_MAINTAINER=	portmgr@FreeBSD.org
 #				  but distfiles can be put on ftp sites and CDROMs.
 # FORBIDDEN		- Package build should not be attempted because of
 #				  security vulnerabilities.
-# LEGAL_TEXT	- Port has legal issues (e.g., special permission to distribute, lacks a license).
-# LEGAL_PACKAGE - Port has no legal issues but defines NO_PACKAGE
 # IGNORE		- Package build should be skipped entirely (e.g.
 #				  because of serious unfixable problems in the build,
 #				  because it cannot be manually fetched, etc).  Error
@@ -2926,17 +2924,6 @@ IGNORE=		is marked as broken on ${OPSYS}: ${BROKEN_${OPSYS}}
 .        endif
 .      elif defined(FORBIDDEN)
 IGNORE=		is forbidden: ${FORBIDDEN}
-.      endif
-
-# Define the text to be output to LEGAL
-.      if defined(LEGAL_TEXT)
-LEGAL= ${LEGAL_TEXT}
-.      elif defined(RESTRICTED)
-LEGAL= ${RESTRICTED}
-.      elif defined(NO_CDROM)
-LEGAL= ${NO_CDROM}
-.      elif defined(NO_PACKAGE) && ! defined(LEGAL_PACKAGE)
-LEGAL= ${NO_PACKAGE}
 .      endif
 
 .      if (defined(MANUAL_PACKAGE_BUILD) && defined(PACKAGE_BUILDING))
