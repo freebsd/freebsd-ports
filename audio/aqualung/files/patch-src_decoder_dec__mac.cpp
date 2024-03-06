77482fd613f088409e3c2ea8b112c6b4581e89c1
Chris Hutchinson <portmaster@bsdforge.com>
2024-03-06 22:18:37 +0300

    audio/aqualung: Update to 1.2
    PR:		276601

--- audio/aqualung/files/patch-src_decoder_dec__mac.cpp
+++ audio/aqualung/files/patch-src_decoder_dec__mac.cpp
@@ -1,11 +1,11 @@
---- src/decoder/dec_mac.cpp.orig	2015-04-19 12:49:49 UTC
+--- src/decoder/dec_mac.cpp.orig	2024-03-05 17:20:36 UTC
 +++ src/decoder/dec_mac.cpp
-@@ -174,7 +174,7 @@ mac_decoder_open(decoder_t * dec, char *
- 
- 
- 	int ret = 0;
+@@ -178,7 +178,7 @@
+ #ifdef __OpenBSD__
+         wchar_t * pUTF16 = GetUTF16FromANSI(filename);
+ #else
 -        wchar_t * pUTF16 = CAPECharacterHelper::GetUTF16FromANSI(filename);
 +        wchar_t * pUTF16 = GetUTF16FromANSI(filename);
+ #endif
          pdecompress = CreateIAPEDecompress(pUTF16, &ret);
          free(pUTF16);
- 
