76c97e7c968a50308665ddbcae3862c3b3d7eea5
Yuri Victorovich <yuri@FreeBSD.org>
2024-03-07 00:03:29 -0800

    security/openfhe: update 1.1.2 → 1.1.3
    Reported by:	portscout

--- security/openfhe/files/patch-src_core_include_math_hal_intnat_ubintnat.h
+++ security/openfhe/files/patch-src_core_include_math_hal_intnat_ubintnat.h
@@ -1,25 +1,26 @@
---- src/core/include/math/hal/intnat/ubintnat.h.orig	2023-12-16 19:03:47 UTC
+--- src/core/include/math/hal/intnat/ubintnat.h.orig	2024-03-05 00:18:51 UTC
 +++ src/core/include/math/hal/intnat/ubintnat.h
-@@ -1935,16 +1935,16 @@ class NativeIntegerT final : public lbcrypto::BigInteg
-         res.lo = x.lo * y;
-         asm("umulh %0, %1, %2\n\t" : "=r"(res.hi) : "r"(x.lo), "r"(y));
-         res.hi += x.hi * y;
--#elif defined(__arm__)  // 32 bit processor
-+#elif defined(__powerpc64__) || defined(__riscv)
-+        U128BITS wres(0), wa(a), wb(b);
-+        wres   = wa * wb;  // should give us 128 bits  of 64 * 64
-+        res.hi = (uint64_t)(wres >> 64);
-+        res.lo = (uint64_t)wres;
-+#elif defined(__arm__) || defined(__powerpc__) // 32 bit processor
-         uint64_t wres(0), wa(a), wb(b);
-         wres   = wa * wb;
-         res.hi = wres >> 32;
-         res.lo = (uint32_t)wres & 0xFFFFFFFF;
--#elif __riscv
--        U128BITS wres(0), wa(a), wb(b);
--        wres   = wa * wb;
--        res.hi = (uint64_t)(wres >> 64);
--        res.lo = (uint64_t)wres;
+@@ -1919,6 +1919,11 @@ class NativeIntegerT final : public lbcrypto::BigInteg
+             uint128_t c{static_cast<uint128_t>(a) * b};
+             res.hi = static_cast<uint64_t>(c >> 64);
+             res.lo = static_cast<uint64_t>(c);
++#elif __riscv
++            U128BITS wres(0), wa(a), wb(b);
++            wres   = wa * wb;
++            res.hi = (uint64_t)(wres >> 64);
++            res.lo = (uint64_t)wres;
  #elif defined(__EMSCRIPTEN__)  // web assembly
-         U64BITS a1 = a >> 32;
-         U64BITS a2 = (uint32_t)a;
+             uint64_t a1 = a >> 32;
+             uint64_t a2 = (uint32_t)a;
+@@ -1957,6 +1962,11 @@ class NativeIntegerT final : public lbcrypto::BigInteg
+             res.lo = x.lo * y;
+             asm("umulh %0, %1, %2\n\t" : "=r"(res.hi) : "r"(x.lo), "r"(y));
+             res.hi += x.hi * y;
++#elif defined(__powerpc64__) || defined(__riscv)
++            U128BITS wres(0), wa(a), wb(b);
++            wres   = wa * wb;  // should give us 128 bits  of 64 * 64
++            res.hi = (uint64_t)(wres >> 64);
++            res.lo = (uint64_t)wres;
+ #elif defined(__arm__) || defined(__powerpc__)  // 32 bit processor
+             uint64_t wres(0), wa(a), wb(b);
+             wres   = wa * wb;
