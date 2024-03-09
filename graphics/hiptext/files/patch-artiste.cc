9e1564bdc210ee3f240983c50712665057420165
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-03-09 21:57:23 +0800

    graphics/hiptext: Fix LICENSE
    Reference:	https://github.com/jart/hiptext/commit/da18b54c614beb74b7cb8671ea501caae2f8e85f
    Approved by:	portmgr (blanket)

--- graphics/hiptext/files/patch-artiste.cc
+++ graphics/hiptext/files/patch-artiste.cc
@@ -1,6 +1,14 @@
 --- artiste.cc.orig	2016-06-21 21:09:17 UTC
 +++ artiste.cc
-@@ -166,7 +166,7 @@ void Artiste::PrintMovie(Movie movie) {
+@@ -9,6 +9,7 @@
+ #include <sys/ioctl.h>
+ #include <sys/termios.h>
+ #include <sys/select.h>
++#include <unistd.h>
+ #include <gflags/gflags.h>
+ #include <glog/logging.h>
+ 
+@@ -166,7 +167,7 @@ void Artiste::PrintMovie(Movie movie) {
    ComputeDimensions(RatioOf(movie.width(), movie.height()));
    movie.PrepareRGB(width_, height_);
    HideCursor();
