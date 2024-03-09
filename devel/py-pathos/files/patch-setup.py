33ab85adea878fd1fc99089180d02d59807204d9
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-03-09 21:48:17 +0800

    devel/py-pathos: Update version requirement of RUN_DEPENDS

--- devel/py-pathos/files/patch-setup.py
+++ devel/py-pathos/files/patch-setup.py
@@ -1,11 +1,8 @@
 --- setup.py.orig	2024-01-28 19:02:51 UTC
 +++ setup.py
-@@ -90,9 +90,9 @@ ppft_version = 'ppft>=1.7.6.8'
- 
- # define dependencies
+@@ -92,7 +92,7 @@ pox_version = 'pox>=0.3.4'
  ppft_version = 'ppft>=1.7.6.8'
--dill_version = 'dill>=0.3.8'
-+dill_version = 'dill>=0.3.7'
+ dill_version = 'dill>=0.3.8'
  pox_version = 'pox>=0.3.4'
 -mp_version = 'multiprocess>=0.70.16'
 +mp_version = 'multiprocess>=0.70.12'
