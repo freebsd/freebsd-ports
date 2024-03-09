9c6884cbb61d18264c39113eb405ed052771f660
Rainer Hurling <rhurlin@FreeBSD.org>
2024-03-09 17:57:42 +0100

    devel/py-spyder-kernels: Update to 2.5.1
    Fix hangs with Maplotlib interactive backends
    
    Changelog: https://github.com/spyder-ide/spyder-kernels/blob/master/CHANGELOG.md#version-251-2024-02-28
    
    Reported by:	Repology

--- devel/py-spyder-kernels/files/patch-setup.py
+++ devel/py-spyder-kernels/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2023-11-06 14:56:06 UTC
+--- setup.py.orig	2024-02-28 01:51:49 UTC
 +++ setup.py
 @@ -41,13 +41,13 @@ REQUIREMENTS = [
      'cloudpickle',
      'ipykernel>=4.5,<5; python_version<"3"',
      'ipykernel>=6.16.1,<6.17; python_version<"3.8"',
--    'ipykernel>=6.23.2,<7; python_version>="3.8"',
-+    'ipykernel>=6.23.2; python_version>="3.8"',
+-    'ipykernel>=6.29.3,<7; python_version>="3.8"',
++    'ipykernel>=6.29.3; python_version>="3.8"',
      'ipython<6; python_version<"3"',
      'ipython>=7.31.1,<8; python_version<"3.8"',
      'ipython>=8.12.2,<8.13; python_version=="3.8"',
@@ -15,5 +15,5 @@
 -    'jupyter-client>=7.4.9,<9; python_version>="3"',
 +    'jupyter-client>=7.4.9; python_version>="3"',
      'pyzmq>=17,<20; python_version<"3"',
-     'pyzmq>=22.1.0; python_version>="3"',
+     'pyzmq>=24.0.0; python_version>="3"',
      'wurlitzer>=1.0.3;platform_system!="Windows"',
