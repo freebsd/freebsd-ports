fdf1eb985c9c78779e0b1074589c3a763dff33c5
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2024-03-10 16:17:14 +0100

    net-mgmt/bsnmp-regex: Fix build time warnings
    - Utilize OPTIONSNG
    - Utilize USES=localbase
    - Refresh patches

--- net-mgmt/bsnmp-regex/files/patch-module_bsnmp-regex.c
+++ net-mgmt/bsnmp-regex/files/patch-module_bsnmp-regex.c
@@ -1,6 +1,6 @@
---- module/bsnmp-regex.c.orig	2011-10-21 16:05:37.000000000 +0700
-+++ module/bsnmp-regex.c	2018-06-05 21:16:44.471250000 +0700
-@@ -788,7 +788,7 @@ config_var (char *name, char *value, int
+--- module/bsnmp-regex.c.orig	2011-10-21 09:05:37 UTC
++++ module/bsnmp-regex.c
+@@ -788,7 +788,7 @@ config_var (char *name, char *value, int line)
      if (strcmp (name, "expire") == 0 || strcmp (name, "expires") == 0) {
  
          i = strtol (value, &t2, 10);
