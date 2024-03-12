1c191a984b77a0305338cd6f3328f11013ef7a5b
Bernard Spil <brnrd@FreeBSD.org>
2024-03-12 14:46:08 +0100

    databases/mariadb1011-server: Update to 10.11.7

---
+++ databases/mariadb1011-server/files/patch-mysys__ssl_openssl.c
@@ -0,0 +1,14 @@
+--- mysys_ssl/openssl.c.orig	2024-02-01 17:44:37 UTC
++++ mysys_ssl/openssl.c
+@@ -30,6 +30,11 @@ int check_openssl_compatibility()
+ {
+   return 0;
+ }
++#elif defined(LIBRESSL_VERSION_NUMBER)
++int check_openssl_compatibility()
++{
++  return 1;
++}
+ #else
+ #include <openssl/evp.h>
+ 
