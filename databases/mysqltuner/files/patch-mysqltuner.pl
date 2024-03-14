a994cc4dce95de181d4fc72e5bf1fd485acaf649
Miroslav Lachman <000.fbsd@quip.cz>
2024-03-14 21:30:20 +0100

    databases/mysqltuner: fix shebang
    PR:	277703

---
+++ databases/mysqltuner/files/patch-mysqltuner.pl
@@ -0,0 +1,7 @@
+--- mysqltuner.pl.orig	2024-03-14 16:58:27 UTC
++++ mysqltuner.pl
+@@ -1,3 +1,4 @@
++#!/usr/local/bin/perl
+ # mysqltuner.pl - Version 2.5.2
+ # High Performance MySQL Tuning Script
+ # Copyright (C) 2015-2023 Jean-Marie Renouard - jmrenouard@gmail.com
