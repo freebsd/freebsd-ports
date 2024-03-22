6832fb6ba42f365f83bf04e2d832d1a6ba16749a
Lars Herschke <lhersch@dssgmbh.de>
2024-03-22 23:30:22 +0300

    deskutils/kdeconnect-kde: fix broken patch for the sftp_mounter
    PR:		277765

--- deskutils/kdeconnect-kde/files/patch-plugins_sftp_mounter.cpp
+++ deskutils/kdeconnect-kde/files/patch-plugins_sftp_mounter.cpp
@@ -1,15 +1,15 @@
---- plugins/sftp/mounter.cpp.orig	2022-11-30 00:45:32 UTC
+--- plugins/sftp/mounter.cpp.orig	2024-02-10 04:18:49 UTC
 +++ plugins/sftp/mounter.cpp
-@@ -129,7 +129,7 @@ void Mounter::onPackageReceived(const NetworkPacket &n
-                       << QStringLiteral("-o") << QStringLiteral("HostKeyAlgorithms=+ssh-dss\\,ssh-rsa") // https://bugs.kde.org/show_bug.cgi?id=351725
+@@ -130,7 +130,7 @@ void Mounter::onPackageReceived(const NetworkPacket &n
+                       << QStringLiteral("-o") << QStringLiteral("PubkeyAcceptedKeyTypes=+ssh-rsa") // https://bugs.kde.org/show_bug.cgi?id=443155
                        << QStringLiteral("-o") << QStringLiteral("uid=") + QString::number(getuid()) << QStringLiteral("-o")
                        << QStringLiteral("gid=") + QString::number(getgid()) << QStringLiteral("-o") << QStringLiteral("reconnect") << QStringLiteral("-o")
 -                      << QStringLiteral("ServerAliveInterval=30") << QStringLiteral("-o") << QStringLiteral("password_stdin");
-+                      << QStringLiteral("-o") << QStringLiteral("ssh_command=sshpass -p %1 %2").arg(np.get<QString>(QStringLiteral("password")), QStringLiteral("ssh"));
++                      << QStringLiteral("ServerAliveInterval=30") << QStringLiteral("-o") << QStringLiteral("ssh_command=sshpass -p %1 %2").arg(np.get<QString>(QStringLiteral("password")), QStringLiteral("ssh"));
  
      m_proc->setProgram(program, arguments);
  
-@@ -137,8 +137,8 @@ void Mounter::onPackageReceived(const NetworkPacket &n
+@@ -138,8 +138,8 @@ void Mounter::onPackageReceived(const NetworkPacket &n
      m_proc->start();
  
      // qCDebug(KDECONNECT_PLUGIN_SFTP) << "Passing password: " << np.get<QString>("password").toLatin1();
