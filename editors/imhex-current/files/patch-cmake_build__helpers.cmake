01011d0297459245b1a808fb197af9971c6d71d1
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2024-03-24 00:28:15 +0900

    editors/imhex-current: Update to 1.33.2

--- editors/imhex-current/files/patch-cmake_build__helpers.cmake
+++ editors/imhex-current/files/patch-cmake_build__helpers.cmake
@@ -1,18 +1,26 @@
---- cmake/build_helpers.cmake.orig	2024-03-02 15:44:54 UTC
+--- cmake/build_helpers.cmake.orig	2024-03-23 09:15:12 UTC
 +++ cmake/build_helpers.cmake
-@@ -480,13 +480,13 @@ function(downloadImHexPatternsFiles dest)
+@@ -483,13 +483,21 @@ function(downloadImHexPatternsFiles dest)
  
      else ()
          # Maybe patterns are cloned to a subdirectory
 -        set(imhex_patterns_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/ImHex-Patterns")
-+        set(imhex_patterns_SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/imhex_patterns_src")
++        if (BSD AND BSD STREQUAL "FreeBSD")
++            set(imhex_patterns_SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/imhex_patterns_src")
++        else ()
++            set(imhex_patterns_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/ImHex-Patterns")
++        endif ()
      endif ()
  
      if (EXISTS ${imhex_patterns_SOURCE_DIR})
          set(PATTERNS_FOLDERS_TO_INSTALL constants encodings includes patterns magic nodes)
          foreach (FOLDER ${PATTERNS_FOLDERS_TO_INSTALL})
 -            install(DIRECTORY "${imhex_patterns_SOURCE_DIR}/${FOLDER}" DESTINATION ${dest} PATTERN "**/_schema.json" EXCLUDE)
-+            install(DIRECTORY "${imhex_patterns_SOURCE_DIR}/${FOLDER}" DESTINATION "share/imhex/" PATTERN "**/_schema.json" EXCLUDE)
++            if (BSD AND BSD STREQUAL "FreeBSD")
++                install(DIRECTORY "${imhex_patterns_SOURCE_DIR}/${FOLDER}" DESTINATION "share/imhex/" PATTERN "**/_schema.json" EXCLUDE)
++            else ()
++                install(DIRECTORY "${imhex_patterns_SOURCE_DIR}/${FOLDER}" DESTINATION ${dest} PATTERN "**/_schema.json" EXCLUDE)
++            endif ()
          endforeach ()
      endif ()
  
