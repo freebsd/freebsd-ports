88316fb64f42a517b898c71a1648078a4d94fe9e
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-03-23 22:20:40 +0800

    databases/py-kyotocabinet: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ databases/py-kyotocabinet/files/patch-setup.py
@@ -0,0 +1,13 @@
+--- setup.py.orig	2010-12-16 23:41:42 UTC
++++ setup.py
+@@ -1,8 +1,8 @@ from subprocess import *
+ from distutils.core import *
+ from subprocess import *
+ 
+-package_name = 'Kyoto Cabinet'
+-package_version = '1.5'
++package_name = 'kyotocabinet'
++package_version = '1.23'
+ package_description = 'a straightforward implementation of DBM'
+ package_author = 'FAL Labs'
+ package_author_email = 'info@fallabs.com'
