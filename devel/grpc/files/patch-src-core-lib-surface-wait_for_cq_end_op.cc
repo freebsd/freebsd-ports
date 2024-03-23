18b6f9d163b303aa6b3590117308ba6b35714285
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-03-23 22:18:14 +0800

    devel/grpc: Update to 1.62.1
    Changes:	https://github.com/grpc/grpc/releases

--- devel/grpc/files/patch-src-core-lib-surface-wait_for_cq_end_op.cc
+++ devel/grpc/files/patch-src-core-lib-surface-wait_for_cq_end_op.cc
@@ -1,13 +1,13 @@
---- src/core/lib/surface/wait_for_cq_end_op.cc.orig	2024-02-20 20:23:19 UTC
+--- src/core/lib/surface/wait_for_cq_end_op.cc.orig	2024-03-08 01:33:47 UTC
 +++ src/core/lib/surface/wait_for_cq_end_op.cc
-@@ -14,6 +14,7 @@
+@@ -12,6 +12,7 @@
+ // See the License for the specific language governing permissions and
+ // limitations under the License.
  
++#include <absl/strings/str_format.h>
  #include <grpc/support/port_platform.h>
  
-+#include "absl/strings/str_format.h"
  #include "src/core/lib/surface/wait_for_cq_end_op.h"
- 
- #include <atomic>
 @@ -72,4 +73,4 @@ std::string WaitForCqEndOp::StateString(const State& s
        [](const Invalid&) -> std::string { return "Invalid{}"; });
  }
