cb8b43a5e2c03b1e9b8ffd5ab86b823b91ef17bd
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-03-23 22:20:29 +0800

    astro/py-horoscopegenerator: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ astro/py-horoscopegenerator/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2015-10-12 21:38:11 UTC
++++ setup.py
+@@ -9,7 +9,7 @@ setup(
+     long_description = f.read()
+ 
+ setup(
+-    name='HoroscopeGenerator',
++    name='horoscopegenerator', 
+     version='0.1.8',
+ 
+     description='Generates horoscopes',
