31b294cd1770f252545e7f21544b995ec1d006c1
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-03-23 22:23:51 +0800

    x11-fonts/py-fontfeatures: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ x11-fonts/py-fontfeatures/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-07-17 10:33:56 UTC
++++ setup.py
+@@ -14,7 +14,7 @@ config = {
+ scripts = glob.glob("bin/*")
+ 
+ config = {
+-    'name': 'fontFeatures',
++    'name': 'fontfeatures',
+     'author': 'Simon Cozens',
+     'author_email': 'simon@simon-cozens.org',
+     'url': 'https://github.com/simoncozens/fontFeatures',
