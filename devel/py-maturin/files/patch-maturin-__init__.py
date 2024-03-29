1f03f45c6eac93f47adc0c8471d6e5bedf73106b
Kai Knoblich <kai@FreeBSD.org>
2024-03-29 10:15:08 +0100

    devel/py-maturin: Update to 1.5.1
    Changelog:
    
    https://github.com/PyO3/maturin/releases/tag/v1.5.1

--- devel/py-maturin/files/patch-maturin-__init__.py
+++ devel/py-maturin/files/patch-maturin-__init__.py
@@ -1,9 +1,9 @@
---- maturin/__init__.py.orig	2022-08-14 11:03:56 UTC
+--- maturin/__init__.py.orig	2024-03-21 12:30:28 UTC
 +++ maturin/__init__.py
-@@ -52,7 +52,7 @@ def _build_wheel(
+@@ -64,7 +64,7 @@ def _build_wheel(
      # PEP 517 specifies that only `sys.executable` points to the correct
      # python interpreter
-     command = [
+     base_command = [
 -        "maturin",
 +        "maturin-%%PYTHON_VER%%",
          "pep517",
