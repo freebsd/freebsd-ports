f6a75e2eaa84570a0ba7a347497775144aac9c96
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-04-01 02:51:08 +0800

    www/node20: Update to 20.12.0
    Changes:	https://github.com/nodejs/node/releases
    		https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_V20.md

--- www/node20/files/patch-tools_install.py
+++ www/node20/files/patch-tools_install.py
@@ -1,11 +1,11 @@
---- tools/install.py.orig	2024-01-27 12:14:48 UTC
+--- tools/install.py.orig	2024-03-26 14:14:20 UTC
 +++ tools/install.py
-@@ -181,7 +181,7 @@ def files(action):
-   action(['deps/v8/tools/gdbinit'], 'share/doc/node/')
-   action(['deps/v8/tools/lldb_commands.py'], 'share/doc/node/')
+@@ -182,7 +182,7 @@ def files(options, action):
+   action(options, [os.path.join(options.v8_dir, 'tools/gdbinit')], 'share/doc/node/')
+   action(options, [os.path.join(options.v8_dir, 'tools/lldb_commands.py')], 'share/doc/node/')
  
 -  if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
 +  if 'openbsd' in sys.platform:
-     action(['doc/node.1'], 'man/man1/')
+     action(options, ['doc/node.1'], 'man/man1/')
    else:
-     action(['doc/node.1'], 'share/man/man1/')
+     action(options, ['doc/node.1'], 'share/man/man1/')
