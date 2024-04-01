257ead13363add38a91c7768c3b38fcd7dd5983c
gatekeeper <tiago.gasiba@gmail.com>
2024-04-01 10:47:09 -0300

    net/netdiscover: Add new port
    Netdiscover is an arp packet-based network address discovery tool
    
    PR:		277932
    Approved by:	garga (mentor)
    Differential Revision:	https://reviews.freebsd.org/D44551

---
+++ net/netdiscover/files/patch-src_data__al.h
@@ -0,0 +1,10 @@
+--- src/data_al.h.orig	2024-03-24 19:35:17 UTC
++++ src/data_al.h
+@@ -30,6 +30,7 @@
+ 
+ #include <pthread.h>
+ #include "screen.h"
++#include <pcap/pcap.h>
+ 
+ #ifdef __cplusplus
+ extern "C"
