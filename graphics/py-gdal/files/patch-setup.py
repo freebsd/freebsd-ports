33965eb7242c4d6f869756f0b40b95edd0a6c6b6
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-04-06 07:57:44 +0800

    graphics/py-gdal: Update to 3.8.5
    - Convert to USE_PYTHON=pep517
    
    Changes:	https://github.com/OSGeo/gdal/releases
    		https://github.com/OSGeo/gdal/blob/release/3.8/NEWS.md

---
+++ graphics/py-gdal/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2024-04-04 17:09:42 UTC
++++ setup.py
+@@ -332,7 +332,7 @@ readme = open('README.rst', encoding="utf-8").read()
+ 
+ readme = open('README.rst', encoding="utf-8").read()
+ 
+-name = 'GDAL'
++name = 'gdal'
+ author = "Frank Warmerdam"
+ author_email = "warmerdam@pobox.com"
+ maintainer = "Howard Butler"
