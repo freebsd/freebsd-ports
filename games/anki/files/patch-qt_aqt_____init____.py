707ad76720184dfe10bf6aa4815ded75a2a54934
Kai Knoblich <kai@FreeBSD.org>
2024-04-06 13:35:52 +0200

    games/anki: Update to 24.04
    * Support for offline builds has now been integrated, so a number of
      patches are no longer required.
    
    * The same applies to some environment variables that are also no longer
      needed or have changed slightly regarding their purpose.
    
    * The workaround introduced in bdcfffd8573e regarding issues with nVidia
      drivers was also merged by upstream and is therefore no longer
      required.
    
    Changelog:
    
    https://github.com/ankitects/anki/releases/tag/24.04

--- games/anki/files/patch-qt_aqt_____init____.py
+++ games/anki/files/patch-qt_aqt_____init____.py
@@ -1,13 +1,29 @@
-Prevents runtime issues with nVidia drivers
+From: Escape0707 <tothesong@gmail.com>
+Date: Fri, 5 Apr 2024 21:12:06 +0900
+Subject: [PATCH] Ignore exception raised by pip_system_certs not found (#3114)
 
---- qt/aqt/__init__.py.orig	2023-05-01 01:47:30 UTC
+This dependency usually doesn't benefit Linux distros with requests library
+configured to use system certificate already. And is not packaged by most
+distros. Making it optional will make most Linux users' installation much
+easier.
+
+Obtained from:
+
+https://github.com/ankitects/anki/commit/97efd49cd8db15a84d2ae2701d21b8283e37bc8c
+
+--- qt/aqt/__init__.py.orig	2024-03-31 06:57:06 UTC
 +++ qt/aqt/__init__.py
-@@ -401,7 +401,7 @@ def setupGL(pm: aqt.profiles.ProfileManager) -> None:
-     driver = pm.video_driver()
+@@ -6,7 +6,12 @@ import sys
+ import logging
+ import sys
  
-     # work around pyqt loading wrong GL library
--    if is_lin:
-+    if is_lin and not sys.platform.startswith("freebsd"):
-         import ctypes
+-import pip_system_certs.wrapt_requests
++try:
++    import pip_system_certs.wrapt_requests
++except ModuleNotFoundError:
++    print(
++        "Python module pip_system_certs is not installed. System certificate store and custom SSL certificates may not work. See: https://github.com/ankitects/anki/issues/3016"
++    )
  
-         ctypes.CDLL("libGL.so.1", ctypes.RTLD_GLOBAL)
+ if sys.version_info[0] < 3 or sys.version_info[1] < 9:
+     raise Exception("Anki requires Python 3.9+")
