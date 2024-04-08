d0a2873960a1f1272fd5efadb7207f01e2b86767
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-04-09 04:13:22 +0800

    devel/py-decoratortools: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

--- devel/py-decoratortools/files/patch-setup.py
+++ devel/py-decoratortools/files/patch-setup.py
@@ -1,6 +1,6 @@
 --- setup.py.orig	2010-05-05 05:55:08 UTC
 +++ setup.py
-@@ -1,8 +1,6 @@
+@@ -1,18 +1,16 @@
  #!/usr/bin/env python
  """Distutils setup file"""
  
@@ -9,7 +9,10 @@
  from setuptools import setup
  
  # Metadata
-@@ -12,7 +10,7 @@ PACKAGES = ['peak', 'peak.util']
+-PACKAGE_NAME = "DecoratorTools"
++PACKAGE_NAME = "decoratortools"
+ PACKAGE_VERSION = "1.8"
+ PACKAGES = ['peak', 'peak.util']
  
  def get_description():
      # Get our long description from the documentation
