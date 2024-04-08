d399ec512e2c3b6dceb14f1f30921273e7f72a60
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-04-09 04:13:23 +0800

    devel/py-deprecated: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-deprecated/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-05-27 15:55:40 UTC
++++ setup.py
+@@ -142,7 +142,7 @@ setup(
+ from setuptools import setup
+ 
+ setup(
+-    name='Deprecated',
++    name='deprecated',
+     version='1.2.14',
+     url='https://github.com/tantale/deprecated',
+     project_urls={
