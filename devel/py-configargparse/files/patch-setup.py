d349395d542f11e724de575c494d095ef6caba91
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-04-08 14:46:10 +0800

    devel/py-configargparse: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-configargparse/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2021-10-03 00:16:57 UTC
++++ setup.py
+@@ -80,7 +80,7 @@ setup(
+ 
+ 
+ setup(
+-    name='ConfigArgParse',
++    name='configargparse',
+     version="1.5.3",
+     description='A drop-in replacement for argparse that allows options to '
+                 'also be set via config files and/or environment variables.',
