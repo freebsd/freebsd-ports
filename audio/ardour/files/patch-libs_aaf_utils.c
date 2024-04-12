edfde42f517aa7e9f9f8f5d325455937748f5c28
Florian Walpen <dev@submerge.ch>
2024-04-12 15:25:23 +0200

    audio/ardour: Update to 8.6.0 release.
    ChangeLog:	https://ardour.org/whatsnew.html
    PR:		278327

--- audio/ardour/files/patch-libs_aaf_utils.c
+++ audio/ardour/files/patch-libs_aaf_utils.c
@@ -1,6 +1,6 @@
---- libs/aaf/AAFIAudioFiles.c.orig	2023-12-14 16:14:03 UTC
-+++ libs/aaf/AAFIAudioFiles.c
-@@ -39,6 +39,9 @@
+--- libs/aaf/utils.c.orig	2024-04-12 12:56:21 UTC
++++ libs/aaf/utils.c
+@@ -32,6 +32,9 @@
  #include <arpa/inet.h>
  #include <mntent.h>
  #include <unistd.h> /* access() */
