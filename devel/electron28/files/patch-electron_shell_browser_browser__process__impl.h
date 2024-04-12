0731023c8be081b7495655c125b63b455a40bb0b
Hiroki Tagato <tagattie@FreeBSD.org>
2024-04-12 19:57:48 +0900

    devel/electron28: update to 28.3.0
    Changelog: https://github.com/electron/electron/releases/tag/v28.3.0
    
    Reported by:	GitHub (watch releases)
    MFH:		2024Q2
    Security:	31617e47-7eec-4c60-9fdf-8aee61622bab

--- devel/electron28/files/patch-electron_shell_browser_browser__process__impl.h
+++ devel/electron28/files/patch-electron_shell_browser_browser__process__impl.h
@@ -1,4 +1,4 @@
---- electron/shell/browser/browser_process_impl.h.orig	2023-12-04 18:19:02 UTC
+--- electron/shell/browser/browser_process_impl.h.orig	2024-04-09 23:57:48 UTC
 +++ electron/shell/browser/browser_process_impl.h
 @@ -23,7 +23,7 @@
  #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -9,16 +9,16 @@
  #include "components/os_crypt/sync/key_storage_util_linux.h"
  #endif
  
-@@ -57,7 +57,7 @@ class BrowserProcessImpl : public BrowserProcess {
-   void SetSystemLocale(const std::string& locale);
+@@ -61,7 +61,7 @@ class BrowserProcessImpl : public BrowserProcess {
    const std::string& GetSystemLocale() const;
+   electron::ResolveProxyHelper* GetResolveProxyHelper();
  
 -#if BUILDFLAG(IS_LINUX)
 +#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
    void SetLinuxStorageBackend(os_crypt::SelectedLinuxBackend selected_backend);
    const std::string& GetLinuxStorageBackend() const;
  #endif
-@@ -132,7 +132,7 @@ class BrowserProcessImpl : public BrowserProcess {
+@@ -140,7 +140,7 @@ class BrowserProcessImpl : public BrowserProcess {
    std::unique_ptr<PrefService> local_state_;
    std::string locale_;
    std::string system_locale_;
