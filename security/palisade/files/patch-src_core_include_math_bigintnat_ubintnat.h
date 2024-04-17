42061cd40e78e7fad897bf0239045201aeecd5c7
Yuri Victorovich <yuri@FreeBSD.org>
2024-04-17 12:27:48 -0700

    security/palisade: update 1.11.6 → 1.11.9

--- security/palisade/files/patch-src_core_include_math_bigintnat_ubintnat.h
+++ security/palisade/files/patch-src_core_include_math_bigintnat_ubintnat.h
@@ -1,6 +1,6 @@
---- src/core/include/math/bigintnat/ubintnat.h.orig	2022-01-28 23:16:29 UTC
+--- src/core/include/math/bigintnat/ubintnat.h.orig	2022-12-02 20:31:23 UTC
 +++ src/core/include/math/bigintnat/ubintnat.h
-@@ -2218,12 +2218,17 @@ class NativeIntegerT
+@@ -2218,7 +2218,12 @@ class NativeIntegerT
      res.lo = x.lo * y;
      asm("umulh %0, %1, %2\n\t" : "=r"(res.hi) : "r"(x.lo), "r"(y));
      res.hi += x.hi * y;
@@ -14,9 +14,3 @@
      uint64_t wres(0), wa(a), wb(b);
  
      wres = wa * wb;  // should give us the lower 64 bits of 32*32
-     res.hi = wres >> 32;
--    res.lo = (uint32_t)wres && 0xFFFFFFFF;
-+    res.lo = (uint32_t)wres & 0xFFFFFFFF;
- #elif defined(__EMSCRIPTEN__)  // web assembly
-     U64BITS a1 = a >> 32;
-     U64BITS a2 = (uint32_t)a;
