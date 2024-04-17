8d540f0bc78e7104ae79469baebeb125046edb82
Fernando Apesteguía <fernape@FreeBSD.org>
2024-04-17 15:08:47 +0200

    sysutils/conky: update to 1.20.1
    Bugfix over the short-lived 1.20.0
    
    ChangeLog: https://github.com/brndnmtthws/conky/releases/tag/v1.20.0
    
    Features
    
     * Build AppImage separately per OS
     * Rotate appimage signing key
     * Add/use clang 15 on ubuntu builds
     * Define text_object.data.l as "long long"
     * Bump web deps, fix lint
     * Updated flake per NixOS/nix#5532
     * Fix bad imlib2 typedefs for imlib<1.10
     * Use --force with brew install, block upgrades
     * Tidy up docs
     * Update README.md
     * Update README.md
     * Handle external PropertyNotify events
     * Label dependency changes
     * Tweak XSendEvent arguments
     * Add modified date to web docs
     * Improve documentation for window types and hints
     * Refactor x11 event handling
     * Issue template: Link docs to obtain stack traces
     * Exclusive XInput event handling
    
    Bug fixes
    
     * Enable wayland in appimage build
     * Fix regression in lua_load path handling
     * Lua imlib2 fixes
     * Increase max length of ACPI temp (fixes #977)
     * Skip propagation of non-input events
     * Fix render order in draw_stuff
     * Fix event propagation on Openbox
     * Fix missing y_abs argument copy

--- sysutils/conky/files/patch-src_x11.h
+++ sysutils/conky/files/patch-src_x11.h
@@ -1,4 +1,4 @@
---- src/x11.h.orig	2022-12-26 18:54:10 UTC
+--- src/x11.h.orig	2024-04-17 02:04:14 UTC
 +++ src/x11.h
 @@ -22,6 +22,7 @@
   *
@@ -7,8 +7,8 @@
 +#ifdef BUILD_X11
  #pragma once
  
- #include <X11/Xatom.h>
-@@ -157,3 +158,5 @@ extern priv::use_xpmdb_setting use_xpmdb;
+ #include "config.h"
+@@ -246,3 +247,5 @@ extern priv::use_xpmdb_setting use_xpmdb;
  #else
  extern priv::use_xpmdb_setting use_xpmdb;
  #endif
