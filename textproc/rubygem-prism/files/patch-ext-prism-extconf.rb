d83e9ee7e8a41c87aeff9702e66ed1094a642d9e
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-04-21 02:29:19 +0800

    textproc/rubygem-prism: Update to 0.26.0
    Changes:	https://github.com/ruby/prism/releases
    		https://github.com/ruby/prism/blob/main/CHANGELOG.md

--- textproc/rubygem-prism/files/patch-ext-prism-extconf.rb
+++ textproc/rubygem-prism/files/patch-ext-prism-extconf.rb
@@ -1,11 +1,11 @@
---- ext/prism/extconf.rb.orig	2024-04-06 20:48:24 UTC
+--- ext/prism/extconf.rb.orig	2024-04-20 15:25:13 UTC
 +++ ext/prism/extconf.rb
-@@ -45,7 +45,7 @@ def make(target)
- # by this script.`
- def make(target)
+@@ -49,7 +49,7 @@ def make(target)
    Dir.chdir(File.expand_path("../..", __dir__)) do
--    system(RUBY_PLATFORM.include?("openbsd") ? "gmake" : "make", target, exception: true)
-+    system((RUBY_PLATFORM.include?("freebsd") || RUBY_PLATFORM.include?("openbsd")) ? "gmake" : "make", target, exception: true)
-   end
- end
- 
+     system(
+       RbConfig::CONFIG.slice(*%w[SOEXT CPPFLAGS CFLAGS CC AR ARFLAGS MAKEDIRS RMALL]), # env
+-      RUBY_PLATFORM.include?("openbsd") ? "gmake" : "make",
++      (RUBY_PLATFORM.include?("freebsd") || RUBY_PLATFORM.include?("openbsd")) ? "gmake" : "make",
+       target,
+       exception: true
+     )
