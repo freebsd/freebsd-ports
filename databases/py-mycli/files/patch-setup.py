2f918ed9e6175410f8a10f0728cb51fc64807b3a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-04-20 22:22:38 +0800

    databases/py-mycli: Allow build with py-sqlparse 0.5.0+
    - Bump PORTREVISION for package change

--- databases/py-mycli/files/patch-setup.py
+++ databases/py-mycli/files/patch-setup.py
@@ -1,6 +1,11 @@
 --- setup.py.orig	2024-03-29 05:11:01 UTC
 +++ setup.py
-@@ -28,7 +28,7 @@ install_requirements = [
+@@ -24,11 +24,11 @@ install_requirements = [
+     'Pygments>=1.6',
+     'prompt_toolkit>=3.0.6,<4.0.0',
+     'PyMySQL >= 0.9.2',
+-    'sqlparse>=0.3.0,<0.5.0',
++    'sqlparse>=0.3.0,<0.6.0',
      'sqlglot>=5.1.3',
      'configobj >= 5.0.5',
      'cli_helpers[styles] >= 2.2.1',
