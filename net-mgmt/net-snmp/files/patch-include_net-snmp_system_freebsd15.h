03e9f0cdfac68b11552c62cac1eaa27690fc0d32
Mark Johnston <markj@FreeBSD.org>
2024-04-29 16:39:49 -0400

    net-mgmt/net-snmp: Fix freebsd15.h
    It was copied from freebsd14.h without updating it.
    
    Also remove some bogus copying in the port makefile.  5.9.4 provides
    freebsdN.h for N up to 14, and we have a patch which adds freebsd15.h.
    So, get rid of the copying too.
    
    Approved by:	zi
    Sponsored by:	Klara, Inc.
    Sponsored by:	Stormshield
    Differential Revision:	https://reviews.freebsd.org/D45029

--- net-mgmt/net-snmp/files/patch-include_net-snmp_system_freebsd15.h
+++ net-mgmt/net-snmp/files/patch-include_net-snmp_system_freebsd15.h
@@ -1,6 +1,6 @@
 --- include/net-snmp/system/freebsd15.h.orig	2024-03-02 16:01:17 UTC
 +++ include/net-snmp/system/freebsd15.h
 @@ -0,0 +1,3 @@
-+/* freebsd14 is a superset of freebsd13 */
-+#include "freebsd13.h"
-+#define freebsd13 freebsd13
++/* freebsd15 is a superset of freebsd14 */
++#include "freebsd14.h"
++#define freebsd14 freebsd14
