6ff322008f1b38763ec94cb2737e71a69560f820
Li-Wen Hsu <lwhsu@FreeBSD.org>
2024-05-10 01:41:02 +0800

    chinese/fcitx5-mcbopomofo: Add missing patch
    Fixes:	251c9fc51cd7a172705c872153973e7327eac341

---
+++ chinese/fcitx5-mcbopomofo/files/patch-src_KeyHandler.cpp
@@ -0,0 +1,10 @@
+--- src/KeyHandler.cpp.orig	2024-05-06 03:07:04 UTC
++++ src/KeyHandler.cpp
+@@ -25,6 +25,7 @@
+ 
+ #include <algorithm>
+ #include <chrono>
++#include <sstream>
+ #include <utility>
+ 
+ #include "ChineseNumbers/ChineseNumbers.h"
