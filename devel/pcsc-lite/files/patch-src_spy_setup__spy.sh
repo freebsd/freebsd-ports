0a1b69a66843b4566f3cf1c70937d6650f59c722
Gleb Popov <arrowd@FreeBSD.org>
2024-05-09 11:14:05 +0300

    devel/pcsc-lite: Update to 2.2.1
    Sponsored by:	Serenity Cybersecurity, LLC
    
    Reported by:	portscout!

--- devel/pcsc-lite/files/patch-src_spy_setup__spy.sh
+++ devel/pcsc-lite/files/patch-src_spy_setup__spy.sh
@@ -1,6 +1,6 @@
---- src/spy/uninstall_spy.sh.orig	2020-08-05 11:01:32 UTC
-+++ src/spy/uninstall_spy.sh
-@@ -5,7 +5,7 @@ set -e
+--- src/spy/setup_spy.sh.orig	2024-05-08 09:57:49 UTC
++++ src/spy/setup_spy.sh
+@@ -5,7 +5,7 @@ PATH="$PATH:/usr/sbin"
  
  # ldconfig --print-cache will return something like
  # libpcsclite.so.1 (libc6,x86-64) => /usr/lib/x86_64-linux-gnu/libpcsclite.so.1
@@ -8,4 +8,4 @@
 +DIR=$(ldconfig -r | grep libpcsclite.so.1)
  
  # get the right part only: /usr/lib/x86_64-linux-gnu/libpcsclite.so.1
- DIR=$(echo $DIR | cut -d'=' -f2 | cut -d' ' -f2)
+ DIR=$(echo "$DIR" | cut -d'=' -f2 | cut -d' ' -f2)
