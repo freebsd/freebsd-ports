7a740b7c908f95b55bf765364c1234b3f6750196
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2024-05-11 18:19:37 +0200

    */tex*: Update version 20230313=>20240312
    - devel/tex-kpathsea: Update version 6.3.5=>6.4.0
    - devel/tex-libtexlua: Update distinfo
    - devel/tex-synctex: Update distinfo
    - devel/tex-web2c: Update version 20230313=>20240312
    - japanese/tex-ptex: Update distinfo
    - print/tex-basic-engines: Update version 20230313=>20240312
    - print/tex-dvipdfmx: Update version 20230313=>20240312
    - print/tex-dvipsk: Update version 20230313=>20240312
    - print/tex-jadetex: Update version 20230313=>20240312
    - print/tex-luatex: Update version 20230313=>20240312
    - print/tex-ptexenc: Update version 1.4.3=>1.4.6
    - print/tex-xdvik: Bump REVISION
    - print/tex-xetex: Update verison 0.99993=>20240312
    - print/tex-xmltex: Update verison 1.9=>20240312
    - print/texlive-base: Update version 20230313=>20240312
    - print/texlive-docs: Update version 20230313=>20240312
    - print/texlive-texmf-source: Update version 20230313=>20240312
    - print/texlive-texmf: Update version 20230313=>20240312
    - print/texlive-tlmgr: Update version 20230313=>20240312
    - print/xpdfopen: Update version 0.86=>20240312
    
    Sponsored by: Nepustil

--- print/texlive-base/files/patch-texk_texlive_linked__scripts_latexmk_latexmk.pl
+++ print/texlive-base/files/patch-texk_texlive_linked__scripts_latexmk_latexmk.pl
@@ -1,6 +1,6 @@
---- texk/texlive/linked_scripts/latexmk/latexmk.pl.orig	2023-03-20 14:47:33 UTC
+--- texk/texlive/linked_scripts/latexmk/latexmk.pl.orig	2024-02-01 21:05:56 UTC
 +++ texk/texlive/linked_scripts/latexmk/latexmk.pl
-@@ -1107,17 +1107,17 @@ else {
+@@ -1156,17 +1156,17 @@ else {
      #
      # The following works on Solaris, LINUX, HP-UX, IRIX
      # Use -f to get full listing, including command line arguments.
@@ -21,7 +21,7 @@
      }
      else {
         $pscmd = "ps -f"; 
-@@ -1134,7 +1134,7 @@ else {
+@@ -1183,7 +1183,7 @@ else {
          # But (19 Aug 2010), the truncation doesn't happen on RHEL4 and 5,
          #    unless the output is written to a terminal.  So the --width 
          #    option is now unnecessary
@@ -30,7 +30,7 @@
      }
      elsif ( $^O eq "darwin" ) {
          # OS-X on Macintosh
-@@ -1155,7 +1155,7 @@ else {
+@@ -1206,7 +1206,7 @@ else {
          $ps_previewer = $ps_previewer_landscape = 'NONE';
          # Others
          $lpr_pdf  = 'lpr %O %S';
