2a4df6b5503fac4ca315d0e47feee6f2709b4169
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:15 +0800

    devel/py-xstatic-rickshaw: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-rickshaw/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2020-04-09 21:06:31 UTC
++++ setup.py
+@@ -7,7 +7,7 @@ setup(
+ from setuptools import setup, find_packages
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-rickshaw',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
