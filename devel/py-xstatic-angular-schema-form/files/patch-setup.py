331a476c530b516ce73012b353dd67aa0fb612d6
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:03 +0800

    devel/py-xstatic-angular-schema-form: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-angular-schema-form/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2016-05-04 13:51:40 UTC
++++ setup.py
+@@ -7,7 +7,7 @@ setup(
+ from setuptools import setup, find_packages
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-angular-schema-form',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
