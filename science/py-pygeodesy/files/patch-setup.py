9e71843145bfe9da5e4beef7db445ce59a62419e
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:06:57 +0800

    science/py-pygeodesy: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ science/py-pygeodesy/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-04-01 06:02:24 UTC
++++ setup.py
+@@ -104,7 +104,7 @@ _KeyWords = ('AER', 'Albers', 'altitude', 'Andoyer', '
+              'Web-Mercator', 'Welford', 'WGRS', 'WGS', 'Whyatt', 'Wildberger', 'Witkam', 'winding-number',
+              'XYZ', 'yaw', 'You')
+ 
+-setup(name='PyGeodesy',
++setup(name='pygeodesy',
+       packages=['pygeodesy', 'pygeodesy.deprecated', 'pygeodesy.geodesicx'],
+       description='Pure Python geodesy tools',
+       version=_version(),
