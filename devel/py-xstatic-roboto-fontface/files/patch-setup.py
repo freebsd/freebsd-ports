ff71c354122d42202050a209fda828d78b0fc3b4
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:16 +0800

    devel/py-xstatic-roboto-fontface: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-roboto-fontface/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2016-11-14 17:08:41 UTC
++++ setup.py
+@@ -6,7 +6,7 @@ setup(
+ long_description = open('README.txt').read()
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-roboto-fontface',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
