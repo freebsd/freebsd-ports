55dcb594907fddaf84b91e0835ac64f586bf04ff
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:00 +0800

    devel/py-xstatic-angular: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-angular/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2022-05-31 15:33:56 UTC
++++ setup.py
+@@ -6,7 +6,7 @@ setup(
+ long_description = open('README.txt').read()
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-angular',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
