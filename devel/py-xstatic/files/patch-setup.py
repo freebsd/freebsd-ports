54d77d15a55f50e92f41018477c4c879a432993d
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:00:59 +0800

    devel/py-xstatic: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-05-04 10:38:54 UTC
++++ setup.py
+@@ -19,7 +19,7 @@ setup(
+ 
+ 
+ setup(
+-    name='XStatic',
++    name='xstatic',
+     version='1.0.3',
+     description='XStatic base package with minimal support code',
+     long_description=long_description,
