8341a0bc53663316800f571ae465f031b1a5ac46
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:04:52 +0800

    net/py-pysocks: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

--- net/py-pysocks/files/patch-setup.py
+++ net/py-pysocks/files/patch-setup.py
@@ -1,5 +1,14 @@
 --- setup.py.orig	2019-05-11 09:18:35 UTC
 +++ setup.py
+@@ -19,7 +19,7 @@ setup(
+     VERSION = re.compile(r'.*__version__ = "(.*?)"', re.S).match(f.read()).group(1)
+ 
+ setup(
+-    name="PySocks",
++    name="pysocks",
+     version=VERSION,
+     description="A Python SOCKS client module. See https://github.com/Anorov/PySocks for more information.",
+     long_description=long_description,
 @@ -32,12 +32,12 @@ setup(
      py_modules=["socks", "sockshandler"],
      install_requires=requirements,
