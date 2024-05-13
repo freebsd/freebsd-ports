4a380763fd90ae57f8948362fc1ec34cacb05e51
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:08 +0800

    devel/py-xstatic-jasmine: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-jasmine/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2019-02-14 06:44:18 UTC
++++ setup.py
+@@ -6,7 +6,7 @@ setup(
+ long_description = open('README.txt').read()
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-jasmine',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
