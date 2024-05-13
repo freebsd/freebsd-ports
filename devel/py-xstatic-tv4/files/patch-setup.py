5cb5c6d0fb3d3d8a3cfc9db32fb7c639900905ab
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:19 +0800

    devel/py-xstatic-tv4: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-tv4/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2016-05-05 05:06:16 UTC
++++ setup.py
+@@ -7,7 +7,7 @@ setup(
+ from setuptools import setup, find_packages
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-tv4',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
