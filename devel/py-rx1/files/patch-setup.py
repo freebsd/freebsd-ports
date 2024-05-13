646bdc87928da59a6ae99e135eda6f281b4daac9
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:00:11 +0800

    devel/py-rx1: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-rx1/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2018-02-17 14:32:28 UTC
++++ setup.py
+@@ -6,7 +6,7 @@ setup(
+     from setuptools import setup
+ 
+ setup(
+-    name='Rx',
++    name='rx',
+     version='1.6.1',
+     description='Reactive Extensions (Rx) for Python',
+     long_description=("is a library for composing asynchronous and "
