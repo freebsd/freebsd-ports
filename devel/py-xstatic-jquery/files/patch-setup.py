c9009e0e6372a50aa0586b68d6942e348d7272ab
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:09 +0800

    devel/py-xstatic-jquery: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-jquery/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2018-09-18 21:49:40 UTC
++++ setup.py
+@@ -8,7 +8,7 @@ setup(
+ from setuptools import setup, find_packages
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-jquery',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
