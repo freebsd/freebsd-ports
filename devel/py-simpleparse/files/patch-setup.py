6cd87c52f9f38eda8d28e86480ac557d5f4beac0
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:00:18 +0800

    devel/py-simpleparse: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

--- devel/py-simpleparse/files/patch-setup.py
+++ devel/py-simpleparse/files/patch-setup.py
@@ -1,11 +1,14 @@
---- setup.py.orig	2020-03-07 01:30:49 UTC
+--- setup.py.orig	2020-03-07 01:41:28 UTC
 +++ setup.py
-@@ -29,7 +29,7 @@ if sys.platform == 'win32':
+@@ -29,9 +29,9 @@ if __name__ == "__main__":
      )['define'] = 'BAD_STATIC_FORWARD'
  
  if __name__ == "__main__":
 -    packages = find_packages(HERE)
 +    packages = find_packages(exclude=['examples*', 'tests*'])
      setup (
-         name = "SimpleParse",
+-        name = "SimpleParse",
++        name = "simpleparse",
          version = findVersion(),
+         description = "A Parser Generator for Python (w/mxTextTools derivative)",
+         author = "Mike C. Fletcher",
