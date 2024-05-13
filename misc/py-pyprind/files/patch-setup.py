b62faa9f1984c9a24b4b797e784391e27693a9ab
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:04:02 +0800

    misc/py-pyprind: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

--- misc/py-pyprind/files/patch-setup.py
+++ misc/py-pyprind/files/patch-setup.py
@@ -1,5 +1,14 @@
---- setup.py.orig	2016-04-14 15:46:12 UTC
+--- setup.py.orig	2021-04-18 17:12:43 UTC
 +++ setup.py
+@@ -16,7 +16,7 @@ VERSION = pyprind.__version__
+ 
+ VERSION = pyprind.__version__
+ 
+-setup(name='PyPrind',
++setup(name='pyprind',
+       version=VERSION,
+       description='Python Progress Bar and Percent Indicator Utility',
+       author='Sebastian Raschka',
 @@ -39,6 +39,7 @@ setup(name='PyPrind',
            'Programming Language :: Python :: 2.7',
            'Programming Language :: Python :: 3',
