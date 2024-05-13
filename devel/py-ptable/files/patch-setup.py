00467b568ac21fe3429efacd759b17e82822d1fc
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 21:59:23 +0800

    devel/py-ptable: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-ptable/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2015-05-12 03:04:29 UTC
++++ setup.py
+@@ -9,7 +9,7 @@ setup(
+ 
+ 
+ setup(
+-    name='PTable',
++    name='ptable',
+     version=version,
+     include_package_data=True,
+     zip_safe=False,
