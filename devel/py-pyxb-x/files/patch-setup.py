3cb9ff977c7c141c179cdae759eda05fd3f463d5
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 21:59:57 +0800

    devel/py-pyxb-x: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-pyxb-x/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2024-02-22 11:49:22 UTC
++++ setup.py
+@@ -87,7 +87,7 @@ setup(
+         package_data.update(b_data)
+ 
+ setup(
+-    name="PyXB-X",
++    name="pyxb-x",
+     description='PyXB-X ("pixbix") is a pure Python package that generates Python source code for classes that correspond to data structures defined by XMLSchema.',
+     author="Peter A. Bigot",
+     author_email="pabigot@users.sourceforge.net",
