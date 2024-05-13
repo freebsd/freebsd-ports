2ebcebda66d2e0659a8c2b167542e1e5fa6038aa
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:05:43 +0800

    net/py-softlayer: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

--- net/py-softlayer/files/patch-setup.py
+++ net/py-softlayer/files/patch-setup.py
@@ -1,5 +1,14 @@
 --- setup.py.orig	2023-12-20 21:04:33 UTC
 +++ setup.py
+@@ -14,7 +14,7 @@ setup(
+     LONG_DESCRIPTION = DESCRIPTION
+ 
+ setup(
+-    name='SoftLayer',
++    name='softlayer',
+     version='v6.1.11',
+     description=DESCRIPTION,
+     long_description=LONG_DESCRIPTION,
 @@ -39,7 +39,7 @@ setup(
          'prompt_toolkit >= 2',
          'pygments >= 2.0.0',
