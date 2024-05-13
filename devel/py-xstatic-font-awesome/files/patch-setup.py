9d9350dafa138acbe5fea6dbc1568452abe3638c
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:07 +0800

    devel/py-xstatic-font-awesome: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-font-awesome/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-01-31 18:18:32 UTC
++++ setup.py
+@@ -7,7 +7,7 @@ setup(
+ from setuptools import setup, find_packages
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-font-awesome',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
