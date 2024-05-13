ecd5c6e8adfa5d7fd56b9b5e82f4674a0806fb87
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:09:31 +0800

    textproc/py-markuppy: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ textproc/py-markuppy/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2017-08-01 22:50:09 UTC
++++ setup.py
+@@ -1,7 +1,7 @@ import textwrap
+ from setuptools import setup
+ import textwrap
+ 
+-setup(name='MarkupPy',
++setup(name='markuppy',
+       version='1.14',
+       description='An HTML/XML generator',
+       url='https://github.com/tylerbakke/MarkupPy',
