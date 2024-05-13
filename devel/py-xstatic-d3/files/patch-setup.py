b81ea907e2a3a53d0de5c61dc01b9a386a3ad066
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:06 +0800

    devel/py-xstatic-d3: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-d3/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2016-11-01 18:59:16 UTC
++++ setup.py
+@@ -6,7 +6,7 @@ setup(
+ long_description = open('README.txt').read()
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-d3',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
