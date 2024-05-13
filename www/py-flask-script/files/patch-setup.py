16740a8faca14703964fb441a66937518cf4b9f9
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:12:50 +0800

    www/py-flask-script: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ www/py-flask-script/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2017-09-06 11:10:04 UTC
++++ setup.py
+@@ -28,7 +28,7 @@ setup(
+ install_requires = ['Flask']
+ 
+ setup(
+-    name='Flask-Script',
++    name='flask-script',
+     version=version,
+     url='http://github.com/smurfix/flask-script',
+ 	download_url = 'https://github.com/smurfix/flask-script/tarball/v'+version,
