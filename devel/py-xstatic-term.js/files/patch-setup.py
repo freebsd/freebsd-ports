1e68ff85db65fefa45f6935dfa1f1df809a7d13b
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:19 +0800

    devel/py-xstatic-term.js: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-term.js/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2016-01-05 22:53:53 UTC
++++ setup.py
+@@ -7,7 +7,7 @@ setup(
+ from setuptools import setup, find_packages
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-term.js',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
