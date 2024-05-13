1e92c8b73b894188de13b4d1113340fcc99ac4c4
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:01:17 +0800

    devel/py-xstatic-smart-table: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ devel/py-xstatic-smart-table/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2016-09-06 15:16:17 UTC
++++ setup.py
+@@ -6,7 +6,7 @@ setup(
+ long_description = open('README.txt').read()
+ 
+ setup(
+-    name=xs.PACKAGE_NAME,
++    name='xstatic-smart-table',
+     version=xs.PACKAGE_VERSION,
+     description=xs.DESCRIPTION,
+     long_description=long_description,
