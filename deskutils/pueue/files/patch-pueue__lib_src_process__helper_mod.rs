f10f5b39edb7a37a61d7bd07baeba1da0a33c859
Ryan Steinmetz <zi@FreeBSD.org>
2024-05-13 19:07:30 -0400

    deskutils/pueue: Update to 3.4.0
    Approved by:	yuri@ (maintainer, via email)

--- deskutils/pueue/files/patch-pueue__lib_src_process__helper_mod.rs
+++ deskutils/pueue/files/patch-pueue__lib_src_process__helper_mod.rs
@@ -1,17 +1,10 @@
---- pueue_lib/src/process_helper/mod.rs.orig	2023-03-31 21:59:47 UTC
+--- pueue_lib/src/process_helper/mod.rs.orig	2024-05-13 20:23:05 UTC
 +++ pueue_lib/src/process_helper/mod.rs
-@@ -21,6 +21,14 @@ mod linux;
- #[cfg(target_os = "linux")]
- pub use self::linux::process_exists;
+@@ -21,6 +21,7 @@ use command_group::Signal;
+ #[cfg_attr(target_os = "linux", path = "linux.rs")]
+ #[cfg_attr(target_vendor = "apple", path = "apple.rs")]
+ #[cfg_attr(target_os = "windows", path = "windows.rs")]
++#[cfg_attr(target_os = "freebsd", path = "freebsd.rs")]
+ mod platform;
+ pub use self::platform::*;
  
-+// FreeBSD specific process support
-+#[cfg(target_os = "freebsd")]
-+mod freebsd;
-+#[cfg(target_os = "freebsd")]
-+pub use self::freebsd::process_exists;
-+#[cfg(all(test, target_os = "freebsd"))]
-+use self::freebsd::tests;
-+
- // Apple specific process support
- #[cfg(target_vendor = "apple")]
- mod apple;
