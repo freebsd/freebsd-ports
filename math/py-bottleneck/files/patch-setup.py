2fc390ba404f039f630fac6685cd1b1f225fb82f
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-13 22:02:17 +0800

    math/py-bottleneck: Convert to USE_PYTHON=pep517
    - Bump PORTREVISION for package change

---
+++ math/py-bottleneck/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2024-02-25 00:30:13 UTC
++++ setup.py
+@@ -175,7 +175,7 @@ metadata = dict(
+ 
+ 
+ metadata = dict(
+-    name="Bottleneck",
++    name="bottleneck",
+     maintainer="Christopher Whelan",
+     maintainer_email="bottle-neck@googlegroups.com",
+     description="Fast NumPy array functions written in C",
