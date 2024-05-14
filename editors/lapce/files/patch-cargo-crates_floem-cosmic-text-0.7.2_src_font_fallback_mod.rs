b9beff373472b8bca33b348314518743a6ef1426
Mikael Urankar <mikael@FreeBSD.org>
2024-05-14 08:23:19 +0100

    editors/lapce: Fix menu display / Improve port
    - Fix menu display: https://github.com/lapce/floem/pull/443
    - Add desktop file and icon
    
    PR:		278950

--- editors/lapce/files/patch-cargo-crates_floem-cosmic-text-0.7.2_src_font_fallback_mod.rs
+++ editors/lapce/files/patch-cargo-crates_floem-cosmic-text-0.7.2_src_font_fallback_mod.rs
@@ -1,11 +1,22 @@
+https://github.com/lapce/cosmic-text/pull/6
+
 --- cargo-crates/floem-cosmic-text-0.7.2/src/font/fallback/mod.rs.orig	2024-05-09 07:49:44 UTC
 +++ cargo-crates/floem-cosmic-text-0.7.2/src/font/fallback/mod.rs
-@@ -11,7 +11,7 @@ use self::platform::*;
+@@ -10,7 +10,7 @@ use self::platform::*;
+ 
  use self::platform::*;
  
- #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows",)))]
--#[path = "other.rs"]
-+#[path = "unix.rs"]
+-#[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows",)))]
++#[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows", target_os = "freebsd",)))]
+ #[path = "other.rs"]
  mod platform;
  
- #[cfg(target_os = "macos")]
+@@ -18,7 +18,7 @@ mod platform;
+ #[path = "macos.rs"]
+ mod platform;
+ 
+-#[cfg(target_os = "linux")]
++#[cfg(any(target_os = "linux", target_os = "freebsd"))]
+ #[path = "unix.rs"]
+ mod platform;
+ 
