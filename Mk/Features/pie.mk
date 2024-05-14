b40aa97071012f71a9c8937ef41d3153f070243f
Jan Beich <jbeich@FreeBSD.org>
2024-05-14 10:35:00 +0200

    Mk/Features: WITH_PIE - add cmake support
    Use cmake specific options to enable position independent code generation.
    
    This fixes several ports (at least multimedia/cmrtlib, math/blas and
    math/lapack) which are build with cmake when WITH_PIE is enabled.
    
    PR:		268981
    Approved by:	portmgr

--- Mk/Features/pie.mk
+++ Mk/Features/pie.mk
@@ -5,11 +5,15 @@ _PIE_MK_INCLUDED=	yes
 PIE_Include_MAINTAINER=	portmgr@FreeBSD.org
 
 .  if !defined(PIE_UNSAFE)
+.    if defined(_INCLUDE_USES_CMAKE_MK)
+CMAKE_ARGS+=	-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true
+.    else
 PIE_CFLAGS?=	-fPIE -fPIC
 CFLAGS+=	${PIE_CFLAGS}
 CXXFLAGS+=	${PIE_CFLAGS}
 LDFLAGS+=	-pie
 STATIC_PIE_ARGS+=	-static-pie
+.    endif
 .  endif
 .endif
 
