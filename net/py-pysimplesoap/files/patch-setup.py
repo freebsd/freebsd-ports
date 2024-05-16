75abab400d5b969c32ee1beab25164de21226971
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-16 14:13:34 +0800

    net/py-pysimplesoap: Add py-pysimplesoap 1.16.2
    Python simple and lightweight SOAP library for client and server webservices
    interfaces, aimed to be as small and easy as possible, supporting most common
    functionality. Initially it was inspired by PHP Soap Extension (mimicking its
    functionality, simplicity and ease of use), with many advanced features added.

---
+++ net/py-pysimplesoap/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2017-12-03 17:06:13 UTC
++++ setup.py
+@@ -25,7 +25,7 @@ setup(
+         warnings.warn("Exception when converting the README format: %s" % e)
+ 
+ setup(
+-    name='PySimpleSOAP',
++    name='pysimplesoap',
+     version=__version__,
+     description='Python simple and lightweight SOAP Library',
+     long_description=long_desc,
