d801fc4fcdf07f00cdd48be25f76f0b7ffa819b6
bruno schwander <bruno.schwander@gmail.com>
2024-05-16 13:02:41 +0200

    add hostd and walletd

---
+++ net-p2p/hostd/files/patch-build_meta.go
@@ -0,0 +1,13 @@
+--- build/meta.go.orig	2024-04-22 06:39:23 UTC
++++ build/meta.go
+@@ -1,7 +1,7 @@ const (
+ package build
+ 
+ const (
+-	commit    = "?"
+-	version   = "?"
+-	buildTime = 0
++	commit    = "bd236b2"
++	version   = "v1.0.4-29-gbd236b2"
++	buildTime = BUILDTIME
+ )
