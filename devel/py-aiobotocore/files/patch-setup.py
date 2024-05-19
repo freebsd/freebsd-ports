353c622ec3c1099e6aa93728cceebacb7f1b3ea3
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-20 06:10:52 +0800

    devel/py-aiobotocore: Update to 2.13.0
    Changes:	https://github.com/aio-libs/aiobotocore/releases

--- devel/py-aiobotocore/files/patch-setup.py
+++ devel/py-aiobotocore/files/patch-setup.py
@@ -1,21 +1,21 @@
---- setup.py.orig	2024-04-11 16:38:18 UTC
+--- setup.py.orig	2024-05-16 19:19:03 UTC
 +++ setup.py
 @@ -7,15 +7,15 @@ install_requires = [
  # NOTE: When updating botocore make sure to update awscli/boto3 versions below
  install_requires = [
      # pegged to also match items in `extras_require`
--    'botocore>=1.34.41,<1.34.70',
-+    'botocore>=1.34.41',
-     'aiohttp>=3.7.4.post0,<4.0.0',
+-    'botocore>=1.34.70,<1.34.107',
++    'botocore>=1.34.70',
+     'aiohttp>=3.9.2,<4.0.0',
      'wrapt>=1.10.10, <2.0.0',
      'aioitertools>=0.5.1,<1.0.0',
  ]
  
  extras_require = {
--    'awscli': ['awscli>=1.32.41,<1.32.70'],
--    'boto3': ['boto3>=1.34.41,<1.34.70'],
+-    'awscli': ['awscli>=1.32.70,<1.32.107'],
+-    'boto3': ['boto3>=1.34.70,<1.34.107'],
 +    'awscli': ['awscli>=1.29.81'],
-+    'boto3': ['boto3>=1.34.41'],
++    'boto3': ['boto3>=1.34.70'],
  }
  
  
