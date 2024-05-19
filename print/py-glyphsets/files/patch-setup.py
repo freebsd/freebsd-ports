62aa7a844ae5fb5adb8dc1d845902a23cdededf2
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-20 00:18:37 +0800

    print/py-glyphsets: Update to 1.0.0
    Changes:	https://github.com/googlefonts/glyphsets/releases

--- print/py-glyphsets/files/patch-setup.py
+++ print/py-glyphsets/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2024-04-17 12:22:53 UTC
+--- setup.py.orig	2024-05-03 09:49:24 UTC
 +++ setup.py
 @@ -65,7 +65,7 @@ setup(
          "requests",
          "setuptools",
          "FontTools[ufo]",
--        "glyphsLib>=6.7.0",
+-        "glyphsLib>=6.7.1",
 +        "glyphsLib",
          "defcon",
          "unicodedata2",
-         "gflanguages>=0.5.16",
+         "gflanguages>=0.6.0",
