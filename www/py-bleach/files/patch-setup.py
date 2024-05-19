c84b9263d8562a799ac05b7316f221646c5b570b
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-20 00:19:53 +0800

    www/py-bleach: Allow build with py-tinycss2 1.3.0+
    - Bump PORTREVISION for package change

---
+++ www/py-bleach/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-10-06 16:19:57 UTC
++++ setup.py
+@@ -32,7 +32,7 @@ EXTRAS_REQUIRE = {
+ 
+ EXTRAS_REQUIRE = {
+     "css": [
+-        "tinycss2>=1.1.0,<1.3",
++        "tinycss2>=1.1.0,<1.4",
+     ],
+ }
+ 
