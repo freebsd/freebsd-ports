cdd1752148745b583a6ffcc92955e994c07e0adb
Pietro Cerutti <gahr@FreeBSD.org>
2024-05-21 07:16:38 +0000

    databases/mysqltcl: fix build with MariaDB
    PR:             277922 (exp-run)
    Reported by:    antoine

--- databases/mysqltcl/files/patch-generic_mysqltcl.c
+++ databases/mysqltcl/files/patch-generic_mysqltcl.c
@@ -1,6 +1,14 @@
---- generic/mysqltcl.c.orig	2024-01-26 14:58:40 UTC
+--- generic/mysqltcl.c.orig	2012-10-24 20:26:34 UTC
 +++ generic/mysqltcl.c
-@@ -911,7 +911,7 @@ static int Mysqltcl_Connect(ClientData clientData, Tcl
+@@ -46,6 +46,7 @@
+ #include <string.h>
+ #include <ctype.h>
+ #include <stdlib.h>
++#include <stdbool.h>
+ 
+ #define MYSQL_SMALL_SIZE  TCL_RESULT_SIZE /* Smaller buffer size. */
+ #define MYSQL_NAME_LEN     80    /* Max. database name length. */
+@@ -911,7 +912,7 @@ static int Mysqltcl_Connect(ClientData clientData, Tcl
  #if (MYSQL_VERSION_ID>=32350)
    if(mysql_options_reconnect)
    {
