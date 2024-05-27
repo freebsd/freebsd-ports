466b18e8dd474038da7a629574a69d7bd3f3214a
Jan Bramkamp <crest@rlwinm.de>
2024-05-27 20:50:02 +0200

    sysutils/runwhen: Add skalibs-2.14 support
    Patch skalibs-2.14 support into runwhen-2021.04.30:
      - Skalibs no longer pollutes the namespace with uint64 (without _t).
      - Patch runwhen to use uint64_t instead of uint64.
      - Update dependency to devel/skalibs>=2.14.1.1
      - Bump port revision.
    
    No functional change intended.
    
    PR:		279116

--- sysutils/runwhen/files/patch-src_rw__scan.c
+++ sysutils/runwhen/files/patch-src_rw__scan.c
@@ -1,14 +1,17 @@
---- src/rw_scan.c.orig	2022-08-10 16:08:50 UTC
+--- src/rw_scan.c.orig	2020-04-23 08:44:17 UTC
 +++ src/rw_scan.c
-@@ -9,7 +9,7 @@
+@@ -9,9 +9,9 @@ extern char const* PROG;
  
  extern char const* PROG;
  
 -void rw_scan(tain_t* stamp, char const* text) {
 +void rw_scan(tain* stamp, char const* text) {
    unsigned int i, scale;
-   uint64 u;
+-  uint64 u;
++  uint64_t u;
    char const* x;
+   switch (text[0]) {
+     case '$':
 @@ -53,7 +53,7 @@ void rw_scan(tain_t* stamp, char const* text) {
        tai_unix(tain_secp(stamp), u);
        break;
