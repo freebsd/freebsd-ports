9a0b8ca57d23a219954c64919f9891e426e402cb
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-28 18:00:38 +0800

    finance/py-yfinance: Update to 0.2.40
    Changes:	https://github.com/ranaroussi/yfinance/releases

--- finance/py-yfinance/files/patch-setup.py
+++ finance/py-yfinance/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2023-09-22 11:04:41 UTC
+--- setup.py.orig	2024-05-19 16:27:51 UTC
 +++ setup.py
 @@ -62,7 +62,7 @@ setup(
      install_requires=['pandas>=1.3.0', 'numpy>=1.16.5',
                        'requests>=2.31', 'multitasking>=0.0.7',
-                       'lxml>=4.9.1', 'appdirs>=1.4.4', 'pytz>=2022.5',
+                       'lxml>=4.9.1', 'platformdirs>=2.0.0', 'pytz>=2022.5',
 -                      'frozendict>=2.3.4', 'peewee>=3.16.2',
 +                      'frozendict>=2.3.4', 'peewee>=3.15.0',
                        'beautifulsoup4>=4.11.1', 'html5lib>=1.1'],
-     # Note: Pandas.read_html() needs html5lib & beautifulsoup4
-     entry_points={
+     extras_require={
+         'nospam': ['requests_cache>=1.0', 'requests_ratelimiter>=0.3.1'],
