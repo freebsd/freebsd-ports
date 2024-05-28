3bf602d2a7d0bc83686fe197fa71ae7d089b63d0
Jason W. Bacon <jwb@FreeBSD.org>
2024-05-28 16:25:08 -0500

    biology/gcta: Unbreak build on 14.x aarch64
    Missing #include <cstdint> in a few files
    
    Reported by:    pkg-fallout

---
+++ biology/gcta/files/patch-include_Covar.h
@@ -0,0 +1,10 @@
+--- include/Covar.h.orig	2024-05-27 23:23:59 UTC
++++ include/Covar.h
+@@ -19,6 +19,7 @@
+ #define gcta2_covar_h
+ #include <vector>
+ #include <string>
++#include <cstdint>
+ #include <map>
+ #include "Pheno.h"
+ 
---
+++ biology/gcta/files/patch-include_Marker.h
@@ -0,0 +1,10 @@
+--- include/Marker.h.orig	2024-05-28 12:14:10 UTC
++++ include/Marker.h
+@@ -20,6 +20,7 @@
+ #include <vector>
+ #include <map>
+ #include <utility>
++#include <cstdint>
+ using std::vector;
+ using std::string;
+ using std::to_string;
