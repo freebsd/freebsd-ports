199fbfa1fd5bf2f98c90151d313b20c91935d00b
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-05-28 18:00:28 +0800

    devel/py-livemark: Update to 0.110.8
    Changes:	https://github.com/frictionlessdata/livemark/releases
    		https://github.com/frictionlessdata/livemark/commits/main

--- devel/py-livemark/files/patch-setup.py
+++ devel/py-livemark/files/patch-setup.py
@@ -1,7 +1,11 @@
---- setup.py.orig	2023-03-16 09:34:49 UTC
+--- setup.py.orig	2024-04-16 13:24:16 UTC
 +++ setup.py
-@@ -40,7 +40,7 @@ INSTALL_REQUIRES = [
-     "marko>=1.0",
+@@ -37,10 +37,10 @@ INSTALL_REQUIRES = [
+ }
+ INSTALL_REQUIRES = [
+     "attrs>=22.0",
+-    "marko==1.*",
++    "marko>=1.0",
      "pyyaml>=5.3",
      "jinja2>=3.0",
 -    "pyquery==1.*",
