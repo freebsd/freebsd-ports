ebf9487ea69e1eecdf9d63acf9b8380463040be4
Jan Beich <jbeich@FreeBSD.org>
2024-05-29 22:00:23 +0200

    multimedia/libvpx: update 1.14.1
    Changes:	https://chromium.googlesource.com/webm/libvpx/+log/v1.14.0..v1.14.1
    Reported by:	portscout

--- multimedia/libvpx/files/patch-build_make_configure.sh
+++ multimedia/libvpx/files/patch-build_make_configure.sh
@@ -2,12 +2,13 @@
 - Recognize DragonFly as x86_64-linux-gcc target by default
 - Don't override -march= on armv6, armv7 and aarch64
 
---- build/make/configure.sh.orig	2024-01-09 21:12:22 UTC
+--- build/make/configure.sh.orig	2024-05-29 20:00:23 UTC
 +++ build/make/configure.sh
-@@ -752,6 +752,14 @@ process_common_toolchain() {
+@@ -785,6 +785,14 @@ process_common_toolchain() {
+     case "$gcctarget" in
        aarch64*)
          tgt_isa=arm64
-         ;;
++        ;;
 +      armv6*-gnueabihf)
 +        tgt_isa=armv6
 +        float_abi=hard
@@ -15,11 +16,10 @@
 +      armv6*-gnueabi)
 +        tgt_isa=armv6
 +        float_abi=softfp
-+        ;;
+         ;;
        armv7*-hardfloat* | armv7*-gnueabihf | arm-*-gnueabihf)
          tgt_isa=armv7
-         float_abi=hard
-@@ -806,7 +814,7 @@ process_common_toolchain() {
+@@ -840,7 +848,7 @@ process_common_toolchain() {
          [ -z "$tgt_isa" ] && tgt_isa=x86
          tgt_os=win32
          ;;
@@ -28,16 +28,16 @@
          tgt_os=linux
          ;;
        *solaris2.10)
-@@ -997,7 +1005,7 @@ EOF
-             fi
-           done
-           ;;
--        armv7|armv7s)
-+        armv6|armv7|armv7s)
-           soft_enable neon
-           # Only enable neon_asm when neon is also enabled.
-           enabled neon && soft_enable neon_asm
-@@ -1017,29 +1025,6 @@ EOF
+@@ -1015,7 +1023,7 @@ EOF
+     arm*)
+       soft_enable runtime_cpu_detect
+ 
+-      if [ ${tgt_isa} = "armv7" ] || [ ${tgt_isa} = "armv7s" ]; then
++      if [ ${tgt_isa} = "armv6" ] || [ ${tgt_isa} = "armv7" ] || [ ${tgt_isa} = "armv7s" ]; then
+         soft_enable neon
+         # Only enable neon_asm when neon is also enabled.
+         enabled neon && soft_enable neon_asm
+@@ -1033,29 +1041,6 @@ EOF
            arch_int=${tgt_isa##armv}
            arch_int=${arch_int%%te}
            tune_cflags="-mtune="
