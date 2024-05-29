b9e49ae19e3284f4a9de2afc0742f34dc2b391e0
Hiroki Tagato <tagattie@FreeBSD.org>
2024-05-30 07:26:51 +0900

    devel/electron29: update to 29.4.1
    Changelog: https://github.com/electron/electron/releases/tag/v29.4.1
    
    Reported by:	GitHub (watch releases)
    Security:	04e78f32-04b2-4c23-bfae-72600842d317

--- devel/electron29/files/patch-electron_shell_browser_api_electron__api__base__window.cc
+++ devel/electron29/files/patch-electron_shell_browser_api_electron__api__base__window.cc
@@ -1,6 +1,6 @@
---- electron/shell/browser/api/electron_api_base_window.cc.orig	2023-10-20 08:29:17 UTC
+--- electron/shell/browser/api/electron_api_base_window.cc.orig	2024-05-20 19:00:16 UTC
 +++ electron/shell/browser/api/electron_api_base_window.cc
-@@ -1079,7 +1079,7 @@ void BaseWindow::SetIconImpl(v8::Isolate* isolate,
+@@ -1081,7 +1081,7 @@ void BaseWindow::SetIconImpl(v8::Isolate* isolate,
    static_cast<NativeWindowViews*>(window_.get())
        ->SetIcon(native_image->GetHICON(GetSystemMetrics(SM_CXSMICON)),
                  native_image->GetHICON(GetSystemMetrics(SM_CXICON)));
