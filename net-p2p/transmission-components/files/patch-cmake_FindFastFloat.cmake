8326d697863137f1665bd21a1fc148b6363540fb
Fernando Apesteguía <fernape@FreeBSD.org>
2024-05-31 20:00:59 +0200

    net-p2p/transmission*: update to 4.0.6
    ChangeLog: https://github.com/transmission/transmission/releases/tag/4.0.6
    
    All Platforms
    
    Improved parsing HTTP tracker announce response.
    
     * Fixed 4.0.0 bug that caused some user scripts to have an invalid
       TR_TORRENT_TRACKERS environment variable.
     * Fixed 4.0.0 bug where alt-speed-enabled had no effect in settings.json.
     * Fixed 4.0.0 bug where the GTK client's "Use authentication" option was not
       saved between's sessions.
     * Fixed 4.0.0 bug where the filename for single-file torrents aren't sanitized.
    
    macOS Client
    
     * Fix: Sparkle support for handling beta version updates.
     * Fixed app unable to start when having many torrents and TimeMachine enabled.
     * Fix: Sparkle Version Comparator.
    
    Qt Client
    
     * Fixed 4.0.0 bug where piece size description text and slider state in torrent
       creation dialog are not always up-to-date.
    
    GTK Client
    
     * Fixed build when compiling with GTKMM 4.
     * Added developer name to metainfo files.
     * Added the launchable desktop-id to metainfo files.
     * Fixed build when compiling on BSD.
    
    Web Client
    
     * Fixed a 4.0.0 bug where the infinite ratio symbol was displayed incorrectly
       in the WebUI.
     * Fixed layout issue in speed display.
     * General UI improvement related to filterbar and fixes download/upload speed
       info wrap.
    
    Daemon
    
     * Fixed a couple of logging issues.
    
    Everything Else
    
     * Updated flatpak release metainfo.
     * Fixed libtransmission build on very old cmake versions.
     * UTP peer connections follow user-defined speed limits better now.
     * Only use a single concurrent queue for timeMachineExclude instead of one
       queue per torrent
     * Fixed 4.0.5 bug where svg and png icons in the WebUI might not be displayed.
     * Fixed 4.0.0 bug where alt-speed-enabled had no effect in settings.json.
     * Fixed 4.0.0 bugs where some RPC methods don't put torrents in recently-active
       anymore.
     * Improved parsing HTTP tracker announce response.
     * Fixed compatibility with clang-format 18.
     * Fixed build when compiling with mbedtls 3.x .
    
    PR:		279396
    Reported by:	mondo.debater_0q@icloud.com (maintainer)

--- net-p2p/transmission-components/files/patch-cmake_FindFastFloat.cmake
+++ net-p2p/transmission-components/files/patch-cmake_FindFastFloat.cmake
@@ -1,8 +1,8 @@
---- cmake/FindFastFloat.cmake.orig	2023-12-08 18:49:17 UTC
+--- cmake/FindFastFloat.cmake.orig	2024-02-22 15:48:36 UTC
 +++ cmake/FindFastFloat.cmake
 @@ -2,4 +2,4 @@ target_include_directories(FastFloat::fast_float
  
  target_include_directories(FastFloat::fast_float
      INTERFACE
--        ${CMAKE_CURRENT_LIST_DIR}/../third-party/fast_float/include)
-+        ${CMAKE_INSTALL_PREFIX}/include)
+-        ${TR_THIRD_PARTY_SOURCE_DIR}/fast_float/include)
++    	${CMAKE_INSTALL_PREFIX}/include)
