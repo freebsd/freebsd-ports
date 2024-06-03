0303e791dc37a475a6e52093b17884121abc7f27
Jan Beich <jbeich@FreeBSD.org>
2024-06-03 15:48:48 +0200

    x11-toolkits/wlroots015: replace override.mk with upstream fix

--- x11-toolkits/wlroots015/override.mk
+++
@@ -1,8 +0,0 @@
-WLR_SUFFIX=	015
-WLR_SOVERSION=	10
-
-LIB_DEPENDS:=	${LIB_DEPENDS:S/wlroots.so/&.${WLR_SOVERSION}/:S/wlroots$/&${WLR_SUFFIX}/}
-
-USES+=		localbase # -isystem
-CONFIGURE_ENV+=	PKG_CONFIG_PATH="${LOCALBASE}/wlroots${WLR_SUFFIX}/libdata/pkgconfig"
-MAKE_ENV+=	PKG_CONFIG_PATH="${LOCALBASE}/wlroots${WLR_SUFFIX}/libdata/pkgconfig"
