39a4ccbcc896b3e67f9c1ab9f65dea77959c3ec5
Craig Leres <leres@FreeBSD.org>
2024-06-06 10:41:07 -0700

    net/ipsumdump: Unbreak amd64 build after 64825b9
    aggwtree.cc:859:7: error: use of undeclared identifier 'bswap_32'
                    add(bswap_32(ubuf[2*i]), bswap_32(ubuf[2*i + 1]));
                        ^
    aggwtree.cc:859:28: error: use of undeclared identifier 'bswap_32'
                    add(bswap_32(ubuf[2*i]), bswap_32(ubuf[2*i + 1]));
                                             ^
    2 errors generated.
    gmake[1]: *** [Makefile:46: aggwtree.o] Error 1
    gmake[1]: *** Waiting for unfinished jobs....
    aggtree.cc:1139:7: error: use of undeclared identifier 'bswap_32'
                    add(bswap_32(ubuf[2*i]), bswap_32(ubuf[2*i + 1]));
                        ^
    aggtree.cc:1139:28: error: use of undeclared identifier 'bswap_32'
                    add(bswap_32(ubuf[2*i]), bswap_32(ubuf[2*i + 1]));
                                             ^
    2 errors generated.

--- net/ipsumdump/files/patch-src_aggtree.cc
+++ net/ipsumdump/files/patch-src_aggtree.cc
@@ -1,13 +1,14 @@
---- src/aggtree.cc.orig	2024-06-01 22:57:34 UTC
+--- src/aggtree.cc.orig	2014-05-02 12:23:53 UTC
 +++ src/aggtree.cc
-@@ -10,10 +10,6 @@
- 
+@@ -11,9 +11,11 @@
  #ifdef HAVE_BYTEORDER_H
  #include <byteorder.h>
--#else
--static inline uint32_t bswap_32(uint32_t u) {
--    return ((u >> 24) | ((u & 0xff0000) >> 8) | ((u & 0xff00) << 8) | ((u & 0xff) << 24));
--}
+ #else
++#if !defined(__powerpc64__) && !defined(POWERPC64)
+ static inline uint32_t bswap_32(uint32_t u) {
+     return ((u >> 24) | ((u & 0xff0000) >> 8) | ((u & 0xff00) << 8) | ((u & 0xff) << 24));
+ }
++#endif
  #endif
  
  
