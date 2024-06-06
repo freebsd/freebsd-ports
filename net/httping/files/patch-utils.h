d362e6d9dbb4a8d3e1692175df7434973c6f5e7f
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-06-07 02:22:27 +0800

    net/httping: Update to 3.7a and newer snapshot (bb94c6d)
    - Update WWW
    - Fix LICENSE
    - Update pkg-descr
    - Take maintainership
    
    Changes:	https://github.com/folkertvanheusden/HTTPing/commits/master

--- net/httping/files/patch-utils.h
+++
@@ -1,8 +0,0 @@
---- utils.h.orig	2022-10-29 19:34:27 UTC
-+++ utils.h
-@@ -16,4 +16,4 @@ char * format_value(double value, int digits_sig, int 
- #define min(x, y)	((x) < (y) ? (x) : (y))
- #define max(x, y)	((x) > (y) ? (x) : (y))
- 
--void myusleep(useconds_t v);
-+void myusleep(__useconds_t v);
