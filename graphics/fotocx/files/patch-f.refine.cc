8bb877912300eabb66d3b3f96d3926b2ab711560
Alexey Dokuchaev <danfe@FreeBSD.org>
2024-06-10 06:08:20 +0000

    graphics/fotocx: the port had been updated to version 24.40.

--- graphics/fotocx/files/patch-f.refine.cc
+++ graphics/fotocx/files/patch-f.refine.cc
@@ -2,22 +2,22 @@
 +++ f.refine.cc
 @@ -223,7 +223,7 @@ void m_edit_dist(GtkWidget *, ch  *menu)
     EFedit_dist.Farea = 2;                                                        //  select area usable
-    EFedit_dist.Frestart = 1;                                                     //  restart allowed
+    EFedit_dist.Fpaintedits = 1;                                                  //  use with paint edits OK               23.50
     EFedit_dist.Fscript = 1;                                                      //  scripting supported 
 -   EFedit_dist.threadfunc = thread;
 +   EFedit_dist.threadfunc = edit_dist_names::thread;
+ 
     if (! edit_setup(EFedit_dist)) return;                                        //  setup edit
  
- /***
 @@ -589,7 +589,7 @@ void m_flatdist(GtkWidget *, ch  *menu)
-    EFflatdist.Frestart = 1;                                                      //  restartable
+    EFflatdist.Farea = 2;                                                         //  select area usable
     EFflatdist.Fpaintedits = 1;                                                   //  use with paint edits OK
     EFflatdist.Fscript = 1;                                                       //  scripting supported
 -   EFflatdist.threadfunc = thread;
 +   EFflatdist.threadfunc = flatdist_names::thread;
+ 
     if (! edit_setup(EFflatdist)) return;                                         //  setup edit
  
-    Eww = E0pxm->ww;
 @@ -1067,7 +1067,7 @@ void flatdist_func(int _radius, int _flatten, int _deb
     EFflatdist.menufunc = m_flatdist;
     EFflatdist.Farea = 2;                                                         //  select area usable
