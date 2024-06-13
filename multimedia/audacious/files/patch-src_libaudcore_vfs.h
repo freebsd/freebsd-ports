135ca672a6990dd1481c6226aec3a6b2f0f7e848
Guido Falsi <madpilot@FreeBSD.org>
2024-06-13 19:51:18 +0200

    multimedia/audacious*: Update to 4.4
    Upstream moved to qt6 and gtk3 as main UI, so make that the default
    flavor.
    
    Cleaned up Makefile, reordering variables according to best practices
    and reordered/reflowed content of many variables.
    
    Please check UPDATING if already using the qt6 flavor.

--- multimedia/audacious/files/patch-src_libaudcore_vfs.h
+++ multimedia/audacious/files/patch-src_libaudcore_vfs.h
@@ -1,4 +1,4 @@
---- src/libaudcore/vfs.h.orig	2023-04-29 20:10:24 UTC
+--- src/libaudcore/vfs.h.orig	2024-06-12 20:16:04 UTC
 +++ src/libaudcore/vfs.h
 @@ -51,6 +51,7 @@ enum VFSSeekType
  
@@ -7,4 +7,4 @@
 +    VFS_SEEK_UNK = -1,
      VFS_SEEK_SET = 0,
      VFS_SEEK_CUR = 1,
-     VFS_SEEK_END = 2
+     VFS_SEEK_END = 2,
