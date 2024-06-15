d3e0e547ee3bfaa37aa10132d6fee0ed689333b6
Wen Heping <wen@FreeBSD.org>
2024-06-15 14:21:25 +0000

    math/py-numpy: Update to 1.26.4
    PR:		277689
    Reported by:	wen@
    Exp-run by:	antoine@

--- math/py-numpy/files/patch-numpy_f2py_f2py2e.py
+++ math/py-numpy/files/patch-numpy_f2py_f2py2e.py
@@ -1,18 +1,18 @@
---- numpy/f2py/f2py2e.py.orig	2019-01-31 02:31:08 UTC
+--- numpy/f2py/f2py2e.py.orig	2024-06-12 01:22:27 UTC
 +++ numpy/f2py/f2py2e.py
-@@ -599,6 +599,7 @@ def run_compile():
-     include_dirs, sources = filter_files('-I', '', sources, remove_prefix=1)
+@@ -693,6 +693,7 @@ def run_compile():
+     extra_objects, sources = filter_files('', '[.](o|a|so|dylib)', sources)
      library_dirs, sources = filter_files('-L', '', sources, remove_prefix=1)
      libraries, sources = filter_files('-l', '', sources, remove_prefix=1)
 +    extra_link_args, sources = filter_files('-Wl,|-B|-f','',sources,remove_prefix=0)
      undef_macros, sources = filter_files('-U', '', sources, remove_prefix=1)
      define_macros, sources = filter_files('-D', '', sources, remove_prefix=1)
      for i in range(len(define_macros)):
-@@ -625,6 +626,7 @@ def run_compile():
-                 'undef_macros': undef_macros,
-                 'extra_objects': extra_objects,
-                 'f2py_options': f2py_flags,
-+                'extra_link_args': extra_link_args,
-                 }
- 
-     if sysinfo_flags:
+@@ -727,6 +728,7 @@ def run_compile():
+         define_macros,
+         undef_macros,
+         f2py_flags,
++	extra_link_args,
+         sysinfo_flags,
+         fc_flags,
+         flib_flags,
