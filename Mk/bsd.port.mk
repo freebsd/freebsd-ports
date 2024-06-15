19d68720f1dac6176557ffa8d9c1684c4cf22608
Rene Ladan <rene@FreeBSD.org>
2024-06-15 20:44:12 +0200

    all: drop support for FreeBSD 13.2 which reached EOL today
    Thanks again to jbeich for noticing various things I overlooked.
    
    Reviewed by:	acm, ashish, eduardo, ehaupt, eugen, jbeich, martymac, mat, nobukata, sunpoet, tagattie, yuri, arrowd, kde
    Differential Revision:	https://reviews.freebsd.org/D45141

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -42,7 +42,7 @@ FreeBSD_MAINTAINER=	portmgr@FreeBSD.org
 # OSREL			- The release version of the operating system as a text
 #				  string (e.g., "12.4").
 # OSVERSION		- The operating system version as a comparable integer;
-#				  the value of __FreeBSD_version (e.g., 1302000).
+#				  the value of __FreeBSD_version (e.g., 1501000).
 #
 # This is the beginning of the list of all variables that need to be
 # defined in a port, listed in order that they should be included
@@ -1179,7 +1179,7 @@ OSVERSION!=	${AWK} '/^\#define[[:blank:]]__FreeBSD_version/ {print $$3}' < ${SRC
 .    endif
 _EXPORTED_VARS+=	OSVERSION
 
-.    if ${OPSYS} == FreeBSD && (${OSVERSION} < 1302000 )
+.    if ${OPSYS} == FreeBSD && (${OSVERSION} < 1303000 )
 _UNSUPPORTED_SYSTEM_MESSAGE=	Ports Collection support for your ${OPSYS} version has ended, and no ports\
 								are guaranteed to build on this system. Please upgrade to a supported release.
 .      if defined(ALLOW_UNSUPPORTED_SYSTEM)
