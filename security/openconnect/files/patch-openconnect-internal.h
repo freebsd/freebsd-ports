31800eab6e497d58e4c3bc8e6f3a286412022bc2
Daniel Engberg <diizzy@FreeBSD.org>
2024-06-16 17:55:23 +0200

    security/openconnect: Fix build with libxml 2.12.0 and newer
    Backport upstream commit 6c83dfd2e1cfb9539cd527424910298a52c6d11f
    
    Reference:
    https://gitlab.com/openconnect/openconnect/-/commit/6c83dfd2e1cfb9539cd527424910298a52c6d11f
    
    PR:		279761
    Reviewed by:	zi (maintainer)

--- security/openconnect/files/patch-openconnect-internal.h
+++ security/openconnect/files/patch-openconnect-internal.h
@@ -1,14 +1,22 @@
---- openconnect-internal.h.orig	2022-08-27 20:09:15 UTC
+--- openconnect-internal.h.orig	2023-05-19 16:12:15 UTC
 +++ openconnect-internal.h
-@@ -135,6 +135,11 @@
- #include <spawn.h>
- #endif
+@@ -85,6 +85,7 @@
+ #define N_(s) s
  
+ #include <libxml/tree.h>
++#include <libxml/parser.h>
+ #include <zlib.h>
+ 
+ #ifdef _WIN32
+@@ -133,6 +134,11 @@ extern char **environ;
+ extern char **environ;
+ #endif
+ #include <spawn.h>
++#endif
++
 +
 +#ifdef __FreeBSD__
 +extern char **environ;
-+#endif
-+
+ #endif
+ 
  /* Equivalent of "/dev/null" on Windows.
-  * See https://stackoverflow.com/a/44163934
-  */
