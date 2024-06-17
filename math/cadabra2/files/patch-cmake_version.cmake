4e5c284cdd21dfc2c299ae421ca07572ca52f74c
Yuri Victorovich <yuri@FreeBSD.org>
2024-06-17 00:18:34 -0700

    math/cadabra2: update 2.4.5.7 → 2.5.2-20240616

--- math/cadabra2/files/patch-cmake_version.cmake
+++ math/cadabra2/files/patch-cmake_version.cmake
@@ -1,15 +1,15 @@
---- cmake/version.cmake.orig	2019-12-25 01:28:15 UTC
+--- cmake/version.cmake.orig	2024-06-16 17:47:41 UTC
 +++ cmake/version.cmake
-@@ -4,7 +4,7 @@ set(CADABRA_VERSION_PATCH 8)
- set(CADABRA_VERSION_TWEAK 1)
- set(COPYRIGHT_YEARS "2001-2019")
+@@ -4,7 +4,7 @@ math(EXPR SYSTEM_BITS "${CMAKE_SIZEOF_VOID_P} * 8")
+ set(CADABRA_VERSION_TWEAK 2)
+ set(COPYRIGHT_YEARS "2001-2024")
  math(EXPR SYSTEM_BITS "${CMAKE_SIZEOF_VOID_P} * 8")
 -find_program(GIT git PATHS ${GIT_DIR})
 +#find_program(GIT git PATHS ${GIT_DIR})
  if(GIT)
    message("-- Git found: ${GIT}")
    execute_process(COMMAND git rev-parse --short HEAD OUTPUT_VARIABLE GIT_SHORT_SHA     OUTPUT_STRIP_TRAILING_WHITESPACE)
-@@ -16,7 +16,7 @@ endif()
+@@ -16,7 +16,7 @@ else()
  if(GIT_SHORT_SHA)
    set(CADABRA_VERSION_BUILD "${GIT_COMMIT_SERIAL}.${GIT_SHORT_SHA}")
  else()
