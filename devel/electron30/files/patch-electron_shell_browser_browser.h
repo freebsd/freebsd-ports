72d04ff33dcdce8eb8e172847c082ed62ce9ea32
Hiroki Tagato <tagattie@FreeBSD.org>
2024-06-20 05:15:55 +0900

    devel/electron30: update to 30.1.1
    Changelog:
    - https://github.com/electron/electron/releases/tag/v30.1.0
    - https://github.com/electron/electron/releases/tag/v30.1.1
    
    Reported by:	GitHub (watch releases)

--- devel/electron30/files/patch-electron_shell_browser_browser.h
+++ devel/electron30/files/patch-electron_shell_browser_browser.h
@@ -1,4 +1,4 @@
---- electron/shell/browser/browser.h.orig	2024-04-16 14:29:17 UTC
+--- electron/shell/browser/browser.h.orig	2024-06-12 10:49:40 UTC
 +++ electron/shell/browser/browser.h
 @@ -145,7 +145,7 @@ class Browser : public WindowListObserver {
  
@@ -9,7 +9,7 @@
    // get the name, icon and path for an application
    v8::Local<v8::Promise> GetApplicationInfoForProtocol(v8::Isolate* isolate,
                                                         const GURL& url);
-@@ -269,10 +269,10 @@ class Browser : public WindowListObserver {
+@@ -273,10 +273,10 @@ class Browser : public WindowListObserver {
    PCWSTR GetAppUserModelID();
  #endif  // BUILDFLAG(IS_WIN)
  
--- devel/electron30/files/patch-third__party_sqlite_src_amalgamation_sqlite3.c
+++ devel/electron30/files/patch-third__party_sqlite_src_amalgamation_sqlite3.c
@@ -1,5 +1,14 @@
---- third_party/sqlite/src/amalgamation/sqlite3.c.orig	2024-04-15 20:35:12 UTC
+--- third_party/sqlite/src/amalgamation/sqlite3.c.orig	2024-06-13 07:46:18 UTC
 +++ third_party/sqlite/src/amalgamation/sqlite3.c
+@@ -14117,7 +14117,7 @@ struct fts5_api {
+ ** But _XOPEN_SOURCE define causes problems for Mac OS X, so omit
+ ** it.
+ */
+-#if !defined(_XOPEN_SOURCE) && !defined(__DARWIN__) && !defined(__APPLE__)
++#if !defined(_XOPEN_SOURCE) && !defined(__DARWIN__) && !defined(__APPLE__) && !defined(__FreeBSD__)
+ #  define _XOPEN_SOURCE 600
+ #endif
+ 
 @@ -44768,7 +44768,12 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
    memset(zBuf, 0, nBuf);
    randomnessPid = osGetpid(0);
