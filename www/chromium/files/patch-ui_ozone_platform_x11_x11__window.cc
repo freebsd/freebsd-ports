007e51c89032b1604ee44a49ed63c26617f3ca93
Robert Nagy <rnagy@FreeBSD.org>
2024-06-20 15:05:10 +0200

    www/chromium: update to 126.0.6478.126
    Security:	https://vuxml.freebsd.org/freebsd/2b68c86a-32d5-11ef-8a0f-a8a1599412c6.html
    (cherry picked from commit 54898b9a6952cbf38e21ea3b3dd6313c1402ed0b)
    (cherry picked from commit 049d204f2bd3db516377f98db8a00322cf08ad84)

--- www/chromium/files/patch-ui_ozone_platform_x11_x11__window.cc
+++ www/chromium/files/patch-ui_ozone_platform_x11_x11__window.cc
@@ -1,11 +1,11 @@
---- ui/ozone/platform/x11/x11_window.cc.orig	2024-06-17 12:56:06 UTC
+--- ui/ozone/platform/x11/x11_window.cc.orig	2024-06-20 12:56:26 UTC
 +++ ui/ozone/platform/x11/x11_window.cc
-@@ -1493,7 +1493,7 @@ void X11Window::OnXWindowStateChanged() {
+@@ -1495,7 +1495,7 @@ void X11Window::OnXWindowStateChanged() {
    WindowTiledEdges tiled_state = GetTiledState();
    if (tiled_state != tiled_state_) {
      tiled_state_ = tiled_state;
 -#if BUILDFLAG(IS_LINUX)
 +#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
      platform_window_delegate_->OnWindowTiledStateChanged(tiled_state);
+     UpdateDecorationInsets();
  #endif
-   }
