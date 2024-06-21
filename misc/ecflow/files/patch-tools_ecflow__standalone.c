8f72064dae764aa4a0a2b20f8e32434adc15fe10
Yuri Victorovich <yuri@FreeBSD.org>
2024-06-20 18:51:35 -0700

    misc/ecflow: update 5.12.4 → 5.13.0
    Reported by:	portscout

--- misc/ecflow/files/patch-tools_ecflow__standalone.c
+++ misc/ecflow/files/patch-tools_ecflow__standalone.c
@@ -1,11 +1,11 @@
---- tools/ecflow_standalone.c.orig	2022-08-25 05:28:39 UTC
+--- tools/ecflow_standalone.c.orig	2024-06-19 10:20:26 UTC
 +++ tools/ecflow_standalone.c
-@@ -183,7 +183,7 @@ int main(argc,argv) int argc; char **argv;
-      for (n=3; n<65535 ;n++) fclose(n); */
+@@ -85,7 +85,7 @@ pid_t do_setsid(void) {
+ }
  
-   /* create a new session from the child process */
--#if defined(linux) || defined(__APPLE__) || defined(__MACH__) || defined(hpux) || defined(solaris) || defined(SGI) || defined(SVR4) || defined(AIX) || defined(SYG) || defined(alpha)
-+#if defined(linux) || defined(__FreeBSD__) || defined(__APPLE__) || defined(__MACH__) || defined(hpux) || defined(solaris) || defined(SGI) || defined(SVR4) || defined(AIX) || defined(SYG) || defined(alpha)
-   if( setsid() == -1 )
+ pid_t do_setsid(void) {
+-#if defined(linux) || defined(__APPLE__) || defined(__MACH__) || defined(hpux) || defined(solaris) || defined(SGI) || \
++#if defined(linux) || defined(__FreeBSD__) || defined(__APPLE__) || defined(__MACH__) || defined(hpux) || defined(solaris) || defined(SGI) || \
+     defined(SVR4) || defined(AIX) || defined(SYG) || defined(alpha) || defined(__NVCOMPILER)
+     return setsid();
  #else
-   if( setsid(0) == -1 )
