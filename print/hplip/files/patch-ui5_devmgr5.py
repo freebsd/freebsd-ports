993bcea0efa7289c269dc188e0c52706b114eb55
Tijl Coosemans <tijl@FreeBSD.org>
2024-06-21 12:37:12 +0200

    print/hplip(-plugin): Update to 3.24.4

--- print/hplip/files/patch-ui5_devmgr5.py
+++ print/hplip/files/patch-ui5_devmgr5.py
@@ -1,4 +1,4 @@
---- ui5/devmgr5.py.orig	2022-02-23 07:41:05 UTC
+--- ui5/devmgr5.py.orig	2024-06-21 09:49:44 UTC
 +++ ui5/devmgr5.py
 @@ -1073,7 +1073,7 @@ class DevMgr5(Ui_MainWindow_Derived, Ui_MainWindow, QM
                      return
@@ -6,6 +6,6 @@
                  hplip_conf = configparser.ConfigParser()
 -                fp = open("/etc/hp/hplip.conf", "r")
 +                fp = open("/usr/local/etc/hp/hplip.conf", "r")
-                 hplip_conf.readfp(fp)
+                 hplip_conf.read_file(fp)
                  fp.close()
  
