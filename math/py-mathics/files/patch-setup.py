0abdce54fedfa246ae456be2f5c317ca678835c1
Thierry Thomas <thierry@FreeBSD.org>
2024-06-23 16:04:52 +0200

    math/py-mathics: chase the upgrade of numpy

--- math/py-mathics/files/patch-setup.py
+++ math/py-mathics/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2023-07-25 13:04:43 UTC
+--- setup.py.orig	2024-06-23 13:56:36 UTC
 +++ setup.py
-@@ -64,7 +64,7 @@ elif sys.version_info[:2] == (3, 6):
+@@ -65,7 +65,7 @@ else:
          sys.exit(-1)
  else:
      INSTALL_REQUIRES += [
 -        "numpy<1.25",
-+        "numpy<=1.25",
++        "numpy<1.27",
          "llvmlite",
          "sympy>=1.8",
          "pillow >= 9.2",
