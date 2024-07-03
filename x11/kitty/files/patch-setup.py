859d9a5a49a016e897d59ce56d46c0de41af1208
Nicola Vitale <nivit@FreeBSD.org>
2024-07-03 17:51:11 +0200

    x11/kitty: Update to 0.35.2
    - Pet portfmt
    - Add a couple of patches to fix these compilation errors:
    
    1)
    
    glfw/wl_init.c:653:5: error: use of undeclared identifier errno
        errno = ENOSYS;
        ^
    glfw/wl_init.c:653:13: error: use of undeclared identifier ENOSYS
        errno = ENOSYS;
                ^
    glfw/wl_init.c:639:25: error: unused parameter fd [-Werror,-Wunused-parameter]
    get_socket_peer_pid(int fd) {
                            ^
    3 errors generated.
     done
    Compiling [wayland] glfw/wl_init.c ...
    cc -MMD -DNDEBUG -DEPOLL_SHIM_DISABLE_WRAPPER_MACROS -I/usr/local/include/libepoll-shim -D_GLFW_WAYLAND -D_GLFW_BUILD_DLL -Wextra -Wfloat-conversion -Wno-missing-field-initializers -Wall -Wstrict-prototypes -std=c11 -pedantic-errors -Werror -O3 -fwrapv -fstack-pro
    tector-strong -pipe -fvisibility=hidden -fno-plt -fPIC -O2 -pipe -Wno-error=invalid-utf8 -Winvalid-utf8 -fstack-protector-strong -fno-strict-aliasing -D_FORTIFY_SOURCE=2 -flto -I/usr/local/include -fcf-protection=full -fPIC -pthread -I/usr/local/include -I/usr/loc
    al/include/libepoll-shim -I/usr/local/include -I/usr/local/include/libepoll-shim -I/usr/local/include -I/usr/local/include/dbus-1.0 -I/usr/local/lib/dbus-1.0/include -c glfw/wl_init.c -o build/glfw-wayland-glfw-wl_init.c.o
    *** Error code 1
    
    2)
    
    [21/120] Compiling kitty/line.c ...
    glfw/wl_client_side_decorations.c:809:19: error: use of undeclared identifier BTN_LEFT
        if (button == BTN_LEFT) {
                      ^
    glfw/wl_client_side_decorations.c:846:24: error: use of undeclared identifier BTN_RIGHT
        else if (button == BTN_RIGHT) {
                           ^
    2 errors generated.
     done
    Compiling [wayland] glfw/wl_client_side_decorations.c ...
    cc -MMD -DNDEBUG -DEPOLL_SHIM_DISABLE_WRAPPER_MACROS -I/usr/local/include/libepoll-shim -D_GLFW_WAYLAND -D_GLFW_BUILD_DLL -Wextra -Wfloat-conversion -Wno-missing-field-initializers -Wall -Wstrict-prototypes -std=c11 -pedantic-errors -Werror -O3 -fwrapv -fstack-pro
    tector-strong -pipe -fvisibility=hidden -fno-plt -fPIC -O2 -pipe -Wno-error=invalid-utf8 -Winvalid-utf8 -fstack-protector-strong -fno-strict-aliasing -D_FORTIFY_SOURCE=2 -flto -I/usr/local/include -fcf-protection=full -fPIC -pthread -I/usr/local/include -I/usr/loc
    al/include/libepoll-shim -I/usr/local/include -I/usr/local/include/libepoll-shim -I/usr/local/include -I/usr/local/include/dbus-1.0 -I/usr/local/lib/dbus-1.0/include -c glfw/wl_client_side_decorations.c -o build/glfw-wayland-glfw-wl_client_side_decorations.c.o
    *** Error code 1
    
    Release changes:	https://sw.kovidgoyal.net/kitty/changelog/#detailed-list-of-changes
    Reported by:	portscout

--- x11/kitty/files/patch-setup.py
+++ x11/kitty/files/patch-setup.py
@@ -1,35 +1,6 @@
 --- setup.py.orig	1979-11-29 23:00:00 UTC
 +++ setup.py
-@@ -218,25 +218,10 @@ def libcrypto_flags() -> Tuple[List[str], List[str]]:
-     # Apple use their special snowflake TLS libraries and additionally
-     # have an ancient broken system OpenSSL, so we need to check for one
-     # installed by all the various macOS package managers.
--    extra_pc_dir = ''
-+    cflags = [f"-I{sys.prefix}/include"]
-+    libs = [f"-L{sys.prefix}/lib", "-lcrypto"]
- 
--    try:
--        cflags = pkg_config('libcrypto', '--cflags-only-I', fatal=False)
--    except subprocess.CalledProcessError:
--        if is_macos:
--            import ssl
--            v = ssl.OPENSSL_VERSION_INFO
--            pats = f'{v[0]}.{v[1]}', f'{v[0]}'
--            for pat in pats:
--                q = f'opt/openssl@{pat}/lib/pkgconfig'
--                openssl_dirs = glob.glob(f'/opt/homebrew/{q}') + glob.glob(f'/usr/local/{q}')
--                if openssl_dirs:
--                    break
--            if not openssl_dirs:
--                raise SystemExit(f'Failed to find OpenSSL version {v[0]}.{v[1]} on your system')
--            extra_pc_dir = os.pathsep.join(openssl_dirs)
--        cflags = pkg_config('libcrypto', '--cflags-only-I', extra_pc_dir=extra_pc_dir)
--    return cflags, pkg_config('libcrypto', '--libs', extra_pc_dir=extra_pc_dir)
-+    return cflags, libs
- 
- 
- def at_least_version(package: str, major: int, minor: int = 0) -> None:
-@@ -1170,8 +1155,8 @@ def create_linux_bundle_gunk(ddir: str, args: Options)
+@@ -1333,8 +1333,8 @@ def create_linux_bundle_gunk(ddir: str, args: Options)
      base = Path(ddir)
      in_src_launcher = base / (f'{libdir_name}/kitty/kitty/launcher/kitty')
      launcher = base / 'bin/kitty'
--- x11/kitty/files/patch-shell-integration_fish_vendor__conf.d_kitty-shell-integration.fish
+++ x11/kitty/files/patch-shell-integration_fish_vendor__conf.d_kitty-shell-integration.fish
@@ -1,28 +1,11 @@
 --- shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish.orig	1979-11-29 23:00:00 UTC
 +++ shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish
-@@ -112,12 +112,12 @@ function __ksi_schedule --on-event fish_prompt -d "Set
+@@ -126,7 +126,7 @@ function __ksi_schedule --on-event fish_prompt -d "Set
      # for sudo it will be clobbered by us, so only install this if sudo is not already function
      if not contains "no-sudo" $_ksi
          and test -n "$TERMINFO" -a "file" = (type -t sudo 2> /dev/null || echo "x")
--        and not test -r "/usr/share/terminfo/x/xterm-kitty" -o -r "/usr/share/terminfo/78/xterm-kitty" 
+-        and not test -r "/usr/share/terminfo/x/xterm-kitty" -o -r "/usr/share/terminfo/78/xterm-kitty"
 +        and not test -r "%%DATADIR%%/terminfo/x/xterm-kitty" -o -r "/usr/share/terminfo/x/xterm-kitty" -o -r "/usr/share/terminfo/78/xterm-kitty"
          # Ensure terminfo is available in sudo
          function sudo
              set --local is_sudoedit "n"
-             for arg in $argv
--                if string match -q -- "-e" "$arg" or string match -q -- "--edit" "$arg" 
-+                if string match -q -- "-e" "$arg" or string match -q -- "--edit" "$arg"
-                     set is_sudoedit "y"
-                     break
-                 end
-@@ -145,8 +145,8 @@ function __ksi_schedule --on-event fish_prompt -d "Set
-             and return 0
-             return 1
-         end
--        if _ksi_s_is_ok "venv" 
--            and test -n "$VIRTUAL_ENV" -a -r "$venv" 
-+        if _ksi_s_is_ok "venv"
-+            and test -n "$VIRTUAL_ENV" -a -r "$venv"
-             set _ksi_sourced "y"
-             set --erase VIRTUAL_ENV _OLD_FISH_PROMPT_OVERRIDE  # activate.fish stupidly exports _OLD_FISH_PROMPT_OVERRIDE
-             source "$venv"
