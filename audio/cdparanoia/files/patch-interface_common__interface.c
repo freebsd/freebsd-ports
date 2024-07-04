d0b1d9ec73207ca2400394af1db811ddcf3d2759
Tobias Rehbein <tobias.rehbein@web.de>
2024-07-04 06:24:52 +0000

    audio/cdparanoia: the port had been updated to version III-10.2
    - GC no longer useful post-patch target and GNU_CONFIGURE_MANPREFIX
    - Replace stripping in post-install with proper BSD_INSTALL_* calls
    
    PR:	200925

--- audio/cdparanoia/files/patch-interface_common__interface.c
+++ audio/cdparanoia/files/patch-interface_common__interface.c
@@ -3,29 +3,35 @@ Index: interface/common_interface.c
 RCS file: /home/cvs/cdparanoia/interface/common_interface.c,v
 retrieving revision 1.1.1.1
 retrieving revision 1.5
---- interface/common_interface.c.orig	2000-04-19 22:41:04 UTC
+--- interface/common_interface.c.orig	2008-08-21 16:08:54 UTC
 +++ interface/common_interface.c
-@@ -13,12 +13,19 @@
+@@ -13,15 +13,25 @@
  #include "utils.h"
  #include "smallft.h"
  
 +#ifdef Linux
  #include <linux/hdreg.h>
++#elif defined(__FreeBSD__)
++#include <sys/cdio.h>
 +#endif
  
  /* Test for presence of a cdrom by pinging with the 'CDROMVOLREAD' ioctl() */
+ /* Also test using CDROM_GET_CAPABILITY (if available) as some newer DVDROMs will
+    reject CDROMVOLREAD ioctl for god-knows-what reason */
  int ioctl_ping_cdrom(int fd){
 +#ifdef Linux
    struct cdrom_volctrl volctl;
-   if (ioctl(fd, CDROMVOLREAD, &volctl)) 
+   if (ioctl(fd, CDROMVOLREAD, &volctl) &&
+       ioctl(fd, CDROM_GET_CAPABILITY, NULL)<0)
 +#elif defined(__FreeBSD__)
 +  struct ioc_vol volctl;
-+  if (ioctl(fd, CDIOCGETVOL, &volctl))
++  if (ioctl(fd, CDIOCGETVOL, &volctl) &&
++     (ioctl(fd, CDIOCCAPABILITY, NULL)<0))
 +#endif
      return(1); /* failure */
  
    return(0);
-@@ -26,6 +33,7 @@ int ioctl_ping_cdrom(int fd){
+@@ -29,6 +39,7 @@ int ioctl_ping_cdrom(int fd){
  }
  
  
@@ -33,7 +39,7 @@ retrieving revision 1.5
  /* Use the ioctl thingy above ping the cdrom; this will get model info */
  char *atapi_drive_info(int fd){
    /* Work around the fact that the struct grew without warning in
-@@ -46,6 +54,7 @@ char *atapi_drive_info(int fd){
+@@ -49,6 +60,7 @@ char *atapi_drive_info(int fd){
    free(id);
    return(ret);
  }
@@ -41,8 +47,8 @@ retrieving revision 1.5
  
  int data_bigendianp(cdrom_drive *d){
    float lsb_votes=0;
-@@ -171,7 +180,9 @@ int data_bigendianp(cdrom_drive *d){
-    knows the leasoud/leadin size. */
+@@ -174,7 +186,9 @@ int data_bigendianp(cdrom_drive *d){
+    knows the leadout/leadin size. */
  
  int FixupTOC(cdrom_drive *d,int tracks){
 +#ifdef Linux
@@ -51,7 +57,7 @@ retrieving revision 1.5
    int j;
    
    /* First off, make sure the 'starting sector' is >=0 */
-@@ -208,6 +219,8 @@ int FixupTOC(cdrom_drive *d,int tracks){
+@@ -211,6 +225,8 @@ int FixupTOC(cdrom_drive *d,int tracks){
    /* For a scsi device, the ioctl must go to the specialized SCSI
       CDROM device, not the generic device. */
  
@@ -60,10 +66,19 @@ retrieving revision 1.5
    if (d->ioctl_fd != -1) {
      int result;
  
-@@ -231,6 +244,7 @@ int FixupTOC(cdrom_drive *d,int tracks){
+@@ -235,6 +251,16 @@ int FixupTOC(cdrom_drive *d,int tracks){
        return 1;
      }
    }
++#elif defined(__FreeBSD__)
++    for (j = tracks-1; j >= 0; j--) {
++      if (j > 0 && !IS_AUDIO(d,j) && IS_AUDIO(d,j-1)) {
++        if ((d->disc_toc[j].dwStartSector > d->disc_toc[j].dwStartSector - 11400) &&
++            (d->disc_toc[j].dwStartSector - 11400 > d->disc_toc[j-1].dwStartSector))
++          d->disc_toc[j].dwStartSector = d->disc_toc[j].dwStartSector - 11400;
++        break;
++      }
++    }
 +#endif
    return 0;
  }
