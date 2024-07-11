325db00a1989abbdb92103bee3e7efe3f4442c4c
Yuri Victorovich <yuri@FreeBSD.org>
2024-07-10 17:05:25 -0700

    audio/surge-synthesizer-xt-lv2: update 1.0.1 → 1.3.2

--- audio/surge-synthesizer-xt-lv2/files/patch-libs_JUCE_modules_juce__audio__processors_format__types_VST3__SDK_base_source_fstring.cpp
+++ audio/surge-synthesizer-xt-lv2/files/patch-libs_JUCE_modules_juce__audio__processors_format__types_VST3__SDK_base_source_fstring.cpp
@@ -1,4 +1,4 @@
---- libs/JUCE/modules/juce_audio_processors/format_types/VST3_SDK/base/source/fstring.cpp.orig	2022-03-20 14:18:51 UTC
+--- libs/JUCE/modules/juce_audio_processors/format_types/VST3_SDK/base/source/fstring.cpp.orig	2024-07-10 14:20:15 UTC
 +++ libs/JUCE/modules/juce_audio_processors/format_types/VST3_SDK/base/source/fstring.cpp
 @@ -44,6 +44,7 @@
  #include <cctype>
@@ -6,5 +6,5 @@
  #include <cstdarg>
 +#include <cwctype>
  #include <utility>
- 
- #if SMTG_OS_WINDOWS
+ #include <complex>
+ #include <cmath>
