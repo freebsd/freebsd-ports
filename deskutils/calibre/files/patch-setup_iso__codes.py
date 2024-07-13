b10953cbc919768e0dac11508e221adb72ac5a8a
Guido Falsi <madpilot@FreeBSD.org>
2024-07-13 09:18:36 +0200

    deskutils/calibre: Update to 7.14.0

--- deskutils/calibre/files/patch-setup_iso__codes.py
+++ deskutils/calibre/files/patch-setup_iso__codes.py
@@ -1,4 +1,4 @@
---- setup/iso_codes.py.orig	2024-06-28 03:34:16 UTC
+--- setup/iso_codes.py.orig	2024-07-12 04:19:04 UTC
 +++ setup/iso_codes.py
 @@ -2,6 +2,7 @@ import fnmatch
  # License: GPLv3 Copyright: 2023, Kovid Goyal <kovid at kovidgoyal.net>
@@ -8,7 +8,12 @@
  import optparse
  import os
  import shutil
-@@ -20,22 +21,29 @@ class ISOData(Command):
+@@ -24,40 +25,59 @@ class ISOData(Command):
+     description = 'Get ISO codes name localization data'
+     top_level_filename =  'iso-codes-main'
+     _zip_data = None
++    extracted = False
+ 
      def add_options(self, parser):
          with suppress(optparse.OptionConflictError):  # ignore if option already added
              parser.add_option('--path-to-isocodes', help='Path to previously downloaded iso-codes-main.zip')
@@ -16,44 +21,32 @@
 +                    help='Isocodes is a directory')
  
      def run(self, opts):
--        if self._zip_data is None:
--            if opts.path_to_isocodes:
--                with open(opts.path_to_isocodes, 'rb') as f:
--                    self._zip_data = BytesIO(f.read())
--                # get top level directory
--                top = {item.split('/')[0] for item in zipfile.ZipFile(self.zip_data).namelist()}
--                assert len(top) == 1
--                self.top_level = top.pop()
--            else:
--                self._zip_data = BytesIO(download_securely(self.URL))
-+        if (opts.isocodes_extracted):
+-        if self._zip_data is None and opts.path_to_isocodes:
+-            with open(opts.path_to_isocodes, 'rb') as f:
+-                self._zip_data = f.read()
+-            # get top level directory
+-            top = {item.split('/')[0] for item in zipfile.ZipFile(self.zip_data).namelist()}
+-            assert len(top) == 1
+-            self.top_level_filename = top.pop()
++        if opts.isocodes_extracted:
 +            self.top_level = opts.path_to_isocodes
 +            self.extracted = True
 +        else:
-+            if self._zip_data is None:
-+                if opts.path_to_isocodes:
-+                    with open(opts.path_to_isocodes, 'rb') as f:
-+                        self._zip_data = BytesIO(f.read())
-+                    # get top level directory
-+                    top = {item.split('/')[0] for item in zipfile.ZipFile(self.zip_data).namelist()}
-+                    assert len(top) == 1
-+                    self.top_level = top.pop()
-+                else:
-+                    self._zip_data = BytesIO(download_securely(self.URL))
- 
-     def __init__(self):
-         super().__init__()
-         self._zip_data = None
-+        self.extracted = False
-         self.top_level = 'iso-codes-main'
++            if self._zip_data is None and opts.path_to_isocodes:
++                with open(opts.path_to_isocodes, 'rb') as f:
++                    self._zip_data = f.read()
++                # get top level directory
++                top = {item.split('/')[0] for item in zipfile.ZipFile(self.zip_data).namelist()}
++                assert len(top) == 1
++                self.top_level_filename = top.pop()
  
      @property
-@@ -43,23 +51,35 @@ class ISOData(Command):
-         return self._zip_data
+     def zip_data(self):
+         return self._zip_data or iso_codes_data()
  
      def db_data(self, name: str) -> bytes:
--        with zipfile.ZipFile(self.zip_data) as zf:
--            with zf.open(f'{self.top_level}/data/{name}') as f:
+-        with zipfile.ZipFile(BytesIO(self.zip_data)) as zf:
+-            with zf.open(f'{self.top_level_filename}/data/{name}') as f:
 +        if self.extracted:
 +            src = f'{self.top_level}/data/{name}'
 +            if not os.path.exists(src):
@@ -61,16 +54,14 @@
 +            with open(src, 'rb') as f:
                  return f.read()
 +        else:
-+            with zipfile.ZipFile(self.zip_data) as zf:
-+                with zf.open(f'{self.top_level}/data/{name}') as f:
++            with zipfile.ZipFile(BytesIO(self.zip_data)) as zf:
++                with zf.open(f'{self.top_level_filename}/data/{name}') as f:
 +                    return f.read()
  
      def extract_po_files(self, name: str, output_dir: str) -> None:
          name = name.split('.', 1)[0]
-         pat = f'{self.top_level}/{name}/*.po'
--        if self.zip_data is None:
--            self._zip_data = BytesIO(download_securely(self.URL))
--        with zipfile.ZipFile(self.zip_data) as zf:
+         pat = f'{self.top_level_filename}/{name}/*.po'
+-        with zipfile.ZipFile(BytesIO(self.zip_data)) as zf:
 -            for name in fnmatch.filter(zf.namelist(), pat):
 +        if self.extracted:
 +            for name in glob.glob(pat):
@@ -82,9 +73,7 @@
 -                os.utime(dest, (date_time, date_time))
 +                shutil.copy2(name, dest)
 +        else:
-+            if self.zip_data is None:
-+                self._zip_data = BytesIO(download_securely(self.URL))
-+            with zipfile.ZipFile(self.zip_data) as zf:
++            with zipfile.ZipFile(BytesIO(self.zip_data)) as zf:
 +                for name in fnmatch.filter(zf.namelist(), pat):
 +                    dest = os.path.join(output_dir, name.split('/')[-1])
 +                    zi = zf.getinfo(name)
