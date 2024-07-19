9a39387e4e721817a9060d10a00a22c331d72b83
Hiroki Tagato <tagattie@FreeBSD.org>
2024-07-19 20:17:31 +0900

    devel/electron29: update to 29.4.5
    Changelog: https://github.com/electron/electron/releases/tag/v29.4.5
    
    Reported by:	GitHub (watch releases)
    MFH:		2024Q3
    Security:	574028b4-a181-455b-a78b-ec5c62781235

--- devel/electron29/files/patch-electron_spec_webview-spec.ts
+++ devel/electron29/files/patch-electron_spec_webview-spec.ts
@@ -1,4 +1,4 @@
---- electron/spec/webview-spec.ts.orig	2024-02-14 18:35:26 UTC
+--- electron/spec/webview-spec.ts.orig	2024-07-15 16:16:34 UTC
 +++ electron/spec/webview-spec.ts
 @@ -555,7 +555,7 @@ describe('<webview> tag', function () {
      });
@@ -9,3 +9,12 @@
        const [w, webview] = await loadWebViewWindow();
        const enterFullScreen = once(w, 'enter-full-screen');
        await webview.executeJavaScript('document.getElementById("div").requestFullscreen()', true);
+@@ -1989,7 +1989,7 @@ describe('<webview> tag', function () {
+ 
+     ifdescribe(features.isPrintingEnabled())('<webview>.printToPDF()', () => {
+       // FIXME: This test is flaky on Linux x64 only in 29-x-y, skip it there.
+-      ifit(process.platform !== 'linux' && process.arch !== 'x64')('rejects on incorrectly typed parameters', async () => {
++      ifit((process.platform !== 'linux' && process.platform !== 'freebsd') && process.arch !== 'x64')('rejects on incorrectly typed parameters', async () => {
+         const badTypes = {
+           landscape: [],
+           displayHeaderFooter: '123',
