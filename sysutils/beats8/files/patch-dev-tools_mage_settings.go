a26ef116788796fbc4d601d5990456b35d7e5638
Oleksii Samorukov <samm@FreeBSD.org>
2024-07-21 14:10:12 +0200

    sysutils/beats8: upgrade to 8.14.3
    - fix beats by integrating patch from the https://github.com/elastic/go-sysinfo/pull/126
    - upgrade to the latest version
    
    ChangeLog: https://www.elastic.co/guide/en/beats/libbeat/current/release-notes.html
    Approved by: otis (elastic)
    PR: 272701

--- sysutils/beats8/files/patch-dev-tools_mage_settings.go
+++ sysutils/beats8/files/patch-dev-tools_mage_settings.go
@@ -1,20 +1,20 @@
---- dev-tools/mage/settings.go.orig	2022-09-27 13:19:27 UTC
-+++ dev-tools/mage/settings.go
-@@ -30,7 +30,6 @@ import (
+--- dev-tools/mage/settings.go.orig	2024-07-09 00:00:36.000000000 +0200
++++ dev-tools/mage/settings.go	2024-07-13 21:26:19.383768000 +0200
+@@ -31,7 +31,6 @@
  	"sync"
  	"time"
  
 -	"github.com/magefile/mage/sh"
- 	"github.com/pkg/errors"
  	"golang.org/x/tools/go/vcs"
  
-@@ -249,7 +248,8 @@ var (
+ 	"github.com/elastic/beats/v7/dev-tools/mage/gotool"
+@@ -251,7 +250,8 @@
  func CommitHash() (string, error) {
  	var err error
  	commitHashOnce.Do(func() {
 -		commitHash, err = sh.Output("git", "rev-parse", "HEAD")
 +		err = nil
-+		commitHash = "%%BEATSVERSION%%"
++		commitHash = "v8.14.3"
  	})
  	return commitHash, err
  }
--- sysutils/beats8/files/patch-libbeat_autodiscover_providers_docker_config.go
+++ sysutils/beats8/files/patch-libbeat_autodiscover_providers_docker_config.go
@@ -1,13 +1,11 @@
---- libbeat/autodiscover/providers/docker/config.go.orig	2022-10-24 06:49:31 UTC
-+++ libbeat/autodiscover/providers/docker/config.go
-@@ -15,8 +15,8 @@
+--- libbeat/autodiscover/providers/docker/config.go.orig	2024-07-09 00:00:36.000000000 +0200
++++ libbeat/autodiscover/providers/docker/config.go	2024-07-13 21:38:12.739090000 +0200
+@@ -15,7 +15,7 @@
  // specific language governing permissions and limitations
  // under the License.
  
 -//go:build linux || darwin || windows
--// +build linux darwin windows
 +//go:build linux || darwin || windows || freebsd
-+// +build linux darwin windows freebsd
  
  package docker
  
