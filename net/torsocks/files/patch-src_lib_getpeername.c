97e12aa04d5dc00271ae925f01a4ec0432efb660
Yuri Victorovich <yuri@FreeBSD.org>
2024-07-27 13:14:51 -0700

    net/torsocks: Fix build failure on 15
    PR:		280467
    Reported by:	Dan Kotowski <dan.kotowski@a9development.com>

--- net/torsocks/files/patch-src_lib_getpeername.c
+++ net/torsocks/files/patch-src_lib_getpeername.c
@@ -1,16 +1,16 @@
 - workaround for https://gitlab.torproject.org/tpo/core/torsocks/-/issues/40023
 
---- src/lib/getpeername.c.orig	2024-07-25 16:31:28 UTC
+--- src/lib/getpeername.c.orig	2022-05-25 14:36:05 UTC
 +++ src/lib/getpeername.c
-@@ -22,6 +22,8 @@
- 
- #include "torsocks.h"
+@@ -92,6 +92,8 @@ libc:
+ 	return tsocks_libc_getpeername(LIBC_GETPEERNAME_ARGS);
+ }
  
 +#if __FreeBSD_cc_version < 1500000
 +
- /* getpeername(2) */
- TSOCKS_LIBC_DECL(getpeername, LIBC_GETPEERNAME_RET_TYPE,
- 		LIBC_GETPEERNAME_SIG)
+ /*
+  * Libc hijacked symbol getpeername(2).
+  */
 @@ -105,3 +107,5 @@ LIBC_GETPEERNAME_DECL
  
  	return tsocks_getpeername(LIBC_GETPEERNAME_ARGS);
