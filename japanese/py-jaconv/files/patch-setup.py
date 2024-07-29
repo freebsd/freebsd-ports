6cbf4dea3e6c0c28f5636aaff33d75068cf8316b
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-07-29 20:39:53 +0800

    japanese/py-jaconv: Update to 0.4.0
    Changes:	https://github.com/ikegami-yukino/jaconv/blob/master/CHANGES.rst

--- japanese/py-jaconv/files/patch-setup.py
+++ japanese/py-jaconv/files/patch-setup.py
@@ -1,11 +1,10 @@
---- setup.py.orig	2023-02-17 17:05:21 UTC
+--- setup.py.orig	2024-07-25 16:31:04 UTC
 +++ setup.py
-@@ -39,7 +39,7 @@ setup(name='jaconv',
-           'Programming Language :: Python :: 3.10',
-           'Programming Language :: Python :: 3.11', 'Topic :: Text Processing'
+@@ -43,7 +43,6 @@ setup(name='jaconv',
+           'Programming Language :: Python :: 3.11',
+           'Programming Language :: Python :: 3.12', 'Topic :: Text Processing'
        ],
 -      data_files=[('', ['README.rst', 'CHANGES.rst'])],
-+      package_data={'': ['README.rst', 'CHANGES.rst']},
        long_description='%s\n\n%s' %
        (open('README.rst', encoding='utf8').read(),
         open('CHANGES.rst', encoding='utf8').read()),
