bb8152d52b377dbcbdae8064152de1f176f071ad
Yuri Victorovich <yuri@FreeBSD.org>
2024-08-01 21:23:02 -0700

    textproc/py-duecredit: update 0.9.3 → 0.10.2
    Reported by:	portscout

--- textproc/py-duecredit/files/patch-setup.py
+++ textproc/py-duecredit/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2018-05-14 00:01:07 UTC
+--- setup.py.orig	2024-06-04 20:51:40 UTC
 +++ setup.py
-@@ -39,7 +39,7 @@ try:
-                                   list(filter(lambda x: x.startswith(' -- '), lines))[0].rstrip())
+@@ -54,7 +54,7 @@ try:
      else:
-         print('Attempting to get version number from git...')
--        git = Popen(['git', 'describe', '--abbrev=4', '--dirty'],
-+        git = Popen(['false', 'describe', '--abbrev=4', '--dirty'],
-                     stdout=PIPE, stderr=sys.stderr)
+         print("Attempting to get version number from git...")
+         git = Popen(
+-            ["git", "describe", "--abbrev=4", "--dirty"], stdout=PIPE, stderr=sys.stderr
++            ["false", "describe", "--abbrev=4", "--dirty"], stdout=PIPE, stderr=sys.stderr
+         )
          if git.wait() != 0:
              raise OSError
