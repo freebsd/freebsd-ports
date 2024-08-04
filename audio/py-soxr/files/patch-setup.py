38924beeeac345436dc8419ca0af435df0f4924c
Yuri Victorovich <yuri@FreeBSD.org>
2024-08-04 15:28:18 -0700

    audio/py-soxr: Fix build by removing -Werror
    Reported by:	fallout

--- audio/py-soxr/files/patch-setup.py
+++ audio/py-soxr/files/patch-setup.py
@@ -1,8 +1,8 @@
 - workaround for https://github.com/dofuuz/python-soxr/issues/25
 
---- setup.py.orig	2024-01-24 02:08:37 UTC
+--- setup.py.orig	2024-07-25 13:58:38 UTC
 +++ setup.py
-@@ -122,7 +122,8 @@ if '-arm' in platform:
+@@ -122,12 +122,13 @@ if '-arm' in platform:
  
  platform = sysconfig.get_platform()
  if '-arm' in platform:
@@ -12,3 +12,9 @@
  elif '-i686' in platform:
      compile_args.append('-msse')
  
+ if get_default_compiler() in ['unix', 'mingw32']:
+-    compile_args += ['-std=gnu99', '-Werror=implicit']
++    compile_args += ['-std=gnu99']
+ 
+ extensions = [
+     CySoxrExtension(
