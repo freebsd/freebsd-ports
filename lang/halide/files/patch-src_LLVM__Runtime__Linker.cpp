6f10bf5387af83c8650040179fe7f7cd214c8b85
Yuri Victorovich <yuri@FreeBSD.org>
2024-08-07 15:33:33 -0700

    lang/halide: update 17.0.1 → 18.0.0

--- lang/halide/files/patch-src_LLVM__Runtime__Linker.cpp
+++ lang/halide/files/patch-src_LLVM__Runtime__Linker.cpp
@@ -1,16 +1,16 @@
---- src/LLVM_Runtime_Linker.cpp.orig	2024-02-20 17:13:06 UTC
+--- src/LLVM_Runtime_Linker.cpp.orig	2024-07-17 16:16:39 UTC
 +++ src/LLVM_Runtime_Linker.cpp
-@@ -432,6 +432,9 @@ llvm::Triple get_triple_for_target(const Target &targe
+@@ -481,6 +481,9 @@ llvm::Triple get_triple_for_target(const Target &targe
          if (target.os == Target::Linux) {
              triple.setOS(llvm::Triple::Linux);
              triple.setEnvironment(llvm::Triple::GNU);
-+	} else if (target.os == Target::Linux || target.os == Target::FreeBSD) {
++	} else if (target.os == Target::FreeBSD) {
 +            triple.setOS(llvm::Triple::FreeBSD);
 +            triple.setEnvironment(llvm::Triple::GNU);
          } else if (target.os == Target::OSX) {
              triple.setVendor(llvm::Triple::Apple);
              triple.setOS(llvm::Triple::MacOSX);
-@@ -478,6 +481,9 @@ llvm::Triple get_triple_for_target(const Target &targe
+@@ -527,6 +530,9 @@ llvm::Triple get_triple_for_target(const Target &targe
          } else if (target.os == Target::Linux) {
              triple.setOS(llvm::Triple::Linux);
              triple.setEnvironment(llvm::Triple::GNUEABIHF);
@@ -20,7 +20,7 @@
          } else if (target.os == Target::Windows) {
              user_assert(target.bits == 64) << "Windows ARM targets must be 64-bit.\n";
              triple.setVendor(llvm::Triple::PC);
-@@ -539,6 +545,8 @@ llvm::Triple get_triple_for_target(const Target &targe
+@@ -588,6 +594,8 @@ llvm::Triple get_triple_for_target(const Target &targe
  
          if (target.os == Target::Linux) {
              triple.setOS(llvm::Triple::Linux);
@@ -29,7 +29,7 @@
          } else if (target.os == Target::Android) {
              triple.setOS(llvm::Triple::Linux);
              triple.setEnvironment(llvm::Triple::Android);
-@@ -855,7 +863,7 @@ std::unique_ptr<llvm::Module> get_initial_module_for_t
+@@ -923,7 +931,7 @@ std::unique_ptr<llvm::Module> get_initial_module_for_t
      if (module_type != ModuleGPU) {
          if (module_type != ModuleJITInlined && module_type != ModuleAOTNoRuntime) {
              // OS-dependent modules
@@ -38,7 +38,7 @@
                  add_allocator();
                  modules.push_back(get_initmod_posix_allocator(c, bits_64, debug));
                  modules.push_back(get_initmod_posix_error_handler(c, bits_64, debug));
-@@ -1045,7 +1053,7 @@ std::unique_ptr<llvm::Module> get_initial_module_for_t
+@@ -1113,7 +1121,7 @@ std::unique_ptr<llvm::Module> get_initial_module_for_t
              }
  
              // Prefer using fopen_lfs on Linux systems, which calls fopen64() to ensure LFS support.
@@ -47,12 +47,3 @@
                  modules.push_back(get_initmod_fopen_lfs(c, bits_64, debug));
              } else {
                  modules.push_back(get_initmod_fopen(c, bits_64, debug));
-@@ -1209,7 +1217,7 @@ std::unique_ptr<llvm::Module> get_initial_module_for_t
-             if (t.os == Target::Android) {
-                 // Only platform that supports OpenGL Compute for now.
-                 modules.push_back(get_initmod_opengl_egl_context(c, bits_64, debug));
--            } else if (t.os == Target::Linux) {
-+            } else if (t.os == Target::Linux || t.os == Target::FreeBSD) {
-                 if (t.has_feature(Target::EGL)) {
-                     modules.push_back(get_initmod_opengl_egl_context(c, bits_64, debug));
-                 } else {
