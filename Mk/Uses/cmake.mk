76b8901dae25efc80678bb950bdb34a780a39479
Jason E. Hale <jhale@FreeBSD.org>
2024-08-13 04:47:30 -0400

    devel/cmake-core: Refactor
    Move the update reminder to Uses/Mk/cmake.mk where the version has
    been changed since after 7bdeb4414c22 and will be more likely noticed.
    
    Switch to an out-of-source build. This eliminates the need to remove
    certain unwanted files prior to install.
    
    Define a sensible CONFIGURE_LOG for users to report as the default
    doesn't exist.
    
    Sort CONFIGURE_ARGS, not necessarily alphabetically, but to improve
    readability.
    
    Pet portclippy(1): hoist the post-patch target.
    
    Settle on one sed(1) delimiter instead of three. Pipes (|) seems to
    be the most readable here.
    
    Remove cruft from ${FILESDIR}/InitialCache.cmake and convert to SUB_FILES
    to take advantage of framework substitutions. archivers/lzmalib is gone,
    so there is no need to patch for that. Remove the CPACK_OPTION_COMMENT
    variable in favor of CMake logic.

--- Mk/Uses/cmake.mk
+++ Mk/Uses/cmake.mk
@@ -62,6 +62,8 @@ _INCLUDE_USES_CMAKE_MK=	yes
 
 _valid_ARGS=		indirect insource noninja run testing _internal
 
+# Reminder: devel/cmake-core, devel/cmake-doc, devel/cmake-gui, and devel/cmake-man
+# are all affected by changing _CMAKE_VERSION. Please check each of these ports.
 _CMAKE_VERSION=		3.30.2
 CMAKE_BIN=		${LOCALBASE}/bin/cmake
 
