c174cd15a902265d9387b99e32d53f574f5be571
Mikhail Teterin <mi@FreeBSD.org>
2024-08-15 18:54:28 +0200

    www/tdom: update to 0.9.4
    Use post-patch-EBASE-on instead of .if conditional.
    Switch to DISTVERSION.
    Pet portlint, portfmt.
    
    Changelog: http://tdom.org/index.html/file?name=CHANGES&ci=trunk
    
    PR:		280832
    Approved by:	submitter is maintainer

--- www/tdom/files/patch-generic-dom.h
+++ www/tdom/files/patch-generic-dom.h
@@ -1,10 +1,11 @@
---- generic/dom.h.orig	2018-09-24 12:17:05.706742000 +0000
-+++ generic/dom.h	2018-09-24 12:17:16.003866000 +0000
-@@ -41,7 +41,6 @@
+--- generic/dom.h	2018-09-24 12:17:05.706742000 +0000
++++ generic/dom.h	2024-08-15 12:03:29.000000000 -0400
+@@ -41,8 +41,7 @@
  #include <string.h>
  #include <ctype.h>
  #include <expat.h>
 -#include <domalloc.h>
+ #include <limits.h>
  
  /*
   * tDOM provides it's own memory allocator which is optimized for
