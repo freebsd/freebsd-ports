a34cf9c2dbf503c8248371ba3bab24f34d2d045d
Bernard Spil <brnrd@FreeBSD.org>
2024-08-18 14:21:26 +0200

    databases/mariadb106-server: Update to 10.6.19
    * Fix a ports LibreSSL issue [1]
    
    PR:		274494 [1]
    Reported by:	Ivan Rozhuk <rozhuk im gmail com>

--- databases/mariadb106-server/patch-mysys__ssl_openssl.c
+++ databases/mariadb106-server/patch-mysys__ssl_openssl.c
@@ -1,17 +1,5 @@
---- mysys_ssl/openssl.c.orig	2024-02-01 17:44:37 UTC
+--- mysys_ssl/openssl.c
 +++ mysys_ssl/openssl.c
-@@ -30,6 +30,11 @@ int check_openssl_compatibility()
- {
-   return 0;
- }
-+#elif defined(LIBRESSL_VERSION_NUMBER)
-+int check_openssl_compatibility()
-+{
-+  return 0;
-+}
- #else
- #include <openssl/evp.h>
- 
 @@ -36,8 +36,12 @@
  static uint testing;
  static size_t alloc_size, alloc_count;
