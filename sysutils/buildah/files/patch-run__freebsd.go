551dc594fa51779c7773129fdc8f97dc9aaa81c0
Doug Rabson <dfr@FreeBSD.org>
2024-08-19 14:04:46 +0100

    sysutils/builldah: fix a nil pointer dereference
    Attempting to use 'buildah run --network=host' caused a nil pointer
    dereference while setting up the container's resolv.conf file.
    
    PR:	280777

---
+++ sysutils/buildah/files/patch-run__freebsd.go
@@ -0,0 +1,11 @@
+--- run_freebsd.go.orig	2024-08-18 10:59:26 UTC
++++ run_freebsd.go
+@@ -244,7 +244,7 @@ func (b *Builder) Run(command []string, options RunOpt
+ 		// Only add entries here if we do not have to do setup network,
+ 		// if we do we have to do it much later after the network setup.
+ 		if !configureNetwork {
+-			err = b.addResolvConfEntries(resolvFile, nil, nil, false, true)
++			err = b.addResolvConfEntries(resolvFile, nil, spec, false, true)
+ 			if err != nil {
+ 				return err
+ 			}
