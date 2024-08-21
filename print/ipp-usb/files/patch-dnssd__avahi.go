0970fbd3aa145467b75331aca690fa6e3639f961
Foo Chuan Wei <chuanwei.foo@hotmail.com>
2024-08-21 20:09:32 +0200

    print/ipp-usb: Update to 0.9.24
    PR:		276546
    Approved by:	submitter is maintainer

--- print/ipp-usb/files/patch-dnssd__avahi.go
+++
@@ -1,8 +0,0 @@
---- dnssd_avahi.go.orig	2023-05-01 00:00:00 UTC
-+++ dnssd_avahi.go
-@@ -1,4 +1,4 @@
--// +build linux
-+// +build linux freebsd
- 
- /* ipp-usb - HTTP reverse proxy, backed by IPP-over-USB connection to device
-  *
