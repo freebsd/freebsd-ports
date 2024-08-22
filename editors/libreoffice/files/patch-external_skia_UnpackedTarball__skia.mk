d8273ae2227f99cf4304e750f0d40c0d1da97925
Dima Panov <fluffy@FreeBSD.org>
2024-08-23 00:18:34 +0300

    editors/libreoffice: update to 24.8.0 release (+)
    New major release!
    
    LibreOffice 24.8, the new major release of the free, volunteer-supported office suite
    has been released with a wealth of improvements:
    
    * Handy "Quick find" deck in the Sidebar
    * New spreadsheet functions, including XLOOKUP
    * Better presentation templates
    * New password-based encryption for documents
    
    and much more:	https://blog.documentfoundation.org/blog/2024/08/22/libreoffice-248/
    
    Release notes:	https://wiki.documentfoundation.org/ReleaseNotes/24.8
    
    Co-Authored-by:	Li-Wen Hsu <lwhsu@FreeBSD.org>
    
    Sponsored by:	Serenity Cybersecurity, LLC

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,9 @@
---- external/skia/UnpackedTarball_skia.mk.orig	2023-12-08 15:11:48 UTC
+--- external/skia/UnpackedTarball_skia.mk.orig	2024-06-11 10:45:33 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -41,6 +41,7 @@ skia_patches := \
-     ubsan-missing-typeinfo.patch.1 \
-     incomplete-type-SkImageGenerator.patch.1 \
+@@ -43,6 +43,7 @@ skia_patches := \
      0001-AvoidCombiningExtrememelyLargeMeshes.patch.1 \
+     sort-comparison-assumption.patch.0 \
+     help-msvc-analyzer.patch \
 +    powerpc64le-skia.patch.0 \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
