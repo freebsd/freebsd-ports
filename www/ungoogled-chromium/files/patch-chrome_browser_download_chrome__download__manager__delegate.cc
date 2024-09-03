f3d4e5ce9571185d2e386dd37f5df90ab83c60ca
Robert Nagy <rnagy@FreeBSD.org>
2024-09-03 22:47:50 +0200

    www/*chromium: update to 128.0.6613.119
    remove test_fonts distfile as it is now bundled in the main tarball
    
    Security:	https://vuxml.freebsd.org/freebsd/26125e09-69ca-11ef-8a0f-a8a1599412c6.html

--- www/ungoogled-chromium/files/patch-chrome_browser_download_chrome__download__manager__delegate.cc
+++ www/ungoogled-chromium/files/patch-chrome_browser_download_chrome__download__manager__delegate.cc
@@ -1,4 +1,4 @@
---- chrome/browser/download/chrome_download_manager_delegate.cc.orig	2024-08-26 14:40:28 UTC
+--- chrome/browser/download/chrome_download_manager_delegate.cc.orig	2024-09-03 20:46:48 UTC
 +++ chrome/browser/download/chrome_download_manager_delegate.cc
 @@ -1724,7 +1724,7 @@ void ChromeDownloadManagerDelegate::OnDownloadTargetDe
  bool ChromeDownloadManagerDelegate::IsOpenInBrowserPreferredForFile(
@@ -9,7 +9,7 @@
    if (path.MatchesExtension(FILE_PATH_LITERAL(".pdf"))) {
      return !download_prefs_->ShouldOpenPdfInSystemReader();
    }
-@@ -1825,7 +1825,7 @@ void ChromeDownloadManagerDelegate::CheckDownloadAllow
+@@ -1843,7 +1843,7 @@ void ChromeDownloadManagerDelegate::CheckDownloadAllow
      content::CheckDownloadAllowedCallback check_download_allowed_cb) {
    DCHECK_CURRENTLY_ON(BrowserThread::UI);
  #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || \
--- www/ungoogled-chromium/files/patch-v8_src_execution_isolate.cc
+++ www/ungoogled-chromium/files/patch-v8_src_execution_isolate.cc
@@ -1,4 +1,4 @@
---- v8/src/execution/isolate.cc.orig	2024-08-26 14:40:28 UTC
+--- v8/src/execution/isolate.cc.orig	2024-09-03 20:46:48 UTC
 +++ v8/src/execution/isolate.cc
 @@ -159,6 +159,10 @@
  #include "src/execution/simulator-base.h"
@@ -11,7 +11,7 @@
  extern "C" const uint8_t v8_Default_embedded_blob_code_[];
  extern "C" uint32_t v8_Default_embedded_blob_code_size_;
  extern "C" const uint8_t v8_Default_embedded_blob_data_[];
-@@ -4785,6 +4789,13 @@ void Isolate::InitializeDefaultEmbeddedBlob() {
+@@ -4803,6 +4807,13 @@ void Isolate::InitializeDefaultEmbeddedBlob() {
    uint32_t code_size = DefaultEmbeddedBlobCodeSize();
    const uint8_t* data = DefaultEmbeddedBlobData();
    uint32_t data_size = DefaultEmbeddedBlobDataSize();
