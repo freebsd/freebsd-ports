3154506a168d8188fe06f252e070b394b6581db3
Tijl Coosemans <tijl@FreeBSD.org>
2024-09-08 00:06:27 +0200

    Mk/bsd.port.mk: Add MACHINE_ARCH to WRK_ENV
    make(1) has a built-in variable MACHINE_ARCH that defaults to the value
    of sysctl hw.machine_arch.  This is not correct when building packages
    under qemu.  In that case poudriere sets MACHINE_ARCH in its make.conf.
    Since commit d7511b9d0074 make.conf is not picked up by make during
    build so pass MACHINE_ARCH through the environment.
    
    This affects building kmod ports under qemu.
    
    This is a workaround for make not looking at UNAME_p when setting
    MACHINE_ARCH.  It does look at UNAME_m when setting MACHINE.
    
    PR:		281674
    Reported by:	jrtc27
    Exp-run by:	antoine
    Approved by:	portmgr (antoine)

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1616,6 +1616,7 @@ PKG_NOTE_flavor=	${FLAVOR}
 .    endif
 
 WRK_ENV+=		HOME=${WRKDIR} \
+				MACHINE_ARCH=${MACHINE_ARCH} \
 				PWD="$${PWD}" \
 				__MAKE_CONF=${NONEXISTENT}
 .    for e in OSVERSION PATH TERM TMPDIR \
