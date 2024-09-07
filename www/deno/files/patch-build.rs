e5be14aead14a9117e49c0eed0eb11b2dbfb7a4d
Mikael Urankar <mikael@FreeBSD.org>
2024-09-07 11:16:59 +0200

    www/deno: Update to 1.46.2

--- www/deno/files/patch-build.rs
+++ www/deno/files/patch-build.rs
@@ -1,5 +1,5 @@
---- cargo-crates/v8-0.97.0/build.rs.orig	2020-07-22 09:53:02 UTC
-+++ cargo-crates/v8-0.97.0/build.rs
+--- cargo-crates/v8-0.105.0/build.rs.orig	2020-07-22 09:53:02 UTC
++++ cargo-crates/v8-0.105.0/build.rs
 @@ -199,10 +199,10 @@ fn build_v8(is_asan: bool) {
    }
    // cross-compilation setup
---
+++ www/deno/files/patch-build_gn__run__binary.py
@@ -0,0 +1,11 @@
+--- cargo-crates/v8-0.105.0/build/gn_run_binary.py.orig	2020-06-26 16:27:54 UTC
++++ cargo-crates/v8-0.105.0/build/gn_run_binary.py
+@@ -23,7 +23,7 @@ if not os.path.isabs(path):
+ # The rest of the arguments are passed directly to the executable.
+ args = [path] + sys.argv[2:]
+ 
+-ret = subprocess.call(args)
++ret = subprocess.call(args, env={"CHROME_EXE_PATH":"${WRKSRC}/out/Release/chrome","LD_LIBRARY_PATH":"${WRKSRC}/out/Release"})
+ if ret != 0:
+   if ret <= -100:
+     # Windows error codes such as 0xC0000005 and 0xC0000409 are much easier to
