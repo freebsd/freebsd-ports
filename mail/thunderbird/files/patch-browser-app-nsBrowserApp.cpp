989ed863e49cb37b60f91fbd346b9cc4aa49401f
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2024-09-09 23:27:43 +0200

    mail/thunderbird: update to 128.2.0
    Release Notes:
      https://www.thunderbird.net/en-US/thunderbird/128.2.0esr/releasenotes/

--- mail/thunderbird/files/patch-browser-app-nsBrowserApp.cpp
+++ mail/thunderbird/files/patch-browser-app-nsBrowserApp.cpp
@@ -1,12 +1,14 @@
---- browser/app/nsBrowserApp.cpp.orig	2016-09-19 16:19:28 UTC
+diff --git browser/app/nsBrowserApp.cpp browser/app/nsBrowserApp.cpp
+index e1f11b9cfd..a15b129402 100644
+--- browser/app/nsBrowserApp.cpp
 +++ browser/app/nsBrowserApp.cpp
-@@ -324,6 +324,9 @@ int main(int argc, char* argv[], char* e
- {
-   mozilla::TimeStamp start = mozilla::TimeStamp::Now();
+@@ -341,6 +341,9 @@ int main(int argc, char* argv[], char* envp[]) {
+     }
+ #  endif
  
-+  setenv("MOZ_GMP_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
-+  setenv("MOZ_PLUGIN_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
++    setenv("MOZ_GMP_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
++    setenv("MOZ_PLUGIN_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
 +
- #ifdef HAS_DLL_BLOCKLIST
-   DllBlocklist_Initialize();
- 
+ #  ifdef HAS_DLL_BLOCKLIST
+     uint32_t initFlags =
+         gBlocklistInitFlags | eDllBlocklistInitFlagIsChildProcess;
