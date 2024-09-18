bcfcbefc31bf5205701ab75d02072271ccd6ba9d
Alexey Dokuchaev <danfe@FreeBSD.org>
2024-09-18 16:37:38 +0000

    misc/mc: update Midnight Commander to version 4.8.32
    PR:		281057
    Reported by:	portscout

--- misc/mc/files/extra-patch-src_diffviewer_ydiff.c
+++ misc/mc/files/extra-patch-src_diffviewer_ydiff.c
@@ -1,9 +1,9 @@
---- src/diffviewer/ydiff.c.orig	2017-03-04 20:51:38 UTC
+--- src/diffviewer/ydiff.c.orig	2024-08-08 07:49:18 UTC
 +++ src/diffviewer/ydiff.c
-@@ -816,7 +816,7 @@ dff_execute (const char *args, const char *extra, cons
+@@ -821,7 +821,7 @@ dff_execute (const char *args, const char *extra, cons
      /* escape potential $ to avoid shell variable substitutions in popen() */
-     file1_esc = strutils_shell_escape (file1);
-     file2_esc = strutils_shell_escape (file2);
+     file1_esc = str_shell_escape (file1);
+     file2_esc = str_shell_escape (file2);
 -    cmd = g_strdup_printf ("diff %s %s %s %s %s", args, extra, opt, file1_esc, file2_esc);
 +    cmd = g_strdup_printf ("gdiff %s %s %s %s %s", args, extra, opt, file1_esc, file2_esc);
      g_free (file1_esc);
