a74ad6ba3a21fdcc34c50cb599e8086f08d2b671
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-09-18 13:56:16 +0800

    deskutils/py-vdirsyncer: Update to 0.19.3
    - Update WWW
    - Convert to USE_PYTHON=pep517
    - Update pkg-descr
    - Take maintainership
    
    Changes:	https://github.com/pimutils/vdirsyncer/blob/main/CHANGELOG.rst

---
+++ deskutils/py-vdirsyncer/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2024-09-11 15:26:58 UTC
++++ setup.py
+@@ -19,7 +19,7 @@ requirements = [
+     # https://github.com/untitaker/python-atomicwrites/commit/4d12f23227b6a944ab1d99c507a69fdbc7c9ed6d  # noqa
+     "atomicwrites>=0.1.7",
+     "aiohttp>=3.8.0,<4.0.0",
+-    "aiostream>=0.4.3,<0.5.0",
++    "aiostream>=0.4.3",
+ ]
+ 
+ 
