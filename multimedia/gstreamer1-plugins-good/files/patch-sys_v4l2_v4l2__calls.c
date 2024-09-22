8c1efc578d6f6e5fc6371bf22d3983675f636b44
Jason E. Hale <jhale@FreeBSD.org>
2024-09-22 04:31:12 -0400

    multimedia/gstreamer1: Update to 1.24.8
    Mk/Uses/gstreamer.mk:
    - Sort, fix whitespace issues and typos
    - Promote several common variables to improve maintainer QOL
    *   There's now no need to set manually set PORTVERSION nor
        SOVERSION in several Makefiles
    *   This will help to prevent not updating some components
    *   A note has been added, however, to remind maintainers
        to remake distinfo for certain ports
    - Improve documentation
    
    multimedia/gstreamer1-plugins-webrtc:
    - New port split from multimedia/gstreamer1-plugins-bad
    - multimedia/gstreamer1-plugins-rust has been updated
      to reflect this change
    
    net/gstreamer1-plugins-sctp:
    - New port split from multimedia/gstreamer1-plugins-bad
    
    x11-toolkits/gstreamer1-plugins-qt5:
    - Moved here from graphics/gstreamer1-plugins-qt to
      align with its new Qt6 counterpart
    
    x11-toolkits/gstreamer1-plugins-qt6:
    - New port: GStreamer Qt6 QML videosink plugin
    
    */*:
    - Improve Makefile order and formatting
    - Sorry for the repo churn, but not fixing this would just lead to more
      poorly formatted gstreamer ports in the future via copypasta
    
    https://gstreamer.freedesktop.org/releases/1.24/#1.24.8
    
    PR:		278914
    Reported by:	vvd

--- multimedia/gstreamer1-plugins-good/files/patch-sys_v4l2_v4l2__calls.c
+++ multimedia/gstreamer1-plugins-good/files/patch-sys_v4l2_v4l2__calls.c
@@ -1,6 +1,6 @@
---- sys/v4l2/v4l2_calls.c.orig	2022-09-13 20:56:55 UTC
+--- sys/v4l2/v4l2_calls.c.orig	2024-08-21 11:25:15 UTC
 +++ sys/v4l2/v4l2_calls.c
-@@ -1329,10 +1329,12 @@ gst_v4l2_query_dv_timings (GstV4l2Object * v4l2object,
+@@ -1334,10 +1334,12 @@ gst_v4l2_query_dv_timings (GstV4l2Object * v4l2object,
  
    if (ret < 0) {
      switch (errno) {
