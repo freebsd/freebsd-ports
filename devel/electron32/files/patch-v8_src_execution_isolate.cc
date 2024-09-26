9819a141fa2730ded476358e5a5ac7e8dd62c3c3
Hiroki Tagato <tagattie@FreeBSD.org>
2024-09-26 23:43:34 +0900

    devel/electron32: update to 32.1.2
    Changelog: https://github.com/electron/electron/releases/tag/v32.1.2
    
    Reported by:	GitHub (watch releases)

--- devel/electron32/files/patch-v8_src_execution_isolate.cc
+++ devel/electron32/files/patch-v8_src_execution_isolate.cc
@@ -1,4 +1,4 @@
---- v8/src/execution/isolate.cc.orig	2024-09-10 18:47:50 UTC
+--- v8/src/execution/isolate.cc.orig	2024-09-25 09:19:35 UTC
 +++ v8/src/execution/isolate.cc
 @@ -159,6 +159,10 @@
  #include "src/execution/simulator-base.h"
@@ -11,7 +11,7 @@
  extern "C" const uint8_t v8_Default_embedded_blob_code_[];
  extern "C" uint32_t v8_Default_embedded_blob_code_size_;
  extern "C" const uint8_t v8_Default_embedded_blob_data_[];
-@@ -4803,6 +4807,13 @@ void Isolate::InitializeDefaultEmbeddedBlob() {
+@@ -4828,6 +4832,13 @@ void Isolate::InitializeDefaultEmbeddedBlob() {
    uint32_t code_size = DefaultEmbeddedBlobCodeSize();
    const uint8_t* data = DefaultEmbeddedBlobData();
    uint32_t data_size = DefaultEmbeddedBlobDataSize();
