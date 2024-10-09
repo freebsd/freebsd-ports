aafe5d31c5f3afdaad0f6345bee06e7a3e2c5ced
Jason W. Bacon <jwb@FreeBSD.org>
2024-10-09 17:06:24 -0500

    biology/htslib: Update to 1.21
    Numerous bug fixes and enhancements since 1.17
    Changes:        https://github.com/samtools/htslib/releases
    
    Reported by:    portscout

--- biology/htslib/files/patch-htscodecs_htscodecs_rANS__static32x16pr__neon.c
+++ biology/htslib/files/patch-htscodecs_htscodecs_rANS__static32x16pr__neon.c
@@ -1,11 +1,11 @@
---- htscodecs/htscodecs/rANS_static32x16pr_neon.c.orig	2023-04-17 21:11:08 UTC
+--- htscodecs/htscodecs/rANS_static32x16pr_neon.c.orig	2024-10-09 21:43:35 UTC
 +++ htscodecs/htscodecs/rANS_static32x16pr_neon.c
 @@ -32,7 +32,7 @@
   */
  
  #include "config.h"
--#ifdef __ARM_NEON
-+#if defined(__ARM_NEON) && defined(__aarch64__)
+-#if defined(__ARM_NEON) && defined(__aarch64__)
++#if defined(__ARM_NEON)
  #include <arm_neon.h>
  
  #include <limits.h>
