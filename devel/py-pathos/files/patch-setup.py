40c0354a708a6116a903c6d996106e867e4caf75
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-10-14 12:49:42 +0800

    devel/py-pathos: Update to 0.3.3
    Changes:	https://github.com/uqfoundation/pathos/releases

--- devel/py-pathos/files/patch-setup.py
+++ devel/py-pathos/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2024-01-28 19:02:51 UTC
+--- setup.py.orig	2024-09-30 19:24:21 UTC
 +++ setup.py
-@@ -92,7 +92,7 @@ pox_version = 'pox>=0.3.4'
- ppft_version = 'ppft>=1.7.6.8'
- dill_version = 'dill>=0.3.8'
- pox_version = 'pox>=0.3.4'
--mp_version = 'multiprocess>=0.70.16'
+@@ -93,7 +93,7 @@ pox_version = 'pox>=0.3.5'
+ ppft_version = 'ppft>=1.7.6.9'
+ dill_version = 'dill>=0.3.9'
+ pox_version = 'pox>=0.3.5'
+-mp_version = 'multiprocess>=0.70.17'
 +mp_version = 'multiprocess>=0.70.12'
- pyina_version = 'pyina>=0.2.7'
- mystic_version = 'mystic>=0.4.0'
+ pyina_version = 'pyina>=0.2.9'
+ mystic_version = 'mystic>=0.4.2'
  # add dependencies
