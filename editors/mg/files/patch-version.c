565f8cc360a5fd805e8ab5be26963e4d8a281839
Mark Willson <cdr.nil@gmail.com>
2024-10-17 20:13:55 +0200

    editors/mg: Update to 20241008
    Pet portlint, portfmt.
    
    Changelog:
    * Ignore universal ctags extended metadata in tagaddress, making search
      patterns work again
    * Fix auto-indent-mode with custom tab widths
    * Add handling for C-u modifier in M-! and M-|
    * Dired-up-directory bound to ^ to go up a directory in dired buffers
    
    PR:		282052
    Approved by:	submitter is maintainer

---
+++ editors/mg/files/patch-version.c
@@ -0,0 +1,11 @@
+--- version.c.orig	2024-10-10 16:12:07 UTC
++++ version.c
+@@ -13,7 +13,7 @@
+ 
+ #include "def.h"
+ 
+-const char	version[] = "Mg 2a";
++const char	version[] = "Mg 2a (20241008)";
+ 
+ /*
+  * Display the version. All this does
