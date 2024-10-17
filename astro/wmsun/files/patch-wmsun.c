f641d910db00b2da0c8ea893b3884a1c839f3c10
Rodrigo Osorio <rodrigo@FreeBSD.org>
2024-10-18 00:00:17 +0200

    astro/wmsun: update to 1.06
    Changelog: https://repo.or.cz/dockapps.git/blob_plain/HEAD:/wmsun/ChangeLog
    
    Major changes:
      * Fix typo in manpage.
      * Move version number definition to Makefile
      * Add 'dist' target to Makefile to generate tarball
    
    Port changes:
      * reformat Makefile to make portfmt happy
      * update WWW (error 404)

--- astro/wmsun/files/patch-wmsun.c
+++ astro/wmsun/files/patch-wmsun.c
@@ -1,20 +1,20 @@
---- wmsun.c.orig	2015-08-28 11:57:39 UTC
+--- wmsun.c.orig	2024-10-17 21:51:08 UTC
 +++ wmsun.c
-@@ -43,7 +43,6 @@
+@@ -43,7 +43,7 @@
  /*
   *   Includes
   */
 -#define _POSIX_C_SOURCE 1
++#define _POSIX_C_SOURCE 200112L
  #include <X11/X.h>                     /* for ButtonPress, ButtonRelease, etc */
  #include <X11/Xlib.h>                  /* for XEvent, ConnectionNumber, etc */
  #include <math.h>                      /* for cos, sin */
-@@ -106,7 +105,8 @@ int main(int argc, char *argv[]) {
+@@ -105,7 +105,7 @@
      int			n;
      int 		Year, Month;
      int			DayOfMonth;
 -    long		CurrentLocalTime, CurrentGMTTime, date;
-+    time_t		CurrentLocalTime, CurrentGMTTime;
-+    long		date;
++    time_t		CurrentLocalTime, CurrentGMTTime, date;
      double		UT, val, LTRise, LTSet, LocalHour, hour24();
      int			H, M;
      struct timeval	timeout;
