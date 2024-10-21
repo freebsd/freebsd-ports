a6f01d004685d79cb052eca55da18aebb582d86f
Robert Clausecker <fuz@FreeBSD.org>
2024-10-21 15:12:49 +0200

    sysutils/fusefs-libs: move to filesystems/fusefs-libs
    Approved by:	portmgr

--- Mk/Uses/fuse.mk
+++ Mk/Uses/fuse.mk
@@ -14,7 +14,7 @@ LIBFUSE_VER=	${fuse_ARGS}
 LIBFUSE_VER?=	2
 
 .  if ${LIBFUSE_VER} == 2
-LIB_DEPENDS+=	libfuse.so:sysutils/fusefs-libs
+LIB_DEPENDS+=	libfuse.so:filesystems/fusefs-libs
 .  elif ${LIBFUSE_VER} == 3
 LIB_DEPENDS+=	libfuse3.so:filesystems/fusefs-libs3
 .  else
