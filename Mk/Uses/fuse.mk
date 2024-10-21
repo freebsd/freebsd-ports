9ec0c169359021af2ef18596c70bcdfeaeffdaa1
Robert Clausecker <fuz@FreeBSD.org>
2024-10-21 15:12:46 +0200

    sysutils/fusefs-libs3: move to filesystems/fusefs-libs3
    Approved by:	portmgr

--- Mk/Uses/fuse.mk
+++ Mk/Uses/fuse.mk
@@ -16,7 +16,7 @@ LIBFUSE_VER?=	2
 .  if ${LIBFUSE_VER} == 2
 LIB_DEPENDS+=	libfuse.so:sysutils/fusefs-libs
 .  elif ${LIBFUSE_VER} == 3
-LIB_DEPENDS+=	libfuse3.so:sysutils/fusefs-libs3
+LIB_DEPENDS+=	libfuse3.so:filesystems/fusefs-libs3
 .  else
 IGNORE=		cannot install: unknown FUSE library version: ${LIBFUSE_VERSION}
 .  endif
