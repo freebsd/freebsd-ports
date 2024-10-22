53b58221ade14504875fbd818f42ee551f65f79e
Alex Dupre <ale@FreeBSD.org>
2024-10-22 17:31:22 +0200

    security/cryptlib: update to 3.4.7 release.
    PR:		282203
    Submitted by:	Alven <alster@vinterdalen.se>
    Security:	CVE-2024-0202

--- security/cryptlib/files/patch-test_certs.c
+++ security/cryptlib/files/patch-test_certs.c
@@ -1,11 +1,11 @@
---- test/certs.c.orig	2023-03-20 10:42:36 UTC
+--- test/certs.c.orig	2023-01-31 00:46:48 UTC
 +++ test/certs.c
 @@ -52,7 +52,7 @@
  #if defined( __MWERKS__ ) || defined( SYMANTEC_C ) || defined( __MRC__ )
-   #define CERTTIME_DATETEST	( ( ( 2021 - 1970 ) * ONE_YEAR_TIME ) + 2082844800L )
+   #define CERTTIME_DATETEST	( ( ( 2022 - 1970 ) * ONE_YEAR_TIME ) + 2082844800L )
  #else
--  #define CERTTIME_DATETEST	( ( 2021 - 1970 ) * ONE_YEAR_TIME )
-+  #define CERTTIME_DATETEST	( ( 2023 - 1970 ) * ONE_YEAR_TIME )
+-  #define CERTTIME_DATETEST	( ( 2022 - 1970 ) * ONE_YEAR_TIME )
++  #define CERTTIME_DATETEST	( ( 2024 - 1970 ) * ONE_YEAR_TIME )
  #endif /* Macintosh-specific weird epoch */
  #if ( ULONG_MAX > 0xFFFFFFFFUL ) || defined( _M_X64 )
    #define SYSTEM_64BIT
