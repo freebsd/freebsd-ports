c3af229b02c5854b8419d3149eae2d2f42c78f74
Pietro Cerutti <gahr@FreeBSD.org>
2024-10-22 09:23:52 +0000

    www/tdom: update to 0.9.5
    * Added the asTypedList method to the domDoc and domNode commands.
    * Added the createFromTypedList method to the dom command.
    * Updated to expat 2.6.3.
    * Added the flag -keepTextStart to the expat command.
    * Text node commands created by createNodeCmd of the json type NULL, TRUE or FALSE may be called without argument.
    * Changed the error messages of the dom, domDoc and domNode commands to mostly confirm to ususal Tcl behaviour.
    * Added the asTclValue method to the domDoc and domNode commands.

--- www/tdom/files/patch-generic-dom.h
+++ www/tdom/files/patch-generic-dom.h
@@ -1,6 +1,6 @@
---- generic/dom.h	2018-09-24 12:17:05.706742000 +0000
-+++ generic/dom.h	2024-08-15 12:03:29.000000000 -0400
-@@ -41,8 +41,7 @@
+--- generic/dom.h.orig	2024-10-22 09:14:54 UTC
++++ generic/dom.h
+@@ -39,7 +39,6 @@ +-----------------------------------------------------
  #include <string.h>
  #include <ctype.h>
  #include <expat.h>
@@ -8,4 +8,3 @@
  #include <limits.h>
  
  /*
-  * tDOM provides it's own memory allocator which is optimized for
