6c7c1bd6a390189bf5b1b88edb8a4de5ad6fad94
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-10-29 22:20:22 +0800

    www/py-wagtail: Allow build with py-pillow 11.0.0+
    - Bump PORTREVISION for package change
    
    Obtained from:	https://github.com/wagtail/wagtail/commit/d404d37ba5d14ed3de2510a20fb81eb4a2e260cf

--- www/py-wagtail/files/patch-setup.py
+++ www/py-wagtail/files/patch-setup.py
@@ -1,11 +1,15 @@
---- setup.py.orig	2024-05-01 12:57:48 UTC
+--- setup.py.orig	2024-09-24 09:22:58 UTC
 +++ setup.py
-@@ -24,7 +24,7 @@ install_requires = [
+@@ -24,10 +24,10 @@ install_requires = [
      "django-permissionedforms>=0.1,<1.0",
-     "django-taggit>=4.0,<5.1",
+     "django-taggit>=5.0,<5.1",
      "django-treebeard>=4.5.1,<5.0",
 -    "djangorestframework>=3.15.1,<4.0",
 +    "djangorestframework>=3.14.0,<4.0",
      "django-filter>=23.3,<25",
      "draftjs_exporter>=2.1.5,<6.0",
-     "Pillow>=9.1.0,<11.0.0",
+-    "Pillow>=9.1.0,<11.0.0",
++    "Pillow>=9.1.0,<12.0.0",
+     "beautifulsoup4>=4.8,<4.13",
+     "Willow[heif]>=1.8.0,<2",
+     "requests>=2.11.1,<3.0",
