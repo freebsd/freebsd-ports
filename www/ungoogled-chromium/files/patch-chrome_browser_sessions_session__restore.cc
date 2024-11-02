ed3cd32164681a86504ee23b1210d2877d2e6cf0
Robert Nagy <rnagy@FreeBSD.org>
2024-11-02 09:27:42 +0100

    www/ungoogled-chromium: update to 130.0.6723.91
    Security:	https://vuxml.freebsd.org/freebsd/e17384ef-c5e8-4b5d-bb62-c13405e7f1f7.html

--- www/ungoogled-chromium/files/patch-chrome_browser_sessions_session__restore.cc
+++ www/ungoogled-chromium/files/patch-chrome_browser_sessions_session__restore.cc
@@ -1,4 +1,6 @@
---- chrome/browser/sessions/session_restore.cc.orig	2024-10-27 06:40:35 UTC
+diff --git chrome/browser/sessions/session_restore.cc chrome/browser/sessions/session_restore.cc	2024-11-02 08:11:27 UTC
+index 04610ad2c0..175c381644 100644
+--- chrome/browser/sessions/session_restore.cc
 +++ chrome/browser/sessions/session_restore.cc
 @@ -103,7 +103,7 @@
  #include "chrome/browser/web_applications/web_app_registrar.h"
@@ -9,7 +11,7 @@
  #include "chrome/browser/ui/webui/whats_new/whats_new_fetcher.h"
  #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
  
-@@ -1076,7 +1076,7 @@ class SessionRestoreImpl : public BrowserListObserver 
+@@ -1081,7 +1081,7 @@ class SessionRestoreImpl : public BrowserListObserver {
      bool is_first_tab = true;
      for (const auto& startup_tab : startup_tabs) {
        const GURL& url = startup_tab.url;
