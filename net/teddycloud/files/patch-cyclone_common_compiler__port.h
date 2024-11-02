2faca847a9b81becdab7355228ed5949002de067
Michael Pape <freebsd@pe82.de>
2024-11-02 08:06:39 +0200

    net/teddycloud: alternative to the public toniebox server
    Small fixes applied to build successfully.
    
    PR:		276541

---
+++ net/teddycloud/files/patch-cyclone_common_compiler__port.h
@@ -0,0 +1,11 @@
+--- cyclone/common/compiler_port.h.orig	2024-01-17 21:26:40 UTC
++++ cyclone/common/compiler_port.h
+@@ -106,7 +106,7 @@ typedef unsigned int uint_t;
+    #define PRIXSIZE "X"
+    #define PRIuTIME "lu"
+ //Linux GCC compiler
+-#elif defined(__linux__)
++#elif defined(__linux__) || defined(__unix__)
+    #define PRIuSIZE "zu"
+    #define PRIXSIZE "zX"
+    #define PRIuTIME "lu"
