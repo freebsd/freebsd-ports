1fa626a715ffccf17bd70209baff44ae795e1ba7
Curtis Hamilton <hamiltcl@verizon.net>
2024-11-04 03:50:28 -0800

    editors/openoffice-*: Fix build on powerpc64
    The upstream bridges code does not yet work on powerpc64.
    
    PR:		251417
    (cherry picked from commit d74f1e0a8d38d54eae26f2494ef077eb05c653fb)

--- editors/openoffice-4/files/patch-bridges_source_cpp__uno_gcc3__freebsd__powerpc64_share.hxx
+++ editors/openoffice-4/files/patch-bridges_source_cpp__uno_gcc3__freebsd__powerpc64_share.hxx
@@ -1,6 +1,65 @@
---- bridges/source/cpp_uno/gcc3_freebsd_powerpc64/share.hxx.orig	2020-10-06 14:16:16 UTC
+--- bridges/source/cpp_uno/gcc3_freebsd_powerpc64/share.hxx.orig
 +++ bridges/source/cpp_uno/gcc3_freebsd_powerpc64/share.hxx
-@@ -35,6 +35,7 @@ namespace CPPU_CURRENT_NAMESPACE
+@@ -25,16 +25,64 @@
+ 
+ #include <typeinfo>
+ #include <exception>
++
++#include <cxxabi.h>
++#ifndef _GLIBCXX_CDTOR_CALLABI // new in GCC 4.7 cxxabi.h
++#define _GLIBCXX_CDTOR_CALLABI
++#endif
+ #include <cstddef>
++
++#ifndef CONFIG_CXXABI_H
++#define CONFIG_CXXABI_H
++
++#define HAVE_CXXABI_H_BASE_CLASS_TYPE_INFO 0
++#define HAVE_CXXABI_H_CLASS_TYPE_INFO 0
++#define HAVE_CXXABI_H_CXA_ALLOCATE_EXCEPTION 0
++#define HAVE_CXXABI_H_CXA_EH_GLOBALS 1
++#define HAVE_CXXABI_H_CXA_EXCEPTION 1
++#define HAVE_CXXABI_H_CXA_GET_GLOBALS 1
++#define HAVE_CXXABI_H_CXA_CURRENT_EXCEPTION_TYPE 1
++#define HAVE_CXXABI_H_CXA_THROW 0
++#define HAVE_CXXABI_H_SI_CLASS_TYPE_INFO 0
++#define HAVE_CXXABI_H_VMI_CLASS_TYPE_INFO 0
++
++#endif
++
++#if !HAVE_CXXABI_H_CLASS_TYPE_INFO
++// <https://mentorembedded.github.io/cxx-abi/abi.html>,
++// libstdc++-v3/libsupc++/cxxabi.h:
++namespace __cxxabiv1 {
++class __class_type_info: public std::type_info {
++public:
++    explicit __class_type_info(char const * n): type_info(n) {}
++    ~__class_type_info();
++};
++}
++#endif
++
++#if !HAVE_CXXABI_H_SI_CLASS_TYPE_INFO
++// <https://mentorembedded.github.io/cxx-abi/abi.html>,
++// libstdc++-v3/libsupc++/cxxabi.h:
++namespace __cxxabiv1 {
++class __si_class_type_info: public __class_type_info {
++public:
++    __class_type_info const * __base_type;
++    explicit __si_class_type_info(
++        char const * n, __class_type_info const *base):
++        __class_type_info(n), __base_type(base) {}
++    ~__si_class_type_info();
++};
++}
++#endif
+ 
+ namespace CPPU_CURRENT_NAMESPACE
+ {
+ 
+-  void dummy_can_throw_anything( char const * );
+-
++void dummy_can_throw_anything( char const * );
  
  // ----- following decl from libstdc++-v3/libsupc++/unwind-cxx.h and unwind.h
  
@@ -8,15 +67,14 @@
  struct _Unwind_Exception
  {
      unsigned exception_class __attribute__((__mode__(__DI__)));
-@@ -63,18 +64,21 @@ struct __cxa_exception
+@@ -63,18 +111,21 @@
      
      _Unwind_Exception unwindHeader;
  };    
 +#endif /* __GLIBCXX__ */
  
  extern "C" void *__cxa_allocate_exception(
--    std::size_t thrown_size ) throw();
-+    size_t thrown_size );
+     std::size_t thrown_size ) throw();
  extern "C" void __cxa_throw (
      void *thrown_exception, std::type_info *tinfo, void (*dest) (void *) ) __attribute__((noreturn));
  
@@ -31,7 +89,7 @@
  
  // -----
  
-@@ -82,6 +86,10 @@ void raiseException(
+@@ -82,11 +133,16 @@
  void raiseException(
      uno_Any * pUnoExc, uno_Mapping * pUno2Cpp );
  //==================================================================================================
@@ -42,3 +100,11 @@
  void fillUnoException(
      __cxa_exception * header, uno_Any *, uno_Mapping * pCpp2Uno );
  }
+ 
+ namespace ppc64
+ {
+-	enum ppclimits { MAX_GPR_REGS = 8, MAX_SSE_REGS = 13 };
++     enum ppclimits { MAX_GPR_REGS = 8, MAX_SSE_REGS = 13 };
++    bool return_in_hidden_param( typelib_TypeDescriptionReference *pTypeRef );
+ }
+
