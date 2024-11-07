799f8ef64e3754b4dcf585c8c41e5df68c66f7d3
Robert Nagy <rnagy@FreeBSD.org>
2024-11-07 08:27:28 +0100

    www/chromium: update to 130.0.6723.116
    Security:https://vuxml.freebsd.org/freebsd/ab254c9d-9c36-11ef-8c1c-a8a1599412c6.html

--- www/chromium/files/patch-chrome_browser_supervised__user_supervised__user__google__auth__navigation__throttle.cc
+++ www/chromium/files/patch-chrome_browser_supervised__user_supervised__user__google__auth__navigation__throttle.cc
@@ -1,15 +1,15 @@
---- chrome/browser/supervised_user/supervised_user_google_auth_navigation_throttle.cc.orig	2024-10-22 08:31:56 UTC
+--- chrome/browser/supervised_user/supervised_user_google_auth_navigation_throttle.cc.orig	2024-11-06 12:34:11 UTC
 +++ chrome/browser/supervised_user/supervised_user_google_auth_navigation_throttle.cc
-@@ -29,7 +29,7 @@
- 
- #if BUILDFLAG(IS_ANDROID)
+@@ -31,7 +31,7 @@
  #include "chrome/browser/supervised_user/child_accounts/child_account_service_android.h"
+ #include "components/signin/public/identity_manager/identity_manager.h"
+ #include "ui/android/view_android.h"
 -#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 +#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_BSD)
  #include "chrome/browser/supervised_user/supervised_user_verification_controller_client.h"
  #include "chrome/browser/supervised_user/supervised_user_verification_page.h"
  #endif
-@@ -145,7 +145,7 @@ SupervisedUserGoogleAuthNavigationThrottle::ShouldProc
+@@ -147,7 +147,7 @@ SupervisedUserGoogleAuthNavigationThrottle::ShouldProc
      return content::NavigationThrottle::DEFER;
    }
  
