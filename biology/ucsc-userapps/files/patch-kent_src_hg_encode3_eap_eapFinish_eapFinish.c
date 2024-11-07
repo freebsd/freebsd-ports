aaa110d4b8fe64383826e03507d1cd4753a607ed
Jason W. Bacon <jwb@FreeBSD.org>
2024-11-07 07:17:36 -0600

    biology/ucsc-userapps: Update to 472
    Numerous fixes and enhancements since v414
    Changes: https://github.com/ucscGenomeBrowser/kent/releases
    
    Reported by:    fuz

--- biology/ucsc-userapps/files/patch-kent_src_hg_encode3_eap_eapFinish_eapFinish.c
+++ biology/ucsc-userapps/files/patch-kent_src_hg_encode3_eap_eapFinish_eapFinish.c
@@ -1,9 +1,9 @@
---- kent/src/hg/encode3/eap/eapFinish/eapFinish.c.orig	2021-04-14 11:23:30 UTC
+--- kent/src/hg/encode3/eap/eapFinish/eapFinish.c.orig	2024-10-16 04:15:27 UTC
 +++ kent/src/hg/encode3/eap/eapFinish/eapFinish.c
 @@ -3,7 +3,7 @@
  
  /* Copyright (C) 2014 The Regents of the University of California 
-  * See README in this or parent directory for licensing information. */
+  * See kent/LICENSE or http://genome.ucsc.edu/license/ for licensing information. */
 -#include <uuid/uuid.h>
 +#include <uuid.h>
  #include "common.h"
