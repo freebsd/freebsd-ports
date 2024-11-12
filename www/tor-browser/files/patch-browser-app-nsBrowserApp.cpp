540a0656dccfc68cf19f4f54b1e3ec62febaef37
Jesper Schmitz Mouridsen <jsm@FreeBSD.org>
2024-11-12 15:51:06 +0100

    www/tor-browser: Update to 14.0.1
    Remove webrtc patches since webrtc is disabled.
    
    PR:	281551
    Approved by:	Martin Filla <freebsd@sysctl.cz> (maintainer)
    Co-authored-by: Martin Filla <freebsd@sysctl.cz>
    
    (cherry picked from commit e1f70209b0eb5b5d6fb3b75f9dc257fc5c9a77b8)

--- www/tor-browser/files/patch-browser-app-nsBrowserApp.cpp
+++ www/tor-browser/files/patch-browser-app-nsBrowserApp.cpp
@@ -1,12 +1,14 @@
---- browser/app/nsBrowserApp.cpp.orig	2016-09-19 16:19:28 UTC
-+++ browser/app/nsBrowserApp.cpp
-@@ -324,6 +324,9 @@ int main(int argc, char* argv[], char* e
- {
-   mozilla::TimeStamp start = mozilla::TimeStamp::Now();
+--- browser/app/nsBrowserApp.cpp.orig	2024-10-21 21:51:41.000000000 +0200
++++ browser/app/nsBrowserApp.cpp	2024-11-07 21:47:05.301097000 +0100
+@@ -391,6 +391,11 @@
+   // Register an external module to report on otherwise uncatchable exceptions.
+   CrashReporter::RegisterRuntimeExceptionModule();
  
 +  setenv("MOZ_GMP_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
 +  setenv("MOZ_PLUGIN_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
++  setenv("FONTCONFIG_PATH", "%%LOCALBASE%%/lib/tor-browser/fontconfig/", 1);
++  setenv("FONTCONFIG_FILE", "fonts.conf", 1);
 +
  #ifdef HAS_DLL_BLOCKLIST
-   DllBlocklist_Initialize();
- 
+   DllBlocklist_Initialize(gBlocklistInitFlags);
+ #endif
