702b257be6fb833e5e0365ea64c4ae28996d0517
Matthias Andree <mandree@FreeBSD.org>
2024-11-14 00:22:48 +0100

    mail/fetchmail: update to 6.5.1, minor polishing.
    Translation updates for files we missed to have translated.
    
    Some improvements to the build experience were made;
    most of the local patches have been integrated upstream.
    
    (Also fixes missing TIMESTAMP and SIZE fields in distinfo,
    vs. Corey's review. -- Thanks!)
    
    Approved by:	Corey Halpin (maintainer)
    PR:		282743

--- mail/fetchmail/files/patch-socket.c
+++ mail/fetchmail/files/patch-socket.c
@@ -13,12 +13,3 @@ for FreeBSD 13.3 and 13.4:
  /* do not warn about OpenSSL 3.2.0, the 3.2.1 fix is of low priority */
  # if OPENSSL_VERSION_NUMBER >= 0x30100000L && OPENSSL_VERSION_NUMBER < 0x30200000L
  #  if OPENSSL_VERSION_NUMBER < 0x30100040L
-@@ -402,7 +402,7 @@ enum { SSL_min_security_level = 2 };
- #endif /* USING_WOLFSSL */
- 
- /* workaround for EVP_MD_fetch API - missing on wolfSSL and LibreSSL */
--#if defined(USING_WOLFSSL) || defined(LIBRESSL_VERSION_NUMBER)
-+#if OPENSSL_VERSION_NUMBER < 0x30000000L
- # define EVP_MD_fetch(x, digest, y) (EVP_get_digestbyname(digest))
- # define EVP_MD_free(x) /* NOOP */
- # define fm_EVP_MD_const const // compatibility const EVP_MD* from EVP_get_digestbyname()
