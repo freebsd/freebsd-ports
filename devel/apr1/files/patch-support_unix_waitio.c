efc00d9fd418d84a1f88c3b74a2d07bd41df7f91
Bernard Spil <brnrd@FreeBSD.org>
2024-11-16 12:20:26 +0100

    devel/apr1: Fix poll(2) timeout issue
    Fix by jkim
    
    PR:		282137
    Reported by:	Tomáš Čiernik <tomas ciernik sk>

---
+++ devel/apr1/files/patch-support_unix_waitio.c
@@ -0,0 +1,14 @@
+--- apr-1.7.5/support/unix/waitio.c	(revision 1921905)
++++ apr-1.7.5/support/unix/waitio.c	(working copy)
+@@ -46,7 +46,10 @@ apr_status_t apr_wait_for_io_or_timeout(apr_file_t
+     pfd.fd     = f        ? f->filedes        : s->socketdes;
+     pfd.events = for_read ? POLLIN            : POLLOUT;
+ 
+-    if (timeout > 0) {
++    if (timeout < 0) {
++        timeout = -1;
++    }
++    else if (timeout > 0) {
+         timeout = (timeout + 999) / 1000;
+     }
+     do {
