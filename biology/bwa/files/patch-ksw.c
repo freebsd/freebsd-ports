f488a27c4d56bbdb72b779282ba86402d1699ca1
Jason W. Bacon <jwb@FreeBSD.org>
2024-11-24 07:31:40 -0600

    biology/bwa: Update to 0.7.18
    Official support for aarch64 (we already had this patched in)
    A few minor enhancements and fixes
    Changes: https://github.com/lh3/bwa/releases
    
    PR:             282923
    Reported by:    alster@vinterdalen.se

--- biology/bwa/files/patch-ksw.c
+++ biology/bwa/files/patch-ksw.c
@@ -1,13 +1,13 @@
---- ksw.c.orig	2021-12-01 13:48:35 UTC
+--- ksw.c.orig	2024-11-22 18:42:53 UTC
 +++ ksw.c
-@@ -26,7 +26,9 @@
- #include <stdlib.h>
+@@ -27,7 +27,9 @@
  #include <stdint.h>
  #include <assert.h>
+ #if defined __SSE2__
 -#include <emmintrin.h>
 +// #include <emmintrin.h>
 +#define SIMDE_ENABLE_NATIVE_ALIASES
 +#include <simde/x86/sse2.h>
- #include "ksw.h"
- 
- #ifdef USE_MALLOC_WRAPPERS
+ #elif defined __ARM_NEON
+ #include "neon_sse.h"
+ #else
