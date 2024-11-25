e25b4bfc5b4c9d8650a7425aef5c2dae451e5457
Robert Nagy <rnagy@FreeBSD.org>
2024-11-25 10:02:39 +0100

    www/*chromium: update to 131.0.6778.85
    Security:	https://vuxml.freebsd.org/freebsd/9dfca0cd-ab09-11ef-8c1c-a8a1599412c6.html

--- www/chromium/files/patch-v8_src_execution_isolate.cc
+++ www/chromium/files/patch-v8_src_execution_isolate.cc
@@ -1,4 +1,4 @@
---- v8/src/execution/isolate.cc.orig	2024-11-14 07:57:23 UTC
+--- v8/src/execution/isolate.cc.orig	2024-11-25 09:01:53 UTC
 +++ v8/src/execution/isolate.cc
 @@ -168,6 +168,10 @@
  #include "src/execution/simulator-base.h"
@@ -11,7 +11,7 @@
  extern "C" const uint8_t v8_Default_embedded_blob_code_[];
  extern "C" uint32_t v8_Default_embedded_blob_code_size_;
  extern "C" const uint8_t v8_Default_embedded_blob_data_[];
-@@ -4994,6 +4998,13 @@ void Isolate::InitializeDefaultEmbeddedBlob() {
+@@ -4995,6 +4999,13 @@ void Isolate::InitializeDefaultEmbeddedBlob() {
    uint32_t code_size = DefaultEmbeddedBlobCodeSize();
    const uint8_t* data = DefaultEmbeddedBlobData();
    uint32_t data_size = DefaultEmbeddedBlobDataSize();
