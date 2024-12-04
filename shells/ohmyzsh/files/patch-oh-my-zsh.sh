3d59402555b169827af94fb67c3f6a269e4adcdc
Serhii (Sergey) Kozlov <skozlov@FreeBSD.org>
2024-12-04 22:05:44 +0000

    shells/ohmyzsh: update to 20241128

--- shells/ohmyzsh/files/patch-oh-my-zsh.sh
+++ shells/ohmyzsh/files/patch-oh-my-zsh.sh
@@ -1,8 +1,8 @@
---- oh-my-zsh.sh.orig	2023-09-16 14:01:58 UTC
+--- oh-my-zsh.sh.orig	2024-12-04 21:57:15 UTC
 +++ oh-my-zsh.sh
-@@ -65,9 +65,6 @@ mkdir -p "$ZSH_CACHE_DIR/completions"
+@@ -67,9 +67,6 @@ mkdir -p "$ZSH_CACHE_DIR/completions"
  mkdir -p "$ZSH_CACHE_DIR/completions"
- (( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
+ (( ${fpath[(Ie)$ZSH_CACHE_DIR/completions]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
  
 -# Check for updates on initial load...
 -source "$ZSH/tools/check_for_upgrade.sh"
