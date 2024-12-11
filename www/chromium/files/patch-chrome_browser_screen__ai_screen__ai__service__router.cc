d5403e7eb37c379deb17380bd23d726cc30f580e
Robert Nagy <rnagy@FreeBSD.org>
2024-12-11 15:01:20 +0100

    www/chromium: update to 131.0.6778.139
    Security:	https://vuxml.freebsd.org/freebsd/aeee5ebd-356c-49c1-8959-7c88981de5fd.html
    Security:	https://vuxml.freebsd.org/freebsd/3d5b7860-48ad-48c2-aa36-601b8ab9cc43.html

--- www/chromium/files/patch-chrome_browser_screen__ai_screen__ai__service__router.cc
+++ www/chromium/files/patch-chrome_browser_screen__ai_screen__ai__service__router.cc
@@ -1,6 +1,6 @@
---- chrome/browser/screen_ai/screen_ai_service_router.cc.orig	2024-10-22 08:31:56 UTC
+--- chrome/browser/screen_ai/screen_ai_service_router.cc.orig	2024-12-11 13:49:08 UTC
 +++ chrome/browser/screen_ai/screen_ai_service_router.cc
-@@ -324,7 +324,7 @@ void ScreenAIServiceRouter::LaunchIfNotRunning() {
+@@ -318,7 +318,7 @@ void ScreenAIServiceRouter::LaunchIfNotRunning() {
    base::FilePath binary_path = state_instance->get_component_binary_path();
  #if BUILDFLAG(IS_WIN)
    std::vector<base::FilePath> preload_libraries = {binary_path};
@@ -9,7 +9,7 @@
    std::vector<std::string> extra_switches = {
        base::StringPrintf("--%s=%s", screen_ai::GetBinaryPathSwitch(),
                           binary_path.MaybeAsASCII().c_str())};
-@@ -338,7 +338,7 @@ void ScreenAIServiceRouter::LaunchIfNotRunning() {
+@@ -332,7 +332,7 @@ void ScreenAIServiceRouter::LaunchIfNotRunning() {
            .WithPreloadedLibraries(
                preload_libraries,
                content::ServiceProcessHostPreloadLibraries::GetPassKey())
