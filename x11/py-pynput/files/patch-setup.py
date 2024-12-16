2eebdf75cd266b95409e09d4923195e027e90ff5
Yuri Victorovich <yuri@FreeBSD.org>
2024-12-16 08:09:04 -0700

    x11/py-pynput: update 1.7.6 → 1.7.7

--- x11/py-pynput/files/patch-setup.py
+++ x11/py-pynput/files/patch-setup.py
@@ -1,16 +1,13 @@
 - setuptools-lint isn't really needed during build
 
---- setup.py.orig	2022-01-01 19:56:27 UTC
+--- setup.py.orig	2024-05-10 13:18:06 UTC
 +++ setup.py
-@@ -23,8 +23,9 @@ SETUP_PACKAGES = [
+@@ -23,8 +23,6 @@ SETUP_PACKAGES = [
  
  #: Additional requirements used during setup
  SETUP_PACKAGES = [
 -    'setuptools-lint >=0.5',
--    'sphinx >=1.3.1']
-+    #'setuptools-lint >=0.5',
-+    #'sphinx >=1.3.1']
-+    ]
+-    'sphinx >=1.3.1',
+     'twine >=4.0']
  
  #: Packages requires for different environments
- EXTRA_PACKAGES = {
