5d17e8f667b7b404725f5598a091d9e2cc7c368c
Tijl Coosemans <tijl@FreeBSD.org>
2024-12-23 14:17:23 +0100

    devel/libtextstyle, devel/gettext*: Update to 0.23
    comms/grig, deskutils/xpad, games/7kaa:
    Fix problem between autopoint (gettext) and aclocal (automake).
    First autopoint installs gettext m4 files in WRKSRC/m4 for the
    version of gettext requested in configure.ac.  And then aclocal
    should use those files.  These ports were either missing an "-I m4"
    flag so aclocal didn't see the autopoint m4 files but used the ones
    from LOCALBASE/share/aclocal, which are the wrong version, or they
    used the --install flag that makes aclocal copy files from
    LOCALBASE/share/aclocal overwriting the autopoint m4 files.
    
    PR:		283536
    Exp-run by:	antoine

--- devel/gettext-runtime/files/patch-intl_osdep.c
+++ devel/gettext-runtime/files/patch-intl_osdep.c
@@ -1,26 +1,34 @@
 --- intl/osdep.c.orig	2019-05-11 11:29:32 UTC
 +++ intl/osdep.c
-@@ -18,6 +18,25 @@
+@@ -18,6 +18,33 @@
  # include "intl-exports.c"
  #elif defined __EMX__ && !defined __KLIBC__
  # include "os2compat.c"
 +#elif defined __FreeBSD__
 +#include <config.h>
-+#include <sys/param.h>
++#include <osreldate.h>
 +#include <dlfcn.h>
 +#include <pthread.h>
 +static __attribute__((constructor)) void
 +libintl_init (void)
 +{
 +#if __FreeBSD_version >= 1400094
-+  /* We don't link with libpthread in order to avoid the overhead for
-+     non-threaded programs.  Instead we dlopen it with RTLD_NOLOAD here
-+     to ensure it is initialised when present.  */
++  /* This dlopen call initialises libpthread if it is present.  Normally
++     this is handled by linking to libpthread but libintl does not link
++     with libpthread to avoid the overhead for non-threaded programs.  */
 +  (void) dlopen ("libpthread.so", RTLD_LAZY | RTLD_GLOBAL | RTLD_NOLOAD);
 +#else
-+  /* For older versions this hack also triggers libpthread
-+     initialisation.  */
-+  (void) pthread_self ();
++  /* The dlopen call above does not work correctly on older versions of
++     FreeBSD.  Call pthread_mutex_timedlock instead.  It initialises
++     libpthread and there's no libc stub that can preempt it.  */
++#pragma weak pthread_mutex_timedlock
++  if (pthread_mutex_timedlock != NULL)
++    {
++      pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
++      pthread_mutex_timedlock (&lock, NULL);
++      pthread_mutex_unlock (&lock);
++      pthread_mutex_destroy (&lock);
++    }
 +#endif
 +}
  #else
