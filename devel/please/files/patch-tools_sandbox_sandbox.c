c39f73b9222b3947906e321f82a8f0bb5f6ed81b
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2024-12-23 05:58:01 +0100

    devel/please: Update version 16.0.0=>17.12.7
    Thanks to Pat for the pointers and help in updating this.
    
    Changelog: https://github.com/thought-machine/please/releases/tag/v17.12.7
    
    PR:		276791
    Reported by:	yonas.yanfa@gmail.com

--- devel/please/files/patch-tools_sandbox_sandbox.c
+++
@@ -1,8 +0,0 @@
---- tools/sandbox/sandbox.c.orig	2024-12-23 04:06:56 UTC
-+++ tools/sandbox/sandbox.c
-@@ -1,4 +1,4 @@
--#include "tools/sandbox/sandbox.h"
-+#include "sandbox.h"
- 
- #define _GNU_SOURCE
- #include <stdio.h>
