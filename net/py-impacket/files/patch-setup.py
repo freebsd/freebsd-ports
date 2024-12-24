1a55753f49342474c94c5aa7c87db89b541ab931
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2024-12-23 21:06:09 -0800

    net/py-impacket: Update to 0.12.0

--- net/py-impacket/files/patch-setup.py
+++ net/py-impacket/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2023-09-21 00:08:28.427651000 -0500
-+++ setup.py	2023-09-21 00:09:29.055566000 -0500
-@@ -39,12 +39,6 @@
+--- setup.py.orig	2024-12-24 05:02:53 UTC
++++ setup.py
+@@ -41,12 +41,6 @@ except Exception:
  except Exception:
      VER_LOCAL = ""
  
@@ -13,11 +13,12 @@
  def read(fname):
      return open(os.path.join(os.path.dirname(__file__), fname)).read()
  
-@@ -66,7 +60,6 @@
+@@ -69,8 +63,6 @@ setup(
                'impacket.examples.ntlmrelayx.servers.socksplugins', 'impacket.examples.ntlmrelayx.utils',
                'impacket.examples.ntlmrelayx.attacks', 'impacket.examples.ntlmrelayx.attacks.httpattacks'],
      scripts=glob.glob(os.path.join('examples', '*.py')),
 -    data_files=data_files,
-     install_requires=['pyasn1>=0.2.3', 'pycryptodomex', 'pyOpenSSL>=21.0.0', 'six', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6',
-                       'ldapdomaindump>=0.9.0', 'flask>=1.0', 'future', 'charset_normalizer', 'dsinternals'],
-     extras_require={'pyreadline:sys_platform=="win32"': [],
+-
+     install_requires=['pyasn1>=0.2.3', 'pyasn1_modules', 'pycryptodomex', 'pyOpenSSL==24.0.0', 'six', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6',
+                       'ldapdomaindump>=0.9.0', 'flask>=1.0', 'setuptools', 'charset_normalizer'],
+     extras_require={':sys_platform=="win32"': ['pyreadline3'],
