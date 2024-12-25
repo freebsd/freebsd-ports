ce12070566064276b210eab81cb19a75d0aead07
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-12-26 02:48:21 +0800

    devel/py-awscrt: Update to 0.23.4
    Changes:	https://github.com/awslabs/aws-crt-python/releases

--- devel/py-awscrt/files/patch-setup.py
+++ devel/py-awscrt/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2024-10-23 17:56:10 UTC
+--- setup.py.orig	2024-12-06 18:35:30 UTC
 +++ setup.py
-@@ -353,7 +353,7 @@ def awscrt_ext():
-         # In Brazil, both shared and static libs are available.
-         # But Lambda requires all shared libs to be explicitly packaged up.
-         # So it's simpler to link them in statically and have less runtime dependencies.
--        libraries = [':lib{}.a'.format(x) for x in libraries]
-+        libraries = [':lib{}.so'.format(x) for x in libraries]
+@@ -359,7 +359,7 @@ def awscrt_ext():
+             # So it's simpler to link them in statically and have less runtime dependencies.
+             #
+             # Don't apply this trick to dependencies that are always on the OS (e.g. librt)
+-            libraries = [':lib{}.a'.format(x) for x in libraries]
++            libraries = [':lib{}.so'.format(x) for x in libraries]
  
          # OpenBSD doesn't have librt; functions are found in libc instead.
          if not sys.platform.startswith('openbsd'):
