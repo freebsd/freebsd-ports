7b5bb5efdad1ca269649861a3b8e38a778c67e54
Nuno Teixeira <eduardo@FreeBSD.org>
2024-12-26 08:26:19 +0000

    devel/rgbds: Update to 0.9.0
    - Switch to cmake build
    - Add tests
    
    ChangeLog: https://github.com/gbdev/rgbds/releases/tag/v0.9.0

---
+++ devel/rgbds/files/patch-test_gfx_rgbgfx__test.cpp
@@ -0,0 +1,10 @@
+--- test/gfx/rgbgfx_test.cpp.orig	2024-12-25 20:36:32 UTC
++++ test/gfx/rgbgfx_test.cpp
+@@ -23,6 +23,7 @@
+ #include <fstream>
+ #include <limits>
+ #include <png.h>
++#include <signal.h>
+ #include <stdarg.h>
+ #include <stdlib.h>
+ #include <string.h>
