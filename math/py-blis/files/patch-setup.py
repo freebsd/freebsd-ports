e064dd616ad96bf4e8fc2104b5d2d46788f8c1b1
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2024-12-31 23:49:47 +0800

    math/py-blis: Update 1.1.0
    Changes:	https://github.com/explosion/cython-blis/releases

--- math/py-blis/files/patch-setup.py
+++ math/py-blis/files/patch-setup.py
@@ -1,15 +1,11 @@
---- setup.py.orig	2024-07-25 17:54:37 UTC
+--- setup.py.orig	2024-12-13 12:03:50 UTC
 +++ setup.py
-@@ -327,10 +327,10 @@ setup(
- setup(
+@@ -328,7 +328,7 @@ setup(
+     python_requires=">=3.9,<3.14",
      setup_requires=[
          "cython>=0.25",
 -        "numpy>=2.0.0,<3.0.0",
-+        "numpy>=0,<3.0.0",
++        "numpy>=1.19.3,<3.0.0",
      ],
      install_requires=[
--        "numpy>=2.0.0,<3.0.0",
-+        "numpy>=0,<3.0.0",
-     ],
-     ext_modules=cythonize(
-         [
+         "numpy>=1.19.3,<3.0.0",
