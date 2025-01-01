60756926e52d57f6053a353c4416486e3a3e2985
Rene Ladan <rene@FreeBSD.org>
2025-01-01 13:19:51 +0100

    security/maia: Remove expired port
    2024-12-31 security/maia: Depends on deprecated dependencies and doesn't work with PHP 8.

--- security/maia/files/patch-scripts_maiadbtool.pl
+++
@@ -1,8 +0,0 @@
---- scripts/maiadbtool.pl.orig	2022-03-02 01:05:20 UTC
-+++ scripts/maiadbtool.pl
-@@ -1,4 +1,4 @@
--#!/usr/bin/perl
-+#!/usr/bin/env perl
- 
- # $Id: $
- 
--- security/maia/files/patch-scripts_process-quarantine.pl
+++
@@ -1,8 +0,0 @@
---- scripts/process-quarantine.pl.orig	2022-03-02 01:05:20 UTC
-+++ scripts/process-quarantine.pl
-@@ -1,4 +1,4 @@
--#!/usr/bin/perl -w
-+#!/usr/bin/env perl
- 
- package Maia::ProcessQuarantine;
- 
