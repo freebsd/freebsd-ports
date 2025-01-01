151bd9399cf8ecac13245fb778aa9553ec0a4907
Rene Ladan <rene@FreeBSD.org>
2025-01-01 12:29:56 +0100

    security/sssd: Remove expired port
    2024-12-31 security/sssd: Not supported, please use deve/sssd2

--- security/sssd/files/patch-src__util__util.h
+++
@@ -1,9 +0,0 @@
---- src/util/util.h.orig	2020-03-17 13:31:28 UTC
-+++ src/util/util.h
-@@ -749,4 +749,6 @@ errno_t create_preauth_indicator(void);
- #define N_ELEMENTS(arr) (sizeof(arr) / sizeof(arr[0]))
- #endif
- 
-+#include "util/sss_bsd_errno.h"
-+
- #endif /* __SSSD_UTIL_H__ */
