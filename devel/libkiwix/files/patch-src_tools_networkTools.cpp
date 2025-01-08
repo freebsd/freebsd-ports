freebsd/freebsd-ports

commit dafb538b3fd6eabe519f34c3f83027cf17e286e8

devel/libkiwix: Update to 14.0.0
PR:		282559
Reported by:	wenheping2000@hotmail.com
Approved by:	maintainer(timeout, > 50 days)

devel/libkiwix/Makefile
@@ -1,5 +1,5 @@
 PORTNAME=	libkiwix
-DISTVERSION=	13.1.0
+DISTVERSION=	14.0.0
 CATEGORIES=	devel
 
 MAINTAINER=	olce.freebsd.ports@certner.fr

devel/libkiwix/distinfo
@@ -1,5 +1,5 @@
-TIMESTAMP = 1716196790
-SHA256 (kiwix-libkiwix-13.1.0_GH0.tar.gz) = 04d93f66b939f937df2c941af517966128d4c9872b707f9ed192177fe51364d3
-SIZE (kiwix-libkiwix-13.1.0_GH0.tar.gz) = 1136812
+TIMESTAMP = 1735180600
+SHA256 (kiwix-libkiwix-14.0.0_GH0.tar.gz) = dae329ebc08ff510daa50b42de71366bf063813e29bf1c09cc49335d90f32094
+SIZE (kiwix-libkiwix-14.0.0_GH0.tar.gz) = 1149884
 SHA256 (kainjow-Mustache-v4.1_GH0.tar.gz) = acd66359feb4318b421f9574cfc5a511133a77d916d0b13c7caa3783c0bfe167
 SIZE (kainjow-Mustache-v4.1_GH0.tar.gz) = 98905

devel/libkiwix/files/patch-src_tools_networkTools.cpp
@@ -1,0 +1,10 @@
+--- src/tools/networkTools.cpp.orig	2024-12-26 02:42:25 UTC
++++ src/tools/networkTools.cpp
+@@ -27,6 +27,7 @@
+ #include <string.h>
+ 
+ #include <curl/curl.h>
++#include <netinet/in.h>
+ 
+ #include <sstream>
+ #include <iostream>

devel/libkiwix/pkg-plist
@@ -4,6 +4,7 @@ include/kiwix/book.h
 include/kiwix/bookmark.h
 include/kiwix/common.h
 include/kiwix/downloader.h
+include/kiwix/i18n.h
 include/kiwix/kiwix_config.h
 include/kiwix/kiwixserve.h
 include/kiwix/library.h
@@ -16,6 +17,6 @@ include/kiwix/version.h
 lib/libkiwix.so
 lib/libkiwix.so.%%PORTMAJ%%
 lib/libkiwix.so.%%PORTVERSION%%
-libdata/pkgconfig/kiwix.pc
+libdata/pkgconfig/libkiwix.pc
 share/man/man1/kiwix-compile-i18n.1.gz
 share/man/man1/kiwix-compile-resources.1.gz

commit 6caffd033b02defbf11db7a593331c4adbffb808

devel/libkiwix: Revert update to 14.0.0 (so, → 13.1.0)
The latest release of www/kiwix-tools, which is still 3.7.0, does not
work with libkiwix 14.0.0, so the update was premature.

We will be able to update libkiwix only when the new release of
www/kiwix-tools is out. (The alternative of maintaining a few libkiwix
ports with different versions does not seem worth it.)

This is a maintainer update (olce.freebsd.ports@certner.fr).

This reverts commit dafb538b3fd6 (devel/libkiwix: Update to 14.0.0, 2024-12-26).

Fixes:          dafb538b3fd6 ("devel/libkiwix: Update to 14.0.0")

devel/libkiwix/Makefile
@@ -1,5 +1,5 @@
 PORTNAME=	libkiwix
-DISTVERSION=	14.0.0
+DISTVERSION=	13.1.0
 CATEGORIES=	devel
 
 MAINTAINER=	olce.freebsd.ports@certner.fr

devel/libkiwix/distinfo
@@ -1,5 +1,5 @@
-TIMESTAMP = 1735180600
-SHA256 (kiwix-libkiwix-14.0.0_GH0.tar.gz) = dae329ebc08ff510daa50b42de71366bf063813e29bf1c09cc49335d90f32094
-SIZE (kiwix-libkiwix-14.0.0_GH0.tar.gz) = 1149884
+TIMESTAMP = 1716196790
+SHA256 (kiwix-libkiwix-13.1.0_GH0.tar.gz) = 04d93f66b939f937df2c941af517966128d4c9872b707f9ed192177fe51364d3
+SIZE (kiwix-libkiwix-13.1.0_GH0.tar.gz) = 1136812
 SHA256 (kainjow-Mustache-v4.1_GH0.tar.gz) = acd66359feb4318b421f9574cfc5a511133a77d916d0b13c7caa3783c0bfe167
 SIZE (kainjow-Mustache-v4.1_GH0.tar.gz) = 98905

devel/libkiwix/files/patch-src_tools_networkTools.cpp
@@ -1,10 +1,0 @@
---- src/tools/networkTools.cpp.orig	2024-12-26 02:42:25 UTC
-+++ src/tools/networkTools.cpp
-@@ -27,6 +27,7 @@
- #include <string.h>
- 
- #include <curl/curl.h>
-+#include <netinet/in.h>
- 
- #include <sstream>
- #include <iostream>

devel/libkiwix/pkg-plist
@@ -4,7 +4,6 @@ include/kiwix/book.h
 include/kiwix/bookmark.h
 include/kiwix/common.h
 include/kiwix/downloader.h
-include/kiwix/i18n.h
 include/kiwix/kiwix_config.h
 include/kiwix/kiwixserve.h
 include/kiwix/library.h
@@ -17,6 +16,6 @@ include/kiwix/version.h
 lib/libkiwix.so
 lib/libkiwix.so.%%PORTMAJ%%
 lib/libkiwix.so.%%PORTVERSION%%
-libdata/pkgconfig/libkiwix.pc
+libdata/pkgconfig/kiwix.pc
 share/man/man1/kiwix-compile-i18n.1.gz
 share/man/man1/kiwix-compile-resources.1.gz