406e2dec9af9c1be3f802f7413a242b2671e347d
Piotr Kubaj <pkubaj@FreeBSD.org>
2025-01-11 12:11:38 +0100

    net/libiscsi: generalize arm and i386 fix for powerpc

--- net/libiscsi/files/patch-utils_iscsi-discard.c
+++ net/libiscsi/files/patch-utils_iscsi-discard.c
@@ -4,7 +4,7 @@
  
  	capacity = block_length * (rc16->returned_lba + 1);
  	if (offset > capacity) {
-+#if defined(__i386__) || defined(__arm__)
++#ifndef __LP64__
 +		fprintf(stderr,"Offset(%llu) exceeds capacity(%llu)\n", offset, capacity);
 +#else
  		fprintf(stderr,"Offset(%lu) exceeds capacity(%lu)\n", offset, capacity);
