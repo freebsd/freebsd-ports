14a708d2bb9164afcda034636a457bcbe0028a4e
Jan Beich <jbeich@FreeBSD.org>
2025-01-11 01:07:37 +0100

    x11/nwg-menu: update to 0.1.7
    Changes:	https://github.com/nwg-piotr/nwg-menu/releases/tag/v0.1.7
    Reported by:	GitHub (watch releases)

--- x11/nwg-menu/files/patch-main.go
+++ x11/nwg-menu/files/patch-main.go
@@ -1,8 +1,8 @@
 - Adapt to BSD-style shutdown
 
---- main.go.orig	2024-09-21 00:07:27 UTC
+--- main.go.orig	2025-01-11 00:07:37 UTC
 +++ main.go
-@@ -121,8 +121,8 @@ var cmdLogout = flag.String("cmd-logout", "swaymsg exi
+@@ -122,8 +122,8 @@ var cmdLogout = flag.String("cmd-logout", "swaymsg exi
  var wm = flag.String("wm", "", "use swaymsg exec (with 'sway' argument) or hyprctl dispatch exec (with 'hyprland') or riverctl spawn (with 'river') to launch programs")
  var cmdLock = flag.String("cmd-lock", "swaylock -f -c 000000", "screen lock command")
  var cmdLogout = flag.String("cmd-logout", "swaymsg exit", "logout command")
@@ -10,6 +10,6 @@
 -var cmdShutdown = flag.String("cmd-shutdown", "systemctl -i poweroff", "shutdown command")
 +var cmdRestart = flag.String("cmd-restart", "shutdown -r now", "reboot command")
 +var cmdShutdown = flag.String("cmd-shutdown", "shutdown -p now", "shutdown command")
- var debug = flag.Bool("debug", false, "Turn on Debug messages")
+ var debug = flag.Bool("debug", false, "turn on Debug messages")
+ var hover = flag.Bool("t", false, "hovering caTegories opens submenus")
  
- func main() {
