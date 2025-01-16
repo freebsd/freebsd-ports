124607f559511427bb68ff74efd83c67e62f45e2
Guido Falsi <madpilot@FreeBSD.org>
2025-01-16 22:24:33 +0100

    games/0ad: Fix build when HOSTTYPE is defined in the env
    Reported by:	dinoex (via IRC)
    Tested by:	dinoex
    MFH:		2025Q1
    
    (cherry picked from commit 01efbf239baaddf6a729627f7ee1709f61955efc)

--- games/0ad/files/patch-build_premake_premake5.lua
+++ games/0ad/files/patch-build_premake_premake5.lua
@@ -1,6 +1,25 @@
---- build/premake/premake5.lua.orig	2021-07-27 21:57:08 UTC
+--- build/premake/premake5.lua.orig	2022-09-23 19:16:45 UTC
 +++ build/premake/premake5.lua
-@@ -367,7 +367,7 @@ function project_set_build_flags()
+@@ -76,14 +76,10 @@ else
+ 	end
+ else
+ 	local machine = "x86_64"
+-	if os.getenv("HOSTTYPE") and os.getenv("HOSTTYPE") ~= '' then
+-		machine = os.getenv("HOSTTYPE")
+-	else
+-		os.execute(cc .. " -dumpmachine > .gccmachine.tmp")
+-		local f = io.open(".gccmachine.tmp", "r")
+-		machine = f:read("*line")
+-		f:close()
+-	end
++	os.execute(cc .. " -dumpmachine > .gccmachine.tmp")
++	local f = io.open(".gccmachine.tmp", "r")
++	machine = f:read("*line")
++	f:close()
+ 	-- Special handling on mac os where xcode needs special flags.
+ 	if os.istarget("macosx") then
+ 		if string.find(machine, "arm64") then
+@@ -380,7 +376,7 @@ function project_set_build_flags()
  
  		if os.istarget("linux") or os.istarget("bsd") then
  			if _OPTIONS["prefer-local-libs"] then
