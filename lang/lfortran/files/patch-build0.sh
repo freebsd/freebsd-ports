a744b2e10698c64ba1bd8fa9b3b7bf562f3148e7
Thierry Thomas <thierry@FreeBSD.org>
2025-01-18 20:08:43 +0100

    lang/lfortran: upgrade to v0.44.0
    Releases notes at https://github.com/lfortran/lfortran/releases

--- lang/lfortran/files/patch-build0.sh
+++ lang/lfortran/files/patch-build0.sh
@@ -1,7 +1,7 @@
---- build0.sh.orig	2024-01-28 14:28:08 UTC
+--- build0.sh.orig	2025-01-17 18:59:54 UTC
 +++ build0.sh
-@@ -4,7 +4,7 @@ set -x
- set -x
+@@ -7,7 +7,7 @@ BISON=${BISON:-bison}
+ BISON=${BISON:-bison}
  
  # Generate the `version` file
 -ci/version.sh
@@ -9,12 +9,12 @@
  
  # Generate a Fortran AST from AST.asdl (C++)
  python src/libasr/asdl_cpp.py grammar/AST.asdl src/lfortran/ast.h
-@@ -18,7 +18,7 @@ python src/libasr/intrinsic_func_registry_util_gen.py
+@@ -21,7 +21,7 @@ python src/libasr/intrinsic_func_registry_util_gen.py
  # Generate the tokenizer and parser
- (cd src/lfortran && re2c -W -b parser/tokenizer.re -o parser/tokenizer.cpp)
- (cd src/lfortran && re2c -W -b parser/preprocessor.re -o parser/preprocessor.cpp)
--(cd src/lfortran/parser && bison -Wall -d -r all parser.yy)
-+(cd src/lfortran/parser && bison -Wall -d -v -r all parser.yy)
+ (cd src/lfortran && ${RE2C} -W -b parser/tokenizer.re -o parser/tokenizer.cpp)
+ (cd src/lfortran && ${RE2C} -W -b parser/preprocessor.re -o parser/preprocessor.cpp)
+-(cd src/lfortran/parser && ${BISON} -Wall -d -r all parser.yy)
++(cd src/lfortran/parser && ${BISON} -Wall -d -v -r all parser.yy)
  
  grep -n "'" src/lfortran/parser/parser.yy && echo "Single quote not allowed" && exit 1
  echo "OK"
