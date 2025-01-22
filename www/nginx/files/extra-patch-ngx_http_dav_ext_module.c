87fca379df1fc42dd50ac3a6d6eabf801f7ae38d
Bernhard Froehlich <decke@FreeBSD.org>
2025-01-22 09:40:48 +0100

    www/nginx: Fix handling for hidden files and folders in nginx-dav-ext-module
    PR:		283748
    Approved by:	joneum@
    Obtained from:	https://github.com/arut/nginx-dav-ext-module/pull/62

--- www/nginx/files/extra-patch-ngx_http_dav_ext_module.c
+++ www/nginx/files/extra-patch-ngx_http_dav_ext_module.c
@@ -1,5 +1,14 @@
---- ../nginx-dav-ext-module-3.0.0/ngx_http_dav_ext_module.c	2018-12-17 11:45:12.000000000 +0300
-+++ ../nginx-dav-ext-module-3.0.0/ngx_http_dav_ext_module.c	2020-03-13 01:20:47.498199000 +0300
+--- ../nginx-dav-ext-module-3.0.0/ngx_http_dav_ext_module.c    2018-12-17 11:45:12.000000000 +0300
++++ ../nginx-dav-ext-module-3.0.0/ngx_http_dav_ext_module.c    2020-03-13 01:20:47.498199000 +0300
+@@ -862,7 +862,7 @@
+         name.len = ngx_de_namelen(&dir);
+         name.data = ngx_de_name(&dir);
+ 
+-        if (name.data[0] == '.') {
++        if ((name.len == 1 && name.data[0] == '.') || (name.len == 2 && name.data[0] == '.' && name.data[1] == '.')) {
+             continue;
+         }
+ 
 @@ -896,10 +896,9 @@
              ngx_cpystrn(last, name.data, name.len + 1);
  
