59f3b797be231de7ed2800bd1f59556b9517fc43
antonfb <antonfb@hesiod.org>
2025-01-25 18:26:37 +0100

    devel/py-p4python: Update to 2024.2.2682690
    Changelog: https://github.com/perforce/p4python/blob/2024.2.0/RELNOTES.txt
    
    PR:		283668

--- devel/py-p4python/files/patch-P4API.cpp
+++ devel/py-p4python/files/patch-P4API.cpp
@@ -1,6 +1,6 @@
---- P4API.cpp.orig	2020-07-15 12:24:50 UTC
+--- P4API.cpp.orig	2024-12-28 20:29:18 UTC
 +++ P4API.cpp
-@@ -337,7 +337,7 @@ static PyObject * P4API_dvcs_init(P4Adapter * self, Py
+@@ -338,7 +338,7 @@ static PyObject * P4API_dvcs_init(P4Adapter * self, Py
              return NULL;
  
  
@@ -9,7 +9,7 @@
  
      if( personalServer.get() == NULL)
  	return NULL;
-@@ -399,7 +399,7 @@ static PyObject * P4API_dvcs_clone(P4Adapter * self, P
+@@ -400,7 +400,7 @@ static PyObject * P4API_dvcs_clone(P4Adapter * self, P
  					 &progress))
  	return NULL;
  
--- devel/py-p4python/files/patch-setup.py
+++ devel/py-p4python/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2023-04-04 23:31:58 UTC
+--- setup.py.orig	2024-12-30 02:45:50 UTC
 +++ setup.py
-@@ -243,7 +243,7 @@ class p4build_ext(build_ext_module):
+@@ -245,7 +245,7 @@ class p4build_ext(build_ext_module):
                      pathToFile = os.path.join(p, "openssl")
                      if os.path.exists(pathToFile) and os.access(pathToFile, os.X_OK):
                          entry = subprocess.check_output("ldd {0} | grep libssl".format(pathToFile),
@@ -9,7 +9,7 @@
                          if entry is not False:
                              libpath = os.path.dirname(entry.split()[2])
  
-@@ -275,7 +275,7 @@ class p4build_ext(build_ext_module):
+@@ -277,23 +277,10 @@ class p4build_ext(build_ext_module):
  
          ssl_ver = ""
          if not p4_ssl_dir:
@@ -18,7 +18,23 @@
                  # check for a version of SSL already installed via 'openssl version'
                  self.ssl, ssl_ver = self.check_installed_ssl()  # return libpath or None
  
-@@ -313,7 +313,7 @@ class p4build_ext(build_ext_module):
+-                # we only support 1.0.2 or 1.1.1 using 2019.1 p4api
+-                if not (("1.0.2" in ssl_ver) or ("1.1.1" in ssl_ver) or ("3.0" in ssl_ver)):
+-                    self.ssl = ""
+-
+-                if not self.ssl:
+-                    # try downloading and building ssl
+-                    if self.is_super():
+-                        (self.ssl, ssl_src, ssl_tarball, loaded_ssl_from_ftp) = self.build_ssl_lib(ssl_ver)
+-                        p4_ssl_dir = self.ssl
+-                        p4_ssl_ver = ssl_ver
+-                    else:
+-                        print("must be root to build and install SSL")
+-
+         if not self.ssl:
+             print("***********************************************", file=sys.stderr)
+             print("** Cannot build P4Python without SSL support **", file=sys.stderr)
+@@ -315,7 +302,7 @@ class p4build_ext(build_ext_module):
          p4_api_dir = self.apidir
  
          try:
@@ -27,7 +43,7 @@
              releaseVersion = VersionInfo(".")
          except IOError:
              print("Cannot find Version file in API dir {0}.".format(p4_api_dir))
-@@ -334,9 +334,18 @@ class p4build_ext(build_ext_module):
+@@ -336,9 +323,18 @@ class p4build_ext(build_ext_module):
          #spawn._nt_quote_args = monkey_nt_quote_args
  
          # add the paths for p4 headers and library
@@ -48,7 +64,7 @@
          # check if the interpreter is mayapy.exe
          namedir = os.path.dirname(os.path.dirname(sys.executable))
          if "maya" in namedir.lower():
-@@ -469,7 +478,7 @@ def cleanup_api(api_dir, tarball):
+@@ -471,7 +467,7 @@ def cleanup_api(api_dir, tarball):
  
  
  # declare some evil globals
