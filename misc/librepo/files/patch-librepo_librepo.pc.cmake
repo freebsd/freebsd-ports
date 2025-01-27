8577bce98c298bf90e24ab6e14bd4e17bf37c42b
Yuri Victorovich <yuri@FreeBSD.org>
2025-01-27 08:01:04 -0700

    misc/librepo: update 1.17.1 → 1.19.0

--- misc/librepo/files/patch-librepo_librepo.pc.cmake
+++ misc/librepo/files/patch-librepo_librepo.pc.cmake
@@ -1,11 +1,11 @@
---- librepo/librepo.pc.cmake.orig	2020-09-20 19:51:54 UTC
+--- librepo/librepo.pc.cmake.orig	2024-11-04 16:54:08 UTC
 +++ librepo/librepo.pc.cmake
-@@ -6,7 +6,7 @@ Name: librepo
+@@ -6,7 +6,7 @@ Requires: glib-2.0
  Description: Repodata downloading library.
  Version: @VERSION@
  Requires: glib-2.0
--Requires.private: libcurl openssl libxml-2.0
-+Requires.private: libcurl libxml-2.0
+-Requires.private: libcurl openssl libxml-2.0 @PKGCONF_DEPENDENCY_ZCK@
++Requires.private: libcurl libxml-2.0 @PKGCONF_DEPENDENCY_ZCK@
  Libs: -L${libdir} -lrepo
  Libs.private: -lgpgme -lgpg-error
  Cflags: -I${includedir} -D_FILE_OFFSET_BITS=64
