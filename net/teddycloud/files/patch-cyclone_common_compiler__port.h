f30264ca1d6d7e1af256325834a9f976e00a11d9
Michael Pape <freebsd@pe82.de>
2025-01-31 16:43:13 +0300

    net/teddycloud: Update 0.6.2 → 0.6.3, update supported architectures
    Changelog:
    https://github.com/toniebox-reverse-engineering/teddycloud/releases/tag/tc_v0.6.3
    
    PR:	284350

--- net/teddycloud/files/patch-cyclone_common_compiler__port.h
+++ net/teddycloud/files/patch-cyclone_common_compiler__port.h
@@ -1,11 +1,13 @@
 --- cyclone/common/compiler_port.h.orig	2024-01-17 21:26:40 UTC
 +++ cyclone/common/compiler_port.h
-@@ -106,7 +106,7 @@ typedef unsigned int uint_t;
+@@ -105,8 +105,8 @@ typedef unsigned int uint_t;
+    #define PRIuSIZE "u"
     #define PRIXSIZE "X"
     #define PRIuTIME "lu"
- //Linux GCC compiler
+-//Linux GCC compiler
 -#elif defined(__linux__)
-+#elif defined(__linux__) || defined(__unix__)
++//Linux/FreeBSD GCC compiler
++#elif defined(__linux__) || defined(__FreeBSD__)
     #define PRIuSIZE "zu"
     #define PRIXSIZE "zX"
     #define PRIuTIME "lu"
