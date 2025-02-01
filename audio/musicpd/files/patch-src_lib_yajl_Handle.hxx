20636a0f9f8963e6d12443c4be5f8cbd359bb5e6
Thomas Zander <riggs@FreeBSD.org>
2025-02-01 09:22:50 +0100

    audio/musicpd: Update to upstream release 0.23.17
    Details:
    - Mostly bugfixes, see
      https://raw.githubusercontent.com/MusicPlayerDaemon/MPD/v0.23.17/NEWS
    
    MFH:		2025Q1
    (cherry picked from commit 273a498b534c1ebc84f550e52abf2f00ba21d5a3)

---
+++ audio/musicpd/files/patch-src_lib_yajl_Handle.hxx
@@ -0,0 +1,11 @@
+--- src/lib/yajl/Handle.hxx.orig	2025-01-29 16:11:53 UTC
++++ src/lib/yajl/Handle.hxx
+@@ -29,7 +29,7 @@
+ 
+ #pragma once
+ 
+-#include <yajl_parse.h>
++#include <yajl/yajl_parse.h>
+ 
+ #include <utility>
+ 
