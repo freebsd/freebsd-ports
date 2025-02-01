873f38c4b378ef250988267cca676b65e525083c
Jason E. Hale <jhale@FreeBSD.org>
2025-02-01 11:03:27 -0600

    Qt6: Update to 6.8.2
    January 31, 2025
    
    We have released Qt 6.8.2 today. As a patch release, Qt 6.8.2 does not
    introduce new features but contains more than 450 bug fixes, security
    updates, and other improvements on top of Qt 6.8.1 release. See more
    information about the most important changes and bug fixes from the
    Qt 6.8.2 release note.
    
    Announcement:
    https://www.qt.io/blog/qt-6.8.2-released
    
    Release note:
    https://code.qt.io/cgit/qt/qtreleasenotes.git/about/qt/6.8.2/release-note.md
    
    MFH:		2025Q1
    Security:	72b8729e-e134-11ef-9e76-4ccc6adda413
    (cherry picked from commit 8a35ffe0dbe55b3c27d3ec722aa7444bce1dcab0)

--- Mk/Uses/qt.mk
+++ Mk/Uses/qt.mk
@@ -23,7 +23,7 @@ _QT_MK_INCLUDED=	qt.mk
 # Qt versions currently supported by the framework.
 _QT_SUPPORTED?=		5 6
 QT5_VERSION?=		5.15.16
-QT6_VERSION?=		6.8.1
+QT6_VERSION?=		6.8.2
 PYSIDE6_VERSION?=	6.8.1.1
 
 # Support for intermediate Qt6 releases. This partially defines
