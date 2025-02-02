07cc225b4106ac0eb09b92ff4e9ebf6d16643817
Namkhai Bourquin <nk@nkeor.me>
2025-02-02 21:21:55 +0100

    net-mgmt/netdata: Update to 2.2.3
    Changelog: https://github.com/netdata/netdata/releases/tag/v2.2.3
    
    Thanks to Namkhai and contributors for the time consuming effort to
    make it suitable for packaging and testing
    
    PR:		275792

---
+++ net-mgmt/netdata/files/patch-packaging_cmake_Modules_NetdataVersion.cmake
@@ -0,0 +1,40 @@
+--- packaging/cmake/Modules/NetdataVersion.cmake.orig	2025-01-02 17:07:16 UTC
++++ packaging/cmake/Modules/NetdataVersion.cmake
+@@ -7,22 +7,23 @@ function(netdata_version)
+ # packaging/version. This version field are used for cmake's project,
+ # cpack's packaging, and the agent's functionality.
+ function(netdata_version)
+-        find_package(Git)
++        #find_package(Git)
+ 
+-        if(GIT_EXECUTABLE)
+-                execute_process(COMMAND ${GIT_EXECUTABLE} describe
+-                                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+-                                RESULT_VARIABLE GIT_DESCRIBE_RESULT
+-                                OUTPUT_VARIABLE GIT_DESCRIBE_OUTPUT)
+-                if(GIT_DESCRIBE_RESULT)
+-                        file(STRINGS "${CMAKE_SOURCE_DIR}/packaging/version" GIT_DESCRIBE_OUTPUT)
+-                        message(WARNING "using version from packaging/version: '${GIT_DESCRIBE_OUTPUT}'")
+-                endif()
+-        else()
+-                file(STRINGS packaging/version GIT_DESCRIBE_OUTPUT)
+-                message(WARNING "using version from packaging/version: '${GIT_DESCRIBE_OUTPUT}'")
+-        endif()
++        #if(GIT_EXECUTABLE)
++        #        execute_process(COMMAND ${GIT_EXECUTABLE} describe
++        #                        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
++        #                        RESULT_VARIABLE GIT_DESCRIBE_RESULT
++        #                        OUTPUT_VARIABLE GIT_DESCRIBE_OUTPUT)
++        #        if(GIT_DESCRIBE_RESULT)
++        #                file(STRINGS "${CMAKE_SOURCE_DIR}/packaging/version" GIT_DESCRIBE_OUTPUT)
++        #                message(WARNING "using version from packaging/version: '${GIT_DESCRIBE_OUTPUT}'")
++        #        endif()
++        #else()
++        #        file(STRINGS packaging/version GIT_DESCRIBE_OUTPUT)
++        #        message(WARNING "using version from packaging/version: '${GIT_DESCRIBE_OUTPUT}'")
++        #endif()
+ 
++        set(GIT_DESCRIBE_OUTPUT "v%%DISTVERSION%%")
+         string(STRIP ${GIT_DESCRIBE_OUTPUT} GIT_DESCRIBE_OUTPUT)
+         set(NETDATA_VERSION_STRING "${GIT_DESCRIBE_OUTPUT}" PARENT_SCOPE)
+ 
