e3b01acaf20f54b0de41281577a40aae38f35403
Nuno Teixeira <eduardo@FreeBSD.org>
2025-02-02 21:19:11 +0000

    devel/rgbds: Update to 0.9.1
    ChangeLog: https://github.com/gbdev/rgbds/releases/tag/v0.9.1

--- devel/rgbds/files/patch-test_run-tests.sh
+++ devel/rgbds/files/patch-test_run-tests.sh
@@ -1,14 +1,14 @@
 Do not run extra tests that require build
 
---- test/run-tests.sh.orig	2024-12-31 01:19:31 UTC
+--- test/run-tests.sh.orig	2025-02-02 21:10:40 UTC
 +++ test/run-tests.sh
-@@ -74,6 +74,6 @@ fi
+@@ -119,6 +119,6 @@ fi
  	test_downstream pret  pokered          compare pokered.gbc     ea9bcae617fdf159b045185467ae58b2e4a48b9a
  	test_downstream zladx LADX-Disassembly default azle.gbc        d90ac17e9bf17b6c61624ad9f05447bdb5efc01a
  fi
--test_downstream AntonioND ucity   all      ucity.gbc 67e67a4eaefb12ddc98d3caa8959102b885c0405
--test_downstream pinobatch libbet  all      libbet.gb 7001af0f0664ba4e941310cde2c4c8392423abd2
+-test_downstream AntonioND ucity   all      ucity.gbc d2f4a7db48ee208b1bd69a78bd492a1c9ac4a030
+-test_downstream pinobatch libbet  all      libbet.gb f117089aa056600e2d404bbcbac96b016fc64611
 -test_downstream LIJI32    SameBoy bootroms build/bin/BootROMs/cgb_boot.bin 113903775a9d34b798c2f8076672da6626815a91
-+#test_downstream AntonioND ucity   all      ucity.gbc 67e67a4eaefb12ddc98d3caa8959102b885c0405
-+#test_downstream pinobatch libbet  all      libbet.gb 7001af0f0664ba4e941310cde2c4c8392423abd2
++#test_downstream AntonioND ucity   all      ucity.gbc d2f4a7db48ee208b1bd69a78bd492a1c9ac4a030
++#test_downstream pinobatch libbet  all      libbet.gb f117089aa056600e2d404bbcbac96b016fc64611
 +#test_downstream LIJI32    SameBoy bootroms build/bin/BootROMs/cgb_boot.bin 113903775a9d34b798c2f8076672da6626815a91
