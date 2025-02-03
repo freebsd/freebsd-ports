174a164cafb86519b1dd9397af355aa0efa3a5ef
Doug Rabson <dfr@FreeBSD.org>
2025-02-03 16:41:02 +0000

    sysutils/podman: update to 5.3.2
    This also changes the image path in pkg-message to
    quay.io/dougrabson/hello to avoid rate limiting.

--- sysutils/podman/files/patch-libpod_container__inspect__freebsd.go
+++ sysutils/podman/files/patch-libpod_container__inspect__freebsd.go
@@ -1,6 +1,6 @@
---- libpod/container_inspect_freebsd.go.orig	2024-06-04 19:54:07 UTC
+--- libpod/container_inspect_freebsd.go.orig	2025-01-21 18:41:34 UTC
 +++ libpod/container_inspect_freebsd.go
-@@ -15,5 +15,14 @@ func (c *Container) platformInspectContainerHostConfig
+@@ -15,10 +15,18 @@ func (c *Container) platformInspectContainerHostConfig
  	// UTS namespace mode
  	hostConfig.UTSMode = c.NamespaceMode(spec.UTSNamespace, ctrSpec)
  
@@ -13,5 +13,10 @@
 +               return err
 +       }
 +
- 	return nil
- }
+ 	// Devices
+ 	// Do not include if privileged - assumed that all devices will be
+ 	// included.
+-	var err error
+ 	hostConfig.Devices, err = c.GetDevices(hostConfig.Privileged, *ctrSpec, map[string]string{})
+ 	if err != nil {
+ 		return err
