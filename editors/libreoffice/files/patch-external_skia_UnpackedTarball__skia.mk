6ffd18ecefc73a041332a8c824a2009f2bf88016
Dima Panov <fluffy@FreeBSD.org>
2025-02-07 00:31:38 +0300

    editors/libreoffice: update to 25.2.0 release (+)
    New big update! LibreOffice 25.2 is here, with:
    
    ->️Better change tracking
    -> More versatile comments
    -> Extra theme support
    
    ...plus compatibility improvements, extra privacy options, and more.
    
    Release Notes:	https://blog.documentfoundation.org/blog/2025/02/06/libreoffice-25-2/
    Changelog:	https://wiki.documentfoundation.org/ReleaseNotes/25.2
    Sponsored by:	Future Crew, LLC

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,10 @@
---- external/skia/UnpackedTarball_skia.mk.orig	2024-06-11 10:45:33 UTC
+--- external/skia/UnpackedTarball_skia.mk.orig	2024-11-22 20:05:31 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -43,6 +43,7 @@ skia_patches := \
-     0001-AvoidCombiningExtrememelyLargeMeshes.patch.1 \
-     sort-comparison-assumption.patch.0 \
+@@ -41,6 +41,8 @@ skia_patches := \
      help-msvc-analyzer.patch \
+     always_inline_and_multiversioning_conflict.patch.1 \
+     windows-define-conflict.patch.1 \
++    freebsd-limits.h.patch.0 \
 +    powerpc64le-skia.patch.0 \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
