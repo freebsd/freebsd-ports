9f4a0cfc5a8e658daf86515686cdb088fe3c4207
Yuri Victorovich <yuri@FreeBSD.org>
2025-02-10 06:59:33 -0700

    science/seacas: Fix build with science/netcdf 4.9.3
    Reported by:	fallout

---
+++ science/seacas/files/patch-packages_seacas_libraries_exodus_src_ex__put__prop__names.c
@@ -0,0 +1,13 @@
+- fix build with science/netcdf 4.9.3
+
+--- packages/seacas/libraries/exodus/src/ex_put_prop_names.c.orig	2025-02-10 20:32:48 UTC
++++ packages/seacas/libraries/exodus/src/ex_put_prop_names.c
+@@ -172,7 +172,7 @@ int ex_put_prop_names(int exoid, ex_entity_type obj_ty
+ 
+     /*   create attribute to cause variable to fill with zeros per routine spec
+      */
+-    if ((status = nc_put_att_longlong(exoid, propid, _FillValue, int_type, 1, vals)) != NC_NOERR) {
++    if ((status = nc_put_att_longlong(exoid, propid, NC_FillValue, int_type, 1, vals)) != NC_NOERR) {
+       snprintf(errmsg, MAX_ERR_LENGTH,
+                "ERROR: failed to create property name fill attribute in file id %d", exoid);
+       ex_err_fn(exoid, __func__, errmsg, status);
