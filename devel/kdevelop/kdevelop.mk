3d21de7a7cb5e96deb20021af04435103806b3ac
Max Brazhnikov <makc@FreeBSD.org>
2025-02-13 23:45:10 +0300

    devel/kdev*: share project version between KDevelop ports
    and do not hardcode KDE Gear version to ease updates.

---
+++ devel/kdevelop/kdevelop.mk
@@ -0,0 +1,11 @@
+# Common variables for KDevelop ports:
+# devel/kdev-php
+# devel/kdev-python
+# devel/kdevelop
+
+_kdevelop_PROJECT_VERSION=	6.1
+PLATFORM_VER=	${_kdevelop_PROJECT_VERSION:S/.//}
+PLIST_SUB+=	PLATFORM_VER="${PLATFORM_VER}"
+
+PRIVATE_VER=	6.0
+PLIST_SUB+=	PRIVATE_VER=${PRIVATE_VER}
