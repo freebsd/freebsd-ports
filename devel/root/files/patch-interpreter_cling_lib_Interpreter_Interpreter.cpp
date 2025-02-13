2f6ec23a698337ef12692b0db4ad775bf33dc962
Erik Jensen <erik@tenku.dk>
2025-02-13 18:28:23 +0000

    devel/root: Update to 6.34/02
    - Mark broken on i386
    
    ChangeLog:	https://root.cern/doc/v634/release-notes.html
    PR:		284139

--- devel/root/files/patch-interpreter_cling_lib_Interpreter_Interpreter.cpp
+++ devel/root/files/patch-interpreter_cling_lib_Interpreter_Interpreter.cpp
@@ -1,6 +1,6 @@
---- interpreter/cling/lib/Interpreter/Interpreter.cpp.orig	2024-05-30 18:23:48 UTC
+--- interpreter/cling/lib/Interpreter/Interpreter.cpp.orig	2024-11-30 13:04:19 UTC
 +++ interpreter/cling/lib/Interpreter/Interpreter.cpp
-@@ -465,8 +465,17 @@ namespace cling {
+@@ -458,8 +458,17 @@ namespace cling {
      const char* Attr = LangOpts.CPlusPlus ? " throw () " : "";
  #else
      const char* LinkageCxx = Linkage;
