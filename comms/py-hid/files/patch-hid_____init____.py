f3dc37b61ee99def8887e1b98bbbc09e2acafc41
Wen Heping <wen@FreeBSD.org>
2025-02-13 08:36:20 +0000

    comms/py-hid: Update to 1.0.6
    Update pkg-descr
    Pass maintainership to submitter
    
    PR:		283732
    Reported by:	nxjoseph@protonmail.com

--- comms/py-hid/files/patch-hid_____init____.py
+++ comms/py-hid/files/patch-hid_____init____.py
@@ -1,6 +1,6 @@
---- hid/__init__.py.orig	2024-04-11 03:39:41 UTC
+--- hid/__init__.py.orig	2025-02-13 08:24:09 UTC
 +++ hid/__init__.py
-@@ -7,6 +7,7 @@ library_paths = (
+@@ -8,6 +8,7 @@ library_paths = (
  
  hidapi = None
  library_paths = (
