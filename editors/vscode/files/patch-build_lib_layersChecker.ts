def21c840b35ae1114cbfe867debe0bdfaab5392
Hiroki Tagato <tagattie@FreeBSD.org>
2025-02-15 14:24:09 +0900

    editors/vscode: update to 1.97.1
    Changelog: https://code.visualstudio.com/updates/v1_97
    
    Reported by:	GitHub (watch releases)
    MFH:		2025Q1
    Security:	cbf5d976-656b-4bb6-805f-3af038e2de3e

--- editors/vscode/files/patch-build_lib_layersChecker.ts
+++ editors/vscode/files/patch-build_lib_layersChecker.ts
@@ -1,6 +1,6 @@
---- build/lib/layersChecker.ts.orig	2024-12-10 22:22:47 UTC
+--- build/lib/layersChecker.ts.orig	2025-02-04 18:19:16 UTC
 +++ build/lib/layersChecker.ts
-@@ -406,7 +406,7 @@ function createProgram(tsconfigPath: string): ts.Progr
+@@ -407,7 +407,7 @@ function createProgram(tsconfigPath: string): ts.Progr
  function createProgram(tsconfigPath: string): ts.Program {
  	const tsConfig = ts.readConfigFile(tsconfigPath, ts.sys.readFile);
  
--- editors/vscode/files/patch-src_bootstrap-node.ts
+++ editors/vscode/files/patch-src_bootstrap-node.ts
@@ -1,6 +1,6 @@
---- src/bootstrap-node.ts.orig	2024-10-31 05:10:14 UTC
+--- src/bootstrap-node.ts.orig	2025-02-10 13:07:56 UTC
 +++ src/bootstrap-node.ts
-@@ -122,7 +122,7 @@ export function configurePortable(product: Partial<IPr
+@@ -152,7 +152,7 @@ export function configurePortable(product: Partial<IPr
  			return process.env['VSCODE_PORTABLE'];
  		}
  
