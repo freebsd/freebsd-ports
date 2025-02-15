a4ab8de0692a1f9e2b7722d9cb08ecf2b99af617
Hiroki Tagato <tagattie@FreeBSD.org>
2025-02-15 14:24:09 +0900

    editors/vscode: update to 1.97.1
    Changelog: https://code.visualstudio.com/updates/v1_97
    
    Reported by:	GitHub (watch releases)
    MFH:		2025Q1
    Security:	cbf5d976-656b-4bb6-805f-3af038e2de3e
    
    (cherry picked from commit def21c840b35ae1114cbfe867debe0bdfaab5392)

--- editors/vscode/files/patch-src_vs_code_electron-main_app.ts
+++ editors/vscode/files/patch-src_vs_code_electron-main_app.ts
@@ -1,6 +1,6 @@
---- src/vs/code/electron-main/app.ts.orig	2024-12-10 22:22:47 UTC
+--- src/vs/code/electron-main/app.ts.orig	2025-02-04 18:19:16 UTC
 +++ src/vs/code/electron-main/app.ts
-@@ -1000,7 +1000,7 @@ export class CodeApplication extends Disposable {
+@@ -1008,7 +1008,7 @@ export class CodeApplication extends Disposable {
  				services.set(IUpdateService, new SyncDescriptor(Win32UpdateService));
  				break;
  
--- editors/vscode/files/patch-src_vs_workbench_services_actions_common_menusExtensionPoint.ts
+++ editors/vscode/files/patch-src_vs_workbench_services_actions_common_menusExtensionPoint.ts
@@ -1,6 +1,6 @@
---- src/vs/workbench/services/actions/common/menusExtensionPoint.ts.orig	2024-12-10 22:22:47 UTC
+--- src/vs/workbench/services/actions/common/menusExtensionPoint.ts.orig	2025-02-04 18:19:16 UTC
 +++ src/vs/workbench/services/actions/common/menusExtensionPoint.ts
-@@ -1135,7 +1135,7 @@ class CommandsTableRenderer extends Disposable impleme
+@@ -1141,7 +1141,7 @@ class CommandsTableRenderer extends Disposable impleme
  
  		switch (platform) {
  			case 'win32': key = rawKeyBinding.win; break;
