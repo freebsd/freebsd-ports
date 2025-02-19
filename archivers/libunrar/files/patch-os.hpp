96e53c0c65faa691e18c0a5558d8f445e6010a70
Jason E. Hale <jhale@FreeBSD.org>
2025-02-19 02:09:07 -0500

    archivers/libunrar: Support crc and crypto flags on aarch64
    If CPU flags crc or crytpo are set, e.g. -march=armv8-a+crc+crypto, build
    would fail with missing headers because this was only implemented for
    glibc and Apple.

--- archivers/libunrar/files/patch-os.hpp
+++ archivers/libunrar/files/patch-os.hpp
@@ -1,13 +1,28 @@
---- os.hpp.orig	2024-05-12 10:19:01 UTC
+--- os.hpp.orig	2025-02-12 14:05:27 UTC
 +++ os.hpp
-@@ -166,6 +166,10 @@
- #endif
- #endif
+@@ -158,10 +158,13 @@
  
+ #if defined(__aarch64__) && (defined(__ARM_FEATURE_CRYPTO) || defined(__ARM_FEATURE_CRC32))
+ #include <arm_neon.h>
+-#ifndef _APPLE
++#if !defined(_APPLE) && !defined(__FreeBSD__)
+ #include <sys/auxv.h>
+ #include <asm/hwcap.h>
+ #endif
++#ifdef __FreeBSD__
++#include <machine/armreg.h>
++#endif
+ #ifdef __ARM_FEATURE_CRYPTO
+ #define USE_NEON_AES
+ #endif
+@@ -169,6 +172,10 @@
+ #define USE_NEON_CRC32
+ #endif
+ #endif
++
 +#if defined(OPENSSL_AES)
 +#include <openssl/evp.h>
 +#endif // OPENSSL_AES
-+
+ 
  #ifdef  S_IFLNK
  #define SAVE_LINKS
- #endif
