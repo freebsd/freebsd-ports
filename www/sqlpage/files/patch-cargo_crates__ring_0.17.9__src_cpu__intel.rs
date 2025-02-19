cc6e9fdbafe898733877f1e6289cd27ed44b3b0a
Palle Girgensohn <girgen@FreeBSD.org>
2025-02-19 10:10:17 +0100

    www/sqlpage: Update to 0.33.0
    Release notes:	https://github.com/sqlpage/SQLPage/releases/tag/v0.33.0
    
    Migrate i386 patch per suggestion from upstreams author:
    https://github.com/briansmith/ring/issues/1999#issuecomment-2658023589

---
+++ www/sqlpage/files/patch-cargo_crates__ring_0.17.9__src_cpu__intel.rs
@@ -0,0 +1,11 @@
+--- cargo-crates/ring-0.17.9/src/cpu/intel.rs.orig	2025-02-18 22:59:16.393822000 +0100
++++ cargo-crates/ring-0.17.9/src/cpu/intel.rs	2025-02-18 22:59:40.189467000 +0100
+@@ -156,8 +156,6 @@
+     // CMOV, it is likely that some of our timing side channel prevention does
+     // not work. Presumably the people who delete these are verifying that it
+     // all works fine.
+-    const _SSE_REQUIRED: () = assert!(cfg!(target_feature = "sse"));
+-    const _SSE2_REQUIRED: () = assert!(cfg!(target_feature = "sse2"));
+ 
+     #[cfg(all(target_arch = "x86", not(target_feature = "sse2")))]
+     {
