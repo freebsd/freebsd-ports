37ce58edb91a6fb06efa1c847ed8710f988d39c4
Cy Schubert <cy@FreeBSD.org>
2025-02-18 19:43:15 -0800

    net/ntp: Replace the workaround from 98e34e8e2557 with a patch from upstream
    43537eb9c3e5 circumvented an upstream patch which caused an IPv6
    pool regresson. This patch removes the circumvention and replaces
    it with an upstream patch planned for the new release of ntp.
    
    Obtained from:	src bc02e6558720
    MFH:		2025Q1

--- net/ntp/files/patch-ntpd_ntp__io.c
+++ net/ntp/files/patch-ntpd_ntp__io.c
@@ -1,6 +1,17 @@
 --- ntpd/ntp_io.c.orig	2024-05-07 04:21:17.000000000 -0700
-+++ ntpd/ntp_io.c	2025-02-18 19:31:18.434141000 -0800
-@@ -3205,7 +3205,7 @@
++++ ntpd/ntp_io.c	2025-02-18 19:38:16.412031000 -0800
+@@ -1486,9 +1486,7 @@
+ 
+ 	if (IS_IPV6(psau)) {
+ 		p6addr = &psau->sa6.sin6_addr;
+-		if (   IN6_IS_ADDR_LINKLOCAL(p6addr)
+-		    || IN6_IS_ADDR_SITELOCAL(p6addr)) {
+-
++		if (IN6_IS_ADDR_LINKLOCAL(p6addr)) {
+ 			return TRUE;
+ 		}
+ 	} else if (IS_IPV4(psau)) {
+@@ -3205,7 +3203,7 @@
  	}
  
  	do {
