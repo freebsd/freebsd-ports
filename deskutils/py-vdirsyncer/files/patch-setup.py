63b71d27df66324fa2ad23bccb21ad97dd82726f
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-02-20 15:58:56 +0800

    deskutils/py-vdirsyncer: Remove deprecated py-atomicwrites from RUN_DEPENDS
    - Bump PORTREVISION for dependency and package change
    
    Obtained from:	https://github.com/pimutils/vdirsyncer/commit/8b063c39cb3e26a2beef5b6beed7e99c9e9641ac

--- deskutils/py-vdirsyncer/files/patch-setup.py
+++ deskutils/py-vdirsyncer/files/patch-setup.py
@@ -1,8 +1,8 @@
 --- setup.py.orig	2024-09-11 15:26:58 UTC
 +++ setup.py
-@@ -19,7 +19,7 @@ requirements = [
-     # https://github.com/untitaker/python-atomicwrites/commit/4d12f23227b6a944ab1d99c507a69fdbc7c9ed6d  # noqa
-     "atomicwrites>=0.1.7",
+@@ -17,7 +17,7 @@ requirements = [
+     "click-log>=0.3.0, <0.5.0",
+     "requests >=2.20.0",
      "aiohttp>=3.8.0,<4.0.0",
 -    "aiostream>=0.4.3,<0.5.0",
 +    "aiostream>=0.4.3",
