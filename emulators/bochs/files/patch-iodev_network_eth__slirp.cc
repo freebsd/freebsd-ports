a613e66a54d54251878412b74c1e99defdac4192
Neel Chauhan <nc@FreeBSD.org>
2025-02-24 20:36:44 -0500

    emulators/bochs: Fix build

--- emulators/bochs/files/patch-iodev_network_eth__slirp.cc
+++ emulators/bochs/files/patch-iodev_network_eth__slirp.cc
@@ -1,5 +1,5 @@
---- ./iodev/network/eth_slirp.cc.orig	2025-02-16 04:15:29.000000000 -0500
-+++ ./iodev/network/eth_slirp.cc	2025-02-18 21:26:22.200515000 -0500
+--- iodev/network/eth_slirp.cc.orig	2025-02-16 09:15:29 UTC
++++ iodev/network/eth_slirp.cc
 @@ -20,6 +20,11 @@
  // eth_slirp.cc  - Bochs port of Qemu's slirp implementation (updated from libslirp 4.8.0)
  // Portion of this software comes with the following license: BSD-3-Clause
