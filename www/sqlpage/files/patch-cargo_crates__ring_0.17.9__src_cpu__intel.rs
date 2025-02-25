082847dca320608f922447b0c01bfd5ea2774d1b
Palle Girgensohn <girgen@FreeBSD.org>
2025-02-25 21:38:37 +0100

    www/sqlpage: Update to 0.33.1
    Release notes:	https://github.com/sqlpage/SQLPage/releases/tag/v0.33.1

--- www/sqlpage/files/patch-cargo_crates__ring_0.17.9__src_cpu__intel.rs
+++ www/sqlpage/files/patch-cargo_crates__ring_0.17.9__src_cpu__intel.rs
@@ -1,5 +1,5 @@
 --- cargo-crates/ring-0.17.9/src/cpu/intel.rs.orig	2025-02-18 22:59:16.393822000 +0100
-+++ cargo-crates/ring-0.17.9/src/cpu/intel.rs	2025-02-18 22:59:40.189467000 +0100
++++ cargo-crates/ring-0.17.11/src/cpu/intel.rs	2025-02-18 22:59:40.189467000 +0100
 @@ -156,8 +156,6 @@
      // CMOV, it is likely that some of our timing side channel prevention does
      // not work. Presumably the people who delete these are verifying that it
