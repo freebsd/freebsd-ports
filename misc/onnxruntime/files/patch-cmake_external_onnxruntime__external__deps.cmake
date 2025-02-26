472282cfecc9a0e371ce7fdc7c7d805809142841
Yuri Victorovich <yuri@FreeBSD.org>
2025-02-25 17:03:44 -0800

    misc/onnxruntime: update 1.18.2 → 1.20.2

--- misc/onnxruntime/files/patch-cmake_external_onnxruntime__external__deps.cmake
+++ misc/onnxruntime/files/patch-cmake_external_onnxruntime__external__deps.cmake
@@ -1,11 +1,11 @@
---- cmake/external/onnxruntime_external_deps.cmake.orig	2024-06-24 02:23:28 UTC
+--- cmake/external/onnxruntime_external_deps.cmake.orig	2025-02-10 16:55:12 UTC
 +++ cmake/external/onnxruntime_external_deps.cmake
-@@ -85,7 +85,7 @@ if (NOT WIN32)
-     google_nsync
+@@ -92,7 +92,7 @@ if (NOT WIN32)
      URL ${DEP_URL_google_nsync}
      URL_HASH SHA1=${DEP_SHA1_google_nsync}
--    FIND_PACKAGE_ARGS NAMES nsync
-+    FIND_PACKAGE_ARGS NAMES nsync_cpp
-     )
- endif()
- list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/external)
+     PATCH_COMMAND ${Patch_EXECUTABLE} --binary --ignore-whitespace -p1 < ${PROJECT_SOURCE_DIR}/patches/nsync/nsync_1.26.0.patch
+-    FIND_PACKAGE_ARGS NAMES nsync unofficial-nsync
++    FIND_PACKAGE_ARGS NAMES nsync_cpp unofficial-nsync
+   )
+   #nsync tests failed on Mac Build
+   set(NSYNC_ENABLE_TESTS OFF CACHE BOOL "" FORCE)
