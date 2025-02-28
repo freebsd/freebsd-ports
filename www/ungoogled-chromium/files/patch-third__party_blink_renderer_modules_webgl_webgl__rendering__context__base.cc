3c6e6c5f8cd988f67da6baa334ebcc39a10d8847
Robert Nagy <rnagy@FreeBSD.org>
2025-02-28 07:11:50 +0100

    www/ungoogled-chromium: update to 133.0.6943.141
    Security:	https://vuxml.freebsd.org/freebsd/a4cb7f9b-f506-11ef-85f3-a8a1599412c6.html
    (cherry picked from commit bda2a09a33856931418dcb5483a3ae83cac93d10)

--- www/ungoogled-chromium/files/patch-third__party_blink_renderer_modules_webgl_webgl__rendering__context__base.cc
+++ www/ungoogled-chromium/files/patch-third__party_blink_renderer_modules_webgl_webgl__rendering__context__base.cc
@@ -1,6 +1,6 @@
---- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc.orig	2025-02-20 09:59:21 UTC
+--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc.orig	2025-02-28 06:11:19 UTC
 +++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
-@@ -6304,7 +6304,7 @@ void WebGLRenderingContextBase::TexImageHelperMediaVid
+@@ -6305,7 +6305,7 @@ void WebGLRenderingContextBase::TexImageHelperMediaVid
    constexpr bool kAllowZeroCopyImages = true;
  #endif
  
