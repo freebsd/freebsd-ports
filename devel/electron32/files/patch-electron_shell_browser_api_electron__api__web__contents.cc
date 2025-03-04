a38d1125b074702e79a7d66e7b2c6b175f1fc002
Hiroki Tagato <tagattie@FreeBSD.org>
2025-03-04 18:49:45 +0900

    devel/electron32: update to 32.3.2
    Changelog:
    - https://github.com/electron/electron/releases/tag/v32.3.1
    - https://github.com/electron/electron/releases/tag/v32.3.2
    
    Reported by:	GitHub (watch releases)
    MFH:		2025Q1
    Security:	f4f3e001-402b-4d6d-8efa-ab11fcf8de2b
    
    (cherry picked from commit 19c8b2eeff26f9afc161af4907cc4445b6816798)

--- devel/electron32/files/patch-electron_shell_browser_api_electron__api__web__contents.cc
+++ devel/electron32/files/patch-electron_shell_browser_api_electron__api__web__contents.cc
@@ -1,4 +1,4 @@
---- electron/shell/browser/api/electron_api_web_contents.cc.orig	2024-11-27 18:42:14 UTC
+--- electron/shell/browser/api/electron_api_web_contents.cc.orig	2025-02-26 12:22:11 UTC
 +++ electron/shell/browser/api/electron_api_web_contents.cc
 @@ -153,11 +153,11 @@
  #include "ui/base/cocoa/defaults_utils.h"
@@ -14,7 +14,7 @@
  #include "ui/gfx/font_render_params.h"
  #endif
  
-@@ -517,7 +517,7 @@ std::optional<base::TimeDelta> GetCursorBlinkInterval(
+@@ -520,7 +520,7 @@ std::optional<base::TimeDelta> GetCursorBlinkInterval(
        ui::TextInsertionCaretBlinkPeriodFromDefaults());
    if (system_value)
      return *system_value;
@@ -23,7 +23,7 @@
    if (auto* linux_ui = ui::LinuxUi::instance())
      return linux_ui->GetCursorBlinkInterval();
  #elif BUILDFLAG(IS_WIN)
-@@ -871,7 +871,7 @@ void WebContents::InitWithSessionAndOptions(
+@@ -874,7 +874,7 @@ void WebContents::InitWithSessionAndOptions(
    accept_languages.pop_back();
    prefs->accept_languages = accept_languages;
  
@@ -32,7 +32,7 @@
    // Update font settings.
    static const gfx::FontRenderParams params(
        gfx::GetFontRenderParams(gfx::FontRenderParamsQuery(), nullptr));
-@@ -2612,7 +2612,7 @@ void WebContents::ForcefullyCrashRenderer() {
+@@ -2619,7 +2619,7 @@ void WebContents::ForcefullyCrashRenderer() {
  
    content::RenderProcessHost* rph = rwh->GetProcess();
    if (rph) {
@@ -41,7 +41,7 @@
      // A generic |CrashDumpHungChildProcess()| is not implemented for Linux.
      // Instead we send an explicit IPC to crash on the renderer's IO thread.
      rph->ForceCrash();
-@@ -3291,7 +3291,7 @@ void WebContents::Focus() {
+@@ -3298,7 +3298,7 @@ void WebContents::Focus() {
  void WebContents::Focus() {
    // Focusing on WebContents does not automatically focus the window on macOS
    // and Linux, do it manually to match the behavior on Windows.
@@ -50,7 +50,7 @@
    if (owner_window())
      owner_window()->Focus(true);
  #endif
-@@ -4152,7 +4152,7 @@ ui::ImageModel WebContents::GetDevToolsWindowIcon() {
+@@ -4159,7 +4159,7 @@ ui::ImageModel WebContents::GetDevToolsWindowIcon() {
  }
  #endif
  
