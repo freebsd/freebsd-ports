99e82a66482284d5b477acca8f5a77b9b3ccc7c6
Dave Cottlehuber <dch@FreeBSD.org>
2025-03-05 07:10:31 +0000

    dns/py-ns1-python: update to 0.23.0
    Sponsored by:	SkunkWerks, GmbH
    Reported by:	portscout

--- dns/py-ns1-python/files/patch-setup.py
+++ dns/py-ns1-python/files/patch-setup.py
@@ -1,12 +1,11 @@
---- setup.py.orig	2022-03-08 12:04:18 UTC
+--- setup.py.orig	2024-12-13 10:23:24 UTC
 +++ setup.py
-@@ -23,8 +23,7 @@ setup(
+@@ -23,8 +23,6 @@ setup(
      url="https://github.com/ns1/ns1-python",
      packages=find_packages(exclude=["tests", "examples"]),
      setup_requires=[
 -        "pytest-runner",
 -        "wheel",
-+        "pytest-runner"
      ],
      tests_require=[
          "pytest",
