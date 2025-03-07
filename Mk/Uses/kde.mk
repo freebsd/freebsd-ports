9cdd0866740cb6bc8010b5a14ee273e8070260f0
Max Brazhnikov <makc@FreeBSD.org>
2025-03-07 21:09:22 +0300

    KDE: Update KDE Gear to 24.12.3
    Announcement: https://kde.org/announcements/gear/24.12.3/

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -108,8 +108,8 @@ KDE_FRAMEWORKS6_BRANCH?=	stable
 
 # Current KDE applications. Update _${PORTNAME}_PROJECT_VERSION for the following ports:
 # devel/kdevelop, games/libkdegames, games/libkmahjongg, graphics/kgraphviewer
-KDE_APPLICATIONS6_VERSION?=	24.12.2
-KDE_APPLICATIONS6_SHLIB_VER?=	6.3.2
+KDE_APPLICATIONS6_VERSION?=	24.12.3
+KDE_APPLICATIONS6_SHLIB_VER?=	6.3.3
 # G as in KDE Gear, and as in "don't make the variable name longer than required".
 KDE_APPLICATIONS6_SHLIB_G_VER?=	${KDE_APPLICATIONS6_VERSION}
 KDE_APPLICATIONS6_BRANCH?=	stable
