6d5ea5b1e6a167cb9759d26df2cf32273be543aa
Dmitry Marakasov <amdmi3@FreeBSD.org>
2025-03-07 22:09:55 +0300

    devel/sdl3: Fix joystick support
    PR:		284919
    Submitted by:	bsdcode@disroot.org

---
+++ devel/sdl3/files/patch-cmake_FindLibUSB.cmake
@@ -0,0 +1,11 @@
+--- cmake/FindLibUSB.cmake.orig	2025-03-04 22:37:06 UTC
++++ cmake/FindLibUSB.cmake
+@@ -10,7 +10,7 @@ find_library(LibUSB_LIBRARY
+ endif()
+ 
+ find_library(LibUSB_LIBRARY
+-  NAMES usb-1.0 libusb-1.0
++  NAMES usb-1.0 libusb-1.0 usb
+   HINTS ${PC_LibUSB_LIBRARY_DIRS}
+ )
+ 
