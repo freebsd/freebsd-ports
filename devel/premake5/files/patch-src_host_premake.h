fa30cdf1ce5c5ecc04ef10efdf2fd9cb0059e821
Martin Filla <freebsd@sysctl.cz>
2025-03-09 19:55:35 +0100

    devel/premake5: Update to 5.0.0-beta5
    Update patches.
    Changelog:
    https://github.com/premake/premake-core/releases/tag/v5.0.0-beta5
    
    PR:		285262
    Approved by:	submitter is maintainer

--- devel/premake5/files/patch-src_host_premake.h
+++ devel/premake5/files/patch-src_host_premake.h
@@ -1,13 +1,13 @@
---- src/host/premake.h.orig	2024-12-11 10:29:35 UTC
+--- src/host/premake.h.orig	2025-03-08 19:35:32 UTC
 +++ src/host/premake.h
-@@ -60,6 +60,10 @@
- #elif defined(__arm__) || defined(__thumb__) || defined(__TARGET_ARCH_ARM) || defined(__TARGET_ARCH_THUMB) || \
-     defined(__ARM) || defined(_M_ARM) || defined(_M_ARM_T) || defined(__ARM_ARCH)
- #define PLATFORM_ARCHITECTURE "ARM"
+@@ -72,6 +72,10 @@
+ #define PLATFORM_ARCHITECTURE "loongarch64"
+ #elif defined(__e2k__)
+ #define PLATFORM_ARCHITECTURE "e2k"
 +#elif defined(__powerpc64__)
 +#define PLATFORM_ARCHITECTURE "PPC64"
 +#elif defined(__powerpc__)
 +#define PLATFORM_ARCHITECTURE "PPC"
+ #elif !defined(RC_INVOKED)
+ #error Unknown architecture detected
  #endif
- 
- /* Pull in platform-specific headers required by built-in functions */
--- devel/premake5/files/patch-tests___tests.lua
+++ devel/premake5/files/patch-tests___tests.lua
@@ -1,12 +1,13 @@
---- tests/_tests.lua.orig	2024-11-14 12:50:13 UTC
+--- tests/_tests.lua.orig	2025-03-08 19:37:23 UTC
 +++ tests/_tests.lua
-@@ -62,8 +62,8 @@ return {
+@@ -62,9 +62,9 @@ return {
  	"base/test_validation.lua",
  
  	-- -- Toolset tests
 -	"tools/test_dotnet.lua",
--	"tools/test_gcc.lua",
 +--	"tools/test_dotnet.lua",
+ 	"tools/test_emcc.lua",
+-	"tools/test_gcc.lua",
 +--	"tools/test_gcc.lua",
  	"tools/test_clang.lua",
 -	"tools/test_msc.lua",
