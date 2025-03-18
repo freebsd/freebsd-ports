5ed1efc8730af4db93a2e608470511d549fe6242
Nicola Vitale <nivit@FreeBSD.org>
2025-03-18 10:59:02 +0100

    misc/py-oterm: Update to 0.9.3
    - Update list of run dependencies
    
    Release changes:
    
    - https://github.com/ggozad/oterm/releases/tag/0.9.2
    - https://github.com/ggozad/oterm/releases/tag/0.9.3
    
    Reported by:	portscout!

--- misc/py-oterm/files/patch-src_oterm_utils.py
+++ misc/py-oterm/files/patch-src_oterm_utils.py
@@ -3,20 +3,20 @@
 @@ -1,4 +1,4 @@
 -import sys
 +import platform
+ from importlib import metadata
  from pathlib import Path
  
- 
-@@ -16,12 +16,13 @@ def get_default_data_dir() -> Path:
+@@ -59,12 +59,13 @@ def get_default_data_dir() -> Path:
      home = Path.home()
  
      system_paths = {
 -        "win32": home / "AppData/Roaming/oterm",
 -        "linux": home / ".local/share/oterm",
 -        "darwin": home / "Library/Application Support/oterm",
-+        "Windows": home / "AppData/Roaming/oterm",
-+        "Linux": home / ".local/share/oterm",
-+        "FreeBSD": home / ".local/share/oterm",
 +        "Darwin": home / "Library/Application Support/oterm",
++        "FreeBSD": home / ".local/share/oterm",
++        "Linux": home / ".local/share/oterm",
++        "Windows": home / "AppData/Roaming/oterm",
      }
  
 -    data_path = system_paths[sys.platform]
