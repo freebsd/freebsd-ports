92fc4ba36cfa5fa37f68228c95295e12a493518d
Christos Chatzaras <chris@cretaforce.gr>
2025-03-19 10:56:43 +0100

    databases/mysql80-{client, server}: Update to 8,0.41
    Bugs Fixed
    
        Incompatible Change: Corruption occurred in a spatial index when an
    update of a geometry with a minimal change in the minimum bounding
    rectangle (MBR) was followed by a delete operation.
    
        When upgrading to this release, it is recommended that you drop any
    spatial indexes beforehand, then re-create them after the upgrade is
    complete. Alternatively, you can drop and re-create such indexes
    immediately following the upgrade, but before making use of any of the
    tables in which they occur. You should also be aware that downgrading to
    any previous version reintroduces the original problem described
    previously.
    
        For more information, see Creating Spatial Indexes. (Bug #36452528)
    
        InnoDB: Concurrently truncating a table while querying the
    Performance Schema sometimes cause MySQL to halt unexpectedly. (Bug
    
        InnoDB: It was possible for an ALTER TABLE operation using the
    INPLACE algorithm on a table containing both a spatial index and an
    auto-increment column to cause corruption or, in debug builds, to
    trigger a debug assert. This was due to the auto-increment column value
    being overwritten in the old records of the spatial index while the new
    record was prepared. (Bug #37189985)
    
        InnoDB: Certain IO buffer serializations triggered an assertion in
    debug builds that caused the system to hang. (Bug #37139618)
    
        InnoDB: Improved InnoDB start up time. (Bug #36880863)
    
        References: This issue is a regression of: Bug #36808732.
    
        InnoDB: An assertion failure was raised when creating a FULLTEXT
    index on a table with an FTS_DOC_ID value greater than 4294967295. (Bug
    
        References: See also: Bug #37387224.
    
        InnoDB: Dropping a primary key, and then adding a new AUTO_INCREMENT
    column as a primary key in descending order using the INPLACE algorithm
    failed.
    
        Our thanks to Shaohua Wang and the team at Alibaba for the
    contribution. (Bug #36658450)
    
        InnoDB: Extending a user tablespace produces file extension redo log
    records (MLOG_FILE_EXTEND), but they were not produced when extending
    the system tablespace. (Bug #36511673)
    
        InnoDB: A DELETE operation on a table with a self referential
    foreign key and full-text index could have triggered an assertion. (Bug
    
        InnoDB: When clearing an AHI index from all buffer pool pages, the
    block state would potentially change to BUF_BLOCK_MEMORY before
    acquiring the block mutex thus causing an unexpected halt. (Bug
    
        InnoDB: Common prefix compression for redo log inserts
    (MLOG_REC_INSERT) was disabled but is now enabled when the versions
    match. (Bug #34946626)
    
        References: This issue is a regression of: Bug #13899.
    
        InnoDB: Virtual column information for a row containing an
    externally stored BLOB was not always logged during an UPDATE operation,
    which sometimes resulted in an Index PRIMARY is corrupted error. (Bug
    
        InnoDB: ON DELETE CASCADE with generated columns containing
    secondary indexes sometimes failed, due to virtual column templates not
    being initialized before deletion.
    
    Full Changelog see:
    - https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-41.html
    - https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-40.html
    
    PR:	285112
    Sponsored by:	Netzkommund GmbH

--- databases/mysql80-server/files/patch-include_my__stacktrace.h
+++ databases/mysql80-server/files/patch-include_my__stacktrace.h
@@ -1,10 +1,10 @@
---- include/my_stacktrace.h.orig	2024-07-12 19:15:25 UTC
+--- include/my_stacktrace.h.orig	2024-12-16 09:20:55 UTC
 +++ include/my_stacktrace.h
-@@ -44,7 +44,7 @@
-   HAVE_BACKTRACE - Linux, FreeBSD, OSX, Solaris
+@@ -45,7 +45,7 @@
    _WIN32 - Windows
+   HAVE_EXT_BACKTRACE - Unixes without backtrace(3)
  */
--#if defined(HAVE_BACKTRACE) || defined(_WIN32)
+-#if defined(HAVE_BACKTRACE) || defined(_WIN32) || defined(HAVE_EXT_BACKTRACE)
 +#if (defined(HAVE_BACKTRACE) || defined(_WIN32)) && (defined(__aarch64__) || defined(__amd64__) || defined(__i386__))
  #define HAVE_STACKTRACE 1
  void my_init_stacktrace();
