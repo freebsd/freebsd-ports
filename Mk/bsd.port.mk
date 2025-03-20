070043800ea320384f9d390a3b677bc262500791
Mathieu Arnold <mat@FreeBSD.org>
2025-03-20 19:50:47 +0100

    Mk: minor style fix bapt forgot to apply

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -5469,10 +5469,9 @@ _STAGE_SEQ=		050:stage-message 100:stage-dir 150:run-depends \
 				860:install-rc-script 870:install-ldconfig-file \
 				880:install-license 890:install-desktop-entries \
 				900:add-plist-info 910:add-plist-docs 920:add-plist-examples \
-				930:add-plist-data 940:add-plist-post ${POST_PLIST:C/^/990:/} \
-				${_OPTIONS_install} ${_USES_install} \
-				${_OPTIONS_stage} ${_USES_stage} ${_FEATURES_stage} \
-				994:stage-sanity
+				930:add-plist-data 940:add-plist-post 994:stage-sanity \
+				${_OPTIONS_install} ${_USES_install} ${POST_PLIST:C/^/990:/} \
+				${_OPTIONS_stage} ${_USES_stage} ${_FEATURES_stage}
 .    if defined(DEVELOPER)
 _STAGE_SEQ+=	995:stage-qa
 .    else
