54a2c18c0acb8d6ca488a50ff018685a4100d4e4
Dima Panov <fluffy@FreeBSD.org>
2025-03-25 00:07:58 +0300

    games/wesnoth: update to 1.18.4 release (+)
    Changelog:	https://github.com/wesnoth/wesnoth/blob/1.18/changelog.md#version-1184
    PR:		285212 (based on)

---
+++ games/wesnoth/files/patch-src_filesystem.hpp
@@ -0,0 +1,10 @@
+--- src/filesystem.hpp.orig	2025-02-17 21:50:08 UTC
++++ src/filesystem.hpp
+@@ -21,6 +21,7 @@
+ #pragma once
+ 
+ #include <ctime>
++#include <cstdint>
+ #include <fstream>
+ #include <iosfwd>
+ #include <memory>
