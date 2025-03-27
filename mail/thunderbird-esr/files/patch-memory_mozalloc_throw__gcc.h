d5989f4c12aba99d1c2e6b35fa1940bebdb0d979
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2025-03-27 20:06:37 +0100

    mail/thunderbird-esr: copy thunderbird 128.9.0
    in preparation for importing Thunderbird 137.0
    
    Release Notes (soon):
      https://www.thunderbird.net/en-US/thunderbird/128.9.0esr/releasenotes/
    
    (cherry picked from commit a9097e0cd9ee6794deea0015289aae24f8176916)

---
+++ mail/thunderbird-esr/files/patch-memory_mozalloc_throw__gcc.h
@@ -0,0 +1,69 @@
+--- memory/mozalloc/throw_gcc.h.orig	2022-03-07 19:34:37 UTC
++++ memory/mozalloc/throw_gcc.h
+@@ -74,50 +74,66 @@ __throw_bad_function_call(void) {
+   mozalloc_abort("fatal: STL threw bad_function_call");
+ }
+ 
++#if !defined(_LIBCPP_VERSION)
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void __throw_logic_error(
+     const char* msg) {
+   mozalloc_abort(msg);
+ }
++#endif  // _LIBCPP_VERSION
+ 
++#if !defined(_LIBCPP_VERSION)
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void __throw_domain_error(
+     const char* msg) {
+   mozalloc_abort(msg);
+ }
++#endif  // _LIBCPP_VERSION
+ 
++#if !defined(_LIBCPP_VERSION)
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void
+ __throw_invalid_argument(const char* msg) {
+   mozalloc_abort(msg);
+ }
++#endif  // _LIBCPP_VERSION
+ 
++#if !defined(_LIBCPP_VERSION)
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void __throw_length_error(
+     const char* msg) {
+   mozalloc_abort(msg);
+ }
++#endif  // _LIBCPP_VERSION
+ 
++#if !defined(_LIBCPP_VERSION)
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void __throw_out_of_range(
+     const char* msg) {
+   mozalloc_abort(msg);
+ }
++#endif  // _LIBCPP_VERSION
+ 
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void __throw_runtime_error(
+     const char* msg) {
+   mozalloc_abort(msg);
+ }
+ 
++#if !defined(_LIBCPP_VERSION)
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void __throw_range_error(
+     const char* msg) {
+   mozalloc_abort(msg);
+ }
++#endif  // _LIBCPP_VERSION
+ 
++#if !defined(_LIBCPP_VERSION)
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void
+ __throw_overflow_error(const char* msg) {
+   mozalloc_abort(msg);
+ }
++#endif  // _LIBCPP_VERSION
+ 
++#if !defined(_LIBCPP_VERSION)
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void
+ __throw_underflow_error(const char* msg) {
+   mozalloc_abort(msg);
+ }
++#endif  // _LIBCPP_VERSION
+ 
+ MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void __throw_ios_failure(
+     const char* msg) {
