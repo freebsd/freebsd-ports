7eb19a67e08cec39e733e1ffd8c63898bdfa7992
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2025-03-27 20:08:07 +0100

    mail/thunderbird: update to 137.0 (rc1)
    Release Notes (soon):
      https://www.thunderbird.net/en-US/thunderbird/137.0/releasenotes/
    
    Mention the big upgrade and the new thunderbird-esr port in UPDATING.

--- mail/thunderbird/files/patch-build_gn__processor.py
+++ mail/thunderbird/files/patch-build_gn__processor.py
@@ -1,13 +1,13 @@
-commit cb7967f55ab2f34635a10da523f277083ebd97a5
-Author: Christoph Moench-Tegeder <cmt@FreeBSD.org>
+commit e884ff2873b6ef8f3c8ee0a09e7651f46b3add95
+Author: Christoph Moench-Tegeder <cmt@burggraben.net>
 
-    chase gn_processor.py changes
+    chase gn_processor.py move
 
-diff --git python/mozbuild/mozbuild/gn_processor.py python/mozbuild/mozbuild/gn_processor.py
-index 3a9b9e7f3b..656a28ae6a 100644
---- python/mozbuild/mozbuild/gn_processor.py
-+++ python/mozbuild/mozbuild/gn_processor.py
-@@ -185,6 +185,7 @@ def filter_gn_config(path, gn_result, sandbox_vars, input_vars, gn_target):
+diff --git build/gn_processor.py build/gn_processor.py
+index 2ba8b92c27..962479b228 100644
+--- build/gn_processor.py
++++ build/gn_processor.py
+@@ -186,6 +186,7 @@ def filter_gn_config(path, gn_result, sandbox_vars, input_vars, gn_target):
      }
      oses = {
          "android": "Android",
@@ -15,7 +15,7 @@ index 3a9b9e7f3b..656a28ae6a 100644
          "linux": "Linux",
          "mac": "Darwin",
          "openbsd": "OpenBSD",
-@@ -742,17 +743,17 @@ def main():
+@@ -780,17 +781,17 @@ def main():
  
      vars_set = []
      for is_debug in (True, False):
@@ -31,14 +31,14 @@ index 3a9b9e7f3b..656a28ae6a 100644
 +            if target_os in ("android", "freebsd", "linux", "win"):
                  target_cpus.append("x86")
 -            if target_os in ("linux", "openbsd"):
-+            if target_os in ("freebsd", "linux", "openbsd"):
++            if target_os in ("linux", "freebsd", "openbsd"):
                  target_cpus.append("riscv64")
 -            if target_os == "linux":
 +            if target_os in ("freebsd", "linux"):
-                 target_cpus.extend(["ppc64", "mipsel", "mips64el"])
+                 target_cpus.extend(["loong64", "ppc64", "mipsel", "mips64el"])
              for target_cpu in target_cpus:
                  vars = {
-@@ -761,7 +762,7 @@ def main():
+@@ -799,7 +800,7 @@ def main():
                      "target_cpu": target_cpu,
                      "target_os": target_os,
                  }
