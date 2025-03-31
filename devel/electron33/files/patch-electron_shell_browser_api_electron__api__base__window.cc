8a0c60baef483cbef59d62f4cce013fed7ceda4d
Hiroki Tagato <tagattie@FreeBSD.org>
2025-03-31 18:59:22 +0900

    devel/electron33: update to 33.4.8
    Changelog:
    - https://github.com/electron/electron/releases/tag/v33.4.7
    - https://github.com/electron/electron/releases/tag/v33.4.8
    
    Reported by:	GitHub (watch releases)
    MFH:		2025Q1
    Security:	01a7e1e1-d249-4dd8-9a4a-ef95b5747afb

--- devel/electron33/files/patch-electron_shell_browser_api_electron__api__base__window.cc
+++ devel/electron33/files/patch-electron_shell_browser_api_electron__api__base__window.cc
@@ -1,6 +1,6 @@
---- electron/shell/browser/api/electron_api_base_window.cc.orig	2024-10-22 02:29:46 UTC
+--- electron/shell/browser/api/electron_api_base_window.cc.orig	2025-03-26 23:59:37 UTC
 +++ electron/shell/browser/api/electron_api_base_window.cc
-@@ -42,7 +42,7 @@
+@@ -41,7 +41,7 @@
  #include "shell/browser/ui/views/win_frame_view.h"
  #include "shell/browser/ui/win/taskbar_host.h"
  #include "ui/base/win/shell.h"
@@ -9,7 +9,7 @@
  #include "shell/browser/ui/views/opaque_frame_view.h"
  #endif
  
-@@ -995,7 +995,7 @@ void BaseWindow::SetIconImpl(v8::Isolate* isolate,
+@@ -994,7 +994,7 @@ void BaseWindow::SetIconImpl(v8::Isolate* isolate,
    static_cast<NativeWindowViews*>(window_.get())
        ->SetIcon(native_image->GetHICON(GetSystemMetrics(SM_CXSMICON)),
                  native_image->GetHICON(GetSystemMetrics(SM_CXICON)));
@@ -18,7 +18,7 @@
    static_cast<NativeWindowViews*>(window_.get())
        ->SetIcon(native_image->image().AsImageSkia());
  #endif
-@@ -1052,7 +1052,7 @@ void BaseWindow::SetAppDetails(const gin_helper::Dicti
+@@ -1051,7 +1051,7 @@ void BaseWindow::SetAppDetails(const gin_helper::Dicti
  }
  #endif
  
@@ -27,7 +27,7 @@
  void BaseWindow::SetTitleBarOverlay(const gin_helper::Dictionary& options,
                                      gin_helper::Arguments* args) {
    // Ensure WCO is already enabled on this window
-@@ -1302,7 +1302,7 @@ void BaseWindow::BuildPrototype(v8::Isolate* isolate,
+@@ -1301,7 +1301,7 @@ void BaseWindow::BuildPrototype(v8::Isolate* isolate,
        .SetMethod("setThumbnailToolTip", &BaseWindow::SetThumbnailToolTip)
        .SetMethod("setAppDetails", &BaseWindow::SetAppDetails)
  #endif
