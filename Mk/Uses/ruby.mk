f0c470c515618bdee1c8ccfeb567cec7195457ea
Rene Ladan <rene@FreeBSD.org>
2025-03-31 18:24:21 +0200

    Mk/Uses: unregister expired lang/ruby31

--- Mk/Uses/ruby.mk
+++ Mk/Uses/ruby.mk
@@ -15,7 +15,7 @@
 # [variables that a user may define]
 #
 # RUBY_VER		- (See below)
-# RUBY_DEFAULT_VER	- Set to (e.g.) "3.1" if you want to refer to "ruby31"
+# RUBY_DEFAULT_VER	- Set to (e.g.) "3.2" if you want to refer to "ruby32"
 #			  just as "ruby".
 # RUBY_ARCH		- (See below)
 #
@@ -144,14 +144,7 @@ RUBY?=			${LOCALBASE}/bin/ruby${RUBY_SUFFIX}
 .    if defined(RUBY_VER)
 # When adding a version, please keep the comment in
 # Mk/bsd.default-versions.mk in sync.
-.      if ${RUBY_VER} == 3.1
-#
-# Ruby 3.1
-#
-RUBY_DISTVERSION=	3.1.6
-RUBY_PORTREVISION=	0
-
-.      elif ${RUBY_VER} == 3.2
+.      if ${RUBY_VER} == 3.2
 #
 # Ruby 3.2
 #
@@ -171,7 +164,7 @@ RUBY_PORTREVISION=	0
 #
 # Other versions
 #
-IGNORE=	Only ruby 3.1, 3,2 and 3.3 are supported
+IGNORE=	Only ruby 3.2 and 3.3 are supported
 _INVALID_RUBY_VER=	1
 .      endif
 RUBY_PORTEPOCH=		1
@@ -180,7 +173,6 @@ RUBY_VERSION=	${RUBY_DISTVERSION:C/^([0-9]+\.[0-9]+\.[0-9]+).*/\1/}
 
 .    if !defined(_INVALID_RUBY_VER)
 
-RUBY31?=		"@comment "
 RUBY32?=		"@comment "
 RUBY33?=		"@comment "
 
