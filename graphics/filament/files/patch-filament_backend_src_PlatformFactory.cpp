2dad0f9f569f56bf19196d862e942cb32b6461c2
Yuri Victorovich <yuri@FreeBSD.org>
2025-04-01 01:45:01 -0700

    graphics/filament: update 1.58.2 → 1.59.0
    Reported by:	portscout

--- graphics/filament/files/patch-filament_backend_src_PlatformFactory.cpp
+++ graphics/filament/files/patch-filament_backend_src_PlatformFactory.cpp
@@ -1,18 +1,18 @@
---- filament/backend/src/PlatformFactory.cpp.orig	2024-05-29 23:19:39 UTC
+--- filament/backend/src/PlatformFactory.cpp.orig	2025-03-31 22:23:27 UTC
 +++ filament/backend/src/PlatformFactory.cpp
-@@ -32,7 +32,7 @@
-     #if defined(FILAMENT_SUPPORTS_OPENGL) && !defined(FILAMENT_USE_EXTERNAL_GLES3)
-         #include <backend/platforms/PlatformCocoaGL.h>
+@@ -41,7 +41,7 @@
+             #include <backend/platforms/PlatformCocoaGL.h>
+         #endif
      #endif
 -#elif defined(__linux__)
 +#elif defined(__linux__) || defined(__FreeBSD__)
      #if defined(FILAMENT_SUPPORTS_X11)
          #if defined(FILAMENT_SUPPORTS_OPENGL) && !defined(FILAMENT_USE_EXTERNAL_GLES3)
              #include "backend/platforms/PlatformGLX.h"
-@@ -119,7 +119,7 @@ Platform* PlatformFactory::create(Backend* backend) no
-             return new PlatformCocoaTouchGL();
-         #elif defined(__APPLE__)
-             return new PlatformCocoaGL();
+@@ -151,7 +151,7 @@ Platform* PlatformFactory::create(Backend* backend) no
+             #else
+                 return new PlatformCocoaGL();
+             #endif
 -        #elif defined(__linux__)
 +        #elif defined(__linux__) || defined(__FreeBSD__)
              #if defined(FILAMENT_SUPPORTS_X11)
