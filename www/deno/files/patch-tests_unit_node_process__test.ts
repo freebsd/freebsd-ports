673866185794ddbc57e4074c69475b721c351b7b
Mikael Urankar <mikael@FreeBSD.org>
2025-04-08 13:29:51 +0200

    www/deno: Add test target

---
+++ www/deno/files/patch-tests_unit_node_process__test.ts
@@ -0,0 +1,11 @@
+--- tests/unit_node/process_test.ts.orig	2025-03-30 15:43:23 UTC
++++ tests/unit_node/process_test.ts
+@@ -569,7 +569,7 @@ Deno.test({
+       listener.close();
+     });
+ 
+-    const shell = "/bin/bash";
++    const shell = "PREFIX/bin/bash";
+     const cmd =
+       `"${Deno.execPath()}" run ${scriptPath} < /dev/tcp/127.0.0.1/9000`;
+     const args = ["-c", cmd];
