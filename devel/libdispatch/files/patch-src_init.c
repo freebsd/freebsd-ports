a27e102b5c12166147b6fa7bd743cbeb42a39ba0
Dima Panov <fluffy@FreeBSD.org>
2025-04-10 01:14:54 +0300

    devel/libdispatch: update to 6.1 release (+)
    Track move GH account apple -> swiftlang
    
    Release notes:	https://github.com/swiftlang/swift-corelibs-libdispatch/releases/tag/swift-6.1-RELEASE

--- devel/libdispatch/files/patch-src_init.c
+++ devel/libdispatch/files/patch-src_init.c
@@ -1,6 +1,6 @@
---- src/init.c.orig	2021-09-17 04:54:52 UTC
+--- src/init.c.orig	2025-03-07 00:50:44 UTC
 +++ src/init.c
-@@ -961,7 +961,6 @@ _dispatch_continuation_get_function_symbol(dispatch_co
+@@ -964,7 +964,6 @@ _dispatch_continuation_get_function_symbol(dispatch_co
  	return dc->dc_func;
  }
  
@@ -8,7 +8,7 @@
  void
  _dispatch_bug_kevent_client(const char *msg, const char *filter,
  		const char *operation, int err, uint64_t ident, uint64_t udata,
-@@ -1005,7 +1004,6 @@ _dispatch_bug_kevent_client(const char *msg, const cha
+@@ -1008,7 +1007,6 @@ _dispatch_bug_kevent_client(const char *msg, const cha
  				msg, strerror(err), err, udata, filter, ident, ident, func);
  	}
  }
@@ -16,7 +16,7 @@
  
  #if RDAR_49023449
  
-@@ -1048,7 +1046,7 @@ _dispatch_bug_kevent_vanished(dispatch_unote_t du)
+@@ -1051,7 +1049,7 @@ _dispatch_bug_kevent_vanished(dispatch_unote_t du)
  			"{ %p[%s], ident: %" PRIdPTR " / 0x%" PRIxPTR ", handler: %p }",
  			dux_type(du._du)->dst_kind, dou._dq,
  			dou._dq->dq_label ? dou._dq->dq_label : "<unknown>",
@@ -25,13 +25,11 @@
  }
  
  #endif // RDAR_49023449
-@@ -1153,8 +1151,8 @@ _dispatch_logv_init(void *context DISPATCH_UNUSED)
- 			}
+@@ -1157,7 +1155,7 @@ _dispatch_logv_init(void *context DISPATCH_UNUSED)
  #else
  			dprintf(dispatch_logfile, "=== log file opened for %s[%u] at "
--					"%ld.%06u ===\n", getprogname() ?: "", getpid(),
+ 					"%ld.%06u ===\n", getprogname() ?: "", getpid(),
 -					tv.tv_sec, (int)tv.tv_usec);
-+					"%lld.%06u ===\n", getprogname() ?: "", getpid(),
 +					(time_t)tv.tv_sec, (int)tv.tv_usec);
  #endif
  		}
