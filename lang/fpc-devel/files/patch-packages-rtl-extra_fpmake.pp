c1369fe26157e9f53281133b998bf3ec0fd2371c
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2025-04-09 20:46:55 -0700

    lang/fpc-devel*: Update to 3.3.1.20250409

--- lang/fpc-devel/files/patch-packages-rtl-extra_fpmake.pp
+++ lang/fpc-devel/files/patch-packages-rtl-extra_fpmake.pp
@@ -1,11 +1,11 @@
---- packages/rtl-extra/fpmake.pp	2024-01-03 08:10:39.000000000 -0500
-+++ packages/rtl-extra/fpmake.pp	2024-01-03 16:21:18.846789000 -0500
+--- packages/rtl-extra/fpmake.pp	2025-04-09 13:36:23.000000000 -0700
++++ packages/rtl-extra/fpmake.pp	2025-04-09 19:49:01.544063000 -0700
 @@ -20,7 +20,7 @@
  //  IPCcdeclOSes  = [Darwin,iphonesim,ios];
  
    PrinterOSes   = [go32v2,msdos,os2,win32,win64,atari]+unixlikes-[beos,haiku,morphos];
 -  SerialOSes    = [android,linux,netbsd,openbsd,win32,win64];
-+  SerialOSes    = [android,linux,freebsd,netbsd,openbsd,win32,win64];
-   UComplexOSes  = [atari,embedded,emx,gba,go32v2,msdos,nativent,nds,netware,netwlibc,os2,sinclairql,human68k,symbian,watcom,wii,wince,win32,win64,freertos,wasi]+UnixLikes+AllAmigaLikeOSes;
-   MatrixOSes    = [atari,embedded,emx,gba,go32v2,msdos,nativent,nds,netware,netwlibc,os2,sinclairql,human68k,symbian,watcom,wii,win32,win64,wince,freertos,wasi]+UnixLikes+AllAmigaLikeOSes;
-   ObjectsOSes   = [atari,embedded,emx,gba,go32v2,macosclassic,msdos,nds,netware,netwlibc,os2,sinclairql,human68k,symbian,watcom,wii,win16,win32,win64,wince,freertos,wasi]+UnixLikes+AllAmigaLikeOSes;
++  SerialOSes    = [android,freebsd,linux,netbsd,openbsd,win32,win64];
+   UComplexOSes  = [atari,embedded,emx,gba,go32v2,msdos,nativent,nds,netware,netwlibc,os2,sinclairql,human68k,symbian,watcom,wii,wince,win32,win64,freertos,wasip1,wasip1threads]+UnixLikes+AllAmigaLikeOSes;
+   MatrixOSes    = [atari,embedded,emx,gba,go32v2,msdos,nativent,nds,netware,netwlibc,os2,sinclairql,human68k,symbian,watcom,wii,win32,win64,wince,freertos,wasip1,wasip1threads]+UnixLikes+AllAmigaLikeOSes;
+   ObjectsOSes   = [atari,embedded,emx,gba,go32v2,macosclassic,msdos,nds,netware,netwlibc,os2,sinclairql,human68k,symbian,watcom,wii,win16,win32,win64,wince,freertos,wasip1,wasip1threads]+UnixLikes+AllAmigaLikeOSes;
