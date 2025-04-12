47241c24b676a90c0ec3d3405a74ffae8f79fcb4
Max Brazhnikov <makc@FreeBSD.org>
2025-04-13 01:23:27 +0300

    KDE: Update KDE Frameworks 6 to 6.13.0
    Announcement: https://kde.org/announcements/frameworks/6/6.13.0/
    
    sysutils/kf6-kuserfeedback:
    
    - Add missing dependency
    - Build Qt help file

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -103,7 +103,7 @@ KDE_FRAMEWORKS5_VERSION?=	5.116.0
 KDE_FRAMEWORKS5_BRANCH?=	stable
 
 # Current KDE Frameworks (Qt6 based).
-KDE_FRAMEWORKS6_VERSION?=	6.12.0
+KDE_FRAMEWORKS6_VERSION?=	6.13.0
 KDE_FRAMEWORKS6_BRANCH?=	stable
 
 # Current KDE applications. Update _${PORTNAME}_PROJECT_VERSION for the following ports:
