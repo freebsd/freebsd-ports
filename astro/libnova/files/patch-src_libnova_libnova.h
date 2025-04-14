6ce9e2dd2d469224ef7e7f4f843d43273294bc36
Älven <alster@vinterdalen.se>
2025-04-14 08:43:09 +0200

    astro/libnova: Set maintainer, improve and update 0.15.0
    ChangeLog: https://sourceforge.net/p/libnova/libnova/ci/v0.16/tree/ChangeLog
    
    Reporter becomes maintainer. Already maintainer of several ports.
    
    PR:		286058
    Reported by:	alster@vinterdalen.se

---
+++ astro/libnova/files/patch-src_libnova_libnova.h
@@ -0,0 +1,11 @@
+--- src/libnova/libnova.h.orig	2015-03-03 11:16:58 UTC
++++ src/libnova/libnova.h
+@@ -16,7 +16,7 @@
+  *  Copyright (C) 2000 - 2005 Liam Girdwood <lgirdwood@gmail.com>
+  */
+ 
+-#define LIBNOVA_VERSION "0.15.0"
++#define LIBNOVA_VERSION "0.16.0"
+ 
+ /*! \mainpage libnova
+ * \image html libnova-logo.jpg
