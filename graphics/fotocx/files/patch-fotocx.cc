9047f2d52e981721a879535eba724f71a5287ec6
Alexey Dokuchaev <danfe@FreeBSD.org>
2025-04-14 15:48:58 +0000

    graphics/fotocx: the port had been updated to version 25.0.

--- graphics/fotocx/files/patch-fotocx.cc
+++ graphics/fotocx/files/patch-fotocx.cc
@@ -1,6 +1,15 @@
---- fotocx.cc.orig	2024-10-01 05:03:10 UTC
+--- fotocx.cc.orig	2025-01-01 12:47:23 UTC
 +++ fotocx.cc
-@@ -335,7 +335,7 @@ int main(int argc, ch *argv[])                        
+@@ -240,7 +240,7 @@ int main(int argc, ch *argv[])                        
+    Nblacklist = 0;                                                               //  blacklist folders/files count
+    Nfuncbusy = 0;                                                                //  nothing busy
+    Fescape = 0;                                                                  //  no escape key pressed
+-   NSMP = 8;                                                                     //  default SMP (core) count
++   NSMP = get_nprocs();
+ 
+    imagefiletypes = zstrdup(".jpg .jpeg .png .jxl .tif .tiff .bmp .ico .ppm .gif .svg .xpm .tga " ,"params",100);      //  jxl
+    RAWfiletypes = zstrdup(".arw .srf .sr2 .crw .cr2 .cr3 .dng .mdc .mrw .nef .nrw .raw .rw2 .srw ", "params",100);
+@@ -307,7 +307,7 @@ int main(int argc, ch *argv[])                        
     //  delete fotocx temp files if owner process is no longer running
  
     pidlist = 0;
@@ -9,12 +18,3 @@
     fid = popen(buff,"r");
     if (fid) {
        pp = fgets_trim(buff,200,fid);
-@@ -466,7 +466,7 @@ int main(int argc, ch *argv[])                        
-    //  Cannot get P-core and E-core counts (methods vary by kernel release). 
-    //  Hyperthreads and E-cores have marginal value.
- 
--   NSMP = 12;                                                                    //  24.60
-+   NSMP = get_nprocs();
-    Plog(1,"SMP thread count: %d \n",NSMP);
- 
-    //  get locale specific name for /home/<user>/Desktop
