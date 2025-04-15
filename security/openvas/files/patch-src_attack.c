b0dbd49d80967f1a0effc57f5eb2dc9b2623cc8e
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2025-04-14 21:16:39 -0700

    security/gvm: Update to 25.2.1
    databases/pg-gvm: Update to 22.6.9
    security/gsa: Update to 24.6.1 (Only amd64 and aarch64)
    security/gsad: Update to 24.2.4
    security/openvas: Update to 23.16.1 [1]
    security/gvmd: Update to 25.2.1
    security/gvm-libs: 22.21.0
    security/py-greenbone-feed-sync: Update to 25.1.0
    security/py-ospd-openvas: Update to 22.8.2
    security/py-gvm-tools: Update to 25.3.0
    security/py-python-gvm: Update to 26.1.1
    
    PR:		285582
    Reported by:	wen [1]

--- security/openvas/files/patch-src_attack.c
+++ security/openvas/files/patch-src_attack.c
@@ -1,5 +1,5 @@
---- src/attack.c	2024-04-24 02:59:35.930683000 -0500
-+++ src/attack.c	2024-04-24 03:02:33.794371000 -0500
+--- src/attack.c	2025-03-03 04:49:55.000000000 -0800
++++ src/attack.c	2025-04-14 20:09:40.968033000 -0700
 @@ -31,7 +31,6 @@
  #include "utils.h"
  
@@ -8,16 +8,17 @@
  #include <errno.h> /* for errno() */
  #include <fcntl.h>
  #include <glib.h>
-@@ -1541,13 +1540,13 @@
- 
+@@ -1555,14 +1554,14 @@
    gettimeofday (&now, NULL);
    if (test_alive_hosts_only)
--    g_message ("Vulnerability scan %s finished in %ld seconds: "
-+    g_message ("Vulnerability scan %s finished in %lld seconds: "
-                "%d alive hosts of %d",
--               globals->scan_id, now.tv_sec - then.tv_sec,
-+               globals->scan_id, (long long)now.tv_sec - then.tv_sec,
-                gvm_hosts_count (alive_hosts_list), gvm_hosts_count (hosts));
+     {
+-      g_message ("Vulnerability scan %s finished in %ld seconds: "
++      g_message ("Vulnerability scan %s finished in %lld seconds: "
+                  "%d alive hosts of %d",
+-                 globals->scan_id, now.tv_sec - then.tv_sec,
++                 globals->scan_id, (long long)now.tv_sec - then.tv_sec,
+                  gvm_hosts_count (alive_hosts_list), gvm_hosts_count (hosts));
+     }
    else
 -    g_message ("Vulnerability scan %s finished in %ld seconds: %d hosts",
 -               globals->scan_id, now.tv_sec - then.tv_sec,
@@ -25,4 +26,4 @@
 +               globals->scan_id, (long long)now.tv_sec - then.tv_sec,
                 gvm_hosts_count (hosts));
  
-   gvm_hosts_free (hosts);
+   if (prefs_get ("report_scripts"))
