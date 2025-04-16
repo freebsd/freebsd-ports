0cf9c1323f5a67eb8cd0e5fde1c8a0866fd1668d
Robert Nagy <rnagy@FreeBSD.org>
2025-04-16 09:53:10 +0200

    www/chromium: update to 135.0.7049.95
    Security:	https://vuxml.freebsd.org/freebsd/bf5d29ea-1a93-11f0-8cb5-a8a1599412c6.html
    (cherry picked from commit 47937c13254470bde26f8245c0cda897e7a4cea1)

--- www/chromium/files/patch-media_base_video__frame.cc
+++ www/chromium/files/patch-media_base_video__frame.cc
@@ -1,4 +1,4 @@
---- media/base/video_frame.cc.orig	2025-04-04 08:52:13 UTC
+--- media/base/video_frame.cc.orig	2025-04-16 07:51:27 UTC
 +++ media/base/video_frame.cc
 @@ -92,7 +92,7 @@ std::string VideoFrame::StorageTypeToString(
        return "OWNED_MEMORY";
@@ -27,7 +27,7 @@
    bool is_native_buffer =
        gpu_memory_buffer
            ? (gpu_memory_buffer->GetType() != gfx::SHARED_MEMORY_BUFFER)
-@@ -913,7 +913,7 @@ scoped_refptr<VideoFrame> VideoFrame::WrapExternalGpuM
+@@ -938,7 +938,7 @@ scoped_refptr<VideoFrame> VideoFrame::WrapExternalGpuM
    return frame;
  }
  
@@ -36,7 +36,7 @@
  // static
  scoped_refptr<VideoFrame> VideoFrame::WrapExternalDmabufs(
      const VideoFrameLayout& layout,
-@@ -1600,7 +1600,7 @@ scoped_refptr<gpu::ClientSharedImage> VideoFrame::shar
+@@ -1625,7 +1625,7 @@ scoped_refptr<gpu::ClientSharedImage> VideoFrame::shar
    return wrapped_frame_ ? wrapped_frame_->shared_image() : shared_image_;
  }
  
