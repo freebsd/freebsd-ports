c05461a94b01d0921b8f2a25ea6a0002a1ce94c6
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-04-18 13:15:50 +0800

    devel/py-optree: Update to 0.15.0
    Changes:	https://github.com/metaopt/optree/releases

--- devel/py-optree/files/patch-setup.py
+++ devel/py-optree/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2025-03-01 19:39:44 UTC
+--- setup.py.orig	2025-04-06 13:55:44 UTC
 +++ setup.py
-@@ -119,10 +119,6 @@ class cmake_build_ext(build_ext):  # noqa: N801
+@@ -211,10 +211,6 @@ class cmake_build_ext(build_ext):  # noqa: N801
                  cmake_args.append(f'-Dpybind11_DIR={pybind11.get_cmake_dir()}')
  
          build_args = ['--config', config]
@@ -11,12 +11,3 @@
  
          build_args.extend(['--target', ext.target, '--'])
  
-@@ -138,7 +134,7 @@ class cmake_build_ext(build_ext):  # noqa: N801
-             with unset_python_path():
-                 self.spawn([cmake, '--version'])  # cmake in the parent virtual environment
- 
--        self.mkpath(build_temp)
-+        self.mkpath(str(build_temp))
-         with spawn_context():
-             self.spawn([cmake, '-S', str(ext.source_dir), '-B', str(build_temp), *cmake_args])
-             self.spawn([cmake, '--build', str(build_temp), *build_args])
