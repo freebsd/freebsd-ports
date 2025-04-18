3cfe50c9b90c5a9daa335c871ca4dd65c92f6a86
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-04-18 13:15:19 +0800

    devel/py-ddtrace: Update to 2.21.5
    - Clean up BUILD_DEPENDS after 1065f238d9528a5d4dc661bf1a0479261aba3374
    - Silence patch message
    
    Changes:	https://github.com/DataDog/dd-trace-py/releases

--- devel/py-ddtrace/files/patch-setup.py
+++ devel/py-ddtrace/files/patch-setup.py
@@ -1,14 +1,25 @@
---- setup.py.orig	2024-08-01 21:44:34 UTC
+--- setup.py.orig	2025-04-07 19:46:51 UTC
 +++ setup.py
-@@ -8,7 +8,6 @@ import tarfile
- import sysconfig
+@@ -9,7 +9,6 @@ import time
  import tarfile
+ import time
  
 -import cmake
  from setuptools_rust import Binding
  from setuptools_rust import RustExtension
  
-@@ -495,10 +494,9 @@ setup(
+@@ -419,9 +418,7 @@ class CMakeBuild(build_ext):
+                 "-DCMAKE_C_FLAGS_%s=-O0" % ext.build_type.upper(),
+                 "-DCMAKE_CXX_FLAGS_%s=-O0" % ext.build_type.upper(),
+             ]
+-        cmake_command = (
+-            Path(cmake.CMAKE_BIN_DIR) / "cmake"
+-        ).resolve()  # explicitly use the cmake provided by the cmake package
++        cmake_command = "cmake"
+         subprocess.run([cmake_command, *cmake_args], cwd=cmake_build_dir, check=True)
+         subprocess.run([cmake_command, "--build", ".", *build_args], cwd=cmake_build_dir, check=True)
+         subprocess.run([cmake_command, "--install", ".", *install_args], cwd=cmake_build_dir, check=True)
+@@ -661,10 +658,9 @@ setup(
      # funcsigs backport required for vendored debtcollector
      cmdclass={
          "build_ext": CMakeBuild,
