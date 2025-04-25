32ee80808c8a234c17c1f18457fb949b0c2f3b35
Alex Levy <a13xlevy@pm.me>
2025-04-25 21:17:49 +0200

    news/nzbget: Update to 24.8
    Changelogs:
    https://github.com/nzbgetcom/nzbget/releases/tag/v24.8
    https://github.com/nzbgetcom/nzbget/releases/tag/v24.7
    https://github.com/nzbgetcom/nzbget/releases/tag/v24.6
    
    PR:		285709
    Approved by:	submitter is maintainer

--- news/nzbget/files/patch-cmake_par2-turbo.cmake
+++ news/nzbget/files/patch-cmake_par2-turbo.cmake
@@ -1,15 +1,15 @@
---- cmake/par2-turbo.cmake.orig	2024-12-05 05:22:43 UTC
+--- cmake/par2-turbo.cmake.orig	2025-03-27 11:43:08 UTC
 +++ cmake/par2-turbo.cmake
-@@ -27,12 +27,6 @@ endif()
+@@ -42,12 +42,6 @@ endif()
  ExternalProject_add(
  	par2-turbo
  	PREFIX			par2-turbo
 -	GIT_REPOSITORY	https://github.com/nzbgetcom/par2cmdline-turbo.git
--	GIT_TAG			v1.1.1-nzbget-20241128
+-	GIT_TAG			v1.2.0-nzbget-20250213
 -	TLS_VERIFY		TRUE
 -	GIT_SHALLOW		TRUE
 -	GIT_PROGRESS	TRUE
 -	DOWNLOAD_EXTRACT_TIMESTAMP	TRUE
- 	CMAKE_ARGS		${CMAKE_ARGS}
+ 	BUILD_BYPRODUCTS ${PAR2_LIBS}
+ 	CMAKE_ARGS		 ${CMAKE_ARGS}
  	INSTALL_COMMAND	""
- )
