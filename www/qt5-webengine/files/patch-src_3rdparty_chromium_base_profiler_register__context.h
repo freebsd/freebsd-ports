85ef7e6db838d5276da8c9d6b658f41cdda13e6c
Robert Clausecker <fuz@FreeBSD.org>
2025-04-26 23:14:26 +0200

    www/qt5-webengine: fix build on armv7
    The build system adds the bogus option --target=arm-linux-gnueabihf to
    the build when building on arm, causing all sorts of issues.  Patch that
    out, then add some polyfill for API differences between ARM Linux and
    ARM FreeBSD.
    
    Approved by:	portmgr (build fix blanket)
    MFH:		2025Q2

--- www/qt5-webengine/files/patch-src_3rdparty_chromium_base_profiler_register__context.h
+++ www/qt5-webengine/files/patch-src_3rdparty_chromium_base_profiler_register__context.h
@@ -1,4 +1,4 @@
---- src/3rdparty/chromium/base/profiler/register_context.h.orig	2021-12-15 16:12:54 UTC
+--- src/3rdparty/chromium/base/profiler/register_context.h.orig	2025-03-06 09:26:10 UTC
 +++ src/3rdparty/chromium/base/profiler/register_context.h
 @@ -17,7 +17,7 @@
  #include <windows.h>
@@ -9,7 +9,7 @@
  #include <sys/ucontext.h>
  #endif
  
-@@ -190,6 +190,48 @@ inline uintptr_t& RegisterContextInstructionPointer(mc
+@@ -190,6 +190,61 @@ inline uintptr_t& RegisterContextInstructionPointer(mc
  }
  
  #endif  // #if defined(ARCH_CPU_ARM_FAMILY) && defined(ARCH_CPU_32_BITS)
@@ -53,6 +53,19 @@
 +
 +inline uintptr_t& RegisterContextInstructionPointer(mcontext_t* context) {
 +  return AsUintPtr(&context->mc_gpregs.gp_elr);
++}
++
++#elif defined(ARCH_CPU_ARM_FAMILY) && defined(ARCH_CPU_32_BITS)
++inline uintptr_t& RegisterContextStackPointer(mcontext_t* context) {
++  return AsUintPtr(&context->__gregs[_REG_SP]);
++}
++
++inline uintptr_t& RegisterContextFramePointer(mcontext_t* context) {
++  return AsUintPtr(&context->__gregs[_REG_FP]);
++}
++
++inline uintptr_t& RegisterContextInstructionPointer(mcontext_t* context) {
++  return AsUintPtr(&context->__gregs[_REG_PC]);
 +}
 +#endif
  
--- www/qt5-webengine/files/patch-src_3rdparty_chromium_third__party_skia_src_core_SkCpu.cpp
+++ www/qt5-webengine/files/patch-src_3rdparty_chromium_third__party_skia_src_core_SkCpu.cpp
@@ -1,4 +1,4 @@
---- src/3rdparty/chromium/third_party/skia/src/core/SkCpu.cpp.orig	2021-12-15 16:12:54 UTC
+--- src/3rdparty/chromium/third_party/skia/src/core/SkCpu.cpp.orig	2025-03-06 09:26:10 UTC
 +++ src/3rdparty/chromium/third_party/skia/src/core/SkCpu.cpp
 @@ -72,6 +72,23 @@
          return features;
@@ -24,3 +24,26 @@
  #elif defined(SK_CPU_ARM64) && __has_include(<sys/auxv.h>)
      #include <sys/auxv.h>
  
+@@ -110,6 +127,22 @@
+                           || 0 == memcmp(kMongoose3, buf, SK_ARRAY_COUNT(buf))) {
+                 features &= ~(SkCpu::ASIMDHP);
+             }
++        }
++        return features;
++    }
++
++#elif defined(SK_CPU_ARM32) && defined(__FreeBSD__)
++    #include <sys/auxv.h>
++
++    static uint32_t read_cpu_features() {
++        uint32_t features = 0;
++        unsigned long hwcaps = 0;
++
++        elf_aux_info(AT_HWCAP, &hwcaps, sizeof hwcaps);
++
++        if (hwcaps & HWCAP_NEON ) {
++            features |= SkCpu::NEON;
++            if (hwcaps & HWCAP_VFPv4) { features |= SkCpu::NEON_FMA|SkCpu::VFP_FP16; }
+         }
+         return features;
+     }
