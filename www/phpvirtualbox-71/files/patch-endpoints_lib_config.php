f1fbc644a8fc38015fa0432471fc3660866b677c
Vladimir Druzenko <vvd@FreeBSD.org>
2025-04-28 17:42:52 +0300

    www/phpvirtualbox-71: Update 7.0 => 7.1-1
    Changelog:
    https://github.com/phpvirtualbox/phpvirtualbox/releases/tag/7.1-1
    
    PR:	284439

--- www/phpvirtualbox-71/files/patch-endpoints_lib_config.php
+++ www/phpvirtualbox-71/files/patch-endpoints_lib_config.php
@@ -1,10 +1,10 @@
---- endpoints/lib/config.php.orig	2025-03-09 01:02:03 UTC
+--- endpoints/lib/config.php.orig	2025-04-26 04:11:44 UTC
 +++ endpoints/lib/config.php
-@@ -151,6 +151,7 @@ class phpVBoxConfigClass {
-          */
-         var $enableHDFlushConfig;
-         
-+	var $authMaster = false;
-         
- 	/**
+@@ -143,6 +143,7 @@ class phpVBoxConfigClass {
+     // added vars to satisfy PHP 8.2+ dynamic property deprecation
+     var $enableAdvancedConfig;
+     var $enableHDFlushConfig;
++    var $authMaster = false;
+ 
+     /**
  	 * Read user configuration, apply defaults, and do some sanity checking
