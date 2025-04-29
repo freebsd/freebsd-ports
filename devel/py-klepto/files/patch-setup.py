c845353682de5b5576450b5e1bab804365a0cd26
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-04-29 19:44:13 +0800

    devel/py-klepto: Update to 0.2.7
    Changes:	https://github.com/uqfoundation/klepto/releases

---
+++ devel/py-klepto/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2025-04-16 00:56:32 UTC
++++ setup.py
+@@ -86,7 +86,7 @@ sysversion = sys.version_info[:3]
+ 
+ # define dependencies
+ sysversion = sys.version_info[:3]
+-dill_version = 'dill>=0.4.0'
++dill_version = 'dill>=0.3.9'
+ pox_version = 'pox>=0.3.6'
+ jsonpickle_version = 'jsonpickle>=0.9.6'
+ cloudpickle_version = 'cloudpickle>=0.5.2'
