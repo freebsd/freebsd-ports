f2eb4791b26748717b99606a418f85b201442f40
Dirk Meyer <dinoex@FreeBSD.org>
2025-05-01 20:41:46 +0200

    graphics/agg: update to 2.7.0
    PR: 285652

--- graphics/agg/files/patch-include__agg_renderer_outline_aa.h
+++ graphics/agg/files/patch-include__agg_renderer_outline_aa.h
@@ -1,8 +1,8 @@
---- include/agg_renderer_outline_aa.h.orig	2006-10-09 13:07:08.000000000 +0900
-+++ include/agg_renderer_outline_aa.h	2012-10-13 05:25:48.000000000 +0900
-@@ -1375,7 +1375,7 @@
+--- include/agg_renderer_outline_aa.h.orig	2023-08-21 15:51:12 UTC
++++ include/agg_renderer_outline_aa.h
+@@ -1366,7 +1366,7 @@ namespace agg
          //---------------------------------------------------------------------
-         void profile(const line_profile_aa& prof) { m_profile = &prof; }
+         void profile(line_profile_aa& prof) { m_profile = &prof; }
          const line_profile_aa& profile() const { return *m_profile; }
 -        line_profile_aa& profile() { return *m_profile; }
 +        const line_profile_aa& profile() { return *m_profile; }
