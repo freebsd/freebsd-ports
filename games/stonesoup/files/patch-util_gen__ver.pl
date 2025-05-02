91bf071f87bd82f0ce9c78426b1c6e7905f6f4fc
Robert Clausecker <fuz@FreeBSD.org>
2025-05-02 18:43:52 +0200

    games/stonesoup: update to 0.33.0
    Changelog: https://github.com/crawl/crawl/releases/tag/0.33.0

--- games/stonesoup/files/patch-util_gen__ver.pl
+++ games/stonesoup/files/patch-util_gen__ver.pl
@@ -1,12 +1,12 @@
---- util/gen_ver.pl.orig	2022-01-25 18:04:53 UTC
+--- util/gen_ver.pl.orig	2025-05-02 16:11:38 UTC
 +++ util/gen_ver.pl
-@@ -20,8 +20,7 @@ mkdir dirname($outfile);
+@@ -21,8 +21,7 @@ mkdir dirname($outfile);
  #
  # Source tarbells distributed as part of a release include this file already
  # generated with the release version.
--$_ = `git describe $mergebase 2> /dev/null`
+-$_ = `git describe $mergebase 2> $nullfile`
 -    || (open(IN, "<", "$scriptpath/release_ver") ? <IN>
 +$_ = (open(IN, "<", "$scriptpath/release_ver") ? <IN>
-         : die "Can't get version information: `git describe` failed (no git, no repository, or shallow clone), and $scriptpath/release_ver doesn't exist.\n")
-     or die "couldn't get the version information\n";
+         : die "Error: Can't get version information: `git describe` failed (no git, no repository, or shallow clone), and $scriptpath/release_ver doesn't exist.\n")
+     or die "Error: couldn't get the version information\n";
  
