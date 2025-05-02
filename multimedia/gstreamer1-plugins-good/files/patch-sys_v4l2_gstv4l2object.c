ff5a16ab9b5316ffcec2cb2cb992aef1cc8746f1
Jason E. Hale <jhale@FreeBSD.org>
2025-05-02 11:51:09 -0400

    GStreamer: Update to 1.26.1
    https://gstreamer.freedesktop.org/releases/1.26/#1.26.1

--- multimedia/gstreamer1-plugins-good/files/patch-sys_v4l2_gstv4l2object.c
+++ multimedia/gstreamer1-plugins-good/files/patch-sys_v4l2_gstv4l2object.c
@@ -1,6 +1,15 @@
---- sys/v4l2/gstv4l2object.c.orig	2025-03-11 20:14:44 UTC
+--- sys/v4l2/gstv4l2object.c.orig	2025-04-24 19:20:14 UTC
 +++ sys/v4l2/gstv4l2object.c
-@@ -5262,7 +5262,9 @@ gst_v4l2_object_probe_caps (GstV4l2Object * v4l2object
+@@ -529,7 +529,7 @@ v4l2_mmap_wrapper (gpointer start, gsize length, gint 
+ 
+ #endif /* SIZEOF_OFF_T < 8 */
+ 
+-#if defined(__linux__) && !defined(__GLIBC__)
++#if !defined(__GLIBC__)
+ /* v4l2_ioctl always takes request as unsigned long int, not ioctl_req_t */
+ static gint
+ v4l2_ioctl_wrapper (gint fd, ioctl_req_t request, ...)
+@@ -5285,7 +5285,9 @@ gst_v4l2_object_probe_caps (GstV4l2Object * v4l2object
      if (v4l2object->ioctl (v4l2object->video_fd, VIDIOC_CROPCAP, &cropcap) < 0) {
  
        switch (errno) {
