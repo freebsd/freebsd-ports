26fb608e7efa64efe928c1df909971a69cc53456
Gleb Popov <arrowd@FreeBSD.org>
2025-05-03 19:56:15 +0300

    lang/ghc: Update to 9.6.7

--- lang/ghc/files/patch-hadrian_src_Hadrian_Haskell_Cabal_Parse.hs
+++ lang/ghc/files/patch-hadrian_src_Hadrian_Haskell_Cabal_Parse.hs
@@ -1,6 +1,6 @@
---- hadrian/src/Hadrian/Haskell/Cabal/Parse.hs.orig	2024-01-08 13:24:59 UTC
+--- hadrian/src/Hadrian/Haskell/Cabal/Parse.hs.orig	2025-03-02 13:05:33 UTC
 +++ hadrian/src/Hadrian/Haskell/Cabal/Parse.hs
-@@ -320,8 +320,8 @@ write_inplace_conf pkg_path res_path pd lbi = do
+@@ -321,8 +321,8 @@ write_inplace_conf pkg_path res_path pd lbi = do
                    pkg_name = C.display (C.pkgName (CP.sourcePackageId installedPkgInfo))
                    final_ipi = installedPkgInfo {
                                   Installed.includeDirs = concatMap fixupIncludeDir (Installed.includeDirs installedPkgInfo),
