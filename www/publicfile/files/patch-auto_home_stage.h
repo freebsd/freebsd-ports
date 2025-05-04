c0c38ad61f55c5006a0588e6ce4ee875300f5c57
Rene Ladan <rene@FreeBSD.org>
2025-05-04 14:06:22 +0200

    www/publicfile: Remove expired port
    2025-04-30 www/publicfile: Unmaintained for years upstream and in tree, consider using www/darkhttpd , www/miniserve and/or ftp/tnftpd , ftp/unftp

--- www/publicfile/files/patch-auto_home_stage.h
+++
@@ -1,9 +0,0 @@
---- auto_home_stage.h.orig	2024-10-25 11:20:18 UTC
-+++ auto_home_stage.h
-@@ -0,0 +1,6 @@
-+#ifndef AUTO_HOME_STAGE_H_
-+#define AUTO_HOME_STAGE_H_
-+
-+extern char auto_home[];
-+
-+#endif
