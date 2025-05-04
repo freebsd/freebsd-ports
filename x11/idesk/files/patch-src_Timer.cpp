aa19dc2e4b621e8f47d18d6f52f0006875f1d865
Fernando Apesteguía <fernape@FreeBSD.org>
2025-05-04 00:11:18 -0700

    x11/idesk: Update to 0.7.9
    - Change WWW to idesk fork site
    
    PR:		286386

--- x11/idesk/files/patch-src_Timer.cpp
+++ x11/idesk/files/patch-src_Timer.cpp
@@ -1,5 +1,5 @@
---- src/Timer.cpp.orig	2005-08-15 05:26:39 UTC
-+++ src/Timer.cpp
+--- src/Timer.cpp	2025-04-28 14:46:12.228799000 -0700
++++ src/Timer.cpp	2025-04-28 14:46:20.177517000 -0700
 @@ -26,6 +26,7 @@
  #include "Database.h"
  #include <X11/Xlib.h>
--- x11/idesk/files/patch-src_XImlib2Background.cpp
+++ x11/idesk/files/patch-src_XImlib2Background.cpp
@@ -1,5 +1,5 @@
---- src/XImlib2Background.cpp.orig	2005-11-10 23:08:55 UTC
-+++ src/XImlib2Background.cpp
+--- src/XImlib2Background.cpp	2025-04-28 14:47:32.419474000 -0700
++++ src/XImlib2Background.cpp	2025-04-28 14:48:10.152608000 -0700
 @@ -26,6 +26,7 @@
  #include "Database.h"
  #include "XImlib2Background.h"
