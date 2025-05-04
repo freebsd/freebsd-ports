d14b5cc7f0cea1f1da88be88ce2377ca4af11008
Yasuhiro Kimura <yasu@FreeBSD.org>
2025-05-04 09:10:04 +0900

    security/logcheck: Update to 1.4.4
    Fix DOCS and EXAMPLES options. Previously docs and examples are always
    installed whether these options are enabled or not.
    
    ChangeLog:	https://salsa.debian.org/debian/logcheck/-/blob/debian/1.4.4/debian/changelog

--- security/logcheck/files/patch-debian_logcheck.cron.d
+++ security/logcheck/files/patch-debian_logcheck.cron.d
@@ -1,16 +1,14 @@
---- debian/logcheck.cron.d.orig	2023-07-18 10:54:00 UTC
+--- debian/logcheck.cron.d.orig	2025-04-28 15:28:43 UTC
 +++ debian/logcheck.cron.d
-@@ -1,9 +1,5 @@
--# /etc/cron.d/logcheck: crontab entries for the logcheck package
--
+@@ -1,8 +1,8 @@
+ # /etc/cron.d/logcheck: crontab entries for the logcheck package
+ # These do nothing under systemd because the systemd timer will take precedence
+ 
 -PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-+# crontab entries for the logcheck package
-+PATH=/sbin:/bin:/usr/sbin:/usr/bin:%%PREFIX%%/sbin:%%PREFIX%%/bin
++PATH=%%PREFIX%%/sbin:%%PREFIX%%/bin:/sbin:/bin:/usr/sbin:/usr/bin
  MAILTO=root
--
--@reboot         logcheck    if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck -R; fi
--2 * * * *       logcheck    if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck; fi
--
--# EOF
-+@reboot    if [ -x %%PREFIX%%/sbin/logcheck ]; then /usr/bin/lockf -t 0 /tmp/.logcheck nice -n10 %%PREFIX%%/sbin/logcheck -R; fi
-+2 * * * *  if [ -x %%PREFIX%%/sbin/logcheck ]; then /usr/bin/lockf -t 0 /tmp/.logcheck nice -n10 %%PREFIX%%/sbin/logcheck; fi
+ 
+-@reboot         logcheck    if [ ! -d /run/systemd/system ] && [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck -R; fi
+-2 * * * *       logcheck    if [ ! -d /run/systemd/system ] && [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck; fi
++@reboot         logcheck    if [ ! -d /run/systemd/system ] && [ -x %%PREFIX%%/sbin/logcheck ]; then nice -n10 %%PREFIX%%/sbin/logcheck -R; fi
++2 * * * *       logcheck    if [ ! -d /run/systemd/system ] && [ -x %%PREFIX%%/sbin/logcheck ]; then nice -n10 %%PREFIX%%/sbin/logcheck; fi
