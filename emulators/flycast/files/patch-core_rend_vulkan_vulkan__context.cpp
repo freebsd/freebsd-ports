ea2b117dee4cf490adf27055a216086f4270e8d3
Stefan Schlosser <bsdcode@disroot.org>
2025-05-08 00:20:25 +0300

    emulators/flycast: Update 2.4 => 2.5
    Changelog:
    https://github.com/flyinghead/flycast/releases/tag/v2.5
    
    * Add net/asio to BUILD_DEPENDS.
    * Update to USES=compiler:c++20-lang.
    * Remove upstreamed patches.
    * Cosmetic non-functional and whitespace changes.
    
    PR:	286653

--- emulators/flycast/files/patch-core_rend_vulkan_vulkan__context.cpp
+++ emulators/flycast/files/patch-core_rend_vulkan_vulkan__context.cpp
@@ -1,14 +1,14 @@
---- core/rend/vulkan/vulkan_context.cpp.orig	2024-10-21 14:13:00 UTC
+--- core/rend/vulkan/vulkan_context.cpp.orig	2025-05-07 13:47:18 UTC
 +++ core/rend/vulkan/vulkan_context.cpp
 @@ -42,6 +42,7 @@ VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
  #endif
  
  #include <memory>
 +#include <set>
+ #include <vulkan/vulkan_format_traits.hpp>
  
  void ReInitOSD();
- 
-@@ -146,7 +147,7 @@ bool VulkanContext::InitInstance(const char** extensio
+@@ -147,7 +148,7 @@ bool VulkanContext::InitInstance(const char** extensio
  #if defined(__ANDROID__) && HOST_CPU == CPU_ARM64
  		vkGetInstanceProcAddr = loadVulkanDriver();
  #else
