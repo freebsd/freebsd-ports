326c7139885fdd62580797fada7cea5eaa4e6a3e
Michael Gmelin <grembo@FreeBSD.org>
2025-05-07 12:04:31 +0200

    deskutils/py-paperless-ngx: Update to 2.15.3
    PR:		286533
    Co-authored-by:	Sebastian <sko@rostwald.de>

--- deskutils/py-paperless-ngx/files/patch-src_documents_management_commands_document__consumer.py
+++ deskutils/py-paperless-ngx/files/patch-src_documents_management_commands_document__consumer.py
@@ -1,4 +1,4 @@
---- src/documents/management/commands/document_consumer.py.orig	2024-03-18 03:45:09 UTC
+--- src/documents/management/commands/document_consumer.py.orig	2025-04-19 22:34:42 UTC
 +++ src/documents/management/commands/document_consumer.py
 @@ -292,7 +292,7 @@ class Command(BaseCommand):
              logger.debug(f"Configuring timeout to {timeout_ms}ms")
@@ -7,5 +7,5 @@
 -        inotify_flags = flags.CLOSE_WRITE | flags.MOVED_TO | flags.MODIFY
 +        inotify_flags = flags.CLOSE_WRITE | flags.MOVED_TO | flags.MODIFY | flags.CREATE
          if recursive:
-             descriptor = inotify.add_watch_recursive(directory, inotify_flags)
+             inotify.add_watch_recursive(directory, inotify_flags)
          else:
--- deskutils/py-paperless-ngx/files/patch-src_paperless__mail_parsers.py
+++ deskutils/py-paperless-ngx/files/patch-src_paperless__mail_parsers.py
@@ -1,6 +1,6 @@
---- src/paperless_mail/parsers.py.orig	2024-08-23 03:40:13 UTC
+--- src/paperless_mail/parsers.py.orig	2025-04-19 22:34:42 UTC
 +++ src/paperless_mail/parsers.py
-@@ -9,8 +9,8 @@ from django.conf import settings
+@@ -8,8 +8,8 @@ from django.conf import settings
  from django.utils.timezone import is_naive
  from django.utils.timezone import make_aware
  from gotenberg_client import GotenbergClient
@@ -11,7 +11,7 @@
  from gotenberg_client.options import PageMarginsType
  from gotenberg_client.options import PageSize
  from gotenberg_client.options import PdfAFormat
-@@ -344,10 +344,10 @@ class MailDocumentParser(DocumentParser):
+@@ -370,10 +370,10 @@ class MailDocumentParser(DocumentParser):
                      .resource(css_file)
                      .margins(
                          PageMarginsType(
@@ -26,7 +26,7 @@
                          ),
                      )
                      .size(PageSize(height=11.7, width=8.27))
-@@ -426,10 +426,10 @@ class MailDocumentParser(DocumentParser):
+@@ -452,10 +452,10 @@ class MailDocumentParser(DocumentParser):
              # Set page size, margins
              route.margins(
                  PageMarginsType(
