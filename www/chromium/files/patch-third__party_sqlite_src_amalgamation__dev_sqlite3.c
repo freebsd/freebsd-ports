514893595decb5b138fb22f03ea85d54178d16c5
Robert Nagy <rnagy@FreeBSD.org>
2025-05-07 08:46:43 +0200

    www/chromium: update to 136.0.7103.92
    Security:	https://vuxml.freebsd.org/freebsd/db221414-2b0d-11f0-8cb5-a8a1599412c6.html
    (cherry picked from commit 922f61e47a89bf13b15be574d678be972867fe2e)

--- www/chromium/files/patch-third__party_sqlite_src_amalgamation__dev_sqlite3.c
+++ www/chromium/files/patch-third__party_sqlite_src_amalgamation__dev_sqlite3.c
@@ -1,6 +1,6 @@
---- third_party/sqlite/src/amalgamation_dev/sqlite3.c.orig	2025-02-19 07:43:18 UTC
+--- third_party/sqlite/src/amalgamation_dev/sqlite3.c.orig	2025-05-07 06:38:02 UTC
 +++ third_party/sqlite/src/amalgamation_dev/sqlite3.c
-@@ -14187,7 +14187,7 @@ struct fts5_api {
+@@ -14486,7 +14486,7 @@ struct fts5_api {
  ** But _XOPEN_SOURCE define causes problems for Mac OS X, so omit
  ** it.
  */
--- www/chromium/files/patch-ui_platform__window_platform__window__delegate.h
+++ www/chromium/files/patch-ui_platform__window_platform__window__delegate.h
@@ -1,6 +1,6 @@
---- ui/platform_window/platform_window_delegate.h.orig	2025-04-04 08:52:13 UTC
+--- ui/platform_window/platform_window_delegate.h.orig	2025-05-07 06:38:02 UTC
 +++ ui/platform_window/platform_window_delegate.h
-@@ -140,7 +140,7 @@ class COMPONENT_EXPORT(PLATFORM_WINDOW) PlatformWindow
+@@ -144,7 +144,7 @@ class COMPONENT_EXPORT(PLATFORM_WINDOW) PlatformWindow
    virtual void OnWindowStateChanged(PlatformWindowState old_state,
                                      PlatformWindowState new_state) = 0;
  
