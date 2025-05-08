2113074ccbc5e3f1ae76c6d310f7bff4a73bed33
Andrea Cocito <andrea@cocito.eu>
2025-05-08 08:42:34 +0000

    Ouch, anothe bug

--- security/tpm2-tss/files/patch-src_tss2-tcti_tcti-device.c
+++ security/tpm2-tss/files/patch-src_tss2-tcti_tcti-device.c
@@ -1,11 +1,12 @@
---- src/tss2-tcti/tcti-device.c.orig	2025-02-22 22:43:21 UTC
-+++ src/tss2-tcti/tcti-device.c
-@@ -58,7 +58,7 @@ static char *default_conf[] = {
- #include "util/log.h"
- 
- static char *default_conf[] = {
--#ifdef __VXWORKS__
-+#if defined(__VXWORKS__) || defined(__FreeBSD__)
-     "/dev/tpm0"
+--- src/tss2-tcti/tcti-device.c.orig	2023-01-23 18:36:16.000000000 +0000
++++ src/tss2-tcti/tcti-device.c	2025-05-08 08:40:29.255475000 +0000
+@@ -61,7 +61,9 @@
+ #ifdef __VXWORKS__
+     "/tpm0"
  #else
++#ifndef __FreeBSD__
      "/dev/tpmrm0",
++#endif /* __FreeBSD__ */
+     "/dev/tpm0",
+ #endif /* __VX_WORKS__ */
+ };
