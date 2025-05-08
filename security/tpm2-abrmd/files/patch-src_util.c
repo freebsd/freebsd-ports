12cb3d0fd6dea8c5ec6babeb897e3a4ed09b8c8c
Andrea Cocito <andrea@cocito.eu>
2025-05-08 02:20:54 +0000

    Removed (harmless) debugging leftover

--- security/tpm2-abrmd/files/patch-src_util.c
+++ security/tpm2-abrmd/files/patch-src_util.c
@@ -9,22 +9,3 @@
             const uint8_t *buf,
             const size_t   size)
  {
-@@ -77,14 +77,15 @@ write_all (GOutputStream *ostream,
-     GError *error = NULL;
- 
-     do {
--        g_debug ("%s: writing %zu bytes to ostream", __func__,
--                 size - written_total);
-+        g_debug ("%s: writing %zu bytes to ostream from %zu", __func__,
-+                 size - written_total, buf);
-         written = g_output_stream_write (ostream,
-                                          (const gchar*)&buf [written_total],
-                                          size - written_total,
-                                          NULL,
-                                          &error);
--        switch (written) {
-+        g_debug ("Passed");
-+	switch (written) {
-         case -1:
-             g_assert (error != NULL);
-             g_warning ("%s: failed to write to ostream: %s", __func__, error->message);
