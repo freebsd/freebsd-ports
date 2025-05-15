a6dfbdf7ecd0a6aa68498764cbc970387e62e532
Nuno Teixeira <eduardo@FreeBSD.org>
2025-05-15 20:33:46 +0100

    editors/xmlcopyeditor: Fix build with libxml2 2.14
    PR:		286784
    MFH:		2025Q2
    (cherry picked from commit d01778f696f00156cb4a557ea6605762d33de60d)

--- editors/xmlcopyeditor/files/patch-src_wraplibxml.cpp
+++ editors/xmlcopyeditor/files/patch-src_wraplibxml.cpp
@@ -1,9 +1,38 @@
-Fix with >libxml2-2.12
-https://sourceforge.net/p/xml-copy-editor/bugs/239/
+Fix with libxml2-2.14
+https://sourceforge.net/p/xml-copy-editor/bugs/242/
 
---- src/wraplibxml.cpp.orig	2025-01-23 10:31:01 UTC
+--- src/wraplibxml.cpp.orig	2025-05-15 17:09:04 UTC
 +++ src/wraplibxml.cpp
-@@ -706,7 +706,7 @@ wxString WrapLibxml::getLastError()
+@@ -54,7 +54,11 @@ class Initializer (public)
+ 		xmlLoadCatalog ( CONV ( catalogPath ) );
+ 		::catalog = xmlLoadACatalog ( CONV ( catalogPath ) );
+ 
++#if LIBXML_VERSION < 21400
+ 		initGenericErrorDefaultFunc ( NULL );
++#else
++		xmlSetGenericErrorFunc ( NULL, NULL );
++#endif
+ 	}
+ 
+ 	~Initializer ()
+@@ -472,7 +476,7 @@ std::string WrapLibxml::dumpXPathObject ( xmlXPathObje
+ 	case XPATH_STRING:
+ 		sstream << obj->stringval;
+ 		break;
+-
++#if LIBXML_VERSION < 21400
+ 	case XPATH_POINT:
+ 		xmlBufferPtr bufferPtr;
+ 		bufferPtr = xmlBufferCreate();
+@@ -486,6 +490,7 @@ std::string WrapLibxml::dumpXPathObject ( xmlXPathObje
+ 
+ 	case XPATH_RANGE:
+ 	case XPATH_LOCATIONSET:
++#endif
+ 	case XPATH_XSLT_TREE:
+ 	default:
+ 		FILE *fp = tmpfile();
+@@ -706,7 +711,7 @@ wxString WrapLibxml::getLastError()
  
  wxString WrapLibxml::getLastError()
  {
@@ -12,7 +41,7 @@ https://sourceforge.net/p/xml-copy-editor/bugs/239/
  
  	if ( !err )
  		return nonParserError;
-@@ -722,7 +722,7 @@ std::pair<int, int> WrapLibxml::getErrorPosition()
+@@ -722,7 +727,7 @@ std::pair<int, int> WrapLibxml::getErrorPosition()
  
  std::pair<int, int> WrapLibxml::getErrorPosition()
  {
