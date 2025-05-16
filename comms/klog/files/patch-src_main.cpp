f0cb9ef404dc16456a0ff116a687c29013d97823
Michael Reifenberger <mr@FreeBSD.org>
2025-05-16 18:51:29 +0200

    comms/klog: Update to 2.4.1

--- comms/klog/files/patch-src_main.cpp
+++ comms/klog/files/patch-src_main.cpp
@@ -1,13 +1,11 @@
---- src/main.cpp.orig	2023-12-26 00:00:15 UTC
+--- src/main.cpp.orig	2025-05-16 09:03:24 UTC
 +++ src/main.cpp
-@@ -151,6 +151,10 @@ int main(int argc, char *argv[])
-             myappTranslator.load(QCoreApplication::applicationDirPath() + "/translations/klog_" + (QLocale::system().name())); /* Flawfinder: ignore */
-         }
+@@ -96,7 +96,7 @@ void loadTranslations(QApplication &app, QTranslator &
+         missingTranslation = false;
+     }
  
-+        else if (((QLocale::system().name()).left(1)) == "C") /* Flawfinder: ignore */
-+        { // If language is C, it will execute without showing message
-+
-+        }
-         else if (((QLocale::system().name()).left(2)) == "en") /* Flawfinder: ignore */
-         { // If language is English, it will execute without showing message
- 
+-    if (missingTranslation && language != "en")
++    if (missingTranslation && language != "en" && language != "C"  )
+     {
+         QMessageBox::warning(nullptr, "KLog",
+                              QString("No translation files for your language were found. KLog will be shown in English.\n")
