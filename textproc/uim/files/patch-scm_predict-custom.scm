40c1a7857d24b9384bc19a9a54c26f169e20b49f
MANTANI Nobutaka <nobutaka@FreeBSD.org>
2025-05-18 20:07:40 +0900

    textproc/uim: Update to 1.9.6
    Bump PORTREVISION of the ports that depend on textproc/uim port

--- textproc/uim/files/patch-scm_predict-custom.scm
+++ textproc/uim/files/patch-scm_predict-custom.scm
@@ -1,11 +1,11 @@
---- scm/predict-custom.scm.orig	2025-02-23 08:00:09 UTC
+--- scm/predict-custom.scm.orig	2025-05-16 13:44:13 UTC
 +++ scm/predict-custom.scm
 @@ -110,7 +110,7 @@
                       (N_ "Look-SKK prediction")
                       (N_ "long description will be here."))
  
 -(define-custom 'predict-custom-look-skk-jisyo "/usr/share/skk/SKK-JISYO.L"
-+(define-custom 'predict-custom-look-skk-jisyo "%PREFIX%/share/skk/SKK-JISYO.L"
++(define-custom 'predict-custom-look-skk-jisyo "/usr/local/share/skk/SKK-JISYO.L"
                 '(predict predict-look-skk)
                 '(pathname regular-file)
                 (N_ "Sorted SKK-JISYO dictionary file")
--- textproc/uim/files/patch-scm_predict-look-skk.scm
+++ textproc/uim/files/patch-scm_predict-look-skk.scm
@@ -1,11 +1,11 @@
---- scm/predict-look-skk.scm.orig	2025-02-23 08:00:09 UTC
+--- scm/predict-look-skk.scm.orig	2025-05-16 13:44:13 UTC
 +++ scm/predict-look-skk.scm
 @@ -37,7 +37,7 @@
  
  (define-class predict-look-skk predict
    '((limit 10)
 -    (jisyo "/usr/share/skk/SKK-JISYO.L")) ;; SKK-JISYO
-+    (jisyo "%PREFIX%/share/skk/SKK-JISYO.L")) ;; SKK-JISYO
++    (jisyo "/usr/local/share/skk/SKK-JISYO.L")) ;; SKK-JISYO
    '(search))
  
  (class-set-method! predict-look-skk search
