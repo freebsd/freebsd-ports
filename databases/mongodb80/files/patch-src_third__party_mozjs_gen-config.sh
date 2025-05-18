751840d1b2631c14b8a989051e1d132d65d9548d
Ronald Klop <ronald@FreeBSD.org>
2025-05-18 21:45:40 +0200

    databases/mongodb80: 8.0.9
    Changes:
    https://www.mongodb.com/docs/v8.0/release-notes/8.0/#8.0.9---may-1--2025
    
    Announcement:
    https://www.mongodb.com/community/forums/t/mongodb-8-0-9-is-released/321462?u=r_k
    
    - refactored do-configure to use HAS_CONFIGURE which properly passes
    CONFIGURE_ENV
    - updates spidermonkey to MozJS 128.8-ESR
    - needs cbindgen now
    - pet portclippy and portfmt (moved vars around)
    - replaced multiple paths with MOZJS_WRKSRC
    - removed a mozjs patch which is in upstream now

--- databases/mongodb80/files/patch-src_third__party_mozjs_gen-config.sh
+++ databases/mongodb80/files/patch-src_third__party_mozjs_gen-config.sh
@@ -1,5 +1,11 @@
---- src/third_party/mozjs/gen-config.sh.orig	2024-07-22 19:17:33 UTC
+--- src/third_party/mozjs/gen-config.sh.orig	2025-05-13 17:12:47 UTC
 +++ src/third_party/mozjs/gen-config.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/bin/sh
+ 
+ set -e
+ set -v
 @@ -29,6 +29,9 @@ case "$_Path" in
  }
  
@@ -10,16 +16,16 @@
      "platform/aarch64/linux")
          _CONFIG_OPTS="--host=aarch64-linux"
      ;;
-@@ -109,7 +112,7 @@ CFLAGS="$CFLAGS -D__STDC_FORMAT_MACROS" \
-     --disable-js-shell \
-     --disable-tests "$_CONFIG_OPTS"
+@@ -111,7 +114,7 @@ CFLAGS="$CFLAGS -D__STDC_FORMAT_MACROS" \
+     --disable-wasm-moz-intgemm \
+     "$_CONFIG_OPTS"
  
 -make recurse_export
 +gmake recurse_export
  
  cd ../../../..
  
-@@ -164,7 +167,7 @@ find "$_Path/build" -name '*.cpp' |
+@@ -166,7 +169,7 @@ find "$_Path/build" -name '*.cpp' |
  find "$_Path/build" -name '*.cpp' |
      while read unified_file ; do
          echo "Processing $unified_file"
