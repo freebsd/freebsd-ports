41c76d28e1333c820f87932bda2bdb0f17344336
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-05-18 11:17:35 +0800

    devel/py-awscrt: Update to 0.27.0
    Changes:	https://github.com/awslabs/aws-crt-python/releases

---
+++ devel/py-awscrt/files/patch-awscrt-__init__.py
@@ -0,0 +1,11 @@
+--- awscrt/__init__.py.orig	2025-04-25 19:28:57 UTC
++++ awscrt/__init__.py
+@@ -13,7 +13,7 @@ __all__ = [
+     'websocket',
+ ]
+ 
+-__version__ = '1.0.0.dev0'
++__version__ = '0.27.0'
+ 
+ 
+ class NativeResource:
