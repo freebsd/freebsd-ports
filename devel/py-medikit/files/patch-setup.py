f1f7cb838f5e236f04c8a3084bd60e67d1825698
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-05-18 11:18:15 +0800

    devel/py-medikit: Allow build with py-whistle 2.0.0+
    - Bump PORTREVISION for package change

--- devel/py-medikit/files/patch-setup.py
+++ devel/py-medikit/files/patch-setup.py
@@ -1,6 +1,6 @@
 --- setup.py.orig	2020-11-25 07:42:37 UTC
 +++ setup.py
-@@ -56,12 +56,12 @@ setup(
+@@ -56,13 +56,13 @@ setup(
      include_package_data=True,
      install_requires=[
          "git-semver ~= 0.3.2",
@@ -11,8 +11,10 @@
          "pip-tools ~= 4.5.0",
 -        "semantic_version < 2.7",
 -        "stevedore ~= 3.0",
+-        "whistle ~= 1.0",
 +        "semantic_version ~= 2.8",
 +        "stevedore >= 3.0",
-         "whistle ~= 1.0",
++        "whistle >= 1.0",
          "yapf ~= 0.20",
      ],
+     extras_require={
