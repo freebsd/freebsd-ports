54c18e0eab87ac61139d9f8f5838362480e2a981
Olivier Cochard <olivier@FreeBSD.org>
2025-05-18 11:25:11 +0800

    devel/py-configargparse: Update to 1.7
    Changes:	https://github.com/bw2/ConfigArgParse/releases
    PR:		285680

--- devel/py-configargparse/files/patch-setup.py
+++ devel/py-configargparse/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2021-10-03 00:16:57 UTC
+--- setup.py.orig	2023-07-23 14:39:04 UTC
 +++ setup.py
-@@ -80,7 +80,7 @@ setup(
+@@ -81,7 +81,7 @@ setup(
  
  
  setup(
 -    name='ConfigArgParse',
 +    name='configargparse',
-     version="1.5.3",
+     version="1.7",
      description='A drop-in replacement for argparse that allows options to '
                  'also be set via config files and/or environment variables.',
