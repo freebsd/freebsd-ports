dbd4812caf0fd7bb757c67d76cdfc9b5481d2233
Alastair Hogge <agh@riseup.net>
2025-05-21 16:39:13 +0300

    graphics/opencv: Update to 4.11.0
    PR:		286423

--- graphics/opencv/files/patch-modules_gapi_cmake_DownloadADE.cmake
+++ graphics/opencv/files/patch-modules_gapi_cmake_DownloadADE.cmake
@@ -1,12 +1,12 @@
 The ade distdata is downloaded and moved to the proper location by the ports 
 framework.
 
---- modules/gapi/cmake/DownloadADE.cmake.orig	2023-12-27 16:46:55 UTC
+--- modules/gapi/cmake/DownloadADE.cmake.orig	2025-04-26 12:11:25 UTC
 +++ modules/gapi/cmake/DownloadADE.cmake
-@@ -2,18 +2,12 @@ set(ade_md5 "dbb095a8bf3008e91edbbf45d8d34885")
- set(ade_filename "v0.1.2d.zip")
- set(ade_subdir "ade-0.1.2d")
- set(ade_md5 "dbb095a8bf3008e91edbbf45d8d34885")
+@@ -2,18 +2,12 @@ set(ade_md5 "962ce79e0b95591f226431f7b5f152cd")
+ set(ade_filename "v0.1.2e.zip")
+ set(ade_subdir "ade-0.1.2e")
+ set(ade_md5 "962ce79e0b95591f226431f7b5f152cd")
 -ocv_download(FILENAME ${ade_filename}
 -             HASH ${ade_md5}
 -             URL
@@ -19,7 +19,7 @@ framework.
 -             UNPACK RELATIVE_URL)
 -
 -if (NOT res)
-+set(file_id ade-0.1.2d/CMakeLists.txt)
++set(file_id ade-0.1.2e/CMakeLists.txt)
 +set(FILENAME ${ade_src_dir}/${file_id})
 +if(EXISTS ${FILENAME})
 +    message("++ ade: Using prefetched ${file_id}")
