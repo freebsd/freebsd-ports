e5183ae44146db83a3b55946df6584c2a8fdcf9f
Sergey A. Osokin <osa@FreeBSD.org>
2025-05-23 16:45:10 -0400

    net-im/tde2e: fix post-patch phase
    No functional changes.

--- net-im/tde2e/files/patch-CMake_GetGitRevisionDescription.cmake
+++ net-im/tde2e/files/patch-CMake_GetGitRevisionDescription.cmake
@@ -5,7 +5,7 @@
    if (GIT_DIR STREQUAL "")
      set(${_refspecvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
 -    set(${_hashvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
-+    set(${_hashvar} "51743df" PARENT_SCOPE)
++    set(${_hashvar} "%%COMMIT%%" PARENT_SCOPE)
      return()
    endif()
  
