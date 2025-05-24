07069946d6affbbe2c53ec6e8233de629cfe4a7d
Mathieu Arnold <mat@FreeBSD.org>
2025-05-24 19:59:59 +0200

    Mk/Features/*: try to add some explanations of what the features are about

--- Mk/Features/bind_now.mk
+++ Mk/Features/bind_now.mk
@@ -1,4 +1,9 @@
 # BIND_NOW Support
+#
+# When generating an executable or shared library, mark it to tell the dynamic
+# linker to resolve all symbols when the program is started, or when the shared
+# library is loaded by dlopen, instead of deferring function call resolution to
+# the point when the function is first called.
 
 .if !defined(_BIND_NOW_MK_INCLUDED)
 _BIND_NOW_MK_INCLUDED=	yes
--- Mk/Features/relro.mk
+++ Mk/Features/relro.mk
@@ -1,4 +1,9 @@
 # RELRO Support
+#
+# Tells the linker to emit RELocation Read-Only (RELRO) protection for certain
+# sections of your ELF file. In short, it makes parts of the binary read-only
+# after relocations have been applied at program startup, helping to prevent
+# GOT- and PLT-based overwrite attacks.
 
 .if !defined(_RELRO_MK_INCLUDED)
 _RELRO_MK_INCLUDED=	yes
