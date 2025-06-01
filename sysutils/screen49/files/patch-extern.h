b1ec099e21357d5115b3bb5ab38502cedcc3d75c
Rene Ladan <rene@FreeBSD.org>
2025-06-01 17:55:50 +0200

    sysutils/screen49: Remove expired port
    2025-05-31 sysutils/screen49: Multiple CVEs

--- sysutils/screen49/files/patch-extern.h
+++
@@ -1,8 +0,0 @@
---- extern.h.orig	2023-08-15 08:44:47 UTC
-+++ extern.h
-@@ -512,3 +512,5 @@
- /* layout.c */
- extern void  RemoveLayout __P((struct layout *));
- extern int   LayoutDumpCanvas __P((struct canvas *, char *));
-+
-+extern time_t SessionCreationTime __P((const char *));
