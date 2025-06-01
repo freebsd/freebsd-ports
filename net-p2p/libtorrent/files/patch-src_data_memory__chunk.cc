5bb5070238f9977dee333abcc7ff7ca615b512c5
Nuno Teixeira <eduardo@FreeBSD.org>
2025-06-01 18:51:13 +0100

    net-p2p/{lib,r}torrent: Update to 0.15.4
    ChangeLog:	https://github.com/rakshasa/rtorrent/releases/tag/v0.15.4
    MFH:		2025Q2 (bugfix release)

--- net-p2p/libtorrent/files/patch-src_data_memory__chunk.cc
+++ net-p2p/libtorrent/files/patch-src_data_memory__chunk.cc
@@ -1,6 +1,6 @@
---- src/data/memory_chunk.cc.orig	2010-02-10 09:55:42 UTC
+--- src/data/memory_chunk.cc.orig	2025-06-01 13:09:26 UTC
 +++ src/data/memory_chunk.cc
-@@ -86,6 +86,9 @@
+@@ -66,6 +66,9 @@ MemoryChunk::unmap() {
    if (!is_valid())
      throw internal_error("MemoryChunk::unmap() called on an invalid object");
  
@@ -9,4 +9,4 @@
 +
    if (munmap(m_ptr, m_end - m_ptr) != 0)
      throw internal_error("MemoryChunk::unmap() system call failed: " + std::string(rak::error_number::current().c_str()));
- }  
+ }
