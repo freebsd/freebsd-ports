114e7181eb2c736e384d6b544889e638e6e0c4e7
jan <jan@glaubitz.org>
2025-06-02 22:02:31 +0300

    multimedia/tautulli: Update 2.13.4 => 2.15.2
    Changelogs:
    https://github.com/Tautulli/Tautulli/releases/tag/v2.14.2
    https://github.com/Tautulli/Tautulli/releases/tag/v2.14.3
    https://github.com/Tautulli/Tautulli/releases/tag/v2.14.4
    https://github.com/Tautulli/Tautulli/releases/tag/v2.14.5
    https://github.com/Tautulli/Tautulli/releases/tag/v2.14.6
    https://github.com/Tautulli/Tautulli/releases/tag/v2.15.0
    https://github.com/Tautulli/Tautulli/releases/tag/v2.15.1
    https://github.com/Tautulli/Tautulli/releases/tag/v2.15.2
    
    Refresh patch.
    
    PR:		286469
    Approved by:	christer.edwards@gmail.com (maitainer, timeout > 1 month)

--- multimedia/tautulli/files/patch-plexpy_config.py
+++ multimedia/tautulli/files/patch-plexpy_config.py
@@ -1,6 +1,6 @@
---- plexpy/config.py.orig	2022-04-22 21:51:37.128263000 -0600
-+++ plexpy/config.py	    2022-04-22 21:51:43.034517000 -0600
-@@ -90,7 +90,7 @@
+--- plexpy/config.py.orig	2025-04-12 23:02:46 UTC
++++ plexpy/config.py
+@@ -84,7 +84,7 @@ _CONFIG_DEFINITIONS = {
      'CACHE_IMAGES': (int, 'General', 1),
      'CACHE_SIZEMB': (int, 'Advanced', 32),
      'CHECK_DOCKER_MOUNT': (int, 'Advanced', 1),
