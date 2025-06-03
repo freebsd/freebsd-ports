7db750b4c241145c5eb3a9a00f2ae78afaac391b
Matthias Andree <mandree@FreeBSD.org>
2025-06-03 19:22:25 +0200

    dns/dnsmasq-devel: update to v2.92test11
    Changes since v2.92test10:
    
    * c378d2c 2025-06-02 | Fix crash in filter_servers(). (tag: v2.92test11) [Simon Kelley]
    * 5846f74 2025-05-29 | Rewrite chunks of inotify.c to remove a memory leak. [Simon Kelley]
    * c9a4240 2025-05-24 | Unbreak compilation on non-Linux systems (FreeBSD) [Matthias Andree]
    
    (The latter was already in FreeBSD's port.)
    
    MFH:		2025Q2

--- dns/dnsmasq-devel/files/update.py
+++ dns/dnsmasq-devel/files/update.py
@@ -25,16 +25,28 @@ cleanenv={'LC_ALL': 'C.UTF-8',
           'PATH': os.environ["PATH"]}
 defargs={"check": "True", "env": cleanenv, "encoding": 'UTF-8'}
 
-distdir, master_site = map(str.strip, traced_run(['make', '-V', 'DISTDIR', '-V', 'MASTER_SITES:N*FreeBSD*'], capture_output=True, **defargs).stdout.splitlines())
-filename_tarball = traced_run('make -V DISTFILES'.split(), capture_output=True, **defargs).stdout.splitlines()[0].strip()
-filename_signature = filename_tarball + '.asc'
-uri_tarball = master_site + filename_tarball
-uri_signature = master_site + filename_signature
-traced_run(['fetch', uri_tarball, uri_signature], **defargs)
-traced_run(['gpg', '--verify', filename_signature, filename_tarball], **defargs)
-traced_run(['rsync', '-avHPW', '--chmod=0644', filename_tarball, filename_signature, 'freefall.freebsd.org:public_distfiles/'], **defargs)
-shutil.move(filename_tarball, '/usr/ports/distfiles/' + filename_tarball)
-os.remove(filename_signature)
-traced_run(['make', 'makesum', 'clean'], **defargs)
-traced_run(['make', 'check-plist', 'package'], **defargs)
-print("\nSUCCESS\n")
+try:
+    distdir, master_site, files_dir, dist_dir = map(str.strip, traced_run(['make', '-V', 'DISTDIR',
+                                                                           '-V', 'MASTER_SITES:N*FreeBSD*',
+                                                                           '-V', 'FILESDIR',
+                                                                           '-V', 'DISTDIR'],
+                                                                          capture_output=True, **defargs).stdout.splitlines())
+    filename_tarball = traced_run('make -V DISTFILES'.split(), capture_output=True, **defargs).stdout.splitlines()[0].strip()
+    filename_signature = filename_tarball + '.asc'
+    uri_tarball = master_site + filename_tarball
+    uri_signature = master_site + filename_signature
+    traced_run(['fetch', uri_tarball, uri_signature], **defargs)
+    traced_run(['gpg', '--no-options', '--with-colons', '--status-fd', '1',
+                                        '--no-default-keyring', '--keyring', files_dir + '/simon-kelley-keyring.asc',
+                                        '--verify', filename_signature, filename_tarball], **defargs)
+    traced_run(['rsync', '-avHPW', '--chmod=0644', filename_tarball, filename_signature, 'freefall.freebsd.org:public_distfiles/'], **defargs)
+    shutil.move(filename_tarball, dist_dir + '/' + filename_tarball)
+    traced_run(['make', 'makesum', 'clean'], **defargs)
+    os.remove(filename_signature)
+    traced_run(['make', 'check-plist', 'package'], **defargs)
+    print("\nSUCCESS\n")
+except Exception as cpe:
+    print("\nERROR\n")
+    print(repr(cpe))
+    print("\nERROR\n")
+    sys.exit(1)
