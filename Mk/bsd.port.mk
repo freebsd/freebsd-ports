3be82964f866f291e3be35ce41c6146f46bd134c
Rene Ladan <rene@FreeBSD.org>
2025-06-07 00:32:08 +0200

    Mk/bsd.port.mk: mark FreeBSD 13.4 as unsupported.
    Drop support for FreeBSD 13.4 (and sometimes 12, 14.0, 14.1) from all
    ports.
    
    Reviewed by:	dch, @gor_clogic.com.ua
    Approved by:	portmgr (implicit)
    Differential Revision:	https://reviews.freebsd.org/D50769

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1165,7 +1165,7 @@ OSVERSION!=	${AWK} '/^\#define[[:blank:]]__FreeBSD_version/ {print $$3}' < ${SRC
 .    endif
 _EXPORTED_VARS+=	OSVERSION
 
-.    if ${OPSYS} == FreeBSD && (${OSVERSION} < 1304000 || (${OSVERSION} >= 1400000 && ${OSVERSION} < 1402000))
+.    if ${OPSYS} == FreeBSD && (${OSVERSION} < 1305000 || (${OSVERSION} >= 1400000 && ${OSVERSION} < 1402000))
 _UNSUPPORTED_SYSTEM_MESSAGE=	Ports Collection support for your ${OPSYS} version has ended, and no ports\
 								are guaranteed to build on this system. Please upgrade to a supported release.
 .      if defined(ALLOW_UNSUPPORTED_SYSTEM)
