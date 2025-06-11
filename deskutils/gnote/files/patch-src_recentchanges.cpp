8b6e50b876600f4c57a8e49179aab49837c5e429
Olivier Duchateau <duchateau.olivier@gmail.com>
2025-06-11 13:35:17 +0200

    deskutils/gnote: update to 47.2
    PR:	286697

--- deskutils/gnote/files/patch-src_recentchanges.cpp
+++ deskutils/gnote/files/patch-src_recentchanges.cpp
@@ -1,10 +1,10 @@
---- src/recentchanges.cpp.orig	2021-09-01 22:35:10 UTC
+--- src/recentchanges.cpp.orig	2023-09-25 18:30:51 UTC
 +++ src/recentchanges.cpp
-@@ -842,7 +842,6 @@ namespace gnote {
+@@ -896,7 +896,6 @@ namespace gnote {
  
-   Gtk::PopoverMenu *NoteRecentChanges::make_window_menu(Gtk::Button *button, std::vector<PopoverWidget> && items)
+   Gtk::Popover *NoteRecentChanges::make_window_menu(Gtk::Button *button, std::vector<PopoverWidget> && items)
    {
 -    std::sort(items.begin(), items.end());
-     Gtk::PopoverMenu *menu = manage(new Gtk::PopoverMenu);
-     Gtk::Box *menu_box = manage(new Gtk::Box(Gtk::ORIENTATION_VERTICAL));
-     utils::set_common_popover_widget_props(*menu_box);
+     auto menu = Gio::Menu::create();
+     if(items.size() > 0) {
+       auto iter = items.begin();
