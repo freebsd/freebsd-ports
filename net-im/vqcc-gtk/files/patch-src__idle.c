12909e529a031acc5be9b338e3d7ef74fca48a3d
Vladimir Druzenko <vvd@FreeBSD.org>
2025-06-14 00:21:30 +0300

    net-im/vqcc-gtk: Improve port, set maintainer
    No functinal changes:
    - Replace PORTVERSION with DISTVERSION.
    - Remove "?" from "CATEGORIES?=" and "MAINTAINER?=".
    - Set maintainer.
    - Remove localbase:ldflags and pathfix from USES - they are unnecessary.
    - Refresh patch.
    
    PR:		285185
    Approved by:	Edward.Sanford.Sutton, III <mirror176@hotmail.com> (new maintainer)

--- net-im/vqcc-gtk/files/patch-src__idle.c
+++ net-im/vqcc-gtk/files/patch-src__idle.c
@@ -1,6 +1,6 @@
---- src/idle.c.orig	Tue Jan  4 05:36:55 2005
-+++ src/idle.c	Mon Jul 17 03:53:55 2006
-@@ -54,6 +54,12 @@
+--- src/idle.c.orig	2005-01-03 20:36:55 UTC
++++ src/idle.c
+@@ -54,6 +54,12 @@ idle_check_cb(gpointer dummy)
  idle_check_cb(gpointer dummy)
  {
  	unsigned idle_time; /* idle time in minutes */
@@ -13,7 +13,7 @@
  
  	/* do not enter auto-away or auto-offline when we're in invisible mode */
  	if(my_mode()==UMODE_INVISIBLE)
-@@ -63,8 +69,6 @@
+@@ -63,8 +69,6 @@ idle_check_cb(gpointer dummy)
  	idle_time = winvqcc_get_last_active() / 60000;
  #else
  #ifdef USE_XSCREENSAVER
