b03942800a9aa2a3a56b8945b20704ca785bd887
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-06-13 14:03:08 +0800

    devel/level-zero: Update to 1.22.3
    Changes:	https://github.com/oneapi-src/level-zero/releases

--- devel/level-zero/files/patch-source-utils-logging.h
+++ devel/level-zero/files/patch-source-utils-logging.h
@@ -1,18 +1,20 @@
---- source/utils/logging.h.orig	2025-04-10 20:05:03 UTC
+--- source/utils/logging.h.orig	2025-05-23 14:18:52 UTC
 +++ source/utils/logging.h
-@@ -29,11 +29,9 @@
+@@ -29,11 +29,11 @@
  #include "ze_api.h"
  #include "ze_util.h"
  
 -#ifdef __linux__
++#if defined(__FreeBSD__) || defined(__linux__)
  #include <unistd.h>
  #include <sys/types.h>
  #include <pwd.h>
 -#endif // __linux__
++#endif // defined(__FreeBSD__) || defined(__linux__)
  
  namespace loader {
  
-@@ -235,4 +233,4 @@ inline std::shared_ptr<Logger> createLogger() {
+@@ -235,4 +235,4 @@ inline std::shared_ptr<Logger> createLogger() {
  
  } // namespace loader
  
