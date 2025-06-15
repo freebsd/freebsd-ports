174a2402fd43bf56afc7e28582f095f4a94fdfa3
Jimmy Olgeni <olgeni@FreeBSD.org>
2025-06-15 12:55:00 +0200

    sysutils/webmin: update to version 2.401
    PR:		273176, 280163
    Security:	CVE-2024-12828
    (cherry picked from commit a60aa4c9da7018cb42762619c4a797060c57cef0)

--- sysutils/webmin/files/patch-setup.sh
+++ sysutils/webmin/files/patch-setup.sh
@@ -1,6 +1,6 @@
---- setup.sh.orig	2022-10-01 06:55:59 UTC
+--- setup.sh.orig
 +++ setup.sh
-@@ -13,6 +13,13 @@ if [ "$bootscript" = "" ]; then
+@@ -13,6 +13,13 @@
  	bootscript="webmin"
  fi
  
@@ -14,7 +14,7 @@
  cd `dirname $0`
  if [ -x /bin/pwd ]; then
  	wadir=`/bin/pwd`
-@@ -112,7 +119,7 @@ echo "you can just accept the defaults."
+@@ -113,7 +120,7 @@
  echo ""
  envetcdir="$config_dir"
  if [ "$envetcdir" = "" ]; then
@@ -23,7 +23,7 @@
  	envetcdirnotfound=1
  fi
  printf "Config file directory [$envetcdir]: "
-@@ -120,7 +127,7 @@ if [ "$config_dir" = "" ]; then
+@@ -121,7 +128,7 @@
  	read config_dir
  fi
  if [ "$config_dir" = "" ]; then
@@ -32,16 +32,7 @@
  fi
  abspath=`echo $config_dir | grep "^/"`
  if [ "$abspath" = "" ]; then
-@@ -214,7 +221,7 @@ if [ "$upgrading" = 1 ]; then
- 	if [ $? != "0" ]; then
- 		echo "logout=$config_dir/logout-flag" >> $config_dir/miniserv.conf
- 	fi
--	
-+
- 	# Check for third-party modules in old version
- 	if [ "$wadir" != "$oldwadir" ]; then
- 		echo "Checking for third-party modules .."
-@@ -231,7 +238,7 @@ if [ "$upgrading" = 1 ]; then
+@@ -232,7 +239,7 @@
  else
  	# Config directory exists .. make sure it is not in use
  	ls $config_dir | grep -v rpmsave >/dev/null 2>&1
@@ -50,7 +41,7 @@
  		echo "ERROR: Config directory $config_dir is not empty"
  		echo ""
  		exit 2
-@@ -240,7 +247,7 @@ else
+@@ -241,7 +248,7 @@
  	# Ask for log directory
  	envvardir="$var_dir"
  	if [ "$envvardir" = "" ]; then
@@ -59,7 +50,7 @@
  		envvardirnotfound=1
  	fi
  	printf "Log file directory [$envvardir]: "
-@@ -248,7 +255,7 @@ else
+@@ -249,7 +256,7 @@
  		read var_dir
  	fi
  	if [ "$var_dir" = "" ]; then
@@ -68,27 +59,3 @@
  	fi
  	abspath=`echo $var_dir | grep "^/"`
  	if [ "$abspath" = "" ]; then
-@@ -305,7 +312,7 @@ else
- 	fi
- 	echo ""
- 
--	# Test perl 
-+	# Test perl
- 	echo "Testing Perl .."
- 	if [ ! -x $perl ]; then
- 		echo "ERROR: Failed to find perl at $perl"
-@@ -780,7 +787,7 @@ if [ -x "$systemctlcmd" ]; then
- 
- 	# Fix existing systemd webmin.service file to update start and stop commands
- 	(cd "$wadir/init" ; WEBMIN_CONFIG=$config_dir WEBMIN_VAR=$var_dir "$wadir/init/updateboot.pl" "$bootscript")
--	
-+
- 	chmod 755 $config_dir/stop $config_dir/start $config_dir/restart $config_dir/restart-by-force-kill $config_dir/reload $config_dir/.pre-install $config_dir/.post-install
- else
- 	# Creating symlinks
-@@ -1053,5 +1060,3 @@ if [ "$oldwadir" != "$wadir" -a "$upgrading" = 1 -a "$
- 	echo "version."
- 	echo ""
- fi
--
--
