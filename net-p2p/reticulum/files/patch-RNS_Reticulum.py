6bb8492e4ff2f5a36767eb2e58941153b22b7d6b
Carlo Strub <cs@FreeBSD.org>
2025-06-15 22:09:58 +0200

    net-p2p/reticulum: Update to 0.9.6
    - License changes to Reticulum License

--- net-p2p/reticulum/files/patch-RNS_Reticulum.py
+++ net-p2p/reticulum/files/patch-RNS_Reticulum.py
@@ -1,6 +1,6 @@
---- RNS/Reticulum.py.orig	2022-11-03 16:31:20 UTC
+--- RNS/Reticulum.py.orig	2025-05-15 20:24:33 UTC
 +++ RNS/Reticulum.py
-@@ -141,6 +141,7 @@ class Reticulum:
+@@ -165,6 +165,7 @@ class Reticulum:
      # named ".reticulum" inside the current users home directory
      userdir          = os.path.expanduser("~")
      configdir        = None
@@ -8,30 +8,33 @@
      configpath       = ""
      storagepath      = ""
      cachepath        = ""
-@@ -181,7 +182,9 @@ class Reticulum:
+@@ -231,8 +232,11 @@ class Reticulum:
          if configdir != None:
              Reticulum.configdir = configdir
          else:
 -            if os.path.isdir("/etc/reticulum") and os.path.isfile("/etc/reticulum/config"):
 +            if os.path.isdir("/usr/local/etc/reticulum") and os.path.isfile("/usr/local/etc/reticulum/config"):
-+                Reticulum.configdir = "/usr/local/etc/reticulum"
-+            elif os.path.isdir("/etc/reticulum") and os.path.isfile("/etc/reticulum/config"):
                  Reticulum.configdir = "/etc/reticulum"
++                Reticulum.interfacepath = Reticulum.configdir+"interfaces"
++            elif os.path.isdir("/etc/reticulum") and os.path.isfile("/etc/reticulum/config"):
++                Reticulum.configdir = "/etc/reticulum"
              elif os.path.isdir(Reticulum.userdir+"/.config/reticulum") and os.path.isfile(Reticulum.userdir+"/.config/reticulum/config"):
                  Reticulum.configdir = Reticulum.userdir+"/.config/reticulum"
-@@ -193,10 +196,7 @@ class Reticulum:
-             RNS.logfile = Reticulum.configdir+"/logfile"
+             else:
+@@ -246,11 +250,8 @@ class Reticulum:
+             RNS.logcall = logdest
          
          Reticulum.configpath    = Reticulum.configdir+"/config"
 -        Reticulum.storagepath   = Reticulum.configdir+"/storage"
 -        Reticulum.cachepath     = Reticulum.configdir+"/storage/cache"
 -        Reticulum.resourcepath  = Reticulum.configdir+"/storage/resources"
 -        Reticulum.identitypath  = Reticulum.configdir+"/storage/identities"
+         Reticulum.interfacepath = Reticulum.configdir+"/interfaces"
 +        Reticulum.dbdir         = Reticulum.configdir
  
          Reticulum.__transport_enabled = False
-         Reticulum.__use_implicit_proof = True
-@@ -226,18 +226,6 @@ class Reticulum:
+         Reticulum.__link_mtu_discovery = Reticulum.LINK_MTU_DISCOVERY
+@@ -291,21 +292,6 @@ class Reticulum:
          self.last_data_persist = time.time()
          self.last_cache_clean = 0
  
@@ -41,25 +44,19 @@
 -        if not os.path.isdir(Reticulum.cachepath):
 -            os.makedirs(Reticulum.cachepath)
 -
+-        if not os.path.isdir(os.path.join(Reticulum.cachepath, "announces")):
+-            os.makedirs(os.path.join(Reticulum.cachepath, "announces"))
+-
 -        if not os.path.isdir(Reticulum.resourcepath):
 -            os.makedirs(Reticulum.resourcepath)
 -
 -        if not os.path.isdir(Reticulum.identitypath):
 -            os.makedirs(Reticulum.identitypath)
 -
-         if os.path.isfile(self.configpath):
-             try:
-                 self.config = ConfigObj(self.configpath)
-@@ -253,7 +241,7 @@ class Reticulum:
+         if not os.path.isdir(Reticulum.interfacepath):
+             os.makedirs(Reticulum.interfacepath)
  
-         self.__apply_config()
-         RNS.log("Configuration loaded from "+self.configpath, RNS.LOG_VERBOSE)
--        
-+
-         RNS.Identity.load_known_destinations()
- 
-         RNS.Transport.start(self)
-@@ -345,6 +333,9 @@ class Reticulum:
+@@ -450,6 +436,9 @@ class Reticulum:
          if "reticulum" in self.config:
              for option in self.config["reticulum"]:
                  value = self.config["reticulum"][option]
@@ -69,20 +66,24 @@
                  if option == "share_instance":
                      value = self.config["reticulum"].as_bool(option)
                      self.share_instance = value
-@@ -369,6 +360,23 @@ class Reticulum:
-                     if v == False:
-                         Reticulum.__use_implicit_proof = False
+@@ -531,6 +520,27 @@ class Reticulum:
+         if self.local_socket_path == None and self.use_af_unix:
+             self.local_socket_path = "default"
  
++
 +        Reticulum.storagepath   = Reticulum.dbdir+"/storage"
 +        Reticulum.cachepath     = Reticulum.dbdir+"/storage/cache"
 +        Reticulum.resourcepath  = Reticulum.dbdir+"/storage/resources"
 +        Reticulum.identitypath  = Reticulum.dbdir+"/storage/identities"
-+        
++
 +        if not os.path.isdir(Reticulum.storagepath):
 +            os.makedirs(Reticulum.storagepath)
 +
 +        if not os.path.isdir(Reticulum.cachepath):
 +            os.makedirs(Reticulum.cachepath)
++
++        if not os.path.isdir(os.path.join(Reticulum.cachepath, "announces")):
++            os.makedirs(os.path.join(Reticulum.cachepath, "announces"))
 +
 +        if not os.path.isdir(Reticulum.resourcepath):
 +            os.makedirs(Reticulum.resourcepath)
@@ -93,12 +94,12 @@
          self.__start_local_interface()
  
          if self.is_shared_instance or self.is_standalone_instance:
-@@ -1275,6 +1283,9 @@ __default_rns_config__ = '''# This is the default Reti
+@@ -1360,6 +1370,9 @@ __default_rns_config__ = '''# This is the default Reti
  
  
  [reticulum]
 +
-+# Define the DB directory
++# Define database directory
 +dbdir = "/var/db/reticulum"
  
  # If you enable Transport, your system will route traffic
--- net-p2p/reticulum/files/patch-RNS_Utilities_rnsd.py
+++ net-p2p/reticulum/files/patch-RNS_Utilities_rnsd.py
@@ -1,10 +1,10 @@
---- RNS/Utilities/rnsd.py.orig	2023-02-23 17:25:44 UTC
+--- RNS/Utilities/rnsd.py.orig	2025-05-15 20:24:33 UTC
 +++ RNS/Utilities/rnsd.py
-@@ -80,6 +80,9 @@ __example_rns_config__ = '''# This is an example Retic
+@@ -93,6 +93,9 @@ __example_rns_config__ = '''# This is an example Retic
  
  [reticulum]
  
-+# Define the DB directory
++# Define database directory
 +dbdir = "/var/db/reticulum"
 +
  # If you enable Transport, your system will route traffic
