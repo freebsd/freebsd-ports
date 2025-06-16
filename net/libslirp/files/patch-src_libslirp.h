80bdef7012135b06f1bce4fd32f99900bd44f35f
Max Brazhnikov <makc@FreeBSD.org>
2025-06-16 19:20:51 +0300

    net/libslirp: update to 4.9.1
    Release notes:
       https://gitlab.freedesktop.org/slirp/libslirp/-/releases/v4.9.1
    
    Port changes:
     - Remove stale patch

--- net/libslirp/files/patch-src_libslirp.h
+++ net/libslirp/files/patch-src_libslirp.h
@@ -1,10 +1,10 @@
---- src/libslirp.h.orig	2021-06-18 07:49:56 UTC
+--- src/libslirp.h.orig	2025-05-27 22:38:05 UTC
 +++ src/libslirp.h
-@@ -12,6 +12,7 @@
- #include <in6addr.h>
- #else
+@@ -26,6 +26,7 @@ typedef ssize_t slirp_ssize_t;
+ #include <sys/types.h>
+ typedef ssize_t slirp_ssize_t;
  #include <netinet/in.h>
 +#include <sys/socket.h>
  #include <arpa/inet.h>
+ #define SLIRP_EXPORT
  #endif
- 
