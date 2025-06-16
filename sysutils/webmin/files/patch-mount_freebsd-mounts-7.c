f8048bff7d9389e27bdb2497986089d05b51479c
Jimmy Olgeni <olgeni@FreeBSD.org>
2025-06-16 07:42:44 +0200

    sysutils/webmin: merge missing bits from PR 280163
    - freebsd-mounts-7.c patch is no longer necessary
    - ajaxterm is no longer shipped, but acme_tiny.py needs compiled
    
    PR:		280163

--- sysutils/webmin/files/patch-mount_freebsd-mounts-7.c
+++
@@ -1,8 +0,0 @@
---- mount/freebsd-mounts-7.c.orig	2015-03-13 23:51:39 UTC
-+++ mount/freebsd-mounts-7.c
-@@ -1,3 +1,5 @@
-+#include <stdlib.h>
-+#include <string.h>
- #include <stdio.h>
- #include <errno.h>
- #include <sys/param.h>
