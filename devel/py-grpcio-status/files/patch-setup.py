79ce56988e5048bb18bd603fd160aab60b6a4184
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-06-18 00:33:42 +0800

    devel/py-grpcio-status: Update to 1.72.1
    Changes:	https://github.com/grpc/grpc/commits/master/src/python/grpcio_status

--- devel/py-grpcio-status/files/patch-setup.py
+++ devel/py-grpcio-status/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2025-04-30 13:49:55 UTC
+--- setup.py.orig	2025-05-30 16:23:52 UTC
 +++ setup.py
 @@ -63,7 +63,7 @@ INSTALL_REQUIRES = (
  }
  
  INSTALL_REQUIRES = (
--    "protobuf>=6.30.0,<7.0dev",
-+    "protobuf>=5.26.1,<7.0dev",
+-    "protobuf>=6.30.0,<7.0.0",
++    "protobuf>=5.26.1,<7.0.0",
      "grpcio>={version}".format(version=grpc_version.VERSION),
      "googleapis-common-protos>=1.5.5",
  )
