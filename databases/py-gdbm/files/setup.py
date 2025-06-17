a73665376a539a144ff74abaab04f6d350f7bdb4
Charlie Li <vishwin@FreeBSD.org>
2025-06-17 15:06:26 -0400

    databases/py-gdbm: fix build with Python 3.12+
    Certain internal headers are included unconditionally starting with
    Python 3.12, but this should have been built with the appropriate
    preprocessor macros for internal headers set anyway.
    
    PR: 286298
    Event: Kitchener-Waterloo Hackathon 202506

--- databases/py-gdbm/files/setup.py
+++ databases/py-gdbm/files/setup.py
@@ -4,12 +4,15 @@ import sysconfig
 from setuptools import setup, Extension
 
 prefix = sysconfig.get_config_var('prefix')
-inc_dirs = [prefix + "/include"]
+inc_dirs = [sysconfig.get_path('include') + "/internal",
+            prefix + "/include"]
 lib_dirs = [prefix + "/lib"]
 libs = ["gdbm"]
+macros = [('Py_BUILD_CORE_MODULE', 1)]
 
 setup(ext_modules = [Extension('_gdbm', ['_gdbmmodule.c'],
                                include_dirs = inc_dirs,
                                libraries = libs,
-                               library_dirs = lib_dirs)]
+                               library_dirs = lib_dirs,
+                               define_macros = macros)]
       )
