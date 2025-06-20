2976009ae22a764b66a0def72f6765896a3d3937
Jan Beich <jbeich@FreeBSD.org>
2025-06-20 20:57:37 +0200

    x11-toolkits/como: unbreak Wayland after 8dc860e74057
    After `gainRealTime()` was expanded `return` changed semantic, aborting
    startup instead of skipping realtime priority.
    
    Reported by:	jsm (via upstream bug)

--- x11-toolkits/como/files/patch-como_base_wayland_app__singleton.h
+++ x11-toolkits/como/files/patch-como_base_wayland_app__singleton.h
@@ -4,7 +4,7 @@
 
 --- como/base/wayland/app_singleton.h.orig	2024-02-27 13:02:31 UTC
 +++ como/base/wayland/app_singleton.h
-@@ -19,12 +19,17 @@ class app_singleton : public base::app_singleton (publ
+@@ -19,12 +19,16 @@ class app_singleton : public base::app_singleton (publ
          setenv("QT_QPA_PLATFORM", "wayland-org.kde.kwin.qpa", true);
          setenv("KWIN_FORCE_OWN_QPA", "1", true);
  
@@ -14,14 +14,13 @@
          sp.sched_priority = minPriority;
 -        sched_setscheduler(0, SCHED_RR | SCHED_RESET_ON_FORK, &sp);
 -#endif
-+        if (pthread_setschedparam(pthread_self(), SCHED_RR, &sp))
-+            return;
-+
-+        pthread_atfork(NULL, NULL, []() {
-+            sched_param sp;
-+            sp.sched_priority = 0;
-+            pthread_setschedparam(pthread_self(), SCHED_OTHER, &sp);
-+        });
++        if (!pthread_setschedparam(pthread_self(), SCHED_RR, &sp)) {
++            pthread_atfork(NULL, NULL, []() {
++                sched_param sp;
++                sp.sched_priority = 0;
++                pthread_setschedparam(pthread_self(), SCHED_OTHER, &sp);
++            });
++        }
  
          qapp = std::make_unique<QApplication>(argc, argv);
          prepare_qapp();
