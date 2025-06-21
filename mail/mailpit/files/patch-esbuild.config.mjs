c3d03018f351714324992810b5bdbfb42f5354ca
Guido Falsi <madpilot@FreeBSD.org>
2025-06-21 16:06:23 +0200

    mail/mailpit: Update to 1.26.2

--- mail/mailpit/files/patch-esbuild.config.mjs
+++ mail/mailpit/files/patch-esbuild.config.mjs
@@ -1,8 +1,8 @@
---- esbuild.config.mjs.orig	2023-05-09 05:17:58 UTC
+--- esbuild.config.mjs.orig	1979-11-29 23:00:00 UTC
 +++ esbuild.config.mjs
 @@ -1,4 +1,4 @@
--import * as esbuild from 'esbuild'
-+import * as esbuild from 'esbuild-wasm'
- import pluginVue from 'esbuild-plugin-vue-next'
- import { sassPlugin } from 'esbuild-sass-plugin'
+-import * as esbuild from "esbuild";
++import * as esbuild from "esbuild-wasm";
+ import pluginVue from "esbuild-plugin-vue-next";
+ import { sassPlugin } from "esbuild-sass-plugin";
  
