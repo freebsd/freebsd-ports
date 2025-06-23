37f31ee8c9d1d22a763f2a14853410b215ab39cd
Daniel Engberg <diizzy@FreeBSD.org>
2025-06-23 21:49:34 +0200

    lang/harec: fix previous commit
    The previous commit missed updates to patch-configs_freebsd.mk.
    
    PR:		287730
    Reported by:	diizzy

--- lang/harec/files/patch-configs_freebsd.mk
+++ lang/harec/files/patch-configs_freebsd.mk
@@ -1,6 +1,6 @@
---- configs/freebsd.mk.orig	2024-02-07 07:40:03 UTC
+--- configs/freebsd.mk.orig	2024-07-14 11:26:54 UTC
 +++ configs/freebsd.mk
-@@ -5,7 +5,7 @@ ARCH = x86_64
+@@ -5,20 +5,20 @@ ARCH = x86_64
  # variables used during build
  PLATFORM = freebsd
  ARCH = x86_64
@@ -9,3 +9,29 @@
  QBEFLAGS =
  ASFLAGS =
  LDLINKFLAGS = --gc-sections -z noexecstack
+-CFLAGS = -g -std=c11 -D_XOPEN_SOURCE=700 -Iinclude \
++CFLAGS += -std=c11 -D_XOPEN_SOURCE=700 -Iinclude \
+ 	-Wall -Wextra -Werror -pedantic -Wno-unused-parameter
+-LDFLAGS =
+-LIBS = -lm
++LDFLAGS ?=
++LIBS += -lm
+ 
+ # commands used by the build script
+-CC = cc
+-AS = as
+-LD = ld
+-QBE = qbe
++CC ?= cc
++AS ?= as
++LD ?= ld
++QBE ?= qbe
+ 
+ # build locations
+ HARECACHE = .cache
+@@ -26,4 +26,4 @@ DEFAULT_TARGET = $(ARCH)
+ 
+ # variables that will be embedded in the binary with -D definitions
+ DEFAULT_TARGET = $(ARCH)
+-VERSION = $$(./scripts/version)
++VERSION ?= $$(./scripts/version)
