f6584c066539444ad342f11d6c77b91abaf5627f
Jimmy Olgeni <olgeni@FreeBSD.org>
2025-06-23 02:48:05 +0200

    sysutils/webmin: make setup script more unattended
    The setup script was hanging during package upgrades because it tried to
    ask the configuration path again.

--- sysutils/webmin/files/patch-setup.sh
+++ sysutils/webmin/files/patch-setup.sh
@@ -14,7 +14,7 @@
  cd `dirname $0`
  if [ -x /bin/pwd ]; then
  	wadir=`/bin/pwd`
-@@ -113,7 +120,7 @@
+@@ -113,15 +120,16 @@
  echo ""
  envetcdir="$config_dir"
  if [ "$envetcdir" = "" ]; then
@@ -23,8 +23,10 @@
  	envetcdirnotfound=1
  fi
  printf "Config file directory [$envetcdir]: "
-@@ -121,7 +128,7 @@
- 	read config_dir
+ if [ "$config_dir" = "" ]; then
+-	read config_dir
++	config_dir="$envetcdir"
++	echo "$envetcdir"
  fi
  if [ "$config_dir" = "" ]; then
 -	config_dir=/etc/webmin
@@ -32,7 +34,7 @@
  fi
  abspath=`echo $config_dir | grep "^/"`
  if [ "$abspath" = "" ]; then
-@@ -232,7 +239,7 @@
+@@ -232,7 +240,7 @@
  else
  	# Config directory exists .. make sure it is not in use
  	ls $config_dir | grep -v rpmsave >/dev/null 2>&1
@@ -41,7 +43,7 @@
  		echo "ERROR: Config directory $config_dir is not empty"
  		echo ""
  		exit 2
-@@ -241,7 +248,7 @@
+@@ -241,15 +249,16 @@
  	# Ask for log directory
  	envvardir="$var_dir"
  	if [ "$envvardir" = "" ]; then
@@ -50,8 +52,10 @@
  		envvardirnotfound=1
  	fi
  	printf "Log file directory [$envvardir]: "
-@@ -249,7 +256,7 @@
- 		read var_dir
+ 	if [ "$var_dir" = "" ]; then
+-		read var_dir
++		var_dir="$envvardir"
++		echo "$envvardir"
  	fi
  	if [ "$var_dir" = "" ]; then
 -		var_dir=/var/webmin
@@ -59,3 +63,26 @@
  	fi
  	abspath=`echo $var_dir | grep "^/"`
  	if [ "$abspath" = "" ]; then
+@@ -290,7 +299,8 @@
+ 	if [ "$perl" = "" ]; then
+ 		if [ "$perldef" = "" ]; then
+ 			printf "Full path to perl: "
+-			read perl
++			perl="%%PERL%%"
++			echo "$perl"
+ 			if [ "$perl" = "" ]; then
+ 				echo "ERROR: No path entered!"
+ 				echo ""
+@@ -298,10 +308,8 @@
+ 			fi
+ 		else
+ 			printf "Full path to perl (default $perldef): "
+-			read perl
+-			if [ "$perl" = "" ]; then
+-				perl=$perldef
+-			fi
++			perl=$perldef
++			echo "$perldef"
+ 		fi
+ 	fi
+ 	echo ""
