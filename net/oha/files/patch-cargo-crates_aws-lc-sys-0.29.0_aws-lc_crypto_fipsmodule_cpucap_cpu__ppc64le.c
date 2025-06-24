0f9f717c20c19a873e6d2147a0819d644be5c147
Juraj Lutter <otis@FreeBSD.org>
2025-06-24 23:46:52 +0200

    net/oha: Update to 1.9.0

--- net/oha/files/patch-cargo-crates_aws-lc-sys-0.29.0_aws-lc_crypto_fipsmodule_cpucap_cpu__ppc64le.c
+++ net/oha/files/patch-cargo-crates_aws-lc-sys-0.29.0_aws-lc_crypto_fipsmodule_cpucap_cpu__ppc64le.c
@@ -1,10 +1,10 @@
---- cargo-crates/aws-lc-sys-0.25.0/aws-lc/crypto/fipsmodule/cpucap/cpu_ppc64le.c.orig	2006-07-24 01:21:28 UTC
-+++ cargo-crates/aws-lc-sys-0.25.0/aws-lc/crypto/fipsmodule/cpucap/cpu_ppc64le.c
+--- cargo-crates/aws-lc-sys-0.29.0/aws-lc/crypto/fipsmodule/cpucap/cpu_ppc64le.c.orig	2025-06-24 21:07:10 UTC
++++ cargo-crates/aws-lc-sys-0.29.0/aws-lc/crypto/fipsmodule/cpucap/cpu_ppc64le.c
 @@ -69,7 +69,11 @@ void OPENSSL_cpuid_setup(void) {
  
  void OPENSSL_cpuid_setup(void) {
  #if defined(AT_HWCAP2)
-+# if defined(__linux__)
++#if defined(__linux__)
    OPENSSL_ppc64le_hwcap2 = getauxval(AT_HWCAP2);
 +#elif defined(__FreeBSD__)
 +  elf_aux_info(AT_HWCAP2, &OPENSSL_ppc64le_hwcap2, sizeof(OPENSSL_ppc64le_hwcap2));
