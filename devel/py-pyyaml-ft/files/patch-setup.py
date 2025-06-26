175e49d58a7fab5375dc79a6c543e29e208509b6
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-06-26 18:00:34 +0800

    devel/py-pyyaml-ft: Update to 8.0.0
    Changes:	https://github.com/Quansight-Labs/pyyaml-ft/releases

--- devel/py-pyyaml-ft/files/patch-setup.py
+++ devel/py-pyyaml-ft/files/patch-setup.py
@@ -1,10 +1,10 @@
---- setup.py.orig	2025-04-28 18:32:18 UTC
+--- setup.py.orig	2025-06-10 15:01:38 UTC
 +++ setup.py
 @@ -1,6 +1,6 @@ import pathlib
  import pathlib
  
 -NAME = 'PyYAML-ft'
 +NAME = 'pyyaml-ft'
- VERSION = '7.0.1'
+ VERSION = '8.0.0'
  DESCRIPTION = "YAML parser and emitter for Python with support for free-threading"
  LONG_DESCRIPTION = (pathlib.Path(__file__).parent / 'README.md').read_text(encoding="utf-8")
