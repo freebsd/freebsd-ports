c4d86b0bbeb91b8db652eb70e77268c779db0dfd
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-06-26 18:03:39 +0800

    Mk/Uses/python.mk: Update PY_TOMLI
    devel/py-requirements-detector 1.4.0 requires textproc/py-tomli 2.2.1+.

--- Mk/Uses/python.mk
+++ Mk/Uses/python.mk
@@ -880,7 +880,7 @@ PY_SETUPTOOLS=	${PYTHON_PKGNAMEPREFIX}setuptools>0:devel/py-setuptools@${PY_FLAV
 # Common Python modules that can be needed but only for some versions of Python.
 .  if ${PYTHON_REL} < 31100
 PY_EXCEPTIONGROUP=	${PYTHON_PKGNAMEPREFIX}exceptiongroup>=1.1.1:devel/py-exceptiongroup@${PY_FLAVOR}
-PY_TOMLI=	${PYTHON_PKGNAMEPREFIX}tomli>=2.0.2<3:textproc/py-tomli@${PY_FLAVOR}
+PY_TOMLI=		${PYTHON_PKGNAMEPREFIX}tomli>=2.2.1<3:textproc/py-tomli@${PY_FLAVOR}
 PY_TYPING_EXTENSIONS=	${PYTHON_PKGNAMEPREFIX}typing-extensions>0:devel/py-typing-extensions@${PY_FLAVOR}
 .  endif
 
