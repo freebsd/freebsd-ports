ffae7d2145fd343e72af7ba3159011a7a0d41319
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-06-27 01:28:21 +0800

    math/py-blis: Remove cython hack
    - Bump PORTREVISION for potential package change

--- math/py-blis/files/patch-setup.py
+++ math/py-blis/files/patch-setup.py
@@ -1,12 +1,10 @@
 --- setup.py.orig	2025-04-03 12:23:45 UTC
 +++ setup.py
-@@ -302,8 +302,8 @@ setup(
- 
+@@ -303,7 +303,7 @@ setup(
  setup(
      setup_requires=[
--        "cython>=3.0,<4.0",
+         "cython>=3.0,<4.0",
 -        "numpy>=2.0.0,<3.0.0",
-+        "cython>=0,<4.0",
 +        "numpy>=1.19.0,<3.0.0",
      ],
      install_requires=[
