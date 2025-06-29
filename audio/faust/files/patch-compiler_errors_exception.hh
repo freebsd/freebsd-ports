1c9508aee998c460c967ea6a4c6449d239cd689c
Yuri Victorovich <yuri@FreeBSD.org>
2025-06-29 13:17:58 -0700

    audio/faust: update 2.77.3 → 2.79.3

--- audio/faust/files/patch-compiler_errors_exception.hh
+++ audio/faust/files/patch-compiler_errors_exception.hh
@@ -1,15 +1,14 @@
---- compiler/errors/exception.hh.orig	2025-01-10 00:10:01 UTC
+--- compiler/errors/exception.hh.orig	2025-03-11 20:48:17 UTC
 +++ compiler/errors/exception.hh
-@@ -25,8 +25,11 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, U
+@@ -24,7 +24,11 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, U
+ #include <sstream>
  #include <stdexcept>
  #ifndef WIN32
- #include <unistd.h>
 +#  if !defined(__FreeBSD__)
  #include <alloca.h>
--#else
 +#  else
 +#include <stdlib.h>
 +#  endif
+ #include <unistd.h>
+ #else
  // #include <io.h>
- #endif
- 
