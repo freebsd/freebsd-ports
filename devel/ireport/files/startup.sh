a27e3376111e52d87a1a0e2e05e431b4b0f4f6b7
Rene Ladan <rene@FreeBSD.org>
2025-06-30 13:17:53 +0200

    devel/ireport: Remove expired port
    2025-06-30 devel/ireport: Outdated, unsupported upstream and unmaintained for years in tree

--- devel/ireport/files/startup.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-#set -x
-
-IREPORT_HOME="$HOME/.ireport/%%PORTVERSION%%"
-
-java -Djdk.home=%%JAVA_HOME%% -classpath "%%DATADIR%%/platform9/lib/boot.jar:%%DATADIR%%/platform9/lib/org-openide-modules.jar:%%DATADIR%%/platform9/lib/org-openide-util.jar:%%CLSSPTH%%" -Dnetbeans.system_http_proxy=DIRECT -Dnetbeans.system_http_non_proxy_hosts= -Dnetbeans.dirs="%%DATADIR%%/ireport:%%DATADIR%%/ide10:%%DATADIR%%/nb6.5:" -Dnetbeans.home="%%DATADIR%%/platform9" -Xms256m -Xmx512m -Dorg.netbeans.ProxyClassLoader.level=1000 org.netbeans.Main --userdir $IREPORT_HOME --branding ireport "$@"
