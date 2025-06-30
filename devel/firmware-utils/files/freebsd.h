73d85f618f84bd633737a4b5cec727c030b41601
Rene Ladan <rene@FreeBSD.org>
2025-06-30 13:13:37 +0200

    devel/firmware-utils: Remove expired port
    2025-06-30 devel/firmware-utils: Outdated and unsupported upstream

--- devel/firmware-utils/files/freebsd.h
+++
@@ -1,9 +0,0 @@
-/*
- *
- */
-
-#define	bswap_8(x)	((x) & 0xff)
-#define bswap_16(x)	((bswap_8(x) << 8) | bswap_8((x) >> 8))
-#define	bswap_32(x)	((bswap_16(x) << 16) | bswap_16((x) >> 16))
-#define bswap_64(x)	((bswap_32(x) << 32) | bswap_32((x) >> 32))
-
