4691740c69fa6569b5ec49c4eb9bf0155ce32b67
Gleb Popov <arrowd@FreeBSD.org>
2025-07-01 19:17:30 +0300

    Features/ssp.mk: Do not pass -fstack-protector as LDFLAGS
    This isn't really a linker flag and worked only because C/C++ compiler is
    usually used as linker.
    
    This is backed by my own study of LLVM's code as well as this reply from
    Flang's dev: https://discourse.llvm.org/t/flang-working-as-linker/87133/5
    
    Approved by:	bapt (portmgr)
    Differential Revision: https://reviews.freebsd.org/D51116

--- Mk/Features/ssp.mk
+++ Mk/Features/ssp.mk
@@ -17,6 +17,5 @@ SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 # Overridable as a user may want to use -fstack-protector-all
 SSP_CFLAGS?=	-fstack-protector-strong
 CFLAGS+=	${SSP_CFLAGS}
-LDFLAGS+=	${SSP_CFLAGS}
 .  endif
 .endif
