5f0568aca8bda90e7c8a1821bc079a32829ee010
Gleb Popov <arrowd@FreeBSD.org>
2025-07-02 21:38:38 +0300

    graphics/clang-uml: Update to 0.6.2
    Reported by:	portscout!

--- graphics/clang-uml/files/patch-src_common_clang__utils.cc
+++ graphics/clang-uml/files/patch-src_common_clang__utils.cc
@@ -1,11 +1,11 @@
---- src/common/clang_utils.cc.orig	2025-01-20 07:36:53 UTC
+--- src/common/clang_utils.cc.orig	2025-06-29 11:26:26 UTC
 +++ src/common/clang_utils.cc
-@@ -1019,7 +1019,7 @@ clang::RawComment *get_raw_comment(const clang::Source
+@@ -1023,7 +1023,7 @@ clang::RawComment *get_raw_comment(const clang::Source
+     const auto *comments = context.Comments.getCommentsInFile(file_id);
  
-     if (!context.Comments.empty() &&
-         context.Comments.getCommentsInFile(file_id) != nullptr) {
+     if (has_comments && comments != nullptr) {
 -        for (const auto [offset, raw_comment] :
 +        for (const auto & [offset, raw_comment] :
              *context.Comments.getCommentsInFile(sm.getFileID(expr_begin))) {
-             const auto comment_end_line = sm.getSpellingLineNumber(
+             const auto comment_end_line = sm.getExpansionLineNumber(
                  raw_comment->getSourceRange().getEnd());
