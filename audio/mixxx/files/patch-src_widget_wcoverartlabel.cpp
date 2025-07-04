06ed075c5ab92cb07559a39397a09d6eab043658
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2025-07-04 16:45:31 -0700

    audio/mixxx: Improve latest patch file to fix qt5/qt6 issues
    PR:		288013
    Reported by:	Chad Jacob Milios <milios _at__ ccsys.com>

--- audio/mixxx/files/patch-src_widget_wcoverartlabel.cpp
+++ audio/mixxx/files/patch-src_widget_wcoverartlabel.cpp
@@ -1,25 +1,23 @@
 --- src/widget/wcoverartlabel.cpp.orig	2025-07-04 17:25:50 UTC
 +++ src/widget/wcoverartlabel.cpp
-@@ -70,11 +70,9 @@ void WCoverArtLabel::setPixmapAndResize(const QPixmap&
+@@ -70,7 +70,7 @@ void WCoverArtLabel::setPixmapAndResize(const QPixmap&
          m_fullSizeCover = px;
          setPixmap(m_loadedCover);
      }
 -#if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
--    QSize newSize = pixmap().size() / devicePixelRatioF();
--#else
-+
++#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
+     QSize newSize = pixmap().size() / devicePixelRatioF();
+ #else
      QSize newSize = pixmap()->size() / devicePixelRatioF();
--#endif
-+
-     // add the frame so the entire pixmap is visible
-     newSize += QSize(frameWidth() * 2, frameWidth() * 2);
-     if (size() != newSize) {
-@@ -92,7 +90,7 @@ void WCoverArtLabel::setMaxSize(const QSize newSize) {
+@@ -92,7 +92,11 @@ void WCoverArtLabel::setMaxSize(const QSize newSize) {
      // Skip resizing the pixmap and label if the pixmap already fits.
      // Check if we got more space in one dimension and don't need it
      // for the other.
--    const QSize pixmapSize = pixmap().size() / devicePixelRatioF();
++#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
+     const QSize pixmapSize = pixmap().size() / devicePixelRatioF();
++#else
 +    const QSize pixmapSize = pixmap()->size() / devicePixelRatioF();
++#endif
      if (m_pixmapSizeMax == pixmapSize ||
              (m_pixmapSizeMax.height() == pixmapSize.height() &&
                      m_pixmapSizeMax.width() > pixmapSize.width()) ||
