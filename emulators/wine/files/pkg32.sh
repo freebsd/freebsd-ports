497de784080cbe67e0ebaceddaa7d3fe3e0edb95
Gerald Pfeifer <gerald@FreeBSD.org>
2025-07-04 07:43:37 +0000

    emulators/wine: Add error handling to WoW 32-bit setup
    Abstract ABI_FILE and check for its existence before using it.
    
    On the way change formatting to consume less vertical space.
    
    PR:		278525

--- emulators/wine/files/pkg32.sh
+++ emulators/wine/files/pkg32.sh
@@ -1,19 +1,23 @@
 #!/bin/sh -e
 
-if [ "$(id -u)" = 0 ]
-then
+if [ "$(id -u)" = 0 ]; then
   echo "Don't run this script as root!"
   exit 1
 fi
 
 I386_ROOT="${WINE_i386_ROOT:-$HOME/.i386-wine-pkg}"
+ABI_FILE=/usr/lib32/libc.so.7
 
-if [ ! -d "$I386_ROOT/usr/share/keys/pkg" ]
-then
+if [ ! -f $ABI_FILE ]; then
+  echo "\"$ABI_FILE\" not found; exiting."
+  exit 1
+fi
+
+if [ ! -d "$I386_ROOT/usr/share/keys/pkg" ]; then
   mkdir -p "$I386_ROOT/usr/share/keys"
   ln -s /usr/share/keys/pkg "$I386_ROOT/usr/share/keys/pkg"
 fi
 
 # Show what we're going to do, then do it.
-echo pkg -o ABI_FILE=/usr/lib32/libc.so.7 -o INSTALL_AS_USER=true -o RUN_SCRIPTS=false --rootdir "$I386_ROOT" "$@"
-exec pkg -o ABI_FILE=/usr/lib32/libc.so.7 -o INSTALL_AS_USER=true -o RUN_SCRIPTS=false --rootdir "$I386_ROOT" "$@"
+echo pkg -o ABI_FILE=$ABI_FILE -o INSTALL_AS_USER=true -o RUN_SCRIPTS=false --rootdir "$I386_ROOT" "$@"
+exec pkg -o ABI_FILE=$ABI_FILE -o INSTALL_AS_USER=true -o RUN_SCRIPTS=false --rootdir "$I386_ROOT" "$@"
