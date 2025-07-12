d72e68a22b9ebe1380a180ffdcf92ffec9e46ad7
Nuno Teixeira <eduardo@FreeBSD.org>
2025-07-12 22:07:25 +0100

    devel/libev: Correct inotify patch
    Use #ifdef's for portability and possible upstreaming
    
    Sugested by:	fluffy
    Fixes:		cf7638c

--- devel/libev/files/patch-ev.c
+++ devel/libev/files/patch-ev.c
@@ -1,15 +1,19 @@
 --- ev.c.orig	2020-03-18 12:27:32 UTC
 +++ ev.c
-@@ -507,7 +507,7 @@
+@@ -507,7 +507,11 @@
  #endif
  
  #if EV_USE_INOTIFY
 -# include <sys/statfs.h>
-+# include <sys/mount.h>
++# ifdef __FreeBSD__
++#  include <sys/mount.h>
++# else
++#  include <sys/statfs.h>
++# endif
  # include <sys/inotify.h>
  /* some very old inotify.h headers don't have IN_DONT_FOLLOW */
  # ifndef IN_DONT_FOLLOW
-@@ -3149,7 +3149,7 @@ ev_recommended_backends (void) EV_NOEXCEPT
+@@ -3149,7 +3153,7 @@ ev_recommended_backends (void) EV_NOEXCEPT
  {
    unsigned int flags = ev_supported_backends ();
  
