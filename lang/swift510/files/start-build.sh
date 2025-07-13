30261c88bb3d9c3568e4129e049b0c631c0aadef
Jordan Gordeev <jgopensource@proton.me>
2025-07-13 10:21:28 +0200

    lang/swift510: fix for building with ccache plus a couple more small changes
    The attached patch includes the following changes:
         - Fix building with ccache
         - Improve readability of MAKE_JOBS_NUMBER code
         - Remove the workaround for libuuid
           After the dependency misc/e2fsprogs-libuuid was replaced with
           misc/libuuid the workaround became unnecessary
         - Bump PORTREVISION
    
    PR:		287968
    Reported by:	kib

--- lang/swift510/files/start-build.sh
+++ lang/swift510/files/start-build.sh
@@ -8,13 +8,26 @@ export PATH="/sbin:/bin:/usr/sbin:/usr/bin:${PATH}"
 
 export CLANG_MODULE_CACHE_PATH=${clang_module_cache_path}
 
+if [ ${CCACHE_ENABLED} = yes ] ; then
+	ccache_fragment="--cmake-c-launcher ${CCACHE_BIN} --cmake-cxx-launcher ${CCACHE_BIN}"
+else
+	ccache_fragment=
+fi
+
+if [ -n "${MAKE_JOBS_NUMBER}" ] ; then
+	jobs_fragment="--jobs ${MAKE_JOBS_NUMBER}"
+else
+	jobs_fragment=
+fi
+
 cd ${swift_project_dir}/swift &&
 utils/build-script --bootstrapping bootstrapping \
 --release \
 --assertions \
-${MAKE_JOBS_NUMBER+--jobs} ${MAKE_JOBS_NUMBER} \
---host-cc ${CCACHE_WRAPPER_PATH:-/usr/bin}/clang \
---host-cxx ${CCACHE_WRAPPER_PATH:-/usr/bin}/clang++ \
+--host-cc /usr/bin/clang \
+--host-cxx /usr/bin/clang++ \
+${ccache_fragment} \
+${jobs_fragment} \
 --llvm-targets-to-build 'AArch64;X86' \
 --skip-early-swift-driver \
 --skip-early-swiftsyntax \
