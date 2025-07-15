49f1cf8197800576b083614284c42531bb6df5ed
Matthias Andree <mandree@FreeBSD.org>
2025-07-15 23:44:46 +0200

    math/py-igraph: 0.11.9 update, fixes libxml2 use
    with its slightly changed xmlerror.h API.
    
    Approved by:	lwhsu@ (maintainer timeout, 12+ months)
    PR:		279961
    Reported by: 	diizzy@
    MFH:		2025Q3

--- math/py-igraph/files/patch-setup.py
+++ math/py-igraph/files/patch-setup.py
@@ -1,24 +1,26 @@
---- setup.py.orig	2023-07-13 11:49:16 UTC
+--- setup.py.orig	2025-06-11 00:02:53 UTC
 +++ setup.py
-@@ -281,7 +281,7 @@ class IgraphCCoreCMakeBuilder:
+@@ -280,8 +280,8 @@ class IgraphCCoreCMakeBuilder:
+             args.append("-DIGRAPH_GRAPHML_SUPPORT:BOOL=OFF")
  
          # Build the Python interface with vendored libraries
-         for deps in "ARPACK BLAS GLPK GMP LAPACK".split():
+-        for deps in "ARPACK BLAS GLPK GMP LAPACK PLFIT".split():
 -            args.append("-DIGRAPH_USE_INTERNAL_" + deps + "=ON")
++        for deps in "ARPACK BLAS GLPK GMP LAPACK".split():
 +            args.append("-DIGRAPH_USE_INTERNAL_" + deps + "=OFF")
  
-         # -fPIC is needed on Linux so we can link to a static igraph lib from a
-         # Python shared library
-@@ -289,6 +289,12 @@ class IgraphCCoreCMakeBuilder:
- 
+         # Use link-time optinization if available
+         args.append("-DIGRAPH_ENABLE_LTO=AUTO")
+@@ -293,6 +293,12 @@ class IgraphCCoreCMakeBuilder:
          # No need to build tests
          args.append("-DBUILD_TESTING=OFF")
-+
+ 
 +        # Always use flex from ports
 +        args.append("-DFLEX_EXECUTABLE:FILEPATH=%%LOCALBASE%%/bin/flex")
 +	
 +        # Don't try to use git
 +        args.append("-DCMAKE_DISABLE_FIND_PACKAGE_Git:BOOL=True")
-         
++        
          # Do not treat compilation warnings as errors in case someone is trying
          # to "pip install" igraph in an environment for which we don't provide
+         # wheels and the compiler complains about harmless things
