c5ac18f858cbb7e9b6f2bebaaaa68f3e79885fa5
Dan Langille <dvl@FreeBSD.org>
2025-07-16 13:12:36 +0000

    net-mgmt/librenms: Update to 25.7.0
    You need to run: `lnms migrate` after upgrading
    
    FYI: tune_port.php & addhost.php are gone.
    
    re: https://github.com/librenms/librenms/releases/tag/25.7.0

--- net-mgmt/librenms/files/patch-includes_html_output_capture.inc.php
+++ net-mgmt/librenms/files/patch-includes_html_output_capture.inc.php
@@ -1,20 +1,20 @@
---- includes/html/output/capture.inc.php.orig	2023-10-27 13:17:37 UTC
+--- includes/html/output/capture.inc.php.orig	2025-07-16 12:40:13 UTC
 +++ includes/html/output/capture.inc.php
-@@ -32,7 +32,7 @@ $type = $_REQUEST['type'];
+@@ -33,7 +33,7 @@ switch ($type) {
  
  switch ($type) {
      case 'poller':
--        $cmd = ['php', \LibreNMS\Config::get('install_dir') . '/lnms', 'device:poll', $hostname, '--no-data', '-vv'];
-+        $cmd = ['%%LOCALBASE%%/bin/php', \LibreNMS\Config::get('install_dir') . '/lnms', 'device:poll', $hostname, '--no-data', '-vv'];
+-        $cmd = ['php', \App\Facades\LibrenmsConfig::get('install_dir') . '/lnms', 'device:poll', $hostname, '--no-data', '-vv'];
++        $cmd = ['%%LOCALBASE%%/bin/php', \App\Facades\LibrenmsConfig::get('install_dir') . '/lnms', 'device:poll', $hostname, '--no-data', '-vv'];
          $filename = "poller-$hostname.txt";
          break;
      case 'snmpwalk':
-@@ -43,7 +43,7 @@ switch ($type) {
+@@ -44,7 +44,7 @@ switch ($type) {
          $filename = $device['os'] . '-' . $device['hostname'] . '.snmpwalk';
          break;
      case 'discovery':
--        $cmd = ['php', \LibreNMS\Config::get('install_dir') . '/discovery.php', '-h', $hostname, '-d'];
-+        $cmd = ['%%LOCALBASE%%/bin/php', \LibreNMS\Config::get('install_dir') . '/discovery.php', '-h', $hostname, '-d'];
+-        $cmd = ['php', \App\Facades\LibrenmsConfig::get('install_dir') . '/discovery.php', '-h', $hostname, '-d'];
++        $cmd = ['%%LOCALBASE%%/bin/php', \App\Facades\LibrenmsConfig::get('install_dir') . '/discovery.php', '-h', $hostname, '-d'];
          $filename = "discovery-$hostname.txt";
          break;
      default:
