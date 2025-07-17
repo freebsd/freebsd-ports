841d56019fa0656d87f1e5cae55140c3986d7153
Pietro Cerutti <gahr@FreeBSD.org>
2025-07-17 19:26:41 +0000

    lang/seed7: update to 05_20250516
    Changes:
    * https://github.com/ThomasMertes/seed7/releases/tag/Seed7_release_2025-03-25
    * https://github.com/ThomasMertes/seed7/releases/tag/Seed7_release_2025-05-16

--- lang/seed7/files/patch-mk_freebsd.mk
+++ lang/seed7/files/patch-mk_freebsd.mk
@@ -1,5 +1,5 @@
---- mk_freebsd.mk.orig	2020-05-21 06:01:46 UTC
-+++ mk_freebsd.mk
+--- src/mk_freebsd.mk.orig	2020-05-21 06:01:46 UTC
++++ src/mk_freebsd.mk
 @@ -153,6 +153,7 @@ chkccomp.h:
  	echo "#define SQL_SERVER_LIBRARY_PATH \"-L%%LOCALBASE%%/lib\"" >> chkccomp.h
  	echo "#define TDS_INCLUDE_OPTIONS \"-I%%LOCALBASE%%/include\"" >> chkccomp.h
