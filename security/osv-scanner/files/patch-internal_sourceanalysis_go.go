dc78be69be02182856cea797a8645788773668da
Gabriel M. Dutra <dutra@FreeBSD.org>
2025-07-19 02:50:15 -0300

    security/osv-scanner: Update to 2.1.0
    ChangeLog:
    https://github.com/google/osv-scanner/releases/tag/v2.1.0
    
    PR:		286490
    Approved by:	maintainer timeout, 2 months

--- security/osv-scanner/files/patch-internal_sourceanalysis_go.go
+++ security/osv-scanner/files/patch-internal_sourceanalysis_go.go
@@ -1,11 +1,11 @@
---- internal/sourceanalysis/go.go.orig	1979-11-29 23:00:00 UTC
+--- internal/sourceanalysis/go.go.orig	1979-11-30 03:00:00 UTC
 +++ internal/sourceanalysis/go.go
-@@ -18,7 +18,7 @@ func goAnalysis(r reporter.Reporter, pkgs []models.Pac
+@@ -19,7 +19,7 @@ func goAnalysis(pkgs []models.PackageVulns, source mod
  )
  
- func goAnalysis(r reporter.Reporter, pkgs []models.PackageVulns, source models.SourceInfo) {
+ func goAnalysis(pkgs []models.PackageVulns, source models.SourceInfo) {
 -	cmd := exec.Command("go", "version")
-+	cmd := exec.Command("go%%GO_SUFFIX%%", "version")
++	cmd := exec.Command("go124", "version")
  	_, err := cmd.Output()
  	if err != nil {
- 		r.Infof("Skipping call analysis on Go code since Go is not installed.\n")
+ 		slog.Info("Skipping call analysis on Go code since Go is not installed.")
