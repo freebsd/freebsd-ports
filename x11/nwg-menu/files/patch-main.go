3d18bcc565fb706104c47de59909d4d30efe87cb
Hiroki Tagato <tagattie@FreeBSD.org>
2025-07-23 12:51:35 +0900

    x11/nwg-menu: update to 0.1.9
    While here, switch to GO_MODULE.
    
    Changelog:
    - https://github.com/nwg-piotr/nwg-menu/releases/tag/v0.1.8
    - https://github.com/nwg-piotr/nwg-menu/releases/tag/v0.1.9
    
    Reported by:	Repology

--- x11/nwg-menu/files/patch-main.go
+++ x11/nwg-menu/files/patch-main.go
@@ -1,6 +1,6 @@
 - Adapt to BSD-style shutdown
 
---- main.go.orig	2025-01-11 00:07:37 UTC
+--- main.go.orig	1979-11-29 15:00:00 UTC
 +++ main.go
 @@ -122,8 +122,8 @@ var cmdLogout = flag.String("cmd-logout", "swaymsg exi
  var wm = flag.String("wm", "", "use swaymsg exec (with 'sway' argument) or hyprctl dispatch exec (with 'hyprland') or riverctl spawn (with 'river') to launch programs")
@@ -12,4 +12,4 @@
 +var cmdShutdown = flag.String("cmd-shutdown", "shutdown -p now", "shutdown command")
  var debug = flag.Bool("debug", false, "turn on Debug messages")
  var hover = flag.Bool("t", false, "hovering caTegories opens submenus")
- 
+ var clickOutside = flag.Bool("k", false, "clicKing outside closes the window")
