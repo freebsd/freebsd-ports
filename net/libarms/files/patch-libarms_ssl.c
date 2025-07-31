16afea3494c1da81aa13548058418093e528beb8
Alexey Dokuchaev <danfe@FreeBSD.org>
2025-07-31 17:29:44 +0000

    net/libarms: undeprecate the port and update to version 6.33
    This is not the latest one, but later versions have broken
    docfiles installation due to seemingly incorrectly populated
    nobase_dist_doc_DATA (install: ./doc/api: Inappropriate file
    type or format).

--- net/libarms/files/patch-libarms_ssl.c
+++ net/libarms/files/patch-libarms_ssl.c
@@ -1,37 +1,12 @@
---- libarms/ssl.c.orig	2012-01-24 02:47:27 UTC
+--- libarms/ssl.c.orig	2019-03-29 01:15:24 UTC
 +++ libarms/ssl.c
-@@ -162,6 +162,7 @@ arms_ssl_servercert_verify_cb(int ok, X509_STORE_CTX *
- {
- 	char cn[256];
- 	const char *errmsg = NULL;
-+	int error;
+@@ -338,7 +338,8 @@ arms_ssl_dhparam(SSL_CTX *ctx)
+ 	}
  
- 	X509_NAME_oneline(
- 		X509_get_subject_name(
-@@ -171,13 +172,15 @@ arms_ssl_servercert_verify_cb(int ok, X509_STORE_CTX *
- 		X509_get_issuer_name(
- 			X509_STORE_CTX_get_current_cert(ctx)), cn, sizeof(cn));
- 
-+	error = X509_STORE_CTX_get_error(ctx);
-+
- 	if (!ok) {
--		switch (ctx->error) {
-+		switch (error) {
- 		case X509_V_ERR_CERT_NOT_YET_VALID:
- 		case X509_V_ERR_CERT_HAS_EXPIRED:
- 			/* XXX: ignore Validity Not Before/Not After field */
- 			ok = 1;
--			ctx->error = X509_V_OK;
-+			X509_STORE_CTX_set_error(ctx, X509_V_OK);
- 			break;
- 
- 		case X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY:
-@@ -187,7 +190,7 @@ arms_ssl_servercert_verify_cb(int ok, X509_STORE_CTX *
- 			break;
- 
- 		default:
--			errmsg = X509_verify_cert_error_string(ctx->error);
-+			errmsg = X509_verify_cert_error_string(error);
- 			break;
- 		}
- 
+ /* for compatibility with before OpenSSL-1.1.0 and LibreSSL-2.7.0 */
+-#if OPENSSL_VERSION_NUMBER < 0x10100000L || LIBRESSL_VERSION_NUMBER < 0x20700000L
++#if (OPENSSL_VERSION_NUMBER < 0x10100000L) || \
++	(defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
+ 	dh->p = p;
+ 	dh->g = g;
+ #else
