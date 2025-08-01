95311eebc5b39c193e9bae58992564c6edb4b8e8
Yuri Victorovich <yuri@FreeBSD.org>
2025-07-31 22:21:47 -0700

    multimedia/obs-studio: update 31.1.1 → 31.1.2

---
+++ multimedia/obs-studio/files/patch-cmake_common_helpers__common.cmake
@@ -0,0 +1,12 @@
+--- cmake/common/helpers_common.cmake.orig	2025-08-01 04:18:14 UTC
++++ cmake/common/helpers_common.cmake
+@@ -467,6 +467,9 @@ function(add_obs_plugin target)
+     set(found_architecture TRUE)
+   else()
+     foreach(architecture IN LISTS _AOP_ARCHITECTURES)
++      if (${architecture} STREQUAL x86_64)
++      	set(architecture "amd64")
++      endif()
+       if(OS_WINDOWS)
+         if("${architecture}" STREQUAL CMAKE_VS_PLATFORM_NAME)
+           set(found_architecture TRUE)
