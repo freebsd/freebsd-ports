c7a17f74b19b62b66ea7499fe86b07b4ef9edc63
Generic Rikka <rikka.goering@outlook.de>
2025-08-02 15:13:34 +0200

    sysutils/slurm-wlm: fix slurmctld
    PR:		288593
    (cherry picked from commit 102f62ebb37a34b8963c0ebf89ccb5877da7f956)

--- sysutils/slurm-wlm/files/patch-src_interfaces_cgroup.c
+++ sysutils/slurm-wlm/files/patch-src_interfaces_cgroup.c
@@ -1,6 +1,6 @@
---- src/interfaces/cgroup.c.orig	2023-11-21 22:33:29 UTC
+--- src/interfaces/cgroup.c.orig	2024-05-21 17:19:51 UTC
 +++ src/interfaces/cgroup.c
-@@ -419,6 +419,7 @@ extern char *autodetect_cgroup_version(void)
+@@ -421,6 +421,7 @@ extern char *autodetect_cgroup_version(void)
  	struct statfs fs;
  	int cgroup_ver = -1;
  
@@ -8,7 +8,7 @@
  	if (statfs("/sys/fs/cgroup/", &fs) < 0) {
  		error("cgroup filesystem not mounted in /sys/fs/cgroup/");
  		return NULL;
-@@ -451,6 +452,7 @@ extern char *autodetect_cgroup_version(void)
+@@ -453,6 +454,7 @@ extern char *autodetect_cgroup_version(void)
  		error("Unknown filesystem type mounted on /sys/fs/cgroup");
  		return NULL;
  	}
