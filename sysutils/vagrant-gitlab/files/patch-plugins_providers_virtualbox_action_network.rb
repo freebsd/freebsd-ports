8d4db2ab398aed140417fa6d71303f685215132f
Matthias Fechner <mfechner@FreeBSD.org>
2025-08-05 08:47:34 +0200

    sysutils/vagrant-gitlab: update to 2.4.7
    Changes:	https://github.com/hashicorp/vagrant/blob/main/CHANGELOG.md
    (cherry picked from commit d0c6233e8c3ec77fb8d228e6837269e0f596e88d)

--- sysutils/vagrant-gitlab/files/patch-plugins_providers_virtualbox_action_network.rb
+++ sysutils/vagrant-gitlab/files/patch-plugins_providers_virtualbox_action_network.rb
@@ -1,6 +1,6 @@
---- plugins/providers/virtualbox/action/network.rb.orig	2021-11-05 21:15:44 UTC
+--- plugins/providers/virtualbox/action/network.rb.orig	2025-06-17 15:13:05 UTC
 +++ plugins/providers/virtualbox/action/network.rb
-@@ -18,7 +18,7 @@ module VagrantPlugins
+@@ -21,7 +21,7 @@ module VagrantPlugins
        class Network
  
          # Location of the VirtualBox networks configuration file
@@ -8,4 +8,4 @@
 +        VBOX_NET_CONF = "@PREFIX@/etc/vbox/networks.conf".freeze
          # Version of VirtualBox that introduced hostonly network range restrictions
          HOSTONLY_VALIDATE_VERSION = Gem::Version.new("6.1.28")
-         # Default valid range for hostonly networks
+         # Version of VirtualBox on darwin platform that ignores restrictions
