6bac56f068c067cbbd6f0326cee640ab62bfcdb6
Matthias Andree <mandree@FreeBSD.org>
2025-08-09 20:11:20 +0200

    cad/PrusaSlicer: Strip FindOpenVDB's Imath::Half handling
    ...which no longer fits Imath 3.2.0, and replace the simpler parts
    of it by just depending on Imath or Imath::Imath.

--- cad/PrusaSlicer/files/patch-cmake_modules_FindOpenVDB.cmake
+++ cad/PrusaSlicer/files/patch-cmake_modules_FindOpenVDB.cmake
@@ -1,28 +1,23 @@
---- cmake/modules/FindOpenVDB.cmake.orig	2025-02-14 18:26:40 UTC
+--- cmake/modules/FindOpenVDB.cmake.orig	2025-04-10 11:26:51 UTC
 +++ cmake/modules/FindOpenVDB.cmake
-@@ -347,24 +347,24 @@ endmacro()
+@@ -347,24 +347,12 @@ endmacro()
    return()
  endmacro()
  
 -find_package(IlmBase QUIET)
 -if(NOT IlmBase_FOUND)
 -  pkg_check_modules(IlmBase QUIET IlmBase)
-+find_package(Imath QUIET COMPONENTS Half)
++find_package(Imath QUIET COMPONENTS Imath)
 +if(NOT Imath_FOUND)
 +  pkg_check_modules(Imath QUIET Imath)
  endif()
 -if (IlmBase_FOUND AND NOT TARGET IlmBase::Half)
 -  message(STATUS "Falling back to IlmBase found by pkg-config...")
-+if (Imath_FOUND AND NOT TARGET Imath::Half)
-+  message(STATUS "Falling back to Imath found by pkg-config...")
- 
+-
 -  find_library(IlmHalf_LIBRARY NAMES Half)
 -  if(IlmHalf_LIBRARY-NOTFOUND OR NOT IlmBase_INCLUDE_DIRS)
 -    just_fail("IlmBase::Half can not be found!")
-+  find_library(Imath_LIBRARY NAMES Imath)
-+  if(Imath_LIBRARY-NOTFOUND OR NOT Imath_INCLUDE_DIRS)
-+    just_fail("Imath::Half can not be found!")
-   endif()
+-  endif()
 -  
 -  add_library(IlmBase::Half UNKNOWN IMPORTED)
 -  set_target_properties(IlmBase::Half PROPERTIES
@@ -30,17 +25,12 @@
 -    INTERFACE_INCLUDE_DIRECTORIES "${IlmBase_INCLUDE_DIRS}")
 -elseif(NOT IlmBase_FOUND)
 -  just_fail("IlmBase::Half can not be found!")
-+ 
-+  add_library(Imath::Half UNKNOWN IMPORTED)
-+  set_target_properties(Imath::Half PROPERTIES
-+						IMPORTED_LOCATION "${Imath_LIBRARY}"
-+						INTERFACE_INCLUDE_DIRECTORIES "${Imath_INCLUDE_DIRS}")
-+elseif(NOT Imath_FOUND)
++if(NOT Imath_FOUND)
 +  just_fail("Imath::Half can not be found!")
  endif()
  find_package(TBB ${_quiet} ${_required} COMPONENTS tbb)
  find_package(ZLIB ${_quiet} ${_required})
-@@ -451,7 +451,7 @@ if(OpenVDB_USES_ILM)
+@@ -451,7 +439,7 @@ if(OpenVDB_USES_ILM)
  endif()
  
  if(OpenVDB_USES_ILM)
@@ -49,7 +39,7 @@
  endif()
  
  if(OpenVDB_USES_EXR)
-@@ -463,7 +463,7 @@ endif()
+@@ -463,7 +451,7 @@ endif()
  endif()
  
  # Set deps. Note that the order here is important. If we're building against
@@ -58,16 +48,16 @@
  # users chosen namespaced headers are correctly prioritized. Otherwise other
  # include paths from shared installs (including houdini) may pull in the wrong
  # headers
-@@ -471,7 +471,7 @@ set(_OPENVDB_VISIBLE_DEPENDENCIES
+@@ -471,7 +459,7 @@ set(_OPENVDB_VISIBLE_DEPENDENCIES
  set(_OPENVDB_VISIBLE_DEPENDENCIES
    Boost::iostreams
    Boost::system
 -  IlmBase::Half
-+  Imath::Half
++  Imath::Imath
  )
  
  set(_OPENVDB_DEFINITIONS)
-@@ -481,10 +481,10 @@ if(OpenVDB_USES_EXR)
+@@ -481,10 +469,10 @@ if(OpenVDB_USES_EXR)
  
  if(OpenVDB_USES_EXR)
    list(APPEND _OPENVDB_VISIBLE_DEPENDENCIES
--- cad/PrusaSlicer/files/patch-src_slic3r_GUI_Preferences.cpp
+++ cad/PrusaSlicer/files/patch-src_slic3r_GUI_Preferences.cpp
@@ -9,7 +9,7 @@
  #include "DesktopIntegrationDialog.hpp"
  #endif //(__linux__) && defined(SLIC3R_DESKTOP_INTEGRATION)
  
-@@ -270,7 +270,7 @@ void PreferencesDialog::build()
+@@ -271,7 +271,7 @@ void PreferencesDialog::build()
  	tabs = new Notebook(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNB_TOP | wxTAB_TRAVERSAL | wxNB_NOPAGETHEME | wxNB_DEFAULT);
  #else
      tabs = new wxNotebook(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxNB_TOP | wxTAB_TRAVERSAL  |wxNB_NOPAGETHEME | wxNB_DEFAULT );
@@ -18,7 +18,7 @@
  	tabs->Bind(wxEVT_NOTEBOOK_PAGE_CHANGED, [this](wxBookCtrlEvent& e) {
  		e.Skip();
  		CallAfter([this]() { tabs->GetCurrentPage()->Layout(); });
-@@ -750,7 +750,7 @@ void PreferencesDialog::accept(wxEvent&)
+@@ -756,7 +756,7 @@ void PreferencesDialog::accept(wxEvent&)
  			downloader->allow(it->second == "1");
  		if (!downloader->on_finish())
  			return;
@@ -27,7 +27,7 @@
  		if(DownloaderUtils::Worker::perform_registration_linux) 
  			DesktopIntegrationDialog::perform_downloader_desktop_integration();
  #endif //(__linux__) && defined(SLIC3R_DESKTOP_INTEGRATION)
-@@ -1108,7 +1108,7 @@ void PreferencesDialog::create_settings_font_widget()
+@@ -1114,7 +1114,7 @@ void PreferencesDialog::create_settings_font_widget()
  		font_example->SetFont(font);
  		m_values[opt_key] = format("%1%", val);
  		stb_sizer->Layout();
