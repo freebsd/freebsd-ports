1b27c3422e813e78878e0fdc8577714e34c5fbd6
Rafe Sadler <mondo.debater_0q@icloud.com>
2025-08-10 12:04:43 +0900

    net-p2p/transmission-components: update for libfmt 11
     Port updated to compile with libfmt ≥11.

---
+++ net-p2p/transmission-components/files/patch-gtk_DetailsDialog.cc
@@ -0,0 +1,10 @@
+--- gtk/DetailsDialog.cc.orig	2025-08-09 13:07:00 UTC
++++ gtk/DetailsDialog.cc
+@@ -49,6 +49,7 @@
+ 
+ #include <fmt/chrono.h>
+ #include <fmt/core.h>
++#include <fmt/ranges.h>
+ #include <fmt/format.h>
+ 
+ #include <algorithm>
