2ef96c82c051d6fe39a194f90ea92905a5d24ff7
Stefan Schlosser <bsdcode@disroot.org>
2025-08-12 00:58:22 +0300

    emulators/Ymir: Update 0.1.6 => 0.1.7
    Changelog:
    https://github.com/StrikerX3/Ymir/releases/tag/v0.1.7
    
    - Remove patch qith support devel/libfmt 10.x after update it 11.2.0.
    - Unbreak on aarch64 - require llvm21.
    
    PR:	288782

--- emulators/Ymir/files/patch-apps_ymir-sdl3_src_app_app.cpp
+++ emulators/Ymir/files/patch-apps_ymir-sdl3_src_app_app.cpp
@@ -1,7 +1,7 @@
---- apps/ymir-sdl3/src/app/app.cpp.orig	2025-07-20 22:35:27 UTC
+--- apps/ymir-sdl3/src/app/app.cpp.orig	2025-08-11 00:12:19 UTC
 +++ apps/ymir-sdl3/src/app/app.cpp
-@@ -108,8 +108,8 @@
- #include <SDL3/SDL_filesystem.h>
+@@ -114,8 +114,8 @@
+ #include <SDL3/SDL_messagebox.h>
  #include <SDL3/SDL_misc.h>
  
 -#include <backends/imgui_impl_sdl3.h>
@@ -11,23 +11,3 @@
  
  #include <imgui.h>
  
-@@ -1898,8 +1898,18 @@ void App::RunEmulator() {
-                                                       actions::save_states::GetLoadStateAction(i),
-                                                       actions::save_states::GetSaveStateAction(i));
-
-+#if FMT_VERSION < 110000
-+                                char timeStr[20];
-+                                tm timeTm = util::to_local_time(state.timestamp);
-+                                if (!strftime(timeStr, sizeof(timeStr), "%F %T", &timeTm)) {
-+                                    memset(timeStr, 0, sizeof(timeStr));
-+                                }
-+#else
-+                                tm timeStr = util::to_local_time(state.timestamp);
-+#endif
-+
-                                 if (ImGui::MenuItem(
--                                        fmt::format("{}: {}", i, util::to_local_time(state.timestamp)).c_str(),
-+                                        fmt::format("{}: {}", i, timeStr).c_str(),
-                                         shortcut.c_str(), m_context.currSaveStateSlot == i, true)) {
-                                     if (io.KeyShift) {
-                                         SaveSaveStateSlot(i);
