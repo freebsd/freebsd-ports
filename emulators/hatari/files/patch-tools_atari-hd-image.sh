5d944f4bb91d8a0d54315912e3b51f2892a6a5f6
Laurent Chardon <laurent.chardon@gmail.com>
2025-08-11 16:57:21 +0300

    emulators/hatari: Update 2.5.0 => 2.6.0
    Upstream migrated to GitLab.
    
    Changelog:
    https://framagit.org/hatari/hatari/-/releases/v2.6.0
    
    Improve manual pages patching and installation.
    Fix warnings from portclippy.
    Remove unused dependency iconv.
    
    PR:		288772
    Co-authored-by:	Vladimir Druzenko <vvd@FreeBSD.org>

--- emulators/hatari/files/patch-tools_atari-hd-image.sh
+++ emulators/hatari/files/patch-tools_atari-hd-image.sh
@@ -1,4 +1,4 @@
---- tools/atari-hd-image.sh.orig	2024-04-26 10:23:56 UTC
+--- tools/atari-hd-image.sh.orig	2025-08-09 22:33:00 UTC
 +++ tools/atari-hd-image.sh
 @@ -33,8 +33,8 @@ export PATH
  export PATH
@@ -6,19 +6,17 @@
  # check tools
 -if [ -z "$(which mkdosfs)" ] || [ -z "$(which python3)" ]; then
 -	echo "ERROR: either mkdosfs or python3 missing!"
-+if [ -z "$(which mkdosfs)" ] || [ -z "$(which %%PYTHON_VERSION%%)" ]; then
-+	echo "ERROR: either mkdosfs or %%PYTHON_VERSION%% missing!"
++if [ -z "$(which mkdosfs)" ] || [ -z "$(which %%PYTHON_CMD%%)" ]; then
++	echo "ERROR: either mkdosfs or %%PYTHON_CMD%% missing!"
  	exit 1
  fi
  
-@@ -147,8 +147,8 @@ echo "$step) Create DOS Master Boot Record / partition
+@@ -147,7 +147,7 @@ echo "$step) Create DOS Master Boot Record / partition
  # - http://en.wikipedia.org/wiki/File_Allocation_Table#Boot_Sector
  # For DOS MBR, the values are little endian.
  # -----------
 -python3 << EOF
--#!/usr/bin/env python3
-+%%PYTHON_VERSION%% << EOF
-+#!/usr/bin/env %%PYTHON_VERSION%%
++%%PYTHON_CMD%% << EOF
+ #!/usr/bin/python3
  mbr = bytearray(512)
  
- def set_long(idx, value):
