af2d11255141a1247764eaf068a72737377c716b
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-08-13 01:52:36 +0800

    devel/py-types-pyyaml: Move devel/py-types-PyYAML to devel/py-types-pyyaml
    - Update WWW
    - Convert to USE_PYTHON=pep517
    - Update pkg-descr
    - Bump PORTREVISION for package change
    - Bump PORTREVISION of dependent ports for dependency change
    - Take maintainership

---
+++ devel/py-types-pyyaml/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2024-12-30 02:44:34 UTC
++++ setup.py
+@@ -1,6 +1,6 @@ from setuptools import setup
+ from setuptools import setup
+ 
+-name = "types-PyYAML"
++name = "types-pyyaml"
+ description = "Typing stubs for PyYAML"
+ long_description = '''
+ ## Typing stubs for PyYAML
