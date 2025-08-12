41187fe4c27104347cb9b168e76d5a92649dc070
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-08-13 01:59:21 +0800

    textproc/py-youseedee: Update to 0.7.0
    Changes:	https://github.com/simoncozens/youseedee/releases
    		https://github.com/simoncozens/youseedee/commits/master

--- textproc/py-youseedee/files/patch-lib-youseedee-__init__.py
+++ textproc/py-youseedee/files/patch-lib-youseedee-__init__.py
@@ -1,11 +1,11 @@
---- lib/youseedee/__init__.py.orig	2024-09-11 19:36:00 UTC
+--- lib/youseedee/__init__.py.orig	2025-07-24 11:24:42 UTC
 +++ lib/youseedee/__init__.py
 @@ -38,7 +38,7 @@ def ucd_dir():
  
  def ucd_dir():
      """Return the directory where Unicode data is stored"""
--    ucddir = expanduser("~/.youseedee")
-+    ucddir = "%%UCDDIR%%"
-     try:
-         os.mkdir(ucddir)
-     except FileExistsError:
+-    return Path(platformdirs.user_cache_dir("youseedee", ensure_exists=True))
++    return "%%UCDDIR%%"
+ 
+ 
+ def ensure_files():
