980965c53a1c59f4ca0606f05b5ed4c20748f45c
takefu <takefu@airport.fm>
2025-08-13 17:37:38 +0300

    net/ndisc6: Update 1.0.7 => 1.0.8, take maintainership
    Changelog:
    https://git.remlab.net/gitweb/?p=ndisc6.git;a=blob;f=NEWS;hb=refs/tags/v1.0.8
    
    Replace gettext with gettext-runtime: the port does not use the tool
    components - it only links with libintl.
    Reduce the number of install command calls.
    
    PR:		288829
    Co-authored-by:	Vladimir Druzenko <vvd@FreeBSD.org>

--- net/ndisc6/files/patch-src_ndisc.c
+++ net/ndisc6/files/patch-src_ndisc.c
@@ -1,6 +1,15 @@
---- src/ndisc.c.orig	2016-12-07 16:43:04 UTC
+--- src/ndisc.c.orig	2023-11-10 15:28:07 UTC
 +++ src/ndisc.c
-@@ -532,7 +532,7 @@ parseadv (const uint8_t *buf, size_t len, const struct
+@@ -451,7 +451,7 @@ parsednssl (const uint8_t *opt)
+ parsednssl (const uint8_t *opt)
+ {
+ 	const uint8_t *base;
+-	uint8_t optlen = opt[1];
++	uint16_t optlen = opt[1];
+ 	if (optlen < 2)
+ 		return -1;
+ 
+@@ -559,7 +559,7 @@ parseadv (const uint8_t *buf, size_t len, const struct
  		printf (_("Stateful other conf.      :          %3s\n"),
  		        gettext ((v & ND_RA_FLAG_OTHER) ? N_ ("Yes") : N_("No")));
  		printf (_("Mobile home agent         :          %3s\n"),
