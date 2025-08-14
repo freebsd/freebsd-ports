b45fe3947d467dd83c888a7edee30963c0230613
Robert Nagy <rnagy@FreeBSD.org>
2025-08-15 00:12:46 +0200

    www/chromium: update to 139.0.7258.127
    Security:	https://vuxml.freebsd.org/freebsd/a60e73e0-7942-11f0-b3f7-a8a1599412c6.html

--- www/chromium/files/patch-chrome_browser_ui_webui_signin_profile__picker__handler.cc
+++ www/chromium/files/patch-chrome_browser_ui_webui_signin_profile__picker__handler.cc
@@ -1,6 +1,6 @@
---- chrome/browser/ui/webui/signin/profile_picker_handler.cc.orig	2025-08-07 06:57:29 UTC
+--- chrome/browser/ui/webui/signin/profile_picker_handler.cc.orig	2025-08-14 19:12:25 UTC
 +++ chrome/browser/ui/webui/signin/profile_picker_handler.cc
-@@ -168,7 +168,7 @@ base::Value::Dict CreateProfileEntry(const ProfileAttr
+@@ -169,7 +169,7 @@ base::Value::Dict CreateProfileEntry(const ProfileAttr
        IDS_PROFILE_PICKER_PROFILE_CARD_LABEL, local_profile_name);
    if (entry->GetIsManaged() == signin::Tribool::kTrue) {
      profile_entry.Set("avatarBadge", "cr:domain");
