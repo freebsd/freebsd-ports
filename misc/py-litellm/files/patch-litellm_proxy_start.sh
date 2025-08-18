e9a3c473c57fa2106ce34216f3c61267bdad23b6
Hiroki Tagato <tagattie@FreeBSD.org>
2025-08-18 20:41:21 +0900

    misc/py-litellm: update to 1.75.8
    While here, make unnecessarily complicated patching simpler.
    
    Changelog: https://github.com/BerriAI/litellm/releases
    
    Reported by:	portscout

--- misc/py-litellm/files/patch-litellm_proxy_start.sh
+++
@@ -1,8 +0,0 @@
---- litellm/proxy/start.sh.orig	2024-02-11 03:13:21 UTC
-+++ litellm/proxy/start.sh
-@@ -1,2 +1,2 @@
--#!/bin/bash
--python3 proxy_cli.py
-\ No newline at end of file
-+#!/bin/sh
-+%%PYTHON_CMD%% proxy_cli.py
