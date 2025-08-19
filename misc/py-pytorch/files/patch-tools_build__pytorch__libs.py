fe981541b1ee5b111bd6330c09f4c615dd4a7c20
Yuri Victorovich <yuri@FreeBSD.org>
2025-08-19 07:59:01 -0700

    misc/py-pytorch: update 2.7.1 → 2.8.0

--- misc/py-pytorch/files/patch-tools_build__pytorch__libs.py
+++ misc/py-pytorch/files/patch-tools_build__pytorch__libs.py
@@ -1,11 +1,12 @@
---- tools/build_pytorch_libs.py.orig	2025-04-24 16:28:33 UTC
+--- tools/build_pytorch_libs.py.orig	2025-08-06 17:08:06 UTC
 +++ tools/build_pytorch_libs.py
-@@ -119,7 +119,7 @@ def build_pytorch(
-     cmake: CMake,
- ) -> None:
-     my_env = _create_build_env()
--    checkout_nccl()
-+    #checkout_nccl()
+@@ -91,7 +91,8 @@ def build_pytorch(
+         and not check_negative_env_flag("USE_NCCL")
+         and not check_env_flag("USE_SYSTEM_NCCL")
+     ):
+-        checkout_nccl()
++        #checkout_nccl()
++        pass
      build_test = not check_negative_env_flag("BUILD_TEST")
      cmake.generate(
          version, cmake_python_library, build_python, build_test, my_env, rerun_cmake
