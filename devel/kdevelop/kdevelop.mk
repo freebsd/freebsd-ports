199061f2d9c9fc45f77983b6b18ff5c18bf50837
Max Brazhnikov <makc@FreeBSD.org>
2025-08-21 23:58:47 +0300

    KDE: Update KDE Gear to 25.08.0
    Announcement: https://kde.org/announcements/gear/25.08.0/
    
    Removed ports:
       devel/cervisia: not ported to Qt6 and excluded from KDE Gear releases
    
    databases/akonadi:
     - Backport upstream patch to fix account management in Merkuro
    
    devel/kdevelop:
     - Bump PLATFORM_VER to 6.3
     - Revert now needless workaround [1]
    
    devel/kpublictranspor:
     - Renew USE_KDE
     - Explicitly disable KF6NetworkManagerQt
    
    graphics/kqtquickcharts:
     - Switch to Qt6/KF6
     - Remove noop DOCS option
    
    math/kig:
     - Switch to Qt6/KF6
    
    misc/artikulate:
     - Switch to Qt6/KF6
    
    misc/kdeedu-data:
     - Update WWW and description
    
    misc/ktouch:
     - Switch to Qt6/KF6
    
    multimedia/dragon:
     - Renew USE_KDE
     - Add dependency on ffmpeg and remove stale pkg-message
    
    multimedia/kamoso:
     - Switch to Qt6/KF6
    
    net-im/neochat:
     - Add now mandatory dependency on devel/kunifiedpush
     - Add missing Qt dependencies
    
    x11-clocks/kteatime:
     - Renew USE_KDE
    
    PR:             287804 [1]
    (cherry picked from commit 76982d517f2a15b387e49943e2390177e4139344)

--- devel/kdevelop/kdevelop.mk
+++ devel/kdevelop/kdevelop.mk
@@ -3,7 +3,7 @@
 # devel/kdev-python
 # devel/kdevelop
 
-_kdevelop_PROJECT_VERSION=	6.2
+_kdevelop_PROJECT_VERSION=	6.3
 PLATFORM_VER=	${_kdevelop_PROJECT_VERSION:S/.//}
 PLIST_SUB+=	PLATFORM_VER="${PLATFORM_VER}"
 
