89c2f39a0798a179121d3f7f9dc12150829b47d7
Vladimir Druzenko <vvd@FreeBSD.org>
2025-08-23 02:57:35 +0300

    audio/supercollider: Update 3.13.1-g20250219 => 3.14.0
    Changelog:
    https://github.com/supercollider/supercollider/releases/tag/Version-3.14.0
    
    Port changes:
    - Add BUILD_DEPENDS=${LOCALBASE}/include/fftw3.h:math/fftw3 - port uses
      this file to find libfftw3f.so.
    - Convert CMAKE_ARGS to CMAKE_ON and CMAKE_OFF.
    - Move SSE, SSE2 and WEBENGINE from CMAKE_ARGS to OPTIONS:
      - SSE and SSE2 are available for x86 only, WEBENGINE is available for
        all arches.
      - SSE and SSE2 are default for amd64 only, WEBENGINE is default for
        x86 only.
    - Rename the HELP option to DOCS - it is now correctly handled by the
      ports infrastructure and is useful for users who have the DOCS option
      forcibly disabled for all ports.
    - Refresh patches.
    
    PR:		288866
    Approved by:	Neal Nelson <ports@nicandneal.net> (maintainer)
    Co-authored-by:	Neal Nelson <ports@nicandneal.net>

--- audio/supercollider/files/patch-external__libraries_nova-simd_vec_vec__int__altivec.hpp
+++ audio/supercollider/files/patch-external__libraries_nova-simd_vec_vec__int__altivec.hpp
@@ -1,6 +1,6 @@
---- external_libraries/nova-simd/vec/vec_int_altivec.hpp.orig	2020-09-03 23:55:08.848538000 +0200
-+++ external_libraries/nova-simd/vec/vec_int_altivec.hpp	2020-09-03 23:58:16.614642000 +0200
-@@ -35,7 +35,7 @@
+--- external_libraries/nova-simd/vec/vec_int_altivec.hpp.orig	2025-07-26 19:07:55 UTC
++++ external_libraries/nova-simd/vec/vec_int_altivec.hpp
+@@ -35,7 +35,7 @@ struct int_vec_altivec (private)
      static ivec set_vector(int i)
      {
  #ifdef __GNUC__
