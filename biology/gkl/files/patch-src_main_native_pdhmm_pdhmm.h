25848df4724ce501e9d6966f5b5547a8d459c9b2
Yuri Victorovich <yuri@FreeBSD.org>
2025-08-23 11:26:47 -0700

    biology/gkl: update 0.8.11-9 → 0.9.1

--- biology/gkl/files/patch-src_main_native_pdhmm_pdhmm.h
+++ biology/gkl/files/patch-src_main_native_pdhmm_pdhmm.h
@@ -1,4 +1,4 @@
---- src/main/native/pdhmm/pdhmm.h.orig	2024-08-04 16:31:53 UTC
+--- src/main/native/pdhmm/pdhmm.h.orig	2025-07-07 07:42:12 UTC
 +++ src/main/native/pdhmm/pdhmm.h
 @@ -27,6 +27,9 @@
  #ifdef linux
@@ -8,5 +8,5 @@
 +#include <omp.h>
 +#endif
  
- inline INT_TYPE roundDownToNearestMultipleOf(INT_TYPE val, INT_TYPE mul) { return (val / mul) * mul; }
+ inline INT_TYPE CONCAT(roundDownToNearestMultipleOf_, SIMD_ENGINE)(INT_TYPE val, INT_TYPE mul) { return (val / mul) * mul; }
  
