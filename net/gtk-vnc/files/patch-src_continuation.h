a571fac6608c9073c483d444351227e7a419ea47
Dima Panov <fluffy@FreeBSD.org>
2025-08-27 15:07:53 +0300

    net/gtk-vnc: update to 1.5.0 release (+)
    Release notes:	https://gitlab.gnome.org/GNOME/gtk-vnc/-/blob/1.5.0/NEWS

--- net/gtk-vnc/files/patch-src_continuation.h
+++ net/gtk-vnc/files/patch-src_continuation.h
@@ -1,10 +1,10 @@
---- src/continuation.h.orig	2011-02-18 16:29:10.000000000 +0000
-+++ src/continuation.h	2011-02-20 17:50:37.000000000 +0000
-@@ -21,6 +21,7 @@
- #ifndef _CONTINUATION_H_
- #define _CONTINUATION_H_
+--- src/continuation.h.orig	2025-02-07 09:45:55 UTC
++++ src/continuation.h
+@@ -23,6 +23,7 @@
  
+ #include <stddef.h>
+ #include <setjmp.h>
 +#include <sys/types.h>
- #include <ucontext.h>
  
  struct continuation
+ {
