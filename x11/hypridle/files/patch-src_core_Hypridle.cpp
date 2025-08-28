4fed32e7d3b3dcc1858193dd927836da9f852e45
Hiroki Tagato <tagattie@FreeBSD.org>
2025-08-28 16:59:49 +0900

    x11/hypridle: update to 0.1.7
    Changelog: https://github.com/hyprwm/hypridle/releases/tag/v0.1.7
    
    Reported by:	GitHub (watch releases)

--- x11/hypridle/files/patch-src_core_Hypridle.cpp
+++ x11/hypridle/files/patch-src_core_Hypridle.cpp
@@ -1,11 +1,11 @@
 https://github.com/hyprwm/hypridle/issues/68#issuecomment-2428697344
 
---- src/core/Hypridle.cpp.orig	2025-08-07 09:04:25 UTC
+--- src/core/Hypridle.cpp.orig	2025-08-27 10:40:01 UTC
 +++ src/core/Hypridle.cpp
-@@ -180,6 +180,7 @@ void CHypridle::enterEventLoop() {
+@@ -190,6 +190,7 @@ void CHypridle::enterEventLoop() {
          }
      });
-
+ 
 +    m_sEventLoopInternals.loopRequestMutex.try_lock();
      while (1) { // dbus events
          // wait for being awakened
