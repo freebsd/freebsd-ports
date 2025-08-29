55e313e339fb2a8cfc6c61a2212f0e409c8e4e4f
Kai Knoblich <kai@FreeBSD.org>
2025-08-29 09:40:03 +0200

    textproc/py-textfsm: Update to 2.1.0
    * Switch to the PEP517 build framework.
    
    * Add TEST_ENV to ensure that tests are run against the new copy of the
      module in the staging directory and remove superfluous TEST_DEPENDS as
      it's already set via "USE_PYTHON=pytest" while I'm here.
    
    Changelogs since 1.1.0:
    
    https://github.com/google/textfsm/releases/tag/v2.1.0
    https://github.com/google/textfsm/releases/tag/v2.0.0

--- textproc/py-textfsm/files/patch-setup.py
+++ textproc/py-textfsm/files/patch-setup.py
@@ -1,13 +1,12 @@
 Remove "testdata" directory from installation. It is only required for the
 testsuite.
 
---- setup.py.orig	2019-11-26 11:17:07 UTC
+--- setup.py.orig	2025-03-21 04:48:22 UTC
 +++ setup.py
-@@ -52,7 +52,6 @@ setup(name='textfsm',
-         ]
-       },      
-       include_package_data=True,
--      package_data={'textfsm': ['../testdata/*']},
-       install_requires=['six', 'future'],
-       setup_requires=['pytest-runner'],
-       tests_require=['pytest'])
+@@ -50,6 +50,4 @@ setup(
+     ],
+     packages=['textfsm'],
+     entry_points={'console_scripts': ['textfsm=textfsm.parser:main']},
+-    include_package_data=True,
+-    package_data={'textfsm': ['../testdata/*']},
+ )
