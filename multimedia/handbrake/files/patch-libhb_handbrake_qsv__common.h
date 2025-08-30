76f17a56f91b458d121633f2f27ea1f4eb71c166
Yuichiro NAITO <naito.yuichiro@gmail.com>
2025-08-30 11:40:40 +0900

    multimedia/handbrake: update to 1.10.1
    Port changes:
    - Updated unbundled dependencies.
    - The libhb/enc_qsv.c file has been removed; I deleted the patch for
      it.
    - Message catalogs that are no longer maintained have been removed; I
      updated pkg-plist to follow.
    - Updated license block in Makefile
    
    Upstream changes:
    - https://github.com/HandBrake/HandBrake/releases/tag/1.10.0
    - https://github.com/HandBrake/HandBrake/releases/tag/1.10.1
    
    PR:		288995
    Reported by:	Yuichiro NAITO <naito.yuichiro@gmail.com> (maintainer)

--- multimedia/handbrake/files/patch-libhb_handbrake_qsv__common.h
+++ multimedia/handbrake/files/patch-libhb_handbrake_qsv__common.h
@@ -1,12 +1,17 @@
---- libhb/handbrake/qsv_common.h.orig	2024-12-01 15:28:40 UTC
+--- libhb/handbrake/qsv_common.h.orig	2025-08-08 16:41:24 UTC
 +++ libhb/handbrake/qsv_common.h
-@@ -171,7 +171,9 @@ typedef struct
+@@ -260,8 +260,13 @@ typedef struct
+ {
      mfxExtCodingOption2   codingOption2;
-     mfxExtVideoSignalInfo videoSignalInfo;
-     mfxExtHyperModeParam hyperEncodeParam;
+     hb_triplet_t*         hyperEncodeParam;
 +#if (MFX_VERSION >= 2012)
      mfxExtAV1ScreenContentTools av1ScreenContentToolsParam;
+-
 +#endif
-     mfxExtChromaLocInfo   chromaLocInfo;
-     mfxExtMasteringDisplayColourVolume masteringDisplayColourVolume;
-     mfxExtContentLightLevelInfo        contentLightLevelInfo;
++    mfxExtChromaLocInfo   chromaLocInfo;
++    mfxExtMasteringDisplayColourVolume masteringDisplayColourVolume;
++    mfxExtContentLightLevelInfo        contentLightLevelInfo;
++    mfxExtAV1BitstreamParam av1BitstreamParam;
+     struct
+     {
+         int b_pyramid;
