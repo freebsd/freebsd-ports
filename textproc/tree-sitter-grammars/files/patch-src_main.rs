c04261748c73260a7a5ce2351a8080ef3b2867f1
Mikael Urankar <mikael@FreeBSD.org>
2025-09-01 14:49:49 +0200

    textproc/tree-sitter-grammars: Add new port
    Collection of tree-sitter grammars
    
    Language support such as tokenizing and highlighting is provided by Tree-sitter.
    Each language requires a Tree-sitter grammar to have tokenization and queries
    to provide additional features such as syntax highlighting.
    
    Lapce has select few (Rust, JSON, TOML) grammars built-in for convenience.
    All other languages need to provide an externally loadable grammar in a form of
    shared library, which this port provides.
    
    PR:		278837

---
+++ textproc/tree-sitter-grammars/files/patch-src_main.rs
@@ -0,0 +1,33 @@
+--- src/main.rs.orig	2025-08-19 16:12:37 UTC
++++ src/main.rs
+@@ -88,14 +88,14 @@ fn main() -> Result<()> {
+         }
+     };
+ 
+-    let helix_dir = tmp_dir.join("helix");
+-    checkout_repo(
+-        &helix_dir,
+-        "https://github.com/helix-editor/helix",
+-        "0a4432b104099534f7a25b8ea4148234db146ab6",
+-    )?;
++//    let helix_dir = tmp_dir.join("helix");
++//    checkout_repo(
++//        &helix_dir,
++//        "https://github.com/helix-editor/helix",
++//        "0a4432b104099534f7a25b8ea4148234db146ab6",
++//    )?;
+ 
+-    let Ok(languages_config) = &fs::read_to_string(helix_dir.join("languages.toml"))
++    let Ok(languages_config) = &fs::read_to_string("languages.toml")
+     else {
+         error!("Failed to read grammars config");
+         bail!("Failed to read grammars config");
+@@ -158,7 +158,7 @@ fn build_grammar(
+     tmp_dir: &Path,
+ ) -> Result<()> {
+     let path = tmp_dir.join(format! {"tree-sitter-{}",grammar.name});
+-    checkout_repo(&path, &grammar.source.git, &grammar.source.rev)?;
++//    checkout_repo(&path, &grammar.source.git, &grammar.source.rev)?;
+     let path = if let Some(subpath) = grammar.source.subpath.as_ref() {
+         path.join(subpath)
+     } else {
