5a2e32373172d34c8024bbb5abf52e1b1f2c555a
Pavel Timofeev <timp87@gmail.com>
2025-09-05 01:30:01 +0300

    devel/c4core: Update 0.1.8 => 0.2.6
    Changelogs:
    https://github.com/biojppm/c4core/releases/tag/v0.1.9
    https://github.com/biojppm/c4core/releases/tag/v0.1.10
    https://github.com/biojppm/c4core/releases/tag/v0.1.11
    https://github.com/biojppm/c4core/releases/tag/v0.2.0
    https://github.com/biojppm/c4core/releases/tag/v0.2.1
    https://github.com/biojppm/c4core/releases/tag/v0.2.2
    https://github.com/biojppm/c4core/releases/tag/v0.2.3
    https://github.com/biojppm/c4core/releases/tag/v0.2.4
    https://github.com/biojppm/c4core/releases/tag/v0.2.5
    https://github.com/biojppm/c4core/releases/tag/v0.2.6
    
    - Refresh patches.
    - Replace PORTVERSION with DISTVERSION and VERSION in PLIST_SUB.
    
    PR:		287839
    Approved by:	Gasol Wu <gasol.wu@gmail.com> (maintainer)
    Co-authored-by:	Vladimir Druzenko <vvd@FreeBSD.org>

--- devel/c4core/files/patch-src_c4_std_string__fwd.hpp
+++ devel/c4core/files/patch-src_c4_std_string__fwd.hpp
@@ -1,4 +1,4 @@
---- src/c4/std/string_fwd.hpp.orig	2022-01-06 01:20:26 UTC
+--- src/c4/std/string_fwd.hpp.orig	2025-04-10 19:49:39 UTC
 +++ src/c4/std/string_fwd.hpp
 @@ -14,7 +14,7 @@
  // forward declarations for std::string
@@ -8,4 +8,4 @@
 +#elif defined(_LIBCPP_VERSION) || defined(__APPLE_CC__) || __cplusplus > 199711L
  #include <iosfwd>  // use the fwd header in stdlibc++
  #elif defined(_MSC_VER)
- //! @todo is there a fwd header in msvc?
+ #include "c4/error.hpp"
