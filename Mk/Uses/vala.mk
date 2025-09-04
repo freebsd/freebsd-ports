2422fef4cad2509a91b48e4fdaf4283141107c32
Charlie Li <vishwin@FreeBSD.org>
2025-09-04 13:11:02 -0400

    vala.mk: add a test arg
    While here, slightly reformat comment-docs and explicitly mention
    maintainer

--- Mk/Uses/vala.mk
+++ Mk/Uses/vala.mk
@@ -1,9 +1,12 @@
 # Handle dependency on lang/vala
 #
 # Valid args:
-# 	- lib:       add a lib depends
-# 	- build:     add a build depends
-# 	- no_depend: only used for lang/vala itself
+# 	- lib:		add a lib depends
+# 	- build:	add a build depends
+# 	- test:		add a test depends
+# 	- no_depend:	only used for lang/vala itself
+#
+# MAINTAINER: desktop@FreeBSD.org
 
 .if ! defined(_INCLUDE_USES_VALA_MK)
 _INCLUDE_USES_VALA_MK=	yes
@@ -20,6 +23,9 @@ LIB_DEPENDS+=		${_VALA_LIBRARY}:${_VALA_PORT}
 .    endif
 .    if ! empty(vala_ARGS:Mbuild)
 BUILD_DEPENDS+=		${_VALA_BINARY}:${_VALA_PORT}
+.    endif
+.    if ! empty(vala_ARGS:Mtest)
+TEST_DEPENDS+=		${_VALA_BINARY}:${_VALA_PORT}
 .    endif
 .  endif
 
