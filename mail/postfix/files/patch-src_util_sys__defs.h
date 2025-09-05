2c0331732ae8f0d23ecd475e51dc2402c7d5282c
Yasuhiro Kimura <yasu@FreeBSD.org>
2025-09-06 03:45:07 +0900

    mail/postfix: Fix build with FreeBSD 16
    Approved by:	portmgr (implicit, just fix it)

--- mail/postfix/files/patch-src_util_sys__defs.h
+++ mail/postfix/files/patch-src_util_sys__defs.h
@@ -1,11 +1,11 @@
---- src/util/sys_defs.h.orig	2023-02-13 19:36:27 UTC
+--- src/util/sys_defs.h.orig	2025-01-03 18:19:26 UTC
 +++ src/util/sys_defs.h
 @@ -31,7 +31,7 @@
      || defined(FREEBSD5) || defined(FREEBSD6) || defined(FREEBSD7) \
      || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
      || defined(FREEBSD11) || defined(FREEBSD12) || defined(FREEBSD13) \
 -    || defined(FREEBSD14) \
-+    || defined(FREEBSD14) || defined(FREEBSD15) \
++    || defined(FREEBSD14) || defined(FREEBSD15) || defined(FREEBSD16) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(OPENBSD5) || defined(OPENBSD6) || defined(OPENBSD7) \
