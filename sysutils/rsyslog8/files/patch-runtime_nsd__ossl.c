8992e2f3e2af6d032214f8ce46d22fb5f4108128
Matthew Seaman <matthew@FreeBSD.org>
2025-09-05 14:50:13 +0100

    sysutils/rsyslog8: update to 8.2508.0
    Delete files/patch-runtime_net__ossl.c and the additional patch
    72b8bcc05318f0a72ec828d1a5329fa98c021eb8.patch from upstream: these
    are both incorporated in this release.
    
    Regenerate patches.
    
    Changes:	https://github.com/rgerhards/rsyslog/blob/v8.2508.0/ChangeLog
    Reported by:	repology

--- sysutils/rsyslog8/files/patch-runtime_nsd__ossl.c
+++ sysutils/rsyslog8/files/patch-runtime_nsd__ossl.c
@@ -1,11 +1,11 @@
---- runtime/nsd_ossl.c.orig	2025-06-10 09:49:19 UTC
+--- runtime/nsd_ossl.c.orig	2025-09-05 13:46:02 UTC
 +++ runtime/nsd_ossl.c
-@@ -854,7 +854,7 @@ osslPostHandshakeCheck(nsd_ossl_t *pNsd)
- 	if (SSL_get_shared_ciphers(pNsd->pNetOssl->ssl,szDbg, sizeof szDbg) != NULL)
- 		dbgprintf("osslPostHandshakeCheck: Debug Shared ciphers = %s\n", szDbg);
+@@ -803,7 +803,7 @@ rsRetVal osslPostHandshakeCheck(nsd_ossl_t *pNsd) {
+     if (SSL_get_shared_ciphers(pNsd->pNetOssl->ssl, szDbg, sizeof szDbg) != NULL)
+         dbgprintf("osslPostHandshakeCheck: Debug Shared ciphers = %s\n", szDbg);
  
--	#if OPENSSL_VERSION_NUMBER >= 0x10002000L
+-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
 +#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER)
- 	if(SSL_get_shared_curve(pNsd->pNetOssl->ssl, -1) == 0) {
- 		// This is not a failure
- 		LogMsg(0, RS_RET_NO_ERRCODE, LOG_INFO, "nsd_ossl: "
+     if (SSL_get_shared_curve(pNsd->pNetOssl->ssl, -1) == 0) {
+         // This is not a failure
+         LogMsg(0, RS_RET_NO_ERRCODE, LOG_INFO,
