0c83dc7d34b681d33665b23329a3be075f0e2ae7
Michael Gmelin <grembo@FreeBSD.org>
2025-09-09 11:06:50 +0200

    devel/py-inotify-simple: Fix polling

--- devel/py-inotify-simple/files/patch-inotify__simple.py
+++ devel/py-inotify-simple/files/patch-inotify__simple.py
@@ -1,6 +1,6 @@
 --- inotify_simple.py.orig	2025-08-25 06:27:48 UTC
 +++ inotify_simple.py
-@@ -76,7 +76,7 @@ class INotify(FileIO):
+@@ -76,12 +76,12 @@ class INotify(FileIO):
                  object is garbage collected or when `~inotify_simple.INotify.close` is
                  called."""
              
@@ -9,3 +9,9 @@
          flags = (not inheritable) * os.O_CLOEXEC | bool(nonblocking) * os.O_NONBLOCK
          fd = _libc_call(_libc.inotify_init1, flags)
          super().__init__(fd, mode='rb', closefd=closefd)
+         self._poller = poll()
+-        self._poller.register(self.fileno())
++        self._poller.register(self.fileno(), POLLIN)
+ 
+     def add_watch(self, path, mask):
+         """Wrapper around ``inotify_add_watch()``. Returns the watch
