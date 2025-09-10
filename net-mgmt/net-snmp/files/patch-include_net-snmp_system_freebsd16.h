ce3f0c2603bfec46e2c5b6d767ac704dba75fc87
Generic Rikka <rkoberman@gmail.com>
2025-09-10 18:12:26 +0100

    net-mgmt/net-snmp: Fix build on 16
    PR:		289351
    Reviewed by:	eduardo, glebius
    Tested by:	eduardo, cy
    Differential Revision:	https://reviews.freebsd.org/D52428

---
+++ net-mgmt/net-snmp/files/patch-include_net-snmp_system_freebsd16.h
@@ -0,0 +1,6 @@
+--- /dev/null	2025-09-10 01:07:21.909920000 +0200
++++ include/net-snmp/system/freebsd16.h	2025-09-10 01:07:12.250409000 +0200
+@@ -0,0 +1,3 @@
++/* freebsd16 is a superset of freebsd15 for now */
++#include "freebsd15.h"
++#define freebsd15 freebsd15
