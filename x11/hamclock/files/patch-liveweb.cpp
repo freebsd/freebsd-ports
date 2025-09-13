607dc303e45242ba723ce3fa1cae21ef363be34a
wen <wenheping2000@hotmail.com>
2025-09-13 14:23:27 +0400

    x11/hamclock: Update 2.80 => 4.21
    Changelogs:
    * https://www.clearskyinstitute.com/ham/HamClock/#tab-download
    
    * Pet portfmt(1) and portlint(1)
    
    PR:		289479
    Approved by:	hamradio@ (alven@)
    Approved by:	yuri@ (Mentor)

---
+++ x11/hamclock/files/patch-liveweb.cpp
@@ -0,0 +1,11 @@
+--- liveweb.cpp.orig	2025-09-12 01:16:46 UTC
++++ liveweb.cpp
+@@ -12,7 +12,7 @@
+ #include "HamClock.h"
+ 
+ // web socket library
+-#include "ws.h"
++#include "wsServer/include/ws.h"
+ 
+ 
+ // import png writer -- complete implementation in a header file -- amazing.
