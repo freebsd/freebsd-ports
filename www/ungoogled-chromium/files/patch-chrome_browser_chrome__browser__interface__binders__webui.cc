2379f815f449705ebd9a50b5f5f629f19be45b84
Robert Nagy <rnagy@FreeBSD.org>
2025-09-13 19:25:43 +0200

    www/ungoogled-chromium: update to 140.0.7339.127
    Security:	https://vuxml.freebsd.org/freebsd/f50640fa-89a4-4795-a302-47b0dea8cee5.html

--- www/ungoogled-chromium/files/patch-chrome_browser_chrome__browser__interface__binders__webui.cc
+++ www/ungoogled-chromium/files/patch-chrome_browser_chrome__browser__interface__binders__webui.cc
@@ -1,4 +1,4 @@
---- chrome/browser/chrome_browser_interface_binders_webui.cc.orig	2025-09-10 13:22:16 UTC
+--- chrome/browser/chrome_browser_interface_binders_webui.cc.orig	2025-09-11 16:30:26 UTC
 +++ chrome/browser/chrome_browser_interface_binders_webui.cc
 @@ -51,18 +51,18 @@
  #include "mojo/public/cpp/bindings/binder_map.h"
@@ -48,7 +48,7 @@
        WhatsNewUI,
  #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
        NewTabPageUI>(map);
-@@ -1190,7 +1190,7 @@ void PopulateChromeWebUIFrameBinders(
+@@ -1191,7 +1191,7 @@ void PopulateChromeWebUIFrameBinders(
  #endif  // BUILDFLAG(IS_CHROMEOS)
  
  #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
--- www/ungoogled-chromium/files/patch-ui_accessibility_accessibility__features.h
+++ www/ungoogled-chromium/files/patch-ui_accessibility_accessibility__features.h
@@ -1,6 +1,6 @@
---- ui/accessibility/accessibility_features.h.orig	2025-09-10 13:22:16 UTC
+--- ui/accessibility/accessibility_features.h.orig	2025-09-11 16:30:26 UTC
 +++ ui/accessibility/accessibility_features.h
-@@ -339,7 +339,7 @@ AX_BASE_EXPORT bool IsScreenAIOCREnabled();
+@@ -345,7 +345,7 @@ AX_BASE_EXPORT bool IsScreenAIOCREnabled();
  AX_BASE_EXPORT BASE_DECLARE_FEATURE(kScreenAITestMode);
  AX_BASE_EXPORT bool IsScreenAITestModeEnabled();
  
@@ -9,7 +9,7 @@
  // Enables advanced partition allocation checks in ScreenAI service.
  // TODO(crbug.com/418199684): Remove when the bug is fixed.
  AX_BASE_EXPORT BASE_DECLARE_FEATURE(
-@@ -366,7 +366,7 @@ AX_BASE_EXPORT BASE_DECLARE_FEATURE(kBlockRootWindowAc
+@@ -372,7 +372,7 @@ AX_BASE_EXPORT BASE_DECLARE_FEATURE(kBlockRootWindowAc
  AX_BASE_EXPORT bool IsBlockRootWindowAccessibleNameChangeEventEnabled();
  #endif  // BUILDFLAG(IS_MAC)
  
