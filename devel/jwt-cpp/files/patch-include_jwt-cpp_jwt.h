92ac6c157ebbe4dba1bdf412f4ea8e25885eaaf2
Xavier Beaudouin <kiwi@FreeBSD.org>
2025-09-15 13:40:47 +0200

    devel/jwt-cpp: Use release assets and other fixes
    After the nice suggestion from @diizzy and @mat, this port
    will use the release assets instead.
    LICENSE_FILE has been added and remove the picojson.h install.
    
    Approved by:	0mp (mentor)
    Approved by:	diizzy
    Differential Revision:	https://reviews.freebsd.org/D50656

---
+++ devel/jwt-cpp/files/patch-include_jwt-cpp_jwt.h
@@ -0,0 +1,11 @@
+--- include/jwt-cpp/jwt.h.orig	2025-09-15 07:23:21 UTC
++++ include/jwt-cpp/jwt.h
+@@ -5,7 +5,7 @@
+ #ifndef PICOJSON_USE_INT64
+ #define PICOJSON_USE_INT64
+ #endif
+-#include "picojson/picojson.h"
++#include "picojson.h"
+ #endif
+ 
+ #ifndef JWT_DISABLE_BASE64
