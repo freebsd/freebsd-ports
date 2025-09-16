aca2d57f1b19d49a76671944f7eba41e337e70a5
Jason W. Bacon <jwb@FreeBSD.org>
2025-09-16 08:36:32 -0500

    deskutils/coregarage: Update to 5.0.0
    Switch to Qt6
    Changes: https://gitlab.com/cubocore/coreapps/coregarage/-/commits/master
    
    PR:             289559
    Reported by:    diizzy

--- deskutils/coregarage/files/patch-src_settings.cpp
+++ deskutils/coregarage/files/patch-src_settings.cpp
@@ -1,9 +1,9 @@
---- settings.cpp.orig	2023-08-27 20:32:18 UTC
-+++ settings.cpp
+--- src/settings.cpp.orig	2025-09-13 11:52:38 UTC
++++ src/settings.cpp
 @@ -177,9 +177,9 @@ void settings::setDefaultSettings(QSettings& settings)
      // Add system font to CorePad, CoreTerminal
      QFont monoFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
-     if ( not monoFont.family().count() ) {
+     if ( not monoFont.family().length() ) {
 -        monoFont = QFont("monospace", 9);}
 +        monoFont = QFont("monospace", 12);}
      if (monoFont.styleHint() != QFont::Monospace) {
