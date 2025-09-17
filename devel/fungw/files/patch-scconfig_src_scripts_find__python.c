6ba8d23fffae9b35ccc047c3375ee809bdf23f49
Robert Clausecker <fuz@FreeBSD.org>
2025-09-17 19:00:10 +0200

    devel/fungw: update to 1.2.2
    Changelog: http://www.repo.hu/projects/fungw/releases/changelog-1.2.2.txt

--- devel/fungw/files/patch-scconfig_src_scripts_find__python.c
+++ devel/fungw/files/patch-scconfig_src_scripts_find__python.c
@@ -1,19 +1,12 @@
---- scconfig/src/scripts/find_python.c.orig	2019-01-09 06:46:09 UTC
+--- scconfig/src/scripts/find_python.c.orig	2025-09-17 16:31:52 UTC
 +++ scconfig/src/scripts/find_python.c
-@@ -43,13 +43,13 @@ static int find_script_python_(const char *name, int l
- 
- 	char *inc_py =
- 		NL "import distutils.sysconfig;"
--		NL "print '-I' + distutils.sysconfig.get_python_inc().replace('\\\\','/')"
-+		NL "print ('-I' + distutils.sysconfig.get_python_inc().replace('\\\\','/'))"
+@@ -47,7 +47,7 @@ static int find_script_python_(const char *name, int l
  		NL;
  	char *lib_py =
  		NL "import distutils.sysconfig;"
--		NL "print '-L' + distutils.sysconfig.PREFIX.replace('\\\\','/') + '/libs',;"
+-		NL "print ('-L' + distutils.sysconfig.PREFIX.replace('\\\\','/') + '/libs')"
 +		NL "print ('-L' + distutils.sysconfig.PREFIX.replace('\\\\','/') + '/lib')"
  		NL "import sys;"
--		NL "print '-lpython' + str(sys.version_info[0]) + str(sys.version_info[1])"
+-		NL "print ('-lpython' + str(sys.version_info[0]) + str(sys.version_info[1]))"
 +		NL "print ('-lpython' + str(sys.version_info[0]) + '.' + str(sys.version_info[1]))"
  		NL;
- 
- 
