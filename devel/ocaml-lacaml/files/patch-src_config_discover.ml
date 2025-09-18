25bfd8147fec94070189e30b8a2ec71227d99974
Robert Clausecker <fuz@FreeBSD.org>
2025-09-18 21:52:00 +0200

    devel/ocaml-lacaml: remove -march=native
    An existing site patch tried to remove -march=native, but this failed
    to be effective.  Rework the patch so -march=native is never passed.
    
    Approved by:	portmgr (build fix blanket)
    MFH:		2025Q3

--- devel/ocaml-lacaml/files/patch-src_config_discover.ml
+++ devel/ocaml-lacaml/files/patch-src_config_discover.ml
@@ -1,11 +1,12 @@
---- src/config/discover.ml.orig	2024-12-26 19:34:31 UTC
+--- src/config/discover.ml.orig	2023-05-16 14:23:01 UTC
 +++ src/config/discover.ml
-@@ -46,7 +46,7 @@ let () =
+@@ -46,8 +46,6 @@ let () =
        (* -march=native is not supported on Apple ARM64 yet.
           Its support was introduced in clang >= 15.0.0 *)
        match maybe_system, maybe_arch with
 -      | Some "macosx", Some "arm64"
-+      | Some "bsd_elf", _
-       | _, (None | Some ("ppc64" | "ppc64le" | "unknown")) -> shared
-       | _ -> "-march=native" :: shared
+-      | _, (None | Some ("ppc64" | "ppc64le" | "unknown")) -> shared
+-      | _ -> "-march=native" :: shared
++      | _ -> shared
      in
+     C.Flags.write_sexp "extra_c_flags.sexp" extra_cflags)
