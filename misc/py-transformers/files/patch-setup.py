a143130d8cc97a9152e5aba2883e4cee18c0384b
Yuri Victorovich <yuri@FreeBSD.org>
2025-09-18 23:39:57 -0700

    misc/py-transformers: Remove upper version limit for the 'tokenizers' dependency

---
+++ misc/py-transformers/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2025-09-19 06:12:24 UTC
++++ setup.py
+@@ -189,7 +189,7 @@ _deps = [
+     "timeout-decorator",
+     "tiktoken",
+     "timm<=1.0.19,!=1.0.18",
+-    "tokenizers>=0.21,<0.22",
++    "tokenizers>=0.21",
+     "torch>=2.1",
+     "torchaudio",
+     "torchvision",
---
+++ misc/py-transformers/files/patch-src_transformers_dependency__versions__table.py
@@ -0,0 +1,11 @@
+--- src/transformers/dependency_versions_table.py.orig	2025-09-19 06:16:57 UTC
++++ src/transformers/dependency_versions_table.py
+@@ -91,7 +91,7 @@ deps = {
+     "timeout-decorator": "timeout-decorator",
+     "tiktoken": "tiktoken",
+     "timm": "timm<=1.0.19,!=1.0.18",
+-    "tokenizers": "tokenizers>=0.21,<0.22",
++    "tokenizers": "tokenizers>=0.21",
+     "torch": "torch>=2.1",
+     "torchaudio": "torchaudio",
+     "torchvision": "torchvision",
