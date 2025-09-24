a448aeb00153c3465a30ff41ecf43f8896a50d52
Mikael Urankar <mikael@FreeBSD.org>
2025-09-24 13:13:28 +0200

    devel/electron37: Fix build with rust 1.90.0
    error:
    ld.lld: error: undefined symbol: __rustc::__rust_alloc_error_handler_should_panic_v2
    
    PR:		289709
    Approved by:	portmgr (build fix blanket)

--- devel/electron37/files/patch-build_rust_allocator_lib.rs
+++ devel/electron37/files/patch-build_rust_allocator_lib.rs
@@ -1,15 +1,21 @@
 --- build/rust/allocator/lib.rs.orig	2025-08-11 20:33:34 UTC
 +++ build/rust/allocator/lib.rs
-@@ -86,6 +86,12 @@ mod both_allocators {
-     /// As part of rustc's contract for using `#[global_allocator]` without
-     /// rustc-generated shims we must define this symbol, since we are opting in
-     /// to unstable functionality. See https://github.com/rust-lang/rust/issues/123015
+@@ -90,7 +90,18 @@ mod both_allocators {
+     #[linkage = "weak"]
+     static __rust_no_alloc_shim_is_unstable: u8 = 0;
+ 
 +    #[rustc_std_internal_symbol]
 +    #[linkage = "weak"]
 +    fn __rust_no_alloc_shim_is_unstable_v2() {}
 +
-+    // TODO(crbug.com/422538133) Remove after rolling past
-+    // https://github.com/rust-lang/rust/pull/141061
-     #[no_mangle]
++    #[rustc_std_internal_symbol]
++    #[linkage = "weak"]
++    fn __rust_alloc_error_handler_should_panic_v2() -> u8 {
++        0
++    }
++
+     // Mangle the symbol name as rustc expects.
++    // TODO(crbug.com/440481922): Remove this after rolling past https://github.com/rust-lang/rust/pull/143387
+     #[rustc_std_internal_symbol]
+     #[allow(non_upper_case_globals)]
      #[linkage = "weak"]
-     static __rust_no_alloc_shim_is_unstable: u8 = 0;
