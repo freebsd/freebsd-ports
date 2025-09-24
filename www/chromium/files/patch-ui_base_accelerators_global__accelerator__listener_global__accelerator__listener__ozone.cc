ba96a3122b3ae832a6198a0b947683e808f15763
Robert Nagy <rnagy@FreeBSD.org>
2025-09-24 08:24:17 +0200

    www/chromium: update to 140.0.7339.127
    Security:	https://vuxml.freebsd.org/freebsd/57b54de1-85a5-439a-899e-75d19cbdff54.html
    (cherry picked from commit 297fe22dde3146bc2980a847c700dbac445637da)

--- www/chromium/files/patch-ui_base_accelerators_global__accelerator__listener_global__accelerator__listener__ozone.cc
+++ www/chromium/files/patch-ui_base_accelerators_global__accelerator__listener_global__accelerator__listener__ozone.cc
@@ -1,4 +1,4 @@
---- ui/base/accelerators/global_accelerator_listener/global_accelerator_listener_ozone.cc.orig	2025-07-02 06:08:04 UTC
+--- ui/base/accelerators/global_accelerator_listener/global_accelerator_listener_ozone.cc.orig	2025-09-24 06:14:47 UTC
 +++ ui/base/accelerators/global_accelerator_listener/global_accelerator_listener_ozone.cc
 @@ -12,7 +12,7 @@
  #include "ui/base/accelerators/accelerator.h"
@@ -8,8 +8,8 @@
 +#if (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)) && BUILDFLAG(USE_DBUS)
  #include "base/environment.h"
  #include "base/feature_list.h"
- #include "build/branding_buildflags.h"
-@@ -22,7 +22,7 @@
+ #include "base/nix/xdg_util.h"
+@@ -23,7 +23,7 @@
  using content::BrowserThread;
  
  namespace {
@@ -18,12 +18,12 @@
  BASE_FEATURE(kGlobalShortcutsPortal,
               "GlobalShortcutsPortal",
               base::FEATURE_ENABLED_BY_DEFAULT);
-@@ -74,7 +74,7 @@ GlobalAcceleratorListener* GlobalAcceleratorListener::
+@@ -75,7 +75,7 @@ GlobalAcceleratorListener* GlobalAcceleratorListener::
      return instance->get();
    }
  
 -#if BUILDFLAG(IS_LINUX) && BUILDFLAG(USE_DBUS)
 +#if (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)) && BUILDFLAG(USE_DBUS)
-   if (base::FeatureList::IsEnabled(kGlobalShortcutsPortal)) {
-     static GlobalAcceleratorListenerLinux* const linux_instance =
-         new GlobalAcceleratorListenerLinux(nullptr, GetSessionName());
+   // ListShortcuts on GNOME will return an empty list when the session is
+   // created, making this class incorrectly believe it must rebind all
+   // shortcuts, leading to a dialog shown on every browser start.
