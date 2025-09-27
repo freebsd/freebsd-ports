c5f57c13a2920628b698c5cebfa1685398c9c3ef
mew14930xvi <mew14930xvi@inbox.lv>
2025-09-27 18:24:37 +0300

    astro/openuniverse: Fix freeglut runtime error, take maintainership
    $ openuniverse
    freeglut (openuniverse): ERROR:  No display callback registered for window 1
    
    PR:	289859

--- astro/openuniverse/files/patch-src_ou.cpp
+++ astro/openuniverse/files/patch-src_ou.cpp
@@ -1,11 +1,10 @@
 --- src/ou.cpp.orig	2000-06-04 21:29:29 UTC
 +++ src/ou.cpp
-@@ -22,7 +22,7 @@
- #include <string.h>
- #include <math.h>
- #include <time.h>
--#include <sys/timeb.h>
-+#include "timeb.h"
- #include <setjmp.h>
- #include "ou.h"
- #include "gui.h"
+@@ -560,6 +560,7 @@ int main(int argc, char *argv[])
+ #else
+ 	glutReshapeFunc(Reshape);
+ #endif
++	glutDisplayFunc(glutPostRedisplay);
+ 	glutMainLoop();
+ 	return 0;
+ }
