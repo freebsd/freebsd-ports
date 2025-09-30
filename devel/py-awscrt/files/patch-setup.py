951eac07b1b4b7ad904beba44f55a61680054e6d
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-09-30 18:43:08 +0800

    devel/py-awscrt: Update to 0.27.6
    Changes:	https://github.com/awslabs/aws-crt-python/releases

--- devel/py-awscrt/files/patch-setup.py
+++ devel/py-awscrt/files/patch-setup.py
@@ -1,6 +1,18 @@
---- setup.py.orig	2024-12-06 18:35:30 UTC
+--- setup.py.orig	2025-08-12 19:03:48 UTC
 +++ setup.py
-@@ -359,7 +359,7 @@ def awscrt_ext():
+@@ -11,7 +11,10 @@ import sysconfig
+ import subprocess
+ import sys
+ import sysconfig
+-from setuptools.command.bdist_wheel import bdist_wheel
++try:
++    from setuptools.command.bdist_wheel import bdist_wheel
++except:
++    from wheel.bdist_wheel import bdist_wheel
+ 
+ if sys.platform == 'win32':
+     # distutils is deprecated in Python 3.10 and removed in 3.12. However, it still works because Python defines a compatibility interface as long as setuptools is installed.
+@@ -435,7 +438,7 @@ def awscrt_ext():
              # So it's simpler to link them in statically and have less runtime dependencies.
              #
              # Don't apply this trick to dependencies that are always on the OS (e.g. librt)
