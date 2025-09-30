b4cb90cbafdf451eb5c349639dcf9c96941493e2
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-09-30 18:44:21 +0800

    security/py-krb5: Update to 0.8.0
    Changes:	https://github.com/jborean93/pykrb5/releases

--- security/py-krb5/files/patch-src-krb5-_principal.pyi
+++ security/py-krb5/files/patch-src-krb5-_principal.pyi
@@ -1,10 +1,10 @@
---- src/krb5/_principal.pyi.orig	2022-08-08 21:14:44 UTC
+--- src/krb5/_principal.pyi.orig	2025-09-01 04:40:28 UTC
 +++ src/krb5/_principal.pyi
 @@ -13,7 +13,6 @@ class PrincipalParseFlags(enum.IntEnum):
-     no_realm: PrincipalParseFlags = ...  #: Error if realm is present
-     require_realm: PrincipalParseFlags = ...  #: Error if realm is not present
-     enterprise: PrincipalParseFlags = ...  #: Create single-component enterprise principal
--    ignore_realm: PrincipalParseFlags = ...  #: Ignore realm if present
+     no_realm = ...  #: Error if realm is present
+     require_realm = ...  #: Error if realm is not present
+     enterprise = ...  #: Create single-component enterprise principal
+-    ignore_realm = ...  #: Ignore realm if present
  
  class PrincipalUnparseFlags(enum.IntEnum):
      """Flags used to control :meth:`unparse_name_flags`."""
