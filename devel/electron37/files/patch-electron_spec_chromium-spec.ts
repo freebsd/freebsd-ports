3261c41d9c6965cece43c41efa1b63696307cfb2
Hiroki Tagato <tagattie@FreeBSD.org>
2025-10-01 05:28:59 +0900

    devel/electron37: update to 37.6.0
    Changelog: https://github.com/electron/electron/releases/tag/v37.6.0
    
    Reported by:	GitHub (watch releases)

--- devel/electron37/files/patch-electron_spec_chromium-spec.ts
+++ devel/electron37/files/patch-electron_spec_chromium-spec.ts
@@ -1,4 +1,4 @@
---- electron/spec/chromium-spec.ts.orig	2025-08-13 14:21:20 UTC
+--- electron/spec/chromium-spec.ts.orig	2025-09-27 17:30:52 UTC
 +++ electron/spec/chromium-spec.ts
 @@ -475,13 +475,13 @@ describe('command line switches', () => {
      it('should not set an invalid locale', async () => testLocale('asdfkl', `${currentLocale}|${currentSystemLocale}|${currentPreferredLanguages}`));
@@ -18,7 +18,7 @@
    });
  
    describe('--remote-debugging-pipe switch', () => {
-@@ -2958,12 +2958,12 @@ describe('font fallback', () => {
+@@ -3213,12 +3213,12 @@ describe('font fallback', () => {
        expect(fonts[0].familyName).to.equal('Arial');
      } else if (process.platform === 'darwin') {
        expect(fonts[0].familyName).to.equal('Helvetica');
@@ -33,7 +33,7 @@
      const html = `
      <html lang="ja-JP">
        <head>
-@@ -3516,7 +3516,7 @@ describe('paste execCommand', () => {
+@@ -3771,7 +3771,7 @@ describe('paste execCommand', () => {
    });
  });
  
