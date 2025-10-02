4b9f219a6e5f61e8785b3a2ed6757a6cf36121d9
Matthias Andree <mandree@FreeBSD.org>
2025-10-02 10:53:49 +0200

    Mk/: update openssl3* comments for newer ports
    Now that security/openssl36 is official
    
    related to:
    PR:		289938
    
    (cherry picked from commit 092db47e9db9d5250b80b983313be71383aa1642)

--- Mk/Uses/ssl.mk
+++ Mk/Uses/ssl.mk
@@ -10,8 +10,8 @@
 #
 # When updating this, please also update the same list in bsd.default-versions.mk
 # and the checks for USES=ssl in qa.sh!
-# Variants being base, openssl, openssl111, openssl31, openssl32,
-# openssl33, libressl, and libressl-devel.
+# Variants being base, openssl, openssl111, openssl33, openssl34, openssl35,
+# openssl36, libressl, and libressl-devel.
 #
 # The Makefile sets these variables:
 # OPENSSLBASE		- "/usr" or ${LOCALBASE}
