4d2bce94cd4a91697cac5f0302e1213d0f59984a
Juraj Lutter <otis@FreeBSD.org>
2025-10-03 11:33:52 +0200

    mail/postfix-current: Update to 3.11-20250928
    - Update to 3.11-20250928
    - Make postscreen blacklistd-aware [1]
    - Fix the build on FreeBSD 16.0-CURRENT
    
    Co-authored-by:	Michael Grimm <trashcan@ellael.org>
    PR:		289737

--- mail/postfix-current/files/patch-src_util_sys__defs.h
+++ mail/postfix-current/files/patch-src_util_sys__defs.h
@@ -5,7 +5,7 @@
      || defined(FREEBSD8) || defined(FREEBSD9) || defined(FREEBSD10) \
      || defined(FREEBSD11) || defined(FREEBSD12) || defined(FREEBSD13) \
 -    || defined(FREEBSD14) \
-+    || defined(FREEBSD14) || defined(FREEBSD15) \
++    || defined(FREEBSD14) || defined(FREEBSD15) || defined(FREEBSD16) \
      || defined(BSDI2) || defined(BSDI3) || defined(BSDI4) \
      || defined(OPENBSD2) || defined(OPENBSD3) || defined(OPENBSD4) \
      || defined(OPENBSD5) || defined(OPENBSD6) || defined(OPENBSD7) \
