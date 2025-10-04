685a4fcad8037cc286378b01d619957e72fe5fbc
Hiroki Tagato <tagattie@FreeBSD.org>
2025-10-05 05:34:41 +0900

    devel/electron38: update to 38.2.1
    While here, fix build with rust 1.90.[1]
    
    Changelog: https://github.com/electron/electron/releases/tag/v38.2.1
    
    Reported by:	GitHub (watch releases)
    Obtained from:	https://cgit.freebsd.org/ports/commit/?id=a448aeb00153c3465a30ff41ecf43f8896a50d52 [1]

--- devel/electron38/files/patch-build_rust_allocator_lib.rs
+++ devel/electron38/files/patch-build_rust_allocator_lib.rs
@@ -1,15 +1,17 @@
---- build/rust/allocator/lib.rs.orig	2025-08-26 20:49:50 UTC
+--- build/rust/allocator/lib.rs.orig	2025-09-09 01:18:53 UTC
 +++ build/rust/allocator/lib.rs
-@@ -90,6 +90,12 @@ mod both_allocators {
+@@ -90,7 +90,14 @@ mod both_allocators {
      #[linkage = "weak"]
      fn __rust_no_alloc_shim_is_unstable_v2() {}
  
-+    // TODO(crbug.com/422538133) Remove after rolling past
-+    // https://github.com/rust-lang/rust/pull/141061
-+    #[no_mangle]
++    #[rustc_std_internal_symbol]
 +    #[linkage = "weak"]
-+    static __rust_no_alloc_shim_is_unstable: u8 = 0;
++    fn __rust_alloc_error_handler_should_panic_v2() -> u8 {
++        0
++    }
 +
      // Mangle the symbol name as rustc expects.
++    // TODO(crbug.com/440481922): Remove this after rolling past https://github.com/rust-lang/rust/pull/143387
      #[rustc_std_internal_symbol]
      #[allow(non_upper_case_globals)]
+     #[linkage = "weak"]
