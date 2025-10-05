d4b1dd743cb498cb16fbd4fdb311b2613ffd12a4
David Hummel <hummeltech@sherpaguru.com>
2025-10-04 20:39:04 -0700

    graphics/mapnik: update to 4.1.3
    New tiles input plugin on i386 building has been fixed, re-enabling.
    Updated patch from dd0ccaf9dd47b0738b9ca84ced8265d8e837cbd4.
    
    Changelog: https://github.com/mapnik/mapnik/blob/v4.1.3/CHANGELOG.md
    Compare changes: https://github.com/mapnik/mapnik/compare/v4.1.0...v4.1.3
    
    PR:		290006
    Reported by:	David Hummel <hummeltech@sherpaguru.com> (maintainer)

--- graphics/mapnik/files/patch-plugins_input_tiles_pmtiles__source.hpp
+++ graphics/mapnik/files/patch-plugins_input_tiles_pmtiles__source.hpp
@@ -1,11 +1,20 @@
---- plugins/input/tiles/pmtiles_source.hpp.orig	2025-09-17 23:55:13 UTC
+--- plugins/input/tiles/pmtiles_source.hpp.orig	2025-10-05 04:06:02 UTC
 +++ plugins/input/tiles/pmtiles_source.hpp
-@@ -511,7 +511,7 @@ class pmtiles_source : public tiles_source
-         }
-         else
-         {
--            metadata = {data() + metadata_offset, metadata_length};
-+            metadata = {data() + metadata_offset, static_cast<size_t>(metadata_length)};
-         }
-         boost::json::value json_value;
-         try
+@@ -470,7 +470,7 @@ class pmtiles_source : public tiles_source,
+             {
+                 std::string decompressed_dir;
+ #if defined(MAPNIK_MEMORY_MAPPED_FILE)
+-                std::string_view buffer{file_.buffer().first + dir_offset, dir_length};
++                std::string_view buffer{file_.buffer().first + dir_offset, static_cast<std::size_t>(dir_length)};
+ #else
+                 std::string buffer;
+                 buffer.resize(dir_length);
+@@ -559,7 +559,7 @@ class pmtiles_source : public tiles_source,
+     {
+         std::string metadata;
+ #if defined(MAPNIK_MEMORY_MAPPED_FILE)
+-        std::string_view buffer{file_.buffer().first + metadata_offset_, metadata_length_};
++        std::string_view buffer{file_.buffer().first + metadata_offset_, static_cast<std::size_t>(metadata_length_)};
+ #else
+         std::string buffer;
+         buffer.resize(metadata_length_);
