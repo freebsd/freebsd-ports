f07bc6e23dc66bc4d886d148259f91ba93344cc3
Jason W. Bacon <jwb@FreeBSD.org>
2025-10-05 13:27:19 -0500

    biology/vsearch: Update to 2.30.1
    Numerous small fixes and enhancements
    Changes: https://github.com/torognes/vsearch/releases
    
    Reported by:    portscout

--- biology/vsearch/files/patch-src_searchcore.cc
+++ biology/vsearch/files/patch-src_searchcore.cc
@@ -1,18 +1,18 @@
---- src/searchcore.cc.orig	2024-09-26 10:48:55 UTC
+--- src/searchcore.cc.orig	2025-10-05 12:35:57 UTC
 +++ src/searchcore.cc
-@@ -239,9 +239,14 @@ auto search_topscores(struct searchinfo_s * si) -> voi
-       if (bitmap)
+@@ -285,9 +285,14 @@ auto search_topscores(struct searchinfo_s * searchinfo
+       if (bitmap != nullptr)
          {
  #ifdef __x86_64__
-+	  // Not sure how to enable ssse3 without enabling other
-+	  // non-portable features.
-+	  // This code is only used rarely, so disable ssse3 for now.
-+	  // It won't affect performance much on the whole.
-+	  // https://github.com/torognes/vsearch/pull/497
-           if (ssse3_present)
++          // Not sure how to enable ssse3 without enabling other
++          // non-portable features.
++          // This code is only used rarely, so disable ssse3 for now.
++          // It won't affect performance much on the whole.
++          // https://github.com/torognes/vsearch/pull/497
+           if (ssse3_present != 0)
              {
--              increment_counters_from_bitmap_ssse3(si->kmers,
-+              increment_counters_from_bitmap_sse2(si->kmers,
+-              increment_counters_from_bitmap_ssse3(searchinfo->kmers,
++              increment_counters_from_bitmap_sse2(searchinfo->kmers,
                                                     bitmap, indexed_count);
              }
            else
