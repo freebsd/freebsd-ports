67ad101165aac9580d158979c150add8f569a78f
Yuri Victorovich <yuri@FreeBSD.org>
2025-10-10 20:48:01 -0700

    biology/seqwish: Unbundle sdsl-lite and fix some clang19 compilation errors

--- biology/seqwish/files/patch-deps_atomic__queue_include_atomic__queue_atomic__queue.h
+++ biology/seqwish/files/patch-deps_atomic__queue_include_atomic__queue_atomic__queue.h
@@ -1,32 +1,8 @@
-- fix for PPC
-- fix build with clang19
+- fix compilation error with clang-19
 
---- deps/paryfor/paryfor.hpp.orig 2024-10-15 18:25:21 UTC
-+++ deps/paryfor/paryfor.hpp
-@@ -51,7 +51,22 @@ static inline void spin_loop_pause() noexcept {
- }
- } // namespace atomic_queue
- } // namespace paryfor
-+#elif defined(__ppc64__) || defined(__powerpc64__)
-+namespace paryfor {
-+namespace atomic_queue {
-+constexpr int CACHE_LINE_SIZE = 128; // TODO: Review that this is the correct value.
-+static inline void spin_loop_pause() noexcept {
-+    asm volatile("or 31,31,31 # very low priority"); // TODO: Review and benchmark that this is the right instruction.
-+}
-+} // namespace atomic_queue
-+} // namespace paryfor
- #else
-+namespace paryfor {
-+namespace atomic_queue {
-+constexpr int CACHE_LINE_SIZE = 64; // TODO: Review that this is the correct value.
-+static inline void spin_loop_pause() noexcept {}
-+} // namespace atomic_queue
-+} // namespace paryfor
- #error "Unknown CPU architecture."
- #endif
- 
-@@ -453,13 +468,13 @@ class AtomicQueue2 : public AtomicQueueCommon<AtomicQ
+--- deps/atomic_queue/include/atomic_queue/atomic_queue.h.orig	2025-10-10 06:48:58.627210000 -0700
++++ deps/atomic_queue/include/atomic_queue/atomic_queue.h	2025-10-10 06:49:07.323108000 -0700
+@@ -393,13 +393,13 @@
  
      T do_pop(unsigned tail) noexcept {
          unsigned index = details::remap_index<SHUFFLE_BITS>(tail % size_);
@@ -42,7 +18,7 @@
      }
  
  public:
-@@ -581,13 +596,13 @@ class AtomicQueue2B : public AtomicQueueCommon<Atomic
+@@ -521,13 +521,13 @@
  
      T do_pop(unsigned tail) noexcept {
          unsigned index = details::remap_index<SHUFFLE_BITS>(tail & (size_ - 1));
