5af70fbdad18148439f858b63c4a3a8b92ff6e13
Hiroki Tagato <tagattie@FreeBSD.org>
2025-10-12 10:47:47 +0900

    editors/vscode: update to 1.105.0
    While here, add USES=display:test for launching and stopping Xvfb for
    test.
    
    Changelog: https://code.visualstudio.com/updates/v1_105
    
    Reported by:	GitHub (watch releases)

--- editors/vscode/files/patch-build_gulpfile.extensions.js
+++ editors/vscode/files/patch-build_gulpfile.extensions.js
@@ -1,6 +1,6 @@
---- build/gulpfile.extensions.js.orig	2025-09-09 22:02:15 UTC
+--- build/gulpfile.extensions.js.orig	2025-10-08 13:50:39 UTC
 +++ build/gulpfile.extensions.js
-@@ -235,7 +235,7 @@ const compileNonNativeExtensionsBuildTask = task.defin
+@@ -236,7 +236,7 @@ const compileNonNativeExtensionsBuildTask = task.defin
   * @note this does not clean the directory ahead of it. See {@link cleanExtensionsBuildTask} for that.
   */
  const compileNonNativeExtensionsBuildTask = task.define('compile-non-native-extensions-build', task.series(
