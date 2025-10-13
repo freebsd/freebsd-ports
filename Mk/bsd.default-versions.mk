84031afde9c509eff50f5ed4af3e365dd11eb704
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-10-13 14:26:02 +0800

    Mk/bsd.default-versions.mk: Update comment: add pgsql 18

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -130,7 +130,7 @@ _PERL5_FROM_BIN!=	${LOCALBASE}/bin/perl -e 'printf "%vd\n", $$^V;'
 _EXPORTED_VARS+=	_PERL5_FROM_BIN
 PERL5_DEFAULT:=		${_PERL5_FROM_BIN:R}
 .  endif
-# Possible values: 13, 14, 15, 16, 17
+# Possible values: 13, 14, 15, 16, 17, 18
 PGSQL_DEFAULT?=		17
 # Possible values: 8.1, 8.2, 8.3, 8.4, 8.5
 PHP_DEFAULT?=		8.3
