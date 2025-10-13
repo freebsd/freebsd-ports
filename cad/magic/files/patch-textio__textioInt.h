034509a7514b9cda008e6a9743fd9cf0c2b15253
Älven <alven@FreeBSD.org>
2025-10-13 12:45:27 +0400

    cad/magic: Update 8.3.526 => 8.3.561
    Changelogs:
    * https://github.com/RTimothyEdwards/magic/releases/tag/8.3.561
    * https://github.com/RTimothyEdwards/magic/compare/8.3.526...8.3.561
    
    * Pet portfmt(1), sort entries
    
    Approved by:		yuri@ (maintainer, Mentor)
    Differential Revision:	https://reviews.freebsd.org/D53054

--- cad/magic/files/patch-textio__textioInt.h
+++ cad/magic/files/patch-textio__textioInt.h
@@ -1,4 +1,4 @@
---- textio/textioInt.h.orig	2025-02-24 07:00:02 UTC
+--- textio/textioInt.h.orig	2025-07-30 06:00:02 UTC
 +++ textio/textioInt.h
 @@ -43,7 +43,7 @@ typedef struct {
  #define TX_CMD_PROMPT	":"
@@ -6,6 +6,6 @@
  /* all of the state associated with a tty terminal */
 -#if !defined(SYSV) && !defined(CYGWIN) && !defined(__OpenBSD__) && !defined(EMSCRIPTEN)
 +#if !defined(SYSV) && !defined(CYGWIN) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(EMSCRIPTEN)
- typedef struct {
-     struct sgttyb tx_i_sgtty;
-     struct tchars tx_i_tchars;
+ #if defined(HAVE_SYS_IOCTL_COMPAT_H) || defined(HAVE_SGTTY_H)
+ #if defined(HAVE_SYS_IOCTL_COMPAT_H)
+ #include <sys/ioctl_compat.h> /* replaced sgtty.h */
