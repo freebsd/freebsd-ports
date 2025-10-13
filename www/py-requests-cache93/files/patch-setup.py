8bab09dbcaec300899de1c348e4eea3ee81daf80
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-10-13 14:26:25 +0800

    www/py-requests-cache93: Relax version requirement of RUN_DEPENDS
    - Bump PORTREVISION for package change

--- www/py-requests-cache93/files/patch-setup.py
+++ www/py-requests-cache93/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	1970-01-01 00:00:00 UTC
+--- setup.py.orig	2025-10-09 14:35:58 UTC
 +++ setup.py
-@@ -12,7 +12,7 @@ package_data = \
+@@ -12,11 +12,11 @@ install_requires = \
  
  install_requires = \
  ['appdirs>=1.4.4,<2.0.0',
@@ -9,3 +9,8 @@
   'cattrs>=1.8,<23.3',
   'requests>=2.22,<3.0',
   'url-normalize>=1.4,<2.0',
+- 'urllib3>=1.25.5,<2.0.0']
++ 'urllib3>=1.25.5,<3.0.0']
+ 
+ extras_require = \
+ {'all': ['boto3>=1.15,<2.0',
