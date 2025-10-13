5f2b41c551fc5b3f9c0cd8e5876c652ed6c36c0b
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-10-13 14:19:05 +0800

    textproc/py-elasticsearch7: Relax version requirement of RUN_DEPENDS
    - Bump PORTREVISION for package change

---
+++ textproc/py-elasticsearch7/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2024-09-19 07:26:17 UTC
++++ setup.py
+@@ -52,7 +52,7 @@ install_requires = [
+ ]
+ 
+ install_requires = [
+-    "urllib3>=1.21.1, <2",
++    "urllib3>=1.21.1, <3",
+     "certifi",
+ ]
+ tests_require = [
