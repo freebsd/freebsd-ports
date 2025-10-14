cc6ab61181b527d1faeceda982ec36b8e4a96c86
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2025-10-14 17:52:25 +0200

    databases/py-berkeleydb: Update version 18.1.14=>18.1.15
    Changelog: https://hg.jcea.es/pybsddb/file/tip/docs/changelog.rst

--- databases/py-berkeleydb/files/patch-setup3.py
+++ databases/py-berkeleydb/files/patch-setup3.py
@@ -1,15 +1,15 @@
---- setup3.py.orig	2024-10-31 03:59:08 UTC
+--- setup3.py.orig	2025-10-12 02:12:47 UTC
 +++ setup3.py
-@@ -41,7 +41,7 @@ os.environ['SETUPTOOLS_USE_DISTUTILS'] = 'local'
- # See https://setuptools.readthedocs.io/en/latest/deprecated/\
- #                                      distutils-legacy.html
- os.environ['SETUPTOOLS_USE_DISTUTILS'] = 'local'
+@@ -39,7 +39,7 @@ import platform
+ import platform
+ 
+ 
 -from setuptools import setup, Extension
 +from distutils.core import setup, Extension
  import distutils.ccompiler
  
  # read the module version number out of the .c file
-@@ -260,8 +260,8 @@ if os.name == 'posix':
+@@ -282,8 +282,8 @@ if os.name == 'posix':
          incdir = os.path.join(BERKELEYDB_DIR, 'include')
      if not libdir:
          libdir = os.path.join(BERKELEYDB_DIR, 'lib')
@@ -20,7 +20,16 @@
      else:
          if debug:
              print("LIBS already contains '-ldb' not adding our own", "'-l"+dblib+"'")
-@@ -270,7 +270,7 @@ if os.name == 'posix':
+@@ -292,7 +292,7 @@ if os.name == 'posix':
+ 
+     # read db.h to figure out what version of Oracle Berkeley DB this is
+     ver = None
+-    with open(os.path.join(incdir, 'db.h'), 'r') as f :
++    with open('%%BDB_INCLUDE_DIR%%/db.h', 'r') as f :
+         db_h_lines = f.readlines()
+     db_ver_re = re.compile(
+         r'^#define\s+DB_VERSION_STRING\s.*Berkeley DB (\d+\.\d+).*')
+@@ -338,7 +338,7 @@ elif os.name == 'nt':
  
      # read db.h to figure out what version of Oracle Berkeley DB this is
      ver = None
