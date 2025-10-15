6af3053952c267f65c4c038b8bdcd1f8566358dd
Yuri Victorovich <yuri@FreeBSD.org>
2025-10-14 20:05:42 -0700

    science/pcmsolver: Fix build
    Fixes:
    - old bundled eigen version wasn't compatible
    - errors due to c++11 were resolved using c++14
    
    Reported by:	fallout

---
+++ science/pcmsolver/files/patch-cmake_custom_compilers_CXXFlags.cmake
@@ -0,0 +1,8 @@
+--- cmake/custom/compilers/CXXFlags.cmake.orig	2025-10-15 01:10:00 UTC
++++ cmake/custom/compilers/CXXFlags.cmake
+@@ -1,4 +1,4 @@
+-set(CMAKE_CXX_STANDARD 11)
++set(CMAKE_CXX_STANDARD 14)
+ set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
+ set(CMAKE_CXX_EXTENSIONS FALSE)
+ 
