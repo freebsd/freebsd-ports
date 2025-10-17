35c42348dc38076cd767610800dbd7d2baa19b76
Robert Clausecker <fuz@FreeBSD.org>
2025-10-17 17:49:12 +0200

    graphics/gimp-app: fpsetmask() -> fedisableexcept()
    Replace the use of the older fpsetmask() call with fedisableexcept().
    The build previously only succeeded on armv7 due to ANSI C semantics
    in which it is permitted to call an undlecared function.
    
    Approved by:	portmgr (build fix blanket)
    MFH:		2025Q4

--- graphics/gimp-app/files/patch-libgimp_gimp.c
+++ graphics/gimp-app/files/patch-libgimp_gimp.c
@@ -1,22 +1,22 @@
 Why is this patch needed?
 
---- libgimp/gimp.c.orig	2023-11-05 23:49:05 UTC
+--- libgimp/gimp.c.orig	2025-05-17 22:51:03 UTC
 +++ libgimp/gimp.c
 @@ -28,6 +28,7 @@
  #include <stdlib.h>
  #include <string.h>
  #include <sys/types.h>
-+#include <floatingpoint.h>
++#include <fenv.h>
  
  #ifdef HAVE_SYS_TIME_H
  #include <sys/time.h>
-@@ -450,6 +451,9 @@ gimp_main (const GimpPlugInInfo *info,
+@@ -348,6 +349,9 @@ gimp_main (GType  plug_in_type,
    gimp_env_init (TRUE);
  
    progname = argv[ARG_PROGNAME];
 +
 +  /* Ignore floating point exceptions */
-+  fpsetmask(0);
++  fedisableexcept(FE_ALL_EXCEPT);
  
    basename = g_path_get_basename (progname);
  
