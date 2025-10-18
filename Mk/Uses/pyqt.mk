8b18d4129ddd9c84f9fcdd848809aa03cd693782
Jason E. Hale <jhale@FreeBSD.org>
2025-10-17 21:33:53 -0400

    devel/py-qtbuilder: Update to 1.19.0
    https://pyqt-builder.readthedocs.io/en/stable/releases.html#v1-19-0

--- Mk/Uses/pyqt.mk
+++ Mk/Uses/pyqt.mk
@@ -108,7 +108,7 @@ MASTER_SITES_PYQTWEBENGINE=	${MASTER_SITES_PYQT${_PYQT_VERSION}WEBENGINE}
 # below, should have a suitable epoch appended to the version.
 
 # Qt version-agnostic components
-PYQTBUILDER_VERSION=		1.18.2
+PYQTBUILDER_VERSION=		1.19.0
 QSCI2_VERSION=			2.14.1
 SIP_VERSION=			6.13.1	# ,1
 
