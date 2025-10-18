da0f8f33a857c7b031323aacb74afcecc124409d
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-10-19 07:54:12 +0800

    security/py-m2crypto: Update to 0.46.0
    Changes:	https://gitlab.com/m2crypto/m2crypto/-/tags
    		https://gitlab.com/m2crypto/m2crypto/-/blob/master/CHANGES

--- security/py-m2crypto/files/patch-setup.py
+++ security/py-m2crypto/files/patch-setup.py
@@ -1,35 +1,31 @@
---- setup.py.orig	2024-10-09 22:15:33 UTC
+--- setup.py.orig	2025-10-01 23:07:58 UTC
 +++ setup.py
-@@ -164,11 +164,6 @@ class _M2CryptoBuildExt(build_ext.build_ext):
-         log.debug('self.openssl_path = %s', self.openssl_path)
-         log.debug('self.bundledlls = %s', self.bundledlls)
+@@ -168,9 +168,6 @@ class _M2CryptoBuildExt(build_ext.build_ext):
+         log.debug("self.openssl_path = %s", self.openssl_path)
+         log.debug("self.bundledlls = %s", self.bundledlls)
  
 -        # swig seems to need the default header file directories
--        self.swig_opts.extend(
--            ['-I%s' % i for i in _get_additional_includes()]
--        )
+-        self.swig_opts.extend(["-I%s" % i for i in _get_additional_includes()])
 -
-         log.debug('self.include_dirs = %s', self.include_dirs)
-         log.debug('self.library_dirs = %s', self.library_dirs)
+         log.debug("self.include_dirs = %s", self.include_dirs)
+         log.debug("self.library_dirs = %s", self.library_dirs)
  
-@@ -238,6 +233,11 @@ class _M2CryptoBuildExt(build_ext.build_ext):
+@@ -237,6 +234,9 @@ class _M2CryptoBuildExt(build_ext.build_ext):
                          f"gcc -E ended with return code {pid.returncode}"
                      )
  
 +        # swig seems to need the default header file directories
-+        self.swig_opts.extend(
-+            ['-I%s' % i for i in _get_additional_includes()]
-+        )
++        self.swig_opts.extend(["-I%s" % i for i in _get_additional_includes()])
 +
          # Some Linux distributor has added the following line in
          # /usr/include/openssl/opensslconf.h:
          #
 @@ -257,7 +257,7 @@ class _M2CryptoBuildExt(build_ext.build_ext):
-                 '-I' + os.path.join(openssl_include_dir, 'openssl')
-             )
+         if platform.system() == "Darwin":
+             self.swig_opts.append("-cpperraswarn")
  
--        self.swig_opts.append('-includeall')
-+        #self.swig_opts.append('-includeall')
-         self.swig_opts.append('-builtin')
+-        self.swig_opts.append("-includeall")
++        #self.swig_opts.append("-includeall")
+         self.swig_opts.append("-builtin")
  
-         build_dir = os.path.join(self.build_lib, 'M2Crypto')
+         build_dir = os.path.join(self.build_lib, "M2Crypto")
