07206270cd127322228e27d68a9537521b250ae6
Mathieu Arnold <mat@FreeBSD.org>
2025-10-22 16:21:37 +0200

    dns/bind9-devel: update to 9.21.14
    Changes:	https://downloads.isc.org/isc/bind9/9.21.14/doc/arm/html/notes.html
    (cherry picked from commit 072263106b4dd95b6a19bd33a234bb5fa619b426)

--- dns/bind9-devel/files/patch-bin_named_include_named_globals.h
+++ dns/bind9-devel/files/patch-bin_named_include_named_globals.h
@@ -1,8 +1,8 @@
 We reference the pid file as being run/named/pid everywere else.
 
---- bin/named/include/named/globals.h.orig	2025-08-13 17:10:38 UTC
+--- bin/named/include/named/globals.h.orig	2025-10-18 09:48:52 UTC
 +++ bin/named/include/named/globals.h
-@@ -113,7 +113,7 @@ EXTERN const char *named_g_defaultpidfile INIT(NAMED_L
+@@ -112,7 +112,7 @@ EXTERN const char *named_g_defaultpidfile INIT(NAMED_L
  
  #if NAMED_RUN_PID_DIR
  EXTERN const char *named_g_defaultpidfile INIT(NAMED_LOCALSTATEDIR "/run/named/"
