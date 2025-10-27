a50f8b26efb19185e02731837b432387cbdec7a2
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-10-27 23:59:40 +0800

    databases/py-dbt-core: Remove unneeded py-cffi from RUN_DEPENDS
    - Bump PORTREVISION for dependency change
    
    from setup.py:
            # Match snowflake-connector-python, to ensure compatibility in dbt-snowflake
            "cffi>=1.9,<2.0.0",
    
    It is needed for upcoming py-cffi 2.0.0 update. This port does not uses cffi but
    list it to ensure compatibility. As of Oct 2025, databases/py-dbt-snowflake and
    databases/py-snowflake-connector-python do not use py-cffi.

--- databases/py-dbt-core/files/patch-setup.py
+++ databases/py-dbt-core/files/patch-setup.py
@@ -12,3 +12,11 @@
          # ----
          # These are major-version-0 packages also maintained by dbt-labs. Accept patches.
          "dbt-extractor~=0.5.0",
+@@ -85,7 +85,6 @@ setup(
+         "typing-extensions>=3.7.4",
+         # ----
+         # Match snowflake-connector-python, to ensure compatibility in dbt-snowflake
+-        "cffi>=1.9,<2.0.0",
+         "idna>=2.5,<4",
+         "requests<3.0.0",
+         "urllib3~=1.0",
