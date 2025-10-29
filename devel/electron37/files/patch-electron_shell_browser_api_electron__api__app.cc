e257686c2554282de76472e852a04e4487ac4ea7
Hiroki Tagato <tagattie@FreeBSD.org>
2025-10-29 22:53:55 +0900

    devel/electron37: Update to 37.8.0
    Changelog: https://github.com/electron/electron/releases/tag/v37.8.0
    
    Reported by:	GitHub (watch releases)

--- devel/electron37/files/patch-electron_shell_browser_api_electron__api__app.cc
+++ devel/electron37/files/patch-electron_shell_browser_api_electron__api__app.cc
@@ -1,4 +1,4 @@
---- electron/shell/browser/api/electron_api_app.cc.orig	2025-08-13 14:21:20 UTC
+--- electron/shell/browser/api/electron_api_app.cc.orig	2025-10-28 08:06:59 UTC
 +++ electron/shell/browser/api/electron_api_app.cc
 @@ -96,7 +96,7 @@
  #include "shell/common/process_util.h"
@@ -45,7 +45,7 @@
    // Read the xdg-activation token and set it in the command line for the
    // duration of the notification in order to ensure this is propagated to an
    // already running electron app instance if it exists.
-@@ -1354,7 +1354,7 @@ std::vector<gin_helper::Dictionary> App::GetAppMetrics
+@@ -1434,7 +1434,7 @@ std::vector<gin_helper::Dictionary> App::GetAppMetrics
        pid_dict.Set("name", process_metric.second->name);
      }
  
@@ -54,7 +54,7 @@
      auto memory_info = process_metric.second->GetMemoryInfo();
  
      auto memory_dict = gin_helper::Dictionary::CreateEmpty(isolate);
-@@ -1738,7 +1738,7 @@ gin::ObjectTemplateBuilder App::GetObjectTemplateBuild
+@@ -1818,7 +1818,7 @@ gin::ObjectTemplateBuilder App::GetObjectTemplateBuild
        .SetMethod(
            "removeAsDefaultProtocolClient",
            base::BindRepeating(&Browser::RemoveAsDefaultProtocolClient, browser))
@@ -63,7 +63,7 @@
        .SetMethod(
            "getApplicationInfoForProtocol",
            base::BindRepeating(&Browser::GetApplicationInfoForProtocol, browser))
-@@ -1796,7 +1796,7 @@ gin::ObjectTemplateBuilder App::GetObjectTemplateBuild
+@@ -1876,7 +1876,7 @@ gin::ObjectTemplateBuilder App::GetObjectTemplateBuild
        .SetMethod("getJumpListSettings", &App::GetJumpListSettings)
        .SetMethod("setJumpList", &App::SetJumpList)
  #endif
--- devel/electron37/files/patch-electron_shell_common_node__bindings.cc
+++ devel/electron37/files/patch-electron_shell_common_node__bindings.cc
@@ -1,6 +1,6 @@
---- electron/shell/common/node_bindings.cc.orig	2025-08-13 14:21:20 UTC
+--- electron/shell/common/node_bindings.cc.orig	2025-10-28 08:06:59 UTC
 +++ electron/shell/common/node_bindings.cc
-@@ -47,7 +47,7 @@
+@@ -50,7 +50,7 @@
  #include "third_party/electron_node/src/debug_utils.h"
  #include "third_party/electron_node/src/module_wrap.h"
  
@@ -9,7 +9,7 @@
  #include "shell/common/crash_keys.h"
  #endif
  
-@@ -162,7 +162,7 @@ void V8FatalErrorCallback(const char* location, const 
+@@ -165,7 +165,7 @@ void V8FatalErrorCallback(const char* location, const 
  void V8FatalErrorCallback(const char* location, const char* message) {
    LOG(ERROR) << "Fatal error in V8: " << location << " " << message;
  
@@ -18,7 +18,7 @@
    electron::crash_keys::SetCrashKey("electron.v8-fatal.message", message);
    electron::crash_keys::SetCrashKey("electron.v8-fatal.location", location);
  #endif
-@@ -184,7 +184,7 @@ void V8OOMErrorCallback(const char* location, const v8
+@@ -187,7 +187,7 @@ void V8OOMErrorCallback(const char* location, const v8
      LOG(ERROR) << "OOM detail: " << details.detail;
    }
  
@@ -27,7 +27,7 @@
    electron::crash_keys::SetCrashKey("electron.v8-oom.is_heap_oom",
                                      std::to_string(details.is_heap_oom));
    if (location) {
-@@ -580,7 +580,7 @@ void NodeBindings::Initialize(v8::Local<v8::Context> c
+@@ -627,7 +627,7 @@ void NodeBindings::Initialize(v8::Local<v8::Context> c
    TRACE_EVENT0("electron", "NodeBindings::Initialize");
    // Open node's error reporting system for browser process.
  
