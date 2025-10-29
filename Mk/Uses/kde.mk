34a52ba8303189d6336954aed54ccc0998f4aff1
Max Brazhnikov <makc@FreeBSD.org>
2025-10-29 21:57:39 +0300

    KDE: Update KDE Plasma to 6.5.0
    Announcement: https://kde.org/announcements/plasma/6/6.5.0/
    
    Caution!!
    This update breaks Plasma Wayland for FreeBSD users. Do not upgrade
    (or switch to the quarterly packages) if you prefer Wayland session.
    
    Ports changes:
    
    Add new Plasma port:
     - sysutils/plasma6-knighttime: helpers for scheduling the dark-light cycle.
    
    kde.mk:
     - Add knighttime to the Plasma components
     - Update dependency entry for milou component
    
    audio/plasma6-kpipewire:
     - Remove stale patch
    
    security/plasma6-ksshaskpass:
     - Depend on KF6Doctools to generate man page
    
    sysutils/plasma6-systemsettings:
     - Drop unused dependency
    
    x11/plasma6-plasma-workspace:
     - Drop unused dependency
     - Remove stale patch
    
    x11-toolkits/plasma6-kdeplasma-addons:
     - Demote plasma5support to a runtime dependency
    
    x11-wm/plasma6-kwin:
     - Fix patch to locate hwdata
     - Xwayland is a runtime dependency
     - Drop unused dependencies
    
    x11-wm/plasma6-kwin-x11:
     - Fix patch to locate hwdata

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -91,7 +91,7 @@ KDE_PLASMA5_VERSION?=		5.27.12
 KDE_PLASMA5_BRANCH?=		stable
 
 # Current KDE Plasma desktop.
-KDE_PLASMA6_VERSION?=		6.4.5
+KDE_PLASMA6_VERSION?=		6.5.0
 KDE_PLASMA6_BRANCH?=		stable
 
 # Legacy KDE frameworks (Qt5 based).
@@ -317,6 +317,7 @@ _USE_PLASMA6_ALL=	activities activities-stats activitymanagerd \
 			aurorae breeze breeze-gtk decoration discover \
 			globalacceld infocenter kde-cli-tools \
 			kde-gtk-config kdeplasma-addons kgamma kmenuedit \
+			knighttime \
 			kpipewire kscreen kscreenlocker ksshaskpass \
 			ksystemstats kwallet-pam kwin kwin-x11 kwrited \
 			layer-shell-qt libkscreen libksysguard libplasma \
@@ -704,6 +705,9 @@ kde-kgamma_PATH=		${QT_PLUGINDIR}/plasma/kcms/systemsettings_qwidgets/kcm_kgamma
 kde-kmenuedit_PORT=		sysutils/plasma${_KDE_VERSION}-kmenuedit
 kde-kmenuedit_PATH=		${KDE_PREFIX}/bin/kmenuedit
 
+kde-knighttime_PORT=		sysutils/plasma${_KDE_VERSION}-knighttime
+kde-knighttime_LIB=		libKNightTime.so
+
 kde-kscreen_PORT=		x11/plasma${_KDE_VERSION}-kscreen
 kde-kscreen_PATH=		${KDE_PREFIX}/bin/kscreen-console
 
@@ -737,7 +741,7 @@ kde-libksysguard_LIB6=		libKSysGuardSystemStats.so
 kde-libksysguard_LIB=		${kde-libksysguard_LIB${_KDE_VERSION}}
 
 kde-milou_PORT=			deskutils/plasma${_KDE_VERSION}-milou
-kde-milou_PATH=		${QT_QMLDIR}/org/kde/milou/libmilouqmlplugin.so
+kde-milou_PATH=		${QT_QMLDIR}/org/kde/milou/libmilou.so
 
 kde-ocean-sound-theme_PORT=	audio/plasma${_KDE_VERSION}-ocean-sound-theme
 kde-ocean-sound-theme_PATH=	${KDE_PREFIX}/share/sounds/ocean/index.theme
