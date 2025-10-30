2b2588097ab2bb4256ada0066cc041e182452e22
Li-Wen Hsu <lwhsu@FreeBSD.org>
2025-10-30 17:23:22 +0800

    net/vncreflector: Fix a buffer overrun issue
    While here, regen the patches and add license information
    
    PR:		290129
    Reported by:	Chun Cheng Yeh <leafoliages@gmail.com>

--- net/vncreflector/files/patch-logging.c
+++ net/vncreflector/files/patch-logging.c
@@ -1,6 +1,6 @@
---- logging.c
+--- logging.c.orig	2003-04-21 17:20:35 UTC
 +++ logging.c
-@@ -138,8 +138,6 @@
+@@ -138,8 +138,6 @@ void log_write(int level, char *format, ...)
    char time_buf[32];
    char level_char = ' ';
  
@@ -9,7 +9,7 @@
    if ( (log_fp != NULL && level <= log_file_level) ||
         level <= log_stderr_level ) {
      now = time(NULL);
-@@ -150,18 +148,21 @@
+@@ -150,18 +148,21 @@ void log_write(int level, char *format, ...)
  
      if (level <= log_file_level) {
        fprintf(log_fp, "%s %c ", time_buf, (int)level_char);
--- net/vncreflector/files/patch-main.c
+++ net/vncreflector/files/patch-main.c
@@ -1,6 +1,6 @@
---- main.c.orig
+--- main.c.orig	2003-01-11 09:44:02 UTC
 +++ main.c
-@@ -464,7 +464,7 @@
+@@ -464,7 +464,7 @@ static int init_screen_info(void)
  
  static int init_screen_info(void)
  {
