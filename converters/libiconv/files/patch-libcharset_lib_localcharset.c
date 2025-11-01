669ebf337d1bc0c4651b771563684f20a1d44a5c
Gleb Popov <arrowd@FreeBSD.org>
2025-11-01 23:48:14 +0300

    converters/libiconv: Update to 1.18
    PR:	290747
    Exp-run by: antoine

--- converters/libiconv/files/patch-libcharset_lib_localcharset.c
+++ converters/libiconv/files/patch-libcharset_lib_localcharset.c
@@ -1,6 +1,6 @@
---- libcharset/lib/localcharset.c.orig	2020-05-09 08:59:22 UTC
+--- libcharset/lib/localcharset.c.orig	2020-04-04 12:50:22 UTC
 +++ libcharset/lib/localcharset.c
-@@ -92,6 +92,7 @@ static const struct table_entry alias_table[] =
+@@ -95,6 +95,7 @@ static const struct table_entry alias_table[] =
  #  if defined __FreeBSD__                                   /* FreeBSD */
    /*{ "ARMSCII-8",  "ARMSCII-8" },*/
      { "Big5",       "BIG5" },
@@ -8,7 +8,7 @@
      { "C",          "ASCII" },
    /*{ "CP1131",     "CP1131" },*/
    /*{ "CP1251",     "CP1251" },*/
-@@ -104,6 +105,7 @@ static const struct table_entry alias_table[] =
+@@ -107,6 +108,7 @@ static const struct table_entry alias_table[] =
      { "ISO8859-13", "ISO-8859-13" },
      { "ISO8859-15", "ISO-8859-15" },
      { "ISO8859-2",  "ISO-8859-2" },
