106c0d36abfccb3d6d89785a10deb4d54f525113
Älven <alven@FreeBSD.org>
2025-11-01 07:06:44 +0400

    comms/direwolf: Update 1.7 => 1.8
    Changelogs:
    * https://github.com/wb2osz/direwolf/releases/tag/1.8
    * https://github.com/wb2osz/direwolf/compare/1.7...1.8
    
    * Make DOCS option really work
    * Pet portclippy(1) and portfmt(1)
    
    Approved by:		hamradio@ (alven@, yuri@)
    Approved by:		yuri@ (Mentor)
    Differential Revision:	https://reviews.freebsd.org/D53511

--- comms/direwolf/files/patch-scripts_dw-start.sh
+++ comms/direwolf/files/patch-scripts_dw-start.sh
@@ -1,12 +1,11 @@
 --- scripts/dw-start.sh.orig	2022-01-28 14:55:02 UTC
 +++ scripts/dw-start.sh
-@@ -68,9 +68,9 @@ DWCMD="$DIREWOLF -a 100"
+@@ -68,9 +68,8 @@ DWCMD="$DIREWOLF -a 100"
  
  
  #Where will logs go - needs to be writable by non-root users
 -LOGFILE=/var/tmp/dw-start.log
-+#LOGFILE=/var/tmp/dw-start.log
-+LOGFILE=/var/var/dw-start.log
++LOGFILE=/var/log/dw-start.log
  
 -
  #-------------------------------------
