eb4b9b42b549e19acdb77a57563d734ccbc5dd09
Daniel Engberg <diizzy@FreeBSD.org>
2025-11-01 14:12:05 +0100

    net-im/tdlib-purple: Remove port
    Marked broken in May 2025 and dead upstream
    
    PR:		278263

--- net-im/tdlib-purple/files/patch-format.cpp
+++
@@ -1,8 +0,0 @@
---- format.cpp	2021-12-17 04:50:38.000000000 -0500
-+++ format.cpp	2023-12-18 01:10:41.084981000 -0500
-@@ -1,4 +1,4 @@
- #include "format.h"
--#include <fmt/format.h>
-+#include <fmt/args.h>
- 
- std::string formatMessage(const char *fmt, std::initializer_list<std::string> args)
