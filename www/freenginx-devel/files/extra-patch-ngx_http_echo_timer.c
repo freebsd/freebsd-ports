09355a84834e733a48042f825a8c760fd9c3c477
Sergey A. Osokin <osa@FreeBSD.org>
2025-11-01 22:11:07 -0400

    www/freenginx-devel: enable third-party echo module
    Bump PORTREVISION.
    
    Sponsored by:	tipi.work

--- www/freenginx-devel/files/extra-patch-ngx_http_echo_timer.c
+++ www/freenginx-devel/files/extra-patch-ngx_http_echo_timer.c
@@ -1,14 +1,23 @@
---- ../echo-nginx-module-4eeda3c/src/ngx_http_echo_timer.c.orig	2025-10-21 16:47:54.085370000 -0400
-+++ ../echo-nginx-module-4eeda3c/src/ngx_http_echo_timer.c	2025-10-21 16:50:55.878182000 -0400
-@@ -32,9 +32,8 @@
-         ngx_http_set_ctx(r, ctx, ngx_http_echo_module);
+--- ../echo-nginx-module-4eeda3c/src/ngx_http_echo_timer.c.orig	2024-08-17 08:36:17.000000000 -0400
++++ ../echo-nginx-module-4eeda3c/src/ngx_http_echo_timer.c	2025-11-01 22:07:05.628904000 -0400
+@@ -33,8 +33,20 @@
      }
  
--    if (ctx->timer_begin.sec == 0) {
--        ctx->timer_begin.sec  = r->start_sec;
--        ctx->timer_begin.msec = (ngx_msec_t) r->start_msec;
-+    if (ctx->timer_begin.time == 0) {
-+        ctx->timer_begin.time  = r->start_time;
+     if (ctx->timer_begin.sec == 0) {
++#if defined freenginx
++        tp = ngx_timeofday();
++
++        tp->sec -= (ngx_current_msec - r->start_time) / 1000;
++        tp->msec -= (ngx_current_msec - r->start_time) % 1000;
++        if (tp->msec > NGX_MAX_INT_T_VALUE) {
++            tp->msec += 1000;
++            tp->sec -= 1;
++        }
++        ctx->timer_begin = *tp;
++#else
+         ctx->timer_begin.sec  = r->start_sec;
+         ctx->timer_begin.msec = (ngx_msec_t) r->start_msec;
++#endif
      }
  
      /* force the ngx timer to update */
