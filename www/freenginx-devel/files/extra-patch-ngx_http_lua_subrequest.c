e2ac006b4a32ed84fb45baa8cf9bf88487a24cb0
Sergey A. Osokin <osa@FreeBSD.org>
2025-11-02 01:08:54 -0400

    www/freenginx-devel: third-party modules management (+)
    *) update lua module: 0.10.28 -> 0.10.29
    *) update stream_lua module: 0.0.16 -> 0.0.17
    
    Bump PORTREVISION.
    
    Sponsored by:	tipi.work

--- www/freenginx-devel/files/extra-patch-ngx_http_lua_subrequest.c
+++ www/freenginx-devel/files/extra-patch-ngx_http_lua_subrequest.c
@@ -1,5 +1,5 @@
---- ../lua-nginx-module-0.10.28/src/ngx_http_lua_subrequest.c.orig	2025-11-01 23:46:33.237622000 -0400
-+++ ../lua-nginx-module-0.10.28/src/ngx_http_lua_subrequest.c	2025-11-01 23:52:42.602198000 -0400
+--- ../lua-nginx-module-0.10.29/src/ngx_http_lua_subrequest.c.orig	2025-11-01 23:46:33.237622000 -0400
++++ ../lua-nginx-module-0.10.29/src/ngx_http_lua_subrequest.c	2025-11-01 23:52:42.602198000 -0400
 @@ -1373,7 +1373,9 @@
      ngx_str_t *uri, ngx_str_t *args, ngx_http_request_t **psr,
      ngx_http_post_subrequest_t *ps, ngx_uint_t flags)
--- www/freenginx-devel/files/extra-patch-ngx_http_lua_time.c
+++ www/freenginx-devel/files/extra-patch-ngx_http_lua_time.c
@@ -1,5 +1,5 @@
---- ../lua-nginx-module-0.10.28/src/ngx_http_lua_time.c.orig	2025-01-17 00:24:35.000000000 -0500
-+++ ../lua-nginx-module-0.10.28/src/ngx_http_lua_time.c	2025-11-02 00:11:45.551096000 -0400
+--- ../lua-nginx-module-0.10.29/src/ngx_http_lua_time.c.orig	2025-01-17 00:24:35.000000000 -0500
++++ ../lua-nginx-module-0.10.29/src/ngx_http_lua_time.c	2025-11-02 00:11:45.551096000 -0400
 @@ -28,7 +28,21 @@
  double
  ngx_http_lua_ffi_req_start_time(ngx_http_request_t *r)
