2aff6e57996eb706bfde954af018f10893f26b75
Sergey A. Osokin <osa@FreeBSD.org>
2025-11-05 09:55:46 -0500

    lang/quickjs: update to the recent 2025.09.13 release, rev.20251018
    PR:		290691
    Approved by:	yuri (maintainer)
    Sponsored by:	tipi.work

--- lang/quickjs/files/patch-qjsc.c
+++ lang/quickjs/files/patch-qjsc.c
@@ -1,6 +1,6 @@
---- qjsc.c.orig	2024-07-27 21:40:48 UTC
+--- qjsc.c.orig	2025-10-18 10:04:12 UTC
 +++ qjsc.c
-@@ -445,7 +445,7 @@ static int output_executable(const char *out_filename,
+@@ -498,7 +498,7 @@ static int output_executable(const char *out_filename,
      if (dynamic_export)
          *arg++ = "-rdynamic";
      *arg++ = cfilename;
