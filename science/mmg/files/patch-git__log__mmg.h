ab637a8dfd5d4b26b8671f9742576003face6ad0
Älven <alven@FreeBSD.org>
2025-11-05 04:51:38 +0400

    science/mmg: Update 5.7.3 => 5.8.0, take maintainership
    Changelogs:
    * https://github.com/MmgTools/mmg/releases/tag/v5.8.0
    * https://github.com/MmgTools/mmg/compare/v5.7.3...v5.8.0
    
    * Add WWW for source code repository
    * Clarify LICENSE and pkg-descr
    * Pet portclippy(1) and portfmt(1)
    * Bash is still needed for running some configure script -
      scripts/git_log_mmg.sh, so is patching it with shebangfix
    
    Approved by:		yuri@ (Mentor)
    Differential Revision:	https://reviews.freebsd.org/D53593

--- science/mmg/files/patch-git__log__mmg.h
+++
@@ -1,9 +0,0 @@
---- git_log_mmg.h.orig	2024-06-23 16:47:08 UTC
-+++ git_log_mmg.h
-@@ -0,0 +1,6 @@
-+#ifndef _GIT_LOG_MMG_H
-+#define _GIT_LOG_MMG_H
-+#define MMG_GIT_BRANCH "No git branch found"
-+#define MMG_GIT_COMMIT "No git commit found"
-+#define MMG_GIT_DATE   "No git commit found"
-+#endif
