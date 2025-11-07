0e6c14eeaa4184b06e46c141eaa99cdbb7f2e800
Hiroki Tagato <tagattie@FreeBSD.org>
2025-11-07 16:54:29 +0900

    misc/codex: Update to 0.55.0
    Changelog:
    - https://github.com/openai/codex/releases/tag/rust-v0.54.0
    - https://github.com/openai/codex/releases/tag/rust-v0.55.0
    
    Reported by:	GitHub (watch releases)

--- misc/codex/files/patch-codex-rs_core_tests_suite_shell__serialization.rs
+++ misc/codex/files/patch-codex-rs_core_tests_suite_shell__serialization.rs
@@ -1,6 +1,6 @@
---- codex-rs/core/tests/suite/shell_serialization.rs.orig	2025-10-25 21:16:34 UTC
+--- codex-rs/core/tests/suite/shell_serialization.rs.orig	2025-11-04 20:00:53 UTC
 +++ codex-rs/core/tests/suite/shell_serialization.rs
-@@ -238,6 +238,9 @@ async fn shell_output_for_freeform_tool_records_durati
+@@ -398,6 +398,9 @@ async fn shell_output_for_freeform_tool_records_durati
      #[cfg(target_os = "linux")]
      let sleep_cmd = vec!["/bin/bash", "-c", "sleep 1"];
  
