bc01cbb5a165d208abe4e9f73e85d7c5a68ed4f9
Jason E. Hale <jhale@FreeBSD.org>
2025-11-07 12:28:02 -0500

    Qt5: Update to 5.15.18
    This is the penultimate release of Qt5. While Qt5 is EOL, due to the
    LGPL releases being a year behind the commercial releases, there will
    still be one more patch release in May 2026.
    
    PySide2: Update to 5.15.18
    
    Announcement:
    https://lists.qt-project.org/pipermail/announce/2025-October/000592.html
    Release notes:
    https://code.qt.io/cgit/qt/qtreleasenotes.git/tree/qt/5.15.18/release-note.md
    
    MFH:		2025Q4

--- Mk/Uses/qt.mk
+++ Mk/Uses/qt.mk
@@ -22,7 +22,7 @@ _QT_MK_INCLUDED=	qt.mk
 
 # Qt versions currently supported by the framework.
 _QT_SUPPORTED?=		5 6
-QT5_VERSION?=		5.15.17
+QT5_VERSION?=		5.15.18
 QT6_VERSION?=		6.9.3
 PYSIDE6_VERSION?=	6.9.3
 
