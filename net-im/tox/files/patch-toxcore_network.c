e400243cd96b1c10dd14ab644e9d455c66be5b66
Vladimir Druzenko <vvd@FreeBSD.org>
2025-11-07 16:25:12 +0300

    net-im/tox: Update 0.2.20 => 0.2.21, take maitainership
    Changelog:
    https://github.com/TokTok/c-toxcore/releases/tag/v0.2.21
    
    While here fix warnings from portclippy.
    
    PR:		290838
    Co-authored-by:	Eric Camachat <eric@camachat.org>
    MFH:		2025Q4
    (cherry picked from commit 20d9bb64cd15a73a54846b0b1f01a50adfd5c79c)

--- net-im/tox/files/patch-toxcore_network.c
+++ net-im/tox/files/patch-toxcore_network.c
@@ -1,16 +1,16 @@
---- toxcore/network.c.orig	2022-04-18 18:12:07 UTC
+--- toxcore/network.c.orig	2025-10-06 10:37:58 UTC
 +++ toxcore/network.c
-@@ -1063,6 +1063,7 @@ Networking_Core *new_networking_ex(
+@@ -1318,6 +1318,7 @@ Networking_Core *new_networking_ex(
       */
      int n = 1024 * 1024 * 2;
  
 +#if !defined(__FreeBSD__)
      if (net_setsockopt(ns, temp->sock, SOL_SOCKET, SO_RCVBUF, &n, sizeof(n)) != 0) {
-         LOGGER_ERROR(log, "failed to set socket option %d", SO_RCVBUF);
+         LOGGER_WARNING(log, "failed to set socket option %d", SO_RCVBUF);
      }
-@@ -1070,6 +1071,7 @@ Networking_Core *new_networking_ex(
+@@ -1325,6 +1326,7 @@ Networking_Core *new_networking_ex(
      if (net_setsockopt(ns, temp->sock, SOL_SOCKET, SO_SNDBUF, &n, sizeof(n)) != 0) {
-         LOGGER_ERROR(log, "failed to set socket option %d", SO_SNDBUF);
+         LOGGER_WARNING(log, "failed to set socket option %d", SO_SNDBUF);
      }
 +#endif
  
