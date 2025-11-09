7eb972450e638fabfcb1a46affaadfa700cee941
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-11-10 00:09:24 +0800

    devel/libdatadog: Update to 23.0.0
    Changes:	https://github.com/DataDog/libdatadog/releases

--- devel/libdatadog/files/patch-datadog-crashtracker-src-collector-emitters.rs
+++ devel/libdatadog/files/patch-datadog-crashtracker-src-collector-emitters.rs
@@ -1,6 +1,6 @@
---- datadog-crashtracker/src/collector/emitters.rs.orig	2025-02-11 17:46:31 UTC
+--- datadog-crashtracker/src/collector/emitters.rs.orig	2025-11-03 12:41:37 UTC
 +++ datadog-crashtracker/src/collector/emitters.rs
-@@ -178,7 +178,7 @@ fn emit_proc_self_maps(w: &mut impl Write) -> Result<(
+@@ -198,7 +198,7 @@ fn emit_proc_self_maps(w: &mut impl Write) -> Result<(
      Ok(())
  }
  
@@ -9,15 +9,15 @@
  fn emit_ucontext(w: &mut impl Write, ucontext: *const ucontext_t) -> Result<(), EmitterError> {
      if ucontext.is_null() {
          return Err(EmitterError::NullUcontext);
-@@ -305,6 +305,11 @@ fn extract_rsp(ucontext: *const ucontext_t) -> usize {
-         return (*(*ucontext).uc_mcontext).__ss.__rsp as usize;
+@@ -325,6 +325,11 @@ fn extract_ip(ucontext: *const ucontext_t) -> usize {
+         return (*(*ucontext).uc_mcontext).__ss.__rip as usize;
          #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-         return (*(*ucontext).uc_mcontext).__ss.__sp as usize;
+         return (*(*ucontext).uc_mcontext).__ss.__pc as usize;
 +
 +        #[cfg(all(target_os = "freebsd", target_arch = "x86_64"))]
-+        return (*ucontext).uc_mcontext.mc_rsp as usize;
++        return (*ucontext).uc_mcontext.mc_rip as usize;
 +        #[cfg(all(target_os = "freebsd", target_arch = "aarch64"))]
-+        return (*ucontext).uc_mcontext.mc_gpregs.gp_sp;
++        return (*ucontext).uc_mcontext.mc_gpregs.gp_pc;
  
          #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-         return (*ucontext).uc_mcontext.gregs[libc::REG_RSP as usize] as usize;
+         return (*ucontext).uc_mcontext.gregs[libc::REG_RIP as usize] as usize;
