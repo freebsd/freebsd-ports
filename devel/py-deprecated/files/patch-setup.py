c41887bd706b97a852c95bff0013116c5a667e4a
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-11-10 00:11:16 +0800

    devel/py-deprecated: Update to 1.3.1
    Changes:	https://github.com/laurent-laporte-pro/deprecated/releases
    		https://deprecated.readthedocs.io/en/latest/changelog.html

--- devel/py-deprecated/files/patch-setup.py
+++ devel/py-deprecated/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2025-01-27 10:16:23 UTC
+--- setup.py.orig	2025-10-30 08:17:57 UTC
 +++ setup.py
 @@ -143,7 +143,7 @@ setup(
  from setuptools import setup
@@ -6,6 +6,6 @@
  setup(
 -    name="Deprecated",
 +    name="deprecated",
-     version="1.2.18",
+     version="1.3.1",
      url="https://github.com/laurent-laporte-pro/deprecated",
      project_urls={
