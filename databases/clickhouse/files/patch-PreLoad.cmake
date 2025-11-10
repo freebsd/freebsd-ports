9d9a2637c7a21b25d1d4bac3a5cf95cd048bf188
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2025-11-10 22:17:19 +0100

    databases/clickhouse: Update version 22.9.1.2603=>23.5.2.7
    Changelog: https://github.com/ClickHouse/ClickHouse/releases/tag/v23.5.2.7
    
    Sponsored by:	Nepustil

--- databases/clickhouse/files/patch-PreLoad.cmake
+++ databases/clickhouse/files/patch-PreLoad.cmake
@@ -1,86 +1,33 @@
---- PreLoad.cmake.orig	2025-11-09 11:45:34 UTC
+--- PreLoad.cmake.orig	2023-04-26 21:51:24 UTC
 +++ PreLoad.cmake
-@@ -19,45 +19,49 @@ endif()
+@@ -15,12 +15,17 @@ endif()
      endif ()
  endif()
  
--# Check if environment is polluted.
--if (NOT "$ENV{CFLAGS}" STREQUAL ""
--    OR NOT "$ENV{CXXFLAGS}" STREQUAL ""
--    OR NOT "$ENV{LDFLAGS}" STREQUAL ""
--    OR CMAKE_C_FLAGS OR CMAKE_CXX_FLAGS OR CMAKE_EXE_LINKER_FLAGS OR CMAKE_SHARED_LINKER_FLAGS OR CMAKE_MODULE_LINKER_FLAGS
--    OR CMAKE_C_FLAGS_INIT OR CMAKE_CXX_FLAGS_INIT OR CMAKE_EXE_LINKER_FLAGS_INIT OR CMAKE_SHARED_LINKER_FLAGS_INIT OR CMAKE_MODULE_LINKER_FLAGS_INIT)
 +# Default toolchain - this is needed to avoid dependency on OS files.
 +execute_process(COMMAND uname -s OUTPUT_VARIABLE OS)
 +execute_process(COMMAND uname -m OUTPUT_VARIABLE ARCH)
++
+ # Check if environment is polluted.
+-if (NOT "$ENV{CFLAGS}" STREQUAL ""
++if ((OS MATCHES "Linux") AND (NOT "$ENV{CFLAGS}" STREQUAL ""
+     OR NOT "$ENV{CXXFLAGS}" STREQUAL ""
+     OR NOT "$ENV{LDFLAGS}" STREQUAL ""
+     OR CMAKE_C_FLAGS OR CMAKE_CXX_FLAGS OR CMAKE_EXE_LINKER_FLAGS OR CMAKE_MODULE_LINKER_FLAGS
+-    OR CMAKE_C_FLAGS_INIT OR CMAKE_CXX_FLAGS_INIT OR CMAKE_EXE_LINKER_FLAGS_INIT OR CMAKE_MODULE_LINKER_FLAGS_INIT)
++    OR CMAKE_C_FLAGS_INIT OR CMAKE_CXX_FLAGS_INIT
++    OR CMAKE_EXE_LINKER_FLAGS_INIT OR CMAKE_MODULE_LINKER_FLAGS_INIT))
  
--    # if $ENV
--    message("CFLAGS: $ENV{CFLAGS}")
--    message("CXXFLAGS: $ENV{CXXFLAGS}")
--    message("LDFLAGS: $ENV{LDFLAGS}")
--    # if *_FLAGS
--    message("CMAKE_C_FLAGS: ${CMAKE_C_FLAGS}")
--    message("CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}")
--    message("CMAKE_EXE_LINKER_FLAGS: ${CMAKE_EXE_LINKER_FLAGS}")
--    message("CMAKE_SHARED_LINKER_FLAGS: ${CMAKE_SHARED_LINKER_FLAGS}")
--    message("CMAKE_MODULE_LINKER_FLAGS: ${CMAKE_MODULE_LINKER_FLAGS}")
--    # if *_FLAGS_INIT
--    message("CMAKE_C_FLAGS_INIT: ${CMAKE_C_FLAGS_INIT}")
--    message("CMAKE_CXX_FLAGS_INIT: ${CMAKE_CXX_FLAGS_INIT}")
--    message("CMAKE_EXE_LINKER_FLAGS_INIT: ${CMAKE_EXE_LINKER_FLAGS_INIT}")
--    message("CMAKE_SHARED_LINKER_FLAGS_INIT: ${CMAKE_SHARED_LINKER_FLAGS_INIT}")
--    message("CMAKE_MODULE_LINKER_FLAGS_INIT: ${CMAKE_MODULE_LINKER_FLAGS_INIT}")
-+if (OS MATCHES "FreeBSD")
-+    message(STATUS "Skipping strict CFLAGS check on FreeBSD")
-+else()
-+	# Check if environment is polluted.
-+	if (NOT "$ENV{CFLAGS}" STREQUAL ""
-+    	OR NOT "$ENV{CXXFLAGS}" STREQUAL ""
-+    	OR NOT "$ENV{LDFLAGS}" STREQUAL ""
-+    	OR CMAKE_C_FLAGS OR CMAKE_CXX_FLAGS OR CMAKE_EXE_LINKER_FLAGS OR CMAKE_SHARED_LINKER_FLAGS OR CMAKE_MODULE_LINKER_FLAGS
-+    	OR CMAKE_C_FLAGS_INIT OR CMAKE_CXX_FLAGS_INIT OR CMAKE_EXE_LINKER_FLAGS_INIT OR CMAKE_SHARED_LINKER_FLAGS_INIT OR CMAKE_MODULE_LINKER_FLAGS_INIT)
+     # if $ENV
+     message("CFLAGS: $ENV{CFLAGS}")
+@@ -49,10 +54,6 @@ endif()
  
--    message(FATAL_ERROR "
--        Some of the variables like CFLAGS, CXXFLAGS, LDFLAGS are not empty.
--        It is not possible to build ClickHouse with custom flags.
--        These variables can be set up by previous invocation of some other build tools.
--        You should cleanup these variables and start over again.
-+    	# if $ENV
-+    	message("CFLAGS: $ENV{CFLAGS}")
-+    	message("CXXFLAGS: $ENV{CXXFLAGS}")
-+    	message("LDFLAGS: $ENV{LDFLAGS}")
-+    	# if *_FLAGS
-+    	message("CMAKE_C_FLAGS: ${CMAKE_C_FLAGS}")
-+    	message("CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}")
-+    	message("CMAKE_EXE_LINKER_FLAGS: ${CMAKE_EXE_LINKER_FLAGS}")
-+    	message("CMAKE_SHARED_LINKER_FLAGS: ${CMAKE_SHARED_LINKER_FLAGS}")
-+    	message("CMAKE_MODULE_LINKER_FLAGS: ${CMAKE_MODULE_LINKER_FLAGS}")
-+    	# if *_FLAGS_INIT
-+    	message("CMAKE_C_FLAGS_INIT: ${CMAKE_C_FLAGS_INIT}")
-+    	message("CMAKE_CXX_FLAGS_INIT: ${CMAKE_CXX_FLAGS_INIT}")
-+    	message("CMAKE_EXE_LINKER_FLAGS_INIT: ${CMAKE_EXE_LINKER_FLAGS_INIT}")
-+	    message("CMAKE_SHARED_LINKER_FLAGS_INIT: ${CMAKE_SHARED_LINKER_FLAGS_INIT}")
-+		message("CMAKE_MODULE_LINKER_FLAGS_INIT: ${CMAKE_MODULE_LINKER_FLAGS_INIT}")
- 
--        Run the `env` command to check the details.
--        You will also need to remove the contents of the build directory.
-+		message(FATAL_ERROR "
-+        	Some of the variables like CFLAGS, CXXFLAGS, LDFLAGS are not empty.
-+        	It is not possible to build ClickHouse with custom flags.
-+        	These variables can be set up by previous invocation of some other build tools.
-+        	You should cleanup these variables and start over again.
- 
--        Note: if you don't like this behavior, you can manually edit the cmake files, but please don't complain to developers.")
--endif()
-+        	Run the `env` command to check the details.
-+        	You will also need to remove the contents of the build directory.
- 
+         Note: if you don't like this behavior, you can manually edit the cmake files, but please don't complain to developers.")
+ endif()
+-
 -# Default toolchain - this is needed to avoid dependency on OS files.
 -execute_process(COMMAND uname -s OUTPUT_VARIABLE OS)
 -execute_process(COMMAND uname -m OUTPUT_VARIABLE ARCH)
-+        	Note: if you don't like this behavior, you can manually edit the cmake files, but please don't complain to developers.")
-+	endif()
-+endif()
  
  # By default, prefer clang on Linux
  # But note, that you still may change the compiler with -DCMAKE_C_COMPILER/-DCMAKE_CXX_COMPILER.
--- databases/clickhouse/files/patch-cmake_freebsd_default__libs.cmake
+++ databases/clickhouse/files/patch-cmake_freebsd_default__libs.cmake
@@ -1,20 +1,14 @@
---- cmake/freebsd/default_libs.cmake.orig	2022-01-22 20:43:13 UTC
+--- cmake/freebsd/default_libs.cmake.orig	2023-04-26 21:51:24 UTC
 +++ cmake/freebsd/default_libs.cmake
-@@ -4,13 +4,13 @@ if (NOT COMPILER_CLANG)
-     message (FATAL_ERROR "FreeBSD build is supported only for Clang")
- endif ()
+@@ -1,12 +1,6 @@ set (DEFAULT_LIBS "-nodefaultlibs")
+ set (DEFAULT_LIBS "-nodefaultlibs")
  
 -if (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "amd64")
 -    execute_process (COMMAND ${CMAKE_CXX_COMPILER} --print-file-name=libclang_rt.builtins-x86_64.a OUTPUT_VARIABLE BUILTINS_LIBRARY OUTPUT_STRIP_TRAILING_WHITESPACE)
 -else ()
 -    execute_process (COMMAND ${CMAKE_CXX_COMPILER} --print-file-name=libclang_rt.builtins-${CMAKE_SYSTEM_PROCESSOR}.a OUTPUT_VARIABLE BUILTINS_LIBRARY OUTPUT_STRIP_TRAILING_WHITESPACE)
 -endif ()
-+#if (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "amd64")
-+#    execute_process (COMMAND ${CMAKE_CXX_COMPILER} --print-file-name=libclang_rt.builtins-x86_64.a OUTPUT_VARIABLE BUILTINS_LIBRARY OUTPUT_STRIP_TRAILING_WHITESPACE)
-+#else ()
-+#    execute_process (COMMAND ${CMAKE_CXX_COMPILER} --print-file-name=libclang_rt.builtins-${CMAKE_SYSTEM_PROCESSOR}.a OUTPUT_VARIABLE BUILTINS_LIBRARY OUTPUT_STRIP_TRAILING_WHITESPACE)
-+#endif ()
- 
+-
 -set (DEFAULT_LIBS "${DEFAULT_LIBS} ${BUILTINS_LIBRARY} ${COVERAGE_OPTION} -lc -lm -lrt -lpthread")
 +set (DEFAULT_LIBS "${DEFAULT_LIBS} ${BUILTINS_LIBRARY} ${COVERAGE_OPTION} -lc -lm -lrt -lpthread -lcompiler_rt -lprocstat")
  
