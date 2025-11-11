aafe9a2135e4d9df2cf564477e4584a56d6b8a69
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-11-12 04:42:27 +0800

    security/py-pyopenssl: Move security/py-openssl to security/py-pyopenssl
    - Update WWW
    - Convert to USE_PYTHON=pep517
    - Clean up Makefile
    - Bump PORTREVISION for package change
    - Bump PORTREVISION of dependent ports for dependency change

---
+++ security/py-pyopenssl/files/patch-src-OpenSSL-version.py
@@ -0,0 +1,11 @@
+--- src/OpenSSL/version.py.orig	2025-09-17 00:31:40 UTC
++++ src/OpenSSL/version.py
+@@ -19,7 +19,7 @@ __version__ = "25.3.0"
+ 
+ __version__ = "25.3.0"
+ 
+-__title__ = "pyOpenSSL"
++__title__ = "pyopenssl"
+ __uri__ = "https://pyopenssl.org/"
+ __summary__ = "Python wrapper module around the OpenSSL library"
+ __author__ = "The pyOpenSSL developers"
