9106c70fc3b8f77f83ba8bf9c7f2be195f867a14
Yuri Victorovich <yuri@FreeBSD.org>
2025-11-12 10:58:53 -0800

    misc/astc-encoder: update 5.1.0 → 5.3.0

--- misc/astc-encoder/files/patch-Source_cmake__core.cmake
+++ misc/astc-encoder/files/patch-Source_cmake__core.cmake
@@ -1,14 +1,6 @@
---- Source/cmake_core.cmake.orig	2024-11-15 11:22:40 UTC
+--- Source/cmake_core.cmake.orig	2025-04-14 08:46:50 UTC
 +++ Source/cmake_core.cmake
-@@ -170,7 +170,6 @@ macro(astcenc_set_properties ASTCENC_TARGET_NAME ASTCE
-             $<${is_gnu_fe}:-Wall>
-             $<${is_gnu_fe}:-Wextra>
-             $<${is_gnu_fe}:-Wpedantic>
--            $<${is_gnu_fe}:-Werror>
-             $<${is_gnu_fe}:-Wshadow>
-             $<${is_gnu_fe}:-Wdouble-promotion>
-             $<${is_clang}:-Wdocumentation>
-@@ -449,8 +448,6 @@ macro(astcenc_set_properties ASTCENC_TARGET_NAME ASTCE
+@@ -437,8 +437,6 @@ macro(astcenc_set_properties ASTCENC_TARGET_NAME ASTCE
          else()
              target_compile_options(${ASTCENC_TARGET_NAME}
                  PRIVATE
