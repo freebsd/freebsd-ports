6255c4f8eb500b1210e505b1fb1cfa17d21a7907
Robert Nagy <rnagy@FreeBSD.org>
2025-11-12 18:52:59 +0100

    www/ungoogled-chromium: update to 142.0.7444.162
    Security:	https://vuxml.freebsd.org/freebsd/6e05f716-bf9e-11f0-b3f7-a8a1599412c6.html

--- www/ungoogled-chromium/files/patch-chrome_browser_renderer__context__menu_render__view__context__menu.cc
+++ www/ungoogled-chromium/files/patch-chrome_browser_renderer__context__menu_render__view__context__menu.cc
@@ -1,4 +1,4 @@
---- chrome/browser/renderer_context_menu/render_view_context_menu.cc.orig	2025-11-01 06:40:37 UTC
+--- chrome/browser/renderer_context_menu/render_view_context_menu.cc.orig	2025-11-12 17:42:57 UTC
 +++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
 @@ -244,7 +244,7 @@
  #include "url/origin.h"
@@ -9,7 +9,7 @@
  #include "components/webapps/isolated_web_apps/scheme.h"
  #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
          // BUILDFLAG(IS_CHROMEOS)
-@@ -4989,7 +4989,7 @@ void RenderViewContextMenu::OpenLinkInSplitView() {
+@@ -4987,7 +4987,7 @@ void RenderViewContextMenu::OpenLinkInSplitView() {
  #endif  // !BUILDFLAG(IS_ANDROID)
  
  #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
