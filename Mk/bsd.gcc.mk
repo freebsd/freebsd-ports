7dd5e43d81554a165648058adde53c394913293e
Takefu <takefu@airport.fm>
2025-11-13 08:23:46 +0000

    Mk/bsd.gcc.mk: Unregister GCC 11
    PR:		290086

--- Mk/bsd.gcc.mk
+++ Mk/bsd.gcc.mk
@@ -35,7 +35,7 @@ GCC_Include_MAINTAINER=		gerald@FreeBSD.org
 # All GCC versions supported by this framework.
 #
 # When updating this, keep Mk/bsd.default-versions.mk in sync.
-GCCVERSIONS=	11 12 13 14 15 16
+GCCVERSIONS=	12 13 14 15 16
 
 # No configurable parts below this. ####################################
 #
