7b519a5940632147ff073292dd182c52fa834edd
Boris Korzun <drtr0jan@yandex.ru>
2025-11-17 00:20:50 +0100

    net/kamailio: update to 6.0.4
     - add a patch to fix the build on armv7
    
    Changelog: https://www.kamailio.org/pub/kamailio/6.0.4/ChangeLog
    
    PR:		291047

--- net/kamailio/files/extra-patch-cmake_modules-docs.cmake
+++ net/kamailio/files/extra-patch-cmake_modules-docs.cmake
@@ -1,15 +1,16 @@
---- cmake/modules-docs.cmake.orig	2025-01-29 11:36:50 UTC
+--- cmake/modules-docs.cmake.orig	2025-11-06 09:29:38 UTC
 +++ cmake/modules-docs.cmake
-@@ -171,12 +171,6 @@ function(docs_add_module module_name)
-       COMMENT "Processing target ${module_name}_man")
+@@ -157,13 +157,6 @@ function(docs_add_module group_name module_name)
+   )
  
-     install(
--      FILES ${CMAKE_CURRENT_SOURCE_DIR}/${module_name}/README
--      RENAME README.${module_name}
--      DESTINATION ${CMAKE_INSTALL_DOCDIR}/modules
--      COMPONENT kamailio_docs)
+   install(
+-    FILES ${CMAKE_CURRENT_SOURCE_DIR}/${module_name}/README
+-    RENAME README.${module_name}
+-    DESTINATION ${CMAKE_INSTALL_DOCDIR}/modules
+-    COMPONENT ${group_name}
+-  )
 -
--    install(
-       FILES ${CMAKE_CURRENT_SOURCE_DIR}/${module_name}/${module_name}.7
-       DESTINATION ${CMAKE_INSTALL_DATADIR}/man/man7
-       COMPONENT kamailio_docs
+-  install(
+     FILES ${CMAKE_CURRENT_SOURCE_DIR}/${module_name}/${module_name}.7
+     DESTINATION ${CMAKE_INSTALL_DATADIR}/man/man7
+     COMPONENT ${group_name}
