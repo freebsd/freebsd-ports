de94dd0965f0479c3f2f367e8979e7b1200565ef
Cy Schubert <cy@FreeBSD.org>
2025-11-17 09:32:25 -0800

    security/wpa_supplicant-devel: Update to latest GH commit
    Update to the latest w1.fi commit, proxied through my GH account.

--- security/wpa_supplicant-devel/files/patch-wpa__supplicant_main.c
+++ security/wpa_supplicant-devel/files/patch-wpa__supplicant_main.c
@@ -1,6 +1,6 @@
---- wpa_supplicant/main.c.orig	2016-11-05 20:56:30 UTC
-+++ wpa_supplicant/main.c
-@@ -66,7 +66,7 @@ static void usage(void)
+--- wpa_supplicant/main.c.orig	2025-11-07 07:33:18.000000000 -0800
++++ wpa_supplicant/main.c	2025-11-17 09:07:02.682216000 -0800
+@@ -67,7 +67,7 @@
  	       "  -c = Configuration file\n"
  	       "  -C = ctrl_interface parameter (only used if -c is not)\n"
  	       "  -d = increase debugging verbosity (-dd even more)\n"
@@ -9,17 +9,17 @@
  	       "  -e = entropy file\n"
  #ifdef CONFIG_DEBUG_FILE
  	       "  -f = log output to debug file instead of stdout\n"
-@@ -105,8 +105,7 @@ static void usage(void)
- 	       "  -W = wait for a control interface monitor before starting\n");
+@@ -107,8 +107,7 @@
+ 	       "  -y = show configuration parsing details in debug log\n");
  
  	printf("example:\n"
 -	       "  wpa_supplicant -D%s -iwlan0 -c/etc/wpa_supplicant.conf\n",
 -	       wpa_drivers[0] ? wpa_drivers[0]->name : "nl80211");
-+                "  wpa_supplicant -Dbsd -iwlan0 -c/etc/wpa_supplicant.conf\n");
- #endif /* CONFIG_NO_STDOUT_DEBUG */
- }
- 
-@@ -199,6 +198,11 @@ int main(int argc, char *argv[])
++	       "  wpa_supplicant -Dbsd -iwlan0 -c/etc/wpa_supplicant.conf\n");
+ 	printf("\nIf run without specifying a network interface or control interface, the a\n"
+ 	       "configuration file is parsed without starting any operation.\n"
+ 		"This can be used to check whether a configuration file has valid contents.\n");
+@@ -204,6 +203,11 @@
  
  	wpa_supplicant_fd_workaround(1);
  
@@ -30,4 +30,4 @@
 +
  	for (;;) {
  		c = getopt(argc, argv,
- 			   "b:Bc:C:D:de:f:g:G:hi:I:KLMm:No:O:p:P:qsTtuvW");
+ 			   "b:Bc:C:D:de:f:g:G:hi:I:KLMm:No:O:p:P:qsTtuvWy");
