cedd95175b8de716c29a53e061942ddadf3b3161
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2025-11-20 14:18:03 -0800

    Mk/bsd.default-versions.mk: Move default version to 4.4
    - Fix lazarus path folder to 4.4
    - Bump PORTREVISION

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -75,11 +75,11 @@ GUILE_DEFAULT?=		2.2
 IMAGEMAGICK_DEFAULT?=	7
 # Possible values: 8, 11, 17, 18, 19, 20, 21, 22, 23, 24, 25
 JAVA_DEFAULT?=		8
-# Possible values: 4.2, 4.99
+# Possible values: 4.4, 4.99
 .  if (defined(WANT_LAZARUS_DEVEL) && !empty(WANT_LAZARUS_DEVEL)) || ${ARCH:Maarch64}
 LAZARUS_DEFAULT?=	4.99
 .  else
-LAZARUS_DEFAULT?=	4.2
+LAZARUS_DEFAULT?=	4.4
 .  endif
 # Possible values: rust, legacy
 .  if empty(ARCH:Naarch64:Namd64:Narmv7:Ni386:Npowerpc64:Npowerpc64le:Npowerpc:Nriscv64)
