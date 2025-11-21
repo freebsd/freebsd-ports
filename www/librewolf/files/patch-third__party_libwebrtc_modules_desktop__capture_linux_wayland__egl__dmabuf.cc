27d0bf1f1daebb6c37a324f70597485591979439
Martin Filla <freebsd@sysctl.cz>
2025-11-21 04:24:02 +0300

    www/librewolf: Update 144.0.2-1 => 145.0.1-1
    Release notes:
    https://www.firefox.com/en-US/firefox/145.0/releasenotes/
    https://www.firefox.com/en-US/firefox/145.0.1/releasenotes/
    
    PR:	291111
    MFH:	2025Q4

--- www/librewolf/files/patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland__egl__dmabuf.cc
+++ www/librewolf/files/patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland__egl__dmabuf.cc
@@ -1,26 +1,19 @@
-commit 505b8f54ca7e3ee3230ed9c94e6553de504fda29
+commit f53507857647459d4a48935901f80f27a5ae58a9
 Author: Christoph Moench-Tegeder <cmt@FreeBSD.org>
 
     avoid linux-only includes
 
 diff --git third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc
-index 9f30378451fb..75381cd39e29 100644
+index 821fdfb487b7..e000c1c104fe 100644
 --- third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc
 +++ third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc
-@@ -11,12 +11,16 @@
- #include "modules/desktop_capture/linux/wayland/egl_dmabuf.h"
- 
- #include <EGL/eglext.h>
+@@ -15,7 +15,9 @@
+ #include <EGL/eglplatform.h>
+ #include <GL/gl.h>
+ #include <GL/glext.h>
 +#if !defined(__FreeBSD__)
  #include <asm/ioctl.h>
 +#endif
  #include <dlfcn.h>
  #include <fcntl.h>
- #include <gdk/gdk.h>
- #include <libdrm/drm_fourcc.h>
-+#if !defined(__FreeBSD__)
- #include <linux/types.h>
-+#endif
- #include <spa/param/video/format-utils.h>
- #include <unistd.h>
- #include <xf86drm.h>
+ #include <gbm.h>
