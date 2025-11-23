ba72838fff3e7fa001d247aa5409e889a7c864c3
Guido Falsi <madpilot@FreeBSD.org>
2025-11-23 22:39:06 +0100

    net/unison240: Fetch from github, deprecate
    The distfiles used by the port are not available anymore, take the one from github. Adapt the port to build with the slightly different layout of this distfile. [1]
    
    The new distfile does not provide html, ps and pdf documentation, so remove those files from the port.
    
    I'm also deprecating this port, it is an ancient version not really supported anymore by upstream. set a long expiration time.
    
    While here:
    
    - Refresh Makefiles for other legacy unison ports
    - Remove CONFLICTS with no more existing -devel port
    
    PR:		291166 [1]
    MFH:		2025Q4

--- net/unison240/files/patch-uigtk2.ml
+++ net/unison240/files/patch-uigtk2.ml
@@ -1,6 +1,6 @@
---- uigtk2.ml.orig	2009-05-14 15:59:33 UTC
+--- uigtk2.ml.orig	2014-12-30 02:21:28 UTC
 +++ uigtk2.ml
-@@ -93,12 +93,12 @@ let fontMonospaceMediumPango = lazy (Pango.Font.from_s
+@@ -89,12 +89,12 @@ let icon =
  (* This does not work with the current version of Lablgtk, due to a bug
  let icon =
    GdkPixbuf.from_data ~width:48 ~height:48 ~has_alpha:true
@@ -14,4 +14,4 @@
 +    (Gpointer.region_of_bytes Pixmaps.icon_data) (GdkPixbuf.get_pixels p);
    p
  
- (*********************************************************************
+ let leftPtrWatch =
