760f32988f5afe2a85249d2e2475c681224d4540
Emanuel Haupt <ehaupt@FreeBSD.org>
2025-11-25 19:27:20 +0100

    filesystems/mergerfs: Update to 2.41.1

--- filesystems/mergerfs/files/patch-tools_preload.c
+++ filesystems/mergerfs/files/patch-tools_preload.c
@@ -1,29 +1,14 @@
---- tools/preload.c.orig	2024-02-29 20:41:22 UTC
+--- tools/preload.c.orig	2025-11-18 14:35:27 UTC
 +++ tools/preload.c
-@@ -25,10 +25,25 @@
- #include <string.h>
+@@ -33,7 +33,11 @@
  #include <sys/ioctl.h>
- #include <stdarg.h>
--#include <sys/stat.h>
-+#include <sys/stat.h>
-+#ifdef __linux__
-+#define S_IFMT S_IFMT
-+#define S_IFREG S_IFREG
-+#else
-+#define S_IFMT 0
-+#define S_IFREG 0
-+#endif
+ #include <sys/stat.h>
  #include <sys/types.h>
- #include <unistd.h>
- #include <fcntl.h>
-+
-+#ifdef __linux__
-+#define O_TMPFILE O_TMPFILE
-+#define O_PATH O_PATH
++#ifdef __FreeBSD__
++#include <sys/extattr.h>
 +#else
-+#define O_TMPFILE 0
-+#define O_PATH 0
+ #include <sys/xattr.h>
 +#endif
+ #include <unistd.h>
  
  typedef char IOCTL_BUF[4096];
- #define IOCTL_APP_TYPE  0xDF
