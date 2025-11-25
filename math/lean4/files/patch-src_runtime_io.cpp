65e52a0876a95697045214a6f184635645eb7345
Yuri Victorovich <yuri@FreeBSD.org>
2025-11-24 20:33:21 -0800

    math/lean4: Update patches

--- math/lean4/files/patch-src_runtime_io.cpp
+++ math/lean4/files/patch-src_runtime_io.cpp
@@ -1,12 +1,11 @@
---- src/runtime/io.cpp.orig	2025-05-06 09:12:17 UTC
+--- src/runtime/io.cpp.orig	2025-11-18 02:29:21 UTC
 +++ src/runtime/io.cpp
-@@ -1253,7 +1253,13 @@ extern "C" LEAN_EXPORT obj_res lean_io_app_path(obj_ar
+@@ -1365,7 +1365,13 @@ extern "C" LEAN_EXPORT obj_res lean_io_app_path(obj_ar
      char dest[PATH_MAX];
      memset(dest, 0, PATH_MAX);
      pid_t pid = getpid();
--    snprintf(path, PATH_MAX, "/proc/%d/exe", pid);
 +#if defined(__linux__)
-+     snprintf(path, PATH_MAX, "/proc/%d/exe", pid);
+     snprintf(path, PATH_MAX, "/proc/%d/exe", pid);
 +#elif defined(__FreeBSD__)
 +    snprintf(path, PATH_MAX, "/proc/%d/file", pid);
 +#else
--- math/lean4/files/patch-src_runtime_stack__overflow.cpp
+++ math/lean4/files/patch-src_runtime_stack__overflow.cpp
@@ -1,11 +1,13 @@
---- src/runtime/stack_overflow.cpp.orig	2025-05-06 09:12:17 UTC
+--- src/runtime/stack_overflow.cpp.orig	2025-11-18 02:29:21 UTC
 +++ src/runtime/stack_overflow.cpp
-@@ -21,6 +21,9 @@ Port of the corresponding Rust code (see links below).
+@@ -21,6 +21,11 @@ Port of the corresponding Rust code (see links below).
  #include <initializer_list>
  #include "runtime/stack_overflow.h"
  
++#if defined(__FreeBSD__)
 +#include <pthread_np.h>
 +#define pthread_getattr_np pthread_attr_get_np
++#endif
 +
  namespace lean {
  // stack guard of the main thread
