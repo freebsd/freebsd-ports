a3f5a1f4fb7f591bf58da7d568eab30b7ab7175e
Hiroki Tagato <tagattie@FreeBSD.org>
2025-11-25 16:32:35 +0900

    editors/zed: Update to 0.213.7
    Changelog:
    - https://github.com/zed-industries/zed/releases/tag/v0.213.4
    - https://github.com/zed-industries/zed/releases/tag/v0.213.6
    - https://github.com/zed-industries/zed/releases/tag/v0.213.7
    
    Reported by:	GitHub (watch releases)

--- editors/zed/files/patch-crates_remote_src_transport_ssh.rs
+++ editors/zed/files/patch-crates_remote_src_transport_ssh.rs
@@ -1,4 +1,4 @@
---- crates/remote/src/transport/ssh.rs.orig	2025-11-21 14:41:36 UTC
+--- crates/remote/src/transport/ssh.rs.orig	2025-11-24 20:23:08 UTC
 +++ crates/remote/src/transport/ssh.rs
 @@ -1058,6 +1058,7 @@ impl SshSocket {
          let os = match os.trim() {
@@ -8,3 +8,14 @@
              _ => anyhow::bail!(
                  "Prebuilt remote servers are not yet available for {os:?}. See https://zed.dev/docs/remote-development"
              ),
+@@ -1069,7 +1070,9 @@ impl SshSocket {
+             || arch.starts_with("aarch64")
+         {
+             "aarch64"
+-        } else if arch.starts_with("x86") {
++        } else if arch.starts_with("x86")
++            || arch.starts_with("amd64")
++        {
+             "x86_64"
+         } else {
+             anyhow::bail!(
--- editors/zed/files/patch-crates_zed_src_zed.rs
+++ editors/zed/files/patch-crates_zed_src_zed.rs
@@ -1,6 +1,6 @@
---- crates/zed/src/zed.rs.orig	2025-11-19 15:41:44 UTC
+--- crates/zed/src/zed.rs.orig	2025-11-24 20:23:08 UTC
 +++ crates/zed/src/zed.rs
-@@ -381,6 +381,7 @@ pub fn initialize_workspace(
+@@ -382,6 +382,7 @@ pub fn initialize_workspace(
          if let Some(specs) = window.gpu_specs() {
              log::info!("Using GPU: {:?}", specs);
              show_software_emulation_warning_if_needed(specs.clone(), window, cx);
