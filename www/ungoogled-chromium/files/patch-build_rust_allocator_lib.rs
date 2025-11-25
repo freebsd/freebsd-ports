f622129068b76b79fdb114f98fdbb20700a44780
Robert Nagy <rnagy@FreeBSD.org>
2025-11-25 14:38:29 +0100

    www/{*chromium,iridium}: unbreak with rust 1.89
    PR:	291107
    
    (cherry picked from commit 31d22775f7f2f7db043c35fe79b9270e4afc53e0)

--- www/ungoogled-chromium/files/patch-build_rust_allocator_lib.rs
+++ www/ungoogled-chromium/files/patch-build_rust_allocator_lib.rs
@@ -1,6 +1,6 @@
---- build/rust/allocator/lib.rs.orig	2025-11-01 06:40:37 UTC
+--- build/rust/allocator/lib.rs.orig	2025-11-25 13:37:36 UTC
 +++ build/rust/allocator/lib.rs
-@@ -96,6 +96,12 @@ mod both_allocators {
+@@ -96,6 +96,18 @@ mod both_allocators {
          0
      }
  
@@ -9,6 +9,12 @@
 +    #[no_mangle]
 +    #[linkage = "weak"]
 +    static __rust_no_alloc_shim_is_unstable: u8 = 0;
++
++    // TODO(crbug.com/440481922): Remove this after rolling past https://github.com/rust-lang/rust/pull/143387
++    #[rustc_std_internal_symbol]
++    #[allow(non_upper_case_globals)]
++    #[linkage = "weak"]
++    static __rust_alloc_error_handler_should_panic: u8 = 0;
 +
      // Mangle the symbol name as rustc expects.
      #[rustc_std_internal_symbol]
