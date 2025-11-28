974099d0bd746251e5166248e3126781f39937c0
Tijl Coosemans <tijl@FreeBSD.org>
2025-11-28 18:29:53 +0100

    devel/libtextstyle, devel/gettext*: Update to 0.26
    Recent versions of gettext no longer install m4 files in
    PREFIX/share/aclocal.  They have to be installed in the work
    directory by autopoint for aclocal to find them.
    
    Patch devel/autoconf so autoreconf runs autopoint when gettext related
    macros are used in configure.ac (e.g. AM_ICONV).  Previously it only
    ran autopoint when AM_GNU_GETTEXT_(REQUIRE_)VERSION was used.
    
    The following ports now require autopoint (i.e. USES=gettext-tools):
    benchmarks/sysbench, devel/libhtp, editors/fxite, filesystems/rar2fs,
    graphics/gd, net/ucarp, news/fidogate, sysutils/cdrdao
    
    Other fixes:
    
    emulators/mednafen, misc/lifelines: Use older version of gettext macros.
    ftp/axel: Patch Makefile.am and configure.ac to get gettext working and
    add NLS option.
    ftp/wzdftpd: Use autoreconf 2.72.
    mail/sympa: Remove hack from configure.ac.
    multimedia/dvdauthor: Use ports autoreconf.
    net/asterisk-chan_sccp: Drop autoreconf and gettext-tools dependencies.
    security/pam_pkcs11: Patch configure.ac so autoreconf can find the
    custom directory with m4 files.
    sysutils/grub2-efi, sysutils/grub2-pcbsd: Add patch to run autoreconf
    with -f flag so it updates all files.
    www/wget2: Patch configure.ac to what autopoint expects.
    
    PR:		287167
    Exp-run by:	antoine

--- Mk/Uses/autoreconf.mk
+++ Mk/Uses/autoreconf.mk
@@ -89,7 +89,7 @@ _AUTORECONF=	2.69
 BUILD_DEPENDS+=	autoconf2.69>=2.69:devel/autoconf2.69
 .  else
 _AUTORECONF=	2.72
-BUILD_DEPENDS+=	autoconf>=${_AUTORECONF}:devel/autoconf
+BUILD_DEPENDS+=	autoconf>=2.72_1:devel/autoconf
 .  endif
 
 BUILD_DEPENDS+=	automake>=1.18.1:devel/automake
