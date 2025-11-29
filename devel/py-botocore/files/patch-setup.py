c9e519bcd497feea5629812fe6e837d3abdbfe60
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2025-11-30 07:23:50 +0800

    devel/py-botocore: Update to 1.41.2
    Changes:	https://github.com/boto/botocore/blob/develop/CHANGELOG.rst

--- devel/py-botocore/files/patch-setup.py
+++ devel/py-botocore/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2025-08-29 18:03:06 UTC
+--- setup.py.orig	2025-11-18 19:01:56 UTC
 +++ setup.py
 @@ -33,7 +33,7 @@ extras_require = {
  ]
  
  extras_require = {
--    'crt': ['awscrt==0.27.6'],
-+    'crt': ['awscrt>=0.27.6'],
+-    'crt': ['awscrt==0.28.4'],
++    'crt': ['awscrt>=0.28.4'],
  }
  
  setup(
