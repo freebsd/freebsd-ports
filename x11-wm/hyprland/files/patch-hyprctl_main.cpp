f728901ba5b34e173adb08e3c0dd72a7a1aa693a
Hiroki Tagato <tagattie@FreeBSD.org>
2025-11-30 22:26:54 +0900

    x11-wm/hyprland: Update to 0.52.1
    Changelog:
    - https://github.com/hyprwm/Hyprland/releases/tag/v0.52.0
    - https://github.com/hyprwm/Hyprland/releases/tag/v0.52.1
    
    Reported by:	GitHub (watch releases)

--- x11-wm/hyprland/files/patch-hyprctl_main.cpp
+++ x11-wm/hyprland/files/patch-hyprctl_main.cpp
@@ -1,11 +1,11 @@
---- hyprctl/main.cpp.orig	2025-09-10 12:53:39 UTC
+--- hyprctl/main.cpp.orig	2025-11-09 23:33:25 UTC
 +++ hyprctl/main.cpp
 @@ -270,7 +270,7 @@ int requestIPC(std::string_view filename, std::string_
      sockaddr_un serverAddress = {0};
      serverAddress.sun_family  = AF_UNIX;
  
 -    std::string socketPath = getRuntimeDir() + "/" + instanceSignature + "/" + filename;
-+    std::string socketPath = getRuntimeDir() + "/" + instanceSignature + "/" + std::string(filename);
++    std::string socketPath = getRuntimeDir() + "/" + instanceSignature + "/" + static_cast<std::string>(filename);
  
      strncpy(serverAddress.sun_path, socketPath.c_str(), sizeof(serverAddress.sun_path) - 1);
  
