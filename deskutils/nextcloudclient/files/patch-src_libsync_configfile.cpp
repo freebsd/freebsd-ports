5c002626cc07340557a1d9ec7d1b5d7bfaceeae3
Michael Reifenberger <mr@FreeBSD.org>
2025-12-03 15:49:13 +0100

    deskutils/nextcloudclient: Update to 4.0.2

--- deskutils/nextcloudclient/files/patch-src_libsync_configfile.cpp
+++ deskutils/nextcloudclient/files/patch-src_libsync_configfile.cpp
@@ -1,25 +1,18 @@
---- src/libsync/configfile.cpp.orig	2025-03-06 09:00:02 UTC
+--- src/libsync/configfile.cpp.orig	2025-12-03 14:37:03 UTC
 +++ src/libsync/configfile.cpp
-@@ -653,11 +653,22 @@ bool ConfigFile::skipUpdateCheck(const QString &connec
-     if (connection.isEmpty())
-         con = defaultConnection();
- 
-+#if 0
-     QVariant fallback = getValue(QLatin1String(skipUpdateCheckC), con, false);
-+#else
-+    QVariant fallback = getValue(QLatin1String(skipUpdateCheckC), con, true);
-+#endif
+@@ -626,11 +626,13 @@ bool ConfigFile::skipUpdateCheck(const QString &connec
+ bool ConfigFile::skipUpdateCheck(const QString &connectionGroupName) const
+ {
+     const auto groupName = connectionGroupName.isEmpty() ? defaultConnectionGroupName() : connectionGroupName;
+-    QVariant fallback = getValue(QLatin1String(skipUpdateCheckC), groupName, false);
++    QVariant fallback = getValue(QLatin1String(skipUpdateCheckC), groupName, true);
      fallback = getValue(QLatin1String(skipUpdateCheckC), QString(), fallback);
  
      QVariant value = getPolicySetting(QLatin1String(skipUpdateCheckC), fallback);
-+#if 0
-     return value.toBool();
-+#else
+-    return value.toBool();
 +    if ( !value.toBool() )
-+        qDebug() << "FreeBSD package disabled the UpdateCheck mechanism.";
-+
++      qDebug() << "FreeBSD package disabled the UpdateCheck mechanism.";
 +    return true;
-+#endif
  }
  
- void ConfigFile::setSkipUpdateCheck(bool skip, const QString &connection)
+ void ConfigFile::setSkipUpdateCheck(bool skip, const QString &connectionGroupName)
