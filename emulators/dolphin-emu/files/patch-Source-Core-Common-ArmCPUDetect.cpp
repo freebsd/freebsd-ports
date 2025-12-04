38556b0dc354caeb004c8e3a1f2e19fb2deabfe7
Jason E. Hale <jhale@FreeBSD.org>
2025-12-04 18:07:03 -0500

    emulators/dolphin-emu: Prepare for Qt 6.10
    Backport upstream patch to fix build with Qt 6.10.
    
    With hat:	kde@

--- emulators/dolphin-emu/files/patch-Source-Core-Common-ArmCPUDetect.cpp
+++ emulators/dolphin-emu/files/patch-Source-Core-Common-ArmCPUDetect.cpp
@@ -1,8 +1,8 @@
---- Source/Core/Common/ArmCPUDetect.cpp.orig    2025-05-02 13:15:23 UTC
+--- Source/Core/Common/ArmCPUDetect.cpp.orig	2025-09-15 19:46:44 UTC
 +++ Source/Core/Common/ArmCPUDetect.cpp
 @@ -20,6 +20,7 @@
  #include <sys/auxv.h>
- #elif defined(__FreeBSD__)
+ #elif defined(HAVE_ELF_AUX_INFO)
  #include <sys/auxv.h>
 +#include <sys/sysctl.h>
  #elif defined(__OpenBSD__)
