30a5665b92bf3285babb23c3a24d807874e00b5c
Michael Gmelin <grembo@FreeBSD.org>
2025-12-05 15:26:39 +0100

    devel/phabricator: Fix error in MFA QR code generation

---
+++ devel/phabricator/files/patch-externals_phpqrcode_phpqrcode.php
@@ -0,0 +1,16 @@
+--- externals/phpqrcode/phpqrcode.php.orig	2025-12-05 14:17:54 UTC
++++ externals/phpqrcode/phpqrcode.php
+@@ -2945,11 +2945,11 @@
+                 if($col >= $this->rsblocks[0]->dataLength) {
+                     $row += $this->b1;
+                 }
+-                $ret = $this->rsblocks[$row]->data[$col];
++                $ret = $this->rsblocks[$row]->data[(int) $col];
+             } else if($this->count < $this->dataLength + $this->eccLength) {
+                 $row = ($this->count - $this->dataLength) % $this->blocks;
+                 $col = ($this->count - $this->dataLength) / $this->blocks;
+-                $ret = $this->rsblocks[$row]->ecc[$col];
++                $ret = $this->rsblocks[$row]->ecc[(int) $col];
+             } else {
+                 return 0;
+             }
