92b25f40e493e37d2068c6b9700862780f86d1c8
Robert Nagy <rnagy@FreeBSD.org>
2025-12-12 13:20:57 +0100

    www/iridium: update to 2025.12.143.1

--- www/iridium/files/patch-chrome_browser_ui_views_frame_browser__view.cc
+++ www/iridium/files/patch-chrome_browser_ui_views_frame_browser__view.cc
@@ -1,4 +1,4 @@
---- chrome/browser/ui/views/frame/browser_view.cc.orig	2025-12-10 15:04:57 UTC
+--- chrome/browser/ui/views/frame/browser_view.cc.orig	2025-12-12 12:17:57 UTC
 +++ chrome/browser/ui/views/frame/browser_view.cc
 @@ -2305,7 +2305,7 @@ void BrowserView::ToolbarSizeChanged(bool is_animating
  }
@@ -9,7 +9,7 @@
    std::vector<ContentsWebView*> contents_web_views =
        GetAllVisibleContentsWebViews();
  
-@@ -5919,7 +5919,7 @@ void BrowserView::MaybeShowProfileSwitchIPH() {
+@@ -5920,7 +5920,7 @@ void BrowserView::MaybeShowProfileSwitchIPH() {
  }
  
  void BrowserView::MaybeShowSupervisedUserProfileSignInIPH() {
@@ -18,7 +18,7 @@
    if (!ShouldShowAvatarToolbarIPH()) {
      return;
    }
-@@ -5929,7 +5929,7 @@ void BrowserView::MaybeShowSupervisedUserProfileSignIn
+@@ -5930,7 +5930,7 @@ void BrowserView::MaybeShowSupervisedUserProfileSignIn
  }
  
  void BrowserView::MaybeShowSignInBenefitsIPH() {
