ad5110ee8b857bec4af18ff43725487c5e3ba936
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2025-12-12 17:06:04 -0800

    x11-servers/xlibre-server: Fix build on aarch64
    Reported by:	fluffy

---
+++ x11-servers/xlibre-server/files/patch-hw_xfree86_os-support_bsd_arm_video.c
@@ -0,0 +1,11 @@
+--- hw/xfree86/os-support/bsd/arm_video.c       2025-12-12 16:55:30.888512000 -0800
++++ hw/xfree86/os-support/bsd/arm_video.c       2025-12-12 16:55:40.571888000 -0800
+@@ -65,7 +65,7 @@
+ #include <X11/X.h>
+
+ #include "xf86.h"
+-#include "xf86_os_support."
++#include "xf86_os_support.h"
+ #include "xf86Priv.h"
+ #include "xf86_OSlib.h"
+ #include "compiler.h"
