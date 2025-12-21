811d4fe46a1a1d991e44e0915f98886ebdef4529
Jason E. Hale <jhale@FreeBSD.org>
2025-12-20 22:08:05 -0500

    devel/immer: Update to 0.9.0
    https://github.com/arximboldi/immer/releases/tag/v0.9.0

--- devel/immer/files/patch-cmake_FindBoehmGC.cmake
+++ devel/immer/files/patch-cmake_FindBoehmGC.cmake
@@ -1,11 +1,16 @@
---- cmake/FindBoehmGC.cmake.orig	2023-10-04 09:47:41 UTC
+CMake Error at cmake/FindBoehmGC.cmake:67 (check_c_source_runs):
+  Unknown CMake command "check_c_source_runs".
+
+https://github.com/arximboldi/immer/pull/315
+
+--- cmake/FindBoehmGC.cmake.orig	2025-12-02 21:25:10 UTC
 +++ cmake/FindBoehmGC.cmake
-@@ -57,7 +57,7 @@ IF (${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
- IF (${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
-   # checks if 'gc' supports 'GC_get_parallel' and if it does
-   # then use it
--  INCLUDE(${CMAKE_ROOT}/Modules/CheckCSourceCompiles.cmake)
-+  INCLUDE(${CMAKE_ROOT}/Modules/CheckCSourceRuns.cmake)
+@@ -52,7 +52,7 @@ if(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
+ # For FreeBSD we need to use gc-threaded
+ if(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
+   # checks if 'gc' supports 'GC_get_parallel' and if it does then use it
+-  include(${CMAKE_ROOT}/Modules/CheckCSourceCompiles.cmake)
++  include(${CMAKE_ROOT}/Modules/CheckCSourceRuns.cmake)
    # not sure if this links properly...
-   FIND_LIBRARY(BOEHM_GC_LIBRARIES NAMES gc
-     HINTS ${PC_BDW_GC_LIBDIR} ${PC_BDW_GC_LIBRARY_DIRS})
+   find_library(
+     BOEHM_GC_LIBRARIES
