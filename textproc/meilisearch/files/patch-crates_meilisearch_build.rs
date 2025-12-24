efb8a72677a2f89037cb400e4f8a28a21ad71fa4
Ashish SHUKLA <ashish@FreeBSD.org>
2025-12-24 23:53:55 +0000

    textproc/meilisearch: Update to 1.31.0

--- textproc/meilisearch/files/patch-crates_meilisearch_build.rs
+++ textproc/meilisearch/files/patch-crates_meilisearch_build.rs
@@ -1,28 +1,28 @@
---- meilisearch/build.rs.orig	2024-03-21 11:21:56 UTC
-+++ meilisearch/build.rs
+--- crates/meilisearch/build.rs.orig	2025-12-22 13:33:28 UTC
++++ crates/meilisearch/build.rs
 @@ -12,9 +12,9 @@ mod mini_dashboard {
- 
+
      use anyhow::Context;
      use cargo_toml::Manifest;
 -    use reqwest::blocking::get;
      use sha1::{Digest, Sha1};
      use static_files::resource_dir;
 +    use std::fs;
- 
+
      pub fn setup_mini_dashboard() -> anyhow::Result<()> {
          let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 @@ -45,12 +45,10 @@ mod mini_dashboard {
              }
          }
- 
+
 -        let url = meta["assets-url"].as_str().unwrap();
 +        let dashboard_assets_bytes = fs::read("%%MINIDASHBOARDFILE%%")?;
- 
+
 -        let dashboard_assets_bytes = get(url)?.bytes()?;
 -
          let mut hasher = Sha1::new();
 -        hasher.update(&dashboard_assets_bytes);
 +        hasher.update(dashboard_assets_bytes.as_slice());
          let sha1 = hex::encode(hasher.finalize());
- 
+
          assert_eq!(
