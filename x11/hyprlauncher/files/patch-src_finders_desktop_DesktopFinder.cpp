a256ca3ea1aa16c9ad1de3158d3c493a5981f77b
Hiroki Tagato <tagattie@FreeBSD.org>
2025-12-28 16:48:17 +0900

    x11/hyprlauncher: Update to 0.1.4
    While here, adjust dependency so that libinotify is required only when
    system does not yet implement native inotify functions.
    
    Changelog: https://github.com/hyprwm/hyprlauncher/releases/tag/v0.1.4
    
    Reported by:	GitHub (watch releases)

--- x11/hyprlauncher/files/patch-src_finders_desktop_DesktopFinder.cpp
+++ x11/hyprlauncher/files/patch-src_finders_desktop_DesktopFinder.cpp
@@ -1,10 +1,10 @@
---- src/finders/desktop/DesktopFinder.cpp.orig	2025-11-23 08:44:11 UTC
+--- src/finders/desktop/DesktopFinder.cpp.orig	2025-12-27 13:54:55 UTC
 +++ src/finders/desktop/DesktopFinder.cpp
 @@ -9,6 +9,7 @@
  #include <fstream>
  #include <sys/inotify.h>
  #include <sys/poll.h>
 +#include <unistd.h>
+ #include <unordered_set>
  
  #include <hyprutils/string/String.hpp>
- #include <hyprutils/os/Process.hpp>
