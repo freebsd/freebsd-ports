d16c897f93919f4a8d7a12b6dec9cdad6616abf0
Martin Matuska <mm@FreeBSD.org>
2025-12-29 14:53:50 +0100

    filesystems/openzfs: update to 2.4.0
    Changelog:	https://github.com/openzfs/zfs/releases/tag/zfs-2.4.0

--- filesystems/openzfs/files/patch-cmd_zfs_zfs__main.c
+++ filesystems/openzfs/files/patch-cmd_zfs_zfs__main.c
@@ -1,26 +1,31 @@
---- cmd/zfs/zfs_main.c.orig	2025-03-30 00:09:17 UTC
-+++ cmd/zfs/zfs_main.c
-@@ -6845,7 +6845,11 @@ print_holds(boolean_t scripted, int nwidth, int tagwid
+--- cmd/zfs/zfs_main.c.orig	2025-12-18 18:41:34.311311571 +0100
++++ cmd/zfs/zfs_main.c	2025-12-29 11:32:45.050147000 +0100
+@@ -6884,17 +6884,28 @@
  
  			if (scripted) {
  				if (parsable) {
 +#if defined(__FreeBSD__) && defined(__i386__)
 +					(void) printf("%s\t%s\t%d\n", zname,
++					    tagname, time);
 +#else
- 					(void) printf("%s\t%s\t%ld\n", zname,
+ 					(void) printf("%s\t%s\t%lld\n", zname,
+ 					    tagname, (long long)time);
 +#endif
- 					    tagname, time);
  				} else {
  					(void) printf("%s\t%s\t%s\n", zname,
-@@ -6853,7 +6857,11 @@ print_holds(boolean_t scripted, int nwidth, int tagwid
+ 					    tagname, tsbuf);
  				}
  			} else {
  				if (parsable) {
 +#if defined(__FreeBSD__) && defined(__i386__)
 +					(void) printf("%-*s  %-*s  %d\n",
++					    nwidth, zname, tagwidth,
++					    tagname, time);
 +#else
- 					(void) printf("%-*s  %-*s  %ld\n",
-+#endif
+ 					(void) printf("%-*s  %-*s  %lld\n",
  					    nwidth, zname, tagwidth,
- 					    tagname, time);
+ 					    tagname, (long long)time);
++#endif
  				} else {
+ 					(void) printf("%-*s  %-*s  %s\n",
+ 					    nwidth, zname, tagwidth,
