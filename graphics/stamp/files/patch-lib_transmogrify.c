cb7d91ccae8fac1b3b139806ae84653b9e1bf8dc
Rene Ladan <rene@FreeBSD.org>
2025-12-31 12:46:15 +0100

    graphics/stamp: Remove expired port
    2025-12-31 graphics/stamp: historical tool, use imagemagick instead

--- graphics/stamp/files/patch-lib_transmogrify.c
+++
@@ -1,9 +0,0 @@
---- lib/transmogrify.c.orig	1999-03-10 11:40:33 UTC
-+++ lib/transmogrify.c
-@@ -1,5 +1,6 @@
- #include <stdio.h>
- #include <stdlib.h>
-+#include <string.h>
- #include <jpeglib.h>
- #include "../lib/common.h"
- 
