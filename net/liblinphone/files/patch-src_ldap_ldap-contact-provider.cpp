9ef0710c63eefc357d6d128e33b7652562e2fbcb
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2026-01-02 10:38:03 +0100

    net/liblinphone: Update version 5.4.26=>5.4.72
    - Update WWW
    
    Changelog: https://gitlab.linphone.org/BC/public/liblinphone/-/tags/5.4.72

--- net/liblinphone/files/patch-src_ldap_ldap-contact-provider.cpp
+++ net/liblinphone/files/patch-src_ldap_ldap-contact-provider.cpp
@@ -1,16 +1,6 @@
---- src/ldap/ldap-contact-provider.cpp.orig	2025-03-20 22:21:32 UTC
+--- src/ldap/ldap-contact-provider.cpp.orig	2025-12-22 14:02:00 UTC
 +++ src/ldap/ldap-contact-provider.cpp
-@@ -269,7 +269,9 @@ void LdapContactProvider::initializeLdap() {
- 			lError() << "[LDAP] Problem initializing TLS on setting require SAN '" << mServerUrl[mServerUrlIndex]
- 			         << "': " << ret << " (" << ldap_err2string(ret) << ")";
- 
-+#ifdef LDAP_OPT_X_TLS_RANDOM_FUNC
- 		ret = ldap_set_option(NULL, LDAP_OPT_X_TLS_RANDOM_FUNC, (void *)randomProvider);
-+#endif
- 		if (ret != LDAP_SUCCESS) {
- 			lError() << "[LDAP] Problem initializing TLS random generator function.";
- 		}
-@@ -291,16 +293,6 @@ void LdapContactProvider::initializeLdap() {
+@@ -279,16 +279,6 @@ void LdapContactProvider::initializeLdap() {
  		         << ldap_err2string(ret) << "), err " << err << " (" << ldap_err2string(err) << ")";
  		fallbackToNextServerUrl();
  	} else if (mConfig.count("use_tls") > 0 && mConfig["use_tls"][0] == "1") {
@@ -20,7 +10,7 @@
 -			belle_generic_uri_t *serverUri = belle_generic_uri_parse(
 -			    mConfig["server"][mConfigServerIndex].c_str()); // mServer are results of sal. Use the root urls.
 -			const char *cHost = belle_generic_uri_get_host(serverUri);
--			std::string hostname = cHost ? cHost : "";
+-			std::string hostname = L_C_TO_STRING(cHost);
 -			ldap_set_option(mLd, LDAP_OPT_X_TLS_PEER_CN, &hostname[0]);
 -			if (serverUri) belle_sip_object_unref(serverUri);
 -		}
