e7922fde842bbdcbb47a11a443f5ec673a842347
Hiroki Tagato <tagattie@FreeBSD.org>
2026-01-04 15:26:39 +0900

    graphics/hyprgraphics: Update to 0.5.0
    Changelog: https://github.com/hyprwm/hyprgraphics/releases/tag/v0.5.0
    
    Reported by:	GitHub (watch releases)

---
+++ graphics/hyprgraphics/files/patch-include_hyprgraphics_resource_resources_ImageResource.hpp
@@ -0,0 +1,10 @@
+--- include/hyprgraphics/resource/resources/ImageResource.hpp.orig	2026-01-04 03:14:14 UTC
++++ include/hyprgraphics/resource/resources/ImageResource.hpp
+@@ -1,6 +1,7 @@
+ #pragma once
+ 
+ #include <string>
++#include <span>
+ #include <hyprutils/math/Vector2D.hpp>
+ #include "./AsyncResource.hpp"
+ #include "../../color/Color.hpp"
---
+++ graphics/hyprgraphics/files/patch-src_image_formats_Svg.hpp
@@ -0,0 +1,10 @@
+--- src/image/formats/Svg.hpp.orig	2026-01-04 03:11:17 UTC
++++ src/image/formats/Svg.hpp
+@@ -4,6 +4,7 @@
+ #include <string>
+ #include <expected>
+ #include <png.h>
++#include <span>
+ #include <hyprutils/math/Vector2D.hpp>
+ 
+ namespace SVG {
