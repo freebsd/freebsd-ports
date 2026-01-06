d2d3b1b37fdc7511cc36935392237a404081f8ee
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2026-01-06 12:29:36 +0100

    Mk/bsd.gecko.mk: use bundled ICU for gecko >= 147
    Current Gecko requires ICU 78, and ports is at ICU 76 right now
    
    (cherry picked from commit ff4ef0c3c8f899610152a7e9ebd8d1d2a9e3a5c1)

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -115,7 +115,11 @@ RUSTFLAGS+=	${CFLAGS:M-mcpu=*:S/-mcpu=/-C target-cpu=/}
 .    endif
 
 # Standard depends
-_ALL_DEPENDS=	av1 event ffi graphite harfbuzz icu jpeg nspr nss png pixman sqlite vpx webp
+_ALL_DEPENDS=	av1 event ffi graphite harfbuzz jpeg nspr nss png pixman sqlite vpx webp
+# from 147 on, gecko needs ICU 78, but ports only has ICU 76 (2025-12-28)
+.    if ${MOZILLA_VER:R:R} < 147
+_ALL_DEPENDS+=	icu
+.    endif
 
 .    if exists(${FILESDIR}/patch-bug1559213)
 av1_LIB_DEPENDS=	libaom.so:multimedia/aom libdav1d.so:multimedia/dav1d
