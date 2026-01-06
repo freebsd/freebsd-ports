d5c4a9a0b84708293793573c8d0386e6f274c3bf
Dan Langille <dvl@FreeBSD.org>
2026-01-06 13:34:09 +0000

    net/syncthing: Update to 2.0.13
    Also updated the patch for the "work around multicast issue" - upstream
    has updated their code slight, but it still doesn't match the patch from
    2021. see 57a3995a942c59fa51caa39552b290243e957613

--- net/syncthing/files/patch-syncthing_lib_beacon_multicast.go
+++ net/syncthing/files/patch-syncthing_lib_beacon_multicast.go
@@ -1,12 +1,11 @@
---- syncthing/lib/beacon/multicast.go.orig	2021-07-06 12:51:32 UTC
+--- syncthing/lib/beacon/multicast.go.orig	2026-01-06 13:20:06 UTC
 +++ syncthing/lib/beacon/multicast.go
-@@ -126,6 +126,9 @@ func readMulticasts(ctx context.Context, outbox chan<-
+@@ -129,7 +129,7 @@ func readMulticasts(ctx context.Context, outbox chan<-
  	pconn := ipv6.NewPacketConn(conn)
  	joined := 0
  	for _, intf := range intfs {
+-		if intf.Flags&net.FlagMulticast == 0 {
 +		if intf.Flags&net.FlagUp == 0 || intf.Flags&net.FlagMulticast == 0 || intf.Name == "ipfw0" {
-+			continue
-+		}
- 		err := pconn.JoinGroup(&intf, &net.UDPAddr{IP: gaddr.IP})
- 		if err != nil {
- 			l.Debugln("IPv6 join", intf.Name, "failed:", err)
+ 			slog.DebugContext(ctx, "Not joining multicast group on non-multicast interface", "name", intf.Name, slog.String("flags", intf.Flags.String()))
+ 			continue
+ 		}
