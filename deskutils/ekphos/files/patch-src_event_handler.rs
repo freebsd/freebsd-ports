de3c1801d9601bb4c499debeea8d704d148e3a96
Nicola Vitale <nivit@FreeBSD.org>
2026-01-07 10:46:21 +0100

    deskutils/ekphos: Update to 0.15.0
    ChangeLog:	https://github.com/hanebox/ekphos/releases/tag/v0.15.0
    Reported by:	han <notifications@github.com>

--- deskutils/ekphos/files/patch-src_event_handler.rs
+++ deskutils/ekphos/files/patch-src_event_handler.rs
@@ -1,7 +1,7 @@
---- src/event/handler.rs.orig	2025-12-31 15:47:52 UTC
+--- src/event/handler.rs.orig	2026-01-06 22:24:18 UTC
 +++ src/event/handler.rs
-@@ -226,7 +226,7 @@ fn handle_mouse_event(app: &mut App, mouse: crossterm:
-                         if let Some(url) = app.find_clicked_link(idx, mouse_x, app.content_area.x) {
+@@ -236,7 +236,7 @@ fn handle_mouse_event(app: &mut App, mouse: crossterm:
+                         else if let Some(url) = app.find_clicked_link(idx, mouse_x, app.content_area.x) {
                              #[cfg(target_os = "macos")]
                              let _ = std::process::Command::new("open").arg(&url).spawn();
 -                            #[cfg(target_os = "linux")]
@@ -9,21 +9,12 @@
                              let _ = std::process::Command::new("xdg-open").arg(&url).spawn();
                              #[cfg(target_os = "windows")]
                              let _ = std::process::Command::new("cmd").args(["/c", "start", "", &url]).spawn();
-@@ -245,7 +245,7 @@ fn handle_mouse_event(app: &mut App, mouse: crossterm:
-                             if should_open {
+@@ -259,7 +259,7 @@ fn handle_mouse_event(app: &mut App, mouse: crossterm:
+                             if let Some(open_path) = open_path {
                                  #[cfg(target_os = "macos")]
-                                 let _ = std::process::Command::new("open").arg(path).spawn();
+                                 let _ = std::process::Command::new("open").arg(&open_path).spawn();
 -                                #[cfg(target_os = "linux")]
 +                                #[cfg(any(target_os = "linux", target_os = "freebsd"))]
-                                 let _ = std::process::Command::new("xdg-open").arg(path).spawn();
+                                 let _ = std::process::Command::new("xdg-open").arg(&open_path).spawn();
                                  #[cfg(target_os = "windows")]
-                                 let _ = std::process::Command::new("cmd").args(["/c", "start", "", path]).spawn();
-@@ -1609,7 +1609,7 @@ fn handle_normal_mode(app: &mut App, key: crossterm::e
-                         crate::app::LinkInfo::Markdown { url, .. } => {
-                             #[cfg(target_os = "macos")]
-                             let _ = std::process::Command::new("open").arg(&url).spawn();
--                            #[cfg(target_os = "linux")]
-+                            #[cfg(any(target_os = "linux", target_os = "freebsd"))]
-                             let _ = std::process::Command::new("xdg-open").arg(&url).spawn();
-                             #[cfg(target_os = "windows")]
-                             let _ = std::process::Command::new("cmd").args(["/c", "start", "", &url]).spawn();
+                                 let _ = std::process::Command::new("cmd").args(["/c", "start", "", &open_path]).spawn();
