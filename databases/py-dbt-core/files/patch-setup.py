68ab564d21e72ca74e13f1a4ab33c58da94e9b45
Matthew Seaman <matthew@FreeBSD.org>
2026-01-08 11:12:14 +0000

    databases/py-dbt-core: chase update to py-pathspec
    Remove upper version limit on py-pathspec dependency now that
    py-pathspec has reached version 1.0.2
    
    PR:		292268
    Approved by:	Pat Maddox (maintainer)

--- databases/py-dbt-core/files/patch-setup.py
+++ databases/py-dbt-core/files/patch-setup.py
@@ -1,10 +1,12 @@
 --- setup.py.orig	2024-03-28 03:30:09 UTC
 +++ setup.py
-@@ -66,9 +66,9 @@ setup(
+@@ -65,10 +65,10 @@ setup(
+         # These packages are major-version-0. Keep upper bounds on upcoming minor versions (which could have breaking changes)
          # and check compatibility / bump in each new minor version of dbt-core.
          "colorama>=0.3.9,<0.5",
-         "pathspec>=0.9,<0.12",
+-        "pathspec>=0.9,<0.12",
 -        "isodate>=0.6,<0.7",
++        "pathspec>=0.9",
 +        "isodate>=0.6",
          # ----
 -        "sqlparse>=0.2.3,<0.5",
