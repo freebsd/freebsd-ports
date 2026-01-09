f9aaa3ec787677b74751e801e5828ed3de5c89ae
Piotr Kubaj <pkubaj@FreeBSD.org>
2026-01-09 11:24:16 +0100

    misc/bedrust: fix build on powerpc64
    Little-endian is misdetected:
      /wrkdirs/usr/ports/misc/bedrust/work/bedrust-0.8.8/cargo-crates/aws-lc-sys-0.27.1/aws-lc/crypto/fipsmodule/sha/sha1-altivec.c:107:5: error: call to undeclared function 'vec_vsx_ld'; ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration]
        107 |     vec_vsx_ld(0, (const unsigned char*) data);
            |     ^
      /wrkdirs/usr/ports/misc/bedrust/work/bedrust-0.8.8/cargo-crates/aws-lc-sys-0.27.1/aws-lc/crypto/fipsmodule/sha/sha1-altivec.c:106:30: error: initializing '__vector unsigned char const' (vector of 16 'unsigned char' values) with an expression of incompatible type 'int'
        106 |   const vector unsigned char unaligned_data =
            |                              ^
        107 |     vec_vsx_ld(0, (const unsigned char*) data);
            |     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      2 errors generated.

---
+++ misc/bedrust/files/patch-cargo-crates_aws-lc-sys-0.27.1_aws-lc_include_openssl_target.h
@@ -0,0 +1,20 @@
+--- cargo-crates/aws-lc-sys-0.27.1/aws-lc/include/openssl/target.h.orig	2025-09-03 10:05:29 UTC
++++ cargo-crates/aws-lc-sys-0.27.1/aws-lc/include/openssl/target.h
+@@ -34,14 +34,14 @@
+ #elif defined(__ARMEL__) || defined(_M_ARM)
+ #define OPENSSL_32_BIT
+ #define OPENSSL_ARM
+-#elif (defined(__PPC64__) || defined(__powerpc64__)) && defined(_LITTLE_ENDIAN)
++#elif (defined(__PPC64__) || defined(__powerpc64__)) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+ #define OPENSSL_64_BIT
+ #define OPENSSL_PPC64LE
+-#elif (defined(__PPC64__) || defined(__powerpc64__)) && defined(_BIG_ENDIAN)
++#elif (defined(__PPC64__) || defined(__powerpc64__)) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+ #define OPENSSL_64_BIT
+ #define OPENSSL_PPC64BE
+ #define OPENSSL_BIG_ENDIAN
+-#elif (defined(__PPC__) || defined(__powerpc__)) && defined(_BIG_ENDIAN)
++#elif (defined(__PPC__) || defined(__powerpc__)) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+ #define OPENSSL_32_BIT
+ #define OPENSSL_PPC32BE
+ #define OPENSSL_BIG_ENDIAN
