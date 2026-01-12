24ff9b2cb480496a550fde814d18ada2f396ff14
Michael Gmelin <grembo@FreeBSD.org>
2026-01-12 10:28:55 +0100

    devel/phabricator: Update to 20231113

--- devel/phabricator/files/patch-src_applications_files_builtin_PhabricatorFilesComposeAvatarBuiltinFile.php
+++ devel/phabricator/files/patch-src_applications_files_builtin_PhabricatorFilesComposeAvatarBuiltinFile.php
@@ -1,10 +1,18 @@
---- src/applications/files/builtin/PhabricatorFilesComposeAvatarBuiltinFile.php.orig	2021-12-18 19:11:09 UTC
+--- src/applications/files/builtin/PhabricatorFilesComposeAvatarBuiltinFile.php.orig	2023-11-13 19:04:04 UTC
 +++ src/applications/files/builtin/PhabricatorFilesComposeAvatarBuiltinFile.php
-@@ -188,7 +188,7 @@ final class PhabricatorFilesComposeAvatarBuiltinFile
-     $g = $rgba[1];
-     $b = $rgba[2];
-     $a = $rgba[3];
+@@ -184,11 +184,11 @@ final class PhabricatorFilesComposeAvatarBuiltinFile
+   }
+ 
+   private static function rgba2gd(array $rgba) {
+-    $r = (int)$rgba[0];
+-    $g = (int)$rgba[1];
+-    $b = (int)$rgba[2];
+-    $a = (int)$rgba[3];
 -    $a = (1 - $a) * 255;
++    $r = $rgba[0];
++    $g = $rgba[1];
++    $b = $rgba[2];
++    $a = $rgba[3];
 +    $a = round(((1 - $a) * 255), 0);
      return ($a << 24) | ($r << 16) | ($g << 8) | $b;
    }
