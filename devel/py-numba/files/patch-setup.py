b44750645266648d36d51ad7a47ef7acde8eacd8
Max Brazhnikov <makc@FreeBSD.org>
2026-01-17 20:10:39 +0300

    devel/py-numba: Update to 0.63.1

--- devel/py-numba/files/patch-setup.py
+++ devel/py-numba/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2025-01-16 17:19:10 UTC
+--- setup.py.orig	2025-12-09 23:42:43 UTC
 +++ setup.py
-@@ -21,7 +21,7 @@ max_python_version = "3.14"  # exclusive
+@@ -21,7 +21,7 @@ max_python_version = "3.15"  # exclusive
  
  min_python_version = "3.10"
- max_python_version = "3.14"  # exclusive
+ max_python_version = "3.15"  # exclusive
 -min_numpy_build_version = "2.0.0rc1"
-+min_numpy_build_version = "1.24"
- min_numpy_run_version = "1.24"
- max_numpy_run_version = "2.2"
- min_llvmlite_version = "0.44.0dev0"
++min_numpy_build_version = "1.22"
+ min_numpy_run_version = "1.22"
+ max_numpy_run_version = "2.4"
+ min_llvmlite_version = "0.46.0dev0"
