9711d6144bf51fcd52f2749c1cfb6fbe1ddbf899
Robert Clausecker <fuz@FreeBSD.org>
2026-01-20 11:41:38 +0100

    multimedia/ccextractor: update to 0.96.5
     - rust now mandatory
     - GUI rewritten in flutter, which we do not support unfortunately
     - hard subtitle extraction works now
    
    Changelog: https://github.com/CCExtractor/ccextractor/releases/tag/v0.96.5
    Changelog: https://github.com/CCExtractor/ccextractor/releases/tag/v0.96.4
    Changelog: https://github.com/CCExtractor/ccextractor/releases/tag/v0.96.3
    Changelog: https://github.com/CCExtractor/ccextractor/releases/tag/v0.96.2
    Changelog: https://github.com/CCExtractor/ccextractor/releases/tag/v0.96.1
    Changelog: https://github.com/CCExtractor/ccextractor/releases/tag/v0.96

--- multimedia/ccextractor/files/patch-src_lib__ccx_configuration.c
+++ multimedia/ccextractor/files/patch-src_lib__ccx_configuration.c
@@ -1,4 +1,4 @@
---- src/lib_ccx/configuration.c.orig	2025-10-28 13:57:45 UTC
+--- src/lib_ccx/configuration.c.orig	2026-01-20 09:45:22 UTC
 +++ src/lib_ccx/configuration.c
 @@ -91,7 +91,7 @@ static void parse_file(FILE *f, struct ccx_s_options *
  static void parse_file(FILE *f, struct ccx_s_options *opt)
--- multimedia/ccextractor/files/patch-src_lib__ccx_params.c
+++ multimedia/ccextractor/files/patch-src_lib__ccx_params.c
@@ -1,18 +1,6 @@
---- src/lib_ccx/params.c.orig	2021-12-15 17:05:37 UTC
+--- src/lib_ccx/params.c.orig	2026-01-20 10:01:34 UTC
 +++ src/lib_ccx/params.c
-@@ -1,7 +1,7 @@
--#include "png.h"
--#include "protobuf-c.h"
--#include "zlib.h"
--#include "gpac/version.h"
-+#include <png.h>
-+#include <protobuf-c.h>
-+#include <zlib.h>
-+#include <gpac/version.h>
- #include "lib_ccx.h"
- #include "ccx_common_option.h"
- #include "utility.h"
-@@ -10,10 +10,10 @@
+@@ -10,7 +10,7 @@
  #include "ccx_common_common.h"
  #include "ccx_decoders_708.h"
  #include "compile_info.h"
@@ -20,8 +8,33 @@
 +#include <sha256.h>
  #include <string.h>
  #include <stdio.h>
--#include <utf8proc/utf8proc.h>
-+#include <utf8proc.h>
+ #if __has_include(<utf8proc.h>)
+@@ -34,8 +34,8 @@
+ #define DEFAULT_FONT_PATH "/System/Library/Fonts/Helvetica.ttc"
+ #define DEFAULT_FONT_PATH_ITALICS "/System/Library/Fonts/Helvetica-Oblique.ttf"
+ #else // Assume Linux
+-#define DEFAULT_FONT_PATH "/usr/share/fonts/truetype/noto/NotoSans-Regular.ttf"
+-#define DEFAULT_FONT_PATH_ITALICS "/usr/share/fonts/truetype/noto/NotoSans-Italic.ttf"
++#define DEFAULT_FONT_PATH "%%LOCALBASE%%/share/fonts/noto/NotoSans-Regular.ttf"
++#define DEFAULT_FONT_PATH_ITALICS "%%LOCALBASE%%/share/fonts/noto/NotoSans-Italic.ttf"
+ #endif
  
- #ifdef ENABLE_OCR
- #include <tesseract/capi.h>
+ extern void ccxr_init_logger();
+@@ -735,7 +735,7 @@ char *calculateSHA256(char *location)
+ 	int size_read, bytes_read, fh = 0;
+ 	SHA256_CTX ctx256;
+ 
+-	CC_SHA256_Init(&ctx256);
++	SHA256_Init(&ctx256);
+ 
+ #ifdef _WIN32
+ 	fh = OPEN(location, O_RDONLY | O_BINARY);
+@@ -751,7 +751,7 @@ char *calculateSHA256(char *location)
+ 	while ((bytes_read = read(fh, sha256_buf, 16384)) > 0)
+ 	{
+ 		size_read += bytes_read;
+-		CC_SHA256_Update(&ctx256, (unsigned char *)sha256_buf, bytes_read);
++		SHA256_Update(&ctx256, (unsigned char *)sha256_buf, bytes_read);
+ 	}
+ 	close(fh);
+ 	SHA256_End(&ctx256, sha256_buf);
