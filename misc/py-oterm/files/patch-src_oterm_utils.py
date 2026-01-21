c6ddaea030793b591097560d89c6c458af65a8d7
Nicola Vitale <nivit@FreeBSD.org>
2026-01-21 15:22:55 +0100

    misc/py-oterm: Update to 0.14.7
    - Update list of run-dependencies
    
    ChangeLog:	https://github.com/ggozad/oterm/releases/tag/0.14.7
    Reported by:	Yiorgis Gozadinos <notifications@github.com>

--- misc/py-oterm/files/patch-src_oterm_utils.py
+++ misc/py-oterm/files/patch-src_oterm_utils.py
@@ -1,22 +1,23 @@
 --- src/oterm/utils.py.orig	2020-02-02 00:00:00 UTC
 +++ src/oterm/utils.py
-@@ -1,4 +1,4 @@
--import sys
+@@ -1,6 +1,7 @@ import sys
+ import asyncio
+ import os
+ import sys
 +import platform
+ from collections.abc import Callable
+ from functools import wraps
  from importlib import metadata
- from pathlib import Path
- 
-@@ -59,12 +59,13 @@ def get_default_data_dir() -> Path:
-     home = Path.home()
+@@ -118,6 +119,7 @@ def get_default_data_dir() -> Path:
  
      system_paths = {
--        "win32": home / "AppData/Roaming/oterm",
--        "linux": home / ".local/share/oterm",
--        "darwin": home / "Library/Application Support/oterm",
-+        "Darwin": home / "Library/Application Support/oterm",
-+        "FreeBSD": home / ".local/share/oterm",
-+        "Linux": home / ".local/share/oterm",
-+        "Windows": home / "AppData/Roaming/oterm",
+         "win32": home / "AppData/Roaming/oterm",
++        "FreeBSD": Path(os.getenv("XDG_DATA_HOME") or Path(home / ".local/share")),
+         "linux": Path(os.getenv("XDG_DATA_HOME") or Path(home / ".local/share"))
+         / "oterm",
+         "darwin": Path(
+@@ -126,7 +128,7 @@ def get_default_data_dir() -> Path:
+         / "oterm",
      }
  
 -    data_path = system_paths[sys.platform]
