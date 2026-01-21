c906445db23551256c9686cf7e1511bc06389ef9
yzrh <yzrh@noema.org>
2026-01-21 19:49:58 +0300

    multimedia/kodi: Update 21.3 => 22.0a2 for unbreak with ffmpeg 8
    Changelogs:
    https://github.com/xbmc/xbmc/releases/tag/22.0a1-Piers
    https://github.com/xbmc/xbmc/releases/tag/22.0a2-Piers
    
    PR:	292609
    MFH:	2026Q1

--- multimedia/kodi/files/patch-cmake_modules_FindCrossGUID.cmake
+++ multimedia/kodi/files/patch-cmake_modules_FindCrossGUID.cmake
@@ -1,20 +1,11 @@
---- cmake/modules/FindCrossGUID.cmake.orig	2024-03-04 21:39:38 UTC
+--- cmake/modules/FindCrossGUID.cmake.orig	2026-01-19 17:28:39 UTC
 +++ cmake/modules/FindCrossGUID.cmake
-@@ -45,7 +45,7 @@ if(NOT TARGET CrossGUID::CrossGUID)
-       set(CROSSGUID_VERSION ${PC_CROSSGUID_VERSION})
-     endif()
+@@ -76,7 +76,7 @@ if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAM
+       add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
  
--    find_path(CROSSGUID_INCLUDE_DIR NAMES crossguid/guid.hpp guid.h
-+    find_path(CROSSGUID_INCLUDE_DIR NAMES guid.hpp guid.h
-                                     HINTS ${DEPENDS_PATH}/include ${PC_CROSSGUID_INCLUDEDIR}
-                                     ${${CORE_PLATFORM_LC}_SEARCH_CONFIG}
-                                     NO_CACHE)
-@@ -59,7 +59,7 @@ if(NOT TARGET CrossGUID::CrossGUID)
-                                          NO_CACHE)
- 
-     # NEW_CROSSGUID >= 0.2.0 release
--    if(EXISTS "${CROSSGUID_INCLUDE_DIR}/crossguid/guid.hpp")
-+    if(EXISTS "${CROSSGUID_INCLUDE_DIR}/guid.hpp")
-       list(APPEND _crossguid_definitions HAVE_NEW_CROSSGUID)
-     endif()
-   endif()
+       # NEW_CROSSGUID >= 0.2.0 release
+-      if(EXISTS "${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR}/crossguid/guid.hpp")
++      if(EXISTS "${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR}/guid.hpp")
+         list(APPEND ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_COMPILE_DEFINITIONS HAVE_NEW_CROSSGUID)
+       endif()
+     else()
