8a6ffe8081c9a5d61f5d280b1578ed72c19f6303
Robert Clausecker <fuz@FreeBSD.org>
2026-01-23 21:50:32 +0100

    devel/fnc: unbundle sqlite3
    PR:		290972
    Approved by:	ashish (maintainer)
    MFH:		2026Q1

--- devel/fnc/files/patch-fnc.bld.mk
+++ devel/fnc/files/patch-fnc.bld.mk
@@ -1,4 +1,4 @@
---- fnc.bld.mk.orig	2024-09-25 08:28:03 UTC
+--- fnc.bld.mk.orig	2026-01-23 20:37:32 UTC
 +++ fnc.bld.mk
 @@ -4,7 +4,7 @@ CC ?=		cc
  
@@ -9,6 +9,36 @@
  MANDIR ?=	/share/man
  VERSION ?=	%%VERSION%%
  HASH !=		cut -f 1 manifest.uuid
+@@ -47,7 +47,7 @@ FNC_CFLAGS =	${CFLAGS} -Wstrict-prototypes -Wmissing-p
+ 		-Wunused-variable -I./lib -I./include -I/usr/include/ncursesw \
+ 		-DFNC_VERSION=${VERSION} -DFNC_HASH=${HASH} -DFNC_DATE="${DATE}"
+ 
+-FNC_LDFLAGS =	${LDFLAGS} -lm -lutil -lz -lpthread
++FNC_LDFLAGS =	${LDFLAGS} -lm -lutil -lz -lpthread -lsqlite3
+ 
+ # Compile-time checks and runtime protection mechanisms from the compiler
+ # hardening document: https://best.openssf.org/Compiler-Hardening-Guides
+@@ -63,7 +63,7 @@ all: bin
+ 
+ all: bin
+ 
+-bin: lib/sqlite3.o lib/libfossil.o src/fnc.o src/fnc
++bin: lib/libfossil.o src/fnc.o src/fnc
+ 
+ lib/sqlite3.o: lib/sqlite3.c lib/sqlite3.h
+ 	${CC} ${SQLITE_CFLAGS} -c $< -o $@
+@@ -85,9 +85,9 @@ src/fnc: src/fnc.o src/diff.o src/error.o compat/reall
+ 	${CC} ${FNC_CFLAGS} -c $< -o $@
+ 
+ src/fnc: src/fnc.o src/diff.o src/error.o compat/reallocarray.o \
+-	lib/libfossil.o lib/sqlite3.o fnc.bld.mk
++	lib/libfossil.o fnc.bld.mk
+ 	${CC} -o $@ src/fnc.o src/diff.o src/error.o compat/reallocarray.o \
+-	lib/libfossil.o lib/sqlite3.o ${FNC_LDFLAGS}
++	lib/libfossil.o ${FNC_LDFLAGS}
+ 
+ dev:
+ 	${MAKE} GOHARD=1
 @@ -114,10 +114,10 @@ install:
  	@echo "generated compile_commands.json"
  
