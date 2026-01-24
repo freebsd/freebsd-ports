64d589c6173ee81267620327952c676e24d249c4
Gleb Popov <arrowd@FreeBSD.org>
2026-01-24 21:46:02 +0300

    devel/libabigail: Update to 2.9
    PR:		292701
    Approved by:	adridg (maintainer)

--- devel/libabigail/files/patch-tools_abisym.cc
+++ devel/libabigail/files/patch-tools_abisym.cc
@@ -1,8 +1,8 @@
---- tools/abisym.cc.orig	2021-03-07 00:17:35 UTC
+--- tools/abisym.cc.orig	2025-01-13 11:40:45 UTC
 +++ tools/abisym.cc
-@@ -26,6 +26,7 @@
- /// in its symbol tables and report what it sees.
+@@ -12,6 +12,7 @@
  
+ #include <libgen.h>
  #include <elf.h>
 +#include <libgen.h>
  #include <cstring>
