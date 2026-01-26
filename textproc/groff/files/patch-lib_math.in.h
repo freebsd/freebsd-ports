809adcb6a0a8f387589a848a1ff1ef9ef41cc86a
Vladimir Druzenko <vvd@FreeBSD.org>
2026-01-26 19:57:59 +0300

    textproc/groff: Improve port (non-functional changes)
    - Replace PORTVERSION with DISTVERSION.
    - Replace spaces with tab in PORTREVISION line.
    - Replace RUN_DEPENDS:= with RUN_DEPENDS= - unnecessary ":".
    - Remove GNU_CONFIGURE_MANPREFIX - this is default value.
    - Refresh local patches.
    - Sort plist.
    
    PR:	292630

--- textproc/groff/files/patch-lib_math.in.h
+++ textproc/groff/files/patch-lib_math.in.h
@@ -1,6 +1,6 @@
---- lib/math.in.h.orig	2018-12-23 14:34:15 UTC
+--- lib/math.in.h.orig	2023-07-05 20:58:27 UTC
 +++ lib/math.in.h
-@@ -34,7 +34,7 @@
+@@ -54,7 +54,7 @@
  #endif
  
  #ifndef _GL_INLINE_HEADER_BEGIN
