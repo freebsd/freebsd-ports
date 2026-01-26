d6c4b060086d7b6a732edc4ac4753db0a1e800c0
Olivier Duchateau <duchateau.olivier@gmail.com>
2026-01-25 21:14:48 -0800

    archivers/file-roller: update to 44.6
    Update file-roller to version 44.6.
    
    Trim LIB_DEPENDS. (truckman)
    
    PR:		290387
    (cherry picked from commit 0a08fff16207f5e89a1b7dd2158e175eb376ca5a)

--- archivers/file-roller/files/patch-src_fr-command-tar.c
+++ archivers/file-roller/files/patch-src_fr-command-tar.c
@@ -1,12 +1,39 @@
---- src/fr-command-tar.c.orig	Sun Oct 15 12:33:56 2006
-+++ src/fr-command-tar.c	Mon Oct 16 19:18:03 2006
-@@ -231,6 +231,9 @@
- #if defined (__SVR4) && defined (__sun)
- 	if (g_file_test ("/usr/sfw/bin/gtar", G_FILE_TEST_IS_EXECUTABLE))
- 		command = g_strdup ("/usr/sfw/bin/gtar");
-+#elif defined(__FreeBSD__)
-+	if (g_file_test ("%%LOCALBASE%%/bin/gtar", G_FILE_TEST_IS_EXECUTABLE))
-+		command = g_strdup ("%%LOCALBASE%%/bin/gtar");
- #endif
- 	if (command != NULL)
- 		fr_process_begin_command (comm->process, command);
+--- src/fr-command-tar.c.orig	2025-10-19 06:50:04 UTC
++++ src/fr-command-tar.c
+@@ -275,8 +275,6 @@ fr_command_tar_list (FrCommand *comm)
+ 	fr_process_set_out_line_func (comm->process, process_line, comm);
+ 
+ 	begin_tar_command (comm);
+-	fr_process_add_arg (comm->process, "--force-local");
+-	fr_process_add_arg (comm->process, "--no-wildcards");
+ 	fr_process_add_arg (comm->process, "-tvf");
+ 	fr_process_add_arg (comm->process, comm->filename);
+ 	add_compress_arg (comm);
+@@ -345,9 +343,6 @@ fr_command_tar_add (FrCommand  *comm,
+ 				      comm);
+ 
+ 	begin_tar_command (comm);
+-	fr_process_add_arg (comm->process, "--force-local");
+-	fr_process_add_arg (comm->process, "--no-recursion");
+-	fr_process_add_arg (comm->process, "--no-wildcards");
+ 	fr_process_add_arg (comm->process, "-v");
+ 	fr_process_add_arg (comm->process, "-p");
+ 	if (follow_links)
+@@ -419,8 +414,6 @@ fr_command_tar_delete (FrCommand  *comm,
+ 
+ 	begin_tar_command (comm);
+ 	fr_process_set_begin_func (comm->process, begin_func__delete, comm);
+-	fr_process_add_arg (comm->process, "--force-local");
+-	fr_process_add_arg (comm->process, "--no-wildcards");
+ 	fr_process_add_arg (comm->process, "-v");
+ 	fr_process_add_arg (comm->process, "--delete");
+ 	fr_process_add_arg (comm->process, "-f");
+@@ -466,8 +459,6 @@ fr_command_tar_extract (FrCommand  *comm,
+ 				      comm);
+ 
+ 	begin_tar_command (comm);
+-	fr_process_add_arg (comm->process, "--force-local");
+-	fr_process_add_arg (comm->process, "--no-wildcards");
+ 	fr_process_add_arg (comm->process, "-v");
+ 	fr_process_add_arg (comm->process, "-p");
+ 
