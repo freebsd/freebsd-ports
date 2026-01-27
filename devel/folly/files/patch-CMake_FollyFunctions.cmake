cecc20df89c9a6240d0204aaacfe272d301d93ab
Yuri Victorovich <yuri@FreeBSD.org>
2026-01-26 21:22:20 -0800

    devel/folly: update 2026.01.19.00 → 2026.01.26.00

--- devel/folly/files/patch-CMake_FollyFunctions.cmake
+++ devel/folly/files/patch-CMake_FollyFunctions.cmake
@@ -1,11 +1,11 @@
---- CMake/FollyFunctions.cmake.orig	2026-01-19 20:28:13 UTC
+--- CMake/FollyFunctions.cmake.orig	2026-01-26 12:08:28 UTC
 +++ CMake/FollyFunctions.cmake
-@@ -476,7 +476,7 @@ function(folly_add_library)
-   endif()
+@@ -587,7 +587,7 @@ function(folly_add_library)
+     )
+   else()
+     # For static builds: create STATIC library (individual .a file for granular linking)
+-    add_library(${_target_name} STATIC $<TARGET_OBJECTS:${_obj_target}>)
++    add_library(${_target_name} $<TARGET_OBJECTS:${_obj_target}>)
+     set_property(TARGET ${_target_name} PROPERTY VERSION ${PACKAGE_VERSION})
  
-   # 2. Create STATIC library (individual .a file for granular linking)
--  add_library(${_target_name} STATIC $<TARGET_OBJECTS:${_obj_target}>)
-+  add_library(${_target_name} $<TARGET_OBJECTS:${_obj_target}>)
-   set_property(TARGET ${_target_name} PROPERTY VERSION ${PACKAGE_VERSION})
- 
-   target_include_directories(${_target_name}
+     target_include_directories(${_target_name}
