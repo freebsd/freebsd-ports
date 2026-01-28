171b4e2640f88291c62681bc6ee9c61cf27d73ec
Robert Nagy <rnagy@FreeBSD.org>
2026-01-28 13:37:25 +0100

    www/chromium: update to 144.0.7559.109
    Security:	https://vuxml.freebsd.org/freebsd/409d70ab-fc23-11f0-85c5-a8a1599412c6.html

--- www/chromium/files/patch-chrome_browser_ui_views_location__bar_location__bar__view.cc
+++ www/chromium/files/patch-chrome_browser_ui_views_location__bar_location__bar__view.cc
@@ -1,6 +1,6 @@
---- chrome/browser/ui/views/location_bar/location_bar_view.cc.orig	2026-01-14 08:33:23 UTC
+--- chrome/browser/ui/views/location_bar/location_bar_view.cc.orig	2026-01-28 08:17:57 UTC
 +++ chrome/browser/ui/views/location_bar/location_bar_view.cc
-@@ -1785,7 +1785,7 @@ void LocationBarView::OnPopupStateChanged(OmniboxPopup
+@@ -1777,7 +1777,7 @@ void LocationBarView::OnPopupStateChanged(OmniboxPopup
                       weak_factory_.GetWeakPtr()),
        base::Milliseconds(100));
  
