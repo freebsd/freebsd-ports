ad610cf7b1fe65d676b6f4a4001ec13aea05a2fd
Dimitry Andric <dim@FreeBSD.org>
2026-02-01 20:53:36 +0100

    audio/rubberband: fix build with libc++ 21
    With libc++ 21 audio/rubberband fails to build, with errors similar to:
    
        In file included from ../src/common/mathmisc.cpp:24:
        ../src/common/mathmisc.h:58:1: error: unknown type name 'size_t'; did you mean 'std::size_t'?
           58 | size_t roundUp(size_t value); // to nearest power of two
              | ^~~~~~
              | std::size_t
        /usr/include/c++/v1/__cstddef/size_t.h:20:7: note: 'std::size_t' declared here
           20 | using size_t = decltype(sizeof(int));
              |       ^
    
    This is because mathmisc.h depended on <stddef.h> being transitively
    included, which is no longer the case. Add #include <stddef.h> to fix
    the problem.
    
    PR:		292868
    Approved by:	acm (maintainer)
    MFH:		2026Q1
    
    (cherry picked from commit 78992d889cf64999b2ff0b82d6940eb7c9edb12d)

---
+++ audio/rubberband/files/patch-src_common_mathmisc.h
@@ -0,0 +1,10 @@
+--- src/common/mathmisc.h.orig	2024-10-25 16:26:08 UTC
++++ src/common/mathmisc.h
+@@ -24,6 +24,7 @@
+ #ifndef RUBBERBAND_MATHMISC_H
+ #define RUBBERBAND_MATHMISC_H
+ 
++#include <stddef.h>
+ #include "sysutils.h"
+ 
+ #ifndef M_PI
