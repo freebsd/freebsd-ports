ca14e5d170a5e96d0b625839dc28c72b7bced7d0
Dustin Marquess <jailbird@fdf.net>
2026-02-02 04:26:00 +0100

    www/elinks: Update to 0.19.0
    Use gettext-runtime and localbase:ldflags.
    Add TEST_TARGET and remove unneeded TEST_USES.
    Add LD_FLAGS.
    Pet portlint/portfmt.
    
    Changelog: https://github.com/rkd77/elinks/releases/tag/v0.19.0
    
    PR:		291966
    Approved by:	submitter is maintainer

--- www/elinks/files/patch-src_document_renderer.h
+++ www/elinks/files/patch-src_document_renderer.h
@@ -1,4 +1,4 @@
---- src/document/renderer.h.orig	2022-12-25 18:17:02 UTC
+--- src/document/renderer.h.orig	2025-12-25 11:06:07 UTC
 +++ src/document/renderer.h
 @@ -16,7 +16,7 @@ void render_document_frames(struct session *ses, int n
  
@@ -6,6 +6,6 @@
  void render_document_frames(struct session *ses, int no_cache);
 -struct conv_table *get_convert_table(char *head, int to_cp, int default_cp, int *from_cp, enum cp_status *cp_status, int ignore_server_cp);
 +struct conv_table *get_convert_table(const char *head, int to_cp, int default_cp, int *from_cp, enum cp_status *cp_status, int ignore_server_cp);
+ void compress_empty_lines(struct document *document);
  void sort_links(struct document *document);
  
- #ifdef __cplusplus
