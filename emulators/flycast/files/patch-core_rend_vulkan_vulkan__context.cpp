82b0a20003e8d7185adbef02414614545bfc8207
Stefan Schlosser <bsdcode@disroot.org>
2026-02-02 20:30:23 +0300

    emulators/flycast: Update 2.5 => 2.6
    Changelog:
    https://github.com/flyinghead/flycast/releases/tag/v2.6
    
    * Update git submodules to latest release versions and reorder them into
      alphabetical sequence.
    * Remove set-include in vulkan_context.cpp patch because it's now in
      upstream.
    * Make slight simplification in post-install-DOCS-on.
    
    PR:	292421

--- emulators/flycast/files/patch-core_rend_vulkan_vulkan__context.cpp
+++ emulators/flycast/files/patch-core_rend_vulkan_vulkan__context.cpp
@@ -1,14 +1,6 @@
---- core/rend/vulkan/vulkan_context.cpp.orig	2025-05-07 13:47:18 UTC
+--- core/rend/vulkan/vulkan_context.cpp.orig	2026-01-12 20:26:48 UTC
 +++ core/rend/vulkan/vulkan_context.cpp
-@@ -42,6 +42,7 @@ VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
- #endif
- 
- #include <memory>
-+#include <set>
- #include <vulkan/vulkan_format_traits.hpp>
- 
- void ReInitOSD();
-@@ -147,7 +148,7 @@ bool VulkanContext::InitInstance(const char** extensio
+@@ -148,7 +148,7 @@ bool VulkanContext::InitInstance(const char** extensio
  #if defined(__ANDROID__) && HOST_CPU == CPU_ARM64
  		vkGetInstanceProcAddr = loadVulkanDriver();
  #else
