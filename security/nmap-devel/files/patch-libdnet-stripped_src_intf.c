51ce0ab7e41419e37fbb0e6d10d246f3f95fc91a
Cy Schubert <cy@FreeBSD.org>
2026-02-02 16:16:13 -0800

    security/nmap-devel: Sync patches and build configs with security/nmap

--- security/nmap-devel/files/patch-libdnet-stripped_src_intf.c
+++ security/nmap-devel/files/patch-libdnet-stripped_src_intf.c
@@ -1,6 +1,6 @@
---- libdnet-stripped/src/intf.c.orig	2025-04-30 19:58:18 UTC
-+++ libdnet-stripped/src/intf.c
-@@ -178,7 +178,7 @@ intf_open(void)
+--- libdnet-stripped/src/intf.c.orig	2025-03-31 12:30:47.000000000 -0700
++++ libdnet-stripped/src/intf.c	2026-02-02 15:34:47.918928000 -0800
+@@ -178,7 +178,7 @@
  #if defined(SIOCGLIFCONF) || defined(SIOCGIFNETMASK_IN6) || defined(SIOCGIFNETMASK6)
  		if ((intf->fd6 = socket(AF_INET6, SOCK_DGRAM, 0)) < 0) {
  #  ifdef EPROTONOSUPPORT
