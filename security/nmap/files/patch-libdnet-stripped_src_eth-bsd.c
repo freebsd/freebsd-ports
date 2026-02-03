be8868737f7ba298396a5bbb94f4fa2b3a5e6efb
Cy Schubert <cy@FreeBSD.org>
2026-02-02 16:24:07 -0800

    security/nmap: Update to 7.98

--- security/nmap/files/patch-libdnet-stripped_src_eth-bsd.c
+++ security/nmap/files/patch-libdnet-stripped_src_eth-bsd.c
@@ -1,11 +1,11 @@
---- libdnet-stripped/src/eth-bsd.c.orig	2009-11-09 04:49:32 UTC
-+++ libdnet-stripped/src/eth-bsd.c
-@@ -45,7 +45,7 @@ eth_open(const char *device)
- 	int i;
+--- libdnet-stripped/src/eth-bsd.c.orig	2025-06-05 15:22:27.000000000 -0700
++++ libdnet-stripped/src/eth-bsd.c	2026-02-02 15:33:01.105641000 -0800
+@@ -46,7 +46,7 @@
  
  	if ((e = calloc(1, sizeof(*e))) != NULL) {
--		for (i = 0; i < 128; i++) {
-+		for (i = 0; i < 1024; i++) {
- 			snprintf(file, sizeof(file), "/dev/bpf%d", i);
+ 		char file[32] = "/dev/bpf";
+-		for (i = 0; i <= 128; i++) {
++		for (i = 0; i <= 1024; i++) {
  			/* This would be O_WRONLY, but Mac OS X 10.6 has a bug
  			   where that prevents other users of the interface
+ 			   from seeing incoming traffic, even in other
--- security/nmap/files/patch-libdnet-stripped_src_intf.c
+++ security/nmap/files/patch-libdnet-stripped_src_intf.c
@@ -1,11 +1,11 @@
---- libdnet-stripped/src/intf.c.orig	2016-07-27 20:11:27 UTC
-+++ libdnet-stripped/src/intf.c
-@@ -163,7 +163,7 @@ intf_open(void)
+--- libdnet-stripped/src/intf.c.orig	2025-03-31 12:30:47.000000000 -0700
++++ libdnet-stripped/src/intf.c	2026-02-02 15:34:47.918928000 -0800
+@@ -178,7 +178,7 @@
  #if defined(SIOCGLIFCONF) || defined(SIOCGIFNETMASK_IN6) || defined(SIOCGIFNETMASK6)
  		if ((intf->fd6 = socket(AF_INET6, SOCK_DGRAM, 0)) < 0) {
  #  ifdef EPROTONOSUPPORT
 -			if (errno != EPROTONOSUPPORT)
 +			if (errno != EPROTONOSUPPORT && errno != EAFNOSUPPORT)
- #  endif
+ #endif
  				return (intf_close(intf));
  		}
