2fc90a5f6119d7afec20f3364ca4253ed608a5d8
Hiroki Tagato <tagattie@FreeBSD.org>
2026-02-04 21:16:37 +0900

    x11-wm/hyprland: Update to 0.53.3
    Changelog:
    - https://github.com/hyprwm/Hyprland/releases/tag/v0.53.2
    - https://github.com/hyprwm/Hyprland/releases/tag/v0.53.3
    
    Reported by:	GitHub (watch releases)

--- x11-wm/hyprland/files/patch-hyprpm_src_core_PluginManager.cpp
+++ x11-wm/hyprland/files/patch-hyprpm_src_core_PluginManager.cpp
@@ -1,6 +1,6 @@
 Avoid hyprpm rebuilding Hyprland outside of ports
 
---- hyprpm/src/core/PluginManager.cpp.orig	2026-01-02 21:24:24 UTC
+--- hyprpm/src/core/PluginManager.cpp.orig	2026-01-24 20:04:36 UTC
 +++ hyprpm/src/core/PluginManager.cpp
 @@ -385,9 +385,6 @@ eHeadersErrors CPluginManager::headersValid() {
  eHeadersErrors CPluginManager::headersValid() {
@@ -10,5 +10,5 @@ Avoid hyprpm rebuilding Hyprland outside of ports
 -        return HEADERS_MISSING;
 -
      // find headers commit
-     const std::string& cmd     = std::format("PKG_CONFIG_PATH=\"{}/share/pkgconfig\" pkgconf --cflags --keep-system-cflags hyprland", DataState::getHeadersPath());
+     const std::string& cmd     = std::format("PKG_CONFIG_PATH='{}' pkgconf --cflags --keep-system-cflags hyprland", getPkgConfigPath());
      auto               headers = execAndGet(cmd);
