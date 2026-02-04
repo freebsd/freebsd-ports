b74c69274b28cf00fe9aab52edd66138507e1705
hard.egg0918 <hard.egg0918@fastmail.com>
2026-02-04 23:52:08 +0300

    net-mgmt/nagios4: Update 4.5.1 => 4.5.11, take maintainership
    Changelog:
    https://www.nagios.org/projects/nagios-core/4x/
    
    Improve port:
    - Replace PORTVERSION with DISTVERSION.
    - Parametrize nagios with ${PORTNAME}.
    - Replace CFLAGS, CPPFLAGS and LIBS with USES+=localbase.
    - Update CONFLICTS.
    - Use ${ETCDIR} instead of ${PREFIX}/etc/nagios.
    - Fix warnings from portclippy.
    - Fix install with non-default NAGIOSUSER/NAGIOSGROUP.
    - Split long line in post-patch.
    - Improve indents.
    - Regenerate patches with changed line numbers.
    
    PR:		292357
    Tested by:	Fabian Wenk <fabian@wenks.ch>
    MFH:		2026Q1
    Co-authored-by:	Vladimir Druzenko <vvd@FreeBSD.org>
    
    (cherry picked from commit 2a8608cad2d3f0d8f971c5d314141c902d573bf9)

--- net-mgmt/nagios4/files/patch-html_main.php
+++ net-mgmt/nagios4/files/patch-html_main.php
@@ -1,14 +1,16 @@
---- html/main.php.orig	2024-03-16 10:46:27.911029000 +0100
-+++ html/main.php	2024-03-16 10:48:12.929841000 +0100
-@@ -142,38 +142,6 @@ $this_year = '2024';
- </div>
+--- html/main.php.orig	2026-01-14 16:37:00 UTC
++++ html/main.php
+@@ -69,37 +69,6 @@ if ($theme != 'dark' && $theme != 'light') {
  
+ <body id="splashpage">
  
 -<div id="currentversioninfo">
--	<div class="product">Nagios<sup><span style="font-size: small;">&reg;</span></sup> Core<sup><span style="font-size: small;">&trade;</span></sup></div>
--	<div class="version">Version <?php echo $this_version; ?></div>
--	<div class="releasedate">February 28, 2024</div>
--	<div class="checkforupdates"><a href="https://www.nagios.org/checkforupdates/?version=<?php echo $this_version; ?>&amp;product=nagioscore" target="_blank">Check for updates</a></div>
+-	<div>
+-		<div class="version">Version <b><?php echo $this_version; ?></b></div>
+-		<div class="releasedate">January 14, 2026</div>
+-	</div>
+-	<div><span id="core-status"></span></div>
+-	<a class="checkforupdates" href="https://www.nagios.org/checkforupdates/?version=<?php echo $this_version; ?>&amp;product=nagioscore" target="_blank">Check for updates</a>
 -</div>
 -
 -
@@ -22,9 +24,7 @@
 -			<div class="submessage">Disabling update checks presents a possible security risk.  Visit <a href="https://www.nagios.org/" target="_blank">nagios.org</a> to check for updates manually or enable update checks in your Nagios config file.</a></div>
 -		</div>
 -<?php
--	} else if (
--		$updateinfo['update_available'] && $this_version < $updateinfo['update_version']
--	) {
+-	} else if ($updateinfo['update_available'] && $this_version < $updateinfo['update_version']) {
 -?>
 -		<div class="updateavailable">
 -			<div class="updatemessage">A new version of Nagios Core is available!</div>
@@ -35,7 +35,6 @@
 -?>
 -</div>
 -
--
- <div id='splashrow0'>
- 	<div id="splashbox0" class="splashbox-media"><!-- info banner feed -->
- 		<div id="splashbox0-contents"></div>
+ <div id="mainsplash">
+ 	<a href="https://www.nagios.org/?utm_campaign=csp&utm_source=nagioscore&utm_medium=splash_thumbnail&utm_content=<?php echo $this_version; ?>" target="_blank"><img src="images/csp-dashboard.avif" /></a>
+ 	<div id="splashtext">
