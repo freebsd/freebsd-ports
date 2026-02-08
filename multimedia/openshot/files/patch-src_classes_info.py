2febb6d56dfbc9e3b97180c95b19b578eace5dda
Tatsuki Makino <tatsuki_makino@hotmail.com>
2026-02-08 18:36:25 +0300

    multimedia/openshot: Update 3.3.0 => 3.4.0
    Changelog:
    https://github.com/OpenShot/openshot-qt/releases/tag/v3.4.0
    
    PR:	285958

--- multimedia/openshot/files/patch-src_classes_info.py
+++ multimedia/openshot/files/patch-src_classes_info.py
@@ -1,6 +1,6 @@
---- src/classes/info.py.orig	2022-12-08 21:31:12 UTC
+--- src/classes/info.py.orig	2025-12-18 03:07:34 UTC
 +++ src/classes/info.py
-@@ -188,6 +188,7 @@ SETUP = {
+@@ -191,6 +191,7 @@ SETUP = {
                         "License :: OSI Approved :: GNU General Public License (GPL)",
                         "Operating System :: OS Independent",
                         "Operating System :: POSIX :: Linux",
