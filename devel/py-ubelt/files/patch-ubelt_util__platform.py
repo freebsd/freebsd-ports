cc746cd26fdce66879aada31a630883919cb8fcc
Yuri Victorovich <yuri@FreeBSD.org>
2026-02-09 19:33:56 -0800

    devel/py-ubelt: update 1.4.0 → 1.4.1
    Reported by:	portscout

--- devel/py-ubelt/files/patch-ubelt_util__platform.py
+++ devel/py-ubelt/files/patch-ubelt_util__platform.py
@@ -1,24 +1,23 @@
---- ubelt/util_platform.py.orig	2022-12-03 05:15:05 UTC
+--- ubelt/util_platform.py.orig	2026-02-08 23:19:13 UTC
 +++ ubelt/util_platform.py
-@@ -42,7 +42,7 @@ from os.path import exists, join, isdir, expanduser, n
- 
- 
+@@ -53,6 +53,7 @@ __all__ = [
  __all__ = [
--    'WIN32', 'LINUX', 'DARWIN', 'POSIX',
-+    'WIN32', 'FREEBSD', 'LINUX', 'DARWIN', 'POSIX',
-     'find_exe', 'find_path',
-     'ensure_app_cache_dir', 'ensure_app_config_dir', 'ensure_app_data_dir',
-     'get_app_cache_dir', 'get_app_config_dir', 'get_app_data_dir',
-@@ -67,6 +67,8 @@ def platform_data_dir():
+     'WIN32',
+     'LINUX',
++    'FREEBSD',
+     'DARWIN',
+     'POSIX',
+     'find_exe',
+@@ -86,6 +87,8 @@ def platform_data_dir() -> str:
      """
      if POSIX:  # nocover
          dpath_ = os.environ.get('XDG_DATA_HOME', '~/.local/share')
 +    elif FREEBSD:  # nocover
 +        dpath_ = os.environ.get('XDG_DATA_HOME', '~/.local/share')
      elif DARWIN:  # nocover
-         dpath_  = '~/Library/Application Support'
+         dpath_ = '~/Library/Application Support'
      elif WIN32:  # nocover
-@@ -86,6 +88,8 @@ def platform_config_dir():
+@@ -105,6 +108,8 @@ def platform_config_dir() -> str:
          str : path to the cache dir used by the current operating system
      """
      if POSIX:  # nocover
@@ -26,4 +25,4 @@
 +    elif FREEBSD:  # nocover
          dpath_ = os.environ.get('XDG_CONFIG_HOME', '~/.config')
      elif DARWIN:  # nocover
-         dpath_  = '~/Library/Application Support'
+         dpath_ = '~/Library/Application Support'
