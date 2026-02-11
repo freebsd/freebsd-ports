460b3a9504afc9b3960be6d3ebee2854e09b1397
Adam Weinberger <adamw@FreeBSD.org>
2026-02-11 13:04:16 -0500

    Mk: Add go126, set GO_DEFAULT?=go125

--- Mk/Uses/go.mk
+++ Mk/Uses/go.mk
@@ -83,7 +83,7 @@ _INCLUDE_USES_GO_MK=	yes
 
 # When adding a version, please keep the comment in
 # Mk/bsd.default-versions.mk in sync.
-GO_VALID_VERSIONS=	1.22 1.23 1.24 1.25
+GO_VALID_VERSIONS=	1.24 1.25 1.26
 
 # Check arguments sanity
 .  if !empty(go_ARGS:N[1-9].[0-9][0-9]+:N[1-9].[0-9][0-9]:Nmodules:Nno_targets:Nrun)
