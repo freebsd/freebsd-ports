5cc28c8ebd451e1a34fed73e0998d42d5c5ad627
Robert Clausecker <fuz@FreeBSD.org>
2026-02-12 18:51:28 +0100

    misc/bb: patch properly
     - remove REGPARM entirely, it doesn't do any good these days and
       blocks portability
     - remove use of gcc, no longer needed
     - patch some portability issues
    
    MFH:		2026Q1
    Sponsored by:	Raptor Computing Systems, LLC

---
+++ misc/bb/files/patch-bb.c
@@ -0,0 +1,10 @@
+--- bb.c.orig	2026-02-12 17:43:44 UTC
++++ bb.c
+@@ -24,6 +24,7 @@
+ #include <string.h>
+ #include <stdlib.h>
+ #include <ctype.h>
++#include <time.h>
+ #include <aalib.h>
+ #include "bb.h"
+ 
