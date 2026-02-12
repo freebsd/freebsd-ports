1921b0353efae4abc660c00288edc95d9d22205d
Dima Panov <fluffy@FreeBSD.org>
2026-02-12 12:27:57 +0300

    editors/libreoffice: update to 26.2.0 release (+)
    We’re pleased to announce the release of LibreOffice 26.2, the newest version of the free and open source office suite
    
    What’s New in LibreOffice 26.2
    
    * Markdown import and export features.
    * Improved performance and responsiveness across the suite, making large documents open, edit, and save more smoothly.
    * Enhanced compatibility with documents created in proprietary and open core office software, reducing formatting issues and surprises.
    * Refined user interface behavior for a cleaner, more consistent experience.
    * Expanded support for open standards, reinforcing long-term access to documents.
    * Hundreds of bug fixes and stability improvements contributed by the global LibreOffice community.
    
    Announce:	https://blog.documentfoundation.org/blog/2026/02/04/libreoffice-26-2-is-here/
    Release notes:	https://wiki.documentfoundation.org/ReleaseNotes/26.2
    Changelog:	https://wiki.documentfoundation.org/Releases/26.2.0/Beta1 \
    		https://wiki.documentfoundation.org/Releases/26.2.0/RC1 \
    		https://wiki.documentfoundation.org/Releases/26.2.0/RC2 \
    		https://wiki.documentfoundation.org/Releases/26.2.0/RC3

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,9 @@
 --- external/skia/UnpackedTarball_skia.mk.orig	2025-05-10 18:09:26 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -43,6 +43,7 @@ skia_patches := \
- 	0002-loong64-Honor-existing-LASX-LSX-settings.patch \
- 	0003-loong64-Fix-missing-rounding-in-loong64-scaled_mult-.patch \
+@@ -44,6 +44,7 @@ skia_patches := \
  	0004-loong64-Fix-the-remaining-implicit-vector-casts.patch \
+     msvc-unknown-attributes.patch.1 \
+ 	fix-semaphore-include.patch.1 \
 +    freebsd-limits.h.patch.0 \
  
  ifneq ($(MSYSTEM),)
--- editors/libreoffice/files/patch-helpcontent2_CustomTarget__html.mk
+++ editors/libreoffice/files/patch-helpcontent2_CustomTarget__html.mk
@@ -1,11 +1,11 @@
---- helpcontent2/CustomTarget_html.mk.orig	2022-12-22 00:14:09 UTC
+--- helpcontent2/CustomTarget_html.mk.orig	2025-11-20 20:45:20 UTC
 +++ helpcontent2/CustomTarget_html.mk
-@@ -115,7 +115,7 @@ $(call gb_CustomTarget_get_workdir,helpcontent2/help3x
- # set of installed languages - has to be language independent
- $(call gb_CustomTarget_get_workdir,helpcontent2/help3xsl)/languages.js : \
-         $(SRCDIR)/helpcontent2/CustomTarget_html.mk
--	printf 'var languagesSet = new Set([%s]);\n' "$(subst $(WHITESPACE),$(COMMA) ,$(patsubst %,'%',$(gb_HELP_LANGS)))" > $@
-+	printf 'var languagesSet = new Set([%s]);\n' "$(subst $(WHITESPACE),$(COMMA) ,$(patsubst %,'%',$(ALL_LANGS)))" > $@
+@@ -87,7 +87,7 @@ $(gb_CustomTarget_workdir)/helpcontent2/help3xsl/langu
  
- define html_gen_langnames_js_dep
- $(call gb_CustomTarget_get_workdir,helpcontent2/help3xsl)/$(1)/langnames.js : \
+ # set of installed languages - has to be language independent
+ $(gb_CustomTarget_workdir)/helpcontent2/help3xsl/languages.js : $(SRCDIR)/helpcontent2/CustomTarget_html.mk
+-	$(file >$@,var languagesSet = new Set([$(subst $(WHITESPACE),$(COMMA) ,$(patsubst %,'%',$(gb_HELP_LANGS)))]);)
++	$(file >$@,var languagesSet = new Set([$(subst $(WHITESPACE),$(COMMA) ,$(patsubst %,'%',$(ALL_LANGS)))]);)
+ 
+ # names of the languages - has to be translated, ie. per language
+ $(gb_CustomTarget_workdir)/helpcontent2/help3xsl/%/langnames.js : \
