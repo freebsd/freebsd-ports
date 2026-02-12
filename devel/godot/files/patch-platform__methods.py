d51054ca6e2d6e5be4f004c728d67750e8c12668
Vladimir Druzenko <vvd@FreeBSD.org>
2026-02-12 17:10:21 +0300

    devel/godot: Improve port
    - Refresh patch patch-platform__methods.py
    - Correct the patch name patch-thirdparty_linuxbsd__headers_udev_libudev.h
    - Remove unused dependency from multimedia/libvpx.
    - Enable opengl3 support.
    - Explicit enable use_sowrap - dynamically load system libraries.
    - Explicit enable dynamically load dbus if installed.
    
    PR:		288408
    Tested by:	Sure Beae <sure@disroot.org>
    Approved by:	Shane <FreeBSD@ShaneWare.Biz> (maintainer, timeout 6 months)

--- devel/godot/files/patch-platform__methods.py
+++ devel/godot/files/patch-platform__methods.py
@@ -1,6 +1,6 @@
---- platform_methods.py.orig	2023-03-23 23:40:42 UTC
+--- platform_methods.py.orig	2025-03-25 23:11:16 UTC
 +++ platform_methods.py
-@@ -92,6 +92,9 @@ architecture_aliases = {
+@@ -28,6 +28,9 @@ architecture_aliases = {
      "rv": "rv64",
      "riscv": "rv64",
      "riscv64": "rv64",
@@ -10,7 +10,7 @@
      "ppcle": "ppc32",
      "ppc": "ppc32",
      "ppc64le": "ppc64",
-@@ -99,7 +102,7 @@ def detect_arch():
+@@ -36,7 +39,7 @@ def detect_arch():
  
  
  def detect_arch():
