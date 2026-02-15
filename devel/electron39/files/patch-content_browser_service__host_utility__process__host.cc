97e8bdfef102ad6d36c68616e30a8ac46228b993
Hiroki Tagato <tagattie@FreeBSD.org>
2026-02-15 21:31:34 +0900

    devel/electron39: Update to 39.6.0
    Changelog:
    - https://github.com/electron/electron/releases/tag/v39.5.2
    - https://github.com/electron/electron/releases/tag/v39.6.0
    
    Reported by:	GitHub (watch releases)

--- devel/electron39/files/patch-content_browser_service__host_utility__process__host.cc
+++ devel/electron39/files/patch-content_browser_service__host_utility__process__host.cc
@@ -1,4 +1,4 @@
---- content/browser/service_host/utility_process_host.cc.orig	2025-11-02 19:17:58 UTC
+--- content/browser/service_host/utility_process_host.cc.orig	2026-02-14 12:04:37 UTC
 +++ content/browser/service_host/utility_process_host.cc
 @@ -68,7 +68,7 @@
  #include "content/browser/v8_snapshot_files.h"
@@ -18,7 +18,7 @@
  base::ScopedFD PassNetworkContextParentDirs(
      std::vector<base::FilePath> network_context_parent_dirs) {
    base::Pickle pickle;
-@@ -525,7 +525,7 @@ bool UtilityProcessHost::StartProcess() {
+@@ -534,7 +534,7 @@ bool UtilityProcessHost::StartProcess() {
        GetV8SnapshotFilesToPreload(*cmd_line));
  #endif  // BUILDFLAG(IS_POSIX)
  
@@ -27,7 +27,7 @@
    // The network service should have access to the parent directories
    // necessary for its usage.
    if (options_.sandbox_type_ == sandbox::mojom::Sandbox::kNetwork) {
-@@ -543,7 +543,7 @@ bool UtilityProcessHost::StartProcess() {
+@@ -552,7 +552,7 @@ bool UtilityProcessHost::StartProcess() {
        video_capture::mojom::VideoCaptureService::Name_) {
      bool pass_gpu_buffer_flag =
          switches::IsVideoCaptureUseGpuMemoryBufferEnabled();
