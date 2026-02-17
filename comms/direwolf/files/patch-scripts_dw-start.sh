4022d4b1a9036ed46c03088a09f2da95e7b1052a
Älven <alven@FreeBSD.org>
2026-02-17 09:49:47 +0400

    comms/direwolf: re-generate patches
    PR:			292181
    Reported by:		takefu@airport.fm
    Approved by:		hamradio@ (alven@)
    Differential Revision:	https://reviews.freebsd.org/D54299

--- comms/direwolf/files/patch-scripts_dw-start.sh
+++ comms/direwolf/files/patch-scripts_dw-start.sh
@@ -1,22 +1,12 @@
---- scripts/dw-start.sh.orig	2022-01-28 14:55:02 UTC
+--- scripts/dw-start.sh.orig	2026-02-17 05:41:09 UTC
 +++ scripts/dw-start.sh
-@@ -68,9 +68,8 @@ DWCMD="$DIREWOLF -a 100"
+@@ -68,8 +68,7 @@ DWCMD="$DIREWOLF -a 100"
  
  
  #Where will logs go - needs to be writable by non-root users
 -LOGFILE=/var/tmp/dw-start.log
+-
 +LOGFILE=/var/log/dw-start.log
  
--
  #-------------------------------------
  # Main functions of the script
- #-------------------------------------
-@@ -157,7 +157,7 @@ function GUI {
- # When running from cron, we have a very minimal environment
- # including PATH=/usr/bin:/bin.
- #
--export PATH=/usr/local/bin:$PATH
-+export PATH=%%PREFIX%%/bin:$PATH
- 
- #Log the start of the script run and re-run
- date >> $LOGFILE
