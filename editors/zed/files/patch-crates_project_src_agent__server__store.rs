91d06d728d1320d2dabb686d3e6125ca996d1661
Hiroki Tagato <tagattie@FreeBSD.org>
2026-02-18 21:21:35 +0900

    editors/zed: Update to 0.223.5
    Changelog:
    - https://github.com/zed-industries/zed/releases/tag/v0.223.4
    - https://github.com/zed-industries/zed/releases/tag/v0.223.5
    
    Reported by:	GitHub (watch releases)

--- editors/zed/files/patch-crates_project_src_agent__server__store.rs
+++ editors/zed/files/patch-crates_project_src_agent__server__store.rs
@@ -1,6 +1,6 @@
---- crates/project/src/agent_server_store.rs.orig	2026-02-06 04:11:05 UTC
+--- crates/project/src/agent_server_store.rs.orig	2026-02-17 20:22:48 UTC
 +++ crates/project/src/agent_server_store.rs
-@@ -1690,6 +1690,8 @@ fn get_platform_info() -> Option<(&'static str, &'stat
+@@ -1672,6 +1672,8 @@ fn get_platform_info() -> Option<(&'static str, &'stat
          "pc-windows-msvc"
      } else if cfg!(target_os = "linux") {
          "unknown-linux-gnu"
@@ -9,7 +9,7 @@
      } else {
          return None;
      };
-@@ -1771,6 +1773,8 @@ impl ExternalAgentServer for LocalExtensionArchiveAgen
+@@ -1753,6 +1755,8 @@ impl ExternalAgentServer for LocalExtensionArchiveAgen
                  "linux"
              } else if cfg!(target_os = "windows") {
                  "windows"
@@ -18,7 +18,7 @@
              } else {
                  anyhow::bail!("unsupported OS");
              };
-@@ -1973,6 +1977,8 @@ impl ExternalAgentServer for LocalRegistryArchiveAgent
+@@ -1955,6 +1959,8 @@ impl ExternalAgentServer for LocalRegistryArchiveAgent
                  "linux"
              } else if cfg!(target_os = "windows") {
                  "windows"
