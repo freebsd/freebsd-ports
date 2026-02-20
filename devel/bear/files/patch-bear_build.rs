014126f409f88db730f23108c16191ab6290b6b4
ShengYi Hung <aokblast@FreeBSD.org>
2026-02-20 17:22:10 +0800

    devel/bear: Upgrade to 4.0.3
    Approved by:    lwhsu (mentor, implicitly)
    Differential Revision: https://reviews.freebsd.org/D55401

--- devel/bear/files/patch-bear_build.rs
+++ devel/bear/files/patch-bear_build.rs
@@ -1,17 +1,19 @@
 // See https://github.com/rizsotto/Bear/issues/648
 
---- bear/build.rs.orig	2026-01-17 07:09:18 UTC
+
+--- bear/build.rs.orig	2026-02-09 10:13:49 UTC
 +++ bear/build.rs
-@@ -25,12 +25,12 @@
- 
- /// Default wrapper executable path
- /// Package creators: modify this entire path to change wrapper location
+@@ -29,13 +29,13 @@
+ ///                   sequence ("C:\Users\..." breaks); use forward slashes
+ ///                   ("C:/Users/..."), escaped backslashes ("C:\\Users\\..."),
+ ///                   or a raw string literal (r"C:\Users\...")
 -const DEFAULT_WRAPPER_PATH: &str = "/usr/local/libexec/bear";
 +const DEFAULT_WRAPPER_PATH: &str = "/usr/local/lib/bear";
  
  /// Default preload library path
  /// Package creators: modify this entire path to change preload library location
  /// Note: $LIB will be expanded at runtime to the appropriate architecture subdirectory
+ /// Note for Windows: preload isn't supported; feel free to ignore this path
 -const DEFAULT_PRELOAD_PATH: &str = "/usr/local/libexec/bear/$LIB";
 +const DEFAULT_PRELOAD_PATH: &str = "/usr/local/lib/bear";
  
