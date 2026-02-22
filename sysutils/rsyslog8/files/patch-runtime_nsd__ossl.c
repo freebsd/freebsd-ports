25fa745e835dad6b9fb78f37edcd3cc90fd4b6e7
Matthew Seaman <matthew@FreeBSD.org>
2026-02-22 14:48:44 +0000

    sysutils/rsyslog8: update to 8.2602.0
    Changes:	https://github.com/rsyslog/rsyslog/blob/v8.2602.0/ChangeLog
    
    Reported by:	repology

--- sysutils/rsyslog8/files/patch-runtime_nsd__ossl.c
+++ sysutils/rsyslog8/files/patch-runtime_nsd__ossl.c
@@ -1,11 +1,11 @@
---- runtime/nsd_ossl.c.orig	2025-09-05 13:46:02 UTC
+--- runtime/nsd_ossl.c.orig	2026-02-07 11:43:26 UTC
 +++ runtime/nsd_ossl.c
-@@ -803,7 +803,7 @@ rsRetVal osslPostHandshakeCheck(nsd_ossl_t *pNsd) {
+@@ -889,7 +889,7 @@ rsRetVal osslPostHandshakeCheck(nsd_ossl_t *pNsd) {
      if (SSL_get_shared_ciphers(pNsd->pNetOssl->ssl, szDbg, sizeof szDbg) != NULL)
          dbgprintf("osslPostHandshakeCheck: Debug Shared ciphers = %s\n", szDbg);
  
--#if OPENSSL_VERSION_NUMBER >= 0x10002000L
-+#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER)
+-#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(ENABLE_WOLFSSL)
++#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER) && !defined(ENABLE_WOLFSSL)
      if (SSL_get_shared_curve(pNsd->pNetOssl->ssl, -1) == 0) {
          // This is not a failure
          LogMsg(0, RS_RET_NO_ERRCODE, LOG_INFO,
