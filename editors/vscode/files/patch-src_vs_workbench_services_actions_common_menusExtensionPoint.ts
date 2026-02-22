df32c2d188db33cec7bdf94aa6ee402cd46f5489
Hiroki Tagato <tagattie@FreeBSD.org>
2026-02-22 17:45:40 +0900

    editors/vscode: Update to 1.109.5
    Changelog: https://code.visualstudio.com/updates/v1_109
    
    Reported by:	GitHub (watch releases)

--- editors/vscode/files/patch-src_vs_workbench_services_actions_common_menusExtensionPoint.ts
+++ editors/vscode/files/patch-src_vs_workbench_services_actions_common_menusExtensionPoint.ts
@@ -1,6 +1,6 @@
---- src/vs/workbench/services/actions/common/menusExtensionPoint.ts.orig	2026-02-04 01:55:52 UTC
+--- src/vs/workbench/services/actions/common/menusExtensionPoint.ts.orig	2026-02-19 16:51:04 UTC
 +++ src/vs/workbench/services/actions/common/menusExtensionPoint.ts
-@@ -1216,7 +1216,10 @@ class CommandsTableRenderer extends Disposable impleme
+@@ -1222,7 +1222,10 @@ class CommandsTableRenderer extends Disposable impleme
  
  		switch (platform) {
  			case 'win32': key = rawKeyBinding.win; break;
