d68a0effe94d7416f270d524e47edd65dfbb2519
Xin LI <delphij@FreeBSD.org>
2026-02-28 23:25:06 -0800

    devel/py-inotify-simple: use native inotify on FreeBSD 15+
    FreeBSD 15 (OSVERSION >= 1500051) provides native inotify in libc.
    Use native libc on FreeBSD 15+ and Linux; fall back to libinotify on
    older FreeBSD releases which require the kqueue-based emulation.
    
    Remove unconditional LIB_DEPENDS on libinotify and make it conditional
    using the bsd.port.pre.mk/bsd.port.post.mk split.
    
    PR:		ports/294447
    Approved by:	maintainer timeout

--- devel/py-inotify-simple/files/patch-inotify__simple.py
+++ devel/py-inotify-simple/files/patch-inotify__simple.py
@@ -1,6 +1,6 @@
 --- inotify_simple.py.orig	2025-08-25 06:27:48 UTC
 +++ inotify_simple.py
-@@ -3,6 +3,7 @@ from enum import IntEnum
+@@ -3,6 +3,7 @@ from select import poll
  from collections import namedtuple
  from struct import unpack_from, calcsize
  from select import poll
@@ -8,12 +8,19 @@
  from time import sleep
  from ctypes import CDLL, get_errno, c_int
  from ctypes.util import find_library
-@@ -76,12 +77,12 @@ class INotify(FileIO):
+@@ -76,12 +77,19 @@ class INotify(FileIO):
                  object is garbage collected or when `~inotify_simple.INotify.close` is
                  called."""
              
 -        global _libc; _libc = _libc or CDLL(find_library('c'), use_errno=True)
-+        global _libc; _libc = _libc or CDLL(find_library('inotify'), use_errno=True)
++        global _libc
++        if _libc is None:
++            _uname = os.uname()
++            if _uname.sysname == 'FreeBSD' and \
++                    int(_uname.release.split('.')[0]) < 15:
++                _libc = CDLL(find_library('inotify'), use_errno=True)
++            else:
++                _libc = CDLL(find_library('c'), use_errno=True)
          flags = (not inheritable) * os.O_CLOEXEC | bool(nonblocking) * os.O_NONBLOCK
          fd = _libc_call(_libc.inotify_init1, flags)
          super().__init__(fd, mode='rb', closefd=closefd)
