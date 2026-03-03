fd109706991af56f37679d3d985fda827a282a27
Vladimir Druzenko <vvd@FreeBSD.org>
2026-03-03 16:12:07 +0300

    graphics/opencv: Fix build issue with VTK option enabled due to lack of #include <iostream>
    Approved by:	portmgr (blanket - fix build)

---
+++ graphics/opencv/files/patch-contrib_modules_viz_src_widget.cpp
@@ -0,0 +1,11 @@
+--- contrib/modules/viz/src/widget.cpp.orig	2025-12-16 10:59:36 UTC
++++ contrib/modules/viz/src/widget.cpp
+@@ -45,6 +45,8 @@
+ 
+ #include "precomp.hpp"
+ 
++#include <iostream>
++
+ ///////////////////////////////////////////////////////////////////////////////////////////////
+ /// widget implementation
+ 
