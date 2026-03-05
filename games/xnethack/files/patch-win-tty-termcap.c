ecb60daf0b5fe44873cf21b978fb395040cf2e5a
Robert Clausecker <fuz@FreeBSD.org>
2026-03-05 18:46:43 +0100

    games/xnethack: update to 9.1
    At last, xNetHack 9.1!
    Regenerate and update patch files.
    
    Changelog: https://github.com/copperwater/xNetHack/blob/master/doc/xnh-changelog-9.1.md

--- games/xnethack/files/patch-win-tty-termcap.c
+++ games/xnethack/files/patch-win-tty-termcap.c
@@ -1,6 +1,6 @@
---- win/tty/termcap.c.orig	2025-03-15 15:02:36 UTC
+--- win/tty/termcap.c.orig	2026-02-28 22:45:03 UTC
 +++ win/tty/termcap.c
-@@ -195,10 +195,6 @@ term_startup(int *wid, int *hgt)
+@@ -196,10 +196,6 @@ term_startup(int *wid, int *hgt)
          error("Terminal must backspace.");
  #else
          if (!(BC = Tgetstr(nhStr("bc")))) { /* termcap also uses bc/bs */
