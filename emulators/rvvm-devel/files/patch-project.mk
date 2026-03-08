56ce17a57a84d3de205162ba1ad2a64bd91ae18a
Robert Clausecker <fuz@FreeBSD.org>
2026-03-08 20:47:58 +0100

    emulators/rvvm-devel: RISC-V Virtual Machine (development version)
    RVVM is a virtual machine / emulator for RISC-V guests, which emphasizes
    on performance, security, lean code and portability. It already runs a
    lot of guest operating systems, including Linux, Haiku, FreeBSD,
    OpenBSD, etc. It also aims to run RISC-V applications on a foreign-arch
    host without full OS guest & isolation (Userland emulation).
    
    This port holds a version of the project that is close to what is
    currently in development.
    
    Requested by:	siva
    See also:	https://reviews.freebsd.org/D55356

---
+++ emulators/rvvm-devel/files/patch-project.mk
@@ -0,0 +1,11 @@
+--- project.mk.orig	2026-03-08 19:32:28 UTC
++++ project.mk
+@@ -1,7 +1,7 @@ override URL     := https://github.com/LekKit/RVVM
+ override NAME    := RVVM
+ override DESC    := The RVVM Project
+ override URL     := https://github.com/LekKit/RVVM
+-override VERSION := v0.7-git
++#override VERSION := v0.7-git
+ 
+ override define LOGO
+ $(BLUE) 🭥█████🭐 ██  ██ ██  ██🭢█🭌🬿  🭊🭁█🭚
