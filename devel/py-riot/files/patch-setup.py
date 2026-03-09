e70c55158857345ea97013de05974c56ad553d09
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2026-03-10 01:57:54 +0800

    devel/py-riot: Update to 0.21.0
    Changes:	https://github.com/DataDog/riot/releases

--- devel/py-riot/files/patch-setup.py
+++ devel/py-riot/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2024-10-18 20:24:28 UTC
+--- setup.py.orig	2026-02-09 14:56:45 UTC
 +++ setup.py
 @@ -29,7 +29,7 @@ setup(
+     python_requires=">=3.8",
      install_requires=[
-         "dataclasses; python_version<'3.7'",
          "click>=7",
--        "virtualenv<=20.26.6",
-+        "virtualenv",
+-        "virtualenv>=20.36.1,<21",
++        "virtualenv>=20.36.1",
          "rich",
          "pexpect",
          "packaging",
