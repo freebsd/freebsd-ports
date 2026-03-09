89d99c8a229ceb7a88bd1acdb4add9744c04f06e
Max Brazhnikov <makc@FreeBSD.org>
2026-03-09 19:38:53 +0300

    multimedia/gstreamer1-plugins-v4l2: fix runtime error
    Restore lost patch.
    
    PR:		236673

--- multimedia/gstreamer1-plugins-good/files/patch-sys_v4l2_gstv4l2object.c
+++ multimedia/gstreamer1-plugins-good/files/patch-sys_v4l2_gstv4l2object.c
@@ -1,6 +1,6 @@
---- sys/v4l2/gstv4l2object.c.orig	2026-01-27 17:02:33 UTC
+--- sys/v4l2/gstv4l2object.c.orig	2026-02-26 01:44:06 UTC
 +++ sys/v4l2/gstv4l2object.c
-@@ -573,7 +573,7 @@ v4l2_mmap_wrapper (gpointer start, gsize length, gint 
+@@ -574,7 +574,7 @@ v4l2_mmap_wrapper (gpointer start, gsize length, gint 
  
  #endif /* SIZEOF_OFF_T < 8 */
  
@@ -9,7 +9,16 @@
  /* v4l2_ioctl always takes request as unsigned long int, not ioctl_req_t */
  static gint
  v4l2_ioctl_wrapper (gint fd, ioctl_req_t request, ...)
-@@ -5385,7 +5385,9 @@ gst_v4l2_object_probe_caps (GstV4l2Object * v4l2object
+@@ -3449,7 +3449,7 @@ gst_v4l2_object_is_dmabuf_supported (GstV4l2Object * v
+ 
+   /* Expected to fail, but ENOTTY tells us that it is not implemented. */
+   v4l2object->ioctl (v4l2object->video_fd, VIDIOC_EXPBUF, &expbuf);
+-  if (errno == ENOTTY)
++  if (errno == ENOTTY || errno == EINVAL)
+     ret = FALSE;
+ 
+   return ret;
+@@ -5393,7 +5393,9 @@ gst_v4l2_object_probe_caps (GstV4l2Object * v4l2object
      if (v4l2object->ioctl (v4l2object->video_fd, VIDIOC_CROPCAP, &cropcap) < 0) {
  
        switch (errno) {
