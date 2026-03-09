53012cd436a9cf1318b224cbff1ae4c0a558881f
Saro <web@saromedia.com>
2026-03-10 00:34:34 +0300

    textproc/kibana8: Update 8.11.3 => 8.19.12
    Changelogs (there are 59 new versions):
    https://www.elastic.co/guide/en/kibana/8.19/release-notes-8.11.4.html
     . . .
    https://www.elastic.co/guide/en/kibana/8.19/release-notes-8.19.12.html
    
    - Add "MASTER_SITES+=LOCAL/vvd/kibana" for those who also can't
      download from the official website: "403 Forbidden".
    - Switch to default nodejs version 24.
    - Improve COMMENT - correct name is "Elasticsearch" without camel-case.
    - Use ${NODEJS_VERSION} instead of hardcoded nodejs version.
    - Remove unnecessary BUILD_DEPENDS - USES+=nodejs also adds it.
    - Remove default args build,run form USES+=nodejs.
    - Update CONFLICTS.
    - Fix warnings from portclippy.
    - Replace "${INSTALL} -l rs" with "${RLN}".
    - Update pkg-descr.
    - Improve pkg-message.
    
    PR:		289759
    Approved by:	elastic (maintainer, timeout > 5 months)
    Co-authored-by:	Vladimir Druzenko <vvd@FreeBSD.org>

--- textproc/kibana8/files/patch-node__modules__kbn_setup-node-env_node__version__validator.js
+++ textproc/kibana8/files/patch-node__modules__kbn_setup-node-env_node__version__validator.js
@@ -1,6 +1,6 @@
---- src/setup_node_env/node_version_validator.js.orig	2023-01-04 12:19:12 UTC
-+++ src/setup_node_env/node_version_validator.js
-@@ -15,13 +15,13 @@ var pkg = require('../../package.json');
+--- node_modules/@kbn/setup-node-env/node_version_validator.js.orig	2025-12-30 21:41:35 UTC
++++ node_modules/@kbn/setup-node-env/node_version_validator.js
+@@ -21,13 +21,13 @@ var requiredVersion = rawRequiredVersion ? 'v' + rawRe
  var currentVersion = process && process.version || null;
  var rawRequiredVersion = pkg && pkg.engines && pkg.engines.node || null;
  var requiredVersion = rawRequiredVersion ? 'v' + rawRequiredVersion : rawRequiredVersion;
@@ -15,6 +15,4 @@
    // Actions to apply when validation fails: error report + exit.
    console.error(errorMessage);
    process.exit(1);
--}
-\ No newline at end of file
-+}
+
