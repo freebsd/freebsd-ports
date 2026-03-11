56a7ffa4d79f016e99fd1672c912914a90c1d34f
Jesús Daniel Colmenares Oviedo <dtxdf@FreeBSD.org>
2026-03-11 15:41:20 -0400

    sysutils/py-rendercv: Update to 2.7
    ChangeLog: https://github.com/rendercv/rendercv/releases/tag/v2.7

--- sysutils/py-rendercv/files/patch-src_rendercv_schema_variant__pydantic__model__generator.py
+++ sysutils/py-rendercv/files/patch-src_rendercv_schema_variant__pydantic__model__generator.py
@@ -1,6 +1,6 @@
---- src/rendercv/schema/variant_pydantic_model_generator.py.orig	2025-12-19 21:48:58 UTC
+--- src/rendercv/schema/variant_pydantic_model_generator.py.orig	2026-03-11 16:54:44 UTC
 +++ src/rendercv/schema/variant_pydantic_model_generator.py
-@@ -6,17 +6,17 @@ from rendercv.exception import RenderCVInternalError
+@@ -6,7 +6,7 @@ from rendercv.exception import RenderCVInternalError
  
  from rendercv.exception import RenderCVInternalError
  
@@ -8,8 +8,14 @@
 +FieldSpec = tuple[type[Any], FieldInfo]
  
  
+ def sanitize_defaults(value: Any) -> Any:
+@@ -29,16 +29,16 @@ def sanitize_defaults(value: Any) -> Any:
+     return value
+ 
+ 
 -def create_variant_pydantic_model[T: pydantic.BaseModel](
 +def create_variant_pydantic_model(
+     *,
      variant_name: str,
      defaults: dict[str, Any],
 -    base_class: type[T],
@@ -17,12 +23,13 @@
      discriminator_field: str,
      class_name_suffix: str,
      module_name: str,
+     require_all_fields: bool = False,
 -) -> type[T]:
 +):
      """Create Pydantic model variant with customized defaults.
  
      Why:
-@@ -190,10 +190,10 @@ def create_discriminator_field_spec(
+@@ -261,10 +261,10 @@ def create_discriminator_field_spec(
      return (cast(type[Any], field_annotation), new_field)
  
  
