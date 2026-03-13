cd97f5eace13e5070e084303991f7c297d19e918
Baptiste Daroussin <bapt@FreeBSD.org>
2026-03-13 19:08:06 +0100

    devel/heimdall: fix flashing on some samung tablet
    Heimdall sends at some empty OUT transfers (as for the ODIN protocol)
    with a 100ms timeou. The device is busy writing on the flash at that
    point and NACKs OUT tokens for more than 100ms. The result is the empty
    transfers times out. and since this is a required ODIN protocol marker,
    the transfers fails.
    
    Make sure those empty out transfers get a longer timeout.

---
+++ devel/heimdall/files/patch-heimdall_source_BridgeManager.h
@@ -0,0 +1,11 @@
+--- heimdall/source/BridgeManager.h.orig	2017-05-11 18:34:25 UTC
++++ heimdall/source/BridgeManager.h
+@@ -82,7 +82,7 @@ namespace Heimdall
+ 			{
+ 				kDefaultTimeoutSend = 3000,
+ 				kDefaultTimeoutReceive = 3000,
+-				kDefaultTimeoutEmptyTransfer = 100
++				kDefaultTimeoutEmptyTransfer = 3000
+ 			};
+ 
+ 			enum class UsbLogLevel
