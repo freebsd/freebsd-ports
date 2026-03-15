818af882aab0ad48e76e68b3ef389078a2225654
Vladimir Druzenko <vvd@FreeBSD.org>
2026-03-15 04:25:32 +0300

    textproc/kibana92: New port: Browser-based analytics and search interface to Elasticsearch 9.2.x
    Release Notes:
    https://www.elastic.co/docs/release-notes/kibana#kibana-9.2.6-release-notes
    
    PR:		290979
    Approved by:	elastic (maintainer, implicit - inactive for more than 6 months)

--- textproc/kibana92/files/patch-node__modules__kbn_setup-node-env_node__version__validator.js
+++ textproc/kibana92/files/patch-node__modules__kbn_setup-node-env_node__version__validator.js
@@ -1,16 +1,16 @@
---- node_modules/@kbn/setup-node-env/node_version_validator.js.orig	2026-01-08 12:19:41 UTC
+--- node_modules/@kbn/setup-node-env/node_version_validator.js.orig	2026-02-24 03:31:48 UTC
 +++ node_modules/@kbn/setup-node-env/node_version_validator.js
-@@ -21,11 +21,11 @@ var requiredVersion = rawRequiredVersion ? 'v' + rawRe
- var currentVersion = process && process.version || null;
- var rawRequiredVersion = pkg && pkg.engines && pkg.engines.node || null;
- var requiredVersion = rawRequiredVersion ? 'v' + rawRequiredVersion : rawRequiredVersion;
--var isVersionValid = !!currentVersion && !!requiredVersion && currentVersion === requiredVersion;
-+var isVersionValid = !!currentVersion && !!requiredVersion && currentVersion >= requiredVersion;
+@@ -21,11 +21,11 @@ if (!process.env.UNSAFE_DISABLE_NODE_VERSION_VALIDATIO
+   var currentVersion = process && process.version || null;
+   var rawRequiredVersion = pkg && pkg.engines && pkg.engines.node || null;
+   var requiredVersion = rawRequiredVersion ? 'v' + rawRequiredVersion : rawRequiredVersion;
+-  var isVersionValid = !!currentVersion && !!requiredVersion && currentVersion === requiredVersion;
++  var isVersionValid = !!currentVersion && !!requiredVersion && currentVersion >= requiredVersion;
  
- // Validates current the NodeJS version compatibility when Kibana starts.
- if (!isVersionValid) {
--  var errorMessage = 'Kibana does not support the current Node.js version ' + currentVersion + '. Please use Node.js ' + requiredVersion + '.';
-+  var errorMessage = 'Kibana does not support the current Node.js version ' + currentVersion + '. Please use Node.js >= ' + requiredVersion + '.';
+   // Validates current the NodeJS version compatibility when Kibana starts.
+   if (!isVersionValid) {
+-    var errorMessage = 'Kibana does not support the current Node.js version ' + currentVersion + '. Please use Node.js ' + requiredVersion + '.';
++    var errorMessage = 'Kibana does not support the current Node.js version ' + currentVersion + '. Please use Node.js >= ' + requiredVersion + '.';
  
-   // Actions to apply when validation fails: error report + exit.
-   console.error(errorMessage);
+     // Actions to apply when validation fails: error report + exit.
+     console.error(errorMessage);
