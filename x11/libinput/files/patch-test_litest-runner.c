3ebc3437a83e91e100bc9fdeaad138b748f1fc40
Gleb Popov <arrowd@FreeBSD.org>
2026-05-04 23:57:45 +0300

    x11/libinput: Update to 1.31.1
    PR:		293585

--- x11/libinput/files/patch-test_litest-runner.c
+++ x11/libinput/files/patch-test_litest-runner.c
@@ -1,10 +1,10 @@
---- test/litest-runner.c.orig	2025-04-01 02:46:07 UTC
+--- test/litest-runner.c.orig	2026-04-02 01:04:12 UTC
 +++ test/litest-runner.c
 @@ -26,7 +26,6 @@
  #include <errno.h>
+ #include <inttypes.h>
  #include <sys/epoll.h>
- #include <sys/timerfd.h>
 -#include <sys/sysinfo.h>
+ #include <sys/timerfd.h>
  #include <sys/wait.h>
  #ifdef HAVE_PIDFD_OPEN
- #include <sys/syscall.h>
--- x11/libinput/files/patch-test_litest.c
+++ x11/libinput/files/patch-test_litest.c
@@ -1,46 +1,22 @@
---- test/litest.c.orig	2025-04-01 02:46:07 UTC
+--- test/litest.c.orig	2026-04-02 01:04:12 UTC
 +++ test/litest.c
-@@ -44,13 +44,13 @@
- #include <sys/wait.h>
+@@ -39,7 +39,6 @@
+ #include <sys/ptrace.h>
+ #include <sys/resource.h>
  #include <sys/stat.h>
- #include <sys/types.h>
 -#include <sys/sysinfo.h>
- #include <libudev.h>
- #if HAVE_LIBSYSTEMD
- #include <systemd/sd-bus.h>
+ #include <sys/timerfd.h>
+ #include <sys/types.h>
+ #include <sys/wait.h>
+@@ -52,10 +51,10 @@
  #endif
  #ifdef __FreeBSD__
  #include <termios.h>
 +#include <sys/kbio.h>
  #endif
  
- #include <valgrind/valgrind.h>
-@@ -66,8 +66,6 @@
- #include "util-backtrace.h"
- #include "util-libinput.h"
- 
+ #include <libevdev/libevdev.h>
 -#include <linux/kd.h>
--
- #define evbit(t, c) ((t) << 16U | (c & 0xffff))
+ #include <valgrind/valgrind.h>
  
- #define UDEV_RULES_D "/run/udev/rules.d"
-@@ -5322,8 +5320,8 @@ check_device_access(void)
- 		return 77;
- 	}
- 
--	if (access("/dev/uinput", F_OK) == -1 &&
--	    access("/dev/input/uinput", F_OK) == -1) {
-+	if (access("/dev/uinput_skip_our_libudev_isnt_ready", F_OK) == -1 &&
-+	    access("/dev/input/uinput_skip_our_libudev_isnt_ready", F_OK) == -1) {
- 		fprintf(stderr,
- 			"uinput device is missing, skipping tests.\n");
- 		return 77;
-@@ -5365,7 +5363,7 @@ main(int argc, char **argv)
- 	if (getenv("NO_COLOR"))
- 		use_colors = false;
- 
--	in_debugger = is_debugger_attached();
-+	in_debugger = false;
- 	if (in_debugger) {
- 		jobs = 0;
- 	} else if ((meson_testthreads = getenv("MESON_TESTTHREADS")) == NULL ||
+ #include "util-backtrace.h"
