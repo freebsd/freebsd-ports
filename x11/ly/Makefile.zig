cd4f167a18d97e3b3afada61ef579d9a7ef3781f
Kenneth Raplee <kenrap@FreeBSD.org>
2026-05-04 08:41:08 -0700

    x11/ly: Update to 1.4.0
    PR:             294741
    Approved by:    arrowd (co-mentor)

--- x11/ly/Makefile.zig
+++ x11/ly/Makefile.zig
@@ -1,4 +1,6 @@
-ZIG_TUPLE=	clap:github.com/Hejsil/zig-clap/archive/5289e0753cd274d65344bef1c114284c6.tar.gz:clap-0.11.0-oBajB-HnAQDPCKYzwF7rO3qDFwRcD39Q0DALlTSz5H7e \
-		ini:github.com/AnErrupTion/ini/archive/918f16d0dcf893d0c1cdffe204faa08bb3584e04.tar.gz:ini-0.1.0-YCQ9Ys0pAABixEvvQvhVXAdqRE3wrZk_wiL9TPNHhB8d \
-		termbox2:github.com/AnErrupTion/termbox2/archive/496730697c662893eec43192f48ff616c.tar.gz:N-V-__8AAOEWBQDt5tNdIzIFY6n8DdZsCP-6MyLoNS20wgpA \
-		zigini:github.com/AnErrupTion/zigini/archive/9281f47702b57779e831d7618e158abb8.tar.gz:zigini-0.3.3-36M0FRJJAADZVq5HPm-hYKMpFFTr0OgjbEYcK2ijKZ5n
+ZIG_TUPLE=	aro:github.com/Vexu/arocc/archive/5f5a050569a95ecc40a426f0c3666ae7e.tar.gz:aro-0.0.0-JSD1Qi7QNgDnfcrdEJf82v3o6MhZySjYVrtdfEf3E4Se \
+		clap:github.com/Hejsil/zig-clap/archive/fc1e5cc3f6d9d3001112385ee6256d694.tar.gz:clap-0.11.0-oBajB7foAQC3Iyn4IVCkUdYaOVVng5IZkSncySTjNig1 \
+		ini:github.com/AshAmetrine/ziglibs-ini/archive/3b733e10adc6c4c48050709b97a22258b.tar.gz:ini-0.1.0-YCQ9YiwsAACghqF8LZyjAF2H_NnL6n29QLuCe0fsmPTo \
+		termbox2:github.com/AnErrupTion/termbox2/archive/c7f241e8888ce243e1748b05c26a42fcf.tar.gz:N-V-__8AAAUXBQD6Fwpi9m0MBqWXFFaqW5l1lVrJC2Ynj7a- \
+		translate_c:codeberg.org/ziglang/translate-c/archive/7a1a9fdc4ab00835748a6657ecbb835e3.tar.gz:translate_c-0.0.0-Q_BUWvP1BgCjAk6PWv5286tOlvzD9-X-NkuTzh0KxY0Q \
+		zigini:github.com/AshAmetrine/zigini/archive/a665d081dda42664a96da2840ea09c5cc.tar.gz:zigini-0.5.0-BSkB7e9WAACfyCBABNZiWL3gFMw18GKn3qBcPs8L1Ec1
