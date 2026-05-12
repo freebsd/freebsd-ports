2ce64c48a7de0a5631c290e21a7802c69f493cbc
Pietro Cerutti <gahr@FreeBSD.org>
2026-05-12 17:38:08 +0000

    lang/tcl86, x11-toolkits/tk86: update to 8.6.18
    Announcement: https://sourceforge.net/p/tcl/mailman/message/59333692/

--- lang/tcl86/files/patch-unix-tclUnixInit.c
+++ lang/tcl86/files/patch-unix-tclUnixInit.c
@@ -1,6 +1,6 @@
---- unix/tclUnixInit.c.orig	2020-12-11 17:46:23 UTC
+--- unix/tclUnixInit.c.orig	2026-04-17 18:51:37 UTC
 +++ unix/tclUnixInit.c
-@@ -395,14 +395,6 @@ TclpInitPlatform(void)
+@@ -391,14 +391,6 @@ TclpInitPlatform(void)
  #endif /* SIGPIPE */
  
  #if defined(__FreeBSD__) && defined(__GNUC__)
