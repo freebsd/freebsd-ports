a6187013727e8bc0a069eca486f40be982192c82
Jan Beich <jbeich@FreeBSD.org>
2026-05-14 01:13:38 +0200

    multimedia/aom: update to 3.14.0
    Changes:	https://aomedia.googlesource.com/aom/+log/v3.13.3..v3.14.0
    Reported by:	Repology

--- multimedia/aom/files/patch-cmake_pkg__config.cmake
+++ multimedia/aom/files/patch-cmake_pkg__config.cmake
@@ -1,7 +1,7 @@
 - Limit internal dependencies to static library
 
---- build/cmake/pkg_config.cmake.orig	2022-06-17 17:46:18 UTC
-+++ build/cmake/pkg_config.cmake
+--- cmake/pkg_config.cmake.orig	2022-06-17 17:46:18 UTC
++++ cmake/pkg_config.cmake
 @@ -51,7 +51,7 @@ file(APPEND "${pkgconfig_file}" "Version: ${package_ve
    APPEND "${pkgconfig_file}"
    "Description: Alliance for Open Media AV1 codec library v${aom_version}.\n")
