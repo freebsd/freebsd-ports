625ea72bc60edccccfd8939a976219c04e021b3c
Dave Cottlehuber <dch@FreeBSD.org>
2026-05-14 06:56:08 +0000

    net-mgmt/simplomon: Fix build on 16-CURRENT
    Reported by:	pkg-fallout
    Sponsored by:	SkunkWerks, GmbH

---
+++ net-mgmt/simplomon/files/patch-notifiers.hh
@@ -0,0 +1,8 @@
+--- notifiers.hh.orig	2026-05-09 08:36:59 UTC
++++ notifiers.hh
+@@ -1,4 +1,5 @@
+ #pragma once
++#include <type_traits>
+ #include <string>
+ #include "sol/sol.hpp"
+ #include "sclasses.hh"
