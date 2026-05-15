b0732514fe4014297ef1fea80aa1005e2cfc0726
Älven <alven@FreeBSD.org>
2026-05-16 03:10:53 +0400

    filesystems/httpdirfs: Update 1.2.7 => 1.2.9
    * Fit COMMENT within 70 characters limit
    * Install more documentation files
    * Prefer using pkg-plist for tracking installed files
    * Combine 2 patches for the same file into one, remove upstreamed hunk
    
    Approved by:	    	db@, yuri@ (Mentors, implicit)

--- filesystems/httpdirfs/files/patch-src_link.c
+++ filesystems/httpdirfs/files/patch-src_link.c
@@ -1,6 +1,46 @@
---- src/link.c.orig	2025-01-12 11:28:19 UTC
+--- src/link.c.orig	2026-05-13 00:00:39 UTC
 +++ src/link.c
-@@ -995,7 +995,7 @@ static CURL *Link_download_curl_setup(Link *link, size
+@@ -66,14 +66,14 @@ static CURL *Link_to_curl(Link *link)
+     if (ret) {
+         lprintf(error, "%s", curl_easy_strerror(ret));
+     }
+-    ret = curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
++    ret = curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+     if (ret) {
+         lprintf(error, "%s", curl_easy_strerror(ret));
+     }
+     /*
+      * for following directories without the '/'
+      */
+-    ret = curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 2);
++    ret = curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 2L);
+     if (ret) {
+         lprintf(error, "%s", curl_easy_strerror(ret));
+     }
+@@ -81,11 +81,11 @@ static CURL *Link_to_curl(Link *link)
+     if (ret) {
+         lprintf(error, "%s", curl_easy_strerror(ret));
+     }
+-    ret = curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1);
++    ret = curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+     if (ret) {
+         lprintf(error, "%s", curl_easy_strerror(ret));
+     }
+-    ret = curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 15);
++    ret = curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 15L);
+     if (ret) {
+         lprintf(error, "%s", curl_easy_strerror(ret));
+     }
+@@ -117,7 +117,7 @@ static CURL *Link_to_curl(Link *link)
+         }
+     }
+     if (CONFIG.insecure_tls) {
+-        ret = curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
++        ret = curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+         if (ret) {
+             lprintf(error, "%s", curl_easy_strerror(ret));
+         }
+@@ -1051,7 +1051,7 @@ static CURL *Link_download_curl_setup(Link *link, size
      size_t end = start + req_size - 1;
  
      char range_str[64];
