6013190848cd2af6709fafa640a5d86a9a751199
Gleb Popov <arrowd@FreeBSD.org>
2026-05-15 11:44:08 +0300

    x11-toolkits/wlroots*: Regenerate the libinput using makepatch and add an upstream ref
    PR:		293585
    Reported by:	jbeich
    Fixes:	64eca8082d8905dc5ce90863d8a3ec21a4a01cbd

--- x11-toolkits/wlroots015/files/patch-backend_libinput_switch.c
+++ x11-toolkits/wlroots015/files/patch-backend_libinput_switch.c
@@ -1,5 +1,7 @@
---- backend/libinput/switch.c.orig	2022-02-03 13:19:54.000000000 -0800
-+++ backend/libinput/switch.c	2026-04-08 15:45:12.489184000 -0700
+https://gitlab.freedesktop.org/wlroots/wlroots/-/commit/c1452d881147
+
+--- backend/libinput/switch.c.orig	2022-02-03 21:19:54 UTC
++++ backend/libinput/switch.c
 @@ -40,6 +40,8 @@ void handle_switch_toggle(struct libinput_event *event
  	case LIBINPUT_SWITCH_TABLET_MODE:
  		wlr_event.switch_type = WLR_SWITCH_TYPE_TABLET_MODE;
--- x11-toolkits/wlroots018/files/patch-backend_libinput_switch.c
+++ x11-toolkits/wlroots018/files/patch-backend_libinput_switch.c
@@ -1,6 +1,8 @@
---- backend/libinput/switch.c.orig	2022-02-03 13:19:54.000000000 -0800
-+++ backend/libinput/switch.c	2026-04-08 15:45:12.489184000 -0700
-@@ -40,6 +40,8 @@ void handle_switch_toggle(struct libinput_event *event
+https://gitlab.freedesktop.org/wlroots/wlroots/-/commit/c1452d881147
+
+--- backend/libinput/switch.c.orig	2025-10-23 10:00:25 UTC
++++ backend/libinput/switch.c
+@@ -36,6 +36,8 @@ void handle_switch_toggle(struct libinput_event *event
  	case LIBINPUT_SWITCH_TABLET_MODE:
  		wlr_event.switch_type = WLR_SWITCH_TYPE_TABLET_MODE;
  		break;
