7086525af5d39c9964269bbe7c00835f4d72f075
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2026-05-15 22:15:11 +0200

    mail/thunderbird: update to 151.0 (rc1)
    Release Notes (soon):
      https://www.thunderbird.net/en-US/thunderbird/151.0/releasenotes/
    
    (cherry picked from commit 6f30752564b9e3ee44e8a1a1eeb5b0dfd2dd66ee)

--- mail/thunderbird/files/patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland__egl__dmabuf.cc
+++ mail/thunderbird/files/patch-third__party_libwebrtc_modules_desktop__capture_linux_wayland__egl__dmabuf.cc
@@ -1,10 +1,10 @@
-commit f53507857647459d4a48935901f80f27a5ae58a9
+commit 7ee076ab9ca300348d5662a894bf2d27e6724837
 Author: Christoph Moench-Tegeder <cmt@FreeBSD.org>
 
     avoid linux-only includes
 
 diff --git third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc
-index 821fdfb487b7..e000c1c104fe 100644
+index c8653928d9e9..d380fc1b22f1 100644
 --- third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc
 +++ third_party/libwebrtc/modules/desktop_capture/linux/wayland/egl_dmabuf.cc
 @@ -15,7 +15,9 @@
@@ -17,3 +17,13 @@ index 821fdfb487b7..e000c1c104fe 100644
  #include <dlfcn.h>
  #include <fcntl.h>
  #include <gbm.h>
+@@ -23,7 +25,9 @@
+ #include <libdrm/drm_fourcc.h>
+ #include <spa/param/video/raw.h>
+ #include <sys/stat.h>
++#if !defined(__FreeBSD__)
+ #include <sys/sysmacros.h>
++#endif
+ #include <sys/types.h>
+ #include <unistd.h>
+ #include <xf86drm.h>
