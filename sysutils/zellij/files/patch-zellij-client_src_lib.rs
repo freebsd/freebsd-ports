85a2b6b74d5ec56aa06e64e1acd55b076eb7762d
Ashish SHUKLA <ashish@FreeBSD.org>
2026-05-17 14:34:47 +0000

    sysutils/zellij: Update to 0.44.3

--- sysutils/zellij/files/patch-zellij-client_src_lib.rs
+++ sysutils/zellij/files/patch-zellij-client_src_lib.rs
@@ -1,27 +1,22 @@
---- zellij-client/src/lib.rs.orig	2026-03-23 08:03:25 UTC
+--- zellij-client/src/lib.rs.orig	2026-05-13 07:48:47 UTC
 +++ zellij-client/src/lib.rs
-@@ -641,7 +641,7 @@ pub fn start_remote_client(
+@@ -683,12 +683,12 @@ pub fn start_remote_client(
  
      let reset_controlling_terminal_state = |e: String, exit_status: i32| {
          os_input.disable_mouse().non_fatal();
 -        os_input.unset_raw_mode().unwrap();
 +        let _ = os_input.unset_raw_mode();
          os_input.restore_console_mode();
-         let goto_start_of_last_line = format!("\u{1b}[{};{}H", full_screen_ws.rows, 1);
-         let restore_alternate_screen = "\u{1b}[?1049l";
-@@ -659,9 +659,8 @@ pub fn start_remote_client(
-         );
-         let _ = os_input
-             .get_stdout_writer()
--            .write(error.as_bytes())
--            .unwrap();
--        let _ = os_input.get_stdout_writer().flush().unwrap();
-+            .write(error.as_bytes());
-+        let _ = os_input.get_stdout_writer().flush();
+         let error = terminal_teardown_message(&e, full_screen_ws.rows, true);
+         let mut stdout = os_input.get_stdout_writer();
+-        stdout.write_all(error.as_bytes()).unwrap();
+-        stdout.flush().unwrap();
++        let _ = stdout.write_all(error.as_bytes());
++        let _ = stdout.flush();
          if exit_status == 0 {
              log::info!("{}", e);
          } else {
-@@ -1249,21 +1248,21 @@ pub fn start_client(
+@@ -1264,16 +1264,16 @@ pub fn start_client(
  
          os_input.disable_mouse().non_fatal();
          info!("{}", exit_msg);
@@ -29,23 +24,16 @@
 +        let _ = os_input.unset_raw_mode();
          os_input.restore_console_mode();
          let mut stdout = os_input.get_stdout_writer();
-         let exit_kitty_keyboard_mode = "\u{1b}[<1u";
-         if !explicitly_disable_kitty_keyboard_protocol {
--            let _ = stdout.write(exit_kitty_keyboard_mode.as_bytes()).unwrap();
--            stdout.flush().unwrap();
-+            let _ = stdout.write(exit_kitty_keyboard_mode.as_bytes());
-+            let _ = stdout.flush();
-         }
--        let _ = stdout.write(goodbye_message.as_bytes()).unwrap();
+-        stdout.write_all(goodbye_message.as_bytes()).unwrap();
 -        stdout.flush().unwrap();
-+        let _ = stdout.write(goodbye_message.as_bytes());
++        let _ = stdout.write_all(goodbye_message.as_bytes());
 +        let _ = stdout.flush();
      } else {
          let clear_screen = "\u{1b}[2J";
          let mut stdout = os_input.get_stdout_writer();
--        let _ = stdout.write(clear_screen.as_bytes()).unwrap();
+-        stdout.write_all(clear_screen.as_bytes()).unwrap();
 -        stdout.flush().unwrap();
-+        let _ = stdout.write(clear_screen.as_bytes());
++        let _ = stdout.write_all(clear_screen.as_bytes());
 +        let _ = stdout.flush();
      }
  
