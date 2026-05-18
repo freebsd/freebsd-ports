11a4dba5bb9dc59924b10137fbe5cbe2ff1f7d80
Stefan Schlosser <bsdcode@disroot.org>
2026-05-18 15:54:16 +0200

    emulators/libmt32emu: Update to 2.8.0
    * Add testing.
    * Remove obsolete patch.
    
    Changelog:
    https://github.com/munt/munt/compare/libmt32emu_2_7_3...libmt32emu_2_8_0
    
    PR:		295385

--- emulators/libmt32emu/files/patch-cmake_project__data.cmake
+++
@@ -1,9 +0,0 @@
---- cmake/project_data.cmake.orig	2025-12-26 14:07:56 UTC
-+++ cmake/project_data.cmake
-@@ -4,5 +4,5 @@ set(libmt32emu_VERSION_MINOR 7)
- 
- set(libmt32emu_VERSION_MAJOR 2)
- set(libmt32emu_VERSION_MINOR 7)
--set(libmt32emu_VERSION_PATCH 2)
-+set(libmt32emu_VERSION_PATCH 3)
- set(libmt32emu_VERSION "${libmt32emu_VERSION_MAJOR}.${libmt32emu_VERSION_MINOR}.${libmt32emu_VERSION_PATCH}")
