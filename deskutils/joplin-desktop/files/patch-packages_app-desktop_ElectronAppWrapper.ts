7535ae4091c2a3dea18603bff1a8e5afa9ad7d6d
Hiroki Tagato <tagattie@FreeBSD.org>
2026-05-19 07:10:00 +0900

    deskutils/joplin-desktop: Update to 3.6.13
    Release Highlights: https://joplinapp.org/news/20260505-release-3-6
    
    Reported by:	GitHub (watch releases)

--- deskutils/joplin-desktop/files/patch-packages_app-desktop_ElectronAppWrapper.ts
+++ deskutils/joplin-desktop/files/patch-packages_app-desktop_ElectronAppWrapper.ts
@@ -1,6 +1,6 @@
---- packages/app-desktop/ElectronAppWrapper.ts.orig	2026-01-10 10:01:41 UTC
+--- packages/app-desktop/ElectronAppWrapper.ts.orig	2026-05-12 09:52:21 UTC
 +++ packages/app-desktop/ElectronAppWrapper.ts
-@@ -254,7 +254,7 @@ export default class ElectronAppWrapper {
+@@ -284,7 +284,7 @@ export default class ElectronAppWrapper {
  
  		// Linux icon workaround for bug https://github.com/electron-userland/electron-builder/issues/2098
  		// Fix: https://github.com/electron-userland/electron-builder/issues/2269
--- deskutils/joplin-desktop/files/patch-packages_lib_models_settings_builtInMetadata.ts
+++ deskutils/joplin-desktop/files/patch-packages_lib_models_settings_builtInMetadata.ts
@@ -1,6 +1,6 @@
---- packages/lib/models/settings/builtInMetadata.ts.orig	2026-01-10 10:01:41 UTC
+--- packages/lib/models/settings/builtInMetadata.ts.orig	2026-05-12 09:52:21 UTC
 +++ packages/lib/models/settings/builtInMetadata.ts
-@@ -1863,7 +1863,7 @@ const builtInMetadata = (Setting: typeof SettingType) 
+@@ -1922,7 +1922,7 @@ const builtInMetadata = (Setting: typeof SettingType) 
  			appTypes: [AppType.Desktop],
  			label: () => 'Enable keychain support',
  			description: () => 'This is an experimental setting to enable keychain support on Linux',
