2006c7c95eb1b81b1872c97d0716fc7761a0e940
Vladimir Druzenko <vvd@FreeBSD.org>
2026-05-20 14:21:21 +0300

    sysutils/beats94: Repocopy from sysutils/beats93
    PR:		293746
    Approved by:	girgen (elastic, maintainer)
    Sponsored by:	UNIS Labs

---
+++ sysutils/beats94/files/patch-dev-tools_mage_settings.go
@@ -0,0 +1,20 @@
+--- dev-tools/mage/settings.go.orig	2026-02-23 21:59:26 UTC
++++ dev-tools/mage/settings.go
+@@ -31,7 +31,6 @@ import (
+ 	"sync"
+ 	"time"
+ 
+-	"github.com/magefile/mage/sh"
+ 	"golang.org/x/text/cases"
+ 	"golang.org/x/text/language"
+ 	"gopkg.in/yaml.v3"
+@@ -282,7 +281,8 @@ func CommitHash() (string, error) {
+ func CommitHash() (string, error) {
+ 	var err error
+ 	commitHashOnce.Do(func() {
+-		commitHash, err = sh.Output("git", "rev-parse", "HEAD")
++		err = nil
++		commitHash = "%%BEATSVERSION%%"
+ 	})
+ 	return commitHash, err
+ }
