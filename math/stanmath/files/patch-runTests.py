48babdb6fc1e81420df18eaa644ef4de1ac2f326
Yuri Victorovich <yuri@FreeBSD.org>
2026-05-19 23:00:27 -0700

    math/stanmath: update 4.8.1 → 5.3.0

--- math/stanmath/files/patch-runTests.py
+++ math/stanmath/files/patch-runTests.py
@@ -1,11 +1,20 @@
---- runTests.py.orig	2021-08-28 03:33:35 UTC
+--- runTests.py.orig	2026-05-20 05:57:17 UTC
 +++ runTests.py
-@@ -172,7 +172,7 @@ def generateTests(j):
-     if isWin():
-         doCommand("mingw32-make -j%d generate-tests -s" % (j or 1))
-     else:
--        doCommand("make -j%d generate-tests -s" % (j or 1))
-+        doCommand("gmake -j%d generate-tests -s" % (j or 1))
+@@ -185,7 +185,7 @@ def generateTests(j):
+ 
+ def generateTests(j):
+     """Generate all tests and pass along the j parameter to make."""
+-    doCommand("make -j%d generate-tests -s" % (j or 1))
++    doCommand("gmake -j%d generate-tests -s" % (j or 1))
  
  
  def divide_chunks(l, n):
+@@ -243,7 +243,7 @@ def makeTest(name, j):
+ 
+ def makeTest(name, j):
+     """Run the make command for a given single test."""
+-    doCommand("make -j%d %s" % (j or 1, name))
++    doCommand("gmake -j%d %s" % (j or 1, name))
+ 
+ 
+ def commandExists(command):
