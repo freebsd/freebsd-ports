7a483af1cf9e8a97018eca2c6e248eb5dcf2966d
Siva Mahadevan <siva@FreeBSD.org>
2026-05-20 15:39:40 -0400

    x11-wm/river: update to 0.4.5
    Approved by:	lwhsu (mentor, implicitly)

--- x11-wm/river/Makefile.zig
+++ x11-wm/river/Makefile.zig
@@ -1,6 +1,7 @@
-ZIG_TUPLE=	pixman:codeberg.org/ifreund/zig-pixman/archive/v0.3.0.tar.gz?dummy=/pixman-0.3.0.tar.gz:pixman-0.3.0-LClMnz2VAAAs7QSCGwLimV5VUYx0JFnX5xWU6HwtMuDX \
-		wayland:codeberg.org/ifreund/zig-wayland/archive/v0.4.0.tar.gz?dummy=/wayland-0.4.0.tar.gz:wayland-0.4.0-lQa1khbMAQAsLS2eBR7M5lofyEGPIbu2iFDmoz8lPC27 \
-		wayland:codeberg.org/ifreund/zig-wayland/archive/v0.5.0.tar.gz?dummy=/wayland-0.5.0.tar.gz:wayland-0.5.0-lQa1knz8AQCh08NA8BeQrwJB9U3CfqcVAdHZYGRKIGuu \
-		wlroots:codeberg.org/ifreund/zig-wlroots/archive/v0.20.0.tar.gz?dummy=/wlroots-0.20.0.tar.gz:wlroots-0.20.0-jmOlcmtCBADS6eoJ6mkeiSNZkibrhD-c5Qwn-LiM86r1 \
+ZIG_TUPLE=	aro:github.com/Vexu/arocc/archive/5f5a050569a95ecc40a426f0c3666ae7e.tar.gz?dummy=/aro-5f5a050569a95ecc40a426f0c3666ae7e.tar.gz:aro-0.0.0-JSD1Qi7QNgDnfcrdEJf82v3o6MhZySjYVrtdfEf3E4Se \
+		pixman:codeberg.org/ifreund/zig-pixman/archive/v0.3.0.tar.gz?dummy=/pixman-0.3.0.tar.gz:pixman-0.3.0-LClMnz2VAAAs7QSCGwLimV5VUYx0JFnX5xWU6HwtMuDX \
+		translate_c:codeberg.org/ziglang/translate-c/archive/57c559cf581b1fcad90494eda219f98ab.tar.gz?dummy=/translate-c-57c559cf581b1fcad90494eda219f98ab.tar.gz:translate_c-0.0.0-Q_BUWlX1BgCD1wo6uo97prlp9VJ4gxAjwN_vZ7nsSjGN \
+		wayland:codeberg.org/ifreund/zig-wayland/archive/v0.6.0.tar.gz?dummy=/wayland-0.6.0.tar.gz:wayland-0.6.0-lQa1kqz8AQADQmdNJsNhLoNHcnEGEUjrOaPV-dtEnEmX \
+		wlroots:codeberg.org/ifreund/zig-wlroots/archive/v0.20.1.tar.gz?dummy=/wlroots-0.20.1.tar.gz:wlroots-0.20.1-jmOlcqNVBAB3uB5oqBTzpRlwu-FmMyyZMVAWCe5kmcSt \
 		xkbcommon:codeberg.org/ifreund/zig-xkbcommon/archive/v0.3.0.tar.gz?dummy=/xkbcommon-0.3.0.tar.gz:xkbcommon-0.3.0-VDqIe3K9AQB2fG5ZeRcMC9i7kfrp5m2rWgLrmdNn9azr \
 		xkbcommon:codeberg.org/ifreund/zig-xkbcommon/archive/v0.4.0.tar.gz?dummy=/xkbcommon-0.4.0.tar.gz:xkbcommon-0.4.0-VDqIe0i2AgDRsok2GpMFYJ8SVhQS10_PI2M_CnHXsJJZ
