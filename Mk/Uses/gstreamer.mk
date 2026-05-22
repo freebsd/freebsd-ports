1d35c6d802c39a0a57a674984a89d8c8f74d6e21
Jason E. Hale <jhale@FreeBSD.org>
2026-05-22 12:18:19 -0400

    GStreamer1: Update to 1.28.3
    multimedia/gstreamer1-plugins-rust: Update to 0.15.2
    
    Changes:
    https://gstreamer.freedesktop.org/releases/1.28/#1.28.2
    https://gstreamer.freedesktop.org/releases/1.28/#1.28.3
    
    MFH:		2026Q2
    Security:	05aadfcc-55f5-11f1-915c-8974b59277b5,
    		ea4c5b9d-55f7-11f1-915c-8974b59277b5

--- Mk/Uses/gstreamer.mk
+++ Mk/Uses/gstreamer.mk
@@ -42,7 +42,7 @@ _GST_VER=	${_GST_VER_DEFAULT}
 
 # When modifying _GST1_VERSION, run `make -C ${PORTSDIR}/multimedia/gstreamer1 makesum-all`
 # to update the distinfo files for the entire suite.
-_GST1_VERSION=		1.28.1
+_GST1_VERSION=		1.28.3
 _GST1_LIB_VER=		1.0
 _GST1_SOVERSION=	0.${_GST1_VERSION:R:E}${${_GST1_VERSION:E} > 9:?:0}${_GST1_VERSION:E}.0
 
@@ -353,7 +353,7 @@ gst-rtsp-server_IMPL=	#
 
 gst-rust_PORT=		multimedia/gstreamer${_GST_VER}-plugins-rust
 gst-rust_IMPL=		#
-gst-rust_GST1_VERSION=	0.15.1
+gst-rust_GST1_VERSION=	0.15.2
 gst-rust_VERSION=	${gst-rust_GST${_GST_VER}_VERSION}
 
 gst-smoothstreaming_PORT=	multimedia/gstreamer${_GST_VER}-plugins-smoothstreaming
