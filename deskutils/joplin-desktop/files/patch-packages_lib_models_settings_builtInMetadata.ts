f8f7f3f294d3fab1d405d785682ecb03d49b49fd
Hiroki Tagato <tagattie@FreeBSD.org>
2026-05-25 20:07:56 +0900

    deskutils/joplin-desktop: Update to 3.6.14
    Changelog: https://github.com/laurent22/joplin/releases/tag/v3.6.14
    
    Reported by:	GitHub (watch releases)

--- deskutils/joplin-desktop/files/patch-packages_lib_models_settings_builtInMetadata.ts
+++ deskutils/joplin-desktop/files/patch-packages_lib_models_settings_builtInMetadata.ts
@@ -1,6 +1,6 @@
---- packages/lib/models/settings/builtInMetadata.ts.orig	2026-05-12 09:52:21 UTC
+--- packages/lib/models/settings/builtInMetadata.ts.orig	2026-05-19 14:33:24 UTC
 +++ packages/lib/models/settings/builtInMetadata.ts
-@@ -1922,7 +1922,7 @@ const builtInMetadata = (Setting: typeof SettingType) 
+@@ -1939,7 +1939,7 @@ const builtInMetadata = (Setting: typeof SettingType) 
  			appTypes: [AppType.Desktop],
  			label: () => 'Enable keychain support',
  			description: () => 'This is an experimental setting to enable keychain support on Linux',
