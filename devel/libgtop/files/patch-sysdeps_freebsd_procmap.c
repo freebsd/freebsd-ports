6fe0bef5a5f5b7c87e8f394b04e6dbecd83c961e
Chris Torek <chris.torek@gmail.com>
2026-05-24 22:39:30 -0700

    devel/libgtop: Fix build on 16.0-CURRENT
    ugly patch to make build on freebsd
    
    As with the lsof patch (bug #395414), this makes things build on
    16-current, but I don't know if it breaks older supported releases.
    
    Build tested on: (truckman)
      14.3-RELEASE, 14.4-RELEASE amd64 and i386
      15.0-RELEASE, 15.1-RC1 amd64
      16.0-CURRENT amd64
    
    PR:		295284

--- devel/libgtop/files/patch-sysdeps_freebsd_procmap.c
+++ devel/libgtop/files/patch-sysdeps_freebsd_procmap.c
@@ -1,6 +1,30 @@
---- sysdeps/freebsd/procmap.c.orig	2023-01-15 10:43:14 UTC
+--- sysdeps/freebsd/procmap.c.orig
 +++ sysdeps/freebsd/procmap.c
-@@ -26,6 +26,8 @@
+@@ -20,12 +20,32 @@
+ */
+ 
+ #include <config.h>
++
++/*
++ * FreeBSD-16 specific stuff that shouldn't hurt earlier FreeBSD:
++ *
++ * Need sys/event.h early with _KERNEL, before glibtop/procmap.h.
++ * This gets us macros for some inline vnode functions that crop up
++ * with _KERNEL later.
++ *
++ * After that, we need to neuter MPASS by including kassert.h.  Last,
++ * a boolean_t conflict appears (sys/types.h vs vm/vm.h), so whack it as well.
++ */
++#define _KERNEL
++#include <sys/types.h>
++#include <sys/event.h>
++#include <sys/kassert.h>
++#undef _KERNEL
++#define HAVE_BOOLEAN /* until vm/vm.h is cleaned-up */
++
+ #include <glibtop.h>
+ #include <glibtop/error.h>
+ #include <glibtop/procmap.h>
  
  #include <glibtop_suid.h>
  
@@ -9,7 +33,15 @@
  #include <kvm.h>
  #include <sys/param.h>
  #include <sys/proc.h>
-@@ -51,12 +53,14 @@
+@@ -43,6 +63,7 @@
+ #define _WANT_FILE
+ #include <sys/file.h>
+ #undef _WANT_FILE
++#include <sys/systm.h> /* early and without _KERNEL, for FreeBSD 16 */
+ #else
+ #define _KERNEL
+ #include <sys/file.h>
+@@ -51,12 +72,14 @@
  #define _KERNEL
  #include <sys/mount.h>
  #include <ufs/ufs/quota.h>
@@ -25,7 +57,7 @@
  
  
  #if (__FreeBSD_version >= 1101001)
-@@ -100,7 +104,7 @@ _glibtop_sysdeps_freebsd_dev_inode (glibtop *server, s
+@@ -100,7 +123,7 @@ _glibtop_sysdeps_freebsd_dev_inode (glibtop *server, struct vnode *vnode,
          *inum = 0;
          *dev = 0;
  
@@ -34,7 +66,7 @@
   	             (char *) &tagptr, sizeof (tagptr)) != sizeof (tagptr) ||
              kvm_read (server->machine->kd, (gulong) tagptr,
  		     (char *) tagstr, sizeof (tagstr)) != sizeof (tagstr))
-@@ -120,7 +124,7 @@ _glibtop_sysdeps_freebsd_dev_inode (glibtop *server, s
+@@ -120,7 +143,7 @@ _glibtop_sysdeps_freebsd_dev_inode (glibtop *server, struct vnode *vnode,
                  return;
          }
  
