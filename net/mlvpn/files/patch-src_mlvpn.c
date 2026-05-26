ad986f4312e3dac10b3a3140e9ef85cf7dae495f
Olivier Cochard <olivier@FreeBSD.org>
2026-05-26 11:55:38 +0200

    net/mlvpn: Update to 2.3.5

--- net/mlvpn/files/patch-src_mlvpn.c
+++ net/mlvpn/files/patch-src_mlvpn.c
@@ -1,54 +1,26 @@
---- src/mlvpn.c.orig	2015-12-28 11:20:16 UTC
+--- src/mlvpn.c.orig	2026-05-23 10:58:16 UTC
 +++ src/mlvpn.c
-@@ -597,7 +597,7 @@ mlvpn_rtun_write(EV_P_ ev_io *w, int rev
- 
- mlvpn_tunnel_t *
- mlvpn_rtun_new(const char *name,
--               const char *bindaddr, const char *bindport,
-+               const char *bindaddr, const char *bindport, uint32_t bindfib,
-                const char *destaddr, const char *destport,
-                int server_mode, uint32_t timeout,
-                int fallback_only, uint32_t bandwidth,
-@@ -653,6 +653,7 @@ mlvpn_rtun_new(const char *name,
-         strlcpy(new->bindaddr, bindaddr, sizeof(new->bindaddr));
-     if (bindport)
-         strlcpy(new->bindport, bindport, sizeof(new->bindport));
-+	new->bindfib = bindfib;
-     if (destaddr)
-         strlcpy(new->destaddr, destaddr, sizeof(new->destaddr));
-     if (destport)
-@@ -780,6 +781,7 @@ static int
- mlvpn_rtun_start(mlvpn_tunnel_t *t)
- {
-     int ret, fd = -1;
-+	int fib = 0;
-     char *addr, *port;
-     struct addrinfo hints, *res;
- 
-@@ -788,9 +790,11 @@ mlvpn_rtun_start(mlvpn_tunnel_t *t)
-     {
-         addr = t->bindaddr;
-         port = t->bindport;
-+		fib = t->bindfib;
-     } else {
-         addr = t->destaddr;
-         port = t->destport;
-+		fib = t->bindfib;
-     }
- 
-     /* Initialize hints */
-@@ -817,6 +821,14 @@ mlvpn_rtun_start(mlvpn_tunnel_t *t)
-             log_warn(NULL, "%s socket creation error",
+@@ -847,13 +847,17 @@ mlvpn_rtun_start(mlvpn_tunnel_t *t)
                  t->name);
          } else {
+             /* Setting fib/routing-table is supported on FreeBSD and OpenBSD only */
+-#if defined(HAVE_FREEBSD)
+-            if (fib > 0 && setsockopt(fd, SOL_SOCKET, SO_SETFIB, &fib, sizeof(fib)) < 0)
+-#elif defined(HAVE_OPENBSD)
+-            if (fib > 0 && setsockopt(fd, SOL_SOCKET, SO_RTABLE, &fib, sizeof(fib)) < 0)
 +#if defined(HAVE_FREEBSD) || defined(HAVE_OPENBSD)
-+			/* Setting SO_SETFIB (fib) supported on FreeBSD and OpenBSD only */
-+			if (setsockopt(fd, SOL_SOCKET, SO_SETFIB, &fib, sizeof(fib)) < 0)
-+			{
-+      			log_warnx(NULL, "Cannot set FIB %d for kernel socket", fib);
-+				goto error;
-+			}
++            if (fib > 0)
+             {
+-                log_warn(NULL, "Cannot set FIB %d for kernel socket", fib);
+-                goto error;
++#if defined(HAVE_FREEBSD)
++                if (setsockopt(fd, SOL_SOCKET, SO_SETFIB, &fib, sizeof(fib)) < 0) {
++#else
++                if (setsockopt(fd, SOL_SOCKET, SO_RTABLE, &fib, sizeof(fib)) < 0) {
 +#endif
++                    log_warn(NULL, "Cannot set FIB %d for kernel socket", fib);
++                    goto error;
++                }
+             }
+ #endif
              t->fd = fd;
-             break;
-         }
