7f5fe7ae4ca336a604a98e2e717ee8ced7b0769c
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2026-05-26 12:55:59 +0800

    databases/py-mycli: Update version requirement of RUN_DEPENDS
    - Bump PORTREVISION for package change

--- databases/py-mycli/files/patch-setup.py
+++ databases/py-mycli/files/patch-setup.py
@@ -1,6 +1,6 @@
 --- setup.py.orig	2024-11-03 18:30:36 UTC
 +++ setup.py
-@@ -24,11 +24,11 @@ install_requirements = [
+@@ -24,7 +24,7 @@ install_requirements = [
      'Pygments>=1.6',
      'prompt_toolkit>=3.0.6,<4.0.0',
      'PyMySQL >= 0.9.2',
@@ -9,8 +9,3 @@
      'sqlglot>=5.1.3',
      'configobj >= 5.0.5',
      'cli_helpers[styles] >= 2.2.1',
--    'pyperclip >= 1.8.1',
-+    'pyperclip >= 1.7.0',
-     'pyaes >= 1.6.1',
-     'pyfzf >= 0.3.1',
- ]
