caabb2c246827dd3fc7fa03e57770a12fff06a4a
Alex Dupre <ale@FreeBSD.org>
2026-05-28 12:44:48 +0200

    mail/roundcube: update to 1.7.1 release
    PR:		293172
    Submitted by:	brnrd

--- mail/roundcube/files/patch-program_lib_Roundcube_rcube__message.php
+++ mail/roundcube/files/patch-program_lib_Roundcube_rcube__message.php
@@ -1,12 +1,14 @@
---- program/lib/Roundcube/rcube_message.php.orig	2022-06-26 20:26:58 UTC
+--- program/lib/Roundcube/rcube_message.php.orig	2026-05-24 08:01:37 UTC
 +++ program/lib/Roundcube/rcube_message.php
-@@ -914,8 +914,7 @@ class rcube_message
-                 else if (
+@@ -1006,9 +1006,8 @@ class rcube_message
+                 elseif (
                      preg_match('/^(inline|attach)/', $mail_part->disposition)
                      || !empty($mail_part->headers['content-id'])
--                    || ($mail_part->filename &&
--                        (empty($mail_part->disposition) || preg_match('/^[a-z0-9!#$&.+^_-]+$/i', $mail_part->disposition)))
+-                    || ($mail_part->filename
+-                        && (empty($mail_part->disposition) || preg_match('/^[a-z0-9!#$&.+^_-]+$/i', $mail_part->disposition)))
+-                ) {
 +                    || $mail_part->filename
-                 ) {
++		) {
                      // skip apple resource forks
                      if ($message_ctype_secondary == 'appledouble' && $secondary_type == 'applefile') {
+                         continue;
