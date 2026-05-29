b1346a408014d2319a633622a2099c3fa1275d90
Jason W. Bacon <jwb@FreeBSD.org>
2026-05-29 16:48:39 -0500

    biology/sra-tools: lib64 -> lib
    Patch to better respect filesystem hierarchy.  The lib64 default was
    left alone originally because the old esoteric build system was
    difficult to patch.  The new system is much simpler.  Reporter
    indicated that the presence of ${PREFIX}/lib64 causes build failures
    in some other ports.
    
    PR:             294837
    Reported by:    milios@ccsys.com

--- biology/sra-tools/files/patch-build_env.cmake
+++ biology/sra-tools/files/patch-build_env.cmake
@@ -1,4 +1,4 @@
---- build/env.cmake.orig	2026-05-12 21:39:17 UTC
+--- build/env.cmake.orig	2026-03-25 20:33:21 UTC
 +++ build/env.cmake
 @@ -470,8 +470,8 @@ if( Python3_EXECUTABLE )
    find_package( Python3 COMPONENTS Interpreter Development )
@@ -11,3 +11,12 @@
   endif()
  endif()
  
+@@ -485,7 +485,7 @@ if ( NOT CMAKE_INSTALL_LIBDIR )
+ endif()
+ 
+ if ( NOT CMAKE_INSTALL_LIBDIR )
+-    set( CMAKE_INSTALL_LIBDIR ${CMAKE_INSTALL_PREFIX}/lib64 )
++    set( CMAKE_INSTALL_LIBDIR ${CMAKE_INSTALL_PREFIX}/lib )
+ endif()
+ 
+ function( ExportStatic name install )
