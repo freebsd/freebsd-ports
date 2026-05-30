1c960ccb48ff89ae505e55999cab283579fc3e2a
Robert Nagy <rnagy@FreeBSD.org>
2026-05-30 14:47:48 +0200

    www/chromium: update to 148.0.7778.215
    Security:	https://vuxml.freebsd.org/freebsd/e725f5d5-5c24-11f1-b189-a8a1599412c6.html
    PR:		295495

--- www/chromium/files/patch-chrome_browser_ui_views_frame_browser__view.cc
+++ www/chromium/files/patch-chrome_browser_ui_views_frame_browser__view.cc
@@ -1,6 +1,6 @@
---- chrome/browser/ui/views/frame/browser_view.cc.orig	2026-05-13 07:46:05 UTC
+--- chrome/browser/ui/views/frame/browser_view.cc.orig	2026-05-30 09:39:35 UTC
 +++ chrome/browser/ui/views/frame/browser_view.cc
-@@ -2472,7 +2472,7 @@ void BrowserView::ToolbarSizeChanged(bool is_animating
+@@ -2473,7 +2473,7 @@ void BrowserView::ToolbarSizeChanged(bool is_animating
  }
  
  void BrowserView::TabDraggingStatusChanged(bool is_dragging) {
@@ -9,7 +9,7 @@
    UpdateFastResizeForContentViews(is_dragging);
  
    if (!is_dragging) {
-@@ -5994,7 +5994,7 @@ void BrowserView::MaybeShowProfileSwitchIPH() {
+@@ -5996,7 +5996,7 @@ void BrowserView::MaybeShowProfileSwitchIPH() {
  }
  
  void BrowserView::MaybeShowSupervisedUserProfileSignInIPH() {
@@ -18,7 +18,7 @@
    if (!ShouldShowAvatarToolbarIPH()) {
      return;
    }
-@@ -6004,7 +6004,7 @@ void BrowserView::MaybeShowSupervisedUserProfileSignIn
+@@ -6006,7 +6006,7 @@ void BrowserView::MaybeShowSupervisedUserProfileSignIn
  }
  
  void BrowserView::MaybeShowSignInBenefitsIPH() {
