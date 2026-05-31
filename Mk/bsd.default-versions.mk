49e725a68ebeeabf565d595b1915f9d796638481
Rene Ladan <rene@FreeBSD.org>
2026-05-31 21:27:41 +0200

    Revert: "Mk: unregister expired www/node20"
    www/node20 is still in use, my search was incomplete.
    
    This reverts commit 1e29ea5d91ca1fc60af2161f26acc3906b39132f.

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -119,7 +119,7 @@ MONO_DEFAULT?=		5.20
 MYSQL_DEFAULT?=		8.4
 # Possible values: ninja, samurai
 NINJA_DEFAULT?=		ninja
-# Possible value: 22, 24, 25, 26, current, lts (Note: current = 26 and lts = 24)
+# Possible value: 20, 22, 24, 25, 26, current, lts (Note: current = 26 and lts = 24)
 NODEJS_DEFAULT?=	lts
 # Possible value: 25, 26
 OPENLDAP_DEFAULT?=	26
