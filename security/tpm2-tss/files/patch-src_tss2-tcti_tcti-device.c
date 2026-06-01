e715576c4d46e007740709475b8d491186b135ac
William Brown <william@firstyear.id.au>
2026-06-02 00:52:43 +0300

    security/tpm2-tss: Update 4.0.1 => 4.1.3 (CVE-2024-29040)
    Changelog:
    https://github.com/tpm2-software/tpm2-tss/blob/4.1.3/CHANGELOG.md
    
    - Fix warnings from portclippy.
    - Merge MKDIRs and RMDIRs.
    - Refresh patches.
    
    PR:		295403
    Approved by:	Tadeusz Struk <tstruk@gmail.com> (maintainer, timeout 2 weeks)
    Security:	CVE-2024-29040
    Sponsored by:	UNIS Labs
    Co-authored-by:	Vladimir Druzenko <vvd@FreeBSD.org>
    MFH:		2026Q2

--- security/tpm2-tss/files/patch-src_tss2-tcti_tcti-device.c
+++ security/tpm2-tss/files/patch-src_tss2-tcti_tcti-device.c
@@ -1,6 +1,6 @@
---- src/tss2-tcti/tcti-device.c.orig	2023-01-23 18:36:16.000000000 +0000
-+++ src/tss2-tcti/tcti-device.c	2025-05-08 08:40:29.255475000 +0000
-@@ -61,7 +61,9 @@
+--- src/tss2-tcti/tcti-device.c.orig	2024-05-21 12:44:23 UTC
++++ src/tss2-tcti/tcti-device.c
+@@ -61,7 +61,9 @@ static char *default_conf[] = {
  #ifdef __VXWORKS__
      "/tpm0"
  #else
@@ -8,5 +8,5 @@
      "/dev/tpmrm0",
 +#endif /* __FreeBSD__ */
      "/dev/tpm0",
+     "/dev/tcm0",
  #endif /* __VX_WORKS__ */
- };
--- security/tpm2-tss/files/patch-test_unit_tctildr-nodl.c
+++ security/tpm2-tss/files/patch-test_unit_tctildr-nodl.c
@@ -1,6 +1,6 @@
---- test/unit/tctildr-nodl.c.orig	2025-02-22 22:43:21 UTC
+--- test/unit/tctildr-nodl.c.orig	2024-05-17 08:04:10 UTC
 +++ test/unit/tctildr-nodl.c
-@@ -65,9 +65,11 @@ test_tctildr_get_default_all_fail (void **state)
+@@ -68,9 +68,11 @@ test_tctildr_get_default_all_fail (void **state)
      /* device:/dev/tpm0 */
      will_return (__wrap_tcti_from_init, tcti_ctx);
      will_return (__wrap_tcti_from_init, TEST_RC);
