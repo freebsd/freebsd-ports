575b654dc46aa032410861925a393e68fab679ab
Piotr Kubaj <pkubaj@FreeBSD.org>
2026-06-05 09:58:41 +0200

    audio/surge-synthesizer-xt-lv2: fix build on powerpc64*
      ld: error: undefined symbol:
      juce::pnglibNamespace::png_init_filter_functions_vsx(juce::pnglibNamespace::png_struct_def*,
      unsigned int)

---
+++ audio/surge-synthesizer-xt-lv2/files/patch-libs_JUCE_modules_juce__dsp_juce__dsp.h
@@ -0,0 +1,29 @@
+--- libs/JUCE/modules/juce_dsp/juce_dsp.h.orig	2026-06-05 07:45:45 UTC
++++ libs/JUCE/modules/juce_dsp/juce_dsp.h
+@@ -84,6 +84,17 @@
+ 
+  #include <arm_neon.h>
+ 
++#elif defined (__powerpc__)
++
++ #ifndef JUCE_USE_SIMD
++  #define JUCE_USE_SIMD 1
++ #endif
++
++ #ifndef SIMDE_ENABLE_NATIVE_ALIASES
++  #define SIMDE_ENABLE_NATIVE_ALIASES
++ #endif
++ #include <simde/x86/sse4.2.h>
++
+ #else
+ 
+  // No SIMD Support
+@@ -225,7 +236,7 @@ namespace util
+  #include "native/juce_SIMDNativeOps_fallback.h"
+ 
+  // include the correct native file for this build target CPU
+- #if defined(__i386__) || defined(__amd64__) || defined(_M_X64) || defined(_X86_) || defined(_M_IX86) || defined(__riscv) || defined(__EMSCRIPTEN__)
++ #if defined(__i386__) || defined(__amd64__) || defined(_M_X64) || defined(_X86_) || defined(_M_IX86) || defined(__riscv) || defined(__EMSCRIPTEN__) || defined(__powerpc__)
+   #ifdef __AVX2__
+    #include "native/juce_SIMDNativeOps_avx.h"
+   #else
---
+++ audio/surge-synthesizer-xt-lv2/files/patch-libs_JUCE_modules_juce__graphics_image__formats_juce__PNGLoader.cpp
@@ -0,0 +1,10 @@
+--- libs/JUCE/modules/juce_graphics/image_formats/juce_PNGLoader.cpp.orig	2026-06-05 05:33:45 UTC
++++ libs/JUCE/modules/juce_graphics/image_formats/juce_PNGLoader.cpp
+@@ -266,6 +266,7 @@ namespace pnglibNamespace
+   #define PNG_LINKAGE_FUNCTION
+ 
+   #define PNG_ARM_NEON_OPT 0
++  #define PNG_POWERPC_VSX_OPT 0
+ 
+   #if ! defined (PNG_USER_WIDTH_MAX)
+    #define PNG_USER_WIDTH_MAX 1000000
