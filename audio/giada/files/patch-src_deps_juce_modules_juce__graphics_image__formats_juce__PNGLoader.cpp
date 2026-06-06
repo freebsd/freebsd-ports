c4303139057df39ec4f23b2df26f0abc4230ffd3
Piotr Kubaj <pkubaj@FreeBSD.org>
2026-06-06 19:53:37 +0200

    audio/giada: fix build on powerpc64le
      ld: error: undefined symbol:
      juce::pnglibNamespace::png_init_filter_functions_vsx(juce::pnglibNamespace::png_struct_def*,
      unsigned int)

---
+++ audio/giada/files/patch-src_deps_juce_modules_juce__graphics_image__formats_juce__PNGLoader.cpp
@@ -0,0 +1,10 @@
+--- src/deps/juce/modules/juce_graphics/image_formats/juce_PNGLoader.cpp.orig	2026-06-06 17:17:21 UTC
++++ src/deps/juce/modules/juce_graphics/image_formats/juce_PNGLoader.cpp
+@@ -254,6 +254,7 @@
+   #define PNG_LINKAGE_FUNCTION
+ 
+   #define PNG_ARM_NEON_OPT 0
++  #define PNG_POWERPC_VSX_OPT 0
+ 
+   #if ! defined (PNG_USER_WIDTH_MAX)
+    #define PNG_USER_WIDTH_MAX 1000000
