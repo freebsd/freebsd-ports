3666da5f42ad79ebe317043e445502ac820e751d
Hiroki Tagato <tagattie@FreeBSD.org>
2026-06-07 19:36:25 +0900

    editors/vscode: Add patches to fix build error of keytar node module with clang 21
    The error in question was:
    
      CXX(target) Release/obj.target/keytar/src/async.o
    In file included from ../src/async.cc:4:
    ../node_modules/node-addon-api/napi.h:1147:39: error: in-class initializer for static data member is not a constant expression
     1147 |     static const napi_typedarray_type unknown_array_type = static_cast<napi_typedarray_type>(-1);
          |                                       ^                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../node_modules/node-addon-api/napi.h:1147:60: note: integer value -1 is outside the valid range of values [0, 15] for the enumeration type 'napi_typedarray_type'
     1147 |     static const napi_typedarray_type unknown_array_type = static_cast<napi_typedarray_type>(-1);
          |                                                            ^
    1 error generated.
    
    PR:		295862
    Reported by:	Alexey Shuvaev <bugsbeastie@gmail.com>

---
+++ editors/vscode/files/patch-node__modules_keytar_node__modules_node-addon-api_napi.h
@@ -0,0 +1,11 @@
+--- node_modules/keytar/node_modules/node-addon-api/napi.h.orig	2026-06-07 09:05:42 UTC
++++ node_modules/keytar/node_modules/node-addon-api/napi.h
+@@ -1144,7 +1144,7 @@ namespace Napi {
+ 
+     TypedArray(napi_env env, napi_value value, napi_typedarray_type type, size_t length);
+ 
+-    static const napi_typedarray_type unknown_array_type = static_cast<napi_typedarray_type>(-1);
++    static inline const napi_typedarray_type unknown_array_type = static_cast<napi_typedarray_type>(-1);
+ 
+     template <typename T>
+     static
