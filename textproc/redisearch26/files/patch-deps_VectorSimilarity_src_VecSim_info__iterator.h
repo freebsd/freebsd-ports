3b599384580650539ae177d947a68676abaf67bb
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2026-06-07 17:50:41 +0200

    textproc/redisearch26: Update version 2.6.0=>2.6.1
    Changelog: https://github.com/RediSearch/RediSearch/releases/tag/v2.6.1

--- textproc/redisearch26/files/patch-deps_VectorSimilarity_src_VecSim_info__iterator.h
+++ textproc/redisearch26/files/patch-deps_VectorSimilarity_src_VecSim_info__iterator.h
@@ -1,4 +1,4 @@
---- deps/VectorSimilarity/src/VecSim/info_iterator.h.orig	2026-05-30 22:20:52 UTC
+--- deps/VectorSimilarity/src/VecSim/info_iterator.h.orig	2022-10-20 13:03:07 UTC
 +++ deps/VectorSimilarity/src/VecSim/info_iterator.h
 @@ -1,5 +1,6 @@
  #pragma once
@@ -7,12 +7,12 @@
  #ifdef __cplusplus
  extern "C" {
  #endif
-@@ -30,7 +31,7 @@ typedef struct {
-     union {
-         double floatingPointValue; // Floating point value. 64 bits float.
-         int64_t integerValue;      // Integer value. Signed 64 bits integer.
--        u_int64_t uintegerValue;   // Unsigned value. Unsigned 64 buts integer.
-+        uint64_t uintegerValue;   // Unsigned value. Unsigned 64 buts integer.
-         const char *stringValue;   // String value.
-     };
- } VecSim_InfoField;
+@@ -21,7 +22,7 @@ typedef union {
+ typedef union {
+     double floatingPointValue; // Floating point value. 64 bits float.
+     int64_t integerValue;      // Integer value. Signed 64 bits integer.
+-    u_int64_t uintegerValue;   // Unsigned value. Unsigned 64 buts integer.
++    uint64_t uintegerValue;   // Unsigned value. Unsigned 64 buts integer.
+     const char *stringValue;   // String value.
+ } FieldValue;
+ 
