d8b87c5ff9885d3df313e2b75a0affe89f62bb8a
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2026-06-10 21:56:36 -0700

    lang/ldc: Update to 1.42.0
    Changelog at:   https://github.com/ldc-developers/ldc/releases/tag/v1.42.0
    PR:		293515

--- lang/ldc/files/extra-patch-ldc2-1.41.0-freebsd-x86_64-import-core-sys-freebsd-config.d
+++ lang/ldc/files/extra-patch-ldc2-1.41.0-freebsd-x86_64-import-core-sys-freebsd-config.d
@@ -1,5 +1,5 @@
---- ldc2-1.41.0-freebsd-x86_64/import/core/sys/freebsd/config.d.orig	2025-06-07 02:43:09 UTC
-+++ ldc2-1.41.0-freebsd-x86_64/import/core/sys/freebsd/config.d
+--- ldc2-1.42.0-freebsd-x86_64/import/core/sys/freebsd/config.d.orig	2025-06-07 02:43:09 UTC
++++ ldc2-1.42.0-freebsd-x86_64/import/core/sys/freebsd/config.d
 @@ -14,7 +14,8 @@
  // NOTE: When adding newer versions of FreeBSD, verify all current versioned
  // bindings are still compatible with the release.
