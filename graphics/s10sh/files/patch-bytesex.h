c3f90023f66c0cd4d6843bee749be5eee32730cc
Robert Clausecker <fuz@FreeBSD.org>
2026-06-11 17:10:15 +0200

    graphics/s10sh: fix endianess code
    Instead of hard-coding a list of architectures, defer to <endian.h>.
    This fixes the build on all platforms tested.
    
    MFH:		2026Q2
    (cherry picked from commit c653d172b2678fed4bdd92782c593b995d41c5e5)

--- graphics/s10sh/files/patch-bytesex.h
+++ graphics/s10sh/files/patch-bytesex.h
@@ -1,12 +1,22 @@
---- bytesex.h.orig	Sat Mar 17 22:27:18 2007
-+++ bytesex.h	Sat Mar 17 22:30:35 2007
-@@ -5,7 +5,8 @@
+--- bytesex.h.orig	2003-08-09 16:42:24 UTC
++++ bytesex.h
+@@ -3,15 +3,11 @@
+ #ifndef S10SH_BYTESEX_H
+ #define S10SH_BYTESEX_H
  
- #if 	defined(__i386__) \
- 	|| defined(__alpha__) \
+-#if 	defined(__i386__) \
+-	|| defined(__alpha__) \
 -	|| (defined(__mips__) && (defined(MIPSEL) || defined (__MIPSEL__)))
-+	|| (defined(__mips__) && (defined(MIPSEL) || defined (__MIPSEL__))) \
-+	|| defined(__amd64__) || defined(__ia64__)
++#include <endian.h>
++
++#if BYTE_ORDER == LITTLE_ENDIAN
  #define BYTE_ORDER_LITTLE_ENDIAN
- #elif 	defined(__mc68000__) \
- 	|| defined (__sparc__) \
+-#elif 	defined(__mc68000__) \
+-	|| defined (__sparc__) \
+-	|| defined (__sparc) \
+-	|| defined (__PPC__) \
+-	|| (defined(__mips__) && (defined(MIPSEB) || defined (__MIPSEB__)))
++#elif BYTE_ORDER == BIG_ENDIAN
+ #define BYTE_ORDER_BIG_ENDIAN
+ #else
+ # error can not find the byte order for this architecture, fix bytesex.h
