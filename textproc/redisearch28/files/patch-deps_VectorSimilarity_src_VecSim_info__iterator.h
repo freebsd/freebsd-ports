76819a3c3c7a44837a6b636b99c449d4c63ba8c0
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2026-06-13 15:52:25 +0200

    textproc/redisearch28: Update version 2.8.1=>2.8.2
    Changelog: https://github.com/RediSearch/RediSearch/releases/tag/v2.8.2

--- textproc/redisearch28/files/patch-deps_VectorSimilarity_src_VecSim_info__iterator.h
+++ textproc/redisearch28/files/patch-deps_VectorSimilarity_src_VecSim_info__iterator.h
@@ -1,4 +1,4 @@
---- deps/VectorSimilarity/src/VecSim/info_iterator.h.orig	2026-05-30 23:32:41 UTC
+--- deps/VectorSimilarity/src/VecSim/info_iterator.h.orig	2023-05-24 22:12:35 UTC
 +++ deps/VectorSimilarity/src/VecSim/info_iterator.h
 @@ -6,6 +6,7 @@
  
@@ -8,12 +8,12 @@
  #ifdef __cplusplus
  extern "C" {
  #endif
-@@ -27,7 +28,7 @@ typedef union {
+@@ -28,7 +29,7 @@ typedef union {
  typedef union {
-     double floatingPointValue; // Floating point value. 64 bits float.
-     int64_t integerValue;      // Integer value. Signed 64 bits integer.
--    u_int64_t uintegerValue;   // Unsigned value. Unsigned 64 buts integer.
-+    uint64_t uintegerValue;   // Unsigned value. Unsigned 64 buts integer.
-     const char *stringValue;   // String value.
+     double floatingPointValue;         // Floating point value. 64 bits float.
+     int64_t integerValue;              // Integer value. Signed 64 bits integer.
+-    u_int64_t uintegerValue;           // Unsigned value. Unsigned 64 buts integer.
++    uint64_t uintegerValue;           // Unsigned value. Unsigned 64 buts integer.
+     const char *stringValue;           // String value.
+     VecSimInfoIterator *iteratorValue; // Iterator value.
  } FieldValue;
- 
--- textproc/redisearch28/files/patch-src_fork__gc.c
+++ textproc/redisearch28/files/patch-src_fork__gc.c
@@ -1,6 +1,6 @@
---- src/fork_gc.c.orig	2023-05-04 07:44:04 UTC
+--- src/fork_gc.c.orig	2023-06-07 19:36:39 UTC
 +++ src/fork_gc.c
-@@ -265,6 +265,7 @@ static void sendHeaderString(ForkGC *gc, void *arg) {
+@@ -266,6 +266,7 @@ static void sendHeaderString(ForkGC *gc, void *arg) {
  }
  
  static void sendHeaderString(ForkGC *gc, void *arg) {
@@ -8,7 +8,7 @@
    struct iovec *iov = arg;
    FGC_sendBuffer(gc, iov->iov_base, iov->iov_len);
  }
-@@ -281,7 +282,9 @@ static void FGC_childCollectTerms(ForkGC *gc, RedisSea
+@@ -282,7 +283,9 @@ static void FGC_childCollectTerms(ForkGC *gc, RedisSea
      RedisModuleKey *idxKey = NULL;
      InvertedIndex *idx = Redis_OpenInvertedIndexEx(sctx, term, strlen(term), 1, NULL, &idxKey);
      if (idx) {
