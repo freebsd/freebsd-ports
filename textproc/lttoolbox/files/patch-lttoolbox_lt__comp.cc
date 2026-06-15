abea90ffb233307e8cbf4660f6c315f048b09fa6
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2026-06-15 19:25:19 +0200

    textproc/lttoolbox: Update version 3.8.0=>3.8.2
    Changelog: https://github.com/apertium/lttoolbox/releases/tag/v3.8.2

--- textproc/lttoolbox/files/patch-lttoolbox_lt__comp.cc
+++ textproc/lttoolbox/files/patch-lttoolbox_lt__comp.cc
@@ -1,4 +1,4 @@
---- lttoolbox/lt_comp.cc.orig	2025-04-05 18:21:01 UTC
+--- lttoolbox/lt_comp.cc.orig	2025-07-26 16:26:30 UTC
 +++ lttoolbox/lt_comp.cc
 @@ -19,6 +19,7 @@
  #include <lttoolbox/lt_locale.h>
@@ -8,16 +8,15 @@
  
  #include <iostream>
  
-@@ -119,7 +120,11 @@ int main(int argc, char *argv[])
-     std::cerr << "Error: Cannot not open file '" << infile << "'." << std::endl << std::endl;
-     exit(EXIT_FAILURE);
-   }
--  initGenericErrorDefaultFunc(NULL);
+@@ -104,6 +105,11 @@ int main(int argc, char *argv[])
+   reader = xmlReaderForFile(infile.c_str(), NULL, 0);
+   xmlGenericErrorFunc handler = (xmlGenericErrorFunc)errorFunc;
+   initGenericErrorDefaultFunc(&handler);
 +#if LIBXML_VERSION < 21200
-+    initGenericErrorDefaultFunc(NULL);
++  initGenericErrorDefaultFunc(NULL);
 +#else
-+    xmlSetGenericErrorFunc(NULL, handler);  // match new signature if needed
++  xmlSetGenericErrorFunc(NULL, handler);  // match new signature if needed
 +#endif
- 
- 
-   if(opc == "lr")
+   if(reader != NULL)
+   {
+     int ret = xmlTextReaderRead(reader);
