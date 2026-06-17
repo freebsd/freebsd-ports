48900abd36a370c67eef52d62c0b981634891bf7
Hiroki Tagato <tagattie@FreeBSD.org>
2026-06-18 03:47:34 +0900

    devel/electron41: Update to 41.8.0
    Changelog: https://github.com/electron/electron/releases/tag/v41.8.0
    
    Reported by:	GitHub (watch releases)

--- devel/electron41/files/patch-electron_shell_browser_api_electron__api__web__contents.h
+++ devel/electron41/files/patch-electron_shell_browser_api_electron__api__web__contents.h
@@ -1,6 +1,6 @@
---- electron/shell/browser/api/electron_api_web_contents.h.orig	2026-03-17 08:46:52 UTC
+--- electron/shell/browser/api/electron_api_web_contents.h.orig	2026-06-16 14:53:17 UTC
 +++ electron/shell/browser/api/electron_api_web_contents.h
-@@ -753,7 +753,7 @@ class WebContents final : public ExclusiveAccessContex
+@@ -756,7 +756,7 @@ class WebContents final : public ExclusiveAccessContex
  #if defined(TOOLKIT_VIEWS) && !BUILDFLAG(IS_MAC)
    ui::ImageModel GetDevToolsWindowIcon() override;
  #endif
--- devel/electron41/files/patch-electron_spec_api-web-contents-spec.ts
+++ devel/electron41/files/patch-electron_spec_api-web-contents-spec.ts
@@ -1,4 +1,4 @@
---- electron/spec/api-web-contents-spec.ts.orig	2026-05-19 01:01:16 UTC
+--- electron/spec/api-web-contents-spec.ts.orig	2026-06-16 14:53:17 UTC
 +++ electron/spec/api-web-contents-spec.ts
 @@ -1277,7 +1277,7 @@ describe('webContents module', () => {
      // back to OpenFolder() which does a blocking DirectoryExists() on the UI
@@ -9,7 +9,7 @@
        'reveals paths under a registered workspace folder without executing them',
        async () => {
          const w = new BrowserWindow({ show: false });
-@@ -3079,7 +3079,7 @@ describe('webContents module', () => {
+@@ -3095,7 +3095,7 @@ describe('webContents module', () => {
      });
  
      // TODO(codebytere): OOPIF printing is disabled on Linux at the moment due to crashes.
