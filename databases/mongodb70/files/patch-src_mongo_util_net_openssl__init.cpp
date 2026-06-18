7faebc4f1a4a8b04a0dc734024021bcbea8d3ab9
Ronald Klop <ronald@FreeBSD.org>
2026-06-18 08:08:41 +0200

    databases/mongodb70: 7.0.36
    Release Notes:
    https://www.mongodb.com/docs/manual/release-notes/7.0/#7.0.36---june-10--2026
    
    - Reduced the amount of modification in patch-src_mongo_util_net_openssl__init.cpp

--- databases/mongodb70/files/patch-src_mongo_util_net_openssl__init.cpp
+++ databases/mongodb70/files/patch-src_mongo_util_net_openssl__init.cpp
@@ -1,18 +1,6 @@
---- src/mongo/util/net/openssl_init.cpp.orig	2026-06-17 14:19:16 UTC
+--- src/mongo/util/net/openssl_init.cpp.orig	2026-06-16 14:14:04 UTC
 +++ src/mongo/util/net/openssl_init.cpp
-@@ -230,37 +230,9 @@ MONGO_INITIALIZER_GENERAL(SetupOpenSSL, ("default"), (
-         setupFIPS();
-     } else {
- #if OPENSSL_VERSION_NUMBER > 0x30000000L
--        // When the OpenSSL 3 FIPS provider is disabled, OpenSSL uses a primary DRBG
--        // to seed per-thread DRBGs. The primary DRBG relies on the system entropy source for
--        // seeding and reseeding. In highly concurrent environments where many threads are
--        // being spawned at once, the primary DRBG will receive many generate requests to
--        // seed all the new threads. Both the reseed request and
--        // the time intervals can be tuned via startup server parameters in response to
--        // observed performance regressions.
--        EVP_RAND_CTX* primary = RAND_get0_primary(NULL /* default OSSL_LIB_CTX */);
--        if (primary == NULL) {
+@@ -242,24 +242,6 @@ MONGO_INITIALIZER_GENERAL(SetupOpenSSL, ("default"), (
              LOGV2_WARNING(12549200,
                            "Unable to fetch primary DRBG to adjust reseed parameters, continuing "
                            "with OpenSSL defaults");
@@ -34,7 +22,6 @@
 -                              "error"_attr =
 -                                  SSLManagerInterface::getSSLErrorMessage(ERR_get_error()));
 -            }
--        }
+         }
  #endif
      }
- 
