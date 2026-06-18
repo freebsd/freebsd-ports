e53f1fbcb0fa173400094405e3f392968ce1ae60
Yuri Victorovich <yuri@FreeBSD.org>
2026-06-17 21:16:39 -0700

    misc/astc-encoder: update 5.4.0 → 5.5.0

--- misc/astc-encoder/files/patch-Source_cmake__core.cmake
+++ misc/astc-encoder/files/patch-Source_cmake__core.cmake
@@ -1,11 +1,11 @@
---- Source/cmake_core.cmake.orig	2025-04-14 08:46:50 UTC
+--- Source/cmake_core.cmake.orig	2026-06-18 04:14:15 UTC
 +++ Source/cmake_core.cmake
-@@ -437,8 +437,6 @@ macro(astcenc_set_properties ASTCENC_TARGET_NAME ASTCE
+@@ -450,8 +450,6 @@ macro(astcenc_set_properties ASTCENC_TARGET_NAME ASTCE
          else()
              target_compile_options(${ASTCENC_TARGET_NAME}
                  PRIVATE
--                    $<${is_clangcl}:-mcpu=native -march=native>
--                    $<${is_gnu_fe}:-mcpu=native -march=native>
+-                    $<${is_clangcl}:-march=native>
+-                    $<${is_gnu_fe}:-march=native>
                      $<${is_gnu_fe}:-Wno-unused-command-line-argument>)
          endif()
      endif()
