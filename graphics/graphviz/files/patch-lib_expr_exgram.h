c4dbd5760a84726589b2be1bd636a707d823d4f5
Dirk Meyer <dinoex@FreeBSD.org>
2026-06-21 22:01:28 +0200

    graphics/graphviz: update to 15.1.0

--- graphics/graphviz/files/patch-lib_expr_exgram.h
+++ graphics/graphviz/files/patch-lib_expr_exgram.h
@@ -1,4 +1,4 @@
---- lib/expr/exgram.h.orig	2024-12-06 23:55:56 UTC
+--- lib/expr/exgram.h.orig	2026-06-18 01:52:21 UTC
 +++ lib/expr/exgram.h
 @@ -19,7 +19,7 @@ extern "C" {
   * with both yacc and bison
@@ -8,4 +8,4 @@
 +#if !defined(_EXGRAM_H)
  #define _EXGRAM_H
  
- #include <cgraph/gv_ctype.h>
+ #include <expr/exlib.h>
