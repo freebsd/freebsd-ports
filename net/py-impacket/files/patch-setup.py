a5ce6de4a080c8c9f86f3f7b6adc2e86c900753b
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2026-06-23 00:21:47 -0700

    net/py-impacket: Update to 0.13.1

--- net/py-impacket/files/patch-setup.py
+++ net/py-impacket/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2024-12-24 05:02:53 UTC
+--- setup.py.orig	2026-06-23 07:20:45 UTC
 +++ setup.py
 @@ -41,12 +41,6 @@ except Exception:
  except Exception:
@@ -13,12 +13,11 @@
  def read(fname):
      return open(os.path.join(os.path.dirname(__file__), fname)).read()
  
-@@ -69,8 +63,6 @@ setup(
+@@ -69,7 +63,6 @@ setup(
                'impacket.examples.ntlmrelayx.servers.socksplugins', 'impacket.examples.ntlmrelayx.utils',
                'impacket.examples.ntlmrelayx.attacks', 'impacket.examples.ntlmrelayx.attacks.httpattacks'],
      scripts=glob.glob(os.path.join('examples', '*.py')),
 -    data_files=data_files,
--
-     install_requires=['pyasn1>=0.2.3', 'pyasn1_modules', 'pycryptodomex', 'pyOpenSSL==24.0.0', 'six', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6',
-                       'ldapdomaindump>=0.9.0', 'flask>=1.0', 'setuptools', 'charset_normalizer'],
-     extras_require={':sys_platform=="win32"': ['pyreadline3'],
+ 
+     install_requires=['pyasn1>=0.2.3', 'pyasn1_modules', 'pycryptodomex', 'pyOpenSSL', 'six', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6',
+                       'ldapdomaindump>=0.9.0', 'flask>=1.0', 'charset_normalizer'],
