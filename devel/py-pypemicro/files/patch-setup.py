a78c436778cf3c004792d2d68a74e290bde2ed19
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2026-06-27 04:47:35 +0800

    devel/py-pypemicro: Fix stage-qa
    - Bump PORTREVISION for package change
    
    ====> Running Q/A tests (stage-qa)
    Error: Python package installs top-level 'tests/' directory in site-packages
    Error:   Location: lib/python3.11/site-packages/tests
    Error: This causes file conflicts with other packages. Exclude it via pyproject.toml:
    Error:   [tool.setuptools.packages.find]
    Error:   exclude = ["tests", "tests.*"]
    Error: See: https://setuptools.pypa.io/en/latest/userguide/package_discovery.html
    *** Error code 1
    
    Approved by:	portmgr (blanket)
    With hat:	python

---
+++ devel/py-pypemicro/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2023-02-23 12:56:33 UTC
++++ setup.py
+@@ -16,7 +16,7 @@ setup_args = dict(
+     python_requires=">=3.7",
+     setup_requires=["setuptools>=40.0"],
+     license="BSD-3-Clause",
+-    packages=find_packages(),
++    packages=find_packages(exclude = ["tests", "tests.*"]),
+     author="Petr Gargulak",
+     author_email="petr.gargulak@nxp.com",
+     keywords=["PEMicro", "PyPEMicro"],
