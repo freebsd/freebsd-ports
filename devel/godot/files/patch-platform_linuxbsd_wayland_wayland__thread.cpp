aad97c3275e2d23c1fccbc9fce760d857efc4080
Shane <FreeBSD@Shaneware.biz>
2026-06-26 13:45:13 +0930

    devel/godot: Update 4.6.2 => 4.7
    While here:
    - Use built-in harfbuzz and icu4c because system-provided icu4c or harfbuzz
      cause known issues for GDExtension (see GH-91401 and GH-100301).
    - Update patch metadata.
    - Update LICENSE.
    
    Release Notes:
    https://godotengine.org/releases/4.7/
    
    Changelog:
    https://godotengine.github.io/godot-interactive-changelog/#4.7
    
    PR:		296268
    Reported by:	Thibault Payet <monwarez@mailoo.org>
    Approved by:	Shane <FreeBSD@ShaneWare.Biz> (maintainer)
    Approved by:	osa, vvd (Mentors, implicit)

--- devel/godot/files/patch-platform_linuxbsd_wayland_wayland__thread.cpp
+++ devel/godot/files/patch-platform_linuxbsd_wayland_wayland__thread.cpp
@@ -1,15 +1,6 @@
---- platform/linuxbsd/wayland/wayland_thread.cpp.orig	2025-10-13 19:05:40 UTC
+--- platform/linuxbsd/wayland/wayland_thread.cpp.orig	2026-06-17 18:46:02 UTC
 +++ platform/linuxbsd/wayland/wayland_thread.cpp
-@@ -95,7 +95,7 @@ Vector<uint8_t> WaylandThread::_read_fd(int fd) {
- 			break;
- 		}
- 
--		DEBUG_LOG_WAYLAND_THREAD(vformat("Read chunk of %d bytes.", last_bytes_read));
-+		DEBUG_LOG_WAYLAND_THREAD(vformat("Read chunk of %d bytes.", (uint64_t)last_bytes_read));
- 
- 		bytes_read += last_bytes_read;
- 
-@@ -1197,7 +1197,7 @@ void WaylandThread::_wl_surface_on_enter(void *data, s
+@@ -1313,7 +1313,7 @@ void WaylandThread::_wl_surface_on_enter(void *data, s
  	WindowState *ws = (WindowState *)data;
  	ERR_FAIL_NULL(ws);
  
@@ -18,7 +9,7 @@
  
  	ws->wl_outputs.insert(wl_output);
  
-@@ -1248,7 +1248,7 @@ void WaylandThread::_wl_surface_on_leave(void *data, s
+@@ -1353,7 +1353,7 @@ void WaylandThread::_wl_surface_on_leave(void *data, s
  
  	ws->wl_outputs.erase(wl_output);
  
@@ -27,7 +18,7 @@
  }
  
  // TODO: Add support to this event.
-@@ -1310,7 +1310,7 @@ void WaylandThread::_wl_output_on_done(void *data, str
+@@ -1419,7 +1419,7 @@ void WaylandThread::_wl_output_on_done(void *data, str
  
  	ss->wayland_thread->_update_scale(ss->data.scale);
  
@@ -36,7 +27,7 @@
  }
  
  void WaylandThread::_wl_output_on_scale(void *data, struct wl_output *wl_output, int32_t factor) {
-@@ -1319,7 +1319,7 @@ void WaylandThread::_wl_output_on_scale(void *data, st
+@@ -1428,7 +1428,7 @@ void WaylandThread::_wl_output_on_scale(void *data, st
  
  	ss->pending_data.scale = factor;
  
@@ -45,21 +36,3 @@
  }
  
  void WaylandThread::_wl_output_on_name(void *data, struct wl_output *wl_output, const char *name) {
-@@ -2509,7 +2509,7 @@ void WaylandThread::_wl_data_source_on_send(void *data
- 		}
- 
- 		if (written_bytes > 0) {
--			DEBUG_LOG_WAYLAND_THREAD(vformat("Clipboard: sent %d bytes.", written_bytes));
-+			DEBUG_LOG_WAYLAND_THREAD(vformat("Clipboard: sent %d bytes.", (uint64_t)written_bytes));
- 		} else if (written_bytes == 0) {
- 			DEBUG_LOG_WAYLAND_THREAD("Clipboard: no bytes sent.");
- 		} else {
-@@ -2697,7 +2697,7 @@ void WaylandThread::_wp_primary_selection_source_on_se
- 		}
- 
- 		if (written_bytes > 0) {
--			DEBUG_LOG_WAYLAND_THREAD(vformat("Clipboard: sent %d bytes.", written_bytes));
-+			DEBUG_LOG_WAYLAND_THREAD(vformat("Clipboard: sent %d bytes.", (uint64_t)written_bytes));
- 		} else if (written_bytes == 0) {
- 			DEBUG_LOG_WAYLAND_THREAD("Clipboard: no bytes sent.");
- 		} else {
