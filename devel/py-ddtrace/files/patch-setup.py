0279780095ea8f4a9e12f5d1114451084efa84c4
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2026-06-27 04:44:44 +0800

    devel/py-ddtrace: Update to 4.10.4
    - Add OPENTELEMETRY option
    - Remove OPENAI option
    
    Changes:	https://github.com/DataDog/dd-trace-py/releases

--- devel/py-ddtrace/files/patch-setup.py
+++ devel/py-ddtrace/files/patch-setup.py
@@ -1,14 +1,32 @@
---- setup.py.orig	2025-04-07 19:46:51 UTC
+--- setup.py.orig	2026-06-10 13:39:49 UTC
 +++ setup.py
-@@ -9,7 +9,6 @@ import time
- import tarfile
- import time
+@@ -21,7 +21,6 @@ from setuptools_rust import build_rust
+ from setuptools_rust import RustExtension
+ from setuptools_rust import build_rust
  
 -import cmake
- from setuptools_rust import Binding
- from setuptools_rust import RustExtension
  
-@@ -419,9 +418,7 @@ class CMakeBuild(build_ext):
+ 
+ from setuptools import Distribution, Extension, find_packages, setup  # isort: skip
+@@ -999,7 +998,7 @@ class CustomBuildExt(build_ext):
+ 
+             install_args = [f"--config {COMPILE_MODE}"]
+ 
+-            cmake_command = (Path(cmake.CMAKE_BIN_DIR) / "cmake").resolve()
++            cmake_command = "cmake"
+             subprocess.run([cmake_command, *cmake_args], cwd=cmake_build_dir, check=True)
+             subprocess.run([cmake_command, "--build", ".", *build_args], cwd=cmake_build_dir, check=True)
+             subprocess.run([cmake_command, "--install", ".", *install_args], cwd=cmake_build_dir, check=True)
+@@ -1038,7 +1037,7 @@ class CustomBuildExt(build_ext):
+         cmake_build_dir = Path(self.build_lib.replace("lib.", "cmake."), f"{dep.name}_build").resolve()
+         cmake_build_dir.mkdir(parents=True, exist_ok=True)
+ 
+-        cmake_command = (Path(cmake.CMAKE_BIN_DIR) / "cmake").resolve()
++        cmake_command = "cmake"
+ 
+         cmake_args = self._base_cmake_args() + [
+             f"-S{dep.cmake_dir}",
+@@ -1333,9 +1332,7 @@ class CustomBuildExt(build_ext):
                  "-DCMAKE_C_FLAGS_%s=-O0" % ext.build_type.upper(),
                  "-DCMAKE_CXX_FLAGS_%s=-O0" % ext.build_type.upper(),
              ]
@@ -19,15 +37,18 @@
          subprocess.run([cmake_command, *cmake_args], cwd=cmake_build_dir, check=True)
          subprocess.run([cmake_command, "--build", ".", *build_args], cwd=cmake_build_dir, check=True)
          subprocess.run([cmake_command, "--install", ".", *install_args], cwd=cmake_build_dir, check=True)
-@@ -661,10 +658,9 @@ setup(
+@@ -1756,14 +1753,12 @@ setup(
      # funcsigs backport required for vendored debtcollector
      cmdclass={
-         "build_ext": CMakeBuild,
+         "build_ext": CustomBuildExt,
 -        "build_py": LibraryDownloader,
+         "build_rust": CustomBuildRust,
          "clean": CleanLibraries,
+         "ext_hashes": ExtensionHashes,
      },
--    setup_requires=["setuptools_scm[toml]>=4", "cython", "cmake>=3.24.2,<3.28", "setuptools-rust"],
-+    setup_requires=["setuptools_scm[toml]>=4", "cython", "setuptools-rust"],
-     ext_modules=ext_modules
-     + cythonize(
-         [
+     setup_requires=[
+         "cython",
+-        "cmake>=3.24.2,<3.28",
+         "setuptools-rust<2",
+         "patchelf>=0.17.0.0; sys_platform == 'linux'",
+     ],
