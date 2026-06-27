ef8402c447e30847d344f4259fb8e64222c30a78
Hiroki Tagato <tagattie@FreeBSD.org>
2026-06-28 05:04:00 +0900

    editors/vscode: Update to 1.126.0
    Changelog: https://code.visualstudio.com/updates/v1_126
    
    Reported by:	GitHub (watch releases)

--- editors/vscode/files/patch-src_vs_code_electron-main_app.ts
+++ editors/vscode/files/patch-src_vs_code_electron-main_app.ts
@@ -1,6 +1,6 @@
---- src/vs/code/electron-main/app.ts.orig	2026-06-15 22:24:03 UTC
+--- src/vs/code/electron-main/app.ts.orig	2026-06-23 01:46:13 UTC
 +++ src/vs/code/electron-main/app.ts
-@@ -1091,6 +1091,7 @@ export class CodeApplication extends Disposable {
+@@ -1092,6 +1092,7 @@ export class CodeApplication extends Disposable {
  				break;
  
  			case 'linux':
