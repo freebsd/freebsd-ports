2f6a6e9e6a20867baead770411a9647318a40e8f
Dima Panov <fluffy@FreeBSD.org>
2026-07-01 17:18:41 +0300

    net/gnu-radius: update to 1.7.0 release

--- net/gnu-radius/files/patch-lib_readline.c
+++ net/gnu-radius/files/patch-lib_readline.c
@@ -1,10 +1,10 @@
---- lib/readline.c.orig	2026-03-26 21:35:33 UTC
+--- lib/readline.c.orig	2025-12-11 11:21:32 UTC
 +++ lib/readline.c
-@@ -144,7 +144,7 @@ grad_readline_init(char *name,
+@@ -129,7 +129,7 @@ grad_readline_init(char *name,
  		return;
  #ifdef WITH_READLINE
  	rl_readline_name = name;
--	rl_attempted_completion_function = (CPPFunction *) completion_fp;
+-	rl_attempted_completion_function = completion_fp;
 +	rl_attempted_completion_function = (rl_completion_func_t *) completion_fp;
  	rl_getc_function = grad_getc;
  #endif
--- net/gnu-radius/files/patch-radwho_radwho.c
+++ net/gnu-radius/files/patch-radwho_radwho.c
@@ -1,16 +1,16 @@
---- radwho/radwho.c.orig	2008-12-15 08:03:24 UTC
+--- radwho/radwho.c.orig	2025-12-11 11:21:33 UTC
 +++ radwho/radwho.c
-@@ -41,7 +41,8 @@ void print_header();
+@@ -39,7 +39,8 @@ int want_rad_record(struct radutmp *rt);
  int want_rad_record(struct radutmp *rt);
  
  /* UTMP stuff. Uses utmpx on svr4 */
--#if defined(__svr4__) || defined(__sgi)  
+-#if defined(__svr4__) || defined(__sgi)
 +#include <sys/param.h>
 +#if defined(__svr4__) || defined(__sgi) || (defined(__FreeBSD__) && __FreeBSD_version >= 900007)
  #  include <utmpx.h>
  #  include <sys/fcntl.h>
  #  define utmp utmpx
-@@ -49,7 +50,9 @@ int want_rad_record(struct radutmp *rt);
+@@ -47,7 +48,9 @@ int want_rad_record(struct radutmp *rt);
  #  define UT_LINESIZE   32
  #  define UT_HOSTSIZE   257
  #  undef UTMP_FILE
@@ -20,7 +20,7 @@
  #  undef WTMP_FILE
  #  define WTMP_FILE WTMPX_FILE
  #else
-@@ -60,13 +63,19 @@ int want_rad_record(struct radutmp *rt);
+@@ -58,11 +61,17 @@ int want_rad_record(struct radutmp *rt);
  #  define UT_LINESIZE   32
  #  define UT_HOSTSIZE   64
  #endif
@@ -30,14 +30,12 @@
  #    define UTMP_FILE "/var/run/utmp"
  #  endif
  #  define ut_user ut_name
- #endif
- 
++#endif
++
 +#if defined(__FreeBSD__) && __FreeBSD_version >= 900007
 +#  define UTMP_FILE "/var/run/utmp"
 +#  define ut_name ut_user
 +#  define ut_time ut_tv.tv_sec
-+#endif
-+
- #define P_CONSOLE -1  /* Special radutmp type value for local users */
+ #endif
  
- int  fingerd;             /* Are we run as fingerd */
+ #define P_CONSOLE -1  /* Special radutmp type value for local users */
