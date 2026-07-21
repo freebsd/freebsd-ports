62e2278cd65508199010a07bc84aa4f0e345d50f
Mikael Urankar <mikael@FreeBSD.org>
2026-07-21 08:59:06 +0200

    multimedia/ringrtc: Update to 2.69.4

--- multimedia/ringrtc/webrtc_fetch.sh
+++ multimedia/ringrtc/webrtc_fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WEBRTC_REV=7778b
+WEBRTC_REV=7778c
 
 boringssl_url="https://boringssl.googlesource.com/boringssl.git/+archive/"
 build_url="https://chromium.googlesource.com/chromium/src/build.git/+archive/"
