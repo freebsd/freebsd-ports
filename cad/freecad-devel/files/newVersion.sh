4baf2f1337a6c9e446829b81192b8a3a44cf69e3
Michael Reifenberger <mr@FreeBSD.org>
2026-07-22 12:02:47 +0200

    cad/freecad-devel: Update to 2026.07.20

--- cad/freecad-devel/files/newVersion.sh
+++ cad/freecad-devel/files/newVersion.sh
@@ -20,8 +20,9 @@ GIT_SRC_DATE2=`git log -n 1 --date=iso-local -n 1 --pretty=format:"%cd" --date=f
     cut -c '1-19' | sed -e 's,-,,g' -e 's,:,,g' -e 's, ,,g'`
 GIT_ONDSEL_HASH=`git ls-tree HEAD src/3rdParty/OndselSolver/ | awk '{print $3}'`
 GIT_GSL_HASH=`git ls-tree HEAD src/3rdParty/GSL/ | awk '{print $3}'`
-# GIT_GTEST_HASH=`git ls-tree HEAD tests/lib/ | awk '{print $3}'`
 GIT_ADDONMGR_HASH=`git ls-tree HEAD src/Mod/AddonManager/ | awk '{print $3}'`
+GIT_COIN_HASH=`git ls-tree HEAD src/3rdParty/coin/ | awk '{print $3}'`
+GIT_PIVY_HASH=`git ls-tree HEAD src/3rdParty/pivy/ | awk '{print $3}'`
 
 echo "$GIT_SRC_HASH, $GIT_SRC_DATE"
 echo "GIT_SRC_HASH=  ${GIT_SRC_HASH}"    > ${CURDIR}/Makefile.git_rev
@@ -29,5 +30,6 @@ echo "GIT_SRC_DATE=  ${GIT_SRC_DATE}"   >> ${CURDIR}/Makefile.git_rev
 echo "GIT_SRC_DATE2=  ${GIT_SRC_DATE2}"   >> ${CURDIR}/Makefile.git_rev
 echo "GIT_ONDSEL_HASH= ${GIT_ONDSEL_HASH}"   >> ${CURDIR}/Makefile.git_rev
 echo "GIT_GSL_HASH= ${GIT_GSL_HASH}"   >> ${CURDIR}/Makefile.git_rev
-# echo "GIT_GTEST_HASH= ${GIT_GTEST_HASH}"   >> ${CURDIR}/Makefile.git_rev
 echo "GIT_ADDONMGR_HASH= ${GIT_ADDONMGR_HASH}"   >> ${CURDIR}/Makefile.git_rev
+echo "GIT_COIN_HASH= ${GIT_COIN_HASH}"   >> ${CURDIR}/Makefile.git_rev
+echo "GIT_PIVY_HASH= ${GIT_PIVY_HASH}"   >> ${CURDIR}/Makefile.git_rev
