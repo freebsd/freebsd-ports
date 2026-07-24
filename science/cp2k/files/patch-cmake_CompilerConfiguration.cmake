24ee2c8cca54fbdf12e98c5d38865dd2fa391cd7
Yuri Victorovich <yuri@FreeBSD.org>
2026-07-23 23:52:14 -0700

    science/cp2k{,-data}: update 2026.1 → 2026.2

--- science/cp2k/files/patch-cmake_CompilerConfiguration.cmake
+++ science/cp2k/files/patch-cmake_CompilerConfiguration.cmake
@@ -1,37 +1,10 @@
---- cmake/CompilerConfiguration.cmake.orig	2026-01-06 09:31:20 UTC
+--- cmake/CompilerConfiguration.cmake.orig	2026-07-24 05:31:47 UTC
 +++ cmake/CompilerConfiguration.cmake
-@@ -51,9 +51,9 @@ add_compile_options(
+@@ -52,7 +52,6 @@ add_compile_options(
  
- # Release
- add_compile_options(
--  "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANG_AND_ID:Fortran,GNU>>:-O3;-march=native;-mtune=native;-funroll-loops>"
--  "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANG_AND_ID:CXX,GNU>>:-O3;-march=native;-mtune=native;-funroll-loops>"
--  "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANG_AND_ID:C,GNU>>:-O3;-march=native;-mtune=native;-funroll-loops>"
-+  "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANG_AND_ID:Fortran,GNU>>:-O3;-funroll-loops>"
-+  "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANG_AND_ID:CXX,GNU>>:-O3;-funroll-loops>"
-+  "$<$<AND:$<CONFIG:RELEASE>,$<COMPILE_LANG_AND_ID:C,GNU>>:-O3;-funroll-loops>"
- )
- 
- # Generic
-@@ -65,9 +65,9 @@ add_compile_options(
- 
- # Debug
- add_compile_options(
--  "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANG_AND_ID:Fortran,GNU>>:-O1;-march=native;-mtune=native>"
--  "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANG_AND_ID:CXX,GNU>>:-O1;-march=native;-mtune=native>"
--  "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANG_AND_ID:C,GNU>>:-O1;-march=native;-mtune=native;-Wall;-Wextra;-Werror>"
-+  "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANG_AND_ID:Fortran,GNU>>:-O1>"
-+  "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANG_AND_ID:CXX,GNU>>:-O1>"
-+  "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANG_AND_ID:C,GNU>>:-O1>"
- )
- add_compile_options(
-   "$<$<AND:$<CONFIG:DEBUG>,$<COMPILE_LANG_AND_ID:Fortran,GNU>>:-fsanitize=leak;-Werror=realloc-lhs>"
-@@ -107,7 +107,7 @@ add_compile_options(
- 
- # Address Sanitizer
- add_compile_options(
--  "$<$<CONFIG:ASAN>:-fsanitize=address;-no-pie;-O3;-march=native;-mtune=native;-funroll-loops>"
-+  "$<$<CONFIG:ASAN>:-fsanitize=address;-no-pie;-O3;-funroll-loops>"
- )
- add_link_options("$<$<CONFIG:ASAN>:-fsanitize=address>")
- 
+ # -- Apple Silicon + GCC: -march=native expands internally to -march=apple-m1
+ # (invalid). Use -mcpu=native instead.
+-set(_CP2K_GNU_NATIVE_TUNE "-march=native;-mtune=native")
+ if(APPLE
+    AND CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64"
+    AND (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU"
