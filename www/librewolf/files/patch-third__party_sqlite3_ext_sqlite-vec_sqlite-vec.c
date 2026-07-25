cb262efa64784f3888bf42c050b5787b6ccf6dbc
Martin Filla <freebsd@sysctl.cz>
2026-07-25 03:24:09 +0300

    www/librewolf: Update 152.0.6-1 => 153.0-3
    Release Notes:
    https://www.firefox.com/en-US/firefox/153.0/releasenotes/
    
    - Fix runtime if build with CPUTYPE=native - required link with -lm.
    
    PR:		296966 296884
    Sponsored by:	UNIS Labs
    MFH:		2026Q3
    
    (cherry picked from commit 9b133df412f69946dd84ab6c48ed614dafb9483a)

--- www/librewolf/files/patch-third__party_sqlite3_ext_sqlite-vec_sqlite-vec.c
+++ www/librewolf/files/patch-third__party_sqlite3_ext_sqlite-vec_sqlite-vec.c
@@ -1,5 +1,10 @@
+commit 6336890d9f6eff8cc697b50d6e29a665b3b66050
+Author: Christoph Moench-Tegeder <cmt@FreeBSD.org>
+
+    additional include on FreeBSD
+
 diff --git third_party/sqlite3/ext/sqlite-vec/sqlite-vec.c third_party/sqlite3/ext/sqlite-vec/sqlite-vec.c
-index 96612e26d385..c43c46136bef 100644
+index 5511263765d7..6f9d64c730de 100644
 --- third_party/sqlite3/ext/sqlite-vec/sqlite-vec.c
 +++ third_party/sqlite3/ext/sqlite-vec/sqlite-vec.c
 @@ -11,6 +11,10 @@
@@ -10,6 +15,6 @@ index 96612e26d385..c43c46136bef 100644
 +#include <sys/types.h>
 +#endif
 +
- #ifndef SQLITE_VEC_OMIT_FS
+ #ifdef SQLITE_VEC_DEBUG
  #include <stdio.h>
  #endif
