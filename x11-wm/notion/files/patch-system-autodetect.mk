9f760ea4ed4ed99d30c8de277efe73bdbc2271db
Nuno Teixeira <eduardo@FreeBSD.org>
2026-07-26 22:44:32 +0100

    x11-wm/notion: Update to 4.0.5
    ChangeLog:	https://github.com/raboof/notion/releases/tag/4.0.5
    		https://github.com/raboof/notion/releases/tag/4.0.4

--- x11-wm/notion/files/patch-system-autodetect.mk
+++ x11-wm/notion/files/patch-system-autodetect.mk
@@ -1,4 +1,8 @@
---- system-autodetect.mk.orig	2025-02-08 16:04:21 UTC
+Apply llvm22 fix from OpenBSD port by commenting
+POSIX_SOURCE and XOPEN_SOURCE.
+fda9c504296741 ("update to notion-4.0.5, unbreaking build with llvm22")
+
+--- system-autodetect.mk.orig	2026-04-21 20:44:17 UTC
 +++ system-autodetect.mk
 @@ -138,6 +138,7 @@ HAS_SYSTEM_ASPRINTF=1
  
@@ -7,4 +11,18 @@
 +LIBS += -L$(LOCALBASE)/lib -lintl
  #CPPFLAGS += -I/usr/local/include
  
+ 
+@@ -195,11 +196,11 @@ EXPORT_DYNAMIC=-Xlinker --export-dynamic
+ 
+ #C89_SOURCE=-ansi
+ 
+-POSIX_SOURCE?=-D_POSIX_C_SOURCE=200112L
++#POSIX_SOURCE?=-D_POSIX_C_SOURCE=200112L
+ BSD_SOURCE?=-D_BSD_SOURCE
+ 
+ # Most systems
+-XOPEN_SOURCE=-D_XOPEN_SOURCE -D_XOPEN_SOURCE_EXTENDED
++#XOPEN_SOURCE=-D_XOPEN_SOURCE -D_XOPEN_SOURCE_EXTENDED
+ # SunOS, (Irix)
+ #XOPEN_SOURCE=-D__EXTENSIONS__
  
