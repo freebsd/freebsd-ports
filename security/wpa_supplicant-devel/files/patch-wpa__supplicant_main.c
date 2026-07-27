86592cb54370bdd07a4aec8e23e868ff1e4c7431
Cy Schubert <cy@FreeBSD.org>
2026-07-27 11:09:54 -0700

    security/wpa_supplicant-devel: Update to latest GH commit
    Update to the latest w1.fi commit, proxied through my GH account.

--- security/wpa_supplicant-devel/files/patch-wpa__supplicant_main.c
+++ security/wpa_supplicant-devel/files/patch-wpa__supplicant_main.c
@@ -1,6 +1,6 @@
---- wpa_supplicant/main.c.orig	2025-11-07 07:33:18.000000000 -0800
-+++ wpa_supplicant/main.c	2025-11-17 09:07:02.682216000 -0800
-@@ -67,7 +67,7 @@
+--- wpa_supplicant/main.c.orig	2026-07-27 09:50:49.000000000 -0700
++++ wpa_supplicant/main.c	2026-07-27 11:04:47.170395000 -0700
+@@ -70,7 +70,7 @@
  	       "  -c = Configuration file\n"
  	       "  -C = ctrl_interface parameter (only used if -c is not)\n"
  	       "  -d = increase debugging verbosity (-dd even more)\n"
@@ -9,17 +9,17 @@
  	       "  -e = entropy file\n"
  #ifdef CONFIG_DEBUG_FILE
  	       "  -f = log output to debug file instead of stdout\n"
-@@ -107,8 +107,7 @@
- 	       "  -y = show configuration parsing details in debug log\n");
+@@ -114,8 +114,7 @@
+ 		);
  
  	printf("example:\n"
 -	       "  wpa_supplicant -D%s -iwlan0 -c/etc/wpa_supplicant.conf\n",
 -	       wpa_drivers[0] ? wpa_drivers[0]->name : "nl80211");
-+	       "  wpa_supplicant -Dbsd -iwlan0 -c/etc/wpa_supplicant.conf\n");
- 	printf("\nIf run without specifying a network interface or control interface, the a\n"
++	        "  wpa_supplicant -Dbsd -iwlan0 -c/etc/wpa_supplicant.conf\n");
+ 	printf("\nIf run without specifying a network interface or control interface, a\n"
  	       "configuration file is parsed without starting any operation.\n"
  		"This can be used to check whether a configuration file has valid contents.\n");
-@@ -204,6 +203,11 @@
+@@ -211,6 +210,11 @@
  
  	wpa_supplicant_fd_workaround(1);
  
@@ -30,4 +30,4 @@
 +
  	for (;;) {
  		c = getopt(argc, argv,
- 			   "b:Bc:C:D:de:f:g:G:hi:I:KLMm:No:O:p:P:qsTtuvWy");
+ 			   "b:Bc:C:D:de:f:g:G:hi:I:KLMm:No:O:p:P:qsTtuvWyz:");
