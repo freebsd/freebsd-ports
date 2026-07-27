bea799878f71b55ef0f953ad3d9386e73bdea046
Hiroki Tagato <tagattie@FreeBSD.org>
2026-07-27 21:04:26 +0900

    x11/hypridle: Update to 0.1.8
    Changelog: https://github.com/hyprwm/hypridle/releases/tag/v0.1.8
    
    Reported by:	GitHub (watch releases)

--- x11/hypridle/files/patch-src_core_Hypridle.cpp
+++ x11/hypridle/files/patch-src_core_Hypridle.cpp
@@ -1,8 +1,8 @@
 https://github.com/hyprwm/hypridle/issues/68#issuecomment-2428697344
 
---- src/core/Hypridle.cpp.orig	2025-08-27 10:40:01 UTC
+--- src/core/Hypridle.cpp.orig	2026-07-26 14:44:59 UTC
 +++ src/core/Hypridle.cpp
-@@ -190,6 +190,7 @@ void CHypridle::enterEventLoop() {
+@@ -212,6 +212,7 @@ void CHypridle::enterEventLoop() {
          }
      });
  
