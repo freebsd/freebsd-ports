d6cf6e1450feacc627a2641123b50f0e341abef0
Hiroki Tagato <tagattie@FreeBSD.org>
2026-07-27 20:44:05 +0900

    x11/hyprshutdown: Unbreak build on -current and 15/stable
    Add missing #include <optional> for std::optional and std::nullopt.
    
    Reported by:	Adam Retter (via private email)
    MFH:		2026Q3
    
    (cherry picked from commit b7ae4aceafcac9d95ae104916a1755c1a0e22963)

---
+++ x11/hyprshutdown/files/patch-src_helpers_OS.cpp
@@ -0,0 +1,10 @@
+--- src/helpers/OS.cpp.orig	2026-07-26 05:17:40 UTC
++++ src/helpers/OS.cpp
+@@ -2,6 +2,7 @@
+ 
+ #include <filesystem>
+ #include <fstream>
++#include <optional>
+ 
+ #include <hyprutils/string/String.hpp>
+ #include <hyprutils/memory/Casts.hpp>
