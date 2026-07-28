238ba5e3fd1954379699698f9bc30c017e1ab0ff
Jason E. Hale <jhale@FreeBSD.org>
2026-07-28 00:58:46 -0400

    audio/libmysofa: Update to 1.3.5
    https://github.com/hoene/libmysofa/releases/tag/v1.3.5

--- audio/libmysofa/files/extrapatch-tests_compareIgnoreNew.sh
+++ audio/libmysofa/files/extrapatch-tests_compareIgnoreNew.sh
@@ -1,14 +1,14 @@
 Undo bashism added upstream in a5a3562. The built-in 'command' command of
 sh(1) does not take multiple arguments.
 
---- tests/compareIgnoreNew.sh.orig	2024-10-19 19:02:44 UTC
+--- tests/compareIgnoreNew.sh.orig	2026-07-19 09:52:37 UTC
 +++ tests/compareIgnoreNew.sh
-@@ -1,7 +1,7 @@ SCRIPTDIR=${0%/*}
- #!/bin/sh
+@@ -25,7 +25,7 @@ SCRIPTDIR=${0%/*}
+ test -x "${mysofa2json}" || usage "No mysofa2json executable: '${mysofa2json}'"
  
  SCRIPTDIR=${0%/*}
 -NODEJS=$(command -v node nodejs false | head -1)
 +NODEJS=$(which node nodejs false | head -1)
- TMP1=`mktemp -p . tmp1-XXXXXXXX.json`
- TMP2=`mktemp -p . tmp2-XXXXXXXX.txt`
- TMP3=`mktemp -p . tmp3-XXXXXXXX.json`
+ TMP1="$(mktemp -p . tmp1-XXXXXXXX.json)"
+ TMP2="$(mktemp -p . tmp2-XXXXXXXX.txt)"
+ TMP3="$(mktemp -p . tmp3-XXXXXXXX.json)"
