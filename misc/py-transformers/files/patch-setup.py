038115800a22acc399430a24c0166dfac5cb9ea1
Yuri Victorovich <yuri@FreeBSD.org>
2026-07-28 00:24:56 -0700

    misc/py-transformers: Patch out tokenizers's upper version limit

---
+++ misc/py-transformers/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2026-07-28 06:12:06 UTC
++++ setup.py
+@@ -143,7 +143,7 @@ _deps = [
+     "timeout-decorator",
+     "tiktoken",
+     "timm>=1.0.23",
+-    "tokenizers>=0.22.0,<=0.23.0",
++    "tokenizers>=0.22.0",
+     "torch>=2.4",
+     "torchaudio",
+     "torchvision",
---
+++ misc/py-transformers/files/patch-src_transformers_dependency__versions__table.py
@@ -0,0 +1,11 @@
+--- src/transformers/dependency_versions_table.py.orig	2026-07-28 06:12:44 UTC
++++ src/transformers/dependency_versions_table.py
+@@ -72,7 +72,7 @@ deps = {
+     "timeout-decorator": "timeout-decorator",
+     "tiktoken": "tiktoken",
+     "timm": "timm>=1.0.23",
+-    "tokenizers": "tokenizers>=0.22.0,<=0.23.0",
++    "tokenizers": "tokenizers>=0.22.0",
+     "torch": "torch>=2.4",
+     "torchaudio": "torchaudio",
+     "torchvision": "torchvision",
