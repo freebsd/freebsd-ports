d8da5e274da332231fafca6a13d0634cf665982d
Norikatsu Shigemura <nork@FreeBSD.org>
2026-07-30 18:00:38 +0900

    devel/py-cfn-lint: Update to 1.52.1
    ChangeLog:	https://github.com/aws-cloudformation/cfn-lint/compare/v1.50.1...v1.52.1
    Approved by:	hrs (mentor, blanket)

--- devel/py-cfn-lint/files/patch-src_cfnlint_maintenance.py
+++ devel/py-cfn-lint/files/patch-src_cfnlint_maintenance.py
@@ -1,6 +1,6 @@
---- src/cfnlint/maintenance.py.orig	2026-04-29 17:17:46 UTC
+--- src/cfnlint/maintenance.py.orig	2026-07-09 18:08:26 UTC
 +++ src/cfnlint/maintenance.py
-@@ -99,9 +99,8 @@ def update_documentation(rules):
+@@ -80,9 +80,8 @@ def update_documentation(rules):
                  "../"
                  + subprocess.check_output(
                      [
