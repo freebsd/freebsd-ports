622fff7546c5818819b6f29bc49584bc8dcb66fc
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2026-07-31 21:13:53 +0200

    databases/redis88: New port
    Redis is an open source, advanced key-value store.  It is often referred
    to as a data structure server since keys can contain strings, hashes,
    lists, sets and sorted sets.
    
    You can run atomic operations on these types, like appending to a string;
    incrementing the value in a hash; pushing to a list; computing set
    intersection, union and difference; or getting the member with highest
    ranking in a sorted set.
    
    In order to achieve its outstanding performance, Redis works with an
    in-memory dataset.  Depending on your use case, you can persist it either
    by dumping the dataset to disk every once in a while, or by appending each
    command to a log.
    
    Redis also supports trivial-to-setup master-slave replication, with very
    fast non-blocking first synchronization, auto-reconnection on net split
    and so forth.
    
    WWW: https://redis.io/
    
    This is redis version 8.8.X as a seperate port to updgrade the redis
    port to 8.10.X.

---
+++ databases/redis88/files/patch-deps_lua_src_lua__cjson.c
@@ -0,0 +1,12 @@
+--- deps/lua/src/lua_cjson.c.orig	2025-08-04 11:05:03 UTC
++++ deps/lua/src/lua_cjson.c
+@@ -47,7 +47,9 @@
+ #include "strbuf.h"
+ #include "fpconv.h"
+ 
++#if defined(__sun)
+ #include "../../../src/solarisfixes.h"
++#endif
+ 
+ #ifndef CJSON_MODNAME
+ #define CJSON_MODNAME   "cjson"
