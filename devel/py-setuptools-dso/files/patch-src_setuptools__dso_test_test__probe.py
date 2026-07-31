4aaa8eab2b5ed1fb69e6350852913613c1550d58
Yuri Victorovich <yuri@FreeBSD.org>
2026-07-30 22:48:48 -0700

    devel/py-setuptools-dso: Add FreeBSD to list of recognized systems; Add test results

--- devel/py-setuptools-dso/files/patch-src_setuptools__dso_test_test__probe.py
+++ devel/py-setuptools-dso/files/patch-src_setuptools__dso_test_test__probe.py
@@ -1,11 +1,15 @@
---- src/setuptools_dso/test/test_probe.py.orig	2024-08-16 07:22:13 UTC
+-- Add FreeBSD to the list of recognized operating systems in test_probe.py
+-- FreeBSD support was added to probe.py but the test still expected only
+-- the original OS list. This patch adds FreeBSD to the assertion.
+
+--- src/setuptools_dso/test/test_probe.py.orig	2026-02-11 01:15:48 UTC
 +++ src/setuptools_dso/test/test_probe.py
-@@ -70,7 +70,7 @@ is a test
+@@ -79,7 +79,7 @@ is a test
  
          self.assertIn(info.compiler, ('clang', 'gcc', 'msvc'))
          self.assertGreater(info.compiler_version, (0,))
 -        self.assertIn(info.target_os, ("cygwin", "linux", "osx", "windows"))
-+        #self.assertIn(info.target_os, ("cygwin", "linux", "osx", "windows"))
++        self.assertIn(info.target_os, ("cygwin", "freebsd", "linux", "osx", "windows"))
          self.assertIn(info.target_arch, ("aarch64", "arm32", "amd64", "i386"))
          self.assertIn(info.address_width, (32, 64))
          self.assertIn(info.endian, ("little", "big"))
