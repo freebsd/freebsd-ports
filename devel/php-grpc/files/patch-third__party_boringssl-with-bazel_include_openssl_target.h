e6cdd1dc70d031e1dd5494797609e2ee4955e2d6
Piotr Kubaj <pkubaj@FreeBSD.org>
2026-08-03 11:31:22 +0200

    devel/php-grpc: add powerpc64* support
    /wrkdirs/usr/ports/devel/php-grpc/work-php82/grpc-php-ext-1.83.0/third_party/boringssl-with-bazel/include/openssl/target.h:74:2: error: "Unknown target CPU"
       74 | #error "Unknown target CPU"
          |  ^
    1 error generated.

---
+++ devel/php-grpc/files/patch-third__party_boringssl-with-bazel_include_openssl_target.h
@@ -0,0 +1,14 @@
+--- third_party/boringssl-with-bazel/include/openssl/target.h.orig	2026-08-03 08:36:24 UTC
++++ third_party/boringssl-with-bazel/include/openssl/target.h
+@@ -54,6 +54,11 @@
+ #define OPENSSL_32_BIT
+ #elif defined(__myriad2__)
+ #define OPENSSL_32_BIT
++#elif defined(__powerpc64__)
++// PowerPC64 has no BoringSSL assembly; the portable C code is used and the
++// build must define OPENSSL_NO_ASM.  BoringSSL only officially supports
++// little-endian, so big-endian ppc64 is unverified.
++#define OPENSSL_64_BIT
+ #else
+ // The list above enumerates the platforms that BoringSSL supports. For these
+ // platforms we keep a reasonable bar of not breaking them: automated test
