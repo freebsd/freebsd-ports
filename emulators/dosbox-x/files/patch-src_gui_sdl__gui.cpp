a03ea60cadac2f745030a0e45d304abaaf5e2979
Nuno Teixeira <eduardo@FreeBSD.org>
2026-08-03 18:34:17 +0100

    emulators/dosbox-x: Update to 2026.08.02
    ChangeLog:
    https://github.com/joncampbell123/dosbox-x/releases/tag/dosbox-x-v2026.08.02

--- emulators/dosbox-x/files/patch-src_gui_sdl__gui.cpp
+++ emulators/dosbox-x/files/patch-src_gui_sdl__gui.cpp
@@ -1,6 +1,6 @@
---- src/gui/sdl_gui.cpp.orig	2026-06-02 15:52:54 UTC
+--- src/gui/sdl_gui.cpp.orig	2026-08-02 19:09:21 UTC
 +++ src/gui/sdl_gui.cpp
-@@ -4075,6 +4075,7 @@ void GUI_Shortcut(int select) {
+@@ -4074,6 +4074,7 @@ void GUI_Shortcut(int select) {
      shortcutid=select;
      shortcut=true;
      sel = select;
@@ -8,7 +8,7 @@
  #if defined(USE_TTF)
      if (ttf.inUse && !confres) {
          ttf_switch_off();
-@@ -4084,12 +4085,14 @@ void GUI_Shortcut(int select) {
+@@ -4083,12 +4084,14 @@ void GUI_Shortcut(int select) {
      } else
  #endif
      RunCfgTool(0);
@@ -23,7 +23,7 @@
  #if defined(USE_TTF)
      if (ttf.inUse) {
          ttf_switch_off();
-@@ -4099,4 +4102,5 @@ void GUI_Run(bool pressed) {
+@@ -4098,4 +4101,5 @@ void GUI_Run(bool pressed) {
      } else
  #endif
      RunCfgTool(0);
