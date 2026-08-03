9159d5a816042ff45dbe9d3d50f76d748291fc5f
Brooks Davis <brooks@FreeBSD.org>
2026-08-03 19:13:25 +0100

    devel/llvm23: bootstrap from devel/llvm22
    This is an unmodified copy of llvm22 (except for the removal of the
    just added patch files/patch-backport-93a67259cf23 which we won't need).
    
    The next commit will convert this this to llvm23 and attach it to the
    tree.
    
    Sponsored by:	DARPA, AFRL

---
+++ devel/llvm23/files/patch-clang_lib_Basic_Targets_PPC.h
@@ -0,0 +1,16 @@
+--- clang/lib/Basic/Targets/PPC.h.orig
++++ clang/lib/Basic/Targets/PPC.h
+@@ -452,7 +452,12 @@
+       ABI = "elfv1";
+     }
+ 
+-    if (Triple.isOSFreeBSD() || Triple.isOSOpenBSD() || Triple.isMusl()) {
++    if (Triple.isOSFreeBSD() && Triple.getArch() == llvm::Triple::ppc64le &&
++        Triple.getOSMajorVersion() >= 16) {
++      LongDoubleWidth = LongDoubleAlign = 128;
++      LongDoubleFormat = &llvm::APFloat::IEEEquad();
++    } else if (Triple.isOSFreeBSD() || Triple.isOSOpenBSD() ||
++               Triple.isMusl()) {
+       LongDoubleWidth = LongDoubleAlign = 64;
+       LongDoubleFormat = &llvm::APFloat::IEEEdouble();
+     }
---
+++ devel/llvm23/files/patch-clang_lib_Driver_ToolChain.cpp
@@ -0,0 +1,13 @@
+--- clang/lib/Driver/ToolChain.cpp.orig
++++ clang/lib/Driver/ToolChain.cpp
+@@ -155,6 +155,10 @@
+ }
+ 
+ bool ToolChain::defaultToIEEELongDouble() const {
++  if (getTriple().isOSFreeBSD() &&
++      getTriple().getArch() == llvm::Triple::ppc64le &&
++      getTriple().getOSMajorVersion() >= 16)
++    return true;
+   return PPC_LINUX_DEFAULT_IEEELONGDOUBLE && getTriple().isOSLinux();
+ }
+ 
