f54a46204ffec8fdfa170ce0429416e0343505da
Pietro Cerutti <gahr@FreeBSD.org>
2026-08-03 09:12:31 +0000

    filesystems/tclvfs: update to 1.5.0
    PR:		297213
    Submitted by:	Simon Wells <swel024+bsd@gmail.com>

--- filesystems/tclvfs/files/patch-generic_vfs.c
+++ filesystems/tclvfs/files/patch-generic_vfs.c
@@ -1,14 +1,14 @@
---- generic/vfs.c.orig	2006-08-30 21:38:03.000000000 +0200
-+++ generic/vfs.c	2013-09-24 10:26:34.000000000 +0200
-@@ -1656,7 +1656,10 @@
-     return returnVal;
- }
+--- generic/vfs.c.orig	2026-08-03 07:46:37 UTC
++++ generic/vfs.c
+@@ -189,11 +189,7 @@ VfsFreeInternalRep(
  
--static CONST char**
-+#ifndef CONST86
-+#define CONST86
-+#endif
-+static CONST char*CONST86 *
- VfsFileAttrStrings(pathPtr, objPtrRef)
-     Tcl_Obj* pathPtr;
-     Tcl_Obj** objPtrRef;
+ static void
+ VfsFreeInternalRep(
+-#if TCL_MAJOR_VERSION < 9
+-		   char* clientData
+-#else
+ 		   ClientData clientData
+-#endif
+ 		   ) {
+     VfsNativeRep *nativeRep = (VfsNativeRep*)clientData;
+     if (nativeRep != NULL) {
