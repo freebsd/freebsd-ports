3fa6049eab77cf3e1f81cdc7320df1feaf563886
Piotr Kubaj <pkubaj@FreeBSD.org>
2026-08-03 17:17:19 +0200

    net-im/libsignal: fix build on powerpc64*
    ring-sys 5.0.2] /wrkdirs/usr/ports/net-im/libsignal/work/target/release/build/boring-sys-ef33d39251a122cc/out/boringssl/crypto/fipsmodule/../internal.h:230:2: error: "Must define either OPENSSL_32_BIT or OPENSSL_64_BIT"

---
+++ net-im/libsignal/files/patch-boringssl_include_openssl_target.h
@@ -0,0 +1,14 @@
+--- ../boring-signal-v5.0.2/boring-sys/deps/boringssl/include/openssl/target.h.orig
++++ ../boring-signal-v5.0.2/boring-sys/deps/boringssl/include/openssl/target.h
+@@ -54,6 +54,11 @@
+ #define OPENSSL_32_BIT
+ #elif defined(__myriad2__)
+ #define OPENSSL_32_BIT
++#elif defined(__powerpc64__)
++// PowerPC64 has no BoringSSL assembly; the portable C code is used and the
++// build must define OPENSSL_NO_ASM.  BoringSSL only officially supports
++// little-endian, so big-endian ppc64 is unverified.
++#define OPENSSL_64_BIT
+ #else
+ // The list above enumerates the platforms that BoringSSL supports. For these
+ // platforms we keep a reasonable bar of not breaking them: automated test
