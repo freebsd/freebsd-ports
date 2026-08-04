4744e66b618a15a657bc75bd1ce78ee610616650
Robert Clausecker <fuz@FreeBSD.org>
2026-08-04 18:20:06 +0200

    games/CWR-CE: fix build on armv7
     - disabled pre-compiled headers to work around a long-standing clang bug
     - apply sse2neon workaround to arm, too
     - fix assumption of size_t being 64 bits in a hash calculation
     - improve architecture name reporting
    
    See also:	https://github.com/llvm/llvm-project/issues/69524
    Approved by:	portmgr (build fix blanket)
    MFH:		2026Q3
    
    (cherry picked from commit 319393265d592e3bd5b422959190a12bd4e02033)

--- games/CWR-CE/files/patch-engine_Poseidon_Foundation_Math_V3QuadsP3.cpp
+++ games/CWR-CE/files/patch-engine_Poseidon_Foundation_Math_V3QuadsP3.cpp
@@ -1,18 +1,17 @@
---- engine/Poseidon/Foundation/Math/V3QuadsP3.cpp.orig
+--- engine/Poseidon/Foundation/Math/V3QuadsP3.cpp.orig	2026-08-04 10:46:28 UTC
 +++ engine/Poseidon/Foundation/Math/V3QuadsP3.cpp
-@@ -7,11 +7,15 @@
+@@ -7,10 +7,14 @@
  #include <Poseidon/Foundation/Math/V3Quads.hpp>
  #include <Poseidon/Graphics/Core/TLVertex.hpp>
  #include <Poseidon/Foundation/Math/Math3D.hpp>
-+#ifdef __aarch64__
++#if defined(__aarch64__) || defined(__arm__)
 +#include <sse2neon.h>
 +#else
  #ifdef _MSC_VER
  #include <intrin.h> // For MMX intrinsics
  #else
  #include <x86intrin.h>
- #endif
 +#endif
-
+ #endif
+ 
  #if defined __ICL
- #define _COMPILER_CAN_PIII 1
