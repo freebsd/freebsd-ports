b17e830a139cf1ff24b9b559ecabe4e0002bbc49
Jochen Neumeister <joneum@FreeBSD.org>
2026-08-06 13:45:06 +0300

    www/freenginx-devel: follow change of other nginx ports
    Reviewed by:	osa (maintainer, mentor)
    Approved by:	osa (maintainer, mentor)
    Differential Revision: https://reviews.freebsd.org/D58692
    
    (cherry picked from commit 8c2ac84b701610ea2c269aeb8f06ead8eae48cb2)

--- www/freenginx-devel/files/extra-patch-nginx-gridfs_ngx_http_gridfs_module.c
+++ www/freenginx-devel/files/extra-patch-nginx-gridfs_ngx_http_gridfs_module.c
@@ -1,11 +1,12 @@
 --- ../nginx-gridfs-059bdc3/nginx-gridfs/ngx_http_gridfs_module.c.orig	2018-01-20 07:53:32.000000000 +0000
 +++ ../nginx-gridfs-059bdc3/nginx-gridfs/ngx_http_gridfs_module.c	2026-07-07 14:27:08.501699000 +0000
-@@ -1,7 +1,7 @@
+@@ -1,7 +1,8 @@
  #include <ngx_config.h>
  #include <ngx_core.h>
  #include <ngx_http.h>
 -#include <mongoc.h>
 +#include <mongoc/mongoc.h>
++#include <assert.h>
  #include <signal.h>
  #include <stdio.h>
  
