e297efc8850c237df2593bc23137fca36787d8b3
Jochen Neumeister <joneum@FreeBSD.org>
2026-08-06 18:21:19 +0200

    net/py-lib389: Fix instance creation on FreeBSD
    Adjust the FreeBSD patch for lib389's instance setup code.
    
    Guard the container detection against a missing systemd-detect-virt
    binary.  Upstream initializes container_result to an integer and then
    unconditionally executes systemd-detect-virt on non-containerised
    systems, which aborts instance creation with a FileNotFoundError on
    FreeBSD.  The check now only runs when the binary is present and
    falls back to the non-container code path otherwise.
    
    Use a plain localhost LDAP connection with the generated temporary
    Directory Manager password for the authenticated setup session, as
    LDAPI autobind via SASL EXTERNAL did not work reliably during setup
    on FreeBSD.
    
    Sponsored by:	Netzkommune GmbH

--- net/py-lib389/files/patch-lib389_instance_setup.py
+++ net/py-lib389/files/patch-lib389_instance_setup.py
@@ -1,4 +1,4 @@
---- lib389/instance/setup.py.orig	2026-06-29 17:03:20 UTC
+--- lib389/instance/setup.py.orig	2026-04-30 12:45:04 UTC
 +++ lib389/instance/setup.py
 @@ -826,10 +826,10 @@ class SetupDs(object):
          # which subsequently breaks containers starting as instance.start then believes
@@ -14,3 +14,34 @@
              # In a container, set the db_home_dir to the db path
              self.log.debug("Container detected setting db home directory to db directory.")
              slapd['db_home_dir'] = slapd['db_dir']
+@@ -962,14 +962,10 @@ class SetupDs(object):
+         if self.containerised:
+             ds_instance.systemd_override = general['systemd']
+ 
+-        # By default SUSE does something extremely silly - it creates a hostname
+-        # that CANT be resolved by DNS. As a result this causes all installs to
+-        # fail. We need to guarantee that we only connect to localhost here, as
+-        # it's the only stable and guaranteed way to connect to the instance
+-        # at this point.
+-        #
+-        # Use ldapi which would prevent the need
+-        # to configure a temp root pw in the setup phase.
++        # Use a local LDAP connection for the temporary authenticated setup session.
++        # FreeBSD's OpenLDAP client stack rejects SASL EXTERNAL over LDAPI here,
++        # while the generated temporary Directory Manager password provides the
++        # required authenticated connection over localhost LDAP.
+         args = {
+             SER_HOST: "localhost",
+             SER_PORT: slapd['port'],
+@@ -977,9 +973,9 @@ class SetupDs(object):
+             SER_ROOT_DN: slapd['root_dn'],
+             SER_ROOT_PW: self._raw_secure_password,
+             SER_DEPLOYED_DIR: slapd['prefix'],
+-            SER_LDAPI_ENABLED: 'on',
++            SER_LDAPI_ENABLED: 'off',
+             SER_LDAPI_SOCKET: slapd['ldapi'],
+-            SER_LDAPI_AUTOBIND: 'on'
++            SER_LDAPI_AUTOBIND: 'off'
+         }
+ 
+         ds_instance.allocate(args)
