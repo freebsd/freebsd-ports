1b3f9be6bf25e7e3be3ab9da05181119d06705fd
Jason E. Hale <jhale@FreeBSD.org>
2026-08-08 02:44:06 -0400

    multimedia/vid.stab: Update to 1.1.2
    Update WWW to the GitHub repo as the main site has not been updated in
    9 years.
    
    Remove pathfix hacks that are now automatically handled.

--- multimedia/vid.stab/files/patch-CMakeModules_FindSSE.cmake
+++ multimedia/vid.stab/files/patch-CMakeModules_FindSSE.cmake
@@ -1,39 +1,39 @@
---- CMakeModules/FindSSE.cmake.orig	2022-05-30 15:49:01 UTC
+--- CMakeModules/FindSSE.cmake.orig	2026-01-04 14:53:14 UTC
 +++ CMakeModules/FindSSE.cmake
-@@ -1,10 +1,32 @@ include(CheckCCompilerFlag)
- # Check if SSE instructions are available by the compiler and target platform (be aware of cross compilation)
- include(CheckCCompilerFlag)
+@@ -11,10 +11,32 @@ else()
  
--check_c_compiler_flag(-msse2 HAVE_SSE2)
--check_c_compiler_flag(-msse3 HAVE_SSE3)
--check_c_compiler_flag(-mssse3 HAVE_SSSE3)
--check_c_compiler_flag(-msse4.1 HAVE_SSE4_1)
-+if(CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
-+      string(REGEX MATCH "sse2" FREEBSD_SSE2 ${CPUINFO})
-+      if(FREEBSD_SSE2)
+ else()
+       # GNU/Clang-like flow
+-      check_c_compiler_flag(-msse2 HAVE_SSE2)
+-      check_c_compiler_flag(-msse3 HAVE_SSE3)
+-      check_c_compiler_flag(-mssse3 HAVE_SSSE3)
+-      check_c_compiler_flag(-msse4.1 HAVE_SSE4_1)
++      if(CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
++            string(REGEX MATCH "sse2" FREEBSD_SSE2 ${CPUINFO})
++            if(FREEBSD_SSE2)
++                  check_c_compiler_flag(-msse2 HAVE_SSE2)
++            endif()
++
++            string(REGEX MATCH "(^|[^s])sse3" FREEBSD_SSE3 ${CPUINFO})
++            if(FREEBSD_SSE3)
++                  check_c_compiler_flag(-msse3 HAVE_SSE3)
++            endif()
++
++            string(REGEX MATCH "ssse3" FREEBSD_SSSE3 ${CPUINFO})
++            if(FREEBSD_SSSE3)
++                  check_c_compiler_flag(-mssse3 HAVE_SSSE3)
++            endif()
++
++            string(REGEX MATCH "sse41" FREEBSD_SSE41 ${CPUINFO})
++            if(FREEBSD_SSE41)
++                  check_c_compiler_flag(-msse4.1 HAVE_SSE4_1)
++            endif()
++      else()
 +            check_c_compiler_flag(-msse2 HAVE_SSE2)
-+      endif()
-+
-+      string(REGEX MATCH "(^|[^s])sse3" FREEBSD_SSE3 ${CPUINFO})
-+      if(FREEBSD_SSE3)
 +            check_c_compiler_flag(-msse3 HAVE_SSE3)
-+      endif()
-+
-+      string(REGEX MATCH "ssse3" FREEBSD_SSSE3 ${CPUINFO})
-+      if(FREEBSD_SSSE3)
 +            check_c_compiler_flag(-mssse3 HAVE_SSSE3)
-+      endif()
-+
-+      string(REGEX MATCH "sse41" FREEBSD_SSE41 ${CPUINFO})
-+      if(FREEBSD_SSE41)
 +            check_c_compiler_flag(-msse4.1 HAVE_SSE4_1)
 +      endif()
-+else()
-+      check_c_compiler_flag(-msse2 HAVE_SSE2)
-+      check_c_compiler_flag(-msse3 HAVE_SSE3)
-+      check_c_compiler_flag(-mssse3 HAVE_SSSE3)
-+      check_c_compiler_flag(-msse4.1 HAVE_SSE4_1)
-+endif()
  
- # Some compilers understand SSE flags, even when target platform doesn't support it (Clang with arm target)
- # It is necessary try to compile actual code
+       if(HAVE_SSE2)
+             try_compile(SSE_OK "${PROJECT_BINARY_DIR}"
