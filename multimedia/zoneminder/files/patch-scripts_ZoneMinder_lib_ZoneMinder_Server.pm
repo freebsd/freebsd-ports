ed03c39dda5d9aba20dd678db0d823be84302be4
Ivan <bsd@abinet.ru>
2026-08-09 18:02:45 +0300

    multimedia/zoneminder: Update 1.38.1 => 1.38.3
    Changelog:
    https://github.com/ZoneMinder/zoneminder/releases/tag/1.38.3
    
    - Fix ONVIF support with add devel/gsoap to BUILD_DEPENDS.
    
    PR:		296995
    Security:	GHSA-g66m-77fq-79v9
    Security:	GHSA-745h-vg7c-73cg
    Sponsored by:	UNIS Labs
    Co-authored-by:	Vladimir Druzenko <vvd@FreeBSD.org>
    MFH:		2026Q3
    
    (cherry picked from commit 2f1af67af413fa32819d0dac645b473769845559)

--- multimedia/zoneminder/files/patch-scripts_ZoneMinder_lib_ZoneMinder_Server.pm
+++ multimedia/zoneminder/files/patch-scripts_ZoneMinder_lib_ZoneMinder_Server.pm
@@ -1,19 +1,11 @@
---- scripts/ZoneMinder/lib/ZoneMinder/Server.pm.orig	2026-02-16 23:32:05 UTC
+--- scripts/ZoneMinder/lib/ZoneMinder/Server.pm.orig	2026-05-28 12:42:49 UTC
 +++ scripts/ZoneMinder/lib/ZoneMinder/Server.pm
-@@ -127,7 +127,15 @@ sub CpuUsage {
- 
-   } else {
-     # Get CPU utilization percentages
--    my $top_output = `top -b -n 1 | grep -i "^%Cpu(s)" | awk '{print \$2, \$4, \$6, \$8}'`;
-+    my $top_output = '';
-+    my $uname_output = lc(qx($ZoneMinder::Config{ZM_PATH_UNAME} -s));
-+    chomp($uname_output);
-+    ## FreeBSD
-+    if ($uname_output eq "freebsd") {
-+      $top_output = `top -b -d 2 -s 0,2 0 | grep -i "^CPU" | tail -n1 | tr -d '%' | awk '{print \$2, \$6, \$4, \$10}'`;
-+    } else {
-+      $top_output = `top -b -n 1 | grep -i "^%Cpu(s)" | awk '{print \$2, \$4, \$6, \$8}'`;
-+    }
-     my ($user, $system, $nice, $idle) = split(/ /, $top_output);
-     $user =~ s/[^\d\.]//g;
-     $system =~ s/[^\d\.]//g;
+@@ -132,7 +132,7 @@ sub CpuUsage {
+     chomp($uname_output);
+     ## FreeBSD
+     if ($uname_output eq "freebsd") {
+-      $top_output = `top -b -n 1 | grep "^CPU" | sed 's/%//g' | awk '{print \$2, \$6, \$4, \$10}'`;
++      $top_output = `top -b -d 2 -s 0.2 0 | grep -i "^CPU" | tail -n1 | tr -d '%' | awk '{print \$2, \$6, \$4, \$10}'`;
+     } else {
+       $top_output = `top -b -n 1 | grep -i "^%Cpu(s)" | awk '{print \$2, \$4, \$6, \$8}'`;
+     }
