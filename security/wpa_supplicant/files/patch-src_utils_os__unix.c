28460afd5dd59ec072e978783bfc3ef92bca9f28
Cy Schubert <cy@FreeBSD.org>
2026-08-10 08:26:44 -0700

    security/wpa_supplicant: Update to 2.12
    This includes the following changes:
    
    * support RSN overriding (e.g., WPA3-Personal Compatibility Mode)
    * improve BSS transition management support
    * EHT/IEEE 802.11be/Wi-Fi 7
      - more complete support
      - fix message validation issues that could enable DoS attacks
    * support Wi-Fi Direct R2
    * support Wi-Fi Aware (add synchronized NAN; extend USD support)
    * support Proximity Ranging
    * support SPP A-MSDU negotiation
    * support IEEE 802.11bi functionality
      - changing SAE password identifiers
      - EPPKE
      - IEEE 802.1X/EAP in Authentication frames
      - Association frame encryption
      - PMKID privacy
    * enable layer 2/Wi-Fi multicast filtering for all networks (not
      just some Passpoint networks which enabled this before)
    * wpa_gui: port to Qt6
    * support OpenSSL 3.0 API changes
    * EAP-TEAP: protocol changes based on RFC 9930; this is not compatible
      with previous versions
    * maintain configuration file permissions when writing updated
      configuration
    * add option to validate PKCS#11/OpenSC engine and module paths
    * fix PMKSA caching to enforce network context to avoid misuse of
      unexpected PMKSA cache entries
    * fix a potential DoS attack in SAE processing of an unexpected element
    * fix incomplete bounds checking of mesh AMPE messages that could have
      resulted in DoS attacks and memory corruption
    * a large number of other fixes, cleanup, and extensions

--- security/wpa_supplicant/files/patch-src_utils_os__unix.c
+++ security/wpa_supplicant/files/patch-src_utils_os__unix.c
@@ -1,14 +1,16 @@
---- src/utils/os_unix.c.orig	2024-07-20 18:04:37 UTC
-+++ src/utils/os_unix.c
-@@ -103,9 +103,11 @@ int os_get_reltime(struct os_reltime *t)
- 			break;
+--- src/utils/os_unix.c.orig	2026-08-07 06:10:53.000000000 -0700
++++ src/utils/os_unix.c	2026-08-10 07:55:55.825032000 -0700
+@@ -124,11 +124,13 @@
  #endif
+ 
  #ifdef CLOCK_MONOTONIC
 +#if !(defined(CLOCK_BOOTTIME) && CLOCK_BOOTTIME == CLOCK_MONOTONIC)
- 		case CLOCK_MONOTONIC:
+ 		if (clock_id == CLOCK_MONOTONIC) {
  			clock_id = CLOCK_REALTIME;
- 			break;
-+#endif
+ 			continue;
+ 		}
  #endif
- 		case CLOCK_REALTIME:
- 			return -1;
++#endif
+ 
+ 		/* No additional clockid_t values to fall back to */
+ 		return -1;
