7346801469aaf9b97d520714704b2f043b383acc
Siva Mahadevan <siva@FreeBSD.org>
2026-08-12 12:21:09 -0400

    x11-wm/river-classic: update to 0.3.17

--- x11-wm/river-classic/Makefile.zig
+++ x11-wm/river-classic/Makefile.zig
@@ -1,4 +1,6 @@
-ZIG_TUPLE=	pixman:codeberg.org/ifreund/zig-pixman/archive/v0.3.0.tar.gz?dummy=/pixman-0.3.0.tar.gz:pixman-0.3.0-LClMnz2VAAAs7QSCGwLimV5VUYx0JFnX5xWU6HwtMuDX \
-		wayland:codeberg.org/ifreund/zig-wayland/archive/v0.4.0.tar.gz?dummy=/wayland-0.4.0.tar.gz:wayland-0.4.0-lQa1khbMAQAsLS2eBR7M5lofyEGPIbu2iFDmoz8lPC27 \
-		wlroots:codeberg.org/ifreund/zig-wlroots/archive/v0.20.0.tar.gz?dummy=/wlroots-0.20.0.tar.gz:wlroots-0.20.0-jmOlcmtCBADS6eoJ6mkeiSNZkibrhD-c5Qwn-LiM86r1 \
-		xkbcommon:codeberg.org/ifreund/zig-xkbcommon/archive/v0.3.0.tar.gz?dummy=/xkbcommon-0.3.0.tar.gz:xkbcommon-0.3.0-VDqIe3K9AQB2fG5ZeRcMC9i7kfrp5m2rWgLrmdNn9azr
+ZIG_TUPLE=	aro:github.com/Vexu/arocc/archive/5f5a050569a95ecc40a426f0c3666ae7ef987ede.tar.gz?dummy=/arocc-5f5a050569a95ecc40a426f0c3666ae7ef987ede.tar.gz:aro-0.0.0-JSD1Qi7QNgDnfcrdEJf82v3o6MhZySjYVrtdfEf3E4Se \
+		pixman:codeberg.org/ifreund/zig-pixman/archive/v0.3.0.tar.gz?dummy=/zig-pixman-0.3.0.tar.gz:pixman-0.3.0-LClMnz2VAAAs7QSCGwLimV5VUYx0JFnX5xWU6HwtMuDX \
+		translate_c:codeberg.org/ziglang/translate-c/archive/7a1a9fdc4ab00835748a6657ecbb835e3d5d45f7.tar.gz?dummy=/translate-c-7a1a9fdc4ab00835748a6657ecbb835e3d5d45f7.tar.gz:translate_c-0.0.0-Q_BUWvP1BgCjAk6PWv5286tOlvzD9-X-NkuTzh0KxY0Q \
+		wayland:codeberg.org/ifreund/zig-wayland/archive/v0.6.0.tar.gz?dummy=/zig-wayland-0.6.0.tar.gz:wayland-0.6.0-lQa1kqz8AQADQmdNJsNhLoNHcnEGEUjrOaPV-dtEnEmX \
+		wlroots:codeberg.org/ifreund/zig-wlroots/archive/v0.20.1.tar.gz?dummy=/zig-wlroots-0.20.1.tar.gz:wlroots-0.20.1-jmOlcqNVBAB3uB5oqBTzpRlwu-FmMyyZMVAWCe5kmcSt \
+		xkbcommon:codeberg.org/ifreund/zig-xkbcommon/archive/v0.3.0.tar.gz?dummy=/zig-xkbcommon-0.3.0.tar.gz:xkbcommon-0.3.0-VDqIe3K9AQB2fG5ZeRcMC9i7kfrp5m2rWgLrmdNn9azr
