ddbfaf9b8ff239e2b7f5d37d5ab2cfa474a11077
Matthias Andree <mandree@FreeBSD.org>
2026-08-14 01:38:25 +0200

    graphics/rawtherapee: Update 5.12 => 5.13
    Extend patch for rtengine/LibRaw.cmake to properly forward our local
    CXXFLAGS (available through ${CMAKE_CXX_FLAGS} inside cmake) to the
    libraw build, else it will build against GCC's default Standard C++
    Library, which might be libstdc++, which then breaks when doing the
    final link against the common libc++ based system (GNU libstdc++ and
    LLVM's libc++ have incompatible ABIs).
    
    Switch to using the system libfmt, which is version 12 and good.
    
    Changelog:	https://rawtherapee.com/downloads/5.13/
    PR:		297513
    Reported by:	mandree (maintainer)
    Approved by:	osa, vvd (Mentors, implicit)
    Pull Request:	https://github.com/freebsd/freebsd-ports/pull/581

--- graphics/rawtherapee/files/patch-rtengine_LibRaw.cmake
+++ graphics/rawtherapee/files/patch-rtengine_LibRaw.cmake
@@ -1,9 +1,20 @@
---- rtengine/LibRaw.cmake.orig	2025-05-28 01:02:22 UTC
+--- rtengine/LibRaw.cmake.orig	2026-07-25 22:58:00 UTC
 +++ rtengine/LibRaw.cmake
-@@ -33,6 +33,27 @@ execute_process(
+@@ -28,7 +28,8 @@ set(CONFIGURE_FLAGS "--disable-examples")
+ 
+ # Configuration flags.
+ set(CONFIGURE_FLAGS "--disable-examples")
+-set(LIBRAW_CXX_FLAGS "${CXX_FLAGS} -std=gnu++11 -Wno-error=unknown-pragmas")
++set(LIBRAW_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX_FLAGS} -std=gnu++11 -Wno-error=unknown-pragmas")
++message("LIBRAW_CXX_FLAGS: ${LIBRAW_CXX_FLAGS}, ENV: $ENV{CXXFLAGS}")
+ # Let the configure script handle OpenMP flags.
+ string(REPLACE "${OpenMP_CXX_FLAGS}" "" LIBRAW_CXX_FLAGS "${LIBRAW_CXX_FLAGS}")
+ if(OPTION_OMP)
+@@ -42,6 +43,27 @@ message(STATUS "Configuring LibRaw")
+ 
  # Configuration commands.
  message(STATUS "Configuring LibRaw")
- execute_process(
++execute_process(
 +	COMMAND pkg-config --cflags-only-I libjpeg
 +	RESULT_VARIABLE PROCESS_RESULT
 +	OUTPUT_VARIABLE JPEG_BIGI_FLAGS
@@ -24,7 +35,6 @@
 +message(STATUS "libjpeg flags (from pkg-config): CFLAGS=\"${JPEG_BIGI_FLAGS}\" LIBS=\"${JPEG_BIGL_FLAGS}\"")
 +set(CONFIGURE_FLAGS "${CONFIGURE_FLAGS} CFLAGS=\"${CFLAGS} ${JPEG_BIGI_FLAGS}\"")
 +set(CONFIGURE_FLAGS "${CONFIGURE_FLAGS} LIBS=\"${LIBS} ${JPEG_BIGL_FLAGS}\"")
-+execute_process(
+ execute_process(
      COMMAND cp -p -R "${CMAKE_CURRENT_SOURCE_DIR}/libraw" .
      WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
-     RESULT_VARIABLE PROCESS_RESULT
