7085414bb689e138b5cef77beec7d63460dc3ccc
Michael Osipov <michaelo@FreeBSD.org>
2026-08-14 23:55:08 +0200

    devel/py-pyyaml: Convert build to use PEP 517
    PR:		297773
    Reviewed by:	vishwin (python)
    Differential Revision:	https://reviews.freebsd.org/D58848

---
+++ devel/py-pyyaml/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2026-08-15 00:00:00 UTC
++++ setup.py
+@@ -1,5 +1,5 @@
+
+-NAME = 'PyYAML'
++NAME = 'pyyaml'
+ VERSION = '6.0.3'
+ DESCRIPTION = "YAML parser and emitter for Python"
+ LONG_DESCRIPTION = """\
+
