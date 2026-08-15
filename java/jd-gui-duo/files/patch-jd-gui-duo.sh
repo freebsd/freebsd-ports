08ea114d8701546cf04ce9c1a4d62ec7ec41d406
Vladimir Druzenko <vvd@FreeBSD.org>
2026-08-15 16:23:01 +0300

    java/jd-gui-duo: Update 2.0.113 => 2.0.114
    Changelog:
    https://github.com/nbauma109/jd-gui-duo/releases/tag/2.0.114
    
    Commit log:
    https://github.com/nbauma109/jd-gui-duo/compare/2.0.113...2.0.114
    
    - Fix start script.
    - Remove unnecessary files accidentally added in the previous commit.
    
    Sponsored by:	UNIS Labs
    MFH:		2026Q3

--- java/jd-gui-duo/files/patch-jd-gui-duo.sh
+++ java/jd-gui-duo/files/patch-jd-gui-duo.sh
@@ -1,11 +1,14 @@
---- jd-gui-duo.sh.orig	2026-06-30 08:39:56 UTC
+--- jd-gui-duo.sh.orig	2026-07-25 16:26:55 UTC
 +++ jd-gui-duo.sh
-@@ -1,9 +1,8 @@
+@@ -1,9 +1,11 @@
 -#!/bin/bash
 -DIR="$(dirname "$0")"
 -"$DIR/jre/bin/java" -ea \
 +#!/bin/sh
-+exec %%JAVA%% -ea \
++if [ -z "${JAVA_HOME}" ]; then
++  JAVA_HOME="%%JAVA_HOME%%"
++fi
++exec "${JAVA_HOME}/bin/java" -ea \
    --add-opens java.base/java.net=ALL-UNNAMED \
    --add-opens java.desktop/javax.swing.plaf.basic=ALL-UNNAMED \
    --add-opens java.desktop/javax.swing.text=ALL-UNNAMED \
