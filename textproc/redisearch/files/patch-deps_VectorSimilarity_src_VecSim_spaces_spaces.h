6849075f9519d389d166ca17c702f72c03f403c0
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2026-08-16 02:08:06 +0200

    textproc/redisearch: Update version 8.8.0=>8.10.0
    Changelog: https://github.com/RediSearch/RediSearch/releases/tag/v8.10.0

--- textproc/redisearch/files/patch-deps_VectorSimilarity_src_VecSim_spaces_spaces.h
+++ textproc/redisearch/files/patch-deps_VectorSimilarity_src_VecSim_spaces_spaces.h
@@ -1,6 +1,6 @@
---- deps/VectorSimilarity/src/VecSim/spaces/spaces.h.orig	2026-05-30 14:27:46 UTC
+--- deps/VectorSimilarity/src/VecSim/spaces/spaces.h.orig	2026-07-12 13:34:36 UTC
 +++ deps/VectorSimilarity/src/VecSim/spaces/spaces.h
-@@ -39,6 +39,9 @@
+@@ -57,6 +57,9 @@ static inline auto getCpuOptimizationFeatures(const vo
  #if defined(CPU_FEATURES_ARCH_AARCH64)
      using FeaturesType = cpu_features::Aarch64Features;
      constexpr auto getFeatures = cpu_features::GetAarch64Info;
--- textproc/redisearch/files/patch-src_redisearch__rs_rqe__iterators_src_utils_timespec.rs
+++ textproc/redisearch/files/patch-src_redisearch__rs_rqe__iterators_src_utils_timespec.rs
@@ -1,7 +1,7 @@
---- src/redisearch_rs/c_entrypoint/iterators_ffi/src/timespec.rs.orig	2026-05-30 15:20:39 UTC
-+++ src/redisearch_rs/c_entrypoint/iterators_ffi/src/timespec.rs
-@@ -65,7 +65,9 @@ fn monotonic_now_timespec() -> ffi::timespec {
- 
+--- src/redisearch_rs/rqe_iterators/src/utils/timespec.rs.orig	2026-08-15 23:39:28 UTC
++++ src/redisearch_rs/rqe_iterators/src/utils/timespec.rs
+@@ -69,7 +69,9 @@ fn monotonic_now_timespec() -> ffi::timespec {
+
      // SAFETY: `&mut ts` is a valid, properly aligned, writable pointer to
      // `libc::timespec`, and `CLOCK_MONOTONIC_RAW` is a valid clock id.
 -    let rc = unsafe { libc::clock_gettime(libc::CLOCK_MONOTONIC_RAW, &mut ts) };
@@ -9,5 +9,5 @@
 +    #[cfg(target_os = "linux")] let clock = libc::CLOCK_MONOTONIC_RAW;
 +    let rc = unsafe { libc::clock_gettime(clock, &mut ts) };
      debug_assert_eq!(rc, 0);
- 
+
      ffi::timespec {
