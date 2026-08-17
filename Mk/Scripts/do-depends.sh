182618bbff520c3e07fe1c7bc844986722d2baa9
Baptiste Daroussin <bapt@FreeBSD.org>
2026-08-17 16:54:25 +0200

    Mk/Scripts/do-depends.sh: propagate DEBUG_MK_* to find-lib.sh
    env -i in find_lib() drops DEBUG_MK_SCRIPTS and
    DEBUG_MK_SCRIPTS_FIND_LIB, so find-lib.sh never enables tracing.
    Pass both through env -i.
    
    PR:		245438

--- Mk/Scripts/do-depends.sh
+++ Mk/Scripts/do-depends.sh
@@ -90,7 +90,9 @@ find_file_path()
 find_lib()
 {
 	echo -n "===>   ${dp_PKGNAME} depends on shared library: $1"
-	libfile=$(env -i PATH="${PATH}" LIB_DIRS="${dp_LIB_DIRS}" LOCALBASE="${dp_LOCALBASE}" ${dp_SH} ${dp_SCRIPTSDIR}/find-lib.sh $1)
+	libfile=$(env -i PATH="${PATH}" LIB_DIRS="${dp_LIB_DIRS}" LOCALBASE="${dp_LOCALBASE}" \
+	    DEBUG_MK_SCRIPTS="${DEBUG_MK_SCRIPTS:-}" DEBUG_MK_SCRIPTS_FIND_LIB="${DEBUG_MK_SCRIPTS_FIND_LIB:-}" \
+	    ${dp_SH} ${dp_SCRIPTSDIR}/find-lib.sh $1)
 	if [ -z "${libfile}" ]; then
 		echo " - not found"
 		return 1
