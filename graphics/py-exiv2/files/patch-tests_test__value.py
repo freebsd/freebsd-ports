bb862b9011aa982312ceeb44d499c449b4d870b3
Christos Longros <chris.longros@gmail.com>
2026-08-18 17:56:26 +0300

    graphics/py-exiv2: Update 0.18.1 => 0.19.0, take maintainership
    Changelog:
    1/ Add exiv2.XmpParser.encode() to Python interface.
    2/ Fix crash when getting strings of malformed values.
    3/ Python 3.15 compatibility.
    https://github.com/jim-easterbrook/python-exiv2/releases/tag/0.19.0
    https://github.com/jim-easterbrook/python-exiv2/blob/0.19.0/CHANGELOG.txt
    
    - Add STRIP_CMD in post-install.
    
    PR:		297603
    Sponsored by:	UNIS Labs
    Co-authored-by:	Vladimir Druzenko <vvd@FreeBSD.org>
    MFH:		2026Q3
    
    (cherry picked from commit 59d9d76281846c9e3427ebecd3da6b28aea5f631)

--- graphics/py-exiv2/files/patch-tests_test__value.py
+++ graphics/py-exiv2/files/patch-tests_test__value.py
@@ -1,8 +1,8 @@
 -- Skip failing 2039 date test which might be due to Y2038 or timezone issues.
 
---- tests/test_value.py.orig	2026-05-15 06:49:00 UTC
+--- tests/test_value.py.orig	2026-07-30 08:14:17 UTC
 +++ tests/test_value.py
-@@ -425,7 +425,7 @@ class TestValueModule(unittest.TestCase):
+@@ -440,7 +440,7 @@ class TestValueModule(unittest.TestCase):
          date = datetime.date(2024, 7, 4)
          with self.subTest(date=date):
              self.do_test_DateValue(date)
