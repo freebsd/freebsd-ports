5f4b643338ae0277647c5d85f89f74d223b4632a
Charlie Li <vishwin@FreeBSD.org>
2026-08-17 22:45:24 -0400

    graphics/opencv: use multimedia/v4l_compat on non-CURRENT
    Since video(4) et al landed in -CURRENT, the multimedia/v4l_compat
    headers conflict with <sys/videoio.h> when both are present. Support
    for <sys/videoio.h> is already included for NetBSD so ensure that
    this path is taken during configure on -CURRENT. Also ensure that
    the Linux-style unsigned int shorthand is defined for us in addition
    to OpenBSD.
    
    PR: 297454

--- graphics/opencv/files/patch-modules_videoio_src_cap__v4l.cpp
+++ graphics/opencv/files/patch-modules_videoio_src_cap__v4l.cpp
@@ -1,6 +1,6 @@
---- modules/videoio/src/cap_v4l.cpp.orig	2021-04-02 11:23:54 UTC
+--- modules/videoio/src/cap_v4l.cpp.orig	2025-12-30 07:52:05 UTC
 +++ modules/videoio/src/cap_v4l.cpp
-@@ -228,7 +228,9 @@ make & enjoy!
+@@ -227,16 +227,18 @@ make & enjoy!
  #include <poll.h>
  
  #ifdef HAVE_CAMV4L2
@@ -8,5 +8,16 @@
  #include <asm/types.h>          /* for videodev2.h */
 +#endif
  #include <linux/videodev2.h>
+ #endif
+ 
+ #ifdef HAVE_VIDEOIO
+-// NetBSD compatibility layer with V4L2
++// NetBSD and FreeBSD compatibility layer with V4L2
+ #include <sys/videoio.h>
+ #endif
+ 
+-#ifdef __OpenBSD__
++#if defined(__OpenBSD__) || defined(__FreeBSD__)
+ typedef uint32_t __u32;
  #endif
  
