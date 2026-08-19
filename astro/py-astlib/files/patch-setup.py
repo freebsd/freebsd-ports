86c3696a70fff22ca3e69a21aafdb74273d2de2c
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2026-08-19 20:25:02 +0800

    astro/py-astlib: Update to 0.13.2
    Changes:	https://github.com/mattyowl/astLib/releases

--- astro/py-astlib/files/patch-setup.py
+++ astro/py-astlib/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2023-10-19 08:41:05 UTC
+--- setup.py.orig	2026-02-12 09:30:47 UTC
 +++ setup.py
-@@ -55,7 +55,7 @@ class build_PyWCSTools_ext(build_ext):
+@@ -56,7 +56,7 @@ class build_PyWCSTools_ext(build_ext):
  
          build_ext.build_extensions(self)
  
 -setup(name='astLib',
 +setup(name='astlib',
-     version='0.11.10',
+     version='0.13.2',
      packages=['astLib', 'PyWCSTools'],
      package_data={'astLib': ['data/*']},
