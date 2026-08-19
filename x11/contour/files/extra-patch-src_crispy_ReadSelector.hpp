018e491b008a0d72b140972abd46a42feae02a52
Hiroki Tagato <tagattie@FreeBSD.org>
2026-08-19 16:09:59 +0900

    x11/contour: Update to 0.7.0.8982
    Changelog: https://github.com/contour-terminal/contour/releases/tag/v0.7.0.8982
    
    Reported by:	GitHub (watch releases)

--- x11/contour/files/extra-patch-src_crispy_ReadSelector.hpp
+++ x11/contour/files/extra-patch-src_crispy_ReadSelector.hpp
@@ -1,11 +1,11 @@
---- src/crispy/read_selector.h.orig	2026-01-06 17:41:07 UTC
-+++ src/crispy/read_selector.h
-@@ -107,7 +107,7 @@ class posix_read_selector
+--- src/crispy/ReadSelector.hpp.orig	2026-08-19 07:00:03 UTC
++++ src/crispy/ReadSelector.hpp
+@@ -131,7 +131,7 @@ class PosixReadSelector
          if (timeout.has_value())
          {
              tv = std::make_unique<timeval>(
 -                timeval { .tv_sec = timeout->count() / 1000,
-+                timeval { .tv_sec = static_cast<int>(timeout->count() / 1000),
++                timeval { .tv_sec = static_cast<time_t>(timeout->count() / 1000),
                            .tv_usec = static_cast<int>((timeout->count() % 1000) * 1000) });
          }
  
