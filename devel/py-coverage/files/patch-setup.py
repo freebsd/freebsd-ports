b0cad56a5d10db288a99bc8d4113ed9f57ae046a
Li-Wen Hsu <lwhsu@FreeBSD.org>
2026-08-22 04:15:49 +0800

    devel/py-coverage: Update to 7.15.4
    Rebase patch-setup.py onto the reorganized console_scripts block, and
    additionally guard the setuptools.command.editable_wheel import, which
    does not exist in current devel/py-setuptools (63.1.0).  Editable
    installs are not used when building the port, so falling back keeps the
    wheel build working.

--- devel/py-coverage/files/patch-setup.py
+++ devel/py-coverage/files/patch-setup.py
@@ -1,16 +1,34 @@
---- setup.py.orig	2024-07-31 02:05:53 UTC
+--- setup.py.orig	2026-08-21 20:04:07 UTC
 +++ setup.py
-@@ -89,12 +89,9 @@ setup_args = dict(
-         ],
-     },
-     entry_points={
--        # Install a script as "coverage", and as "coverage3", and as
--        # "coverage-3.7" (or whatever).
-+        # Install a script as "coverage".
+@@ -19,7 +19,12 @@ from setuptools.command.build_ext import build_ext
+ 
+ from setuptools import Extension, errors, setup
+ from setuptools.command.build_ext import build_ext
+-from setuptools.command.editable_wheel import editable_wheel
++try:
++    from setuptools.command.editable_wheel import editable_wheel
++except ImportError:
++    # setuptools < 64 has no editable_wheel command; editable installs are
++    # unsupported in that case, but regular wheel builds still work.
++    editable_wheel = object
+ 
+ 
+ def get_version_data() -> dict[str, Any]:
+@@ -187,9 +192,6 @@ setup_args = dict(
          "console_scripts": [
+             # Install a script as "coverage".
              "coverage = coverage.cmdline:main",
--            "coverage%d = coverage.cmdline:main" % sys.version_info[:1],
--            "coverage-%d.%d = coverage.cmdline:main" % sys.version_info[:2],
+-            # And as "coverage3", and as "coverage-3.7" (or whatever), but deprecated.
+-            "coverage%d = coverage.cmdline:main_deprecated" % sys.version_info[:1],
+-            "coverage-%d.%d = coverage.cmdline:main_deprecated" % sys.version_info[:2],
          ],
      },
      extras_require={
+@@ -198,7 +200,6 @@ setup_args = dict(
+     },
+     cmdclass={
+         "build_ext": ve_build_ext,
+-        "editable_wheel": EditableWheelWithPth,
+     },
+     # We need to get HTML assets from our htmlfiles directory.
+     zip_safe=False,
