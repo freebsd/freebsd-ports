68ca583ed72ad368a361c97e89f4531f405ec420
Christoph Moench-Tegeder <cmt@FreeBSD.org>
2026-08-21 20:00:19 +0200

    astro/qmapshack: update to 1.21.0
    Release Notes:
      https://github.com/Maproom/qmapshack/releases/tag/V_1.21.0

--- astro/qmapshack/files/patch-cmake_Modules_DefineInstallationPaths.cmake
+++ astro/qmapshack/files/patch-cmake_Modules_DefineInstallationPaths.cmake
@@ -1,20 +1,13 @@
---- cmake/Modules/DefineInstallationPaths.cmake.orig	2025-07-04 12:38:57 UTC
+diff --git cmake/Modules/DefineInstallationPaths.cmake cmake/Modules/DefineInstallationPaths.cmake
+index 66e60854..72cb3642 100644
+--- cmake/Modules/DefineInstallationPaths.cmake
 +++ cmake/Modules/DefineInstallationPaths.cmake
-@@ -51,7 +51,7 @@ if (UNIX)
-     CACHE PATH "The parent directory where applications can install their data (default prefix/share/${_PROJECT_NAME})"
-   )
-   SET(HTML_INSTALL_DIR
--    "${DATA_INSTALL_PREFIX}/doc/HTML"
-+	  "${DATA_INSTALL_PREFIX}/doc/${_PROJECT_NAME}/HTML"
-     CACHE PATH "The HTML install dir for documentation (default data/doc/html)"
-   )
-   SET(ICON_INSTALL_DIR
-@@ -82,7 +82,7 @@ if (UNIX)
-     CACHE PATH "The ${_PROJECT_NAME} sysconfig install dir (default prefix/etc)"
-   )
-   SET(MAN_INSTALL_DIR
--    "${SHARE_INSTALL_PREFIX}/man"
-+    "${CMAKE_INSTALL_PREFIX}/share/man"
-     CACHE PATH "The ${_PROJECT_NAME} man install dir (default prefix/man)"
-   )
-   SET(INFO_INSTALL_DIR
+@@ -20,7 +20,7 @@ if(WIN32)
+ else()
+     set(BIN_INSTALL_DIR     "${CMAKE_INSTALL_FULL_BINDIR}"                  CACHE PATH "Where the binaries go")
+     set(DATA_INSTALL_PREFIX "${CMAKE_INSTALL_FULL_DATAROOTDIR}"             CACHE PATH "Parent directory for application data")
+-    set(HTML_INSTALL_DIR    "${CMAKE_INSTALL_FULL_DATAROOTDIR}/doc/HTML"    CACHE PATH "Where the offline help goes")
++    set(HTML_INSTALL_DIR    "${CMAKE_INSTALL_FULL_DATAROOTDIR}/doc/qmapshack/HTML"    CACHE PATH "Where the offline help goes")
+     set(ICON_INSTALL_DIR    "${CMAKE_INSTALL_FULL_DATAROOTDIR}/icons"       CACHE PATH "Where the icon theme goes")
+     set(XDG_APPS_DIR        "${CMAKE_INSTALL_FULL_DATAROOTDIR}/applications" CACHE PATH "Where the .desktop files go")
+     set(MAN_INSTALL_DIR     "${CMAKE_INSTALL_FULL_MANDIR}"                  CACHE PATH "Where the man pages go")
