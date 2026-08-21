1de7b19da85181ab00eee3c1b4f9d8ba69157011
Gleb Popov <arrowd@FreeBSD.org>
2026-08-21 16:01:24 +0300

    security/blst: Update to 0.3.17
    PR:		288483

--- security/blst/files/patch-build.sh
+++ security/blst/files/patch-build.sh
@@ -1,9 +1,9 @@
---- build.sh.orig	2025-03-20 16:46:21 UTC
+--- build.sh.orig	2026-07-24 13:48:23 UTC
 +++ build.sh
-@@ -124,5 +124,6 @@ if [ $shared ]; then
+@@ -129,5 +129,5 @@ if [ $shared ]; then
+                 CFLAGS="${CFLAGS} -Wl,-Bsymbolic";;
      esac
      (set -x; ${CC} -shared -o $sharedlib \
-                    -Wl,--whole-archive,libblst.a,--no-whole-archive ${CFLAGS} \
-+		   -Wl,--soname=${sharedlib} \
-                    -Wl,-Bsymbolic)
+-                   -Wl,--whole-archive,libblst.a,--no-whole-archive ${CFLAGS})
++                   -Wl,--soname=${sharedlib} -Wl,--whole-archive,libblst.a,--no-whole-archive ${CFLAGS})
  fi
