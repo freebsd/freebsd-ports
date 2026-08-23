ee8b4413057c8c5853bfb61dffda18812d2b52b1
Nicola Vitale <nivit@FreeBSD.org>
2026-08-23 23:43:28 +0200

    www/py-urlgrabber: Update to 4.1.0
    - Convert to PEP517
    - Pet port(clippy|fmt)
    
    Reported by: repology.org

--- www/py-urlgrabber/files/patch-setup.py
+++ www/py-urlgrabber/files/patch-setup.py
@@ -1,22 +1,27 @@
---- setup.py.orig	2019-02-25 13:49:04 UTC
+--- setup.py.orig	2019-10-09 04:06:06 UTC
 +++ setup.py
-@@ -1,4 +1,5 @@
- # urlgrabber distutils setup
+@@ -1,8 +1,14 @@ from setuptools import setup
+ from setuptools import setup
 +import os
- import re as _re
- import sys as _sys
  
-@@ -32,9 +33,12 @@ packages = ['urlgrabber']
- package_dir = {'urlgrabber':'urlgrabber'}
- scripts = ['scripts/urlgrabber']
- data_files = [
--    ('share/doc/' + name + '-' + version, ['README','LICENSE', 'TODO', 'ChangeLog']),
-     ('libexec', ['scripts/urlgrabber-ext-down']),
- ]
-+if (sys.version_info.major == 2 and os.environ.has_key('INSTALL_DOCS')) or \
-+        (sys.version_info.major == 3 and os.environ.get('INSTALL_DOCS')):
-+    data_files.append(('%%DOCSDIR%%', ['README', 'TODO', 'ChangeLog']))
+ pkg_name = "urlgrabber"
+ pkg_version = "4.1.0"
+ 
++if os.environ.get('INSTALL_DOCS'):
++    docs_files = ('%%DOCSDIR%%', ['README', 'TODO', 'ChangeLog'])
++else:
++    docs_file = None
 +
- setup_requires = ['six']
- install_requires = ['pycurl', 'six']
- options = { 'clean' : { 'all' : 1 } }
+ setup(
+     name=pkg_name,
+     version=pkg_version,
+@@ -52,7 +58,8 @@ setup(
+     ],
+     scripts = ['scripts/urlgrabber'],
+     data_files = [
+-        ('share/doc/' + pkg_name + '-' + pkg_version, ['README','LICENSE', 'TODO', 'ChangeLog']),
+         ('libexec', ['scripts/urlgrabber-ext-down']),
++        docs_files
+    ],
++
+ )
