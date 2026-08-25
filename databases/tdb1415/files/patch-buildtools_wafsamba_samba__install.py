e8d4fc75699e3fccbacf0a2d6acbc0daf608698a
Jordan Ostreff <jordan@ostreff.info>
2026-08-25 14:50:06 +0700

    databases/tdb1415: new port
    New dependency for net/samba424
    
    PR:	294596
    Approved by:	samba (kiwi)

---
+++ databases/tdb1415/files/patch-buildtools_wafsamba_samba__install.py
@@ -0,0 +1,11 @@
+--- buildtools/wafsamba/samba_install.py.orig	2019-02-14 11:21:38 UTC
++++ buildtools/wafsamba/samba_install.py
+@@ -118,7 +118,7 @@ def install_library(self):
+                 inst_name    = bld.make_libname(t.target)
+         elif self.vnum:
+             vnum_base    = self.vnum.split('.')[0]
+-            install_name = bld.make_libname(target_name, version=self.vnum)
++            install_name = bld.make_libname(target_name, version=vnum_base)
+             install_link = bld.make_libname(target_name, version=vnum_base)
+             inst_name    = bld.make_libname(t.target)
+             if not self.private_library:
