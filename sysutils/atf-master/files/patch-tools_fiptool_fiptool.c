6a6e7a0c191225be06de5ce018f4929958e464bf
Emmanuel Vadot <manu@FreeBSD.org>
2026-08-28 08:17:42 +0200

    sysutils/atf: Update to 2.14.6
    Update to the latest LTS release.

--- sysutils/atf-master/files/patch-tools_fiptool_fiptool.c
+++ sysutils/atf-master/files/patch-tools_fiptool_fiptool.c
@@ -1,10 +1,11 @@
---- tools/fiptool/fiptool.c.orig	2023-09-22 09:29:57 UTC
+--- tools/fiptool/fiptool.c.orig	2026-07-31 14:13:28 UTC
 +++ tools/fiptool/fiptool.c
-@@ -4,6 +4,7 @@
+@@ -4,6 +4,8 @@
   * SPDX-License-Identifier: BSD-3-Clause
   */
  
 +#define __BSD_VISIBLE 1
- #ifndef _MSC_VER
++
+ #ifdef __linux__
  #include <sys/mount.h>
  #endif
