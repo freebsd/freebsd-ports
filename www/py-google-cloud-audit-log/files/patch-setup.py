28e325a60539b46756c23f62e305cd8be5c91870
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2026-08-31 13:21:08 +0800

    www/py-google-cloud-audit-log: Update to 0.6.1
    Changes:	https://github.com/googleapis/google-cloud-python/releases
    		https://github.com/googleapis/google-cloud-python/blob/main/packages/google-cloud-audit-log/CHANGELOG.md

--- www/py-google-cloud-audit-log/files/patch-setup.py
+++ www/py-google-cloud-audit-log/files/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2026-03-30 22:16:48 UTC
+--- setup.py.orig	2026-08-06 05:54:32 UTC
 +++ setup.py
-@@ -58,7 +58,7 @@ setuptools.setup(
+@@ -57,7 +57,7 @@ setuptools.setup(
      long_description_content_type="text/markdown",
      install_requires=dependencies,
      license="Apache-2.0",
 -    packages=find_namespace_packages(exclude=("tests*", "testing*")),
 +    packages=find_namespace_packages(exclude=("docs", "docs.*", "tests*", "testing*")),
-     python_requires=">=3.9",
-     url="https://github.com/googleapis/python-audit-log",
+     python_requires=">=3.10",
+     url="https://github.com/googleapis/google-cloud-python/tree/main/packages/google-cloud-audit-log",
      include_package_data=True,
