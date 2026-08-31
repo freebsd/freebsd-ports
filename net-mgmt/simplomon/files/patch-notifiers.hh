b379f1912df740d491b93ec9d69f9297bac3a649
Dave Cottlehuber <dch@FreeBSD.org>
2026-08-31 10:57:19 +0000

    net-mgmt/simplomon: Fix build on 16-CURRENT

--- net-mgmt/simplomon/files/patch-notifiers.hh
+++
@@ -1,8 +0,0 @@
---- notifiers.hh.orig	2026-05-09 08:36:59 UTC
-+++ notifiers.hh
-@@ -1,4 +1,5 @@
- #pragma once
-+#include <type_traits>
- #include <string>
- #include "sol/sol.hpp"
- #include "sclasses.hh"
---
+++ net-mgmt/simplomon/files/patch-sol_sol.hpp
@@ -0,0 +1,18 @@
+--- sol/sol.hpp.orig	2025-02-19 22:01:58 UTC
++++ sol/sol.hpp
+@@ -6747,12 +6747,10 @@ namespace sol {
+ 		/// one.
+ 		///
+ 		/// \group emplace
+-		template <class... Args>
+-		T& emplace(Args&&... args) noexcept {
+-			static_assert(std::is_constructible<T, Args&&...>::value, "T must be constructible with Args");
+-
++		T& emplace(T& arg) noexcept {
+ 			*this = nullopt;
+-			this->construct(std::forward<Args>(args)...);
++			m_value = &arg;
++			return **this;
+ 		}
+ 
+ 		/// Swaps this optional with the other.
