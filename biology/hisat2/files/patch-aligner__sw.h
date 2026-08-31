b426e0c5effa720eb5e80abac9d7eef4e3015eca
Jason W. Bacon <jwb@FreeBSD.org>
2026-08-31 10:52:06 -0500

    biology/hisat2: Update to 2.2.3
    No functional changes.  Upstream added explicit support for ARM,
    which is not relevant since the FreeBSD port has been using SIMDE
    patches for some time.  Still waiting on upstream to act:
    https://github.com/DaehwanKimLab/hisat2/pull/251
    
    Upgrading the port just to silence portscout.
    
    Reported by:    portscout

--- biology/hisat2/files/patch-aligner__sw.h
+++ biology/hisat2/files/patch-aligner__sw.h
@@ -1,17 +1,16 @@
---- aligner_sw.h.orig	2020-07-24 20:07:54 UTC
+--- aligner_sw.h.orig	2026-08-31 12:31:38 UTC
 +++ aligner_sw.h
-@@ -66,11 +66,13 @@
- 
- #define INLINE_CUPS
- 
-+#define	SIMDE_ENABLE_NATIVE_ALIASES
-+
-+#include <simde/x86/sse2.h>
- #include <stdint.h>
+@@ -70,11 +70,8 @@
  #include <iostream>
  #include <limits>
  #include "threading.h"
--#include <emmintrin.h>
+-#if defined(__aarch64__) || defined(__arm__) || defined(_M_ARM64)
+-#  include "sse2neon.h"
+-#else
+-#  include <emmintrin.h>
+-#endif
++#define SIMDE_ENABLE_NATIVE_ALIASES
++#include <simde/x86/sse2.h>
  #include "aligner_sw_common.h"
  #include "aligner_sw_nuc.h"
  #include "ds.h"
--- biology/hisat2/files/patch-sse__util.h
+++ biology/hisat2/files/patch-sse__util.h
@@ -1,17 +1,17 @@
---- sse_util.h.orig	2021-11-27 23:25:39 UTC
+--- sse_util.h.orig	2026-08-31 12:36:06 UTC
 +++ sse_util.h
-@@ -20,11 +20,13 @@
- #ifndef SSE_UTIL_H_
- #define SSE_UTIL_H_
- 
-+#define SIMDE_ENABLE_NATIVE_ALIASES
-+#include <simde/x86/sse2.h>
-+
- #include "assert_helpers.h"
+@@ -24,11 +24,9 @@
  #include "ds.h"
  #include "limit.h"
  #include <iostream>
--#include <emmintrin.h>
+-#if defined(__aarch64__) || defined(__arm__) || defined(_M_ARM64)
+-#  include "sse2neon.h"
+-#else
+-#  include <emmintrin.h>
+-#endif
++
++#define SIMDE_ENABLE_NATIVE_ALIASES
++#include <simde/x86/sse2.h>
  
  class EList_m128i {
  public:
