3ea9475a58b2f0906cd52fc4ced038f6e8086315
Jose Alonso Cardenas Marquez <acm@FreeBSD.org>
2026-09-01 22:11:39 -0700

    security/veracrypt: Fix build on aarch64

--- security/veracrypt/files/patch-src_Main_TextUserInterface.cpp
+++ security/veracrypt/files/patch-src_Main_TextUserInterface.cpp
@@ -2,9 +2,9 @@ TextUserInterface.cpp:122:5: error: const_cast from 'const wxScopedWCharBuffer' 
                                 const_cast <wchar_t *> (passwordStr.wc_str())[i] = L'X';
                                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
---- src/Main/TextUserInterface.cpp.orig	2017-07-09 22:31:19 UTC
+--- src/Main/TextUserInterface.cpp.orig	2026-06-09 10:55:00 UTC
 +++ src/Main/TextUserInterface.cpp
-@@ -119,7 +119,7 @@ namespace VeraCrypt
+@@ -172,7 +172,7 @@ namespace VeraCrypt
  			for (size_t i = 0; i < length && i < VolumePassword::MaxSize; ++i)
  			{
  				passwordBuf[i] = (wchar_t) passwordStr[i];
