efce6b376fbf93fcbee542f47dcea038080171ef
Piotr Kubaj <pkubaj@FreeBSD.org>
2026-09-04 15:45:09 +0200

    cad/brlcad: drop VSX requirement on powerpc64
    Merge upstream eigen patches to drop VSX requirement on powerpc64
    and build on G5.

---
+++ cad/brlcad/files/patch-src_other_Eigen_Eigen_src_Core_arch_AltiVec_PacketMath.h
@@ -0,0 +1,15 @@
+--- src/other/Eigen/Eigen/src/Core/arch/AltiVec/PacketMath.h.orig	2022-05-09 03:00:08 UTC
++++ src/other/Eigen/Eigen/src/Core/arch/AltiVec/PacketMath.h	2026-09-02 17:05:45 UTC
+@@ -1340,9 +1340,11 @@
+ template<> EIGEN_STRONG_INLINE Packet8bf psub<Packet8bf>(const Packet8bf& a, const Packet8bf& b) {
+   BF16_TO_F32_BINARY_OP_WRAPPER(psub<Packet4f>, a, b);
+ }
++
++template<> Packet4f psqrt<Packet4f>(const Packet4f& x);
+ 
+ template<> EIGEN_STRONG_INLINE Packet8bf psqrt<Packet8bf> (const Packet8bf& a){
+-  BF16_TO_F32_UNARY_OP_WRAPPER(vec_sqrt, a);
++  BF16_TO_F32_UNARY_OP_WRAPPER(psqrt<Packet4f>, a);
+ }
+ template<> EIGEN_STRONG_INLINE Packet8bf prsqrt<Packet8bf> (const Packet8bf& a){
+   BF16_TO_F32_UNARY_OP_WRAPPER(prsqrt<Packet4f>, a);
