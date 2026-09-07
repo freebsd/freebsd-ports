3ae52a12d808a798c14706679437c1b85ed8c94a
Hiroki Tagato <tagattie@FreeBSD.org>
2026-09-07 14:57:08 +0900

    net-im/teams: Update to 2.20.0
    Changelog:
    - https://github.com/IsmaelMartinez/teams-for-linux/releases/tag/v2.19.0
    - https://github.com/IsmaelMartinez/teams-for-linux/releases/tag/v2.20.0
    
    Reported by:	portscout

--- net-im/teams/files/patch-scripts_afterpack.js
+++ net-im/teams/files/patch-scripts_afterpack.js
@@ -1,6 +1,6 @@
---- scripts/afterpack.js.orig	2025-07-28 16:14:12 UTC
+--- scripts/afterpack.js.orig	2026-09-03 16:52:25 UTC
 +++ scripts/afterpack.js
-@@ -14,7 +14,7 @@ function getAppFileName(context) {
+@@ -15,7 +15,7 @@ function getAppFileName(context) {
        return `${productFileName}.app`;
      case "mas":
        return `${productFileName}.app`;
@@ -9,12 +9,12 @@
        return context.packager.executableName;
      default:
        return "";
-@@ -24,7 +24,7 @@ exports.default = async function afterPack(context) {
+@@ -25,7 +25,7 @@ exports.default = async function afterPack(context) {
  exports.default = async function afterPack(context) {
    try {
      // Ensure release info is generated for Linux publishing
 -    if (context.electronPlatformName === "linux") {
 +    if (context.electronPlatformName === "linux" || context.electronPlatformName === "freebsd") {
        await generateReleaseInfoForLinux();
+       await patchSnapLauncherIfNeeded(context);
      }
- 
