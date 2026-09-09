b91ea28798d937fd902228d93afcc3907bb48a78
Po-Chuan Hsieh <sunpoet@FreeBSD.org>
2026-09-09 22:37:50 +0800

    devel/orc: Update to 0.4.43
    Changes:	https://gitlab.freedesktop.org/gstreamer/orc/-/blob/main/RELEASE

--- devel/orc/files/patch-orc-orccpu_powerpc.c
+++ devel/orc/files/patch-orc-orccpu_powerpc.c
@@ -1,9 +1,9 @@
---- orc/orccpu-powerpc.c.orig	2023-05-31 21:29:36 UTC
+--- orc/orccpu-powerpc.c.orig	2026-08-28 18:57:31 UTC
 +++ orc/orccpu-powerpc.c
-@@ -55,6 +55,7 @@
+@@ -58,6 +58,7 @@
  #endif
  
- #if defined(__FreeBSD__) || defined(__APPLE__) || defined(__NetBSD__)
+ #if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__)
 +#include <stdio.h>
  #include <sys/types.h>
  #include <sys/sysctl.h>
