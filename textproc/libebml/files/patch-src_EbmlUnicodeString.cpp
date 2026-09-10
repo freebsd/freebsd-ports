78685217acb211a316cba393bdca44d0e5f47b4b
Max Brazhnikov <makc@FreeBSD.org>
2026-09-10 17:17:53 +0300

    textproc/libebml: Update to 1.4.7

--- textproc/libebml/files/patch-src_EbmlUnicodeString.cpp
+++ textproc/libebml/files/patch-src_EbmlUnicodeString.cpp
@@ -1,11 +1,11 @@
---- src/EbmlUnicodeString.cpp.orig	2023-12-17 02:35:17 UTC
+--- src/EbmlUnicodeString.cpp.orig	2026-07-28 06:10:04 UTC
 +++ src/EbmlUnicodeString.cpp
-@@ -40,7 +40,7 @@
+@@ -15,7 +15,7 @@
  
  #include "ebml/EbmlUnicodeString.h"
  
--#include "lib/utf8-cpp/source/utf8/checked.h"
-+#include "utf8cpp/utf8/checked.h"
+-#include <utf8/checked.h>
++#include <utf8cpp/utf8/checked.h>
  
  namespace libebml {
  
