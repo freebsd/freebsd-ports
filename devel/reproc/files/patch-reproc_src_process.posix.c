964bdf7385a5e959c61f95114510579db40961dc
Yuri Victorovich <yuri@FreeBSD.org>
2026-09-11 16:51:36 -0700

    devel/reproc: update 14.2.7 → 14.2.8

--- devel/reproc/files/patch-reproc_src_process.posix.c
+++ devel/reproc/files/patch-reproc_src_process.posix.c
@@ -1,20 +1,21 @@
 -- patch to fix tests and maybe run-time failures
 
---- reproc/src/process.posix.c	2026-04-15 03:47:51.000000000 -0700
-+++ reproc/src/process.posix.c	2026-04-15 23:44:29.815332000 -0700
-@@ -1,4 +1,6 @@
+--- reproc/src/process.posix.c.orig	2026-08-28 12:05:59 UTC
++++ reproc/src/process.posix.c
+@@ -1,5 +1,7 @@
  #define _POSIX_C_SOURCE 200809L
 +#include <pthread.h>
-+
  
++
  #include "process.h"
  
-@@ -100,7 +102,7 @@
-   return cwd;
- }
+ #include <dirent.h>
+@@ -13,6 +15,8 @@
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <unistd.h>
++
++#include <sys/stat.h>
  
--static const int MAX_FD_LIMIT = 1024 * 1024;
-+static const int MAX_FD_LIMIT = 1024 * 1024 * 2;
- 
- static int get_max_fd(void)
- {
+ #include "error.h"
+ #include "macro.h"
