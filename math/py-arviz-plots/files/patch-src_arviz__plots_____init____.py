7c614e3a8844427e5925e3e671c51c9200c22bcb
Yuri Victorovich <yuri@FreeBSD.org>
2026-09-10 22:20:33 -0700

    math/py-arviz-plots: update 1.2.0 → 1.3.1

--- math/py-arviz-plots/files/patch-src_arviz__plots_____init____.py
+++ math/py-arviz-plots/files/patch-src_arviz__plots_____init____.py
@@ -1,8 +1,9 @@
---- src/arviz_plots/__init__.py.orig	2026-04-17 17:39:26 UTC
+-- Add arviz_darkgrid_template to the del block after the plotly templates are added to prevent namespace leakage.
+--- src/arviz_plots/__init__.py.orig	2026-09-10 20:53:17 UTC
 +++ src/arviz_plots/__init__.py
-@@ -330,6 +330,14 @@ try:
-     pio.templates["arviz-vibrant"] = arviz_vibrant_template
+@@ -335,6 +335,15 @@ try:
      pio.templates["arviz-tumma"] = arviz_tumma_template
+     pio.templates["arviz-darkgrid"] = arviz_darkgrid_template
  
 +    del (
 +        pio,
@@ -11,6 +12,7 @@
 +        arviz_variat_template,
 +        arviz_vibrant_template,
 +        arviz_tumma_template,
++        arviz_darkgrid_template,
 +    )
  
  except ImportError:
--- math/py-arviz-plots/files/patch-tests_conftest.py
+++ math/py-arviz-plots/files/patch-tests_conftest.py
@@ -1,10 +1,11 @@
---- tests/conftest.py.orig	2025-04-17 17:07:02 UTC
+-- Remove Hypothesis deadline to prevent spurious test failures on slow build machines.
+--- tests/conftest.py.orig	2026-09-10 20:53:50 UTC
 +++ tests/conftest.py
-@@ -13,7 +13,7 @@ _log = logging.getLogger("arviz_plots")
+@@ -18,7 +18,7 @@ _log = logging.getLogger("arviz_plots")
  
  _log = logging.getLogger("arviz_plots")
  
--settings.register_profile("fast", deadline=5000, max_examples=15)
+-settings.register_profile("fast", deadline=2000, max_examples=15)
 +settings.register_profile("fast", deadline=None, max_examples=15)
  settings.register_profile("chron", deadline=5000, max_examples=500)
  settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "fast"))
