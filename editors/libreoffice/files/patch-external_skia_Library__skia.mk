7431ef214ba05b08805bec2bd369c5224990a09c
Dima Panov <fluffy@FreeBSD.org>
2026-09-13 23:26:34 +0300

    editors/libreoffice: fix some crashes and VCL selection (+)
    * Fix crash with SAL_USE_VCLPLUGIN=gen and enabled Skia [1]
    * Allow Qt VCL plugin when LO started outside the known DE (e.g., pure X) [2]
    
    PR:	298257 [1], 298227, [2]

--- editors/libreoffice/files/patch-external_skia_Library__skia.mk
+++ editors/libreoffice/files/patch-external_skia_Library__skia.mk
@@ -1,6 +1,6 @@
---- external/skia/Library_skia.mk.orig	2022-12-22 00:14:09 UTC
+--- external/skia/Library_skia.mk.orig	2026-08-21 12:51:44 UTC
 +++ external/skia/Library_skia.mk
-@@ -96,7 +96,7 @@ $(eval $(call gb_Library_use_externals,skia,\
+@@ -109,7 +109,7 @@ $(eval $(call gb_Library_use_externals,skia,\
      libpng \
  ))
  
@@ -9,3 +9,12 @@
  $(eval $(call gb_Library_add_libs,skia,\
      -lm \
      -ldl \
+@@ -297,7 +297,7 @@ $(eval $(call gb_Library_add_generated_exception_objec
+ #
+ $(eval $(call gb_Library_add_generated_exception_objects,skia,\
+     UnpackedTarball/skia/tools/window/WindowContext \
+-	$(if $(filter LINUX,$(OS)),UnpackedTarball/skia/tools/window/unix/RasterWindowContext_unix) \
++	$(if $(filter DRAGONFLY FREEBSD LINUX NETBSD OPENBSD,$(OS)),UnpackedTarball/skia/tools/window/unix/RasterWindowContext_unix) \
+     $(if $(filter WNT,$(OS)),UnpackedTarball/skia/tools/window/win/RasterWindowContext_win) \
+ ))
+ ifeq ($(SKIA_GPU),METAL)
--- editors/libreoffice/files/patch-vcl_source_app_salplug.cxx
+++ editors/libreoffice/files/patch-vcl_source_app_salplug.cxx
@@ -1,6 +1,6 @@
 --- vcl/source/app/salplug.cxx.orig	2026-06-16 11:40:38 UTC
 +++ vcl/source/app/salplug.cxx
-@@ -184,8 +184,14 @@ std::vector<OUString> autodetect_plugin_list()
+@@ -184,14 +184,26 @@ std::vector<OUString> autodetect_plugin_list()
          if (eDesktop == DesktopType::Plasma6)
              aPlugins.push_back(u"kf6"_ustr);
  #endif
@@ -9,9 +9,21 @@
 +#endif
  #if ENABLE_KF5
          aPlugins.push_back(u"kf5"_ustr);
+ #endif
++#if ENABLE_QT5
++        aPlugins.push_back(u"qt5"_ustr);
++#endif
+ #if ENABLE_GTK3_KDE5
+         aPlugins.push_back(u"gtk3_kde5"_ustr);
+ #endif
+     }
+ 
++#if ENABLE_QT6
++        aPlugins.push_back(u"qt6"_ustr);
 +#endif
 +#if ENABLE_QT5
 +        aPlugins.push_back(u"qt5"_ustr);
++#endif
+ #if ENABLE_GTK3
+     aPlugins.push_back(u"gtk3"_ustr);
  #endif
- #if ENABLE_GTK3_KDE5
-         aPlugins.push_back(u"gtk3_kde5"_ustr);
