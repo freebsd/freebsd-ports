07679900322e1a1ac7aca93d7fae94be87adde90
Mathieu Arnold <mat@FreeBSD.org>
2026-09-14 15:31:19 +0200

    Mk: add a BUILD_RUN_DEPENDS variable
    Add BUILD_RUN_DEPENDS as a syntastic sugar for adding dependencies to
    both BUILD_DEPENDS and RUN_DEPENDS without side effects.
    
    Differential Revision:	https://reviews.freebsd.org/D59664

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -4043,6 +4043,11 @@ package-noinstall: package
 # Dependency checking
 ################################################################
 
+.    for sp in ${_PKGS}
+BUILD_DEPENDS${_SP.${sp}}+=		${BUILD_RUN_DEPENDS${_SP.${sp}}}
+RUN_DEPENDS${_SP.${sp}}+=		${BUILD_RUN_DEPENDS${_SP.${sp}}}
+.    endfor
+
 .    if !target(depends)
 depends: pkg-depends extract-depends patch-depends lib-depends fetch-depends build-depends run-depends
 
