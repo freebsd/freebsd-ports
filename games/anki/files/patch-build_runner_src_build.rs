65434f71218e5a8114ea642231acaeb609ced7bd
Kai Knoblich <kai@FreeBSD.org>
2026-09-14 19:41:16 +0200

    games/anki: Update to 26.09
    * Migrate to Yarn v4 to be in sync with upstream.  To ensure that the
      preinstalled Yarn v4 is actually used, a few minor workarounds and a
      new variable PREINSTALLED_YARN are required.
    
      The latter is necessary because, with Yarn v2 and newer, environment
      variables prefixed with YARN_ can no longer be used.
    
    * Sort pkg-plist while I'm here.
    
    Changelog:
    
    https://github.com/ankitects/anki/releases/tag/26.09
    
    MFH:		2026Q3
    Security:	ce54d883-b060-11f1-b187-901b0edee044

--- games/anki/files/patch-build_runner_src_build.rs
+++ games/anki/files/patch-build_runner_src_build.rs
@@ -1,9 +1,9 @@
 * Make ninja build verbose
 * Make cargo build for components of the build framework (= runner) more verbose
 
---- build/runner/src/build.rs.orig	2024-02-29 18:06:02 UTC
+--- build/runner/src/build.rs.orig	2026-08-25 18:50:38 UTC
 +++ build/runner/src/build.rs
-@@ -57,6 +57,7 @@ pub fn run_build(args: BuildArgs) {
+@@ -59,6 +59,7 @@ pub fn run_build(args: BuildArgs) {
      let start_time = Instant::now();
      let mut command = Command::new(get_ninja_command());
      command
@@ -11,12 +11,12 @@
          .arg("-f")
          .arg(&build_file)
          .args(ninja_args)
-@@ -150,7 +151,7 @@ fn bootstrap_build() {
+@@ -149,7 +150,7 @@ fn bootstrap_build() {
  
  fn bootstrap_build() {
      let status = Command::new("cargo")
--        .args(["run", "-p", "configure"])
-+        .args(["run", "-p", "configure", "--verbose"])
+-        .args(["run", "-p", "configure", "--profile", bootstrap_profile()])
++        .args(["run", "-p", "configure", "--verbose", "--profile", bootstrap_profile()])
          .status();
      assert!(status.expect("ninja").success());
  }
