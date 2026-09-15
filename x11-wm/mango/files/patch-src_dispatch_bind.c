ac90bfda353b358b04e676452323ef75b2db6438
Nicola Vitale <nivit@FreeBSD.org>
2026-09-15 23:28:47 +0200

    x11-wm/mango: Update to 0.17.1
    - Update list of lib dependencies
    
    ChangeLog:
    
    1. https://github.com/mangowm/mango/releases/tag/0.17.1
    
    Reported by:	DreamMaoMao <notifications@github.com>

--- x11-wm/mango/files/patch-src_dispatch_bind.c
+++ x11-wm/mango/files/patch-src_dispatch_bind.c
@@ -1,11 +1,11 @@
---- src/dispatch/bind.c.orig	2026-09-12 12:52:03 UTC
+--- src/dispatch/bind.c.orig	2026-09-15 13:40:08 UTC
 +++ src/dispatch/bind.c
-@@ -1224,7 +1224,7 @@ static void close_inherited_fds(void) {
+@@ -1243,7 +1243,7 @@ static void close_inherited_fds(void) {
+ }
  
  static void close_inherited_fds(void) {
- #ifdef SYS_close_range
 -	extern long syscall(long number, ...);
-+	extern long syscall(int number, ...);
- 	if (syscall(SYS_close_range, 3, ~0U, 0) == 0)
- 		return;
- #endif
++	extern int syscall(int number, ...);
+ 	syscall(SYS_close_range, 3, ~0U, 0);
+ }
+ 
