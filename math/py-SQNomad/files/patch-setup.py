ed8403f487c89db137152b73ba1c81145a0f6e4c
Yuri Victorovich <yuri@FreeBSD.org>
2026-09-15 20:15:29 -0700

    math/py-SQNomad: Unbreak

--- math/py-SQNomad/files/patch-setup.py
+++ math/py-SQNomad/files/patch-setup.py
@@ -1,7 +1,18 @@
---- setup.py.orig	2023-11-12 00:10:52 UTC
+-- Use numpy.get_include() to find NumPy's C headers instead of building
+-- the path manually, because NumPy >= 2.0 moved its C API headers from
+-- numpy/core/include to numpy/_core/include, breaking the previous
+-- hardcoded path.
+-- Also enable the FreeBSD platform check that was missing so the C++14
+-- flag is applied on FreeBSD as well as Linux/macOS.
+
+--- setup.py.orig	2021-03-29 23:39:12 UTC
 +++ setup.py
-@@ -55,7 +55,7 @@ class my_build_extension(_build_ext):
-         ext.extra_compile_args += ['-I'+os.path.join(numpy.__path__[0], 'core/include/numpy')]
+@@ -52,10 +52,10 @@ class my_build_extension(_build_ext):
+ 
+         # adding numpy late to allow setup to install it in the build env
+         import numpy
+-        ext.extra_compile_args += ['-I'+os.path.join(numpy.__path__[0], 'core/include/numpy')]
++        ext.extra_compile_args += ['-I'+numpy.get_include(), '-I'+os.path.join(numpy.get_include(), 'numpy')]
  
          # force C++14
 -        if 'linux' in sys.platform or 'darwin' in sys.platform:
