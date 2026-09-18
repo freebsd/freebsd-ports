7e2f1f94fca157d8b63a55fd721b3e0715f43271
Hiroki Tagato <tagattie@FreeBSD.org>
2026-09-19 03:41:16 +0900

    devel/electron42: Update to 42.11.5
    Changelog: https://github.com/electron/electron/releases/tag/v42.11.5
    
    Reported by:	GitHub (watch releases)

--- devel/electron42/files/patch-electron_spec_chromium-spec.ts
+++ devel/electron42/files/patch-electron_spec_chromium-spec.ts
@@ -1,6 +1,6 @@
---- electron/spec/chromium-spec.ts.orig	2026-09-08 07:41:45 UTC
+--- electron/spec/chromium-spec.ts.orig	2026-09-17 03:11:02 UTC
 +++ electron/spec/chromium-spec.ts
-@@ -604,15 +604,15 @@ describe('command line switches', () => {
+@@ -606,15 +606,15 @@ describe('command line switches', () => {
        testLocale('asdfkl', `${currentLocale}|${currentSystemLocale}|${currentPreferredLanguages}`));
  
      const lcAll = String(process.env.LC_ALL);
@@ -20,7 +20,7 @@
        testLocale('', lcAll, true)
      );
    });
-@@ -4234,12 +4234,12 @@ describe('font fallback', () => {
+@@ -4236,12 +4236,12 @@ describe('font fallback', () => {
        expect(fonts[0].familyName).to.equal('Arial');
      } else if (process.platform === 'darwin') {
        expect(fonts[0].familyName).to.equal('Helvetica');
@@ -35,7 +35,7 @@
      'should fall back to Japanese font for sans-serif Japanese script',
      async function () {
        const html = `
-@@ -4928,7 +4928,7 @@ describe('paste execCommand', () => {
+@@ -4930,7 +4930,7 @@ describe('paste execCommand', () => {
    });
  });
  
