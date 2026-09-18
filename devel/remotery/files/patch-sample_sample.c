3d84f52ff09b1b8565a79221444009cf25e8880d
Mateusz Piotrowski <0mp@FreeBSD.org>
2026-09-18 20:25:22 +0200

    devel/remotery: Update to 1.2.1

--- devel/remotery/files/patch-sample_sample.c
+++ devel/remotery/files/patch-sample_sample.c
@@ -1,11 +1,11 @@
---- sample/sample.c.orig	2019-02-01 11:18:44 UTC
+--- sample/sample.c.orig	2022-07-01 20:35:19 UTC
 +++ sample/sample.c
 @@ -2,7 +2,7 @@
  #include <math.h>
  #include <signal.h>
  #include <stdio.h>
--#include "..\lib\Remotery.h"
+-#include "../lib/Remotery.h"
 +#include <Remotery.h>
  
  void aggregateFunction() {
-     rmt_BeginCPUSample(aggregate, RMTSF_Aggregate);
+     rmt_BeginCPUSample(aggregate, RMTSF_Aggregate);    
