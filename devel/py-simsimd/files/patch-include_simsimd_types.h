86fda279501145fbce684d840d5b9d3cf276f49c
Yuri Victorovich <yuri@FreeBSD.org>
2026-09-19 15:29:18 -0700

    devel/py-simsimd: fix build on arm64
    Reported by:	fallout

--- devel/py-simsimd/files/patch-include_simsimd_types.h
+++ devel/py-simsimd/files/patch-include_simsimd_types.h
@@ -1,12 +1,16 @@
 -- fix bug that __bfloat16 isn't defined
+-- guard the immintrin.h include to x86/i386 only, since it is an x86-specific
+-- header and unconditionally including it breaks the build on arm64
 
---- include/simsimd/types.h.orig	2026-08-02 22:31:43 UTC
+--- include/simsimd/types.h.orig	2026-03-07 14:05:46 UTC
 +++ include/simsimd/types.h
-@@ -326,6 +326,8 @@ typedef unsigned short simsimd_f16_t;
+@@ -326,6 +326,10 @@ typedef unsigned short simsimd_f16_t;
  typedef unsigned short simsimd_f16_t;
  #endif
  
++#if defined(__x86_64__) || defined(__i386__)
 +#include <immintrin.h>
++#endif
 +
  #if !defined(SIMSIMD_NATIVE_BF16) || SIMSIMD_NATIVE_BF16
  /**
