534e9aa094889b75681dc85a45df7c849ad5a5e9
Gerard Seibert <gerard_seibert@outlook.com>
2026-09-25 00:31:13 +0300

    net/phpldapadmin: Fix PHP 8.4 XML callback and E_STRICT deprecations
    PR:		298807
    Approved by:	Krzysztof <ports@bsdserwis.com> (maintainer)
    Approved by:	osa, vvd (Mentors, implicit)
    MFH:		2026Q3

--- net/phpldapadmin/files/patch-lib_functions.php
+++ net/phpldapadmin/files/patch-lib_functions.php
@@ -1,5 +1,14 @@
 --- lib/functions.php.orig	2024-01-10 22:23:54 UTC
 +++ lib/functions.php
+@@ -145,7 +145,7 @@ function app_error_handler($errno,$errstr,$file,$linen
+ 	$errtype = '';
+ 
+ 	switch ($errno) {
+-		case E_STRICT: $errtype = 'E_STRICT'; break;
++		case E_DEPRECATED: $errtype = 'E_DEPRECATED'; break;
+ 		case E_ERROR: $errtype = 'E_ERROR'; break;
+ 		case E_WARNING: $errtype = 'E_WARNING'; break;
+ 		case E_PARSE: $errtype = 'E_PARSE'; break;
 @@ -359,7 +359,7 @@ function memory_str_to_int($value) {
  	$value = trim(strtolower($value));
  	if (intval($value) > 0 && preg_match('/^(\d+)([kmg])?$/', $value, $match, PREG_UNMATCHED_AS_NULL)) {
