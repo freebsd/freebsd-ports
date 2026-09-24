58c14f51d849c02e18e4e3d89d393c2f9eb3ea5d
Gerard Seibert <gerard_seibert@outlook.com>
2026-09-25 00:31:13 +0300

    net/phpldapadmin: Fix PHP 8.4 XML callback and E_STRICT deprecations
    PR:		298807
    Approved by:	Krzysztof <ports@bsdserwis.com> (maintainer)
    Approved by:	osa, vvd (Mentors, implicit)
    MFH:		2026Q3
    
    (cherry picked from commit 534e9aa094889b75681dc85a45df7c849ad5a5e9)

--- net/phpldapadmin/files/patch-lib_xml2array.php
+++ net/phpldapadmin/files/patch-lib_xml2array.php
@@ -1,6 +1,19 @@
---- lib/xml2array.php.orig	2026-06-05 14:24:55 UTC
+--- lib/xml2array.php.orig	2024-01-10 22:23:54 UTC
 +++ lib/xml2array.php
-@@ -51,7 +51,7 @@ class xml2array {
+@@ -33,10 +33,9 @@ class xml2array {
+ 
+ 	public function parseXML($strInputXML,$filename) {
+ 		$this->resParser = xml_parser_create();
+-		xml_set_object($this->resParser,$this);
+-		xml_set_element_handler($this->resParser,'tagOpen','tagClosed');
++		xml_set_element_handler($this->resParser,[$this, 'tagOpen'],[$this, 'tagClosed']);
+ 
+-		xml_set_character_data_handler($this->resParser,'tagData');
++		xml_set_character_data_handler($this->resParser,[$this, 'tagData']);
+ 
+ 		$this->push_pos($this->arrOutput);
+ 
+@@ -48,7 +47,7 @@ class xml2array {
  				xml_get_current_line_number($this->resParser),
  				$filename));
  
