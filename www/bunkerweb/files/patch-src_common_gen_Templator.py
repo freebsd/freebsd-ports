379784574f45e219b4e85fa06e658ea8c825dc7a
Jochen Neumeister <joneum@FreeBSD.org>
2026-09-24 17:01:56 +0200

    www/bunkerweb: Enable ModSecurity when the module is available
    Render the ModSecurity directives only if
    ngx_http_modsecurity_module.so is installed, like upstream does for
    the stream module. Build www/openresty with the MODSECURITY option to
    get it.
    
    Point the remaining paths at their FreeBSD locations, error pages,
    assets and rule files were read from /usr/share/bunkerweb.
    
    Sponsored by:	Netzkommune GmbH

--- www/bunkerweb/files/patch-src_common_gen_Templator.py
+++ www/bunkerweb/files/patch-src_common_gen_Templator.py
@@ -1,6 +1,23 @@
---- src/common/gen/Templator.py.orig	2026-05-28 20:12:40 UTC
+--- src/common/gen/Templator.py.orig	2026-09-21 08:05:39 UTC
 +++ src/common/gen/Templator.py
-@@ -650,6 +650,7 @@ class Templator:
+@@ -9,7 +9,7 @@
+ from glob import glob
+ from math import ceil
+ import multiprocessing as mp
+-from os.path import basename, join, sep
++from os.path import basename, isfile, join, sep
+ from pathlib import Path
+ from random import choice
+ from ssl import PROTOCOL_TLS_SERVER, SSLContext
+@@ -441,6 +441,7 @@
+             "import": import_module,
+             "resolve_ssl_ecdh_curve": resolve_ssl_ecdh_curve,
+             "normalize_memory_size": Templator._normalize_memory_size,
++            "has_modsecurity": isfile("/usr/local/nginx/modules/ngx_http_modsecurity_module.so"),
+         }
+ 
+         self._server_env_cache: Dict[str, Environment] = {}
+@@ -721,6 +722,7 @@
          template_vars = self._base_template_vars.copy()
          template_vars["all"] = full_config
          template_vars.update(config)
