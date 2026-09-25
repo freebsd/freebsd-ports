649f5c8840cdbfbcb2353ba5fdea91be8d1de9d5
Guido Falsi <madpilot@FreeBSD.org>
2026-09-25 22:20:30 +0200

    net/ntopng: Update to 7.0 branch

--- net/ntopng/files/patch-autogen.sh
+++ net/ntopng/files/patch-autogen.sh
@@ -1,4 +1,4 @@
---- autogen.sh.orig	2025-12-28 10:02:12 UTC
+--- autogen.sh.orig	2026-09-22 07:53:34 UTC
 +++ autogen.sh
 @@ -8,31 +8,6 @@ PKG_CONFIG=$(command -v pkg-config)
  AUTORECONF=$(command -v autoreconf)
@@ -32,22 +32,10 @@
  ##########################################
  
  TODAY=`date +%y%m%d`
-@@ -74,20 +49,3 @@ rm -f config.h config.h.in *~ #*
-     > configure.ac
+@@ -84,8 +59,3 @@ fi
+ fi
  
- rm -f config.h config.h.in *~ #*
--
--git submodule init
--git submodule update --remote
--
--if test -d "pro"; then
--N=`grep __FreeBSD__ third-party/clickhouse-cpp/clickhouse/base/platform.h|wc -l|tr -d "[[:space:]]"`
--if test "${N}" = "0"; then
-- cd third-party/clickhouse-cpp/; cat ../../clickhouse/clickhouse-cpp.diff | patch -p1 ; cd ../..
--fi
--fi
--
--# git submodule update --init --recursive
+ # git submodule update --init --recursive
 -
 -echo "Wait please..."
 -autoreconf -if
