4b2ff5266c239dfe67c43f78df7838dd99da22cb
Joachim Werner <jockl@pianojockl.org>
2026-09-26 16:52:10 +0300

    games/bsdgames: Update 0.76 => 0.77
    This release fixes segmentation faults after reloading a save game.
    
    While here:
    - Drop backported patches since they are in now upstream.
    - Regenerate tetris patch with `make makepatch` (stage-qa).
    
    Commit log:
    https://github.com/pianojockl/bsdgames/compare/v0.76...v0.77
    
    PR:		281947
    Approved by:	osa, vvd (Mentors, implicit)
    MFH:		2026Q3

--- games/bsdgames/files/patch-tetris_screen.c
+++ games/bsdgames/files/patch-tetris_screen.c
@@ -1,10 +1,10 @@
---- ./tetris/screen.c.ORIG	2024-09-17 22:29:09.000000000 +0200
-+++ ./tetris/screen.c	2026-05-12 07:56:04.845628000 +0200
+--- tetris/screen.c.orig	2026-09-26 10:54:51 UTC
++++ tetris/screen.c
 @@ -48,6 +48,7 @@
  #include <stdlib.h>
  #include <string.h>
  #include <term.h>
 +#include <termios.h>
  #include <unistd.h>
-
+ 
  #include "screen.h"
